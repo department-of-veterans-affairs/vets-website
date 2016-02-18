cb8e00eff303a0eb0731939f863c414754036c08
Joseph Stewart <joseph.stewart.va@gmail.com>
2016-02-18 10:05:25 -0500

    Migrate 3 spouse fields.

--- spec/javascripts/health-care/health-care.spec.js
+++ spec/javascripts/health-care/health-care.spec.js
@@ -198,6 +198,9 @@ describe("Health Care Form", function() {
     form.elements['veteran[children][net_business_income]'].value = "43.00";
     form.elements['veteran[children][other_income]'].value = "32.00";
     form.elements['veteran[children][education_expenses]'].value = "11235.00";
+    form.elements['veteran[spouses][last_name]'].value = "SpouseLast";
+    form.elements['veteran[spouses][first_name]'].value = "SpouseFirst";
+    form.elements['veteran[spouses][middle_name]'].value = "SpouseMiddle";
 
     $.get('/base/spec/fixtures/javascripts/health-care/anonymous-submission.xml')
       .done(function(data) {
@@ -250,4 +253,3 @@ describe("Health Care Form", function() {
     expect(form.elements['veteran[children][other_income]'].dataset.validationType).to.be.equal('monetary');
   });
 });
-
