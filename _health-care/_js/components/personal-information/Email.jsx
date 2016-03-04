5a3ca959b4fa714014b079b1f3d502bed28ccff5
akainic <annekainic@gmail.com>
2016-03-04 11:25:20 -0500

    Create email component and coresponding tests

---
+++ _health-care/_js/components/personal-information/Email.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import ErrorableTextInput from '../form-elements/ErrorableTextInput';
+import { isValidEmail } from '../../utils/validations.js';
+
+/**
+ * Input component for collecting a Email number.
+ *
+ * Validates the input data. Does NOT consider "invalid" email numbers such
+ * as 000-000-0000 to be errors. This is to allow integration testing with
+ * fake data.
+ *
+ * Props:
+ * `error` - Function that returns a string. Only necessary for confirming email.
+ * `label` - String. Not required as a prop, can be passed directly to
+ *            ErrorableTextInput
+ * `value` - String. Stores the email address.
+ * `onValueChange` - a function with this prototype: (newValue)
+ */
+class Email extends React.Component {
+  render() {
+    let errorMessage = undefined;
+    if (this.props.error !== undefined) {
+      errorMessage = this.props.error;
+    } else {
+      errorMessage = isValidEmail(this.props.value) ? undefined : 'Please put your email in this format x@x.xxx';
+    }
+    return (
+      <div className="usa-input-grid usa-input-grid-large">
+        <ErrorableTextInput
+            errorMessage={errorMessage}
+            label={this.props.label}
+            placeholder="x@x.xxx"
+            value={this.props.value}
+            onValueChange={this.props.onValueChange}/>
+      </div>
+    );
+  }
+}
+
+Email.propTypes = {
+  error: React.PropTypes.string,
+  label: React.PropTypes.string,
+  value: React.PropTypes.string.isRequired,
+  onValueChange: React.PropTypes.func.isRequired,
+};
+
+export default Email;
--- _health-care/_js/utils/validations.js
+++ _health-care/_js/utils/validations.js
@@ -17,8 +17,13 @@ function isValidPhone(value) {
   return /^\d{3}-\d{3}-\d{4}$/.test(value);
 }
 
+function isValidEmail(value) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value);
+}
+
 export {
   isValidDate,
   isValidSSN,
-  isValidPhone
+  isValidPhone,
+  isValidEmail
 };
