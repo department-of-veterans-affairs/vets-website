eabeb8e0ca6e64441a65d7929768a7dab7667a6a
akainic <annekainic@gmail.com>
2016-03-08 10:13:25 -0500

    Reactify Financial Assessment panel
    FinancialAssessment panel partially complete, reorder files

--- spec/javascripts/health-care/components/FinancialAssessmentPanel.spec.jsx
+++ spec/javascripts/health-care/components/FinancialAssessmentPanel.spec.jsx
@@ -5,7 +5,8 @@ import FinancialAssessmentPanel from '../../../../_health-care/_js/components/Fi
 
 describe('<FinancialAssessmentPanel>', () => {
   it('Sanity check the component renders', () => {
-    const tree = SkinDeep.shallowRender(<FinancialAssessmentPanel/>);
+    const tree = SkinDeep.shallowRender(
+      <FinancialAssessmentPanel applicationData={{ financialAssessment: '' }}/>);
     const vdom = tree.getRenderOutput();
     expect(vdom).to.have.property('type', 'div');
   });
