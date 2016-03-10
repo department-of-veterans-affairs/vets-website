fd64b769e0ac13e0c8d9d18a2f886ceaa88d1597
Courtney Eimerman-Wallace <crwallace39@gmail.com>
2016-03-10 13:59:17 -0500

    Fixed new linting errors on service information section.

--- _health-care/_js/components/military-service/ServiceInformationSection.jsx
+++ _health-care/_js/components/military-service/ServiceInformationSection.jsx
@@ -11,35 +11,43 @@ class ServiceInformationSection extends React.Component {
       <div>
         <div className="row">
           <div className="small-12 columns">
-            <h4>Service Information</h4>
+            <h4>
+              Service Information
+            </h4>
 
-            <ErrorableSelect label="Last branch of service"
-                  options={branchesServed}
-                  value={this.props.data.lastServiceBranch}
-                  onUserInput={(update) => {this.props.onStateChange('lastServiceBranch', update);}}/>
+            <ErrorableSelect
+              label="Last branch of service"
+              options={branchesServed}
+              value={this.props.data.lastServiceBranch}
+              onUserInput={(update) => {this.props.onStateChange('lastServiceBranch', update);}}/>
           </div>
           <div className="small-12 columns">
-              <p>Last entry date</p>
-              <DateInput
-                  day={this.props.data.lastEntryDate.day}
-                  month={this.props.data.lastEntryDate.month}
-                  year={this.props.data.lastEntryDate.year}
-                  onValueChange={(update) => {this.props.onStateChange('lastEntryDate', update);}}/>
-            </div>
+            <p>
+              Last entry date
+            </p>
+            <DateInput
+              day={this.props.data.lastEntryDate.day}
+              month={this.props.data.lastEntryDate.month}
+              year={this.props.data.lastEntryDate.year}
+              onValueChange={(update) => {this.props.onStateChange('lastEntryDate', update);}}/>
+          </div>
           <div className="small-12 columns">
-              <p>Last discharge date</p>
-              <DateInput
-                  day={this.props.data.lastDischargeDate.day}
-                  month={this.props.data.lastDischargeDate.month}
-                  year={this.props.data.lastDischargeDate.year}
-                  onValueChange={(update) => {this.props.onStateChange('lastDischargeDate', update);}}/>
-            </div>
+            <p>
+              Last discharge date
+            </p>
+            <DateInput
+              day={this.props.data.lastDischargeDate.day}
+              month={this.props.data.lastDischargeDate.month}
+              year={this.props.data.lastDischargeDate.year}
+              onValueChange={(update) => {this.props.onStateChange('lastDischargeDate', update);}}/>
+          </div>
           <div className="small-12 columns">
-              <ErrorableSelect label="Discharge Type"
-                options={dischargeTypes}
-                value={this.props.data.dischargeType}
-                onUserInput={(update) => {this.props.onStateChange('dischargeType', update);}}/>
-            </div>
+            <ErrorableSelect
+              label="Discharge Type"
+              options={dischargeTypes}
+              value={this.props.data.dischargeType}
+              onUserInput={(update) => {this.props.onStateChange('dischargeType', update);}}/>
+          </div>
         </div>
       </div>
     );
