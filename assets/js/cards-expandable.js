8c8228d9106656c45a6000ecb1774fe1b491c2f5
Tiffany Brown <tiffany@webinista.com>
2016-03-28 12:31:37 -0700

    Switching e.target.nodeName to $(e.target).is()
    Also renamed:    assets/js/vendor/cards-expandable.js -> assets/js/cards-expandable.js

--- assets/js/cards-expandable.js
+++ assets/js/cards-expandable.js
@@ -1,11 +1,11 @@
 // JS for expandable cards (.va-card--expands)
 $(document).ready(function() {
 	$('.va-card-body').on('click', function(e){
-    	if(e.target.nodeName == 'DIV') {
+    	if( $(e.target).is('DIV') ) {
     		$(e.currentTarget).parent().toggleClass('va-card--hasdrawer--open');
     	}
     	// If the drawer is open, disable links in the card's body
-    	if(e.target.nodeName == 'A' && $(e.currentTarget).parent().hasClass('va-card--hasdrawer--open')) {
+    	if($(e.target).is('A') && $(e.currentTarget).parent().hasClass('va-card--hasdrawer--open')) {
     		e.preventDefault();
     	}
 	});
