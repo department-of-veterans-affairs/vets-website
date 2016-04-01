b17fe56051047df54f275c045a2ce01b2ed4b457
Albert J. Wong <awong.dev@gmail.com>
2016-03-31 22:40:54 -0700

    Add debug panel and <RoutesDropdown>
    <RoutesDropdown> is a debug navigation element for easily accessing any
    page in the site.

--- _health-care/_js/components/HealthCareApp.jsx
+++ _health-care/_js/components/HealthCareApp.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { hashHistory } from 'react-router';
+import _ from 'lodash';
 
 import IntroductionSection from './IntroductionSection.jsx';
 import Nav from './Nav.jsx';
+import PopulateVeteranButton from './debug/PopulateVeteranButton';
+import PerfPanel from './debug/PerfPanel';
 import ProgressButton from './ProgressButton';
+import RoutesDropdown from './debug/RoutesDropdown';
 import { ensureFieldsInitialized } from '../actions';
 import { pathToData } from '../store';
-import PopulateVeteranButton from './debug/PopulateVeteranButton';
 
 import * as validations from '../utils/validations';
 
 class HealthCareApp extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.handleBack = this.handleBack.bind(this);
     this.handleContinue = this.handleContinue.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -52,7 +54,7 @@ class HealthCareApp extends React.Component {
 
     this.context.store.dispatch(ensureFieldsInitialized(path));
     if (validations.isValidSection(path, sectionData)) {
-      hashHistory.push(this.getUrl('next'));
+      this.context.router.push(this.getUrl('next'));
       if (document.getElementsByClassName('progress-box').length > 0) {
         document.getElementsByClassName('progress-box')[0].scrollIntoView();
       }
@@ -65,7 +67,7 @@ class HealthCareApp extends React.Component {
   }
 
   handleBack() {
-    hashHistory.push(this.getUrl('back'));
+    this.context.router.push(this.getUrl('back'));
     if (document.getElementsByClassName('progress-box').length > 0) {
       document.getElementsByClassName('progress-box')[0].scrollIntoView();
     }
@@ -132,6 +134,19 @@ class HealthCareApp extends React.Component {
         </div>
       );
     }
+    let devPanel = undefined;
+    // TODO(awong): Wrap to disable on production sites.
+    const queryParams = _.fromPairs(
+      window.location.search.substring(1).split('&').map((v) => { return v.split('='); }));
+    if (queryParams.devPanel === '1') {
+      devPanel = (
+        <div className="row">
+          <RoutesDropdown/>
+          <PopulateVeteranButton/>
+          <PerfPanel/>
+        </div>
+      );
+    }
 
     return (
       <div className="row">
@@ -139,9 +154,9 @@ class HealthCareApp extends React.Component {
           <Nav currentUrl={this.props.location.pathname}/>
         </div>
         <div className="medium-8 columns">
+          {devPanel}
           <div className="progress-box">
             <div className="form-panel">
-              <PopulateVeteranButton/>
               {children}
               {buttons}
             </div>
@@ -154,6 +169,9 @@ class HealthCareApp extends React.Component {
 
 // TODO(awong): Hack to allow access to the store for now while migrating.
 // All uses of this.context.store in this file are WRONG!!!
-HealthCareApp.contextTypes = { store: React.PropTypes.object };
+HealthCareApp.contextTypes = {
+  router: React.PropTypes.object.isRequired,
+  store: React.PropTypes.object
+};
 
 export default HealthCareApp;
