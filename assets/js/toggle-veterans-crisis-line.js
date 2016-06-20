7f43d28cd95e85eff9f3502323daeb4a62b23fde
Tiffany Brown <tiffany@webinista.com>
2016-06-20 16:02:33 -0700

    Changes resulting out of design review.
    - Removing div.touch from footer, in favor of hiding below a certain viewport width.
    - Changing bottom position from rem to px for more precision.
    - Added `position: relative` to .footer to constrain VCL placement at wide viewports
    - Switched animation to use transforms. Better perf in WebKit browsers.
    - Removing console.log from toggle-veterans-crisis-line.js
    - Adding width, height attributes to up, down arrow and close SVG icons (fixes rendering issues in IE9)
    - Making VCL right-aligned with the content area.

--- assets/js/toggle-veterans-crisis-line.js
+++ assets/js/toggle-veterans-crisis-line.js
@@ -1,8 +1,11 @@
 $(function() {
   var toggleVCL = function(e) {
     e.preventDefault();
-    console.log( $(this).parents('.va-crisis-panel').toggleClass('va-crisis-panel--open') );
+    // Only affects Desktop version of the Veterans Crisis line.
+    // Mobile is handled via menu.js and works with all overlays.
+    $(this).parents('.va-crisis-panel').toggleClass('va-crisis-panel--open');
   }
   $('.va-crisis-line--notouch').on('click', toggleVCL);
 });
+
 
