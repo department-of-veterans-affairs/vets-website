587e70e2c097a025170c0abd5c50ccca9f2ec4a3
Tiffany Brown <tiffany@webinista.com>
2016-06-22 13:09:21 -0700

    Turns off draggability for Facility Locator map. closes #2479

--- facility-locator/map.js
+++ facility-locator/map.js
@@ -13,7 +13,9 @@ FacilityLocator = (function() {
       directionsStart,
       geolocated = false; // Used when determining starting location for driving directions
       zoomLevelToShowMarkers = 5;
-      infowindows = {};
+      infowindows = {},
+      // Checks whether the matchMedia is a function (supported) and whether this is a small screen
+      isSmallScreen = !(typeof matchMedia === 'function') && (matchMedia('(max-width: 40.063em)').matches);
 
   FacilityLocator.prototype.init = function() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -22,7 +24,8 @@ FacilityLocator = (function() {
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       mapTypeControl: false,
       maxZoom: 12,
-      scrollwheel:  false
+      scrollwheel:  false,
+      draggable: isSmallScreen
     });
 
     // Load facilities
