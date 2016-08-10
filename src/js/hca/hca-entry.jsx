a28c62a7f0f47310fe7d9213edd25acf89428aa9
Albert J. Wong <awong.dev@gmail.com>
2016-08-10 15:48:07 -0700

    Add back in hca SCSS.

--- src/js/hca/hca-entry.jsx
+++ src/js/hca/hca-entry.jsx
@@ -10,6 +10,8 @@ import initReact from '../common/init-react';
 import reducer from './reducers';
 import routes from './routes.jsx';
 
+require('../../sass/hca.scss');
+
 const store = createStore(reducer);
 
 const browserHistory = useRouterHistory(createHistory)({
