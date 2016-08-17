adcf8893afe8ba66814645ec7f01eea7b3c5c077
Albert J. Wong <awong.dev@gmail.com>
2016-08-17 09:03:12 -0400

    Enable nightwatch tests.
    Use PhantomJS and port over HCA's mock API server plus one e2e test for
    the proof of concept.

--- src/js/hca/components/HealthCareApp.jsx
+++ src/js/hca/components/HealthCareApp.jsx
@@ -118,7 +118,7 @@ class HealthCareApp extends React.Component {
       this.props.onUpdateSubmissionStatus('submitPending');
 
       // POST data to endpoint
-      fetch('/api/hca/v1/application', {
+      fetch(`${__API_URL__}/api/hca/v1/application`, {
         method: 'POST',
         headers: {
           Accept: 'application/json',
