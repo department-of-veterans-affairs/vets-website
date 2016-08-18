24067cfbf345ff4f1ddf2fcf6db0efb7a2a09971
Eugene Doan <doan.eugene@gmail.com>
2016-08-18 19:33:01 -0400

    Added action to load single Rx and handler in Rx reducer.

--- src/js/rx/actions/prescriptions.js
+++ src/js/rx/actions/prescriptions.js
@@ -1,4 +1,15 @@
-export function loadData() {
+export function loadData(id = null) {
+  if (id) {
+    // TODO: Remove this line when API can retrieve any individual Rx.
+    id = 1435525;
+    return dispatch => fetch(`/api/prescriptions/${id}`)
+      .then(res => res.json())
+      .then(
+        data => dispatch({ type: 'LOAD_PRESCRIPTION_SUCCESS', data }),
+        err => dispatch({ type: 'LOAD_PRESCRIPTION_FAILURE', err })
+      );
+  }
+
   return dispatch => fetch('/api/prescriptions')
     .then(res => res.json())
     .then(
