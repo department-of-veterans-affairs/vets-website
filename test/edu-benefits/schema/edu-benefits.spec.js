b0f2dd75a3c8033429822c47c64bb2b85bc37819
Lihan Li <frankieteardrop@gmail.com>
2016-08-18 17:59:54 -0400

    working on edu benefit ssn validation spec

--- test/edu-benefits/schema/edu-benefits.spec.js
+++ test/edu-benefits/schema/edu-benefits.spec.js
@@ -1,5 +1,24 @@
 import { expect } from 'chai';
 import schema from '../../../src/js/edu-benefits/schema/edu-benefits';
+import Ajv from 'ajv';
 
 describe('education benefits json schema', () => {
+  let ajv;
+  let validateSchema = (data) => {
+    return ajv.validate('edu-benefits-schema', data);
+  };
+  let expectValidData = (data) => {
+    expect(validateSchema(data)).to.be.true;
+  };
+
+  before(() => {
+    ajv = new Ajv();
+    ajv.addSchema(schema, 'edu-benefits-schema');
+  });
+
+  context('ssn validations', () => {
+    it('should allow an ssn with no dashes', () => {
+      expectValidData({ socialSecurityNumber: "123456789" });
+    });
+  });
 });
