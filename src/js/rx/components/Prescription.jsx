c234b05fdf2627d39ba432b4e544c2ba12c5c7ca
Tiffany Brown <tiffany@webinista.com>
2016-08-18 13:28:36 -0700

    Restoring Facility name field to card.

--- src/js/rx/components/Prescription.jsx
+++ src/js/rx/components/Prescription.jsx
@@ -57,6 +57,9 @@ class Prescription extends React.Component {
           <div className="rx-prescription-number">
             Rx #: {attrs['prescription-id']}
           </div>
+          <div className="rx-prescription-facility">
+            Facility name: {attrs['facility-name']}
+          </div>
           <div className="rx-prescription-refilled">
             Last refilled: {moment(attrs['refill-date']).format('ll')}
           </div>
