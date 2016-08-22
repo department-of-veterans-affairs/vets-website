d790f63755b238a902b404c1dd126096b9d08c68
Courtney Eimerman-Wallace <crwallace39@gmail.com>
2016-08-22 15:33:20 -0400

    adds sign in button

---
+++ src/js/common/components/SignInProfileButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SignInProfileButton extends React.Component {
+  render() {
+    return (
+      <div className="va-action-bar--header">
+        <div className="row">
+          <div className="small-12 columns">
+            <a className="usa-button-primary" href="#">Sign In</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SignInProfileButton;
--- src/js/user-profile/containers/Main.jsx
+++ src/js/user-profile/containers/Main.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import SignInProfileButton from '../../common/components/SignInProfileButton';
 import TabNav from '../components/TabNav';
 
 class Main extends React.Component {
   render() {
     return (
-      <div className="rx-app row">
-        <TabNav/>
-        {this.props.children}
+      <div>
+        <SignInProfileButton/>
+        <div className="rx-app row">
+          <TabNav/>
+          {this.props.children}
+        </div>
       </div>
     );
   }
