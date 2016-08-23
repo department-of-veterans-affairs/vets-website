da1266df06aa427322ff9e44b69f276d62632d39
Tiffany Brown <webinista@users.noreply.github.com>
2016-08-23 13:27:09 -0700

    Adding Disclaimer box. closes #2802 (#2839)
    * Adding Disclaimer box. closes #2802
    
    * Fixing lint errors.
    
    * Somehow forgot to attach the actions, visibility.
    
    * Removing OPEN_DISCLAIMER, openDisclaimer action and reducer.
    
    Not necessary, since it should/will be open by default.

--- src/js/rx/containers/Main.jsx
+++ src/js/rx/containers/Main.jsx
@@ -2,22 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { closeAlert } from '../actions/alert.js';
+import { closeDisclaimer } from '../actions/disclaimer.js';
 
 import AlertBox from '../components/AlertBox';
 import TabNav from '../components/TabNav';
+import Disclaimer from '../components/Disclaimer';
 
 class Main extends React.Component {
   render() {
     return (
-      <div className="rx-app row">
-        <AlertBox
-            content={this.props.alert.content}
-            isVisible={this.props.alert.visible}
-            onCloseAlert={this.props.closeAlert}
-            status={this.props.alert.status}/>
-        <h1>Mail Order Prescriptions</h1>
-        <TabNav/>
-        {this.props.children}
+      <div>
+        <Disclaimer
+            isVisible={this.props.disclaimer.visible}
+            handleClose={this.props.closeDisclaimer}/>
+        <div className="rx-app row">
+          <AlertBox
+              content={this.props.alert.content}
+              isVisible={this.props.alert.visible}
+              onCloseAlert={this.props.closeAlert}
+              status={this.props.alert.status}/>
+          <h1>Mail Order Prescriptions</h1>
+          <TabNav/>
+          {this.props.children}
+        </div>
       </div>
     );
   }
@@ -27,4 +34,7 @@ const mapStateToProps = (state) => {
   return state;
 };
 
-export default connect(mapStateToProps, { closeAlert })(Main);
+export default connect(mapStateToProps, {
+  closeAlert,
+  closeDisclaimer
+})(Main);
