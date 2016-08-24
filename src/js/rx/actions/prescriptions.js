718a86b7c21fb72e58107d13f8a71a25948db587
Eugene Doan <doan.eugene@gmail.com>
2016-08-24 12:46:37 -0400

    Fixed variable naming to camel-case.

--- src/js/rx/actions/prescriptions.js
+++ src/js/rx/actions/prescriptions.js
@@ -2,8 +2,8 @@ export function loadData(id) {
   if (id) {
     // TODO: Use id param instead of test id
     // when API is able to retrieve any individual Rx.
-    const test_id = 1435525;
-    return dispatch => fetch(`/api/prescriptions/${test_id}`)
+    const testId = 1435525;
+    return dispatch => fetch(`/api/prescriptions/${testId}`)
       .then(res => res.json())
       .then(
         data => dispatch({ type: 'LOAD_PRESCRIPTION_SUCCESS', data }),
