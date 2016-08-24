700a42244a1e329a34dbe7a87f3512d9e36188fb
Eugene Doan <doan.eugene@gmail.com>
2016-08-23 22:10:02 -0400

    Hard-coding Rx id instead of reassigning to function arg. Temp fix to get Travis build working.

--- src/js/rx/actions/prescriptions.js
+++ src/js/rx/actions/prescriptions.js
@@ -1,8 +1,8 @@
 export function loadData(id) {
   if (id) {
-    // TODO: Remove this line when API can retrieve any individual Rx.
-    id = 1435525;
-    return dispatch => fetch(`/api/prescriptions/${id}`)
+    // TODO: Use id param instead of hard-coded id
+    // when API is able to retrieve any individual Rx.
+    return dispatch => fetch(`/api/prescriptions/1435525`)
       .then(res => res.json())
       .then(
         data => dispatch({ type: 'LOAD_PRESCRIPTION_SUCCESS', data }),
