f551ba50878588f6dc5b99b746893267025e3b73
Tiffany Brown <webinista@users.noreply.github.com>
2016-08-23 18:23:09 -0700

    Adds GlossaryPage component, route. closes #2805. (#2848)
    - Added GlossaryList component that can be used for any definition list.
    - Changing BackLink component to accept a text string prop.
    - Adding a glossary file with placeholder text.
    
    Adding glossary styles.

---
+++ src/js/rx/glossary.js
@@ -0,0 +1,18 @@
+const glossary = [
+  {
+    term: 'dog',
+    definition: 'mammal with four legs. loves people.'
+  },
+  {
+    term: 'cow',
+    definition: 'mammal with four legs. tasty when cooked.'
+  },
+  {
+    term: 'cat',
+    definition: 'mammal with four legs. plots world domination. Beatae praesentium quidem repellendus consequatur qui quae. Aut minima dolorem dolore. Natus porro non quia quaerat est illo. Soluta eos quia voluptatem. Dicta ullam et sint non.\nVelit beatae odit itaque id dolor. Sed eos deserunt occaecati. Qui reprehenderit ut dolores cupiditate aperiam dolor unde. Possimus occaecati saepe in exercitationem totam.\nConsequatur libero dolor repudiandae ipsam occaecati alias. Provident ut ab laborum asperiores a aut eum aliquid. Earum non dolor totam.\nConsequatur sapiente asperiores fugiat itaque est cumque aut. Ut enim eaque dolores numquam labore omnis eum vel. Aliquid dolore debitis eveniet quaerat vel veniam consectetur. Illum ut sint culpa cumque unde. Et vitae nihil et. Ut et dolorum dolorum.\nEt neque officiis rerum repellendus adipisci illo aperiam. Illum cumque est consequatur aut eum corrupti laborum illo. Non pariatur aliquam hic iste. Id sunt et numquam. Qui non occaecati quod expedita dolorem mollitia. Iusto eos vel nobis iure.'
+  }
+];
+
+export function getGlossary() {
+  return glossary;
+}
