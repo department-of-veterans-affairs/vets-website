e6173deef40f7bc773468d9715250fa6f132b0e1
Eugene Doan <U-DON@users.noreply.github.com>
2016-08-25 18:00:53 -0400

    Updated Rx Detail Page with Data from API (#2846)
    * add API proxy to unblock rx development
    
    * Fixing Prescription card layout.
    
    - Also removed a constructor that's no longer necessary since the code changed.
    
    * Removing unused variable.
    
    * remove tabs
    
    * Added action to load single Rx and handler in Rx reducer.
    
    * Updated Rx detail page with table and data from API.
    
    * Removed console.log line.
    
    * Updated ContactCard styles that were lost through the merge.
    
    * Skip checking for is-refillable since it's currently always false.
    
    * Formatting and styling changes for contents of detail page.
    
    * Added detail page stylesheet partial. Minor whitespace fixes.
    
    * Don't render AlertBox component at all (not even empty div) if not visible.
    
    * Added title header to detail page.
    
    * Removed overridden header style.
    
    * Fixes to pass eslint checks in Travis build.
    
    * Hard-coding Rx id instead of reassigning to function arg. Temp fix to get Travis build working.
    
    * Fix to pass string quoting error on Travis build. Assign test id to var instead of hard-coding for loading single Rx data.
    
    * Fixed variable naming to camel-case.
    
    * Changed cssClass prop on TableVerticalHeader to className. PropType validation on the same prop on TrackPackageLink.
    
    * Fixed missing comma.
    
    * Reorganized some CSS classes for table in Rx detail page.
    
    * Replaced tracking button on Rx card with TrackPackageLink.
    
    * remove unused variable
    
    * Revert "remove unused variable"
    
    This reverts commit 93d6b776daa9078aade0b70e78b5a32a0312242c.
    
    * Replaced tracking button on Rx card with TrackPackageLink.
    
    * Revert "Replaced tracking button on Rx card with TrackPackageLink."
    
    This reverts commit da3e4ae1a7956f03eed7aa9bf3ff6e39f4861c8b.
    
    * Fixed mistake in merge.
    
    * Fixing another error from merge.

--- src/js/rx/reducers/prescriptions.js
+++ src/js/rx/reducers/prescriptions.js
@@ -1,11 +1,16 @@
 import set from 'lodash/fp/set';
 
-const initialState = { items: [] };
+const initialState = {
+  currentItem: null,
+  items: []
+};
 
 export default function prescriptions(state = initialState, action) {
   switch (action.type) {
     case 'LOAD_PRESCRIPTIONS_SUCCESS':
       return set('items', action.data.data, state);
+    case 'LOAD_PRESCRIPTION_SUCCESS':
+      return set('currentItem', action.data.data, state);
     default:
       return state;
   }
