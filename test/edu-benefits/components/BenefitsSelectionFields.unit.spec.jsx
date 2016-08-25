8cfbf7b21c9fbdc18b58b30a4b062509441e2c54
Jeff Balboni <jeff@adhocteam.us>
2016-08-25 17:10:25 -0400

    More tests

--- test/edu-benefits/components/BenefitsSelectionFields.unit.spec.jsx
+++ test/edu-benefits/components/BenefitsSelectionFields.unit.spec.jsx
@@ -25,7 +25,10 @@ describe('<BenefitsSelectionFields>', () => {
     expect(tree.everySubTree('RadioButtonsSubSection')[0].props.showIfValueChosen).to.equal('chapter33');
   });
   it('should render four checkboxes', () => {
-    expect(tree.everySubTree('ErrorableCheckbox').length).to.equal(4);
+    expect(tree.everySubTree('ErrorableCheckbox')[0].props.name).to.equal('chapter30Relinquished');
+    expect(tree.everySubTree('ErrorableCheckbox')[1].props.name).to.equal('chapter1606Relinquished');
+    expect(tree.everySubTree('ErrorableCheckbox')[2].props.name).to.equal('chapter1607Relinquished');
+    expect(tree.everySubTree('ErrorableCheckbox')[3].props.name).to.equal('notEligible');
   });
   it('should render a value for benefits chosen', () => {
     expect(tree.everySubTree('ErrorableRadioButtons')[0].props.value.value).to.equal(data.benefitsChosen.value);
