819e0f104d6a1c4b0a1566b6b47f7bec2f12f610
Eugene Doan <doan.eugene@gmail.com>
2016-08-26 16:50:38 -0400

    Added OrderHistory to Detail page.

--- src/js/rx/containers/Detail.jsx
+++ src/js/rx/containers/Detail.jsx
@@ -6,6 +6,7 @@ import moment from 'moment';
 import { loadData } from '../actions/prescriptions.js';
 import BackLink from '../components/BackLink';
 import ContactCard from '../components/ContactCard';
+import OrderHistory from '../components/OrderHistory';
 import TableVerticalHeader from '../components/tables/TableVerticalHeader';
 
 class Detail extends React.Component {
@@ -16,42 +17,52 @@ class Detail extends React.Component {
   render() {
     let header;
     let content;
+    let orderHistory;
     const item = this.props.prescriptions.currentItem;
 
     if (item) {
-      const attrs = item.attributes;
-      const data = {
-        Quantity: attrs.quantity,
-        // 'Prescription status': attrs[''],
-        'Prescription date': moment(
-            attrs['refill-submit-date']
-          ).format('D MMM YYYY'),
-        'Expiration date': moment(
-            attrs['expiration-date']
-          ).format('D MMM YYYY'),
-        'Prescription #': attrs['prescription-number'],
-        Refills: (
-          <span>
-            {attrs['refill-remaining']} left
-            &nbsp;&nbsp;&nbsp;<a>Refill prescription</a>
-          </span>
-        )
-      };
+      if (item.rx) {
+        const attrs = item.rx.attributes;
+        const data = {
+          Quantity: attrs.quantity,
+          // 'Prescription status': attrs[''],
+          'Prescription date': moment(
+              attrs['refill-submit-date']
+            ).format('D MMM YYYY'),
+          'Expiration date': moment(
+              attrs['expiration-date']
+            ).format('D MMM YYYY'),
+          'Prescription #': attrs['prescription-number'],
+          Refills: (
+            <span>
+              {attrs['refill-remaining']} left
+              &nbsp;&nbsp;&nbsp;<a>Refill prescription</a>
+            </span>
+          )
+        };
 
-      header = (
-        <h2 className="rx-detail-header">
-          {attrs['prescription-name']}
-        </h2>
-      );
+        header = (
+          <h2 className="rx-detail-header">
+            {attrs['prescription-name']}
+          </h2>
+        );
 
-      content = (
-        <div>
-          <TableVerticalHeader
-              className="usa-table-borderless rx-table"
-              data={data}/>
-          <ContactCard/>
-        </div>
-      );
+        content = (
+          <div>
+            <TableVerticalHeader
+                className="usa-table-borderless rx-table"
+                data={data}/>
+            <ContactCard/>
+          </div>
+        );
+      }
+
+      if (item.trackings) {
+        orderHistory = (
+          <OrderHistory
+              items={item.trackings}/>
+        );
+      }
     }
 
     return (
@@ -60,6 +71,7 @@ class Detail extends React.Component {
         <BackLink text="Back to list"/>
         {header}
         {content}
+        {orderHistory}
       </div>
     );
   }
