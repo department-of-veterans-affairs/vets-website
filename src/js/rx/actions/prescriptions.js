eb4e57b4adacff2cf78c8409fee06ea2088cd238
Eugene Doan <doan.eugene@gmail.com>
2016-08-29 15:32:18 -0400

    Improved readability of fetch for Rx and tracking history with separate lines.

--- src/js/rx/actions/prescriptions.js
+++ src/js/rx/actions/prescriptions.js
@@ -5,8 +5,12 @@ export function loadData(id) {
     const testId = 1435525;
     const rxUrl = `/rx-api/prescriptions/${testId}`;
     const rxUrls = [rxUrl, `${rxUrl}/trackings`];
+    // Fetch both the prescription and its tracking history and
+    // wait for retrieval and read of both resources to resolve.
     return dispatch => {
-      Promise.all(rxUrls.map(url => fetch(url).then(res => res.json()))).then(
+      Promise.all(
+        rxUrls.map(url => fetch(url).then(res => res.json()))
+      ).then(
         data => dispatch({
           type: 'LOAD_PRESCRIPTION_SUCCESS',
           data: { rx: data[0].data, trackings: data[1].data }
