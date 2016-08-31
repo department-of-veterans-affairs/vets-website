0e3932e2fe3fafb26d856feaca451e226a1c7321
Alexander Ose <alex@alexose.com>
2016-08-31 16:50:01 -0700

    add form components

--- src/js/edu-benefits/components/RotcHistoryFields.jsx
+++ src/js/edu-benefits/components/RotcHistoryFields.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import ErrorableRadioButtons from '../../common/components/form-elements/ErrorableRadioButtons';
+import ErrorableTextInput from '../../common/components/form-elements/ErrorableTextInput';
+import ProgressButton from '../../common/components/form-elements/ProgressButton';
 
 export default class BenefitsSectionFields extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addNew = this.addNew.bind(this);
+  }
+  addNew() {
+    // TODO: block this out
+  }
   render() {
     return (<fieldset>
       <p>(<span className="form-required-span">*</span>) Indicates a required field</p>
       <p>ROTC placeholder</p>
       <div className="input-section">
+        <p>Scholarship details</p>
+        <ErrorableTextInput
+            label="Amount recieved"
+            name="RotcAmount"
+            field={{ value: '', dirty: false }}
+            placeholder="$"
+            onValueChange={(update) => {this.props.onStateChange('RotcAmount', update);}}/>
+        <ErrorableTextInput
+            label="Year recieved"
+            name="RotcYear"
+            field={{ value: '', dirty: false }}
+            placeholder="YYYY"
+            onValueChange={(update) => {this.props.onStateChange('RotcYear', update);}}/>
+        <div className="row progress-buttons">
+          <div className="small-6 medium-5 columns">
+            <ProgressButton
+                onButtonClick={this.addNew}
+                buttonText="Add"
+                buttonClass="usa-button-primary"
+                beforeText=""/>
+          </div>
+        </div>
+        <ErrorableRadioButtons
+            label="Are you currently participating in a senior ROTC scholarship program that pays for your tuition, fees, books and supplies under Section 2107 of Title 10, U.S. Code?"
+            options={['Yes', 'No']}
+            value={{ value: 'Yes', dirty: false }}
+            name="RotcTuition"
+            onValueChange={(update) => {this.props.onStateChange('RotcAmount', update);}}/>
       </div>
     </fieldset>
     );
