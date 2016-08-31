31c6854b40a2edc85cc128cf5a8640488aec7ba1
James Kassemi <jkassemi@users.noreply.github.com>
2016-08-31 09:18:50 -0600

    Add automated accessibility testing with axe (#2849)
    * Add automated accessibility testing with axe
    
    * Accessibility testing for URLs from sitemap
    
    * Travis build script for added logic
    
    * Only use section508 rules

---
+++ test/accessibility/sample.spec.js
@@ -0,0 +1,12 @@
+const E2eHelpers = require('../util/e2e-helpers');
+const Timeouts = require('../util/timeouts.js');
+
+module.exports = E2eHelpers.createE2eTest(
+  (client) => {
+    client
+      .url(E2eHelpers.baseUrl)
+      .waitForElementVisible('body', Timeouts.normal)
+      .axeCheck('document');
+
+    client.end();
+  });
