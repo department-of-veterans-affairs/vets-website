cb854170a492bc7985e2c8721707633bcc03c797
Benjamin Shyong <ben.shyong@gmail.com>
2016-09-08 12:12:00 -0700

    Search controls placeholder

--- src/js/facility-locator/components/ResultsPane.jsx
+++ src/js/facility-locator/components/ResultsPane.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import SearchControls from './SearchControls';
 
 class ResultsPane extends Component {
   render() {
     return (
       <div>
-        <h3>ResultsPane</h3>
+        <SearchControls/>
+        <hr/>
+        <h4>Search Results:</h4>
       </div>
     );
   }
---
+++ src/js/facility-locator/components/SearchControls.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchControls extends Component {
+  render() {
+    return (
+      <div>
+        <h4>SearchControls</h4>
+      </div>
+    );
+  }
+}
+
+export default SearchControls;
