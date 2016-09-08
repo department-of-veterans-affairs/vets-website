e6554f5aed7571752b3c2b3f65fbf267708b9a7c
James Kassemi <jkassemi@users.noreply.github.com>
2016-09-08 04:32:18 -0600

    Tempoary updates to address staging build (#2998)

--- test/rx/00-required.e2e.spec.js
+++ test/rx/00-required.e2e.spec.js
@@ -1,18 +1,22 @@
-const E2eHelpers = require('../util/e2e-helpers');
-const Timeouts = require('../util/timeouts.js');
-const RxHelpers = require('../util/rx-helpers');
+// TODO(james): address this with a feature flag
 
-module.exports = E2eHelpers.createE2eTest(
-  (client) => {
-    RxHelpers.initApplicationSubmitMock();
+if (process.env.BUILDTYPE !== 'production') {
+  const E2eHelpers = require('../util/e2e-helpers');
+  const Timeouts = require('../util/timeouts.js');
+  const RxHelpers = require('../util/rx-helpers');
 
-    // Ensure introduction page renders.
-    client
-      .url(`${E2eHelpers.baseUrl}/rx/`)
-			.waitForElementVisible('body', Timeouts.normal)
-      .assert.title('Refill your prescriptions: Vets.gov')
-      .waitForElementVisible('.va-tab-content', Timeouts.slow)
-      .click('.va-tabs li:last-child a')
-      .waitForElementVisible('.rx-table', Timeouts.normal);
-  }
-);
+  module.exports = E2eHelpers.createE2eTest(
+    (client) => {
+      RxHelpers.initApplicationSubmitMock();
+
+      // Ensure introduction page renders.
+      client
+        .url(`${E2eHelpers.baseUrl}/rx/`)
+                          .waitForElementVisible('body', Timeouts.normal)
+        .assert.title('Refill your prescriptions: Vets.gov')
+        .waitForElementVisible('.va-tab-content', Timeouts.slow)
+        .click('.va-tabs li:last-child a')
+        .waitForElementVisible('.rx-table', Timeouts.normal);
+    }
+  );
+}
