882b425e84dc90407c2d22221298d1420ff8dd3a
Eugene Doan <doan.eugene@gmail.com>
2016-09-09 19:40:42 -0400

    Added Message component.

--- src/js/messaging/containers/Thread.jsx
+++ src/js/messaging/containers/Thread.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { fetchThread } from '../actions/messages';
+import Message from '../components/Message';
 
 class Thread extends React.Component {
   componentWillMount() {
@@ -14,19 +15,24 @@ class Thread extends React.Component {
   render() {
     const thread = this.props.thread;
     let subject;
+    let messages;
 
     if (thread.length > 0) {
       subject = thread[0].subject;
+      messages = thread.map((message, i) => <Message key={i} attrs={message}/>);
     }
 
     return (
       <div>
-        <p>
+        <p className="messaging-thread-note">
           <strong>Note:</strong> This message may not be from the person you
           originally messaged. It may have been reassigned in an effort to
           address your question as effectively and efficiently as possible.
         </p>
-        <h2>{subject}</h2>
+        <h2 className="messaging-thread-name">{subject}</h2>
+        <div>
+          {messages}
+        </div>
       </div>
     );
   }
