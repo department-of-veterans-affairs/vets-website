eeb3a9657cafffa75721d6ab4d103f1bec53d3e6
Benjamin Shyong <ben.shyong@gmail.com>
2016-09-08 18:32:27 -0700

    use mapbox tilelayer

--- src/js/facility-locator/containers/VAMap.jsx
+++ src/js/facility-locator/containers/VAMap.jsx
@@ -17,8 +17,8 @@ class VAMap extends Component {
         <div className="medium-9 columns">
           <Map ref="map" center={position} zoom={13} >
             <TileLayer
-                url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+                url="https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYXlhbGVsb2VociIsImEiOiJjaWtmdnA1MHAwMDN4dHdtMnBqbGR3djJxIn0.fuqVOKCu8mE-9IdxTa4R8g"
+                attribution='Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'/>
             <Marker position={position}>
               <Popup>
                 <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
