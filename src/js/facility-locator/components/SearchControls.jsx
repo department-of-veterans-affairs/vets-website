d74dc7e2d49062164b2c27e616f42c2687c44e76
Benjamin Shyong <ben.shyong@gmail.com>
2016-09-09 18:33:37 -0700

    mock out search controls

--- src/js/facility-locator/components/SearchControls.jsx
+++ src/js/facility-locator/components/SearchControls.jsx
@@ -2,9 +2,25 @@ import React, { Component } from 'react';
 
 class SearchControls extends Component {
   render() {
+    const style = {
+      padding: '1.5rem 1rem',
+    };
+
     return (
-      <div>
-        <h4>SearchControls</h4>
+      <div style={style}>
+        <h4>Find a VA Facility</h4>
+        <div>Search for facilities near you or for a specific service or benefit.</div>
+        <form className="usa-form">
+          <label htmlFor="Street, City, State or Zip">Enter Street, City, State or Zip</label>
+          <input name="streetCityStateZip" type="text"/>
+          <label htmlFor="serviceType">Service Type</label>
+          <select name="services" defaultValue="all">
+            <option value="all">All</option>
+            <option value="health">Health</option>
+            <option value="benefits">Benefits</option>
+            <option value="cemeteries">Cemeteries</option>
+          </select>
+        </form>
       </div>
     );
   }
