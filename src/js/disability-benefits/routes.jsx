ce1dd14a3c8f703e058a838aa2ecb2942fdab7f2
Maury Lindo <maury@cagedcrown.com>
2016-09-12 13:53:57 -0400

    WIP: Building out react app for disability benefits

---
+++ src/js/disability-benefits/routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+const routes = [
+  // Introduction route.
+  <Route
+      component={IntroductionPage}
+      key="/introduction"
+      path="/introduction"/>
+];
+
+export default routes;
