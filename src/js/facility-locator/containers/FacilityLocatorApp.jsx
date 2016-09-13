af6b7a7f2cb65a2fd03656bdf34d415144bb61ed
Benjamin Shyong <ben.shyong@gmail.com>
2016-09-13 15:54:21 -0700

    Fix breadcrumbs for detail pages

--- src/js/facility-locator/containers/FacilityLocatorApp.jsx
+++ src/js/facility-locator/containers/FacilityLocatorApp.jsx
@@ -1,6 +1,31 @@
+import { Link } from 'react-router';
 import React from 'react';
 
 class FacilityLocatorApp extends React.Component {
+  renderBreadcrumbs() {
+    const { location } = this.props;
+
+    if (location.pathname.match(/facilities\/facility\/\d/)) {
+      return (
+        <span>
+          <li>
+            <Link to="/facilities">
+              Facility Locator
+            </Link>
+          </li>
+          <li className="active">
+            Facility Details
+          </li>
+        </span>
+      );
+    }
+    return (
+      <li className="active">
+        Facility Locator
+      </li>
+    );
+  }
+
   render() {
     return (
       <div>
@@ -8,9 +33,7 @@ class FacilityLocatorApp extends React.Component {
           <nav className="va-nav-breadcrumbs">
             <ul className="row va-nav-breadcrumbs-list" role="menubar" aria-label="Primary">
               <li><a href="/">Home</a></li>
-              <li className="active">
-                Facility Locator
-              </li>
+              {this.renderBreadcrumbs()}
             </ul>
           </nav>
           <h3>Facility Locator</h3>
