6e0f0e43b29c38c5d97d3b4da9aa3559683a4dfc
Tiffany Brown <tiffany@webinista.com>
2016-09-13 12:35:27 -0700

    Removing ButonDelete instances from Compose.jsx.

--- src/js/messaging/containers/Compose.jsx
+++ src/js/messaging/containers/Compose.jsx
@@ -6,8 +6,6 @@ import MessageCategory from '../components/compose/MessageCategory';
 import MessageSubject from '../components/compose/MessageSubject';
 import { setCategory, setSubject } from '../actions/compose.js';
 
-import ButtonDelete from '../components/buttons/ButtonDelete';
-
 class Compose extends React.Component {
   constructor() {
     super();
@@ -41,11 +39,6 @@ class Compose extends React.Component {
             errorMessage={composeMessageErrors.subjet}
             onValueChange={this.dispatchSubjectChange}
             value={this.props.compose.subject}/>
-        <ButtonDelete
-            compact
-            onClick={() => {}}/>
-        <ButtonDelete
-            onClick={() => {}}/>
       </div>
     );
   }
