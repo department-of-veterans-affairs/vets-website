d0ea0de7b31466e22d7c472224d68063f5aa6781
Tiffany Brown <webinista@users.noreply.github.com>
2016-09-14 09:37:33 -0700

    Adds MessageRecipient component (#3047)
    * Adds MessageRecipient component
    
    - Closes department-of-veterans-affairs/messaging-fe#36
    
    * Removing suffix from imports. Changing 'Recipient:' to 'To:'
    
    * Removing extraneous div element
    
    * Removing link between category and subject line.
    
    * Consistently naming parameters in set* functions.
    
    * Education / training history (#3058)
    
    * EducationHistory components.  still need to do conditional logic and tests
    
    * add conditional logic and test
    
    * add test
    
    * isValidSection -> isValidPage
    
    * Adds 'Confirm Refill' flow to Detail page. closes #3022
    
    - Renamed SubmitButton to SubmitRefill.
    - Made SubmitRefill into a form rather than a submit button.
    - Removing redundant properties in SubmitRefill.
    - Made openRefillModal part of props in Prescription.
    - Moving loadPrescription, openGlossaryModal to props.
    - Removed unnecessary name attribute from SubmitRefill.
    - Adding rx-trigger class to make buttons that look like links.
    - Changed glossary trigger to be a button instead of an `a` element for better compatibility with assistive tech.
    
    * Move components into folders and refactor page container components (#3060)
    
    * Move components into folders, remove page components
    
    * Update tests
    
    * Fix test
    
    * Calling loadPrescription via this.props because undefined otherwise.
    
    * De-links category, subject fields. (#3059)
    
    * De-links category, subject fields.
    
    WIP on department-of-veterans-affairs/messaging-fe#37
    
    - Removed category radio buttons
    - Updated CSS for new Subject line element group.
    - Moved messaging-thread to partials directory
    - Renamed message-category to _message-subject
    - Renaming dispatch* methods to handle* methods.
    - Added conditional require when the category field is set to 'Other'
    
    * Adds ButtonDelete component. (#3048)
    
    * Adds ButtonDelete component.
    
    Closes department-of-veterans-affairs/messaging-fe#22
    
    * Removing outline from delete button hover state.
    
    * Adds ButtonDelete component.
    
    Closes department-of-veterans-affairs/messaging-fe#22
    
    * Removing outline from delete button hover state.
    
    * Adding a new line to the end of messaging-btn-delete partial.
    
    * Removing ButonDelete instances from Compose.jsx.
    
    * Moved references to API URLs to project-specific configs. (#3049)
    
    * Education / training history (#3058)
    
    * EducationHistory components.  still need to do conditional logic and tests
    
    * add conditional logic and test
    
    * add test
    
    * isValidSection -> isValidPage
    
    * Move components into folders and refactor page container components (#3060)
    
    * Move components into folders, remove page components
    
    * Update tests
    
    * Fix test
    
    * Camel-casing fieldstate to fieldState.
    
    * Moving app 'shell'-related CSS to a partial.
    
    * De-links category, subject fields.
    
    WIP on department-of-veterans-affairs/messaging-fe#37
    
    - Removed category radio buttons
    - Updated CSS for new Subject line element group.
    - Moved messaging-thread to partials directory
    - Renamed message-category to _message-subject
    - Renaming dispatch* methods to handle* methods.
    - Added conditional require when the category field is set to 'Other'
    
    * Camel-casing fieldstate to fieldState.
    
    * Moving app 'shell'-related CSS to a partial.
    
    * Adds MessageRecipient component
    
    - Closes department-of-veterans-affairs/messaging-fe#36
    
    * Removing suffix from imports. Changing 'Recipient:' to 'To:'
    
    * Updating Compose.jsx with changes from master
    
    - Renamed message-subject.scss to message-compose.scss
    
    * Making Compose sub-components use a cssClass property.
    
    * Cleaning up props.

--- src/js/messaging/containers/Compose.jsx
+++ src/js/messaging/containers/Compose.jsx
@@ -4,13 +4,25 @@ import { connect } from 'react-redux';
 import { messageCategories, composeMessageErrors, composeMessagePlaceholders } from '../config';
 import MessageCategory from '../components/compose/MessageCategory';
 import MessageSubject from '../components/compose/MessageSubject';
-import { setCategory, setSubject, setSubjectRequired } from '../actions/compose';
+import MessageRecipient from '../components/compose/MessageRecipient';
+import {
+  setCategory,
+  setRecipient,
+  setSubject,
+  setSubjectRequired,
+  fetchRecipients
+} from '../actions/compose';
 
 class Compose extends React.Component {
   constructor() {
     super();
     this.handleCategoryChange = this.handleCategoryChange.bind(this);
     this.handleSubjectChange = this.handleSubjectChange.bind(this);
+    this.handleRecipientChange = this.handleRecipientChange.bind(this);
+  }
+
+  componentWillMount() {
+    this.props.fetchRecipients();
   }
 
   handleCategoryChange(valueObj) {
@@ -22,32 +34,45 @@ class Compose extends React.Component {
     this.props.setSubject(valueObj);
   }
 
+  handleRecipientChange(valueObj) {
+    this.props.setRecipient(valueObj);
+  }
+
   render() {
     return (
-      <div>
+      <form id="messaging-compose">
         <h2>New message</h2>
         <p>
           <strong>Note:</strong> Messages may be saved to your health record at
           your team's discretion.
         </p>
+
+        <MessageRecipient
+            categories={messageCategories}
+            errorMessage={composeMessageErrors.recipient}
+            cssClass="messaging-recipient"
+            onValueChange={this.handleRecipientChange}
+            options={this.props.compose.recipients}
+            value={this.props.compose.recipient}/>
+
         <fieldset className="messaging-subject-group">
           <legend>Subject line:</legend>
           <div>
             <MessageCategory
                 categories={messageCategories}
+                cssClass="messaging-category"
                 errorMessage={composeMessageErrors.category}
                 onValueChange={this.handleCategoryChange}
                 value={this.props.compose.category}/>
             <MessageSubject
-                additionalClass="messaging-subject-subject"
+                cssClass="messaging-subject"
                 errorMessage={composeMessageErrors.subjet}
                 onValueChange={this.handleSubjectChange}
                 placeholder={composeMessagePlaceholders.subject}
-                required={this.props.compose.subject.required}
-                value={this.props.compose.subject.value}/>
+                required={this.props.compose.subject.required}/>
           </div>
         </fieldset>
-      </div>
+      </form>
     );
   }
 }
@@ -60,7 +85,9 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = {
   setCategory,
   setSubject,
-  setSubjectRequired
+  setSubjectRequired,
+  setRecipient,
+  fetchRecipients
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Compose);
