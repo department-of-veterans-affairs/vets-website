bb24f75019412fd137ed132388392c13067c04e0
Eugene Doan <doan.eugene@gmail.com>
2016-09-16 17:59:33 -0400

    Made icons larger. Re-aligned folder nav arrow better.

--- src/js/messaging/components/FolderNav.jsx
+++ src/js/messaging/components/FolderNav.jsx
@@ -37,7 +37,10 @@ class FolderNav extends React.Component {
       return this.context.router.isActive(link.props.to, true);
     };
     const myFoldersActive = myFolderLinks.find(isLinkActive);
-    const myFoldersClass = classNames({ 'usa-current': myFoldersActive });
+    const myFoldersClass = classNames({
+      'messaging-my-folders': true,
+      'usa-current': myFoldersActive
+    });
 
     /* Render 'My folders' as expanded or collapsed. */
 
@@ -62,7 +65,7 @@ class FolderNav extends React.Component {
     });
 
     return (
-      <li key="myFolders" className="messaging-my-folders">
+      <li key="myFolders">
         <a className={myFoldersClass} onClick={this.props.onToggleFolders}>
           <span>My folders</span>
           <i className={iconClass}></i>
