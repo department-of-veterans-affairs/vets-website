d882f054f8ae4f883f42166bdbb6d1a83444028d
Eugene Doan <doan.eugene@gmail.com>
2016-09-16 13:05:44 -0400

    Further developed reply controls on message thread. Still need to be able to switch back to 'compact' form and 'focused' form.

--- src/js/messaging/containers/Thread.jsx
+++ src/js/messaging/containers/Thread.jsx
@@ -3,21 +3,46 @@ import { connect } from 'react-redux';
 
 import { fetchThread, setVisibleDetails } from '../actions/messages';
 import Message from '../components/Message';
+import MessageSend from '../components/compose/MessageSend';
+import MessageWrite from '../components/compose/MessageWrite';
 import NoticeBox from '../components/NoticeBox';
+import { composeMessagePlaceholders } from '../config';
 
 class Thread extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleReplyChange = this.handleReplyChange.bind(this);
+    this.handleReplySave = this.handleReplySave.bind(this);
+    this.handleReplySend = this.handleReplySend.bind(this);
+    this.handleReplyDelete = this.handleReplyDelete.bind(this);
+  }
+
   componentDidMount() {
     const id = this.props.params.id;
     this.props.fetchThread(id);
   }
 
+  handleReplyChange() {
+  }
+
+  handleReplySave() {
+  }
+
+  handleReplySend() {
+  }
+
+  handleReplyDelete() {
+  }
+
   render() {
     const thread = this.props.thread;
     let subject;
+    let lastSender;
     let messages;
 
     if (thread.length > 0) {
       subject = thread[0].subject;
+      lastSender = thread[thread.length - 1].sender_name;
       messages = thread.map((message) => {
         const detailsVisible =
           this.props.visibleDetailsId === message.message_id;
@@ -39,7 +64,19 @@ class Thread extends React.Component {
           {messages}
         </div>
         <div className="messaging-thread-reply">
-          <textarea placeholder="Click here to reply"/>
+          <div className="messaging-thread-reply-recipient">
+            <label>To:</label>
+            {lastSender}
+          </div>
+          <MessageWrite
+              cssClass="messaging-write"
+              onValueChange={this.handleReplyChange}
+              placeholder={composeMessagePlaceholders.message}/>
+          <MessageSend
+              cssClass="messaging-send-group"
+              onSave={this.handleReplySave}
+              onSend={this.handleReplySend}
+              onDelete={this.handleReplyDelete}/>
         </div>
         <NoticeBox/>
       </div>
