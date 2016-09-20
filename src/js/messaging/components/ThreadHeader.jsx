d5a761b580ab40a972052aa07de093f9911a66a9
Eugene Doan <U-DON@users.noreply.github.com>
2016-09-20 12:33:53 -0400

    [SM] Message Navigation + Message Count (#3113)
    * Thread header styles.
    
    * Only show ToggleThread button if thread has more than one message.
    
    * Query for thread combines thread with the most recent message. Also returns the thread with most recent message last.
    
    * Added ButtonMove component.
    
    * Added MessageNav component.
    
    * Included MessageNav in ThreadHeader.
    
    * Fixed look of folder actions in nav. Icon size, spacing, and matching underline.
    
    * Spacing between icons and label on buttons in thread controls.
    
    * Changed prop name.
    
    * Top navigation bar for threads is now functional aside from move button.
    
    * Moved styles around the count and message nav into a separate stylesheet.
    
    * Added count and nav to folder view.
    
    * Re-corrected handling of previous and next in thread navigation.

--- src/js/messaging/components/ThreadHeader.jsx
+++ src/js/messaging/components/ThreadHeader.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router';
 
+import { paths } from '../config';
 import ButtonDelete from './buttons/ButtonDelete';
+import ButtonMove from './buttons/ButtonMove';
 import ButtonPrint from './buttons/ButtonPrint';
+import MessageNav from './MessageNav';
 import ToggleThread from './ToggleThread';
 
 class ThreadHeader extends React.Component {
@@ -14,16 +18,37 @@ class ThreadHeader extends React.Component {
   }
 
   render() {
+    let toggleThread;
+
+    if (this.props.threadMessageCount > 1) {
+      toggleThread = (
+        <ToggleThread
+            messagesCollapsed={this.props.messagesCollapsed}
+            onClick={this.props.onToggleThread}/>
+      );
+    }
+
     return (
       <div className="messaging-thread-header">
-        <h2 className="messaging-thread-subject">{this.props.title}</h2>
-        <div className="messaging-thread-controls">
-          <ToggleThread
-              messagesCollapsed={this.props.messagesCollapsed}
-              onClick={this.props.onToggleThread}/>
-          <ButtonDelete
-              onClickHandler={this.handleDelete}/>
-          <ButtonPrint/>
+        <div className="messaging-thread-nav">
+          <div>
+            <Link to={paths.INBOX_URL}>&lt; Back to Inbox</Link>
+            <ButtonMove/>
+          </div>
+          <MessageNav
+              currentRange={this.props.currentMessageNumber}
+              messageCount={this.props.folderMessageCount}
+              handlePrev={this.props.handlePrev}
+              handleNext={this.props.handleNext}/>
+        </div>
+        <div className="messaging-thread-title">
+          <h2 className="messaging-thread-subject">{this.props.subject}</h2>
+          <div className="messaging-thread-controls">
+            {toggleThread}
+            <ButtonDelete
+                onClickHandler={this.handleDelete}/>
+            <ButtonPrint/>
+          </div>
         </div>
       </div>
     );
@@ -31,7 +56,12 @@ class ThreadHeader extends React.Component {
 }
 
 ThreadHeader.propTypes = {
-  title: React.PropTypes.string.isRequired,
+  currentMessageNumber: React.PropTypes.number.isRequired,
+  folderMessageCount: React.PropTypes.number.isRequired,
+  handlePrev: React.PropTypes.func,
+  handleNext: React.PropTypes.func,
+  subject: React.PropTypes.string.isRequired,
+  threadMessageCount: React.PropTypes.number.isRequired,
   messagesCollapsed: React.PropTypes.bool,
   onToggleThread: React.PropTypes.func
 };
