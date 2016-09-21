a372b9e936cfdb7c54dab10ae6d17dc848673b95
Benjamin Shyong <ben.shyong@gmail.com>
2016-09-21 16:01:55 -0700

    add icons and style to search results

--- src/js/facility-locator/components/ResultsPane.jsx
+++ src/js/facility-locator/components/ResultsPane.jsx
@@ -16,18 +16,32 @@ class ResultsPane extends Component {
 
     return (
       facilities.map(f => {
-        const { address } = f.attributes;
+        const { address, phone, name } = f.attributes;
+        const addressString = [
+          compact([address.building, address.street, address.suite]).join(' '),
+          `${address.city}, ${address.state} ${address.zip}-${address.zip4}`
+        ];
 
         return (
           <li key={f.id} className="facility-result">
             <Link to={`facilities/facility/${f.id}`}>
-              <h5>{f.name}</h5>
+              <h5>{name}</h5>
             </Link>
             <strong>Facility type: {f.type}</strong>
             <p>
-              {compact([address.building, address.street, address.suite].join(' '))}<br/>
-              {`${address.city}, ${address.state} ${address.zip}-${address.zip4}`}
+              {addressString[0]}<br/>
+              {addressString[1]}
             </p>
+            <span style={{ marginRight: '1rem' }}>
+              <a href={`tel:${phone.main}`}>
+                <i className="fa fa-phone"/> {phone.main}
+              </a>
+            </span>
+            <span>
+              <a href={`https://maps.google.com?saddr=Current+Location&daddr=${addressString.join(' ')}`} target="_blank">
+                <i className="fa fa-map"/> Directions
+              </a>
+            </span>
           </li>
         );
       })
