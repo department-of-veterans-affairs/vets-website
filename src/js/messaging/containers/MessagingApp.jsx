61f62991a0bbec634807c431bc8385b769bbebd8
Eugene Doan <doan.eugene@gmail.com>
2016-09-21 12:04:29 -0400

    Removing note that's no longer in the mocks.

--- src/js/messaging/containers/MessagingApp.jsx
+++ src/js/messaging/containers/MessagingApp.jsx
@@ -12,11 +12,6 @@ class MessagingApp extends React.Component {
     return (
       <div id="messaging-app" className="row">
         <h1>Message your health care team</h1>
-        <p>
-          <strong>Important:</strong> Secure messaging is not instantly read.
-          It can take up to 2 hours for a message to be seen
-          and/or a response to be sent.
-        </p>
         {this.props.children}
       </div>
     );
