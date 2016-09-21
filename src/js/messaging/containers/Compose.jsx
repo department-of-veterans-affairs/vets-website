b5cd37ddd60aad99908a97fc763269fbbf7baef3
Eugene Doan <eugene@adhocteam.us>
2016-09-21 15:20:04 -0400

    Removed compose view note to match mock.

--- src/js/messaging/containers/Compose.jsx
+++ src/js/messaging/containers/Compose.jsx
@@ -81,11 +81,9 @@ class Compose extends React.Component {
 
     return (
       <div>
-        <h2>New message</h2>
-        <p>
-          <strong>Note:</strong> Messages may be saved to your health record at
-          your team's discretion.
-        </p>
+        <div id="messaging-compose-header">
+          <h2>New message</h2>
+        </div>
         <form
             id="messaging-compose"
             onSubmit={(domEvent) => { domEvent.preventDefault(); }}>
