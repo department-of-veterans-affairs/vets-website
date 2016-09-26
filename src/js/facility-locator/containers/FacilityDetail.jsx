8346999d232dfa98943ab2d88e84bbadd45a4959
Benjamin Shyong <ben.shyong@gmail.com>
2016-09-26 11:54:03 -0700

    Detail page facility info clean up and styling

--- src/js/facility-locator/containers/FacilityDetail.jsx
+++ src/js/facility-locator/containers/FacilityDetail.jsx
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
-import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { compact } from 'lodash';
+import { connect } from 'react-redux';
 import { fetchVAFacility } from '../actions';
-import FacilityInfo from '../components/FacilityInfo';
-import ServicesAtFacility from '../components/ServicesAtFacility';
-import FacilityMap from '../components/FacilityMap';
+import { Link } from 'react-router';
 import FacilityHours from '../components/FacilityHours';
+import FacilityMap from '../components/FacilityMap';
+import React, { Component } from 'react';
+import ServicesAtFacility from '../components/ServicesAtFacility';
 
 class FacilityDetail extends Component {
   componentWillMount() {
     this.props.fetchVAFacility(this.props.params.id);
   }
 
+  renderFacilityInfo() {
+    const { facility } = this.props;
+    const { address, phone, name } = facility.attributes;
+    const addressString = [
+      compact([address.building, address.street, address.suite]).join(' '),
+      `${address.city}, ${address.state} ${address.zip}-${address.zip4}`
+    ];
+
+    return (
+      <div>
+        <h3>{name}</h3>
+        <div>
+          {addressString[0]} {addressString[1]}
+        </div>
+        <p>
+          <a href={`tel:${phone.main}`}>
+            <i className="fa fa-phone"/> {phone.main}
+          </a>
+        </p>
+        <p>
+          <a href={`https://maps.google.com?saddr=Current+Location&daddr=${addressString.join(' ')}`} target="_blank">
+            <i className="fa fa-map"/> Directions
+          </a>
+        </p>
+      </div>
+    );
+  }
+
   render() {
+    const { facility } = this.props;
+
+    if (!facility) {
+      return null;
+    }
+
     return (
       <div className="row facility-detail">
         <div className="medium-8 columns">
-          <Link to="/facilities" className="facility-back-link show-for-small-only">
+          <Link to="/facilities" className="facility-back-link">
             <i className="fa fa-chevron-left" aria-hidden="true"></i>Back to list
           </Link>
           <div className="details-map">
-            <h3>{this.props.facility ? this.props.facility.name : ''}</h3>
-            <FacilityInfo info={this.props.facility} currentQuery={this.props.currentQuery}/>
+            {this.renderFacilityInfo()}
             <ServicesAtFacility info={this.props.facility}/>
           </div>
         </div>
