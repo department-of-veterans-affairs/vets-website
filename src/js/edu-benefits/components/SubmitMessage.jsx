a97461b11b0bbceceef286d05f03d4a99ff4f1d7
Caitlin <cweber@sunlightfoundation.com>
2016-09-26 10:33:12 -0400

    documentation placeholder

--- src/js/edu-benefits/components/SubmitMessage.jsx
+++ src/js/edu-benefits/components/SubmitMessage.jsx
@@ -68,8 +68,8 @@ export default class SubmitMessage extends React.Component {
         <p>This form doesn’t currently allow you to upload documents. Nothing is required right now. VA will review your application.</p>
         <p><b>VA may eventually need the following documents:</b></p>
         <ul>
-          <li></li>
-          <li></li>
+          <li>Your Reserve Kicker (if applicable)</li>
+          <li>Documentation of contributions to increase the amount of your monthly benefits. (if applicable)</li>
         </ul>
         <p>You can upload these documents, if you have them, through the <a href="https://gibill.custhelp.com/app/utils/login_form/redirect/account%252">GI Bill site.</a></p>
         <p>Need help? If you have questions, call 1-888-442-4551 (1-888-GI-Bill)</p>
