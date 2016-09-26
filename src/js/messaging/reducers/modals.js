07551f5f8f4e6ac425f0ca672f74fc361f678397
Tiffany Brown <webinista@users.noreply.github.com>
2016-09-26 12:11:31 -0700

    Closes department-of-veterans-affairs/messaging-fe#67 (#3183)
    - Adds error check for:
        - Existing folder name.
        - Blank folder name field (including spaces-only folder names)
        - Disallowed characters.
    - Disables submit button if there is an error.
    - Adds maximum length config option, error messages for ModalCreateFolder.
    - Changes top margin of va-modal-button-group. Refactoring va-modal SCSS.

--- src/js/messaging/reducers/modals.js
+++ src/js/messaging/reducers/modals.js
@@ -1,9 +1,11 @@
 import set from 'lodash/fp/set';
 
+import { makeField } from '../../common/model/fields.js';
 import {
   TOGGLE_CONFIRM_DELETE,
   TOGGLE_ATTACHMENTS,
-  TOGGLE_CREATE_FOLDER
+  TOGGLE_CREATE_FOLDER,
+  SET_NEW_FOLDER_NAME
 } from '../actions/modals';
 
 const initialState = {
@@ -14,7 +16,8 @@ const initialState = {
     visible: false
   },
   createFolder: {
-    visible: false
+    visible: false,
+    newFolderName: makeField('')
   }
 };
 
@@ -26,6 +29,8 @@ export default function modals(state = initialState, action) {
       return set('attachments.visible', !state.attachments.visible, state);
     case TOGGLE_CREATE_FOLDER:
       return set('createFolder.visible', !state.createFolder.visible, state);
+    case SET_NEW_FOLDER_NAME:
+      return set('createFolder.newFolderName', action.folderName, state);
     default:
       return state;
   }
