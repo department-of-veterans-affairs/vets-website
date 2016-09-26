2c7a721ecc84452939a0883479934f018b7dd968
Tiffany Brown <tiffany@webinista.com>
2016-09-26 11:47:51 -0700

    Closes department-of-veterans-affairs/messaging-fe#67
    - Adds error check for:
        - Existing folder name.
        - Blank folder name field (including spaces-only folder names)
        - Disallowed characters.
    - Disables submit button if there is an error.
    - Adds maximum length config option, error messages for ModalCreateFolder.
    - Changes top margin of va-modal-button-group. Refactoring va-modal SCSS.

--- src/js/messaging/actions/modals.js
+++ src/js/messaging/actions/modals.js
@@ -1,6 +1,7 @@
 export const TOGGLE_CONFIRM_DELETE = 'TOGGLE_CONFIRM_DELETE';
 export const TOGGLE_ATTACHMENTS = 'TOGGLE_ATTACHMENTS';
 export const TOGGLE_CREATE_FOLDER = 'TOGGLE_CREATE_FOLDER';
+export const SET_NEW_FOLDER_NAME = 'SET_NEW_FOLDER_NAME';
 
 
 export function toggleConfirmDelete() {
@@ -22,3 +23,10 @@ export function toggleCreateFolderModal() {
   };
 }
 
+
+export function setNewFolderName(folderName) {
+  return {
+    type: SET_NEW_FOLDER_NAME,
+    folderName
+  };
+}
