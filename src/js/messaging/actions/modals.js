b86557c02add6792b1f302142cf42e176fd01aa7
Eugene Doan <U-DON@users.noreply.github.com>
2016-10-03 18:51:06 -0400

    [Messaging] Move messages between folders / Settings page (#3252)
    * Alert when creating new folder succeeds or fails.
    
    * Added Settings page. Manage folder goes to settings page. Folders can be deleted.
    
    * Shortened action type name.
    
    * Added PATCH settings to API config.
    
    * Fixed warnings for Message component when it can't be collapsed or has no read receipt info.
    
    * Integrated API to move messages to folders.
    
    * Validate on submit of create folder. Dirty the field when input starts changing.
    
    * Split toggle create folder into open and close actions. On opening, pass flag to move message after create.
    
    * Implemented move to new folder.
    
    * When creating a folder in order to move a message, add the folder to the list, but don't display an alert.
    
    * Fix lint errors.
    
    * Folder settings table links to each folder. Spacing tweaks for settings..
    
    * Removed comment.
    
    * Moved common function call out of conditional cases.
    
    * Added create folder button to settings page.
    
    * Separated action for opening create folder modal that explicitly takes messageId so that the main usage doesn't accidentally use an event object.
    
    * Fixed bug that allows submitting create folder modal without changing input.
    
    * Removed unused variable.
    
    * Mark dirty on value change in create folder modal again.

--- src/js/messaging/actions/modals.js
+++ src/js/messaging/actions/modals.js
@@ -1,28 +1,31 @@
+export const CLOSE_CREATE_FOLDER = 'CLOSE_CREATE_FOLDER';
+export const OPEN_CREATE_FOLDER = 'OPEN_CREATE_FOLDER';
 export const TOGGLE_CONFIRM_DELETE = 'TOGGLE_CONFIRM_DELETE';
 export const TOGGLE_ATTACHMENTS = 'TOGGLE_ATTACHMENTS';
-export const TOGGLE_CREATE_FOLDER = 'TOGGLE_CREATE_FOLDER';
 export const SET_NEW_FOLDER_NAME = 'SET_NEW_FOLDER_NAME';
 
+export function closeCreateFolderModal() {
+  return { type: CLOSE_CREATE_FOLDER };
+}
+
+export function openCreateFolderModal() {
+  return { type: OPEN_CREATE_FOLDER };
+}
+
+export function openMoveToNewFolderModal(messageId) {
+  return {
+    type: OPEN_CREATE_FOLDER,
+    messageId
+  };
+}
 
 export function toggleConfirmDelete() {
-  return {
-    type: TOGGLE_CONFIRM_DELETE
-  };
+  return { type: TOGGLE_CONFIRM_DELETE };
 }
-
 
 export function toggleAttachmentsModal() {
-  return {
-    type: TOGGLE_ATTACHMENTS
-  };
+  return { type: TOGGLE_ATTACHMENTS };
 }
-
-export function toggleCreateFolderModal() {
-  return {
-    type: TOGGLE_CREATE_FOLDER
-  };
-}
-
 
 export function setNewFolderName(folderName) {
   return {
--- src/js/messaging/config.js
+++ src/js/messaging/config.js
@@ -26,6 +26,10 @@ module.exports = {
       'delete': {
         method: 'DELETE',
         headers
+      },
+      patch: {
+        method: 'PATCH',
+        headers
       }
     }
   },
