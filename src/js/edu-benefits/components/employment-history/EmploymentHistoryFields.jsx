a349e22824b295dfb1999e6c16571673fb30fe57
Jeff Balboni <jeff@adhocteam.us>
2016-10-04 17:08:40 -0400

    Add comments, use it on employment history

--- src/js/edu-benefits/components/employment-history/EmploymentHistoryFields.jsx
+++ src/js/edu-benefits/components/employment-history/EmploymentHistoryFields.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import ErrorableRadioButtons from '../../../common/components/form-elements/ErrorableRadioButtons';
 import GrowableTable from '../../../common/components/form-elements/GrowableTable';
+import ExpandingGroup from '../../../common/components/form-elements/ExpandingGroup';
 
 import EmploymentPeriod from './EmploymentPeriod';
 import { createEmploymentPeriod } from '../../utils/veteran';
@@ -21,30 +22,32 @@ export default class EmploymentHistoryFields extends React.Component {
     return (<fieldset>
       <legend className="hide-for-small-only">Employment history</legend>
       <div className="input-section">
-        <ErrorableRadioButtons
-            label="Have you ever held a license (for example, as a contractor or plumber) or a journeyman rating to practice a profession?"
-            name="hasNonMilitaryJobs"
-            options={yesNo}
-            value={this.props.data.hasNonMilitaryJobs}
-            onValueChange={(update) => {this.props.onStateChange('hasNonMilitaryJobs', update);}}/>
-      </div>
-      {this.props.data.hasNonMilitaryJobs.value === 'Y'
-        ? <div className="input-section">
-          <h4>Employment</h4>
-          <hr/>
+        <ExpandingGroup open={this.props.data.hasNonMilitaryJobs.value === 'Y'} additionalClass="edu-benefits-employ-group">
           <div className="input-section">
-            <GrowableTable
-                component={EmploymentPeriod}
-                createRow={createEmploymentPeriod}
-                data={this.props.data}
-                initializeCurrentElement={() => this.props.initializeFields(periodFields, 'nonMilitaryJobs')}
-                onRowsUpdate={(update) => {this.props.onStateChange('nonMilitaryJobs', update);}}
-                path="/employment-history/employment-information"
-                rows={this.props.data.nonMilitaryJobs}
-                isValidSection={isValidPage}/>
+            <ErrorableRadioButtons
+                label="Have you ever held a license (for example, as a contractor or plumber) or a journeyman rating to practice a profession?"
+                name="hasNonMilitaryJobs"
+                options={yesNo}
+                value={this.props.data.hasNonMilitaryJobs}
+                onValueChange={(update) => {this.props.onStateChange('hasNonMilitaryJobs', update);}}/>
           </div>
-        </div>
-      : null}
+          <div className="input-section">
+            <h4>Employment</h4>
+            <hr/>
+            <div className="input-section">
+              <GrowableTable
+                  component={EmploymentPeriod}
+                  createRow={createEmploymentPeriod}
+                  data={this.props.data}
+                  initializeCurrentElement={() => this.props.initializeFields(periodFields, 'nonMilitaryJobs')}
+                  onRowsUpdate={(update) => {this.props.onStateChange('nonMilitaryJobs', update);}}
+                  path="/employment-history/employment-information"
+                  rows={this.props.data.nonMilitaryJobs}
+                  isValidSection={isValidPage}/>
+            </div>
+          </div>
+        </ExpandingGroup>
+      </div>
     </fieldset>
     );
   }
