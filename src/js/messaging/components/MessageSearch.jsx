ac40c914925fea14f5b109ea66c8d3c8a1cf40c9
Tiffany Brown <webinista@users.noreply.github.com>
2016-10-04 07:00:40 -0700

    Adds toggle action to show/hide Advanced Search fields. (#3259)
    Closes department-of-veterans-affairs/messaging-fe#91

--- src/js/messaging/components/MessageSearch.jsx
+++ src/js/messaging/components/MessageSearch.jsx
@@ -16,18 +16,24 @@ class MessageSearch extends React.Component {
               id="msg-search-simple"
               name="mquery"
               type="search"/>
-          <button type="submit" className="msg-search-btn">
+          <button
+              type="submit"
+              className="msg-search-btn">
             <i className="fa fa-search"></i>
             <span className="msg-search-btn-text">Search</span>
           </button>
         </div>
-        <MessageSearchAdvanced isOpen/>
+        <MessageSearchAdvanced
+            isVisible={this.props.isAdvancedVisible}
+            onAdvancedSearch={this.props.onAdvancedSearch}/>
       </form>);
   }
 }
 
 MessageSearch.propTypes = {
   cssClass: React.PropTypes.string,
+  isAdvancedVisible: React.PropTypes.bool.isRequired,
+  onAdvancedSearch: React.PropTypes.func.isRequired,
   onSubmit: React.PropTypes.func
 };
 
