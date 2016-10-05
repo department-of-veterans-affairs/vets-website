172ad8cbfb9bb66319434fce29e55fd6d2add530
Jeff Balboni <jeff@adhocteam.us>
2016-10-05 13:48:28 -0400

    Fix error handling

--- src/js/disability-benefits/actions/index.js
+++ src/js/disability-benefits/actions/index.js
@@ -1,19 +1,24 @@
+import environment from '../../common/helpers/environment';
+
 export const SET_CLAIMS = 'SET_CLAIMS';
 export const CHANGE_CLAIMS_PAGE = 'CHANGE_CLAIMS_PAGE';
 
-// localhost needs to be replaced to make this api calls work in dev or staging
-// http://localhost:3000/v0 -> /api/v0
-
 export function getClaims() {
   return (dispatch) => {
-    fetch('//localhost:3000/v0/disability_claims', {
+    fetch(`${environment.API_URL}/v0/disability_claims`, {
       method: 'GET',
       mode: 'cors',
       headers: {
         'X-Key-Inflection': 'camel',
       }
     })
-      .then(res => res.json())
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+
+        return Promise.reject(res.statusText);
+      })
       .then(claims => dispatch({ type: 'SET_CLAIMS', claims: claims.data }));
   };
 }
