345d69f00bd74edec3a9212d11d48e4e369c30e5
Jeff Balboni <jeff@adhocteam.us>
2016-10-05 17:35:39 -0400

    Update data and formatting

--- src/js/disability-benefits/components/ClaimsListItem.jsx
+++ src/js/disability-benefits/components/ClaimsListItem.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router';
+import moment from 'moment';
 
 export default function ClaimsListItem({ claim }) {
-  const filesNeeded = claim.attributes.trackedItems.stillNeedFromYouList ? claim.attributes.trackedItems.stillNeedFromYouList.length : null;
   return (
     <Link className="claim-list-item" to={`/your-claims/${claim.id}/status`}>
       <h4 className="claim-list-item-header">Compensation Claim</h4>
-      <p className="status">Status: {claim.attributes.phaseDates.latestPhaseType}</p>
-      {filesNeeded !== null
-        ? <p><i className="fa fa-exclamation-triangle"></i>We need {filesNeeded} {filesNeeded > 1 ? 'files' : 'file'} from you</p>
-        : null}
-      <p><i className="fa fa-envelope"></i>We sent you a development letter (TODO)</p>
-      <p>Last Update: TODO</p>
+      <p className="status">Status: TODO</p>
+      <p className="communications">
+        {claim.attributes.developmentLetterSent && !claim.attributes.decisionLetter
+          ? <p><i className="fa fa-envelope"></i>We sent you a development letter</p>
+          : null}
+        {claim.attributes.decisionLetterSent
+          ? <p><i className="fa fa-envelope"></i>We sent you a decision letter</p>
+          : null}
+        <p><i className="fa fa-exclamation-triangle"></i>We need files from you TODO</p>
+      </p>
+      <p>Last update: {moment(claim.attributes.phaseChangeDate).format('MMM M, YYYY')}</p>
     </Link>
   );
 }
