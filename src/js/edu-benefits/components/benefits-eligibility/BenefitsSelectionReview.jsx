afbf81958b37f61c0811fa4f6d4c9dc91e8dbe7c
Jeff Balboni <jeff@adhocteam.us>
2016-10-06 09:55:22 -0400

    WIP

--- src/js/edu-benefits/components/benefits-eligibility/BenefitsSelectionReview.jsx
+++ src/js/edu-benefits/components/benefits-eligibility/BenefitsSelectionReview.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { getLabel, displayDateIfValid, showRelinquishedEffectiveDate } from '../../utils/helpers';
+import { getLabel, displayDateIfValid, showRelinquishedEffectiveDate, getListOfBenefits } from '../../utils/helpers';
 import { relinquishableBenefits } from '../../utils/options-for-select';
 
 export default class BenefitsSelectionReview extends React.Component {
@@ -9,8 +9,8 @@ export default class BenefitsSelectionReview extends React.Component {
       <table className="review usa-table-borderless">
         <tbody>
           <tr>
-            <td>Post-9/11 GI Bill (Chapter 33)</td>
-            <td>{this.props.data.chapter33 ? 'Yes' : 'No'}</td>
+            <td>Benefits selection:</td>
+            <td>{getListOfBenefits(this.props.data).map((benefit, index) => <span key={index}>{index === 0 ? null : <br/>}{benefit}</span>)}</td>
           </tr>
         </tbody>
         {this.props.data.chapter33
@@ -27,20 +27,6 @@ export default class BenefitsSelectionReview extends React.Component {
               : null}
           </tbody>
           : null}
-        <tbody>
-          <tr>
-            <td>Montgomery GI Bill (MGIB or Chapter 30) Education Assistance Program:</td>
-            <td>{this.props.data.chapter30 ? 'Yes' : 'No'}</td>
-          </tr>
-          <tr>
-            <td>Montgomery GI Bill Selected Reserve (MGIB-SR or Chapter 1606) Educational Assistance Program</td>
-            <td>{this.props.data.chapter1606 ? 'Yes' : 'No'}</td>
-          </tr>
-          <tr>
-            <td>Post-Vietnam Era Veterans' Educational Assistance Program (VEAP or chapter 32)</td>
-            <td>{this.props.data.chapter32 ? 'Yes' : 'No'}</td>
-          </tr>
-        </tbody>
       </table>
     );
   }
