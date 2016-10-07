a77f0b9303705c8d5db2d5eef4683926355d088f
Maury Lindo <maury@cagedcrown.com>
2016-10-07 15:14:41 -0400

    Fix merge conflict and add css class to NoClaims.jsx

--- src/js/disability-benefits/components/NoClaims.jsx
+++ src/js/disability-benefits/components/NoClaims.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function NoClaims() {
   return (
-    <div className="claims-alert">
+    <div className="you-have-no-claims claims-alert">
       <h4>You do not have any submitted claims</h4>
       <p>Claims that you have submitted will appear here. If you have an open application for a claim but have not yet submitted it, you can continue your application on <a href="https://www.ebenefits.va.gov/">ebenefits</a></p>
     </div>
