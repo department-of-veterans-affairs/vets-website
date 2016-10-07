08861e7652f62fc4614f64daaaa573758e0d9ba5
Maury Lindo <maury@cagedcrown.com>
2016-10-07 14:28:53 -0400

    Fix tab nav

--- src/js/disability-benefits/containers/StatusPage.jsx
+++ src/js/disability-benefits/containers/StatusPage.jsx
@@ -16,58 +16,61 @@ class StatusPage extends React.Component {
             <h6>Your Claimed Conditions:</h6>
             <p className="list">{"Tinnitus, Arthritis, PTSD"}</p>
             <TabNav/>
-            <AddingDetails/>
-            <NeedFilesFromYou/>
-            <AskVAToDecide/>
-            <ClaimsDecision/>
+            <div className="va-tab-content">
+              <AddingDetails/>
+              <NeedFilesFromYou/>
+              <AskVAToDecide/>
+              <ClaimsDecision/>
+              <ol className="process form-process">
+                <li role="presentation" className="one  step one wow fadeIn animated section-complete" >
+                  <div>
+                    <h5>Claim Recieved</h5>
+                  </div>
+                </li>
+                <li role="presentation" className="two  step one wow fadeIn animated section-complete" >
+                  <div>
+                    <h5>Initial Review</h5>
+                  </div>
+                </li>
+                <li role="presentation" className="three  step one wow fadeIn animated section-current" >
+                  <div>
+                    <h5>Evidence gathering, Review, and Decision</h5>
+                    <p>
+                      If VA needs more information, the Veterans Service Representative (VSR) will request it from you on your behalf. Once VA has all the information it needs, the VSR will confirm, issue by issue, that the claim is ready for a decision.
+                    </p>
+                    <div className="claims-evidence">
+                      <p className="claims-evidence-date">Jul 17, 2016</p>
+                      <p className="claims-evidence-item">We requested <a href="/">Copy of DD214</a> from you</p>
+                    </div>
+                    <div className="claims-evidence">
+                      <p className="claims-evidence-date">Jul 7, 2016</p>
+                      <p className="claims-evidence-item">We requested <a href="/">PTSD questionnaire</a> from you</p>
+                    </div>
+                    <div className="claims-evidence">
+                      <p className="claims-evidence-date">Jul 1, 2016</p>
+                      <p className="claims-evidence-item">Your claim moved to Evidence gathering for review</p>
+                    </div>
+                  </div>
+                </li>
+                <li role="presentation" className="four  step one wow fadeIn animated" >
+                  <div>
+                    <h5>Preparing for decision</h5>
+                  </div>
+                </li>
+                <li role="presentation" className="five last step one wow fadeIn animated">
+                  <div className="completion-container">
+                    <h5>Complete</h5>
+                    <div className="claim-completion-estimation">
+                      <p className="date-estimation">Estimated Mar 11, 2018</p>
+                      <p><a href="/">Learn about this estimation</a></p>
+                    </div>
+                  </div>
+                </li>
+              </ol>
+            </div>
           </div>
           <div name="topScrollElement"></div>
-          <ol className="process form-process">
-            <li role="presentation" className="one  step one wow fadeIn animated section-complete" >
-              <div>
-                <h5>Claim Recieved</h5>
-              </div>
-            </li>
-            <li role="presentation" className="two  step one wow fadeIn animated section-complete" >
-              <div>
-                <h5>Initial Review</h5>
-              </div>
-            </li>
-            <li role="presentation" className="three  step one wow fadeIn animated section-current" >
-              <div>
-                <h5>Evidence gathering, Review, and Decision</h5>
-                <p>
-                  If VA needs more information, the Veterans Service Representative (VSR) will request it from you on your behalf. Once VA has all the information it needs, the VSR will confirm, issue by issue, that the claim is ready for a decision.
-                </p>
-                <div className="claims-evidence">
-                  <p className="claims-evidence-date">Jul 17, 2016</p>
-                  <p className="claims-evidence-item">We requested <a href="/">Copy of DD214</a> from you</p>
-                </div>
-                <div className="claims-evidence">
-                  <p className="claims-evidence-date">Jul 7, 2016</p>
-                  <p className="claims-evidence-item">We requested <a href="/">PTSD questionnaire</a> from you</p>
-                </div>
-                <div className="claims-evidence">
-                  <p className="claims-evidence-date">Jul 1, 2016</p>
-                  <p className="claims-evidence-item">Your claim moved to Evidence gathering for review</p>
-                </div>
-              </div>
-            </li>
-            <li role="presentation" className="four  step one wow fadeIn animated" >
-              <div>
-                <h5>Preparing for decision</h5>
-              </div>
-            </li>
-            <li role="presentation" className="five last step one wow fadeIn animated">
-              <div className="completion-container">
-                <h5>Complete</h5>
-                <div className="claim-completion-estimation">
-                  <p className="date-estimation">Estimated Mar 11, 2018</p>
-                  <p><a href="/">Learn about this estimation</a></p>
-                </div>
-              </div>
-            </li>
-          </ol>
+
         </div>
         <AskVAQuestions/>
       </div>
