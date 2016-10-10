fcd0a74314d1207af06396a79cdc9ed41ca94315
Eugene Doan <doan.eugene@gmail.com>
2016-10-10 18:49:24 -0400

    Added modals reducer unit tests for opening attachments modal.

--- test/messaging/reducers/modals.unit.spec.js
+++ test/messaging/reducers/modals.unit.spec.js
@@ -1,11 +1,13 @@
 import { expect } from 'chai';
 
 import { makeField } from '../../../src/js/common/model/fields';
+import { composeMessage } from '../../../src/js/messaging/config';
 import modalsReducer from '../../../src/js/messaging/reducers/modals';
 
 import {
   CLOSE_ATTACHMENTS_MODAL,
   CLOSE_CREATE_FOLDER,
+  OPEN_ATTACHMENTS_MODAL,
   TOGGLE_CONFIRM_DELETE
 } from '../../../src/js/messaging/utils/constants';
 
@@ -32,6 +34,26 @@ describe('modals reducer', () => {
     expect(newState.deleteConfirm.visible).to.be.true;
     newState = modalsReducer(newState, { type: TOGGLE_CONFIRM_DELETE });
     expect(newState.deleteConfirm.visible).to.be.false;
+  });
+
+  it('should open an error modal for attachments that are too big', () => {
+    const newState = modalsReducer(initialState, {
+      type: OPEN_ATTACHMENTS_MODAL,
+      error: { type: 'tooLarge' }
+    });
+    expect(newState.attachments.visible).to.be.true;
+    expect(newState.attachments.message)
+      .to.eql(composeMessage.errors.attachments['tooLarge']);
+  });
+
+  it('should open an error modal for too many attachments', () => {
+    const newState = modalsReducer(initialState, {
+      type: OPEN_ATTACHMENTS_MODAL,
+      error: { type: 'tooMany' }
+    });
+    expect(newState.attachments.visible).to.be.true;
+    expect(newState.attachments.message)
+      .to.eql(composeMessage.errors.attachments['tooMany']);
   });
 
   it('should close the attachments error modal', () => {
