9c9c8a23dc979e576cb5817ad0c22d16c3860b8b
Kainic <vacokainia@VACOLTKAINIA.local>
2016-10-11 11:37:49 -0400

    Add additional fixes

--- src/js/common/utils/sticky-action-box.js
+++ src/js/common/utils/sticky-action-box.js
@@ -1,32 +1,48 @@
 import _ from 'lodash';
 
-function setActionBoxAtTop(actionBox) {
-  actionBox.style.position = 'fixed';
-  actionBox.style.right = `${(document.body.offsetWidth - 1000) / 2}px`;
-  actionBox.style.top = '10px';
+function setLargeActionBoxAtTop(largeActionBox) {
+  largeActionBox.style.position = 'fixed';
+  largeActionBox.style.right = `${(document.body.offsetWidth - 1000) / 2}px`;
+  largeActionBox.style.top = '10px';
 }
 
-function resetActionBox(actionBox) {
-  actionBox.style.position = 'absolute';
-  actionBox.style.right = 0;
-  actionBox.style.top = '30px';
+function setSmallActionBoxAtTop(smallActionBox) {
+  smallActionBox.style.position = 'fixed';
+  smallActionBox.style.top = '10px';
 }
 
-window.onload = function stickyActionBox() {
-  const actionBox = document.querySelectorAll('.show-for-large-up .sticky-action-box')[0];
-  let offset = actionBox.getBoundingClientRect();
+function resetLargeActionBox(largeActionBox) {
+  largeActionBox.style.position = 'absolute';
+  largeActionBox.style.right = 0;
+  largeActionBox.style.top = '30px';
+}
 
+function resetSmallActionBox(smallActionBox) {
+  smallActionBox.style.position = 'initial';
+  smallActionBox.style.top = 'initial';
+}
+
+function moveActionBoxOnScroll(actionBox, offset, positionFunction, resetFunction) {
   window.addEventListener('scroll', _.throttle(() => {
     if (offset.top < 0) {
-      setActionBoxAtTop(actionBox);
-
+      positionFunction(actionBox);
       offset = actionBox.getBoundingClientRect();
     }
 
     if (document.body.scrollTop > offset.top) {
-      setActionBoxAtTop(actionBox);
+      positionFunction(actionBox);
     } else {
-      resetActionBox(actionBox);
+      resetFunction(actionBox);
     }
   }, 100));
+}
+
+window.onload = function stickyActionBox() {
+  const largeActionBox = document.querySelectorAll('.show-for-large-up .sticky-action-box')[0];
+  const smallActionBox = document.querySelectorAll('.hide-for-large-up .sticky-action-box')[0];
+  let offsetLarge = largeActionBox.getBoundingClientRect();
+  let offsetSmall = smallActionBox.getBoundingClientRect();
+
+  moveActionBoxOnScroll(largeActionBox, offsetLarge, setLargeActionBoxAtTop, resetLargeActionBox);  
+  moveActionBoxOnScroll(smallActionBox, offsetSmall, setSmallActionBoxAtTop, resetSmallActionBox);
 };
