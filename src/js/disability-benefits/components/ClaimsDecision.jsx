0ca366efde2ff53980e135382133edf1195e2ab1
Jeff Balboni <jbalboni@gmail.com>
2016-10-11 16:37:45 -0400

    Claims status page connected to benefits api (#3321)

--- src/js/disability-benefits/components/ClaimsDecision.jsx
+++ src/js/disability-benefits/components/ClaimsDecision.jsx
@@ -5,9 +5,9 @@ class ClaimsDecision extends React.Component {
     return (
       <div className="claim-decision-is-ready usa-alert usa-alert-info claims-no-icon">
         <h4>Your claim decision is ready</h4>
-        <p>VA sent you a claim decision by U.S mail on {"Sep 12, 2016"}. Please allow up to 8 business days for it to arrive.</p>
-        <p>Do you disagree with your claim decision? <a href="/">File an appeal</a></p>
-        <p>If you have new evidence to support your claim and have no yet appealed, you can ask VA to <a href="/">Reopen your claim</a></p>
+        <p>VA sent you a claim decision by U.S mail. Please allow up to 8 business days for it to arrive.</p>
+        <p>Do you disagree with your claim decision? <a href="/disability-benefits/claims-appeal">File an appeal</a></p>
+        <p>If you have new evidence to support your claim and have no yet appealed, you can ask VA to <a href="/disability-benefits/claims-process/claim-types/reopened-claim">Reopen your claim</a></p>
       </div>
     );
   }
