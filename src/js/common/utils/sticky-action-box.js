0d59c18f162163475d2841b210225834cc34486a
Kainic <vacokainia@VACOLTKAINIA.local>
2016-10-11 00:27:10 -0400

    refactor

--- src/js/common/utils/sticky-action-box.js
+++ src/js/common/utils/sticky-action-box.js
@@ -1,19 +1,34 @@
-$(function() {
-  var $actionBox  = $('.show-for-large-up .sticky-action-box'),
-    $window    = $(window),
-    offset     = $actionBox.offset();
+// $(function() {
+//   var $actionBox = $('.show-for-large-up .sticky-action-box'),
+//     $window = $(window),
+//     offset = $actionBox.offset();
 
-	$window.scroll(function() {
-    if ($window.scrollTop() > offset.top) {
-      $actionBox.css({
-      	position: "fixed",
-      	right: ($window.width()-1000)/2
-      });
+// 	$window.scroll(function() {
+//     if ($window.scrollTop() > offset.top) {
+//       $actionBox.css({
+//       	position: "fixed",
+//       	right: ($window.width()-1000)/2
+//       });
+//     } else {
+//       $actionBox.css({
+//       	position: "absolute",
+//       	right: 0
+//       });
+//     }
+//   });
+// });
+
+window.onload = function() {
+  const actionBox = document.querySelectorAll('.show-for-large-up .sticky-action-box')[0],
+    offset = actionBox.getBoundingClientRect();
+
+  window.addEventListener('scroll', function() {
+    if (document.body.scrollTop > offset.top) {
+      actionBox.style.position = "fixed";
+      actionBox.style.right = (document.body.offsetWidth-1000)/2 + "px";
     } else {
-      $actionBox.css({
-      	position: "absolute",
-      	right: 0
-      });
+      actionBox.style.position = "absolute";
+      actionBox.style.right = 0;
     }
   });
-});
+};
