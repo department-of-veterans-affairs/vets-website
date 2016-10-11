ad2bb886839c0ec4bccc81318deca6526229d564
Kainic <vacokainia@VACOLTKAINIA.local>
2016-10-11 00:39:31 -0400

    Fix lint errors

--- src/js/common/utils/sticky-action-box.js
+++ src/js/common/utils/sticky-action-box.js
@@ -1,13 +1,13 @@
-window.onload = function() {
-  const actionBox = document.querySelectorAll('.show-for-large-up .sticky-action-box')[0],
-    offset = actionBox.getBoundingClientRect();
+window.onload = function stickyActionBox() {
+  const actionBox = document.querySelectorAll('.show-for-large-up .sticky-action-box')[0];
+  const offset = actionBox.getBoundingClientRect();
 
-  window.addEventListener('scroll', function() {
+  window.addEventListener('scroll', () => {
     if (document.body.scrollTop > offset.top) {
-      actionBox.style.position = "fixed";
-      actionBox.style.right = (document.body.offsetWidth-1000)/2 + "px";
+      actionBox.style.position = 'fixed';
+      actionBox.style.right = `${(document.body.offsetWidth - 1000) / 2}px`;
     } else {
-      actionBox.style.position = "absolute";
+      actionBox.style.position = 'absolute';
       actionBox.style.right = 0;
     }
   });
