f7941f581c30d8a3a90a2dfe5db3d849575a4870
Jeff Balboni <jeff@adhocteam.us>
2016-10-11 13:48:22 -0400

    Update review and test

--- src/js/edu-benefits/components/benefits-eligibility/BenefitsWaiverReview.jsx
+++ src/js/edu-benefits/components/benefits-eligibility/BenefitsWaiverReview.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { getLabel, displayDateIfValid, showRelinquishedEffectiveDate, getListOfBenefits } from '../../utils/helpers';
+import { getLabel, displayDateIfValid, showRelinquishedEffectiveDate } from '../../utils/helpers';
 import { relinquishableBenefits } from '../../utils/options-for-select';
 
 export default class BenefitsSelectionReview extends React.Component {
@@ -9,24 +9,16 @@ export default class BenefitsSelectionReview extends React.Component {
       <table className="review usa-table-borderless">
         <tbody>
           <tr>
-            <td>Benefits selection:</td>
-            <td>{getListOfBenefits(this.props.data).map((benefit, index) => <span key={index}>{index === 0 ? null : <br/>}{benefit}</span>)}</td>
+            <td>I elect to receive Chapter 33 education benefits in lieu of the education benefit(s) I am giving up below:</td>
+            <td>{getLabel(relinquishableBenefits, this.props.data.benefitsRelinquished.value)}</td>
           </tr>
-        </tbody>
-        {this.props.data.chapter33
-          ? <tbody>
-            <tr>
-              <td>I elect to receive Chapter 33 education benefits in lieu of the education benefit(s) I am giving up below:</td>
-              <td>{getLabel(relinquishableBenefits, this.props.data.benefitsRelinquished.value)}</td>
+          {showRelinquishedEffectiveDate(this.props.data.benefitsRelinquished.value)
+            ? <tr>
+              <td>Effective date:</td>
+              <td>{displayDateIfValid(this.props.data.benefitsRelinquishedDate)}</td>
             </tr>
-            {showRelinquishedEffectiveDate(this.props.data.benefitsRelinquished.value)
-              ? <tr>
-                <td>Effective date:</td>
-                <td>{displayDateIfValid(this.props.data.benefitsRelinquishedDate)}</td>
-              </tr>
-              : null}
-          </tbody>
-          : null}
+            : null}
+        </tbody>
       </table>
     );
   }
