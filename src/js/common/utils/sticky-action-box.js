21a1ae374967a15f48aa674330a84c3c71488886
Kainic <vacokainia@VACOLTKAINIA.local>
2016-10-11 00:27:39 -0400

    Working without jQuery

--- src/js/common/utils/sticky-action-box.js
+++ src/js/common/utils/sticky-action-box.js
@@ -1,23 +1,3 @@
-// $(function() {
-//   var $actionBox = $('.show-for-large-up .sticky-action-box'),
-//     $window = $(window),
-//     offset = $actionBox.offset();
-
-// 	$window.scroll(function() {
-//     if ($window.scrollTop() > offset.top) {
-//       $actionBox.css({
-//       	position: "fixed",
-//       	right: ($window.width()-1000)/2
-//       });
-//     } else {
-//       $actionBox.css({
-//       	position: "absolute",
-//       	right: 0
-//       });
-//     }
-//   });
-// });
-
 window.onload = function() {
   const actionBox = document.querySelectorAll('.show-for-large-up .sticky-action-box')[0],
     offset = actionBox.getBoundingClientRect();
