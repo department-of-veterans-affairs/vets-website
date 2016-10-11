9068645096c202297d810f2a88fbf02d8b956eee
Kainic <vacokainia@VACOLTKAINIA.local>
2016-10-11 11:01:32 -0400

    Add throttling and fix js bug

--- src/js/common/utils/sticky-action-box.js
+++ src/js/common/utils/sticky-action-box.js
@@ -1,16 +1,32 @@
+import _ from 'lodash';
+
+function setActionBoxAtTop(actionBox) {
+  actionBox.style.position = 'fixed';
+  actionBox.style.right = `${(document.body.offsetWidth - 1000) / 2}px`;
+  actionBox.style.top = '10px';
+}
+
+function resetActionBox(actionBox) {
+  actionBox.style.position = 'absolute';
+  actionBox.style.right = 0;
+  actionBox.style.top = '30px';
+}
+
 window.onload = function stickyActionBox() {
   const actionBox = document.querySelectorAll('.show-for-large-up .sticky-action-box')[0];
-  const offset = actionBox.getBoundingClientRect();
+  let offset = actionBox.getBoundingClientRect();
 
-  window.addEventListener('scroll', () => {
-    if (document.body.scrollTop > offset.top) {
-      actionBox.style.position = 'fixed';
-      actionBox.style.right = `${(document.body.offsetWidth - 1000) / 2}px`;
-      actionBox.style.top = '10px';
-    } else {
-      actionBox.style.position = 'absolute';
-      actionBox.style.right = 0;
-      actionBox.style.top = 0;
+  window.addEventListener('scroll', _.throttle(() => {
+    if (offset.top < 0) {
+      setActionBoxAtTop(actionBox);
+
+      offset = actionBox.getBoundingClientRect();
     }
-  });
+
+    if (document.body.scrollTop > offset.top) {
+      setActionBoxAtTop(actionBox);
+    } else {
+      resetActionBox(actionBox);
+    }
+  }, 100));
 };
