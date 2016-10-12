bb2a8fc6527d6664bc5eb2ba608a1a48ac1b7bfb
Courtney Eimerman-Wallace <crwallace39@gmail.com>
2016-10-12 17:56:37 -0400

    adds date and personal data in profile

--- src/js/user-profile/components/UserDataSection.jsx
+++ src/js/user-profile/components/UserDataSection.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import moment from 'moment';
+
 class UserDataSection extends React.Component {
   render() {
     return (
       <div className="profile-section medium-12 columns">
         <h4 className="section-header">User Profile</h4>
         <div className="info-conatiner medium-8 columns">
-          <p><span className="label medium-4 columns">Member Name:</span>Courtney Eimerman-Wallace</p>
-          <p><span className="label medium-4 columns">Sex:</span>Female</p>
-          <p><span className="label medium-4 columns">Date of Birth:</span>03/09/1988</p>
+          <p><span className="label medium-4 columns">Member Name:</span>{this.props.name.first} {this.props.name.middle} {this.props.name.last} {this.props.name.suffix}</p>
+          <p><span className="label medium-4 columns">Sex:</span>{`${this.props.profile.gender ? this.props.profile.gender : 'Not Provided'}`}</p>
+          <p><span className="label medium-4 columns">Date of Birth:</span>{moment(`${this.props.profile.dob}`).format('MM/DD/YYYY')}</p>
         </div>
       </div>
     );
@@ -18,7 +20,10 @@ class UserDataSection extends React.Component {
 
 // TODO: fill this out
 const mapStateToProps = (state) => {
-  return state;
+  return {
+    name: state.profile.userFullName,
+    profile: state.profile
+  };
 };
 
 // TODO(awong): Remove the pure: false once we start using ImmutableJS.
