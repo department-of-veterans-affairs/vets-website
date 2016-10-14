63cb11a3ec00252b393cf6f62ba4c86a96cc2efe
Tiffany Brown <tiffany@webinista.com>
2016-10-13 17:16:47 -0700

    Reordering contact card content. Closes #3377.
    Part of #3306.
    
    - Changed margins as needed.
    - Refactored rx-message-provider-link styles.

--- src/js/rx/components/MessageProviderLink.jsx
+++ src/js/rx/components/MessageProviderLink.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class MessageProviderLink extends React.Component {
   render() {
     // TODO: Link to secure messaging.
-    return <a className="rx-message-provider-link">Message my provider</a>;
+    return <a className="usa-button rx-message-provider-link">Message my provider</a>;
   }
 }
 
