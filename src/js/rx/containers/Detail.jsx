4f2e02ba978f3e289dc9e009db43b77e24007782
Eugene Doan <eugene@adhocteam.us>
2016-10-17 15:01:51 -0400

    Fixed accidentally duplicated JSX.

--- src/js/rx/containers/Detail.jsx
+++ src/js/rx/containers/Detail.jsx
@@ -114,14 +114,9 @@ export class Detail extends React.Component {
         phoneNumber = currentPackage.rxInfoPhoneNumber;
 
         orderHistoryTable = (
-          <ScrollElement
-              id="rx-order-history"
-              name="orderHistory">
-            <h3 className="rx-heading va-h-ruled">Order History</h3>
-            <OrderHistory
-                className="usa-table-borderless rx-table rx-table-list"
-                items={item.trackings}/>
-          </ScrollElement>
+          <OrderHistory
+              className="usa-table-borderless rx-table rx-table-list"
+              items={item.trackings}/>
         );
       }
 
