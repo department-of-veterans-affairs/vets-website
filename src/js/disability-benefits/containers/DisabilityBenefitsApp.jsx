721bbf21abd1cce21ab12f8abe4540be3240729a
Jeff Balboni <jbalboni@gmail.com>
2016-10-20 12:03:23 -0400

    Add feature warning banner to claims status (#3465)

--- src/js/disability-benefits/containers/DisabilityBenefitsApp.jsx
+++ src/js/disability-benefits/containers/DisabilityBenefitsApp.jsx
@@ -3,15 +3,20 @@ import { connect } from 'react-redux';
 
 import ClaimsUnavailable from '../components/ClaimsUnavailable';
 import ClaimSyncWarning from '../components/ClaimSyncWarning';
+import FeaturesWarning from '../components/FeaturesWarning';
 
 class DisabilityBenefitsApp extends React.Component {
 
   render() {
     const { available, synced, syncedDate } = this.props;
+    const isListPage = this.props.location.pathname === '/your-claims';
     return (
       <div>
         {available && !synced
           ? <ClaimSyncWarning syncedDate={syncedDate}/>
+          : null}
+        {available && synced && isListPage
+          ? <FeaturesWarning/>
           : null}
         {available
           ? <div>
