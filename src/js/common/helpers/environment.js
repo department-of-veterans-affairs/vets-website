d243bbe73f9c393fb2faf8d4d7d03f8ca2862df5
Courtney Eimerman-Wallace <crwallace@users.noreply.github.com>
2016-10-24 12:53:14 -0400

    changes env variables for correct environments

--- src/js/common/helpers/environment.js
+++ src/js/common/helpers/environment.js
@@ -1,7 +1,7 @@
 const _Environments = {
   staging: { API_URL: 'https://staging-api.vets.gov', BASE_URL: 'https://staging.vets.gov' },
-  production: { API_URL: 'https://dev-api.vets.gov', BASE_URL: 'https://dev.vets.gov' },
-  development: { API_URL: 'http://localhost:3000', BASE_URL: 'http://localhost:3001' },
+  development: { API_URL: 'https://dev-api.vets.gov', BASE_URL: 'https://dev.vets.gov' },
+  local: { API_URL: 'http://localhost:3000', BASE_URL: 'http://localhost:3001' },
 };
 
 function getEnvironment() {
