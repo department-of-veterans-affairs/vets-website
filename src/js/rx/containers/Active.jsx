4a219dce5f7dfbe19f2a656bc3efdfe0c77f9fa0
Tiffany Brown <tiffany@webinista.com>
2016-10-24 09:31:04 -0700

    Setting a class name for the default sort value. closes #3501
    - Sets defaultCssClass when the option value === this.props.selected

--- src/js/rx/containers/Active.jsx
+++ src/js/rx/containers/Active.jsx
@@ -42,9 +42,11 @@ class Active extends React.Component {
   render() {
     const items = this.props.prescriptions.items;
     let content;
+    
+    const sortParam = this.props.location.query.sort;
 
     if (items) {
-      const sortValue = this.props.location.query.sort;
+      const sortValue = sortParam ? sortParam : 'lastRequested';
 
       content = (
         <div>
