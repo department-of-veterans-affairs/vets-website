96deddb8e23d9e6873e9827959b8d49b017bcd20
Alexander Ose <alex@alexose.com>
2016-10-25 14:17:28 -0700

    Make ROTC year optional (#3400)
    * make ROTC year optional
    
    * update validation function
    
    * fix validation logic
    
    * add new validation for optional year

--- src/js/edu-benefits/components/military-history/RotcHistoryFields.jsx
+++ src/js/edu-benefits/components/military-history/RotcHistoryFields.jsx
@@ -5,7 +5,7 @@ import ExpandingGroup from '../../../common/components/form-elements/ExpandingGr
 import GrowableTable from '../../../common/components/form-elements/GrowableTable';
 import RotcScholarship from './RotcScholarship';
 import { createRotcScholarship } from '../../utils/veteran';
-import { isValidPage, validateIfDirty, isValidYear, isValidRotcScholarshipAmount } from '../../utils/validations';
+import { isValidPage, validateIfDirty, isValidYearOrBlank, isValidRotcScholarshipAmount } from '../../utils/validations';
 import { yesNo } from '../../utils/options-for-select';
 
 export default class RotcHistoryFields extends React.Component {
@@ -27,9 +27,9 @@ export default class RotcHistoryFields extends React.Component {
             onValueChange={(update) => {this.props.onStateChange('seniorRotcCommissioned', update);}}/>
         <div>
           <div className="input-section">
-            <ErrorableNumberInput required
+            <ErrorableNumberInput
                 additionalClass="usa-input-medium"
-                errorMessage={validateIfDirty(this.props.data.seniorRotc.commissionYear, isValidYear) ? undefined : 'Please enter a valid year'}
+                errorMessage={validateIfDirty(this.props.data.seniorRotc.commissionYear, isValidYearOrBlank) ? undefined : 'Please enter a valid year'}
                 label="Year of commission"
                 min="1900"
                 name="commissionYear"
