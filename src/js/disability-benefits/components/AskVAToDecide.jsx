7aae5eace626fd8dbbafae41721346dd74043a4d
Maury Lindo <maury@cagedcrown.com>
2016-10-26 11:27:59 -0400

    Update text on ask va page

--- src/js/disability-benefits/components/AskVAToDecide.jsx
+++ src/js/disability-benefits/components/AskVAToDecide.jsx
@@ -5,9 +5,9 @@ class AskVAToDecide extends React.Component {
   render() {
     return (
       <div className="usa-alert usa-alert-info claims-no-icon claims-alert-status">
-        <h4>Ask VA to decide your claims</h4>
+        <h4>Ask VA for a Claim Decision</h4>
         <p>
-          You can ask VA to start evaluating your claim if you don't have anymore documents or evidence to file.
+          You can ask VA to start evaluating your claim if you don't have any more documents or evidence to file.
         </p>
         <button
             className="va-button-secondary request-decision-button"
