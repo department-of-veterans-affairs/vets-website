a8a8806adfd275bdc84225a2dbc49075cbabea78
Tiffany Brown <webinista@users.noreply.github.com>
2016-10-26 16:26:01 -0700

    Makes simple search request (#3552)
    * Makes simple search request
    
    WIP for department-of-veterans-affairs/messaging-fe#119
    
    - Added SET_SEARCH, SEARCH_SUCCESS, SEARCH_FAILURE constants.
    - Changed messaging search input to use ErrorableTextInput.
    - Passes current folder ID to MessageSearch component.
    - Added SET_SEARCH constant
    - Adding an onSearchChange handler.
    - Updating search state structure to be {params, advanced}
    - Updating PropTypes definitions for MessageSearch, MessageSearchAdvanced
    
    Related to department-of-veterans-affairs/messaging-fe#93
    
    * Updating props, search params path, removing action / reducer
    
    - Added onFieldChange, params props to MessageSearchAdvanced.
    - Made params prop required in MessageSearch, MessageSearchAdvanced.
    - Changing this.props.params to this.props.attributes.
    - Removed SEARCH_SUCCESS action / reducer (to be resolved in a follow up PR)
    - Modified object path of for search results.

--- src/js/messaging/actions/search.js
+++ src/js/messaging/actions/search.js
@@ -1,8 +1,15 @@
+import { api } from '../config';
+
 import {
-  TOGGLE_ADVANCED_SEARCH,
+  SEARCH_SUCCESS,
+  SEARCH_FAILURE,
   SET_ADVSEARCH_START_DATE,
-  SET_ADVSEARCH_END_DATE
+  SET_ADVSEARCH_END_DATE,
+  SET_SEARCH_PARAM,
+  TOGGLE_ADVANCED_SEARCH,
 } from '../utils/constants';
+
+const baseUrl = `${api.url}/folders`;
 
 export function toggleAdvancedSearch() {
   return {
@@ -18,5 +25,67 @@ export function setDateRange(date, start = true) {
   return {
     type: mode,
     date
+  };
+}
+
+export function setSearchParam(path, field) {
+  return {
+    type: SET_SEARCH_PARAM,
+    path,
+    field
+  };
+}
+
+export function sendSearch(params, folderId = 0) {
+  const term = params.term.value;
+  const from = params.from;
+  const subject = params.subject;
+  const dateRange = params.dateRange;
+  const filters = [];
+
+  if (params.term.value) {
+    filters.push(`filter[[subject][match]]=${term}`);
+  }
+
+  if (from.field.value) {
+    const fromExact = from.exact ? 'eq' : 'match';
+    filters.push(`filter[[sender_name][${fromExact}]]=${params.from.field.value}`);
+  }
+
+  if (subject.field.value) {
+    const subjectExact = subject.exact ? 'eq' : 'match';
+    filters.push(`filter[[subject][${subjectExact}]]=${params.subject.field.value}`);
+  }
+
+  if (dateRange.start) {
+    filters.push(`filter[[sent_date][gteq]]=${dateRange.start.format()}`);
+  }
+
+  if (dateRange.end) {
+    filters.push(`filter[[sent_date][lteq]]=${dateRange.end.format()}`);
+  }
+
+  const searchURL = `${baseUrl}/${folderId}/messages?${filters.join('&')}`;
+
+  return dispatch => {
+    fetch(searchURL, api.settings.get)
+    .then(response => {
+      return response.json();
+    }).then(
+      data => {
+        if (data.errors) {
+          return dispatch({
+            type: SEARCH_FAILURE,
+            error: data.errors
+          });
+        }
+
+        return dispatch({
+          type: SEARCH_SUCCESS,
+          messages: data
+        });
+      },
+      error => dispatch({ type: SEARCH_FAILURE, error })
+    );
   };
 }
