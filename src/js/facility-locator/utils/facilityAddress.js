5136f23992a205d55dbb9925fb56a25d090ed70e
Benjamin Shyong <ben.shyong@gmail.com>
2016-10-27 11:42:46 -0700

    Use unifed address format for facilities

--- src/js/facility-locator/utils/facilityAddress.js
+++ src/js/facility-locator/utils/facilityAddress.js
@@ -1,21 +1,12 @@
 import { compact } from 'lodash';
 
 export function buildAddressArray(facility) {
-  const { address: { physical: address }, facility_type: facilityType } = facility.attributes;
+  const { address: { physical: address } } = facility.attributes;
 
-  switch (facilityType) {
-    case 'va_health_facility':
-      return [
-        compact([address.building, address.street, address.suite]).join(' '),
-        `${address.city}, ${address.state} ${address.zip}`
-      ];
-    case 'va_cemetery':
-      return compact([
-        address.address1,
-        address.address2,
-        `${address.city}, ${address.state} ${address.zip}`
-      ]);
-    default:
-      return [];
-  }
+  return compact([
+    address.address_1,
+    address.address_2,
+    address.address_3,
+    `${address.city}, ${address.state} ${address.zip}`
+  ]);
 }
