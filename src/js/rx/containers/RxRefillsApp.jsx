e4c19cc75f134bc93b04456add979b13299c581d
Tiffany Brown <tiffany@webinista.com>
2016-10-27 08:52:01 -0700

    Revert "Rx and SM require login and pass user tokens in requests. (#3530)"
    This reverts commit 2914bc8073c3aec93abd389d92c5533f47aaa768.

--- src/js/rx/containers/RxRefillsApp.jsx
+++ src/js/rx/containers/RxRefillsApp.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import RequiredLoginView from '../../common/components/RequiredLoginView';
-import { openAlert } from '../actions/alert.js';
-import { openRefillModal, closeRefillModal, closeGlossaryModal } from '../actions/modal.js';
-import { refillPrescription } from '../actions/prescriptions.js';
 import ConfirmRefillModal from '../components/ConfirmRefillModal';
 import GlossaryModal from '../components/GlossaryModal';
 
+import { openAlert } from '../actions/alert.js';
+import { openRefillModal, closeRefillModal, closeGlossaryModal } from '../actions/modal.js';
+import { refillPrescription } from '../actions/prescriptions.js';
+
 class RxRefillsApp extends React.Component {
   render() {
-    const view = (
+    return (
       <div>
         {this.props.children}
         <ConfirmRefillModal
@@ -24,10 +24,6 @@ class RxRefillsApp extends React.Component {
             isVisible={this.props.modal.glossary.visible}
             onCloseModal={this.props.closeGlossaryModal}/>
       </div>
-    );
-
-    return (
-      <RequiredLoginView authRequired={3} component={view}/>
     );
   }
 }
