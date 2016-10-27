b1bab82a792927e4f769768ede627fd5368d292c
Eugene Doan <eugene@adhocteam.us>
2016-10-27 12:18:56 -0400

    Fix lint errors.

--- src/js/messaging/containers/MessagingApp.jsx
+++ src/js/messaging/containers/MessagingApp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import AlertBox from '../../common/components/AlertBox';
-import RequiredLoginView from '../../common/components/RequiredLoginView';
+// import RequiredLoginView from '../../common/components/RequiredLoginView';
 
 import {
   closeAlert,
