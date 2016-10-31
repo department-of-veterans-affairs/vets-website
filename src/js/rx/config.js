c6fcc34ef1c50d4e925936a4cb74df94e94c5d9c
Tiffany Brown <tiffany@webinista.com>
2016-10-31 14:05:43 -0700

    Removing 'Pending' status from glossary.

--- src/js/rx/config.js
+++ src/js/rx/config.js
@@ -32,10 +32,6 @@ module.exports = {
     definition: 'This prescription cannot be refilled until a hold is resolved by the pharmacy. Contact your VA pharmacy if you need this prescription now.'
   },
   {
-    term: 'Pending',
-    definition: 'This prescription was sent to the pharmacy for review. Contact your VA pharmacy if you need this medication now.'
-  },
-  {
     term: 'Submitted',
     definition: 'Your prescription refill has been submitted.'
   },
