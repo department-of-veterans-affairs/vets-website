487174981dbb3db04cd366f7697a1a148cfad7fb
Courtney Eimerman-Wallace <crwallace39@gmail.com>
2016-10-31 18:31:58 -0400

    updates profile to show view for verified users

--- src/js/user-profile/containers/UserProfileApp.jsx
+++ src/js/user-profile/containers/UserProfileApp.jsx
@@ -14,7 +14,7 @@ class UserProfileApp extends React.Component {
     let account;
     let view;
 
-    if (this.props.profile.accountType === 'loa3') {
+    if (this.props.profile.accountType === 3) {
       components = (
         <div>
           <UserDataSection/>
