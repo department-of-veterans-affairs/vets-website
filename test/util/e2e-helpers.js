c56aac669483b76673d8fc04c7dcf72b040ad42a
Jeff Balboni <jbalboni@gmail.com>
2016-11-01 22:56:20 -0400

    Set up for e2e and 508 tests behind login component (#3643)

--- test/util/e2e-helpers.js
+++ test/util/e2e-helpers.js
@@ -29,7 +29,7 @@ function overrideVetsGovApi(client) {
 }
 
 module.exports = {
-  baseUrl: 'http://localhost:3001',
+  baseUrl: 'http://localhost:3333',
   apiUrl: 'http://localhost:4000',
   createE2eTest,
   expectNavigateAwayFrom,
