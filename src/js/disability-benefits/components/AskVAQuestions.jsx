7e092dd26b92e7aa3fea04d41f2f3c979f30e7d3
Jeff Balboni <jeff@adhocteam.us>
2016-11-02 08:30:54 -0400

    Remove 9

--- src/js/disability-benefits/components/AskVAQuestions.jsx
+++ src/js/disability-benefits/components/AskVAQuestions.jsx
@@ -6,7 +6,7 @@ class AskVAQuestions extends React.Component {
       <div className="ask-va-questions claims-sidebar-box">
         <h4 className="claims-sidebar-header">Questions?</h4>
         <p className="talk">Talk to a VA representative:</p>
-        <p className="phone-number"><a href="tel:+1-800-827-10009">1-800-827-1000</a></p>
+        <p className="phone-number"><a href="tel:+1-800-827-1000">1-800-827-1000</a></p>
         <p><a href="https://iris.custhelp.com/">Submit a question to the VA</a></p>
       </div>
     );
