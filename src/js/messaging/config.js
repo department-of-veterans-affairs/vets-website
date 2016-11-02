8dd6d1d4c375825bc6c3b238faf71a3ca5bc8be4
Eugene Doan <eugene@adhocteam.us>
2016-11-02 14:13:18 -0400

    Reverting Rx/Messaging workaround to use proxy instead of local API.

--- src/js/messaging/config.js
+++ src/js/messaging/config.js
@@ -13,9 +13,9 @@ function getHeaders(optionalHeaders = {}) {
 }
 
 function getApiUrl() {
-  let url = '/api';
+  let url;
 
-  if (environment && environment.BASE_URL !== 'http://localhost:3001') {
+  if (environment) {
     url = environment.API_URL;
   }
 
