3e6a4fae191909bf9a6b7851741a8a284feef1e7
Eugene Doan <eugene@adhocteam.us>
2016-11-01 20:47:53 -0400

    Renamed modals reducer module.

--- src/js/rx/reducers/index.js
+++ src/js/rx/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 import alert from './alert';
 import disclaimer from './disclaimer';
-import modals from './modal';
+import modals from './modals';
 import prescriptions from './prescriptions';
 
 export default combineReducers({
