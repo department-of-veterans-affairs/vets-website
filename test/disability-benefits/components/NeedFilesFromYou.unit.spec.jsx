ea50c7377d081bc18b199a02edbf728d3e2642c7
Jeff Balboni <jeff@adhocteam.us>
2016-11-02 08:17:34 -0400

    Add test

--- test/disability-benefits/components/NeedFilesFromYou.unit.spec.jsx
+++ test/disability-benefits/components/NeedFilesFromYou.unit.spec.jsx
@@ -15,6 +15,19 @@ describe('<NeedFilesFromYou>', () => {
     expect(tree.text()).to.contain('item needs');
     expect(tree.subTree('Link').props.to).to.equal('your-claims/2/files');
   });
+  it('should render count based on only need from you files', () => {
+    const tree = SkinDeep.shallowRender(
+      <NeedFilesFromYou
+          events={[
+            { type: 'still_need_from_others_list' },
+            { type: 'still_need_from_you_list' }
+          ]}
+          claimId={2}/>
+    );
+
+    expect(tree.text()).to.contain('item needs');
+    expect(tree.subTree('Link').props.to).to.equal('your-claims/2/files');
+  });
   it('should render multiple files needed', () => {
     const events = Array(2).fill({
       type: 'still_need_from_you_list'
