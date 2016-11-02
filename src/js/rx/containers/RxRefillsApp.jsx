d77343bac483854fee5a39688a95dd7ddccfe83f
Eugene Doan <eugene@adhocteam.us>
2016-11-01 20:34:26 -0400

    Specified mapStateToProps and mapDispatchToProps for RxRefillsApp.

--- src/js/rx/containers/RxRefillsApp.jsx
+++ src/js/rx/containers/RxRefillsApp.jsx
@@ -21,14 +21,14 @@ class RxRefillsApp extends React.Component {
           {this.props.children}
         </div>
         <ConfirmRefillModal
-            {...this.props.modal.refill.prescription}
-            isVisible={this.props.modal.refill.visible}
+            {...this.props.refillModal.prescription}
+            isVisible={this.props.refillModal.visible}
             openAlert={this.props.openAlert}
             refillPrescription={this.props.refillPrescription}
             onCloseModal={this.props.closeRefillModal}/>
         <GlossaryModal
-            content={this.props.modal.glossary.content}
-            isVisible={this.props.modal.glossary.visible}
+            content={this.props.glossaryModal.content}
+            isVisible={this.props.glossaryModal.visible}
             onCloseModal={this.props.closeGlossaryModal}/>
       </div>
     );
@@ -49,17 +49,23 @@ RxRefillsApp.propTypes = {
   children: React.PropTypes.element
 };
 
-// TODO: fill this out
 const mapStateToProps = (state) => {
-  return state;
+  const modals = state.modals;
+
+  return {
+    disclaimer: state.disclaimer,
+    glossaryModal: modals.glossary,
+    refillModal: modals.refill
+  };
 };
 
-export default connect(
-  mapStateToProps, {
-    openAlert,
-    openRefillModal,
-    closeDisclaimer,
-    closeGlossaryModal,
-    closeRefillModal,
-    refillPrescription
-  })(RxRefillsApp);
+const mapDispatchToProps = {
+  openAlert,
+  openRefillModal,
+  closeDisclaimer,
+  closeGlossaryModal,
+  closeRefillModal,
+  refillPrescription
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RxRefillsApp);
