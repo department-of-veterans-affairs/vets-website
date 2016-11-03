c11a2535195419cf8b983d90bb482597564bf994
mark olson <markolson@users.noreply.github.com>
2016-11-03 09:27:22 -0400

    Use the production backend in production

--- src/js/common/helpers/environment.js
+++ src/js/common/helpers/environment.js
@@ -1,5 +1,5 @@
 const _Environments = {
-  production: { API_URL: 'https://dev-api.vets.gov', BASE_URL: 'https://www.vets.gov' },
+  production: { API_URL: 'https://api.vets.gov', BASE_URL: 'https://www.vets.gov' },
   staging: { API_URL: 'https://staging-api.vets.gov', BASE_URL: 'https://staging.vets.gov' },
   development: { API_URL: 'https://dev-api.vets.gov', BASE_URL: 'https://dev.vets.gov' },
   local: { API_URL: 'http://localhost:3000', BASE_URL: 'http://localhost:3001' }
