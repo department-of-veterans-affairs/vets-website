30387a1c837e1e322af68c19a00dd23d826a9257
Jeff Balboni <jeff@adhocteam.us>
2016-11-03 17:30:20 -0400

    Add see all link to claim detail header

--- src/js/disability-benefits/components/ClaimDetailLayout.jsx
+++ src/js/disability-benefits/components/ClaimDetailLayout.jsx
@@ -27,8 +27,11 @@ export default class ClaimDetailLayout extends React.Component {
             <h6>Your Claimed Conditions:</h6>
             <p className="list">
               {claim.attributes.contentionList && claim.attributes.contentionList.length
-                ? claim.attributes.contentionList.join(', ')
+                ? claim.attributes.contentionList.slice(0, 3).join(', ')
                 : 'Not available'}
+              {claim.attributes.contentionList && claim.attributes.contentionList.length > 3
+                  ? <span><br/><Link to={`your-claims/${claim.id}/details`}>See all</Link></span>
+                : null}
             </p>
           </div>
           <TabNav id={this.props.claim.id}/>
