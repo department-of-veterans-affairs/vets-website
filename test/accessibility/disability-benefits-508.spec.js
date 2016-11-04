efbdc6a30d90310fb63ff1d55804e92b37c9f2a9
Kainic <vacokainia@VACOLTKAINIA.local>
2016-11-03 21:38:33 -0400

    Wrap new accessibility tests in buildtype guards

--- test/accessibility/disability-benefits-508.spec.js
+++ test/accessibility/disability-benefits-508.spec.js
@@ -1,35 +1,37 @@
-const E2eHelpers = require('../util/e2e-helpers');
-const Timeouts = require('../util/timeouts.js');
-const DisabilityHelpers = require('../util/disability-helpers');
-const LoginHelpers = require('../util/login-helpers');
+if (!process.env.BUILDTYPE || process.env.BUILDTYPE === 'development') {
+  const E2eHelpers = require('../util/e2e-helpers');
+  const Timeouts = require('../util/timeouts.js');
+  const DisabilityHelpers = require('../util/disability-helpers');
+  const LoginHelpers = require('../util/login-helpers');
 
-module.exports = E2eHelpers.createE2eTest(
-  (client) => {
-    DisabilityHelpers.initClaimsListMock();
-    DisabilityHelpers.initClaimDetailMocks();
+  module.exports = E2eHelpers.createE2eTest(
+    (client) => {
+      DisabilityHelpers.initClaimsListMock();
+      DisabilityHelpers.initClaimDetailMocks();
 
-    LoginHelpers.logIn(client, '/disability-benefits/track-claims', 3);
+      LoginHelpers.logIn(client, '/disability-benefits/track-claims', 3);
 
-    // Claim list page
-    client
-      .url(`${E2eHelpers.baseUrl}/disability-benefits/track-claims`)
-      .waitForElementVisible('a.claim-list-item', Timeouts.normal)
-      .axeCheck('.main');
+      // Claim list page
+      client
+        .url(`${E2eHelpers.baseUrl}/disability-benefits/track-claims`)
+        .waitForElementVisible('a.claim-list-item', Timeouts.normal)
+        .axeCheck('.main');
 
-    client
-      .click('a.claims-combined')
-      .waitForElementVisible('.claims-status-upload-header', Timeouts.normal);
+      client
+        .click('a.claims-combined')
+        .waitForElementVisible('.claims-status-upload-header', Timeouts.normal);
 
-    client
-      .axeCheck('.main');
+      client
+        .axeCheck('.main');
 
-    // Claim status tab
-    client
-      .click('a.claim-list-item:first-child')
-      .waitForElementVisible('.claim-title', Timeouts.normal)
-      .axeCheck('.main');
+      // Claim status tab
+      client
+        .click('a.claim-list-item:first-child')
+        .waitForElementVisible('.claim-title', Timeouts.normal)
+        .axeCheck('.main');
 
-    client.end();
-  }
-);
+      client.end();
+    }
+  );
+}
 
