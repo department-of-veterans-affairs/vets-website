70824b40ad4a3c3dceb73e1d7935092fc4e3b00d
Maury Lindo <mo@cagedcrown.com>
2016-11-04 19:39:08 -0400

    Style fixes for disability claims (#3739)
    * Initial commit
    
    * Fix typo
    
    * Move button location
    
    * Change column width of questons on ask va page
    
    * Remove unused styles
    
    * Fix js lint errors
    
    * Fix Ask VA placement

--- src/js/disability-benefits/components/AskVAToDecide.jsx
+++ src/js/disability-benefits/components/AskVAToDecide.jsx
@@ -4,16 +4,19 @@ import { Link } from 'react-router';
 export default class AskVAToDecide extends React.Component {
   render() {
     return (
-      <div className="usa-alert usa-alert-info claims-no-icon claims-alert-status">
-        <h4>Ask VA for a Claim Decision</h4>
-        <p>
-          You can ask VA to start evaluating your claim if you don't have any more documents or evidence to file.
-        </p>
+      <div className="usa-alert usa-alert-info claims-no-icon claims-alert-status ask-va-alert">
+        <div className="item-title-container">
+          <h4>Ask VA for a Claim Decision</h4>
+        </div>
         <Link
-            className="usa-button"
+            className="usa-button view-details-button"
             to={`/your-claims/${this.props.id}/ask-va-to-decide`}>
           View Details
         </Link>
+        <div className="clearfix"></div>
+        <p>
+          You can ask VA to start evaluating your claim if you don't have any more documents or evidence to file.
+        </p>
       </div>
     );
   }
@@ -22,4 +25,3 @@ export default class AskVAToDecide extends React.Component {
 AskVAToDecide.propTypes = {
   id: React.PropTypes.string.isRequired
 };
-
