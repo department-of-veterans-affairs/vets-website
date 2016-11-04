59bab51880b11a1f339f58c11ff8361a16926981
Maury Lindo <maury@cagedcrown.com>
2016-11-04 13:10:14 -0400

    Fix js lint errors

--- src/js/disability-benefits/components/AskVAToDecide.jsx
+++ src/js/disability-benefits/components/AskVAToDecide.jsx
@@ -8,12 +8,12 @@ export default class AskVAToDecide extends React.Component {
         <div className="item-title-container">
           <h4>Ask VA for a Claim Decision</h4>
         </div>
-          <Link
-              className="usa-button view-details-button"
-              to={`/your-claims/${this.props.id}/ask-va-to-decide`}>
-            View Details
-          </Link>
-          <div className="clearfix"></div>
+        <Link
+            className="usa-button view-details-button"
+            to={`/your-claims/${this.props.id}/ask-va-to-decide`}>
+          View Details
+        </Link>
+        <div className="clearfix"></div>
         <p>
           You can ask VA to start evaluating your claim if you don't have any more documents or evidence to file.
         </p>
