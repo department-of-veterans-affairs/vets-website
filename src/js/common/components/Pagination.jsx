abbdef905228d3d48b8ffa61aea621cf5d570af9
Tiffany Brown <tiffany@webinista.com>
2016-11-07 15:05:27 -0800

    Offset pages when there are more than 10. closes #3339
    - Restyling pagination to remove flexbox.
    - Moving next/previous links outside of va-pagination-inner.

--- src/js/common/components/Pagination.jsx
+++ src/js/common/components/Pagination.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import classNames from 'classnames';
+import range from 'lodash/fp/range';
 
 class Pagination extends React.Component {
   constructor(props) {
     super(props);
     this.next = this.next.bind(this);
     this.prev = this.prev.bind(this);
+    this.pageNumbers = this.pageNumbers.bind(this);
   }
 
   next() {
     let nextPage;
     if (this.props.pages > this.props.page) {
       nextPage = (
-        <a className="va-pagination-next" onClick={() => {this.props.onPageSelect(this.props.page + 1);}}>
+        <a onClick={() => {this.props.onPageSelect(this.props.page + 1);}}>
           Next
         </a>
       );
@@ -24,43 +26,50 @@ class Pagination extends React.Component {
     let prevPage;
     if (this.props.page > 1) {
       prevPage = (
-        <a className="va-pagination-prev" onClick={() => {this.props.onPageSelect(this.props.page - 1);}}>
-          Previous
+        <a onClick={() => {this.props.onPageSelect(this.props.page - 1);}}>
+          <abbr title="Previous">Prev</abbr>
         </a>
       );
     }
     return prevPage;
   }
 
+  pageNumbers(limit) {
+    const totalPages = this.props.pages;
+    // If there are more pages returned than the limit to show
+    // cap the upper range at limit + the page number.
+    const end = (totalPages > limit) ? (limit + this.props.page) : totalPages;
+
+    return range(this.props.page, end);
+  }
 
   render() {
     if (this.props.pages === 1) {
       return <div/>;
     }
 
-    const pageList = Array(this.props.pages).fill().map((e, i) => {
-      const pageNumber = i + 1;
+    const pageList = this.pageNumbers(10).map((e) => {
       const pageClass = classNames({
-        'va-pagination-active': this.props.page === pageNumber
+        'va-pagination-active': this.props.page === e
       });
 
       return (
         <a
-            key={pageNumber}
+            key={e}
             className={pageClass}
-            onClick={() => this.props.onPageSelect(pageNumber)}>
-          {pageNumber}
+            onClick={() => this.props.onPageSelect(e)}>
+          {e}
         </a>
       );
     });
 
     return (
       <div className="va-pagination">
+        <span className="va-pagination-prev">{this.prev()}</span>
         <div className="va-pagination-inner">
-          {this.prev()}
           {pageList}
-          {this.next()}
         </div>
+        <span className="va-pagination-next">{this.next()}</span>
       </div>
     );
   }
