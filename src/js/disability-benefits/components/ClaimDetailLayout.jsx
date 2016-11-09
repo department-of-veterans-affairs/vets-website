66dc9cbc594b900eb8b57f2a170242b939f5b608
Jeff Balboni <jbalboni@gmail.com>
2016-11-09 08:55:47 -0500

    Add aria and focus management to loading indicator (#3806)

--- src/js/disability-benefits/components/ClaimDetailLayout.jsx
+++ src/js/disability-benefits/components/ClaimDetailLayout.jsx
@@ -44,7 +44,7 @@ export default class ClaimDetailLayout extends React.Component {
         </div>
       );
     } else {
-      content = <LoadingIndicator/>;
+      content = <LoadingIndicator setFocus message="Loading claim information"/>;
     }
 
     return (
