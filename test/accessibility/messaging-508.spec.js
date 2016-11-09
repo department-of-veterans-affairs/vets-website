1394c19b7b94cc0e5e7cca8070b426b343db470f
Courtney Eimerman-Wallace <crwallace@users.noreply.github.com>
2016-11-09 16:35:42 -0500

    A11y tests (#3851)
    * auth 508 tests
    
    * adds 508 user profile test
    
    * adds secure messaging tests

---
+++ test/accessibility/messaging-508.spec.js
@@ -0,0 +1,34 @@
+const E2eHelpers = require('../util/e2e-helpers');
+const Timeouts = require('../util/timeouts.js');
+const LoginHelpers = require('../util/login-helpers');
+
+module.exports = E2eHelpers.createE2eTest(
+  (client) => {
+    LoginHelpers.logIn(client, '/healthcare/messaging', 3);
+
+    // Ensure active page renders.
+    client
+      .url(`${E2eHelpers.baseUrl}/healthcare/messaging`)
+      .waitForElementVisible('body', Timeouts.normal)
+      .axeCheck('.main');
+
+    // Ensure history page renders.
+    client
+      .url(`${E2eHelpers.baseUrl}/healthcare/messaging/folder/0`)
+      .waitForElementVisible('body', Timeouts.normal)
+      .axeCheck('.main');
+
+    // Ensure detail page renders.
+    client
+      .url(`${E2eHelpers.baseUrl}/healthcare/messaging/compose`)
+      .waitForElementVisible('body', Timeouts.normal)
+      .axeCheck('.main');
+
+    // Ensure glossary page renders.
+    client
+      .url(`${E2eHelpers.baseUrl}/healthcare/messaging/settings`)
+      .waitForElementVisible('body', Timeouts.normal)
+      .axeCheck('.main');
+  }
+);
+
