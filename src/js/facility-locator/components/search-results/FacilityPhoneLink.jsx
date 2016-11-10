7fa182f405c4a95efa16c4baafde1e9b92e7e7f6
Benjamin Shyong <ben.shyong@gmail.com>
2016-11-10 12:39:25 -0800

    Resolves #3915; missing vba data should not show up as blank

--- src/js/facility-locator/components/search-results/FacilityPhoneLink.jsx
+++ src/js/facility-locator/components/search-results/FacilityPhoneLink.jsx
@@ -1,11 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 
 class FacilityPhoneLink extends Component {
-
-
-  renderMentalHealthPhone() {
-    const { attributes: { phone } } = this.props.facility;
-    if (!phone.mental_health_clinic) {
+  renderPhoneNumber(title, phone, icon = 'fw') {
+    if (!phone) {
       return null;
     }
 
@@ -13,28 +10,22 @@ class FacilityPhoneLink extends Component {
 
     return (
       <div>
-        <a href={`tel:${phone.mental_health_clinic}`}>
-          <i className="fa fa-fw"/>
-          Mental Health:<br/>
-          <i className="fa fa-fw"/>{phone.mental_health_clinic.replace(re, '$1-$2-$3 $4$5')}
+        <a href={`tel:${phone}`}>
+          <i className={`fa fa-${icon}`}/>
+          {title}:<br/>
+          <i className="fa fa-fw"/>{phone.replace(re, '$1-$2-$3 $4$5').replace(/x$/, '')}
         </a>
       </div>
     );
   }
+
   render() {
     const { attributes: { phone } } = this.props.facility;
-    const re = /^(\d{3})[ -]?(\d{3})[ -]?(\d{4}) x (\d*)/;
 
     return (
       <div>
-        <div>
-          <a href={`tel:${phone.main}`}>
-            <i className="fa fa-phone"/>
-            Main:<br/>
-            <i className="fa fa-fw"/>{phone.main.replace(re, '$1-$2-$3 x$4').replace(/ x$/, '')}
-          </a>
-        </div>
-        {this.renderMentalHealthPhone()}
+        {this.renderPhoneNumber('Main', phone.main, 'phone')}
+        {this.renderPhoneNumber('Mental Health', phone.mental_health_clinic)}
       </div>
     );
   }
