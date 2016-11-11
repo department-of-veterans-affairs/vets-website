a5bfc889da1ceb10145d254101a4d226cd281dbf
Jeff Balboni <jeff@adhocteam.us>
2016-11-11 16:00:02 -0500

    Use isDataAvailable prop to show unavailable message

--- src/js/disability-benefits/containers/DisabilityBenefitsApp.jsx
+++ src/js/disability-benefits/containers/DisabilityBenefitsApp.jsx
@@ -5,27 +5,38 @@ import ClaimsUnavailable from '../components/ClaimsUnavailable';
 import ClaimSyncWarning from '../components/ClaimSyncWarning';
 import RequiredLoginView from '../../common/components/RequiredLoginView';
 
-class DisabilityBenefitsApp extends React.Component {
+// this needs to be a React component for RequiredLoginView to pass down props
+function AppContent({ available, synced, children, isDataAvailable }) {
+  // prop is only passed on failure
+  const canUseApp = isDataAvailable === true || typeof isDataAvailable === 'undefined';
+  return (
+    <div className="disability-benefits-content">
+      {available && canUseApp && !synced
+        ? <ClaimSyncWarning/>
+        : null}
+      {available && canUseApp
+        ? <div>
+          {children}
+        </div>
+        : <div className="row"><div className="columns medium-8"><ClaimsUnavailable/></div></div>}
+    </div>
+  );
+}
 
+class DisabilityBenefitsApp extends React.Component {
   render() {
     const { available, synced } = this.props;
 
     return (
       <RequiredLoginView authRequired={3} serviceRequired={"disability-benefits"}>
-        <div className="disability-benefits-content">
-          {available && !synced
-            ? <ClaimSyncWarning/>
-            : null}
-          {available
-            ? <div>
-              {this.props.children}
-            </div>
-            : <div className="row"><div className="columns medium-8"><ClaimsUnavailable/></div></div>}
-        </div>
+        <AppContent
+            available={available}
+            synced={synced}>
+          {this.props.children}
+        </AppContent>
       </RequiredLoginView>
     );
   }
-
 }
 
 function mapStateToProps(state) {
@@ -37,4 +48,4 @@ function mapStateToProps(state) {
 
 export default connect(mapStateToProps)(DisabilityBenefitsApp);
 
-export { DisabilityBenefitsApp };
+export { DisabilityBenefitsApp, AppContent };
--- test/disability-benefits/components/DisabilityBenefitsApp.unit.spec.jsx
+++ test/disability-benefits/components/DisabilityBenefitsApp.unit.spec.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import SkinDeep from 'skin-deep';
 import { expect } from 'chai';
 
-import { DisabilityBenefitsApp } from '../../../src/js/disability-benefits/containers/DisabilityBenefitsApp';
+import { DisabilityBenefitsApp, AppContent } from '../../../src/js/disability-benefits/containers/DisabilityBenefitsApp';
 
 describe('<DisabilityBenefitsApp>', () => {
-  it('should render children', () => {
+  it('should render children and login view', () => {
     const tree = SkinDeep.shallowRender(
       <DisabilityBenefitsApp
           available
@@ -13,27 +13,57 @@ describe('<DisabilityBenefitsApp>', () => {
         <div className="test-child"/>
       </DisabilityBenefitsApp>
     );
+
+    expect(tree.everySubTree('.test-child')).not.to.be.empty;
+    expect(tree.everySubTree('RequiredLoginView')).not.to.be.empty;
+    expect(tree.subTree('RequiredLoginView').props.serviceRequired).to.equal('disability-benefits');
+    expect(tree.subTree('RequiredLoginView').props.authRequired).to.equal(3);
+  });
+  it('should render children', () => {
+    const tree = SkinDeep.shallowRender(
+      <AppContent
+          available
+          synced>
+        <div className="test-child"/>
+      </AppContent>
+    );
+
     expect(tree.everySubTree('.test-child')).not.to.be.empty;
     expect(tree.everySubTree('ClaimSyncWarning')).to.be.empty;
     expect(tree.everySubTree('ClaimsUnavailable')).to.be.empty;
   });
+  it('should render unavailable when app is not authorized', () => {
+    const tree = SkinDeep.shallowRender(
+      <AppContent
+          available
+          synced
+          isDataAvailable={false}>
+        <div className="test-child"/>
+      </AppContent>
+    );
+
+    expect(tree.everySubTree('ClaimSyncWarning')).to.be.empty;
+    expect(tree.everySubTree('ClaimsUnavailable')).not.to.be.empty;
+  });
   it('should render sync warning', () => {
     const tree = SkinDeep.shallowRender(
-      <DisabilityBenefitsApp
+      <AppContent
           available>
         <div className="test-child"/>
-      </DisabilityBenefitsApp>
+      </AppContent>
     );
+
     expect(tree.everySubTree('.test-child')).not.to.be.empty;
     expect(tree.everySubTree('ClaimSyncWarning')).not.to.be.empty;
     expect(tree.everySubTree('ClaimsUnavailable')).to.be.empty;
   });
   it('should render unavailable', () => {
     const tree = SkinDeep.shallowRender(
-      <DisabilityBenefitsApp>
+      <AppContent>
         <div className="test-child"/>
-      </DisabilityBenefitsApp>
+      </AppContent>
     );
+
     expect(tree.everySubTree('.test-child')).to.be.empty;
     expect(tree.everySubTree('ClaimSyncWarning')).to.be.empty;
     expect(tree.everySubTree('ClaimsUnavailable')).not.to.be.empty;
