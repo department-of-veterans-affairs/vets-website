7894119feb50847e132553c48ed7cbd9bea81169
akainic <annekainic@gmail.com>
2016-11-15 13:23:16 -0500

    Fix file paths again

--- src/js/hca/components/Address.jsx
+++ src/js/hca/components/Address.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import _ from 'lodash';
 
-import ErrorableSelect from '../../../common/components/form-elements/ErrorableSelect';
-import ErrorableTextInput from '../../../common/components/form-elements/ErrorableTextInput';
-import { isBlank, isNotBlank, validateIfDirty } from '../../utils/validations';
-import { countries, states } from '../../../common/utils/options-for-select';
+import ErrorableSelect from '../../common/components/form-elements/ErrorableSelect';
+import ErrorableTextInput from '../../common/components/form-elements/ErrorableTextInput';
+import { isBlank, isNotBlank, validateIfDirty } from '../utils/validations';
+import { countries, states } from '../../common/utils/options-for-select';
 
 /**
  * Input component for an address.
