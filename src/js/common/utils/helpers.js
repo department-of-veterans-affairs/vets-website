9c818528eacba4a6c8c226747f6be0f753fff2ce
Jeff Balboni <jeff@adhocteam.us>
2016-11-16 16:39:33 -0500

    Use common subway map component

--- src/js/common/utils/helpers.js
+++ src/js/common/utils/helpers.js
@@ -1,5 +1,44 @@
 import _ from 'lodash';
 
+export function getPageList(routes) {
+  return routes.map(route => {
+    const obj = {
+      name: route.props.path,
+      label: route.props.name
+    };
+    if (route.props.depends) {
+      obj.depends = route.props.depends;
+    }
+    return obj;
+  }).filter(page => page.name !== '/submit-message');
+}
+
+export function groupPagesIntoChapters(routes) {
+  const pageList = routes
+    .filter(route => route.props.chapter)
+    .map(page => {
+      const obj = {
+        name: page.props.name,
+        chapter: page.props.chapter,
+        path: page.props.path
+      };
+
+      if (page.props.depends) {
+        obj.depends = page.props.depends;
+      }
+
+      return obj;
+    });
+
+  const pageGroups = _.groupBy(pageList, page => page.chapter);
+
+  return Object.keys(pageGroups).map(chapter => {
+    return {
+      name: chapter,
+      pages: pageGroups[chapter]
+    };
+  });
+}
 export function isActivePage(page, data) {
   if (typeof page.depends === 'function') {
     return page.depends(data);
