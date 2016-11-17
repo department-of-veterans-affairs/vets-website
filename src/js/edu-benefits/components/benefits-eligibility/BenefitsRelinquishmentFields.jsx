22f18a590c370f0a51bd6a254de816dfe1ab6e30
Alexander Ose <alex@alexose.com>
2016-11-17 16:47:12 -0500

    re-add expandablegroup

--- src/js/edu-benefits/components/benefits-eligibility/BenefitsRelinquishmentFields.jsx
+++ src/js/edu-benefits/components/benefits-eligibility/BenefitsRelinquishmentFields.jsx
@@ -56,15 +56,21 @@ export default class BenefitsRelinquishmentFields extends React.Component {
       <div className="input-section">
         <p>If you are eligible for both the Post 9/11 GI Bill and another program, such as the Montgomery GI Bill – Active Duty (MGIB-AD), you have to choose which one you want to use. Once you <strong>apply</strong> for one or the other, <strong>your decision is irrevocable</strong> (meaning you can’t change your mind).</p>
         <fieldset className="edu-benefits-info-no-icon">
-          <div>
-            <ErrorableRadioButtons required={this.props.data.chapter33}
-                errorMessage={validateIfDirty(this.props.data.benefitsRelinquished, isNotBlank) ? '' : 'Please select a response'}
-                label="I choose to give up:"
-                name="benefitsRelinquished"
-                options={options}
-                value={this.props.data.benefitsRelinquished}
-                onValueChange={(update) => {this.props.onStateChange('benefitsRelinquished', update);}}/>
-          </div>
+          <ExpandingGroup
+              additionalClass="edu-benefits-active-group"
+              open={showRelinquishedEffectiveDate(this.props.data.benefitsRelinquished.value)}>
+            <div>
+              <ErrorableRadioButtons
+                  required={this.props.data.chapter33}
+                  errorMessage={validateIfDirty(this.props.data.benefitsRelinquished, isNotBlank) ? '' : 'Please select a response'}
+                  label="I choose to give up:"
+                  name="benefitsRelinquished"
+                  options={options}
+                  value={this.props.data.benefitsRelinquished}
+                  onValueChange={(update) => { this.props.onStateChange('benefitsRelinquished', update); }}/>
+            </div>
+            <div></div>
+          </ExpandingGroup>
         </fieldset>
         <p>If you have questions or don’t understand the choice, talk to a specialist at 1-888-442-4551 (1-888-GI-BILL-1) from 8:00 a.m. - 7:00 p.m. ET Mon - Fri.</p>
       </div>
