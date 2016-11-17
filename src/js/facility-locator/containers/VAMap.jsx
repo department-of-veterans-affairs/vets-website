c89c2487645b797bc1efcb22cfdc3384d803c32c
Benjamin Shyong <ben.shyong@gmail.com>
2016-11-17 11:14:31 -0800

    remove you are here pin

--- src/js/facility-locator/containers/VAMap.jsx
+++ src/js/facility-locator/containers/VAMap.jsx
@@ -306,10 +306,6 @@ class VAMap extends Component {
                 <TileLayer
                     url={`https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=${mapboxToken}`}
                     attribution='Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'/>
-                <DivMarker position={position} popupContent={<span>You are here</span>}>
-                  <div className="current-position-icon map-marker">
-                  </div>
-                </DivMarker>
                 <FeatureGroup ref="facilityMarkers">
                   {this.renderFacilityMarkers()}
                 </FeatureGroup>
@@ -347,10 +343,6 @@ class VAMap extends Component {
               <TileLayer
                   url={`https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=${mapboxToken}`}
                   attribution='Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'/>
-              <DivMarker position={position} popupContent={<span>You are here</span>}>
-                <div className="current-position-icon map-marker">
-                </div>
-              </DivMarker>
               <FeatureGroup ref="facilityMarkers">
                 {this.renderFacilityMarkers()}
               </FeatureGroup>
