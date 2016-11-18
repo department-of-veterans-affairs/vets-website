7cadd32366dff4391bccbaa25f24d80a9c386773
Eugene Doan <eugene@adhocteam.us>
2016-11-18 12:52:05 -0500

    Updated props in Folder unit test.

--- test/messaging/containers/Folder.unit.spec.jsx
+++ test/messaging/containers/Folder.unit.spec.jsx
@@ -6,27 +6,31 @@ import { expect } from 'chai';
 import { Folder } from '../../../src/js/messaging/containers/Folder';
 
 const props = {
-  folder: {
-    attributes: {
-      folderId: 0,
-      name: 'Inbox'
-    },
-    messages: [],
-    pagination: {
-      currentPage: 0,
-      perPage: 0,
-      totalEntries: 0,
-      totalPages: 0
-    },
-    persistFolder: 0
+  attributes: {
+    folderId: 0,
+    name: 'Inbox'
   },
   currentRange: '1 - 5',
+  folders: new Map([
+    ['inbox', {}],
+    ['drafts', {}],
+    ['sent', {}],
+    ['deleted', {}],
+    ['personal-folder', {}],
+  ]),
+  filter: undefined,
+  loading: false,
+  messages: [
+    { body: 'test1' },
+    { body: 'test2' },
+    { body: 'test3' },
+    { body: 'test4' },
+    { body: 'test5' }
+  ],
   messageCount: 5,
   page: 1,
+  params: { folderName: 'inbox' },
   totalPages: 1,
-  isAdvancedVisible: false,
-  searchDateRangeStart: moment(),
-  searchDateRangeEnd: moment(),
 
   // No-op function to override dispatch
   dispatch: () => {}
