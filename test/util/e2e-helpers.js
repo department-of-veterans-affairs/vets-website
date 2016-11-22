4d445ab42ea62aeb625345ec9df1deae366b05e5
Kenneth Koski <ken@adhocteam.us>
2016-11-22 10:36:40 -0600

    Adding Jenkinsfile

--- test/util/e2e-helpers.js
+++ test/util/e2e-helpers.js
@@ -26,7 +26,7 @@ function expectInputToNotBeSelected(client, field) {
 
 function overrideVetsGovApi(client) {
   client.execute(() => {
-    window.VetsGov.api.url = 'http://localhost:4000';
+    window.VetsGov.api.url = `http://localhost:${process.env.API_PORT || 4000}`;
     return window.VetsGov.api.url;
   },
   [],
@@ -37,8 +37,8 @@ function overrideVetsGovApi(client) {
 }
 
 module.exports = {
-  baseUrl: 'http://localhost:3333',
-  apiUrl: 'http://localhost:4000',
+  baseUrl: `http://localhost:${process.env.WEB_PORT || 3333}`,
+  apiUrl: `http://localhost:${process.env.API_PORT || 4000}`,
   createE2eTest,
   expectNavigateAwayFrom,
   expectValueToBeBlank,
