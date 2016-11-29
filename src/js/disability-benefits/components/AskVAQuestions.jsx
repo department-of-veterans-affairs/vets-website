a7960205720f1230f3d891d8383d1953e8f7d172
Suzanne Chapman <suzanne.chapman@va.gov>
2016-11-29 15:45:54 -0500

    changes to ask va questions component

--- src/js/disability-benefits/components/AskVAQuestions.jsx
+++ src/js/disability-benefits/components/AskVAQuestions.jsx
@@ -4,13 +4,13 @@ class AskVAQuestions extends React.Component {
   render() {
     return (
       <div className="ask-va-questions claims-sidebar-box">
-        <h4 className="claims-sidebar-header">Questions?</h4>
-        <p className="talk">Ask the vets.gov Help Desk</p>
+        <h4 className="claims-sidebar-header">Need Help?</h4>
+        <p className="talk">Ask the vets.gov Help Desk:</p>
         <p className="phone-number">
-          <a href="tel:+1-855-574-7286">1-855-574-7286</a><br/>
-          Monday - Friday, 8:00 am - 8:00 pm ET
+          <a href="tel:+1-855-574-7286">855-574-7286</a><br/>
+          Monday - Friday, 8:00 a.m. - 8:00 p.m. ET
         </p>
-        <p><a href="https://iris.custhelp.com/">Submit a question to the VA</a></p>
+        <p><a href="https://iris.custhelp.com/">Submit a question to VA</a></p>
       </div>
     );
   }
