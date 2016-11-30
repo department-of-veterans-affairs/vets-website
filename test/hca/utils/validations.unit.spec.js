1d7d6a124287d2f54378e1ff51e4d211ed737287
Anne Kainic <annekainic@gmail.com>
2016-11-30 17:21:24 -0500

    Hca require min 2 characters for name (#4157)
    * Add custom validation for FullName
    
    * Fix issue for education
    
    * Improve function
    
    * Fix broken test
    
    * Add test
    
    * Rename function

--- test/hca/utils/validations.unit.spec.js
+++ test/hca/utils/validations.unit.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 
 import _ from 'lodash';
 
-import { isValidSpouseInformation } from '../../../src/js/hca/utils/validations.js';
+import { isValidSpouseInformation, isValidLastName } from '../../../src/js/hca/utils/validations.js';
 
 describe('Validations unit tests', () => {
   describe('isValidSpouseInformation', () => {
@@ -44,6 +44,29 @@ describe('Validations unit tests', () => {
 
       it('should return false', () => {
         expect(isValidSpouseInformation(spouseTestDataInvalidAddress)).to.be.false;
+      });
+    });
+  });
+});
+
+describe('Validations unit tests', () => {
+  describe('isValidLastName', () => {
+    [
+      {
+        lastName: 'fo',
+        allowed: true
+      },
+      {
+        lastName: 'foo',
+        allowed: true
+      },
+      {
+        lastName: 'f',
+        allowed: false
+      },
+    ].forEach((lastNameTest) => {
+      it(`should${lastNameTest.allowed ? '' : "n't"} allow a last name of ${lastNameTest.lastName}`, () => {
+        expect(isValidLastName(lastNameTest.lastName)).to.eql(lastNameTest.allowed);
       });
     });
   });
