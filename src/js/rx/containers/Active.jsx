611ef173e9e1995eefe2413c98658592e8333c42
Alex Yale-Loehr <alex.yale-loehr@va.gov>
2016-12-02 11:35:23 -0500

    Fix Rx error (#4207)
    * Add call center number to Rx error
    
    * went with 2/3 width. found one more
    
    * fix tab
    
    * remove trailing spaces

--- src/js/rx/containers/Active.jsx
+++ src/js/rx/containers/Active.jsx
@@ -73,6 +73,8 @@ class Active extends React.Component {
         <p className="rx-tab-explainer rx-loading-error">
           We couldn't retrieve your prescriptions.
           Please refresh this page or try again later.
+          If this problem persists, please call the Vets.gov Help Desk
+          at 1-855-574-7286, Monday ‒ Friday, 8:00 a.m. ‒ 8:00 p.m. (ET).
         </p>
       );
     }
