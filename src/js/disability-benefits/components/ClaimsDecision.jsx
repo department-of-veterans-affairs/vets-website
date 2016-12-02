c5bedec1b65e3023fc836ecedd9b1e752aca886a
Suzanne Chapman <suzanne.chapman@va.gov>
2016-12-02 10:33:34 -0500

    minor content changes per SO co recs

--- src/js/disability-benefits/components/ClaimsDecision.jsx
+++ src/js/disability-benefits/components/ClaimsDecision.jsx
@@ -10,7 +10,7 @@ class ClaimsDecision extends React.Component {
         <p>{completedDate ? `Your claim was completed on ${moment(completedDate).format('MMM D, YYYY')}. ` : null}
         We sent you a packet by U.S. mail that includes details of the decision or award. Please allow 7 business days for your packet to arrive before contacting a VA call center.</p>
         <p>Do you disagree with your claim decision? <a href="/disability-benefits/claims-appeal">File an appeal</a></p>
-        <p>If you have new evidence to support your claim and have not yet appealed, you can ask VA to <a href="/disability-benefits/claims-process/claim-types/reopened-claim">Reopen your claim</a></p>
+        <p>If you have new evidence to support your claim and have not yet appealed, you can ask us to <a href="/disability-benefits/claims-process/claim-types/reopened-claim">Reopen your claim</a></p>
       </div>
     );
   }
