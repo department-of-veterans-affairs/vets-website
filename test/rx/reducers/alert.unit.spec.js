ee462795a177ff1a6ff4ddfc48d5f0da70e6032c
Eugene Doan <eugene@adhocteam.us>
2016-12-06 02:09:59 -0500

    Updated initial states for alert reducer tests.

--- test/rx/reducers/alert.unit.spec.js
+++ test/rx/reducers/alert.unit.spec.js
@@ -4,7 +4,7 @@ import alertReducer from '../../../src/js/rx/reducers/alert.js';
 
 describe('alert reducer', () => {
   it('should open the alert', () => {
-    const state = alertReducer(undefined, {
+    const state = alertReducer({ visible: false, }, {
       type: 'OPEN_ALERT',
       content: 'testing',
       status: 'success'
@@ -16,7 +16,7 @@ describe('alert reducer', () => {
   });
 
   it('should close the alert', () => {
-    const state = alertReducer(undefined, {
+    const state = alertReducer({ visible: true }, {
       type: 'CLOSE_ALERT'
     });
 
@@ -24,22 +24,32 @@ describe('alert reducer', () => {
   });
 
   it('should alert an error for a failed refill', () => {
-    const state = alertReducer(undefined, {
+    const state = alertReducer({
+      visible: false,
+      content: '',
+      status: 'info'
+    }, {
       type: 'REFILL_FAILURE',
       prescription: { prescriptionId: 1 }
     });
 
     expect(state.visible).to.be.true;
+    expect(state.content).to.be.not.empty;
     expect(state.status).to.eql('error');
   });
 
   it('should alert a success for a successful refill', () => {
-    const state = alertReducer(undefined, {
+    const state = alertReducer({
+      visible: false,
+      content: '',
+      status: 'info'
+    }, {
       type: 'REFILL_SUCCESS',
       prescription: { prescriptionId: 1 }
     });
 
     expect(state.visible).to.be.true;
+    expect(state.content).to.be.not.empty;
     expect(state.status).to.eql('success');
   });
 });
