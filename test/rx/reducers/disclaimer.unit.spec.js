f7a13a5e3973662f55241355b754a219c310675f
Eugene Doan <eugene@adhocteam.us>
2016-12-06 02:21:17 -0500

    Updated initial state for disclaimer reducer test.

--- test/rx/reducers/disclaimer.unit.spec.js
+++ test/rx/reducers/disclaimer.unit.spec.js
@@ -4,9 +4,10 @@ import disclaimerReducer from '../../../src/js/rx/reducers/disclaimer.js';
 
 describe('disclaimer reducer', () => {
   it('should close the disclaimer', () => {
-    const state = disclaimerReducer(undefined, {
-      type: 'CLOSE_DISCLAIMER'
-    });
+    const state = disclaimerReducer(
+      { open: true },
+      { type: 'CLOSE_DISCLAIMER' }
+    );
 
     expect(state.open).to.be.false;
   });
