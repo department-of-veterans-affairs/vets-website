070eb06265d6c3f669a04d577792396c18e76f40
Jeff Balboni <jeff@adhocteam.us>
2016-12-08 10:55:52 -0500

    WIP

--- src/js/edu-benefits/edu-benefits-entry.jsx
+++ src/js/edu-benefits/edu-benefits-entry.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createHistory } from 'history';
-import { IndexRedirect, Route, Router, useRouterHistory } from 'react-router';
+import { Router, useRouterHistory, Link } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-import EduBenefitsApp from './containers/EduBenefitsApp.jsx';
 import initReact from '../common/init-react';
-import reducer from './reducers';
-import routes from './routes.jsx';
+import reducer from './1990/reducers';
 
 require('../../sass/edu-benefits.scss');
 
@@ -26,15 +24,40 @@ const browserHistory = useRouterHistory(createHistory)({
   basename: '/education/apply-for-education-benefits/application'
 });
 
+function QuestionsIntro() {
+  return (
+    <div>
+      Hey, which form do you want?
+      <Link to="1990">1990</Link>
+    </div>
+  );
+}
+
+
+const routes = {
+  path: '/',
+  indexRoute: { component: QuestionsIntro },
+  childRoutes: [
+    {
+      path: '1990',
+      getComponent: (nextState, cb) => {
+        require.ensure([], (require) => {
+          cb(null, require('./1990/containers/EduBenefitsApp'));
+        }, 'edu-1990');
+      },
+      getChildRoutes: (nextState, cb) => {
+        require.ensure([], (require) => {
+          cb(null, require('./1990/routes.jsx'));
+        }, 'edu-1990');
+      }
+    }
+  ]
+};
+
 function init() {
   ReactDOM.render((
     <Provider store={store}>
-      <Router history={browserHistory}>
-        <Route path="/" component={EduBenefitsApp}>
-          <IndexRedirect to="/introduction"/>
-          {routes}
-        </Route>
-      </Router>
+      <Router history={browserHistory} routes={routes}/>
     </Provider>
     ), document.getElementById('react-root'));
 }
