7ecdae31ca8bdb293d25d51b64f51ae4a162d724
Ben Damman <benjamin_c_damman@omb.eop.gov>
2016-12-13 12:44:23 -0500

    profile estimator

--- src/js/gi/components/profile/ProfileEstimator.jsx
+++ src/js/gi/components/profile/ProfileEstimator.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import Estimator from '../../utils/Estimator';
 
 class ProfileEstimator extends React.Component {
 
   constructor(props) {
     super(props);
-    // this.renderHeader = this.renderHeader.bind(this);
+    this.estimate = this.estimate.bind(this);
+  }
+
+  // WIP
+  estimate() {
+    const school = this.props.institution;
+    console.log(school)
+    // get current values about the user
+    const e = this.props.estimator;
+    e.setMilitaryStatus = 'active duty';
+    e.setSpouseActiveDuty = 'no';
+    e.setGiBillChap = '31';
+    e.setNumberOfDepend = '0';
+    e.setPost911Elig = 'yes';
+    e.setCumulativeService = '1.0';
+    e.setEnlistmentService = '3123412314';
+    e.setConsecutiveService = '3123412314';
+    e.setOnline = 'yes';
+    // set institution values
+    e.setInstitutionType = school.institution_type.name;
+    e.setCountry = school.country.toLowerCase();
+    e.setBah = school.bah;
   }
 
   render() {
+    this.estimate();
+
     return (
       <div id="benefits" className="large-12 columns accordion-vert-spacing">
         <ul className="accordion" data-accordion>
@@ -20,7 +44,9 @@ class ProfileEstimator extends React.Component {
                 <div className="icon benefits-estimator-icon-box">
                   <i className="fa fa-graduation-cap fa-profile-benefits"></i>
                 </div>
-                <p id="est-tuition-fees" className="profile-benefits-values"></p>
+                <p id="est-tuition-fees" className="profile-benefits-values">
+                  {this.props.estimator.renderTuitionFees()}
+                </p>
               </div>
             </div>
 
@@ -30,7 +56,9 @@ class ProfileEstimator extends React.Component {
                 <div className="icon benefits-estimator-icon-box">
                   <i className="fa fa-home fa-profile-benefits"></i>
                 </div>
-                <p id="est-housing-allowance" className="profile-benefits-values"></p>
+                <p id="est-housing-allowance" className="profile-benefits-values">
+                  {this.props.estimator.renderHousingAllowance()}
+                </p>
               </div>
             </div>
 
@@ -40,7 +68,9 @@ class ProfileEstimator extends React.Component {
                 <div className="icon benefits-estimator-icon-box">
                   <i className="fa fa-book fa-profile-benefits"></i>
                 </div>
-                <p id="est-book-stipend" className="profile-benefits-values"></p>
+                <p id="est-book-stipend" className="profile-benefits-values">
+                  {this.props.estimator.renderBookStipend()}
+                </p>
               </div>
             </div>
           </li>
@@ -52,12 +82,11 @@ class ProfileEstimator extends React.Component {
 }
 
 ProfileEstimator.propTypes = {
-  institution: React.PropTypes.object.isRequired,
-  expanded: React.PropTypes.bool.isRequired
+  institution: React.PropTypes.object.isRequired
 };
 
 ProfileEstimator.defaultProps = {
-  expanded: true
+  estimator: new Estimator()
 };
 
 export default ProfileEstimator;
