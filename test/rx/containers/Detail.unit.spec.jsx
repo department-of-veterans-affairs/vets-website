0b46c2ffbe5d03e96e8fedcf489cc550b3e30c9c
Eugene Doan <eugene@adhocteam.us>
2016-12-13 20:09:29 -0500

    Added test for hidden order history table.

--- test/rx/containers/Detail.unit.spec.jsx
+++ test/rx/containers/Detail.unit.spec.jsx
@@ -43,15 +43,26 @@ describe('<Detail>', () => {
 
   it('should render details if there is a prescription', () => {
     const tree = SkinDeep.shallowRender(<Detail {...props}/>);
-    expect(tree.dive(['h2']).text())
-      .to.equal(item.attributes.prescriptionName);
+    expect(tree.dive(['h2']).text()).to.equal(item.attributes.prescriptionName);
     expect(tree.dive(['#rx-info'])).to.not.be.undefined;
   });
 
-  it('should render order history if there is order history', () => {
+  it('should render order history table if there is tracking data', () => {
     const tree = SkinDeep.shallowRender(<Detail {...props}/>);
     const orderHistory = tree.dive(['OrderHistory']);
     expect(orderHistory).to.not.be.undefined;
+  });
+
+  it('should not render order history table without tracking data', () => {
+    const prescription = { rx: item, trackings: [] };
+    const tree = SkinDeep.shallowRender(
+      <Detail {...props} prescription={prescription}/>
+    );
+
+    // Should still render the order history section with a disclaimer
+    // about expiration of tracking data.
+    const orderHistory = tree.dive(['#rx-order-history']);
+    expect(orderHistory.subTree('OrderHistory')).to.be.false;
   });
 
   it('should render a contact card if there is an prescription', () => {
@@ -60,5 +71,7 @@ describe('<Detail>', () => {
     expect(contactCard).to.not.be.false;
     expect(contactCard.props.facilityName)
       .to.equal(item.attributes.facilityName);
+    expect(contactCard.props.phoneNumber)
+      .to.equal(props.prescription.trackings[0].attributes.rxInfoPhoneNumber);
   });
 });
