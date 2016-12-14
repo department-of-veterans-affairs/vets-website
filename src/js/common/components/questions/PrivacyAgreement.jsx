e8058503b9815d240f061f4ec2767f40312898c3
akainic <Anne.Kainic@va.gov>
2016-12-14 15:33:46 -0500

    Fix failing education e2e tests

--- src/js/common/components/questions/PrivacyAgreement.jsx
+++ src/js/common/components/questions/PrivacyAgreement.jsx
@@ -7,6 +7,7 @@ export default function PrivacyAgreement({ onChange, checked, showError }) {
       <ErrorableCheckbox required
           checked={checked}
           onValueChange={onChange}
+          name="privacyAgreement"
           errorMessage={showError && !checked ? 'You must accept the privacy policy before continuing' : undefined}
           label={<span>I have read and accept the <a target="_blank" href="/privacy">privacy policy</a></span>}/>
     </div>
