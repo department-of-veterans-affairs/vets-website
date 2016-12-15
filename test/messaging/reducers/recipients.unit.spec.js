783a1101d494dce31512d1ab77bbb130a092c8e7
Eugene Doan <eugene@adhocteam.us>
2016-12-15 18:11:55 -0500

    Updated container unit tests with loading props.

--- test/messaging/reducers/recipients.unit.spec.js
+++ test/messaging/reducers/recipients.unit.spec.js
@@ -11,20 +11,11 @@ import {
 import { testData } from '../../util/messaging-helpers';
 
 describe('recipients reducer', () => {
-  it('should indicate when it\'s loading recipients', () => {
-    const state = recipientsReducer(undefined, {
-      type: LOADING_RECIPIENTS
-    });
-
-    expect(state.loading).to.be.true;
-  });
-
   it('should have no data when it fails to load recipients', () => {
     const state = recipientsReducer(undefined, {
       type: FETCH_RECIPIENTS_FAILURE
     });
 
-    expect(state.loading).to.be.false;
     expect(state.data).to.be.null;
   });
 
@@ -34,8 +25,6 @@ describe('recipients reducer', () => {
       type: FETCH_RECIPIENTS_SUCCESS,
       recipients
     });
-
-    expect(state.loading).to.be.false;
 
     recipients.data.forEach((recipient) => {
       expect(state.data).to.contain({
