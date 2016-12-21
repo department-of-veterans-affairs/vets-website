14f0af95f07a2d92ee59d6d1f16d696f6f8e3798
Eugene Doan <eugene@adhocteam.us>
2016-12-21 13:54:52 -0500

    Make authorized requests for attachments and open in new window.

--- src/js/messaging/components/MessageAttachmentsViewItem.jsx
+++ src/js/messaging/components/MessageAttachmentsViewItem.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import { authFetch } from '../utils/helpers';
+
 class MessageAttachmentsViewItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.downloadAttachment = this.downloadAttachment.bind(this);
+  }
+
+  downloadAttachment(event) {
+    event.preventDefault();
+    authFetch(this.props.url)
+    .then(response => response.blob())
+    .then(blob => {
+      window.open(URL.createObjectURL(blob), '_blank');
+    });
+  }
+
   render() {
     return (
       <li>
-        <a href={this.props.url} className="msg-attachment-item" download>
+        <a onClick={this.downloadAttachment} href={this.props.url} className="msg-attachment-item" download>
           <i className="fa fa-paperclip msg-attachment-icon"></i>
           {this.props.name}
         </a>
