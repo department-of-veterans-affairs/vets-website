01b989f317fdb40485e08606df00b486a4355b78
Eugene Doan <eugene@adhocteam.us>
2016-12-21 14:44:01 -0500

    Cache blob URL for attachments so requests for them don't have to be repeated. Show loader while file is being downloaded.

--- src/js/messaging/components/MessageAttachmentsViewItem.jsx
+++ src/js/messaging/components/MessageAttachmentsViewItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import { authFetch } from '../utils/helpers';
 
@@ -6,22 +7,46 @@ class MessageAttachmentsViewItem extends React.Component {
   constructor(props) {
     super(props);
     this.downloadAttachment = this.downloadAttachment.bind(this);
+    this.downloadUrl = null;
+    this.state = { downloading: false };
   }
 
   downloadAttachment(event) {
     event.preventDefault();
-    authFetch(this.props.url)
+
+    if (this.state.downloading) return;
+
+    if (this.downloadUrl) {
+      window.open(this.downloadUrl, '_blank');
+      return;
+    }
+
+    this.setState({ downloading: true });
+    const requestUrl = this.props.url;
+    authFetch(requestUrl)
     .then(response => response.blob())
     .then(blob => {
-      window.open(URL.createObjectURL(blob), '_blank');
+      const downloadUrl = URL.createObjectURL(blob);
+      this.downloadUrl = downloadUrl;
+      this.setState({ downloading: false });
+      window.open(this.downloadUrl, '_blank');
     });
   }
 
   render() {
+    const iconClass = classNames({
+      fa: true,
+      'fa-paperclip': !this.state.downloading,
+      'fa-spinner': this.state.downloading,
+      'fa-pulse': this.state.downloading,
+      'msg-attachment-icon': true
+    });
+
     return (
       <li>
-        <a onClick={this.downloadAttachment} href={this.props.url} className="msg-attachment-item" download>
-          <i className="fa fa-paperclip msg-attachment-icon"></i>
+        <a onClick={this.downloadAttachment} href={this.props.url} className="msg-attachment-item">
+          <i className={iconClass}></i>
+          <span className="usa-sr-only">Loading...</span>
           {this.props.name}
         </a>
       </li>
