46b34a5d823f077cb5cec9ede1d81e3e1808b57d
James Kassemi <jkassemi@gmail.com>
2016-12-22 12:34:56 -0700

    Tests should no longer be gated

--- test/disability-benefits/e2e/01.claim-status-decision.e2e.spec.js
+++ test/disability-benefits/e2e/01.claim-status-decision.e2e.spec.js
@@ -1,31 +1,29 @@
-if (!process.env.BUILDTYPE || process.env.BUILDTYPE === 'development') {
-  const E2eHelpers = require('../../util/e2e-helpers');
-  const Timeouts = require('../../util/timeouts.js');
-  const DisabilityHelpers = require('../../util/disability-helpers');
-  const LoginHelpers = require('../../util/login-helpers');
+const E2eHelpers = require('../../util/e2e-helpers');
+const Timeouts = require('../../util/timeouts.js');
+const DisabilityHelpers = require('../../util/disability-helpers');
+const LoginHelpers = require('../../util/login-helpers');
 
-  module.exports = E2eHelpers.createE2eTest(
-    (client) => {
-      const token = LoginHelpers.getUserToken();
+module.exports = E2eHelpers.createE2eTest(
+  (client) => {
+    const token = LoginHelpers.getUserToken();
 
-      DisabilityHelpers.initClaimsListMock(token);
-      DisabilityHelpers.initClaimDetailMocks(token, true, true, false, null);
+    DisabilityHelpers.initClaimsListMock(token);
+    DisabilityHelpers.initClaimDetailMocks(token, true, true, false, null);
 
-      LoginHelpers.logIn(token, client, '/disability-benefits/track-claims', 3)
-        .waitForElementVisible('a.claim-list-item', Timeouts.slow);
+    LoginHelpers.logIn(token, client, '/disability-benefits/track-claims', 3)
+      .waitForElementVisible('a.claim-list-item', Timeouts.slow);
 
-      client
-        .click('a.claim-list-item:first-child')
-        .waitForElementVisible('body', Timeouts.normal)
-        .waitForElementVisible('.claim-title', Timeouts.slow);
+    client
+      .click('a.claim-list-item:first-child')
+      .waitForElementVisible('body', Timeouts.normal)
+      .waitForElementVisible('.claim-title', Timeouts.slow);
 
-      client
-        .expect.element('.claim-decision-is-ready').to.be.visible;
+    client
+      .expect.element('.claim-decision-is-ready').to.be.visible;
 
-      client
-        .expect.element('.disability-benefits-timeline').not.to.be.present;
+    client
+      .expect.element('.disability-benefits-timeline').not.to.be.present;
 
-      client.end();
-    }
-  );
-}
+    client.end();
+  }
+);
--- test/rx/00-required.e2e.spec.js
+++ test/rx/00-required.e2e.spec.js
@@ -3,20 +3,18 @@ const Timeouts = require('../util/timeouts.js');
 const RxHelpers = require('../util/rx-helpers');
 const LoginHelpers = require('../util/login-helpers');
 
-if (!process.env.BUILDTYPE || process.env.BUILDTYPE === 'development') {
-  module.exports = E2eHelpers.createE2eTest(
-    (client) => {
-      const token = LoginHelpers.getUserToken();
+module.exports = E2eHelpers.createE2eTest(
+  (client) => {
+    const token = LoginHelpers.getUserToken();
 
-      RxHelpers.initApplicationSubmitMock(token);
+    RxHelpers.initApplicationSubmitMock(token);
 
-      // Ensure introduction page renders.
-      LoginHelpers.logIn(token, client, '/healthcare/prescriptions', 3)
-        .assert.title('Refill your prescriptions: Vets.gov')
-        .waitForElementVisible('#rx-active', Timeouts.slow)
-        .click('.va-tabs li:last-child a')
-        .waitForElementVisible('#rx-history', Timeouts.normal)
-        .waitForElementVisible('.rx-table', Timeouts.slow);
-    }
-  );
-}
+    // Ensure introduction page renders.
+    LoginHelpers.logIn(token, client, '/healthcare/prescriptions', 3)
+      .assert.title('Refill your prescriptions: Vets.gov')
+      .waitForElementVisible('#rx-active', Timeouts.slow)
+      .click('.va-tabs li:last-child a')
+      .waitForElementVisible('#rx-history', Timeouts.normal)
+      .waitForElementVisible('.rx-table', Timeouts.slow);
+  }
+);
