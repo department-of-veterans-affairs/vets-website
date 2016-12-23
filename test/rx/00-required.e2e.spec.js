1e74d4667b409b626533a1782aa0c84c029086d9
Benjamin Shyong <ben.shyong@gmail.com>
2016-12-23 09:33:28 -0800

    Glossary modal tests

--- test/rx/00-required.e2e.spec.js
+++ test/rx/00-required.e2e.spec.js
@@ -18,19 +18,30 @@ if (!process.env.BUILDTYPE || process.env.BUILDTYPE === 'development') {
       // ensure that prescription card renders
       client.expect.element('.rx-prescription').to.be.visible;
 
+      // ensure glossary modal triggers correctly
+      client
+        .click('button.rx-trigger')
+        .expect.element('.rx-modal-body').to.be.visible;
+      // ensure glossary modal can be dismissed
+      client
+        .click('.va-modal-button-group button')
+        .expect.element('.rx-modal-body').to.not.be.found;
+
+      // ensure prescription detail page is accessible
       client
         .click('.rx-prescription-info .rx-prescription-title a')
         .waitForElementVisible('#rx-detail', Timeouts.slow)
         .waitForElementVisible('#rx-detail h2', Timeouts.slow)
         .assert.containsText('#rx-detail h2', 'ACETAMINOPHEN 325MG TAB');
 
+      // ensure history view is accessible
       client
         .click('.rx-breadcrumbs a[href="/healthcare/prescriptions/"]')
         .waitForElementVisible('#rx-active', Timeouts.slow)
         .click('.va-tabs li:last-child a')
         .waitForElementVisible('#rx-history', Timeouts.normal)
         .waitForElementVisible('.rx-table', Timeouts.slow);
-      // ensure history tab renders
+      // ensure history view renders
       client.expect.element('.rx-tab-explainer').to.be.visible;
 
       client.end();
