7e3f8907232c5e50cf747b1c6fabfc74d6b939a0
Tiffany Brown <tiffany@webinista.com>
2016-12-27 10:56:30 -0800

    Removed jQuery dependency from toggle-veterans-crisis-line.js closes #4446
    - Removed redundant markup for Veteran's Crisis Line.
    - Changed desktop VCL trigger from anchor to button for clarity
    - Adjusted CSS to reflect markup changes.

--- src/js/legacy/toggle-veterans-crisis-line.js
+++ src/js/legacy/toggle-veterans-crisis-line.js
@@ -1,9 +1,17 @@
-$(function() {
-  var toggleVCL = function(e) {
-    e.preventDefault();
-    // Only affects Desktop version of the Veterans Crisis line.
-    // Mobile is handled via menu.js and works with all overlays.
-    $(this).parents('.crisis-line-container').toggleClass('va-crisis-panel--open');
-  }
-  $('a.va-overlay-trigger').on('click', toggleVCL);
+/*
+This script only affects the DESKTOP/WIDESCREEN version
+of the Veteran's Crisis Line trigger
+*/
+document.addEventListener('DOMContentLoaded', () => {
+  const vcls = document.querySelectorAll('.crisis-line-container--wide');
+  const toggleVcl = (vcl) =>
+    vcl.addEventListener('click', (domEvent) => {
+      domEvent.preventDefault();
+      // Check that it was the va-crisis-line trigger element clicked
+      if(domEvent.target.classList.contains('va-crisis-line')) {
+        domEvent.currentTarget.classList.toggle('va-crisis-panel--open');
+      }
+    }); 
+
+  [].map.call(vcls, toggleVcl);
 });
