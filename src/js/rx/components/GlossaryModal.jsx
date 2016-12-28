0c00096637ccc4fc4e46236560d084c8cc5fd6a2
Benjamin Shyong <ben.shyong@gmail.com>
2016-12-28 13:37:54 -0800

    Resolves #4517; use common modal component for glossary modal

--- src/js/rx/components/GlossaryModal.jsx
+++ src/js/rx/components/GlossaryModal.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Modal from '../../common/components/Modal';
 import GlossaryList from './GlossaryList';
 
 class GlossaryModal extends React.Component {
   constructor(props) {
     super(props);
     this.handleCloseModal = this.handleCloseModal.bind(this);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (!prevProps.visible && this.props.visible) {
-      document.getElementById('rx-glossary-modal').focus();
-    }
   }
 
   handleCloseModal(event) {
@@ -23,28 +18,32 @@ class GlossaryModal extends React.Component {
     let element;
     if (this.props.isVisible) {
       element = (
-        <section className="va-modal rx-modal" id="rx-glossary-modal" role="dialog" aria-labelledby="Glossary">
-          <div className="va-modal-inner">
-            <h3 className="va-modal-title">Glossary</h3>
-            <div className="va-modal-body rx-modal-body">
-              <GlossaryList terms={this.props.content}/>
-              <div className="va-modal-button-group cf">
-                <button type="button" onClick={this.handleCloseModal}>Close</button>
-                <Link
-                    to="/glossary"
-                    onClick={this.props.onCloseModal}>
-                  See all status definitions
-                </Link>
-              </div>
-            </div>
+        <div>
+          <GlossaryList terms={this.props.content}/>
+          <div className="va-modal-button-group cf">
+            <button type="button" onClick={this.handleCloseModal}>Close</button>
+            <Link
+                to="/glossary"
+                onClick={this.props.onCloseModal}>
+              See all status definitions
+            </Link>
           </div>
-        </section>
+        </div>
       );
     } else {
       element = (<div/>);
     }
 
-    return element;
+    return (
+      <Modal
+          cssClass="va-modal rx-modal"
+          contents={element}
+          hideCloseButton
+          id="rx-glossary-modal"
+          onClose={this.handleCloseModal}
+          title="Glossary"
+          visible={this.props.isVisible}/>
+      );
   }
 }
 
