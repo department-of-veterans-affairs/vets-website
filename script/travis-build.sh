59c41ce22aeac2de13ed57b4c62e3156755116d0
James Kassemi <jkassemi@gmail.com>
2017-01-04 08:43:31 -0700

    .

--- script/travis-build.sh
+++ script/travis-build.sh
@@ -58,6 +58,14 @@ echo "TRAVIS_COMMIT=$TRAVIS_COMMIT" >> $BUILD_DETAILS_FILE
 echo "TRAVIS_COMMIT_RANGE=$TRAVIS_COMMIT_RANGE" >> $BUILD_DETAILS_FILE
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH" >> $BUILD_DETAILS_FILE
 
+# Don't run tests for pushes to production. Tests were already run with the
+# PR build that was merged and caused this to run.
+
+if [[ $TRAVIS_BRANCH == "production" ]]
+then
+  exit 0;
+fi
+
 # Run unit tests
 time npm run test:unit;
 
