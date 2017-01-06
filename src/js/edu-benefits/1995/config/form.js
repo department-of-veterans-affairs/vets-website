b67ad7b7a97086f621c6685c6d979615e1f7cb18
Jeff Balboni <jeff@adhocteam.us>
2017-01-05 21:53:58 -0500

    Update config to match actual first page of form

--- src/js/edu-benefits/1995/config/form.js
+++ src/js/edu-benefits/1995/config/form.js
@@ -10,34 +10,35 @@ const formConfig = {
       title: 'Veteran Information',
       pages: {
         veteranInformation: {
-          path: 'veteran-information',
+          path: 'applicant-information',
           initialData: {},
           errorMessages: {
             veteranSocialSecurityNumber: {
               pattern: 'Please enter a valid nine digit SSN (dashes allowed)'
-            },
-            veteranDateOfBirth: {
-              pattern: 'Please provide a valid date'
             }
           },
           uiSchema: {
+            veteranFullName: {
+              suffix: {
+                'ui:options': {
+                  widgetClassNames: 'form-select-medium'
+                }
+              }
+            },
             veteranSocialSecurityNumber: {
               'ui:options': {
                 widgetClassNames: 'usa-input-medium'
               }
-            },
-            gender: {
-              'ui:widget': 'radio'
             }
           },
           schema: {
             type: 'object',
-            title: 'Personal information',
+            title: 'Applicant information',
             definitions: {
               fullName,
               ssn
             },
-            required: ['veteranFullName', 'veteranSocialSecurityNumber', 'veteranDateOfBirth'],
+            required: ['veteranFullName', 'veteranSocialSecurityNumber'],
             properties: {
               veteranFullName: {
                 $ref: '#/definitions/fullName'
@@ -45,16 +46,9 @@ const formConfig = {
               veteranSocialSecurityNumber: {
                 $ref: '#/definitions/ssn'
               },
-              veteranDateOfBirth: {
-                title: 'Date of birth',
-                type: 'string',
-                pattern: '^(\\d{4}|XXXX)-(0[1-9]|1[0-2]|XX)-(0[1-9]|[1-2][0-9]|3[0-1]|XX)$'
-              },
-              gender: {
-                title: 'Gender',
-                type: 'string',
-                'enum': ['F', 'M'],
-                enumNames: ['Female', 'Male']
+              fileNumber: {
+                title: 'File number',
+                type: 'number'
               }
             }
           }
