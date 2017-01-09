82fed309e64237abf6b79f721aeb2a29bf9bb0b8
Jeff Balboni <jeff@adhocteam.us>
2017-01-09 10:53:26 -0500

    Don't put routes on staging or prod

--- src/js/edu-benefits/routes.jsx
+++ src/js/edu-benefits/routes.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, IndexRedirect } from 'react-router';
 import EduBenefitsApp from './1990/containers/EduBenefitsApp';
-import Form1995App from './1995/Form1995App';
 import routes1990 from './1990/routes';
-import routes1995 from './1995/routes';
 import form1990 from './1990/reducers';
+
+import Form1995App from './1995/Form1995App';
+import routes1995 from './1995/routes';
 
 export default function createRoutes(store) {
   // It will be confusing to have multiple forms in one app living
@@ -12,6 +13,12 @@ export default function createRoutes(store) {
   const onEnter = (reducer) => () => {
     store.replaceReducer(reducer);
   };
+
+  const routesFor1995 = __BUILDTYPE__ !== 'production' && __BUILDTYPE__ !== 'staging'
+    ? <Route path="1995" component={Form1995App}>
+      {routes1995}
+    </Route>
+    : null;
 
   // when we actually have more than one form, we should probably load them in
   // separate bundles
@@ -21,9 +28,7 @@ export default function createRoutes(store) {
       <Route path="1990" onEnter={onEnter(form1990)} component={EduBenefitsApp}>
         {routes1990}
       </Route>
-      <Route path="1995" component={Form1995App}>
-        {routes1995}
-      </Route>
+      {routesFor1995}
     </Route>
   );
 }
