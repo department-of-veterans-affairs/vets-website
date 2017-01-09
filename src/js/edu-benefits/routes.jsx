ae58d700aadad10a6242fc8a0bd17c3e24802497
Jeff Balboni <jbalboni@gmail.com>
2017-01-09 12:01:50 -0500

    Form scaffolding for 1995 (#4605)

--- src/js/edu-benefits/routes.jsx
+++ src/js/edu-benefits/routes.jsx
@@ -4,12 +4,22 @@ import EduBenefitsApp from './1990/containers/EduBenefitsApp';
 import routes1990 from './1990/routes';
 import form1990 from './1990/reducers';
 
+// import Form1995App from './1995/Form1995App';
+// import routes1995 from './1995/routes';
+
 export default function createRoutes(store) {
   // It will be confusing to have multiple forms in one app living
   // side by side in the Redux store, so just replace everything when you go into a form
   const onEnter = (reducer) => () => {
     store.replaceReducer(reducer);
   };
+
+  const routesFor1995 = null;
+  // const routesFor1995 = __BUILDTYPE__ !== 'production' && __BUILDTYPE__ !== 'staging'
+  //   ? <Route path="1995" component={Form1995App}>
+  //     {routes1995}
+  //   </Route>
+  //   : null;
 
   // when we actually have more than one form, we should probably load them in
   // separate bundles
@@ -19,6 +29,7 @@ export default function createRoutes(store) {
       <Route path="1990" onEnter={onEnter(form1990)} component={EduBenefitsApp}>
         {routes1990}
       </Route>
+      {routesFor1995}
     </Route>
   );
 }
