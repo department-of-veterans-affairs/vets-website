1323b740468d8d2cce92a0d83634dc963043906f
Eugene Doan <U-DON@users.noreply.github.com>
2017-01-13 18:40:30 -0500

    [Rx] Sorting fixes and unit tests. (#4692)
    * Apply sort query params to Active page.
    
    * Removed incorrect label for prescription name on History page in mobile. Also removed obsolete table styles.
    
    * Make sortable table headers tabbable without href.
    
    * Initial unit tests for SortMenu.
    
    * Updated SortMenu unit test.
    
    * Added unit tests for sort links in SortMenu.
    
    * Added unit tests for active link in SortMenu.
    
    * Attempt to pass tests.
    
    * Revert "Attempt to pass tests."
    
    This reverts commit e49d1c9ab64ad691fe096289ff78c65e60420fdd.
    
    * Another attempt to pass tests.

---
+++ test/rx/components/SortMenu.unit.spec.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import SkinDeep from 'skin-deep';
+import { expect } from 'chai';
+
+import SortMenu from '../../../src/js/rx/components/SortMenu';
+
+const props = {
+  onChange: () => {},
+  onClick: () => {},
+  options: [
+    {
+      label: 'Prescription name',
+      value: 'prescriptionName'
+    },
+    {
+      label: 'Facility name',
+      value: 'facilityName'
+    },
+    {
+      label: 'Last submit date',
+      value: 'lastSubmitDate'
+    },
+    {
+      label: 'Last fill date',
+      value: 'refillDate'
+    }
+  ],
+  selected: {
+    order: 'ASC',
+    value: 'prescriptionName'
+  }
+};
+
+describe('<SortMenu>', () => {
+  it('should render', () => {
+    const tree = SkinDeep.shallowRender(<SortMenu {...props}/>);
+    expect(tree.getRenderOutput()).to.exist;
+  });
+
+  it('should render the correct options', () => {
+    const tree = SkinDeep.shallowRender(<SortMenu {...props}/>).dive(['select']);
+    const options = tree.everySubTree('option');
+    expect(options).to.have.length(props.options.length);
+    options.forEach((option, index) => {
+      const { label, value } = props.options[index];
+      expect(option.text()).to.equal(label);
+      expect(option.props.value).to.equal(value);
+    });
+  });
+
+  it('should render the sort links for string-based sorts', () => {
+    const tree = SkinDeep.shallowRender(<SortMenu {...props}/>).dive(['ul']);
+    const links = tree.everySubTree('li');
+    expect(links).to.have.length(2);
+    expect(links[0].text()).to.equal('A-Z');
+    expect(links[1].text()).to.equal('Z-A');
+  });
+
+  it('should render the sort links for date-based sorts', () => {
+    const tree = SkinDeep.shallowRender(
+      <SortMenu
+          {...props}
+          selected={{
+            order: 'ASC',
+            value: 'refillDate'
+          }}/>
+    ).dive(['ul']);
+    const links = tree.everySubTree('li');
+    expect(links).to.have.length(2);
+    expect(links[0].text()).to.equal('Newest to Oldest');
+    expect(links[1].text()).to.equal('Oldest to Newest');
+  });
+
+  it('should set the string-based ascending order link to active', () => {
+    const tree = SkinDeep.shallowRender(<SortMenu {...props}/>);
+    const activeLink = tree.subTree('.rx-sort-active');
+    expect(activeLink).to.be.ok;
+    expect(activeLink.text()).to.equal('A-Z');
+  });
+
+  it('should set the string-based descending order link to active', () => {
+    const tree = SkinDeep.shallowRender(
+      <SortMenu
+          {...props}
+          selected={{
+            order: 'DESC',
+            value: 'prescriptionName'
+          }}/>
+    );
+    const activeLink = tree.subTree('.rx-sort-active');
+    expect(activeLink).to.be.ok;
+    expect(activeLink.text()).to.equal('Z-A');
+  });
+
+  it('should set the date-based ascending order link to active', () => {
+    const tree = SkinDeep.shallowRender(
+      <SortMenu
+          {...props}
+          selected={{
+            order: 'ASC',
+            value: 'lastSubmitDate'
+          }}/>
+    );
+    const activeLink = tree.subTree('.rx-sort-active');
+    expect(activeLink).to.be.ok;
+    expect(activeLink.text()).to.equal('Oldest to Newest');
+  });
+
+  it('should set the date-based descending order link to active', () => {
+    const tree = SkinDeep.shallowRender(
+      <SortMenu
+          {...props}
+          selected={{
+            order: 'DESC',
+            value: 'lastSubmitDate'
+          }}/>
+    );
+    const activeLink = tree.subTree('.rx-sort-active');
+    expect(activeLink).to.be.ok;
+    expect(activeLink.text()).to.equal('Newest to Oldest');
+  });
+});
