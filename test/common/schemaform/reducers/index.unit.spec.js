6e56b1c534b445056f3bff4dcc033c0909781f87
Jeff Balboni <jeff@adhocteam.us>
2017-01-18 10:26:38 -0500

    WIP

---
+++ test/common/schemaform/reducers/index.unit.spec.js
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+
+import { SET_DATA,
+  SET_VALID,
+  SET_EDIT_MODE,
+  SET_PRIVACY_AGREEMENT,
+  SET_SUBMISSION,
+  SET_SUBMITTED
+} from '../../../../src/js/common/schemaform/actions';
+
+import createSchemaFormReducer from '../../../../src/js/common/schemaform/reducers';
+
+describe('schemaform reducer', () => {
+  it('', () => {
+    
+  });
+});
