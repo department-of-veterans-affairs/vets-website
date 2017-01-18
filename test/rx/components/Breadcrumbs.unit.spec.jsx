0d1f12bfa9e9fd4b81b7e0bc9b4079edf1ebc97c
Benjamin Shyong <ben.shyong@gmail.com>
2017-01-18 14:34:18 -0800

    add breadcrumbs spec

---
+++ test/rx/components/Breadcrumbs.unit.spec.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import SkinDeep from 'skin-deep';
+import { expect } from 'chai';
+
+import Breadcrumbs from '../../../src/js/rx/components/Breadcrumbs.jsx';
+
+const props = {
+  location: {
+    pathname: 'path',
+  },
+  prescription: {
+    rx: {
+      attributes: {
+        prescriptionName: 'prescriptionName',
+      }
+    }
+  }
+};
+
+describe('<Breadcrumbs>', () => {
+  it('should render', () => {
+    const tree = SkinDeep.shallowRender(<Breadcrumbs {...props}/>);
+    const vdom = tree.getRenderOutput();
+    expect(vdom).to.not.be.undefined;
+  });
+
+  it('should have the expected classname', () => {
+    const tree = SkinDeep.shallowRender(<Breadcrumbs {...props}/>);
+
+    expect(tree.props.className).to.equal('rx-breadcrumbs');
+  });
+
+  it('should render the basecase correctly', () => {
+    const tree = SkinDeep.shallowRender(<Breadcrumbs {...props}/>);
+
+    expect(tree.everySubTree('span').pop()).to.be.ok;
+    expect(tree.everySubTree('span').pop().text()).to.equal('Prescription Refills');
+  });
+
+  it('should render on prescription detail pages correctly', () => {
+    const tree = SkinDeep.shallowRender(<Breadcrumbs {...props} location={{ pathname: 'foo/123' }}/>);
+
+    expect(tree.everySubTree('span').pop()).to.be.ok;
+    expect(tree.everySubTree('span').pop().text()).to.equal('prescriptionName');
+  });
+
+  it('should render on prescription detail pages correctly', () => {
+    const tree = SkinDeep.shallowRender(<Breadcrumbs {...props} location={{ pathname: 'foo/123' }}/>);
+
+    expect(tree.everySubTree('span').pop()).to.be.ok;
+    expect(tree.everySubTree('span').pop().text()).to.equal('prescriptionName');
+  });
+
+  it('should render on glossary page correctly', () => {
+    const tree = SkinDeep.shallowRender(<Breadcrumbs {...props} location={{ pathname: 'foo/glossary/' }}/>);
+
+    expect(tree.everySubTree('span').pop()).to.be.ok;
+    expect(tree.everySubTree('span').pop().text()).to.equal('Glossary');
+  });
+});
