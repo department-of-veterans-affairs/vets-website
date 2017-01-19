cfba46458babfd8d3abb8903e25927f767f2d613
Benjamin Shyong <ben.shyong@gmail.com>
2017-01-19 10:37:12 -0800

    Message provider link unit test

---
+++ test/rx/components/MessageProviderLink.unit.spec.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import SkinDeep from 'skin-deep';
+import { expect } from 'chai';
+
+import MessageProviderLink from '../../../src/js/rx/components/MessageProviderLink';
+// glossary terms are defined in the config file
+
+describe('<MessageProviderLink>', () => {
+  it('should render', () => {
+    const tree = SkinDeep.shallowRender(<MessageProviderLink/>);
+    const vdom = tree.getRenderOutput();
+    expect(vdom).to.not.be.undefined;
+  });
+
+  it('should have the expected className', () => {
+    const tree = SkinDeep.shallowRender(<MessageProviderLink/>);
+
+    expect(tree.props.className).to.equal('usa-button rx-message-provider-link');
+  });
+});
