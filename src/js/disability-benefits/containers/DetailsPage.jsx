262e0f6a37c572e7980eee01c8370a1f216c3ebe
Austin <amartin857+github@gmail.com>
2017-01-23 13:09:39 -0800

    Update claimed contentions header text

--- src/js/disability-benefits/containers/DetailsPage.jsx
+++ src/js/disability-benefits/containers/DetailsPage.jsx
@@ -35,7 +35,7 @@ class DetailsPage extends React.Component {
             <p>{claim.attributes.claimType || 'Not Available'}</p>
           </div>
           <div className="claim-contentions-list">
-            <h6>Your Claimed Contentions</h6>
+            <h6>What you've claimed</h6>
             {claim.attributes.contentionList && claim.attributes.contentionList.length
             ? claim.attributes.contentionList.map((contention, index) =>
               <li key={index}>{contention}</li>
