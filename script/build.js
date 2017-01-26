3a7c63c149ce60cd5c07c256a4073f3d84aca3c3
Aubrey Holland <aubreyholland@gmail.com>
2017-01-26 13:57:27 -0500

    fix lint

--- script/build.js
+++ script/build.js
@@ -279,20 +279,20 @@ if (options.buildtype !== 'development') {
   // are passed a list of files that are included in the build. Those files are not yet written
   // to disk, but the contents are held in memory.
   //
-  smith.use(function(files, metalsmith, done) {
+  smith.use((files, metalsmith, done) => {
     // Read in the data from the manifest file.
-    var manifestKey = Object.keys(files).find(function(filename) {
+    const manifestKey = Object.keys(files).find((filename) => {
       return filename.match(/file-manifest.json$/) !== null;
     });
-    var originalManifest = JSON.parse(files[manifestKey].contents.toString());
+    const originalManifest = JSON.parse(files[manifestKey].contents.toString());
 
     // The manifest contains the original filenames without the addition of .entry
     // on the JS files. This finds all of those and modifies them to add .entry.
-    var manifest = {};
-    Object.keys(originalManifest).forEach(function(originalManifestKey) {
-      var matchData = originalManifestKey.match(/(.*)\.js$/);
+    const manifest = {};
+    Object.keys(originalManifest).forEach((originalManifestKey) => {
+      const matchData = originalManifestKey.match(/(.*)\.js$/);
       if (matchData !== null) {
-        var newKey = `${matchData[1]}.entry.js`;
+        const newKey = `${matchData[1]}.entry.js`;
         manifest[newKey] = originalManifest[originalManifestKey];
       } else {
         manifest[originalManifestKey] = originalManifest[originalManifestKey];
@@ -302,13 +302,13 @@ if (options.buildtype !== 'development') {
     // For each file in the build, if it is a HTML or CSS file, loop over all
     // of the keys in the manifest object and do a search and replace for the
     // key with the value.
-    Object.keys(files).forEach(function(filename) {
+    Object.keys(files).forEach((filename) => {
       if (filename.match(/\.(html|css)$/) !== null) {
-        Object.keys(manifest).forEach(function(originalAssetFilename) {
-          var newAssetFilename = manifest[originalAssetFilename];
-          var file = files[filename];
-          var contents = file.contents.toString();
-          var regex = new RegExp(originalAssetFilename, 'g');
+        Object.keys(manifest).forEach((originalAssetFilename) => {
+          const newAssetFilename = manifest[originalAssetFilename];
+          const file = files[filename];
+          const contents = file.contents.toString();
+          const regex = new RegExp(originalAssetFilename, 'g');
           file.contents = new Buffer(contents.replace(regex, newAssetFilename));
         });
       }
