fdd4997084d1320f54dfe7eb51462e63f4fc5943
Benjamin Shyong <ben.shyong@gmail.com>
2017-01-30 10:55:21 -0800

    scaffold entry point app

---
+++ src/js/blue-button/containers/BlueButtonApp.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import RequiredLoginView from '../../common/components/RequiredLoginView';
+
+// This needs to be a React component for RequiredLoginView to pass down
+// the isDataAvailable prop, which is only passed on failure.
+function AppContent({ children, isDataAvailable }) {
+  const unregistered = isDataAvailable === false;
+  let view;
+
+  if (unregistered) {
+    view = (
+      <h4>
+        Placeholder message when data is not available
+      </h4>
+    );
+  } else {
+    view = children;
+  }
+
+  return <div className="blue-button-app">{view}</div>;
+}
+
+class BlueButtonApp extends React.Component {
+  render() {
+    return (
+      <RequiredLoginView authRequired={3} serviceRequired={"bluebutton"}>
+        <AppContent>
+          <h1>Blue Button App</h1>
+        </AppContent>
+      </RequiredLoginView>
+    );
+  }
+}
+
+BlueButtonApp.propTypes = {
+  children: React.PropTypes.element
+};
+
+const mapStateToProps = (state) => state;
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BlueButtonApp);
