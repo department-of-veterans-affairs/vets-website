586ed57c418628479b86fba19d9dca00054f5715
Eugene Doan <eugene@adhocteam.us>
2017-02-01 14:35:23 -0500

    Pass placeholder empty props in unit test.

--- test/blue-button/containers/DownloadPage.unit.spec.jsx
+++ test/blue-button/containers/DownloadPage.unit.spec.jsx
@@ -4,8 +4,10 @@ import { expect } from 'chai';
 
 import { DownloadPage } from '../../../src/js/blue-button/containers/DownloadPage';
 
-describe('DownloadPage', () => {
-  const tree = SkinDeep.shallowRender(<DownloadPage/>);
+const props = {};
+
+describe('<DownloadPage>', () => {
+  const tree = SkinDeep.shallowRender(<DownloadPage {...props}/>);
 
   it('should render', () => {
     const vdom = tree.getRenderOutput();
