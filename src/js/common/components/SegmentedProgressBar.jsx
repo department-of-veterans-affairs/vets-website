2de594488bb2d58f2e377913b282ffe286fac2c3
Christopher Valarida <cvalarida@gmail.com>
2017-02-03 14:24:18 -0800

    Clarifies params for SegmentedProgressBar
    Based on what I found in common/schemaform/FormNav.jsx, both params for SegmentedProgressBar are numbers.

--- src/js/common/components/SegmentedProgressBar.jsx
+++ src/js/common/components/SegmentedProgressBar.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import _ from 'lodash';
 
+/**
+ * Create a segmented progress bar for multi-page forms.
+ *
+ * @param {number} current - The index of the current chapter
+ * @param {number} total   - The total number of chapters in the form
+ */
 export default function SegmentedProgressBar({ current, total }) {
   return (
     <div className="progress-bar-segmented" role="progressbar" aria-valuenow={current} aria-valuemin="0" aria-valuemax={total} tabIndex="0">
