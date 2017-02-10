5169f21f36db0052cdb6d4c42288e2200f7ae22c
Jeff Balboni <jeff@adhocteam.us>
2017-02-10 11:55:44 -0500

    Add tests

--- src/js/common/schemaform/review/ReviewFieldTemplate.jsx
+++ src/js/common/schemaform/review/ReviewFieldTemplate.jsx
@@ -7,8 +7,19 @@ import React from 'react';
 export default function ReviewFieldTemplate(props) {
   const { children, uiSchema, schema } = props;
   const label = uiSchema['ui:title'] || props.label;
+  const hasTextDescription = typeof uiSchema['ui:description'] === 'string';
+  const DescriptionField = !hasTextDescription && typeof uiSchema['ui:description'] === 'function'
+    ? uiSchema['ui:description']
+    : null;
 
   return schema.type === 'object' || schema.type === 'array'
     ? children
-    : <div className="review-row"><dt>{label}</dt><dd>{children}</dd></div>;
+    : <div className="review-row">
+      <dt>
+        {label}
+        {hasTextDescription && <p>{uiSchema['ui:description']}</p>}
+        {DescriptionField && <DescriptionField options={uiSchema['ui:options']}/>}
+      </dt>
+      <dd>{children}</dd>
+    </div>;
 }
