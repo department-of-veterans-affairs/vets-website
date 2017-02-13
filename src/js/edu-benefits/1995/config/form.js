5e79af5ccb7bb27c41119a509b3618332ad74ce8
Christopher Valarida <cvalarida@gmail.com>
2017-02-13 14:28:57 -0800

    Added examples

--- src/js/edu-benefits/1995/config/form.js
+++ src/js/edu-benefits/1995/config/form.js
@@ -252,7 +252,7 @@ const formConfig = {
             },
             trainingEndDate: _.merge(date.uiSchema, { 'ui:title': 'When did you stop taking classes or participating in the training program?' }),
             reasonForChange: {
-              'ui:title': 'Why did you stop taking classes or participating in the training program?'
+              'ui:title': 'Why did you stop taking classes or participating in the training program? (For example, "Graduated" or "Moved" or "Did not like the school.")'
             }
           },
           schema: {
