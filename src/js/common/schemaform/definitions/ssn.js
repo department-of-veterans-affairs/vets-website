4dc8523b16e39c2c5c1aa9c11bea5ccd6c57ee6c
Jeff Balboni <jbalboni@gmail.com>
2017-02-16 12:15:43 -0500

    Allow dashes on SSN for 1995 form (#4859)

--- src/js/common/schemaform/definitions/ssn.js
+++ src/js/common/schemaform/definitions/ssn.js
@@ -1,9 +1,13 @@
 import { validateSSN } from '../validation';
 import commonDefinitions from 'vets-json-schema/dist/definitions.json';
+import SSNWidget from '../widgets/SSNWidget';
+import SSNReviewWidget from '../review/SSNWidget';
 
 export const schema = commonDefinitions.ssn;
 
 export const uiSchema = {
+  'ui:widget': SSNWidget,
+  'ui:reviewWidget': SSNReviewWidget,
   'ui:title': 'Social Security number',
   'ui:options': {
     widgetClassNames: 'usa-input-medium'
---
+++ test/common/schemaform/review/SSNWidget.unit.spec.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { expect } from 'chai';
+import SkinDeep from 'skin-deep';
+
+import SSNWidget from '../../../../src/js/common/schemaform/review/SSNWidget';
+
+describe('Schemaform review <SSNWidget>', () => {
+  it('should format ssn', () => {
+    const tree = SkinDeep.shallowRender(
+      <SSNWidget value="123456789"/>
+    );
+
+    expect(tree.text()).to.equal('123-45-6789');
+  });
+  it('should render empty value', () => {
+    const tree = SkinDeep.shallowRender(
+      <SSNWidget/>
+    );
+
+    expect(tree.text()).to.equal('');
+  });
+});
