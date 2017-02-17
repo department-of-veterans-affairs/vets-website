2b0e401dd59ec552010954fac4f523425974baf3
Christopher Valarida <cvalarida@gmail.com>
2017-02-17 15:54:18 -0800

    Formats the phone number on the review page

---
+++ src/js/common/schemaform/widgets/PhoneNumberWidget.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import TextWidget from './TextWidget';
+
+/*
+ * Handles removing dashes from SSNs by keeping the user input in local state
+ * and saving the transformed version instead
+ */
+export default class PhoneNumberWidget extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { val: props.value };
+  }
+  handleChange = (val) => {
+    const stripped = val.replace(/[\D]/g, '');
+    this.setState({ val }, () => {
+      this.props.onChange(stripped);
+    });
+  }
+  render() {
+    return <TextWidget {...this.props} value={this.state.val} onChange={this.handleChange}/>;
+  }
+}
