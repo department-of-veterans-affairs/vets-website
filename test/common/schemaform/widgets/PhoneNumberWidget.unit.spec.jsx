c1589435b27ed773afdd4984df86d070c5dafaf8
Christopher Valarida <cvalarida@gmail.com>
2017-02-21 13:54:28 -0800

    Add test for undefined

--- test/common/schemaform/widgets/PhoneNumberWidget.unit.spec.jsx
+++ test/common/schemaform/widgets/PhoneNumberWidget.unit.spec.jsx
@@ -13,14 +13,24 @@ describe('Schemaform <PhoneNumberWidget>', () => {
     );
     expect(tree.subTree('TextWidget').props.value).to.equal('1234567890');
   });
-  it('should remove all non-digit characters on change', () => {
+  it('should strip spaces, (, ), -, +, and x on change', () => {
     const onChange = sinon.spy();
     const tree = SkinDeep.shallowRender(
       <PhoneNumberWidget
           value=""
           onChange={onChange}/>
     );
-    tree.subTree('TextWidget').props.onChange('(154945 -5677');
+    tree.subTree('TextWidget').props.onChange('+(154) 945-56x77');
     expect(onChange.calledWith('1549455677')).to.be.true;
+  });
+  it('should call onChange with undefined if value is blank', () => {
+    const onChange = sinon.spy();
+    const tree = SkinDeep.shallowRender(
+      <PhoneNumberWidget
+          value="1231231234"
+          onChange={onChange}/>
+    );
+    tree.subTree('TextWidget').props.onChange('');
+    expect(onChange.calledWith(undefined)).to.be.true;
   });
 });
