80d0e5580b07ae6b17d9aabd0186ba3e3592e3ab
Christopher Valarida <cvalarida@gmail.com>
2017-02-22 11:12:16 -0800

    DRYing out the date formatting

--- src/js/edu-benefits/1995/components/ServicePeriodView.jsx
+++ src/js/edu-benefits/1995/components/ServicePeriodView.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
-
-/**
- * Accepts YYYY-MM-DD and turns it into DD/MM/YYYY
- *
- * @param {String} date
- * @return {String}
- */
-function formatDisplayDate(date) {
-  return date.replace(/([\dX]{4})-([\dX]{2})-([\dX]{2})/, '$2/$3/$1');
-}
+import { formatReviewDate } from '../../../common/schemaform/helpers';
 
 export default function ServicePeriodView({ formData }) {
-  const from = formatDisplayDate(formData.dateRange.from);
-  const to = formatDisplayDate(formData.dateRange.to);
+  const from = formatReviewDate(formData.dateRange.from);
+  const to = formatReviewDate(formData.dateRange.to);
   return (
     <div>
       <strong>{formData.serviceBranch}</strong><br/>
