7ea2e9097bc63582dd1a480881ebc6483100d41c
Christopher Valarida <cvalarida@gmail.com>
2017-02-22 10:39:19 -0800

    Formats service period dates

--- src/js/edu-benefits/1995/components/ServicePeriodView.jsx
+++ src/js/edu-benefits/1995/components/ServicePeriodView.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
+/**
+ * Accepts YYYY-MM-DD and turns it into DD/MM/YYYY
+ *
+ * @param {String} date
+ * @return {String}
+ */
+function formatDisplayDate(date) {
+  return date.replace(/([\dX]{4})-([\dX]{2})-([\dX]{2})/, '$2/$3/$1');
+}
+
 export default function ServicePeriodView({ formData }) {
+  const from = formatDisplayDate(formData.dateRange.from);
+  const to = formatDisplayDate(formData.dateRange.to);
   return (
     <div>
       <strong>{formData.serviceBranch}</strong><br/>
-      {formData.dateRange.from} &mdash; {formData.dateRange.to}
+      {from} &mdash; {to}
     </div>
   );
 }
