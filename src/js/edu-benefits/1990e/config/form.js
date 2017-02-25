089d57fb306a0260841f12af056a0a0f1347e3d8
raquelromano-gov <raquel_a_romano@omb.eop.gov>
2017-02-24 19:53:42 -0800

    Add page titles and trnasform function

--- src/js/edu-benefits/1990e/config/form.js
+++ src/js/edu-benefits/1990e/config/form.js
@@ -1,3 +1,5 @@
+import transform from '../helpers';
+
 import IntroductionPage from '../components/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
 
@@ -5,32 +7,43 @@ const formConfig = {
   urlPrefix: '/1990e/',
   submitUrl: '/v0/education_benefits_claims/1990e',
   trackingPrefix: 'edu-1990e-',
+  transformForSubmit: transform,
   introduction: IntroductionPage,
   confirmation: ConfirmationPage,
   title: 'Apply for transferred education benefits',
   subTitle: 'Form 22-1990e',
   chapters: {
     dependentInformation: {
+      title: 'Dependent Information',
       pages: {
+        dependentInformation: {
+          path: 'dependent-information',
+          title: 'Dependent information'
+        }
       }
     },
     benefitSelection: {
+      title: 'Education Benefit',
       pages: {
       }
     },
     educationHistory: {
+      title: 'Education History',
       pages: {
       }
     },
     employmentHistory: {
+      title: 'Employment History',
       pages: {
       }
     },
     schoolSelection: {
+      title: 'School Selection',
       pages: {
       }
     },
     personalInformation: {
+      title: 'Personal Information',
       pages: {
       }
     }
