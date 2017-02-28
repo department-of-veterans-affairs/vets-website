bb4008f0636269242216f778d2cd5d990e523287
Christopher Valarida <cvalarida@gmail.com>
2017-02-28 10:25:10 -0800

    Fight -> flight

--- src/js/edu-benefits/definitions/educationType.js
+++ src/js/edu-benefits/definitions/educationType.js
@@ -7,7 +7,7 @@ const educationTypeLabels = {
   college: 'College, university, or other educational program, including online courses',
   correspondence: 'Correspondence',
   apprenticeship: 'Apprenticeship or on-the-job training',
-  flightTraining: 'Vocational fight training',
+  flightTraining: 'Vocational flight training',
   testReimbursement: 'National test reimbursement (for example, SAT or CLEP)',
   licensingReimbursement: 'Licensing or certification test reimbursement (for example, MCSE, CCNA, EMT, or NCLEX)',
   tuitionTopUp: 'Tuition assistance top up (Post 9/11 GI Bill and MGIB-AD only)',
