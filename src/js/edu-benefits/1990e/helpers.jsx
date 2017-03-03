c7e97a9cd3c50a66c247f83efb29b860a1c81389
Christopher Valarida <cvalarida@gmail.com>
2017-03-03 13:21:05 -0800

    Add benefit selection

--- src/js/edu-benefits/1990e/helpers.jsx
+++ src/js/edu-benefits/1990e/helpers.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { transformForSubmit } from '../../common/schemaform/helpers';
 
 export function transform(formConfig, form) {
@@ -8,3 +10,24 @@ export function transform(formConfig, form) {
     }
   });
 }
+
+export function eligibilityDescription() {
+  return (
+    <div className="usa-alert usa-alert-warning usa-content">
+      <div className="usa-alert-body">
+        <ul>
+          <li>You may be eligible for more than 1 education benefit program.</li>
+          <li>You can only get payments from 1 program at a time.</li>
+          <li>You can’t get more than 48 months of benefits under any combination of VA education programs.</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export const benefitsLabels = {
+  chapter33: <p>Post-9/11 GI Bill (Chapter 33)<br/><a href="/education/gi-bill/post-9-11/" target="_blank">Learn more</a></p>,
+  chapter30: <p>Montgomery GI Bill (MGIB-AD, Chapter 30)<br/><a href="/education/gi-bill/montgomery-active-duty/" target="_blank">Learn more</a></p>,
+  chapter1606: <p>Montgomery GI Bill Selected Reserve (MGIB-SR, Chapter 1606)<br/><a href="/education/gi-bill/montgomery-selected-reserve/" target="_blank">Learn more</a></p>,
+  chapter1607: <p>Reserve Educational Assistance Program (REAP, Chapter 1607)<br/><a href="/education/other-educational-assistance-programs/reap/" target="_blank">Learn more</a></p>,
+};
