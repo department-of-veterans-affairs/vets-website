e65c5f13bf185e092e3504a9b7b8d739a0ec0677
raquelromano-gov <raquel_a_romano@omb.eop.gov>
2017-03-07 11:04:56 -0800

    Start of unit test for 1990e personal information

--- src/js/edu-benefits/utils/helpers.jsx
+++ src/js/edu-benefits/utils/helpers.jsx
@@ -24,6 +24,17 @@ export const benefitsLabels = {
   transferOfEntitlement: <p>Transfer of Entitlement Program (TOE)<br/><a href="/education/gi-bill/transfer/" target="_blank">Learn more</a></p>
 };
 
+export const preferredContactMethodLabels = {
+  mail: 'Mail',
+  email: 'Email',
+  phone: 'Phone'
+};
+
+export const relationshipLabels = {
+  spouse: 'Spouse',
+  child: 'Child'
+};
+
 export function getLabel(options, value) {
   const matched = _.find(options, option => option.value === value);
 
@@ -135,14 +146,3 @@ export function showYesNo(field) {
 export function enumToNames(enumValues, names) {
   return enumValues.map(item => names[item]);
 }
-
-export const preferredContactMethodLabels = {
-  mail: 'Mail',
-  email: 'Email',
-  phone: 'Phone'
-};
-
-export const relationshipLabels = {
-  spouse: 'Spouse',
-  child: 'Child'
-};
---
+++ test/edu-benefits/1990e/config/personalInformation.unit.spec.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { expect } from 'chai';
+import ReactTestUtils from 'react-addons-test-utils';
+
+import { DefinitionTester } from '../../../util/schemaform-utils.jsx';
+import formConfig from '../../../../src/js/edu-benefits/1990e/config/form';
+
+describe('Edu 1990e personalInformation', () => {
+  const { schema, uiSchema } = formConfig.chapters.benefitEligibility.pages.personalInformation;
+  const definitions = formConfig.defaultDefinitions;
+  it('should render', () => {
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          data={{}}
+          uiSchema={uiSchema}
+          definitions={definitions}/>
+    );
+
+    expect(ReactTestUtils.scryRenderedDOMComponentsWithTag(form, 'input'))
+      .to.not.be.empty;
+  });
+});
+
