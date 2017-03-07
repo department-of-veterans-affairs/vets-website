5e19fdd7e7fc204e539edae717f65fa71a479324
Ben Damman <benjamin_c_damman@omb.eop.gov>
2017-03-07 00:36:32 -0500

    some fixes to outcome graphs

--- src/js/gi/selectors/outcomes.js
+++ src/js/gi/selectors/outcomes.js
@@ -3,36 +3,100 @@ import { createSelector } from 'reselect';
 const getConstants = (state) => state.constants.constants;
 
 const getRequiredAttributes = (state) => {
-  const { graduationRateVeteran, graduationRateAllStudents, salaryAllStudents } = state.profile.attributes;
+  const {
+    graduationRateVeteran,
+    graduationRateAllStudents,
+    salaryAllStudents,
+    predDegreeAwarded,
+    vaHighestDegreeOffered,
+    retentationRateVeteranOtb,
+    retentionRateVeteranBa,
+    retentionAllStudentsBa,
+    retentionAllStudentsOtb,
+    repaymentRateAllStudents,
+  } = state.profile.attributes;
   return {
     graduationRateVeteran,
     graduationRateAllStudents,
-    salaryAllStudents
+    salaryAllStudents,
+    predDegreeAwarded,
+    vaHighestDegreeOffered,
+    retentationRateVeteranOtb,
+    retentionRateVeteranBa,
+    retentionAllStudentsBa,
+    retentionAllStudentsOtb,
+    repaymentRateAllStudents,
+  };
+};
+
+const isNumeric = (n) => ( !Number.isNaN(parseFloat(n)) );
+
+const whenDataAvailable = (n1, n2, obj) => {
+  if (isNumeric(n1) || isNumeric(n2)) return obj;
+  return {
+    error: 'Data Not Available'
   };
 };
 
 export const outcomeNumbers = createSelector(
   [getConstants, getRequiredAttributes],
   (constant, institution) => {
-    const retention = {
-      rate: 30,
-      all: 97.83,
-      average: constant.AVERETENTIONRATE,
+    const getVeteranRetentionRate = () => {
+      const a = [3, 4].includes(institution.predDegreeAwarded);
+      const b = String(institution.vaHighestDegreeOffered).toLowerCase() === '4-year';
+      const upperClass = a || b;
+      if (upperClass) {
+        return institution.retentationRateVeteranBa || institution.retentionRateVeteranOtb;
+      } else {
+        return institution.retentationRateVeteranOtb || institution.retentionRateVeteranBa;
+      }
     };
-    const graduation = {
-      rate: Number(institution.graduationRateVeteran * 100),
-      all: Number(institution.graduationRateAllStudents * 100),
-      average: constant.AVEGRADRATE,
+    const getAllStudentRetentionRate = () => {
+      const a = [3, 4].includes(institution.predDegreeAwarded);
+      const b = String(institution.vaHighestDegreeOffered).toLowerCase() === '4-year';
+      const upperClass = a || b;
+      if (upperClass) {
+        return institution.retentionAllStudentsBa || institution.retentionAllStudentsOtb;
+      } else {
+        return institution.retentionAllStudentsOtb || institution.retentionAllStudentsBa;
+      }
     };
-    const salary = {
-      all: institution.salaryAllStudents,
-      average: constant.AVESALARY,
-    };
-    const repayment = {
-      rate: Number(institution.graduationRateVeteran * 100),
-      all: Number(institution.graduationRateAllStudents * 100),
-      average: constant.AVEGRADRATE,
-    };
+
+    const retention = whenDataAvailable(
+      getVeteranRetentionRate(),
+      getAllStudentRetentionRate(),
+      {
+        rate: isNumeric(getVeteranRetentionRate()) ? Number(getVeteranRetentionRate(institution) * 100) : null,
+        all: isNumeric(getAllStudentRetentionRate()) ? Number(getAllStudentRetentionRate(institution) * 100) : null,
+        average: constant.AVERETENTIONRATE,
+      }
+    );
+    const graduation = whenDataAvailable(
+      institution.graduationRateVeteran,
+      institution.graduationRateAllStudents,
+      {
+        rate: Number(institution.graduationRateVeteran * 100),
+        all: Number(institution.graduationRateAllStudents * 100),
+        average: constant.AVEGRADRATE,
+      }
+    );
+    const salary = whenDataAvailable(
+      null,
+      institution.salaryAllStudents,
+      {
+        all: institution.salaryAllStudents,
+        average: constant.AVESALARY,
+      }
+    );
+    const repayment = whenDataAvailable(
+      null,
+      institution.repaymentRateAllStudents,
+      {
+        rate: null,
+        all: Number(institution.repaymentRateAllStudents * 100),
+        average: constant.AVEREPAYMENTRATE,
+      }
+    );
     return {
       retention,
       graduation,
@@ -41,56 +105,3 @@ export const outcomeNumbers = createSelector(
     };
   }
 );
-
-
-// const getVeteranRetentionRate = (it) => {
-//   const a = [3, 4].includes(it.pred_degree_awarded);
-//   const b = String(it.va_highest_degree_offered).toLowerCase() === '4-year';
-//   const upperClass = a || b;
-//   if (upperClass) {
-//     return it.retentation_rate_veteran_ba || it.retention_rate_veteran_otb;
-//   } else {
-//     return it.retentation_rate_veteran_otb || it.retention_rate_veteran_ba;
-//   }
-// };
-// const getAllStudentRetentionRate = (it) => {
-//   const a = [3, 4].includes(it.pred_degree_awarded);
-//   const b = String(it.va_highest_degree_offered).toLowerCase() === '4-year';
-//   const upperClass = a || b;
-//   if (upperClass) {
-//     return it.retention_all_students_ba || it.retention_all_students_otb;
-//   } else {
-//     return it.retention_all_students_otb || it.retention_all_students_ba;
-//   }
-// };
-// const isNumeric = (n) => { return !Number.isNaN(parseFloat(n)); };
-//
-// const retention = {
-//   rate: isNumeric(getVeteranRetentionRate(institution)) ? Number(getVeteranRetentionRate(institution) * 100) : null,
-//   all: isNumeric(getAllStudentRetentionRate(institution)) ? Number(getAllStudentRetentionRate(institution) * 100) : null,
-//   average: constant.AVERETENTIONRATE,
-// };
-// const repayment = {
-//   rate: null,
-//   all: Number(institution.repayment_rate_all_students * 100),
-//   average: constant.AVEREPAYMENTRATE,
-// };
-// // const graduation = {
-// //   rate: () => {
-// //     console.log(graduationRate: institution.graduation_rate_veteran)
-// //     isNumeric(institution.graduation_rate_veteran) ? Number(institution.graduation_rate_veteran* 100) : null
-// //   },
-// //   all: () => {
-// //     console.log({graduationAll: institution.graduation_rate_all_students})
-// //     isNumeric(institution.graduation_rate_all_students) ? Number(institution.graduation_rate_all_students* 100) : null
-// //   },
-// //   average: constant.AVEGRADRATE,
-// // };
-//
-// const graduation = function(){
-//   return {
-//     rate: Number(institution.graduation_rate_veteran* 100),
-//     all: Number(institution.graduation_rate_all_students* 100),
-//     average: constant.AVEGRADRATE
-//   };
-// }();
