8e4cb2cf49d4e385ebaa676c7f09def75ab28e7f
Christopher Valarida <cvalarida@gmail.com>
2017-03-07 17:14:29 -0800

    Unit test for applicantService

---
+++ test/edu-benefits/5490/config/applicantService.unit.spec.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { findDOMNode } from 'react-dom';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import ReactTestUtils from 'react-addons-test-utils';
+
+import { DefinitionTester, submitForm } from '../../../util/schemaform-utils.jsx';
+import formConfig from '../../../../src/js/edu-benefits/5490/config/form';
+
+describe('Edu 5490 militaryService -> applicantService', () => {
+  const { schema, uiSchema } = formConfig.chapters.militaryService.pages.applicantService;
+  it('should render', () => {
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          data={{}}
+          uiSchema={uiSchema}/>
+    );
+    const fields = Array.from(findDOMNode(form).querySelectorAll('input, select'));
+
+    expect(fields.length).to.equal(8);
+  });
+
+  it('should have no required inputs', () => {
+    const onSubmit = sinon.spy();
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          onSubmit={onSubmit}
+          data={{}}
+          uiSchema={uiSchema}/>
+    );
+    const formDOM = findDOMNode(form);
+    submitForm(form);
+
+    expect(Array.from(formDOM.querySelectorAll('.usa-input-error'))).to.be.empty;
+
+    expect(onSubmit.called).to.be.true;
+  });
+
+  it('should add another', () => {
+    const onSubmit = sinon.spy();
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          onSubmit={onSubmit}
+          data={{}}
+          uiSchema={uiSchema}/>
+    );
+    const formDOM = findDOMNode(form);
+    ReactTestUtils.Simulate.change(formDOM.querySelector('input'), {
+      target: {
+        value: 'Army'
+      }
+    });
+    ReactTestUtils.Simulate.click(formDOM.querySelector('.va-growable-add-btn'));
+
+    expect(formDOM.querySelector('.va-growable-background').textContent)
+      .to.contain('Army');
+  });
+});
