b394f7ed6c7f85ef05271940e64d21ef34323cb7
Jeff Balboni <jeff@adhocteam.us>
2017-03-09 17:23:56 -0500

    Fix 1995

--- src/js/edu-benefits/1995/config/form.js
+++ src/js/edu-benefits/1995/config/form.js
@@ -15,7 +15,7 @@ import * as date from '../../../common/schemaform/definitions/date';
 import * as dateRange from '../../../common/schemaform/definitions/dateRange';
 import * as address from '../../../common/schemaform/definitions/address';
 
-import * as educationType from '../../definitions/educationType';
+import educationTypeUISchema from '../../definitions/educationType';
 import * as serviceBefore1977 from '../../definitions/serviceBefore1977';
 import { uiSchema as toursOfDutyUI } from '../../definitions/toursOfDuty';
 import contactInformation from '../../definitions/contactInformation';
@@ -37,7 +37,8 @@ const {
 const {
   preferredContactMethod,
   school,
-  vaFileNumber
+  vaFileNumber,
+  educationType
 } = fullSchema1995.definitions;
 
 const formConfig = {
@@ -48,7 +49,6 @@ const formConfig = {
   introduction: IntroductionPage,
   confirmation: ConfirmationPage,
   defaultDefinitions: {
-    educationType,
     preferredContactMethod,
     serviceBefore1977,
     date: date.schema,
@@ -190,7 +190,7 @@ const formConfig = {
             newSchoolName: {
               'ui:title': 'Name of school, university, or training facility'
             },
-            educationType: educationType.uiSchema,
+            educationType: educationTypeUISchema,
             newSchoolAddress: _.merge(address.uiSchema(), {
               'ui:options': {
                 hideIf: (form) => !showSchoolAddress(form.educationType)
@@ -216,7 +216,7 @@ const formConfig = {
               newSchoolName: {
                 type: 'string'
               },
-              educationType: educationType.schema,
+              educationType,
               newSchoolAddress: address.schema(),
               educationObjective,
               nonVaAssistance,
