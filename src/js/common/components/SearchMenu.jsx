1e43032957247ef1f1df3e8352232b50dd25a2ab
Tiffany Brown <tiffanybbrown.va@gmail.com>
2017-03-08 16:47:05 -0800

    Adding focus to search input when visible.

--- src/js/common/components/SearchMenu.jsx
+++ src/js/common/components/SearchMenu.jsx
@@ -1,36 +1,73 @@
 import React from 'react';
-import DropDown from './DropDown';
+import classNames from 'classnames';
+
 import IconSearch from './svgicons/IconSearch';
 
 class SearchMenu extends React.Component {
-  render() {
-    const icon = <IconSearch color="#fff"/>;
+  constructor(props) {
+    super(props);
+    this.makeForm = this.makeForm.bind(this);
+    this.toggleSearchForm = this.toggleSearchForm.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
 
-    const dropDownContents = (
-      <form acceptCharset="UTF-8" action="https://search.vets.gov/search" id="search" method="get">
+  componentDidUpdate() {
+    this.refs.searchField.focus();
+  }
+
+  toggleSearchForm() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  makeForm() {
+    return (
+      <form
+          acceptCharset="UTF-8"
+          action="https://search.vets.gov/search"
+          className="va-dropdown-panel"
+          hidden={!this.state.isOpen}
+          id="search"
+          method="get">
         <div className="csp-inline-patch-header">
           <input name="utf8" type="hidden" value="&#x2713;"/>
         </div>
         <input id="affiliate" name="affiliate" type="hidden" value="vets.gov_search"/>
         <label htmlFor="query" className="usa-sr-only">Search:</label>
+
         <div className="va-flex">
-          <input autoComplete="off" className="usagov-search-autocomplete" id="query" name="query" type="text"/>
+          <input autoComplete="off" ref="searchField" className="usagov-search-autocomplete" id="query" name="query" type="text"/>
           <button type="submit">
             <IconSearch color="#fff"/>
             <span className="usa-sr-only">Search</span>
           </button>
         </div>
-      </form>
+      </form>);
+  }
+
+  render() {
+    const buttonClasses = classNames(
+      this.props.cssClass,
+      'va-btn-withicon',
+      'va-dropdown-trigger'
     );
 
     return (
-      <DropDown
-          buttonText="Search"
-          contents={dropDownContents}
-          cssClass={this.props.cssClass}
-          id="searchmenu"
-          icon={icon}
-          isOpen={false}/>
+      <div className="va-dropdown">
+        <button className={buttonClasses}
+            aria-controls="search"
+            aria-expanded={this.state.isOpen}
+            onClick={this.toggleSearchForm}
+            ref="clickTrigger">
+          <span>
+            <IconSearch color="#fff"/>
+            Search
+          </span>
+        </button>
+        {this.makeForm()}
+      </div>
+
     );
   }
 }
