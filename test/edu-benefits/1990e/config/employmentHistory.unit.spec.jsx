57614c7096c4b812d0a48cd3eba87c2c8c5b35f3
Anne Kainic <annekainicUSDS@users.noreply.github.com>
2017-03-13 13:33:33 -0400

    1990e Chapter 5 Employment History (#5073)

---
+++ test/edu-benefits/1990e/config/employmentHistory.unit.spec.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { expect } from 'chai';
+import ReactTestUtils from 'react-addons-test-utils';
+
+import { DefinitionTester } from '../../../util/schemaform-utils.jsx';
+import formConfig from '../../../../src/js/edu-benefits/1990e/config/form';
+
+describe('Edu 1990e employmentHistory', () => {
+  const { schema, uiSchema } = formConfig.chapters.employmentHistory.pages.employmentHistory;
+  const definitions = formConfig.defaultDefinitions;
+  it('should render', () => {
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          data={{}}
+          uiSchema={uiSchema}
+          definitions={definitions}/>
+    );
+
+    const inputs = ReactTestUtils.scryRenderedDOMComponentsWithTag(form, 'input');
+
+    expect(inputs.length).to.equal(1);
+  });
+});
