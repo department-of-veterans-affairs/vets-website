87d695cab6ad9b4d8cf09c7125fa7a440831cab7
akainic <Anne.Kainic@va.gov>
2017-03-13 11:18:11 -0400

    Remove unnecessary component

--- test/common/schemaform/definitions/nonMilitaryJobs.unit.spec.jsx
+++ test/common/schemaform/definitions/nonMilitaryJobs.unit.spec.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { findDOMNode } from 'react-dom';
 import { expect } from 'chai';
+import sinon from 'sinon';
 import ReactTestUtils from 'react-addons-test-utils';
 
 import { DefinitionTester } from '../../../util/schemaform-utils.jsx';
@@ -19,5 +20,26 @@ describe('Schemaform definition nonMilitaryJobs', () => {
     const inputs = formDOM.querySelectorAll('input');
 
     expect(inputs.length).to.equal(4);
+  });
+
+  xit('should add another', () => {
+    const onSubmit = sinon.spy();
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          onSubmit={onSubmit}
+          data={{}}
+          uiSchema={uiSchema}/>
+    );
+    const formDOM = findDOMNode(form);
+    ReactTestUtils.Simulate.change(formDOM.querySelector('input'), {
+      target: {
+        value: 'A job title'
+      }
+    });
+    ReactTestUtils.Simulate.click(formDOM.querySelector('.va-growable-add-btn'));
+
+    expect(formDOM.querySelector('.va-growable-background').textContent)
+      .to.contain('A job title');
   });
 });
