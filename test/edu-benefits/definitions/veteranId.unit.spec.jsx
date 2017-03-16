10ff6e511baef22a5d33d9204cb89412f2ec7218
raquelromano-gov <raquel_a_romano@omb.eop.gov>
2017-03-16 16:16:36 -0700

    Beef up unit test

--- test/edu-benefits/definitions/veteranId.unit.spec.jsx
+++ test/edu-benefits/definitions/veteranId.unit.spec.jsx
@@ -3,7 +3,7 @@ import { findDOMNode } from 'react-dom';
 import { expect } from 'chai';
 import ReactTestUtils from 'react-addons-test-utils';
 
-import { DefinitionTester } from '../../util/schemaform-utils.jsx';
+import { DefinitionTester, submitForm } from '../../../util/schemaform-utils.jsx';
 import * as formConfig from '../../../src/js/edu-benefits/definitions/veteranId';
 import definitions from 'vets-json-schema/dist/definitions.json';
 
@@ -21,5 +21,35 @@ describe('Edu veteranId', () => {
     const inputs = Array.from(formDOM.querySelectorAll('input, select'));
 
     expect(inputs.length).to.equal(2);
+  });
+
+  it('should conditionally require file number', () => {
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          formData={{}}
+          schema={schema}
+          data={{}}
+          uiSchema={uiSchema}/>
+    );
+
+    const formDOM = findDOMNode(form);
+    submitForm(form);
+
+    // error shown for empty ssn input; file number input is not visible
+    expect(formDOM.querySelector('.usa-input-error #root_veteranSocialSecurityNumber')).not.to.be.null;
+    expect(formDOM.querySelector('#root_vaFileNumber')).to.be.null;
+    const noSSNBox = ReactTestUtils.scryRenderedDOMComponentsWithTag(form, 'input')
+      .find(input => input.getAttribute('name') === 'root_view:noSSN');
+
+    ReactTestUtils.Simulate.change(noSSNBox,
+      {
+        target: {
+          checked: true
+        }
+      });
+
+    // no error shown for empty ssn input; empty file number input is visible with error
+    expect(formDOM.querySelector('.usa-input-error #root_veteranSocialSecurityNumber')).to.be.null;
+    expect(formDOM.querySelector('.usa-input-error #root_vaFileNumber')).not.to.be.null;
   });
 });
