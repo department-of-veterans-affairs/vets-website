839b42190118b29d3e36260a2e736dc636293e28
James Kassemi <jkassemi@gmail.com>
2017-03-16 10:26:14 -0600

    Fix spacing issue in script/build.js

--- script/build.js
+++ script/build.js
@@ -392,8 +392,8 @@ if (options.buildtype !== 'development') {
 
 smith.use(redirect({
   '/2015/11/11/why-we-are-designing-in-beta.html': '/2015/11/11/why-we-are-designing-in-beta/',
-  '/disability-benefits/apply-for-benefits/':'/disability-benefits/apply/',
-  '/education/apply-for-education-benefits/':'/education/apply/'
+  '/disability-benefits/apply-for-benefits/': '/disability-benefits/apply/',
+  '/education/apply-for-education-benefits/': '/education/apply/'
 }));
 
 /* eslint-disable no-console */
