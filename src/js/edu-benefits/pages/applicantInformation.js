4d66720acca5db4bdd7ebd38b528dfce35d96056
Christopher Valarida <cvalarida@gmail.com>
2017-03-17 14:01:45 -0700

    Account for year not passing pattern

--- src/js/edu-benefits/pages/applicantInformation.js
+++ src/js/edu-benefits/pages/applicantInformation.js
@@ -51,10 +51,13 @@ export default function applicantInformation(schema, options) {
       'ui:order': fields,
       relativeFullName: fullName.uiSchema,
       relativeSocialSecurityNumber: ssn.uiSchema,
-      relativeDateOfBirth: _.set(
-        'ui:errorMessages.futureDate',
-        'Please provide a valid date',
-        currentOrPastDate.uiSchema('Date of birth')
+      relativeDateOfBirth: _.assign(currentOrPastDate.uiSchema('Date of birth'),
+        {
+          'ui:errorMessages': {
+            pattern: 'Please provide a valid date',
+            futureDate: 'Please provide a valid date'
+          }
+        }
       ),
       gender: {
         'ui:widget': 'radio',
