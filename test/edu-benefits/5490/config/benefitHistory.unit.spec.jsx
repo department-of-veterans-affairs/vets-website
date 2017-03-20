d3504761698ec6d1a487746586ca5c253744e01f
Jeff Balboni <jeff@adhocteam.us>
2017-03-20 12:22:21 -0400

    Add back test files

---
+++ test/edu-benefits/5490/config/benefitHistory.unit.spec.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { findDOMNode } from 'react-dom';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import ReactTestUtils from 'react-addons-test-utils';
+
+import { DefinitionTester, submitForm } from '../../../util/schemaform-utils.jsx';
+import formConfig from '../../../../src/js/edu-benefits/5490/config/form';
+
+describe('Edu 5490 benefitHistory', () => {
+  const { schema, uiSchema } = formConfig.chapters.benefitSelection.pages.benefitHistory;
+  it('should render', () => {
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          formData={{}}
+          schema={schema}
+          data={{}}
+          definitions={formConfig.defaultDefinitions}
+          uiSchema={uiSchema}/>
+    );
+    expect(ReactTestUtils.scryRenderedDOMComponentsWithTag(form, 'input').length)
+      .to.equal(7);
+  });
+
+  it('should expand options conditionally', () => {
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          data={{}}
+          definitions={formConfig.defaultDefinitions}
+          uiSchema={uiSchema}/>
+    );
+    const formDOM = findDOMNode(form);
+
+    // Starts with 7 inputs (tested above)
+
+    // Expand both of the expandables
+    const inputs = Array.from(formDOM.querySelectorAll('input'));
+    ReactTestUtils.Simulate.change(inputs.find((i) => i.id === 'root_previousBenefits_view:ownServiceBenefits'), {
+      target: {
+        checked: true
+      }
+    });
+    ReactTestUtils.Simulate.change(inputs.find((i) => i.id === 'root_previousBenefits_view:claimedSponsorService'), {
+      target: {
+        checked: true
+      }
+    });
+
+    // Should expand to 16
+    expect(Array.from(formDOM.querySelectorAll('input,select')).length)
+      .to.equal(16);
+  });
+
+  // This test fails to produce an error message as expected
+  // I've tried submitting the form unmodified first then expanding the fields
+  //  and checking for the error message, but that doesn't work either.
+  // Also, I've tried passing data to the DefinitionTester to bypass modifying
+  //  the DOM before submitting, but that failed to render outright.
+  it.skip('should only have require fields conditionally', () => {
+    const onSubmit = sinon.spy();
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          onSubmit={onSubmit}
+          data={{}}
+          definitions={formConfig.defaultDefinitions}
+          uiSchema={uiSchema}/>
+    );
+    const formDOM = findDOMNode(form);
+
+    // Check the someone else's service box
+    const inputs = Array.from(formDOM.querySelectorAll('input'));
+    ReactTestUtils.Simulate.change(inputs.find((i) => i.id === 'root_previousBenefits_view:claimedSponsorService'), {
+      target: {
+        checked: true
+      }
+    });
+    // expect(ReactTestUtils.scryRenderedDOMComponentsWithTag(form, 'input').length)
+    //   .to.equal(7);
+    // Submit form -- should fail
+    submitForm(form);
+    expect(Array.from(formDOM.querySelectorAll('.usa-input-error'))).to.not.be.empty;
+
+
+    // Uncheck the box
+    ReactTestUtils.Simulate.change(inputs.find((i) => i.id === 'root_previousBenefits_view:claimedSponsorService'), {
+      target: {
+        checked: false
+      }
+    });
+    // expect(ReactTestUtils.scryRenderedDOMComponentsWithTag(form, 'input').length)
+    //   .to.equal(7);
+    // Submit form -- should succeed
+    submitForm(form);
+    expect(Array.from(formDOM.querySelectorAll('.usa-input-error'))).to.be.empty;
+
+    expect(onSubmit.called).to.be.true;
+  });
+});
---
+++ test/edu-benefits/5490/config/sponsorInformation.unit.spec.jsx
@@ -0,0 +1,54 @@
+// Could be renamed to militaryServiceContributions, but for consistency...
+
+import React from 'react';
+import { findDOMNode } from 'react-dom';
+import { expect } from 'chai';
+import ReactTestUtils from 'react-addons-test-utils';
+
+import { DefinitionTester } from '../../../util/schemaform-utils.jsx';
+import formConfig from '../../../../src/js/edu-benefits/5490/config/form';
+
+describe('Edu 5490 sponsorInformation', () => {
+  const { schema, uiSchema } = formConfig.chapters.sponsorInformation.pages.sponsorInformation;
+  it('should render', () => {
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          definitions={formConfig.defaultDefinitions}
+          data={{}}
+          uiSchema={uiSchema}/>
+    );
+
+    const formDOM = findDOMNode(form);
+
+    expect(formDOM.querySelectorAll('input,select').length).to.equal(11);
+  });
+
+  it('should conditionally show spouseInfo options', () => {
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          definitions={formConfig.defaultDefinitions}
+          data={{}}
+          formData={{ relationship: 'spouse' }}
+          uiSchema={uiSchema}/>
+    );
+
+    const formDOM = findDOMNode(form);
+    expect(formDOM.querySelectorAll('input,select').length).to.equal(15);
+
+    // The divorce input should be there now
+    expect(formDOM.querySelector('label[for=root_spouseInfo_divorcePending]')).to.not.be.null;
+    // But remarriage date shouldn't be yet
+    expect(formDOM.querySelector('label[for=root_spouseInfo_remarriageDate]')).to.be.null;
+
+    // Select remarried
+    ReactTestUtils.Simulate.change(formDOM.querySelector('#root_spouseInfo_remarriedYes'), {
+      target: {
+        checked: true
+      }
+    });
+    // Remarried date should now appear
+    expect(formDOM.querySelector('label[for=root_spouseInfo_remarriageDate]')).to.not.be.null;
+  });
+});
