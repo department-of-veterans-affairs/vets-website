cef3d6962b12f70db2085433080f233acf454a3f
Eugene Doan <eugene@adhocteam.us>
2017-03-20 14:26:08 -0400

    Removed unused helper function.

--- src/js/gi/utils/helpers.js
+++ src/js/gi/utils/helpers.js
@@ -1,13 +1,3 @@
-export function getCurrency(value) {
-  const isNumeric = (n) => (!Number.isNaN(parseFloat(n)));
-
-  const amount = value && value[0] === '$'
-               ? value.substring(1)
-               : value;
-
-  return isNumeric(amount) ? +amount : 0;
-}
-
 export function formatCurrency(value) {
   const str = Math.round(+value).toString();
   return `$${str.replace(/\d(?=(\d{3})+$)/g, '$&,')}`;
