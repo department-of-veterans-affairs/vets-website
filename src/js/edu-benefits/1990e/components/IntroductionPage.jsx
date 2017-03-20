9f764ee88258ba70e5afe2314b88f592e2b97254
raquelromano-gov <raquel_a_romano@omb.eop.gov>
2017-03-20 15:18:05 -0700

    Update 1990e intro heading

--- src/js/edu-benefits/1990e/components/IntroductionPage.jsx
+++ src/js/edu-benefits/1990e/components/IntroductionPage.jsx
@@ -15,7 +15,7 @@ class IntroductionPage extends React.Component {
   render() {
     return (
       <div className="schemaform-intro">
-        <FormTitle title="Manage Your Education Benefits"/>
+        <FormTitle title=" Apply for Education Benefits"/>
         <p>This application is equivalent to Form 22-1990e (Application for Family Member to Use Transferred Benefits).</p>
         <div className="process schemaform-process">
           <ol>
