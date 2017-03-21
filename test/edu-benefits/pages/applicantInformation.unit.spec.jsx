f942977fe3e8eeb98ce2c0e46c2c7608c6d074df
raquelromano-gov <raquel_a_romano@omb.eop.gov>
2017-03-21 13:59:58 -0700

    SSN/FN fixes to applicant/relative and veteran/sponsor for 1990e and 5490 (#5138)

--- test/edu-benefits/pages/applicantInformation.unit.spec.jsx
+++ test/edu-benefits/pages/applicantInformation.unit.spec.jsx
@@ -1,3 +1,5 @@
+import _ from 'lodash/fp';
+
 import React from 'react';
 import { findDOMNode } from 'react-dom';
 import { expect } from 'chai';
@@ -5,7 +7,7 @@ import sinon from 'sinon';
 import ReactTestUtils from 'react-addons-test-utils';
 
 import { DefinitionTester, submitForm } from '../../util/schemaform-utils.jsx';
-import formConfig from '../../../src/js/edu-benefits/pages/applicantInformation.js';
+import applicantInformation from '../../../src/js/edu-benefits/pages/applicantInformation.js';
 
 import fullSchema1990e from 'vets-json-schema/dist/transfer-benefits-schema.json';
 import fullSchema5490 from 'vets-json-schema/dist/dependents-benefits-schema.json';
@@ -48,8 +50,50 @@ function fillInformation(find) {
   });
 }
 
+describe('Basic applicantInformation', () => {
+  const applicantSchema = {
+    definitions: _.pick(['fullName', 'ssn', 'date', 'gender', 'relationship'],
+                        fullSchema1990e.definitions)
+  };
+  const { schema, uiSchema } = applicantInformation(applicantSchema);
+  it('should conditionally require SSN', () => {
+    const onSubmit = sinon.spy();
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          onSubmit={onSubmit}
+          data={{}}
+          uiSchema={uiSchema}/>
+    );
+
+    const formDOM = findDOMNode(form);
+    const find = formDOM.querySelector.bind(formDOM);
+
+    // Error appears when no SSN is provided
+    fillInformation(find);
+    ReactTestUtils.Simulate.change(find('#root_relativeSocialSecurityNumber'), {
+      target: {
+        value: null
+      }
+    });
+    submitForm(form);
+    expect(Array.from(formDOM.querySelectorAll('.usa-input-error'))).not.to.be.empty;
+
+    // Error disappears when no-SSN is checked
+    const noSSNBox = ReactTestUtils.scryRenderedDOMComponentsWithTag(form, 'input')
+                                   .find(input => input.id === 'root_view:noSSN');
+    ReactTestUtils.Simulate.change(noSSNBox,
+      {
+        target: {
+          checked: true
+        }
+      });
+    expect(Array.from(formDOM.querySelectorAll('.usa-input-error'))).to.be.empty;
+  });
+});
+
 describe('Edu 1990e applicantInformation', () => {
-  const { schema, uiSchema } = formConfig(fullSchema1990e);
+  const { schema, uiSchema } = applicantInformation(fullSchema1990e);
   it('should render', () => {
     const form = ReactTestUtils.renderIntoDocument(
       <DefinitionTester
@@ -59,7 +103,7 @@ describe('Edu 1990e applicantInformation', () => {
     );
     const formDOM = findDOMNode(form);
 
-    expect(formDOM.querySelectorAll('input, select').length).to.equal(12);
+    expect(formDOM.querySelectorAll('input, select').length).to.equal(13);
   });
   it('should show errors when required fields are empty', () => {
     const onSubmit = sinon.spy();
@@ -98,7 +142,7 @@ describe('Edu 1990e applicantInformation', () => {
 
 describe('Edu 5490 applicantInformation', () => {
   // Make sure these match the fields listed in the 5490
-  const { schema, uiSchema } = formConfig(fullSchema5490, [
+  const { schema, uiSchema } = applicantInformation(fullSchema5490, [
     'relativeFullName',
     'relativeSocialSecurityNumber',
     'relativeDateOfBirth',
@@ -113,7 +157,7 @@ describe('Edu 5490 applicantInformation', () => {
     );
     const formDOM = findDOMNode(form);
 
-    expect(formDOM.querySelectorAll('input, select').length).to.equal(12);
+    expect(formDOM.querySelectorAll('input, select').length).to.equal(13);
   });
   it('should show errors when required fields are empty', () => {
     const onSubmit = sinon.spy();
