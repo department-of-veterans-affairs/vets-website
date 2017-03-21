0b7a29fa5ba50908768ec9a90909761f59f3a343
Jeff Balboni <jeff@adhocteam.us>
2017-03-21 14:07:57 -0400

    remove error

--- src/js/edu-benefits/1995/components/IntroductionPage.jsx
+++ src/js/edu-benefits/1995/components/IntroductionPage.jsx
@@ -13,7 +13,6 @@ class IntroductionPage extends React.Component {
     this.props.router.push(this.props.route.pageList[1].path);
   }
   render() {
-    throw new Error('testing');
     return (
       <div className="schemaform-intro">
         <FormTitle title="Manage your education benefits"/>
