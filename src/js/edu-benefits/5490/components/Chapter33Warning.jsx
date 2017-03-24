6cca2184bba22415b202ad29104a77cd51d00157
Alex Taylor <alex@goldenmeanie.net>
2017-03-24 19:19:52 -0400

    Trying a different layout of disclaimers

--- src/js/edu-benefits/5490/components/Chapter33Warning.jsx
+++ src/js/edu-benefits/5490/components/Chapter33Warning.jsx
@@ -4,7 +4,7 @@ export default function Chapter33Warning() {
   return (
     <div className="usa-alert usa-alert-warning usa-content edu-warning-single-line">
       <div className="usa-alert-body">
-        If you apply for the Fry Scholarship you won’t be eligible for DEA.
+        By selecting this benefit I acknowledge that I understand this election is irrevocable and may not be changed.
       </div>
     </div>
   );
--- src/js/edu-benefits/5490/components/Chapter35Warning.jsx
+++ src/js/edu-benefits/5490/components/Chapter35Warning.jsx
@@ -4,7 +4,7 @@ export default function Chapter33Warning() {
   return (
     <div className="usa-alert usa-alert-warning usa-content edu-warning-single-line">
       <div className="usa-alert-body">
-        If you apply for DEA you won’t be eligible for the Fry Scholarship.
+        By selecting this benefit I acknowledge that I understand this election is irrevocable and may not be changed.
       </div>
     </div>
   );
