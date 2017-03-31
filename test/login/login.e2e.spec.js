1c0df6526cea87a336e3ea3cdf9b95f637243d05
omgitsbillryan <bill.ryan@adhocteam.us>
2017-04-01 05:12:36 +1030

    removing unneeded boilerplate

--- test/login/login.e2e.spec.js
+++ test/login/login.e2e.spec.js
@@ -2,34 +2,32 @@ const E2eHelpers = require('../util/e2e-helpers');
 const Timeouts = require('../util/timeouts.js');
 const LoginHelpers = require('../util/login-helpers');
 
-if (!process.env.BUILDTYPE || process.env.BUILDTYPE === 'development') {
-  module.exports = E2eHelpers.createE2eTest(
-    (client) => {
-      const token = LoginHelpers.getUserToken();
-      const logoutUrl = LoginHelpers.getLogoutUrl();
+module.exports = E2eHelpers.createE2eTest(
+  (client) => {
+    const token = LoginHelpers.getUserToken();
+    const logoutUrl = LoginHelpers.getLogoutUrl();
 
-      // log in and wait for little person icon to appear next to the username
-      LoginHelpers.logIn(token, client, '/', 3)
-        .assert.title('Vets.gov')
-        .waitForElementVisible('#login-root > div > div.sign-in-link > div > button > span > svg', Timeouts.slow);
+    // log in and wait for little person icon to appear next to the username
+    LoginHelpers.logIn(token, client, '/', 3)
+      .assert.title('Vets.gov')
+      .waitForElementVisible('#login-root > div > div.sign-in-link > div > button > span > svg', Timeouts.slow);
 
-      // click dropdown on username
-      client.click('#login-root > div > div.sign-in-link > div > button');
+    // click dropdown on username
+    client.click('#login-root > div > div.sign-in-link > div > button');
 
-      // ensure Sign Out button is there
-      client.expect.element('#accountMenu > ul > li:nth-child(2) > a').to.be.visible;
+    // ensure Sign Out button is there
+    client.expect.element('#accountMenu > ul > li:nth-child(2) > a').to.be.visible;
 
-      // click Sign Out & verify new window has correct logout url
-      client
-        .click('#accountMenu > ul > li:nth-child(2) > a')
-        .windowHandles(function windowHandlesCallback(result) {
-          this.verify.equal(result.value.length, 2, 'There should be 2 windows open');
-          const newWindow = result.value[1];
-          this.switchWindow(newWindow);
-          this.verify.urlContains(logoutUrl);
-        });
+    // click Sign Out & verify new window has correct logout url
+    client
+      .click('#accountMenu > ul > li:nth-child(2) > a')
+      .windowHandles(function windowHandlesCallback(result) {
+        this.verify.equal(result.value.length, 2, 'There should be 2 windows open');
+        const newWindow = result.value[1];
+        this.switchWindow(newWindow);
+        this.verify.urlContains(logoutUrl);
+      });
 
-      client.end();
-    }
-  );
-}
+    client.end();
+  }
+);
