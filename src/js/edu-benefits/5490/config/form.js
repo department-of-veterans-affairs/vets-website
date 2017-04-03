87b1ffc93646cdf087c4ca555cd5c6185fb4d7d3
Chris Valarida <cvalarida@gmail.com>
2017-04-03 09:05:19 -0700

    QI -> sponsor (#5226)

--- src/js/edu-benefits/5490/config/form.js
+++ src/js/edu-benefits/5490/config/form.js
@@ -414,7 +414,7 @@ const formConfig = {
               'ui:title': 'Branch of service'
             },
             currentlyActiveDuty: {
-              'ui:title': 'Is the qualifying individual on active duty?',
+              'ui:title': 'Is your sponsor on active duty?',
               'ui:widget': 'yesNo'
             },
             outstandingFelony: {
