e69b67b3162227d1a569a44ff445072ecb1526f4
Aubrey Holland <aubreyholland@gmail.com>
2017-04-04 15:35:39 -0400

    Revert "add hacks to see whats up"
    This reverts commit 6327230c7d246bdf11dcd3492beefcf6f035fdf0.

--- script/build.js
+++ script/build.js
@@ -144,36 +144,27 @@ smith.use(ignore(ignoreList));
 smith.use(filenames());
 
 smith.use((files, metalsmith, done) => {
-  /* eslint-disable no-console */
   /* eslint-disable no-var */
   var runningCount = 0;
-  console.log('DOING THIS THING!!!!!!!!!!!!!!!!!');
   /* eslint-enable no-var */
   Object.keys(files).forEach((filename) => {
     if (filename.match(/\.md$/) !== null) {
       runningCount += 1;
       const fullPath = `content/pages/${filename}`;
-      console.log(`GIT LOG FOR ${filename}`);
       git.log({ file: fullPath }, (err, log) => {
-        console.log(`RESULT FOR FILENAME ${filename} LENGTH ${log.all.length}`);
         if (log.all.length > 1 && files[filename]) {
-          console.log(`GOOD NEWS ${log.latest.date}`);
           /* eslint-disable no-param-reassign */
           files[filename].lastupdate = new Date(log.latest.date);
           /* eslint-enable no-param-reassign */
-        } else {
-          console.log('TERRIBLE NEWS');
         }
       }).then(() => {
         runningCount -= 1;
         if (runningCount === 0) {
-          console.log('DONE WITH THING');
           done();
         }
       });
     }
   });
-  /* eslint-disable no-console */
 });
 
 smith.use(define({
