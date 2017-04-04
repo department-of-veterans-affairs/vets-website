7b2e63b683db9d20943c4802e5406e1db53c53a1
Aubrey Holland <aubreyholland@gmail.com>
2017-04-03 20:44:38 -0400

    yet more hacking

--- script/build.js
+++ script/build.js
@@ -164,7 +164,14 @@ smith.use((files, metalsmith, done) => {
           /* eslint-disable no-param-reassign */
           files[filename].lastupdate = new Date(log.latest.date);
           /* eslint-enable no-param-reassign */
+        } else if (err) {
+          /* eslint-disable no-param-reassign */
+          files[filename].lastupdate = 'ERROR';
+          /* eslint-enable no-param-reassign */
         } else {
+          /* eslint-disable no-param-reassign */
+          files[filename].lastupdate = 'BAD NEWS';
+          /* eslint-enable no-param-reassign */
           console.log('TERRIBLE NEWS');
         }
       }).then(() => {
