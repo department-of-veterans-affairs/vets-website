ddec19c3c3e2ea20533a6b09d9aa1fd0c2742bc1
Robert Fairhead <robertfairhead@users.noreply.github.com>
2017-04-10 15:48:13 -0400

    No quotes

--- src/js/gi/components/search/KeywordSearch.jsx
+++ src/js/gi/components/search/KeywordSearch.jsx
@@ -42,7 +42,7 @@ export class KeywordSearch extends React.Component {
 
   handleSuggestionSelected(event, data) {
     window.dataLayer.push({
-      'event': 'gibct-autosuggest',
+      event: 'gibct-autosuggest',
       'gibct-autosuggest-value': data.suggestionValue,
     });
     this.props.onFilterChange('name', data.suggestionValue);
