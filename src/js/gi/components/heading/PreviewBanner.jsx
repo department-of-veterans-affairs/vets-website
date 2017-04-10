0ba98aa81093d4bdebbf2f4a5299622e4b9ee7f7
Eugene Doan <eugene@adhocteam.us>
2017-04-10 15:15:54 -0400

    Fixed and refactored preview banner markup and styling.

--- src/js/gi/components/heading/PreviewBanner.jsx
+++ src/js/gi/components/heading/PreviewBanner.jsx
@@ -10,17 +10,18 @@ class PreviewBanner extends React.Component {
 
     return (
       <div className="gi-preview-banner">
-        <div className="outer"/>
-        <div className="inner">
-          <h5>Preview draft</h5>
-          <p>
-            This is what the version of this data from {when} will look like.
-            <span className="actions">
-              <a onClick={this.props.onViewLiveVersion}>View live version</a>
-              &nbsp;&nbsp;|&nbsp;&nbsp;
-              <a href={this.props.toolUrl}>Go back to the data tool</a>
-            </span>
-          </p>
+        <div className="outer small-12 medium-12 large-9">
+          <div className="inner">
+            <h5>Preview draft</h5>
+            <p>
+              This is what the version of this data from {when} will look like.&nbsp;
+              <span className="actions">
+                <a onClick={this.props.onViewLiveVersion}>View live version</a>
+                &nbsp;&nbsp;|&nbsp;&nbsp;
+                <a href={this.props.toolUrl}>Go back to the data tool</a>
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     );
