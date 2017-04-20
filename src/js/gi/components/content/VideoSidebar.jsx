8ae63c65f55e0e3b79a7a4997a81236cdcf63802
Eugene Doan <eugene@adhocteam.us>
2017-04-19 20:11:36 -0400

    Used relative path for apply for benefits link on landing page.

--- src/js/gi/components/content/VideoSidebar.jsx
+++ src/js/gi/components/content/VideoSidebar.jsx
@@ -12,7 +12,7 @@ class VideoSidebar extends React.Component {
         <iframe width="100%" src="https://www.youtube.com/embed/Z1ttkv9oRI4"
             title="Know Before You Go" frameBorder="0" allowFullScreen></iframe>
         <h5>Ready to apply?</h5>
-        <a href="https://www.vets.gov/education/apply/" target="_blank">
+        <a href="/education/apply/" target="_blank">
           Apply for education benefits
         </a>
       </div>
