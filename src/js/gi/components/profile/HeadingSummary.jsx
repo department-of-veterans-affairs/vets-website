91178064484e8c0d9e21832bfe79cb09c4eff1eb
Eugene Doan <eugene@adhocteam.us>
2017-04-19 20:31:19 -0400

    Tweaked spacing of icons with info and additional resources section.

--- src/js/gi/components/profile/HeadingSummary.jsx
+++ src/js/gi/components/profile/HeadingSummary.jsx
@@ -5,7 +5,7 @@ import AlertBox from '../../../common/components/AlertBox';
 import { formatNumber } from '../../utils/helpers';
 
 const AdditionalResources = () => (
-  <div className="medium-4 small-12 column">
+  <div className="additional-resources medium-4 small-12 column">
     <h4 className="highlight">Additional Resources</h4>
     <p>
       <a href="http://www.benefits.va.gov/gibill/careerscope.asp" target="_blank">
@@ -27,7 +27,11 @@ const AdditionalResources = () => (
 
 const IconWithInfo = ({ icon, children, present }) => {
   if (!present) return null;
-  return <span><i className={`fa fa-${icon}`}/>&nbsp;{children}</span>;
+  return (
+    <p className="icon-with-info">
+      <i className={`fa fa-${icon}`}/>&nbsp;{children}
+    </p>
+  );
 };
 
 class HeadingSummary extends React.Component {
@@ -56,50 +60,38 @@ class HeadingSummary extends React.Component {
                 isVisible={!!it.cautionFlag}
                 status="warning"/>
           </div>
-          <p style={{ marginBottom: '1.5em' }}>
-            <strong>{formatNumber(it.studentCount)}</strong> GI Bill students
-            (<a onClick={this.props.onLearnMore}>Learn more</a>)
-          </p>
+          <div className="column">
+            <p>
+              <strong>{formatNumber(it.studentCount)}</strong> GI Bill students
+              (<a onClick={this.props.onLearnMore}>Learn more</a>)
+            </p>
+          </div>
           <div>
             <div className="small-12 medium-6 column">
-              <p>
-                <IconWithInfo icon="map-marker" present={it.city && it.country}>
-                  {it.city}, {it.state || it.country}
-                </IconWithInfo>
-              </p>
-              <p style={{ display: 'block' }}>
-                <IconWithInfo icon="globe" present={it.website}>
-                  <a href={it.website} target="_blank">{it.website}</a>
-                </IconWithInfo>
-              </p>
-              <p>
-                <IconWithInfo icon="calendar-o" present={it.type !== 'ojt' && it.highestDegree}>
-                  {_.isFinite(it.highestDegree) ? `${it.highestDegree} year` : it.highestDegree} program
-                </IconWithInfo>
-              </p>
+              <IconWithInfo icon="map-marker" present={it.city && it.country}>
+                {it.city}, {it.state || it.country}
+              </IconWithInfo>
+              <IconWithInfo icon="globe" present={it.website}>
+                <a href={it.website} target="_blank">{it.website}</a>
+              </IconWithInfo>
+              <IconWithInfo icon="calendar-o" present={it.type !== 'ojt' && it.highestDegree}>
+                {_.isFinite(it.highestDegree) ? `${it.highestDegree} year` : it.highestDegree} program
+              </IconWithInfo>
             </div>
             <div className="small-12 medium-6 column">
-              <p>
-                <IconWithInfo icon="briefcase" present={it.type === 'ojt'}>
-                  On-the-job training
-                </IconWithInfo>
-              </p>
-              <p>
-                <IconWithInfo icon="institution" present={it.type && it.type !== 'ojt'}>
-                  {_.capitalize(it.type)}&nbsp;
-                  {it.type === 'for profit' ? 'school' : 'institution'}
-                </IconWithInfo>
-              </p>
-              <p>
-                <IconWithInfo icon="map" present={it.localeType && it.type && it.type !== 'ojt'}>
-                  {_.capitalize(it.localeType)} locale
-                </IconWithInfo>
-              </p>
-              <p>
-                <IconWithInfo icon="group" present={it.type && it.type !== 'ojt'}>
-                  {schoolSize(it.undergradEnrollment)} size
-                </IconWithInfo>
-              </p>
+              <IconWithInfo icon="briefcase" present={it.type === 'ojt'}>
+                On-the-job training
+              </IconWithInfo>
+              <IconWithInfo icon="institution" present={it.type && it.type !== 'ojt'}>
+                {_.capitalize(it.type)}&nbsp;
+                {it.type === 'for profit' ? 'school' : 'institution'}
+              </IconWithInfo>
+              <IconWithInfo icon="map" present={it.localeType && it.type && it.type !== 'ojt'}>
+                {_.capitalize(it.localeType)} locale
+              </IconWithInfo>
+              <IconWithInfo icon="group" present={it.type && it.type !== 'ojt'}>
+                {schoolSize(it.undergradEnrollment)} size
+              </IconWithInfo>
             </div>
           </div>
         </div>
