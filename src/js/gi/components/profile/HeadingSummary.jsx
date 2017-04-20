bb29074a55588388ce5fd5d695674ddf7e0f9579
Eugene Doan <eugene@adhocteam.us>
2017-04-19 20:13:01 -0400

    Fixed widths of heading icon info columns.

--- src/js/gi/components/profile/HeadingSummary.jsx
+++ src/js/gi/components/profile/HeadingSummary.jsx
@@ -60,47 +60,48 @@ class HeadingSummary extends React.Component {
             <strong>{formatNumber(it.studentCount)}</strong> GI Bill students
             (<a onClick={this.props.onLearnMore}>Learn more</a>)
           </p>
-          <div className="small-12 medium-4 column">
-            <p>
-              <IconWithInfo icon="map-marker" present={it.city && it.country}>
-                {it.city}, {it.state || it.country}
-              </IconWithInfo>
-            </p>
-            <p style={{ display: 'block' }}>
-              <IconWithInfo icon="globe" present={it.website}>
-                <a href={it.website} target="_blank">{it.website}</a>
-              </IconWithInfo>
-            </p>
-            <p>
-              <IconWithInfo icon="calendar-o" present={it.type !== 'ojt' && it.highestDegree}>
-                {_.isFinite(it.highestDegree) ? `${it.highestDegree} year` : it.highestDegree} program
-              </IconWithInfo>
-            </p>
+          <div>
+            <div className="small-12 medium-6 column">
+              <p>
+                <IconWithInfo icon="map-marker" present={it.city && it.country}>
+                  {it.city}, {it.state || it.country}
+                </IconWithInfo>
+              </p>
+              <p style={{ display: 'block' }}>
+                <IconWithInfo icon="globe" present={it.website}>
+                  <a href={it.website} target="_blank">{it.website}</a>
+                </IconWithInfo>
+              </p>
+              <p>
+                <IconWithInfo icon="calendar-o" present={it.type !== 'ojt' && it.highestDegree}>
+                  {_.isFinite(it.highestDegree) ? `${it.highestDegree} year` : it.highestDegree} program
+                </IconWithInfo>
+              </p>
+            </div>
+            <div className="small-12 medium-6 column">
+              <p>
+                <IconWithInfo icon="briefcase" present={it.type === 'ojt'}>
+                  On-the-job training
+                </IconWithInfo>
+              </p>
+              <p>
+                <IconWithInfo icon="institution" present={it.type && it.type !== 'ojt'}>
+                  {_.capitalize(it.type)}&nbsp;
+                  {it.type === 'for profit' ? 'school' : 'institution'}
+                </IconWithInfo>
+              </p>
+              <p>
+                <IconWithInfo icon="map" present={it.localeType && it.type && it.type !== 'ojt'}>
+                  {_.capitalize(it.localeType)} locale
+                </IconWithInfo>
+              </p>
+              <p>
+                <IconWithInfo icon="group" present={it.type && it.type !== 'ojt'}>
+                  {schoolSize(it.undergradEnrollment)} size
+                </IconWithInfo>
+              </p>
+            </div>
           </div>
-          <div className="small-12 medium-4 column">
-            <p>
-              <IconWithInfo icon="briefcase" present={it.type === 'ojt'}>
-                On-the-job training
-              </IconWithInfo>
-            </p>
-            <p>
-              <IconWithInfo icon="institution" present={it.type && it.type !== 'ojt'}>
-                {_.capitalize(it.type)}&nbsp;
-                {it.type === 'for profit' ? 'school' : 'institution'}
-              </IconWithInfo>
-            </p>
-            <p>
-              <IconWithInfo icon="map" present={it.localeType && it.type && it.type !== 'ojt'}>
-                {_.capitalize(it.localeType)} locale
-              </IconWithInfo>
-            </p>
-            <p>
-              <IconWithInfo icon="group" present={it.type && it.type !== 'ojt'}>
-                {schoolSize(it.undergradEnrollment)} size
-              </IconWithInfo>
-            </p>
-          </div>
-          <div className="small-12 medium-4 column"></div>
         </div>
         <AdditionalResources/>
       </div>
