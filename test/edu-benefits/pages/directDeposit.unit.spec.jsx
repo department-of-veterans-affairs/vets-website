fd9a1d73579c8742ccea019d6ef13e3eb2457583
Jeff Balboni <jeff@adhocteam.us>
2017-04-24 17:34:25 -0400

    Update tests to set expected required fields

--- test/edu-benefits/pages/directDeposit.unit.spec.jsx
+++ test/edu-benefits/pages/directDeposit.unit.spec.jsx
@@ -9,7 +9,7 @@ import formConfig1990n from '../../../src/js/edu-benefits/1990n/config/form';
 import formConfig1990e from '../../../src/js/edu-benefits/1990e/config/form';
 import formConfig5490 from '../../../src/js/edu-benefits/5490/config/form';
 
-const pageTests = (page, isRequired = false) => {
+const pageTests = (page, requiredErrors = 0) => {
   const { schema, uiSchema } = page;
   it('should render', () => {
     const form = ReactTestUtils.renderIntoDocument(
@@ -24,40 +24,45 @@ const pageTests = (page, isRequired = false) => {
     expect(formDOM.querySelectorAll('input').length).to.equal(4);
   });
 
-  if (isRequired) {
-    it('should show required errors', () => {
-      const form = ReactTestUtils.renderIntoDocument(
-        <DefinitionTester
-            schema={schema}
-            data={{}}
-            uiSchema={uiSchema}/>
-      );
+  it('should show correct number of required errors', () => {
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          data={{}}
+          uiSchema={uiSchema}/>
+    );
 
-      const formDOM = findDOMNode(form);
-      submitForm(form);
+    const formDOM = findDOMNode(form);
+    submitForm(form);
 
-      expect(formDOM.querySelectorAll('.usa-input-error').length).to.equal(3);
-    });
-  } else {
-    it('should submit form', () => {
-      const onSubmit = sinon.spy();
-      const form = ReactTestUtils.renderIntoDocument(
-        <DefinitionTester
-            schema={schema}
-            data={{}}
-            onSubmit={onSubmit}
-            uiSchema={uiSchema}/>
-      );
+    expect(formDOM.querySelectorAll('.usa-input-error').length).to.equal(requiredErrors);
+  });
 
-      submitForm(form);
+  it('should submit form', () => {
+    const onSubmit = sinon.spy();
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          data={{}}
+          onSubmit={onSubmit}
+          uiSchema={uiSchema}/>
+    );
 
+    submitForm(form);
+
+    if (requiredErrors > 0) {
+      expect(onSubmit.called).to.be.false;
+    } else {
       expect(onSubmit.called).to.be.true;
-    });
-  }
+    }
+  });
 };
 
 describe('Edu directDepositChangePage', () => {
-  describe('1990e', () => pageTests(formConfig1990e.chapters.personalInformation.pages.directDeposit));
-  describe('1990n', () => pageTests(formConfig1990n.chapters.personalInformation.pages.directDeposit, true));
-  describe('5490', () => pageTests(formConfig5490.chapters.personalInformation.pages.directDeposit));
+  describe('1990e', () =>
+    pageTests(formConfig1990e.chapters.personalInformation.pages.directDeposit));
+  describe('1990n', () =>
+    pageTests(formConfig1990n.chapters.personalInformation.pages.directDeposit, 3));
+  describe('5490', () =>
+    pageTests(formConfig5490.chapters.personalInformation.pages.directDeposit));
 });
