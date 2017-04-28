7bdba84fa898e6af46a8c68f1d673d166837d8d8
Ben <bshyong@users.noreply.github.com>
2017-04-28 13:13:16 -0700

    Add unit tests for GIBCT containers (#5449)
    * add empty files
    
    * add task for gibct unit tests
    
    * wip basic tests for gibct containers
    
    * landing page test
    
    * gibillapp loading state test
    
    * clean up state code
    
    * add search page test
    
    * be consistent across containers for unit tests
    
    * test undecorated Modal container and fix typo
    
    * fix props typo

---
+++ test/gibct/containers/GiBillApp.unit.spec.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import SkinDeep from 'skin-deep';
+import { expect } from 'chai';
+import { createStore } from 'redux';
+
+import { GiBillApp } from '../../../src/js/gi/containers/GiBillApp';
+import reducer from '../../../src/js/gi/reducers';
+
+const defaultProps = createStore(reducer).getState();
+
+describe('<GiBillApp>', () => {
+  it('should render', () => {
+    const tree = SkinDeep.shallowRender(<GiBillApp {...defaultProps}/>);
+    const vdom = tree.getRenderOutput();
+    expect(vdom).to.not.be.undefined;
+  });
+
+  it('should render LoadingIndicator', () => {
+    const props = {
+      ...defaultProps,
+      constants: {
+        ...defaultProps.constants,
+        inProgress: true,
+      }
+    };
+    const tree = SkinDeep.shallowRender(<GiBillApp {...props}/>);
+    expect(tree.subTree('LoadingIndicator')).to.be.ok;
+  });
+});
