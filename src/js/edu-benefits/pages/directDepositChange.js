0ee83c89a167d5b60cfb841e4799f2e55c6b128b
Jeff Balboni <jeff@adhocteam.us>
2017-05-08 14:21:49 -0400

    Clean up

--- src/js/edu-benefits/pages/directDepositChange.js
+++ src/js/edu-benefits/pages/directDepositChange.js
@@ -7,6 +7,10 @@ import {
   directDepositWarning
 } from '../utils/helpers';
 
+function isStartUpdate(form) {
+  return _.get('bankAccountChange', form) === 'startUpdate';
+}
+
 export default function createDirectDepositChangePage(schema) {
   const { bankAccountChange, bankAccount } = schema.definitions;
   return {
@@ -24,16 +28,16 @@ export default function createDirectDepositChangePage(schema) {
       },
       bankAccount: _.merge(bankAccountUI, {
         'ui:options': {
-          hideIf: (formData) => formData.bankAccountChange !== 'startUpdate'
+          hideIf: formData => !isStartUpdate(formData)
         },
         accountType: {
-          'ui:required': (formData) => _.get('bankAccountChange', formData) === 'startUpdate'
+          'ui:required': isStartUpdate
         },
         accountNumber: {
-          'ui:required': (formData) => _.get('bankAccountChange', formData) === 'startUpdate'
+          'ui:required': isStartUpdate
         },
         routingNumber: {
-          'ui:required': (formData) => _.get('bankAccountChange', formData) === 'startUpdate'
+          'ui:required': isStartUpdate
         }
       }),
       'view:stopWarning': {
