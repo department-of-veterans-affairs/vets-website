b6c4341a06da73e10e6ae583da7ab7f96b226ae6
raquelromano-gov <raquel_a_romano@omb.eop.gov>
2017-05-09 08:34:10 -0700

    Separate actions for updating login and verify urls

--- src/js/common/actions/index.js
+++ src/js/common/actions/index.js
@@ -1,7 +1,15 @@
 export const LOG_OUT = 'LOG_OUT';
+export const UPDATE_VERIFY_URL = 'UPDATE_VERIFY_URL';
 
 export function logOut() {
   return {
     type: LOG_OUT
+  };
+}
+
+export function updateVerifyUrl(value) {
+  return {
+    type: UPDATE_VERIFY_URL,
+    value
   };
 }
--- src/js/login/actions/index.js
+++ src/js/login/actions/index.js
@@ -9,12 +9,9 @@ export function updateLoggedInStatus(value) {
   };
 }
 
-// Action creator for updating either the login URL (propertyPath == 'first')
-// or the verify URL for up-leveling from LOA1 to LOA3 (propertyPath == 'third')
-export function updateLogInUrl(propertyPath, value) {
+export function updateLogInUrl(value) {
   return {
     type: UPDATE_LOGIN_URL,
-    propertyPath,
     value
   };
 }
