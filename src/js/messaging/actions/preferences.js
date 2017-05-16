42d653f1863f81b1d6f7f75641c56e5db6dc6331
Eugene Doan <eugene@adhocteam.us>
2017-05-15 20:16:31 -0400

    Renamed variable.

--- src/js/messaging/actions/preferences.js
+++ src/js/messaging/actions/preferences.js
@@ -21,9 +21,9 @@ export function fetchPreferences() {
     apiRequest(
       baseUrl,
       null,
-      data => dispatch({
+      response => dispatch({
         type: SM_FETCH_PREFERENCES_SUCCESS,
-        preferences: data.data.attributes
+        preferences: response.data.attributes
       }),
       () => dispatch({ type: SM_FETCH_PREFERENCES_FAILURE })
     );
