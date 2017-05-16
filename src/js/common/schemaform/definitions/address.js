5b6e45b48ef9378f5d6b65cf477ccddb596303a4
Jeff Balboni <jeff@adhocteam.us>
2017-05-16 10:41:46 -0400

    Working reselect version

--- src/js/common/schemaform/definitions/address.js
+++ src/js/common/schemaform/definitions/address.js
@@ -1,4 +1,6 @@
 import _ from 'lodash/fp';
+import { createSelector } from 'reselect';
+
 import { countries, states } from '../../utils/options-for-select';
 import { validateAddress } from '../validation';
 
@@ -54,61 +56,71 @@ export function uiSchema(label = 'Address', useStreet3 = false) {
     fieldOrder = fieldOrder.filter(field => field !== 'street3');
   }
 
+  const addressChangeSelector = createSelector(
+    ({ formData, path }) => _.get(path.concat('country'), formData),
+    ({ formData, path }) => _.get(path.concat('city'), formData),
+    _.get('addressSchema'),
+    (currentCountry, city, addressSchema) => {
+      const schemaUpdate = { properties: addressSchema.properties };
+      const country = currentCountry || addressSchema.properties.country.default;
+      const isRequired = addressSchema.required.length > 0;
+
+      let stateList;
+      if (country === 'USA') {
+        stateList = usaStates;
+      } else if (country === 'CAN') {
+        stateList = canProvinces;
+      } else if (country === 'MEX') {
+        stateList = mexStates;
+      }
+
+      if (stateList) {
+        // We have a list and it's different, so we need to make schema updates
+        if (addressSchema.properties.state.enum !== stateList) {
+          schemaUpdate.properties = _.set('state.enum', stateList, schemaUpdate.properties);
+
+          // all the countries with state lists require the state field, so add that if necessary
+          if (isRequired && !addressSchema.required.some(field => field === 'state')) {
+            schemaUpdate.required = addressSchema.required.concat('state');
+          }
+        }
+      // We don't have a state list for the current country, but there's an enum in the schema
+      // so we need to update it
+      } else if (addressSchema.properties.state.enum) {
+        schemaUpdate.properties = _.unset('state.enum', schemaUpdate.properties);
+        if (isRequired) {
+          schemaUpdate.required = addressSchema.required.filter(field => field !== 'state');
+        }
+      }
+
+      // Canada has a different title than others, so set that when necessary
+      if (country === 'CAN' && addressSchema.properties.state.title !== 'Province') {
+        schemaUpdate.properties = _.set('state.title', 'Province', schemaUpdate.properties);
+      } else if (country !== 'CAN' && addressSchema.properties.state.title !== 'State') {
+        schemaUpdate.properties = _.set('state.title', 'State', schemaUpdate.properties);
+      }
+
+      // We constrain the state list when someone picks a city that's a military base
+      if (country === 'USA' && isMilitaryCity(city) && schemaUpdate.properties.state.enum !== militaryStates) {
+        schemaUpdate.properties = _.set('state.enum', militaryStates, schemaUpdate.properties);
+      }
+
+      return schemaUpdate;
+    }
+  );
+
   return {
     'ui:title': label,
-    // 'ui:field': 'address',
     'ui:validations': [
       validateAddress
     ],
     'ui:options': {
-      updateSchema: (formData, addressSchema, addressUiSchema, index, path) => {
-        const schemaUpdate = { properties: addressSchema.properties };
-        const country = _.get(path.concat('country'), formData) || addressSchema.properties.country.default;
-        const isRequired = addressSchema.required.length > 0;
-
-        let stateList;
-        if (country === 'USA') {
-          stateList = usaStates;
-        } else if (country === 'CAN') {
-          stateList = canProvinces;
-        } else if (country === 'MEX') {
-          stateList = mexStates;
-        }
-
-        if (stateList) {
-          // We have a list and it's different, so we need to make schema updates
-          if (addressSchema.properties.state.enum !== stateList) {
-            schemaUpdate.properties = _.set('state.enum', stateList, schemaUpdate.properties);
-
-            // all the countries with state lists require the state field, so add that if necessary
-            if (isRequired && !addressSchema.required.some(field => field === 'state')) {
-              schemaUpdate.required = addressSchema.required.concat('state');
-            }
-          }
-        // We don't have a state list for the current country, but there's an enum in the schema
-        // so we need to update it
-        } else if (addressSchema.properties.state.enum) {
-          schemaUpdate.properties = _.unset('state.enum', schemaUpdate.properties);
-          if (isRequired) {
-            schemaUpdate.required = addressSchema.required.filter(field => field !== 'state');
-          }
-        }
-
-        // Canada has a different title than others, so set that when necessary
-        if (country === 'CAN' && schema.title !== 'Province') {
-          schemaUpdate.properties = _.set('state.title', 'Province', schemaUpdate.properties);
-        } else if (country !== 'CAN' && schema.title !== 'State') {
-          schemaUpdate.properties = _.set('state.title', 'State', schemaUpdate.properties);
-        }
-
-        // We constrain the state list when someone picks a city that's a military base
-        const city = _.get(path.concat('city'), formData);
-        if (country === 'USA' && isMilitaryCity(city) && schemaUpdate.properties.state.enum !== militaryStates) {
-          schemaUpdate.properties = _.set('state.enum', militaryStates, schemaUpdate.properties);
-        }
-
-        return schemaUpdate;
-      }
+      updateSchema: (formData, addressSchema, addressUiSchema, index, path) =>
+        addressChangeSelector({
+          formData,
+          addressSchema,
+          path
+        })
     },
     'ui:order': fieldOrder,
     country: {
