130582d00410f20284caedca26d82bc4247c0156
Jeff Balboni <jeff@adhocteam.us>
2017-05-17 14:58:32 -0400

    Fix intro page

--- src/js/hca-rjsf/components/IntroductionPage.jsx
+++ src/js/hca-rjsf/components/IntroductionPage.jsx
@@ -16,9 +16,28 @@ class IntroductionPage extends React.Component {
     return (
       <div className="schemaform-intro">
         <FormTitle title="Apply online for health care with the 10-10ez"/>
-        <p>[PLACEHOLDER] Once you’ve figured out if you qualify, applying for VA health care benefits is easy. Find out how to apply.</p>
-        <div className="process schemaform-process">
-          <div className="small-6 medium-5 end columns">
+        <p>
+          Fill out this application with the most accurate information you have. The more accurate it is, the more likely you are to get a rapid response.
+        </p>
+        <p>
+          VA uses the information you submit to determine your eligibility and to provide you with the best service.
+        </p>
+        <p>
+          Federal law provides criminal penalties, including a fine and/or imprisonment for up to 5 years, for concealing a material fact or making a materially false statement. (See <a href="https://www.justice.gov/usam/criminal-resource-manual-903-false-statements-concealment-18-usc-1001" target="_blank">18 U.S.C. 1001</a>)
+        </p>
+        <div className="usa-alert usa-alert-info">
+          <div className="usa-alert-body">
+            <strong>You won’t be able to save your work or come back to finish.</strong> So before you start, it’s a good idea to gather information about your military and education history, and the school you want to attend.
+          </div>
+        </div>
+        <br/>
+        <div className="row progress-box progress-box-schemaform form-progress-buttons schemaform-buttons">
+          <div className="small-6 usa-width-five-twelfths medium-5 columns">
+            <a href="/healthcare/apply">
+              <button className="usa-button-outline">« Back</button>
+            </a>
+          </div>
+          <div className="small-6 usa-width-five-twelfths medium-5 end columns">
             <ProgressButton
                 onButtonClick={this.goForward}
                 buttonText="Continue"
@@ -27,7 +46,7 @@ class IntroductionPage extends React.Component {
           </div>
         </div>
         <div className="omb-info--container">
-          <OMBInfo resBurden={0} ombNumber="2900-0091" expDate="??/??/????"/>
+          <OMBInfo resBurden={30} ombNumber="2900-0091" expDate="05/31/2018"/>
         </div>
       </div>
     );
