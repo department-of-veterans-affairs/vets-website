14edd2a94b27c9179f57ada4bae9a85af3671f66
Claire Hsu <HsuZe2@Claires-MacBook-Pro.local>
2017-05-23 16:56:39 -0700

    Use event delegation and refine warning toggle conditions

--- src/js/edu-benefits/education-wizard.js
+++ src/js/edu-benefits/education-wizard.js
@@ -28,41 +28,39 @@
     const container = document.querySelector('.wizard-container');
     const button = container.querySelector('.wizard-button');
     const content = container.querySelector('.wizard-content');
-    const radios = container.querySelectorAll('input');
     const apply = container.querySelector('#apply-now-button');
     const warning = container.querySelector('#transfer-warning');
-    radios.forEach(radio => {
-      radio.addEventListener('change', () => {
-        const otherChoice = radio.dataset.alternate;
-        const otherNextQuestion = container.querySelector(`#${otherChoice}`).dataset.nextQuestion;
-        if (otherNextQuestion) {
-          const otherNextQuestionElement = container.querySelector(`[data-question=${otherNextQuestion}`);
-          closeStateAndCheckChild(otherNextQuestionElement, container);
+    content.addEventListener('change', (e) => {
+      const radio = e.target;
+      const otherChoice = radio.dataset.alternate;
+      const otherNextQuestion = container.querySelector(`#${otherChoice}`).dataset.nextQuestion;
+      if (otherNextQuestion) {
+        const otherNextQuestionElement = container.querySelector(`[data-question=${otherNextQuestion}`);
+        closeStateAndCheckChild(otherNextQuestionElement, container);
+      }
+      if (!radio.dataset.selectedForm && (apply.dataset.state === 'open')) {
+        closeState(apply);
+      }
+      if (radio.dataset.selectedForm) {
+        if (apply.dataset.state === 'closed') {
+          openState(apply);
         }
-        if (!radio.dataset.selectedForm && (apply.dataset.state === 'open')) {
-          closeState(apply);
+        if ((warning.dataset.state === 'open') && (!radio.id !== 'create-non-transfer')) {
+          closeState(warning);
         }
-        if (radio.dataset.selectedForm) {
-          if (apply.dataset.state === 'closed') {
-            openState(apply);
-          }
-          if (!radio.id !== 'create-non-transfer') {
-            closeState(warning);
-          }
-          if (radio.id === 'create-non-transfer') {
-            openState(warning);
-          }
-          apply.href = `/education/apply-for-education-benefits/application/${radio.dataset.selectedForm}/introduction`;
-          return false;
+        if ((warning.dataset.state === 'closed') && (radio.id === 'create-non-transfer')) {
+          openState(warning);
         }
-        if (radio.dataset.nextQuestion) {
-          const nextQuestion = radio.dataset.nextQuestion;
-          const nextQuestionElement = container.querySelector(`[data-question=${nextQuestion}]`);
-          openState(nextQuestionElement);
-          nextQuestionElement.querySelector('input').focus();
-        }
+        apply.href = `/education/apply-for-education-benefits/application/${radio.dataset.selectedForm}/introduction`;
         return false;
-      });
+      }
+      if (radio.dataset.nextQuestion) {
+        const nextQuestion = radio.dataset.nextQuestion;
+        const nextQuestionElement = container.querySelector(`[data-question=${nextQuestion}]`);
+        openState(nextQuestionElement);
+        nextQuestionElement.querySelector('input').focus();
+      }
+      return false;
     });
     button.addEventListener('click', () => {
       toggleClass(content, 'wizard-content-closed');
