ad3eb308934c23971814fad3b8ec8da760eb136d
Jeff Balboni <jeff@adhocteam.us>
2017-05-23 22:03:25 -0400

    WIP

--- src/js/pensions/config/form.js
+++ src/js/pensions/config/form.js
@@ -2,7 +2,7 @@
 
 // import fullSchemaPensions from 'vets-json-schema/dist/21-527-schema.json';
 
-import ArrayPage from '../../common/schemaform/ArrayPage';
+// import ArrayPage from '../../common/schemaform/ArrayPage';
 import { transform } from '../helpers';
 import IntroductionPage from '../components/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
@@ -31,27 +31,21 @@ const formConfig = {
         spouseNetWorth: {
           path: '/financial-disclosure/net-worth/spouse',
           title: 'Net worth',
+          // TODO: Update with spouse check
+          depends: () => true,
           initialData: {
           }
         },
-        householdDependentsNetWorth: {
-          path: '/financial-disclosure/net-worth/dependents/household/:index',
-          title: 'Net worth',
-          arrayPath: 'childrenInHousehold',
-          component: ArrayPage,
-          initialData: {
-            childrenInHousehold: [{}]
-          }
-        },
-        nonHouseholdDependentsNetWorth: {
-          path: '/financial-disclosure/net-worth/dependents/:index',
-          title: 'Net worth',
-          arrayPath: 'childrenNotInHousehold',
-          component: ArrayPage,
-          initialData: {
-            childrenNotInHousehold: [{}]
-          }
-        }
+        // dependentsNetWorth: {
+        //   path: '/financial-disclosure/net-worth/dependents/household/:index',
+        //   title: 'Net worth',
+        //   arrayPath: 'childrenInHousehold',
+        //   itemFilter: (item) => item.childNotInHousehold,
+        //   component: ArrayPage,
+        //   initialData: {
+        //     childrenInHousehold: [{}]
+        //   }
+        // }
       }
     }
   }
