e87127832d4769296c1c0dd6841a8c6d8b8ce036
Benjamin Shyong <ben.shyong@gmail.com>
2017-05-24 11:54:44 -0700

    clean up appointment info; resolves #1879

--- src/js/facility-locator/components/AppointmentInfo.jsx
+++ src/js/facility-locator/components/AppointmentInfo.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import pluralize from 'pluralize';
-
-import StatsBar from './StatsBar';
+import { isEmpty, compact } from 'lodash';
 
 export default class AppointmentInfo extends Component {
   render() {
@@ -17,6 +16,21 @@ export default class AppointmentInfo extends Component {
 
     const healthAccessAttrs = facility.attributes.access.health;
 
+    if (isEmpty(compact([
+      healthAccessAttrs.primary_care_wait_days,
+      healthAccessAttrs.specialty_care_wait_days,
+      healthAccessAttrs.mental_health_wait_days,
+    ]))) { return null; }
+
+    const renderStat = (label, value) => {
+      if (value) {
+        return (
+          <li>{label}: <strong>{pluralize('day', value.toFixed(0), true)}</strong></li>
+        );
+      }
+      return null;
+    };
+
     return (
       <div className="mb2">
         <h4 className="highlight">Appointments</h4>
@@ -24,18 +38,10 @@ export default class AppointmentInfo extends Component {
           <h4>New patient wait times</h4>
           <p>The average number of days a Veteran who hasn't been to this location has to wait for a non-urgent appointment</p>
           <ul>
-            <li>Primary care: <strong>{pluralize('day', healthAccessAttrs.primary_care_wait_days.toFixed(0), true)}</strong></li>
-            <li>Specialty care: <strong>{pluralize('day', healthAccessAttrs.specialty_care_wait_days.toFixed(0), true)}</strong></li>
-            <li>Mental health care: <strong>{pluralize('day', healthAccessAttrs.mental_health_wait_days.toFixed(0), true)}</strong></li>
+            {renderStat('Primary care', healthAccessAttrs.primary_care_wait_days)}
+            {renderStat('Specialty care', healthAccessAttrs.specialty_care_wait_days)}
+            {renderStat('Mental health care', healthAccessAttrs.mental_health_wait_days)}
           </ul>
-        </div>
-        <div className="mb2">
-          <h4>Timeliness of urgent appointments</h4>
-          <p>
-            % of Veterans who were referred to a specialist for urgent care and saw that specialist within 48 hours
-          </p>
-          <StatsBar percent={healthAccessAttrs.urgent_consult_percentage * 100} label="At this location"/>
-          <StatsBar percent={70} label="National VA average" color="grey"/>
         </div>
       </div>
     );
