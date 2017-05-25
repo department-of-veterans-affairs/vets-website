c82245f90cbbc4135dc8153de98c1183089c9417
Claire Hsu <HsuZe2@Claires-MacBook-Pro.local>
2017-05-25 13:21:55 -0700

    Replace references to original definitions

--- test/edu-benefits/definitions/personId.unit.spec.jsx
+++ test/edu-benefits/definitions/personId.unit.spec.jsx
@@ -5,7 +5,7 @@ import sinon from 'sinon';
 import ReactTestUtils from 'react-dom/test-utils';
 
 import { DefinitionTester, submitForm } from '../../util/schemaform-utils.jsx';
-import * as personId from '../../../src/js/edu-benefits/definitions/personId';
+import * as personId from '../../../src/common/schemaform/definitions/personId';
 import commonDefinitions from 'vets-json-schema/dist/definitions.json';
 
 describe('Edu personId', () => {
