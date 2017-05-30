a7a92c8420566ba9be6bb02930ae18b13fd43d7b
Jeff Balboni <jeff@adhocteam.us>
2017-05-30 13:14:20 -0400

    Update name field

--- src/js/pensions/components/FullNameField.jsx
+++ src/js/pensions/components/FullNameField.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function FullNameField({ formData }) {
   return (
     <div>
-      <strong>{formData.first} {formData.middle} {formData.last}</strong>
+      <strong>{formData.first} {formData.middle} {formData.last}{formData.suffix && `, ${formData.suffix}`}</strong>
     </div>
   );
 }
