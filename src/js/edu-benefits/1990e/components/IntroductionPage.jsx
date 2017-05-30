46008d5f445a6fed1adab84e058e8ec2272dcabd
Claire Hsu <HsuZe2@Claires-MacBook-Pro.local>
2017-05-30 10:07:56 -0700

    Boldface work cannot be saved alert text

--- src/js/edu-benefits/1990e/components/IntroductionPage.jsx
+++ src/js/edu-benefits/1990e/components/IntroductionPage.jsx
@@ -31,7 +31,7 @@ class IntroductionPage extends React.Component {
               </ul>
               <div className="usa-alert usa-alert-info">
                 <div className="usa-alert-body">
-                  <span>You won’t be able to save your work or come back to finish. So before you start, it’s a good idea to gather information about your education history and the school you want to attend.</span>
+                  <span><strong>You won’t be able to save your work or come back to finish. So before you start, it’s a good idea to gather information about your education history and the school you want to attend.</strong></span>
                 </div>
               </div>
               <p>We strongly encourage you to work with a trained professional who can help you pick the right program. <a href="/disability-benefits/apply/help/index.html">Find an accredited representative.</a> Or call 888-442-4551 (888-GI-BILL-1) from 8:00 a.m. to 7:00 p.m. (ET) to talk with an education specialist.</p>
