f256ad9d117d0120299c1cf6d865d0a1f7b14fe2
Patrick Vinograd <patrick@adhocteam.us>
2017-06-07 11:11:01 -0700

    Test fixes

--- test/common/components/RequiredTermsAcceptance.unit.spec.jsx
+++ test/common/components/RequiredTermsAcceptance.unit.spec.jsx
@@ -11,6 +11,7 @@ const defaultProps = {
   checkAcceptance: sinon.spy(),
   fetchLatestTerms: sinon.spy(),
   acceptTerms: sinon.spy(),
+  termsNeeded: true,
   terms: {
     acceptance: false,
   }
@@ -23,7 +24,6 @@ describe('<RequiredTermsAcceptanceView>', () => {
     );
     tree.getMountedInstance().componentWillMount();
 
-    expect(defaultProps.checkAcceptance.called).to.be.true;
     expect(defaultProps.fetchLatestTerms.called).to.be.true;
   });
 
@@ -44,6 +44,7 @@ describe('<RequiredTermsAcceptanceView>', () => {
   it('should properly render children if terms accepted', () => {
     const props = {
       ...defaultProps,
+      termsNeeded: false,
       terms: {
         acceptance: true,
       }
