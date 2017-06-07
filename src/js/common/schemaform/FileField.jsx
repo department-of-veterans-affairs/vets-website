ed2e7375e4df4f2c16dea093402f5234ac2e0c0b
Jeff Balboni <jeff@adhocteam.us>
2017-06-07 09:10:44 -0400

    WIP

--- src/js/common/schemaform/FileField.jsx
+++ src/js/common/schemaform/FileField.jsx
@@ -3,22 +3,71 @@ import React from 'react';
 import _ from 'lodash/fp';
 // import Scroll from 'react-scroll';
 import { displayFileSize } from '../../common/utils/helpers';
+import environment from '../../common/helpers/environment';
 
 export default class FileField extends React.Component {
-  uploadFile = () => {
+  onAddFile = (e) => {
     const files = this.props.formData || [];
     this.props.onChange(files.concat({
       uploading: true
     }));
-    setTimeout(() => {
-      const uploadingFiles = this.props.formData;
-      this.props.onChange(_.set(uploadingFiles.length - 1, {
-        uploading: false,
-        fileName: 'testing.txt',
-        fileSize: 32444,
-        confirmationNumber: '123123'
-      }, uploadingFiles));
-    }, 2000);
+    this.uploadFile(e.target.files[0])
+      .then(fileInfo => {
+        const uploadingFiles = this.props.formData;
+        this.props.onChange(_.set(uploadingFiles.length - 1, {
+          serverErrorMessage: null,
+          uploading: false,
+          fileName: fileInfo.fileName,
+          fileSize: fileInfo.fileSize,
+          confirmationNumber: fileInfo.confirmationNumber
+        }, uploadingFiles));
+      })
+      .catch(message => {
+        const uploadingFiles = this.props.formData;
+        this.props.onChange(_.set(uploadingFiles.length - 1, {
+          uploading: false,
+          serverErrorMessage: message
+        }, uploadingFiles));
+      });
+    // setTimeout(() => {
+    //   const uploadingFiles = this.props.formData;
+    //   this.props.onChange(_.set(uploadingFiles.length - 1, {
+    //     uploading: false,
+    //     fileName: 'testing.txt',
+    //     fileSize: 32444,
+    //     confirmationNumber: '123123'
+    //   }, uploadingFiles));
+    // }, 2000);
+  }
+
+  uploadFile = (files) => {
+    const uiSchema = this.props.uiSchema;
+
+    const payload = new FormData();
+    Array.from(files).forEach(file => {
+      payload.append('uploads[]', file);
+    });
+
+    return fetch(`${environment.API_URL}${uiSchema['ui:options'].endpoint}`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'X-Key-Inflection': 'camel'
+      },
+      body: payload
+    }).then(resp => {
+      if (resp.ok) {
+        return resp.json();
+      }
+
+      return Promise.reject(resp.statusText);
+    }, error => {
+      return Promise.reject(error.message);
+    });
+  }
+  removeFile = (index) => {
+    const newFileList = this.props.formData.filter((file, idx) => index !== idx);
+    this.props.onChange(newFileList);
   }
   render() {
     const {
@@ -41,8 +90,10 @@ export default class FileField extends React.Component {
           return (
             <ul key={index} className="schemaform-file-list">
               {file.uploading && 'Uploading file...'}
-              {!file.uploading &&
+              {file.confirmationNumber &&
                 <li>{file.fileName} ({displayFileSize(file.fileSize)}) <button className="usa-button usa-button-outline schemaform-file-remove-button" type="button">Remove</button></li>}
+              {file.serverErrorMessage &&
+                <li>Error: {file.serverErrorMessage} <button className="usa-button usa-button-outline schemaform-file-remove-button" type="button">Remove</button></li>}
             </ul>
           );
         })}
@@ -59,7 +110,7 @@ export default class FileField extends React.Component {
                 style={{ display: 'none' }}
                 id={idSchema.$id}
                 name={idSchema.$id}
-                onChange={this.uploadFile}/>
+                onChange={this.onAddFile}/>
             <p><strong>Allowed file types:</strong><br/>{uiOptions.fileTypes}</p>
             <p><strong>Maximum file size:</strong><br/>{displayFileSize(uiOptions.maxSize)}</p>
           </div>}
