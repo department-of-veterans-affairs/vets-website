e48fbd047a1956d59ddec51b5636c5b41051d745
Patrick Vinograd <patrick@adhocteam.us>
2017-06-07 09:38:03 -0700

    Beta activation endpoint

---
+++ src/js/health-beta/routes.jsx
@@ -0,0 +1,9 @@
+import HealthBetaEnrollment from './containers/HealthBetaEnrollment';
+
+const routes = {
+  path: '/health-beta',
+  component: HealthBetaEnrollment,
+};
+
+export default routes;
+
