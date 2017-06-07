f37288178b549e737c486b1a21f6e0f9ff71ced2
Benjamin Shyong <ben.shyong@gmail.com>
2017-06-07 16:33:32 -0700

    Fix terms model on user profile page

--- src/js/user-profile/components/AccountManagementSection.jsx
+++ src/js/user-profile/components/AccountManagementSection.jsx
@@ -3,14 +3,21 @@ import { connect } from 'react-redux';
 import AcceptTermsPrompt from '../../common/components/AcceptTermsPrompt';
 import Modal from '../../common/components/Modal';
 
-const modalContents = () => (
-  <AcceptTermsPrompt/>
-);
+import {
+  checkAcceptance,
+  fetchLatestTerms,
+  acceptTerms,
+} from '../actions';
 
 class AccountManagementSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = { modalOpen: false };
+  }
+
+  componentDidMount() {
+    this.props.checkAcceptance('mhvac');
+    this.props.fetchLatestTerms('mhvac');
   }
 
   openModal = () => {
@@ -19,6 +26,24 @@ class AccountManagementSection extends React.Component {
 
   closeModal = () => {
     this.setState({ modalOpen: false });
+  }
+
+  renderModalContents() {
+    const { terms } = this.props.profile;
+    // terms.terms is not a typo
+    if (terms.acceptance) {
+      return (
+        <div>
+          <h3>
+            You have already accepted the terms and conditions.
+          </h3>
+          <div>
+            <button type="submit" onClick={this.closeModal}>Ok</button>
+          </div>
+        </div>
+      );
+    }
+    return <AcceptTermsPrompt terms={terms.terms} onAccept={this.props.acceptTerms}/>;
   }
 
   render() {
@@ -33,7 +58,7 @@ class AccountManagementSection extends React.Component {
         </div>
         <Modal
             cssClass="va-modal-large"
-            contents={modalContents()}
+            contents={this.renderModalContents()}
             id="mhvac-modal"
             visible={this.state.modalOpen}
             onClose={() => this.closeModal()}/>
@@ -42,6 +67,12 @@ class AccountManagementSection extends React.Component {
   }
 }
 
+const mapDispatchToProps = {
+  checkAcceptance,
+  fetchLatestTerms,
+  acceptTerms,
+};
+
 // TODO: fill this out
 const mapStateToProps = (state) => {
   const userState = state.user;
@@ -49,5 +80,5 @@ const mapStateToProps = (state) => {
 };
 
 // TODO(awong): Remove the pure: false once we start using ImmutableJS.
-export default connect(mapStateToProps)(AccountManagementSection);
+export default connect(mapStateToProps, mapDispatchToProps)(AccountManagementSection);
 export { AccountManagementSection };
