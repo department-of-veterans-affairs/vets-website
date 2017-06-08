979b0f274ff04108dc9a54cb5ded784350a944d7
Jeff Balboni <jeff@adhocteam.us>
2017-06-08 10:14:05 -0400

    Add claimant information page

--- src/js/burials/config/form.js
+++ src/js/burials/config/form.js
@@ -1,10 +1,22 @@
-// import _ from 'lodash/fp';
+import _ from 'lodash/fp';
 
-// import fullSchemaHca from 'vets-json-schema/dist/10-10EZ-schema.json';
+import fullSchemaBurials from 'vets-json-schema/dist/21P-530-schema.json';
 
 // import { transform } from '../helpers';
 import IntroductionPage from '../components/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
+import { relationshipLabels } from '../labels.jsx';
+
+import fullNameUI from '../../common/schemaform/definitions/fullName';
+
+const {
+  relationship,
+  claimantFullName
+} = fullSchemaBurials.properties;
+
+const {
+  fullName
+} = fullSchemaBurials.definitions;
 
 const formConfig = {
   urlPrefix: '/',
@@ -12,11 +24,51 @@ const formConfig = {
   trackingPrefix: 'burials-',
   introduction: IntroductionPage,
   confirmation: ConfirmationPage,
+  disableSave: true,
   title: 'Apply for burial benefits',
   subTitle: 'Form 21P-530',
   defaultDefinitions: {
+    fullName
   },
-  chapters: {}
+  chapters: {
+    claimantInformation: {
+      title: 'Claimant Information',
+      pages: {
+        claimantInformation: {
+          title: 'Claimant information',
+          path: 'claimant-information',
+          uiSchema: {
+            claimantFullName: fullNameUI,
+            relationship: {
+              type: {
+                'ui:title': 'Relationship to the deceased veteran',
+                'ui:widget': 'radio',
+                'ui:options': {
+                  labels: relationshipLabels
+                }
+              },
+              other: {
+                'ui:title': 'Please specify',
+                'ui:required': (form) => _.get('relationship.type', form) === 'other',
+                'ui:options': {
+                  expandUnder: 'type',
+                  expandUnderCondition: 'other'
+                }
+              }
+            }
+          },
+          schema: {
+            type: 'object',
+            required: ['claimantFullName', 'relationship'],
+            properties: {
+              claimantFullName,
+              relationship
+            }
+          }
+        }
+      }
+    }
+  }
 };
 
 export default formConfig;
---
+++ src/js/burials/labels.jsx
@@ -0,0 +1,7 @@
+export const relationshipLabels = {
+  spouse: 'Spouse',
+  child: 'Child',
+  parent: 'Parent',
+  executor: 'Executor/Administrator of Estate',
+  other: 'Other'
+};
