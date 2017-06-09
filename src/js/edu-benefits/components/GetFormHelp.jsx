277b45f94c2c89789b419fbe0fd968ca1ce0e12c
Claire Hsu <HsuZe2@Claires-MacBook-Pro.local>
2017-06-09 14:08:55 -0700

    Provide education service question submission link

--- src/js/edu-benefits/components/GetFormHelp.jsx
+++ src/js/edu-benefits/components/GetFormHelp.jsx
@@ -8,7 +8,7 @@ function GetFormHelp() {
       <p className="phone-number">
         <a href="tel:+888-442-4551">888-442-4551</a><br/>
         Monday - Friday, 8:00 a.m. - 7:00 p.m. ET<br/>
-        TTY: <a href="tel:+1-800-829-4833">???-???-????</a>
+        <p><a href="https://gibill.custhelp.com/app/ask/">Submit a question to Education Service</a></p>
       </p>
     </div>
   );
