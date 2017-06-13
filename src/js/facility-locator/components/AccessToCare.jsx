bab520c91a52186e6a5847ee37bdcb5f527ac23b
Benjamin Shyong <ben.shyong@gmail.com>
2017-06-13 08:12:43 -0700

    content change for access data

--- src/js/facility-locator/components/AccessToCare.jsx
+++ src/js/facility-locator/components/AccessToCare.jsx
@@ -41,6 +41,7 @@ export default class AccessToCare extends Component {
       <div className="mb2">
         <h4 className="highlight">Veteran-reported Satisfaction Scores</h4>
         <div className="mb2">
+          <p>Veteran-reported satisfaction scores come from the Consumer Assessment of Health and Systems survey.</p>
           <p>Current as of <strong>{healthFeedbackAttrs.effectiveDateRange}</strong></p>
           <h4>Urgent care appointments</h4>
           <p>% of Veterans who say they usually or always get an appointment when they need care right away</p>
