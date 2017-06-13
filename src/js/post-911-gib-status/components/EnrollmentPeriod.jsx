c1651430c6081d3b1f318362669910fa80e5b4cf
raquelromano-gov <raquel_a_romano@omb.eop.gov>
2017-06-13 07:03:26 -0700

    Change history

--- src/js/post-911-gib-status/components/EnrollmentPeriod.jsx
+++ src/js/post-911-gib-status/components/EnrollmentPeriod.jsx
@@ -1,35 +1,99 @@
+import drop from 'lodash/fp/drop';
+
 import React from 'react';
 import PropTypes from 'prop-types';
+import Scroll from 'react-scroll';
 
 import InfoPair from './InfoPair';
 
+import { getScrollOptions } from '../../common/utils/helpers';
 import { formatDateShort } from '../utils/helpers';
 
-class EnrollmentPeriod extends React.Component {
-  render() {
-    const { enrollment } = this.props;
+const scroller = Scroll.scroller;
 
-    const fullTimeHours = <InfoPair label="On-campus Hours" value={enrollment.fullTimeHours}/>;
-    const distanceHours = <InfoPair label="Distance Hours" value={enrollment.distanceHours}/>;
-    const typeOfChange = enrollment.amendmentList && enrollment.amendmentList.length > 0 &&
-      <InfoPair label="Type of Change" value={enrollment.amendmentList[0].type}/>;
-    const changeEffectiveDate = enrollment.amendmentList && enrollment.amendmentList.length > 0 &&
-      <InfoPair label="Change Effective Date" value={enrollment.amendmentList[0].changeEffectiveDate}/>;
+class EnrollmentPeriod extends React.Component {
+  constructor() {
+    super();
+    this.scrollToEnrollment = this.scrollToEnrollment.bind(this);
+    this.toggleHistory = this.toggleHistory.bind(this);
+    this.state = { historyOpen: false };
+  }
+
+  scrollToEnrollment() {
+    const options = getScrollOptions({ delay: 2 });
+    scroller.scrollTo(`collapsible-${this.props.id}`, options);
+  }
+
+  toggleHistory() {
+    const willOpen = !this.state.historyOpen;
+    this.setState({ historyOpen: !this.state.historyOpen });
+    if (willOpen) {
+      this.scrollToEnrollment();
+    }
+  }
+
+  render() {
+    const { enrollment, id } = this.props;
+    const amendments = enrollment.amendments || [];
+    const firstAmendment = amendments.length > 0 && amendments[0];
+
+    // TODO: should fullTimeHours be used anywhere?
+    // const fullTimeHours = <InfoPair label="Full-time Hours" value={enrollment.fullTimeHours}/>;
+    const onCampusHours = <InfoPair label="On-campus Hours" value={enrollment.onCampusHours}/>;
+    const onlineHours = <InfoPair label="Online Hours" value={enrollment.onlineHours}/>;
+    const typeOfChange = firstAmendment &&
+      <InfoPair label="Type of Change" value={firstAmendment.type}/>;
+    const changeEffectiveDate = firstAmendment &&
+      <InfoPair label="Change Effective Date" value={formatDateShort(firstAmendment.changeEffectiveDate)}/>;
+
+    const moreAmendments = amendments.length > 0 && drop(1, amendments);
+    const changeHistory = moreAmendments.length > 0 && (
+      <div className="usa-accordion-bordered">
+        <ul className="usa-unstyled-list">
+          <li>
+            <div id={`collapsible-${id}`} className="usa-accordion-content">
+            {/* <div id={`collapsible-${id}`} className="accordion-header clearfix"> */}
+              <button
+                  className="usa-button-unstyled"
+                  aria-expanded={this.state.historyOpen ? 'true' : 'false'}
+                  aria-controls={`collapsible-${id}`}
+                  onClick={this.toggleHistory}>
+                See Change History
+              </button>
+            </div>
+           {this.state.historyOpen && moreAmendments.map((amendment, index) => {
+             return (
+               <span key={`amendment-${this.props.key}-${index}`}>
+                 <InfoPair label="On-campus Hours" value={amendment.onCampusHours}/>
+                 <InfoPair label="Online Hours" value={amendment.onlineHours}/>
+                 <InfoPair label="Type of Change" value={amendment.type}/>
+                 <InfoPair
+                     label="Change Effective Date"
+                     value={formatDateShort(amendment.changeEffectiveDate)}/>
+               </span>
+             );
+           })}
+          </li>
+        </ul>
+      </div>
+    );
 
     return (
       <div>
         <hr/>
         <h4>{formatDateShort(enrollment.beginDate)} to {formatDateShort(enrollment.endDate)} at {enrollment.facilityName} ({enrollment.facilityCode})</h4>
-        {fullTimeHours}
-        {distanceHours}
+        {onCampusHours}
+        {onlineHours}
         {typeOfChange}
         {changeEffectiveDate}
+        {changeHistory}
       </div>
     );
   }
 }
 
 EnrollmentPeriod.propTypes = {
+  id: PropTypes.string,
   enrollment: PropTypes.object
 };
 
