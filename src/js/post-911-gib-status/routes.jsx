7fa61f60389f001d04240e59845cc65890c82963
akainic <Anne.Kainic@va.gov>
2017-06-14 13:21:58 -0400

    remove comments

--- src/js/post-911-gib-status/routes.jsx
+++ src/js/post-911-gib-status/routes.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router';
 
-// import Post911GIBStatusApp from './containers/Post911GIBStatusApp';
 import PrintPage from './containers/PrintPage';
-
-// const routes = {
-//   path: '/',
-//   component: Post911GIBStatusApp
-// };
 
 const routes = [
   <Route
