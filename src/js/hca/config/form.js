971cb817af8e3c38e437c1b40c85d1c5a3c76b7a
Jeff Balboni <jeff@adhocteam.us>
2017-06-14 16:44:00 -0400

    Argh, dumb comments

--- src/js/hca/config/form.js
+++ src/js/hca/config/form.js
@@ -116,7 +116,7 @@ const formConfig = {
   trackingPrefix: 'hca-',
   formId: 'hca',
   version: 0,
-  // disableSave: true,
+  disableSave: true,
   transformForSubmit: transform,
   // TODO: When save in progress is released, change the intro page
   introduction: IntroductionPage,
