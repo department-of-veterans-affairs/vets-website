0ed402dab9d28a94cb1906f2def5a1804e4cc178
Jeff Balboni <jeff@adhocteam.us>
2017-06-14 23:51:51 -0400

    Add selection fields

--- src/js/burials/config/form.js
+++ src/js/burials/config/form.js
@@ -5,7 +5,8 @@ import fullSchemaBurials from 'vets-json-schema/dist/21P-530-schema.json';
 // import { transform } from '../helpers';
 import IntroductionPage from '../components/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
-import { relationshipLabels, locationOfDeathLabels } from '../labels.jsx';
+import { relationshipLabels, locationOfDeathLabels, allowanceLabels } from '../labels.jsx';
+import { validateBooleanGroup } from '../../common/schemaform/validation';
 
 import * as address from '../../common/schemaform/definitions/address';
 import fullNameUI from '../../common/schemaform/definitions/fullName';
@@ -141,6 +142,185 @@ const formConfig = {
               burialDate,
               deathDate,
               locationOfDeath
+            }
+          }
+        }
+      }
+    },
+    benefitsSelection: {
+      title: 'Benefits Selection',
+      pages: {
+        benefitsSelection: {
+          title: 'Benefits selection',
+          path: 'benefits/selection',
+          uiSchema: {
+            'view:claimedBenefits': {
+              'ui:title': 'What benefits are you claiming?',
+              burialAllowance: {
+                'ui:title': 'Burial Allowance'
+              },
+              plotAllowance: {
+                'ui:title': 'Plot or Interment Allowance'
+              },
+              transportation: {
+                'ui:title': 'Transportation Reimbursement'
+              },
+              amountIncurred: {
+                'ui:title': 'Amount incurred',
+                'ui:options': {
+                  expandUnder: 'transportation'
+                }
+              },
+              'ui:validations': [
+                validateBooleanGroup
+              ],
+              'ui:errorMessages': {
+                atLeastOne: 'Please choose at least one benefit'
+              },
+              'ui:options': {
+                showFieldLabel: true
+              }
+            }
+          },
+          schema: {
+            type: 'object',
+            required: ['view:claimedBenefits'],
+            properties: {
+              'view:claimedBenefits': {
+                type: 'object',
+                properties: {
+                  burialAllowance: {
+                    type: 'boolean'
+                  },
+                  plotAllowance: {
+                    type: 'boolean'
+                  },
+                  transportation: {
+                    type: 'boolean'
+                  },
+                  amountIncurred: {
+                    type: 'number'
+                  }
+                }
+              }
+            }
+          }
+        },
+        burialAllowance: {
+          title: 'Burial allowance',
+          path: 'benefits/burial-allowance',
+          depends: form => _.get('view:claimedBenefits.burialAllowance', form) === true,
+          uiSchema: {
+            'ui:title': 'Burial allowance',
+            burialAllowanceRequested: {
+              'ui:title': 'Type of burial allowance requested',
+              'ui:widget': 'radio',
+              'ui:options': {
+                labels: allowanceLabels
+              }
+            },
+            burialCost: {
+              'ui:title': 'Actual burial cost',
+              'ui:options': {
+                expandUnder: 'burialAllowanceRequested',
+                expandUnderCondition: 'vaMC'
+              }
+            },
+            previouslyReceiveAllowance: {
+              'ui:title': 'Did you previously recieve a VA burial allowance?',
+              'ui:widget': 'yesNo',
+              'ui:required': form => _.get('relationship.type', form) === 'spouse',
+              'ui:options': {
+                hideIf: form => _.get('relationship.type', form) !== 'spouse'
+              }
+            },
+            incurredExpenses: {
+              'ui:title': 'Did you incur expenses for the Veteran’s burial?',
+              'ui:widget': 'yesNo'
+            },
+            benefitsUnclaimedRemains: {
+              'ui:title': 'Are you seeking burial benefits for the unclaimed remains of a veteran?',
+              'ui:widget': 'yesNo',
+              'ui:required': form => _.get('relationship.type', form) === 'other',
+              'ui:options': {
+                hideIf: form => _.get('relationship.type', form) !== 'other'
+              }
+            }
+          },
+          schema: {
+            type: 'object',
+            required: ['burialAllowanceRequested'],
+            properties: {
+              burialAllowanceRequested: {
+                type: 'string',
+                'enum': [
+                  'nonService',
+                  'service',
+                  'vaMC'
+                ]
+              },
+              burialCost: {
+                type: 'number'
+              },
+              previouslyReceiveAllowance: {
+                type: 'boolean'
+              },
+              incurredExpenses: {
+                type: 'boolean'
+              },
+              benefitsUnclaimedRemains: {
+                type: 'boolean'
+              }
+            }
+          }
+        },
+        plotAllowance: {
+          title: 'Plot or Interment Allowance',
+          path: 'benefits/plot-allowance',
+          depends: form => _.get('view:claimedBenefits.plotAllowance', form) === true,
+          uiSchema: {
+            'ui:title': 'Plot or Interment Allowance',
+            placeOfRemains: {
+              'ui:title': 'Place of Burial or Location of Deceased Veteran’s Remains'
+            },
+            federalCemetary: {
+              'ui:title': 'Was the Veteran buried in a national cemetery, or one owned by the federal government?',
+              'ui:widget': 'yesNo'
+            },
+            stateCemetary: {
+              'ui:title': 'Was the Veteran buried in a state Veteran’s cemetery?',
+              'ui:widget': 'yesNo'
+            },
+            govtContributions: {
+              'ui:title': 'Did a federal/state government or the veterans employer contribute to the burial?',
+              'ui:widget': 'yesNo'
+            },
+            amountGovtContribution: {
+              'ui:title': 'Amount of government or employer contribution:',
+              'ui:options': {
+                expandUnder: 'govtContributions'
+              }
+            }
+          },
+          schema: {
+            type: 'object',
+            required: ['placeOfRemains', 'federalCemetary', 'stateCemetary', 'govtContributions'],
+            properties: {
+              placeOfRemains: {
+                type: 'string'
+              },
+              federalCemetary: {
+                type: 'boolean'
+              },
+              stateCemetary: {
+                type: 'boolean'
+              },
+              govtContributions: {
+                type: 'boolean'
+              },
+              amountGovtContribution: {
+                type: 'number'
+              }
             }
           }
         }
