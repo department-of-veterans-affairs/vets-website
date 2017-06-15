92aa67c9ed793653d8dd79be6e79fc0b0902766e
Benjamin Shyong <ben.shyong@gmail.com>
2017-06-14 17:35:44 -0700

    mock list item

--- src/js/disability-benefits/components/AppealListItem.jsx
+++ src/js/disability-benefits/components/AppealListItem.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import { Link } from 'react-router';
 import moment from 'moment';
 
-import { isClaimComplete, getAppealsStatusDescription } from '../utils/helpers';
+import { getAppealsStatusDescription } from '../utils/helpers';
+
+function nextAction(claim) {
+  const iconName = 'fa-exclamation-triangle';
+  const icon = claim ? <i className={`fa ${iconName}`}></i> : null;
+  return <p>{icon} Next action text</p>;
+}
 
 export default function AppealListItem({ claim }) {
-  const inProgress = !isClaimComplete(claim);
-
   const lastEvent = claim.attributes.events.slice(-1)[0];
   const firstEvent = claim.attributes.events[0];
 
@@ -16,15 +20,7 @@ export default function AppealListItem({ claim }) {
       <h4 className="claim-list-item-header">Compensation Appeal – Received {moment(firstEvent.date).format('MMMM D, YYYY')}</h4>
       <p className="status"><span className="claim-item-label">Status:</span> {getAppealsStatusDescription(lastEvent)}</p>
       <div className="communications">
-        {inProgress && claim.attributes.developmentLetterSent
-          ? <p><i className="fa fa-envelope"></i>We sent you a development letter</p>
-          : null}
-        {claim.attributes.decisionLetterSent
-          ? <p><i className="fa fa-envelope"></i>We sent you a decision letter</p>
-          : null}
-        {inProgress && claim.attributes.documentsNeeded
-          ? <p><i className="fa fa-exclamation-triangle"></i>Items need attention</p>
-          : null}
+        {nextAction(lastEvent)}
       </div>
       <p><span className="claim-item-label">Last update:</span> {moment(lastEvent.date).format('MMM D, YYYY')}</p>
     </Link>
