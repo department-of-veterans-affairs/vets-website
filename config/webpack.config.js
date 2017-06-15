e68f9dbb8949294449f703a76c79b02c982732f2
Claire Hsu <clairehsu86@gmail.com>
2017-06-15 09:15:00 -0700

    Remove references to foundation-sites and jQuery (#5725)

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -41,7 +41,6 @@ const configGenerator = (options) => {
   filesToBuild.vendor = [
     './src/js/common/polyfills',
     'history',
-    'jquery',
     'react',
     'react-dom',
     'react-redux',
@@ -169,8 +168,7 @@ const configGenerator = (options) => {
     },
     resolve: {
       alias: {
-        modernizr$: path.resolve(__dirname, './modernizrrc'),
-        jquery: 'jquery/src/jquery'
+        modernizr$: path.resolve(__dirname, './modernizrrc')
       },
       extensions: ['*', '.js', '.jsx']
     },
@@ -185,13 +183,6 @@ const configGenerator = (options) => {
           API_URL: process.env.API_URL ? JSON.stringify(process.env.API_URL) : null,
           BASE_URL: process.env.BASE_URL ? JSON.stringify(process.env.BASE_URL) : null,
         }
-      }),
-
-      // See http://stackoverflow.com/questions/28969861/managing-jquery-plugin-dependency-in-webpack
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery'
       }),
 
       new ExtractTextPlugin({
