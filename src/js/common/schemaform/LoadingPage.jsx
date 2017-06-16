776fb89e0b6c654512edd4f6ca4e6073f6a7181d
cvalarida <chris.valarida@adhocteam.us>
2017-06-16 15:01:44 -0700

    Add loading indicator

--- src/js/common/schemaform/LoadingPage.jsx
+++ src/js/common/schemaform/LoadingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { LOAD_STATUSES } from './save-load-actions';
+import LoadingIndicator from '../components/LoadingIndicator';
 
 class LoadingPage extends React.Component {
   render() {
@@ -16,7 +17,7 @@ class LoadingPage extends React.Component {
       case LOAD_STATUSES.success:
         content = 'oh goody! does this mean I win? no, because we should have redirected by now :('; break;
       default: // pending
-        content = 'spinner here'; break;
+        content = <LoadingIndicator message="Wait a moment while we retrieve your saved form."/>; break;
     }
 
     return (
