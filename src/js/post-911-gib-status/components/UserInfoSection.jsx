1e2b88944f93dfd4deb4a48aa9de613602fb2ad4
akainic <Anne.Kainic@va.gov>
2017-06-16 08:42:12 -0400

    Add first conditional case

--- src/js/post-911-gib-status/components/UserInfoSection.jsx
+++ src/js/post-911-gib-status/components/UserInfoSection.jsx
@@ -25,6 +25,42 @@ class UserInfoSection extends React.Component {
       );
     }
 
+    let entitlementInfo;
+    if (enrollmentData.percentageBenefit === 0 && enrollmentData.originalEntitlement === 0) {
+      entitlementInfo = (
+        <div>
+          <h4>When You Can Receive Benefits</h4>
+          <div className="usa-alert usa-alert-warning usa-content">
+            <div className="usa-alert-body">
+              <h2>Currently Disallowed</h2>
+              We have received your application and have determined that you are not currently eligible
+              for Post-9/11 GI Bill benefits. Additional service time could change this determination.
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      entitlementInfo = (
+        <div>
+          <div>
+            <h4>When You Can Receive Benefits</h4>
+            <div className="section-line">
+              You are eligible to receive benefits between <strong>{formatDateShort(enrollmentData.eligibilityDate)}</strong> and <strong>{formatDateShort(enrollmentData.delimitingDate)}</strong>.
+            </div>
+          </div>
+          <div>
+            <h4>Your Benefit Level</h4>
+            <div className="section-line">
+              You are eligible to receive benefits at a rate of <strong>{percentageBenefit}</strong>.
+            </div>
+          </div>
+          <InfoPair label="Total months received" value={enrollmentData.originalEntitlement}/>
+          <InfoPair label="Used" value={enrollmentData.usedEntitlement}/>
+          <InfoPair label="Remaining" value={enrollmentData.remainingEntitlement}/>
+        </div>
+      );
+    }
+
     return (
       <div>
         {currentAsOfAlert}
@@ -45,21 +81,7 @@ class UserInfoSection extends React.Component {
             label="Regional Processing Office"
             value={enrollmentData.regionalProcessingOffice}
             spacingClass="section-line"/>
-        <div>
-          <h4>When You Can Receive Benefits</h4>
-          <div className="section-line">
-            You are eligible to receive benefits between <strong>{formatDateShort(enrollmentData.eligibilityDate)}</strong> and <strong>{formatDateShort(enrollmentData.delimitingDate)}</strong>.
-          </div>
-        </div>
-        <div>
-          <h4>Your Benefit Level</h4>
-          <div className="section-line">
-            You are eligible to receive benefits at a rate of <strong>{percentageBenefit}</strong>.
-          </div>
-        </div>
-        <InfoPair label="Total months received" value={enrollmentData.originalEntitlement}/>
-        <InfoPair label="Used" value={enrollmentData.usedEntitlement}/>
-        <InfoPair label="Remaining" value={enrollmentData.remainingEntitlement}/>
+        {entitlementInfo}
       </div>
     );
   }
