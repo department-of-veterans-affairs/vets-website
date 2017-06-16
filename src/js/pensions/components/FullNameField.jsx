271ac8246a442441605a5db7325c6c86a87e35c7
Jeff Balboni <jeff@adhocteam.us>
2017-06-16 16:39:05 -0400

    Remove change

--- src/js/pensions/components/FullNameField.jsx
+++ src/js/pensions/components/FullNameField.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function FullNameField({ formData }) {
-  const { first, middle, last, suffix } = formData.fullName || formData;
+  const { first, middle, last, suffix } = formData;
   return (
     <div>
       <strong>{first} {middle && `${middle} `}{last}{suffix && `, ${suffix}`}</strong>
