bbe67be78becf2cefbf836c1bdf194e3eb07cbbc
omgitsbillryan <bill.ryan@adhocteam.us>
2017-06-20 04:25:05 +0930

    unit test for UserInfoSection component

--- test/post-911-gib-status/components/UserInfoSection.unit.spec.js
+++ test/post-911-gib-status/components/UserInfoSection.unit.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SkinDeep from 'skin-deep';
 import { expect } from 'chai';
+import _ from 'lodash';
 
 import UserInfoSection from '../../../src/js/post-911-gib-status/components/UserInfoSection.jsx';
 
@@ -8,6 +9,7 @@ const props = {
   enrollmentData: {
   }
 };
+const currentHeadingSelector = '#up-to-date-header';
 
 describe('<UserInfoSection>', () => {
   it('should render', () => {
@@ -15,5 +17,36 @@ describe('<UserInfoSection>', () => {
     const vdom = tree.getRenderOutput();
     expect(vdom).to.not.be.undefined;
   });
-  // maybe test print button does something
+
+  describe('props.showCurrentAsOfAlert=false', () => {
+    it('should omit the "current as of" date', () => {
+      const currentAsOfProps = _.merge({}, props, { showCurrentAsOfAlert: false });
+      const tree = SkinDeep.shallowRender(<UserInfoSection {...currentAsOfProps}/>);
+      expect(tree.subTree(currentHeadingSelector)).to.be.false;
+    });
+  });
+
+  describe('props.showCurrentAsOfAlert=null', () => {
+    it('should omit the "current as of" date', () => {
+      const tree = SkinDeep.shallowRender(<UserInfoSection {...props}/>);
+      expect(tree.subTree(currentHeadingSelector)).to.be.false;
+    });
+  });
+
+  describe('props.showCurrentAsOfAlert=true', () => {
+    it('should display the "current as of" date', () => {
+      const currentAsOfProps = _.merge({}, props, { showCurrentAsOfAlert: true });
+      const tree = SkinDeep.shallowRender(<UserInfoSection {...currentAsOfProps}/>);
+      expect(tree.subTree(currentHeadingSelector)).to.not.be.false;
+    });
+  });
+
+  describe('props.percentageBenefit=null', () => {
+    it('should display "unavilable"', () => {
+      const tree = SkinDeep.shallowRender(<UserInfoSection {...props}/>);
+      const benefitLevel = tree.subTree('#benefit-level');
+      expect(benefitLevel).to.not.be.false;
+      expect(benefitLevel.text()).to.contain('unavailable');
+    });
+  });
 });
