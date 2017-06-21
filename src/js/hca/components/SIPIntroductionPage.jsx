72599f128c9b83a507d2eb31492dd38a18887911
Jeff Balboni <jeff@adhocteam.us>
2017-06-21 09:48:19 -0400

    Update ez

--- src/js/hca/components/SIPIntroductionPage.jsx
+++ src/js/hca/components/SIPIntroductionPage.jsx
@@ -31,7 +31,7 @@ class IntroductionPage extends React.Component {
             <div style={{ paddingBottom: '8px' }}>Application status: <strong>In progress</strong></div>
             <br/>
             <div>Last saved on {moment(savedAt).format('MM/DD/YYYY [at] hh:mma')}</div>
-            <div>Complete the form before submitting to apply for health care with the 10-10ez.</div>
+            <div>Complete the form before submitting to apply for health care with the 10-10EZ.</div>
           </div>);
       }
     } else {
