2ecb3ace4759029b328e6fd5aeb413ba6d18bdd8
akainic <Anne.Kainic@va.gov>
2017-06-21 16:56:27 -0400

    Basic collapsible panels are working

--- src/js/va-letters/components/LetterList.jsx
+++ src/js/va-letters/components/LetterList.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import CollapsiblePanel from './CollapsiblePanel';
+
 class LetterList extends React.Component {
   render() {
     // Replace this with collapsible panels
-    const letterItems = (this.props.letters || []).map((letter) => {
+    const letterItems = (this.props.letters || []).map((letter, index) => {
       return (
-        <li key={letter.letterType}>
-          <a href="#">{letter.name}</a>
-        </li>
+        <CollapsiblePanel panelName={letter.name} key={`collapsiblePanel-${index}`}>
+          <li key={letter.letterType}>
+            <a href="#">{letter.name}</a>
+          </li>
+        </CollapsiblePanel>
       );
     });
     return (
       <div className="step-content">
+        <p>
+          To see what is included in each letter and how it can be used, expand the box
+          using the (+). Then, download the letter.
+        </p>
         {letterItems}
       </div>
     );
