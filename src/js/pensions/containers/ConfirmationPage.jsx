f2ef960fdd599a4c72aa71f1c8d2a422dabc2aa2
Jeff Balboni <jeff@adhocteam.us>
2017-06-22 16:16:45 -0400

    Remove unnecessary expanded state

--- src/js/pensions/containers/ConfirmationPage.jsx
+++ src/js/pensions/containers/ConfirmationPage.jsx
@@ -15,19 +15,9 @@ const scrollToTop = () => {
 };
 
 class ConfirmationPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isExpanded: false };
-  }
-
   componentDidMount() {
     focusElement('.pensions-page-title');
     scrollToTop();
-  }
-
-  toggleExpanded = (e) => {
-    e.preventDefault();
-    this.setState({ isExpanded: !this.state.isExpanded });
   }
 
   render() {
