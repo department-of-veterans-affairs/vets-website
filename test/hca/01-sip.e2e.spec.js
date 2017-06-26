198c9cb833521fba1504874eb302594eb4f516d2
Jeff Balboni <jeff@adhocteam.us>
2017-06-25 20:37:53 -0400

    WIP

---
+++ test/hca/01-sip.e2e.spec.js
@@ -0,0 +1,29 @@
+const E2eHelpers = require('../e2e/e2e-helpers');
+const Timeouts = require('../e2e/timeouts.js');
+const HcaHelpers = require('../e2e/hca-helpers.js');
+
+module.exports = E2eHelpers.createE2eTest(
+  (client) => {
+    HcaHelpers.initSaveInProgressMock();
+
+    // Ensure introduction page renders.
+    client
+      .url(`${E2eHelpers.baseUrl}/healthcare/apply/application`)
+      .waitForElementVisible('body', Timeouts.normal)
+      .assert.title('Apply for Health Care: Vets.gov')
+      .waitForElementVisible('.schemaform-title', Timeouts.slow)  // First render of React may be slow.
+      .click('.schemaform-buttons .usa-button-primary');
+
+    E2eHelpers.overrideVetsGovApi(client);
+    E2eHelpers.overrideSmoothScrolling(client);
+    E2eHelpers.expectNavigateAwayFrom(client, '/introduction');
+    client.assert.urlContains('/veteran-information/birth-information');
+
+    client
+      .click('.form-panel a')
+      .waitForElementVisible('.usa-alert', Timeouts.normal);
+
+    client.axeCheck('.main');
+
+    client.end();
+  });
