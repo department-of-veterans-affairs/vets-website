d8c06eaa060c8f3f323ab2f19b4deabef0a5d2b4
Claire Hsu <clairehsu86@gmail.com>
2017-06-26 06:54:34 -0700

    Make medical centers lower case and use currency styling for payment input fields (#5841)

--- src/js/pensions/config/form.js
+++ src/js/pensions/config/form.js
@@ -313,7 +313,10 @@ const formConfig = {
                 expandUnder: 'view:receivedSeverancePay'
               },
               amount: {
-                'ui:title': 'Amount'
+                'ui:title': 'Amount',
+                'ui:options': {
+                  classNames: 'schemaform-currency-input'
+                }
               },
               type: {
                 'ui:title': 'Pay Type',
@@ -367,7 +370,7 @@ const formConfig = {
             },
             // TODO: update schema with this field if stakeholders approve
             hasVisitedVAMC: {
-              'ui:title': 'Have you been treated at a VA Medical Center for the above disability?',
+              'ui:title': 'Have you been treated at a VA medical center for the above disability?',
               'ui:widget': 'yesNo'
             }
           },
@@ -421,7 +424,10 @@ const formConfig = {
                     'ui:title': 'How many days lost to disability'
                   },
                   annualEarnings: {
-                    'ui:title': 'Total annual earnings'
+                    'ui:title': 'Total annual earnings',
+                    'ui:options': {
+                      classNames: 'schemaform-currency-input'
+                    }
                   }
                 }
               }
@@ -654,6 +660,7 @@ const formConfig = {
               'ui:title': spouseContribution,
               'ui:required': form => form.liveWithSpouse === false,
               'ui:options': {
+                classNames: 'schemaform-currency-input',
                 expandUnder: 'liveWithSpouse',
                 expandUnderCondition: false
               }
@@ -831,7 +838,6 @@ const formConfig = {
               'view:hasDependents': {
                 type: 'boolean'
               },
-              // merge with definition, provide minItems: 1, and items.required: ['relationship', 'fullName']
               dependents: {
                 type: 'array',
                 minItems: 1,
