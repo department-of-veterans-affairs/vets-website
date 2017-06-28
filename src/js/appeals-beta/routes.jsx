3078ac741c8be08cc9e9b74f643e5a7af1cf9693
Benjamin Shyong <ben.shyong@gmail.com>
2017-06-28 16:50:03 -0700

    add appeals beta registration container

---
+++ src/js/appeals-beta/routes.jsx
@@ -0,0 +1,8 @@
+import AppealsBetaEnrollment from './containers/AppealsBetaEnrollment';
+
+const routes = {
+  path: '/appeals-beta',
+  component: AppealsBetaEnrollment,
+};
+
+export default routes;
