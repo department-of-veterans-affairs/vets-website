1f0f0dc59b3946a1ce70a12495ed0221ed9009b8
Jeff Balboni <jeff@adhocteam.us>
2017-06-28 13:42:41 -0400

    Move fields, extra validation

--- src/js/burials/validation.js
+++ src/js/burials/validation.js
@@ -1,8 +1,11 @@
 import moment from 'moment';
 
-export function validateBurialDate(errors, page) {
-  const { burialDate, deathDate } = page;
+export function validateBurialAndDeathDates(errors, page) {
+  const { burialDate, deathDate, veteranDateOfBirth } = page;
   if (burialDate && deathDate && moment(burialDate).isBefore(moment(deathDate))) {
     errors.burialDate.addError('Date of burial must be on or after date of death');
+  }
+  if (deathDate && veteranDateOfBirth && moment(deathDate).isBefore(moment(veteranDateOfBirth))) {
+    errors.deathDate.addError('Date of death must be after date of birth');
   }
 }
