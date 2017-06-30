ab3dc1000d6a394279fd48f1be664598d3908408
patrickvinograd <patrick@adhocteam.us>
2017-06-29 21:07:20 -0700

    Refactor health-beta to avoid RequiredLoginView conflict (#5924)

--- src/js/health-beta/routes.jsx
+++ src/js/health-beta/routes.jsx
@@ -1,8 +1,10 @@
 import HealthBetaEnrollment from './containers/HealthBetaEnrollment';
+import Main from './containers/Main';
 
 const routes = {
   path: '/health-beta',
   component: HealthBetaEnrollment,
+  indexRoute: { component: Main },
 };
 
 export default routes;
