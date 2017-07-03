24f8df326053cd3a47fd861afafcfaa8ae318983
akainic <Anne.Kainic@va.gov>
2017-07-03 11:40:46 -0400

    Make iterative updates except for no data case

--- src/js/post-911-gib-status/components/InfoPair.jsx
+++ src/js/post-911-gib-status/components/InfoPair.jsx
@@ -9,24 +9,41 @@ class InfoPair extends React.Component {
         `usa-grid-full ${this.props.spacingClass}`
         : 'usa-grid-full';
 
-    return (
-      this.props.value &&
-        <div className={gridRowClasses}>
-          <div className="usa-width-one-third">
-            <span><strong>{this.props.label}: </strong></span>
-          </div>
-          <div className="usa-width-one-third">
-            {this.props.value}
-          </div>
+    const row = (
+      <div className={gridRowClasses}>
+        <div className="usa-width-one-third">
+          <span><strong>{this.props.label}: </strong></span>
         </div>
-    || null);
+        <div className="usa-width-two-thirds">
+          {this.props.value}
+        </div>
+      </div>
+    );
+
+    let rowToDisplay;
+    // Some rows should display the value even if the value is 0,
+    // while other rows shouldn't display anything if the value is 0.
+    // If this prop is passed, the value should be displayed regardless
+    // of if it is 0 or not.
+    // If the prop is not passed, check first if the value exists before
+    // determining whether or not to display it.
+    if (this.props.displayIfZero) {
+      rowToDisplay = row;
+    } else {
+      rowToDisplay = this.props.value && row;
+    }
+
+    return (
+      rowToDisplay || null
+    );
   }
 }
 
 InfoPair.propTypes = {
   label: PropTypes.string.isRequired,
   value: PropTypes.any,
-  spacingClass: PropTypes.string
+  spacingClass: PropTypes.string,
+  displayIfZero: PropTypes.bool
 };
 
 export default InfoPair;
