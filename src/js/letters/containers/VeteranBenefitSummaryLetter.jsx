c105a729406a36440b690e7d062d41fc2e027773
raquelromano-gov <raquel_a_romano@omb.eop.gov>
2017-07-05 14:13:48 -0700

    Fix merge conflicts

--- src/js/letters/containers/VeteranBenefitSummaryLetter.jsx
+++ src/js/letters/containers/VeteranBenefitSummaryLetter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import _ from 'lodash';
+import _ from 'lodash/fp';
 
 import { updateBenefitSummaryOption } from '../actions/letters';
 import {
