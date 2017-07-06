bdb203f87dd0c2ff818e00be31441c42453b1d1f
akainic <Anne.Kainic@va.gov>
2017-07-06 10:03:56 -0400

    Make code review updates

--- src/js/letters/containers/Main.jsx
+++ src/js/letters/containers/Main.jsx
@@ -14,12 +14,25 @@ class Main extends React.Component {
   render() {
     let appContent;
 
-    if (this.props.lettersAvailable) {
+    if (this.props.lettersAvailability === 'available') {
       appContent = this.props.children;
-    } else if (this.props.lettersAvailable === undefined) {
-      appContent = <LoadingIndicator message="Loading your application..."/>;
-    } else {
-      appContent = <div>Warning message: need content for when fetch of data from vets-api fails</div>;
+    } else if (this.props.lettersAvailability === 'awaitingResponse') {
+      appContent = <LoadingIndicator message="Loading your letters..."/>;
+    } else if (this.props.lettersAvailability === 'unavailable') {
+      appContent = (
+        <div>
+          <div className="usa-alert usa-alert-error" role="alert">
+            <div className="usa-alert-body">
+              <h4 className="usa-alert-heading">Letters Unavailable</h4>
+              <p className="usa-alert-text">
+                We weren't able to retrieve your VA letters. Please call
+                1-855-574-7286 between Monday-Friday 8:00 a.m. - 8:00 p.m. (ET).
+              </p>
+            </div>
+          </div>
+          <br/>
+        </div>
+      );
     }
 
     return (
@@ -35,7 +48,7 @@ function mapStateToProps(state) {
   return {
     letters: letterState.letters,
     destination: letterState.destination,
-    lettersAvailable: letterState.lettersAvailable,
+    lettersAvailability: letterState.lettersAvailability,
     benefitSummaryOptions: {
       benefitInfo: letterState.benefitInfo,
       serviceInfo: letterState.serviceInfo
--- src/js/post-911-gib-status/containers/Main.jsx
+++ src/js/post-911-gib-status/containers/Main.jsx
@@ -13,12 +13,25 @@ class Main extends React.Component {
   render() {
     let appContent;
 
-    if (this.props.available) {
+    if (this.props.availability === 'available') {
       appContent = this.props.children;
-    } else if (this.props.available === undefined) {
-      appContent = <LoadingIndicator message="Loading your application..."/>;
-    } else {
-      appContent = <div>Warning message: need content for when fetch of data from vets-api fails</div>;
+    } else if (this.props.availability === 'awaitingResponse') {
+      appContent = <LoadingIndicator message="Loading your Post-9/11 GI Bill benefit information..."/>;
+    } else if (this.props.availability === 'unavailable') {
+      appContent = (
+        <div>
+          <div className="usa-alert usa-alert-error" role="alert">
+            <div className="usa-alert-body">
+              <h4 className="usa-alert-heading">Post-9/11 GI Bill Benefit Information Unavailable</h4>
+              <p className="usa-alert-text">
+                We weren't able to retrieve your Post-9/11 GI Bill benefit information. Please call
+                888-442-4551 (888-GI-BILL-1) from 8 a.m. to 7 p.m. (ET).
+              </p>
+            </div>
+          </div>
+          <br/>
+        </div>
+      );
     }
 
     return (
@@ -32,7 +45,7 @@ class Main extends React.Component {
 function mapStateToProps(state) {
   return {
     enrollmentData: state.post911GIBStatus.enrollmentData,
-    available: state.post911GIBStatus.available
+    availability: state.post911GIBStatus.availability
   };
 }
 
