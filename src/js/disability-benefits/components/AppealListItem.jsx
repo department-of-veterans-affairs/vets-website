be1a11790736c0b580837497e2ce304a3654781f
Ben <bshyong@users.noreply.github.com>
2017-07-06 09:51:04 -0700

    Various fixes for Appeals (#5969)
    * Fix appeals sorting; date label
    
    * content change for bva remand status
    
    * code formatting
    
    * pass tests

--- src/js/disability-benefits/components/AppealListItem.jsx
+++ src/js/disability-benefits/components/AppealListItem.jsx
@@ -18,12 +18,12 @@ export default function AppealListItem({ appeal }) {
   // always show merged event on top
   const events = _.orderBy(appeal.attributes.events, [e => e.type === 'merged', e => moment(e.date).unix()], ['desc', 'desc']);
   const lastEvent = events[0];
-  const previousHistory = events.slice(1);
   const firstEvent = events[events.length - 1];
+  const previousHistory = events.slice(1);
 
   return (
     <div className="claim-list-item-container">
-      <div className="last-changed-label"><strong>Last changed: </strong> {moment(firstEvent.date).format('MMM D, YYYY')}</div>
+      <div className="last-changed-label"><strong>Last changed: </strong> {moment(lastEvent.date).format('MMM D, YYYY')}</div>
       <Link className="claim-list-item" to={`appeals/${appeal.id}/status`}>
         <h4 className="claim-list-item-header">Compensation Appeal – Received {moment(firstEvent.date).format('MMM D, YYYY')}</h4>
         <p className="status"><span className="claim-item-label">Status:</span> {appealStatusDescriptions(lastEvent, previousHistory).status.title}</p>
