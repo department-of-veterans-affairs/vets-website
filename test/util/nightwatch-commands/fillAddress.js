d967f32668d4ce37fe7f19ab0bad52b5fda2849a
cvalarida <chris.valarida@adhocteam.us>
2017-07-06 10:56:18 -0700

    schemaformBaseName -> baseName

--- test/util/nightwatch-commands/fillAddress.js
+++ test/util/nightwatch-commands/fillAddress.js
@@ -1,14 +1,14 @@
 /**
  * Fills the address form elements.
  */
-exports.command = function fillAddress(schemaformBaseName, address) {
+exports.command = function fillAddress(baseName, address) {
   this
-    .selectDropdown(`${schemaformBaseName}_country`, address.country)
-    .fill(`input[name="${schemaformBaseName}_street"]`, address.street)
-    .fill(`input[name="${schemaformBaseName}_street2"]`, address.street2)
-    .fill(`input[name="${schemaformBaseName}_city"]`, address.city)
-    .selectDropdown(`${schemaformBaseName}_state`, address.state)
-    .fill(`input[name="${schemaformBaseName}_postalCode"]`, address.postalCode);
+    .selectDropdown(`${baseName}_country`, address.country)
+    .fill(`input[name="${baseName}_street"]`, address.street)
+    .fill(`input[name="${baseName}_street2"]`, address.street2)
+    .fill(`input[name="${baseName}_city"]`, address.city)
+    .selectDropdown(`${baseName}_state`, address.state)
+    .fill(`input[name="${baseName}_postalCode"]`, address.postalCode);
 
   return this;
 };
