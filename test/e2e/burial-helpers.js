19fb16157e7cfa6496962f4b63713eb5e3214757
cvalarida <chris.valarida@adhocteam.us>
2017-07-07 15:48:05 -0700

    Remove initSaveInProgressMock

--- test/e2e/burial-helpers.js
+++ test/e2e/burial-helpers.js
@@ -1,6 +1,5 @@
 const mock = require('./mock-helpers');
 const Timeouts = require('./timeouts.js');
-const LoginHelpers = require('./login-helpers.js');
 
 function completeClaimantInformation(client, data) {
   client
@@ -138,114 +137,6 @@ function initApplicationSubmitMock() {
   });
 }
 
-function initSaveInProgressMock(url, client) {
-  const token = LoginHelpers.getUserToken();
-
-  initApplicationSubmitMock();
-
-  /* eslint-disable camelcase */
-  mock(token, {
-    path: '/v0/sessions',
-    verb: 'delete',
-    value: {
-      logout_via_get: 'http://fake'
-    }
-  });
-
-  mock(token, {
-    path: '/v0/sessions/new',
-    verb: 'get',
-    value: {
-      logout_via_get: 'http://fake'
-    }
-  });
-
-  mock(token, {
-    path: '/v0/user',
-    verb: 'get',
-    value: {
-      data: {
-        attributes: {
-          profile: {
-            email: 'fake@fake.com',
-            loa: {
-              current: 3
-            },
-            first_name: 'Jane',
-            middle_name: '',
-            last_name: 'Doe',
-            gender: 'F',
-            birth_date: '1985-01-01',
-          },
-          in_progress_forms: [{
-            form: '1010ez'
-          }],
-          prefills_available: [],
-          services: ['facilities', 'hca', 'edu-benefits', 'evss-claims', 'user-profile', 'rx', 'messaging'],
-          va_profile: {
-            status: 'OK',
-            birth_date: '19511118',
-            family_name: 'Hunter',
-            gender: 'M',
-            given_names: ['Julio', 'E'],
-            active_status: 'active'
-          }
-        }
-      }
-    }
-  });
-
-  mock(token, {
-    path: '/v0/in_progress_forms/1010ez',
-    verb: 'get',
-    value: {
-      form_data: {
-        privacyAgreementAccepted: false,
-        veteranSocialSecurityNumber: '123445544',
-        veteranFullName: {
-        },
-        'view:placeOfBirth': {
-        },
-        'view:demographicCategories': {
-        },
-        isSpanishHispanicLatino: false,
-        veteranAddress: {
-          country: 'USA'
-        },
-        spouseFullName: {
-
-        },
-        isEssentialAcaCoverage: false,
-        'view:preferredFacility': {
-        },
-        'view:locator': {
-
-        }
-      },
-      metadata: {
-        version: 0,
-        returnUrl: '/veteran-information/birth-information',
-        savedAt: 1498588443698,
-        expires_at: 1503772443,
-        last_updated: 1498588443
-      }
-    }
-  });
-  mock(token, {
-    path: '/v0/in_progress_forms/21P-530',
-    verb: 'put',
-    value: {
-    }
-  });
-  /* eslint-enable camelcase */
-
-  client
-    .url(url)
-    .waitForElementVisible('body', Timeouts.normal);
-
-  LoginHelpers.setUserToken(token, client);
-}
-
 module.exports = {
   completeClaimantInformation,
   completeVeteranInformation,
@@ -257,6 +148,5 @@ module.exports = {
   completePlotAllowance,
   completeClaimantContactInformation,
 
-  initApplicationSubmitMock,
-  initSaveInProgressMock
+  initApplicationSubmitMock
 };
