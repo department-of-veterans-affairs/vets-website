470d32e3b6c5c5cd20299b8434835f366414699b
cvalarida <chris.valarida@adhocteam.us>
2017-07-10 12:16:52 -0700

    Test works

--- test/burials/schema/schema.unit.test.js
+++ test/burials/schema/schema.unit.test.js
@@ -3,27 +3,27 @@ import fs from 'fs';
 import path from 'path';
 import { Validator } from 'jsonschema';
 
-import { transform } from '../../../src/js/pensions/helpers';
-import formConfig from '../../../src/js/pensions/config/form';
-import fullSchema527ez from 'vets-json-schema/dist/21P-527EZ-schema.json';
+import { transform } from '../../../src/js/burials/helpers';
+import formConfig from '../../../src/js/burials/config/form';
+import fullSchema530 from 'vets-json-schema/dist/21P-530-schema.json';
 
-describe('527ez schema tests', () => {
+describe('530 schema tests', () => {
   const v = new Validator();
   const files = fs.readdirSync(__dirname);
   files
     .filter(file => file.endsWith('json'))
     .forEach((file) => {
       const contents = JSON.parse(fs.readFileSync(path.join(__dirname, file), 'utf8'));
-      const submitData = JSON.parse(transform(formConfig, contents)).pensionClaim.form;
+      const submitData = JSON.parse(transform(formConfig, contents)).burialClaim.form;
       // console.log('submitData:', JSON.stringify(JSON.parse(submitData), null, 2));
       it(`should validate ${file}`, () => {
         const result = v.validate(
           JSON.parse(submitData),
-          fullSchema527ez
+          fullSchema530
         );
 
         if (!result.valid) {
-          console.log(result.errors.map(e => e.message)); // eslint-disable-line
+          console.log(result.errors); // eslint-disable-line
         }
         expect(result.valid).to.be.true;
       });
