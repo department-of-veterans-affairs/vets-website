5e2982feae921cfe95ef8208c850a5724ba0dfd6
Chris Valarida <cvalarida@gmail.com>
2017-07-14 13:08:28 -0700

    Update labels on burial allowance page (#6001)

--- src/js/burials/labels.jsx
+++ src/js/burials/labels.jsx
@@ -14,8 +14,8 @@ export const locationOfDeathLabels = {
 };
 
 export const allowanceLabels = {
-  nonService: 'Non-service-connected death',
-  service: 'Service-connected death',
+  nonService: 'Non-service-connected death (for Veteran receiving a pension or disability compensation)',
+  service: 'Service-connected death (for Veteran death related to, or the result of, a service-connected disability)',
   vaMC: 'VA medical center death'
 };
 
