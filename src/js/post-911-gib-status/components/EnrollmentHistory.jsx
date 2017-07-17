03970d96cc1828c235fd4b9c76d6e98e552095ba
akainic <Anne.Kainic@va.gov>
2017-07-17 10:13:04 -0400

    Conditional logic in place

--- src/js/post-911-gib-status/components/EnrollmentHistory.jsx
+++ src/js/post-911-gib-status/components/EnrollmentHistory.jsx
@@ -7,7 +7,6 @@ class EnrollmentHistory extends React.Component {
   render() {
     const enrollmentData = this.props.enrollmentData || {};
     const enrollments = enrollmentData.enrollments || [];
-    const currentlyAllowed = enrollmentData.percentageBenefit !== 0 || enrollmentData.originalEntitlement !== 0;
 
     // TODO: find out when this warning should be shown.
     /*
@@ -22,28 +21,38 @@ class EnrollmentHistory extends React.Component {
     */
 
     // History explanation box
+    let historyExplanationBox;
     const benefitMaybePending =
       (enrollmentData.percentageBenefit > 0 || enrollmentData.originalEntitlement > 0) &&
       enrollmentData.usedEntitlement === 0;
     const historyMayLookIncorrect = enrollments.length > 0 || benefitMaybePending;
-    const noEnrollmentHistory = enrollments.length === 0 && enrollmentData.usedEntitlement > 0;
-    const historyHeading = noEnrollmentHistory ?
-                           (<h4>You don't have any enrollment history</h4>) :
-                           (<h4>Does something look wrong in your enrollment history?</h4>);
-    const historyExplanation = noEnrollmentHistory ?
-                               (<span>Your enrollment history may not be available if:</span>) :
-                               (<span>Certain enrollments may not be displayed in this history if:</span>);
-    const historyExplanationBox = (historyMayLookIncorrect || noEnrollmentHistory) && (
-      <div className="feature">
-        {historyHeading}
-        {historyExplanation}
-        <ul>
-          <li>Your school made a request to us that's still in process, or</li>
-          <li>You made a request to us that's still in process, or</li>
-          <li>You used or are using your benefit for flight, on-the-job, apprenticeship, or correspondence training</li>
-        </ul>
-      </div>
-    );
+    const noEnrollmentHistory = enrollmentData.usedEntitlement === 0;
+    const benefitUsedUp = enrollmentData.remainingEntitlement === 0;
+
+    if (noEnrollmentHistory) {
+      historyExplanationBox = (
+        <div className="feature">
+          <h4>You don't have any enrollment history</h4>
+          <span>Your enrollment history may not be available if:</span>
+          <ul>
+            <li>You or your school did not yet make a request to us, <strong>or</strong></li>
+            <li>You or your school made a request that's still in process</li>
+          </ul>
+        </div>
+      );
+    } else if (historyMayLookIncorrect || benefitUsedUp) {
+      historyExplanationBox = (
+        <div className="feature">
+          <h4>Does something look wrong in your enrollment history?</h4>
+          <span>Certain enrollments may not be displayed in this history if:</span>
+          <ul>
+            <li>Your school made a request to us that's still in process, <strong>or</strong></li>
+            <li>You made a request to us that's still in process, <strong>or</strong></li>
+            <li>You used or are using your benefit for flight, on-the-job, apprenticeship, or correspondence training</li>
+          </ul>
+        </div>
+      );
+    }
 
     // Render enrollment periods
     const enrollmentHistory = enrollments.map((enrollment, index) => {
@@ -57,7 +66,7 @@ class EnrollmentHistory extends React.Component {
     });
 
     let sectionContent;
-    if (currentlyAllowed) {
+    if (enrollmentData.veteranIsEligible) {
       sectionContent = (
         <div>
           <h3 className="section-header">Enrollment History</h3>
