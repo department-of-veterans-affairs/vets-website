31312dcc37a44b04e5ca6d41a94cac2649571251
Jeff Balboni <jeff@adhocteam.us>
2017-07-18 13:34:02 -0400

    Styling tweak

--- src/js/pensions/components/FinancialDisclosureDescription.jsx
+++ src/js/pensions/components/FinancialDisclosureDescription.jsx
@@ -23,24 +23,24 @@ export default function FinancialDisclosureDescription({ viewedPages, pageKeys }
     alertBody = (
       <div className="usa-alert-body">
         Does your spouse have any net worth, income, or expenses?
-        <p>
-          <button className="va-button-link" onClick={jumpToFirst}>Yes, jump to spouse</button>
-        </p>
+        <br/>
+        <br/>
+        <button className="va-button-link" onClick={jumpToFirst}>Yes, jump to spouse</button>
       </div>
     );
   } else if (unViewedPages.some(key => key.includes('dependents'))) {
     alertBody = (
       <div className="usa-alert-body">
         Do the dependents you added have any net worth, income, or expenses?
-        <p>
-          <button className="va-button-link" onClick={jumpToFirst}>Yes, jump to new dependents</button>
-        </p>
+        <br/>
+        <br/>
+        <button className="va-button-link" onClick={jumpToFirst}>Yes, jump to new dependents</button>
       </div>
     );
   }
 
   return (
-    <div className="usa-alert usa-alert-warning">
+    <div className="usa-alert usa-alert-warning pensions-disclosure-warning">
       {alertBody}
     </div>
   );
