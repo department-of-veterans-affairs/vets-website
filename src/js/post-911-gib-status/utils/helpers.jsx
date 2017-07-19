51879b6a86f9669d6a32a0a8b5533b8688e5c6dd
raquelromano-gov <raquel_a_romano@omb.eop.gov>
2017-07-18 23:45:18 -0700

    Step 2: changing add/delete to mv

--- src/js/post-911-gib-status/utils/helpers.jsx
+++ src/js/post-911-gib-status/utils/helpers.jsx
@@ -1,9 +1,4 @@
-/* eslint-disable camelcase */
 import React from 'react';
-
-import merge from 'lodash/fp/merge';
-
-import environment from '../../common/helpers/environment';
 import { formatDateLong } from '../../common/utils/helpers';
 
 export function formatPercent(percent) {
@@ -20,7 +15,8 @@ export function formatVAFileNumber(n) {
   const number = n || '';
   const lengthOfXString = number.length > 4 ? number.length - 4 : 0;
 
-  return number.replace(number.substring(0, lengthOfXString), `${'x'.repeat(lengthOfXString)}-`);
+  return number.replace(number.substring(0, lengthOfXString),
+                        `${'x'.repeat(lengthOfXString)}-`);
 }
 
 export function formatMonthDayFields(field) {
@@ -82,34 +78,52 @@ export function benefitEndDateExplanation(condition, delimitingDate) {
   }
 }
 
-function isJson(response) {
-  const contentType = response.headers.get('content-type');
-  return contentType && contentType.indexOf('application/json') !== -1;
+export function systemDownMessage() {
+  return (
+    <div className="row" id="systemDownMessage">
+      <div className="small-12 columns">
+        <div className="react-container">
+          <h3>Sorry, our system is temporarily down while we fix a few things. Please try again later.</h3>
+          <a href="/"><button>Go Back to Vets.gov</button></a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export function apiRequest(url, optionalSettings = {}, success, error) {
-  const requestUrl = `${environment.API_URL}${url}`;
+export function unableToFindRecordWarning() {
+  return (
+    <div id="recordNotFound">
+      <div className="small-12 columns">
+        <div className="react-container">
+          <h3>We weren't able to find your records.</h3>
+          <h4>Please call 855-574-7286 between Monday - Friday, 8:00 a.m. - 8:00 p.m. ET.</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-  const defaultSettings = {
-    method: 'GET',
-    headers: {
-      Authorization: `Token token=${sessionStorage.userToken}`,
-      'X-Key-Inflection': 'camel',
-    }
-  };
-
-  const settings = merge(defaultSettings, optionalSettings);
-
-  return fetch(requestUrl, settings)
-    .then((response) => {
-      if (!response.ok) {
-        // Refresh to show login view when requests are unauthorized.
-        if (response.status === 401) { return window.location.reload(); }
-        return Promise.reject(response);
-      } else if (isJson(response)) {
-        return response.json();
-      }
-      return Promise.resolve(response);
-    })
-    .then(success, error);
+export function noChapter33BenefitsWarning() {
+  // TODO: expand vertical spacing
+  return (
+    <div className="feature" id="noChapter33Benefits">
+      <h4>You don't have any Post-9/11 GI Bill Benefits</h4>
+      We may not have information about your Post-9/11 GI Bill benefit because
+      <ul>
+        <li>You haven't applied for Post-9/11 GI Bill benefits, <strong>or</strong></li>
+        <li>Your application for Post-9/11 GI Bill benefits is still in process, <strong>or</strong></li>
+        <li>You have education benefits through a different benefit or chapter.</li>
+      </ul>
+      Need to apply?
+      <ul>
+        <li><a href="">Check to make sure you qualify for Post-9/11 GI Bill benefits</a></li>
+        <li><a href="">Apply for benefits</a></li>
+      </ul>
+      Not sure if you have benefits through another program?
+      <ul>
+        <li><a href="" target="_blank">Check your benefit status through our Web Automated Verification of Enrollment (W.A.V.E.) system</a></li>
+      </ul>
+    </div>
+  );
 }
