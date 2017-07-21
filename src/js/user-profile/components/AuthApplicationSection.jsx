8432bb083080b29fb292aaa454c3afc111037eae
Rach <rroueche@users.noreply.github.com>
2017-07-21 15:59:24 -0400

    updated Check to Get

--- src/js/user-profile/components/AuthApplicationSection.jsx
+++ src/js/user-profile/components/AuthApplicationSection.jsx
@@ -17,7 +17,7 @@ class AuthApplicationSection extends React.Component {
           <p><a href="/health-care/messaging">Message your health care team</a></p>
           <p><a href="/health-care/health-records">Get your VA health records</a></p>
           <p><a href="/track-claims">Check your claim and appeal status</a></p>
-          <p><a href="/education/gi-bill/post-9-11/ch-33-benefit">Check your Post-9/11 GI Bill statement of benefits</a></p>
+          <p><a href="/education/gi-bill/post-9-11/ch-33-benefit">Get your Post-9/11 GI Bill statement of benefits</a></p>
         </div>
       );
     } else {
