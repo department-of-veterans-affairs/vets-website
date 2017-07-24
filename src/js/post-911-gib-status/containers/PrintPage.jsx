0c1a5943f9f1a9c53f36ef03c5f2986abb83a3a6
raquelromano-gov <raquel_a_romano@omb.eop.gov>
2017-07-24 08:44:10 -0700

    Add trademark symbol to print page

--- src/js/post-911-gib-status/containers/PrintPage.jsx
+++ src/js/post-911-gib-status/containers/PrintPage.jsx
@@ -18,9 +18,10 @@ class PrintPage extends React.Component {
             <img src="/img/design/logo/va-logo.png" alt="VA logo" width="300"/>
             <h1 className="section-header">Post-9/11 GI Bill Statement of Benefits</h1>
             <p>
-              The information in this letter is the Post-9/11 GI Bill Statement of Benefits
-              for the beneficiary listed below as of {todayFormatted}. Any pending or recent
-              changes to enrollment may affect remaining entitlement.
+              The information in this letter is the Post-9/11 GI Bill<sup>TM</sup>&nbsp;
+              Statement of Benefits for the beneficiary listed below as of
+              {todayFormatted}. Any pending or recent changes to enrollment may
+              affect remaining entitlement.
             </p>
             <UserInfoSection enrollmentData={enrollmentData}/>
           </div>
