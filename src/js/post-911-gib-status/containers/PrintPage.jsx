837d968833435aba0d457757caddf45b25a09e2b
raquelromano-gov <raquel_a_romano@omb.eop.gov>
2017-07-24 16:15:45 -0700

    Fix content/spacing

--- src/js/post-911-gib-status/containers/PrintPage.jsx
+++ src/js/post-911-gib-status/containers/PrintPage.jsx
@@ -12,17 +12,16 @@ class PrintPage extends React.Component {
     const todayFormatted = formatDateLong(new Date());
 
     return (
-        <div className="usa-width-two-thirds medium-8 columns gib-info">
-            <div className="print-status">
-                <div className="print-screen">
-                    <img src="/img/design/logo/va-logo.png" alt="VA logo" width="300"/>
-                    <h1 className="section-header">Post-9/11 GI Bill<sup>&reg;</sup> Statement of Benefits</h1>
-                    <p>
-                        The information in this letter is the Post-9/11 GI Bill Statement of Benefits for the beneficiary listed below as of {todayFormatted}. Any pending or recent changes to enrollment may affect remaining entitlement.
-                    </p>
-                    <UserInfoSection enrollmentData={enrollmentData}/>
-                </div>
-            </div>
+      <div className="usa-width-two-thirds medium-8 columns gib-info">
+        <div className="print-status">
+          <div className="print-screen">
+            <img src="/img/design/logo/va-logo.png" alt="VA logo" width="300"/>
+            <h1 className="section-header">Post-9/11 GI Bill<sup>&reg;</sup> Statement of Benefits</h1>
+            <p>
+              The information in this letter is the Post-9/11 GI Bill Statement of Benefits for the beneficiary listed below as of {todayFormatted}. Any pending or recent changes to enrollment may affect remaining entitlement.
+            </p>
+            <UserInfoSection enrollmentData={enrollmentData}/>
+          </div>
         </div>
     );
   }
