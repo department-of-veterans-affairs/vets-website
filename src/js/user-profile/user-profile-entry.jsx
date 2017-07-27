f2d40694d61082febe8960b496c7e85b23d8b991
Eugene Doan <eugene@adhocteam.us>
2017-07-26 21:04:37 -0400

    Removed artifacts from copying Rx entry file.

--- src/js/user-profile/user-profile-entry.jsx
+++ src/js/user-profile/user-profile-entry.jsx
@@ -11,27 +11,12 @@ import createCommonStore from '../common/store';
 import createLoginWidget from '../login/login-entry';
 
 require('../common');  // Bring in the common javascript.
-require('../../sass/rx/rx.scss');
 require('../../sass/user-profile.scss');
 
 const commonStore = createCommonStore();
 createLoginWidget(commonStore);
 
 function init() {
-  /*
-   * Invoked when the URL changes. A way to handle query
-   * string data.
-   *
-   * Plan is to make this trigger a sort when the query
-   * parameter is `sortby`.
-   */
-  const handleChangedURL = (event) => {
-    // Here so eslint doesn't tell us about an unused variable.
-    return event;
-  };
-  browserHistory.listen(handleChangedURL);
-  // End URL listening
-
   ReactDOM.render((
     <Provider store={commonStore}>
       <Router history={browserHistory} routes={routes}/>
