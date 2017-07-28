1ac59e66da5b1009337ceeb3ef6b8f63be2a9608
cvalarida <chris.valarida@adhocteam.us>
2017-07-28 14:36:37 -0700

    Add global event listener

--- src/js/common/polyfills.js
+++ src/js/common/polyfills.js
@@ -24,3 +24,13 @@ if (!Modernizr.fetch) {
 
 // This polyfill has its own test logic so no need to conditionally require.
 require('polyfill-function-prototype-bind');
+
+// Disable scrolling on input[type="number"] because it was changing the value unexpectedly
+// NOTE: This also just stops the user from scrolling when hovering over a number
+//  input that has focus. This isn't ideal, but until a solution is found to that,
+//  this is what we've got.
+document.addEventListener('mousewheel', (event) => {
+  if (event.target.type === 'number' && document.activeElement === event.target) {
+    event.preventDefault();
+  }
+});
