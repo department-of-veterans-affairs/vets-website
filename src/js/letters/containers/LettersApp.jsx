59b53507960d88a3f6d270c4996ac1e95a1c7c00
akainic <Anne.Kainic@va.gov>
2017-07-31 15:47:26 -0400

    Change basic error message content

--- src/js/letters/containers/LettersApp.jsx
+++ src/js/letters/containers/LettersApp.jsx
@@ -12,7 +12,8 @@ function AppContent({ children, isDataAvailable }) {
   if (unregistered) {
     view = (
       <h4>
-        To view and download your VA letters, you need to verify your identity (or whatever).
+        We weren't able to find information about your VA letters.
+        If you think you should be able to access this information, please call the Vets.gov Help Desk at 855-574-7286 (TTY: 800-829-4833). We're here Monday–Friday, 8:00 a.m.–8:00 p.m. (ET).
       </h4>
     );
   } else {
