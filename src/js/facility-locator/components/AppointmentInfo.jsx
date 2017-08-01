1162cc74f93d3d8dccaf0ddd9c5bbc1f68805a6e
Robert Fairhead <bob@adhocteam.us>
2017-08-01 13:04:03 -0500

    Adding an event for using the Show More for waittimes

--- src/js/facility-locator/components/AppointmentInfo.jsx
+++ src/js/facility-locator/components/AppointmentInfo.jsx
@@ -69,6 +69,7 @@ export default class AppointmentInfo extends Component {
       }
 
       const onClick = () => {
+        window.dataLayer.push({ event: 'fl-show-waittimes' });
         this.setState({
           [showHideKey]: !this.state[showHideKey],
         });
