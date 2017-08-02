9f7c83af58a890f9610cfdb291843e0a69e6b59d
Robert Fairhead <robertfairhead@users.noreply.github.com>
2017-08-02 16:30:49 -0500

    Removed overly fancy terms events naming scheme (#6037)
    * Removed overly fancy term naming scheme
    
    * Wrong quotes ¯\_(ツ)_/¯

--- src/js/common/components/RequiredTermsAcceptanceView.jsx
+++ src/js/common/components/RequiredTermsAcceptanceView.jsx
@@ -16,8 +16,7 @@ export class RequiredTermsAcceptanceView extends React.Component {
       this.props.fetchLatestTerms(this.props.termsName);
       window.scrollTo(0, 0);
     }
-    const app = window.location.pathname.split('/').pop();
-    window.dataLayer.push({ event: `terms-shown-${app}` });
+    window.dataLayer.push({ event: 'terms-shown' });
   }
 
   componentDidUpdate(prevProps) {
