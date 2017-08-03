0a559ba16a7bd8546d7273a50c4134784925f2ab
Sophia Dengo <sdengo@gmail.com>
2017-08-03 17:43:15 -0400

    gibct breadcrumb

--- src/js/gi/components/heading/Breadcrumbs.jsx
+++ src/js/gi/components/heading/Breadcrumbs.jsx
@@ -1,13 +1,28 @@
+import { Link, browserHistory } from 'react-router';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 class Breadcrumbs extends React.Component {
   render() {
+    const { pathname, query: { version } } = this.props.location;
+
     const crumbs = [
       <a href="/" key="home">Home</a>,
       <a href="/education" key="education">Education Benefits</a>,
       <a href="/education/gi-bill" key="gi-bill">GI Bill</a>,
     ];
+
+    if (pathname.match(/search|profile/)) {
+      const root = { pathname: '/', query: (version ? { version } : {}) };
+      crumbs.push(<Link to={root} key="main">GI Bill® Comparison Tool</Link>);
+    }
+
+    if (pathname.match(/profile/)) {
+      if (true) { // TODO: if got here by searching
+        crumbs.push(<a onClick={browserHistory.goBack} key="search-results">Search Results</a>);
+      }
+      crumbs.push(<span key="profile"><strong>{this.props.profileName || 'Profile'}</strong></span>);
+    }
 
     return (
       <nav className="va-nav-breadcrumbs">
