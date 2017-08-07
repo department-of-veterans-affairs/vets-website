3b2f02d17e47cae225992b6b10085f1f5bbe5d9a
Jeff Balboni <jeff@adhocteam.us>
2017-08-07 17:30:28 -0400

    fix some ie sauce issues

--- test/util/nightwatch-commands/selectDropdown.js
+++ test/util/nightwatch-commands/selectDropdown.js
@@ -1,21 +1,6 @@
 /**
- * Change select value and trigger change event programatically. This
- * is necessary because long select boxes tend to render offscreen,
- * causing Selenium to fail in unexpected ways.
  * The first parameter is the field name, not the whole selector.
  */
 exports.command = function selectDropdown(name, value) {
-  const select = `select[name='${name}']`;
-  this.execute((clientSelect, clientValue) => {
-    /* eslint-disable */
-    var element = document.querySelector(clientSelect);
-    var event = new Event('change', { bubbles: true });
-    element.value = clientValue;
-    element.dispatchEvent(event);
-    return element.value;
-    /* eslint-disable */
-  },
-  [select, value]);
-
-  return this;
+  this.click(`select[name='${name}'] option[value='${value}']`);
 };
