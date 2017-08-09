ff34880e8872a18b42785ca7966462d1888f5512
Jeff Balboni <jeff@adhocteam.us>
2017-08-09 15:50:34 -0400

    Fix dropdowns on IE

--- test/util/nightwatch-commands/selectDropdown.js
+++ test/util/nightwatch-commands/selectDropdown.js
@@ -2,5 +2,7 @@
  * The first parameter is the field name, not the whole selector.
  */
 exports.command = function selectDropdown(name, value) {
-  this.click(`select[name='${name}'] option[value='${value}']`);
+  this
+    .click(`select[name='${name}']`)
+    .click(`select[name='${name}'] option[value='${value}']`);
 };
