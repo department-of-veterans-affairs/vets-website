b28f17dbd32d750b7cf147b1a3da845bb378d433
cvalarida <chris.valarida@adhocteam.us>
2017-08-15 14:35:06 -0700

    Another test

--- test/edu-benefits/1990-rjsf/config/benefitsRelinquishment.unit.spec.jsx
+++ test/edu-benefits/1990-rjsf/config/benefitsRelinquishment.unit.spec.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { expect } from 'chai';
 import ReactTestUtils from 'react-dom/test-utils';
 import { getFormDOM } from '../../../util/schemaform-utils';
+import sinon from 'sinon';
+import _ from 'lodash/fp';
 
 import { DefinitionTester } from '../../../util/schemaform-utils.jsx';
 import formConfig from '../../../../src/js/edu-benefits/1990-rjsf/config/form.js';
 
 describe('Edu 1990 benefitsRelinquishment', () => {
-  const { schema, uiSchema } = formConfig.chapters.benefitsEligibility.pages.benefitsRelinquishment;
-  const defaultData = {
+  const { schema, uiSchema, initialData } = formConfig.chapters.benefitsEligibility.pages.benefitsRelinquishment;
+  const defaultData = _.merge(initialData, {
     // Not sure this is needed, strictly speaking, but this page _does_ depend on it
     'view:selectedBenefits': {
       chapter30: true
     }
-  };
+  });
 
   it('should render', () => {
     const form = ReactTestUtils.renderIntoDocument(
@@ -29,11 +31,13 @@ describe('Edu 1990 benefitsRelinquishment', () => {
   });
 
   it('should require benefitsRelinquished', () => {
+    const onSubmit = sinon.spy();
     const form = ReactTestUtils.renderIntoDocument(
       <DefinitionTester
           schema={schema}
           data={defaultData}
           uiSchema={uiSchema}
+          onSubmit={onSubmit}
           definitions={formConfig.defaultDefinitions}/>
     );
     const formDOM = getFormDOM(form);
@@ -41,6 +45,7 @@ describe('Edu 1990 benefitsRelinquishment', () => {
     // Submit with no information and expect an error message
     formDOM.submitForm();
     expect(formDOM.querySelectorAll('.usa-input-error').length).to.equal(1);
+    expect(onSubmit.called).to.be.false;
 
     // Fill in the information
     formDOM.selectRadio('benefitsRelinquished', 'unknown');
@@ -48,6 +53,7 @@ describe('Edu 1990 benefitsRelinquishment', () => {
     // Submit and the message should be gone
     formDOM.submitForm();
     expect(formDOM.querySelectorAll('.usa-input-error').length).to.equal(0);
+    expect(onSubmit.called).to.be.true;
   });
 
   it('should show nested content', () => {
@@ -76,10 +82,28 @@ describe('Edu 1990 benefitsRelinquishment', () => {
     const formDOM = getFormDOM(form);
 
     formDOM.selectRadio('benefitsRelinquished', 'chapter1607');
-    // Input an invalid date to see if it validates
+    // Input an invalid date since it defaults to today
     formDOM.fillData('#root_view\\:benefitsRelinquishedContainer_benefitsRelinquishedDateYear', '100');
     formDOM.submitForm();
 
     expect(formDOM.querySelectorAll('.usa-input-error').length).to.equal(1);
+  });
+
+  it('should submit with benefitsRelinquishedDate', () => {
+    const onSubmit = sinon.spy();
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          data={defaultData}
+          uiSchema={uiSchema}
+          onSubmit={onSubmit}
+          definitions={formConfig.defaultDefinitions}/>
+    );
+    const formDOM = getFormDOM(form);
+
+    formDOM.selectRadio('benefitsRelinquished', 'chapter1607');
+    formDOM.submitForm();
+
+    expect(onSubmit.called).to.be.true;
   });
 });
