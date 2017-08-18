bff2572a9b20ddde97614b7e053cef4989207efa
akainic <Anne.Kainic@va.gov>
2017-08-18 15:22:42 -0400

    Fix failing test and add ebenefits link

--- test/letters/utils/helpers.unit.spec.jsx
+++ test/letters/utils/helpers.unit.spec.jsx
@@ -30,7 +30,7 @@ describe('Letters helpers: ', () => {
       _.forEach(option => {
         expect(getBenefitOptionText(option, true, true)).not.to.be.undefined;
         expect(getBenefitOptionText(option, false, true)).to.be.undefined;
-      }, ['hasNonServiceConnectedPension', 'hasAdaptedHousing', 'hasIndividualUnemployabilityGranted', 'hasSpecialMonthlyCompensation']);
+      }, ['hasAdaptedHousing', 'hasIndividualUnemployabilityGranted', 'hasSpecialMonthlyCompensation']);
     });
     it('should only be defined for dependents if value is true', () => {
       _.forEach(option => {
@@ -50,7 +50,7 @@ describe('Letters helpers: ', () => {
       _.forEach(option => {
         expect(getBenefitOptionText(option, true, true)).not.to.be.undefined;
         expect(getBenefitOptionText(option, false, true)).not.to.be.undefined;
-      }, ['hasServiceConnectedDisabilities']);
+      }, ['hasNonServiceConnectedPension', 'hasServiceConnectedDisabilities']);
       // For dependents only
       _.forEach(option => {
         expect(getBenefitOptionText(option, true, false)).not.to.be.undefined;
