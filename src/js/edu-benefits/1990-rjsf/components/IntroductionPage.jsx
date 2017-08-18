5f5dca1dd3fede402287fe16cb67841f77d1c0d5
peggygannon <peggy@thesocompany.com>
2017-08-18 14:33:26 -0700

    Fixed spacing in code

--- src/js/edu-benefits/1990-rjsf/components/IntroductionPage.jsx
+++ src/js/edu-benefits/1990-rjsf/components/IntroductionPage.jsx
@@ -43,7 +43,7 @@ class IntroductionPage extends React.Component {
             <li className="process-step list-two">
               <div><h5>Apply</h5></div>
               <p>Complete this education benefits form.</p>
-               <p>After submitting the form, you’ll get a confirmation message that you can print.</p>
+              <p>After submitting the form, you’ll get a confirmation message that you can print.</p>
             </li>
             <li className="process-step list-three">
               <div><h5>VA Review</h5></div>
