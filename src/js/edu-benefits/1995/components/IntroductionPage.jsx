753e89a20f7d1413712a922d514e54989caa7f02
peggygannon <peggy@thesocompany.com>
2017-08-21 06:42:07 -0700

    Content edits to 1995 intro page

--- src/js/edu-benefits/1995/components/IntroductionPage.jsx
+++ src/js/edu-benefits/1995/components/IntroductionPage.jsx
@@ -16,7 +16,7 @@ class IntroductionPage extends React.Component {
     return (
       <div className="schemaform-intro">
         <FormTitle title="Manage your education benefits"/>
-        <p>This application is equivalent to Form 22-1995 (Request for Change of Program or Place of Training).</p>
+        <p>Equal to VA Form 22-1995 (Request for Change of Program or Place of Training).</p>
         <div className="process schemaform-process">
           <ol>
             <li className="process-step list-one">
@@ -41,8 +41,8 @@ class IntroductionPage extends React.Component {
               </ul>
             </li>
             <li className="process-step list-two">
-              <div><h5>Apply for Benefits</h5></div>
-              <p>Complete this form.</p>
+              <div><h5>Apply</h5></div>
+              <p>Complete this education benefits form.</p>
             </li>
             <li className="process-step list-three">
               <div><h5>VA Review</h5></div>
