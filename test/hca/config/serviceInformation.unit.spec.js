086b8c3823ee70ee744488efd2bb760ac8eab8e6
cvalarida <chris.valarida@adhocteam.us>
2017-08-21 11:51:35 -0700

    Add test

--- test/hca/config/serviceInformation.unit.spec.js
+++ test/hca/config/serviceInformation.unit.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import ReactTestUtils from 'react-dom/test-utils';
+import moment from 'moment';
 
 import { DefinitionTester, getFormDOM } from '../../util/schemaform-utils.jsx';
 import formConfig from '../../../src/js/hca/config/form';
@@ -56,6 +57,29 @@ describe('Hca serviceInformation', () => {
     formDOM.fillDate('root_lastEntryDate', '2010-1-1');
     formDOM.fillDate('root_lastDischargeDate', '2011-1-1');
     formDOM.fillData('#root_dischargeType', 'honorable');
+
+    formDOM.submitForm();
+    expect(formDOM.querySelectorAll('.usa-input-error').length).to.equal(0);
+    expect(onSubmit.called).to.be.true;
+  });
+
+  it('should hide and not require discharge status if discharge date is in the future', () => {
+    const onSubmit = sinon.spy();
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+          schema={schema}
+          definitions={definitions}
+          onSubmit={onSubmit}
+          uiSchema={uiSchema}/>
+    );
+    const formDOM = getFormDOM(form);
+
+    formDOM.fillData('#root_lastServiceBranch', 'army');
+    formDOM.fillDate('root_lastEntryDate', '2010-1-1');
+    // TODO: Use a constant instead of a magic string
+    formDOM.fillDate('root_lastDischargeDate', moment().add(100, 'days').format('YYYY-MM-DD'));
+
+    expect(formDOM.querySelector('#root_dischargeType')).to.be.null;
 
     formDOM.submitForm();
     expect(formDOM.querySelectorAll('.usa-input-error').length).to.equal(0);
