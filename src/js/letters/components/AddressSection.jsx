4e8de336bb737e467b015fee964fd006e9aa97a2
Rach <rroueche@users.noreply.github.com>
2017-08-22 09:26:09 -0400

    updating address lang

--- src/js/letters/components/AddressSection.jsx
+++ src/js/letters/components/AddressSection.jsx
@@ -30,7 +30,7 @@ class AddressSection extends React.Component {
           <div className="letters-address">{addressLines.join('').toLowerCase()}</div>
           <div className="letters-address">{(destination.city || '').toLowerCase()}, {destination.state} {(destination.zipCode || '').toLowerCase()}</div>
           <div>EDIT</div>
-          <div>When you download a letter, it will show this address on it. You may want to update it if it's incorrect, but you can still download and use letters with an incorrect address.</div>
+          <div>When you download a letter, it will show this address. If this address is incorrect you may want to update it, but your letter will still be valid even with the incorrect address.</div>
         </div>
       );
     }
