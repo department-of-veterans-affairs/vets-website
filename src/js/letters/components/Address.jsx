6bc7187fdaee0bbaf70617f7a88da20fc217b0d2
akainic <Anne.Kainic@va.gov>
2017-08-29 09:38:47 -0400

    Basic Address component set up

---
+++ src/js/letters/components/Address.jsx
@@ -0,0 +1,137 @@
+import React from 'react';
+import _ from 'lodash';
+import { set } from 'lodash/fp';
+
+import ErrorableSelect from '../../common/components/form-elements/ErrorableSelect';
+import ErrorableTextInput from '../../common/components/form-elements/ErrorableTextInput';
+import { isNotBlank, isBlankAddress, isValidUSZipCode, isValidCanPostalCode } from '../../common/utils/validations';
+import { countries, states } from '../../common/utils/options-for-select';
+
+/**
+ * Input component for an address.
+ *
+ * No validation is provided through a currently stubbed isAddressValid function.
+ */
+class Address extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.validateAddressField = this.validateAddressField.bind(this);
+    this.validatePostalCode = this.validatePostalCode.bind(this);
+  }
+
+  componentWillMount() {
+    this.id = _.uniqueId('address-input-');
+  }
+
+  // TODO: Look into if this is the best way to update address,
+  // it is incredibly slow right now
+  handleChange(path, update) {
+    let address = set(path, update, this.props.value);
+    // if country is changing we should clear the state
+    if (path === 'country') {
+      address = set('state.value', '', address);
+    }
+
+    this.props.onUserInput(address);
+  }
+
+  validateAddressField(field) {
+    if (this.props.required || !isBlankAddress(this.props.value)) {
+      return isNotBlank(field);
+    }
+
+    return true;
+  }
+
+  validatePostalCode(postalCodeField) {
+    let isValid = true;
+
+    if (this.props.required || !isBlankAddress(this.props.value)) {
+      isValid = isValid && isNotBlank(postalCodeField);
+    }
+
+    if (this.props.value.country === 'USA' && isNotBlank(postalCodeField)) {
+      isValid = isValid && isValidUSZipCode(postalCodeField);
+    }
+
+    if (this.props.value.country === 'CAN' && isNotBlank(postalCodeField)) {
+      isValid = isValid && isValidCanPostalCode(postalCodeField);
+    }
+
+    return isValid;
+  }
+
+  isMilitaryCity(city) {
+    const lowerCity = city.toLowerCase().trim();
+
+    return lowerCity === 'apo' || lowerCity === 'fpo' || lowerCity === 'dpo';
+  }
+
+  render() {
+    let stateList = [];
+    const selectedCountry = this.props.value.country === 'US' ? 'USA' : this.props.value.country;
+    if (states[selectedCountry]) {
+      stateList = states[selectedCountry];
+      if (this.props.value.city && this.isMilitaryCity(this.props.value.city)) {
+        stateList = stateList.filter(state => state.value === 'AE' || state.value === 'AP' || state.value === 'AA');
+      }
+    }
+
+    const stateProvince = _.hasIn(states, this.props.value.country === 'US' ? 'USA' : this.props.value.country)
+      ? (<ErrorableSelect errorMessage={this.validateAddressField(this.props.value.state) ? undefined : 'Please enter a valid state/province'}
+        label={this.props.value.country === 'CAN' ? 'Province' : 'State'}
+        name="state"
+        autocomplete="address-level1"
+        options={stateList}
+        value={{ value: this.props.value.state }}
+        required={this.props.required}
+        onValueChange={(update) => {this.handleChange('state', update);}}/>)
+      : (<ErrorableTextInput label="State/province"
+        name="province"
+        autocomplete="address-level1"
+        field={{ value: this.props.value.state }}
+        required={false}
+        onValueChange={(update) => {this.handleChange('state', update);}}/>);
+
+    return (
+      <div>
+        <ErrorableSelect errorMessage={this.validateAddressField(this.props.value.country) ? undefined : 'Please enter a country'}
+          label="Country"
+          name="country"
+          autocomplete="country"
+          options={countries}
+          value={{ value: this.props.value.country === 'US' ? 'USA' : this.props.value.country }}
+          required={this.props.required}
+          onValueChange={(update) => {this.handleChange('country', update);}}/>
+        <ErrorableTextInput errorMessage={this.validateAddressField(this.props.value.addressLine1) ? undefined : 'Please enter a street address'}
+          label="Street"
+          name="address"
+          autocomplete="street-address"
+          charMax={30}
+          field={{ value: this.props.value.addressLine1 }}
+          required={this.props.required}
+          onValueChange={(update) => {this.handleChange('addressLine1', update);}}/>
+        <ErrorableTextInput errorMessage={this.validateAddressField(this.props.value.city) ? undefined : 'Please enter a city'}
+          label={<span>City <em>(or APO/FPO/DPO)</em></span>}
+          name="city"
+          autocomplete="address-level2"
+          charMax={30}
+          field={{ value: this.props.value.city }}
+          required={this.props.required}
+          onValueChange={(update) => {this.handleChange('city', update);}}/>
+        {stateProvince}
+        <ErrorableTextInput errorMessage={this.validatePostalCode(this.props.value.zipCode) ? undefined : 'Please enter a valid Postal code'}
+          additionalClass="usa-input-medium"
+          label={this.props.value.country === 'USA' ? 'Zip code' : 'Postal code'}
+          name="postalCode"
+          autocomplete="postal-code"
+          field={{ value: this.props.value.zipCode }}
+          required={this.props.required}
+          onValueChange={(update) => {this.handleChange('zipCode', update);}}/>
+      </div>
+    );
+  }
+}
+
+export default Address;
--- src/js/letters/containers/AddressSection.jsx
+++ src/js/letters/containers/AddressSection.jsx
@@ -3,8 +3,14 @@ import { connect } from 'react-redux';
 import isEmpty from 'lodash/isEmpty';
 
 import { invalidAddressProperty } from '../utils/helpers.jsx';
+import Address from '../components/Address';
 
 export class AddressSection extends React.Component {
+  constructor() {
+    super();
+    this.state = { editAddress: false };
+  }
+
   render() {
     const destination = this.props.destination || {};
     const addressLines = [
@@ -12,6 +18,25 @@ export class AddressSection extends React.Component {
       destination.addressLine2 ? `, ${destination.addressLine2}` : '',
       destination.addressLine3 ? ` ${destination.addressLine3}` : ''
     ];
+
+    let addressFields;
+    if (this.state.editAddress) {
+      addressFields = (
+        <div>
+          <Address value={destination} onUserInput={{}} required/>
+          <button className="usa-button-primary" onClick={() => this.setState({ editAddress: false })}>Update</button>
+          <button className="usa-button-outline" onClick={() => this.setState({ editAddress: false })}>Cancel</button>
+        </div>
+      );
+    } else {
+      addressFields = (
+        <div>
+          <div className="letters-address">{addressLines.join('').toLowerCase()}</div>
+          <div className="letters-address">{(destination.city || '').toLowerCase()}, {destination.state} {(destination.zipCode || '').toLowerCase()}</div>
+          <button className="usa-button-outline" onClick={() => this.setState({ editAddress: true })}>Edit</button>
+        </div>
+      );
+    }
 
     let addressContent;
     if (isEmpty(destination)) {
@@ -26,11 +51,11 @@ export class AddressSection extends React.Component {
           <p>
             Downloaded documents will list your address as:
           </p>
-          <div className="letters-address">{(destination.fullName || '').toLowerCase()}</div>
-          <div className="letters-address">{addressLines.join('').toLowerCase()}</div>
-          <div className="letters-address">{(destination.city || '').toLowerCase()}, {destination.state} {(destination.zipCode || '').toLowerCase()}</div>
-          <h5>Why is this address important?</h5>
-          <div>When you download a letter, it will show this address on it. If this address is incorrect you may want to update it, but your letter will still be valid even with the incorrect address. <a href="https://eauth.va.gov/wssweb/wss-common-webparts/mvc/getPCIUUpdateForm" target="_blank">Update the address that appears on your letter(s)</a>.</div>
+          <div className="address-block">
+            <h5 className="letters-address">{(destination.fullName || '').toLowerCase()}</h5>
+            {addressFields}
+          </div>
+          <div>A correct address is not required to download, but you can update it if you want to./</div>
         </div>
       );
     }
