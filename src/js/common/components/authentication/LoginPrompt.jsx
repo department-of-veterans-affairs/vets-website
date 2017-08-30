308150f1e6de248c1ad52f31c47d6eb72d44b6a5
Sophia Dengo <sdengo@gmail.com>
2017-08-30 10:49:08 -0400

    made content edits to log in page per Peggy (#6291)
    * made content edits to log in page per Peggy
    
    * modified test for new language
    
    * capitalization per content

--- src/js/common/components/authentication/LoginPrompt.jsx
+++ src/js/common/components/authentication/LoginPrompt.jsx
@@ -37,16 +37,17 @@ class LoginPrompt extends React.Component {
       <div className="row primary">
         <div className="medium-12 small-12 columns">
           <div className="react-container">
-            <h1>Log In With Your Account for Vets.gov</h1>
+            <h1>Log In to Your Vets.gov Account</h1>
             <p>Log in with your verified ID.me account on Vets.gov to:</p>
             <ul>
-              <li>Use health tools, including refilling your VA prescription, sending messages to your healthcare team, and accessing your VA health record</li>
+              <li>Use health tools to refill your VA prescriptions, send secure messages to your health care team, and access your VA health records</li>
               <li>Check the status of your claims and appeals</li>
-              <li>View Post-9/11 GI Bill enrollment information</li>
+              <li>View your Post-9/11 GI Bill benefits summary and enrollment information</li>
+              <li>Download VA letters and documents related to your benefits and eligibility</li>
             </ul>
             <p>
-              <button className="usa-button-primary va-button-primary usa-button-big" onClick={this.handleLogin}><strong>Log in</strong></button>
-              <button className="va-button-secondary usa-button-big" onClick={this.handleSignup}><strong>Sign up</strong></button>
+              <button className="usa-button-primary va-button-primary usa-button-big" onClick={this.handleLogin}><strong>Log In</strong></button>
+              <button className="va-button-secondary usa-button-big" onClick={this.handleSignup}><strong>Sign Up</strong></button>
             </p>
             <p>Even without an account, you can apply for VA benefits.</p>
             <h5>Having trouble signing in?</h5>
