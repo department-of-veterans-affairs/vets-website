e1e2f1cd96185ab1eeae0d8d766c6ce44c8e7fe5
Claire Hsu <claire@adhocteam.us>
2017-08-29 21:55:12 -0700

    Squash bug

--- src/js/user-profile/helpers.jsx
+++ src/js/user-profile/helpers.jsx
@@ -1,16 +1,5 @@
 import Raven from 'raven-js';
 
-import fullSchema1010ez from '../hca/config/form';
-import fullSchema1990 from '../edu-benefits/1990-rjsf/config/form';
-import fullSchema1990e from '../edu-benefits/1990e/config/form';
-import fullSchema1990n from '../edu-benefits/1990n/config/form';
-import fullSchema1995 from '../edu-benefits/1995/config/form';
-import fullSchema5490 from '../edu-benefits/5490/config/form';
-import fullSchema5495 from '../edu-benefits/5495/config/form';
-import fullSchema527EZ from '../pensions/config/form';
-import fullSchema530 from '../burials/config/form';
-
-
 export const formTitles = {
   '21P-527EZ': 'Veterans pension benefits (21P-527EZ)',
   '21P-530': 'burial benefits (21P-530)',
@@ -36,15 +25,15 @@ export const formLinks = {
 };
 
 export const trackingPrefixes = {
-  '21P-527EZ': fullSchema527EZ.trackingPrefix,
-  '21P-530': fullSchema530.trackingPrefix,
-  '1010ez': fullSchema1010ez.trackingPrefix,
-  '22-1990': fullSchema1990.trackingPrefix,
-  '22-1990E': fullSchema1990e.trackingPrefix,
-  '22-1990N': fullSchema1990n.trackingPrefix,
-  '22-1995': fullSchema1995.trackingPrefix,
-  '22-5490': fullSchema5490.trackingPrefix,
-  '22-5495': fullSchema5495.trackingPrefix
+  '21P-527EZ': 'pensions-527EZ-',
+  '21P-530': 'burials-530-',
+  '1010ez': 'hca-',
+  '22-1990': 'edu-',
+  '22-1990E': 'edu-1990e-',
+  '22-1990N': 'edu-1990n-',
+  '22-1995': 'edu-1995-',
+  '22-5490': 'edu-5490-',
+  '22-5495': 'edu-5495-'
 };
 
 export const sipEnabledForms = new Set(['1010ez', '21P-527EZ', '21P-530']);
