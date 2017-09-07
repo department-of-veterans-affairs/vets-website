54a15e565dd5cabea4763e26d371aded072d9f39
Claire Hsu <claire@adhocteam.us>
2017-09-07 11:13:12 -0700

    Remove tabindex as button now appears in tab order

--- src/js/common/components/OMBInfo.jsx
+++ src/js/common/components/OMBInfo.jsx
@@ -32,7 +32,7 @@ class OMBInfo extends React.Component {
         <div>Respondent burden: <strong>{resBurden} minutes</strong></div>
         <div>OMB Control #: <strong>{ombNumber}</strong></div>
         <div>Expiration date: <strong>{expDate}</strong></div>
-        <div><button tabIndex={"0"} className={'va-button-link'} onClick={this.openModal}>Privacy Act Statement</button></div>
+        <div><button className={'va-button-link'} onClick={this.openModal}>Privacy Act Statement</button></div>
         <Modal
           cssClass="va-modal-large"
           contents={modalContents(resBurden)}
