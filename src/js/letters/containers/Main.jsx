59cfe3b46de02f84832594160b5e083d1801b98e
Mark Greenburg <marek.greenburg@gmail.com>
2017-09-08 13:20:26 -0500

    Add whitespace before anchor tag by breaking line on anchor tag instead of text

--- src/js/letters/containers/Main.jsx
+++ src/js/letters/containers/Main.jsx
@@ -42,9 +42,8 @@ export class Main extends React.Component {
               <div className="usa-alert-body">
                 <h4 className="usa-alert-heading">Letters Unavailable</h4>
                 <p className="usa-alert-text">
-                  We weren't able to retrieve your VA letters. Please call
-                  <a href="tel:855-574-7286">1-855-574-7286</a> between Monday-Friday
-                  8:00 a.m. - 8:00 p.m. (ET).
+                  We weren't able to retrieve your VA letters. Please call <a href="tel:855-574-7286">
+                  1-855-574-7286</a> between Monday-Friday 8:00 a.m. - 8:00 p.m. (ET).
                 </p>
               </div>
             </div>
