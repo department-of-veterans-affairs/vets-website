4196587628264fd74637cb06c4bca094f4209cff
cvalarida <chris.valarida@adhocteam.us>
2017-09-08 15:02:29 -0700

    Change apostrophes

--- src/js/letters/containers/LettersApp.jsx
+++ src/js/letters/containers/LettersApp.jsx
@@ -12,8 +12,8 @@ function AppContent({ children, isDataAvailable }) {
   if (unregistered) {
     view = (
       <h4>
-        We weren't able to find information about your VA letters.
-        If you think you should be able to access this information, please call the Vets.gov Help Desk at <a href="tel:855-574-7286">1-855-574-7286</a> (TTY: 800-829-4833). We're here Monday–Friday, 8:00 a.m.–8:00 p.m. (ET).
+        We weren’t able to find information about your VA letters.
+        If you think you should be able to access this information, please call the Vets.gov Help Desk at <a href="tel:855-574-7286">1-855-574-7286</a> (TTY: 800-829-4833). We’re here Monday–Friday, 8:00 a.m.–8:00 p.m. (ET).
       </h4>
     );
   } else {
