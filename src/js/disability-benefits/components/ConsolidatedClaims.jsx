2f815a2ac2901db4481b9c7c23602c7636fa0842
Sophia Dengo <sdengo@gmail.com>
2017-09-08 10:45:15 -0400

    consolidatedClaims modal

--- src/js/disability-benefits/components/ConsolidatedClaims.jsx
+++ src/js/disability-benefits/components/ConsolidatedClaims.jsx
@@ -4,18 +4,16 @@ import React from 'react';
 export default function ConsolidatedClaims({ onClose }) {
   return (
     <div>
-      <div className="va-modal-inner claims-status-upload-header">
-        <h3 id="consolidated-claims-title">A note about consolidated claims</h3>
-        <button
-          className="va-modal-close"
-          type="button"
-          onClick={onClose}>
-          <i className="fa fa-close"></i>
-          <span className="usa-sr-only">Close this modal</span>
-        </button>
-        <div className="va-modal-body">
-          If you turn in a new claim while we’re reviewing another one from you, we’ll add any new information to the original claim and close the new claim, with no action required from you.
-        </div>
+      <h3 id="consolidated-claims-title claims-status-upload-header">A note about consolidated claims</h3>
+      <button
+        className="va-modal-close"
+        type="button"
+        onClick={onClose}>
+        <i className="fa fa-close"></i>
+        <span className="usa-sr-only">Close this modal</span>
+      </button>
+      <div className="va-modal-body">
+        If you turn in a new claim while we’re reviewing another one from you, we’ll add any new information to the original claim and close the new claim, with no action required from you.
       </div>
     </div>
   );
