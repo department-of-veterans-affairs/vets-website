a3cf05d056f460e52dc55fee250ae7046940d02a
Sophia Dengo <sdengo@gmail.com>
2017-09-11 16:27:55 -0400

    mail or fax modal

--- src/js/disability-benefits/components/MailOrFax.jsx
+++ src/js/disability-benefits/components/MailOrFax.jsx
@@ -4,23 +4,25 @@ import React from 'react';
 export default function MailOrFax({ onClose }) {
   return (
     <div>
-      <div className="claims-status-upload-header">Mail and Fax Instructions</div>
-      <div className="claims-status-upload-inner">
-        <p>
-          Please upload your documents online here to help us process your claim quickly.
-        </p>
-        <p>
-          If you can’t upload documents:
-        </p>
-        <ol>
-          <li>Make copies of the documents.</li>
-          <li>Make sure you write your name and claim number on every page.</li>
-          <li>Mail or fax them to the <a target="_blank" href="http://www.benefits.va.gov/COMPENSATION/mailingaddresses.asp">VA Claims Intake Center.</a></li>
-        </ol>
-        <p>
-          <button className="usa-button" onClick={onClose}>Close</button>
-        </p>
-      </div>
+      <h3 className="claims-status-upload-header">Mail and Fax Instructions</h3>
+      <button
+        className="va-modal-close"
+        type="button"
+        onClick={onClose}>
+        <i className="fa fa-close"></i>
+        <span className="usa-sr-only">Close this modal</span>
+      </button>
+      <p>
+        Please upload your documents online here to help us process your claim quickly.
+      </p>
+      <p>
+        If you can’t upload documents:
+      </p>
+      <ol>
+        <li>Make copies of the documents.</li>
+        <li>Make sure you write your name and claim number on every page.</li>
+        <li>Mail or fax them to the <a target="_blank" href="http://www.benefits.va.gov/COMPENSATION/mailingaddresses.asp">VA Claims Intake Center.</a></li>
+      </ol>
     </div>
   );
 }
