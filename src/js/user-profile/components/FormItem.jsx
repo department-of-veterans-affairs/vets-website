fa22922a08da0c09f1f1d322f68bfe536b29c906
Claire Hsu <claire@adhocteam.us>
2017-09-11 21:09:24 -0700

    Provide h5 padding

--- src/js/user-profile/components/FormItem.jsx
+++ src/js/user-profile/components/FormItem.jsx
@@ -12,25 +12,28 @@ class FormItem extends React.Component {
     const { last_updated: lastSaved, expires_at: expirationTime } = savedFormData.metadata;
     const lastSavedDateTime = moment.unix(lastSaved).format('M/D/YYYY [at] h:mm a');
     const expirationDate = moment.unix(expirationTime);
-    //const isExpired = moment(expirationDate).isBefore();
-    const isExpired = true;
+    const isExpired = moment(expirationDate).isBefore();
     const activeView = (
       <div className="card information">
         <div className="saved-form-information">
-          <h5 className="saved-form-title">Application for {formTitles[formId]}</h5>
+          <h5 className="form-title saved">Application for {formTitles[formId]}</h5>
           {!!lastSaved && !!expirationDate && <div className="saved-form-metadata-container">
             <span className="saved-form-item-metadata">Last saved on {lastSavedDateTime}</span>
             <p className="saved-form-item-metadata">Your saved application <span className="saved-form-item-expires">will expire in {dateDiffDesc(expirationDate)}.</span></p>
           </div>}
         </div>
         <div className="row small-collapse">
-          <div className="small-12 medium-8 large-8 columns application-route-container">
-            <a className="usa-button-primary application-route" href={formLinks[formId]}>Continue Your Application</a>
+          <div className="small-12 large-8 columns">
+            <div className="application-route-container">
+              <a className="usa-button-primary application-route" href={formLinks[formId]}>Continue Your Application</a>
+            </div>
           </div>
-          <div className="small-12 medium-4 large-4 columns remove-saved-application-container">
-            <button className="va-button-link remove-saved-application-button" onClick={() => {this.props.toggleModal(formId);}}>
-              <i className="fa fa-trash"></i><span className="remove-saved-application-label">Delete</span>
-            </button>
+          <div className="small-12 large-4 columns">
+            <div className="remove-saved-application-container">
+              <button className="va-button-link remove-saved-application-button" onClick={() => {this.props.toggleModal(formId);}}>
+                <i className="fa fa-trash"></i><span className="remove-saved-application-label">Delete</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -41,9 +44,9 @@ class FormItem extends React.Component {
           <i className="fa fa-close" aria-label="Close icon"></i>
         </button>
         <div className="usa-alert-body">
-          <h5>Your saved {formTitles[formId]} application has expired.</h5>
+          <h5 className="form-title">Your saved {formTitles[formId]} application has expired.</h5>
           <p>To apply for {formBenefits[formId]}, please start a new application.</p>
-          <div className="small-12 medium-8 large-8 columns application-route-container">
+          <div className="small-12 large-8 columns application-route-container">
             <a className="usa-button-primary application-route" href={formLinks[formId]}>Start a New Application</a>
           </div>
         </div>
