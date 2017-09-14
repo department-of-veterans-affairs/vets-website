7c163758141345e666b2e49564c7667f449774d1
Sophia Dengo <sdengo@gmail.com>
2017-09-14 10:35:14 -0400

    added click handler to close button; needs testing

--- src/js/rx/components/GlossaryModal.jsx
+++ src/js/rx/components/GlossaryModal.jsx
@@ -20,7 +20,7 @@ class GlossaryModal extends React.Component {
     if (this.props.isVisible) {
       element = (
         <div>
-          <button className="va-modal-close" type="button"><i className="fa fa-close"></i><span className="usa-sr-only">Close this modal</span></button>
+          <button className="va-modal-close" type="button"><i className="fa fa-close"></i><span className="usa-sr-only" onClick={this.props.onCloseModal}>Close this modal</span></button>
           <GlossaryList terms={this.props.content}/>
           <div className="va-modal-button-group cf">
             <button type="button" onClick={this.handleCloseModal}>Close</button>
