29a14efed0cc3d53be643d635dd9f42a308d9127
peggygannon <peggy@thesocompany.com>
2017-09-14 10:35:52 -0700

    Content edits to intro page

--- src/js/edu-benefits/1990-rjsf/components/IntroductionPage.jsx
+++ src/js/edu-benefits/1990-rjsf/components/IntroductionPage.jsx
@@ -49,11 +49,9 @@ class IntroductionPage extends React.Component {
             <li className="process-step list-three">
               <div><h5>VA Review</h5></div>
               <div><h6>How long does it take VA to make a decision?</h6></div>
-              <ul><li>We usually process claims within 30 days.</li></ul>
-              <div><h6>What should I do while I wait?</h6></div>
-              <ul><li>We offer tools and counseling programs to help you make the most of your educational options. <a href="/education/tools-programs/">Learn about career counseling options.</a></li></ul>
-              <div><h6>What if VA needs more information?</h6></div>
-              <ul><li>We’ll contact you if we need more information.</li></ul>
+              <p>We usually process claims within 30 days.</p>
+              <p>We’ll let you know by mail if we need more information.</p>
+              <p>We offer tools and counseling programs to help you make the most of your educational options. <a href="/education/tools-programs/">Learn about career counseling options.</a></li></ul>
             </li>
             <li className="process-step list-four">
               <div><h5>Decision</h5></div>
