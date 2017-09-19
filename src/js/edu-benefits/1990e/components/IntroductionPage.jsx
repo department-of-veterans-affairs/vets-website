8a78e2f20680c25183b1a1afcd119db774e89305
peggygannon <peggy@thesocompany.com>
2017-09-19 10:31:49 -0700

    Content edits to intro

--- src/js/edu-benefits/1990e/components/IntroductionPage.jsx
+++ src/js/edu-benefits/1990e/components/IntroductionPage.jsx
@@ -37,9 +37,7 @@ class IntroductionPage extends React.Component {
               <p>An accredited representative with a Veterans Service Organization (VSO) can help you fill out your claim. <a href="/disability-benefits/apply/help/index.html">Find an accredited representative</a>.</p>
               <h6>Learn about educational programs</h6>
               <ul>
-                <p>See what benefits you’ll get at the school you want to attend.</p> <a href="/gi-bill-comparison-tool/">Use the GI Bill Comparison Tool</a>.</li>
-              </ul>
-            </li>
+              <p>See what benefits you’ll get at the school you want to attend.</p> <a href="/gi-bill-comparison-tool/">Use the GI Bill Comparison Tool</a>.</p>
             <li className="process-step list-two">
               <div><h5>Apply</h5></div>
               <p>Complete this education benefits form.</p>
@@ -48,15 +46,12 @@ class IntroductionPage extends React.Component {
             <li className="process-step list-three">
               <div><h5>VA Review</h5></div>
               <p>We usually process claims within 30 days. We’ll let you know by mail if we need more information.</p>
-              <p>We offer tools and counseling programs to help you make the most of your educational options. <a href="/education/tools-programs/">Learn about career counseling options.</a></li></ul>
-            </li>
+              <p>We offer tools and counseling programs to help you make the most of your educational options. <a href="/education/tools-programs/">Learn about career counseling options.</a></p>
             <li className="process-step list-four">
               <div><h5>Decision</h5></div>
               <ul><li>You’ll get a Certificate of Eligibility (COE), or award letter, in the mail if we've approved your application.</li></ul>
               <ul><li>If your application wasn’t approved, you’ll get a denial letter in the mail.</li></ul>
             </li>
-          </ol>
-        </div>
         <SaveInProgressIntro
           messages={this.props.route.formConfig.savedFormMessages}
           pageList={this.props.route.pageList}
