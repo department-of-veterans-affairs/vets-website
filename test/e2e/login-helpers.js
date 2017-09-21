09615f7dc0b8b67facfe14e33a211807b0f7cc1b
Ben Shyong <ben.shyong@gmail.com>
2017-09-21 14:32:44 -0700

    Revert "Modal based signin flow (#6445)"
    This reverts commit ded34d3948be8a39977b324259f41e1e0e6ff4b8.

--- test/e2e/login-helpers.js
+++ test/e2e/login-helpers.js
@@ -108,12 +108,14 @@ function testUnauthedUserFlow(client, path) {
     .waitForElementVisible('body', Timeouts.normal);
 
   client
-    .waitForElementVisible('.login', Timeouts.normal)
-    .expect.element('h1').text.to.equal('Sign in to Vets.gov');
+    .waitForElementVisible('.react-container', Timeouts.normal)
+    .expect.element('h1').text.to.equal('Log In to Your Vets.gov Account');
 
   logIn(token, client, path, 1)
-    .waitForElementVisible('.verify', Timeouts.normal)
-    .expect.element('h1').text.to.equal('Verify your identity');
+    .waitForElementVisible('.react-container', Timeouts.normal)
+    .expect.element('h1').text.to.equal('Verify your Identity with ID.me');
+  client
+    .expect.element('button.usa-button-big').text.to.equal('Get Started');
 }
 
 module.exports = {
