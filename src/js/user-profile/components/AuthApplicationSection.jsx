f2b6b126a699fa159205c12c4f5c4000f696b5e6
Ben Shyong <ben.shyong@gmail.com>
2017-09-25 15:29:11 -0700

    resolve 5008

--- src/js/user-profile/components/AuthApplicationSection.jsx
+++ src/js/user-profile/components/AuthApplicationSection.jsx
@@ -31,7 +31,7 @@ class AuthApplicationSection extends React.Component {
           <p><span className="label">Your account will allow you to:</span></p>
           <p><a href="/health-care/apply">Apply for health care</a></p>
           <p><a href="/education/apply-for-education-benefits">Apply for education benefits</a></p>
-          <p><span className="label">You need to <button className="va-button-link" onClick={this.verifyUser}>verify your account</button> in order to:</span></p>
+          <p><span className="label">You need to <a href="/verify?next=/profile">verify your account</a> in order to:</span></p>
           <p>Refill your prescription</p>
           <p>Message your health care team</p>
           <p>Check your claim status</p>
