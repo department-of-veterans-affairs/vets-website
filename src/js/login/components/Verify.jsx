e3a9ef681b11f3810b89a4c998f2e0f9c3050a8b
Ben Shyong <ben.shyong@gmail.com>
2017-09-25 15:45:18 -0700

    add missing polyfill

--- src/js/login/components/Verify.jsx
+++ src/js/login/components/Verify.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import URLSearchParams from 'url-search-params';
 
 import { handleVerify } from '../../common/helpers/login-helpers.js';
 
