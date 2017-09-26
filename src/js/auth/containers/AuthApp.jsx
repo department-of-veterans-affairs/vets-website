5e284778bff2621ce977532efb47d5d2705cc154
Ben Shyong <ben.shyong@gmail.com>
2017-09-25 17:05:34 -0700

    Revert "Revert "Kudos identity => master for testing (#6461)" (#6467)"
    This reverts commit 758054a14bbe93e7728603abaf68a741c9477096.

--- src/js/auth/containers/AuthApp.jsx
+++ src/js/auth/containers/AuthApp.jsx
@@ -51,17 +51,20 @@ class AuthApp extends React.Component {
     }).then(json => {
       const userData = json.data.attributes.profile;
       if (userData.loa.highest === 3) {
-        if (userData.loa.current === 3 && sessionStorage.mfa_start) {
+        if ((userData.loa.current === 3 && sessionStorage.mfa_start) || userData.authnContext !== null) {
           this.setMyToken(myToken);
         } else {
           sessionStorage.setItem('mfa_start', true);
 
-          this.serverRequest = fetch(`${environment.API_URL}/v0/sessions/new?level=3`, {
+          this.serverRequest = fetch(`${environment.API_URL}/v0/sessions/identity_proof`, {
             method: 'GET',
+            headers: new Headers({
+              Authorization: `Token token=${myToken}`
+            })
           }).then(response => {
             return response.json();
           }).then(innerJson => {
-            window.location.href = appendQuery(innerJson.authenticate_via_get, { clientId: gaClientId() });
+            window.location.href = appendQuery(innerJson.identity_proof_url, { clientId: gaClientId() });
           });
         }
       } else {
