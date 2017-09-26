198ba2a2208287f94e26202d66b500a7859694d3
Claire Hsu <claire@adhocteam.us>
2017-09-25 17:23:36 -0700

    Tidy

--- src/js/edu-benefits/5490/containers/ConfirmationPage.jsx
+++ src/js/edu-benefits/5490/containers/ConfirmationPage.jsx
@@ -75,10 +75,10 @@ class ConfirmationPage extends React.Component {
           </ul>
         </div>
         <div className="confirmation-guidance-container">
-        <h4 className="confirmation-guidance-heading">What happens after I apply?</h4>
-        <p className="confirmation-guidance-message"><a href="/education/after-you-apply">Find out what happens after you apply</a>.</p>
-        <h4 className="confirmation-guidance-heading">Need help?</h4>
-        <p className="confirmation-guidance-message">If you have questions, call <a href="tel:888-442-4551">1-888-442-4551</a> (1-888-GI-BILL-1), 8:00 a.m. - 7:00 p.m. (ET) Monday - Friday.</p>
+          <h4 className="confirmation-guidance-heading">What happens after I apply?</h4>
+          <p className="confirmation-guidance-message"><a href="/education/after-you-apply">Find out what happens after you apply</a>.</p>
+          <h4 className="confirmation-guidance-heading">Need help?</h4>
+          <p className="confirmation-guidance-message">If you have questions, call <a href="tel:888-442-4551">1-888-442-4551</a> (1-888-GI-BILL-1), 8:00 a.m. - 7:00 p.m. (ET) Monday - Friday.</p>
         </div>
         <div className="row form-progress-buttons schemaform-back-buttons">
           <div className="small-6 usa-width-one-half medium-6 columns">
