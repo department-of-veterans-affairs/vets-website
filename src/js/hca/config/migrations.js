d08ebba3ce14904d3569f7d50825b2d8577d4686
Claire Hsu <claire@adhocteam.us>
2017-09-27 12:00:22 -0700

    Update existing hca migration

--- src/js/hca/config/migrations.js
+++ src/js/hca/config/migrations.js
@@ -4,10 +4,10 @@ export default [
   // 0 -> 1, we had a bug where isSpanishHispanicLatino was defaulted in the wrong place
   // and this removes it and defaults it in the right spot if necessary
   (savedData) => {
-    const newData = _.unset('isSpanishHispanicLatino', savedData);
+    const newData = _.unset('isSpanishHispanicLatino', savedData.formData);
 
-    if (typeof _.get('view:demographicCategories.isSpanishHispanicLatino', newData) === 'undefined') {
-      return _.set('view:demographicCategories.isSpanishHispanicLatino', false, newData);
+    if (typeof _.get('view:demographicCategories.isSpanishHispanicLatino', newData.formData) === 'undefined') {
+      return _.set('view:demographicCategories.isSpanishHispanicLatino', false, newData.formData);
     }
 
     return newData;
