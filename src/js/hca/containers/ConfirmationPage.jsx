614a9d5056da95d6f18cd660695c7cec622f11cf
Claire Hsu <claire@adhocteam.us>
2017-09-27 10:05:09 -0700

    Include/update find out what happens after you apply link (#6473)
    * Include/update find out what happens after you apply link
    
    * Adjust structure
    
    * Tidy
    
    * Update pension confirmation page need help style as well
    
    * Make burial confirmation page need help consistent with other pages
    
    * Remove .swp file

--- src/js/hca/containers/ConfirmationPage.jsx
+++ src/js/hca/containers/ConfirmationPage.jsx
@@ -33,14 +33,22 @@ class ConfirmationPage extends React.Component {
           <h4 className="success-copy">You have successfully submitted your application for health care.</h4>
         </div>
         <div>
-          <p>We are processing your application. The Department of Veterans Affairs will contact you when we finish our review.</p>
+          <p>
+            We are processing your application.<br/>
+            The Department of Veterans Affairs will contact you when we finish our review.<br/>
+            <i>Please print this page for your records.</i>
+          </p>
           {time &&
             <div className="success-alert-box">
               <p className="success-copy">Form submitted: {moment(time).format('MMMM D, YYYY, h:mm a')}</p>
             </div>
           }
-          <p>Please print this page for your records.</p>
-          <p>If you have questions, call 1-877-222-VETS (8387) and press 2.</p>
+          <div className="confirmation-guidance-container">
+            <h4 className="confirmation-guidance-heading">What happens after I apply?</h4>
+            <p className="confirmation-guidance-message"><a href="/health-care/after-you-apply">Find out what happens after you apply.</a></p>
+            <h4 className="confirmation-guidance-heading">Need help?</h4>
+            <p className="confirmation-guidance-message">If you have questions, call <a href="tel:+1-877-222-8387">1-800-827-VETS (8387)</a> and press 2.</p>
+          </div>
         </div>
       </div>
     );
