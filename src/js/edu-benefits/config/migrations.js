2ca29f498be95f6259e2c00bcd799cf6c7429d61
Claire Hsu <claire@adhocteam.us>
2017-10-06 09:26:33 -0700

    PR comment

--- src/js/edu-benefits/config/migrations.js
+++ src/js/edu-benefits/config/migrations.js
@@ -1,12 +1,12 @@
 import _ from 'lodash/fp';
 
-export function urlMigration(formId) {
+export function urlMigration(urlPrefix) {
   // 0 -> 1, we have split the edu bundle into form-specific bundles with a new urlPrefix
-  // and this replaces the saved returnUrl if necessary
+  // and this replaces the saved return_url if necessary
   return (savedData) => {
     const savedReturnUrl = _.get('return_url', savedData.metadata);
 
-    if (savedReturnUrl.includes(formId)) {
+    if (savedReturnUrl.includes(urlPrefix)) {
       const newData = _.set('metadata.return_url', savedReturnUrl.replace(formId, ''), savedData);
       return newData;
     }
