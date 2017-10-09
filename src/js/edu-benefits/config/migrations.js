f64e95613ec58946c729a2b978ec348912e5ddcd
Jeff Balboni <jeff@adhocteam.us>
2017-10-09 17:04:30 -0400

    Handle both url styles

--- src/js/edu-benefits/config/migrations.js
+++ src/js/edu-benefits/config/migrations.js
@@ -4,10 +4,11 @@ export function urlMigration(urlPrefix) {
   // 0 -> 1, we have split the edu bundle into form-specific bundles with a new urlPrefix
   // and this replaces the saved return_url if necessary
   return (savedData) => {
-    const savedReturnUrl = _.get('return_url', savedData.metadata).toLowerCase();
+    const savedReturnUrl = (_.get('returnUrl', savedData.metadata)
+      || _.get('return_url', savedData.metadata)).toLowerCase();
 
     if (savedReturnUrl.includes(urlPrefix)) {
-      const newData = _.set('metadata.return_url', savedReturnUrl.replace(urlPrefix, ''), savedData);
+      const newData = _.set('metadata.returnUrl', savedReturnUrl.replace(urlPrefix, ''), savedData);
       return newData;
     }
     return savedData;
