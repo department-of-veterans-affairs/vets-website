927b79f8664101468f62b64c49b2d8f7f4cbff82
Jeff Balboni <jbalboni@gmail.com>
2017-10-09 19:01:57 -0400

    Switch edu md pages to upper case, handle return urls safely (#6574)
    * Handle both url styles
    
    * Fix test
    
    * Actually move files
    
    * Remove linter comments
    
    * Fix tests
    
    * Update urls

--- test/edu-benefits/config/migrations.unit.spec.js
+++ test/edu-benefits/config/migrations.unit.spec.js
@@ -16,88 +16,88 @@ const migrations5495 = formConfig5495.migrations;
 
 describe('EDU migrations', () => {
   describe('first migration', () => {
-    it('should remove urlPrefix from return_url', () => {
+    it('should remove urlPrefix from returnUrl', () => {
       const data1990 = {
         formId: formConfig1990.formId,
         metadata: {
-          return_url: '/1990/abc/' // eslint-disable-line camelcase
+          returnUrl: '/1990/abc/'
         }
       };
 
       const data1990e = {
         formId: formConfig1990e.formId,
         metadata: {
-          return_url: '/1990E/abc/' // eslint-disable-line camelcase
+          returnUrl: '/1990E/abc/'
         }
       };
 
       const data1990n = {
         formId: formConfig1990n.formId,
         metadata: {
-          return_url: '/1990n/abc/' // eslint-disable-line camelcase
+          returnUrl: '/1990n/abc/'
         }
       };
 
       const data1995 = {
         formId: formConfig1995.formId,
         metadata: {
-          return_url: '/1995/abc/' // eslint-disable-line camelcase
+          returnUrl: '/1995/abc/'
         }
       };
 
       const data5490 = {
         formId: formConfig5490.formId,
         metadata: {
-          return_url: '/5490/abc/' // eslint-disable-line camelcase
+          returnUrl: '/5490/abc/'
         }
       };
 
       const data5495 = {
         formId: formConfig5495.formId,
         metadata: {
-          return_url: '/5495/abc/' // eslint-disable-line camelcase
+          returnUrl: '/5495/abc/'
         }
       };
 
       expect(migrations1990[0](data1990)).to.eql({
         formId: formConfig1990.formId,
         metadata: {
-          return_url: '/abc/' // eslint-disable-line camelcase
+          returnUrl: '/abc/'
         }
       });
 
       expect(migrations1990e[0](data1990e)).to.eql({
         formId: formConfig1990e.formId,
         metadata: {
-          return_url: '/abc/' // eslint-disable-line camelcase
+          returnUrl: '/abc/'
         }
       });
 
       expect(migrations1990n[0](data1990n)).to.eql({
         formId: formConfig1990n.formId,
         metadata: {
-          return_url: '/abc/' // eslint-disable-line camelcase
+          returnUrl: '/abc/'
         }
       });
 
       expect(migrations1995[0](data1995)).to.eql({
         formId: formConfig1995.formId,
         metadata: {
-          return_url: '/abc/' // eslint-disable-line camelcase
+          returnUrl: '/abc/'
         }
       });
 
       expect(migrations5490[0](data5490)).to.eql({
         formId: formConfig5490.formId,
         metadata: {
-          return_url: '/abc/' // eslint-disable-line camelcase
+          returnUrl: '/abc/'
         }
       });
 
       expect(migrations5495[0](data5495)).to.eql({
         formId: formConfig5495.formId,
         metadata: {
-          return_url: '/abc/' // eslint-disable-line camelcase
+          returnUrl: '/abc/'
         }
       });
     });
