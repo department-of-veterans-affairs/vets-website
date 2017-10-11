6ca346a2a4df1f8fcfd7b4dd63095716e4a80fac
Mark Greenburg <marek.greenburg@gmail.com>
2017-10-11 10:05:23 -0500

    Add tests for untested cases

--- test/letters/containers/Main.unit.spec.jsx
+++ test/letters/containers/Main.unit.spec.jsx
@@ -27,6 +27,9 @@ const defaultProps = {
   getAddressStates: getAddressStateSpy
 };
 
+const testText = 'test child element';
+const children = (<span>{testText}</span>);
+
 describe('<Main>', () => {
   it('renders', () => {
     const tree = SkinDeep.shallowRender(<Main {...defaultProps}/>);
@@ -35,15 +38,13 @@ describe('<Main>', () => {
   });
 
   it('renders its children when letters are available', () => {
-    const testText = 'test child component';
-    const children = (<span>{testText}</span>);
     const props = { ...defaultProps, children };
     const tree = SkinDeep.shallowRender(<Main {...props}/>);
     const childText = tree.subTree('span').text();
     expect(childText).to.equal(testText);
   });
 
-  it('shows a loading spinner when waiting for response', () => {
+  it('shows a loading spinner when awaiting response', () => {
     const props = { ...defaultProps, lettersAvailability: 'awaitingResponse' };
     const tree = SkinDeep.shallowRender(<Main {...props}/>);
     expect(tree.subTree('LoadingIndicator')).to.be.ok;
@@ -67,9 +68,22 @@ describe('<Main>', () => {
     expect(tree.subTree('#systemDownMessage')).to.be.ok;
   });
 
+  it('renders children for letter eligibility errors', () => {
+    const props = { ...defaultProps, lettersAvailability: 'letterEligibilityError', children };
+    const tree = SkinDeep.shallowRender(<Main {...props}/>);
+    const childText = tree.subTree('span').text();
+    expect(childText).to.equal(testText);
+  });
+
   it('should show letters unavailable message when service is unavailable', () => {
     const props = { ...defaultProps, lettersAvailability: 'unavailable' };
     const tree = SkinDeep.shallowRender(<Main {...props}/>);
     expect(tree.subTree('#lettersUnavailable')).to.be.ok;
+  });
+
+  it('renders system down message for all unspecified errors', () => {
+    const props = { ...defaultProps, lettersAvailability: 'bogusError' };
+    const tree = SkinDeep.shallowRender(<Main {...props}/>);
+    expect(tree.subTree('#systemDownMessage')).to.be.ok;
   });
 });
