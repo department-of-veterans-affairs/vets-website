579f94af6aa5ae7555383d87415d55e1901fbe77
Patrick Vinograd <patrick@adhocteam.us>
2017-10-11 16:39:01 -0700

    Improve gating

--- src/js/veteran-id-card/components/RequiredVeteranView.jsx
+++ src/js/veteran-id-card/components/RequiredVeteranView.jsx
@@ -7,17 +7,21 @@ class RequiredVeteranView extends React.Component {
 
   render() {
     let view;
-
+    const serviceAvailable = this.props.userProfile.services.indexOf('id-card') !== -1;
     if (this.props.userProfile.veteranStatus === 'SERVER_ERROR') {
       // If eMIS status is null, show a system down message.
-      view = <SystemDownView messageLine1="Sorry, we are temporarily unable to verify your Veteran status. Please try again later."/>;
+      view = <SystemDownView messageLine1="We’re sorry. We can’t proceed with your request for a Veteran ID card because we can't confirm your military history right now. Please try again in a few minutes." messageLine2="If it still doesn’t work, please call the Vets.gov Help Desk at 1-855-574-7286 (TTY: 1-800-829-4833). We’re here Monday–Friday, 8:00 a.m.–8:00 p.m. (ET)."/>;
     } else if (this.props.userProfile.veteranStatus === 'NOT_FOUND') {
       // If eMIS status is "not found", show message that we cannot find the user
       // in our system.
-      view = <SystemDownView messageLine1="We couldn’t find records verifying your Veteran status." messageLine2="Please call the Vets.gov Help Desk at 1-855-574-7286. We’re open Monday‒Friday, 8:00 a.m.‒8:00 p.m. (ET)."/>;
+      view = <SystemDownView messageLine1="We’re sorry. We can’t proceed with your request for a Veteran ID card because we can't confirm your military history right now. Please try again in a few minutes." messageLine2="If it still doesn’t work, please call the Vets.gov Help Desk at 1-855-574-7286 (TTY: 1-800-829-4833). We’re here Monday–Friday, 8:00 a.m.–8:00 p.m. (ET)."/>;
     } else if (this.props.userProfile.veteranStatus === 'OK') {
       if (this.props.userProfile.isVeteran !== true) {
-        view = <SystemDownView messageLine1="Printed ID cards are only available to Veterans. We are unable to verify your Veteran status" messageLine2="If you believe this is an error, please call the Vets.gov Help Desk at 1-855-574-7286. We’re open Monday‒Friday, 8:00 a.m.‒8:00 p.m. (ET)."/>;
+        view = <SystemDownView messageLine1="We can’t proceed with your request for a Veteran ID card because our records don’t confirm your status as a Veteran." messageLine2="If you think this is incorrect, please call the Vets.gov Help Desk at 1-855-574-7286 (TTY: 1-800-829-4833). We’re here Monday–Friday, 8:00 a.m.–8:00 p.m. (ET)."/>;
+      } else if (!serviceAvailable) {
+        // If all above conditions are true and service is still not present in user profile, then user
+        // is not enrolled in beta, show a general message.
+        view = <SystemDownView messageLine1="We can't proceed with your request for a Veteran ID Card." messageLine2="We’re working to expand the program to all eligible Veterans, so please check back again soon to see if you’re eligible."/>;
       } else {
         view = this.props.children;
       }
