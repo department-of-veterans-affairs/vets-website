edb7c4fcd8fb06a0f5b214cea0227e8a3466a186
Nick Sullivan <nick.sullivan@adhocteam.us>
2017-10-13 18:01:35 -0400

    Add logic for saving baseline images

--- test/visual-regression/index.e2e.spec.js
+++ test/visual-regression/index.e2e.spec.js
@@ -7,35 +7,39 @@ const {sitemapURLs} = require('../e2e/sitemap-helpers');
 const screenshotDirectory = path.join(__dirname, '../../logs/visual-regression');
 
 // Define a function for saving a screenshot
-function saveScreenshotAsBaseline(browser, route){
-    const fileName = `${screenshotDirectory}/baseline/test.png`;
-
-    console.log(fileName)
-
-    return new Promise((resolve, reject) => browser.saveScreenshot(fileName, resolve));
+function saveScreenshotAsBaseline(browser, baselineFileName, uri){
+    console.log(`Writing file ${baselineFileName}`);
+    return new Promise((resolve, reject) => browser.saveScreenshot(baselineFileName, resolve));
 }
 
 // Define a function for calculating a diff
-function calculateScreenshotDiff(browser, route){
+function calculateScreenshotDiff(browser, baselineFileName, uri){
 
 }
 
-function getApplication(handleRoute){
-    return function beginApplication(browser) {
+function mapRouteToRouteHandler(browser, route, routeHandler){
+    const uri = route.replace(baseUrl, '');
+    const baselineFileName = `${screenshotDirectory}/baseline${uri}.png`;
+    const onRouteLoaded = () => routeHandler(browser, baselineFileName, uri);
 
+    return browser.url(route, onRouteLoaded);
+}
+
+function getApplication(routeHandler){
+    return function beginApplication(browser) {
         const routesRequest = new Promise((resolve, reject) => sitemapURLs(resolve));
 
         return routesRequest
 
-            // Call the handler function for every 
-            .then(routes => routes.slice(0,2).map(route => {
-                browser.url(route);
-                return handleRoute(browser, route);
-            }))
+            // Map the array of routes into an array of operation promises
+            .then(routes => routes.map(route => mapRouteToRouteHandler(browser, route, routeHandler)))
 
-            .then(pendingOperations => Promise.all(pendingOperations))
+            // Wait for all operations to complete
+            .then(pendingRouteResults => Promise.all(pendingRouteResults))
             
-            .then(() => browser.end());
+            .then(() => browser.end())
+
+            .catch(error => console.log(error));
     }
 }
 
@@ -52,15 +56,23 @@ function getRouteHandler(){
 
     switch (command){
         case commands.CREATE_BASELINE_IMAGES:
+            console.log('Generating baseline images...');
             return saveScreenshotAsBaseline;
 
         case commands.CALCULATE_DIFFS:
         default:
+            console.log('Calculating image diffs with baseline...');
             return calculateScreenshotDiff;
     }
 }
 
-const routeHandler = getRouteHandler();
-const beginApplication = getApplication(routeHandler);
+function setup(){
+    const routeHandler = getRouteHandler();
+    const beginApplication = getApplication(routeHandler);
 
-module.exports = createE2eTest(beginApplication);
+    return createE2eTest(beginApplication);
+}
+
+
+
+module.exports = setup();
