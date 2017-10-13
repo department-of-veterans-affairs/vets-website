c24c25d5a589563fd9bd624c59a1ee6a96c9df73
Nick Sullivan <nick.sullivan@adhocteam.us>
2017-10-13 09:04:55 -0400

    Getting started with visual regression testing

---
+++ test/visual-regression/index.js
@@ -0,0 +1,26 @@
+console.log('hey there')
+
+// Check command flags for creating the baseline screenshots or calculating the diff
+
+// 1. Baseline - take screenshots and save them to an unversioned directory called "baseline"
+// 2. Diff - take screenshots and compute the diff for the corresponding baseline screenshot
+
+// Define a function for saving a screenshot
+function saveScreenshot(){
+
+}
+
+// Define a function for taking a screenshot
+function takeScreenshot(){
+
+}
+
+// Define a function for calculating a diff
+function calculateImageDiff(baseline, changed){
+
+}
+
+// Define a function that steps through a list of URL's, then either saves the screenshot or computes the diff
+function main(){
+
+}
