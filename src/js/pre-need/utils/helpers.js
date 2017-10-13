f9a848d3f40bb28e47d55e59902a195aceb7c668
Eugene Doan <eugene@adhocteam.us>
2017-10-13 05:18:29 -0400

    Added transformForSubmit.

--- src/js/pre-need/utils/helpers.js
+++ src/js/pre-need/utils/helpers.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import set from 'lodash/fp/set';
 
 import { validateBooleanGroup } from '../../common/schemaform/validation';
+import { transformForSubmit } from '../../common/schemaform/helpers';
 
 export function isVeteran(item) {
   return item.claimant.relationshipToVet.type === 1;
@@ -21,6 +23,48 @@ export function claimantHeader({ formData }) {
   return (
     <h4 className="highlight">{name}</h4>
   );
+}
+
+
+export function transform(formConfig, form) {
+  const matchClaimant = name => a => formatName(a.claimant.name) === name;
+  const formCopy = Object.assign({}, form);
+
+  // Fill in veteran info in each application
+  // where the sponsor is another claimant.
+  formCopy.applications = formCopy.applications.map(application => {
+    const sponsorName = application['view:sponsor'];
+    if (sponsorName !== 'Other') {
+      const veteranApplication = form.applications.find(matchClaimant(sponsorName));
+      return set('veteran', veteranApplication.veteran, application);
+    }
+
+    return application;
+  });
+
+  // Fill in applicant info in each application
+  // if the applicant is another claimant.
+  const applicantName = form['view:preparer'];
+  if (applicantName !== 'Other') {
+    const applicantApplication = form.applications.find(matchClaimant(applicantName));
+    const { address, email, name, phoneNumber } = applicantApplication.claimant;
+    formCopy.applications = formCopy.applications.map(application => set('applicant',  {
+      applicantEmail: email,
+      applicantPhoneNumber: phoneNumber,
+      applicantRelationshipToClaimant: application.claimant.ssn === applicantApplication.claimant.ssn ? 'Self' : 'Authorized Agent/Rep',
+      completingReason: '',
+      mailingAddress: address,
+      name
+    }, application));
+  }
+
+  const formData = transformForSubmit(formConfig, formCopy);
+
+  return JSON.stringify({
+    preNeedClaim: {
+      form: formData
+    },
+  });
 }
 
 export const veteranUISchema = {
