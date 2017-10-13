8ad797750fc30d903a5cf67b0e3833ccc3b519c3
Nick Sullivan <nick.sullivan@adhocteam.us>
2017-10-13 19:04:46 -0400

    Refactoring; getting ready calculating image diff

--- test/visual-regression/index.e2e.spec.js
+++ test/visual-regression/index.e2e.spec.js
@@ -3,42 +3,40 @@ const commandLineArgs = require('command-line-args');
 const Timeouts = require('../e2e/timeouts');
 const {baseUrl, createE2eTest} = require('../e2e/e2e-helpers');
 const LoginHelpers = require('../e2e/login-helpers');
-const {sitemapURLs} = require('../e2e/sitemap-helpers');
+const {sitemapURLs: getRoutes} = require('../e2e/sitemap-helpers');
+
 const screenshotDirectory = path.join(__dirname, '../../logs/visual-regression');
 
-// Define a function for saving a screenshot
-function saveScreenshotAsBaseline(browser, baselineFileName, uri){
-    console.log(`Writing file ${baselineFileName}`);
-    return new Promise((resolve, reject) => browser.saveScreenshot(baselineFileName, resolve));
-}
-
-// Define a function for calculating a diff
-function calculateScreenshotDiff(browser, baselineFileName, uri){
-
-}
-
-function mapRouteToRouteHandler(browser, route, routeHandler){
+function getBaselineFileName(route){
     const uri = route.replace(baseUrl, '');
-    const baselineFileName = `${screenshotDirectory}/baseline${uri}.png`;
-    const onRouteLoaded = () => routeHandler(browser, baselineFileName, uri);
 
-    return browser.url(route, onRouteLoaded);
+    return `${screenshotDirectory}/baseline${uri}.png`;
+}
+
+function saveScreenshotAsBaseline(browser, route, callback){
+    const fileName = getBaselineFileName(route);
+    
+    browser.url(route, () => browser.saveScreenshot(fileName, callback));
+}
+
+function calculateScreenshotDiff(browser, route, callback){
+    
+    // Promise 1 - Read the baseline image contents
+    // Promise 2 - Update the URL and take a screenshot
+    // Once both promises have resolved, compare the images.
+    // If possible, create and save a new image showing the diff.
+
 }
 
 function getApplication(routeHandler){
     return function beginApplication(browser) {
-        const routesRequest = new Promise((resolve, reject) => sitemapURLs(resolve));
 
-        return routesRequest
+        return new Promise((resolve, reject) => getRoutes(resolve))
 
-            // Map the array of routes into an array of operation promises
-            .then(routes => routes.map(route => mapRouteToRouteHandler(browser, route, routeHandler)))
+            .then(routes => routes.map(route => new Promise((resolve, reject) => routeHandler(browser, route, resolve))))
 
-            // Wait for all operations to complete
             .then(pendingRouteResults => Promise.all(pendingRouteResults))
             
-            .then(() => browser.end())
-
             .catch(error => console.log(error));
     }
 }
