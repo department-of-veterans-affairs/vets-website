d2f2432faa4badc531a7ce963b1fa4461fd17b96
Nick Sullivan <nick.sullivan@adhocteam.us>
2017-10-16 16:02:09 -0400

    Add logic for computing diff

--- test/visual-regression/index.e2e.spec.js
+++ test/visual-regression/index.e2e.spec.js
@@ -3,95 +3,106 @@ const path = require('path');
 const commandLineArgs = require('command-line-args');
 const resemble = require('node-resemble-js');
 const Timeouts = require('../e2e/timeouts');
-const {baseUrl, createE2eTest} = require('../e2e/e2e-helpers');
+const { baseUrl, createE2eTest } = require('../e2e/e2e-helpers');
 const LoginHelpers = require('../e2e/login-helpers');
-const {sitemapURLs: getRoutes} = require('../e2e/sitemap-helpers');
+const { sitemapURLs: getRoutes } = require('../e2e/sitemap-helpers');
 const screenshotDirectory = path.join(__dirname, '../../logs/visual-regression');
 
-function saveScreenshotAsBaseline(browser, baselineFileName, callback){
-    console.log(`Writing file ${baselineFileName}`);
-    browser.saveScreenshot(baselineFileName, callback);
+function getFileNames(route){
+    const uri = route.replace(baseUrl, '');
+    const baseline = path.join(screenshotDirectory, `/baseline${uri}.png`);
+    const diff = path.join(screenshotDirectory, `/diffs${uri}.png`);
+
+    return [baseline, diff];
 }
 
-function calculateScreenshotDiff(browser, baselineFileName, callback){
-    
+function saveScreenshotAsBaseline(browser, route) {
+    const [baselineFileName] = getFileNames(route);
+    return new Promise((resolve, reject) => browser.saveScreenshot(baselineFileName, resolve));
+}
+
+function calculateScreenshotDiff(browser, route) {
+
+    const [baselineFileName, diffFileName] = getFileNames(route);
+
     // Read the baseline image contents
     const readBaselineImage = new Promise((resolve, reject) =>
-         fs.readFile(baselineFileName, (err, result) => err ? reject(err) : resolve(result)));
+        fs.readFile(baselineFileName, (err, result) => err ? reject(err) : resolve(result)));
 
     // Take a screenshot of the browser page
     const logScreenshotData = false;
     const takeScreenshot = new Promise((resolve, reject) => browser.screenshot(logScreenshotData, resolve));
 
-    Promise
-    
+    return Promise
+
         // Wait for both operations to complete
-        .all([readBaselineImage, takeScreenshot])
+        .all([ readBaselineImage, takeScreenshot ])
 
         // Convert the results to base64 and run the comparison operation
-        .then(([baselineImageBuffer, screenshotResult]) => {
-            const baselineImageBase64 = new Buffer(baselineImageBuffer, 'base64');
-            const screenshotBase64 = new Buffer(screenshotResult.value, 'base64');
-            
+        .then(([ baselineImageBuffer, screenshotResult ]) => {
+            const baselineImage = new Buffer(baselineImageBuffer, 'base64');
+            const screenshot = new Buffer(screenshotResult.value, 'base64');
+
             // Use Resemble to determine any differences between the results
             return new Promise((resolve, reject) => {
-                resemble(baselineImageBase64)
-                    .compareTo(screenshotBase64)
+                resemble(baselineImage)
+                    .compareTo(screenshot)
                     .onComplete(resolve);
             });
         })
 
         // Run assertions on the results
         .then(comparisonResult => {
-            console.log(comparisonResult);
+            const misMatchPercentage = parseFloat(comparisonResult.misMatchPercentage);
+
+            if (misMatchPercentage <= 0.01) {
+                return;
+            }
+
+            const diffImageStream = comparisonResult.getDiffImage();
+            const writer = fs.createWriteStream(diffFileName);
+
+            return new Promise((resolve, reject) => {
+                diffImageStream.pack().pipe(writer);
+                diffImageStream.on('finish', resolve);
+            });
         })
 
-        .then(callback)
-
-        // Catch errors to allow the other routes to continue even if this fails
         .catch(err => console.error(err));
 }
 
-function enqueueRoute(browser, routeHandler, route) {
-    const uri = route.replace(baseUrl, '');
-    const baselineFileName = path.join(screenshotDirectory, `/baseline${uri}.png`);
-    const wrappedRouteHandler = resolve => browser.url(route, () => routeHandler(browser, baselineFileName, resolve));
-
-    return new Promise((resolve, reject) => wrappedRouteHandler(resolve));
-}
-
-function getApplication(routeHandler){
+function getApplication(routeHandler) {
     return function beginApplication(browser) {
 
         return new Promise((resolve, reject) => getRoutes(resolve))
 
-            .then(routes => routes.slice(0, 2))
-
             // Map the array of URI's into promises returned by the route handler
-            .then(routes => routes.map(route => enqueueRoute(browser, routeHandler, route)))
+            .then(routes => routes.map(route => 
+                new Promise((resolve, reject) => browser.url(route, resolve))
+                    .then(() => routeHandler(browser, route))
+            ))
 
-            // Wait for all operations to complete.
+            // Wait for all operations to complete
             .then(pendingRouteResults => Promise.all(pendingRouteResults))
 
-            // Close the window. We're out of here.
             .then(() => browser.closeWindow())
 
             .catch(error => console.log(error));
     }
 }
 
-function getRouteHandler(){
+function getRouteHandler() {
     const commands = {
         CREATE_BASELINE_IMAGES: 'baseline',
         CALCULATE_DIFFS: 'diff'
     };
-    
-    const {command} = commandLineArgs([
+
+    const { command } = commandLineArgs([
         { name: 'command', type: String },
         { name: 'config', type: String, alias: 'c' }
     ]);
 
-    switch (command){
+    switch (command) {
         case commands.CREATE_BASELINE_IMAGES:
             console.log('Generating baseline images...');
             return saveScreenshotAsBaseline;
@@ -103,7 +114,7 @@ function getRouteHandler(){
     }
 }
 
-function setup(){
+function setup() {
     const routeHandler = getRouteHandler();
     const beginApplication = getApplication(routeHandler);
 
