7513d0d763233499407899e083886f75c63fead5
Claire Hsu <claire@adhocteam.us>
2017-10-16 16:28:31 -0700

    Update test

--- test/hca/containers/ConfirmationPage.unit.spec.jsx
+++ test/hca/containers/ConfirmationPage.unit.spec.jsx
@@ -31,7 +31,6 @@ describe('hca <ConfirmationPage>', () => {
     expect(tree.everySubTree('span')[2].text()).to.contain('Jan 1, 2010');
     expect(tree.everySubTree('span')[3].text()).to.contain('3702390024');
     expect(tree.everySubTree('p')[0].text()).to.contain('Normally processed within a week.');
-    expect(tree.everySubTree('p')[2].text()).to.contain('Health Eligibility Center');
     expect(tree.everySubTree('.confirmation-guidance-message')[0].text()).to.contain('Find out what happens after you apply.');
     expect(tree.everySubTree('.confirmation-guidance-message')[1].text()).to.contain('If you have questions, call 1-877-222-VETS (8387) and press 2, Monday - Friday, 8:00 a.m. - 7:00 p.m. (ET).');
   });
@@ -54,7 +53,6 @@ describe('hca <ConfirmationPage>', () => {
     );
     expect(tree.subTree('.confirmation-page-title').text()).to.contain('Claim received');
     expect(tree.everySubTree('p')[0].text()).to.contain('Normally processed within a week.');
-    expect(tree.everySubTree('p')[2].text()).to.contain('Health Eligibility Center');
     expect(tree.everySubTree('.confirmation-guidance-message')[0].text()).to.contain('Find out what happens after you apply.');
     expect(tree.everySubTree('.confirmation-guidance-message')[1].text()).to.contain('If you have questions, call 1-877-222-VETS (8387) and press 2, Monday - Friday, 8:00 a.m. - 7:00 p.m. (ET).');
   });
