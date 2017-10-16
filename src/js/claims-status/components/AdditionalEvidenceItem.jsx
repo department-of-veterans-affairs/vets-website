a9895dd49b2518a07b47cb71b70e8c502d8e5989
Jeff Balboni <jeff@adhocteam.us>
2017-10-16 14:53:32 -0400

    WIP

--- src/js/claims-status/components/AdditionalEvidenceItem.jsx
+++ src/js/claims-status/components/AdditionalEvidenceItem.jsx
@@ -8,8 +8,8 @@ export default function AdditionalEvidenceItem({ item }) {
   const itemDate = getItemDate(item);
   return (
     <div className="submitted-file-list-item">
-      <p className="submission-file-type additional-evidence">Additional evidence</p>
-      <p className="submission-item">
+      <div className="submission-file-type additional-evidence">Additional evidence</div>
+      <p className="submission-description">
         <span className="claim-item-label">File:</span> {item.filename}<br/>
         <span className="claim-item-label">Type:</span> {item.fileType}
       </p>
