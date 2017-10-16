2ec0094c1a54c16a357b88c20191630fb91a5760
Nick Sullivan <nick.sullivan@adhocteam.us>
2017-10-16 18:51:28 -0400

    Add dependency for recursively creating directories

--- test/visual-regression/util/get-file-names.js
+++ test/visual-regression/util/get-file-names.js
@@ -1,30 +1,25 @@
 const fs = require('fs');
 const path = require('path');
+const mkdirp = require('mkdirp');
 const { baseUrl } = require('../../e2e/e2e-helpers');
 
 const DIR_EXISTS_ALREADY = 'EEXIST';
 const screenshotDirectory = path.join(__dirname, '../../../logs/visual-regression');
-const baselineDir =  path.join(screenshotDirectory, '/baseline');
-const diffDir =  path.join(screenshotDirectory, '/diffs');
+const baselineDir = path.join(screenshotDirectory, '/baseline');
+const diffDir = path.join(screenshotDirectory, '/diffs');
 
-function getFileNames(route){
+function getFileNames(route) {
     const uri = route.replace(baseUrl, '');
     const baseline = path.join(baselineDir, `/${uri}.png`);
     const diff = path.join(diffDir, `/${uri}.png`);
 
-    return [baseline, diff];
+    return [ baseline, diff ];
 }
 
-function createDirectoryIfNotExist(filePath){
+function createDirectoryIfNotExist(filePath) {
     const directory = path.dirname(filePath);
 
-    // Try to create directory, and ignore the error if the directory couldn't be created because it already exists.
-    return new Promise((resolve, reject) => fs.mkdir(directory, err => {
-        if (!err || err.code == DIR_EXISTS_ALREADY){
-            return resolve();
-        }
-        reject(err);
-    }))
+    return new Promise((resolve, reject) => mkdirp(directory, err => err ? reject(err) : resolve()));
 }
 
 module.exports = {
