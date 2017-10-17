9cbd2e79865d79b1b21b7f923852a92d1ca88a36
Claire Hsu <claire@adhocteam.us>
2017-10-17 13:26:17 -0700

    Update tests

--- test/hca/containers/ConfirmationPage.unit.spec.jsx
+++ test/hca/containers/ConfirmationPage.unit.spec.jsx
@@ -29,16 +29,15 @@ describe('hca <ConfirmationPage>', () => {
 
     expect(tree.subTree('.confirmation-page-title').text()).to.contain('Claim received');
     expect(tree.subTree('.claim-list')).to.exist;
-    expect(tree.everySubTree('span')[2].text()).to.contain('Jan 1, 2010');
+    expect(tree.everySubTree('span')[2].text()).to.contain('Jan. 1, 2010');
     expect(tree.everySubTree('span')[3].text()).to.contain('3702390024');
-    expect(tree.everySubTree('p')[0].text()).to.contain('Normally processed within a week.');
+    expect(tree.everySubTree('p')[0].text()).to.contain('We usually process claims within a week.');
     expect(tree.everySubTree('.confirmation-guidance-message')[0].text()).to.contain('Find out what happens after you apply.');
-    expect(tree.everySubTree('.confirmation-guidance-message')[1].text()).to.contain('If you have questions, call 1-877-222-VETS (8387) and press 2, Monday - Friday, 8:00 a.m. - 7:00 p.m. (ET).');
+    expect(tree.everySubTree('.confirmation-guidance-message')[1].text()).to.contain('If you have questions, please call 1-877-222-VETS (8387) and press 2, Monday - Friday, 8:00 a.m. - 7:00 p.m. (ET).');
   });
   it('should render without response properties', () => {
     const form = {
       submission: {
-        response: {}
       },
       data: {
         veteranFullName: {
@@ -53,9 +52,9 @@ describe('hca <ConfirmationPage>', () => {
       <ConfirmationPage form={form}/>
     );
     expect(tree.subTree('.confirmation-page-title').text()).to.contain('Claim received');
-    expect(tree.everySubTree('p')[0].text()).to.contain('Normally processed within a week.');
+    expect(tree.everySubTree('p')[0].text()).to.contain('We usually process claims within a week.');
     expect(tree.subTree('.claim-list')).to.be.false;
     expect(tree.everySubTree('.confirmation-guidance-message')[0].text()).to.contain('Find out what happens after you apply.');
-    expect(tree.everySubTree('.confirmation-guidance-message')[1].text()).to.contain('If you have questions, call 1-877-222-VETS (8387) and press 2, Monday - Friday, 8:00 a.m. - 7:00 p.m. (ET).');
+    expect(tree.everySubTree('.confirmation-guidance-message')[1].text()).to.contain('If you have questions, please call 1-877-222-VETS (8387) and press 2, Monday - Friday, 8:00 a.m. - 7:00 p.m. (ET).');
   });
 });
