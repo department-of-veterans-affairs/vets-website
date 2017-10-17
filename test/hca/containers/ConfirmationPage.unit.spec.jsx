1740fdd1dffdde1cae6af10a35b7abe8efdd99e7
Claire Hsu <claire@adhocteam.us>
2017-10-16 17:06:05 -0700

    Update test

--- test/hca/containers/ConfirmationPage.unit.spec.jsx
+++ test/hca/containers/ConfirmationPage.unit.spec.jsx
@@ -4,30 +4,31 @@ import SkinDeep from 'skin-deep';
 
 import { ConfirmationPage } from '../../../src/js/hca/containers/ConfirmationPage';
 
-let form = {
-  submission: {
-    response: {
-      timestamp: '2010-01-01',
-      formSubmissionId: '3702390024'
-    }
-  },
-  data: {
-    veteranFullName: {
-      first: 'Joe',
-      middle: 'Marjorie',
-      last: 'Smith',
-      suffix: 'Sr.'
-    }
-  }
-};
-
 describe('hca <ConfirmationPage>', () => {
   it('should render', () => {
+    const form = {
+      submission: {
+        response: {
+          timestamp: '2010-01-01',
+          formSubmissionId: '3702390024'
+        }
+      },
+      data: {
+        veteranFullName: {
+          first: 'Joe',
+          middle: 'Marjorie',
+          last: 'Smith',
+          suffix: 'Sr.'
+        }
+      }
+    };
+
     const tree = SkinDeep.shallowRender(
       <ConfirmationPage form={form}/>
     );
 
     expect(tree.subTree('.confirmation-page-title').text()).to.contain('Claim received');
+    expect(tree.subTree('.claim-list')).to.exist;
     expect(tree.everySubTree('span')[2].text()).to.contain('Jan 1, 2010');
     expect(tree.everySubTree('span')[3].text()).to.contain('3702390024');
     expect(tree.everySubTree('p')[0].text()).to.contain('Normally processed within a week.');
@@ -35,7 +36,59 @@ describe('hca <ConfirmationPage>', () => {
     expect(tree.everySubTree('.confirmation-guidance-message')[1].text()).to.contain('If you have questions, call 1-877-222-VETS (8387) and press 2, Monday - Friday, 8:00 a.m. - 7:00 p.m. (ET).');
   });
   it('should render without time', () => {
-    form = {
+    const form = {
+      submission: {
+        response: {
+          formSubmissionId: '3702390024'
+        }
+      },
+      data: {
+        veteranFullName: {
+          first: 'Joe',
+          middle: 'Marjorie',
+          last: 'Smith',
+          suffix: 'Sr.'
+        }
+      }
+    };
+    const tree = SkinDeep.shallowRender(
+      <ConfirmationPage form={form}/>
+    );
+    expect(tree.subTree('.confirmation-page-title').text()).to.contain('Claim received');
+    expect(tree.subTree('.claim-list')).to.exist;
+    expect(tree.everySubTree('span')[2].text()).to.contain('3702390024');
+    expect(tree.everySubTree('p')[0].text()).to.contain('Normally processed within a week.');
+    expect(tree.everySubTree('.confirmation-guidance-message')[0].text()).to.contain('Find out what happens after you apply.');
+    expect(tree.everySubTree('.confirmation-guidance-message')[1].text()).to.contain('If you have questions, call 1-877-222-VETS (8387) and press 2, Monday - Friday, 8:00 a.m. - 7:00 p.m. (ET).');
+  });
+  it('should render without confirmation number', () => {
+    const form = {
+      submission: {
+        response: {
+          timestamp: '2010-01-01',
+        }
+      },
+      data: {
+        veteranFullName: {
+          first: 'Joe',
+          middle: 'Marjorie',
+          last: 'Smith',
+          suffix: 'Sr.'
+        }
+      }
+    };
+    const tree = SkinDeep.shallowRender(
+      <ConfirmationPage form={form}/>
+    );
+    expect(tree.subTree('.confirmation-page-title').text()).to.contain('Claim received');
+    expect(tree.subTree('.claim-list')).to.exist;
+    expect(tree.everySubTree('span')[2].text()).to.contain('Jan 1, 2010');
+    expect(tree.everySubTree('p')[0].text()).to.contain('Normally processed within a week.');
+    expect(tree.everySubTree('.confirmation-guidance-message')[0].text()).to.contain('Find out what happens after you apply.');
+    expect(tree.everySubTree('.confirmation-guidance-message')[1].text()).to.contain('If you have questions, call 1-877-222-VETS (8387) and press 2, Monday - Friday, 8:00 a.m. - 7:00 p.m. (ET).');
+  });
+  it('should render without response properties', () => {
+    const form = {
       submission: {
         response: {}
       },
@@ -53,6 +106,7 @@ describe('hca <ConfirmationPage>', () => {
     );
     expect(tree.subTree('.confirmation-page-title').text()).to.contain('Claim received');
     expect(tree.everySubTree('p')[0].text()).to.contain('Normally processed within a week.');
+    expect(tree.subTree('.claim-list')).to.be.false;
     expect(tree.everySubTree('.confirmation-guidance-message')[0].text()).to.contain('Find out what happens after you apply.');
     expect(tree.everySubTree('.confirmation-guidance-message')[1].text()).to.contain('If you have questions, call 1-877-222-VETS (8387) and press 2, Monday - Friday, 8:00 a.m. - 7:00 p.m. (ET).');
   });
