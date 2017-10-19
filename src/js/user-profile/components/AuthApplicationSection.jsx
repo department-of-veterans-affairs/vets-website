ad2101dd03c63c62fd864163f5a18e4b1b85a8b7
Ben Shyong <ben.shyong@gmail.com>
2017-10-19 10:52:50 -0700

    copy change on user profile page

--- src/js/user-profile/components/AuthApplicationSection.jsx
+++ src/js/user-profile/components/AuthApplicationSection.jsx
@@ -27,7 +27,7 @@ class AuthApplicationSection extends React.Component {
           <p><span className="label">Your account will allow you to:</span></p>
           <p><a href="/health-care/apply">Apply for health care</a></p>
           <p><a href="/education/apply-for-education-benefits">Apply for education benefits</a></p>
-          <p><span className="label">You need to <a href="/verify?next=/profile">verify your account</a> in order to:</span></p>
+          <p><span className="label">You need to <a href="/verify?next=/profile">verify your identity</a> in order to:</span></p>
           <p>Refill your prescription</p>
           <p>Message your health care team</p>
           <p>Get your VA health records</p>
