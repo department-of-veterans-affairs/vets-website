52e05e72428173133468d7c2cc390f365c546197
Ben <bshyong@users.noreply.github.com>
2017-10-20 11:53:17 -0700

    timeout on VIC failure (#6645)

--- src/js/veteran-id-card/reducers/idcard.js
+++ src/js/veteran-id-card/reducers/idcard.js
@@ -1,13 +1,15 @@
 import {
   ATTRS_FETCHING,
   ATTRS_SUCCESS,
-  ATTRS_FAILURE
+  ATTRS_FAILURE,
+  REDIRECT_TIMED_OUT,
 } from '../actions';
 
 const initialState = {
   redirect: null,
   errors: null,
-  fetching: false
+  fetching: false,
+  vicError: false,
 };
 
 function idcard(state = initialState, action) {
@@ -15,7 +17,8 @@ function idcard(state = initialState, action) {
     case ATTRS_FETCHING:
       return {
         ...state,
-        fetching: true
+        fetching: true,
+        vicError: false,
       };
     case ATTRS_SUCCESS:
       return {
@@ -30,6 +33,11 @@ function idcard(state = initialState, action) {
         ...state,
         errors: action.errors,
         fetching: false
+      };
+    case REDIRECT_TIMED_OUT:
+      return {
+        ...initialState,
+        vicError: true,
       };
     default:
       return state;
