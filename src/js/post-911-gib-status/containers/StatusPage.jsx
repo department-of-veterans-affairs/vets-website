ff7c5e546ba775154a9bca1e98533ee3e5152bed
akainic <Anne.Kainic@va.gov>
2017-10-26 11:31:16 -0400

    Add missing period

--- src/js/post-911-gib-status/containers/StatusPage.jsx
+++ src/js/post-911-gib-status/containers/StatusPage.jsx
@@ -52,7 +52,7 @@ class StatusPage extends React.Component {
         <div>
           If you've received education benefit payments through this program, <a target="_blank"
             href="https://www.ebenefits.va.gov/ebenefits/about/feature?feature=payment-history">
-          you can see your payment history on eBenefits</a>
+          you can see your payment history on eBenefits</a>.
         </div>
         <EnrollmentHistory enrollmentData={enrollmentData}/>
         <div className="feature help-desk">
