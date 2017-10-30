a4ddb7242a0b3497b90f16703a03678e8c99abb9
Claire Hsu <claire@adhocteam.us>
2017-10-29 20:35:37 -0700

    Unnest ask-va-questions and consolidate neighboring classes

--- src/js/common/schemaform/AskVAQuestions.jsx
+++ src/js/common/schemaform/AskVAQuestions.jsx
@@ -4,13 +4,13 @@ function AskVAQuestions(props) {
   return (
     <div className="row">
       <div className="usa-width-two-thirds medium-8 columns">
-        <div className="ask-va-questions help-sidebar-box">
-          <h2 className="help-h2-border help-h2">Need Help?</h2>
+        <div className="help-sidebar-box">
+          <h2 className="help-heading">Need Help?</h2>
           {props.children}
           <p className="talk">To report a problem with this form,<br/>
           call the Vets.gov Technical Help Desk:</p>
           <p className="phone-number">
-            <a href="tel:+1-855-574-7286">1-855-574-7286</a><br/>
+            <a className="phone-number-link" href="tel:+1-855-574-7286">1-855-574-7286</a><br/>
             TTY: <a href="tel:+18008778339">1-800-877-8339</a><br/>
             Monday - Friday, 8:00 a.m. - 8:00 p.m. ET
           </p>
