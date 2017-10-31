2fe6a90bcd770780a8f269b9aa6df1e448ca8760
Claire Hsu <claire@adhocteam.us>
2017-10-31 11:00:06 -0700

    PR comment: provide more desciptive name for ask-va-questions replacements

--- src/js/claims-status/components/AskVAQuestions.jsx
+++ src/js/claims-status/components/AskVAQuestions.jsx
@@ -5,9 +5,9 @@ class AskVAQuestions extends React.Component {
     return (
       <div>
         <h2 className="help-heading">Need Help?</h2>
-        <p className="talk">Call Veterans Affairs Benefits and Services:</p>
-        <p className="phone-number">
-          <a className="phone-number-link" href="tel:+1-800-827-1000">1-800-827-1000</a><br/>
+        <p className="help-talk">Call Veterans Affairs Benefits and Services:</p>
+        <p className="help-phone-number">
+          <a className="help-phone-number-link" href="tel:+1-800-827-1000">1-800-827-1000</a><br/>
           Monday — Friday, 8:00 a.m. — 9:00 p.m. (ET)
         </p>
         <p><a href="https://iris.custhelp.com/">Submit a question to VA</a></p>
--- src/js/common/schemaform/AskVAQuestions.jsx
+++ src/js/common/schemaform/AskVAQuestions.jsx
@@ -7,10 +7,10 @@ function AskVAQuestions(props) {
         <div className="help-sidebar-box">
           <h2 className="help-heading">Need Help?</h2>
           {props.children}
-          <p className="talk">To report a problem with this form,<br/>
+          <p className="help-talk">To report a problem with this form,<br/>
           call the Vets.gov Technical Help Desk:</p>
-          <p className="phone-number">
-            <a className="phone-number-link" href="tel:+1-855-574-7286">1-855-574-7286</a><br/>
+          <p className="help-phone-number">
+            <a className="help-phone-number-link" href="tel:+1-855-574-7286">1-855-574-7286</a><br/>
             TTY: <a href="tel:+18008778339">1-800-877-8339</a><br/>
             Monday - Friday, 8:00 a.m. - 8:00 p.m. ET
           </p>
