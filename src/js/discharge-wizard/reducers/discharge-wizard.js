2bc93d241d79ba317a41ce5d27260b2566798381
Ben <bshyong@users.noreply.github.com>
2017-10-31 10:54:03 -0700

    Add discharge page entry point (#6694)
    * spike discharge wizard entry point; wip
    
    * adds discharge wizard entry page
    
    * address PR feedback

---
+++ src/js/discharge-wizard/reducers/discharge-wizard.js
@@ -0,0 +1,11 @@
+
+const initialState = {};
+
+function dischargeWizard(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default dischargeWizard;
---
+++ src/js/discharge-wizard/routes.jsx
@@ -0,0 +1,8 @@
+import DischargeWizardApp from './containers/DischargeWizardApp';
+
+const routes = {
+  path: '/',
+  component: DischargeWizardApp
+};
+
+export default routes;
