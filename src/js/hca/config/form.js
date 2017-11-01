939038edc6f4195653f92f2d61e8dd3789525c21
Jeff Balboni <jeff@adhocteam.us>
2017-11-01 10:00:00 -0400

    Fix linting error, add missing validation

--- src/js/hca/config/form.js
+++ src/js/hca/config/form.js
@@ -43,6 +43,7 @@ import { schema as addressSchema, uiSchema as addressUI } from '../../common/sch
 
 import { createDependentSchema, uiSchema as dependentUI, createDependentIncomeSchema, dependentIncomeUiSchema } from '../definitions/dependent';
 import currentOrPastDateUI from '../../common/schemaform/definitions/currentOrPastDate';
+import dateUI from '../../common/schemaform/definitions/date';
 import ssnUI from '../../common/schemaform/definitions/ssn';
 import currencyUI from '../../common/schemaform/definitions/currency';
 
@@ -366,10 +367,7 @@ const formConfig = {
             // TODO: this should really be a dateRange, but that requires a backend schema change. For now
             // leaving them as dates, but should change these to get the proper dateRange validation
             lastEntryDate: currentOrPastDateUI('Start of service period'),
-            lastDischargeDate: {
-              'ui:title': 'Date of discharge',
-              'ui:widget': 'date'
-            },
+            lastDischargeDate: dateUI('Date of discharge'),
             dischargeType: {
               'ui:title': 'Character of discharge',
               // TODO: Use a constant instead of a magic string
