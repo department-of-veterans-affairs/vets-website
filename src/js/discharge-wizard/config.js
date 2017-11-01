99d4b348e24f4b000fc7a1c9f0440d98e93801ed
Ben Shyong <ben.shyong@gmail.com>
2017-10-31 22:21:59 -0700

    add careful consideration content and question keys

---
+++ src/js/discharge-wizard/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  labels: {
+    drb: 'Discharge Review Board',
+    bcmr: 'Board for Correction of Military Records (BCMR)',
+    bcnr: 'Board for Correction of Naval Records (BCNR)',
+  }
+};
--- src/js/discharge-wizard/reducers/discharge-wizard.js
+++ src/js/discharge-wizard/reducers/discharge-wizard.js
@@ -1,5 +1,16 @@
 
-const initialState = {};
+const initialState = {
+  reason: null, // 1
+  dischargeType: null, // 1a
+  intention: null, // 1b
+  dischargeYear: null, // 2
+  dischargeMonth: null, // 2a
+  courtMartial: null, // 3
+  branchOfService: null, // 4
+  prevApplication: null, // 5
+  prevApplicationYear: null, // 5a
+  prevApplicationType: null, // 5b
+};
 
 function dischargeWizard(state = initialState, action) {
   switch (action.type) {
