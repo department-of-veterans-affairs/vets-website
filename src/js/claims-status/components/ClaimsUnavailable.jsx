7d08c3ba2fcc224729e08e16d64307e04f4a165e
Claire Hsu <claire@adhocteam.us>
2017-11-02 15:09:39 -0700

    Reorganize claims-status alerts structure

--- src/js/claims-status/components/ClaimsUnavailable.jsx
+++ src/js/claims-status/components/ClaimsUnavailable.jsx
@@ -4,9 +4,13 @@ class ClaimsUnavailable extends React.Component {
   render() {
     return (
       <div className="usa-alert usa-alert-warning claims-unavailable">
-        <h4 className="claims-alert-header">Claim status is unavailable</h4>
-        Vets.gov is having trouble loading claims information at this time. Please check back again in a hour.
-        Please note: You are still able to review appeals information.
+        <div className="usa-alert-body">
+          <h4 className="claims-alert-header">Claim status is unavailable</h4>
+          <p className="usa-alert-text">
+            Vets.gov is having trouble loading claims information at this time. Please check back again in a hour.
+            Please note: You are still able to review appeals information.
+          </p>
+        </div>
       </div>
     );
   }
