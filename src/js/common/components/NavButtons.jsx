96db7f16ce47728da2ac6a0c28b3558e6c57438e
peggygannon <peggy@thesocompany.com>
2017-11-02 11:44:26 -0700

    Content edit

--- src/js/common/components/NavButtons.jsx
+++ src/js/common/components/NavButtons.jsx
@@ -129,7 +129,7 @@ export default class NavButtons extends React.Component {
           <div className="usa-alert usa-alert-error">
             <div className="usa-alert-body">
               <p><strong>There is currently an issue with submitting this form. We apologize for the inconvenience.</strong></p>
-              <p><a href="tel:855-574-7286">1-855-574-7286</a>, TTY: <a href="tel:18008778339">1-800-877-8339</a>, Monday &#8211; Friday, 8:00 a.m. &#8211; 8:00 p.m. (ET).</p>
+              <p>Please call <a href="tel:855-574-7286">1-855-574-7286</a>, TTY: <a href="tel:18008778339">1-800-877-8339</a>, Monday &#8211; Friday, 8:00 a.m. &#8211; 8:00 p.m. (ET).</p>
             </div>
           </div>
         );
