fef46192f81684795c8ddff67609b94ab70521d6
Claire Hsu <claire@adhocteam.us>
2017-11-03 11:55:28 -0400

    Claims styles cleanup bug fix (#6722)
    * Provide left padding for alert header
    
    * Replace claim-file-border
    
    * Rename help-sidebar-box help-footer-box
    
    * Revert padding change
    
    * Reorganize claims-status alerts structure
    
    * Remove unused alert

--- src/js/claims-status/components/ClaimsUnauthorized.jsx
+++ src/js/claims-status/components/ClaimsUnauthorized.jsx
@@ -4,8 +4,12 @@ class ClaimsUnauthorized extends React.Component {
   render() {
     return (
       <div className="usa-alert usa-alert-warning claims-unavailable">
-        <h4 className="claims-alert-header">Sorry, your session has expired</h4>
-        You may need to refresh the page and sign in again.
+        <div className="usa-alert-body">
+          <h4 className="claims-alert-header">Sorry, your session has expired</h4>
+          <p className="usa-alert-text">
+            You may need to refresh the page and sign in again.
+          </p>
+        </div>
       </div>
     );
   }
