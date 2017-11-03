4d26653e1857b53f5ec944b595fcd92b60e9c735
Claire Hsu <claire@adhocteam.us>
2017-11-03 09:04:47 -0700

    Unbold all

--- src/js/common/schemaform/ApplicantDescription.jsx
+++ src/js/common/schemaform/ApplicantDescription.jsx
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default <p>You aren’t required to fill in <strong>all</strong> fields, but we can review your application faster if you provide more information.</p>;
+export default <p>You aren’t required to fill in all fields, but we can review your application faster if you provide more information.</p>;
