a9bfe4610fdf1cc03e013739ad70c3614e1fc2c8
Ben Shyong <ben.shyong@gmail.com>
2017-11-06 06:57:19 -0800

    question content

--- src/js/discharge-wizard/containers/DischargeWizardApp.jsx
+++ src/js/discharge-wizard/containers/DischargeWizardApp.jsx
@@ -1,12 +1,55 @@
+import { connect } from 'react-redux';
 import React from 'react';
 
+import { updateField } from '../actions';
+import ErrorableRadioButtons from '../../common/components/form-elements/ErrorableRadioButtons';
+
 class DischargeWizardApp extends React.Component {
+  handleChange() {
+
+  }
+
+
+  renderQuestionOne() {
+    const radioButtonProps = {
+      name: 'dateRange',
+      label: '',
+      options: [
+        { label: '1. I suffered from an undiagnosed, misdiagnosed, or untreated mental health condition including posttraumatic stress disorder (PTSD) in the service, and was discharged for reasons related to this condition.', value: 1 },
+        { label: '2. I suffered from an undiagnosed, misdiagnosed, or untreated Traumatic Brain Injury (TBI) in the service, and was discharged for reasons related to this condition.', value: 2 },
+        { label: '3. I was discharged due to homosexual conduct under Don’t Ask Don’t Tell (DADT) or preceding policies.', value: 3 },
+        { label: '4. I was the victim of sexual assault or harassment in the service, and was discharged for reasons related to this incident.', value: 4 },
+        { label: '5. I am transgender, and my discharge shows my birth name instead of my current name.', value: 5 },
+        { label: '6. There is an error on my discharge paperwork for other reasons.', value: 6 },
+        { label: '7. My discharge is unjust or unfair punishment for other reasons.', value: 7 },
+      ],
+      onValueChange: (v) => {
+        if (v.dirty) {
+          this.props.changeDateOption(v.value);
+          this.setState({
+            startDateError: null,
+            endDateError: null,
+          });
+        }
+      },
+      value: {
+        value: this.props.dischargeWizard['1_reason'],
+      }
+    };
+
+    return <ErrorableRadioButtons {...radioButtonProps}/>;
+  }
+
   render() {
     return (
-      <div>
+      <div className="discharge-wizard">
         <div className="row">
           <div className="columns small-12">
             <h1>Upgrading Your Discharge Status: What to know</h1>
+            <div className="medium-8">
+              <h4>Which of the following best describes why you want to change your discharge paperwork?</h4>
+              {this.renderQuestionOne()}
+            </div>
           </div>
         </div>
       </div>
@@ -14,4 +57,13 @@ class DischargeWizardApp extends React.Component {
   }
 }
 
-export default DischargeWizardApp;
+const mapStateToProps = (state) => {
+  return {
+    dischargeWizard: state.dischargeWizard,
+  };
+};
+const mapDispatchToProps = {
+  updateField,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DischargeWizardApp);
--- src/js/discharge-wizard/reducers/discharge-wizard.js
+++ src/js/discharge-wizard/reducers/discharge-wizard.js
@@ -2,16 +2,16 @@ import { DW_UPDATE_FIELD } from '../actions';
 import _ from 'lodash';
 
 const initialState = {
-  reason: null, // 1
-  dischargeType: null, // 1a
-  intention: null, // 1b
-  dischargeYear: null, // 2
-  dischargeMonth: null, // 2a
-  courtMartial: null, // 3
-  branchOfService: null, // 4
-  prevApplication: null, // 5
-  prevApplicationYear: null, // 5a
-  prevApplicationType: null, // 5b
+  '1_reason': null, // 1
+  '1_dischargeType': null, // 1a
+  '1_intention': null, // 1b
+  '2_dischargeYear': null, // 2
+  '2_dischargeMonth': null, // 2a
+  '3_courtMartial': null, // 3
+  '4_branchOfService': null, // 4
+  '5_prevApplication': null, // 5
+  '5_prevApplicationYear': null, // 5a
+  '5_prevApplicationType': null, // 5b
 };
 
 function dischargeWizard(state = initialState, action) {
