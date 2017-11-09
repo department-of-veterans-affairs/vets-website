f46adad761113bdcdd06173779929c08f539192f
cvalarida <chris.valarida@adhocteam.us>
2017-11-09 14:43:41 -0800

    Add Timeline for real this time

---
+++ src/js/claims-status/components/appeals-v2/Timeline.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Timeline is in charge of the past events and current status.
+ * TODO: Add the current status
+ */
+class Timeline extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false
+    };
+  }
+
+  getPastEvents() {
+    return this.props.events.map((event, index) => (
+      <li key={index}>
+        <h3>{event.title}</h3>
+        <span>{event.date}</span> {/* Need to format */}
+        <p>description here</p>
+      </li>
+    ));
+  }
+
+  toggleExpanded = () => {
+    this.setState({ expanded: !this.state.expanded });
+  }
+
+  render() {
+    const eventList = this.state.expanded ? this.getPastEvents() : [];
+    eventList.push(
+      <li key={eventList.length} className="list-group-item">
+        <h4><a href="#" onClick={this.toggleExpanded}>{this.state.expanded ? 'Hide past events' : 'Show past events'}</a></h4>
+        <span>Date range here</span>
+      </li>
+    );
+
+    return (
+      // May not want this as an ol...
+      <ul className="vertical-list-group">{eventList}</ul>
+    );
+  }
+}
+
+Timeline.propTypes = {
+  events: PropTypes.arrayOf(PropTypes.shape({
+    type: PropTypes.string,
+    date: PropTypes.string,
+    details: PropTypes.object
+  }))
+};
+
+export default Timeline;
+
