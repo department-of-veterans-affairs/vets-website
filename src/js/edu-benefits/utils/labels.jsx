5dd881dfbb285ead2e2e6f57e6784821532d07be
Claire Hsu <claire@adhocteam.us>
2017-11-09 21:49:27 -0800

    Update edu benefit labels

--- src/js/edu-benefits/utils/labels.jsx
+++ src/js/edu-benefits/utils/labels.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { states } from '../../common/utils/options-for-select';
 import { createUSAStateLabels } from '../../common/schemaform/helpers';
+import AdditionalInfo from '../../common/components/AdditionalInfo';
 
 export const chapterNames = {
   veteranInformation: 'Veteran Information',
@@ -15,12 +16,35 @@ export const chapterNames = {
 };
 
 export const benefitsLabels = {
-  chapter33: <p>Post-9/11 GI Bill (Chapter 33)<br/><a href="/education/gi-bill/post-9-11/" target="_blank">Learn more</a></p>,
-  chapter30: <p>Montgomery GI Bill (MGIB-AD, Chapter 30)<br/><a href="/education/gi-bill/montgomery-active-duty/" target="_blank">Learn more</a></p>,
-  chapter1606: <p>Montgomery GI Bill Selected Reserve (MGIB-SR, Chapter 1606)<br/><a href="/education/gi-bill/montgomery-selected-reserve/" target="_blank">Learn more</a></p>,
-  chapter32: <p>Post-Vietnam Era Veterans’ Educational Assistance Program<br/>(VEAP, Chapter 32)<br/><a href="/education/other-educational-assistance-programs/veap/" target="_blank">Learn more</a></p>,
-  chapter1607: <p>Reserve Educational Assistance Program (REAP, Chapter 1607)<br/><a href="/education/other-educational-assistance-programs/reap/" target="_blank">Learn more</a></p>,
-  transferOfEntitlement: <p>Transfer of Entitlement Program (TOE)<br/><a href="/education/gi-bill/transfer/" target="_blank">Learn more</a></p>
+  chapter33: <p>Post-9/11 GI Bill (Chapter 33)<br/><AdditionalInfo triggerText="Learn more.">
+      If you served on active duty after September 10, 2001, you may qualify for Post-9/11 GI Bill education benefits—like money for tuition, housing, books, and supplies. <a href="/education/gi-bill/post-9-11/" target="_blank">Find out if you’re eligible for the Post-9/11 GI Bill.</a>
+  </AdditionalInfo>
+  </p>,
+  chapter30: <p>Montgomery GI Bill (MGIB-AD, Chapter 30)<br/>
+    <AdditionalInfo triggerText="Learn more.">
+       If you served at least 2 years on active duty, you may qualify for MGIB-AD benefits—like money for tuition or training. <a href="/education/gi-bill/montgomery-active-duty/" target="_blank">Find out if you’re eligible for the MGIB-AD program.</a>
+   </AdditionalInfo>
+  </p>,
+  chapter1606: <p>Montgomery GI Bill Selected Reserve (MGIB-SR, Chapter 1606)<br/>
+    <AdditionalInfo triggerText="Learn more.">
+      If you served in the Select Reserve for at least 6 years, you may qualify for up to 36 months of education and training benefits under the MGIB-SR program. <a href="/education/gi-bill/montgomery-selected-reserve/" target="_blank">Find out if you’re eligible for MGIB-SR benefits.</a>
+    </AdditionalInfo>
+  </p>,
+  chapter32: <p>Post-Vietnam Era Veterans’ Educational Assistance Program<br/>(VEAP, Chapter 32)<br/>
+    <AdditionalInfo triggerText="Learn more.">
+    You can continue your education by contributing part of your military pay to help cover the cost of school. The government will match $2 for every $1 you contribute. <a href="/education/other-educational-assistance-programs/veap/" target="_blank">Find out if you can get benefits through VEAP.</a>
+    </AdditionalInfo>
+  </p>,
+  chapter1607: <p>Reserve Educational Assistance Program (REAP, Chapter 1607)<br/>
+    <AdditionalInfo triggerText="Learn more.">
+      REAP ended on November 25, 2015, but some REAP benefits will remain in place until November 2019. The Post-9/11 GI Bill has replaced REAP for Reserve and National Guard members called to active duty on or after September 11, 2001. If you’d like to stop using REAP and use the Post-9/11 GI Bill instead, call 1-888-GI-BILL-1 (1-888-442-4551). <a href="/education/other-educational-assistance-programs/reap/" target="_blank">Learn more about REAP benefits.</a>
+    </AdditionalInfo>
+  </p>,
+  transferOfEntitlement: <p>Transfer of Entitlement Program (TOE)<br/>
+    <AdditionalInfo triggerText="Learn more.">
+      If you don’t use all of your GI Bill education benefits, you can transfer some of your unused benefits to your spouse or dependent children. You must request to transfer benefits while on active duty. <a href="/education/gi-bill/transfer/" target="_blank">Find out how to transfer your education benefits.</a>
+    </AdditionalInfo>
+  </p>
 };
 
 // The links and labels are different from the above
