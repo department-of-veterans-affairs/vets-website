ad978071a793b77fb30ba575ed2f4084a46a1ab5
Jeff Balboni <jeff@adhocteam.us>
2017-11-30 13:18:46 -0500

    Update test

--- test/common/schemaform/routing.unit.spec.js
+++ test/common/schemaform/routing.unit.spec.js
@@ -1,62 +1,67 @@
 import { expect } from 'chai';
 
-describe('Schemaform routing', () => {
-  describe('getNextPage', () => {
-    xit('should match current page correctly when an array page', () => {
-      const route = {
-        pageConfig: {
-          pageKey: 'testPage',
-          showPagePerItem: true,
-          arrayPath: 'arrayProp',
-          errorMessages: {},
-          title: '',
-          path: '/testing/:index'
-        },
-        pageList: [
-          {
-            pageKey: 'blah',
-            path: 'a-path'
-          },
-          {
-            pageKey: 'testPage',
-            path: '/testing/:index',
-            showPagePerItem: true,
-            arrayPath: 'arrayProp'
-          }
-        ]
-      };
-      const form = {
-        pages: {
-          testPage: {
-            schema: {
-              properties: {
-                arrayProp: {
-                  items: [{}]
-                }
-              }
-            },
-            uiSchema: {
-              arrayProp: {
-                items: {}
-              }
-            }
-          }
-        },
-        data: {
-          arrayProp: [{}]
-        }
-      };
-      const user = {
-        profile: {
-          savedForms: []
-        },
-        login: {
-          currentlyLoggedIn: false
-        }
-      };
+import { getNextPage, getPreviousPage } from '../../../src/js/common/schemaform/routing';
 
-      const page = getNextPage(pageList, data, pathname, pageConfig);
-      expect(page).to.equal(1);
-    });
+describe('Schemaform routing', () => {
+  it('getNextPage should get next page when it\'s an array page', () => {
+    const pageConfig = {
+      pageKey: 'testPage',
+      showPagePerItem: true,
+      arrayPath: 'arrayProp',
+      errorMessages: {},
+      title: '',
+      path: '/testing/:index'
+    };
+
+    const pageList = [
+      {
+        pageKey: 'blah',
+        path: 'a-path'
+      },
+      {
+        pageKey: 'testPage',
+        path: '/testing/:index',
+        showPagePerItem: true,
+        arrayPath: 'arrayProp'
+      }
+    ];
+    const data = {
+      arrayProp: [{}]
+    };
+    const pathname = 'a-path';
+
+    const page = getNextPage(pageList, data, pathname, pageConfig);
+    expect(page).to.equal('/testing/0');
+  });
+
+  it('getPreviousPage should get previous page when on an array page', () => {
+    const pageConfig = {
+      pageKey: 'testPage',
+      showPagePerItem: true,
+      arrayPath: 'arrayProp',
+      errorMessages: {},
+      title: '',
+      path: '/testing/:index'
+    };
+
+    const pageList = [
+      {
+        pageKey: 'blah',
+        path: 'a-path'
+      },
+      {
+        pageKey: 'testPage',
+        path: '/testing/:index',
+        showPagePerItem: true,
+        arrayPath: 'arrayProp'
+      }
+    ];
+    const data = {
+      arrayProp: [{}]
+    };
+    const pathname = '/testing/0';
+
+    const page = getPreviousPage(pageList, data, pathname, pageConfig);
+    expect(page).to.equal('a-path');
   });
 });
