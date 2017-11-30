c820645ad33513b56c5f0e07a5b6ff5b33b4ee88
Ben Shyong <ben.shyong@gmail.com>
2017-11-30 13:31:41 -0800

    set up URL for production

--- src/js/discharge-wizard/routes.jsx
+++ src/js/discharge-wizard/routes.jsx
@@ -1,12 +1,14 @@
 import DischargeWizardApp from './DischargeWizardApp';
 import GuidancePage from './containers/GuidancePage';
 import FormPage from './containers/FormPage';
+import InstructionsPage from './components/InstructionsPage';
 
 const routes = {
   path: '/',
   component: DischargeWizardApp,
-  indexRoute: { component: FormPage },
+  indexRoute: { component: InstructionsPage },
   childRoutes: [
+    { path: 'questions', component: FormPage },
     { path: 'guidance', component: GuidancePage },
   ],
 };
