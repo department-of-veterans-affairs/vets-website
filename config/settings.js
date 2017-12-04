51f8963c48210d874e8921ca0b8c3cce9bbc6244
Nick Sullivan <nick.sullivan@adhocteam.us>
2017-12-04 18:56:15 -0500

    Move process vars into process property

--- config/settings.js
+++ config/settings.js
@@ -1,9 +1,11 @@
 // These values will become globally available under window.settings in a totally static file at /js/settings.js.
 // Post-build, it can be edited in {build_directory}/js/settings.js.
 const settings = {
-  NODE_ENV: process.env.NODE_ENV,
-  API_URL: process.env.API_URL,
-  BASE_URL: process.env.BASE_URL,
+  process: {
+    NODE_ENV: process.env.NODE_ENV,
+    API_URL: process.env.API_URL,
+    BASE_URL: process.env.BASE_URL
+  },
   vic: {
     rateLimit: 0.1
   }
