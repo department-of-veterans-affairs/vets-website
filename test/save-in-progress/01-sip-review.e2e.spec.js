c053c8664d621aa6f4ea40fd18d144228f32c974
Jeff Balboni <jeff@adhocteam.us>
2017-12-05 16:13:01 -0500

    Move tests

---
+++ test/save-in-progress/01-sip-review.e2e.spec.js
@@ -0,0 +1,53 @@
+const E2eHelpers = require('../e2e/e2e-helpers');
+const Timeouts = require('../e2e/timeouts.js');
+const HcaHelpers = require('../e2e/hca-helpers.js');
+
+module.exports = E2eHelpers.createE2eTest(
+  (client) => {
+    const url = `${E2eHelpers.baseUrl}/health-care/apply/application`;
+    const reviewUrl = `${url}/review-and-submit`;
+    const token = HcaHelpers.initSaveInProgressMock(url, client);
+
+    // Ensure introduction page renders.
+    client
+      .url(reviewUrl)
+      .waitForElementVisible('body', Timeouts.normal)
+      .assert.title('Apply for Health Care: Vets.gov')
+      .waitForElementVisible('.schemaform-chapter-accordion-header', Timeouts.slow);  // First render of React may be slow.
+
+    E2eHelpers.overrideVetsGovApi(client);
+    E2eHelpers.overrideSmoothScrolling(client);
+
+    client
+      .click('.schemaform-chapter-accordion-header:first-child > .usa-button-unstyled')
+      .click('.edit-btn')
+      .fill('input[name="root_veteranFullName_first"]', 'Jane')
+      .pause(1500);
+
+    // client
+    //   .expect.element('.saved-success-container').to.be.visible;
+
+    // save and finish a form later
+    client
+      .click('.schemaform-sip-save-link');
+
+    E2eHelpers.expectNavigateAwayFrom(client, '/review-and-submit');
+    client.assert.urlContains('form-saved');
+
+    client
+      .url(reviewUrl)
+      .waitForElementVisible('body', Timeouts.normal)
+      .mockData({
+        path: '/v0/in_progress_forms/1010ez',
+        verb: 'put',
+        value: {},
+        status: 500
+      }, token)
+      .click('.schemaform-sip-save-link');
+
+    client.assert.urlContains('review-and-submit');
+
+    client.expect.element('.usa-alert-error').text.to.contain('We’re sorry. Something went wrong when saving your form');
+
+    client.end();
+  });
