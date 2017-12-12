167ee2c3701819af908f810150fd7e6d533fe868
Nick Sullivan <nick.sullivan@adhocteam.us>
2017-12-12 11:53:22 -0500

    Add DefaultView test cases

---
+++ test/feedback/components/DefaultView.unit.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import enzyme from 'enzyme';
+import chai from 'chai';
+
+import DefaultView from '../../../src/js/feedback/components/DefaultView';
+
+const props = {
+  feedbackButtonClicked() {}
+};
+
+describe('<DefaultView/>', () => {
+
+  it('should render', () => {
+    const wrapper = enzyme.shallow(<DefaultView {...props}/>);
+    const text = wrapper.text();
+    chai.assert.isTrue(text.includes('Tell us what you think'), 'The title was rendered.');
+  });
+
+});
