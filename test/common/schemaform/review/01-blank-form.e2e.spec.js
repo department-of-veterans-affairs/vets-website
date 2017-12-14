f3f406df92f7ef24c3f1cc2b4de483c3c12a5cb2
akainic <Anne.Kainic@va.gov>
2017-12-14 15:30:36 -0500

    Add another test

--- test/common/schemaform/review/01-blank-form.e2e.spec.js
+++ test/common/schemaform/review/01-blank-form.e2e.spec.js
@@ -9,25 +9,41 @@ module.exports = E2eHelpers.createE2eTest(
 
     // Ensure introduction page renders.
     client
-      .url(`${E2eHelpers.baseUrl}/health-care/apply/application`)
+      .url(`${E2eHelpers.baseUrl}/health-care/apply/application/review-and-submit`)
       .waitForElementVisible('body', Timeouts.normal)
       .assert.title('Apply for Health Care: Vets.gov')
-      .waitForElementVisible('.schemaform-title', Timeouts.slow)  // First render of React may be slow.
-      .click('.usa-button-primary');
+      .waitForElementVisible('.schemaform-title', Timeouts.slow);  // First render of React may be slow.
 
     E2eHelpers.overrideVetsGovApi(client);
     E2eHelpers.overrideSmoothScrolling(client);
-    E2eHelpers.expectNavigateAwayFrom(client, '/introduction');
 
-    // // Complete entire form
-    // HcaHelpers.completeEntireForm(client, testData.data);
+    // Open first panel
+    client
+      .waitForElementVisible('.usa-accordion-bordered', Timeouts.normal)
+      .click('button.usa-button-unstyled')
+      .pause(1200);
+    client.expect.element('.edit-btn').to.be.visible;
 
-    // // Open first panel
-    // client
-    //   .waitForElementVisible('.usa-accordion-bordered', Timeouts.normal)
-    //   .click('button.usa-button-unstyled')
-    //   .pause(1200);
-    // client.expect.element('.edit-btn').to.be.visible;
+    // Try to save form with validation errors
+    client
+      .waitForElementVisible('.edit-btn', Timeouts.normal)
+      .click('.edit-btn');
+    client.expect.element('input[name="root_veteranFullName_first"]').to.be.visible;
+
+    client
+      .waitForElementVisible('.form-review-panel-page .usa-button-primary', Timeouts.normal)
+      .click('.form-review-panel-page .usa-button-primary');
+
+    client.expect.element('.usa-input-error').to.be.visible;
+    client.expect.element('input[name="root_veteranFullName_first"]').to.be.visible;
+
+    // Fix validation errors and save successfully
+    client
+      .fill('input[name="root_veteranFullName_first"]', 'Jean-Pierre')
+      .fill('input[name="root_veteranFullName_last"]', 'Robespierre')
+      .click('.form-review-panel-page .usa-button-primary');
+
+    client.expect.element('.review-row span').text.to.equal('Jean-Pierre');
 
     // // Edit form fields successfully
     // client
@@ -40,31 +56,6 @@ module.exports = E2eHelpers.createE2eTest(
     //   .click('.usa-button-primary');
 
     // client.expect.element('.review-row span').text.to.equal('Jacques');
-
-    // // Edit form fields unsuccessfully
-    // client.waitForElementVisible('.edit-btn', Timeouts.normal);
-    // client
-    //   .click('.edit-btn');
-    // client.expect.element('input[name="root_veteranFullName_first"]').to.be.visible;
-
-    // client
-    //   .removeText('input[name="root_veteranFullName_first"]')
-    //   .pause(1200)
-    //   .click('input[name="root_veteranFullName_middle"]')
-    //   .pause();
-
-    // client
-    //   .click('.usa-button-primary');
-
-    // client.expect.element('.usa-input-error').to.be.visible;
-    // client.expect.element('input[name="root_veteranFullName_first"]').to.be.visible;
-
-    // // Fix validation errors and save successfully
-    // client
-    //   .fill('input[name="root_veteranFullName_first"]', 'Jean-Pierre')
-    //   .click('.usa-button-primary');
-
-    // client.expect.element('.review-row span').text.to.equal('Jean-Pierre');
 
     // // Close panel
     // client.expect.element('.edit-btn').to.be.visible;
@@ -81,14 +72,16 @@ module.exports = E2eHelpers.createE2eTest(
       // .axeCheck('.main')
       .click('.form-progress-buttons .usa-button-primary');
 
-    E2eHelpers.expectNavigateAwayFrom(client, '/review-and-submit');
-    client.expect.element('.js-test-location').attribute('data-location')
-      .to.not.contain('/review-and-submit').before(Timeouts.slow);
+    client.expect.element('.usa-alert').to.be.visible;
 
-    // Submit message
-    client.expect.element('.confirmation-page-title').to.be.visible;
+    // E2eHelpers.expectNavigateAwayFrom(client, '/review-and-submit');
+    // client.expect.element('.js-test-location').attribute('data-location')
+    //   .to.not.contain('/review-and-submit').before(Timeouts.slow);
 
-    client.axeCheck('.main');
+    // // Submit message
+    // client.expect.element('.confirmation-page-title').to.be.visible;
+
+    // client.axeCheck('.main');
 
     client.end();
   });
