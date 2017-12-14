e6c3e98955fb595306b45430428a1a9b29b3b51a
Nick Sullivan <nick.sullivan@adhocteam.us>
2017-12-14 14:30:19 -0500

    Remove ternary

--- src/js/feedback/components/FeedbackSubmitted.jsx
+++ src/js/feedback/components/FeedbackSubmitted.jsx
@@ -4,9 +4,9 @@ function FeedbackSubmitted({ shouldSendResponse }) {
   return (
     <div id="feedback-submitted">
       <h4 className="feedback-widget-title">Thank you for your feedback!</h4>
-      {shouldSendResponse ? (
+      {shouldSendResponse && (
         <p className="feedback-widget-intro">We'll get back to you soon.</p>
-      ) : null}
+      )}
     </div>
   );
 }
