9d16ba1ad11987a698ab7d18959e6976d78fc788
Ben <bshyong@users.noreply.github.com>
2017-12-16 03:59:55 +0800

    add discharge wizard to production ignorelist (#6970)

--- script/build.js
+++ script/build.js
@@ -95,6 +95,7 @@ const ignore = require('metalsmith-ignore');
 const ignoreList = [];
 if (options.buildtype === 'production') {
   ignoreList.push('burials-and-memorials/pre-need/form-10007-apply-for-eligibility.md');
+  ignoreList.push('discharge-upgrade-instructions/index.md');
 }
 smith.use(ignore(ignoreList));
 
