be5649e2bc529a50c5dd1cc05ebc72f34256fccd
Nick Sullivan <ncksllvn@gmail.com>
2017-12-21 16:03:18 -0500

    Revert feedback widget (delayed til after the holidays) (#6993)
    * Change to include to USWDS typography
    
    * Spruce up HTML to match USWDS markup
    
    * Bump uswds to 1.4.2
    
    * Change USWDS import order
    
    * Add Bitter
    
    * Add Bitter
    
    * Fix Bitter not showing up
    
    * Remove reference to Roboto Slab
    
    * Define bold Bitter
    
    * Remove font-size on .feature
    
    * Consolidate header style for process steps
    
    * Remove commented code
    
    * Fix h6 style
    
    * Remove margin-top on h1
    
    * add a comment
    
    * Remove margin top from modal titles
    
    * remove margin-top from header sign-in modal
    
    * Switch header to usa-font-lead
    
    * Remove margin-bottom on tabs
    
    * Default table width to 100%
    
    * Sprucing up GI profile page
    
    * Use sans-serif font on search results cards
    
    * FIxing margin-top problems
    
    * Remove margin-top from alert heading
    
    * Fixes for alert dialog headings margin-top
    
    * Remove borderless table class
    
    * H6 adjustments
    
    * Move font-faces into their own file
    
    * Add usa-font-lead styles to va-introtext
    
    * Add borderless tables back
    
    * Remove top margins
    
    * Implement helper function for rendering all site-wide components
    
    * Add space before bracket
    
    * Initial start to feedback widget
    
    * Getting started
    
    * Switch to Foundation grid
    
    * Add redux flow
    
    * Finish auto-merge with master
    
    * Define component appearance
    
    * Add the new actions, reducers, and general prop flow
    
    * Fix for label appearing in wrong spot on mobile
    
    * Fix lint errors
    
    * Hooking up to the API
    
    * Change parameter name to description
    
    * Add validation
    
    * Fix linter
    
    * Switch to api helper method
    
    * Add action tests
    
    * Linter
    
    * Add reducer tests
    
    * Add DefaultView test cases
    
    * Add FeedbackSubmitted test case
    
    * Add additional test case for FeedbackSubmitted
    
    * Add FeedbackForm test cases
    
    * Add Main test cases
    
    * Override Authorization header so that mock requests will work without login
    
    * Add ID for e2e tests to target
    
    * Add e2e tests
    
    * Add some margin below the alert banner
    
    * Change extensions to jsx
    
    * Change fn name to match file
    
    * Change fn name to match file
    
    * Change click handler to reavealForm
    
    * Remove unneeded state
    
    * Use plain object for mapDispatchToProps
    
    * Move everything into Redux state
    
    * Convert into a plain function
    
    * Remove unneeded parameter
    
    * Add errors into props; switch to errorable inputs
    
    * Fix test cases
    
    * Make selector more specific
    
    * Flattening CSS
    
    * Trying to clean up CSS
    
    * Fix for toggling email checkbox enabling the submit btn
    
    * Change tests to expect syntax
    
    * Suppress errors until blur...
    
    * Suppress errors until blur...
    
    * Add errorable checkbox
    
    * Clean up logic for formIsSubmittable
    
    * Cleaning up formIsSubmittable again...
    
    * Fix tests
    
    * Remove newline
    
    * Fix tests
    
    * Remove ternary
    
    * Remove getState
    
    * Fix tests...again
    
    * Fix e2e test selector
    
    * Fixes from code reviews
    
    * Fixes for using tab and screen readers
    
    * Change apostrophe
    
    * Fixing test cases after refactor
    
    * Revert "Add user feedback form above footer (#6815)"
    
    This reverts commit 5ee062362552684b1f91a864f9e110e31bf1a596.

--- src/js/common/init-common.js
+++ src/js/common/init-common.js
@@ -1,10 +1,8 @@
 import createLoginWidget from '../login/login-entry';
-import createFeedbackWidget from '../feedback/feedback-entry';
 import createCommonStore from './store';
 
 export function renderCommonComponents(commonStore) {
   createLoginWidget(commonStore);
-  createFeedbackWidget(commonStore);
 }
 
 export default function initCommon(reducer) {
