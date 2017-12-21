9ceb325b6b70b99fccb2a779533f65d927312729
Sophia Dengo <sdengo@gmail.com>
2017-12-21 09:14:34 -0500

    Update BenefitSelectionWarning.jsx

--- src/js/edu-benefits/5490/components/BenefitSelectionWarning.jsx
+++ src/js/edu-benefits/5490/components/BenefitSelectionWarning.jsx
@@ -7,7 +7,7 @@ export default function BenefitSelectionWarning(chapter, relationship) {
     if (relationship === 'child') {
       warningText = 'I understand that I am choosing to receive Fry Scholarship benefits instead of DEA benefits for which I am currently eligible based on the death of the qualifying individual on this application. I understand that even after this choice I will continue to be eligible for DEA benefits if the eligibility is based on the death of the qualifying individual on this application, based on a parent who has a permanent and total service-connected disability, or based on any other criteria as listed in 38 U.S.C. § 3501(a)(1).';
     } else {
-      // spous
+      // spouse
       warningText = 'I understand that I am choosing to receive Fry Scholarship benefits instead of any DEA benefits for which I am currently eligible. This includes DEA benefits based on the death of the qualifying individual on this application, based on the death of any other individuals not listed on this application, based on a spouse who has a permanent and total service-connected disability, or based on any other criteria as listed in 38 U.S.C. § 3501(a)(1).';
     }
   } else {
