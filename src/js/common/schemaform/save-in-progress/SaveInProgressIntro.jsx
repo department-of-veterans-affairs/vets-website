8d66f2a0c3a66118e1899442b7fc3a6c25a40f30
Claire Hsu <clairehsu86@gmail.com>
2017-12-24 18:58:16 -0500

    PR comment: Remove errant space

--- src/js/common/schemaform/save-in-progress/SaveInProgressIntro.jsx
+++ src/js/common/schemaform/save-in-progress/SaveInProgressIntro.jsx
@@ -40,7 +40,7 @@ export default class SaveInProgressIntro extends React.Component {
             <div>
               <div className="usa-alert usa-alert-info schemaform-sip-alert">
                 <div className="usa-alert-body">
-                  <strong>Note:</strong>  If you’re signed in to your account, we can prefill part of your application based on your account details. You can also save your form in progress, and come back later to finish filling it out. You have 60 days from the date you start your pension application to submit the form. After 60 days, the form won't be saved and you'll need to start over.
+                  <strong>Note:</strong> If you’re signed in to your account, we can prefill part of your application based on your account details. You can also save your form in progress, and come back later to finish filling it out. You have 60 days from the date you start your pension application to submit the form. After 60 days, the form won't be saved and you'll need to start over.
                 </div>
               </div>
               <br/>
