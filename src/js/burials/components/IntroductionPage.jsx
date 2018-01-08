72434c6da0c85e5da7f7779753012e8567c6d278
Jeff Balboni <jeff@adhocteam.us>
2018-01-08 13:25:15 -0500

    Fix link

--- src/js/burials/components/IntroductionPage.jsx
+++ src/js/burials/components/IntroductionPage.jsx
@@ -58,7 +58,7 @@ class IntroductionPage extends React.Component {
             </li>
             <li className="process-step list-two">
               <div><h5>Apply</h5></div>
-              <p>Complete an Application for Burial Benefits (VA Form 21P-530).<br/><a href="/pension/apply/">Download VA Form 21P-530</a>.</p>
+              <p>Complete an Application for Burial Benefits (VA Form 21P-530).<br/><a href="https://www.vba.va.gov/pubs/forms/VBA-21P-530-ARE.pdf">Download VA Form 21P-530</a>.</p>
               <p>Mail the application and other paperwork to your local regional benefit office.<br/><a href="/facilities">Find your local regional benefit office</a>.</p>
             </li>
             <li className="process-step list-three">
