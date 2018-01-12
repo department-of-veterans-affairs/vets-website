dbe0f3b3eb30a52076f262b995ac2f1d00751a1b
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-01-12 13:16:31 -0500

    Implement on HCA

--- src/js/hca/HealthCareApp.jsx
+++ src/js/hca/HealthCareApp.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import DowntimeNotification, { services } from '../common/containers/DowntimeNotification';
 import FormApp from '../common/schemaform/containers/FormApp';
 import formConfig from './config/form';
 
 export default function HealthCareEntry({ location, children }) {
   return (
-    <FormApp formConfig={formConfig} currentLocation={location}>
-      {children}
-    </FormApp>
+    <DowntimeNotification appTitle="Healthcare Application" dependencies={[services.es]}>
+      <FormApp formConfig={formConfig} currentLocation={location}>
+        {children}
+      </FormApp>
+    </DowntimeNotification>
   );
 }
