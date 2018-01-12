ee44684794cff3c82a7089d3f71e74ac56951bf4
Jeff Balboni <jeff@adhocteam.us>
2018-01-12 12:51:07 -0500

    Fix name

--- src/js/vic-v2/routes.jsx
+++ src/js/vic-v2/routes.jsx
@@ -1,10 +1,10 @@
 import { createRoutes as createFormRoutes } from '../common/schemaform/helpers';
 import formConfig from './config/form';
-import Chapter36App from './VeteranIDCardApp';
+import VeteranIDCardApp from './VeteranIDCardApp';
 
 const route = {
   path: '/',
-  component: Chapter36App,
+  component: VeteranIDCardApp,
   indexRoute: { onEnter: (nextState, replace) => replace('/introduction') },
   childRoutes: createFormRoutes(formConfig)
 };
