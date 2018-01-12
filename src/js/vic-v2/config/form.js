35d4d883e5845642e89eaa5cbc22808dcac4dc75
Jeff Balboni <jbalboni@gmail.com>
2018-01-12 13:28:42 -0500

    Add VIC scaffolding (#7089)

---
+++ src/js/vic-v2/config/form.js
@@ -0,0 +1,47 @@
+// import _ from 'lodash/fp';
+
+// import { transform } from '../helpers';
+// import fullSchemaVIC from 'vets-json-schema/dist/VIC-schema.json';
+
+import IntroductionPage from '../components/IntroductionPage';
+import ConfirmationPage from '../containers/ConfirmationPage';
+
+// const { } = fullSchemaVIC.properties;
+//
+// const { } = fullSchemaVIC.definitions;
+
+const formConfig = {
+  urlPrefix: '/',
+  submitUrl: '/v0/vic',
+  trackingPrefix: 'veteran-id-card-',
+  introduction: IntroductionPage,
+  confirmation: ConfirmationPage,
+  formId: 'VIC',
+  version: 0,
+  prefillEnabled: true,
+  savedFormMessages: {
+    notFound: 'Please start over to apply for a veteran id card.',
+    noAuth: 'Please sign in again to resume your application for a veteran id card.'
+  },
+  title: 'Apply for veteran id card',
+  defaultDefinitions: {},
+  chapters: {
+    applicantInformation: {
+      title: 'Applicant Information',
+      pages: {
+      }
+    },
+    contactInformation: {
+      title: 'Contact Information',
+      pages: {
+      }
+    },
+    documentUpload: {
+      title: 'Document Upload',
+      pages: {
+      }
+    }
+  }
+};
+
+export default formConfig;
