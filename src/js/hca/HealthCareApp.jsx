df0fead187af259771ae791e8eabbef030001553
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-01-12 15:39:37 -0500

    Revert "Implement on HCA"
    This reverts commit dbe0f3b3eb30a52076f262b995ac2f1d00751a1b.

--- src/js/hca/HealthCareApp.jsx
+++ src/js/hca/HealthCareApp.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
-import DowntimeNotification, { services } from '../common/containers/DowntimeNotification';
 import FormApp from '../common/schemaform/containers/FormApp';
 import formConfig from './config/form';
 
 export default function HealthCareEntry({ location, children }) {
   return (
-    <DowntimeNotification appTitle="Healthcare Application" dependencies={[services.es]}>
-      <FormApp formConfig={formConfig} currentLocation={location}>
-        {children}
-      </FormApp>
-    </DowntimeNotification>
+    <FormApp formConfig={formConfig} currentLocation={location}>
+      {children}
+    </FormApp>
   );
 }
