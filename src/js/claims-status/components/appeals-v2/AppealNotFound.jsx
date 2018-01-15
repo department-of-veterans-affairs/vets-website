885dd5ce9e1b09174f1dfa8801df8bdf9ec4b332
Mark Greenburg <marek.greenburg@gmail.com>
2018-01-15 13:19:11 -0600

    Update apostrophe to fancy apostrophe

--- src/js/claims-status/components/appeals-v2/AppealNotFound.jsx
+++ src/js/claims-status/components/appeals-v2/AppealNotFound.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 const AppealNotFound = () => {
   return (
     <div className="row">
-      <h1>Sorry, we couldn't find that appeal.</h1>
+      <h1>Sorry, we couldn’t find that appeal.</h1>
       <Link to="your-claims">Go back to your claims and appeals</Link>
     </div>
   );
