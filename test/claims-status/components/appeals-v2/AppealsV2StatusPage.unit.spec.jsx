b903caa9c8ccae68a614a088866dbd90f80aa59c
Mark Greenburg <marek.greenburg@gmail.com>
2018-01-16 12:31:51 -0600

    boilerplate

---
+++ test/claims-status/components/appeals-v2/AppealsV2StatusPage.unit.spec.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+describe('<AppealsV2StatusPage/>', () => {
+  it('should render', () => {
+  });
+});
