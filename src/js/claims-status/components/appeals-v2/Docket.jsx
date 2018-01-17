2765b62c8a35b9d37cd27d902215a171018261a1
Christopher Valarida <chris.valarida@adhocteam.us>
2018-01-16 16:47:10 -0800

    Round 1

--- src/js/claims-status/components/appeals-v2/Docket.jsx
+++ src/js/claims-status/components/appeals-v2/Docket.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 class Docket extends React.Component {
   render() {
-    return (null);
+    const { ahead, ready } = this.props;
+
+    // TODO: Assess how accessible this is
+    return (
+      <div className="docket-container">
+        <span className="appeals-ahead">{ahead.toLocaleString()}</span>
+        <p>Appeals ahead of you</p>
+        <div className="docket-bar">
+          <div className="completed" style={{ width: `${ahead / ready}%` }}/>
+        </div>
+      </div>
+    );
   }
 }
 
