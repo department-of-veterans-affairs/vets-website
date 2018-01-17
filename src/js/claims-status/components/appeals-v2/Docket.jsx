6b89480582801756e019f2628395f1c34927c77d
Christopher Valarida <chris.valarida@adhocteam.us>
2018-01-17 14:37:43 -0800

    Actually add the test...and turn it to a function

--- src/js/claims-status/components/appeals-v2/Docket.jsx
+++ src/js/claims-status/components/appeals-v2/Docket.jsx
@@ -1,42 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Docket extends React.Component {
-  render() {
-    const { ahead, total } = this.props;
-    const completedWidth = { width: `${((total - ahead) / total) * 100}%` };
+function Docket({ ahead, total }) {
+  const completedWidth = { width: `${((total - ahead) / total) * 100}%` };
 
-    // TODO: Assess how accessible this is
-    return (
-      <div className="docket-container">
-        <span className="appeals-ahead">{ahead.toLocaleString()}</span>
-        <p>Appeals ahead of you</p>
+  // TODO: Assess how accessible this is
+  return (
+    <div className="docket-container">
+      <span className="appeals-ahead">{ahead.toLocaleString()}</span>
+      <p>Appeals ahead of you</p>
 
-        <div className="marker-container">
-          <div>
-            <div className="marker-text-spacer" style={completedWidth}/>
-            <span className="marker-text">You are here</span>
-          </div>
-          <div>
-            <div className="spacer" style={completedWidth}/>
-            <div className="marker">
-              <img src="/img/Docket-line-pin.svg" alt=""/>
-            </div>
-          </div>
-        </div>
-
+      <div className="marker-container">
         <div>
-          <div className="docket-bar">
-            <div className="completed" style={completedWidth}/>
-          </div>
-          <div className="end-of-docket"/>
+          <div className="marker-text-spacer" style={completedWidth}/>
+          <span className="marker-text">You are here</span>
         </div>
-
-        <div className="front-of-docket-text"><p>Front of docket line</p></div>
-        <p><strong>{total.toLocaleString()}</strong> total appeals on the docket</p>
+        <div>
+          <div className="spacer" style={completedWidth}/>
+          <div className="marker">
+            <img src="/img/Docket-line-pin.svg" alt=""/>
+          </div>
+        </div>
       </div>
-    );
-  }
+
+      <div>
+        <div className="docket-bar">
+          <div className="completed" style={completedWidth}/>
+        </div>
+        <div className="end-of-docket"/>
+      </div>
+
+      <div className="front-of-docket-text"><p>Front of docket line</p></div>
+      <p><strong>{total.toLocaleString()}</strong> total appeals on the docket</p>
+    </div>
+  );
 }
 
 Docket.propTypes = {
