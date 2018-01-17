e00e331f56fd324abe42c98a7685a61ef6e7b134
Christopher Valarida <chris.valarida@adhocteam.us>
2018-01-16 17:13:56 -0800

    Easy part = done

--- src/js/claims-status/components/appeals-v2/Docket.jsx
+++ src/js/claims-status/components/appeals-v2/Docket.jsx
@@ -9,9 +9,14 @@ class Docket extends React.Component {
       <div className="docket-container">
         <span className="appeals-ahead">{ahead.toLocaleString()}</span>
         <p>Appeals ahead of you</p>
-        <div className="docket-bar">
-          <div className="completed" style={{ width: `${(ahead / total) * 100}%` }}/>
+        <div>
+          <div className="docket-bar">
+            <div className="completed" style={{ width: `${(ahead / total) * 100}%` }}/>
+          </div>
+          <div className="end-of-docket"/>
         </div>
+        <div className="front-of-docket-text"><p>Front of docket line</p></div>
+        <p><strong>{total.toLocaleString()}</strong> total appeals on the docket</p>
       </div>
     );
   }
