518cad39aaf84b0031b6acb8d3309841cc1e8e55
Christopher Valarida <chris.valarida@adhocteam.us>
2018-01-17 11:14:48 -0800

    Mostly working

--- src/js/claims-status/components/appeals-v2/Docket.jsx
+++ src/js/claims-status/components/appeals-v2/Docket.jsx
@@ -3,15 +3,24 @@ import React from 'react';
 class Docket extends React.Component {
   render() {
     const { ahead, total } = this.props;
+    // const { ahead, total } = { ahead: 0, total: 100 };
+    const completedWidth = { width: `${((total - ahead) / total) * 100}%` };
 
     // TODO: Assess how accessible this is
     return (
       <div className="docket-container">
         <span className="appeals-ahead">{ahead.toLocaleString()}</span>
         <p>Appeals ahead of you</p>
+        <div className="marker-container">
+          <div className="marker-spacer" style={completedWidth}/>
+          <div className="marker">
+            <p>You are here</p>
+            <img src="/img/Docket-line-pin.svg" alt=""/>
+          </div>
+        </div>
         <div>
           <div className="docket-bar">
-            <div className="completed" style={{ width: `${(ahead / total) * 100}%` }}/>
+            <div className="completed" style={completedWidth}/>
           </div>
           <div className="end-of-docket"/>
         </div>
