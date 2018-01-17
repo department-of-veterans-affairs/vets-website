fcccfd12f4264a186faf056d97a91b337460f3bc
Christopher Valarida <chris.valarida@adhocteam.us>
2018-01-17 13:46:05 -0800

    Don't let the text go off the edges

--- src/js/claims-status/components/appeals-v2/Docket.jsx
+++ src/js/claims-status/components/appeals-v2/Docket.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 class Docket extends React.Component {
   render() {
     const { ahead, total } = this.props;
-    // const { ahead, total } = { ahead: 0, total: 100 };
     const completedWidth = { width: `${((total - ahead) / total) * 100}%` };
 
     // TODO: Assess how accessible this is
@@ -11,19 +10,27 @@ class Docket extends React.Component {
       <div className="docket-container">
         <span className="appeals-ahead">{ahead.toLocaleString()}</span>
         <p>Appeals ahead of you</p>
+
         <div className="marker-container">
-          <div className="marker-spacer" style={completedWidth}/>
-          <div className="marker">
-            <p>You are here</p>
-            <img src="/img/Docket-line-pin.svg" alt=""/>
+          <div>
+            <div className="marker-text-spacer" style={completedWidth}/>
+            <span className="marker-text">You are here</span>
+          </div>
+          <div>
+            <div className="spacer" style={completedWidth}/>
+            <div className="marker">
+              <img src="/img/Docket-line-pin.svg" alt=""/>
+            </div>
           </div>
         </div>
+
         <div>
           <div className="docket-bar">
             <div className="completed" style={completedWidth}/>
           </div>
           <div className="end-of-docket"/>
         </div>
+
         <div className="front-of-docket-text"><p>Front of docket line</p></div>
         <p><strong>{total.toLocaleString()}</strong> total appeals on the docket</p>
       </div>
