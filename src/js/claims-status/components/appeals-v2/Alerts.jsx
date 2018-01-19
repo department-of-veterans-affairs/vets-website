8d8f941163357e60daa8be4102ffe1ea5fb4a47b
Mark Greenburg <marek.greenburg@gmail.com>
2018-01-18 22:10:17 -0600

    Update Alerts to segment take action and info alerts

--- src/js/claims-status/components/appeals-v2/Alerts.jsx
+++ src/js/claims-status/components/appeals-v2/Alerts.jsx
@@ -2,29 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Alert from './Alert';
 
-const Alerts = (props) => {
-  if (typeof props.alerts === 'undefined' || props.alerts.length === 0) {
+const Alerts = ({alerts}) => {
+  if (typeof alerts === 'undefined' || alerts.length === 0) {
     return null;
   }
 
-  const alertsList = props.alerts.map((alert, index) => {
-    const key = `${alert.type}-${index}`;
-    return (<Alert key={key} alert={alert}/>);
-  });
+  const takeActionAlerts = alerts.filter((alert) => (alert.details.type === 'take_action'));
+  const infoAlert = alerts.filter((alert) => (alert.details.type === 'info'));
+
+  const alertsList = takeActionAlerts
+    .concat(infoAlert)
+    .map((alert, index) => {
+      const key = `${alert.type}-${index}`;
+      return (<Alert key={key} alert={alert}/>);
+    });
+  
+  const takeActionHeader = (takeActionAlerts.length)
+    ? (<h3>Take Action</h3>)
+    : null;
 
   return (
-    <ul className="alerts-list">
-      {alertsList}
-    </ul>
+    <div>
+      {takeActionHeader}
+      <ul className="alerts-list">
+        {alertsList}
+      </ul>
+    </div>
   );
-};
 
-Alerts.propTypes = {
-  alerts: PropTypes.arrayOf(PropTypes.shape({
-    type: PropTypes.string.isRequired,
-    details: PropTypes.object
-  }))
+//   const alertsList = alerts.map((alert, index) => {
+//     const key = `${alert.type}-${index}`;
+//     return (<Alert key={key} alert={alert}/>);
+//   });
+
+//   return (
+//     <ul className="alerts-list">
+//       {alertsList}
+//     </ul>
+//   );
+// };
+
+// Alerts.propTypes = {
+//   alerts: PropTypes.arrayOf(PropTypes.shape({
+//     type: PropTypes.string.isRequired,
+//     details: PropTypes.object
+//   }))
 };
 
 export default Alerts;
-
