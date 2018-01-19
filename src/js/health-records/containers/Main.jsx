a4e82ccc48c9ae23fc8979506abc39e8f3d8e2e3
Ben Shyong <ben.shyong@gmail.com>
2018-01-19 11:07:43 -0800

    health records custom date selection accessibility

--- src/js/health-records/containers/Main.jsx
+++ src/js/health-records/containers/Main.jsx
@@ -8,8 +8,8 @@ import DatePicker from 'react-datepicker';
 
 import LoadingIndicator from '../../common/components/LoadingIndicator';
 import ErrorableCheckbox from '../../common/components/form-elements/ErrorableCheckbox';
+import ErrorableRadioButtons from '../../common/components/form-elements/ErrorableRadioButtons';
 
-import ErrorableRadioButtons from '../components/ErrorableRadioButtons';
 import ErrorView from '../components/ErrorView';
 import { reportTypes as reportTypesConfig } from '../config';
 import {
