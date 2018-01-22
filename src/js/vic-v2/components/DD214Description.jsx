6592d535e711c1fd0990008c34709ec50a661e39
Jeff Balboni <jeff@adhocteam.us>
2018-01-22 16:24:18 -0500

    PR comment

--- src/js/vic-v2/components/DD214Description.jsx
+++ src/js/vic-v2/components/DD214Description.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function DD214Description() {
   return (
     <div>
-      <p>Please upload a copy of your DD 214 to verify that you were honorably discharged.</p>
+      <p>Please upload a copy of your DD214 to verify that you were honorably discharged.</p>
       File types you can upload: PDF, JPG, PNG
     </div>
   );
