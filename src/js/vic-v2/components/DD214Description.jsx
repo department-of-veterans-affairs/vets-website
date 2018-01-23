717e10169fa9f230a3996de69551d3566be6c029
Jeff Balboni <jbalboni@gmail.com>
2018-01-23 09:17:03 -0500

    VIC DD 214 document upload (#7119)

---
+++ src/js/vic-v2/components/DD214Description.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function DD214Description() {
+  return (
+    <div>
+      <p>Please upload a copy of your DD214 to verify that you were honorably discharged.</p>
+      File types you can upload: PDF, JPG, PNG
+    </div>
+  );
+}
