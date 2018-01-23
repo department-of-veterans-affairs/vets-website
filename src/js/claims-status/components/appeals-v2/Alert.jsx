ce74a706c609387b13b41a286319ea3386d4d879
Mark Greenburg <marek.greenburg@gmail.com>
2018-01-22 19:18:54 -0600

    Refactor alert component to abstract away css class implementation, add displayType

--- src/js/claims-status/components/appeals-v2/Alert.jsx
+++ src/js/claims-status/components/appeals-v2/Alert.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Alert = ({ title, description, cssClass }) => {
+const Alert = ({ title, description, displayType }) => {
+  let cssClass;
+  if (displayType === 'take_action') {
+    cssClass = 'usa-alert-warning';
+  } else if (displayType === 'info') {
+    cssClass = 'usa-alert-info';
+  }
+
   return (
     <li>
       <div className={`usa-alert ${cssClass}`}>
         <div className="usa-alert-body">
           <h4 className="usa-alert-heading">{title}</h4>
-          <p className="usa-alert-text">{description}</p>
+          <div className="usa-alert-text">{description}</div>
         </div>
       </div>
     </li>
@@ -17,7 +24,7 @@ const Alert = ({ title, description, cssClass }) => {
 Alert.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.object.isRequired, // nullable JSX snippet
-  cssClass: PropTypes.object.isRequired,
+  displayType: PropTypes.string.isRequired,
 };
 
 export default Alert;
