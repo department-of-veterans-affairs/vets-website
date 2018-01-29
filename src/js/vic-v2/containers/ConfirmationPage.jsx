e3c8ba0dacf420b978390a0451955c73ad2ce537
Jeff Balboni <jeff@adhocteam.us>
2018-01-29 16:50:17 -0500

    PR comments

--- src/js/vic-v2/containers/ConfirmationPage.jsx
+++ src/js/vic-v2/containers/ConfirmationPage.jsx
@@ -44,24 +44,25 @@ class ConfirmationPage extends React.Component {
 
     return (
       <div>
-        <p>You have completed your application.<br/>
+        <p>We’ve received your application. Thank you for applying for a Veteran ID Card.<br/>
         We process applications and print cards in the order we receive them.</p>
 
-        <h4>What happens after I apply?</h4>
+        <h2 className="schemaform-confirmation-section-header">What happens after I apply?</h2>
         {!verified && <div>
           <p>We’ll review your application to verify your eligibility. We may contact you if we have any questions or need more information.</p>
-          <p>You must have separated under honorable conditions to be eligible for a Veteran ID Card. If you have an other than honorable discharge, you can apply for an upgrade or correction. If your application goes through and your discharge is upgraded, you’ll be eligible for the Veteran ID Card. <a href="/discharge-upgrade-instructions" target="_blank">Find out how to apply for a discharge upgrade</a>.</p>
-          <p>If you uploaded a copy of your DD214, we can use it to review your application and verify that you were honorably discharged.</p>
+          <p>To be eligible for a Veteran ID Card, you must have separated under honorable conditions. If you have an other than honorable discharge, you can apply for an upgrade or correction.<br/>
+            <a href="/discharge-upgrade-instructions" target="_blank">Find out how to apply for a discharge upgrade</a>.</p>
+          <p>If you upload a copy of your discharge document, we can use it to verify that you were honorably discharged.</p>
         </div>}
         {verified && <div>
           <p>You should receive your Veteran ID Card in the mail in about 60 days.<br/>
-          In the meantime, you can print and download a temporary digital Veteran ID Card.</p>
+          In the meantime, you can print a temporary digital Veteran ID Card.</p>
           <VeteranIDCard/>
           <button type="button" className="va-button-link" onClick={() => window.print()}>Print your temporary Veteran ID Card.</button>
         </div>}
         <p><em>Please print this page for your records</em></p>
         <div className="inset">
-          <h4>Veteran ID Card claim</h4>
+          <h3 className="schemaform-confirmation-claim-header">Veteran ID Card claim</h3>
           <span>for {veteranFullName.first} {veteranFullName.middle} {veteranFullName.last} {veteranFullName.suffix}</span>
           <ul className="claim-list">
             <li>
@@ -76,7 +77,7 @@ class ConfirmationPage extends React.Component {
         </div>
         <div className="confirmation-guidance-container">
           <h4 className="confirmation-guidance-heading">Need help?</h4>
-          <p className="confirmation-guidance-message">If you have questions, call <a href="tel:+1-800-827-1000">1-800-827-1000</a>, Monday &#8211; Friday, 8:00 a.m. &#8211; 9:00 p.m. (ET). Please have your Social Security number or VA file number ready. For Telecommunication Relay Services, dial <a href="tel:711">711</a>.</p>
+          <p className="confirmation-guidance-message">If you have questions, call <a href="tel:+1-855-673-4357">1-855-673-4357</a>, Monday &#8211; Friday, 8:00 a.m. &#8211; 8:00 p.m. (ET). For Telecommunication Relay Services, dial <a href="tel:711">711</a>.</p>
         </div>
         <div className="row form-progress-buttons schemaform-back-buttons">
           <div className="small-6 usa-width-one-half medium-6 columns">
