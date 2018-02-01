6587c73523444150b2cadf397d0ac6ca1a4c935d
Mark Greenburg <marek.greenburg@gmail.com>
2018-02-01 11:44:10 -0600

    Fix up Issues tests

--- test/claims-status/components/appeals-v2/Issues.unit.spec.jsx
+++ test/claims-status/components/appeals-v2/Issues.unit.spec.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import Issues from '../../../../src/js/claims-status/components/appeals-v2/Issues';
-import { categorizeIssues } from '../../../../src/js/claims-status/utils/appeals-v2-helpers';
+import { addStatusToIssues } from '../../../../src/js/claims-status/utils/appeals-v2-helpers';
 import { mockData } from '../../../../src/js/claims-status/utils/helpers';
 
 describe('<Issues/>', () => {
-  const emptyIssues = { issues: categorizeIssues(mockData.data[0].attributes.issues) };
-  const oneIssues = { issues: categorizeIssues(mockData.data[1].attributes.issues) };
-  const manyIssues = { issues: categorizeIssues(mockData.data[2].attributes.issues) };
+  const emptyIssues = { issues: addStatusToIssues([]) };
+  const oneOpenIssue = { issues: addStatusToIssues(mockData.data[1].attributes.issues) };
+  const oneClosedIssue = { issues: addStatusToIssues([mockData.data[2].attributes.issues[3]]) };
+  const manyIssues = { issues: addStatusToIssues(mockData.data[2].attributes.issues) };
 
   it('should render', () => {
     const wrapper = shallow(<Issues {...emptyIssues}/>);
@@ -21,8 +22,13 @@ describe('<Issues/>', () => {
     expect(wrapper.find('CollapsiblePanel').length).to.equal(0);
   });
 
-  it('should render one panel when only an open *OR* a closed issue is passed in', () => {
-    const wrapper = shallow(<Issues {...oneIssues}/>);
+  it('should render one panel when only an open issue is passed in', () => {
+    const wrapper = shallow(<Issues {...oneOpenIssue}/>);
+    expect(wrapper.find('CollapsiblePanel').length).to.equal(1);
+  });
+
+  it('should render one panel when only a closed issue is passed in', () => {
+    const wrapper = shallow(<Issues {...oneClosedIssue}/>);
     expect(wrapper.find('CollapsiblePanel').length).to.equal(1);
   });
 
@@ -35,7 +41,7 @@ describe('<Issues/>', () => {
     const props = { issues: [{ status: 'open', description: 'test open issue' }] };
     const wrapper = mount(<Issues {...props }/>);
     const panelButton = wrapper.find('.usa-accordion-button');
-    expect(panelButton.html()).to.contain('Currently On Appeal');
+    expect(panelButton.html()).to.contain('Currently on appeal');
     // expand the collapsed accordion to reveal child HTML
     panelButton.simulate('click');
     const expandedPanel = wrapper.find('.usa-accordion-bordered');
