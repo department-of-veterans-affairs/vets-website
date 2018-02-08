9408a7feb9a29d372fdfa40262bc369381163dac
DanielleSoCompany <danielle@thesocompany.com>
2018-02-08 18:17:48 -0500

    VIC welcome page (#7102)

--- script/build.js
+++ script/build.js
@@ -97,6 +97,7 @@ if (options.buildtype === 'production') {
   ignoreList.push('burials-and-memorials/pre-need/form-10007-apply-for-eligibility.md');
   ignoreList.push('employment/vocational-rehab-and-employment/application/chapter36.md');
   ignoreList.push('veteran-id-card/apply.md');
+  ignoreList.push('veteran-id-card/how-to-get.md');
 }
 smith.use(ignore(ignoreList));
 
@@ -596,6 +597,7 @@ if (!options.watch && !(process.env.CHECK_BROKEN_LINKS === 'no')) {
         '/pension/application/527EZ',
         '/burials-and-memorials/application/530',
         '/health-care/apply/application',
+        '/veteran-id-card/apply',
         '/letters'].join('|'))
   }));
 }
