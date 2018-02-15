bfa2a87bf5dd1b1dc92565253c175abe72a33ac7
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-02-15 17:37:39 -0500

    Add the accordion

--- src/js/user-profile/containers/UserProfileApp.jsx
+++ src/js/user-profile/containers/UserProfileApp.jsx
@@ -1,63 +1,83 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import moment from 'moment';
-
 import { getVerifyUrl } from '../../common/helpers/login-helpers.js';
 import { updateVerifyUrl } from '../../login/actions';
 import { removeSavedForm } from '../actions';
-import AuthApplicationSection from '../components/AuthApplicationSection';
-import FormList from '../components/FormList';
 import RequiredLoginView from '../../common/components/RequiredLoginView';
 import DowntimeNotification, { services } from '../../common/containers/DowntimeNotification';
 
-moment.updateLocale('en', {
-  meridiem: (hour) => {
-    if (hour < 12) {
-      return 'a.m.';
-    }
-    return 'p.m.';
-  },
-  monthsShort: [
-    'Jan.',
-    'Feb.',
-    'Mar.',
-    'Apr.',
-    'May',
-    'June',
-    'July',
-    'Aug.',
-    'Sept.',
-    'Oct.',
-    'Nov.',
-    'Dec.'
-  ]
-});
+const accordionIds = {
+  veteranInformation: 'veteranInformation',
+  militaryService: 'militaryService'
+};
 
 class UserProfileApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activeAccordionId: accordionIds.veteranInformation
+    };
+  }
+
   componentDidMount() {
     if (!this.props.verifyUrl) {
       getVerifyUrl(this.props.updateVerifyUrl);
     }
   }
 
-  render() {
-    const view = (
-      <div className="row user-profile-row">
-        <div className="usa-width-two-thirds medium-8 small-12 columns">
-          <h1>Your Profile</h1>
-          <div>
-            <FormList
-              userProfile={this.props.profile}
-              removeSavedForm={this.props.removeSavedForm}
-              savedForms={this.props.profile.savedForms}/>
-            <AuthApplicationSection
-              userProfile={this.props.profile}
-              verifyUrl={this.props.verifyUrl}/>
-          </div>
-        </div>
+  getAccordionToggle(accordionId) {
+    return () => {
+      const activeAccordionId = this.state.activeAccordionId === accordionId ? '' : accordionId;
+      this.setState({ activeAccordionId });
+    };
+  }
+
+  accordionButton(buttonText, accordionId) {
+    return (
+      <div className="accordion-header">
+        <button className="usa-accordion-button usa-button-unstyled"
+          onClick={this.getAccordionToggle(accordionId)}
+          aria-expanded={this.state.activeAccordionId === accordionId}
+          aria-controls={accordionId}>
+          {buttonText}
+        </button>
       </div>
     );
+  }
 
+  accordionSection(accordionId, title, content) {
+    return (
+      <li>
+        <div className="accordion-header">
+          <button className="usa-accordion-button usa-button-unstyled"
+            onClick={this.getAccordionToggle(accordionId)}
+            aria-expanded={this.state.activeAccordionId === accordionId}
+            aria-controls={accordionId}>
+            {title}
+          </button>
+        </div>
+        <div className="usa-accordion-content"
+          id={accordionId}
+          aria-hidden={this.state.activeAccordionId !== accordionId}>
+          {content}
+        </div>
+      </li>
+    );
+  }
+
+  renderAccordion() {
+    return (
+      <ul className="usa-accordion usa-accordion-bordered form-review-panel">
+        { this.accordionSection('veteranInformation', 'Veteran Information', 'My veteran information') }
+        { this.accordionSection('militaryService', 'Military Service', 'My military service') }
+        { this.accordionSection('vaBenefits', 'VA Benefits', 'My va benefits') }
+        { this.accordionSection('householdInformation', 'Household Information', 'My household information') }
+        { this.accordionSection('insuranceInformation', 'Insurance Information', 'My insurance information') }
+      </ul>
+    );
+  }
+
+  render() {
     return (
       <div>
         <RequiredLoginView
@@ -67,7 +87,14 @@ class UserProfileApp extends React.Component {
           loginUrl={this.props.loginUrl}
           verifyUrl={this.props.verifyUrl}>
           <DowntimeNotification appTitle="user profile page" dependencies={[services.mvi, services.emis]}>
-            {view}
+            <div className="row user-profile-row">
+              <div className="usa-width-two-thirds medium-8 small-12 columns">
+                <h1>Your Profile</h1>
+                <div>
+                  {this.renderAccordion()}
+                </div>
+              </div>
+            </div>
           </DowntimeNotification>
         </RequiredLoginView>
       </div>
