41c57d80bfe97308ba8b83a38193915c7a84b4fb
Jeff Balboni <jeff@adhocteam.us>
2018-02-15 16:03:52 -0500

    Update content

--- src/js/vic-v2/components/RateLimitContent.jsx
+++ src/js/vic-v2/components/RateLimitContent.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 export default function RateLimitContent() {
   return (
-    <div className="row">
-      <div className="usa-width-two-thirds medium-8 columns">
-        <div>Limited!</div>
+    <div className="vic-layover">
+      <div className="row">
+        <div className="usa-width-two-thirds medium-8 columns">
+          <h2>We’re rolling out a new Veteran ID Card experience</h2>
+          <p>Thank you for your patience with the Veteran ID Card application. With feedback from Veterans like you, we’ve changed some things to make the process of applying easier.</p>
+          <p>Our updates should be ready by tomorrow. Please check back.</p>
+        </div>
       </div>
     </div>
   );
