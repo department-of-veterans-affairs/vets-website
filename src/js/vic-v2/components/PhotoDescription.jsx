34ba92cdc456c3321491bebdb2bd2f97b577e656
DanielleSoCompany <danielle@thesocompany.com>
2018-02-16 16:05:18 -0500

    VIC photo guidance page (#7256)

--- src/js/vic-v2/components/PhotoDescription.jsx
+++ src/js/vic-v2/components/PhotoDescription.jsx
@@ -17,6 +17,9 @@ export default function PhotoDescription() {
         <h3>Examples of good ID photos</h3>
         <img className="example-photo" alt="placeholder" src="/img/example-photo-1.png"/>
         <img className="example-photo" alt="placeholder" src="/img/example-photo-2.png"/>
+        <p>
+          <a href="/veteran-id-card/how-to-upload-photo" target="_blank">Learn more about uploading a photo for your Veteran ID Card</a>.
+        </p>
       </div>
     </div>
   );
