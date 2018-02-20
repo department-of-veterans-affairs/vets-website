ca53df0233e85d3a14fc70a1be3df43151c83fc4
Rian Fowler <me@rian.codes>
2018-02-20 10:21:47 -0600

    7140 vre contact info (#7300)
    * add applicant address with unit tests
    
    * add unit test for applicant address
    
    * add phone and email formwith unit tests
    
    * remove only from unit tests
    
    * separate applicant address test
    
    * remove only from tests

---
+++ test/vre/chapter36/config/contactInformation.unit.spec.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+
+import { DefinitionTester, fillData } from '../../../util/schemaform-utils.jsx';
+import formConfig from '../../../../src/js/vre/chapter36/config/form.js';
+
+describe('VRE chapter 36 contact information', () => {
+  const { schema, uiSchema } = formConfig.chapters.contactInformation.pages.contactInformation;
+  it('renders applicant address form', () => {
+    const form = mount(
+      <DefinitionTester
+        definitions={formConfig.defaultDefinitions}
+        schema={schema}
+        data={{}}
+        formData={{}}
+        uiSchema={uiSchema}/>
+    );
+    expect(form.find('input').length).to.equal(4);
+  });
+
+  it('submits with info', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        definitions={formConfig.defaultDefinitions}
+        schema={schema}
+        data={{}}
+        formData={{}}
+        onSubmit={onSubmit}
+        uiSchema={uiSchema}/>
+    );
+    fillData(form, 'input#root_applicantHomePhone', '8017777777');
+    fillData(form, 'input#root_applicantMobilePhone', '8017777777');
+    fillData(form, 'input#root_applicantEmail', 'test@test.com');
+    fillData(form, 'input[name="root_view:confirmEmail"]', 'test@test.com');
+
+    form.find('form').simulate('submit');
+
+    expect(form.find('.usa-input-error').length).to.equal(0);
+    expect(onSubmit.called).to.be.true;
+  });
+
+  it('does not submit with non matching email addresses', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        definitions={formConfig.defaultDefinitions}
+        schema={schema}
+        data={{}}
+        formData={{}}
+        onSubmit={onSubmit}
+        uiSchema={uiSchema}/>
+    );
+    fillData(form, 'input#root_applicantEmail', 'test@test.com');
+    fillData(form, 'input[name="root_view:confirmEmail"]', 'test@nottest.com');
+
+    form.find('form').simulate('submit');
+
+    expect(form.find('.usa-input-error').length).to.equal(1);
+    expect(onSubmit.called).to.be.false;
+  });
+});
