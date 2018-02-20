313b2e9bc4cccfe49cc4463692c8793559b8211d
Rian Fowler <rian.fowler@adhocteam.us>
2018-02-20 08:32:54 -0600

    separate applicant address test

--- test/vre/chapter36/config/applicantAddress.unit.spec.jsx
+++ test/vre/chapter36/config/applicantAddress.unit.spec.jsx
@@ -6,8 +6,8 @@ import { mount } from 'enzyme';
 import { DefinitionTester, fillData } from '../../../util/schemaform-utils.jsx';
 import formConfig from '../../../../src/js/vre/chapter36/config/form.js';
 
-describe.only('VRE chapter 36 contact information', () => {
-  const { schema, uiSchema } = formConfig.chapters.contactInformation.pages.contactInformation;
+describe.only('VRE chapter 36 applicant address', () => {
+  const { schema, uiSchema } = formConfig.chapters.contactInformation.pages.applicantAddress;
   it('renders applicant address form', () => {
     const form = mount(
       <DefinitionTester
@@ -18,9 +18,10 @@ describe.only('VRE chapter 36 contact information', () => {
         uiSchema={uiSchema}/>
     );
     expect(form.find('input').length).to.equal(4);
+    expect(form.find('select').length).to.equal(2);
   });
 
-  it('submits with info', () => {
+  it('does not submit without required info', () => {
     const onSubmit = sinon.spy();
     const form = mount(
       <DefinitionTester
@@ -31,34 +32,33 @@ describe.only('VRE chapter 36 contact information', () => {
         onSubmit={onSubmit}
         uiSchema={uiSchema}/>
     );
-    fillData(form, 'input#root_applicantHomePhone', '8017777777');
-    fillData(form, 'input#root_applicantMobilePhone', '8017777777');
-    fillData(form, 'input#root_applicantEmail', 'test@test.com');
-    fillData(form, 'input[name="root_view:confirmEmail"]', 'test@test.com');
+
+    form.find('form').simulate('submit');
+    expect(form.find('.usa-input-error').length).to.equal(4);
+
+    expect(onSubmit.called).to.be.false;
+  });
+
+  it('submits with required info', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        definitions={formConfig.defaultDefinitions}
+        schema={schema}
+        data={{}}
+        formData={{}}
+        onSubmit={onSubmit}
+        uiSchema={uiSchema}/>
+    );
+    fillData(form, 'select#root_applicantAddress_country', 'USA');
+    fillData(form, 'input#root_applicantAddress_street', '123 test st');
+    fillData(form, 'input#root_applicantAddress_city', 'Los Angeles');
+    fillData(form, 'select#root_applicantAddress_state', 'CA');
+    fillData(form, 'input#root_applicantAddress_postalCode', '90034');
 
     form.find('form').simulate('submit');
 
     expect(form.find('.usa-input-error').length).to.equal(0);
     expect(onSubmit.called).to.be.true;
-  });
-
-  it('does not submit with non matching email addresses', () => {
-    const onSubmit = sinon.spy();
-    const form = mount(
-      <DefinitionTester
-        definitions={formConfig.defaultDefinitions}
-        schema={schema}
-        data={{}}
-        formData={{}}
-        onSubmit={onSubmit}
-        uiSchema={uiSchema}/>
-    );
-    fillData(form, 'input#root_applicantEmail', 'test@test.com');
-    fillData(form, 'input[name="root_view:confirmEmail"]', 'test@nottest.com');
-
-    form.find('form').simulate('submit');
-
-    expect(form.find('.usa-input-error').length).to.equal(1);
-    expect(onSubmit.called).to.be.false;
   });
 });
