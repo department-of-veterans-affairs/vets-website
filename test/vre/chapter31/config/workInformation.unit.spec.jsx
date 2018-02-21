900ce07c14860f36c7a842f4f7d1d31ee77afa7a
Rian Fowler <rian.fowler@adhocteam.us>
2018-02-21 10:48:43 -0600

    add unit tests

---
+++ test/vre/chapter31/config/workInformation.unit.spec.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+
+import { DefinitionTester, selectRadio } from '../../../util/schemaform-utils.jsx';
+import formConfig from '../../../../src/js/vre/chapter31/config/form.js';
+
+describe.only('VRE chapter 31 work information', () => {
+  const { schema, uiSchema } = formConfig.chapters.workInformation.pages.workInformation;
+  it('renders work information form', () => {
+    const form = mount(<DefinitionTester
+      definitions={formConfig.defaultDefinitions}
+      schema={schema}
+      data={{}}
+      formData={{}}
+      uiSchema={uiSchema}/>
+    );
+
+    expect(form.find('input').length).to.equal(2);
+  });
+
+  it('renders job information form when working is true', () => {
+    const form = mount(<DefinitionTester
+      definitions={formConfig.defaultDefinitions}
+      schema={schema}
+      data={{}}
+      formData={{}}
+      uiSchema={uiSchema}/>
+    );
+
+    selectRadio(form, 'root_view:isWorking', 'Y');
+
+    expect(form.find('input').length).to.equal(8);
+    expect(form.find('select').length).to.equal(2);
+  });
+
+  it('submits with required information', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        onSubmit={onSubmit}
+        definitions={formConfig.defaultDefinitions}
+        schema={schema}
+        data={{}}
+        uiSchema={uiSchema}/>
+    );
+
+    selectRadio(form, 'root_view:isWorking', 'Y');
+    form.find('form').simulate('submit');
+
+    expect(form.find('.usa-input-error').length).to.equal(0);
+    expect(onSubmit.called).to.be.true;
+  });
+
+  it('does not submit without required information', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        onSubmit={onSubmit}
+        definitions={formConfig.defaultDefinitions}
+        schema={schema}
+        data={{}}
+        uiSchema={uiSchema}/>
+    );
+
+    form.find('form').simulate('submit');
+
+    expect(form.find('.usa-input-error').length).to.equal(1);
+    expect(onSubmit.called).to.be.false;
+  });
+});
