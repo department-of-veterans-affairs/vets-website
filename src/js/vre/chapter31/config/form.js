cd42e140f4b4d6a08fbb453c56cb58809a1c5ade
Rian Fowler <rian.fowler@adhocteam.us>
2018-02-22 14:43:08 -0600

    add chapter 31 / 6 form

--- src/js/vre/chapter31/config/form.js
+++ src/js/vre/chapter31/config/form.js
@@ -1,21 +1,29 @@
-// import _ from 'lodash/fp';
+import _ from 'lodash/fp';
 
 import fullSchema31 from 'vets-json-schema/dist/28-1900-schema.json';
+
+import * as address from '../../../common/schemaform/definitions/address';
+import phoneUI from '../../../common/schemaform/definitions/phone';
 
 import IntroductionPage from '../components/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
 import createVeteranInfoPage from '../../pages/veteranInfo';
 import { facilityLocatorLink } from '../helpers';
+import { validateMatch } from '../../../common/schemaform/validation';
 
 const {
+  daytimePhone,
+  email,
+  eveningPhone,
   vaRecordsOffice
 } = fullSchema31.properties;
 
 const {
-  fullName,
   date,
+  fullName,
+  phone,
   ssn,
-  vaFileNumber
+  vaFileNumber,
 } = fullSchema31.definitions;
 
 const formConfig = {
@@ -36,6 +44,7 @@ const formConfig = {
   defaultDefinitions: {
     fullName,
     date,
+    phone,
     ssn,
     vaFileNumber,
   },
@@ -115,12 +124,62 @@ const formConfig = {
     contactInformation: {
       title: 'Contact Information',
       pages: {
+        veteranAddress: {
+          path: 'veteran-address',
+          title: 'Address information',
+          uiSchema: {
+            veteranAddress: address.uiSchema(),
+            'view:isMoving': {
+              'ui:title': 'Are you moving within the next 30 days?',
+              'ui:widget': 'yesNo'
+            },
+            veteranNewAddress: _.merge(
+              address.uiSchema('', false, (formData) => formData['view:isMoving']),
+              {
+                'ui:options': {
+                  expandUnder: 'view:isMoving'
+                }
+              }
+            )
+          },
+          schema: {
+            type: 'object',
+            required: ['veteranAddress'],
+            properties: {
+              veteranAddress: address.schema(fullSchema31, true),
+              'view:isMoving': {
+                type: 'boolean'
+              },
+              veteranNewAddress: address.schema(fullSchema31)
+            }
+          }
+        },
         contactInformation: {
           path: 'contact-information',
-          title: 'Contact Information',
+          title: 'Contact information',
+          uiSchema: {
+            daytimePhone: phoneUI('Daytime phone number'),
+            eveningPhone: phoneUI('Evening phone number'),
+            email: {
+              'ui:title': 'Email address'
+            },
+            'view:confirmEmail': {
+              'ui:title': 'Re-enter email address',
+              'ui:options': {
+                hideOnReview: true
+              }
+            },
+            'ui:validations': [
+              validateMatch('email', 'view:confirmEmail')
+            ]
+          },
           schema: {
             type: 'object',
             properties: {
+              daytimePhone,
+              eveningPhone,
+              email,
+              'view:confirmEmail': email,
             }
           }
         }
