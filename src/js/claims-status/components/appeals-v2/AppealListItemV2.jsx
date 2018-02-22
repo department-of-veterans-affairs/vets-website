1af817b95a6d282fd61e4d38097df954886ac434
Christopher Valarida <chris.valarida@adhocteam.us>
2018-02-22 11:53:26 -0800

    Remove unneeded cruft

--- src/js/claims-status/components/appeals-v2/AppealListItemV2.jsx
+++ src/js/claims-status/components/appeals-v2/AppealListItemV2.jsx
@@ -4,18 +4,8 @@ import moment from 'moment';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { appealStatusDescriptions } from '../../utils/appeal-helpers';
 import { getStatusContents } from '../../utils/appeals-v2-helpers';
 
-// TODO: Remove this function and the related 'communications' div once
-// confirmed that this is no longer relevant
-const renderNextAction = (lastEvent, previousHistory) => {
-  if (lastEvent.type === 'ssoc' && previousHistory[0].type !== 'soc') {
-    return null;
-  }
-  const nextAction = appealStatusDescriptions(lastEvent, previousHistory).nextAction;
-  return nextAction && nextAction.title;
-};
 
 // TODO: Get a proper mapping of programArea -> display output
 const appealTypeMap = {
@@ -37,9 +27,7 @@ export default function AppealListItem({ appeal, name }) {
   const { status } = appeal.attributes;
   // always show merged event on top
   const events = _.orderBy(appeal.attributes.events, [e => e.type === 'merged', e => moment(e.date).unix()], ['desc', 'desc']);
-  const lastEvent = events[0];
   const firstEvent = events[events.length - 1];
-  const previousHistory = events.slice(1);
 
   return (
     <div className="claim-list-item-container">
@@ -54,9 +42,6 @@ export default function AppealListItem({ appeal, name }) {
       </div>
       {appeal.attributes.description &&
         <span><strong>{appeal.attributes.issues.length === 1 ? 'Issue' : 'Issues'} on appeal:</strong> {appeal.attributes.description}</span>}
-      <div className="communications">
-        {renderNextAction(lastEvent, previousHistory)}
-      </div>
       <Link className="usa-button usa-button-primary" to={`appeals-v2/${appeal.id}/status`}>View status<i className="fa fa-chevron-right"/></Link>
     </div>
   );
