453ccc31b572dc266ce538701c7f58969cb3a39e
Jeff Balboni <jbalboni@gmail.com>
2018-02-23 13:07:20 -0500

    Move sanitizeForm so we don't import form code into every bundle (#7334)

--- src/js/common/utils/helpers.js
+++ src/js/common/utils/helpers.js
@@ -267,3 +267,18 @@ export function sortListByFuzzyMatch(value, list, prop = 'label') {
     })
     .map(sorted => sorted.original);
 }
+
+export function sanitizeForm(formData) {
+  try {
+    const suffixes = ['vaFileNumber', 'first', 'last', 'accountNumber', 'socialSecurityNumber', 'dateOfBirth'];
+    return JSON.stringify(formData, (key, value) => {
+      if (value && suffixes.some(suffix => key.toLowerCase().endsWith(suffix.toLowerCase()))) {
+        return 'removed';
+      }
+
+      return value;
+    });
+  } catch (e) {
+    return null;
+  }
+}
