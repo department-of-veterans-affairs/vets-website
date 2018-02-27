21e62c153f9fefd8da02ac1e16cce666d408b7c9
Ben Shyong <ben.shyong@gmail.com>
2018-02-26 18:49:35 -0800

    sample widget

--- src/js/dashboard/containers/DashboardApp.jsx
+++ src/js/dashboard/containers/DashboardApp.jsx
@@ -5,9 +5,8 @@ import moment from 'moment';
 import { getVerifyUrl } from '../../common/helpers/login-helpers.js';
 import { updateVerifyUrl } from '../../login/actions';
 import { removeSavedForm } from '../actions';
-import UserDataSection from '../components/UserDataSection';
-import AuthApplicationSection from '../components/AuthApplicationSection';
-import FormList from '../components/FormList';
+import MessagingWidget from './MessagingWidget';
+
 import RequiredLoginView from '../../common/components/RequiredLoginView';
 import DowntimeNotification, { services } from '../../common/containers/DowntimeNotification';
 
@@ -45,16 +44,9 @@ class DashboardApp extends React.Component {
     const view = (
       <div className="row user-profile-row">
         <div className="usa-width-two-thirds medium-8 small-12 columns">
-          <h1>Your Account</h1>
+          <h1>Dashboard</h1>
           <div>
-            <FormList
-              userProfile={this.props.profile}
-              removeSavedForm={this.props.removeSavedForm}
-              savedForms={this.props.profile.savedForms}/>
-            <AuthApplicationSection
-              userProfile={this.props.profile}
-              verifyUrl={this.props.verifyUrl}/>
-            <UserDataSection/>
+            <MessagingWidget/>
           </div>
         </div>
       </div>
