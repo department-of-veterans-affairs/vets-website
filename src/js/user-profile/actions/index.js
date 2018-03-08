7106fb81ebf689cdee88cf668ee4970a42a5ad2f
Ben Shyong <ben.shyong@gmail.com>
2018-03-08 15:51:25 -0800

    restore user profile from master

--- src/js/user-profile/actions/index.js
+++ src/js/user-profile/actions/index.js
@@ -14,10 +14,6 @@ export const REMOVING_SAVED_FORM = 'REMOVING_SAVED_FORM';
 export const REMOVING_SAVED_FORM_SUCCESS = 'REMOVING_SAVED_FORM_SUCCESS';
 export const REMOVING_SAVED_FORM_FAILURE = 'REMOVING_SAVED_FORM_FAILURE';
 
-export * from './mhv';
-
-export * from './mhv';
-
 export function updateProfileFields(newState) {
   return {
     type: UPDATE_PROFILE_FIELDS,
@@ -31,6 +27,24 @@ export function profileLoadingFinished() {
   };
 }
 
+export function removingSavedForm() {
+  return {
+    type: REMOVING_SAVED_FORM
+  };
+}
+
+export function removingSavedFormSuccess() {
+  return {
+    type: REMOVING_SAVED_FORM_SUCCESS
+  };
+}
+
+export function removingSavedFormFailure() {
+  return {
+    type: REMOVING_SAVED_FORM_FAILURE
+  };
+}
+
 export function fetchLatestTerms(termsName) {
   return dispatch => {
     dispatch({ type: FETCHING_LATEST_MHV_TERMS });
@@ -38,11 +52,11 @@ export function fetchLatestTerms(termsName) {
     apiRequest(
       `/terms_and_conditions/${termsName}/versions/latest`,
       null,
-      ({ data }) => dispatch({
+      response => dispatch({
         type: FETCHING_LATEST_MHV_TERMS_SUCCESS,
-        terms: data.attributes
+        terms: response.data.attributes
       }),
-      ({ errors }) => dispatch({ type: FETCHING_LATEST_MHV_TERMS_FAILURE, errors })
+      () => dispatch({ type: FETCHING_LATEST_MHV_TERMS_FAILURE })
     );
   };
 }
@@ -67,19 +81,19 @@ export function acceptTerms(termsName) {
         dispatch({ type: ACCEPTING_LATEST_MHV_TERMS_SUCCESS });
         getUserData(dispatch);
       },
-      ({ errors }) => dispatch({ type: ACCEPTING_LATEST_MHV_TERMS_FAILURE, errors })
+      () => dispatch({ type: ACCEPTING_LATEST_MHV_TERMS_FAILURE })
     );
   };
 }
 
 export function removeSavedForm(formId) {
   return dispatch => {
-    dispatch({ type: REMOVING_SAVED_FORM });
+    dispatch(removingSavedForm());
     return removeFormApi(formId)
       .then(() => {
-        dispatch({ type: REMOVING_SAVED_FORM_SUCCESS });
+        dispatch(removingSavedFormSuccess());
         getUserData(dispatch);
       })
-      .catch(() => dispatch({ type: REMOVING_SAVED_FORM_FAILURE }));
+      .catch(() => dispatch(removingSavedFormFailure()));
   };
 }
