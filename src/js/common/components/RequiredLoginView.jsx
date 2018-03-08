3687df19ee1d0bbb8dd6438ce1d504dcf1d9d36d
Eugene Doan <eugene@adhocteam.us>
2018-03-07 20:57:17 -0500

    Refactored RequiredLoginView to use verified flag.

--- src/js/common/components/RequiredLoginView.jsx
+++ src/js/common/components/RequiredLoginView.jsx
@@ -8,11 +8,13 @@ import LoadingIndicator from '../../common/components/LoadingIndicator';
 import Main from '../../login/containers/Main';
 
 const healthTools = ['health-records', 'rx', 'messaging'];
+const nextQuery = { next: window.location.pathname };
+const signInUrl = appendQuery('/', nextQuery);
 
 class RequiredLoginView extends React.Component {
   isServiceAvailable() {
     const requiredApp = this.props.serviceRequired;
-    const userServices = this.props.userProfile.services;
+    const userServices = this.props.user.profile.services;
     // Checks that
     // 1) session has a valid authentication token, and
     // 2) the application being loaded (requiredApp) is in the list of
@@ -29,90 +31,87 @@ class RequiredLoginView extends React.Component {
     return false;
   }
 
-  renderContent() {
-    const { userProfile, serviceRequired: requiredApp } = this.props;
+  renderVerifiedContent = () => {
+    const { serviceRequired, user } = this.props;
 
-    if (userProfile.loading === true) {
+    // Ignore required verification when accessing health tools with MHV sign-in.
+    const hasMhvAccess =
+      healthTools.includes(serviceRequired) &&
+      user.profile.authnContext === 'myhealthevet';
+
+    if (user.profile.verified || hasMhvAccess) {
+      // TODO: Delete the logic around attemptingAppealsAccess once we
+      // resolve the MVI/Appeals Users issues.
+      // if app we are trying to access includes appeals,
+      // bypass the checks for user profile status
+      let attemptingAppealsAccess;
+
+      if (Array.isArray(serviceRequired)) {
+        attemptingAppealsAccess = serviceRequired.indexOf('appeals-status') !== -1;
+      } else {
+        attemptingAppealsAccess = serviceRequired === 'appeals-status';
+      }
+
+      if (user.profile.status === 'SERVER_ERROR' && !attemptingAppealsAccess) {
+        // If va_profile is null, show a system down message.
+        return <SystemDownView messageLine1="Sorry, our system is temporarily down while we fix a few things. Please try again later."/>;
+      } else if (user.profile.status === 'NOT_FOUND' && !attemptingAppealsAccess) {
+        // If va_profile is "not found", show message that we cannot find the user
+        // in our system.
+        return <SystemDownView messageLine1="We couldn’t find your records with that information." messageLine2="Please call the Vets.gov Help Desk at 1-855-574-7286, TTY: 1-800-877-8339. We're open Monday &#8211; Friday, 8:00 a.m. &#8211; 8:00 p.m. (ET)."/>;
+      }
+
+      // If va_profile has any other value, continue on to check if this user can
+      // use this specific service.
+      // If they have the required service, show the application view.
+      if (this.isServiceAvailable()) { return this.props.children; }
+
+      // If the required service is not available, the component will still be rendered,
+      // but we pass an `isDataAvailable` prop to child components indicating there is
+      // no data. (Only add this prop to React components (functions), and not ordinary
+      // DOM elements.)
+      return React.Children.map(this.props.children, (child) => {
+        const props = typeof child.type === 'function'
+          ? { isDataAvailable: false }
+          : null;
+        return React.cloneElement(child, props);
+      });
+    }
+
+    // If the user is not verified and doesn't have MHV access, they need to verify.
+    return <Main renderType="verifyPage"/>;
+  }
+
+  renderWrappedContent = () => {
+    const { user, verify } = this.props;
+
+    if (user.profile.loading) {
       return <LoadingIndicator setFocus message="Loading your information..."/>;
     }
 
-    const nextQuery = { next: window.location.pathname };
-    const signInUrl = appendQuery('/', nextQuery);
-
-    if (this.props.authRequired === 1) {
-      if (userProfile.accountType >= 1) {
-        return this.props.children;
-      }
-
+    if (!user.login.currentlyLoggedIn) {
       return window.location.replace(signInUrl);
+    }
 
-    } else if (this.props.authRequired === 3) {
-      // Ignore LOA when accessing health tools with MHV sign-in.
-      const mhvAccess =
-        healthTools.includes(requiredApp) &&
-        userProfile.authnContext === 'myhealthevet';
-
-      if (userProfile.accountType === 3 || mhvAccess) {
-        // TODO: Delete the logic around attemptingAppealsAccess once we
-        // resolve the MVI/Appeals Users issues.
-        // if app we are trying to access includes appeals,
-        // bypass the checks for userProfile status
-        let attemptingAppealsAccess;
-
-        if (Array.isArray(requiredApp)) {
-          attemptingAppealsAccess = requiredApp.indexOf('appeals-status') !== -1;
-        } else {
-          attemptingAppealsAccess = requiredApp === 'appeals-status';
-        }
-
-        if (userProfile.status === 'SERVER_ERROR' && !attemptingAppealsAccess) {
-          // If va_profile is null, show a system down message.
-          return <SystemDownView messageLine1="Sorry, our system is temporarily down while we fix a few things. Please try again later."/>;
-        } else if (userProfile.status === 'NOT_FOUND' && !attemptingAppealsAccess) {
-          // If va_profile is "not found", show message that we cannot find the user
-          // in our system.
-          return <SystemDownView messageLine1="We couldn’t find your records with that information." messageLine2="Please call the Vets.gov Help Desk at 1-855-574-7286, TTY: 1-800-877-8339. We're open Monday &#8211; Friday, 8:00 a.m. &#8211; 8:00 p.m. (ET)."/>;
-        }
-
-        // If va_profile has any other value, continue on to check if this user can
-        // use this specific service.
-        if (this.isServiceAvailable()) {
-          // If you have the required service, show the application view.
-          return this.props.children;
-        }
-
-        // If the required service is not available, the component will still be rendered,
-        // but we pass an `isDataAvailable` prop to child components indicating there is
-        // no data. (Only add this prop to React components (functions), and not ordinary
-        // DOM elements.)
-        return React.Children.map(this.props.children, (child) => {
-          const props = typeof child.type === 'function'
-            ? { isDataAvailable: false }
-            : null;
-          return React.cloneElement(child, props);
-        });
-      } else if (userProfile.accountType === 1) {
-        return  <Main renderType="verifyPage"/>;
-      }
-
-      return window.location.replace(signInUrl);
+    if (verify) {
+      return this.renderVerifiedContent();
     }
 
     return this.props.children;
   }
 
   render() {
-    return <div>{this.renderContent()}</div>;
+    return <div>{this.renderWrappedContent()}</div>;
   }
 }
 
 RequiredLoginView.propTypes = {
-  authRequired: PropTypes.number.isRequired,
   serviceRequired: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.array,
   ]).isRequired,
-  userProfile: PropTypes.object.isRequired
+  user: PropTypes.object.isRequired,
+  verify: PropTypes.bool
 };
 
 export default RequiredLoginView;
