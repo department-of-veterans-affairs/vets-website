2ef5c5c7c3aac66e4cc0d72ad56495a1c32296fe
Nick Sullivan <ncksllvn@gmail.com>
2018-03-22 18:05:01 -0400

    WIP - Personalization - Update account to settings (#7477)
    * Change account to settings
    
    * Refactor profile into settings
    
    * Break out components into files
    
    * Remove unused
    
    * Remove unused
    
    * Switch to loa instead of accountType
    
    * Change back to account
    
    * Change to solid button
    
    * Change name, add a comment
    
    * Remove lodash
    
    * Convert main component into a component, not a container

--- src/js/personalization/account-beta/actions/index.js
+++ src/js/personalization/account-beta/actions/index.js
@@ -1,4 +1,3 @@
-import { removeFormApi } from '../../../common/schemaform/save-in-progress/api';
 import { apiRequest } from '../../../common/helpers/api';
 import { getUserData } from '../../../common/helpers/login-helpers';
 
@@ -10,9 +9,6 @@ export const FETCHING_LATEST_MHV_TERMS_FAILURE = 'FETCHING_LATEST_MHV_TERMS_FAIL
 export const ACCEPTING_LATEST_MHV_TERMS = 'ACCEPTING_LATEST_MHV_TERMS';
 export const ACCEPTING_LATEST_MHV_TERMS_SUCCESS = 'ACCEPTING_LATEST_MHV_TERMS_SUCCESS';
 export const ACCEPTING_LATEST_MHV_TERMS_FAILURE = 'ACCEPTING_LATEST_MHV_TERMS_FAILURE';
-export const REMOVING_SAVED_FORM = 'REMOVING_SAVED_FORM';
-export const REMOVING_SAVED_FORM_SUCCESS = 'REMOVING_SAVED_FORM_SUCCESS';
-export const REMOVING_SAVED_FORM_FAILURE = 'REMOVING_SAVED_FORM_FAILURE';
 
 export function updateProfileFields(newState) {
   return {
@@ -24,24 +20,6 @@ export function updateProfileFields(newState) {
 export function profileLoadingFinished() {
   return {
     type: PROFILE_LOADING_FINISHED
-  };
-}
-
-export function removingSavedForm() {
-  return {
-    type: REMOVING_SAVED_FORM
-  };
-}
-
-export function removingSavedFormSuccess() {
-  return {
-    type: REMOVING_SAVED_FORM_SUCCESS
-  };
-}
-
-export function removingSavedFormFailure() {
-  return {
-    type: REMOVING_SAVED_FORM_FAILURE
   };
 }
 
@@ -83,17 +61,5 @@ export function acceptTerms(termsName) {
       },
       () => dispatch({ type: ACCEPTING_LATEST_MHV_TERMS_FAILURE })
     );
-  };
-}
-
-export function removeSavedForm(formId) {
-  return dispatch => {
-    dispatch(removingSavedForm());
-    return removeFormApi(formId)
-      .then(() => {
-        dispatch(removingSavedFormSuccess());
-        getUserData(dispatch);
-      })
-      .catch(() => dispatch(removingSavedFormFailure()));
   };
 }
