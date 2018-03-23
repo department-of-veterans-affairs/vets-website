c2b92d3f29323ac3e48e8cb356b94df6a39f9c15
Jeff Balboni <jeff@adhocteam.us>
2018-03-23 10:38:15 -0400

    Add all the manifests

--- script/build.js
+++ script/build.js
@@ -25,7 +25,11 @@ const webpackConfigGenerator = require('../config/webpack.config');
 const webpackDevServer = require('./metalsmith-webpack').webpackDevServerPlugin;
 const createSettings = require('../config/create-settings');
 const nonceTransformer = require('./metalsmith/nonceTransformer');
-const { getWebpackEntryPoints, getAppManifests } = require('./manifest-helpers.js');
+const {
+  getRoutes,
+  getWebpackEntryPoints,
+  getAppManifests
+} = require('./manifest-helpers.js');
 
 const sourceDir = '../content/pages';
 
@@ -76,7 +80,18 @@ switch (options.buildtype) {
     throw new Error(`Unknown buildtype: '${options.buildtype}'`);
 }
 const manifests = getAppManifests(path.join(__dirname, '..'));
-const webpackConfig = webpackConfigGenerator(options, getWebpackEntryPoints(manifests));
+
+let manifestsToBuild = manifests;
+if (options.entry) {
+  const entryNames = options.entry.split(',').map(name => name.trim());
+  manifestsToBuild = manifests
+    .filter(manifest => entryNames.includes(manifest.entryName));
+}
+
+const webpackConfig = webpackConfigGenerator(
+  options,
+  getWebpackEntryPoints(manifestsToBuild)
+);
 
 // Custom liquid filter(s)
 liquid.filters.humanizeDate = (dt) => moment(dt).format('MMMM D, YYYY');
@@ -100,12 +115,8 @@ if (options.buildtype === 'production') {
   manifests.filter(m => !m.production).forEach(m => {
     ignoreList.push(m.contentPath);
   });
-  ignoreList.push('burials-and-memorials/pre-need/form-10007-apply-for-eligibility.md');
-  ignoreList.push('employment/vocational-rehab-and-employment/application/chapter31.md');
-  ignoreList.push('employment/vocational-rehab-and-employment/application/chapter36.md');
   ignoreList.push('veteran-id-card/how-to-get.md');
   ignoreList.push('veteran-id-card/how-to-upload-photo.md');
-  ignoreList.push('disability-benefits/526/apply-for-increase.md');
 }
 smith.use(ignore(ignoreList));
 
@@ -435,12 +446,12 @@ if (options.watch) {
     })
   );
 
-  const appRewrites = manifests.filter(m => m.rootUrl && !m.skipRewrite).map(m => {
+  const appRewrites = getRoutes(manifests).map(url => {
     return {
-      from: `^${m.rootUrl}(.*)`,
-      to: `${m.rootUrl}/`
+      from: `^${url}(.*)`,
+      to: `${url}/`
     };
-  });
+  }).sort((a, b) => b.from.length - a.from.length);
 
   // If in watch mode, assume hot reloading for JS and use webpack devserver.
   const devServerConfig = {
@@ -448,28 +459,6 @@ if (options.watch) {
     historyApiFallback: {
       rewrites: [
         ...appRewrites,
-        { from: '^/education/apply-for-education-benefits/application/1990[eE](.*)', to: '/education/apply-for-education-benefits/application/1990E/' },
-        { from: '^/education/apply-for-education-benefits/application/1990[nN](.*)', to: '/education/apply-for-education-benefits/application/1990N/' },
-        { from: '^/education/apply-for-education-benefits/application/1990(.*)', to: '/education/apply-for-education-benefits/application/1990/' },
-        { from: '^/education/apply-for-education-benefits/application/1995(.*)', to: '/education/apply-for-education-benefits/application/1995/' },
-        { from: '^/education/apply-for-education-benefits/application/5490(.*)', to: '/education/apply-for-education-benefits/application/5490/' },
-        { from: '^/education/apply-for-education-benefits/application/5495(.*)', to: '/education/apply-for-education-benefits/application/5495/' },
-        { from: '^/facilities(.*)', to: '/facilities/' },
-        { from: '^/discharge-upgrade-instructions(.*)', to: '/discharge-upgrade-instructions/' },
-        { from: '^/gi-bill-comparison-tool(.*)', to: '/gi-bill-comparison-tool/' },
-        { from: '^/education/gi-bill/post-9-11/ch-33-benefit(.*)', to: '/education/gi-bill/post-9-11/ch-33-benefit/' },
-        { from: '^/health-care/apply/application(.*)', to: '/health-care/apply/application/' },
-        { from: '^/health-care/health-records(.*)', to: '/health-care/health-records/' },
-        { from: '^/health-care/messaging(.*)', to: '/health-care/messaging/' },
-        { from: '^/health-care/prescriptions(.*)', to: '/health-care/prescriptions/' },
-        { from: '^/letters(.*)', to: '/letters/' },
-        { from: '^/pension/application/527EZ(.*)', to: '/pension/application/527EZ/' },
-        { from: '^/burials-and-memorials/application/530(.*)', to: '/burials-and-memorials/application/530/' },
-        { from: '^/burials-and-memorials/pre-need/form-10007-apply-for-eligibility(.*)', to: '/burials-and-memorials/pre-need/form-10007-apply-for-eligibility/' },
-        { from: '^/employment/vocational-rehab-and-employment/application/chapter31(.*)', to: '/employment/vocational-rehab-and-employment/application/chapter31/' },
-        { from: '^/employment/vocational-rehab-and-employment/application/chapter36(.*)', to: '/employment/vocational-rehab-and-employment/application/chapter36/' },
-        { from: '^/veteran-id-card/apply(.*)', to: '/veteran-id-card/apply/' },
-        { from: '^/disability-benefits/526/apply-for-increase(.*)', to: '/disability-benefits/526/apply-for-increase/' },
         { from: '^/(.*)', to(context) { return context.parsedUrl.pathname; } }
       ],
     },
--- script/manifest-helpers.js
+++ script/manifest-helpers.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const find = require('find');
 const path = require('path');
 const fs = require('fs');
@@ -12,30 +13,48 @@ function convertUrlToMarkdownPath(root, url) {
 }
 
 function getAppManifests(root) {
-  const files = find.fileSync(/manifest\.json$/, path.join(root, './src/js'));
-  const manifests = files
-    .map(file => require(file))
+  return find.fileSync(/manifest\.json$/, path.join(root, './src/js'))
+    .map(file => {
+      // eslint-disable-next-line import/no-dynamic-require
+      const manifest = require(file);
 
-  manifests.forEach(manifest => {
-    if (manifest.rootUrl) {
-      manifest.contentPage = convertUrlToMarkdownPath(root, manifest.rootUrl);
-    }
-  });
+      manifest.filePath = file;
+      manifest.entryFile = path.relative(root, path.join(path.dirname(file), manifest.entryFile));
+      if (manifest.rootUrl) {
+        manifest.contentPage = convertUrlToMarkdownPath(root, manifest.rootUrl);
+      }
 
-  console.log(manifests);
-  return manifests;
+      return manifest;
+    });
 }
 
 function getWebpackEntryPoints(manifests) {
   return manifests.reduce((apps, next) => {
-    apps[next.bundleName] = next.entryPoint;
+    // eslint-disable-next-line no-param-reassign
+    apps[next.entryName] = `./${next.entryFile}`;
     return apps;
   }, {});
 }
 
-getAppManifests(path.join(__dirname, '..'));
+function getRoutes(manifests) {
+  return manifests.filter(m => !m.noRouting)
+    .map(m => m.rootUrl);
+}
+
+function displayApplications() {
+  const root = path.join(__dirname, '..');
+  getAppManifests(root)
+    .forEach(manifest => {
+      console.log(manifest.appName);
+      console.log(path.relative(root, manifest.filePath));
+      console.log(`Production: ${manifest.production || false}`);
+      console.log('');
+    });
+}
 
 module.exports = {
+  displayApplications,
   getWebpackEntryPoints,
+  getRoutes,
   getAppManifests
 };
