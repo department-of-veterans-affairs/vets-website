9ebea83fd9bad5d060bd6c25dc7ccaca7ebe55cd
egarreau <ergarreau@gmail.com>
2018-03-23 14:35:11 -0500

    revert nightwatch

--- config/nightwatch.js
+++ config/nightwatch.js
@@ -38,7 +38,7 @@ module.exports = {
         acceptSslCerts: true,
         webStorageEnabled: true,
         chromeOptions: {
-          // binary: electron,
+          binary: electron,
           args: ['--window-size=1024,768']
         }
       },
