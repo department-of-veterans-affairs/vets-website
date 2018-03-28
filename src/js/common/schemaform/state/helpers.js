9d8d62f2b4c3d77f4891002e7ef9509bba2d514b
Jeff Balboni <jbalboni@gmail.com>
2018-03-28 19:05:10 -0400

    Update RJSF to fix React 16 prop type warnings and autofill issue (#7496)

--- src/js/common/schemaform/state/helpers.js
+++ src/js/common/schemaform/state/helpers.js
@@ -1,5 +1,5 @@
 import _ from 'lodash/fp';
-import { getDefaultFormState } from 'react-jsonschema-form/lib/utils';
+import { getDefaultFormState } from '@department-of-veterans-affairs/react-jsonschema-form/lib/utils';
 
 import {
   checkValidSchema,
