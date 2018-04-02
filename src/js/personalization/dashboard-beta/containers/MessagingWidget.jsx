ead422fd6b4d4e33a895a431d321a9234894bea7
Ben Shyong <ben.shyong@gmail.com>
2018-04-01 23:42:50 -0700

    stash changes for messaging widget

--- src/js/personalization/dashboard-beta/containers/MessagingWidget.jsx
+++ src/js/personalization/dashboard-beta/containers/MessagingWidget.jsx
@@ -1,32 +1,85 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import classNames from 'classnames';
+import { Link } from 'react-router';
+
+import SortableTable from '../../../common/components/SortableTable';
+import { formattedDate } from '../../../messaging/utils/helpers';
+
+import { fetchFolder } from '../../../messaging/actions';
 
 class MessagingWidget extends React.Component {
   componentDidMount() {
+    this.props.fetchFolder(0, { page: 1, sort: '-sent_date' });
   }
 
   render() {
-    return null;
+    const fields = [
+      { label: 'From', value: 'senderName' },
+      { label: 'Subject line', value: 'subject' },
+      { label: '', value: 'hasAttachment', nonSortable: true },
+      { label: 'Date', value: 'sentDate' }
+    ];
 
-    // return (
-    //   <div>
-    //     <h2>Messages</h2>
-    //   </div>
-    // );
+    const makeMessageLink = (content, id) => {
+      return <Link to={`/${this.props.params.folderName}/${id}`}>{content}</Link>;
+    };
+
+    const { messages } = this.props;
+
+    const data = messages.map(message => {
+      const id = message.messageId;
+      const rowClass = classNames({
+        'messaging-message-row': true,
+        'messaging-message-row--unread':
+          message.readReceipt !== 'READ'
+      });
+
+      const attachmentIcon = message.attachment ? (<i className="fa fa-paperclip" aria-label="Message has an attachment"></i>) : null;
+
+      return {
+        id,
+        rowClass,
+        hasAttachment: attachmentIcon,
+        recipientName: makeMessageLink(message.recipientName, id),
+        senderName: makeMessageLink(message.senderName, id),
+        subject: makeMessageLink(message.subject, id),
+        sentDate: makeMessageLink(formattedDate(message.sentDate), id)
+      };
+    });
+
+    return (
+      <div>
+        <h2>Secure messages</h2>
+
+        <SortableTable
+          className="usa-table-borderless va-table-list msg-table-list"
+          currentSort={this.props.sort}
+          data={data}
+          fields={fields}
+          onSort={this.handleSort}/>
+      </div>
+    );
   }
 }
 
 const mapStateToProps = (state) => {
-  const userState = state.user;
+  const msgState = state.health.msg;
+  const folder = msgState.folders.data.currentItem;
+  const { attributes, messages, pagination, sort } = folder;
 
   return {
-    profile: userState.profile,
-    loginUrl: userState.login.loginUrl,
-    verifyUrl: userState.login.verifyUrl
+    attributes,
+    loading: msgState.loading,
+    messages,
+    sort,
+    pagination,
   };
 };
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  fetchFolder,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(MessagingWidget);
 export { MessagingWidget };
--- src/js/personalization/dashboard-beta/reducers/index.js
+++ src/js/personalization/dashboard-beta/reducers/index.js
@@ -1,7 +1,18 @@
 import claimsAppeals from '../../../claims-status/reducers';
-import prescriptions from '../../../rx/reducers';
+import prescriptions from '../../../rx/reducers/prescriptions';
+import messages from '../../../messaging/reducers/messages';
+import folders from '../../../messaging/reducers/folders';
+import { combineReducers } from 'redux';
 
 export default {
   ...claimsAppeals,
-  ...prescriptions,
+  health: combineReducers({
+    rx: combineReducers({
+      prescriptions,
+    }),
+    msg: combineReducers({
+      messages,
+      folders,
+    }),
+  })
 };
