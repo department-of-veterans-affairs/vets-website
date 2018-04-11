d02f7cf191cd4269974d7f320941fd5aa6784534
Xiaolu Bai <icebunnydoll@gmail.com>
2018-04-11 13:40:34 -0700

    626 test moved to disability-benefits

--- test/disability-benefits/686/00.686-dependent-status.e2e.spec.js
+++ test/disability-benefits/686/00.686-dependent-status.e2e.spec.js
@@ -1,6 +1,6 @@
-const E2eHelpers = require('../../src/platform/testing/e2e/helpers');
-const manifest = require('../../src/js/disability-benefits/686/manifest.json');
-const Timeouts = require('../../src/platform/testing/e2e/timeouts.js');
+const E2eHelpers = require('../../../src/platform/testing/e2e/helpers');
+const manifest = require('../../../src/js/disability-benefits/686/manifest.json');
+const Timeouts = require('../../../src/platform/testing/e2e/timeouts.js');
 
 module.exports = E2eHelpers.createE2eTest((client) => {
   client
