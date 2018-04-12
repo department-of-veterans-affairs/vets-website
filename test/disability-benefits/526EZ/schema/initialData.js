32a5db4813a155c04a5bcc801448d1136ead5c3e
Mark Greenburg <marek.greenburg@gmail.com>
2018-04-12 14:45:44 -0500

    Update initial data to match expected response

--- test/disability-benefits/526EZ/schema/initialData.js
+++ test/disability-benefits/526EZ/schema/initialData.js
@@ -2,56 +2,52 @@ export default {
   // For testing purposes only
   disabilities: [
     {
-      disability: { // Is this extra nesting necessary?
-        diagnosticText: 'PTSD',
-        decisionCode: 'Filler text', // Should this be a string?
-        // Is this supposed to be an array?
-        specialIssues: {
-          specialIssueCode: 'Filler text',
-          specialIssueName: 'Filler text'
+      diagnosticText: 'PTSD',
+      decisionCode: 'Filler text', // Should this be a string?
+      // Is this supposed to be an array?
+      specialIssues: {
+        specialIssueCode: 'Filler text',
+        specialIssueName: 'Filler text'
+      },
+      ratedDisabilityId: '12345',
+      disabilityActionType: 'Filler text',
+      ratingDecisionId: '67890',
+      diagnosticCode: 'Filler text',
+      // Presumably, this should be an array...
+      secondaryDisabilities: [
+        {
+          diagnosticText: 'First secondary disability',
+          disabilityActionType: 'Filler text'
         },
-        ratedDisabilityId: '12345',
-        disabilityActionType: 'Filler text',
-        ratingDecisionId: '67890',
-        diagnosticCode: 'Filler text',
-        // Presumably, this should be an array...
-        secondaryDisabilities: [
-          {
-            diagnosticText: 'First secondary disability',
-            disabilityActionType: 'Filler text'
-          },
-          {
-            diagnosticText: 'Second secondary disability',
-            disabilityActionType: 'Filler text'
-          }
-        ]
-      }
+        {
+          diagnosticText: 'Second secondary disability',
+          disabilityActionType: 'Filler text'
+        }
+      ]
     },
     {
-      disability: { // Is this extra nesting necessary?
-        diagnosticText: 'Second Disability',
-        decisionCode: 'Filler text', // Should this be a string?
-        // Is this supposed to be an array?
-        specialIssues: {
-          specialIssueCode: 'Filler text',
-          specialIssueName: 'Filler text'
+      diagnosticText: 'Second Disability',
+      decisionCode: 'Filler text', // Should this be a string?
+      // Is this supposed to be an array?
+      specialIssues: {
+        specialIssueCode: 'Filler text',
+        specialIssueName: 'Filler text'
+      },
+      ratedDisabilityId: '54321',
+      disabilityActionType: 'Filler text',
+      ratingDecisionId: '09876',
+      diagnosticCode: 'Filler text',
+      // Presumably, this should be an array...
+      secondaryDisabilities: [
+        {
+          diagnosticText: 'First secondary disability',
+          disabilityActionType: 'Filler text'
         },
-        ratedDisabilityId: '54321',
-        disabilityActionType: 'Filler text',
-        ratingDecisionId: '09876',
-        diagnosticCode: 'Filler text',
-        // Presumably, this should be an array...
-        secondaryDisabilities: [
-          {
-            diagnosticText: 'First secondary disability',
-            disabilityActionType: 'Filler text'
-          },
-          {
-            diagnosticText: 'Second secondary disability',
-            disabilityActionType: 'Filler text'
-          }
-        ]
-      }
+        {
+          diagnosticText: 'Second secondary disability',
+          disabilityActionType: 'Filler text'
+        }
+      ]
     }
   ]
 };
