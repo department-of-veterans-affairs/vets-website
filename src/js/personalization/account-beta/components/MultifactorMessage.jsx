f67419244e066c4fbc3993edc53bd0f3bc7a5b0c
Ben Shyong <ben.shyong@gmail.com>
2018-04-16 08:52:02 -1000

    id me settings section

--- src/js/personalization/account-beta/components/MultifactorMessage.jsx
+++ src/js/personalization/account-beta/components/MultifactorMessage.jsx
@@ -14,7 +14,7 @@ export default function MultifactorMessage({ multifactor }) {
   return (
     <div>
       <h4>Want to make your account more secure?</h4>
-      <p><i className="fa fa-check-circle"/> Add an extra layer of security (called 2-factor authentication). This helps to make sure only you can access your account-even if someone gets your password.</p>;
+      <p><i className="fa fa-check-circle"/> Add an extra layer of security (called 2-factor authentication). This helps to make sure only you can access your account—even if someone gets your password.</p>;
       <button className="usa-button-primary" onClick={mfa}>Secure Your Account</button>
     </div>
   );
