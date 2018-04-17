8ee84cf9e7af261bae1a5c2b4b69bddcf8e8f443
Christopher Valarida <chris.valarida@adhocteam.us>
2018-04-17 10:29:10 -0700

    Comment

--- src/js/disability-benefits/526EZ/validations.js
+++ src/js/disability-benefits/526EZ/validations.js
@@ -1,7 +1,7 @@
 import get from '../../common/utils/data-utils/get';
 
 export function requireOneSelected(errors, fieldData, formData, schema, errorMessages, options) {
-  // If fieldData isn't an array, then what?
+  // TODO: If fieldData isn't an array, then what?
   const foundOne = fieldData.some(data => !!get(options.selectedPropName, data));
 
   if (!foundOne) {
