1b3d2323b0f462090709da31f3e5fa50d9a870dc
Jeff Balboni <jeff@adhocteam.us>
2018-04-18 11:15:44 -0400

    Move accessibility tests

---
+++ src/js/claims-status/tests/e2e/01.claim-status-decision.e2e.spec.js
@@ -0,0 +1,33 @@
+const E2eHelpers = require('../../../../platform/testing/e2e/helpers');
+const Timeouts = require('../../../../platform/testing/e2e/timeouts.js');
+const DisabilityHelpers = require('../../../../../test/e2e/disability-helpers');
+const LoginHelpers = require('../../../../../test/e2e/login-helpers');
+
+module.exports = E2eHelpers.createE2eTest(
+  (client) => {
+    const token = LoginHelpers.getUserToken();
+
+    DisabilityHelpers.initClaimsListMock(token);
+    DisabilityHelpers.initClaimDetailMocks(token, true, true, false, null);
+
+    LoginHelpers.logIn(token, client, '/track-claims', 3)
+      .waitForElementVisible('.claim-list-item-container', Timeouts.slow)
+      .axeCheck('.main');
+
+    client
+      .click('.claim-list-item-container:first-child a.usa-button-primary')
+      .waitForElementVisible('body', Timeouts.normal)
+      .waitForElementVisible('.claim-title', Timeouts.slow);
+
+    client
+      .expect.element('.usa-alert').to.be.visible;
+
+    client
+      .expect.element('.usa-alert').text.to.contain('Your claim decision is ready');
+
+    client
+      .expect.element('.disability-benefits-timeline').not.to.be.present;
+
+    client.end();
+  }
+);
