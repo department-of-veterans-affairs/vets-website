8ee461d3f5386270b58419c06506a3ce0c0e6679
Christopher Valarida <chris.valarida@adhocteam.us>
2018-04-19 12:42:48 -0700

    Remove parens because of reasons

--- test/disability-benefits/526EZ/schema/initialData.js
+++ test/disability-benefits/526EZ/schema/initialData.js
@@ -3,7 +3,7 @@ export default {
   disabilities: [
     {
       diagnosticCode: '123',
-      name: 'Post-Traumatic Stress Disorder. (This could also be claimed as Insomnia.)',
+      name: 'Post-Traumatic Stress Disorder. This could also be claimed as Insomnia.',
       ratingPercentage: 30,
       // Is this supposed to be an array?
       specialIssues: [
@@ -29,7 +29,7 @@ export default {
     },
     {
       diagnosticCode: '1234',
-      name: 'Intervertebral Disc Degeneration and Osteoarthritisstatus post-anterior disc fusion L4-S1 and L5-S1 microdiscectomy. (Also claimed as muscle spasms back, herniated disc L4-L5, L5-S1.)',
+      name: 'Intervertebral Disc Degeneration and Osteoarthritisstatus post-anterior disc fusion L4-S1 and L5-S1 microdiscectomy. Also claimed as muscle spasms back, herniated disc L4-L5, L5-S1.',
       ratingPercentage: 20,
       // Is this supposed to be an array?
       specialIssues: [
