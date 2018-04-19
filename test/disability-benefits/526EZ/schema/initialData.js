1c5f45f7506111878e7920ba1f55f08487c9bc22
Claire Hsu <clairehsu86@gmail.com>
2018-04-18 23:44:50 -0700

    Tidy

--- test/disability-benefits/526EZ/schema/initialData.js
+++ test/disability-benefits/526EZ/schema/initialData.js
@@ -62,5 +62,55 @@ export default {
         }
       ]
     }
+  ],
+  selectedDisabilities: [
+    {
+      name: 'PTSD',
+      // Is this supposed to be an array?
+      specialIssues: {
+        specialIssueCode: 'Filler text',
+        specialIssueName: 'Filler text'
+      },
+      ratedDisabilityId: '12345',
+      disabilityActionType: 'Filler text',
+      ratingDecisionId: '67890',
+      diagnosticCode: 'Filler text',
+      classificationCode: 'Filler Code',
+      // Presumably, this should be an array...
+      secondaryDisabilities: [
+        {
+          name: 'First secondary disability',
+          disabilityActionType: 'Filler text'
+        },
+        {
+          name: 'Second secondary disability',
+          disabilityActionType: 'Filler text'
+        }
+      ]
+    },
+    {
+      name: 'Second Disability',
+      // Is this supposed to be an array?
+      specialIssues: {
+        specialIssueCode: 'Filler text',
+        specialIssueName: 'Filler text'
+      },
+      ratedDisabilityId: '54321',
+      disabilityActionType: 'Filler text',
+      ratingDecisionId: '09876',
+      diagnosticCode: 'Filler text',
+      classificationCode: 'Filler Code',
+      // Presumably, this should be an array...
+      secondaryDisabilities: [
+        {
+          name: 'First secondary disability',
+          disabilityActionType: 'Filler text'
+        },
+        {
+          name: 'Second secondary disability',
+          disabilityActionType: 'Filler text'
+        }
+      ]
+    }
   ]
 };
