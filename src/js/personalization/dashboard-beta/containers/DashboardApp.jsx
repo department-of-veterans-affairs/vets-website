788cc0c18dc46b52b9213045276e61ef633880f2
Ben Shyong <ben.shyong@gmail.com>
2018-04-22 15:39:44 -0700

    dashboard content and style updates

--- src/js/personalization/dashboard-beta/containers/DashboardApp.jsx
+++ src/js/personalization/dashboard-beta/containers/DashboardApp.jsx
@@ -54,6 +54,59 @@ class DashboardApp extends React.Component {
             <MessagingWidget/>
             <PrescriptionsWidget/>
           </div>
+          <div>
+            <h2>Manage Your Health and Benefits</h2>
+
+            <ul className="va-nav-linkslist-list">
+              <li>
+                <a href="/health-care/schedule-an-appointment/">
+                  <h4 className="va-nav-linkslist-title">Schedule a VA Appointment</h4>
+                  <p className="va-nav-linkslist-description">Find out how to make a doctor's appointment with a member of your VA health care team online or by phone.</p>
+                </a>
+              </li>
+              <li>
+                <a href="/education/gi-bill/post-9-11/ch-33-benefit">
+                  <h4 className="va-nav-linkslist-title">Check Post-9/11 GI Bill Benefits</h4>
+                  <p className="va-nav-linkslist-description">View and print your statement of benefits.</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Request your Records</h2>
+
+            <ul className="va-nav-linkslist-list">
+              <li>
+                <a href="/discharge-upgrade-instructions/">
+                  <h4 className="va-nav-linkslist-title">How to Apply for a Discharge Upgrade</h4>
+                  <p className="va-nav-linkslist-description">Answer a series of questions to get customized step-by-step instructions on how to apply for a discharge upgrade or correction.</p>
+                </a>
+              </li>
+              <li>
+                <a href="/health-care/health-records/">
+                  <h4 className="va-nav-linkslist-title">Get Your VA Health Records</h4>
+                  <p className="va-nav-linkslist-description">View, download, and print your VA health records.</p>
+                </a>
+              </li>
+              <li>
+                <a href="/download-va-letters/">
+                  <h4 className="va-nav-linkslist-title">Download Your VA Letters</h4>
+                  <p className="va-nav-linkslist-description">Access and download benefit letters and documents proving your status online.</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2>View Your Profile</h2>
+            <p>Review your contact, personal, and military service information—and find out how to make any needed updates or corrections.<br/>
+              <a className="usa-button-primary" href="#">View Your Profile</a>
+            </p>
+
+            <h2>Manage Your Account</h2>
+            <p>View your current account settings—and find out how to update them as needed to access more site tools or add extra security to your account.<br/>
+              <a className="usa-button-primary" href="#">View Your Account Settings</a>
+            </p>
+          </div>
         </div>
       </div>
     );
