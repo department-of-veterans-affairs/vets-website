51eddd90160b78b4ce6390aeb38e98077b497606
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-04-25 10:46:06 -0400

    Add refactored functions

--- src/js/personalization/va-profile-beta/utils/index.js
+++ src/js/personalization/va-profile-beta/utils/index.js
@@ -1,17 +1,87 @@
-import { ADDRESS_TYPES } from '../../../letters/utils/constants';
-import { toGenericAddress, inferAddressType } from '../../../letters/utils/helpers';
+import militaryStatesJson from '../constants/military-states.json';
 
-export { toGenericAddress };
+const MILITARY_STATES = new Set(militaryStatesJson);
 
-export function expandGenericAddress(address) {
-  const transformedAddress = inferAddressType(address);
+/**
+* @typedef {string} AddressType
+*/
 
-  if (transformedAddress.type !== ADDRESS_TYPES.military) return transformedAddress;
+/**
+* @readonly
+* @enum {AddressType}
+*/
+export const ADDRESS_TYPES = {
+  domestic: 'DOMESTIC',
+  international: 'INTERNATIONAL',
+  military: 'MILITARY'
+};
 
-  transformedAddress.militaryPostOfficeTypeCode = transformedAddress.city;
-  transformedAddress.militaryStateCode = transformedAddress.stateCode;
-  delete transformedAddress.city;
-  delete transformedAddress.stateCode;
-  delete transformedAddress.countryName;
-  return transformedAddress;
+/**
+ * @typedef {object} Address
+ * @property {AddressType} type
+ * @property {string} countryName
+ * @property {string} [addressOne]
+ * @property {string} [addressTwo]
+ * @property {string} [addressThree]
+ * @property {string} [addressEffectiveAt]
+ * @property {string} [city]
+ * @property {string} [stateCode]
+ * @property {string} [zipCode]
+ * @property {string} [zipSuffix]
+ * @property {string} [militaryPostOfficeTypeCode]
+ * @property {string} [militaryStateCode]
+ */
+
+/**
+ * Converts an address into a standardized format so that military address follow the same interface as other address types.
+ * @param {Address} address
+ * @returns {Address}
+ */
+export function consolidateAddress(address) {
+  const consolidated = {
+    ...address
+  };
+
+  if (address.type === ADDRESS_TYPES.military) {
+    consolidated.city = consolidated.militaryPostOfficeTypeCode;
+    consolidated.stateCode = consolidated.militaryStateCode;
+    consolidated.countryName = 'USA';
+    delete consolidated.militaryPostOfficeTypeCode;
+    delete consolidated.militaryStateCode;
+  }
+
+  delete consolidated.addressEffectiveDate;
+  return consolidated;
+}
+
+/**
+ * @param {Address} address
+ * @returns {AddressType}
+ */
+export function inferAddressType(address) {
+  if (address.countryName !== 'USA') return ADDRESS_TYPES.international;
+  if (MILITARY_STATES.has(address.stateCode)) return ADDRESS_TYPES.military;
+  return ADDRESS_TYPES.domestic;
+}
+
+/**
+ * Converts an address that may have been modified or standardized and needs its type to be inferred
+ * @param {Address} address
+ * @returns {Address}
+ */
+export function expandAddress(address) {
+  const expanded = {
+    ...address,
+    type: inferAddressType(address)
+  };
+
+  if (expanded.type === ADDRESS_TYPES.military) {
+    expanded.militaryPostOfficeTypeCode = expanded.city;
+    expanded.militaryStateCode = expanded.stateCode;
+    delete expanded.city;
+    delete expanded.stateCode;
+    delete expanded.countryName;
+  }
+
+  return expanded;
 }
