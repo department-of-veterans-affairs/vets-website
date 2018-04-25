9a5a7059062acda6cf89064f96de8c2f7a8fead7
Eugene Doan <eugene@adhocteam.us>
2018-04-25 15:37:07 -0400

    Fixed require path in unauthed flows test.

--- test/e2e/unauthed-flows.e2e.spec.js
+++ test/e2e/unauthed-flows.e2e.spec.js
@@ -3,12 +3,12 @@ const E2eHelpers = require('../../src/platform/testing/e2e/helpers');
 const GibsHelpers = require('../../src/js/post-911-gib-status/tests/post-911-gib-status-helpers');
 const LettersHelpers = require('../../src/js/letters/tests/letters-helpers');
 const MessagingHelpers = require('../../src/js/messaging/tests/messaging-helpers');
-const LoginHelpers = require('../../src/platform/site-wide/login/tests/login-helpers');
+const Auth = require('../../src/platform/testing/e2e/auth');
 const RxHelpers = require('../../src/js/rx/tests/rx-helpers');
 
 module.exports = E2eHelpers.createE2eTest(
   (client) => {
-    const token = LoginHelpers.getUserToken();
+    const token = Auth.getUserToken();
 
     // init mocks
     AccountCreationHelpers.initMHVTermsMocks(token);
@@ -29,7 +29,7 @@ module.exports = E2eHelpers.createE2eTest(
 
     // Test flow for unauthed and LOA1 users
     appPaths.forEach(path => {
-      LoginHelpers.testUnauthedUserFlow(client, path);
+      Auth.testUnauthedUserFlow(client, path);
     });
 
     client.end();
