2162b0f2b3168a902d37ecc6be8afbbe8c8596e2
Eugene Doan <eugene@adhocteam.us>
2018-04-25 00:22:16 -0400

    Renamed entry files.

--- src/platform/site-wide/feedback/index.js
+++ src/platform/site-wide/feedback/index.js
@@ -1,20 +1,21 @@
 /**
  * This application creates a widget for providing feedback on Vets.gov
- * 
+ *
  * @module platform/site-wide/feedback
  */
 
 import React from 'react';
 import { Provider } from 'react-redux';
-import Main from './containers/Main';
+
 import startReactApp from '../../startup/react';
+import Main from './containers/Main';
 
 /**
  * Sets up the feedback widget with the given store at feedback-root
  *
  * @param {Redux.Store} store The common store used on the site
  */
-export default function createFeedbackWidget(store) {
+export default function startFeedbackWidget(store) {
   const feedbackRoot = document.getElementById('feedback-root');
   if (!feedbackRoot) return;
 
