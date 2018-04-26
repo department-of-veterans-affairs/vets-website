3fee3297d50efe3f03c8c2b52d4b3132ce0e2672
Jeff Balboni <jeff@adhocteam.us>
2018-04-26 10:00:35 -0400

    Change sass file name

--- src/platform/site-wide/user-nav/index.js
+++ src/platform/site-wide/user-nav/index.js
@@ -8,7 +8,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import './sass/login.scss';
+import './sass/user-nav.scss';
 import startReactApp from '../../startup/react';
 import Main from './containers/Main';
 
