d5a30c49204122ae6892f1d25facd9dc90668841
Claire Hsu <clairehsu86@gmail.com>
2018-05-07 15:24:04 -0700

    Tidy

--- src/js/disability-benefits/526EZ/containers/ConfirmationPage.jsx
+++ src/js/disability-benefits/526EZ/containers/ConfirmationPage.jsx
@@ -40,7 +40,7 @@ class ConfirmationPage extends React.Component {
         <p>We may contact you if we have questions or need more information. You can print this page for your records.</p>
         
         <p>**If you need us to get your private medical records from your doctor,** you’ll need to fill out an Authorization to Disclose Information to the VA (VA Form 21-4142). If you didn’t upload this form to your application, you can download it here and mail it in.
-        [Download VA Form 21-4142](https://www.vba.va.gov/pubs/forms/VBA-21-4142-ARE.pdf).</p><br>
+          [Download VA Form 21-4142](https://www.vba.va.gov/pubs/forms/VBA-21-4142-ARE.pdf).</p><br/>
 
       Please print the form, fill it out, and send it to:<br>
         
