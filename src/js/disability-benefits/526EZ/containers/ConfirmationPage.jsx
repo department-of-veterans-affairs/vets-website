f32402989005694b5ab95a72981fe53322fb9aad
Claire Hsu <clairehsu86@gmail.com>
2018-05-07 15:33:38 -0700

    Tidy

--- src/js/disability-benefits/526EZ/containers/ConfirmationPage.jsx
+++ src/js/disability-benefits/526EZ/containers/ConfirmationPage.jsx
@@ -38,8 +38,7 @@ class ConfirmationPage extends React.Component {
         <h3 className="confirmation-page-title">Your claim has been submitted</h3>
         <p>We usually process claims within <strong>99 days</strong>.</p>
         <p>We may contact you if we have questions or need more information. You can print this page for your records.</p>
-        <p>**If you need us to get your private medical records from your doctor,** you’ll need to fill out an Authorization to Disclose Information to the VA (VA Form 21-4142). If you didn’t upload this form to your application, you can download it here and mail it in.
-          [Download VA Form 21-4142](https://www.vba.va.gov/pubs/forms/VBA-21-4142-ARE.pdf).</p><br/>
+        <p><strong>If you need us to get your private medical records from your doctor,</strong> you’ll need to fill out an Authorization to Disclose Information to the VA (VA Form 21-4142). If you didn’t upload this form to your application, you can download it here and mail it in.  <a href="https://www.vba.va.gov/pubs/forms/VBA-21-4142-ARE.pdf">Download VA Form 21-4142</a>.</p><br/>
 
         Please print the form, fill it out, and send it to:<br/>
         <dl className="va-address-block">
