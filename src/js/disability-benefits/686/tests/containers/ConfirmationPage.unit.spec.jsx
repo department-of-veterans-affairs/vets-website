c4be41f7ab5c7211b1665d3df05c0eae04e7f4c2
Rian Fowler <rian.fowler@adhocteam.us>
2018-05-08 10:27:28 -0600

    update unit test

--- src/js/disability-benefits/686/tests/containers/ConfirmationPage.unit.spec.jsx
+++ src/js/disability-benefits/686/tests/containers/ConfirmationPage.unit.spec.jsx
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 
 import { ConfirmationPage } from '../../containers/ConfirmationPage';
 
-describe.only('686 <ConfirmationPage>', () => {
+describe('686 <ConfirmationPage>', () => {
   it('should render', () => {
     const form = {
       submission: {
@@ -30,7 +30,7 @@ describe.only('686 <ConfirmationPage>', () => {
     expect(tree.find('.confirmation-page-title').text()).to.contain('Claim submitted');
     expect(tree.find('.claim-list').exists()).to.be.true;
     expect(tree.find('span').at(2).text()).to.contain('3702390024');
-    expect(tree.find('span').at(3).text()).to.contain('Jan. 1, 2010');
+    expect(tree.find('span').at(3).text()).to.contain('01/01/2010');
     expect(tree.find('p').first().text()).to.contain('We process applications in the order we receive them.');
   });
 
