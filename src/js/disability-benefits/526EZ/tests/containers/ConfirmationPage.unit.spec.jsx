9c4f1cfb29f8b5e02645f3ce5d348db95cfba22c
Claire Hsu <clairehsu86@gmail.com>
2018-05-09 22:04:49 -0700

    Update test

--- src/js/disability-benefits/526EZ/tests/containers/ConfirmationPage.unit.spec.jsx
+++ src/js/disability-benefits/526EZ/tests/containers/ConfirmationPage.unit.spec.jsx
@@ -21,9 +21,9 @@ describe('Disability Benefits 526EZ <ConfirmationPage>', () => {
       <ConfirmationPage form={form}/>
     );
 
-    expect(tree.find('.confirmation-page-title').text()).to.equal('Claim submitted');
+    expect(tree.find('.confirmation-page-title').text()).to.equal('Your claim has been submitted.');
     expect(tree.find('span').at(1).text().trim()).to.equal('For Sally  Alphonse');
-    expect(tree.find('p').at(0).text()).to.contain('We usually process claims within 103 days.');
+    expect(tree.find('p').at(0).text()).to.contain('We usually process claims within 99 days.');
     expect(tree.find('p').at(1).text()).to.contain('We may contact you if we have questions or need more information. You can print this page for your records.');
     expect(tree.find('.disability-list').text()).to.contain('PTSD');
   });
