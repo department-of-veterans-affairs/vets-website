64e07c6ff7524bb0b9250deaa9ad5856d68d857d
Eugene Doan <eugene@adhocteam.us>
2018-05-14 16:48:41 -0400

    Implemented initial set of messages for ineligibility states.

--- src/platform/user/authorization/containers/MHVApp.jsx
+++ src/platform/user/authorization/containers/MHVApp.jsx
@@ -15,6 +15,46 @@ import {
 
 const TERMS_NAME = 'mhvac';
 
+/* eslint-disable camelcase */
+const INELIGIBLE_MESSAGES = {
+  needs_identity_verification: null,
+
+  needs_ssn_resolution: {
+    headline: 'We can’t give you access to the Vets.gov health tools',
+    content: (
+      <div>
+        <p>We’re sorry. We can’t match your Social Security number to our Veteran records. We won’t be able to give you access to the Vets.gov health tools until we can match your information to verify your identity.</p>
+        <p>Please check the information you entered and make sure it matches the information in your records. If you feel you’ve entered your information correctly, and it’s still not matching, please contact your nearest VA medical center. Let them know you need to verify the information in your records, and update it as needed. The operator, or a patient advocate, can connect with you with the right person who can help.</p>
+      </div>
+    )
+  },
+
+  needs_va_patient: {
+    headline: 'We can’t give you access to the Vets.gov health tools',
+    content: (
+      <div>
+        <p>We’re sorry. We can’t give you access to the Vets.gov health tools because we can’t verify that you’re a VA patient. Only patients who’ve received care at a VA health facility can use these tools.</p>
+        <p>If you’ve received care at a VA medical center, clinic, or Vet center, please call that facility to find out if you’re in their records.</p>
+      </div>
+    )
+  },
+
+  state_ineligible: null,
+
+  country_ineligible: null,
+
+  has_multiple_active_mhv_ids: {
+    headline: <span>It looks like you have more than one My Health<em>e</em>Vet account</span>,
+    content: (
+      <div>
+        <p>We’re sorry. We can’t give you access to the Vets.gov health tools because we’ve found more than one active account for you in the My Health<em>e</em>Vet system.</p>
+        <p>Please call the My HealtheVet Help Desk at 1-877-327-0022 (TTY: 1-800-877-8339), 7:00 a.m. - 7:00 p.m. (CT), and ask for help to delete any extra accounts in the system.</p>
+      </div>
+    )
+  },
+};
+/* eslint-enable camelcase */
+
 export class MHVApp extends React.Component {
   componentDidMount() {
     const { account } = this.props;
@@ -46,7 +86,7 @@ export class MHVApp extends React.Component {
 
   hasService = () => this.props.availableServices.includes(this.props.serviceRequired);
 
-  isIneligible = () => this.props.account.state === 'ineligible';
+  isIneligible = () => this.props.account.state in INELIGIBLE_MESSAGES;
 
   handleAccountState = () => {
     if (this.isIneligible()) { return; }
@@ -56,6 +96,18 @@ export class MHVApp extends React.Component {
     } else if (!this.hasAccount()) {
       this.props.createMHVAccount();
     }
+  }
+
+  renderIneligibleMessage = (ineligibleState) => {
+    const alertProps = INELIGIBLE_MESSAGES[ineligibleState];
+
+    return alertProps && (
+      <AlertBox
+        headline={alertProps.headline}
+        content={alertProps.content}
+        isVisible
+        status="error"/>
+    );
   }
 
   render() {
@@ -87,6 +139,10 @@ export class MHVApp extends React.Component {
           isVisible
           status="error"/>
       );
+    }
+
+    if (this.isIneligible()) {
+      return this.renderIneligibleMessage(this.props.account.state);
     }
 
     if (this.needsTermsAcceptance()) {
