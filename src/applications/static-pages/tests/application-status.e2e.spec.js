eb3fd0c57ae69350e0c31931faaf76f13b95ec5b
Jeff Balboni <jeff@adhocteam.us>
2018-05-17 11:10:07 -0400

    All forms

--- src/applications/static-pages/tests/application-status.e2e.spec.js
+++ src/applications/static-pages/tests/application-status.e2e.spec.js
@@ -1,59 +1,6 @@
 const E2eHelpers = require('../../../platform/testing/e2e/helpers');
 const Timeouts = require('../../../platform/testing/e2e/timeouts.js');
 const Auth = require('../../../platform/testing/e2e/auth');
-const mock = require('../../../platform/testing/e2e/mock-helpers');
-
-/* eslint-disable camelcase */
-function initUserMock(token, level) {
-  mock(token, {
-    path: '/v0/user',
-    verb: 'get',
-    value: {
-      data: {
-        attributes: {
-          profile: {
-            authn_context: 'idme',
-            email: 'fake@fake.com',
-            loa: { current: level },
-            first_name: 'Jane',
-            middle_name: '',
-            last_name: 'Doe',
-            gender: 'F',
-            birth_date: '1985-01-01',
-            verified: level === 3
-          },
-          veteran_status: {
-            status: 'OK',
-            is_veteran: true,
-          },
-          in_progress_forms: [
-            {
-              form: '1010ez',
-              metadata: {}
-            },
-            {
-              form: '21P-527EZ',
-              metadata: {}
-            }
-          ],
-          prefills_available: [],
-          services: ['facilities', 'hca', 'edu-benefits', 'evss-claims', 'user-profile', 'rx', 'messaging'],
-          mhv_account_state: 'upgraded',
-          health_terms_current: true,
-          va_profile: {
-            status: 'OK',
-            birth_date: '19511118',
-            family_name: 'Hunter',
-            gender: 'M',
-            given_names: ['Julio', 'E'],
-            active_status: 'active'
-          }
-        }
-      }
-    }
-  });
-}
-/* eslint-enable camelcase */
 
 function testStatus(client, page, url) {
   client
@@ -81,16 +28,64 @@ module.exports = E2eHelpers.createE2eTest(
 
     Auth.logIn(token, client, '/', 3);
 
-    initUserMock(token, 3);
+    /* eslint-disable camelcase */
+    client.mockData({
+      path: '/v0/user',
+      verb: 'get',
+      value: {
+        data: {
+          attributes: {
+            profile: {
+              authn_context: 'idme',
+              email: 'fake@fake.com',
+              loa: { current: 3 },
+              first_name: 'Jane',
+              middle_name: '',
+              last_name: 'Doe',
+              gender: 'F',
+              birth_date: '1985-01-01',
+              verified: true
+            },
+            veteran_status: {
+              status: 'OK',
+              is_veteran: true,
+            },
+            in_progress_forms: [
+              {
+                form: '1010ez',
+                metadata: {}
+              },
+              {
+                form: '22-1995',
+                metadata: {}
+              },
+              {
+                form: '21P-530',
+                metadata: {}
+              },
+              {
+                form: '21P-527EZ',
+                metadata: {}
+              }
+            ],
+            prefills_available: [],
+            services: ['facilities', 'hca', 'edu-benefits', 'evss-claims', 'user-profile', 'rx', 'messaging'],
+            mhv_account_state: 'upgraded',
+            health_terms_current: true,
+            va_profile: {
+              status: 'OK',
+              birth_date: '19511118',
+              family_name: 'Hunter',
+              gender: 'M',
+              given_names: ['Julio', 'E'],
+              active_status: 'active'
+            }
+          }
+        }
+      }
+    }, token);
+    /* eslint-enable camelcase */
 
-    // const pensionPages = new Set(['/pension/', '/pension/apply/', '/pension/eligibility/']);
-    // const healthcarePages = new Set(['/health-care/', '/health-care/apply/', '/health-care/eligibility/']);
-    // const burialPages = new Set([
-    //   '/burials-and-memorials/',
-    //   '/burials-and-memorials/survivor-and-dependent-benefits/',
-    //   '/burials-and-memorials/survivor-and-dependent-benefits/burial-costs/'
-    // ]);
-    // const eduPages = new Set(['/education/', '/education/apply/', '/education/eligibility/']);
     testStatus(client, '/health-care/apply', '/health-care/apply/application/resume');
     testStatus(client, '/health-care/', '/health-care/apply/application/resume');
     testStatus(client, '/health-care/eligibility', '/health-care/apply/application/resume');
@@ -98,6 +93,13 @@ module.exports = E2eHelpers.createE2eTest(
     testStatus(client, '/pension/apply', '/pension/application/527EZ/resume');
     testStatus(client, '/pension/', '/pension/application/527EZ/resume');
     testStatus(client, '/pension/eligibility', '/pension/application/527EZ/resume');
+
+    testStatus(client, '/burials-and-memorials/', '/burials-and-memorials/application/530/resume');
+    testStatus(client, '/burials-and-memorials/survivor-and-dependent-benefits/burial-costs', '/burials-and-memorials/application/530/resume');
+
+    testStatus(client, '/education/apply', '/education/apply-for-education-benefits/application/1995/resume');
+    testStatus(client, '/education/', '/education/apply-for-education-benefits/application/1995/resume');
+    testStatus(client, '/education/eligibility', '/education/apply-for-education-benefits/application/1995/resume');
     client.end();
   }
 );
