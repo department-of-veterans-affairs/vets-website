d8280036f3affefa822b05e802ede6cc98019825
Ben Shyong <ben.shyong@gmail.com>
2018-05-16 23:36:07 -0700

    move styles to allow proper parsing

--- src/applications/personalization/dashboard/dashboard-entry.jsx
+++ src/applications/personalization/dashboard/dashboard-entry.jsx
@@ -1,6 +1,8 @@
 import '../../../platform/polyfills';
 import '../../user-profile/sass/user-profile.scss';
+import '../../claims-status/sass/claims-status.scss';
 import './sass/dashboard.scss';
+import '../../messaging/sass/messaging.scss';
 
 import startApp from '../../../platform/startup';
 
