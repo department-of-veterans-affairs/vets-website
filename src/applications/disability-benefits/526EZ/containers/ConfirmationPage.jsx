ef6ee578c747d86f3da72a0fbba9dd5defa993d0
Mark Greenburg <marek.greenburg@gmail.com>
2018-05-18 09:33:46 -0500

    Update confirmation page address formatting

--- src/applications/disability-benefits/526EZ/containers/ConfirmationPage.jsx
+++ src/applications/disability-benefits/526EZ/containers/ConfirmationPage.jsx
@@ -44,12 +44,12 @@ class ConfirmationPage extends React.Component {
         download it here and mail it in.</p>
         <a href="https://www.vba.va.gov/pubs/forms/VBA-21-4142-ARE.pdf" target="_blank">
         Download VA Form 21-4142</a>
-        <p>
-          Please print the form, fill it out, and send it to:<br/>
+        <p>Please print the form, fill it out, and send it to:</p>
+        <p className="va-address-block">
           Department of Veterans Affairs<br/>
           Claims Intake Center<br/>
           PO Box 4444<br/>
-          Janesville, WI 53547-4444<br/>
+          Janesville, WI 53547-4444
         </p>
         <h4 className="confirmation-guidance-heading">
           What happens after I apply?
