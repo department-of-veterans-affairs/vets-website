fafc8c5ed06d6cd93bb5656bbd0aecf60d7d7102
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-05-21 00:16:23 -0400

    update function signatures

--- src/applications/personalization/dashboard-beta/containers/ClaimsAppealsWidget.jsx
+++ src/applications/personalization/dashboard-beta/containers/ClaimsAppealsWidget.jsx
@@ -82,15 +82,15 @@ class ClaimsAppealsWidget extends React.Component {
   }
 
   renderWidgetDowntimeNotification = (appName) => {
-    return (status, downtimeWindow, downtimeMap, children) => {
-      switch (status) {
+    return (downtime, children) => {
+      switch (downtime.status) {
         case 'down':
           return (
             <div>
               <AlertBox
                 content={<div>
                   <h4 className="usa-alert-heading">{appName} is down for maintenance</h4>
-                  <p>We’re making some updates to our {appName.toLowerCase()} tool. We’re sorry it’s not working right now and hope to be finished by {downtimeWindow.startTime.format('MMMM Do')}, {downtimeWindow.endTime.format('LT')}. Please check back soon.</p>
+                  <p>We’re making some updates to our {appName.toLowerCase()} tool. We’re sorry it’s not working right now and hope to be finished by {downtime.startTime.format('MMMM Do')}, {downtime.endTime.format('LT')}. Please check back soon.</p>
                 </div>}
                 isVisible
                 status="warning"/>
