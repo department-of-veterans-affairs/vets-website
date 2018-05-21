13d7afd2dc4fa406f94ce6f7c0210acf6563231c
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-05-21 00:49:27 -0400

    Add comments

--- src/platform/monitoring/DowntimeNotification/util/helpers.js
+++ src/platform/monitoring/DowntimeNotification/util/helpers.js
@@ -1,6 +1,12 @@
 import moment from 'moment';
 import serviceStatus from '../config/serviceStatus';
 
+/**
+ * Derives downtime status based on a time range
+ * @param {string|Date|Moment} startTime
+ * @param {string|Date|Moment} endTime
+ * @returns {string} A service status
+ */
 export function getStatusForTimeframe(startTime, endTime) {
   const now = moment();
   const hasStarted = now.isSameOrAfter(startTime);
@@ -20,6 +26,11 @@ export function getStatusForTimeframe(startTime, endTime) {
   return serviceStatus.ok;
 }
 
+/**
+ * Creates a Map of downtime information using the "externalService" property as keys
+ * @param {Array} maintenanceWindows The raw JSON data from the API
+ * @returns {Map}
+ */
 export function createServiceMap(maintenanceWindows = []) {
   const serviceMap = new Map();
 
@@ -47,6 +58,12 @@ export function createServiceMap(maintenanceWindows = []) {
   return serviceMap;
 }
 
+/**
+ * Determines the downtime with the soonest startTime by using a service map to look up downtime information for each service in a list of service names
+ * @param {Map} serviceMap A Map as created by createServiceMap
+ * @param {Array<string>} serviceNames A list of external services
+ * @returns {object} A downtime object containing properties "externalService", "status", "startTime", and "endTime"
+ */
 export function getSoonestDowntime(serviceMap, serviceNames) {
   return serviceNames
     .map(serviceName => serviceMap.get(serviceName))
