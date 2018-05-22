19104e960f8fa7f0c873a8df82464d21b30cb00a
Rian Fowler <rian.fowler@adhocteam.us>
2018-05-22 09:16:15 -0600

    add pattern error messages

--- src/applications/hca/config/form.js
+++ src/applications/hca/config/form.js
@@ -173,9 +173,16 @@ const formConfig = {
           uiSchema: {
             'ui:description': applicantDescription,
             veteranFullName: _.merge(fullNameUI, {
+              first: {
+                'ui:errorMessages': {
+                  minLength: 'Please provide a valid name. Must be at least 1 character.',
+                  pattern: 'Please provide a valid name. Must be at least 1 character.'
+                }
+              },
               last: {
                 'ui:errorMessages': {
-                  minLength: 'Please provide a valid name. Must be at least 2 characters.'
+                  minLength: 'Please provide a valid name. Must be at least 2 characters.',
+                  pattern: 'Please provide a valid name. Must be at least 2 characters.'
                 }
               }
             }),
@@ -301,7 +308,18 @@ const formConfig = {
           title: 'Permanent address',
           initialData: {},
           uiSchema: {
-            veteranAddress: addressUI('Permanent address', true)
+            veteranAddress: _.merge(addressUI('Permanent address', true), {
+              street: {
+                'ui:errorMessages': {
+                  pattern: 'Please provide a valid street. Must be at least 1 character.'
+                }
+              },
+              city: {
+                'ui:errorMessages': {
+                  pattern: 'Please provide a valid city. Must be at least 1 character.'
+                }
+              }
+            })
           },
           schema: {
             type: 'object',
@@ -526,7 +544,7 @@ const formConfig = {
           title: 'Spouse’s information',
           initialData: {},
           depends: (formData) => formData.discloseFinancialInformation && formData.maritalStatus &&
-            (formData.maritalStatus.toLowerCase() === 'married' || formData.maritalStatus.toLowerCase() === 'separated'),
+          (formData.maritalStatus.toLowerCase() === 'married' || formData.maritalStatus.toLowerCase() === 'separated'),
           uiSchema: {
             'ui:title': 'Spouse’s information',
             'ui:description': 'Please fill this out to the best of your knowledge. The more accurate your responses, the faster we can process your application.',
@@ -640,7 +658,7 @@ const formConfig = {
               'ui:title': 'Spouse income',
               'ui:options': {
                 hideIf: (formData) => !formData.maritalStatus ||
-                  (formData.maritalStatus.toLowerCase() !== 'married' && formData.maritalStatus.toLowerCase() !== 'separated')
+                (formData.maritalStatus.toLowerCase() !== 'married' && formData.maritalStatus.toLowerCase() !== 'separated')
               },
               spouseGrossIncome: _.merge(currencyUI('Spouse gross annual income from employment'), {
                 'ui:required': (formData) => formData.maritalStatus && (formData.maritalStatus.toLowerCase() === 'married' || formData.maritalStatus.toLowerCase() === 'separated')
