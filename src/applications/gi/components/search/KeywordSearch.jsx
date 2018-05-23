a6c3e749978d10c8f3bd1315215666b89637d953
Xiaolu Bai <icebunnydoll@gmail.com>
2018-05-22 21:35:40 -0700

    updates auto suggest -> downshift

--- src/applications/gi/components/search/KeywordSearch.jsx
+++ src/applications/gi/components/search/KeywordSearch.jsx
@@ -1,34 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Autosuggest from 'react-autosuggest-ie11-compatible';
 import { debounce } from 'lodash';
 import recordEvent from '../../../../platform/monitoring/record-event';
+import Downshift from 'downshift';
 
 export class KeywordSearch extends React.Component {
   constructor(props) {
     super(props);
-    this.clickedSuggestionValue = this.clickedSuggestionValue.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleKeyUp = this.handleKeyUp.bind(this);
-    this.handleSuggestionSelected = this.handleSuggestionSelected.bind(this);
-    this.renderSuggestion = this.renderSuggestion.bind(this);
-    this.shouldRenderSuggestions = this.shouldRenderSuggestions.bind(this);
-    this.handleInputBlur = this.handleInputBlur.bind(this);
-
     this.handleFetchSuggestion = debounce(
       this.handleFetchSuggestion.bind(this),
-      250, { trailing: true }
+      150, { trailing: true }
     );
   }
 
-  componentDidMount() {
-    const searchQuery = this.props.location && this.props.location.query;
-    if (searchQuery && searchQuery.name) {
-      this.handleChange(null, { newValue: searchQuery.name, method: 'enter' });
-    }
-  }
-
-  handleKeyUp(e) {
+  handleKeyUp = (e) => {
     const { onFilterChange, autocomplete } = this.props;
     if ((e.which || e.keyCode) === 13) {
       e.target.blur();
@@ -36,8 +21,21 @@ export class KeywordSearch extends React.Component {
     }
   }
 
-  handleChange(event, data) {
-    this.props.onUpdateAutocompleteSearchTerm(data.newValue);
+  handleChange = (e, searchQuery) => {
+    if (e) {
+      let value;
+      if (typeof (e) === 'string') {
+        value = e;
+      } else {
+        value = e.target.value;
+      }
+      this.props.onUpdateAutocompleteSearchTerm(value);
+      this.handleFetchSuggestion({ value });
+    }
+    if (searchQuery) {
+      this.props.onUpdateAutocompleteSearchTerm(searchQuery);
+      this.handleFetchSuggestion({ searchQuery });
+    }
   }
 
   handleFetchSuggestion({ value }) {
@@ -45,35 +43,20 @@ export class KeywordSearch extends React.Component {
     this.props.onFetchAutocompleteSuggestions(value, version);
   }
 
-  handleSuggestionSelected(event, data) {
+  handleSuggestionSelected = (searchQuery) => {
     recordEvent({
       event: 'gibct-autosuggest',
-      'gibct-autosuggest-value': data.suggestionValue,
+      'gibct-autosuggest-value': searchQuery,
     });
-    this.props.onFilterChange('name', data.suggestionValue);
+    this.props.onFilterChange('name', searchQuery);
   }
 
-  clickedSuggestionValue(suggestion) {
-    return suggestion.label;
-  }
-
-  shouldRenderSuggestions(searchTerm) {
-    const checkLength = searchTerm.trim().length > 2;
-    const finished = true; //! this.props.autocomplete.inProgress;
-    return checkLength && finished;
-  }
-
-  handleInputBlur() {
+  handleInputBlur = () => {
     this.props.onFilterChange('name', this.props.autocomplete.searchTerm);
-  }
-
-  renderSuggestion(suggestion) {
-    return <div>{suggestion.label}</div>;
   }
 
   render() {
     const { suggestions, searchTerm } = this.props.autocomplete;
-
     return (
       <div className="keyword-search">
         <label
@@ -82,22 +65,42 @@ export class KeywordSearch extends React.Component {
           htmlFor="institution-search">
           {this.props.label}
         </label>
-        <Autosuggest
-          getSuggestionValue={this.clickedSuggestionValue}
-          highlightFirstSuggestion
-          onSuggestionsClearRequested={this.props.onClearAutocompleteSuggestions}
-          onSuggestionSelected={this.handleSuggestionSelected}
-          onSuggestionsFetchRequested={this.handleFetchSuggestion}
-          renderSuggestion={this.renderSuggestion}
-          shouldRenderSuggestions={this.shouldRenderSuggestions}
-          suggestions={suggestions}
-          inputProps={{
-            value: searchTerm,
-            onChange: this.handleChange,
-            onKeyUp: this.handleKeyUp,
-            'aria-labelledby': 'institution-search-label',
-            onBlur: this.handleInputBlur,
-          }}/>
+        <Downshift onChange={this.handleChange} onSelect={(item) => this.handleSuggestionSelected(item)}>
+          {({
+            getInputProps,
+            getItemProps,
+            isOpen,
+            inputValue,
+            highlightedIndex
+          }) => (
+            <div>
+              <input {...getInputProps({
+                value: searchTerm || inputValue,
+                onChange: this.handleChange,
+                onKeyUp: this.handleKeyUp,
+                'aria-labelledby': 'institution-search-label',
+                onBlur: this.handleInputBlur
+              })}/>
+              {isOpen && (<div className="suggestions-list">
+                {suggestions.map((item, index) => (
+                  <div
+                    key={index}
+                    className="suggestion"
+                    {...getItemProps({
+                      item: item.label,
+                      style: {
+                        backgroundColor:
+                        highlightedIndex === index ? '#9bdaf1' : 'white',
+                      },
+                    })}>
+                    {item.label}
+                  </div>
+                ))}
+              </div>
+              )}
+            </div>
+          )}
+        </Downshift>
       </div>
     );
   }
