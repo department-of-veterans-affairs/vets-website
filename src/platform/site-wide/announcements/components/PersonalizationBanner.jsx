c35276272f1810e80fe18907438c8563422105fd
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-05-23 19:26:38 -0400

    Add the new badge

--- src/platform/site-wide/announcements/components/PersonalizationBanner.jsx
+++ src/platform/site-wide/announcements/components/PersonalizationBanner.jsx
@@ -4,7 +4,7 @@ export default function PersonalizationBanner({ dismiss, isLoggedIn }) {
   if (!isLoggedIn) return <div/>;
   return (
     <div className="personalization-announcement">
-      <a onClick={dismiss} href="/dashboard">Check out your new personalized homepage</a>
+      <span className="usa-label va-label-primary">New</span> <a onClick={dismiss} href="/dashboard">Check out your new personalized homepage</a>
       <button type="button" onClick={dismiss} className="va-modal-close">
         <i className="fa fa-close"/>
       </button>
