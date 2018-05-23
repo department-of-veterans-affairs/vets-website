fb2b8ff56fc834accd5c069ebb9816d2e7fa692e
Nick Sullivan <ncksllvn@gmail.com>
2018-05-23 18:46:27 -0400

    Downtime Notifications refactor (#7757)
    * refactor to move map logic into helpers
    
    * separate out components
    
    * update function signatures
    
    * Fix tests, lint errors
    
    * Fix tests
    
    * Add comments
    
    * update tests after refactor
    
    * update dashboard custom render
    
    * add test for custom render prop
    
    * use undefined instead of null
    
    * change lifecycle methods
    
    * remove unused
    
    * update embarrassing typo
    
    * update embarrassing typo
    
    * change .find to .some
    
    * cleanup render
    
    * update typo
    
    * remove semicolon
    
    * Move sessionstorage into reducer
    
    * Rename reducer tests
    
    * Whoops, rename reducer again
    
    * Move createServiceMap to reducer
    
    * Increase times for slow runs
    
    * Move downtime helper to mapStateToProps
    
    * update function params
    
    * Update typo
    
    * Move session logic into actions
    
    * Update/Add unit tests

--- src/platform/monitoring/DowntimeNotification/tests/reducer.unit.spec.js
+++ src/platform/monitoring/DowntimeNotification/tests/reducer.unit.spec.js
@@ -4,18 +4,17 @@ import { RECEIVE_SCHEDULED_DOWNTIME } from '../actions';
 
 describe('Downtime Notification Reducer', () => {
   describe('scheduledDowntime', () => {
-    const scheduledDowntimeInterface = ['isReady', 'values'];
+    const scheduledDowntimeInterface = ['isReady', 'serviceMap', 'isPending', 'dismissedDowntimeWarnings'];
     it('returns the initial state', () => {
       const result = scheduledDowntime(undefined, { type: 'SHOULD_NOT_MATTER' });
       expect(result).to.have.all.keys(scheduledDowntimeInterface);
     });
     it('flips the isReady flag and sets value when RECEIVE_SCHEDULED_DOWNTIME is dispatched', () => {
-      const value = [];
-      const action = { type: RECEIVE_SCHEDULED_DOWNTIME, value };
+      const action = { type: RECEIVE_SCHEDULED_DOWNTIME, data: [] };
       const result = scheduledDowntime(undefined, action);
 
       expect(result).to.have.all.keys(scheduledDowntimeInterface);
-      expect(result.values).to.be.equal(value);
+      expect(result.serviceMap).to.be.instanceof(Map);
     });
   });
 });
