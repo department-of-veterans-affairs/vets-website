847c3ab80a9fe16e3a48e5135e46fbff2db68026
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-05-29 16:06:46 -0400

    fix test title

--- src/platform/site-wide/user-nav/tests/user-nav.e2e.spec.js
+++ src/platform/site-wide/user-nav/tests/user-nav.e2e.spec.js
@@ -14,7 +14,7 @@ module.exports = E2eHelpers.createE2eTest(
 
     // log in & wait for little person icon to appear next to the username
     Auth.logIn(token, client, '/', 3)
-      .assert.title('Vets.gov')
+      .assert.title('Your Vets.gov Dashboard: Vets.gov')
       .waitForElementVisible(selectors.menu, Timeouts.slow);
 
     // click dropdown on username
