9772b15c2f59d87bfca13e2df14b9b75d8f5c4d3
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-05-29 09:49:44 -0400

    Add test for utility

--- src/applications/personalization/va-profile/util/sendAndMergeApiRequests.js
+++ src/applications/personalization/va-profile/util/sendAndMergeApiRequests.js
@@ -1,6 +1,6 @@
-import { apiRequest } from '../../../../platform/utilities/api';
+import { apiRequest as _apiRequest } from '../../../../platform/utilities/api';
 
-export default async function sendAndMergeApiRequests(requestMap) {
+export default async function sendAndMergeApiRequests(requestMap, apiRequest = _apiRequest) {
   const props = Object.keys(requestMap);
   const requests = props.map(async prop => {
     const apiRoute = requestMap[prop];
