fb63943fc80f887bb56bb15278e0e32bef0b21d3
Claire Hsu <clairehsu86@gmail.com>
2018-05-30 15:53:14 -0700

    Tidy

--- src/applications/common/schemaform/fields/ArrayField.jsx
+++ src/applications/common/schemaform/fields/ArrayField.jsx
@@ -254,7 +254,7 @@ export default class ArrayField extends React.Component {
                       {isEditing &&
                         <div className="row small-collapse">
                           <div className="small-6 left columns">
-                            {!isAdding && <button className="float-left" onClick={() => this.handleUpdate(index)}>Update</button>}
+                            <button className="float-left" onClick={() => this.handleUpdate(index)}>Update</button>
                           </div>
                           <div className="small-6 right columns">
                             <button className="usa-button-secondary float-right" type="button" onClick={() => this.handleRemove(index)}>Remove</button>
