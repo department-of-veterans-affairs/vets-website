728c152c4d87d1523b50eda553093c3b1945ee39
Xiaolu Bai <icebunnydoll@gmail.com>
2018-05-31 02:06:11 -0700

    make currentlyBuriedPersonsMinItem name required

--- src/applications/pre-need/config/form.jsx
+++ src/applications/pre-need/config/form.jsx
@@ -68,6 +68,12 @@ const {
 
 const nonRequiredFullName = _.omit('required', fullName);
 
+function currentlyBuriedPersonsMinItem() {
+  const copy = Object.assign({}, currentlyBuriedPersons);
+  copy.minItems = 1;
+  return _.set('items.properties.cemeteryNumber', autosuggest.schema, copy);
+}
+
 const formConfig = {
   urlPrefix: '/',
   submitUrl: `${environment.API_URL}/v0/preneeds/burial_forms`,
@@ -541,11 +547,7 @@ const formConfig = {
                     }
                   },
                   hasCurrentlyBuried,
-                  currentlyBuriedPersons: _.set(
-                    'items.properties.cemeteryNumber',
-                    autosuggest.schema,
-                    currentlyBuriedPersons
-                  )
+                  currentlyBuriedPersons: currentlyBuriedPersonsMinItem()
                 }
               }
             }
