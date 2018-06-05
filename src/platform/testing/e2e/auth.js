558eb496f39bf90f249368aa5534649fea353bb6
Eugene Doan <U-DON@users.noreply.github.com>
2018-06-05 13:23:20 -0400

    Added Health Records E2E test. (#7807)
    * Added Health Records E2E test.
    
    * Updated test.

--- src/platform/testing/e2e/auth.js
+++ src/platform/testing/e2e/auth.js
@@ -48,7 +48,16 @@ function initUserMock(token, level) {
             metadata: {}
           }],
           prefills_available: [],
-          services: ['facilities', 'hca', 'edu-benefits', 'evss-claims', 'user-profile', 'rx', 'messaging'],
+          services: [
+            'facilities',
+            'hca',
+            'edu-benefits',
+            'evss-claims',
+            'user-profile',
+            'health-records',
+            'rx',
+            'messaging'
+          ],
           mhv_account_state: 'upgraded',
           health_terms_current: true,
           va_profile: {
