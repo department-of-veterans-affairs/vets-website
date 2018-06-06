af51eb9df598b1ac49dc54722dc832e2ecd872e3
Rian Fowler <rian.fowler@adhocteam.us>
2018-06-06 15:46:24 -0600

    remove routes from vic v1

--- src/applications/veteran-id-card/veteran-id-card-entry.jsx
+++ src/applications/veteran-id-card/veteran-id-card-entry.jsx
@@ -1,12 +1,16 @@
+import React from 'react';
 import '../../platform/polyfills';
 import './sass/veteran-id-card.scss';
 
 import startApp from '../../platform/startup';
+import VeteranIDCard from './containers/VeteranIDCard';
+import Main from './containers/Main';
 
-import routes from './routes';
 import reducer from './reducers';
 
 startApp({
   reducer,
-  routes
+  component: (<VeteranIDCard>
+    <Main />
+  </VeteranIDCard>)
 });
