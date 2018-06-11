5bf8327fcedc57a5948b746fe9dbbc4301ab9cfd
Christopher Valarida <chris.valarida@adhocteam.us>
2018-06-11 14:53:26 -0700

    Orientation

--- src/applications/disability-benefits/526EZ/helpers.jsx
+++ src/applications/disability-benefits/526EZ/helpers.jsx
@@ -87,11 +87,11 @@ export function prefillTransformer(pages, formData, metadata, state) {
 
 export const supportingEvidenceOrientation = (
   <p>
-    We’ll now ask you where we can find medical records or evidence about your
-    worsened conditions after they were rated. You don’t need to turn in any
-    medical records that you’ve already submitted with your original claim. <strong>
-      We only need new medical records or other evidence about your condition
-      after you got your disability rating.</strong>
+    On the next few screens, we’ll ask you where we can find medical records or
+    supporting evidence that show your rated condition has gotten worse. You don’t
+    need to turn in any medical records that you already submitted with your original
+    claim. <strong>We only need new medical records or evidence that show your rated
+    condition has gotten worse.</strong>
   </p>
 );
 
