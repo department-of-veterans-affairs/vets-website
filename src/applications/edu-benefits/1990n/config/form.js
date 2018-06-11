07dd5053c163b9a9b361129e5ddb63002d13e152
Kainic <vacokainia@VACOLTKAINIA1.fios-router.home>
2018-06-11 19:41:27 -0400

    Convert 1990n

--- src/applications/edu-benefits/1990n/config/form.js
+++ src/applications/edu-benefits/1990n/config/form.js
@@ -3,7 +3,7 @@ import _ from 'lodash/fp';
 import fullSchema1990n from 'vets-json-schema/dist/22-1990N-schema.json';
 
 import schoolSelectionPage, { schoolSelectionOptionsFor } from '../../pages/schoolSelection';
-import applicantInformationPage from '../../../common/schemaform/pages/applicantInformation';
+import applicantInformationPage from '../../../../platform/forms/pages/applicantInformation';
 import FormFooter from '../../../../platform/forms/components/FormFooter';
 import environment from '../../../../platform/utilities/environment';
 import GetFormHelp from '../../components/GetFormHelp';
