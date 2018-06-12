fcfa6c214b83c3d1eb23aba1e5a541378a9952fd
Jeff Balboni <jeff@adhocteam.us>
2018-06-12 16:48:46 -0400

    Add error messages

--- src/applications/hca/config/form.js
+++ src/applications/hca/config/form.js
@@ -818,11 +818,17 @@ const formConfig = {
                 },
                 insurancePolicyNumber: {
                   'ui:title': 'Policy number (either this or the group code is required)',
-                  'ui:required': (formData, index) => !_.get(`providers[${index}].insuranceGroupCode`, formData)
+                  'ui:required': (formData, index) => !_.get(`providers[${index}].insuranceGroupCode`, formData),
+                  'ui:errorMessages': {
+                    pattern: 'Please provide a valid policy number.'
+                  }
                 },
                 insuranceGroupCode: {
                   'ui:title': 'Group code (either this or policy number is required)',
-                  'ui:required': (formData, index) => !_.get(`providers[${index}].insurancePolicyNumber`, formData)
+                  'ui:required': (formData, index) => !_.get(`providers[${index}].insurancePolicyNumber`, formData),
+                  'ui:errorMessages': {
+                    pattern: 'Please provide a valid policy number.'
+                  }
                 }
               }
             }
