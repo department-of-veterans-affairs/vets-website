def571231748f8d4ca65201ad6b7e0c8773a8846
Jeff Balboni <jeff@adhocteam.us>
2018-06-12 17:26:30 -0400

    New schema

--- src/applications/pre-need/config/form.jsx
+++ src/applications/pre-need/config/form.jsx
@@ -63,7 +63,7 @@ const {
   email,
   phone,
   files,
-  vaFileNumber
+  centralMailVaFile
 } = fullSchemaPreNeed.definitions;
 
 const nonRequiredFullName = _.omit('required', fullName);
@@ -100,7 +100,7 @@ const formConfig = {
     email,
     phone,
     files,
-    vaFileNumber
+    centralMailVaFile
   },
   chapters: {
     applicantInformation: {
