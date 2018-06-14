d1e352ea37b348b5e5b62e7aec514fcdd0b10855
Ben <bshyong@users.noreply.github.com>
2018-06-14 12:58:12 -0700

    Deletion of individual fields on vet360 profile (#7880)
    * confirm delete views
    
    * address deletion
    
    * refactor for reusable action buttons
    
    * action button field names
    
    * hook up phone and email fields for delete
    
    * conditional delete action

--- src/applications/personalization/profile360/components/LoadingButton.jsx
+++ src/applications/personalization/profile360/components/LoadingButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function LoadingButton({ isLoading, children }) {
+export default function LoadingButton({ isLoading, children, onClick }) {
   const contents = isLoading ? <i className="fa fa-spinner fa-spin"/> : children;
-  return <button disabled={isLoading} className="usa-button">{contents}</button>;
+  return <button disabled={isLoading} onClick={onClick} className="usa-button">{contents}</button>;
 }
