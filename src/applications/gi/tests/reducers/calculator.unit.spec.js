91d6fa82ed3e8822211d3174f9c4c771fa9dda2b
Rian Fowler <rian.fowler@adhocteam.us>
2018-06-14 16:34:18 -0600

    add reducer unit tests

--- src/applications/gi/tests/reducers/calculator.unit.spec.js
+++ src/applications/gi/tests/reducers/calculator.unit.spec.js
@@ -65,4 +65,139 @@ describe('calculator reducer', () => {
       tuitionFees: 100,
     });
   });
+
+  it('should update yellowRibbonDivisionOptions, yellowRibbonDivison, and yellowRibbonAmount when yellowRibbonDegreeLevel is changed', () => {
+    const previousState = {
+      yellowRibbonPrograms: [
+        {
+          divisionProfessionalSchool: 'division1',
+          degreeLevel: 'graduate',
+          contributionAmount: 5000
+        },
+        {
+          divisionProfessionalSchool: 'division2',
+          degreeLevel: 'undergraduate',
+          contributionAmount: 5
+        },
+        {
+          divisionProfessionalSchool: 'division3',
+          degreeLevel: 'undergraduate',
+          contributionAmount: 25
+        }
+      ]
+    };
+    const state = calculatorReducer(
+      previousState,
+      {
+        type: 'CALCULATOR_INPUTS_CHANGED',
+        field: 'yellowRibbonDegreeLevel',
+        value: 'undergraduate'
+      }
+    );
+
+    expect(state).to.eql({
+      ...previousState,
+      yellowRibbonDegreeLevel: 'undergraduate',
+      yellowRibbonDivisionOptions: ['division2', 'division3'],
+      yellowRibbonDivision: 'division2',
+      yellowRibbonAmount: 5
+    });
+  });
+
+
+  it('should update yellowRibbonAmount when yellowRibbonDivision is changed', () => {
+    const previousState = {
+      yellowRibbonDegreeLevel: 'undergraduate',
+      yellowRibbonPrograms: [
+        {
+          divisionProfessionalSchool: 'division1',
+          degreeLevel: 'graduate',
+          contributionAmount: 5000
+        },
+        {
+          divisionProfessionalSchool: 'division2',
+          degreeLevel: 'undergraduate',
+          contributionAmount: 5
+        },
+        {
+          divisionProfessionalSchool: 'division3',
+          degreeLevel: 'undergraduate',
+          contributionAmount: 25
+        }
+      ]
+    };
+    const state = calculatorReducer(
+      previousState,
+      {
+        type: 'CALCULATOR_INPUTS_CHANGED',
+        field: 'yellowRibbonDivision',
+        value: 'division3'
+      }
+    );
+
+    expect(state).to.eql({
+      ...previousState,
+      yellowRibbonDegreeLevel: 'undergraduate',
+      yellowRibbonDivision: 'division3',
+      yellowRibbonAmount: 25
+    });
+  });
+
+
+  it('should add yellowRibbonDegreeLevelOptions, yellowRibbonDivisonOptions, yellowRibbonAmount, yellowRibbonDegreeLevel, and yellowRibbonDivision when institution has yellowRibbonPrograms', () => {
+    const state = calculatorReducer(
+      {},
+      {
+        type: 'FETCH_PROFILE_SUCCEEDED',
+        payload: {
+          data: {
+            attributes: {
+              yellowRibbonPrograms: [
+                {
+                  divisionProfessionalSchool: 'division1',
+                  degreeLevel: 'graduate',
+                  contributionAmount: 5000
+                },
+                {
+                  divisionProfessionalSchool: 'division2',
+                  degreeLevel: 'undergraduate',
+                  contributionAmount: 5
+                },
+                {
+                  divisionProfessionalSchool: 'division3',
+                  degreeLevel: 'undergraduate',
+                  contributionAmount: 25
+                }
+              ]
+            }
+          }
+        }
+      }
+    );
+
+    expect(state).to.include({
+      yellowRibbonDegreeLevel: 'graduate',
+      yellowRibbonDivision: 'division1',
+      yellowRibbonAmount: 5000,
+    });
+    expect(state.yellowRibbonDivisionOptions).to.eql(['division1']);
+    expect(state.yellowRibbonDegreeLevelOptions).to.eql(['graduate', 'undergraduate']);
+    expect(state.yellowRibbonPrograms).to.eql([
+      {
+        divisionProfessionalSchool: 'division1',
+        degreeLevel: 'graduate',
+        contributionAmount: 5000
+      },
+      {
+        divisionProfessionalSchool: 'division2',
+        degreeLevel: 'undergraduate',
+        contributionAmount: 5
+      },
+      {
+        divisionProfessionalSchool: 'division3',
+        degreeLevel: 'undergraduate',
+        contributionAmount: 25
+      }
+    ]);
+  });
 });
