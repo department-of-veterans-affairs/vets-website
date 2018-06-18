22d9dfb5efa923addf23acb026b33163515aa618
Eugene Doan <eugene@adhocteam.us>
2018-06-18 07:21:13 -0400

    Change dropdown toggle back to div to support Safari accessbility.

--- src/applications/facility-locator/components/FacilityTypeDropdown.jsx
+++ src/applications/facility-locator/components/FacilityTypeDropdown.jsx
@@ -17,6 +17,8 @@ const facilityOptionClasses = (item, selected) => classNames(
 
 const itemToString = (item) => facilityTypes[item] || 'All Facilities';
 
+const focusDropdown = () => document.querySelector('#facility-dropdown-toggle').focus();
+
 const FacilityTypeDropdown = ({
   getButtonProps,
   getItemProps,
@@ -27,15 +29,13 @@ const FacilityTypeDropdown = ({
 }) => {
   const handleKeyDown = (e) => {
     switch (e.keyCode) {
-      // Allow (1) ENTER with nothing highlighted or
-      // (2) blurring focus (with TAB) to close dropdown.
-      case keyMap.ENTER:
+      // Allow blurring focus (with TAB) to close dropdown.
       case keyMap.TAB:
         if (isOpen) { toggleMenu(); }
         break;
 
-      // Allow SPACE to toggle state of menu without making a selection.
-      case keyMap.SPACE:
+      // Allow ENTER to toggle state of menu without making a selection.
+      case keyMap.ENTER:
         toggleMenu();
         break;
 
@@ -54,27 +54,28 @@ const FacilityTypeDropdown = ({
     </li>
   ));
 
+  /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
   return (
     <div>
-      <label htmlFor="facility-dropdown-toggle">
+      <label htmlFor="facility-dropdown-toggle" onClick={focusDropdown}>
         Select Facility Type
       </label>
       <div id="facility-dropdown">
-        <button {...getButtonProps({
+        <div {...getButtonProps({
           id: 'facility-dropdown-toggle',
           className: facilityOptionClasses(selectedItem),
           onKeyDown: handleKeyDown,
           tabIndex: 0,
-          type: 'button',
           'aria-expanded': isOpen
         })}>
           {itemToString(selectedItem)}
           <i className="fa fa-chevron-down dropdown-toggle"/>
-        </button>
+        </div>
         {isOpen && (<ul className="dropdown" role="listbox">{options}</ul>)}
       </div>
     </div>
   );
+  /* eslint-enable jsx-a11y/no-noninteractive-element-interactions */
 };
 
 class Wrapper extends Component {
