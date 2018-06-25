c65eb7467c1d788262af3fdb91ba13eb6596ca92
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-06-25 11:10:11 -0400

    Wrap in a function

--- src/platform/utilities/storage/localStorage.js
+++ src/platform/utilities/storage/localStorage.js
@@ -2,16 +2,16 @@
 // to throw errors. However, even when unavailable, "localStorage" is still a read-only property
 // of the window, so we can't polyfill it.
 
-let _storage;
-
-try {
-  const testItem = 'testItem';
-  localStorage.setItem(testItem, testItem);
-  localStorage.removeItem(testItem);
-  _storage = localStorage;
-} catch (err) {
-  _storage = require('local-storage-fallback').storage;
+function getLocalStorage() {
+  try {
+    const testItem = 'testItem';
+    const localStorage = window.localStorage;
+    localStorage.setItem(testItem, testItem);
+    localStorage.removeItem(testItem);
+    return localStorage;
+  } catch (err) {
+    return require('local-storage-fallback').storage;
+  }
 }
 
-const storage = _storage;
-export default storage;
+export default getLocalStorage();
