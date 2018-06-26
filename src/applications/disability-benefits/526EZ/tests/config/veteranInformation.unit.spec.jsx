9c22ca0be2b0c4426e5f85a71fa8fa666dc17ea3
Christopher Valarida <chris.valarida@adhocteam.us>
2018-06-25 17:55:18 -0700

    Remove outdated test

--- src/applications/disability-benefits/526EZ/tests/config/veteranInformation.unit.spec.jsx
+++ src/applications/disability-benefits/526EZ/tests/config/veteranInformation.unit.spec.jsx
@@ -5,25 +5,10 @@ import { mount } from 'enzyme';
 
 import { DefinitionTester } from '../../../../../platform/testing/unit/schemaform-utils.jsx';
 import formConfig from '../../config/form.js';
-import initialData from '../schema/initialData.js';
-
-initialData.prefilled = false;
 
 describe('526EZ veteran information', () => {
   const page = formConfig.chapters.veteranDetails.pages.veteranInformation;
   const { schema, uiSchema } = page;
-
-  it('should render', () => {
-    const form = mount(<DefinitionTester
-      definitions={formConfig.defaultDefinitions}
-      schema={schema}
-      formData={{}}
-      data={{}}
-      uiSchema={uiSchema}/>
-    );
-
-    expect(form.find('AsyncDisplayWidget').length).to.equal(1);
-  });
 
   it('should submit without validation errors', () => {
     const onSubmit = sinon.spy();
