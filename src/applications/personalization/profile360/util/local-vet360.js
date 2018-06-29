cbafd26bf2d348aebd95e34c48815a434e30a71b
Ben <bshyong@users.noreply.github.com>
2018-06-29 17:40:32 -0400

    360 profile formatting for military address (#7944)
    * 360 proftile formatting for military address
    
    * disable deletion on mailing address

--- src/applications/personalization/profile360/util/local-vet360.js
+++ src/applications/personalization/profile360/util/local-vet360.js
@@ -86,6 +86,29 @@ export const mockContactInformation = {
     zipCode: '97062',
     zipCodeSuffix: '1234'
   },
+  residentialAddress: {
+    addressLine1: '1493 Martin Luther King Rd',
+    addressLine2: '',
+    addressLine3: '',
+    addressPou: 'RESIDENCE/CHOICE',
+    addressType: 'domestic',
+    city: 'FPO',
+    countryName: 'United States',
+    countryCodeFips: 'US',
+    countryCodeIso2: 'US',
+    countryCodeIso3: 'USA',
+    createdAt: '2018-04-21T20:09:50Z',
+    effectiveEndDate: '2018-04-21T20:09:50Z',
+    effectiveStartDate: '2018-04-21T20:09:50Z',
+    id: 124,
+    internationalPostalCode: '54321',
+    province: 'string',
+    sourceDate: '2018-04-21T20:09:50Z',
+    stateCode: 'AE',
+    updatedAt: '2018-04-21T20:09:50Z',
+    zipCode: '09618',
+    zipCodeSuffix: '1234'
+  },
 };
 
 
