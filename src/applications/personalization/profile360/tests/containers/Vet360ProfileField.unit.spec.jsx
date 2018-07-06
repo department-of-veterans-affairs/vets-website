1686e164edeb0c09b4c0ced0893e3acdb3d4a74e
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-07-06 15:11:28 -0400

    Add test for edit link

--- src/applications/personalization/profile360/tests/containers/Vet360ProfileField.unit.spec.jsx
+++ src/applications/personalization/profile360/tests/containers/Vet360ProfileField.unit.spec.jsx
@@ -4,6 +4,7 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 
 import { Vet360ProfileField } from '../../containers/Vet360ProfileField';
+import { TRANSACTION_STATUS } from '../../constants/vet360';
 
 function Content() {
   return <h1>Content</h1>;
@@ -77,5 +78,19 @@ describe('<Vet360ProfileField/>', () => {
 
     const args = props.EditModal.getCall(0).args[0];
     expect(args, 'All props were passed to the EditModal constructor').to.have.all.keys(Object.keys(props));
+  });
+
+  it('renders the edit link', () => {
+    component = enzyme.shallow(<Vet360ProfileField {...props}/>);
+
+    let onEditClick = component.find('Vet360ProfileFieldHeading').props().onEditClick;
+    expect(onEditClick).to.be.equal(props.onEdit);
+
+    component.setProps({
+      transaction: { data: { attributes: { transactionStatus: TRANSACTION_STATUS.RECEIVED } } }
+    });
+
+    onEditClick = component.find('Vet360ProfileFieldHeading').props().onEditClick;
+    expect(onEditClick).to.be.false;
   });
 });
