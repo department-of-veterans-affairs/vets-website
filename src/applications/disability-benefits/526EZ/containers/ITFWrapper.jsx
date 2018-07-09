a7487a90cee5261fe465a3b95a129fb9e1a1098e
Christopher Valarida <chris.valarida@adhocteam.us>
2018-07-09 13:18:49 -0700

    PropTypes and class

--- src/applications/disability-benefits/526EZ/containers/ITFWrapper.jsx
+++ src/applications/disability-benefits/526EZ/containers/ITFWrapper.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import LoadingIndicator from '@department-of-veterans-affairs/formation/LoadingIndicator';
@@ -12,66 +13,96 @@ import { createITF as createITFAction, fetchITF as fetchITFAction } from '../act
 const fetchWaitingStates = [requestStates.notCalled, requestStates.pending];
 
 
-export const ITFWrapper = ({ location, children, itf, fetchITF, createITF }) => {
-  // If the location is the intro or confirmation pages, don't fetch an ITF
-  if (['/introduction', '/confirmation'].includes(location.pathname)) {
-    return children;
-  }
+export class ITFWrapper extends React.Component {
+  render() {
+    const { location, children, itf, fetchITF, createITF } = this.props;
+    // If the location is the intro or confirmation pages, don't fetch an ITF
+    if (['/introduction', '/confirmation'].includes(location.pathname)) {
+      return children;
+    }
 
-  // If we haven't checked the ITF status yet, do so
-  if (itf.fetchCallState === requestStates.notCalled) {
-    fetchITF();
-  }
+    // If we haven't checked the ITF status yet, do so
+    if (itf.fetchCallState === requestStates.notCalled) {
+      fetchITF();
+    }
 
-  // While we're waiting, show the loading indicator...
-  if (fetchWaitingStates.includes(itf.fetchCallState)) {
-    return <LoadingIndicator message="Checking your Intent to File status..."/>;
-  }
+    // While we're waiting, show the loading indicator...
+    if (fetchWaitingStates.includes(itf.fetchCallState)) {
+      return <LoadingIndicator message="Checking your Intent to File status..."/>;
+    }
 
-  // We'll get here after the fetchITF promise is fulfilled
+    // We'll get here after the fetchITF promise is fulfilled
 
-  if (itf.fetchCallState === requestStates.failed) {
-    // TODO: Get better content for this
+    if (itf.fetchCallState === requestStates.failed) {
+      // TODO: Get better content for this
+      return (
+        <div className="usa-grid" style={{ marginBottom: '2em' }}>
+          <AlertBox
+            isVisible
+            headline="Something went wrong"
+            content="Sorry, we’re unable to check your ITF status right now."
+            status="error"/>
+        </div>
+      );
+    }
+
+    // If we have an active ITF, we're good to go--render that form!
+    if (itf.currentITF && itf.currentITF.status === itfStatuses.active) {
+      // TODO: Render a success alert box (only the first time)
+      //  Probably will have to use state for this
+      return children;
+    }
+
+    // If not, try to submit a new ITF
+    if (itf.creationCallState === requestStates.notCalled) {
+      createITF();
+    }
+
+    // While we're waiting (again), show the loading indicator...again
+    if (fetchWaitingStates.includes(itf.creationCallState)) {
+      return <LoadingIndicator message="Submitting a new Intent to File..."/>;
+    }
+
+    // We'll get here after the createITF promise is fulfilled and we have no active ITF
+    //  because of a failed creation call
+    // TODO: Get better content for this content
     return (
       <div className="usa-grid" style={{ marginBottom: '2em' }}>
         <AlertBox
           isVisible
           headline="Something went wrong"
-          content="Sorry, we’re unable to check your ITF status right now."
+          content="We’re sorry, we couldn’t find an active ITF nor file a new one for you. Please try again later."
           status="error"/>
       </div>
     );
   }
+}
 
-  // If we have an active ITF, we're good to go--render that form!
-  if (itf.currentITF && itf.currentITF.status === itfStatuses.active) {
-    // TODO: Render a success alert box (only the first time)
-    //  Probably will have to use state for this
-    return children;
-  }
 
-  // If not, try to submit a new ITF
-  if (itf.creationCallState === requestStates.notCalled) {
-    createITF();
-  }
+const requestStateEnum = Object.keys(requestStates);
 
-  // While we're waiting (again), show the loading indicator...again
-  if (fetchWaitingStates.includes(itf.creationCallState)) {
-    return <LoadingIndicator message="Submitting a new Intent to File..."/>;
-  }
+const itfShape = {
+  id: PropTypes.string,
+  creationDate: PropTypes.string,
+  expirationDate: PropTypes.string.isRequired,
+  participantId: PropTypes.number,
+  source: PropTypes.string,
+  status: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired
+};
 
-  // We'll get here after the createITF promise is fulfilled and we have no active ITF
-  //  because of a failed creation call
-  // TODO: Get better content for this content
-  return (
-    <div className="usa-grid" style={{ marginBottom: '2em' }}>
-      <AlertBox
-        isVisible
-        headline="Something went wrong"
-        content="We’re sorry, we couldn’t find an active ITF nor file a new one for you. Please try again later."
-        status="error"/>
-    </div>
-  );
+ITFWrapper.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string
+  }).isRequired,
+  itf: PropTypes.shape({
+    fetchCallState: PropTypes.oneOf(requestStateEnum).isRequired,
+    creationCallState: PropTypes.oneOf(requestStateEnum).isRequired,
+    currentITF: PropTypes.shape(itfShape),
+    previousITF: PropTypes.shape(itfShape)
+  }),
+  fetchITF: PropTypes.func.isRequired,
+  createITF: PropTypes.func.isRequired
 };
 
 
