42348145f15fbdf9f53348ba0c85197ad4f33348
Claire Hsu <clairehsu86@gmail.com>
2018-07-11 16:23:30 -0700

    PR comment

--- src/applications/edu-benefits/0993/helpers.js
+++ src/applications/edu-benefits/0993/helpers.js
@@ -1,7 +1,9 @@
 import _ from 'lodash/fp';
 
-export function prefillTransformer(pages, formData, metadata) {
-  const newFormData = _.set('verified', !!metadata.verified, formData);
+export function prefillTransformer(pages, formData, metadata, state) {
+  const { verified } = state.user.profile;
+
+  const newFormData = _.set('verified', !!verified, formData);
 
   return {
     metadata,
