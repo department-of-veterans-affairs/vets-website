62d3a36540657cc95bfe68306ca8db6ab1602d7b
Rian Fowler <rian.fowler@adhocteam.us>
2018-07-12 14:45:20 -0600

    make telephone number a hyper link

--- src/applications/edu-benefits/components/OptOutWizard.jsx
+++ src/applications/edu-benefits/components/OptOutWizard.jsx
@@ -39,7 +39,7 @@ export default class OptOutWizard extends React.Component {
             </ul>
           </div>
 
-          <p><strong>Please note:</strong> If you submit an opt-out request and then change your mind, you’ll need to call the Education Call Center at 1-888-442-4551 to opt back in.</p>
+          <p><strong>Please note:</strong> If you submit an opt-out request and then change your mind, you’ll need to call the Education Call Center at <a className="help-phone-number-link" href="tel:+1-888-442-4551">1-888-442-4551</a> to opt back in.</p>
           <div>
             <a
               href="/education/opt-out-information-sharing/opt-out-form-0993"
