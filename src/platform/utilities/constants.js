e9c23bd9c5d801db168d115f89ce4f3973e81152
Chris Valarida <cvalarida@gmail.com>
2018-07-11 21:13:00 -0700

    526 itf wrapper (#7986)
    * Mostly-implemented ITF wrapper
    
    * WIP -- mostly done...hopefully?
    
    * Use the right submit ITF endpoint
    
    * Started writing tests
    
    * Moar tests!
    
    * PropTypes and class
    
    * Moved ITF calls to lifecycle methods
    
    * ITF messaging and testing
    
    * Don't try to submit a new ITF when the fetch failed
    
    * Tests
    
    * Updated content
    
    * One last test fix...
    
    * PR comments
    
    * Updated content
    
    * Testing testing testing....

---
+++ src/platform/utilities/constants.js
@@ -0,0 +1,18 @@
+// TODO: Rename this to something that makes more sense
+export const constantHandler = {
+  get(target, propName) {
+    if (!(propName in target)) {
+      throw new Error(`${propName} not in target.`);
+    }
+
+    return target[propName];
+  }
+};
+
+
+export const requestStates = new Proxy({
+  notCalled: 'not called',
+  pending: 'pending',
+  succeeded: 'succeeded',
+  failed: 'failed'
+}, constantHandler);
