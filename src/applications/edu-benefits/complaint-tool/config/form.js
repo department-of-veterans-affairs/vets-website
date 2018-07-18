8e771fd62fe36a7f889b7107d5e28ffbfd54d7ba
R Matthews <remimatthews@brandeis.edu>
2018-07-18 19:41:23 -0400

    first page setup

--- src/applications/edu-benefits/complaint-tool/config/form.js
+++ src/applications/edu-benefits/complaint-tool/config/form.js
@@ -1,11 +1,44 @@
+import _ from 'lodash/fp';
+import React from 'react';
 // import fullSchema from 'vets-json-schema/dist/686-schema.json';
 
 import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
 
+import fullNameUI from 'us-forms-system/lib/js/definitions/fullName';
+import dateRangeUI from 'us-forms-system/lib/js/definitions/dateRange';
+
 // const { } = fullSchema.properties;
 
 // const { } = fullSchema.definitions;
+
+function isNotAnonymous(formData) {
+  if (!!formData && formData !== 'I want to submit my feedback anonymously') {
+    return true;
+  }
+  return false;
+}
+
+const suffixes = [
+  'Jr.',
+  'Sr.',
+  'II',
+  'III',
+  'IV'
+];
+
+const date = {
+  pattern: '^(\\d{4}|XXXX)-(0[1-9]|1[0-2]|XX)-(0[1-9]|[1-2][0-9]|3[0-1]|XX)$',
+  type: 'string'
+};
+
+const dateRange = {
+  type: 'object',
+  properties: {
+    from: date,
+    to: date
+  }
+};
 
 const formConfig = {
   urlPrefix: '/',
@@ -33,7 +66,87 @@ const formConfig = {
           uiSchema: {
             onBehalfOf: {
               'ui:widget': 'radio',
-              'ui:title': 'I’m filing on behalf of...'
+              'ui:title': 'I’m filing on behalf of...',
+              'ui:options': {
+                nestedContent: {
+                  Myself: () => <div className="usa-alert-info no-background-image"><i>(We’ll only share your name with the school.)</i></div>,
+                  'Someone else': () => <div className="usa-alert-info no-background-image"><i>(We’ll only share your name with the school.)</i></div>,
+                  'I want to submit my feedback anonymously': () => <div className="usa-alert-info no-background-image"><i>None of your personal information will be shared outside of the VA.</i></div>
+                },
+                expandUnderClassNames: 'schemaform-expandUnder',
+              }
+            },
+            fullName: _.merge(fullNameUI, {
+              first: {
+                'ui:title': 'Your first name',
+                'ui:required': (formData) => !!formData && (formData.onBehalfOf !== 'I want to submit my feedback anonymously')
+              },
+              last: {
+                'ui:title': 'Your last name',
+                'ui:required': (formData) => !!formData && (formData.onBehalfOf !== 'I want to submit my feedback anonymously')
+              },
+              middle: {
+                'ui:title': 'Your middle name'
+              },
+              suffix: {
+                'ui:title': 'Your suffix'
+              },
+              'ui:options': {
+                expandUnder: 'onBehalfOf',
+                expandUnderCondition: isNotAnonymous
+              }
+            }),
+            dob: {
+              'ui:title': 'Date of birth',
+              'ui:options': {
+                expandUnder: 'onBehalfOf',
+                expandUnderCondition: isNotAnonymous
+              }
+            },
+            serviceAffiliation: { // could wrap service info in an object
+              'ui:title': 'Service affiliation',
+              'ui:options': {
+                expandUnder: 'onBehalfOf',
+                expandUnderCondition: 'Myself'
+              },
+              'ui:required': (formData) => !!formData && (formData.onBehalfOf === 'Myself')
+            },
+            serviceBranch: {
+              'ui:title': 'Branch',
+              'ui:options': {
+                hideIf: (formData) => {
+                  if (!formData.serviceAffiliation || formData.serviceAffiliation !== 'Veteran') {
+                    return true;
+                  }
+                  return false;
+                },
+                expandUnder: 'onBehalfOf',
+                expandUnderCondition: 'Myself'
+              }
+            },
+            serviceDateRange: _.merge(dateRangeUI(
+              'Service start date',
+              'Service end date',
+              'End of service must be after start of service'
+            ),
+            {
+              'ui:options': {
+                hideIf: (formData) => {
+                  if (!formData.serviceAffiliation || formData.serviceAffiliation !== 'Veteran') {
+                    return true;
+                  }
+                  return false;
+                },
+                expandUnder: 'onBehalfOf',
+                expandUnderCondition: 'Myself'
+              }
+            }),
+            email: {
+              'ui:title': 'Email',
+              'ui:options': {
+                expandUnder: 'onBehalfOf',
+                expandUnderCondition: 'I want to submit my feedback anonymously'
+              }
             }
           },
           schema: {
@@ -47,7 +160,7 @@ const formConfig = {
                 'enum': [
                   'Myself',
                   'Someone else',
-                  'I want to submit my complaint anonymously.'
+                  'I want to submit my feedback anonymously'
                 ]
               },
               fullName: {
@@ -66,26 +179,19 @@ const formConfig = {
                   first: {
                     type: 'string',
                     minLength: 1,
-                    maxLength: 15
+                    maxLength: 100
                   },
                   middle: {
-                    type: 'string',
-                    maxLength: 15
+                    type: 'string'
                   },
                   last: {
                     type: 'string',
                     minLength: 1,
-                    maxLength: 25
+                    maxLength: 100
                   },
                   suffix: {
                     type: 'string',
-                    'enum': [
-                      'Jr.',
-                      'Sr.',
-                      'II',
-                      'III',
-                      'IV'
-                    ]
+                    'enum': suffixes
                   }
                 },
                 required: [
@@ -97,7 +203,7 @@ const formConfig = {
                 type: 'string',
                 format: 'date'
               },
-              serviceAffiliation: {
+              serviceAffiliation: { // design may change
                 type: 'string',
                 'enum': [
                   'Service Member',
@@ -106,6 +212,21 @@ const formConfig = {
                   'Other'
                 ]
               },
+              serviceBranch: {
+                type: 'string',
+                'enum': [
+                  'Army',
+                  'Navy',
+                  'Marines',
+                  'Air Force',
+                  'Coast Guard',
+                  'NOAA/PHS'
+                ]
+              },
+              serviceDateRange: dateRange,
+              email: {
+                type: 'string'
+              }
             }
           }
         }
