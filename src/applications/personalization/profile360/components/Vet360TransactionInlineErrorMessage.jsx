52ffedecd31f3218b2b90ea8aa77fbb26a54bfe1
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-07-18 14:29:25 -0400

    Move error handler into modal

--- src/applications/personalization/profile360/components/Vet360TransactionInlineErrorMessage.jsx
+++ src/applications/personalization/profile360/components/Vet360TransactionInlineErrorMessage.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import {
-  hasUserIsDeceasedError
-} from '../util/transactions';
+// import {
+//   hasUserIsDeceasedError
+// } from '../util/transactions';
 
-export default function Vet360TransactionInlineErrorMessage({ title: fieldTitle, transaction }) {
+export default function Vet360TransactionInlineErrorMessage({ title: fieldTitle }) {
   let content = null;
   switch (true) {
-    case hasUserIsDeceasedError(transaction):
-      content = <span>We can’t make this update because our records show the Veteran is deceased. If this isn’t true, please contact your nearest VA medical center. <a href="/facilities/">Find your nearest VA medical center</a>.</span>;
-      break;
+    // case hasUserIsDeceasedError(transaction):
+    //   content = <span>We can’t make this update because our records show the Veteran is deceased. If this isn’t true, please contact your nearest VA medical center. <a href="/facilities/">Find your nearest VA medical center</a>.</span>;
+    //   break;
 
     default:
       content = <span>We couldn’t save your recent {fieldTitle.toLowerCase()} update. Please try again later.</span>;
