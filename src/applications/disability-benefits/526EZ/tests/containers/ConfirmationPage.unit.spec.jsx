f04797e2071aca9e05fb5e6863faaa79a242a308
Mark Greenburg <marek.greenburg@gmail.com>
2018-07-19 17:38:15 -0500

    526 confirmation id mg (#8024)
    * Remove old commented code
    
    * Show real confirmation number on review page
    
    * More specific mapStateToProps
    
    * Improve tests
    
    * Add test for date
    
    * Remove test restriction

--- src/applications/disability-benefits/526EZ/tests/containers/ConfirmationPage.unit.spec.jsx
+++ src/applications/disability-benefits/526EZ/tests/containers/ConfirmationPage.unit.spec.jsx
@@ -6,22 +6,19 @@ import _ from 'lodash/fp';
 import { ConfirmationPage } from '../../containers/ConfirmationPage';
 import initialData from '../schema/initialData.js';
 
-const selectedData = _.set("disabilities[0]['view:selected']", true, initialData);
 describe('Disability Benefits 526EZ <ConfirmationPage>', () => {
+  const defaultProps = {
+    fullName: {
+      first: 'Sally',
+      last: 'Alphonse'
+    },
+    disabilities: _.set("disabilities[0]['view:selected']", true, initialData).disabilities,
+    claimId: '123456',
+    submittedAt: '2018-04-12'
+  };
+
   it('should render', () => {
-    const form = {
-      submission: {
-        response: {
-          attributes: {
-            confirmationNumber: 'V-DCCI-3986'
-          }
-        }
-      },
-      data: selectedData
-    };
-    const tree = shallow(
-      <ConfirmationPage form={form}/>
-    );
+    const tree = shallow(<ConfirmationPage {...defaultProps}/>);
 
     expect(tree.find('.confirmation-page-title').render().text()).to.equal('Your claim has been submitted.');
     expect(tree.find('span').at(1)
@@ -31,83 +28,49 @@ describe('Disability Benefits 526EZ <ConfirmationPage>', () => {
     ).to.equal('For Sally  Alphonse');
     expect(tree.find('p').at(0).render().text()).to.contain('We usually process claims within 99 days.');
     expect(tree.find('p').at(1).render().text()).to.contain('We may contact you if we have questions or need more information. You can print this page for your records.');
-    expect(tree.find('.disability-list').render().text()).to.contain('Post traumatic stress disorder');
-    expect(tree.find('.disability-list').render().text()).to.not.contain('Intervertebral disc syndrome');
+    expect(tree.find('.disability-list').render().text()).to.contain(initialData.disabilities[0].name);
+    expect(tree.find('.disability-list').render().text()).to.not.contain(initialData.disabilities[1].name);
   });
 
   it('should render 4142 helper text when 4142 option selected for any disability', () => {
-    const newData = {
-      fullName: {
-        first: 'Sally',
-        last: 'Alphonse'
-      },
-      disabilities: [
-        {
-          name: 'Post traumatic stress disorder',
-          'view:selected': true,
-          'view:uploadPrivateRecords': 'no'
-        },
-        {
-          name: 'Intervertebral disc syndrome',
-          'view:selected': true,
-          'view:uploadPrivateRecords': 'yes'
-        }
-      ]
-    };
+    const newProps = _.set('disabilities[0][view:uploadPrivateRecords]', 'no', defaultProps);
 
-    const newForm = {
-      submission: {
-        response: {
-          attributes: {
-            confirmationNumber: 'V-DCCI-3986'
-          }
-        }
-      },
-      data: newData
-    };
-
-    const wrapper = shallow(<ConfirmationPage form={newForm}/>);
+    const wrapper = shallow(<ConfirmationPage {...newProps}/>);
     expect(wrapper.render().text()).to.contain(
       'you’ll need to fill out an Authorization to Disclose Information to the VA (VA Form 21-4142).'
     );
   });
 
   it('should not render 4142 helper text when 4142 option not selected for any disability', () => {
-    const newData = {
-      fullName: {
-        first: 'Sally',
-        last: 'Alphonse'
-      },
-      disabilities: [
-        {
-          name: 'Post traumatic stress disorder',
-          'view:selected': true
-        },
-        {
-          name: 'Intervertebral disc syndrome',
-          'view:selected': true,
-          'view:uploadPrivateRecords': 'yes'
-        },
-        {
-          name: 'Third Disability',
-        }
-      ]
-    };
+    const newProps = _.set('disabilities[0][view:uploadPrivateRecords]', 'yes', defaultProps);
 
-    const newForm = {
-      submission: {
-        response: {
-          attributes: {
-            confirmationNumber: 'V-DCCI-3986'
-          }
-        }
-      },
-      data: newData
-    };
-
-    const wrapper = shallow(<ConfirmationPage form={newForm}/>);
+    const wrapper = shallow(<ConfirmationPage {...newProps}/>);
     expect(wrapper.render().text()).to.not.contain(
       'you’ll need to fill out an Authorization to Disclose Information to the VA (VA Form 21-4142).'
     );
+  });
+
+  it('should render a confirmation number', () => {
+    const wrapper = shallow(<ConfirmationPage {...defaultProps}/>);
+    expect(wrapper.render().text()).to.contain(defaultProps.claimId);
+  });
+
+  it('should render a formatted full name', () => {
+    const fullName = {
+      first: 'Sally',
+      middle: 'Ruth',
+      last: 'Alphonse',
+      suffix: 'Jr.'
+    };
+    const newProps = _.set('fullName', fullName, defaultProps);
+
+    const wrapper = shallow(<ConfirmationPage {...newProps}/>);
+    const { first, middle, last, suffix } = fullName;
+    expect(wrapper.render().text()).to.contain(`${first} ${middle} ${last} ${suffix}`);
+  });
+
+  it('should render a formatted submission date', () => {
+    const wrapper = shallow(<ConfirmationPage {...defaultProps}/>);
+    expect(wrapper.render().text()).to.contain('April 12, 2018');
   });
 });
