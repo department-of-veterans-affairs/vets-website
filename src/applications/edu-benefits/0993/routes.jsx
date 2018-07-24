50032a759d0946a4355a6731bb0d29e132938198
Claire Hsu <clairehsu86@gmail.com>
2018-07-24 09:05:51 -0700

    Restore common app, page, and review page

--- src/applications/edu-benefits/0993/routes.jsx
+++ src/applications/edu-benefits/0993/routes.jsx
@@ -1,18 +1,11 @@
 import { createRoutesWithSaveInProgress } from '../../../platform/forms/save-in-progress/helpers';
 import formConfig from './config/form';
-import Form0993App from './containers/Form0993App.jsx';
+import Form0993App from './Form0993App';
 import RoutedSavableSinglePageForm from './containers/RoutedSavableSinglePageForm';
 
-const filteredRoutes = new Set(['introduction', 'review-and-submit']);
+const filteredRoutes = new Set(['introduction']);
 
 const childRoutes = createRoutesWithSaveInProgress(formConfig).filter(route => !filteredRoutes.has(route.path));
-
-
-// Set single page form component
-childRoutes[0].component = RoutedSavableSinglePageForm;
-
-// Provision form config
-childRoutes[0].formConfig = formConfig;
 
 const route = {
   path: '/',
