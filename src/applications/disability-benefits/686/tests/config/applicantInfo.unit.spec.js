a1df3f43b1a42b853a2cb6d09964c6fe673c9cb6
Sophia Dengo <sdengo@gmail.com>
2018-07-25 10:54:40 -0400

    adding untracked files

---
+++ src/applications/disability-benefits/686/tests/config/applicantInfo.unit.spec.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+
+import { DefinitionTester, fillData, selectRadio } from '../../../../../platform/testing/unit/schemaform-utils.jsx';
+import formConfig from '../../config/form';
+
+describe('686 applicant information', () => {
+  const { schema, uiSchema } = formConfig.chapters.applicantInformation.pages.applicantInformation;
+
+  it('should render', () => {
+    const form = mount(
+      <DefinitionTester
+        schema={schema}
+        data={{}}
+        definitions={formConfig.defaultDefinitions}
+        uiSchema={uiSchema}/>
+    );
+    expect(form.find('input').length).to.equal(7);
+    expect(form.find('select').length).to.equal(1);
+  });
+
+  it('should not submit empty form', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        schema={schema}
+        definitions={formConfig.defaultDefinitions}
+        onSubmit={onSubmit}
+        uiSchema={uiSchema}/>
+    );
+    form.find('form').simulate('submit');
+    expect(form.find('.usa-input-error').length).to.equal(3);
+    expect(onSubmit.called).to.be.false;
+  });
+
+  it('should submit form if applicant is veteran', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        schema={schema}
+        definitions={formConfig.defaultDefinitions}
+        data={{}}
+        onSubmit={onSubmit}
+        uiSchema={uiSchema}/>
+    );
+
+    fillData(form, 'input#root_claimantFullName_first', 'test');
+    fillData(form, 'input#root_claimantFullName_last', 'test');
+    selectRadio(form, 'root_view:relationshipToVet', '1');
+
+    form.find('form').simulate('submit');
+    expect(form.find('.usa-input-error').length).to.equal(0);
+    expect(onSubmit.called).to.be.true;
+  });
+
+  it('should expand spouse description if relationship is spouse', () => {
+    const form = mount(
+      <DefinitionTester
+        schema={schema}
+        definitions={formConfig.defaultDefinitions}
+        data={{}}
+        uiSchema={uiSchema}/>
+    );
+    selectRadio(form, 'root_view:relationshipToVet', '2');
+
+    expect(form.find('.schemaform-radio-indent').length).to.equal(1);
+  });
+
+  it('should expand child description if relationship is child', () => {
+    const form = mount(
+      <DefinitionTester
+        schema={schema}
+        definitions={formConfig.defaultDefinitions}
+        data={{}}
+        uiSchema={uiSchema}/>
+    );
+    selectRadio(form, 'root_view:relationshipToVet', '3');
+
+    expect(form.find('.schemaform-radio-indent').length).to.equal(1);
+  });
+
+  it('should expand other description if relationship is other', () => {
+    const form = mount(
+      <DefinitionTester
+        schema={schema}
+        definitions={formConfig.defaultDefinitions}
+        data={{}}
+        uiSchema={uiSchema}/>
+    );
+    selectRadio(form, 'root_view:relationshipToVet', '4');
+
+    expect(form.find('.schemaform-radio-indent').length).to.equal(1);
+  });
+});
---
+++ src/platform/site-wide/announcements/selectors.js
@@ -0,0 +1,18 @@
+import _config from './config';
+
+export function selectAnnouncement(state, config = _config, path = document.location.pathname) {
+  const announcements = state.announcements;
+  let announcement;
+
+  if (announcements.isInitialized) {
+    announcement = config.announcements
+      .filter(a => !a.disabled)
+      .find(a => a.paths.test(path));
+
+    if (announcement && announcements.dismissed.includes(announcement.name)) {
+      announcement = null;
+    }
+  }
+
+  return announcement;
+}
