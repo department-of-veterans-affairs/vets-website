79bcd71e15d0bc04d6e1978fc7cb20315c66a30c
Claire Hsu <clairehsu86@gmail.com>
2018-07-26 11:03:03 -0700

    Tidy

--- src/applications/edu-benefits/complaint-tool/config/form.js
+++ src/applications/edu-benefits/complaint-tool/config/form.js
@@ -89,7 +89,7 @@ const formConfig = {
                       schoolCountry,
                       schoolPostalCode
                     }
-                  }  
+                  }
                 }
               }
             }
