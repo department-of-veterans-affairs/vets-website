05b11d3191213d73dcdad7dac8f8881efa83485a
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-07-26 16:04:44 -0400

    Remove sendAndMergeApiRequests

--- src/applications/personalization/profile360/actions/index.js
+++ src/applications/personalization/profile360/actions/index.js
@@ -1,4 +1,4 @@
-import sendAndMergeApiRequests from '../util/sendAndMergeApiRequests';
+import { apiRequest } from '../../../../platform/utilities/api';
 
 export const FETCH_HERO_SUCCESS = 'FETCH_HERO_SUCCESS';
 export const FETCH_PERSONAL_INFORMATION_SUCCESS = 'FETCH_PERSONAL_INFORMATION_SUCCESS';
@@ -7,36 +7,34 @@ export const FETCH_ADDRESS_CONSTANTS_SUCCESS = 'FETCH_ADDRESS_CONSTANTS_SUCCESS'
 
 export function fetchHero() {
   return async (dispatch) => {
-    const hero = await sendAndMergeApiRequests({
-      userFullName: '/profile/full_name'
-    });
+    const userFullName = await apiRequest('/profile/full_name');
     dispatch({
       type: FETCH_HERO_SUCCESS,
-      hero
+      hero: {
+        userFullName: userFullName.data.attributes
+      }
     });
   };
 }
 
 export function fetchPersonalInformation() {
   return async (dispatch) => {
-    const result = await sendAndMergeApiRequests({
-      personalInformation: '/profile/personal_information'
-    });
+    const personalInformation = await apiRequest('/profile/personal_information');
     dispatch({
       type: FETCH_PERSONAL_INFORMATION_SUCCESS,
-      personalInformation: result.personalInformation
+      personalInformation: personalInformation.data.attributes
     });
   };
 }
 
 export function fetchMilitaryInformation() {
   return async (dispatch) => {
-    const militaryInformation = await sendAndMergeApiRequests({
-      serviceHistory: '/profile/service_history'
-    });
+    const serviceHistory = await apiRequest('/profile/service_history');
     dispatch({
       type: FETCH_MILITARY_INFORMATION_SUCCESS,
-      militaryInformation
+      militaryInformation: {
+        serviceHistory: serviceHistory.data.attributes
+      }
     });
   };
 }
