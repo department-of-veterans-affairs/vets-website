428c605b8a866fa67d686da78ccce6c9edd91b9e
R Matthews <remimatthews@brandeis.edu>
2018-07-27 19:01:13 -0400

    setup applicant info page and test (#8013)
    * setup applicant info page and test
    
    * added name fields to form
    
    * added full name, dob, serviceaffiliation
    
    * updated headers and subway page
    
    * changing apostrophes to ’
    
    * first page setup
    
    * content changes to first page & unit tests
    
    * fixed apostrophes and content
    
    * name change to GI Bill® School Feedback Tool
    
    * added prefix and created consts for the filing names
    
    * fixed unit tests to incoporate prefix field
    
    * fixed lint errors
    
    * merged content into applicant-info
    
    * Fixes content errors with name and onBehalfOf description error
    
    * show military info as service member
    
    * changed name to branch of service
    
    * Update manifest.json
    
    * PR comments
    
    * Update vets-json-schema version
    
    * Add default definition
    
    * Update tests
    
    * Add feedback tool to profile helpers
    
    * Update migration test
    
    * PR comment

--- src/applications/edu-benefits/complaint-tool/config/form.js
+++ src/applications/edu-benefits/complaint-tool/config/form.js
@@ -1,13 +1,57 @@
-// import fullSchema from 'vets-json-schema/dist/686-schema.json';
+import _ from 'lodash/fp';
+import React from 'react';
+import fullSchema from 'vets-json-schema/dist/complaint-tool-schema.json';
 
 import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
 
+import fullNameUI from 'us-forms-system/lib/js/definitions/fullName';
+import dateUI from 'us-forms-system/lib/js/definitions/date';
+import dateRangeUI from 'us-forms-system/lib/js/definitions/dateRange';
+
 import { transform } from '../helpers';
 
-// const { } = fullSchema.properties;
+const {
+  onBehalfOf,
+  fullName,
+  dob,
+  serviceAffiliation,
+  serviceBranch,
+  serviceDateRange,
+  email
+} = fullSchema.properties;
 
-// const { } = fullSchema.definitions;
+const {
+  date,
+  dateRange
+} = fullSchema.definitions;
+
+const myself = 'Myself';
+const someoneElse = 'Someone else';
+const anonymous = 'Anonymous';
+const anonymousLabel = 'I want to submit my feedback anonymously'; // Only anonymous has a label that differs form its value
+
+function isNotAnonymous(formData) {
+  if (!!formData && formData !== anonymous) {
+    return true;
+  }
+  return false;
+}
+
+function hasNotAnonymous(formData) {
+  return !!formData && (formData.onBehalfOf !== anonymous);
+}
+
+function hasMyself(formData) {
+  return !!formData && (formData.onBehalfOf === myself);
+}
+
+function isNotVeteranOrServiceMember(formData) {
+  if (!formData.serviceAffiliation || ((formData.serviceAffiliation !== 'Veteran') && (formData.serviceAffiliation !== 'Service Member'))) {
+    return true;
+  }
+  return false;
+}
 
 const formConfig = {
   urlPrefix: '/',
@@ -15,7 +59,7 @@ const formConfig = {
   trackingPrefix: 'complaint-tool',
   introduction: IntroductionPage,
   confirmation: ConfirmationPage,
-  formId: '686',
+  formId: 'complaint-tool',
   version: 0,
   prefillEnabled: true,
   savedFormMessages: {
@@ -25,22 +69,111 @@ const formConfig = {
   title: 'GI Bill® School Feedback Tool',
   transformForSubmit: transform,
   defaultDefinitions: {
+    date,
+    dateRange
   },
   chapters: {
-    form: {
-      title: 'Form',
+    applicantInformation: {
+      title: 'Applicant Information',
       pages: {
-        page1: {
-          path: 'form-page',
-          title: 'First Page',
+        applicantInformation: {
+          path: 'applicant-information',
+          title: 'Applicant Information',
           uiSchema: {
+            onBehalfOf: {
+              'ui:widget': 'radio',
+              'ui:title': 'I’m submitting feedback on behalf of...',
+              'ui:options': {
+                nestedContent: {
+                  [myself]: () => <div className="usa-alert-info no-background-image"><i>(We’ll only share your name with the school.)</i></div>,
+                  [someoneElse]: () => <div className="usa-alert-info no-background-image"><i>(We’ll only share your name with the school.)</i></div>,
+                  [anonymous]: () => <div className="usa-alert-info no-background-image"><i>(Your personal information won’t be shared with anyone outside of VA.)</i></div>
+                },
+                expandUnderClassNames: 'schemaform-expandUnder',
+              }
+            },
+            fullName: _.merge(fullNameUI, {
+              prefix: {
+                'ui:title': 'Prefix',
+                'ui:options': {
+                  widgetClassNames: 'form-select-medium'
+                }
+              },
+              first: {
+                'ui:title': 'Your first name',
+                'ui:required': hasNotAnonymous
+              },
+              last: {
+                'ui:title': 'Your last name',
+                'ui:required': hasNotAnonymous
+              },
+              middle: {
+                'ui:title': 'Your middle name'
+              },
+              suffix: {
+                'ui:title': 'Your suffix'
+              },
+              'ui:order': ['prefix', 'first', 'middle', 'last', 'suffix'],
+              'ui:options': {
+                expandUnder: 'onBehalfOf',
+                expandUnderCondition: isNotAnonymous
+              }
+            }),
+            dob: _.merge(dateUI('Date of birth'), {
+              'ui:options': {
+                expandUnder: 'onBehalfOf',
+                expandUnderCondition: isNotAnonymous
+              }
+            }),
+            serviceAffiliation: { // could wrap service info in an object
+              'ui:title': 'Service affiliation',
+              'ui:options': {
+                expandUnder: 'onBehalfOf',
+                expandUnderCondition: myself
+              },
+              'ui:required': hasMyself,
+            },
+            serviceBranch: {
+              'ui:title': 'Branch of service',
+              'ui:options': {
+                hideIf: isNotVeteranOrServiceMember,
+                expandUnder: 'onBehalfOf',
+                expandUnderCondition: myself
+              }
+            },
+            serviceDateRange: _.merge(dateRangeUI(
+              'Service start date',
+              'Service end date',
+              'End of service must be after start of service'
+            ),
+            {
+              'ui:options': {
+                hideIf: isNotVeteranOrServiceMember,
+                expandUnder: 'onBehalfOf',
+                expandUnderCondition: myself
+              }
+            }),
+            email: {
+              'ui:title': 'Email',
+              'ui:options': {
+                expandUnder: 'onBehalfOf',
+                expandUnderCondition: anonymous
+              }
+            }
           },
           schema: {
             type: 'object',
+            required: [
+              'onBehalfOf',
+            ],
             properties: {
-              name: {
-                type: 'string'
-              }
+              onBehalfOf: _.set('enumNames', [myself, someoneElse, anonymousLabel], onBehalfOf),
+              fullName,
+              dob,
+              serviceAffiliation,
+              serviceBranch,
+              serviceDateRange,
+              email
             }
           }
         }
