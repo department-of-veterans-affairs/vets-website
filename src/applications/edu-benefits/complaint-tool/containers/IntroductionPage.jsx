cd3769858fe8e927887378f6b0cce2888409315e
Claire Hsu <clairehsu86@gmail.com>
2018-08-01 12:02:48 -0700

    Content edits

--- src/applications/edu-benefits/complaint-tool/containers/IntroductionPage.jsx
+++ src/applications/edu-benefits/complaint-tool/containers/IntroductionPage.jsx
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 
 import { focusElement } from '../../../../platform/utilities/ui';
 import FormTitle from 'us-forms-system/lib/js/components/FormTitle';
+import OMBInfo from '@department-of-veterans-affairs/formation/OMBInfo';
 import SaveInProgressIntro, { introActions, introSelector } from '../../../../platform/forms/save-in-progress/SaveInProgressIntro';
 
 class IntroductionPage extends React.Component {
@@ -15,8 +16,7 @@ class IntroductionPage extends React.Component {
     return (
       <div className="schemaform-intro">
         <FormTitle title="GI Bill® School Feedback Tool"/>
-        <p>You can submit feedback to us if your GI Bill school isn’t following the Principles of Excellence guidelines or if you have any other concerns or issues you’d like to raise with us. The Principles of Excellence program requires schools to follow certain guidelines in order to get federal funding through the GI Bill.</p>
-        <p>You can choose to submit feedback anonymously or on behalf of someone else. Any feedback sent in anonymously isn’t shared with schools or employers.</p>
+        <p>If you have an issue or complaint about a school or training facility that receives GI Bill benefits, you can submit feedback to VA.</p>       <p>You can choose to submit feedback anonymously or on behalf of someone else. We’ll share any information you provide with the schools. If you submit feedback on behalf of someone, we’ll share only your information with the school, and not the person you’re submitting on behalf of.</p>
         <p>Get started right now by filling out a short form. Follow the steps below.</p>
         <div className="process schemaform-process">
           <ol>
@@ -24,10 +24,11 @@ class IntroductionPage extends React.Component {
               <div><h5>Prepare</h5></div>
               <div><h6>To fill out this form, you’ll need to:</h6></div>
               <ul>
-                <li>Enter your school information and address</li>
-                <li>Tell us which education benefit you’re using</li>
-                <li>Give us your feedback and how you think we could make things better. (1,000 characters maximum)</li>
-                <li>Provide your email if you want us to get back to you</li>
+                <li>Provide your school’s information and address.</li>
+                <li>Tell us the education benefits you’re using.</li>
+                <li>Give us your feedback. Please provide as much detail as possible, so we understand your issue or complaint.</li>
+                <li>Let us know how you think we could resolve your issue.</li>
+                <li>Provide your email address if you want us to get in touch with you directly</li>
               </ul>
             </li>
             <li className="process-step list-two">
@@ -36,16 +37,17 @@ class IntroductionPage extends React.Component {
             </li>
             <li className="process-step list-three">
               <div><h5>VA Review</h5></div>
-              <p>We review feedback in the order we receive it. We may contact you if we need more information from you.</p>
-              <p>If we need to get in touch with you, we’ll contact you from an email that will look like this: process.vbavaco@va.gov.</p>
+              <p>We’ll review your feedback and then send it to the school. We may contact you if we need more information from you.</p>
+              <p>Feedback that isn’t related to VA education benefits may be sent to another agency for review. We’ll get back to you within 45 days to let you know how we’re handling your feedback.</p>
             </li>
             <li className="process-step list-four">
-              <div><h5>VA Follow Up</h5></div>
-              <p>We’ll get back to you within 45 days to let you know how we’re handling your feedback and if we’ve had any communication with your school. Feedback that isn’t related to VA education benefits may be sent to another agency for review.</p>
+              <div><h5>School Follow Up</h5></div>
+              <p>The school will review your feedback and send us their response.</p>
             </li>
             <li className="process-step list-five">
-              <div><h5>School Follow Up</h5></div>
-              <p>The school will review your feedback. We’ll send you the school’s response and ask you if you think their response resolves your issue. In some cases, the school may get in touch with you directly to discuss your feedback.</p>
+              <div><h5>VA Follow Up</h5></div>
+              <p>We’ll send you the school’s response to your feedback and ask you if their response resolves your issue.
+In some cases, the school may also get in touch with you directly to discuss your feedback.</p>
             </li>
           </ol>
         </div>
@@ -56,6 +58,9 @@ class IntroductionPage extends React.Component {
           startText="Submit Your Feedback"
           {...this.props.saveInProgressActions}
           {...this.props.saveInProgress}/>
+        <div className="omb-info--container" style={{ paddingLeft: '0px' }}>
+          <OMBInfo resBurden={5} ombNumber="2900-0797" expDate="12/31/2018"/>
+        </div>
       </div>
     );
   }
