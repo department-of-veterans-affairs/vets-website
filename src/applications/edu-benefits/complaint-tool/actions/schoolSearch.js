3f3aa8c8533a39bde6ad6dc7a40913651cfdf865
Rian Fowler <rian.fowler@adhocteam.us>
2018-08-01 07:57:13 -0600

    update action names to past tense

--- src/applications/edu-benefits/complaint-tool/actions/schoolSearch.js
+++ src/applications/edu-benefits/complaint-tool/actions/schoolSearch.js
@@ -5,19 +5,19 @@ import {
 export const LOAD_SCHOOLS_STARTED = 'LOAD_SCHOOLS_STARTED';
 export const LOAD_SCHOOLS_SUCCEEDED = 'LOAD_SCHOOLS_SUCCEEDED';
 export const LOAD_SCHOOLS_FAILED = 'LOAD_SCHOOLS_FAILED';
-export const SEARCH_INPUT_CHANGE = 'SEARCH_INPUT_CHANGE';
-export const SELECT_INSTITUTION = 'SELECT_INSTITUTION';
+export const SEARCH_INPUT_CHANGED = 'SEARCH_INPUT_CHANGED';
+export const INSTITUTION_SELECTED = 'INSTITUTION_SELECTED';
 
 export function searchInputChange({ searchInputValue }) {
   return {
-    type: SEARCH_INPUT_CHANGE,
+    type: SEARCH_INPUT_CHANGED,
     searchInputValue
   };
 }
 
 export function selectInstitution({ city, facilityCode, name, state }) {
   return {
-    type: SELECT_INSTITUTION,
+    type: INSTITUTION_SELECTED,
     city,
     facilityCode,
     name,
