6c83a9f99d260530754c69a804bab49468d8a4e6
Mark Greenburg <marek.greenburg@gmail.com>
2018-08-01 13:22:22 -0500

    Import schema from vets-json-schema and fix unit tests

--- src/applications/disability-benefits/526EZ/config/form.js
+++ src/applications/disability-benefits/526EZ/config/form.js
@@ -1,8 +1,8 @@
 import _ from '../../../../platform/utilities/data';
 
-// import fullSchema526EZ from 'vets-json-schema/dist/21-526EZ-schema.json';
+import fullSchema526EZ from 'vets-json-schema/dist/21-526EZ-schema.json';
 // NOTE: Easier to run schema locally with hot reload for dev
-import fullSchema526EZ from '/Users/adhocteam/Sites/vets-json-schema/dist/21-526EZ-schema.json';
+// import fullSchema526EZ from '/path/Sites/vets-json-schema/dist/21-526EZ-schema.json';
 
 import fileUploadUI from 'us-forms-system/lib/js/definitions/file';
 import ServicePeriodView from '../../../../platform/forms/components/ServicePeriodView';
