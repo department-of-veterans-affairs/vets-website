1b05c13c6c18f33d2b3149f0253a95f97eb7ab81
Rian Fowler <rianfowler@outlook.com>
2018-08-02 08:17:40 -0600

    11760 add no results state (#8098)
    * add school search field and connect it to config
    
    * add pagination behavior
    
    * add loading indicator
    
    * decouple search / pagination behavior from search input value
    
    * move helpers into component
    
    * Add manual entry fields
    
    * Update schema version
    
    * Tidy
    
    * Provide titles
    
    * move state to redux
    
    * fix schema
    
    * disable failing unit tests
    
    * Provide ZIP error message
    
    * fix school schema
    
    * add reducer tests for LOAD_SCHOOLS_SUCCESS and LOAD_SCHOOLS_STARTED; remove test exclusion
    
    * move reducer unit tests. update display title
    
    * add definition tests
    
    * add component tests; add proptypes
    
    * make facility code required
    
    * update json schema hash
    
    * yarn lock update
    
    * try using set data directly
    
    * update action names to past tense
    
    * remove commented code
    
    * remove unnecessary parenthesis
    
    * remove extra line in styling; cancel debounced method on unmount
    
    * add no results box and behavior
    
    * update unit tests
    
    * add unit tests for actions
    
    * fix typo in unit test title
    
    * use button instead of a linkj for manual entry button

---
+++ src/applications/edu-benefits/tests/complaint-tool/actions/schoolSearch.unit.spec.js
@@ -0,0 +1,89 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mockFetch, resetFetch } from '../../../../../platform/testing/unit/helpers.js';
+import {
+  setCannotFindSchool,
+  searchInputChange,
+  searchSchools,
+  selectInstitution
+} from '../../../complaint-tool/actions/schoolSearch';
+
+function setFetchResponse(stub, data) {
+  const response = new Response();
+  response.ok = true;
+  response.json = () => Promise.resolve(data);
+  stub.resolves(response);
+}
+
+describe.only('schoolSearch actions', () => {
+  describe('setCannotFindSchool', () => {
+    it('should return a SET_DATA action', () => {
+      expect(setCannotFindSchool()).to.eql({
+        type: 'SET_DATA',
+        data: {
+          school: {
+            'view:cannotFindSchool': true
+          }
+        }
+      });
+    });
+  });
+  describe('searchInputChange', () => {
+    it('should return a SEARCH_INPUT_CHANGED action', () => {
+      expect(searchInputChange({ searchInputValue: 'test' })).to.eql({
+        type: 'SEARCH_INPUT_CHANGED',
+        searchInputValue: 'test'
+      });
+    });
+  });
+  describe('searchSchools', () => {
+    it('should dispatch LOAD_SCHOOLS_STARTED and LOAD_SCHOOLS_SUCCEEDED actions', (done) => {
+      const payload = { test: 'test' };
+      mockFetch();
+      setFetchResponse(global.fetch.onFirstCall(), payload);
+
+      const dispatch = sinon.spy();
+
+      const schoolSearchQuery = {
+        institutionQuery: 'testQuery',
+        page: 1
+      };
+
+      searchSchools(schoolSearchQuery)(dispatch);
+
+      expect(dispatch.firstCall.calledWith({
+        type: 'LOAD_SCHOOLS_STARTED',
+        institutionQuery: 'testQuery',
+        page: 1
+      })).to.be.true;
+
+      setTimeout(() => {
+        expect(dispatch.secondCall.calledWith({
+          type: 'LOAD_SCHOOLS_SUCCEEDED',
+          payload,
+          institutionQuery: 'testQuery'
+        })).to.be.true;
+        resetFetch();
+        done();
+      }, 0);
+    });
+  });
+  describe('selectInstitution', () => {
+    it('should return an INSTITUTION_SELECTED action', () => {
+      const action = selectInstitution({
+        city: 'testCity',
+        facilityCode: 'testFacilityCode',
+        name: 'testName',
+        state: 'testState'
+      });
+
+      expect(action).to.eql({
+        type: 'INSTITUTION_SELECTED',
+        city: 'testCity',
+        facilityCode: 'testFacilityCode',
+        name: 'testName',
+        state: 'testState'
+      });
+    });
+  });
+});
