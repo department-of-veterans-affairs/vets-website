f410a988f2c4275781f551cb4dda02cd648b9822
R Matthews <remimatthews@brandeis.edu>
2018-08-03 13:27:01 -0400

    selects radio button

--- src/applications/edu-benefits/tests/complaint-tool/00.complaint-tool.e2e.spec.js
+++ src/applications/edu-benefits/tests/complaint-tool/00.complaint-tool.e2e.spec.js
@@ -7,29 +7,29 @@ const testData = require('./schema/maximal-test.json');
 // const FormsTestHelpers = require('../../../../platform/testing/e2e/form-helpers');
 
 module.exports = E2eHelpers.createE2eTest((client) => {
-  // client
-  //   .url(`${E2eHelpers.baseUrl}/education/complaint-tool/form/introduction`)
-  //   .waitForElementVisible('body', Timeouts.normal)
-  //   .waitForElementVisible('.usa-button-primary', Timeouts.slow)
-  //   .axeCheck('.main')
-  //   .click('.usa-button-primary');
-  // E2eHelpers.expectNavigateAwayFrom(client, '/introduction');
+  client
+    .url(`${E2eHelpers.baseUrl}/education/complaint-tool/form/introduction`)
+    .waitForElementVisible('body', Timeouts.normal)
+    .waitForElementVisible('.usa-button-primary', Timeouts.slow)
+    .axeCheck('.main')
+    .click('.usa-button-primary');
+  E2eHelpers.expectNavigateAwayFrom(client, '/introduction');
 
 
   // TODO: Fix this!
   // applicant information
   // client.waitForElementVisible('label [for="root_onBehalfOf"]', Timeouts.normal).axeCheck('.main');
-  // PageHelpers.completeApplicantInformation(client, testData.data);
-  // client
-  //   .axeCheck('.main')
-  //   .click('.form-progress-buttons .usa-button-primary');
-
-  client.url(`${E2eHelpers.baseUrl}/education/complaint-tool/form/benefits-information`);
-  client.waitForElementVisible('label [for="root_programs_Post-9/11 Ch 33"]', Timeouts.normal).axeCheck('.main');
-  PageHelpers.completeBenefitsInformation(client, testData.data);
+  PageHelpers.completeApplicantInformation(client, testData.data);
   client
     .axeCheck('.main')
     .click('.form-progress-buttons .usa-button-primary');
+
+  // client.url(`${E2eHelpers.baseUrl}/education/complaint-tool/form/benefits-information`);
+  // client.waitForElementVisible('label [for="root_programs_Post-9/11 Ch 33"]', Timeouts.normal).axeCheck('.main');
+  // PageHelpers.completeBenefitsInformation(client, testData.data);
+  // client
+  //   .axeCheck('.main')
+  //   .click('.form-progress-buttons .usa-button-primary');
 
   client.end();
 });
--- src/applications/edu-benefits/tests/complaint-tool/complaint-tool-helpers.js
+++ src/applications/edu-benefits/tests/complaint-tool/complaint-tool-helpers.js
@@ -4,17 +4,20 @@ function completeApplicantInformation(client, data) {
   client
     // .waitForElementVisible('root_onBehalfOf', Timeouts.normal)
     // .axeCheck('.main')
-    .selectRadio('input#root_onBehalfOf', data.onBehalfOf)
-    // .fillName('root_FullName', data.applicantFullName);
+    .pause(2000)
+    .selectRadio('root_onBehalfOf', data.onBehalfOf)
+    // .pause(2000)
+    // .fill('root_fullName_first', 'data.applicantFullName.first');
+    // .fillName('root_FullName', 'data.applicantFullName');
     // .waitForElementVisible('input[name="root_serviceAffiliation"]', Timeouts.slow);
 }
 
-function completeBenefitsInformation(client, data) {
-  client
-    .click('label [for="root_programs_Post-9/11 Ch 33"]');
-}
+// function completeBenefitsInformation(client, data) {
+//   client
+//     .click('label [for="root_programs_Post-9/11 Ch 33"]');
+// }
 
 module.exports = {
-  // completeApplicantInformation
-  completeBenefitsInformation
+  completeApplicantInformation
+  // completeBenefitsInformation
 };
