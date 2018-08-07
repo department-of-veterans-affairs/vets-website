6525a3250182e71d5c9c28457e8e6443a9c2a9d0
R Matthews <remimatthews@brandeis.edu>
2018-08-07 12:21:04 -0400

    added templates for other pages

--- src/applications/edu-benefits/tests/complaint-tool/00.complaint-tool.e2e.spec.js
+++ src/applications/edu-benefits/tests/complaint-tool/00.complaint-tool.e2e.spec.js
@@ -1,32 +1,51 @@
 const E2eHelpers = require('../../../../platform/testing/e2e/helpers');
 // const manifest = require('../../complaint-tool/manifest.json');
-const Timeouts = require('../../../../platform/testing/e2e/timeouts.js');
+// const Timeouts = require('../../../../platform/testing/e2e/timeouts.js');
 const PageHelpers = require('./complaint-tool-helpers');
-const testData = require('./schema/maximal-test.json');
 // const testData = require('./schema/maximal-test.json');
+const testData = require('./schema/maximal-test.json');
 // const FormsTestHelpers = require('../../../../platform/testing/e2e/form-helpers');
 
 module.exports = E2eHelpers.createE2eTest((client) => {
-  client
-    .url(`${E2eHelpers.baseUrl}/education/complaint-tool/form/introduction`)
-    .waitForElementVisible('body', Timeouts.normal)
-    .waitForElementVisible('.usa-button-primary', Timeouts.slow)
-    .axeCheck('.main')
-    .click('.usa-button-primary');
-  E2eHelpers.expectNavigateAwayFrom(client, '/introduction');
+  // subway page
+  // client
+  //   .url(`${E2eHelpers.baseUrl}/education/complaint-tool/form/introduction`)
+  //   .waitForElementVisible('body', Timeouts.normal)
+  //   .waitForElementVisible('.usa-button-primary', Timeouts.slow)
+  //   .axeCheck('.main')
+  //   .click('.usa-button-primary');
+  // E2eHelpers.expectNavigateAwayFrom(client, '/introduction');
 
 
   // TODO: Fix this!
   // applicant information
-  client.waitForElementVisible('label[for="root_onBehalfOf_0"]', Timeouts.normal);
-  PageHelpers.completeApplicantInformation(client, testData.data);
-  client
-    .axeCheck('.main')
-    .click('.form-progress-buttons .usa-button-primary');
+  // client.waitForElementVisible('label[for="root_onBehalfOf_0"]', Timeouts.normal);
+  // PageHelpers.completeApplicantInformation(client, testData.data);
+  // client
+  //   .axeCheck('.main')
+  //   .click('.form-progress-buttons .usa-button-primary');
 
   // contact information
-  client.waitForElementVisible('#root_address_street', Timeouts.normal).axeCheck('.main');
-  PageHelpers.completeContactInformation(client, testData.data);
+  // client.waitForElementVisible('#root_address_street', Timeouts.normal).axeCheck('.main');
+  // PageHelpers.completeContactInformation(client, testData.data);
+  // client
+  //   .axeCheck('.main')
+  //   .click('.form-progress-buttons .usa-button-primary');
+
+  // // benefits information
+  // client
+  //   .url(`${E2eHelpers.baseUrl}/education/complaint-tool/form/benefits-information`);
+  // // client.waitForElementVisible('root_programs_TATU', Timeouts.normal).axeCheck('.main');
+  // PageHelpers.completeBenefitsInformation(client);
+  // client
+  //   .axeCheck('.main')
+  //   .click('.form-progress-buttons .usa-button-primary');
+
+  // issue information
+  client
+    .url(`${E2eHelpers.baseUrl}/education/complaint-tool/form/feedback-information`);
+  // client.waitForElementVisible('root_programs_TATU', Timeouts.normal).axeCheck('.main');
+  PageHelpers.completeFeedbackInformation(client, testData.data);
   client
     .axeCheck('.main')
     .click('.form-progress-buttons .usa-button-primary');
