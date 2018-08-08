ae668e92a03b5d6f2b125c2b0672ad05438b81c9
R Matthews <remimatthews@brandeis.edu>
2018-08-08 14:04:43 -0400

    removed unnecessary comments

--- src/applications/edu-benefits/tests/complaint-tool/00.complaint-tool.e2e.spec.js
+++ src/applications/edu-benefits/tests/complaint-tool/00.complaint-tool.e2e.spec.js
@@ -1,61 +1,54 @@
 const E2eHelpers = require('../../../../platform/testing/e2e/helpers');
 // const manifest = require('../../complaint-tool/manifest.json');
-// const Timeouts = require('../../../../platform/testing/e2e/timeouts.js');
+const Timeouts = require('../../../../platform/testing/e2e/timeouts.js');
 const PageHelpers = require('./complaint-tool-helpers');
 // const testData = require('./schema/maximal-test.json');
 const testData = require('./schema/maximal-test.json');
 // const FormsTestHelpers = require('../../../../platform/testing/e2e/form-helpers');
 
 module.exports = E2eHelpers.createE2eTest((client) => {
-  // // subway page
-  // client
-  //   .url(`${E2eHelpers.baseUrl}/education/complaint-tool/form/introduction`)
-  //   .waitForElementVisible('body', Timeouts.normal)
-  //   .waitForElementVisible('.usa-button-primary', Timeouts.slow)
-  //   .axeCheck('.main')
-  //   .click('.usa-button-primary');
-  // E2eHelpers.expectNavigateAwayFrom(client, '/introduction');
+  // subway page
+  client
+    .url(`${E2eHelpers.baseUrl}/education/complaint-tool/form/introduction`)
+    .waitForElementVisible('body', Timeouts.normal)
+    .waitForElementVisible('.usa-button-primary', Timeouts.slow)
+    .axeCheck('.main')
+    .click('.usa-button-primary');
+  E2eHelpers.expectNavigateAwayFrom(client, '/introduction');
 
+  // applicant information
+  client.waitForElementVisible('label[for="root_onBehalfOf_0"]', Timeouts.normal);
+  PageHelpers.completeApplicantInformation(client, testData.data);
+  client
+    .axeCheck('.main')
+    .click('.form-progress-buttons .usa-button-primary');
 
-  // // TODO: Fix this!
-  // // applicant information
-  // client.waitForElementVisible('label[for="root_onBehalfOf_0"]', Timeouts.normal);
-  // PageHelpers.completeApplicantInformation(client, testData.data);
-  // client
-  //   .axeCheck('.main')
-  //   .click('.form-progress-buttons .usa-button-primary');
+  // contact information
+  client.waitForElementVisible('[id="root_address_street"]', Timeouts.normal);
+  PageHelpers.completeContactInformation(client, testData.data);
+  client
+    .axeCheck('.main')
+    .click('.form-progress-buttons .usa-button-primary');
 
-  // // contact information
-  // client.waitForElementVisible('[id="root_address_street"]', Timeouts.normal);
-  // PageHelpers.completeContactInformation(client, testData.data);
-  // client
-  //   .axeCheck('.main')
-  //   .click('.form-progress-buttons .usa-button-primary');
+  // benefits information
+  PageHelpers.completeBenefitsInformation(client);
+  client
+    .axeCheck('.main')
+    .click('.form-progress-buttons .usa-button-primary');
 
-  // // // benefits information
-  // // client.waitForElementVisible('root_programs_TATU', Timeouts.normal).axeCheck('.main');
-  // PageHelpers.completeBenefitsInformation(client);
-  // client
-  //   .axeCheck('.main')
-  //   .click('.form-progress-buttons .usa-button-primary');
-
-    // school information
-    client
-    .url(`${E2eHelpers.baseUrl}/education/complaint-tool/form/school-information`);
-  // client.waitForElementVisible('root_programs_TATU', Timeouts.normal).axeCheck('.main');
+  // school information
+  // Note: I haven't been able to test this without the mock submission
+  client.waitForElementVisible('.search-input input', Timeouts.normal).axeCheck('.main');
   PageHelpers.completeSchoolInformation(client, testData.data);
   client
     .axeCheck('.main')
     .click('.form-progress-buttons .usa-button-primary');
 
-  // // issue information
-  // client
-  //   .url(`${E2eHelpers.baseUrl}/education/complaint-tool/form/feedback-information`);
-  // // client.waitForElementVisible('root_programs_TATU', Timeouts.normal).axeCheck('.main');
-  // PageHelpers.completeFeedbackInformation(client, testData.data);
-  // client
-  //   .axeCheck('.main')
-  //   .click('.form-progress-buttons .usa-button-primary');
+  // issue information
+  PageHelpers.completeFeedbackInformation(client, testData.data);
+  client
+    .axeCheck('.main')
+    .click('.form-progress-buttons .usa-button-primary');
 
   client.end();
 });
--- src/applications/edu-benefits/tests/complaint-tool/complaint-tool-helpers.js
+++ src/applications/edu-benefits/tests/complaint-tool/complaint-tool-helpers.js
@@ -2,8 +2,6 @@ const Timeouts = require('../../../../platform/testing/e2e/timeouts.js');
 
 function completeApplicantInformation(client, data) {
   client
-    // HACK: Shouldn't need to wait twice
-    .waitForElementVisible('label[for="root_onBehalfOf_0"]', Timeouts.normal)
     // This is failing randomly?
     .selectRadio('root_onBehalfOf', data.onBehalfOf)
     // .click('label[for="root_onBehalfOf_0"]')
@@ -15,7 +13,6 @@ function completeApplicantInformation(client, data) {
 
 function completeContactInformation(client, data) {
   client
-    // .waitForElementVisible('#root_address_street', Timeouts.normal)
     .fill('#root_address_street', data.street)
     .fill('#root_address_city', data.city)
     .waitForElementVisible('#root_address_state', Timeouts.normal)
@@ -28,25 +25,20 @@ function completeContactInformation(client, data) {
 
 function completeBenefitsInformation(client) {
   client
-    // .waitForElementVisible('label [for="root_programs_Post-9/11 Ch 33"]', Timeouts.normal)
     .fillCheckbox('#root_programs_TATU');
-  // Note: I don't think this likes spaces...
-  // .fillCheckbox('#root_programs_Post-9/11 Ch 33');
+  // Note: I don't think this likes spaces... for example: .fillCheckbox('#root_programs_Post-9/11 Ch 33');
 }
 
 function completeSchoolInformation(client) {
   client
-    // .waitForElementVisible('label [for="root_programs_Post-9/11 Ch 33"]', Timeouts.normal)
     .fill('.search-input input', 'Brandeis')
     .click('.search-schools-button')
-    .pause(4000)
     .waitForElementVisible('.radio-button', Timeouts.normal)
     .selectRadio('page-1-0', 31003921);
 }
 
 function completeFeedbackInformation(client, data) {
   client
-    // .waitForElementVisible('label [for="#root_issue_accreditation"]', Timeouts.normal)
     .fillCheckbox('#root_issue_accreditation')
     .fill('#root_issueDescription', data.issueDescription)
     .fill('#root_issueResolution', data.issueResolution);
