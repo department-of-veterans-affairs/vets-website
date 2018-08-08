233ae22ca1b926e4a5b8077b96ad31fbb92781a4
Bill Tran <bill@billjohntran.com>
2018-08-07 18:02:18 -0700

    add menu toggle for megamenu

--- src/platform/site-wide/mega-menu/actions/index.js
+++ src/platform/site-wide/mega-menu/actions/index.js
@@ -1,9 +1,15 @@
 export const UPDATE_CURRENT_SECTION = 'UPDATE_CURRENT_SECTION';
 export const TOGGLE_PANEL_OPEN = 'TOGGLE_PANEL_OPEN';
+export const TOGGLE_DISPLAY_HIDDEN = 'TOGGLE_DISPLAY_HIDDEN';
 
 export const togglePanel = (megaMenu) => ({
   type: 'TOGGLE_PANEL_OPEN',
   megaMenu,
+});
+
+export const toggleDisplayHidden = (display) => ({
+  type: 'TOGGLE_DISPLAY_HIDDEN',
+  display,
 });
 
 export function updateCurrentSection(currentSection) {
@@ -12,6 +18,22 @@ export function updateCurrentSection(currentSection) {
     currentSection,
   };
 }
+
+export const toggleMobileDisplayHidden = (hidden) => {
+  return (dispatch, getState) => {
+    const state = getState();
+
+    if (window.innerWidth > 768) {
+      dispatch(toggleDisplayHidden({}));
+    } else if (hidden) {
+      dispatch(toggleDisplayHidden({ hidden: true }));
+    } else if (!Object.prototype.hasOwnProperty.call(state.megaMenu.display, 'hidden')) {
+      dispatch(toggleDisplayHidden({ hidden: true }));
+    } else {
+      dispatch(toggleDisplayHidden({}));
+    }
+  };
+};
 
 export function togglePanelOpen(currentDropdown) {
   return (dispatch, getState) => {
