4da98e310a8a13f151ae2b102d7392069402926c
Mark Greenburg <marek.greenburg@gmail.com>
2018-08-09 16:52:07 -0500

    Update test

--- src/applications/post-911-gib-status/tests/containers/IntroPage.unit.spec.jsx
+++ src/applications/post-911-gib-status/tests/containers/IntroPage.unit.spec.jsx
@@ -33,8 +33,8 @@ describe('<IntroPage/>', () => {
     expect(wrapper.find('Link').first().props().to).to.equal('status');
   });
 
-  it('should render an alert', () => {
+  it('should render an alert when GIBS status down', () => {
     const wrapper = shallow(<IntroPage {...defaultProps} serviceAvailability={SERVICE_AVAILABILITY_STATES.down}/>);
-    expect(wrapper.find('.usa-alert')).to.have.lengthOf(1);
+    expect(wrapper.find('.usa-alert')).to.have.lengthOf(2);
   });
 });
