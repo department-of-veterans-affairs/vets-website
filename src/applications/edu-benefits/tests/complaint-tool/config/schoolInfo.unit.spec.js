226d7451f3762cd9e429eee65942f93041df05bb
Rian Fowler <rianfowler@outlook.com>
2018-08-10 14:58:02 -0600

    12067 move manual entry checkbox (#8148)
    * add checkbox to custom form and start working through error display
    
    * add error message display logic
    
    * add action unit test
    
    * add new tests to reducer
    
    * add new unit test for schoolselectfield
    
    * fix form config unit tests
    
    * remove space
    
    * update checkbox label content
    
    * hide radio button selection in review mode when manual entry selected
    
    * return null when not rendering SchoolSelectField
    
    * prevent start over from hiding errors
    
    * fix address3 bug
    
    * fix some styling issues
    
    * use unset instead of delete

--- src/applications/edu-benefits/tests/complaint-tool/config/schoolInfo.unit.spec.js
+++ src/applications/edu-benefits/tests/complaint-tool/config/schoolInfo.unit.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { mount } from 'enzyme';
-
+import _ from 'lodash';
 import {
   DefinitionTester,
   fillData
@@ -11,6 +11,7 @@ import formConfig from '../../../complaint-tool/config/form';
 
 describe('complaint tool school info', () => {
   const { schema, uiSchema } = formConfig.chapters.schoolInformation.pages.schoolInformation;
+  _.unset(uiSchema, 'school.facilityCode');
 
   it('should render', () => {
     const form = mount(
@@ -18,7 +19,9 @@ describe('complaint tool school info', () => {
         schema={schema}
         data={{
           school: {
-            'view:cannotFindSchool': true
+            facilityCode: {
+              manualSchoolEntryChecked: true
+            }
           }
         }}
         definitions={formConfig.defaultDefinitions}
@@ -35,7 +38,9 @@ describe('complaint tool school info', () => {
         schema={schema}
         data={{
           school: {
-            'view:cannotFindSchool': true
+            facilityCode: {
+              manualSchoolEntryChecked: true
+            }
           }
         }}
         definitions={formConfig.defaultDefinitions}
@@ -55,7 +60,9 @@ describe('complaint tool school info', () => {
         schema={schema}
         data={{
           school: {
-            'view:cannotFindSchool': true
+            facilityCode: {
+              manualSchoolEntryChecked: true
+            }
           }
         }}
         definitions={formConfig.defaultDefinitions}
