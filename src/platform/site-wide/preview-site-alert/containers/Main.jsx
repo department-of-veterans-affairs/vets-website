b1532fc4dadb396d99bcc5e3cf74bf310f02a43b
Bill Tran <bill@billjohntran.com>
2018-08-20 11:53:19 -0700

    update preview site alert

--- src/platform/site-wide/preview-site-alert/containers/Main.jsx
+++ src/platform/site-wide/preview-site-alert/containers/Main.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import AlertBox from '@department-of-veterans-affairs/formation/AlertBox';
 
 const headline = "You're viewing a preview of the new VA.gov";
-const content =  "We're building a new online experience to make it easier for Veterans to find, access, and manage the benefits they've earned. And we're building it with input from Veterans at every step. Please note that the content and the way the site functions may change as we continue to build the new VA.gov.";
+const content =  "We're building a new online experience to make it easier for Veterans to find, access, and manage the benefits they've earned. And we're building it with input from Veterans at every step. Please note: We work iteratively using agile development practices, so you'll see new content, features, and functionality added to the site every day.";
 
 export class Main extends React.Component {
   render() {
