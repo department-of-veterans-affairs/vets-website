cf3bf0966ce5e59ac4d015f2684782ecb1ef855e
Mark Greenburg <marek.greenburg@gmail.com>
2018-08-22 10:18:11 -0500

    Unit tests for separation / training pay page

---
+++ src/applications/disability-benefits/all-claims/tests/pages/separationTrainingPay.unit.spec.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+import { DefinitionTester } from '../../../../../platform/testing/unit/schemaform-utils';
+import formConfig from '../../config/form';
+
+describe('Separation or Training Pay', () => {
+  const {
+    schema,
+    uiSchema
+  } = formConfig.chapters.veteranDetails.pages.separationTrainingPay;
+  const { defaultDefinitions: definitions } = formConfig;
+
+  it('should render', () => {
+    const form = mount(
+      <DefinitionTester
+        definitions={definitions}
+        schema={schema}
+        uiSchema={uiSchema}
+        data={{}}
+        formData={{}}/>
+    );
+
+    // Expect two questions with two radio inputs each
+    expect(form.find('.form-radio-buttons').length).to.equal(2);
+    expect(form.find('input').length).to.equal(4);
+  });
+
+  it('should fail to submit if no answers provided', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        definitions={definitions}
+        schema={schema}
+        uiSchema={uiSchema}
+        data={{}}
+        formData={{}}
+        onSubmit={onSubmit}/>
+    );
+
+    form.find('form').simulate('submit');
+    expect(form.find('.usa-input-error-message').length).to.equal(2);
+    expect(onSubmit.called).to.be.false;
+  });
+
+  it('should submit if both questions answered', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        definitions={definitions}
+        schema={schema}
+        uiSchema={uiSchema}
+        data={{
+          trainingPay: false,
+          separationPay: false
+        }}
+        formData={{}}
+        onSubmit={onSubmit}/>
+    );
+
+    form.find('form').simulate('submit');
+    expect(form.find('.usa-input-error-message').length).to.equal(0);
+    expect(onSubmit.calledOnce).to.be.true;
+  });
+});
