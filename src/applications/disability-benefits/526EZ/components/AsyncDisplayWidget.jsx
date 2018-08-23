802e027571f3e6754da4d922d142b4ecb399b45f
Christopher Valarida <chris.valarida@adhocteam.us>
2018-08-23 15:44:06 -0700

    Check for truthiness

--- src/applications/disability-benefits/526EZ/components/AsyncDisplayWidget.jsx
+++ src/applications/disability-benefits/526EZ/components/AsyncDisplayWidget.jsx
@@ -44,7 +44,7 @@ export default class AsyncDisplayWidget extends React.Component {
     // TODO: Don't call the callback _every_ time the component is mounted
     const cbPromise = this.props.options.callback();
     // instanceof Promise doesn't work in Firefox, so we just check for .then() and hope it's a promise
-    if (typeof cbPromise.then === 'function') {
+    if (cbPromise && typeof cbPromise.then === 'function') {
       cbPromise.then((data) => {
         this.setState({
           data,
