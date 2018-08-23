ef29ff84864fcb6db40cccbf39cd9a93d86be0d1
Bill Tran <bill@billjohntran.com>
2018-08-23 13:14:23 -0700

    add data file to health-car left-rail-nav

---
+++ src/platform/site-wide/left-rail-navs/health-care/data.js
@@ -0,0 +1,131 @@
+const data = [
+  {
+    title: 'Get Benefits',
+    links: [
+      {
+        text: 'About VA Health Benefits',
+        href: 'health-care/about-va-health-care'
+      },
+      {
+        text: 'Eligibility',
+        href: 'health-care/eligibility/'
+      },
+      {
+        text: 'How to Apply',
+        href: 'health-care/apply'
+      },
+      {
+        text: 'Apply Now',
+        href: 'health-care/apply/application/introduction'
+      },
+      {
+        text: 'After You Apply',
+        href: 'health-care/after-you-apply'
+      },
+      {
+        text: 'Family and Caregiver Benefits',
+        href: 'health-care/family-caregiver-health-benefits'
+      },
+    ]
+  },
+  {
+    title: 'Manage Your Health',
+    links: [
+      {
+        text: 'Refill and Track Prescriptions',
+        href: '#'
+      },
+      {
+        text: 'Use Secure Messaging',
+        href: '#'
+      },
+      {
+        text: 'Schedule and View Appointments',
+        href: '#'
+      },
+      {
+        text: 'View Test and Lab Results',
+        href: '#'
+      },
+      {
+        text: 'Order Hearing Aid Batteries and Prosthetic Socks',
+        href: '#'
+      },
+      {
+        text: 'Get Medical Records',
+        href: '#'
+      },
+      {
+        text: 'Update Health Benefits Info',
+        href: '#'
+      },
+      {
+        text: 'Make a Payment',
+        href: '#'
+      },
+      {
+        text: 'Check Appeal Status',
+        href: '#'
+      },
+      {
+        text: 'Get Travel Pay',
+        href: '#'
+      },
+      {
+        text: 'Request a Health ID Card',
+        href: '#'
+      },
+    ]
+  },
+  {
+    title: 'More Resources',
+    links: [
+      {
+        text: 'Costs of Care',
+        href: '#',
+      },
+      {
+        text: 'Health Needs and Conditions',
+        href: '#',
+      },
+      {
+        text: 'Health Benefits Explorer',
+        href: '#',
+      },
+      {
+        text: 'Wellness Programs',
+        href: '#',
+      },
+      {
+        text: 'Chemical or Hazardous Material Exposures',
+        href: '#',
+      },
+      {
+        text: 'Veterans Choice Program',
+        href: '#',
+      },
+      {
+        text: 'Affordable Care Act',
+        href: '#',
+      },
+      {
+        text: 'The Million Veteran Program',
+        href: '#',
+      },
+      {
+        text: 'Access and Quality in VA Health Care',
+        href: '#',
+      },
+      {
+        text: 'Patient Rights and Responsibilities',
+        href: '#',
+      },
+      {
+        text: 'Community Care',
+        href: '#',
+      },
+    ]
+  },
+];
+
+export default data;
