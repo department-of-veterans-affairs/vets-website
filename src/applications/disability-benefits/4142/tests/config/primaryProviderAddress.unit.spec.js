b87c582a46390e7b1dc6a718cf89c0974b125764
Imee Cuison <cuison_imelda@bah.com>
2018-08-23 08:28:19 -0400

    WIP unit test for 4142 form

--- src/applications/disability-benefits/4142/tests/config/primaryProviderAddress.unit.spec.js
+++ src/applications/disability-benefits/4142/tests/config/primaryProviderAddress.unit.spec.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { expect } from 'chai';
-// import sinon from 'sinon';
+import sinon from 'sinon';
 import { mount } from 'enzyme';
 
 import {
   DefinitionTester, // selectCheckbox
 } from '../../../../../platform/testing/unit/schemaform-utils.jsx';
 import formConfig from '../../config/form.js';
-// import { STATES, COUNTRIES} from '../../constants';
+import initialData from '../schema/initialData.js';
+
 
 describe('Disability benefits 4142 provider primary address', () => {
   const {
@@ -20,46 +21,47 @@ describe('Disability benefits 4142 provider primary address', () => {
       <DefinitionTester
         definitions={formConfig.defaultDefinitions}
         schema={schema}
-        data={{}}
+        data={{ initialData }}
         uiSchema={uiSchema}/>,
     );
 
     expect(form.find('input').length).to.equal(9);
     expect(form.find('select').length).to.equal(6);
   });
-  // it('does not submit without required info', () => {
+
+  // it('does not submit without required info', () => { //TODO: TEST IS FAILING.
   //   const onSubmit = sinon.spy();
   //   const form = mount(
   //     <DefinitionTester
   //       definitions={formConfig.defaultDefinitions}
   //       schema={schema}
-  //       data={{
-  //         veteran: {
-  //           phoneEmailCard: {
-  //             primaryPhone: '',
-  //             emailAddress: '',
-  //           },
-  //           mailingAddress: {
-  //             country: '',
-  //             addressLine1: '',
-  //             city: '',
-  //           },
-  //           'view:hasForwardingAddress': true,
-  //           forwardingAddress: {
-  //             effectiveDate: '',
-  //             country: '',
-  //             addressLine1: '',
-  //             city: '',
-  //           },
-  //         },
-  //       }}
+  //       data={{ }}
   //       formData={{}}
   //       onSubmit={onSubmit}
   //       uiSchema={uiSchema}/>,
   //   );
   //
   //   form.find('form').simulate('submit');
-  //   expect(form.find('.usa-input-error-message').length).to.equal(9);
+  //   expect(form.find('.usa-input-error').length).to.equal(6);
+  //
   //   expect(onSubmit.called).to.be.false;
   // });
+
+  it('should submit with required info', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        definitions={formConfig.defaultDefinitions}
+        schema={schema}
+        data={{ initialData }}
+        formData={{ initialData }}
+        onSubmit={onSubmit}
+        uiSchema={uiSchema}/>,
+    );
+
+    form.find('form').simulate('submit');
+    expect(form.find('.usa-input-error').length).to.equal(0);
+
+    expect(onSubmit.called).to.be.true;
+  });
 });
---
+++ src/applications/disability-benefits/4142/tests/schema/initialData.js
@@ -0,0 +1,26 @@
+export default {
+  privateMedicalProvider: [
+    {
+      privatePrimaryPhoneNumber: 5420,
+      privateProviderCity: 'Post traumatic stress disorder',
+      privateProviderCountry: '9459392',
+      privateProviderPostalCode: '63655',
+      privateProviderState: 40,
+      privateProviderStreetAddress: ''
+      // specialIssues: [
+      //   {
+      //     code: 'TRM',
+      //     name: 'Personal Trauma PTSD',
+      //   },
+      // ],
+    },
+    // {
+    //   diagnosticCode: 5678,
+    //   name: 'Intervertebral disc syndrome',
+    //   ratedDisabilityId: '1231231',
+    //   ratingPercentage: 0,
+    //   ratingDecisionId: '63456',
+    //   specialIssues: [],
+    // },
+  ],
+};
