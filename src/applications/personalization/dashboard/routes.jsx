2d7c7e83a169ad1f4a17ec5cbc909927c9a4d16a
Ben Shyong <ben.shyong@gmail.com>
2018-08-24 09:24:55 -0500

    route gating

--- src/applications/personalization/dashboard/routes.jsx
+++ src/applications/personalization/dashboard/routes.jsx
@@ -2,20 +2,24 @@ import DashboardApp from './containers/DashboardApp';
 import DashboardAppWrapper from './containers/DashboardAppWrapper';
 import SetPreferences from '../preferences/SetPreferences';
 
+const preferencesRoute = {
+  path: 'preferences',
+  component: SetPreferences,
+  key: 'preferences',
+  name: 'Find VA Benefits',
+};
+
+if (document.location.hostname !== 'staging.vets.gov') {
+  preferencesRoute.onEnter = (nextState, replace) => replace('/');
+}
+
 const routes = {
   path: '/',
   component: DashboardAppWrapper,
   indexRoute: { component: DashboardApp },
-  childRoutes: []
+  childRoutes: [
+    preferencesRoute,
+  ]
 };
-
-if (document.location.hostname === 'staging.vets.gov') {
-  routes.childRoutes.push({
-    path: 'preferences',
-    component: SetPreferences,
-    key: 'preferences',
-    name: 'Find VA Benefits',
-  });
-}
 
 export default routes;
