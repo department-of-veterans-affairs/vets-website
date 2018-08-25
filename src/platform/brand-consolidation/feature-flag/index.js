d810facd88874dabd38060ee432130d4d4d12d10
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-08-25 15:06:09 -0400

    Change module to a function instead

--- src/platform/brand-consolidation/feature-flag/index.js
+++ src/platform/brand-consolidation/feature-flag/index.js
@@ -1,6 +1,8 @@
 /**
- * A value for determining whether the current Webpack build is executing as the brand-consolidation of Vets.gov/VA.gov.
- * @type {Boolean}
+ * Returns whether the current Webpack build is executing as the brand-consolidation of Vets.gov/VA.gov.
+ * @returns {boolean}
  * @module platform/brand-consolidation/feature-flag
  */
-export default process.env.IS_BRAND_CONSOLIDATED_BUILD;
+export default function isBrandConsolidation() {
+  return process.env.IS_BRAND_CONSOLIDATED_BUILD !== undefined;
+}
