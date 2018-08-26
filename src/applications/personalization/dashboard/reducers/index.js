29582131da406f149ccce88b515aa01c446479a3
Ben <bshyong@users.noreply.github.com>
2018-08-26 13:15:11 -0500

    Ihub widget (#8243)
    * ihub appointments widget
    
    * update unit test
    
    * only gate in production

--- src/applications/personalization/dashboard/reducers/index.js
+++ src/applications/personalization/dashboard/reducers/index.js
@@ -2,10 +2,12 @@ import claimsAppeals from '../../../claims-status/reducers';
 import prescriptions from '../../../rx/reducers/prescriptions';
 import recipients from '../../../messaging/reducers/recipients';
 import folders from '../../../messaging/reducers/folders';
+import appointments from '../../appointments/reducers';
 import { combineReducers } from 'redux';
 
 export default {
   ...claimsAppeals,
+  appointments,
   health: combineReducers({
     rx: combineReducers({
       prescriptions,
