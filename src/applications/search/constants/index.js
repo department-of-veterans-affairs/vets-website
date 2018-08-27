d98278170f832cbbcbdd7ab5af3f709e02a6af48
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-08-27 14:42:00 -0400

    Plug in user input to action creator

---
+++ src/applications/search/constants/index.js
@@ -0,0 +1,3 @@
+export const BASE_URI = 'https://search.usa.gov/api/v2/search';
+export const AFFILIATE = 'vets.gov_search';
+export const ACCESS_KEY = process.env.SEARCH_ACCESS_KEY || 'put access key here';
--- src/applications/search/containers/SearchApp.jsx
+++ src/applications/search/containers/SearchApp.jsx
@@ -14,13 +14,34 @@ class SearchApp extends React.Component {
     fetchSearchResults: PropTypes.func.isRequired
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: ''
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      userInput: event.target.value
+    });
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.fetchSearchResults(this.state.userInput);
+  }
+
   render() {
     return (
       <div className="row user-profile-row">
         <div className="usa-width-two-thirds medium-8 small-12 columns">
           <h1>Search App</h1>
+          <form onSubmit={this.handleFormSubmit}>
+            <input type="text" name="query" value={this.state.userInput} onChange={this.handleInputChange}/>
+            <button type="submit">Fetch search results</button>
+          </form>
           <h2>{JSON.stringify(this.props.search.results)}</h2>
-          <button onClick={this.props.fetchSearchResults}>Fetch search results</button>
         </div>
       </div>
     );
