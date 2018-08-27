1188a39836d072de91e15ae541bfb050565f2b8b
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-08-27 18:34:04 -0400

    Add a comment explaning why we can't rely on booleans here

--- src/platform/brand-consolidation/feature-flag.js
+++ src/platform/brand-consolidation/feature-flag.js
@@ -4,5 +4,11 @@
  * @module platform/brand-consolidation/feature-flag
  */
 export default function isBrandConsolidationEnabled() {
+
+  // Properties defined on process.env are always parsed into a string. We're using Webpack,
+  // however, so our process.env isn't a true Node process.env, so we can use booleans. The problem
+  // though is that Mocha does use a true process.env, so false becomes 'false', and makes the unit
+  // tests confusing. For consistency, the existence of BRAND_CONSOLIDATION_ENABLED evaluates to true.
+
   return process.env.BRAND_CONSOLIDATION_ENABLED !== undefined;
 }
