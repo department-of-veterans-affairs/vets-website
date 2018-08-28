82c34506aea12cd8fa2e5cc9c88d7d790828fcc5
Erik P. Hansen <eph@erikphansen.com>
2018-08-28 08:31:07 -0700

    Send the same school data regardless of school input method (#8267)
    * Send the same school data regardless of school input method
    
    * Convert "USA" to "United States"

--- src/applications/edu-benefits/feedback-tool/selectors/schoolSearch.js
+++ src/applications/edu-benefits/feedback-tool/selectors/schoolSearch.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 export const selectCurrentPageNumber = state => _.get(state, 'schoolSelect.currentPageNumber');
-export const selectFacilityCodeErrorMessages = ownProps => _.get(ownProps, 'errorSchema.facilityCode.__errors');
+export const selectFacilityCodeErrorMessages = ownProps => _.get(ownProps, 'errorSchema.view:facilityCode.__errors');
 export const selectFormSubmitted = ownProps => _.get(ownProps, 'formContext.submitted');
 export const selectInstitutionQuery = state => _.get(state, 'schoolSelect.institutionQuery');
 export const selectInstitutions = state => _.get(state, 'schoolSelect.institutions');
