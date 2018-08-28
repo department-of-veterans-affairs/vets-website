a6c1ef3ab0d20c91d4243b4526836e625fd5f557
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-08-28 18:46:15 -0400

    Change arg name

--- src/platform/brand-consolidation/routes/index.js
+++ src/platform/brand-consolidation/routes/index.js
@@ -6,21 +6,21 @@ export function setupRoutes(_state) {
 
 /**
  * Accepts a Vets.gov page path and resolves it to the VA.gov page path in the event that brand-consolidation is enabled and that page has moved.
- * @param {string} path - The Vets.gov page path
+ * @param {string} vetsPath - The Vets.gov page path
  * @returns {string} The resolved path of the page
  */
-export default function getRoute(path) {
+export default function getRoute(vetsPath) {
   const {
     brandConsolidationEnabled,
     routes = []
   } = state.buildSettings;
 
-  if (!brandConsolidationEnabled) return path;
+  if (!brandConsolidationEnabled) return vetsPath;
 
-  const route = routes.find(r => r['vets.gov'] === path);
+  const route = routes.find(r => r['vets.gov'] === vetsPath);
   if (!route) {
     /* eslint-disable no-console */
-    console.warn(`Route ${path} was not found.`);
+    console.warn(`Route ${vetsPath} was not found.`);
   }
 
   return route['va.gov'];
