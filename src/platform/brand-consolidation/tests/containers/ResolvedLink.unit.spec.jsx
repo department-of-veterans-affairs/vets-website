4dc65c8f02950ddb4b4d9f65b9517ed3bc83ef3f
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-08-29 17:13:41 -0400

    Add className to test tag

--- src/platform/brand-consolidation/tests/containers/ResolvedLink.unit.spec.jsx
+++ src/platform/brand-consolidation/tests/containers/ResolvedLink.unit.spec.jsx
@@ -25,7 +25,7 @@ describe('<ResolvedLink/>', () => {
   ];
 
   it('replaces HREF values with the VA.gov path if that Vets.gov value exists in redirects.json', () => {
-    const link = <a href="/health-care/apply/">Apply now</a>;
+    const link = <a className="test" href="/health-care/apply/">Apply now</a>;
     const props = mapStateToProps(state, { children: link }, ROUTES);
     const wrapper = enzyme.shallow(
       <ResolveLink {...props}>
@@ -33,12 +33,12 @@ describe('<ResolvedLink/>', () => {
       </ResolveLink>
     );
 
-    expect(wrapper.html()).to.be.equal('<a href="/health-care/how-to-apply/">Apply now</a>');
+    expect(wrapper.html()).to.be.equal('<a class="test" href="/health-care/how-to-apply/">Apply now</a>');
   });
 
   it('does not replace the HREF if brandConsolidation is not enabled', () => {
     state.buildSettings.brandConsolidation.enabled = false;
-    const link = <a href="/health-care/apply/">Apply now</a>;
+    const link = <a className="test" href="/health-care/apply/">Apply now</a>;
     const props = mapStateToProps(state, { children: link }, ROUTES);
     const wrapper = enzyme.shallow(
       <ResolveLink {...props}>
@@ -46,11 +46,11 @@ describe('<ResolvedLink/>', () => {
       </ResolveLink>
     );
 
-    expect(wrapper.html()).to.be.equal('<a href="/health-care/apply/">Apply now</a>');
+    expect(wrapper.html()).to.be.equal('<a class="test" href="/health-care/apply/">Apply now</a>');
   });
 
   it('does not replace the HREF it the route is not found during the lookup', () => {
-    const link = <a href="/health-care/apply/">Apply now</a>;
+    const link = <a className="test" href="/health-care/apply/">Apply now</a>;
     const props = mapStateToProps(state, { children: link }, []);
     const wrapper = enzyme.shallow(
       <ResolveLink {...props}>
@@ -58,6 +58,6 @@ describe('<ResolvedLink/>', () => {
       </ResolveLink>
     );
 
-    expect(wrapper.html()).to.be.equal('<a href="/health-care/apply/">Apply now</a>');
+    expect(wrapper.html()).to.be.equal('<a class="test" href="/health-care/apply/">Apply now</a>');
   });
 });
