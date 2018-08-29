6ef9ba8dea43947dd279d2ea902a5c4913768a0d
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-08-29 14:03:24 -0400

    Implement toggle into reducer

--- src/platform/monitoring/BuildSettings/reducer.js
+++ src/platform/monitoring/BuildSettings/reducer.js
@@ -1,7 +1,27 @@
+import {
+  TOGGLE_BRAND_CONSOLIDATION_ENABLED
+} from '../../brand-consolidation/actions/dev';
+
 const initialState = {
+  brandConsolidation: {
+    enabled: false,
+    routes: []
+  },
   ...window.settings
 };
 
-export default function buildSettingsReducer(state = initialState) {
-  return state;
+export default function buildSettingsReducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_BRAND_CONSOLIDATION_ENABLED:
+      return {
+        ...state,
+        brandConsolidation: {
+          ...state.brandConsolidation,
+          enabled: !state.brandConsolidation.enabled
+        }
+      };
+
+    default:
+      return state;
+  }
 }
