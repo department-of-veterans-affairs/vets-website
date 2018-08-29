496b5140a743d98a98836fe30334b55a5ab4d016
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-08-29 14:14:17 -0400

    add a comment discouraging use

--- src/platform/brand-consolidation/feature-flag.js
+++ src/platform/brand-consolidation/feature-flag.js
@@ -1,5 +1,6 @@
 /**
  * Returns whether the current Webpack build is executing as the brand-consolidation of Vets.gov/VA.gov.
+ * Since this is driven by the window.settings, rather than state, you should prefer to use the containers instead.
  * @returns {boolean}
  * @module platform/brand-consolidation/feature-flag
  */
