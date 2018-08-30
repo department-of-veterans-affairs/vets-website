c0a63879ede12553882dbe12dbb684befe2c30e0
Mark Greenburg <marek.greenburg@gmail.com>
2018-08-30 13:40:05 -0500

    Add ratedDisabilities page schema and uiSchema to all-claims form

--- src/applications/disability-benefits/all-claims/pages/ratedDisabilities.js
+++ src/applications/disability-benefits/all-claims/pages/ratedDisabilities.js
@@ -1,7 +1,40 @@
+import fullSchema from '../config/schema';
+import SelectArrayItemsWidget from '../components/SelectArrayItemsWidget';
+import {
+  disabilityOption,
+  disabilitiesClarification
+} from '../content/ratedDisabilities';
+
+const { disabilities: disabilitiesSchema } = fullSchema.properties;
+
 export const uiSchema = {
+  'ui:title': 'Rated Disabilities',
+  'ui:description':
+    `Below are your rated disabilities. Please choose the disability that 
+    you’re filing for an increase because the condition has gotten worse.`,
+  disabilities: {
+    'ui:field': 'StringField',
+    'ui:widget': SelectArrayItemsWidget,
+    'ui:options': {
+      showFieldLabel: 'label',
+      label: disabilityOption,
+      widgetClassNames: 'widget-outline',
+      keepInPageOnReview: true
+    }
+  },
+  'view:disabilitiesClarification': {
+    'ui:description': disabilitiesClarification
+  }
+
 };
 
 export const schema = {
   type: 'object',
-  properties: {}
+  properties: {
+    disabilities: disabilitiesSchema,
+    'view:disabilitiesClarification': {
+      type: 'object',
+      properties: {}
+    }
+  }
 };
