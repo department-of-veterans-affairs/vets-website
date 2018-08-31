57878a9c61adc016245da5414d31cd6e7fba35cc
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-08-31 15:57:55 -0400

    Remove unneeded code

--- src/platform/monitoring/BuildSettings/reducer.js
+++ src/platform/monitoring/BuildSettings/reducer.js
@@ -1,26 +1,7 @@
-import {
-  TOGGLE_BRAND_CONSOLIDATION_ENABLED
-} from '../../brand-consolidation/actions/dev';
-
 const initialState = {
-  brandConsolidation: {
-    enabled: false
-  },
   ...window.settings
 };
 
-export default function buildSettingsReducer(state = initialState, action) {
-  switch (action.type) {
-    case TOGGLE_BRAND_CONSOLIDATION_ENABLED:
-      return {
-        ...state,
-        brandConsolidation: {
-          ...state.brandConsolidation,
-          enabled: !state.brandConsolidation.enabled
-        }
-      };
-
-    default:
-      return state;
-  }
+export default function buildSettingsReducer(state = initialState) {
+  return state;
 }
