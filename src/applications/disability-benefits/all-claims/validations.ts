61260f6516958e6278f52a0e4f51bfab24203206
Christopher Valarida <chris.valarida@adhocteam.us>
2018-09-05 14:19:37 -0700

    validations.ts

---
+++ src/applications/disability-benefits/all-claims/validations.ts
@@ -0,0 +1,7 @@
+import _ from '../../../platform/utilities/data';
+
+export const hasMilitaryRetiredPay = (data: {}): boolean => _.get('view:hasMilitaryRetiredPay', data, false);
+
+export const hasSeparationPay = (data: {}): boolean => _.get('view:hasSeparationPay', data, false);
+
+export const hasTrainingPay = (data: {}): boolean => _.get('view:hasTrainingPay', data, false);
