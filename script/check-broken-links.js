09b876f35a7681cdb31c1cbb4164154775daad0d
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-09-13 19:56:41 -0400

    Add "apply for edu benefits" to broken-link exceptions

--- script/check-broken-links.js
+++ script/check-broken-links.js
@@ -2,9 +2,17 @@
 const createBrokenLinkChecker = require('metalsmith-broken-link-checker');
 
 const pagesOutsideOfContent = [
+  // These pages live outside of the content directory but are local links, so
+  // they should be explicitly skipped.
   '/employment/job-seekers/skills-translator',
   '/employment/job-seekers/create-resume',
-  '/employment/employers'
+  '/employment/employers',
+
+  // At this time, we still have link-references to this page, which no longer exists.
+  // We should updated all reference instead to /education/apply/, but it's probably not
+  // worth it during the brand-consolidation effort where pages are already moving.
+  // This is a valid broken link though, so it should be addressed or removed soon.
+  '/education/apply-for-education-benefits'
 ];
 
 function checkBrokenLinks() {
