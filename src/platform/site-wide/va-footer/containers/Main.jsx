7374f9b64152b8dff60e9742f1537ba736891c15
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2018-09-12 18:09:50 -0700

    cleans up open veterans crisis modal

--- src/platform/site-wide/va-footer/containers/Main.jsx
+++ src/platform/site-wide/va-footer/containers/Main.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { groupBy, orderBy } from 'lodash'
-import links from '../../../static-data/footer-links.json'
+import groupBy from 'lodash/groupBy';
+import orderBy from 'lodash/orderBy';
+import links from '../../../static-data/footer-links.json';
 
 export class Main extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = { isMobile: false };
-    this._linkObj = groupBy(links, "column")
+    this.linkObj = groupBy(links, 'column');
   }
-
   componentWillMount() {
-
     function setSize(t) {
 
       window.addEventListener('resize', () => {
@@ -25,175 +22,198 @@ export class Main extends React.Component {
         isMobile: window.innerWidth < 767
       });
     }
-
     setSize(this);
-
   }
-  _generateLinkItems = (column, direction="desc") => (
+  openModal = () => {
+    this.setState({ modalOpen: true });
+  }
+  generateLinkItems = (column, direction = 'desc') => (
     <ul className="va-footer-links">
-      {orderBy(this._linkObj[column],"order", direction).map(link => 
-        <li><a href={link.href} target={link.target}>{link.title}</a></li>
+      {orderBy(this.linkObj[column], 'order', direction).map(link =>
+        <li key={`${link.column}-${link.order}`}><a href={link.href} target={link.target}>{link.title}</a></li>
       )}
     </ul>
   )
+  buildContact = () => {
+    let innerClassName = '';
+    let buttonEnabled = '';
+    let buttonClasses = '';
+    if (this.state.isMobile) {
+      innerClassName = 'usa-accordion-content';
+      buttonEnabled = '';
+      buttonClasses = 'usa-button-unstyled usa-accordion-button';
 
+    } else {
+      innerClassName = '';
+      buttonEnabled = 'disabled';
+      buttonClasses = 'va-footer-button';
+    }
+    if (this.state.isMobile) {
+      return (
+        <div>
+          <ul className="va-footer-linkgroup usa-width-one-fourth usa-accordion" id="veteran-crisis-line" aria-hidden="true">
+            <li>
+              <h4 className="va-footer-linkgroup-title">
+                <button className="usa-button-unstyled usa-accordion-button" aria-controls="veteran-crisis" itemProp="name" aria-expanded="false">In Crisis? Get Help Now</button>
+              </h4>
+            </li>
+            <li className="usa-accordion-content" id="veteran-crisis" aria-hidden="true">
+              <ul className="va-footer-links">
+                <li>
+                  <a onClick={this.openModal} >Veterans Crisis Line</a>
+                  <a >Veterans Crisis Line</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul className="va-footer-linkgroup usa-width-one-fourth usa-accordion" id="veteran-contact-us" aria-hidden="true">
+            <li id="footer-vcl">
+              <h4 className="va-footer-linkgroup-title">
+                <button disabled={buttonEnabled} className={buttonClasses} aria-controls="veteran-contact" itemProp="name" aria-expanded="false">Contact Us</button>
+              </h4>
+            </li>
+            <li className={innerClassName} id="veteran-contact" aria-hidden="true">
+              <ul className="va-footer-links">
+                <li><a href="#">Find a VA Location</a></li>
+                <li><a href="#">Submit a Help Request</a></li>
+                <li><a href="#">Go to VA Help Center</a></li>
+                <li><a href="#">Call Us</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+    return (
+      <ul className="va-footer-linkgroup usa-width-one-fourth" id="veteran-crisis" aria-hidden="true">
+        <li>
+          <h4 className="va-footer-linkgroup-title">
+              In Crisis? Get Help Now
+          </h4>
+        </li>
+        <li>
+          <a onClick={this.openModal} >Veterans Crisis Line</a>
+        </li>
+        <li id="footer-vcl">
+          <h4 className="va-footer-linkgroup-title">
+              Contact Us
+          </h4>
+        </li>
+        <li id="veteran-contact" aria-hidden="true">
+          <ul className="va-footer-links">
+            <li><a href="#">Find a VA Location</a></li>
+            <li><a href="#">Submit a Help Request</a></li>
+            <li><a href="#">Go to VA Help Center</a></li>
+            <li><a href="#">Call Us</a></li>
+          </ul>
+        </li>
+      </ul>
+    );
+  }
+  closeModal = () => {
+    this.setState({ modalOpen: false });
+  }
+  renderModal() {
+    return (
+      <div id="modal-crisisline" className="va-overlay va-modal va-modal-large va-overlay--open" role="alertdialog">
+        <div className="va-crisis-panel va-modal-inner">
+
+          <h3>Get help from Veterans Crisis Line</h3>
+
+          <button className="va-modal-close va-overlay-close" onClick={this.closeModal} type="button">
+            <i className="fa fa-close va-overlay-close"></i>
+            <span className="usa-sr-only va-overlay-close">Close this modal</span>
+          </button>
+          <div className="va-overlay-body va-crisis-panel-body">
+            <ul>
+              <li><a href="tel:18002738255">Call <strong>1-800-273-8255 (Press 1)</strong></a></li>
+              <li><a href="sms:838255">Text to <b>838255</b></a></li>
+              <li><a href="https://www.veteranscrisisline.net/ChatTermsOfService.aspx?account=Veterans%20Chat">Chat <b>confidentially now</b></a></li>
+            </ul>
+            <p>If you are in crisis or having thoughts of suicide,
+      visit <a href="https://www.veteranscrisisline.net/">VeteransCrisisLine.net</a> for more resources.</p>
+          </div>
+        </div>
+      </div>
+
+    );
+
+  }
   render() {
-
     // there is probably a better way to write this but I am going for fast, not elegant
+    // hacky hacky
+    const contactVCL = this.buildContact(this);
+
     let className = '';
     let innerClassName = '';
     let buttonEnabled = '';
     let buttonClasses = '';
-
-    function buildContact(t) {
-      if (t.state.isMobile) {
-        return (
-          <div>
-            <ul className="va-footer-linkgroup usa-width-one-fourth usa-accordion" id="veteran-crisis-line" aria-hidden="true">
-              <li>
-                <h4 className="va-footer-linkgroup-title">
-                  <button className="usa-button-unstyled usa-accordion-button" aria-controls="veteran-crisis" itemProp="name" aria-expanded="false">In Crisis? Get Help Now</button>
-                </h4>
-              </li>
-              <li className="usa-accordion-content" id="veteran-crisis" aria-hidden="true">
-                <ul className="va-footer-links">
-                  <li>
-                    <a href="http://www.veteranscrisisline.net/">Veterans Crisis Line</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            <ul className="va-footer-linkgroup usa-width-one-fourth usa-accordion" id="veteran-contact-us" aria-hidden="true">
-              <li id="footer-vcl">
-                <h4 className="va-footer-linkgroup-title">
-                  <button disabled={buttonEnabled} className={buttonClasses} aria-controls="veteran-contact" itemProp="name" aria-expanded="false">Contact Us</button>
-                </h4>
-              </li>
-              <li className={innerClassName} id="veteran-contact" aria-hidden="true">
-                <ul className="va-footer-links">
-                  <li><a href="#">Find a VA Location</a></li>
-                  <li><a href="#">Submit a Help Request</a></li>
-                  <li><a href="#">Go to VA Help Center</a></li>
-                  <li><a href="#">Call Us</a></li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        );
-      }
-      return (
-        <ul className="va-footer-linkgroup usa-width-one-fourth" id="veteran-crisis" aria-hidden="true">
-          <li>
-            <h4 className="va-footer-linkgroup-title">
-              In Crisis? Get Help Now
-            </h4>
-          </li>
-          <li>
-            <a href="http://www.veteranscrisisline.net/">Veterans Crisis Line</a>
-          </li>
-          <li id="footer-vcl">
-            <h4 className="va-footer-linkgroup-title">
-              Contact Us
-            </h4>
-          </li>
-          <li id="veteran-contact" aria-hidden="true">
-            <ul className="va-footer-links">
-              <li><a href="#">Find a VA Location</a></li>
-              <li><a href="#">Submit a Help Request</a></li>
-              <li><a href="#">Go to VA Help Center</a></li>
-              <li><a href="#">Call Us</a></li>
-            </ul>
-          </li>
-        </ul>
-      );
-
-    }
-
     if (this.state.isMobile) {
-
       className = 'va-footer-linkgroup usa-width-one-fourth usa-accordion';
       innerClassName = 'usa-accordion-content';
       buttonEnabled = '';
       buttonClasses = 'usa-button-unstyled usa-accordion-button';
 
     } else {
-
       className = 'va-footer-linkgroup usa-width-one-fourth';
       innerClassName = '';
       buttonEnabled = 'disabled';
       buttonClasses = 'va-footer-button';
-
     }
-
-    // hacky hacky
-    const contactVCL = buildContact(this);
-
     return (
-      <div className="footer-inner">
+      <div>
+        {this.state.modalOpen && this.renderModal()}
+        <div className="footer-inner">
 
-        <div className="usa-grid">
+          <div className="usa-grid">
+            <ul className={className} id="footer-first-child">
+              <li>
+                <h4 className="va-footer-linkgroup-title">
+                  <button disabled={buttonEnabled} className={buttonClasses} aria-controls="veteran-programs" itemProp="name" aria-expanded="false">Veteran Programs and Services</button>
+                </h4>
+              </li>
+              <li className={innerClassName} id="veteran-programs" aria-hidden="true">
+                {this.generateLinkItems('1')}
+              </li>
+            </ul>
+            <ul className={className} id="footer-services">
+              <li>
+                <h4 className="va-footer-linkgroup-title">
+                  <button disabled={buttonEnabled} className={buttonClasses} aria-controls="veteran-resources" itemProp="name" aria-expanded="false">More VA Resources</button>
+                </h4>
+              </li>
+              <li className={innerClassName} id="veteran-resources" aria-hidden="true">
+                {this.generateLinkItems('2')}
+              </li>
+            </ul>
+            <ul className={className} id="footer-popular">
+              <li>
+                <h4 className="va-footer-linkgroup-title">
+                  <button disabled={buttonEnabled} className={buttonClasses} aria-controls="veteran-connect" itemProp="name" aria-expanded="false">Connect with Us</button>
+                </h4>
+              </li>
 
-          <ul className={className} id="footer-first-child">
-            <li>
-              <h4 className="va-footer-linkgroup-title">
-                <button disabled={buttonEnabled} className={buttonClasses} aria-controls="veteran-programs" itemProp="name" aria-expanded="false">Veteran Programs and Services</button>
-              </h4>
-            </li>
-            <li className={innerClassName} id="veteran-programs" aria-hidden="true">
-              {this._generateLinkItems("1")}
-            </li>
-          </ul>
-
-          <ul className={className} id="footer-services">
-
-            <li>
-              <h4 className="va-footer-linkgroup-title">
-                <button disabled={buttonEnabled} className={buttonClasses} aria-controls="veteran-resources" itemProp="name" aria-expanded="false">More VA Resources</button>
-              </h4>
-            </li>
-
-            <li className={innerClassName} id="veteran-resources" aria-hidden="true">
-              {this._generateLinkItems("2")}
-            </li>
-
-          </ul>
-
-          <ul className={className} id="footer-popular">
-            <li>
-              <h4 className="va-footer-linkgroup-title">
-                <button disabled={buttonEnabled} className={buttonClasses} aria-controls="veteran-connect" itemProp="name" aria-expanded="false">Connect with Us</button>
-              </h4>
-            </li>
-
-            <li className={innerClassName} id="veteran-connect" aria-hidden="true">
-              {this._generateLinkItems("3")}
-            </li>
-          </ul>
-
-          {contactVCL}
-
-        </div>
-
-        <div className="usa-grid footer-banner">
-          <a href="https://preview.va.gov" className="va-footer-logo" title="Go to VA.gov">
-            <img src="/img/homepage/va-logo-white.png" alt="VA logo"/>
-          </a>
-        </div>
-
-        <div className="usa-grid footer-links">
-          <ul>
-            {orderBy(this._linkObj["bottom_rail"],"order", 'desc').map(link => {
-              return(<li><a href={link.href} target={link.target}>{link.title}</a> | </li>)
-            })}
-          </ul>
+              <li className={innerClassName} id="veteran-connect" aria-hidden="true">
+                {this.generateLinkItems('3')}
+              </li>
+            </ul>
+            {contactVCL}
+          </div>
+          <div className="usa-grid footer-banner">
+            <a href="https://preview.va.gov" className="va-footer-logo" title="Go to VA.gov">
+              <img src="/img/homepage/va-logo-white.png" alt="VA logo"/>
+            </a>
+          </div>
+          <div className="usa-grid footer-links">
+            <ul>
+              {orderBy(this.linkObj.bottom_rail, 'order', 'desc').map(link => {
+                return (<li key={`${link.order}`}><a href={link.href} target={link.target}>{link.title}</a>{' | '}</li>);
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     );
   }
 }
-
-const mapDispatchToProps = () => {
-  return {};
-};
-
-export default connect(mapDispatchToProps)(Main);
+export default Main;
