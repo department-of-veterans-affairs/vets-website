75cd723d4437d114651b494459bf2da375167b27
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-09-18 13:32:38 -0400

    Update HCA manifest

--- src/applications/hca/hca-entry.jsx
+++ src/applications/hca/hca-entry.jsx
@@ -5,7 +5,7 @@ import startApp from '../../platform/startup';
 
 import routes from './routes';
 import reducer from './reducer';
-import manifest from './manifest.json';
+import manifest from './manifest';
 
 let rootUrl = manifest.rootUrl;
 if (window.location.pathname.indexOf('healthcare/') >= 0) {
