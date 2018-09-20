c0bad7b233b68058d39430a17c8c24bcca10895f
Mark Greenburg <marek.greenburg@gmail.com>
2018-09-20 14:02:01 -0500

    Update content styling

--- src/applications/disability-benefits/all-claims/content/claimExamsInfo.jsx
+++ src/applications/disability-benefits/all-claims/content/claimExamsInfo.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import CollapsiblePanel from '@department-of-veterans-affairs/formation/CollapsiblePanel';
 
 export const claimExamsDescription = (
-  <p>
-    After we review your disability claim and supporting evidence, we may ask
-    you to have a claim exam (also known as a C&P exam) if we need more
-    information to decide your claim. We’ll use the results of this exam, in
-    addition to your evidence, to rate your disability and make a decision on
-    your claim.
-  </p>
+  <div>
+    <p>
+      After we review your disability claim and supporting evidence, we may ask
+      you to have a claim exam (also known as a C&P exam) if we need more
+      information to decide your claim. We’ll use the results of this exam, in
+      addition to your evidence, to rate your disability and make a decision on
+      your claim.
+    </p>
+    <h4>
+      Does everyone who files a disability claim need to have a claim exam?
+    </h4>
+    <p>
+      No. We’ll ask you to have a claim exam only if we need more information
+      to decide your claim. If you’ve provided enough supporting evidence for
+      your claimed disability, we won’t ask you to have a claim exam.
+    </p>
+  </div>
 );
 
 export const claimExamsFAQ = (
   <div>
-    <CollapsiblePanel
-      panelName="Does everyone who files a disability claim need to have a claim exam?"
-      startOpen>
-      <p>
-        No. We’ll ask you to have a claim exam only if we need more information
-        to decide your claim. If you’ve provided enough supporting evidence for
-        your claimed disability, we won’t ask you to have a claim exam.
-      </p>
-    </CollapsiblePanel>
     <CollapsiblePanel panelName="How will you contact me if I need to have an exam?">
       <p>
         The staff at your local VA medical center will contact you to schedule
