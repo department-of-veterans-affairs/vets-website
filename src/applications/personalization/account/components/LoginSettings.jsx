6fe235a1ddbee36910c0f571dd368fdfe575218e
Joe <jstanek@thesocompany.com>
2018-09-24 17:36:33 -0400

    ID.me description update

--- src/applications/personalization/account/components/LoginSettings.jsx
+++ src/applications/personalization/account/components/LoginSettings.jsx
@@ -9,7 +9,7 @@ export default function LoginSettings() {
         <a href="https://wallet.id.me/settings" target="_blank">Go to ID.me to manage your account</a>
       </p>
       <p>
-        <strong>Note:</strong> ID.me is a digital identity platform that provides the strongest identity verification system available to prevent fraud and identity theft. We use ID.me to help you create a verified account on Vets.gov-or to connect your premium MyHealtheVet or DS Logon account to our site-as well as to add an extra layer of security to your account.<br/>
+        <strong>Note:</strong> ID.me is a digital identity platform that provides the strongest identity verification system available to prevent fraud and identity theft. You can create, secure, and verify your VA.gov account through ID.me. If you already have a premium MyHealtheVet or premium DS Logon account, you can use ID.me to connect to VA.gov.<br/>
         <a href="/faq/#what-is-idme" onClick={() => recordEvent({ event: 'account-navigation', 'account-action': 'view-link', 'account-section': 'learn-more-idme' })}>Learn more about ID.me</a>
       </p>
     </div>
