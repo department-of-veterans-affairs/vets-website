9c808793aad3877aa832a9ea05a4ac4e16c6f08f
Mark Greenburg <marek.greenburg@gmail.com>
2018-09-24 16:49:24 -0500

    Content updates

--- src/applications/disability-benefits/all-claims/content/evidenceTypes.jsx
+++ src/applications/disability-benefits/all-claims/content/evidenceTypes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AdditionalInfo from '@department-of-veterans-affairs/formation/AdditionalInfo';
+import AlertBox from '@department-of-veterans-affairs/formation/AlertBox';
 
 export const evidenceTypeHelp = (
   <AdditionalInfo triggerText="Which evidence type should I choose?">
@@ -13,7 +14,7 @@ export const evidenceTypeHelp = (
     <p>
       If you were treated by a private doctor, including a Veteran’s Choice
       doctor, you’ll have private medical records. We’ll need to see those
-      records to make a decision on your claim. A Disability Benefit
+      records to make a decision on your claim. A Disability Benefits
       Questionnaire is an example of a private medical record.
     </p>
     <h4>Lay statements or other evidence</h4>
@@ -21,22 +22,29 @@ export const evidenceTypeHelp = (
       A lay statement is a written statement from family, friends, or coworkers
       to help support your claim. Lay statements are also called “buddy
       statements.” In most cases, you’ll only need your medical records to
-      support your disability claim. Some claims, for example, for Posttraumatic
-      Stress Disorder or for military sexual trauma, could benefit from a lay or
+      support your disability claim. Some claims—such as those for Posttraumatic
+      Stress Disorder or military sexual trauma—could benefit from a lay or
       buddy statement.
     </p>
   </AdditionalInfo>
 );
 
 export const noEvidenceDescription = (
-  <p>
-    <strong>Please note,</strong> while you don’t have to submit evidence for
-    your claim, we recommend that you do provide supporting information that
-    relates to your claimed disability. If you don’t provide any supporting
-    information, we may schedule a claim exam for you to help us decide your
-    claim. You have up to 1 year from the date we receive your claim to turn in
-    any evidence. If you don’t have supporting evidence right now, you can save
-    your application and return to it later when you have your evidence ready to
-    upload.
-  </p>
+  <AlertBox status="info" isVisible>
+    <div>
+      <p>
+        <strong>Please note:</strong> You don’t have to submit evidence for
+        your claim, but we recommend that you do provide some supporting
+        information related to your claimed disability. If you don’t submit any
+        evidence, we may schedule a claim exam for you to help us decide your
+        claim.
+      </p>
+      <p>
+        You have up to 1 year from the date we receive your claim to turn in any
+        evidence. If you don’t have supporting evidence right now, you can save
+        your application and return to it later when your evidence is ready to
+        upload.
+      </p>
+    </div>
+  </AlertBox>
 );
