7cee8f59f4d5f0776b1f9d66e298bc77d65460c7
Rian Fowler <rian.fowler@adhocteam.us>
2018-09-24 11:45:51 -0600

    add new links and logged in conditional

--- src/platform/site-wide/mega-menu/containers/Main.jsx
+++ src/platform/site-wide/mega-menu/containers/Main.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import data from '../data.json';
+import defaultLinkData from '../mega-menu-link-data.json';
+import authenticatedUserLinkData from '../mega-menu-link-data-for-authenticated-users.json';
 import { togglePanelOpen, toggleMobileDisplayHidden } from '../actions';
+import { isLoggedIn } from '../../../user/selectors';
 
 import MegaMenu from '@department-of-veterans-affairs/formation/MegaMenu';
 
 // const SESSION_REFRESH_INTERVAL_MINUTES = 45;
+//               {window.location.pathname.endsWith('eligibility/') && <p><a href={applyLink}>Learn more about how to apply</a>.</p>}
+
 
 export class Main extends React.Component {
   render() {
     return (
-      <MegaMenu {...this.props} data={data}></MegaMenu>
+      <MegaMenu {...this.props}></MegaMenu>
     );
   }
 }
 
-const mapStateToProps = ({ megaMenu }) => ({ ...megaMenu });
+const mapStateToProps = (state) => {
+  const data = [
+    ...defaultLinkData,
+    ...isLoggedIn(state) ? authenticatedUserLinkData : []
+  ];
+
+  return {
+    ...state.megaMenu,
+    data
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
