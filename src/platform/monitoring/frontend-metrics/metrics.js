cd522a099bfd8f57fdb63be49ca15d162e2546bd
Keifer Furzland <kfrz.code@gmail.com>
2018-09-26 14:16:03 -0500

    Build metrics payload to send to backend

--- src/platform/monitoring/frontend-metrics/metrics.js
+++ src/platform/monitoring/frontend-metrics/metrics.js
@@ -8,12 +8,68 @@
 import Raven from 'raven-js';
 
 /**
+ * Returns false if Paint timing is not supported or the time of first-contentful-paint if present.
+ * @returns (number) The number retrieved from the paintEntries.startTime value, else false
+ */
+function contentfulPaintEntry() {
+  const paintEntries = performance.getEntriesByName('first-contentful-paint');
+  if (typeof paintEntries === 'undefined') {
+    return false;
+  }
+  return paintEntries[0].startTime;
+}
+
+/**
+ * Returns a formatted metrics payload FormData object to send to backend
+ * @param (PerformanceNavigationTiming) An object with information from the browser's Performance API
+ * @returns (FormData) A new FormData object with an array of metrics and page_id parameters
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/PerformanceEntry
+ */
+function buildMetricsPayload(entry) {
+  const totalPageLoad = entry.duration;
+  const firstByte = entry.responseStart;
+  const domProcessing = entry.domComplete - entry.domInteractive;
+  const domComplete = entry.domComplete - entry.requestStart;
+  const domInteractive = entry.domInteractive;
+
+  let metrics = {
+    totalPageLoad,
+    firstByte,
+    domProcessing,
+    domComplete,
+    domInteractive,
+  };
+
+  if (contentfulPaintEntry()) {
+    const firstContentfulPaint = contentfulPaintEntry();
+    metrics = Object.assign({
+      firstContentfulPaint
+    }, metrics);
+  }
+  const metricsPayload = new FormData();
+  const metricsArray = [];
+  const pageUrl = window.location.pathname;
+
+  Object.keys(metrics).forEach(metric => {
+    metricsArray.push({
+      metric,
+      duration: metrics[metric]
+    });
+  });
+  const data = JSON.stringify({ metrics: metricsArray, pageId: pageUrl});
+
+  metricsPayload.append('data', data);
+  return metricsPayload;
+}
+
+/**
  * Capture metrics with a PerformanceObserver object
  * @see https://developer.mozilla.org/en-US/docs/Web/API/PerformanceObserver/observe
  */
 function captureMetrics() {
   const observer = new PerformanceObserver(list => {
     list.getEntriesByType('navigation').forEach(entry => {
+      const metricsPayload = buildMetricsPayload(entry);
       window.addEventListener('unload', () => {
         // TODO: Send method to backend outlined in https://github.com/department-of-veterans-affairs/vets.gov-team/issues/13355
       });
