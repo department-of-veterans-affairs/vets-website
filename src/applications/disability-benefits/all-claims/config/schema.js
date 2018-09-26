6df75870cdddea53dae6bf91445bd6da4b257c6f
Charles Stone <charles@zstones.com>
2018-09-26 14:00:52 -0700

    Change treatmentDateRange to Array and used centralAddress

--- src/applications/disability-benefits/all-claims/config/schema.js
+++ src/applications/disability-benefits/all-claims/config/schema.js
@@ -10,11 +10,45 @@ const serviceBranches = [
   'Public Health Service'
 ];
 
+const suffixes = [
+  'Jr.',
+  'Sr.',
+  'II',
+  'III',
+  'IV'
+];
+
 const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'APPLICATION FOR DISABILITY BENEFITS',
   type: 'object',
   definitions: {
+    fullName: {
+      type: 'object',
+      properties: {
+        first: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 30
+        },
+        middle: {
+          type: 'string'
+        },
+        last: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 30
+        },
+        suffix: {
+          type: 'string',
+          'enum': suffixes
+        },
+      },
+      required: [
+        'first',
+        'last'
+      ]
+    },
     phone: {
       type: 'string',
       pattern: '^\\d{10}$'
@@ -28,6 +62,21 @@ const schema = {
       minLength: 6,
       maxLength: 80,
       pattern: '^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$'
+    },
+    privacyAgreementAccepted: {
+      type: 'boolean',
+      'enum': [true]
+    },
+    usaPostalCode: {
+      type: 'string',
+      anyOf: [
+        {
+          pattern: '^\\d{5}$'
+        },
+        {
+          pattern: '^\\d{5}-\\d{4}$'
+        }
+      ]
     },
     address: {
       type: 'object',
@@ -799,6 +848,399 @@ const schema = {
         }
       }
     },
+    centralMailAddress: {
+      type: 'object',
+      required: [
+        'country',
+        'city',
+        'addressLine1'
+      ],
+      properties: {
+        country: {
+          type: 'string',
+          'enum': [
+            'Afghanistan',
+            'Albania',
+            'Algeria',
+            'Angola',
+            'Anguilla',
+            'Antigua',
+            'Antigua and Barbuda',
+            'Argentina',
+            'Armenia',
+            'Australia',
+            'Austria',
+            'Azerbaijan',
+            'Azores',
+            'Bahamas',
+            'Bahrain',
+            'Bangladesh',
+            'Barbados',
+            'Barbuda',
+            'Belarus',
+            'Belgium',
+            'Belize',
+            'Benin',
+            'Bermuda',
+            'Bhutan',
+            'Bolivia',
+            'Bosnia-Herzegovina',
+            'Botswana',
+            'Brazil',
+            'Brunei',
+            'Bulgaria',
+            'Burkina Faso',
+            'Burma',
+            'Burundi',
+            'Cambodia',
+            'Cameroon',
+            'Canada',
+            'Cape Verde', 'Cayman Islands',
+            'Central African Republic',
+            'Chad',
+            'Chile',
+            'China',
+            'Colombia',
+            'Comoros',
+            'Congo, Democratic Republic of',
+            'Congo, People\'s Republic of',
+            'Costa Rica',
+            'Cote d\'Ivoire',
+            'Croatia',
+            'Cuba',
+            'Cyprus',
+            'Czech Republic',
+            'Denmark',
+            'Djibouti',
+            'Dominica',
+            'Dominican Republic',
+            'Ecuador',
+            'Egypt',
+            'El Salvador',
+            'England',
+            'Equatorial Guinea',
+            'Eritrea',
+            'Estonia',
+            'Ethiopia',
+            'Fiji',
+            'Finland',
+            'France',
+            'French Guiana',
+            'Gabon',
+            'Gambia',
+            'Georgia',
+            'Germany',
+            'Ghana',
+            'Gibraltar',
+            'Great Britain',
+            'Great Britain and Gibraltar',
+            'Greece',
+            'Greenland',
+            'Grenada',
+            'Guadeloupe',
+            'Guatemala',
+            'Guinea',
+            'Guinea, Republic of Guinea',
+            'Guinea-Bissau',
+            'Guyana',
+            'Haiti',
+            'Honduras',
+            'Hong Kong',
+            'Hungary',
+            'Iceland',
+            'India',
+            'Indonesia',
+            'Iran',
+            'Iraq',
+            'Ireland',
+            'Israel (Jerusalem)',
+            'Israel (Tel Aviv)',
+            'Italy',
+            'Jamaica',
+            'Japan',
+            'Jordan',
+            'Kazakhstan',
+            'Kenya',
+            'Kosovo',
+            'Kuwait',
+            'Kyrgyzstan',
+            'Laos',
+            'Latvia',
+            'Lebanon',
+            'Leeward Islands',
+            'Lesotho',
+            'Liberia',
+            'Libya',
+            'Liechtenstein',
+            'Lithuania',
+            'Luxembourg',
+            'Macao',
+            'Macedonia',
+            'Madagascar',
+            'Malawi',
+            'Malaysia',
+            'Mali',
+            'Malta',
+            'Martinique',
+            'Mauritania',
+            'Mauritius',
+            'Mexico',
+            'Moldavia',
+            'Mongolia',
+            'Montenegro',
+            'Montserrat',
+            'Morocco',
+            'Mozambique',
+            'Namibia',
+            'Nepal',
+            'Netherlands',
+            'Netherlands Antilles',
+            'Nevis',
+            'New Caledonia',
+            'New Zealand',
+            'Nicaragua',
+            'Niger',
+            'Nigeria',
+            'North Korea',
+            'Northern Ireland',
+            'Norway',
+            'Oman',
+            'Pakistan',
+            'Panama',
+            'Papua New Guinea',
+            'Paraguay',
+            'Peru',
+            'Philippines',
+            'Philippines (restricted payments)',
+            'Poland',
+            'Portugal',
+            'Qatar',
+            'Republic of Yemen',
+            'Romania',
+            'Russia',
+            'Rwanda',
+            'Sao-Tome/Principe',
+            'Saudi Arabia',
+            'Scotland',
+            'Senegal',
+            'Serbia',
+            'Serbia/Montenegro',
+            'Seychelles',
+            'Sicily',
+            'Sierra Leone',
+            'Singapore',
+            'Slovakia',
+            'Slovenia',
+            'Somalia',
+            'South Africa',
+            'South Korea',
+            'Spain',
+            'Sri Lanka',
+            'St. Kitts',
+            'St. Lucia',
+            'St. Vincent',
+            'Sudan',
+            'Suriname',
+            'Swaziland',
+            'Sweden',
+            'Switzerland',
+            'Syria',
+            'Taiwan',
+            'Tajikistan',
+            'Tanzania',
+            'Thailand',
+            'Togo',
+            'Trinidad and Tobago',
+            'Tunisia',
+            'Turkey (Adana only)',
+            'Turkey (except Adana)',
+            'Turkmenistan',
+            'USA',
+            'Uganda',
+            'Ukraine',
+            'United Arab Emirates',
+            'United Kingdom',
+            'Uruguay',
+            'Uzbekistan',
+            'Vanuatu',
+            'Venezuela',
+            'Vietnam',
+            'Wales',
+            'Western Samoa',
+            'Yemen Arab Republic',
+            'Zambia',
+            'Zimbabwe'
+          ],
+          'default': 'USA'
+        },
+        addressLine1: {
+          type: 'string',
+          maxLength: 20,
+          pattern: "^([-a-zA-Z0-9'.,&#]([-a-zA-Z0-9'.,&# ])?)+$"
+        },
+        addressLine2: {
+          type: 'string',
+          maxLength: 20,
+          pattern: "^([-a-zA-Z0-9'.,&#]([-a-zA-Z0-9'.,&# ])?)+$"
+        },
+        addressLine3: {
+          type: 'string',
+          maxLength: 20,
+          pattern: "^([-a-zA-Z0-9'.,&#]([-a-zA-Z0-9'.,&# ])?)+$"
+        },
+        city: {
+          type: 'string',
+          maxLength: 30,
+          pattern: "^([-a-zA-Z0-9'.#]([-a-zA-Z0-9'.# ])?)+$"
+        },
+        state: {
+          type: 'string',
+          'enum': [
+            'AL',
+            'UM',
+            'AS',
+            'AZ',
+            'AR',
+            'AA',
+            'AE',
+            'AP',
+            'CA',
+            'CO',
+            'CT',
+            'DE',
+            'DC',
+            'FM',
+            'FL',
+            'GA',
+            'GU',
+            'HI',
+            'ID',
+            'IL',
+            'IN',
+            'IA',
+            'KS',
+            'KY',
+            'LA',
+            'ME',
+            'MH',
+            'MD',
+            'MA',
+            'MI',
+            'MN',
+            'MS',
+            'AK',
+            'MT',
+            'NE',
+            'NV',
+            'NH',
+            'NJ',
+            'NM',
+            'NY',
+            'NC',
+            'ND',
+            'MP',
+            'OH',
+            'OK',
+            'OR',
+            'PW',
+            'PA',
+            'PR',
+            'RI',
+            'SC',
+            'SD',
+            'TN',
+            'TX',
+            'UT',
+            'VT',
+            'VI',
+            'VA',
+            'WA',
+            'WV',
+            'WI',
+            'WY',
+            'PI',
+            'MO'
+          ],
+          enumNames: [
+            'Alabama',
+            'U.S. Minor Outlying Islands',
+            'American Samoa',
+            'Arizona',
+            'Arkansas',
+            'Armed Forces Americas (AA)',
+            'Armed Forces Europe (AE)',
+            'Armed Forces Pacific (AP)',
+            'California',
+            'Colorado',
+            'Connecticut',
+            'Delaware',
+            'District Of Columbia',
+            'Federated States Of Micronesia',
+            'Florida',
+            'Georgia',
+            'Guam',
+            'Hawaii',
+            'Idaho',
+            'Illinois',
+            'Indiana',
+            'Iowa',
+            'Kansas',
+            'Kentucky',
+            'Louisiana',
+            'Maine',
+            'Marshall Islands',
+            'Maryland',
+            'Massachusetts',
+            'Michigan',
+            'Minnesota',
+            'Mississippi',
+            'Alaska',
+            'Montana',
+            'Nebraska',
+            'Nevada',
+            'New Hampshire',
+            'New Jersey',
+            'New Mexico',
+            'New York',
+            'North Carolina',
+            'North Dakota',
+            'Northern Mariana Islands',
+            'Ohio',
+            'Oklahoma',
+            'Oregon',
+            'Palau',
+            'Pennsylvania',
+            'Puerto Rico',
+            'Rhode Island',
+            'South Carolina',
+            'South Dakota',
+            'Tennessee',
+            'Texas',
+            'Utah',
+            'Vermont',
+            'Virgin Islands',
+            'Virginia',
+            'Washington',
+            'West Virginia',
+            'Wisconsin',
+            'Wyoming',
+            'Philippine Islands',
+            'Missouri'
+          ]
+        },
+        zipCode: {
+          type: 'string',
+          anyOf: [
+            {
+              pattern: '^\\d{5}$'
+            },
+            {
+              pattern: '^\\d{5}-\\d{4}$'
+            }
+          ]
+        }
+      }
+    },
     dateRangeAllRequired: {
       type: 'object',
       properties: {
@@ -813,104 +1255,6 @@ const schema = {
         'from',
         'to'
       ]
-    },
-    form4142: {
-      type: 'object',
-      properties: {
-        limitedConsent: {
-          type: 'string'
-        },
-        providerFacility: {
-          type: 'array',
-          required: ['providerFacilityName', 'treatmentDateRange', 'providerFacilityAddress'],
-          items: {
-            type: 'object',
-            properties: {
-              providerFacilityName: {
-                type: 'string'
-              },
-              treatmentDateRange: {
-                $ref: '#/definitions/dateRange'
-              },
-              providerFacilityAddress: {
-                $ref: '#/definitions/address'
-              }
-            }
-          }
-        },
-        privacyAgreementAccepted: {
-          $ref: '#/definitions/privacyAgreementAccepted'
-        }
-      }
-    },
-    form0781: {
-      type: 'object',
-      incident: {
-        type: 'array',
-        items: {
-          type: 'object',
-          properties: {
-            personalAssault: {
-              type: 'boolean'
-            },
-            medalsCitations: {
-              type: 'string'
-            },
-            incidentDate: {
-              $ref: '#/definitions/date'
-            },
-            incidentLocation: {
-              type: 'string'
-            },
-            incidentDescription: {
-              type: 'string'
-            },
-            unitAssigned: {
-              type: 'string'
-            },
-            unitAssignedDates: {
-              $ref: '#/definitions/dateRange'
-            },
-            remarks: {
-              type: 'string'
-            },
-            personInvolved: {
-              type: 'array',
-              items: {
-                type: 'object',
-                name: {
-                  $ref: '#/definitions/fullName'
-                },
-                rank: {
-                  type: 'string'
-                },
-                injuryDeath: {
-                  type: 'string',
-                  'enum': ['Killed in Action', 'Killed Non-Battle', 'Wounded in Action', 'Injured Non-Battle', 'Other']
-                },
-                injuryDeathDate: {
-                  $ref: '#/definitions/date'
-                },
-                unitAssigned: {
-                  type: 'string'
-                }
-              }
-            },
-            source: {
-              type: 'array',
-              items: {
-                type: 'object',
-                name: {
-                  $ref: '#/definitions/fullName'
-                },
-                address: {
-                  $ref: '#/definitions/address'
-                }
-              }
-            }
-          }
-        }
-      }
     }
   },
   properties: {
@@ -1189,6 +1533,112 @@ const schema = {
         },
         phoneNumber: {
           $ref: '#/definitions/phone'
+        }
+      }
+    },
+    form4142: {
+      type: 'object',
+      properties: {
+        limitedConsent: {
+          type: 'string'
+        },
+        providerFacility: {
+          type: 'array',
+          required: ['providerFacilityName', 'treatmentDateRange', 'providerFacilityAddress'],
+          items: {
+            type: 'object',
+            properties: {
+              providerFacilityName: {
+                type: 'string'
+              },
+              treatmentDateRange: {
+                type: 'array',
+                properties: {
+                  from: {
+                    $ref: '#/definitions/date'
+                  },
+                  to: {
+                    $ref: '#/definitions/date'
+                  }
+                }
+              },
+              providerFacilityAddress: {
+                $ref: '#/definitions/centralMailAddress'
+              }
+            }
+          }
+        },
+        privacyAgreementAccepted: {
+          $ref: '#/definitions/privacyAgreementAccepted'
+        }
+      }
+    },
+    form0781: {
+      type: 'object',
+      incident: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            personalAssault: {
+              type: 'boolean'
+            },
+            medalsCitations: {
+              type: 'string'
+            },
+            incidentDate: {
+              $ref: '#/definitions/date'
+            },
+            incidentLocation: {
+              type: 'string'
+            },
+            incidentDescription: {
+              type: 'string'
+            },
+            unitAssigned: {
+              type: 'string'
+            },
+            unitAssignedDates: {
+              $ref: '#/definitions/dateRange'
+            },
+            remarks: {
+              type: 'string'
+            },
+            personInvolved: {
+              type: 'array',
+              items: {
+                type: 'object',
+                name: {
+                  $ref: '#/definitions/fullName'
+                },
+                rank: {
+                  type: 'string'
+                },
+                injuryDeath: {
+                  type: 'string',
+                  'enum': ['Killed in Action', 'Killed Non-Battle', 'Wounded in Action', 'Injured Non-Battle', 'Other']
+                },
+                injuryDeathDate: {
+                  $ref: '#/definitions/date'
+                },
+                unitAssigned: {
+                  type: 'string'
+                }
+              }
+            },
+            source: {
+              type: 'array',
+              items: {
+                type: 'object',
+                name: {
+                  $ref: '#/definitions/fullName'
+                },
+                address: {
+                  $ref: '#/definitions/address'
+                }
+              }
+            }
+          }
         }
       }
     }
