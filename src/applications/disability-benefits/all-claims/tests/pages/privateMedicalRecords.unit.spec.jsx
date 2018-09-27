db0a497523b7c7dd9e866e656ca71e6d805c0a1a
Mark Greenburg <marek.greenburg@gmail.com>
2018-09-27 14:28:06 -0500

    lint

--- src/applications/disability-benefits/all-claims/tests/pages/privateMedicalRecords.unit.spec.jsx
+++ src/applications/disability-benefits/all-claims/tests/pages/privateMedicalRecords.unit.spec.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { mount } from 'enzyme';
-
 import { DefinitionTester } from '../../../../../platform/testing/unit/schemaform-utils.jsx';
 import formConfig from '../../config/form.js';
-import initialData from '../initialData.js';
-
 
 describe('526 All Claims Private medical records', () => {
   const errorClass = '.usa-input-error-message';
