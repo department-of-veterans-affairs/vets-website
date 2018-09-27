8b3b45ec901b3e4d73f2e453ebca503ed3a14525
Joe <jstanek@thesocompany.com>
2018-09-27 16:32:16 -0400

    Content changes from Vets.gov to VA.gov

--- src/applications/rx/containers/History.jsx
+++ src/applications/rx/containers/History.jsx
@@ -103,7 +103,7 @@ class History extends React.Component {
       return (
         <p className="rx-tab-explainer rx-loading-error">
           We couldn’t retrieve your prescriptions.
-          Please refresh this page or try again later. If this problem persists, please call the Vets.gov Help Desk
+          Please refresh this page or try again later. If this problem persists, please call the VA.gov Help Desk
           at <a href="tel:8555747286">1-855-574-7286</a>, TTY: <a href="tel:18008778339">1-800-877-8339</a>, Monday &#8211; Friday, 8:00 a.m. &#8211; 8:00 p.m. (ET).
         </p>
       );
@@ -111,7 +111,7 @@ class History extends React.Component {
 
     if (!items.length) {
       const content = (
-        <p>It looks like you don’t have any active VA prescriptions. If you’re taking a medicine that you don’t see listed here—or if you have any questions about your current medicines—please contact your VA health care team. If you need more help, please call the Vets.gov Help Desk at <a href="tel:8555747286">1-855-574-7286</a> (TTY: <a href="tel:18008778339">1-800-877-8339</a>). We’re here Monday–Friday, 8:00 a.m.–8:00 p.m. (ET). We’re here Monday–Friday, 8:00 a.m.–8:00 p.m. (ET).</p>
+        <p>It looks like you don’t have any active VA prescriptions. If you’re taking a medicine that you don’t see listed here—or if you have any questions about your current medicines—please contact your VA health care team. If you need more help, please call the VA.gov Help Desk at <a href="tel:8555747286">1-855-574-7286</a> (TTY: <a href="tel:18008778339">1-800-877-8339</a>). We’re here Monday–Friday, 8:00 a.m.–8:00 p.m. (ET). We’re here Monday–Friday, 8:00 a.m.–8:00 p.m. (ET).</p>
       );
 
       return (
