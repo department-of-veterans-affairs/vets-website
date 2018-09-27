4441f8939de17b07924b32d59e90f1de920886dc
Keifer Furzland <kfrz.code@gmail.com>
2018-09-27 11:46:55 -0500

    Add comment with matched whitelist in API

--- src/platform/monitoring/frontend-metrics/whitelisted-paths.js
+++ src/platform/monitoring/frontend-metrics/whitelisted-paths.js
@@ -1,3 +1,10 @@
+/**
+ * This array of paths must remain identical to its sibling whitelist maintained
+ * in the department-of-veterans-affairs/vets-api repository at:
+ * https://github.com/department-of-veterans-affairs/vets-api/blob/master/lib/benchmark/whitelist.rb
+ * Any changes made must be made to both.
+ *
+ */
 export const whitelistedPaths = [
   '/',
   '/disability',
