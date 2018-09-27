f01e66404f863fa971e2dd7fd8696c0688bf3688
Joe <jstanek@thesocompany.com>
2018-09-27 16:08:55 -0400

    Content changes from Vets.gov to VA.gov

--- src/applications/veteran-id-card/components/RequiredVeteranView.jsx
+++ src/applications/veteran-id-card/components/RequiredVeteranView.jsx
@@ -9,7 +9,7 @@ class RequiredVeteranView extends React.Component {
 
     if (this.props.userProfile.veteranStatus === 'SERVER_ERROR') {
       // If eMIS status is null, show a system down message.
-      view = <SystemDownView messageLine1="We’re sorry. We can’t process your request for a Veteran ID Card right now because we can't access your records at the moment. Please try again in a few minutes." messageLine2="If it still doesn’t work, please call the Vets.gov Help Desk at 1-855-574-7286, TTY: 1-800-877-8339. We’re here Monday &#8211; Friday, 8:00 a.m. &#8211; 8:00 p.m. (ET)."/>;
+      view = <SystemDownView messageLine1="We’re sorry. We can’t process your request for a Veteran ID Card right now because we can't access your records at the moment. Please try again in a few minutes." messageLine2="If it still doesn’t work, please call the VA.gov Help Desk at 1-855-574-7286, TTY: 1-800-877-8339. We’re here Monday &#8211; Friday, 8:00 a.m. &#8211; 8:00 p.m. (ET)."/>;
     } else {
       view = this.props.children;
     }
