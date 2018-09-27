fb287c5d1712a8331789c11b0941d64f886e2ba5
Joe <jstanek@thesocompany.com>
2018-09-27 14:57:19 -0400

    Content change from Vets.gov to VA.gov

--- src/platform/site-wide/user-nav/components/HelpMenu.jsx
+++ src/platform/site-wide/user-nav/components/HelpMenu.jsx
@@ -23,7 +23,7 @@ class HelpMenu extends React.Component {
     } else {
       dropDownContents = (
         <div>
-          <p><b>Call the Vets.gov Help Desk</b></p>
+          <p><b>Call the VA.gov Help Desk</b></p>
           <p><a href="tel:18555747286">1-855-574-7286</a></p>
           <p>TTY: <a href="tel:+18008778339">1-800-877-8339</a></p>
           <p>Monday &ndash; Friday, 8:00 a.m. &ndash; 8:00 p.m. (ET)</p>
