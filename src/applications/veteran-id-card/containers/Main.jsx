89224bdb4ba01ad3b761fd2cc69c45177a324723
Joe <jstanek@thesocompany.com>
2018-09-27 16:10:41 -0400

    Content changes from Vets.gov to VA.gov

--- src/applications/veteran-id-card/containers/Main.jsx
+++ src/applications/veteran-id-card/containers/Main.jsx
@@ -57,7 +57,7 @@ class Main extends React.Component {
 
   renderVicError() {
     const headline = (<h4>We're sorry. Something went wrong when loading the page.</h4>);
-    const content = (<p>Please refresh the page or try again later. You can also call the Vets.gov Help Desk at <a href="tel:855-574-7286">1-855-574-7286</a>, TTY: <a href="tel:18008778339">1-800-877-8339</a>, Monday &#8211; Friday, 8:00 a.m. &#8211; 8:00 p.m. (ET).</p>);
+    const content = (<p>Please refresh the page or try again later. You can also call the VA.gov Help Desk at <a href="tel:855-574-7286">1-855-574-7286</a>, TTY: <a href="tel:18008778339">1-800-877-8339</a>, Monday &#8211; Friday, 8:00 a.m. &#8211; 8:00 p.m. (ET).</p>);
 
     return (
       <AlertBox
