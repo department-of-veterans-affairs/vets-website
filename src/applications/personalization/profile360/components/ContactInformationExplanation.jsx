d44c7cc5e8b7395aacfb40821005a53448bd3e65
Rian Fowler <rianfowler@outlook.com>
2018-09-27 18:23:28 -0600

    content updates (#8584)

--- src/applications/personalization/profile360/components/ContactInformationExplanation.jsx
+++ src/applications/personalization/profile360/components/ContactInformationExplanation.jsx
@@ -10,10 +10,6 @@ export default function ContactInformationExplanation() {
       <div style={{ marginBottom: 10 }}>
         <AlertBox status="info" isVisible>
           <p>We’ll use this information to contact you about certain benefits and services, including disability compensation, pension benefits, and claims and appeals. If you’re enrolled in the VA health care program, your health care team may also use this information to communicate with you.</p>
-          <a
-            href="/health-care/"
-            onClick={() => { recordEvent({ event: 'profile-navigation', 'profile-action': 'view-link', 'profile-section': 'learn-more-va-benefits' }); }}>
-          Learn more about VA health benefits</a>.
         </AlertBox>
       </div>
       <AdditionalInfo
