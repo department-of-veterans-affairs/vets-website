f6b0efd812ea48618cf5703decc1b6de81696ca7
Jeff Balboni <jeff@adhocteam.us>
2018-10-01 11:41:08 -0400

    Footer updates

--- src/platform/site-wide/va-footer/containers/Main.jsx
+++ src/platform/site-wide/va-footer/containers/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import groupBy from 'lodash/groupBy';
 import orderBy from 'lodash/orderBy';
 import links from '../../../static-data/footer-links.json';
-import { isWideScreen, isEnter } from '../../../utilities/accessibility/index';
+import { isWideScreen } from '../../../utilities/accessibility/index';
 
 export class Main extends React.Component {
   constructor(props) {
@@ -21,9 +21,6 @@ export class Main extends React.Component {
       isMobile: !isWideScreen()
     });
   }
-  openModal = () => {
-    this.setState({ modalOpen: true });
-  }
   generateLinkItems = (column, direction = 'asc') => (
     <ul className="va-footer-links">
       {orderBy(this.linkObj[column], 'order', direction).map(link =>
@@ -31,11 +28,6 @@ export class Main extends React.Component {
       )}
     </ul>
   )
-  handleKeyPress = (e) => {
-    if (isEnter(e)) {
-      this.openModal();
-    }
-  }
   buildContact = () => {
     let innerClassName = '';
     let buttonEnabled = '';
@@ -62,7 +54,7 @@ export class Main extends React.Component {
             <li className="usa-accordion-content" id="veteran-crisis" aria-hidden="true">
               <ul className="va-footer-links">
                 <li>
-                  <a role="button" tabIndex="0" onClick={this.openModal} onKeyPress={this.handleKeyPress} >Veterans Crisis Line</a>
+                  <button className="va-button-link va-overlay-trigger" data-show="#modal-crisisline">Veterans Crisis Line</button>
                 </li>
               </ul>
             </li>
@@ -88,7 +80,7 @@ export class Main extends React.Component {
           </h4>
         </li>
         <li>
-          <a role="button" tabIndex="0" onClick={this.openModal}  onKeyPress={this.handleKeyPress} >Veterans Crisis Line</a>
+          <button className="va-button-link va-overlay-trigger" data-show="#modal-crisisline">Veterans Crisis Line</button>
         </li>
         <li id="footer-vcl">
           <h4 className="va-footer-linkgroup-title">
@@ -99,47 +91,6 @@ export class Main extends React.Component {
           {this.generateLinkItems('4')}
         </li>
       </ul>
-    );
-  }
-  closeModal = () => {
-    this.setState({ modalOpen: false });
-  }
-  renderModal() {
-    return (
-      <div id="modal-crisisline" className="va-overlay va-modal va-modal-large va-overlay--open" role="alertdialog">
-        <div className="va-crisis-panel va-modal-inner">
-
-          <button className="va-modal-close va-overlay-close va-crisis-panel-close" type="button">
-            <i className="fa fa-times-circle-o va-overlay-close" aria-hidden="true"></i>
-            <span className="usa-sr-only va-overlay-close">Close this modal</span>
-          </button>
-
-          <div className="va-overlay-body va-crisis-panel-body">
-            <h3 className="va-crisis-panel-title">We're here anytime, day or night</h3>
-            <p>Whatever you're struggling with, our responders can offer confidential help 24/7. Many of them are Veterans themselves.</p>
-            <ul className="va-crisis-panel-list">
-              <li>
-                <i className="fa fa-phone va-crisis-panel-icon" aria-hidden="true"></i>
-                <a href="tel:18002738255">Call <strong>1-800-273-8255 (Press 1)</strong></a>
-              </li>
-              <li>
-                <i className="fa fa-mobile va-crisis-panel-icon" aria-hidden="true"></i>
-                <a href="sms:838255">Text <strong>838255</strong></a>
-              </li>
-              <li>
-                <i className="fa fa-comments-o va-crisis-panel-icon" aria-hidden="true"></i>
-                <a className="no-external-icon" href="https://www.veteranscrisisline.net/ChatTermsOfService.aspx?account=Veterans%20Chat">Start a confidential chat</a>
-              </li>
-              <li>
-                <i className="fa fa-deaf va-crisis-panel-icon" aria-hidden="true"></i>
-                <a href="tel:18007994889">Call TTY if you have hearing loss <strong>1-800-799-4889</strong></a>
-              </li>
-            </ul>
-
-            Get more resources at <a className="no-external-icon" href="https://www.veteranscrisisline.net/">VeteransCrisisLine.net</a>.
-          </div>
-        </div>
-      </div>
     );
   }
   render() {
@@ -165,7 +116,6 @@ export class Main extends React.Component {
     }
     return (
       <div>
-        {this.state.modalOpen && this.renderModal()}
         <div className="footer-inner">
           <div className="usa-grid usa-grid-flex-mobile">
             <ul className={className} id="footer-first-child">
