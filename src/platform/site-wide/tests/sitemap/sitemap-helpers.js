8d669aebee3b5e3e6ef46712879a206f372d7108
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2018-10-02 10:52:18 -0700

    Makes sitemap helpers promise based again

--- src/platform/site-wide/tests/sitemap/sitemap-helpers.js
+++ src/platform/site-wide/tests/sitemap/sitemap-helpers.js
@@ -7,34 +7,39 @@ const SITEMAP_URL = `${E2eHelpers.baseUrl}/sitemap.xml`;
 const SITEMAP_LOC_NS = 'http://www.sitemaps.org/schemas/sitemap/0.9';
 const DOMAIN_REGEX = /http[s]?:\/\/(.*?)\//;
 
-async function sitemapURLs() {
-  const res = await fetch(SITEMAP_URL);
-  const body =  await res.text();
-  const doc = libxmljs.parseXml(body);
-
-  const urls = doc
-    .find('//xmlns:loc', SITEMAP_LOC_NS)
-    .map(n => n.text().replace(DOMAIN_REGEX, `${E2eHelpers.baseUrl}/`))
-    .filter(url => !(url.endsWith('auth/login/callback/')))
-    .filter(url => {
-      return !(/.*opt-out-information-sharing.*/.test(url));
+function sitemapURLs() {
+  return fetch(SITEMAP_URL)
+    .then(res => {
+      return  res.text();
+    }).then(body => {
+      return libxmljs.parseXml(body);
+    }).then(doc => {
+      return doc
+        .find('//xmlns:loc', SITEMAP_LOC_NS)
+        .map(n => n.text().replace(DOMAIN_REGEX, `${E2eHelpers.baseUrl}/`))
+        .filter(url => !(url.endsWith('auth/login/callback/')))
+        .filter(url => {
+          return !(/.*opt-out-information-sharing.*/.test(url));
+        });
+    })
+    .then(urls => {
+      const onlyTest508Rules = [
+      // 404 page contains 2 search auto-suggest elements with the same element ID,
+      // which violates WCAG 2.0 standards. This element id is referenced by
+      // https://search.usa.gov/assets/sayt_loader_libs.js, so if we change the ID
+      // of one of the elements, search won't work.
+        '/404.html',
+        // This is here because aXe bug flags the custom select component on this page
+        '/facilities/',
+        // This is here because an aXe bug flags the autosuggest component on this page
+        '/gi-bill-comparison-tool/'
+      ];
+      // Whitelist of URLs to only test against the 'section508' rule set and not
+      // the stricter 'wcag2a' rule set. For each URL added to this list, please
+      // add a comment explaining why it cannot be tested against stricter rules.
+      return { urls, onlyTest508Rules };
     });
 
-  // Whitelist of URLs to only test against the 'section508' rule set and not
-  // the stricter 'wcag2a' rule set. For each URL added to this list, please
-  // add a comment explaining why it cannot be tested against stricter rules.
-  const onlyTest508Rules = [
-    // 404 page contains 2 search auto-suggest elements with the same element ID,
-    // which violates WCAG 2.0 standards. This element id is referenced by
-    // https://search.usa.gov/assets/sayt_loader_libs.js, so if we change the ID
-    // of one of the elements, search won't work.
-    '/404.html',
-    // This is here because aXe bug flags the custom select component on this page
-    '/facilities/',
-    // This is here because an aXe bug flags the autosuggest component on this page
-    '/gi-bill-comparison-tool/'
-  ];
-  return { urls, onlyTest508Rules };
 
 }
 
