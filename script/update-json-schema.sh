c5fe549880a997d15672d83d3f83788f00cdd230
Erik P. Hansen <eph@erikphansen.com>
2018-10-02 09:24:27 -0700

    Add script to update the vets-json-schema
    - Updates package.json to use the latest version of the vets-json-schema
    - Runs `yarn` to install the new version

---
+++ script/update-json-schema.sh
@@ -0,0 +1,4 @@
+NEW_HASH=$(git ls-remote git://github.com/department-of-veterans-affairs/vets-json-schema/ refs/heads/master | awk '{print $1}')
+sed -i -e 's/\(vets-json-schema\.git#\)\(.*\)\(",$\)/\1'$NEW_HASH'\3/' package.json
+rm package.json-e
+yarn
