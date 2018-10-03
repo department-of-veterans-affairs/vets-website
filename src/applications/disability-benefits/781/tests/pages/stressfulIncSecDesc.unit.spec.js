70da7a47ee4487d177761c64d643c268a88c71a2
Rob Felder <felder_robert@bah.com>
2018-10-03 10:33:38 -0400

    Unit tests

--- src/applications/disability-benefits/781/tests/pages/stressfulIncSecDesc.unit.spec.js
+++ src/applications/disability-benefits/781/tests/pages/stressfulIncSecDesc.unit.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
-// import sinon from 'sinon';
+import sinon from 'sinon';
 import { DefinitionTester } from '../../../../../platform/testing/unit/schemaform-utils.jsx';
 import { mount } from 'enzyme';
 import formConfig from '../../config/form';
@@ -19,5 +19,23 @@ describe('Add Secondary Stressful Incident Description', () => {
     );
 
     expect(form.find('textarea').length).to.equal(1);
+  });
+
+  it('should submit', () => {
+    const onSubmit = sinon.spy();
+
+    const form = mount(
+      <DefinitionTester
+        definitions={formConfig}
+        schema={schema}
+        uiSchema={uiSchema}
+        data={{}}
+        formData={{}}
+        onSubmit={onSubmit}/>
+    );
+
+    form.find('form').simulate('submit');
+    expect(form.find('.usa-input-error-message').length).to.equal(0);
+    expect(onSubmit.called).to.be.true;
   });
 });
