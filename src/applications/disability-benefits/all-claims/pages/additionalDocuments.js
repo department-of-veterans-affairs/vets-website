6ae3cf66877903fb34a59c92d591534f373895e5
Christopher Valarida <chris.valarida@adhocteam.us>
2018-10-03 16:12:07 -0700

    Please the linter, please the linter...

--- src/applications/disability-benefits/all-claims/pages/additionalDocuments.js
+++ src/applications/disability-benefits/all-claims/pages/additionalDocuments.js
@@ -8,45 +8,54 @@ import full526EZSchema from '../config/schema.js';
 const { attachments } = full526EZSchema;
 
 export const uiSchema = {
-  additionalDocuments: Object.assign({},
+  additionalDocuments: Object.assign(
+    {},
     fileUploadUI('Lay statements or other evidence', {
       itemDescription: 'Adding additional evidence:',
       fileUploadUrl: `${environment.API_URL}/v0/upload_supporting_evidence`,
       buttonText: 'Upload Document',
       addAnotherLabel: 'Add Another Document',
-      fileTypes: ['pdf', 'jpg', 'jpeg', 'png', 'gif', 'bmp', 'tif', 'tiff', 'txt'],
+      fileTypes: [
+        'pdf',
+        'jpg',
+        'jpeg',
+        'png',
+        'gif',
+        'bmp',
+        'tif',
+        'tiff',
+        'txt',
+      ],
       maxSize: FIFTY_MB,
-      createPayload: (file) => {
+      createPayload: file => {
         const payload = new FormData();
         payload.append('supporting_evidence_attachment[file_data]', file);
 
         return payload;
       },
-      parseResponse: (response, file) => {
-        return {
-          name: file.name,
-          confirmationCode: response.data.attributes.guid
-        };
-      },
+      parseResponse: (response, file) => ({
+        name: file.name,
+        confirmationCode: response.data.attributes.guid,
+      }),
       // this is the uiSchema passed to FileField for the attachmentId schema
       // FileField requires this name be used
       attachmentSchema: {
-        'ui:title': 'Document type'
+        'ui:title': 'Document type',
       },
       // this is the uiSchema passed to FileField for the name schema
       // FileField requires this name be used
       attachmentName: {
-        'ui:title': 'Document name'
-      }
+        'ui:title': 'Document name',
+      },
     }),
-    { 'ui:description': additionalDocumentDescription }
-  )
+    { 'ui:description': additionalDocumentDescription },
+  ),
 };
 
 export const schema = {
   type: 'object',
   required: ['additionalDocuments'],
   properties: {
-    additionalDocuments: attachments
-  }
+    additionalDocuments: attachments,
+  },
 };
