5613f0f8d586007237f3487631179bc8ec6c2e17
Claire Hsu <clairehsu86@gmail.com>
2018-10-05 08:51:46 -0700

    Performance hack test

--- src/platform/testing/unit/enzyme-setup.js
+++ src/platform/testing/unit/enzyme-setup.js
@@ -2,7 +2,15 @@
  * Adapter for enzyme configuration, imported in mocha.opts
  */
 
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
-
-configure({ adapter: new Adapter() });
+let mockSetup = false;
+// Here we're "mocking" enzyme, so that we can delay loading of
+// the utility functions until emzyme is imported in tests.
+jest.mock('enzyme', () => {
+  const actualEnzyme = require.requireActual('enzyme');
+  if (!mockSetup) {
+    mockSetup = true;
+    const Adapter = require('enzyme-adapter-react-15');
+    actualEnzyme.configure({ adapter: new Adapter() });
+  }
+  return actualEnzyme;
+});
