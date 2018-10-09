62a6375c716ada732843dbe6d9bc2a543ea6a125
Christopher Valarida <chris.valarida@adhocteam.us>
2018-10-08 17:40:33 -0700

    Fix unit test

--- src/applications/claims-status/tests/utils/helpers.unit.spec.js
+++ src/applications/claims-status/tests/utils/helpers.unit.spec.js
@@ -483,9 +483,12 @@ describe('Disability benefits helpers: ', () => {
       const type = 123;
       const contents = getStatusContents(type);
       expect(contents.title).to.equal('We don’t know your appeal status');
-      expect(contents.description.props.children).to.equal(
-        `We’re sorry, ${siteName} will soon be updated to show your status.`,
-      );
+      expect(contents.description.props.children).to.eql([
+        // React splits it up into separate nodes when variables are inserted
+        'We’re sorry, ',
+        siteName,
+        ' will soon be updated to show your status.',
+      ]);
     });
 
     // 'remand' and 'bva_decision' do a fair amount of dynamic content generation and formatting
