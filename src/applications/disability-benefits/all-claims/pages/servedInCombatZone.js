a8848d045d6b0a01dab9abd3b7789a8b4226415f
Mark Greenburg <marek.greenburg@gmail.com>
2018-10-12 10:47:03 -0500

    Update question content

--- src/applications/disability-benefits/all-claims/pages/servedInCombatZone.js
+++ src/applications/disability-benefits/all-claims/pages/servedInCombatZone.js
@@ -4,7 +4,7 @@ const { servedInCombatZonePost911 } = fullSchema.properties;
 
 export const uiSchema = {
   servedInCombatZonePost911: {
-    'ui:title': 'Did you serve in a combat zone after 9/11/2001?',
+    'ui:title': 'Did you serve in a combat zone after Sept. 11, 2001?',
     'ui:widget': 'yesNo',
   },
 };
