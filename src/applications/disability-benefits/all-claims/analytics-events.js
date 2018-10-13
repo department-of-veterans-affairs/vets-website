db411aaa601b95a893c0224f5b581d7ed8cedbbc
Christopher Valarida <chris.valarida@adhocteam.us>
2018-10-12 17:01:02 -0700

    Give nuance

--- src/applications/disability-benefits/all-claims/analytics-events.js
+++ src/applications/disability-benefits/all-claims/analytics-events.js
@@ -12,7 +12,7 @@ const analyticsEvents = [
   },
   {
     action: ITF_CREATION_FAILED,
-    event: 'disability-526EZ-itf-creation-failed',
+    event: 'disability-526EZ-itf-not-creation',
   },
   {
     action: ITF_FETCH_SUCCEEDED,
@@ -20,7 +20,12 @@ const analyticsEvents = [
   },
   {
     action: ITF_FETCH_FAILED,
-    event: 'disability-526EZ-itf-fetch-failed',
+    event: store => {
+      const state = store.getState();
+      return state.user.profile.savedForms.find(f => f.form === '21-526EZ')
+        ? 'disability-526EZ-itf-not-retrieved-existing-user'
+        : 'disability-526EZ-itf-not-retrieved-new-user';
+    },
   },
 ];
 
