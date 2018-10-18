c3e781f965c2d8af0845280c5078f3c8325afd36
Rob Felder <felder_robert@bah.com>
2018-10-18 09:08:47 -0400

    Using countries and states from schema instead of helper list

--- src/applications/disability-benefits/526EZ/config/form.js
+++ src/applications/disability-benefits/526EZ/config/form.js
@@ -64,9 +64,6 @@ import {
   limitedConsentTextTitle,
   limitedConsentDescription,
   validateZIP,
-  countries,
-  states,
-  stateNames,
 } from '../helpers';
 
 import {
@@ -941,13 +938,13 @@ const formConfig = {
                               },
                               country: {
                                 type: 'string',
-                                enum: countries,
+                                enum: address.properties.country.enum,
                                 default: 'USA',
                               },
                               state: {
                                 type: 'string',
-                                enum: states,
-                                enumNames: stateNames,
+                                enum: address.properties.state.enum,
+                                enumNames: address.properties.state.enumNames,
                               },
                             },
                           },
@@ -970,114 +967,6 @@ const formConfig = {
             },
           },
         },
-
-        // TODO: Re-enable after 4142 PDF integration
-        // privateMedicalRecordRelease: {
-        //   title: '',
-        //   path: 'supporting-evidence/:index/private-medical-records-release',
-        //   showPagePerItem: true,
-        //   itemFilter: (item) => _.get('view:selected', item),
-        //   arrayPath: 'disabilities',
-        //   depends: (formData, index) => {
-        //     const hasRecords = _.get(`disabilities.${index}.view:selectableEvidenceTypes.view:privateMedicalRecords`, formData);
-        //     const requestsRecords = _.get(`disabilities.${index}.view:uploadPrivateRecords`, formData) === 'no';
-        //     return hasRecords && requestsRecords;
-        //   },
-        //   uiSchema: {
-        //     disabilities: {
-        //       items: {
-        //         'ui:description': 'Please let us know where and when you received treatment. We’ll request your private medical records for you. If you have your private medical records available, you can upload them later in the application',
-        //         privateRecordReleases: {
-        //           'ui:options': {
-        //             itemName: 'Private Medical Record Release',
-        //             viewField: treatmentView
-        //           },
-        //           items: {
-        //             'ui:order': [
-        //               'treatmentCenterName',
-        //               'privateMedicalRecordsReleaseRestricted',
-        //               'view:releaseRestrictedNotice',
-        //               'treatmentDateRange',
-        //               'treatmentCenterAddress'
-        //             ],
-        //             treatmentCenterName: {
-        //               'ui:title': 'Name of private provider or hospital'
-        //             },
-        //             treatmentDateRange: dateRangeUI(
-        //               'Approximate date of first treatment',
-        //               'Approximate date of last treatment',
-        //               'Date of last treatment must be after date of first treatment'
-        //             ),
-        //             privateMedicalRecordsReleaseRestricted: {
-        //               'ui:title': 'I give my consent, or permission, to my doctor to only release records related to this condition'
-        //             },
-        //             'view:releaseRestrictedNotice': {
-        //               'ui:description': () => recordReleaseWarning,
-        //               'ui:options': {
-        //                 expandUnder: 'privateMedicalRecordsReleaseRestricted'
-        //               }
-        //             },
-        //             treatmentCenterAddress: {
-        //               'ui:order': [
-        //                 'country',
-        //                 'addressLine1',
-        //                 'addressLine2',
-        //                 'city',
-        //                 'state',
-        //                 'zipCode'
-        //               ],
-        //               // TODO: confirm validation for PCIU address across all usage
-        //               // 'ui:validations': [validateAddress],
-        //               country: {
-        //                 'ui:title': 'Country'
-        //               },
-        //               addressLine1: {
-        //                 'ui:title': 'Street address'
-        //               },
-        //               addressLine2: {
-        //                 'ui:title': 'Street address'
-        //               },
-        //               city: {
-        //                 'ui:title': 'City'
-        //               },
-        //               state: {
-        //                 'ui:title': 'State'
-        //               },
-        //               zipCode: {
-        //                 'ui:title': 'Postal code',
-        //                 'ui:options': {
-        //                   widgetClassNames: 'usa-input-medium',
-        //                 }
-        //               }
-        //             }
-        //           }
-        //         }
-        //       }
-        //     }
-        //   },
-        //   schema: {
-        //     type: 'object',
-        //     properties: {
-        //       disabilities: {
-        //         type: 'array',
-        //         items: {
-        //           type: 'object',
-        //           properties: {
-        //             privateRecordReleases: _.set(
-        //               'items.properties.view:releaseRestrictedNotice',
-        //               {
-        //                 type: 'object',
-        //                 'ui:collapsed': true,
-        //                 properties: {}
-        //               },
-        //               privateRecordReleases
-        //             )
-        //           }
-        //         }
-        //       }
-        //     },
-        //   }
-        // },
         recordUpload: {
           title: 'Upload your private medical records',
           depends: (formData, index) => {
