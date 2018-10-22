e04155ba5fd0e9f75693b7a0f55f01a762856f0b
Claire Hsu <claire@adhocteam.us>
2018-10-22 14:32:40 -0700

    Form expired messages (#8712)
    * Add expired messages to application status and SIP intro
    
    * Update in progress message
    
    * PR comments

--- src/platform/forms/save-in-progress/FormStartControls.jsx
+++ src/platform/forms/save-in-progress/FormStartControls.jsx
@@ -57,16 +57,22 @@ class FormStartControls extends React.Component {
     if (this.props.formSaved) {
       return (
         <div>
-          <ProgressButton
-            onButtonClick={this.handleLoadForm}
-            buttonText="Continue Your Application"
-            buttonClass="usa-button-primary no-text-transform"
-          />
+          {!this.props.isExpired && (
+            <ProgressButton
+              onButtonClick={this.handleLoadForm}
+              buttonText="Continue Your Application"
+              buttonClass="usa-button-primary no-text-transform"
+            />
+          )}
           {!this.props.resumeOnly && (
             <ProgressButton
               onButtonClick={this.toggleModal}
-              buttonText="Start Over"
-              buttonClass="usa-button-secondary"
+              buttonText="Start a New Application"
+              buttonClass={
+                this.props.isExpired
+                  ? 'usa-button-primary'
+                  : 'usa-button-secondary'
+              }
             />
           )}
           <Modal
@@ -79,7 +85,7 @@ class FormStartControls extends React.Component {
             <p>Are you sure you want to start over?</p>
             <ProgressButton
               onButtonClick={this.startOver}
-              buttonText="Start Over"
+              buttonText="Start a New Application"
               buttonClass="usa-button-primary"
             />
             <ProgressButton
