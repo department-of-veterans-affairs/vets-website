7634aeb97f485b884ab1478c9e135dd9c8b724ed
Devin <mccurdy_devin@bah.com>
2018-10-22 09:26:06 -0400

    Removed 781 walkthrough choice and document upload pages

--- src/applications/disability-benefits/all-claims/pages/index.js
+++ src/applications/disability-benefits/all-claims/pages/index.js
@@ -74,16 +74,6 @@ import {
 } from './uploadPtsdDocuments';
 
 import {
-  uiSchema as ptsdWalkthroughChoice781aUISchema,
-  schema as ptsdWalkthroughChoice781aSchema,
-} from './ptsdWalkthroughChoice781a';
-
-import {
-  uiSchema as uploadPersonalPtsdDocumentsUISchema,
-  schema as uploadPersonalPtsdDocumentsSchema,
-} from './uploadPersonalPtsdDocuments';
-
-import {
   uiSchema as summaryOfDisabilitiesUISchema,
   schema as summaryOfDisabilitiesSchema,
 } from './summaryOfDisabilities';
@@ -211,16 +201,6 @@ export const ptsdWalkthroughChoice781 = {
 export const uploadPtsdDocuments = {
   uiSchema: uploadPtsdDocumentsUISchema,
   schema: uploadPtsdDocumentsSchema,
-};
-
-export const ptsdWalkthroughChoice781a = {
-  uiSchema: ptsdWalkthroughChoice781aUISchema,
-  schema: ptsdWalkthroughChoice781aSchema,
-};
-
-export const uploadPersonalPtsdDocuments = {
-  uiSchema: uploadPersonalPtsdDocumentsUISchema,
-  schema: uploadPersonalPtsdDocumentsSchema,
 };
 
 export const summaryOfDisabilities = {
