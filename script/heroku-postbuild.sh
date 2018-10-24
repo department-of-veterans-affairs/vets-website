131924ee174c441f717d3b366e546ae384371a53
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-24 14:02:34 -0400

    Add arg to clone command

--- script/heroku-postbuild.sh
+++ script/heroku-postbuild.sh
@@ -5,7 +5,7 @@ vagov_content_dir=${vagov_apps_dir}/../vagov_content
 echo Installing vagov_content into ${vagov_content_dir}
 
 # clone the content into the sibling directory
-git clone --depth=1 https://github.com/department-of-veterans-affairs/vagov-content ${vagov_content_dir}
+git clone --depth=1 https://github.com/department-of-veterans-affairs/vagov-content --content-directory ${vagov_content_dir}
 
 # cd back to the apps
 cd ${vagov_apps_dir}
