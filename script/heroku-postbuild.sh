f7c99c44d6f7b8ce6722f1233449e2d1c84cc9dd
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-24 14:44:43 -0400

    Too tired for this

--- script/heroku-postbuild.sh
+++ script/heroku-postbuild.sh
@@ -5,10 +5,10 @@ vagov_content_dir=${vagov_apps_dir}/../vagov_content
 echo Installing vagov_content into ${vagov_content_dir}
 
 # clone the content into the sibling directory
-git clone --depth=1 https://github.com/department-of-veterans-affairs/vagov-content --content-directory ${vagov_content_dir}
+git clone --depth=1 https://github.com/department-of-veterans-affairs/vagov-content ${vagov_content_dir}
 
 # cd back to the apps
 cd ${vagov_apps_dir}
 
 # build the site as usual
-npm run build --  --entry static-pages
+npm run build --  --entry static-pages --content-directory ${vagov_content_dir}
