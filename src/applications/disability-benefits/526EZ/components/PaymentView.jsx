83cea7b7a45bf0c7b46bd1cff8e44cc65654ac1b
Mark Greenburg <marek.greenburg@gmail.com>
2018-10-24 10:41:03 -0500

    Update 'no payment' content

--- src/applications/disability-benefits/526EZ/components/PaymentView.jsx
+++ src/applications/disability-benefits/526EZ/components/PaymentView.jsx
@@ -34,48 +34,45 @@ const PaymentView = response => {
     // should we also check bankName?
     return (
       <div>
-        <PaymentDescription />
-        <div className="blue-bar-block">
-          <p>
-            <strong>
-              We don’t have any bank account information for you in our system.
-            </strong>
-            <br />
-            You can still complete this form, but we’ll need your payment
-            information before we can pay you any compensation.
-          </p>
-          <p>
-            <strong>You can update your payment information a few ways:</strong>
-          </p>
-          <ul>
-            <li>
-              Fill out and send us a Direct Deposit Enrollment form (VA Form
-              24-0296).{' '}
-              <a
-                href="https://www.vba.va.gov/pubs/forms/vba-24-0296-are.pdf"
-                target="_blank"
-              >
-                Download VA Form 24-0296
-              </a>
-              , <strong>or</strong>
-            </li>
-            <li>
-              Go to eBenefits to add your payment information to your account.{' '}
-              <a
-                href="https://www.ebenefits.va.gov/ebenefits/about/feature?feature=direct-deposit-and-contact-information"
-                target="_blank"
-              >
-                Go to eBenefits
-              </a>
-              , <strong>or</strong>
-            </li>
-            <li>
-              Call Veterans Benefits Assistance at{' '}
-              <a href="tel:1-800-827-1000">1-800-827-1000</a>, Monday – Friday,
-              8:00 a.m. – 9:00 p.m. (ET).
-            </li>
-          </ul>
-        </div>
+        <p>
+          <strong>
+            We don’t have any bank account information for you in our system.
+          </strong>
+          <br />
+          You can still complete this form, but we’ll need your payment
+          information before we can pay you any compensation.
+        </p>
+        <p>
+          <strong>You can update your payment information a few ways:</strong>
+        </p>
+        <ul>
+          <li>
+            Fill out and send us a Direct Deposit Enrollment form (VA Form
+            24-0296).{' '}
+            <a
+              href="https://www.vba.va.gov/pubs/forms/vba-24-0296-are.pdf"
+              target="_blank"
+            >
+              Download VA Form 24-0296
+            </a>
+            , <strong>or</strong>
+          </li>
+          <li>
+            Go to eBenefits to add your payment information to your account.{' '}
+            <a
+              href="https://www.ebenefits.va.gov/ebenefits/about/feature?feature=direct-deposit-and-contact-information"
+              target="_blank"
+            >
+              Go to eBenefits
+            </a>
+            , <strong>or</strong>
+          </li>
+          <li>
+            Call Veterans Benefits Assistance at{' '}
+            <a href="tel:1-800-827-1000">1-800-827-1000</a>, Monday – Friday,
+            8:00 a.m. – 9:00 p.m. (ET).
+          </li>
+        </ul>
       </div>
     );
   }
