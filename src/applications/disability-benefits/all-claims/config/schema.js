25a309cec73178fac2e733609f0354b97c6193b4
Charles Stone <charles@zstones.com>
2018-10-25 21:56:37 -0700

    Add injuryDeathOther element to manage text from other selection

--- src/applications/disability-benefits/all-claims/config/schema.js
+++ src/applications/disability-benefits/all-claims/config/schema.js
@@ -543,6 +543,114 @@ const schema = {
         },
       },
     },
+    form4142: {
+      type: 'object',
+      properties: {
+        limitedConsent: {
+          type: 'string',
+        },
+        providerFacility: {
+          type: 'array',
+          required: [
+            'providerFacilityName',
+            'treatmentDateRange',
+            'providerFacilityAddress',
+          ],
+          items: {
+            type: 'object',
+            properties: {
+              providerFacilityName: {
+                type: 'string',
+              },
+              treatmentDateRange: {
+                $ref: '#/definitions/dateRange',
+              },
+              providerFacilityAddress: {
+                $ref: '#/definitions/address',
+              },
+            },
+          },
+        },
+        privacyAgreementAccepted: {
+          $ref: '#/definitions/privacyAgreementAccepted',
+        },
+      },
+    },
+    form0781: {
+      type: 'object',
+      incident: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            personalAssault: {
+              type: 'boolean',
+            },
+            medalsCitations: {
+              type: 'string',
+            },
+            incidentDate: {
+              $ref: '#/definitions/date',
+            },
+            incidentLocation: {
+              type: 'string',
+            },
+            incidentDescription: {
+              type: 'string',
+            },
+            unitAssigned: {
+              type: 'string',
+            },
+            unitAssignedDates: {
+              $ref: '#/definitions/dateRange',
+            },
+            remarks: {
+              type: 'string',
+            },
+            personInvolved: {
+              type: 'array',
+              items: {
+                type: 'object',
+                name: {
+                  $ref: '#/definitions/fullName',
+                },
+                rank: {
+                  type: 'string',
+                },
+                injuryDeath: {
+                  type: 'string',
+                  enum: [
+                    'Killed in Action',
+                    'Killed Non-Battle',
+                    'Wounded in Action',
+                    'Injured Non-Battle',
+                    'Other',
+                  ],
+                },
+                injuryDeathDate: {
+                  $ref: '#/definitions/date',
+                },
+                unitAssigned: {
+                  type: 'string',
+                },
+              },
+            },
+            source: {
+              type: 'array',
+              items: {
+                type: 'object',
+                name: {
+                  $ref: '#/definitions/fullName',
+                },
+                address: {
+                  $ref: '#/definitions/address',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
   properties: {
     alternateNames: {
