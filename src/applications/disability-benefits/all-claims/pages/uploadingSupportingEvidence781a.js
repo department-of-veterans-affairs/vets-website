5d5bde7eb6ba309e8d73fea55fa7790b3045795a
Aaron LeCheminant <aaronjlech@gmail.com>
2018-10-26 13:17:27 -0400

    update url for uploadas

--- src/applications/disability-benefits/all-claims/pages/uploadingSupportingEvidence781a.js
+++ src/applications/disability-benefits/all-claims/pages/uploadingSupportingEvidence781a.js
@@ -15,7 +15,7 @@ export const uiSchema = {
   ptsd781: fileUploadUI('', {
     itemDescription: 'PTSD 781a form',
     hideLabelText: true,
-    fileUploadUrl: `${environment.API_URL}/v0`,
+    fileUploadUrl: `${environment.API_URL}/v0/upload_supporting_evidence`,
     fileTypes: [
       'pdf',
       'jpg',
