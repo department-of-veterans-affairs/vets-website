8bdbc0bfae54e92c3c17e22dff335018493c1d28
Claire Hsu <clairehsu86@gmail.com>
2018-10-25 17:00:24 -0700

    PR comments

--- src/applications/proxy-rewrite/tests/health.integration.spec.js
+++ src/applications/proxy-rewrite/tests/health.integration.spec.js
@@ -1,37 +1,43 @@
 const puppeteer = require('puppeteer');
 
+const fullScreen = { width: 1024, height: 800 };
+const mobileScreen = { width: 400, height: 700 };
+
+const VAGOV_HEALTH_URL = 'https://staging.va.gov/health/';
+
+const HEADER = 'header';
+const FOOTER = 'footer';
+
 describe('jest-image-snapshot usage with an image received from puppeteer', () => {
   let browser;
+  let page;
   jest.setTimeout(20000);
+
+  const compareScreenshots = async selector => {
+    const element = await page.$(selector);
+    await page.waitFor(1000);
+    const image = await element.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  };
   beforeAll(async () => {
     browser = await puppeteer.launch();
   });
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto(VAGOV_HEALTH_URL);
+  });
 
   it('compares screenshots of elements', async () => {
-    const page = await browser.newPage();
-    page.setViewport({ width: 1024, height: 800 });
-    await page.goto('https://preview.va.gov/health/');
-    const TESTELEMENT = 'footer';
-    const element = await page.$(TESTELEMENT);
-    await page.waitFor(1000);
-    const image = await element.screenshot();
-
-    expect(image).toMatchImageSnapshot();
+    page.setViewport(fullScreen);
+    compareScreenshots(FOOTER);
   });
   it('compares small width screenshots', async () => {
-    const page = await browser.newPage();
-    page.setViewport({ width: 400, height: 700 });
-    await page.goto('https://preview.va.gov/health/');
-    const TESTELEMENT = 'header';
-    const element = await page.$(TESTELEMENT);
-    await page.waitFor(1000);
-    const image = await element.screenshot();
-    expect(image).toMatchImageSnapshot();
+    page.setViewport(mobileScreen);
+    compareScreenshots(HEADER);
   });
   it('checks for expanded content', async () => {
-    const page = await browser.newPage();
-    page.setViewport({ width: 1024, height: 800 });
-    await page.goto('https://preview.va.gov/health/');
+    page.setViewport(fullScreen);
     page.click('#usa-banner-toggle');
     await page.waitForSelector('#gov-banner', {
       visible: true,
