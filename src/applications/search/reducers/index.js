989ed27b9112c4cfc599bb5a0b80259146f8ac54
Ben <bshyong@users.noreply.github.com>
2018-10-29 11:02:41 -0700

    Search V2 page-based pagination (#8917)
    * new pagination wip
    
    * fix analytics error; search logic
    
    * page based pagination

--- src/applications/search/reducers/index.js
+++ src/applications/search/reducers/index.js
@@ -22,18 +22,21 @@ function SearchReducer(state = initialState, action) {
     case FETCH_SEARCH_RESULTS_SUCCESS: {
       const { query } = action.results;
       const {
-        previous: prevOffset,
-        next: nextOffset,
-      } = action.results.pagination;
-      const { results, total } = action.results.web;
+        currentPage,
+        perPage,
+        totalPages,
+        totalEntries,
+      } = action.meta.pagination;
+      const { results } = action.results.web;
 
       return {
         ...state,
         query,
         results,
-        total,
-        prevOffset,
-        nextOffset,
+        totalEntries,
+        currentPage,
+        perPage,
+        totalPages,
         loading: false,
       };
     }
