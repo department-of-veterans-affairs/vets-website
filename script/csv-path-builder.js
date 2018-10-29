5c2cce339ae6b5477f737259c4cd4f04148e3d99
Bill Tran <bill@billjohntran.com>
2018-10-29 15:02:03 -0700

    add conditional for specific link cases

--- script/csv-path-builder.js
+++ script/csv-path-builder.js
@@ -4,13 +4,6 @@ const cheerio = require('cheerio');
 const Json2csvParser = require('json2csv').Parser;
 
 const rootPath = 'build/localhost';
-const fields = [
-  'PAGE TITLE',
-  'LINK TEXT',
-  'TARGET TITLE',
-  'PAGE URL',
-  'TARGET URL',
-];
 
 function getFilesFromDir(dir, fileTypes) {
   const filesToReturn = [];
@@ -38,9 +31,85 @@ function getFilesFromDir(dir, fileTypes) {
 
 const fileList = getFilesFromDir(rootPath, ['.md', '.html']);
 
+const getTargetTitle = href => {
+  if (href) {
+    if (href.startsWith('/')) {
+      let filePath = `${rootPath}${href}/index.html`;
+
+      if (href === '/') {
+        filePath = `${rootPath}/index.html`;
+      }
+
+      if (href.includes('.pdf')) {
+        if (fs.existsSync(`${rootPath}${href}`)) {
+          return `This is a ${path.extname(href)} file`;
+        }
+
+        return 'Page does not exist';
+      }
+
+      if (href.includes('/introduction')) {
+        filePath = `${rootPath}${href.replace('/introduction', '')}/index.html`;
+        const fileContent = fs.readFileSync(filePath);
+        const data = fileContent.toString();
+
+        const $ = cheerio.load(data);
+
+        return $('title')
+          .text()
+          .trim();
+      }
+
+      if (fs.existsSync(filePath)) {
+        const fileContent = fs.readFileSync(filePath);
+        const data = fileContent.toString();
+
+        const $ = cheerio.load(data);
+
+        return $('title')
+          .text()
+          .trim();
+      }
+
+      if (filePath.includes('#')) {
+        filePath = `${rootPath}${href.split('/#')[0]}/index.html`;
+
+        if (fs.existsSync(filePath)) {
+          const fileContent = fs.readFileSync(filePath);
+          const data = fileContent.toString();
+          const $ = cheerio.load(data);
+
+          return $('title')
+            .text()
+            .trim();
+        }
+      }
+
+      return `Page does not exist`;
+    }
+
+    if (href.includes('localhost')) {
+      if (href.includes('.asp')) {
+        return 'External Link';
+      }
+
+      return href;
+    }
+
+    if (href.includes('http')) {
+      return 'External Link';
+    }
+  }
+
+  return false;
+};
+
 const fileLinks = fileList.reduce((acc, file) => {
   const fileContent = fs.readFileSync(`${rootPath}${file}`);
   const data = fileContent.toString();
+
+  // eslint-disable-next-line
+  console.log(`processing ${file}`);
 
   const $ = cheerio.load(data);
   const $linkTags = $('a');
@@ -49,25 +118,46 @@ const fileLinks = fileList.reduce((acc, file) => {
     .trim();
 
   const linkObjects = $linkTags.map(function updateItem() {
+    let linkText = $(this)
+      .text()
+      .trim();
+
+    if (
+      !$(this)
+        .text()
+        .trim() &&
+      $(this).children('img').length === 1
+    ) {
+      const src = $(this)
+        .children('img')
+        .attr('src');
+
+      if (src.includes('data:image')) {
+        linkText = 'image/png:base64';
+      } else {
+        linkText = src;
+      }
+    }
+
     return {
       'PAGE TITLE': pageTitle,
-      'LINK TEXT': $(this)
-        .text()
-        .trim(),
-      'TARGET TITLE': $(this)
-        .text()
-        .trim(),
-      'PAGE URL': file,
-      'TARGET URL': this.attribs.href,
+      'PAGE URL': file.replace('/index.html', '').replace('.html', ''),
+      'LINK TEXT': linkText,
+      'LINK URL': this.attribs.href ? this.attribs.href : '',
+      'TARGET TITLE':
+        getTargetTitle(this.attribs.href) ||
+        $(this)
+          .text()
+          .trim(),
     };
   });
 
   return [...acc, ...linkObjects.toArray()];
 }, []);
 
-const json2csvParser = new Json2csvParser({ fields });
+const json2csvParser = new Json2csvParser();
 const csv = json2csvParser.parse(fileLinks);
 
-fs.writeFileSync('./all-links.csv', csv);
-
-// console.log('Done generating your csv file');
+fs.writeFileSync(`./all-links.csv`, csv);
+// eslint-disable-next-line
+console.log('Done generating your all-links.csv file');
