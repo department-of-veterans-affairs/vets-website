259cb302ce01ed09e3a9cdfbcd782ffa0b8a5a99
Aaron LeCheminant <aaronjlech@gmail.com>
2018-10-29 10:33:17 -0400

    fix title for upload choice additional info

--- src/applications/disability-benefits/all-claims/content/ptsdWalkthroughChoice.jsx
+++ src/applications/disability-benefits/all-claims/content/ptsdWalkthroughChoice.jsx
@@ -3,18 +3,21 @@ import React from 'react';
 import AdditionalInfo from '@department-of-veterans-affairs/formation/AdditionalInfo';
 import { getPtsdClassification } from './ptsdClassification';
 
-export const PtsdUploadChoiceDescription = (
+export const PtsdUploadChoiceDescription = ({ formType }) => (
   <AdditionalInfo triggerText="What does this mean?">
     <h5>Continue answering questions</h5>
     <p>
       If you choose to answer questions, we’ll ask you several questions to
       learn more about your PTSD.
     </p>
-    <h5>Upload VA Form 21-0781</h5>
+    <h5>
+      Upload VA Form 21-0
+      {formType}
+    </h5>
     <p>
-      If you upload a completed VA Form 21-0781, we won’t ask you questions
-      about your PTSD, and you’ll move to the next section of the disability
-      application.
+      If you upload a completed VA Form 21-0
+      {formType}, we won’t ask you questions about your PTSD, and you’ll move to
+      the next section of the disability application.
     </p>
   </AdditionalInfo>
 );
@@ -24,7 +27,7 @@ const UploadExplanation = ({ formType }) => (
     <p>
       If you have already completed a Claim for Service Connection for
       Post-Traumatic Stress Disorder (VA Form 21-0
-      {`${formType}`}
+      {formType}
       ), you can upload it here instead of answering the questions about your
       PTSD.
     </p>
