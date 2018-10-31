d90b0501cf2faf308565ca62be8a9ae534670ae9
Nick Sullivan <ncksllvn@gmail.com>
2018-10-31 09:49:47 -0400

    Create the Brand Consolidation announcement dialog (#8829)
    * Create the brand-consolidation modal
    
    * add the graphic
    
    * update announcement name
    
    * Add the logo into the component
    
    * Add the redirect test page
    
    * change file extension
    
    * Rename the announcment
    
    * add dummy redirect page
    
    * incorporate document.referrer
    
    * Linter
    
    * Add test code
    
    * Show the dialog every time
    
    * Change prop name to showEverytime
    
    * Remove pointless class name
    
    * Add a search query to force the modal on lower environments
    
    * Linter
    
    * Delete test file
    
    * Tinify the image
    
    * Revert "Delete test file"
    
    This reverts commit 4ad3936f3bc4e857e34678117bbb43631cec344a.
    
    * Add staging test
    
    * PR comments
    
    * Clean up function names and remove weird unit test

--- src/platform/site-wide/announcements/actions/index.js
+++ src/platform/site-wide/announcements/actions/index.js
@@ -5,35 +5,46 @@ export const DISMISS_ANNOUNCEMENT = 'DISMISS_ANNOUNCEMENT';
 
 export const ANNOUNCEMENTS_LOCAL_STORAGE = 'DISMISSED_ANNOUNCEMENTS';
 
-function localAnnouncements(dismissedAnnouncementName) {
-  const fromLocalStorage = localStorage.getItem(ANNOUNCEMENTS_LOCAL_STORAGE);
+const previouslyDismissedAnnouncements = (() => {
   let parsed = [];
 
-  if (fromLocalStorage) {
-    try {
-      parsed = JSON.parse(fromLocalStorage);
-    } catch (err) {
-      // Value will default to an empty array
-    }
-  }
+  return {
+    initializeFromLocalStorage() {
+      const fromLocalStorage = localStorage.getItem(
+        ANNOUNCEMENTS_LOCAL_STORAGE,
+      );
+      if (fromLocalStorage) {
+        try {
+          parsed = JSON.parse(fromLocalStorage);
+        } catch (err) {
+          // Value will default to an empty array
+        }
+      }
+      return parsed;
+    },
 
-  if (dismissedAnnouncementName) {
-    parsed.push(dismissedAnnouncementName);
-    localStorage.setItem(ANNOUNCEMENTS_LOCAL_STORAGE, JSON.stringify(parsed));
-  }
+    getAll() {
+      return parsed;
+    },
 
-  return parsed;
-}
+    save(dismissedAnnouncementName) {
+      parsed.push(dismissedAnnouncementName);
+      localStorage.setItem(ANNOUNCEMENTS_LOCAL_STORAGE, JSON.stringify(parsed));
+    },
+  };
+})();
 
 export function initDismissedAnnouncements() {
+  previouslyDismissedAnnouncements.initializeFromLocalStorage();
   return {
     type: INIT_DISMISSED_ANNOUNCEMENTS,
-    dismissedAnnouncements: localAnnouncements(),
+    dismissedAnnouncements: previouslyDismissedAnnouncements.getAll(),
   };
 }
 
-export function dismissAnnouncement(announcement) {
-  localAnnouncements(announcement);
+export function dismissAnnouncement(announcement, showEverytime) {
+  if (!showEverytime) previouslyDismissedAnnouncements.save(announcement);
+
   return {
     type: DISMISS_ANNOUNCEMENT,
     announcement,
