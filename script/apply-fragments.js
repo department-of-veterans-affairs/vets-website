9f98c7568e2af6096452326f20dc5bebd2416420
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-30 22:09:21 -0400

    Add some comment documentation

--- script/apply-fragments.js
+++ script/apply-fragments.js
@@ -1,5 +1,38 @@
 /* eslint-disable no-continue */
 
+/**
+ * Files can contain a "fragments" property in the front matter, which can be
+ * used to read a YAML file into a property name.
+ *
+ * For example, if "index.md" contains front matter:
+ *
+ * ------
+ * title: Home
+ * banner:
+ *   visible: true
+ *   type: warning
+ *   title: Some VA.gov tools and features may not be working as expected
+ *   content: Some content
+ * ------
+ *
+ * You could break out the "banner" property into a separate file via fragments.
+ * index.md could be rewritten with fragments like:
+ *
+ * ------
+ * title: Home
+ * fragments:
+ *   banner: home/banner
+ * ------
+ *
+ * Then, in "{fragments_root}/home/banner.yml", you could store the banner data -
+ *
+ * visible: true
+ * type: warning
+ * title: Some VA.gov tools and features may not be working as expected
+ * content: Some content
+ *
+ */
+
 const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
