074d55329a3d67326fb23e3bfb695686f847a963
Edward Paget <edward@cassetteta.pe>
2018-11-01 14:26:13 -0400

    Implement actions for connected-accounts app
    Refactor into smaller components as well.

---
+++ src/applications/personalization/connected-accounts/actions/index.js
@@ -0,0 +1,41 @@
+import { apiRequest } from '../../../../platform/utilities/api';
+
+export * from '../../../../platform/user/profile/actions';
+
+export const LOADING_CONNECTED_ACCOUNTS = 'LOADING_CONNECTED_ACCOUNTS';
+export const FINISHED_CONNECTED_ACCOUNTS = 'FINISHED_CONNECTED_ACCOUNTS';
+export const ERROR_CONNECTED_ACCOUNTS = 'ERROR_CONNECTED_ACCOUNTS';
+export const DELETING_CONNECTED_ACCOUNT = 'DELETING_CONNECTED_ACCOUNT';
+export const ERROR_DELETING_CONNECTED_ACCOUNT =
+  'ERROR_DELETING_CONNECTED_ACCOUNT';
+export const FINISHED_DELETING_CONNECTED_ACCOUNT =
+  'FINISHED_DELETING_CONNECTED_ACCOUNT';
+
+const grantsUrl = '/profile/connected_applications';
+
+export function loadConnectedAccounts() {
+  return async dispatch => {
+    dispatch({ type: 'LOADING_CONNECTED_ACCOUNTS' });
+
+    return apiRequest(
+      grantsUrl,
+      null,
+      ({ data }) => dispatch({ type: FINISHED_CONNECTED_ACCOUNTS, data }),
+      ({ errors }) => dispatch({ type: ERROR_CONNECTED_ACCOUNTS, errors }),
+    );
+  };
+}
+
+export function deleteConnectedAccount(accountId) {
+  return async dispatch => {
+    dispatch({ type: DELETING_CONNECTED_ACCOUNT, accountId });
+
+    return apiRequest(
+      `${grantsUrl}/${accountId}`,
+      { method: 'DELETE' },
+      () => dispatch({ type: FINISHED_DELETING_CONNECTED_ACCOUNT, accountId }),
+      ({ errors }) =>
+        dispatch({ type: ERROR_DELETING_CONNECTED_ACCOUNT, errors }),
+    );
+  };
+}
---
+++ src/applications/personalization/connected-accounts/components/NoConnectedApps.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import recordEvent from '../../../../platform/monitoring/record-event';
+
+const recordAction = () => {
+  recordEvent({
+    event: 'account-navigation',
+    'account-action': 'view-link',
+    'account-section': 'vets-faqs',
+  });
+};
+
+export function NoConnectedApps({ errors, propertyName }) {
+  let title;
+  if (errors.length > 0) {
+    title = <h3>Something went wrong loading your connected Accounts.</h3>;
+  } else {
+    title = <h3>You do not have any connected accounts.</h3>;
+  }
+
+  return (
+    <div className="row va-connected-acct-null">
+      <div className="usa-width-two-thirds medium-8 small-12 columns">
+        {title}
+        <div className="feature">
+          <h3>Have questions about signing in to {propertyName}?</h3>
+          <p>
+            Get answers to frequently asked questions about how to sign in,
+            common issues with verifying your identity, and your privacy and
+            security on {propertyName}.
+          </p>
+
+          <a href="/faq" onClick={recordAction}>
+            Go to {propertyName} FAQs
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
