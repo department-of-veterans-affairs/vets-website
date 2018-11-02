5106839176625c0b5acdf5d14fda14fb738a4ad4
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2018-11-02 10:52:49 -0400

    Removed last bits of conditionalStorage.

--- src/platform/site-wide/user-nav/selectors.js
+++ src/platform/site-wide/user-nav/selectors.js
@@ -1,13 +1,13 @@
 import { createSelector } from 'reselect';
 import { startCase, toLower } from 'lodash';
 
-import conditionalStorage from '../../utilities/storage/conditionalStorage';
+import localStorage from '../../utilities/storage/localStorage';
 import { selectProfile } from '../../user/selectors';
 
 export const selectUserGreeting = createSelector(
   state => selectProfile(state).userFullName,
   state => selectProfile(state).email,
-  () => conditionalStorage().getItem('userFirstName'),
+  () => localStorage.getItem('userFirstName'),
   (name, email, sessionFirstName) => {
     if (name.first || sessionFirstName) {
       return startCase(toLower(name.first || sessionFirstName));
