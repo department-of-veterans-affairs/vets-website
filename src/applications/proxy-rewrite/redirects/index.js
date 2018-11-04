d93266c1f124ef63cafe67fb351472c6d83248a7
Jeff Balboni <jeff@adhocteam.us>
2018-11-04 14:59:29 -0500

    Enable disability redirects

--- src/applications/proxy-rewrite/redirects/index.js
+++ src/applications/proxy-rewrite/redirects/index.js
@@ -1,7 +1,9 @@
 import environment from '../../../platform/utilities/environment';
 // These pages are old va.gov pages that we are redirecting to
 // pages that we control
-import redirects from './otherDomainRedirects.json';
+// import redirects from './otherDomainRedirects.json';
+// Currently we only have approval for disability related ones:
+import redirects from './disabilityRedirects.json';
 
 /*
  * Redirect to a www.va.gov page if we're on a page that's being
