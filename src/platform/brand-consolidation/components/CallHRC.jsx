5c47af85abf50b603c535453db22a99f88975b5f
Jeff Balboni <jeff@adhocteam.us>
2018-11-05 14:36:39 -0500

    Adjust text

--- src/platform/brand-consolidation/components/CallHRC.jsx
+++ src/platform/brand-consolidation/components/CallHRC.jsx
@@ -8,7 +8,7 @@ export default function CallHRC({ children, startSentence }) {
 
   return (
     <span>
-      {startSentence ? 'Call' : 'call'} HRC for help:{' '}
+      {startSentence ? 'Call' : 'call'} us at{' '}
       <a href="tel:18555747286">1-855-574-7286</a>. If you have hearing loss,
       call TTY: 711.
     </span>
