704b4ff8bc97bb3546fa941d5710301600a208eb
Erik P. Hansen <eph@erikphansen.com>
2018-11-05 11:28:18 -0800

    Show Welcome modal: only on homepage, only once

---
+++ src/platform/site-wide/announcements/components/WelcomeToNewVAModal.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Modal from '@department-of-veterans-affairs/formation/Modal';
+import brandConsolidation from '../../../brand-consolidation';
+
+export default class WelcomeToNewVAModal extends React.Component {
+  static isEnabled() {
+    if (!brandConsolidation.isEnabled()) return false;
+
+    if (__BUILDTYPE__ === 'preview') return false;
+
+    return true;
+  }
+
+  render() {
+    const { dismiss } = this.props;
+
+    return (
+      <Modal
+        cssClass="va-modal announcement-brand-consolidation"
+        visible
+        onClose={dismiss}
+        id="modal-announcement"
+      >
+        <div className="announcement-heading-brand-consolidation">
+          <img
+            className="announcement-brand-consolidation-logo"
+            src="/img/design/logo/va-plus-vets.png"
+            alt="VA.gov plus Vets.gov"
+          />
+        </div>
+        <h3 className="announcement-title">
+          Welcome to the New VA.gov — Built with Veterans, for Veterans
+        </h3>
+        <p>
+          Our new site offers one place to access all VA benefits and health
+          care services. You can sign in with your My HealtheVet, DS Logon, or
+          ID.me account to track your claims, refill your prescriptions, and
+          more.
+        </p>
+        <button type="button" onClick={dismiss}>
+          Continue to the new VA.gov
+        </button>
+      </Modal>
+    );
+  }
+}
