1990af611b542880494b5b754364d5bf044c2e88
592176 <imeecuison@C02T50D8H03Q.local>
2018-11-06 14:29:37 -0500

    full medical dates and info

--- src/applications/disability-benefits/all-claims/pages/hospitalizationHistory.js
+++ src/applications/disability-benefits/all-claims/pages/hospitalizationHistory.js
@@ -1,20 +1,41 @@
-// import dateRangeUI from 'us-forms-system/lib/js/definitions/dateRange';
-// import HospitalizationPeriodView from '../components/HospitalizationPeriodView';
-//
-// import fullSchema from '../config/schema';
+import dateRangeUI from 'us-forms-system/lib/js/definitions/dateRange';
+
+import HospitalizationPeriodView from '../components/HospitalizationPeriodView';
 
 export const uiSchema = {
   hospitalizationHistory: {
-    // items: {
-    hospitalName: {
-      // 'ui:options': {
-      //   itemName: 'Provider',
-      //   viewField: HospitalizationPeriodView,
-      //   hideTitle: true,
-      // },
-      'ui:title': 'Name of hospital',
+    'ui:title': 'Hospitalization',
+    'ui:description': 'Dates you were hospitalized?',
+    'ui:options': {
+      itemName: 'Hospital',
+      viewField: HospitalizationPeriodView,
+      hideTitle: true,
     },
-    // },
+    items: {
+      hospitalizationtDateRange: dateRangeUI(
+        'From',
+        'To',
+        'End of hospitalization must be after start of treatment',
+      ),
+      hospitalName: {
+        'ui:title': 'Name of hospital',
+      },
+      streetAddress: {
+        'ui:title': 'Street address',
+      },
+      streetAddress2: {
+        'ui:title': 'Street address (optional)',
+      },
+      city: {
+        'ui:title': 'City',
+      },
+      state: {
+        'ui:title': 'State',
+      },
+      zip: {
+        'ui:title': 'ZIP',
+      },
+    },
   },
 };
 
@@ -22,15 +43,41 @@ export const schema = {
   type: 'object',
   properties: {
     hospitalizationHistory: {
-      // type: 'array',
-      // items: {
-      type: 'object',
-      properties: {
-        hospitalName: {
-          type: 'string',
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          hospitalizationDateRange: {
+            type: 'object',
+            properties: {
+              from: {
+                $ref: '#/definitions/date',
+              },
+              to: {
+                $ref: '#/definitions/date',
+              },
+            },
+          },
+          hospitalName: {
+            type: 'string',
+          },
+          streetAddress: {
+            type: 'string',
+          },
+          streetAddress2: {
+            type: 'string',
+          },
+          city: {
+            type: 'string',
+          },
+          state: {
+            type: 'string',
+          },
+          zip: {
+            type: 'string',
+          },
         },
       },
-      // },
     },
   },
 };
