edd3b43875fdd962fd653967555f8999c3d13d24
Mark Greenburg <marek.greenburg@gmail.com>
2018-11-08 10:19:13 -0600

    Remove unit address and unit phone from reserves schema

--- src/applications/disability-benefits/all-claims/config/schema.js
+++ src/applications/disability-benefits/all-claims/config/schema.js
@@ -724,22 +724,11 @@ const schema = {
         },
         reservesNationalGuardService: {
           type: 'object',
-          required: [
-            'unitName',
-            'obligationTermOfServiceDateRange',
-            'waiveVABenefitsToRetainTrainingPay',
-          ],
           properties: {
             unitName: {
               type: 'string',
               maxLength: 256,
               pattern: "^([a-zA-Z0-9\\-'.#][a-zA-Z0-9\\-'.# ]?)*$",
-            },
-            unitAddress: {
-              $ref: '#/definitions/address',
-            },
-            unitPhone: {
-              $ref: '#/definitions/phone',
             },
             obligationTermOfServiceDateRange: {
               $ref: '#/definitions/dateRangeAllRequired',
