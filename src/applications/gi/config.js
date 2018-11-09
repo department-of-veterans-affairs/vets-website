d853dd9ca8362326c49a1c3989f9b252cc861e43
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2018-11-09 13:50:55 -0500

    Include credentials for GIBCT.

--- src/applications/gi/config.js
+++ src/applications/gi/config.js
@@ -3,6 +3,7 @@ import environment from '../../platform/utilities/environment';
 export const api = {
   url: `${environment.API_URL}/v0/gi`,
   settings: {
+    credentials: 'include',
     headers: {
       'X-Key-Inflection': 'camel',
     },
