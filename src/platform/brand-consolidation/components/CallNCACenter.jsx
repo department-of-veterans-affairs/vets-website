4990ddf531f10b56fba246f78429cbe4ea8a9e68
Jeff Balboni <jeff@adhocteam.us>
2018-11-14 13:49:54 -0500

    PR comments

--- src/platform/brand-consolidation/components/CallNCACenter.jsx
+++ src/platform/brand-consolidation/components/CallNCACenter.jsx
@@ -8,8 +8,8 @@ export default function CallNCACenter({ children, startSentence }) {
 
   return (
     <span>
-      {startSentence ? 'Call' : 'call'} the National Cemetery Scheduling Office:{' '}
-      <a href="tel:18005351117">1-800-535-1117</a>.<br /> If you have hearing
+      {startSentence ? 'Call' : 'call'} the National Cemetery Scheduling Office
+      at <a href="tel:18005351117">1-800-535-1117</a>.<br /> If you have hearing
       loss, call TTY: 711.
     </span>
   );
