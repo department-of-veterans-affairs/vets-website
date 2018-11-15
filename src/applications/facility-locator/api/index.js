department-of-veterans-affairs/vets-website

commit 46725d54feeb5cc3d5cc42b60012126278ac9d08

Tear down Brand Consolidation feature flag from build script (#9095)
* Separate vets.gov environments into its own build step

* Flatten Heroku logic

* Combine default w/BC overrides

* Delete deprecated collection

* Flatten out robots.txt

* Fix missing variable

* Clean up messy conditionals

* Fix build error

* Remove redundant conditionals

* Move hostUrl to its own fn

* Hardcode bcEnabled to true

* Remove weird sanity arg

* Update yarn in engine prop

* Yarn install

* Set mergedbuild to true

* Remove merged build

* Reconfigure watch task

* Clean up for Heroku hostUrl

* Fix for checking valid env

* Default servers to vagovdev

* Change references to development to instead use localhost

* Revert "Change references to development to instead use localhost"

This reverts commit 9afc0f7ea45406f8f157da43db413199d1dc7de3.

* Revert "Revert "Change references to development to instead use localhost""

This reverts commit 7533d67fc76cb5bda91783df6ffbd832817d23b0.

* Add vagovdev into analytics dev

* Apply default mocha buildtype to be localhost

* Undo changes to content file

* Default mocha to vagovdev

* Fix for dynamic base URLs

* Fix for confusing Nightwatch E2E tests

* Revert "Update yarn in engine prop"

This reverts commit 9e860f35772489399a4930007171184000049760.

* Yarn install

* Remove list of Vets.gov links ignored by link checker

* Add localhost into hostnames for consistency; don't autoexecute options

src/applications/facility-locator/api/index.js
@@ -3,4 +3,4 @@ import MockApi from './MockLocatorApi';
 import LiveApi from './LocatorApi';
 
 /* global __BUILDTYPE__ */
-export default (__BUILDTYPE__ === 'development') ? MockApi : LiveApi;
+export default (__BUILDTYPE__ === 'localhost') ? MockApi : LiveApi;

script/vets-gov-to-va-gov/index.js
@@ -6,6 +6,7 @@ const yaml = require('js-yaml');
 
 const environments = require('../constants/environments');
 const hostnames = require('../constants/hostnames');
+const getOptions = require('../options');
 
 const createStandardRedirectHtml = require('./standard-redirect');
 const createAppRedirectHtml = require('./app-redirect');
@@ -68,4 +69,9 @@ function generateRedirectedPages(BUILD_OPTIONS) {
   );
 }
 
-module.exports = generateRedirectedPages;
+function main() {
+  const options = getOptions();
+  generateRedirectedPages(options);
+}
+
+main();

src/platform/utilities/environment/index.js
@@ -1,12 +1,4 @@
 const _Environments = {
-  production: {
-    API_URL: 'https://api.vets.gov',
-    BASE_URL: 'https://www.vets.gov',
-  },
-  staging: {
-    API_URL: 'https://staging-api.vets.gov',
-    BASE_URL: 'https://staging.vets.gov',
-  },
   preview: {
     API_URL: 'https://api.va.gov',
     BASE_URL: 'https://preview.va.gov',
@@ -23,11 +15,7 @@ const _Environments = {
     API_URL: 'https://dev-api.va.gov',
     BASE_URL: 'https://dev.va.gov',
   },
-  development: {
-    API_URL: process.env.API_URL || 'https://dev-api.vets.gov',
-    BASE_URL: process.env.BASE_URL || 'https://dev.vets.gov',
-  },
-  local: {
+  localhost: {
     API_URL: `http://${location.hostname}:3000`,
     BASE_URL: `http://${location.hostname}:3001`,
   },
@@ -41,7 +29,7 @@ function getEnvironment() {
   let platform;
 
   if (location.port === '3001') {
-    platform = 'local';
+    platform = 'localhost';
   } else if (location.port === `${process.env.WEB_PORT || 3333}`) {
     platform = 'e2e';
   } else {

Jenkinsfile
@@ -1,10 +1,46 @@
 import org.kohsuke.github.GitHub
 
-def getIsCMSDeploy(ref) {
-  // check to see if this ref has already been built
-  // if it has then we are doing a cms deployment, not a full pipeline
-  return sh (script: "aws s3 ls s3://vetsgov-website-builds-s3-upload/${ref}/",
-             returnStatus: true) == 0
+env.CONCURRENCY = 10
+
+VETSGOV_BUILDTYPES = [
+  'development',
+  'staging',
+  'production'
+]
+
+VAGOV_BUILDTYPES = [
+  'preview',
+  'vagovdev',
+  'vagovstaging',
+  'vagovprod'
+]
+
+DEV_BRANCH = 'master'
+STAGING_BRANCH = 'master'
+PROD_BRANCH = 'master'
+
+IS_DEV_BRANCH = env.BRANCH_NAME == DEV_BRANCH
+IS_STAGING_BRANCH = env.BRANCH_NAME == STAGING_BRANCH
+IS_PROD_BRANCH = env.BRANCH_NAME == PROD_BRANCH
+
+def isReviewable = {
+  !IS_DEV_BRANCH && !IS_STAGING_BRANCH && !IS_PROD_BRANCH
+}
+
+def isDeployable = {
+  (IS_DEV_BRANCH ||
+   IS_STAGING_BRANCH) &&
+    !env.CHANGE_TARGET &&
+    !currentBuild.nextBuild // if there's a later build on this job (branch), don't deploy
+}
+
+def shouldBail = {
+  // abort the job if we're not on deployable branch (usually master) and there's a newer build going now
+  !IS_DEV_BRANCH &&
+  !IS_STAGING_BRANCH &&
+  !IS_PROD_BRANCH &&
+  !env.CHANGE_TARGET &&
+  currentBuild.nextBuild
 }
 
 def runDeploy(jobName, ref) {
@@ -12,58 +48,6 @@ def runDeploy(jobName, ref) {
     booleanParam(name: 'notify_slack', value: true),
     stringParam(name: 'ref', value: ref),
   ], wait: false
-}
-
-def isCMSDeploy = false
-
-def getEnvNames(isCMSDeploy) {
-  def vetsgovEnvNames = [
-    'development', 'staging', 'production'
-  ]
-  def vagovEnvNames = [
-    'preview', 'vagovdev', 'vagovstaging', 'vagovprod'
-  ]
-
-  if (isCMSDeploy) {
-    // only return va.gov envs
-    return vagovEnvNames
-  }
-  return vetsgovEnvNames + vagovEnvNames
-}
-
-def envNames = [
-  // Vets.gov envs
-  'development', 'staging', 'production',
-  // VA.gov envs
-  'preview', 'vagovdev', 'vagovstaging', 'vagovprod'
-]
-
-def devBranch = 'master'
-def stagingBranch = 'master'
-def prodBranch = 'master'
-
-def isReviewable = {
-  env.BRANCH_NAME != devBranch &&
-    env.BRANCH_NAME != stagingBranch &&
-    env.BRANCH_NAME != prodBranch
-}
-
-env.CONCURRENCY = 10
-
-def isDeployable = {
-  (env.BRANCH_NAME == devBranch ||
-   env.BRANCH_NAME == stagingBranch) &&
-    !env.CHANGE_TARGET &&
-    !currentBuild.nextBuild // if there's a later build on this job (branch), don't deploy
-}
-
-def shouldBail = {
-  // abort the job if we're not on deployable branch (usually master) and there's a newer build going now
-  env.BRANCH_NAME != devBranch &&
-  env.BRANCH_NAME != stagingBranch &&
-  env.BRANCH_NAME != prodBranch &&
-  !env.CHANGE_TARGET &&
-  currentBuild.nextBuild
 }
 
 def buildDetails = { vars ->
@@ -79,35 +63,12 @@ def buildDetails = { vars ->
 }
 
 def notify = { ->
-  if (env.BRANCH_NAME == devBranch ||
-      env.BRANCH_NAME == stagingBranch ||
-      env.BRANCH_NAME == prodBranch) {
+  if (IS_DEV_BRANCH || IS_STAGING_BRANCH || IS_PROD_BRANCH) {
     message = "vets-website ${env.BRANCH_NAME} branch CI failed. |${env.RUN_DISPLAY_URL}".stripMargin()
     slackSend message: message,
     color: 'danger',
     failOnError: true
   }
-}
-
-def comment_broken_links = {
-  // Pull down console log replacing URL with IP since we can't hit internal DNS
-  sh "curl -O \$(echo ${env.BUILD_URL} | sed 's/jenkins.vetsgov-internal/172.31.1.100/')consoleText"
-
-  // Find all lines with broken links in production build
-  def broken_links = sh (
-    script: 'grep -o \'\\[production\\].*>>> href: ".*",\' consoleText',
-    returnStdout: true
-  ).trim()
-
-  def github = GitHub.connect()
-  def repo = github.getRepository('department-of-veterans-affairs/vets-website')
-  def pr = repo.queryPullRequests().head("department-of-veterans-affairs:${env.BRANCH_NAME}").list().asList().get(0)
-
-
-  // Post our comment with broken links formatted as a Markdown table
-  pr.comment("### Broken links found by Jenkins\n\n|File| Link URL to be fixed|\n|--|--|\n" +
-             broken_links.replaceAll(/\[production\] |>>> href: |,/,"|") +
-             "\n\n _Note: Long file names or URLs may be cut-off_")
 }
 
 node('vetsgov-general-purpose') {
@@ -131,8 +92,6 @@ node('vetsgov-general-purpose') {
 
       dir("vets-website") {
         ref = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
-        isCMSDeploy = false // getIsCMSDeploy(ref)
-        envNames = getEnvNames(isCMSDeploy)
 
         sh "mkdir -p build"
         sh "mkdir -p logs/selenium"
@@ -154,7 +113,6 @@ node('vetsgov-general-purpose') {
   }
 
   stage('Lint|Security|Unit') {
-    if (isCMSDeploy) { return }
     try {
       parallel (
         lint: {
@@ -189,25 +147,17 @@ node('vetsgov-general-purpose') {
     }
   }
 
-  // Perform a build for each build type
-
-  stage('Build') {
+  stage('Build: Redirects') {
     if (shouldBail()) { return }
 
     try {
       def builds = [:]
 
-      for (int i=0; i<envNames.size(); i++) {
-        def envName = envNames.get(i)
-
-        def cmsDeployArg = ""
-        if (isCMSDeploy) {
-          cmsDeployArg = "--content-deployment"
-        }
-
+      for (int i=0; i<VETSGOV_BUILDTYPES.size(); i++) {
+        def envName = VETSGOV_BUILDTYPES.get(i)
         builds[envName] = {
           dockerImage.inside(args) {
-            sh "cd /application && npm --no-color run build -- ${cmsDeployArg} --buildtype=${envName}"
+            sh "cd /application && npm --no-color run build:redirects -- --buildtype=${envName}"
             sh "cd /application && echo \"${buildDetails('buildtype': envName, 'ref': ref)}\" > build/${envName}/BUILD.txt"
           }
         }
@@ -216,18 +166,38 @@ node('vetsgov-general-purpose') {
       parallel builds
     } catch (error) {
       notify()
+      throw error
+    }
+  }
 
-      // For content team PRs, add comment in GH so they don't need direct Jenkins access to find broken links
-      if (env.BRANCH_NAME.startsWith("content")) {
-        comment_broken_links()
+  // Perform a build for each build type
+
+  stage('Build') {
+    if (shouldBail()) { return }
+
+    try {
+      def builds = [:]
+
+      for (int i=0; i<VAGOV_BUILDTYPES.size(); i++) {
+        def envName = VAGOV_BUILDTYPES.get(i)
+        builds[envName] = {
+          dockerImage.inside(args) {
+            sh "cd /application && npm --no-color run build -- --buildtype=${envName}"
+            sh "cd /application && echo \"${buildDetails('buildtype': envName, 'ref': ref)}\" > build/${envName}/BUILD.txt"
+          }
+        }
       }
+
+      parallel builds
+    } catch (error) {
+      notify()
       throw error
     }
   }
 
   // Run E2E and accessibility tests
   stage('Integration') {
-    if (shouldBail() || isCMSDeploy) { return }
+    if (shouldBail()) { return }
     dir("vets-website") {
       try {
         parallel (
@@ -253,18 +223,11 @@ node('vetsgov-general-purpose') {
   stage('Pre-archive optimizations') {
     if (shouldBail()) { return }
 
-    def optimizationEnvironments = [
-      'vagovdev',
-      'vagovstaging',
-      'vagovprod',
-      'preview',
-    ]
-
     try {
       def builds = [:]
 
-      for (int i=0; i<optimizationEnvironments.size(); i++) {
-        def envName = optimizationEnvironments.get(i)
+      for (int i=0; i<VAGOV_BUILDTYPES.size(); i++) {
+        def envName = VAGOV_BUILDTYPES.get(i)
 
         builds[envName] = {
           dockerImage.inside(args) {
@@ -284,6 +247,7 @@ node('vetsgov-general-purpose') {
   stage('Archive') {
     if (shouldBail()) { return }
 
+    def envNames = VETSGOV_BUILDTYPES + VAGOV_BUILDTYPES
     try {
       dockerImage.inside(args) {
         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'vetsgov-website-builds-s3-upload',
@@ -332,17 +296,13 @@ node('vetsgov-general-purpose') {
         }
       }
 
-      if (env.BRANCH_NAME == devBranch) {
-        if (!isCMSDeploy) {
-          runDeploy('deploys/vets-website-dev', commit)
-        }
+      if (IS_DEV_BRANCH) {
+        runDeploy('deploys/vets-website-dev', commit)
         runDeploy('deploys/vets-website-vagovdev', commit)
       }
 
-      if (env.BRANCH_NAME == stagingBranch) {
-        if (!isCMSDeploy) {
-          runDeploy('deploys/vets-website-staging', commit)
-        }
+      if (IS_STAGING_BRANCH) {
+        runDeploy('deploys/vets-website-staging', commit)
         runDeploy('deploys/vets-website-vagovstaging', commit)
       }
     } catch (error) {

config/webpack.config.js
@@ -35,6 +35,7 @@ const globalEntryFiles = {
 };
 
 const configGenerator = (options, apps) => {
+  const isDev = ['localhost', 'vagovdev'].includes(options.buildtype);
   const entryFiles = Object.assign({}, apps, globalEntryFiles);
   const baseConfig = {
     mode: 'development',
@@ -42,10 +43,10 @@ const configGenerator = (options, apps) => {
     output: {
       path: `${options.destination}/generated`,
       publicPath: '/generated/',
-      filename: ['development', 'vagovdev'].includes(options.buildtype)
+      filename: isDev
         ? '[name].entry.js'
         : `[name].entry.[chunkhash]-${timestamp}.js`,
-      chunkFilename: ['development', 'vagovdev'].includes(options.buildtype)
+      chunkFilename: isDev
         ? '[name].entry.js'
         : `[name].entry.[chunkhash]-${timestamp}.js`,
     },
@@ -188,7 +189,7 @@ const configGenerator = (options, apps) => {
       }),
 
       new ExtractTextPlugin({
-        filename: ['development', 'vagovdev'].includes(options.buildtype)
+        filename: isDev
           ? '[name].css'
           : `[name].[contenthash]-${timestamp}.css`,
       }),

script/build.js
@@ -9,11 +9,10 @@ const layouts = require('metalsmith-layouts');
 const liquid = require('tinyliquid');
 const markdown = require('metalsmith-markdownit');
 const moment = require('moment');
-const moveRemove = require('metalsmith-move-remove');
 const navigation = require('metalsmith-navigation');
 const permalinks = require('metalsmith-permalinks');
 
-const generateRedirectedPages = require('./vets-gov-to-va-gov');
+const getOptions = require('./options');
 const createBuildSettings = require('./create-build-settings');
 const createRedirects = require('./create-redirects');
 const createSitemaps = require('./create-sitemaps');
@@ -42,7 +41,6 @@ function defaultBuild(BUILD_OPTIONS) {
   smith.metadata({
     buildtype: BUILD_OPTIONS.buildtype,
     hostUrl: BUILD_OPTIONS.hostUrl,
-    mergedbuild: !!BUILD_OPTIONS['brand-consolidation-enabled'], // @deprecated - We use a separate Metalsmith directory for VA.gov. We shouldn't ever need this info in Metalsmith files.
   });
 
   smith.use(createEnvironmentFilter(BUILD_OPTIONS));
@@ -52,18 +50,12 @@ function defaultBuild(BUILD_OPTIONS) {
   // plugin chain.
   smith.use(filenames());
 
-  if (BUILD_OPTIONS.contentFragments) {
-    smith.use(applyFragments(BUILD_OPTIONS));
-  }
-
+  smith.use(applyFragments(BUILD_OPTIONS));
   smith.use(collections(BUILD_OPTIONS.collections));
   smith.use(leftRailNavResetLevels());
   smith.use(dateInFilename(true));
-
   smith.use(assets(BUILD_OPTIONS.appAssets));
-  if (BUILD_OPTIONS.contentAssets) {
-    smith.use(assets(BUILD_OPTIONS.contentAssets));
-  }
+  smith.use(assets(BUILD_OPTIONS.contentAssets));
 
   // smith.use(cspHash({ pattern: ['js/*.js', 'generated/*.css', 'generated/*.js'] }))
 
@@ -154,7 +146,6 @@ function defaultBuild(BUILD_OPTIONS) {
   smith.use(createSitemaps(BUILD_OPTIONS));
   smith.use(createRedirects(BUILD_OPTIONS));
   smith.use(checkBrokenLinks(BUILD_OPTIONS));
-  smith.use(moveRemove(BUILD_OPTIONS));
 
   /* eslint-disable no-console */
   smith.build(err => {
@@ -168,12 +159,8 @@ function defaultBuild(BUILD_OPTIONS) {
 }
 
 function main() {
-  const BUILD_OPTIONS = require('./options');
-  if (BUILD_OPTIONS['vets-gov-to-va-gov']) {
-    generateRedirectedPages(BUILD_OPTIONS);
-  } else {
-    defaultBuild(BUILD_OPTIONS);
-  }
+  const buildOptions = getOptions();
+  defaultBuild(buildOptions);
 }
 
 main();

script/check-broken-links.js
@@ -2,20 +2,6 @@
 const createBrokenLinkChecker = require('metalsmith-broken-link-checker');
 const cheerio = require('cheerio');
 
-const pagesOutsideOfContent = [
-  // These pages live outside of the content directory but are local links, so
-  // they should be explicitly skipped.
-  '/employment/job-seekers/skills-translator',
-  '/employment/job-seekers/create-resume',
-  '/employment/employers',
-
-  // At this time, we still have link-references to this page, which no longer exists.
-  // We should updated all reference instead to /education/apply/, but it's probably not
-  // worth it during the brand-consolidation effort where pages are already moving.
-  // This is a valid broken link though, so it should be addressed or removed soon.
-  '/education/apply-for-education-benefits',
-];
-
 const lazyImageFiles = [];
 
 function addLazySrcAttribute(file, fileName) {
@@ -45,7 +31,7 @@ function removeLazySrcAttribute(files) {
 
 function checkBrokenLinks() {
   return (files, metalsmith, done) => {
-    const ignorePaths = [...pagesOutsideOfContent];
+    const ignorePaths = [];
 
     for (const fileName of Object.keys(files)) {
       const file = files[fileName];

script/configure-assets.js
@@ -8,7 +8,7 @@ const addAssetHashes = require('./add-asset-hashes');
 
 function configureAssets(smith, buildOptions) {
   const isContentDeployment = buildOptions['content-deployment'];
-  const isDevBuild = [environments.DEVELOPMENT, environments.VAGOVDEV].includes(
+  const isDevBuild = [environments.LOCALHOST, environments.VAGOVDEV].includes(
     buildOptions.buildtype,
   );
 

commit bb30d7ddbc6752fdba4db0104067456ddb3677f3

Fixing Prettier formatting problems introduced in #9095

src/applications/facility-locator/api/index.js
@@ -2,4 +2,4 @@ import MockApi from './MockLocatorApi';
 import LiveApi from './LocatorApi';
 
 /* global __BUILDTYPE__ */
-export default (__BUILDTYPE__ === 'localhost') ? MockApi : LiveApi;
+export default (__BUILDTYPE__ === 'localhost' ? MockApi : LiveApi);

src/applications/facility-locator/containers/FacilityDetail.jsx
@@ -33,14 +33,15 @@ class FacilityDetail extends Component {
         <div>
           <LocationPhoneLink location={facility} />
         </div>
-        {website && website !== 'NULL' && (
-          <span>
-            <a href={website} target="_blank">
-              <i className="fa fa-globe" />
-              Website
-            </a>
-          </span>
-        )}
+        {website &&
+          website !== 'NULL' && (
+            <span>
+              <a href={website} target="_blank">
+                <i className="fa fa-globe" />
+                Website
+              </a>
+            </span>
+          )}
         <div>
           <LocationDirectionsLink location={facility} />
         </div>