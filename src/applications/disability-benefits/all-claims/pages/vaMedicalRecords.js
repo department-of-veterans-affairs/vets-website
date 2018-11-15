4971a7ea7d51bbe89cc2b16d530afe3aafb30b5d
Christopher Valarida <chris.valarida@adhocteam.us>
2018-11-15 13:56:58 -0800

    Remove redundant required property

--- src/applications/disability-benefits/all-claims/pages/vaMedicalRecords.js
+++ src/applications/disability-benefits/all-claims/pages/vaMedicalRecords.js
@@ -93,7 +93,6 @@ export const schema = {
     },
     vaTreatmentFacilities: merge({}, vaTreatmentFacilities, {
       items: {
-        required: ['treatmentCenterName', 'treatedDisabilityNames'],
         properties: {
           treatedDisabilityNames: {
             type: 'object',
