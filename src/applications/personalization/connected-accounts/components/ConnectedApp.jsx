599093de90cd7437ed0994083974604a2895088a
Alex Teal <arteal89@gmail.com>
2018-11-16 10:33:54 -0500

    Toggle detail block row showing all of the grants

--- src/applications/personalization/connected-accounts/components/ConnectedApp.jsx
+++ src/applications/personalization/connected-accounts/components/ConnectedApp.jsx
@@ -8,7 +8,7 @@ import { AccountModal } from './AccountModal';
 class ConnectedApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { modalOpen: false };
+    this.state = { modalOpen: false, detailsOpen: false };
   }
 
   closeModal = () => {
@@ -23,36 +23,64 @@ class ConnectedApp extends React.Component {
     this.props.confirmDelete(this.props.id);
   };
 
+  toggleDetails = () => {
+    this.setState({ detailsOpen: !this.state.detailsOpen })
+  }
+
   render() {
     const { href, logo, title, created, grants } = this.props.attributes;
+    const cssPrefix = "va-connected-acct";
     return (
       <tr>
-        <th scope="row">
-          <a href={href} className="no-external-icon">
-            <img src={logo} alt={`${title} logo`} width="100" />
-          </a>
-        </th>
-        <th>
-          <a href={href}>{title}</a> <br />
-          Connected at {moment(created).format('MMMM Do, YYYY')}
-          <ul>
-            {grants.map((a, idx) => (
-              <li key={idx}>{a.title}</li>
-            ))}
-          </ul>
-        </th>
-        <th>
-          <button className="usa-button-primary" onClick={this.openModal}>
-            Disconnect
-          </button>
-        </th>
-        <AccountModal
-          appName={title}
-          modalOpen={this.state.modalOpen}
-          onCloseModal={this.closeModal}
-          onConfirmDelete={this.confirmDelete}
-          propertyName={this.props.propertyName}
-        />
+        <table>
+          <tbody>
+            <tr className={`${cssPrefix}-row ${this.state.detailsOpen && cssPrefix + '-details-toggled'}`} onClick={this.toggleDetails}>
+              <th scope="row">
+                <a href={href} className="no-external-icon">
+                  <img src={logo} alt={`${title} logo`} width="100" />
+                </a>
+              </th>
+              <th>
+                Connected on {moment(created).format('MMMM Do, YYYY')}
+              </th>
+              <th className={`${cssPrefix}-row-details `}>
+                <a className={`${cssPrefix}-row-details-toggle`} href="#">
+                  Details
+                  <i className={`fa fa-chevron-${this.state.detailsOpen ? 'down' : 'right'}`}></i>
+                </a>
+                <AccountModal
+                  appName={title}
+                  modalOpen={this.state.modalOpen}
+                  onCloseModal={this.closeModal}
+                  onConfirmDelete={this.confirmDelete}
+                  propertyName={this.props.propertyName}
+                />
+              </th>
+            </tr>
+            { this.state.detailsOpen &&
+              <tr className={`${cssPrefix}-row-details-block`}>
+                <th colSpan="3">
+                  <div className={`${cssPrefix}-row-details-block-wrapper`}>
+                    <div className={`${cssPrefix}-row-details-block-content`}>
+                      <p>
+                        <a href={href}>{title}</a>
+                        &nbsp;has access to the following information:
+                        <button className="usa-button-primary" onClick={this.openModal}>
+                          Disconnect
+                        </button>
+                      </p>
+                      <ul>
+                        {grants.map((a, idx) => (
+                          <li key={idx}>{a.title}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                </th>
+              </tr>
+            }
+            </tbody>
+          </table>
       </tr>
     );
   }
