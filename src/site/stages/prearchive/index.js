da49764b1a7caae86f8ad81914607fff139c5ffc
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-11-16 13:02:54 -0500

    Update prearchive path

--- src/site/stages/prearchive/index.js
+++ src/site/stages/prearchive/index.js
@@ -35,7 +35,11 @@ function getFiles(directory, fileList = []) {
 
 function main() {
   const options = gatherCommandsArgs();
-  const directory = path.join(__dirname, '../../build', options.buildtype);
+  const directory = path.join(
+    __dirname,
+    '../../../../build',
+    options.buildtype,
+  );
   const fileNames = getFiles(directory);
 
   linkAssetsToBucket(options, fileNames);
