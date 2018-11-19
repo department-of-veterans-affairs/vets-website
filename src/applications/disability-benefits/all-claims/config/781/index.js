c35db920e08c0835847647a944dafe4bd8eb5c45
Aaron LeCheminant <aaronjlech@gmail.com>
2018-11-19 08:43:55 -0500

    remove spread from 781a

--- src/applications/disability-benefits/all-claims/config/781/index.js
+++ src/applications/disability-benefits/all-claims/config/781/index.js
@@ -26,15 +26,13 @@ export function formConfig781a(iterations) {
     configObj = {
       ...configObj,
       // 781a PAGE CONFIGS GO HERE
-      ...{
-        [`secondaryIncidentDate${index}`]: {
-          title: `781a PTSD Incident date ${index + 1}`,
-          path: `disabilities/ptsd-secondary-incident-date-${index}`,
-          // The Depends will need to be refactored to account for the page index/incident Number
-          depends: isAnswering781aQuestions,
-          uiSchema: secondaryIncidentDate.uiSchema(index),
-          schema: secondaryIncidentDate.schema(index),
-        },
+      [`secondaryIncidentDate${index}`]: {
+        title: `781a PTSD Incident date ${index + 1}`,
+        path: `disabilities/ptsd-secondary-incident-date-${index}`,
+        // The Depends will need to be refactored to account for the page index/incident Number
+        depends: isAnswering781aQuestions,
+        uiSchema: secondaryIncidentDate.uiSchema(index),
+        schema: secondaryIncidentDate.schema(index),
       },
     };
   }
