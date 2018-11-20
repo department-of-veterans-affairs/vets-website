41b011469db6d98e37c6ee31ed8d41c085878214
Christopher Valarida <chris.valarida@adhocteam.us>
2018-11-20 11:11:44 -0800

    Change "qualify"

--- src/applications/disability-benefits/all-claims/pages/individualUnemployability.js
+++ src/applications/disability-benefits/all-claims/pages/individualUnemployability.js
@@ -1,7 +1,7 @@
 export const uiSchema = {
   'ui:title': 'Individual Unemployability',
   'ui:description':
-    'If you can’t work because of your service-connected disability you may qualify for Individual Unemployability and increased disability payments.',
+    'If you can’t work because of your service-connected disability you may be eligible for Individual Unemployability and increased disability payments.',
   'view:unemployable': {
     'ui:title':
       'Does your disability prevent you from holding down a job that you could otherwise do? (This doesn’t include being retired.)',
