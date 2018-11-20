b12d08420c45a0f7c15a9fe8d17f09578c5f3fb6
Christopher Valarida <chris.valarida@adhocteam.us>
2018-11-20 11:08:44 -0800

    Remove "your"

--- src/applications/disability-benefits/all-claims/pages/aidAndAttendance.js
+++ src/applications/disability-benefits/all-claims/pages/aidAndAttendance.js
@@ -4,7 +4,7 @@ export const uiSchema = {
     'If you need help with everyday activities or are confined to your home because of your service-connected disability, you may be eligible for extra help.',
   'view:aidAndAttendance': {
     'ui:title':
-      'Are you confined to your home or need help with your everyday activities?',
+      'Are you confined to your home or need help with everyday activities?',
     'ui:widget': 'yesNo',
   },
 };
