038cb5fb72ddab1d662dc22eaabc84ad4c200270
Aaron LeCheminant <aaronjlech@gmail.com>
2018-11-21 08:41:12 -0500

    change config function naming

--- src/applications/disability-benefits/all-claims/tests/config/781.unit.spec.js
+++ src/applications/disability-benefits/all-claims/tests/config/781.unit.spec.js
@@ -1,17 +1,17 @@
 import { expect } from 'chai';
 
-import { formConfig781, formConfig781a } from '../../config/781';
+import { createFormConfig781, createFormConfig781a } from '../../config/781';
 
 import { PTSD_INCIDENT_ITERATION } from '../../constants';
 
 describe('781/781a incident form config iterators', () => {
   describe('781 incident form config', () => {
     it('should return a config object', () => {
-      expect(formConfig781(PTSD_INCIDENT_ITERATION)).to.be.an('object');
+      expect(createFormConfig781(PTSD_INCIDENT_ITERATION)).to.be.an('object');
     });
 
     it('should return three incident date page config objects', () => {
-      const config = formConfig781(PTSD_INCIDENT_ITERATION);
+      const config = createFormConfig781(PTSD_INCIDENT_ITERATION);
 
       const testMultipleIncidentDateProps = () => {
         for (let i = 0; i < PTSD_INCIDENT_ITERATION; i++) {
@@ -23,7 +23,7 @@ describe('781/781a incident form config iterators', () => {
       testMultipleIncidentDateProps();
     });
     it('should contain three incident properties', () => {
-      const config = formConfig781(PTSD_INCIDENT_ITERATION);
+      const config = createFormConfig781(PTSD_INCIDENT_ITERATION);
 
       const testMultipleIncidentDateObjects = () => {
         for (let i = 0; i < PTSD_INCIDENT_ITERATION; i++) {
@@ -40,11 +40,11 @@ describe('781/781a incident form config iterators', () => {
   });
   describe('781a incident form config', () => {
     it('should return a config object', () => {
-      expect(formConfig781a(PTSD_INCIDENT_ITERATION)).to.be.an('object');
+      expect(createFormConfig781a(PTSD_INCIDENT_ITERATION)).to.be.an('object');
     });
 
     it('should return three incident date page config objects', () => {
-      const config = formConfig781a(PTSD_INCIDENT_ITERATION);
+      const config = createFormConfig781a(PTSD_INCIDENT_ITERATION);
 
       const testMultipleIncidentDateProps = () => {
         for (let i = 0; i < PTSD_INCIDENT_ITERATION; i++) {
@@ -56,7 +56,7 @@ describe('781/781a incident form config iterators', () => {
       testMultipleIncidentDateProps();
     });
     it('should contain three incident properties', () => {
-      const config = formConfig781a(PTSD_INCIDENT_ITERATION);
+      const config = createFormConfig781a(PTSD_INCIDENT_ITERATION);
 
       const testMultipleIncidentDateObjects = () => {
         for (let i = 0; i < PTSD_INCIDENT_ITERATION; i++) {
