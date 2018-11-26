b2ef05dbed0e36d4cddf82afbf307a3491c065ed
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-11-26 17:48:39 -0500

    Consume env constants in platform code

--- src/platform/utilities/environment/index.js
+++ src/platform/utilities/environment/index.js
@@ -1,43 +1,49 @@
-const _Environments = {
-  preview: {
-    API_URL: 'https://api.va.gov',
-    BASE_URL: 'https://preview.va.gov',
-  },
-  vagovprod: {
+const ENVIRONMENTS = require('../../../site/constants/environments');
+
+const RESERVED_E2E_PORT = process.env.WEB_PORT || 3333;
+const E2E_API_PORT = process.env.API_PORT;
+
+const ENVIRONMENT_CONFIGURATIONS = {
+  [ENVIRONMENTS.VAGOVPROD]: {
     API_URL: 'https://api.va.gov',
     BASE_URL: 'https://www.va.gov',
   },
-  vagovstaging: {
+
+  [ENVIRONMENTS.VAGOVSTAGING]: {
     API_URL: 'https://staging-api.va.gov',
     BASE_URL: 'https://staging.va.gov',
   },
-  vagovdev: {
+
+  [ENVIRONMENTS.VAGOVDEV]: {
     API_URL: 'https://dev-api.va.gov',
     BASE_URL: 'https://dev.va.gov',
   },
-  localhost: {
+
+  [ENVIRONMENTS.LOCALHOST]: {
     API_URL: `http://${location.hostname}:3000`,
     BASE_URL: `http://${location.hostname}:3001`,
   },
-  e2e: {
-    API_URL: `http://localhost:${process.env.API_PORT || 3000}`,
-    BASE_URL: `http://localhost:${process.env.WEB_PORT || 3333}`,
-  },
 };
 
-function getEnvironment() {
-  let platform;
+const currentEnvironment = process.env.BUILDTYPE;
+const currentEnvironmentConfig = ENVIRONMENT_CONFIGURATIONS[currentEnvironment];
 
-  if (location.port === '3001') {
-    platform = 'localhost';
-  } else if (location.port === `${process.env.WEB_PORT || 3333}`) {
-    platform = 'e2e';
-  } else {
-    platform = __BUILDTYPE__;
-  }
+if (location.port === RESERVED_E2E_PORT) {
+  // E2E tests are an edge case - they test a certain build-type,
+  // but execute under the localhost hostname.
 
-  return _Environments[platform];
+  const e2eConfig = {
+    API_URL: `http://localhost:${E2E_API_PORT}`,
+    BASE_URL: `http://localhost:${RESERVED_E2E_PORT}`,
+  };
+
+  Object.assign(currentEnvironmentConfig, e2eConfig);
 }
 
-const environment = getEnvironment();
-module.exports = environment;
+module.exports = {
+  ...currentEnvironmentConfig,
+  isProduction: () => currentEnvironment === ENVIRONMENTS.VAGOVPROD,
+  isStaging: () => currentEnvironment === ENVIRONMENTS.VAGOVSTAGING,
+  isDev: () => currentEnvironment === ENVIRONMENTS.VAGOVDEV,
+  isLocal: () => currentEnvironment === ENVIRONMENTS.LOCALHOST,
+};
--- src/platform/utilities/environment/isProduction.js
+++ src/platform/utilities/environment/isProduction.js
@@ -1,11 +1,8 @@
-const prodEnvironments = [
-  // www.va.gov
-  'vagovprod',
-];
+const environment = require('./index');
 
 /**
  * Checks to see if the current build-type is a production-ready environment.
  */
 export default function isProduction() {
-  return prodEnvironments.includes(__BUILDTYPE__);
+  return environment.isProduction();
 }
