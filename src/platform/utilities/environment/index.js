249a3643de66efa0555e0e0d76e2cd7222ec0278
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-11-27 10:06:51 -0500

    More clean up

--- src/platform/utilities/environment/index.js
+++ src/platform/utilities/environment/index.js
@@ -8,36 +8,36 @@ import ENVIRONMENTS from '../../../site/constants/environments';
 const {
   WEB_PORT: RESERVED_E2E_PORT = 3333,
   API_PORT: E2E_API_PORT = 3000,
-  BUILDTYPE = ENVIRONMENTS.LOCALHOST,
+  BUILDTYPE = ENVIRONMENTS.VAGOVDEV,
 } = process.env;
 
 const ENVIRONMENT_CONFIGURATIONS = {
   [ENVIRONMENTS.VAGOVPROD]: {
     BUILDTYPE: ENVIRONMENTS.VAGOVPROD,
-    API_URL: 'https://api.va.gov',
     BASE_URL: 'https://www.va.gov',
+    API_URL: 'https://api.va.gov',
   },
 
   [ENVIRONMENTS.VAGOVSTAGING]: {
     BUILDTYPE: ENVIRONMENTS.VAGOVSTAGING,
-    API_URL: 'https://staging-api.va.gov',
     BASE_URL: 'https://staging.va.gov',
+    API_URL: 'https://staging-api.va.gov',
   },
 
   [ENVIRONMENTS.VAGOVDEV]: {
     BUILDTYPE: ENVIRONMENTS.VAGOVDEV,
-    API_URL: 'https://dev-api.va.gov',
     BASE_URL: 'https://dev.va.gov',
+    API_URL: 'https://dev-api.va.gov',
   },
 
   [ENVIRONMENTS.LOCALHOST]: {
     BUILDTYPE: ENVIRONMENTS.LOCALHOST,
-    API_URL: `http://${location.hostname}:3000`,
     BASE_URL: `http://${location.hostname}:3001`,
+    API_URL: `http://${location.hostname}:3000`,
   },
 };
 
-const currentEnvConfig = ENVIRONMENT_CONFIGURATIONS[BUILDTYPE];
+const environment = ENVIRONMENT_CONFIGURATIONS[BUILDTYPE];
 
 if (location.port === RESERVED_E2E_PORT) {
   // E2E tests are an edge case - they test a certain build-type,
@@ -48,39 +48,34 @@ if (location.port === RESERVED_E2E_PORT) {
     BASE_URL: `http://localhost:${RESERVED_E2E_PORT}`,
   };
 
-  Object.assign(currentEnvConfig, e2eConfig);
+  Object.assign(environment, e2eConfig);
 }
 
-/**
- * Determines whether the current environment is a production environment.
- * @returns {boolean}
- */
+/** Determines whether the current environment is a production environment. */
 export function isProduction() {
   return BUILDTYPE === ENVIRONMENTS.VAGOVPROD;
 }
 
-/**
- * Determines whether the current environment is a staging environment.
- * @returns {boolean}
- */
+/** Determines whether the current environment is a staging environment. */
 export function isStaging() {
   return BUILDTYPE === ENVIRONMENTS.VAGOVSTAGING;
 }
 
-/**
- * Determines whether the current environment is a dev environment.
- * @returns {boolean}
- */
+/** Determines whether the current environment is a dev environment. */
 export function isDev() {
   return BUILDTYPE === ENVIRONMENTS.VAGOVDEV;
 }
 
-/**
- * Determines whether the current environment is a local environment.
- * @returns {boolean}
- */
+/** Determines whether the current environment is a local environment. */
 export function isLocal() {
   return BUILDTYPE === ENVIRONMENTS.LOCALHOST;
 }
 
-export default currentEnvConfig;
+export default {
+  /** The name of the environment under which the site is currently executing. */
+  BUILDTYPE: environment.BUILDTYPE,
+  /** The address of the FE website configured for this environment. */
+  BASE_URL: 'https://dev.va.gov',
+  /** The address of the API configured for this environment. */
+  API_URL: 'https://dev-api.va.gov',
+};
