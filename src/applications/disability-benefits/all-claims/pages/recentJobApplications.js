b7f769b41b42f941a4ae2e8632a3e0202ebbd16a
582623 <matthewroth@c02wc0jnhtdg.users.bah.com>
2018-11-29 13:26:50 -0500

    more commits

--- src/applications/disability-benefits/all-claims/pages/recentJobApplications.js
+++ src/applications/disability-benefits/all-claims/pages/recentJobApplications.js
@@ -1,30 +1,108 @@
+import { merge, omit } from 'lodash';
+import fullSchema from '../config/schema';
+
 import {
   recentJobApplicationsDescription,
   substantiallyGainfulEmployment,
 } from '../content/recentJobApplications';
 
+import {
+  uiSchema as addressUI,
+  schema as addressSchema,
+} from '../../../../platform/forms/definitions/address';
+import { validateZIP } from '../validations';
+
+import ArrayField from 'us-forms-system/lib/js/fields/ArrayField';
+import dateUI from 'us-forms-system/lib/js/definitions/date';
+
 export const uiSchema = {
   'ui:title': 'Recent job applications',
-  'ui:description': recentJobApplicationsDescription(),
-  'view:recentJobApplications': {
-    'ui:title': ' ',
+  'view:hasRecentJobApplications': {
+    'ui:title': recentJobApplicationsDescription(),
     'ui:widget': 'yesNo',
   },
-  'view:substantiallyGainfulEmploymentInfo': {
+  appliedEmployers: {
+    'ui:options': {
+      viewField: ArrayField,
+      expandUnder: 'view:hasRecentJobApplications',
+      itemName: 'Job',
+    },
+    items: {
+      name: {
+        'ui:title': "Company's Name",
+      },
+      address: merge(addressUI('', false), {
+        'ui:order': [
+          'country',
+          'addressLine1',
+          'addressLine2',
+          'city',
+          'state',
+          'zipCode',
+        ],
+        addressLine1: {
+          'ui:title': 'Street',
+        },
+        addressLine2: {
+          'ui:title': 'Street 2',
+        },
+        state: {
+          'ui:title': 'State',
+        },
+        zipCode: {
+          'ui:title': 'Postal Code',
+          'ui:validations': [validateZIP],
+        },
+      }),
+      workType: {
+        'ui:title': 'Type of work',
+      },
+      date: dateUI('Date you applied'),
+    },
+  },
+  substantiallyGainfulEmploymentInfo: {
     'ui:title': ' ',
     'ui:description': substantiallyGainfulEmployment(),
   },
 };
 
-export const schema = {
-  type: 'object',
-  properties: {
-    'view:recentJobApplications': {
-      type: 'boolean',
+export const schema = () => {
+  const address = addressSchema(fullSchema);
+  return {
+    type: 'object',
+    properties: {
+      'view:hasRecentJobApplications': {
+        type: 'boolean',
+        properties: {},
+      },
+      appliedEmployers: {
+        type: 'array',
+        items: {
+          type: 'object',
+          name: {
+            type: 'string',
+          },
+          address: {
+            ...address,
+            properties: {
+              ...omit(address.properties, ['addressLine3', 'postalCode']),
+              zipCode: {
+                type: 'string',
+              },
+            },
+          },
+          workType: {
+            type: 'string',
+          },
+          date: {
+            $ref: '#/definitions/date',
+          },
+        },
+      },
+      substantiallyGainfulEmploymentInfo: {
+        type: 'object',
+        properties: {},
+      },
     },
-    'view:substantiallyGainfulEmploymentInfo': {
-      type: 'object',
-      properties: {},
-    },
-  },
+  };
 };
