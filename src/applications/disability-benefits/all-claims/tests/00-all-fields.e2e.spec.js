7dea06e02f1f0f1beb12a13ae323fab5c4794152
Matt Roth <roth_matthew@bah.com>
2018-11-30 16:19:13 -0500

    will the test pass or failgit status

--- src/applications/disability-benefits/all-claims/tests/00-all-fields.e2e.spec.js
+++ src/applications/disability-benefits/all-claims/tests/00-all-fields.e2e.spec.js
@@ -23,8 +23,10 @@ const runTest = E2eHelpers.createE2eTest(client => {
 
   // Ensure introduction page renders.
   client
-    .url(`${E2eHelpers.baseUrl}/disability-benefits/apply/form-526-all-claims`)
+    // eslint-disable-next-line prettier/prettier
+    .url(`${E2eHelpers.baseUrl}/disability-benefits/apply/form-526-all-claims/introduction`,)
     .waitForElementVisible('body', Timeouts.normal)
+    .assert.title('Apply for disability benefits: VA.gov')
     // First render of React may be slow.
     .waitForElementVisible('.schemaform-title', Timeouts.slow) // First render of React may be slow.
     .waitForElementVisible(
