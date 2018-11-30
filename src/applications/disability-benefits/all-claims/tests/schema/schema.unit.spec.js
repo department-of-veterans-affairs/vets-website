877251a84a50f492d73551314304deadfff644ac
Mark Greenburg <marek.greenburg@gmail.com>
2018-11-30 11:13:40 -0600

    Update schema validation test to match new parsing

--- src/applications/disability-benefits/all-claims/tests/schema/schema.unit.spec.js
+++ src/applications/disability-benefits/all-claims/tests/schema/schema.unit.spec.js
@@ -6,7 +6,7 @@ import { Validator } from 'jsonschema';
 import formConfig from '../../config/form';
 import fullSchema from '../../config/schema';
 
-describe('526 all claims schema tests', () => {
+describe.only('526 all claims schema tests', () => {
   const v = new Validator();
   const files = fs.readdirSync(__dirname);
   files.filter(file => file.endsWith('json')).forEach(file => {
@@ -16,8 +16,8 @@ describe('526 all claims schema tests', () => {
       );
       const submitData = JSON.parse(
         formConfig.transformForSubmit(formConfig, contents),
-      ).form526;
-      const result = v.validate(JSON.parse(submitData), fullSchema);
+      );
+      const result = v.validate(submitData, fullSchema);
 
       if (!result.valid) {
         console.log(`Validation errors found in ${file}`); // eslint-disable-line
