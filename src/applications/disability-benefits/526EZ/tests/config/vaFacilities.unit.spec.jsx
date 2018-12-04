773043e3d511c632e8891259befa3810464fbeb9
Rian Fowler <rian.fowler@adhocteam.us>
2018-12-04 08:26:41 -0700

    short circuit debounced fetch

--- src/applications/disability-benefits/526EZ/tests/config/vaFacilities.unit.spec.jsx
+++ src/applications/disability-benefits/526EZ/tests/config/vaFacilities.unit.spec.jsx
@@ -28,6 +28,9 @@ describe('Disability benefits 526EZ VA facility', () => {
     uiSchema,
     arrayPath,
   } = formConfig.chapters.supportingEvidence.pages.vaFacilities;
+  uiSchema.disabilities.items.treatments.items.treatmentCenterName[
+    'ui:options'
+  ].getOptions = () => Promise.resolve([]);
   it('renders VA facility form', () => {
     const form = mount(
       <DefinitionTester
