27911aaa731ccdc3239fc72c71594dc15c648e4b
Aaron LeCheminant <aaronjlech@gmail.com>
2018-12-04 10:58:12 -0500

    update content

--- src/applications/disability-benefits/all-claims/pages/conclusionCombat.js
+++ src/applications/disability-benefits/all-claims/pages/conclusionCombat.js
@@ -4,8 +4,8 @@ import { ptsd781NameTitle } from '../content/ptsdClassification';
 const conclusionDescription = (
   <div>
     <p>
-      Thank you for taking the time to answer our questions. The information you
-      provided will help us research your claim.
+      Thank you for answering our questions. The information you provided will
+      help us research your claim.
     </p>
   </div>
 );
