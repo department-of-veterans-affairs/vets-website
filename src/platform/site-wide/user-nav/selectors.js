14e8d3326e92548436beb4741dab048386507771
Bill Tran <bill@billjohntran.com>
2018-12-04 01:44:14 -0800

    fix for not displaying email on desktop

--- src/platform/site-wide/user-nav/selectors.js
+++ src/platform/site-wide/user-nav/selectors.js
@@ -4,12 +4,19 @@ import { startCase, toLower } from 'lodash';
 import conditionalStorage from '../../utilities/storage/conditionalStorage';
 import { selectProfile } from '../../user/selectors';
 
+const desktopMediaQuery = window.matchMedia('(min-width: 768px)');
+
 export const selectUserGreeting = createSelector(
   state => selectProfile(state).userFullName,
+  state => selectProfile(state).email,
   () => conditionalStorage().getItem('userFirstName'),
-  (name, sessionFirstName) => {
-    if (name.first || sessionFirstName) {
+  (name, email, sessionFirstName) => {
+    if (name.firsts || sessionFirstName) {
       return startCase(toLower(name.first || sessionFirstName));
+    }
+
+    if (desktopMediaQuery.matches) {
+      return email;
     }
 
     return 'My Account';
