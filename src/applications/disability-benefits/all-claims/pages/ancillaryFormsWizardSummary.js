178127b46942512c74e43265ed1197ecb3715191
Christopher Valarida <chris.valarida@adhocteam.us>
2018-12-04 16:08:05 -0800

    Depends test

--- src/applications/disability-benefits/all-claims/pages/ancillaryFormsWizardSummary.js
+++ src/applications/disability-benefits/all-claims/pages/ancillaryFormsWizardSummary.js
@@ -1,10 +1,12 @@
 import summaryDescription from '../content/ancillaryFormsWizardSummary';
 
 export const depends = formData =>
-  formData['view:modifyingHome'] ||
-  formData['view:modifyingCar'] ||
-  formData['view:aidAndAttendance'] ||
-  formData['view:unemployable'];
+  !!(
+    formData['view:modifyingHome'] ||
+    formData['view:modifyingCar'] ||
+    formData['view:aidAndAttendance'] ||
+    formData['view:unemployable']
+  );
 
 export const uiSchema = {
   'ui:title': 'Summary of additional benefits',
