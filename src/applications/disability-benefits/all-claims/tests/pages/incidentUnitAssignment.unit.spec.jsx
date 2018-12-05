c743b3fbcccd1a64e715fe657d9acefe6025dde0
Devin McCurdy <mccurdy_devin@bah.com>
2018-12-05 15:37:42 -0500

    [All Claims] 781/781a Incident Unit Assignment (#9137)
    Added PTSD incident unit assignment pages for the 781 and 781a flow

---
+++ src/applications/disability-benefits/all-claims/tests/pages/incidentUnitAssignment.unit.spec.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
+
+import { ERR_MSG_CSS_CLASS } from '../../constants';
+
+import {
+  DefinitionTester,
+  fillDate,
+  fillData,
+} from '../../../../../platform/testing/unit/schemaform-utils';
+import formConfig from '../../config/form';
+
+describe('781 Unit Assignment Details', () => {
+  const page = formConfig.chapters.disabilities.pages.incidentUnitAssignment0;
+  const { schema, uiSchema } = page;
+
+  it('should render', () => {
+    const form = mount(
+      <DefinitionTester schema={schema} uiSchema={uiSchema} />,
+    );
+    expect(form.find('input').length).to.equal(3);
+    expect(form.find('select').length).to.equal(4);
+  });
+
+  it('should fill in unit assignment details', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        onSubmit={onSubmit}
+        schema={schema}
+        uiSchema={uiSchema}
+      />,
+    );
+
+    fillData(form, 'input#root_incident0_unitAssigned', '21st Airborne');
+    fillDate(form, 'root_incident0_unitAssignedDates_from', '2016-07-10');
+    fillDate(form, 'root_incident0_unitAssignedDates_to', '2017-06-12');
+
+    form.find('form').simulate('submit');
+    expect(form.find(ERR_MSG_CSS_CLASS).length).to.equal(0);
+    expect(onSubmit.called).to.be.true;
+  });
+
+  it('should allow submission if no assigned unit details are submitted', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        onSubmit={onSubmit}
+        schema={schema}
+        uiSchema={uiSchema}
+      />,
+    );
+
+    form.find('form').simulate('submit');
+    expect(form.find(ERR_MSG_CSS_CLASS).length).to.equal(0);
+    expect(onSubmit.called).to.be.true;
+  });
+});
