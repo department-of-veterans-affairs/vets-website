e0924268ca690a1c927b1450ee7b1045b8a9c46e
Matt Roth <roth_matthew@bah.com>
2018-12-06 10:42:13 -0500

    it works

--- src/applications/disability-benefits/all-claims/pages/recentEarnedIncome.js
+++ src/applications/disability-benefits/all-claims/pages/recentEarnedIncome.js
@@ -1,42 +1,62 @@
 import { grossIncomeAdditionalInfo } from '../content/recentEarnedIncome';
 import currencyUI from 'us-forms-system/lib/js/definitions/currency';
 
+const currentMonthlyEarnedIncomeCurrency = currencyUI(
+  "What's your current gross monthly income?",
+);
+
 export const uiSchema = {
   'ui:title': 'Recent earnings',
-  past12MonthsEarnedIncome: currencyUI(
-    'What was your gross income over the past 12 months?',
-  ),
-  'view:grossIncomeAdditionalInfo': {
-    'ui:title': ' ',
-    'ui:description': grossIncomeAdditionalInfo,
-  },
-  'view:isEmployed': {
-    'ui:title': 'Are you employed now?',
-    'ui:widget': 'yesNo',
-  },
-  currentMonthlyEarnedIncome: {
-    'ui:options': {
-      expandUnder: 'view:isEmployed',
+  unemployability: {
+    past12MonthsEarnedIncome: currencyUI(
+      'What was your gross income over the past 12 months?',
+    ),
+    'view:grossIncomeAdditionalInfo': {
+      'ui:title': ' ',
+      'ui:description': grossIncomeAdditionalInfo,
     },
-    'ui:title': "What's your current gross monthly income?",
+    'view:isEmployed': {
+      'ui:title': 'Are you employed now?',
+      'ui:widget': 'yesNo',
+    },
+    currentMonthlyEarnedIncome: {
+      ...currentMonthlyEarnedIncomeCurrency,
+      'ui:options': {
+        ...currentMonthlyEarnedIncomeCurrency['ui:options'],
+        expandUnder: 'view:isEmployed',
+      },
+    },
   },
 };
 
 export const schema = {
   type: 'object',
   properties: {
-    past12MonthsEarnedIncome: {
-      type: 'string',
-    },
-    'view:grossIncomeAdditionalInfo': {
+    unemployability: {
       type: 'object',
-      properties: {},
-    },
-    'view:isEmployed': {
-      type: 'boolean',
-    },
-    currentMonthlyEarnedIncome: {
-      type: 'string',
+      properties: {
+        past12MonthsEarnedIncome: {
+          type: 'number',
+          minimum: 0,
+          maximum: 9999999.99,
+        },
+        'view:grossIncomeAdditionalInfo': {
+          type: 'object',
+          properties: {},
+        },
+        'view:isEmployed': {
+          type: 'boolean',
+        },
+        'view:currentMonthlyEarnedIncome': {
+          type: 'object',
+          properties: {},
+        },
+        currentMonthlyEarnedIncome: {
+          type: 'number',
+          minimum: 0,
+          maximum: 9999999.99,
+        },
+      },
     },
   },
 };
