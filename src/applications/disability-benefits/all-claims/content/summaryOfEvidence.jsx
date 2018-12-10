489f14ad25f14603cdec459c68f4015f9b9d74a5
Mark Greenburg <marek.greenburg@gmail.com>
2018-12-10 13:15:22 -0600

    Fix empty / incorrect contents bug for summaryOfEvidence, beef up tests

--- src/applications/disability-benefits/all-claims/content/summaryOfEvidence.jsx
+++ src/applications/disability-benefits/all-claims/content/summaryOfEvidence.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import _ from '../../../../platform/utilities/data';
+import { DATA_PATHS } from '../constants';
 
 export const summaryOfEvidenceDescription = ({ formData }) => {
   const vaEvidence = _.get('vaTreatmentFacilities', formData, []);
-  const privateEvidence = _.get('privateMedicalRecords', formData, []);
-  const layEvidence = _.get('additionalDocuments', formData, []);
-  const evidenceLength = !!vaEvidence.concat(privateEvidence, layEvidence)
-    .length;
+  const privateEvidence = _.get('providerFacility', formData, []);
+  const privateEvidenceUploads = _.get('privateMedicalRecords', formData, []);
+  const layEvidenceUploads = _.get('additionalDocuments', formData, []);
+  const evidenceLength = !!vaEvidence.concat(
+    privateEvidence,
+    privateEvidenceUploads,
+    layEvidenceUploads,
+  ).length;
   const selectedEvidence = _.get('view:hasEvidence', formData, false);
-  const privateFacility = _.get('providerFacility', formData, []);
   // Evidence isn't always properly cleared out from form data if removed so
   // need to also check that 'no evidence' was explicitly selected
-  if ((!evidenceLength || !selectedEvidence) && !privateFacility) {
+  if (!evidenceLength || !selectedEvidence) {
     return (
       <p>
         You haven’t uploaded any evidence. This may delay us processing your
@@ -24,9 +28,27 @@ export const summaryOfEvidenceDescription = ({ formData }) => {
   let vaContent = null;
   let privateContent = null;
   let layContent = null;
-  let privateFacilityContent = null;
+  let privateEvidenceContent = null;
 
-  if (vaEvidence.length) {
+  const vaEvidenceSelected = _.get(DATA_PATHS.hasVAEvidence, formData, false);
+  const privateEvidenceSelected = _.get(
+    DATA_PATHS.hasPrivateEvidence,
+    formData,
+    false,
+  );
+  const uploadPrivateEvidenceSelected = _.get(
+    DATA_PATHS.hasPrivateRecordsToUpload,
+    formData,
+    false,
+  );
+
+  const additionalEvidenceSelected = _.get(
+    DATA_PATHS.hasAdditionalDocuments,
+    formData,
+    false,
+  );
+
+  if (vaEvidence.length && vaEvidenceSelected) {
     const facilitiesList = vaEvidence.map(facility => (
       <li key={facility.treatmentCenterName}>{facility.treatmentCenterName}</li>
     ));
@@ -38,40 +60,48 @@ export const summaryOfEvidenceDescription = ({ formData }) => {
     );
   }
 
-  if (privateEvidence.length) {
-    const privateEvidenceList = privateEvidence.map(upload => (
+  if (
+    privateEvidenceUploads.length &&
+    privateEvidenceSelected &&
+    uploadPrivateEvidenceSelected
+  ) {
+    const privateEvidenceUploadsList = privateEvidenceUploads.map(upload => (
       <li key={upload.name}>{upload.name}</li>
     ));
     privateContent = (
       <div>
         <p>We’ll submit the below private medical records you uploaded:</p>
+        <ul>{privateEvidenceUploadsList}</ul>
+      </div>
+    );
+  }
+
+  if (
+    privateEvidence.length &&
+    privateEvidenceSelected &&
+    !uploadPrivateEvidenceSelected
+  ) {
+    const privateEvidenceList = privateEvidence.map(facility => (
+      <li key={facility.providerFacilityName}>
+        {facility.providerFacilityName}
+      </li>
+    ));
+    privateEvidenceContent = (
+      <div>
+        <p>We'll get your private medical records from:</p>
         <ul>{privateEvidenceList}</ul>
       </div>
     );
   }
 
-  if (layEvidence.length) {
-    const layEvidenceList = layEvidence.map(upload => (
+  if (layEvidenceUploads.length && additionalEvidenceSelected) {
+    const layEvidenceUploadsList = layEvidenceUploads.map(upload => (
       <li key={upload.name}>{upload.name}</li>
     ));
     layContent = (
       <div>
         <p>We’ll submit the below supporting evidence you uploaded:</p>
-        <ul>{layEvidenceList}</ul>
-      </div>
-    );
-  }
-
-  if (privateFacility.length) {
-    const privateFacilityList = privateFacility.map(facility => (
-      <li key={facility.providerFacilityName}>
-        {facility.providerFacilityName}
-      </li>
-    ));
-    privateFacilityContent = (
-      <div>
-        <p>We'll get your private medical records from:</p>
-        <ul>{privateFacilityList}</ul>
+        <ul>{layEvidenceUploadsList}</ul>
       </div>
     );
   }
@@ -80,8 +110,8 @@ export const summaryOfEvidenceDescription = ({ formData }) => {
     <div>
       {vaContent}
       {privateContent}
+      {privateEvidenceContent}
       {layContent}
-      {privateFacilityContent}
     </div>
   );
 };
