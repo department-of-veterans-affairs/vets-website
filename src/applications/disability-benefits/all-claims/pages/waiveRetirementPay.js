e25633cecf8f001ac14b9d989b13960903c8e54a
Mark Greenburg <marek.greenburg@gmail.com>
2018-12-11 10:53:54 -0600

    Remove yesNoReverse

--- src/applications/disability-benefits/all-claims/pages/waiveRetirementPay.js
+++ src/applications/disability-benefits/all-claims/pages/waiveRetirementPay.js
@@ -12,7 +12,6 @@ export const uiSchema = {
     'ui:title': 'What type of pay you would like to receive?',
     'ui:widget': 'yesNo',
     'ui:options': {
-      yesNoReverse: true, // If veteran elects to not receive VA pay, they waive their VA pay
       labels: {
         Y: 'I want to receive VA compensation pay.',
         N: ' I don’t want to receive tax-free VA compensation pay.', // waiveRetirementPay = true because yesNoReverse
