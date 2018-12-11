aeb030c122a15823229531c0ec033e698f70a7d3
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-12-11 15:19:26 -0500

    Revert MHVApp

--- src/platform/user/authorization/containers/MHVApp.jsx
+++ src/platform/user/authorization/containers/MHVApp.jsx
@@ -141,7 +141,10 @@ export class MHVApp extends React.Component {
     switch (accountState) {
       case 'needs_terms_acceptance': {
         const redirectQuery = { tc_redirect: window.location.pathname }; // eslint-disable-line camelcase
-        const termsConditionsUrl = appendQuery('/health-care/', redirectQuery);
+        const termsConditionsUrl = appendQuery(
+          '/health-care/medical-information-terms-conditions/',
+          redirectQuery,
+        );
         window.location.replace(termsConditionsUrl);
         break;
       }
