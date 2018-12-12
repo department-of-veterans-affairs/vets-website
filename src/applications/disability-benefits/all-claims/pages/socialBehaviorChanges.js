7db90b53570cdf3a8a9f36172985303265a9df1d
592176 <imeecuison@imee.local>
2018-12-12 08:18:23 -0500

    remove string

--- src/applications/disability-benefits/all-claims/pages/socialBehaviorChanges.js
+++ src/applications/disability-benefits/all-claims/pages/socialBehaviorChanges.js
@@ -4,7 +4,7 @@ import { PtsdNameTitle } from '../content/ptsdClassification';
 
 const socialDescriptionChanges = (
   <div>
-    <h5>Changes in social behavior</h5>
+    <h5>in social behavior</h5>
     <p>
       Please tell us about any changes in your social life as a result the
       event(s). You may have experienced some or none of these. (Please check
