1aee4623e2aa867d237b62bc0dcdc4a2c51e7b69
Matt Roth <roth_matthew@bah.com>
2018-12-12 16:23:31 -0500

    update branch, use common stuff

--- src/applications/disability-benefits/all-claims/pages/secondaryUploadSources.js
+++ src/applications/disability-benefits/all-claims/pages/secondaryUploadSources.js
@@ -1,54 +1,15 @@
-import React from 'react';
-import fileUploadUI from 'us-forms-system/lib/js/definitions/file';
-import environment from '../../../../platform/utilities/environment';
+import { ancillaryFormUploadUi } from '../utils';
 
 import { uploadDescription } from '../content/fileUploadDescriptions';
-import { PtsdNameTitle } from '../content/ptsdClassification';
-
-const FIFTY_MB = 52428800;
+import { ptsd781aNameTitle } from '../content/ptsdClassification';
 
 export const uiSchema = index => ({
-  'ui:title': ({ formData }) => (
-    <PtsdNameTitle formData={formData} formType="781a" />
-  ),
+  'ui:title': ptsd781aNameTitle,
   'ui:description': uploadDescription,
-  [`secondaryUploadSources${index}`]: fileUploadUI('', {
-    itemDescription: 'PTSD 781a form supporting documents',
-    hideLabelText: true,
-    fileUploadUrl: `${environment.API_URL}/v0/upload_supporting_evidence`,
-    fileTypes: [
-      'pdf',
-      'jpg',
-      'jpeg',
-      'png',
-      'gif',
-      'bmp',
-      'tif',
-      'tiff',
-      'txt',
-    ],
-    maxSize: FIFTY_MB,
-    createPayload: file => {
-      const payload = new FormData();
-      payload.append('supporting_evidence_attachment[file_data]', file);
-
-      return payload;
-    },
-    parseResponse: (response, file) => ({
-      name: file.name,
-      confirmationCode: response.data.attributes.guid,
-    }),
-    // this is the uiSchema passed to FileField for the attachmentId schema
-    // FileField requires this name be used
-    attachmentSchema: {
-      'ui:title': 'Document type',
-    },
-    // this is the uiSchema passed to FileField for the name schema
-    // FileField requires this name be used
-    attachmentName: {
-      'ui:title': 'Document name',
-    },
-  }),
+  [`secondaryUploadSources${index}`]: ancillaryFormUploadUi(
+    '',
+    'PTSD 781a form supporting documents',
+  ),
 });
 
 export const schema = index => ({
