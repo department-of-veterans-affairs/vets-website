b6b07780a6cea4c42bfd0fed56948b0d720ae8c0
Matt Roth <roth_matthew@bah.com>
2018-12-12 14:28:01 -0500

    fix unit tests

--- src/applications/disability-benefits/all-claims/tests/pages/unemployabilityCertification.unit.spec.jsx
+++ src/applications/disability-benefits/all-claims/tests/pages/unemployabilityCertification.unit.spec.jsx
@@ -3,7 +3,10 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import { mount } from 'enzyme';
 
-import { DefinitionTester } from '../../../../../platform/testing/unit/schemaform-utils';
+import {
+  DefinitionTester,
+  selectCheckbox,
+} from '../../../../../platform/testing/unit/schemaform-utils';
 import formConfig from '../../config/form';
 import { ERR_MSG_CSS_CLASS } from '../../constants';
 
@@ -22,6 +25,35 @@ describe('Recent Job Applications', () => {
     );
 
     expect(form.find('input').length).to.equal(2);
+    form.unmount();
+  });
+
+  it('should certify both statements', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        onSubmit={onSubmit}
+        definitions={formConfig.defaultDefinitions}
+        schema={schema}
+        uiSchema={uiSchema}
+      />,
+    );
+
+    selectCheckbox(
+      form,
+      'root_unemployability_view:statementsAreTrue_view:statementsAreTrueAccept',
+      true,
+    );
+
+    selectCheckbox(
+      form,
+      'root_unemployability_view:informOfReturnToWork_view:informOfReturnToWorkAccept',
+      true,
+    );
+
+    form.find('form').simulate('submit');
+    expect(form.find(ERR_MSG_CSS_CLASS).length).to.equal(0);
+    expect(onSubmit.called).to.be.true;
     form.unmount();
   });
 
