4b36fab3527e30191fc941b2e4ca15a6233fceed
Claire Hsu <clairehsu86@gmail.com>
2018-12-13 15:23:37 -0800

    Tidy

--- src/applications/personalization/preferences/components/PreferenceItem.jsx
+++ src/applications/personalization/preferences/components/PreferenceItem.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router';
 
 import AdditionalInfo from '@department-of-veterans-affairs/formation/AdditionalInfo';
 import AlertBox from '@department-of-veterans-affairs/formation/AlertBox';
