f1a68359fdacd634bc619e5ee4a15313598be476
Mark Greenburg <marek.greenburg@gmail.com>
2018-12-14 12:44:26 -0600

    Add validation to schema

--- src/applications/disability-benefits/all-claims/pages/vaMedicalRecords.js
+++ src/applications/disability-benefits/all-claims/pages/vaMedicalRecords.js
@@ -7,6 +7,7 @@ import { queryForFacilities, addCheckboxPerDisability } from '../utils';
 import {
   validateMilitaryTreatmentCity,
   validateMilitaryTreatmentState,
+  startedAfterServicePeriod,
 } from '../validations';
 import { USA } from '../constants';
 import { validateBooleanGroup } from 'us-forms-system/lib/js/validation';
@@ -57,10 +58,16 @@ export const uiSchema = {
           required: 'Please select at least one condition',
         },
       },
-      treatmentDateRange: dateRangeUI(
-        'When did you first visit this facility?',
-        'When was your most recent visit?',
-        'Date of last treatment must be after date of first treatment',
+      treatmentDateRange: merge(
+        {},
+        dateRangeUI(
+          'When did you first visit this facility?',
+          'When was your most recent visit?',
+          'Date of last treatment must be after date of first treatment',
+        ),
+        {
+          from: { 'ui:validations': [startedAfterServicePeriod] },
+        },
       ),
       treatmentCenterAddress: {
         'ui:order': ['country', 'state', 'city'],
