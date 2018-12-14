3dd8c26f9c09cc753858b19de199ec79302582b4
Rob Felder <felder_robert@bah.com>
2018-12-14 10:08:57 -0500

    Revert "Updated 781a list of doc types"
    This reverts commit 1624e06043e90b98d404b501f0b596874fabee8d.

--- src/applications/disability-benefits/all-claims/pages/uploadSecondaryPtsdForms.js
+++ src/applications/disability-benefits/all-claims/pages/uploadSecondaryPtsdForms.js
@@ -1,16 +1,52 @@
 import { uploadDescription } from '../content/fileUploadDescriptions';
 import { ptsd781aNameTitle } from '../content/ptsdClassification';
-import { ancillaryFormUploadUi } from '../utils';
+import fileUploadUI from 'us-forms-system/lib/js/definitions/file';
+import environment from '../../../../platform/utilities/environment';
 import fullSchema from 'vets-json-schema/dist/21-526EZ-ALLCLAIMS-schema.json';
 
-const { attachments } = fullSchema.properties;
-const secondarySupportStatement = 'L229';
+const { completedFormAttachments } = fullSchema.properties;
+
+const FIFTY_MB = 52428800;
 
 export const uiSchema = {
   'ui:title': ptsd781aNameTitle,
   'ui:description': uploadDescription,
-  ptsd781a: ancillaryFormUploadUi('', 'PTSD 781a form', {
-    attachmentId: secondarySupportStatement,
+  ptsd781a: fileUploadUI('', {
+    itemDescription: 'PTSD 781a form',
+    hideLabelText: true,
+    fileUploadUrl: `${environment.API_URL}/v0/upload_supporting_evidence`,
+    fileTypes: [
+      'pdf',
+      'jpg',
+      'jpeg',
+      'png',
+      'gif',
+      'bmp',
+      'tif',
+      'tiff',
+      'txt',
+    ],
+    maxSize: FIFTY_MB,
+    createPayload: file => {
+      const payload = new FormData();
+      payload.append('supporting_evidence_attachment[file_data]', file);
+
+      return payload;
+    },
+    parseResponse: (response, file) => ({
+      name: file.name,
+      confirmationCode: response.data.attributes.guid,
+      attachmentId:
+        'VA Form 21-0781a-Statement in Support of Claim for PTSD Secondary to Personal Assault',
+    }),
+    // this is the uiSchema passed to FileField for the attachmentId schema
+    // FileField requires this name be used
+    attachmentSchema: {
+      'ui:title': 'Document type',
+      'ui:disabled': true,
+      'ui:widget': 'textarea',
+    },
+    classNames: 'upload-completed-form',
   }),
 };
 
@@ -18,6 +54,6 @@ export const schema = {
   type: 'object',
   required: ['ptsd781a'],
   properties: {
-    ptsd781a: attachments,
+    ptsd781a: completedFormAttachments,
   },
 };
