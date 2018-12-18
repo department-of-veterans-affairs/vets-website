a64f6db76d5f0465e441ecbc0242e3ca8989ec36
Aaron LeCheminant <aaronjlech@gmail.com>
2018-12-18 11:21:10 -0500

    move descriptions into titles for inputs/fields

--- src/applications/disability-benefits/all-claims/content/incidentUnitAssignment.jsx
+++ src/applications/disability-benefits/all-claims/content/incidentUnitAssignment.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-export const ptsdAssignmentDescription = (
-  <div>
-    <h5>Your assignment</h5>
-  </div>
-);
+export const ptsdAssignmentDescription = <h5>Your assignment</h5>;
 
 export const ptsdAssignmentDatesDescription = (
   <p>Dates you were assigned to this unit</p>
--- src/applications/disability-benefits/all-claims/pages/additionalRemarks781.jsx
+++ src/applications/disability-benefits/all-claims/pages/additionalRemarks781.jsx
@@ -2,26 +2,14 @@ import React from 'react';
 
 import { ptsd781NameTitle } from '../content/ptsdClassification';
 
-const additionalRemarksDescription = (
-  <div>
-    <h3>Additional Remarks</h3>
-    <p>
-      If there is anything else you would like to tell us about the stressful
-      events that contributed to your PTSD, you can provide that information
-      below.
-    </p>
-    <p>
-      If you have any supporting documents or buddy statements to support your
-      claim, you‘ll have a chance to upload those later in the application.
-    </p>
-  </div>
-);
+const additionalRemarksDescription = <h3>Additional Remarks</h3>;
 
 export const uiSchema = {
   'ui:title': ptsd781NameTitle,
   'ui:description': additionalRemarksDescription,
   additionalRemarks781: {
-    'ui:title': ' ',
+    'ui:title':
+      'If there is anything else you would like to tell us about the stressful events that contributed to your PTSD, you can provide that information below. If you have any supporting documents or buddy statements to support your claim, you‘ll have a chance to upload those later in the application.',
     'ui:widget': 'textarea',
     'ui:options': {
       rows: 5,
