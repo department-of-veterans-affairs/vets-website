0eaf25a49f987fc16adc6c40336d2ba07127585e
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-12-18 10:50:40 -0500

    Cleaning up a bit

--- src/applications/proxy-rewrite/local-proxy-rewrite.js
+++ src/applications/proxy-rewrite/local-proxy-rewrite.js
@@ -4,9 +4,10 @@ const path = require('path');
 
 const BUCKETS = require('../../site/constants/buckets');
 const ENVIRONMENTS = require('../../site/constants/environments');
+const HOSTNAMES = require('../../site/constants/hostnames');
 
 function injectLocalBundle(buildOptions) {
-  const { hostUrl } = buildOptions;
+  const { hostUrl: localhost } = buildOptions;
   const prodBucket = `https://${BUCKETS[ENVIRONMENTS.VAGOVPROD]}`;
 
   return async (req, res, next) => {
@@ -24,20 +25,24 @@ function injectLocalBundle(buildOptions) {
     const vaPageResponse = await fetch(vaGovUrl);
     let vaPageHtml = await vaPageResponse.text();
 
-    vaPageHtml = vaPageHtml.replace(prodBucket, hostUrl);
+    vaPageHtml = vaPageHtml.replace(prodBucket, localhost);
     res.send(vaPageHtml);
   };
 }
 
 function fallbackToTeamSiteServer(buildOptions) {
+  const prodDomain = `https://${HOSTNAMES[ENVIRONMENTS.VAGOVPROD]}`;
+
   return (req, res, next) => {
     const fullFilePath = path.join(buildOptions.destination, req.path);
 
-    fs.pathExists(fullFilePath, (err, doesExist) => {
-      if (err || !doesExist) {
-        res.redirect(`https://www.va.gov${req.path}`);
-      } else {
+    fs.pathExists(fullFilePath, (err, existsOnLocalhost) => {
+      if (err) {
+        next(err);
+      } else if (existsOnLocalhost) {
         next();
+      } else {
+        res.redirect(`${prodDomain}${req.path}`);
       }
     });
   };
