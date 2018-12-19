ff6cc568f03ff8a3a61f17aa702fcc8fb0516a7c
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-12-19 10:21:22 -0500

    add a colon

--- src/applications/proxy-rewrite/proxy-rewrite-entry.jsx
+++ src/applications/proxy-rewrite/proxy-rewrite-entry.jsx
@@ -80,13 +80,14 @@ function renderFooter() {
 
     if (lastUpdated) {
       const lastUpdatedPanel = document.createElement('div');
+      const lastUpdatedDate = lastUpdated.replace('Last updated ', '');
 
       lastUpdatedPanel.innerHTML = `
         <div class="footer-lastupdated">
           <div class="usa-grid">
             <div class="col-md-3"></div>
             <div class="col-md-9">
-              ${lastUpdated}
+              Last updated: ${lastUpdatedDate}
             </div>
           </div>
         </div>
