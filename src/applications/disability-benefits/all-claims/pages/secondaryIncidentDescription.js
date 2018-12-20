fc843d01005c4522700020e66232c2e1aebd8485
Aaron LeCheminant <aaronjlech@gmail.com>
2018-12-20 14:55:04 -0500

    Bah 781 508 label bugs (#9282)
    Adds labels to input fields within 781/781a that had none for 508 accessibility.

--- src/applications/disability-benefits/all-claims/pages/secondaryIncidentDescription.js
+++ src/applications/disability-benefits/all-claims/pages/secondaryIncidentDescription.js
@@ -3,16 +3,7 @@ import React from 'react';
 import fullSchema from 'vets-json-schema/dist/21-526EZ-ALLCLAIMS-schema.json';
 import { ptsd781aNameTitle } from '../content/ptsdClassification';
 
-const incidentDescriptionInstructions = (
-  <div>
-    <h3>Event description</h3>
-    <p>
-      Please tell us what happened during the event or situation. Provide the
-      level of detail that you‘re comfortable sharing. You don‘t have to repeat
-      any information that you‘ve already shared.
-    </p>
-  </div>
-);
+const incidentDescriptionInstructions = <h3>Event description</h3>;
 
 const { description } = fullSchema.definitions.secondaryPtsdIncident.properties;
 
@@ -21,7 +12,8 @@ export const uiSchema = index => ({
   'ui:description': incidentDescriptionInstructions,
   [`secondaryIncident${index}`]: {
     description: {
-      'ui:title': ' ',
+      'ui:title':
+        'Please tell us what happened during the event or situation. Provide the level of detail that you‘re comfortable sharing. You don‘t have to repeat any information that you‘ve already shared.',
       'ui:widget': 'textarea',
       'ui:options': {
         rows: 5,
