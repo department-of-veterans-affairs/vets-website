8ceb623f7fce5845f92a6bd1ce695b33ecbb9328
Aaron LeCheminant <aaronjlech@gmail.com>
2018-12-27 14:27:35 -0500

    Bah 781 content updates (#9299)
    * init commit, setup page schema
    
    * wip employmentHistory page
    
    * update content
    
    * update content for location and date page
    
    * update individuals involved and additional remarks
    
    * more content updates
    
    * remove employmenthistory out of branch
    
    * update 781a content
    
    * fix 781 issues, update event date page for 781a
    
    * remove extra variable
    
    * fix apostraphe
    
    * update individuals involved message
    
    * update UploadDescription
    
    * fix walkthrough
    
    * add intro page for 781
    
    * fix additional information page
    
    * add 781a title

--- src/applications/disability-benefits/all-claims/content/individualsInvolved.jsx
+++ src/applications/disability-benefits/all-claims/content/individualsInvolved.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const individualsInvolved = <h5>Individuals Involved</h5>;
+export const individualsInvolved = <h5>Individuals involved</h5>;
 
 export const personDescriptionText = (
   <div>
@@ -14,8 +14,7 @@ export const individualsDescription = (
     {individualsInvolved}
     <p>Please provide the name of a person injured or killed.</p>
     <p>
-      If you don’t know the full name of the person, provide a nickname. If you
-      don’t know the person’s name at all, please briefly provide details that
+      If you don’t know the person’s name, please briefly provide details that
       would help us identify the person.
     </p>
   </div>
