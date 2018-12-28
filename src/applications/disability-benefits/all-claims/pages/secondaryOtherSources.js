910b52093d25b5a04d786f7e440dd87d84931c67
Aaron LeCheminant <aaronjlech@gmail.com>
2018-12-28 15:16:28 -0500

    update keys to match 781a not 781

--- src/applications/disability-benefits/all-claims/pages/secondaryOtherSources.js
+++ src/applications/disability-benefits/all-claims/pages/secondaryOtherSources.js
@@ -31,7 +31,7 @@ export const uiSchema = index => ({
 export const schema = index => ({
   type: 'object',
   properties: {
-    [`incident${index}`]: {
+    [`secondaryIncident${index}`]: {
       type: 'object',
       properties: {
         otherSources: {
