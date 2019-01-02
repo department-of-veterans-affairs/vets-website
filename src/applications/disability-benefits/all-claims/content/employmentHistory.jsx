f16afc557ca6605f0410d4922964a1d097d40111
Aaron LeCheminant <aaronjlech@gmail.com>
2019-01-02 09:20:08 -0500

    update content

--- src/applications/disability-benefits/all-claims/content/employmentHistory.jsx
+++ src/applications/disability-benefits/all-claims/content/employmentHistory.jsx
@@ -7,7 +7,10 @@ export const employmentDescription = (
       Now we’re going to ask you about your employment history. Please enter
       your most recent employer first and work back through the past 5 years. Be
       sure to include any time you were on military duty and inactive duty for
-      training. If you were self-employed, enter your own name as the employer.
+      training.
+    </p>
+    <p>
+      For any time you were self-employed, enter your own name as the employer.
     </p>
   </div>
 );
