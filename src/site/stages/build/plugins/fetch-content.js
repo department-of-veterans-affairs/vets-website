2f0e3b05b8a4b885ca07f823c8d820cc0a26b62a
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-01-03 17:51:58 -0500

    wip

--- src/site/stages/build/plugins/fetch-content.js
+++ src/site/stages/build/plugins/fetch-content.js
@@ -3,8 +3,43 @@ const { createHttpLink } = require('apollo-link-http');
 const { InMemoryCache } = require('apollo-cache-inmemory');
 const fetch = require('node-fetch');
 
-function fetchContent(buildOptions) {
+const CONTENT_APIS = require('../../../constants/content-apis');
 
+const query = gql`
+{
+    nodeQuery{
+        count
+        entities {
+            ... on NodePage {
+                nid
+                entityBundle
+                entityPublished
+                title
+                fieldIntroText
+                fieldContentBlock {
+                    entity {
+                        ... on Paragraph {
+                            id
+                            entityBundle
+                            entityRendered
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+`;
+
+function fetchContent(buildOptions) {
+  const contentApiRoute = CONTENT_APIS[buildOptions.buildtype];
+  const link = createHttpLink({ uri: contentApiRoute, fetch });
+  const cache = new InMemoryCache();
+  const contentApi = new ApolloClient({ link, cache });
+
+  return async (pages, metalsmith, done) => {
+
+  };
 }
 
 module.exports = fetchContent;
