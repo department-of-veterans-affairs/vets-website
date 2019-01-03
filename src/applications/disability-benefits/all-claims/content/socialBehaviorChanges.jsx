812a124fed2142e68fe910504b82ceafe2a14230
Devin McCurdy <mccurdy_devin@bah.com>
2019-01-03 16:11:35 -0500

    [All Claims] Form0781 transform (#9262)
    Transformed 0781 from data

---
+++ src/applications/disability-benefits/all-claims/content/socialBehaviorChanges.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const changeDescription = (
+  <div>
+    <h5>Changes in social behavior</h5>
+    <p>
+      Please tell us about any changes in your social life as a result the
+      event(s). You may have experienced some or none of these. (Please check
+      any that apply.)
+    </p>
+  </div>
+);
