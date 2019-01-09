66163034fa23e1c36d8856cb526772f17e8a0af2
Rob Felder <felder_robert@bah.com>
2019-01-09 10:32:37 -0500

    Updating css for display of a drop-down & changed supporting documents upload page to have drop-down

--- src/applications/disability-benefits/all-claims/pages/unemployabilityFormUpload.js
+++ src/applications/disability-benefits/all-claims/pages/unemployabilityFormUpload.js
@@ -3,13 +3,20 @@ import { ancillaryFormUploadUi } from '../utils';
 import fullSchema from 'vets-json-schema/dist/21-526EZ-ALLCLAIMS-schema.json';
 
 const { attachments } = fullSchema.properties;
-const unemployabilityComp = 'L149';
+const unemployabilityComp = {
+  enum: ['L149'],
+  enumNames: [
+    'VA 21-8940-Veterans Application for Increased Compensation Based on Unemployability',
+  ],
+};
 
 export const uiSchema = {
   'ui:title': 'Upload VA Form 21-8940',
   'ui:description': UploadDescription,
   form8940Upload: ancillaryFormUploadUi('', 'Adding additional evidence', {
     attachmentId: unemployabilityComp,
+    customClasses: 'upload-completed-form',
+    isDisabled: true,
   }),
 };
 
