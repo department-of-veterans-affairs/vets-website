3bf0d914f8a62f4eb7af2b6092bf38156da844c5
Christopher Valarida <chris.valarida@adhocteam.us>
2019-01-11 12:41:28 -0800

    Add test script

---
+++ script/run-mockapi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ensure all running servers are terminated on script exit.
+trap 'if [ $(jobs -p) ] ; then kill $(jobs -p); fi' EXIT
+
+# Check to see if we already have an API server running on port 3000
+if [ "$(nc -z localhost 3000; echo $?)" -ne 0 ]; then
+    echo "Starting mockapi.js..."
+    node src/platform/testing/e2e/mockapi.js &
+else
+    echo "Error: Port 3000 is already in use.  If you're sure that's OK, tests will continue in 5 seconds..."
+    sleep 5;
+fi
