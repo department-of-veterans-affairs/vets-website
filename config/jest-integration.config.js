366f61231dfccdb308329c532464c7f2713fd5c7
Nick Sullivan <ncksllvn@gmail.com>
2019-01-10 19:30:40 -0500

    Update Jest config (#9374)

--- config/jest-integration.config.js
+++ config/jest-integration.config.js
@@ -104,7 +104,7 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  // rootDir: null,
+  rootDir: '../src',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
