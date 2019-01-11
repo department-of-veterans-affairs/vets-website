65733e794611717c6b8e1160549001b12305a41c
Aaron LeCheminant <aaronjlech@gmail.com>
2019-01-11 08:29:15 -0500

    Update Doc type for ancillary form uploads & add uploaded docs to attachments (#9366)
    * trying something
    
    * setup doc types
    
    * setup attachments
    
    * update transform schemas
    
    * trying to get transform tests fixed
    
    * add disabled and wrap text
    
    * working on test schemas
    
    * add maximal data update
    
    * remove change for test script
    
    * remove logs
    
    * move comment.
    
    * update submit transformer, add tests for ptsd form uploads
    
    * update name

--- src/applications/disability-benefits/all-claims/pages/uploadPtsdDocuments.jsx
+++ src/applications/disability-benefits/all-claims/pages/uploadPtsdDocuments.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { UploadDescription } from '../content/fileUploadDescriptions';
 import { ptsd781NameTitle } from '../content/ptsdClassification';
-import { ancillaryFormUploadUi } from '../utils';
-import fullSchema from 'vets-json-schema/dist/21-526EZ-ALLCLAIMS-schema.json';
+import { ancillaryFormUploadUi, getAttachmentsSchema } from '../utils';
 
-const { completedFormAttachments } = fullSchema.properties;
+const PTSD_781_ATTACHMENT_ID = 'L228';
 
 export const uiSchema = {
   'ui:title': ptsd781NameTitle,
   'ui:description': <UploadDescription uploadTitle="Upload VA Form 21-0781" />,
-  ptsd781: ancillaryFormUploadUi('', 'PTSD 781 form', {
-    attachmentId: 'VA Form 21-781 - Statement in Support of Claim for PTSD',
-    widgetType: 'textarea',
+  form781Upload: ancillaryFormUploadUi('', 'PTSD 781 form', {
+    attachmentId: PTSD_781_ATTACHMENT_ID,
     customClasses: 'upload-completed-form',
     isDisabled: true,
   }),
@@ -19,8 +17,8 @@ export const uiSchema = {
 
 export const schema = {
   type: 'object',
-  required: ['ptsd781'],
+  required: ['form781Upload'],
   properties: {
-    ptsd781: completedFormAttachments,
+    form781Upload: getAttachmentsSchema(PTSD_781_ATTACHMENT_ID),
   },
 };
