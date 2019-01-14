d8c3277e5f7a9ebb44900d6545b05a579805c18e
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-01-14 10:54:01 -0500

    cleaning up

--- src/site/stages/build/plugins/create-megamenu.js
+++ src/site/stages/build/plugins/create-megamenu.js
@@ -4,14 +4,14 @@ const yaml = require('js-yaml');
 
 const { applyFragments } = require('./apply-fragments');
 
-const MEGAMENU_DATA_SOURCE_FILENAME = 'megamenu/index.yml'
+const MEGAMENU_DATA_SOURCE_FILENAME = 'megamenu/index.yml';
 const MEGAMENU_CACHE_FILENAME = 'megamenu.json';
 
-const chai = require('chai');
-const CURRENT_MEGAMENU_DATA = require('../../../../platform/site-wide/mega-menu/mega-menu-link-data.json');
-
 function writeToCache(buildOptions, megaMenuData) {
-  const megamenuDataCacheFileName = path.join(buildOptions.cacheDirectory, MEGAMENU_CACHE_FILENAME);
+  const megamenuDataCacheFileName = path.join(
+    buildOptions.cacheDirectory,
+    MEGAMENU_CACHE_FILENAME,
+  );
   const serialized = JSON.stringify(megaMenuData, null, 4);
 
   fs.ensureDirSync(buildOptions.cacheDirectory);
@@ -21,15 +21,14 @@ function writeToCache(buildOptions, megaMenuData) {
 function createMegaMenu(buildOptions) {
   return (files, metalsmith, done) => {
     const fragmentsRoot = metalsmith.path(buildOptions.contentFragments);
-    const megaMenuDataSourceFile = path.join(fragmentsRoot, MEGAMENU_DATA_SOURCE_FILENAME);
+    const megaMenuDataSourceFile = path.join(
+      fragmentsRoot,
+      MEGAMENU_DATA_SOURCE_FILENAME,
+    );
     const megaMenuFile = fs.readFileSync(megaMenuDataSourceFile);
     const megaMenuData = yaml.safeLoad(megaMenuFile);
 
-    const [
-      vaBenefitsAndHealthCare,
-      aboutVa,
-    ] = megaMenuData;
-
+    const [vaBenefitsAndHealthCare, aboutVa] = megaMenuData;
 
     // "promotions", which is data that (as of writing) are located under the "columnThree"
     // property, has to exist in a separate file for editing/organizational reasons.
@@ -42,11 +41,6 @@ function createMegaMenu(buildOptions) {
     applyFragments(buildOptions, metalsmith, aboutVa.menuSections);
     delete aboutVa.menuSections.fragments;
 
-    console.log(megaMenuData)
-
-    console.log('comparing megamenu data structures....');
-    chai.expect(CURRENT_MEGAMENU_DATA).to.be.deep.equal(megaMenuData, 'The data is not equal!');
-    console.log('Looks good!')
     writeToCache(buildOptions, megaMenuData);
     done();
   };
