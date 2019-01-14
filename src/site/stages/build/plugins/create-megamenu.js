8910d85532427a06413305f4fb9045157c3f6613
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-01-14 10:51:39 -0500

    Borrow from fragments middleware

--- src/site/stages/build/plugins/create-megamenu.js
+++ src/site/stages/build/plugins/create-megamenu.js
@@ -2,6 +2,8 @@ const fs = require('fs-extra');
 const path = require('path');
 const yaml = require('js-yaml');
 
+const { applyFragments } = require('./apply-fragments');
+
 const MEGAMENU_DATA_SOURCE_FILENAME = 'megamenu/index.yml'
 const MEGAMENU_CACHE_FILENAME = 'megamenu.json';
 
@@ -22,6 +24,25 @@ function createMegaMenu(buildOptions) {
     const megaMenuDataSourceFile = path.join(fragmentsRoot, MEGAMENU_DATA_SOURCE_FILENAME);
     const megaMenuFile = fs.readFileSync(megaMenuDataSourceFile);
     const megaMenuData = yaml.safeLoad(megaMenuFile);
+
+    const [
+      vaBenefitsAndHealthCare,
+      aboutVa,
+    ] = megaMenuData;
+
+
+    // "promotions", which is data that (as of writing) are located under the "columnThree"
+    // property, has to exist in a separate file for editing/organizational reasons.
+    // To accomplish this, we borrow use the fragments concept. More info in that middleware.
+    for (const hub of vaBenefitsAndHealthCare.menuSections) {
+      applyFragments(buildOptions, metalsmith, hub.links);
+      delete hub.links.fragments;
+    }
+
+    applyFragments(buildOptions, metalsmith, aboutVa.menuSections);
+    delete aboutVa.menuSections.fragments;
+
+    console.log(megaMenuData)
 
     console.log('comparing megamenu data structures....');
     chai.expect(CURRENT_MEGAMENU_DATA).to.be.deep.equal(megaMenuData, 'The data is not equal!');
