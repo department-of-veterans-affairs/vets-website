d18a060daffe982671b77d6c41d1f44e389c3cc2
Matt Roth <roth_matthew@bah.com>
2019-01-15 16:24:49 -0500

    commit

--- src/applications/disability-benefits/all-claims/pages/pastEmploymentFormIntro.js
+++ src/applications/disability-benefits/all-claims/pages/pastEmploymentFormIntro.js
@@ -26,7 +26,7 @@ export const uiSchema = {
     },
     'view:upload4192': {
       'ui:title':
-        ' want to upload a completed Request for Employment Information (VA Form 21-4192).',
+        'I want to upload a completed Request for Employment Information (VA Form 21-4192).',
     },
     'view:sendRequests': {
       'ui:title': 'I would like you to handle these requests for me.',
