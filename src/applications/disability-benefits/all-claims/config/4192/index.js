b01a72ec55452d3f16866d1b9ca4942a03c2388a
Matt Roth <roth_matthew@bah.com>
2019-01-15 14:02:24 -0500

    commit

--- src/applications/disability-benefits/all-claims/config/4192/index.js
+++ src/applications/disability-benefits/all-claims/config/4192/index.js
@@ -4,15 +4,40 @@ import { pastEmploymentFormIntro } from '../../pages';
 
 import { needsToEnterUnemployability } from '../../utils';
 
+// const showFormTutorial = formData => _.get(formData, 'view:upload4192Choice.view:4192Info', false);
+// const isDownloading = formData =>
+// _.get(formData, 'view:upload4192Choice.view:download4192', false);
+// const isUploading = formData =>
+//   _.get(formData, 'view:upload4192Choice.view:upload4192', false);
+// const isExiting = formData => _.get(formData, 'view:upload4192Choice.view:sendRequests', false);
 export default function() {
   let configObj = {};
   if (!environment.isProduction()) {
     configObj = {
       pastEmploymentFormIntro: {
         path: 'past-employment-walkthrough-choice',
+        depends: needsToEnterUnemployability,
         uiSchema: pastEmploymentFormIntro.uiSchema,
         schema: pastEmploymentFormIntro.schema,
       },
+      // Form Tutorial (multiple pages)
+      // Download
+      // Upload
+      pastEmploymentFormUpload: {
+        path: '',
+      },
+      // ***Below page comments for when logic is added in***
+      // Conditional Options (Intro page again if A and not A && B && C)
+      // Form Tutorial (multiple pages)
+      // Download
+      // Upload
+      // Conditional Options (Intro page again if A and not A && B && C)
+      // Form Tutorial (multiple pages)
+      // Download
+      // Upload
+      // Conditional Options (Intro page again if A and not A && B && C)
+      // ***Above page comments for when logic is added in***
+      // Exit
       conclusion4192: {
         title: 'Conclusion 4192',
         path: 'disabilities/conclusion-4192',
