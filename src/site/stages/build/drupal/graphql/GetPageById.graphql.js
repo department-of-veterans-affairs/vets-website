9e90d4584ee5012645fd330784b5a659a5654923
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-01-16 18:33:26 -0500

    add hub

--- src/site/stages/build/drupal/graphql/GetPageById.graphql.js
+++ src/site/stages/build/drupal/graphql/GetPageById.graphql.js
@@ -1,13 +1,16 @@
+const Hub = require('./Hub.graphql');
 const StandardPage = require('./StandardPage.graphql');
 
 module.exports = `
 
+  ${Hub}
   ${StandardPage}
 
   query GetPageById($path: String!) {
     route: route(path: $path) {
       ... on EntityCanonicalUrl {
         entity {
+          ... Hub
           ... StandardPage
         }
       }
