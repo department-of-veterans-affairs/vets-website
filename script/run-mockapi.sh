ad99bee2d5a5629426a7e5bb908ca1da42f85aca
Christopher Valarida <chris.valarida@adhocteam.us>
2019-01-16 10:47:44 -0800

    Ensure the mock api is started before continuing

--- script/run-mockapi.sh
+++ script/run-mockapi.sh
@@ -7,6 +7,7 @@ trap 'if [ $(jobs -p) ] ; then kill $(jobs -p); fi' EXIT
 if [ "$(nc -z localhost 3000; echo $?)" -ne 0 ]; then
     echo "Starting mockapi.js..."
     node src/platform/testing/e2e/mockapi.js &
+    sleep 5;
 else
     # echo "Error: Port 3000 is already in use.  If you're sure that's OK, tests will continue in 5 seconds..."
     echo "Port 3000 is already in use."
