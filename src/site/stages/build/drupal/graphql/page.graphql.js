b6ce8774bfadfe9e8f8d56c98d226af93a21af8e
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-01-17 15:26:06 -0500

    add a comment

--- src/site/stages/build/drupal/graphql/page.graphql.js
+++ src/site/stages/build/drupal/graphql/page.graphql.js
@@ -1,5 +1,6 @@
 /**
- *
+ * A standard content page, that is ordinarily two-levels deep (a child page of a landingPage)
+ * For example, /health-care/apply.
  */
 module.exports = `
   fragment page on NodePage {
