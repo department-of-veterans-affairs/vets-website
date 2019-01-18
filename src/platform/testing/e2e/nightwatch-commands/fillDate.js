91929d69b78e3f29d956a468d591289d888cc59e
Christopher Valarida <chris.valarida@adhocteam.us>
2019-01-18 12:19:05 -0800

    Revert changes to fillDate()

--- src/platform/testing/e2e/nightwatch-commands/fillDate.js
+++ src/platform/testing/e2e/nightwatch-commands/fillDate.js
@@ -3,16 +3,14 @@
  *
  * @param {String} fieldName The name the field without the Month, Day, or Year
  *                           e.g. root_spouseInfo_remarriageDate
- * @param {String} dateString The date as a string (day is optional)
- *                            e.g. 1990-1-28 or 1990-1
+ * @param {String} dateString The date as a string
+ *                            e.g. 1990-1-28
  */
 exports.command = function fillDate(fieldName, dateString) {
   const date = dateString.split('-');
-  this.selectDropdown(`${fieldName}Month`, parseInt(date[1], 10).toString());
-  if (date[2]) {
-    this.selectDropdown(`${fieldName}Day`, parseInt(date[2], 10).toString());
-  }
-  this.fill(`input[name="${fieldName}Year"]`, parseInt(date[0], 10).toString());
+  this.selectDropdown(`${fieldName}Month`, parseInt(date[1], 10).toString())
+    .selectDropdown(`${fieldName}Day`, parseInt(date[2], 10).toString())
+    .fill(`input[name="${fieldName}Year"]`, parseInt(date[0], 10).toString());
 
   return this;
 };
