895ce99f8bdac8d46cbb667d6805547a9b921d8d
Jeff Balboni <jeff@adhocteam.us>
2019-01-23 16:23:39 -0500

    Empty tests

---
+++ src/platform/user/tests/profile/utilities/index.unit.spec.js
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+
+import { mapRawUserDataToState } from '../../../profile/utilities';
+
+describe('Profile utilities', () => {
+  describe('mapRawUserDataToState', () => {
+    it('should map profile', () => {
+      
+    });
+
+    it('should map veteran status', () => {
+      
+    });
+
+    it('should map vet 360 info', () => {
+      
+    });
+
+    it('should handle profile error', () => {
+      
+    });
+
+    it('should handle veteran status error', () => {
+      
+    });
+
+    it('should handle vet 360 error', () => {
+      
+    });
+  });
+});
