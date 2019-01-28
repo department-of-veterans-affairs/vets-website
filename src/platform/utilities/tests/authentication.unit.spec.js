8a22564f781ff2d6f5669978953738df78d871e2
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2019-01-28 09:53:03 -0500

    Lint fixes.

--- src/platform/utilities/tests/authentication.unit.spec.js
+++ src/platform/utilities/tests/authentication.unit.spec.js
@@ -1,6 +1,4 @@
 import { expect } from 'chai';
-import sinon from 'sinon';
-import Raven from 'raven-js';
 
 import {
   login,
@@ -17,70 +15,66 @@ let oldWindow;
 
 const fakeWindow = () => {
   oldSessionStorage = global.sessionStorage;
-  oldWindow = window;
+  oldWindow = global.window;
   global.sessionStorage = { setItem: () => {} };
-  window = {
+  global.window = {
     dataLayer: [],
     location: {
-      get: () => window.location,
-      set: value => { window.location = value; },
-    }
+      get: () => global.window.location,
+      set: value => {
+        global.window.location = value;
+      },
+    },
   };
 };
 
 describe('auth URL helpers', () => {
   beforeEach(fakeWindow);
   afterEach(() => {
-    window = oldWindow;
+    global.window = oldWindow;
     global.sessionStorage = oldSessionStorage;
     resetFetch();
   });
 
   it('should redirect to an error page', () => {
     mockApiRequest({ error: "Couldn't find url" }, false);
-    login('idme')
-      .then(() => {
-        expect(window.location).to.include('/auth/login/callback');
-      });
+    login('idme').then(() => {
+      expect(global.window.location).to.include('/auth/login/callback');
+    });
   });
 
   it('should redirect for signup', () => {
     mockApiRequest({ url: 'signup-url' });
-    signup()
-      .then(() => {
-        expect(window.location).to.eq('signup-url');
-      });
+    signup().then(() => {
+      expect(global.window.location).to.eq('signup-url');
+    });
   });
 
   it('should redirect for login', () => {
     mockApiRequest({ url: 'login-url' });
-    login('idme')
-      .then(() => {
-        expect(window.location).to.eq('login-url');
-      });
+    login('idme').then(() => {
+      expect(global.window.location).to.eq('login-url');
+    });
   });
 
   it('should redirect for logout', () => {
     mockApiRequest({ url: 'logout-url' });
-    logout()
-      .then(() => {
-        expect(window.location).to.eq('logout-url');
-      });
+    logout().then(() => {
+      expect(global.window.location).to.eq('logout-url');
+    });
   });
 
   it('should redirect for MFA', () => {
     mockApiRequest({ url: 'mfa-url' });
-    mfa()
-      .then(() => {
-        expect(window.location).to.eq('mfa-url');
-      });
+    mfa().then(() => {
+      expect(global.window.location).to.eq('mfa-url');
+    });
   });
 
   it('should redirect for verify', () => {
     mockApiRequest({ url: 'verify-url' });
-    verify()
-      .then(() => {
-        expect(window.location).to.eq('verify-url');
-      });
+    verify().then(() => {
+      expect(global.window.location).to.eq('verify-url');
+    });
   });
 });
