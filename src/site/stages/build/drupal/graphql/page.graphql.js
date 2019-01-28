6b36c1cb5f6ff4dc15d48fbe1d95f148e585068a
lightning <aurora.hay@agile6.com>
2019-01-28 13:49:16 -0800

    list of link teasers and link teasers graphql

--- src/site/stages/build/drupal/graphql/page.graphql.js
+++ src/site/stages/build/drupal/graphql/page.graphql.js
@@ -1,12 +1,15 @@
 const wysiwygParagraph = require('./paragraphs/wysiwyg.paragraph.graphql');
+const listOfLinkTeasers = require('./paragraphs/listOfLinkTeasers.paragraph.graphql');
 /**
  * A standard content page, that is ordinarily two-levels deep (a child page of a landingPage)
  * For example, /health-care/apply.
  */
 const WYSIWYG_FRAGMENT = '...wysiwyg';
+const LISTOFLINKTEASERS_FRAGMENT = '...listOfLinkTeasers';
 
 module.exports = `
   ${wysiwygParagraph}
+  ${listOfLinkTeasers}
 
   fragment page on NodePage {
     entityUrl {
@@ -21,6 +24,7 @@ module.exports = `
         entityType
         entityBundle
         ${WYSIWYG_FRAGMENT}
+        ${LISTOFLINKTEASERS_FRAGMENT}
       }
     }
     changed
---
+++ src/site/stages/build/drupal/graphql/paragraphs/listOfLinkTeasers.paragraph.graphql.js
@@ -0,0 +1,20 @@
+const linkTeaser = require('./linkTeaser.paragraph.graphql');
+/**
+ * The 'List of link teasers' bundle of the 'Paragraph' entity type.
+ */
+const LINKTEASER_FRAGMENT = '...linkTeaser';
+
+module.exports = `
+  ${linkTeaser}
+  
+  fragment listOfLinkTeasers on ParagraphListOfLinkTeasers {
+    fieldTitle
+    fieldVaParagraphs {
+      targetId
+      targetRevisionId
+      entity {
+        ${LINKTEASER_FRAGMENT}
+      }    
+    }
+  }
+`;
