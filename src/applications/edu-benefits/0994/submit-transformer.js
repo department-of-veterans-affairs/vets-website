9d9b3d0a17cc644c5ef8a367788134fc9b02ea65
Matt Roth <roth_matthew@bah.com>
2019-01-29 13:10:49 -0500

    comit

--- src/applications/edu-benefits/0994/submit-transformer.js
+++ src/applications/edu-benefits/0994/submit-transformer.js
@@ -1,24 +1,42 @@
+import _ from '../../../platform/utilities/data';
 import { transformForSubmit } from 'us-forms-system/lib/js/helpers';
 
 export function transform(formConfig, form) {
-  let formData = JSON.parse(transformForSubmit(formConfig, form));
+  const usFormTransform = () =>
+    JSON.parse(transformForSubmit(formConfig, form));
 
-  if (form.data['view:phoneAndEmail']) {
-    const { dayTimePhone, nightTimePhone, emailAddress } = form.data[
-      'view:phoneAndEmail'
-    ];
+  const addPhoneAndEmail = formData => {
+    const clonedData = _.cloneDeep(formData);
+    if (form.data['view:phoneAndEmail']) {
+      const { dayTimePhone, nightTimePhone, emailAddress } = form.data[
+        'view:phoneAndEmail'
+      ];
 
-    formData = {
-      ...formData,
-      dayTimePhone,
-      nightTimePhone,
-      emailAddress,
-    };
-  }
+      return {
+        ...clonedData,
+        dayTimePhone,
+        nightTimePhone,
+        emailAddress,
+      };
+    }
+    return clonedData;
+  };
+
+  const transformHighTechnologyEmploymentType = formData => {
+    const clonedData = _.cloneDeep(formData);
+
+    return clonedData;
+  };
+
+  const tranformedData = [
+    usFormTransform,
+    addPhoneAndEmail,
+    transformHighTechnologyEmploymentType,
+  ].reduce((formData, transformer) => transformer(formData), form.data);
 
   return JSON.stringify({
     educationBenefitsClaim: {
-      form: JSON.stringify(formData),
+      form: JSON.stringify(tranformedData),
     },
   });
 }
