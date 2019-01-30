fdcbd16ad2da2f7f8a6cc5e5c86e0f0501ae9823
Mark Greenburg <marek.greenburg@gmail.com>
2019-01-30 14:23:40 -0600

    PTSD Unit assignment page content update

--- src/applications/disability-benefits/all-claims/content/incidentUnitAssignment.jsx
+++ src/applications/disability-benefits/all-claims/content/incidentUnitAssignment.jsx
@@ -3,5 +3,8 @@ import React from 'react';
 export const ptsdAssignmentDescription = <h5>Your assignment</h5>;
 
 export const ptsdAssignmentDatesDescription = (
-  <p>Approximate dates you were assigned to this unit</p>
+  <p>
+    When were you assigned to this unit? (If you don’t know the exact date you
+    can give us an estimate.)
+  </p>
 );
