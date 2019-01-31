c11d84907b513e37ad773f161b5fa978ce02c42e
Mark Greenburg <marek.greenburg@gmail.com>
2019-01-31 14:00:22 -0600

    Update secondary incident date content for 526 ptsd

--- src/applications/disability-benefits/all-claims/content/incidentDate.jsx
+++ src/applications/disability-benefits/all-claims/content/incidentDate.jsx
@@ -19,32 +19,9 @@ export const ptsdDateDescription = (
   <div>
     <h5>Estimated event date</h5>
     <p>
-      Please tell us when this event or situation happened. If it happened over
-      a period of time, please tell us when it started.
+      Please tell us when this event happened. If it happened over a period of
+      time, please tell us when it started.
     </p>
     {eventHelpText}
   </div>
 );
-
-export const SecondaryDateDescription = ({ index }) => {
-  const isFirstIncident = index === 0;
-  return (
-    <div>
-      <h5>Event date</h5>
-      {isFirstIncident && (
-        <p>
-          Now we’ll ask about the first event that caused your PTSD. If there is
-          more than one event or situation you want to tell us about, we’ll ask
-          questions about each one separately.
-        </p>
-      )}
-      <p>
-        Please tell us when this event or situation happened. If it happened
-        over a period of time, please tell us around when it started. (If you’re
-        having trouble remembering the exact date you can provide a month and
-        year. You’ll have a chance to provide more information later.)
-      </p>
-      {eventHelpText}
-    </div>
-  );
-};
