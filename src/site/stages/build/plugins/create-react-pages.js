d70df93144d13b900b7a5efe0f72ee198e5275de
Jeff Balboni <jeff@adhocteam.us>
2019-01-31 11:49:24 -0500

    Comments, clean up

--- src/site/stages/build/plugins/create-react-pages.js
+++ src/site/stages/build/plugins/create-react-pages.js
@@ -7,33 +7,39 @@ const prodEnvironments = new Set([BUILD_TYPE.vagovprod]);
 
 function createTemporaryReactPages(options) {
   return (files, metalsmith, done) => {
-    const root = path.join(__dirname, '../../../../..');
-    manifestHelpers
-      .getAppManifests(root)
-      .filter(m => m.rootUrl)
-      .forEach(({ entryName, appName, rootUrl }) => {
-        const trimmedUrl = rootUrl.endsWith('/')
-          ? rootUrl.substring(1, rootUrl.length - 1)
-          : rootUrl.substring(1);
-        const filePath = `${trimmedUrl}/index.html`;
-        if (!prodEnvironments.has(options.buildtype) && !files[filePath]) {
-          console.log(`Creating temp React landing page: ${rootUrl}`);
-          files[filePath] = {
-            title: appName,
-            entryname: entryName,
-            layout: 'page-react.html',
-            path: trimmedUrl,
-            contents: new Buffer(`
-            <nav aria-label="Breadcrumb" aria-live="polite" class="va-nav-breadcrumbs" id="va-breadcrumbs">
-              <ul class="row va-nav-breadcrumbs-list columns" id="va-breadcrumbs-list">
-                <li><a href="/">Home</a></li>
-                <li><a aria-current="page" href="${rootUrl}">${appName}</a></li>
-              </ul>
-            </nav>
-            `),
-          };
-        }
-      });
+    if (!prodEnvironments.has(options.buildtype)) {
+      const root = path.join(__dirname, '../../../../..');
+      manifestHelpers
+        .getAppManifests(root)
+        // Skip manifests that are mapping the url from settings.js
+        .filter(m => m.rootUrl)
+        .forEach(({ entryName, appName, rootUrl }) => {
+          // Removing leading and trailing slashes
+          const trimmedUrl = rootUrl.endsWith('/')
+            ? rootUrl.substring(1, rootUrl.length - 1)
+            : rootUrl.substring(1);
+          const filePath = `${trimmedUrl}/index.html`;
+
+          if (!files[filePath]) {
+            console.log(`Creating temp React landing page: ${rootUrl}`);
+            files[filePath] = {
+              title: appName,
+              entryname: entryName,
+              layout: 'page-react.html',
+              path: trimmedUrl,
+              contents: new Buffer(`
+              <nav aria-label="Breadcrumb" aria-live="polite" class="va-nav-breadcrumbs" id="va-breadcrumbs">
+                <ul class="row va-nav-breadcrumbs-list columns" id="va-breadcrumbs-list">
+                  <li><a href="/">Home</a></li>
+                  <li><a aria-current="page" href="${rootUrl}">${appName}</a></li>
+                </ul>
+              </nav>
+              `),
+            };
+          }
+        });
+    }
+
     done();
   };
 }
