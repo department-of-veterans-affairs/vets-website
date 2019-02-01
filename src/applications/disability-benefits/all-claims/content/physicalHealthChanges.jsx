e35f8b33d9bd6b99a74465c1270f2e58a46e18ab
Mark Greenburg <marek.greenburg@gmail.com>
2019-02-01 11:29:24 -0600

    781a ptsd physical health changes content updates

--- src/applications/disability-benefits/all-claims/content/physicalHealthChanges.jsx
+++ src/applications/disability-benefits/all-claims/content/physicalHealthChanges.jsx
@@ -4,15 +4,9 @@ export const changeDescription = (
   <div>
     <h5>Changes in physical health</h5>
     <p>
-      Now we’re going to ask you questions about changes in your physical,
-      mental, professional and social behavior as a result of the event or
-      events. You may have experienced some of these or none of them. There’s
-      space at the end of these questions for you to add comments or describe
-      other changes in behavior you’d like us to know.
-    </p>
-    <p>
-      Please identify any changes in your physical health as a result of the
-      event(s). (Check any that apply.)
+      Please let us know about any changes in your physical health as a result
+      of the event. You may have experienced some or none of these. (Check any
+      that apply.)
     </p>
   </div>
 );
