a892b423231ba83b6211c50a59c2f8328862fc9b
Mark Greenburg <marek.greenburg@gmail.com>
2019-02-01 11:38:54 -0600

    ptsd 781a mental health changes content updates

--- src/applications/disability-benefits/all-claims/content/mentalHealthChanges.jsx
+++ src/applications/disability-benefits/all-claims/content/mentalHealthChanges.jsx
@@ -5,8 +5,8 @@ export const changeDescription = (
     <h5>Changes in mental health or substance abuse</h5>
     <p>
       Please tell us about any changes in your mental health as a result of the
-      event(s). You may have experienced some or none of these. (Please check
-      any that apply.)
+      event. You may have experienced some or none of these. (Please check any
+      that apply.)
     </p>
   </div>
 );
