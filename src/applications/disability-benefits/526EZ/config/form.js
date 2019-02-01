f6ec14856a6c0966de00cb3a773bbf6db1d7215e
Christopher Valarida <chris.valarida@adhocteam.us>
2019-01-31 17:06:46 -0800

    Redirect from v1 to v2

--- src/applications/disability-benefits/526EZ/config/form.js
+++ src/applications/disability-benefits/526EZ/config/form.js
@@ -60,6 +60,7 @@ import {
 import {
   hasGuardOrReservePeriod,
   queryForFacilities,
+  getBaseUrl,
 } from '../../all-claims/utils';
 
 import {
@@ -117,6 +118,7 @@ const formConfig = {
   submitUrl: `${environment.API_URL}/v0/disability_compensation_form/submit`,
   trackingPrefix: 'disability-526EZ-',
   formId: '21-526EZ',
+  getBaseUrl,
   version: 1,
   migrations: [],
   prefillTransformer,
