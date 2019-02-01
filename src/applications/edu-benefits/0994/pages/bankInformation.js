5b77d754e3b4284d8ff40c8391249de789c26234
Rob Felder <felder_robert@bah.com>
2019-02-01 10:43:10 -0500

    Removing bank info functional updates to reduce size

--- src/applications/edu-benefits/0994/pages/bankInformation.js
+++ src/applications/edu-benefits/0994/pages/bankInformation.js
@@ -1,7 +1,5 @@
 import fullSchema from 'vets-json-schema/dist/22-0994-schema.json';
 import bankAccountUI from '../../../../platform/forms/definitions/bankAccount';
-import ReviewCardField from '../components/ReviewCardField';
-import PaymentView from '../components/PaymentView';
 
 import {
   bankInfoTitle,
@@ -19,18 +17,7 @@ const {
 export const uiSchema = {
   'ui:title': bankInfoTitle,
   'ui:description': bankInfoDescription,
-  'view:bankAccount': {
-    'ui:field': ReviewCardField,
-    'ui:options': {
-      viewComponent: PaymentView,
-      reviewTitle: 'Payment information',
-      editTitle: 'Add new bank account',
-      itemName: 'account',
-      startInEdit: formData => !formData['view:hasBankInformation'],
-      volatileData: true,
-    },
-    bankAccount: bankAccountUI,
-  },
+  bankAccount: bankAccountUI,
   'view:bankInfoNote': {
     'ui:title': ' ',
     'ui:description': bankInfoNote,
@@ -44,17 +31,13 @@ export const uiSchema = {
 export const schema = {
   type: 'object',
   properties: {
-    'view:bankAccount': {
+    bankAccount: {
       type: 'object',
+      required: ['accountType', 'routingNumber', 'accountNumber'],
       properties: {
-        bankAccount: {
-          type: 'object',
-          properties: {
-            accountType,
-            routingNumber,
-            accountNumber,
-          },
-        },
+        accountType,
+        routingNumber,
+        accountNumber,
       },
     },
     'view:bankInfoNote': {
