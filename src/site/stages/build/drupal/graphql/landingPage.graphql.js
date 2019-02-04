a0e9a5d86ccf10b9b0d36e7b33d0386a8c829696
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2019-02-04 13:46:49 -0700

    VAGOV-1471: Admin taxonomy entity reference field fragment for hub landing pages query

--- src/site/stages/build/drupal/graphql/landingPage.graphql.js
+++ src/site/stages/build/drupal/graphql/landingPage.graphql.js
@@ -1,8 +1,14 @@
+const administration = require('./taxonomy-fragments/administration.taxonomy.graphql');
+
 /**
  * The top-level page for a section of the website.
  * Examples include /health-care/, /disability/, etc.
  */
+const ADMIN = '...administration';
+
 module.exports = `
+  ${administration};
+  
   fragment landingPage on NodeLandingPage {
     entityUrl {
       ... on EntityCanonicalUrl {
@@ -20,5 +26,6 @@ module.exports = `
     entityPublished
     title
     fieldIntroText
+    ${ADMIN}
   }
 `;
---
+++ src/site/stages/build/drupal/graphql/taxonomy-fragments/administration.taxonomy.graphql.js
@@ -0,0 +1,38 @@
+/**
+ * A Drupal entity reference field that references the administration taxonomy.
+ * Appears on hub landing pages.
+ */
+
+module.exports = `
+    fragment administration on NodeLandingPage {
+        fieldAdministration {
+        ... on FieldNodeFieldAdministration {
+          entity {
+            ... on TaxonomyTermAdministration {
+              fieldIntroText
+              fieldDescription
+              fieldAcronym
+              fieldLink {
+                url {
+                  path
+                }
+              }
+              fieldEmailUpdatesUrl
+              fieldEmailUpdatesLinkText
+              fieldSocialMediaLinks {
+                platformValues
+                value
+                platform
+              }
+              fieldSupportServices {
+                entity {
+                  nid
+                  title
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+`;
