6dbf43829718fccdd0bb114bdbc3e2d34f74f9c0
Jeff Balboni <jeff@adhocteam.us>
2019-02-05 14:04:22 -0500

    Update query

--- src/site/stages/build/drupal/graphql/GetLatestPageById.graphql.js
+++ src/site/stages/build/drupal/graphql/GetLatestPageById.graphql.js
@@ -1,5 +1,6 @@
 const landingPage = require('./landingPage.graphql');
 const page = require('./page.graphql');
+const fragments = require('./fragments.graphql');
 
 /**
  * Queries for a page by the page path. This will most likely need to be updated once we determine
@@ -8,6 +9,7 @@ const page = require('./page.graphql');
  */
 module.exports = `
 
+  ${fragments}
   ${landingPage}
   ${page}
 
