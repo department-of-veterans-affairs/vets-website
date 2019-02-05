567d2ec75d2b9e7043bf859348f4b34c5901b1ae
Jeff Balboni <jeff@adhocteam.us>
2019-02-05 14:50:22 -0500

    Update comment

--- src/site/stages/build/drupal/graphql/GetLatestPageById.graphql.js
+++ src/site/stages/build/drupal/graphql/GetLatestPageById.graphql.js
@@ -3,8 +3,7 @@ const page = require('./page.graphql');
 const fragments = require('./fragments.graphql');
 
 /**
- * Queries for a page by the page path. This will most likely need to be updated once we determine
- * how we'll query for drafts - will we switch to nodeId, or something else?
+ * Queries for a page by the node id, getting the latest revision
  * To execute, run this query at http://staging.va.agile6.com/graphql/explorer.
  */
 module.exports = `
