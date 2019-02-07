863f2ded8d81bd3f81163bac4fab1539c3d563dc
Matt Roth <mattcroth@gmail.com>
2019-02-07 12:46:06 -0500

    VetTec: Write e2e tests for all functionality in the launched product through submission#16633 (#9515)
    As a MVP development team, I need to write e2e tests so that we have full test coverage.

--- src/applications/edu-benefits/tests/0994/schema/transformedData.js
+++ src/applications/edu-benefits/tests/0994/schema/transformedData.js
@@ -6,8 +6,15 @@ const transformedMinimalDataActual = {
   },
   appliedForVaEducationBenefits: false,
   activeDuty: true,
-  dayTimePhone: '4445551212',
+  dayTimePhone: '1234567890',
   emailAddress: 'test2@test1.net',
+  mailingAddress: {
+    street: 'MILITARY ADDY 3',
+    city: 'Nowhere',
+    country: 'USA',
+    state: 'MI',
+    postalCode: '22312',
+  },
 };
 
 export const transformedMinimalData = JSON.stringify({
@@ -29,7 +36,7 @@ const transformedMaximalDataActual = {
     street: 'MILITARY ADDY 3',
     street2: 'teasdf',
     street3: 'teasdf',
-    city: 'DPO',
+    city: 'Nowhere',
     country: 'USA',
     state: 'MI',
     postalCode: '22312',
