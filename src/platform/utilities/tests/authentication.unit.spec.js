19ec7ff9a6c3800bb2f3bc14c8c8e61760a32792
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2019-02-11 14:15:46 -0500

    Updated unit tests.

--- src/platform/utilities/tests/authentication.unit.spec.js
+++ src/platform/utilities/tests/authentication.unit.spec.js
@@ -8,8 +8,6 @@ import {
   signup,
 } from '../../user/authentication/utilities';
 
-import { mockApiRequest, resetFetch } from '../../testing/unit/helpers';
-
 let oldSessionStorage;
 let oldWindow;
 
@@ -28,53 +26,35 @@ const fakeWindow = () => {
   };
 };
 
-describe('auth URL helpers', () => {
+describe('authentication URL helpers', () => {
   beforeEach(fakeWindow);
   afterEach(() => {
     global.window = oldWindow;
     global.sessionStorage = oldSessionStorage;
-    resetFetch();
-  });
-
-  it('should redirect to an error page', () => {
-    mockApiRequest({ error: "Couldn't find url" }, false);
-    login('idme').then(() => {
-      expect(global.window.location).to.include('/auth/login/callback');
-    });
   });
 
   it('should redirect for signup', () => {
-    mockApiRequest({ url: 'signup-url' });
-    signup().then(() => {
-      expect(global.window.location).to.eq('signup-url');
-    });
+    signup();
+    expect(global.window.location).to.include('/sessions/idme/new?signup');
   });
 
   it('should redirect for login', () => {
-    mockApiRequest({ url: 'login-url' });
-    login('idme').then(() => {
-      expect(global.window.location).to.eq('login-url');
-    });
+    login('idme');
+    expect(global.window.location).to.include('/sessions/idme/new');
   });
 
   it('should redirect for logout', () => {
-    mockApiRequest({ url: 'logout-url' });
-    logout().then(() => {
-      expect(global.window.location).to.eq('logout-url');
-    });
+    logout();
+    expect(global.window.location).to.include('/sessions/slo/new');
   });
 
   it('should redirect for MFA', () => {
-    mockApiRequest({ url: 'mfa-url' });
-    mfa().then(() => {
-      expect(global.window.location).to.eq('mfa-url');
-    });
+    mfa();
+    expect(global.window.location).to.include('/sessions/mfa/new');
   });
 
   it('should redirect for verify', () => {
-    mockApiRequest({ url: 'verify-url' });
-    verify().then(() => {
-      expect(global.window.location).to.eq('verify-url');
-    });
+    verify();
+    expect(global.window.location).to.include('/sessions/verify/new');
   });
 });
