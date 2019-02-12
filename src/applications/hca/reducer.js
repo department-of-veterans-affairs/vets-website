b81737f159ed563a0d52edc5ce54d063b6e3acc0
Erik P. Hansen <eph@erikphansen.com>
2019-02-12 08:34:43 -0800

    Add new IDForm and IDPage to hold it. (#9561)
    * Add new IDForm and IDPage to hold it.
    
    Add IDPage as new route to HCA
    
    Expect nonFormRoutes to be defined in the formConfig
    
    Switch back to `introduction` path for the ID page
    
    Connect the ID Form page to Redux
    
    * better gating of ID Page

--- src/applications/hca/reducer.js
+++ src/applications/hca/reducer.js
@@ -1,6 +1,30 @@
 import formConfig from './config/form';
-import { createSaveInProgressFormReducer } from '../../platform/forms/save-in-progress/reducers';
+import { createSaveInProgressFormReducer } from 'platform/forms/save-in-progress/reducers';
+import {
+  SUBMIT_ID_FORM_STARTED,
+  SUBMIT_ID_FORM_SUCCEEDED,
+  SUBMIT_ID_FORM_FAILED,
+} from './actions';
+
+const initialState = {
+  isLoading: false,
+  error: null,
+};
+
+function hcaIDForm(state = initialState, action) {
+  switch (action.type) {
+    case SUBMIT_ID_FORM_STARTED:
+      return { ...state, isLoading: true };
+    case SUBMIT_ID_FORM_SUCCEEDED:
+      return { ...state, isLoading: false };
+    case SUBMIT_ID_FORM_FAILED:
+      return { ...state, isLoading: false, error: action.error };
+    default:
+      return state;
+  }
+}
 
 export default {
   form: createSaveInProgressFormReducer(formConfig),
+  hcaIDForm,
 };
