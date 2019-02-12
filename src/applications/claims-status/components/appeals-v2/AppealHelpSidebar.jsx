6ed8f8c9513009be05fafa97b7edc98d81bd9c66
Chris Given <cmgiven@gmail.com>
2019-02-12 09:38:07 -0500

    Update appeals status "current status" and "next events" (#9553)
    * Update appeals status alerts (#9554)
    * Refactor AppealsListItemV2 for the personalization dashboard
    * Update hotline information

--- src/applications/claims-status/components/appeals-v2/AppealHelpSidebar.jsx
+++ src/applications/claims-status/components/appeals-v2/AppealHelpSidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Raven from 'raven-js';
 
+import { AOJS } from '../../utils/appeals-v2-helpers';
+
 const vbaVersion = (
   <div>
     <h2 className="help-heading">Need help?</h2>
-    <p>Call the Veterans Affairs Benefits and Services</p>
+    <p>Call Veterans Affairs Benefits and Services</p>
     <p className="help-phone-number">
       <a className="help-phone-number-link" href="tel:1-800-827-1000">
         1-800-827-1000
@@ -14,50 +16,38 @@ const vbaVersion = (
   </div>
 );
 
-const boardVersion = (
+const vhaVersion = (
   <div>
     <h2 className="help-heading">Need help?</h2>
-    <p>Call the Board of Veterans’ Appeals</p>
+    <p>Call Health Care Benefits</p>
     <p className="help-phone-number">
-      <a className="help-phone-number-link" href="tel:1-800-923-8387">
-        1-800-923-8387
+      <a className="help-phone-number-link" href="tel:1-877-222-8387">
+        1-877-222-VETS (8387)
       </a>
     </p>
-    <p>Monday - Friday, 9:00am - 4:30pm (ET)</p>
+    <p>Monday - Friday, 8:00am - 8:00pm (ET)</p>
   </div>
 );
 
 /**
  * Displays the "Need help?" sidebar content based on the appeal's location.
  *
- * @param {String} location  The location of the appeal. Possible options:
- *                            ['aoj', 'bva']
  * @param {String} aoj       The Agency of Original Jurisdiction.
  *                            Used if the location is 'aoj'
  *                            Possible options:
  *                            ['vba', 'vha', 'nca', 'other']
  */
-export default function AppealHelpSidebar({ location, aoj }) {
-  if (location === 'aoj') {
-    // If the location is 'aoj', we have to check which agency it came from to show
-    //  the appropriate information.
-    switch (aoj) {
-      case 'vba':
-        return vbaVersion;
-      case 'vha':
-        return null; // vha version (coming soon to a sidebar near you!)
-      case 'nca':
-        return null; // nca version (coming soon to a sidebar near you!)
-      case 'other':
-        return boardVersion;
-      default:
-        Raven.captureMessage(`appeal-status-unexpected-aoj: ${aoj}`);
-    }
-  } else if (location === 'bva') {
-    return boardVersion;
-  } else {
-    Raven.captureMessage(`appeal-status-unexpected-location: ${location}`);
+export default function AppealHelpSidebar({ aoj }) {
+  switch (aoj) {
+    case AOJS.vba:
+      return vbaVersion;
+    case AOJS.vha:
+      return vhaVersion;
+    case AOJS.nca:
+      return null; // nca version (coming soon to a sidebar near you!)
+    case AOJS.other:
+      return null;
+    default:
+      Raven.captureMessage(`appeal-status-unexpected-aoj: ${aoj}`);
   }
-
-  return null;
 }
