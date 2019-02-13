25e30ade731a50d3cfee4926065255f8410eaf8f
Rob Felder <felder_robert@bah.com>
2019-02-13 14:50:02 -0500

    BAH VET TEC Content Updates (#9576)
    * Updating content
    
    * Various content updates throughout VET TEC application

--- src/applications/edu-benefits/0994/content/militaryService.jsx
+++ src/applications/edu-benefits/0994/content/militaryService.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 export const activeDutyNotice = (
   <p>
-    VET TEC is an education benefit available only to Veterans. Active duty
-    service members are not eligible. If you’re on active duty now, you can
-    still apply to see if you’ll be eligible once you are no longer active duty.
+    VET TEC is available only to Veterans. Active duty service members are not
+    eligible. If you’re on active duty now, please consider applying for VET TEC
+    in the future.
   </p>
 );
 
 export const benefitNotice = (
   <p>
-    If you are called to active duty during your participation in VET TEC, it
-    may impact your eligibility for the program. If this happens, VA will notify
-    you on how to proceed.
+    If you’re called to active duty during your participation in VET TEC, it may
+    impact your eligibility for the program. If this happens, please notify VA
+    as soon as possible.
   </p>
 );
