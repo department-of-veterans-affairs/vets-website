32b6114534776ad6db1b49deb52e78974f57097e
Christopher Valarida <chris.valarida@adhocteam.us>
2019-02-13 12:21:22 -0800

    Use the review card for the mailing address

--- src/applications/disability-benefits/all-claims/pages/contactInformation.js
+++ src/applications/disability-benefits/all-claims/pages/contactInformation.js
@@ -62,6 +62,7 @@ export const uiSchema = {
   mailingAddress: addressUISchema(
     ADDRESS_TYPES.mailingAddress,
     'Mailing address',
+    true,
   ),
   'view:hasForwardingAddress': {
     'ui:title': 'My address will be changing soon.',
