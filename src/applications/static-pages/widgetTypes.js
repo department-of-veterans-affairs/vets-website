c83a915ecffef2b536ad05d431bae8079a165c4c
Jeff Balboni <jeff@adhocteam.us>
2019-02-13 12:20:49 -0500

    Add widget types file

---
+++ src/applications/static-pages/widgetTypes.js
@@ -0,0 +1,9 @@
+export default {
+  CTA: 'cta',
+  EDUCATION_APP_STATUS: 'education-app-status',
+  HEALTH_CARE_APP_STATUS: 'health-care-app-status',
+  PENSION_APP_STATUS: 'pension-app-status',
+  BURIAL_APP_STATUS: 'burial-app-status',
+  OPT_OUT_APP_STATUS: 'opt-out-app-status',
+  DISABILITY_APP_STATUS: 'disability-app-status',
+};
