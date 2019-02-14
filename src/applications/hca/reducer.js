4830904103329712de37b0c0bd740fed524575f0
Erik P. Hansen <eph@erikphansen.com>
2019-02-14 09:44:33 -0800

    rework redux state; move more logic to redux and simplify component a bit

--- src/applications/hca/reducer.js
+++ src/applications/hca/reducer.js
@@ -1,5 +1,6 @@
 import formConfig from './config/form';
 import { createSaveInProgressFormReducer } from 'platform/forms/save-in-progress/reducers';
+import { UPDATE_LOGGEDIN_STATUS } from 'platform/user/authentication/actions';
 import {
   SUBMIT_ID_FORM_STARTED,
   SUBMIT_ID_FORM_SUCCEEDED,
@@ -7,18 +8,21 @@ import {
 } from './actions';
 
 const initialState = {
-  isLoading: false,
+  isSubmitting: false,
   error: null,
+  shouldHideIDForm: false,
 };
 
 function hcaIDForm(state = initialState, action) {
   switch (action.type) {
     case SUBMIT_ID_FORM_STARTED:
-      return { ...state, isLoading: true };
+      return { ...state, isSubmitting: true };
     case SUBMIT_ID_FORM_SUCCEEDED:
-      return { ...state, isLoading: false };
+      return { ...state, isSubmitting: false };
     case SUBMIT_ID_FORM_FAILED:
-      return { ...state, isLoading: false, error: action.error };
+      return { ...state, isSubmitting: false, error: action.error };
+    case UPDATE_LOGGEDIN_STATUS:
+      return { ...state, shouldHideIDForm: action.value };
     default:
       return state;
   }
