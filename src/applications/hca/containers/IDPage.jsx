f330aa3976b5cea15a3d2b4d94a03323285eb9ed
Erik P. Hansen <eph@erikphansen.com>
2019-02-14 07:52:15 -0800

    inline all the rendering

--- src/applications/hca/containers/IDPage.jsx
+++ src/applications/hca/containers/IDPage.jsx
@@ -26,70 +26,67 @@ class IDPage extends React.Component {
     }
   }
 
-  getContent = () => {
-    if (this.props.isProfileLoading) {
-      return <LoadingIndicator />;
-    }
-    return (
-      <React.Fragment>
-        <AlertBox
-          isVisible
-          status="info"
-          headline="Help us fit this application to your specific needs"
-          content={
-            <React.Fragment>
-              <p>
-                Before you start your health care application, please provide
-                the information below. This will help us make sure the
-                application gathers the right information for us to determine
-                your eligibility.
-              </p>
-              <p>
-                <strong>Want to skip this step?</strong>
-              </p>
-              <button
-                className="va-button-link"
-                onClick={() => this.props.toggleLoginModal(true)}
-              >
-                Sign in to start your application.
-              </button>
-            </React.Fragment>
-          }
-        />
-        <br />
-        <IDForm
-          isLoading={this.props.isLoading}
-          handleSubmit={this.props.submitForm}
-        />
-        {this.props.error && (
-          <AlertBox
-            isVisible
-            status="error"
-            headline="Please sign in to continue your application"
-            content={
-              <React.Fragment>
-                <p>
-                  We’re sorry for the interruption, but we need you to review
-                  some information before you continue applying. Please sign in
-                  below to review. If you don’t have an account, you can create
-                  one now.
-                </p>
-                <button className="usa-button-primary" onClick={this.goToLogin}>
-                  Sign in to VA.gov
-                </button>
-              </React.Fragment>
-            }
-          />
-        )}
-      </React.Fragment>
-    );
-  };
-
   render() {
     return (
       <div className="schemaform-intro">
         <FormTitle title="Apply for health care benefits" />
-        {this.getContent()}
+        {this.props.isProfileLoading && <LoadingIndicator />}
+        {!this.props.isProfileLoading && (
+          <React.Fragment>
+            <AlertBox
+              isVisible
+              status="info"
+              headline="Help us fit this application to your specific needs"
+              content={
+                <React.Fragment>
+                  <p>
+                    Before you start your health care application, please
+                    provide the information below. This will help us make sure
+                    the application gathers the right information for us to
+                    determine your eligibility.
+                  </p>
+                  <p>
+                    <strong>Want to skip this step?</strong>
+                  </p>
+                  <button
+                    className="va-button-link"
+                    onClick={() => this.props.toggleLoginModal(true)}
+                  >
+                    Sign in to start your application.
+                  </button>
+                </React.Fragment>
+              }
+            />
+            <br />
+            <IDForm
+              isLoading={this.props.isLoading}
+              handleSubmit={this.props.submitForm}
+            />
+            {this.props.error && (
+              <AlertBox
+                isVisible
+                status="error"
+                headline="Please sign in to continue your application"
+                content={
+                  <React.Fragment>
+                    <p>
+                      We’re sorry for the interruption, but we need you to
+                      review some information before you continue applying.
+                      Please sign in below to review. If you don’t have an
+                      account, you can create one now.
+                    </p>
+                    <button
+                      className="usa-button-primary"
+                      onClick={this.goToLogin}
+                    >
+                      Sign in to VA.gov
+                    </button>
+                  </React.Fragment>
+                }
+              />
+            )}
+          </React.Fragment>
+        )}
         <div className="omb-info--container" style={{ paddingLeft: '0px' }}>
           <OMBInfo resBurden={30} ombNumber="2900-0091" expDate="05/31/2018" />
         </div>
