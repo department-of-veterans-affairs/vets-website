85a230b7c000228e2b11081e62477db33dec4670
Matt Roth <roth_matthew@bah.com>
2019-02-14 14:36:51 -0500

    whoops

--- src/applications/edu-benefits/tests/0994/utils.unit.spec.js
+++ src/applications/edu-benefits/tests/0994/utils.unit.spec.js
@@ -1,9 +1,20 @@
+import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
-import { maskBankInformation } from '../../0994/utils';
+import { srSubstitute, maskBankInformation } from '../../0994/utils';
 
 describe('0994 utils', () => {
+  describe('srSubstitute', () => {
+    it('do the srSubstitute', () => {
+      const srIgnored = <span>Test</span>;
+      const substitutionText = 'substitutionText';
+      const srSub = shallow(srSubstitute(srIgnored, substitutionText));
+
+      expect(srSub.text()).to.contain('Test');
+      srSub.unmount();
+    });
+  });
   describe('maskBankInformation', () => {
     it('should mask all but last 4 characters', () => {
       const maskedBankInfo = shallow(maskBankInformation('000000000', 4));
