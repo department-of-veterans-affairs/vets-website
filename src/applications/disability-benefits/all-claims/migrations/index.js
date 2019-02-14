6c2dc7a770dad703a799be499f649ec25ec31210
Christopher Valarida <chris.valarida@adhocteam.us>
2019-02-14 14:05:48 -0800

    Add migration

--- src/applications/disability-benefits/all-claims/migrations/index.js
+++ src/applications/disability-benefits/all-claims/migrations/index.js
@@ -1,8 +1,9 @@
 import redirectToClaimTypePage from './01-require-claim-type';
+import convertCountryCode from './02-convert-country-code';
 
 // We launched at version 1 and not version 0, so the first _real_ migration is at
 //  migrations[1]
 // NOTE: This will probably just get skipped over, but it's here to be safe
 const emptyMigration = savedData => savedData;
 
-export default [emptyMigration, redirectToClaimTypePage];
+export default [emptyMigration, redirectToClaimTypePage, convertCountryCode];
