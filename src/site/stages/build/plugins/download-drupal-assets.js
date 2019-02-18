54fd846ab4492da230cce4b83e4dd1728c0b67b5
Jeff Balboni <jeff@adhocteam.us>
2019-02-18 11:24:54 -0500

    Add some comments

--- src/site/stages/build/plugins/download-drupal-assets.js
+++ src/site/stages/build/plugins/download-drupal-assets.js
@@ -10,10 +10,16 @@ const DRUPAL_COLORIZED_OUTPUT = chalk.rgb(73, 167, 222);
 // eslint-disable-next-line no-console
 const log = message => console.log(DRUPAL_COLORIZED_OUTPUT(message));
 
+/*
+ * Convert a Drupal asset url to a local one
+ */
 function convertAssetPath(drupalInstance, url) {
+  // After this path are other folders in the image paths,
+  // but it's hard to tell if we can strip them, so I'm leaving them alone
   const withoutHost = url.replace(`${drupalInstance}/sites/default/files/`, '');
   const path = withoutHost.split('?', 2)[0];
 
+  // This is sort of naive, but we'd like to have images in the img folder
   if (
     ['png', 'jpg', 'jpeg', 'gif', 'svg'].some(ext =>
       path.toLowerCase().endsWith(ext),
@@ -25,6 +31,10 @@ function convertAssetPath(drupalInstance, url) {
   return `files/${path}`;
 }
 
+/*
+ * Simple attribute update that replaces the url for the asset
+ * with the local one
+ */
 function updateAttr(attr, doc, client) {
   const assetsToDownload = [];
   doc(`[${attr}^="${client.getSiteUri()}/sites"]`).each((i, el) => {
@@ -67,14 +77,17 @@ function downloadDrupalAssets(options) {
         if (hrefAssets.length) fileUpdated = true;
         assetsToDownload = assetsToDownload.concat(hrefAssets);
 
+        // srset is more complicated, a requires us to parse the attribute out
         // eslint-disable-next-line no-loop-func
         doc(`[srcset*="${drupalClient.getSiteUri()}/sites"]`).each((i, el) => {
           fileUpdated = true;
           const item = doc(el);
           const srcAttr = item.attr('srcset');
+          // srcset is a comma delimited list of srcs
           const sources = srcAttr.split(',');
           const newPaths = [];
           sources.forEach(source => {
+            // Each src has a url and a size, separated by a space
             const [url, size] = source.split(' ', 2);
             const newAssetPath = convertAssetPath(
               drupalClient.getSiteUri(),
@@ -111,6 +124,8 @@ function downloadDrupalAssets(options) {
               contents: await response.buffer(),
             };
           } else {
+            // For now, not going to fail the build for a missing asset
+            // Should get caught by the broken link checker, though
             errorCount++;
             log(`Image download failed: ${response.statusText}: ${asset.src}`);
           }
