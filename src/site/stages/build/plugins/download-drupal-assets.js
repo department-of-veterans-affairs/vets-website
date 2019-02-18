63bd0ce4a9312d852cab19c3447862808392fe18
Jeff Balboni <jeff@adhocteam.us>
2019-02-18 10:50:26 -0500

    Cleanup

--- src/site/stages/build/plugins/download-drupal-assets.js
+++ src/site/stages/build/plugins/download-drupal-assets.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-param-reassign */
 require('isomorphic-fetch');
 const cheerio = require('cheerio');
-const getDrupalClient = require('../drupal/api');
 const chalk = require('chalk');
+
+const getDrupalClient = require('../drupal/api');
 
 const DRUPAL_COLORIZED_OUTPUT = chalk.rgb(73, 167, 222);
 
@@ -24,11 +25,28 @@ function convertAssetPath(drupalInstance, url) {
   return `files/${path}`;
 }
 
+function updateAttr(attr, doc, client) {
+  const assetsToDownload = [];
+  doc(`[${attr}^="${client.getSiteUri()}/sites"]`).each((i, el) => {
+    const item = doc(el);
+    const srcAttr = item.attr(attr);
+    const newAssetPath = convertAssetPath(client.getSiteUri(), srcAttr);
+    assetsToDownload.push({
+      src: srcAttr,
+      dest: newAssetPath,
+    });
+
+    item.attr('src', `/${newAssetPath}`);
+  });
+
+  return assetsToDownload;
+}
+
 function downloadDrupalAssets(options) {
   const drupalClient = getDrupalClient(options);
 
   return async (files, metalsmith, done) => {
-    const assetsToDownload = [];
+    let assetsToDownload = [];
 
     for (const fileName of Object.keys(files)) {
       const file = files[fileName];
@@ -36,20 +54,40 @@ function downloadDrupalAssets(options) {
 
       if (file.isDrupalPage && fileName.endsWith('html')) {
         const doc = cheerio.load(file.contents);
-        doc(`[src^="${drupalClient.getSiteUri()}"]`).each((i, el) => {
+
+        const srcAssets = updateAttr('src', doc, drupalClient);
+        if (srcAssets.length) fileUpdated = true;
+        assetsToDownload = assetsToDownload.concat(srcAssets);
+
+        const dataSrcAssets = updateAttr('data-src', doc, drupalClient);
+        if (dataSrcAssets.length) fileUpdated = true;
+        assetsToDownload = assetsToDownload.concat(dataSrcAssets);
+
+        const hrefAssets = updateAttr('href', doc, drupalClient);
+        if (hrefAssets.length) fileUpdated = true;
+        assetsToDownload = assetsToDownload.concat(hrefAssets);
+
+        // eslint-disable-next-line no-loop-func
+        doc(`[srcset*="${drupalClient.getSiteUri()}/sites"]`).each((i, el) => {
           fileUpdated = true;
           const item = doc(el);
-          const srcAttr = item.attr('src');
-          const newAssetPath = convertAssetPath(
-            drupalClient.getSiteUri(),
-            srcAttr,
-          );
-          assetsToDownload.push({
-            src: srcAttr,
-            dest: newAssetPath,
+          const srcAttr = item.attr('srcset');
+          const sources = srcAttr.split(',');
+          const newPaths = [];
+          sources.forEach(source => {
+            const [url, size] = source.split(' ', 2);
+            const newAssetPath = convertAssetPath(
+              drupalClient.getSiteUri(),
+              url,
+            );
+            assetsToDownload.push({
+              src: url,
+              dest: newAssetPath,
+            });
+            newPaths.push(`/${newAssetPath} ${size}`);
           });
 
-          item.attr('src', `/${newAssetPath}`);
+          item.attr('srcset', newPaths.join(','));
         });
 
         if (fileUpdated) {
@@ -59,24 +97,29 @@ function downloadDrupalAssets(options) {
     }
 
     if (assetsToDownload.length) {
+      let downloadCount = 0;
+      let errorCount = 0;
       const downloads = assetsToDownload.map(async asset => {
         if (!files[asset.dest]) {
           const response = await fetch(asset.src);
 
           if (response.ok) {
+            downloadCount++;
             files[asset.dest] = {
               path: asset.dest,
               isDrupalAsset: true,
               contents: await response.buffer(),
             };
           } else {
-            done(`Image download failed: ${asset.src}`);
+            errorCount++;
+            log(`Image download failed: ${response.statusText}: ${asset.src}`);
           }
         }
       });
 
       await Promise.all(downloads);
-      log(`Downloaded ${downloads.length} assets from Drupal`);
+      log(`Downloaded ${downloadCount} asset(s) from Drupal`);
+      log(`${errorCount} error(s) downloading assets`);
     }
 
     done();
