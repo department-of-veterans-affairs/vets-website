75b5eb62fc95e18428246045c6a9a94999dae4ab
Ben <bshyong@users.noreply.github.com>
2019-02-18 21:44:51 -0600

    526 V2 beta gate (#9587)
    * add beta enrollment page for all-claims
    
    * gate all claims with beta app
    
    * button text default prop
    
    * add beta banner and probability logic
    
    * fix banner url
    
    * update percentages and only show banner in production
    
    * remove uncessary conditional
    
    * typo in feature name

--- src/applications/disability-benefits/all-claims/Form526EZApp.jsx
+++ src/applications/disability-benefits/all-claims/Form526EZApp.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import RoutedSavableApp from '../../../platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from './config/form';
+import BetaApp from '../../beta-enrollment/containers/BetaApp';
+import { features } from '../../beta-enrollment/routes';
 
 import ITFWrapper from './containers/ITFWrapper';
 import EVSSClaimsGate from './containers/EVSSClaimsGate';
 
 export default function Form526Entry({ location, children }) {
-  return (
+  const content = (
     <EVSSClaimsGate location={location}>
       <ITFWrapper location={location}>
         <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
@@ -15,5 +17,15 @@ export default function Form526Entry({ location, children }) {
         </RoutedSavableApp>
       </ITFWrapper>
     </EVSSClaimsGate>
+  );
+
+  // wraps the app and redirects user if they are not enrolled
+  return (
+    <BetaApp
+      featureName={features.allClaims}
+      redirect="/disability/how-to-file-claim/"
+    >
+      {content}
+    </BetaApp>
   );
 }
