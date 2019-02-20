45bdab3e38f9b65ff6f5be7c4f7fa7dd20b95549
Ben Shyong <ben.shyong@gmail.com>
2019-02-20 01:37:04 -0600

    PR feedback; clean up

--- src/platform/user/authentication/utilities.js
+++ src/platform/user/authentication/utilities.js
@@ -1,6 +1,5 @@
 import appendQuery from 'append-query';
 import Raven from 'raven-js';
-import URLSearchParams from 'url-search-params';
 
 import recordEvent from '../../monitoring/record-event';
 import environment from '../../utilities/environment';
@@ -42,11 +41,7 @@ export function clearRavenLoginType() {
 
 function redirect(redirectUrl, clickedEvent) {
   // Keep track of the URL to return to after auth operation.
-  sessionStorage.setItem(
-    authnSettings.RETURN_URL,
-    new URLSearchParams(window.location.search).get('next') ||
-      window.location.pathname,
-  );
+  sessionStorage.setItem(authnSettings.RETURN_URL, window.location);
   recordEvent({ event: clickedEvent });
   window.location = redirectUrl;
 }
