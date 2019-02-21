6ed4ddb60ed9c76e5ff3b74361711e8ad78195e5
lightning <aurora.hay@agile6.com>
2019-02-21 11:27:41 -0800

    merge with master

--- src/site/constants/drupals.js
+++ src/site/constants/drupals.js
@@ -1,7 +1,7 @@
 const ENVIRONMENTS = require('./environments');
 
 const DRUPAL_DEV = {
-  address: 'http://staging.va.agile6.com',
+  address: 'http://dev.va.agile6.com',
   credentials: {
     username: 'api',
     password: 'drupal8',
