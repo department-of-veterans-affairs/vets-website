150a9867939b011ab385b908fa54cae67dd12ec0
Rian Fowler <rianfowler@outlook.com>
2019-02-25 15:34:56 -0700

    16689 merge us forms system (#9622)
    * pull in us-forms-system
    
    * move us-forms-system into forms-system
    
    * migrate one form
    
    * try updating imports
    
    * remove tests
    
    * fix linting issues
    
    * revert test changes
    
    * add us forms system tests
    
    * add tests, fix their lints
    
    * fix tests
    
    * remove auto prefixer

---
+++ src/platform/forms-system/test/js/widgets/TextWidget.unit.spec.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import { expect } from 'chai';
+import SkinDeep from 'skin-deep';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+
+import TextWidget from '../../../src/js/widgets/TextWidget';
+
+describe('Schemaform <TextWidget>', () => {
+  it('should render', () => {
+    const onChange = sinon.spy();
+    const tree = SkinDeep.shallowRender(
+      <TextWidget
+        id="1"
+        value="testing"
+        schema={{ type: 'string' }}
+        required
+        disabled={false}
+        onChange={onChange}
+        options={{}}
+      />,
+    );
+    expect(tree.subTree('input').props.value).to.equal('testing');
+    expect(tree.subTree('input').props.type).to.equal('text');
+  });
+  it('should render autocomplete attribute', () => {
+    const onChange = sinon.spy();
+    const tree = mount(
+      <TextWidget
+        id="1"
+        value="testing"
+        schema={{ type: 'string' }}
+        required
+        disabled={false}
+        onChange={onChange}
+        options={{
+          autocomplete: 'date',
+        }}
+      />,
+    );
+    expect(
+      tree
+        .find('input')
+        .getDOMNode()
+        .getAttribute('autocomplete'),
+    ).to.equal('date');
+    tree.unmount();
+  });
+  it('should render empty string when undefined', () => {
+    const onChange = sinon.spy();
+    const tree = SkinDeep.shallowRender(
+      <TextWidget
+        id="1"
+        schema={{ type: 'string' }}
+        required
+        disabled={false}
+        onChange={onChange}
+        options={{}}
+      />,
+    );
+    expect(tree.subTree('input').props.value).to.equal('');
+  });
+  it('should render number', () => {
+    const onChange = sinon.spy();
+    const tree = SkinDeep.shallowRender(
+      <TextWidget
+        id="1"
+        value="1"
+        schema={{ type: 'number' }}
+        required
+        disabled={false}
+        onChange={onChange}
+        options={{}}
+      />,
+    );
+    expect(tree.subTree('input').props.type).to.equal('number');
+  });
+  it('should handle change', () => {
+    const onChange = sinon.spy();
+    const tree = SkinDeep.shallowRender(
+      <TextWidget
+        id="1"
+        value="testing"
+        schema={{ type: 'string' }}
+        required
+        disabled={false}
+        onChange={onChange}
+        options={{}}
+      />,
+    );
+    tree.subTree('input').props.onChange({
+      target: {
+        value: 'nextvalue',
+      },
+    });
+    expect(onChange.calledWith('nextvalue')).to.be.true;
+  });
+  it('should handle blur', () => {
+    const onChange = sinon.spy();
+    const onBlur = sinon.spy();
+    const tree = SkinDeep.shallowRender(
+      <TextWidget
+        id="1"
+        value="testing"
+        schema={{ type: 'string' }}
+        required
+        disabled={false}
+        onChange={onChange}
+        onBlur={onBlur}
+        options={{}}
+      />,
+    );
+    tree.subTree('input').props.onBlur();
+    expect(onBlur.calledWith('1')).to.be.true;
+  });
+});
