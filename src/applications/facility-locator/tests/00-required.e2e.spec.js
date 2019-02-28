e4e3fd72506612f3333939d2a52c7d4d24e4e59e
aubreyarcangel <aubrey@thesocompany.com>
2019-02-27 21:03:32 -0500

    changed timeouts in e2e tests

--- src/applications/facility-locator/tests/00-required.e2e.spec.js
+++ src/applications/facility-locator/tests/00-required.e2e.spec.js
@@ -26,7 +26,7 @@ module.exports = E2eHelpers.createE2eTest(client => {
   client
     .waitForElementVisible('.facility-result a h5', Timeouts.slow)
     .click('.facility-result a h5')
-    .waitForElementVisible('.facility-detail', Timeouts.slow)
+    .waitForElementVisible('.facility-detail', Timeouts.verySlow)
     .axeCheck('.main');
 
   client.end();
--- src/applications/facility-locator/tests/02-breadcrumbs.e2e.spec.js
+++ src/applications/facility-locator/tests/02-breadcrumbs.e2e.spec.js
@@ -43,7 +43,7 @@ module.exports = E2eHelpers.createE2eTest(client => {
   client
     .waitForElementVisible('.facility-result a h5', Timeouts.slow)
     .click('.facility-result a h5')
-    .waitForElementVisible('.facility-detail', Timeouts.slow);
+    .waitForElementVisible('.facility-detail', Timeouts.verySlow);
 
   client.waitForElementVisible('a[aria-current="page"', Timeouts.normal);
 
