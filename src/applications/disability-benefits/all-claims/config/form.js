218ff145d09ba987a6e42222f72ded0c7a8aba17
Chris Valarida <cvalarida@gmail.com>
2019-02-28 11:32:05 -0800

    526 v2 downtime notifications (#9641)
    * Fix a comment
    
    * Add the dependencies to 526 v2
    
    * Wrap start button in the downtime notification if required for prefill
    
    * Pass downtime to SaveInProgressIntro

--- src/applications/disability-benefits/all-claims/config/form.js
+++ src/applications/disability-benefits/all-claims/config/form.js
@@ -3,6 +3,8 @@ import environment from '../../../../platform/utilities/environment';
 import FormFooter from '../../../../platform/forms/components/FormFooter';
 import preSubmitInfo from '../../../../platform/forms/preSubmitInfo';
 
+import { externalServices as services } from 'platform/monitoring/DowntimeNotification';
+
 import submitFormFor from './submitForm';
 
 import IntroductionPage from '../components/IntroductionPage';
@@ -118,6 +120,10 @@ const formConfig = {
   }/v0/disability_compensation_form/submit_all_claim`,
   submit: submitFormFor('disability-526EZ'),
   trackingPrefix: 'disability-526EZ-',
+  downtime: {
+    requiredForPrefill: true,
+    dependencies: [services.evss, services.emis, services.mvi, services.vet360],
+  },
   formId: '21-526EZ',
   onFormLoaded: directToCorrectForm,
   version: migrations.length,
