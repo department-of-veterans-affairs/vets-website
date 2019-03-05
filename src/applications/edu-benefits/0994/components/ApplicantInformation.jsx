67094b0d892a88e2cfb8be6640e1056644c60c74
Rob Felder <felder_robert@bah.com>
2019-03-05 13:01:20 -0500

    BAH VET TEC Intro Page Update (#9669)
    This PR addresses the need to update the intro page to use the Call to action component.

--- src/applications/edu-benefits/0994/components/ApplicantInformation.jsx
+++ src/applications/edu-benefits/0994/components/ApplicantInformation.jsx
@@ -45,11 +45,11 @@ export class ApplicantInformation extends React.Component {
           <strong>Note:</strong> If you need to update your personal
           information, please call Veterans Benefits Assistance at{' '}
           <a href="tel:1-800-827-1000">1-800-827-1000</a>, Monday through
-          Friday, 8:00 a.m. to 9:00 p.m. (ET).
+          Friday, 8:00 a.m. to 9:00 p.m. ET.
         </p>
         <p>You can also update your information on your profile page.</p>
         <p>
-          <a href="/profile">Go to your Profile page</a>.
+          <a href="/profile">Go to your profile page</a>.
         </p>
       </div>
     );
