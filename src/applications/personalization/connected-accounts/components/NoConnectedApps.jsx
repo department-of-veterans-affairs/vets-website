5d0050f57b35623374c27c77864ef9bffe5c4dbb
Kalil Smith-Nuevelle <kalil@adhocteam.us>
2019-03-07 12:54:45 -0600

    Satisfy linter

--- src/applications/personalization/connected-accounts/components/NoConnectedApps.jsx
+++ src/applications/personalization/connected-accounts/components/NoConnectedApps.jsx
@@ -11,25 +11,32 @@ const recordAction = () => {
 };
 
 export class NoConnectedApps extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
     this.header.focus();
   }
 
   noAccountsMessage() {
-    return this.props.errors.length > 0 ? "Sorry — we can’t seem to find any connected accounts." : "You do not have any connected accounts.";
+    return this.props.errors.length > 0
+      ? 'Sorry — we can’t seem to find any connected accounts.'
+      : 'You do not have any connected accounts.';
   }
 
-  render () {
+  render() {
     return (
       <div className="row va-connected-acct-null">
         <div className="usa-width-two-thirds medium-8 small-12 columns">
-          <h3 tabIndex="-1" ref={header => this.header = header}>{this.noAccountsMessage()}</h3>
+          <h3
+            tabIndex="-1"
+            ref={header => {
+              this.header = header;
+            }}
+          >
+            {this.noAccountsMessage()}
+          </h3>
           <div className="feature">
-            <h3>Have questions about signing in to {this.props.propertyName}?</h3>
+            <h3>
+              Have questions about signing in to {this.props.propertyName}?
+            </h3>
             <p>
               Get answers to frequently asked questions about how to sign in,
               common issues with verifying your identity, and your privacy and
