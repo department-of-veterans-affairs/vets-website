beae441f8b0d59286f050cda7f1bc061a041a47c
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2019-03-07 12:43:23 -0700

    VAGOV-1639: reverting alert query hange

--- src/site/stages/build/drupal/graphql/alerts.graphql.js
+++ src/site/stages/build/drupal/graphql/alerts.graphql.js
@@ -4,13 +4,13 @@
 
 module.exports = `
     alerts:   blockContentQuery(filter: {conditions: [{field: "type", value: "alert"}, {field: "status", value: "1"}]},
-    sort: {field: "field_alert_scope", direction: DESC}
+    sort: {field: "field_node_reference", direction: DESC}
     limit: 100) {
     entities {
       ... on BlockContentAlert {
         id
         fieldAlertFrequency
-        fieldAlertScope {
+        fieldNodeReference {
           targetId
         }
         fieldAlertType
