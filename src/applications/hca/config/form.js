3552ffdea2fa374c662fcb01d4c389af7375b22b
Erik P. Hansen <eph@erikphansen.com>
2019-03-11 11:33:40 -0700

    move fileHelp from form config to form helpers

--- src/applications/hca/config/form.js
+++ src/applications/hca/config/form.js
@@ -1,5 +1,4 @@
 import _ from 'lodash/fp';
-import React from 'react';
 
 import fullSchemaHca from 'vets-json-schema/dist/10-10EZ-schema.json';
 
@@ -41,6 +40,7 @@ import {
   expensesGreaterThanIncomeWarning,
   expensesLessThanIncome,
   facilityHelp,
+  fileHelp,
   financialDisclosureText,
   incomeDescription,
   isAfterCentralTimeDate,
@@ -183,47 +183,6 @@ const attachmentsSchema = {
     },
   },
 };
-
-function fileHelp({ formContext }) {
-  if (formContext.reviewMode) {
-    return <p />;
-  }
-
-  return (
-    <React.Fragment>
-      <p>
-        Please upload a copy of your military discharge papers (like your DD214,
-        DD256, DD257, NGB22, or other separation documents). If you have more
-        than one discharge document, please upload the one with the highest
-        character of discharge. If you don’t have your discharge papers, you can
-        upload a copy of other official military documents (like proof of
-        military awards or your disability rating letter).
-      </p>
-      <br />
-      <p>
-        You don’t have to upload these documents. But it can help us verify your
-        military service and may speed up your application process.
-      </p>
-      <br />
-      <p>
-        <strong>Tips for uploading:</strong>
-      </p>
-      <ul>
-        <li>
-          Upload documents as one of these file types: .jpg, .png, .pdf, .doc,
-          .rtf
-        </li>
-        <li>
-          Upload one or more files that add up to no more than 10 MB total.
-        </li>
-        <li>
-          If you don’t have a digital copy of a document, you can scan or take a
-          photo of it and then upload the image from your computer or phone.
-        </li>
-      </ul>
-    </React.Fragment>
-  );
-}
 
 const formConfig = {
   urlPrefix: '/',
