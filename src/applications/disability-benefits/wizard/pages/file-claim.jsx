8dd98f261a429f497856a768f4247119b5bcab36
Christopher Valarida <chris.valarida@adhocteam.us>
2019-03-12 17:09:21 -0700

    Add file claim page

---
+++ src/applications/disability-benefits/wizard/pages/file-claim.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FileClaimPage = () => (
+  <div>
+    <p>
+      <a
+        href="/disability/file-disability-claim-form-21-526ez/introduction"
+        className="usa-button-primary"
+      >
+        File a Disability Compensation Claim
+      </a>
+    </p>
+  </div>
+);
+
+export default {
+  name: 'file-claim',
+  component: FileClaimPage,
+};
