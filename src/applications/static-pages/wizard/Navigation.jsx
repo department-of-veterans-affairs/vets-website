9e43f8641fdefc15411f63dba4ed3ecf1314fbb2
Christopher Valarida <chris.valarida@adhocteam.us>
2019-03-12 20:44:01 -0700

    Add arrows

--- src/applications/static-pages/wizard/Navigation.jsx
+++ src/applications/static-pages/wizard/Navigation.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Navigation = ({ noBackButton, goForward, goBack, forwardAllowed }) => (
   <div>
-    {!noBackButton && <button onClick={goBack}>Back</button>}
+    {!noBackButton && (
+      <button onClick={goBack}>
+        <span className="button-icon">« </span>
+        Back
+      </button>
+    )}
     <button disabled={!forwardAllowed} onClick={goForward}>
       Next
+      <span className="button-icon"> »</span>
     </button>
   </div>
 );
