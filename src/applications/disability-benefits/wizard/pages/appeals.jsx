862e78216b932f742864aa643e1d17c7b40def40
Christopher Valarida <chris.valarida@adhocteam.us>
2019-03-12 17:03:12 -0700

    Add more pages

--- src/applications/disability-benefits/wizard/pages/appeals.jsx
+++ src/applications/disability-benefits/wizard/pages/appeals.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import ErrorableRadioButtons from '@department-of-veterans-affairs/formation-react/ErrorableRadioButtons';
+import Navigation from '../../../static-pages/wizard/Navigation';
 
-// const AppealsPage = ({ setPageState, goForward, goBack, state = {} }) => {
-const AppealsPage = () => <div>Appeals!</div>;
+const options = [
+  {
+    value: 'file-claim',
+    label:
+      'I’m filing a claim for a new condition or for a condition that has gotten worse.',
+  },
+  {
+    value: 'disagreeing',
+    label: 'I’m disagreeing with a VA decision on my claim.',
+  },
+];
+
+const AppealsPage = ({ setPageState, goForward, goBack, state = {} }) => {
+  const goToNextPage = () => goForward(state.selected);
+  return (
+    <div>
+      <ErrorableRadioButtons
+        name="appeals-page-option"
+        label="Are you filing a new claim or are you disagreeing with a VA decision on a prior claim?"
+        id="appeals-page-option"
+        options={options}
+        onValueChange={({ value }) => setPageState({ selected: value })}
+        value={{ value: state.selected }}
+      />
+      <Navigation
+        goForward={goToNextPage}
+        forwardAllowed={state.selected}
+        goBack={goBack}
+      />
+    </div>
+  );
+};
 
 export default {
   name: 'appeals',
--- src/applications/disability-benefits/wizard/pages/index.js
+++ src/applications/disability-benefits/wizard/pages/index.js
@@ -1,5 +1,7 @@
 import start from './start';
 import appeals from './appeals';
 import bdd from './bdd';
+import fileClaim from './file-claim';
+import disagreeing from './disagreeing';
 
-export default [start, appeals, bdd];
+export default [start, appeals, bdd, fileClaim, disagreeing];
