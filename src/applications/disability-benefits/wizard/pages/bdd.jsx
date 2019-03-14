c15cfe6ff39dc8bcb4d0eebaf7b8b127353158da
Christopher Valarida <chris.valarida@adhocteam.us>
2019-03-14 14:54:20 -0700

    Use dates

--- src/applications/disability-benefits/wizard/pages/bdd.jsx
+++ src/applications/disability-benefits/wizard/pages/bdd.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import moment from 'moment';
 import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
 import { pageNames } from './pageList';
 
 // TODO: Add in the dates for 180 and 90 days in the future
+const dateFormat = 'MMMM DD, YYYY';
+const ninetyDays = moment()
+  .add(90, 'd')
+  .format(dateFormat);
+const oneHundredEightyDays = moment()
+  .add(180, 'd')
+  .format(dateFormat);
 
 const alertContent = (
   <>
     <p>
-      <strong>If your separation date is in the next 180 to 90 days,</strong>{' '}
-      you can file a disability claim through the Benefits Delivery at Discharge
-      (BDD) program.
+      <strong>
+        If your separation date is between {ninetyDays} and{' '}
+        {oneHundredEightyDays}
+      </strong>{' '}
+      (90 and 180 days from today), you can file a disability claim through the
+      Benefits Delivery at Discharge (BDD) program.
     </p>
     <p>
-      <strong>If your separation date is less than 90 days away</strong> you
-      can’t file a BDD claim, but you can still begin the process of filing your
-      claim on eBenefits.
+      <strong>If your separation date is before {ninetyDays},</strong> you can’t
+      file a BDD claim, but you can still begin the process of filing your claim
+      on eBenefits.
     </p>
     <a
       href="https://www.ebenefits.va.gov/ebenefits/about/feature?feature=disability-compensation"
