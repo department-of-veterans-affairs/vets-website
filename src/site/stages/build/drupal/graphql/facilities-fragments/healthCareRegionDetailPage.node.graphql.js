3f9f00eb2d09f40bb504dd31d636849de1041511
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2019-03-15 03:33:20 -0600

    VAGOV-1685: Detail page query

---
+++ src/site/stages/build/drupal/graphql/facilities-fragments/healthCareRegionDetailPage.node.graphql.js
@@ -0,0 +1,85 @@
+/**
+ * The detail page containing static content for a health care region
+ */
+
+const {
+  FIELD_RELATED_LINKS,
+} = require('../paragraph-fragments/listOfLinkTeasers.paragraph.graphql');
+
+const entityElementsFromPages = require('../entityElementsForPages.graphql');
+
+const WYSIWYG = '... wysiwyg';
+const COLLAPSIBLE_PANEL = '... collapsiblePanel';
+const PROCESS = '... process';
+const QA_SECTION = '... qaSection';
+const QA = '... qa';
+const LIST_OF_LINK_TEASERS = '... listOfLinkTeasers';
+const REACT_WIDGET = '... reactWidget';
+const NUMBER_CALLOUT = '... numberCallout';
+
+const DETAIL_PAGE_RESULTS = `
+  entities {
+    ... on NodeHealthCareRegionDetailPage {
+    title
+    ${entityElementsFromPages}
+    entityBundle
+    changed
+    ${FIELD_RELATED_LINKS}
+    ${WYSIWYG}
+    ${COLLAPSIBLE_PANEL}
+    ${PROCESS}
+    ${QA_SECTION}
+    ${QA}
+    ${LIST_OF_LINK_TEASERS}
+    ${REACT_WIDGET}
+    ${NUMBER_CALLOUT}
+    fieldMedia {
+      entity {
+       entityId
+       entityBundle
+       name
+       ...on MediaDocument {
+        fieldDocument {
+          entity {
+            ...on File {
+              filename
+              url          
+            }
+          }
+        }
+      }   
+     ...on MediaImage {        
+      image {          
+        alt
+        url
+      }
+     }
+    ...on MediaVideo {
+      fieldMediaVideoEmbedField        
+    }
+  }
+  }
+    }
+  }
+`;
+
+function queryFilter(isAll) {
+  return `
+    reverseFieldOfficeNode(
+    filter: {
+      conditions: [
+        { field: "type", value: "health_care_detail_page"}
+        { field: "status", value: "1"}
+      ]
+    } 
+    sort: {field: "field_office", direction: DESC }
+    limit:${isAll ? '500' : '10'})
+  `;
+}
+
+module.exports = `
+  allHealthcareDetailPages: ${queryFilter(true)}
+    {
+    ${DETAIL_PAGE_RESULTS}
+  }
+`;
