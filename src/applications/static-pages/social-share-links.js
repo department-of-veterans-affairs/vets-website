2fe5cdab7f4e625ae9a2cc0c27628c4ecbff2d0f
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2019-03-18 23:40:49 -0600

    VAGOV-1532: Mobile share behavior

--- src/applications/static-pages/social-share-links.js
+++ src/applications/static-pages/social-share-links.js
@@ -4,13 +4,31 @@ Open share links in a new modal window
 
 export function openShareLink() {
   const shareLinks = document.querySelectorAll('.va-js-share-link');
+  const ifNavigatorShare = navigator.share !== undefined;
+  const metaTitle = document.querySelector("meta[property='og:title']").content;
+  const metaDescription = document.querySelector("meta[name='description']")
+    .content;
+
   shareLinks.forEach(link => {
     link.addEventListener('click', event => {
-      window.open(
-        link.href,
-        'social_popup',
-        'left=20,top=20,width=700,height=500,toolbar=0,menubar=0,status=0,location=0,resizable=0',
-      );
+      if (!ifNavigatorShare) {
+        // Desktop share
+        window.open(
+          link.href,
+          'social_popup',
+          'left=20,top=20,width=700,height=500,toolbar=0,menubar=0,status=0,location=0,resizable=0',
+        );
+      } else {
+        // Android native share
+        navigator
+          .share({
+            title: metaTitle,
+            text: metaDescription,
+            url: link.href,
+          })
+          .then(() => event.preventDefault())
+          .catch(() => document.location.assign(link.href));
+      }
       event.preventDefault();
     });
   });
