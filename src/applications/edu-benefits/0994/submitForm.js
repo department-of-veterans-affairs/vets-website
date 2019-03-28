b245b10362b9a15e5a1056afb23d68d3b2f86b09
Devin McCurdy <mccurdy_devin@bah.com>
2019-03-28 13:29:18 -0400

    Moved submission form data submit before submission

--- src/applications/edu-benefits/0994/submitForm.js
+++ src/applications/edu-benefits/0994/submitForm.js
@@ -10,40 +10,34 @@ const submitForm = (form, formConfig) => {
   const body = formConfig.transformForSubmit
     ? formConfig.transformForSubmit(formConfig, form)
     : transformForSubmit(formConfig, form);
-
-  return submitToUrl(
-    body,
-    formConfig.submitUrl,
-    formConfig.trackingPrefix,
-  ).then(results => {
-    recordEvent({
-      'edu-0994-appliedPastBenefits': _.get(
-        form,
-        'data.appliedForVaEducationBenefits',
-        '',
-      ),
-      activeDuty: _.get(form, 'data.activeDuty', ''),
-      calledActiveDuty: _.get(form, 'data.activeDutyDuringVetTec', ''),
-      educationCompleted: _.get(form, 'data.highestLevelofEducation', ''),
-      'edu-0994-currentlyWorkingIndustry': _.get(
-        form,
-        'data.currentHighTechnologyEmployment',
-        '',
-      ),
-      salary: _.get(form, 'data.currentSalary', ''),
-      'edu-0994-programSelection': _.get(
-        form,
-        'data.view:trainingProgramsChoice',
-        '',
-      ),
-      'edu-0994-edu-0994-programs-saved': _.get(
-        form,
-        'data.vetTecPrograms.length',
-        0,
-      ),
-    });
-    return results;
+  recordEvent({
+    'edu-0994-appliedPastBenefits': _.get(
+      form,
+      'data.appliedForVaEducationBenefits',
+      '',
+    ),
+    activeDuty: _.get(form, 'data.activeDuty', ''),
+    calledActiveDuty: _.get(form, 'data.activeDutyDuringVetTec', ''),
+    educationCompleted: _.get(form, 'data.highestLevelofEducation', ''),
+    'edu-0994-currentlyWorkingIndustry': _.get(
+      form,
+      'data.currentHighTechnologyEmployment',
+      '',
+    ),
+    salary: _.get(form, 'data.currentSalary', ''),
+    'edu-0994-programSelection': _.get(
+      form,
+      'data.view:trainingProgramsChoice',
+      '',
+    ),
+    'edu-0994-edu-0994-programs-saved': _.get(
+      form,
+      'data.vetTecPrograms.length',
+      0,
+    ),
   });
+
+  return submitToUrl(body, formConfig.submitUrl, formConfig.trackingPrefix);
 };
 
 export default submitForm;
