444d41283be2b977d702d4dc2eafa76059540be7
Matt Roth <roth_matthew@bah.com>
2019-03-29 15:43:16 -0400

    more fixes

--- src/applications/edu-benefits/0994/migrations/version2.js
+++ src/applications/edu-benefits/0994/migrations/version2.js
@@ -2,17 +2,17 @@ import _ from 'lodash';
 
 export default function version2() {
   return savedData => {
-    const trainingProgramsChoice = _.get(
-      savedData.metadata,
-      'view:trainingProgramsChoice',
-    );
+    const trainingProgramsChoice =
+      savedData.formData['view:trainingProgramsChoice'];
 
-    if (trainingProgramsChoice) {
+    if (typeof trainingProgramsChoice !== 'undefined') {
       const newData = _.set(
-        savedData.metadata,
+        savedData.formData,
         'hasSelectedPrograms',
         trainingProgramsChoice,
       );
+      delete newData['view:trainingProgramsChoice'];
+
       return newData;
     }
     return savedData;
