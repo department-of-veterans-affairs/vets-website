252b0e47cf647e7c3ce146c48d7260df11183977
Ben <bshyong@users.noreply.github.com>
2019-04-02 19:13:41 -0500

    Redirect 526 v1 to v2 if no saved forms exist (#9853)
    * redirect to v2 if no saved forms
    
    * url fix
    
    * pass unit tests
    
    * add comment

--- src/applications/disability-benefits/526EZ/tests/components/IntroductionPage.unit.spec.jsx
+++ src/applications/disability-benefits/526EZ/tests/components/IntroductionPage.unit.spec.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import moment from 'moment';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
 import { IntroductionPage } from '../../components/IntroductionPage';
 
 const defaultProps = {
+  user: {
+    profile: {
+      // need to have a saved form or else form will redirect to v2
+      savedForms: [
+        {
+          form: '21-526EZ',
+          metadata: { lastUpdated: 3000, expiresAt: moment().unix() + 2000 },
+        },
+      ],
+    },
+  },
   saveInProgress: {
     user: {},
   },
@@ -22,7 +34,10 @@ const defaultProps = {
 };
 
 describe('IntroductionPage', () => {
-  it('should render FormStartControls', () => {
+  it('should render SaveInProgressIntro', () => {
+    const oldWindow = global.window;
+    global.window = { location: { replace: () => {} } };
+
     const tree = shallow(<IntroductionPage {...defaultProps} />);
     const formStartControls = tree.find('FormStartControls');
     expect(formStartControls.length).to.equal(2);
@@ -30,5 +45,7 @@ describe('IntroductionPage', () => {
       'disability-526EZ-start',
     );
     tree.unmount();
+
+    global.window = oldWindow;
   });
 });
