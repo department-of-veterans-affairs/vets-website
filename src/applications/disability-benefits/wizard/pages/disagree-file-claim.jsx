65ef3eef872ac3adf1b2fb0fe95356a505696c19
Christopher Valarida <chris.valarida@adhocteam.us>
2019-04-03 12:31:42 -0700

    Change link

--- src/applications/disability-benefits/wizard/pages/disagree-file-claim.jsx
+++ src/applications/disability-benefits/wizard/pages/disagree-file-claim.jsx
@@ -8,11 +8,8 @@ const alertContent = (
       If you disagree with a VA decision that you received more than a year ago,
       you can file a Supplemental Claim.
     </p>
-    <a
-      href="/disability/file-disability-claim-form-21-526ez/introduction"
-      className="usa-button-primary va-button-primary"
-    >
-      File a Disability Compensation Claim
+    <a href="/decision-reviews/supplemental-claim/">
+      Find out how to file a Supplemental Claim
     </a>
   </>
 );
