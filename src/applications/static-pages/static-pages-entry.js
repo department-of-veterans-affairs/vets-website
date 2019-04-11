c7a4e3f11480c3c2ac99e5c38ddb3f92a57e4760
Dan Hinze <dan.hinze@adhocteam.us>
2019-04-10 19:41:51 -0500

    Use existing additional info widget and override styles

--- src/applications/static-pages/static-pages-entry.js
+++ src/applications/static-pages/static-pages-entry.js
@@ -6,7 +6,6 @@ import createCommonStore from '../../platform/startup/store';
 import startSitewideComponents from '../../platform/site-wide';
 import './alerts-dismiss-view';
 import './ics-generator';
-import './additional-info-toggle';
 
 import widgetTypes from './widgetTypes';
 import createAdditionalInfoWidget from './createAdditionalInfoWidget';
