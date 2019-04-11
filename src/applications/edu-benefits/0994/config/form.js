44a1ff35b08ace473df69e5bd93b3feca67d94ff
Devin McCurdy <mccurdy_devin@bah.com>
2019-04-11 12:05:18 -0400

    form config fix

--- src/applications/edu-benefits/0994/config/form.js
+++ src/applications/edu-benefits/0994/config/form.js
@@ -63,8 +63,8 @@ const formConfig = {
         applicantInformation: {
           title: 'Applicant Information',
           path: 'applicant/information',
-          uiSchema: applicantInformation.uiSchema(),
-          schema: applicantInformation.schema(),
+          uiSchema: applicantInformation.uiSchema,
+          schema: applicantInformation.schema,
         },
         benefitsEligibility: {
           title: 'Applicant Information',
