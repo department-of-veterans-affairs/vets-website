7fd3c163fa1abab0b359bfcf0472bae3be343100
Tze Lei <tze@adhocteam.us>
2019-04-12 11:03:15 -0700

    17765 - Update Dashboard contents.

--- src/applications/personalization/dashboard/containers/DashboardApp.jsx
+++ src/applications/personalization/dashboard/containers/DashboardApp.jsx
@@ -48,14 +48,14 @@ function recordDashboardClick(product, actionType = 'view-link') {
 
 const ManageHealthBenefits = () => (
   <>
-    <h2>Manage Your Health and Benefits</h2>
+    <h2>Manage your health and benefits</h2>
     <ul className="va-nav-linkslist-list">
       <li>
         <a
           href="/health-care/schedule-view-va-appointments/"
           onClick={recordDashboardClick('schedule-appointment')}
         >
-          <h4 className="va-nav-linkslist-title">Schedule a VA Appointment</h4>
+          <h4 className="va-nav-linkslist-title">Schedule a VA appointment</h4>
           <p className="va-nav-linkslist-description">
             Find out how to make a doctor’s appointment with a member of your VA
             health care team online or by phone.
@@ -68,7 +68,7 @@ const ManageHealthBenefits = () => (
           onClick={recordDashboardClick('post-911')}
         >
           <h4 className="va-nav-linkslist-title">
-            Check Post-9/11 GI Bill Benefits
+            Check Post-9/11 GI Bill benefits
           </h4>
           <p className="va-nav-linkslist-description">
             View and print your statement of benefits.
@@ -81,14 +81,14 @@ const ManageHealthBenefits = () => (
 
 const RequestYourRecords = () => (
   <>
-    <h2>Request Your Records</h2>
+    <h2>Request your records</h2>
     <ul className="va-nav-linkslist-list">
       <li>
         <a
           href="/health-care/get-medical-records/"
           onClick={recordDashboardClick('health-records')}
         >
-          <h4 className="va-nav-linkslist-title">Get Your VA Health Records</h4>
+          <h4 className="va-nav-linkslist-title">Get your VA health records</h4>
           <p className="va-nav-linkslist-description">
             View, download, and print your VA health records.
           </p>
@@ -99,7 +99,7 @@ const RequestYourRecords = () => (
           href={lettersManifest.rootUrl}
           onClick={recordDashboardClick('download-letters')}
         >
-          <h4 className="va-nav-linkslist-title">Download Your VA Letters</h4>
+          <h4 className="va-nav-linkslist-title">Download your VA letters</h4>
           <p className="va-nav-linkslist-description">
             Access and download benefit letters and documents proving your
             status online.
@@ -112,7 +112,7 @@ const RequestYourRecords = () => (
 
 const ViewYourProfile = () => (
   <>
-    <h2>View Your Profile</h2>
+    <h2>View your profile</h2>
     <p>
       Review your contact, personal, and military service information—and find
       out how to make any needed updates or corrections.
@@ -122,7 +122,7 @@ const ViewYourProfile = () => (
         href={profileManifest.rootUrl}
         onClick={recordDashboardClick('view-your-profile', 'view-button')}
       >
-        View Your Profile
+        View your profile
       </a>
     </p>
   </>
@@ -130,7 +130,7 @@ const ViewYourProfile = () => (
 
 const ManageYourAccount = () => (
   <>
-    <h2>Manage Your Account</h2>
+    <h2>Manage your account</h2>
     <p>
       View your current account settings—and find out how to update them as
       needed to access more site tools or add extra security to your account.
@@ -143,7 +143,7 @@ const ManageYourAccount = () => (
           'view-button',
         )}
       >
-        View Your Account Settings
+        View your account settings
       </a>
     </p>
   </>
@@ -350,7 +350,6 @@ class DashboardApp extends React.Component {
               >
                 Find your nearest VA medical center
               </a>
-              .
             </p>
           </div>
         }
