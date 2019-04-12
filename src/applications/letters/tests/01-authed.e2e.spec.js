26f7edca07ee0b40fb7bc3df29c46ed5f2ffee50
Aubrey Arcangel <aubreyarcangel@users.noreply.github.com>
2019-04-12 16:03:48 -0400

    Update Liquid Meta Tags Template to Add Keywords and Custom Title (#9903)
    * Facility Locator: Update Metadata for SEO
    
    * replaced :VA.gov with | Veterans Affairs
    
    * updating tests to expect  | Veterans Affairs
    
    * updating tests to expect  | Veterans Affairs
    
    * updated tests expect | Veterans Affairs in title
    
    * npm run lint:js:fix

--- src/applications/letters/tests/01-authed.e2e.spec.js
+++ src/applications/letters/tests/01-authed.e2e.spec.js
@@ -28,7 +28,7 @@ module.exports = E2eHelpers.createE2eTest(client => {
   Auth.logIn(token, client, '/download-va-letters/letters', 3)
     .waitForElementVisible('body', Timeouts.normal)
     .axeCheck('.main')
-    .assert.title('Download VA Letters and Documents: VA.gov')
+    .assert.title('Download VA Letters and Documents | Veterans Affairs')
     .waitForElementVisible('.letters', Timeouts.slow); // First render of React may be slow.
 
   client.axeCheck('.main');
