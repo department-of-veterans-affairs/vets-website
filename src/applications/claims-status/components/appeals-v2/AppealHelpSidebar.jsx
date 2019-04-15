3f9fa1c488ce147c6cb2ef21eeebe8ce0913e92f
peggygannon <peggy@thesocompany.com>
2019-04-15 11:35:40 -0700

    added 1 back into coded phone number

--- src/applications/claims-status/components/appeals-v2/AppealHelpSidebar.jsx
+++ src/applications/claims-status/components/appeals-v2/AppealHelpSidebar.jsx
@@ -8,7 +8,7 @@ const vbaVersion = (
     <h2 className="help-heading">Need help?</h2>
     <p>Call Veterans Affairs Benefits and Services</p>
     <p className="help-phone-number">
-      <a className="help-phone-number-link" href="tel:800-827-1000">
+      <a className="help-phone-number-link" href="tel:1-800-827-1000">
         800-827-1000
       </a>
     </p>
@@ -21,7 +21,7 @@ const vhaVersion = (
     <h2 className="help-heading">Need help?</h2>
     <p>Call Health Care Benefits</p>
     <p className="help-phone-number">
-      <a className="help-phone-number-link" href="tel:877-222-8387">
+      <a className="help-phone-number-link" href="tel:1-877-222-8387">
         877-222-VETS (8387)
       </a>
     </p>
