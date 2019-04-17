3e7daca36ae04b7263eb12f0d889c75f305391ed
Chris Valarida <cvalarida@gmail.com>
2019-04-17 11:49:26 -0700

    Styleguide updates (#9922)
    * (ET) -> ET
    
    * (CT) -> CT
    
    * Remove 1- from phone numbers
    
    * Run prettier --write on src/
    
    * Sentence-case 526 page titles
    
    * Sentence case 8940
    
    * Sentence case pensions
    
    * Sentence case burials
    
    * Sentence-case letter
    
    * uppercasing Veteran
    
    * adding 1 back into coded phone number
    
    * added 1 back into coded phone number
    
    * added 1 to phone number code
    
    * add 1 to phone number code
    
    * added 1 to phone number code
    
    * Update helpers.jsx
    
    * Update AuthorizationMessage.unit.spec.jsx
    
    * capping IU as it's proper name
    
    * capping proper names
    
    * added 1 to coded phone number
    
    * add 1 to coded phone number
    
    * add 1 to coded phone number
    
    * 1 to coded phone number
    
    * add 1 to coded phone number
    
    * 1 to coded phone number
    
    * Update bankInformation.jsx
    
    * updated coded phone number
    
    * add 1 to coded phone number
    
    * added 1 to coded phone number
    
    * Update LetterList.jsx
    
    * style tweaks
    
    * style tweaks
    
    * Update ConfirmationPage.jsx
    
    * phone number tweak
    
    * phone number tweak
    
    * style tweaks
    
    * Update ConfirmationPage.jsx
    
    * Update VerifyApp.jsx
    
    * Update GetFormHelp.jsx
    
    * Pacify the linter
    
    * Fix failing tests

--- src/applications/burials/config/form.js
+++ src/applications/burials/config/form.js
@@ -178,7 +178,7 @@ const formConfig = {
       },
     },
     veteranInformation: {
-      title: 'Deceased Veteran Information',
+      title: 'Deceased Veteran information',
       pages: {
         veteranInformation: {
           title: 'Deceased Veteran information',
@@ -274,10 +274,10 @@ const formConfig = {
       },
     },
     militaryHistory: {
-      title: 'Military History',
+      title: 'Military history',
       pages: {
         servicePeriods: {
-          title: 'Service Periods',
+          title: 'Service periods',
           path: 'military-history/service-periods',
           uiSchema: {
             'view:serviceRecordNotification': {
@@ -297,7 +297,7 @@ const formConfig = {
           },
         },
         previousNames: {
-          title: 'Previous Names',
+          title: 'Previous names',
           path: 'military-history/previous-names',
           uiSchema: {
             previousNames: {
@@ -328,7 +328,7 @@ const formConfig = {
       },
     },
     benefitsSelection: {
-      title: 'Benefits Selection',
+      title: 'Benefits selection',
       pages: {
         benefitsSelection: {
           title: 'Benefits selection',
@@ -540,7 +540,7 @@ const formConfig = {
       },
     },
     additionalInformation: {
-      title: 'Additional Information',
+      title: 'Additional information',
       pages: {
         claimantContactInformation: {
           title: 'Claimant contact information',
--- src/applications/claims-status/components/appeals-v2/AppealHelpSidebar.jsx
+++ src/applications/claims-status/components/appeals-v2/AppealHelpSidebar.jsx
@@ -9,10 +9,10 @@ const vbaVersion = (
     <p>Call Veterans Affairs Benefits and Services</p>
     <p className="help-phone-number">
       <a className="help-phone-number-link" href="tel:1-800-827-1000">
-        1-800-827-1000
+        800-827-1000
       </a>
     </p>
-    <p>Monday - Friday, 8:00am - 9:00pm (ET)</p>
+    <p>Monday - Friday, 8:00am - 9:00pm ET</p>
   </div>
 );
 
@@ -22,10 +22,10 @@ const vhaVersion = (
     <p>Call Health Care Benefits</p>
     <p className="help-phone-number">
       <a className="help-phone-number-link" href="tel:1-877-222-8387">
-        1-877-222-VETS (8387)
+        877-222-VETS (8387)
       </a>
     </p>
-    <p>Monday - Friday, 8:00am - 8:00pm (ET)</p>
+    <p>Monday - Friday, 8:00am - 8:00pm ET</p>
   </div>
 );
 
