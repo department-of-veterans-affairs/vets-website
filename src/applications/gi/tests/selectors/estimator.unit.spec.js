a10ed23cdc7d28fb0866bd9848b791ce27fac677
Devin McCurdy <mccurdy_devin@bah.com>
2019-04-22 12:41:29 -0400

    Added estimator unit test

---
+++ src/applications/gi/tests/selectors/estimator.unit.spec.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+
+import reducer from '../../reducers';
+import { calculatorConstants } from '../gibct-helpers';
+import createCommonStore from 'platform/startup/store';
+import { estimatedBenefits } from '../../selectors/estimator';
+
+const defaultState = createCommonStore(reducer).getState();
+
+defaultState.constants = {
+  constants: {},
+  version: calculatorConstants.meta.version,
+  inProgress: false,
+};
+
+calculatorConstants.data.forEach(c => {
+  defaultState.constants.constants[c.attributes.name] = c.attributes.value;
+});
+
+describe('estimatedBenefits', () => {
+  it('should estimate no tuition allowance for ojt school', () => {
+    expect(
+      estimatedBenefits(defaultState, {
+        type: 'ojt',
+        bah: 1000,
+        country: 'usa',
+      }).tuition.value,
+    ).to.equal('N/A');
+  });
+});
