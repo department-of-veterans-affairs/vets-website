cc6bdb1eb84b45bf5107794b2d781c94b0827607
wconroy <conroy_william@bah.com>
2019-04-29 11:14:36 -0400

    formatting improvements

--- src/applications/gi/containers/Modals.jsx
+++ src/applications/gi/containers/Modals.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import Modal from '@department-of-veterans-affairs/formation-react/Modal';
-import environment from "../../../platform/utilities/environment";
+import environment from '../../../platform/utilities/environment';
 
 export class Modals extends React.Component {
   constructor(props) {
@@ -470,38 +470,39 @@ export class Modals extends React.Component {
           visible={this.shouldDisplayModal('stemOffered')}
         >
           <h3>STEM</h3>
-          {
-            !environment.isProduction() ?
-              <div>
-                <p>
-                  The Edith Nourse Rogers STEM Scholarship provides up to 9 months of
-                  additional Post-9/11 GI Bill benefits, to a maximum of $30,000.
-                </p>
-                <p>
-                  Veterans and Fry Scholars may qualify for this scholarship if
-                  they're enrolled in an undergraduate program for Science,
-                  Technology, Engineering, or Math (STEM), or if they've earned a STEM
-                  degree and are getting a teaching certification.
-                </p>
-                <p>
-                  To learn more about the STEM Scholarship,{' '}
-                  <a
-                    href="https://benefits.va.gov/gibill/fgib/stem.asp"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {' '} visit the VBA STEM website.
-                  </a>
-                </p>
-              </div>
-              :
+          {!environment.isProduction() ? (
+            <div>
               <p>
-                VA will provide up to 9 months of additional Post-9/11 GI Bill
-                benefits if you're eligible and enrolled in a Science, Technology,
-                Engineering, or Math educational program.
+                The Edith Nourse Rogers STEM Scholarship provides up to 9 months
+                of additional Post-9/11 GI Bill benefits, to a maximum of
+                $30,000.
               </p>
-          }
-          </Modal>
+              <p>
+                Veterans and Fry Scholars may qualify for this scholarship if
+                they're enrolled in an undergraduate program for Science,
+                Technology, Engineering, or Math (STEM), or if they've earned a
+                STEM degree and are getting a teaching certification.
+              </p>
+              <p>
+                To learn more about the STEM Scholarship,{' '}
+                <a
+                  href="https://benefits.va.gov/gibill/fgib/stem.asp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {' '}
+                  visit the VBA STEM website.
+                </a>
+              </p>
+            </div>
+          ) : (
+            <p>
+              VA will provide up to 9 months of additional Post-9/11 GI Bill
+              benefits if you're eligible and enrolled in a Science, Technology,
+              Engineering, or Math educational program.
+            </p>
+          )}
+        </Modal>
         <Modal
           onClose={this.props.hideModal}
           visible={this.shouldDisplayModal('iStudy')}
