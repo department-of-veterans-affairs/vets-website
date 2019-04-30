d8b4c4be2bcdc755cc6aa167fd7837601adb3357
Ethan Teague <metrowebz@gmail.com>
2019-04-30 17:58:33 -0400

    Refactoring code to be more semantic.

--- src/applications/static-pages/libraries/library-pages.js
+++ src/applications/static-pages/libraries/library-pages.js
@@ -1,9 +1,15 @@
 import { libraryProcess } from './library-process';
+import * as Bucket from '../../../../src/site/constants/buckets';
+import * as Environment from '../../../../src/platform/utilities/environment';
 /**
  * For adding library pages.
  */
 export function libraryPages() {
-  const outReachMaterials = `https://raw.githubusercontent.com/ethanteague/va-assets-array/master/media-assets.json`;
+  const outReachMaterials =
+    Environment.default.BUILDTYPE === 'vagovprod'
+      ? `${Bucket.vagovprod}/generated/outreach-assets.json`
+      : `${Bucket.vagovstaging}/generated/outreach-assets.json`;
+
   const path = window.location.pathname.split('/');
 
   if (path.includes('public-outreach-materials')) {
