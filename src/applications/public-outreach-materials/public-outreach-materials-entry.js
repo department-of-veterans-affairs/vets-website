1aa93e4029453e9ede5fd64a727ea25081061f00
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-05-03 16:29:42 -0400

    Write JSON to page instead of JSON file

--- src/applications/public-outreach-materials/public-outreach-materials-entry.js
+++ src/applications/public-outreach-materials/public-outreach-materials-entry.js
@@ -7,17 +7,8 @@ import startSitewideComponents from '../../platform/site-wide';
 
 import { libraryProcess } from './libraries/library-process';
 
-/**
- * For adding library pages.
- */
-export function libraryPages() {
-  const outReachMaterials = '/generated/outreach-assets.json';
-
-  libraryProcess(outReachMaterials);
-}
-
 const store = createCommonStore();
 
 startSitewideComponents(store);
 
-document.addEventListener('DOMContentLoaded', libraryPages);
+document.addEventListener('DOMContentLoaded', libraryProcess);
