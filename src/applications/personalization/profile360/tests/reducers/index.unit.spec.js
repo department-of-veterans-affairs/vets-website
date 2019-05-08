7de112361b9a8af01f52c61fe7e980b625d15fcf
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-05-08 15:50:28 -0400

    add reducer test

--- src/applications/personalization/profile360/tests/reducers/index.unit.spec.js
+++ src/applications/personalization/profile360/tests/reducers/index.unit.spec.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 
 import profileReducer from '../../reducers/index';
+import * as paymentInfoActions from '../../actions/paymentInformation';
 
 const { vaProfile } = profileReducer;
 
@@ -27,5 +28,55 @@ describe('index reducer', () => {
     );
 
     expect(state.personalInformation).to.eql('personalInformation');
+  });
+
+  it('updates payment info and ui state', () => {
+    const fetchAction = {
+      type: paymentInfoActions.FETCH_PAYMENT_INFORMATION_SUCCESS,
+      paymentInformation: {
+        something: 'something',
+      },
+    };
+
+    let state = vaProfile({}, fetchAction);
+
+    expect(state.paymentInformation).to.be.deep.equal(
+      fetchAction.paymentInformation,
+      'paymentInformation is initialized',
+    );
+
+    const editAction = {
+      type: paymentInfoActions.SET_PAYMENT_INFO_UI_STATE,
+      state: {
+        isEditing: true,
+      },
+    };
+
+    state = vaProfile(state, editAction);
+
+    expect(state.paymentInformationUiState.isEditing).to.be.true;
+
+    const saveAction = {
+      type: paymentInfoActions.SAVE_PAYMENT_INFORMATION,
+    };
+
+    state = vaProfile(state, saveAction);
+
+    expect(state.paymentInformationUiState.isEditing).to.be.true;
+    expect(state.paymentInformationUiState.isSaving).to.be.true;
+
+    const savedAction = {
+      type: paymentInfoActions.SAVE_PAYMENT_INFORMATION_SUCCESS,
+      paymentInformation: {
+        somethingElse: 'somethingElse',
+      },
+    };
+
+    state = vaProfile(state, savedAction);
+
+    expect(state.paymentInformation).to.be.deep.equal(
+      savedAction.paymentInformation,
+      'paymentInformation is updated',
+    );
   });
 });
