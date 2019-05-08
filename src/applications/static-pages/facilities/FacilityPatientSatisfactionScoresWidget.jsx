b77caa70e47e12d8a758467797aba715e33eedca
ahay-agile6 <aurora.hampton@agile6.com>
2019-05-08 08:05:08 -0700

    VAGOV-2271: Implement "wait times" in local view of health services accordions (#10038)
    * VAGOV-2271:
    
    + re-factor Facility pages to use redux store to *attempt* to reduce number of times api is called
    + add FacilityAppointmentWaitTimesWidget
    + add FacilityAppointmentWaitTimesWidget to facility health service liquid template
    
    * VAGOV-2271: fix up satisfaction scores widget
    
    + refactor pushing html
    + fix unit test
    
    * VAGOV-2271: add reducer unit tests
    
    + a few minor ui tweaks to appointment wait times
    
    * VAGOV-2271: [VAGOV-3240] unit tests
    
    + add actions unit tests
    + add FacilityAppointmentWaitTimesWidget component unit tests
    + some ui clean up and accessibility fixes (possible duplicate ids)

--- src/applications/static-pages/facilities/FacilityPatientSatisfactionScoresWidget.jsx
+++ src/applications/static-pages/facilities/FacilityPatientSatisfactionScoresWidget.jsx
@@ -1,122 +1,29 @@
 import React from 'react';
-import { apiRequest } from '../../../platform/utilities/api';
 import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
 import { formatDateLong } from '../../../platform/utilities/date';
 import { displayPercent } from '../../../platform/utilities/ui';
+import FacilityApiAlert from './FacilityApiAlert';
+import { connect } from 'react-redux';
 
-export default class FacilityPatientSatisfactionScoresWidget extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-    };
-  }
-
-  componentDidMount() {
-    const facilityId = this.props.facilityId;
-    this.request = apiRequest(
-      `/facilities/va/${facilityId}`,
-      null,
-      this.handleFacilitySuccess,
-      this.handleFacilityError,
-    );
-  }
-
-  handleFacilitySuccess = facility => {
-    this.setState({
-      loading: false,
-      facility: facility.data,
-    });
-  };
-
-  handleFacilityError = () => {
-    this.setState({ error: true });
-  };
-
+export class FacilityPatientSatisfactionScoresWidget extends React.Component {
   render() {
-    if (this.state.loading) {
+    if (this.props.loading || !Object.keys(this.props.facility).length) {
       return (
         <LoadingIndicator message="Loading facility patient satisfaction scores..." />
       );
     }
 
-    const routineScoreMarkup = [];
-    const urgentScoreMarkup = [];
-
-    if (this.state.facility.attributes.feedback.health.primaryCareRoutine) {
-      urgentScoreMarkup.push(
-        <div className="facility-satisfaction-tile vads-u-background-color--gray-lightest vads-u-padding--1p5 vads-u-margin-right--1">
-          <p className="vads-u-margin--0">Primary care</p>
-          <p
-            id="facility-patient-satisfaction-scores-primary-urgent-score"
-            className="vads-u-font-size--lg vads-u-font-weight--bold vads-u-margin--0 vads-u-font-family--serif"
-          >
-            {displayPercent(
-              this.state.facility.attributes.feedback.health.primaryCareUrgent,
-            )}
-          </p>
-        </div>,
-      );
+    if (this.props.error) {
+      return <FacilityApiAlert />;
     }
 
-    if (this.state.facility.attributes.feedback.health.specialtyCareUrgent) {
-      urgentScoreMarkup.push(
-        <div className="facility-satisfaction-tile vads-u-background-color--gray-lightest vads-u-padding--1p5">
-          <p className="vads-u-margin--0">Specialty care</p>
-          <p
-            id="facility-patient-satisfaction-scores-specialty-urgent-score"
-            className="vads-u-font-size--lg vads-u-font-weight--bold vads-u-margin--0 vads-u-font-family--serif"
-          >
-            {displayPercent(
-              this.state.facility.attributes.feedback.health
-                .specialtyCareUrgent,
-            )}
-          </p>
-        </div>,
-      );
-    }
-
-    if (this.state.facility.attributes.feedback.health.primaryCareRoutine) {
-      routineScoreMarkup.push(
-        <div className="facility-satisfaction-tile vads-u-background-color--gray-lightest vads-u-padding--1p5 vads-u-margin-right--1">
-          <p className="vads-u-margin--0">Primary care</p>
-          <p
-            id="facility-patient-satisfaction-scores-primary-routine-score"
-            className="vads-u-font-size--lg vads-u-font-weight--bold vads-u-margin--0 vads-u-font-family--serif"
-          >
-            {displayPercent(
-              this.state.facility.attributes.feedback.health.primaryCareRoutine,
-            )}
-          </p>
-        </div>,
-      );
-    }
-
-    if (this.state.facility.attributes.feedback.health.specialtyCareRoutine) {
-      routineScoreMarkup.push(
-        <div className="facility-satisfaction-tile vads-u-background-color--gray-lightest vads-u-padding--1p5">
-          <p className="vads-u-margin--0">Specialty care</p>
-          <p
-            id="facility-patient-satisfaction-scores-specialty-routine-score"
-            className="vads-u-font-size--lg vads-u-font-weight--bold vads-u-margin--0 vads-u-font-family--serif"
-          >
-            {displayPercent(
-              this.state.facility.attributes.feedback.health
-                .specialtyCareRoutine,
-            )}
-          </p>
-        </div>,
-      );
-    }
+    const facility = this.props.facility.attributes;
 
     return (
       <div>
         <h2>Our patient satisfaction scores</h2>
         <p id="facility-patient-satisfaction-scores-effective-date">
-          Last updated:{' '}
-          {formatDateLong(
-            this.state.facility.attributes.feedback.health.effectiveDate,
-          )}
+          Last updated: {formatDateLong(facility.feedback.health.effectiveDate)}
         </p>
         <p>
           Veteran-reported satisfaction scores come from the Consumer Assessment
@@ -130,7 +37,28 @@ export default class FacilityPatientSatisfactionScoresWidget extends React.Compo
         </p>
         <div className="usa-grid-full">
           <div className="usa-width-one-half vads-u-display--flex">
-            {urgentScoreMarkup}
+            {!!facility.feedback.health.primaryCareUrgent && (
+              <div className="facility-satisfaction-tile vads-u-background-color--gray-lightest vads-u-padding--1p5 vads-u-margin-right--1">
+                <p className="vads-u-margin--0">Primary care</p>
+                <p
+                  id="facility-patient-satisfaction-scores-primary-urgent-score"
+                  className="vads-u-font-size--lg vads-u-font-weight--bold vads-u-margin--0 vads-u-font-family--serif"
+                >
+                  {displayPercent(facility.feedback.health.primaryCareUrgent)}
+                </p>
+              </div>
+            )}
+            {!!facility.feedback.health.specialtyCareUrgent && (
+              <div className="facility-satisfaction-tile vads-u-background-color--gray-lightest vads-u-padding--1p5">
+                <p className="vads-u-margin--0">Specialty care</p>
+                <p
+                  id="facility-patient-satisfaction-scores-specialty-urgent-score"
+                  className="vads-u-font-size--lg vads-u-font-weight--bold vads-u-margin--0 vads-u-font-family--serif"
+                >
+                  {displayPercent(facility.feedback.health.specialtyCareUrgent)}
+                </p>
+              </div>
+            )}
           </div>
         </div>
 
@@ -141,10 +69,44 @@ export default class FacilityPatientSatisfactionScoresWidget extends React.Compo
         </p>
         <div className="usa-grid-full">
           <div className="usa-width-one-half vads-u-display--flex">
-            {routineScoreMarkup}
+            {!!facility.feedback.health.primaryCareRoutine && (
+              <div className="facility-satisfaction-tile vads-u-background-color--gray-lightest vads-u-padding--1p5 vads-u-margin-right--1">
+                <p className="vads-u-margin--0">Primary care</p>
+                <p
+                  id="facility-patient-satisfaction-scores-primary-routine-score"
+                  className="vads-u-font-size--lg vads-u-font-weight--bold vads-u-margin--0 vads-u-font-family--serif"
+                >
+                  {displayPercent(facility.feedback.health.primaryCareRoutine)}
+                </p>
+              </div>
+            )}
+
+            {!!facility.feedback.health.specialtyCareRoutine && (
+              <div className="facility-satisfaction-tile vads-u-background-color--gray-lightest vads-u-padding--1p5">
+                <p className="vads-u-margin--0">Specialty care</p>
+                <p
+                  id="facility-patient-satisfaction-scores-specialty-routine-score"
+                  className="vads-u-font-size--lg vads-u-font-weight--bold vads-u-margin--0 vads-u-font-family--serif"
+                >
+                  {displayPercent(
+                    facility.feedback.health.specialtyCareRoutine,
+                  )}
+                </p>
+              </div>
+            )}
           </div>
         </div>
       </div>
     );
   }
 }
+
+const mapStateToProps = store => ({
+  facility: store.facility.data,
+  loading: store.facility.loading,
+  error: store.facility.error,
+});
+
+export default connect(mapStateToProps)(
+  FacilityPatientSatisfactionScoresWidget,
+);
