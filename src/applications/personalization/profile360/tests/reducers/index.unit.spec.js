f779f2b2372d8a84c6a6caf9b8a23d0c2277f71f
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-05-10 16:47:41 -0400

    update reducer tests

--- src/applications/personalization/profile360/tests/reducers/index.unit.spec.js
+++ src/applications/personalization/profile360/tests/reducers/index.unit.spec.js
@@ -7,67 +7,81 @@ const { vaProfile } = profileReducer;
 
 describe('index reducer', () => {
   it('should fetch hero info', () => {
-    const state = vaProfile(
-      {},
-      {
-        type: 'FETCH_HERO_SUCCESS',
-        hero: 'heroContent',
-      },
-    );
+    const state = vaProfile(undefined, {
+      type: 'FETCH_HERO_SUCCESS',
+      hero: 'heroContent',
+    });
 
     expect(state.hero).to.eql('heroContent');
   });
 
   it('should fetch personalInformation', () => {
-    const state = vaProfile(
-      {},
-      {
-        type: 'FETCH_PERSONAL_INFORMATION_SUCCESS',
-        personalInformation: 'personalInformation',
-      },
-    );
+    const state = vaProfile(undefined, {
+      type: 'FETCH_PERSONAL_INFORMATION_SUCCESS',
+      personalInformation: 'personalInformation',
+    });
 
     expect(state.personalInformation).to.eql('personalInformation');
   });
 
-  it('updates payment info and ui state', () => {
+  it('fetches paymentInformation', () => {
     const fetchAction = {
       type: paymentInfoActions.PAYMENT_INFORMATION_FETCH_SUCCEEDED,
-      paymentInformation: {
+      response: {
         something: 'something',
       },
     };
 
-    let state = vaProfile({}, fetchAction);
+    const state = vaProfile(undefined, fetchAction);
 
     expect(state.paymentInformation).to.be.deep.equal(
-      fetchAction.paymentInformation,
+      fetchAction.response,
       'paymentInformation is initialized',
     );
+  });
 
-    const editAction = {
-      type: paymentInfoActions.PAYMENT_INFO_UI_STATE_CHANGED,
-      state: {
-        isEditing: true,
+  it('opens the paymentInformation-edit modal', () => {
+    const modalOpenAction = {
+      type: paymentInfoActions.PAYMENT_INFORMATION_EDIT_MODAL_TOGGLED,
+    };
+
+    const state = vaProfile(undefined, modalOpenAction);
+
+    expect(state.paymentInformationUiState.isEditing).to.be.true;
+  });
+
+  it('updates a field in the paymentInformation-edit modal', () => {
+    const editFieldAction = {
+      type: paymentInfoActions.PAYMENT_INFORMATION_EDIT_MODAL_FIELD_CHANGED,
+      fieldName: 'accountNumber',
+      fieldValue: {
+        errorMessage: 'Example error message',
+        field: {
+          value: '123',
+          dirty: true,
+        },
       },
     };
 
-    state = vaProfile(state, editAction);
+    const state = vaProfile(undefined, editFieldAction);
 
-    expect(state.paymentInformationUiState.isEditing).to.be.true;
+    expect(
+      state.paymentInformationUiState.editModalForm.accountNumber,
+    ).to.deep.equal(editFieldAction.fieldValue);
+  });
 
+  it('saves paymentInformation', () => {
     const saveAction = {
       type: paymentInfoActions.PAYMENT_INFORMATION_SAVE_STARTED,
     };
 
-    state = vaProfile(state, saveAction);
+    let state = vaProfile(undefined, saveAction);
 
-    expect(state.paymentInformationUiState.isEditing).to.be.true;
     expect(state.paymentInformationUiState.isSaving).to.be.true;
 
     const savedAction = {
       type: paymentInfoActions.PAYMENT_INFORMATION_SAVE_SUCCEEDED,
-      paymentInformation: {
+      response: {
         somethingElse: 'somethingElse',
       },
     };
@@ -75,7 +89,7 @@ describe('index reducer', () => {
     state = vaProfile(state, savedAction);
 
     expect(state.paymentInformation).to.be.deep.equal(
-      savedAction.paymentInformation,
+      savedAction.response,
       'paymentInformation is updated',
     );
   });
