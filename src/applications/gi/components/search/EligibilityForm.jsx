1ce926153906f516a766ae0f4d6bae647591263d
Matt Roth <roth_matthew@bah.com>
2019-05-17 10:04:49 -0400

    review chagnes

--- src/applications/gi/components/search/EligibilityForm.jsx
+++ src/applications/gi/components/search/EligibilityForm.jsx
@@ -14,14 +14,15 @@ export class EligibilityForm extends React.Component {
   }
 
   cumulativeServiceOptions = () => {
-    if (!environment.isProduction()) {
+    if (environment.isProduction()) {
       return [
         { value: '1.0', label: '36+ months: 100% (includes BASIC)' }, // notice not 1.00
         { value: '0.9', label: '30 months: 90% (includes BASIC)' },
         { value: '0.8', label: '24 months: 80% (includes BASIC)' },
         { value: '0.7', label: '18 months: 70% (excludes BASIC)' },
-        { value: '0.6', label: '6 months: 60% (excludes BASIC)' },
-        { value: '0.5', label: '90 days: 50% (excludes BASIC)' },
+        { value: '0.6', label: '12 months: 60% (excludes BASIC)' },
+        { value: '0.5', label: '6 months: 50% (excludes BASIC)' },
+        { value: '0.4', label: '90 days: 40% (excludes BASIC)' },
         { value: '0.0', label: 'Less than 90 days 0% (excludes BASIC)' },
         { value: '1.00', label: 'GYSGT Fry Scholarship: 100%' }, // notice not 1.0
         {
@@ -37,9 +38,8 @@ export class EligibilityForm extends React.Component {
       { value: '0.9', label: '30 months: 90% (includes BASIC)' },
       { value: '0.8', label: '24 months: 80% (includes BASIC)' },
       { value: '0.7', label: '18 months: 70% (excludes BASIC)' },
-      { value: '0.6', label: '12 months: 60% (excludes BASIC)' },
-      { value: '0.5', label: '6 months: 50% (excludes BASIC)' },
-      { value: '0.4', label: '90 days: 40% (excludes BASIC)' },
+      { value: '0.6', label: '6 months: 60% (excludes BASIC)' },
+      { value: '0.5', label: '90 days: 50% (excludes BASIC)' },
       { value: '0.0', label: 'Less than 90 days 0% (excludes BASIC)' },
       { value: '1.00', label: 'GYSGT Fry Scholarship: 100%' }, // notice not 1.0
       {
@@ -49,6 +49,7 @@ export class EligibilityForm extends React.Component {
       { value: 'purple heart', label: 'Purple Heart Service: 100%' },
     ];
   };
+
   renderLearnMoreLabel({ text, modal }) {
     return (
       <span>
