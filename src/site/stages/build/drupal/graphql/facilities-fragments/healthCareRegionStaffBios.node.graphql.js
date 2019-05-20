efa9524a903fe586a6db0053296e7732e7a9cf11
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2019-05-20 00:46:01 -0600

    VAGOV-3663: Revert remaining graphql image query changes

--- src/site/stages/build/drupal/graphql/facilities-fragments/healthCareRegionStaffBios.node.graphql.js
+++ src/site/stages/build/drupal/graphql/facilities-fragments/healthCareRegionStaffBios.node.graphql.js
@@ -26,7 +26,7 @@ const PERSON_PROFILE_RESULTS = `
               alt
               title
               url
-              derivative(style: _11SQUAREMEDIUMTHUMBNAIL) {
+              derivative(style: _1_1_SQUARE_MEDIUM_THUMBNAIL) {
                 url
                 width
                 height
--- src/site/utilities/stringHelpers.js
+++ src/site/utilities/stringHelpers.js
@@ -1,3 +1,4 @@
+// If string starts with a number prepend an underscore to it
 function prependUnderscore(string) {
   const firstNumber = new RegExp('^\\d');
   if (firstNumber.test(string)) {
@@ -6,6 +7,7 @@ function prependUnderscore(string) {
   return string;
 }
 
+// Remove all underscores from body of string and then prepend an underscore if string starts with number
 function shiftUnderscores(matchedString) {
   const findUnderscores = new RegExp('_', 'g');
   return prependUnderscore(matchedString.replace(findUnderscores, ''));
