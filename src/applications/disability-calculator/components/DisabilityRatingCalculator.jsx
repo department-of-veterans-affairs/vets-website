0495c33fd27c6e95751727be3e618fd0a41164d7
aubreyarcangel <aubrey@thesocompany.com>
2019-05-20 13:26:45 -0400

    added more content elements

--- src/applications/disability-calculator/components/DisabilityRatingCalculator.jsx
+++ src/applications/disability-calculator/components/DisabilityRatingCalculator.jsx
@@ -9,6 +9,7 @@ export default class DisabilityRatingCalculator extends React.Component {
 
     addRating() {
         this.setState({ ratings: [...this.state.ratings, ""] })
+        console.log(this.state.ratings)
     }
     render() {
         return (
@@ -17,23 +18,80 @@ export default class DisabilityRatingCalculator extends React.Component {
                 <p>Use our calculator if you have more than one disability
                     rating to determine your VA comined disability rating
                 </p>
-                <label>Rating</label>
-                {
-                    this.state.ratings.map((rating, index) => {
-                        return (
-                            <div key={index}>
-                                <input value={rating} />
+                <div className="vads-l-grid-container">
+                    <div className="vads-l-row">
+                        <div className="vads-l-col--3 vads-u-padding-right--2">
+                            Disability rating
+                      </div>
+                        <div className="vads-l-col--8">
+                            Optional description
+                      </div>
+                    </div>
+                    <div className="vads-l-row">
+                        <div className="vads-l-col--2 vads-u-padding-right--2">
+                            <input
+                                type="number"
+                            />
+                        </div>
+                        <div className="vads-l-col--8">
+                            <input />
+                        </div>
+                        <div className="vads-l-col--2">
+                            <button><i class="fas fa-trash-alt"></i></button> <a href="#">Delete</a>
+                        </div>
+                    </div>
+                    {
+                        this.state.ratings.map((rating, index) => {
+                            return (
+                                <div className="vads-l-row" key={index}>
+                                    <div className="vads-l-col--2 vads-u-padding-right--2">
+                                        <input value={rating} />
+                                    </div>
+                                    <div className="vads-l-col--8">
+                                        <input />
+                                    </div>
+                                    <div className="vads-l-col--2">
+                                        <button><i class="fas fa-trash-alt"></i></button> <a href="#">Delete</a>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                    {/* </div> */}
+                    <br />
+                    <div className="vads-l-grid-container">
+                        <div className="vads-l-row">
+                            <div className="vads-l-col--3">
+                                <button onClick={(e) => this.addRating(e)}><i class="fas fa-plus-circle"></i></button><a onClick={(e) => this.addRating(e)}>Add rating</a>
                             </div>
-                        )
-                    })
-                }
-                <button className="usa-button-primary" onClick={(e) => this.addRating(e)}>Add rating</button>
-                <button className="usa-button-primary">Submit</button>
+                            <div className="vads-l-col--8">
 
-
-
+                            </div>
+                        </div>
+                        <div className="vads-l-row">
+                            <div className="vads-l-col--3 vads-u-padding-right--2">
+                                <button className="usa-button-primary">Calculate</button>
+                            </div>
+                            <div className="vads-l-col--8">
+                                <a href="#">Clear all</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <p className="vads-u-font-weight--bold">This is your VA Rating:</p>
+                    <div className="ratingContainer">
+                        <p className="vads-u-font-size--h2">60%</p>
+                    </div>
+                    <p>Your actual combined disability rating is 64%/ We round this number
+                        to the nearest 10% to get your <i>VA Disability rating </i>.
+                        We round down values in 1 to 4, and round up values ending in 5 to 9.
+                    </p>
+                    <a href="#">Find your monthly amount</a>
+                </div>
 
             </div>
+
         )
     }
 }
