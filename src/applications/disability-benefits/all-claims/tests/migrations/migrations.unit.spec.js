04bcd81932b712b0b4da99cc162246f36066248c
Christopher Valarida <chris.valarida@adhocteam.us>
2019-05-21 11:53:00 -0700

    Add migration tests

--- src/applications/disability-benefits/all-claims/tests/migrations/migrations.unit.spec.js
+++ src/applications/disability-benefits/all-claims/tests/migrations/migrations.unit.spec.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 
 import redirectToClaimTypePage from '../../migrations/01-require-claim-type';
 import upgradeHasSeparationPay from '../../migrations/03-upgrade-hasSeparationPay';
+import addDisabilityUUID from '../../migrations/05-add-disability-uuid';
 import formConfig from '../../config/form';
 
 describe('526 v2 migrations', () => {
@@ -43,6 +44,7 @@ describe('526 v2 migrations', () => {
       );
     });
   });
+
   describe('02-upgrade-separationPay', () => {
     it('should migrate view:hasSeparationPay to hasSeparationPay', () => {
       const savedData = {
@@ -52,6 +54,47 @@ describe('526 v2 migrations', () => {
       expect(migratedData['view:hasSeparationPay']).to.be.undefined;
       expect(savedData['view:hasSeparationPay']).to.be.true;
       expect(migratedData.hasSeparationPay).to.be.true;
+    });
+  });
+
+  describe('05-add-disability-uuid', () => {
+    const savedData = {
+      formData: {
+        ratedDisabilities: [{ name: 'Something' }],
+        newDisabilities: [{ condition: 'Something else' }],
+        vaTreatmentFacilities: [
+          { treatedDisabilityNames: { something: true } },
+        ],
+        'view:isPow': { powDisabilities: { 'something else': true } },
+      },
+    };
+
+    const result = addDisabilityUUID(savedData);
+
+    it('should not break with missing data', () => {
+      // If an error is thrown `expect` will catch it and fail the test.
+      expect(addDisabilityUUID({ formData: {} }));
+    });
+
+    it('should add uuids to rated disabilities', () => {
+      expect(result.formData.ratedDisabilities[0].uuid).to.have.length(32);
+    });
+
+    it('should add uuids to new disabilities', () => {
+      expect(result.formData.newDisabilities[0].uuid).to.have.length(32);
+    });
+
+    it('should replace property names in treatedDisabilityNames with uuids', () => {
+      const uuid = result.formData.ratedDisabilities[0].uuid;
+      expect(
+        result.formData.vaTreatmentFacilities[0].treatedDisabilityNames[uuid],
+      ).to.be.true;
+    });
+
+    it('should replace property names in powDisabiliteis with uuids', () => {
+      const uuid = result.formData.newDisabilities[0].uuid;
+      // console.log(result.formData['view:isPow'].powDisabilities);
+      expect(result.formData['view:isPow'].powDisabilities[uuid]).to.be.true;
     });
   });
 });
