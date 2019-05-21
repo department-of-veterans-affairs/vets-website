caaf9724e8eacaec16efe29c56d8887e95b0ef30
Christopher Valarida <chris.valarida@adhocteam.us>
2019-05-21 09:37:03 -0700

    newDisabilitiesHook tests

--- src/applications/disability-benefits/all-claims/tests/pages/addDisabilities.unit.spec.jsx
+++ src/applications/disability-benefits/all-claims/tests/pages/addDisabilities.unit.spec.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { DefinitionTester } from '../../../../../platform/testing/unit/schemaform-utils.jsx';
+import set from '../../../../../platform/utilities/data/set';
 import { mount } from 'enzyme';
 import formConfig from '../../config/form';
+
+import { newDisabilitiesHook } from '../../pages/addDisabilities';
 
 describe('Add new disabilities', () => {
   const {
@@ -79,5 +82,65 @@ describe('Add new disabilities', () => {
     expect(form.find('.usa-input-error-message').length).to.equal(0);
     expect(onSubmit.called).to.be.true;
     form.unmount();
+  });
+
+  describe('newDisabilitiesHook', () => {
+    // It's a function just to make sure we're not mutating it anywhere along the way
+    const oldData = () => ({
+      newDisabilities: [
+        { name: 'Something', uuid: '9238da0f1bc9316de95ab0c3f68ea35' },
+      ],
+      vaTreatmentFacilities: [
+        { treatedDisabilityNames: { '9238da0f1bc9316de95ab0c3f68ea35': true } },
+      ],
+      'view:isPow': {
+        powDisabilities: { '9238da0f1bc9316de95ab0c3f68ea35': true },
+      },
+    });
+
+    it('should not modify oldData', () => {
+      const old = oldData();
+      newDisabilitiesHook(
+        old,
+        set('newDisabilities[1]', { name: 'Something else' }, oldData()),
+      );
+      expect(old).to.deep.equal(oldData());
+    });
+
+    it('should give a uuid to a new disability', () => {
+      const newData = set(
+        'newDisabilities[1]',
+        { name: 'Something else' },
+        oldData(),
+      );
+      const result = newDisabilitiesHook(oldData(), newData);
+      // uuids are strings with a length of 32
+      expect(result.newDisabilities[1].uuid).to.have.length(32);
+    });
+
+    it('should remove a deleted disability from treatedDisabilityNames and powDisabilities', () => {
+      const newData = Object.assign(oldData(), { newDisabilities: [] });
+      const result = newDisabilitiesHook(oldData(), newData);
+      expect(result.vaTreatmentFacilities[0].treatedDisabilityNames).to.be
+        .empty;
+      expect(result['view:isPow'].powDisabilities).to.be.empty;
+    });
+
+    it('should not change any data if a disability was modified', () => {
+      const newData = set(
+        'newDisabilities[0].name',
+        'Something else',
+        oldData(),
+      );
+      const result = newDisabilitiesHook(oldData(), newData);
+      expect(
+        result.vaTreatmentFacilities[0].treatedDisabilityNames,
+      ).to.deep.equal(
+        oldData().vaTreatmentFacilities[0].treatedDisabilityNames,
+      );
+      expect(result['view:isPow'].powDisabilities).to.deep.equal(
+        oldData()['view:isPow'].powDisabilities,
+      );
+    });
   });
 });
