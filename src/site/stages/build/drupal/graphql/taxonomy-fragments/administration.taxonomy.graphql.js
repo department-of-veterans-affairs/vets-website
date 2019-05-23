04138970c31f85405d2c31facafb277b5897b51d
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2019-05-22 20:06:11 -0600

    VAGOV-3663: remaining administration field fix

--- src/site/stages/build/drupal/graphql/taxonomy-fragments/administration.taxonomy.graphql.js
+++ src/site/stages/build/drupal/graphql/taxonomy-fragments/administration.taxonomy.graphql.js
@@ -3,23 +3,10 @@
  * Appears on hub landing pages.
  */
 
-// Get current feature flags
-let pageAdministration;
-const {
-  featureFlags,
-  enabledFeatureFlags,
-} = require('./../../../../../utilities/featureFlags');
-
-if (enabledFeatureFlags[featureFlags.GRAPHQL_MODULE_UPDATE]) {
-  pageAdministration = 'FieldNodeLandingPageFieldAdministration';
-} else {
-  pageAdministration = 'FieldNodeAdministration';
-}
-
 module.exports = `
     fragment administration on NodeLandingPage {
         fieldAdministration {
-        ... on ${pageAdministration} {
+        ... on FieldNodeLandingPageFieldAdministration {
           entity {
             ... on TaxonomyTermAdministration {
               name
