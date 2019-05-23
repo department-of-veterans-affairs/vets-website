fe317217d6d550183999dbfe3f86e59ece4b196b
Narin Ratana <narin@adhoc.team>
2019-05-23 14:48:41 -0700

    Replaced USWDS accordions with formation-react components

--- src/applications/validate-mhv-account/containers/CreateMHVAccountFailed.jsx
+++ src/applications/validate-mhv-account/containers/CreateMHVAccountFailed.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import MessageTemplate from '../components/MessageTemplate';
+import CollapsiblePanel from '@department-of-veterans-affairs/formation-react/CollapsiblePanel';
 
+import MessageTemplate from '../components/MessageTemplate';
 import { createAndUpgradeMHVAccount } from '../../../platform/user/profile/actions';
 
 class CreateMHVAccountFailed extends React.Component {
@@ -32,74 +33,60 @@ class CreateMHVAccountFailed extends React.Component {
               your account in one of these ways:
             </strong>
           </p>
-          <ul className="usa-accordion">
-            <li>
-              <button
-                className="usa-accordion-button"
-                aria-expanded="false"
-                aria-controls="a1"
-              >
-                Call the My HealtheVet help desk
-              </button>
-              <div id="a1" className="usa-accordion-content">
+          <CollapsiblePanel
+            panelName="Call the My HealtheVet help desk"
+            borderless
+          >
+            <p>
+              Call the My HealtheVet help desk at{' '}
+              <a href="tel:877-327-0022">877-327-0022</a>. We’re here Monday
+              through Friday, 8:00 a.m. to 8:00 p.m. ET. If you have hearing
+              loss, call TTY: 800-877-3399.
+            </p>
+            <p>
+              Tell the representative that you tried to sign in to use health
+              tools on VA.gov, but received an error message telling you that we
+              couldn’t create an account for you.
+            </p>
+          </CollapsiblePanel>
+
+          <CollapsiblePanel
+            panelName="Submit an online help request to My HealtheVet"
+            borderless
+          >
+            <p>
+              Use the My HealtheVet contact form to submit an online request for
+              help.
+            </p>
+            <p>
+              <strong>Fill in the form fields as below:</strong>
+            </p>
+            <ul>
+              <li>
+                <strong>Topic:</strong> Select <strong>Account Login</strong>.
+              </li>
+              <li>
+                <strong>Category:</strong> Select{' '}
+                <strong>Request for Assistance</strong>.
+              </li>
+              <li>
+                <strong>Comments:</strong> Type, or copy and paste, in the
+                message below:
                 <p>
-                  Call the My HealtheVet help desk at{' '}
-                  <a href="tel:877-327-0022">877-327-0022</a>. We’re here Monday
-                  through Friday, 8:00 a.m. to 8:00 p.m. ET. If you have hearing
-                  loss, call TTY: 800-877-3399.
+                  “When I tried to sign in to use health tools on VA.gov, I
+                  received an error message telling me that the site couldn't
+                  create a My HealtheVet account for me.”
                 </p>
-                <p>
-                  Tell the representative that you tried to sign in to use
-                  health tools on VA.gov, but received an error message telling
-                  you that we couldn’t create an account for you.
-                </p>
-              </div>
-            </li>
-            <li>
-              <button
-                className="usa-accordion-button"
-                aria-expanded="false"
-                aria-controls="a2"
-              >
-                Submit an online help request to My HealtheVet
-              </button>
-              <div id="a2" className="usa-accordion-content">
-                <p>
-                  Use the My HealtheVet contact form to submit an online request
-                  for help.
-                </p>
-                <p>
-                  <strong>Fill in the form fields as below:</strong>
-                </p>
-                <ul>
-                  <li>
-                    <strong>Topic:</strong> Select{' '}
-                    <strong>Account Login</strong>.
-                  </li>
-                  <li>
-                    <strong>Category:</strong> Select{' '}
-                    <strong>Request for Assistance</strong>.
-                  </li>
-                  <li>
-                    <strong>Comments:</strong> Type, or copy and paste, in the
-                    message below:
-                    <p>
-                      “When I tried to sign in to use health tools on VA.gov, I
-                      received an error message telling me that the site
-                      couldn't create a My HealtheVet account for me.”
-                    </p>
-                  </li>
-                </ul>
-                <p>
-                  Then, complete the rest of the form and click{' '}
-                  <strong>Submit</strong>
-                </p>
-                <a href="https://www.myhealth.va.gov/mhv-portal-web/contact-us">
-                  Go to the My HealtheVet contact form
-                </a>
-              </div>
-            </li>
-          </ul>
+              </li>
+            </ul>
+            <p>
+              Then, complete the rest of the form and click{' '}
+              <strong>Submit</strong>
+            </p>
+            <a href="https://www.myhealth.va.gov/mhv-portal-web/contact-us">
+              Go to the My HealtheVet contact form
+            </a>
+          </CollapsiblePanel>
         </>
       ),
     };
