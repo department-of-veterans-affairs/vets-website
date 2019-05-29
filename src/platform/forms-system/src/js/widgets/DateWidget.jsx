01da44928fc1a3278ab049ec8b35077f7cad3772
Bill Tran <bill@billjohntran.com>
2019-05-29 15:11:52 -0700

    add ariadescribe to date widget

--- src/platform/forms-system/src/js/widgets/DateWidget.jsx
+++ src/platform/forms-system/src/js/widgets/DateWidget.jsx
@@ -87,6 +87,7 @@ export default class DateWidget extends React.Component {
             Month
           </label>
           <select
+            aria-describedby={this.props.id}
             name={`${id}Month`}
             id={`${id}Month`}
             value={month}
@@ -106,6 +107,7 @@ export default class DateWidget extends React.Component {
               Day
             </label>
             <select
+              aria-describedby={this.props.id}
               name={`${id}Day`}
               id={`${id}Day`}
               value={day}
@@ -126,6 +128,7 @@ export default class DateWidget extends React.Component {
             Year
           </label>
           <input
+            aria-describedby={this.props.id}
             type="number"
             autoComplete={options.autocomplete}
             name={`${id}Year`}
