28e5e07b4a8c49ca107eee93f0ce3432d48a5f94
Christopher Valarida <chris.valarida@adhocteam.us>
2019-05-29 12:28:09 -0700

    Use nextUrl instead of an arbitrary waitFor

--- src/platform/testing/e2e/form-tester/form-filler.js
+++ src/platform/testing/e2e/form-tester/form-filler.js
@@ -370,9 +370,7 @@ const fillForm = async (page, testData, testConfig, log) => {
       // If we're still on the page, it may be because an element was expanded
       //  and we tried to enter data too fast; try again
       // NOTE: This won't trigger if the field we missed isn't required!
-      if (page.url() === url) {
-        // TODO: Figure out how to remove this arbitrary time
-        await page.waitFor(500);
+      if ((await nextUrl(page, { previousUrl: url })) === url) {
         await removeForeseeOverlay(page);
         await fillPage(page, testData, testConfig, log);
         log('Clicking continue again');
