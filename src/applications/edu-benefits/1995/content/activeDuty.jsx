2faad1172de803e2a02294889f1b3075d5959559
Matt Roth <roth_matthew@bah.com>
2019-05-30 14:13:30 -0400

    code review changes

--- src/applications/edu-benefits/1995/content/activeDuty.jsx
+++ src/applications/edu-benefits/1995/content/activeDuty.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 export const housingPaymentInfo = () => (
   <div className="feature">
     If you are on active duty or if you anticipate going on active duty, it may
-    impact your housing payment while receiving the STEM scholarship
+    impact your housing payment while receiving the STEM scholarship.
   </div>
 );
