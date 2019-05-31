54e4ef87d48ae562a876a7552169e4c5443bce9e
lightning <aurora.hay@agile6.com>
2019-05-31 10:19:50 -0700

    VAGOV-3748: fix linting in test

--- src/applications/static-pages/tests/facilities/FacilityAppointmentWaitTimesWidget.unit.spec.jsx
+++ src/applications/static-pages/tests/facilities/FacilityAppointmentWaitTimesWidget.unit.spec.jsx
@@ -24,9 +24,7 @@ describe('facilities <FacilityAppointmentWaitTimesWidget>', () => {
     expect(tree.find('LoadingIndicator').exists()).to.be.false;
 
     const appointmentWaitTimesHeader = tree.find('h3');
-    expect(appointmentWaitTimesHeader.text()).to.contain(
-      'Patient wait times',
-    );
+    expect(appointmentWaitTimesHeader.text()).to.contain('Patient wait times');
 
     const facilityAppointmentWaitTimesEffectiveDate = tree.find(
       '#facility-primaryCare-appointment-wait-times-effective-date',
