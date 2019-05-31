7e22b326fd7feae6c8e7be11ed79095d352ac605
Chris Valarida <chris.valarida@adhocteam.us>
2019-05-31 09:35:28 -0700

    18656: STEM: Update 1995 Education Benefit Page: Remove unused iport from new-chpaters.js

---
+++ src/applications/edu-benefits/1995/pages/benefitSelection.js
@@ -0,0 +1,23 @@
+import fullSchema from 'vets-json-schema/dist/22-1995-schema.json';
+
+import { benefitsLabels } from '../../utils/labels';
+
+const { benefit } = fullSchema.properties;
+
+export const uiSchema = {
+  benefit: {
+    'ui:widget': 'radio',
+    'ui:title':
+      'Which benefit are you currently using or have you used most recently?',
+    'ui:options': {
+      labels: benefitsLabels,
+    },
+  },
+};
+
+export const schema = {
+  type: 'object',
+  properties: {
+    benefit,
+  },
+};
--- src/applications/edu-benefits/1995/pages/index.js
+++ src/applications/edu-benefits/1995/pages/index.js
@@ -1,3 +1,4 @@
 import * as activeDuty from './activeDuty';
+import * as benefitSelection from './benefitSelection';
 
-export { activeDuty };
+export { activeDuty, benefitSelection };
