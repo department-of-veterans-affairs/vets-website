207c367bd53c2407b9723d007fe16b17076caa06
aubreyarcangel <aubrey@thesocompany.com>
2019-06-04 13:40:28 -0400

    moved calculator footer to own component

--- src/applications/disability-calculator/components/CalculatedDisabilityRating.jsx
+++ src/applications/disability-calculator/components/CalculatedDisabilityRating.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { calculateRating, roundRating } from '../utils/helpers';
+import { calculateRating } from '../utils/helpers';
 
-
-export class CalculatedDisabilityRating extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        console.log('props ratings: ', this.props)
-        return (
-            <div>
-                Your rating is: {calculateRating(this.props.ratings)} %
-            </div>
-        )
-    }
-}
+export const CalculatedDisabilityRating = props => {
+  //   console.log('props ratings: ', this.props);
+  const ratings = props.ratings;
+  return (
+    <div className="vads-u-padding--4">
+      <p className="vads-u-font-weight--bold">
+        Your VA disability rating
+        <br />
+        <span className="vads-u-font-weight--bold vads-u-font-size--2xl">
+          {calculateRating(ratings)} %
+        </span>
+      </p>
+      <p>
+        The final combined value of your disability ratings is 64%. We round
+        this number to the nearest 10% to get your combined rating. We round
+        combined values ending in 1 to 4 down, and those ending in 5 to 9 up. We
+        round down values ending in 1 to 4, and round up values ending in 5 to
+        9.
+      </p>
+      <a href="#">Find your monthly payment amount</a>
+    </div>
+  );
+};
--- src/applications/disability-calculator/components/DisabilityRatingCalculator.jsx
+++ src/applications/disability-calculator/components/DisabilityRatingCalculator.jsx
@@ -170,25 +170,8 @@ export default class DisabilityRatingCalculator extends React.Component {
             </div>
           </div>
         </div>
-        <div className="vads-u-padding--4">
-          <p className="vads-u-font-weight--bold">
-            Your VA disability rating
-            <br />
-            <span className="vads-u-font-weight--bold vads-u-font-size--2xl">
-              {calculateRating(ratings)} %
-            </span>
-          </p>
-          <p>
-            The final combined value of your disability ratings is 64%. We round
-            this number to the nearest 10% to get your combined rating. We round
-            combined values ending in 1 to 4 down, and those ending in 5 to 9
-            up. We round down values ending in 1 to 4, and round up values
-            ending in 5 to 9.
-          </p>
-          <br />
-          <a href="#">Find your monthly payment amount</a>
-        </div>
-        {/* </div> */}
+
+        <CalculatedDisabilityRating ratings={ratings} />
       </div>
     );
   }
