afed5d8066f3ad1ca4cd5f615ebb0aa8afe2e2dd
Adrienne <adrienne.cabouet@gmail.com>
2019-06-06 13:01:18 -0600

    VAGOV-2803: Outreach Hub Event Detail Page (#10191)
    * VAGOV-2803: Correct sidebar for outreach hub
    
    * VAGOV-2803: Fixing character link issues
    
    * VAGOV-2803: More ics generator fixes
    
    * VAGOV-2803: encodeURIComponent instead of encodeURI
    
    * VAGOV-2803: downloading ics another way
    
    * VAGOV-2803: Working ics download

--- src/applications/static-pages/ics-generator.js
+++ src/applications/static-pages/ics-generator.js
@@ -4,24 +4,46 @@ export function icsCreate(calendarLink) {
     const event = new ICS.VEVENT();
     const addToCalendarLink = document.getElementById('add-to-calendar-link');
     const start = addToCalendarLink.getAttribute('data-start');
+    const startDateObj = new Date(start);
     const end = addToCalendarLink.getAttribute('data-end');
+    const endDateObj = new Date(end);
     const location = addToCalendarLink.getAttribute('data-location');
     const description = addToCalendarLink.getAttribute('data-description');
+    const title = addToCalendarLink.getAttribute('data-subject');
 
     cal.addProp('VERSION', 2);
     cal.addProp('PRODID', 'VA');
     event.addProp('UID');
-    event.addProp('SUMMARY', [description]);
+    event.addProp('SUMMARY', [title]);
+    event.addProp('DESCRIPTION', [description]);
     event.addProp('LOCATION', [location]);
-    event.addProp('DTSTAMP', new Date(`${start} UTC`), { VALUE: 'DATE-TIME' });
-    event.addProp('DTSTART', new Date(`${start} UTC`), { VALUE: 'DATE-TIME' });
-    event.addProp('DTEND', new Date(`${end} UTC`), { VALUE: 'DATE-TIME' });
+    event.addProp('DTSTAMP', startDateObj);
+    event.addProp('DTSTART', startDateObj);
+    event.addProp('DTEND', endDateObj);
     cal.addComponent(event);
     const calLink = cal.toString();
 
     // Download the ics.
+    function download(filename, text) {
+      const element = document.createElement('a');
+      element.setAttribute(
+        'href',
+        `data:text/calendar;charset=utf8,${encodeURIComponent(text)}`,
+      );
+      element.setAttribute('download', filename);
+
+      element.style.display = 'none';
+      document.body.appendChild(element);
+
+      element.click();
+
+      document.body.removeChild(element);
+    }
+
     function calDown(e) {
-      window.open(`data:text/calendar;charset=utf8,${calLink}`);
+      // window.open(`data:text/calendar;charset=utf8,${calURI}`);
+      const filename = `${title}.ics`;
+      download(filename, calLink);
       e.preventDefault();
     }
 
