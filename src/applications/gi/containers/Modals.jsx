9ffd8ff443066627ab7354df678846a5a5272f23
Devin McCurdy <mccurdy_devin@bah.com>
2019-06-06 09:50:04 -0400

    Removed environment

--- src/applications/gi/containers/Modals.jsx
+++ src/applications/gi/containers/Modals.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import Modal from '@department-of-veterans-affairs/formation-react/Modal';
-import environment from '../../../platform/utilities/environment';
 
 export class Modals extends React.Component {
   constructor(props) {
