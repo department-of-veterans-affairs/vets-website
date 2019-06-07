913fa607e21188d6306e74a17d2591e1efaa5fcb
Erik P. Hansen <eph@erikphansen.com>
2019-06-07 11:51:37 -0700

    Show ESR server errors on Dashboard (#10146)
    * Show ESR server errors on Dashboard
    
    * disable eslint
    
    * Update content of ESR-related error messages
    
    * fix error headline

---
+++ src/applications/personalization/dashboard/components/ESRError.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
+
+export const ESR_ERROR_TYPES = Object.freeze({
+  generic: 'generic',
+  inContext: 'inContext',
+});
+
+const headlines = {
+  [ESR_ERROR_TYPES.generic]:
+    'We can’t access health care enrollment information right now',
+  [ESR_ERROR_TYPES.inContext]:
+    'We can’t display your VA health care enrollment information right now',
+};
+
+const contents = {
+  [ESR_ERROR_TYPES.generic]:
+    'We’re sorry. We’re having trouble accessing health care enrollment information right now. If you’ve recently applied for VA health care, or if you’re currently enrolled, some of your information may not appear below. We’re working to fix this as quickly as we can.',
+  [ESR_ERROR_TYPES.inContext]:
+    'We’re sorry. We’re having trouble accessing health care enrollment information right now. Your application and enrollment dates, as well as your preferred VA medical center, won’t appear until we fix this problem. We’re working to fix it as quickly as we can.',
+};
+
+/**
+ * Error message to show on the Dashboard if there is an error connecting to ESR
+ * via the enrollment_status endpoint
+ */
+const ESRError = ({ errorType = ESR_ERROR_TYPES.generic }) => (
+  <AlertBox
+    content={contents[errorType]}
+    headline={headlines[errorType]}
+    status="error"
+  />
+);
+
+export default ESRError;
