50d65a8369b8675399e44f5fb0d54eff1c8a7907
Narin Ratana <narin@users.noreply.github.com>
2019-06-17 14:34:40 -0700

    Disability Rating Calculator (continued) (#10251)
    * Update focus handling
    
    * Remove unused id attribute
    
    * Use focusFirstInput() after clearAll()
    
    * Rename focusFirstInput to focusFirstRatingInput
    
    * removed unused code
    
    * Replaced bold utility with <strong>
    
    * Removed all uses of eslint-disable
    
    * replaced css background style with utility class
    
    * adjusted spacing utilities
    
    * fixed lint return error in calcuateRating()
    
    * Add .tern-port IDE pref to gitignore
    
    * Get rid of hover styles by replating padding class with margin
    
    * added more calculation tests
    
    * added aria-labels on inputs
    
    * removed unncessary <span>

--- src/applications/disability-calculator/components/CalculatedDisabilityRating.jsx
+++ src/applications/disability-calculator/components/CalculatedDisabilityRating.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
-// import { calculateRating } from '../utils/helpers';
 
 export const CalculatedDisabilityRating = props => {
-  // const calculatedRating = props.calculatedRating;
   const displayRating = props.calculatedRating[0];
   const actualRating = props.calculatedRating[1];
 
   return (
     <div className="vads-u-padding--4">
-      <p className="vads-u-font-weight--bold">
-        Your VA disability rating
+      <p>
+        <strong>Your VA disability rating</strong>
         <br />
-        <span className="vads-u-font-weight--bold vads-u-font-size--2xl">
-          {displayRating} %
-        </span>
+        <strong>
+          <span className="vads-u-font-size--2xl">{displayRating} %</span>
+        </strong>
       </p>
       <p>
         The actual combined value of your disability ratings is
-        <span className="vads-u-font-weight--bold"> {actualRating}%</span>. We
-        round this number to the nearest 10% to get your combined rating. We
-        round values ending in 1 to 4 down, and those ending in 5 to 9 up. We
-        then use this VA disability rating to determine your monthly disability
-        compensation payment.
+        <strong> {actualRating}%</strong>. We round this number to the nearest
+        10% to get your combined rating. We round values ending in 1 to 4 down,
+        and those ending in 5 to 9 up. We then use this VA disability rating to
+        determine your monthly disability compensation payment.
       </p>
       <a href="#">Find your monthly payment amount</a>
     </div>
