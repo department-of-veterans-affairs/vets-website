7f831099add9b472fe511e9177f7834a1fd75825
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-06-18 17:40:19 -0400

    wip

--- src/platform/site-wide/announcements/components/MissionAct.jsx
+++ src/platform/site-wide/announcements/components/MissionAct.jsx
@@ -1,20 +1,62 @@
 import React from 'react';
 
+const PromoBannerTypes = {
+  announcement: 'announcement',
+  news: 'news',
+  emailSignup: 'email-signup',
+};
+
+const promoBannerIcons = new Map([
+  [PromoBannerTypes.announcement, 'fas fa-bullhorn fa-stack-1x'],
+  [PromoBannerTypes.news, 'fas fa-newspaper'],
+  [PromoBannerTypes.emailSignup, 'fas fa-envelope'],
+]);
+
+function PromoBanner({ children, destination, dismiss, type }) {
+  const icon = promoBannerIcons.get(type);
+
+  return (
+    <div className="va-promo-banner">
+      <div className="usa-grid-full">
+        <div className="vads-u-display--flex">
+          <div className="va-promo-banner-type vads-u-flex--auto">
+            <span className="vads-u-color--link-default fa-stack fa-lg">
+              <i className="vads-u-color--white fa fa-circle fa-stack-2x" />
+              <i className={icon} />
+            </span>
+          </div>
+
+          <div className="va-promo-banner-content vads-u-flex--1">
+            <a href={destination} onClick={dismiss}>
+              <strong>{children}</strong>{' '}
+              <i className="fas fa-angle-right vads-u-margin-left--1" />
+            </a>
+          </div>
+
+          <div className="va-promo-banner-dismiss vads-u-flex--auto">
+            <button
+              type="button"
+              aria-label="Dismiss this announcement"
+              onClick={dismiss}
+              className="va-button-link vads-u-margin-top--1"
+            >
+              <i className="fas fa-times-circle vads-u-font-size--lg" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function MissionAct({ dismiss }) {
   return (
-    <div className="personalization-announcement">
-      <span className="usa-label va-label-primary">New</span>{' '}
-      <a onClick={dismiss} href="https://missionact.va.gov/">
-        Learn how you can get easier access to health care with the MISSION Act
-      </a>
-      <button
-        type="button"
-        aria-label="Dismiss this announcement"
-        onClick={dismiss}
-        className="va-modal-close"
-      >
-        <i className="fas fa-times-circle" />
-      </button>
-    </div>
+    <PromoBanner
+      dismiss={dismiss}
+      type={PromoBannerTypes.announcement}
+      destination="https://missionact.va.gov/"
+    >
+      Learn how you can get easier access to health care with the MISSION Act
+    </PromoBanner>
   );
 }
