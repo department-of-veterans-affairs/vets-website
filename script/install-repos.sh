68c601fb964b151f21591cfd81537bdb3a0d6467
Jon Pugh <jon@thinkdrop.net>
2019-06-19 00:42:24 -0400

    VAGOV-4404: Add a yarn script to clone all dependent repos on yarn install

---
+++ script/install-repos.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+git clone git@github.com:department-of-veterans-affairs/vets-website.git
+git clone git@github.com:department-of-veterans-affairs/vagov-content.git
+git clone git@github.com:department-of-veterans-affairs/vets-json-schema.git
+git clone git@github.com:department-of-veterans-affairs/veteran-facing-services-tools.git
+git clone git@github.com:department-of-veterans-affairs/vets-api.git
+git clone git@github.com:department-of-veterans-affairs/vets-api-mockdata.git
