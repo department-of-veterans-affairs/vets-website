53671c4ff62be9f3e7bdf2435734f2dbea7f639b
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-06-19 14:17:41 -0400

    Refinements

--- src/platform/site-wide/announcements/components/MissionAct.jsx
+++ src/platform/site-wide/announcements/components/MissionAct.jsx
@@ -1,67 +1,13 @@
 import React from 'react';
-import classnames from 'classnames';
-
-const PromoBannerTypes = {
-  announcement: 'announcement',
-  news: 'news',
-  emailSignup: 'email-signup',
-};
-
-const promoBannerIcons = new Map([
-  [PromoBannerTypes.announcement, 'fas fa-bullhorn fa-stack-1x'],
-  [PromoBannerTypes.news, 'fas fa-newspaper'],
-  [PromoBannerTypes.emailSignup, 'fas fa-envelope'],
-]);
-
-function PromoBanner({ children, destination, dismiss, type }) {
-  const iconClasses = classnames(
-    'fas',
-    'fa-stack-1x',
-    promoBannerIcons.get(type),
-  );
-
-  return (
-    <div className="va-promo-banner">
-      <div className="usa-grid-full">
-        <div className="va-promo-banner-body">
-          <div className="va-promo-banner-content">
-            <span className="va-promo-banner-icon fa-stack fa-lg">
-              <i className="vads-u-color--white fa fa-circle fa-stack-2x" />
-              <i className={iconClasses} />
-            </span>
-            <a
-              className="va-promo-banner-link"
-              href={destination}
-              onClick={dismiss}
-            >
-              <strong>{children}</strong>{' '}
-              <i className="fas fa-angle-right vads-u-margin-left--1" />
-            </a>
-          </div>
-          <div className="va-promo-banner-dismiss">
-            <button
-              type="button"
-              aria-label="Dismiss this announcement"
-              onClick={dismiss}
-              className="va-button-link vads-u-margin-top--1"
-            >
-              <i className="fas fa-times-circle vads-u-font-size--lg" />
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+import PromoBanner, { PROMO_BANNER_TYPES } from './PromoBanner';
 
 export default function MissionAct({ dismiss }) {
   return (
     <PromoBanner
-      dismiss={dismiss}
-      type={PromoBannerTypes.announcement}
-      destination="https://missionact.va.gov/"
-    >
-      Learn how you can get easier access to health care with the MISSION Act
-    </PromoBanner>
+      type={PROMO_BANNER_TYPES.announcement}
+      onClose={dismiss}
+      href="https://missionact.va.gov/"
+      text="Learn how you can get easier access to health care with the MISSION Act"
+    />
   );
 }
---
+++ src/platform/site-wide/announcements/components/PromoBanner.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const PROMO_BANNER_TYPES = {
+  announcement: 'announcement',
+  news: 'news',
+  emailSignup: 'email-signup',
+};
+
+class PromoBanner extends React.Component {
+  static propTypes = {
+    type: PropTypes.oneOf(Object.values(PROMO_BANNER_TYPES)).isRequired,
+    onClose: PropTypes.func.isRequired,
+    render: PropTypes.func,
+    href: PropTypes.string,
+    text: PropTypes.string,
+    children: PropTypes.node,
+  };
+
+  static icons = new Map([
+    [PROMO_BANNER_TYPES.announcement, 'fas fa-bullhorn fa-stack-1x'],
+    [PROMO_BANNER_TYPES.news, 'fas fa-newspaper'],
+    [PROMO_BANNER_TYPES.emailSignup, 'fas fa-envelope'],
+  ]);
+
+  render() {
+    const iconClasses = classnames(
+      'fas',
+      'fa-stack-1x',
+      PromoBanner.icons.get(this.props.type),
+    );
+
+    return (
+      <div className="va-promo-banner">
+        <div className="usa-grid-full">
+          <div className="va-promo-banner-body">
+            <div className="va-promo-banner-content">
+              <div className="va-promo-banner-icon">
+                <span className="fa-stack fa-lg">
+                  <i className="vads-u-color--white fa fa-circle fa-stack-2x" />
+                  <i className={iconClasses} />
+                </span>
+              </div>
+
+              {this.props.render ? (
+                this.props.render()
+              ) : (
+                <a
+                  className="va-promo-banner-link"
+                  href={this.props.href}
+                  onClick={this.props.onClose}
+                >
+                  {this.props.text}{' '}
+                  <i className="fas fa-angle-right fa-lg vads-u-margin-left--1" />
+                </a>
+              )}
+            </div>
+
+            <div className="va-promo-banner-close">
+              <button
+                type="button"
+                aria-label="Dismiss this announcement"
+                onClick={this.props.onClose}
+                className="va-button-link vads-u-margin-top--1"
+              >
+                <i className="fas fa-times-circle vads-u-font-size--lg" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PromoBanner;
+
+export { PROMO_BANNER_TYPES };
