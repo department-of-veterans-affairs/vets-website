cf282e9cc449ae1f3060f7bb22be3a1971c76e8e
Jon Pugh <jon@thinkdrop.net>
2019-06-19 10:01:25 -0400

    VAGOV-2937: Don't error if repo already exists. Move to parent folder.

--- script/install-repos.sh
+++ script/install-repos.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-git clone git@github.com:department-of-veterans-affairs/vets-website.git
-git clone git@github.com:department-of-veterans-affairs/vagov-content.git
-git clone git@github.com:department-of-veterans-affairs/vets-json-schema.git
-git clone git@github.com:department-of-veterans-affairs/veteran-facing-services-tools.git
-git clone git@github.com:department-of-veterans-affairs/vets-api.git
-git clone git@github.com:department-of-veterans-affairs/vets-api-mockdata.git
+if [ ! -d ../vagov-content ]; then
+  git clone git@github.com:department-of-veterans-affairs/vagov-content.git ../vagov-content
+else
+  echo "Repo vagov-content already cloned."
+fi
+
+# @TODO: if these are not needed anymore, remove.
+# git clone git@github.com:department-of-veterans-affairs/vets-json-schema.git
+# git clone git@github.com:department-of-veterans-affairs/veteran-facing-services-tools.git
+# git clone git@github.com:department-of-veterans-affairs/vets-api.git
+# git clone git@github.com:department-of-veterans-affairs/vets-api-mockdata.git
