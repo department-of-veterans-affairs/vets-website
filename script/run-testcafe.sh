27d7de65d682803376d429e21ecdcaf361a543ae
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2019-06-20 04:52:13 -0400

    Added TestCafe and scripts to use it.

---
+++ script/run-testcafe.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+SAUCE=false
+
+for i in "$@"
+do
+case $i in
+  --sauce)
+      SAUCE=true
+      shift # past argument=value
+      ;;
+esac
+done
+
+# Harness for running end to end tests. The end to end test framework requires
+# a mock API server as well as a webserver with the content to provide
+# endpoints that selenium can run against.
+
+# Ensure all running servers are terminated on script exit.
+trap 'if [ $(jobs -p) ] ; then kill $(jobs -p); fi' EXIT
+
+BUILDTYPE=${BUILDTYPE:-vagovdev}
+
+"$(dirname "$0")"/run-mockapi.sh &
+
+# Check to see if we already have a server running on port 3001 (as with 'npm run build')
+ if [ "$(nc -z localhost 3001; echo $?)" -ne 0 ]; then
+  echo "Starting test-server.js..."
+  node src/platform/testing/e2e/test-server.js --buildtype ${BUILDTYPE} &
+else
+  echo "Using webpack-dev-server as test server on port 3001"
+  export WEB_PORT=3001
+fi
+
+
+# Wait for api server and web server to begin accepting connections
+# via http://unix.stackexchange.com/questions/5277
+while ! echo exit | nc localhost ${API_PORT:-3000}; do sleep 3; done
+while ! echo exit | nc localhost ${WEB_PORT:-3333}; do sleep 3; done
+
+# Webpack dev server blocks when attempting to read a generated file
+# until it is ready so executing a curl command for such a file ensures
+# the server is started before continuing.
+#
+# Do this after the nc localhost 3001 wait to ensure the server is up
+# otherwise curl may race the server start and not actually block.
+#curl http://localhost:3001/generated/hca.entry.js > /dev/null 2>&1
+
+# Execute the actual tests.
+if [ $SAUCE == true ]; then
+  yarn testcafe "saucelabs:Chrome@75.0:macOS 10.14,saucelabs:Firefox@67.0:macOS 10.14,saucelabs:Internet Explorer@11.285:Windows 10,saucelabs:MicrosoftEdge@16.16299:Windows 10,saucelabs:Safari@12.0:macOS 10.14,saucelabs:iPhone X Simulator@12.2,saucelabs:Samsung Galaxy S9 HD GoogleAPI Emulator@8.1" "${@}"
+else
+  yarn testcafe chrome "${@}"
+fi
