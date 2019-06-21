f61334033a7a5c2c91035450aa54f434fe4bc3e5
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2019-06-21 06:00:11 -0600

    VAGOV-4084: graphql and feature flag for get in touch box on local healthcare facilities

---
+++ src/site/stages/build/drupal/graphql/facilities-fragments/healthCareSocialMedia.fields.graphql.js
@@ -0,0 +1,35 @@
+/**
+ * Social media fields on health care systems and health care facilities
+ */
+module.exports = `
+   fieldFacebook {
+      url {
+        path
+      }
+      title
+    }
+    fieldTwitter {
+      url {
+        path
+      }
+      title
+    }
+    fieldFlickr {
+      url {
+        path
+      }
+      title
+    }
+    fieldInstagram {
+      url {
+        path
+      }
+      title
+    }
+    fieldEmailSubscription {
+      url {
+        path
+      }
+      title
+    }
+`;
