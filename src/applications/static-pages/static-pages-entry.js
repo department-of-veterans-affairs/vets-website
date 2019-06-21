08414b1fb93f4abdf6d2c88d176733b40cf342f8
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-06-21 10:45:24 -0400

    Update static-pages-entry.js

--- src/applications/static-pages/static-pages-entry.js
+++ src/applications/static-pages/static-pages-entry.js
@@ -45,7 +45,7 @@ createApplicationStatus(store, {
   applyHeading: 'How do I apply?',
   additionalText: 'You can apply online right now.',
   applyLink: '/pension/how-to-apply/',
-  applyText: 'Apply for veterans pension benefits',
+  applyText: 'Apply for Veterans pension benefits',
   widgetType: widgetTypes.PENSION_APP_STATUS,
 });
 
