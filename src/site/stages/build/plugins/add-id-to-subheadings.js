bc50e13309056196ccaf71a17d1435ad4c429ba9
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2019-06-21 02:46:48 -0600

    VAGOV-3897: add id to headings in build

--- src/site/stages/build/plugins/add-id-to-subheadings.js
+++ src/site/stages/build/plugins/add-id-to-subheadings.js
@@ -3,12 +3,11 @@
  */
 
 const cheerio = require('cheerio');
-// const _ = require('lodash');
 
 const usedHeaders = [];
 
 function createUniqueId(headingEl) {
-  const headingString = headingEl.innerHTML;
+  const headingString = headingEl.text();
   let anchor = headingString
     .trim()
     .toLowerCase()
@@ -24,31 +23,30 @@ function createUniqueId(headingEl) {
   return anchor;
 }
 
-function addUniqueIds(headingsArray) {
-  headingsArray.each(heading => {
-    const $ = cheerio.load(heading);
-    const parent = $.parent();
-    const isInWysiwyg = parent.hasClass('processed-content');
-    const isInAccordionButton = parent.hasClass('usa-accordion-button');
-
-    // skip heading if it already has an id and skip heading if it's in wysiwyg content
-    if (!$.id && !isInWysiwyg && !isInAccordionButton) {
-      const headingID = createUniqueId($);
-      $.attr('id', headingID);
-    }
-  });
-}
-
 function generateHeadingIds() {
   return (files, metalsmith, done) => {
     for (const fileName of Object.keys(files)) {
       const file = files[fileName];
+      let idAdded = false;
 
       if (fileName.endsWith('html')) {
-        const $ = cheerio.load(file.contents);
-        const headings = $('h2, h3');
-        if (headings.length > 0) {
-          addUniqueIds(headings);
+        const doc = cheerio.load(file.contents);
+        doc('h2, h3').each((i, el) => {
+          const heading = doc(el);
+          const parent = heading.parent();
+          const isInWysiwyg = parent.hasClass('processed-content');
+          const isInAccordionButton = parent.hasClass('usa-accordion-button');
+
+          // skip heading if it already has an id and skip heading if it's in wysiwyg content
+          if (!heading.attr('id') && !isInWysiwyg && !isInAccordionButton) {
+            const headingID = createUniqueId(heading);
+            heading.attr('id', headingID);
+            idAdded = true;
+          }
+        });
+
+        if (idAdded) {
+          file.contents = new Buffer(doc.html());
         }
       }
     }
