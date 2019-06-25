39526ed2f1be99d03ef274022824ff666cee2317
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2019-06-25 15:13:16 -0400

    Included aXe checks for TestCafe.

--- src/applications/facility-locator/tests/00-required.testcafe.e2e.spec.js
+++ src/applications/facility-locator/tests/00-required.testcafe.e2e.spec.js
@@ -1,14 +1,19 @@
 import { Selector } from 'testcafe';
+import { axeCheck, createReport } from 'axe-testcafe';
 
 import { baseUrl } from '../../../platform/testing/e2e/helpers';
 
 fixture('Facility Locator').page(`${baseUrl}/find-locations/`);
 
 test('Search facility and view details', async t => {
+  const { violations } = await axeCheck(t);
+
   await t
     .typeText('input[name="street-city-state-zip"]', 'Seattle, WA')
     .click('input[type="submit"]')
     .click('.facility-result a h5')
     .expect(Selector('.all-details').exists)
-    .ok();
+    .ok()
+    .expect(violations.length === 0)
+    .ok(createReport(violations));
 });
