953ec67b188572c3c4d775df88241b4e45f7da79
Dustin Hughes <hughes_dustin@bah.com>
2019-06-26 13:34:19 -0400

    Added Stem scholarship notification to landing page

---
+++ src/applications/gi/components/content/StemScholarshipNotification.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const StemScholarshipNotification = () => (
+  <div>
+    <span className="usa-label">New</span>
+    <div className="feature">
+      <h4>The Rogers STEM Scholarship</h4>
+      <p>
+        On August 1, 2019, VA is launching the Edith Nourse Rogers STEM
+        Scholarship program for students training in high demand STEM (Science,
+        Technology, Engineering, and Math) fields.
+      </p>
+      <p>
+        To learn more about this scholarship,{' '}
+        <a
+          href="https://benefits.va.gov/gibill/fgib/stem.asp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {' '}
+          visit the Rogers STEM Scholarship website.
+        </a>
+      </p>
+    </div>
+  </div>
+);
+
+export default StemScholarshipNotification;
