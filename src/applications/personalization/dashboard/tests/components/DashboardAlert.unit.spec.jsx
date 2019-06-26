f7511fbab1eeb45e86d36a043941354a6fe72079
Jeff Balboni <jeff@adhocteam.us>
2019-06-26 13:58:54 -0400

    Add unit test

--- src/applications/personalization/dashboard/tests/components/DashboardAlert.unit.spec.jsx
+++ src/applications/personalization/dashboard/tests/components/DashboardAlert.unit.spec.jsx
@@ -26,4 +26,23 @@ describe('<DashboardAlert />', () => {
     ).to.equal(true);
     wrapper.unmount();
   });
+
+  it('should have a valid header and id', () => {
+    const wrapper = shallow(
+      <DashboardAlert
+        id="alert"
+        content={content}
+        headline={headline}
+        statusHeadline={statusHeadline}
+        status={DASHBOARD_ALERT_TYPES.closed}
+      />,
+    );
+    expect(wrapper.find('#dashboard-alert-header-alert').exists()).to.equal(
+      true,
+    );
+    expect(wrapper.find('.dashboard-alert').prop('aria-labelledby')).to.equal(
+      'dashboard-alert-header-alert',
+    );
+    wrapper.unmount();
+  });
 });
