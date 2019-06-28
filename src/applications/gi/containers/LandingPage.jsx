944885586435b8d0080e6462720e0f403bc4f29b
Matt Roth <roth_matthew@bah.com>
2019-06-28 11:03:44 -0400

    wrap StemScholarshipNotification in nonProd flag

--- src/applications/gi/containers/LandingPage.jsx
+++ src/applications/gi/containers/LandingPage.jsx
@@ -13,6 +13,7 @@ import VideoSidebar from '../components/content/VideoSidebar';
 import KeywordSearch from '../components/search/KeywordSearch';
 import EligibilityForm from '../components/search/EligibilityForm';
 import StemScholarshipNotification from '../components/content/StemScholarshipNotification';
+import environment from '/platform/utilities/environment';
 
 export class LandingPage extends React.Component {
   constructor(props) {
@@ -92,7 +93,7 @@ export class LandingPage extends React.Component {
 
           <div className="small-12 usa-width-one-third medium-4 columns">
             <VideoSidebar />
-            <StemScholarshipNotification />
+            {!environment.isProduction() && <StemScholarshipNotification />}
           </div>
         </div>
       </span>
