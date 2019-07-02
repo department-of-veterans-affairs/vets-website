308aac7fe3f1db0dae676f6730684436a12d5bde
Ben Shyong <hello@benshyong.com>
2019-07-02 14:50:43 -0700

    unit tests and linting

--- src/applications/letters/components/UpdateFailureAlert.jsx
+++ src/applications/letters/components/UpdateFailureAlert.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
 
-const UpdateFailureAlert = ({ addressObject, recipientName }) => {
-  const addressLines = Object.keys(addressObject).map(key => (
-    <div className="letters-address" key={key}>
-      {addressObject[key]}
-    </div>
-  ));
-  return (
-    <div className="usa-alert usa-alert-warning">
-      <div className="usa-alert-body">
-        <h4 className="usa-alert-heading">
-          We aren't able to save your updated address
-        </h4>
-        <div className="usa-alert-text">
-          <div>
-            Your VA letters and documents are still valid with your old address.
-            <strong>
-              Please continue to download your VA letter or document.
-            </strong>{' '}
-            You can come back later and try again.
-          </div>
+const UpdateFailureAlert = () => (
+  <div className="usa-alert usa-alert-warning">
+    <div className="usa-alert-body">
+      <h4 className="usa-alert-heading">
+        We aren't able to save your updated address
+      </h4>
+      <div className="usa-alert-text">
+        <div>
+          Your VA letters and documents are still valid with your old address.
+          <strong>
+            Please continue to download your VA letter or document.
+          </strong>{' '}
+          You can come back later and try again.
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default UpdateFailureAlert;
