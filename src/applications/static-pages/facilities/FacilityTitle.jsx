7a0a81b4fa8e6c2c8ebcd572533050237f999802
lightning <aurora.hampton@agile6.com>
2019-07-02 22:22:12 -0700

    VAOGV-3223 && VAGOV-4079: fixes to styling in facility templates

--- src/applications/static-pages/facilities/FacilityTitle.jsx
+++ src/applications/static-pages/facilities/FacilityTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class FacilityTitle extends React.Component {
   render() {
     return (
-      <h3 className="vads-u-margin-bottom--1 vads-u-margin-top--0 vads-u-font-size--md">
+      <h3 className="vads-u-margin-bottom--1 vads-u-margin-top--0 vads-u-font-size--md medium-screen:vads-u-font-size--lg">
         <a href={this.props.regionPath}>
           {this.props.facility.attributes.name}
         </a>
