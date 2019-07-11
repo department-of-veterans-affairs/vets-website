0f66e3bc7fe60ea41754ef4cfc114c194e2aeba4
Joe <elder_joseph@bah.com>
2019-07-11 16:08:03 -0400

    19177: CT 111: Show STEM Filter and Indicator in Comparison Tool: Add StemScholarshipNotification component back and remove the production flag used to its rendering.

---
+++ src/applications/gi/components/content/StemScholarshipNotification.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const StemScholarshipNotification = () => (
+  <div className="stem-notification">
+    <span className="usa-label">New</span>
+    <div className="feature">
+      <h4>The Rogers STEM Scholarship</h4>
+      <p>
+        On August 1, 2019, VA is launching the Edith Nourse Rogers STEM
+        Scholarship for students enrolled in a high-demand STEM (Science,
+        Technology, Engineering, and Math) program.
+      </p>
+      <p>
+        To learn more about this scholarship,{' '}
+        <a
+          href="https://benefits.va.gov/gibill/fgib/stem.asp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {' '}
+          visit the Rogers STEM Scholarship website
+        </a>
+        .
+      </p>
+    </div>
+  </div>
+);
+
+export default StemScholarshipNotification;
