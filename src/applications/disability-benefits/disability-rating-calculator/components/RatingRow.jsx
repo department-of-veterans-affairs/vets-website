f5b3168732a8c5fd01bb9281149e91c48d439771
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-07-11 15:31:16 -0400

    Change to use React class

--- src/applications/disability-benefits/disability-rating-calculator/components/RatingRow.jsx
+++ src/applications/disability-benefits/disability-rating-calculator/components/RatingRow.jsx
@@ -1,78 +1,75 @@
 import React from 'react';
 
-const RatingRow = ({
-  disability,
-  indx,
-  updateDisability,
-  removeDisability,
-  inputRef,
-  disabled,
-}) => {
-  const onRatingChange = e => {
+class RatingRow extends React.Component {
+  onRatingChange = e => {
     const val = e.target.value;
     const re = /^[0-9\b]+$/;
 
     if (re.test(val) || val === '') {
-      updateDisability(indx, {
-        ...disability,
+      this.props.updateDisability(this.props.indx, {
+        ...this.props.disability,
         rating: val.length ? Number(val) : val,
       });
     }
   };
 
-  const onDescriptionChange = e => {
-    updateDisability(indx, { ...disability, description: e.target.value });
+  onDescriptionChange = e => {
+    this.props.updateDisability(this.props.indx, {
+      ...this.props.disability,
+      description: e.target.value,
+    });
   };
 
-  const rowId = `disability-row-${indx + 1}`;
+  render() {
+    const rowId = `disability-row-${this.props.indx + 1}`;
 
-  return (
-    <div className="rating vads-l-row">
-      <span id={rowId} className="sr-only">
-        row {indx + 1}
-      </span>
-      <div className="vads-l-col--4 small-screen:vads-l-col--3 vads-u-padding-right--2">
-        <input
-          type="text"
-          min="0"
-          onChange={onRatingChange}
-          className="ratingInput"
-          maxLength="2"
-          value={disability.rating}
-          pattern="\d+"
-          name="rating"
-          ref={inputRef}
-          aria-labelledby={`ratingLabel ${rowId}`}
-        />
-      </div>
-      <div className="vads-l-col--6 small-screen:vads-l-col--6">
-        <input
-          className="descriptionInput"
-          name="description"
-          onChange={onDescriptionChange}
-          value={disability.description}
-          aria-labelledby={`descriptionLabel ${rowId}`}
-        />
-      </div>
-      <div className="vads-l-col--2 small-screen:vads-l-col--3">
-        <div>
-          <button
-            type="button"
-            aria-label={`Delete disability rating row ${indx + 1}`}
-            onClick={removeDisability(indx)}
-            className="va-button-link usa-button vads-u-margin--1p5"
-            disabled={disabled}
-          >
-            <i className="fas fa-trash-alt vads-u-padding-right--0p5" />
-            <span className="vads-u-display--none medium-screen:vads-u-display--inline">
-              Delete
-            </span>
-          </button>
+    return (
+      <div className="rating vads-l-row">
+        <span id={rowId} className="sr-only">
+          row {this.props.indx + 1}
+        </span>
+        <div className="vads-l-col--4 small-screen:vads-l-col--3 vads-u-padding-right--2">
+          <input
+            type="number"
+            className="ratingInput"
+            name="rating"
+            min="10"
+            max="99"
+            ref={this.props.inputRef}
+            onChange={this.onRatingChange}
+            value={this.props.disability.rating}
+            aria-labelledby={`ratingLabel ${rowId}`}
+          />
+        </div>
+        <div className="vads-l-col--6 small-screen:vads-l-col--6">
+          <input
+            className="descriptionInput"
+            name="description"
+            onChange={this.onDescriptionChange}
+            value={this.props.disability.description}
+            aria-labelledby={`descriptionLabel ${rowId}`}
+          />
+        </div>
+        <div className="vads-l-col--2 small-screen:vads-l-col--3">
+          <div>
+            <button
+              type="button"
+              aria-label={`Delete disability rating row ${this.props.indx + 1}`}
+              onClick={this.props.removeDisability(this.props.indx)}
+              className="va-button-link usa-button vads-u-margin--1p5"
+              disabled={this.props.disabled}
+            >
+              <i className="fas fa-trash-alt vads-u-padding-right--0p5" />
+              <span className="vads-u-display--none medium-screen:vads-u-display--inline">
+                Delete
+              </span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default React.forwardRef((props, ref) => (
   <RatingRow inputRef={ref} {...props} />
