e49a2ae2cdb94c337c4eec1c0366e17f5601fc20
Tze-chiu Lei <tclei2009@gmail.com>
2019-07-15 12:59:55 -0700

    16156 benefit cards a11y updates (#10392)
    * 16156 - Add optional ariaLabelledBy prop to Find-Benefits checkboxes.
    
    * 16156 - Change My-VA Find-Benefits checkbox-component import.
    
    * 16156 - Remove label prop from My-VA Find-Benefits checkboxes.
    
    * 16156 - Bump formation & formation-react dependency versions.
    
    * 16156 - Include yarn.lock for formation-react upgrade.

--- src/applications/personalization/preferences/components/PreferenceOption.jsx
+++ src/applications/personalization/preferences/components/PreferenceOption.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 
-import Checkbox from 'applications/gi/components/Checkbox.jsx';
+import ErrorableCheckbox from '@department-of-veterans-affairs/formation-react/ErrorableCheckbox';
 
 export default ({ item, onChange, checked }) => {
+  const itemId = item.shortTitle
+    ? item.shortTitle.replace(/\s/g, '')
+    : item.title.replace(/\s/g, '');
   const itemContent = (
     <div
       className="preference-item"
       onClick={() => onChange(item.code, !checked)}
     >
       <div className="title-container">
-        <h5 className="title-item">{item.shortTitle || item.title}</h5>
-        <Checkbox
+        <h5 id={`itemTitle${itemId}`} className="title-item">
+          {item.shortTitle || item.title}
+        </h5>
+        <ErrorableCheckbox
           name={item.code}
           checked={checked}
-          label={
-            <span className="usa-sr-only">{item.shortTitle || item.title}</span>
-          }
-          onChange={() => onChange(item.code, !checked)}
+          ariaLabelledBy={`itemTitle${itemId}`}
+          onValueChange={() => onChange(item.code, !checked)}
         />
       </div>
       <p>{item.description}</p>
