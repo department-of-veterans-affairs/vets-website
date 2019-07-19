3d42e30a70cfc5c6840abf0a280b03a30606ace7
Joe <elder_joseph@bah.com>
2019-07-19 10:43:26 -0400

    19177: CT 111: Show STEM Filter and Indicator in Comparison Tool: Update import of environment

--- src/applications/gi/components/profile/AdditionalInformation.jsx
+++ src/applications/gi/components/profile/AdditionalInformation.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import environment from '../../../../platform/utilities/environment';
+import environment from 'platform/utilities/environment';
 
 export class AdditionalInformation extends React.Component {
   updateFiscalYear() {
