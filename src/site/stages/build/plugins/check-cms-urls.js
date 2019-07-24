c8a6bc5bfe0e82e87ec26d3e7aa984a364929e96
Christopher Valarida <chris.valarida@adhocteam.us>
2019-07-24 16:41:54 -0700

    checkForCMSUrls throws error in staging

--- src/site/stages/build/plugins/check-cms-urls.js
+++ src/site/stages/build/plugins/check-cms-urls.js
@@ -26,7 +26,11 @@ function checkForCMSUrls(BUILD_OPTIONS) {
       );
       console.log(filesWithBadUrls.join('\n'));
 
-      if (BUILD_OPTIONS.buildtype === ENVIRONMENTS.VAGOVPROD) {
+      if (
+        [ENVIRONMENTS.VAGOVPROD, ENVIRONMENTS.VAGOVSTAGING].includes(
+          BUILD_OPTIONS.buildtype,
+        )
+      ) {
         throw new Error('Pages found that reference internal CMS urls');
       }
     }
