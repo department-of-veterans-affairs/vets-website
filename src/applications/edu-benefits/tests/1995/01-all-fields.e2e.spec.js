050992aa69a08db6ab6f23602009dee8b822acdf
Devin McCurdy <mccurdy_devin@bah.com>
2019-07-26 16:43:06 -0400

    1995 Prefill and Submit Transformer Updates (#10473)
    * 1995 prefill transformer

--- src/applications/edu-benefits/tests/1995/01-all-fields.e2e.spec.js
+++ src/applications/edu-benefits/tests/1995/01-all-fields.e2e.spec.js
@@ -2,7 +2,7 @@ const E2eHelpers = require('../../../../platform/testing/e2e/helpers');
 const Timeouts = require('../../../../platform/testing/e2e/timeouts.js');
 const EduHelpers = require('../1990/edu-helpers');
 const Edu1995Helpers = require('./edu-1995-helpers');
-const testData = require('./schema/maximal-test.json');
+const testData = require('./schema/e2e-maximal-test.json');
 const FormsTestHelpers = require('../../../../platform/testing/e2e/form-helpers');
 
 module.exports = E2eHelpers.createE2eTest(client => {
