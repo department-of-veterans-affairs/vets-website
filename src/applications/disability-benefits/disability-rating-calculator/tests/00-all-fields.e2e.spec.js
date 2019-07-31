c3edbeb986ca24f24048328f018c23f26bcfe25e
Tze Lei <tze@adhocteam.us>
2019-07-30 17:07:41 -0700

    19385 - Refactor Disability Rating Calcultor e2e-test row-deletion.

--- src/applications/disability-benefits/disability-rating-calculator/tests/00-all-fields.e2e.spec.js
+++ src/applications/disability-benefits/disability-rating-calculator/tests/00-all-fields.e2e.spec.js
@@ -12,24 +12,25 @@ module.exports = E2eHelpers.createE2eTest(client => {
     .openUrl(`${E2eHelpers.baseUrl}/disability/about-disability-ratings-beta/`)
     .waitForElementVisible('body', Timeouts.normal)
     .assert.title('How VA Assigns Disability Ratings | Veterans Affairs');
-  // console.log('Now on /about-disability-ratings-beta');
 
   // Test row deletion.
-  // TODO: Cover edge-case for deleting AFTER calculating combined rating.
-  // console.log('TESTING RATING-ROW DELECTION..................................');
+  // TODO: Cover edge-case* for deleting AFTER calculating combined rating.
+  // *Design Team to decide what should happen for this edge-case.
   client
     .waitForElementPresent('.btn-add', Timeouts.slow)
     .expect.element('.btn-add').to.be.visible;
-  DrcE2eHelpers.fillRatings(client, inputs[0]);
-  client.expect
-    .element('.row-1 .btn-delete')
-    .to.be.active.before(100)
-    .click('.row-2 .btn-delete')
-    .expect.elements('.ratingInput')
-    .count.to.equal(2);
+  DrcE2eHelpers.fillRatings(client, { ratings: [30, 20, 10] });
+  client
+    .waitForElementVisible('.row-3 .btn-delete', Timeouts.normal)
+    .click('.row-2 .btn-delete');
+  client.elements('css selector', '.ratingInput', result => {
+    client.assert.equal(result.value.length, 2);
+  });
+  client
+    .waitForElementVisible('.btn-clearall', Timeouts.normal)
+    .click('.btn-clearall');
 
   // Test combined-rating calculations / input error.
-  // console.log('TESTING COMBINED-RATING CALCULATIONS / INPUT ERRORS...........');
   for (const input of inputs) {
     client
       .waitForElementVisible('.rating.row-2', Timeouts.normal)
