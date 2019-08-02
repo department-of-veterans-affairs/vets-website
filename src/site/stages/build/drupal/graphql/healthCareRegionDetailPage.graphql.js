1fcb96f561730853bc50eb66af2a6e0b5d9134bd
lightning <aurora.hampton@agile6.com>
2019-08-01 17:32:27 -0700

    VAGOV-5261: found the missing alerts

--- src/site/stages/build/drupal/graphql/healthCareRegionDetailPage.graphql.js
+++ src/site/stages/build/drupal/graphql/healthCareRegionDetailPage.graphql.js
@@ -17,6 +17,7 @@ const QA = '... qa';
 const LIST_OF_LINK_TEASERS = '... listOfLinkTeasers';
 const REACT_WIDGET = '... reactWidget';
 const NUMBER_CALLOUT = '... numberCallout';
+const ALERT_PARAGRAPH = '... alertParagraph';
 const entityElementsFromPages = require('./entityElementsForPages.graphql');
 
 // Get current feature flags
@@ -68,6 +69,7 @@ module.exports = `
         ${LIST_OF_LINK_TEASERS}
         ${REACT_WIDGET}
         ${NUMBER_CALLOUT}
+        ${ALERT_PARAGRAPH}
       }
     }
     ${FIELD_RELATED_LINKS}
