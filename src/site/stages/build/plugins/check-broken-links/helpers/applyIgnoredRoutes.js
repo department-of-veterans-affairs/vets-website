f3bfd2e74443d198892af7450e9da57798da9f43
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-08-02 13:20:05 -0400

    Add unit test

--- src/site/stages/build/plugins/check-broken-links/helpers/applyIgnoredRoutes.js
+++ src/site/stages/build/plugins/check-broken-links/helpers/applyIgnoredRoutes.js
@@ -1,4 +1,17 @@
-function applyIgnoredRoutes(brokenPages, files) {
+function _getReactLandingPages(files) {
+  const reactLandingPages = Object.keys(files)
+    .map(fileName => files[fileName])
+    .filter(file => !!file.entryname)
+    .map(file => `/${file.path}`);
+
+  return reactLandingPages;
+}
+
+function applyIgnoredRoutes(
+  brokenPages,
+  files,
+  getReactLandingPages = _getReactLandingPages,
+) {
   // We ignore React landing pages, because React apps have
   // dynamic, client-side routing and only the root page
   // will have an HTML page.
@@ -7,26 +20,29 @@ function applyIgnoredRoutes(brokenPages, files) {
   // pages are under the same domain, they are not part of our
   // source code, so we can't validate them.
 
-  const reactLandingPages = Object.keys(files)
-    .map(fileName => files[fileName])
-    .filter(file => !!file.entryname)
-    .map(file => file.path);
+  const reactLandingPages = getReactLandingPages(files);
 
-  const filteredBrokenPages = brokenPages.filter(brokenPage => {
-    const filteredBrokenPage = { ...brokenPage };
+  const filteredBrokenPages = brokenPages
+    .map(brokenPage => {
+      const filteredLinkErrors = brokenPage.linkErrors
+        .filter(linkError => !linkError.target.endsWith('.asp'))
+        .filter(
+          linkError =>
+            !reactLandingPages.some(reactPath =>
+              linkError.target.startsWith(reactPath),
+            ),
+        );
 
-    filteredBrokenPage.linkErrors = brokenPage.linkErrors
-      .filter(linkError => !linkError.target.endsWith('.asp'))
-      .filter(linkError =>
-        reactLandingPages.some(reactPath =>
-          linkError.target.startsWith(reactPath),
-        ),
-      );
+      return {
+        ...brokenPage,
+        linkErrors: filteredLinkErrors,
+      };
+    })
 
-    return filteredBrokenPage.linkErrors.length > 0;
-  });
+    .filter(brokenPage => brokenPage.linkErrors.length > 0);
 
   return filteredBrokenPages;
 }
 
 module.exports = applyIgnoredRoutes;
+module.exports.getReactLandingPages = _getReactLandingPages;
