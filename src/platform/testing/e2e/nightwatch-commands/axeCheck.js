259d6c11e67145b5ef2de9e33f21b424aad23862
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2019-08-05 07:38:03 -0400

    Revert unnecessary change?

--- src/platform/testing/e2e/nightwatch-commands/axeCheck.js
+++ src/platform/testing/e2e/nightwatch-commands/axeCheck.js
@@ -1,4 +1,4 @@
-require('axe-core').axeCore;
+import axeCore from 'axe-core'; // eslint-disable-line no-unused-vars
 
 /**
  * Runs aXe checker on the given context
@@ -6,7 +6,7 @@ require('axe-core').axeCore;
  * @param  {object} config Additional axe configuration options
  * @api commands
  */
-exports.command = function axeCheck(context, config, _callback) {
+export function command(context, config, _callback) {
   // Find the source of the axe module
 
   // TODO: since this is executed in the context of the browser,
@@ -78,4 +78,4 @@ ${nodeInfo}`;
       });
     },
   );
-};
+}
