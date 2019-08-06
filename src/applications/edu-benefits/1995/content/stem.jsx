5fcc2cb587abc7ba15edbbea7c5fd0376c88a78e
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2019-08-06 13:15:12 -0400

    changed inline styling to VA design standards

--- src/applications/edu-benefits/1995/content/stem.jsx
+++ src/applications/edu-benefits/1995/content/stem.jsx
@@ -4,17 +4,17 @@ import AdditionalInfo from '@department-of-veterans-affairs/formation-react/Addi
 export const rogersStemScholarshipInfo = (
   <AdditionalInfo triggerText="What is the Rogers STEM Scholarship?">
     <div>
-      <div style={{ marginBottom: '15px' }}>
+      <div className="vads-u-margin-bottom--2">
         The Edith Nourse Rogers STEM Scholarship provides up to 9 months of
         additional Post-9/11 GI Bill benefits, to a maximum of $30,000.
       </div>
-      <div style={{ marginBottom: '15px' }}>
+      <div className="vads-u-margin-bottom--2">
         Veterans and Fry Scholars may qualify for this scholarship if they're
         enrolled in an undergraduate program for Science, Technology,
         Engineering, or Math (STEM), or if they've earned a STEM degree and are
         getting a teaching certification.
       </div>
-      <div style={{ marginBottom: '15px' }}>
+      <div className="vads-u-margin-bottom--2">
         To learn more about the STEM Scholarship,{' '}
         <a
           href="https://benefits.va.gov/gibill/fgib/stem.asp"
