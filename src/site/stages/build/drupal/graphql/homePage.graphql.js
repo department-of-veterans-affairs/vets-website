d8318c2bd63dbd50dfc086c22c862411157335c2
Jonathan Bourland <jonathanbourland@hotmail.com>
2019-08-13 15:31:11 -0400

    Complete GQL query; add all pieces into home.js processing; update template with new vars

--- src/site/stages/build/drupal/graphql/homePage.graphql.js
+++ src/site/stages/build/drupal/graphql/homePage.graphql.js
@@ -2,19 +2,77 @@
  * Home page
  */
 
+const menu = 'homepage-top-tasks-blocks';
+const hubListQueue = 'home_page_hub_list';
+const promoBlocksQueue = 'home_page_promos';
+
 module.exports = `
- fragment entitySubqueueById(id: "home_page_hub_list") {
-  ... on EntitySubqueueHomePageHubList {
-    itemsOfEntitySubqueueHomePageHubList {
-      targetId
-      entity {
-        ... on NodeLandingPage {
-          entityId
-          entityLabel
-          fieldDescription
+  homePageMenuQuery:menuByName(name: "${menu}") {
+    name
+    links {
+      label
+      url {
+        path
+      }
+      links {
+        label
+        url {
+          path
         }
       }
     }
   }
-}
+  homePageHubListQuery: entitySubqueueById(id: "${hubListQueue}") {
+    ... on EntitySubqueueHomePageHubList {
+      itemsOfEntitySubqueueHomePageHubList {
+        entity {
+          ... on NodeLandingPage {
+            entityId
+            entityLabel
+            fieldTeaserText
+            fieldTitleIcon
+          }
+        }
+      }
+    }
+  }
+  homePagePromoBlockQuery: entitySubqueueById(id: "${promoBlocksQueue}") {
+    ... on EntitySubqueueHomePagePromos {
+      itemsOfEntitySubqueueHomePagePromos {
+         entity {
+          ... on BlockContentPromo {
+            entityId
+            entityLabel
+            fieldImage {
+              targetId
+              entity {
+                ...on MediaImage {
+                  image {
+                    url
+                    alt
+                  }
+                }
+
+              }
+            }
+            fieldPromoLink {
+              targetId
+              ...on FieldBlockContentPromoFieldPromoLink {
+                entity {
+                  ... on ParagraphLinkTeaser {
+                    fieldLink {
+                      uri
+                      title
+                      options
+                    }
+                    fieldLinkSummary
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 `;
--- src/site/stages/build/drupal/home.js
+++ src/site/stages/build/drupal/home.js
@@ -1,18 +1,26 @@
 /* eslint-disable no-param-reassign, no-continue */
-const {
-  createEntityUrlObj,
-  createFileObj,
-  paginatePages,
-  updateEntityUrlObj,
-  generateBreadCrumbs,
-} = require('./page');
+const { createEntityUrlObj, createFileObj } = require('./page');
 
+function addHomeContent(contentData, files) {
+  const menuLength = 4;
+  const hubListLength = 11;
+  const promoBlockLength = 3;
+  const homeEntityObj = createEntityUrlObj('/');
+  const {
+    data: { homePageMenuQuery, homePageHubListQuery, homePagePromoBlockQuery },
+  } = contentData;
 
-function addHomeContent(contentData, files, drupalPagePath) {
-  files[`${drupalPagePath}/index.html`] = createFileObj(
-    createEntityUrlObj({}, 'Home'),
-    'home.drupal.liquid',
+  homeEntityObj.cards = homePageMenuQuery.links.slice(0, menuLength);
+  homeEntityObj.hubs = homePageHubListQuery.itemsOfEntitySubqueueHomePageHubList.slice(
+    0,
+    hubListLength,
   );
+  homeEntityObj.promos = homePagePromoBlockQuery.itemsOfEntitySubqueueHomePagePromos.slice(
+    0,
+    promoBlockLength,
+  );
+
+  files[`./index.html`] = createFileObj(homeEntityObj, 'home.drupal.liquid');
 }
 
 module.exports = { addHomeContent };
