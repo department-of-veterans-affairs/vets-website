dd9a9706ff7a09051bbce465960035ef7bb3ef09
Matt Roth <roth_matthew@bah.com>
2019-08-15 13:34:01 -0400

    logo path fix

--- src/applications/gi/utils/render.jsx
+++ src/applications/gi/utils/render.jsx
@@ -65,7 +65,7 @@ export const renderLearnMoreLabel = ({ text, modal, showModal, component }) =>
 export const renderVetTecLogo = classNames => (
   <img
     className={classNames}
-    src={require('site/assets/img/logo/vet-tec-logo.png')}
+    src={'/img/logo/vet-tec-logo.png'}
     alt="Vet Tec Logo"
   />
 );
