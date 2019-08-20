bd483203b21df23527d9bf0e888eedc9df668b64
Rian Fowler <rian.fowler@adhocteam.us>
2019-08-20 16:11:02 -0600

    remove commented code

--- src/platform/utilities/feature-toggles/index.jsx
+++ src/platform/utilities/feature-toggles/index.jsx
@@ -34,7 +34,6 @@ async function connectFeatureToggle(
 
   if (!environments.isProduction()) {
     const newToggleValues = await fetchToggleValues();
-    // const newToggleValues = {};
 
     dispatch({
       type: 'FETCH_TOGGLE_VALUES_SUCCEEDED',
