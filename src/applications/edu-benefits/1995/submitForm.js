272fe16378cbf40b2f71f15fb5cae03b8a75119e
Nate Johnson <Johnson_Nathaniel@bah.com>
2019-08-22 15:04:09 -0400

    updated to add requested fields to event datalayer

--- src/applications/edu-benefits/1995/submitForm.js
+++ src/applications/edu-benefits/1995/submitForm.js
@@ -1,15 +1,28 @@
-import { submitToUrl } from 'platform/forms-system/src/js/actions';
-import { transformForSubmit } from 'platform/forms-system/src/js/helpers';
+import {
+  submitToUrl
+} from 'platform/forms-system/src/js/actions';
+import {
+  transformForSubmit
+} from 'platform/forms-system/src/js/helpers';
 
 const submitForm = (form, formConfig) => {
-  const body = formConfig.transformForSubmit
-    ? formConfig.transformForSubmit(formConfig, form)
-    : transformForSubmit(formConfig, form);
+  const body = formConfig.transformForSubmit ?
+    formConfig.transformForSubmit(formConfig, form) :
+    transformForSubmit(formConfig, form);
 
   const eventData = {
-    'edu-stem-applicant': form.data.isEdithNourseRogersScholarship
-      ? 'Yes'
-      : 'No',
+    'benefitsUsedRecently': form.data.benefit,
+    'edu-stem-applicant': form.data.isEdithNourseRogersScholarship ?
+      'Yes' : 'No',
+    'edu-undergradStem': form.data.isEnrolledStem ?
+      'Yes' : 'No',
+    'edu-pursueTeaching': form.data.isPursuingTeachingCert ?
+      'Yes' : 'No',
+    'activeDuty': form.data.isActiveDuty ?
+      'Yes' : 'No',
+    'calledActiveDuty': form.data.isActiveDuty ?
+      'Yes' : 'No',
+    'preferredContactMethod': form.data.preferredContactMethod,
   };
 
   return submitToUrl(
