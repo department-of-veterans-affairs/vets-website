832c1dfc2e756679759164c512e1bbcffa112cf0
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-08-28 14:22:09 -0400

    Add CMS feature flag

--- src/site/utilities/featureFlags.js
+++ src/site/utilities/featureFlags.js
@@ -113,6 +113,7 @@ const flagsByBuildtype = {
     featureFlags.FEATURE_REGION_DETAIL_PAGE_FEATURED_CONTENT,
     featureFlags.FEATURE_REGION_PAGE_LINKS,
     featureFlags.FEATURE_HOME_PAGE,
+    featureFlags.FEATURE_DOWNLOADABLE_FILE,
   ],
 };
 
