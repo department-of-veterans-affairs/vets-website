e2e29026ffff0fe3dec59c65922ff4f14fabf502
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-08-30 14:45:12 -0400

    Implement Sentry

--- src/applications/static-pages/renderHomepageBanner.js
+++ src/applications/static-pages/renderHomepageBanner.js
@@ -1,38 +1,51 @@
-const yaml = require('js-yaml');
+import yaml from 'js-yaml';
+import * as Sentry from '@sentry/browser';
 
 const HOMEPAGE_BANNER_YML_LOCATION =
   'https://raw.githubusercontent.com/department-of-veterans-affairs/vagov-content/master/fragments/home/banner.yml';
 
-async function renderHomepageBanner() {
+async function loadConfigFromGitHub() {
   const response = await fetch(HOMEPAGE_BANNER_YML_LOCATION, {
     mode: 'cors',
   });
 
   const homepageBannerYml = await response.text();
-  const homepageBanner = yaml.safeLoad(homepageBannerYml);
+  return yaml.safeLoad(homepageBannerYml);
+}
 
-  if (!homepageBanner.visible) {
+function renderToDocument(banner) {
+  if (!banner.visible) {
     return;
   }
 
   const root = document.getElementById('homepage-banner');
 
   root.innerHTML = `
-    <div class="usa-alert-full-width usa-alert-full-width-${
-      homepageBanner.type
-    }">
+    <div class="usa-alert-full-width usa-alert-full-width-${banner.type}">
       <div aria-live="polite" role="alert" class="usa-alert usa-alert-${
-        homepageBanner.type
+        banner.type
       }">
         <div class="usa-alert-body">
-          <h3 class="usa-alert-heading">${homepageBanner.title}</h3>
+          <h3 class="usa-alert-heading">${banner.title}</h3>
           <div class="usa-alert-text">
-            ${homepageBanner.content}
+            ${banner.content}
           </div>
         </div>
       </div>
     </div>
   `;
+}
+
+async function renderHomepageBanner() {
+  try {
+    const banner = await loadConfigFromGitHub();
+    renderToDocument(banner);
+  } catch (error) {
+    Sentry.withScope(scope => {
+      scope.setExtra('error', error);
+      Sentry.captureMessage(`Homepage Banner Error: ${error.message}`);
+    });
+  }
 }
 
 export default renderHomepageBanner;
