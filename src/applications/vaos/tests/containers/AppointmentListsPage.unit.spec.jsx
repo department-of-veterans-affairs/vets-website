53f505f5b0b8dc00f946940621cbdad71ed82717
Narin Ratana <n.ratana@gmail.com>
2019-09-07 16:41:15 -0700

    Update AppointmentListsPage test to check for appointment type headers

--- src/applications/vaos/tests/containers/AppointmentListsPage.unit.spec.jsx
+++ src/applications/vaos/tests/containers/AppointmentListsPage.unit.spec.jsx
@@ -9,20 +9,8 @@ describe('VAOS <AppointmentListsPage>', () => {
   it('should render a loading indicator', () => {
     const defaultProps = {
       appointments: {
-        pending: [
-          {
-            id: 1,
-          },
-          { id: 2 },
-        ],
         pendingLoading: true,
-        confirmed: [
-          {
-            id: 1,
-          },
-        ],
         confirmedLoading: true,
-        past: [],
         pastLoading: true,
       },
     };
@@ -42,7 +30,7 @@ describe('VAOS <AppointmentListsPage>', () => {
     tree.unmount();
   });
 
-  it('should render badge counts for each appointment type', () => {
+  it('should render 3 appointments types and badge counts for each', () => {
     const defaultProps = {
       appointments: {
         pending: [
@@ -74,10 +62,15 @@ describe('VAOS <AppointmentListsPage>', () => {
       />,
     );
 
+    const headers = tree.find('h2.vads-u-font-size--lg');
+    expect(headers.length).to.equal(3);
+    expect(headers.at(0).text()).to.have.string('Confirmed appointments');
+    expect(headers.at(1).text()).to.have.string('Pending appointments');
+    expect(headers.at(2).text()).to.have.string('Appointment history');
+
     const badges = tree.find('span.vaos-appt-list__badge');
     expect(tree.find('LoadingIndicator').exists()).to.be.false;
     expect(badges.length).to.equal(2);
-
     expect(badges.at(0).text()).to.have.string('1');
     expect(badges.at(1).text()).to.have.string('2');
 
