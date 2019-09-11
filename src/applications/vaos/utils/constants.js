689518162ffcd50e1a3192d33bed93bc15147e26
Jeff Balboni <jeff@adhocteam.us>
2019-09-11 11:59:04 -0400

    Add pending appointment detail page (#10704)

--- src/applications/vaos/utils/constants.js
+++ src/applications/vaos/utils/constants.js
@@ -4,3 +4,16 @@ export const FETCH_STATUS = {
   succeeded: 'succeeded',
   failed: 'failed',
 };
+
+export const TIME_TEXT = {
+  AM: 'in the morning',
+  PM: 'in the afternoon',
+  'No Time Selected': '',
+};
+
+export const PURPOSE_TEXT = {
+  'routine-follow-up': 'Routine/Follow-Up',
+  'new-issue': 'New Issue',
+  'medication-concern': 'Medication Concern',
+  other: 'Other',
+};
