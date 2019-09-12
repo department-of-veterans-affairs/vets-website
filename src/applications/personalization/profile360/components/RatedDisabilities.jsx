0a09cf5caea23c99dbf308c1ac25d91c69c93c46
Micah Chiang <micahkchiang@gmail.com>
2019-09-12 16:58:43 -0700

    table view for disability ratings.

--- src/applications/personalization/profile360/components/RatedDisabilities.jsx
+++ src/applications/personalization/profile360/components/RatedDisabilities.jsx
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 import featureFlags from '../featureFlags';
 
+import SortableTable from '@department-of-veterans-affairs/formation-react/SortableTable';
+import moment from 'moment';
+
 class RatedDisabilities extends React.Component {
   static propTypes = {
     fetchRatedDisabilities: PropTypes.func.isRequired,
@@ -24,10 +27,37 @@ class RatedDisabilities extends React.Component {
       return <h1>Loading!</h1>;
     }
 
+    if (this.props.ratedDisabilities?.error) {
+      return (
+        <h1>
+          We're having trouble accessing your rated disabilities at this time.
+        </h1>
+      );
+    }
+    // Format date strings
+    const formattedDisabilities = this.props.ratedDisabilities.ratedDisabilities.map(
+      d => {
+        const effectiveDate = {
+          effectiveDate: moment(d.effectiveDate).format('DD/MM/YYYY'),
+        };
+        const disability = Object.assign({}, d, effectiveDate);
+        return disability;
+      },
+    );
     return (
       <>
         <h2 className="va-profile-heading">Rated disabilities</h2>
-        <code>{JSON.stringify(this.props.ratedDisabilities)}</code>;
+        <SortableTable
+          className="va-table"
+          currentSort={{ value: 'String', order: 'ASC' }}
+          fields={[
+            { label: 'Disability', value: 'name' },
+            { label: 'Rating', value: 'ratingPercentage' },
+            { label: 'Decision', value: 'decisionText' },
+            { label: 'Effective Date', value: 'effectiveDate' },
+          ]}
+          data={[...formattedDisabilities]}
+        />
       </>
     );
   }
