c535256b9092893982c1b4bca0317ee215357bfa
Ethan Teague <metrowebz@gmail.com>
2019-09-13 15:31:09 -0400

    VAGOV-5835: Removing legacy feature flags.

--- src/site/stages/build/drupal/graphql/healthCareRegionPage.graphql.js
+++ src/site/stages/build/drupal/graphql/healthCareRegionPage.graphql.js
@@ -5,13 +5,9 @@
 const entityElementsFromPages = require('./entityElementsForPages.graphql');
 const healthCareLocalFacilities = require('./facilities-fragments/healthCareLocalFacility.node.graphql');
 const healthCareRegionHealthServices = require('./facilities-fragments/healthCareRegionHealthServices.node.graphql');
-const healthCareRegionFeaturedHealthServices = require('./facilities-fragments/healthCareRegionFeaturedHealthServces.node.graphql');
 const healthCareRegionNewsStories = require('./facilities-fragments/healthCareRegionNewsStories.node.graphql');
 const healthCareRegionEvents = require('./facilities-fragments/healthCareRegionEvents.node.graphql');
 const healthCareStaffBios = require('./facilities-fragments/healthCareRegionStaffBios.node.graphql');
-
-// Get current feature flags
-const { cmsFeatureFlags } = global;
 
 module.exports = `
   fragment healthCareRegionPage on NodeHealthCareRegionPage {
@@ -62,37 +58,23 @@ module.exports = `
       }
       title
     }
-    ${
-      cmsFeatureFlags.FEATURE_REGION_PAGE_LINKS
-        ? 'fieldLinks'
-        : 'fieldEmailSubscription'
-    } {
+    fieldLinks {
       url {
         path
       }
       title
     }
-    ${
-      cmsFeatureFlags.FEATURE_REGION_PAGE_LINKS
-        ? `
-        fieldOperatingStatus {
-          url {
-            path
-          }
-          title
-        }
-        `
-        : ''
+    fieldOperatingStatus {
+      url {
+        path
+      }
+      title
     }
     reverseFieldRegionPageNode(limit: 100000, filter:{conditions:[{field: "type", value: "health_care_local_facility"}]}) {
       entities {
         ... on NodeHealthCareLocalFacility {
           title
-          ${
-            cmsFeatureFlags.FEATURE_FIELD_OPERATING_STATUS_FACILITY
-              ? 'fieldOperatingStatusFacility'
-              : ''
-          }
+          fieldOperatingStatusFacility
         }
       }
     }
@@ -120,11 +102,7 @@ module.exports = `
       processed
     }
     ${healthCareLocalFacilities}
-    ${
-      cmsFeatureFlags.FEATURE_FIELD_OTHER_VA_LOCATIONS
-        ? 'fieldOtherVaLocations'
-        : ''
-    }
+    fieldOtherVaLocations
     fieldIntroTextNewsStories {
       processed
     }
@@ -135,11 +113,6 @@ module.exports = `
     ${healthCareRegionEvents}
     fieldClinicalHealthCareServi {
       processed
-    }
-    ${
-      cmsFeatureFlags.FEATURE_FEATURED_HEALTH_SERVICE_CONTENT
-        ? healthCareRegionFeaturedHealthServices
-        : ''
     }
     ${healthCareRegionHealthServices}
     fieldPressReleaseBlurb {
