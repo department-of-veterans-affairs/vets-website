baa2a01d9abce80b52ddffb56904981119583992
hughes-dustin <hughes_dustin@bah.com>
2019-09-16 13:25:51 -0400

    fix text for zip code question

--- src/applications/gi/components/profile/CalculatorForm.jsx
+++ src/applications/gi/components/profile/CalculatorForm.jsx
@@ -561,7 +561,7 @@ class CalculatorForm extends React.Component {
         <div>
           <ErrorableTextInput
             errorMessage={inputs.beneficiaryZIPError}
-            label="Please enter the ZIP Code where you'll take your classes"
+            label="Please enter the zip code where you’l take your classes"
             name="beneficiaryZIPCode"
             field={{ value: inputs.beneficiaryZIP }}
             onValueChange={this.handleBeneficiaryZIPCodeChanged}
