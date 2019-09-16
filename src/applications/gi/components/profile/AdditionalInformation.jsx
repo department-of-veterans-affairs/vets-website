01c13a0b9c978b2083b1bddbc5fa951d001c0a8b
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2019-09-16 10:35:58 -0400

    removed import space

--- src/applications/gi/components/profile/AdditionalInformation.jsx
+++ src/applications/gi/components/profile/AdditionalInformation.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+
 export class AdditionalInformation extends React.Component {
   updateFiscalYear() {
     const constants = this.props.constants;
