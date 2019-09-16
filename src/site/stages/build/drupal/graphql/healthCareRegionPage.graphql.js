128241f4606f5a7c5911b66d9d9da20cb06cec59
Ethan Teague <metrowebz@gmail.com>
2019-09-16 10:53:45 -0400

    VAGOV-5835: Adding featured content fragment back to query.

--- src/site/stages/build/drupal/graphql/healthCareRegionPage.graphql.js
+++ src/site/stages/build/drupal/graphql/healthCareRegionPage.graphql.js
@@ -5,6 +5,7 @@
 const entityElementsFromPages = require('./entityElementsForPages.graphql');
 const healthCareLocalFacilities = require('./facilities-fragments/healthCareLocalFacility.node.graphql');
 const healthCareRegionHealthServices = require('./facilities-fragments/healthCareRegionHealthServices.node.graphql');
+const healthCareRegionFeaturedHealthServices = require('./facilities-fragments/healthCareRegionFeaturedHealthServces.node.graphql');
 const healthCareRegionNewsStories = require('./facilities-fragments/healthCareRegionNewsStories.node.graphql');
 const healthCareRegionEvents = require('./facilities-fragments/healthCareRegionEvents.node.graphql');
 const healthCareStaffBios = require('./facilities-fragments/healthCareRegionStaffBios.node.graphql');
@@ -114,6 +115,7 @@ module.exports = `
     fieldClinicalHealthCareServi {
       processed
     }
+    ${healthCareRegionFeaturedHealthServices}
     ${healthCareRegionHealthServices}
     fieldPressReleaseBlurb {
       processed
