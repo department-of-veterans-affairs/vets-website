38110f72a108545df67af82a789caaf8c8846bee
Elwood Gary <kenneth.gary@va.gov>
2019-09-18 11:18:58 -0400

    1283: Adding more unit tests

--- src/platform/user/profile/vet360/tests/containers/ReceiveTextMessages.unit.spec.jsx
+++ src/platform/user/profile/vet360/tests/containers/ReceiveTextMessages.unit.spec.jsx
@@ -15,6 +15,9 @@ describe('<ReceiveTextMessages/>', () => {
       createTransaction() {},
       clearTransactionStatus() {},
       componentDidMount() {},
+      componentWillReceiveProps() {},
+      onChange() {},
+      isSuccessVisible() {},
       profile: {
         verified: true,
         vet360: { mobilePhone: { isTextPermitted: false } },
@@ -53,6 +56,40 @@ describe('<ReceiveTextMessages/>', () => {
     expect(
       props.componentDidMount.calledWith(props),
       'componentDidMount was called with user profile to initialize enrollment status',
+    ).to.be.false;
+    component.unmount();
+  });
+
+  it('calls componentWillReceiveProps with verified profile', () => {
+    sinon.stub(props, 'componentWillReceiveProps');
+    const component = enzyme.shallow(<ReceiveTextMessages {...props} />);
+    const nextProps = props;
+    expect(
+      props.componentWillReceiveProps.calledWith(nextProps),
+      'componentWillReceiveProps was called with nextProps',
+    ).to.be.false;
+    component.unmount();
+  });
+
+  it('calls onChange to create a transaction', () => {
+    sinon.stub(props, 'onChange');
+    const component = enzyme.shallow(<ReceiveTextMessages {...props} />);
+    expect(props.onChange.calledWith(), 'onChange was called').to.be.false;
+    component.unmount();
+  });
+
+  it('calls isSuccessVisible to see if success message should be visible', () => {
+    props.transactionSuccess = true;
+    sinon.stub(props, 'isSuccessVisible');
+    const component = enzyme.shallow(<ReceiveTextMessages {...props} />);
+    component.setState({
+      startedTransaction: true,
+      completedTransaction: true,
+      lastTransaction: props.transaction,
+    });
+    expect(
+      props.isSuccessVisible.calledWith(),
+      'isSuccessVisible was called with transaction success',
     ).to.be.false;
     component.unmount();
   });
