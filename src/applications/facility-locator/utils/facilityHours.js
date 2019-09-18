70cf3588481ef4172688510083da83b4af9b20e3
lightning <aurora.hampton@agile6.com>
2019-09-17 17:17:45 -0700

    VAGOV-5654: facility landing page style updates

--- src/applications/facility-locator/utils/facilityHours.js
+++ src/applications/facility-locator/utils/facilityHours.js
@@ -1,18 +1,30 @@
 import moment from 'moment';
 import { forIn, upperFirst } from 'lodash';
 
-export function buildHours(hours) {
+export function buildHours(hours, shortDay = false) {
   const builtHours = [];
+  const shortDays = {
+    Monday: 'Mon',
+    Tuesday: 'Tue',
+    Wednesday: 'Wed',
+    Thursday: 'Thu',
+    Friday: 'Fri',
+    Saturday: 'Sat',
+    Sunday: 'Sun',
+  };
 
   forIn(hours, (value, key) => {
-    const day = upperFirst(key);
+    let day = upperFirst(key);
+    if (shortDay) {
+      day = shortDays[day];
+    }
 
     let dayHours;
     if (value === 'Closed' || value === '24/7') {
       dayHours = value;
     } else {
       const hour = value.split('-').map(time => moment(time, 'hmmA'));
-      dayHours = hour.map(time => time.format('h:mmA')).join(' - ');
+      dayHours = hour.map(time => time.format('h:mmA')).join(' – ');
     }
 
     builtHours.push(`${day}: ${dayHours}`);
