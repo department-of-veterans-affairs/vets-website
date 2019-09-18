19a5398e767847e85fb9187d4c6c5a57e2463f44
ksantiagomorgan6 <50601724+ksantiagomorgan6@users.noreply.github.com>
2019-09-18 13:40:51 -0400

    added SR audio feedback to zipcode entry (#10710)
    * added SR audio feedback to zipcode entry
    
    * changed class to className
    
    * fixed so aria live label renders in the begining
    
    * deleted varible
    
    * reverted back to master
    
    * added empty <div> and <p>
    
    * removed sass
    
    * fixed premature message

--- src/applications/gi/components/profile/CalculatorForm.jsx
+++ src/applications/gi/components/profile/CalculatorForm.jsx
@@ -567,9 +567,6 @@ class CalculatorForm extends React.Component {
             onValueChange={this.handleBeneficiaryZIPCodeChanged}
             charMax={5}
           />
-          <p>
-            <strong>{inputs.housingAllowanceCity}</strong>
-          </p>
         </div>
       );
     }
@@ -604,6 +601,10 @@ class CalculatorForm extends React.Component {
         />
         {extensionSelector}
         {amountInput}
+        <p aria-live="polite" aria-atomic="true">
+          <span className="sr-only">Your postal code is located in</span>
+          <strong>{inputs.housingAllowanceCity}</strong>
+        </p>
       </div>
     );
   };
