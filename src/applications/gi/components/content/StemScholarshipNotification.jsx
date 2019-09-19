aec5b4d930804910a89aecff44094f9f50125422
hughes-dustin <hughes_dustin@bah.com>
2019-09-19 08:17:42 -0400

    update scholarship notification on landing page

--- src/applications/gi/components/content/StemScholarshipNotification.jsx
+++ src/applications/gi/components/content/StemScholarshipNotification.jsx
@@ -4,21 +4,26 @@ export const StemScholarshipNotification = () => (
   <div className="stem-notification">
     <span className="usa-label">New</span>
     <div className="feature">
-      <h4>The Edith Nourse Rogers STEM Scholarship</h4>
+      <h4>Attending classes at an extension campus?</h4>
       <p>
-        On August 1, 2019, VA launched the Edith Nourse Rogers STEM Scholarship
-        for students enrolled in a high-demand STEM (Science, Technology,
-        Engineering, and Math) program.
+        Under the Forever GI Bill, the Monthly Housing Allowance (MHA) is
+        calculated based on the campus location where you physically attend the
+        majority of your classes.
       </p>
       <p>
-        To learn more about this scholarship,{' '}
+        To estimate your MHA benefit, you can go a school or employer's profile
+        page in the GI Bill Comparison Tool and answer the question "Where will
+        you take the majority of your classes?"
+      </p>
+      <p>
+        To learn more about the Location-Based Housing Allowance,{' '}
         <a
-          href="https://benefits.va.gov/gibill/fgib/stem.asp"
+          href="https://va.gov/education/about-gi-bill-benefits/post-9-11/#location-based107"
           target="_blank"
           rel="noopener noreferrer"
         >
           {' '}
-          visit the Edith Nourse Rogers STEM Scholarship website
+          visit the Post-9/11 GI Bill website
         </a>
         .
       </p>
