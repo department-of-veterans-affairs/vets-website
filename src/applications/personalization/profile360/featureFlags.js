6504bf2363015ac0cc4397dc4f4cbf0d5e3e3391
Micah Chiang <micahkchiang@gmail.com>
2019-09-20 14:38:04 -0700

    update feature flag

--- src/applications/personalization/profile360/featureFlags.js
+++ src/applications/personalization/profile360/featureFlags.js
@@ -2,7 +2,6 @@ import environment from 'platform/utilities/environment';
 
 const featureFlags = Object.freeze({
   directDeposit: !environment.isProduction(),
-  ratedDisabilities: !environment.isProduction(),
 });
 
 export default featureFlags;
