c9fbb39147f23cae570c4ef0dc24336cdec18d50
Robin Garrison <robin.garrison@adhocteam.us>
2019-09-23 14:01:44 -0500

    Start work on Higher-Level Review form

---
+++ src/applications/disability-benefits/996/TODO.md
@@ -0,0 +1,72 @@
+## TODO (form 0996 HLR)
+
+### After API available
+
+- Update `IntroductionPage.jsx`
+  - [ ] Replace placeholder `isInLegacySystem` with value from API
+  - [ ] Add code to update `isInLegacySystem` (or whatever it's named).
+-  Update `WithdrawFromLegacySystem.jsx`
+  - [ ] Consider converting the `WithdrawFromLegacySystem` component into a
+        standalone form with it's own form schema - suggested by Erik Hansen.
+- Update `form.js`
+  - Add `submitUrl`.
+  - Update `formConfig.submit` (`confirmationNumber`?)
+
+### Build Template & Introduction Page (Step 0)
+
+- [x] Build template.
+- [x] Include saved form data component.
+- [ ] Update checkbox error message (needs content approval).
+- [ ] Given multiple entry points, where should the "Back" button lead? Is it
+      bad form to use `history.back()`?
+- [ ] URL destination for the "Learn more about the review options" link?
+      Currently it is pointing to `/decision-reviews`
+- [ ] Destination or component that opens when the "See all your contested
+      issues" link is used.
+
+### Confirm Veteran Details (Step 1)
+
+- [ ] Build form `confirmVeteranDetails` in `config/form.js`
+- [ ] Get last 4 of SSN? Look for API entry.
+- [ ] Auto-fill contact info review (collapsed with edit button? - talk to design)
+
+### Select your contested issues (Step 2)
+
+- [ ] Build form `selectContestedIssues` in `config/form.js`
+- [ ] Get eligible issues API call?
+- [ ] Desination of "See all your issues" click?
+
+### Add notes (Step 3)
+
+- [ ] Build form `addNotes` in `config/form.js`
+- [ ] Awaiting design on if notes should be a textarea (not a blocker)
+- [ ] Always show "You can't submit any evidence"?
+
+### Request original jurisdiction (Step 4)
+
+- [ ] Build form `requestOriginalJurisdiction` in `config/form.js`
+- [ ] Blocked by design on how to deal with choosing regional office.
+
+### Request an informal conference (Step 5)
+
+- [ ] Build form `requestAnInformalConference` in `config/form.js`
+- [ ] Awaiting design on "Weekday" dropdown content
+
+### Submit your application (Step 6)
+
+- [ ] Build form in ?? (wording appears to be "Review and submit your
+      application", can it be changed to match the design?)
+- [ ] Add privacy policy check
+
+### Confirmation page
+
+- [ ] Is there a design? This page was automaticaly added by the form builder.
+
+### Before Production
+- [ ] Check for, and remove, all console logs.
+- [ ] Move `20-0996-schema.json` to `vets-json-schema` repo.
+- [ ] Ensure matching tracking prefixes (if changed)
+  - `src/applications/disability-benefits/996/config/form.js` in `formConfig.trackingPrefix`.
+  - `src/applications/personalization/dashboard/helpers.jsx` ~line 146
+- [ ] Clear production flag in `config/form.js`.
+- [ ] Delete this to-do file.
---
+++ src/applications/disability-benefits/996/config/form.js
@@ -0,0 +1,154 @@
+// Example of an imported schema:
+import fullSchema from '../20-0996-schema.json';
+// In a real app this would be imported from `vets-json-schema`:
+// import fullSchema from 'vets-json-schema/dist/20-0996-schema.json';
+
+// In a real app this would not be imported directly; instead the schema you
+// imported above would import and use these common definitions:
+import commonDefinitions from 'vets-json-schema/dist/definitions.json';
+
+// import environment from 'platform/utilities/environment';
+import { VA_FORM_IDS } from 'platform/forms/constants';
+import fullNameUI from 'platform/forms-system/src/js/definitions/fullName';
+import phoneUI from 'platform/forms-system/src/js/definitions/phone';
+import * as address from 'platform/forms-system/src/js/definitions/address';
+
+import IntroductionPage from '../components/IntroductionPage';
+import ConfirmationPage from '../containers/ConfirmationPage';
+
+const { fullName, date, dateRange, usaPhone } = commonDefinitions;
+
+// Define all the fields in the form to aid reuse
+const formFields = {
+  fullName: 'fullName',
+  address: 'address',
+  email: 'email',
+  altEmail: 'altEmail',
+  phoneNumber: 'phoneNumber',
+};
+
+// Define all the form pages to help ensure uniqueness across all form chapters
+const formPages = {
+  confirmVeteranDetails: 'confirmVeteranDetails',
+  selectContestedIssues: 'selectContestedIssues',
+  addNotes: 'addNotes',
+  requestOriginalJurisdiction: 'requestOriginalJurisdiction',
+  // requestAnInformalConference: 'requestAnInformalConference',
+};
+
+const formConfig = {
+  urlPrefix: '/',
+  // submitUrl: `${environment.API_URL}/v0/higher_level_review/submit`,
+  submit: () =>
+    Promise.resolve({ attributes: { confirmationNumber: '123123123' } }),
+  trackingPrefix: 'hlr-0996-',
+  introduction: IntroductionPage,
+  confirmation: ConfirmationPage,
+  formId: VA_FORM_IDS.FORM_20_0996,
+  version: 0,
+  prefillEnabled: true,
+  savedFormMessages: {
+    notFound: 'Please start over to request a Higher-Level Review.',
+    noAuth:
+      'Please sign in again to continue your request for Higher-Level Review.',
+  },
+  title: 'Higher-Level Review',
+  defaultDefinitions: {
+    fullName,
+    date,
+    dateRange,
+    usaPhone,
+  },
+  testData: {
+    test: true,
+  },
+  chapters: {
+    confirmVeteranDetails: {
+      title: 'Confirm Veteran details',
+      pages: {
+        [formPages.confirmVeteranDetails]: {
+          path: 'confirm-veteran-details',
+          title: 'Confirm Veteran details',
+          uiSchema: {
+            [formFields.fullName]: fullNameUI,
+          },
+          schema: {
+            type: 'object',
+            // required: [formFields.fullName],
+            properties: {
+              [formFields.fullName]: fullName,
+            },
+          },
+        },
+      },
+    },
+    selectContestedIssues: {
+      title: 'Select your contested issues',
+      pages: {
+        [formPages.selectContestedIssues]: {
+          path: 'select-your-contested-issues',
+          title: 'Select your contested issues',
+          uiSchema: {
+            [formFields.address]: address.uiSchema('Mailing address'),
+            [formFields.email]: {
+              'ui:title': 'Primary email',
+            },
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              [formFields.address]: address.schema(fullSchema, true),
+              [formFields.email]: {
+                type: 'string',
+                format: 'email',
+              },
+            },
+          },
+        },
+      },
+    },
+    addNotes: {
+      title: 'Add notes',
+      pages: {
+        [formPages.selectContestedIssues]: {
+          path: 'add-notes',
+          title: 'Add notes (optional)',
+          uiSchema: {
+            [formFields.altEmail]: {
+              'ui:title': 'Secondary email',
+            },
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              [formFields.altEmail]: {
+                type: 'string',
+                format: 'email',
+              },
+            },
+          },
+        },
+      },
+    },
+    requestOriginalJurisdiction: {
+      title: 'Request original jurisdiction',
+      pages: {
+        [formPages.selectContestedIssues]: {
+          path: 'request-original-jurisdiction',
+          title: 'Request original jurisdiction',
+          uiSchema: {
+            [formFields.phoneNumber]: phoneUI('Daytime phone'),
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              [formFields.phoneNumber]: usaPhone,
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+export default formConfig;
