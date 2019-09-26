48247b8a1fda68761361295cdb5ba3be6ce257f8
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-09-26 10:53:07 -0400

    FIx another e2e heading

--- src/applications/facility-locator/tests/00-required.e2e.spec.js
+++ src/applications/facility-locator/tests/00-required.e2e.spec.js
@@ -25,7 +25,7 @@ module.exports = E2eHelpers.createE2eTest(client => {
   // check detail pages
   client
     .waitForElementVisible('.facility-result a h2', Timeouts.slow)
-    .click('.facility-result a h5')
+    .click('.facility-result a h2')
     .waitForElementVisible('.all-details', Timeouts.slow)
     .axeCheck('.main');
 
