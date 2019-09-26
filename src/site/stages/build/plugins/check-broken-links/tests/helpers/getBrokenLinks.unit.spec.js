ce4cd31b2e23c4e29a83fa82418763375d417a44
Christopher Valarida <chris.valarida@adhocteam.us>
2019-09-25 17:11:36 -0700

    Use parsedContent in broken link unit test

--- src/site/stages/build/plugins/check-broken-links/tests/helpers/getBrokenLinks.unit.spec.js
+++ src/site/stages/build/plugins/check-broken-links/tests/helpers/getBrokenLinks.unit.spec.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
+const cheerio = require('cheerio');
 
 const getBrokenLinks = require('../../helpers/getBrokenLinks');
 
@@ -8,53 +9,41 @@ const img = '<img src="/another-link.png">';
 const span = '<span>Not a link</span>';
 const anchorWithoutHref = '<a>Link</a>';
 
+const getFile = tag => {
+  const contents = `<div>${anchor}${tag}</div>`;
+  return {
+    path: '/health-care',
+    contents,
+    parsedContent: cheerio.load(contents),
+  };
+};
+
 describe('getBrokenLinks', () => {
+  const detectAllLinksBroken = sinon.stub().returns(true);
+  const detectAllLinksOkay = sinon.stub().returns(false);
+
   it('finds broken links', () => {
-    const file = {
-      path: '/health-care',
-      contents: `<div>${anchor}${img}</div>`,
-    };
-
-    const detectAllLinksBroken = sinon.stub().returns(true);
-    const linkErrors = getBrokenLinks(file, [], detectAllLinksBroken);
-
+    const linkErrors = getBrokenLinks(getFile(img), [], detectAllLinksBroken);
     expect(linkErrors).to.have.lengthOf(2);
   });
 
   it('does not detect non-links as a link', () => {
-    const file = {
-      path: '/health-care',
-      contents: `<div>${anchor}${span}</div>`,
-    };
-
-    const detectAllLinksBroken = sinon.stub().returns(true);
-    const linkErrors = getBrokenLinks(file, [], detectAllLinksBroken);
-
+    const linkErrors = getBrokenLinks(getFile(span), [], detectAllLinksBroken);
     expect(linkErrors).to.have.lengthOf(1);
     expect(linkErrors[0].html).to.be.equal(anchor);
   });
 
   it('does not detect valid links as broken', () => {
-    const file = {
-      path: '/health-care',
-      contents: `<div>${anchor}${img}</div>`,
-    };
-
-    const detectAllLinksOkay = sinon.stub().returns(false);
-    const linkErrors = getBrokenLinks(file, [], detectAllLinksOkay);
-
+    const linkErrors = getBrokenLinks(getFile(img), [], detectAllLinksOkay);
     expect(linkErrors).to.have.lengthOf(0);
   });
 
   it('skips anchors without an HREF attribute', () => {
-    const file = {
-      path: '/health-care',
-      contents: `<div>${anchorWithoutHref}</div>`,
-    };
-
-    const detectAllLinksBroken = sinon.stub().returns(true);
-    const linkErrors = getBrokenLinks(file, [], detectAllLinksBroken);
-
+    const linkErrors = getBrokenLinks(
+      getFile(anchorWithoutHref),
+      [],
+      detectAllLinksBroken,
+    );
     expect(linkErrors).to.have.lengthOf(0);
   });
 });
