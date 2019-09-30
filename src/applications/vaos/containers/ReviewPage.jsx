65d5da5e42ac02ed7dbdd500a08408e1eaa83536
Jeff Balboni <jeff@adhocteam.us>
2019-09-30 15:00:13 -0400

    Remove comments

--- src/applications/vaos/containers/ReviewPage.jsx
+++ src/applications/vaos/containers/ReviewPage.jsx
@@ -10,62 +10,6 @@ import ReviewDirectScheduleInfo from '../components/ReviewDirectScheduleInfo';
 import ReviewRequestInfo from '../components/ReviewRequestInfo';
 import LoadingButton from 'platform/site-wide/loading-button/LoadingButton';
 
-// const dsState = {
-//   newAppointment: {
-//     facilities: {
-//       '323_983': [
-//         {
-//           institution: {
-//             institutionCode: '983GB',
-//             name: 'CHYSHR-Sidney VA Clinic',
-//             city: 'Sidney',
-//             stateAbbrev: 'NE',
-//             authoritativeName: 'CHYSHR-Sidney VA Clinic',
-//             rootStationCode: '983',
-//             adminParent: false,
-//             parentStationCode: '983',
-//           },
-//           requestSupported: true,
-//           directSchedulingSupported: true,
-//           expressTimes: null,
-//           institutionTimezone: 'America/Denver',
-//         },
-//       ],
-//     },
-//     clinics: {
-//       '323_983GB': [
-//         {
-//           siteCode: '983GB',
-//           clinicId: '2276',
-//           clinicName: 'DC/PACT GREEN FOUR',
-//           clinicFriendlyLocationName: 'GREEN VASSALL',
-//           primaryStopCode: '323',
-//           secondaryStopCode: '',
-//           directSchedulingFlag: 'Y',
-//           displayToPatientFlag: 'Y',
-//           institutionName: 'Washington VA Medical Center',
-//           institutionCode: '983GB',
-//           objectType: 'CdwClinic',
-//           link: [],
-//         },
-//       ],
-//     },
-//     data: {
-//       isDirectSchedule: true,
-//       typeOfCareId: '323',
-//       vaSystem: '983',
-//       visitType: 'office',
-//       vaFacility: '983GB',
-//       clinicId: '2276',
-//       appointmentTime: '2019-10-03T08:30:00',
-//       phoneNumber: '5555555555',
-//       email: 'fake@va.gov',
-//       additionalDetails: 'My foot hurts',
-//       reasonForAppointment: 'routine-follow-up',
-//     },
-//   },
-// };
-
 export class ReviewPage extends React.Component {
   render() {
     const { data, facility, clinic } = this.props;
