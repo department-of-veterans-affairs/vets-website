ea32dc58a89bc160dfe89f5130d3815a047b7fd6
Kelson Adams <kadams@governmentcio.com>
2019-10-01 11:10:38 -0400

    Add update-robots build plugin

---
+++ src/site/stages/build/plugins/update-robots.js
@@ -0,0 +1,26 @@
+// Dependencies
+const ENVIRONMENTS = require('../../../constants/environments');
+
+function updateRobots(buildOptions) {
+  // Use the default robots.txt file on production.
+  if (buildOptions.buildtype === ENVIRONMENTS.VAGOVPROD) {
+    // eslint-disable-next-line
+    console.log('Using the production robots.txt');
+    return () => {};
+  }
+
+  return (files, smith, done) => {
+    // Derive the robots file.
+    const robots = files['robots.txt'];
+
+    // Update the robots.txt contents to disallow crawlers.
+    robots.contents = new Buffer('User-agent: *\nDisallow: /\n');
+
+    // eslint-disable-next-line
+    console.log('Using the non-production robots.txt');
+
+    done();
+  };
+}
+
+module.exports = updateRobots;
