c83be6cd230ebff8c0a7cf1267f3520e1acbeb40
lightning <aurora.hampton@agile6.com>
2019-10-02 15:23:39 -0700

    VAGOV-6110: only show mentalHealthClinic number if it is there

--- src/applications/static-pages/facilities/FacilityPhone.jsx
+++ src/applications/static-pages/facilities/FacilityPhone.jsx
@@ -19,32 +19,34 @@ export default class FacilityPhone extends React.Component {
           <div className="main-phone vads-u-margin-bottom--1">
             <strong>Main phone: </strong>
             <a
-              href={`tel:${this.props.facility.attributes.phone.main.replace(
-                /[ ]?x/,
-                '',
-              )}`}
+              href={`tel:${
+                // this replaces an "x" that appears from the api data with a space
+                this.props.facility.attributes.phone.main.replace(/[ ]?x/, '')
+              }`}
             >
               {this.props.facility.attributes.phone.main.replace(/[ ]?x/, '')}
             </a>
           </div>
         )}
-        <div className="mental-health-clinic-phone">
-          <strong>Mental health clinic: </strong>
-          <a
-            href={`tel:${
-              // this replaces an "x" that appears from the api data with a space
-              this.props.facility.attributes.phone.mentalHealthClinic.replace(
+        {this.props.facility.attributes.phone.mentalHealthClinic && (
+          <div className="mental-health-clinic-phone">
+            <strong>Mental health clinic: </strong>
+            <a
+              href={`tel:${
+                // this replaces an "x" that appears from the api data with a space
+                this.props.facility.attributes.phone.mentalHealthClinic.replace(
+                  /[ ]?x/,
+                  '',
+                )
+              }`}
+            >
+              {this.props.facility.attributes.phone.mentalHealthClinic.replace(
                 /[ ]?x/,
                 '',
-              )
-            }`}
-          >
-            {this.props.facility.attributes.phone.mentalHealthClinic.replace(
-              /[ ]?x/,
-              '',
-            )}
-          </a>
-        </div>
+              )}
+            </a>
+          </div>
+        )}
       </div>
     );
   }
