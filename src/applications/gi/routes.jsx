25d252affdacf1967ffcc9cbf66e93dcee1f9420
Devin McCurdy <mccurdy_devin@bah.com>
2019-10-08 09:29:10 -0400

    Updated routes

--- src/applications/gi/routes.jsx
+++ src/applications/gi/routes.jsx
@@ -18,7 +18,10 @@ const routes = {
   childRoutes: [
     { path: 'search', component: SearchPage },
     { path: 'profile/:facilityCode', component: ProfilePage },
-    { path: 'profile/:facilityCode/:programName', component: ProfilePage },
+    {
+      path: 'profile/:facilityCode/:preSelectedProgram',
+      component: ProfilePage,
+    },
     { ...legacyRoutes },
   ],
 };
