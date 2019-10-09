1229a70e84d9f915677fb4e4dc5188284b594f9f
Joe <elder_joseph@bah.com>
2019-10-09 11:32:27 -0400

    19736: CT 116: Update VET TEC Profile Header Static Content

--- src/applications/gi/components/vet-tec/VetTecHeadingSummary.jsx
+++ src/applications/gi/components/vet-tec/VetTecHeadingSummary.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import VetTecAdditionalResources from './VetTecAdditionalResources';
 import { locationInfo } from '../../utils/helpers';
+import environment from 'platform/utilities/environment';
 
 const IconWithInfo = ({ icon, iconClassName, children, present }) => {
   if (!present) return null;
@@ -22,6 +23,11 @@ export const VetTecHeadingSummary = ({ institution, showModal }) => {
     institution.country,
   );
   const addressPresent = formattedAddress !== ''; // if locationInfo returns a blank string, icon should not show
+  const providerWebsitePresent = institution.programs[0].providerWebsite !== '';
+  const phonePresent =
+    institution.programs[0].phoneAreaCode !== '' &&
+    institution.programs[0].phoneNumber !== '';
+  const schoolLocalePresent = institution.programs[0].schoolLocale !== '';
 
   return (
     <div className="heading row">
@@ -47,11 +53,46 @@ export const VetTecHeadingSummary = ({ institution, showModal }) => {
                 (Learn more)
               </button>
             </IconWithInfo>
-            <IconWithInfo icon="map-marker" present={addressPresent}>
-              {formattedAddress}
-            </IconWithInfo>
           </div>
         </div>
+      </div>
+      <div className="usa-width-two-thirds medium-8 small-12 column vads-u-margin-top--2">
+        <div className="usa-width-one-half medium-6 small-12 column">
+          <IconWithInfo icon="map-marker" present={addressPresent}>
+            {formattedAddress}
+          </IconWithInfo>
+          {!environment.isProduction() &&
+            institution.programs[0] && (
+              <IconWithInfo icon="globe" present={providerWebsitePresent}>
+                <a
+                  href={institution.programs[0].providerWebsite}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {institution.programs[0].providerWebsite}
+                </a>
+              </IconWithInfo>
+            )}
+        </div>
+        {!environment.isProduction() &&
+          institution.programs[0] && (
+            <div className="usa-width-one-half medium-6 small-12 column">
+              <IconWithInfo icon="phone" present={phonePresent}>
+                <a
+                  href={`tel:+1${`${institution.programs[0].phoneAreaCode}-${
+                    institution.programs[0].phoneNumber
+                  }`}`}
+                >
+                  {institution.programs[0].phoneAreaCode}
+                  {'-'}
+                  {institution.programs[0].phoneNumber}
+                </a>
+              </IconWithInfo>
+              <IconWithInfo icon="map" present={schoolLocalePresent}>
+                {institution.programs[0].schoolLocale + ' locale'}
+              </IconWithInfo>
+            </div>
+          )}
       </div>
       <VetTecAdditionalResources />
     </div>
