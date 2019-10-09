58d85faac3b37ae879c40367208dbde3e3169374
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-10-09 18:08:40 -0400

    Switch to inline functions

--- src/applications/static-pages/subscribeAdditionalInfoEvents.js
+++ src/applications/static-pages/subscribeAdditionalInfoEvents.js
@@ -1,17 +1,13 @@
 import recordEvent from 'platform/monitoring/record-event';
 
-const recordExpand = () => recordEvent({ event: 'nav-additionalInfo-expand' });
-const recordCollapse = () =>
-  recordEvent({ event: 'nav-additionalInfo-collapse' });
-
 export default function subscribeAdditionalInfoAnalytics() {
   document.body.addEventListener(
     '@department-of-veterans-affairs/formation/additional-info/expand',
-    recordExpand,
+    () => recordEvent({ event: 'nav-additionalInfo-expand' }),
   );
 
   document.body.addEventListener(
     '@department-of-veterans-affairs/formation/additional-info/collapse',
-    recordCollapse,
+    () => recordEvent({ event: 'nav-additionalInfo-collapse' }),
   );
 }
