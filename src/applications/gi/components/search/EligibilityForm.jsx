8588c75d58a8a8203dc0b1fe389afb4188cccad4
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2019-10-10 11:32:44 -0400

    updated renderLearnMore method to handle aria-label text

--- src/applications/gi/components/search/EligibilityForm.jsx
+++ src/applications/gi/components/search/EligibilityForm.jsx
@@ -23,10 +23,11 @@ export class EligibilityForm extends React.Component {
     { value: 'purple heart', label: 'Purple Heart Service: 100%' },
   ];
 
-  renderLearnMoreLabel = ({ text, modal }) =>
+  renderLearnMoreLabel = ({ text, modal, learnMoreMessage }) =>
     renderLearnMoreLabel({
       text,
       modal,
+      learnMoreMessage,
       showModal: this.props.showModal,
       component: this,
     });
@@ -69,6 +70,7 @@ export class EligibilityForm extends React.Component {
           label={this.renderLearnMoreLabel({
             text: 'Which GI Bill benefit do you want to use?',
             modal: 'giBillChapter',
+            learnMoreMessage: 'Learn more about GI Bill benefits',
           })}
           name="giBillChapter"
           options={[
@@ -123,6 +125,8 @@ export class EligibilityForm extends React.Component {
           label={this.renderLearnMoreLabel({
             text: 'Cumulative Post-9/11 active duty service',
             modal: 'cumulativeService',
+            learnMoreMessage:
+              '“Learn more about cumulative Post-9/11 active duty service”',
           })}
           name="cumulativeService"
           options={this.cumulativeServiceOptions()}
