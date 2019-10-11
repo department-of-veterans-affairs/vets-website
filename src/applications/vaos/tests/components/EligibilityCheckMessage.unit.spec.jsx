92c0652d66bfb1f4aa54ec9e4a85057e6de52dcd
Jeff Balboni <jeff@adhocteam.us>
2019-10-11 14:13:17 -0400

    Conditionally send users to direct schedule flow in VAOS (#10872)

---
+++ src/applications/vaos/tests/components/EligibilityCheckMessage.unit.spec.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+
+import EligibilityCheckMessage from '../../components/EligibilityCheckMessage';
+
+describe('VAOS <EligibilityCheckMessage>', () => {
+  it('should render past visit message', () => {
+    const eligibility = {
+      requestPastVisit: false,
+      requestPastVisitValue: 24,
+    };
+
+    const tree = mount(<EligibilityCheckMessage eligibility={eligibility} />);
+
+    expect(tree.text()).to.contain('seen within the past 24 months');
+    tree.unmount();
+  });
+
+  it('should render limit message', () => {
+    const eligibility = {
+      requestPastVisit: true,
+      requestLimit: false,
+    };
+
+    const tree = mount(<EligibilityCheckMessage eligibility={eligibility} />);
+
+    expect(tree.text()).to.contain('more outstanding requests');
+    tree.unmount();
+  });
+
+  it('should render generic message', () => {
+    const eligibility = {
+      requestPastVisit: true,
+      requestLimit: true,
+    };
+
+    const tree = mount(<EligibilityCheckMessage eligibility={eligibility} />);
+
+    expect(tree.text()).to.contain('trouble verifying');
+    tree.unmount();
+  });
+});
