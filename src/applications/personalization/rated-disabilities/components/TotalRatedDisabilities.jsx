0e211efefb3d3cf0a59b89ebfd1a426ffe437139
Jesse Cohn <jesse.cohn@adhocteam.us>
2019-10-11 12:15:36 -0700

    Adjusted based on feedback on PR

--- src/applications/personalization/rated-disabilities/components/TotalRatedDisabilities.jsx
+++ src/applications/personalization/rated-disabilities/components/TotalRatedDisabilities.jsx
@@ -95,24 +95,12 @@ const TotalRatedDisabilities = props => {
                 </a>
               </p>
             </div>
-            <div className="vads-l-col--12 medium-screen:vads-l-col--6 small-desktop-screen:vads-l-col--3">
+            <div className="vads-l-col--12 medium-screen:vads-l-col--6 small-desktop-screen:vads-l-col--3 medium-screen:vads-u-padding-top--3">
               <PercetnageCalloutBox
                 value={totalDisabilityRating}
                 isPercentage
                 label="Your combined VA disability rating"
               />
-            </div>
-          </div>
-          <div className="vads-l-row">
-            <div className="vads-l-col--12 medium-screen:vads-l-col--6 small-desktop-screen:vads-l-col--8">
-              <h3>What if I disagree with my rating?</h3>
-              <p>
-                If you disagree with your rating, you can file an appeal. You'll
-                need to do this within 1 year of getting your decision notice.
-              </p>
-              <p>
-                <a href="#">Learn how to file an appeal</a>
-              </p>
             </div>
           </div>
         </div>
