3949c6dec52d6a54d8009c28a5aa206bfb6887b0
Jeff Balboni <jeff@adhocteam.us>
2019-10-14 12:12:40 -0400

    Add focus handling to landing page

--- src/applications/vaos/components/LandingPage.jsx
+++ src/applications/vaos/components/LandingPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router';
 import Breadcrumbs from './Breadcrumbs';
+import { useScrollAndFocus } from '../utils/scrollAndFocus';
 
 export default function LandingPage() {
+  useScrollAndFocus();
+
   return (
     <div className="vads-l-grid-container vads-u-padding-x--2p5 large-screen:vads-u-padding-x--0 vads-u-padding-bottom--2p5">
       <Breadcrumbs />
