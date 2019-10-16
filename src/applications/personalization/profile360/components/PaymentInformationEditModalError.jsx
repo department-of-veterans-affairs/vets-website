97c611a708a5b59f68a276dee58467657c9e1a49
Erik P. Hansen <eph@erikphansen.com>
2019-10-16 13:21:41 -0700

    Add error bad contact info error messages (#10900)
    * Add error bad contact info error messages
    
    And provide a link from the error message to the profile so the user can
    quickly update the bad data in their profile.
    
    * handle newly discovered routing number error
    
    * update error messages

--- src/applications/personalization/profile360/components/PaymentInformationEditModalError.jsx
+++ src/applications/personalization/profile360/components/PaymentInformationEditModalError.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
 
-const ACCOUNT_FLAGGED_FOR_FRAUD = 'cnp.payment.flashes.on.record.message';
-const INVALID_ROUTING_NUMBER = 'payment.accountRoutingNumber.invalidCheckSum';
+// possible values for the `key` property on error messages we get from the server
+const ACCOUNT_FLAGGED_FOR_FRAUD_KEY = 'cnp.payment.flashes.on.record.message';
+const INVALID_ROUTING_NUMBER_KEY =
+  'payment.accountRoutingNumber.invalidCheckSum';
+const GENERIC_ERROR_KEY = 'cnp.payment.generic.error.message';
 
 function FlaggedAccount() {
   return (
@@ -41,27 +44,151 @@ function GenericError() {
   );
 }
 
-function hasError(errors, errorKey) {
+// Since we don't know what the error message looks like when there's a problem
+// with the user's home address, we'll use a single error message for any and
+// all address-related errors
+function UpdateAddressError({ closeModal }) {
+  return (
+    <p>
+      We’re sorry. We couldn’t update your direct deposit bank information
+      because your address is missing or invalid. Please go back to{' '}
+      <a
+        href="/profile/#contact-information"
+        onClick={() => {
+          closeModal();
+        }}
+      >
+        your profile
+      </a>{' '}
+      and fill in this required information.
+    </p>
+  );
+}
+
+function UpdatePhoneNumberError({ closeModal, phoneNumberType = 'home' }) {
+  return (
+    <p>
+      We’re sorry. We couldn’t update your direct deposit bank information
+      because your {phoneNumberType} phone number is missing or invalid. Please
+      go back to{' '}
+      <a
+        href="/profile/#contact-information"
+        onClick={() => {
+          closeModal();
+        }}
+      >
+        your profile
+      </a>{' '}
+      and fill in this required information.
+    </p>
+  );
+}
+
+function hasErrorMessageKey(errors, errorKey) {
   return errors.some(err =>
     err.meta.messages.some(message => message.key === errorKey),
   );
 }
 
-export default function PaymentInformationEditModalError({ responseError }) {
+function hasErrorMessageText(errors, errorText) {
+  return errors.some(err =>
+    err.meta.messages.some(message =>
+      message.text.toLowerCase().includes(errorText.toLowerCase()),
+    ),
+  );
+}
+
+function hasFlaggedForFraudError(errors) {
+  return hasErrorMessageKey(errors, ACCOUNT_FLAGGED_FOR_FRAUD_KEY);
+}
+
+function hasInvalidRoutingNumberError(errors) {
+  let result = false;
+  if (hasErrorMessageKey(errors, INVALID_ROUTING_NUMBER_KEY)) {
+    result = true;
+  }
+  if (
+    hasErrorMessageKey(errors, GENERIC_ERROR_KEY) &&
+    hasErrorMessageText(errors, 'Invalid Routing Number')
+  ) {
+    result = true;
+  }
+  return result;
+}
+
+function hasInvalidAddressError(errors) {
+  let result = false;
+  if (
+    hasErrorMessageKey(errors, GENERIC_ERROR_KEY) &&
+    hasErrorMessageText(errors, 'address update')
+  ) {
+    result = true;
+  }
+  return result;
+}
+
+function hasInvalidHomePhoneNumberError(errors) {
+  let result = false;
+  if (
+    hasErrorMessageKey(errors, GENERIC_ERROR_KEY) &&
+    (hasErrorMessageText(errors, 'night phone number') ||
+      hasErrorMessageText(errors, 'night area number'))
+  ) {
+    result = true;
+  }
+  return result;
+}
+
+function hasInvalidWorkPhoneNumberError(errors) {
+  let result = false;
+  if (
+    hasErrorMessageKey(errors, GENERIC_ERROR_KEY) &&
+    (hasErrorMessageText(errors, 'day phone number') ||
+      hasErrorMessageText(errors, 'day area number'))
+  ) {
+    result = true;
+  }
+  return result;
+}
+
+export default function PaymentInformationEditModalError({
+  responseError,
+  closeModal,
+}) {
   let content = <GenericError />;
 
   if (responseError.error) {
     const { errors = [] } = responseError.error;
 
-    if (hasError(errors, ACCOUNT_FLAGGED_FOR_FRAUD)) {
+    if (hasFlaggedForFraudError(errors)) {
       content = <FlaggedAccount />;
-    } else if (hasError(errors, INVALID_ROUTING_NUMBER)) {
+    } else if (hasInvalidRoutingNumberError(errors)) {
       content = <InvalidRoutingNumber />;
+    } else if (hasInvalidAddressError(errors)) {
+      content = <UpdateAddressError closeModal={closeModal} />;
+    } else if (hasInvalidHomePhoneNumberError(errors)) {
+      content = (
+        <UpdatePhoneNumberError
+          closeModal={closeModal}
+          phoneNumberType="home"
+        />
+      );
+    } else if (hasInvalidWorkPhoneNumberError(errors)) {
+      content = (
+        <UpdatePhoneNumberError
+          closeModal={closeModal}
+          phoneNumberType="work"
+        />
+      );
     }
   }
 
   return (
-    <AlertBox status="error" isVisible>
+    <AlertBox
+      status="error"
+      headline="We couldn’t update your bank information"
+      isVisible
+    >
       {content}
     </AlertBox>
   );
