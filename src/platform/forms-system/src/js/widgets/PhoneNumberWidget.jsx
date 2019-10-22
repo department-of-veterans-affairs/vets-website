b8a42156359f9ee316fea50342fab65d651453fd
Erik P. Hansen <eph@erikphansen.com>
2019-10-22 09:38:50 -0700

    Prefill phone number in VAOS contact page
    - adds new vet360 Redux selectors
    - fixes issue with prefill not working correctly with the PhoneUI widget

--- src/platform/forms-system/src/js/widgets/PhoneNumberWidget.jsx
+++ src/platform/forms-system/src/js/widgets/PhoneNumberWidget.jsx
@@ -10,6 +10,11 @@ export default class PhoneNumberWidget extends React.Component {
     super(props);
     this.state = { val: props.value };
   }
+  componentDidUpdate(previousProps) {
+    if (previousProps.value !== this.props.value) {
+      this.handleChange(this.props.value);
+    }
+  }
   handleChange = val => {
     let stripped;
     if (val) {
