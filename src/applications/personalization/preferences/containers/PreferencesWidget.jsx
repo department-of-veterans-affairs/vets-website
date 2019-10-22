f66fd42f609ee1fbcb306ad58a2b455674276716
Amen Moja Ra <ara@governmentcio.com>
2019-10-22 12:13:45 -0400

    My VA Benefits screen is reading a lot of data to some assistive devices on content load (#10919)
    * removing the aria-live attribute
    
    * removed the extra wrapping div

--- src/applications/personalization/preferences/containers/PreferencesWidget.jsx
+++ src/applications/personalization/preferences/containers/PreferencesWidget.jsx
@@ -250,7 +250,7 @@ class PreferencesWidget extends React.Component {
             />
           )}
         </ReactCSSTransitionGroup>
-        <div aria-live="polite">{this.renderContent()}</div>
+        {this.renderContent()}
       </div>
     );
   }
