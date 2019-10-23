cca1ed004ff64ad89cdbf1073430f2a07a8f3717
Narin Ratana <n.ratana@gmail.com>
2019-10-22 22:45:52 -0700

    Add selector

--- src/applications/vaos/utils/selectors.js
+++ src/applications/vaos/utils/selectors.js
@@ -64,6 +64,19 @@ export function getChosenFacilityInfo(state) {
   );
 }
 
+export function getDateTimeSelect(state, pageKey) {
+  const formInfo = getFormPageInfo(state, pageKey);
+  const availableSlots = getNewAppointment(state).availableSlots;
+
+  return {
+    ...formInfo,
+    facility: getChosenFacilityInfo(state),
+    availableSlots,
+    availableDates: availableSlots?.map(s => s.date),
+    loadingAppointmentSlots: getNewAppointment(state).loadingAppointmentSlots,
+  };
+}
+
 export function hasSingleValidVALocation(state) {
   const formInfo = getFormPageInfo(state, 'vaFacility');
 
