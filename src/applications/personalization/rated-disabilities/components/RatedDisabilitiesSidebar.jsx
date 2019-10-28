02749d6d8f463bc7bb39874c29da0631154153ef
Jesse Cohn <jesse.cohn@adhocteam.us>
2019-10-28 11:34:08 -0700

    changed classes based on Visual QA

--- src/applications/personalization/rated-disabilities/components/RatedDisabilitiesSidebar.jsx
+++ src/applications/personalization/rated-disabilities/components/RatedDisabilitiesSidebar.jsx
@@ -2,43 +2,35 @@ import React from 'react';
 
 const RatedDisabilitiesSidebar = () => (
   <div className="medium-screen:vads-u-padding-left--4">
-    <div>
-      <p className="vads-u-display--inline-block vads-u-font-family--serif vads-u-font-weight--bold vads-u-font-size--lg vads-u-border-bottom--3px vads-u-border-color--primary vads-u-margin-top--0 vads-u-padding-bottom--2p5">
-        How did I get this rating?
-      </p>
-      <p className="vads-u-margin-top--0 vads-u-padding-top--1p5">
-        Your total disability rating is based on evidence you provide, the
-        results of your VA claim exam, and information from other sources.
-      </p>
-      <a
-        href="https://www.youtube.com/watch?v=oM7oYzL2DCg"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Compensation 101: How did I get this rating (YouTube)
-      </a>
-    </div>
-
-    <div className="vads-u-margin-top--2p5">
-      <p className="vads-u-display--inline-block vads-u-font-family--serif vads-u-font-weight--bold vads-u-font-size--lg vads-u-border-bottom--3px vads-u-border-color--primary vads-u-padding-bottom--2p5">
-        What if I don't see the disability rating I was expecting?
-      </p>
-      <p className="vads-u-margin-top--0 vads-u-padding-top--1p5">
-        If you filed a new disability claim to add a condition, or if you
-        appealed a disability decision in the past 3 months, we might still be
-        processing your request.
-      </p>
-    </div>
-
-    <div className="vads-u-margin-top--2p5">
-      <p className="vads-u-display--inline-block vads-u-font-family--serif vads-u-font-weight--bold vads-u-font-size--lg vads-u-border-bottom--3px vads-u-border-color--primary vads-u-padding-bottom--2p5">
-        What if I have questions?
-      </p>
-      <p className="vads-u-margin-top--0 vads-u-padding-top--1p5">
-        You can call us at <a href="tel:800-827-1000">800-827-1000</a>. We're
-        here Monday through Friday, 8:00 a.m. to 9:00 p.m. ET.
-      </p>
-    </div>
+    <h3 className="vads-u-padding-bottom--1p5 vads-u-border-bottom--3px vads-u-border-color--primary">
+      How did I get this rating?
+    </h3>
+    <p>
+      Your total disability rating is based on evidence you provide, the results
+      of your VA claim exam, and information from other sources.
+    </p>
+    <a
+      href="https://www.youtube.com/watch?v=oM7oYzL2DCg"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Compensation 101: How did I get this rating (YouTube)
+    </a>
+    <h3 className="vads-u-padding-bottom--1p5 vads-u-border-bottom--3px vads-u-border-color--primary">
+      What if I don't see the disability rating I was expecting?
+    </h3>
+    <p>
+      If you filed a new disability claim to add a condition, or if you appealed
+      a disability decision in the past 3 months, we might still be processing
+      your request.
+    </p>
+    <h3 className="vads-u-padding-bottom--1p5 vads-u-border-bottom--3px vads-u-border-color--primary">
+      What if I have questions?
+    </h3>
+    <p>
+      You can call us at <a href="tel:800-827-1000">800-827-1000</a>. We're here
+      Monday through Friday, 8:00 a.m. to 9:00 p.m. ET.
+    </p>
   </div>
 );
 
