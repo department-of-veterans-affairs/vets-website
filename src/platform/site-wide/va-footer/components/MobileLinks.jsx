1a83af62e98612a942b8b5f353ac0355c5ead51f
Robin Garrison <robin.garrison@adhocteam.us>
2019-10-31 10:37:32 -0500

    Remove onClick handler in footer
    Tracking will be moved

--- src/platform/site-wide/va-footer/components/MobileLinks.jsx
+++ src/platform/site-wide/va-footer/components/MobileLinks.jsx
@@ -12,7 +12,6 @@ export default function MobileLinks({ links, visible }) {
       <ul className="usa-accordion va-footer-accordion">
         <li>
           <button
-            onClick={() => recordEvent({ event: 'nav-jumplink-click' })}
             className="usa-button-unstyled usa-accordion-button va-footer-button"
             aria-controls="veteran-contact"
             itemProp="name"
