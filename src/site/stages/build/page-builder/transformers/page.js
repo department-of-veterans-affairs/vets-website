41090bb75e124e13dd9ef015465410182f61de16
Brooks Johnson <brooks.johnson@pm.me>
2019-10-31 14:23:45 -0700

    Refactor helper func for getting drupal value

--- src/site/stages/build/page-builder/transformers/page.js
+++ src/site/stages/build/page-builder/transformers/page.js
@@ -1,17 +1,5 @@
 const { flatten, isEmpty } = require('lodash');
-
-/**
- * A very specific helper function that expects to receive an
- * array with one item which is an object with a single `value` property
- *
- */
-function arrayCollapse(arr) {
-  return arr.reduce((value, item) => {
-    // eslint-disable-next-line no-param-reassign
-    value = item.value;
-    return value;
-  }, null);
-}
+const { getDrupalValue } = require('./helpers');
 
 function pageTransform(entity) {
   const transformed = entity;
@@ -26,13 +14,13 @@ function pageTransform(entity) {
   } = entity;
   // collapse title
   // Question: Can we always assume that title is an array of one item, with that item being an object with a `value` key?
-  transformed.title = arrayCollapse(title);
+  transformed.title = getDrupalValue(title);
   transformed.entityBundle = 'page';
 
-  transformed.fieldIntroText = arrayCollapse(fieldIntroText);
-  transformed.changed = new Date(arrayCollapse(changed)).getTime() / 1000;
+  transformed.fieldIntroText = getDrupalValue(fieldIntroText);
+  transformed.changed = new Date(getDrupalValue(changed)).getTime() / 1000;
   transformed.fieldPageLastBuilt = new Date(
-    arrayCollapse(fieldPageLastBuilt),
+    getDrupalValue(fieldPageLastBuilt),
   ).toUTCString();
 
   transformed.entityPublished = published === 'published';
