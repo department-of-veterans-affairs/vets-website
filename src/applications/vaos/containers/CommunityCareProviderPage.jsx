cfa7b90a29d3e9cfafae0b111c5d74dbb6537bb8
Jeff Balboni <jeff@adhocteam.us>
2019-11-04 12:06:58 -0500

    WIP

--- src/applications/vaos/containers/CommunityCareProviderPage.jsx
+++ src/applications/vaos/containers/CommunityCareProviderPage.jsx
@@ -12,34 +12,133 @@ import {
 } from '../actions/newAppointment.js';
 import { getFormPageInfo } from '../utils/selectors';
 
+const address = {
+  type: 'object',
+  properties: {
+    state: {
+      type: 'string',
+      enum: [
+        'AL',
+        'AK',
+        'AS',
+        'AZ',
+        'AR',
+        'AA',
+        'AE',
+        'AP',
+        'CA',
+        'CO',
+        'CT',
+        'DE',
+        'DC',
+        'FM',
+        'FL',
+        'GA',
+        'GU',
+        'HI',
+        'ID',
+        'IL',
+        'IN',
+        'IA',
+        'KS',
+        'KY',
+        'LA',
+        'ME',
+        'MH',
+        'MD',
+        'MA',
+        'MI',
+        'MN',
+        'MS',
+        'MO',
+        'MT',
+        'NE',
+        'NV',
+        'NH',
+        'NJ',
+        'NM',
+        'NY',
+        'NC',
+        'ND',
+        'MP',
+        'OH',
+        'OK',
+        'OR',
+        'PW',
+        'PA',
+        'PR',
+        'RI',
+        'SC',
+        'SD',
+        'TN',
+        'TX',
+        'UT',
+        'VT',
+        'VI',
+        'VA',
+        'WA',
+        'WV',
+        'WI',
+        'WY',
+      ],
+    },
+    street: {
+      type: 'string',
+      minLength: 1,
+      maxLength: 30,
+      pattern: '^.*\\S.*',
+    },
+    street2: {
+      type: 'string',
+      maxLength: 30,
+    },
+    street3: {
+      type: 'string',
+      maxLength: 30,
+    },
+    city: {
+      type: 'string',
+      minLength: 1,
+      maxLength: 51,
+      pattern: '^.*\\S.*',
+    },
+    postalCode: {
+      type: 'string',
+      maxLength: 51,
+    },
+  },
+  required: ['street', 'city'],
+};
+
 const initialSchema = {
   type: 'object',
-  required: ['hasCommunityCareProvider'],
+  required: ['hasCommunityCareProvider', 'systemId'],
+  definitions: { address },
   properties: {
+    systemId: {
+      type: 'string',
+      enum: [],
+    },
     hasCommunityCareProvider: {
       type: 'boolean',
     },
-    communityCareProviders: {
-      type: 'array',
-      maxItems: 3,
-      minItems: 1,
-      items: {
-        type: 'object',
-        required: ['firstName', 'lastName', 'phone'],
-        properties: {
-          practiceName: {
-            type: 'string',
-          },
-          firstName: {
-            type: 'string',
-          },
-          lastName: {
-            type: 'string',
-          },
-          phone: {
-            type: 'string',
-            minLength: 10,
-          },
+    communityCareProvider: {
+      type: 'object',
+      required: ['firstName', 'lastName', 'phone'],
+      properties: {
+        practiceName: {
+          type: 'string',
+        },
+        firstName: {
+          type: 'string',
+        },
+        lastName: {
+          type: 'string',
+        },
+        address: addressUI(),
+        phone: {
+          type: 'string',
+          minLength: 10,
         },
       },
     },
