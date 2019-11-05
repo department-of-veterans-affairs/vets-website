9a785a2aa0acf63921d0faacca287daa252bfce0
Tony Wiliiams <tony.williams@va.gov>
2019-11-05 11:02:54 -0600

    Only show veterans CC / VA choice page if 1+ facility enabled for CC (#10966)
    * Draft changes.
    
    * Modified per code review.
    
    * Fixed failing test.
    
    * Modified per code review.
    
    * Move skip typeOfFacility logic from action to newAppointmentFlow.js.  Updated unit tests
    
    * Update reducer
    
    * Remove unused imports
    
    * Update unit test
    
    * Update unit test
    
    * Rename action
    
    * Remove unused action constants
    
    * Remove unused action constants
    
    * Reuse getUserSystems() action instead of performing a separate fetch in openFacilityPage()
    
    * Add getUserSystems to unit test
    
    * Move getUserSystems into newAppointmentFlow
    
    * Clean up debugger and unused vars
    
    * Clean up debugger and unused vars
    
    * Update tests
    
    * Remove whitespace
    
    * Remove unused parameter
    
    * Memoize getSystemIdentifiers().  Use it instead of getUserSystems action
    
    * Remove excess comments
    
    * Cleanup
    
    * Update test to account for memoized getSystemIdentifiers
    
    * Clean up check for cc enabled system

--- src/applications/vaos/api/index.js
+++ src/applications/vaos/api/index.js
@@ -15,6 +15,7 @@ import mockFacility984Data from './facilities_984.json';
 import mockClinicList from './clinicList983.json';
 import mockPACT from './pact.json';
 import mockCancelReasons from './cancel_reasons.json';
+import sitesSupportingVAR from './sites-supporting-var.json';
 
 // This wil go away once we stop mocking api calls
 const TEST_TIMEOUT = navigator.userAgent === 'node.js' ? 1 : null;
@@ -67,13 +68,24 @@ export const getPastAppointments = (() => {
 })();
 
 // GET /vaos/systems
-export function getSystemIdentifiers() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(mockSystems);
-    }, TEST_TIMEOUT || 600);
-  });
-}
+export const getSystemIdentifiers = (() => {
+  let promise = null;
+
+  return () => {
+    if (!promise) {
+      promise = new Promise(resolve => {
+        setTimeout(() => {
+          resolve(
+            mockSystems
+              .filter(id => id.assigningAuthority.startsWith('dfn'))
+              .map(id => id.assigningCode),
+          );
+        }, TEST_TIMEOUT || 600);
+      });
+    }
+    return promise;
+  };
+})();
 
 // GET /vaos/facilities
 // eslint-disable-next-line no-unused-vars
@@ -199,6 +211,14 @@ export function getFacilityInfo(facilityId) {
   return apiRequest(`/facilities/va/vha_${getStagingId(facilityId)}`).then(
     resp => resp.data,
   );
+}
+
+export function getSitesSupportingVAR() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(sitesSupportingVAR);
+    }, TEST_TIMEOUT || 1500);
+  });
 }
 
 export function getAvailableSlots() {
