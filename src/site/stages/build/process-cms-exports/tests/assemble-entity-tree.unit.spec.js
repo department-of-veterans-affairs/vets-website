59d3e1a7978095f81f24d95381c96314202e9791
Christopher Valarida <chris.valarida@adhocteam.us>
2019-11-04 16:09:23 -0800

    Add assembleEntityTree tests

--- src/site/stages/build/process-cms-exports/tests/assemble-entity-tree.unit.spec.js
+++ src/site/stages/build/process-cms-exports/tests/assemble-entity-tree.unit.spec.js
@@ -1,15 +1,18 @@
 const path = require('path');
-// const fs = require('fs');
+const fs = require('fs');
 const { expect } = require('chai');
+const chalk = require('chalk');
 
 const entityMap = require('./entities');
 
 const entityDir = path.join(__dirname, 'entities');
-// const assembleEntityTree = require('../index')(entityDir);
+const assembleEntityTree = require('../index')(entityDir);
 
 const { getContentModelType, readEntity } = require('../helpers');
+const { getEntityTransformer } = require('../transform');
 
-// const transformedDir = path.join(__dirname, 'transformed-entities');
+const transformedEntitiesDir = path.join(__dirname, 'transformed-entities');
+const transformedEntitiesList = fs.readdirSync(transformedEntitiesDir);
 
 Object.entries(entityMap).forEach(([contentModelType, entityFileName]) => {
   describe(contentModelType, () => {
@@ -24,6 +27,34 @@ Object.entries(entityMap).forEach(([contentModelType, entityFileName]) => {
 
     // Test 2: Look for a post-transformation json file matching the entity type
     //         If one exists, run the transformation on it and compare the result
-    it('should transform to match the test output', () => {});
+    if (getEntityTransformer(contentModelType, false)) {
+      it('should transform to match the test output', () => {
+        const transformedFileName = `${contentModelType}.json`;
+
+        // We have a transformer, but no transformed JSON file;
+        // Output what we think the transformed entity should look like
+        if (!transformedEntitiesList.includes(transformedFileName)) {
+          process.stdout.write(
+            `${chalk.red(
+              `No transformed entity JSON file found for ${contentModelType}.\n`,
+            )}\nassembleEntityTree result for ${contentModelType}:\n${JSON.stringify(
+              assembleEntityTree(entity),
+              null,
+              2,
+            )}\n`,
+          );
+          throw new Error(
+            `No transformed entity JSON file found for ${contentModelType}`,
+          );
+        }
+
+        const transformedEntity = JSON.parse(
+          fs.readFileSync(
+            path.join(transformedEntitiesDir, transformedFileName),
+          ),
+        );
+        expect(assembleEntityTree(entity)).to.deep.equal(transformedEntity);
+      });
+    }
   });
 });
