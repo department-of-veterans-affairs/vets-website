86613404a59b6a11ea2e3a8cfd9a457f14b65c92
Chris Valarida <chris.valarida@adhocteam.us>
2019-11-06 13:37:45 -0800

    Assemble entity tree test scaffolding (#11016)
    * getContentModelType returns a hyphenated string
    
    * Use 'node-page' as the content model name
    
    * Flatten index of test files
    
    * Flatten entities
    
    * Rename flat-entities -> entities
    
    * getContentModelType handles more
    
    * Refactor assembleEntityTree to read from any content directory
    
    * Use filenames instead of requiring the entities
    
    * Test that all entity types in the map are what they say they are
    
    * Use hyphenated content model name in transformers
    
    * Export getEntityTransformer
    
    * Add verbosity flag to getEntityTransformer
    
    * Use empty transformer in transformEntity
    
    * Add assembleEntityTree tests
    
    * node-page assembler test
    
    * Export the property names that the subtypes can be found in
    
    * Don't try to expand the type properties
    
    * Be clearer, man!
    
    * If there are no filters, return the whole raw entity
    
    * Testing, testing, one, two, three...
    
    * Add more test JSON files so we can expand the node-page fully
    
    * Now that the expansion works again, update the test file

--- src/site/stages/build/process-cms-exports/helpers.js
+++ src/site/stages/build/process-cms-exports/helpers.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const get = require('lodash/get');
 
 /**
  * This assumes the tome-sync output is sibling to the vets-website
@@ -11,24 +12,33 @@ const contentDir = path.join(
 );
 
 /**
+ * The sub-type can be found in a few different properties, depending
+ * on the entity
+ */
+const typeProperties = ['type', 'bundle', 'vid'];
+
+/**
  * Get the content model type of an entity. This is used for
  * determining how to handle specific entities. For example, in some
  * node entities, we want to ignore certain properties to avoid
  * circular references.
  *
- * @param {String} entityType - The type of entity; corresponds to
- *                              the name of the file. We may not end
- *                              up using this.
  * @param {Object} entity - The contents of the entity itself before
  *                          reference expansion.
  *
- * @return {String} - The content model type
+ * @return {String} - The content model type like 'node-page'
  */
 function getContentModelType(entity) {
-  return entity.type ? entity.type[0].target_id : entity.baseType;
+  const subType = typeProperties.reduce(
+    (foundType, tp) => foundType || get(entity, `${tp}[0].target_id`),
+    null,
+  );
+  return [entity.baseType, subType].filter(x => x).join('-');
 }
 
 module.exports = {
+  contentDir,
+  typeProperties,
   getContentModelType,
 
   /**
@@ -52,10 +62,10 @@ module.exports = {
    *
    * @return {Object} - The contents of the file.
    */
-  readEntity(baseType, uuid) {
+  readEntity(dir, baseType, uuid) {
     const entity = JSON.parse(
       fs
-        .readFileSync(path.join(contentDir, `${baseType}.${uuid}.json`))
+        .readFileSync(path.join(dir, `${baseType}.${uuid}.json`))
         .toString('utf8'),
     );
     // Add what we already know about the entity
