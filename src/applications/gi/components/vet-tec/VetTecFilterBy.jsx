4d2c5ef269ee33dc4bbaf1fdbdc5681297387216
Matt Roth <roth_matthew@bah.com>
2019-11-06 14:03:52 -0500

    qa feedback for 19918

--- src/applications/gi/components/vet-tec/VetTecFilterBy.jsx
+++ src/applications/gi/components/vet-tec/VetTecFilterBy.jsx
@@ -46,7 +46,7 @@ class VetTecFilterBy extends React.Component {
     renderLearnMoreLabel({
       modal: 'preferredProvider',
       showModal: this.props.showModal,
-      ariaLabel: ariaLabels.preferredProvider,
+      ariaLabel: ariaLabels.learnMore.preferredProvider,
       component: this,
     });
 
@@ -75,7 +75,7 @@ class VetTecFilterBy extends React.Component {
   render() {
     const label = (
       <span className="preferred-flag">
-        Preferred provider&nbsp;&nbsp;
+        Preferred providers&nbsp;&nbsp;
         <i className="fa fa-star vads-u-color--gold" />
       </span>
     );
