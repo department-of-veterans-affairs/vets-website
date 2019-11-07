26ae5f30da8b2df6ce0217e2ac02d52d80fbdac5
Christopher Valarida <chris.valarida@adhocteam.us>
2019-11-06 17:32:55 -0800

    Add all the schema files to ajv validator

--- src/site/stages/build/process-cms-exports/validator.js
+++ src/site/stages/build/process-cms-exports/validator.js
@@ -1,20 +1,25 @@
+const fs = require('fs');
+const path = require('path');
 const Ajv = require('ajv');
 
 const ajv = new Ajv();
 
-const {
-  entityReference,
-  entityReferenceArray,
-} = require('./schemas/common/entity-reference');
-const genericNestedString = require('./schemas/common/generic-nested-string');
-
 // Custom formats
 ajv.addFormat('custom-date-time', /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/);
 
-// Common $ref schemas
-ajv.addSchema(entityReference);
-ajv.addSchema(entityReferenceArray);
-ajv.addSchema(genericNestedString);
+/**
+ * Loads all the schemas in a directory into ajv
+ */
+const loadSchemas = dir => {
+  fs.readdirSync(dir).forEach(fileName =>
+    // eslint-disable-next-line import/no-dynamic-require
+    ajv.addSchema(require(path.join(dir, fileName))),
+  );
+};
+
+loadSchemas(path.join(__dirname, 'schemas', 'common'));
+loadSchemas(path.join(__dirname, 'schemas', 'raw'));
+loadSchemas(path.join(__dirname, 'schemas', 'transformed'));
 
 module.exports = (entity, schema) => {
   const validate = ajv.compile(schema);
