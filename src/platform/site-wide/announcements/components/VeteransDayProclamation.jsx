c5e16f9552ecd89dc7aa4bd2ef8b1c66d83cb3b2
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-11-07 11:37:17 -0500

    [PromoBanner] Open VeteransDayProclamation in a new tab

--- src/platform/site-wide/announcements/components/VeteransDayProclamation.jsx
+++ src/platform/site-wide/announcements/components/VeteransDayProclamation.jsx
@@ -8,6 +8,7 @@ export default function VeteransDayProclamation({ dismiss }) {
     <PromoBanner
       type={PROMO_BANNER_TYPES.announcement}
       onClose={dismiss}
+      target="_blank"
       href="https://www.va.gov/opa/vetsday/docs/National_Veterans_and_Military_Families_Month_Proclamation_2019.pdf"
       text="Read President Donald Trump's proclamation celebrating National Veterans and Military Families Month"
     />
