3e943ab44e608adc4e24b37ae72db5b80b6c6bb5
Devin McCurdy <mccurdy_devin@bah.com>
2019-11-08 10:50:19 -0500

    WIP

--- src/applications/static-pages/school-resources/SchoolResources.jsx
+++ src/applications/static-pages/school-resources/SchoolResources.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import scoEvents from './constants/events.json';
 
-export default async function createScoEventListWidget() {
+export async function createScoEventListWidget() {
   const widgets = Array.from(
     document.querySelectorAll(`
     [data-widget-type="sco-event-list"]`),
@@ -16,6 +16,27 @@ export default async function createScoEventListWidget() {
 
     widgets.forEach(el => {
       ReactDOM.render(<ScoEventListWidget scoEvents={scoEvents} />, el);
+    });
+  }
+}
+
+export async function createScoAnnouncementsWidget() {
+  const widgets = Array.from(
+    document.querySelectorAll(`
+    [data-widget-type="sco-event-list"]`),
+  );
+
+  if (widgets.length) {
+    const {
+      default: ScoAnnouncementsWidget,
+    } = await import(/* webpackChunkName: "sco-announcements" */ './ScoAnnouncementsWidget');
+    const announcements = await import(/* webpackChunkName: "sco-announcements-data" */ './ScoAnnouncementsWidget');
+
+    widgets.forEach(el => {
+      ReactDOM.render(
+        <ScoAnnouncementsWidget accouncements={announcements} />,
+        el,
+      );
     });
   }
 }
---
+++ src/applications/static-pages/school-resources/ScoAnnouncementsWidget.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import announcements from './constants/announcements.json';
+
+export default class ScoAnnouncementsWidget extends React.Component {
+  static propTypes = {
+    scoEvents: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+      }),
+    ),
+  };
+
+  displayDate = event => {
+    const eventMonths = [
+      ...new Set([
+        moment(event.eventStartDate).format('MMMM'),
+        moment(event.eventEndDate).format('MMMM'),
+      ]),
+    ];
+    const eventYears = [
+      ...new Set([
+        moment(event.eventStartDate).format('Y'),
+        moment(event.eventEndDate).format('Y'),
+      ]),
+    ];
+
+    if (!event.eventEndDate) {
+      return `${moment(event.eventStartDate).format('MMMM D, Y')}`;
+    } else if (eventYears.length === 2) {
+      return `${moment(event.eventStartDate).format('MMMM D Y')} - ${moment(
+        event.eventEndDate,
+      ).format('MMMM D Y')}`;
+    }
+    return eventMonths.length === 2
+      ? `${moment(event.eventStartDate).format('MMMM D')} - ${moment(
+          event.eventEndDate,
+        ).format('MMMM D')}, ${eventYears[0]}`
+      : `${moment(event.eventStartDate).format('MMMM D')} - ${moment(
+          event.eventEndDate,
+        ).format('D')}, ${eventYears[0]}`;
+  };
+
+  eventComparer = (eventA, eventB) =>
+    moment(eventA.eventStartDate).isBefore(moment(eventB.eventStartDate))
+      ? -1
+      : 1;
+
+  shouldDisplay = event =>
+    moment(event.displayStartDate).isBefore(moment()) &&
+    ((event.displayEndDate &&
+      moment().isBefore(moment(event.displayEndDate))) ||
+      moment().isBefore(moment(event.eventStartDate).add(30, 'days')));
+
+  renderEvents = () => {
+    const scoEvents = this.props.scoEvents
+      .filter(this.shouldDisplay)
+      .sort(this.eventComparer)
+      .map((scoEvent, index) => (
+        <li key={index} className="hub-page-link-list__item">
+          <a href={scoEvent.url}>
+            <b>{`${scoEvent.name} >`}</b>
+          </a>
+          <br />
+          <b>{`${this.displayDate(scoEvent)} — ${scoEvent.location}`}</b>
+        </li>
+      ));
+    return (
+      <ul id="get" className="hub-page-link-list">
+        {scoEvents}
+      </ul>
+    );
+  };
+
+  render() {
+    if (!this.props.scoEvents) {
+      return null;
+    }
+
+    return (
+      <div>
+        <h2 id="upcoming-events">Upcoming events</h2>
+        {this.renderEvents()}
+        <p>
+          See full list of{' '}
+          <a href="https://www.benefits.va.gov/gibill/resources/education_resources/school_certifying_officials/conferences_and_events.asp">
+            Conferences and Events
+          </a>{' '}
+          |{' '}
+          <a href="https://www.benefits.va.gov/gibill/resources/education_resources/school_certifying_officials/presentations.asp">
+            Training Webinars
+          </a>{' '}
+        </p>
+      </div>
+    );
+  }
+}
