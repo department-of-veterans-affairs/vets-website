6bc9696bb64113042b8e72e7e41f276557661f7a
Devin McCurdy <mccurdy_devin@bah.com>
2019-11-08 10:54:14 -0500

    Async load of event data

--- src/applications/static-pages/school-resources/SchoolResources.jsx
+++ src/applications/static-pages/school-resources/SchoolResources.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import scoEvents from './constants/events.json';
-
 export default async function createScoEventListWidget() {
   const widgets = Array.from(
     document.querySelectorAll(`
@@ -13,6 +11,9 @@ export default async function createScoEventListWidget() {
     const {
       default: ScoEventListWidget,
     } = await import(/* webpackChunkName: "sco-event-list" */ './ScoEventListWidget');
+    const {
+      default: scoEvents,
+    } = await import(/* webpackChunkName: "sco-event-list-data" */ './constants/events.json');
 
     widgets.forEach(el => {
       ReactDOM.render(<ScoEventListWidget scoEvents={scoEvents} />, el);
