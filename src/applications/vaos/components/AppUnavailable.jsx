c3ce507be8f184ae20ecc8c0174486c24c308aba
Jeff Balboni <jeff@adhocteam.us>
2019-11-12 10:38:39 -0500

    Add tests

--- src/applications/vaos/components/AppUnavailable.jsx
+++ src/applications/vaos/components/AppUnavailable.jsx
@@ -10,7 +10,7 @@ export default function AppUnavailable() {
         <div className="vads-l-col--12 medium-screen:vads-l-col--8 vads-u-margin-bottom--4">
           <AlertBox
             status="warning"
-            headline="Sorry, the new VA online scheduling application isn't available right now"
+            headline="We’re sorry, the new VA online scheduling application isn’t available right now"
             className="vads-u-margin-top--0"
           >
             If you need to schedule an appointment, please contact your{' '}
