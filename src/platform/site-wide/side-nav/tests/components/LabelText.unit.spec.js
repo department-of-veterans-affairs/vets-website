2ac2cc9eb8341338f473940d8cba1161589832dd
Kelson Adams <kadams@governmentcio.com>
2019-11-15 09:27:33 -0500

    Add unit tests for LabelText

---
+++ src/platform/site-wide/side-nav/tests/components/LabelText.unit.spec.js
@@ -0,0 +1,46 @@
+// Dependencies
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+// Relative
+import LabelText from '../../components/LabelText';
+
+describe('<LabelText>', () => {
+  it('should render an <a /> tag when a link is provided.', () => {
+    const defaultProps = {
+      item: {
+        description: 'Some description',
+        expanded: false,
+        hasChildren: false,
+        href: '/pittsburgh-health-care',
+        id: '00c9a1ff-3550-4f54-9239-c769fc6edab1',
+        label: 'Location',
+        order: 0,
+        parentID: '8e5668a6-90d9-4913-bc9f-c8f6788d7a2e',
+      },
+    };
+
+    const wrapper = shallow(<LabelText {...defaultProps} />);
+    expect(wrapper.name()).to.equal('a');
+    wrapper.unmount();
+  });
+
+  it('should render a <div /> tag when a link is not provided.', () => {
+    const defaultProps = {
+      item: {
+        description: 'Some description',
+        expanded: true,
+        hasChildren: true,
+        href: '',
+        id: '00c9a1ff-3550-4f54-9239-c769fc6edab1',
+        label: 'Location',
+        order: 0,
+        parentID: '8e5668a6-90d9-4913-bc9f-c8f6788d7a2e',
+      },
+    };
+
+    const wrapper = shallow(<LabelText {...defaultProps} />);
+    expect(wrapper.name()).to.not.equal('div');
+    wrapper.unmount();
+  });
+});
