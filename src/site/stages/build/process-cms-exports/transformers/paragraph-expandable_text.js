809fe4f1273083164fca8c27c3f0d588fd248cd7
Christopher Valarida <chris.valarida@adhocteam.us>
2019-11-15 11:48:24 -0800

    The base object contains _only_ entity

--- src/site/stages/build/process-cms-exports/transformers/paragraph-expandable_text.js
+++ src/site/stages/build/process-cms-exports/transformers/paragraph-expandable_text.js
@@ -2,12 +2,12 @@ const { getDrupalValue } = require('./helpers');
 
 const transform = entity => {
   const { fieldWysiwyg, fieldTextExpander } = entity;
-  return Object.assign({}, entity, {
+  return {
     entity: {
       fieldWysiwyg: { processed: getDrupalValue(fieldWysiwyg) },
       fieldTextExpander: getDrupalValue(fieldTextExpander),
     },
-  });
+  };
 };
 
 module.exports = transform;
