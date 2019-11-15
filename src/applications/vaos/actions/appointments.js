3c19b40eb01dcc50bc65ee0411cf15413cc44d5b
Jeff Balboni <jeff@adhocteam.us>
2019-11-15 10:00:50 -0500

    Send time part of date for confirmed va appts (#11104)

--- src/applications/vaos/actions/appointments.js
+++ src/applications/vaos/actions/appointments.js
@@ -70,21 +70,24 @@ export function fetchFutureAppointments() {
         const data = await Promise.all([
           getConfirmedAppointments(
             'va',
-            moment().format('YYYY-MM-DD'),
             moment()
-              .add(4, 'months')
-              .format('YYYY-MM-DD'),
+              .startOf('day')
+              .toISOString(),
+            moment()
+              .startOf('day')
+              .add(120, 'days')
+              .toISOString(),
           ),
           getConfirmedAppointments(
             'cc',
             moment().format('YYYY-MM-DD'),
             moment()
-              .add(4, 'months')
+              .add(120, 'days')
               .format('YYYY-MM-DD'),
           ),
           getPendingAppointments(
             moment()
-              .subtract(4, 'months')
+              .subtract(30, 'days')
               .format('YYYY-MM-DD'),
             moment().format('YYYY-MM-DD'),
           ),
