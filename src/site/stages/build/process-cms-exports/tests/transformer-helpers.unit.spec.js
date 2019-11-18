7cd3c7b89528f70c1205809ae0dfea6551867495
Christopher Valarida <chris.valarida@adhocteam.us>
2019-11-18 11:58:27 -0800

    Simplify getWysiwigString
    Turns out the wysiwyg transformation isn't consistent from the raw ->
    pages.json, so...let's keep things simple for now.

--- src/site/stages/build/process-cms-exports/tests/transformer-helpers.unit.spec.js
+++ src/site/stages/build/process-cms-exports/tests/transformer-helpers.unit.spec.js
@@ -8,29 +8,9 @@ describe('CMS export transformer helpers', () => {
         '<p>If you need support for a specific mental health problem\u2014or if you\u2019re having problems sleeping, controlling your anger, or readjusting to civilian life\u2014you are not alone. And we can help.</p>\r\n\r\n';
 
       const transformed =
-        '<p>If you need support for a specific mental health problem—or if you’re having problems sleeping, controlling your anger, or readjusting to civilian life—you are not alone. And we can help.</p>';
+        '<p>If you need support for a specific mental health problem—or if you’re having problems sleeping, controlling your anger, or readjusting to civilian life—you are not alone. And we can help.</p>\r\n\r\n';
 
       expect(getWysiwygString(raw)).to.equal(transformed);
-    });
-
-    it('should unescape unicode code points', () => {
-      expect(getWysiwygString('\u2014')).to.equal('—');
-    });
-
-    it('should unescape html entities', () => {
-      expect(getWysiwygString('&amp;')).to.equal('&');
-    });
-
-    it('should remove \\r\\n', () => {
-      expect(getWysiwygString('\r\n\r\n')).to.equal('');
-    });
-
-    it('should remove \\t', () => {
-      expect(getWysiwygString('\t')).to.equal('');
-    });
-
-    it('should not remove \\n', () => {
-      expect(getWysiwygString('\n')).to.equal('\n');
     });
   });
 });
