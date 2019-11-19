38a773fe0a99d05b48c7388b613180a29c43d318
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2019-11-19 10:34:22 -0600

    Fix input typeahead loses focus
    - Provide default selected key when selected element is null otherwise on rerendering when states updates the key is null again and the focus moves

--- src/applications/facility-locator/components/ServiceTypeAhead.jsx
+++ src/applications/facility-locator/components/ServiceTypeAhead.jsx
@@ -66,7 +66,7 @@ class ServiceTypeAhead extends Component {
             clearSelection();
           }
         }}
-        key={defaultSelectedItem}
+        key={defaultSelectedItem || 'defaultSelectedItem'}
       >
         {({
           getInputProps,
