69f542a547af0c0ae3eab61d48a1c643c32a2847
Joe <elder_joseph@bah.com>
2019-11-19 10:46:40 -0500

    3540: GIBCT Section 116 - Benefit amounts wrap to two lines when zoomed above 300%

--- src/applications/gi/components/vet-tec/VetTecCalculator.jsx
+++ src/applications/gi/components/vet-tec/VetTecCalculator.jsx
@@ -73,7 +73,9 @@ export class VetTecCalculator extends React.Component {
           <h4 className="vads-u-font-size--h5">Tuition & fees:</h4>
         </div>
         <div className="small-6 columns vads-u-text-align--right">
-          <h5>{outputs.vetTecTuitionFees}</h5>
+          <h5 className="estimated-benefit-values">
+            {outputs.vetTecTuitionFees}
+          </h5>
         </div>
       </div>
       {this.renderScholarshipBenefitSection(outputs)}
@@ -92,7 +94,9 @@ export class VetTecCalculator extends React.Component {
           </div>
         </div>
         <div className="small-4 columns vads-u-text-align--right">
-          <div>{outputs.vaPaysToProvider}</div>
+          <div className="estimated-benefit-values">
+            {outputs.vaPaysToProvider}
+          </div>
         </div>
       </div>
       <div className={this.indentVaPaysToProvider()}>
@@ -101,7 +105,9 @@ export class VetTecCalculator extends React.Component {
             <div>Upon enrollment in program (25%):</div>
           </div>
           <div className="small-3 xsmall-screen:small-2 vads-u-text-align--right columns value">
-            <div>{outputs.quarterVetTecPayment}</div>
+            <div className="estimated-benefit-values">
+              {outputs.quarterVetTecPayment}
+            </div>
           </div>
         </div>
         <div className="row vads-u-margin-top--0p5 small-screen:vads-u-padding-right--7">
@@ -109,7 +115,9 @@ export class VetTecCalculator extends React.Component {
             <div>Upon completion of program (25%):</div>
           </div>
           <div className="small-3 xsmall-screen:small-2 vads-u-text-align--right columns value">
-            <div>{outputs.quarterVetTecPayment}</div>
+            <div className="estimated-benefit-values">
+              {outputs.quarterVetTecPayment}
+            </div>
           </div>
         </div>
         <div className="row vads-u-margin-top--0p5 small-screen:vads-u-padding-right--7">
@@ -117,7 +125,9 @@ export class VetTecCalculator extends React.Component {
             <div>Upon employment (50%):</div>
           </div>
           <div className="small-3 xsmall-screen:small-2 vads-u-text-align--right columns value">
-            <div>{outputs.halfVetTecPayment}</div>
+            <div className="estimated-benefit-values">
+              {outputs.halfVetTecPayment}
+            </div>
           </div>
         </div>
       </div>
@@ -128,7 +138,7 @@ export class VetTecCalculator extends React.Component {
           </h4>
         </div>
         <div className="small-6 columns vads-u-text-align--right">
-          <h5 className="vads-u-font-family--sans">
+          <h5 className="vads-u-font-family--sans estimated-benefit-values">
             {outputs.outOfPocketTuitionFees}
           </h5>
         </div>
