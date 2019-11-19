2ff6adef8047b7e2312145037ff475844d76e68d
elder-joseph <48804834+elder-joseph@users.noreply.github.com>
2019-11-19 11:48:50 -0500

    Bah 19985 (#11113)
    * conditionally change search results link
    
    * check for vettec facility code by index
    
    * fix vettec logo sizing and position when in single-column display
    
    * adjust sizing to be less than 641px
    
    * 19: Benefits estimator is shifting to botton of calculator fields from display sizes of 1200px to 768px
    
    * change column sizes for search results count row
    
    * 19985: Sub-story 19 fix benefits estimation styling
    
    * add reference for single-column-width

--- src/applications/gi/containers/VetTecSearchPage.jsx
+++ src/applications/gi/containers/VetTecSearchPage.jsx
@@ -154,7 +154,7 @@ export class VetTecSearchPage extends React.Component {
     const resultsClass = classNames(
       'search-results',
       'small-12',
-      'usa-width-three-fourths medium-9',
+      'medium-9',
       'columns',
       { opened: !search.filterOpened },
     );
@@ -232,15 +232,15 @@ export class VetTecSearchPage extends React.Component {
     return (
       <ScrollElement name="searchPage" className="search-page">
         <div>
-          <div className="vads-u-display--block small-screen:vads-u-display--none vettec-logo-container">
+          <div className="vads-u-display--block single-column-display-none  vettec-logo-container">
             {renderVetTecLogo(classNames('vettec-logo'))}
           </div>
           <div className="vads-l-row vads-u-justify-content--space-between vads-u-align-items--flex-end">
-            <div className="vads-l-col--10 search-results-count">
+            <div className="vads-l-col--9 search-results-count">
               {this.renderSearchResultsHeader(this.props.search)}
             </div>
-            <div className="vads-l-col--2">
-              <div className="vads-u-display--none small-screen:vads-u-display--block vettec-logo-container">
+            <div className="vads-l-col--3">
+              <div className="vads-u-display--none single-column-display-block vettec-logo-container">
                 {renderVetTecLogo(classNames('vettec-logo'))}
               </div>
             </div>
