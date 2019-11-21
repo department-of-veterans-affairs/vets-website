a021324afdebb0c96dbe138431a76b01191f6234
Kelson Adams <kadams@governmentcio.com>
2019-11-21 10:12:54 -0500

    Do not render duplicate label + line when the nav item !hasChildren

--- src/platform/site-wide/side-nav/components/DuplicateLineLabel.js
+++ src/platform/site-wide/side-nav/components/DuplicateLineLabel.js
@@ -10,14 +10,25 @@ import LabelText from './LabelText';
 const DuplicateLineLabel = ({ depth, item }) => {
   // Derive the item properties.
   const expanded = get(item, 'expanded');
+  const hasChildren = get(item, 'hasChildren');
   const href = get(item, 'href');
   const isSelected = get(item, 'isSelected');
 
   // Determine if the nav item is 2nd level.
   const isSecondLevel = depth === 2;
 
-  // Do not render if we are collapsed or if the nav item is not 2nd level.
-  if (!expanded || !isSecondLevel) {
+  // Do not render if we are collapsed.
+  if (!expanded) {
+    return null;
+  }
+
+  // Do not render if the nav item is not 2nd level.
+  if (!isSecondLevel) {
+    return null;
+  }
+
+  // Do not render if the nav item has no children.
+  if (!hasChildren) {
     return null;
   }
 
