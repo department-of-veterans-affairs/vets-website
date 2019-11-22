cf03c932bb7ce5ad8266ecaf25f72a4dde5df1da
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-11-22 11:03:23 -0500

    Scaffold the search form

--- src/applications/find-va-forms/components/FindVaForms.jsx
+++ src/applications/find-va-forms/components/FindVaForms.jsx
@@ -6,7 +6,7 @@ import TopTasks from './TopTasks';
 export default function() {
   return (
     <>
-      <div className="vads-u-padding--2 vads-u-background-color--gray-lightest">
+      <div className="vads-u-padding--4 vads-u-background-color--gray-lightest">
         <SearchForm />
         <SearchResults />
       </div>
--- src/applications/find-va-forms/containers/SearchForm.jsx
+++ src/applications/find-va-forms/containers/SearchForm.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
 function SearchForm() {
-  return <div />;
+  return (
+    <form
+      name="find-va-form"
+      className="vads-l-grid-container vads-u-padding--0"
+    >
+      <label htmlFor="va-form-query" className="vads-u-margin--0">
+        Keyword, form name, or number
+      </label>
+      <div className="vads-l-row">
+        <div style={{ flexGrow: 1 }} className="vads-u-margin-right--2">
+          <input
+            className="usa-input vads-u-max-width--100 vads-u-width--full"
+            name="va-form-query"
+            type="text"
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="usa-button vads-u-margin--0 vads-u-margin-y--1"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default SearchForm;
