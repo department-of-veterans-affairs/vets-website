ca0cf333034cb1dec2fc7dc54cac80a58e4203cf
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-11-22 10:37:15 -0500

    Add HREF values

--- src/applications/find-va-forms/components/TopTasks.jsx
+++ src/applications/find-va-forms/components/TopTasks.jsx
@@ -63,14 +63,17 @@ export default function TopTasks() {
         <Card
           heading="File a VA disability claim"
           description="Equal to VA Form 21-526EZ"
+          href="/disability/file-disability-claim-form-21-526ez"
         />
         <Card
           heading="Apply for the GI Bill and other education benefits"
           description="Equal to VA Forms 22-1990 and 22-1995"
+          href="/education/apply-for-education-benefits/application/1990"
         />
         <Card
           heading="Apply for VA health care benefits"
           description="Equal to VA Form 10-10EZ"
+          href="/health-care/apply/application"
         />
       </CardRow>
     </>
