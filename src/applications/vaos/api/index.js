8ce558a2f6f1eeb3765b2fd194f2615f82015cc9
Jeff Balboni <jeff@adhocteam.us>
2019-11-22 09:35:13 -0500

    Connect cancel req api

--- src/applications/vaos/api/index.js
+++ src/applications/vaos/api/index.js
@@ -360,14 +360,19 @@ export function updateAppointment(appt) {
   return promise;
 }
 
-// PUT /vaos/requests
-// eslint-disable-next-line no-unused-vars
-export function updateRequest(appt) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve();
-    }, 500);
-  });
+export function updateRequest(req) {
+  let promise;
+  if (USE_MOCK_DATA) {
+    promise = Promise.resolve();
+  } else {
+    promise = apiRequest(`/vaos/appointment_requests/${req.uniqueId}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(req),
+    });
+  }
+
+  return promise;
 }
 
 export function submitRequest(type, request) {
