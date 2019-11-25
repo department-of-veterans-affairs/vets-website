43414b10fb4008c40fe8e63881f31f1594a18fa4
Christopher Valarida <chris.valarida@adhocteam.us>
2019-11-25 12:45:19 -0800

    Add paragraph-alert schemas

---
+++ src/site/stages/build/process-cms-exports/schemas/transformed/paragraph-alert.js
@@ -0,0 +1,20 @@
+module.exports = {
+  type: 'object',
+  properties: {
+    contentModelType: { type: 'string', enum: ['paragraph-alert'] },
+    entityType: { type: 'string' },
+    entityBundle: { type: 'string' },
+    fieldAlertType: { type: 'string' },
+    fieldAlertHeading: { type: 'string' },
+    fieldVaParagraphs: {
+      type: 'array',
+      items: { $ref: 'Paragraph' },
+    },
+    fieldAlertBlockReference: {
+      type: ['null', 'array'],
+      items: {
+        $ref: 'BlockContent',
+      },
+    },
+  },
+};
