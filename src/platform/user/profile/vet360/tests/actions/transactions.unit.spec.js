f09adc3a8809923b0863d986979fd8cf7765942f
Brad Conley <bradl3yC@users.noreply.github.com>
2019-11-25 11:20:56 -0500

    Add missing keys to mock data (#11179)

---
+++ src/platform/user/profile/vet360/tests/actions/transactions.unit.spec.js
@@ -0,0 +1,154 @@
+import {
+  validateAddress,
+  ADDRESS_VALIDATION_CONFIRM,
+} from '../../actions/transactions';
+import sinon from 'sinon';
+import { expect } from 'chai';
+
+import { mockApiRequest } from 'platform/testing/unit/helpers.js';
+
+const route = 'foo';
+const method = 'PUT';
+const fieldName = { value: 'mailingAddress' };
+const payload = {
+  id: 123,
+  addressLine1: '1493 Martin Luther King Rd',
+  addressLine2: 'string',
+  addressLine3: 'string',
+  addressType: 'DOMESTIC',
+  city: 'Fulton',
+  countryName: 'United States',
+  stateCode: 'NY',
+  zipCode: '97062',
+  addressPou: 'CORRESPONDENCE',
+};
+const analyticsSectionName = 'bar';
+
+describe('validateAddress', () => {
+  it('verify return data', () => {
+    const dispatch = sinon.spy();
+    const mockReturn = {
+      addresses: [
+        {
+          address: {
+            addressLine1: '400 N 65th St',
+            addressType: 'DOMESTIC',
+            city: 'Seattle',
+            countryName: 'USA',
+            countryCodeIso3: 'USA',
+            countyCode: '53033',
+            countyName: 'King',
+            stateCode: 'WA',
+            zipCode: '98103',
+            zipCodeSuffix: '5252',
+          },
+          addressMetaData: {
+            confidenceScore: 100.0,
+            addressType: 'Domestic',
+            deliveryPointValidation: 'UNDELIVERABLE',
+          },
+        },
+        {
+          address: {
+            addressLine1: '400 NW 65th St',
+            addressType: 'DOMESTIC',
+            city: 'Seattle',
+            countryName: 'USA',
+            countryCodeIso3: 'USA',
+            countyCode: '53033',
+            countyName: 'King',
+            stateCode: 'WA',
+            zipCode: '98117',
+            zipCodeSuffix: '5026',
+          },
+          addressMetaData: {
+            confidenceScore: 100.0,
+            addressType: 'Domestic',
+            deliveryPointValidation: 'CONFIRMED',
+            residentialDeliveryIndicator: 'RESIDENTIAL',
+          },
+        },
+        {
+          address: {
+            addressLine1: '400 NW 65th St',
+            addressType: 'DOMESTIC',
+            city: 'Seattle',
+            countryName: 'USA',
+            countryCodeIso3: 'USA',
+            countyCode: '53033',
+            countyName: 'King',
+            stateCode: 'WA',
+            zipCode: '98117',
+            zipCodeSuffix: '5026',
+          },
+          addressMetaData: {
+            confidenceScore: 100.0,
+            addressType: 'Domestic',
+            deliveryPointValidation: 'CONFIRMED',
+            residentialDeliveryIndicator: 'RESIDENTIAL',
+          },
+        },
+        {
+          address: {
+            addressLine1: '400 NE 65th St',
+            addressType: 'DOMESTIC',
+            city: 'Seattle',
+            countryName: 'USA',
+            countryCodeIso3: 'USA',
+            countyCode: '53033',
+            countyName: 'King',
+            stateCode: 'WA',
+            zipCode: '98115',
+            zipCodeSuffix: '6463',
+          },
+          addressMetaData: {
+            confidenceScore: 98.0,
+            addressType: 'Domestic',
+            deliveryPointValidation:
+              'STREET_NUMBER_VALIDATED_BUT_MISSING_UNIT_NUMBER',
+            residentialDeliveryIndicator: 'RESIDENTIAL',
+          },
+        },
+      ],
+      validationKey: 178464079,
+    };
+    mockApiRequest(mockReturn);
+    return validateAddress(
+      route,
+      method,
+      fieldName,
+      payload,
+      analyticsSectionName,
+    )(dispatch).then(() => {
+      expect(dispatch.firstCall.args[0].type).to.equal(
+        ADDRESS_VALIDATION_CONFIRM,
+      );
+      expect(dispatch.firstCall.args[0].suggestedAddresses).to.deep.equal([
+        {
+          addressLine1: '400 NW 65th St',
+          addressType: 'DOMESTIC',
+          city: 'Seattle',
+          countryName: 'USA',
+          countryCodeIso3: 'USA',
+          countyCode: '53033',
+          countyName: 'King',
+          stateCode: 'WA',
+          zipCode: '98117',
+          zipCodeSuffix: '5026',
+        },
+        {
+          addressLine1: '400 NW 65th St',
+          addressType: 'DOMESTIC',
+          city: 'Seattle',
+          countryName: 'USA',
+          countryCodeIso3: 'USA',
+          countyCode: '53033',
+          countyName: 'King',
+          stateCode: 'WA',
+          zipCode: '98117',
+          zipCodeSuffix: '5026',
+        },
+      ]);
+    });
+  });
+});
