965374b514fc7efc06a991bc0b70f717e8add973
Christopher Valarida <chris.valarida@adhocteam.us>
2019-11-26 12:16:50 -0800

    Nest the schema in entity; add required properties

--- src/site/stages/build/process-cms-exports/schemas/transformed/paragraph-alert.js
+++ src/site/stages/build/process-cms-exports/schemas/transformed/paragraph-alert.js
@@ -1,20 +1,35 @@
 module.exports = {
   type: 'object',
   properties: {
-    contentModelType: { type: 'string', enum: ['paragraph-alert'] },
-    entityType: { type: 'string' },
-    entityBundle: { type: 'string' },
-    fieldAlertType: { type: 'string' },
-    fieldAlertHeading: { type: 'string' },
-    fieldVaParagraphs: {
-      type: 'array',
-      items: { $ref: 'Paragraph' },
-    },
-    fieldAlertBlockReference: {
-      type: ['null', 'array'],
-      items: {
-        $ref: 'BlockContent',
+    entity: {
+      type: 'object',
+      properties: {
+        contentModelType: { type: 'string', enum: ['paragraph-alert'] },
+        entityType: { type: 'string' },
+        entityBundle: { type: 'string' },
+        fieldAlertType: { type: 'string' },
+        fieldAlertHeading: { type: 'string' },
+        fieldVaParagraphs: {
+          type: 'array',
+          items: { $ref: 'Paragraph' },
+        },
+        fieldAlertBlockReference: {
+          type: ['null', 'array'],
+          items: {
+            $ref: 'BlockContent',
+          },
+        },
       },
+      required: [
+        'contentModelType',
+        'entityType',
+        'entityBundle',
+        'fieldAlertType',
+        'fieldAlertHeading',
+        'fieldVaParagraphs',
+        'fieldAlertBlockReference',
+      ],
     },
   },
+  required: ['entity'],
 };
