43261a78ac5beb3ac7c670b4b09d295d7e5b7b62
Jeff Balboni <jeff@adhocteam.us>
2019-12-02 16:53:51 -0500

    Add more tests

--- src/applications/vaos/components/ErrorMessage.jsx
+++ src/applications/vaos/components/ErrorMessage.jsx
@@ -4,8 +4,10 @@ import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
 
 export default function ErrorMessage() {
   return (
-    <AlertBox status="error" headline="Sorry, something went wrong">
-      We're sorry, we ran into an error. Please try again later.
-    </AlertBox>
+    <div aria-atomic="true" aria-live="assertive">
+      <AlertBox status="error" headline="Sorry, something went wrong">
+        We're sorry, we ran into an error. Please try again later.
+      </AlertBox>
+    </div>
   );
 }
