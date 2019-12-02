d1f3a2e436baa2a4f7c880f73d5a86f6e559d256
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-12-02 14:37:55 -0500

    Add a filter for better API simulation

--- src/applications/find-va-forms/api/index.js
+++ src/applications/find-va-forms/api/index.js
@@ -4,9 +4,13 @@ import { normalizeFormsForTable } from '../helpers';
 
 // `fetchFormsApi` will need to be updated to make the request to api.va.gov once the endpoint is ready.
 // fetch(`https://api.va.gov/find-va-forms?q=${query}`);
-export const fetchFormsApi = () => {
+export const fetchFormsApi = (URL, query) => {
+  const forms = mockForms.data.filter(form =>
+    form.attributes.title?.toLowerCase().includes(query),
+  );
+
   // Normalize the forms data we get back from the API resopnse.
-  const normalizedForms = normalizeFormsForTable(mockForms?.data);
+  const normalizedForms = normalizeFormsForTable(forms);
 
   // Give back the normalized forms data.
   return normalizedForms;
