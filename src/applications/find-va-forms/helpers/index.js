b46c0152a7b9f662f6e1a207f226cc5cf9ca9160
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-12-03 11:20:30 -0500

    Add test for helper

--- src/applications/find-va-forms/helpers/index.js
+++ src/applications/find-va-forms/helpers/index.js
@@ -1,7 +1,7 @@
 // Dependencies.
 import React from 'react';
 
-export const normalizeFormsForTable = forms =>
+export const normalizeFormsForTable = ({ data: forms }) =>
   forms.map(form => {
     // Derive the form properties.
     const id = form?.id;
