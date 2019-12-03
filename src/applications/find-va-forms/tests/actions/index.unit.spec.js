1227fe8c679fe0bf18cfee45cc3b964e130b3894
Nick Sullivan <nick.sullivan@adhocteam.us>
2019-12-03 11:01:48 -0500

    Add test for thunk

--- src/applications/find-va-forms/tests/actions/index.unit.spec.js
+++ src/applications/find-va-forms/tests/actions/index.unit.spec.js
@@ -1,10 +1,13 @@
 // Dependencies.
 import { expect } from 'chai';
+import sinon from 'sinon';
+
 // Relative imports.
 import {
   fetchFormsAction,
   fetchFormsFailure,
   fetchFormsSuccess,
+  fetchFormsThunk,
 } from '../../actions';
 import {
   FETCH_FORMS,
@@ -44,6 +47,61 @@ describe('Find VA Forms actions', () => {
         response,
         type: FETCH_FORMS_SUCCESS,
       });
+    });
+  });
+
+  describe('fetchFormsThunk', () => {
+    let oldWindow;
+
+    beforeEach(() => {
+      oldWindow = global.window;
+
+      global.window = {
+        location: {
+          search: '',
+          pathname: '',
+        },
+        history: {
+          replaceState: sinon.stub(),
+        },
+      };
+    });
+
+    afterEach(() => {
+      global.window = oldWindow;
+    });
+
+    it('updates search params', async () => {
+      const dispatch = () => {};
+      const query = 'health';
+      const thunk = fetchFormsThunk(query);
+
+      await thunk(dispatch);
+
+      const replaceStateStub = global.window.history.replaceState;
+
+      expect(replaceStateStub.calledOnce).to.be.true;
+      expect(replaceStateStub.firstCall.args[2]).to.be.equal('?q=health');
+    });
+
+    it('calls dispatch', async () => {
+      const dispatch = sinon.stub();
+      const query = 'health';
+      const thunk = fetchFormsThunk(query);
+
+      await thunk(dispatch);
+
+      expect(
+        dispatch.firstCall.calledWith({
+          type: FETCH_FORMS,
+          query,
+        }),
+      ).to.be.true;
+
+      const secondCallAction = dispatch.secondCall.args[0];
+
+      expect(secondCallAction.type).to.be.equal(FETCH_FORMS_SUCCESS);
+      expect(secondCallAction.response).to.have.keys(['data']);
     });
   });
 });
