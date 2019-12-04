bb186ef1d8a628ef747df697190a1f281bc98cab
Jeff Balboni <jeff@adhocteam.us>
2019-12-04 12:58:27 -0500

    Update src/applications/vaos/components/ErrorMessage.jsx
    Co-Authored-By: Chris Valarida <chris.valarida@adhocteam.us>

--- src/applications/vaos/components/ErrorMessage.jsx
+++ src/applications/vaos/components/ErrorMessage.jsx
@@ -6,7 +6,7 @@ export default function ErrorMessage() {
   return (
     <div aria-atomic="true" aria-live="assertive">
       <AlertBox status="error" headline="Sorry, something went wrong">
-        We're sorry, we ran into an error. Please try again later.
+        We’re sorry, we ran into an error. Please try again later.
       </AlertBox>
     </div>
   );
