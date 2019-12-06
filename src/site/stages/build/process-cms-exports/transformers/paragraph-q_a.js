676f6475abd76e80092ee4a431142cd37dd630d4
Christopher Valarida <chris.valarida@adhocteam.us>
2019-12-06 12:32:34 -0800

    Remove unnecessary comment

--- src/site/stages/build/process-cms-exports/transformers/paragraph-q_a.js
+++ src/site/stages/build/process-cms-exports/transformers/paragraph-q_a.js
@@ -2,16 +2,6 @@ const { getDrupalValue, getRawParentFieldName } = require('./helpers');
 
 const transform = (entity, uuid, ancestors) => ({
   entity: {
-    // TODO: Pass the ancestry tree into the transformer
-    // TODO: Pass the UUID of the current entity into the transformer
-    // TODO: Write a helper to look up the UUID in an entity and return the property name it's found in
-    // TODO: Use all the above to look up the parentFieldName
-
-    // The snake-cased name of the parent field
-    //
-    // For example, if this `paragraph-q_a` was in a
-    // `fieldQuestions` property in the parent entity, the value of
-    // `parentFieldName` would be `field_questions`
     parentFieldName: getRawParentFieldName(ancestors[ancestors.length], uuid),
     entityType: 'paragraph',
     entityBundle: 'q_a',
