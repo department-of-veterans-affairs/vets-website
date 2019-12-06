c7ffa00fb70b99196a5b5d67f64f24c420fdc0d6
Christopher Valarida <chris.valarida@adhocteam.us>
2019-12-06 10:15:40 -0800

    Use plain ol' fieldAnswer since it's an array of entities

--- src/site/stages/build/process-cms-exports/transformers/paragraph-q_a.js
+++ src/site/stages/build/process-cms-exports/transformers/paragraph-q_a.js
@@ -6,10 +6,16 @@ const transform = entity => ({
     // TODO: Pass the UUID of the current entity into the transformer
     // TODO: Write a helper to look up the UUID in an entity and return the property name it's found in
     // TODO: Use all the above to look up the parentFieldName
+
+    // The snake-cased name of the parent field
+    //
+    // For example, if this `paragraph-q_a` was in a
+    // `fieldQuestions` property in the parent entity, the value of
+    // `parentFieldName` would be `field_questions`
     parentFieldName: '',
     entityType: 'paragraph',
     entityBundle: 'q_a',
-    fieldAnswer: getDrupalValue(entity.fieldAnswer),
+    fieldAnswer: entity.fieldAnswer,
     fieldQuestion: getDrupalValue(entity.fieldQuestion),
   },
 });
