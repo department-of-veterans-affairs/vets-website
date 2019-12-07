6f5c613464922859da802755687ef9b938afcd7c
Christopher Valarida <chris.valarida@adhocteam.us>
2019-12-06 16:32:27 -0800

    Update transformed schema

--- src/site/stages/build/process-cms-exports/schemas/transformed/paragraph-q_a.js
+++ src/site/stages/build/process-cms-exports/schemas/transformed/paragraph-q_a.js
@@ -1,15 +1,18 @@
 module.exports = {
   type: 'object',
   properties: {
-    parentFieldName: { type: 'string' },
     // entityId: { type: 'string' }, // Also should validate that the string is a number
     contentModelType: { enum: ['paragraph-q_a'] },
     entity: {
       type: 'object',
       properties: {
+        parentFieldName: { type: 'string' },
         entityType: { enum: ['paragraph'] },
         entityBundle: { enum: ['q_a'] },
-        fieldAnswer: { type: 'string' },
+        fieldAnswer: {
+          type: 'array',
+          items: { $ref: 'Paragraph' },
+        },
         fieldQuestion: { type: 'string' },
       },
       required: ['fieldAnswer', 'fieldQuestion'],
