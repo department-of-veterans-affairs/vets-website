51daeeefb48198345526307843ff36eb8ecd2d31
Brooks Johnson <brooks.johnson@pm.me>
2019-12-09 11:45:10 -0800

    Add parentFieldName to transform

--- src/site/stages/build/process-cms-exports/transformers/paragraph-list_of_link_teasers.js
+++ src/site/stages/build/process-cms-exports/transformers/paragraph-list_of_link_teasers.js
@@ -1,11 +1,15 @@
-const { getDrupalValue } = require('./helpers');
+const { getDrupalValue, getRawParentFieldName } = require('./helpers');
 
-const transform = entity => ({
+const transform = (entity, uuid, ancestors) => ({
   entity: {
     entityType: 'paragraph',
     entityBundle: 'list_of_link_teasers',
     fieldTitle: getDrupalValue(entity.fieldTitle),
     fieldVaParagraphs: entity.fieldVaParagraphs,
+    parentFieldName: ancestors.length
+      ? getRawParentFieldName(ancestors[ancestors.length - 1].entity, uuid)
+      : // The entity in the unit test won't have any parents
+        '',
   },
 });
 module.exports = {
