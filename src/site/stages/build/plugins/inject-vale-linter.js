c71ed15834c07ebfcbddab2e70cc051acc08e6f1
Keifer Furzland <kfrz.code@gmail.com>
2019-12-10 11:49:55 -0600

    Update language

--- src/site/stages/build/plugins/inject-vale-linter.js
+++ src/site/stages/build/plugins/inject-vale-linter.js
@@ -60,7 +60,7 @@ function buildDetailsMarkup(file, issues) {
     '<details class="vads-u-background-color--primary-alt-lightest vads-u-border-color--secondary-lighter vads-u-border-bottom--2px vads-u-padding--1">';
   details += `<summary><h4 class="vads-u-display--inline-block vads-u-margin-y--2">There are (${
     issues.length
-  }) language linting suggestions found on this page.</h4></summary>`;
+  }) content suggestions found on this page.</h4></summary>`;
 
   let issuesList =
     '<ul class="usa-unstyled-list vads-u-border-color--primary-darker vads-u-border-top--1px vads-u-padding-x--6 vads-u-padding-y--2">';
@@ -69,8 +69,7 @@ function buildDetailsMarkup(file, issues) {
     let issueEl = '<li class=vads-u-margin-y--1">';
     issueEl += '<details>';
     issueEl += `<summary><strong>${issue.Message}</strong></summary>`;
-    issueEl +=
-      '<ul class="usa-unstyled-list vads-u-padding-y--1 vads-u-padding-x--2">';
+    issueEl += `<ul class="usa-unstyled-list vads-u-padding-y--1 vads-u-padding-x--2">`;
 
     issueEl += '</ul>';
     issueEl += '</details>';
