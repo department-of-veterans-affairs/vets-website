4af63d700e8ae1839df3b88a6abdffa5cc852d80
Micah Chiang <micahkchiang@gmail.com>
2019-12-10 13:31:00 -0800

    Convert div to dl

--- src/applications/personalization/rated-disabilities/components/RatedDisabilityListItem.jsx
+++ src/applications/personalization/rated-disabilities/components/RatedDisabilityListItem.jsx
@@ -7,50 +7,35 @@ const RatedDisabilityListItem = ({ ratedDisability }) => {
     ratingPercentage,
     name,
     effectiveDate,
-    relatedTo,
   } = ratedDisability;
   return (
-    <div className="vads-l-col--12 vads-u-background-color--gray-lightest vads-u-margin-top--0 vads-u-margin-bottom--2 vads-u-padding-top--1 vads-u-padding-bottom--2 vads-u-padding-x--2">
-      <dl className="vads-l-row vads-u-margin--0">
-        <dt className="vads-l-col--9 vads-u-font-weight--bold vads-u-margin-top--0p25 vads-u-margin-bottom--0 vads-u-margin-x--0 vads-u-font-size--base">
-          {name}
+    <dl className="vads-u-display--block vads-l-col--12 vads-u-margin--0  vads-u-background-color--gray-lightest vads-u-margin-top--0 vads-u-margin-bottom--2 vads-u-padding-top--1 vads-u-padding-bottom--2 vads-u-padding-x--2">
+      {typeof ratingPercentage === 'number' ? (
+        <dt className="vads-u-display--inline-block vads-u-font-size--lg vads-u-font-weight--bold vads-u-margin--0 vads-u-border-color--gray-light vads-u-border-bottom--1px">
+          {ratingPercentage}%
         </dt>
-        {typeof ratingPercentage === 'number' ? (
-          <dd className="vads-l-col--3 vads-u-font-weight--bold vads-u-font-size--lg vads-u-text-align--right vads-u-margin--0">
-            {ratingPercentage}%
-          </dd>
-        ) : null}
-      </dl>
-      <div className="vads-l-row vads-u-margin-y--0p5">
-        <div className="vads-l-col--12 vads-u-display--flex vads-u-flex-direction--column small-screen:vads-u-flex-direction--row">
-          <p className="vads-u-margin--0">
-            {decisionText === 'Service Connected' ? (
-              <i className="fas fa-medal vads-u-margin-right--0p5" />
-            ) : null}
-            <span className="vads-u-font-weight--bold">{decisionText}</span>{' '}
-          </p>
-          {relatedTo ? (
-            <>
-              <p className="vads-u-display--none small-screen:vads-u-display--inline vads-u-margin-x--0p5 vads-u-margin-y--0">
-                {' '}
-                |{' '}
-              </p>
-              <p className="vads-u-margin--0">Related To: {relatedTo}</p>
-            </>
-          ) : null}
-        </div>
-      </div>
-      {effectiveDate !== null ? (
-        <div className="vads-l-row">
-          <dl className="vads-u-margin--0 vads-l-col--12 medium-screen:vads-l-col--12">
-            <dt className="vads-u-margin--0 vads-u-display--inline">
-              Effective date:
-            </dt>{' '}
-            <dd className="vads-u-display--inline">{effectiveDate}</dd>
-          </dl>
-        </div>
       ) : null}
-    </div>
+      <dt className="vads-u-font-size--lg vads-u-font-weight--bold vads-u-margin-top--0p25 vads-u-margin-bottom--0 vads-u-margin-x--0">
+        {name}
+      </dt>
+      <dt className="vads-u-display--inline-block vads-u-margin--0">
+        <strong>Service-connected disability?</strong>{' '}
+      </dt>
+      <dd className="vads-u-display--inline-block vads-u-margin-y--0 vads-u-margin-left--1">
+        {decisionText === 'Service Connected' ? 'Yes' : 'No'}
+      </dd>
+      <br />
+      {effectiveDate !== null ? (
+        <>
+          <dt className="vads-u-display--inline-block vads-u-margin--0">
+            <strong>Effective date:</strong>
+          </dt>
+          <dd className="vads-u-display--inline-block vads-u-margin-left--0p5">
+            {effectiveDate}
+          </dd>
+        </>
+      ) : null}
+    </dl>
   );
 };
 
