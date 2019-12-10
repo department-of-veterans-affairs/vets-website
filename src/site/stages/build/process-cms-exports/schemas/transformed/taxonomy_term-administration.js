711f5a66b77f5d108740860a5f1be635bdb7affe
Christopher Valarida <chris.valarida@adhocteam.us>
2019-12-10 12:53:31 -0800

    Update raw schema

--- src/site/stages/build/process-cms-exports/schemas/transformed/taxonomy_term-administration.js
+++ src/site/stages/build/process-cms-exports/schemas/transformed/taxonomy_term-administration.js
@@ -1,13 +1,32 @@
-const contentModelType = 'taxonomy_term-administration';
-
 module.exports = {
   type: 'object',
   properties: {
-    baseType: {
-      enum: ['taxonomy_term'],
-    },
-    contentModelType: {
-      enum: [contentModelType],
+    contentModelType: { enum: ['taxonomy_term-administration'] },
+    entity: {
+      type: 'object',
+      properties: {
+        entityType: { enum: ['taxonomy_term'] },
+        entityBundle: { enum: ['administration'] },
+        name: { type: 'string' },
+        fieldAcronym: { type: 'string' },
+        fieldDescription: { type: 'string' },
+        fieldEmailUpdatesLinkText: { type: 'string' },
+        fieldEmailUpdatesUrl: { type: 'string' },
+        fieldIntroText: { type: 'string' },
+        fieldLink: { type: 'string' },
+        fieldSocialMediaLinks: { type: 'string' },
+      },
+      required: [
+        'name',
+        'fieldAcronym',
+        'fieldDescription',
+        'fieldEmailUpdatesLinkText',
+        'fieldEmailUpdatesUrl',
+        'fieldIntroText',
+        'fieldLink',
+        'fieldSocialMediaLinks',
+      ],
     },
   },
+  required: ['entity'],
 };
