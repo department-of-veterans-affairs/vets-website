bc3dc300d83e545e816708b9dae2dc9686b0f965
Brad Conley <bradl3yC@users.noreply.github.com>
2019-12-12 14:09:48 -0500

    bradl3yC 3891 Bad Unit Number (#11297)
    * Add bad unit number
    
    * Remove unreachable message
    
    * Add tests
    
    * Revert test and data changes
    
    * Make suggested updates
    
    * Resolve variable shadowing

--- src/platform/user/profile/vet360/containers/AddressValidationModal.jsx
+++ src/platform/user/profile/vet360/containers/AddressValidationModal.jsx
@@ -10,8 +10,10 @@ import {
   createTransaction,
   updateSelectedAddress,
   updateValidationKeyAndSave,
-  closeModal as closeValidationModal,
+  closeModal as closeAddressValidationModal,
 } from '../actions';
+import { getValidationMessageKey } from '../../utilities';
+import { ADDRESS_VALIDATION_MESSAGES } from '../../constants/addressValidationMessages';
 
 import * as VET360 from '../constants';
 
@@ -53,60 +55,6 @@ class AddressValidationModal extends React.Component {
         this.props.analyticsSectionName,
       );
     }
-  };
-
-  renderWarningText = () => {
-    const {
-      addressValidationError,
-      validationKey,
-      suggestedAddresses,
-    } = this.props;
-
-    let warningText;
-
-    if (suggestedAddresses.length && validationKey) {
-      warningText = `We couldn’t confirm your address with the U.S. Postal Service.  Please verify your address so we can save it to your VA profile.  If the address you entered isn’t correct, please edit it or choose a suggested address below`;
-    }
-
-    if (suggestedAddresses.length && !validationKey) {
-      warningText = `We’re sorry.  We couldn’t verify your address with the U.S. Postal Service, so we won't be able to deliver your VA mail to that address.  Please edit the address you entered or choose a suggested address below.`;
-    }
-
-    if (addressValidationError && validationKey) {
-      warningText = `We couldn’t confirm your address with the U.S. Postal Service.  Please verify your address so we can save it to your VA profile.  If the address you entered isn’t correct, please edit it.`;
-    }
-
-    if (addressValidationError && !validationKey) {
-      warningText = `We’re sorry.  We couldn’t verify your address with the U.S. Postal Service, so we will not be able to deliver your VA mail to that address.  Please edit the address you entered.`;
-    }
-
-    return warningText;
-  };
-
-  renderWarningHeadline = () => {
-    const {
-      addressValidationError,
-      validationKey,
-      suggestedAddresses,
-    } = this.props;
-
-    let warningHeadline;
-
-    if (
-      (suggestedAddresses.length > 1 && validationKey) ||
-      (addressValidationError && validationKey)
-    ) {
-      warningHeadline = `Please confirm your address`;
-    }
-
-    if (
-      (suggestedAddresses.length > 1 && !validationKey) ||
-      (addressValidationError && !validationKey)
-    ) {
-      warningHeadline = `We couldn’t verify your address`;
-    }
-
-    return warningHeadline;
   };
 
   renderPrimaryButton = () => {
@@ -201,7 +149,19 @@ class AddressValidationModal extends React.Component {
       addressValidationType,
       suggestedAddresses,
       addressFromUser,
+      validationKey,
+      addressValidationError,
+      closeModal,
     } = this.props;
+
+    const validationMessageKey = getValidationMessageKey(
+      suggestedAddresses,
+      validationKey,
+      addressValidationError,
+    );
+
+    const addressValidationMessage =
+      ADDRESS_VALIDATION_MESSAGES[validationMessageKey];
 
     const shouldShowSuggestions = suggestedAddresses.length > 0;
 
@@ -213,15 +173,15 @@ class AddressValidationModal extends React.Component {
             : 'Edit home address'
         }
         id="address-validation-warning"
-        onClose={closeValidationModal}
+        onClose={closeModal}
         visible={isAddressValidationModalVisible}
       >
         <AlertBox
           className="vads-u-margin-bottom--1"
           status="warning"
-          headline={this.renderWarningHeadline()}
+          headline={addressValidationMessage.headline}
         >
-          <p>{this.renderWarningText()}</p>
+          <p>{addressValidationMessage.modalText}</p>
         </AlertBox>
         <form onSubmit={this.onSubmit}>
           <span className="vads-u-font-weight--bold">You entered:</span>
@@ -236,10 +196,7 @@ class AddressValidationModal extends React.Component {
               this.renderAddressOption(address, String(index)),
             )}
           {this.renderPrimaryButton()}
-          <button
-            className="usa-button-secondary"
-            onClick={closeValidationModal}
-          >
+          <button className="usa-button-secondary" onClick={closeModal}>
             Cancel
           </button>
         </form>
@@ -270,7 +227,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
   ...bindActionCreators(
     {
-      closeValidationModal,
+      closeModal: closeAddressValidationModal,
       openModal,
       updateSelectedAddress,
       updateValidationKeyAndSave,
