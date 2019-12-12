5b7e8851a8f379d38a3818dc6dbf9b52b44afd84
Trevor Pierce <trevor@adhocteam.us>
2019-12-12 15:53:10 -0600

    Properly santizing issue string.

--- src/site/assets/js/execute-axe-check.js
+++ src/site/assets/js/execute-axe-check.js
@@ -16,10 +16,11 @@ function processAxeCheckResults(error, results) {
 
   results.violations.forEach(function(violation) {
     var violationEl = '<li class="vads-u-margin-y--1">';
-    var violationHelp = violation.help.replace(/\</g,'').replace(/>/g,'');
+    var violationHelp = document.createElement('strong');
+    violationHelp.innerText = violation.help;
 
     violationEl += '<details>';
-    violationEl += '<summary><strong>' + violationHelp + '</strong></summary>';
+    violationEl += '<summary>' + violationHelp.outerHTML + '</summary>';
     violationEl += '<ul class="usa-unstyled-list vads-u-padding-y--1 vads-u-padding-x--2">';
 
     violationEl += '<li><strong>Description</strong>: ' + violation.description + '</li>';
