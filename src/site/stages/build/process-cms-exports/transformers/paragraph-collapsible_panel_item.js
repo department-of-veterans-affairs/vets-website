adfaace1ae3590b2620629e38bcb6a0a2397eca0
Christopher Valarida <chris.valarida@adhocteam.us>
2019-12-13 14:20:26 -0800

    Add paragraph-collapsible_panel_item

---
+++ src/site/stages/build/process-cms-exports/transformers/paragraph-collapsible_panel_item.js
@@ -0,0 +1,15 @@
+const { getDrupalValue, getWysiwygString } = require('./helpers');
+
+const transform = entity => ({
+  entity: {
+    entityType: 'paragraph',
+    entityBundle: 'collapsible_panel_item',
+    fieldTitle: getDrupalValue(entity.fieldTitle),
+    fieldVaParagraphs: entity.fieldVaParagraphs,
+    fieldWysiwyg: { processed: getWysiwygString(entity.fieldWysiwyg.value) },
+  },
+});
+module.exports = {
+  filter: ['field_title', 'field_va_paragraphs', 'field_wysiwyg'],
+  transform,
+};
