01ffd9e95b8a995a78dd8e1e0f3e25981933e17d
Narin Ratana <n.ratana@gmail.com>
2019-12-13 14:25:57 -0800

    Use grid system to style hours instead of custom css

--- src/applications/vaos/components/FacilityHours.jsx
+++ src/applications/vaos/components/FacilityHours.jsx
@@ -24,74 +24,114 @@ const FacilityHours = ({ location }) => {
   const isVetCenter = facilityType === 'vet_center';
 
   return (
-    <>
-      <span className="vads-u-font-weight--bold">Hours:</span>
-      <div className="vads-u-margin-left--3">
-        {/* Sunday */}
-        {sunday && (
-          <div className="vads-u-display--flex">
-            <div className="vaos-facility-details__day">Sunday</div>
-            <div className="vaos-facility-details__hours">{sunday}</div>
-          </div>
-        )}
+    <div className="vads-l-grid-container vads-u-padding--0">
+      <div className="vads-l-row">
+        <div className="vads-l-col--8 xsmall-screen:vads-l-col--12">
+          <div className="vads-l-row">
+            <div className="vads-l-col--3 xsmall-screen:vads-l-col--4 small-screen:vads-l-col--3 medium-screen:vads-l-col--2 vads-u-font-weight--bold">
+              Hours:
+            </div>
+            <div className="vads-l-col--9 xsmall-screen:vads-l-col--12 small-screen:vads-l-col--6">
+              <div className="vads-l-row">
+                {/* Sunday */}
+                {sunday && (
+                  <>
+                    <div className="vaos-facility-details__day vads-l-col--6 xsmall-screen:vads-l-col--7">
+                      Sunday
+                    </div>
+                    <div className="vaos-facility-details__hours vads-l-col--6 xsmall-screen:vads-l-col--5">
+                      {sunday}
+                    </div>
+                  </>
+                )}
 
-        {/* Monday */}
-        {monday && (
-          <div className="vads-u-display--flex">
-            <div className="vaos-facility-details__day">Monday</div>
-            <div className="vaos-facility-details__hours">{monday}</div>
-          </div>
-        )}
+                {/* Monday */}
+                {monday && (
+                  <>
+                    <div className="vaos-facility-details__day vads-l-col--6 xsmall-screen:vads-l-col--7">
+                      Monday
+                    </div>
+                    <div className="vaos-facility-details__hours vads-l-col--6 xsmall-screen:vads-l-col--5">
+                      {monday}
+                    </div>
+                  </>
+                )}
 
-        {/* Tuesday */}
-        {tuesday && (
-          <div className="vads-u-display--flex">
-            <div className="vaos-facility-details__day">Tuesday</div>
-            <div className="vaos-facility-details__hours">{tuesday}</div>
-          </div>
-        )}
+                {/* Tuesday */}
+                {tuesday && (
+                  <>
+                    <div className="vaos-facility-details__day vads-l-col--6 xsmall-screen:vads-l-col--7">
+                      Tuesday
+                    </div>
+                    <div className="vaos-facility-details__hours vads-l-col--6 xsmall-screen:vads-l-col--5">
+                      {tuesday}
+                    </div>
+                  </>
+                )}
 
-        {/* Wednesday */}
-        {wednesday && (
-          <div className="vads-u-display--flex">
-            <div className="vaos-facility-details__day">Wednesday</div>
-            <div className="vaos-facility-details__hours">{wednesday}</div>
-          </div>
-        )}
+                {/* Wednesday */}
+                {wednesday && (
+                  <>
+                    <div className="vaos-facility-details__day vads-l-col--6 xsmall-screen:vads-l-col--7">
+                      Wednesday
+                    </div>
+                    <div className="vaos-facility-details__hours vads-l-col--6 xsmall-screen:vads-l-col--5">
+                      {wednesday}
+                    </div>
+                  </>
+                )}
 
-        {/* Thursday */}
-        {thursday && (
-          <div className="vads-u-display--flex">
-            <div className="vaos-facility-details__day">Thursday</div>
-            <div className="vaos-facility-details__hours">{thursday}</div>
-          </div>
-        )}
+                {/* Thursday */}
+                {thursday && (
+                  <>
+                    <div className="vaos-facility-details__day vads-l-col--6 xsmall-screen:vads-l-col--7">
+                      Thursday
+                    </div>
+                    <div className="vaos-facility-details__hours vads-l-col--6 xsmall-screen:vads-l-col--5">
+                      {thursday}
+                    </div>
+                  </>
+                )}
 
-        {/* Friday */}
-        {friday && (
-          <div className="vads-u-display--flex">
-            <div className="vaos-facility-details__day">Friday</div>
-            <div className="vaos-facility-details__hours">{friday}</div>
-          </div>
-        )}
+                {/* Friday */}
+                {friday && (
+                  <>
+                    <div className="vaos-facility-details__day vads-l-col--6 xsmall-screen:vads-l-col--7">
+                      Friday
+                    </div>
+                    <div className="vaos-facility-details__hours vads-l-col--6 xsmall-screen:vads-l-col--5">
+                      {friday}
+                    </div>
+                  </>
+                )}
 
-        {/* Saturday */}
-        {saturday && (
-          <div className="vads-u-display--flex">
-            <div className="vaos-facility-details__day">Saturday</div>
-            <div className="vaos-facility-details__hours">{saturday}</div>
+                {/* Saturday */}
+                {saturday && (
+                  <>
+                    <div className="vaos-facility-details__day vads-l-col--6 xsmall-screen:vads-l-col--7">
+                      Saturday
+                    </div>
+                    <div className="vaos-facility-details__hours vads-l-col--6 xsmall-screen:vads-l-col--5">
+                      {saturday}
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
           </div>
-        )}
+
+          {isVetCenter && (
+            <p>
+              In addition to the hours listed above, all Vet Centers maintain
+              non-traditional hours that are specific to each site and can
+              change periodically given local Veteran, Service member & Family
+              needs. Please contact your Vet Center to obtain the current
+              schedule.
+            </p>
+          )}
+        </div>
       </div>
-      {isVetCenter && (
-        <p>
-          In addition to the hours listed above, all Vet Centers maintain
-          non-traditional hours that are specific to each site and can change
-          periodically given local Veteran, Service member & Family needs.
-          Please contact your Vet Center to obtain the current schedule.
-        </p>
-      )}
-    </>
+    </div>
   );
 };
 
