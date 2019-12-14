966102ce4640ce65b943d0bec58ca642b5c28df6
Brooks Johnson <brooks.johnson@pm.me>
2019-12-13 16:20:04 -0800

    Create variable for reuse

--- src/site/stages/build/process-cms-exports/transformers/paragraph-table.js
+++ src/site/stages/build/process-cms-exports/transformers/paragraph-table.js
@@ -1,21 +1,22 @@
 const { combineItemsInIndexedObject, getDrupalValue } = require('./helpers');
 
-const transform = entity => ({
-  entity: {
-    entityType: 'paragraph',
-    entityBundle: 'table',
-    fieldTable: {
-      tableValue: []
-        .concat(
-          ...combineItemsInIndexedObject(getDrupalValue(entity.fieldTable)),
-        )
-        .join(' '),
-      // This is to get the `format` & `caption` fields in
-      ...entity.fieldTable[0],
-      value: getDrupalValue(entity.fieldTable),
+const transform = entity => {
+  const fieldTable = getDrupalValue(entity.fieldTable);
+  return {
+    entity: {
+      entityType: 'paragraph',
+      entityBundle: 'table',
+      fieldTable: {
+        tableValue: []
+          .concat(...combineItemsInIndexedObject(fieldTable))
+          .join(' '),
+        // This is to get the `format` & `caption` fields in
+        ...entity.fieldTable[0],
+        value: fieldTable,
+      },
     },
-  },
-});
+  };
+};
 module.exports = {
   filter: ['field_table'],
   transform,
