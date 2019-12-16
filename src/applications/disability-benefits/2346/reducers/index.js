814fa31dc63ea082ab4af99ee019812c04478686
Mahariel Rosario <maharielrosario@USRMonitors-MacBook-Pro.local>
2019-12-16 17:33:48 -0500

    fix form loading issue

--- src/applications/disability-benefits/2346/reducers/index.js
+++ src/applications/disability-benefits/2346/reducers/index.js
@@ -1,12 +1,9 @@
-// import formConfig from '../config/form';
-// import { createSaveInProgressFormReducer } from '../../../../platform/forms/save-in-progress/reducers';
-
-// export default {
-//   form: createSaveInProgressFormReducer(formConfig),
-// };
-
-// Relative imports.
+import formConfig from '../config/form';
+import { createSaveInProgressFormReducer } from '../../../../platform/forms/save-in-progress/reducers';
 import form2346Reducer from './form2346Reducer';
+
 export default {
-  form2346Reducer,
+  form: createSaveInProgressFormReducer(formConfig),
+  form2346Reducer
 };
+
