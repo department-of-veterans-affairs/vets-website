04e2c93bed253557d4649bcdeae071de00803f02
Jesse Cohn <jesse.cohn@adhocteam.us>
2019-12-16 10:07:33 -0800

    restructured sidebar component to be more reusable

---
+++ src/applications/personalization/view-dependents/components/ViewDependentsSidebar/ViewDependentsSidebarBlockStates/ViewDependentSidebarBlockStates.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+export const firstSidebarBlock = {
+  heading: 'When do I need to declare or modify a dependent?',
+  content: (
+    <>
+      <p>
+        You need to let VA know when there is a{' '}
+        <strong>change in a dependent's status.</strong>
+        Changes in status could include:
+      </p>
+      <ul>
+        <li>The birth or adoption of a child</li>
+        <li>If you get married or divorced</li>
+        <li>If a child becomes seriously disabled</li>
+        <li>If your child is over 18 and is not attending school</li>
+      </ul>
+    </>
+  ),
+};
+
+export const secondSidebarBlock = {
+  heading: 'What if I need help with my claim?',
+  content: (
+    <>
+      <p>
+        You can work with an accredited Veterans Service Officer (VSO). We trust
+        these professionals because they are trained and certified in the VA
+        claims and appeals process. A VSO can answer your questions or even file
+        a claim for you.
+      </p>
+      <a
+        href="#"
+        aria-label="Get help filing a claim"
+        title="Get help filiing a claim"
+      >
+        Get help filing a claim
+      </a>
+    </>
+  ),
+};
+
+export const thirdSidebarBlock = {
+  heading: 'What if I have questions?',
+  content: (
+    <>
+      <p>
+        You can call us at <a href="tel:8008271000">800-827-1000</a>. We're here
+        Monday through Friday, 8:00 a.m. to 9:00 p.m. E.T.
+      </p>
+    </>
+  ),
+};
