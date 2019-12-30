0647e34a773f091d660736da59b866cf987f566e
Brooks Johnson <brooks.johnson@pm.me>
2019-12-30 11:45:03 -0800

    Do some refactoring based on the link_teaser

--- src/site/stages/build/process-cms-exports/transformers/node-support_service.js
+++ src/site/stages/build/process-cms-exports/transformers/node-support_service.js
@@ -1,20 +1,26 @@
-const { getDrupalValue } = require('./helpers');
+const { getDrupalValue, uriToUrl } = require('./helpers');
 
-const transform = entity => ({
-  entity: {
-    entityType: 'node',
-    entityBundle: 'support_service',
-    title: getDrupalValue(entity.title),
-    fieldLink: {
-      url: {
-        path: entity.fieldLink[0].uri,
-      },
-      title: entity.fieldLink[0].title,
-      options: entity.fieldLink[0].options,
+const transform = entity => {
+  const { fieldLink } = entity;
+  const { uri, title, options } = fieldLink[0] || {};
+
+  return {
+    entity: {
+      entityType: 'node',
+      entityBundle: 'support_service',
+      title: getDrupalValue(entity.title),
+      fieldLink: fieldLink[0]
+        ? {
+            url: { path: uriToUrl(uri) },
+            title,
+            options,
+          }
+        : null,
+
+      fieldPhoneNumber: getDrupalValue(entity.fieldPhoneNumber),
     },
-    fieldPhoneNumber: getDrupalValue(entity.fieldPhoneNumber),
-  },
-});
+  };
+};
 module.exports = {
   filter: ['title', 'field_link', 'field_phone_number'],
   transform,
