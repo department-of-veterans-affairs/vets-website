ff0ff706db7043263581bdf66b64fb7b66824ee9
Brooks Johnson <brooks.johnson@pm.me>
2019-12-30 11:30:15 -0800

    Adjust field_link raw schema

--- src/site/stages/build/process-cms-exports/schemas/raw/node-support_service.js
+++ src/site/stages/build/process-cms-exports/schemas/raw/node-support_service.js
@@ -4,7 +4,19 @@ module.exports = {
   type: 'object',
   properties: {
     title: { $ref: 'GenericNestedString' },
-    field_link: { $ref: 'GenericNestedString' },
+    field_link: {
+      type: 'array',
+      maxItems: 1,
+      items: {
+        type: 'object',
+        properties: {
+          uri: { type: 'string' },
+          title: { type: 'string' },
+          options: { type: 'array' },
+        },
+        required: ['uri', 'title', 'options'],
+      },
+    },
     field_phone_number: { $ref: 'GenericNestedString' },
   },
   required: ['title', 'field_link', 'field_phone_number'],
