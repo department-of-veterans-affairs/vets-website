a9ddc36f134161167a4925fb9b232640ee2216bf
Jeff Balboni <jeff@adhocteam.us>
2020-01-03 14:56:19 -0500

    Remove space, fix quote

--- src/applications/vaos/components/ConfirmationRequestInfo.jsx
+++ src/applications/vaos/components/ConfirmationRequestInfo.jsx
@@ -38,9 +38,9 @@ export default function ConfirmationRequestInfo({ data, facility }) {
       </h1>
       <AlertBox status="success">
         <strong>Your appointment request has been submitted.</strong>
-        <br /> We're reviewing your request. You don't have anything to do right
-        now. A scheduler will contact you to schedule the first available
-        appointment.
+        <br />
+        We’re reviewing your request. You don’t have anything to do right now. A
+        scheduler will contact you to schedule the first available appointment.
       </AlertBox>
       <AlertBox backgroundOnly status="info">
         <h2 className="vads-u-font-size--h3 vads-u-margin-top--0">
