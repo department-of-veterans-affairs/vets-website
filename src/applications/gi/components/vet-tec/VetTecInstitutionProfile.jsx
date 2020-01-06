64dc0bdba0d6ba8f88ff370607365ff6294896d2
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2020-01-06 10:57:44 -0500

    removed display option in vetTecInstitution

--- src/applications/gi/components/vet-tec/VetTecInstitutionProfile.jsx
+++ src/applications/gi/components/vet-tec/VetTecInstitutionProfile.jsx
@@ -10,59 +10,49 @@ import VetTecContactInformation from './VetTecContactInformation';
 import { renderVetTecLogo } from '../../utils/render';
 import classNames from 'classnames';
 import VetTecVeteranPrograms from './VetTecVeteranPrograms';
-import _ from 'lodash';
 
 const VetTecInstitutionProfile = ({
   institution,
   showModal,
   preSelectedProgram,
-}) => {
-  const displayVeteranPrograms = () => {
-    const firstProgram = _.get(institution, 'programs[0]', {});
-    return firstProgram.studentVetGroup || firstProgram.vetSuccessName;
-  };
-
-  return (
-    <div>
-      <div className="vads-u-display--block small-screen:vads-u-display--none vettec-logo-container">
-        {renderVetTecLogo(classNames('vettec-logo'))}
-      </div>
-      <VetTecHeadingSummary institution={institution} showModal={showModal} />
-      <div className="usa-accordion">
-        <ul>
-          <AccordionItem button="Approved programs">
-            <VetTecApprovedPrograms
-              institution={institution}
-              preSelectedProgram={preSelectedProgram}
-            />
-          </AccordionItem>
-          <AccordionItem button="Estimate your benefits">
-            <VetTecCalculator showModal={showModal} />
-          </AccordionItem>
-          {displayVeteranPrograms() && (
-            <AccordionItem button="Veteran programs">
-              <VetTecVeteranPrograms
-                institution={institution}
-                onShowModal={showModal}
-              />
-            </AccordionItem>
-          )}
-          <AccordionItem button="Application process">
-            <VetTecApplicationProcess institution={institution} />
-          </AccordionItem>
-          <AccordionItem button="Contact details">
-            <VetTecContactInformation institution={institution} />
-          </AccordionItem>
-          <AccordionItem button="Additional information">
-            <VetTecAdditionalInformation
-              institution={institution}
-              showModal={showModal}
-            />
-          </AccordionItem>
-        </ul>
-      </div>
+}) => (
+  <div>
+    <div className="vads-u-display--block small-screen:vads-u-display--none vettec-logo-container">
+      {renderVetTecLogo(classNames('vettec-logo'))}
     </div>
-  );
-};
+    <VetTecHeadingSummary institution={institution} showModal={showModal} />
+    <div className="usa-accordion">
+      <ul>
+        <AccordionItem button="Approved programs">
+          <VetTecApprovedPrograms
+            institution={institution}
+            preSelectedProgram={preSelectedProgram}
+          />
+        </AccordionItem>
+        <AccordionItem button="Estimate your benefits">
+          <VetTecCalculator showModal={showModal} />
+        </AccordionItem>
+        <AccordionItem button="Veteran programs">
+          <VetTecVeteranPrograms
+            institution={institution}
+            onShowModal={showModal}
+          />
+        </AccordionItem>
+        <AccordionItem button="Application process">
+          <VetTecApplicationProcess institution={institution} />
+        </AccordionItem>
+        <AccordionItem button="Contact details">
+          <VetTecContactInformation institution={institution} />
+        </AccordionItem>
+        <AccordionItem button="Additional information">
+          <VetTecAdditionalInformation
+            institution={institution}
+            showModal={showModal}
+          />
+        </AccordionItem>
+      </ul>
+    </div>
+  </div>
+);
 
 export default VetTecInstitutionProfile;
