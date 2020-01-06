77a5ca02d31f4d683f0d1fe7c349b47a9a7d5344
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2020-01-06 09:12:35 -0500

    removed prod flag on VetTecInstitutionProfile

--- src/applications/gi/components/vet-tec/VetTecInstitutionProfile.jsx
+++ src/applications/gi/components/vet-tec/VetTecInstitutionProfile.jsx
@@ -40,15 +40,7 @@ const VetTecInstitutionProfile = ({
           <AccordionItem button="Estimate your benefits">
             <VetTecCalculator showModal={showModal} />
           </AccordionItem>
-          {/* prod flag for BAH 4227. There are also flags in Programs.jsx and VetTecPrograms.jsx */}
-          {environment.isProduction() && displayVeteranPrograms() ? (
-            <AccordionItem button="Veteran programs">
-              <VetTecVeteranPrograms
-                institution={institution}
-                onShowModal={showModal}
-              />
-            </AccordionItem>
-          ) : (
+          {displayVeteranPrograms() && (
             <AccordionItem button="Veteran programs">
               <VetTecVeteranPrograms
                 institution={institution}
