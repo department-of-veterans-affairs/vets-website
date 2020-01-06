bf13e3ab1fe4978d83f05758dec64611a09799eb
Jeff Balboni <jeff@adhocteam.us>
2020-01-06 10:06:02 -0500

    WIP

--- src/applications/vaos/components/ConfirmationDirectScheduleInfo.jsx
+++ src/applications/vaos/components/ConfirmationDirectScheduleInfo.jsx
@@ -18,16 +18,59 @@ export default function ConfirmationDirectScheduleInfo({
         <strong>Your appointment is confirmed.</strong> Please see your
         appointment details below.
       </AlertBox>
-      <AlertBox backgroundOnly status="info">
-        <h2 className="vads-u-font-size--h3 vads-u-margin-top--0">
+      <div className="vads-u-border-top--4px vads-u-border-color--green">
+        <div className="vaos-form__title vads-u-font-size--sm vads-u-font-weight--normal vads-u-font-family--sans">
+          VA Appointment
+        </div>
+        <h2 className="vaos-appts__date-time vads-u-font-size--lg vads-u-margin-x--0">
           {moment(data.calendarData.selectedDates[0].datetime).format(
             'MMMM D, YYYY [at] hh:mm a',
           )}{' '}
         </h2>
-        <hr className="vads-u-margin-y--2" />
-        <h3 className="vads-u-font-size--base vads-u-font-family--sans vads-u-margin-top--0 vads-u-margin-bottom--2">
-          {getTypeOfCare(data)?.name} appointment
-        </h3>
+        <div className="vads-u-display--flex vads-u-justify-content--space-between vads-u-margin-top--2">
+          <div className="vaos-appts__status vads-u-flex--1">
+            {canceled ? (
+              <i className="fas fa-exclamation-circle" />
+            ) : (
+              <i className="fas fa-check-circle" />
+            )}
+            <span
+              id={`card-${index}`}
+              className="vads-u-font-weight--bold vads-u-margin-left--1 vads-u-display--inline-block"
+            >
+              {canceled ? 'Canceled' : 'Confirmed'}
+              <span className="sr-only"> appointment</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="vaos-appts__split-section vads-u-margin-top--2">
+          <div className="vads-u-flex--1">
+            {isVideoVisit(appointment) ? (
+              <VideoVisitSection appointment={appointment} />
+            ) : (
+              <dl className="vads-u-margin--0">
+                <dt className="vads-u-font-weight--bold">
+                  {getLocationHeader(appointment)}
+                </dt>
+                <dd>{getAppointmentLocation(appointment, facility)}</dd>
+              </dl>
+            )}
+          </div>
+          {hasInstructions(appointment) && (
+            <div className="vads-u-flex--1">
+              <dl className="vads-u-margin--0">
+                <dt className="vads-u-font-weight--bold">
+                  {getAppointmentInstructionsHeader(appointment)}
+                </dt>
+                <dd>{getAppointmentInstructions(appointment)}</dd>
+              </dl>
+            </div>
+          )}
+        </div>
+
+      </div>
+      <AlertBox backgroundOnly status="info">
         <div className="vads-u-display--flex">
           <div className="vads-u-flex--1">
             <dl className="vads-u-margin-y--0">
