a675cd3d0dc8a95f3719022ddfc4fe1b1eed034d
Christopher Valarida <chris.valarida@adhocteam.us>
2020-01-07 12:31:30 -0800

    Get the test to pass the schema validation

--- src/site/stages/build/process-cms-exports/transformers/node-event.js
+++ src/site/stages/build/process-cms-exports/transformers/node-event.js
@@ -1,18 +1,42 @@
-const { getDrupalValue, utcToEpochTime } = require('./helpers');
+const {
+  getDrupalValue,
+  utcToEpochTime,
+  getWysiwygString,
+} = require('./helpers');
+const { mapKeys, camelCase } = require('lodash');
+const assert = require('assert');
+
+function fakeUtc(timeString) {
+  // Assume the timeString looks like 2019-05-30T21:00:00
+  // If it doesn't, we'll need to do some real conversions; fail for now
+  assert(
+    /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/.test(timeString),
+    `Expected timeString to look like 2019-05-30T21:00:00, but found ${timeString}`,
+  );
+  return `${timeString.replace('T', ' ')} UTC`;
+}
 
 const transform = entity => ({
   entityType: 'node',
   entityBundle: 'event',
   title: getDrupalValue(entity.title),
-  uid: entity.uid,
+  uid: entity.uid[0],
   changed: utcToEpochTime(getDrupalValue(entity.changed)),
   path: getDrupalValue(entity.path),
   fieldAdditionalInformationAbo: getDrupalValue(
     entity.fieldAdditionalInformationAbo,
   ),
-  fieldAddress: getDrupalValue(entity.fieldAddress),
-  fieldBody: getDrupalValue(entity.fieldBody),
-  fieldDate: getDrupalValue(entity.fieldDate),
+  // The keys of fieldAddress[0] are snake_case, but we want camelCase
+  fieldAddress: mapKeys(entity.fieldAddress[0], (v, k) => camelCase(k)),
+  fieldBody: {
+    processed: getWysiwygString(getDrupalValue(entity.fieldBody)),
+  },
+  fieldDate: {
+    startDate: fakeUtc(entity.fieldDate[0].value),
+    value: entity.fieldDate[0].value,
+    endDate: fakeUtc(entity.fieldDate[0].end_value),
+    endValue: entity.fieldDate[0].end_value,
+  },
   fieldDescription: getDrupalValue(entity.fieldDescription),
   fieldEventCost: getDrupalValue(entity.fieldEventCost),
   fieldEventCta: getDrupalValue(entity.fieldEventCta),
