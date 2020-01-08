166e87c71aa567dc8e0370a8964c596cd0faba35
Christopher Valarida <chris.valarida@adhocteam.us>
2020-01-08 11:27:00 -0800

    Update transformed schema

--- src/site/stages/build/process-cms-exports/schemas/transformed/media-image.js
+++ src/site/stages/build/process-cms-exports/schemas/transformed/media-image.js
@@ -7,7 +7,24 @@ module.exports = {
       properties: {
         entityType: { enum: ['media'] },
         entityBundle: { enum: ['image'] },
-        image: { type: 'string' },
+        image: {
+          type: 'object',
+          properties: {
+            alt: { type: 'string' },
+            title: { type: 'string' },
+            url: { type: 'string' },
+            derivative: {
+              type: 'object',
+              properties: {
+                url: { type: 'string' },
+                width: { type: 'number' },
+                height: { type: 'number' },
+              },
+              required: ['url', 'width', 'height'],
+            },
+          },
+          required: ['alt', 'title', 'url', 'derivative'],
+        },
       },
       required: ['image'],
     },
