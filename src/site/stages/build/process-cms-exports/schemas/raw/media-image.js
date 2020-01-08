c3f423c835427e7b17c525a24eb6210a8d6f3002
Christopher Valarida <chris.valarida@adhocteam.us>
2020-01-08 11:26:48 -0800

    Update raw schema

--- src/site/stages/build/process-cms-exports/schemas/raw/media-image.js
+++ src/site/stages/build/process-cms-exports/schemas/raw/media-image.js
@@ -3,7 +3,25 @@
 module.exports = {
   type: 'object',
   properties: {
-    image: { $ref: 'GenericNestedString' },
+    image: {
+      type: 'array',
+      items: {
+        allOf: [
+          { $ref: 'EntityReference' },
+          {
+            type: 'object',
+            properties: {
+              alt: { type: 'string' },
+              title: { type: 'string' },
+              width: { type: 'number' },
+              height: { type: 'number' },
+            },
+            required: ['alt', 'width', 'height'],
+          },
+        ],
+      },
+      maxItems: 1,
+    },
   },
   required: ['image'],
 };
