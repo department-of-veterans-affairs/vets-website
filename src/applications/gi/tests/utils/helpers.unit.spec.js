0bb2dde3a7c97f32ad22972af38e978304e15faf
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2020-01-09 22:49:21 -0500

    added unit test for helper function

--- src/applications/gi/tests/utils/helpers.unit.spec.js
+++ src/applications/gi/tests/utils/helpers.unit.spec.js
@@ -8,6 +8,7 @@ import {
   isCountryUSA,
   isCountryInternational,
   rubyifyKeys,
+  sortOptionsByStateName,
 } from '../../utils/helpers';
 
 describe('GIBCT helpers:', () => {
@@ -80,6 +81,24 @@ describe('GIBCT helpers:', () => {
         testKey: ['a', 'b'],
       };
       expect(rubyifyKeys(data)).to.have.key('test_key[]');
+    });
+  });
+
+  describe('sortOptionsByStateName', () => {
+    it('should sort an array of objects by label', () => {
+      const data = [
+        { value: 'AP', label: 'Apple' },
+        { value: 'OG', label: 'Orange' },
+        { value: 'GP', label: 'Grape' },
+        { value: 'BN', label: 'Banana' },
+      ];
+      const sortedData = [
+        { value: 'AP', label: 'Apple' },
+        { value: 'BN', label: 'Banana' },
+        { value: 'GP', label: 'Grape' },
+        { value: 'OG', label: 'Orange' },
+      ];
+      expect(data.sort(sortOptionsByStateName)).to.deep.equal(sortedData);
     });
   });
 });
