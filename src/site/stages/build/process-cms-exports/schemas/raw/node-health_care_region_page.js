5be15ed34d0e9481f3192429a2f7f69e9f6a45ed
Brooks Johnson <brooks.johnson@pm.me>
2020-01-10 13:33:42 -0800

    Update raw path schema
    This is prime opportunity for refactoring into a common schema

--- src/site/stages/build/process-cms-exports/schemas/raw/node-health_care_region_page.js
+++ src/site/stages/build/process-cms-exports/schemas/raw/node-health_care_region_page.js
@@ -4,7 +4,17 @@ module.exports = {
   type: 'object',
   properties: {
     title: { $ref: 'GenericNestedString' },
-    path: { $ref: 'GenericNestedString' },
+    path: {
+      type: 'array',
+      maxItems: 1,
+      items: {
+        type: 'object',
+        properties: {
+          alias: { type: 'string' },
+        },
+        required: ['alias'],
+      },
+    },
     field_nickname_for_this_facility: { $ref: 'GenericNestedString' },
   },
   required: ['title', 'path', 'field_nickname_for_this_facility'],
