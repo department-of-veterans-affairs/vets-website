96d49e194b8b8e74e97407285c8609d7ad294527
Rian Fowler <rian.fowler@adhocteam.us>
2020-01-10 12:31:05 -0500

    update downtime

--- src/platform/monitoring/DowntimeNotification/config/scheduledDowntimeWindow.js
+++ src/platform/monitoring/DowntimeNotification/config/scheduledDowntimeWindow.js
@@ -1,6 +1,6 @@
 import moment from 'moment-timezone';
 
 export default {
-  downtimeStart: moment.tz('2020-01-10 14:00', 'America/New_York'),
-  downtimeEnd: moment.tz('2020-01-10 15:00', 'America/New_York'),
+  downtimeStart: moment.tz('2020-01-11 22:00', 'America/New_York'),
+  downtimeEnd: moment.tz('2020-01-12 00:01', 'America/New_York'),
 };
