80bc4efcfe8291f0e588ae33621c6441996bf1bf
Ethan Teague <metrowebz@gmail.com>
2020-01-13 16:22:02 -0500

    VAGOV-6403: Adding breadcrumb cleanup logic.

--- src/platform/site-wide/index.js
+++ src/platform/site-wide/index.js
@@ -7,6 +7,7 @@ import './legacy/menu'; // Used in the footer.
 import './moment-setup';
 import './popups';
 import './wysiwyg-analytics-setup';
+import './breadcrumb-title-match';
 import addMenuListeners from './accessible-menus';
 import startUserNavWidget from './user-nav';
 import startMegaMenuWidget from './mega-menu';
