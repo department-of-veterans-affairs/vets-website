7bae00b85976e561a4385b9b93c83cd1cd7aef32
Rian Fowler <rian.fowler@adhocteam.us>
2020-01-13 08:33:12 -0500

    remove global downtime window period

--- src/platform/monitoring/DowntimeNotification/config/scheduledDowntimeWindow.js
+++ src/platform/monitoring/DowntimeNotification/config/scheduledDowntimeWindow.js
@@ -1,6 +1,10 @@
-import moment from 'moment-timezone';
+// import moment from 'moment-timezone';
+
+// Global downtime notification will be set if status api fails and
+// current time is during this window.
+// example value: moment.tz('2020-01-12 00:01', 'America/New_York')
 
 export default {
-  downtimeStart: moment.tz('2020-01-11 22:00', 'America/New_York'),
-  downtimeEnd: moment.tz('2020-01-12 00:01', 'America/New_York'),
+  downtimeStart: null,
+  downtimeEnd: null,
 };
