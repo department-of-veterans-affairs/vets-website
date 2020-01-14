8cdff062d2d290d1d3c5f4146e420537f29ab342
Narin Ratana <n.ratana@gmail.com>
2020-01-13 22:03:25 -0800

    Update unit test

--- src/applications/vaos/tests/components/NeedHelp.unit.spec.jsx
+++ src/applications/vaos/tests/components/NeedHelp.unit.spec.jsx
@@ -11,7 +11,7 @@ describe('VAOS <NeedHelp>', () => {
     expect(tree.find('h2').text()).to.contain('Need help?');
 
     const links = tree.find('a');
-    expect(links.length).to.equal(4);
+    expect(links.length).to.equal(5);
     expect(links.at(0).props().href).to.equal('tel:8772228387');
     expect(links.at(0).text()).to.equal('877-222-8387');
     expect(links.at(1).props().href).to.equal('tel:8008778339');
@@ -20,6 +20,10 @@ describe('VAOS <NeedHelp>', () => {
     expect(links.at(2).text()).to.equal('877-222-8387');
     expect(links.at(3).props().href).to.equal('tel:8008778339');
     expect(links.at(3).text()).to.equal('800-877-8339');
+    expect(links.at(4).props().href).to.equal(
+      'https://veteran.apps.va.gov/feedback-web/v1/?appId=85870ADC-CC55-405E-9AC3-976A92BBBBEE',
+    );
+    expect(links.at(4).text()).to.equal('Leave feedback for this application');
     tree.unmount();
   });
 });
