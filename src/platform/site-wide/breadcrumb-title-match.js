c6ffbf4aade90a5ac35b1a779723ee06eeb7f03d
Ethan Teague <metrowebz@gmail.com>
2020-01-14 16:43:30 -0500

    VAGOV-6403: Adding dom loaded listener.

--- src/platform/site-wide/breadcrumb-title-match.js
+++ src/platform/site-wide/breadcrumb-title-match.js
@@ -1,10 +1,13 @@
 // Nodes with identical titles result in url strings with digits appended.
 // This creates ugly breadcrumbs - logic below prevents this.
-const lastCrumb = document.querySelector('#va-breadcrumbs-list li:last-child a')
-  .textContent;
-const pageTitle = document.getElementsByTagName('h1')[0].innerHTML;
-if (pageTitle !== undefined && lastCrumb !== pageTitle) {
-  document.querySelector(
+document.addEventListener('DOMContentLoaded', () => {
+  const lastCrumb = document.querySelector(
     '#va-breadcrumbs-list li:last-child a',
-  ).textContent = pageTitle;
-}
+  ).textContent;
+  const pageTitle = document.getElementsByTagName('h1')[0].innerHTML;
+  if (pageTitle !== undefined && lastCrumb !== pageTitle) {
+    document.querySelector(
+      '#va-breadcrumbs-list li:last-child a',
+    ).textContent = pageTitle;
+  }
+});
