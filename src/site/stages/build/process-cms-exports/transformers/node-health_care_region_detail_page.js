34dd203f1d013b8f532ce7f2b7e1d7d49e4bbd44
Brooks Johnson <brooks.johnson@pm.me>
2020-01-15 09:23:10 -0800

    Use new metatags helper

--- src/site/stages/build/process-cms-exports/transformers/node-health_care_region_detail_page.js
+++ src/site/stages/build/process-cms-exports/transformers/node-health_care_region_detail_page.js
@@ -1,51 +1,31 @@
 const {
   getDrupalValue,
   getTimeAsSeconds,
-  createMetaTag,
+  createMetaTagArray,
 } = require('./helpers');
 
-const transform = entity => {
-  const {
-    metatag: { value: metaTags },
-  } = entity;
-  return {
-    entityType: 'node',
-    entityBundle: 'health_care_region_detail_page',
-    title: getDrupalValue(entity.title),
+const transform = entity => ({
+  entityType: 'node',
+  entityBundle: 'health_care_region_detail_page',
+  title: getDrupalValue(entity.title),
 
-    changed: getTimeAsSeconds(getDrupalValue(entity.changed)),
+  changed: getTimeAsSeconds(getDrupalValue(entity.changed)),
 
-    entityPublished: getDrupalValue(entity.moderationState) === 'published',
-    entityMetatags: [
-      createMetaTag('MetaValue', 'title', metaTags.title),
-      createMetaTag('MetaValue', 'twitter:card', 'summary_large_image'),
-      createMetaTag('MetaProperty', 'og:site_name', metaTags.og_site_name),
-      createMetaTag(
-        'MetaValue',
-        'twitter:description',
-        metaTags.twitter_cards_description,
-      ),
-      createMetaTag('MetaValue', 'description', metaTags.description),
-      createMetaTag('MetaValue', 'twitter:title', metaTags.twitter_cards_title),
-
-      createMetaTag('MetaValue', 'twitter:site', metaTags.twitter_cards_site),
-      createMetaTag('MetaProperty', 'og:title', metaTags.og_title),
-      createMetaTag('MetaProperty', 'og:description', metaTags.og_description),
-    ],
-    entityUrl: {
-      path: entity.path[0].alias,
-    },
-    fieldAlert: getDrupalValue(entity.fieldAlert),
-    fieldContentBlock: entity.fieldContentBlock,
-    fieldFeaturedContent: entity.fieldFeaturedContent,
-    fieldIntroText: getDrupalValue(entity.fieldIntroText),
-    fieldOffice: entity.fieldOffice[0],
-    fieldRelatedLinks: entity.fieldRelatedLinks[0],
-    fieldTableOfContentsBoolean: getDrupalValue(
-      entity.fieldTableOfContentsBoolean,
-    ),
-  };
-};
+  entityPublished: getDrupalValue(entity.moderationState) === 'published',
+  entityMetatags: createMetaTagArray(entity.metatag.value),
+  entityUrl: {
+    path: entity.path[0].alias,
+  },
+  fieldAlert: getDrupalValue(entity.fieldAlert),
+  fieldContentBlock: entity.fieldContentBlock,
+  fieldFeaturedContent: entity.fieldFeaturedContent,
+  fieldIntroText: getDrupalValue(entity.fieldIntroText),
+  fieldOffice: entity.fieldOffice[0],
+  fieldRelatedLinks: entity.fieldRelatedLinks[0],
+  fieldTableOfContentsBoolean: getDrupalValue(
+    entity.fieldTableOfContentsBoolean,
+  ),
+});
 module.exports = {
   filter: [
     'title',
