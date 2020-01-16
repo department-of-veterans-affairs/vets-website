273ac761f34b58ff78acfafa9f85baed95e23008
Brad Conley <brad.conl3y@gmail.com>
2020-01-16 14:19:48 -0500

    Add aria label

--- src/platform/user/profile/vet360/components/base/Vet360ProfileFieldHeading.jsx
+++ src/platform/user/profile/vet360/components/base/Vet360ProfileFieldHeading.jsx
@@ -6,6 +6,7 @@ function Vet360ProfileFieldHeading({ children, onEditClick }) {
       <h3 style={{ display: 'inline-block' }}>{children}</h3>{' '}
       {onEditClick && (
         <button
+          aria-label={`Edit ${children}`}
           type="button"
           data-action="edit"
           onClick={onEditClick}
