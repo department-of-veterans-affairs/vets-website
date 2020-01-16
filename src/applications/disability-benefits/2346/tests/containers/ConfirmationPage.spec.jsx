a904b07b9b59bc7a326857a1a27ab9468d2fe9d0
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-01-16 14:32:23 -0500

    unmounted the components in each test

--- src/applications/disability-benefits/2346/tests/containers/ConfirmationPage.spec.jsx
+++ src/applications/disability-benefits/2346/tests/containers/ConfirmationPage.spec.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ConfirmationPage from '../../containers/ConfirmationPage';
 
-describe("<ConfirmationPage />", () => {
-    it('should be defined', () => {
-        const wrapper = shallow(<ConfirmationPage />);
+describe('<ConfirmationPage />', () => {
+  it('should be defined', () => {
+    const wrapper = shallow(<ConfirmationPage />);
 
-        expect(wrapper).toBeDefined();
-    })
-})
+    expect(wrapper).toBeDefined();
+    wrapper.unmount();
+  });
+});
--- src/applications/disability-benefits/2346/tests/containers/IntroductionPage.spec.jsx
+++ src/applications/disability-benefits/2346/tests/containers/IntroductionPage.spec.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import shallow from 'enzyme';
 import IntroductionPage from '../../containers/IntroductionPage';
 
-describe("<IntroductionPage />", () => {
-    it('should be defined', () => {
-        const wrapper = shallow(<IntroductionPage />);
+describe('<IntroductionPage />', () => {
+  it('should be defined', () => {
+    const wrapper = shallow(<IntroductionPage />);
 
-        expect(wrapper).toBeDefined();
-    })
-})
+    expect(wrapper).toBeDefined();
+    wrapper.unmount();
+  });
+});
