cca82dcfda4109410b5ac660bf4572d3ae80fef8
Mahariel Rosario <maharielrosario@USRMonitors-MacBook-Pro.local>
2020-01-17 16:59:33 -0500

    set up input boxes and checkboxes part 1
    Still need to work out individual state management for checkboxes

--- src/applications/disability-benefits/2346/Components/ConfirmAddress.jsx
+++ src/applications/disability-benefits/2346/Components/ConfirmAddress.jsx
@@ -1,69 +1,101 @@
-import ErrorableCheckbox from '@department-of-veterans-affairs/formation-react/ErrorableCheckbox';
 import ErrorableTextInput from '@department-of-veterans-affairs/formation-react/ErrorableTextInput';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router';
 
-export const ConfirmAddress = () => (
-  <div>
-    <h1>Confirm your shipping address</h1>
-    <p>Your order will take 5-10 business days.</p>
+export const ConfirmAddress = () => {
+  const [checked, setChecked] = useState(false);
+  const handleChange = e => {
+    const value = e.target.checked;
+    const name = e.target.name;
+
+    // eslint-disable-next-line no-debugger
+    debugger;
+    setChecked({
+      ...checked,
+      [name]: value,
+    });
+  };
+  return (
     <div>
-      <ErrorableCheckbox onValueChange={value => value} />
-      <p>Regina Philange</p>
-      <p>
-        1234 Fake St.
-        <br /> Tampa, FL 12345
-      </p>
+      <h1>Confirm your shipping address</h1>
+      <p>Your order will take 5-10 business days.</p>
+      <div>
+        <input
+          name="confirmAddress"
+          id="confirmAddress"
+          type="checkbox"
+          checked={checked}
+          onChange={handleChange}
+        />
+        <label htmlFor="confirmAddress">Confirm Address</label>
+        <p>Regina Philange</p>
+        <p>
+          1234 Fake St.
+          <br /> Tampa, FL 12345
+        </p>
+      </div>
+      <div>
+        <input
+          name="differentAddress"
+          id="differentAddress"
+          type="checkbox"
+          checked={checked}
+          onChange={handleChange}
+        />
+        <label htmlFor="differentAddress">Different Address</label>
+        <p>Different Address</p>
+        <ErrorableTextInput
+          disabled={false}
+          label="Address Line 1"
+          placeholder="Address Line 1"
+          name="addressLine1"
+          required
+          field=""
+          onValueChange={() => {}}
+        />
+        <ErrorableTextInput
+          disabled={false}
+          label="Address Line 2"
+          placeholder="Address Line 2"
+          name="addressLine2"
+          required
+          field=""
+          onValueChange={() => {}}
+        />
+        <ErrorableTextInput
+          disabled={false}
+          label="City"
+          placeholder="City"
+          name="city"
+          required
+          field=""
+          onValueChange={() => {}}
+        />
+        <ErrorableTextInput
+          disabled={false}
+          label="State"
+          placeholder="State"
+          name="state"
+          required
+          field=""
+          onValueChange={() => {}}
+        />
+        <ErrorableTextInput
+          disabled={false}
+          label="Zip"
+          placeholder="Zip Code"
+          name="zip"
+          required
+          field=""
+          onValueChange={() => {}}
+        />
+      </div>
+      <Link to="/home">
+        <button>Back</button>
+      </Link>
+      <Link to="/orderpage">
+        <button>Confirm Address</button>
+      </Link>
     </div>
-    <div>
-      <ErrorableCheckbox onValueChange={value => value} onChange />
-      <p>Different Address</p>
-      <ErrorableTextInput
-        disabled={false}
-        label="Label"
-        placeholder="Address Line 1"
-        name="Name"
-        required
-        onValueChange={field => this.setState({ field })}
-      />
-      <ErrorableTextInput
-        disabled={false}
-        label="Label"
-        placeholder="Address Line 2"
-        name="Name"
-        required
-        onValueChange={field => this.setState({ field })}
-      />
-      <ErrorableTextInput
-        disabled={false}
-        label="Label"
-        placeholder="City"
-        name="Name"
-        required
-        onValueChange={field => this.setState({ field })}
-      />
-      <ErrorableTextInput
-        disabled={false}
-        label="Label"
-        placeholder="State"
-        name="Name"
-        required
-        onValueChange={field => this.setState({ field })}
-      />
-      <ErrorableTextInput
-        disabled={false}
-        label="Label"
-        placeholder="Zip Code"
-        name="Name"
-        required
-        onValueChange={field => this.setState({ field })}
-      />
-    </div>
-    <Link to="/home">
-      <button>Back</button>
-    </Link>
-    <Link to="/orderpage">
-      <button>Confirm Address</button>
-    </Link>
-  </div>
-);
+  );
+};
