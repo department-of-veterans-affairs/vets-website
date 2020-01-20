09db88c25844c45f7a225febf8cb23450f032243
Jeff Balboni <jeff@adhocteam.us>
2020-01-20 10:09:03 -0500

    Fix test

--- src/applications/vaos/tests/e2e/vaos-helpers.js
+++ src/applications/vaos/tests/e2e/vaos-helpers.js
@@ -62,7 +62,7 @@ function appointmentDateTimeTest(client, assertText) {
   client
     .click('.vaos-calendar__calendars button[id^="date-cell"]:not([disabled])')
     .click(
-      'vaos-calendar__cell-current .vaos-calendar__options input[id$="_0"]',
+      '.vaos-calendar__cell-current .vaos-calendar__options input[id$="_0"]',
     )
     .axeCheck('.main')
     .click('.rjsf [type="submit"]')
