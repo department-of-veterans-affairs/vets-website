30ee053f8158c1b7ea8ac7ec05045385e2ad52ab
Christopher Valarida <chris.valarida@adhocteam.us>
2020-01-20 10:04:12 -0800

    Transformed schema

--- src/site/stages/build/process-cms-exports/schemas/transformed/node-health_care_local_facility.js
+++ src/site/stages/build/process-cms-exports/schemas/transformed/node-health_care_local_facility.js
@@ -9,34 +9,70 @@ module.exports = {
         entityBundle: { enum: ['health_care_local_facility'] },
         title: { type: 'string' },
         changed: { type: 'string' },
-        moderationState: { type: 'string' },
-        metatag: { type: 'string' },
-        path: { type: 'string' },
-        fieldAddress: { type: 'string' },
+        entityPublished: { type: 'boolean' },
+        entityMetatags: { $ref: 'MetaTags' },
+        entityUrl: { $ref: 'EntityUrl' },
+        fieldAddress: { type: 'RawAddress' },
         fieldEmailSubscription: { type: 'string' },
         fieldFacebook: { type: 'string' },
-        fieldFacilityHours: { type: 'string' },
+        fieldFacilityHours: {
+          type: 'object',
+          properties: {
+            value: {
+              type: 'array',
+              items: {
+                type: 'array',
+                items: { type: 'string' },
+                // Expect [0] to be the day name and [1] to be the hours
+                minItems: 2,
+                maxItems: 2,
+              },
+            },
+            // Expect all the days of the week
+            minItems: 7,
+            maxItems: 7,
+          },
+          required: ['value'],
+        },
         fieldFacilityLocatorApiId: { type: 'string' },
         fieldFlickr: { type: 'string' },
         fieldInstagram: { type: 'string' },
         fieldIntroText: { type: 'string' },
-        fieldLocalHealthCareService: { type: 'string' },
-        fieldLocationServices: { type: 'string' },
-        fieldMainLocation: { type: 'string' },
-        fieldMedia: { type: 'string' },
+        fieldLocalHealthCareService: {
+          type: 'array',
+          // Alternatively, we can pull out only the bits of this schema that we'll use,
+          // but for now, that' just more work.
+          items: { $ref: 'transformed/node-health_care_local_health_service' },
+        },
+        fieldLocationServices: {
+          type: 'array',
+          items: {
+            $ref: 'transformed/paragraph-health_care_local_facility_servi',
+          },
+        },
+        fieldMainLocation: { type: 'boolean' },
+        fieldMedia: {
+          oneOf: [{ $ref: 'Media' }, { type: 'null' }],
+        },
         fieldMentalHealthPhone: { type: 'string' },
         fieldNicknameForThisFacility: { type: 'string' },
+        // Could probably be an enum, but it's not clear what all the possible values are
         fieldOperatingStatusFacility: { type: 'string' },
         fieldPhoneNumber: { type: 'string' },
-        fieldRegionPage: { type: 'string' },
+        fieldRegionPage: {
+          oneOf: [
+            { $ref: 'transformed/node-health_care_region_page' },
+            { type: 'null' },
+          ],
+        },
         fieldTwitter: { type: 'string' },
       },
       required: [
         'title',
         'changed',
-        'moderationState',
-        'metatag',
-        'path',
+        'entityPublished',
+        'entityMetatags',
+        'entityUrl',
         'fieldAddress',
         'fieldEmailSubscription',
         'fieldFacebook',
