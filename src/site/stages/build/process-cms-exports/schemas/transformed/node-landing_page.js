69b4f8be6d93b99db34324bff3e0009492a8fe8d
Brooks Johnson <brooks.johnson@pm.me>
2020-01-22 17:10:06 -0800

    Get transformer & schema closer to expected output
    Still some issues to be resolved

--- src/site/stages/build/process-cms-exports/schemas/transformed/node-landing_page.js
+++ src/site/stages/build/process-cms-exports/schemas/transformed/node-landing_page.js
@@ -9,24 +9,52 @@ module.exports = {
         entityBundle: { enum: ['landing_page'] },
         title: { type: 'string' },
         changed: { type: 'string' },
-        path: { type: 'string' },
-        fieldAdministration: { type: 'string' },
-        fieldAlert: { type: 'string' },
+        entityUrl: { $ref: 'EntityUrl' },
+        fieldAdministration: {
+          $ref: 'transformed/taxonomy_term-administration',
+        },
+        fieldAlert: { type: ['string', 'null'] },
         fieldDescription: { type: 'string' },
         fieldIntroText: { type: 'string' },
-        fieldLinks: { type: 'string' },
+        fieldLinks: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              title: { type: 'string' },
+              url: {
+                type: 'object',
+                properties: {
+                  path: { type: 'string' },
+                },
+                required: ['path'],
+              },
+            },
+            required: ['title', 'url'],
+          },
+        },
         fieldPageLastBuilt: { type: 'string' },
-        fieldPlainlanguageDate: { type: 'string' },
-        fieldPromo: { type: 'string' },
-        fieldRelatedLinks: { type: 'string' },
-        fieldSpokes: { type: 'string' },
-        fieldSupportServices: { type: 'string' },
+        fieldPlainlanguageDate: { type: ['string', 'null'] },
+        fieldPromo: { $ref: 'transformed/block_content-promo' },
+        fieldRelatedLinks: {
+          $ref: 'transformed/paragraph-list_of_link_teasers',
+        },
+        fieldSpokes: {
+          type: 'array',
+          items: {
+            $ref: 'transformed/paragraph-list_of_link_teasers',
+          },
+        },
+        fieldSupportServices: {
+          type: 'array',
+          items: { $ref: 'transformed/node-support_service' },
+        },
         fieldTitleIcon: { type: 'string' },
       },
       required: [
         'title',
         'changed',
-        'path',
+        'entityUrl',
         'fieldAdministration',
         'fieldAlert',
         'fieldDescription',
