d8ccff8562550bd4d93467558282b67df61e27f1
Christopher Valarida <chris.valarida@adhocteam.us>
2020-01-23 15:47:30 -0800

    Update transformed schema

--- src/site/stages/build/process-cms-exports/schemas/transformed/node-vamc_operating_status_and_alerts.js
+++ src/site/stages/build/process-cms-exports/schemas/transformed/node-vamc_operating_status_and_alerts.js
@@ -8,11 +8,24 @@ module.exports = {
     entityPublished: { type: 'boolean' },
     entityMetatags: { $ref: 'MetaTags' },
     entityUrl: { $ref: 'EntityUrl' },
-    fieldBannerAlert: { type: 'string' },
-    fieldFacilityOperatingStatus: { type: 'string' },
-    fieldLinks: { type: 'string' },
-    fieldOffice: { type: 'string' },
-    fieldOperatingStatusEmergInf: { type: 'string' },
+    fieldBannerAlert: { $ref: 'transformed/node-full_width_banner_alert' },
+    fieldFacilityOperatingStatus: {
+      type: 'array',
+      items: { $ref: 'transformed/node-health_care_local_facility' },
+    },
+    fieldLinks: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          title: { type: 'string' },
+          uri: { type: 'string' },
+        },
+        required: ['title', 'uri'],
+      },
+    },
+    fieldOffice: { $ref: 'transformed/node-health_care_region_page' },
+    fieldOperatingStatusEmergInf: { $ref: 'GenericNestedString' },
   },
   required: [
     'title',
