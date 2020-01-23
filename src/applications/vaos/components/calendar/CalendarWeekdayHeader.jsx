8e7f0d7699c29095f220b27ebfd5836fc1647928
Jeff Balboni <jeff@adhocteam.us>
2020-01-23 14:42:40 -0500

    Revert format

--- src/applications/vaos/components/calendar/CalendarWeekdayHeader.jsx
+++ src/applications/vaos/components/calendar/CalendarWeekdayHeader.jsx
@@ -23,29 +23,29 @@ const days = [
   },
 ];
 
-export default function CalendarWeekdayHeader() {
-  return (
-    <div role="rowgroup">
-      <div
-        className="vaos-calendar__weekday-container vads-u-display--flex vads-u-justify-content--space-between"
-        role="row"
-      >
-        {days.map((day, index) => (
-          <div
-            key={`weekday-${index}`}
-            role="columnheader"
-            className="vaos-calendar__weekday vads-u-font-weight--bold vads-u-text-align--center vads-u-margin-bottom--0p5"
-          >
-            <span className="vads-u-display--none small-screen:vads-u-display--inline">
-              {day.name}
-            </span>
-            <span className="small-screen:vads-u-display--none">
-              <span aria-hidden="true">{day.abbr}</span>
-              <span className="sr-only">{day.name}</span>
-            </span>
-          </div>
-        ))}
-      </div>
+const CalendarWeekdayHeader = () => (
+  <div role="rowgroup">
+    <div
+      className="vaos-calendar__weekday-container vads-u-display--flex vads-u-justify-content--space-between"
+      role="row"
+    >
+      {days.map((day, index) => (
+        <div
+          key={`weekday-${index}`}
+          role="columnheader"
+          className="vaos-calendar__weekday vads-u-font-weight--bold vads-u-text-align--center vads-u-margin-bottom--0p5"
+        >
+          <span className="vads-u-display--none small-screen:vads-u-display--inline">
+            {day.name}
+          </span>
+          <span className="small-screen:vads-u-display--none">
+            <span aria-hidden="true">{day.abbr}</span>
+            <span className="sr-only">{day.name}</span>
+          </span>
+        </div>
+      ))}
     </div>
-  );
-}
+  </div>
+);
+
+export default CalendarWeekdayHeader;
