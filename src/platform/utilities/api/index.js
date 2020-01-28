6501ffbe371c85cebcc75f2e48208753558c96dc
Kelson Adams <kadams@governmentcio.com>
2020-01-28 12:30:49 -0700

    Undo a commit that added a tweak to the apiRequest URL

--- src/platform/utilities/api/index.js
+++ src/platform/utilities/api/index.js
@@ -44,7 +44,7 @@ function isJson(response) {
  * @param {Function} **(DEPRECATED)** error - Callback to execute if the fetch fails to resolve.
  */
 export function apiRequest(resource, optionalSettings = {}, success, error) {
-  const baseUrl = `${environment.API_URL}.va.gov/v0`;
+  const baseUrl = `${environment.API_URL}/v0`;
   const url = resource[0] === '/' ? [baseUrl, resource].join('') : resource;
 
   if (success) {
