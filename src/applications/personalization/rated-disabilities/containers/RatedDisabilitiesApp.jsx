2036683272df8fe1173875d6ea9c666dbb21c934
Micah Chiang <micahkchiang@gmail.com>
2020-01-29 12:02:48 -0800

    Update e2e test, breadcrumbs and vagovprod flag.

--- src/applications/personalization/rated-disabilities/containers/RatedDisabilitiesApp.jsx
+++ src/applications/personalization/rated-disabilities/containers/RatedDisabilitiesApp.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import backendServices from 'platform/user/profile/constants/backendServices';
+import Breadcrumbs from '@department-of-veterans-affairs/formation-react/Breadcrumbs';
 import DowntimeNotification, {
   externalServices,
 } from 'platform/monitoring/DowntimeNotification';
@@ -14,6 +15,32 @@ class RatedDisabilitiesApp extends React.Component {
     const { ratedDisabilities } = this.props.ratedDisabilities;
     return (
       <>
+        <div className="medium-screen:vads-u-padding-left--1p5 large-screen:vads-u-padding-left--6">
+          <Breadcrumbs>
+            {[
+              <a href="/" aria-label="back to VA Home page" key="1">
+                Home
+              </a>,
+              <a
+                href="/disability"
+                aria-label="Back to the Disability Benefits page"
+                key="2"
+              >
+                Disability Benefits
+              </a>,
+              <a
+                href="/disability/view-disability-rating"
+                aria-label="back to the view your VA disability rating page"
+                key="3"
+              >
+                View your VA disability rating
+              </a>,
+              <a href="/disability/view-disability-rating/rating" key="4">
+                Your VA disability rating
+              </a>,
+            ]}
+          </Breadcrumbs>
+        </div>
         <RequiredLoginView
           authRequired={1}
           serviceRequired={backendServices.USER_PROFILE}
