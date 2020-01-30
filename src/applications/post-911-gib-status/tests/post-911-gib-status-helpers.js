9eff9d4b51e32e17693f10536f7cb67b828849f4
Jeff Balboni <jeff@adhocteam.us>
2020-01-30 16:25:22 -0500

    Add feature flag mock

--- src/applications/post-911-gib-status/tests/post-911-gib-status-helpers.js
+++ src/applications/post-911-gib-status/tests/post-911-gib-status-helpers.js
@@ -99,6 +99,15 @@ function initApplicationMock(token) {
     verb: 'get',
     value: backendStatus,
   });
+  mock(token, {
+    path: '/v0/feature_toggles',
+    verb: 'get',
+    value: {
+      data: {
+        features: [],
+      },
+    },
+  });
 }
 
 module.exports = {
