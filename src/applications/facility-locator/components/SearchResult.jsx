af9b2c66292e3ede64ebafdc3363f3ad64476b43
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-01-31 16:21:30 -0600

    Add search result literal in from

--- src/applications/facility-locator/components/SearchResult.jsx
+++ src/applications/facility-locator/components/SearchResult.jsx
@@ -7,13 +7,9 @@ import LocationDirectionsLink from './search-results/LocationDirectionsLink';
 // revert to stateless component given: 19fd5178f
 const SearchResult = ({ result, query }) => (
   <div className="facility-result" id={result.id}>
-    <LocationInfoBlock
-      location={result}
-      from={SearchResult.name}
-      query={query}
-    />
-    <LocationDirectionsLink location={result} from={SearchResult.name} />
-    <LocationPhoneLink location={result} from={SearchResult.name} />
+    <LocationInfoBlock location={result} from={'SearchResult'} query={query} />
+    <LocationDirectionsLink location={result} from={'SearchResult'} />
+    <LocationPhoneLink location={result} from={'SearchResult'} />
   </div>
 );
 
