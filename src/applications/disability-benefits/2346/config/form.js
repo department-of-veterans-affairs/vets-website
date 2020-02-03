4c43b3dd2ca158591f0bcb40fdb6937043d14c4e
mr0sari0 <mahariel.rosario@gmail.com>
2020-02-03 14:59:19 -0500

    set up confirmation page initial draft

--- src/applications/disability-benefits/2346/config/form.js
+++ src/applications/disability-benefits/2346/config/form.js
@@ -49,7 +49,7 @@ const formPages = {
 
 const formConfig = {
   urlPrefix: '/',
-  // submitUrl: '/v0/api',
+  submitUrl: 'http://localhost:3004/posts',
   submit: () =>
     Promise.resolve({ attributes: { confirmationNumber: '123123123' } }),
   trackingPrefix: 'complex-form-',
