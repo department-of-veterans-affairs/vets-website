7fedf5d8bbb93a9eaeadfd4e28a25934b4cfdefb
Jeff Balboni <jeff@adhocteam.us>
2020-02-04 10:16:14 -0500

    WIP

--- src/applications/vaos/routes.jsx
+++ src/applications/vaos/routes.jsx
@@ -1,46 +1,139 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import NewAppointmentLayout from './components/NewAppointmentLayout';
+import asyncLoader from 'platform/utilities/ui/asyncLoader';
 import AppointmentsPage from './containers/AppointmentsPage';
-import ReasonForAppointmentPage from './containers/ReasonForAppointmentPage';
-import TypeOfCarePage from './containers/TypeOfCarePage';
-import CommunityCarePreferencesPage from './containers/CommunityCarePreferencesPage';
-import TypeOfAudiologyCarePage from './containers/TypeOfAudiologyCarePage';
-import TypeOfFacilityPage from './containers/TypeOfFacilityPage';
-import ContactInfoPage from './containers/ContactInfoPage';
-import VAFacilityPage from './containers/VAFacilityPage';
-import TypeOfVisitPage from './containers/TypeOfVisitPage';
-import ReviewPage from './containers/ReviewPage';
-import ClinicChoicePage from './containers/ClinicChoicePage';
-import TypeOfSleepCarePage from './containers/TypeOfSleepCarePage';
-import PreferredDatePage from './containers/PreferredDatePage';
-import DateTimeRequestPage from './containers/DateTimeRequestPage';
-import DateTimeSelectPage from './containers/DateTimeSelectPage';
-import ConfirmationPage from './containers/ConfirmationPage';
 import VAOSApp from './containers/VAOSApp';
 
 const routes = (
   <Route path="/" component={VAOSApp}>
     <IndexRoute component={AppointmentsPage} />
-    <Route path="new-appointment" component={NewAppointmentLayout}>
-      <Route path="reason-appointment" component={ReasonForAppointmentPage} />
-      <IndexRoute component={TypeOfCarePage} />
-      <Route path="contact-info" component={ContactInfoPage} />
-      <Route path="choose-facility-type" component={TypeOfFacilityPage} />
-      <Route path="choose-visit-type" component={TypeOfVisitPage} />
-      <Route path="choose-sleep-care" component={TypeOfSleepCarePage} />
-      <Route path="audiology" component={TypeOfAudiologyCarePage} />
-      <Route path="preferred-date" component={PreferredDatePage} />
-      <Route path="request-date" component={DateTimeRequestPage} />
-      <Route path="select-date" component={DateTimeSelectPage} />
-      <Route path="va-facility" component={VAFacilityPage} />
+    <Route
+      path="new-appointment"
+      component={asyncLoader(() =>
+        import(/* webpackChunkName: "vaos-form" */ './components/NewAppointmentLayout').then(
+          m => m.default,
+        ),
+      )}
+    >
+      <IndexRoute
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/TypeOfCarePage').then(
+            m => m.default,
+          ),
+        )}
+      />
+      <Route
+        path="contact-info"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/ContactInfoPage').then(
+            m => m.default,
+          ),
+        )}
+      />
+      <Route
+        path="choose-facility-type"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/TypeOfFacilityPage').then(
+            m => m.default,
+          ),
+        )}
+      />
+      <Route
+        path="choose-visit-type"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/TypeOfVisitPage').then(
+            m => m.default,
+          ),
+        )}
+      />
+      <Route
+        path="choose-sleep-care"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/TypeOfSleepCarePage').then(
+            m => m.default,
+          ),
+        )}
+      />
+      <Route
+        path="audiology"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/TypeOfAudiologyCarePage').then(
+            m => m.default,
+          ),
+        )}
+      />
+      <Route
+        path="preferred-date"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/PreferredDatePage').then(
+            m => m.default,
+          ),
+        )}
+      />
+      <Route
+        path="request-date"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/DateTimeRequestPage').then(
+            m => m.default,
+          ),
+        )}
+      />
+      <Route
+        path="select-date"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/DateTimeSelectPage').then(
+            m => m.default,
+          ),
+        )}
+      />
+      <Route
+        path="va-facility"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/VAFacilityPage').then(
+            m => m.default,
+          ),
+        )}
+      />
       <Route
         path="community-care-preferences"
-        component={CommunityCarePreferencesPage}
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/CommunityCarePreferencesPage').then(
+            m => m.default,
+          ),
+        )}
       />
-      <Route path="clinics" component={ClinicChoicePage} />
-      <Route path="review" component={ReviewPage} />
-      <Route path="confirmation" component={ConfirmationPage} />
+      <Route
+        path="clinics"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/ClinicChoicePage').then(
+            m => m.default,
+          ),
+        )}
+      />
+      <Route
+        path="reason-appointment"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/ReasonForAppointmentPage').then(
+            m => m.default,
+          ),
+        )}
+      />
+      <Route
+        path="review"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/ReviewPage').then(
+            m => m.default,
+          ),
+        )}
+      />
+      <Route
+        path="confirmation"
+        component={asyncLoader(() =>
+          import(/* webpackChunkName: "vaos-form" */ './containers/ConfirmationPage').then(
+            m => m.default,
+          ),
+        )}
+      />
     </Route>
   </Route>
 );
