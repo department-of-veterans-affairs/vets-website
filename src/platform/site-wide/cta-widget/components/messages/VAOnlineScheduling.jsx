0e7ba272b030a88531ab8b80c8e42feb1121656e
Jeff Balboni <jeff@adhocteam.us>
2020-02-04 13:43:59 -0500

    Fix headings

--- src/platform/site-wide/cta-widget/components/messages/VAOnlineScheduling.jsx
+++ src/platform/site-wide/cta-widget/components/messages/VAOnlineScheduling.jsx
@@ -2,36 +2,27 @@ import React from 'react';
 import CallToActionAlert from './../CallToActionAlert';
 import { widgetTypes } from '../../helpers';
 
-const headerAndDesc = {
-  [widgetTypes.VIEW_APPOINTMENTS]: {
-    heading: 'View your VA appointments online',
-    alertText: (
-      <p>
-        <strong>We’ve launched a new online scheduling tool on VA.gov.</strong>
-        <br />
-        We invite you to try our VA.gov scheduling tool to view your
-        appointments online. With this tool, you’ll also be able to see any
-        appointments you’ve made through My HealtheVet.
-      </p>
-    ),
-  },
-  [widgetTypes.SCHEDULE_APPOINTMENTS]: {
-    heading: 'Schedule or cancel a VA appointment online',
-    alertText: (
-      <p>
-        <strong>We’ve launched a new online scheduling tool on VA.gov.</strong>
-        <br />
-        We invite you to try our VA.gov scheduling tool to schedule or cancel
-        your appointments online. With this tool, you’ll also be able to see any
-        appointments you’ve made through My HealtheVet.
-      </p>
-    ),
-  },
+const alertText = {
+  [widgetTypes.VIEW_APPOINTMENTS]: (
+    <p>
+      We invite you to try our VA.gov scheduling tool to view your appointments
+      online. With this tool, you’ll also be able to see any appointments you’ve
+      made through My HealtheVet.
+    </p>
+  ),
+  [widgetTypes.SCHEDULE_APPOINTMENTS]: (
+    <p>
+      We invite you to try our VA.gov scheduling tool to schedule or cancel your
+      appointments online. With this tool, you’ll also be able to see any
+      appointments you’ve made through My HealtheVet.
+    </p>
+  ),
 };
 
 const VAOnlineScheduling = ({ appId }) => {
   const content = {
-    ...headerAndDesc[appId],
+    heading: 'We’ve launched a new online scheduling tool on VA.gov',
+    alertText: alertText[appId],
     primaryButtonText: 'Go to VA.gov scheduling tool',
     primaryButtonHandler: () => {
       window.location =
