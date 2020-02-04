ce27eaefb27da31ed758b84801259b736d5601e9
Brooks Johnson <brooks.johnson@pm.me>
2020-02-04 12:12:41 -0800

    Remove docker container & try to run git diff
    This is to avoid having to have the git repo inside the docker container

--- .github/actions/hello-world-action/index.js
+++ .github/actions/hello-world-action/index.js
@@ -1,15 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+async function getDiff() {
+  const exitCode = await exec.exec('git', [
+    'diff',
+    'origin/master...',
+    '-G"eslint-disable"',
+    '--exit-code',
+  ]);
+
+  return exitCode;
+}
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput('time', time);
+  // const time = new Date().toTimeString();
+  // core.setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2);
+  // console.log(`The event payload: ${payload}`);
+
+  const exitCode = getDiff();
+
+  console.log(`The git diff exit code: ${exitCode}`);
 } catch (error) {
   core.setFailed(error.message);
 }
