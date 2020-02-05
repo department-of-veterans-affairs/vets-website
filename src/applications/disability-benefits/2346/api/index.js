68bec97c3e81af7447cdaee913ff4870eac71077
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-02-05 10:12:17 -0500

    changed credentials from same-origin to include

--- src/applications/disability-benefits/2346/api/index.js
+++ src/applications/disability-benefits/2346/api/index.js
@@ -10,7 +10,7 @@ export const getUserInformation = () =>
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
     mode: 'cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
+    credentials: 'include', // include, *same-origin, omit
     headers: {
       // 'Content-Type': 'application/json',
       'Content-Type': 'application/x-www-form-urlencoded',
