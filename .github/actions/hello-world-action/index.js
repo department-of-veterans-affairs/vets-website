a8deda7243140cf34309f8147a97100562f77395
Brooks Johnson <brooks.johnson@pm.me>
2020-02-04 20:21:36 -0800

    Remove unused output

--- .github/actions/hello-world-action/index.js
+++ .github/actions/hello-world-action/index.js
@@ -3,8 +3,6 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 try {
-  // const time = new Date().toTimeString();
-  // core.setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2);
   // console.log(`The event payload: ${payload}`);
