09ca6650db88b804ada03b519cfa4bb91f450962
Brooks Johnson <brooks.johnson@pm.me>
2020-02-04 16:46:16 -0800

    Try to make review comment on specific line

--- .github/actions/hello-world-action/index.js
+++ .github/actions/hello-world-action/index.js
@@ -26,16 +26,19 @@ try {
     })
     .catch(err => {
       console.log(`The error: ${err}`);
-      const { GITHUB_TOKEN, GITHUB_REPOSITORY, PR } = process.env;
+      const { GITHUB_SHA, GITHUB_REPOSITORY, PR } = process.env;
       const url = `https://api.github.com/${GITHUB_REPOSITORY}/issues/${PR}/comments`;
 
       const token = core.getInput('token');
       const octokit = new github.GitHub(token);
-      octokit.issues.createComment({
+      octokit.pulls.createComment({
         owner: 'department-of-veterans-affairs',
         repo: 'vets-website',
-        issue_number: PR,
+        pull_number: PR,
         body: 'Testing',
+        commit_id: GITHUB_SHA,
+        path: 'src/site/stages/build/process-cms-exports/schema-validation.js',
+        line: 13,
       });
     });
 } catch (error) {
