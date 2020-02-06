d3ac9c9b95a84e65a4df0d91179cd0abef71ef8a
Brad Conley <brad.conl3y@gmail.com>
2020-02-06 15:43:44 -0500

    single out problematic func

--- src/platform/forms-system/src/js/fields/BasicArrayField.jsx
+++ src/platform/forms-system/src/js/fields/BasicArrayField.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import set from 'platform/utilities/data/set';
-import get from 'lodash/get';
+import _ from 'lodash/fp'; // eslint-disable-line no-restricted-imports
+import get from 'platform/utilities/data/get';
 import classNames from 'classnames';
 import Scroll from 'react-scroll';
 
@@ -18,7 +18,7 @@ class BasicArrayField extends React.Component {
     !deepEquals(this.props, nextProps) || nextState !== this.state;
 
   onItemChange = (indexToChange, value) => {
-    const newItems = set(indexToChange, value, this.props.formData) || [];
+    const newItems = _.set(indexToChange, value, this.props.formData || []);
     this.props.onChange(newItems);
   };
 
@@ -41,7 +41,7 @@ class BasicArrayField extends React.Component {
     const { TitleField, SchemaField } = registry.fields;
 
     const title = uiSchema['ui:title'] || schema.title;
-    const hideTitle = !!get(uiSchema, ['ui:options', 'hideTitle']);
+    const hideTitle = !!get(['ui:options', 'hideTitle'], uiSchema);
     const description = uiSchema['ui:description'];
     const textDescription =
       typeof description === 'string' ? description : null;
