30820bc6f70f4350cfa3f8b4c3ac045ed9ced2e6
Erik P. Hansen <eph@erikphansen.com>
2020-02-06 07:13:14 -0800

    Make new address form that uses SchemaForm (#11601)
    * Make new address form that uses SchemaForm
    
    WIP: making new address schema form
    
    Mostly cleanup + one function rename
    
    Update schema when livesOnMilitaryBase
    
    Update the options for country, city, and state when the "lives on military base" checkbox is selected.
    
    update country when 'lives on base' is checked
    
    Add form system option to not delete hidden field data
    
    Stop cleaning data on each data change
    
    to stop deleting data for fields that are no longer being shown. We want to persist that data in case the user wants to see those fields again. We will clean up on submission instead.
    
    Refactor DOM of Vet360EditModal
    
    we don't want to wrap the SchemaForm in a form since the SchemaForm renders its own form element
    
    fix failing unit test
    
    Only show checkbox when we should
    
    * Fix copy mailing address functionality
    
    * remove unused attribute
    
    * refactor MILITARY_STATES creation
    
    * Force country to USA when 'livesOnBase' is checked
    
    Create a simple wrapper function to transform the form data before calling the Redux action creator.
    
    * Optionally pre-check the `livesOnBase` checkbox
    
    When popping open the edit mailing address modal, if the current address is on a military base, the corresponding checkbox should be pre-selected.
    
    * Gray out disabled select fields
    
    * disable country dropdown when needed
    
    * refactor ui unit test
    
    * move schemas, etc. to their own file
    
    * Trigger Jenkins
    
    * Further cleaning of initial form data
    
    String fields were getting set to empty strings which led to form validation errors or worse if they were set to null initially the app would crash due to null not being a valid value for the string field.
    
    * Trigger Jenkins
    
    * fix e2e test

--- src/platform/user/profile/vet360/components/AddressField/AddressEditModal.jsx
+++ src/platform/user/profile/vet360/components/AddressField/AddressEditModal.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import pickBy from 'lodash/pickBy';
 
-import { FIELD_NAMES, ADDRESS_FORM_VALUES, USA } from 'vet360/constants';
+import {
+  ADDRESS_FORM_VALUES,
+  ADDRESS_POU,
+  ADDRESS_TYPES,
+  FIELD_NAMES,
+  USA,
+} from 'vet360/constants';
 
 import Vet360EditModal from '../base/Vet360EditModal';
 
 import CopyMailingAddress from 'vet360/containers/CopyMailingAddress';
 import AddressForm from './AddressForm';
+import AddressFormV2 from './AddressFormV2';
+
+import environment from 'platform/utilities/environment';
+
+const useNewAddressForm = !environment.isProduction();
 
 class AddressEditModal extends React.Component {
   onBlur = field => {
@@ -21,35 +33,120 @@ class AddressEditModal extends React.Component {
     this.props.onChange(newFieldValue, field, true);
   };
 
+  onInputV2 = (value, schema, uiSchema) => {
+    const newFieldValue = {
+      ...value,
+    };
+    if (newFieldValue['view:livesOnMilitaryBase']) {
+      newFieldValue.countryName = USA.COUNTRY_NAME;
+    }
+    this.props.onChangeFormDataAndSchemas(newFieldValue, schema, uiSchema);
+  };
+
   getInitialFormValues = () =>
     this.props.modalData ||
-    this.props.data || { countryName: USA.COUNTRY_NAME };
+    this.transformInitialFormValues(this.props.data) || {
+      countryName: USA.COUNTRY_NAME,
+    };
 
   getIsMailingAddress = () =>
-    this.props.title.toLowerCase().includes('mailing');
+    this.props.fieldName === FIELD_NAMES.MAILING_ADDRESS;
+
+  /**
+   * Returns a copy of the input object with keys removed for values that are
+   * falsy
+   *
+   */
+  removeEmptyKeys = data =>
+    pickBy(
+      {
+        id: data.id,
+        addressLine1: data.addressLine1,
+        addressLine2: data.addressLine2,
+        addressLine3: data.addressLine3,
+        addressType: data.addressType,
+        city: data.city,
+        countryName: data.countryName,
+        stateCode: data.stateCode,
+        internationalPostalCode: data.internationalPostalCode,
+        zipCode: data.zipCode,
+        province: data.province,
+        addressPou: data.Pou,
+      },
+      e => !!e,
+    );
+
+  /**
+   * Returns a copy of the input object with an added `view:livesOnMilitaryBase`
+   * value if the address is a overseas military mailing address
+   *
+   */
+  selectLivesOnMilitaryBaseCheckbox = data => {
+    if (
+      data?.addressPou === ADDRESS_POU.CORRESPONDENCE &&
+      data?.addressType === ADDRESS_TYPES.OVERSEAS_MILITARY
+    ) {
+      return { ...data, 'view:livesOnMilitaryBase': true };
+    }
+    return data;
+  };
+
+  /**
+   * Helper function that:
+   * - totally removes data fields that are not set
+   * - sets the form data's `view:livesOnMilitaryBase` prop to `true` if this is
+   *   an overseas military mailing address
+   */
+  transformInitialFormValues = initialFormValues => {
+    let transformedData = this.removeEmptyKeys(initialFormValues);
+    transformedData = this.selectLivesOnMilitaryBaseCheckbox(transformedData);
+    return transformedData;
+  };
 
   copyMailingAddress = mailingAddress => {
     const newAddressValue = { ...this.props.field.value, ...mailingAddress };
-    this.props.onChange(newAddressValue, null, true);
+    if (useNewAddressForm) {
+      this.props.onChangeFormDataAndSchemas(
+        newAddressValue,
+        this.props.field.formSchema,
+        this.props.field.uiSchema,
+      );
+    } else {
+      this.props.onChange(newAddressValue, null, true);
+    }
   };
 
-  renderForm = () => (
+  renderForm = (formButtons, onSubmit) => (
     <div>
       {this.props.fieldName === FIELD_NAMES.RESIDENTIAL_ADDRESS && (
         <CopyMailingAddress
           convertNextValueToCleanData={this.props.convertNextValueToCleanData}
           copyMailingAddress={this.copyMailingAddress}
+          useNewAddressForm={useNewAddressForm}
         />
       )}
-      <AddressForm
-        isMailingAddress={this.getIsMailingAddress()}
-        address={this.props.field.value}
-        onInput={this.onInput}
-        onBlur={this.onBlur}
-        errorMessages={this.props.field.validations}
-        states={ADDRESS_FORM_VALUES.STATES}
-        countries={ADDRESS_FORM_VALUES.COUNTRIES}
-      />
+      {useNewAddressForm && (
+        <AddressFormV2
+          address={this.props.field.value}
+          formSchema={this.props.field.formSchema}
+          uiSchema={this.props.field.uiSchema}
+          onUpdateFormData={this.onInputV2}
+          onSubmit={onSubmit}
+        >
+          {formButtons}
+        </AddressFormV2>
+      )}
+      {!useNewAddressForm && (
+        <AddressForm
+          isMailingAddress={this.getIsMailingAddress()}
+          address={this.props.field.value}
+          onInput={this.onInput}
+          onBlur={this.onBlur}
+          errorMessages={this.props.field.validations}
+          states={ADDRESS_FORM_VALUES.STATES}
+          countries={ADDRESS_FORM_VALUES.COUNTRIES}
+        />
+      )}
     </div>
   );
 
@@ -57,9 +154,9 @@ class AddressEditModal extends React.Component {
     return (
       <Vet360EditModal
         getInitialFormValues={this.getInitialFormValues}
-        onBlur={this.onBlur}
-        onSubmit={this.onSubmit}
+        onBlur={useNewAddressForm ? null : this.onBlur}
         render={this.renderForm}
+        useNewAddressForm={useNewAddressForm}
         {...this.props}
       />
     );
