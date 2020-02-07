29d34c991299813c4f2eff2290e55f0bdd47bf43
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-02-07 11:53:10 -0800

    added massive tests

--- src/applications/personalization/view-dependents/tests/containers/ViewDependentsApp.unit.spec.jsx
+++ src/applications/personalization/view-dependents/tests/containers/ViewDependentsApp.unit.spec.jsx
@@ -6,30 +6,24 @@ import { ViewDependentsApp } from '../../containers/ViewDependentsApp';
 
 describe('<ViewDependentsApp />', () => {
   it('should render a ViewDependentsLayout', () => {
-    const stub = sinon.stub(
-      ViewDependentsApp.prototype,
-      'divideIntoOnAwardAndNot',
-    );
+    const fetchAllDependentsMock = sinon.spy();
     const wrapper = shallow(
-      <ViewDependentsApp>
+      <ViewDependentsApp fetchAllDependents={fetchAllDependentsMock}>
         <div>App Children</div>
       </ViewDependentsApp>,
     );
     expect(wrapper.find('ViewDependentsLayout').length).to.equal(1);
-    expect(stub.calledOnce).to.be.true;
-    expect(wrapper.state('allDependents')).to.exist;
     wrapper.unmount();
   });
 
   it('should render a RequiredLoginView', () => {
+    const fetchAllDependentsMock = sinon.spy();
     const wrapper = shallow(
-      <ViewDependentsApp>
+      <ViewDependentsApp fetchAllDependents={fetchAllDependentsMock}>
         <div>App Children</div>
       </ViewDependentsApp>,
     );
     expect(wrapper.find('RequiredLoginView').length).to.equal(1);
     wrapper.unmount();
   });
-
-  it('should split people into onAward and notOnAward', () => {});
 });
