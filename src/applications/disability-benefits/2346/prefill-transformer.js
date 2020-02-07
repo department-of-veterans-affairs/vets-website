213f853cbf2c828bc75062cb1a1101960e309eff
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-02-06 20:24:40 -0500

    refactored paths

--- src/applications/disability-benefits/2346/prefill-transformer.js
+++ src/applications/disability-benefits/2346/prefill-transformer.js
@@ -1,4 +1,4 @@
-import _ from '../../../platform/utilities/data';
+import _ from 'platform/utilities/data';
 import { SERVICE_CONNECTION_TYPES, disabilityActionTypes } from './constants';
 import { viewifyFields } from './utils';
 
