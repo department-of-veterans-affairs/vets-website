fbc79409ae00f35d3e4547e53caccc33a6ac8d34
Brad Conley <brad.conl3y@gmail.com>
2020-02-10 12:28:23 -0500

    wip

--- src/applications/claims-status/reducers/claimsV2.js
+++ src/applications/claims-status/reducers/claimsV2.js
@@ -1,4 +1,5 @@
-import _ from 'lodash/fp';
+import set from 'platform/utilities/data/set';
+import merge from 'lodash/merge';
 import {
   FETCH_CLAIMS_PENDING,
   FETCH_CLAIMS_SUCCESS,
@@ -29,57 +30,81 @@ const initialState = {
 export default function claimsV2Reducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_CLAIMS_PENDING:
-      return _.set('claimsLoading', true, state);
+      return set('claimsLoading', true, state);
     case FETCH_CLAIMS_SUCCESS:
-      return _.merge(state, {
-        claims: action.claims,
-        claimsLoading: false,
-        pages: action.pages,
-        claimsAvailability: claimsAvailability.AVAILABLE,
-      });
+      return merge(
+        {
+          claims: action.claims,
+          claimsLoading: false,
+          pages: action.pages,
+          claimsAvailability: claimsAvailability.AVAILABLE,
+        },
+        state,
+      );
     case FETCH_CLAIMS_ERROR:
       // TO-DO: Parse errors out
-      return _.merge(state, {
-        claimsLoading: false,
-        claimsAvailability: claimsAvailability.UNAVAILABLE,
-      });
+      return merge(
+        {
+          claimsLoading: false,
+          claimsAvailability: claimsAvailability.UNAVAILABLE,
+        },
+        state,
+      );
     case FETCH_APPEALS_PENDING:
-      return _.set('appealsLoading', true, state);
+      return set('appealsLoading', true, state);
     case FETCH_APPEALS_SUCCESS:
-      return _.merge(state, {
-        appeals: action.appeals,
-        appealsLoading: false,
-        available: true,
-        v2Availability: appealsAvailability.AVAILABLE,
-      });
+      return merge(
+        {
+          appeals: action.appeals,
+          appealsLoading: false,
+          available: true,
+          v2Availability: appealsAvailability.AVAILABLE,
+        },
+        state,
+      );
     case USER_FORBIDDEN_ERROR:
-      return _.merge(state, {
-        appealsLoading: false,
-        v2Availability: appealsAvailability.USER_FORBIDDEN_ERROR,
-      });
+      return merge(
+        {
+          appealsLoading: false,
+          v2Availability: appealsAvailability.USER_FORBIDDEN_ERROR,
+        },
+        state,
+      );
     case RECORD_NOT_FOUND_ERROR:
-      return _.merge(state, {
-        appealsLoading: false,
-        v2Availability: appealsAvailability.RECORD_NOT_FOUND_ERROR,
-      });
+      return merge(
+        {
+          appealsLoading: false,
+          v2Availability: appealsAvailability.RECORD_NOT_FOUND_ERROR,
+        },
+        state,
+      );
     case VALIDATION_ERROR:
-      return _.merge(state, {
-        appealsLoading: false,
-        v2Availability: appealsAvailability.VALIDATION_ERROR,
-      });
+      return merge(
+        {
+          appealsLoading: false,
+          v2Availability: appealsAvailability.VALIDATION_ERROR,
+        },
+        state,
+      );
     case BACKEND_SERVICE_ERROR:
-      return _.merge(state, {
-        appealsLoading: false,
-        v2Availability: appealsAvailability.BACKEND_SERVICE_ERROR,
-      });
+      return merge(
+        {
+          appealsLoading: false,
+          v2Availability: appealsAvailability.BACKEND_SERVICE_ERROR,
+        },
+        state,
+      );
     case FETCH_APPEALS_ERROR:
-      return _.merge(state, {
-        appealsLoading: false,
-        v2Availability: appealsAvailability.FETCH_APPEALS_ERROR,
-      });
+      return merge(
+        {
+          appealsLoading: false,
+          v2Availability: appealsAvailability.FETCH_APPEALS_ERROR,
+        },
+        state,
+      );
 
     case CHANGE_INDEX_PAGE:
-      return _.set('page', action.page, state);
+      return set('page', action.page, state);
     default:
       return state;
   }
