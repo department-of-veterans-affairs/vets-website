a6ba71fa744179d64dbfe2b01f227cb159b9188a
Dan Shank <danshank@gmail.com>
2020-02-12 23:54:11 -0800

    update config windows

--- src/platform/monitoring/DowntimeNotification/config/scheduledDowntimeWindow.js
+++ src/platform/monitoring/DowntimeNotification/config/scheduledDowntimeWindow.js
@@ -1,6 +1,6 @@
 import moment from 'moment-timezone';
 
 export default {
-  downtimeStart: moment.tz('2020-01-11 22:00', 'America/New_York'),
-  downtimeEnd: moment.tz('2020-01-12 00:01', 'America/New_York'),
+  downtimeStart: moment.tz('2020-02-13 21:00', 'America/New_York'),
+  downtimeEnd: moment.tz('2020-02-13 21:30', 'America/New_York'),
 };
