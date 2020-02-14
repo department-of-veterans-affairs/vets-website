0b5e3028dd5ad3b459fe20634de65bdfa3078771
Dan Shank <danshank@gmail.com>
2020-02-14 13:48:14 -0800

    sign in modal to display 12 hour format

--- src/platform/user/authentication/components/SignInModal.jsx
+++ src/platform/user/authentication/components/SignInModal.jsx
@@ -131,7 +131,7 @@ class SignInModal extends React.Component {
           'You may have trouble signing in or using some tools or services',
           'warning',
           `We’re doing some work on VA.gov right now. We hope to finish our work by ${
-            scheduledDowntimeWindow.downtimeEnd
+            scheduledDowntimeWindow.downtimeEnd.format('MMMM Do YYYY, h:mm:ss a')
           }. If you have trouble signing in or using any tools or services, please check back after then.`,
           this.setGlobalDowntimeState,
         )}
