bcf05c49936b91fc0c0e7956dc1cb89c3762503e
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-02-13 16:01:54 -0800

    Updated ViewDependentsHeader component text

--- src/applications/personalization/view-dependents/components/ViewDependentsHeader/ViewDependentsHeader.jsx
+++ src/applications/personalization/view-dependents/components/ViewDependentsHeader/ViewDependentsHeader.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const ViewDependentsHeader = () => (
   <div className="vads-l-row">
     <div className="vads-l-col--12">
-      <h1>Your Dependents</h1>
+      <h1>Your VA Dependents</h1>
       <p className="vads-u-font-size--md vads-u-font-family--serif">
-        Below is the information we have for your dependents. You can file a
-        claim for more disability compensation whenever you add a dependent. If
-        something changes with a dependent's status, you should notify VA.
+        Below is a list of dependents we have on file for you. You can file a
+        claim for additional disability compensation whenever you add a new
+        dependent.
       </p>
       <a href="#" className="usa-button-primary va-button-primary">
-        Go to eBenefits to add a dependent or change a dependent's status
+        Add or remove a dependent
       </a>
     </div>
   </div>
