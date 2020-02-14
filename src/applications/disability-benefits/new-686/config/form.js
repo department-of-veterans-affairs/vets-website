de4fac15e67cb2f0b43e76eb27b84caed6da8e62
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-02-14 10:27:57 -0800

    5278 2 build workflows (#11671)
    * Added new 686 form
    
    * Fixed lint errors
    
    * Adjusted based on feedback
    
    * Added basic file structure
    
    * Added generic schema file and constants file
    
    * moved files and adjusted for PR
    
    * Added new schema stuffs
    
    * Updated generic-schema and veteran info schema
    
    * further refactored folders and files
    
    * Fixed linting issues
    
    * Refactored folders and files
    
    * fixed linter issues
    
    * Changed chapter name to fit naming convention

--- src/applications/disability-benefits/new-686/config/form.js
+++ src/applications/disability-benefits/new-686/config/form.js
@@ -1,17 +1,10 @@
-// import fullSchema from 'vets-json-schema/dist/21-686-schema.json';
 import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
 
 // Chapter imports
 import { wizard } from './chapters/taskWizard';
+import { veteranInformation } from './chapters/veteran-information';
 import { deceasedDependentInformation } from './chapters/report-dependent-death';
-// const { } = fullSchema.properties;
-
-// const { } = fullSchema.definitions;
-
-const formFields = {
-  firstName: 'firstName',
-};
 
 const formConfig = {
   urlPrefix: '/',
@@ -30,6 +23,17 @@ const formConfig = {
   title: 'New 686',
   defaultDefinitions: {},
   chapters: {
+    veteranInformation: {
+      title: "Veteran's Information",
+      pages: {
+        page1: {
+          path: 'veteran-information',
+          title: 'Veteran Information - Page 1',
+          uiSchema: veteranInformation.uiSchema,
+          schema: veteranInformation.schema,
+        },
+      },
+    },
     optionSelection: {
       title: '686c Options',
       pages: {
@@ -38,29 +42,6 @@ const formConfig = {
           path: '686-options-selection',
           uiSchema: wizard.uiSchema,
           schema: wizard.schema,
-        },
-      },
-    },
-    chapter1: {
-      title: 'Personal Information',
-      pages: {
-        page1: {
-          path: 'first-name',
-          title: 'Personal Information - Page 1',
-          uiSchema: {
-            [formFields.firstName]: {
-              'ui:title': 'First Name',
-            },
-          },
-          schema: {
-            required: [formFields.firstName],
-            type: 'object',
-            properties: {
-              [formFields.firstName]: {
-                type: 'string',
-              },
-            },
-          },
         },
       },
     },
