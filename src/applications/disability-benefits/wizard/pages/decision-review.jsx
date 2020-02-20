432544274bf4c4d653e708c8c7087474979f112a
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-02-20 09:53:21 -0500

    Fix wording

--- src/applications/disability-benefits/wizard/pages/decision-review.jsx
+++ src/applications/disability-benefits/wizard/pages/decision-review.jsx
@@ -5,9 +5,11 @@ import { pageNames } from './pageList';
 const alertContent = (
   <>
     <p>
-      In most situations, you have one year from the date on your decision
-      letter to request a decision review. The deadline to file may be different
-      if you have a fiduciary claim, a{' '}
+      If you disagree with a VA decision on your claim, you can request a
+      decision review on all or some of the decision. In most situations, you
+      have one year from the date on your decision letter to request a decision
+      review. The deadline to file may be different if you have a fiduciary
+      claim, a{' '}
       <a href="/decision-reviews/multiple-party-claims/">
         claim contested by multiple parties
       </a>
