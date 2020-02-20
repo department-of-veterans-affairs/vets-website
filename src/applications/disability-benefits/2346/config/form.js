c804db40b30ce7f3eb2d07f8b2259c595e664a9d
mr0sari0 <mahariel.rosario@gmail.com>
2020-02-19 21:36:12 -0500

    remove comments

--- src/applications/disability-benefits/2346/config/form.js
+++ src/applications/disability-benefits/2346/config/form.js
@@ -1,9 +1,3 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-sequences */
-// In a real app this would be imported from `vets-json-schema`:
-// import fullSchema from 'vets-json-schema/dist/2346-schema.json';
-// In a real app this would not be imported directly; instead the schema you
-// imported above would import and use these common definitions:
 import { VA_FORM_IDS } from 'platform/forms/constants';
 import commonSchemaDefinitions from 'vets-json-schema/dist/definitions.json';
 import Schema2346 from '../2346-schema.json';
