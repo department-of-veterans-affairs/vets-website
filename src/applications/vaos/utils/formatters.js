8382daed4812ef9a13d90ad5f5e216cb6b98b85e
Jeff Balboni <jeff@adhocteam.us>
2020-02-21 13:28:55 -0500

    Formatter and facility info tests

--- src/applications/vaos/utils/formatters.js
+++ src/applications/vaos/utils/formatters.js
@@ -1,15 +1,5 @@
 import moment from 'moment';
 
-export function formatTimeToCall(timeToCall) {
-  if (timeToCall.length === 1) {
-    return timeToCall[0].toLowerCase();
-  } else if (timeToCall.length === 2) {
-    return `${timeToCall[0].toLowerCase()} or ${timeToCall[1].toLowerCase()}`;
-  }
-
-  return `${timeToCall[0].toLowerCase()}, ${timeToCall[1].toLowerCase()}, or ${timeToCall[2].toLowerCase()}`;
-}
-
 export function formatBestTimeToCall(bestTime) {
   const times = [];
   if (bestTime?.morning) {
