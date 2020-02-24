bd466f073f82b346b142476be9252f100e275c19
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-02-24 12:02:49 -0600

    build

--- src/applications/facility-locator/api/index.js
+++ src/applications/facility-locator/api/index.js
@@ -1,5 +1,4 @@
-import MockApi from './MockLocatorApi';
 import LiveApi from './LocatorApi';
 import environment from '../../../platform/utilities/environment';
-
+// build
 export default (environment.isLocalhost() ? LiveApi : LiveApi);
