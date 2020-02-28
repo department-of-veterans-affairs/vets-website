a2fb83e4e135734afd58e53d5de2dbeb096842c9
Robin Garrison <robin.garrison@adhocteam.us>
2020-02-28 10:42:01 -0600

    Show resubmit message after errors clear

--- src/platform/forms-system/src/js/review/SubmitButtons.jsx
+++ src/platform/forms-system/src/js/review/SubmitButtons.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 
 import ProgressButton from '../components/ProgressButton';
@@ -14,6 +14,10 @@ export default function SubmitButtons(props) {
     renderErrorMessage,
     formErrors,
   } = props;
+  const [hadErrors, setHadErrors] = useState(false);
+  if (!hadErrors && formErrors.errors?.length > 0) {
+    setHadErrors(true);
+  }
   let submitButton;
   let submitMessage;
   if (submission.status === false) {
@@ -88,6 +92,7 @@ export default function SubmitButtons(props) {
     );
   } else if (submission.status === 'validationError') {
     const errors = formErrors?.errors || [];
+    const errLen = errors.length;
     submitButton = (
       <ProgressButton
         onButtonClick={onSubmit}
@@ -95,61 +100,71 @@ export default function SubmitButtons(props) {
         buttonClass="usa-button-primary"
       />
     );
-    submitMessage = (
-      <div className="usa-alert usa-alert-error schemaform-failure-alert">
-        <div className="usa-alert-body">
-          <p className="schemaform-warning-header">
-            <strong>
-              We’re sorry. Some information in your application is missing or
-              not valid.
+    submitMessage =
+      hadErrors && errLen === 0 ? (
+        <div className="usa-alert usa-alert-warning schemaform-failure-alert">
+          <div className="usa-alert-body">
+            <strong className="schemaform-warning-header">
+              The information in your application now appears to be valid,
+              please try resubmitting it now.
             </strong>
-          </p>
-          {errors.length > 0 && (
-            <fieldset>
-              <legend
-                className="error-message-focus vads-u-font-size--base"
-                tabIndex={-1}
-              >
-                The following required
-                {errors.length === 1 ? ' item is ' : ' items are '}
-                preventing submission:
-              </legend>
-              <ul className="vads-u-margin-left--3">
-                {errors.map(error => (
-                  <li key={error.name} className="error-message-list-item">
-                    {error.chapterKey ? (
-                      <a
-                        href="#"
-                        className="error-message-list-link"
-                        onClick={event => {
-                          event.preventDefault();
-                          props.openReviewChapter(error.chapterKey);
-                          props.setEditMode(
-                            error.pageKey,
-                            true, // enable edit mode
-                            error.index || null,
-                          );
-                          // props.formContext.onError();
-                          focusAndScrollToReviewElement(error);
-                        }}
-                      >
-                        {error.message}
-                      </a>
-                    ) : (
-                      error.message
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </fieldset>
-          )}
-          <p>
-            Please check each section of your application to make sure you’ve
-            filled out all the information that is required.
-          </p>
+          </div>
         </div>
-      </div>
-    );
+      ) : (
+        <div className="usa-alert usa-alert-error schemaform-failure-alert">
+          <div className="usa-alert-body">
+            <p className="schemaform-warning-header">
+              <strong>
+                We’re sorry. Some information in your application is missing or
+                not valid.
+              </strong>
+            </p>
+            {errLen > 0 && (
+              <fieldset>
+                <legend
+                  className="error-message-focus vads-u-font-size--base"
+                  tabIndex={-1}
+                >
+                  The following required
+                  {errLen === 1 ? ' item is ' : ' items are '}
+                  preventing submission:
+                </legend>
+                <ul className="vads-u-margin-left--3">
+                  {errors.map(error => (
+                    <li key={error.name} className="error-message-list-item">
+                      {error.chapterKey ? (
+                        <a
+                          href="#"
+                          className="error-message-list-link"
+                          onClick={event => {
+                            event.preventDefault();
+                            props.openReviewChapter(error.chapterKey);
+                            props.setEditMode(
+                              error.pageKey,
+                              true, // enable edit mode
+                              error.index || null,
+                            );
+                            // props.formContext.onError();
+                            focusAndScrollToReviewElement(error);
+                          }}
+                        >
+                          {error.message}
+                        </a>
+                      ) : (
+                        error.message
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </fieldset>
+            )}
+            <p>
+              Please check each section of your application to make sure you’ve
+              filled out all the information that is required.
+            </p>
+          </div>
+        </div>
+      );
   } else {
     if (renderErrorMessage) {
       submitMessage = renderErrorMessage(formErrors?.errors);
