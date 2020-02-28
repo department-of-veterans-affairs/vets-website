d7dbd1eb3959b67e0d67a2cec3acb8b27cb4f3d5
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-02-28 11:19:27 -0800

    Added unit tests for utils

---
+++ src/applications/personalization/view-dependents/tests/util/index.unit.spec.jsx
@@ -0,0 +1,36 @@
+import { expect } from 'chai';
+import { splitPersons, isServerError, isClientError } from '../../util/index';
+
+describe('View Dependents splitPersons Utility', () => {
+  const mockData = [
+    {
+      awardIndicator: 'N',
+    },
+    {
+      awardIndicator: 'N',
+    },
+  ];
+
+  it('should return an object with two arrays', () => {
+    const data = splitPersons(mockData);
+
+    expect(data.onAward).to.exist;
+    expect(data.notOnAward).to.exist;
+  });
+});
+
+describe('View Dependents isServerError Utility', () => {
+  it('should return true on a 500 error', () => {
+    const errorCheck = isServerError(500);
+
+    expect(errorCheck).to.be.true;
+  });
+});
+
+describe('View Dependents isClientError Utility', () => {
+  it('should return true on a 400 error', () => {
+    const errorCheck = isClientError(400);
+
+    expect(errorCheck).to.be.true;
+  });
+});
