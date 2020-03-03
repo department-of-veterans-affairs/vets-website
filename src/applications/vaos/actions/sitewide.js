0268703fef7dd2ca0b2d8432e98de1f2caa3a8a3
Jeff Balboni <jeff@adhocteam.us>
2020-03-03 11:27:33 -0500

    Update comments

--- src/applications/vaos/actions/sitewide.js
+++ src/applications/vaos/actions/sitewide.js
@@ -1,7 +1,6 @@
 /*
- * These are actions that are shared between the appointments reducer
- * and the newAppointmnt reducer. The newAppointment code is lazy loaded,
- * so we don't want to import it load code we don't need yet.
+ * These are actions that are shared by multiple reducers. Because we are lazy loading
+ * reducers, importing across those boundaries may pull in code we don't want.
  */
 export const STARTED_NEW_APPOINTMENT_FLOW = 'vaos/STARTED_NEW_APPOINTMENT_FLOW';
 export const FORM_SUBMIT_SUCCEEDED = 'vaos/FORM_SUBMIT_SUCCEEDED';
