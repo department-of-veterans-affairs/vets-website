c8935d80db361efe323295bd7009eff627c0f32f
Micah Chiang <micahkchiang@gmail.com>
2020-03-05 13:39:22 -0800

    avoid linting for now.

---
+++ src/applications/disability-benefits/new-686/tests/config/spouseMarriageFormerPartners.unit.spec.jsx
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import { expect } from 'chai';
+// import sinon from 'sinon';
+// import { mount } from 'enzyme';
+// import {
+//   DefinitionTester,
+//   fillData,
+//   selectRadio,
+// } from 'platform/testing/unit/schemaform-utils.jsx';
+// import { changeDropdown } from '../helpers/index.js';
+
+// import formConfig from '../../config/form';
+
+// describe('686 spouse former partner names', () => {
+//   const {
+//     schema,
+//     uiSchema,
+//   } = formConfig.chapters.addSpouse.pages.currentMarriageInformation;
+
+//   const formData = {
+//     'view:selectable686Options': {
+//       addSpouse: true,
+//     },
+//   };
+// });
