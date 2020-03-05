8826b3739d482aea2b67f41f109dc689c9426430
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-03-05 11:52:04 -0500

    added all the ui elements needed for this scenaroio of the page

---
+++ src/applications/disability-benefits/2346/components/customFields/productNameField.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const productName = () => (
+  <p>
+    <b>Battery:</b> ZA1239
+  </p>
+);
+
+export default productName;
---
+++ src/applications/disability-benefits/2346/components/customFields/quantityField.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const quantityField = () => (
+  <p>
+    <b>quantity:</b> 1
+  </p>
+);
+
+export default quantityField;
