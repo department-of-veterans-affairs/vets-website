e3aab3972889f2ef67c0fdf9606beb11c68aac4c
Brooks Johnson <brooks.johnson@pm.me>
2020-03-05 12:58:00 -0800

    Remove icon additions
    These were for testing

--- src/applications/hca/components/GetFormHelp.jsx
+++ src/applications/hca/components/GetFormHelp.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 function GetFormHelp() {
   return (
     <div>
-      <i className="fa-times" aria-hidden="true" />
       <p className="help-talk">
         Need help enrolling or have questions about enrollment or eligibility?
         Call our toll-free number:
@@ -17,7 +16,6 @@ function GetFormHelp() {
         <a className="help-phone-number-link" href="tel:1-800-877-8339">
           800-877-8339
         </a>
-        <i className="fa-times" aria-hidden="true" />
         <br />
         Monday &#8211; Friday, 8:00 a.m. &#8211; 8:00 p.m. ET
       </p>
