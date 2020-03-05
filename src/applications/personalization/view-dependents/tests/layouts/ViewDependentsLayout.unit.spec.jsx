604da07ae30622bd86cfb85bf6ad47afb84d3f5f
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-03-05 10:09:53 -0800

    Added unit test for View Dependents (#11788)
    * Added unit test for View Dependents
    
    * Added unit tests for utils
    
    * Added some things from PR feedback

--- src/applications/personalization/view-dependents/tests/layouts/ViewDependentsLayout.unit.spec.jsx
+++ src/applications/personalization/view-dependents/tests/layouts/ViewDependentsLayout.unit.spec.jsx
@@ -42,4 +42,46 @@ describe('<ViewDependentsLayout />', () => {
     expect(wrapper.find('div.large-screen:vads-u-padding-left--6')).to.exist;
     wrapper.unmount();
   });
+
+  it('should show an info alert when there are no dependents', () => {
+    const wrapper = mount(
+      <ViewDependentsLayout
+        loading={false}
+        error={false}
+        onAwardDependents={null}
+        notOnAwardDependents={null}
+      />,
+    );
+
+    expect(wrapper.find('div.usa-alert-info')).to.exist;
+    wrapper.unmount();
+  });
+
+  it('should show an info alert when there is a 400 error', () => {
+    const wrapper = mount(
+      <ViewDependentsLayout
+        loading={false}
+        error={400}
+        onAwardDependents={mockState.onAwardDependents}
+        notOnAwardDependents={mockState.notOnAwardDependents}
+      />,
+    );
+
+    expect(wrapper.find('div.usa-alert-info')).to.exist;
+    wrapper.unmount();
+  });
+
+  it('should show an error alert when there is a 500 error', () => {
+    const wrapper = mount(
+      <ViewDependentsLayout
+        loading={false}
+        error={500}
+        onAwardDependents={mockState.onAwardDependents}
+        notOnAwardDependents={mockState.notOnAwardDependents}
+      />,
+    );
+
+    expect(wrapper.find('div.usa-alert-error')).to.exist;
+    wrapper.unmount();
+  });
 });
