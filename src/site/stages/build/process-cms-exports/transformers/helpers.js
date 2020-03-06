6cf955287efb47748d582b8bbd350b78701568fa
Christopher Valarida <chris.valarida@adhocteam.us>
2020-03-05 16:17:25 -0800

    findMatchingEntities unit tests

--- src/site/stages/build/process-cms-exports/transformers/helpers.js
+++ src/site/stages/build/process-cms-exports/transformers/helpers.js
@@ -1,11 +1,7 @@
-const fs = require('fs');
 const assert = require('assert');
 const { sortBy, unescape, pick, omit } = require('lodash');
 const moment = require('moment-timezone');
 const { readEntity } = require('../helpers');
-
-const { readEntity } = require('../helpers');
-const assembleEntityTreeFactory = require('../index');
 
 /**
  * Takes a string with escaped unicode code points and replaces them
@@ -225,66 +221,5 @@ module.exports = {
 
     // Return the new schema
     return newSchema;
-  },
-
-  /**
-   * Search for all entities matching the parameters and return the transformed
-   * entities.
-   *
-   * @param {String} baseType - The base type of the entity
-   * @param {Path} contentDir - The directory to read the files from
-   * @param {String} subType  - [Optional] The subType of the entity
-   * @param {Function} filter - [Optional] A function which recieves the object
-   *                            read from the JSON file and returns true if we
-   *                            should keep the entity or false if we should not
-   *
-   * TODO: Memoize this function to improve speed if the build is slow because
-   * of this CMS content transformation process.
-   *
-   * @return {Array<Object>} - The transformed entities whose raw form matches
-   *                           the parameters
-   */
-  findMatchingEntities(baseType, contentDir, { subType, filter }) {
-    // Sanity checks
-    assert(
-      typeof baseType === 'string',
-      `baseType needs to be a string. Found ${typeof baseType} (${baseType})`,
-    );
-    assert(
-      fs.lstatSync(contentDir).isDirectory(),
-      `${contentDir} is not a directory.`,
-    );
-    if (subType)
-      assert(
-        typeof subType === 'string',
-        `subType needs to be a string. Found ${typeof subType} (${subType})`,
-      );
-    if (filter)
-      assert(
-        typeof filter === 'function',
-        `filter needs to be a string. Found ${typeof filter} (${filter})`,
-      );
-
-    const assembleEntityTree = assembleEntityTreeFactory(contentDir);
-
-    // Look through contentDir for all `${baseType}.*.json` files
-    return (
-      fs
-        .readdirSync(contentDir)
-        .filter(name => name.startsWith(`${baseType}.`))
-        .map(name => {
-          const uuid = name.split('.')[1];
-          return readEntity(contentDir, baseType, uuid);
-        })
-        .filter(
-          // Filter them by `subType` if available
-          // NOTE: Not all content models keep their subType in `.type[0]`
-          // If we have to search for those content models, we'll need to update this
-          entity => (subType ? entity.type[0].target_id === subType : true),
-        )
-        // Filter them by `filter` if available
-        .filter(entity => (filter ? filter(entity) : true))
-        .map(entity => assembleEntityTree(entity))
-    );
   },
 };
