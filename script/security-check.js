df6bc6ec2f79a42cef1c134becb54ba92eb649e4
Brooks Johnson <brooks.johnson@pm.me>
2020-03-06 15:06:41 -0800

    Remove acorn exception

--- script/security-check.js
+++ script/security-check.js
@@ -14,7 +14,6 @@ const exceptionSet = new Set([
   'https://npmjs.com/advisories/788',
   'https://npmjs.com/advisories/813',
   'https://npmjs.com/advisories/996',
-  'https://npmjs.com/advisories/1488',
 ]);
 
 const severitySet = new Set(['high', 'critical', 'moderate']);
