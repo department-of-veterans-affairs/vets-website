d58a2d59fd7d09ba25a01afc710203b9d557273f
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-03-09 10:09:55 -0400

    adding order accessories page

--- src/applications/disability-benefits/2346/actions/index.js
+++ src/applications/disability-benefits/2346/actions/index.js
@@ -2,6 +2,8 @@ import * as Sentry from '@sentry/browser';
 import {
   FETCH_VETERAN_INFORMATION,
   FETCH_VETERAN_INFORMATION_FAILURE,
+  FETCH_REORDER_BATTERY_AND_ACCESSORIES_INFORMATION,
+  FETCH_REORDER_BATTERY_AND_ACCESSORIES_INFORMATION_FAILURE,
 } from '../constants';
 
 export const fetchVeteranInformation = data => ({
@@ -11,6 +13,16 @@ export const fetchVeteranInformation = data => ({
 
 export const fetchVeteranInformationFailure = error => ({
   type: FETCH_VETERAN_INFORMATION_FAILURE,
+  error,
+});
+
+export const fetchReOrderBatteryAndAccessoriesInformation = data => ({
+  type: FETCH_REORDER_BATTERY_AND_ACCESSORIES_INFORMATION,
+  data,
+});
+
+export const fetchReOrderBatteryAndAccessoriesInformationFailure = error => ({
+  type: FETCH_REORDER_BATTERY_AND_ACCESSORIES_INFORMATION_FAILURE,
   error,
 });
 
@@ -27,6 +39,30 @@ export const getVeteranInformationData = data => async dispatch => {
   } catch (error) {
     dispatch(
       fetchVeteranInformationFailure(
+        error,
+        Sentry.captureMessage('failed to retrieve data from the api'),
+      ),
+    );
+  }
+};
+
+export const getReOrderBatteryAndAccessoriesInformationData = data => async dispatch => {
+  try {
+    const batteryAndAccessoriesInformation = {
+      veteranFullName: data.formData.veteranFullName,
+      gender: data.formData.gender,
+      dateOfBirth: data.formData.dateOfBirth,
+      veteranAddress: data.formData.veteranAddress,
+      email: data.formData.email,
+    };
+    dispatch(
+      fetchReOrderBatteryAndAccessoriesInformation(
+        batteryAndAccessoriesInformation,
+      ),
+    );
+  } catch (error) {
+    dispatch(
+      fetchReOrderBatteryAndAccessoriesInformationFailure(
         error,
         Sentry.captureMessage('failed to retrieve data from the api'),
       ),
