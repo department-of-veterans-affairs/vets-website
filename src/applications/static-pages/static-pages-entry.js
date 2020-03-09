def75af291593ff17cd22d1e6fab4be6d7719726
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-03-09 11:51:05 -0400

    Revert "[Homepage Banner] Convert to React component (#11818)" (#11838)
    This reverts commit 104c47f4f4d70df8e11c3404fdd5190a3d837877.

--- src/applications/static-pages/static-pages-entry.js
+++ src/applications/static-pages/static-pages-entry.js
@@ -15,7 +15,7 @@ import subscribeAccordionEvents from './subscribeAccordionEvents';
 import createApplicationStatus from './createApplicationStatus';
 import createCallToActionWidget from './createCallToActionWidget';
 import createMyVALoginWidget from './createMyVALoginWidget';
-import createHomepageBanner from './homepage-banner/createHomepageBanner';
+import renderHomepageBanner from './renderHomepageBanner';
 import createDisabilityFormWizard from '../disability-benefits/wizard/createWizard';
 import createDisabilityRatingCalculator from '../disability-benefits/disability-rating-calculator/createCalculator';
 import createEducationApplicationStatus from '../edu-benefits/components/createEducationApplicationStatus';
@@ -116,10 +116,9 @@ createScoAnnouncementsWidget();
 
 createFindVaForms(store, widgetTypes.FIND_VA_FORMS);
 
-createHomepageBanner(store, widgetTypes.HOMEPAGE_BANNER);
-
 // homepage widgets
 if (location.pathname === '/') {
+  renderHomepageBanner();
   createMyVALoginWidget(store);
 }
 
