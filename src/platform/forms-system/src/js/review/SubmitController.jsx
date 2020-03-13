3818af357c9f4f32f14447c5998c5c871e8eb83f
LinnJS <justinslinn727@gmail.com>
2020-03-13 13:36:48 -0400

    Refactor submitButtons so I do not undo Robins work

--- src/platform/forms-system/src/js/review/SubmitController.jsx
+++ src/platform/forms-system/src/js/review/SubmitController.jsx
@@ -114,20 +114,17 @@ class SubmitController extends React.Component {
 
   render() {
     const { form, renderErrorMessage } = this.props;
-    // Render inside SubmitButtons so the alert is _above_ the submit button;
+    // Render inside SubmitButtons by using `preSubmitBlock` so the alert is _above_ the submit button;
     // helps with accessibility
 
     return (
-      <div>
-        {this.RenderPreSubmitSection()}
-        <SubmitButtons
-          onBack={this.goBack}
-          onSubmit={this.handleSubmit}
-          submission={form.submission}
-          renderErrorMessage={renderErrorMessage}
-          preSubmitBlock={this.RenderPreSubmitSection}
-        />
-      </div>
+      <SubmitButtons
+        onBack={this.goBack}
+        onSubmit={this.handleSubmit}
+        submission={form.submission}
+        renderErrorMessage={renderErrorMessage}
+        preSubmitBlock={this.RenderPreSubmitSection}
+      />
     );
   }
 }
