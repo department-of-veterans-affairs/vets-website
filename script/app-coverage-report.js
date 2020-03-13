1bd9d0b280acee77907c59cc500e8f178cb8cff8
Theo Bentum <theo@adhocteam.us>
2020-03-13 08:10:48 -0700

    Improvements to the app coverage script

--- script/app-coverage-report.js
+++ script/app-coverage-report.js
@@ -3,18 +3,32 @@
 const fs = require('fs');
 const path = require('path');
 const Table = require('cli-table');
+const {
+  getAppManifests,
+} = require('../src/site/stages/build/webpack/manifest-helpers');
 
 const printCoverage = coverageResults => {
   // Create a new table with headers
   const coverageTable = new Table({
-    head: ['Application', 'Lines', 'Functions', 'Statements', 'Branches'],
+    head: [
+      'Application (src/applications)',
+      'Lines',
+      'Functions',
+      'Statements',
+      'Branches',
+    ],
   });
 
   // Add each app coverage result to the table
   Object.keys(coverageResults).forEach(key => {
     if (key !== 'total') {
+      const appLocation = `${coverageResults[key].path.substr(
+        0,
+        coverageResults[key].path.lastIndexOf('/'),
+      )}`;
+
       coverageTable.push({
-        [key]: [
+        [appLocation]: [
           `${coverageResults[key].lines.pct}%`,
           `${coverageResults[key].functions.pct}%`,
           `${coverageResults[key].statements.pct}%`,
@@ -27,34 +41,67 @@ const printCoverage = coverageResults => {
   console.log(coverageTable.toString());
 };
 
-const generateCoverage = (rootDir, coverageSummary) =>
-  Object.keys(coverageSummary).reduce((acc, coverageResult) => {
+const generateCoverage = (rootDir, coverageSummary) => {
+  // Get application manifests & entry names
+  const manifests = getAppManifests(path.join(__dirname, '../'));
+  const entryNames = manifests.map(manifest => manifest.entryName);
+
+  // Set initial coverage object
+  const initialCoverage = Object.assign(
+    ...entryNames.map(entryName => ({
+      [entryName]: {
+        path: manifests
+          .find(obj => obj.entryName === entryName)
+          .entryFile.replace(rootDir, ''),
+        lines: { total: 0, covered: 0, skipped: 0, pct: 0 },
+        statements: { total: 0, covered: 0, skipped: 0, pct: 0 },
+        functions: { total: 0, covered: 0, skipped: 0, pct: 0 },
+        branches: { total: 0, covered: 0, skipped: 0, pct: 0 },
+      },
+    })),
+  );
+
+  return Object.keys(coverageSummary).reduce((acc, coverageResult) => {
     const appName = coverageResult.replace(rootDir, '').split(path.sep)[0];
-    // If appName exists, add coverageResult to overall coverage
+
     if (appName) {
-      if (acc[appName]) {
+      // Find coverage if it exists by checking if the path of the coverage result file
+      // includes an application path, e.g. 'vre/chapter36/'
+      const resultFilePath = coverageResult.replace(rootDir, '');
+      const coverageItem = Object.values(acc).find(appCov =>
+        resultFilePath.includes(
+          `${appCov.path.substring(0, appCov.path.lastIndexOf('/'))}/`,
+        ),
+      );
+
+      // Get key of coverageItem
+      const coverageKey = Object.keys(acc).find(
+        key => acc[key] === coverageItem,
+      );
+
+      // Update coverage
+      if (acc[coverageKey]) {
         // Average coverageResult with respective acc segment if present
         // Each 'seg' represents coverage by line, function, statement, and branch
-        Object.keys(acc[appName]).forEach(seg => {
-          acc[appName][seg].total += coverageSummary[coverageResult][seg].total;
-          acc[appName][seg].covered +=
-            coverageSummary[coverageResult][seg].covered;
-          acc[appName][seg].skipped +=
-            coverageSummary[coverageResult][seg].skipped;
-          acc[appName][seg].pct = (
-            (acc[appName][seg].covered / acc[appName][seg].total) *
-            100
-          ).toFixed(2);
-        });
-      } else {
-        // Create new coverageResult entry
-        Object.assign(acc, {
-          [appName]: coverageSummary[coverageResult],
+        Object.keys(acc[coverageKey]).forEach(seg => {
+          if (seg !== 'path') {
+            acc[coverageKey][seg].total +=
+              coverageSummary[coverageResult][seg].total;
+            acc[coverageKey][seg].covered +=
+              coverageSummary[coverageResult][seg].covered;
+            acc[coverageKey][seg].skipped +=
+              coverageSummary[coverageResult][seg].skipped;
+            acc[coverageKey][seg].pct = (
+              (acc[coverageKey][seg].covered / acc[coverageKey][seg].total) *
+                100 || 0
+            ).toFixed(2);
+          }
         });
       }
     }
     return acc;
-  }, {});
+  }, initialCoverage);
+};
 
 // Root directory of application folders
 const applicationDir = path.join(__dirname, '../src/applications/');
@@ -65,8 +112,8 @@ if (fs.existsSync(path.join(__dirname, '../coverage/coverage-summary.json'))) {
     fs.readFileSync(path.join(__dirname, '../coverage/coverage-summary.json')),
   );
   // Generate and print coverage
-  const appCoverage = generateCoverage(applicationDir, coverageSummaryJson);
-  printCoverage(appCoverage);
+  const appCoverages = generateCoverage(applicationDir, coverageSummaryJson);
+  printCoverage(appCoverages);
 } else {
   console.log('./coverage/coverage-summary.json not found.');
 }
