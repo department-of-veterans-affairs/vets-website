4f4b7824f5d3ddbde23a85fdaa9e918d2d619d89
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-03-17 13:16:52 -0500

    remove pharmacy option

--- src/applications/facility-locator/constants/index.js
+++ src/applications/facility-locator/constants/index.js
@@ -1,5 +1,3 @@
-import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
-
 /**
  * Enum for the type attribute of a Facility/Provider search result
  */
@@ -45,29 +43,14 @@ export const PinNames = {
 /**
  * Defines the options available for the Location Type Dropdown
  */
-let locOptions;
-
-if (FEATURE_FLAG_NAMES.facilitiesPpmsSuppressPharmacies) {
-  locOptions = [
-    LocationType.HEALTH,
-    LocationType.URGENT_CARE,
-    LocationType.CC_PROVIDER,
-    LocationType.BENEFITS,
-    LocationType.CEMETARY,
-    LocationType.VET_CENTER,
-  ];
-} else {
-  locOptions = [
-    LocationType.HEALTH,
-    LocationType.URGENT_CARE,
-    LocationType.URGENT_CARE_FARMACIES,
-    LocationType.CC_PROVIDER,
-    LocationType.BENEFITS,
-    LocationType.CEMETARY,
-    LocationType.VET_CENTER,
-  ];
-}
-export const LOCATION_OPTIONS = locOptions;
+export const LOCATION_OPTIONS = [
+  LocationType.HEALTH,
+  LocationType.URGENT_CARE,
+  LocationType.CC_PROVIDER,
+  LocationType.BENEFITS,
+  LocationType.CEMETARY,
+  LocationType.VET_CENTER,
+];
 
 /**
  * Defines the ± change in bounding box size for the map when changing zoom
