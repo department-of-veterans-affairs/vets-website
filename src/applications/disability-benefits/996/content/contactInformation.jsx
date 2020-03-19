5f094a389729d556e851ffd360e446705e202d6d
Robin Garrison <robin.garrison@adhocteam.us>
2020-03-19 12:54:54 -0500

    Contact info page tweaks

--- src/applications/disability-benefits/996/content/contactInformation.jsx
+++ src/applications/disability-benefits/996/content/contactInformation.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 export const contactInfoDescription = () => (
   <>
-    This is the contact information we have on file for you. We’ll send any
-    important information about your Higher-Level Review to this address.
+    <p>
+      This is the contact information we have on file for you. We’ll send any
+      important information about your Higher-Level Review to this address.
+    </p>
     <p className="vads-u-margin-top--1p5">
       Any updates you make here will be reflected on your profile page.
     </p>
