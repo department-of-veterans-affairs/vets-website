af679bda18a382fde95a7e0357889c48e91214c2
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2020-03-20 15:05:24 -0400

    updated error language

--- src/applications/gi/components/ServiceError.jsx
+++ src/applications/gi/components/ServiceError.jsx
@@ -5,8 +5,8 @@ export const ServiceError = () => (
     <h3 className="backend-error-bold"> We’ve run into a problem. </h3>
 
     <div className="backend-error vads-u-padding-bottom--5">
-      We're sorry. Something went wrong on our end. Please try again or check
-      back soon.
+      We're sorry. Something went wrong on our end. Please refresh this page or
+      check back soon.
     </div>
   </div>
 );
