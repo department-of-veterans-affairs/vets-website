2b86c782464eb7c14d2a0f7e289691bd61ad4564
Micah Chiang <micahkchiang@gmail.com>
2020-03-20 09:13:16 -0700

    6024 674 p1 unit tests (#11945)
    * Add chapter for 674.
    
    * space
    
    * pages for school information.
    
    * make address reusable
    
    * remove class name
    
    * zipcode fix
    
    * remove comment
    
    * hide military base checkbox if needed.
    
    * Unit tests.
    
    * grammar

---
+++ src/applications/disability-benefits/686c-674/tests/config/studentNameSSN674.unit.spec.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+import { changeDropdown } from '../helpers/index.js';
+import {
+  DefinitionTester,
+  fillData,
+} from 'platform/testing/unit/schemaform-utils.jsx';
+
+import formConfig from '../../config/form';
+
+describe('Report 674 student personal information', () => {
+  const {
+    schema,
+    uiSchema,
+  } = formConfig.chapters.report674.pages.studentNameAndSSN;
+  const formData = {
+    'view:selectable686Options': {
+      report674: true,
+    },
+  };
+
+  it('should render', () => {
+    const form = mount(
+      <DefinitionTester
+        schema={schema}
+        uiSchema={uiSchema}
+        definitions={formConfig.defaultDefinitions}
+        data={formData}
+      />,
+    );
+    expect(form.find('input').length).to.equal(5);
+    expect(form.find('select').length).to.equal(3);
+    form.unmount();
+  });
+
+  it('should not progress without the required fields', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        schema={schema}
+        uiSchema={uiSchema}
+        definitions={formConfig.defaultDefinitions}
+        data={formData}
+        onSubmit={onSubmit}
+      />,
+    );
+    form.find('form').simulate('submit');
+    expect(form.find('.usa-input-error').length).to.equal(4);
+    expect(onSubmit.called).to.be.false;
+    form.unmount();
+  });
+
+  it('should progress with the required fields filled', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        schema={schema}
+        uiSchema={uiSchema}
+        definitions={formConfig.defaultDefinitions}
+        data={formData}
+        onSubmit={onSubmit}
+      />,
+    );
+    fillData(form, 'input#root_studentFullName_first', 'Bill');
+    fillData(form, 'input#root_studentFullName_last', 'Bob');
+    fillData(form, 'input#root_studentSSN', '555-55-5551');
+    changeDropdown(form, 'select#root_studentDOBMonth', 1);
+    changeDropdown(form, 'select#root_studentDOBDay', 1);
+    fillData(form, 'input#root_studentDOBYear', '2002');
+
+    form.find('form').simulate('submit');
+    expect(form.find('.usa-input-error').length).to.equal(0);
+    expect(onSubmit.called).to.be.true;
+    form.unmount();
+  });
+});
