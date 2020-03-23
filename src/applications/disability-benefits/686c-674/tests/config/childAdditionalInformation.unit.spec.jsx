8b5c568d23574290a28214c05e7eba1f579a6056
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-03-23 10:33:21 -0700

    Added additional unit tests

--- src/applications/disability-benefits/686c-674/tests/config/childAdditionalInformation.unit.spec.jsx
+++ src/applications/disability-benefits/686c-674/tests/config/childAdditionalInformation.unit.spec.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { expect } from 'chai';
+import sinon from 'sinon';
 import { mount } from 'enzyme';
-import { DefinitionTester } from 'platform/testing/unit/schemaform-utils.jsx';
+import {
+  DefinitionTester,
+  fillData,
+} from 'platform/testing/unit/schemaform-utils.jsx';
 
 import formConfig from '../../config/form';
 
@@ -22,6 +26,13 @@ describe('686 add child - child additional information', () => {
         last: 'Bob',
         ssn: '370947141',
         birthDate: '1997-04-02',
+        childPlaceOfBirth: {
+          state: 'California',
+        },
+        childStatus: {
+          stepchild: false,
+        },
+        doesChildLiveWithYou: false,
       },
     ],
   };
@@ -37,7 +48,72 @@ describe('686 add child - child additional information', () => {
         data={formData}
       />,
     );
-    expect(form.find('input').length).to.equal(2);
+    expect(form.find('input').length).to.equal(12);
+    form.unmount();
+  });
+
+  it('should not progress without the required fields', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        pagePerItemIndex={0}
+        arrayPath={arrayPath}
+        schema={schema}
+        uiSchema={uiSchema}
+        definitions={formConfig.defaultDefinitions}
+        data={formData}
+        onSubmit={onSubmit}
+      />,
+    );
+    form.find('form').simulate('submit');
+    expect(form.find('.usa-input-error').length).to.equal(6);
+    expect(onSubmit.called).to.be.false;
+    form.unmount();
+  });
+
+  it('should progress with the required fields filled', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        pagePerItemIndex={0}
+        arrayPath={arrayPath}
+        schema={schema}
+        uiSchema={uiSchema}
+        definitions={formConfig.defaultDefinitions}
+        data={formData}
+        onSubmit={onSubmit}
+      />,
+    );
+    fillData(
+      form,
+      'input#root_childAddressInfo_personChildLivesWith_first',
+      'Bill',
+    );
+    fillData(
+      form,
+      'input#root_childAddressInfo_personChildLivesWith_last',
+      'Bob',
+    );
+    fillData(
+      form,
+      'input#root_childAddressInfo_childAddress_country',
+      'United States',
+    );
+    fillData(
+      form,
+      'input#root_childAddressInfo_childAddress_street',
+      'Sunny Road',
+    );
+    fillData(
+      form,
+      'input#root_childAddressInfo_childAddress_city',
+      'Someplace',
+    );
+    fillData(form, 'input#root_childAddressInfo_childAddress_postal', '12345');
+
+    form.find('form').simulate('submit');
+    expect(form.find('.usa-input-error').length).to.equal(0);
+    expect(onSubmit.called).to.be.true;
     form.unmount();
   });
 });
