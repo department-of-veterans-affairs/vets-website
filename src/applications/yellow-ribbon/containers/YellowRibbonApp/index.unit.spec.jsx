517edd84b9b833cd9e1bde7c77be71d4d7050b6c
Kelson Adams <kadams@governmentcio.com>
2020-03-23 09:09:36 -0600

    Add another assertion to unit test

--- src/applications/yellow-ribbon/containers/YellowRibbonApp/index.unit.spec.jsx
+++ src/applications/yellow-ribbon/containers/YellowRibbonApp/index.unit.spec.jsx
@@ -79,6 +79,7 @@ describe('Yellow Ribbon container <YellowRibbonApp>', () => {
     expect(text).to.include('Yellow Ribbon school search results');
 
     // Expect there NOT to be:
+    expect(text).to.not.include('Find a Yellow Ribbon school');
     expect(text).to.not.include('Learn more about the Yellow Ribbon Program.');
     expect(text).to.not.include(
       'You may be eligible for Yellow Ribbon program funding if you:',
