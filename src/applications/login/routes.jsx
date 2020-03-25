ac3e23645c254797b951fbe109c6d235dbeaeb25
Dan Hinze <hinzed1127@gmail.com>
2020-03-25 12:49:57 -0500

    Add an alert for a successful SSO sign out state (#11979)
    * Add an alert for a successful sign out state
    
    * Content Updates
    
    * Fix environment buildtype variable for IAM mapping

--- src/applications/login/routes.jsx
+++ src/applications/login/routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router';
-import App from './containers/App.jsx';
+import SignInApp from './containers/SignInApp';
 
-const routes = <Route path="/" component={App} />;
+const routes = <Route path="/" component={SignInApp} />;
 
 export default routes;
