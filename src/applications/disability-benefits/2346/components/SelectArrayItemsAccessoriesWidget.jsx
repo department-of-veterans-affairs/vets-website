fcd632ff507004db1b625b063c6c240aa84683cb
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-03-25 14:07:08 -0400

    adding separate widgets for supplies and accessories

--- src/applications/disability-benefits/2346/components/SelectArrayItemsAccessoriesWidget.jsx
+++ src/applications/disability-benefits/2346/components/SelectArrayItemsAccessoriesWidget.jsx
@@ -3,11 +3,6 @@ import moment from 'moment';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getReOrderBatteryAndAccessoriesInformationData } from '../actions';
-import {
-  HEARING_AID_ACCESSORIES,
-  BLUE_BACKGROUND,
-  WHITE_BACKGROUND,
-} from '../constants';
 // TODO: Safety checks for `selected` callback and `label` element
 
 class SelectArrayItemsAccessoriesWidget extends React.Component {
@@ -35,54 +30,48 @@ class SelectArrayItemsAccessoriesWidget extends React.Component {
     const { supplies } = this.props;
     const { selectedItems } = this.state;
 
-    return supplies.map(
-      supply =>
-        supply.productGroup === HEARING_AID_ACCESSORIES &&
-        supply.availableForReorder === true ? (
-          <div key={supply.productId} className="order-background">
-            <p className="vads-u-font-size--md vads-u-font-weight--bold">
-              {supply.productName}
+    return supplies.map(supply => (
+      <div key={supply.productId} className="order-background">
+        <p className="vads-u-font-size--md vads-u-font-weight--bold">
+          {supply.productName}
+        </p>
+        <div className="vads-u-border-left--10px vads-u-border-color--primary-alt">
+          <div className="usa-alert-body mdot-alert-body">
+            <p className="vads-u-margin--1px">
+              <span className="vads-u-font-weight--bold">Battery: </span>
+              {supply.productId}
             </p>
-            <div className="vads-u-border-left--10px vads-u-border-color--primary-alt">
-              <div className="usa-alert-body mdot-alert-body">
-                <p className="vads-u-margin--1px">
-                  <span className="vads-u-font-weight--bold">Battery: </span>
-                  {supply.productId}
-                </p>
-                <p className="vads-u-margin--1px">
-                  <span className="vads-u-font-weight--bold">Quantity: </span>
-                  {supply.quantity} <br />
-                  Approximately {supply.quantity} months supply
-                </p>
-                <p className="vads-u-margin--1px">
-                  <span className="vads-u-font-weight--bold">
-                    Last order date:{' '}
-                  </span>{' '}
-                  {moment(supply.lastOrderDate).format('MM/DD/YYYY')}
-                </p>
-              </div>
-            </div>
-            <div
-              className={
-                selectedItems.includes(supply.productId)
-                  ? BLUE_BACKGROUND
-                  : WHITE_BACKGROUND
-              }
-            >
-              <input
-                name={supply.productId}
-                type="checkbox"
-                onChange={this.handleChecked}
-              />
-              <label htmlFor={supply.productId} className="main">
-                Order accessoires for this device
-              </label>
-            </div>
+            <p className="vads-u-margin--1px">
+              <span className="vads-u-font-weight--bold">Quantity: </span>
+              {supply.quantity} <br />
+              Approximately {supply.quantity} months supply
+            </p>
+            <p className="vads-u-margin--1px">
+              <span className="vads-u-font-weight--bold">
+                Last order date:{' '}
+              </span>{' '}
+              {moment(supply.lastOrderDate).format('MM/DD/YYYY')}
+            </p>
           </div>
-        ) : (
-          ''
-        ),
-    );
+        </div>
+        <div
+          className={
+            selectedItems.includes(supply.productId)
+              ? 'vads-u-background-color--primary button-dimensions vads-u-color--white vads-u-border-color--primary vads-u-border--2px'
+              : 'vads-u-background-color--white vads-u-color--link-default button-dimensions vads-u-border-color--primary vads-u-border--2px'
+          }
+        >
+          <input
+            name={supply.productId}
+            type="checkbox"
+            onChange={this.handleChecked}
+          />
+          <label htmlFor={supply.productId} className="main">
+            Order batteries for this device
+          </label>
+        </div>
+      </div>
+    ));
   }
 }
 
