9cbf7c185bb4a80e636bd2e8975441b2db89d5ee
Dan Hinze <dan.hinze@adhocteam.us>
2020-03-29 23:04:59 -0500

    Cover dismiss handler in separate unit test

--- src/platform/site-wide/announcements/tests/components/SingleSignOnInfoModal.unit.spec.jsx
+++ src/platform/site-wide/announcements/tests/components/SingleSignOnInfoModal.unit.spec.jsx
@@ -7,26 +7,6 @@ import localStorage from 'platform/utilities/storage/localStorage';
 import SingleSignOnInfoModal from '../../components/SingleSignOnInfoModal';
 
 describe('Announcements <SingleSignOnInfoModal>', () => {
-  it('renders for logged in SSO users', () => {
-    localStorage.setItem('hasSessionSSO', true);
-    const dismiss = sinon.spy();
-
-    const tree = mount(<SingleSignOnInfoModal isLoggedIn dismiss={dismiss} />);
-
-    expect(tree.text()).to.contain(
-      'Sign in once to access the VA sites you use most',
-    );
-
-    tree
-      .find('button')
-      .at(1)
-      .props()
-      .onClick();
-    expect(dismiss.called).to.be.true;
-
-    tree.unmount();
-  });
-
   it('does not render for users that are logged in but without SSO', () => {
     localStorage.setItem('hasSessionSSO', false);
     const tree = mount(<SingleSignOnInfoModal isLoggedIn />);
@@ -37,5 +17,34 @@ describe('Announcements <SingleSignOnInfoModal>', () => {
   });
   afterEach(() => {
     localStorage.clear();
+  });
+
+  it('renders for logged in SSO users', () => {
+    localStorage.setItem('hasSessionSSO', true);
+    const dismiss = sinon.spy();
+
+    const tree = mount(<SingleSignOnInfoModal isLoggedIn dismiss={dismiss} />);
+
+    expect(tree.text()).to.contain(
+      'Sign in once to access the VA sites you use most',
+    );
+
+    tree.unmount();
+  });
+
+  it('closes when the dismiss handler is fired', () => {
+    localStorage.setItem('hasSessionSSO', true);
+    const dismiss = sinon.spy();
+
+    const tree = mount(<SingleSignOnInfoModal isLoggedIn dismiss={dismiss} />);
+
+    tree
+      .find('button')
+      .at(1)
+      .props()
+      .onClick();
+    expect(dismiss.called).to.be.true;
+
+    tree.unmount();
   });
 });
