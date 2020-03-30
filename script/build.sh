881ec9612c7bf018b2baa85ac917fe6b857da9d4
Christopher Valarida <chris.valarida@adhocteam.us>
2020-03-30 12:47:43 -0700

    Start a consolidated build script

---
+++ script/build.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+##
+# Run the application and content builds. This script builds or fetches the
+# pre-built application assets based on the --asset-source option.
+###
+
+# Default to local
+assetSource="local"
+
+# Save the arguments to this script for later; they get drained in the following for loop
+args="$*"
+
+# Get options
+for o in "$@"; do
+    case "${o}" in
+        --asset-source)
+            assetSource="$2"
+            shift # past "--asset-source"
+            shift # past value
+            ;;
+        --asset-source=*)
+            assetSource="${o#*=}" # grab the value
+            shift # past the --asset-source=value
+            ;;
+        *)
+            ;;
+    esac
+done
+
+echo "assetSource: ${assetSource}"
+
+# Only run Webpack if the assetSource = local
+if [ "${assetSource}" = "local" ]; then
+    echo "Building application assets"
+    # yarn build:webpack "${args}"
+else
+    echo "Fetching application assets"
+    # TODO: Actually fetch them
+fi
+
+# Always build the content
+# yarn build:content "${args}"
