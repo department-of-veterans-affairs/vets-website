8378591441f5a08c18d5a6844ad988e3f71bb94f
hughes-dustin <hughes_dustin@bah.com>
2020-03-31 11:31:10 -0400

    create copy of 1995 to separate STEM functionality

---
+++ src/applications/edu-benefits/1995s/submit-transformer.js
@@ -0,0 +1,58 @@
+import _ from 'lodash';
+import { transformForSubmit } from 'platform/forms-system/src/js/helpers';
+import { determineEligibilityFor1995Stem } from './helpers';
+
+export function transform(formConfig, form) {
+  const newSchoolTransform = formData => {
+    let clonedData = _.cloneDeep(formData);
+
+    delete clonedData.newSchoolName;
+    delete clonedData.newSchoolAddress;
+
+    clonedData = {
+      ...clonedData,
+      newSchool: {
+        ...clonedData.newSchool,
+        name: formData.newSchoolName,
+        address: formData.newSchoolAddress,
+      },
+    };
+    return clonedData;
+  };
+
+  const fryScholarshipTransform = formData => {
+    // 1995-STEM related
+    const clonedData = _.cloneDeep(formData);
+    if (clonedData.benefit === 'fryScholarship') {
+      clonedData.benefit = 'chapter33';
+    }
+    const submitAs1995Stem = determineEligibilityFor1995Stem(clonedData);
+    if (submitAs1995Stem !== undefined && !submitAs1995Stem) {
+      clonedData.isEdithNourseRogersScholarship = false;
+    }
+    return clonedData;
+  };
+
+  // This needs to be last function call in array below
+  const usFormTransform = formData =>
+    transformForSubmit(formConfig, { ...form, data: formData });
+
+  const contactInfoTransform = formData => ({
+    ...formData,
+    email: _.get(formData, 'view:otherContactInfo.email', undefined),
+    homePhone: _.get(formData, 'view:otherContactInfo.homePhone', undefined),
+  });
+
+  const transformedData = [
+    newSchoolTransform,
+    fryScholarshipTransform,
+    contactInfoTransform,
+    usFormTransform, // This needs to be last function call in array
+  ].reduce((formData, transformer) => transformer(formData), form.data);
+
+  return JSON.stringify({
+    educationBenefitsClaim: {
+      form: transformedData,
+    },
+  });
+}
