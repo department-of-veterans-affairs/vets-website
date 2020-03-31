14480c53e37eaf83a476e31a2d1f6053bdd4b3d1
mr0sari0 <mahariel.rosario@gmail.com>
2020-03-31 13:12:59 -0400

    comment out setData action for now

--- src/applications/disability-benefits/2346/actions/index.js
+++ src/applications/disability-benefits/2346/actions/index.js
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/browser';
-import { setData } from 'platform/forms-system/src/js/actions';
+// import { setData } from 'platform/forms-system/src/js/actions';
 import {
   FETCH_REORDER_BATTERY_AND_ACCESSORIES_INFORMATION,
   FETCH_REORDER_BATTERY_AND_ACCESSORIES_INFORMATION_FAILURE,
@@ -120,7 +120,7 @@ export const tempAddressIsAMilitaryBaseStatusChangeFailure = error => ({
 export const permAddressIsSelected = data => dispatch => {
   try {
     dispatch(permAddressSelectedSuccessful(data));
-    dispatch(setData(data));
+    // dispatch(setData(data));
   } catch (error) {
     permAddressSelectedFailure(
       error,
@@ -132,7 +132,7 @@ export const permAddressIsSelected = data => dispatch => {
 export const tempAddressIsSelected = data => dispatch => {
   try {
     dispatch(tempAddressSelectedSuccessful(data));
-    dispatch(setData(data));
+    // dispatch(setData(data));
   } catch (error) {
     tempAddressSelectedFailure(
       error,
@@ -158,7 +158,7 @@ export const permAddressMilitaryBaseChecker = data => dispatch => {
         ),
       );
     }
-    dispatch(setData(data));
+    // dispatch(setData(data));
   } catch (error) {
     permAddressMilitaryBaseStatusChangeFailure(
       error,
@@ -186,7 +186,7 @@ export const tempAddressMilitaryBaseChecker = data => dispatch => {
         ),
       );
     }
-    dispatch(setData(data));
+    // dispatch(setData(data));
   } catch (error) {
     tempAddressIsAMilitaryBaseStatusChangeFailure(
       error,
