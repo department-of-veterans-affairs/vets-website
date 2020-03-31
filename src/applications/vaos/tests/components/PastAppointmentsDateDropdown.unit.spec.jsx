acf4869f04ac1b7f33b9983a63d26f32c3c173f7
Narin Ratana <narin@users.noreply.github.com>
2020-03-31 13:04:18 -0700

    Past appointments (#12042)
    * Add tabs.  Componentize future appointments.
    
    * Fetch and display past appointments.  Hide cancel/calendar.
    
    * Start dropdown
    
    * Add delay to mock confirm calls for testing
    
    * Implement past appointment date range dropdown
    
    * Update based on changed from master
    
    * Styling for mobile
    
    * Unit tests
    
    * Remove css nesting
    
    * Change const export to function for PastAppointmentsDateDropdown
    
    * Fix test
    
    * Remove unused import
    
    * Update mock data for past appointments
    
    * Add tests
    
    * Fix date option time formatting
    
    * Hide border color and video link if past appointment
    
    * Update url based on tab
    
    * Update test
    
    * Add test
    
    * Add test for this.fetchPastAppointments()
    
    * Add test for onTabSelect.  Use router push instead of browserHistory
    
    * Move mhv link back into correct spot
    
    * Move list headers into AppointmentsPage

---
+++ src/applications/vaos/tests/components/PastAppointmentsDateDropdown.unit.spec.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+import moment from 'moment';
+import PastAppointmentsDateDropdown from '../../components/PastAppointmentsDateDropdown';
+import { getPastAppointmentDateRangeOptions } from '../../utils/appointment';
+
+const dateRanges = getPastAppointmentDateRangeOptions(moment('2020-02-02'));
+
+describe('VAOS <PastAppointmentsDateDropdown/>', () => {
+  it('should render options', () => {
+    const onChange = sinon.spy();
+
+    const tree = mount(
+      <PastAppointmentsDateDropdown
+        onChange={onChange}
+        options={dateRanges}
+        value={0}
+      />,
+    );
+
+    const label = tree.find('label');
+    expect(label.props().htmlFor).to.equal('options');
+    const options = tree.find('option');
+    expect(tree.find('select').exists()).to.be.true;
+    expect(options.length).to.equal(6);
+    tree.unmount();
+  });
+
+  it('should call onChange event', () => {
+    const onChange = sinon.spy();
+
+    const tree = mount(
+      <PastAppointmentsDateDropdown
+        onChange={onChange}
+        options={dateRanges}
+        value={0}
+      />,
+    );
+
+    tree.find('select').simulate('change', { target: { value: 1 } });
+    expect(onChange.called).to.be.true;
+    tree.unmount();
+  });
+});
