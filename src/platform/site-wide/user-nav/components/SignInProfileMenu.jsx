185221aebd4614d712d5101f51e89b39d4dbc602
Eric Buckley <eric.buckley@gmail.com>
2020-03-30 17:28:21 -0700

    fixing import of redux connected PersonalizationDropdown component

--- src/platform/site-wide/user-nav/components/SignInProfileMenu.jsx
+++ src/platform/site-wide/user-nav/components/SignInProfileMenu.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import DropDownPanel from '@department-of-veterans-affairs/formation-react/DropDownPanel';
 import IconUser from '@department-of-veterans-affairs/formation-react/IconUser';
 
-import PersonalizationDropdown from './PersonalizationDropdown';
+import { PersonalizationDropdown } from './PersonalizationDropdown';
 
 class SignInProfileMenu extends React.Component {
   render() {
