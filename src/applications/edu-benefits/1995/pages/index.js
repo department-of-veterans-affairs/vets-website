23fa232f062ee2878c3d3ab9b943ff9eaf347621
Joe <elder_joseph@bah.com>
2020-04-01 13:55:03 -0400

    7272: Correct linting

--- src/applications/edu-benefits/1995/pages/index.js
+++ src/applications/edu-benefits/1995/pages/index.js
@@ -1,6 +1,4 @@
 import * as activeDuty from './activeDuty';
 import * as benefitSelection from './benefitSelection';
-import * as stem from './stem';
-import * as stemEligibility from './stemEligibility';
 
-export { activeDuty, benefitSelection, stem, stemEligibility };
+export { activeDuty, benefitSelection };
