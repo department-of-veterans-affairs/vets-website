a017782835ee75948725f598e49fe69ca49d51a4
Eric Buckley <eric.buckley@gmail.com>
2020-04-02 12:59:46 -0700

    converting PaymentInformation2FARequired back to a function, we dont need it to be a React.Component to connect it

--- src/applications/personalization/profile360/components/PaymentInformation2FARequired.jsx
+++ src/applications/personalization/profile360/components/PaymentInformation2FARequired.jsx
@@ -7,41 +7,39 @@ import { ssoe } from 'platform/user/authentication/selectors';
 import { mfa } from 'platform/user/authentication/utilities';
 import recordEvent from 'platform/monitoring/record-event';
 
-export class PaymentInformation2FARequired extends React.Component {
-  mfaHandler = () => {
-    recordEvent({ event: 'multifactor-link-clicked' });
-    mfa(this.props.useSSOe ? 'v1' : 'v0');
-  };
+function mfaHandler(useSSOe) {
+  recordEvent({ event: 'multifactor-link-clicked' });
+  mfa(useSSOe ? 'v1' : 'v0');
+}
 
-  render() {
-    return (
-      <AlertBox
-        status="success"
-        isVisible
-        headline="You’ll need to set up 2-factor authentication before you can edit your direct deposit information."
-      >
-        <>
-          <p>
-            We require this to help protect your bank account information and
-            prevent fraud.
-          </p>
-          <p>
-            Authentication gives you an extra layer of security by letting you
-            into your account only after you’ve signed in with a password and a
-            6-digit code sent directly to your mobile or home phone. This helps
-            to make sure that no one but you can access your account—even if
-            they get your password.
-          </p>
-          <button
-            className="usa-button-primary va-button-primary"
-            onClick={this.mfaHandler}
-          >
-            Set up 2-factor authentication
-          </button>
-        </>
-      </AlertBox>
-    );
-  }
+export function PaymentInformation2FARequired({ useSSOe }) {
+  return (
+    <AlertBox
+      status="success"
+      isVisible
+      headline="You’ll need to set up 2-factor authentication before you can edit your direct deposit information."
+    >
+      <>
+        <p>
+          We require this to help protect your bank account information and
+          prevent fraud.
+        </p>
+        <p>
+          Authentication gives you an extra layer of security by letting you
+          into your account only after you’ve signed in with a password and a
+          6-digit code sent directly to your mobile or home phone. This helps to
+          make sure that no one but you can access your account—even if they get
+          your password.
+        </p>
+        <button
+          className="usa-button-primary va-button-primary"
+          onClick={() => mfaHandler(useSSOe)}
+        >
+          Set up 2-factor authentication
+        </button>
+      </>
+    </AlertBox>
+  );
 }
 
 function mapStateToProps(state) {
