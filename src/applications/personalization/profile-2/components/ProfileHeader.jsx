2f88ce938f8472b8e6cba41324f6751dd025f2bc
Brad Conley <brad.conl3y@gmail.com>
2020-04-02 16:20:36 -0400

    Fix styles and convert to connected

--- src/applications/personalization/profile-2/components/ProfileHeader.jsx
+++ src/applications/personalization/profile-2/components/ProfileHeader.jsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const ProfileHeader = () => (
-  <div className="headerWrapper vads-u-background-color--gray-dark vads-u-color--white vads-u-margin-bottom--2 vads-u-padding-y--3 vads-u-display--flex vads-u-align-items--center vads-u-justify-content--center">
-    <div className="headerContentWrapper vads-u-display--flex vads-u-align-items--center vads-u-justify-content--space-evenly">
-      <div className="vads-u-padding-x--6">
-        <img
-          className="profileServiceBadge"
-          src="/img/vic-army-symbol.png"
-          alt="service badge"
-        />
+class ProfileHeader extends Component {
+  render() {
+    const { first, middle, last } = this.props.userFullName;
+    return (
+      <div className="headerWrapper vads-u-background-color--gray-dark vads-u-color--white vads-u-margin-bottom--2 vads-u-padding-y--3 vads-u-display--flex vads-u-align-items--center vads-u-justify-content--center">
+        <div className="headerContentWrapper usa-grid usa-grid-full vads-u-display--flex vads-u-align-items--center">
+          <div className="vads-u-padding-x--6 usa-width-one-fourth">
+            <img
+              className="profileServiceBadge"
+              src="/img/vic-army-symbol.png"
+              alt="service badge"
+            />
+          </div>
+          <div className="headerNameWrapper vads-u-flex-direction--column">
+            <h1 className="profileHeading vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--normal">
+              Your Profile
+            </h1>
+            <h2 className="vads-u-font-size--h3">
+              {first.toLowerCase()} {middle.toLowerCase()} {last.toLowerCase()}
+            </h2>
+            <h3 className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--normal">
+              United States Army Reserve
+            </h3>
+          </div>
+        </div>
       </div>
-      <div className="headerNameWrapper vads-u-flex-direction--column">
-        <h1 className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--normal">
-          Your Profile
-        </h1>
-        <h2 className="vads-u-font-size--h3">
-          Kimberly Elizabeth Smith Washington
-        </h2>
-        <h3 className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--normal">
-          United States Army Reserve
-        </h3>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
-export default ProfileHeader;
+const mapStateToProps = state => ({
+  userFullName: state.user.profile.userFullName,
+});
+
+export default connect(mapStateToProps)(ProfileHeader);
