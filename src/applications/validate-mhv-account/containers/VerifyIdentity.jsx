99f64fb3b7533544dcf8fb2f2a5a368e9989177b
Eric Buckley <eric.buckley@gmail.com>
2020-04-02 13:51:22 -0700

    converting VerifyIdentity back to a function, we dont need it to be a React.Component to connect it

--- src/applications/validate-mhv-account/containers/VerifyIdentity.jsx
+++ src/applications/validate-mhv-account/containers/VerifyIdentity.jsx
@@ -6,31 +6,28 @@ import MessageTemplate from './../components/MessageTemplate';
 import { ssoe } from 'platform/user/authentication/selectors';
 import { verify } from 'platform/user/authentication/utilities';
 
-export class VerifyIdentity extends React.Component {
-  render() {
-    const authVersion = this.props.useSSOe ? 'v1' : 'v0';
+export function VerifyIdentity({ useSSOe }) {
+  const authVersion = useSSOe ? 'v1' : 'v0';
+  const content = {
+    heading: 'Verify your identity to access health tools',
+    body: (
+      <>
+        <p>
+          We take your privacy seriously, and we’re committed to protecting your
+          information. You’ll need to verify your identity before we can give
+          you access to your personal health information.
+        </p>
+        <button
+          onClick={() => verify(authVersion)}
+          className="usa-button-primary va-button-primary"
+        >
+          Verify your identity
+        </button>
+      </>
+    ),
+  };
 
-    const content = {
-      heading: 'Verify your identity to access health tools',
-      body: (
-        <>
-          <p>
-            We take your privacy seriously, and we’re committed to protecting
-            your information. You’ll need to verify your identity before we can
-            give you access to your personal health information.
-          </p>
-          <button
-            onClick={() => verify(authVersion)}
-            className="usa-button-primary va-button-primary"
-          >
-            Verify your identity
-          </button>
-        </>
-      ),
-    };
-
-    return <MessageTemplate content={content} />;
-  }
+  return <MessageTemplate content={content} />;
 }
 
 function mapStateToProps(state) {
