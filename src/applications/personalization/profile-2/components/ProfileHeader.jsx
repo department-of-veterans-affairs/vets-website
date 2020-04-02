4b7600cb31aa022eaadbf19735277db91b56276e
Brad Conley <brad.conl3y@gmail.com>
2020-04-02 16:33:06 -0400

    add defaultProps and derive image from props

--- src/applications/personalization/profile-2/components/ProfileHeader.jsx
+++ src/applications/personalization/profile-2/components/ProfileHeader.jsx
@@ -2,15 +2,38 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class ProfileHeader extends Component {
+  static defaultProps = {
+    userFullName: {
+      first: '',
+      middle: '',
+      last: '',
+    },
+    // defaulting to Army for now as placeholder
+    latestBranchOfService: 'A',
+  };
+
   render() {
-    const { first, middle, last } = this.props.userFullName;
+    const imagePaths = {
+      A: '/img/vic-army-symbol.png',
+      C: '/img/vic-cg-emblem.png',
+      F: '/img/vic-air-force-coat-of-arms.png',
+      N: '/img/vic-navy-emblem.png',
+      M: '/img/vic-usmc-emblem.png',
+      VASeal: '/img/vic-va-seal.png',
+    };
+
+    const {
+      userFullName: { first, middle, last },
+      latestBranchOfService,
+    } = this.props;
+
     return (
       <div className="headerWrapper vads-u-background-color--gray-dark vads-u-color--white vads-u-margin-bottom--2 vads-u-padding-y--3 vads-u-display--flex vads-u-align-items--center vads-u-justify-content--center">
         <div className="headerContentWrapper usa-grid usa-grid-full vads-u-display--flex vads-u-align-items--center">
           <div className="vads-u-padding-x--6 usa-width-one-fourth">
             <img
               className="profileServiceBadge"
-              src="/img/vic-army-symbol.png"
+              src={imagePaths[`${latestBranchOfService}`]}
               alt="service badge"
             />
           </div>
