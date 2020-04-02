c7ad53ee38a15b024c277df1b8926a25a23a4e3d
Micah Chiang <micahkchiang@gmail.com>
2020-04-02 16:27:40 -0700

    update a couple addresses

--- src/applications/disability-benefits/686c-674/config/chapters/report-add-a-spouse/does-live-with-spouse/doesLiveWithSpouse.js
+++ src/applications/disability-benefits/686c-674/config/chapters/report-add-a-spouse/does-live-with-spouse/doesLiveWithSpouse.js
@@ -1,12 +1,8 @@
 import { genericSchemas } from '../../../generic-schema';
-
+import { buildAddressSchema, addressUISchema } from '../../../address-schema';
 import { doesLiveTogether } from './helpers';
 
-const {
-  countryDropdown,
-  genericTextInput,
-  genericNumberAndDashInput: numberAndDashInput,
-} = genericSchemas;
+const { genericTextInput } = genericSchemas;
 
 export const schema = {
   type: 'object',
@@ -15,18 +11,7 @@ export const schema = {
       type: 'boolean',
     },
     currentSpouseReasonForSeparation: genericTextInput,
-    currentSpouseAddress: {
-      type: 'object',
-      properties: {
-        currentSpouseCountry: countryDropdown,
-        currentSpouseStreet: genericTextInput,
-        currentSpouseLine2: genericTextInput,
-        currentSpouseLine3: genericTextInput,
-        currentSpouseCity: genericTextInput,
-        currentSpouseState: genericTextInput,
-        currentSpousePostalCode: numberAndDashInput,
-      },
-    },
+    currentSpouseAddress: buildAddressSchema(false),
   },
 };
 
@@ -47,46 +32,24 @@ export const uiSchema = {
     'ui:errorMessages': { required: 'Please give a brief explanation' },
   },
   currentSpouseAddress: {
-    'ui:title': 'Your spouse’s address',
-    'ui:options': {
-      expandUnder: 'spouseDoesLiveWithVeteran',
-      expandUnderCondition: false,
-    },
-    currentSpouseCountry: {
-      'ui:title': 'Country',
-      'ui:required': doesLiveTogether,
-      'ui:errorMessages': { required: 'Please select a country' },
-    },
-    currentSpouseStreet: {
-      'ui:title': 'Street',
-      'ui:required': doesLiveTogether,
-      'ui:errorMessages': { required: 'Please enter a street address' },
-    },
-    currentSpouseLine2: {
-      'ui:title': 'Line 2',
-    },
-    currentSpouseLine3: {
-      'ui:title': 'Line 3',
-    },
-    currentSpouseCity: {
-      'ui:title': 'City',
-      'ui:required': doesLiveTogether,
-      'ui:errorMessages': { required: 'Please enter a city' },
-    },
-    currentSpouseState: {
-      'ui:title': 'State',
-      'ui:required': doesLiveTogether,
-      'ui:errorMessages': {
-        required: 'Please enter a state, or country if outside the U.S.',
-      },
-    },
-    currentSpousePostalCode: {
-      'ui:required': doesLiveTogether,
+    ...addressUISchema(false, 'currentSpouseAddress', doesLiveTogether),
+    ...{
+      'ui:title': 'Your spouse’s address',
       'ui:options': {
-        widgetClassNames: 'usa-input-medium',
+        expandUnder: 'spouseDoesLiveWithVeteran',
+        expandUnderCondition: false,
+        updateSchema: (formData, formSchema) => {
+          // if someone selects a country, and then changes their mind and selects 'no' for spouseDoesLiveWithVeteran,
+          // The collapsed form will silently throw an error because some fields are required based on country.
+          // manually clearning the required array fixes this issue.
+          if (formData.spouseDoesLiveWithVeteran) {
+            return {
+              required: [],
+            };
+          }
+          return formSchema;
+        },
       },
-      'ui:title': 'Postal Code',
-      'ui:errorMessages': { required: 'Please enter a postal code' },
     },
   },
 };
