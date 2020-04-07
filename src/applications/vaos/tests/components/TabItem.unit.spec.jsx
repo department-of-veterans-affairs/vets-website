ecdf08894b35f7a2970f7e06aa49605c8d2bea80
Narin Ratana <n.ratana@gmail.com>
2020-04-07 16:28:06 -0700

    Add tab unit tests

---
+++ src/applications/vaos/tests/components/TabItem.unit.spec.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import { TabItem } from '../../components/TabItem';
+
+const location = {
+  pathname: '/upcoming',
+};
+
+describe('<TabItem>', () => {
+  it('should render tab', () => {
+    const tree = shallow(
+      <TabItem
+        shortcut={1}
+        title="Title"
+        location={location}
+        tabpath="upcoming"
+      />,
+    );
+
+    expect(tree.find('IndexLink').props()['aria-controls']).to.equal(
+      'tabPanelTitle',
+    );
+    expect(tree.find('IndexLink').props().to).to.equal('upcoming');
+    tree.unmount();
+  });
+
+  it('should use id if present', () => {
+    const tree = shallow(
+      <TabItem
+        shortcut={1}
+        id="TitleHere"
+        title="Title Here"
+        location={location}
+        tabpath="upcoming"
+      />,
+    );
+
+    expect(tree.find('IndexLink').props()['aria-controls']).to.equal(
+      'tabPanelTitleHere',
+    );
+    expect(tree.find('IndexLink').props().id).to.equal('tabTitleHere');
+    expect(tree.find('IndexLink').props().to).to.equal('upcoming');
+    tree.unmount();
+  });
+});
