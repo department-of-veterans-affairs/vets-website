c8a4a14de8baf43e31dab61fd2e6197b6c086808
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-04-08 16:27:05 -0400

    Stub responses in search E2E test with Pact interactions.

--- src/applications/search/tests/search.cypress.spec.js
+++ src/applications/search/tests/search.cypress.spec.js
@@ -1,6 +1,38 @@
 import URLSearchParams from 'url-search-params';
 
+import contract from '../../../../pacts/va.gov_search-va.gov_api.json';
+
+const scenarios = contract.interactions.reduce(
+  (acc, cur) => ({
+    ...acc,
+    [cur.providerState]: cur.response.body,
+  }),
+  {},
+);
+
 describe('Search', () => {
+  before(() => {
+    cy.server();
+
+    cy.route(
+      'GET',
+      '/v0/search?query=test1',
+      scenarios['no matching results exist'],
+    ).as('searchWithNoResults');
+
+    cy.route(
+      'GET',
+      '/v0/search?query=test2',
+      scenarios['multiple matching results exist'],
+    ).as('searchWithMultipleResults');
+
+    // Workaround to intercept requests made with fetch API.
+    cy.on('window:before:load', window => {
+      // eslint-disable-next-line no-param-reassign
+      delete window.fetch;
+    });
+  });
+
   it('searches from the home page', () => {
     cy.visit('http://localhost:3001');
 
@@ -11,14 +43,28 @@ describe('Search', () => {
     cy.contains('Search').click();
 
     cy.get('#query')
-      .type('test')
+      .type('test1')
       .type('{enter}');
 
     cy.location().should(({ pathname, search }) => {
       expect(pathname).to.eq('/search/');
-      expect(new URLSearchParams(search).get('query')).to.eq('test');
+      expect(new URLSearchParams(search).get('query')).to.eq('test1');
     });
 
-    cy.get('form.search-box input').should('have.value', 'test');
+    cy.get('form.search-box input').should('have.value', 'test1');
+
+    cy.wait('@searchWithNoResults')
+      .get('ul.results-list')
+      .should('not.exist');
+
+    cy.contains('Search').click();
+
+    cy.get('#query')
+      .type('test2')
+      .type('{enter}');
+
+    cy.wait('@searchWithMultipleResults')
+      .get('ul.results-list')
+      .should('exist');
   });
 });
