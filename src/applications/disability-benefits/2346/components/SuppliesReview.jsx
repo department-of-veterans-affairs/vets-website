b26dbbb2b12998e5992961835ee8151da06b7dab
Brad Conley <brad.conl3y@gmail.com>
2020-04-09 16:36:54 -0400

    Use breadcrumb component and remove unused components

--- src/applications/disability-benefits/2346/components/SuppliesReview.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const SuppliesReview = () => <div />;
-
-export default SuppliesReview;
--- src/applications/disability-benefits/2346/containers/App.jsx
+++ src/applications/disability-benefits/2346/containers/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-
+import Breadcrumbs from '@department-of-veterans-affairs/formation-react/Breadcrumbs';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from '../config/form';
 
 export default function App({ location, children }) {
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+      <Breadcrumbs>
+        {[
+          <a href="#" key="1">
+            Home
+          </a>,
+          <a href="#" key="2">
+            Health care
+          </a>,
+          <a href="#" key="3">
+            Order hearing aid batteries and accessories
+          </a>,
+        ]}
+      </Breadcrumbs>
       {children}
     </RoutedSavableApp>
   );
