742b9cbf301cb5ea674ab9f9f70215886b1ce8eb
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2020-04-09 10:15:43 -0400

    added sec103 feature flag to featureFlagNames.js

--- src/platform/utilities/feature-toggles/featureFlagNames.js
+++ src/platform/utilities/feature-toggles/featureFlagNames.js
@@ -12,4 +12,5 @@ export default Object.freeze({
   vaProfileAddressValidation: 'vaProfileAddressValidation',
   ssoe: 'ssoe',
   eduBenefitsStemScholarship: 'eduBenefitsStemScholarship',
+  eduEstimateYourBenefitsSection103: 'eduEstimateYourBenefitsSection103',
 });
