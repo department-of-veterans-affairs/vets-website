afe66b8483c67eb0e60ecaa956cba00bb2d1bdc1
Matt Roth <roth_matthew@bah.com>
2020-04-10 12:43:07 -0400

    break out page specific behavior into functions, expand and collapse all the main accordion sections

--- src/applications/gi/tests/00-required.e2e.spec.js
+++ src/applications/gi/tests/00-required.e2e.spec.js
@@ -4,7 +4,7 @@ const GiHelpers = require('./gibct-helpers');
 const institutionProfile = require('./e2e/institution-profile.json');
 
 module.exports = E2eHelpers.createE2eTest(client => {
-  const institution = institutionProfile.data;
+  const institutionAttributes = institutionProfile.data.attributes;
 
   GiHelpers.initApplicationMock();
 
@@ -20,39 +20,34 @@ module.exports = E2eHelpers.createE2eTest(client => {
     .axeCheck('.main');
 
   // Landing Page: Search for an institution
-  client
-    .waitForElementVisible(
-      '.keyword-search input[type="text"]',
-      Timeouts.normal,
-    )
-    .clearValue('.keyword-search input[type="text"]')
-    .setValue(
-      '.keyword-search input[type="text"]',
-      institution.attributes.name,
-    );
+  GiHelpers.searchForInstitution(client);
 
-  // Landing Page: Search
-  client.click('#search-button');
-
-  // client.pause(10000000);
-  // Search Page window.location.href
-  // E2eHelpers.expectLocation(client, '/search');
-  client
-    .waitForElementVisible('.search-page', Timeouts.normal)
-    .axeCheck('.main');
-
-  // Search Page: Go to first result
-  client
-    .waitForElementVisible('.search-result a', Timeouts.normal)
-    .click('.search-result a');
+  // Search Page
+  GiHelpers.selectSearchResult(client);
 
   // Profile Page
-  E2eHelpers.expectLocation(
+  GiHelpers.expectLocation(
     client,
-    `/profile/${institution.attributes.facility_code}`,
+    `/profile/${institutionAttributes.facility_code}`,
   );
   client.waitForElementVisible('.profile-page', Timeouts.normal);
-  // .axeCheck('.main');
+  // .axeCheck('.main'); // commented out until 7727 is fixed
+
+  // Estimate your benefits
+  GiHelpers.expandCollapseAccordion(client, 'Estimate your benefits');
+  GiHelpers.editEligibilityDetails(client);
+
+  // Veteran programs
+  GiHelpers.expandCollapseAccordion(client, 'Veteran programs');
+
+  // Cautionary information
+  GiHelpers.expandCollapseAccordion(client, 'Cautionary information');
+
+  // Contact details
+  GiHelpers.expandCollapseAccordion(client, 'Contact details');
+
+  // Additional information
+  GiHelpers.expandCollapseAccordion(client, 'Additional information');
 
   client.end();
 });
--- src/applications/gi/tests/gibct-helpers.js
+++ src/applications/gi/tests/gibct-helpers.js
@@ -1,21 +1,19 @@
-const Timeouts = require('platform/testing/e2e/timeouts.js');
+const Timeouts = require('../../../platform/testing/e2e/timeouts');
 const autocomplete = require('./e2e/autocomplete.json');
 const institutionProfile = require('./e2e/institution-profile.json');
 const searchResults = require('./e2e/search-results.json');
 const calculatorConstants = require('./e2e/calculator-constants.json');
-
-// Expects navigation lands at a path with the given `urlSubstring`.
-const expectLocation = (client, urlSubstring) => {
-  client.expect
-    .url()
-    .to.contain(urlSubstring)
-    .before(Timeouts.slow);
-};
-
 const mock = require('../../../platform/testing/e2e/mock-helpers');
 
+const institutionAttributes = institutionProfile.data.attributes;
+
+// Expects navigation lands at a path containing the given `urlSubstring`.
+const expectLocation = (client, urlSubstring) => {
+  client.assert.urlContains(urlSubstring);
+};
+
 // Create API routes
-function initApplicationMock() {
+const initApplicationMock = () => {
   mock(null, {
     path: '/v0/gi/institutions/search',
     verb: 'get',
@@ -55,9 +53,54 @@ function initApplicationMock() {
     verb: 'get',
     value: {},
   });
-}
+};
+
+const searchForInstitution = client => {
+  client
+    .waitForElementVisible(
+      '.keyword-search input[type="text"]',
+      Timeouts.normal,
+    )
+    .clearValue('.keyword-search input[type="text"]')
+    .setValue('.keyword-search input[type="text"]', institutionAttributes.name)
+    .click('#search-button');
+};
+
+const selectSearchResult = client => {
+  expectLocation(
+    client,
+    `/search?category=school&name=${institutionAttributes.name.replace(
+      /\s/g,
+      '+',
+    )}`,
+  );
+  client
+    .waitForElementVisible('.search-page', Timeouts.normal)
+    .axeCheck('.main');
+
+  // Select first result
+  client
+    .waitForElementVisible('.search-result a', Timeouts.normal)
+    .click('.search-result a');
+};
+
+const expandCollapseAccordion = (client, name) => {
+  const id = `#${name.toLowerCase().replace(/\s/g, '-')}`;
+  client
+    .waitForElementVisible(id, Timeouts.normal)
+    .click(`${id} button`)
+    .click(`${id} button`);
+};
+
+const editEligibilityDetails = client => {
+  client.waitForElementVisible('.eligibility-details', Timeouts.normal);
+};
 
 module.exports = {
-  initApplicationMock,
   expectLocation,
+  initApplicationMock,
+  searchForInstitution,
+  selectSearchResult,
+  expandCollapseAccordion,
+  editEligibilityDetails,
 };
