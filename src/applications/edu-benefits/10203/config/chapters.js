63a160e66b50d078f994d4124a49e05ff3bf57be
Joe <elder_joseph@bah.com>
2020-04-10 09:15:57 -0400

    7753: Remove unnecessary 22-1995 artifacts

--- src/applications/edu-benefits/10203/config/chapters.js
+++ src/applications/edu-benefits/10203/config/chapters.js
@@ -81,119 +81,11 @@ export const chapters = {
         uiSchema: activeDuty.uiSchema,
         schema: activeDuty.schema,
       },
-      servicePeriods: {
-        path: 'military/service',
-        title: 'Service periods',
-        depends: form => !display10203StemFlow(form), // 1995-STEM related
-        uiSchema: {
-          'view:newService': {
-            'ui:title':
-              'Do you have any new periods of service to record since you last applied for education benefits?',
-            'ui:widget': 'yesNo',
-          },
-          toursOfDuty: _.merge(toursOfDuty.uiSchema, {
-            'ui:options': { expandUnder: 'view:newService' },
-          }),
-        },
-        schema: {
-          type: 'object',
-          properties: {
-            'view:newService': {
-              type: 'boolean',
-            },
-            toursOfDuty: fullSchema1995.properties.toursOfDuty,
-          },
-        },
-      },
-      militaryHistory: {
-        title: 'Military history',
-        path: 'military/history',
-        depends: form => !display10203StemFlow(form), // 1995-STEM related
-        uiSchema: {
-          'view:hasServiceBefore1978': {
-            'ui:title':
-              'Do you have any periods of service that began before 1978?',
-            'ui:widget': 'yesNo',
-          },
-        },
-        schema: {
-          type: 'object',
-          properties: {
-            'view:hasServiceBefore1978': {
-              type: 'boolean',
-            },
-          },
-        },
-      },
-    },
-  },
-  schoolSelection: {
-    title: 'School Selection',
-    pages: {
-      newSchool: {
-        path: 'school-selection/new-school',
-        title:
-          'School, university, program, or training facility you want to attend',
-        initialData: {
-          newSchoolAddress: {},
-        },
-        uiSchema: {
-          'ui:title':
-            'School, university, program, or training facility you want to attend',
-          // Broken up because we need to fit educationType between name and address
-          // Put back together again in transform()
-          newSchoolName: {
-            'ui:title': 'Name of school, university, or training facility',
-            'ui:validations': [
-              (errors, newSchoolName) => {
-                validateWhiteSpace(errors, newSchoolName);
-              },
-            ],
-          },
-          educationType: educationTypeUISchema,
-          newSchoolAddress: _.merge(address.uiSchema(), {
-            'ui:options': {
-              hideIf: formData => !showSchoolAddress(formData.educationType),
-            },
-          }),
-          educationObjective: {
-            'ui:title':
-              'Education or career goal (for example, “Get a bachelor’s degree in criminal justice” or “Get an HVAC technician certificate” or “Become a police officer.”)',
-            'ui:widget': 'textarea',
-          },
-          nonVaAssistance: {
-            'ui:title':
-              'Are you getting, or do you expect to get any money (including, but not limited to, federal tuition assistance) from the Armed Forces or public health services for any part of your coursework or training?',
-            'ui:widget': 'yesNo',
-          },
-          civilianBenefitsAssistance: {
-            'ui:title':
-              'Are you getting benefits from the U.S. Government as a civilian employee during the same time as you’re seeking benefits from VA?',
-            'ui:widget': 'yesNo',
-          },
-        },
-        schema: {
-          type: 'object',
-          required: ['educationType', 'newSchoolName'],
-          properties: {
-            newSchoolName: {
-              type: 'string',
-            },
-            educationType,
-            newSchoolAddress: address.schema(fullSchema1995),
-            educationObjective,
-            nonVaAssistance,
-            civilianBenefitsAssistance,
-          },
-        },
-      },
-      oldSchool: createOldSchoolPage(fullSchema1995),
     },
   },
   personalInformation: {
     title: 'Personal Information',
     pages: {
-      contactInformation: createContactInformationPage(fullSchema1995),
       dependents: {
         title: 'Dependents',
         path: 'personal-information/dependents',
