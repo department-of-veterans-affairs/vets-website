095d12f1ffc06fdf0767e669ef3e6698b1c93855
Ethan Teague <metrowebz@gmail.com>
2020-04-10 19:36:12 -0400

    VAVFS-6206: Updating facility names with full titles.

--- src/site/stages/build/drupal/graphql/healthServicesListingPage.graphql.js
+++ src/site/stages/build/drupal/graphql/healthServicesListingPage.graphql.js
@@ -64,6 +64,7 @@ module.exports = `
                             }
                           }
                           fieldNicknameForThisFacility
+                          title
                         }
                       }
                     }
