b0526108f331cbbd054ba34524d2bf577feee81a
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-04-10 10:24:25 -0700

    Updated child marriage and unit tests

--- src/applications/disability-benefits/686c-674/config/chapters/report-marriage-of-child/child-information/childInformation.js
+++ src/applications/disability-benefits/686c-674/config/chapters/report-marriage-of-child/child-information/childInformation.js
@@ -9,43 +9,53 @@ const { fullName, date } = genericSchemas;
 
 export const schema = {
   type: 'object',
-  required: ['marriedChildName', 'dateChildMarried'],
   properties: {
-    marriedChildName: fullName,
-    dateChildMarried: date,
+    childMarriage: {
+      type: 'object',
+      required: ['marriedChildName', 'dateChildMarried'],
+      properties: {
+        marriedChildName: fullName,
+        dateChildMarried: date,
+      },
+    },
   },
 };
 
 export const uiSchema = {
-  'ui:title': 'Child who is now married',
-  marriedChildName: {
-    'ui:validations': [validateName],
-    first: {
-      'ui:title': 'Child’s first name',
-      'ui:errorMessages': { required: 'Please enter a first name' },
+  childMarriage: {
+    'ui:title': 'Child who is now married',
+    marriedChildName: {
+      'ui:validations': [validateName],
+      first: {
+        'ui:title': 'Child’s first name',
+        'ui:errorMessages': { required: 'Please enter a first name' },
+        'ui:required': formData =>
+          isChapterFieldRequired(
+            formData,
+            TASK_KEYS.reportMarriageOfChildUnder18,
+          ),
+      },
+      middle: { 'ui:title': 'Child’s middle name' },
+      last: {
+        'ui:title': 'Child’s last name',
+        'ui:errorMessages': { required: 'Please enter a last name' },
+        'ui:required': formData =>
+          isChapterFieldRequired(
+            formData,
+            TASK_KEYS.reportMarriageOfChildUnder18,
+          ),
+      },
+      suffix: {
+        'ui:title': 'Child’s suffix',
+        'ui:options': { widgetClassNames: 'form-select-medium' },
+      },
+    },
+    dateChildMarried: merge(currentOrPastDateUI('Date of marriage'), {
       'ui:required': formData =>
         isChapterFieldRequired(
           formData,
           TASK_KEYS.reportMarriageOfChildUnder18,
         ),
-    },
-    middle: { 'ui:title': 'Child’s middle name' },
-    last: {
-      'ui:title': 'Child’s last name',
-      'ui:errorMessages': { required: 'Please enter a last name' },
-      'ui:required': formData =>
-        isChapterFieldRequired(
-          formData,
-          TASK_KEYS.reportMarriageOfChildUnder18,
-        ),
-    },
-    suffix: {
-      'ui:title': 'Child’s suffix',
-      'ui:options': { widgetClassNames: 'form-select-medium' },
-    },
+    }),
   },
-  dateChildMarried: merge(currentOrPastDateUI('Date of marriage'), {
-    'ui:required': formData =>
-      isChapterFieldRequired(formData, TASK_KEYS.reportMarriageOfChildUnder18),
-  }),
 };
