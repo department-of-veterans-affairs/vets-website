62a5822faf6d8ac3225d91d250e85ad8ed4e490f
Matt Roth <roth_matthew@bah.com>
2020-04-13 12:38:44 -0400

    Update featureFlagNames.js

--- src/platform/utilities/feature-toggles/featureFlagNames.js
+++ src/platform/utilities/feature-toggles/featureFlagNames.js
@@ -12,4 +12,5 @@ export default Object.freeze({
   vaGlobalDowntimeNotification: 'vaGlobalDowntimeNotification',
   ssoe: 'ssoe',
   eduBenefitsStemScholarship: 'eduBenefitsStemScholarship',
+  gibctEstimateYourBenefits: 'gibctEstimateYourBenefits',
 });
