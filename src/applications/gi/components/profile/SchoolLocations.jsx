6c473f47a67573603417e15164a336a5b2a7cedd
Devin McCurdy <mccurdy_devin@bah.com>
2020-04-14 08:39:17 -0400

    Updated location list text (#12196)

--- src/applications/gi/components/profile/SchoolLocations.jsx
+++ src/applications/gi/components/profile/SchoolLocations.jsx
@@ -351,7 +351,7 @@ export class SchoolLocations extends React.Component {
     return (
       <div className="vads-u-padding-top--2">
         <i>
-          Showing {viewableRows} out of {totalRows}
+          Showing {viewableRows} of {totalRows} locations
         </i>
       </div>
     );
