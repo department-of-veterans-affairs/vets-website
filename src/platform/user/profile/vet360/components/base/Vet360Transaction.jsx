25fad6bc68c32504e99577c52d84b82361ce62f7
Brooks Johnson <brooks.johnson@pm.me>
2020-04-14 14:55:43 -0700

    Convert Vet360Transaction

--- src/platform/user/profile/vet360/components/base/Vet360Transaction.jsx
+++ src/platform/user/profile/vet360/components/base/Vet360Transaction.jsx
@@ -10,51 +10,51 @@ import {
 import Vet360TransactionInlineErrorMessage from './Vet360TransactionInlineErrorMessage';
 import Vet360TransactionPending from './Vet360TransactionPending';
 
-export default class Vet360Transaction extends React.Component {
-  static propTypes = {
-    children: PropTypes.node.isRequired,
-    refreshTransaction: PropTypes.func.isRequired,
-    title: PropTypes.string.isRequired,
-    transaction: PropTypes.object,
-  };
+function Vet360Transaction(props) {
+  const {
+    id,
+    isModalOpen,
+    children,
+    refreshTransaction,
+    title,
+    transaction,
+    transactionRequest,
+  } = props;
 
-  render() {
-    const {
-      id,
-      isModalOpen,
-      children,
-      refreshTransaction,
-      title,
-      transaction,
-      transactionRequest,
-    } = this.props;
+  const method = transactionRequest ? transactionRequest.method : 'PUT';
+  const hasError = isFailedTransaction(transaction);
+  const classes = classNames('vet360-profile-field-content', {
+    'usa-input-error': hasError,
+  });
 
-    const method = transactionRequest ? transactionRequest.method : 'PUT';
-    const hasError = isFailedTransaction(transaction);
-    const classes = classNames('vet360-profile-field-content', {
-      'usa-input-error': hasError,
-    });
-
-    return (
-      <div className={classes}>
-        {hasError && <Vet360TransactionInlineErrorMessage {...this.props} />}
-        {isPendingTransaction(transaction) ? (
-          <div id={id}>
-            <Vet360TransactionPending
-              title={title}
-              refreshTransaction={refreshTransaction}
-              method={method}
-            >
-              {/* if this field's modal is open, pass in the children so prevent
+  return (
+    <div className={classes}>
+      {hasError && <Vet360TransactionInlineErrorMessage {...props} />}
+      {isPendingTransaction(transaction) ? (
+        <div id={id}>
+          <Vet360TransactionPending
+            title={title}
+            refreshTransaction={refreshTransaction}
+            method={method}
+          >
+            {/* if this field's modal is open, pass in the children so prevent
                the `Vet360TransactionPending` component from rendering the
                "we're saving your info..." message */}
-              {isModalOpen && children}
-            </Vet360TransactionPending>
-          </div>
-        ) : (
-          children
-        )}
-      </div>
-    );
-  }
+            {isModalOpen && children}
+          </Vet360TransactionPending>
+        </div>
+      ) : (
+        children
+      )}
+    </div>
+  );
 }
+
+Vet360Transaction.propTypes = {
+  children: PropTypes.node.isRequired,
+  refreshTransaction: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  transaction: PropTypes.object,
+};
+
+export default Vet360Transaction;
