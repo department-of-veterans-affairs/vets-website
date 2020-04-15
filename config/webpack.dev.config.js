b878159fc28a261b33525dfb811a6a5c15b61da1
Chris Valarida <chris.valarida@adhocteam.us>
2020-04-15 13:01:02 -0700

    Remove Webpack from the Metalsmith build script (#12149)
    * Rename to build:webpack for consistency
    
    * Rename to build:content
    
    * Consolidated build script
    
    * Change watch command to run the dev server
    
    * Rename build.js -> build-content.js
    
    * Start a consolidated build script
    
    * Only download webpack assets from Metalsmith
    
    This is because it would take more work than it's possibly worth to make
    the downloadAssets script standalone.
    
    * Update the echo in the build script
    
    * Run the yarn commands
    
    * Remove vestigial metalsmith-webpack code
    
    * Move manifest-helpers.js
    
    * Update standalone webpack config
    
    * Delete webpack config
    
    * Rename standalone-webpack.config.js -> webpack.config.js
    
    * Read existing Webpack assets into Metalsmith
    
    * Fix prebuild script
    
    * Refactor getAppManifests
    
    * Preserve Webpack output in the content build
    
    * Fix readAssetsFromDisk plugin
    
    * Remove comment
    
    * Add landing pages
    
    * Only build landing pages during watch task
    
    * Add comment for why writeToDisk needs to be true
    
    * Don't build new landing pages if old ones exist
    
    * Use path.join to find the right path
    
    * Only try to preserve the webpack assets if they exist
    
    * Double-logging looked dumb
    
    * Only read Webpack assets if they're there
    
    * Create a src/platform/landing-pages/ and move the template
    
    * Move headerFooterData into dedicated JSON file
    
    * Remove vestigial parameter to getAppManifests()
    
    * Remove pointless empty object literal
    
    * Import the manifest helpers from the app coverage script
    
    * Use build:webpack in circle
    
    * Overwrite any temporary files that might already be there
    
    * Missed a spot
    
    * Testing console logs to debug Jenkins error
    
    * Remove build directory based on buildtype
    
    * Don't quite args in the build script
    
    This was causing `yarn build` to tack on a `''` at the end of these
    invocations, which made Webpack angry.
    
    * Use the build script for `yarn build`
    
    * Pass CLI args to webpack
    
    * Simplify buildOptions
    
    * Clean imports a bit
    
    * Use JSDoc for getEntryPoints
    
    * Refactor getSettings
    
    * Rename to getDevServerSettings
    
    * Update comment
    
    * Refactor HtmlWebpackPlugin section
    
    * Update JSDoc to reflect what getEntryPoints actually returns
    
    * Watch static content and pages
    
    * Use the buildtype in the temporary storage of webpack assets
    
    * Clean up console logs
    
    * Clean up tmp/
    
    * Don't lie, now!
    
    * Don't use the concurrently shorthand
    
    Turns out this wasn't passing the args as expected. I think.
    
    * Name the processes

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -45,6 +45,8 @@ function generateWebpackDevConfig(buildOptions) {
       modules: false,
       warnings: true,
     },
+    // Needed to write the landing pages to disk so webpack-dev-server will actually serve them
+    writeToDisk: true,
     before: app => {
       // We're doing this because some of the pages
       // that we are redirecting end with asp and we want
