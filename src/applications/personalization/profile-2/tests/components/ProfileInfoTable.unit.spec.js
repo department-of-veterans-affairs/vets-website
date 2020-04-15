ea05a8b14fe316189c2a1a92bdc4c26922dc7269
Erik P. Hansen <eph@erikphansen.com>
2020-04-15 12:10:31 -0700

    Add support for title-less ProfileInfoTable

--- src/applications/personalization/profile-2/tests/components/ProfileInfoTable.unit.spec.js
+++ src/applications/personalization/profile-2/tests/components/ProfileInfoTable.unit.spec.js
@@ -7,18 +7,19 @@ import ProfileInfoTable from '../../components/ProfileInfoTable';
 
 describe('ProfileInfoTable', () => {
   let dataTransformerSpy;
-  const props = {
-    title: 'Table Title',
-    fieldName: 'profileField',
-    data: [
-      { title: 'row 1', value: 'value 1' },
-      { title: 'row 2', value: 'value 2' },
-    ],
-  };
+  let props;
   let wrapper;
   beforeEach(() => {
     dataTransformerSpy = sinon.spy(arg => arg);
-    props.dataTransformer = dataTransformerSpy;
+    props = {
+      title: 'Table Title',
+      fieldName: 'profileField',
+      data: [
+        { title: 'row 1', value: 'value 1' },
+        { title: 'row 2', value: 'value 2' },
+      ],
+      dataTransformer: dataTransformerSpy,
+    };
     wrapper = shallow(<ProfileInfoTable {...props} />);
   });
   afterEach(() => {
@@ -34,6 +35,9 @@ describe('ProfileInfoTable', () => {
     const h3 = wrapper.find('h3');
     expect(h3.text()).to.equal(props.title);
   });
+  it('should render an h3 tag as the first child element', () => {
+    expect(wrapper.childAt(0).type()).to.equal('h3');
+  });
   it('renders a table row div for each entry in the data prop', () => {
     const tableRows = wrapper.find('div > div.table-row');
     expect(tableRows.length).to.equal(props.data.length);
@@ -47,6 +51,30 @@ describe('ProfileInfoTable', () => {
       const { title, value } = props.data[index];
       expect(row.text().includes(title)).to.be.true;
       expect(row.text().includes(value)).to.be.true;
+    });
+  });
+  describe('when no title is set', () => {
+    beforeEach(() => {
+      props = {
+        fieldName: 'profileField',
+        data: [
+          { title: 'row 1', value: 'value 1' },
+          { title: 'row 2', value: 'value 2' },
+        ],
+      };
+      wrapper = shallow(<ProfileInfoTable {...props} />);
+    });
+    afterEach(() => {
+      wrapper.unmount();
+    });
+    it('should not render an h3 tag', () => {
+      const h3 = wrapper.find('h3');
+      expect(h3.length).to.equal(0);
+    });
+    it('should render a table-row div as its first child', () => {
+      const firstChild = wrapper.childAt(0);
+      expect(firstChild.type()).to.equal('div');
+      expect(firstChild.hasClass('table-row')).to.be.true;
     });
   });
 });
