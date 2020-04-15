7adfc545c6b843b26a1d448a977d366381df4911
Kelson Adams <kelsonic@users.noreply.github.com>
2020-04-15 09:45:00 -0600

    Kelsonic 7979 yr css tweaks (#12222)
    * Add serif font to first data point on search result card
    
    * Show chevron up when showing mobile form
    
    * Simplify content styling

--- src/applications/yellow-ribbon/components/SearchResult/index.jsx
+++ src/applications/yellow-ribbon/components/SearchResult/index.jsx
@@ -110,7 +110,7 @@ const deriveDivisionProfessionalSchool = (school = {}) => {
 export const SearchResult = ({ school }) => (
   <li className="usa-unstyled-list vads-l-col vads-u-margin-bottom--2 vads-u-padding-x--2 vads-u-padding-y--2 vads-u-background-color--gray-light-alt">
     {/* School Name */}
-    <p className="vads-u-font-size--h3 vads-u-font-weight--bold vads-u-margin--0">
+    <p className="vads-u-font-size--h3 vads-u-font-weight--bold vads-u-font-family--serif vads-u-margin--0">
       <span className="sr-only">School name</span>
       {deriveNameLabel(school)}
     </p>
