5685b3e5ab69069713fee42702e611afbf16c6ad
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-04-16 13:44:33 -0700

    Adjusted capitalization

--- src/applications/disability-benefits/686c-674/config/chapters/taskWizard/wizard/wizard.js
+++ src/applications/disability-benefits/686c-674/config/chapters/taskWizard/wizard/wizard.js
@@ -13,7 +13,7 @@ export const schema = {
         reportDeath: { type: 'boolean', default: false },
         reportStepchildNotInHousehold: { type: 'boolean', default: false },
         reportMarriageOfChildUnder18: { type: 'boolean', default: false },
-        reportChild18orOlderIsNotAttendingSchool: {
+        reportChild18OrOlderIsNotAttendingSchool: {
           type: 'boolean',
           default: false,
         },
