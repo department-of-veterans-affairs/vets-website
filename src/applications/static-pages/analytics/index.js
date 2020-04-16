c9382e4695a3a0e6832de2984d8a9a96a19ab3a4
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-04-16 16:05:23 -0400

    Add homepage banner events

--- src/applications/static-pages/analytics/index.js
+++ src/applications/static-pages/analytics/index.js
@@ -1,15 +1,19 @@
 import addJumplinkListeners from './addJumpLinkListeners';
 import addQaSectionListeners from './addQaSectionListeners';
 import addTeaserListeners from './addTeaserListeners';
+import addHomepageBannerListeners from './addHomepageBannerListeners';
 
+const homepageUrl = '/';
 const coronavirusFaqUrl = '/coronavirus-veteran-frequently-asked-questions/';
-const isCoronavirusFaq = document.location.pathname.startsWith(
-  coronavirusFaqUrl,
-);
+
+const isCoronavirusFaq = document.location.pathname === coronavirusFaqUrl;
+const isHomepage = document.location.pathname === homepageUrl;
 
 if (isCoronavirusFaq) {
   addJumplinkListeners();
   addQaSectionListeners();
+} else if (isHomepage) {
+  addHomepageBannerListeners();
 }
 
 addTeaserListeners();
