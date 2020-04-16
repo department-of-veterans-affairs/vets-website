9b44f1aaade38a90069cf6162c71678cac3749ca
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-04-16 16:21:33 -0400

    Reduce to a map

--- src/applications/static-pages/analytics/index.js
+++ src/applications/static-pages/analytics/index.js
@@ -3,17 +3,26 @@ import addQaSectionListeners from './addQaSectionListeners';
 import addTeaserListeners from './addTeaserListeners';
 import addHomepageBannerListeners from './addHomepageBannerListeners';
 
-const homepageUrl = '/';
-const coronavirusFaqUrl = '/coronavirus-veteran-frequently-asked-questions/';
+const pageListenersMap = new Map();
 
-const isCoronavirusFaq = document.location.pathname === coronavirusFaqUrl;
-const isHomepage = document.location.pathname === homepageUrl;
+pageListenersMap.set('/coronavirus-veteran-frequently-asked-questions/', [
+  addJumplinkListeners,
+  addQaSectionListeners,
+]);
 
-if (isCoronavirusFaq) {
-  addJumplinkListeners();
-  addQaSectionListeners();
-} else if (isHomepage) {
-  addHomepageBannerListeners();
+pageListenersMap.set('/', [addHomepageBannerListeners]);
+
+const specialListeners = pageListenersMap.get(document.location.pathname);
+
+if (specialListeners) {
+  specialListeners.forEach(f => f());
 }
 
+// Global listeners
 addTeaserListeners();
+
+// Prevent the window from navigating away.
+// Useful to verifying events when links are clicked.
+// window.onbeforeunload = function() {
+//   return ''
+// }
