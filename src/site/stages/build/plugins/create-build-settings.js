3742663021ac11b0e6a312b5390b1cf58f5c04fe
Brooks Johnson <brooks.johnson@pm.me>
2020-04-16 14:30:53 -0700

    Inline a function

--- src/site/stages/build/plugins/create-build-settings.js
+++ src/site/stages/build/plugins/create-build-settings.js
@@ -1,18 +1,6 @@
 /* eslint-disable no-continue, no-param-reassign */
 
 /**
- * The result of this function will become globally available under window.settings in a totally static file at /js/settings.js.
- * Post-build, it can be edited in {build_directory}/js/settings.js.
- * @param {object} options
- */
-function getBuildSettings(options) {
-  return {
-    type: options.buildtype,
-    applications: {},
-  };
-}
-
-/**
  * Writes a human-readable JavaScript file containing build properties available globally under `window.settings`.
  * @param {object} options The build options as passed to the build script and processed through Metalsmith.
  */
@@ -20,7 +8,10 @@ function createBuildSettings(options) {
   const settingsPath = 'js/settings.js';
 
   return (files, metalsmith, done) => {
-    const settings = getBuildSettings(options);
+    const settings = {
+      type: options.buildtype,
+      applications: {},
+    };
 
     for (const file of Object.values(files)) {
       const { entryname: entryName } = file;
