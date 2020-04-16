51c7edd59b47204a3998f97a82a6c4327fa9d0d1
Christopher Valarida <chris.valarida@adhocteam.us>
2020-04-16 14:46:39 -0700

    Rebuild if files are added or removed

--- script/build-content.js
+++ script/build-content.js
@@ -6,21 +6,23 @@ const build = require('../src/site/stages/build');
 async function buildContent() {
   const buildOptions = await getOptions();
 
+  const rebuild = (event, path) => {
+    // eslint-disable-next-line no-console
+    console.log(
+      `Rebuilding content. Reason: ${chalk.green(event)} ${chalk.blue(path)}`,
+    );
+    build(buildOptions);
+  };
+
   if (buildOptions.watch) {
     chokidar
       .watch(buildOptions.watchPaths)
       .on('ready', () => {
         build(buildOptions);
       })
-      .on('change', (event, path) => {
-        // eslint-disable-next-line no-console
-        console.log(
-          `Rebuilding content. Reason: ${chalk.green(event)} ${chalk.blue(
-            path,
-          )}`,
-        );
-        build(buildOptions);
-      });
+      .on('add', rebuild)
+      .on('change', rebuild)
+      .on('unlink', rebuild);
   } else {
     build(buildOptions);
   }
