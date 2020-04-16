933085cc2b57838744957e21202fb6af234c2ba1
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-04-16 14:05:25 -0400

    Add faq links

--- src/applications/static-pages/analytics.js
+++ src/applications/static-pages/analytics.js
@@ -1,33 +1,72 @@
 import recordEvent from 'platform/monitoring/record-event';
 
-const ANCHOR_ELEMENT = 'A';
-
-const SELECTORS = {
-  QA_SECTION: '[data-template="paragraphs/q_a_section"]',
-  LIST_OF_LINK_TEASERS: '[data-template="paragraphs/list_of_link_teasers"]',
-  NAV_JUMPLINK_CLICK: '[data-template="paragraphs/wysiwyg"] #on-this-page',
-};
+function isAnchor(node) {
+  return node?.nodeName === 'A';
+}
 
 function addQaSectionListeners() {
+  const selectors = {
+    template: '[data-template="paragraphs/q_a_section"]',
+    faq: '[data-template="paragraphs/q_a"]',
+  };
 
+  const faqs = document.querySelectorAll(selectors.faq);
+
+  // Cycle through each FAQ, binding the FAQ data to each anchor
+  // tag generated from the WYSIWYG content.
+  [...faqs].forEach(faq => {
+    const anchors = faq.querySelectorAll('a');
+
+    [...anchors].forEach(anchor => {
+      anchor.dataset.faqSection = faq.dataset.analyticsFaqSection; // eslint-disable-line no-param-reassign
+      anchor.dataset.faqText = faq.dataset.analyticsFaqText; // eslint-disable-line no-param-reassign
+    });
+  });
+
+  const containers = document.querySelectorAll(selectors.template);
+
+  [...containers].forEach(container => {
+    container.addEventListener('click', event => {
+      if (!isAnchor(event.target)) {
+        return;
+      }
+
+      const analytic = {
+        event: 'nav-covid-link-click',
+        faqText: event.target.dataset.faqText,
+        faqSection: event.target.dataset.faqSection,
+      };
+
+      recordEvent(analytic);
+    });
+  });
 }
 
 function addTeaserListeners() {
-
+  const selectors = {
+    template: '[data-template="paragraphs/list_of_link_teasers"]',
+  };
 }
 
 function addJumplinkListeners() {
-  const jumplinkHeading = document.querySelector(SELECTORS.NAV_JUMPLINK_CLICK);
+  const selectors = {
+    heading: '[data-template="paragraphs/wysiwyg"] #on-this-page',
+  };
+
+  const jumplinkHeading = document.querySelector(selectors.heading);
   const container = jumplinkHeading.parentElement;
 
   container.addEventListener('click', event => {
-    if (event.target.nodeName.toUpperCase() === ANCHOR_ELEMENT) {
-      const analytic = {
-        event: 'nav-jumplink-click',
-      };
-      recordEvent(analytic);
+    if (!isAnchor(event.target)) {
+      return;
     }
-  })
+
+    const analytic = {
+      event: 'nav-jumplink-click',
+    };
+
+    recordEvent(analytic);
+  });
 }
 
 const coronavirusFaqUrl = '/coronavirus-veteran-frequently-asked-questions/';
@@ -35,4 +74,5 @@ const isCoronavirusFaq = document.location.pathname === coronavirusFaqUrl;
 
 if (isCoronavirusFaq) {
   addJumplinkListeners();
+  addQaSectionListeners();
 }
