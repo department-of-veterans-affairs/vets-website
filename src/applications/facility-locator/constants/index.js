28a3a811dca2834cf68d133a18242adb05b6c6f9
Gibran <brangi@users.noreply.github.com>
2020-04-16 11:15:06 -0500

    Facility Locator does not load in IE11 (#12243)
    replace spread operator with split
    
    Co-authored-by: Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>

--- src/applications/facility-locator/constants/index.js
+++ src/applications/facility-locator/constants/index.js
@@ -60,6 +60,4 @@ export const BOUNDING_RADIUS = 0.75;
 /**
  *Defines the marker letter list
  */
-export const MARKER_LETTERS = new Set([
-  ...'abcdefghijklmnopqrstuvwxyz'.toUpperCase(),
-]);
+export const MARKER_LETTERS = new Set('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''));
