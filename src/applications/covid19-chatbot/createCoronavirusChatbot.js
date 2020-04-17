16ffbd13b8b0ace0b78d945ff0d95a090ae2dfe9
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-04-17 10:45:40 -0400

    Wip chatbot widget 2 (#12258)
    * Install botframework-webchat
    
    * Separate out and uncomment code
    
    * Update createCoronavirusChatbot.js
    
    * Update createCoronavirusChatbot.js
    
    * Fix for root

---
+++ src/applications/covid19-chatbot/createCoronavirusChatbot.js
@@ -0,0 +1,15 @@
+export default (_store, widgetType) => {
+  // Derive the element to render our widget.
+  const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
+
+  // Escape early if no element was found.
+  if (!root) {
+    return;
+  }
+
+  // webpackChunkName: "chatbot"
+  import('./index').then(module => {
+    const initializeChatbot = module.default;
+    initializeChatbot(root);
+  });
+};
