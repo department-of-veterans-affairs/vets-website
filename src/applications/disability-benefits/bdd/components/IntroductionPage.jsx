7a29b9c14c5096e7a74f9e8241d663b792c0760a
Nick Sprinkle <53942725+ndsprinkle@users.noreply.github.com>
2020-04-22 07:45:17 -0400

    BDD: Introduction page and form.js updates (#12284)
    * Update BDD form.js file to more closely match 526 form.js file
    
    * Update BDD Introduction page to match design

--- src/applications/disability-benefits/bdd/components/IntroductionPage.jsx
+++ src/applications/disability-benefits/bdd/components/IntroductionPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import { focusElement } from '../../../../platform/utilities/ui';
 import OMBInfo from '@department-of-veterans-affairs/formation-react/OMBInfo';
 import FormTitle from 'platform/forms-system/src/js/components/FormTitle';
 import SaveInProgressIntro from '../../../../platform/forms/save-in-progress/SaveInProgressIntro';
+
+import { BDD_INFO_URL } from '../../constants';
 
 class IntroductionPage extends React.Component {
   componentDidMount() {
@@ -13,58 +17,112 @@ class IntroductionPage extends React.Component {
   render() {
     return (
       <div className="schemaform-intro">
-        <FormTitle title="21-526EZ Benefits Delivery at Discharge" />
+        <FormTitle title="File for Benefits Delivery at Discharge" />
         <p>
-          Equal to VA Form 21-526EZ (21-526EZ Benefits Delivery at Discharge).
+          Service members who have an illness or injury that was caused - or
+          made worse - by their active duty service, can file a claim for
+          disability benefits 180 to 90 days before they leave the military
+          through the Benefits Delivery at Discharge (BDD) program.
+        </p>
+        <p>
+          <a href={BDD_INFO_URL}>
+            Learn more about Benefits Delivery at Discharge (BDD)
+          </a>
         </p>
         <SaveInProgressIntro
+          hideUnauthedStartLink
           prefillEnabled={this.props.route.formConfig.prefillEnabled}
-          messages={this.props.route.formConfig.savedFormMessages}
+          formId={this.props.formId}
           pageList={this.props.route.pageList}
-          startText="Start the Application"
-        >
-          Please complete the 21-526EZ form to apply for benefits delivery at
-          discharge.
-        </SaveInProgressIntro>
-        <h4>
-          Follow the steps below to apply for benefits delivery at discharge.
-        </h4>
+          startText="Start a Benefits Delivery at Discharge claim"
+          retentionPeriod="1 year"
+          downtime={this.props.route.formConfig.downtime}
+        />
+        <h4>Follow the steps below to file a BDD claim.</h4>
         <div className="process schemaform-process">
           <ol>
             <li className="process-step list-one">
-              <h5>Prepare</h5>
-              <h6>To fill out this application, you’ll need your:</h6>
+              <div>
+                <h5>Prepare</h5>
+              </div>
+              <div>
+                <h6>
+                  When you file a disability claim, you’ll have a chance to
+                  provide evidence to support your claim. Evidence could
+                  include:
+                </h6>
+              </div>
               <ul>
-                <li>Social Security number (required)</li>
+                <li>
+                  VA medical records and hospital records that relate to your
+                  claimed condition or that show your rated disability has
+                  gotten worse
+                </li>
+                <li>
+                  Private medical records and hospital reports that relate to
+                  your claimed condition or that show your disability has gotten
+                  worse
+                </li>
+                <li>
+                  Supporting statements from family, friends, coworkers, clergy,
+                  or law enforcement personnel with knowledge about how and when
+                  your disability happened or how it got worse
+                </li>
               </ul>
               <p>
-                <strong>What if I need help filling out my application?</strong>{' '}
+                If you need help filing a disability claim, you can contact a VA
+                regional office and ask to speak to a counselor. To find the
+                nearest regional office, please call{' '}
+                <a href="tel:18008271000">800-827-1000</a>.
+              </p>
+              <p>
                 An accredited representative, like a Veterans Service Officer
                 (VSO), can help you fill out your claim.{' '}
-                <a href="/disability-benefits/apply/help/index.html">
+              </p>
+              <p>
+                <a href="/disability/get-help-filing-claim/">
                   Get help filing your claim
                 </a>
                 .
               </p>
             </li>
             <li className="process-step list-two">
-              <h5>Apply</h5>
-              <p>Complete this benefits delivery at discharge form.</p>
+              <div>
+                <h5>Apply</h5>
+              </div>
+              <div>
+                <h6>
+                  Complete the Benefits Delivery at Discharge form. These are
+                  the steps you can expect:
+                </h6>
+              </div>
+              <ul>
+                <li>Provide your Service Member information</li>
+                <li>Provide your military history</li>
+                <li>
+                  Describe the conditions you are submitting (a) claim(s) for
+                </li>
+              </ul>
               <p>
-                After submitting the form, you’ll get a confirmation message.
+                After submitting the form, you'll get a confirmation message.
                 You can print this for your records.
               </p>
             </li>
             <li className="process-step list-three">
-              <h5>VA Review</h5>
+              <div>
+                <h5>VA Review</h5>
+              </div>
               <p>
-                We process claims within a week. If more than a week has passed
-                since you submitted your application and you haven’t heard back,
-                please don’t apply again. Call us at.
+                We process applications in the order we receive them. The amount
+                of time it takes to process your claim depends on how many
+                injuries or disabilities you claim and how long it takes us to
+                gather evidence needed to decide your claim.
               </p>
             </li>
             <li className="process-step list-four">
-              <h5>Decision</h5>
+              <div>
+                <h5>Decision</h5>
+              </div>
               <p>
                 Once we’ve processed your claim, you’ll get a notice in the mail
                 with our decision.
@@ -73,17 +131,36 @@ class IntroductionPage extends React.Component {
           </ol>
         </div>
         <SaveInProgressIntro
+          hideUnauthedStartLink
           buttonOnly
-          messages={this.props.route.formConfig.savedFormMessages}
+          prefillEnabled={this.props.route.formConfig.prefillEnabled}
+          formId={this.props.formId}
           pageList={this.props.route.pageList}
-          startText="Start the Application"
+          startText="Start a Benefits Delivery at Discharge claim"
+          downtime={this.props.route.formConfig.downtime}
         />
-        <div className="omb-info--container" style={{ paddingLeft: '0px' }}>
-          <OMBInfo resBurden={15} ombNumber="2900-0747" expDate="3/31/2021" />
+        <div className="omb-info--container">
+          <OMBInfo resBurden={15} ombNumber="2900-0747" expDate="03/31/2021" />
         </div>
       </div>
     );
   }
 }
 
-export default IntroductionPage;
+function mapStateToProps(state) {
+  return { formId: state.form.formId };
+}
+
+IntroductionPage.propTypes = {
+  formId: PropTypes.string.isRequired,
+  route: PropTypes.shape({
+    formConfig: PropTypes.shape({
+      prefillEnabled: PropTypes.bool,
+    }),
+    pageList: PropTypes.array.isRequired,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps)(IntroductionPage);
+
+export { IntroductionPage };
