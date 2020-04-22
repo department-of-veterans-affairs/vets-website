e2430d379e98d7d0cffd9b0a52da6b1a789da46e
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-04-22 10:41:12 -0400

    Read data from JSON file.

---
+++ src/applications/disability-benefits/all-claims/tests/all-claims.cypress.spec.js
@@ -0,0 +1,84 @@
+import path from 'path';
+
+/*
+const testConfig = {
+  debug: true,
+  setup: userToken => {
+    PageHelpers.initDocumentUploadMock();
+    PageHelpers.initApplicationSubmitMock(userToken);
+    PageHelpers.initItfMock(userToken);
+    PageHelpers.initPaymentInformationMock(userToken);
+  },
+  setupPerTest: ({ testData: data, userToken }) => {
+    // Pre-fill with the expected ratedDisabilities, but nix view:selected since that's not pre-filled
+    const sanitizedRatedDisabilities = (data.ratedDisabilities || []).map(d =>
+      _.omit('view:selected', d),
+    );
+    const sanitizedData =
+      sanitizedRatedDisabilities === []
+        ? data
+        : { ...data, ratedDisabilities: sanitizedRatedDisabilities };
+    PageHelpers.initInProgressMock(userToken, sanitizedData);
+  },
+  url: '/disability/file-disability-claim-form-21-526ez/introduction',
+  logIn: true,
+  testDataPathPrefix: 'data',
+  pageHooks: {
+    '/disability/file-disability-claim-form-21-526ez/introduction': async page => {
+      // Hit the start button
+      await page.click('.usa-button-primary.schemaform-start-button');
+
+      // Click past the ITF message
+      await page.waitFor('.usa-button-primary:not(.schemaform-start-button)');
+      await page.click('.usa-button-primary');
+    },
+    '/disability/file-disability-claim-form-21-526ez/disabilities/rated-disabilities': async (
+      page,
+      data,
+      config,
+      log,
+    ) => {
+      await Promise.all(
+        data.ratedDisabilities.map(async (disability, index) => {
+          if (disability['view:selected']) {
+            log(`Selecting ${disability.name} (index ${index})`);
+            await page.click(`input[name="root_ratedDisabilities_${index}"]`);
+          }
+        }),
+      );
+      await page.click('.form-progress-buttons .usa-button-primary');
+    },
+    '/disability/file-disability-claim-form-21-526ez/payment-information': async (
+      page,
+      data,
+      config,
+      log,
+    ) => {
+      if (data['view:bankAccount']) {
+        if (await page.$('.usa-button-primary.edit-button')) {
+          // Only click edit if new bank info is in the data file
+          await page.click('.usa-button-primary.edit-button');
+        }
+        await formFiller.fillPage(page, data, config, log);
+        await page.click('.usa-button-primary.update-button');
+      }
+      await page.click('button[type=submit].usa-button-primary');
+    },
+  },
+  // TODO: Remove this in favor of importing the formConfig and finding them all
+  arrayPages: [
+    {
+      path: 'new-disabilities/follow-up/:index',
+      arrayPath: 'newDisabilities',
+    },
+  ],
+};
+*/
+
+describe('526 All Claims', () => {
+  it('fills the form', () => {
+    cy.testForm({
+      testDataDir: path.join(__dirname, 'data'),
+    });
+  });
+});
--- src/platform/testing/e2e/cypress/plugins/index.js
+++ src/platform/testing/e2e/cypress/plugins/index.js
@@ -1,1 +1,18 @@
-module.exports = () => {};
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (on /* config */) => {
+  on('task', {
+    getTestData(relativeFilePath) {
+      // The expected file path is relative to the project root.
+      // Convert it to an absolute file path now that we are in Node.js.
+      const absoluteFilePath = path.join(
+        __dirname,
+        '../../../../../..',
+        relativeFilePath,
+      );
+
+      return JSON.parse(fs.readFileSync(absoluteFilePath), 'utf8');
+    },
+  });
+};
