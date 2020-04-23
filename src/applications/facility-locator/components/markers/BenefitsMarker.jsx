2cb22775f95c835cba52f5ac33961b1ea6212f81
Brooks Johnson <brooks.johnson@pm.me>
2020-04-23 10:16:11 -0700

    Convert BenefitsMarker
    This also removes defaultProps in favor of a default argument

--- src/applications/facility-locator/components/markers/BenefitsMarker.jsx
+++ src/applications/facility-locator/components/markers/BenefitsMarker.jsx
@@ -1,26 +1,18 @@
 import DivMarker from './DivMarker';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 
-class BenefitsMarker extends Component {
-  render() {
-    const { position, children, style, onClick } = this.props;
-
-    return (
-      <DivMarker
-        position={position}
-        popupContent={<div>{children}</div>}
-        onClick={onClick}
-      >
-        <div className="benefits-icon map-marker" style={style} />
-      </DivMarker>
-    );
-  }
+function BenefitsMarker({ position, style, onClick = () => {}, children }) {
+  return (
+    <DivMarker
+      position={position}
+      popupContent={<div>{children}</div>}
+      onClick={onClick}
+    >
+      <div className="benefits-icon map-marker" style={style} />
+    </DivMarker>
+  );
 }
-
-BenefitsMarker.defaultProps = {
-  onClick: () => {},
-};
 
 BenefitsMarker.propTypes = {
   position: PropTypes.array,
