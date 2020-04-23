b349a19267d40a34ca0ec30b7ac37c20b256a918
Brooks Johnson <brooks.johnson@pm.me>
2020-04-22 18:09:35 -0700

    Revert "Convert FileOriginalClaimPage"
    This reverts commit cfc393f6dde91d09e04c26a52a4ea43c2f2d3e8c.

--- src/applications/disability-benefits/wizard/pages/file-original-claim.jsx
+++ src/applications/disability-benefits/wizard/pages/file-original-claim.jsx
@@ -5,33 +5,35 @@ import { pageNames } from './pageList';
 import { isLoggedIn as isLoggedInSelector } from 'platform/user/selectors';
 import recordEvent from 'platform/monitoring/record-event';
 
-function FileOriginalClaimPage({ isLoggedIn }) {
-  return (
-    <AlertBox
-      status="error"
-      headline="You’ll need to file a claim on eBenefits"
-      content={
-        <>
-          <p>
-            To file your first disability claim, please go to our eBenefits
-            website.
-          </p>
-          <a
-            href="https://www.ebenefits.va.gov/ebenefits/about/feature?feature=disability-compensation"
-            className="usa-button-primary va-button-primary"
-            onClick={() =>
-              isLoggedIn &&
-              recordEvent({
-                event: 'nav-ebenefits-click',
-              })
-            }
-          >
-            Go to eBenefits
-          </a>
-        </>
-      }
-    />
-  );
+class FileOriginalClaimPage extends React.Component {
+  render() {
+    return (
+      <AlertBox
+        status="error"
+        headline="You’ll need to file a claim on eBenefits"
+        content={
+          <>
+            <p>
+              To file your first disability claim, please go to our eBenefits
+              website.
+            </p>
+            <a
+              href="https://www.ebenefits.va.gov/ebenefits/about/feature?feature=disability-compensation"
+              className="usa-button-primary va-button-primary"
+              onClick={() =>
+                this.props.isLoggedIn &&
+                recordEvent({
+                  event: 'nav-ebenefits-click',
+                })
+              }
+            >
+              Go to eBenefits
+            </a>
+          </>
+        }
+      />
+    );
+  }
 }
 
 function mapStateToProps(state) {
