0401a3f34d7c1dcc9379acbd7bb69978e8adb994
Nick Sprinkle <53942725+ndsprinkle@users.noreply.github.com>
2020-04-23 12:09:06 -0400

    BDD: Step 1 (Service Member) (#12312)
    * BDD Step 1 (Service Member)
    
    * Replace platform imports with direct path

--- src/applications/disability-benefits/bdd/form-entry.jsx
+++ src/applications/disability-benefits/bdd/form-entry.jsx
@@ -1,7 +1,7 @@
-import '../../../platform/polyfills';
+import 'platform/polyfills';
 import '../all-claims/sass/disability-benefits.scss';
 
-import startApp from '../../../platform/startup';
+import startApp from 'platform/startup';
 
 import routes from './routes';
 import reducer from './reducers';
