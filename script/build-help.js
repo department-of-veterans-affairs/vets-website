8e0f32971eac63231c41e6e383162e538ac673ed
Christopher Valarida <chris.valarida@adhocteam.us>
2020-04-24 08:26:34 -0700

    Clarify some things and add another pro tip

--- script/build-help.js
+++ script/build-help.js
@@ -9,35 +9,36 @@ const helpSections = [
         name: 'env.entry',
         typeLabel: '{underline string}',
         description:
-          'Build only a subset of applications. Uses a comma-delimited string. Entry names can be found in the manifest.json files.',
+          'Build only comma-delimited a subset of applications. Entry names can be found in the manifest.json files.\nE.g. {bold static-pages,auth}',
       },
       {
         name: 'env.buildtype',
         typeLabel: '{underline string}',
         description:
-          'Use a specific buildtype. Options include localhost (default), vagovdev, vagovstaging, vagovprod.',
+          'Use a specific buildtype.\nOptions include {bold localhost} (default), {bold vagovdev}, {bold vagovstaging}, {bold vagovprod}.',
       },
       {
         name: 'env.api',
         typeLabel: '{underline string}',
         description:
-          'Point vets-website to a running api. E.g. https://dev-api.va.gov. Defaults to http://localhost:3000.',
+          'Point vets-website to a running api.\nE.g. {bold https://dev-api.va.gov}. Defaults to {bold http://localhost:3000}.',
       },
       {
         name: 'env.host',
         typeLabel: '{underline IP}',
-        description: 'Specify a different host.',
+        description:
+          'Specify a different host. Use with --env.public to serve the website to your local network.\nE.g. {bold 0.0.0.0}',
       },
       {
         name: 'env.port',
         typeLabel: '{underline number}',
-        description: 'Run on a specific port. Defaults to 3001.',
+        description: 'Run on a specific port. Defaults to {bold 3001}.',
       },
       {
         name: 'env.public',
         typeLabel: '{underline IP}',
         description:
-          "Serve the website to your local network by setting this to your machine's IP address. E.g. 192.168.0.111",
+          "Serve the website to your local network by setting this to your machine's IP address.\nE.g. {bold 192.168.0.111}\nUse with --env.host 0.0.0.0",
       },
     ],
   },
@@ -48,6 +49,10 @@ const helpSections = [
         desc:
           "1. Build the static HTML pages once before running the webpack-dev-server to see your application's landing page as it would appear on VA.gov.",
         example: '$ yarn build:content',
+      },
+      {
+        desc: '2. Serve the website to devices on your local network',
+        example: '$ yarn watch --env.host 0.0.0.0 --env.public 192.168.0.111',
       },
     ],
   },
