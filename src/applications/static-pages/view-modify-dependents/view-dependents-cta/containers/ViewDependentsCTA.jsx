374a942b2a6278a5e7ce04897a6f7fb04a29b8c8
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-04-27 16:05:54 -0700

    Removed unneeded component and refactored

--- src/applications/static-pages/view-modify-dependents/view-dependents-cta/containers/ViewDependentsCTA.jsx
+++ src/applications/static-pages/view-modify-dependents/view-dependents-cta/containers/ViewDependentsCTA.jsx
@@ -1,48 +1,35 @@
-import React, { Component } from 'react';
-import { toggleLoginModal } from 'platform/site-wide/user-nav/actions';
+import React from 'react';
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
 import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
 import { connect } from 'react-redux';
 
 import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
 
-class ViewDependentsCTA extends Component {
-  toggleModal = () => {
-    this.props.toggleLoginModal(true);
-  };
-
-  render() {
-    let content;
-    if (this.props.includedInFlipper === undefined) {
-      content = <LoadingIndicator message="Loading..." />;
-    } else if (this.props.includedInFlipper === false) {
-      content = (
-        <a
-          className="usa-button-primary va-button-primary"
-          href="https://www.ebenefits.va.gov/ebenefits/about/feature?feature=dependent-compensation"
-        >
-          Go to eBenefits to add or modify a dependent
-        </a>
-      );
-    } else {
-      content = (
-        <a
-          href="/disability/view-dependents/"
-          className="usa-button-primary va-button-primary"
-        >
-          View your dependents
-        </a>
-      );
-    }
-    return <div>{content}</div>;
+const ViewDependentsCTA = props => {
+  let content;
+  if (props.includedInFlipper === undefined) {
+    content = <LoadingIndicator message="Loading..." />;
+  } else if (props.includedInFlipper === false) {
+    content = (
+      <a
+        className="usa-button-primary va-button-primary"
+        href="https://www.ebenefits.va.gov/ebenefits/about/feature?feature=dependent-compensation"
+      >
+        Go to eBenefits to add or modify a dependent
+      </a>
+    );
+  } else {
+    content = (
+      <a
+        href="/disability/view-dependents/"
+        className="usa-button-primary va-button-primary"
+      >
+        View your dependents
+      </a>
+    );
   }
-}
-
-const mapDispatchToProps = dispatch => ({
-  toggleLoginModal: update => {
-    dispatch(toggleLoginModal(update));
-  },
-});
+  return <div>{content}</div>;
+};
 
 const mapStateToProps = store => ({
   user: store.user,
@@ -51,7 +38,4 @@ const mapStateToProps = store => ({
   ],
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(ViewDependentsCTA);
+export default connect(mapStateToProps)(ViewDependentsCTA);
