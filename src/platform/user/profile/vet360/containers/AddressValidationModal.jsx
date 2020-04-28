84b6bea13dec9fd13ea85d9c9bb1681ce01834ac
Sandra Hallie <sandra.adamshallie@gmail.com>
2020-04-28 11:50:16 -0600

    Update address in address validation modal

--- src/platform/user/profile/vet360/containers/AddressValidationModal.jsx
+++ src/platform/user/profile/vet360/containers/AddressValidationModal.jsx
@@ -173,18 +173,17 @@ class AddressValidationModal extends React.Component {
         key={id}
         className="vads-u-margin-bottom--1p5 address-validation-container"
       >
-        {isFirstOptionOrEnabled &&
-          hasConfirmedSuggestions && (
-            <input
-              className="address-validation-input"
-              type="radio"
-              id={id}
-              onChange={
-                isFirstOptionOrEnabled && this.onChangeHandler(address, id)
-              }
-              checked={selectedAddressId === id}
-            />
-          )}
+        {isFirstOptionOrEnabled && hasConfirmedSuggestions && (
+          <input
+            className="address-validation-input"
+            type="radio"
+            id={id}
+            onChange={
+              isFirstOptionOrEnabled && this.onChangeHandler(address, id)
+            }
+            checked={selectedAddressId === id}
+          />
+        )}
         <label
           htmlFor={id}
           className="vads-u-margin-top--2 vads-u-display--flex vads-u-align-items--center"
@@ -194,31 +193,24 @@ class AddressValidationModal extends React.Component {
             {addressLine2 && <span>{` ${addressLine2}`}</span>}
             {addressLine3 && <span>{` ${addressLine3}`}</span>}
 
-            {city &&
-              stateCode &&
-              zipCode && <span>{` ${city}, ${stateCode} ${zipCode}`}</span>}
-            {city &&
-              province &&
-              internationalPostalCode && (
-                <span>
-                  {`${city}, ${province}, ${internationalPostalCode}`}
-                </span>
-              )}
+            {city && stateCode && zipCode && (
+              <span>{` ${city}, ${stateCode} ${zipCode}`}</span>
+            )}
+            {city && province && internationalPostalCode && (
+              <span>{` ${city}, ${province}, ${internationalPostalCode}`}</span>
+            )}
             {/* State/Province/Region is not required with international addresses */}
-            {city &&
-              !province &&
-              internationalPostalCode && (
-                <span>{` ${city}, ${internationalPostalCode}`}</span>
-              )}
+            {city && !province && internationalPostalCode && (
+              <span>{` ${city}, ${internationalPostalCode}`}</span>
+            )}
 
             {displayCountryName && <span>{displayCountryName}</span>}
 
-            {isAddressFromUser &&
-              showEditLink && (
-                <button className="va-button-link" onClick={this.onEditClick}>
-                  Edit Address
-                </button>
-              )}
+            {isAddressFromUser && showEditLink && (
+              <button className="va-button-link" onClick={this.onEditClick}>
+                Edit Address
+              </button>
+            )}
           </div>
         </label>
       </div>
