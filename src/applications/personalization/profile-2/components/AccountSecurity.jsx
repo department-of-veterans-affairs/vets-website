c5f64d26c008d3e086e4843cdf690ef8c2d01538
Erik P. Hansen <eph@erikphansen.com>
2020-04-28 15:44:57 -0700

    refactor to React.Class
    for the sake of unit tests

--- src/applications/personalization/profile-2/components/AccountSecurity.jsx
+++ src/applications/personalization/profile-2/components/AccountSecurity.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import DowntimeNotification, {
   externalServices,
 } from 'platform/monitoring/DowntimeNotification';
@@ -6,27 +6,34 @@ import { focusElement } from 'platform/utilities/ui';
 
 import AccountSecurityContent from './AccountSecurityContent';
 
-const AccountSecurity = () => {
-  useEffect(() => {
+// using a class instead of functional component + useEffect hook since we would
+// have to do a full mount (vs shallow mount) of the component for tests to
+// work, and that would mount the connected DowntimeNotification component,
+// which would require mocking the redux store and honestly seems like more
+// trouble than it's worth
+class AccountSecurity extends Component {
+  componentDidMount() {
     focusElement('h2.profile-section-title');
-  }, []);
+  }
 
-  return (
-    <>
-      <h2
-        tabIndex="-1"
-        className="vads-u-margin-y--2 medium-screen:vads-u-margin-y--4 profile-section-title"
-      >
-        Account security
-      </h2>
-      <DowntimeNotification
-        appTitle="Account Security"
-        dependencies={[externalServices.emis, externalServices.mvi]}
-      >
-        <AccountSecurityContent />
-      </DowntimeNotification>
-    </>
-  );
-};
+  render() {
+    return (
+      <>
+        <h2
+          tabIndex="-1"
+          className="vads-u-margin-y--2 medium-screen:vads-u-margin-y--4 profile-section-title"
+        >
+          Account security
+        </h2>
+        <DowntimeNotification
+          appTitle="Account Security"
+          dependencies={[externalServices.emis, externalServices.mvi]}
+        >
+          <AccountSecurityContent />
+        </DowntimeNotification>
+      </>
+    );
+  }
+}
 
 export default AccountSecurity;
