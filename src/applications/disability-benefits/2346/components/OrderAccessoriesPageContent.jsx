de1cb954a212b876d1f343948ffe33cccc62c608
Mahariel Rosario <csdesigner@techie.com>
2020-04-28 12:31:37 -0400

    MDOT Order Accessories Page: State Setup and "Can't Reorder Batteries Until" Next Availability Date UI Screen (#12388)
    * set up accessories page to meet design specs
    
    * refactor ternary
    
    * update vets-json-schema link
    
    Co-authored-by: mr0sari0 <mahariel.rosario@gmail.com>

--- src/applications/disability-benefits/2346/components/OrderAccessoriesPageContent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const OrderAccessoriesPageContent = () => (
-  <p>
-    Do you need to order accessories for any of your hearing aids?{' '}
-    <span className="vads-u-color--secondary-darkest">(*Required)</span>
-  </p>
-);
-export default OrderAccessoriesPageContent;
