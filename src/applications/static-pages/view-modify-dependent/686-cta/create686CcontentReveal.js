333f01290e67978965ebdb5fb1c7fe900b7e7269
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-04-30 14:46:55 -0700

    updated webpackChunkName

--- src/applications/static-pages/view-modify-dependent/686-cta/create686CcontentReveal.js
+++ src/applications/static-pages/view-modify-dependent/686-cta/create686CcontentReveal.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 export default function create686ContentReveal(store, widgetType) {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   if (root) {
-    import(/* webpackChunkName: "HomepageBanner" */
+    import(/* webpackChunkName: "Form686ContentReveal" */
     './containers/Form686ContentReveal.jsx').then(module => {
       const Form686ContentReveal = module.default;
       ReactDOM.render(<Form686ContentReveal store={store} />, root);
