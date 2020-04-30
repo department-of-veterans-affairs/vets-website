8c950790ea248c3383222b486586a83f6ff54376
Erik P. Hansen <eph@erikphansen.com>
2020-04-30 10:51:15 -0700

    add some Profile 2.0 tests (#12430)
    * add tests for profile 2 actions
    
    * fix sass-lint error
    
    * add tests for profile 2 reducer
    
    * remove aria-hidden attr
    
    this shouldn't be needed as the entire menu will be set to display: none when it's hidden
    
    * prefix actions
    
    * create and use selectors
    
    * fix bug with focusTriggerButton
    
    previously if focusTriggerButton was true and different prop changed, it was possible for the focus to get set on the trigger button again erroneously

--- src/applications/personalization/profile-2/selectors.js
+++ src/applications/personalization/profile-2/selectors.js
@@ -1,5 +1,14 @@
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
 import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
 
-export const showProfile2 = state =>
+const selectProfileUi = state => state.profileUi || {};
+
+export const selectIsSideNavOpen = state =>
+  selectProfileUi(state).isSideNavOpen;
+export const selectIsMenuTriggerPinned = state =>
+  selectProfileUi(state).isMenuTriggerPinned;
+export const selectFocusTriggerButton = state =>
+  selectProfileUi(state).focusTriggerButton;
+
+export const selectShowProfile2 = state =>
   toggleValues(state)[FEATURE_FLAG_NAMES.profileShowProfile2];
