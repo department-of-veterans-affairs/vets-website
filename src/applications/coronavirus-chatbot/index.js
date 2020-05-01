2d61f2c0f0b5ff3622c2cb00f9ddaf7c15b2f826
Shaun Hubbard <shaun.hubbard@thoughtworks.com>
2020-04-30 22:54:55 -0500

    [department-of-veterans-affairs/covid19-chatbot#178][Shaun] fix for additional-lint on ci

--- src/applications/coronavirus-chatbot/index.js
+++ src/applications/coronavirus-chatbot/index.js
@@ -1,5 +1,5 @@
-import { apiRequest } from '../../platform/utilities/api';
-import recordEvent from '../../platform/monitoring/record-event';
+import { apiRequest } from 'platform/utilities/api';
+import recordEvent from 'platform/monitoring/record-event';
 import { GA_PREFIX, addEventListenerToButtons } from './utils';
 import * as Sentry from '@sentry/browser';
 
