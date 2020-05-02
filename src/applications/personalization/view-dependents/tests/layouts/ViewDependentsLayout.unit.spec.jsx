26cef0402142af78aef621cc887331cd005bb066
Eric Buckley <eric.buckley@gmail.com>
2020-05-01 19:32:10 -0700

    adding Providers to the tests

--- src/applications/personalization/view-dependents/tests/layouts/ViewDependentsLayout.unit.spec.jsx
+++ src/applications/personalization/view-dependents/tests/layouts/ViewDependentsLayout.unit.spec.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { mount } from 'enzyme';
 import { expect } from 'chai';
 
+import createCommonStore from 'platform/startup/store';
+
 import ViewDependentsLayout from '../../layouts/ViewDependentsLayout';
+
+const defaultStore = createCommonStore();
 
 describe('<ViewDependentsLayout />', () => {
   const mockState = {
@@ -32,12 +37,14 @@ describe('<ViewDependentsLayout />', () => {
 
   it('should render', () => {
     const wrapper = mount(
-      <ViewDependentsLayout
-        loading={false}
-        error={false}
-        onAwardDependents={mockState.onAwardDependents}
-        notOnAwardDependents={mockState.notOnAwardDependents}
-      />,
+      <Provider store={defaultStore}>
+        <ViewDependentsLayout
+          loading={false}
+          error={false}
+          onAwardDependents={mockState.onAwardDependents}
+          notOnAwardDependents={mockState.notOnAwardDependents}
+        />
+      </Provider>,
     );
 
     expect(wrapper.find('div.large-screen:vads-u-padding-left--6')).to.exist;
@@ -46,12 +53,14 @@ describe('<ViewDependentsLayout />', () => {
 
   it('should show an info alert when there are no dependents', () => {
     const wrapper = mount(
-      <ViewDependentsLayout
-        loading={false}
-        error={false}
-        onAwardDependents={null}
-        notOnAwardDependents={null}
-      />,
+      <Provider store={defaultStore}>
+        <ViewDependentsLayout
+          loading={false}
+          error={false}
+          onAwardDependents={null}
+          notOnAwardDependents={null}
+        />
+      </Provider>,
     );
 
     expect(wrapper.find('div.usa-alert-info')).to.exist;
@@ -60,12 +69,14 @@ describe('<ViewDependentsLayout />', () => {
 
   it('should show an info alert when there is a 400 error', () => {
     const wrapper = mount(
-      <ViewDependentsLayout
-        loading={false}
-        error={400}
-        onAwardDependents={mockState.onAwardDependents}
-        notOnAwardDependents={mockState.notOnAwardDependents}
-      />,
+      <Provider store={defaultStore}>
+        <ViewDependentsLayout
+          loading={false}
+          error={400}
+          onAwardDependents={mockState.onAwardDependents}
+          notOnAwardDependents={mockState.notOnAwardDependents}
+        />
+      </Provider>,
     );
 
     expect(wrapper.find('div.usa-alert-info')).to.exist;
@@ -74,12 +85,14 @@ describe('<ViewDependentsLayout />', () => {
 
   it('should show an error alert when there is a 500 error', () => {
     const wrapper = mount(
-      <ViewDependentsLayout
-        loading={false}
-        error={500}
-        onAwardDependents={mockState.onAwardDependents}
-        notOnAwardDependents={mockState.notOnAwardDependents}
-      />,
+      <Provider store={defaultStore}>
+        <ViewDependentsLayout
+          loading={false}
+          error={500}
+          onAwardDependents={mockState.onAwardDependents}
+          notOnAwardDependents={mockState.notOnAwardDependents}
+        />
+      </Provider>,
     );
 
     expect(wrapper.find('div.usa-alert-error')).to.exist;
