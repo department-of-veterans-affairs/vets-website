eb7861f503944888788b05bf8a129b4887ab404c
Jeff Balboni <jeff@adhocteam.us>
2020-05-04 12:57:10 -0400

    Refactor instructions logic

---
+++ src/applications/vaos/tests/components/AppointmentInstructions.unit.spec.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import AppointmentInstructions from '../../components/AppointmentInstructions';
+
+describe('VAOS <AppointmentInstructions>', () => {
+  it('should return instructions', () => {
+    const tree = shallow(
+      <AppointmentInstructions instructions="Follow-up/Routine: Testing" />,
+    );
+    expect(tree.text()).to.contain('Follow-up/Routine');
+    expect(tree.text()).to.contain('Testing');
+
+    tree.unmount();
+  });
+
+  it('should not return instructions without matching prefix', () => {
+    const tree = shallow(<AppointmentInstructions instructions="test" />);
+    expect(tree.text()).to.be.empty;
+    tree.unmount();
+  });
+
+  it('should not return instructions if empty', () => {
+    const tree = shallow(<AppointmentInstructions instructions="" />);
+    expect(tree.text()).to.be.empty;
+    tree.unmount();
+  });
+});
