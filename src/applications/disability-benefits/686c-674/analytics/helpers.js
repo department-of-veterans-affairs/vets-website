371a37f5a9452eb6a6ee062c988680a599f280ca
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-05-04 13:20:03 -0700

    Updated accessed variables

--- src/applications/disability-benefits/686c-674/analytics/helpers.js
+++ src/applications/disability-benefits/686c-674/analytics/helpers.js
@@ -3,11 +3,11 @@ import recordEvent from 'platform/monitoring/record-event';
 export const fireAnalytics = submission => {
   recordEvent({
     event: 'disability-21-686c-submission-successful',
+    'disability-claimSpouse': submission['view:selectable686Options'].addSpouse,
     'disability-under18AndUnmarried':
-      submission['view:selectable686Options'].reportMarriageOfChildUnder18,
+      submission['view:selectable686Options'].addChild,
     'disability-childAttendingSchool':
-      submission['view:selectable686Options']
-        .reportChild18OrOlderIsNotAttendingSchool,
+      submission['view:selectable686Options'].report674,
     'disability-reportingDivorce':
       submission['view:selectable686Options'].reportDivorce,
     'disability-stepchildLeftHousehold':
@@ -15,8 +15,9 @@ export const fireAnalytics = submission => {
     'disability-deathOfDependent':
       submission['view:selectable686Options'].reportDeath,
     'disability-marriageOfChild':
-      submission['view:selectable686Options'].reportStepchildNotInHousehold,
+      submission['view:selectable686Options'].reportMarriageOfChildUnder18,
     'disability-childStoppedAttendingSchool':
-      submission['view:selectable686Options'].reportStepchildNotInHousehold,
+      submission['view:selectable686Options']
+        .reportChild18OrOlderIsNotAttendingSchool,
   });
 };
