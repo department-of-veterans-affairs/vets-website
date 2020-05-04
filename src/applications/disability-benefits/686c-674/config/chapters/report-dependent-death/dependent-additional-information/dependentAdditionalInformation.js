64e2eeae072bd343af3ae69b289cf1ab2976ca23
Micah Chiang <micahkchiang@gmail.com>
2020-05-04 10:07:00 -0700

    7736 686 ui refactor (#12467)
    * begin ui refactor
    
    * more ui updates
    
    * address refactor.
    
    * refactor
    
    * remove question mark.

--- src/applications/disability-benefits/686c-674/config/chapters/report-dependent-death/dependent-additional-information/dependentAdditionalInformation.js
+++ src/applications/disability-benefits/686c-674/config/chapters/report-dependent-death/dependent-additional-information/dependentAdditionalInformation.js
@@ -3,6 +3,7 @@ import { TASK_KEYS } from '../../../constants';
 import { isChapterFieldRequired } from '../../../helpers';
 import { deceasedDependents } from '../../../utilities';
 import DependentViewField from '../../../../components/DependentViewField';
+import { DependentNameHeader } from './helpers';
 
 export const schema =
   deceasedDependents.properties.dependentAdditionalInformation;
@@ -11,11 +12,12 @@ export const uiSchema = {
   deaths: {
     'ui:options': { viewField: DependentViewField },
     items: {
+      'ui:title': DependentNameHeader,
       date: currentOrPastDateUI('Dependent’s date of death'),
       location: {
         'ui:title': 'Place of death',
         city: {
-          'ui:title': 'City (or APO/FPO/DPO)',
+          'ui:title': 'City or county',
           'ui:required': formData =>
             isChapterFieldRequired(formData, TASK_KEYS.reportDeath),
         },
