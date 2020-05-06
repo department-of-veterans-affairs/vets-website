6b42c6951cab8a3a96cc89cbf53affad04aab98a
Eric Buckley <eric.buckley@gmail.com>
2020-05-06 12:20:38 -0700

    its a async function we dont need to use then

--- src/platform/utilities/sso/index.js
+++ src/platform/utilities/sso/index.js
@@ -8,22 +8,21 @@ const keepAlive = environment.isLocalhost() ? mockKeepAlive : liveKeepAlive;
 const keepAliveThreshold = 5 * 60 * 1000; // 5 minutes
 
 export async function ssoKeepAliveSession() {
-  return keepAlive().then(ttl => {
-    if (ttl > 0) {
-      // ttl is positive, user has an active session
-      const expirationTime = new Date();
-      // 'ttl' is in seconds, convert to milliseconds
-      expirationTime.setTime(expirationTime.getTime() + ttl * 1000);
-      localStorage.setItem('sessionExpirationSSO', expirationTime);
-      localStorage.setItem('hasSessionSSO', true);
-    } else if (ttl === 0) {
-      // ttl is 0, user has an inactive session
-      localStorage.setItem('hasSessionSSO', false);
-    } else {
-      // ttl is null, we can't determine if the user has a session or not
-      localStorage.removeItem('hasSessionSSO');
-    }
-  });
+  const ttl = await keepAlive();
+  if (ttl > 0) {
+    // ttl is positive, user has an active session
+    const expirationTime = new Date();
+    // 'ttl' is in seconds, convert to milliseconds
+    expirationTime.setTime(expirationTime.getTime() + ttl * 1000);
+    localStorage.setItem('sessionExpirationSSO', expirationTime);
+    localStorage.setItem('hasSessionSSO', true);
+  } else if (ttl === 0) {
+    // ttl is 0, user has an inactive session
+    localStorage.setItem('hasSessionSSO', false);
+  } else {
+    // ttl is null, we can't determine if the user has a session or not
+    localStorage.removeItem('hasSessionSSO');
+  }
 }
 export function checkAndUpdateSSOeSession() {
   if (hasSessionSSO() === 'true') {
