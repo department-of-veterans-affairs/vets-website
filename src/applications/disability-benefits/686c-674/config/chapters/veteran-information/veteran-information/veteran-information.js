71feb600b28ab70bce644e136ab633e34169799a
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-05-07 13:52:21 -0700

    Refactored so the component does not show up in review page

--- src/applications/disability-benefits/686c-674/config/chapters/veteran-information/veteran-information/veteran-information.js
+++ src/applications/disability-benefits/686c-674/config/chapters/veteran-information/veteran-information/veteran-information.js
@@ -3,19 +3,13 @@ import VeteranInformationComponent from './VeteranInformationComponent.js';
 
 export const schema = {
   type: 'object',
-  properties: {
-    veteranInformation: {
-      type: 'string',
-    },
-  },
+  properties: {},
 };
 
 export const uiSchema = {
-  veteranInformation: {
-    'ui:widget': VeteranInformationComponent,
-    'ui:title': 'This is the personal information we have on file for you.',
-    'ui:options': {
-      hideOnReview: true,
-    },
+  'ui:description': VeteranInformationComponent,
+  'ui:title': 'This is the personal information we have on file for you.',
+  'ui:options': {
+    hideOnReview: true,
   },
 };
