f0a690b9f9e27797f876069f8705d36084049ad3
jhonnyoddball <jhonny.gonzalez@oddball.io>
2020-05-07 10:07:27 -0400

    Fix 'Find a VA Location' link for subdomains

--- src/platform/utilities/environment/va-subdomain.js
+++ src/platform/utilities/environment/va-subdomain.js
@@ -5,6 +5,6 @@
  */
 export default function isVATeamSiteSubdomain() {
   return !!window.location.hostname.match(
-    /(cem|choose|benefits|telehealth|volunteer|ptsd|nutrition).va/gi,
+    /(benefits|cem|choose|ethics|healthquality|hepatitis|hiv|mentalhealth|move|nutrition|oedca|oefoif|oprm|osp|patientcare|patientsafety|polytrauma|ptsd|publichealth|ruralhealth|sci|telehealth|vetcenter|volunteer).va/gi,
   );
 }
