d55c46d70a82fb3ccb4f8660966144153d6516ce
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-05-07 15:03:17 -0700

    Added 'Need Help' section in footer

--- src/applications/disability-benefits/686c-674/config/form.js
+++ src/applications/disability-benefits/686c-674/config/form.js
@@ -1,5 +1,4 @@
 import fullSchema from 'vets-json-schema/dist/686C-674-schema.json';
-
 import environment from 'platform/utilities/environment';
 import preSubmitInfo from 'platform/forms/preSubmitInfo';
 import { VA_FORM_IDS } from 'platform/forms/constants';
@@ -8,6 +7,8 @@ import { isChapterFieldRequired } from './helpers';
 
 import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
+import FormFooter from 'platform/forms/components/FormFooter';
+import GetFormHelp from '../components/GetFormHelp.jsx';
 
 // Chapter imports
 import { formerSpouseInformation } from './chapters/report-divorce';
@@ -62,6 +63,8 @@ const formConfig = {
   formId: VA_FORM_IDS.FORM_21_686C,
   version: 0,
   prefillEnabled: true,
+  footerContent: FormFooter,
+  getHelp: GetFormHelp,
   savedFormMessages: {
     notFound: 'Please start over to apply for declare or remove a dependent.',
     noAuth:
@@ -69,6 +72,7 @@ const formConfig = {
   },
   title: 'New 686',
   defaultDefinitions: { ...fullSchema.definitions },
+
   chapters: {
     optionSelection: {
       title: 'What do you want to do?',
