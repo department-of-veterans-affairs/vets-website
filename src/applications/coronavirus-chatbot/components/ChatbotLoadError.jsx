5531a63a2c8d6211c6eebc4ab96b900aeab7315e
Shaun Hubbard <shaun.hubbard@thoughtworks.com>
2020-05-07 10:17:12 -0500

    [#190][Shaun] added load error text if window.WebChat is defined or another error occurs

---
+++ src/applications/coronavirus-chatbot/components/ChatbotLoadError.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
+
+export default function ChatbotLoadError(_props) {
+  return (
+    <AlertBox
+      headline="Error Loading Chatbot"
+      content="We are sorry there was an issue loading the chatbot, please refresh the page to try again. If the problem persists there may be a caching issue, please clear your cache and refresh the page."
+      status="error"/>
+  );
+}
