2c519373d132b3f9da88b9ed61920ed8f769cc28
Katie Ho <kho@thoughtworks.com>
2020-05-07 14:58:40 -0400

    [department-of-veterans-affairs/covid19-chatbot#190] Updates AlertBox content with approved copy

--- src/applications/coronavirus-chatbot/components/ChatbotLoadError.jsx
+++ src/applications/coronavirus-chatbot/components/ChatbotLoadError.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
 
 export default function ChatbotLoadError() {
+  const alertMessage = (
+    <div>
+      <p>
+        Please make sure you’re connected to the internet, and refresh this page
+        to try again.
+      </p>
+      <p>
+        If it still doesn’t work, you may need to clear your internet browser’s
+        history (sometimes called “cached data”). You can find how to do this
+        within your browser’s privacy and security settings.
+      </p>
+    </div>
+  );
+
   return (
     <AlertBox
-      headline="Error Loading Chatbot"
-      content="We are sorry there was an issue loading the chatbot, please refresh the page to try again. If the problem persists there may be a caching issue, please clear your cache and refresh the page."
+      headline="We can’t load the chatbot"
+      content={alertMessage}
       status="error"
     />
   );
