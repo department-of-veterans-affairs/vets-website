121908bb1bb5de89c2d076cfa5c86e500509a865
Dan Shank <danshank@gmail.com>
2020-05-07 21:36:47 -0700

    add axe e2e lint rule

---
+++ script/eslint-plugin-va/rules/axe-e2e-tests.js
@@ -0,0 +1,34 @@
+const MESSAGE =
+  'THIS IS A SUPER AWESOME ERROR MESSAGE ABOUT AXE CHECKS IN E2E TESTS';
+
+const rule = {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description: MESSAGE,
+      category: 'best practices',
+      recommended: false,
+    },
+  },
+  create: context => {
+    let axeCheckCount = 0;
+    return {
+      MemberExpression: node => {
+        if (node.property.name === 'axeCheck') {
+          axeCheckCount++;
+        }
+      },
+      'Program:exit': function(node) {
+        if (axeCheckCount === 0) {
+          const message = `${MESSAGE}`;
+          context.report({
+            node,
+            message,
+          });
+        }
+      },
+    };
+  },
+};
+
+module.exports = rule;
