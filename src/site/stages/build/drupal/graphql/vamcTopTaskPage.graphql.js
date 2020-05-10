c3f939217f26bbaba13426ca4d0f3dffd6fe02d7
Ethan Teague <metrowebz@gmail.com>
2020-05-10 13:19:11 -0400

    VAVFS-8770: Update top level page query.

--- src/site/stages/build/drupal/graphql/vamcTopTaskPage.graphql.js
+++ src/site/stages/build/drupal/graphql/vamcTopTaskPage.graphql.js
@@ -6,20 +6,6 @@ const {
   FIELD_RELATED_LINKS,
 } = require('./paragraph-fragments/listOfLinkTeasers.paragraph.graphql');
 const { FIELD_ALERT } = require('./block-fragments/alert.block.graphql');
-
-const WYSIWYG = '... wysiwyg';
-const STAFF = '... staffProfile';
-const COLLAPSIBLE_PANEL = '... collapsiblePanel';
-const PROCESS = '... process';
-const QA_SECTION = '... qaSection';
-const QA = '... qa';
-const LIST_OF_LINK_TEASERS = '... listOfLinkTeasers';
-const REACT_WIDGET = '... reactWidget';
-const NUMBER_CALLOUT = '... numberCallout';
-const TABLE = '... table';
-const ALERT_PARAGRAPH = '... alertParagraph';
-const DOWNLOADABLE_FILE_PARAGRAPH = '... downloadableFile';
-const MEDIA_PARAGRAPH = '... embeddedImage';
 const entityElementsFromPages = require('./entityElementsForPages.graphql');
 
 module.exports = `
@@ -28,33 +14,93 @@ module.exports = `
     ${entityElementsFromPages}
     entityBundle
     changed
-    fieldIntroText
     fieldTableOfContentsBoolean
-    fieldFeaturedContent {
+    fieldIntroTextTokenized {
       entity {
-        entityType
-        entityBundle
-        ${WYSIWYG}
-        ${QA}
+        ... on ParagraphReusableNationalTokenContent {
+          fieldNationalContentTemplates {
+            entity {
+              ... on NodeVamcNationalContentEditable {
+                title
+                body {
+                  processed
+                }
+              }
+            }
+          }
+        }
       }
     }
     fieldContentBlock {
       entity {
-        entityType
-        entityBundle
-        ${STAFF}
-        ${WYSIWYG}
-        ${COLLAPSIBLE_PANEL}
-        ${PROCESS}
-        ${QA_SECTION}
-        ${QA}
-        ${LIST_OF_LINK_TEASERS}
-        ${REACT_WIDGET}
-        ${NUMBER_CALLOUT}
-        ${TABLE}
-        ${ALERT_PARAGRAPH}
-        ${DOWNLOADABLE_FILE_PARAGRAPH}
-        ${MEDIA_PARAGRAPH}
+        ... on ParagraphReusableNationalContent {
+          fieldContent {
+            entity {
+              ... on NodeVamcNationalContent {
+                title
+                body {
+                  processed
+                }
+              }
+            }
+          }
+          fieldNationalContentTemplates {
+            entity {
+              ... on NodeVamcNationalContentEditable {
+                title
+                body {
+                  processed
+                }
+              }
+            }
+          }
+          fieldSystemLevelCustomConten {
+            entity {
+              ... on NodeVamcSystemContentCustom {
+                title
+                body {
+                  processed
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    fieldFeaturedContent {
+      entity {
+        ... on ParagraphReusableNationalContent {
+          fieldContent {
+            entity {
+              ... on NodeVamcNationalContent {
+                title
+                body {
+                  processed
+                }
+              }
+            }
+          }
+          fieldNationalContentTemplates {
+            entity {
+              ... on NodeVamcNationalContentEditable {
+                title
+                body {
+                  processed
+                }
+              }
+            }
+          }
+          fieldSystemLevelCustomConten {
+            entity {
+              ... on NodeVamcSystemContentCustom {
+                title
+                body {
+                  processed
+                }
+              }
+            }
+          }
+        }
       }
     }
     ${FIELD_RELATED_LINKS}
