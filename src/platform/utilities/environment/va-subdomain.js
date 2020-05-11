c24777917a7459c9952fd5db0b55add0160aaabf
jhonnyoddball <jhonny.gonzalez@oddball.io>
2020-05-11 12:11:26 -0400

    Refactor va-subdomain file

--- src/platform/utilities/environment/va-subdomain.js
+++ src/platform/utilities/environment/va-subdomain.js
@@ -4,33 +4,5 @@
  * @module platform/brand-consolidation/va-subdomain
  */
 export default function isVATeamSiteSubdomain() {
-  const subdomains = [
-    'benefits',
-    'cem',
-    'choose',
-    'ethics',
-    'healthquality',
-    'hepatitis',
-    'hiv',
-    'mentalhealth',
-    'move',
-    'nutrition',
-    'oedca',
-    'oefoif',
-    'oprm',
-    'osp',
-    'patientcare',
-    'patientsafety',
-    'polytrauma',
-    'ptsd',
-    'publichealth',
-    'ruralhealth',
-    'sci',
-    'telehealth',
-    'vetcenter',
-    'volunteer',
-  ].join('|');
-  const reg = new RegExp(`(${subdomains}).va`, 'gi');
-
-  return !!window.location.hostname.match(reg);
+  return !window.location.hostname.match(/(www|staging|dev).va.gov/gi);
 }
