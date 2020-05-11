d9076b81eb6e1f6dd950b5bf67aa24fe9fc6ff1d
mr0sari0 <mahariel.rosario@gmail.com>
2020-05-11 17:04:41 -0400

    set up test file

---
+++ src/applications/disability-benefits/2346/tests/SelectArrayItemsAccessoriesWidget.unit.spec.jsx
@@ -0,0 +1,53 @@
+// const fakeStore = {
+//   getState: () => ({
+//     form: {
+//       data: {
+//         supplies: [
+//           {
+//             productName: 'DOME',
+//             productGroup: 'hearing aid accessories',
+//             productId: '3',
+//             availableForReorder: true,
+//             lastOrderDate: '2019-06-30',
+//             nextAvailabilityDate: '2019-12-15',
+//             quantity: 10,
+//             size: '6mm',
+//           },
+//           {
+//             productName: 'DOME',
+//             productGroup: 'hearing aid accessories',
+//             productId: '4',
+//             availableForReorder: true,
+//             lastOrderDate: '2019-06-30',
+//             nextAvailabilityDate: '2019-12-15',
+//             quantity: 10,
+//             size: '7mm',
+//           },
+//           {
+//             productName: 'WaxBuster Single Unit',
+//             productGroup: 'hearing aid accessories',
+//             productId: '5',
+//             availableForReorder: true,
+//             lastOrderDate: '2019-06-30',
+//             nextAvailabilityDate: '2019-12-15',
+//             quantity: 10,
+//           },
+//         ],
+//         selectedProducts: [{ productId: '4' }, { productId: '5' }],
+//       },
+//     },
+//   }),
+//   subscribe: () => {},
+//   dispatch: () => {},
+// };
+
+describe('SelectArrayItemsAccessoriesWidget', () => {
+  it('should display accessories that are able to be ordered online', () => {
+    // const wrapper = shallow(<SelectArrayItemsAccessoriesWidget />);
+  });
+  it('should house the selected accessories inside selectedProducts array', () => {});
+  it('should display the quantity of the accessory', () => {});
+  it('should display the last order date of the accessory', () => {});
+  it('should display the product name of the accessory', () => {});
+  it('should display the accessories with a light gray background', () => {});
+});
