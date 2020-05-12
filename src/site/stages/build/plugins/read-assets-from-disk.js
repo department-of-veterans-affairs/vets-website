2e3713e2e91041542c385e1ad68c284fe675e48f
Brooks Johnson <brooks.johnson@pm.me>
2020-05-12 16:04:36 -0700

    Don't bother with handling directories
    There should be no directories in the generated folder - just files

--- src/site/stages/build/plugins/read-assets-from-disk.js
+++ src/site/stages/build/plugins/read-assets-from-disk.js
@@ -10,18 +10,15 @@ function readAssetsFromDisk(buildOptions) {
   );
   return files => {
     if (fs.existsSync(webpackOutput)) {
-      fs.readdirSync(webpackOutput, { withFileTypes: true }).forEach(dirent => {
-        const filename = dirent.name;
-
-        // TODO: still need to copy over files in sub-directories
-        if (dirent.isDirectory()) return;
-
-        // eslint-disable-next-line no-param-reassign
-        files[`generated/${filename}`] = {
-          path: `generated/{$filename}`,
-          contents: fs.readFileSync(path.join(webpackOutput, filename)),
-        };
-      });
+      fs.readdirSync(webpackOutput).forEach(
+        // eslint-disable-next-line no-return-assign
+        filename =>
+          // eslint-disable-next-line no-param-reassign
+          (files[`generated/${filename}`] = {
+            path: `generated/${filename}`,
+            contents: fs.readFileSync(path.join(webpackOutput, filename)),
+          }),
+      );
     } else {
       // eslint-disable-next-line no-console
       console.log('No Webpack assets found');
