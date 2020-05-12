f1cf7ffe35a21b6bf2d8718c79f5b2910f5ad472
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-05-11 22:56:06 -0400

    Applied form tester to HCA.

---
+++ src/applications/hca/tests/hca.cypress.spec.js
@@ -0,0 +1,55 @@
+import testForm from '../../../platform/testing/e2e/cypress/support/form-tester';
+
+import testData from './data/maximal-test.json';
+
+const testConfig = {
+  url: '/health-care/apply/application',
+  testData,
+  pageHooks: {
+    '/health-care/apply/application/introduction': () => {
+      // Hit the start button
+      cy.findAllByText(/start.+without signing in/i, { selector: 'button' })
+        .first()
+        .click();
+    },
+    '/health-care/apply/application/id-form': () => {
+      const { first, last } = testData.veteranFullName;
+
+      const [year, month, day] = testData.veteranDateOfBirth.split('-');
+
+      cy.findByLabelText(/first name/i).type(first);
+      cy.findByLabelText(/last name/i).type(last);
+      cy.findByLabelText(/month/i).select(parseInt(month, 10).toString());
+      cy.findByLabelText(/day/i).select(parseInt(day, 10).toString());
+      cy.findByLabelText(/year/i).type(year);
+      cy.findByLabelText(/social security/i).type(
+        testData.veteranSocialSecurityNumber,
+      );
+      cy.findAllByText(/continue/i, { selector: 'button' })
+        .first()
+        .click();
+    },
+  },
+  setup: () => {
+    cy.route({
+      method: 'GET',
+      url: '/v0/health_care_applications/enrollment_status*',
+      status: 404,
+      response: {
+        errors: [
+          {
+            title: 'Record not found',
+            detail: 'The record identified by  could not be found',
+            code: '404',
+            status: '404',
+          },
+        ],
+      },
+    }).route('POST', '/v0/health_care_applications', {
+      formSubmissionId: '123fake-submission-id-567',
+      timestamp: '2016-05-16',
+    });
+  },
+};
+
+testForm('HCA', testConfig);
