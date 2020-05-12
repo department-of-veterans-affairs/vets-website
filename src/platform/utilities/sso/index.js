99100077be7cc498ba52eb65f12ebb94ea93e95e
Eric Buckley <eric.buckley@gmail.com>
2020-05-11 21:09:18 -0700

    adding sso utility functions to track if the user needs to force authentication

--- src/platform/utilities/sso/index.js
+++ src/platform/utilities/sso/index.js
@@ -34,3 +34,20 @@ export function checkAndUpdateSSOeSession() {
     }
   }
 }
+
+/*
+ * When set, the `requiresForceAuth` indicates that the current unauthenticated
+ * user will have to "force" authentication against SSOe in order to establish a
+ * session.
+ */
+export const getForceAuth = () => {
+  localStorage.getItem('requiresForceAuth');
+};
+
+export const setForceAuth = () => {
+  localStorage.setItem('requiresForceAuth', true);
+};
+
+export const deleteForceAuth = () => {
+  localStorage.deletItem('requiresForceAuth');
+};
