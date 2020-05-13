b1fce515def319fc0841e06e6e3aa987457a673b
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-05-13 15:13:52 -0400

    Make less broken

--- src/applications/static-pages/ebenefits-link-update.js
+++ src/applications/static-pages/ebenefits-link-update.js
@@ -1,25 +1,26 @@
 import { isLoggedIn as isLoggedInSelector } from 'platform/user/selectors';
 import { shouldUseProxyUrl } from 'platform/site-wide/ebenefits/selectors';
-import { proxyUrl, defaultUrl } from 'platform/site-wide/ebenefits/utilities';
+import { proxyUrl } from 'platform/site-wide/ebenefits/utilities';
 
 export default function createMyVALoginWidget(store) {
   let unsubscribe;
   const updateEbenefitsLinks = () => {
-
     const state = store.getState();
     const isLoggedIn = isLoggedInSelector(state);
     const useProxyUrl = shouldUseProxyUrl(state);
 
     if (isLoggedIn && useProxyUrl) {
-
+      const ebenefitsDomain = 'https://www.ebenefits.va.gov';
       const ebenefitsLinks = [
-        ...document.querySelectorAll('a[href^="https://ebenefits.va.gov'),
+        ...document.querySelectorAll(`a[href^="${ebenefitsDomain}"]`),
       ];
 
       ebenefitsLinks.forEach(anchor => {
-        const url = props.useProxyUrl ? proxyUrl : defaultUrl;
         const { href } = anchor;
-        // modeled after <Ebenefits/>
+        const path = href.replace(ebenefitsDomain, '');
+
+        // eslint-disable-next-line no-param-reassign
+        anchor.href = proxyUrl(path);
       });
 
       unsubscribe();
