0d76957cfd03e4c2e3e292f2a42d8e172ba3325f
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-05-13 00:35:12 -0400

    Use our Webpack config for Cypress.

--- src/platform/testing/e2e/cypress/plugins/index.js
+++ src/platform/testing/e2e/cypress/plugins/index.js
@@ -1,1 +1,11 @@
-module.exports = (/* on, config */) => {};
+const webpackPreprocessor = require('@cypress/webpack-preprocessor');
+
+module.exports = on => {
+  const env = 'localhost';
+
+  const options = {
+    webpackOptions: require('../../../../../../config/webpack.config.js')(env),
+  };
+
+  on('file:preprocessor', webpackPreprocessor(options));
+};
