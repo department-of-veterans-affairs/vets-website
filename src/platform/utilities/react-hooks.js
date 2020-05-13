6eef65ce7d9c9a9ee9e08d92d45e166a61e9a0c1
Erik P. Hansen <eph@erikphansen.com>
2020-05-12 17:32:33 -0700

    add jsdoc comment

--- src/platform/utilities/react-hooks.js
+++ src/platform/utilities/react-hooks.js
@@ -10,18 +10,22 @@ import { useEffect, useRef } from 'react';
  *
  * This post does a deeper dive for those who are new to hooks:
  * https://blog.logrocket.com/how-to-get-previous-props-state-with-react-hooks/
+ *
+ * @param {*} nextValue - The value to return the next time this hook is run
+ * @returns {*} - The value that was passed to this hook the previous time that
+ * it was run. Or `undefined` if the hook is being run for the first time.
  */
-export const usePrevious = value => {
-  const valueRef = useRef();
+export const usePrevious = nextValue => {
+  const previousValue = useRef();
   useEffect(
     () => {
-      valueRef.current = value;
+      previousValue.current = nextValue;
     },
-    // only run this useEffect hook if `value` changes
-    [value],
+    // only run this useEffect hook if `nextValue` changes
+    [nextValue],
   );
-  // this returns _before_ the `useEffect` call completes, so this will return
-  // whatever was stored in `ref.current` _before_ this function's `useEffect`
-  // callback runs
-  return valueRef.current;
+  // This returns _before_ the `useEffect` call completes, so this will return
+  // whatever was stored in `previousValue.current` _before_ this function's
+  // `useEffect` callback runs
+  return previousValue.current;
 };
