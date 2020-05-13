8a851d43c3825d4bb1f0bda74739f7596d6a9592
Eric Buckley <eric.buckley@gmail.com>
2020-05-13 13:57:55 -0700

    fixing checkAutoSession unit tests

--- src/platform/site-wide/user-nav/tests/helpers.unit.spec.js
+++ src/platform/site-wide/user-nav/tests/helpers.unit.spec.js
@@ -2,7 +2,8 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 
 import * as authUtils from 'platform/user/authentication/utilities';
-import localStorage from 'platform/utilities/storage/localStorage';
+import * as ssoUtils from 'platform/utilities/sso';
+import * as profUtils from 'platform/user/profile/utilities';
 
 import { parseqs, checkAutoSession } from '../helpers';
 
@@ -27,69 +28,80 @@ describe('parseqs', () => {
 });
 
 describe('checkAutoSession', () => {
-  afterEach(() => localStorage.clear());
-
-  it('should auto logout if user is logged in and they do not have a SSOe session', () => {
-    localStorage.setItem('hasSession', true);
-    localStorage.setItem('hasSessionSSO', false);
-    const stub = sinon.stub(authUtils, 'autoLogout');
-    checkAutoSession().then(() => {
-      sinon.assert.calledOnce(stub);
-    });
-    stub.restore();
+  it('should auto logout if user is logged in and they do not have a SSOe session', async () => {
+    const sandbox = sinon.createSandbox();
+    sandbox.stub(profUtils, 'hasSession').returns(true);
+    sandbox.stub(profUtils, 'hasSessionSSO').returns(false);
+    sandbox.stub(ssoUtils, 'getForceAuth').returns(undefined);
+    const auto = sandbox.stub(authUtils, 'autoLogout');
+    await checkAutoSession();
+    sandbox.restore();
+    sinon.assert.calledOnce(auto);
   });
 
-  it('should not auto logout if user is logged in and they have a SSOe session', () => {
-    localStorage.setItem('hasSession', true);
-    localStorage.setItem('hasSessionSSO', true);
-    const stub = sinon.stub(authUtils, 'autoLogout');
-    checkAutoSession().then(() => {
-      sinon.assert.notCalled(stub);
-    });
-    stub.restore();
+  it('should not auto logout if user is logged in and they have a SSOe session', async () => {
+    const sandbox = sinon.createSandbox();
+    sandbox.stub(profUtils, 'hasSession').returns(true);
+    sandbox.stub(profUtils, 'hasSessionSSO').returns(true);
+    sandbox.stub(ssoUtils, 'getForceAuth').returns(undefined);
+    const auto = sandbox.stub(authUtils, 'autoLogout');
+    await checkAutoSession();
+    sandbox.restore();
+    sinon.assert.notCalled(auto);
   });
 
-  it('should not auto logout if user is logged in and we dont know if they have a SSOe session', () => {
-    localStorage.setItem('hasSession', true);
-    const stub = sinon.stub(authUtils, 'autoLogout');
-    checkAutoSession().then(() => {
-      sinon.assert.notCalled(stub);
-    });
-    stub.restore();
+  it('should not auto logout if user is logged in and we dont know if they have a SSOe session', async () => {
+    const sandbox = sinon.createSandbox();
+    sandbox.stub(profUtils, 'hasSession').returns(true);
+    sandbox.stub(profUtils, 'hasSessionSSO').returns(null);
+    sandbox.stub(ssoUtils, 'getForceAuth').returns(undefined);
+    const auto = sandbox.stub(authUtils, 'autoLogout');
+    await checkAutoSession();
+    sandbox.restore();
+    sinon.assert.notCalled(auto);
   });
 
-  it('should auto login if user is logged out, they have a SSOe session and dont need to force auth', () => {
-    localStorage.setItem('hasSessionSSO', true);
-    const stub = sinon.stub(authUtils, 'autoLogin');
-    checkAutoSession().then(() => {
-      sinon.assert.calledOnce(stub);
-    });
-    stub.restore();
+  it('should auto login if user is logged out, they have a SSOe session and dont need to force auth', async () => {
+    const sandbox = sinon.createSandbox();
+    sandbox.stub(profUtils, 'hasSession').returns(false);
+    sandbox.stub(profUtils, 'hasSessionSSO').returns(true);
+    sandbox.stub(ssoUtils, 'getForceAuth').returns(undefined);
+    const auto = sandbox.stub(authUtils, 'autoLogin');
+    await checkAutoSession();
+    sandbox.restore();
+    sinon.assert.calledOnce(auto);
   });
 
-  it('should not auto login if user is logged out, they dont have a SSOe session and dont need to force auth', () => {
-    localStorage.setItem('hasSessionSSO', false);
-    const stub = sinon.stub(authUtils, 'autoLogin');
-    checkAutoSession().then(() => {
-      sinon.assert.notCalled(stub);
-    });
-    stub.restore();
+  it('should not auto login if user is logged out, they dont have a SSOe session and dont need to force auth', async () => {
+    const sandbox = sinon.createSandbox();
+    sandbox.stub(profUtils, 'hasSession').returns(false);
+    sandbox.stub(profUtils, 'hasSessionSSO').returns(false);
+    sandbox.stub(ssoUtils, 'getForceAuth').returns(undefined);
+    const auto = sandbox.stub(authUtils, 'autoLogin');
+    await checkAutoSession();
+    sandbox.restore();
+    sinon.assert.notCalled(auto);
   });
 
-  it('should not auto login if user is logged out, they have a SSOe session and need to force auth', () => {
-    localStorage.setItem('hasSessionSSO', true);
-    localStorage.setItem('requiresForceAuth', true);
-    const stub = sinon.stub(authUtils, 'autoLogin');
-    checkAutoSession().then(() => {
-      sinon.assert.notCalled(stub);
-    });
-    stub.restore();
+  it('should not auto login if user is logged out, they have a SSOe session and need to force auth', async () => {
+    const sandbox = sinon.createSandbox();
+    sandbox.stub(profUtils, 'hasSession').returns(false);
+    sandbox.stub(profUtils, 'hasSessionSSO').returns(true);
+    sandbox.stub(ssoUtils, 'getForceAuth').returns(true);
+    const auto = sandbox.stub(authUtils, 'autoLogin');
+    await checkAutoSession();
+    sandbox.restore();
+    sinon.assert.notCalled(auto);
   });
 
-  it('should invoke callback if provided', () => {
-    const callback = sinon.spy();
-    checkAutoSession().then(() => {
-      sinon.assert.calledOnce(callback);
-    });
+  it('should invoke callback if provided', async () => {
+    const sandbox = sinon.createSandbox();
+    sandbox.stub(profUtils, 'hasSession').returns(false);
+    sandbox.stub(profUtils, 'hasSessionSSO').returns(false);
+    sandbox.stub(ssoUtils, 'getForceAuth').returns(undefined);
+    const callback = sandbox.spy();
+    await checkAutoSession(callback);
+    sandbox.restore();
+    sinon.assert.calledOnce(callback);
   });
 });
