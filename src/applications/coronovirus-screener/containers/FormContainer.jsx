16898d639356b85f0fb846935430595a59cc8bc2
Michael Chelen <michael.chelen@va.gov>
2020-05-14 11:52:43 -0400

    enable cough question

--- src/applications/coronovirus-screener/containers/FormContainer.jsx
+++ src/applications/coronovirus-screener/containers/FormContainer.jsx
@@ -4,6 +4,7 @@ import FormQuestion from '../components/FormQuestion';
 
 export default function FormContainer() {
   const [feverValue, setFeverValue] = React.useState(null);
+  const [coughValue, setCoughValue] = React.useState(null);
   const [fluValue, setFluValue] = React.useState(null);
 
   return (
@@ -15,12 +16,19 @@ export default function FormContainer() {
         panelName="Question 1"
       />
       <FormQuestion
+        question={questions.cough}
+        value={coughValue}
+        onChange={setCoughValue}
+        panelName="Question 2"
+      />
+      <FormQuestion
         question={questions.flu}
         value={fluValue}
         onChange={setFluValue}
-        panelName="Question 2"
+        panelName="Question 3"
       />
       <div>feverValue: {feverValue}</div>
+      <div>coughValue: {coughValue}</div>
       <div>fluValue: {fluValue}</div>
     </div>
   );
