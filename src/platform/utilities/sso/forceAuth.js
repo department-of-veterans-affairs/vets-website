a69d9a1e299215471b17f4f59d3edd4cead8a5a9
Eric Buckley <eric.buckley@gmail.com>
2020-05-14 13:23:03 -0700

    fixing forceAuth syntax

--- src/platform/utilities/sso/forceAuth.js
+++ src/platform/utilities/sso/forceAuth.js
@@ -1,18 +1,14 @@
-import localStorage from '../storage/localStorage';
+import localStorage from 'platform/utilities/storage/localStorage';
 
 /*
  * When set, the `requiresForceAuth` indicates that the current unauthenticated
  * user will have to "force" authentication against SSOe in order to establish a
  * session.
  */
-export const getForceAuth = () => {
-  localStorage.getItem('requiresForceAuth');
-};
+export const getForceAuth = () => localStorage.getItem('requiresForceAuth');
 
-export const setForceAuth = () => {
+export const setForceAuth = () =>
   localStorage.setItem('requiresForceAuth', true);
-};
 
-export const removeForceAuth = () => {
+export const removeForceAuth = () =>
   localStorage.removeItem('requiresForceAuth');
-};
