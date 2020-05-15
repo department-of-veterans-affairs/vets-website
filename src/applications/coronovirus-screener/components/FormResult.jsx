c0ad9dbe7a78679be925742edc8e02f55159ddf2
Michael Chelen <michael.chelen@va.gov>
2020-05-15 17:47:35 -0400

    update result screen styling

--- src/applications/coronovirus-screener/components/FormResult.jsx
+++ src/applications/coronovirus-screener/components/FormResult.jsx
@@ -10,22 +10,26 @@ export default function FormResult({ formState }) {
 
   const pass = (
     <div>
-      <h2>PASS</h2>
-      <div>Valid for: {moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
-      <div>
-        Please show this screen to the staff member at the facility entrance.
-        Thank you for helping us protect you and others during this time.
+      <h2 className="vads-u-font-size--h1">PASS</h2>
+      <h3>Valid for: {moment().format('MMMM Do YYYY, h:mm:ss a')}</h3>
+      <div className="vads-u-font-size--h3">
+        <p>
+          Please show this screen to the staff member at the facility entrance.
+        </p>
+        <p>Thank you for helping us protect you and others during this time.</p>
       </div>
     </div>
   );
 
   const fail = (
     <div>
-      <h2>More screening needed</h2>
-      <div>Valid for: {moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
-      <div>
-        Please show this screen to the staff member at the facility entrance.
-        Thank you for helping us protect you and others during this time.
+      <h2 className="vads-u-font-size--h1">More screening needed</h2>
+      <h3>Valid for: {moment().format('MMMM Do YYYY, h:mm:ss a')}</h3>
+      <div className="vads-u-font-size--h3">
+        <p>
+          Please show this screen to the staff member at the facility entrance.
+        </p>
+        <p>Thank you for helping us protect you and others during this time.</p>
       </div>
     </div>
   );
