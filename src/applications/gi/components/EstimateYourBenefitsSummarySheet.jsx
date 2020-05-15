f9ea021ecadd010a434e98937822a8bc8fd91c5e
Joe <elder_joseph@bah.com>
2020-05-15 10:23:48 -0400

    8918: Spacing and font updates for EYB sheet

--- src/applications/gi/components/EstimateYourBenefitsSummarySheet.jsx
+++ src/applications/gi/components/EstimateYourBenefitsSummarySheet.jsx
@@ -7,13 +7,14 @@ const CalculatorSheetResultRow = ({
   value,
   header,
   visible,
+  bold,
   boldLabel,
   plainTextValue,
 }) =>
   visible ? (
     <div
       id={`calculator-result-row-${id == null ? label : id}`}
-      className="row calculator-result"
+      className={classNames('row', 'calculator-result', { bold })}
     >
       <div className="small-8 columns">
         {header ? (
@@ -63,30 +64,30 @@ export const EstimateYourBenefitsSummarySheet = props => (
                 visible={props.outputs.giBillPaysToSchool.visible}
               />
             </div>
-            <div className="vads-u-margin-y--1p5">
+            <div className="vads-u-margin-y--0p5">
               <CalculatorSheetResultRow
                 label="Tuition and fees charged"
                 value={props.outputs.tuitionAndFeesCharged.value}
                 visible={props.outputs.tuitionAndFeesCharged.visible}
               />
             </div>
-            <div className="vads-u-margin-y--1p5">
+            <div className="vads-u-margin-top--1 vads-u-margin-bottom--0">
               <CalculatorSheetResultRow
                 label="Your scholarships"
                 value={props.outputs.yourScholarships.value}
                 visible={props.outputs.yourScholarships.visible}
               />
             </div>
-            <div className="vads-u-margin-y--1p5">
+            <div className="vads-u-margin-top--1 vads-u-margin-bottom--1p5">
               <CalculatorSheetResultRow
                 label="Out of pocket tuition"
                 value={props.outputs.outOfPocketTuition.value}
-                header
+                bold
                 visible={props.outputs.outOfPocketTuition.visible}
               />
             </div>
           </div>
-          <div className="vads-u-margin-y--1p5">
+          <div className="vads-u-margin-top--1 vads-u-margin-bottom--2">
             <CalculatorSheetResultRow
               label="Housing allowance"
               value={props.outputs.housingAllowance.value}
@@ -94,7 +95,7 @@ export const EstimateYourBenefitsSummarySheet = props => (
               visible={props.outputs.housingAllowance.visible}
             />
           </div>
-          <div className="vads-u-margin-y--1p5">
+          <div className="vads-u-margin-top--1p5 vads-u-margin-bottom--1p5">
             <CalculatorSheetResultRow
               label="Book stipend"
               value={props.outputs.bookStipend.value}
@@ -105,6 +106,7 @@ export const EstimateYourBenefitsSummarySheet = props => (
           <div className="vads-u-margin-y--1p5">
             <CalculatorSheetResultRow
               label="Yellow Ribbon"
+              bold
               value={
                 props.outputs.perTerm.yellowRibbon.terms.find(
                   item => item.label === 'Total per year',
@@ -113,11 +115,11 @@ export const EstimateYourBenefitsSummarySheet = props => (
               visible={props.yellowRibbon}
             />
           </div>
-          <div className="vads-u-margin-y--1p5">
+          <div className="vads-u-margin-y--1">
             <CalculatorSheetResultRow
               label="Total paid to you"
               value={props.outputs.totalPaidToYou.value}
-              header
+              bold
               visible={props.outputs.totalPaidToYou.visible}
             />
           </div>
