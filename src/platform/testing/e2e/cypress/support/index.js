09d39c5bac1e5713087c77257523117b40169fab
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-05-18 16:12:54 -0400

    Imported common commands in main support file.

--- src/platform/testing/e2e/cypress/support/index.js
+++ src/platform/testing/e2e/cypress/support/index.js
@@ -1,5 +1,7 @@
 import '@testing-library/cypress/add-commands';
 
+import './commands';
+
 // Workaround to allow Cypress to intercept requests made with the Fetch API.
 // This forces fetch to fall back to the polyfill, which can get intercepted.
 // https://github.com/cypress-io/cypress/issues/95
