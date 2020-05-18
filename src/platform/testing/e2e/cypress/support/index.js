edd21229acfc15f7d631dc10cdb2fbfe63f886f6
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-05-18 15:29:29 -0400

    Workaround for supporting Fetch with Cypress stubs.

---
+++ src/platform/testing/e2e/cypress/support/index.js
@@ -0,0 +1,7 @@
+// Workaround to allow Cypress to intercept requests made with the Fetch API.
+// This forces fetch to fall back to the polyfill, which can get intercepted.
+// https://github.com/cypress-io/cypress/issues/95
+Cypress.on('window:before:load', window => {
+  // eslint-disable-next-line no-param-reassign
+  delete window.fetch;
+});
