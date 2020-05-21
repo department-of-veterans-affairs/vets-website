589aa6919313b96f163a2b6fb4b18a7b035c00a4
batemapf <batemapf@gmail.com>
2020-05-20 22:33:46 -0400

    add noindex meta tag to route

--- src/applications/coronavirus-screener/containers/App.jsx
+++ src/applications/coronavirus-screener/containers/App.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import MetaTags from 'react-meta-tags';
 import MultiQuestionForm from '../components/MultiQuestionForm';
 import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
 import { questions } from '../config/questions';
 
 export default function App() {
   return (
-    <div className="vads-l-grid-container">
-      <AlertBox
-        headline="As part of our coronavirus screening, you'll need to answer some questions before entering a VA facility."
-        content="We won't store or share your data."
-        status="info"
-        isVisible
-      />
-      <MultiQuestionForm questions={questions} />
+    <div>
+      <MetaTags>
+        <meta name="robots" content="noindex" />
+      </MetaTags>
+      <div className="vads-l-grid-container">
+        <AlertBox
+          headline="As part of our coronavirus screening, you'll need to answer some questions before entering a VA facility."
+          content="We won't store or share your data."
+          status="info"
+          isVisible
+        />
+        <MultiQuestionForm questions={questions} />
+      </div>
     </div>
   );
 }
