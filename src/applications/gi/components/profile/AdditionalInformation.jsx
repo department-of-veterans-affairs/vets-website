566fdf086947dfd2bbf5f1ca888f0e58a120204d
Devin McCurdy <mccurdy_devin@bah.com>
2020-05-21 11:39:31 -0400

    Cleanup

--- src/applications/gi/components/profile/AdditionalInformation.jsx
+++ src/applications/gi/components/profile/AdditionalInformation.jsx
@@ -8,6 +8,29 @@ export class AdditionalInformation extends React.Component {
 
     return constants.FISCALYEAR;
   }
+
+  renderSection103Info = section103Message => (
+    <div className="section-103-message">
+      <strong>
+        <button
+          type="button"
+          className="va-button-link learn-more-button"
+          onClick={() => {
+            recordEvent({
+              event: 'gibct-modal-displayed',
+              'gibct-modal-displayed': 'protection-against-late-va-payments',
+            });
+            this.props.onShowModal('section103');
+          }}
+        >
+          Protection against late VA payments:
+        </button>
+      </strong>
+      &nbsp;
+      {section103Message}
+    </div>
+  );
+
   renderInstitutionSummary() {
     const { institution } = this.props;
     const isOJT = institution.type.toLowerCase() === 'ojt';
@@ -16,26 +39,7 @@ export class AdditionalInformation extends React.Component {
       return (
         <div className="institution-summary">
           <h3>Institution summary</h3>
-          <div className="section-103-message">
-            <strong>
-              <button
-                type="button"
-                className="va-button-link learn-more-button"
-                onClick={() => {
-                  recordEvent({
-                    event: 'gibct-modal-displayed',
-                    'gibct-modal-displayed':
-                      'protection-against-late-va-payments',
-                  });
-                  this.props.onShowModal('section103');
-                }}
-              >
-                Protection against late VA payments:
-              </button>
-            </strong>
-            &nbsp;
-            {institution.section103Message}
-          </div>
+          {this.renderSection103Info(institution.section103Message)}
         </div>
       );
     }
@@ -108,28 +112,8 @@ export class AdditionalInformation extends React.Component {
         {typeOfAccreditation}
         {vetTuitionPolicy}
         {this.props.eduSection103 &&
-          institution.section103Message && (
-            <div className="section-103-message">
-              <strong>
-                <button
-                  type="button"
-                  className="va-button-link learn-more-button"
-                  onClick={() => {
-                    recordEvent({
-                      event: 'gibct-modal-displayed',
-                      'gibct-modal-displayed':
-                        'protection-against-late-va-payments',
-                    });
-                    this.props.onShowModal('section103');
-                  }}
-                >
-                  Protection against late VA payments:
-                </button>
-              </strong>
-              &nbsp;
-              {institution.section103Message}
-            </div>
-          )}
+          institution.section103Message &&
+          this.renderSection103Info(institution.section103Message)}
         {!this.props.eduSection103 && (
           <div>
             <strong>
