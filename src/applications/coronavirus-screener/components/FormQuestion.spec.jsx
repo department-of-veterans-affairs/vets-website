06379829010b63aa20d2aa4640b53b2254f8d44d
Michael Chelen <michael.chelen@va.gov>
2020-05-26 14:32:57 -0400

    add tests for button class

--- src/applications/coronavirus-screener/components/FormQuestion.spec.jsx
+++ src/applications/coronavirus-screener/components/FormQuestion.spec.jsx
@@ -29,9 +29,36 @@ describe('coronavirus-screener', () => {
           scrollNext={mockScrollNext}
         />,
       );
-
       expect(wrapper.find('h2').text()).to.equal(mockQuestion.text);
-
+      wrapper.unmount();
+    });
+    it('sets button class when no option is selected', () => {
+      const wrapper = shallow(
+        <FormQuestion
+          question={mockQuestion}
+          formState={mockFormState}
+          setFormState={mockSetFormState}
+          scrollNext={mockScrollNext}
+        />,
+      );
+      expect(wrapper.find('.usa-button-secondary')).to.have.lengthOf(2);
+      expect(wrapper.find('.usa-button')).to.have.lengthOf(0);
+      wrapper.unmount();
+    });
+    it('sets button class when some option is selected', () => {
+      mockFormState = {
+        sample1: 'yes',
+      };
+      const wrapper = shallow(
+        <FormQuestion
+          question={mockQuestion}
+          formState={mockFormState}
+          setFormState={mockSetFormState}
+          scrollNext={mockScrollNext}
+        />,
+      );
+      expect(wrapper.find('.usa-button')).to.have.lengthOf(1);
+      expect(wrapper.find('.usa-button-secondary')).to.have.lengthOf(1);
       wrapper.unmount();
     });
   });
