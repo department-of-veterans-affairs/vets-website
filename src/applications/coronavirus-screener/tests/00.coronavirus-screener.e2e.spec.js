b1688fa676113582d6e2dfc9a09abaa7d54d5098
Michael Chelen <michael.chelen@va.gov>
2020-05-26 15:42:08 -0400

    minor

--- src/applications/coronavirus-screener/tests/00.coronavirus-screener.e2e.spec.js
+++ src/applications/coronavirus-screener/tests/00.coronavirus-screener.e2e.spec.js
@@ -1,5 +1,5 @@
-const Timeouts = require('../../../platform/testing/e2e/timeouts');
-const E2eHelpers = require('../../../platform/testing/e2e/helpers');
+const Timeouts = require('platform/testing/e2e/timeouts');
+const E2eHelpers = require('platform/testing/e2e/helpers');
 const manifest = require('../manifest.json');
 
 module.exports = E2eHelpers.createE2eTest(client => {
