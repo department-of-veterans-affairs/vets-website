de8293681d8c3e15a51fbed56046f3ad9ecf87f9
Brad Conley <brad.conl3y@gmail.com>
2020-05-26 11:59:37 -0400

    Stand up initial debt letters entry

--- src/applications/debt-letters/app-entry.jsx
+++ src/applications/debt-letters/app-entry.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import 'platform/polyfills';
 import './sass/debt-letters.scss';
+import DebtLettersWrapper from './components/DebtLettersWrapper';
 
 import startApp from '../../platform/startup';
 
 import reducer from './reducers';
-import manifest from './manifest';
+import manifest from './manifest.json';
 
 startApp({
   url: manifest.rootUrl,
-  component: () => <h1>Foo</h1>,
+  component: <DebtLettersWrapper />,
   reducer,
   entryName: manifest.entryName,
 });
