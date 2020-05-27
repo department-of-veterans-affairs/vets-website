24165a18987ded399215e24e7ad7f6b6aac68a80
Matt Roth <roth_matthew@bah.com>
2020-05-27 13:53:18 -0400

    create conditional question component

---
+++ src/applications/gi/components/ConditionalQuestion.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export const ConditionalQuestion = ({ children }) => (
+  <div
+    className={classNames(
+      'vads-u-margin-left--neg2p5',
+      'vads-u-padding-left--1p5',
+      'vads-u-border-left--7px',
+      'vads-u-border-color--primary-alt-light',
+    )}
+  >
+    {children}
+  </div>
+);
+
+export default ConditionalQuestion;
