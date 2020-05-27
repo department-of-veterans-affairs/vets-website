98497407247fcd219d37820dfabc03738b51dbd5
Brad Conley <brad.conl3y@gmail.com>
2020-05-27 13:56:36 -0400

    Debt letters mock response and redux

--- src/applications/debt-letters/reducers/index.js
+++ src/applications/debt-letters/reducers/index.js
@@ -1,1 +1,39 @@
-export default {};
+import {
+  DEBTS_FETCH_INITIATED,
+  DEBTS_FETCH_SUCCESS,
+  DEBTS_FETCH_FAILURE,
+} from '../actions';
+
+const initialState = {
+  isPending: false,
+  isError: false,
+  debts: [],
+};
+
+export const debtsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case DEBTS_FETCH_INITIATED:
+      return {
+        ...state,
+        isPending: true,
+        isError: false,
+      };
+    case DEBTS_FETCH_SUCCESS:
+      return {
+        ...state,
+        isPending: false,
+        isError: false,
+        debts: action.debts,
+      };
+    case DEBTS_FETCH_FAILURE:
+      return {
+        ...state,
+        isPending: false,
+        isError: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default { debts: debtsReducer };
