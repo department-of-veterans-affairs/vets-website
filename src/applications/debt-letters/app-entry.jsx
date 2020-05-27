36a59860cd50e1b0d79fcc244f21ab9195d46ebe
Brad Conley <brad.conl3y@gmail.com>
2020-05-27 09:35:09 -0400

    fix import

--- src/applications/debt-letters/app-entry.jsx
+++ src/applications/debt-letters/app-entry.jsx
@@ -3,7 +3,7 @@ import 'platform/polyfills';
 import './sass/debt-letters.scss';
 import DebtLettersWrapper from './components/DebtLettersWrapper';
 
-import startApp from '../../platform/startup';
+import startApp from 'platform/startup';
 
 import reducer from './reducers';
 import manifest from './manifest.json';
