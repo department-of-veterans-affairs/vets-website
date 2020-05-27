8fb88506df5d5caec69b75b883e7a278acbf450b
Ethan Teague <metrowebz@gmail.com>
2020-05-27 18:02:36 -0400

    VAVFS-9237: Adding facility service local handling.

---
+++ src/site/stages/build/drupal/graphql/paragraph-fragments/locationInfo.paragraph.graphql.js
@@ -0,0 +1,50 @@
+/**
+ * A Drupal paragraph containing location info.
+ *
+ */
+module.exports = `
+  fragment locationInfo on ParagraphLocationInfo {
+    fieldAdditionalPhoneNumbers
+    fieldAdditionalPhNum1Title
+    fieldAdditionalPhNum2Title
+    fieldAdditionalPhoneNumber2
+    fieldLink {
+      title
+      url {
+        path
+      }
+    }
+    fieldAddress {
+      addressLine1
+      addressLine2
+      additionalName
+      administrativeArea
+      postalCode
+      locality
+      dependentLocality
+      countryCode
+      sortingCode
+    }
+    fieldAppointmentPhone
+    fieldTitle
+    fieldExtension
+    fieldWysiwyg {
+      value
+    }
+    fieldLocationHours {
+      day
+      starthours
+      endhours
+      comment
+    }
+    fieldMainPhone
+    fieldOnlineSchedulingAvailabl
+    fieldOperatingStatus
+    fieldOperatingStatusNotes {
+      value
+    }
+    fieldReferralRequired
+    fieldServiceOffered
+    fieldWalkInsAccepted
+  }
+`;
