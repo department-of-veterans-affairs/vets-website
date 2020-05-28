5360bec2951508b3d29c35a166345cae338c9ba2
Theo Bentum <theo@adhocteam.us>
2020-05-28 13:43:33 -0700

    check if tests exist before running

--- script/run-cypress-tests-circle.sh
+++ script/run-cypress-tests-circle.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+if [ 'ls src/**/tests/**/*.cypress.spec.js | wc -l' == 0 ]; then
+  echo "No Cypress tests found."
+  exit 0
+fi
+
 # Use the junit reporter & save results in './test-results'.
-reporsterAgrs="--reporter junit --reporter-options \"mochaFile=test-results/e2e-test-output-[hash].xml\""
+reporterArgs="--reporter junit --reporter-options \"mochaFile=test-results/e2e-test-output-[hash].xml\""
     
 # Use the CircleCI CLI to get Cypress tests & split them on different machines by timing. Commas are inserted as the delimiter for file paths.
 specArgs="--spec $(echo '"$(circleci tests glob "src/**/tests/**/*.cypress.spec.js" | circleci tests split --split-by=timings | paste -sd "," -)"')"
 
 # Start the server & wait for 'http://localhost:3001' to go live.
-yarn start-server-and-test watch http://localhost:3001 "cypress run $reporsterAgrs $specArgs"
+yarn start-server-and-test watch http://localhost:3001 "cypress run $reporterArgs $specArgs"
