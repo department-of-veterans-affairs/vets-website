576036c79b3977f7bc1fcf33fb697bba4289c8be
Theo Bentum <theo@adhocteam.us>
2020-05-27 20:27:40 -0700

    update run-cypress-tests

--- script/run-cypress-tests-circle.sh
+++ script/run-cypress-tests-circle.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Use the junit reporter & save results in './test-results'.
+reporterArgs="--reporter junit --reporter-options "mochaFile=test-results/e2e-test-output-[hash].xml""
+
+# Use the CircleCI CLI to get Cypress tests & split them on different machines by timing. 
+# Commas are inserted as the delimiter for file paths.
+specArgs="--spec "$(circleci tests glob "src/**/tests/**/*.cypress.spec.js" | circleci tests split --split-by=timings | paste -sd "," -)""
+
 # Start the server & wait for 'http://localhost:3001' to go live.
-yarn start-server-and-test watch http://localhost:3001 \
-  cypress run \
-  # Use the junit reporter & save results in './test-results'.
-  --reporter junit --reporter-options "mochaFile=test-results/e2e-test-output-[hash].xml" \
-  # Use the CircleCI CLI to get Cypress tests & split them on different machines by timing. Commas are inserted as the delimiter for file paths.
-  --spec "$(circleci tests glob "src/**/tests/**/*.cypress.spec.js" | circleci tests split --split-by=timings | paste -sd "," -)"
+yarn start-server-and-test watch http://localhost:3001 cypress run $reporterArgs $specArgs
