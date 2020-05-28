07c98db4b80a6ec056771f8e1a40c5676963c29c
Theo Bentum <theo@adhocteam.us>
2020-05-28 14:07:03 -0700

    exit if no tests

--- script/run-cypress-tests-circle.sh
+++ script/run-cypress-tests-circle.sh
@@ -4,13 +4,13 @@
 if [ 'ls src/**/tests/**/*.cypress.spec.js | wc -l' == 0 ]; then
   echo "No Cypress tests found."
   exit 0
-fi
-
-# Use the junit reporter & save results in './test-results'.
-reporterArgs="--reporter junit --reporter-options \"mochaFile=test-results/e2e-test-output-[hash].xml\""
+else
+  # Use the junit reporter & save results in './test-results'.
+  reporterArgs="--reporter junit --reporter-options \"mochaFile=test-results/e2e-test-output-[hash].xml\""
     
-# Use the CircleCI CLI to get Cypress tests & split them on different machines by timing. Commas are inserted as the delimiter for file paths.
-specArgs="--spec $(echo '"$(circleci tests glob "src/**/tests/**/*.cypress.spec.js" | circleci tests split --split-by=timings | paste -sd "," -)"')"
+  # Use the CircleCI CLI to get Cypress tests & split them on different machines by timing. Commas are inserted as the delimiter for file paths.
+  specArgs="--spec $(echo '"$(circleci tests glob "src/**/tests/**/*.cypress.spec.js" | circleci tests split --split-by=timings | paste -sd "," -)"')"
 
-# Start the server & wait for 'http://localhost:3001' to go live.
-yarn start-server-and-test watch http://localhost:3001 "cypress run $reporterArgs $specArgs"
+  # Start the server & wait for 'http://localhost:3001' to go live.
+  yarn start-server-and-test watch http://localhost:3001 "cypress run $reporterArgs $specArgs"
+fi
