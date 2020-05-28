98369aff17d96575226d76ae3a64e312e477c46f
Brad Conley <brad.conl3y@gmail.com>
2020-05-28 13:15:51 -0400

    WIP wiring up presentational component

--- src/applications/debt-letters/components/DebtLettersWrapper.jsx
+++ src/applications/debt-letters/components/DebtLettersWrapper.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
+import RequiredLoginView from 'platform/user/authorization/components/RequiredLoginView';
+import { bindActionCreators } from 'redux';
+import { fetchDebtLetters } from '../actions';
 
-const DebtLettersWrapper = () => (
-  <div className="usa-grid usa-grid-full">
-    <h2>Debt Letters</h2>
-  </div>
-);
+class DebtLettersWrapper extends Component {
+  componentDidMount() {
+    this.props.fetchDebtLetters();
+  }
+  render() {
+    const { user, isPending } = this.props;
+    return (
+      <RequiredLoginView verify user={user}>
+        {isPending && <LoadingIndicator />}
+        {!isPending && (
+          <div className="usa-grid usa-grid-full">
+            <h2>Debt Letters</h2>
+          </div>
+        )}
+      </RequiredLoginView>
+    );
+  }
+}
 
-export default DebtLettersWrapper;
+const mapStateToProps = state => ({
+  isFetching: state.debts.isFetching,
+  user: state.user,
+});
+
+const mapDispatchToProps = dispatch => ({
+  ...bindActionCreators({ fetchDebtLetters }, dispatch),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(DebtLettersWrapper);
