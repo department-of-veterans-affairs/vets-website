8bea6a711bfc006b16e6a88fd308dfe27f1728a1
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-05-28 16:16:00 -0400

    WIP

--- src/applications/static-pages/homepage-banner/HomepageBanner.jsx
+++ src/applications/static-pages/homepage-banner/HomepageBanner.jsx
@@ -1,38 +1,10 @@
 // Node modules.
 import React, { Component } from 'react';
-import * as Sentry from '@sentry/browser';
 import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
 import classnames from 'classnames';
-import sanitizeHtml from 'sanitize-html';
-import yaml from 'js-yaml';
 // Relative imports.
 import environment from 'platform/utilities/environment';
 import localStorage from 'platform/utilities/storage/localStorage';
-
-const VAGOV_CONTENT =
-  'https://raw.githubusercontent.com/department-of-veterans-affairs/vagov-content/master';
-
-const HOMEPAGE_BANNER_YML_LOCATION = `${VAGOV_CONTENT}/fragments/home/banner.yml?cache-break=${new Date().getTime()}`;
-
-const ACCEPTABLE_CONTENT_TAGS = [
-  'h4',
-  'h5',
-  'h6',
-  'blockquote',
-  'p',
-  'a',
-  'ul',
-  'ol',
-  'li',
-  'b',
-  'i',
-  'strong',
-  'em',
-  'hr',
-  'br',
-  'div',
-  'span',
-];
 
 const HOMEPAGE_BANNER_LOCALSTORAGE = 'HOMEPAGE_BANNER';
 
@@ -40,67 +12,17 @@ class HomepageBanner extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      banner: null,
-      dismissed: false,
+      dismissed: localStorage.getItem(HOMEPAGE_BANNER_LOCALSTORAGE) ===
+      this.prepareHomepageBannerID(),
     };
   }
 
-  async componentDidMount() {
-    try {
-      const banner = await this.getConfig();
-      this.processConfig(banner);
-    } catch (error) {
-      Sentry.withScope(scope => {
-        scope.setExtra('error', error);
-        Sentry.captureMessage(`Homepage Banner Error: ${error.message}`);
-      });
-    }
-  }
-
-  async getConfig() {
-    const response = await fetch(HOMEPAGE_BANNER_YML_LOCATION, {
-      mode: 'cors',
-    });
-
-    const homepageBannerYml = await response.text();
-    return yaml.safeLoad(homepageBannerYml);
-  }
-
-  processConfig(bannerConfig) {
-    const type = sanitizeHtml(bannerConfig.type, {
-      allowedTags: [],
-    });
-
-    const title = sanitizeHtml(bannerConfig.title, {
-      allowedTags: ['b', 'i', 'strong', 'em'],
-    });
-
-    const content = sanitizeHtml(bannerConfig.content, {
-      allowedTags: ACCEPTABLE_CONTENT_TAGS,
-      allowedSchemes: ['http', 'https', 'mailto', 'tel'],
-      allowedSchemesAppliedToAttributes: ['href', 'target', 'rel'],
-    });
-
-    const dismissed =
-      localStorage.getItem(HOMEPAGE_BANNER_LOCALSTORAGE) ===
-      this.prepareHomepageBannerID({ title, content });
-
-    const banner = {
-      visible: bannerConfig.visible,
-      type,
-      title,
-      content,
-    };
-
-    this.setState({ banner, dismissed });
-  }
-
-  prepareHomepageBannerID = banner => `${banner.title}:${banner.content}`;
+  prepareHomepageBannerID = () => `${this.props.bannerTitle}:${this.props.bannerContent}`;
 
   dismiss = () => {
     localStorage.setItem(
       HOMEPAGE_BANNER_LOCALSTORAGE,
-      this.prepareHomepageBannerID(this.state.banner),
+      this.prepareHomepageBannerID(),
     );
     this.setState({
       dismissed: true,
@@ -108,13 +30,14 @@ class HomepageBanner extends Component {
   };
 
   render() {
-    const { banner, dismissed } = this.state;
+    const {
+      bannerTitle: title,
+      bannerContent: content,
+      bannerType: type,
+      bannerVisible: visible,
+    } = this.props;
 
-    // Derive banner properties.
-    const content = banner?.content;
-    const title = banner?.title;
-    const type = banner?.type;
-    const visible = banner?.visible;
+    const { dismissed } = this.state;
 
     // Escape early if the banner isn't visible or is dismissed.
     if (!visible || dismissed) {
--- src/applications/static-pages/homepage-banner/createHomepageBanner.js
+++ src/applications/static-pages/homepage-banner/createHomepageBanner.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HomepageBanner from './HomepageBanner';
 
 export default function createHomepageBanner(store, widgetType) {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   if (root) {
-    import(/* webpackChunkName: "HomepageBanner" */
-    './HomepageBanner').then(module => {
-      const HomepageBanner = module.default;
-      ReactDOM.render(<HomepageBanner />, root);
-    });
+    const data = root.dataset;
+    ReactDOM.render(<HomepageBanner {...data} />, root);
   }
 }
