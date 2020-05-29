8bf2a063e8d2bd5f83e6836e6d7c05b5da92f197
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-05-29 11:12:29 -0400

    [Caregivers] Implement Flipper into the app

--- src/applications/caregivers/containers/App.jsx
+++ src/applications/caregivers/containers/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
+
+import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
+import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from '../config/form';
 
-export default ({ location, children }) => (
-  <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-    {children}
-  </RoutedSavableApp>
-);
+const App = ({ loading, isFormAvailable, location, children }) => {
+  if (loading) {
+    return <LoadingIndicator />;
+  }
+
+  if (isFormAvailable === false) {
+    return document.location.replace('/');
+  }
+
+  return (
+    <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+      {children}
+    </RoutedSavableApp>
+  );
+};
+
+const mapStateToProps = state => ({
+  loading: toggleValues(state).loading,
+  isFormAvailable: toggleValues(state)[
+    FEATURE_FLAG_NAMES.allowOnline1010cgSubmissions
+  ],
+});
+
+export default connect(mapStateToProps)(App);
