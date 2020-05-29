4ffa3ffb5eb6e48cd12d3a42036e7ffec9256f1e
Andrew Gunsch <andrew@gunsch.cc>
2020-05-29 15:28:43 -0700

    Example form

---
+++ src/applications/example-form/config/form.js
@@ -0,0 +1,56 @@
+// import fullSchema from 'vets-json-schema/dist/123-456-schema.json';
+
+import IntroductionPage from '../containers/IntroductionPage';
+import ConfirmationPage from '../containers/ConfirmationPage';
+
+// const { } = fullSchema.properties;
+
+// const { } = fullSchema.definitions;
+
+const formFields = {
+  firstName: 'firstName',
+};
+
+const formConfig = {
+  urlPrefix: '/',
+  submitUrl: '/v0/api',
+  trackingPrefix: 'example-form-',
+  introduction: IntroductionPage,
+  confirmation: ConfirmationPage,
+  formId: '123-456',
+  version: 0,
+  prefillEnabled: true,
+  savedFormMessages: {
+    notFound: 'Please start over to apply for example benefits.',
+    noAuth: 'Please sign in again to continue your application for example benefits.',
+  },
+  title: 'Example Form',
+  defaultDefinitions: {},
+  chapters: {
+    chapter1: {
+      title: 'Personal Information',
+      pages: {
+        page1: {
+          path: 'first-name',
+          title: 'Personal Information - Page 1',
+          uiSchema: {
+            [formFields.firstName]: {
+              'ui:title': 'First Name',
+            },
+          },
+          schema: {
+            required: [formFields.firstName],
+            type: 'object',
+            properties: {
+              [formFields.firstName]: {
+                type: 'string',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+export default formConfig;
