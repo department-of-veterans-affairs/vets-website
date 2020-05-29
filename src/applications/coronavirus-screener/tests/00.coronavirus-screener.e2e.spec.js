c2b313d2018569f3fcb76e9c8008028329691d71
Michael Chelen <michael.chelen@va.gov>
2020-05-29 12:07:45 -0400

    add test for incomplete status

--- src/applications/coronavirus-screener/tests/00.coronavirus-screener.e2e.spec.js
+++ src/applications/coronavirus-screener/tests/00.coronavirus-screener.e2e.spec.js
@@ -2,16 +2,15 @@ const Timeouts = require('platform/testing/e2e/timeouts');
 const E2eHelpers = require('platform/testing/e2e/helpers');
 const manifest = require('../manifest.json');
 
-module.exports = E2eHelpers.createE2eTest(client => {
-  client
+module.exports = E2eHelpers.createE2eTest(browser => {
+  browser
     .url(`${E2eHelpers.baseUrl}/covid19screen`)
     .waitForElementVisible('body', Timeouts.normal)
     .axeCheck('.main');
 
-  client.expect.element('div[id="question-fever"]').to.be.visible;
+  const incompleteClass = 'covid-screener-results-incomplete';
 
-  client.end();
+  browser.expect.element(`div.${incompleteClass}`).to.be.visible;
+
+  browser.end();
 });
-
-module.exports['@disabled'] =
-  !manifest.production || __BUILDTYPE__ !== 'production';
