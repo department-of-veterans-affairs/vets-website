2fcb242d74dffe841774026cacf87e2f5efbf9e2
Bill Fienberg <billfienberg@gmail.com>
2020-05-29 10:20:58 -0500

    Fix linting errors that are causing the additional-linting job to fail

--- src/applications/planning-guidance/8832/app-entry.jsx
+++ src/applications/planning-guidance/8832/app-entry.jsx
@@ -1,7 +1,7 @@
 import '../../../platform/polyfills';
 import './sass/8832-planning-and-guidance.scss';
 
-import startApp from '../../../platform/startup';
+import startApp from 'platform/startup';
 
 import routes from './routes';
 import reducer from './reducers';
