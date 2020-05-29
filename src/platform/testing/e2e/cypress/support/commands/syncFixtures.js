0ffd1778fd43298e5c13b3630f2f69a0a34d8af8
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-05-29 16:21:39 -0400

    Modify fixtures shorthand with cy.route when using cy.syncFixtures. (#12866)
    * Modify fixtures shorthand with cy.route when using cy.syncFixtures.
    
    * Updated comment.

--- src/platform/testing/e2e/cypress/support/commands/syncFixtures.js
+++ src/platform/testing/e2e/cypress/support/commands/syncFixtures.js
@@ -5,7 +5,8 @@ let initialized = false;
 
 /**
  * Runs task to sync fixtures under a temp path in the Cypress fixtures folder
- * then overwrites cy.fixture to look for fixtures under that temp path.
+ * then overwrites `cy.fixture` and the fixture shorthand in `cy.route`
+ * to look for fixtures under that temp path.
  */
 Cypress.Commands.add('syncFixtures', fixtures => {
   const args = { fixtures, initialized };
@@ -13,8 +14,25 @@ Cypress.Commands.add('syncFixtures', fixtures => {
 
   cy.task('_syncFixtures', args, opts).then(dir => {
     if (!initialized) {
+      // `cy.fixture` should look for fixtures under the temp path.
       Cypress.Commands.overwrite('fixture', (originalFn, path, options) =>
         originalFn(`${dir}/${path}`, options),
+      );
+
+      // The fixture shorthand in `cy.route` should be relative to temp path.
+      Cypress.Commands.overwrite(
+        'route',
+        (originalFn, method, url, response, options) => {
+          const modifiedResponse =
+            typeof response === 'string'
+              ? response.replace(
+                  /^(fx:|fixture:)/,
+                  (_, pattern) => `${pattern}${dir}/`,
+                )
+              : response;
+
+          return originalFn(method, url, modifiedResponse, options);
+        },
       );
 
       initialized = true;
