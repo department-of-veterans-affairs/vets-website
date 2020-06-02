3de50635a975ef76c1de20c7bd276cf4aa9869bd
Stephen Barrs <stephen.barrs@va.gov>
2020-06-02 15:54:29 -0400

    updated tests for staff or Veteran workflow

--- src/applications/coronavirus-screener/tests/00.coronavirus-screener.e2e.spec.js
+++ src/applications/coronavirus-screener/tests/00.coronavirus-screener.e2e.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 const Timeouts = require('platform/testing/e2e/timeouts');
 const E2eHelpers = require('platform/testing/e2e/helpers');
 const manifest = require('../manifest.json');
@@ -11,21 +12,37 @@ function resetBrowser(browser) {
     .url(`${E2eHelpers.baseUrl}/covid19screen`)
     .waitForElementVisible('body', Timeouts.normal);
 }
+
 function testIncompleteOnLoad(browser) {
   const incompleteClass = 'covid-screener-results-incomplete';
   browser.expect.element(`div.${incompleteClass}`).to.be.visible;
 }
 
-// tests end state when all answers are the same, e.g. yes or no
-function testEndStateForAnswer(browser, value) {
+function testIsStaffButton(browser, isStaff) {
+  const isStaffButton = `button[value=${isStaff}]`;
+  const firstScrollDiv = 'div[name=multi-question-form-1-scroll-element]';
+
+  browser.waitForElementVisible(isStaffButton, Timeouts.normal);
+  browser.click(isStaffButton);
+  browser.waitForElementPresent(firstScrollDiv, Timeouts.normal);
+}
+
+/* tests end state when all answers are the same, e.g. yes or no
+    @params: 
+      isStaff: yes|no - is the user a staff member
+      value: yes|no = the answer for all subsequent questions
+*/
+function testEndStateForAnswer(browser, value, isStaff) {
   resetBrowser(browser);
   const visibleResultClass = value === 'no' ? passClass : failClass;
   const notPresentResultClass = value === 'no' ? failClass : passClass;
   const buttonValue = `button[value=${value}]`;
 
-  browser.elements('css selector', buttonValue, links => {
-    for (let i = 0; i < links.value.length; i++) {
-      browser.pause(1000).elementIdClick(links.value[i].ELEMENT);
+  testIsStaffButton(browser, isStaff);
+
+  browser.elements('css selector', buttonValue, buttons => {
+    for (let i = 1; i < buttons.value.length; i++) {
+      browser.pause(1000).elementIdClick(buttons.value[i].ELEMENT);
     }
   });
   browser.expect.element(`div.${visibleResultClass}`).to.be.visible;
@@ -33,10 +50,13 @@ function testEndStateForAnswer(browser, value) {
   browser.axeCheck('.main');
 }
 
-function testEndStateForMixedAnswers(browser) {
+function testEndStateForMixedAnswers(browser, isStaff) {
   resetBrowser(browser);
+
+  testIsStaffButton(browser, isStaff);
+
   browser.elements('css selector', `button[value=no]`, links => {
-    for (let i = 0; i < links.value.length; i += 2) {
+    for (let i = 2; i < links.value.length; i += 2) {
       browser.pause(1000).elementIdClick(links.value[i].ELEMENT);
     }
   });
@@ -53,11 +73,15 @@ function testEndStateForMixedAnswers(browser) {
 module.exports = E2eHelpers.createE2eTest(browser => {
   resetBrowser(browser);
   browser.axeCheck('.main');
-
   testIncompleteOnLoad(browser);
-  testEndStateForAnswer(browser, 'no');
-  testEndStateForAnswer(browser, 'yes');
-  testEndStateForMixedAnswers(browser);
+
+  const yesNo = ['yes', 'no'];
+  yesNo.map(option => {
+    yesNo.map(option2 => {
+      testEndStateForAnswer(browser, option, option2);
+    });
+    testEndStateForMixedAnswers(browser, option);
+  });
 
   browser.end();
 });
