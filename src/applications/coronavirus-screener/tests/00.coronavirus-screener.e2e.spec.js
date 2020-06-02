794a09c63def93cfde5c16910f7674eba468a8d8
Stephen Barrs <stephen.barrs@va.gov>
2020-06-02 11:25:02 -0400

    added end state tests

--- src/applications/coronavirus-screener/tests/00.coronavirus-screener.e2e.spec.js
+++ src/applications/coronavirus-screener/tests/00.coronavirus-screener.e2e.spec.js
@@ -3,6 +3,27 @@ const E2eHelpers = require('platform/testing/e2e/helpers');
 const manifest = require('../manifest.json');
 const registry = require('applications/registry.json');
 
+// tests when all answers are the same, e.g. yes or no
+function testEndStateForAnswer(browser, value) {
+  const visibleResultClass =
+    value === 'no'
+      ? 'covid-screener-results-pass'
+      : 'covid-screener-results-fail';
+  const notPresentResultClass =
+    value === 'no'
+      ? 'covid-screener-results-fail'
+      : 'covid-screener-results-pass';
+
+  const buttonValue = `button[value=${value}]`;
+
+  browser.elements('css selector', buttonValue, links => {
+    for (let i = 0; i < links.value.length; i++) {
+      browser.pause(1000).elementIdClick(links.value[i].ELEMENT);
+    }
+    browser.expect.element(`div.${visibleResultClass}`).to.be.visible;
+    browser.expect.element(`div.${notPresentResultClass}`).to.not.be.present;
+  });
+}
 module.exports = E2eHelpers.createE2eTest(browser => {
   browser
     .url(`${E2eHelpers.baseUrl}/covid19screen`)
@@ -10,8 +31,10 @@ module.exports = E2eHelpers.createE2eTest(browser => {
     .axeCheck('.main');
 
   const incompleteClass = 'covid-screener-results-incomplete';
-
   browser.expect.element(`div.${incompleteClass}`).to.be.visible;
+
+  testEndStateForAnswer(browser, 'no');
+  testEndStateForAnswer(browser, 'yes');
 
   browser.end();
 });
