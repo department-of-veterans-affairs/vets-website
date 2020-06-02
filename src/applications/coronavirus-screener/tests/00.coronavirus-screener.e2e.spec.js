e7b500fb4ad0566471b3eded36c4224f875199ce
Stephen Barrs <stephen.barrs@va.gov>
2020-06-02 12:39:00 -0400

    added test for mixed answers, refactored

--- src/applications/coronavirus-screener/tests/00.coronavirus-screener.e2e.spec.js
+++ src/applications/coronavirus-screener/tests/00.coronavirus-screener.e2e.spec.js
@@ -3,38 +3,61 @@ const E2eHelpers = require('platform/testing/e2e/helpers');
 const manifest = require('../manifest.json');
 const registry = require('applications/registry.json');
 
-// tests when all answers are the same, e.g. yes or no
-function testEndStateForAnswer(browser, value) {
-  const visibleResultClass =
-    value === 'no'
-      ? 'covid-screener-results-pass'
-      : 'covid-screener-results-fail';
-  const notPresentResultClass =
-    value === 'no'
-      ? 'covid-screener-results-fail'
-      : 'covid-screener-results-pass';
+const passClass = 'covid-screener-results-pass';
+const failClass = 'covid-screener-results-fail';
 
+function resetBrowser(browser) {
+  browser
+    .url(`${E2eHelpers.baseUrl}/covid19screen`)
+    .waitForElementVisible('body', Timeouts.normal);
+}
+function testIncompleteOnLoad(browser) {
+  const incompleteClass = 'covid-screener-results-incomplete';
+  browser.expect.element(`div.${incompleteClass}`).to.be.visible;
+}
+
+// tests end state when all answers are the same, e.g. yes or no
+function testEndStateForAnswer(browser, value) {
+  resetBrowser(browser);
+  const visibleResultClass = value === 'no' ? passClass : failClass;
+  const notPresentResultClass = value === 'no' ? failClass : passClass;
   const buttonValue = `button[value=${value}]`;
 
   browser.elements('css selector', buttonValue, links => {
     for (let i = 0; i < links.value.length; i++) {
       browser.pause(1000).elementIdClick(links.value[i].ELEMENT);
     }
-    browser.expect.element(`div.${visibleResultClass}`).to.be.visible;
-    browser.expect.element(`div.${notPresentResultClass}`).to.not.be.present;
   });
+  browser.expect.element(`div.${visibleResultClass}`).to.be.visible;
+  browser.expect.element(`div.${notPresentResultClass}`).to.not.be.present;
+  browser.axeCheck('.main');
 }
+
+function testEndStateForMixedAnswers(browser) {
+  resetBrowser(browser);
+  browser.elements('css selector', `button[value=no]`, links => {
+    for (let i = 0; i < links.value.length; i += 2) {
+      browser.pause(1000).elementIdClick(links.value[i].ELEMENT);
+    }
+  });
+  browser.elements('css selector', `button[value=yes]`, links => {
+    for (let i = 1; i < links.value.length; i += 2) {
+      browser.pause(1000).elementIdClick(links.value[i].ELEMENT);
+    }
+  });
+  browser.expect.element(`div.${failClass}`).to.be.visible;
+  browser.expect.element(`div.${passClass}`).to.not.be.present;
+  browser.axeCheck('.main');
+}
+
 module.exports = E2eHelpers.createE2eTest(browser => {
-  browser
-    .url(`${E2eHelpers.baseUrl}/covid19screen`)
-    .waitForElementVisible('body', Timeouts.normal)
-    .axeCheck('.main');
+  resetBrowser(browser);
+  browser.axeCheck('.main');
 
-  const incompleteClass = 'covid-screener-results-incomplete';
-  browser.expect.element(`div.${incompleteClass}`).to.be.visible;
-
+  testIncompleteOnLoad(browser);
   testEndStateForAnswer(browser, 'no');
   testEndStateForAnswer(browser, 'yes');
+  testEndStateForMixedAnswers(browser);
 
   browser.end();
 });
