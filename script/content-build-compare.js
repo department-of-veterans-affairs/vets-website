ab7e1c5008893c27f586cc4e9e4d452c9a20f89e
Brooks Johnson <brooks.johnson@pm.me>
2020-06-01 17:13:58 -0700

    Create files containing hashes of each build
    The diff will come later

---
+++ script/content-build-compare.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const path = require('path');
+
+// Modeled after https://coderrocketfuel.com/article/recursively-list-all-the-files-in-a-directory-using-node-js
+function getAllFiles(dirPath, arrayOfFiles = []) {
+  const files = fs.readdirSync(dirPath);
+
+  files.forEach(file => {
+    if (fs.statSync(`${dirPath}/${file}`).isDirectory()) {
+      /* eslint-disable no-param-reassign */
+      arrayOfFiles = getAllFiles(`${dirPath}/${file}`, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(path.join(__dirname, dirPath, '/', file));
+    }
+  });
+
+  return arrayOfFiles;
+}
+
+function writeArrayToFile(arr, outputFile) {
+  const file = fs.createWriteStream(outputFile);
+  file.on('error', err => {
+    /* error handling */
+    throw err;
+  });
+
+  arr.forEach(({ filename, hash }) => {
+    file.write(`${hash} ${filename}\n`);
+  });
+  file.end();
+}
+
+function hashBuildOutput(outputDir, hashFile) {
+  const buildFiles = getAllFiles(outputDir);
+
+  const fileHashes = buildFiles.map(filename => {
+    const data = fs.readFileSync(filename, { encoding: 'utf8' });
+    const hash = crypto.createHash('md5');
+    hash.update(data);
+
+    return { filename, hash: hash.digest('hex') };
+  });
+
+  writeArrayToFile(fileHashes, hashFile);
+}
+
+hashBuildOutput('../build/localhost', 'buildOutput.txt');
+
+hashBuildOutput('../../content-build/build/localhost', 'buildOutput2.txt');
