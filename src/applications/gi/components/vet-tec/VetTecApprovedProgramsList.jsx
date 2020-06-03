9c99b1b30a9cff2a3f796adcea3b455321874b6d
Matt Roth <roth_matthew@bah.com>
2020-06-03 10:31:19 -0400

    switch to component with state in prep for additional work

--- src/applications/gi/components/vet-tec/VetTecApprovedProgramsList.jsx
+++ src/applications/gi/components/vet-tec/VetTecApprovedProgramsList.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ContactInformation from '../profile/ContactInformation';
 import { formatCurrency, isPresent } from '../../utils/helpers';
 import classNames from 'classnames';
 
-const programLength = program =>
-  isPresent(program.lengthInHours) && program.lengthInHours !== '0'
-    ? `${program.lengthInHours} hours`
-    : 'TBD';
+class VetTecApprovedProgramsList extends React.Component {
+  programLength = program =>
+    isPresent(program.lengthInHours) && program.lengthInHours !== '0'
+      ? `${program.lengthInHours} hours`
+      : 'TBD';
 
-const tuition = program =>
-  isPresent(program.tuitionAmount)
-    ? formatCurrency(program.tuitionAmount)
-    : 'TBD';
+  programTuition = program =>
+    isPresent(program.tuitionAmount)
+      ? formatCurrency(program.tuitionAmount)
+      : 'TBD';
 
-const isSelected = (program, selectedProgram) =>
-  selectedProgram &&
-  program.description.toLowerCase() === selectedProgram.toLowerCase();
+  isProgramSelected = (program, selectedProgram) =>
+    selectedProgram &&
+    program.description.toLowerCase() === selectedProgram.toLowerCase();
 
-function VetTecApprovedProgramsList({
-  selectedProgram,
-  institution: { programs },
-}) {
-  if (programs && programs.length) {
-    const programRows = programs.map((program, index) => {
-      const selected = isSelected(program, selectedProgram);
+  programRows = () => {
+    const {
+      selectedProgram,
+      institution: { programs },
+    } = this.props;
+
+    return programs.map((program, index) => {
+      const selected = this.isProgramSelected(program, selectedProgram);
       const programDescriptionClassNames = classNames('vads-l-col--10', {
         'vads-u-font-weight--bold': selected,
       });
@@ -42,15 +43,24 @@ function VetTecApprovedProgramsList({
             </div>
           </th>
           <td id="program-length" className="vads-u-padding-y--0">
-            {programLength(program)}
+            {this.programLength(program)}
           </td>
-          <td className="vads-u-padding-y--0">{tuition(program)}</td>
+          <td className="vads-u-padding-y--0">
+            {this.programTuition(program)}
+          </td>
         </tr>
       );
     });
+  };
 
-    const programList = programs.map((program, index) => {
-      const selected = isSelected(program, selectedProgram);
+  programList = () => {
+    const {
+      selectedProgram,
+      institution: { programs },
+    } = this.props;
+
+    return programs.map((program, index) => {
+      const selected = this.isProgramSelected(program, selectedProgram);
 
       const programDescriptionClassNames = classNames('program-description', {
         'vads-u-font-weight--bold': selected,
@@ -64,64 +74,72 @@ function VetTecApprovedProgramsList({
           </div>
           <div id="program-length">
             <b>Length: </b>
-            {programLength(program)}
+            {this.programLength(program)}
           </div>
           <div>
             <b>Tuition and fees: </b>
-            {tuition(program)}
+            {this.programTuition(program)}
           </div>
         </div>
       );
     });
+  };
 
+  render() {
+    const {
+      institution: { programs },
+    } = this.props;
+
+    if (programs && programs.length) {
+      return (
+        <div className="vads-u-margin-top--2">
+          <span>The following training programs are approved for VET TEC:</span>
+          <div className="vet-tec-programs-list vads-u-margin-top--4">
+            {this.programList()}
+          </div>
+          <table className="vet-tec-programs-table">
+            <colgroup>
+              <col className="name-col" />
+              <col className="hours-col" />
+              <col className="tuition-col" />
+            </colgroup>
+            <thead>
+              <tr>
+                <th scope="col" id="program-name-header">
+                  Program name
+                </th>
+                <th scope="col">Length</th>
+                <th scope="col">Tuition & Fees</th>
+              </tr>
+            </thead>
+            <tbody>{this.programRows()}</tbody>
+          </table>
+          <div className="vads-u-margin-top--4 vads-u-font-style--italic">
+            Showing {programs.length} of {programs.length} programs
+          </div>
+        </div>
+      );
+    }
     return (
-      <div className="vads-u-margin-top--2">
-        <span>The following training programs are approved for VET TEC:</span>
-        <div className="vet-tec-programs-list vads-u-margin-top--4">
-          {programList}
-        </div>
-        <table className="vet-tec-programs-table">
-          <colgroup>
-            <col className="name-col" />
-            <col className="hours-col" />
-            <col className="tuition-col" />
-          </colgroup>
-          <thead>
-            <tr>
-              <th scope="col" id="program-name-header">
-                Program name
-              </th>
-              <th scope="col">Length</th>
-              <th scope="col">Tuition & Fees</th>
-            </tr>
-          </thead>
-          <tbody>{programRows}</tbody>
-        </table>
-        <div className="vads-u-margin-top--4 vads-u-font-style--italic">
-          Showing {programs.length} of {programs.length} programs
-        </div>
+      <div>
+        <p>
+          Program data will be available for this provider soon.{' '}
+          <a
+            href={
+              '/education/about-gi-bill-benefits/how-to-use-benefits/vettec-high-tech-program/'
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn more about VET TEC programs
+          </a>
+        </p>
       </div>
     );
   }
-  return (
-    <div>
-      <p>
-        Program data will be available for this provider soon.{' '}
-        <a
-          href={
-            '/education/about-gi-bill-benefits/how-to-use-benefits/vettec-high-tech-program/'
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn more about VET TEC programs
-        </a>
-      </p>
-    </div>
-  );
 }
 
-ContactInformation.propTypes = {
+VetTecApprovedProgramsList.propTypes = {
   institution: PropTypes.object,
   selectedProgram: PropTypes.string,
 };
