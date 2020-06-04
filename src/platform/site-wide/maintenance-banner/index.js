4efe2688681bb0e3c75900c97a671b3c4a3e3578
Kelson Adams <kadams@governmentcio.com>
2020-06-04 11:49:01 -0600

    Fix linting issue

--- src/platform/site-wide/maintenance-banner/index.js
+++ src/platform/site-wide/maintenance-banner/index.js
@@ -9,11 +9,12 @@ import App from './components/App';
 // this React widget is used.
 export default data => {
   // Derive the root element to place the App.
-  const root = document.querySelector(`[data-widget-type="maintenance-banner"]`);
+  const root = document.querySelector(
+    `[data-widget-type="maintenance-banner"]`,
+  );
 
   // Escape early if there is no root element found.
   if (!root) {
-    console.log('data widget maintenance banner not found');
     return;
   }
 
