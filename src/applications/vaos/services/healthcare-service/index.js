d5db3f4fd1bc82d3612f88861a9c79a75a9a5315
Tony Williams <tony.williams@va.gov>
2020-06-04 13:14:37 -0500

    Modified per code review

---
+++ src/applications/vaos/services/healthcare-service/index.js
@@ -0,0 +1,37 @@
+import { getAvailableClinics } from '../../api';
+import { transformAvailableClinics } from './transformers';
+import { mapToFHIRErrors } from '../../utils/fhir';
+import { getLocation } from '../location';
+
+/*
+ * This is used to parse the fake FHIR ids we create for organizations
+ */
+function parseId(id) {
+  return id.replace('var', '');
+}
+
+export async function getAvailableHealthcareServices(
+  facilityId,
+  typeOfCareId,
+  systemId,
+) {
+  try {
+    const clinics = await getAvailableClinics(
+      parseId(facilityId),
+      typeOfCareId,
+      systemId,
+    );
+
+    return transformAvailableClinics(
+      parseId(facilityId),
+      typeOfCareId,
+      clinics,
+    );
+  } catch (e) {
+    if (e.errors) {
+      throw mapToFHIRErrors(e.errors);
+    }
+
+    throw e;
+  }
+}
---
+++ src/applications/vaos/tests/services/healthcare-service/index.unit.spec.js
@@ -0,0 +1,67 @@
+import { expect } from 'chai';
+import {
+  mockFetch,
+  setFetchJSONResponse,
+  setFetchJSONFailure,
+  mockMultipleApiRequests,
+} from 'platform/testing/unit/helpers';
+
+import { getLocation } from '../../../services/location';
+import { getAvailableHealthcareServices } from '../../../services/healthcare-service';
+import facilities983 from '../../../api/facilities_983.json';
+import clinicList983 from '../../../api/clinicList983.json';
+
+describe('VAOS Healthcare service', () => {
+  before(() => {
+    mockFetch();
+    setFetchJSONResponse(global.fetch, clinicList983);
+  });
+
+  describe('getAvailableHealthcareServices', () => {
+    it('should make successful request', async () => {
+      const data = await getAvailableHealthcareServices('var983', '123', '456');
+
+      expect(global.fetch.firstCall.args[0]).to.contain(
+        '/v0/facilities/983/clinics?type_of_care_id=123&system_id=456',
+      );
+    });
+
+    it('should set identifier', async () => {
+      const data = await getAvailableHealthcareServices('var983', '123', '456');
+
+      expect(data[0].identifier[0].value).to.equal(
+        'urn:va:healthcareservice:983:983:308',
+      );
+    });
+
+    it('should set providedBy', async () => {
+      const data = await getAvailableHealthcareServices('var983', '123', '456');
+
+      expect(data[2].providedBy).to.equal('Organization/var983');
+    });
+
+    it('should set location', async () => {
+      const data = await getAvailableHealthcareServices('var983', '123', '456');
+
+      expect(data[2].location.reference).to.equal('Location/var983');
+    });
+
+    it('should set serviceType', async () => {
+      const data = await getAvailableHealthcareServices('var983', '123', '456');
+
+      expect(data[2].serviceType[0].type.coding.code).to.equal('123');
+    });
+
+    it('should set service name to clinic name', async () => {
+      const data = await getAvailableHealthcareServices('var983', '123', '456');
+
+      expect(data[1].serviceName).to.equal('CHY PC CASSIDY');
+    });
+
+    it('should set service name to friendly name when present', async () => {
+      const data = await getAvailableHealthcareServices('var983', '123', '456');
+
+      expect(data[0].serviceName).to.equal('Green Team Clinic1');
+    });
+  });
+});
