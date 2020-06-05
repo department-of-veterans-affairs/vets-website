9395340e3da6ea972b0272edf60f93606d2313ea
Brooks Johnson <2008881+bkjohnson@users.noreply.github.com>
2020-06-05 14:23:18 -0700

    Content build validation (#12958)
    * Create files containing hashes of each build
    
    The diff will come later
    
    * Write the relative path from build dir, not full
    
    This allows for a more accurate diff
    
    * Only hash the html build files
    
    The JS & CSS files can be ignored since those aren't affected by the
    content build
    
    * Add an arg for buildtype
    
    * Compare the arrays for equality
    
    This might be easier than running a diff command
    
    * Add an npm script for hashing & comparing
    
    This is a bit hacky having both the diff and the array isEqual check.
    I'll try to simplify things
    
    * Choose better names for the files
    
    * Move file hashing into helper function
    
    * Shorten the map function

---
+++ script/content-build-compare.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const path = require('path');
+const { isEqual } = require('lodash');
+
+// Modeled after https://coderrocketfuel.com/article/recursively-list-all-the-files-in-a-directory-using-node-js
+function getAllFiles(dirPath, arrayOfFiles = []) {
+  const files = fs.readdirSync(dirPath);
+
+  files.forEach(file => {
+    if (fs.statSync(`${dirPath}/${file}`).isDirectory()) {
+      /* eslint-disable no-param-reassign */
+      arrayOfFiles = getAllFiles(`${dirPath}/${file}`, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(path.join(dirPath, '/', file));
+    }
+  });
+
+  return arrayOfFiles;
+}
+
+/**
+ * Writes the array of filenames & hashes to a file
+ */
+function writeArrayToFile(arr, outputFile) {
+  const file = fs.createWriteStream(outputFile);
+  file.on('error', err => {
+    /* error handling */
+    throw err;
+  });
+
+  arr.forEach(({ filename, hash }) => {
+    file.write(`${hash} ${filename}\n`);
+  });
+  file.end();
+}
+
+/**
+ * Reads a file and returns its md5 hash
+ */
+function getFileHash(filename) {
+  const data = fs.readFileSync(filename, { encoding: 'utf8' });
+  const hash = crypto.createHash('md5');
+  hash.update(data);
+
+  return hash.digest('hex');
+}
+
+/**
+ * Hash all of the build files in the outputDir and create a file
+ * listing the hash for each build file
+ */
+function hashBuildOutput(outputDir, hashFile) {
+  // Get only the HTML build files
+  const buildFiles = getAllFiles(outputDir).filter(
+    filename => filename.split('.').slice(-1)[0] === 'html',
+  );
+
+  // Create a list of filenames and their hashed contents
+  const fileHashes = buildFiles.map(filename => ({
+    filename: path.relative(outputDir, filename),
+    hash: getFileHash(filename),
+  }));
+
+  writeArrayToFile(fileHashes, hashFile);
+  return fileHashes;
+}
+
+function compareBuilds(buildtype) {
+  const websiteContentBuild = hashBuildOutput(
+    path.join(__dirname, `../build/${buildtype}`),
+    'websiteContentBuildHash.txt',
+  );
+  const standaloneContentBuild = hashBuildOutput(
+    path.join(__dirname, `../../content-build/build/${buildtype}`),
+    'standaloneContentBuildHash.txt',
+  );
+
+  /* eslint-disable no-console */
+  if (isEqual(websiteContentBuild, standaloneContentBuild)) {
+    console.log('The content builds match!');
+  } else {
+    console.log('The content builds do not match');
+  }
+  /* eslint-enable no-console */
+}
+
+compareBuilds('localhost');
