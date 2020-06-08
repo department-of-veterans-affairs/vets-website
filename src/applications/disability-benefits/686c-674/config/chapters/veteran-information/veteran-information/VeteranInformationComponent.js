1a1fae6be35d36f5ca122665ab1a75a1b3d15160
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-06-08 13:07:33 -0700

    Adjusted for accessibility

--- src/applications/disability-benefits/686c-674/config/chapters/veteran-information/veteran-information/VeteranInformationComponent.js
+++ src/applications/disability-benefits/686c-674/config/chapters/veteran-information/veteran-information/VeteranInformationComponent.js
@@ -45,10 +45,8 @@ const VeteranInformationComponent = ({
       <p>
         <strong>Note:</strong> If you need to update your personal information,
         please call Veterans Benefits Assistance at{' '}
-        <a aria-label="800. 8 2 7. 1000" href="tel:800 827 1000">
-          800-827-1000
-        </a>{' '}
-        between 8:00 a.m. and 9:00 p.m. ET Monday through Friday.
+        <a href="tel:800 827 1000">800-827-1000</a> between 8:00 a.m. and 9:00
+        p.m. ET Monday through Friday.
       </p>
     </div>
   );
