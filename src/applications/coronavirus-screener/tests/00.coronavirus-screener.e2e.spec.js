99843a53b394334651a74f8112625cad9fbbb4ca
Michael Chelen <michael.chelen@va.gov>
2020-06-08 10:42:30 -0400

    test appearance of following questions

--- src/applications/coronavirus-screener/tests/00.coronavirus-screener.e2e.spec.js
+++ src/applications/coronavirus-screener/tests/00.coronavirus-screener.e2e.spec.js
@@ -1,15 +1,38 @@
-const Timeouts = require('../../../platform/testing/e2e/timeouts');
-const E2eHelpers = require('../../../platform/testing/e2e/helpers');
-const manifest = require('../manifest.json');
+import { normal, slow } from 'platform/testing/e2e/timeouts';
+import { createE2eTest, baseUrl } from 'platform/testing/e2e/helpers';
+import { production } from '../manifest.json';
 
-module.exports = E2eHelpers.createE2eTest(client => {
+export default createE2eTest(client => {
   client
-    .url(`${E2eHelpers.baseUrl}/covid19screen`)
-    .waitForElementVisible('body', Timeouts.normal)
+    .url(`${baseUrl}/covid19screen`)
+    .waitForElementVisible('body', normal)
+    .assert.visible('div[id=question-isStaff]')
+    .assert.visible('div[class*=covid-screener-results-incomplete]')
     .axeCheck('.main');
+
+  // answering question brings up next question
+  client
+    .pause(1000) // needed for click to work
+    .click('div[id=question-isStaff] > button[value=no]')
+    .waitForElementVisible('div[id=question-fever]', slow)
+    .assert.visible('div[id=question-fever]')
+
+    .pause(1000) // needed for click to work
+    .click('div[id=question-fever] > button[value=no]')
+    .waitForElementVisible('div[id=question-cough]', slow)
+    .assert.visible('div[id=question-cough]')
+
+    .pause(1000) // needed for click to work
+    .click('div[id=question-cough] > button[value=no]')
+    .waitForElementVisible('div[id=question-flu]', slow)
+    .assert.visible('div[id=question-flu]')
+
+    .pause(1000) // needed for click to work
+    .click('div[id=question-flu] > button[value=no]')
+    .waitForElementVisible('div[id=question-congestion]', slow)
+    .assert.visible('div[id=question-congestion]');
 
   client.end();
 });
 
-module.exports['@disabled'] =
-  !manifest.production || __BUILDTYPE__ !== 'production';
+// module.exports['@disabled'] = !production || __BUILDTYPE__ !== 'production';
