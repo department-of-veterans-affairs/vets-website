d21509db3612ffec7500dfc86735d86e44422381
Sandra Hallie <sandra.adamshallie@gmail.com>
2020-06-09 08:29:22 -0600

    Fixes

--- src/applications/personalization/profile-2/components/connected-apps/AppDeletedAlert.jsx
+++ src/applications/personalization/profile-2/components/connected-apps/AppDeletedAlert.jsx
@@ -5,27 +5,28 @@ import { focusElement } from 'platform/utilities/ui';
 
 import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
 
-export const AppDeletedAlert = props => {
-  useEffect(() => {
+export class AppDeletedAlert extends Component {
+  componentDidMount() {
     focusElement('[data-focus-target]');
-  }, []);
+  }
 
   const { id, title, dismissAlert } = props;
   const alertMessage = `${title} won’t be able to see any new information
   about you from VA, but may still have access to information that was previously
   shared. To remove any stored data, contact ${title} and request permanent deletion`;
 
-  return (
-    <div tabIndex="-1" data-focus-target>
-      <AlertBox
-        status="success"
-        headline="This app has been disconnected"
-        content={alertMessage}
-        onCloseAlert={() => dismissAlert(id)}
-      />
-    </div>
-  );
-};
+    return (
+      <div tabIndex="-1" data-focus-target>
+        <AlertBox
+          status="success"
+          headline="This app has been disconnected"
+          content={alertMessage}
+          onCloseAlert={() => dismissAlert(id)}
+        />
+      </div>
+    );
+  }
+}
 
 AppDeletedAlert.propTypes = {
   id: PropTypes.string.isRequired,
