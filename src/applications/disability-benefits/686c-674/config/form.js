a07239ebdda40a9df2584f8934fe86e6e63278cf
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-06-09 11:16:34 -0700

    Fixed config file

--- src/applications/disability-benefits/686c-674/config/form.js
+++ src/applications/disability-benefits/686c-674/config/form.js
@@ -1,13 +1,14 @@
 import fullSchema from 'vets-json-schema/dist/686C-674-schema.json';
-
 import environment from 'platform/utilities/environment';
 import preSubmitInfo from 'platform/forms/preSubmitInfo';
 import { VA_FORM_IDS } from 'platform/forms/constants';
 import { TASK_KEYS, MARRIAGE_TYPES } from './constants';
 import { isChapterFieldRequired } from './helpers';
-
+import { customTransformForSubmit } from './utilities';
 import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
+import FormFooter from 'platform/forms/components/FormFooter';
+import GetFormHelp from '../components/GetFormHelp.jsx';
 
 // Chapter imports
 import { formerSpouseInformation } from './chapters/report-divorce';
@@ -52,37 +53,30 @@ import {
   studentNetworthInformation,
 } from './chapters/674';
 
-function prefillTransformer(pages, formData, metadata) {
-  console.log('the pages are ');
-  console.log(pages);
-
-  console.log('The formData is ');
-  console.log(formData);
-}
-
-const emptyMigration = savedData => savedData;
-const migrations = [emptyMigration];
-
 const formConfig = {
   urlPrefix: '/',
-  submitUrl: `${environment.API_URL}/v0/21-686c`,
+  // NOTE: e2e tests will fail until the dependents_applications endpoint gets merged in to vets-api.
+  // All e2e tests will be disabled until then. If you need to run an e2e test, temporarily change
+  // dependents_appilcations to 21-686c.
+  submitUrl: `${environment.API_URL}/v0/dependents_applications`,
   trackingPrefix: 'disability-21-686c',
   introduction: IntroductionPage,
   confirmation: ConfirmationPage,
   preSubmitInfo,
   formId: VA_FORM_IDS.FORM_21_686C,
-  version: migrations.length,
-  migrations,
+  version: 0,
   prefillEnabled: true,
-  prefillTransformer,
-  verifyRequiredPrefill: true,
+  footerContent: FormFooter,
+  getHelp: GetFormHelp,
   savedFormMessages: {
     notFound: 'Please start over to apply for declare or remove a dependent.',
     noAuth:
       'Please sign in again to continue your application for declare or remove a dependent.',
   },
-  title: 'New 686',
+  title: 'Add or remove dependents from your VA benefits',
+  subTitle: 'VA Form 21-686c (and 21-674)',
   defaultDefinitions: { ...fullSchema.definitions },
+  transformForSubmit: customTransformForSubmit,
   chapters: {
     optionSelection: {
       title: 'What do you want to do?',
@@ -384,17 +378,16 @@ const formConfig = {
           uiSchema: studentLastTerm.uiSchema,
           schema: studentLastTerm.schema,
         },
+        // NOTE: These are temporarily disabled, and will be reintroduced post-launch as part of 674 pension support.
         studentIncomeInformation: {
-          depends: formData =>
-            isChapterFieldRequired(formData, TASK_KEYS.report674),
+          depends: () => false,
           title: 'Information needed to add a student 18 to 23 years old',
           path: 'report-674-student-income-information',
           uiSchema: studentIncomeInformation.uiSchema,
           schema: studentIncomeInformation.schema,
         },
         studentNetworthInformation: {
-          depends: formData =>
-            isChapterFieldRequired(formData, TASK_KEYS.report674),
+          depends: () => false,
           title: 'Information needed to add a student 18 to 23 years old',
           path: 'report-674-student-networth-information',
           uiSchema: studentNetworthInformation.uiSchema,
