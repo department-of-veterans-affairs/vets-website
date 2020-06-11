975977138088b993eb09142a8c706a80560532d4
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-06-11 16:24:33 -0500

    use baseUrl on tests

--- src/applications/facility-locator/tests/00-required.e2e.spec.js
+++ src/applications/facility-locator/tests/00-required.e2e.spec.js
@@ -3,9 +3,7 @@ const Timeouts = require('../../../platform/testing/e2e/timeouts.js');
 const FacilityHelpers = require('./facility-helpers');
 
 module.exports = E2eHelpers.createE2eTest(client => {
-  // Don't know why using the testing instance does not load
-  // Use staging ?
-  client.openUrl(`https://staging.va.gov/find-locations/`);
+  client.openUrl(`${E2eHelpers.baseUrl}/find-locations/`);
 
   E2eHelpers.overrideSmoothScrolling(client);
   FacilityHelpers.initApplicationMock();
