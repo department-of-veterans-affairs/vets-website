210508543d5b8e662c652b6c05f6bc2557ba5aad
Justin Linn <justinslinn727@gmail.com>
2020-06-11 12:03:16 -0400

    Linn js qa feedback (#13092)
    * First pass at QA feedback

--- src/applications/caregivers/components/PreSubmitInfo/components/SignatureInput/index.jsx
+++ src/applications/caregivers/components/PreSubmitInfo/components/SignatureInput/index.jsx
@@ -3,31 +3,38 @@ import ErrorableTextInput from '@department-of-veterans-affairs/formation-react/
 
 const SignatureInput = ({ fullName, required, label, setIsSigned }) => {
   const inputRef = useRef(null);
+  const firstName = fullName.first?.toLowerCase();
+  const lastName = fullName.last?.toLowerCase();
+  const middleName = fullName.middle?.toLowerCase();
+  const firstLetterOfMiddleName = middleName ? middleName.charAt(0) : '';
+  const signatureRegEx = new RegExp(
+    `${firstName}(\\s{1,4}|\\s{0,4}${firstLetterOfMiddleName}\\s{0,4}|\\s{0,4}${middleName}\\s{0,4})${lastName}`,
+  );
   const [signature, setSignature] = useState({
     value: '',
     dirty: false,
   });
   const [hasError, setError] = useState(false);
-  const firstAndLast =
-    signature.value.startsWith(fullName.first) &&
-    signature.value.endsWith(fullName.last);
+  const hasSignatureMatched = signatureRegEx.test(
+    signature.value?.toLowerCase(),
+  );
 
   useEffect(
     () => {
       const isDirty = signature.dirty;
       const activeElementId = document.activeElement.id;
       const inputId = inputRef.current.inputId;
-      setIsSigned(firstAndLast);
+      setIsSigned(hasSignatureMatched);
 
-      if (isDirty && activeElementId !== inputId && !firstAndLast) {
+      if (isDirty && activeElementId !== inputId && !hasSignatureMatched) {
         setError(true);
       }
 
-      if (firstAndLast) {
+      if (hasSignatureMatched) {
         setError(false);
       }
     },
-    [setIsSigned, signature, firstAndLast],
+    [setIsSigned, signature, hasSignatureMatched],
   );
 
   return (
