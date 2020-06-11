c2da667b34044682654d7def8c2522018bb1683e
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-06-11 12:03:47 -0400

    Moved Cypress config and enabled multiple reporters. Added NPM scripts.

--- script/run-cypress-tests-docker.sh
+++ script/run-cypress-tests-docker.sh
@@ -2,5 +2,5 @@ if [ "$(find src -name '*.cypress.spec.js' | wc -l)" -eq 0 ]; then
   echo "No Cypress tests found."
   exit 0
 else
-  yarn cypress run --config baseUrl=http://vets-website:3001
+  yarn cy:run --config baseUrl=http://vets-website:3001
 fi
