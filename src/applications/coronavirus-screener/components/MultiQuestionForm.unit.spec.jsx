d480e8fc4b56f208c3ad4aaf4aebe891f2f28e87
Michael Chelen <michael.chelen@va.gov>
2020-06-12 11:21:54 -0400

    add initial unit test for <MultiQuestionForm>

---
+++ src/applications/coronavirus-screener/components/MultiQuestionForm.unit.spec.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import MultiQuestionForm from './MultiQuestionForm';
+import sinon from 'sinon';
+
+let mockQuestions;
+let mockDefaultOptions;
+
+beforeEach(() => {
+  mockQuestions = [
+    {
+      id: 'myFirstQuestion',
+      text: 'This is the first question text',
+      startQuestion: true,
+    },
+    {
+      id: 'mySecondQuestion',
+      text: 'This is the second question text',
+    },
+  ];
+  mockDefaultOptions = [
+    { optionValue: 'yes', optionText: 'Yes' },
+    { optionValue: 'no', optionText: 'No' },
+  ];
+});
+
+describe('coronavirus-screener', () => {
+  describe('MultiQuestionForm', () => {
+    it('outputs first question text', () => {
+      const wrapper = shallow(
+        <MultiQuestionForm
+          questions={mockQuestions}
+          defaultOptions={mockDefaultOptions}
+        />,
+      );
+
+      const firstQuestionId = `#question-${mockQuestions[0].id}`;
+      const firstQuestionText = mockQuestions[0].text;
+
+      // doesn't work
+      // const firstQuestion = wrapper.find(firstQuestionId);
+
+      const firstQuestion = wrapper.render().find(firstQuestionId);
+
+      expect(firstQuestion.find('h2').text()).to.equal(firstQuestionText);
+
+      wrapper.unmount();
+    });
+  });
+});
