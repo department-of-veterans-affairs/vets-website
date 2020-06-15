e6a049f8a6212315efcdc42c8c2fad0dd9fb3833
Robin Garrison <robin.garrison@adhocteam.us>
2020-06-15 11:06:37 -0500

    Update unit test

--- src/applications/disability-benefits/all-claims/tests/containers/Form526EZApp.unit.spec.jsx
+++ src/applications/disability-benefits/all-claims/tests/containers/Form526EZApp.unit.spec.jsx
@@ -114,7 +114,7 @@ describe('Form 526EZ Entry Page', () => {
     });
     expect(tree.find('main')).to.have.lengthOf(0);
     expect(tree.find('AlertBox')).to.have.lengthOf(1);
-    expect(tree.find('AlertBox').text()).to.contain('missing some information');
+    expect(tree.find('AlertBox').text()).to.contain('need some information');
     tree.unmount();
   });
 
