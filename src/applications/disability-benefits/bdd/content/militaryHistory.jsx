8037a34601293f0ef8725c39a35a7e6e5bf2c2e4
Nicholas Sprinkle <ndsprinkle@gmail.com>
2020-06-16 14:38:22 -0400

    Separation location description typos

--- src/applications/disability-benefits/bdd/content/militaryHistory.jsx
+++ src/applications/disability-benefits/bdd/content/militaryHistory.jsx
@@ -9,13 +9,13 @@ export const SeparationLocationDescription = (
     <AdditionalInfo triggerText="What's this">
       <>
         <p>
-          Your place of anticipated separation is th location where you expect
+          Your place of anticipated separation is the location where you expect
           to be separated from your military service. Usually, service members
           separate from their last active duty station, or the nearest regional
           facility that handles separation details.
         </p>
-        Providing us with the separation location allow us to more easily gather
-        details to help process your claim (such as the exact date you
+        Providing us with the separation location allows us to more easily
+        gather details to help process your claim (such as the exact date you
         separated).
       </>
     </AdditionalInfo>
