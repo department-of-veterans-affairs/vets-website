5ad0d0938d08352a447f0b0e80b075722ec7291c
Sandra Hallie <sandra.adamshallie@gmail.com>
2020-06-18 08:41:34 -0600

    Convert to functional component

--- src/applications/personalization/profile-2/components/personal-information/PersonalInformation.jsx
+++ src/applications/personalization/profile-2/components/personal-information/PersonalInformation.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Prompt } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -7,41 +7,41 @@ import { focusElement } from 'platform/utilities/ui';
 
 import PersonalInformationContent from './PersonalInformationContent';
 
-class PersonalInformation extends Component {
-  componentDidMount() {
+const PersonalInformation = ({ hasUnsavedEdits }) => {
+  useEffect(() => {
     focusElement('[data-focus-target]');
-  }
+  }, []);
 
-  componentDidUpdate() {
-    // Show alert when navigating away.
-    if (this.props.hasUnsavedEdits) {
-      window.onbeforeunload = () => true;
-      return;
-    }
+  useEffect(
+    () => {
+      // Show alert when navigating away
+      if (hasUnsavedEdits) {
+        window.onbeforeunload = () => true;
+        return;
+      }
 
-    // Do not show alert when navigating away.
-    window.onbeforeunload = undefined;
-  }
+      window.onbeforeunload = undefined;
+    },
+    [hasUnsavedEdits],
+  );
 
-  render() {
-    return (
-      <>
-        <Prompt
-          message="Are you sure you want to leave? If you leave, your in-progress work won't be saved."
-          when={this.props.hasUnsavedEdits}
-        />
-        <h2
-          tabIndex="-1"
-          className="vads-u-margin-y--2 medium-screen:vads-u-margin-bottom--4 medium-screen:vads-u-margin-top--3"
-          data-focus-target
-        >
-          Personal and contact information
-        </h2>
-        <PersonalInformationContent />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Prompt
+        message="Are you sure you want to leave? If you leave, your in-progress work won't be saved."
+        when={hasUnsavedEdits}
+      />
+      <h2
+        tabIndex="-1"
+        className="vads-u-margin-y--2 medium-screen:vads-u-margin-bottom--4 medium-screen:vads-u-margin-top--3"
+        data-focus-target
+      >
+        Personal and contact information
+      </h2>
+      <PersonalInformationContent />
+    </>
+  );
+};
 
 PersonalInformation.propTypes = {
   hasUnsavedEdits: PropTypes.bool.isRequired,
