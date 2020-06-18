107aff8e8103aee04fbe47501dd427833ae7cd34
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-06-17 17:28:34 -0700

    Added more pages

--- src/applications/vre/28-8832/config/form.js
+++ src/applications/vre/28-8832/config/form.js
@@ -4,6 +4,9 @@ import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
 
 import { veteranInformation } from './chapters/veteran-information';
+import { militaryHistory } from './chapters/military-history';
+import { dependentInformation } from './chapters/dependent-information';
+import { additionalInformation } from './chapters/additional-information';
 
 const formConfig = {
   urlPrefix: '/',
@@ -22,14 +25,47 @@ const formConfig = {
   title: '28-8832-planning-and-guidance',
   defaultDefinitions: {},
   chapters: {
-    chapter1: {
+    veteranDetails: {
       title: 'Personal Information',
       pages: {
-        page1: {
+        veteranInformation: {
           path: 'first-name',
           title: 'Personal Information - Page 1',
           uiSchema: veteranInformation.uiSchema,
           schema: veteranInformation.schema,
+        },
+      },
+    },
+    militaryHistory: {
+      title: 'MIlitary History',
+      pages: {
+        militaryHistory: {
+          path: 'military-history',
+          title: 'Military History',
+          uiSchema: militaryHistory.uiSchema,
+          schema: militaryHistory.schema,
+        },
+      },
+    },
+    dependentInformation: {
+      title: 'Dependent Information',
+      pages: {
+        dependentInformation: {
+          path: 'dependent-information',
+          title: 'Dependent Information',
+          uiSchema: dependentInformation.uiSchema,
+          schema: dependentInformation.schema,
+        },
+      },
+    },
+    additionalInformation: {
+      title: 'Additional Information',
+      pages: {
+        additionalInformation: {
+          path: 'additional-information',
+          title: 'Additional Information',
+          uiSchema: additionalInformation.uiSchema,
+          schema: additionalInformation.schema,
         },
       },
     },
