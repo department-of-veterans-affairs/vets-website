cbd86225e722999803af5bc7e9fd550c3b664462
Christopher Valarida <chris.valarida@adhocteam.us>
2020-06-23 12:52:13 -0700

    Register more schema combinations for getDrupalValue

--- src/site/stages/build/process-cms-exports/field-transformers/getDrupalValue.js
+++ src/site/stages/build/process-cms-exports/field-transformers/getDrupalValue.js
@@ -5,7 +5,62 @@ const { getDrupalValue } = require('../transformers/helpers');
 const schemaMap = [
   {
     input: { $ref: 'GenericNestedString' },
-    output: [{ type: 'string' }],
+    output: [
+      { type: 'string' },
+      {
+        oneOf: [
+          {
+            $ref: 'ProcessedString',
+          },
+          {
+            type: 'null',
+          },
+        ],
+      },
+      // Unsure about this one
+      {
+        $ref: 'ProcessedString',
+      },
+      {
+        type: ['string', 'null'],
+      },
+      // This probably shouldn't have GenericNestedString as the input schema,
+      // but it's already in use like that
+      {
+        type: 'number',
+      },
+    ],
+  },
+  {
+    // Input schemas with this should probably use a common schema definition
+    input: {
+      items: {
+        type: 'object',
+        properties: {
+          value: {
+            type: 'string',
+          },
+          format: {
+            type: 'string',
+          },
+        },
+        required: ['value', 'format'],
+      },
+      maxItems: 1,
+      type: 'array',
+    },
+    output: [{ type: 'number' }],
+  },
+  {
+    input: { $ref: 'GenericNestedBoolean' },
+    output: [{ type: 'boolean' }],
+  },
+  {
+    input: {
+      maxItems: 0,
+      type: 'array',
+    },
+    output: [{ type: 'null' }, { type: ['string', 'null'] }],
   },
 ];
 
