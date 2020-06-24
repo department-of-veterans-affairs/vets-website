a16048ccf53209dd2ad8602dbf08b15b9a5dd57b
mr0sari0 <mrosario@governmentcio.com>
2020-06-24 14:12:05 -0400

    add content for if there are no eligible products

--- src/applications/disability-benefits/2346/components/ReviewPageSupplies.jsx
+++ src/applications/disability-benefits/2346/components/ReviewPageSupplies.jsx
@@ -6,6 +6,7 @@ const ReviewPageSupplies = ({
   accessorySupplies,
   selectedBatteryProductInfo,
   selectedAccessoryProductInfo,
+  eligibilty,
 }) => (
   <>
     <dt className="vads-u-display--flex vads-u-flex-direction--column vads-u-margin-bottom--2">
@@ -16,23 +17,31 @@ const ReviewPageSupplies = ({
         ({selectedBatteryProductInfo?.length || 0} out of{' '}
         {batterySupplies?.length || 0} selected)
       </span>
+      {!eligibilty?.batteries && (
+        <p className="vads-u-font-style--italic">
+          You can't add batteries to your order at this time due to
+          ineligibility
+        </p>
+      )}
     </dt>
-    <div className="vads-u-margin-bottom--3">
-      {selectedBatteryProductInfo &&
-        selectedBatteryProductInfo.map((product, index) => (
-          <dd
-            key={`${product.productId}_${index}`}
-            className="vads-u-background-color--gray-light-alt vads-u-width--full vads-u-padding--4 vads-u-display--flex vads-u-flex-direction--column vads-u-margin-bottom--1p5"
-          >
-            <h5 className="vads-u-font-size--h4 vads-u-margin-top--0">
-              {product.deviceName}
-            </h5>
-            <span>
-              {product.productName} batteries (Quantity: {product.quantity})
-            </span>
-          </dd>
-        ))}
-    </div>
+    {eligibilty?.batteries && (
+      <div className="vads-u-margin-bottom--3">
+        {selectedBatteryProductInfo &&
+          selectedBatteryProductInfo.map((product, index) => (
+            <dd
+              key={`${product.productId}_${index}`}
+              className="vads-u-background-color--gray-light-alt vads-u-width--full vads-u-padding--4 vads-u-display--flex vads-u-flex-direction--column vads-u-margin-bottom--1p5"
+            >
+              <h5 className="vads-u-font-size--h4 vads-u-margin-top--0">
+                {product.deviceName}
+              </h5>
+              <span>
+                {product.productName} batteries (Quantity: {product.quantity})
+              </span>
+            </dd>
+          ))}
+      </div>
+    )}
     <dd className="vads-u-display--flex vads-u-flex-direction--column vads-u-margin-bottom--2">
       <span className="vads-u-font-weight--bold">
         You have requested to receive the following accessories:
@@ -41,21 +50,29 @@ const ReviewPageSupplies = ({
         ({selectedAccessoryProductInfo?.length || 0} out of{' '}
         {accessorySupplies?.length || 0} selected)
       </span>
+      {!eligibilty?.accessories && (
+        <p className="vads-u-font-style--italic">
+          You can't add accessories to your order at this time due to
+          ineligibility
+        </p>
+      )}
     </dd>
-    <div className="vads-u-margin-bottom--3">
-      {selectedAccessoryProductInfo &&
-        selectedAccessoryProductInfo.map((product, index) => (
-          <dd
-            key={`${product.productId}_${index}`}
-            className="vads-u-background-color--gray-light-alt vads-u-width--full vads-u-padding--4 vads-u-display--flex vads-u-flex-direction--column vads-u-margin-bottom--1p5"
-          >
-            <h5 className="vads-u-font-size--h4 vads-u-margin-top--0">
-              {product.productName}
-            </h5>
-            <span>Quantity: {product.quantity}</span>
-          </dd>
-        ))}
-    </div>
+    {eligibilty?.accessories && (
+      <div className="vads-u-margin-bottom--3">
+        {selectedAccessoryProductInfo &&
+          selectedAccessoryProductInfo.map((product, index) => (
+            <dd
+              key={`${product.productId}_${index}`}
+              className="vads-u-background-color--gray-light-alt vads-u-width--full vads-u-padding--4 vads-u-display--flex vads-u-flex-direction--column vads-u-margin-bottom--1p5"
+            >
+              <h5 className="vads-u-font-size--h4 vads-u-margin-top--0">
+                {product.productName}
+              </h5>
+              <span>Quantity: {product.quantity}</span>
+            </dd>
+          ))}
+      </div>
+    )}
   </>
 );
 
@@ -81,6 +98,7 @@ const mapStateToProps = state => {
     accessorySupplies,
     selectedBatteryProductInfo,
     selectedAccessoryProductInfo,
+    eligibilty: state.form?.data?.eligibilty,
   };
 };
 
