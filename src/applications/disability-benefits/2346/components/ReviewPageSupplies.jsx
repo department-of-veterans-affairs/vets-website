92606527a59215b0b03d2bbafa31621904ce9213
mr0sari0 <mrosario@governmentcio.com>
2020-06-24 19:51:15 -0400

    add conditionals for accessories

--- src/applications/disability-benefits/2346/components/ReviewPageSupplies.jsx
+++ src/applications/disability-benefits/2346/components/ReviewPageSupplies.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+// import { getFormContext } from 'platform/forms/save-in-progress/selectors';
 
 const ReviewPageSupplies = ({
   batterySupplies,
@@ -7,6 +8,7 @@ const ReviewPageSupplies = ({
   selectedBatteryProductInfo,
   selectedAccessoryProductInfo,
   eligibility,
+  formContext,
 }) => (
   <>
     <dt className="vads-u-display--flex vads-u-flex-direction--column vads-u-margin-bottom--2">
@@ -20,9 +22,21 @@ const ReviewPageSupplies = ({
       {!eligibility?.batteries && (
         <p className="vads-u-font-style--italic">
           You can't add batteries to your order at this time due to
-          ineligibility
+          ineligibility.
         </p>
       )}
+      {eligibility?.batteries &&
+        selectedBatteryProductInfo?.length <= 0 && (
+          <p className="vads-u-font-style--italic">
+            No batteries added.{' '}
+            <button
+              className="va-button-link"
+              onClick={() => formContext.onEdit()}
+            >
+              Add batteries to your order
+            </button>
+          </p>
+        )}
     </dt>
     {eligibility?.batteries && (
       <div className="vads-u-margin-bottom--3">
@@ -53,30 +67,43 @@ const ReviewPageSupplies = ({
       {!eligibility?.accessories && (
         <p className="vads-u-font-style--italic">
           You can't add accessories to your order at this time due to
-          ineligibility
+          ineligibility.
         </p>
       )}
-    </dd>
-    {eligibility?.accessories && (
-      <div className="vads-u-margin-bottom--3">
-        {selectedAccessoryProductInfo &&
-          selectedAccessoryProductInfo.map((product, index) => (
-            <dd
-              key={`${product.productId}_${index}`}
-              className="vads-u-background-color--gray-light-alt vads-u-width--full vads-u-padding--4 vads-u-display--flex vads-u-flex-direction--column vads-u-margin-bottom--1p5"
+      {eligibility?.accessories &&
+        selectedAccessoryProductInfo?.length <= 0 && (
+          <p className="vads-u-font-style--italic">
+            No accessories added.{' '}
+            <button
+              className="va-button-link"
+              onClick={() => formContext.onEdit()}
             >
-              <h5 className="vads-u-font-size--h4 vads-u-margin-top--0">
-                {product.productName}
-              </h5>
-              <span>Quantity: {product.quantity}</span>
-            </dd>
-          ))}
-      </div>
-    )}
+              Add accessories to your order
+            </button>
+          </p>
+        )}
+    </dd>
+    {eligibility?.accessories &&
+      selectedAccessoryProductInfo?.length > 0 && (
+        <div className="vads-u-margin-bottom--3">
+          {selectedAccessoryProductInfo &&
+            selectedAccessoryProductInfo.map((product, index) => (
+              <dd
+                key={`${product.productId}_${index}`}
+                className="vads-u-background-color--gray-light-alt vads-u-width--full vads-u-padding--4 vads-u-display--flex vads-u-flex-direction--column vads-u-margin-bottom--1p5"
+              >
+                <h5 className="vads-u-font-size--h4 vads-u-margin-top--0">
+                  {product.productName}
+                </h5>
+                <span>Quantity: {product.quantity}</span>
+              </dd>
+            ))}
+        </div>
+      )}
   </>
 );
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
   const supplies = state.form?.data?.supplies;
   const batterySupplies = supplies?.filter(supply =>
     supply.productGroup?.includes('BATTERIES'),
@@ -99,6 +126,7 @@ const mapStateToProps = state => {
     selectedBatteryProductInfo,
     selectedAccessoryProductInfo,
     eligibility: state.form?.data?.eligibility,
+    formContext: ownProps.formContext,
   };
 };
 
