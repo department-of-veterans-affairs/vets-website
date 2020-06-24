5167a91772c4d073993aabb1b55f532d4fb17885
Devin McCurdy <mccurdy_devin@bah.com>
2020-06-24 16:23:01 -0400

    Test coverage

--- src/applications/edu-benefits/10203/helpers.jsx
+++ src/applications/edu-benefits/10203/helpers.jsx
@@ -1,15 +1,5 @@
-const isEdithNourseRogersScholarship = form =>
-  form.isEdithNourseRogersScholarship;
-
 export const isChapter33 = form =>
   form.benefit === 'chapter33' || form.benefit === 'fryScholarship';
-
-const isEligibleForEdithNourseRogersScholarship = form =>
-  (isChapter33(form) || form.benefit === undefined) &&
-  isEdithNourseRogersScholarship(form) &&
-  (form['view:exhaustionOfBenefits'] ||
-    form['view:exhaustionOfBenefitsAfterPursuingTeachingCert']) &&
-  (form.isEnrolledStem || form.isPursuingTeachingCert);
 
 export const displayConfirmEligibility = form =>
   !isChapter33(form) ||
---
+++ src/applications/edu-benefits/10203/tests/containers/ConfirmEligibilityView.unit.spec.jsx
@@ -0,0 +1,169 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { expect } from 'chai';
+
+import ConfirmEligibilityView from '../../containers/ConfirmEligibilityView';
+import createCommonStore from 'platform/startup/store';
+import { Provider } from 'react-redux';
+
+const createStore = (data = {}) =>
+  createCommonStore({
+    form: () => ({
+      data: {
+        benefit: 'chapter33',
+        isEnrolledStem: true,
+        isPursuingTeachingCert: false,
+        benefitLeft: 'none',
+        ...data,
+      },
+    }),
+  });
+const defaultStore = createStore();
+const defaultProps = {
+  ...defaultStore.getState(),
+  formData: {},
+  errorSchema: {
+    'view:confirmEligibility': {
+      __errors: [],
+    },
+  },
+};
+
+describe('<ConfirmEligibilityView>', () => {
+  it('should render', () => {
+    const tree = mount(
+      <Provider store={defaultStore}>
+        <ConfirmEligibilityView {...defaultProps} />
+      </Provider>,
+    );
+    expect(tree).to.not.be.undefined;
+    tree.unmount();
+  });
+
+  it('should display checkmark for correct benefit', () => {
+    const tree = mount(
+      <Provider store={defaultStore}>
+        <ConfirmEligibilityView {...defaultProps} />
+      </Provider>,
+    );
+    const icon = tree
+      .find('li')
+      .at(0)
+      .find('i');
+    expect(icon.hasClass('fa-check')).to.equal(true);
+    expect(icon.hasClass('vads-u-color--green')).to.equal(true);
+
+    tree.unmount();
+  });
+
+  it('should display X for incorrect benefit', () => {
+    const store = createStore({
+      benefit: 'other',
+    });
+    const tree = mount(
+      <Provider store={store}>
+        <ConfirmEligibilityView {...defaultProps} />
+      </Provider>,
+    );
+    const icon = tree
+      .find('li')
+      .at(0)
+      .find('i');
+
+    expect(icon.hasClass('fa-times')).to.equal(true);
+    expect(icon.hasClass('vads-u-color--gray-medium')).to.equal(true);
+
+    tree.unmount();
+  });
+
+  it('should display checkmark for correct isEnrolledStem', () => {
+    const tree = mount(
+      <Provider store={defaultStore}>
+        <ConfirmEligibilityView {...defaultProps} />
+      </Provider>,
+    );
+    const icon = tree
+      .find('li')
+      .at(1)
+      .find('i');
+    expect(icon.hasClass('fa-check')).to.equal(true);
+    expect(icon.hasClass('vads-u-color--green')).to.equal(true);
+
+    tree.unmount();
+  });
+
+  it('should display checkmark for correct isPursuingTeachingCert', () => {
+    const store = createStore({
+      isEnrolledStem: false,
+      isPursuingTeachingCert: true,
+    });
+    const tree = mount(
+      <Provider store={store}>
+        <ConfirmEligibilityView {...defaultProps} />
+      </Provider>,
+    );
+    const icon = tree
+      .find('li')
+      .at(1)
+      .find('i');
+    expect(icon.hasClass('fa-check')).to.equal(true);
+    expect(icon.hasClass('vads-u-color--green')).to.equal(true);
+
+    tree.unmount();
+  });
+
+  it('should display X for invalid enrollment', () => {
+    const store = createStore({
+      isEnrolledStem: false,
+      isPursuingTeachingCert: false,
+    });
+    const tree = mount(
+      <Provider store={store}>
+        <ConfirmEligibilityView {...defaultProps} />
+      </Provider>,
+    );
+    const icon = tree
+      .find('li')
+      .at(1)
+      .find('i');
+    expect(icon.hasClass('fa-times')).to.equal(true);
+    expect(icon.hasClass('vads-u-color--gray-medium')).to.equal(true);
+
+    tree.unmount();
+  });
+
+  it('should display checkmark for correct benefitLeft', () => {
+    const tree = mount(
+      <Provider store={defaultStore}>
+        <ConfirmEligibilityView {...defaultProps} />
+      </Provider>,
+    );
+    const icon = tree
+      .find('li')
+      .at(2)
+      .find('i');
+    expect(icon.hasClass('fa-check')).to.equal(true);
+    expect(icon.hasClass('vads-u-color--green')).to.equal(true);
+
+    tree.unmount();
+  });
+
+  it('should display X for incorrect benefitLeft', () => {
+    const store = createStore({
+      benefitLeft: 'moreThanSixMonths',
+    });
+    const tree = mount(
+      <Provider store={store}>
+        <ConfirmEligibilityView {...defaultProps} />
+      </Provider>,
+    );
+    const icon = tree
+      .find('li')
+      .at(2)
+      .find('i');
+    expect(icon.hasClass('fa-times')).to.equal(true);
+    expect(icon.hasClass('vads-u-color--gray-medium')).to.equal(true);
+
+    tree.unmount();
+  });
+});
