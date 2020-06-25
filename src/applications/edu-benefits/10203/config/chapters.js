1bb7ca2dad8a0b22ebc69d8180e7e04d13cdf237
elder-joseph <48804834+elder-joseph@users.noreply.github.com>
2020-06-25 11:07:37 -0400

    9790: Update name label text for 10203 (#13251)
    * 9790: Update name label text for 10203
    
    * Add ui:description messages
    
    * Make SSN required field

--- src/applications/edu-benefits/10203/config/chapters.js
+++ src/applications/edu-benefits/10203/config/chapters.js
@@ -1,21 +1,31 @@
 import fullSchema10203 from 'vets-json-schema/dist/22-10203-schema.json';
+import fullSchema1995 from 'vets-json-schema/dist/22-1995-schema.json';
 import createContactInformationPage from '../../pages/contactInformation';
 import createDirectDepositChangePage from '../../pages/directDepositChange';
 import createApplicantInformationPage from 'platform/forms/pages/applicantInformation';
 
 import { display10203StemFlow, displayStemEligibility } from '../helpers';
 
-import { activeDuty, benefitSelection, stem, stemEligibility } from '../pages';
+import {
+  applicantInformation,
+  activeDuty,
+  benefitSelection,
+  stem,
+  stemEligibility,
+} from '../pages';
 
 export const chapters = {
   applicantInformation: {
     title: 'Applicant Information',
     pages: {
-      applicantInformation: createApplicantInformationPage(fullSchema10203, {
-        isVeteran: true,
-        fields: ['veteranFullName', 'veteranSocialSecurityNumber'],
-        required: ['veteranFullName'],
-      }),
+      applicantInformation: {
+        ...createApplicantInformationPage(fullSchema10203, {
+          isVeteran: true,
+          fields: ['veteranFullName', 'veteranSocialSecurityNumber'],
+          required: ['veteranFullName', 'veteranSocialSecurityNumber'],
+        }),
+        uiSchema: applicantInformation.uiSchema,
+      },
     },
   },
   benefitSelection: {
