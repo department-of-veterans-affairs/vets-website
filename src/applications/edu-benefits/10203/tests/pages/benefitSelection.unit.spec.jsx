863eef5a33134b80b1b04453d6127405427e1c0e
Matt Roth <roth_matthew@bah.com>
2020-06-25 10:34:20 -0400

    10203 page unit tests

---
+++ src/applications/edu-benefits/10203/tests/pages/benefitSelection.unit.spec.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { findDOMNode } from 'react-dom';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+import ReactTestUtils from 'react-dom/test-utils';
+
+import {
+  DefinitionTester,
+  submitForm,
+} from 'platform/testing/unit/schemaform-utils.jsx';
+import formConfig from '../../../10203/config/form';
+
+describe('Edu 1995 benefitSelection', () => {
+  const {
+    schema,
+    uiSchema,
+  } = formConfig.chapters.benefitSelection.pages.benefitSelection;
+  it('renders the correct amount of options for the benefit selection radio button', () => {
+    const form = mount(
+      <DefinitionTester
+        definitions={formConfig.defaultDefinitions}
+        schema={schema}
+        uiSchema={uiSchema}
+      />,
+    );
+    expect(form.find('input').length).to.equal(6);
+    form.unmount();
+  });
+
+  it('should have no required inputs', () => {
+    const onSubmit = sinon.spy();
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+        schema={schema}
+        onSubmit={onSubmit}
+        data={{}}
+        uiSchema={uiSchema}
+      />,
+    );
+    const formDOM = findDOMNode(form);
+    submitForm(form);
+    expect(Array.from(formDOM.querySelectorAll('.usa-input-error'))).to.be
+      .empty;
+
+    submitForm(form);
+
+    expect(onSubmit.called).to.be.true;
+  });
+});
