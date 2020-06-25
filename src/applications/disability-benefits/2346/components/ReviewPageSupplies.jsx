8417c921b4536dbab0eca74fa04eb421f5889d7e
mr0sari0 <mrosario@governmentcio.com>
2020-06-24 20:23:09 -0400

    remove commented out import

--- src/applications/disability-benefits/2346/components/ReviewPageSupplies.jsx
+++ src/applications/disability-benefits/2346/components/ReviewPageSupplies.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import { getFormContext } from 'platform/forms/save-in-progress/selectors';
 
 const ReviewPageSupplies = ({
   batterySupplies,
