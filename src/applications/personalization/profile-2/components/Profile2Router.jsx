46ad40c08a59c82a33208505fad411a62b2e9b2c
Sandra Hallie <sandra.adamshallie@gmail.com>
2020-06-25 12:08:31 -0600

    Remove keys

--- src/applications/personalization/profile-2/components/Profile2Router.jsx
+++ src/applications/personalization/profile-2/components/Profile2Router.jsx
@@ -79,11 +79,7 @@ class Profile2Router extends Component {
                 (route.requiresMVI && !this.props.isInMVI)
               ) {
                 return (
-                  <Redirect
-                    from={route.path}
-                    key="/profile/account-security"
-                    to="/profile/account-security"
-                  />
+                  <Redirect from={route.path} to="/profile/account-security" />
                 );
               }
 
@@ -100,14 +96,12 @@ class Profile2Router extends Component {
             <Redirect
               exact
               from="/profile#contact-information"
-              key="/profile/personal-information"
               to="/profile/personal-information"
             />
 
             <Redirect
               exact
               from="/profile"
-              key="/profile/personal-information"
               to="/profile/personal-information"
             />
 
