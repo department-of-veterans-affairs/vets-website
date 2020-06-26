1e87b3f3ecc6f1b4b434072045b6b1ea382ee257
mr0sari0 <mrosario@governmentcio.com>
2020-06-26 10:38:49 -0400

    make MDOT cypress file run all tests

--- src/applications/disability-benefits/2346/tests/MDOT.cypress.spec.js
+++ src/applications/disability-benefits/2346/tests/MDOT.cypress.spec.js
@@ -9,232 +9,67 @@ const dataSetToUserMap = {
   noTempAddress: 'fx:users/markUserData',
   noBatteries: 'fx:users/jerryUserData',
   noAccessories: 'fx:users/eddieUserData',
-  noItemsEligible: 'fx:users/paulineUserData',
 };
-
-const happyPathPageHooks = {
-  introduction: () => {
-    cy.findAllByText(/order/i, { selector: 'button' })
-      .first()
-      .click();
-  },
-
-  address: () => {
-    cy.findAllByText('Edit permanent address', { selector: 'button' })
-      .first()
-      .click();
-    cy.findByLabelText(/Country/i).select('Canada');
-    cy.findByLabelText(/Province/i).type('Alberta');
-    cy.findByLabelText(/International Postal Code/i).type('T7N');
-    cy.findByText(/Save permanent address/i).click();
-    cy.findByLabelText(/Re-enter email address/i).type('vet@vet.com');
-    cy.findByText(/Continue/i).click();
-  },
-
-  supplies: () => {
-    cy.get('#1').click();
-    cy.get('#3').click();
-    cy.get('#5').click();
-    cy.findAllByText(/Continue/i, { selector: 'button' })
-      .first()
-      .click();
-  },
-};
-
-const noTempAddressPageHooks = {
-  introduction: () => {
-    cy.findAllByText(/order/i, { selector: 'button' })
-      .first()
-      .click();
-  },
-
-  address: () => {
-    cy.findAllByText('Add a temporary address', { selector: 'button' })
-      .first()
-      .click();
-    cy.findByLabelText(/Country/i).select('Canada');
-    cy.findByLabelText(/Street address/i).type('205 Test Lane');
-    cy.findByLabelText(/City/i).type('Calgary');
-    cy.findByLabelText(/Province/i).type('Alberta');
-    cy.findByLabelText(/International Postal Code/i).type('T7N');
-    cy.findByText(/Save temporary address/i).click();
-    cy.findAllByLabelText(/Email address/i)
-      .first()
-      .type('vet@vet.com');
-    cy.findByLabelText(/Re-enter email address/i).type('vet@vet.com');
-    cy.findByText(/Continue/i).click();
-  },
-
-  supplies: () => {
-    cy.get('#1').click();
-    cy.get('#3').click();
-    cy.get('#5').click();
-    cy.findAllByText(/Continue/i, { selector: 'button' })
-      .first()
-      .click();
-  },
-};
-
-const noBatteriesPageHooks = {
-  introduction: () => {
-    cy.findAllByText(/order/i, { selector: 'button' })
-      .first()
-      .click();
-  },
-
-  address: () => {
-    cy.findAllByText('Edit permanent address', { selector: 'button' })
-      .first()
-      .click();
-    cy.findByLabelText(/Country/i).select('Canada');
-    cy.findByLabelText(/Province/i).type('Alberta');
-    cy.findByLabelText(/International Postal Code/i).type('T7N');
-    cy.findByText(/Save permanent address/i).click();
-    cy.findAllByLabelText(/Email address/i)
-      .first()
-      .type('vet@vet.com');
-    cy.findByLabelText(/Re-enter email address/i).type('vet@vet.com');
-    cy.findByText(/Continue/i).click();
-  },
-
-  supplies: () => {
-    cy.get('#3').click();
-    cy.get('#5').click();
-    cy.findAllByText(/Continue/i, { selector: 'button' })
-      .first()
-      .click();
-  },
-};
-
-const noAccessoriesPageHooks = {
-  introduction: () => {
-    cy.findAllByText(/order/i, { selector: 'button' })
-      .first()
-      .click();
-  },
-
-  address: () => {
-    cy.findAllByText('Edit permanent address', { selector: 'button' })
-      .first()
-      .click();
-    cy.findByLabelText(/Country/i).select('Canada');
-    cy.findByLabelText(/Province/i).type('Alberta');
-    cy.findByLabelText(/International Postal Code/i).type('T7N');
-    cy.findByText(/Save permanent address/i).click();
-    cy.findAllByLabelText(/Email address/i)
-      .first()
-      .type('vet@vet.com');
-    cy.findByLabelText(/Re-enter email address/i).type('vet@vet.com');
-    cy.findByText(/Continue/i).click();
-  },
-
-  supplies: () => {
-    cy.get('#1').click();
-    cy.findAllByText(/Continue/i, { selector: 'button' })
-      .first()
-      .click();
-  },
-};
-
-const noItemsEligiblePageHooks = {
-  introduction: () => {
-    cy.findAllByText(/order/i, { selector: 'button' })
-      .first()
-      .click();
-  },
-
-  address: () => {
-    cy.findAllByText('Edit permanent address', { selector: 'button' })
-      .first()
-      .click();
-    cy.findByLabelText(/Country/i).select('Canada');
-    cy.findByLabelText(/Province/i).type('Alberta');
-    cy.findByLabelText(/International Postal Code/i).type('T7N');
-    cy.findByText(/Save permanent address/i).click();
-    cy.findAllByLabelText(/Email address/i)
-      .first()
-      .type('vet@vet.com');
-    cy.findByLabelText(/Re-enter email address/i).type('vet@vet.com');
-    cy.findByText(/Continue/i).click();
-  },
-
-  supplies: () => {
-    cy.findAllByText(/Continue/i, { selector: 'button' })
-      .first()
-      .click();
-  },
-};
-
-// let pageHooksData = {};
-
-// const getPageHookData = () => {
-//   cy.get('@testKey').then(testKey => {
-//     switch (testKey) {
-//       case 'happyPath':
-//         pageHooksData = happyPathPageHooks;
-//         break;
-//       case 'noTempAddress':
-//         pageHooksData = noTempAddressPageHooks;
-//         break;
-//       case 'noBatteries':
-//         pageHooksData = noBatteriesPageHooks;
-//         break;
-//       case 'noAccessories':
-//         pageHooksData = noAccessoriesPageHooks;
-//         break;
-//       case 'noItemsEligible':
-//         pageHooksData = noItemsEligiblePageHooks;
-//         break;
-//       default:
-//         break;
-//     }
-//     return pageHooksData;
-//   });
-// };
 
 const testConfig = createTestConfig(
   {
     dataPrefix: 'testData',
 
-    dataSets: [
-      'happyPath',
-      'noTempAddress',
-      'noBatteries',
-      'noAccessories',
-      'noItemsEligible',
-    ],
+    dataSets: ['happyPath', 'noTempAddress', 'noBatteries', 'noAccessories'],
 
     fixtures: {
       data: path.join(__dirname, 'data'),
       users: path.join(__dirname, 'data/users'),
     },
 
-    pageHooks: () => {
-      let pageHooksData;
-      cy.wrap(
+    pageHooks: {
+      introduction: () => {
+        cy.findAllByText(/order/i, { selector: 'button' })
+          .first()
+          .click();
+      },
+      address: () => {
         cy.get('@testKey').then(testKey => {
-          switch (testKey) {
-            case 'happyPath':
-              pageHooksData = happyPathPageHooks;
-              break;
-            case 'noTempAddress':
-              pageHooksData = noTempAddressPageHooks;
-              break;
-            case 'noBatteries':
-              pageHooksData = noBatteriesPageHooks;
-              break;
-            case 'noAccessories':
-              pageHooksData = noAccessoriesPageHooks;
-              break;
-            case 'noItemsEligible':
-              pageHooksData = noItemsEligiblePageHooks;
-              break;
-            default:
-              break;
+          if (testKey === 'noTempAddress') {
+            cy.findByText('Add a temporary address', {
+              selector: 'button',
+            }).click();
+            cy.findByLabelText(/Country/i).select('Canada');
+            cy.findByLabelText(/Street address/i).type('205 Test Lane');
+            cy.findByLabelText(/City/i).type('Calgary');
+            cy.findByLabelText(/Province/i).type('Alberta');
+            cy.findByLabelText(/International Postal Code/i).type('T7N');
+            cy.findByText(/Save temporary address/i).click();
+            cy.findByLabelText(/Re-enter email address/i).type('vet@vet.com');
+            cy.findByText(/Continue/i).click();
+          } else {
+            cy.findByText('Edit permanent address', {
+              selector: 'button',
+            }).click();
+            cy.findByLabelText(/Country/i).select('Canada');
+            cy.findByLabelText(/Province/i).type('Alberta');
+            cy.findByLabelText(/International Postal Code/i).type('T7N');
+            cy.findByText(/Save permanent address/i).click();
+            cy.findByLabelText(/Re-enter email address/i).type('vet@vet.com');
+            cy.findByText(/Continue/i).click();
           }
-          return pageHooksData;
-        }),
-      );
+        });
+      },
+      supplies: () => {
+        cy.get('@testKey').then(testKey => {
+          if (testKey === 'noBatteries') {
+            cy.get('#3').click();
+            cy.get('#5').click();
+          } else if (testKey === 'noAccessories') {
+            cy.get('#1').click();
+          } else {
+            cy.get('#1').click();
+            cy.get('#3').click();
+            cy.get('#5').click();
+          }
+          cy.findByText(/Continue/i, { selector: 'button' }).click();
+        });
+      },
     },
 
     setup: () => {
