6ab6e4523dbed5a21deebaae363124a67e421edc
Brooks Johnson <brooks.johnson@pm.me>
2020-06-29 15:27:43 -0700

    Revert "Convert to functional component"
    This reverts commit 03b618b2a2d343cbddda628380ebca368df2811d.

--- src/platform/forms-system/src/js/fields/ObjectField.jsx
+++ src/platform/forms-system/src/js/fields/ObjectField.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import _ from 'lodash/fp'; // eslint-disable-line no-restricted-imports
 
@@ -61,175 +61,213 @@ function orderAndFilterProperties(schema, uiSchema) {
   return _.values(groupedProperties);
 }
 
-function ObjectField({
-  onChange,
-  onBlur,
-  schema,
-  formData: originalFormData,
-  formContext,
-  uiSchema = {},
-  errorSchema = {},
-  idSchema = {},
-  registry = getDefaultRegistry(),
-  required = false,
-  disabled = false,
-  readonly = false,
-}) {
-  const SchemaField = pureWithDeepEquals(registry.fields.SchemaField);
-  let orderedProperties = null;
-
-  useEffect(() => {
-    setFirstFields(idSchema.$id);
-  });
-
-  useEffect(
-    () => {
-      orderedProperties = orderAndFilterProperties(schema, uiSchema);
-    },
-    schema,
-    uiSchema,
-  );
-
-  const onPropertyChange = name => {
-    return value => {
-      const formData = Object.keys(originalFormData || {}).length
-        ? originalFormData
-        : getDefaultFormState(schema, undefined, registry.definitions);
-      onChange(_.set(name, value, formData));
-    };
+class ObjectField extends React.Component {
+  static defaultProps = {
+    uiSchema: {},
+    errorSchema: {},
+    idSchema: {},
+    registry: getDefaultRegistry(),
+    required: false,
+    disabled: false,
+    readonly: false,
   };
 
-  const isRequired = name => {
+  constructor(props) {
+    super(props);
+    this.onPropertyChange = this.onPropertyChange.bind(this);
+    this.isRequired = this.isRequired.bind(this);
+    this.SchemaField = pureWithDeepEquals(
+      this.props.registry.fields.SchemaField,
+    );
+    this.orderedProperties = orderAndFilterProperties(
+      props.schema,
+      props.uiSchema,
+    );
+  }
+
+  componentDidMount() {
+    setFirstFields(this.props.idSchema.$id);
+  }
+  /* eslint-disable-next-line camelcase */
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if (
+      this.props.schema !== nextProps.schema ||
+      this.props.uiSchema !== nextProps.uiSchema
+    ) {
+      this.orderedProperties = orderAndFilterProperties(
+        nextProps.schema,
+        nextProps.uiSchema,
+      );
+    }
+  }
+
+  componentDidUpdate() {
+    setFirstFields(this.props.idSchema.$id);
+  }
+
+  onPropertyChange(name) {
+    return value => {
+      const formData = Object.keys(this.props.formData || {}).length
+        ? this.props.formData
+        : getDefaultFormState(
+            this.props.schema,
+            undefined,
+            this.props.registry.definitions,
+          );
+      this.props.onChange(_.set(name, value, formData));
+    };
+  }
+
+  isRequired(name) {
+    const { schema } = this.props;
     return (
       Array.isArray(schema.required) && schema.required.indexOf(name) !== -1
     );
-  };
-
-  const { definitions, fields } = registry;
-  const { TitleField } = fields;
-  const formData = Object.keys(originalFormData || {}).length
-    ? originalFormData
-    : getDefaultFormState(schema, {}, definitions);
-  const uiOptions = uiSchema['ui:options'] || {};
-
-  // description and title setup
-  const showFieldLabel = uiOptions.showFieldLabel;
-  const fieldsetClassNames = uiOptions.classNames;
-  const title = uiSchema['ui:title'] || schema.title;
-  const CustomTitleField = typeof title === 'function' ? title : null;
-
-  const description = uiSchema['ui:description'];
-  const textDescription = typeof description === 'string' ? description : null;
-  const DescriptionField =
-    typeof description === 'function' ? uiSchema['ui:description'] : null;
-
-  const hasTitleOrDescription = !!title || !!description;
-  const isRoot = idSchema.$id === 'root';
-
-  const containerClassNames = classNames({
-    'input-section': isRoot,
-    'schemaform-field-container': true,
-    'schemaform-block': title && !isRoot,
-  });
-
-  const renderProp = propName => (
-    <div key={propName}>
-      <SchemaField
-        name={propName}
-        required={isRequired(propName)}
-        schema={schema.properties[propName]}
-        uiSchema={uiSchema[propName]}
-        errorSchema={errorSchema[propName]}
-        idSchema={idSchema[propName]}
-        formData={formData[propName]}
-        onChange={onPropertyChange(propName)}
-        onBlur={onBlur}
-        registry={registry}
-        disabled={disabled}
-        readonly={readonly}
-      />
-    </div>
-  );
-
-  // Id's are not always unique on the review and submit page
-  const id =
-    isRoot && formContext.onReviewPage
-      ? Object.keys(idSchema)
-          .reduce((ids, key) => {
-            ids.push(idSchema[key].$id || '');
-            return ids;
-          }, [])
-          .filter(k => k)
-          .join('_')
-      : idSchema.$id;
-
-  const fieldContent = (
-    <div className={containerClassNames}>
-      {hasTitleOrDescription && (
-        <div className="schemaform-block-header">
-          {CustomTitleField && !showFieldLabel ? (
-            <CustomTitleField
-              id={`${id}__title`}
-              formData={formData}
-              formContext={formContext}
-              required={required}
-            />
-          ) : null}
-          {!CustomTitleField && title && !showFieldLabel ? (
-            <TitleField
-              id={`${id}__title`}
-              title={title}
-              required={required}
-              formContext={formContext}
-            />
-          ) : null}
-          {textDescription && <p>{textDescription}</p>}
-          {DescriptionField && (
-            <DescriptionField
-              formData={formData}
-              formContext={formContext}
-              options={uiSchema['ui:options']}
-            />
-          )}
-          {!textDescription && !DescriptionField && description}
-        </div>
-      )}
-      {orderedProperties.map((objectFields, index) => {
-        if (objectFields.length > 1) {
-          const [first, ...rest] = objectFields;
-          const visible = rest.filter(
-            prop => !_.get(['properties', prop, 'ui:collapsed'], schema),
-          );
-          return (
-            <ExpandingGroup open={visible.length > 0} key={index}>
-              {renderProp(first)}
-              <div
-                className={_.get(
-                  [first, 'ui:options', 'expandUnderClassNames'],
-                  uiSchema,
-                )}
-              >
-                {visible.map(renderProp)}
-              </div>
-            </ExpandingGroup>
-          );
-        }
-
-        // if fields have expandUnder, but are the only item, that means the
-        // field they’re expanding under is hidden, and they should be hidden, too
-        return !_.get([objectFields[0], 'ui:options', 'expandUnder'], uiSchema)
-          ? // eslint-disable-next-line sonarjs/no-extra-arguments
-            renderProp(objectFields[0], index)
-          : undefined;
-      })}
-    </div>
-  );
-
-  if (title) {
-    return <fieldset className={fieldsetClassNames}>{fieldContent}</fieldset>;
   }
 
-  return <div className={fieldsetClassNames}>{fieldContent}</div>;
+  render() {
+    const {
+      uiSchema,
+      errorSchema,
+      idSchema,
+      schema,
+      required,
+      disabled,
+      readonly,
+      onBlur,
+      formContext,
+    } = this.props;
+    const { definitions, fields } = this.props.registry;
+    const { TitleField } = fields;
+    const SchemaField = this.SchemaField;
+    const formData = Object.keys(this.props.formData || {}).length
+      ? this.props.formData
+      : getDefaultFormState(schema, {}, definitions);
+    const uiOptions = uiSchema['ui:options'] || {};
+
+    // description and title setup
+    const showFieldLabel = uiOptions.showFieldLabel;
+    const fieldsetClassNames = uiOptions.classNames;
+    const title = uiSchema['ui:title'] || schema.title;
+    const CustomTitleField = typeof title === 'function' ? title : null;
+
+    const description = uiSchema['ui:description'];
+    const textDescription =
+      typeof description === 'string' ? description : null;
+    const DescriptionField =
+      typeof description === 'function' ? uiSchema['ui:description'] : null;
+
+    const hasTitleOrDescription = !!title || !!description;
+    const isRoot = idSchema.$id === 'root';
+
+    const containerClassNames = classNames({
+      'input-section': isRoot,
+      'schemaform-field-container': true,
+      'schemaform-block': title && !isRoot,
+    });
+
+    const renderProp = propName => (
+      <div key={propName}>
+        <SchemaField
+          name={propName}
+          required={this.isRequired(propName)}
+          schema={schema.properties[propName]}
+          uiSchema={uiSchema[propName]}
+          errorSchema={errorSchema[propName]}
+          idSchema={idSchema[propName]}
+          formData={formData[propName]}
+          onChange={this.onPropertyChange(propName)}
+          onBlur={onBlur}
+          registry={this.props.registry}
+          disabled={disabled}
+          readonly={readonly}
+        />
+      </div>
+    );
+
+    // Id's are not always unique on the review and submit page
+    const id =
+      isRoot && formContext.onReviewPage
+        ? Object.keys(idSchema)
+            .reduce((ids, key) => {
+              ids.push(idSchema[key].$id || '');
+              return ids;
+            }, [])
+            .filter(k => k)
+            .join('_')
+        : idSchema.$id;
+
+    const fieldContent = (
+      <div className={containerClassNames}>
+        {hasTitleOrDescription && (
+          <div className="schemaform-block-header">
+            {CustomTitleField && !showFieldLabel ? (
+              <CustomTitleField
+                id={`${id}__title`}
+                formData={formData}
+                formContext={formContext}
+                required={required}
+              />
+            ) : null}
+            {!CustomTitleField && title && !showFieldLabel ? (
+              <TitleField
+                id={`${id}__title`}
+                title={title}
+                required={required}
+                formContext={formContext}
+              />
+            ) : null}
+            {textDescription && <p>{textDescription}</p>}
+            {DescriptionField && (
+              <DescriptionField
+                formData={formData}
+                formContext={formContext}
+                options={uiSchema['ui:options']}
+              />
+            )}
+            {!textDescription && !DescriptionField && description}
+          </div>
+        )}
+        {this.orderedProperties.map((objectFields, index) => {
+          if (objectFields.length > 1) {
+            const [first, ...rest] = objectFields;
+            const visible = rest.filter(
+              prop => !_.get(['properties', prop, 'ui:collapsed'], schema),
+            );
+            return (
+              <ExpandingGroup open={visible.length > 0} key={index}>
+                {renderProp(first)}
+                <div
+                  className={_.get(
+                    [first, 'ui:options', 'expandUnderClassNames'],
+                    uiSchema,
+                  )}
+                >
+                  {visible.map(renderProp)}
+                </div>
+              </ExpandingGroup>
+            );
+          }
+
+          // if fields have expandUnder, but are the only item, that means the
+          // field they’re expanding under is hidden, and they should be hidden, too
+          return !_.get(
+            [objectFields[0], 'ui:options', 'expandUnder'],
+            uiSchema,
+          )
+            ? // eslint-disable-next-line sonarjs/no-extra-arguments
+              renderProp(objectFields[0], index)
+            : undefined;
+        })}
+      </div>
+    );
+
+    if (title) {
+      return <fieldset className={fieldsetClassNames}>{fieldContent}</fieldset>;
+    }
+
+    return <div className={fieldsetClassNames}>{fieldContent}</div>;
+  }
 }
 
 ObjectField.propTypes = {
