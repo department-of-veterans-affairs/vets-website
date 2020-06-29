a59eec9b35b71e375bbf2c148bd7bb2d2105a576
Brad Conley <brad.conl3y@gmail.com>
2020-06-29 12:50:39 -0400

    add unit tests

--- src/applications/debt-letters/tests/DebtLettersWrapper.unit.spec.jsx
+++ src/applications/debt-letters/tests/DebtLettersWrapper.unit.spec.jsx
@@ -2,13 +2,8 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 import DebtLettersWrapper from '../components/DebtLettersWrapper';
-import CallToActionWidget from 'platform/site-wide/cta-widget';
 
 describe('DebtLettersWrapper', () => {
-  //   isLoggedIn: state.user.login.currentlyLoggedIn,
-  //   isFetching: state.debtLetters.isFetching,
-  //   debts: state.debtLetters.debts,
-  //   isError: state.debtLetters.isError,
   it('mounts wrapper component', () => {
     const fakeStore = {
       getState: () => ({
@@ -50,27 +45,6 @@ describe('DebtLettersWrapper', () => {
     expect(wrapper.dive().find('Connect(CallToActionWidget)').length).to.equal(
       1,
     );
-    wrapper.unmount();
-  });
-  it('renders loading indicator if loading', () => {
-    const fakeStore = {
-      getState: () => ({
-        user: {
-          login: {
-            currentlyLoggedIn: true,
-          },
-        },
-        debtLetters: {
-          isFetching: true,
-          debts: [],
-          isError: false,
-        },
-      }),
-      subscribe: () => {},
-      dispatch: () => {},
-    };
-    const wrapper = mount(<DebtLettersWrapper store={fakeStore} />);
-    console.log(wrapper.debug())
     wrapper.unmount();
   });
 });
