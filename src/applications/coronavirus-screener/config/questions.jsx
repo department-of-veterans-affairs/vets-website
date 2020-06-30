0c17779015b19eb10a0e63dbe79d346fc0e31fb2
Patrick Bateman <batemapf@gmail.com>
2020-06-30 11:55:28 -0400

    another whitespace issue

--- src/applications/coronavirus-screener/config/questions.jsx
+++ src/applications/coronavirus-screener/config/questions.jsx
@@ -14,8 +14,7 @@ export const questions = [
   },
   {
     id: 'cough',
-    text:
-      "In the past 7 days, have you had a cough, shortness of breath, or difficulty breathing that's new or getting worse?",
+    text: "In the past 7 days, have you had a cough, shortness of breath, or difficulty breathing that's new or getting worse?",
   },
   {
     id: 'flu',
