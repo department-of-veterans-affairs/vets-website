3694df75fff14dce7fae7a321318a9abf6a778dc
Dan Hinze <dan.hinze@adhocteam.us>
2020-07-01 20:26:17 -0500

    Update keepalive logic to use session-alive header and ssoe vets-api flag

--- src/platform/utilities/sso/index.js
+++ src/platform/utilities/sso/index.js
@@ -26,34 +26,36 @@ async function hasVAGovSession() {
 }
 
 export async function ssoKeepAliveSession() {
-  const { ttl, authn } = await keepAlive();
-  if (ttl > 0) {
-    // ttl is positive, user has an active session
+  const { ttl, authn, sessionAlive } = await keepAlive();
+  if (!sessionAlive) {
+    localStorage.setItem('hasSessionSSO', false);
+  } else if (sessionAlive && ttl > 0) {
     // ttl is in seconds, add from now
     const expirationTime = moment().add(ttl, 's');
     localStorage.setItem('sessionExpirationSSO', expirationTime);
     localStorage.setItem('hasSessionSSO', true);
-  } else if (ttl === 0) {
-    // ttl is 0, user has an inactive session
-    localStorage.setItem('hasSessionSSO', false);
   } else {
-    // ttl is null, we can't determine if the user has a session or not
+    // ttl is null or 0, we can't determine if the user has a session or not
     localStorage.removeItem('hasSessionSSO');
   }
   return { ttl, authn };
 }
 
-export async function checkAutoSession(application = null, to = null) {
-  const [{ ttl, authn }, hasSession] = await Promise.all([
-    ssoKeepAliveSession(),
-    hasVAGovSession(),
-  ]);
-  if (hasSession && ttl === 0) {
+export async function checkAutoSession(
+  authenticatedWithSSOe,
+  application = null,
+  to = null,
+) {
+  const [
+    { ttl, authn, sessionAlive: sessionAliveSSO },
+    hasSession,
+  ] = await Promise.all([ssoKeepAliveSession(), hasVAGovSession()]);
+  if (hasSession && authenticatedWithSSOe && !sessionAliveSSO) {
     // explicitly check to see if the TTL for the SSO3 session is 0, as it
     // could also be null if we failed to get a response from the SSOe server,
     // in which case we don't want to logout the user because we don't know
     logout('v1', 'sso-automatic-logout');
-  } else if (!hasSession && ttl > 0 && !getLoginAttempted() && authn) {
+  } else if (!hasSession && !sessionAliveSSO && !getLoginAttempted() && authn) {
     // only attempt an auto login if the user is
     // a) does not have a VA.gov session
     // b) has an SSOe session
