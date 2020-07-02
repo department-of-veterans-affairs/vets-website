67faca758c93beafc7e42d6bfdfef56eba8f49b6
Kelson Adams <kelsonic@users.noreply.github.com>
2020-07-02 10:03:54 -0600

    Add legacy content into health care widgets (#13361)
    * Create initial boilerplate for LegacyContent components
    
    * Update legacy content for LegacyContents
    
    * Fix misplaced divs
    
    * Update unit tests

--- src/applications/static-pages/health-care-manage-benefits/view-test-and-lab-results-page/components/App/index.js
+++ src/applications/static-pages/health-care-manage-benefits/view-test-and-lab-results-page/components/App/index.js
@@ -4,10 +4,16 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 // Relative imports.
 import AuthContent from '../AuthContent';
+import LegacyContent from '../LegacyContent';
 import UnauthContent from '../UnauthContent';
+import environment from 'platform/utilities/environment';
 import { selectIsCernerPatient } from 'platform/user/selectors';
 
 export const App = ({ isCernerPatient }) => {
+  if (environment.isProduction()) {
+    return <LegacyContent />;
+  }
+
   if (isCernerPatient) {
     return <AuthContent />;
   }
