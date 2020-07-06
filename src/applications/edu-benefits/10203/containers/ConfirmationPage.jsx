b38415e5312ed06aa7344af439f3f5b78e1a2263
Matt Roth <roth_matthew@bah.com>
2020-07-06 16:03:38 -0400

    print only stuff

--- src/applications/edu-benefits/10203/containers/ConfirmationPage.jsx
+++ src/applications/edu-benefits/10203/containers/ConfirmationPage.jsx
@@ -5,8 +5,6 @@ import Scroll from 'react-scroll';
 
 import { focusElement } from 'platform/utilities/ui';
 
-import { benefitsLabels } from '../../utils/labels';
-
 const scroller = Scroll.scroller;
 const scrollToTop = () => {
   scroller.scrollTo('topScrollElement', {
@@ -38,54 +36,44 @@ class ConfirmationPage extends React.Component {
       ? this.props.form.submission.response.attributes
       : {};
     const name = form.data.veteranFullName;
-    const benefit = form.data.benefit;
-
-    const docExplanation = this.state.isExpanded ? (
-      <div className="usa-accordion-content" aria-hidden="false">
-        <p>In the future, you might need:</p>
-        <ul>
-          <li>Your reserve kicker</li>
-          <li>
-            Documentation of additional contributions that would increase your
-            monthly benefits.
-          </li>
-        </ul>
-        <p>
-          Documents can be uploaded using the{' '}
-          <a href="https://gibill.custhelp.com/app/utils/login_form/redirect/account%252">
-            GI Bill site
-          </a>
-          .
-        </p>
-      </div>
-    ) : null;
 
     return (
       <div>
-        <h3 className="confirmation-page-title">Claim received</h3>
+        <div className="print-only">
+          <img src="/img/design/logo/va-logo.png" alt="VA logo" width="300" />
+          <h1 className="vads-u-font-size--h3">
+            Apply for the Rogers STEM Scholarship
+          </h1>
+          <span>Form 22-10203</span>
+        </div>
+        <h3 className="confirmation-page-title screen-only">
+          We've received your application.
+        </h3>
+        <h4 className="print-only">We've received your application.</h4>
         <p>
-          We usually process claims within <strong>30 days</strong>.
+          We usually process claims within <strong>30 days</strong>.<br />
+          We may contact you for more information or documents.
         </p>
         <p>
-          We may contact you for more information or documents.
-          <br />
-          <i>Please print this page for your records.</i>
+          <button
+            className="usa-button-primary screen-only"
+            onClick={() => window.print()}
+          >
+            Print this page
+          </button>
         </p>
         <div className="inset">
-          <h4>
-            Education Benefit Claim{' '}
-            <span className="additional">(Form 22-10203)</span>
+          <h4 className="vads-u-margin-top--0">
+            Rogers STEM Scholarship{' '}
+            <span className="vads-u-margin--0 vads-u-display--inline-block">
+              (Form 22-10203)
+            </span>
           </h4>
           <span>
             for {name.first} {name.middle} {name.last} {name.suffix}
           </span>
 
           <ul className="claim-list">
-            <li>
-              <strong>Benefit to be transferred</strong>
-              <br />
-              {benefitsLabels[benefit]}
-            </li>
             <li>
               <strong>Confirmation number</strong>
               <br />
@@ -104,23 +92,6 @@ class ConfirmationPage extends React.Component {
               <address className="schemaform-address-view">
                 {response.regionalOffice}
               </address>
-            </li>
-          </ul>
-        </div>
-        <div id="collapsiblePanel" className="usa-accordion-bordered">
-          <ul className="usa-unstyled-list">
-            <li>
-              <div className="accordion-header clearfix">
-                <button
-                  className="usa-button-unstyled"
-                  aria-expanded={this.state.isExpanded ? 'true' : 'false'}
-                  aria-controls="collapsible-document-explanation"
-                  onClick={this.toggleExpanded}
-                >
-                  No documents required at this time
-                </button>
-              </div>
-              <div id="collapsible-document-explanation">{docExplanation}</div>
             </li>
           </ul>
         </div>
