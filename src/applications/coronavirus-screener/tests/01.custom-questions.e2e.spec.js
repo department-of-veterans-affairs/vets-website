84fc95aeaa6877cd2aa81bc3541ae575d11cb167
Michael Chelen <michael.chelen@va.gov>
2020-07-08 11:33:09 -0500

    fix & cleanup

--- src/applications/coronavirus-screener/tests/01.custom-questions.e2e.spec.js
+++ src/applications/coronavirus-screener/tests/01.custom-questions.e2e.spec.js
@@ -2,6 +2,7 @@ import { normal } from 'platform/testing/e2e/timeouts';
 import { createE2eTest, baseUrl } from 'platform/testing/e2e/helpers';
 import { testQuestionScenario } from './question-scenario-helper';
 
+// visitor travel passing answers
 const visitorPassTravel459 = {
   title: 'Visitor pass travel 459',
   questions: [
@@ -18,6 +19,7 @@ const visitorPassTravel459 = {
   },
 };
 
+// visitor travel needs more screening
 const visitorScreeningTravel459 = {
   title: 'Visitor needs more screening travel 459',
   questions: [
@@ -34,6 +36,7 @@ const visitorScreeningTravel459 = {
   },
 };
 
+// staff travel passing
 const staffPassTravel459 = {
   title: 'Staff pass travel 459',
   questions: [
@@ -50,6 +53,7 @@ const staffPassTravel459 = {
   },
 };
 
+// staff travel more screening
 const staffScreeningTravel459 = {
   title: 'Staff needs more screening travel 459',
   questions: [
@@ -66,8 +70,20 @@ const staffScreeningTravel459 = {
   },
 };
 
-// custom travel question /459
+const visitorPassTravel459GH = visitorPassTravel459;
+visitorPassTravel459GH.question[6].id = 'question-travel-459GH';
+
+const visitorScreeningTravel459GH = visitorScreeningTravel459;
+visitorScreeningTravel459GH.question[6].id = 'question-travel-459GH';
+
+const staffPassTravel459GH = staffPassTravel459;
+staffPassTravel459GH.question[6].id = 'question-travel-459GH';
+
+const staffScreeningTravel459GH = staffScreeningTravel459;
+staffScreeningTravel459GH.question[6].id = 'question-travel-459GH';
+
 export default createE2eTest(client => {
+  // custom travel question /459
   client
     .url(`${baseUrl}/covid19screen/459`)
     .waitForElementVisible('body', normal)
@@ -75,37 +91,12 @@ export default createE2eTest(client => {
     .assert.visible('div[class*=covid-screener-results-incomplete]')
     .axeCheck('.main');
 
-  // visitor travel passing answers
   testQuestionScenario({ scenario: visitorPassTravel459, client });
-
-  // visitor travel needs more screening
   testQuestionScenario({ scenario: visitorScreeningTravel459, client });
-
-  // staff travel passing
   testQuestionScenario({ scenario: staffPassTravel459, client });
-
-  // staff travel more screening
   testQuestionScenario({ scenario: staffScreeningTravel459, client });
-});
 
-
-
-const visitorPassTravel459GH = visitorPassTravel459;
-visitorPassTravel459GH.question.[6].id = 'question-travel-459GH';
-
-const visitorScreeningTravel459GH = visitorScreeningTravel459;
-visitorScreeningTravel459GH.question.[6].id = 'question-travel-459GH';
-
-const staffPassTravel459GH = staffPassTravel459;
-staffPassTravel459GH.question.[6].id = 'question-travel-459GH';
-
-const staffScreeningTravel459GH = staffScreeningTravel459;
-staffScreeningTravel459GH.question.[6].id = 'question-travel-459GH';
-
-
-
-// custom travel question /459GH
-export default createE2eTest(client => {
+  // custom travel question /459GH
   client
     .url(`${baseUrl}/covid19screen/459GH`)
     .waitForElementVisible('body', normal)
@@ -113,22 +104,12 @@ export default createE2eTest(client => {
     .assert.visible('div[class*=covid-screener-results-incomplete]')
     .axeCheck('.main');
 
-  // visitor travel passing answers
   testQuestionScenario({ scenario: visitorPassTravel459GH, client });
-
-  // visitor travel needs more screening
   testQuestionScenario({ scenario: visitorScreeningTravel459GH, client });
-
-  // staff travel passing
   testQuestionScenario({ scenario: staffPassTravel459GH, client });
-
-  // staff travel more screening
   testQuestionScenario({ scenario: staffScreeningTravel459GH, client });
-});
 
-
-// custom travel question case insensitive /459gh 
-export default createE2eTest(client => {
+  // custom travel question case insensitive /459gh
   client
     .url(`${baseUrl}/covid19screen/459gh`)
     .waitForElementVisible('body', normal)
@@ -136,16 +117,8 @@ export default createE2eTest(client => {
     .assert.visible('div[class*=covid-screener-results-incomplete]')
     .axeCheck('.main');
 
-  // visitor travel passing answers
   testQuestionScenario({ scenario: visitorPassTravel459GH, client });
-
-  // visitor travel needs more screening
   testQuestionScenario({ scenario: visitorScreeningTravel459GH, client });
-
-  // staff travel passing
   testQuestionScenario({ scenario: staffPassTravel459GH, client });
-
-  // staff travel more screening
   testQuestionScenario({ scenario: staffScreeningTravel459GH, client });
 });
-
