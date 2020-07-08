4359019f55b7b1cfbff37b8a9b9fdf88adbbe507
Christopher Valarida <chris.valarida@adhocteam.us>
2020-07-08 10:31:02 -0700

    Simplify input schema

--- src/site/stages/build/process-cms-exports/schemas/raw/paragraph-media.js
+++ src/site/stages/build/process-cms-exports/schemas/raw/paragraph-media.js
@@ -3,17 +3,17 @@
 module.exports = {
   type: 'object',
   properties: {
-    parent_field_name: { $ref: 'GenericNestedString' },
     field_allow_clicks_on_this_image: { $ref: 'GenericNestedBoolean' },
-    // At the time of writing, field_markup in all paragraph-media entities
-    // was empty, but the OpenAPI says it'll be a string
-    field_markup: { $ref: 'GenericNestedString' },
+    // At the time of writing, field_markup in all paragraph-media entities was
+    // empty, but the OpenAPI says it'll be a string. Commenting this out
+    // because it doesn't look like it's used.
+    //
+    // field_markup: { $ref: 'GenericNestedString' },
     field_media: { $ref: 'EntityReferenceArray' },
   },
   required: [
-    'parent_field_name',
     'field_allow_clicks_on_this_image',
-    'field_markup',
+    // 'field_markup',
     'field_media',
   ],
 };
