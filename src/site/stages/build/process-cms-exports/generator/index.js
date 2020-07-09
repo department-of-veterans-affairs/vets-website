9e0ef10507c325475fe451564dc430e19c1d5742
Christopher Valarida <chris.valarida@adhocteam.us>
2020-07-09 12:30:19 -0700

    Autocomplete fields

--- src/site/stages/build/process-cms-exports/generator/index.js
+++ src/site/stages/build/process-cms-exports/generator/index.js
@@ -1,8 +1,46 @@
 const Generator = require('yeoman-generator');
+const fuzzy = require('fuzzy');
+
+const customRefs = [
+  `{ $ref: 'GenericNestedString' }`,
+  `{ $ref: 'GenericNestedBoolean' }`,
+  `{ $ref: 'GenericNestedNumber' }`,
+  `{ $ref: 'RawMetaTag' }`, // Used in the input schema
+  `{ $ref: 'MetaTag' }`, // Used in the output schema
+];
+
+const schemaTypes = [
+  `{ type: 'string' }`,
+  `{ type: 'object', properties: {} }`,
+  `{ type: 'array' }`,
+  `{ type: 'number' }`,
+  `{ type: 'boolean' }`,
+];
+
+const schemaOptions = schemaTypes.concat(customRefs);
+
+const getFieldType = async (answers, input = '') =>
+  fuzzy.filter(input, schemaOptions).map(el => el.original);
 
 module.exports = class extends Generator {
-  getBundleData() {
-    this.log('Hello');
+  constructor(args, opts) {
+    super(args, opts);
+    this.env.adapter.promptModule.registerPrompt(
+      'autocomplete',
+      require('inquirer-autocomplete-prompt'),
+    );
+  }
+  async getBundleData() {
+    this.log('Hello, world!');
+    this.prompt([
+      {
+        type: 'autocomplete',
+        name: 'fieldType',
+        suggestOnly: true,
+        message: 'Field type',
+        source: getFieldType,
+      },
+    ]);
   }
 
   generateInputSchema() {}
