0e94d7ef9c8718a861f6b7c38a6e983e52fd5954
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-07-09 14:18:41 -0500

    Add more unit test coverage

--- src/applications/facility-locator/tests/components/UrgentCareSearch.unit.spec.js
+++ src/applications/facility-locator/tests/components/UrgentCareSearch.unit.spec.js
@@ -10,10 +10,23 @@ describe('UrgentCareSearch', () => {
     expect(urgentCallNotReturn).to.equal(null);
   });
 
-  it('Should render urgentCareCall in result item.', () => {
+  it('Should render urgentCareCall in result item - cc_provider, urgent_care_service ', () => {
     const urgentCall = urgentCareCall({
       facilityType: LocationType.CC_PROVIDER,
       serviceType: CLINIC_URGENTCARE_SERVICE,
+    });
+    const wrapper = shallow(urgentCall);
+    expect(wrapper.type()).to.not.equal(null);
+    const p = wrapper.find('p');
+    expect(p.length).to.equal(1);
+    expect(p.text()).to.be.a('string');
+    wrapper.unmount();
+  });
+
+  it('Should render urgentCareCall in result item - urgent_care, non_va_urgentcare  ', () => {
+    const urgentCall = urgentCareCall({
+      facilityType: LocationType.URGENT_CARE,
+      serviceType: 'NonVAUrgentCare',
     });
     const wrapper = shallow(urgentCall);
     expect(wrapper.type()).to.not.equal(null);
@@ -33,8 +46,10 @@ describe('UrgentCareSearch', () => {
 
   it('Should return correct labels for urgent care (UrgentCare, WalkIn)', () => {
     const walkIn = urgentCareProviderNames(17);
-    const urgentCare = urgentCareProviderNames(20);
+    const urgentCare1 = urgentCareProviderNames(20);
     expect(walkIn).to.equal('RETAIL/WALK-IN CARE');
-    expect(urgentCare).to.equal('URGENT CARE');
+    expect(urgentCare1).to.equal('URGENT CARE');
+    const urgentCare2 = urgentCareProviderNames(undefined);
+    expect(urgentCare2).to.equal('URGENT CARE');
   });
 });
