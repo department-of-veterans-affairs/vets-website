0b9bf224b9b76919e0fac879e1af41bbd160914c
Joe <elder_joseph@bah.com>
2020-07-13 16:04:10 -0400

    10774: Add additional unit tests for the entitlement remaining alert

--- src/applications/edu-benefits/10203/tests/containers/IntroductionPage.unit.spec.jsx
+++ src/applications/edu-benefits/10203/tests/containers/IntroductionPage.unit.spec.jsx
@@ -3,7 +3,6 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
 import { IntroductionPage } from '../../containers/IntroductionPage';
-
 import { getEnrollmentData } from '../../actions/post-911-gib-status';
 
 describe('Edu 10203 <IntroductionPage>', () => {
@@ -24,10 +23,49 @@ describe('Edu 10203 <IntroductionPage>', () => {
         }}
       />,
     );
+
     expect(tree.find('FormTitle').props().title).to.contain('Apply');
     expect(tree.find('withRouter(Connect(SaveInProgressIntro))').exists()).to.be
       .true;
     expect(tree.find('.process-step').length).to.equal(5);
+    tree.unmount();
+  });
+
+  it('should show entitlement remaining alert', () => {
+    const enrollmentDataGreaterThan180 = {
+      remainingEntitlement: { months: 6, days: 3 },
+    };
+
+    const tree = shallow(
+      <IntroductionPage
+        isLoggedIn
+        enrollmentData={enrollmentDataGreaterThan180}
+        getEnrollmentData={getEnrollmentData}
+        route={{
+          formConfig: {},
+        }}
+      />,
+    );
+    expect(tree.find('.usa-alert-warning').exists()).to.be.true;
+    tree.unmount();
+  });
+
+  it('should not show entitlement remaining alert', () => {
+    const enrollmentDataLessThan180 = {
+      remainingEntitlement: { months: 1, days: 3 },
+    };
+
+    const tree = shallow(
+      <IntroductionPage
+        isLoggedIn
+        enrollmentData={enrollmentDataLessThan180}
+        getEnrollmentData={getEnrollmentData}
+        route={{
+          formConfig: {},
+        }}
+      />,
+    );
+    expect(tree.find('.usa-alert-warning').exists()).to.be.false;
     tree.unmount();
   });
 });
