40d75cc9094e340b333531b5ef765f22f98c7854
Kelson Adams <kadams@governmentcio.com>
2020-07-13 11:50:41 -0600

    Remove custom banners code and replace with design system components

--- src/platform/site-wide/banners/components/Banners/index.js
+++ src/platform/site-wide/banners/components/Banners/index.js
@@ -1,9 +1,12 @@
 // Node modules.
 import React from 'react';
 import PropTypes from 'prop-types';
+import EmergencyBanner from '@department-of-veterans-affairs/formation-react/EmergencyBanner';
+import MaintenanceBanner from '@department-of-veterans-affairs/formation-react/MaintenanceBanner';
 // Relative imports.
-import HomepageBanner from '../HomepageBanner';
-import MaintenanceBanner from '../MaintenanceBanner';
+import config from '../../config/maintenanceBanner';
+import environment from 'platform/utilities/environment';
+import recordEvent from 'platform/monitoring/record-event';
 
 export const Banners = ({
   homepageBannerContent,
@@ -13,15 +16,26 @@ export const Banners = ({
 }) => (
   <>
     {/* Homepage Banner */}
-    <HomepageBanner
+    <EmergencyBanner
       content={homepageBannerContent}
+      recordEvent={recordEvent}
       title={homepageBannerTitle}
+      showClose={!environment.isProduction()}
       type={homepageBannerType}
       visible={homepageBannerVisible === 'true'}
     />
 
     {/* Maintenance Banner */}
-    <MaintenanceBanner />
+    <MaintenanceBanner
+      content={config.content}
+      expiresAt={config.expiresAt}
+      id={config.id}
+      startsAt={config.startsAt}
+      title={config.title}
+      warnContent={config.warnContent}
+      warnStartsAt={config.warnStartsAt}
+      warnTitle={config.warnTitle}
+    />
   </>
 );
 
