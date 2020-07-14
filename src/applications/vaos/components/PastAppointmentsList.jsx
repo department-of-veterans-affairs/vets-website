8397035288c8beac851a6583ec983c9d5bc773b0
Tony Williams <tony.williams@va.gov>
2020-07-14 14:50:54 -0500

    Modified per 508 review.

--- src/applications/vaos/components/PastAppointmentsList.jsx
+++ src/applications/vaos/components/PastAppointmentsList.jsx
@@ -40,14 +40,14 @@ export class PastAppointmentsList extends React.Component {
     }
   }
 
-  componentDidUpdate(prevProps) {
-    if (
-      prevProps.appointments.pastStatus === FETCH_STATUS.loading &&
-      this.props.appointments.pastStatus === FETCH_STATUS.succeeded
-    ) {
-      focusElement('#queryResultLabel');
-    }
-  }
+  // componentDidUpdate(prevProps) {
+  //   if (
+  //     prevProps.appointments.pastStatus === FETCH_STATUS.loading &&
+  //     this.props.appointments.pastStatus === FETCH_STATUS.succeeded
+  //   ) {
+  //     focusElement('#queryResultLabel');
+  //   }
+  // }
 
   onDateRangeChange = index => {
     const selectedDateRange = this.dateRangeOptions[index];
