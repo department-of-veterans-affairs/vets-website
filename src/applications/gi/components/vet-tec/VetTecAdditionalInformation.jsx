9cb75983dc70edecf79ff161fcd22672b9b526da
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2020-07-14 09:25:29 -0400

    Stopped excess reading from bottom modals

--- src/applications/gi/components/vet-tec/VetTecAdditionalInformation.jsx
+++ src/applications/gi/components/vet-tec/VetTecAdditionalInformation.jsx
@@ -7,9 +7,9 @@ export const VetTecAdditionalInformation = ({
 }) => (
   <div className="additional-information row vads-u-margin-top--1">
     <div className="usa-width-one-half medium-6 columns">
-      <div className="institution-codes usa-width-one-whole">
+      <div aria-live="off" className="institution-codes usa-width-one-whole">
         <h3>Institution codes</h3>
-        <div>
+        <div aria-live="off">
           <strong>
             <button
               id="facilityCode-button"
