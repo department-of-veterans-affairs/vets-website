c2c2c6c28769e26d1ab2e246b928e11b11fe734c
Joe <elder_joseph@bah.com>
2020-07-14 13:07:42 -0400

    10774: Simplify post 911 gib status and unit test

--- src/applications/edu-benefits/10203/reducers/index.js
+++ src/applications/edu-benefits/10203/reducers/index.js
@@ -1,37 +1,20 @@
 import set from 'lodash/fp/set';
 
-import {
-  BACKEND_AUTHENTICATION_ERROR,
-  BACKEND_SERVICE_ERROR,
-  GET_ENROLLMENT_DATA_FAILURE,
-  GET_ENROLLMENT_DATA_SUCCESS,
-  NO_CHAPTER33_RECORD_AVAILABLE,
-} from '../utils/constants';
+import { GET_ENROLLMENT_DATA_SUCCESS } from '../utils/constants';
 
 const initialState = {
   enrollmentData: null,
-  availability: 'awaitingResponse',
 };
 
 function post911GIBStatus(state = initialState, action) {
-  switch (action.type) {
-    case GET_ENROLLMENT_DATA_SUCCESS:
-      return {
-        ...state,
-        enrollmentData: action.data,
-        availability: 'available',
-      };
-    case BACKEND_SERVICE_ERROR:
-      return set('availability', 'backendServiceError', state);
-    case BACKEND_AUTHENTICATION_ERROR:
-      return set('availability', 'backendAuthenticationError', state);
-    case NO_CHAPTER33_RECORD_AVAILABLE:
-      return set('availability', 'noChapter33Record', state);
-    case GET_ENROLLMENT_DATA_FAILURE:
-      return set('availability', 'getEnrollmentDataFailure', state);
-    default:
-      return state;
+  if (action.type === GET_ENROLLMENT_DATA_SUCCESS) {
+    return {
+      ...state,
+      enrollmentData: action.data,
+    };
   }
+
+  return state;
 }
 
 export default {
