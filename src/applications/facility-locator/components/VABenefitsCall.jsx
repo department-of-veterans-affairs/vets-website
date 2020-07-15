7daee8dbd719aaf08c0492521d3dfb15c3f10f04
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-07-15 17:26:44 -0500

    Remove unused code

--- src/applications/facility-locator/components/VABenefitsCall.jsx
+++ src/applications/facility-locator/components/VABenefitsCall.jsx
@@ -4,10 +4,6 @@ import Telephone, {
   CONTACTS,
 } from '@department-of-veterans-affairs/formation-react/Telephone';
 
-const { formattedPhoneNumber, extension, contact } = parsePhoneNumber(
-  '800-827-1000',
-);
-
 export default function VABenefitsCall() {
   return (
     <div>
