cda9d36d37575f2622d20782cfcb0a0988d05d5b
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-07-15 00:57:30 -0400

    Added JSDoc for contractTest helper.

--- src/platform/testing/contract/index.js
+++ src/platform/testing/contract/index.js
@@ -1,5 +1,13 @@
 import { Pact } from '@pact-foundation/pact';
 
+/**
+ * Helper for writing a Pact contract test that abstracts the boilerplate
+ * for setting up the mock API and using it to verify the mocked interactions.
+ *
+ * @param {string} consumer - String label for the consumer.
+ * @param {string} provider - String label for the provider.
+ * @param {function} test - Callback function for a test suite (describe block).
+ */
 const contractTest = (consumer, provider, test) => {
   describe(consumer, () => {
     const mockApi = new Pact({
