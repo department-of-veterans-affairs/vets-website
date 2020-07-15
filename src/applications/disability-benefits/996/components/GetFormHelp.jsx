646f97164ab21cc291d2ebfa220d452b34b07f4d
Robin Garrison <robin.garrison@adhocteam.us>
2020-07-15 10:02:43 -0500

    Update telephone links

--- src/applications/disability-benefits/996/components/GetFormHelp.jsx
+++ src/applications/disability-benefits/996/components/GetFormHelp.jsx
@@ -4,18 +4,15 @@ import Telephone, {
   PATTERNS,
 } from '@department-of-veterans-affairs/formation-react/Telephone';
 
+import Telephone, {
+  CONTACTS,
+} from '@department-of-veterans-affairs/formation-react/Telephone';
+
 const GetFormHelp = () => (
   <p className="help-talk">
     Need help filling out the form or have questions about eligibility? Please
     call VA Benefits and Services at{' '}
-    <a
-      href="tel:1-800-827-1000"
-      aria-label="8 0 0. 8 2 7. 1 0 0 0."
-      className="nowrap"
-    >
-      800-827-1000
-    </a>
-    .<br />
+    <Telephone contact={CONTACTS.VA_BENEFITS} />.<br />
     <br />
     If you have hearing loss, call TTY:{' '}
     <Telephone contact={CONTACTS['711']} pattern={PATTERNS['911']} />.
