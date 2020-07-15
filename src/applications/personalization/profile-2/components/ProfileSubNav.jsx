a40781c0b281e3a6f4b274bdcf2527bb2b9c2691
Erik P. Hansen <eph@erikphansen.com>
2020-07-15 16:05:14 -0700

    filter subnav routes when user is LOA1

--- src/applications/personalization/profile-2/components/ProfileSubNav.jsx
+++ src/applications/personalization/profile-2/components/ProfileSubNav.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-export function ProfileMenuItems({ routes, clickHandler = null }) {
+import { isLOA3 as isLOA3Selector } from 'platform/user/selectors';
+
+export function ProfileMenuItems({ routes, isLOA3, clickHandler = null }) {
   return (
     <ul>
       {routes.map(route => {
+        // Do not render route if it is not isLOA3
+        if (route.requiresLOA3 && !isLOA3) {
+          return null;
+        }
+
         return (
           <li key={route.path}>
             <NavLink
@@ -24,18 +32,19 @@ export function ProfileMenuItems({ routes, clickHandler = null }) {
   );
 }
 
-const ProfileSubNav = ({ routes }) => {
+const ProfileSubNav = ({ isLOA3, routes }) => {
   return (
     <nav className="va-subnav" aria-label="Secondary">
       <div>
         <h1 className="vads-u-font-size--h4">Your profile</h1>
-        <ProfileMenuItems routes={routes} />
+        <ProfileMenuItems routes={routes} isLOA3={isLOA3} />
       </div>
     </nav>
   );
 };
 
 ProfileSubNav.propTypes = {
+  isLOA3: PropTypes.bool.isRequired,
   routes: PropTypes.arrayOf(
     PropTypes.shape({
       path: PropTypes.string.isRequired,
@@ -44,4 +53,10 @@ ProfileSubNav.propTypes = {
   ).isRequired,
 };
 
-export default ProfileSubNav;
+const mapStateToProps = state => ({
+  isLOA3: isLOA3Selector(state),
+});
+
+export { ProfileSubNav };
+
+export default connect(mapStateToProps)(ProfileSubNav);
