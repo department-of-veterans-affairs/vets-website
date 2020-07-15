47c80424c6f952fa80c41fd1f702991f63d93a4e
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-07-15 00:56:20 -0400

    Tag published pacts with branch name.

--- script/publish-pacts.js
+++ script/publish-pacts.js
@@ -8,11 +8,18 @@ const consumerVersion = execSync('git rev-parse --verify HEAD')
   .toString()
   .trim();
 
+const branchName = execSync('git rev-parse --abbrev-ref HEAD')
+  .toString()
+  .trim();
+
+const tags = [branchName];
+
 const opts = {
   pactFilesOrDirs: [path.resolve(__dirname, '../pacts')],
   // pactBroker: 'http://localhost:9292',
   pactBroker: process.env.PACT_BROKER_URL,
   consumerVersion,
+  tags,
 };
 
 pact
