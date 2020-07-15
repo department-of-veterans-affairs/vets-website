e984705d9f2dd8e49cff7486d5eb9a3c852f55c7
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-07-15 10:50:43 -0500

    Fix add more copy

--- src/applications/facility-locator/components/VABenefitsCall.jsx
+++ src/applications/facility-locator/components/VABenefitsCall.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import { parsePhoneNumber } from '../utils/phoneNumbers';
+import Telephone from '@department-of-veterans-affairs/formation-react/Telephone';
+
+const { formattedPhoneNumber, extension, contact } = parsePhoneNumber(
+  '800-827-1000',
+);
 
 export default function VABenefitsCall() {
   return (
     <div>
-      <p>
+      <p className="p1">
         <strong>To check on the status of your claim or appeal</strong>, use our{' '}
         <a href="/claim-or-appeal-status">online claim status tool</a>.
       </p>
-      <p>
-        <strong>To get help with benefits</strong>, call 800-827-1000 [hyperlink
-        phone number] toll-free. We’re here Monday through Friday, 8:00 a.m. to
-        9:00 p.m. ET. If you have hearing loss, call TTY 711.
+      <p className="p1">
+        <strong>To get help with benefits</strong>, call{' '}
+        <Telephone contact={contact} extension={extension}>
+          {' '}
+          {formattedPhoneNumber}
+        </Telephone>{' '}
+        toll-free. We’re here Monday through Friday, 8:00 a.m. to 9:00 p.m. ET.
+        If you have hearing loss, call TTY 711.
+      </p>
+      <p className="p1">
+        <strong>For other benefit questions</strong>, use our online customer
+        service tool called{' '}
+        <a href="https://iris.custhelp.va.gov/">
+          IRIS (Inquiry Routing & Information System)
+        </a>
+        .
       </p>
     </div>
   );
