3160673c22e756ebe3fda5da1f74978b75435d0d
Micah Chiang <micahkchiang@gmail.com>
2020-07-16 14:39:08 -0400

    Protect routes from being accessed without va file number check.

--- src/applications/disability-benefits/686c-674/containers/App.jsx
+++ src/applications/disability-benefits/686c-674/containers/App.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-import formConfig from '../config/form';
 
-export default function App({ location, children }) {
-  return (
+import formConfig from '../config/form';
+import { verifyVaFileNumber } from '../actions';
+
+function App({ location, children, isLoggedIn, isLoading, vaFileNumber }) {
+  const content = (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
     </RoutedSavableApp>
   );
+
+  // If on intro page, just return
+  if (location.pathname === '/introduction') {
+    return content;
+  }
+
+  // Handle loading
+  if (isLoading) {
+    return <LoadingIndicator message="Loading your information..." />;
+  }
+
+  // If a user is not logged in OR
+  // a user is logged in, but hasn't gone through va file number validation
+  // redirect them to the introduction page.
+  if (
+    !isLoggedIn ||
+    (isLoggedIn && !vaFileNumber?.hasVaFileNumber?.validVaFileNumber)
+  ) {
+    document.location.replace(
+      '/view-change-dependents/add-remove-form-686c/introduction',
+    );
+    return <LoadingIndicator message="Redirecting to introduction page..." />;
+  }
+
+  return content;
 }
+
+const mapStateToProps = state => ({
+  isLoggedIn: state?.user?.login?.currentlyLoggedIn,
+  isLoading: state?.user?.profile?.loading,
+  vaFileNumber: state?.vaFileNumber,
+});
+
+export default connect(mapStateToProps)(App);
