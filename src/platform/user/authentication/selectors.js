81ff3cb0b27bf508b77b19215bd6771bcd3511a4
Dan Hinze <dan.hinze@adhocteam.us>
2020-07-16 12:43:34 -0500

    Update eBenefits links to use isAuthenticatedWithSSOe selector

--- src/platform/user/authentication/selectors.js
+++ src/platform/user/authentication/selectors.js
@@ -1,4 +1,3 @@
-import environment from 'platform/utilities/environment';
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
 import { selectProfile } from 'platform/user/selectors';
 import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
