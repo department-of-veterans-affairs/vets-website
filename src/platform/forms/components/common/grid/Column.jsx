ebdb656520286a19165644d1b8a8358641971d78
Dan Shank <danshank@gmail.com>
2020-07-16 12:15:51 -0700

    fix linting

--- src/platform/forms/components/common/grid/Column.jsx
+++ src/platform/forms/components/common/grid/Column.jsx
@@ -6,11 +6,13 @@ import React from 'react';
  * @param {string} classNames strig of classNames to be added to the columns class
  * @param {node} children content that the columns class wraps
  * @param {string} role ARIA role for component if needed
-*/
+ */
 function Column(props) {
   const { children, classNames = '', role = false } = props;
 
-  return <div className={`${classNames} columns`} role={role}>{children}</div>;
+  return (
+    <div className={`${classNames} columns`} role={role}>{children}</div>
+  );
 }
 
 export default Column;
