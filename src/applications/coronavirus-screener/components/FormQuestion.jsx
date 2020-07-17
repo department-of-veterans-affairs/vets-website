fa4b58d6a853fc212782ae2c471fff3a5e1841ae
Michael Chelen <michael.chelen@va.gov>
2020-07-17 17:05:21 -0400

    create <QuestionOptions> component to use html radio <input>

--- src/applications/coronavirus-screener/components/FormQuestion.jsx
+++ src/applications/coronavirus-screener/components/FormQuestion.jsx
@@ -28,29 +28,60 @@ export default function FormQuestion({
     }
   });
 
-  const options = optionsConfig.map((option, index) => (
-    <button
-      key={index}
-      type="button"
-      className={classnames(
-        'usa-button-big',
-        (question.value === option.optionValue ? 'usa-button' : null) ?? [
-          'usa-button-secondary',
-          'vads-u-background-color--white', // TODO: resolve upstream design system bug https://github.com/department-of-veterans-affairs/va.gov-team/issues/9610
-        ],
-      )}
-      onClick={handleClick}
-      value={option.optionValue}
-    >
-      {option.optionText}
-    </button>
+  function QuestionOptions({ questionId, options }) {
+    return options.map(option => (
+      <>
+        <input
+          id={`option-${questionId}-${option.optionValue}`}
+          key={`option-${questionId}-${option.optionValue}`}
+          type="radio"
+          onChange={handleClick}
+          name={`option-${questionId}`}
+          value={option.optionValue}
+        />
+        <label htmlFor={`option-${option.optionValue}`}>
+          {option.optionText}
+        </label>
+      </>
+    ));
+  }
+
+  const inputOptions = optionsConfig.map(option => (
+    <>
+      <label htmlFor={`option-${option.optionValue}`}>
+        {option.optionText}
+      </label>
+      <input
+        id={`option-${option.optionValue}`}
+        key={`option-${option.optionValue}`}
+        type="radio"
+        // onClick={handleClick}
+        name={'question'}
+        value={option.optionValue}
+      />
+    </>
+    // <button
+    //   key={index}
+    //   type="button"
+    //   className={classnames(
+    //     'usa-button-big',
+    //     (question.value === option.optionValue ? 'usa-button' : null) ?? [
+    //       'usa-button-secondary',
+    //       'vads-u-background-color--white', // TODO: resolve upstream design system bug https://github.com/department-of-veterans-affairs/va.gov-team/issues/9610
+    //     ],
+    //   )}
+    //   onClick={handleClick}
+    //   value={option.optionValue}
+    // >
+    //   {option.optionText}
+    // </button>
   ));
 
   return (
-    <div className="feature" id={`question-${question.id}`}>
-      <Element name={scrollElementName} />
-      <h2>{question.text}</h2>
-      {options}
-    </div>
+    <fieldset className="feature" id={`question-${question.id}`}>
+      {/* <Element name={scrollElementName} /> */}
+      <legend id={question.id}>{question.text}</legend>
+      <QuestionOptions questionId={question.id} options={optionsConfig} />
+    </fieldset>
   );
 }
