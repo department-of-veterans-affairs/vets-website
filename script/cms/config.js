80286ccbfa52b31e2711ef380b3ced1eb516136f
Dror matalon <dror@matal.com>
2020-07-17 01:27:18 +0000

    Use path resolve so we're not relying on running in a specific dir

--- script/cms/config.js
+++ script/cms/config.js
@@ -1,20 +1,33 @@
+const path = require('path');
+
 /**
  * Configuration file to indicate where to find the different artifcats of the transfomer
  */
 class Conf {
   constructor() {
     // The cashe dir where the build stores files
-    this.cacheDir = `../../.cache/localhost/`;
+    this.cacheDir = path.resolve(__dirname, `../../.cache/localhost/`);
     // The directory for the nodes that the tome sync created in the tar file
-    this.cmsExportDir = `${this.cacheDir}/cms-export-content`;
+    this.cmsExportDir = path.resolve(
+      __dirname,
+      `${this.cacheDir}/cms-export-content`,
+    );
     // The open API schema of Drupal
-    this.cmsExportSchema = `${this.cmsExportDir}/meta/schema.json`;
+    this.cmsExportSchema = path.resolve(
+      __dirname,
+      `${this.cmsExportDir}/meta/schema.json`,
+    );
     // The graphQL file created by the graphQL query
-    this.graphqlFile = `${this.cacheDir}/drupal/pages.json`;
+    this.graphqlFile = path.resolve(
+      __dirname,
+      `${this.cacheDir}/drupal/pages.json`,
+    );
     // Where to store the nodes from the ${graphQL} file
-    this.nodeFileDir = `${this.cacheDir}/drupal/nodes`;
-    this.transformerDir =
-      '../../src/site/stages/build/process-cms-exports/transformers';
+    this.nodeFileDir = path.resolve(__dirname, `${this.cacheDir}/drupal/nodes`);
+    this.transformerDir = path.resolve(
+      __dirname,
+      '../../src/site/stages/build/process-cms-exports/transformers',
+    );
   }
 }
 
