0ad67a5164fd9b4832f35a14215de55f8ff8a67e
Micah Chiang <micahkchiang@gmail.com>
2020-07-17 16:50:51 -0400

    update nesting

--- src/applications/disability-benefits/686c-674/config/chapters/report-add-a-spouse/current-marriage-information/currentMarriageInformation.js
+++ src/applications/disability-benefits/686c-674/config/chapters/report-add-a-spouse/current-marriage-information/currentMarriageInformation.js
@@ -11,84 +11,71 @@ import { locationUISchema } from '../../../location-schema';
 
 const { currentMarriageInformation } = addSpouse.properties;
 
-//export const schema = {
-  //type: 'object',
-  //properties: {
-  //  currentMarriageInformation,
-  //},
-//};
+// export const schema = {
+// type: 'object',
+// properties: {
+//  currentMarriageInformation,
+// },
+// };
 
 export const schema = {
   type: 'object',
   properties: {
-  currentMarriageInformation: {
-    type: "object",
-    properties: {
-      date: {
-        pattern: "^(\\d{4}|XXXX)-(0[1-9]|1[0-2]|XX)-(0[1-9]|[1-2][0-9]|3[0-1]|XX)$",
-        type: "string"
-      },
-      location: {
-        type: "object",
-        properties: {
-          isOutsideUS: {
-            type: "boolean",
-            default: false,
+    currentMarriageInformation: {
+      type: 'object',
+      properties: {
+        date: {
+          pattern:
+            '^(\\d{4}|XXXX)-(0[1-9]|1[0-2]|XX)-(0[1-9]|[1-2][0-9]|3[0-1]|XX)$',
+          type: 'string',
+        },
+        location: {
+          type: 'object',
+          properties: {
+            isOutsideUS: {
+              type: 'boolean',
+              default: false,
+            },
+            state: {
+              type: 'string',
+              enum: ['CA', 'AL', 'FL'],
+              enumNames: ['California', 'Alabama', 'Florida'],
+            },
+            country: {
+              type: 'string',
+              maxLength: 50,
+              pattern: '^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$',
+            },
+            city: {
+              type: 'string',
+              maxLength: 30,
+              pattern: '^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$',
+            },
           },
-          state: {
-            type: "string",
-            enum: [
-              "CA",
-              "AL",
-              "FL",
-            ],
-            enumNames: [
-              "California",
-              "Alabama",
-              "Florida",
-            ],
-          },
-          country: {
-            type: 'string',
-            maxLength: 50,
-            pattern: "^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$",
-          },
-          city: {
-            type: "string",
-            maxLength: 30,
-            pattern: "^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$"
-          }
-        }
+        },
+        type: {
+          type: 'string',
+          enum: ['CEREMONIAL', 'COMMON-LAW', 'TRIBAL', 'PROXY', 'OTHER'],
+          enumNames: [
+            'Religious or civil ceremony (minister, justice of the peace, etc.)',
+            'Common-law',
+            'Tribal',
+            'Proxy',
+            'Other',
+          ],
+        },
+        typeOther: {
+          type: 'string',
+          maxLength: 50,
+        },
+        'view:marriageTypeInformation': {
+          type: 'object',
+          properties: {},
+        },
       },
-      type: {
-        type: "string",
-        enum: [
-          "CEREMONIAL",
-          "COMMON-LAW",
-          "TRIBAL",
-          "PROXY",
-          "OTHER"
-        ],
-        enumNames: [
-          "Religious or civil ceremony (minister, justice of the peace, etc.)",
-          "Common-law",
-          "Tribal",
-          "Proxy",
-          "Other"
-        ]
-      },
-      typeOther: {
-        type: "string",
-        maxLength: 50,
-      },
-      "view:marriageTypeInformation": {
-        type: "object",
-        properties: {}
-      }
-    }
+    },
   },
-},
-}
+};
 
 export const uiSchema = {
   currentMarriageInformation: {
--- src/applications/disability-benefits/686c-674/config/chapters/report-add-a-spouse/current-spouse-marriage-history-details/currentSpouseMarriageHistoryDetails.js
+++ src/applications/disability-benefits/686c-674/config/chapters/report-add-a-spouse/current-spouse-marriage-history-details/currentSpouseMarriageHistoryDetails.js
@@ -1,191 +1,198 @@
 import currentOrPastDateUI from 'platform/forms-system/src/js/definitions/currentOrPastDate';
 import { addSpouse } from '../../../utilities';
 import { SpouseTitle } from '../../../../components/ArrayPageItemSpouseTitle';
-import { stateTitle, cityTitle } from '../../../helpers';
+import {
+  stateTitle,
+  cityTitle,
+  isChapterFieldRequired,
+} from '../../../helpers';
 import { locationUISchema } from '../../../location-schema';
+
 import { get } from 'lodash';
 
-// export const schema = addSpouse.properties.spouseMarriageHistoryDetails;
+export const schema = addSpouse.properties.spouseMarriageHistoryDetails;
 
-export const schema = {
-  type: 'object',
-  properties: {
-    spouseMarriageHistoryDetails: {
-      type: 'object',
-      properties: {
-        spouseMarriageHistory: {
-          type: 'array',
-          items: {
-            type: 'object',
-            minItems: 1,
-            properties: {
-              startDate: {
-                pattern:
-                  '^(\\d{4}|XXXX)-(0[1-9]|1[0-2]|XX)-(0[1-9]|[1-2][0-9]|3[0-1]|XX)$',
-                type: 'string',
-              },
-              startLocation: {
-                type: 'object',
-                properties: {
-                  isOutsideUS: {
-                    type: 'boolean',
-                    default: false,
-                  },
-                  state: {
-                    type: 'string',
-                    enum: ['CA', 'AL', 'FL'],
-                    enumNames: ['California', 'Alabama', 'Florida'],
-                  },
-                  country: {
-                    type: 'string',
-                    maxLength: 50,
-                    pattern: '^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$',
-                  },
-                  city: {
-                    type: 'string',
-                    maxLength: 30,
-                    pattern: '^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$',
-                  },
-                },
-              },
-              reasonMarriageEnded: {
-                type: 'string',
-                enum: ['Divorce', 'Death', 'Other'],
-                enumNames: ['Divorce', 'Death', 'Annulment/Other'],
-              },
-              reasonMarriageEndedOther: {
-                type: 'string',
-                maxLength: 50,
-              },
-              endDate: {
-                pattern:
-                  '^(\\d{4}|XXXX)-(0[1-9]|1[0-2]|XX)-(0[1-9]|[1-2][0-9]|3[0-1]|XX)$',
-                type: 'string',
-              },
-              endLocation: {
-                type: 'object',
-                properties: {
-                  isOutsideUS: {
-                    type: 'boolean',
-                    default: false,
-                  },
-                  state: {
-                    type: 'string',
-                    enum: ['CA', 'AL', 'FL'],
-                    enumNames: ['California', 'Alabama', 'Florida'],
-                  },
-                  country: {
-                    type: 'string',
-                    maxLength: 50,
-                    pattern: '^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$',
-                  },
-                  city: {
-                    type: 'string',
-                    maxLength: 30,
-                    pattern: '^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$',
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-};
+// export const schema = {
+//   type: 'object',
+//   properties: {
+//     spouseMarriageHistoryDetails: {
+//       type: 'object',
+//       properties: {
+//         spouseMarriageHistory: {
+//           type: 'array',
+//           items: {
+//             type: 'object',
+//             minItems: 1,
+//             properties: {
+//               startDate: {
+//                 pattern:
+//                   '^(\\d{4}|XXXX)-(0[1-9]|1[0-2]|XX)-(0[1-9]|[1-2][0-9]|3[0-1]|XX)$',
+//                 type: 'string',
+//               },
+//               startLocation: {
+//                 type: 'object',
+//                 properties: {
+//                   isOutsideUS: {
+//                     type: 'boolean',
+//                     default: false,
+//                   },
+//                   state: {
+//                     type: 'string',
+//                     enum: ['CA', 'AL', 'FL'],
+//                     enumNames: ['California', 'Alabama', 'Florida'],
+//                   },
+//                   country: {
+//                     type: 'string',
+//                     maxLength: 50,
+//                     pattern: '^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$',
+//                   },
+//                   city: {
+//                     type: 'string',
+//                     maxLength: 30,
+//                     pattern: '^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$',
+//                   },
+//                 },
+//               },
+//               reasonMarriageEnded: {
+//                 type: 'string',
+//                 enum: ['Divorce', 'Death', 'Other'],
+//                 enumNames: ['Divorce', 'Death', 'Annulment/Other'],
+//               },
+//               reasonMarriageEndedOther: {
+//                 type: 'string',
+//                 maxLength: 50,
+//               },
+//               endDate: {
+//                 pattern:
+//                   '^(\\d{4}|XXXX)-(0[1-9]|1[0-2]|XX)-(0[1-9]|[1-2][0-9]|3[0-1]|XX)$',
+//                 type: 'string',
+//               },
+//               endLocation: {
+//                 type: 'object',
+//                 properties: {
+//                   isOutsideUS: {
+//                     type: 'boolean',
+//                     default: false,
+//                   },
+//                   state: {
+//                     type: 'string',
+//                     enum: ['CA', 'AL', 'FL'],
+//                     enumNames: ['California', 'Alabama', 'Florida'],
+//                   },
+//                   country: {
+//                     type: 'string',
+//                     maxLength: 50,
+//                     pattern: '^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$',
+//                   },
+//                   city: {
+//                     type: 'string',
+//                     maxLength: 30,
+//                     pattern: '^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$',
+//                   },
+//                 },
+//               },
+//             },
+//           },
+//         },
+//       },
+//     },
+//   },
+// };
 
 export const uiSchema = {
-  spouseMarriageHistoryDetails: {
-    spouseMarriageHistory: {
-      items: {
-        'ui:title': SpouseTitle,
-        startDate: {
-          ...currentOrPastDateUI('Date of marriage'),
-          ...{
-            'ui:required': formData => formData.spouseWasMarriedBefore,
-          },
+  spouseMarriageHistory: {
+    items: {
+      'ui:title': SpouseTitle,
+      startDate: {
+        ...currentOrPastDateUI('Date of marriage'),
+        ...{
+          'ui:required': formData => formData.spouseWasMarriedBefore,
         },
-        startLocation: {
-          'ui:title': 'Where were you married?',
-          isOutsideUS: {
-            'ui:title': 'This occurred outsite the US',
-          },
-          country: {
-            'ui:title': 'Country',
+      },
+      startLocation: {
+        'ui:title': 'Where were you married?',
+        isOutsideUS: {
+          'ui:title': 'This occurred outsite the US',
+        },
+        country: {
+          'ui:title': 'Country',
 
-            'ui:options': {
-              hideIf: (formData, index) => {
-                const isOutsideUS = get(
-                  formData,
-                  'pouseMarriageHistory[`${index}`].startLocation.isOutsideUS',
-                  false,
-                );
-                if (isOutsideUS) {
-                  return true;
-                }
-              },
+          'ui:options': {
+            hideIf: (formData, index) => {
+              const isOutsideUS = get(
+                formData,
+                'spouseMarriageHistory[' +
+                  `${index}` +
+                  '].startLocation.isOutsideUS',
+                false,
+              );
+              if (isOutsideUS) {
+                return true;
+              }
+              return false;
             },
           },
-          state: {
-            'ui:title': 'State',
+        },
+        state: {
+          'ui:title': 'State',
 
-            'ui:options': {
-              hideIf: (formData, index) => {
-                const isOutsideUS = get(
-                  formData,
-                  'pouseMarriageHistory[`${index}`].startLocation.isOutsideUS',
-                  true,
-                );
-                if (isOutsideUS) {
-                  return true;
-                }
-              },
+          'ui:options': {
+            hideIf: (formData, index) => {
+              const isOutsideUS = get(
+                formData,
+                `spouseMarriageHistory[${index}].startLocation.isOutsideUS`,
+                true,
+              );
+              if (isOutsideUS) {
+                return true;
+              }
+              return false;
             },
           },
-          city: {
-            'ui:required': formData =>
-              isChapterFieldRequired(formData, 'addSpouse'),
-            'ui:title': 'City',
-          },
         },
-        reasonMarriageEnded: {
-          'ui:required': formData => formData.spouseWasMarriedBefore === true,
-          'ui:title': 'Reason marriage ended',
-          'ui:widget': 'radio',
-          'ui:options': {
-            updateSchema: () => ({
-              enumNames: ['Divorce', 'Death', 'Annulment or other'],
-            }),
-          },
+        city: {
+          'ui:required': formData =>
+            isChapterFieldRequired(formData, 'addSpouse'),
+          'ui:title': 'City',
         },
-        reasonMarriageEndedOther: {
-          'ui:required': (formData, index) =>
-            formData.spouseMarriageHistory[`${index}`].reasonMarriageEnded ===
-            'Other',
-          'ui:title': 'Please give a brief explanation',
-          'ui:options': {
-            expandUnder: 'reasonMarriageEnded',
-            expandUnderCondition: 'Other',
-            keepInPageOnReview: true,
-            widgetClassNames: 'vads-u-margin-y--0',
-          },
+      },
+      reasonMarriageEnded: {
+        'ui:required': formData => formData.spouseWasMarriedBefore === true,
+        'ui:title': 'Reason marriage ended',
+        'ui:widget': 'radio',
+        'ui:options': {
+          updateSchema: () => ({
+            enumNames: ['Divorce', 'Death', 'Annulment or other'],
+          }),
         },
-        endDate: {
-          ...currentOrPastDateUI('Date marriage ended'),
-          ...{
-            'ui:required': formData => formData.spouseWasMarriedBefore,
-          },
+      },
+      reasonMarriageEndedOther: {
+        'ui:required': (formData, index) =>
+          formData.spouseMarriageHistory[`${index}`].reasonMarriageEnded ===
+          'Other',
+        'ui:title': 'Please give a brief explanation',
+        'ui:options': {
+          expandUnder: 'reasonMarriageEnded',
+          expandUnderCondition: 'Other',
+          keepInPageOnReview: true,
+          widgetClassNames: 'vads-u-margin-y--0',
         },
-        endLocation: {
-          'ui:title': 'Place marriage with former spouse ended',
-          state: {
-            'ui:required': formData => formData.spouseWasMarriedBefore,
-            'ui:title': stateTitle,
-          },
-          city: {
-            'ui:required': formData => formData.spouseWasMarriedBefore,
-            'ui:title': cityTitle,
-          },
+      },
+      endDate: {
+        ...currentOrPastDateUI('Date marriage ended'),
+        ...{
+          'ui:required': formData => formData.spouseWasMarriedBefore,
+        },
+      },
+      endLocation: {
+        'ui:title': 'Place marriage with former spouse ended',
+        state: {
+          'ui:required': formData => formData.spouseWasMarriedBefore,
+          'ui:title': stateTitle,
+        },
+        city: {
+          'ui:required': formData => formData.spouseWasMarriedBefore,
+          'ui:title': cityTitle,
         },
       },
     },
