62ecc5b71539e11cfa40adfb62ad53f5c8b45fbf
Christopher Valarida <chris.valarida@adhocteam.us>
2020-07-17 13:45:39 -0700

    Remove entityUrl

--- src/site/stages/build/process-cms-exports/transformers/node-event_listing.js
+++ src/site/stages/build/process-cms-exports/transformers/node-event_listing.js
@@ -7,11 +7,6 @@ const {
 const transform = entity => ({
   entityType: 'node',
   entityBundle: 'event_listing',
-  entityUrl: {
-    // TODO: Get the breadcrumb from the CMS export when it's available
-    breadcrumb: [],
-    path: entity.path[0].alias,
-  },
   entityMetatags: createMetaTagArray(entity.metatag.value),
   title: getDrupalValue(entity.title),
   changed: utcToEpochTime(getDrupalValue(entity.changed)),
@@ -19,13 +14,6 @@ const transform = entity => ({
   fieldOffice: entity.fieldOffice[0],
 });
 module.exports = {
-  filter: [
-    'title',
-    'changed',
-    'metatag',
-    'path',
-    'field_intro_text',
-    'field_office',
-  ],
+  filter: ['title', 'changed', 'metatag', 'field_intro_text', 'field_office'],
   transform,
 };
