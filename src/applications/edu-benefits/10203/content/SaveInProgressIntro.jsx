72b859b255f6b7ee5079b4f17d6447ff6bbfefdd
mr0sari0 <mrosario@governmentcio.com>
2020-07-20 18:05:36 -0400

    revert path change

--- src/applications/edu-benefits/10203/content/SaveInProgressIntro.jsx
+++ src/applications/edu-benefits/10203/content/SaveInProgressIntro.jsx
@@ -27,7 +27,7 @@ import DowntimeMessage from 'platform/forms/save-in-progress/DowntimeMessage';
 import {
   APP_TYPE_DEFAULT,
   UNAUTH_SIGN_IN_DEFAULT_MESSAGE,
-} from 'platform/forms/constants';
+} from 'platform/forms/save-in-progress/constants';
 
 class SaveInProgressIntro extends React.Component {
   getAlert = savedForm => {
