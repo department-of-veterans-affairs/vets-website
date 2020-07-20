b277e4e7f8ed2ca8a8a807e94e63c1a79eba650c
Jeff Balboni <jeff@adhocteam.us>
2020-07-20 11:30:06 -0400

    Update prompt

--- src/applications/vaos/components/RequestExpressCare.jsx
+++ src/applications/vaos/components/RequestExpressCare.jsx
@@ -9,15 +9,23 @@ export default function RequestExpressCare() {
   return (
     <div className="vads-u-padding-y--3 vads-u-border-top--1px vads-u-border-bottom--1px vads-u-border-color--gray-lighter">
       <h2 className="vads-u-font-size--h3 vads-u-margin-y--0">
-        Request an Express Care screening
+        Create a new Express Care request
       </h2>
       <p>
         Have a health concern that you need help with today, but isn’t an
         emergency? Submit a request for a same-day Express Care screening with a
-        VA health care provider. You can sumbit your request Monday through
-        Friday, 00:00 to 00:00. You’ll receive a phone screening between 00:00
-        and 00:00 the same day. If needed, we may schedule a follow-up phone,
-        video, or in-person visit or other care.
+        VA health care provider.
+        <ul>
+          <li>
+            You can submit your request [Day] through [Day], [Hours] to [Hours]
+            a.m. [time zone].
+          </li>
+          <li>A VA health care provider will call you today.</li>
+          <li>
+            If needed, we may schedule a follow-up phone, video, or in-person
+            visit or other care.
+          </li>
+        </ul>
       </p>
       <a
         className="usa-button"
@@ -25,7 +33,7 @@ export default function RequestExpressCare() {
         target="_blank"
         rel="noreferrer nofollow"
       >
-        Request an Express Care screening
+        Request Express Care
       </a>
     </div>
   );
