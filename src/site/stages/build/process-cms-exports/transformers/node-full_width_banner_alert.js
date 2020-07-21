c3f325c44342705c23ff4b887e86e5a95d49678a
jhonnyoddball <jhonny.gonzalez@oddball.io>
2020-07-21 10:50:03 -0400

    New transformer for node-full_width_banner_alert

---
+++ src/site/stages/build/process-cms-exports/transformers/node-full_width_banner_alert.js
@@ -0,0 +1,60 @@
+const {
+  getDrupalValue,
+  createMetaTagArray,
+  utcToEpochTime,
+} = require('./helpers');
+
+const transform = entity => ({
+  entityType: 'node',
+  entityBundle: 'full_width_banner_alert',
+  title: getDrupalValue(entity.title),
+  changed: utcToEpochTime(getDrupalValue(entity.changed)),
+  entityMetatags: createMetaTagArray(entity.metatag.value),
+  path: getDrupalValue(entity.path),
+  fieldAdministration: getDrupalValue(entity.fieldAdministration),
+  fieldAlertDismissable: getDrupalValue(entity.fieldAlertDismissable),
+  fieldAlertEmailUpdatesButton: getDrupalValue(
+    entity.fieldAlertEmailUpdatesButton,
+  ),
+  fieldAlertFindFacilitiesCta: getDrupalValue(
+    entity.fieldAlertFindFacilitiesCta,
+  ),
+  fieldAlertInheritanceSubpages: getDrupalValue(
+    entity.fieldAlertInheritanceSubpages,
+  ),
+  fieldAlertOperatingStatusCta: getDrupalValue(
+    entity.fieldAlertOperatingStatusCta,
+  ),
+  fieldAlertType: getDrupalValue(entity.fieldAlertType),
+  fieldBannerAlertComputdvalues: getDrupalValue(
+    entity.fieldBannerAlertComputdvalues,
+  ),
+  fieldBannerAlertVamcs: getDrupalValue(entity.fieldBannerAlertVamcs),
+  fieldBody: getDrupalValue(entity.fieldBody),
+  fieldOperatingStatusSendemail: getDrupalValue(
+    entity.fieldOperatingStatusSendemail,
+  ),
+  fieldSituationUpdates: getDrupalValue(entity.fieldSituationUpdates),
+});
+
+module.exports = {
+  filter: [
+    'title',
+    'changed',
+    'metatag',
+    'path',
+    'field_administration',
+    'field_alert_dismissable',
+    'field_alert_email_updates_button',
+    'field_alert_find_facilities_cta',
+    'field_alert_inheritance_subpages',
+    'field_alert_operating_status_cta',
+    'field_alert_type',
+    'field_banner_alert_computdvalues',
+    'field_banner_alert_vamcs',
+    'field_body',
+    'field_operating_status_sendemail',
+    'field_situation_updates',
+  ],
+  transform,
+};
