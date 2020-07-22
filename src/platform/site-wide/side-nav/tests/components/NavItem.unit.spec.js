adba4725d078f286bc261aa61c221a79bb59cc41
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2020-07-22 14:48:53 -0400

    Hide VAMC SideNav sub-items on mobile (#13514)
    * Hide VAMC SideNav sub-items on mobile
    
    https://github.com/department-of-veterans-affairs/va.gov-team/issues/10997
    
    Previously, the VAMC SideNav sub-items were not being hidden
    from keyboard nav or screen readers. This could leave users
    trapped in the sub-items if navigating with a keyboard, and
    caused screen readers to read the sub-items even though they
    were not clickable.
    
    * Implement debounce
    
    * set resize debounce timeout to 250ms, remove comments
    
    * Add unit tests
    
    * rename fn
    
    Co-authored-by: Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>

--- src/platform/site-wide/side-nav/tests/components/NavItem.unit.spec.js
+++ src/platform/site-wide/side-nav/tests/components/NavItem.unit.spec.js
@@ -41,4 +41,66 @@ describe('<NavItem>', () => {
     expect(wrapper.contains(<ul />)).to.equal(true);
     wrapper.unmount();
   });
+
+  it('should render child nav item, vads-u-display--none is absent', () => {
+    const noop = () => {};
+
+    const item = {
+      description: 'Some description',
+      expanded: true,
+      hasChildren: true,
+      href: '/pittsburgh-health-care',
+      id: uniqueId('sidenav_'),
+      isSelected: true,
+      label: 'Location',
+      order: 0,
+      parentID: uniqueId('sidenav_'),
+    };
+
+    const props = {
+      depth: 2,
+      item,
+      index: 1,
+      renderChildItems: noop,
+      sortedNavItems: [item],
+      trackEvents: noop,
+      navExpanded: true,
+    };
+
+    const wrapper = shallow(<NavItem {...props} />);
+    const li = wrapper.find('li');
+    expect(li.hasClass('vads-u-display--none')).to.equal(false);
+    wrapper.unmount();
+  });
+
+  it('should render child nav item, vads-u-display--none is present.', () => {
+    const noop = () => {};
+
+    const item = {
+      description: 'Some description',
+      expanded: true,
+      hasChildren: true,
+      href: '/pittsburgh-health-care',
+      id: uniqueId('sidenav_'),
+      isSelected: true,
+      label: 'Location',
+      order: 0,
+      parentID: uniqueId('sidenav_'),
+    };
+
+    const props = {
+      depth: 2,
+      item,
+      index: 1,
+      renderChildItems: noop,
+      sortedNavItems: [item],
+      trackEvents: noop,
+      navExpanded: false,
+    };
+
+    const wrapper = shallow(<NavItem {...props} />);
+    const li = wrapper.find('li');
+    expect(li.hasClass('vads-u-display--none')).to.equal(true);
+    wrapper.unmount();
+  });
 });
