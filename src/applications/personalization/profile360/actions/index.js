080b7a96037366fcc1e4b46f4a4a9c9b3bca2ac5
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2020-07-24 12:39:30 -0600

    Hide Header when errors fetching full name

--- src/applications/personalization/profile360/actions/index.js
+++ src/applications/personalization/profile360/actions/index.js
@@ -1,7 +1,9 @@
 import { getData } from '../util';
 import environment from 'platform/utilities/environment';
 
+export const FETCH_HERO = 'FETCH_HERO';
 export const FETCH_HERO_SUCCESS = 'FETCH_HERO_SUCCESS';
+export const FETCH_HERO_FAILED = 'FETCH_HERO_FAILED';
 export const FETCH_PERSONAL_INFORMATION_SUCCESS =
   'FETCH_PERSONAL_INFORMATION_SUCCESS';
 export const FETCH_MILITARY_INFORMATION_SUCCESS =
@@ -11,12 +13,15 @@ export const FETCH_ADDRESS_CONSTANTS_SUCCESS =
 
 export function fetchHero() {
   return async dispatch => {
-    dispatch({
-      type: FETCH_HERO_SUCCESS,
-      hero: {
-        userFullName: await getData('/profile/full_name'),
-      },
-    });
+    dispatch({ type: FETCH_HERO });
+
+    await getData('/profile/full_name')
+      .then(data => {
+        dispatch({ type: FETCH_HERO_SUCCESS, hero: { userFullName: data } });
+      })
+      .catch(errors => {
+        dispatch({ type: FETCH_HERO_FAILED, hero: { errors } });
+      });
   };
 }
 
