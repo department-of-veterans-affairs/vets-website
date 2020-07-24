b338267920d2e2d54217c006a00bb18d3e56af9a
Dan Shank <danshank@gmail.com>
2020-07-23 23:18:02 -0700

    linting, yay!

--- src/platform/utilities/error-boundary/index.jsx
+++ src/platform/utilities/error-boundary/index.jsx
@@ -4,23 +4,20 @@ import * as Sentry from '@sentry/react';
 
 // default fallback component
 function FallbackComponent() {
-  return (<div />);
-  
-  return (
-    <div>An error has occured</div>
-  )
+  return <div />;
 }
 
-class ErrorBoundary extends React.Component {
-  render() {
-    const { children, fallback, showDialog = false } = this.props;
+function ErrorBoundary(props) {
+  const { children, fallback, showDialog = false } = props;
 
-    return (
-      <Sentry.ErrorBoundary fallback={fallback || FallbackComponent} showDialog={showDialog}>
-        {children}
-      </Sentry.ErrorBoundary>
-    )
-  }
+  return (
+    <Sentry.ErrorBoundary
+      fallback={fallback || FallbackComponent}
+      showDialog={showDialog}
+    >
+      {children}
+    </Sentry.ErrorBoundary>
+  );
 }
 
 export default ErrorBoundary;
