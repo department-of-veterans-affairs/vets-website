201a1786756aa63c0010219b12c14ad809503ba2
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-07-25 03:53:23 -0400

    Updated HCA contract tests to add save-in-progress test and use new helpers.

--- src/applications/hca/tests/hca.contract.spec.js
+++ src/applications/hca/tests/hca.contract.spec.js
@@ -1,97 +1,96 @@
-import { Pact, Matchers } from '@pact-foundation/pact';
-import { expect } from 'chai';
+import { Matchers } from '@pact-foundation/pact';
 
 import contractTest from 'platform/testing/contract';
-import environment from 'platform/utilities/environment';
+import {
+  setupFormSubmitTest,
+  setupInProgressFormTest,
+} from 'platform/testing/contract/helpers';
+
+import formConfig from '../config/form';
+import testData from './schema/maximal-test.json';
+
+const { boolean, integer, iso8601DateTimeWithMillis } = Matchers;
 
 contractTest('HCA', 'VA.gov API', mockApi => {
-  const { like, iso8601DateTimeWithMillis } = Matchers;
+  describe('POST /v0/health_care_applications', () => {
+    context('without attachments', () => {
+      it('responds with success', async () => {
+        const [requestBody, testFormSubmit] = setupFormSubmitTest(
+          formConfig,
+          testData,
+        );
 
-  const PAYLOAD = {
-    asyncCompatible: true,
-    form: JSON.stringify({
-      isEssentialAcaCoverage: false,
-      vaMedicalFacility: '523GC',
-      wantsInitialVaContact: false,
-      isCoveredByHealthInsurance: false,
-      isMedicaidEligible: true,
-      isEnrolledMedicarePartA: false,
-      deductibleMedicalExpenses: 0,
-      deductibleFuneralExpenses: 0,
-      deductibleEducationExpenses: 0,
-      veteranGrossIncome: 50000,
-      veteranNetIncome: 0,
-      veteranOtherIncome: 0,
-      dependents: [],
-      gender: 'F',
-      maritalStatus: 'Never Married',
-      isSpanishHispanicLatino: false,
-      discloseFinancialInformation: true,
-      vaCompensationType: 'lowDisability',
-      lastServiceBranch: 'air force',
-      lastEntryDate: '1970-01-01',
-      lastDischargeDate: '1990-01-01',
-      dischargeType: 'honorable',
-      veteranAddress: {
-        street: '123 Cambridge St',
-        city: 'Boston',
-        postalCode: '02182',
-        country: 'USA',
-        state: 'MA',
-      },
-      veteranDateOfBirth: '1900-01-01',
-      veteranSocialSecurityNumber: '012349232',
-      veteranFullName: {
-        first: 'First',
-        last: 'Last',
-      },
-      privacyAgreementAccepted: true,
-    }),
-  };
+        const responseBody = {
+          success: true,
+          formSubmissionId: integer(3806115661),
+          timestamp: iso8601DateTimeWithMillis(),
+        };
 
-  const EXPECTED_BODY = {
-    success: true,
-    formSubmissionId: like(3806115661),
-    timestamp: iso8601DateTimeWithMillis(),
-  };
+        await mockApi.addInteraction({
+          state: 'enrollment service is up',
+          uponReceiving: 'a request to submit a health care application',
+          withRequest: {
+            method: 'POST',
+            path: '/v0/health_care_applications',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-Key-Inflection': 'camel',
+            },
+            body: requestBody,
+          },
+          willRespondWith: {
+            status: 200,
+            body: responseBody,
+          },
+        });
 
-  const interactions = {
-    SUBMISSION_WITHOUT_ATTACHMENTS: {
-      state: 'enrollment service is up',
-      uponReceiving: 'an application for health care',
-      withRequest: {
-        method: 'POST',
-        path: '/v0/health_care_applications',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Key-Inflection': 'camel',
+        await testFormSubmit();
+      });
+    });
+
+    // context('with attachments', () => {});
+  });
+
+  describe('PUT /v0/in_progress_forms', () => {
+    it('responds with success', async () => {
+      const [requestBody, testSaveInProgress] = setupInProgressFormTest(
+        formConfig,
+        testData,
+      );
+
+      const responseBody = {
+        data: {
+          attributes: {
+            formData: testData,
+            metaData: {
+              prefill: boolean(true),
+              returnUrl: integer('/veteran-information'),
+              version: integer(formConfig.version),
+            },
+          },
         },
-        body: PAYLOAD,
-      },
-      willRespondWith: {
-        status: 200,
-        body: EXPECTED_BODY,
-      },
-    },
-  };
+      };
 
-  describe('POST /health_care_applications', () => {
-    it('without attachments responds with success', async () => {
-      await mockApi.addInteraction(interactions.SUBMISSION_WITHOUT_ATTACHMENTS);
-
-      const response = await fetch(
-        `${environment.API_URL}/v0/health_care_applications`,
-        {
-          method: 'POST',
+      await mockApi.addInteraction({
+        state: 'enrollment service is up',
+        uponReceiving:
+          'a request to save an in-progress health care application',
+        withRequest: {
+          method: 'PUT',
+          path: `/v0/in_progress_forms/${formConfig.formId}`,
           headers: {
             'Content-Type': 'application/json',
             'X-Key-Inflection': 'camel',
           },
-          body: JSON.stringify(PAYLOAD),
+          body: requestBody,
         },
-      );
+        willRespondWith: {
+          status: 200,
+          body: responseBody,
+        },
+      });
 
-      expect(response.ok).to.be.true;
+      await testSaveInProgress();
     });
   });
 });
