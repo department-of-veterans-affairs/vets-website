ed0c3761184d4a65506de01d40b6312a865cfa0c
mr0sari0 <mrosario@governmentcio.com>
2020-07-26 02:58:25 -0400

    add nested ui schema helper and default registry

--- src/platform/forms-system/src/js/review/StringField.jsx
+++ src/platform/forms-system/src/js/review/StringField.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import _ from 'lodash/fp'; // eslint-disable-line no-restricted-imports
-
+import { checkForNestedUISchema } from '../helpers';
 import {
   getUiOptions,
   getWidget,
   optionsList,
+  getDefaultRegistry,
 } from '@department-of-veterans-affairs/react-jsonschema-form/lib/utils';
-
-import { getNestedUISchema } from './ReviewFieldTemplate';
 
 /*
  * This is a minimal string field implementation for the
@@ -15,20 +14,23 @@ import { getNestedUISchema } from './ReviewFieldTemplate';
  * in the config
  */
 export default function StringField(props) {
-  const { registry, schema, uiSchema, formData } = props;
-  const realUiSchema = getNestedUISchema(uiSchema);
+  const { registry = getDefaultRegistry(), schema, uiSchema, formData } = props;
+  const realUiSchema = checkForNestedUISchema(uiSchema);
   const uiOptions = getUiOptions(realUiSchema);
   const labels = uiOptions.labels || {};
   const enumOptions = Array.isArray(schema.enum) && optionsList(schema);
+  let Widget =
+    _.get('ui:reviewWidget', realUiSchema) || _.get('widget', uiOptions);
 
-  let Widget = _.get('ui:reviewWidget', realUiSchema);
   if (!Widget) {
-    const defaultWidget = schema.format || (enumOptions ? 'select' : 'text');
-    Widget = getWidget(
-      schema,
-      uiOptions.widget || defaultWidget,
-      registry.widgets,
-    );
+    if (schema.type !== 'object') {
+      const defaultWidget = schema.format || (enumOptions ? 'select' : 'text');
+      Widget = getWidget(schema, defaultWidget, registry.widgets);
+    } else {
+      throw Error(
+        'There are no default widgets available for object type schemas. Please provide either a ui:reviewWidget or a ui:options widget in your uiSchema.',
+      );
+    }
   }
 
   return (
