899cc94e273e189fdbcb54d47d72dc4969b58188
mr0sari0 <mrosario@governmentcio.com>
2020-07-26 02:41:14 -0400

    utilize nested schema helper function

--- src/platform/forms-system/src/js/review/ReviewFieldTemplate.jsx
+++ src/platform/forms-system/src/js/review/ReviewFieldTemplate.jsx
@@ -1,43 +1,28 @@
 import React from 'react';
+import { checkForNestedUISchema } from '../helpers';
+
 /*
  * This is the template for each field (which in the schema library means label + widget)
  */
 
-// The uiSchema passed in from props could be a nested object
-export const getNestedUISchema = uiSchema => {
-  const objKeys = Object.keys(uiSchema);
-  const isThisAUISchema = objKeys.find(objKey => objKey.includes('ui:'));
-  let nestedUISchema;
-  if (!isThisAUISchema) {
-    const nestedObjKeys = Object.keys(uiSchema);
-    nestedObjKeys.forEach(nestedObjKey => {
-      nestedUISchema = uiSchema[nestedObjKey];
-      getNestedUISchema(nestedUISchema);
-    });
-  }
-  return nestedUISchema || uiSchema;
-};
 export default function ReviewFieldTemplate(props) {
   const { children, uiSchema, schema } = props;
-  const realUISchema = getNestedUISchema(uiSchema);
+  const realUISchema = checkForNestedUISchema(uiSchema);
   const label = realUISchema['ui:title'] || props.label;
   const description = realUISchema['ui:description'];
   const textDescription = typeof description === 'string' ? description : null;
   const DescriptionField =
     typeof description === 'function' ? realUISchema['ui:description'] : null;
 
-  // `hideEmptyValueInReview` option is ignored if a 'ui:reviewField' is defined
-  // give priority to defined `ui:reviewField components`
   if (realUISchema?.['ui:reviewField']) {
     return realUISchema['ui:reviewField'](props);
   }
-
   if (schema.type === 'object' || schema.type === 'array') {
     return children;
   }
 
   // The custom reviewField should handle empty values
-
+  // `hideEmptyValueInReview` option is ignored if a 'ui:reviewField' is defined
   if (realUISchema?.['ui:options']?.hideEmptyValueInReview) {
     let value = children;
     if (typeof children !== 'undefined') {
