a45074cd981c1b7fbde17f2c41553cd09b832951
Narin Ratana <n.ratana@gmail.com>
2020-07-26 22:13:00 -0700

    Setup cancel button redirect

--- src/applications/vaos/containers/ExpressCareInfo.jsx
+++ src/applications/vaos/containers/ExpressCareInfo.jsx
@@ -3,21 +3,38 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import FormButtons from '../components/express-care/FormButtons';
 
-import { routeToNextAppointmentPage } from '../actions/expressCare';
+import {
+  routeToNextAppointmentPage,
+  routeToPreviousAppointmentPage,
+} from '../actions/expressCare';
 
-function ExpressCareInfo() {
-  return (
-    <div>
-      <h1>How Express Care Works</h1>
-      <FormButtons
-        backButtonText="Cancel"
-        nextButtonText="Continue with Express Care request"
-      />
-    </div>
-  );
+const pageKey = 'info';
+
+class ExpressCareInfo extends React.Component {
+  goBack = () => {
+    this.props.routeToPreviousAppointmentPage(this.props.router, pageKey);
+  };
+
+  goForward = () => {
+    this.props.routeToNextAppointmentPage(this.props.router, pageKey);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>How Express Care Works</h1>
+        <FormButtons
+          backButtonText="Cancel"
+          nextButtonText="Continue with Express Care request"
+          onBack={this.goBack}
+        />
+      </div>
+    );
+  }
 }
 const mapDispatchToProps = {
   routeToNextAppointmentPage,
+  routeToPreviousAppointmentPage,
 };
 
 export default connect(
