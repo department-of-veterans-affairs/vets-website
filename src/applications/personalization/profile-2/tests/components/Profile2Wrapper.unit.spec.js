617af97361278468db42f295ad18c71d68c019bd
Sandra Hallie <sandra.adamshallie@gmail.com>
2020-07-27 09:42:41 -0600

    Update unit test

--- src/applications/personalization/profile-2/tests/components/Profile2Wrapper.unit.spec.js
+++ src/applications/personalization/profile-2/tests/components/Profile2Wrapper.unit.spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import { expect } from 'chai';
 
@@ -7,16 +8,29 @@ import getRoutes from '../../routes';
 import { PROFILE_PATHS } from '../../constants';
 
 import { renderWithProfileReducers as render } from '../unit-test-helpers';
+import { Test } from 'mocha';
 
 describe('Profile2Wrapper', () => {
   it('should render the correct breadcrumb (Personal and contact Information)', () => {
     const config = {};
+
+    const initialState = {
+      vaProfile: {
+        hero: {
+          fullUserName: {
+            first: 'Test',
+            last: 'Test',
+          },
+        },
+      },
+    };
+
     const ui = (
       <MemoryRouter initialEntries={[PROFILE_PATHS.PERSONAL_INFORMATION]}>
         <Profile2Wrapper routes={getRoutes(config)} />
       </MemoryRouter>
     );
-    const { getByTestId } = render(ui);
+    const { getByTestId } = render(ui, { initialState });
 
     const breadcrumbs = getByTestId('breadcrumbs');
 
