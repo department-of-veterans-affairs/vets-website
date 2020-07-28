96f66d105b6ce4058bc8ebacb92789779363919f
mr0sari0 <mrosario@governmentcio.com>
2020-07-27 21:46:02 -0400

    add normal timeout to e2e

--- src/applications/edu-benefits/tests/1990e/01-all-fields.e2e.spec.js
+++ src/applications/edu-benefits/tests/1990e/01-all-fields.e2e.spec.js
@@ -16,8 +16,8 @@ module.exports = E2eHelpers.createE2eTest(client => {
       }/education/apply-for-education-benefits/application/1990E`,
     )
     .waitForElementVisible('body', Timeouts.normal)
-    .waitForElementVisible('skip-wizard-link')
-    .click('.skip-wizard-link')
+    .waitForElementVisible('skip-wizard-link', Timeouts.normal)
+    .click('.skip-wizard-link', Timeouts.normal)
     .waitForElementVisible('.schemaform-start-button', Timeouts.slow)
     .axeCheck('.main')
     .click('.schemaform-start-button');
