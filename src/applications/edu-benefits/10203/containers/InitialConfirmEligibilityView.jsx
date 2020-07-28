b82dc531353c77cf3915b48d4d8a73894099789f
Devin McCurdy <mccurdy_devin@bah.com>
2020-07-28 08:09:31 -0400

    10203 chapter 2 confirm eligibility page

---
+++ src/applications/edu-benefits/10203/containers/InitialConfirmEligibilityView.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function InitialConfirmEligibilityView(props) {
+  if (props.onReviewPage) {
+    return null;
+  }
+  return (
+    <div>
+      <div>
+        <div className="usa-alert usa-alert-warning">
+          <div className="usa-alert-body">
+            <h4 className="usa-alert-heading">
+              Based on your response, you may not be eligible for the Rogers
+              STEM Scholarship
+            </h4>
+            <div className="usa-alert-text">
+              <p>
+                <strong>
+                  You must be a Post-9/11 GI Bill or Fry Scholarship beneficiary
+                  to qualify for the scholarship.
+                </strong>{' '}
+                Please consider that ineligible applications delay the
+                processing of benefits for eligible applicants.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div>
+        <a
+          className={'usa-button-primary wizard-button va-button-primary'}
+          href="/education/"
+          target="_self"
+          rel="noopener noreferrer"
+        >
+          Exit application
+        </a>
+      </div>
+      <br />
+      <span>
+        If you'd still like to apply, you can continue with your application.
+      </span>
+    </div>
+  );
+}
+
+const mapStateToProps = (state, props) => {
+  return {
+    onReviewPage: props?.formContext?.onReviewPage,
+  };
+};
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(InitialConfirmEligibilityView);
