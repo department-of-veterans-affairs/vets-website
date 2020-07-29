ffcd648562801ed9f2413cc696993cf648f0ad3c
Devin McCurdy <mccurdy_devin@bah.com>
2020-07-29 13:21:12 -0400

    Update submit GA data

--- src/applications/edu-benefits/1995/helpers.jsx
+++ src/applications/edu-benefits/1995/helpers.jsx
@@ -1,4 +1,5 @@
 import environment from 'platform/utilities/environment';
+import _ from 'lodash';
 
 // 1995-STEM related
 const isEdithNourseRogersScholarship = form =>
@@ -27,3 +28,45 @@ export const display1995StemFlow = form =>
   isEdithNourseRogersScholarship(form) &&
   (isEligibleForEdithNourseRogersScholarship(form) ||
     determineEligibilityFor1995Stem(form));
+
+export const buildSubmitEventData = formData => {
+  const yesNoOrUndefined = value => {
+    if (value === undefined) {
+      return undefined;
+    }
+    return value ? 'Yes' : 'No';
+  };
+
+  return {
+    'benefits-used-recently': formData.benefit,
+    'new-service-periods-to-record': yesNoOrUndefined(
+      formData['view:newService'],
+    ),
+    'service-details': _.get(formData, 'toursOfDuty', []).map(tour => ({
+      'service-branch': tour.serviceBranch,
+      'service-start-date': tour.dateRange.from,
+      'service-end-date': tour.dateRange.to,
+    })),
+    'service-before-1978': yesNoOrUndefined(
+      formData['view:hasServiceBefore1978'],
+    ),
+    'edu-desired-facility-name': formData.newSchoolName,
+    'edu-desired-type-of-education': formData.educationType,
+    'edu-desired-facility-state': formData.newSchoolAddress.state,
+    'edu-desired-facility-city': formData.newSchoolAddress.city,
+    'edu-prior-facility-name': formData.oldSchool.name,
+    'edu-prior-facility-state': formData.oldSchool.address.state,
+    'edu-prior-facility-city': formData.oldSchool.address.city,
+    'edu-prior-facility-end-date': formData.trainingEndDate,
+    'preferred-contact-method': formData.preferredContactMethod,
+    married: yesNoOrUndefined(formData.serviceBefore1977.married),
+    'dependent-children': yesNoOrUndefined(
+      formData.serviceBefore1977.haveDependents,
+    ),
+    'dependent-parent': yesNoOrUndefined(
+      formData.serviceBefore1977.parentDependent,
+    ),
+    'direct-deposit-method': formData.bankAccountChange,
+    'direct-deposit-account-type': formData.bankAccount?.accountType,
+  };
+};
