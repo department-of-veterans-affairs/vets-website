fce17018e7b02f3d23fc55fff41db31e88d951d6
Kelson Adams <kelsonic@users.noreply.github.com>
2020-07-29 14:02:23 -0600

    Use Telephone for all TTY numbers (#13607)
    * Use Telephone for all TTY numbers
    
    * Use 911 pattern for phone numbers
    
    * Update unit tests
    
    * Tweak unit test
    
    * Remove "staff member" duplicate content
    
    * Add Telephone for other phone number links on MilitaryInformation
    
    * Fix linting errors

--- src/applications/validate-mhv-account/components/errors/DeactivatedMHVId.jsx
+++ src/applications/validate-mhv-account/components/errors/DeactivatedMHVId.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import CollapsiblePanel from '@department-of-veterans-affairs/formation-react/CollapsiblePanel';
+import Telephone, {
+  CONTACTS,
+} from '@department-of-veterans-affairs/formation-react/Telephone';
 
 import MessageTemplate from '../MessageTemplate';
 
@@ -29,11 +32,7 @@ const DeactivatedMHVId = () => {
           <p>
             Call us at <a href="tel:877-327-0022">877-327-0022</a>. We’re here
             Monday through Friday, 8:00 a.m. to 8:00 p.m. ET. If you have
-            hearing loss, call TTY:{' '}
-            <a href="tel:8008773399" aria-label="8 0 0. 8 7 7. 3 3 9 9.">
-              800-877-3399
-            </a>
-            .
+            hearing loss, call TTY: <Telephone contact={CONTACTS.HELP_TTY} />.
           </p>
           <p>
             Tell the representative that you tried to sign in to use health
--- src/platform/site-wide/cta-widget/components/messages/DeactivatedMHVIds.jsx
+++ src/platform/site-wide/cta-widget/components/messages/DeactivatedMHVIds.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import CallToActionAlert from '../CallToActionAlert';
 import AdditionalInfo from '@department-of-veterans-affairs/formation-react/AdditionalInfo';
+import Telephone, {
+  CONTACTS,
+} from '@department-of-veterans-affairs/formation-react/Telephone';
 
 const DeactivatedMHVIds = () => {
   const content = {
@@ -19,11 +22,7 @@ const DeactivatedMHVIds = () => {
           <p>
             Call us at <a href="tel:877-327-0022">877-327-0022</a>. We’re here
             Monday through Friday, 8:00 a.m. to 8:00 p.m. ET. If you have
-            hearing loss, call TTY:{' '}
-            <a href="tel:8008773399" aria-label="8 0 0. 8 7 7. 3 3 9 9.">
-              800-877-3399
-            </a>
-            .
+            hearing loss, call TTY: <Telephone contact={CONTACTS.HELP_TTY} />.
           </p>
           <p>
             Tell the representative that you tried to sign in to use the health
