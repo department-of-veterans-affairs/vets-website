3e6f5d8a69392abda4669d939c6f61d0895e7afd
Stephen Barrs <stephen.barrs@va.gov>
2020-07-30 11:03:04 -0400

    add contact info and validations

--- src/applications/covid-vaccine-trial/config/form.js
+++ src/applications/covid-vaccine-trial/config/form.js
@@ -1,12 +1,36 @@
+import _ from 'lodash/fp';
+
 // import fullSchema from 'vets-json-schema/dist/covid-vaccine-trial-schema.json';
 import fullSchema from '../schema/covid-vaccine-trial-schema.json';
+import schemaDefinitions from 'vets-json-schema/dist/definitions.json';
+// import * as address from 'platform/forms/definitions/address';
+
+import PhoneNumberWidget from 'platform/forms-system/src/js/widgets/PhoneNumberWidget';
+import PhoneNumberReviewWidget from 'platform/forms-system/src/js/review/PhoneNumberWidget';
+
+import fullNameUI from 'platform/forms/definitions/fullName';
+import phoneUI from 'platform/forms-system/src/js/definitions/phone';
+import emailUI from 'platform/forms-system/src/js/definitions/email';
+import dataUtils from 'platform/utilities/data/index';
 
 import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
 
-const formFields = {
-  firstName: 'firstName',
-};
+const { fullName } = schemaDefinitions;
+const { get, omit, set } = dataUtils;
+
+export function validateEmailsMatch(errors, pageData) {
+  const { email, confirmEmail } = pageData;
+  if (email !== confirmEmail) {
+    errors.confirmEmail.addError('Please ensure your entries match');
+  }
+}
+export function validatePhone(errors, pageData) {
+  const { phone } = pageData;
+  if (phone && !/\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}$/.test(phone)) {
+    errors.phone.addError('Please enter a valid 10-digit phone number');
+  }
+}
 
 const formConfig = {
   urlPrefix: '/',
@@ -55,12 +79,32 @@ const formConfig = {
             residents: fullSchema.multiQuestions.residents.residentsUISchema,
             closeContact:
               fullSchema.multiQuestions.closeContact.closeContactUISchema,
-            [formFields.firstName]: {
-              'ui:title': 'First Name',
+            'view:contactInfoDescription': {
+              'ui:description':
+                'Your contact information (all fields required)',
             },
+            'ui:validations': [validateEmailsMatch, validatePhone],
+            fullName: _.merge(fullNameUI, {
+              first: {
+                'ui:title': 'Your first name',
+              },
+              last: {
+                'ui:title': 'Your last name',
+              },
+              middle: {
+                'ui:title': 'Your middle name',
+              },
+              suffix: {
+                'ui:title': 'Your suffix',
+              },
+              'ui:order': ['first', 'middle', 'last', 'suffix'],
+            }),
+            email: emailUI(),
+            confirmEmail: emailUI('Confirm email address'),
+            phone: phoneUI(),
           },
           schema: {
-            required: [formFields.firstName],
+            required: ['phone'],
             type: 'object',
             properties: {
               diagnosed: {
@@ -82,7 +126,20 @@ const formConfig = {
               residents: fullSchema.multiQuestions.residents.residentsSchema,
               closeContact:
                 fullSchema.multiQuestions.closeContact.closeContactSchema,
-              [formFields.firstName]: {
+              'view:contactInfoDescription': {
+                type: 'object',
+                properties: {},
+              },
+              fullName: set('required', ['first', 'last'], fullName),
+              email: {
+                type: 'string',
+                format: 'email',
+              },
+              confirmEmail: {
+                type: 'string',
+                format: 'email',
+              },
+              phone: {
                 type: 'string',
               },
             },
