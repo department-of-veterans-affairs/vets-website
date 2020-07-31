9a0f64fb766ea6d7198c134afc657281f57d2226
Dan Hinze <dan.hinze@adhocteam.us>
2020-07-31 15:39:48 -0500

    Change language of verify page, redirect directly

--- src/applications/login/components/VerifyPage.jsx
+++ src/applications/login/components/VerifyPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
 
+import { verify } from 'platform/user/authentication/utilities';
 import recordEvent from 'platform/monitoring/record-event';
 
 export default function VerifyPage() {
@@ -12,55 +13,20 @@ export default function VerifyPage() {
             <h4 className="usa-alert-heading">
               Please verify your identity before continuing to My VA Health
             </h4>
-            <p>This process should take about 5 to 10 minutes.</p>
             <p>
-              <strong>If you’re applying for the first time</strong>
+              We take your privacy seriously, and we’re committed to protecting
+              your information. You’ll need to verify your identity before we
+              can give you access to your personal health information.
             </p>
-            <p>
-              We need to verify your identity so we can help you track the
-              status of your application once you’ve submitted it. As soon as
-              you’re finished verifying your identity, you can continue to the
-              application.
-            </p>
-            <p>
-              <strong>If you’ve applied before</strong>
-            </p>
-            <p>
-              We need to verify your identity so we can show you the status of
-              your past application. We take your privacy seriously, and we need
-              to make sure we’re sharing your personal information only with
-              you.
-            </p>
-            <p>
-              <strong>
-                If you need more information or help with verifying your
-                identity:
-              </strong>
-            </p>
-            <ul>
-              <li>
-                <a href="/sign-in-faq/#verifying-your-identity">
-                  Read our identity verification FAQs
-                </a>
-              </li>
-              <li>
-                Or call us at <a href="tel:+18772228387">877-222-8387</a>. If
-                you have hearing loss, call TTY:{' '}
-                <a href="tel:8008778339" aria-label="8 0 0. 8 7 7. 8 3 3 9.">
-                  800-877-8339
-                </a>
-                . We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
-              </li>
-            </ul>
-            <a
-              className="usa-button-primary va-button-primary"
-              href={`/verify${window.location.search}`}
+            <button
               onClick={() => {
                 recordEvent({ event: 'verify-link-clicked' });
+                verify('v1');
               }}
+              className="usa-button-primary va-button-primary"
             >
               Verify your identity
-            </a>
+            </button>
           </div>
         }
         isVisible
