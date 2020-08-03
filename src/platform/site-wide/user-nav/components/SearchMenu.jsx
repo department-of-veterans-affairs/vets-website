6038537f08dceb121f94f264613545a0fc34b17d
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-08-03 16:22:26 -0400

    Switch back to original searchmenu

--- src/platform/site-wide/user-nav/components/SearchMenu.jsx
+++ src/platform/site-wide/user-nav/components/SearchMenu.jsx
@@ -2,43 +2,90 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 import recordEvent from 'platform/monitoring/record-event';
-import environment from 'platform/utilities/environment';
 
-import SearchForm from './SearchForm';
-import SearchTypeahead from './SearchTypeahead';
-
+import { replaceWithStagingDomain } from '../../../utilities/environment/stagingDomains';
 import IconSearch from '@department-of-veterans-affairs/formation-react/IconSearch';
 import DropDownPanel from '@department-of-veterans-affairs/formation-react/DropDownPanel';
 
-const isTypeaheadEnabled = false; // !environment.isProduction();
+import Typeahead, { isTypeaheadEnabled, typeaheadListId } from './Typeahead';
 
-export default function SearchMenu({ cssClass, clickHandler, isOpen }) {
-  const buttonClasses = classNames(
-    cssClass,
-    'va-btn-withicon',
-    'va-dropdown-trigger',
-  );
+class SearchMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchAction: replaceWithStagingDomain('https://www.va.gov/search/'),
+      userInput: '',
+    };
+  }
 
-  const icon = <IconSearch color="#fff" role="presentation" />;
-  const searchInput = isTypeaheadEnabled ? (
-    <SearchTypeahead isVisible={isOpen} />
-  ) : (
-    <SearchForm />
-  );
+  componentDidUpdate() {
+    this.refs.searchField.focus();
+  }
 
-  return (
-    <DropDownPanel
-      onClick={() => recordEvent({ event: 'nav-jumplink-click' })}
-      buttonText="Search"
-      clickHandler={clickHandler}
-      cssClass={buttonClasses}
-      id="search-menu"
-      icon={icon}
-      isOpen={isOpen}
-    >
-      {searchInput}
-    </DropDownPanel>
-  );
+  handleInputChange = e => {
+    this.setState({ userInput: e.target.value });
+  };
+
+  makeForm = () => {
+    const validUserInput =
+      this.state.userInput &&
+      this.state.userInput.replace(/\s/g, '').length > 0;
+
+    return (
+      <form
+        acceptCharset="UTF-8"
+        action={this.state.searchAction}
+        id="search"
+        method="get"
+      >
+        <label htmlFor="query" className="usa-sr-only">
+          Search:
+        </label>
+
+        <div className="va-flex">
+          <input
+            autoComplete="off"
+            ref="searchField"
+            className="usagov-search-autocomplete"
+            id="query"
+            name="query"
+            type="text"
+            list={typeaheadListId}
+            onChange={this.handleInputChange}
+          />
+          {isTypeaheadEnabled && <Typeahead userInput={this.state.userInput} />}
+          <button type="submit" disabled={!validUserInput}>
+            <IconSearch color="#fff" />
+            <span className="usa-sr-only">Search</span>
+          </button>
+        </div>
+      </form>
+    );
+  };
+
+  render() {
+    const buttonClasses = classNames(
+      this.props.cssClass,
+      'va-btn-withicon',
+      'va-dropdown-trigger',
+    );
+
+    const icon = <IconSearch color="#fff" role="presentation" />;
+
+    return (
+      <DropDownPanel
+        onClick={() => recordEvent({ event: 'nav-jumplink-click' })}
+        buttonText="Search"
+        clickHandler={this.props.clickHandler}
+        cssClass={buttonClasses}
+        id="search-menu"
+        icon={icon}
+        isOpen={this.props.isOpen}
+      >
+        {this.makeForm()}
+      </DropDownPanel>
+    );
+  }
 }
 
 SearchMenu.propTypes = {
@@ -46,3 +93,5 @@ SearchMenu.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   clickHandler: PropTypes.func,
 };
+
+export default SearchMenu;
---
+++ src/platform/site-wide/user-nav/components/Typeahead.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import environment from 'platform/utilities/environment';
+
+export const isTypeaheadEnabled = !environment.isProduction();
+export const typeaheadListId = 'onsite-search-typeahead';
+
+export default class Typeahead extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      suggestions: [],
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    const inputChanged = prevProps.userInput !== this.props.userInput;
+    if (inputChanged) {
+      this.getSuggestions();
+    }
+  }
+
+  async getSuggestions() {
+    const input = this.props.userInput;
+    if (input?.length < 2) return;
+
+    const encodedInput = encodeURIComponent(input);
+    const response = await fetch(
+      `https://search.usa.gov/sayt?=&name=va&q=${encodedInput}`,
+    );
+
+    const suggestions = await response.json();
+
+    this.setState({ suggestions });
+  }
+
+  render() {
+    return (
+      <datalist id={typeaheadListId}>
+        {this.state.suggestions.map(suggestion => (
+          <option key={suggestion} value={suggestion} />
+        ))}
+      </datalist>
+    );
+  }
+}
