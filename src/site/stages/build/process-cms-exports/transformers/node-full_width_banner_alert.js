d4eb068b8c67bdc605f4e8cf71d98c93c0f91011
Jhonny Gonzalez <55560129+jhonnyoddball@users.noreply.github.com>
2020-08-04 13:46:39 -0400

    New transformer for node-full_width_banner_alert (#13553)
    * New transformer for node-full_width_banner_alert
    
    * fix entity references
    
    * minor refactor
    
    * minor refactoring of field_administration
    
    * Solve the case of the mysterious entity wrap
    
    * Added missing field
    
    * fixed null values
    
    * fieldBannerAlertVamcs supports array
    
    * fieldBannerAlertVamcs takes only 1 reference
    
    * fixed fieldBannerAlertComputdvalues as string
    
    * Improved fieldBannerAlertVamcs schema
    
    Co-authored-by: Christopher Valarida <chris.valarida@adhocteam.us>

---
+++ src/site/stages/build/process-cms-exports/transformers/node-full_width_banner_alert.js
@@ -0,0 +1,67 @@
+const {
+  getDrupalValue,
+  createMetaTagArray,
+  utcToEpochTime,
+} = require('./helpers');
+
+const transform = entity => ({
+  entityType: 'node',
+  entityBundle: 'full_width_banner_alert',
+  title: getDrupalValue(entity.title),
+  created: utcToEpochTime(getDrupalValue(entity.created)),
+  changed: utcToEpochTime(getDrupalValue(entity.changed)),
+  entityMetatags: createMetaTagArray(entity.metatag.value),
+  path: getDrupalValue(entity.path),
+  fieldAdministration: entity.fieldAdministration[0],
+  fieldAlertDismissable: getDrupalValue(entity.fieldAlertDismissable),
+  fieldAlertEmailUpdatesButton: getDrupalValue(
+    entity.fieldAlertEmailUpdatesButton,
+  ),
+  fieldAlertFindFacilitiesCta: getDrupalValue(
+    entity.fieldAlertFindFacilitiesCta,
+  ),
+  fieldAlertInheritanceSubpages: getDrupalValue(
+    entity.fieldAlertInheritanceSubpages,
+  ),
+  fieldAlertOperatingStatusCta: getDrupalValue(
+    entity.fieldAlertOperatingStatusCta,
+  ),
+  fieldAlertType: getDrupalValue(entity.fieldAlertType),
+  fieldBannerAlertComputdvalues: getDrupalValue(
+    entity.fieldBannerAlertComputdvalues,
+  ),
+  fieldBannerAlertVamcs: entity.fieldBannerAlertVamcs.map(n => ({
+    entity: {
+      fieldOffice: n.fieldOffice,
+      entityUrl: n.entityUrl,
+    },
+  })),
+  fieldBody: getDrupalValue(entity.fieldBody),
+  fieldOperatingStatusSendemail: getDrupalValue(
+    entity.fieldOperatingStatusSendemail,
+  ),
+  fieldSituationUpdates: entity.fieldSituationUpdates || null,
+});
+
+module.exports = {
+  filter: [
+    'title',
+    'created',
+    'changed',
+    'metatag',
+    'path',
+    'field_administration',
+    'field_alert_dismissable',
+    'field_alert_email_updates_button',
+    'field_alert_find_facilities_cta',
+    'field_alert_inheritance_subpages',
+    'field_alert_operating_status_cta',
+    'field_alert_type',
+    'field_banner_alert_computdvalues',
+    'field_banner_alert_vamcs',
+    'field_body',
+    'field_operating_status_sendemail',
+    'field_situation_updates',
+  ],
+  transform,
+};
