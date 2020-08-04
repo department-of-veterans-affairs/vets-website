502916ada94be72a6d84b269c989d9eb406322a7
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2020-08-04 13:13:40 -0400

    updated styling

--- src/applications/gi/components/vet-tec/VetTecProgramSearchResult.jsx
+++ src/applications/gi/components/vet-tec/VetTecProgramSearchResult.jsx
@@ -120,7 +120,7 @@ function VetTecProgramSearchResult(props) {
             </div>
           </div>
           <div className="row vads-u-padding-top--1p5">
-            <div className="view-details columns vads-u-display--inline-block">
+            <div className="tag-margin view-details columns vads-u-display--inline-block">
               {isPresent(lengthInHours) && (
                 <div className="info-flag">{displayHours}</div>
               )}
