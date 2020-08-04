b997e2302211848ef5411ee4fbadec539803aca4
Theo Bentum <theo@adhocteam.us>
2020-08-04 15:50:30 -0700

    Update the node-event_listing transformer

--- src/site/stages/build/process-cms-exports/schemas/input/node-event_listing.js
+++ src/site/stages/build/process-cms-exports/schemas/input/node-event_listing.js
@@ -4,35 +4,37 @@ module.exports = {
   type: 'object',
   properties: {
     title: { $ref: 'GenericNestedString' },
-    changed: {
-      type: 'array',
-      items: {
-        type: 'object',
-        properties: {
-          value: { type: 'string' },
-          format: { type: 'string' },
-        },
-        required: ['value', 'format'],
-      },
-      maxItems: 1,
-    },
+    created: { $ref: 'GenericNestedString' },
+    changed: { $ref: 'GenericNestedString' },
+    moderation_state: { $ref: 'GenericNestedString' },
     metatag: { $ref: 'RawMetaTags' },
     path: { $ref: 'RawPath' },
+    field_administration: {
+      type: 'array',
+      maxItems: 1,
+      items: { $ref: 'EntityReference' },
+    },
+    field_description: { $ref: 'GenericNestedString' },
     field_intro_text: { $ref: 'GenericNestedString' },
+    field_meta_title: { $ref: 'GenericNestedString' },
     field_office: {
       type: 'array',
-      items: {
-        $ref: 'EntityReference',
-      },
       maxItems: 1,
+      items: { $ref: 'EntityReference' },
     },
+    reverse_field_list: { $ref: 'EntityReferenceArray' },
   },
   required: [
     'title',
+    'created',
     'changed',
+    'moderation_state',
     'metatag',
     'path',
+    'field_administration',
+    'field_description',
     'field_intro_text',
+    'field_meta_title',
     'field_office',
   ],
 };
--- src/site/stages/build/process-cms-exports/transformers/node-event_listing.js
+++ src/site/stages/build/process-cms-exports/transformers/node-event_listing.js
@@ -2,18 +2,48 @@ const {
   getDrupalValue,
   utcToEpochTime,
   createMetaTagArray,
+  isPublished,
 } = require('./helpers');
 
 const transform = entity => ({
   entityType: 'node',
   entityBundle: 'event_listing',
-  entityMetatags: createMetaTagArray(entity.metatag.value),
   title: getDrupalValue(entity.title),
+  created: utcToEpochTime(getDrupalValue(entity.created)),
   changed: utcToEpochTime(getDrupalValue(entity.changed)),
+  entityMetatags: createMetaTagArray(entity.metatag.value),
+  entityPublished: isPublished(getDrupalValue(entity.moderationState)),
+  fieldAdministration: entity.fieldAdministration[0],
+  fieldDescription: getDrupalValue(entity.fieldDescription),
   fieldIntroText: getDrupalValue(entity.fieldIntroText),
+  fieldMetaTitle: getDrupalValue(entity.fieldMetaTitle),
   fieldOffice: entity.fieldOffice[0],
+  reverseFieldListingNode: { entities: entity.reverseFieldList },
+  pastEvents: {
+    entities: entity.reverseFieldList.map(reverseField => ({
+      title: reverseField.title,
+      entityUrl: reverseField.entityUrl,
+      uid: reverseField.uid,
+      fieldFeatured: reverseField.fieldFeatured,
+      fieldDate: reverseField.fieldDate,
+      fieldDescription: reverseField.fieldDescription,
+      fieldLocationHumanreadable: reverseField.fieldLocationHumanreadable,
+    })),
+  },
 });
 module.exports = {
-  filter: ['title', 'changed', 'metatag', 'field_intro_text', 'field_office'],
+  filter: [
+    'title',
+    'created',
+    'changed',
+    'metatag',
+    'moderation_state',
+    'field_administration',
+    'field_description',
+    'field_intro_text',
+    'field_meta_title',
+    'field_office',
+    'reverse_field_list',
+  ],
   transform,
 };
