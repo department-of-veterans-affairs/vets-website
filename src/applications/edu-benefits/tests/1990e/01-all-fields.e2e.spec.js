2afa330e4ef2d02a9ffb535f94ba535c6986e0fe
mr0sari0 <mrosario@governmentcio.com>
2020-08-05 19:17:24 -0400

    uncomment e2e test

--- src/applications/edu-benefits/tests/1990e/01-all-fields.e2e.spec.js
+++ src/applications/edu-benefits/tests/1990e/01-all-fields.e2e.spec.js
@@ -16,10 +16,9 @@ module.exports = E2eHelpers.createE2eTest(client => {
       }/education/apply-for-education-benefits/application/1990E`,
     )
     .waitForElementVisible('body', Timeouts.normal)
-    // uncomment when in production
-    // .waitForElementVisible('.wizard-container', Timeouts.normal)
-    // .waitForElementVisible('.skip-wizard-link', Timeouts.normal)
-    // .click('.skip-wizard-link')
+    .waitForElementVisible('.wizard-container', Timeouts.normal)
+    .waitForElementVisible('.skip-wizard-link', Timeouts.normal)
+    .click('.skip-wizard-link')
     .waitForElementVisible('.schemaform-start-button', Timeouts.slow)
     .axeCheck('.main')
     .click('.schemaform-start-button');
