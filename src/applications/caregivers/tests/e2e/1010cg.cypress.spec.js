aa3ed80ddc548d9cf19f9736064c53e015cbff1b
LinnJS <justinslinn727@gmail.com>
2020-08-10 12:21:02 -0400

    Skip test

--- src/applications/caregivers/tests/e2e/1010cg.cypress.spec.js
+++ src/applications/caregivers/tests/e2e/1010cg.cypress.spec.js
@@ -34,9 +34,6 @@ const testSecondaryTwo = createTestConfig(
         cy.findAllByText(/start/i, { selector: 'button' })
           .first()
           .click();
-
-        // Click past the ITF message
-        cy.findByText(/continue/i, { selector: 'button' }).click();
       },
       'review-and-submit': () => {
         cy.get('@testKey').then(testKey => {
