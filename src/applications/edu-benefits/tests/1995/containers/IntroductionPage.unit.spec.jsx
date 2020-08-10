ecf709ed43242a62c91b1f15bc00626324a57aa4
mr0sari0 <mrosario@governmentcio.com>
2020-08-10 16:57:44 -0400

    use store in tests

--- src/applications/edu-benefits/tests/1995/containers/IntroductionPage.unit.spec.jsx
+++ src/applications/edu-benefits/tests/1995/containers/IntroductionPage.unit.spec.jsx
@@ -10,14 +10,8 @@ import {
 import { sessionStorageSetup } from 'platform/testing/utilities';
 
 describe('the Edu-Benefit 1995 Introduction Page', () => {
-  let defaultProps;
-
-  before(() => {
-    sessionStorageSetup();
-  });
-
-  beforeEach(() => {
-    defaultProps = {
+  const store = {
+    getState: () => ({
       shouldEduBenefits1995WizardShow: true,
       route: {
         formConfig: {},
@@ -30,31 +24,48 @@ describe('the Edu-Benefit 1995 Introduction Page', () => {
           },
         },
       },
-    };
+    }),
+    subscribe: () => {},
+    dispatch: () => {},
+  };
+  let defaultProps;
+
+  before(() => {
+    sessionStorageSetup();
+  });
+
+  beforeEach(() => {
+    defaultProps = store.getState();
   });
 
   afterEach(() => {
     sessionStorage.clear();
   });
 
-  it('should show the wizard on initial render if shouldEduBenefits1990EWizardShow is set to true', () => {
-    const wrapper = shallow(<IntroductionPage {...defaultProps} />);
+  it('should show the wizard on initial render if shouldEduBenefits1995WizardShow is set to true', () => {
+    const wrapper = shallow(
+      <IntroductionPage store={store} {...defaultProps} />,
+    );
     expect(wrapper.exists('WizardContainer')).to.equal(true);
     expect(wrapper.exists('.subway-map')).to.equal(false);
     wrapper.unmount();
   });
-  it('should show the subway map on initial render if shouldEduBenefits1990EWizardShow is set to false', () => {
-    const props = {
+  it('should show the subway map on initial render if shouldEduBenefits1995WizardShow is set to false', () => {
+    const updatedProps = {
       ...defaultProps,
       shouldEduBenefits1995WizardShow: false,
     };
-    const wrapper = shallow(<IntroductionPage {...props} />);
+    const wrapper = shallow(
+      <IntroductionPage store={store} {...updatedProps} />,
+    );
     expect(wrapper.exists('WizardContainer')).to.equal(false);
     expect(wrapper.exists('.subway-map')).to.equal(true);
     wrapper.unmount();
   });
   it('should show the subway map if the wizard was completed', () => {
-    const wrapper = shallow(<IntroductionPage {...defaultProps} />);
+    const wrapper = shallow(
+      <IntroductionPage store={store} {...defaultProps} />,
+    );
     const instance = wrapper.instance();
     instance.setWizardStatus(WIZARD_STATUS_COMPLETE);
     const wizardStatus = getWizardStatus().then(() => {
