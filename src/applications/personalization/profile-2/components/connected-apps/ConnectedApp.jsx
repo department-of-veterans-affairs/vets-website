d78a43e8e0985e8a79871cf5d0ddf2f5689a534f
Sandra Hallie <sandra.adamshallie@gmail.com>
2020-08-11 09:25:51 -0600

    Sanlouise 11683 add connected apps cypress (#13634)
    * Add connected apps tests Cypress

--- src/applications/personalization/profile-2/components/connected-apps/ConnectedApp.jsx
+++ src/applications/personalization/profile-2/components/connected-apps/ConnectedApp.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { isEmpty } from 'lodash';
 import moment from 'moment';
 
 import PropTypes from 'prop-types';
@@ -51,6 +52,7 @@ export class ConnectedApp extends Component {
               aria-label={`Disconnect ${title} from your account`}
               className="usa-button-secondary vads-u-margin-bottom--3 medium-screen:vads-u-margin-bottom--0"
               onClick={this.openModal}
+              data-testid={`disconnect-app-${this.props.id}`}
             >
               Disconnect
             </button>
@@ -59,7 +61,7 @@ export class ConnectedApp extends Component {
           <ConnectedAppDeleteModal
             deleting={this.props.deleting}
             title={title}
-            modalOpen={this.state.modalOpen}
+            modalOpen={this.state.modalOpen && isEmpty(this.props.errors)}
             closeModal={this.closeModal}
             confirmDelete={this.confirmDelete}
           />
@@ -83,6 +85,7 @@ ConnectedApp.propTypes = {
   id: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   deleting: PropTypes.bool,
+  errors: PropTypes.array,
   attributes: PropTypes.shape({
     title: PropTypes.string.isRequired,
     logo: PropTypes.string.isRequired,
