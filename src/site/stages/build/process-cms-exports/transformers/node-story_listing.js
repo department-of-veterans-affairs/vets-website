442856cebcf03cb400aad9c8fb0c75002c6958ca
Theo Bentum <theo@adhocteam.us>
2020-08-11 08:21:15 -0700

    Add reverse fields to node-story_listing (#13772)
    * Add 'status' field to node-news_story transformer
    
    * Add entityPublished field to node-story_listing transformer
    
    * Add fieldFeatured to node-news_story
    
    * Add reverse fields to node-story_listing transformer
    
    * Add placeholders for uid in node-story_listing
    
    * Remove reverse_field_list from required in node-story_listing
    
    * Handle empty reverse field nodes and update reverse filtering
    
    * Use status for determining entityPublished in node-story_listing

--- src/site/stages/build/process-cms-exports/transformers/node-story_listing.js
+++ src/site/stages/build/process-cms-exports/transformers/node-story_listing.js
@@ -2,6 +2,7 @@ const {
   createMetaTagArray,
   getDrupalValue,
   utcToEpochTime,
+  isPublished,
 } = require('./helpers');
 
 const transform = entity => ({
@@ -10,12 +11,39 @@ const transform = entity => ({
   title: getDrupalValue(entity.title),
   created: utcToEpochTime(getDrupalValue(entity.created)),
   changed: utcToEpochTime(getDrupalValue(entity.changed)),
+  entityPublished: getDrupalValue(entity.status),
   entityMetatags: createMetaTagArray(entity.metatag.value),
   fieldAdministration: entity.fieldAdministration[0],
   fieldDescription: getDrupalValue(entity.fieldDescription),
   fieldIntroText: getDrupalValue(entity.fieldIntroText),
   fieldMetaTitle: getDrupalValue(entity.fieldMetaTitle),
   fieldOffice: entity.fieldOffice[0],
+  reverseFieldListingNode: {
+    entities: entity.reverseFieldList
+      ? entity.reverseFieldList
+          .filter(
+            reverseField =>
+              reverseField.entityBundle === 'news_story' &&
+              reverseField.isPublished,
+          )
+          .map(reverseField => ({
+            entityId: reverseField.entityId,
+            title: reverseField.title,
+            fieldFeatured: reverseField.fieldFeatured,
+            entityUrl: reverseField.entityUrl,
+            // Ignoring for now since some uids are missing from the export
+            // uid: reverseField.uid,
+            promote: reverseField.promote,
+            created: reverseField.created,
+            fieldAuthor: reverseField.fieldAuthor,
+            fieldImageCaption: reverseField.fieldImageCaption,
+            fieldIntroText: reverseField.fieldIntroText,
+            fieldMedia: reverseField.fieldMedia,
+            fieldFullStory: reverseField.fieldFullStory,
+          }))
+          .sort((a, b) => b.created - a.created)
+      : [],
+  },
 });
 
 module.exports = {
@@ -23,6 +51,7 @@ module.exports = {
     'title',
     'created',
     'changed',
+    'moderation_state',
     'metatag',
     'path',
     'field_administration',
@@ -30,6 +59,8 @@ module.exports = {
     'field_intro_text',
     'field_meta_title',
     'field_office',
+    'reverse_field_list',
+    'status',
   ],
   transform,
 };
