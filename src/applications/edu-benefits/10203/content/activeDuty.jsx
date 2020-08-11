746511b29dfc5a08c3ae72b5f8452d37a22c0cd8
Devin McCurdy <mccurdy_devin@bah.com>
2020-08-11 11:21:29 -0400

    Moved fix

--- src/applications/edu-benefits/10203/content/activeDuty.jsx
+++ src/applications/edu-benefits/10203/content/activeDuty.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 export const housingPaymentInfo = () => (
-  // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-  <div className="feature" tabIndex="0">
+  <div className="feature" role="alert">
     You can receive the Rogers STEM Scholarship while you’re on active duty, but
     you won’t be eligible for the scholarship's housing allowance.
   </div>
