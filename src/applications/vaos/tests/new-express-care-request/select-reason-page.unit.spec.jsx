bc29d31e22ab13c072be72672f5922c04a5b7d1e
Narin Ratana <n.ratana@gmail.com>
2020-08-11 12:40:37 -0700

    Fix alertbox test

--- src/applications/vaos/tests/new-express-care-request/select-reason-page.unit.spec.jsx
+++ src/applications/vaos/tests/new-express-care-request/select-reason-page.unit.spec.jsx
@@ -71,8 +71,9 @@ describe('VAOS integration: Express Care form', () => {
     const radio = screen.getByLabelText('Cough');
     fireEvent.click(radio);
     expect(radio.checked).to.be.true;
-    const alert = await screen.findByRole('alert');
-    expect(alert).to.contain.text('Same-day mental health appointments');
+    expect(screen.baseElement).to.contain.text(
+      'Same-day mental health appointments',
+    );
     await screen.getByRole('button', { name: /back/i });
     await screen.getByRole('button', { name: /continue/i });
   });
