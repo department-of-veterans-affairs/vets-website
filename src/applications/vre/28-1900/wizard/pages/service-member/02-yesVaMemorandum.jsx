64131102c2136e4d6f307355860d6b58bffe6a84
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-08-12 11:00:16 -0700

    12113 scaffold wizard (#13794)
    * Scaffold and set up a few initial pages
    
    * Added all pages for veteran path of questions
    
    * Added service member path files
    
    * Added other page path files
    
    * addressed PR feedback
    
    * Changed chunk name

---
+++ src/applications/vre/28-1900/wizard/pages/service-member/02-yesVaMemorandum.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ErrorableRadioButtons from '@department-of-veterans-affairs/formation-react/ErrorableRadioButtons';
+import { serviceMemberPathPageNames } from '../pageList';
+
+const options = [
+  { value: serviceMemberPathPageNames.yesInterestedInHelp, label: 'Yes' },
+  { value: serviceMemberPathPageNames.notInterestedInHelp, label: 'No' },
+];
+
+const yesVaMemorandum = ({ setPageState, state = {} }) => (
+  <ErrorableRadioButtons
+    name={`${serviceMemberPathPageNames.yesVaMemorandum}-option`}
+    label={<p>Are you interested in readiness & employment help?</p>}
+    id={`${serviceMemberPathPageNames.yesVaMemorandum}-option`}
+    options={options}
+    onValueChange={({ value }) => setPageState({ selected: value }, value)}
+    value={{ value: state.selected }}
+  />
+);
+
+export default {
+  name: serviceMemberPathPageNames.yesVaMemorandum,
+  component: yesVaMemorandum,
+};
