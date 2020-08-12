dbca7d7c050d9f058aa6a33bc2237e73ab8dd02e
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-08-12 15:54:33 -0400

    [department-of-veterans-affairs/orchid#17] fixes full name in confirmation message
    Co-authored-by: Jacob Gacek <jacob.gacek@thoughtworks.com>
    Co-authored-by: Anita DeWitt <adewitt@thoughtworks.com>

--- src/applications/ask-a-question/containers/ConfirmationPage.jsx
+++ src/applications/ask-a-question/containers/ConfirmationPage.jsx
@@ -23,7 +23,7 @@ export class ConfirmationPage extends React.Component {
   render() {
     const { submission, data } = this.props.form;
     const { response } = submission;
-    const name = data.veteranFullName;
+    const name = data.fullName;
 
     return (
       <div>
