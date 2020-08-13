8b3f1041fb4cf5d98156435fd2c340847724e932
Robin Garrison <robin.garrison@adhocteam.us>
2020-08-13 13:39:03 -0500

    Show link on hub page

--- src/applications/disability-benefits/wizard/createWizard.jsx
+++ src/applications/disability-benefits/wizard/createWizard.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { VA_FORM_IDS } from 'platform/forms/constants';
+import environment from 'platform/utilities/environment';
+
+import manifest from '../all-claims/manifest.json';
 
 const disabilityForms = new Set([VA_FORM_IDS.FORM_21_526EZ]);
 
@@ -11,44 +14,56 @@ export default function createDisabilityIncreaseApplicationStatus(
 ) {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   if (root) {
-    import(/* webpackChunkName: "disability-application-status" */
-    './wizard-entry').then(module => {
-      const { ApplicationStatus, Wizard, pages } = module.default;
-      ReactDOM.render(
-        <Provider store={store}>
-          <ApplicationStatus
-            formIds={disabilityForms}
-            formType="disability compensation"
-            showApplyButton={
-              root.getAttribute('data-hide-apply-button') === null
-            }
-            stayAfterDelete
-            applyRender={() => (
-              <div itemScope itemType="http://schema.org/Question">
-                <h2 itemProp="name">Can I file my claim online?</h2>
-                <div
-                  itemProp="acceptedAnswer"
-                  itemScope
-                  itemType="http://schema.org/Answer"
-                >
-                  <div itemProp="text">
-                    <p>
-                      It depends on your situation. Answer a few questions, and
-                      we&apos;ll guide you to the right place.
-                    </p>
-                    <Wizard
-                      pages={pages}
-                      expander
-                      buttonText="Let's get started"
-                    />
+    if (environment.isProduction()) {
+      import(/* webpackChunkName: "disability-application-status" */
+      './wizard-entry').then(module => {
+        const { ApplicationStatus, Wizard, pages } = module.default;
+        ReactDOM.render(
+          <Provider store={store}>
+            <ApplicationStatus
+              formIds={disabilityForms}
+              formType="disability compensation"
+              showApplyButton={
+                root.getAttribute('data-hide-apply-button') === null
+              }
+              stayAfterDelete
+              applyRender={() => (
+                <div itemScope itemType="http://schema.org/Question">
+                  <h2 itemProp="name">Can I file my claim online?</h2>
+                  <div
+                    itemProp="acceptedAnswer"
+                    itemScope
+                    itemType="http://schema.org/Answer"
+                  >
+                    <div itemProp="text">
+                      <p>
+                        It depends on your situation. Answer a few questions,
+                        and we&apos;ll guide you to the right place.
+                      </p>
+                      <Wizard
+                        pages={pages}
+                        expander
+                        buttonText="Let's get started"
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-            )}
-          />
-        </Provider>,
+              )}
+            />
+          </Provider>,
+          root,
+        );
+      });
+    } else {
+      ReactDOM.render(
+        <a
+          href={manifest.rootUrl}
+          className="usa-button-primary va-button-primary"
+        >
+          File a disability compensation claim
+        </a>,
         root,
       );
-    });
+    }
   }
 }
