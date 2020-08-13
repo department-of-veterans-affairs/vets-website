430fad3aed4e94648d6cbb967993eb83ed4999b2
Erik P. Hansen <eph@erikphansen.com>
2020-08-13 12:00:11 -0700

    Fix logic for Profile focus management

--- src/applications/personalization/profile-2/components/personal-information/PersonalInformation.jsx
+++ src/applications/personalization/profile-2/components/personal-information/PersonalInformation.jsx
@@ -28,7 +28,7 @@ const PersonalInformation = ({
       // root profile route. If a user got to the Profile via a link to /profile
       // or /profile/ we want to focus on the "Your Profile" sub-nav H1, not the
       // H2 on this page
-      const pathRegExp = new RegExp(`${PROFILE_PATHS.PROFILE_ROOT}/?`);
+      const pathRegExp = new RegExp(`${PROFILE_PATHS.PROFILE_ROOT}/?$`);
       if (lastLocation?.pathname.match(new RegExp(pathRegExp))) {
         return;
       }
