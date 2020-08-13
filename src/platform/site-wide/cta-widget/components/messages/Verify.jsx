71b64856dd4fc20374278f2a115b690c389f9aa1
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-08-13 10:29:54 -0400

    [CTA widget] Implement Change Address (#13804)
    * [CTA widget] Implement Change Address
    
    * Remove dup cta widget name
    
    * Add a slash to profile route
    
    * Update button text of direct deposit
    
    * Update text of sign-in
    
    * Update Verify text
    
    * add featuretoggles to prop types

--- src/platform/site-wide/cta-widget/components/messages/Verify.jsx
+++ src/platform/site-wide/cta-widget/components/messages/Verify.jsx
@@ -6,15 +6,21 @@ const Verify = ({ serviceDescription, primaryButtonHandler }) => {
   const content = {
     heading: `Please verify your identity to ${serviceDescription}`,
     alertText: (
-      <p>
-        We take your privacy seriously, and we’re committed to protecting your
-        information. You’ll need to verify your identity before we can give you
-        access to your personal health information.
-      </p>
+      <>
+        <p>
+          We need to make sure you’re you — and not someone pretending to be you
+          — before we can give you access to your personal and health-related
+          information. This helps us keep your information safe and prevent
+          fraud and identity theft.
+        </p>
+        <p>
+          <strong>This one-time process takes about 5 to 10 minutes.</strong>
+        </p>
+      </>
     ),
     primaryButtonText: 'Verify your identity',
     primaryButtonHandler,
-    status: 'continue',
+    status: 'warning',
   };
 
   return <CallToActionAlert {...content} />;
--- src/platform/site-wide/cta-widget/helpers.js
+++ src/platform/site-wide/cta-widget/helpers.js
@@ -27,6 +27,7 @@ export const widgetTypes = {
   VIEW_APPOINTMENTS: 'view-appointments',
   VIEW_DEPENDENTS: 'view-dependents',
   ADD_REMOVE_DEPENDENTS: 'add-remove-dependents',
+  CHANGE_ADDRESS: 'change-address',
 };
 
 const HEALTH_TOOLS = [
@@ -192,6 +193,12 @@ export const toolUrl = (appId, authenticatedWithSSOe = false) => {
         redirect: false,
       };
 
+    case widgetTypes.CHANGE_ADDRESS:
+      return {
+        url: '/profile/personal-information',
+        redirect: false,
+      };
+
     default:
       return {};
   }
@@ -292,6 +299,9 @@ export const serviceDescription = appId => {
 
     case widgetTypes.ADD_REMOVE_DEPENDENTS:
       return 'add or remove dependents';
+
+    case widgetTypes.CHANGE_ADDRESS:
+      return 'change your address';
 
     default:
       return 'use this service';
