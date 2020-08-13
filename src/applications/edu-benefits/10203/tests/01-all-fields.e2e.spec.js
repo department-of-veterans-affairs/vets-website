e0f7348a1655977e3124ae572a604450f93ca714
hughes-dustin <hughes_dustin@bah.com>
2020-08-13 15:42:55 -0400

    fix lint error

--- src/applications/edu-benefits/10203/tests/01-all-fields.e2e.spec.js
+++ src/applications/edu-benefits/10203/tests/01-all-fields.e2e.spec.js
@@ -11,7 +11,8 @@ module.exports = E2eHelpers.createE2eTest(client => {
   // Ensure introduction page renders.
   client
     .openUrl(
-      `${E2eHelpers.baseUrl}/education/other-va-education-benefits/stem-scholarship/apply-for-scholarship-form-22-10203`,
+      `${E2eHelpers.baseUrl}
+      /education/other-va-education-benefits/stem-scholarship/apply-for-scholarship-form-22-10203`,
     )
     .waitForElementVisible('body', Timeouts.normal)
     .waitForElementVisible('.schemaform-start-button', Timeouts.slow)
