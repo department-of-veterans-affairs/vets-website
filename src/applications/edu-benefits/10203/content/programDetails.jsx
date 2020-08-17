d87dce2cec1ad1a504d0f2ab32ffa413f7843054
hughes-dustin <hughes_dustin@bah.com>
2020-08-17 11:37:48 -0400

    fix styling on 10203 review chapter

--- src/applications/edu-benefits/10203/content/programDetails.jsx
+++ src/applications/edu-benefits/10203/content/programDetails.jsx
@@ -5,10 +5,6 @@ const schoolStudentIdTitle = (
     <p>
       <strong>Your school ID and contact details</strong>
     </p>
-    <p>
-      These details will help us review your application faster, but aren't
-      required.
-    </p>
     <div>Your school student ID number</div>
   </div>
 );
