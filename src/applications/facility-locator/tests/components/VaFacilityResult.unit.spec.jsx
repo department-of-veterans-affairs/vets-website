8994f6c668784a4eabd28a8b19a29ad478e53fb4
Gibran <brangi@users.noreply.github.com>
2020-08-17 17:28:35 -0500

    Search result items for urgent care search mashup (#13734)
    * Search results card design and behavior for urgent care mash-up
    
    Issues:
    - https://github.com/department-of-veterans-affairs/va.gov-team/issues/11460
    - https://github.com/department-of-veterans-affairs/va.gov-team/issues/12064
    
    Co-authored-by: Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
    Co-authored-by: Michael Pelz-Sherman <mpelzsherman@gmail.com>

---
+++ src/applications/facility-locator/tests/components/VaFacilityResult.unit.spec.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import VaFacilityResult from '../../components/search-results-items/VaFacilityResult';
+import testData from '../../constants/mock-facility-data-v1.json';
+import { LocationType } from '../../constants';
+
+describe('VaFacilityResult', () => {
+  it('Should render VaFacilityResult, facility type health', () => {
+    const query = {
+      facilityType: LocationType.HEALTH,
+    };
+    const wrapper = shallow(
+      <VaFacilityResult location={testData.data[0]} query={query} />,
+    );
+    expect(wrapper).to.matchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('Should render VaFacilityResult, facility type benefits', () => {
+    const query = {
+      facilityType: LocationType.BENEFITS,
+    };
+    const wrapper = shallow(
+      <VaFacilityResult location={testData.data[6]} query={query} />,
+    );
+    expect(wrapper).to.matchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('Should render VaFacility VA, facility type cemetery', () => {
+    const query = {
+      facilityType: LocationType.CEMETARY,
+    };
+    const wrapper = shallow(
+      <VaFacilityResult location={testData.data[7]} query={query} />,
+    );
+    expect(wrapper).to.matchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('Should render VaFacility VA, facility type Vet Center', () => {
+    const query = {
+      facilityType: LocationType.VET_CENTER,
+    };
+    const wrapper = shallow(
+      <VaFacilityResult location={testData.data[8]} query={query} />,
+    );
+    expect(wrapper).to.matchSnapshot();
+    wrapper.unmount();
+  });
+});
