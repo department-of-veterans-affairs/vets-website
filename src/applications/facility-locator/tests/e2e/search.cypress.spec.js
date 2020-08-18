ee0295529e61446e9e763027536369530db5eda7
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-08-18 13:18:27 -0500

    Add e2e test that covers fix error search state

--- src/applications/facility-locator/tests/e2e/search.cypress.spec.js
+++ src/applications/facility-locator/tests/e2e/search.cypress.spec.js
@@ -88,4 +88,40 @@ describe('Facility search', () => {
 
     cy.get('#facility-search-results').should('not.exist');
   });
+
+  it('should recover search from an error response state - invalid input location', () => {
+    cy.visit('/find-locations');
+
+    // Invalid location search
+    cy.route('GET', '/geocoding/**/*', 'fx:constants/mock-failed-location').as(
+      'failedLocation',
+    );
+
+    cy.get('#street-city-state-zip').type('31234asd0o203o213');
+    cy.get('#facility-type-dropdown').select('VA health');
+    cy.get('#facility-search').click();
+    cy.get('.facility-search-results').contains(
+      /We’re sorry. We couldn’t complete your request/gi,
+    );
+
+    cy.injectAxe();
+    cy.axeCheck();
+
+    // Valid location search
+    cy.route('GET', '/geocoding/**/*', 'fx:constants/mock-geocoding-data').as(
+      'validLocationSearch',
+    );
+
+    cy.get('#street-city-state-zip').type('Austin, TX');
+    cy.get('#facility-type-dropdown').select('VA health');
+    cy.get('#service-type-dropdown').select('Primary care');
+    cy.get('#facility-search').click();
+    cy.get('#facility-search-results').contains(
+      'Results for "VA health", "Primary care" near "Austin, Texas"',
+    );
+    cy.get('.facility-result a').should('exist');
+
+    cy.injectAxe();
+    cy.axeCheck();
+  });
 });
