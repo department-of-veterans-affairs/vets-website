9c6979957761687a8446828edbbd4e115f43cc2c
Joe <elder_joseph@bah.com>
2020-08-19 13:00:14 -0400

    12593: Add check in helper to determine if on the review and submit page for 10203

--- src/applications/edu-benefits/10203/helpers.jsx
+++ src/applications/edu-benefits/10203/helpers.jsx
@@ -1,7 +1,14 @@
 export const isChapter33 = form =>
   !!form['view:benefit']?.chapter33 || !!form['view:benefit']?.fryScholarship;
 
+const onReviewAndSubmitPage = () => {
+  return (
+    window.location.pathname.split('/').slice(-1)[0] === 'review-and-submit'
+  );
+};
+
 export const displayConfirmEligibility = form =>
   !isChapter33(form) ||
   (!form.isEnrolledStem && !form.isPursuingTeachingCert) ||
-  form.benefitLeft === 'moreThanSixMonths';
+  form.benefitLeft === 'moreThanSixMonths' ||
+  onReviewAndSubmitPage();
