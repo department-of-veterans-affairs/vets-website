f6746ffb63104cdc9764febadb495fee1368811d
Joe <elder_joseph@bah.com>
2020-08-19 15:53:49 -0400

    12593: Revert previous helper check and update css for review page

--- src/applications/edu-benefits/10203/helpers.jsx
+++ src/applications/edu-benefits/10203/helpers.jsx
@@ -1,14 +1,7 @@
 export const isChapter33 = form =>
   !!form['view:benefit']?.chapter33 || !!form['view:benefit']?.fryScholarship;
 
-const onReviewAndSubmitPage = () => {
-  return (
-    window.location.pathname.split('/').slice(-1)[0] === 'review-and-submit'
-  );
-};
-
 export const displayConfirmEligibility = form =>
   !isChapter33(form) ||
   (!form.isEnrolledStem && !form.isPursuingTeachingCert) ||
-  form.benefitLeft === 'moreThanSixMonths' ||
-  onReviewAndSubmitPage();
+  form.benefitLeft === 'moreThanSixMonths';
