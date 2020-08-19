9b8fd718ddae9e9edaacf6b0bc6fffaeb59870d1
Kelson Adams <kadams@governmentcio.com>
2020-08-19 14:53:26 -0600

    Update getCernerURL helper

--- src/platform/utilities/cerner/index.js
+++ src/platform/utilities/cerner/index.js
@@ -6,8 +6,12 @@ export const isCernerLive = !environment.isProduction();
 
 export const getCernerURL = path => {
   const root = environment.isProduction()
-    ? 'https://patientportal.myhealth.va.gov'
-    : 'https://ehrm-va-test.patientportal.us.healtheintent.com';
+    ? encodeURIComponent(
+        'https://patientportal.myhealth.va.gov/clear-session?to=https://patientportal.myhealth.va.gov',
+      )
+    : encodeURIComponent(
+        'https://ehrm-va-test.patientportal.us.healtheintent.com/clear-session?to=https://ehrm-va-test.patientportal.us.healtheintent.com',
+      );
 
   return `${root}${path}`;
 };
