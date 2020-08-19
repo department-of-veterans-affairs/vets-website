be484d71ae538fb696286eafdafa38e7b4f4e6b7
Kelson Adams <kadams@governmentcio.com>
2020-08-19 09:20:30 -0600

    Add feature flag support for all Cerner pages

--- src/applications/static-pages/health-care-manage-benefits/get-medical-records-page/components/App/index.js
+++ src/applications/static-pages/health-care-manage-benefits/get-medical-records-page/components/App/index.js
@@ -9,8 +9,9 @@ import UnauthContent from '../UnauthContent';
 import { isCernerLive } from 'platform/utilities/cerner';
 import { selectIsCernerPatient } from 'platform/user/selectors';
 
-export const App = ({ isCernerPatient }) => {
-  if (!isCernerLive) {
+export const App = ({ isCernerPatient, showNewGetMedicalRecordsPage }) => {
+  // Show legacy content if Cerner isn't live or if we explicitly shouldn't show the page via a feature flag.
+  if (!isCernerLive || showNewGetMedicalRecordsPage === false) {
     return <LegacyContent />;
   }
 
@@ -24,10 +25,13 @@ export const App = ({ isCernerPatient }) => {
 App.propTypes = {
   // From mapStateToProps.
   isCernerPatient: PropTypes.bool,
+  showNewGetMedicalRecordsPage: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
   isCernerPatient: selectIsCernerPatient(state),
+  showNewGetMedicalRecordsPage:
+    state?.featureToggles?.showNewGetMedicalRecordsPage,
 });
 
 export default connect(
