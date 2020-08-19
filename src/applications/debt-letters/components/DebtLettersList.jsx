52db086d7af88e7b8cea1685205c2f08208398f4
Brad Conley <brad.conl3y@gmail.com>
2020-08-19 14:25:30 -0400

    Update anchor text

--- src/applications/debt-letters/components/DebtLettersList.jsx
+++ src/applications/debt-letters/components/DebtLettersList.jsx
@@ -99,15 +99,10 @@ const DebtLettersList = ({ debtLinks, isVBMSError }) => {
                           role="img"
                           className="fas fa-download vads-u-padding-right--1"
                         />
-                        Download
+                        <span aria-hidden="true">Download letter </span>
                         <span className="sr-only">
-                          {debtLetter.typeDescription}
-                        </span>{' '}
-                        letter{' '}
-                        <span className="sr-only">
-                          dated{' '}
-                          {moment(debtLetter.receivedAt).format('MMM D, YYYY')}`
-                        </span>{' '}
+                          Download Second Demand Letter dated May 29, 2020
+                        </span>
                         <dfn>
                           <abbr title="Portable Document Format">(PDF)</abbr>
                         </dfn>
