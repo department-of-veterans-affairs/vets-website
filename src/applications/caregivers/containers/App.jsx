959418fec46d272f8a91bb9892a101e382521313
LinnJS <justinslinn727@gmail.com>
2020-08-19 14:06:53 -0400

    No index 1010CG

--- src/applications/caregivers/containers/App.jsx
+++ src/applications/caregivers/containers/App.jsx
@@ -22,6 +22,7 @@ const App = ({ loading, isFormAvailable, location, children }) => {
 
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+      {/* TODO: remove after prod testing */}
       <MetaTags>
         <meta name="robots" content="noindex" />
       </MetaTags>
