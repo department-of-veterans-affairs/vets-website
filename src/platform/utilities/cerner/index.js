77117263a50a9c53714cad2a7654338ad42fd128
Kelson Adams <kadams@governmentcio.com>
2020-08-19 14:54:17 -0600

    Refactor

--- src/platform/utilities/cerner/index.js
+++ src/platform/utilities/cerner/index.js
@@ -6,14 +6,10 @@ export const isCernerLive = !environment.isProduction();
 
 export const getCernerURL = path => {
   const root = environment.isProduction()
-    ? encodeURIComponent(
-        'https://patientportal.myhealth.va.gov/clear-session?to=https://patientportal.myhealth.va.gov',
-      )
-    : encodeURIComponent(
-        'https://ehrm-va-test.patientportal.us.healtheintent.com/clear-session?to=https://ehrm-va-test.patientportal.us.healtheintent.com',
-      );
+    ? 'https://patientportal.myhealth.va.gov/clear-session?to=https://patientportal.myhealth.va.gov'
+    : 'https://ehrm-va-test.patientportal.us.healtheintent.com/clear-session?to=https://ehrm-va-test.patientportal.us.healtheintent.com';
 
-  return `${root}${path}`;
+  return encodeURIComponent(`${root}${path}`);
 };
 
 export const appointmentsToolLink =
