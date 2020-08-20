bf8a6b42248074eeef05033fb7e34800400681a3
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2020-08-20 12:07:13 -0600

    Minor copy fixes

--- src/applications/personalization/profile-2/components/connected-apps/AdditionalInfoSections.jsx
+++ src/applications/personalization/profile-2/components/connected-apps/AdditionalInfoSections.jsx
@@ -69,8 +69,8 @@ export const AdditionalInfoSections = ({ activeApps }) => {
               it will ask you to sign in.
             </li>
             <li className="vads-u-padding-left--1">
-              Sign in with your preferred VA.gov account: DS Logon, My
-              HealtheVet, or ID.me.
+              Sign in with your preferred VA.gov account: <strong>DS Logon</strong>, <strong>My
+              HealtheVet</strong>, or <strong>ID.me</strong>.
             </li>
             <li className="vads-u-padding-left--1">
               Review the information the app is asking to access.
@@ -108,7 +108,7 @@ export const AdditionalInfoSections = ({ activeApps }) => {
               </li>
               <li className="vads-u-padding-left--1">
                 <strong>If your information isn’t accurate:</strong> Call VA311
-                at <Telephone contact={CONTACTS.VA_311} />. If you have hearing{' '}
+                at <Telephone contact={CONTACTS.VA_311} />. If you have hearing loss, call{' '}
                 <Telephone
                   contact={CONTACTS['711']}
                   pattern={PATTERNS['911']}
@@ -123,7 +123,7 @@ export const AdditionalInfoSections = ({ activeApps }) => {
               </li>
               <li className="vads-u-padding-left--1">
                 <strong>If you’re getting an “unreadable data” message:</strong>{' '}
-                This means the This means the connected app has access to your
+                This means the connected app has access to your
                 information, but isn’t using it in its interface. It’s nothing
                 to worry about. If you have questions about this, send feedback
                 directly to the app.
