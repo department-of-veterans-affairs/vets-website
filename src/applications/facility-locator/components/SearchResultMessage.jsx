77359f80e9a67d91efceb450f3ecc46592c8accc
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2020-08-20 12:12:44 -0400

    use es6 syntax, removed inner function

--- src/applications/facility-locator/components/SearchResultMessage.jsx
+++ src/applications/facility-locator/components/SearchResultMessage.jsx
@@ -1,57 +1,53 @@
 import React from 'react';
 
-function SearchResultMessage({
+const SearchResultMessage = ({
   message,
   error,
   resultsFound,
   resultRef,
   facilityType,
-}) {
-  const renderMessage = (err, msg, found, ref, facility) => {
-    if (facility && err) {
-      return (
-        <div className="search-result-title" ref={ref}>
-          <p>{msg}</p>
-          <p>
-            If you need care right away for a minor illness or injury, select
-            Urgent care under facility type, then select either VA or community
-            providers as the service type.
-          </p>
-          <p>
-            If you have a medical emergency, please go to your nearest emergency
-            room or call 911.
-          </p>
-        </div>
-      );
-    } else if (facility && !found) {
-      return (
-        <div className="search-result-title" ref={ref}>
-          We didn't find any facilities near you. <br />
-          <strong>To try again, please enter a different:</strong>
-          <ul className="vads-u-margin-y--1p5">
-            <li>
-              <strong>Search term</strong> (street, city, state, or postal
-              code), <strong>or</strong>
-            </li>
-            <li>
-              <strong>Service type</strong> (like “primary care”), and select
-              the option that best meets your needs
-            </li>
-          </ul>
-          Then click <strong>Search</strong>.
-        </div>
-      );
-    }
-
+}) => {
+  if (facilityType && error) {
     return (
-      <div className="search-result-title">
-        Please enter a location (street, city, state or postal code) and click
-        search above to find facilities.
+      <div className="search-result-title" ref={resultRef}>
+        <p>{message}</p>
+        <p>
+          If you need care right away for a minor illness or injury, select
+          Urgent care under facility type, then select either VA or community
+          providers as the service type.
+        </p>
+        <p>
+          If you have a medical emergency, please go to your nearest emergency
+          room or call 911.
+        </p>
       </div>
     );
-  };
+  } else if (facilityType && !resultsFound) {
+    return (
+      <div className="search-result-title" ref={resultRef}>
+        We didn't find any facilities near you. <br />
+        <strong>To try again, please enter a different:</strong>
+        <ul className="vads-u-margin-y--1p5">
+          <li>
+            <strong>Search term</strong> (street, city, state, or postal code),{' '}
+            <strong>or</strong>
+          </li>
+          <li>
+            <strong>Service type</strong> (like “primary care”), and select the
+            option that best meets your needs
+          </li>
+        </ul>
+        Then click <strong>Search</strong>.
+      </div>
+    );
+  }
 
-  return renderMessage(error, message, resultsFound, resultRef, facilityType);
-}
+  return (
+    <div className="search-result-title">
+      Please enter a location (street, city, state or postal code) and click
+      search above to find facilities.
+    </div>
+  );
+};
 
 export default SearchResultMessage;
