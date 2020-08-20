bafa3ca98a416fe750817678cf07c7d805f38f33
Stephen Barrs <stephen.barrs@va.gov>
2020-08-20 16:04:34 -0400

    remove unneeded return to make linter happy

--- src/applications/coronavirus-research/routes.jsx
+++ src/applications/coronavirus-research/routes.jsx
@@ -2,29 +2,27 @@ import { createRoutesWithSaveInProgress } from 'platform/forms/save-in-progress/
 import formConfig from './config/form';
 import App from './containers/App.jsx';
 
-// let hasRedirected = false;
-
 const route = {
   path: '/',
   component: App,
   indexRoute: {
-    onEnter: (nextState, replace) => replace('/sign-up'),
+    // onEnter: (nextState, replace) => replace('/sign-up'),
+    onEnter: (() => {
+      let hasRedirected = false;
+      return (nextState, replace) => {
+        if (!hasRedirected) {
+          hasRedirected = true;
+          replace('/sign-up');
+        }
+      };
+    })(),
   },
   childRoutes: createRoutesWithSaveInProgress(formConfig),
 };
 const introRouteIndex = route.childRoutes.findIndex(
   cr => cr.path === 'introduction',
 );
-
 route.childRoutes[introRouteIndex].onEnter = (nextState, replace) => {
   replace('/sign-up');
 };
-// route.childRoutes[introRouteIndex].onEnter = (nextState, replace) => {
-//   console.log('has redirected: ', hasRedirected);
-//   if (!hasRedirected) {
-//     hasRedirected = true;
-//     console.log('On Enter called ...');
-//     replace('/sign-up');
-//   }
-// };
 export default route;
