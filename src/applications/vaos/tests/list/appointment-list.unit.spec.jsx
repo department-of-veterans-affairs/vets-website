1ccbe83d636628ce58bd2f458a81cac7c6388579
Jeff Balboni <jeff@adhocteam.us>
2020-08-20 16:29:51 -0400

    Updates from analytics team review of VAOS EC events (#13914)
    * Updates from analytics team review
    
    * Add tab event, convert to function component
    
    * Missing tests
    
    * Add events checks
    
    * Add tab click event
    
    * Fix tests

--- src/applications/vaos/tests/list/appointment-list.unit.spec.jsx
+++ src/applications/vaos/tests/list/appointment-list.unit.spec.jsx
@@ -272,6 +272,11 @@ describe('VAOS integration: appointment list', () => {
       getByText(/View your upcoming, past, and Express Care appointments/i),
     ).to.have.tagName('h2');
 
+    expect(
+      global.window.dataLayer.find(
+        ev => ev.event === 'vaos-express-care-request-button-clicked',
+      ),
+    ).not.to.exist;
     fireEvent.click(button);
 
     await waitFor(() =>
@@ -279,9 +284,11 @@ describe('VAOS integration: appointment list', () => {
         '/new-express-care-request',
       ),
     );
-    expect(global.window.dataLayer[1]).to.deep.equal({
-      event: `vaos-express-care-request-button-clicked`,
-    });
+    expect(
+      global.window.dataLayer.find(
+        ev => ev.event === 'vaos-express-care-request-button-clicked',
+      ),
+    ).to.exist;
   });
 
   it('should not show express care action when outside of express care window', async () => {
