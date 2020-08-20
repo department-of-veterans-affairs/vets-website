872ac817373c2e46dd7fe836dd2a657bb9852dae
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-08-20 14:43:22 -0400

    Changed CI flag to be consistent between Jenkins and Circle.

--- script/run-cypress-tests-docker.sh
+++ script/run-cypress-tests-docker.sh
@@ -3,6 +3,6 @@ if [ "$(find src -name '*.cypress.spec.js' | wc -l)" -eq 0 ]; then
   exit 0
 else
   export CYPRESS_BASE_URL=http://vets-website:3001
-  export CYPRESS_BUILDTYPE=$BUILDTYPE
+  export CYPRESS_CI=$CI
   yarn cy:run
 fi
