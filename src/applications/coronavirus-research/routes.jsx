8ccdae19f98b9bbfa0349a8e3539006900873240
Stephen Barrs <stephen.barrs@va.gov>
2020-08-20 09:49:04 -0400

    modify route onEnter as conditional

--- src/applications/coronavirus-research/routes.jsx
+++ src/applications/coronavirus-research/routes.jsx
@@ -2,20 +2,35 @@ import { createRoutesWithSaveInProgress } from 'platform/forms/save-in-progress/
 import formConfig from './config/form';
 import App from './containers/App.jsx';
 
+let hasRedirected = false;
+
 const route = {
   path: '/',
   component: App,
-  // indexRoute: { onEnter: (nextState, replace) => replace('/introduction') },
   indexRoute: {
     onEnter: (nextState, replace) => replace('/coronavirus-research/volunteer'),
   },
-
   childRoutes: createRoutesWithSaveInProgress(formConfig),
 };
 const introRouteIndex = route.childRoutes.findIndex(
   cr => cr.path === 'introduction',
 );
-route.childRoutes[introRouteIndex].onEnter = (nextState, replace) =>
-  replace('/coronavirus-research/volunteer');
 
+route.childRoutes[introRouteIndex].onEnter = (nextState, replace) => {
+  console.log('has redirected: ', hasRedirected);
+  console.log('next state: ', nextState);
+  if (!hasRedirected) {
+    hasRedirected = true;
+    console.log('On Enter called ...');
+    replace('/coronavirus-research/volunteer');
+  }
+};
+// route.childRoutes[introRouteIndex].onEnter = (nextState, replace) => {
+//   console.log('has redirected: ', hasRedirected);
+//   if (!hasRedirected) {
+//     hasRedirected = true;
+//     console.log('On Enter called ...');
+//     replace('/coronavirus-research/volunteer');
+//   }
+// };
 export default route;
