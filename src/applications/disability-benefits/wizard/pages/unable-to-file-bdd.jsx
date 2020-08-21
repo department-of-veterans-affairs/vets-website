bea730912d8c293bf3d18d7daf7988a10fd1b3c3
Nicholas Sprinkle <ndsprinkle@gmail.com>
2020-08-21 09:50:03 -0400

    BDD content changes

--- src/applications/disability-benefits/wizard/pages/unable-to-file-bdd.jsx
+++ src/applications/disability-benefits/wizard/pages/unable-to-file-bdd.jsx
@@ -20,9 +20,14 @@ function alertContent(getPageStateFromPageName) {
   return (
     <>
       <p>
-        You are ineligible to file a claim at this time. Please return in{' '}
-        <b>{differenceBetweenDatesInDays - 180}</b> day(s) in order to file your
-        BDD claim.
+        Based on your separation date, you may be eligible to file a disability
+        claim under the Benefits Delivery at Discharge (BDD) program. This
+        program allows service members to apply for disability benefits 180 to
+        90 days before they leave the military.
+      </p>
+      <p>
+        Since you have more than 180 days left on active duty, you can return in{' '}
+        <b>{differenceBetweenDatesInDays - 180}</b> day(s) to file a BDD claim.
       </p>
       <p>
         <a href={BDD_INFO_URL}>Learn more about the BDD program</a>
@@ -34,7 +39,7 @@ function alertContent(getPageStateFromPageName) {
 const UnableToFileBDDPage = ({ getPageStateFromPageName }) => (
   <AlertBox
     status="warning"
-    headline="You are ineligible to file a BDD claim"
+    headline="You may be eligible to file a BDD claim"
     content={alertContent(getPageStateFromPageName)}
   />
 );
