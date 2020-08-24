bba2501c22617b79738def43d134172edb1012a3
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-08-24 10:10:17 -0700

    added full schema

--- src/applications/vre/28-8832/config/chapters/veteran-information/veteranInformation.js
+++ src/applications/vre/28-8832/config/chapters/veteran-information/veteranInformation.js
@@ -4,60 +4,67 @@ import ssnUI from 'platform/forms-system/src/js/definitions/ssn';
 export const schema = {
   type: 'object',
   properties: {
-    fullName: {
+    veteranInformation: {
       type: 'object',
       properties: {
-        first: {
+        fullName: {
+          type: 'object',
+          properties: {
+            first: {
+              type: 'string',
+            },
+            middle: {
+              type: 'string',
+            },
+            last: {
+              type: 'string',
+            },
+            suffix: {
+              type: 'string',
+              enum: ['Jr.', 'Sr.', 'II', 'III', 'IV'],
+            },
+          },
+        },
+        vaFileNumber: {
           type: 'string',
         },
-        middle: {
+        ssn: {
           type: 'string',
-        },
-        last: {
-          type: 'string',
-        },
-        suffix: {
-          type: 'string',
-          enum: ['Jr.', 'Sr.', 'II', 'III', 'IV'],
         },
       },
-    },
-    vaFileNumber: {
-      type: 'string',
-    },
-    ssn: {
-      type: 'string',
     },
   },
 };
 
 export const uiSchema = {
-  fullName: {
-    first: {
-      'ui:title': 'First Name',
-      'ui:required': formData => isVeteran(formData),
+  veteranInformation: {
+    fullName: {
+      first: {
+        'ui:title': 'First Name',
+        'ui:required': formData => isVeteran(formData),
+      },
+      middle: {
+        'ui:title': 'Middle Name',
+      },
+      last: {
+        'ui:title': 'Last Name',
+        'ui:required': formData => isVeteran(formData),
+      },
+      suffix: {
+        'ui:title': 'Suffix',
+        'ui:options': {
+          widgetClassNames: 'usa-input-medium',
+        },
+      },
     },
-    middle: {
-      'ui:title': 'Middle Name',
-    },
-    last: {
-      'ui:title': 'Last Name',
-      'ui:required': formData => isVeteran(formData),
-    },
-    suffix: {
-      'ui:title': 'Suffix',
+    vaFileNumber: {
+      'ui:title': 'Your VA file number',
       'ui:options': {
         widgetClassNames: 'usa-input-medium',
       },
     },
-  },
-  vaFileNumber: {
-    'ui:title': 'Your VA file number',
-    'ui:options': {
-      widgetClassNames: 'usa-input-medium',
+    ssn: {
+      ...ssnUI,
     },
-  },
-  ssn: {
-    ...ssnUI,
   },
 };
