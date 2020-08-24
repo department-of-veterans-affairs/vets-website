f33c2045fe4aab9020357320ea0873c62a2861b1
Gibran <brangi@users.noreply.github.com>
2020-08-24 12:24:08 -0500

    Fix sticky error state after invalid location search (#13901)
    * Fix sticky error state after invalid location search
    
    * Add e2e test that covers fix error search state
    
    * Fix error type
    
    * Fix refactor search mesages, errors, results
    
    * fix the focus class
    
    * No need to add address/street names type as we only want  city, state or postal code
    
    * use es6 syntax, removed inner function
    
    * Add unit test
    
    * Add more coverage
    
    Co-authored-by: Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
    Co-authored-by: Michael Pelz-Sherman <mpelzsherman@gmail.com>

---
+++ src/applications/facility-locator/components/SearchResultMessage.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SearchResultMessage = ({
+  message,
+  error,
+  resultsFound,
+  resultRef,
+  facilityType,
+}) => {
+  if (facilityType && error) {
+    return (
+      <div className="search-result-title" ref={resultRef}>
+        <p>{message}</p>
+        <p>
+          If you need care right away for a minor illness or injury, select
+          Urgent care under facility type, then select either VA or community
+          providers as the service type.
+        </p>
+        <p>
+          If you have a medical emergency, please go to your nearest emergency
+          room or call 911.
+        </p>
+      </div>
+    );
+  } else if (facilityType && !resultsFound) {
+    return (
+      <div className="search-result-title" ref={resultRef}>
+        We didn't find any facilities near you. <br />
+        <strong>To try again, please enter a different:</strong>
+        <ul className="vads-u-margin-y--1p5">
+          <li>
+            <strong>Search term</strong> (street, city, state, or postal code),{' '}
+            <strong>or</strong>
+          </li>
+          <li>
+            <strong>Service type</strong> (like “primary care”), and select the
+            option that best meets your needs
+          </li>
+        </ul>
+        Then click <strong>Search</strong>.
+      </div>
+    );
+  }
+
+  return (
+    <div className="search-result-title">
+      Please enter a location (street, city, state or postal code) and click
+      search above to find facilities.
+    </div>
+  );
+};
+
+SearchResultMessage.propTypes = {
+  message: PropTypes.string,
+  error: PropTypes.any,
+  resultsFound: PropTypes.bool,
+  resultRef: PropTypes.any,
+  facilityType: PropTypes.string,
+};
+
+export default SearchResultMessage;
