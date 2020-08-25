231db986ea133c1e26647b973445b9da2a3cf028
Micah Chiang <micahkchiang@gmail.com>
2020-08-25 10:47:07 -0400

    686 copy changes (#13992)
    * 686 copy changes
    
    * lower case breadcrumb

--- src/applications/disability-benefits/686c-674/components/IntroductionPageHeader.jsx
+++ src/applications/disability-benefits/686c-674/components/IntroductionPageHeader.jsx
@@ -3,7 +3,7 @@ import FormTitle from 'platform/forms-system/src/js/components/FormTitle';
 
 export const IntroductionPageHeader = () => (
   <>
-    <FormTitle title="Add or remove dependents from your VA benefits" />
+    <FormTitle title="Add or remove a dependent on your VA disability benefits" />
     <p>
       Equal to VA Form 21-686c (Application Request to Add And/Or Remove
       Dependents) and/or Equal to VA Form 21-674 (Request for Approval of School
