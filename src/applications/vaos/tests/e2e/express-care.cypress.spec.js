c5e8ead0a3e58a1f7dde460a4932c502873a74ec
Narin Ratana <n.ratana@gmail.com>
2020-08-25 08:50:43 -0700

    Add url checks

--- src/applications/vaos/tests/e2e/express-care.cypress.spec.js
+++ src/applications/vaos/tests/e2e/express-care.cypress.spec.js
@@ -1,7 +1,7 @@
 import { initExpressCareMocks } from './vaos-cypress-helpers';
 
 describe('Express Care request flow', () => {
-  it('should go through flow successfully', () => {
+  it('should submit a request successfully', () => {
     initExpressCareMocks();
     cy.visit('health-care/schedule-view-va-appointments/appointments/');
     cy.injectAxe();
@@ -10,17 +10,21 @@ describe('Express Care request flow', () => {
     cy.axeCheck();
     cy.findByText('Create an Express Care request').click();
     cy.findByText('How Express Care works');
+    cy.url().should('include', '/new-express-care-request');
     cy.axeCheck();
     cy.findByText('Continue with Express Care request').click({ force: true });
     cy.wait('@getRequestLimits');
     cy.findByText('Select a reason for your Express Care request');
+    cy.url().should('include', '/new-express-care-request/select-reason');
     cy.axeCheck();
     cy.findByText('Cough').click();
     cy.findByText('Continue').click();
     cy.findByText('Express Care request details');
+    cy.url().should('include', '/new-express-care-request/additional-details');
     cy.axeCheck();
     cy.get('textarea').type('heavy cough');
     cy.findByText('Submit Express Care request').click();
     cy.findByText('You’ve successfully submitted your Express Care request');
+    cy.url().should('include', '/new-express-care-request/confirmation');
   });
 });
