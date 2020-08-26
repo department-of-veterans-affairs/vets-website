6c04af2e7622525bcb63e2cab1416e7c21c7ca06
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-08-26 15:08:45 -0500

    Fix

--- src/applications/facility-locator/tests/e2e/search.cypress.spec.js
+++ src/applications/facility-locator/tests/e2e/search.cypress.spec.js
@@ -233,8 +233,7 @@ describe('Facility search', () => {
     cy.get('.facility-phone-group').should('exist');
     cy.findByText(/Get Directions/i).should('exist');
     cy.get('[alt="Static map"]').should('exist');
-    cy.get('#hours-op').should('exist');
-    cy.get('h3').contains('Hours of operation');
+    cy.get('#hours-op h3').contains('Hours of operation');
 
     cy.axeCheck();
   });
