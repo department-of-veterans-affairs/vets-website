184a2ec8727e4868815d78eb413c11de9a1fbf1a
Rob Garrison <robin.garrison@adhocteam.us>
2020-08-27 15:17:47 -0500

    Use intro page url for wizard check (#14041)
    * Use rootUrl for wizard check
    
    * Use constant

--- src/applications/disability-benefits/wizard/pages/file-claim-early.jsx
+++ src/applications/disability-benefits/wizard/pages/file-claim-early.jsx
@@ -7,20 +7,20 @@ const FileClaimPage = ({ setWizardStatus }) => (
   <div>
     <p>
       {/* Remove link to introduction page once show526Wizard flipper is at 100% */}
-      {window.location.pathname.includes('how-to-file-claim') ? (
-        <a
-          href={`${DISABILITY_526_V2_ROOT_URL}/introduction`}
-          className="usa-button-primary va-button-primary"
-        >
-          File a disability compensation claim
-        </a>
-      ) : (
+      {window.location.pathname.includes(DISABILITY_526_V2_ROOT_URL) ? (
         <button
           onClick={() => setWizardStatus(WIZARD_STATUS_COMPLETE)}
           className="usa-button-primary va-button-primary"
         >
           File a disability compensation claim
         </button>
+      ) : (
+        <a
+          href={`${DISABILITY_526_V2_ROOT_URL}/introduction`}
+          className="usa-button-primary va-button-primary"
+        >
+          File a disability compensation claim
+        </a>
       )}
     </p>
   </div>
