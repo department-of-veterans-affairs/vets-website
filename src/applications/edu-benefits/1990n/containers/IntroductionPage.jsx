c2e77cfd84d4e555a5429b1baf5a99b50c7aabcb
Craig Wheeler <craig@craigwheeler.net>
2020-08-27 18:16:45 -0400

    refactor form 1990n

--- src/applications/edu-benefits/1990n/containers/IntroductionPage.jsx
+++ src/applications/edu-benefits/1990n/containers/IntroductionPage.jsx
@@ -4,18 +4,12 @@ import OMBInfo from '@department-of-veterans-affairs/formation-react/OMBInfo';
 import FormTitle from 'platform/forms-system/src/js/components/FormTitle';
 import SaveInProgressIntro from 'platform/forms/save-in-progress/SaveInProgressIntro';
 import WizardContainer from 'applications/edu-benefits/wizard/containers/WizardContainer';
-import {
-  WIZARD_STATUS_NOT_STARTED,
-  WIZARD_STATUS_COMPLETE,
-} from 'applications/static-pages/wizard';
+import { WIZARD_STATUS_COMPLETE } from 'applications/static-pages/wizard';
 import { connect } from 'react-redux';
 import { showEduBenefits1990NWizard } from 'applications/edu-benefits/selectors/educationWizard';
 
 export class IntroductionPage extends React.Component {
-  state = {
-    wizardStatus:
-      sessionStorage.getItem('wizardStatus') || WIZARD_STATUS_NOT_STARTED,
-  };
+  state = { status: sessionStorage.getItem('wizardStatus') };
 
   componentDidMount() {
     focusElement('.va-nav-breadcrumbs-list');
@@ -23,17 +17,15 @@ export class IntroductionPage extends React.Component {
 
   setWizardStatus = value => {
     sessionStorage.setItem('wizardStatus', value);
-    this.setState({ wizardStatus: value });
+    this.setState({ status: value });
   };
+
   render() {
-    const { wizardStatus } = this.state;
-    const { shouldEduBenefits1990NWizardShow } = this.props;
-    const shouldSubwayMapShow =
-      !shouldEduBenefits1990NWizardShow ||
-      wizardStatus === WIZARD_STATUS_COMPLETE;
-    const shouldWizardShow =
-      shouldEduBenefits1990NWizardShow &&
-      wizardStatus !== WIZARD_STATUS_COMPLETE;
+    const { status } = this.state;
+    const { showWizard } = this.props;
+    const show = showWizard && status !== WIZARD_STATUS_COMPLETE;
+
+    if (showWizard === undefined) return null;
     return (
       <div className="schemaform-intro">
         <FormTitle title="Apply for education benefits under the National Call to Service program" />
@@ -41,10 +33,9 @@ export class IntroductionPage extends React.Component {
           Equal to VA Form 22-1990N (Application for VA Education Benefits Under
           the National Call to Service (NCS) Program).
         </p>
-        {shouldWizardShow && (
+        {show ? (
           <WizardContainer setWizardStatus={this.setWizardStatus} />
-        )}
-        {shouldSubwayMapShow && (
+        ) : (
           <div className="subway-map">
             <SaveInProgressIntro
               prefillEnabled={this.props.route.formConfig.prefillEnabled}
@@ -155,7 +146,7 @@ export class IntroductionPage extends React.Component {
 }
 
 const mapStateToProps = state => ({
-  shouldEduBenefits1990NWizardShow: showEduBenefits1990NWizard(state),
+  showWizard: showEduBenefits1990NWizard(state),
 });
 
 export default connect(mapStateToProps)(IntroductionPage);
