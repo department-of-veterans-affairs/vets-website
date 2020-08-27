cfc5f5ea9e50a8e2e63a281c41c61b3256036849
Kelson Adams <kadams@governmentcio.com>
2020-08-27 13:01:31 -0600

    Use cerner-page-related feature toggles fixtures

--- src/applications/static-pages/health-care-manage-benefits/schedule-view-va-appointments-page/tests/index.cypress.spec.js
+++ src/applications/static-pages/health-care-manage-benefits/schedule-view-va-appointments-page/tests/index.cypress.spec.js
@@ -4,27 +4,34 @@ import notCernerUser from '../../fixtures/user/notCerner.json';
 const TEST_URL = '/health-care/schedule-view-va-appointments/';
 
 const setup = ({ authenticated, isCerner } = {}) => {
-  // Clear announcements.
-  window.localStorage.setItem(
-    'DISMISSED_ANNOUNCEMENTS',
-    JSON.stringify(['single-sign-on-intro']),
-  );
+  cy.fixture(
+    '../../src/applications/static-pages/health-care-manage-benefits/fixtures/feature-toggles/enabled.json',
+  ).then(features => {
+    // Mock feature toggles route.
+    cy.route('GET', '/v0/feature_toggles*', features);
 
-  // Navigate straight to the test URL if unauth.
-  if (!authenticated) {
+    // Clear announcements.
+    window.localStorage.setItem(
+      'DISMISSED_ANNOUNCEMENTS',
+      JSON.stringify(['single-sign-on-intro']),
+    );
+
+    // Navigate straight to the test URL if unauth.
+    if (!authenticated) {
+      cy.visit(TEST_URL);
+      return;
+    }
+
+    // Log in the user.
+    if (isCerner) {
+      cy.login(cernerUser);
+    } else {
+      cy.login(notCernerUser);
+    }
+
+    // Visit the test URL.
     cy.visit(TEST_URL);
-    return;
-  }
-
-  // Log in the user.
-  if (isCerner) {
-    cy.login(cernerUser);
-  } else {
-    cy.login(notCernerUser);
-  }
-
-  // Visit the test URL.
-  cy.visit(TEST_URL);
+  });
 };
 
 describe('The schedule view VA appointments page', () => {
