54d0bebfeb974a7fabb43db441dc777d732a31d3
Mark Dewey <mdewey@governmentcio.com>
2020-08-28 09:58:11 -0400

    working

--- src/applications/healthcare/questionnaire/tests/e2e/01.questionnaire.demographics.cypress.spec.js
+++ src/applications/healthcare/questionnaire/tests/e2e/01.questionnaire.demographics.cypress.spec.js
@@ -1,13 +1,14 @@
 const Timeouts = require('platform/testing/e2e/timeouts.js');
 
-import mockUser from './fixtures/users/user-x.json';
+import mockUser from './fixtures/users/user-basic.json';
 
 it('healthcare questionnaire -- demographics -- loads', () => {
   cy.fixture(
     '../../src/applications/healthcare/questionnaire/tests/e2e/fixtures/mocks/feature-toggles.enabled.json',
-  ).then(features => {
-    cy.route('GET', '/v0/feature_toggles*', features);
+  ).then(async features => {
+    // console.log({ mockUser });
     cy.login(mockUser);
+    cy.route('GET', '/v0/feature_toggles*', features);
     cy.visit('/healthcare/questionnaire/demographics');
     cy.get('.schemaform-title>h1', { timeout: Timeouts.normal }).contains(
       'Healthcare Questionnaire',
