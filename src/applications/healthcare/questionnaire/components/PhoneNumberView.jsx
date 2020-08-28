40f2fd8b941a9455fd523ee29acf4f592b5ed5de
Mark Dewey <mdewey@governmentcio.com>
2020-08-28 16:51:19 -0400

    added unit tests for phone number view

--- src/applications/healthcare/questionnaire/components/PhoneNumberView.jsx
+++ src/applications/healthcare/questionnaire/components/PhoneNumberView.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export default function PhoneNumberView({ number }) {
-  const fullphone = `${number.data.areaCode}${number.data.phoneNumber}`;
+  const fullPhone = `${number.data.areaCode}${number.data.phoneNumber}`;
   let i = 0;
-  const phone = '###-###-####'.replace(/#/g, () => fullphone[i++] || '');
+  if (fullPhone.length !== 10) {
+    return <></>;
+  }
+  const phone = '###-###-####'.replace(/#/g, () => fullPhone[i++] || '');
   return (
     <p className="vads-u-margin--1px">
       <span className=" vads-u-font-weight--bold">{number.label}: </span>
