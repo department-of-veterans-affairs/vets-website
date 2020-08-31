ec449289551ecdb0929aabe3f20b7e5bbea48250
Rob Garrison <robin.garrison@adhocteam.us>
2020-08-31 11:54:55 -0500

    [Development] HLR updates (#13501)
    * Update no contestable issues alert
    
    * Update telephone links
    
    * Update unit tests
    
    * Remove server error response
    
    * Update content based on feedback
    
    * Update unit tests
    
    * Download link updates
    
    * Update eligible info

--- src/applications/disability-benefits/996/containers/ConfirmationPage.jsx
+++ src/applications/disability-benefits/996/containers/ConfirmationPage.jsx
@@ -5,6 +5,9 @@ import Scroll from 'react-scroll';
 
 import { focusElement } from 'platform/utilities/ui';
 import { selectProfile } from 'platform/user/selectors';
+import Telephone, {
+  CONTACTS,
+} from '@department-of-veterans-affairs/formation-react/Telephone';
 
 const scroller = Scroll.scroller;
 const scrollToTop = () => {
@@ -104,14 +107,7 @@ export class ConfirmationPage extends React.Component {
         <p>
           If you requested a decision review and haven’t heard back from VA yet,
           please don’t request another review. Call VA at{' '}
-          <a
-            href="tel:1-800-827-1000"
-            aria-label="8 0 0. 8 2 7. 1 0 0 0."
-            className="nowrap"
-          >
-            800-827-1000
-          </a>
-          .
+          <Telephone contact={CONTACTS.VA_BENEFITS} />.
         </p>
         <br />
         <a
