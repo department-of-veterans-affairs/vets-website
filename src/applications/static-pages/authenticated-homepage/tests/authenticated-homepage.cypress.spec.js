981aac87d5b60373bdf65cdb43cda1926142a676
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2020-08-31 12:44:24 -0600

    Updates to test

--- src/applications/static-pages/authenticated-homepage/tests/authenticated-homepage.cypress.spec.js
+++ src/applications/static-pages/authenticated-homepage/tests/authenticated-homepage.cypress.spec.js
@@ -1,5 +1,5 @@
 import mockUser from 'applications/personalization/profile-2/tests/fixtures/users/user-36.json';
-import mockFeatureToggles from 'applications/static-pages/authenticated-homepage/tests/fixtures/feature-toggles.json';
+// import mockFeatureToggles from 'applications/static-pages/authenticated-homepage/tests/fixtures/feature-toggles.json';
 
 describe('The authenticated homepage', () => {
   describe('does not render when the user is not logged in and', () => {
@@ -14,14 +14,18 @@ describe('The authenticated homepage', () => {
 
     it('the localStorage value of AUTHENTICATED_HOMEPAGE is "true"', () => {
       localStorage.setItem('AUTHENTICATED_HOMEPAGE', true);
+      cy.reload();
       cy.get('[id="homepage"]').should('be.visible');
       cy.get('[data-widget-type="authenticated-homepage"]').should('be.empty');
     });
 
-    it('the feature flag is on', () => {
-      cy.get('[id="homepage"]').should('be.visible');
-      cy.get('[data-widget-type="authenticated-homepage"]').should('be.empty');
-    });
+    // Uncomment when the feature flag is added to vets-api
+    // it('the feature flag is on', () => {
+    //   cy.route('GET', '/v0/feature_toggles*', mockFeatureToggles);
+    //   cy.reload();
+    //   cy.get('[id="homepage"]').should('be.visible');
+    //   cy.get('[data-widget-type="authenticated-homepage"]').should('be.empty');
+    // });
   });
 
   describe('does not render when the user is logged in and', () => {
@@ -33,14 +37,16 @@ describe('The authenticated homepage', () => {
     it('the localStorage value of AUTHENTICATED_HOMEPAGE is "true" and NO_AUTHENTICATED_HOMEPAGE is "true"', () => {
       localStorage.setItem('AUTHENTICATED_HOMEPAGE', true);
       localStorage.setItem('NO_AUTHENTICATED_HOMEPAGE', true);
+      cy.reload();
       cy.get('[id="homepage"]').should('be.visible');
       cy.get('[data-widget-type="authenticated-homepage"]').should('be.empty');
     });
 
-    // Uncomment and fix this when the feature flag is added to vets-api
+    // Uncomment when the feature flag is added to vets-api
     // it('the feature flag is on and NO_AUTHENICATED_HOMEPAGE is "true"', () => {
     //   cy.route('GET', '/v0/feature_toggles*', mockFeatureToggles);
     //   localStorage.setItem('NO_AUTHENTICATED_HOMEPAGE', true);
+    // cy.visit('/');
     //   cy.get('[id="homepage"]').should('be.visible');
     //   cy.get('[data-widget-type="authenticated-homepage"]').should('be.empty');
     // });
@@ -58,6 +64,7 @@ describe('The authenticated homepage', () => {
 
     it('the localStorage value of AUTHENTICATED_HOMEPAGE is "true"', () => {
       localStorage.setItem('AUTHENTICATED_HOMEPAGE', true);
+      cy.reload();
       cy.get('[id="homepage"]').should('not.be.visible');
       cy.get('[data-widget-type="authenticated-homepage"]').should(
         'be.visible',
@@ -67,7 +74,25 @@ describe('The authenticated homepage', () => {
       );
     });
 
-    // Uncomment and fix this when the feature flag is added to vets-api
+    it('the localStorage value of NO_AUTHENTICATED_HOMEPAGE is flipped twice', () => {
+      localStorage.setItem('AUTHENTICATED_HOMEPAGE', true);
+      cy.reload();
+      cy.get('[id="homepage"]').should('not.be.visible');
+      localStorage.setItem('NO_AUTHENTICATED_HOMEPAGE', true);
+      cy.reload();
+      cy.get('[id="homepage"]').should('be.visible');
+      localStorage.setItem('NO_AUTHENTICATED_HOMEPAGE', false);
+      cy.reload();
+      cy.get('[id="homepage"]').should('not.be.visible');
+      cy.get('[data-widget-type="authenticated-homepage"]').should(
+        'be.visible',
+      );
+      cy.get('[data-widget-type="authenticated-homepage"]').should(
+        'not.be.empty',
+      );
+    });
+
+    // Uncomment when the feature flag is added to vets-api
     // it('the feature flag is on', () => {
     //   cy.route('GET', '/v0/feature_toggles*', mockFeatureToggles);
     //   cy.get('[id="homepage"]').should('be.empty');
