2658d79ae03eccc5994546246a044c0342ed3ea2
Jeff Balboni <jeff@adhocteam.us>
2020-09-01 10:33:31 -0400

    Attempt to handle lazy loading errors in VAOS (#14044)

--- src/applications/vaos/routes.jsx
+++ src/applications/vaos/routes.jsx
@@ -4,6 +4,26 @@ import asyncLoader from 'platform/utilities/ui/asyncLoader';
 import AppointmentsPage from './containers/AppointmentsPage';
 import VAOSApp from './containers/VAOSApp';
 import ErrorBoundary from './components/ErrorBoundary';
+import { captureError } from './utils/error';
+import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
+import ErrorMessage from './components/ErrorMessage';
+import FullWidthLayout from './components/FullWidthLayout';
+
+// Handles errors loading components by doing one page reload and showing
+// an error message otherwise
+function handleLoadError(err) {
+  if (window.location.search?.includes('retry=1')) {
+    captureError(new Error(`vaos_lazy_loading: ${err.message}`));
+    return () => (
+      <FullWidthLayout>
+        <ErrorMessage />
+      </FullWidthLayout>
+    );
+  } else {
+    window.location.replace(`${window.location.pathname}?retry=1`);
+    return () => <LoadingIndicator message="Reloading page" />;
+  }
+}
 
 export default function createRoutesWithStore(store) {
   return (
@@ -13,23 +33,23 @@ export default function createRoutesWithStore(store) {
           <Route
             path="/new-appointment"
             component={asyncLoader(() =>
-              import(/* webpackChunkName: "vaos-form" */ './new-appointment').then(
-                ({ NewAppointment, reducer }) => {
+              import(/* webpackChunkName: "vaos-form" */ './new-appointment')
+                .then(({ NewAppointment, reducer }) => {
                   store.injectReducer('newAppointment', reducer);
                   return NewAppointment;
-                },
-              ),
+                })
+                .catch(handleLoadError),
             )}
           />
           <Route
             path="/new-express-care-request"
             component={asyncLoader(() =>
-              import(/* webpackChunkName: "express-care" */ './express-care').then(
-                ({ NewExpressCareRequest, reducer }) => {
+              import(/* webpackChunkName: "express-care" */ './express-care')
+                .then(({ NewExpressCareRequest, reducer }) => {
                   store.injectReducer('expressCare', reducer);
                   return NewExpressCareRequest;
-                },
-              ),
+                })
+                .catch(handleLoadError),
             )}
           />
           <Route path="/" component={AppointmentsPage} />
