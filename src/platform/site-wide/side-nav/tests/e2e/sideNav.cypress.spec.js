9195632f78a3d7c5b4e2b48893226c5ce0a77dea
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-09-01 15:52:51 -0500

    migrate e2e left nav test to cypress

---
+++ src/platform/site-wide/side-nav/tests/e2e/sideNav.cypress.spec.js
@@ -0,0 +1,44 @@
+it('should tab access the links on the left nav and verify active link elements', () => {
+  cy.visit('/pittsburgh-health-care');
+
+  cy.injectAxe();
+  cy.axeCheck();
+
+  // Accept initial modal and start atop
+  cy.findByText(/Continue to the website/i)
+    .first()
+    .click();
+  cy.scrollTo(0, 0);
+
+  // Start tab access level one
+  cy.get('.va-sidenav-item-label')
+    .first()
+    .focus();
+
+  do {
+    cy.tab();
+    cy.focused().should('have.attr', 'aria-label');
+  } while (
+    !cy
+      .get('.va-sidenav-level-2 a')
+      .last()
+      .focus()
+  );
+
+  // Start tab access level 2
+  cy.findByText(/Locations/i, { selector: 'a' })
+    .first()
+    .click();
+  cy.get('.va-sidenav-item-label')
+    .first()
+    .focus();
+  do {
+    cy.tab();
+    cy.focused().should('have.attr', 'aria-label');
+  } while (
+    !cy
+      .get('.va-sidenav-level-3 a')
+      .last()
+      .focus()
+  );
+});
