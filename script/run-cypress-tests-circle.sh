013ff78fc3fe675ada6c14ee3fe224680775f8fe
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-09-02 16:39:06 -0500

    Set env var for circle ci and skip test

--- script/run-cypress-tests-circle.sh
+++ script/run-cypress-tests-circle.sh
@@ -10,6 +10,9 @@ else
   # Use mocha-junit-reporter and save results in './test-results'.
   reporterArgs="--reporter cypress-multi-reporters --reporter-options \"configFile=config/cypress-reporters.json\""
 
+  #Env args
+  envArgs =" --env ci_name=circle"
+
   # Start the web server & run Cypress tests.
-  yarn start-server-and-test "node src/platform/testing/e2e/test-server.js --buildtype=vagovprod --port=3001 > /dev/null 2>&1" :3001 "yarn cy:run $reporterArgs"
+  yarn start-server-and-test "node src/platform/testing/e2e/test-server.js --buildtype=vagovprod --port=3001 > /dev/null 2>&1" :3001 "yarn cy:run $reporterArgs $envArgs"
 fi
