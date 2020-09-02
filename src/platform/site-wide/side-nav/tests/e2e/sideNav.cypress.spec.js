7bd51b8b677b8e1eb3fa0248491139436ca70d1e
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-09-02 18:03:28 -0500

    Disable cypress tests against content on circleCI

--- src/platform/site-wide/side-nav/tests/e2e/sideNav.cypress.spec.js
+++ src/platform/site-wide/side-nav/tests/e2e/sideNav.cypress.spec.js
@@ -1,45 +1,53 @@
-it('should tab access the links on the left nav and verify active link elements', () => {
-  if (Cypress.env('ci_name') && Cypress.env('ci_name') === 'circle') return;
-  cy.visit('/pittsburgh-health-care');
-  cy.injectAxe();
-  cy.axeCheck();
+describe('Facilities VAMC SideNav', () => {
+  beforeEach(function() {
+    if (Cypress.env('ci_name') && Cypress.env('ci_name') === 'circle') {
+      this.skip();
+    }
+  });
 
-  // Accept initial modal and start atop
-  cy.findByText(/Continue to the website/i)
-    .first()
-    .click();
-  cy.scrollTo(0, 0);
+  it('should tab access the links on the left nav and verify focus', () => {
+    if (Cypress.env('ci_name') && Cypress.env('ci_name') === 'circle') return;
+    cy.visit('/pittsburgh-health-care');
+    cy.injectAxe();
+    cy.axeCheck();
 
-  // Start tab access level one
-  cy.get('.va-sidenav-item-label')
-    .first()
-    .focus();
+    // Accept initial modal and start atop
+    cy.findByText(/Continue to the website/i)
+      .first()
+      .click();
+    cy.scrollTo(0, 0);
 
-  do {
-    cy.tab();
-    cy.focused().should('have.attr', 'aria-label');
-  } while (
-    !cy
-      .get('.va-sidenav-level-2 a')
-      .last()
-      .focus()
-  );
+    // Start tab access level one
+    cy.get('.va-sidenav-item-label')
+      .first()
+      .focus();
 
-  // Start tab access level 2
-  cy.findByText(/Locations/i, { selector: 'a' })
-    .first()
-    .click();
-  cy.get('.va-sidenav-item-label')
-    .first()
-    .focus();
+    do {
+      cy.tab();
+      cy.focused().should('have.attr', 'aria-label');
+    } while (
+      !cy
+        .get('.va-sidenav-level-2 a')
+        .last()
+        .focus()
+    );
 
-  do {
-    cy.tab();
-    cy.focused().should('have.attr', 'aria-label');
-  } while (
-    !cy
-      .get('.va-sidenav-level-3 a')
-      .last()
-      .focus()
-  );
+    // Start tab access level 2
+    cy.findByText(/Locations/i, { selector: 'a' })
+      .first()
+      .click();
+    cy.get('.va-sidenav-item-label')
+      .first()
+      .focus();
+
+    do {
+      cy.tab();
+      cy.focused().should('have.attr', 'aria-label');
+    } while (
+      !cy
+        .get('.va-sidenav-level-3 a')
+        .last()
+        .focus()
+    );
+  });
 });
