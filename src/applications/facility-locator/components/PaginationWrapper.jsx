7176820394836e3bd690e7e1b2761375a601f647
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2020-09-02 21:39:05 -0400

    Hide pagination controls on community provider searches
    https://github.com/department-of-veterans-affairs/va.gov-team/issues/12715

---
+++ src/applications/facility-locator/components/PaginationWrapper.jsx
@@ -0,0 +1,65 @@
+import Pagination from '@department-of-veterans-affairs/formation-react/Pagination';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { LocationType } from '../constants';
+
+class PaginationWrapper extends Component {
+  shouldComponentUpdate = nextProps =>
+    nextProps.results !== this.props.results ||
+    nextProps.inProgress !== this.props.inProgress;
+
+  render() {
+    const {
+      shouldRender,
+      handlePageSelect,
+      currentPage,
+      totalPages,
+      results,
+    } = this.props;
+
+    if (
+      shouldRender &&
+      currentPage &&
+      totalPages &&
+      results &&
+      results.length > 0
+    ) {
+      return (
+        <Pagination
+          onPageSelect={handlePageSelect}
+          page={currentPage}
+          pages={totalPages}
+        />
+      );
+    }
+    return null;
+  }
+}
+
+const mapStateToProps = state => {
+  let isCommunityProviderSearch = false;
+
+  if (
+    [LocationType.CC_PROVIDER, LocationType.URGENT_CARE_PHARMACIES].includes(
+      state.searchQuery.facilityType,
+    )
+  ) {
+    isCommunityProviderSearch = true;
+  }
+
+  if (
+    state.searchQuery.facilityType === LocationType.URGENT_CARE &&
+    (!state.searchQuery.serviceType ||
+      ['AllUrgentCare', 'NonVAUrgentCare'].includes(
+        state.searchQuery.serviceType,
+      ))
+  ) {
+    isCommunityProviderSearch = true;
+  }
+
+  return {
+    shouldRender: !isCommunityProviderSearch,
+  };
+};
+
+export default connect(mapStateToProps)(PaginationWrapper);
