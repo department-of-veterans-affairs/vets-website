1b9399117073547b9c0e0cb4c2a7a6db3c9275d5
Matt Roth <roth_matthew@bah.com>
2020-09-03 14:50:15 -0400

    transfer remaining tests into new format

--- src/platform/forms/tests/forms.unit.spec.js
+++ src/platform/forms/tests/forms.unit.spec.js
@@ -21,7 +21,7 @@ const missingFromVetsJsonSchema = [
 
 const root = path.join(__dirname, '../../../');
 
-const formIdTest = formId => {
+const validFormId = formId => {
   if (!missingFromVetsJsonSchema.includes(formId)) {
     return expect(Object.keys(schemas)).to.include(
       formId,
@@ -31,7 +31,7 @@ const formIdTest = formId => {
   return true;
 };
 
-const migrationsTest = formConfig => {
+const validMigrations = formConfig => {
   const { migrations } = formConfig;
   if (migrations || formConfig.version > 0) {
     return (
@@ -46,6 +46,36 @@ const migrationsTest = formConfig => {
     );
   }
   return true;
+};
+
+const validTitle = ({ title }) => {
+  const formTitle =
+    typeof title === 'function' ? title({ formData: {} }) : title;
+  return expect(formTitle).to.be.a('string', 'title does not return a string');
+};
+
+const validProperty = (formConfig, name, type, required = true) => {
+  const property = formConfig[name];
+  if (required || property) {
+    return expect(property).to.be.a(type, `${name} is not a ${type}`);
+  }
+  return true;
+};
+
+const validObjectProperty = (formConfig, name, required = true) => {
+  return validProperty(formConfig, name, 'object', required);
+};
+
+const validFunctionProperty = (formConfig, name, required = true) => {
+  return validProperty(formConfig, name, 'function', required);
+};
+
+const validBooleanProperty = (formConfig, name, required = true) => {
+  return validProperty(formConfig, name, 'boolean', required);
+};
+
+const validStringProperty = (formConfig, name, required = true) => {
+  return validProperty(formConfig, name, 'string', required);
 };
 
 describe('form:', () => {
@@ -65,130 +95,30 @@ describe('form:', () => {
             formId = remapFormId[formId];
           }
 
-          return formIdTest(formId) && migrationsTest(formConfig);
+          return (
+            validFormId(formId) &&
+            validMigrations(formConfig) &&
+            validObjectProperty(formConfig, 'chapters') &&
+            validObjectProperty(formConfig, 'defaultDefinitions') &&
+            validFunctionProperty(formConfig, 'introduction', false) &&
+            validBooleanProperty(formConfig, 'prefillEnabled', false) &&
+            validFunctionProperty(formConfig, 'prefillTransformer', false) &&
+            validStringProperty(formConfig, 'trackingPrefix') &&
+            validTitle(formConfig) &&
+            validStringProperty(formConfig, 'subTitle', false) &&
+            validStringProperty(formConfig, 'urlPrefix', false) &&
+            validStringProperty(formConfig, 'submitUrl', false) &&
+            validFunctionProperty(formConfig, 'submit', false) &&
+            validObjectProperty(formConfig, 'saveFormMessages', false) &&
+            validFunctionProperty(formConfig, 'transformForSubmit', false) &&
+            validFunctionProperty(formConfig, 'confirmation') &&
+            validObjectProperty(formConfig, 'preSubmitInfo', false) &&
+            validFunctionProperty(formConfig, 'footerContent', false) &&
+            validFunctionProperty(formConfig, 'getHelp', false) &&
+            validFunctionProperty(formConfig, 'errorText', false)
+          );
         }),
       ).to.eventually.be.ok;
     });
   });
 });
-
-// configs.forEach(form => {
-//   describe(`${form.formId}:`, () => {
-//     describe('migrations:', () => {
-//       const { migrations } = form;
-//       if (migrations || form.version > 0) {
-//         it('should have a length equal to the version number', () => {
-//           expect(migrations.length).to.equal(form.version);
-//         });
-//         it('should be typeof array', () => {
-//           expect(migrations).to.be.an('array');
-//         });
-//         it('should be array of functions', () => {
-//           expect(
-//             migrations.every(migration => typeof migration === 'function'),
-//           ).to.be.true;
-//         });
-//       }
-//     });
-//
-//     it('should have chapters object', () => {
-//       expect(form.chapters).to.be.an('object');
-//     });
-//
-//     it('should have defaultDefinitions object', () => {
-//       expect(form.defaultDefinitions).to.be.an('object');
-//     });
-//
-//     if (form.introduction) {
-//       it('should have introduction function', () => {
-//         expect(form.introduction).to.be.a('function');
-//       });
-//     }
-//
-//     if (form.prefillEnabled) {
-//       it('should have prefillEnabled boolean', () => {
-//         expect(form.prefillEnabled).to.be.a('boolean');
-//       });
-//     }
-//
-//     if (form.prefillTransformer) {
-//       it('should have prefillTransformer function', () => {
-//         expect(form.prefillTransformer).to.be.a('function');
-//       });
-//     }
-//
-//     it('should have trackingPrefix', () => {
-//       expect(form.trackingPrefix).to.be.a('string');
-//     });
-//
-//     it('should have title', () => {
-//       const title =
-//         typeof form.title === 'function'
-//           ? form.title({ formData: {} })
-//           : form.title;
-//       expect(title).to.be.a('string');
-//     });
-//
-//     if (form.subTitle) {
-//       it('should have subTitle', () => {
-//         expect(form.subTitle).to.be.a('string');
-//       });
-//     }
-//
-//     it('should have urlPrefix', () => {
-//       expect(form.urlPrefix).to.be.a('string');
-//     });
-//
-//     if (form.submitUrl) {
-//       it('should have submitUrl', () => {
-//         expect(form.submitUrl).to.be.a('string');
-//       });
-//     }
-//
-//     if (form.submit) {
-//       it('should have submit', () => {
-//         expect(form.submit).to.be.a('function');
-//       });
-//     }
-//
-//     if (form.savedFormMessages) {
-//       it('should have savedFormMessages', () => {
-//         expect(form.savedFormMessages).to.be.a('object');
-//       });
-//     }
-//
-//     if (form.transformForSubmit) {
-//       it('should have transformForSubmit', () => {
-//         expect(form.transformForSubmit).to.be.a('function');
-//       });
-//     }
-//
-//     it('should have confirmation', () => {
-//       expect(form.confirmation).to.be.a('function');
-//     });
-//
-//     if (form.preSubmitInfo) {
-//       it('should have preSubmitInfo', () => {
-//         expect(form.preSubmitInfo).to.be.a('object');
-//       });
-//     }
-//
-//     if (form.footerContent) {
-//       it('should have footerContent', () => {
-//         expect(form.footerContent).to.be.a('function');
-//       });
-//     }
-//
-//     if (form.getHelp) {
-//       it('should have getHelp', () => {
-//         expect(form.getHelp).to.be.a('function');
-//       });
-//     }
-//
-//     if (form.errorText) {
-//       it('should have errorText', () => {
-//         expect(form.errorText).to.be.a('function');
-//       });
-//     }
-//   });
-// });
