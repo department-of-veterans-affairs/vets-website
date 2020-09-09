19b49b88d7bbbb151ae97c23f14b59949bd566a7
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-09-09 15:13:12 -0400

    Add fieldButtons

--- src/site/stages/build/drupal/graphql/nodeQa.graphql.js
+++ src/site/stages/build/drupal/graphql/nodeQa.graphql.js
@@ -1,17 +1,8 @@
 const entityElementsFromPages = require('./entityElementsForPages.graphql');
 
 const WYSIWYG = '... wysiwyg';
-const COLLAPSIBLE_PANEL = '... collapsiblePanel';
-const PROCESS = '... process';
-const QA_SECTION = '... qaSection';
-const LIST_OF_LINK_TEASERS = '... listOfLinkTeasers';
-const REACT_WIDGET = '... reactWidget';
-const SPANISH_SUMMARY = '... spanishSummary';
 const ALERT_PARAGRAPH = '... alertParagraph';
-const TABLE = '... table';
-const DOWNLOADABLE_FILE_PARAGRAPH = '... downloadableFile';
-const MEDIA_PARAGRAPH = '... embeddedImage';
-const NUMBER_CALLOUT = '... numberCallout';
+const BUTTON = '... button';
 
 const fragment = `
 fragment nodeQa on NodeQA {
@@ -22,17 +13,12 @@ fragment nodeQa on NodeQA {
       entityType
       entityBundle
       ${WYSIWYG}
-      ${COLLAPSIBLE_PANEL}
-      ${PROCESS}
-      ${QA_SECTION}
-      ${LIST_OF_LINK_TEASERS}
-      ${REACT_WIDGET}
-      ${SPANISH_SUMMARY}
-      ${TABLE}
       ${ALERT_PARAGRAPH}
-      ${DOWNLOADABLE_FILE_PARAGRAPH}
-      ${MEDIA_PARAGRAPH}
-      ${NUMBER_CALLOUT}
+    }
+  }
+  fieldButtons {
+    entity {
+      ${BUTTON}
     }
   }
 }
