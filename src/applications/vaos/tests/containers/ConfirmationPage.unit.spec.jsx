ebe44b709d60721f888fd4fceb9ecbf998daf979
Tony Williams <tony.williams@va.gov>
2020-09-09 14:50:29 -0500

    Modified to use RTL

--- src/applications/vaos/tests/containers/ConfirmationPage.unit.spec.jsx
+++ src/applications/vaos/tests/containers/ConfirmationPage.unit.spec.jsx
@@ -1,138 +1,182 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { FLOW_TYPES } from '../../utils/constants';
+import { fireEvent } from '@testing-library/react';
+import { renderWithStoreAndRouter } from '../mocks/setup';
+import reducers from '../../redux/reducer';
+import moment from 'moment';
 
 import { ConfirmationPage } from '../../containers/ConfirmationPage';
 
+const start = moment.now();
+const end = start;
+
+const initialState = {
+  featureToggles: {
+    vaOnlineSchedulingCancel: true,
+    // eslint-disable-next-line camelcase
+    show_new_schedule_view_appointments_page: true,
+  },
+};
+
+const closeConfirmationPage = sinon.spy();
+const fetchFacilityDetails = sinon.spy();
+const startNewAppointmentFlow = sinon.spy();
+
+// Fixes error:
+// Unable to find an element with the text: Hello world.
+// This could be because the text is broken up by multiple elements.
+// In this case, you can provide a function for your text
+// matcher to make your matcher more flexible.
+// see https://www.polvara.me/posts/five-things-you-didnt-know-about-testing-library/
+const withHtmlTags = query => {
+  return (content, node) => {
+    const hasText = n => {
+      return n.textContent === query;
+    };
+    const nodeHasText = hasText(node);
+    const childrenDontHaveText = Array.from(node.children).every(
+      child => !hasText(child),
+    );
+    return nodeHasText && childrenDontHaveText;
+  };
+};
+
 describe('VAOS <ConfirmationPage>', () => {
-  it('should render direct schedule view', () => {
+  it('should render appointment direct schedule view', async () => {
     const flowType = FLOW_TYPES.DIRECT;
     const data = {
       typeOfCareId: '323',
       vaFacility: '983',
     };
-    const fetchFacilityDetails = sinon.spy();
 
-    const tree = shallow(
+    const screen = renderWithStoreAndRouter(
       <ConfirmationPage
         fetchFacilityDetails={fetchFacilityDetails}
         flowType={flowType}
         data={data}
+        slot={{ start, end }}
+        systemId="578"
       />,
+      {
+        initialState,
+        reducers,
+      },
     );
 
-    expect(tree.find('ConfirmationDirectScheduleInfo').exists()).to.be.true;
-    expect(document.title).contain('Your appointment has been scheduled');
-    expect(fetchFacilityDetails.called).to.be.true;
-
-    tree.unmount();
+    await screen.findAllByText(/Your appointment has been scheduled/i);
+    await screen.findByText(
+      withHtmlTags(moment().format('MMMM D, YYYY [at] h:mm a CT')),
+    );
   });
 
-  it('should render request view', () => {
+  it('should render appointment request view', async () => {
     const flowType = FLOW_TYPES.REQUEST;
     const data = {
       typeOfCareId: '323',
       vaFacility: '983',
     };
-    const fetchFacilityDetails = sinon.spy();
 
-    const tree = shallow(
+    const screen = renderWithStoreAndRouter(
       <ConfirmationPage
         fetchFacilityDetails={fetchFacilityDetails}
         flowType={flowType}
         data={data}
       />,
+      {
+        initialState,
+        reducers,
+      },
     );
 
-    expect(tree.find('ConfirmationRequestInfo').exists()).to.be.true;
-    expect(fetchFacilityDetails.called).to.be.true;
-    expect(document.title).contain(
-      'Your appointment request has been submitted',
+    await screen.findByText(
+      withHtmlTags('Your appointment request has been submitted'),
     );
-
-    tree.unmount();
   });
 
-  it('should call startNewAppointmentFlow when new appointment button clicked', () => {
+  it('should render new appointment page when "New appointment" button is clicked', () => {
     const flowType = FLOW_TYPES.REQUEST;
-    const startNewAppointmentFlow = sinon.spy();
     const data = {
       typeOfCareId: '323',
       vaFacility: '983',
     };
-    const fetchFacilityDetails = sinon.spy();
 
-    const tree = shallow(
+    const screen = renderWithStoreAndRouter(
       <ConfirmationPage
         fetchFacilityDetails={fetchFacilityDetails}
         startNewAppointmentFlow={startNewAppointmentFlow}
         flowType={flowType}
         data={data}
       />,
+      {
+        initialState,
+        reducers,
+      },
     );
 
-    tree
-      .find('Link')
-      .at(1)
-      .props()
-      .onClick();
-    expect(startNewAppointmentFlow.called).to.be.true;
+    // Simulate user clicking button
+    const button = screen.getByText(/New appointment/i);
+    fireEvent.click(button);
 
-    tree.unmount();
+    // Expect router to route to new appointment page
+    expect(screen.history.push.called).to.be.true;
+    expect(screen.history.push.getCall(0).args[0]).to.equal('new-appointment');
   });
 
-  it('should render view/schedule appointment buttons and fire GA event on click', () => {
+  it('should render appointment list page when "View your appointments" button is clicked', () => {
     const flowType = FLOW_TYPES.DIRECT;
-    const closeConfirmationPage = sinon.spy();
     const data = {
       typeOfCareId: '323',
       vaFacility: '983',
     };
-    const fetchFacilityDetails = sinon.spy();
-    const startNewAppointmentFlow = sinon.spy();
 
-    const tree = shallow(
+    const screen = renderWithStoreAndRouter(
       <ConfirmationPage
         fetchFacilityDetails={fetchFacilityDetails}
         closeConfirmationPage={closeConfirmationPage}
         flowType={flowType}
         data={data}
         startNewAppointmentFlow={startNewAppointmentFlow}
+        slot={{ start, end }}
       />,
+      {
+        initialState,
+        reducers,
+      },
     );
 
-    const LinkButtons = tree.find('Link');
-    expect(LinkButtons.length).to.equal(2);
-    LinkButtons.at(1).simulate('click');
-    expect(global.window.dataLayer[0].event).to.equal(
-      'vaos-schedule-another-appointment-button-clicked',
-    );
-    tree.unmount();
+    // Simulate user clicking button
+    const button = screen.getByText(/View your appointments/i);
+    fireEvent.click(button);
+
+    // Expect router to route to new appointment page
+    expect(screen.history.push.called).to.be.true;
+    expect(screen.history.push.getCall(0).args[0]).to.equal('/');
   });
 
-  it('should redirect if no form data', () => {
+  it('should redirect to new appointment page if no form data', () => {
     const flowType = FLOW_TYPES.REQUEST;
     const data = {};
-    const fetchFacilityDetails = sinon.spy();
     const history = {
       replace: sinon.spy(),
     };
 
-    const tree = shallow(
+    renderWithStoreAndRouter(
       <ConfirmationPage
         fetchFacilityDetails={fetchFacilityDetails}
         flowType={flowType}
         data={data}
         history={history}
       />,
+      {
+        initialState,
+        reducers,
+      },
     );
 
-    expect(tree.find('ConfirmationRequestInfo').exists()).to.be.true;
-    expect(fetchFacilityDetails.called).to.be.false;
+    // Expect router to route to new appointment page
     expect(history.replace.called).to.be.true;
-
-    tree.unmount();
+    expect(history.replace.getCall(0).args[0]).to.equal('/new-appointment');
   });
 });
