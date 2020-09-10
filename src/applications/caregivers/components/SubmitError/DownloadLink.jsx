fc0ad3538eb47350f405e37f2bdc810211b50520
LinnJS <justinslinn727@gmail.com>
2020-09-10 19:17:23 -0400

    Add loading indicator

--- src/applications/caregivers/components/SubmitError/DownloadLink.jsx
+++ src/applications/caregivers/components/SubmitError/DownloadLink.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import * as Sentry from '@sentry/browser';
 import moment from 'moment';
+import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
 
 import formConfig from 'applications/caregivers/config/form';
 import environment from 'platform/utilities/environment';
@@ -12,12 +13,14 @@ import { createFormPageList } from 'platform/forms-system/src/js/helpers';
 
 const DownLoadLink = ({ form }) => {
   const [PDFLink, setPDFLink] = useState(null);
+  const [isLoading, setLoading] = useState(false);
   const formData = submitTransform(formConfig, form);
   const veteranFullName = form.data.veteranFullName;
   const pageList = createFormPageList(formConfig);
   const isFormValid = isValidForm(form, pageList);
 
   const downloadPDF = () => {
+    setLoading(true);
     apiRequest(
       `${environment.API_URL}/v0/caregivers_assistance_claims/download_pdf`,
       {
@@ -33,39 +36,53 @@ const DownLoadLink = ({ form }) => {
       .then(blob => {
         const url = URL.createObjectURL(blob);
         setPDFLink(url);
+        setLoading(false);
         recordEvent({ event: 'caregivers-10-10cg-pdf-download--success' });
       })
       .catch(error => {
+        setLoading(false);
         Sentry.withScope(scope => scope.setExtra('error', error));
         recordEvent({ event: 'caregivers-10-10cg-pdf--failure' });
       });
   };
 
-  return (
-    <div className="vads-u-margin-top--2">
-      <a
-        onClick={() => isFormValid.isValid && downloadPDF()}
-        aria-label="Download 1010CG filled out PDF form"
-        href={PDFLink}
-        download={`10-10CG_${veteranFullName.first}_${veteranFullName.last}`}
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        <i
-          aria-hidden="true"
-          role="img"
-          className="fas fa-download vads-u-padding-right--1"
-        />
-        Download your completed application
-        <span className="sr-only vads-u-margin-right--0p5">
-          `dated ${moment(Date.now()).format('MMM D, YYYY')}`
-        </span>
-        <dfn>
-          <abbr title="Portable Document Format">(PDF)</abbr>
-        </dfn>
-      </a>
-    </div>
-  );
+  const renderPDFLink = () => {
+    return (
+      <div className="pdf-download-link--loaded vads-u-margin-top--2">
+        <a
+          onClick={() => isFormValid.isValid && downloadPDF()}
+          aria-label="Download 1010CG filled out PDF form"
+          href={PDFLink}
+          download={`10-10CG_${veteranFullName.first}_${veteranFullName.last}`}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <i
+            aria-hidden="true"
+            role="img"
+            className="fas fa-download vads-u-padding-right--1"
+          />
+          Download your completed application
+          <span className="sr-only vads-u-margin-right--0p5">
+            `dated ${moment(Date.now()).format('MMM D, YYYY')}`
+          </span>
+          <dfn>
+            <abbr title="Portable Document Format">(PDF)</abbr>
+          </dfn>
+        </a>
+      </div>
+    );
+  };
+
+  const renderLoadingIndicator = () => {
+    return (
+      <div className="pdf-download-link--loading">
+        <LoadingIndicator message="Downloading PDF..." />
+      </div>
+    );
+  };
+
+  return isLoading ? renderLoadingIndicator() : renderPDFLink();
 };
 
 export default DownLoadLink;
