afe390cf79df8c7ad234b8a7f095facaebde0365
Brooks Johnson <brooks.johnson@pm.me>
2020-09-10 11:07:00 -0700

    Remove performance optimization
    The React.memo wasjust implementing what shouldComponentUpdate was doing
    previously, but this is causing problems with some unit tests. Removing
    it for now - maybe we'll add it back in later

--- src/platform/forms-system/src/js/components/FormNav.jsx
+++ src/platform/forms-system/src/js/components/FormNav.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import _ from 'lodash/fp'; // eslint-disable-line no-restricted-imports
-import shallowEqual from 'recompose/shallowEqual';
 
 import SegmentedProgressBar from './SegmentedProgressBar';
 
@@ -13,69 +12,63 @@ import {
 import PropTypes from 'prop-types';
 import { REVIEW_APP_DEFAULT_MESSAGE } from '../constants';
 
-const FormNav = React.memo(
-  props => {
-    const { formConfig, currentPath, formData } = props;
+export default function FormNav(props) {
+  const { formConfig, currentPath, formData } = props;
 
-    // This is converting the config into a list of pages with chapter keys,
-    // finding the current page, then getting the chapter name using the key
-    const formPages = createFormPageList(formConfig);
-    const pageList = createPageList(formConfig, formPages);
+  // This is converting the config into a list of pages with chapter keys,
+  // finding the current page, then getting the chapter name using the key
+  const formPages = createFormPageList(formConfig);
+  const pageList = createPageList(formConfig, formPages);
 
-    const eligiblePageList = getActiveExpandedPages(pageList, formData);
+  const eligiblePageList = getActiveExpandedPages(pageList, formData);
 
-    const chapters = _.uniq(
-      eligiblePageList.map(p => p.chapterKey).filter(key => !!key),
+  const chapters = _.uniq(
+    eligiblePageList.map(p => p.chapterKey).filter(key => !!key),
+  );
+
+  let page = eligiblePageList.filter(p => p.path === currentPath)[0];
+  // If the page isn’t active, it won’t be in the eligiblePageList
+  // This is a fallback to still find the chapter name if you open the page directly
+  // (the chapter index will probably be wrong, but this isn’t a scenario that happens in normal use)
+  if (!page) {
+    page = formPages.find(
+      p => `${formConfig.urlPrefix}${p.path}` === currentPath,
     );
+  }
 
-    let page = eligiblePageList.filter(p => p.path === currentPath)[0];
-    // If the page isn’t active, it won’t be in the eligiblePageList
-    // This is a fallback to still find the chapter name if you open the page directly
-    // (the chapter index will probably be wrong, but this isn’t a scenario that happens in normal use)
-    if (!page) {
-      page = formPages.find(
-        p => `${formConfig.urlPrefix}${p.path}` === currentPath,
-      );
+  let current;
+  let chapterName;
+  if (page) {
+    current = chapters.indexOf(page.chapterKey) + 1;
+    // The review page is always part of our forms, but isn’t listed in chapter list
+    chapterName =
+      page.chapterKey === 'review'
+        ? formConfig?.customText?.reviewPageTitle || REVIEW_APP_DEFAULT_MESSAGE
+        : formConfig.chapters[page.chapterKey].title;
+    if (typeof chapterName === 'function') {
+      chapterName = chapterName();
     }
+  }
 
-    let current;
-    let chapterName;
-    if (page) {
-      current = chapters.indexOf(page.chapterKey) + 1;
-      // The review page is always part of our forms, but isn’t listed in chapter list
-      chapterName =
-        page.chapterKey === 'review'
-          ? formConfig?.customText?.reviewPageTitle ||
-            REVIEW_APP_DEFAULT_MESSAGE
-          : formConfig.chapters[page.chapterKey].title;
-      if (typeof chapterName === 'function') {
-        chapterName = chapterName();
-      }
-    }
+  const stepText = `Step ${current} of ${chapters.length}: ${chapterName}`;
 
-    const stepText = `Step ${current} of ${chapters.length}: ${chapterName}`;
-
-    return (
-      <div>
-        <SegmentedProgressBar total={chapters.length} current={current} />
-        <div className="schemaform-chapter-progress">
-          <div
-            aria-valuenow={current}
-            aria-valuemin="1"
-            aria-valuetext={stepText}
-            aria-valuemax={chapters.length}
-            className="nav-header nav-header-schemaform"
-          >
-            <h2 className="vads-u-font-size--h4">{stepText}</h2>
-          </div>
+  return (
+    <div>
+      <SegmentedProgressBar total={chapters.length} current={current} />
+      <div className="schemaform-chapter-progress">
+        <div
+          aria-valuenow={current}
+          aria-valuemin="1"
+          aria-valuetext={stepText}
+          aria-valuemax={chapters.length}
+          className="nav-header nav-header-schemaform"
+        >
+          <h2 className="vads-u-font-size--h4">{stepText}</h2>
         </div>
       </div>
-    );
-  },
-  (prevProps, nextProps) => !shallowEqual(prevProps, nextProps),
-);
-
-export default FormNav;
+    </div>
+  );
+}
 
 FormNav.defaultProps = {
   formConfig: {
