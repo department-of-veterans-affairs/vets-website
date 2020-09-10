2bdce4a37bfcb8f5ed525d9ce48fcb884c9cb5ab
Tony Williams <tony.williams@va.gov>
2020-09-10 12:09:49 -0500

    Modified to use RTL

--- src/applications/vaos/tests/new-appointment/components/ConfirmationPage/index.unit.spec.jsx
+++ src/applications/vaos/tests/new-appointment/components/ConfirmationPage/index.unit.spec.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { expect } from 'chai';
-import sinon from 'sinon';
-import { FLOW_TYPES } from '../../../../utils/constants';
-import { fireEvent } from '@testing-library/react';
-import { renderWithStoreAndRouter } from '../../../mocks/setup';
-import reducers from '../../../../redux/reducer';
 import moment from 'moment';
-
+import React from 'react';
+import sinon from 'sinon';
 import { ConfirmationPage } from '../../../../new-appointment/components/ConfirmationPage';
+import reducers from '../../../../redux/reducer';
+import { FLOW_TYPES } from '../../../../utils/constants';
+import { renderWithStoreAndRouter } from '../../../mocks/setup';
 
 const start = moment.now();
 const end = start;
@@ -23,25 +22,6 @@ const initialState = {
 const closeConfirmationPage = sinon.spy();
 const fetchFacilityDetails = sinon.spy();
 const startNewAppointmentFlow = sinon.spy();
-
-// Fixes error:
-// Unable to find an element with the text: Hello world.
-// This could be because the text is broken up by multiple elements.
-// In this case, you can provide a function for your text
-// matcher to make your matcher more flexible.
-// see https://www.polvara.me/posts/five-things-you-didnt-know-about-testing-library/
-const withHtmlTags = query => {
-  return (content, node) => {
-    const hasText = n => {
-      return n.textContent === query;
-    };
-    const nodeHasText = hasText(node);
-    const childrenDontHaveText = Array.from(node.children).every(
-      child => !hasText(child),
-    );
-    return nodeHasText && childrenDontHaveText;
-  };
-};
 
 describe('VAOS <ConfirmationPage>', () => {
   it('should render appointment direct schedule view', async () => {
@@ -65,10 +45,8 @@ describe('VAOS <ConfirmationPage>', () => {
       },
     );
 
-    await screen.findAllByText(/Your appointment has been scheduled/i);
-    await screen.findByText(
-      withHtmlTags(moment().format('MMMM D, YYYY [at] h:mm a CT')),
-    );
+    screen.getByText(/Your appointment has been scheduled./i);
+    screen.getByText(moment().format('MMMM D, YYYY [at] h:mm a CT'));
   });
 
   it('should render appointment request view', async () => {
@@ -90,9 +68,7 @@ describe('VAOS <ConfirmationPage>', () => {
       },
     );
 
-    await screen.findByText(
-      withHtmlTags('Your appointment request has been submitted'),
-    );
+    screen.getByText(/Your appointment request has been submitted./i);
   });
 
   it('should render new appointment page when "New appointment" button is clicked', () => {
@@ -117,7 +93,7 @@ describe('VAOS <ConfirmationPage>', () => {
 
     // Simulate user clicking button
     const button = screen.getByText(/New appointment/i);
-    fireEvent.click(button);
+    userEvent.click(button);
 
     // Expect router to route to new appointment page
     expect(screen.history.push.called).to.be.true;
@@ -148,7 +124,7 @@ describe('VAOS <ConfirmationPage>', () => {
 
     // Simulate user clicking button
     const button = screen.getByText(/View your appointments/i);
-    fireEvent.click(button);
+    userEvent.click(button);
 
     // Expect router to route to new appointment page
     expect(screen.history.push.called).to.be.true;
