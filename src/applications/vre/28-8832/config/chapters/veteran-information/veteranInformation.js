1f51e44706bdf7edc808b3e60560a2b19f3507f8
Micah Chiang <micahkchiang@gmail.com>
2020-09-11 10:37:09 -0400

    13172 pcpg schema properties (#14172)
    * bring in schema from vets-json-schema
    
    * use schema properties from vets-json-schema
    
    * Update forms.unit.spec.js
    
    * update commit hash, update tests

--- src/applications/vre/28-8832/config/chapters/veteran-information/veteranInformation.js
+++ src/applications/vre/28-8832/config/chapters/veteran-information/veteranInformation.js
@@ -1,67 +1,27 @@
 import ssnUI from 'platform/forms-system/src/js/definitions/ssn';
+import { veteranInformation } from '../../utilities';
 
-export const schema = {
-  type: 'object',
-  properties: {
-    veteranInformation: {
-      type: 'object',
-      properties: {
-        fullName: {
-          type: 'object',
-          properties: {
-            first: {
-              type: 'string',
-            },
-            middle: {
-              type: 'string',
-            },
-            last: {
-              type: 'string',
-            },
-            suffix: {
-              type: 'string',
-              enum: ['Jr.', 'Sr.', 'II', 'III', 'IV'],
-            },
-          },
-        },
-        vaFileNumber: {
-          type: 'string',
-        },
-        ssn: {
-          type: 'string',
-        },
-      },
-    },
-  },
-};
+export const schema = veteranInformation;
 
 export const uiSchema = {
-  veteranInformation: {
-    fullName: {
-      first: {
-        'ui:title': 'First Name',
-        'ui:required': () => true,
-      },
-      middle: {
-        'ui:title': 'Middle Name',
-      },
-      last: {
-        'ui:title': 'Last Name',
-        'ui:required': () => true,
-      },
-      suffix: {
-        'ui:title': 'Suffix',
-        'ui:options': {
-          widgetClassNames: 'usa-input-medium',
-        },
-      },
+  fullName: {
+    first: {
+      'ui:title': 'First Name',
+      'ui:required': () => true,
     },
-    vaFileNumber: {
-      'ui:title': 'Your VA file number',
+    middle: {
+      'ui:title': 'Middle Name',
+    },
+    last: {
+      'ui:title': 'Last Name',
+      'ui:required': () => true,
+    },
+    suffix: {
+      'ui:title': 'Suffix',
       'ui:options': {
         widgetClassNames: 'usa-input-medium',
       },
     },
-    ssn: { ...ssnUI, 'ui:required': () => true },
   },
+  ssn: { ...ssnUI, 'ui:required': () => true },
 };
