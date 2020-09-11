1887d2e20a48bd1152d498e2680d59e3607eb610
Kelson Adams <kadams@governmentcio.com>
2020-09-11 10:08:12 -0600

    Update step by step template

--- src/site/stages/build/drupal/graphql/nodeStepByStep.graphql.js
+++ src/site/stages/build/drupal/graphql/nodeStepByStep.graphql.js
@@ -1,38 +1,53 @@
 const entityElementsFromPages = require('./entityElementsForPages.graphql');
 
-const ALERT_PARAGRAPH = '... alertParagraph';
-const COLLAPSIBLE_PANEL = '... collapsiblePanel';
-const DOWNLOADABLE_FILE_PARAGRAPH = '... downloadableFile';
-const LIST_OF_LINK_TEASERS = '... listOfLinkTeasers';
-const MEDIA_PARAGRAPH = '... embeddedImage';
-const NUMBER_CALLOUT = '... numberCallout';
-const PROCESS = '... process';
-const QA_SECTION = '... qaSection';
-const REACT_WIDGET = '... reactWidget';
-const SPANISH_SUMMARY = '... spanishSummary';
-const TABLE = '... table';
-const WYSIWYG = '... wysiwyg';
-
 const fragment = `
 fragment nodeStepByStep on NodeStepByStep {
   ${entityElementsFromPages}
   entityBundle
+
+  title
+  fieldIntroTextLimitedHtml {
+    processed
+  }
+  fieldAlertSingle {
+    entity {
+      ... alertSingle
+    }
+  }
+  fieldButtons {
+    entity {
+      ... button
+    }
+  }
   fieldSteps {
     entity {
-      entityBundle
-      entityType
-      ${ALERT_PARAGRAPH}
-      ${COLLAPSIBLE_PANEL}
-      ${DOWNLOADABLE_FILE_PARAGRAPH}
-      ${LIST_OF_LINK_TEASERS}
-      ${MEDIA_PARAGRAPH}
-      ${NUMBER_CALLOUT}
-      ${PROCESS}
-      ${QA_SECTION}
-      ${REACT_WIDGET}
-      ${SPANISH_SUMMARY}
-      ${TABLE}
-      ${WYSIWYG}
+      ... on ParagraphStepByStep {
+        entityLabel
+        entityId
+        entityBundle
+        fieldStep {
+          entity {
+            ... on ParagraphStep {
+              entityId
+              entityLabel
+              entityBundle
+              fieldWysiwyg {
+                processed
+              }
+              fieldMedia {
+                entity {
+                  ... on Media {
+                    entityLabel
+                    thumbnail {
+                      url
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
