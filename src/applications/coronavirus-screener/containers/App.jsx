d4c6b4a78838d4a098e2b0ab62b5f2b9197506bd
Michael Chelen <michael.chelen@va.gov>
2020-09-14 16:01:32 -0400

    minor

--- src/applications/coronavirus-screener/containers/App.jsx
+++ src/applications/coronavirus-screener/containers/App.jsx
@@ -3,21 +3,27 @@ import MetaTags from 'react-meta-tags';
 import MultiQuestionForm from '../components/MultiQuestionForm';
 import { questions, defaultOptions } from '../config/questions';
 
+const IntroductionText = (
+  <>
+    <h1>COVID-19 screening tool</h1>
+    <div className="va-introtext">
+      <p>
+        Please answer the questions listed below. Share your results with the
+        staff member at the facility entrance.
+      </p>
+      <p>We won't store or share your data.</p>
+    </div>
+  </>
+);
+
 export default function App({ params }) {
   return (
     <div className="covid-screener">
       <MetaTags>
         <meta name="robots" content="noindex" />
       </MetaTags>
+      <IntroductionText />
       <div className="vads-l-grid-container">
-        <h1>COVID-19 screening tool</h1>
-        <div className="va-introtext">
-          <p>
-            Please answer the questions listed below. Share your results with
-            the staff member at the facility entrance.
-          </p>
-          <p>We won't store or share your data.</p>
-        </div>
         <MultiQuestionForm
           questions={questions}
           defaultOptions={defaultOptions}
