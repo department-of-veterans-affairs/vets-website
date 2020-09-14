3a196d1e9947bb7367b1836e390c6b9fbe5d0398
Devin McCurdy <mccurdy_devin@bah.com>
2020-09-14 10:43:55 -0400

    Cleanup

--- src/applications/gi/containers/ProfilePage.jsx
+++ src/applications/gi/containers/ProfilePage.jsx
@@ -34,7 +34,6 @@ export function ProfilePage({
   const institutionName = _.get(profile, 'attributes.name');
 
   useEffect(() => {
-    // dispatchFetchProfile(facilityCode, version);
     return () => {
       dispatchHideModal();
     };
