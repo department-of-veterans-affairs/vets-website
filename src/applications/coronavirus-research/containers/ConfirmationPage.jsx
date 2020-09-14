bcf0a24cb987dd39ee9396623f9dc79059bc9a02
Danielle Thierry <62957278+DanielleThierryUSDSVA@users.noreply.github.com>
2020-09-14 19:05:00 -0400

    copy & formatting tweaks

--- src/applications/coronavirus-research/containers/ConfirmationPage.jsx
+++ src/applications/coronavirus-research/containers/ConfirmationPage.jsx
@@ -26,8 +26,7 @@ export class ConfirmationPage extends React.Component {
           Thank you for signing up for our coronavirus research volunteer list
         </h2>
         <p>
-          We appreciate your commitment to helping others through this important
-          research effort.
+          We appreciate your commitment to helping others through this important effort.
         </p>
         <div>
           <h2>
@@ -66,10 +65,11 @@ export class ConfirmationPage extends React.Component {
             If you have questions or would like to be removed from the volunteer list
           </h2>
           <p>
-            If you have questions about our coronavirus research volunteer list, call us at <a href="tel:+18664416075" aria-label="8 6 6. 4 4 1. 6 0 7 5.">866-441-6075</a>, select 1. We're here Monday through Friday, 8:00 a.m. to 6:00 p.m.           
+            If you have questions, call us at <a href="tel:+18664416075" aria-label="8 6 6. 4 4 1. 6 0 7 5.">866-441-6075</a>, select 1. We're here Monday through Friday, 8:00 a.m. to 6:00 p.m.           
           </p>
           <p>
             You can decide to stop being part of this list at anytime. If you'd like to be removed from the list, send us an email at <a href="mailto:research@va.gov">research@va.gov</a>.
+          </p>  
         </div>
       </div>
       
