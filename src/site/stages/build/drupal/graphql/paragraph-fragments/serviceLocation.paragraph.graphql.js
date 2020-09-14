21298ec0352b6a585c9203ca5d12301b6f32f332
Ethan Teague <metrowebz@gmail.com>
2020-09-14 18:29:25 -0400

    VACMS-2722: Add new facility service model queries.

---
+++ src/site/stages/build/drupal/graphql/paragraph-fragments/serviceLocation.paragraph.graphql.js
@@ -0,0 +1,56 @@
+/*
+*
+* A service location for a facility service.
+*
+ */
+module.exports = `
+  fieldServiceLocation {
+    entity {
+      ... on ParagraphServiceLocation {
+        fieldServiceLocationAddress {
+          entity {
+            ... on ParagraphServiceLocationAddress {
+              fieldAddress {
+                addressLine1
+                addressLine2
+                additionalName
+                administrativeArea
+                postalCode
+                locality
+                organization
+                dependentLocality
+                countryCode
+                sortingCode
+              }
+            }
+          }
+        }
+        fieldEmailContacts {
+          entity {
+            ... on ParagraphEmailContact {
+              fieldEmailAddress
+              fieldEmailLabel
+            }
+          }
+        }
+        fieldFacilityServiceHours {
+          value
+          caption
+          format
+        }
+        fieldHours
+        fieldPhone {
+          entity {
+            ... on ParagraphPhoneNumber {
+              fieldPhoneExtension
+              fieldPhoneLabel
+              fieldPhoneNumber
+              fieldPhoneNumberType
+            }
+          }
+        }
+        fieldUseMainFacilityPhone
+      }
+    }
+  }
+`;
