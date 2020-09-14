136d7d86ec271fe62272b6dfeef7c68393f2ec65
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2020-09-14 16:52:42 -0400

    Add pact tests for facility search

--- src/applications/facility-locator/api/index.js
+++ src/applications/facility-locator/api/index.js
@@ -4,7 +4,13 @@ import environment from 'platform/utilities/environment';
 
 // To use vets-api data locally, replace the following with this:
 // export default LiveApi;
-export default ((environment.isLocalhost() || window.Cypress) &&
-!environment.API_URL.includes('review.vetsgov')
-  ? MockApi
-  : LiveApi);
+
+const getAPI = () => {
+  return !window.Mocha &&
+    (environment.isLocalhost() || window.Cypress) &&
+    !environment.API_URL.includes('review.vetsgov')
+    ? MockApi
+    : LiveApi;
+};
+
+export default getAPI();
