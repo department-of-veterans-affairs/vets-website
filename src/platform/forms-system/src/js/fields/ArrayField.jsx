e22be0fd03fd52d82563baec210edafa10ab0a10
Mark Dewey <mdewey@governmentcio.com>
2020-09-15 15:47:18 -0400

    removed a note

--- src/platform/forms-system/src/js/fields/ArrayField.jsx
+++ src/platform/forms-system/src/js/fields/ArrayField.jsx
@@ -105,7 +105,6 @@ export default class ArrayField extends React.Component {
   }
 
   scrollToRow(id) {
-    // TODO: Test
     if (!this.props.uiSchema['ui:options'].doNotScrollOnAdd) {
       setTimeout(() => {
         scroller.scrollTo(
