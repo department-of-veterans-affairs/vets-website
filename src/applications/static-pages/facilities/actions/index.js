ea58c62cc1b1882c00af2b0b2ab23c1590fbc1ee
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2020-09-15 18:36:59 -0400

    Fix for facility wait time infinite spinner
    https://github.com/department-of-veterans-affairs/va.gov-team/issues/13585

--- src/applications/static-pages/facilities/actions/index.js
+++ src/applications/static-pages/facilities/actions/index.js
@@ -37,7 +37,7 @@ export function fetchFacility(id) {
     dispatch(fetchFacilityStarted());
 
     // eslint-disable-next-line consistent-return
-    return apiRequest(`/facilities/va/${id}`)
+    return apiRequest(`/facilities/va/${id}`, {}, null, null, 'v1')
       .then(facility => dispatch(fetchFacilitySuccess(facility.data)))
       .catch(() => dispatch(fetchFacilityFailed()));
   };
