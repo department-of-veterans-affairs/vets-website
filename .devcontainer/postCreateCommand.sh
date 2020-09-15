750cc66126f5b6da7c012f0a7f694b1f090c5ce4
Michael Chelen <michael.chelen@va.gov>
2020-09-15 14:21:30 -0400

    disable nvm

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # configure NVM
-/usr/local/share/nvm install || true # ignore exit code due to npm prefix
-/usr/local/share/nvm use --delete-prefix
+# /usr/local/share/nvm install || true # ignore exit code due to npm prefix
+# nvm use --delete-prefix
 yarn install
