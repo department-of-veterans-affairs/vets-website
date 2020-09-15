dd1f5fbefb2c39ac55cd22aee999cfa5083a7e87
Micah Chiang <micahkchiang@gmail.com>
2020-09-15 14:46:02 -0400

    update content on flipper component

--- src/applications/static-pages/vre-chapter36/Chapter36CTA.js
+++ src/applications/static-pages/vre-chapter36/Chapter36CTA.js
@@ -15,12 +15,34 @@ const Chapter36CTA = props => {
     content = <LoadingIndicator message="Loading..." />;
   } else if (props.includedInFlipper === false) {
     content = (
-      <EbenefitsLink
-        path="ebenefits/payments"
-        className="usa-button-primary va-button-primary"
-      >
-        Go to eBenefits to view your VA payment history
-      </EbenefitsLink>
+      <>
+        <div className="process schemaform-process">
+          <ol>
+            <li className="process-step list-one">
+              Sign in to your eBenefits account.
+            </li>
+            <li className="process-step list-two">
+              Select <strong>Apply</strong>.
+            </li>
+            <li className="process-step list-three">
+              Click <strong>Veteran Readiness and Employment Program</strong>.
+            </li>
+            <li className="process-step list-four">
+              Apply <strong>for Education and Career Counseling</strong>.
+            </li>
+            <li className="process-step list-five">
+              If you're eligible, we'll invite you to an orientation session at
+              your nearest VA regional office.
+            </li>
+          </ol>
+        </div>
+        <EbenefitsLink
+          path="ebenefits/payments"
+          className="usa-button-primary va-button-primary"
+        >
+          Go to eBenefits to apply
+        </EbenefitsLink>
+      </>
     );
   } else {
     content = <CallToActionWidget appId="chapter-36-cta" />;
