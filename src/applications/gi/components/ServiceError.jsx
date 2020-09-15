ba5b9a00c0e47fb277f0bf10cdee853e971cba5c
Matt Roth <roth_matthew@bah.com>
2020-09-15 10:35:11 -0400

    profile error axe check

--- src/applications/gi/components/ServiceError.jsx
+++ src/applications/gi/components/ServiceError.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export const ServiceError = () => (
-  <div id="fail" className="backenderror">
+  <div className="backenderror">
     <h3 className="backend-error-bold"> We’ve run into a problem. </h3>
 
-    <div id="fail" className="backend-error vads-u-padding-bottom--5">
+    <div className="backend-error vads-u-padding-bottom--5">
       We're sorry. Something went wrong on our end. Please refresh this page or
       check back soon.
     </div>
--- src/applications/gi/tests/e2e/cypress-helpers.js
+++ src/applications/gi/tests/e2e/cypress-helpers.js
@@ -1,5 +1,44 @@
+const calculatorConstantsJson = require('../data/calculator-constants.json');
+const autocomplete = require('../data/autocomplete.json');
+const institutionProfile = require('../data/institution-profile.json');
+const searchResults = require('../data/search-results.json');
+
 Cypress.on('uncaught:exception', (_err, _runnable) => {
   // returning false here prevents Cypress from
   // failing the test
   return false;
 });
+
+// Default responses.
+beforeEach(() => {
+  cy.route('GET', '/v0/gi/calculator_constants', {
+    data: calculatorConstantsJson,
+  });
+});
+
+/**
+ * Mocks the call for the profile
+ * @param profile
+ */
+export const initMockProfile = profile => {
+  const facilityCode = profile.data.attributes.facility_code;
+  cy.route('GET', `/v0/gi/institutions/${facilityCode}`, {
+    data: profile,
+  });
+};
+
+// Create API routes
+export const initApplicationMock = (
+  profile = institutionProfile,
+  results = searchResults,
+) => {
+  cy.route('GET', '/v0/gi/institutions/autocomplete', {
+    data: autocomplete,
+  });
+
+  cy.route('GET', '/v0/gi/institutions/search', {
+    data: results,
+  });
+
+  initMockProfile(profile);
+};
