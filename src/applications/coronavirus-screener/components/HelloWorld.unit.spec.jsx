5015764c932d57dfa6f9d4a05b7bd8a3d5d01656
Michael Chelen <michael.chelen@va.gov>
2020-09-15 11:38:12 -0400

    try document.body.innerHTML

--- src/applications/coronavirus-screener/components/HelloWorld.unit.spec.jsx
+++ src/applications/coronavirus-screener/components/HelloWorld.unit.spec.jsx
@@ -3,6 +3,8 @@ import { render, screen } from '@testing-library/react';
 
 import App from './HelloWorld';
 
+// yarn test:unit --path src/applications/coronavirus-screener/components/HelloWorld.unit.spec.jsx
+
 describe('App', () => {
   it('renders App component', () => {
     render(<App />);
@@ -17,5 +19,25 @@ describe('App', () => {
     screen.getByText('Hello React');
     // yarn test:unit --path src/applications/coronavirus-screener/components/HelloWorld.unit.spec.jsx
     // TestingLibraryElementError: Unable to find an element with the text: Hello React.
+  });
+
+  it('gets text from exampleHTML', () => {
+    const exampleHTML = `<body>
+    <div>
+      <div>hello</div>
+    </div>
+  </body>`;
+    document.body.innerHTML = exampleHTML;
+
+    screen.debug();
+    const example = screen.getByText(/hello/i);
+  });
+
+  it('gets text from super simple exampleHTML', () => {
+    const exampleHTML = `<div>hello</div>`;
+    document.body.innerHTML = exampleHTML;
+
+    screen.debug();
+    const example = screen.getByText('hello');
   });
 });
