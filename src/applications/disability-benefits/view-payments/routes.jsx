1081d5e637fb4d023f7cd1404be47f976a5ab361
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-09-16 09:59:00 -0700

    Added required login view (#14213)
    * Added required login view
    
    * Added downtime notification
    
    * Updated app component in routes.jsx

--- src/applications/disability-benefits/view-payments/routes.jsx
+++ src/applications/disability-benefits/view-payments/routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router';
-import App from './containers/App.jsx';
+import ViewPaymentsApp from './containers/App.jsx';
 
-const routes = <Route path="/" component={App} />;
+const routes = <Route path="/" component={ViewPaymentsApp} />;
 
 export default routes;
