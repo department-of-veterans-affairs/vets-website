586b385f27cb43d752d006f45f63a7f33c05dab3
Mark Dewey <mdewey@governmentcio.com>
2020-09-16 15:46:57 -0400

    added reducers

--- src/applications/healthcare/questionnaire/containers/App.jsx
+++ src/applications/healthcare/questionnaire/containers/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from '../config/form';
 
-const App = ({ location, children }) => {
+import {
+  questionnaireAppointmentLoading,
+  questionnaireAppointmentLoaded,
+} from '../actions';
+
+const App = props => {
+  const { location, children } = props;
+  const { setLoading, setLoadedAppointment } = props;
+
+  useEffect(
+    () => {
+      setLoading();
+      setTimeout(() => {
+        setLoadedAppointment({ hello: 'world' });
+      }, 3000);
+    },
+    [setLoading, setLoadedAppointment],
+  );
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
@@ -12,8 +29,17 @@ const App = ({ location, children }) => {
   );
 };
 
-const mapStateToProps = _state => ({});
-const mapDispatchToProps = _dispatch => ({});
+const mapStateToProps = state => ({
+  showApplication: true,
+  questionnaire: state?.questionnaireData,
+});
+const mapDispatchToProps = dispatch => {
+  return {
+    setLoading: () => dispatch(questionnaireAppointmentLoading()),
+    setLoadedAppointment: value =>
+      dispatch(questionnaireAppointmentLoaded(value)),
+  };
+};
 
 export default connect(
   mapStateToProps,
