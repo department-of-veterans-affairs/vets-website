1feb350bbff37b979a76ea9a17bebb1a7c6b16ae
Micah Chiang <micahkchiang@gmail.com>
2020-09-16 14:24:44 -0400

    12000 connect view payments to backend (#14203)
    * Initial wiring for api call
    
    * handle null fields in data
    
    * Convert dates to more readable format. Update unit tests.
    
    * remove commented out code, reorder imports
    
    * adjust based off PR feedback
    
    * handle empty payment arrays
    
    * update no content notification text
    
    * Refactor and update alert design

--- src/applications/disability-benefits/view-payments/containers/App.jsx
+++ src/applications/disability-benefits/view-payments/containers/App.jsx
@@ -5,7 +5,6 @@ import backendServices from 'platform/user/profile/constants/backendServices';
 import DowntimeNotification, {
   externalServices,
 } from 'platform/monitoring/DowntimeNotification';
-import ViewPaymentsHeader from '../components/view-payments-header/ViewPaymentsHeader.jsx';
 import ViewPaymentsLists from '../components/view-payments-lists/ViewPaymentsLists.jsx';
 import ViewPaymentsSidebar from '../components/ViewPaymentsSidebar/ViewPaymentsSidebar.jsx';
 import ViewPaymentsSidebarBlock from '../components/ViewPaymentsSidebar/ViewPaymentsSidebarBlock.jsx';
@@ -29,19 +28,8 @@ function ViewPaymentsApp(props) {
           <div className="vads-l-grid-container vads-u-padding--0">
             <div className="vads-l-row">
               <div className="vads-l-col--12 medium-screen:vads-l-col--12 large-screen:vads-l-col--8 vads-u-padding--1p5 large-screen:vads-u-padding--0">
-                <ViewPaymentsHeader />
+                <h1>Your VA payments</h1>
                 <ViewPaymentsLists />
-                <p className="vads-u-font-size--lg vads-u-font-weight--bold">
-                  What if I find a check that I reported missing?
-                </p>
-                <p>
-                  If you reported a check missing and found it later, you must
-                  return the original check to the U.S. Department of the
-                  Treasury and wait to receive your replacement check. If you
-                  endorse both the original and replacement check, you'll get a
-                  double payment. If this happens, VA Debt Management Center
-                  will contact you about collection.
-                </p>
               </div>
               <div className="vads-l-col--12 medium-screen:vads-l-col--12 large-screen:vads-l-col--4">
                 <ViewPaymentsSidebar>
