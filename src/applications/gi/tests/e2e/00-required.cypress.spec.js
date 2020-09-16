4750da8e243a93136a2b6c1a80847f4091dd266c
Joe <elder_joseph@bah.com>
2020-09-16 12:16:52 -0400

    13369: Convert CT required e2e test to cypress

---
+++ src/applications/gi/tests/e2e/00-required.cypress.spec.js
@@ -0,0 +1,69 @@
+import { initMockProfile, expectLocation } from './cypress-helpers';
+import institutionProfile from '../data/institution-profile.json';
+import searchResults from '../data/search-results.json';
+import { createId } from '../../utils/helpers';
+import {
+  typeOfInstitution,
+  search,
+  selectSearchResult,
+  displayLearnMoreModal,
+  collapseExpandAccordion,
+} from './gi-helpers';
+
+const institutionAttributes = institutionProfile.data.attributes;
+
+describe('Institution', () => {
+  beforeEach(() => {
+    cy.route('/v0/gi/institutions/', searchResults);
+
+    initMockProfile(institutionProfile);
+    cy.visit('/gi-bill-comparison-tool').injectAxe();
+    cy.axeCheck();
+  });
+
+  it('Default institution profile flow with giBillChapter chapter 33', () => {
+    // Landing Page
+    typeOfInstitution('school');
+
+    cy.axeCheck();
+    cy
+      .get('.keyword-search input[type="text"]')
+      .type(institutionAttributes.name).type;
+    search();
+    expectLocation('/search');
+
+    // Search Page
+    searchResults.data.forEach(result => {
+      const resultId = `#search-result-${result.attributes.facility_code}`;
+      cy.log(resultId);
+      cy.get(createId(resultId)).should('be.visible');
+    });
+
+    const profileLink = `/profile/${
+      searchResults.data[0].attributes.facility_code
+    }`;
+
+    selectSearchResult(profileLink);
+
+    // Profile Page
+    displayLearnMoreModal();
+
+    // Estimate your benefits
+    collapseExpandAccordion('Estimate your benefits');
+
+    // Veteran programs
+    collapseExpandAccordion('Veteran programs');
+
+    // School locations
+    collapseExpandAccordion('School locations');
+
+    // Cautionary information
+    collapseExpandAccordion('Cautionary information');
+
+    // Contact details
+    collapseExpandAccordion('Contact details');
+
+    // Additional information
+    collapseExpandAccordion('Additional information');
+  });
+});
