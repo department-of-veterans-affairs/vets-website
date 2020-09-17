cbd634b7b6bf8a35ae3205112642f25d474d3d99
Michael Chelen <michael.chelen@va.gov>
2020-09-17 11:30:06 -0400

    postStartCommand not supported by codespaces https://github.com/microsoft/vscode/issues/106944

--- .devcontainer/postStartCommand.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# workaround for https://github.com/microsoft/vscode-dev-containers/issues/557
-if [[ $(which code-insiders 2>&1) && ! $(which code 2>&1) ]]; then 
-    alias code=code-insiders
-fi
