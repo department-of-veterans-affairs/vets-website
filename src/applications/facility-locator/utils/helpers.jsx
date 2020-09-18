5c2e48e5127fb0b3344433f0a64cda8db1506e9a
Gibran Rodriguez <gribranrodriguez@USRMonitorsMBP2.attlocal.net>
2020-09-18 16:56:54 -0500

    Fixes

--- src/applications/facility-locator/utils/helpers.jsx
+++ src/applications/facility-locator/utils/helpers.jsx
@@ -253,13 +253,15 @@ export const showDialogUrgCare = currentQuery => {
 export const recordMarkerEvents = r => {
   const { classification, name, facilityType } = r.attributes;
   const distance = r.distance;
-  recordEvent({ event: 'fl-map-pin-click' });
 
   if (classification && name && facilityType && distance) {
-    recordEvent({ 'fl-facility-type': facilityType });
-    recordEvent({ 'fl-facility-classification': classification });
-    recordEvent({ 'fl-facility-name': name });
-    recordEvent({ 'fl-facility-distance-from-search': distance });
+    recordEvent({
+      event: 'fl-map-pin-click',
+      'fl-facility-type': facilityType,
+      'fl-facility-classification': classification,
+      'fl-facility-name': name,
+      'fl-facility-distance-from-search': distance,
+    });
   }
 };
 
@@ -293,15 +295,17 @@ export const recordZoomPanEvents = (e, searchCoords, currentZoomLevel) => {
 export const recordResultEvents = (location, index) => {
   const { classification, name, facilityType, id } = location.attributes;
   const currentPage = location.currentPage;
-  recordEvent({ event: 'fl-results-click' });
-  recordEvent({ 'fl-result-page-number': currentPage });
-  recordEvent({ 'fl-result-position': index + 1 });
 
   if (classification && name && facilityType && id) {
-    recordEvent({ 'fl-facility-type': facilityType });
-    recordEvent({ 'fl-facility-classification': classification });
-    recordEvent({ 'fl-facility-name': name });
-    recordEvent({ 'fl-facility-id': id });
+    recordEvent({
+      event: 'fl-results-click',
+      'fl-result-page-number': currentPage,
+      'fl-result-position': index + 1,
+      'fl-facility-type': facilityType,
+      'fl-facility-classification': classification,
+      'fl-facility-name': name,
+      'fl-facility-id': id,
+    });
   }
 };
 
@@ -309,19 +313,23 @@ export const recordResultEvents = (location, index) => {
  * Helper fn to record search data layer
  */
 export const recordSearchEvents = (data, meta) => {
+  let dataPush = {};
+
   if (data) {
-    recordEvent({ event: 'fl-search-results' });
-    recordEvent({ 'fl-results-returned': !!data.length });
-  }
+    dataPush = {
+      event: 'fl-search-results',
+      'fl-results-returned': !!data.length,
+    };
 
-  if (meta.pagination.totalEntries) {
-    recordEvent({ 'fl-total-number-of-results': meta.pagination.totalEntries });
-  }
+    if (meta.pagination && meta.pagination.totalEntries) {
+      dataPush['fl-total-number-of-results'] = meta.pagination.totalEntries;
+    }
 
-  if (meta.pagination.totalPages) {
-    recordEvent({
-      'fl-total-number-of-result-pages': meta.pagination.totalPages,
-    });
+    if (meta.pagination && meta.pagination.totalPages) {
+      dataPush['fl-total-number-of-result-pages'] = meta.pagination.totalPages;
+    }
+
+    recordEvent(dataPush);
   }
 };
 
