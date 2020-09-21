deb41a905146eefaf4e1dc6b38fccfb10614c56a
Brooks Johnson <brooks.johnson@pm.me>
2020-09-21 15:00:21 -0700

    Allow form tester to skip axeCheck

--- src/platform/testing/e2e/cypress/support/form-tester/index.js
+++ src/platform/testing/e2e/cypress/support/form-tester/index.js
@@ -135,8 +135,8 @@ const addNewArrayItem = $form => {
  *
  * @param {string} pathname - The pathname of the page to run the page hook on.
  */
-const performPageActions = pathname => {
-  cy.axeCheck();
+const performPageActions = (pathname, { runAxeCheck }) => {
+  if (runAxeCheck) cy.axeCheck();
 
   cy.execHook(pathname).then(({ hookExecuted, postHook }) => {
     const shouldAutofill = !pathname.match(
@@ -146,7 +146,8 @@ const performPageActions = pathname => {
     if (!hookExecuted && shouldAutofill) cy.fillPage();
 
     cy.expandAccordions();
-    cy.axeCheck();
+
+    if (runAxeCheck) cy.axeCheck();
 
     const postHookPromise = new Promise(resolve => {
       postHook();
@@ -161,9 +162,9 @@ const performPageActions = pathname => {
  * Top level loop that invokes all of the processing for a form page and
  * asserts that it proceeds to the next page until it gets to the confirmation.
  */
-const processPage = () => {
+const processPage = ({ runAxeCheck = true }) => {
   cy.location('pathname', NO_LOG_OPTION).then(pathname => {
-    performPageActions(pathname);
+    performPageActions(pathname, { runAxeCheck });
 
     if (!pathname.endsWith('/confirmation')) {
       cy.location('pathname', NO_LOG_OPTION)
@@ -172,7 +173,7 @@ const processPage = () => {
             throw new Error(`Expected to navigate away from ${pathname}`);
           }
         })
-        .then(processPage);
+        .then(() => processPage({ runAxeCheck }));
     }
   });
 };
@@ -490,6 +491,7 @@ const testForm = testConfig => {
     setup = () => {},
     setupPerTest = () => {},
     skip,
+    runAxeCheck = true,
   } = testConfig;
 
   const skippedTests = Array.isArray(skip) && new Set(skip);
@@ -549,7 +551,7 @@ const testForm = testConfig => {
 
           cy.get(LOADING_SELECTOR)
             .should('not.exist')
-            .then(processPage);
+            .then(() => processPage({ runAxeCheck }));
         });
       });
 
