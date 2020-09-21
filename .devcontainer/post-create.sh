b1c80237d6f97419252bd5f1ac2ac5a99986a288
Mike Chelen <michael.chelen@va.gov>
2020-09-21 14:24:18 +0000

    add newlines

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 # configure NVM
+printf "\n\n#####" Configuring Node, NPM, and Yarn #####\n""
 source $NVM_DIR/nvm.sh
 nvm install || true # ignore exit code due to npm prefix
 nvm use --delete-prefix
 
 # check versions
-printf "node version: "
+printf "Node version: "
 node --version
 
-printf "yarn version: "
+printf "Yarn version: "
 yarn --version
 
 # install dependencies
-printf "\n\n#####" Installing dependencies #####""
+printf "\n\n#####" Installing dependencies #####\n""
 yarn install
 
 # download content repo
-printf "\n\n##### Downloading content repo #####"
+printf "\n\n##### Downloading content repo #####\n"
 yarn install-repos
 
 # download drupal cache
-printf "\n\n##### Downloading Drupal cache #####"
+printf "\n\n##### Downloading Drupal cache #####\n"
 yarn fetch-drupal-cache
 
 # build content
 # yarn build
 
-printf "\n\n##### Codespace setup complete! #####"
+printf "\n\n##### Codespace setup complete! #####\n"
