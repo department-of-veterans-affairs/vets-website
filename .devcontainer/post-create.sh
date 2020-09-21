0bbcdcd121304ef8cab757b3228e9831bd0dec50
Mike Chelen <michael.chelen@va.gov>
2020-09-21 13:45:46 +0000

    use printf instead of echo

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,19 +6,25 @@ nvm install || true # ignore exit code due to npm prefix
 nvm use --delete-prefix
 
 # check versions
-echo "node version: "
+printf "node version: "
 node --version
-echo "yarn version: "
+
+printf "yarn version: "
 yarn --version
 
 # install dependencies
+printf "\n\n#####" Installing dependencies #####""
 yarn install
 
 # download content repo
+printf "\n\n##### Downloading content repo #####"
 yarn install-repos
 
 # download drupal cache
+printf "\n\n##### Downloading Drupal cache #####"
 yarn fetch-drupal-cache
 
 # build content
 # yarn build
+
+printf "\n\n##### Codespace setup complete! #####"
