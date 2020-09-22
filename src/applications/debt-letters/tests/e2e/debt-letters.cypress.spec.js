56dc78d9594d1d73444918021b106168a6415868
Craig Wheeler <craig@craigwheeler.net>
2020-09-22 14:08:48 -0400

    create cypress spec

---
+++ src/applications/debt-letters/tests/e2e/debt-letters.cypress.spec.js
@@ -0,0 +1,21 @@
+describe('Debt Letters', () => {
+  it('displays the current debts section and navigates to debt details', () => {
+    expect(true).to.equal(true);
+  });
+
+  it('displays download debt letters', () => {
+    expect(true).to.equal(true);
+  });
+
+  it('displays how do I pay my VA debt?', () => {
+    expect(true).to.equal(true);
+  });
+
+  it('displays how do I get financial help?', () => {
+    expect(true).to.equal(true);
+  });
+
+  it('displays how do I dispute a debt?', () => {
+    expect(true).to.equal(true);
+  });
+});
