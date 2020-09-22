0ce9a361aca46101c6ac2bc0b7e645bbff1686d0
Kelson Adams <kelsonic@users.noreply.github.com>
2020-09-22 10:10:15 -0600

    [Templates] Add Checklist layout (#14285)
    * Update step by step with buttons repeat
    
    * Add boilerplate for checklist
    
    * Add checklist sections
    
    * Fix liquid syntax errors
    
    * Fix accessibility error
    
    * Fix axe error
    
    * Fix graphql error
    
    Co-authored-by: Nick Sullivan <nick.sullivan@adhocteam.us>

---
+++ src/site/stages/build/drupal/graphql/nodeChecklist.graphql.js
@@ -0,0 +1,48 @@
+const entityElementsFromPages = require('./entityElementsForPages.graphql');
+
+const fragment = `
+fragment nodeChecklist on NodeChecklist {
+  ${entityElementsFromPages}
+  entityBundle
+
+  title
+  fieldDescription
+  fieldIntroTextLimitedHtml {
+    processed
+  }
+
+  fieldButtonsRepeat
+  fieldButtons {
+    entity {
+      ... button
+    }
+  }
+  fieldAlertSingle {
+    entity {
+      ... alertSingle
+    }
+  }
+  fieldRelatedLinks {
+    entity {
+      ... listOfLinkTeasers
+    }
+  }
+  fieldChecklist {
+    entity {
+      ... on ParagraphChecklist {
+        fieldChecklistSections {
+          entity {
+            ... on ParagraphChecklistItem {
+              fieldSectionIntro
+              fieldSectionHeader
+              fieldChecklistItems
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+module.exports = fragment;
