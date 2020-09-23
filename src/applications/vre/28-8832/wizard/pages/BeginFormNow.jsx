b6da284bd2030f9cd5dc47b9a92050a3e9c9c13d
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-09-23 13:30:18 -0700

    Added GA event to begin form question

--- src/applications/vre/28-8832/wizard/pages/BeginFormNow.jsx
+++ src/applications/vre/28-8832/wizard/pages/BeginFormNow.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import recordEvent from 'platform/monitoring/record-event';
 import ErrorableRadioButtons from '@department-of-veterans-affairs/formation-react/ErrorableRadioButtons';
 import { pageNames } from './pageList';
 
@@ -13,16 +14,28 @@ const options = [
   },
 ];
 
-const BeginFormNow = ({ setPageState, state = {} }) => (
-  <ErrorableRadioButtons
-    name="begin-form-now"
-    label="Would you like to apply for career planning and guidance benefits now?"
-    options={options}
-    id="begin-form-now"
-    onValueChange={({ value }) => setPageState({ selected: value }, value)}
-    value={{ value: state.selected }}
-  />
-);
+const BeginFormNow = ({ setPageState, state = {} }) => {
+  const handleValueChange = ({ value }) => {
+    recordEvent({
+      event: `howToWizard-formChange`,
+      'form-field-type': 'form-radio-buttons',
+      'form-field-label':
+        'Would you like to apply for career planning and guidance benefits now?',
+      'form-field-value': value,
+    });
+    setPageState({ selected: value }, value);
+  };
+  return (
+    <ErrorableRadioButtons
+      name="begin-form-now"
+      label="Would you like to apply for career planning and guidance benefits now?"
+      options={options}
+      id="begin-form-now"
+      onValueChange={handleValueChange}
+      value={{ value: state.selected }}
+    />
+  );
+};
 
 export default {
   name: 'beginFormNow',
