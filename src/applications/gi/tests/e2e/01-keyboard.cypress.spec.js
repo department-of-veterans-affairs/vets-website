7454da1647dbca3d03efc6372225a6135a5e990c
Matt Roth <roth_matthew@bah.com>
2020-09-23 15:38:21 -0400

    Update 01-keyboard.cypress.spec.js

--- src/applications/gi/tests/e2e/01-keyboard.cypress.spec.js
+++ src/applications/gi/tests/e2e/01-keyboard.cypress.spec.js
@@ -38,17 +38,17 @@ describe('Comparison Tool', () => {
 
     // Assert skip navigation link works correctly
     // Comment out for local testing
-    // cy.get('body').tab();
-    // cy.get('a.show-on-focus').should('have.focus');
-    // cy.get('a.show-on-focus')
-    //   .type('{enter}')
-    //   .tab()
-    //   .tab()
-    //   .tab();
+    cy.get('body').tab();
+    cy.get('a.show-on-focus').should('have.focus');
+    cy.get('a.show-on-focus')
+      .type('{enter}')
+      .tab()
+      .tab()
+      .tab();
 
     cy.get('.va-nav-breadcrumbs-list > li > a')
       .first()
-      .focus() // uncomment for local testing
+      // .focus() // uncomment for local testing
       .should('have.focus')
       .should('have.attr', 'href', '/');
 
