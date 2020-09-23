6f4fba7996c5ae87dcb099e949fb6ce6c4be8aa7
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-09-23 13:19:51 -0700

    Added GA event for VRE benefits question

--- src/applications/vre/28-8832/wizard/pages/VREBenefits.jsx
+++ src/applications/vre/28-8832/wizard/pages/VREBenefits.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import recordEvent from 'platform/monitoring/record-event';
 import ErrorableRadioButtons from '@department-of-veterans-affairs/formation-react/ErrorableRadioButtons';
 import { pageNames } from './pageList';
 
@@ -13,16 +14,28 @@ const options = [
   },
 ];
 
-const VREBenefits = ({ setPageState, state = {} }) => (
-  <ErrorableRadioButtons
-    name="vre-benefits"
-    label="Are you receiving Chapter 31 Veteran Readiness and Employment (VR&E) benefits?"
-    options={options}
-    id="vre-benefits"
-    onValueChange={({ value }) => setPageState({ selected: value }, value)}
-    value={{ value: state.selected }}
-  />
-);
+const VREBenefits = ({ setPageState, state = {} }) => {
+  const handleValueChange = ({ value }) => {
+    recordEvent({
+      event: `howToWizard-formChange`,
+      'form-field-type': 'form-radio-buttons',
+      'form-field-label':
+        'Are you receiving Chapter 31 Veteran Readiness and Employment (VR&E) benefits?',
+      'form-field-value': value,
+    });
+    setPageState({ selected: value }, value);
+  };
+  return (
+    <ErrorableRadioButtons
+      name="vre-benefits"
+      label="Are you receiving Chapter 31 Veteran Readiness and Employment (VR&E) benefits?"
+      options={options}
+      id="vre-benefits"
+      onValueChange={handleValueChange}
+      value={{ value: state.selected }}
+    />
+  );
+};
 
 export default {
   name: 'VREBenefits',
