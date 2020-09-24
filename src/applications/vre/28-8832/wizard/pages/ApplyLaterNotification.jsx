dbcbcf49524f53447bb85830c917e44b15adbd4e
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-09-24 07:44:35 -0700

    Added GA events for notification boxes

--- src/applications/vre/28-8832/wizard/pages/ApplyLaterNotification.jsx
+++ src/applications/vre/28-8832/wizard/pages/ApplyLaterNotification.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react';
+import recordEvent from 'platform/monitoring/record-event';
 import { WIZARD_STATUS, WIZARD_STATUS_COMPLETE } from '../../constants';
 
 const ApplyLaterNotification = () => {
   useEffect(() => {
     sessionStorage.setItem(WIZARD_STATUS, WIZARD_STATUS_COMPLETE);
+    recordEvent({
+      event: `howToWizard-notice-displayed`,
+      'reason-for-notice': 'chose not to apply now',
+    });
   });
 
   return (
--- src/applications/vre/28-8832/wizard/pages/VRECounselorNotification.jsx
+++ src/applications/vre/28-8832/wizard/pages/VRECounselorNotification.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import recordEvent from 'platform/monitoring/record-event';
 import { VRE_COUNSELOR_ROOT_URL } from '../../constants';
 
-const VRECounselorNotification = () => (
-  <div className="vads-u-margin-top--2 vads-u-background-color--primary-alt-lightest vads-u-padding--3">
-    <p className="vads-u-margin-top--0">
-      Please contact your vocational rehabilitation counselor to learn more
-      about how to get career planning and guidance benefits.
-    </p>
-    <a href={VRE_COUNSELOR_ROOT_URL}>
-      Contact a vocational rehabilitation counselor
-    </a>
-  </div>
-);
+const VRECounselorNotification = () => {
+  useEffect(() => {
+    recordEvent({
+      event: `howToWizard-notice-displayed`,
+      'reason-for-notice': 'ineligibility - eligible for chapter 31',
+    });
+  });
+  return (
+    <div className="vads-u-margin-top--2 vads-u-background-color--primary-alt-lightest vads-u-padding--3">
+      <p className="vads-u-margin-top--0">
+        Please contact your vocational rehabilitation counselor to learn more
+        about how to get career planning and guidance benefits.
+      </p>
+      <a href={VRE_COUNSELOR_ROOT_URL}>
+        Contact a vocational rehabilitation counselor
+      </a>
+    </div>
+  );
+};
 
 export default {
   name: 'VRECounselorNotification',
