f17f6e2d37eb22accada7e9a44a8f2cb244a77c3
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-09-28 11:36:28 -0700

    Switched to use React Testing Library

--- src/applications/disability-benefits/view-payments/tests/components/view-payments-lists/view-payments-lists.unit.spec.jsx
+++ src/applications/disability-benefits/view-payments/tests/components/view-payments-lists/view-payments-lists.unit.spec.jsx
@@ -9,7 +9,7 @@ const store = createCommonStore(allPayments);
 
 import ViewPaymentsLists from '../../../components/view-payments-lists/ViewPaymentsLists.jsx';
 
-import { thePayments } from './helpers';
+import { payments } from '../../helpers';
 
 describe('View Payments Lists', () => {
   it('renders View Payments Lists component', () => {
@@ -19,62 +19,62 @@ describe('View Payments Lists', () => {
         store={store}
         isLoading={false}
         error={false}
-        payments={thePayments}
+        payments={payments}
         getAllPayments={getAllPayments}
       />,
     );
-
-    expect(wrapper.findByText(/Payments you received/)).to.exist;
-    expect(getAllPayments.toBeCalled);
-    wrapper.unmount();
+    // We need to set a timeout to wait for the getAllPayments call to resolve
+    setTimeout(async () => {
+      expect(wrapper.getByText(/Payments you received/)).to.exist;
+      expect(getAllPayments.toBeCalled);
+      wrapper.unmount();
+    }, 3000);
   });
-  /*
+
   it('shows a loading indicator when loading', () => {
-    const wrapper = shallow(<ViewPaymentsLists store={store} isLoading />);
+    const wrapper = render(<ViewPaymentsLists store={store} isLoading />);
 
-    expect(wrapper.find('.loading-indicator-container')).to.exist;
+    expect(wrapper.getByText(/Loading payment information.../)).to.exist;
     wrapper.unmount();
   });
 
-  it('should render the header above the tables', () => {
+  it('should render both the tables', () => {
     const getAllPayments = sinon.spy();
-    const wrapper = mount(
+    const wrapper = render(
       <ViewPaymentsLists
         store={store}
         isLoading={false}
         error={false}
-        payments={thePayments}
+        payments={payments}
         getAllPayments={getAllPayments}
       />,
     );
 
-    expect(wrapper.find('.vads-u-font-size--lg')).to.exist;
-    wrapper.unmount();
+    setTimeout(async () => {
+      expect(wrapper.getByText(/Payments you received/)).to.exist;
+      expect(wrapper.getByText(/Payments returned/)).to.exist;
+      expect(getAllPayments.toBeCalled);
+      wrapper.unmount();
+    }, 3000);
   });
 
-  it('should render the tables', () => {
+  it('should render one table when data for the other in unavailable', () => {
     const getAllPayments = sinon.spy();
-    const wrapper = mount(
+    const wrapper = render(
       <ViewPaymentsLists
         store={store}
         isLoading={false}
         error={false}
-        payments={thePayments}
+        payments={payments.payments}
         getAllPayments={getAllPayments}
       />,
     );
 
-    expect(wrapper.find('.va-table')).to.exist;
-    wrapper.unmount();
+    setTimeout(async () => {
+      expect(wrapper.getByText(/Payments you received/)).to.exist;
+      expect(wrapper.getByText(/Payments returned/)).to.not.exist;
+      expect(getAllPayments.toBeCalled);
+      wrapper.unmount();
+    }, 3000);
   });
-
-  it('should render an alert when no payments are present', () => {
-    const wrapper = shallow(
-      <ViewPaymentsLists store={store} isLoading={false} />,
-    );
-
-    expect(wrapper.find('.usa-alert-info')).to.exist;
-    wrapper.unmount();
-  });
-  */
 });
