a55a5cbe24997f2bde0fe826c9d5a708905c86fb
Robin Garrison <robin.garrison@adhocteam.us>
2020-09-29 11:33:33 -0500

    Update HLR prefill transformer

--- src/applications/disability-benefits/996/config/prefill-transformer.js
+++ src/applications/disability-benefits/996/config/prefill-transformer.js
@@ -1,4 +1,59 @@
+/* vets-api/config/form_profile_mappings/20-0996.yml
+data:
+  attributes:
+    veteran:
+      address:
+        zipCode5:
+      phone:
+        phoneNumber:
+      emailAddressText:
+nonPrefill:
+  veteranAddress:
+    street:
+    street2:
+    city:
+    state:
+    country:
+    postal_code: // not used
+    zip_code: // not used
+  veteranSsnLastFour:
+  veteranVaFileNumberLastFour:
+*/
+
 export default function prefillTransformer(pages, formData, metadata) {
-  const veteran = formData?.data?.attributes?.veteran || {};
-  return { pages, formData: { veteran }, metadata };
+  const {
+    address: { zipCode5 = '' } = {},
+    phone: { phoneNumber = '' } = {},
+    emailAddressText = '',
+  } = formData?.data?.attributes?.veteran || {};
+  const {
+    veteranAddress: {
+      street = '',
+      street2 = '',
+      city = '',
+      state = '',
+      country = '',
+    } = {},
+    veteranSsnLastFour = '',
+    veteranVaFileNumberLastFour = '',
+  } = formData?.nonPrefill || {};
+
+  return {
+    pages,
+    formData: {
+      veteran: {
+        street,
+        street2,
+        city,
+        state,
+        country,
+        zipCode5,
+        phoneNumber,
+        emailAddress: emailAddressText,
+        ssnLastFour: veteranSsnLastFour,
+        vaFileLastFour: veteranVaFileNumberLastFour,
+      },
+    },
+    metadata,
+  };
 }
