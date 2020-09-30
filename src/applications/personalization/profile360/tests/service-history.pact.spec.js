5849a42d149f904f6ef4d5898d71740b750a66a9
Erik P. Hansen <eph@erikphansen.com>
2020-09-30 16:39:21 -0700

    add Pact testing notes, mapping users to test cases

--- src/applications/personalization/profile360/tests/service-history.pact.spec.js
+++ src/applications/personalization/profile360/tests/service-history.pact.spec.js
@@ -9,6 +9,7 @@ const { eachLike, like, string, term } = Matchers;
 
 contractTest('VA Profile', 'VA.gov API', mockApi => {
   describe('GET /service_history', () => {
+    // user 36 will have this behavior
     it('responds with an array of serviceHistory objects when there is a service history', async () => {
       await mockApi().addInteraction({
         state: 'at least one entry in the service history exists',
@@ -45,6 +46,7 @@ contractTest('VA Profile', 'VA.gov API', mockApi => {
       const dispatch = sinon.stub();
       await fetchMilitaryInformation()(dispatch);
     });
+    // user 1 will have this behavior
     it('responds with an empty serviceHistory array when there is no service history', async () => {
       await mockApi().addInteraction({
         state: 'there are no service history records',
@@ -77,6 +79,7 @@ contractTest('VA Profile', 'VA.gov API', mockApi => {
       const dispatch = sinon.stub();
       await fetchMilitaryInformation()(dispatch);
     });
+    // user 10 will have this behavior
     it('responds with a 403 error when the user is not a Veteran', async () => {
       await mockApi().addInteraction({
         state: 'not a Veteran',
