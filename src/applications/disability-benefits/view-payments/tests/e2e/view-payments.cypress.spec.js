5af08243500bef65a767fb558d69347d1626bbf5
Micah Chiang <micahkchiang@gmail.com>
2020-09-30 12:20:21 -0400

    Update test

--- src/applications/disability-benefits/view-payments/tests/e2e/view-payments.cypress.spec.js
+++ src/applications/disability-benefits/view-payments/tests/e2e/view-payments.cypress.spec.js
@@ -1,10 +1,20 @@
 import mockUser from '../test-user.json';
-import { payments } from '../helpers';
+import mockPayments from '../test-payments-response.json';
 
 const PAYMENTS_PATH = '/va-payment-history/payments';
 
 const testPagination = () => {
   cy.visit(PAYMENTS_PATH);
+  cy.findByText(/Your VA payments/i).should('exist');
+  cy.findByText(/Loading payment information/i).should('exist');
+  cy.findByText(/Loading payment information/i).should('not.exist');
+  cy.findByText(/Payments you received/i).should('exist');
+  // check a11y
+  cy.injectAxe();
+  cy.axeCheck();
+  // Paginate to next set of data points
+  cy.findByText(/Next/i).click();
+  cy.findByText(/Displaying 6 - 9 of 9/i).should('exist');
 };
 
 describe('View payment history', () => {
@@ -14,7 +24,7 @@ describe('View payment history', () => {
       JSON.stringify(['single-sign-on-intro']),
     );
     cy.login(mockUser);
-    cy.route('GET', '/v0/profile/payment_history', payments);
+    cy.route('GET', '/v0/profile/payment_history', mockPayments);
   });
   it('should pass an aXe scan and paginate through payment data', () => {
     testPagination();
