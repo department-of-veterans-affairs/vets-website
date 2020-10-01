d6da89b966bee53ccbb5d97fefc21addfa9e9533
Sandra Hallie <sandra.adamshallie@gmail.com>
2020-10-01 14:36:33 -0600

    Update cypress test

--- src/applications/personalization/profile-2/tests/e2e/profile.update-email-error.cypress.spec.js
+++ src/applications/personalization/profile-2/tests/e2e/profile.update-email-error.cypress.spec.js
@@ -1,6 +1,7 @@
 import { PROFILE_PATHS } from '../../constants';
 import mockUser from '../fixtures/users/user-36.json';
 import mockFeatureToggles from '../fixtures/feature-toggles.json';
+import emailUpdateResponseFailure from '../fixtures/email-400.json';
 
 const setup = (mobile = false) => {
   window.localStorage.setItem(
@@ -16,41 +17,41 @@ const setup = (mobile = false) => {
   cy.route('GET', '/v0/feature_toggles*', mockFeatureToggles);
   cy.visit(PROFILE_PATHS.PROFILE_ROOT);
 
+  cy.route({
+    method: 'PUT',
+    url: '/v0/profile/email_addresses',
+    status: 400,
+    response: emailUpdateResponseFailure,
+  }).as('mockEmailUpdateFailure');
+
   // should show a loading indicator
   cy.findByRole('progressbar').should('exist');
   cy.findByText(/loading your information/i).should('exist');
-
-  // and then the loading indicator should be removed
-  cy.findByText(/loading your information/i).should('not.exist');
-  cy.findByRole('progressbar').should('not.exist');
-};
-
-const checkEmailAddressAlertCopy = () => {
-  const errorText = 'It looks like the email you entered isn’t valid.';
-
-  // Open edit view
-  cy.findByRole('button', {
-    name: new RegExp(`edit email address`, 'i'),
-  }).click({
-    force: true,
-  });
-
-  cy.get(`#root_emailAddress`)
-    .click()
-    .clear()
-    .type('test@test.cocococococococo');
-
-  // Open edit view
-  cy.findByRole('button', { name: 'Update' }).click({
-    force: true,
-  });
-
-  cy.contains(errorText).should('exist');
 };
 
 describe('The update email Alert', () => {
   it('should render with the correct copy', () => {
     setup();
-    checkEmailAddressAlertCopy();
+    const errorText = 'It looks like the email you entered isn’t valid.';
+
+    // Open edit view
+    cy.findByRole('button', {
+      name: new RegExp(`edit email address`, 'i'),
+    }).click({
+      force: true,
+    });
+
+    cy.get(`#root_emailAddress`)
+      .click()
+      .clear()
+      .type('test@test.cocococococom');
+
+    // Open edit view
+    cy.findByRole('button', { name: 'Update' }).click({
+      force: true,
+    });
+
+    cy.wait('@mockEmailUpdateFailure');
+    cy.contains(errorText).should('exist');
   });
 });
