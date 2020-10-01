c7b2b381dad942f5e309be7f6df6e900c76ebff6
Kelson Adams <kadams@governmentcio.com>
2020-10-01 08:57:02 -0600

    Add about template

---
+++ src/site/stages/build/drupal/graphql/nodeSupportResourcesDetailPage.graphql.js
@@ -0,0 +1,52 @@
+const entityElementsFromPages = require('./entityElementsForPages.graphql');
+
+const fragment = `
+fragment nodeSupportResourcesDetailPage on NodeSupportResourcesDetailPage {
+  ${entityElementsFromPages}
+  entityBundle
+  changed
+  title
+
+  fieldContentBlock {
+    entity {
+      entityType
+      entityBundle
+      ... wysiwyg
+      ... collapsiblePanel
+      ... process
+      ... qaSection
+      ... qa
+      ... listOfLinkTeasers
+      ... reactWidget
+      ... spanishSummary
+      ... alertParagraph
+      ... table
+      ... downloadableFile
+      ... embeddedImage
+      ... numberCallout
+    }
+  }
+  fieldTableOfContentsBoolean
+  fieldIntroTextLimitedHtml {
+    processed
+  }
+  fieldAlertSingle {
+    entity {
+      ... alertSingle
+    }
+  }
+  fieldButtonsRepeat
+  fieldButtons {
+    entity {
+      ... button
+    }
+  }
+  fieldRelatedInformation {
+    entity {
+      ... listOfLinkTeasers
+    }
+  }
+}
+`;
+
+module.exports = fragment;
