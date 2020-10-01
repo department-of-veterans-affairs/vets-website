02d52241b85d6f69ca66ce30cc882e40e7f81fcc
Mark Dewey <mdewey@governmentcio.com>
2020-10-01 11:02:48 -0400

    Updated Vets infor on review page

--- src/applications/healthcare/questionnaire/components/veteran-info/VeteranInfoReviewPage.jsx
+++ src/applications/healthcare/questionnaire/components/veteran-info/VeteranInfoReviewPage.jsx
@@ -1,5 +1,52 @@
 import React from 'react';
+import moment from 'moment';
 
-export default function VeteranInfoReviewPage() {
-  return <div>Show me the dd and dt</div>;
+import { genderLabels } from 'platform/static-data/labels';
+import { formatPhoneNumber, addressToDisplay } from './utils';
+
+const phonesToDisplay = phones => {
+  return phones.filter(f => f.data).map(phone => {
+    return {
+      label: phone.label,
+      value: formatPhoneNumber(phone.data.areaCode + phone.data.phoneNumber),
+    };
+  });
+};
+
+export default function VeteranInfoReviewPage({ formData }) {
+  const veteran = formData?.veteranInfo;
+  const display = [
+    {
+      label: 'Full Name',
+      value: veteran.fullName,
+    },
+    {
+      label: 'Date of Birth',
+      value: moment(veteran.dateOfBirth).format('MMMM DD, YYYY'),
+    },
+    {
+      label: 'Gender',
+      value: genderLabels[veteran.gender]
+        ? genderLabels[veteran.gender]
+        : 'UNKNOWN',
+    },
+    ...addressToDisplay(veteran.addresses?.mailing),
+    ...addressToDisplay(veteran.addresses?.residential),
+    ...phonesToDisplay(veteran.phones),
+  ];
+  return (
+    <>
+      <dl className="review">
+        {display.map((row, i) => {
+          const { label, value } = row;
+          return value ? (
+            <div key={i} className="review-row">
+              <dt>{label}</dt>
+              <dd>{value}</dd>
+            </div>
+          ) : null;
+        })}
+      </dl>
+    </>
+  );
 }
---
+++ src/applications/healthcare/questionnaire/components/veteran-info/utils/index.js
@@ -0,0 +1,62 @@
+const addressToDisplay = address => {
+  const rv = [];
+  if (!address) {
+    return rv;
+  }
+
+  // sa1
+  if (address.addressLine1) {
+    rv.push({
+      label: `Address${address.addressLine2 ? ' 1' : ''}`,
+      value: address.addressLine1,
+    });
+  }
+  // sa2
+  if (address.addressLine2) {
+    rv.push({
+      label: `Address 2`,
+      value: address.addressLine2,
+    });
+  }
+  // sa3
+  if (address.addressLine3) {
+    rv.push({
+      label: `Address 3`,
+      value: address.addressLine3,
+    });
+  }
+  // city
+  if (address.city) {
+    rv.push({
+      label: 'City',
+      value: address.city,
+    });
+  }
+  // stateCode
+  if (address.stateCode) {
+    rv.push({
+      label: 'State',
+      value: address.stateCode,
+    });
+  }
+  // zipCode
+  if (address.zipCode) {
+    rv.push({
+      label: 'Zip',
+      value: address.zipCode,
+    });
+  }
+  return rv;
+};
+
+const formatPhoneNumber = phoneNumberString => {
+  const cleaned = `${phoneNumberString}`.replace(/\D/g, '');
+  const match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
+  if (match) {
+    const intlCode = match[1] ? '+1 ' : '';
+    return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
+  }
+  return null;
+};
+
+export { addressToDisplay, formatPhoneNumber };
