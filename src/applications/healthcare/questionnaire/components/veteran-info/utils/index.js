186277265973eb37a505c63553392664bc885412
Mark Dewey <mdewey@governmentcio.com>
2020-10-01 11:08:33 -0400

    used better phone display

--- src/applications/healthcare/questionnaire/components/veteran-info/utils/index.js
+++ src/applications/healthcare/questionnaire/components/veteran-info/utils/index.js
@@ -54,7 +54,7 @@ const formatPhoneNumber = phoneNumberString => {
   const match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
   if (match) {
     const intlCode = match[1] ? '+1 ' : '';
-    return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
+    return [intlCode, '', match[2], '-', match[3], '-', match[4]].join('');
   }
   return null;
 };
