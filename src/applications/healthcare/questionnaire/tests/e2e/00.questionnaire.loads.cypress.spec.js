699a1275c68847cd96dc12d3afd440810e6c6e5e
Mark Dewey <mdewey@users.noreply.github.com>
2020-10-02 14:58:21 -0400

    Healthcare 12282 landing page (#14396)
    * added intro content
    
    * updated button
    
    * updated headers, not quite right uyet
    
    * bumped version number and added some css
    
    * added formating for OMB info
    
    * cleaned some code
    
    * finsihed with styling, need to do accessilbilty
    
    * added custom qording
    
    * accessbility checks
    
    * fixed typos
    
    * unauthed is done
    
    * finished authed page
    
    * Added and updatd tests
    
    * added the top alert
    
    * disabled failing tests to deal with in #14175

--- src/applications/healthcare/questionnaire/tests/e2e/00.questionnaire.loads.cypress.spec.js
+++ src/applications/healthcare/questionnaire/tests/e2e/00.questionnaire.loads.cypress.spec.js
@@ -8,7 +8,9 @@ it('healthcare questionnaire -- loads introduction page -- feature enabled', () 
     cy.login(basicUser);
     cy.visit('/healthcare/questionnaire/introduction');
     cy.title().should('contain', 'Healthcare Questionnaire');
-    cy.get('.schemaform-title>h1').contains('Healthcare Questionnaire');
+    cy.get('.schemaform-title>h1').contains(
+      'Upcoming appointment questionnaire',
+    );
   });
 });
 
@@ -17,8 +19,8 @@ it('healthcare questionnaire -- can not see feature -- feature disabled', () => 
     '../../src/applications/healthcare/questionnaire/tests/e2e/fixtures/mocks/feature-toggles.disabled.json',
   ).then(features => {
     cy.route('GET', '/v0/feature_toggles*', features);
-    const feautureRoute = '/healthcare/questionnaire/introduction';
-    cy.visit(feautureRoute);
+    const featureRoute = '/healthcare/questionnaire/introduction';
+    cy.visit(featureRoute);
 
     cy.url().should('not.match', /healthcare/);
   });
