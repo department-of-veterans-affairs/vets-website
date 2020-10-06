06a7eb749588962a5727a6c2e49f9e802a1d0e28
Ethan Teague <metrowebz@gmail.com>
2020-10-06 07:44:58 -0400

    VACMS-12970: Remove hardcoded menus list and pull from graphql endpoint.

--- src/site/stages/build/drupal/graphql/navigation-fragments/facilitySidebar.nav.graphql.js
+++ src/site/stages/build/drupal/graphql/navigation-fragments/facilitySidebar.nav.graphql.js
@@ -5,26 +5,8 @@
 
 // String Helpers
 const { camelize } = require('./../../../../../utilities/stringHelpers');
-
-const FACILITY_MENU_NAMES = [
-  'pittsburgh-health-care',
-  'va-altoona-health-care',
-  'va-butler-health-care',
-  'va-cheyenne-health-care',
-  'va-coatesville-health-care',
-  'va-eastern-colorado-health-care',
-  'va-eastern-oklahoma-health-care',
-  'va-erie-health-care',
-  'va-lebanon',
-  'va-montana-health-care',
-  'va-oklahoma-health-care',
-  'va-philadelphia-health-care',
-  'va-salt-lake-city-health-care',
-  'va-sheridan-health-care',
-  'va-western-colorado-health-care',
-  'va-wilkes-barre-health-care',
-  'va-wilmington-health-care',
-];
+/* eslint-disable no-undef */
+const FACILITY_MENU_NAMES = cmsSideNavs;
 
 const FACILITY_SIDEBAR_QUERY = `
     name
