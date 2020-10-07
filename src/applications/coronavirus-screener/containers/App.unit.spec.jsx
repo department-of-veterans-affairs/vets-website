069dca030cdbfcceac4188b6403416c0b73edaee
Michael Chelen <michael.chelen@va.gov>
2020-10-07 11:01:27 -0400

    add tests for customId param

--- src/applications/coronavirus-screener/containers/App.unit.spec.jsx
+++ src/applications/coronavirus-screener/containers/App.unit.spec.jsx
@@ -2,17 +2,35 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import App from './App';
+import MultiQuestionForm from '../components/MultiQuestionForm';
 
 describe('coronavirus-screener', () => {
+  let mockParams = {};
+  beforeEach(() => {
+    mockParams = {};
+  });
+
   describe('App', () => {
-    const mockParams = {};
     it('outputs introduction text', () => {
-      const wrapper = shallow(<App params={mockParams} />);
+      const wrapper = shallow(<App params={{}} />);
 
       expect(wrapper.hasClass('covid-screener')).to.equal(true);
 
       expect(wrapper.find('h1').text()).to.equal('COVID-19 screening tool');
 
+      wrapper.unmount();
+    });
+
+    it('passes id param only if it exists', () => {
+      const wrapper = shallow(<App params={mockParams} />);
+      expect(wrapper.find(MultiQuestionForm).prop('customId')).to.be.undefined;
+      wrapper.unmount();
+    });
+
+    it('uppercases id param', () => {
+      mockParams.id = 'abc';
+      const wrapper = shallow(<App params={mockParams} />);
+      expect(wrapper.find(MultiQuestionForm).prop('customId')).to.equal('ABC');
       wrapper.unmount();
     });
   });
