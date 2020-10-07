00ae08bca33eb7fd77f9133c0758252dcdb248a9
Patrick Bateman <batemapf@gmail.com>
2020-10-07 11:00:12 -0400

    update unit test

--- src/applications/coronavirus-screener/components/MultiQuestionForm.unit.spec.jsx
+++ src/applications/coronavirus-screener/components/MultiQuestionForm.unit.spec.jsx
@@ -10,17 +10,35 @@ beforeEach(() => {
   mockQuestions = [
     {
       id: 'myFirstQuestion',
-      text: 'This is the first question text',
+      text: {
+        en: 'This is the first question text',
+        es: 'Este es el primer texto de la pregunta',
+      },
       startQuestion: true,
     },
     {
       id: 'mySecondQuestion',
-      text: 'This is the second question text',
+      text: {
+        en: 'This is the second question text',
+        es: 'Este es el texto de la segunda pregunta',
+      },
     },
   ];
   mockDefaultOptions = [
-    { optionValue: 'yes', optionText: 'Yes' },
-    { optionValue: 'no', optionText: 'No' },
+    {
+      optionValue: 'yes',
+      optionText: {
+        en: 'Yes',
+        es: 'Sí',
+      },
+    },
+    {
+      optionValue: 'no',
+      optionText: {
+        en: 'No',
+        es: 'No',
+      },
+    },
   ];
 });
 
