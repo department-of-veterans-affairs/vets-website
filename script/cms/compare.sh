33e57ac44b200b5ac7511ed7db6f8feaaa2caab9
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2020-10-07 15:18:01 -0400

    write diff to file instead of piping to delta

--- script/cms/compare.sh
+++ script/cms/compare.sh
@@ -3,4 +3,4 @@ mv build/localhost/ build/graphql-localhost/
 yarn build:content --pull-drupal --use-cms-export
 mv build/localhost build/cms-export-localhost/
 cd build
-diff -ur graphql-localhost/ cms-export-localhost/ | delta
+diff -ur graphql-localhost/ cms-export-localhost/ > cms.diff
