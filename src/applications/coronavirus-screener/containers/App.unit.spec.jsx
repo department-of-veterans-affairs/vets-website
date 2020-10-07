1e3d00c8eac311711b27ce6a122f4663c9893cf1
Michael Chelen <michael.chelen@va.gov>
2020-10-07 11:02:24 -0400

    cleanup

--- src/applications/coronavirus-screener/containers/App.unit.spec.jsx
+++ src/applications/coronavirus-screener/containers/App.unit.spec.jsx
@@ -6,18 +6,16 @@ import MultiQuestionForm from '../components/MultiQuestionForm';
 
 describe('coronavirus-screener', () => {
   let mockParams = {};
+
   beforeEach(() => {
     mockParams = {};
   });
 
   describe('App', () => {
     it('outputs introduction text', () => {
-      const wrapper = shallow(<App params={{}} />);
-
+      const wrapper = shallow(<App params={mockParams} />);
       expect(wrapper.hasClass('covid-screener')).to.equal(true);
-
       expect(wrapper.find('h1').text()).to.equal('COVID-19 screening tool');
-
       wrapper.unmount();
     });
 
