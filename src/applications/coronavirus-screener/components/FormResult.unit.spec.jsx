b2b30e3dc0af19baac195760d74e5e421bd8b64b
Michael Chelen <michael.chelen@va.gov>
2020-10-07 12:08:09 -0400

    add tests for other results, use getByText() shorthand

--- src/applications/coronavirus-screener/components/FormResult.unit.spec.jsx
+++ src/applications/coronavirus-screener/components/FormResult.unit.spec.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { expect } from 'chai';
 import FormResult from './FormResult';
 
 describe('FormResult', () => {
@@ -10,7 +9,20 @@ describe('FormResult', () => {
     };
     // unclear why screen variable has to be created instead of imported such as https://testing-library.com/docs/react-testing-library/example-intro#full-example
     const screen = render(<FormResult formState={formState} />);
-    expect(screen.findByText('Please answer all the questions above.')).to.not
-      .be.null;
+    screen.getByText('Please answer all the questions above.');
+  });
+  it('shows pass message', () => {
+    const formState = {
+      status: 'pass',
+    };
+    const screen = render(<FormResult formState={formState} />);
+    screen.getByText('OK to proceed');
+  });
+  it('shows more screening message', () => {
+    const formState = {
+      status: 'more-screening',
+    };
+    const screen = render(<FormResult formState={formState} />);
+    screen.getByText('More screening needed');
   });
 });
