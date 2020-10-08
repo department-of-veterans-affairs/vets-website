4414685746cec457e92b18dc6aafb8e1ad32902d
Matt Roth <roth_matthew@bah.com>
2020-10-08 14:20:30 -0400

    Update AdditionalResources.unit.spec.js

--- src/applications/gi/tests/components/content/AdditionalResources.unit.spec.js
+++ src/applications/gi/tests/components/content/AdditionalResources.unit.spec.js
@@ -13,8 +13,11 @@ describe('<AdditionalResources>', () => {
 
   it('should use non vet tec classes', () => {
     const wrapper = shallow(<AdditionalResources />);
+
     expect(wrapper.find('.additional-resources-responsive')).to.not.be
       .undefined;
+    expect(wrapper.find('AdditionalResourcesLinks').props().vetTec).to.be.false;
+
     wrapper.unmount();
   });
 
@@ -24,6 +27,18 @@ describe('<AdditionalResources>', () => {
     expect(wrapper.find('.vettec-logo-additional-resources')).to.not.be
       .undefined;
     expect(wrapper.find('.additional-resources-vettec')).to.not.be.undefined;
+    expect(wrapper.find('AdditionalResourcesLinks').props().vetTec).to.be.true;
+
+    wrapper.unmount();
+  });
+
+  it('should display header as h2 with h4 font', () => {
+    const wrapper = shallow(<AdditionalResources vetTec />);
+
+    expect(wrapper.find('h2.vads-u-font-size--h4')).to.not.be.undefined;
+    expect(wrapper.find('h2.vads-u-font-size--h4').text()).to.equal(
+      'Additional resources',
+    );
 
     wrapper.unmount();
   });
