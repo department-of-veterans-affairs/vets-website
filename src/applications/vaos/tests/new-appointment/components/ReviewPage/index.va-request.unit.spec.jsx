1ad36177066fb0aacad0c696680ed964e19403e7
Jeff Balboni <jeff@adhocteam.us>
2020-10-09 10:36:18 -0400

    Add review page tests for request flow

---
+++ src/applications/vaos/tests/new-appointment/components/ReviewPage/index.va-request.unit.spec.jsx
@@ -0,0 +1,258 @@
+import React from 'react';
+import moment from 'moment';
+import { expect } from 'chai';
+
+import userEvent from '@testing-library/user-event';
+import { waitFor } from '@testing-library/dom';
+import { Route } from 'react-router-dom';
+
+import {
+  setFetchJSONFailure,
+  mockFetch,
+  resetFetch,
+} from 'platform/testing/unit/helpers';
+import environment from 'platform/utilities/environment';
+
+import { FACILITY_TYPES } from '../../../../utils/constants';
+import {
+  createTestStore,
+  renderWithStoreAndRouter,
+} from '../../../mocks/setup';
+
+import ReviewPage from '../../../../new-appointment/components/ReviewPage';
+import {
+  onCalendarChange,
+  startRequestAppointmentFlow,
+} from '../../../../new-appointment/redux/actions';
+import {
+  mockFacilityFetch,
+  mockMessagesFetch,
+  mockPreferences,
+  mockRequestSubmit,
+} from '../../../mocks/helpers';
+import { getVAFacilityMock } from '../../../mocks/v0';
+
+const initialState = {
+  featureToggles: {
+    vaOnlineSchedulingCancel: true,
+    // eslint-disable-next-line camelcase
+    show_new_schedule_view_appointments_page: true,
+  },
+};
+
+describe('VAOS <ReviewPage> VA request', () => {
+  let store;
+  let start;
+
+  beforeEach(() => {
+    mockFetch();
+    start = moment();
+    store = createTestStore({
+      ...initialState,
+      newAppointment: {
+        pages: {},
+        data: {
+          facilityType: FACILITY_TYPES.VAMC,
+          typeOfCareId: '323',
+          phoneNumber: '1234567890',
+          email: 'joeblow@gmail.com',
+          reasonForAppointment: 'routine-follow-up',
+          reasonAdditionalInfo: 'I need an appt',
+          vaParent: 'var983',
+          vaFacility: 'var983',
+          visitType: 'telehealth',
+          bestTimeToCall: {
+            morning: true,
+            afternoon: true,
+            evening: true,
+          },
+        },
+        clinics: {},
+        parentFacilities: [
+          {
+            id: 'var983',
+            identifier: [
+              { system: 'urn:oid:2.16.840.1.113883.6.233', value: '983' },
+              {
+                system: 'http://med.va.gov/fhir/urn',
+                value: 'urn:va:facility:983',
+              },
+            ],
+          },
+        ],
+        facilityDetails: {
+          var983: {
+            id: 'var983',
+            name: 'Cheyenne VA Medical Center',
+            address: {
+              postalCode: '82001-5356',
+              city: 'Cheyenne',
+              state: 'WY',
+              line: ['2360 East Pershing Boulevard'],
+            },
+          },
+        },
+        facilities: {
+          '323_var983': [
+            {
+              id: 'var983',
+              name: 'Cheyenne VA Medical Center',
+              identifier: [
+                { system: 'urn:oid:2.16.840.1.113883.6.233', value: '983' },
+              ],
+            },
+          ],
+        },
+      },
+    });
+    store.dispatch(startRequestAppointmentFlow());
+    store.dispatch(
+      onCalendarChange({
+        currentlySelectedDate: start.format('YYYY-MM-DD'),
+        selectedDates: [
+          {
+            date: start.format('YYYY-MM-DD'),
+            optionTime: 'AM',
+          },
+        ],
+      }),
+    );
+  });
+  afterEach(() => resetFetch());
+
+  it('should show form information for review', async () => {
+    const screen = renderWithStoreAndRouter(<ReviewPage />, {
+      store,
+    });
+
+    await screen.findByText(/requesting a primary care appointment/i);
+    expect(screen.getByText('Primary care')).to.have.tagName('h2');
+    const [
+      pageHeading,
+      descHeading,
+      typeOfCareHeading,
+      dateHeading,
+      clinicHeading,
+      reasonHeading,
+      visitTypeHeading,
+      contactHeading,
+    ] = screen.getAllByRole('heading');
+    expect(pageHeading).to.contain.text('Review your appointment details');
+    expect(descHeading).to.contain.text(
+      'requesting a primary care appointment',
+    );
+    expect(typeOfCareHeading).to.contain.text('Primary care');
+    expect(screen.baseElement).to.contain.text('VA Appointment');
+
+    expect(dateHeading).to.contain.text('Preferred date and time');
+    expect(screen.baseElement).to.contain.text(
+      `${start.format('MMMM DD, YYYY')} in the morning`,
+    );
+
+    expect(clinicHeading).to.contain.text('Cheyenne VA Medical Center');
+
+    expect(reasonHeading).to.contain.text('Follow-up/Routine');
+    expect(screen.baseElement).to.contain.text('I need an appt');
+
+    expect(visitTypeHeading).to.contain.text('How to be seen');
+    expect(screen.baseElement).to.contain.text(
+      'Telehealth (through VA Video Connect)',
+    );
+
+    expect(contactHeading).to.contain.text('Your contact details');
+    expect(screen.baseElement).to.contain.text('joeblow@gmail.com');
+    expect(screen.baseElement).to.contain.text('1234567890');
+    expect(screen.baseElement).to.contain.text('Call anytime during the day');
+
+    const editLinks = screen.getAllByText(/^Edit/, { selector: 'a' });
+    const uniqueLinks = new Set();
+    editLinks.forEach(link => {
+      expect(link).to.have.attribute('aria-label');
+      uniqueLinks.add(link.getAttribute('aria-label'));
+    });
+    expect(uniqueLinks.size).to.equal(editLinks.length);
+  });
+
+  it('should submit successfully', async () => {
+    const screen = renderWithStoreAndRouter(<Route component={ReviewPage} />, {
+      store,
+    });
+
+    mockRequestSubmit('va', {
+      id: 'fake_id',
+    });
+    mockPreferences(null);
+    mockMessagesFetch('fake_id', {});
+    await screen.findByText(/requesting a primary care appointment/i);
+
+    userEvent.click(screen.getByText(/Request appointment/i));
+    await waitFor(() => {
+      expect(screen.history.push.lastCall.args[0]).to.equal(
+        '/new-appointment/confirmation',
+      );
+    });
+    const submitData = JSON.parse(global.fetch.getCall(0).args[1].body);
+
+    expect(submitData.facility.facilityCode).to.equal('983');
+    expect(submitData.facility.parentSiteCode).to.equal('983');
+    expect(submitData.typeOfCareId).to.equal('323');
+
+    const messageData = JSON.parse(global.fetch.getCall(1).args[1].body);
+    expect(messageData.messageText).to.equal('I need an appt');
+
+    const preferences = JSON.parse(global.fetch.getCall(3).args[1].body);
+    expect(preferences.emailAddress).to.equal('joeblow@gmail.com');
+  });
+
+  it('should show error message on failure', async () => {
+    const screen = renderWithStoreAndRouter(<Route component={ReviewPage} />, {
+      store,
+    });
+
+    mockFacilityFetch('vha_442', {
+      id: 'vha_442',
+      attributes: {
+        ...getVAFacilityMock().attributes,
+        uniqueId: '442',
+        name: 'Cheyenne VA Medical Center',
+        address: {
+          physical: {
+            zip: '82001-5356',
+            city: 'Cheyenne',
+            state: 'WY',
+            address1: '2360 East Pershing Boulevard',
+          },
+        },
+        phone: {
+          main: '307-778-7550',
+        },
+      },
+    });
+    setFetchJSONFailure(
+      global.fetch.withArgs(
+        `${environment.API_URL}/vaos/v0/appointment_requests?type=va`,
+      ),
+      {
+        errors: [{}],
+      },
+    );
+    await screen.findByText(/requesting a primary care appointment/i);
+
+    userEvent.click(screen.getByText(/Request appointment/i));
+
+    await screen.findByText('We couldn’t schedule this appointment');
+
+    expect(screen.baseElement).contain.text(
+      'Something went wrong when we tried to submit your request and you’ll need to start over. We suggest you wait a day',
+    );
+
+    await screen.findByText('307-778-7550');
+
+    // Not sure of a better way to search for test just within the alert
+    const alert = screen.baseElement.querySelector('.usa-alert');
+    expect(alert).contain.text('Cheyenne VA Medical Center');
+    expect(alert).contain.text('2360 East Pershing Boulevard');
+    expect(alert).contain.text('Cheyenne, WY 82001-5356');
+    expect(screen.history.push.called).to.be.false;
+  });
+});
