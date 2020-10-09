3ac67b4aed444078202875aea55c62b6268a4bb8
jhonnyoddball <jhonny.gonzalez@oddball.io>
2020-10-09 08:39:47 -0400

    minor fix on getDrupalProcessed

--- src/site/stages/build/process-cms-exports/transformers/helpers.js
+++ src/site/stages/build/process-cms-exports/transformers/helpers.js
@@ -84,9 +84,9 @@ function getDrupalValue(arr) {
 function getDrupalProcessed(arr) {
   if (arr.length === 0) return null;
   if (arr.length === 1)
-    return typeof arr[0].value === 'string'
+    return typeof arr[0].processed === 'string'
       ? unescapeUnicode(arr[0].processed)
-      : arr[0].value;
+      : arr[0].processed;
   // eslint-disable-next-line no-console
   console.warn(`Unexpected argument: ${arr.toString()}`);
   return null;
