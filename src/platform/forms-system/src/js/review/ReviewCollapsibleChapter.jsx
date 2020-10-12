886825617c70cc450fe6453afa4b087819b4c1b0
Robin Garrison <robin.garrison@adhocteam.us>
2020-10-12 15:32:17 -0500

    Remove unneeded prop

--- src/platform/forms-system/src/js/review/ReviewCollapsibleChapter.jsx
+++ src/platform/forms-system/src/js/review/ReviewCollapsibleChapter.jsx
@@ -233,7 +233,6 @@ class ReviewCollapsibleChapter extends React.Component {
                       pageKey={page.pageKey}
                       pageTitle={page.title}
                       arrayData={_.get(arrayField.path, form.data)}
-                      editing={editing}
                       formData={form.data}
                       appStateData={page.appStateData}
                       formContext={formContext}
