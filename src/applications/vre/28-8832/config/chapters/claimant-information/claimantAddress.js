4140217a28f115bf51653ac466170447a72c6217
Micah Chiang <micahkchiang@gmail.com>
2020-10-12 10:48:39 -0400

    Mc address schema investigation (#14520)
    * first pass
    
    * refactor fields
    
    * revert veteran-address and move changes to new file
    
    * Refactor extraneous properties
    
    * Remove unused imports
    
    * fix tests
    
    * Refactor imports, add jsdoc comments
    
    * remove irrelevant comment
    
    * update street naming convention
    
    * remove unused pattern message

--- src/applications/vre/28-8832/config/chapters/claimant-information/claimantAddress.js
+++ src/applications/vre/28-8832/config/chapters/claimant-information/claimantAddress.js
@@ -1,15 +1,13 @@
 import emailUI from 'platform/forms-system/src/js/definitions/email';
 import {
-  buildAddressSchema,
-  addressUISchema,
-} from 'applications/disability-benefits/686c-674/config/address-schema';
+  addressSchema,
+  addressUiSchema,
+} from 'applications/vre/definitions/profileAddress';
 import { claimantEmailAddress, claimantPhoneNumber } from '../../utilities';
 
-const claimantAddress = buildAddressSchema(true);
-// reset boolean type for checkbox
-claimantAddress.properties['view:livesOnMilitaryBase'] = {
-  type: 'boolean',
-};
+const claimantAddress = addressSchema;
+const checkboxTitle =
+  'I live on a United States military base outside of the U.S.';
 
 export const schema = {
   type: 'object',
@@ -22,13 +20,11 @@ export const schema = {
 };
 
 export const uiSchema = {
-  // Need to overwrite the default title brought over from addressUISchema() because it defaults to "They live..."
-  claimantAddress: {
-    ...addressUISchema(true, 'claimantAddress', () => true),
-    'view:livesOnMilitaryBase': {
-      'ui:title': 'I live on a United States military base outside of the U.S.',
-    },
-  },
+  claimantAddress: addressUiSchema(
+    'claimantAddress',
+    checkboxTitle,
+    () => true,
+  ),
   claimantPhoneNumber: {
     'ui:options': {
       widgetClassNames: 'usa-input-medium',
