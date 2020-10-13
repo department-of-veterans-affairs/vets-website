81ec9ed3d811c5c5613c5c77ee0ac1f5726fb394
Brooks Johnson <brooks.johnson@pm.me>
2020-10-13 09:58:34 -0700

    Check formConfig.disableSave

--- src/platform/forms-system/src/js/review/submit-states/GenericError.jsx
+++ src/platform/forms-system/src/js/review/submit-states/GenericError.jsx
@@ -3,15 +3,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // components
+import ErrorMessage from 'platform/forms/components/common/alerts/ErrorMessage';
 import PreSubmitSection from 'platform/forms/components/review/PreSubmitSection';
 import FormSaveErrorMessage from 'platform/forms/components/review/FormSaveErrorMessage';
 import { Column, Row } from 'platform/forms/components/common/grid';
 // const FormSaveErrorMessage = props => props?.children;
 export default function GenericError(props) {
-  const { formConfig, onSubmit, testId } = props;
+  const { appType, formConfig, onSubmit, testId } = props;
   let submitButton;
+  let submitMessage;
 
-  const submitMessage = <FormSaveErrorMessage formConfig={formConfig} />;
+  if (!formConfig.disableSave) {
+    submitMessage = <FormSaveErrorMessage formConfig={formConfig} />;
+  } else {
+    submitMessage = (
+      <ErrorMessage
+        active
+        title={`We’re sorry, the ${appType} didn’t go through.`}
+        message="You’ll have to start over. We suggest you wait 1 day while we fix
+        this problem."
+      />
+    );
+  }
 
   if (process.env.NODE_ENV !== 'production') {
     submitButton = (
