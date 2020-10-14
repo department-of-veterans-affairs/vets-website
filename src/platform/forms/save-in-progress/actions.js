bfa039b61b35a356b0663af9319faecda7b3ff64
Christopher Valarida <chris.valarida@adhocteam.us>
2020-10-14 14:31:03 -0700

    Remove unused function sanitizeForm

--- src/platform/forms/save-in-progress/actions.js
+++ src/platform/forms/save-in-progress/actions.js
@@ -4,7 +4,7 @@ import recordEvent from '../../monitoring/record-event';
 import { logOut } from '../../user/authentication/actions';
 import environment from '../../utilities/environment';
 import { fetchAndUpdateSessionExpiration as fetch } from '../../utilities/api';
-import { sanitizeForm } from '../helpers';
+// import { sanitizeForm } from '../helpers';
 import { removeFormApi, saveFormApi } from './api';
 import { REMOVING_SAVED_FORM_SUCCESS } from '../../user/profile/actions';
 
