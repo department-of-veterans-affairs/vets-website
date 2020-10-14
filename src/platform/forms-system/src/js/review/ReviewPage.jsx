b52edb4b82eaa92f2c03b9b23299635bc082d641
LinnJS <justinslinn727@gmail.com>
2020-10-14 10:11:24 -0400

    Revert platform code

--- src/platform/forms-system/src/js/review/ReviewPage.jsx
+++ src/platform/forms-system/src/js/review/ReviewPage.jsx
@@ -37,7 +37,6 @@ class ReviewPage extends React.Component {
           formConfig={formConfig}
           pageList={pageList}
           path={path}
-          renderErrorMessage
         />
       </div>
     );
