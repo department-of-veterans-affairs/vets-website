4e5184f0d7ffdf653a6194b5aced332fe6f23e1b
Michael Chelen <michael.chelen@va.gov>
2020-10-14 15:23:16 -0400

    try with async

--- src/applications/coronavirus-screener/components/HelloWorld.unit.spec.jsx
+++ src/applications/coronavirus-screener/components/HelloWorld.unit.spec.jsx
@@ -6,7 +6,7 @@ import App from './HelloWorld';
 // yarn test:unit --path src/applications/coronavirus-screener/components/HelloWorld.unit.spec.jsx
 
 describe('App', () => {
-  it('renders App component', () => {
+  it('renders App component', async () => {
     // does not work with:
     // import { render, screen } from '@testing-library/react';
     // render(<App />);
@@ -15,6 +15,6 @@ describe('App', () => {
     // for some reason screen variable must be created instead of following https://testing-library.com/docs/react-testing-library/example-intro#full-example
     const screen = render(<App />);
     screen.debug();
-    screen.getByText('Hello React');
+    await screen.findByText('Hello React');
   });
 });
