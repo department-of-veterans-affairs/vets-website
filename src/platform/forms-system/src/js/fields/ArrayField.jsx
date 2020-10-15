e8071cafd2514649092b84891a73cda853d1c7f1
Joe <elder_joseph@bah.com>
2020-10-15 16:09:18 -0400

    14639: Update casing for 1995 fields to be up to date with DEPO standards

--- src/platform/forms-system/src/js/fields/ArrayField.jsx
+++ src/platform/forms-system/src/js/fields/ArrayField.jsx
@@ -372,7 +372,7 @@ export default class ArrayField extends React.Component {
             disabled={!this.props.formData || addAnotherDisabled}
             onClick={this.handleAdd}
           >
-            Add Another {uiOptions.itemName}
+            Add another {uiOptions.itemName}
           </button>
           <p>
             {addAnotherDisabled &&
