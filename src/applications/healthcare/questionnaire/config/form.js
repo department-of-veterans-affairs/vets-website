1a895fd87e4f61b94843bee35e8562f174b4394b
Mark Dewey <mdewey@users.noreply.github.com>
2020-10-15 13:40:21 -0400

    Healthcare 13159 review page feedback (#14578)
    * added review page title
    
    * added extra spacing
    
    * added more extra spacing
    
    * addedd trailing space;

--- src/applications/healthcare/questionnaire/config/form.js
+++ src/applications/healthcare/questionnaire/config/form.js
@@ -53,6 +53,7 @@ const formConfig = {
   },
   title: 'Upcoming appointment questionnaire',
   defaultDefinitions: {},
+  customText: { reviewPageTitle: 'Review', appType: 'questionnaire' },
   chapters: {
     chapter1: {
       title: "Veteran's Information",
