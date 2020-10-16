1828cc3f390a287ca744323b4cbc43654d640c3b
Jesse Cohn <jesse.cohn@teamadhoc.us>
2020-10-16 14:08:32 -0700

    fixed unit test

--- src/applications/disability-benefits/view-payments/tests/components/view-payments-lists/Payments/Payments.unit.spec.jsx
+++ src/applications/disability-benefits/view-payments/tests/components/view-payments-lists/Payments/Payments.unit.spec.jsx
@@ -29,15 +29,14 @@ describe('<Payments />', () => {
     const mockEmptyPayments = [];
     const screen = render(
       <Payments
+        tableVersion="received"
         fields={paymentsReceivedFields}
         data={mockEmptyPayments}
         textContent={paymentsReceivedContent}
       />,
     );
 
-    expect(
-      await screen.findByText(/We don’t have a record of returned payments/),
-    ).to.exist;
+    expect(await screen.findByText(/No received payments/)).to.exist;
   });
 
   it('should update the display numbers when paginating', async () => {
