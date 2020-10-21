9c6299fcb06a4df8cc7643afe52312fda75381aa
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-10-21 12:41:41 -0400

    MAKE SURE WE REVERT THISgit add .git add .git add .

--- src/platform/user/profile/utilities/index.js
+++ src/platform/user/profile/utilities/index.js
@@ -127,6 +127,14 @@ export function mapRawUserDataToState(json) {
     userState.vet360 = { status: getErrorStatusDesc(errorStatus) };
   }
 
+  userState.vaPatient = true;
+  userState.isCernerPatient = true;
+
+  userState.facilities = [
+    { facilityId: '668', isCerner: false },
+    { facilityId: '757', isCerner: false },
+  ]
+
   return userState;
 }
 
