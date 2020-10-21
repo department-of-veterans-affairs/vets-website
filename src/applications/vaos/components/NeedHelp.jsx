197a646fbb2f8d37659ce76cf8824084f2adeead
linuxlance <LanceGundersen@users.noreply.github.com>
2020-10-21 12:06:19 -0400

    Update text

--- src/applications/vaos/components/NeedHelp.jsx
+++ src/applications/vaos/components/NeedHelp.jsx
@@ -15,8 +15,8 @@ export default function NeedHelp() {
         className="vads-u-margin-y--1p5 vads-u-border-color--primary"
       />
       <p className="vads-u-margin-top--0">
-        If you need help scheduling a VA or community care appointment tool,
-        please call your VA or community care facility.{' '}
+        If you have questions about using the VA appointments tool, or if the
+        tool isn’t working right, please call{' '}
         <a href="tel:8774705947">877-470-5947</a> (TTY:{' '}
         <Telephone contact={CONTACTS['711']} pattern={PATTERNS['911']} />
         ). We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
