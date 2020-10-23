344efd41de93f1afea95e15507fe65c1cadf76c5
Robin Garrison <robin.garrison@adhocteam.us>
2020-10-23 09:07:14 -0500

    Fix unit test

--- src/platform/forms-system/test/js/definitions/file.unit.spec.jsx
+++ src/platform/forms-system/test/js/definitions/file.unit.spec.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { findDOMNode } from 'react-dom';
 import { expect } from 'chai';
 import ReactTestUtils from 'react-dom/test-utils';
+import { Provider } from 'react-redux';
 
+import { uploadStore } from 'platform/forms-system/test/config/helpers';
 import { DefinitionTester } from 'platform/testing/unit/schemaform-utils.jsx';
 import fileUploadUI, { fileSchema } from '../../../src/js/definitions/file';
 
@@ -10,7 +12,9 @@ describe('Schemaform definition file', () => {
   it('should render file', () => {
     const uiSchema = fileUploadUI('Files');
     const form = ReactTestUtils.renderIntoDocument(
-      <DefinitionTester schema={fileSchema} uiSchema={uiSchema} />,
+      <Provider store={uploadStore}>
+        <DefinitionTester schema={fileSchema} uiSchema={uiSchema} />
+      </Provider>,
     );
 
     const formDOM = findDOMNode(form);
