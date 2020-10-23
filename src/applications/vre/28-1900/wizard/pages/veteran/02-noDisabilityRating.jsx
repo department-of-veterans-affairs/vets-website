da310043a805cd85728bdfc23505c11e43b7b30d
Micah Chiang <micahkchiang@gmail.com>
2020-10-23 12:19:50 -0400

    Move chapter 31 wizard into App container

--- src/applications/vre/28-1900/wizard/pages/veteran/02-noDisabilityRating.jsx
+++ src/applications/vre/28-1900/wizard/pages/veteran/02-noDisabilityRating.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { veteranPathPageNames } from '../pageList';
+import {
+  WIZARD_STATUS,
+  WIZARD_STATUS_INELIGIBLE,
+} from 'applications/vre/28-1900/constants';
 
-const noDisabilityRating = () => (
-  <div className="feature">
-    <p>
-      To apply for VR&E benefits, you must be within 12 years of whichever is
-      later: your date of discharge or the date you received your disability
-      rating.
-    </p>
-  </div>
-);
+const NoDisabilityRating = () => {
+  useEffect(() => {
+    sessionStorage.setItem(WIZARD_STATUS, WIZARD_STATUS_INELIGIBLE);
+  });
+  return (
+    <div className="feature">
+      <p>
+        To apply for VR&E benefits, you must be within 12 years of whichever is
+        later: your date of discharge or the date you received your disability
+        rating.
+      </p>
+    </div>
+  );
+};
 
 export default {
   name: veteranPathPageNames.noDisabilityRating,
-  component: noDisabilityRating,
+  component: NoDisabilityRating,
 };
