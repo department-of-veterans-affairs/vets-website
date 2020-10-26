64fc1b028b3d5dbfce4e698a1ffd1d52bd3c4196
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-10-26 02:02:38 -0400

    Add pagination

--- src/applications/resources-and-support/containers/ResourcesAndSupportSearchApp.jsx
+++ src/applications/resources-and-support/containers/ResourcesAndSupportSearchApp.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import URLSearchParams from 'url-search-params';
 import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
+import Pagination from '@department-of-veterans-affairs/formation-react/Pagination';
+import { focusElement } from 'platform/utilities/ui';
 
 import SearchBar from '../components/SearchBar';
 import SearchResultList from '../components/SearchResultList';
+
+const RESULTS_PER_PAGE = 10;
 
 export default function ResourcesAndSupportSearchApp() {
   const [articles, setArticles] = useState(null);
   const [userInput, setUserInput] = useState('');
   const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
   const [results, setResults] = useState([]);
+
+  const totalPages = results.length / RESULTS_PER_PAGE;
 
   // Load up the article data file
   useEffect(
@@ -43,7 +50,7 @@ export default function ResourcesAndSupportSearchApp() {
     [articles, setUserInput, setQuery],
   );
 
-  // Refresh the results list when the query is submitted
+  // Refresh the results list when the query is submitted or the page is changed.
   useEffect(
     () => {
       if (!articles || !query) {
@@ -55,8 +62,9 @@ export default function ResourcesAndSupportSearchApp() {
       });
 
       setResults(filteredArticles);
+      focusElement('#pagination-summary');
     },
-    [articles, query, setResults],
+    [articles, setResults, query, page],
   );
 
   const onSearch = useCallback(
@@ -67,10 +75,23 @@ export default function ResourcesAndSupportSearchApp() {
       const newUrl = `${window.location.pathname}?${queryParams}`;
       history.replaceState({}, '', newUrl);
 
+      setPage(1);
       setQuery(userInput);
     },
     [userInput, setQuery],
   );
+
+  const onPageSelect = useCallback(
+    selectedPage => {
+      setPage(selectedPage);
+    },
+    [setPage],
+  );
+
+  const startIndex = (page - 1) * RESULTS_PER_PAGE;
+  const endIndex = Math.min(page * RESULTS_PER_PAGE, results.length);
+
+  const currentPageOfResults = results.slice(startIndex, endIndex);
 
   return (
     <div className="usa-grid usa-grid-full">
@@ -84,10 +105,28 @@ export default function ResourcesAndSupportSearchApp() {
                 userInput={userInput}
                 onInputChange={setUserInput}
               />
-              <p>
-                Showing {results.length} results for "<strong>{query}</strong>"
+              <p id="pagination-summary">
+                {results.length > 0 ? (
+                  <>
+                    Showing {startIndex + 1} - {endIndex} of {results.length}{' '}
+                    results for "<strong>{query}</strong>"
+                  </>
+                ) : !query ? (
+                  <>Please enter a query.</>
+                ) : (
+                  <>
+                    No results found for "<strong>{query}</strong>"
+                  </>
+                )}
               </p>
-              <SearchResultList results={results} />
+              <SearchResultList results={currentPageOfResults} />
+              <Pagination
+                maxPageListLength={RESULTS_PER_PAGE}
+                onPageSelect={onPageSelect}
+                page={page}
+                pages={totalPages}
+                showLastPage
+              />
             </>
           ) : (
             <LoadingIndicator message="Please wait while we load the application for you." />
