5bd9c7a481320096223da2e199d7a03e1c2d1f35
Robin Garrison <robin.garrison@adhocteam.us>
2020-10-26 09:44:37 -0500

    Convert to functional component

--- src/platform/forms-system/src/js/utilities/file/ShowPdfPassword.jsx
+++ src/platform/forms-system/src/js/utilities/file/ShowPdfPassword.jsx
@@ -1,59 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ErrorableTextInput from '@department-of-veterans-affairs/formation-react/ErrorableTextInput';
 
-class ShowPdfPassword extends React.Component {
-  state = {
-    // isEncrypted: false,
-    field: {
-      dirty: false,
-      charMax: 255,
-      value: '',
-    },
-  };
+const ShowPdfPassword = ({ file, index, onSubmitPassword }) => {
+  const [fieldObj, setFieldObj] = useState({
+    dirty: false,
+    charMax: 255,
+    value: '',
+  });
 
-  render() {
-    const { file, index, onSubmitPassword } = this.props;
-    const { field } = this.state;
-    const showError = field.dirty && !field.value;
-
-    return (
-      <>
-        <ErrorableTextInput
-          label={'PDF password'}
-          errorMessage={
-            showError && 'Please provide a password to decrypt this file'
+  const showError = fieldObj.dirty && !fieldObj.value;
+  return (
+    <>
+      <ErrorableTextInput
+        label={'PDF password'}
+        errorMessage={
+          showError && 'Please provide a password to decrypt this file'
+        }
+        name={`get_password_${index}`}
+        required
+        field={fieldObj}
+        onValueChange={updatedField => {
+          setFieldObj(updatedField);
+        }}
+      />
+      <button
+        type="button"
+        className="usa-button-primary va-button-primary vads-u-width--auto"
+        onClick={() => {
+          if (fieldObj.value) {
+            onSubmitPassword(file, index, fieldObj.value);
+          } else {
+            setFieldObj({
+              dirty: true,
+              charMax: 255,
+              value: '',
+            });
           }
-          name={`get_password_${index}`}
-          required
-          field={field}
-          onValueChange={updatedField => {
-            this.setState({ field: updatedField });
-          }}
-        />
-        <button
-          type="button"
-          className="usa-button-primary va-button-primary vads-u-width--auto"
-          onClick={() => {
-            if (field.value) {
-              onSubmitPassword(file, index, field.value);
-            } else {
-              this.setState({
-                field: {
-                  dirty: true,
-                  charMax: 255,
-                  value: '',
-                },
-              });
-            }
-          }}
-        >
-          Add password
-        </button>
-        <p />
-      </>
-    );
-  }
-}
+        }}
+      >
+        Add password
+      </button>
+      <p />
+    </>
+  );
+};
 
 const PasswordLabel = () => (
   <p>
