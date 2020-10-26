0f4131cfd1005b1c75f7894ca84b83e227f70c3c
LinnJS <justinslinn727@gmail.com>
2020-10-26 11:10:42 -0400

    Add radio buttons

--- src/applications/caregivers/components/PreSubmitInfo/SignatureCheckbox.jsx
+++ src/applications/caregivers/components/PreSubmitInfo/SignatureCheckbox.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import ErrorableCheckbox from '@department-of-veterans-affairs/formation-react/ErrorableCheckbox';
+import ErrorableRadioButtons from '@department-of-veterans-affairs/formation-react/ErrorableRadioButtons';
 
 import SignatureInput from './SignatureInput';
 
@@ -15,6 +15,13 @@ const SignatureCheckbox = ({
   const [isSigned, setIsSigned] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
   const [hasError, setError] = useState(null);
+  const [certification, setCertification] = useState({
+    value: '',
+    dirty: false,
+  });
+  console.log('label: ', label);
+  console.log('certification: ', certification);
+  console.log('showError: ', showError);
   const isSignatureComplete = isSigned && isChecked;
   const createInputContent = inputLabel => `Enter ${inputLabel} full name`;
 
@@ -52,12 +59,29 @@ const SignatureCheckbox = ({
         showError={showError}
       />
 
-      <ErrorableCheckbox
-        onValueChange={value => setIsChecked(value)}
-        label="I certify the information above is correct and true to the best of my knowledge and belief."
-        errorMessage={hasError && 'Must certify by checking box'}
-        required={isRequired}
-      />
+      {label !== 'Veteran\u2019s' && (
+        <ErrorableRadioButtons
+          onValueChange={value => setCertification({ ...value })}
+          errorMessage={
+            showError && 'You must certify this party before continuing'
+          }
+          label="Select one:"
+          options={[
+            {
+              label:
+                "I certify that I am a member of the Veteran's family (including a parent, spouse, a son or daughter, a step-family member, or an extended family member).",
+              value: 'a-value-like-this',
+            },
+            {
+              label:
+                "I certify that I am not a member of the Veteran's family, and I reside with the Veteran full-time or will do so upon designation as the Veteran's Primary Family Caregiver.",
+              value: 'a-value-like-that',
+            },
+          ]}
+          value={certification}
+          required
+        />
+      )}
     </article>
   );
 };
