31eef23a3572aafaa2164c69670c68da3204be9e
Craig Wheeler <craig@craigwheeler.net>
2020-10-28 16:26:10 -0400

    15163: DebtRes Ch1 - Veteran Information (#14688)

--- src/applications/financial-status-report/containers/App.jsx
+++ src/applications/financial-status-report/containers/App.jsx
@@ -1,12 +1,60 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from '../config/form';
+import { connect } from 'react-redux';
+import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
+import ErrorMessage from '../components/ErrorMessage';
+import { fetchFormStatus } from '../actions/index';
 
-export default function App({ location, children }) {
-  return (
-    <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-      {children}
-    </RoutedSavableApp>
+const App = ({
+  location,
+  children,
+  isError,
+  pending,
+  isLoggedIn,
+  getFormStatus,
+}) => {
+  const showMainContent = !pending && !isError;
+
+  useEffect(
+    () => {
+      getFormStatus();
+    },
+    [getFormStatus],
   );
-}
+
+  return (
+    <>
+      {pending && (
+        <LoadingIndicator setFocus message="Loading your information..." />
+      )}
+      {isError &&
+        !pending &&
+        isLoggedIn && (
+          <div className="row vads-u-margin-bottom--3">
+            <ErrorMessage />
+          </div>
+        )}
+      {showMainContent && (
+        <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+          {children}
+        </RoutedSavableApp>
+      )}
+    </>
+  );
+};
+
+const mapStateToProps = state => ({
+  isLoggedIn: state.user.login.currentlyLoggedIn,
+  isError: state.fsr.isError,
+  pending: state.fsr.pending,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getFormStatus: () => dispatch(fetchFormStatus()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
