493858f6eabbb63bbb3e56add126b7bc42ace821
Jesse Cohn <jesse.cohn@teamadhoc.us>
2020-10-29 10:52:26 -0700

    Added content for user included in flipper

--- src/applications/static-pages/vre-chapter31/Chapter31CTA.js
+++ src/applications/static-pages/vre-chapter31/Chapter31CTA.js
@@ -1,10 +1,7 @@
 import React from 'react';
-
 import { connect } from 'react-redux';
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
 import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
-
-import CallToActionWidget from 'platform/site-wide/cta-widget';
 import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
 
 import { buildChapter31Content } from './buildChapter31Content';
@@ -12,12 +9,27 @@ import { buildChapter31Content } from './buildChapter31Content';
 const Chapter31CTA = props => {
   let content;
   const currentURL = window.location.href.split('/');
+  // If the page is loading, show a loading indicator
+  // If the user is NOT included in flipper, build the content for the page based on parts of the URL
+  // else if the user IS included in flipper, show them the button that leads to the Chapter 31 form on VA.gov
   if (props.includedInFlipper === undefined) {
     content = <LoadingIndicator message="Loading..." />;
   } else if (props.includedInFlipper === false) {
-    content = buildChapter31Content(currentURL[5]);
+    content = buildChapter31Content(currentURL[5], currentURL[5]);
   } else {
-    content = <div>Hey</div>;
+    content = (
+      <>
+        <p className="vads-u-font-size--xl vads-u-font-family--serif vads-u-margin-bottom--0 vads-u-font-weight--bold">
+          Ready to apply?
+        </p>
+        <a
+          href="/careers-employment/vocational-rehabilitation/apply"
+          className="usa-button-primary va-button-primary"
+        >
+          Apply for Veteran Readiness and Employment
+        </a>
+      </>
+    );
   }
   return <div>{content}</div>;
 };
