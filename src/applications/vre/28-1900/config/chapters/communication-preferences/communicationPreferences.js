5b04b4f8a9a3da86bbd7a12b2460db694df6038b
Micah Chiang <micahkchiang@gmail.com>
2020-10-29 13:13:48 -0400

    address WAVE warning

--- src/applications/vre/28-1900/config/chapters/communication-preferences/communicationPreferences.js
+++ src/applications/vre/28-1900/config/chapters/communication-preferences/communicationPreferences.js
@@ -82,7 +82,7 @@ export const uiSchema = {
   },
   appointmentTimePreferences: {
     'ui:title': (
-      <p className="vads-u-margin--0 vads-u-margin-top--3 vads-u-display--inline-block vads-u-font-weight--normal vads-u-color--base vads-u-font-family--sans vads-u-font-size--base">
+      <p className="vads-u-margin--0 vads-u-display--inline-block vads-u-font-weight--normal vads-u-color--base vads-u-font-family--sans vads-u-font-size--base">
         When are the best times to meet with your VR&E counselor?{' '}
         <span className="schemaform-required-span vads-u-display--block">
           (*At lease one required)
@@ -91,9 +91,9 @@ export const uiSchema = {
     ),
     'ui:validations': [validateAtLeastOneSelected],
     'ui:options': {
-      // this needed because ObjectField adds the schemaform-block class to this, which creates a huge top margin.
-      classNames: 'vads-u-margin-top--neg2',
-      showFieldLabel: true,
+      // negative margin is needed because ObjectField adds the schemaform-block class to this, which creates a huge top margin.
+      classNames: 'vads-u-margin-top--neg3',
+      showFieldLabel: false,
     },
     morning: {
       'ui:title': 'Mornings 6:00 to 10:00 a.m.',
