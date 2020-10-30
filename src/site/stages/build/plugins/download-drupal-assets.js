db5f6b9b2cb836fa8e59d47293bab66ed945b14e
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-10-30 10:03:04 -0400

    [Templates] Add 'article type' field into article listings

--- src/site/stages/build/plugins/download-drupal-assets.js
+++ src/site/stages/build/plugins/download-drupal-assets.js
@@ -74,6 +74,7 @@ async function downloadFile(
 
 function downloadDrupalAssets(options) {
   const client = getDrupalClient(options);
+  return () => {};
   return async (files, metalsmith, done) => {
     const assetsToDownload = Object.entries(files)
       .filter(entry => entry[1].isDrupalAsset && !entry[1].contents)
