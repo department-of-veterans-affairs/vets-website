9b7566b01241d23353360f1686ef6775a08b396d
Kevin Lee <kevin.lee@adhocteam.us>
2020-10-29 19:48:20 -0700

    Remove FAQ text

--- src/applications/third-party-app-directory/components/AppDirectory/index.jsx
+++ src/applications/third-party-app-directory/components/AppDirectory/index.jsx
@@ -16,19 +16,6 @@ export const AppDirectory = () => (
 
     {/* App list */}
     <ThirdPartyAppList />
-
-    {/* Questions Content */}
-    <div className="feature">
-      <h3>Have questions about connected apps?</h3>
-      <p>
-        Get answers to frequently asked questions about how connected
-        third-party apps work, what types of information they can see, and the
-        benefits of sharing your information.
-      </p>
-      <a href="" rel="noopener noreferrer">
-        Go to connected apps FAQs
-      </a>
-    </div>
   </div>
 );
 
