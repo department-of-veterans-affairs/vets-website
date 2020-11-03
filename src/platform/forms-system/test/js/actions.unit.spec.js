7e378901e02d520029f748b2920d75e929216e51
Robin Garrison <robin.garrison@adhocteam.us>
2020-11-03 15:09:55 -0600

    Fix unit test

--- src/platform/forms-system/test/js/actions.unit.spec.js
+++ src/platform/forms-system/test/js/actions.unit.spec.js
@@ -437,6 +437,7 @@ describe('Schemaform actions:', () => {
         name: 'Test name',
         size: 1234,
         confirmationCode: 'Test code',
+        isEncrypted: false,
       });
     });
 
