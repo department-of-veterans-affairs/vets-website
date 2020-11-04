9be1b6c6682629a377f4c77178e8616e8c175548
Brad Conley <brad.conl3y@gmail.com>
2020-11-04 15:04:50 -0500

    checkboxes working

--- src/applications/financial-status-report/components/DebtCard.jsx
+++ src/applications/financial-status-report/components/DebtCard.jsx
@@ -1,62 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import head from 'lodash/head';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import { deductionCodes } from '../../debt-letters/const/deduction-codes';
+import { setData } from 'platform/forms-system/src/js/actions';
 import classnames from 'classnames';
 
-const DebtCard = ({ debt, key }) => {
-  const mostRecentHistory = head(debt.debtHistory);
-  const debtCardHeading =
-    deductionCodes[debt.deductionCode] || debt.benefitType;
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  });
+class DebtCard extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="vads-u-background-color--gray-lightest vads-u-padding--3 vads-u-margin-bottom--2">
-      <h3 className="vads-u-font-size--h4 vads-u-margin--0">
-        {debtCardHeading}
-      </h3>
-      {mostRecentHistory && (
-        <p className="vads-u-margin-top--1 vads-u-margin-bottom--0">
-          Updated on {moment(mostRecentHistory.date).format('MMMM D, YYYY')}
+    this.state = {
+      fsrDebts: [],
+    };
+  }
+
+  onChange(debtTitle) {
+    const alreadyIncluded = this.props?.fsrDebts?.includes(debtTitle);
+    if (alreadyIncluded) {
+      const fsrDebts = this.props?.fsrDebts?.filter(
+        debtEntry => debtEntry !== debtTitle,
+      );
+      return this.props.setData({ ...this.props.formData, fsrDebts });
+    } else {
+      const newFsrDebts = this.props.fsrDebts.length
+        ? [...this.props.fsrDebts, debtTitle]
+        : [debtTitle];
+      return this.props.setData({
+        ...this.props.formData,
+        fsrDebts: newFsrDebts,
+      });
+    }
+  }
+
+  render() {
+    const { debt } = this.props;
+    const mostRecentHistory = head(debt.debtHistory);
+    const debtCardHeading =
+      deductionCodes[debt.deductionCode] || debt.benefitType;
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    });
+
+    return (
+      <div className="vads-u-background-color--gray-lightest vads-u-padding--3 vads-u-margin-bottom--2">
+        <h3 className="vads-u-font-size--h4 vads-u-margin--0">
+          {debtCardHeading}
+        </h3>
+        {mostRecentHistory && (
+          <p className="vads-u-margin-top--1 vads-u-margin-bottom--0">
+            Updated on {moment(mostRecentHistory.date).format('MMMM D, YYYY')}
+          </p>
+        )}
+        <p className="vads-u-margin-y--2 vads-u-font-size--md vads-u-font-family--sans">
+          <strong>Amount owed: </strong>
+          {debt.currentAr && formatter.format(parseFloat(debt.currentAr))}
         </p>
-      )}
-      <p className="vads-u-margin-y--2 vads-u-font-size--md vads-u-font-family--sans">
-        <strong>Amount owed: </strong>
-        {debt.currentAr && formatter.format(parseFloat(debt.currentAr))}
-      </p>
-      <p className="vads-u-margin-y--2 vads-u-font-size--md vads-u-font-family--sans">
-        <strong>Status: </strong>
-        {debt.diaryCodeDescription}
-      </p>
+        <p className="vads-u-margin-y--2 vads-u-font-size--md vads-u-font-family--sans">
+          <strong>Status: </strong>
+          {debt.diaryCodeDescription}
+        </p>
 
-      <div className="vads-u-margin-top--2">
-        <input
-          id={key}
-          type="checkbox"
-          className=" vads-u-width--auto"
-          checked={false}
-          onChange={() => null}
-        />
-        <label
-          className={classnames({
-            'usa-button vads-u-font-weight--bold vads-u-border--2px vads-u-border-color--primary vads-u-margin-bottom--0 vads-u-width--auto vads-u-text-align--left vads-u-padding-x--2': true,
-            'vads-u-color--white': false,
-            'vads-u-background-color--white vads-u-color--primary': true,
-          })}
-          htmlFor={key}
-        >
-          Request assistance for this debt
-        </label>
+        <div className="vads-u-margin-top--2">
+          <input
+            id={debt.diaryCodeDescription}
+            type="checkbox"
+            className=" vads-u-width--auto"
+            checked={this.props.fsrDebts.includes(debtCardHeading)}
+            onChange={() => this.onChange(debtCardHeading)}
+          />
+          <label
+            className={classnames({
+              'usa-button vads-u-font-weight--bold vads-u-border--2px vads-u-border-color--primary vads-u-margin-bottom--0 vads-u-width--auto vads-u-text-align--left vads-u-padding-x--2': true,
+              'vads-u-color--white': false,
+              'vads-u-background-color--white vads-u-color--primary': true,
+            })}
+            htmlFor={debt.diaryCodeDescription}
+          >
+            Request assistance for this debt
+          </label>
+        </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 DebtCard.propTypes = {
   debt: PropTypes.shape({
     currentAr: PropTypes.number,
@@ -77,6 +107,19 @@ DebtCard.defaultProps = {
     deductionCode: '',
     originalAr: 0,
   },
+  fsrDebts: [],
 };
 
-export default DebtCard;
+const mapStateToProps = state => ({
+  formData: state.form.data,
+  fsrDebts: state.form.data.fsrDebts,
+});
+
+const mapDispatchToProps = {
+  setData,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(DebtCard);
