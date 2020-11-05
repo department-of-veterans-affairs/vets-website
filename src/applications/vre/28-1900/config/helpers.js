d37bc7869b59ff6df1f361f29a47f077e379b7a2
Micah Chiang <micahkchiang@gmail.com>
2020-11-05 08:55:57 -0500

    wrap payload

--- src/applications/vre/28-1900/config/helpers.js
+++ src/applications/vre/28-1900/config/helpers.js
@@ -1,3 +1,14 @@
+import { transformForSubmit } from 'platform/forms-system/src/js/helpers';
+
 export const isFieldRequired = (formData, requiredProp) => {
   return formData[requiredProp];
+};
+
+export const transform = (formConfig, form) => {
+  const formData = transformForSubmit(formConfig, form);
+  return JSON.stringify({
+    veteranReadinessEmploymentClaim: {
+      form: formData,
+    },
+  });
 };
