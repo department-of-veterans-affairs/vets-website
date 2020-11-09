fcd9289177566ed8cda498aba6fad1a729e9bf44
Brad Conley <brad.conl3y@gmail.com>
2020-11-09 14:02:58 -0500

    import to form config

--- src/applications/financial-status-report/pages/availableDebts.js
+++ src/applications/financial-status-report/pages/availableDebts.js
@@ -1,2 +1,16 @@
-export const uiSchema = {};
-export const schema = {};
+import AvailableDebts from '../components/AvailableDebts';
+
+export const uiSchema = {
+  'ui:title': 'Available Debts',
+  'ui:description': '',
+  'ui:widget': AvailableDebts,
+};
+
+export const schema = {
+  type: 'object',
+  properties: {
+    fsrDebts: {
+      type: 'array',
+    },
+  },
+};
