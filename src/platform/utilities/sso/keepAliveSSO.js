bd688d934a908254521208445cf6cbd9748a5234
Alexander Garcia <agarcia@clarityinnovates.com>
2020-11-10 15:54:00 -0500

    adds unit tests from eric b.; checks null for alive

--- src/platform/utilities/sso/keepAliveSSO.js
+++ src/platform/utilities/sso/keepAliveSSO.js
@@ -15,15 +15,22 @@ export default async function keepAlive() {
     const resp = await fetch(ssoKeepAliveEndpoint(), {
       method: 'HEAD',
       credentials: 'include',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
       cache: 'no-store',
     });
-    const alive = Boolean(resp.headers.get('session-alive')) === true;
+
+    const alive = resp.headers.get('session-alive');
+
+    if (alive === null) {
+      // session-alive response header is missing, possibly because the
+      // browser failed to send the "Origin" request header.  with missing
+      // response headers, we can't determine if the user is logged in or
+      // logged out, so just return back an empty response (the same that
+      // would be returned in the event of an error)
+      return {};
+    }
+
     return {
-      ttl: alive ? Number(resp.headers.get('session-timeout')) : 0,
+      ttl: alive === 'true' ? Number(resp.headers.get('session-timeout')) : 0,
       transactionid: resp.headers.get('va_eauth_transactionid'),
       // for DSLogon or mhv, use a mapped authn context value, however for
       // idme, we need to use the provided authncontextclassref as it could be
