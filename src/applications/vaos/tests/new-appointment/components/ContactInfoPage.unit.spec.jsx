8209c9f7acf3470631ea162c1c1f3d1993280103
Tony Williams <tony.williams@va.gov>
2020-11-10 13:39:42 -0600

    Modified per code review.

--- src/applications/vaos/tests/new-appointment/components/ContactInfoPage.unit.spec.jsx
+++ src/applications/vaos/tests/new-appointment/components/ContactInfoPage.unit.spec.jsx
@@ -1,42 +1,55 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { expect } from 'chai';
-import sinon from 'sinon';
 import userEvent from '@testing-library/user-event';
-import { waitFor } from '@testing-library/dom';
 
-import { ContactInfoPage } from '../../../new-appointment/components/ContactInfoPage';
-import { renderWithStoreAndRouter } from '../../mocks/setup';
+import ContactInfoPage from '../../../new-appointment/components/ContactInfoPage';
+import { createTestStore, renderWithStoreAndRouter } from '../../mocks/setup';
+import { FETCH_STATUS } from '../../../utils/constants';
 
 describe('VAOS <ContactInfoPage>', () => {
   it('should render', async () => {
-    const openFormPage = sinon.spy();
-    const updateFormData = sinon.spy();
+    const store = createTestStore({
+      newAppointment: {
+        pages: [],
+        previousPages: [],
+      },
+    });
 
-    const screen = renderWithStoreAndRouter(
-      <ContactInfoPage
-        openFormPage={openFormPage}
-        updateFormData={updateFormData}
-        data={{}}
-      />,
-      {},
-    );
+    const screen = renderWithStoreAndRouter(<ContactInfoPage />, {
+      store,
+    });
+
     expect(await screen.findByText('Your contact information')).to.be.ok;
   });
 
   it('should not submit empty form', async () => {
-    const openFormPage = sinon.spy();
-    const history = {
-      push: sinon.spy(),
-    };
+    const store = createTestStore({
+      newAppointment: {
+        data: {
+          calendarData: {},
+        },
+        eligibility: [],
+        pages: [],
+        previousPages: [],
+        appointmentSlotsStatus: FETCH_STATUS.succeeded,
+      },
+    });
 
     const screen = renderWithStoreAndRouter(
-      <ContactInfoPage
-        openFormPage={openFormPage}
-        history={history}
-        data={{}}
-      />,
-      {},
+      <Route component={ContactInfoPage} />,
+      {
+        store,
+      },
     );
+
+    // it should display page heading
+    expect(
+      await screen.findByRole('heading', {
+        level: 1,
+        name: /Your contact information/i,
+      }),
+    ).to.be.ok;
 
     const button = await screen.findByText(/^Continue/);
     userEvent.click(button);
@@ -45,70 +58,73 @@ describe('VAOS <ContactInfoPage>', () => {
     expect(screen.findByText(/^Please choose at least one option/)).to.be.ok;
     expect(screen.findByText(/^Please provide a response/)).to.be.ok;
 
-    expect(history.push.called).to.be.false;
+    userEvent.click(button);
+    expect(screen.history.push.called).to.be.false;
   });
 
   it('should call updateFormData after change', async () => {
-    const openFormPage = sinon.spy();
-    const updateFormData = sinon.spy();
-    const history = {
-      push: sinon.spy(),
-    };
+    const store = createTestStore({
+      newAppointment: {
+        pages: [],
+        previousPages: [],
+      },
+    });
 
-    renderWithStoreAndRouter(
-      <ContactInfoPage
-        openFormPage={openFormPage}
-        updateFormData={updateFormData}
-        history={history}
-        data={{}}
-      />,
-      {},
-    );
+    const screen = renderWithStoreAndRouter(<ContactInfoPage />, {
+      store,
+    });
+
+    // it should display page heading
+    expect(
+      await screen.findByRole('heading', {
+        level: 1,
+        name: /Your contact information/i,
+      }),
+    ).to.be.ok;
 
     const input = document.getElementById('root_phoneNumber');
+    expect(input.value).to.equal('');
+
     userEvent.type(input, '5555555555');
 
-    await waitFor(() => {
-      expect(updateFormData.callCount).to.equal(10);
-    });
+    expect(input.value).to.equal('5555555555');
   });
 
   it('should submit with valid data', async () => {
-    const openFormPage = sinon.spy();
-    const routeToNextAppointmentPage = sinon.spy();
+    const store = createTestStore({
+      newAppointment: {
+        pages: [],
+        previousPages: [],
+      },
+    });
 
     const screen = renderWithStoreAndRouter(
-      <ContactInfoPage
-        openFormPage={openFormPage}
-        routeToNextAppointmentPage={routeToNextAppointmentPage}
-        data={{
-          phoneNumber: '5555555555',
-          email: 'fake@va.gov',
-          bestTimeToCall: {
-            morning: true,
-          },
-        }}
-      />,
-      {},
+      <Route component={ContactInfoPage} />,
+      {
+        store,
+      },
     );
+
+    // it should display page heading
+    expect(
+      await screen.findByRole('heading', {
+        level: 1,
+        name: /Your contact information/i,
+      }),
+    ).to.be.ok;
+
+    let input = document.getElementById('root_phoneNumber');
+    userEvent.type(input, '5555555555');
+
+    const checkbox = document.getElementById('root_bestTimeToCall_morning');
+    userEvent.click(checkbox);
+
+    input = document.getElementById('root_email');
+    userEvent.type(input, 'joe.blow@gmail.com');
 
     const button = await screen.findByText(/^Continue/);
+
     userEvent.click(button);
-
-    expect(routeToNextAppointmentPage.called).to.be.true;
-  });
-
-  it('document title should match h1 text', async () => {
-    const openFormPage = sinon.spy();
-    const pageTitle = 'Your contact information';
-
-    const screen = renderWithStoreAndRouter(
-      <ContactInfoPage openFormPage={openFormPage} data={{}} />,
-      {},
-    );
-
-    expect(await screen.findByRole('heading', { level: 1, name: pageTitle })).to
-      .be.ok;
-    expect(document.title).to.equal(`${pageTitle} | Veterans Affairs`);
+    expect(screen.history.push.called).to.be.true;
   });
 });
