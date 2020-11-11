8aa5171d2c8934975fd4fcbfe67985691fb89d75
Brooks Johnson <brooks.johnson@pm.me>
2020-11-10 16:47:15 -0800

    Try to lazy load the right form config for edu

--- src/applications/edu-benefits/components/createEducationApplicationStatus.jsx
+++ src/applications/edu-benefits/components/createEducationApplicationStatus.jsx
@@ -13,16 +13,39 @@ const eduForms = new Set([
   VA_FORM_IDS.FORM_22_1990N,
 ]);
 
+const formConfigMap = new Map([
+  [eduForms[0], '../0994/config/form'],
+  [eduForms[1], '../1990/config/form'],
+  [eduForms[2], '../1995/config/form'],
+  [eduForms[3], '../5490/config/form'],
+  [eduForms[4], '../5495/config/form'],
+  [eduForms[5], '../1990e/config/form'],
+  [eduForms[6], '../1990n/config/form'],
+]);
+
 export default function createEducationApplicationStatus(store, widgetType) {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   if (root) {
-    import(/* webpackChunkName: "education-application-status" */
-    '../utils/educationStatus').then(module => {
-      const { ApplicationStatus, EducationWizard } = module.default;
+    const state = store.getState();
+    const savedForms = state.user.profile.savedForms;
+    const matchingForms = savedForms.filter(({ form }) => eduForms.has(form));
+    const inProgressFormId = matchingForms.length
+      ? matchingForms.sort(({ metadata }) => -1 * metadata.lastUpdated)[0].form
+      : null;
+
+    const configPath = formConfigMap[inProgressFormId];
+    Promise.all([
+      import(/* webpackChunkName: "education-application-status" */
+      '../utils/educationStatus'),
+      (!!configPath && import(configPath)) || null,
+    ]).then(([appStatusModule, formConfigModule]) => {
+      const { ApplicationStatus, EducationWizard } = appStatusModule.default;
+      const formConfig = formConfigModule?.default || {};
       ReactDOM.render(
         <Provider store={store}>
           <ApplicationStatus
             formIds={eduForms}
+            formConfig={formConfig}
             formType="education"
             showApplyButton={
               root.getAttribute('data-hide-apply-button') === null
