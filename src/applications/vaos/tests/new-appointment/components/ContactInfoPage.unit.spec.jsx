ca0954e15113d1d5e656994e14e8b73e304e06c0
Tony Williams <tony.williams@va.gov>
2020-11-12 10:02:49 -0600

    Refactored per code review.

--- src/applications/vaos/tests/new-appointment/components/ContactInfoPage.unit.spec.jsx
+++ src/applications/vaos/tests/new-appointment/components/ContactInfoPage.unit.spec.jsx
@@ -6,9 +6,10 @@ import userEvent from '@testing-library/user-event';
 import ContactInfoPage from '../../../new-appointment/components/ContactInfoPage';
 import { createTestStore, renderWithStoreAndRouter } from '../../mocks/setup';
 import { FETCH_STATUS } from '../../../utils/constants';
+import { cleanup } from 'axe-core';
 
 describe('VAOS <ContactInfoPage>', () => {
-  it('should render', async () => {
+  it('should submit with valid data', async () => {
     const store = createTestStore({
       newAppointment: {
         pages: [],
@@ -16,12 +17,44 @@ describe('VAOS <ContactInfoPage>', () => {
       },
     });
 
-    const screen = renderWithStoreAndRouter(<ContactInfoPage />, {
-      store,
-    });
+    let screen = renderWithStoreAndRouter(
+      <Route component={ContactInfoPage} />,
+      {
+        store,
+      },
+    );
 
     // it should display page heading
     expect(await screen.findByText('Your contact information')).to.be.ok;
+
+    let input = screen.getByLabelText(/^Your phone number/);
+    userEvent.type(input, '5555555555');
+
+    let checkbox = screen.getByLabelText(/^Morning \(8 a.m. – noon\)/);
+    userEvent.click(checkbox);
+
+    input = screen.getByLabelText(/^Your email address/);
+    userEvent.type(input, 'joe.blow@gmail.com');
+
+    const button = await screen.findByText(/^Continue/);
+
+    userEvent.click(button);
+    expect(screen.history.push.called).to.be.true;
+
+    // Expect the previously entered form data is still there if you unmount and remount the page with the same store, 
+    await cleanup();
+    screen = renderWithStoreAndRouter(<Route component={ContactInfoPage} />, {
+      store,
+    });
+
+    input = await screen.findByLabelText(/^Your phone number/);
+    expect(input.value).to.equal('5555555555');
+
+    checkbox = screen.getByLabelText(/^Morning \(8 a.m. – noon\)/);
+    expect(checkbox.checked).to.be.true;
+
+    input = screen.getByLabelText(/^Your email address/);
+    expect(input.value).to.equal('joe.blow@gmail.com');
   });
 
   it('should not submit empty form', async () => {
@@ -47,70 +80,14 @@ describe('VAOS <ContactInfoPage>', () => {
     // it should display page heading
     expect(await screen.findByText('Your contact information')).to.be.ok;
 
-    const button = await screen.findByText(/^Continue/);
+    const button = screen.getByText(/^Continue/);
     userEvent.click(button);
 
-    expect(screen.findByText(/^Please enter a phone number/)).to.be.ok;
-    expect(screen.findByText(/^Please choose at least one option/)).to.be.ok;
-    expect(screen.findByText(/^Please provide a response/)).to.be.ok;
+    expect(await screen.findByText(/^Please enter a phone number/)).to.be.ok;
+    expect(screen.getByText(/^Please choose at least one option/)).to.be.ok;
+    expect(screen.getByText(/^Please provide a response/)).to.be.ok;
 
     userEvent.click(button);
     expect(screen.history.push.called).to.be.false;
-  });
-
-  it('should call updateFormData after change', async () => {
-    const store = createTestStore({
-      newAppointment: {
-        pages: [],
-        previousPages: [],
-      },
-    });
-
-    const screen = renderWithStoreAndRouter(<ContactInfoPage />, {
-      store,
-    });
-
-    // it should display page heading
-    expect(await screen.findByText('Your contact information')).to.be.ok;
-
-    const input = document.getElementById('root_phoneNumber');
-    expect(input.value).to.equal('');
-
-    userEvent.type(input, '5555555555');
-
-    expect(input.value).to.equal('5555555555');
-  });
-
-  it('should submit with valid data', async () => {
-    const store = createTestStore({
-      newAppointment: {
-        pages: [],
-        previousPages: [],
-      },
-    });
-
-    const screen = renderWithStoreAndRouter(
-      <Route component={ContactInfoPage} />,
-      {
-        store,
-      },
-    );
-
-    // it should display page heading
-    expect(await screen.findByText('Your contact information')).to.be.ok;
-
-    let input = document.getElementById('root_phoneNumber');
-    userEvent.type(input, '5555555555');
-
-    const checkbox = document.getElementById('root_bestTimeToCall_morning');
-    userEvent.click(checkbox);
-
-    input = document.getElementById('root_email');
-    userEvent.type(input, 'joe.blow@gmail.com');
-
-    const button = await screen.findByText(/^Continue/);
-
-    userEvent.click(button);
-    expect(screen.history.push.called).to.be.true;
   });
 });
