76f8d128c1eb7737e92bd032c89ed80d542afdb5
LinnJS <justinslinn727@gmail.com>
2020-11-17 15:35:17 -0500

    Update signature input data to reflect optional primaty

--- src/applications/caregivers/components/PreSubmitInfo/index.jsx
+++ src/applications/caregivers/components/PreSubmitInfo/index.jsx
@@ -8,12 +8,12 @@ const PreSubmitCheckboxGroup = ({ onSectionComplete, formData, showError }) => {
   const primaryLabel = `Primary Family Caregiver applicant\u2019s`;
   const secondaryOneLabel = `Secondary Family Caregiver applicant\u2019s`;
   const secondaryTwoLabel = `Secondary Family Caregiver (2) applicant\u2019s`;
+  const hasPrimary = formData['view:hasPrimaryCaregiverView'];
   const hasSecondaryOne = formData['view:hasSecondaryCaregiverOne'];
   const hasSecondaryTwo = formData['view:hasSecondaryCaregiverTwo'];
 
   const [signatures, setSignatures] = useState({
     [veteranLabel]: false,
-    [primaryLabel]: false,
   });
 
   const unSignedLength = Object.values(signatures).filter(
@@ -37,6 +37,14 @@ const PreSubmitCheckboxGroup = ({ onSectionComplete, formData, showError }) => {
 
   useEffect(
     () => {
+      if (!hasPrimary) {
+        setSignatures(prevState => {
+          const newState = cloneDeep(prevState);
+          delete newState[primaryLabel];
+          return newState;
+        });
+      }
+
       if (!hasSecondaryOne) {
         setSignatures(prevState => {
           const newState = cloneDeep(prevState);
@@ -54,7 +62,14 @@ const PreSubmitCheckboxGroup = ({ onSectionComplete, formData, showError }) => {
       }
     },
 
-    [hasSecondaryOne, hasSecondaryTwo, secondaryOneLabel, secondaryTwoLabel],
+    [
+      hasPrimary,
+      hasSecondaryOne,
+      hasSecondaryTwo,
+      secondaryOneLabel,
+      secondaryTwoLabel,
+      primaryLabel,
+    ],
   );
 
   const PrivacyPolicy = () => (
@@ -114,7 +129,7 @@ const PreSubmitCheckboxGroup = ({ onSectionComplete, formData, showError }) => {
 
   /*
     - Vet first && last name must match, and be checked
-    - PrimaryCaregiver first && last name must match, and be checked
+    - if hasPrimaryCaregiver first && last name must match, and be checked
     - if hasSecondary one || two, first & last name must match, and be checked to submit
    */
 
@@ -143,49 +158,52 @@ const PreSubmitCheckboxGroup = ({ onSectionComplete, formData, showError }) => {
         <PrivacyPolicy />
       </SignatureCheckbox>
 
-      <SignatureCheckbox
-        fullName={formData.primaryFullName}
-        label={primaryLabel}
-        signatures={signatures}
-        setSignature={setSignatures}
-        isRequired
-        showError={showError}
-      >
-        <h3 className="vads-u-margin-top--4">
-          Primary Family Caregiver applicant&apos;s statement of truth
-        </h3>
+      {hasPrimary && (
+        <SignatureCheckbox
+          fullName={formData.primaryFullName}
+          label={primaryLabel}
+          signatures={signatures}
+          setSignature={setSignatures}
+          isRequired
+          showError={showError}
+        >
+          <h3 className="vads-u-margin-top--4">
+            Primary Family Caregiver applicant&apos;s statement of truth
+          </h3>
 
-        <p className="vads-u-margin-y--4">
-          I certify that I am at least 18 years of age.
-        </p>
+          <p className="vads-u-margin-y--4">
+            I certify that I am at least 18 years of age.
+          </p>
 
-        <p>
-          I certify that I am a family member of the Veteran named in this
-          application or I reside with the Veteran, or will do so upon
-          designation as the Veteran's Primary Family Caregiver.
-        </p>
+          <p>
+            I certify that I am a family member of the Veteran named in this
+            application or I reside with the Veteran, or will do so upon
+            designation as the Veteran's Primary Family Caregiver.
+          </p>
 
-        <p>
-          I agree to perform personal care services as the Primary Family
-          Caregiver for the Veteran named on this application.
-        </p>
+          <p>
+            I agree to perform personal care services as the Primary Family
+            Caregiver for the Veteran named on this application.
+          </p>
 
-        <p>
-          I understand that the Veteran or Veteran’s surrogate may request my
-          discharge from the Program of Comprehensive Assistance for Family
-          Caregivers (PCAFC) at any time. I understand that my designation as a
-          Primary Family Caregiver may be revoked or I may be discharged from
-          the program by the Secretary of Veterans Affairs or his designee, as
-          set forth in 38 CFR 71.45.
-        </p>
+          <p>
+            I understand that the Veteran or Veteran’s surrogate may request my
+            discharge from the Program of Comprehensive Assistance for Family
+            Caregivers (PCAFC) at any time. I understand that my designation as
+            a Primary Family Caregiver may be revoked or I may be discharged
+            from the program by the Secretary of Veterans Affairs or his
+            designee, as set forth in 38 CFR 71.45.
+          </p>
 
-        <p>
-          I understand that participation in PCAFC does not create an employment
-          relationship between me and the Department of Veterans Affairs.
-        </p>
+          <p>
+            I understand that participation in PCAFC does not create an
+            employment relationship between me and the Department of Veterans
+            Affairs.
+          </p>
 
-        <PrivacyPolicy />
-      </SignatureCheckbox>
+          <PrivacyPolicy />
+        </SignatureCheckbox>
+      )}
 
       {hasSecondaryOne && (
         <SignatureCheckbox
