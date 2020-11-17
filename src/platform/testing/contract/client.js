92d6f5b1ffa1b1d943a4b1965e0de0749c5e78d8
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-11-17 11:07:04 -0500

    Added can-i-deploy to PactBrokerClient.

--- src/platform/testing/contract/client.js
+++ src/platform/testing/contract/client.js
@@ -14,6 +14,21 @@ require('isomorphic-fetch');
  */
 
 /**
+ * typedef {Object} BrokerOperationOptions
+ * @param {string} pactDir - The directory of pacts to be published.
+ * @param {string} version - The version to apply to the pacts.
+ * @param {string} tag - A tag for the consumers on this version.
+ */
+
+/**
+ * typedef {Object} ConsumerDeployContext
+ * @param {string} consumer - The consumer name.
+ * @param {string} version - The consumer version.
+ * @param {string} tag - The environment to deploy the consumer.
+ * @param {string} url - The endpoint to invoke for can-i-deploy.
+ */
+
+/**
  * Client that interacts with a Pact Broker.
  */
 module.exports = class PactBrokerClient {
@@ -147,10 +162,7 @@ module.exports = class PactBrokerClient {
 
   /**
    * Publishes and tags all pacts in the pacts folder.
-   * @param {Object} options
-   * @param {string} options.pactDir - The directory of pacts to be published.
-   * @param {string} options.version - The version to apply to the pacts.
-   * @param {string} options.tag - A tag for the consumers on this version.
+   * @param {BrokerOperationOptions} options
    */
   publishAndTagPacts = async options => {
     const requiredOptions = ['pactDir', 'version', 'tag'];
@@ -174,6 +186,89 @@ module.exports = class PactBrokerClient {
     } catch (e) {
       console.error('Failed to publish and tag pacts.');
       throw e;
+    }
+  };
+
+  /**
+   * Creates a set of objects that provide context for figuring out
+   * whether the consumers from the generated pacts can be deployed
+   * to the desired environment.
+   * @param {BrokerOperationOptions} options
+   * @return {ConsumerDeployContext[]}
+   */
+  createConsumerContexts = options => {
+    const { pactDir, version, tag = 'master' } = options;
+
+    const baseUrl = new URL(path.join(this.url, 'matrix'));
+    // Get latest result for each consumer version and provider (cvp) pairing.
+    // In other words, checks if the consumer has passed verification with all
+    // of its providers on the latest provider versions with the given tag.
+    baseUrl.searchParams.append('latestby', 'cvp');
+    baseUrl.searchParams.append('latest', true);
+    baseUrl.searchParams.append('tag', tag);
+    baseUrl.searchParams.append('q[]version', version);
+
+    return this.parsePacts(pactDir).map(data => {
+      const consumer = data.consumer.name;
+      const url = new URL(baseUrl);
+      url.searchParams.append('q[]pacticipant', consumer);
+      return { consumer, version, tag, url };
+    });
+  };
+
+  /**
+   * Determines whether a consumer is deployable.
+   * @param {ConsumerDeployContext}
+   * @return {boolean}
+   */
+  isConsumerDeployable = async ({ consumer, version, tag, url }) => {
+    const response = await fetch(url.toString());
+    const { summary, errors } = await response.json();
+    const deployable = response.ok && summary?.deployable;
+
+    if (!deployable) {
+      console.log(`Can't deploy version ${version} to ${tag}: ${consumer}.`);
+
+      // This error from Pact seems to appear in a specific scenario.
+      // Attempted a better error message based on interpretation of that.
+      if (errors?.includes('Please provide 1 or more version selectors.')) {
+        console.log(`There are no providers currently deployed on ${tag}.`);
+      }
+    }
+
+    if (summary?.reason) console.log(summary.reason);
+
+    return deployable;
+  };
+
+  /**
+   * Checks whether all consumers from the generated pacts can be deployed
+   * to the desired environment. Throws an error if the check fails.
+   * @param {BrokerOperationOptions}
+   */
+  canDeploy = async options => {
+    const requiredOptions = ['pactDir', 'version'];
+    let results;
+
+    try {
+      requiredOptions.forEach(key => {
+        if (!options[key]) throw new Error(`canDeploy: ${key} is required`);
+      });
+
+      const consumerContexts = this.createConsumerContexts(options);
+      results = await Promise.all(
+        consumerContexts.map(this.isConsumerDeployable),
+      );
+    } catch (e) {
+      console.log('Failed to check deployability.');
+      throw e;
+    }
+
+    const deployable = results.reduce((arr, cur) => arr && cur);
+    if (!deployable) {
+      throw new Error(
+        "Can't deploy. Try again after providers have successfully verified pacts for the consumers that couldn't deploy.",
+      );
     }
   };
 };
