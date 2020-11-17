ae87a0b81f86d12fe6b4abab35ce8e95779f0599
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-11-17 11:07:46 -0500

    Updated can-i-deploy script to use PactBrokerClient.

--- script/can-i-deploy.js
+++ script/can-i-deploy.js
@@ -7,37 +7,26 @@
  */
 
 const { execSync } = require('child_process');
-const fs = require('fs-extra');
 const path = require('path');
-const pact = require('@pact-foundation/pact-node');
 
-const pactFilesOrDirs = path.resolve(__dirname, '../pacts');
-let pacts;
+const PactBrokerClient = require('../src/platform/testing/contract/client');
 
-try {
-  pacts = fs.readdirSync(pactFilesOrDirs);
-  if (!pacts.length) throw new Error('No pacts found.');
-} catch (e) {
-  console.warn(e.message);
-  console.log('Skipping can-i-deploy check.');
-  return;
-}
+const pactBrokerClient = new PactBrokerClient({
+  url: process.env.PACT_BROKER_URL,
+});
 
-const version = execSync('git rev-parse --verify HEAD')
+const pactsFolder = path.resolve(__dirname, '../pacts');
+
+const commitHash = execSync('git rev-parse --verify HEAD')
   .toString()
   .trim();
 
-const pacticipants = pacts
-  .map(filename => fs.readJsonSync(`${pactFilesOrDirs}/${filename}`))
-  .map(({ consumer: { name } }) => ({ name, version }));
-
-const options = {
-  output: 'table',
-  pactBroker: process.env.PACT_BROKER_URL,
-  pacticipants,
-  retryInterval: 10,
-  retryWhileUnknown: 3,
-  to: 'master',
-};
-
-pact.canDeploy(options).catch(() => process.exit(1));
+pactBrokerClient
+  .canDeploy({
+    pactDir: pactsFolder,
+    version: commitHash,
+  })
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
