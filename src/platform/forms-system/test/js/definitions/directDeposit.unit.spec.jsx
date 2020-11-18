bf1dde708a98165d862451ad7b59d6eea1434cfd
Christopher Valarida <chris.valarida@adhocteam.us>
2020-11-18 12:42:16 -0800

    Refactor field order tests

--- src/platform/forms-system/test/js/definitions/directDeposit.unit.spec.jsx
+++ src/platform/forms-system/test/js/definitions/directDeposit.unit.spec.jsx
@@ -12,65 +12,45 @@ const getFieldNames = wrapper =>
     ),
   ).map(f => getNodeText(f));
 
+const testFieldOrder = (getSchemasOptions, expectedFieldNames) => {
+  const { schema, uiSchema } = getSchemas(getSchemasOptions);
+  const form = render(<DefinitionTester schema={schema} uiSchema={uiSchema} />);
+
+  expect(getFieldNames(form)).to.eql(expectedFieldNames);
+};
+
 describe('Direct deposit definition', () => {
-  it('should render all the fields', () => {
-    const { schema, uiSchema } = getSchemas();
-    const form = render(
-      <DefinitionTester schema={schema} uiSchema={uiSchema} />,
-    );
-
-    expect(getFieldNames(form)).to.eql([
+  it('should render all the fields', () =>
+    testFieldOrder(null, [
       'Account type',
       'Bank routing number',
       'Bank account number',
-    ]);
-  });
+    ]));
 
-  it('should render bankName', () => {
-    const { schema, uiSchema } = getSchemas({ bankName: true });
-    const form = render(
-      <DefinitionTester schema={schema} uiSchema={uiSchema} />,
-    );
-
-    expect(getFieldNames(form)).to.eql([
+  it('should render bankName', () =>
+    testFieldOrder({ bankName: true }, [
       'Account type',
       'Bank name',
       'Bank routing number',
       'Bank account number',
-    ]);
-  });
+    ]));
 
-  it('should render declineDirectDeposit', () => {
-    const { schema, uiSchema } = getSchemas({ declineDirectDeposit: true });
-    const form = render(
-      <DefinitionTester schema={schema} uiSchema={uiSchema} />,
-    );
-
-    expect(getFieldNames(form)).to.eql([
+  it('should render declineDirectDeposit', () =>
+    testFieldOrder({ declineDirectDeposit: true }, [
       'Account type',
       'Bank routing number',
       'Bank account number',
       'I don’t want to use direct deposit',
-    ]);
-  });
+    ]));
 
-  it('should render all optional fields in the correct order', () => {
-    const { schema, uiSchema } = getSchemas({
-      bankName: true,
-      declineDirectDeposit: true,
-    });
-    const form = render(
-      <DefinitionTester schema={schema} uiSchema={uiSchema} />,
-    );
-
-    expect(getFieldNames(form)).to.eql([
+  it('should render all optional fields in the correct order', () =>
+    testFieldOrder({ bankName: true, declineDirectDeposit: true }, [
       'Account type',
       'Bank name',
       'Bank routing number',
       'Bank account number',
       'I don’t want to use direct deposit',
-    ]);
-  });
+    ]));
 
   it('should render optional fields with supplied schema and uiSchema', () => {});
 });
