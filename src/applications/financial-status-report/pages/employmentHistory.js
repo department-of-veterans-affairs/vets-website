8e601b93ec1d706b4492f04435b8338551a23fb6
Craig Wheeler <craig@craigwheeler.net>
2020-11-18 14:08:36 -0500

    15219: veteran income form fields (#14981)

---
+++ src/applications/financial-status-report/pages/employmentHistory.js
@@ -0,0 +1,93 @@
+import EmploymentHistory from '../components/EmploymentHistory';
+
+export const uiSchema = {
+  employmentHistory: {
+    'ui:title': 'Your employment history',
+    'view:hasBeenEmployed': {
+      'ui:title': 'Have you been employed within the past two years?',
+      'ui:widget': 'yesNo',
+      'ui:required': () => true,
+    },
+    'view:isEmployed': {
+      'ui:options': {
+        expandUnder: 'view:hasBeenEmployed',
+      },
+      'view:currentlyEmployed': {
+        'ui:title': 'Are you currently employed?',
+        'ui:widget': 'yesNo',
+        'ui:required': () => true,
+      },
+      'view:isCurrentlyEmployed': {
+        'ui:field': EmploymentHistory,
+        'ui:options': {
+          expandUnder: 'view:currentlyEmployed',
+        },
+        employmentType: {
+          'ui:title': 'Type of employment',
+        },
+        employmentStart: {
+          'ui:title': 'Employment start date',
+          'ui:widget': 'date',
+        },
+        employerName: {
+          'ui:title': 'Employer name',
+        },
+        monthlyIncome: {
+          'ui:title': 'Gross monthly income',
+        },
+        // payrollDeductions: {},
+      },
+    },
+  },
+};
+
+export const schema = {
+  type: 'object',
+  properties: {
+    employmentHistory: {
+      type: 'object',
+      properties: {
+        'view:hasBeenEmployed': {
+          type: 'boolean',
+        },
+        'view:isEmployed': {
+          type: 'object',
+          properties: {
+            'view:currentlyEmployed': {
+              type: 'boolean',
+            },
+            'view:isCurrentlyEmployed': {
+              type: 'object',
+              properties: {
+                employmentType: {
+                  type: 'string',
+                },
+                employmentStart: {
+                  type: 'string',
+                },
+                employerName: {
+                  type: 'string',
+                },
+                monthlyIncome: {
+                  type: 'number',
+                },
+                payrollDeductions: {
+                  type: 'number',
+                },
+
+                // payrollDeductions: {
+                //   type: 'array',
+                //   items: {
+                //     type: 'number',
+                //     title: 'Input Amount',
+                //     default: 0,
+                //   },
+                // },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
