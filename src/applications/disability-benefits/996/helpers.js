8db3c2ecf05e17d93a1280aa9052535c00fe50b7
Rob Garrison <robin.garrison@adhocteam.us>
2020-11-18 12:27:08 -0600

    [Development] HLR: Filter out ineligible contestable issues (#14998)
    * Filter out ineligible contestable issues
    
    * Update tests & mock data

--- src/applications/disability-benefits/996/helpers.js
+++ src/applications/disability-benefits/996/helpers.js
@@ -1,4 +1,5 @@
 import Scroll from 'react-scroll';
+import moment from 'moment';
 
 // import the toggleValues helper
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
@@ -16,5 +17,55 @@ export const scrollToTop = () => {
     duration: 500,
     delay: 0,
     smooth: true,
+  });
+};
+
+/**
+ * @typedef ContestableIssues
+ * @type {Array<Object>}
+ * @property {ContestableIssueItem}
+ */
+/**
+ * @typedef ContestableIssueItem
+ * @type {Object}
+ * @property {String} type - always set to "contestableIssue"
+ * @property {ContestableIssueAttributes} attributes - essential properties
+ * @property {Boolean} 'view:selected' - internal boolean indicating that the
+ *   issue has been selected by the user
+ */
+/**
+ * @typedef ContestableIssueAttributes
+ * @type {Object}
+ * @property {String} ratingIssueSubjectText - title of issue
+ * @property {String} description - issue description
+ * @property {Number} ratingIssuePercentNumber - disability rating percentage
+ * @property {String} approxDecisionDate - decision date (YYYY-MM-DD)
+ * @property {Number} decisionIssueId - decision id
+ * @property {String} ratingIssueReferenceId - issue reference number
+ * @property {String} ratingDecisionReferenceId - decision reference id
+ */
+/** Filter out ineligible contestable issues:
+ * - remove issues more than one year past their decision date
+ * - remove issues that are deferred
+ * @prop {ContestableIssues} - Array of both eligible & ineligible contestable
+ *  issues
+ */
+export const getEligibleContestableIssues = issues => {
+  const today = moment().startOf('day');
+  return (issues || []).filter(issue => {
+    const {
+      approxDecisionDate = '',
+      ratingIssueSubjectText = '',
+      description = '',
+    } = issue?.attributes || {};
+
+    const isDeferred = [ratingIssueSubjectText, description]
+      .join(' ')
+      .includes('deferred');
+    const date = moment(approxDecisionDate);
+    if (isDeferred || !date.isValid()) {
+      return false;
+    }
+    return date.add(1, 'years').isAfter(today);
   });
 };
