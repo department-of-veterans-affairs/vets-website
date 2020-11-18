484464cce53c72dbf373616649ef7c4de35e9b75
Robin Garrison <robin.garrison@adhocteam.us>
2020-11-18 12:19:35 -0600

    Remove extra recordEvent calls & update unit test

--- src/applications/disability-benefits/996/config/submitForm.js
+++ src/applications/disability-benefits/996/config/submitForm.js
@@ -1,6 +1,18 @@
 import { submitToUrl } from 'platform/forms-system/src/js/actions';
 import { transformForSubmit } from 'platform/forms-system/src/js/helpers';
-import recordEvent from 'platform/monitoring/record-event';
+
+export const buildEventData = ({ sameOffice, informalConference }) => {
+  let informalConf = 'no';
+  if (informalConference !== 'no') {
+    informalConf = informalConference === 'rep' ? 'yes-with-rep' : 'yes';
+  }
+  return {
+    // or 'no'; this event should be renamed to decision-reviews-sameOffice
+    'decision-reviews-differentOffice': sameOffice ? 'yes' : 'no',
+    // or 'no', or 'yes-with-rep'
+    'decision-reviews-informalConf': informalConf,
+  };
+};
 
 const submitForm = (form, formConfig) => {
   const { submitUrl, trackingPrefix } = formConfig;
@@ -8,24 +20,9 @@ const submitForm = (form, formConfig) => {
     ? formConfig.transformForSubmit(formConfig, form)
     : transformForSubmit(formConfig, form);
 
-  // event data needed on successful submission
-  const { sameOffice, informalConference } = form.data;
-  let informalConf = 'no';
-  if (informalConference !== 'no') {
-    informalConf = informalConference === 'rep' ? 'yes-with-rep' : 'yes';
-  }
-  const eventData = {
-    // or 'no'
-    'decision-reviews-differentOffice': sameOffice ? 'yes' : 'no',
-    // or 'no', or 'yes-with-rep'
-    'decision-reviews-informalConf': informalConf,
-  };
-  return submitToUrl(body, submitUrl, trackingPrefix, eventData).catch(() => {
-    recordEvent({
-      event: `${trackingPrefix}-submission-failed`,
-      ...eventData,
-    });
-  });
+  // eventData for analytics
+  const eventData = buildEventData(form.data);
+  return submitToUrl(body, submitUrl, trackingPrefix, eventData);
 };
 
 export default submitForm;
--- src/applications/disability-benefits/996/tests/schema/submitForm.unit.spec.js
+++ src/applications/disability-benefits/996/tests/schema/submitForm.unit.spec.js
@@ -1,57 +1,26 @@
 import { expect } from 'chai';
 
-import formConfig from '../../config/form';
+import { buildEventData } from '../../config/submitForm';
 
-const getFormData = (sameOffice, informalConference) => ({
-  data: {
-    sameOffice,
-    informalConference,
-    informalConferenceTimes: [],
-  },
-});
-
-const getEvent = (result, office, conf) => ({
-  event: `${formConfig.trackingPrefix}-submission${result}`,
-  'decision-reviews-differentOffice': office,
-  'decision-reviews-informalConf': conf,
-});
-
-describe('HLR submit form', () => {
-  it('should record a submission attempt & failed attempt', done => {
-    const config = {
-      ...formConfig,
-      submitUrl: '', // something that will always fail
-    };
-    formConfig.submit(getFormData(false, 'no'), config).finally(() => {
-      expect(global.window.dataLayer[0]).to.deep.equal(
-        getEvent('-failed', 'no', 'no'),
-      );
-      done();
+describe('HLR submit event data', () => {
+  it('should build submit event data', () => {
+    expect(
+      buildEventData({ sameOffice: true, informalConference: 'no' }),
+    ).to.deep.equal({
+      'decision-reviews-differentOffice': 'yes',
+      'decision-reviews-informalConf': 'no',
+    });
+    expect(
+      buildEventData({ sameOffice: false, informalConference: 'rep' }),
+    ).to.deep.equal({
+      'decision-reviews-differentOffice': 'no',
+      'decision-reviews-informalConf': 'yes-with-rep',
+    });
+    expect(
+      buildEventData({ sameOffice: false, informalConference: 'yes' }),
+    ).to.deep.equal({
+      'decision-reviews-differentOffice': 'no',
+      'decision-reviews-informalConf': 'yes',
     });
   });
-  it('should record a submission attempt & failed attempt with different data', done => {
-    const config = {
-      ...formConfig,
-      submitUrl: '', // something that will always fail
-    };
-    formConfig.submit(getFormData(true, 'yes'), config).finally(() => {
-      expect(global.window.dataLayer[0]).to.deep.equal(
-        getEvent('-failed', 'yes', 'yes'),
-      );
-      done();
-    });
-  });
-  it('should record a submission attempt & failed attempt with different data', done => {
-    const config = {
-      ...formConfig,
-      submitUrl: '', // something that will always fail
-    };
-    formConfig.submit(getFormData(true, 'rep'), config).finally(() => {
-      expect(global.window.dataLayer[0]).to.deep.equal(
-        getEvent('-failed', 'yes', 'yes-with-rep'),
-      );
-      done();
-    });
-  });
-  // TODO: Test successful XMLHttpRequest
 });
