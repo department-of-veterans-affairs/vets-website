04c9b615419172b316d868b34390dbd976389ae5
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-11-19 15:14:21 -0500

    Fix Sign-in FAQ links

--- src/platform/user/authentication/components/SignInModal.jsx
+++ src/platform/user/authentication/components/SignInModal.jsx
@@ -295,9 +295,18 @@ export class SignInModal extends React.Component {
                 Having trouble signing in?
               </h2>
               <p>
+                Get answers to common questions about{' '}
                 <a href="/resources/signing-in-to-vagov/" target="_blank">
-                  Get answers to Frequently Asked Questions
+                  signing in
+                </a>{' '}
+                and{' '}
+                <a
+                  href="/resources/verifying-your-identity-on-va-gov/"
+                  target="_blank"
+                >
+                  verifying your identity
                 </a>
+                .
               </p>
               <p>
                 <SubmitSignInForm startSentence />
