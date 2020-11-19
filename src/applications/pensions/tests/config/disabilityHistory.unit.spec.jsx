de138af34ba882bf85b5b93a7d90303f3e534eb9
Tim Wright <timwright12@gmail.com>
2020-11-19 16:04:21 -0500

    disabling more tests

--- src/applications/pensions/tests/config/disabilityHistory.unit.spec.jsx
+++ src/applications/pensions/tests/config/disabilityHistory.unit.spec.jsx
@@ -10,7 +10,7 @@ import {
 } from 'platform/testing/unit/schemaform-utils.jsx';
 import formConfig from '../../config/form.js';
 
-describe('Pensions disability history', () => {
+xdescribe('Pensions disability history', () => {
   const {
     schema,
     uiSchema,
