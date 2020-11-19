27e990d8c3d75b188a6f14940ff837085f9c6053
Theo Bentum <theo@adhocteam.us>
2020-11-19 12:45:18 -0800

    Update the derivative url in getImageCrop

--- src/site/stages/build/process-cms-exports/transformers/helpers.js
+++ src/site/stages/build/process-cms-exports/transformers/helpers.js
@@ -107,7 +107,14 @@ function getImageCrop(obj, imageStyle = null) {
       imageObj.image.derivative.url
     }`.replace('public:/', 'public');
     imageObj.image.url = url;
-    imageObj.image.derivative.url = url;
+    // Derivative urls have a full path starting with
+    // 'https://{buildtype}.cms.va.gov/sites/default/files/', so add that here.
+    // It doesn't matter what the build type is since it gets stripped out in convertDrupalFilesToLocal
+    // so just use prod here
+    imageObj.image.derivative.url = `https://prod.cms.va.gov/sites/default/files/${url.replace(
+      '/img/',
+      '',
+    )}`;
     imageObj.image.derivative.width = image.width;
     imageObj.image.derivative.height = image.height;
     return imageObj;
