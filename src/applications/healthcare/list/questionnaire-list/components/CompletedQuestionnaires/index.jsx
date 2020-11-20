95c91fc6c117bea51831032262ba9c4c4551e440
Mark Dewey <mdewey@governmentcio.com>
2020-11-20 15:48:09 -0500

    populating data and iteration, need to style

--- src/applications/healthcare/list/questionnaire-list/components/CompletedQuestionnaires/index.jsx
+++ src/applications/healthcare/list/questionnaire-list/components/CompletedQuestionnaires/index.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default function index() {
-  return <div id="tabpanel_completed">Completed.</div>;
+const index = props => {
+  const { questionnaires } = props;
+  return (
+    <div id="tabpanel_completed">
+      <h2>Title goes here</h2>
+      <ul>
+        {questionnaires.map(questionnaire => {
+          return (
+            <li key={questionnaire.appointment.id}>
+              {questionnaire.appointment.facilityName}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    isLoading: state.questionnaireListData?.list?.status?.isLoading,
+    questionnaires:
+      state.questionnaireListData?.list?.questionnaires?.completed,
+  };
 }
+
+export default connect(mapStateToProps)(index);
--- src/applications/healthcare/list/questionnaire-list/components/ToDoQuestionnaires/index.jsx
+++ src/applications/healthcare/list/questionnaire-list/components/ToDoQuestionnaires/index.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default function index() {
-  return <div id="tabpanel_toDo">Not Completed</div>;
+const index = props => {
+  const { questionnaires } = props;
+  return (
+    <div id="tabpanel_toDo">
+      <h2>Title goes here</h2>
+      <ul>
+        {questionnaires.map(questionnaire => {
+          return (
+            <li key={questionnaire.appointment.id}>
+              {questionnaire.appointment.facilityName}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    isLoading: state.questionnaireListData?.list?.status?.isLoading,
+    questionnaires: state.questionnaireListData?.list?.questionnaires?.toDo,
+  };
 }
+
+export default connect(mapStateToProps)(index);
