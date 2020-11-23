e8287e545caaa30e190cdc5af65ef47546df7df1
Mark Dewey <mdewey@governmentcio.com>
2020-11-23 15:31:48 -0500

    added sorting

--- src/applications/healthcare/list/utils/index.js
+++ src/applications/healthcare/list/utils/index.js
@@ -3,6 +3,11 @@ const sortQuestionnairesByStatus = questionnaires => {
   if (!data) {
     data = [];
   }
+  data.sort((first, second) => {
+    const f = first.appointment.appointmentTime;
+    const s = second.appointment.appointmentTime;
+    return new Date(f) - new Date(s);
+  });
   const completed = data.filter(f => f.questionnaireResponse?.completed);
   const toDo = data.filter(f => !f.questionnaireResponse?.completed);
 
