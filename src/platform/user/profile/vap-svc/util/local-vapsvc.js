e84660894738ea1290117d68a42e1ef72c3011c3
Sandra Hallie <sandra.adamshallie@gmail.com>
2020-11-23 13:02:34 -0700

    Fix

--- src/platform/user/profile/vap-svc/util/local-vapsvc.js
+++ src/platform/user/profile/vap-svc/util/local-vapsvc.js
@@ -5,7 +5,8 @@ export function isVAProfileServiceConfigured() {
   return (
     // using the existence of VetsGov.pollTimeout as an indicator that we are
     // running unit tests and therefore _do_ want the FE to make real API calls
-    window.VetsGov.pollTimeout || window.Cypress
+    window.VetsGov.pollTimeout ||
+    window.Cypress ||
     [
       'dev.va.gov',
       'preview.va.gov',
