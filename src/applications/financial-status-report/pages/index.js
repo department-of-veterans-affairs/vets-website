f89fac1b94d855ec2914ebbe2ad0aabb6043592e
Brad Conley <bradl3yC@users.noreply.github.com>
2020-11-24 12:26:49 -0500

    bradl3yC - 15222 - fsr dependents and itemloop (#15067)
    * Add dependents page
    
    * remove 2nd progressive disclosure requirement
    
    * wip
    
    * wip
    
    * Correct minValue implementation and tests
    
    * Convert to es6 funcs

--- src/applications/financial-status-report/pages/index.js
+++ src/applications/financial-status-report/pages/index.js
@@ -2,5 +2,12 @@ import * as availableDebts from './availableDebts';
 import * as employmentHistory from './employmentHistory';
 import * as veteranInfo from './veteranInfo';
 import * as otherIncome from './otherIncome';
+import * as dependents from './dependents';
 
-export { availableDebts, employmentHistory, otherIncome, veteranInfo };
+export {
+  availableDebts,
+  employmentHistory,
+  otherIncome,
+  veteranInfo,
+  dependents,
+};
