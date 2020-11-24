02f7a2264b038611e4426448e87990059b920d4f
Mark Dewey <mdewey@governmentcio.com>
2020-11-24 10:25:36 -0500

    added messging for completed

--- src/applications/healthcare/list/questionnaire-list/components/CompletedQuestionnaires/index.jsx
+++ src/applications/healthcare/list/questionnaire-list/components/CompletedQuestionnaires/index.jsx
@@ -3,41 +3,55 @@ import { connect } from 'react-redux';
 import moment from 'moment';
 
 import QuestionnaireItem from '../QuestionnaireItem';
+import EmptyMessage from '../Messages/EmptyMessage';
+import ServiceDown from '../Messages/ServiceDown';
 
 const index = props => {
   const { questionnaires } = props;
+
   return (
     <div id="tabpanel_completed">
       <h2 className="questionnaire-list-header">Completed questionnaires</h2>
-      <ul className="questionnaire-list completed">
-        {questionnaires.map(questionnaire => {
-          const { questionnaireResponse } = questionnaire;
-          return (
-            <QuestionnaireItem
-              key={questionnaire.appointment.id}
-              data={questionnaire}
-              Actions={() => (
-                <button className="va-button">View and print questions</button>
-              )}
-              DueDate={() => (
-                <p className="completed-date">
-                  Submitted on{' '}
-                  {moment(questionnaireResponse.submittedOn).format(
-                    'MMMM D, YYYY',
-                  )}
-                </p>
-              )}
-            />
-          );
-        })}
-      </ul>
+      {questionnaires ? (
+        <>
+          {questionnaires.length === 0 ? (
+            <EmptyMessage />
+          ) : (
+            <ul className="questionnaire-list completed">
+              {questionnaires.map(questionnaire => {
+                const { questionnaireResponse } = questionnaire;
+                return (
+                  <QuestionnaireItem
+                    key={questionnaire.appointment.id}
+                    data={questionnaire}
+                    Actions={() => (
+                      <button className="va-button">
+                        View and print questions
+                      </button>
+                    )}
+                    DueDate={() => (
+                      <p className="completed-date">
+                        Submitted on{' '}
+                        {moment(questionnaireResponse.submittedOn).format(
+                          'MMMM D, YYYY',
+                        )}
+                      </p>
+                    )}
+                  />
+                );
+              })}
+            </ul>
+          )}
+        </>
+      ) : (
+        <ServiceDown />
+      )}
     </div>
   );
 };
 
 function mapStateToProps(state) {
   return {
-    isLoading: state.questionnaireListData?.list?.status?.isLoading,
     questionnaires:
       state.questionnaireListData?.list?.questionnaires?.completed,
   };
