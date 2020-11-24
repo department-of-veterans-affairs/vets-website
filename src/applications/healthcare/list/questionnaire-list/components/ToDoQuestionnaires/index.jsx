47000781d4ad9178c39b2f5924404c2d8f6776a4
Mark Dewey <mdewey@governmentcio.com>
2020-11-24 10:31:43 -0500

    updated the todo list

--- src/applications/healthcare/list/questionnaire-list/components/ToDoQuestionnaires/index.jsx
+++ src/applications/healthcare/list/questionnaire-list/components/ToDoQuestionnaires/index.jsx
@@ -3,53 +3,63 @@ import { connect } from 'react-redux';
 import moment from 'moment';
 
 import QuestionnaireItem from '../QuestionnaireItem';
+import EmptyMessage from '../Messages/EmptyMessage';
+import ServiceDown from '../Messages/ServiceDown';
 
 const index = props => {
   const { questionnaires } = props;
   return (
     <div id="tabpanel_toDo">
       <h2 className="questionnaire-list-header">To do questionnaires</h2>
-      <ul className="questionnaire-list toDo">
-        {questionnaires.map(questionnaire => {
-          const { appointment } = questionnaire;
-
-          return (
-            <QuestionnaireItem
-              key={appointment.id}
-              data={questionnaire}
-              Actions={() => (
-                <a
-                  className="usa-button va-button"
-                  href={`/healthcare/questionnaire?id=${appointment.id}`}
-                  aria-label={`Fill out your pre-appointment questionnaire for your primary care visit at ${
-                    appointment.facilityName
-                  } on ${moment(appointment.appointmentTime).format(
-                    'MMMM, D, YYYY',
-                  )}`}
-                >
-                  Answer questions
-                </a>
-              )}
-              DueDate={() => {
-                const dueDate = moment(appointment.appointmentTime).subtract(
-                  1,
-                  'day',
-                );
-                const meridiem = dueDate.hours() > 12 ? 'p.m.' : 'a.m.';
+      {questionnaires ? (
+        <>
+          {questionnaires.length === 0 ? (
+            <EmptyMessage />
+          ) : (
+            <ul className="questionnaire-list toDo">
+              {questionnaires.map(questionnaire => {
+                const { appointment } = questionnaire;
                 return (
-                  <section className="due-date">
-                    <p>Due date:</p>
-                    <p>{dueDate.format('dddd, MMMM D, YYYY')}</p>
-                    <p>
-                      {dueDate.format(`H:MM`)} {meridiem}
-                    </p>
-                  </section>
+                  <QuestionnaireItem
+                    key={appointment.id}
+                    data={questionnaire}
+                    Actions={() => (
+                      <a
+                        className="usa-button va-button"
+                        href={`/healthcare/questionnaire?id=${appointment.id}`}
+                        aria-label={`Fill out your pre-appointment questionnaire for your primary care visit at ${
+                          appointment.facilityName
+                        } on ${moment(appointment.appointmentTime).format(
+                          'MMMM, D, YYYY',
+                        )}`}
+                      >
+                        Answer questions
+                      </a>
+                    )}
+                    DueDate={() => {
+                      const dueDate = moment(
+                        appointment.appointmentTime,
+                      ).subtract(1, 'day');
+                      const meridiem = dueDate.hours() > 12 ? 'p.m.' : 'a.m.';
+                      return (
+                        <section className="due-date">
+                          <p>Due date:</p>
+                          <p>{dueDate.format('dddd, MMMM D, YYYY')}</p>
+                          <p>
+                            {dueDate.format(`H:MM`)} {meridiem}
+                          </p>
+                        </section>
+                      );
+                    }}
+                  />
                 );
-              }}
-            />
-          );
-        })}
-      </ul>
+              })}
+            </ul>
+          )}
+        </>
+      ) : (
+        <ServiceDown />
+      )}
     </div>
   );
 };
