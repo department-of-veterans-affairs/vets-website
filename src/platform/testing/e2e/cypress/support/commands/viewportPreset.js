f5bfdd59aa7fb4621f2bcb9cca5fa564f81bbcae
Holden Hinkle <holdenhinkle@gmail.com>
2020-11-30 09:31:08 -0500

    update preset names

--- src/platform/testing/e2e/cypress/support/commands/viewportPreset.js
+++ src/platform/testing/e2e/cypress/support/commands/viewportPreset.js
@@ -1,22 +1,22 @@
 const presets = {
   // Mobile top 5 presets by traffic percentage, descending
-  'mobile-top5-1': { width: 414, height: 896 },
-  'mobile-top5-2': { width: 375, height: 667 },
-  'mobile-top5-3': { width: 375, height: 812 },
-  'mobile-top5-4': { width: 414, height: 736 },
-  'mobile-top5-5': { width: 320, height: 568 },
+  'va-top-mobile-1': { width: 414, height: 896 },
+  'va-top-mobile-2': { width: 375, height: 667 },
+  'va-top-mobile-3': { width: 375, height: 812 },
+  'va-top-mobile-4': { width: 414, height: 736 },
+  'va-top-mobile-5': { width: 320, height: 568 },
   // Tablet top 5 presets by traffic percentage, descending
-  'tablet-top5-1': { width: 768, height: 1024 },
-  'tablet-top5-2': { width: 810, height: 1080 },
-  'tablet-top5-3': { width: 1024, height: 1366 },
-  'tablet-top5-4': { width: 834, height: 1112 },
-  'tablet-top5-5': { width: 834, height: 1194 },
+  'va-top-tablet-1': { width: 768, height: 1024 },
+  'va-top-tablet-2': { width: 810, height: 1080 },
+  'va-top-tablet-3': { width: 1024, height: 1366 },
+  'va-top-tablet-4': { width: 834, height: 1112 },
+  'va-top-tablet-5': { width: 834, height: 1194 },
   // Desktop top 5 presets by traffic percentage, descending
-  'desktop-top5-1': { width: 1920, height: 1080 },
-  'desktop-top5-2': { width: 1366, height: 768 },
-  'desktop-top5-3': { width: 1280, height: 960 },
-  'desktop-top5-4': { width: 1440, height: 900 },
-  'desktop-top5-5': { width: 1536, height: 864 },
+  'va-top-desktop-1': { width: 1920, height: 1080 },
+  'va-top-desktop-2': { width: 1366, height: 768 },
+  'va-top-desktop-3': { width: 1280, height: 960 },
+  'va-top-desktop-4': { width: 1440, height: 900 },
+  'va-top-desktop-5': { width: 1536, height: 864 },
 };
 
 const isValidPreset = preset => presets[preset] !== undefined;
