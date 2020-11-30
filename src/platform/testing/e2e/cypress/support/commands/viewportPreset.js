046b824f8355f8b761338f28e3af39fa2818010d
Holden Hinkle <holdenhinkle@gmail.com>
2020-11-30 09:32:56 -0500

    update comments in preset object

--- src/platform/testing/e2e/cypress/support/commands/viewportPreset.js
+++ src/platform/testing/e2e/cypress/support/commands/viewportPreset.js
@@ -1,17 +1,17 @@
 const presets = {
-  // Mobile top 5 presets by traffic percentage, descending
+  // Top mobile presets by traffic percentage, descending
   'va-top-mobile-1': { width: 414, height: 896 },
   'va-top-mobile-2': { width: 375, height: 667 },
   'va-top-mobile-3': { width: 375, height: 812 },
   'va-top-mobile-4': { width: 414, height: 736 },
   'va-top-mobile-5': { width: 320, height: 568 },
-  // Tablet top 5 presets by traffic percentage, descending
+  // Top tablet presets by traffic percentage, descending
   'va-top-tablet-1': { width: 768, height: 1024 },
   'va-top-tablet-2': { width: 810, height: 1080 },
   'va-top-tablet-3': { width: 1024, height: 1366 },
   'va-top-tablet-4': { width: 834, height: 1112 },
   'va-top-tablet-5': { width: 834, height: 1194 },
-  // Desktop top 5 presets by traffic percentage, descending
+  // Top desktop presets by traffic percentage, descending
   'va-top-desktop-1': { width: 1920, height: 1080 },
   'va-top-desktop-2': { width: 1366, height: 768 },
   'va-top-desktop-3': { width: 1280, height: 960 },
