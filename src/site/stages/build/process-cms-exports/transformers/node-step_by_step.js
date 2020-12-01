cf61a6413c97a1f93bf2a1dcd4eca177887eb3fb
Kelson Adams <kadams@governmentcio.com>
2020-12-01 09:23:03 -0700

    Update transformer

--- src/site/stages/build/process-cms-exports/transformers/node-step_by_step.js
+++ src/site/stages/build/process-cms-exports/transformers/node-step_by_step.js
@@ -1,14 +1,54 @@
 // Remove eslint-disable when transformer is complete
 /* eslint-disable no-unused-vars */
-const { getDrupalValue } = require('./helpers');
+const {
+  getDrupalValue,
+  utcToEpochTime,
+  getWysiwygString,
+  createLink,
+  createMetaTagArray,
+  isPublished,
+  getImageCrop,
+} = require('./helpers');
 
-const transform = entity => ({
-  entityType: 'node',
-  entityBundle: 'step_by_step',
-  fieldIntroTextLimitedHtml: {},
-});
+const transform = entity => {
+  return {
+    entityType: 'node',
+    entityBundle: 'step_by_step',
+    changed: utcToEpochTime(getDrupalValue(entity.changed)),
+    title: getDrupalValue(entity.title),
+    fieldIntroTextLimitedHtml: entity.fieldIntroTextLimitedHtml,
+    fieldAlertSingle: entity.fieldAlertSingle,
+    fieldButtons: entity.fieldButtons,
+    fieldButtonsRepeat: entity.fieldButtonsRepeat,
+    fieldSteps: entity.fieldSteps,
+    fieldContactInformation: entity.fieldContactInformation,
+    fieldRelatedBenefitHubs: entity.fieldRelatedBenefitHubs,
+    fieldRelatedInformation: entity.fieldRelatedInformation,
+    fieldPrimaryCategory: entity.fieldPrimaryCategory,
+    fieldOtherCategories: entity.fieldOtherCategories,
+    fieldTags: entity.fieldTags,
+  };
+};
 
 module.exports = {
-  filter: [''],
+  filter: [
+    'changed',
+    'field_alert_single',
+    'field_buttons',
+    'field_buttons_repeat',
+    'field_contact_information',
+    'field_intro_text_limited_html',
+    'field_other_categories',
+    'field_primary_category',
+    'field_related_benefit_hubs',
+    'field_related_information',
+    'field_steps',
+    'field_tags',
+    'metatag',
+    'path',
+    'status',
+    'title',
+    'uid',
+  ],
   transform,
 };
