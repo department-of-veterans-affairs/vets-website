0e85a7501b26ed6352bfba91cae1c07aef881798
Craig Wheeler <craig@craigwheeler.net>
2020-12-01 08:45:39 -0500

    initial commit (#15121)

--- src/applications/financial-status-report/pages/index.js
+++ src/applications/financial-status-report/pages/index.js
@@ -3,6 +3,7 @@ import * as employmentHistory from './employmentHistory';
 import * as veteranInfo from './veteranInfo';
 import * as otherIncome from './otherIncome';
 import * as spouseEmployment from './spouseEmployment';
+import * as spouseBenefits from './spouseBenefits';
 import * as spouseAdditionalEmployment from './spouseAdditionalEmployment';
 import * as dependents from './dependents';
 import * as householdAssets from './householdAssets';
@@ -14,6 +15,7 @@ export {
   otherIncome,
   veteranInfo,
   spouseEmployment,
+  spouseBenefits,
   spouseAdditionalEmployment,
   dependents,
   householdAssets,
