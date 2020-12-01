9ff7f117ab9cd49c6a23959ef272ab0b44bf695f
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-01 14:14:16 -0500

    15835 create helper functions for cypress (#15117)
    * create cypress environment variables for mobile and desktop viewports
    
    * create new cypress customer command
    
    * add last line
    
    * create cypress environment variables for different viewport groups
    
    * update environment viewport variables with data from va.gov google analytics
    
    * create new cypress custom command viewportPreset()
    
    * create new cypress custom command viewportPreset() with top 5 presets for mobile, tablet, and desktop derived from va.gov google analytics
    
    * create new cypress custom command viewportPreset() with top 5 presets for mobile, tablet, and desktop derived from va.gov google analytics
    
    * update comment
    
    * remove redundant 'Error: ' from error message string
    
    * fix validation for options argument
    
    * fix eslint errors
    
    * create function to format errors. fix isValidOptions validation.
    
    * create function to format errors. fix isValidOptions validation.
    
    * rename helper function
    
    * update comments about presets by traffic. ascending should be descending
    
    * update function to handle optional arguments for axeCheck
    
    * add comments to injectAxeThenAxeCheck custom command
    
    * rename top5 environment variables
    
    * add rank property to viewport objects
    
    * remove allIPhoneViewports env var
    
    * add list property to viewport objects
    
    * rename name to devicesWithViewport
    
    * update value for devicesWithViewport property in vaTopDesktopViewports objects
    
    * delete iphone and ipad resets. delete viewport variables. update preset values with width and height objects
    
    * update preset names
    
    * update comments in preset object
    
    * update list value in viewport objects
    
    * add viewportPreset property to viewport cypress env var objects

--- src/platform/testing/e2e/cypress/support/commands/viewportPreset.js
+++ src/platform/testing/e2e/cypress/support/commands/viewportPreset.js
@@ -1,99 +1,22 @@
-// iPhone viewports
-const viewport320X480 = { width: 320, height: 480 };
-const viewport320X568 = { width: 320, height: 568 };
-const viewport360X780 = { width: 360, height: 780 };
-const viewport375X667 = { width: 375, height: 667 };
-const viewport375X812 = { width: 375, height: 812 };
-const viewport390X844 = { width: 390, height: 844 };
-const viewport414X736 = { width: 414, height: 736 };
-const viewport414X896 = { width: 414, height: 896 };
-const viewport428X926 = { width: 428, height: 926 };
-// iPad viewports
-const viewPort768X1024 = { width: 768, height: 1024 };
-const viewPort810X1080 = { width: 810, height: 1080 };
-const viewPort834X1112 = { width: 834, height: 1112 };
-const viewPort834X1194 = { width: 834, height: 1194 };
-const viewPort1024X1366 = { width: 1024, height: 1366 };
-// Deskop viewports
-const viewPort1280X960 = { width: 1280, height: 960 };
-const viewPort1366X768 = { width: 1366, height: 768 };
-const viewPort1440X900 = { width: 1440, height: 900 };
-const viewPort1536X864 = { width: 1536, height: 864 };
-const viewPort1920X1080 = { width: 1920, height: 1080 };
-
 const presets = {
-  // iPhone presets
-  'iphone-1st-gen': viewport320X480,
-  'iphone-3g': viewport320X480,
-  'iphone-3gs': viewport320X480,
-  'iphone-4': viewport320X480,
-  'iphone-4s': viewport320X480,
-  'iphone-5': viewport320X568,
-  'iphone-5c': viewport320X568,
-  'iphone-5s': viewport320X568,
-  'iphone-6-plus': viewport414X736,
-  'iphone-6': viewport375X667,
-  'iphone-6s-plus': viewport414X736,
-  'iphone-6s': viewport375X667,
-  'iphone-se-1st-gen': viewport320X568,
-  'iphone-7-plus': viewport414X736,
-  'iphone-7': viewport375X667,
-  'iphone-8-plus': viewport414X736,
-  'iphone-8': viewport375X667,
-  'iphone-x': viewport375X812,
-  'iphone-xs-max': viewport414X896,
-  'iphone-xs': viewport375X812,
-  'iphone-xr': viewport414X896,
-  'iphone-11-pro-max': viewport414X896,
-  'iphone-11': viewport414X896,
-  'iphone-11-pro': viewport375X812,
-  'iphone-se-2nd-gen': viewport375X667,
-  'iphone-12-pro-max': viewport428X926,
-  'iphone-12': viewport390X844,
-  'iphone-12-pro': viewport390X844,
-  'iphone-12-mini': viewport360X780,
-  // iPad presets
-  'ipad-1': viewPort768X1024,
-  'ipad-2': viewPort768X1024,
-  'ipad-3': viewPort768X1024,
-  'ipad-mini-1': viewPort768X1024,
-  'ipad-4': viewPort768X1024,
-  'ipad-air-1': viewPort768X1024,
-  'ipad-mini-2': viewPort768X1024,
-  'ipad-air-2': viewPort768X1024,
-  'ipad-mini-3': viewPort768X1024,
-  'ipad-mini-4': viewPort768X1024,
-  'ipad-pro-1-12.9': viewPort1024X1366,
-  'ipad-pro-1-9.7': viewPort768X1024,
-  'ipad-5': viewPort768X1024,
-  'ipad-pro-2-12.9': viewPort1024X1366,
-  'ipad-pro-2--10.5': viewPort834X1112,
-  'ipad-6': viewPort768X1024,
-  'ipad-pro-3-12.9': viewPort1024X1366,
-  'ipad-pro-3-11': viewPort834X1194,
-  'ipad-air-3': viewPort834X1112,
-  'ipad-mini-5': viewPort768X1024,
-  'ipad-7': viewPort810X1080,
-  'ipad-pro-4-12.9': viewPort1024X1366,
-  'ipad-pro-4-11': viewPort834X1194,
-  // Mobile top 5 presets by traffic percentage, descending
-  'mobile-top5-1': viewport414X896,
-  'mobile-top5-2': viewport375X667,
-  'mobile-top5-3': viewport375X812,
-  'mobile-top5-4': viewport414X736,
-  'mobile-top5-5': viewport320X568,
-  // Tablet top 5 presets by traffic percentage, descending
-  'tablet-top5-1': viewPort768X1024,
-  'tablet-top5-2': viewPort810X1080,
-  'tablet-top5-3': viewPort1024X1366,
-  'tablet-top5-4': viewPort834X1112,
-  'tablet-top5-5': viewPort834X1194,
-  // Desktop top 5 presets by traffic percentage, descending
-  'desktop-top5-1': viewPort1920X1080,
-  'desktop-top5-2': viewPort1366X768,
-  'desktop-top5-3': viewPort1280X960,
-  'desktop-top5-4': viewPort1440X900,
-  'desktop-top5-5': viewPort1536X864,
+  // Top mobile presets by traffic percentage, descending
+  'va-top-mobile-1': { width: 414, height: 896 },
+  'va-top-mobile-2': { width: 375, height: 667 },
+  'va-top-mobile-3': { width: 375, height: 812 },
+  'va-top-mobile-4': { width: 414, height: 736 },
+  'va-top-mobile-5': { width: 320, height: 568 },
+  // Top tablet presets by traffic percentage, descending
+  'va-top-tablet-1': { width: 768, height: 1024 },
+  'va-top-tablet-2': { width: 810, height: 1080 },
+  'va-top-tablet-3': { width: 1024, height: 1366 },
+  'va-top-tablet-4': { width: 834, height: 1112 },
+  'va-top-tablet-5': { width: 834, height: 1194 },
+  // Top desktop presets by traffic percentage, descending
+  'va-top-desktop-1': { width: 1920, height: 1080 },
+  'va-top-desktop-2': { width: 1366, height: 768 },
+  'va-top-desktop-3': { width: 1280, height: 960 },
+  'va-top-desktop-4': { width: 1440, height: 900 },
+  'va-top-desktop-5': { width: 1536, height: 864 },
 };
 
 const isValidPreset = preset => presets[preset] !== undefined;
