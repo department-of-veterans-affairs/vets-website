1265fb17fff22980b1aab21ea7308c3385f2b730
linuxlance <LanceGundersen@users.noreply.github.com>
2020-12-01 11:53:43 -0500

    Add cc providers list form

--- src/applications/vaos/new-appointment/components/CommunityCareProviderSelectionPage/ProviderSelectionField.jsx
+++ src/applications/vaos/new-appointment/components/CommunityCareProviderSelectionPage/ProviderSelectionField.jsx
@@ -1,5 +1,236 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { selectProviderSelectionInfo } from '../../../utils/selectors';
+import ResidentialAddress from '../../../components/ResidentialAddress';
+import { connect } from 'react-redux';
+import * as actions from '../../redux/actions';
+import { FETCH_STATUS } from '../../../utils/constants';
+import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
+import { distanceBetween } from '../../../utils/address';
+import { scrollAndFocus } from '../../../utils/scrollAndFocus';
+import ErrorMessage from '../../../components/ErrorMessage';
 
-export default function ProviderSelectionField() {
-  return <strong>List stuff goes here</strong>;
+const INITIAL_PROVIDER_DISPLAY_COUNT = 5;
+
+function ProviderSelectionField({
+  address,
+  formData,
+  onChange,
+  idSchema,
+  requestStatus,
+  requestProvidersList,
+  communityCareProviderList,
+}) {
+  const [checkedProvider, setCheckedProvider] = useState();
+  const [mounted, setMounted] = useState(false);
+  const [showProvidersList, setShowProvidersList] = useState(false);
+  const [providersListLength, setProvidersListLength] = useState(
+    INITIAL_PROVIDER_DISPLAY_COUNT,
+  );
+  const currentlyShownProvidersList = communityCareProviderList?.slice(
+    0,
+    providersListLength,
+  );
+  const loadingProviders =
+    requestStatus === FETCH_STATUS.loading ||
+    requestStatus === FETCH_STATUS.notStarted;
+  const providerSelected = 'id' in formData;
+  useEffect(() => {
+    requestProvidersList(address);
+    setMounted(true);
+  }, []);
+
+  useEffect(
+    () => {
+      if (showProvidersList && !loadingProviders) {
+        scrollAndFocus('h2');
+      } else if (mounted && !loadingProviders) {
+        scrollAndFocus('.va-button-link');
+      }
+    },
+    [showProvidersList],
+  );
+
+  return (
+    <div className="vads-u-background-color--gray-lightest small-screen:vads-u-padding--2 medium-screen:vads-u-padding--3">
+      {!showProvidersList &&
+        !providerSelected && (
+          <button
+            className="va-button-link"
+            type="button"
+            onClick={() => setShowProvidersList(true)}
+          >
+            <i className="fas fa-plus vads-u-padding-right--0p5" />
+            Choose a provider
+          </button>
+        )}
+      {!showProvidersList &&
+        providerSelected && (
+          <>
+            <span className="vads-u-display--block vads-u-font-weight--bold">
+              {formData?.name}
+            </span>
+            <span className="vads-u-display--block">
+              {formData?.address?.line}
+            </span>
+            <span className="vads-u-display--block">
+              {formData?.address?.city}, {formData?.address?.state}{' '}
+              {formData?.address?.postalCode}
+            </span>
+            <span className="vads-u-display--block vads-u-font-size--sm vads-u-font-weight--bold">
+              {distanceBetween(
+                formData?.position?.latitude,
+                formData?.position?.longitude,
+                address.latitude,
+                address.longitude,
+              )}{' '}
+              miles
+            </span>
+            <div className="vads-u-display--flex">
+              <button
+                type="button"
+                className="vaos-appts__cancel-btn va-button-link vads-u-margin--0 vads-u-flex--0 vads-u-margin-right--2"
+                onClick={() => {
+                  setProvidersListLength(INITIAL_PROVIDER_DISPLAY_COUNT);
+                  setShowProvidersList(true);
+                }}
+              >
+                Change Provider
+              </button>
+            </div>
+          </>
+        )}
+      {loadingProviders &&
+        showProvidersList && (
+          <div className="vads-u-padding-bottom--2">
+            <LoadingIndicator message="Loading the list of providers" />
+          </div>
+        )}
+      {requestStatus === FETCH_STATUS.failed &&
+        showProvidersList && (
+          <div className="vads-u-padding-bottom--2">
+            <ErrorMessage />
+          </div>
+        )}
+      {requestStatus === FETCH_STATUS.succeeded &&
+        showProvidersList && (
+          <>
+            <h2 className="vads-u-font-size--h3 vads-u-margin-top--0">
+              Choose a provider
+            </h2>
+            <p>Your address on file:</p>
+            <ResidentialAddress address={address} />
+            <p>
+              Displaying 1 to {currentlyShownProvidersList.length} of{' '}
+              {communityCareProviderList.length} providers
+            </p>
+            {currentlyShownProvidersList.map(provider => {
+              const { name, position } = provider;
+              const checked = provider.id === checkedProvider;
+              const distance = distanceBetween(
+                position.latitude,
+                position.longitude,
+                address.latitude,
+                address.longitude,
+              );
+              return (
+                <div className="form-radio-buttons" key={provider.id}>
+                  <input
+                    type="radio"
+                    checked={checked}
+                    id={`${idSchema.$id}_${provider.id}`}
+                    name={`${idSchema.$id}`}
+                    value={provider.id}
+                    onChange={_ => setCheckedProvider(provider.id)}
+                    disabled={loadingProviders}
+                  />
+                  <label htmlFor={`${idSchema.$id}_${provider.id}`}>
+                    <span className="vads-u-display--block vads-u-font-weight--bold">
+                      {name}
+                    </span>
+                    <span className="vads-u-display--block">
+                      {provider.address?.line}
+                    </span>
+                    <span className="vads-u-display--block">
+                      {provider.address.city}, {provider.address.state}{' '}
+                      {provider.address.postalCode}
+                    </span>
+                    <span className="vads-u-display--block vads-u-font-size--sm vads-u-font-weight--bold">
+                      {distance} miles
+                    </span>
+                  </label>
+                  {checked && (
+                    <button
+                      type="button"
+                      onClick={() => {
+                        onChange(
+                          communityCareProviderList.find(
+                            p => p.id === checkedProvider,
+                          ),
+                        );
+                        setCheckedProvider();
+                        setShowProvidersList(false);
+                      }}
+                    >
+                      Choose Provider
+                    </button>
+                  )}
+                </div>
+              );
+            })}
+          </>
+        )}
+      {requestStatus === FETCH_STATUS.succeeded &&
+        showProvidersList && (
+          <div className="vads-u-display--flex">
+            {requestStatus === FETCH_STATUS.succeeded &&
+              !loadingProviders &&
+              showProvidersList &&
+              providersListLength < communityCareProviderList.length && (
+                <>
+                  <button
+                    type="button"
+                    className="additional-info-button va-button-link vads-u-display--block vads-u-margin-right--2"
+                    onClick={() =>
+                      setProvidersListLength(providersListLength + 5)
+                    }
+                  >
+                    <span className="additional-info-title">
+                      +{' '}
+                      {Math.min(
+                        communityCareProviderList.length - providersListLength,
+                        INITIAL_PROVIDER_DISPLAY_COUNT,
+                      )}{' '}
+                      more providers
+                      <i className="fas fa-angle-down vads-u-padding-right--0p5" />
+                    </span>
+                  </button>
+                  <button
+                    type="button"
+                    className="vaos-appts__cancel-btn va-button-link vads-u-margin--0 vads-u-flex--0"
+                    onClick={() => {
+                      setProvidersListLength(INITIAL_PROVIDER_DISPLAY_COUNT);
+                      setShowProvidersList(false);
+                    }}
+                  >
+                    Cancel
+                  </button>
+                </>
+              )}
+          </div>
+        )}
+    </div>
+  );
 }
+
+function mapStateToProps(state) {
+  return selectProviderSelectionInfo(state);
+}
+
+const mapDispatchToProps = {
+  requestProvidersList: actions.requestProvidersList,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ProviderSelectionField);
--- src/applications/vaos/tests/new-appointment/components/CommunityCareProviderSelectionPage/index.unit.spec.jsx
+++ src/applications/vaos/tests/new-appointment/components/CommunityCareProviderSelectionPage/index.unit.spec.jsx
@@ -14,9 +14,13 @@ import { getParentSiteMock } from '../../../mocks/v0';
 import {
   mockCommunityCareEligibility,
   mockParentSites,
+  mockCCProviderFetch,
 } from '../../../mocks/helpers';
 
 import CommunityCareProviderSelectionPage from '../../../../new-appointment/components/CommunityCareProviderSelectionPage';
+import { waitFor } from '@testing-library/dom';
+import { calculateBoundingBox } from '../../../../utils/address';
+import { CC_PROVIDERS_DATA } from './cc_providers_data';
 
 const initialState = {
   featureToggles: {
@@ -25,13 +29,22 @@ const initialState = {
   user: {
     profile: {
       facilities: [{ facilityId: '983', isCerner: false }],
+      vapContactInfo: {
+        residentialAddress: {
+          addressLine1: '123 big sky st',
+          city: 'Cincinnati',
+          stateCode: 'OH',
+          zipCode: '45220',
+          latitude: 39.1,
+          longitude: -84.6,
+        },
+      },
     },
   },
 };
 describe('VAOS <CommunityCareProviderSelectionPage>', () => {
-  beforeEach(() => mockFetch());
-  afterEach(() => resetFetch());
-  it('should display closest city question when user has multiple supported sites', async () => {
+  beforeEach(() => {
+    mockFetch();
     mockParentSites(
       ['983'],
       [
@@ -73,6 +86,19 @@ describe('VAOS <CommunityCareProviderSelectionPage>', () => {
       supportedSites: ['983', '983GJ'],
       careType: 'PrimaryCare',
     });
+    mockCCProviderFetch(
+      initialState.user.profile.vapContactInfo.residentialAddress,
+      ['208D00000X', '207R00000X', '261QP2300X'],
+      calculateBoundingBox(
+        initialState.user.profile.vapContactInfo.residentialAddress.latitude,
+        initialState.user.profile.vapContactInfo.residentialAddress.longitude,
+        60,
+      ),
+      CC_PROVIDERS_DATA,
+    );
+  });
+  afterEach(() => resetFetch());
+  it('should display closest city question when user has multiple supported sites', async () => {
     const store = createTestStore(initialState);
     await setTypeOfCare(store, /primary care/i);
     await setTypeOfFacility(store, /Community Care/i);
@@ -86,11 +112,133 @@ describe('VAOS <CommunityCareProviderSelectionPage>', () => {
     expect((await screen.findAllByRole('radio')).length).to.equal(2);
     expect(screen.getByLabelText('Bozeman, MT')).to.exist;
     expect(screen.getByLabelText('Belgrade, MT')).to.exist;
+    expect(screen.baseElement).to.contain.text(
+      'Request a Primary care provider. (Optional)',
+    );
+    expect(screen.baseElement).to.contain.text(
+      'You can request a provider you’d prefer for this appointment. If they aren’t available, we’ll schedule your appointment with a provider close to your home.',
+    );
+    expect(screen.baseElement).to.contain.text('Choose a provider');
 
     // Continue without filling in required fields
     userEvent.click(screen.getByText(/Continue/i));
 
     expect((await screen.findAllByRole('alert')).length).to.equal(2);
     expect(screen.history.push.called).to.be.false;
+  });
+
+  it('should display list of providers when choose a provider clicked', async () => {
+    const store = createTestStore(initialState);
+    await setTypeOfCare(store, /primary care/i);
+    await setTypeOfFacility(store, /Community Care/i);
+    const screen = renderWithStoreAndRouter(
+      <CommunityCareProviderSelectionPage />,
+      {
+        store,
+      },
+    );
+
+    // Trigger provider list loading
+    userEvent.click(await screen.findByText(/Choose a provider/i));
+    await waitFor(() => screen.getByText(/your address on file:/i));
+
+    // Verify provider list count and get load more button
+    expect(screen.baseElement).to.contain.text(
+      '123 big sky stCincinnati, OH 45220',
+    );
+    expect(screen.baseElement).to.contain.text(
+      'Displaying 1 to 5 of 16 providers',
+    );
+    expect((await screen.findAllByRole('radio')).length).to.equal(7);
+
+    userEvent.click(await screen.findByText(/\+ 5 more providers/i));
+    await waitFor(() =>
+      screen.getByText(/displaying 1 to 10 of 16 providers/i),
+    );
+    expect((await screen.findAllByRole('radio')).length).to.equal(12);
+
+    userEvent.click(await screen.findByText(/\+ 5 more providers/i));
+    await waitFor(() =>
+      screen.getByText(/displaying 1 to 15 of 16 providers/i),
+    );
+    expect((await screen.findAllByRole('radio')).length).to.equal(17);
+
+    userEvent.click(await screen.findByText(/\+ 1 more providers/i));
+    await waitFor(() =>
+      screen.getByText(/displaying 1 to 16 of 16 providers/i),
+    );
+    expect((await screen.findAllByRole('radio')).length).to.equal(18);
+
+    // Choose Provider
+    userEvent.click((await screen.findAllByRole('radio'))[3]);
+    userEvent.click(
+      await waitFor(() =>
+        screen.getByRole('button', { name: /choose provider/i }),
+      ),
+    );
+    expect(screen.baseElement).to.contain.text(
+      'AJADI, ADEDIWURA700 CONSTITUTION AVE NEWASHINGTON, DC 20002-65999349.3 miles',
+    );
+
+    // Change Provider
+    userEvent.click(
+      await screen.getByRole('button', { name: /change provider/i }),
+    );
+    userEvent.click((await screen.findAllByRole('radio'))[4]);
+    userEvent.click(
+      await waitFor(() =>
+        screen.getByRole('button', { name: /choose provider/i }),
+      ),
+    );
+    expect(screen.baseElement).to.contain.text(
+      'LYONS, KRISTYN1785 S HAYES STARLINGTON, VA 22202-27149347.1 miles',
+    );
+
+    // Cancel Selection (not clearing of a selected provider)
+    userEvent.click(
+      await screen.getByRole('button', { name: /change provider/i }),
+    );
+    await waitFor(() => screen.getByText(/displaying 1 to 5 of 16 providers/i));
+    await waitFor(() => screen.getByRole('button', { name: /cancel/i }));
+    expect(screen.baseElement).to.contain.text(
+      'LYONS, KRISTYN1785 S HAYES STARLINGTON, VA 22202-27149347.1 miles',
+    );
+  });
+  // TODO: show more
+  // TODO: provider fetch failure
+  it('should display an error when choose a provider clicked and provider fetch error', async () => {
+    const store = createTestStore(initialState);
+    await setTypeOfCare(store, /primary care/i);
+    await setTypeOfFacility(store, /Community Care/i);
+    mockCCProviderFetch(
+      initialState.user.profile.vapContactInfo.residentialAddress,
+      ['208D00000X', '207R00000X', '261QP2300X'],
+      calculateBoundingBox(
+        initialState.user.profile.vapContactInfo.residentialAddress.latitude,
+        initialState.user.profile.vapContactInfo.residentialAddress.longitude,
+        60,
+      ),
+      CC_PROVIDERS_DATA,
+      true,
+    );
+    const screen = renderWithStoreAndRouter(
+      <CommunityCareProviderSelectionPage />,
+      {
+        store,
+      },
+    );
+
+    // Trigger provider list loading
+    userEvent.click(await screen.findByText(/Choose a provider/i));
+    expect(
+      await screen.getByRole('heading', {
+        name: /we’re sorry\. we’ve run into a problem/i,
+      }),
+    );
+    expect(
+      await screen.getByText(
+        /something went wrong on our end. please try again later./i,
+      ),
+    );
   });
 });
