cdea6ef1c282303b1d9668925a3f541b1464793e
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-12-01 11:24:22 -0500

    small tweak

--- src/site/stages/build/process-cms-exports/schemas/output/node-q_a.js
+++ src/site/stages/build/process-cms-exports/schemas/output/node-q_a.js
@@ -3,9 +3,8 @@ module.exports = {
   properties: {
     entityType: { type: 'string', enum: ['node'] },
     entityBundle: { type: 'string', enum: ['q_a'] },
+    entityUrl: { $ref: 'EntityUrl' },
     title: { type: 'string' },
   },
-  required: [
-    'title',
-  ],
+  required: ['title'],
 };
