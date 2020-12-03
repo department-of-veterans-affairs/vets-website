013173510eddd9abeb112a09661647785b839aa5
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-12-03 14:10:08 -0500

    [department-of-veterans-affairs/orchid#190] Uses sentence casing for inquiry status
    Co-authored-by: Paul Phillips <paul.phillips@thoughtworks.com>

--- src/applications/messages/actions/index.js
+++ src/applications/messages/actions/index.js
@@ -1,15 +1,18 @@
-import { apiRequest } from 'platform/utilities/api';
+import _ from 'lodash';
 import moment from 'moment';
+
+import { apiRequest } from 'platform/utilities/api';
 
 export const FETCH_INQUIRIES_SUCCESS = 'FETCH_INQUIRIES_SUCCESS';
 
-const processInquiries = inquiries => {
+export const processInquiries = inquiries => {
   return inquiries.map(inquiry => {
     return {
       ...inquiry,
       dateLastUpdated: moment(inquiry.lastActiveTimestamp).format(
         'MMMM D, YYYY',
       ),
+      status: _.capitalize(inquiry.status),
     };
   });
 };
--- src/applications/messages/tests/actions.unit.spec.js
+++ src/applications/messages/tests/actions.unit.spec.js
@@ -3,7 +3,40 @@ import sinon from 'sinon';
 
 import { mockApiRequest, resetFetch } from 'platform/testing/unit/helpers';
 
-import { fetchInquiries, FETCH_INQUIRIES_SUCCESS } from '../actions';
+import {
+  processInquiries,
+  fetchInquiries,
+  FETCH_INQUIRIES_SUCCESS,
+} from '../actions';
+
+describe('processInquiries', () => {
+  const mockData = [
+    {
+      subject: 'Prosthetics',
+      confirmationNumber: '000-010',
+      status: 'OPEN',
+      creationTimestamp: '2020-11-01T14:58:00',
+      lastActiveTimestamp: '2020-11-04T13:00:00',
+      links: {
+        thread: {
+          href: '/v1/user/{:user-id}/inquiry/000-010',
+        },
+      },
+    },
+  ];
+
+  it('formats dateLastUpdated', () => {
+    const result = processInquiries(mockData);
+
+    expect(result[0].dateLastUpdated).to.eql('November 4, 2020');
+  });
+
+  it('formats status', () => {
+    const result = processInquiries(mockData);
+
+    expect(result[0].status).to.eql('Open');
+  });
+});
 
 describe('fetchInquiries', () => {
   const mockData = {
@@ -14,7 +47,7 @@ describe('fetchInquiries', () => {
         status: 'OPEN',
         creationTimestamp: '2020-11-01T14:58:00',
         lastActiveTimestamp: '2020-11-04T13:00:00',
-        _links: {
+        links: {
           thread: {
             href: '/v1/user/{:user-id}/inquiry/000-010',
           },
