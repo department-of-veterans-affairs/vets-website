c15002f250a33f46662aee08651c0bfab80ef7ba
Craig Wheeler <craig@craigwheeler.net>
2020-12-03 13:41:57 -0500

    group chapters by folder

--- src/applications/financial-status-report/pages/householdExpenses/householdRepayments.js
+++ src/applications/financial-status-report/pages/householdExpenses/householdRepayments.js
@@ -1,5 +1,5 @@
-import ItemLoop from '../components/ItemLoop';
-import CardDetailsView from '../components/CardDetailsView';
+import ItemLoop from '../../components/ItemLoop';
+import CardDetailsView from '../../components/CardDetailsView';
 
 export const uiSchema = {
   'ui:title': 'Your installment contracts and other repayments',
