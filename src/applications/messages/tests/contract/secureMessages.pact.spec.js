da5f5f94ada54b2ea085c40651cdbeba5ba65bdd
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-12-04 14:07:05 -0500

    [department-of-veterans-affairs/orchid#190] updates inquiries endpoint in pact test
    Co-authored-by: Paul Phillips <paul.phillips@thoughtworks.com>

--- src/applications/messages/tests/contract/secureMessages.pact.spec.js
+++ src/applications/messages/tests/contract/secureMessages.pact.spec.js
@@ -11,14 +11,14 @@ import { fetchInquiries } from '../../actions/';
 import contractTest from 'platform/testing/contract';
 
 contractTest('My Messages', 'VA.gov API', mockApi => {
-  describe('GET /messages/inquiries', () => {
+  describe('GET /ask/inquiries', () => {
     it('returns a list of messages for user', async () => {
       await mockApi().addInteraction({
         state: 'logged in user with messages',
         uponReceiving: 'a GET request',
         withRequest: {
           method: 'GET',
-          path: '/v0/messages/inquiries',
+          path: '/v0/ask/inquiries',
           headers: {
             'X-Key-Inflection': 'camel',
           },
@@ -59,7 +59,7 @@ contractTest('My Messages', 'VA.gov API', mockApi => {
         uponReceiving: 'a GET request',
         withRequest: {
           method: 'GET',
-          path: '/v0/messages/inquiries',
+          path: '/v0/ask/inquiries',
           headers: {
             'X-Key-Inflection': 'camel',
           },
