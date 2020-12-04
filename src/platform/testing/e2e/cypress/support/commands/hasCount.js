a1bd1cb93f26c59e23e9b8fa0200fe4f0c0a2999
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-04 08:33:55 -0500

    refactor methods

--- src/platform/testing/e2e/cypress/support/commands/hasCount.js
+++ src/platform/testing/e2e/cypress/support/commands/hasCount.js
@@ -8,7 +8,7 @@ const elementsWithinCount = (elements, selector, count, msg) => {
   });
 };
 
-const sharedElements = [
+const commonElements = [
   'a[href]',
   'button',
   'details',
@@ -30,43 +30,40 @@ const sharedElements = [
   'textarea',
 ];
 
+const focusableElements = [
+  'input[type="radio"]',
+  '[tabindex="0"]',
+  '[tabindex="-1"]',
+];
+
+const tabbableElements = [
+  'input[type="radio"]:checked',
+  '[tabindex]:not([tabindex="-1"])',
+];
+
 /**
  * Checks if the count of focusable elements is correct. Focusable elements are those
  * in the normal tab order (native focusable elements or those with tabIndex 0).
  * The count logic will break on tabindexes > 0 because we do not want to override the
  * browser's base tab order.
- *
- * This solution is inspired by two blog posts:
- * https://zellwk.com/blog/keyboard-focusable-elements/
- * https://hiddedevries.nl/en/blog/2017-01-29-using-javascript-to-trap-focus-in-an-element
  */
-export const hasFocusableCount = (selector, count) => {
-  const focusableElements = [
-    'input[type="radio"]',
-    '[tabindex="0"]',
-    '[tabindex="-1"]',
-  ];
-  const elements = [...sharedElements, ...focusableElements];
-  const msg = `Page does not contain ${count} focusable elements.`;
-  elementsWithinCount(elements, selector, count, msg);
-};
 
 /**
  * Checks if the count of tabbable elements is correct. Tabbable elements are those
  * in the normal tab order (native focusable elements or those with tabIndex >= 0).
  */
-export const hasTabbableCount = (selector, count) => {
-  const tabbableElements = [
-    'input[type="radio"]:checked',
-    '[tabindex]:not([tabindex="-1"])',
-  ];
-
-  const elements = [...sharedElements, ...tabbableElements];
-  const msg = `Page does not contain ${count} tabbable elements.`;
-  elementsWithinCount(elements, selector, count, msg);
-};
 
 Cypress.Commands.add('hasCount', (elementCategory, selector, count) => {
-  if (elementCategory === 'focusable') hasFocusableCount(selector, count);
-  if (elementCategory === 'tabbable') hasTabbableCount(selector, count);
+  let elements;
+  let msg;
+
+  if (elementCategory === 'focusable') {
+    elements = [...commonElements, ...focusableElements];
+    msg = `Page does not contain ${count} focusable elements.`;
+  } else if (elementCategory === 'tabbable') {
+    elements = [...commonElements, ...tabbableElements];
+    msg = `Page does not contain ${count} tabbable elements.`;
+  }
+
+  elementsWithinCount(elements, selector, count, msg);
 });
