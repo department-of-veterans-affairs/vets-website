1e1d03365dfbd869bd9be30613390c23e085b5ac
Fareez Ahmed <fareezahmed@Fareez-GCIO.local>
2020-12-04 20:52:49 -0800

    save work so far

--- src/applications/static-pages/i18Content.js
+++ src/applications/static-pages/i18Content.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
+// this can be easily replaced with an API call. this follows the pattern of i18 here, namely using a json file
+// to store content in differnt languages
+// https://react.i18next.com/getting-started
 const i18Content = {
   en: { linkTitle: 'Read this page in English' },
   es: { linkTitle: 'Leer esta pagina en espanol' },
 };
 function toggleLanguage(e) {
-  //   console.log(e, 'EVENT');
   if (e.target.dataset.lang === 'en') {
     e.target.dataset.lang = 'es';
     e.target.innerText = i18Content.en.linkTitle;
