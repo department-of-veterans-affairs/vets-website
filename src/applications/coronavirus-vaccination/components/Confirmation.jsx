a3f691309d2d71d9cb4730f3e576ce6246d8ab0a
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-12-06 11:16:21 -0500

    redirect from /confirmation on direct nav

--- src/applications/coronavirus-vaccination/components/Confirmation.jsx
+++ src/applications/coronavirus-vaccination/components/Confirmation.jsx
@@ -3,12 +3,40 @@ import AlertBox, {
   ALERT_TYPE,
 } from '@department-of-veterans-affairs/formation-react/AlertBox';
 
-export default function Confirmation() {
-  return (
-    <AlertBox
-      status={ALERT_TYPE.SUCCESS}
-      headline="Your application has been received"
-      content="Thank you"
-    />
-  );
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+
+function Confirmation({ router, formIsSubmitted }) {
+  if (formIsSubmitted) {
+    return (
+      <AlertBox
+        status={ALERT_TYPE.SUCCESS}
+        headline="Your application has been received"
+        content="Thank you"
+      />
+    );
+  }
+
+  // Redirect to the homepage if there isn't any form data in state.
+  // This is the case for direct navigation to "/confirmation/".
+
+  router.replace('/');
+  return null;
 }
+
+const mapStateToProps = state => {
+  return {
+    formIsSubmitted: state.coronavirusVaccinationApp.formState !== null,
+  };
+};
+
+const mapDispatchToProps = {};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(Confirmation),
+);
+
+export { Confirmation };
