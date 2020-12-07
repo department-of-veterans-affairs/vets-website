460ee493bcf57803f4b5f39055751b04238c3551
Mark Dewey <mdewey@governmentcio.com>
2020-12-07 16:44:24 -0500

    updated tests

--- src/applications/health-care-questionnaire/list/tests/e2e/00.home.cypress.spec.js
+++ src/applications/health-care-questionnaire/list/tests/e2e/00.home.cypress.spec.js
@@ -2,11 +2,11 @@ import basicUser from './fixtures/users/user-basic.json';
 
 it('healthcare questionnaire list -- loads manager page -- feature enabled', () => {
   cy.fixture(
-    '../../src/applications/healthcare/questionnaire/tests/e2e/fixtures/mocks/feature-toggles.enabled.json',
+    '../../src/applications/health-care-questionnaire/questionnaire/tests/e2e/fixtures/mocks/feature-toggles.enabled.json',
   ).then(features => {
     cy.route('GET', '/v0/feature_toggles*', features);
     cy.login(basicUser);
-    cy.visit('/healthcare/list');
+    cy.visit('/health-care/health-questionnaires/questionnaires/');
     cy.get('h1').contains('Your health questionnaires');
     cy.injectAxe();
     cy.axeCheck();
@@ -15,10 +15,10 @@ it('healthcare questionnaire list -- loads manager page -- feature enabled', () 
 
 it('healthcare questionnaire  list-- can not manager page -- feature disabled', () => {
   cy.fixture(
-    '../../src/applications/healthcare/questionnaire/tests/e2e/fixtures/mocks/feature-toggles.disabled.json',
+    '../../src/applications/health-care-questionnaire/questionnaire/tests/e2e/fixtures/mocks/feature-toggles.disabled.json',
   ).then(features => {
     cy.route('GET', '/v0/feature_toggles*', features);
-    const featureRoute = '/healthcare/list';
+    const featureRoute = '/health-care/health-questionnaires/questionnaires/';
     cy.visit(featureRoute);
     cy.url().should('not.match', /healthcare/);
   });
