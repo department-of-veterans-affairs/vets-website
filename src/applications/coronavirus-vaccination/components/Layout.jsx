9436aca9fbb5a6ae40e545c0ee7d2568a48a2841
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-12-07 14:47:32 -0500

    fix layout

--- src/applications/coronavirus-vaccination/components/Layout.jsx
+++ src/applications/coronavirus-vaccination/components/Layout.jsx
@@ -6,14 +6,16 @@ import * as userSelectors from 'platform/user/selectors';
 
 function Layout({ isProfileLoading, children }) {
   return (
-    <div className="usa-grid usa-grid-full">
-      <div className="usa-content vads-u-margin-bottom--3">
-        <h1>Request a COVID-19 vaccination</h1>
-        {isProfileLoading ? (
-          <LoadingIndicator message="Loading your profile..." />
-        ) : (
-          children
-        )}
+    <div className="vads-l-grid-container vads-u-padding-x--2p5 large-screen:vads-u-padding-x--0 vads-u-padding-bottom--2p5">
+      <div className="vads-l-row">
+        <div className="vads-l-col--12">
+          <h1>Request a COVID-19 vaccination</h1>
+          {isProfileLoading ? (
+            <LoadingIndicator message="Loading your profile..." />
+          ) : (
+            children
+          )}
+        </div>
       </div>
     </div>
   );
