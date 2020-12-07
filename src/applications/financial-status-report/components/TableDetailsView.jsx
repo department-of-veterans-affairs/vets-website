6cb813b6c198ce87cda07fd0b6bce24eb81dc47e
Craig Wheeler <craig@craigwheeler.net>
2020-12-07 13:07:28 -0500

    set table vals

--- src/applications/financial-status-report/components/TableDetailsView.jsx
+++ src/applications/financial-status-report/components/TableDetailsView.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import startCase from 'lodash/startCase';
 
 const TableDetailsView = ({ formData, onEdit, index }) => {
+  const values = Object.values(formData);
+
   return (
     <tr className="vads-u-border-bottom--1px">
       <td className="vads-u-border--0 vads-u-padding-left--3">
-        {formData?.utilityType}
+        {startCase(values[0])}
       </td>
-      <td className="vads-u-border--0">{formData?.monthlyUtilityAmount}</td>
+      <td className="vads-u-border--0">{values[1]}</td>
       <td className="vads-u-border--0">
         <a
           target="_blank"
