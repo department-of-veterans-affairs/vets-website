d8debbda9c11fd73ecfdbb3820fd3ceed92f2bb6
Jennie McGibney <jennie.mcgibney@va.gov>
2020-12-08 14:51:27 -0500

    revert TabItem unit test

--- src/applications/vaos/tests/appointment-list/components/AppointmentsPage/TabItem.unit.spec.jsx
+++ src/applications/vaos/tests/appointment-list/components/AppointmentsPage/TabItem.unit.spec.jsx
@@ -17,7 +17,6 @@ describe('<TabItem>', () => {
     expect(tree.find('NavLink').props().to).to.equal('upcoming');
     tree.unmount();
   });
-
   it('should remove hyphens from aria-label', () => {
     const tree = shallow(
       <TabItem id="id-with-hyphens" title="Title Here" tabpath="upcoming" />,
@@ -29,12 +28,10 @@ describe('<TabItem>', () => {
     expect(tree.find('NavLink').props().to).to.equal('upcoming');
     tree.unmount();
   });
-
   it('should render active tab', () => {
     const tree = shallow(
       <TabItem id="id" isActive title="Title Here" tabpath="upcoming" />,
     );
-
     expect(tree.find('NavLink').props()['aria-controls']).to.equal(
       'tabpanelid',
     );
@@ -46,12 +43,10 @@ describe('<TabItem>', () => {
     expect(tree.find('NavLink').props().to).to.equal('upcoming');
     tree.unmount();
   });
-
   it('should call previous tab on arrow left', () => {
     const tree = shallow(
       <TabItem id="id" isActive title="Title Here" tabpath="upcoming" />,
     );
-
     expect(tree.find('NavLink').props()['aria-controls']).to.equal(
       'tabpanelid',
     );
@@ -63,7 +58,6 @@ describe('<TabItem>', () => {
     expect(tree.find('NavLink').props().to).to.equal('upcoming');
     tree.unmount();
   });
-
   it('should call next tab on arrow right', () => {
     const onNextTab = sinon.spy();
     const tree = shallow(
@@ -75,12 +69,10 @@ describe('<TabItem>', () => {
         onNextTab={onNextTab}
       />,
     );
-
     tree.find('NavLink').simulate('keydown', { key: 'ArrowRight' });
     expect(onNextTab.called).to.be.true;
     tree.unmount();
   });
-
   it('should call previous tab on arrow left', () => {
     const onPreviousTab = sinon.spy();
     const tree = shallow(
@@ -92,12 +84,10 @@ describe('<TabItem>', () => {
         onPreviousTab={onPreviousTab}
       />,
     );
-
     tree.find('NavLink').simulate('keydown', { key: 'ArrowLeft' });
     expect(onPreviousTab.called).to.be.true;
     tree.unmount();
   });
-
   it('should focus on tab panel on arrow down', () => {
     const tree = shallow(
       <TabItem id="id" isActive title="Title Here" tabpath="upcoming" />,
@@ -105,12 +95,10 @@ describe('<TabItem>', () => {
     const panel = document.createElement('div');
     panel.id = 'tabpanelid';
     document.body.appendChild(panel);
-
     tree.find('NavLink').simulate('keydown', { key: 'ArrowDown' });
     expect(document.activeElement.id).to.equal('tabpanelid');
     tree.unmount();
   });
-
   it('should render flex css for tab', () => {
     const tree = shallow(
       <TabItem id="id" title="Title Here" tabpath="upcoming" />,
@@ -118,7 +106,6 @@ describe('<TabItem>', () => {
     expect(tree.find('NavLink').props().className).to.contain(
       'vaos-appts__tab',
     );
-
     tree.unmount();
   });
 });
