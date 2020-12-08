8844e6b7bcc94283d9f5245108720312aacae52e
Matt Roth <roth_matthew@bah.com>
2020-12-08 10:01:59 -0500

    ac4

--- src/applications/edu-benefits/1990n/containers/ConfirmationPage.jsx
+++ src/applications/edu-benefits/1990n/containers/ConfirmationPage.jsx
@@ -66,8 +66,8 @@ class ConfirmationPage extends React.Component {
           <br />
           <i>Please print this page for your records.</i>
         </p>
-        <div className="inset vads-u-margin-top--0">
-          <h4>
+        <div className="inset">
+          <h4 className="vads-u-margin-top--0">
             Education benefit application{' '}
             <span className="additional">(Form 22-1990N)</span>
           </h4>
@@ -115,18 +115,31 @@ class ConfirmationPage extends React.Component {
           </ul>
         </div>
         <div className="confirmation-guidance-container">
-          <h4 className="confirmation-guidance-heading">
-            What happens after I apply?
-          </h4>
+          <p>
+            <h4 className="confirmation-guidance-heading">
+              What happens after I apply?
+            </h4>
+          </p>
           <p className="confirmation-guidance-message">
-            <a href="/education/after-you-apply/">
-              Find out what happens after you apply
+            We usually decide on applications within 30 days.
+          </p>
+          <p>
+            You’ll get a Certificate of Eligibility (COE) or decision letter in
+            the mail. If we’ve approved your application, you can bring the COE
+            to the VA certifying official at your school.
+          </p>
+          <p>
+            <a href="/education/after-you-apply/" className="screen-only">
+              Learn more about what happens after you apply
             </a>
           </p>
-          <h4 className="confirmation-guidance-heading">Need help?</h4>
+          <h4 className="confirmation-guidance-heading vads-u-border-bottom--3px vads-u-border-color--primary vads-u-line-height--4">
+            Need help?
+          </h4>
+
           <p className="confirmation-guidance-message">
-            If you have questions, call 888-GI-BILL-1 (
-            <a href="tel:+18884424551">888-442-4551</a>
+            If you have questions, call 1-888-GI-BILL-1 (
+            <a href="tel:+18884424551">1-888-442-4551</a>
             ), Monday &#8211; Friday, 8:00 a.m. &#8211; 7:00 p.m. ET.
           </p>
         </div>
