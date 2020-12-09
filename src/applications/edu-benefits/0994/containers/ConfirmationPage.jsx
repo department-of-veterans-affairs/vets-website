6bc82fd06b3250191f4ac5e46106417ec8cb7931
Matt Roth <roth_matthew@bah.com>
2020-12-09 11:00:19 -0500

    AC3

--- src/applications/edu-benefits/0994/containers/ConfirmationPage.jsx
+++ src/applications/edu-benefits/0994/containers/ConfirmationPage.jsx
@@ -53,7 +53,7 @@ class ConfirmationPage extends React.Component {
     return (
       <div>
         <h3 className="confirmation-page-title">
-          Your claim has been received
+          We’ve received your application
         </h3>
         <p>
           We usually process claims within <strong>30 days</strong>.
@@ -77,8 +77,9 @@ class ConfirmationPage extends React.Component {
           }
         />
         <div className="inset">
-          <h4>
-            Education Claim <span className="additional">(Form 22-0994)</span>
+          <h4 className="vads-u-margin-top--0">
+            Education benefit application{' '}
+            <span className="additional">(Form 22-0994)</span>
           </h4>
           <span>
             for {name && name.first} {name && name.middle} {name && name.last}{' '}
@@ -127,27 +128,38 @@ class ConfirmationPage extends React.Component {
           </div>
         )}
         <div className="confirmation-guidance-container">
-          <h4 className="confirmation-guidance-heading">
-            What happens after I apply?
-          </h4>
+          <p>
+            <h4 className="confirmation-guidance-heading">
+              What happens after I apply?
+            </h4>
+          </p>
           <p className="confirmation-guidance-message">
-            <a href="/education/after-you-apply/">
-              Find out what happens after you apply
+            We usually decide on applications within 30 days.
+          </p>
+          <p>
+            You’ll get a Certificate of Eligibility (COE) or decision letter in
+            the mail. If we’ve approved your application, you can bring the COE
+            to the VA certifying official at your school.
+          </p>
+          <p>
+            <a href="/education/after-you-apply/" className="screen-only">
+              Learn more about what happens after you apply
             </a>
           </p>
-          <h4 className="confirmation-guidance-heading">Need help?</h4>
+          <h4 className="confirmation-guidance-heading vads-u-border-bottom--3px vads-u-border-color--primary vads-u-line-height--4">
+            Need help?
+          </h4>
+
           <p className="confirmation-guidance-message">
             If you have questions, call 1-888-GI-BILL-1 (
-            <a href="tel:+18884424551">888-442-4551</a>
+            <a href="tel:+18884424551">1-888-442-4551</a>
             ), Monday &#8211; Friday, 8:00 a.m. &#8211; 7:00 p.m. ET.
           </p>
         </div>
-        <div className="row form-progress-buttons schemaform-back-buttons">
-          <div className="small-6 usa-width-one-half medium-6 columns">
-            <a href="/">
-              <button className="usa-button-primary">Go back to VA.gov</button>
-            </a>
-          </div>
+        <div className="form-progress-buttons schemaform-back-buttons">
+          <a href="/">
+            <button className="usa-button-primary">Go back to VA.gov</button>
+          </a>
         </div>
       </div>
     );
