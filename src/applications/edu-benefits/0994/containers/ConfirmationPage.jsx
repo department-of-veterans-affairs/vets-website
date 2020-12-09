986a291ec5a8710e115b8350dafe68636f768dad
Matt Roth <roth_matthew@bah.com>
2020-12-09 11:41:51 -0500

    Guidance and return

--- src/applications/edu-benefits/0994/containers/ConfirmationPage.jsx
+++ src/applications/edu-benefits/0994/containers/ConfirmationPage.jsx
@@ -10,6 +10,8 @@ import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox';
 import {
   ConfirmationPageTitle,
   ConfirmationPageSummary,
+  ConfirmationGuidance,
+  ReturnHome,
 } from '../../components/ConfirmationPage';
 
 const scroller = Scroll.scroller;
@@ -95,40 +97,8 @@ class ConfirmationPage extends React.Component {
             </div>
           </div>
         )}
-        <div className="confirmation-guidance-container">
-          <p>
-            <h4 className="confirmation-guidance-heading">
-              What happens after I apply?
-            </h4>
-          </p>
-          <p className="confirmation-guidance-message">
-            We usually decide on applications within 30 days.
-          </p>
-          <p>
-            You’ll get a Certificate of Eligibility (COE) or decision letter in
-            the mail. If we’ve approved your application, you can bring the COE
-            to the VA certifying official at your school.
-          </p>
-          <p>
-            <a href="/education/after-you-apply/" className="screen-only">
-              Learn more about what happens after you apply
-            </a>
-          </p>
-          <h4 className="confirmation-guidance-heading vads-u-border-bottom--3px vads-u-border-color--primary vads-u-line-height--4">
-            Need help?
-          </h4>
-
-          <p className="confirmation-guidance-message">
-            If you have questions, call 1-888-GI-BILL-1 (
-            <a href="tel:+18884424551">1-888-442-4551</a>
-            ), Monday &#8211; Friday, 8:00 a.m. &#8211; 7:00 p.m. ET.
-          </p>
-        </div>
-        <div className="form-progress-buttons schemaform-back-buttons">
-          <a href="/">
-            <button className="usa-button-primary">Go back to VA.gov</button>
-          </a>
-        </div>
+        <ConfirmationGuidance />
+        <ReturnHome />
       </div>
     );
   }
