ea2595271207d102916eda18564b69f487984346
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-12-09 12:14:06 -0500

    update chunk name

--- src/applications/static-pages/covid-vaccine-updates-cta/createCovidVaccineUpdatesWidget.js
+++ src/applications/static-pages/covid-vaccine-updates-cta/createCovidVaccineUpdatesWidget.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 export default function createCovidVaccineUpdatesWidget(store, widgetType) {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   if (root) {
-    import(/* webpackChunkName: "CaregiverContentToggle" */
+    import(/* webpackChunkName: "CovidVaccineUpdatesCTA" */
     './widget').then(module => {
       const CovidVaccineUpdatesCTA = module.default;
       ReactDOM.render(<CovidVaccineUpdatesCTA store={store} />, root);
