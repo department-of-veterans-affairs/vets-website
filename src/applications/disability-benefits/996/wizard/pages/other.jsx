2983455e235bd0fbb45ab0f2de35d81205a32d0d
Robin Garrison <robin.garrison@adhocteam.us>
2020-12-09 09:47:59 -0600

    Fix nesting paragraph tag

--- src/applications/disability-benefits/996/wizard/pages/other.jsx
+++ src/applications/disability-benefits/996/wizard/pages/other.jsx
@@ -5,15 +5,15 @@ import DownloadLink from '../../content/DownloadLink';
 import { BENEFIT_OFFICES_URL } from '../../constants';
 
 const DecisionReviewPage = () => (
-  <p className="vads-u-background-color--gray-lightest vads-u-padding--2 vads-u-margin-top--2">
+  <div className="vads-u-background-color--gray-lightest vads-u-padding--2 vads-u-margin-top--2">
     You’ll need to fill out and submit VA form 20-0996 by mail or in person.
     Send the completed form to the{' '}
     <a href={BENEFIT_OFFICES_URL}>benefit office</a> that matches the benefit
     type you select on the form.
     <p className="vads-u-margin-bottom--0">
-      <DownloadLink content={'Download VA Form 20-0996'} />
+      <DownloadLink content={'Download VA Form 20-0996'} wizardEvent />
     </p>
-  </p>
+  </div>
 );
 
 export default {
