6f3f9041ff6e8c94fb7a0df4a55043be5c5f7167
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-10 11:39:36 -0500

    write pact test for get request to retrieve a saved application

--- src/applications/coronavirus-vaccination/tests/contract/apply.pact.spec.js
+++ src/applications/coronavirus-vaccination/tests/contract/apply.pact.spec.js
@@ -1,11 +1,45 @@
 import contractTest from 'platform/testing/contract';
 import { like, term } from '@pact-foundation/pact/dsl/matchers';
-import { apiPostRequest } from '../../apiCalls/';
+import { apiGetRequest, apiPostRequest } from '../../apiCalls/';
 import environment from 'platform/utilities/environment';
 import authenticatedApplicationData from '../cypress/fixtures/data/authenticated-coronavirus-vaccination-application.json';
 import unauthenticatedApplicationData from '../cypress/fixtures/data/unauthenticated-coronavirus-vaccination-application.json';
 
 contractTest('Coronavirus Vaccination', 'VA.gov API', mockApi => {
+  describe('GET /registration', () => {
+    it('Retrieves a saved application', async () => {
+      const url = `${environment.API_URL}/covid_vaccine/v0/registration`;
+
+      const interaction = {
+        state: 'authenticated user application data',
+        uponReceiving: 'a GET request',
+        withRequest: {
+          method: 'GET',
+          path: '/covid_vaccine/v0/registration',
+          headers: {
+            'X-Key-Inflection': 'camel',
+          },
+        },
+        willRespondWith: {
+          status: 200,
+          headers: {
+            'Content-Type': term({
+              matcher: '^application/json',
+              generate: 'application/json',
+            }),
+          },
+          body: {
+            data: like({}), // waiting for example data
+          },
+        },
+      };
+
+      await mockApi().addInteraction(interaction);
+
+      await apiGetRequest(url);
+    });
+  });
+
   describe('POST /registration', () => {
     it('Authenticated success case: submit valid form will return a 201 Created HTTP response', async () => {
       const url = `${environment.API_URL}/covid_vaccine/v0/registration`;
