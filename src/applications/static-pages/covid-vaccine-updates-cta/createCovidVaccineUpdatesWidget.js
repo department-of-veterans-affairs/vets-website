973f74a61d9506fffd9d419d58ab707bb9d5441e
Nick Sullivan <nick.sullivan@adhocteam.us>
2020-12-10 20:56:46 -0500

    Auto mount the vaccine updates widget

--- src/applications/static-pages/covid-vaccine-updates-cta/createCovidVaccineUpdatesWidget.js
+++ src/applications/static-pages/covid-vaccine-updates-cta/createCovidVaccineUpdatesWidget.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default function createCovidVaccineUpdatesWidget(store, widgetType) {
-  const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
-  if (root) {
+export default function createCovidVaccineUpdatesWidget(store, _widgetType) {
+  const isCovidVaccineUpdatesPage =
+    document.location.pathname === '/health-care/covid-19-vaccine/';
+
+  if (!isCovidVaccineUpdatesPage) {
+    return;
+  }
+
+  const introText = document.querySelector('.va-introtext');
+  const wrapper = introText.parentNode;
+  const reactRoot = document.createElement('div');
+
+  wrapper.insertBefore(reactRoot, introText);
+
+  if (reactRoot) {
     import(/* webpackChunkName: "CovidVaccineUpdatesCTA" */
     './widget').then(module => {
       const CovidVaccineUpdatesCTA = module.default;
-      ReactDOM.render(<CovidVaccineUpdatesCTA store={store} />, root);
+      ReactDOM.render(<CovidVaccineUpdatesCTA store={store} />, reactRoot);
     });
   }
 }
