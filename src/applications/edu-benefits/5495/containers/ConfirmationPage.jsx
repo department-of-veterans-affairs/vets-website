3a9cb6916ddfa944e41a9d305bd6ad325f26c2df
Joe <elder_joseph@bah.com>
2020-12-11 13:19:04 -0500

    16898: Add generic ConfirmationPage component to 5495; Include UX feedback; Add print page

--- src/applications/edu-benefits/5495/containers/ConfirmationPage.jsx
+++ src/applications/edu-benefits/5495/containers/ConfirmationPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import moment from 'moment';
 import { connect } from 'react-redux';
 import Scroll from 'react-scroll';
 
 import { focusElement } from 'platform/utilities/ui';
 
 import { survivorBenefitsLabels } from '../../utils/labels';
+import {
+  claimList,
+  ConfirmationGuidance,
+  ConfirmationPageSummary,
+  ConfirmationPageTitle,
+  ConfirmationReturnHome,
+} from '../../components/ConfirmationPage';
 
 const scroller = Scroll.scroller;
 const scrollToTop = () => {
@@ -35,90 +41,29 @@ class ConfirmationPage extends React.Component {
     const name = form.data.relativeFullName;
     const benefit = form.data.benefit;
 
+    const { formId, submission } = form;
+
+    const claimInfoList = claimList(response, submission);
+    claimInfoList.unshift(
+      <li key={'benefit'}>
+        <strong>Benefit to be transferred</strong>
+        <br />
+        {survivorBenefitsLabels[benefit]}
+      </li>,
+    );
+
     return (
       <div>
-        <h3 className="confirmation-page-title">
-          We’ve received your application.
-        </h3>
-        <p>
-          We usually process claims within <strong>30 days</strong>.
-        </p>
-        <p>
-          We may contact you for more information or documents.
-          <br />
-          <i>Please print this page for your records.</i>
-        </p>
-        <div className="inset">
-          <h4 className="vads-u-margin-top--0">
-            Education benefit application{' '}
-            <span className="additional">(Form 22-5495)</span>
-          </h4>
-          <span>
-            for {name.first} {name.middle} {name.last} {name.suffix}
-          </span>
-
-          <ul className="claim-list">
-            {benefit && (
-              <li>
-                <strong>Benefit</strong>
-                <br />
-                {survivorBenefitsLabels[benefit]}
-              </li>
-            )}
-            <li>
-              <strong>Confirmation number</strong>
-              <br />
-              <span>{response.confirmationNumber}</span>
-            </li>
-            <li>
-              <strong>Date received</strong>
-              <br />
-              <span>
-                {moment(form.submission.submittedAt).format('MMM D, YYYY')}
-              </span>
-            </li>
-            <li>
-              <strong>Your claim was sent to</strong>
-              <br />
-              <address className="schemaform-address-view">
-                {response.regionalOffice}
-              </address>
-            </li>
-          </ul>
-        </div>
-        <div className="confirmation-guidance-container">
-          <h4 className="confirmation-guidance-heading">
-            What happens after I apply?
-          </h4>
-          <p className="confirmation-guidance-message">
-            <div className="vads-u-margin-y--1">
-              We usually decide on applications within 30 days.
-            </div>
-            <div className="vads-u-margin-y--1">
-              You’ll get a Certificate of Eligibility (COE) or decision letter
-              in the mail. If we’ve approved your application, you can bring the
-              COE to the VA certifying official at your school.
-            </div>
-            <a href="/education/after-you-apply/">
-              Learn more about what happens after you apply
-            </a>
-          </p>
-          <h4 className="confirmation-guidance-heading pagebreak vads-u-border-bottom--3px vads-u-border-color--primary vads-u-line-height--4">
-            Need help?
-          </h4>
-          <p className="confirmation-guidance-message">
-            If you have questions, call 888-GI-BILL-1 (
-            <a href="tel:+18884424551">888-442-4551</a>
-            ), Monday &#8211; Friday, 8:00 a.m. &#8211; 7:00 p.m. ET.
-          </p>
-        </div>
-        <div className="row form-progress-buttons schemaform-back-buttons">
-          <div className="small-6 usa-width-one-half medium-6 columns">
-            <a href="/">
-              <button className="usa-button-primary">Go back to VA.gov</button>
-            </a>
-          </div>
-        </div>
+        <ConfirmationPageTitle formId={formId} />
+        <ConfirmationPageSummary
+          formId={formId}
+          response={response}
+          submission={submission}
+          name={name}
+          claimInfoList={claimInfoList}
+        />
+        <ConfirmationGuidance />
+        <ConfirmationReturnHome />
       </div>
     );
   }
--- src/applications/edu-benefits/5495/tests/containers/ConfirmationPage.unit.spec.jsx
+++ src/applications/edu-benefits/5495/tests/containers/ConfirmationPage.unit.spec.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { expect } from 'chai';
-import SkinDeep from 'skin-deep';
 
-import { ConfirmationPage } from '../../../5495/containers/ConfirmationPage';
+import { ConfirmationPage } from '../../containers/ConfirmationPage';
+import { shallow } from 'enzyme';
+import {
+  ConfirmationGuidance,
+  ConfirmationPageSummary,
+  ConfirmationPageTitle,
+  ConfirmationReturnHome,
+} from '../../../components/ConfirmationPage';
 
 const form = {
   submission: {
@@ -11,37 +17,23 @@ const form = {
     },
   },
   data: {
-    relativeFullName: {
+    veteranFullName: {
       first: 'Jane',
       last: 'Doe',
     },
-    benefit: 'chapter35',
+    benefit: 'chapter30',
   },
 };
 
-describe('Edu 5495 <ConfirmationPage>', () => {
+describe('<ConfirmationPage>', () => {
   it('should render', () => {
-    const tree = SkinDeep.shallowRender(<ConfirmationPage form={form} />);
+    const tree = shallow(<ConfirmationPage form={form} />);
+    expect(tree).to.not.be.undefined;
+    expect(tree.find(ConfirmationPageTitle)).to.not.be.undefined;
+    expect(tree.find(ConfirmationPageSummary)).to.not.be.undefined;
+    expect(tree.find(ConfirmationGuidance)).to.not.be.undefined;
+    expect(tree.find(ConfirmationReturnHome)).to.not.be.undefined;
 
-    expect(tree.subTree('.confirmation-page-title').text()).to.equal(
-      'We’ve received your application.',
-    );
-    expect(
-      tree
-        .everySubTree('span')[1]
-        .text()
-        .trim(),
-    ).to.equal('for Jane Doe');
-    expect(tree.everySubTree('p')[0].text()).to.contain(
-      'We usually process claims within 30 days.',
-    );
-    expect(tree.everySubTree('p')[1].text()).to.contain(
-      'We may contact you for more information or documents.Please print this page for your records',
-    );
-    expect(
-      tree.everySubTree('.confirmation-guidance-message')[0].text(),
-    ).to.contain(
-      'We usually decide on applications within 30 days.You’ll get a Certificate of Eligibility (COE) or decision letter in the mail. If we’ve approved your application, you can bring the COE to the VA certifying official at your school.Learn more about what happens after you apply',
-    );
+    tree.unmount();
   });
 });
