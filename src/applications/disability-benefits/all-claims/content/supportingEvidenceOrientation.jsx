d6dfe1d589259864b8a406ee96aaffbe5740a061
Nicholas Sprinkle <ndsprinkle@gmail.com>
2020-12-11 10:40:40 -0500

    Rework supporting evidence orienation to support the BDD flow along with claiming new and claiming increase

--- src/applications/disability-benefits/all-claims/content/supportingEvidenceOrientation.jsx
+++ src/applications/disability-benefits/all-claims/content/supportingEvidenceOrientation.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
+import { isClaimingNew, isClaimingIncrease } from '../utils';
 
-export const supportingEvidenceOrientation = (
-  <div>
-    <p>
-      On the next few screens, we’ll ask you where we can find evidence
-      (supporting documents like doctor’s reports, X-rays, and medical test
-      results) related to:
-    </p>
-    <ul>
-      <li>Your rated service-connected disabilities, if you have any</li>
-      <li>Your new service-connected disabilities or conditions</li>
-    </ul>
-    <p>
-      You don’t need to turn in any evidence that you submitted with an earlier
-      claim.{' '}
-      <strong>
-        You only need to submit new evidence that VA doesn’t already have.
-      </strong>
-    </p>
-  </div>
-);
+export const supportingEvidenceOrientation = ({ formData }) => {
+  return (
+    <div>
+      <p>
+        On the next few screens, we’ll ask you where we can find evidence
+        (supporting documents like doctor’s reports, X-rays, and medical test
+        results) related to:
+      </p>
+      <ul>
+        {isClaimingIncrease(formData) && (
+          <li>Your rated service-connected disabilities</li>
+        )}
+        {isClaimingNew(formData) && (
+          <li>Your new service-connected disabilities or conditions</li>
+        )}
+      </ul>
+      <p>
+        You don’t need to turn in any evidence that you submitted with an
+        earlier claim.{' '}
+        <strong>
+          You only need to submit new evidence that VA doesn’t already have.
+        </strong>
+      </p>
+    </div>
+  );
+};
