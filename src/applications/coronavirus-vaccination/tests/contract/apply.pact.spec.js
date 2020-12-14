d946f154f050edb0c9ea4fb2c6772cf01ce7b508
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-14 08:40:19 -0500

    update api module imports and tests for GET method to registration endpoint

--- src/applications/coronavirus-vaccination/tests/contract/apply.pact.spec.js
+++ src/applications/coronavirus-vaccination/tests/contract/apply.pact.spec.js
@@ -1,10 +1,6 @@
 import contractTest from 'platform/testing/contract';
 import { like, term } from '@pact-foundation/pact/dsl/matchers';
-import {
-  apiGetRequest,
-  apiPostRequest,
-  apiPatchRequest,
-} from '../../apiCalls/';
+import { retrievePreviouslySubmittedForm, saveForm } from '../../api/';
 import environment from 'platform/utilities/environment';
 import authenticatedApplicationData from '../cypress/fixtures/data/authenticated-coronavirus-vaccination-application.json';
 import unauthenticatedApplicationData from '../cypress/fixtures/data/unauthenticated-coronavirus-vaccination-application.json';
@@ -21,11 +17,11 @@ import unauthenticatedApplicationData from '../cypress/fixtures/data/unauthentic
 
 contractTest('Coronavirus Vaccination', 'VA.gov API', mockApi => {
   describe('GET /registration', () => {
-    it('authenticated case: request for saved registration will return a 200 OK HTTP response', async () => {
+    it('request for saved submission that exists returns a 200 OK HTTP response and the registration data', async () => {
       const url = `${environment.API_URL}/covid_vaccine/v0/registration`;
 
       const interaction = {
-        state: 'retreives saved registration for authenticated user',
+        state: 'retreives previously saved submission data for user',
         uponReceiving: 'a GET request',
         withRequest: {
           method: 'GET',
@@ -43,14 +39,70 @@ contractTest('Coronavirus Vaccination', 'VA.gov API', mockApi => {
             }),
           },
           body: {
-            data: like({}), // waiting for example data
+            data: {
+              id: like('BC79619E54A14BFF0D1607952232618112202'),
+              type: 'covid_vaccine_v0_registration_submissions',
+              attributes: {
+                createdAt: like('2020-12-14T13:23:52.929Z'),
+                vaccineInterest: like('INTERESTED'),
+                zipCode: like('10001'),
+                zipCodeDetails: like('Yes'),
+                phone: like('1112223333'),
+                email: like('test@example.com'),
+                firstName: like('JAMES'),
+                lastName: like('BECK'),
+                birthDate: like('1989-11-11'),
+              },
+            },
           },
         },
       };
 
       await mockApi().addInteraction(interaction);
 
-      await apiGetRequest(url);
+      await retrievePreviouslySubmittedForm(url);
+    });
+
+    it('request for saved registration that does not exist returns a 404 OK HTTP response and errors', async () => {
+      const url = `${environment.API_URL}/covid_vaccine/v0/registration`;
+
+      const interaction = {
+        state:
+          'does not retrieve perviously saved submission data for user becase it does not exist',
+        uponReceiving: 'a GET request',
+        withRequest: {
+          method: 'GET',
+          path: '/covid_vaccine/v0/registration',
+          headers: {
+            'X-Key-Inflection': 'camel',
+          },
+        },
+        willRespondWith: {
+          status: 404,
+          headers: {
+            'Content-Type': term({
+              matcher: '^application/json',
+              generate: 'application/json',
+            }),
+          },
+          body: {
+            data: {
+              errors: [
+                {
+                  title: 'Record not found',
+                  detail: 'The record identified by  could not be found',
+                  code: '404',
+                  status: '404',
+                },
+              ],
+            },
+          },
+        },
+      };
+
+      await mockApi().addInteraction(interaction);
+
+      await retrievePreviouslySubmittedForm(url);
     });
   });
 
@@ -92,7 +144,7 @@ contractTest('Coronavirus Vaccination', 'VA.gov API', mockApi => {
 
       await mockApi().addInteraction(interaction);
 
-      await apiPostRequest(url, authenticatedApplicationData);
+      await saveForm(url, authenticatedApplicationData);
     });
 
     it('unauthenticated success case: submit valid registration will return a 201 Created HTTP response', async () => {
@@ -132,7 +184,7 @@ contractTest('Coronavirus Vaccination', 'VA.gov API', mockApi => {
 
       await mockApi().addInteraction(interaction);
 
-      await apiPostRequest(url, unauthenticatedApplicationData);
+      await saveForm(url, unauthenticatedApplicationData);
     });
   });
 
@@ -169,7 +221,7 @@ contractTest('Coronavirus Vaccination', 'VA.gov API', mockApi => {
 
       await mockApi().addInteraction(interaction);
 
-      await apiPatchRequest(url, authenticatedApplicationData);
+      await saveForm(url, authenticatedApplicationData);
     });
   });
 });
