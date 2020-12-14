b3247c12d36d1ec1e73075bdaacc3c0da2e3f4f9
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-14 08:41:29 -0500

    delete unecessary test for PATCH method to url endpoint

--- src/applications/coronavirus-vaccination/tests/contract/apply.pact.spec.js
+++ src/applications/coronavirus-vaccination/tests/contract/apply.pact.spec.js
@@ -187,41 +187,4 @@ contractTest('Coronavirus Vaccination', 'VA.gov API', mockApi => {
       await saveForm(url, unauthenticatedApplicationData);
     });
   });
-
-  // I JUST FOUND OUT THAT THE UPDATE WILL ALSO BE A POST REQUEST. WAITING FOR MORE DETAILS.
-  describe('PATCH /registration', () => {
-    it('authenticated success case: submit valid updated registration will return a 204 No Content HTTP response', async () => {
-      const url = `${environment.API_URL}/covid_vaccine/v0/registration`; // might need to be updated
-
-      const interaction = {
-        state: 'authenticated user updates registration data',
-        uponReceiving: 'a PATCH request',
-        withRequest: {
-          method: 'PATCH',
-          path: '/covid_vaccine/v0/registration', // might need to be updated
-          headers: {
-            'X-Key-Inflection': 'camel',
-            'Content-Type': 'application/json',
-          },
-          body: authenticatedApplicationData, // might need to be updated
-        },
-        willRespondWith: {
-          status: 204, // might need to be updated
-          headers: {
-            'Content-Type': term({
-              matcher: '^application/json',
-              generate: 'application/json',
-            }),
-          },
-          body: {
-            data: like({}), // needs to be updated
-          },
-        },
-      };
-
-      await mockApi().addInteraction(interaction);
-
-      await saveForm(url, authenticatedApplicationData);
-    });
-  });
 });
