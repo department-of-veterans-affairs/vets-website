d3e86109e8d71d2fe56b9a794169a5cd3092f750
Robin Garrison <robin.garrison@adhocteam.us>
2020-12-15 14:10:03 -0600

    Update no-issue dropdown content

--- src/applications/disability-benefits/996/content/contestedIssues.jsx
+++ src/applications/disability-benefits/996/content/contestedIssues.jsx
@@ -8,7 +8,6 @@ import Telephone, {
 
 import {
   BOARD_APPEALS_URL,
-  CLAIM_STATUS_TOOL_URL,
   COVID_FAQ_URL,
   DECISION_REVIEWS_URL,
   NULL_CONDITION_STRING,
@@ -78,82 +77,57 @@ export const disabilityOption = ({ attributes }) => {
   );
 };
 
-const leadInContent = (
-  <>
-    <p className="vads-u-margin-top--2p5">
-      Please refer to your decision notice for further information on decision
-      review options and HLR eligibility. For information on how COVID-19
-      affects claims and appeals, please visit our{' '}
-      <a href={COVID_FAQ_URL}>Coronavirus FAQ page</a>.
-    </p>
-    <h2 className="vads-u-font-size--h4">
-      Where can I find more information on review options?
-    </h2>
-    <p>
-      Information regarding review options can be found at our{' '}
-      <a href={DECISION_REVIEWS_URL}>decision review information page</a>, or
-      you can call us at <Telephone contact={CONTACTS.VA_BENEFITS} />.
-      Alternatively, you can work with an accredited representative to get help
-      filing a claim for disability compensation.
-    </p>
-  </>
-);
-
-// Wrapped in a <div> on purpose
 const disabilitiesList = (
   <div>
     <ul>
       <li>
-        VA decided that issue more than one year ago (an HLR can only be
-        requested within one year of the last decision). The one year time
-        period from the date on your decision letter has expired for
-        Higher-Level Review. <strong>Note:</strong> If you weren’t able to
-        request a timely review due to the COVID-19 pandemic, we may grant you
-        an extension of the deadline. To request an extension, download and fill
-        out VA Form 20-0996 PDF (1.5MB), and attach a note that you’re
-        requesting a filing extension due to COVID-19.
+        We made the decision over a year ago. You have 1 year from the date on
+        your decision letter to request a Higher-Level Review.{' '}
+        <strong>Note:</strong> If you aren’t able to request a timely review due
+        to COVID-19, we may grant you a deadline extension. To request an
+        extension, fill out and submit VA Form 20-0996, with a note attached
+        that you’re requesting an extension due to COVID-19.
       </li>
       <li>
-        Your issue isn’t related to monthly compensation, pension or survivor
-        benefits. For all other benefits, like health care, insurance or
-        education, you’ll need to download and fill out VA Form 20-0996 PDF
-        (1.5MB) and submit it by mail or in person.
+        Your issue is for a benefit type other than compensation or pension. To
+        request a Higher-Level Review for another benefit type, you’ll need to
+        fill out VA Form 20-0996 and submit it by mail or in person.
       </li>
       <li>
-        The VA has not yet reached a decision on this claim. Please refer to the{' '}
-        <a href={CLAIM_STATUS_TOOL_URL}>Claims Status Tool</a> for more
-        information.
+        The issue or decision isn’t our system yet. You’ll need to fill VA Form
+        20-0996 and submit it by mail or in person.
       </li>
       <li>
-        The issue or decision isn’t in our system yet. You’ll need to download
-        and fill out VA Form 20-0996 PDF (1.5MB) and submit it by mail.
-      </li>
-      <li>
-        You’re applying for a benefit that another surviving dependent of the
-        Veteran is also applying for. And by law, only 1 of you can receive that
-        benefit. You’ll need to{' '}
+        You and another surviving dependent of the Veteran are applying for the
+        same benefit. And by law, only 1 of you can receive that benefit. You’ll
+        need to{' '}
         <a href={BOARD_APPEALS_URL}>appeal to the Board of Veterans’ Appeals</a>
         .
       </li>
       <li>
-        You’re requesting a review of a Board of Veterans’ Appeals decision or
-        an existing decision already under Higher-Level Review. You’ll need to
-        refer to the Board’s decision notice for your review options.
+        You’re requesting a review of a Board of Veterans’ Appeals decision.
+        Refer to your decision notice for your options.
       </li>
       <li>
-        You’re requesting a review of a Higher-Level Review decision. You can
-        either submit a Supplemental Claim or appeal to the Board of Veterans’
-        Appeals.
-      </li>
-      <li>VA has not previously made a decision on that issue.</li>
-      <li>
-        The issue is ineligible under a Higher-Level Review (it cannot be
-        requested on contested claims, such as disagreements with attorney
-        fees).
+        You’re requesting a review of a Higher-Level Review decision. You’ll
+        need to either file a Supplemental Claim or appeal to the Board of
+        Veterans’ Appeals.
       </li>
     </ul>
-    <p className="vads-u-padding-bottom--2p5">
+    <p>
       <DownloadLink content={'Download VA Form 20-0996'} />
+    </p>
+    <p className="vads-u-margin-top--2p5">
+      To learn more about how COVID-19 affect claims or appeals, please visit
+      our <a href={COVID_FAQ_URL}>Coronavirus FAQ page</a>.
+    </p>
+    <p className="vads-u-padding-bottom--2p5">
+      To learn more about decision review options, please visit our{' '}
+      <a href={DECISION_REVIEWS_URL}>decision reviews and appeals</a>{' '}
+      information page. You can call us at{' '}
+      <Telephone contact={CONTACTS.VA_BENEFITS} /> or work with an accredited
+      representative to{' '}
+      <a href="/disability/get-help-filing-claim/">get help with your claim</a>.
     </p>
   </div>
 );
@@ -162,7 +136,6 @@ export const disabilitiesExplanationAlert = (
   <>
     <p className="vads-u-margin-top--2p5" />
     <AdditionalInfo triggerText={'Why isn’t my issue eligible?'}>
-      {leadInContent}
       <p>Your issue may not be eligible if:</p>
       {disabilitiesList}
     </AdditionalInfo>
@@ -171,7 +144,6 @@ export const disabilitiesExplanationAlert = (
 
 export const disabilitiesExplanation = (
   <AdditionalInfo triggerText={'Don’t see the issue you’re looking for?'}>
-    {leadInContent}
     <p>There are many reasons a decision might not appear in the list above.</p>
     {disabilitiesList}
   </AdditionalInfo>
