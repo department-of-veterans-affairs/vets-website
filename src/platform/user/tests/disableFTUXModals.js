620c6d26026182ae81609183059155701871d4b2
Sandra Hallie <sandra.adamshallie@gmail.com>
2020-12-16 14:50:41 -0700

    Update

--- src/platform/user/tests/disableFTUXModals.js
+++ src/platform/user/tests/disableFTUXModals.js
@@ -1,4 +1,6 @@
 // make sure no first-time UX modals are in the way
-export function disableFTUXModals() {
+const disableFTUXModals = () => {
   window.localStorage.setItem('DISMISSED_ANNOUNCEMENTS', '*');
-}
+};
+
+export default disableFTUXModals;
