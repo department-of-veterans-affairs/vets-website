bb4811197180f07e96d6241628adee145d8deb73
Robin Garrison <robin.garrison@adhocteam.us>
2020-12-16 15:53:19 -0600

    Add a11y ref to form start button

--- src/applications/disability-benefits/996/wizard/pages/legacyNo.jsx
+++ src/applications/disability-benefits/996/wizard/pages/legacyNo.jsx
@@ -16,39 +16,42 @@ const LegacyNo = ({ setWizardStatus }) => {
   });
   return (
     <div className="vads-u-background-color--gray-lightest vads-u-padding--2 vads-u-margin-top--2">
-      You can request a Higher-Level Review online using{' '}
-      <strong>VA Form 20-0996</strong>.
-      <p>
-        <button
-          onClick={() => {
-            recordEvent({
-              event: 'howToWizard-hidden',
-              'reason-for-hidden-wizard': 'wizard completed, starting form',
-            });
-            recordEvent({
-              event: 'cta-button-click',
-              'button-type': 'primary',
-              'button-click-label': 'Request a Higher-Level Review online',
-            });
-            setWizardStatus(WIZARD_STATUS_COMPLETE);
-          }}
-          className="usa-button-primary va-button-primary"
-        >
-          Request a Higher-Level Review online
-        </button>
+      <p className="vads-u-margin-top--0">
+        You can request a Higher-Level Review online using{' '}
+        <strong>VA Form 20-0996</strong>.
       </p>
-      <a
-        href={HLR_INFO_URL}
+      <button
         onClick={() => {
           recordEvent({
-            event: 'howToWizard-alert-link-click',
-            'howToWizard-alert-link-click-label':
-              'Learn about other ways you can request a Higher-Level Review',
+            event: 'howToWizard-hidden',
+            'reason-for-hidden-wizard': 'wizard completed, starting form',
           });
+          recordEvent({
+            event: 'cta-button-click',
+            'button-type': 'primary',
+            'button-click-label': 'Request a Higher-Level Review online',
+          });
+          setWizardStatus(WIZARD_STATUS_COMPLETE);
         }}
+        className="usa-button-primary va-button-primary"
+        aria-describedby="other_ways_to_request_hlr"
       >
-        Learn about other ways you can request a Higher-Level Review
-      </a>
+        Request a Higher-Level Review online
+      </button>
+      <p id="other_ways_to_request_hlr" className="vads-u-margin-bottom--0">
+        <a
+          href={HLR_INFO_URL}
+          onClick={() => {
+            recordEvent({
+              event: 'howToWizard-alert-link-click',
+              'howToWizard-alert-link-click-label':
+                'Learn about other ways you can request a Higher-Level Review',
+            });
+          }}
+        >
+          Learn about other ways you can request a Higher-Level Review
+        </a>
+      </p>
     </div>
   );
 };
