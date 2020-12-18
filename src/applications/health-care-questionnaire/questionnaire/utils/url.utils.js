b92eb3da61a87b46541bdb2fb020d736ecfe45da
Mark Dewey <mdewey@governmentcio.com>
2020-12-18 15:41:48 -0500

    added tests for urls utils

--- src/applications/health-care-questionnaire/questionnaire/utils/url.utils.js
+++ src/applications/health-care-questionnaire/questionnaire/utils/url.utils.js
@@ -1,11 +1,15 @@
 import URLSearchParams from 'url-search-params';
 
 const getAppointmentIdFromUrl = (window, key = 'id') => {
+  if (!window) return null;
+  if (!window.location) return null;
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(key);
 };
 
 const addAppointmentIdToFormId = (appointmentId, formId) => {
+  if (!formId) return null;
+  if (!appointmentId) return formId;
   return formId.includes(appointmentId) ? formId : `${formId}-${appointmentId}`;
 };
 
