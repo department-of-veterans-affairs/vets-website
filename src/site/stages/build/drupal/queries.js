3106eaf785dd266e90e1291a732cfbd00a935ed8
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2020-12-21 14:24:12 -0500

    Refactored GraphQL query requires to support webpack build.

--- src/site/stages/build/drupal/queries.js
+++ src/site/stages/build/drupal/queries.js
@@ -1,15 +1,15 @@
 const queries = {
-  GET_ALL_PAGES: './graphql/GetAllPages.graphql',
-  GET_LATEST_PAGE_BY_ID: './graphql/GetLatestPageById.graphql',
+  GET_ALL_PAGES: 'GetAllPages.graphql',
+  GET_LATEST_PAGE_BY_ID: 'GetLatestPageById.graphql',
 };
 
 function getQuery(query, { useTomeSync } = {}) {
   if (query === queries.GET_ALL_PAGES) {
     // eslint-disable-next-line import/no-dynamic-require
-    return require(query)({ useTomeSync });
+    return require(`./graphql/${query}`)({ useTomeSync });
   }
   // eslint-disable-next-line import/no-dynamic-require
-  return require(query);
+  return require(`./graphql/${query}`);
 }
 
 module.exports = {
