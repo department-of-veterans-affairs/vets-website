7ec7e8580703ce6a8309daddae363dd9dfe74fb2
Jesse Cohn <jesse.cohn@teamadhoc.us>
2020-12-28 13:34:09 -0800

    added events for when someone is included in flip

--- src/applications/static-pages/vre-chapter36/Chapter36CTA.js
+++ src/applications/static-pages/vre-chapter36/Chapter36CTA.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
 import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
-
+import recordEvent from 'platform/monitoring/record-event';
 import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
 
 import EbenefitsLink from 'platform/site-wide/ebenefits/containers/EbenefitsLink';
@@ -31,6 +31,10 @@ const Chapter36CTA = props => {
   if (props.includedInFlipper === undefined) {
     content = <LoadingIndicator message="Loading..." />;
   } else if (props.includedInFlipper === false) {
+    recordEvent({
+      event: 'phased-roll-out-disabled',
+      'product-description': 'Chapter 36',
+    });
     content = (
       <>
         {header}
@@ -89,6 +93,10 @@ const Chapter36CTA = props => {
       </>
     );
   } else {
+    recordEvent({
+      event: 'phased-roll-out-enabled',
+      'product-description': 'Chapter 36',
+    });
     content = (
       <>
         {!isEducationAndCareerPage ? <h2>How do I apply?</h2> : null}
