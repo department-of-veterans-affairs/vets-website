9aebbbfc7e46119bd3b0d1c701ce6be8f6a93140
Craig Wheeler <craig@craigwheeler.net>
2020-12-28 17:34:59 -0500

    17583: installment contracts and other debts (#15477)

--- src/applications/financial-status-report/pages/householdIncome/socialSecurity.js
+++ src/applications/financial-status-report/pages/householdIncome/socialSecurity.js
@@ -1,4 +1,5 @@
 import currencyUI from 'platform/forms-system/src/js/definitions/currency';
+import _ from 'lodash/fp';
 
 export const uiSchema = {
   'ui:title': 'Your other income',
@@ -12,8 +13,15 @@ export const uiSchema = {
       'ui:options': {
         expandUnder: 'hasSocialSecurityPayments',
       },
-      socialSecurityAmount: currencyUI(
-        'How much do you receive for Social Security each month?',
+      socialSecurityAmount: _.merge(
+        currencyUI('How much do you receive for Social Security each month?'),
+        {
+          'ui:options': {
+            widgetClassNames: 'input-size-3',
+          },
+          'ui:required': formData =>
+            formData.additionalIncome.hasSocialSecurityPayments,
+        },
       ),
     },
   },
