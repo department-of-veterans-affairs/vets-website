39e9a270116755972e6bdde2de4a035cb78b2716
Mark Dewey <mdewey@governmentcio.com>
2020-12-30 14:52:57 -0500

    added reading 3 outta 4 answers

--- src/applications/health-care-questionnaire/questionnaire/config/submitForm.js
+++ src/applications/health-care-questionnaire/questionnaire/config/submitForm.js
@@ -13,9 +13,12 @@ const submit = (form, formConfig) => {
   );
 };
 
+const createAnAnswer = valueString => ({ valueString });
+
 const transformForSubmit = (_formConfig, form) => {
   // console.log({ formConfig, form });
   const { questionnaireId, appointmentId } = form.data['hidden:fields'];
+  const { reasonForVisitDescription, lifeEvents, questions } = form.data;
   return {
     appointmentId,
     questionnaireId,
@@ -23,40 +26,28 @@ const transformForSubmit = (_formConfig, form) => {
       {
         linkId: '01',
         text: 'What is the reason for this appointment?',
-        answer: [
-          {
-            valueString: 'Eye is hurting',
-          },
-        ],
+        answer: [createAnAnswer('Some reason to visit')],
       },
       {
         linkId: '02',
         text:
           "Are there any additional details you'd like to share with your provider about this appointment?",
-        answer: [
-          {
-            valueString: 'cant look into the sun',
-          },
-        ],
+        answer: [createAnAnswer(reasonForVisitDescription)],
       },
       {
         linkId: '03',
         text:
           'Are there any life events that are positively or negatively affecting your health (e.g. marriage, divorce, new job, retirement, parenthood, or finances)?',
-        answer: [
-          {
-            valueString: 'no',
-          },
-        ],
+        answer: [createAnAnswer(lifeEvents)],
       },
       {
         linkId: '04',
         text:
           'Do you have other questions you want to ask your provider? Please enter them below with your most important question listed first.',
         answer: [
-          {
-            valueString: 'Is this covered by my VA Benfits?',
-          },
+          ...questions
+            .filter(answer => answer.additionalQuestions)
+            .map(answer => createAnAnswer(answer.additionalQuestions)),
         ],
       },
     ],
