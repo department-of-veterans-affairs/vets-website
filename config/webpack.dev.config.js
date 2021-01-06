c797786cd55d2017e673e0d3a75c3cd9370b8a43
Tim Wright <timwright12@gmail.com>
2021-01-06 11:41:52 -0500

    testing process for staging and dev

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -13,8 +13,7 @@ function generateWebpackDevConfig(buildOptions) {
 
   // This buildType likely always be 'localhost', but adding in to match patterns elsewhere and just incase we ever need it
   const publicAssetPath =
-    buildOptions.buildtype === 'vagovstaging' ||
-    buildOptions.buildtype === 'vagovdev' // just for staging and dev  so we can test it
+    buildOptions.buildtype === 'vagovstaging' // just for staging and dev  so we can test it
       ? `${BUCKETS[buildOptions.buildtype]}/generated/`
       : '/generated/';
 
