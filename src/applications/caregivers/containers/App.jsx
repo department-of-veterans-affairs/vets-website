746d99aea2b5ec64930401538f23cdb4793045ad
LinnJS <justinslinn727@gmail.com>
2021-01-06 10:59:37 -0500

    Remove unused vars

--- src/applications/caregivers/containers/App.jsx
+++ src/applications/caregivers/containers/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import MetaTags from 'react-meta-tags';
 
 import LoadingIndicator from '@department-of-veterans-affairs/formation-react/LoadingIndicator';
 
