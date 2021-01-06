dbae028f414140e66cd6f13338442f0ab2afd081
Robin Garrison <robin.garrison@adhocteam.us>
2021-01-06 09:23:30 -0600

    Update unit tests

--- src/applications/disability-benefits/996/tests/components/sameOffice.unit.spec.jsx
+++ src/applications/disability-benefits/996/tests/components/sameOffice.unit.spec.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { expect } from 'chai';
+import sinon from 'sinon';
 import ReactTestUtils from 'react-dom/test-utils';
 
 import {
   DefinitionTester,
+  submitForm,
   getFormDOM,
 } from 'platform/testing/unit/schemaform-utils';
 
-import { $ } from '../../helpers';
+import { $$ } from '../../helpers';
 
 import formConfig from '../../config/form.js';
 import initialData from '../schema/initialData.js';
@@ -15,7 +17,7 @@ import initialData from '../schema/initialData.js';
 describe('Higher-Level Review 0996 choose same office', () => {
   const { schema, uiSchema } = formConfig.chapters.sameOffice.pages.sameOffice;
   // Office for review section
-  it('should render the same office checkbox', () => {
+  it('should render the same office yes/no radio', () => {
     const form = ReactTestUtils.renderIntoDocument(
       <DefinitionTester
         definitions={formConfig.defaultDefinitions}
@@ -25,21 +27,26 @@ describe('Higher-Level Review 0996 choose same office', () => {
       />,
     );
     const formDOM = getFormDOM(form);
-    expect($('#root_sameOffice', formDOM)).to.not.be.false;
+    const radios = $$('[name="root_sameOffice"]', formDOM);
+    expect(radios).to.have.lengthOf(2);
+    // "No" should be selected by default
+    expect(radios[1].checked).to.be.true;
   });
 
-  // "No" should be selected by default
-  it('should show info alert when checked', () => {
+  it('should allow submit', () => {
+    const onSubmit = sinon.spy();
     const form = ReactTestUtils.renderIntoDocument(
       <DefinitionTester
         definitions={formConfig.defaultDefinitions}
+        onSubmit={onSubmit}
         schema={schema}
         data={initialData}
         uiSchema={uiSchema}
       />,
     );
     const formDOM = getFormDOM(form);
-    formDOM.setCheckbox('#root_sameOffice', true);
-    expect($('#root_sameOfficeAlert__title', formDOM)).to.not.be.false;
+    formDOM.setYesNo('[name="root_sameOffice"]', true);
+    submitForm(form);
+    expect(onSubmit.called).to.be.true;
   });
 });
