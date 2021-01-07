61d4de88390ba1457d05e8005b5ad7ef6b57648a
Tim Wright <timwright12@gmail.com>
2021-01-07 16:56:39 -0500

    trying to trigger a manual review

--- src/applications/burials/components/ErrorText.jsx
+++ src/applications/burials/components/ErrorText.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CallVBACenter from 'platform/static-data/CallVBACenter';
-
+/* eslint-disable no-alert, no-console */
 export default function ErrorText() {
   return (
     <p>
