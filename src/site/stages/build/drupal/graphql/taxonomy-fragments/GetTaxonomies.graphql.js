fcb77554a77844a52cba2a48083b005561fe0683
Nick Sullivan <nick.sullivan@adhocteam.us>
2021-01-08 17:26:22 -0500

    use fragments in query

--- src/site/stages/build/drupal/graphql/taxonomy-fragments/GetTaxonomies.graphql.js
+++ src/site/stages/build/drupal/graphql/taxonomy-fragments/GetTaxonomies.graphql.js
@@ -2,20 +2,9 @@ module.exports = `
   allTaxonomies: taxonomyTermQuery(limit: 1000) {
     entities {
       entityBundle
-      ... on TaxonomyTermAudienceNonBeneficiaries {
-        fieldAudienceRsHomepage
-        name
-        path {
-          alias
-        }
-      }
-      ... on TaxonomyTermAudienceBeneficiaries {
-        fieldAudienceRsHomepage
-        name
-        path {
-          alias
-        }
-      }
+      ... taxonomyTermAudienceBeneficiaries
+      ... taxonomyTermAudienceNonBeneficiaries
+      ... taxonomyTermLcCategories
     }
   }
 `;
