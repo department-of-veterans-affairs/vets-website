c000b60b508ed89b7331c3f6fe95af7aa40e50f5
Craig Wheeler <craig@craigwheeler.net>
2021-01-12 15:13:43 -0500

    update alerts

--- src/applications/debt-letters/components/DebtLettersDownload.jsx
+++ src/applications/debt-letters/components/DebtLettersDownload.jsx
@@ -5,9 +5,12 @@ import { connect } from 'react-redux';
 import Breadcrumbs from '@department-of-veterans-affairs/formation-react/Breadcrumbs';
 import { DebtLettersTable } from './DebtLettersTable';
 import { MobileTableView } from './MobileTableView';
-import Telephone from '@department-of-veterans-affairs/formation-react/Telephone';
 import scrollToTop from 'platform/utilities/ui/scrollToTop';
 import { setPageFocus } from '../utils/page';
+import Telephone, {
+  CONTACTS,
+  PATTERNS,
+} from '@department-of-veterans-affairs/formation-react/Telephone';
 
 const DebtLettersDownload = ({ debtLinks, isVBMSError }) => {
   useEffect(() => {
@@ -74,22 +77,32 @@ const DebtLettersDownload = ({ debtLinks, isVBMSError }) => {
           debtLinks.length < 1 && (
             <div className="vads-u-background-color--gray-lightest vads-u-padding--3 vads-u-margin-top--3">
               <h2 className="vads-u-font-family--serif vads-u-margin-top--0">
-                VA debt collection is on hold due to the coronavirus
+                We’re collecting again on VA debt
               </h2>
               <p className="vads-u-font-family--sans vads-u-margin-bottom--0">
-                We’ve taken action to stop collection on newly established
-                Veteran debt and make it easier for Veterans to request extended
-                repayment plans and address other financial needs during this
-                time.
+                On April 3, 2020, we paused collections on new VA debt. On{' '}
+                <strong>January 1, 2021</strong>, we started to send out debt
+                collection letters again. If we granted you an extension due to
+                COVID-19, we’ll start collection again on{' '}
+                <strong>February 1, 2021</strong>.
               </p>
               <p className="vads-u-font-family--sans vads-u-margin-bottom--0">
-                You won’t receive any debt collection letters in the mail until
-                after December 31, 2020. For the latest information about
-                managing VA debt, visit our{' '}
-                <a href="http://va.gov/coronavirus-veteran-frequently-asked-questions/">
-                  coronavirus FAQs
+                If you can’t make your payments, we can help. To avoid late
+                charges, interest, or other collection actions, make a payment
+                or request help now. Call us at{' '}
+                {<Telephone contact={CONTACTS.DMC || '800-827-0648'} />} (or{' '}
+                {
+                  <Telephone
+                    contact={CONTACTS.DMC_OVERSEAS || '1-612-713-6415'}
+                    pattern={PATTERNS.OUTSIDE_US}
+                  />
+                }{' '}
+                from overseas) We’re here Monday through Friday, 7:30 a.m. to
+                7:00 p.m. ET. Or send us a question through our{' '}
+                <a href="https://iris.custhelp.va.gov/app/ask">
+                  {' '}
+                  online question form (called IRIS)
                 </a>
-                {'.'}
               </p>
             </div>
           )}
