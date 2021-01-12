dff6eb13c7f696497b859868f73756dcef16900e
LinnJS <justinslinn727@gmail.com>
2021-01-12 15:16:02 -0500

    Add signaturebox content unit test

--- src/applications/caregivers/tests/components/PreSubmitInfo/index.spec.js
+++ src/applications/caregivers/tests/components/PreSubmitInfo/index.spec.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import {
+  veteranSignatureContent,
+  primaryCaregiverContent,
+  secondaryCaregiverContent,
+} from 'applications/caregivers/definitions/content.js';
 
 import SignatureBoxes from '../../../components/PreSubmitInfo/index';
 import maximalFormData from '../../e2e/fixtures/data/twoSecondaryCaregivers.json';
 
-describe('<SignatureBoxes>', () => {
-  it('renders correctly', () => {
-    const props = {
-      onSectionComplete: () => false,
-      formData: {
-        ...maximalFormData,
-      },
-      showError: false,
-    };
+describe('SignatureBox content', () => {
+  it('veteranSignatureContent has not changed', () => {
+    const mockVeteranSignatureContent = [
+      'I certify that I give consent to the individual(s) named in this application to perform personal care services for me upon being approved as Primary and/or Secondary Family Caregivers in the Program of Comprehensive Assistance for Family Caregivers.',
+    ];
 
-    const wrapper = shallow(<SignatureBoxes.CustomComponent {...props} />);
-    const text = wrapper.text();
+    expect(mockVeteranSignatureContent).to.deep.equal(veteranSignatureContent);
+  });
 
-    expect(text).to.include(
-      "Please review information entered into this application. The Veteran and each family caregiver applicant must sign the appropriate section.<Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) />Note: According to federal law, there are criminal penalties, including a fine and/or imprisonment for up to 5 years, for withholding information or providing incorrect information. (See 18 U.S.C. 1001)' to include 'Please review information entered into this application. The Veteran and each family caregiver applicant must sign the appropriate section.<Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) />Note: According to federal law, there are criminal penalties, including a fine and/or imprisonment for up to 5 years, for withholding information or providing incorrect information. (See 18 U.S.C. 1001)' to include 'Please review information entered into this application. The Veteran and each family caregiver applicant must sign the appropriate section.<Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) />Note: According to federal law, there are criminal penalties, including a fine and/or imprisonment for up to 5 years, for withholding information or providing incorrect information. (See 18 U.S.C. 1001)' to include 'Please review information entered into this application. The Veteran and each family caregiver applicant must sign the appropriate section.<Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) />Note: According to federal law, there are criminal penalties, including a fine and/or imprisonment for up to 5 years, for withholding information or providing incorrect information. (See 18 U.S.C. 1001)' to include 'Please review information entered into this application. The Veteran and each family caregiver applicant must sign the appropriate section.<Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) /><Connect(SignatureCheckbox) />Note: According to federal law, there are criminal penalties, including a fine and/or imprisonment for up to 5 years, for withholding information or providing incorrect information. (See 18 U.S.C. 1001)' to include 'This app can’t access any new information from your VA.gov profile",
+  it('veteranSignatureContent has not changed', () => {
+    const mockPrimaryCaregiverContent = [
+      'I certify that I am at least 18 years of age.',
+      "I certify that I am a family member of the Veteran named in this application or I reside with the Veteran, or will do so upon designation as the Veteran's Primary Family Caregiver.",
+      'I agree to perform personal care services as the Primary Family Caregiver for the Veteran named on this application.',
+      'I understand that the Veteran or Veteran’s surrogate may request my discharge from the Program of Comprehensive Assistance for Family Caregivers (PCAFC) at any time. I understand that my designation as a Primary Family Caregiver may be revoked or I may be discharged from the program by the Secretary of Veterans Affairs or his designee, as set forth in 38 CFR 71.45.',
+      'I understand that participation in PCAFC does not create an employment relationship between me and the Department of Veterans Affairs.',
+    ];
+
+    expect(mockPrimaryCaregiverContent).to.deep.equal(primaryCaregiverContent);
+  });
+
+  it('veteranSignatureContent has not changed', () => {
+    const mockSecondaryCaregiverContent = [
+      'I certify that I am at least 18 years of age.',
+      "I certify that I am a family member of the Veteran named in this application or I reside with the Veteran, or will do so upon designation as the Veteran's Secondary Family Caregiver.",
+      'I agree to perform personal care services as the Secondary Family Caregiver for the Veteran named on this application.',
+      'I understand that the Veteran or Veteran’s surrogate may request my discharge from the Program of Comprehensive Assistance for Family Caregivers (PCAFC) at any time. I understand that my designation as a Secondary Family Caregiver may be revoked or I may be discharged from the program by the Secretary of Veterans Affairs or his designee, as set forth in 38 CFR 71.45.',
+      'I understand that participation in PCAFC does not create an employment relationship between me and the Department of Veterans Affairs.',
+    ];
+
+    expect(mockSecondaryCaregiverContent).to.deep.equal(
+      secondaryCaregiverContent,
     );
-
-    wrapper.unmount();
   });
 });
