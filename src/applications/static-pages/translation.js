fd573a650fffedff27e78d2f2bced6b1a2ad16d1
Fareez Ahmed <fareezahmed@Fareez-GCIO.local>
2021-01-13 12:26:08 -0800

    enable link in spanish page

--- src/applications/static-pages/translation.js
+++ src/applications/static-pages/translation.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import i18Content from './i18Content.json';
 import recordEvent from 'platform/monitoring/record-event';
 
@@ -14,21 +15,28 @@ const configureTranslationLink = (e, currentLang, targetLang) => {
 };
 
 const displayTranslationLink = () => {
+  // console.log('DISPLAYING....');
   const i18LinkWrapper = document.getElementById('i18-link-wrapper');
   if (!i18LinkWrapper) return;
-  const isSpanish = window.location.href.includes('-esp');
+  const isSpanish =
+    window.location.href.includes('-esp') ||
+    // comment out this line once ready to merge
+    window.location.href.includes('nodeId=14580');
   const translatableLinks = [
     // remove the preview nodes from this array when ready to merge
     'nodeId=3014',
     'nodeId=6785',
     'coronavirus-veteran-frequently-asked-questions',
+    'nodeId=14580',
     // 'covid-19-vaccine',
     // 'coronavirus-research',
   ];
-
   const isTranslatable = translatableLinks.some(url =>
     window.location.href.includes(url),
   );
+
+  // console.log(isSpanish, isTranslatable, 'YO');
+
   if (!isTranslatable) {
     i18LinkWrapper.style.display = 'none';
   }
