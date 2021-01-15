ec664ce6cbe903f97e4f91ee668fe74daec2a92b
Jesse Cohn <jesse.cohn@teamadhoc.us>
2021-01-15 11:01:56 -0800

    updates to View Representative page

--- src/applications/personalization/view-representative/components/ViewRepresentativeLayout.jsx
+++ src/applications/personalization/view-representative/components/ViewRepresentativeLayout.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import SideBar from './SideBar';
 import ViewRepresentativeBody from './ViewRepresentativeBody';
 
 const ViewRepresentativeLayout = props => {
@@ -7,13 +6,10 @@ const ViewRepresentativeLayout = props => {
     <div className="vads-l-row">
       <main className="vads-l-col--12 medium-screen:vads-l-col--8">
         <header>
-          <h1>View your representative for VA claims</h1>
+          <h1>View your VA representative</h1>
           <ViewRepresentativeBody representative={props.representative} />
         </header>
       </main>
-      <section className="vads-l-col--12 medium-screen:vads-l-col--4">
-        <SideBar />
-      </section>
     </div>
   );
 };
