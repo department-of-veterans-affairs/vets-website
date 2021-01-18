5757e5fc0f1634b30a5d3824f0c88e437a7744a3
Tim Wright <timwright12@gmail.com>
2021-01-18 13:13:40 -0500

    revert to webpack build

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -247,8 +247,8 @@ def buildAll(String ref, dockerContainer, Boolean contentOnlyBuild) {
 
 def prearchive(dockerContainer, envName) {
   dockerContainer.inside(DOCKER_ARGS) {
-    if (envName == 'vagovdev') {
-      sh "cd /application && NODE_ENV=production yarn build --buildtype ${envName} --setPublicPath"
+    if (envName == 'vagovdev' || envName == 'vagovstaging') {
+      sh "cd /application && NODE_ENV=production yarn build:webpack --env.buildtype ${envName} --env.setPublicPath"
     }
     sh "cd /application && node --max-old-space-size=10240 script/prearchive.js --buildtype=${envName}"
   }
