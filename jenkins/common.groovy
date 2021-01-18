d8cd977ddc5936b972ca7212003c3025c7efb3c9
Tim Wright <timwright12@gmail.com>
2021-01-18 10:01:18 -0500

    updated build params

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -248,7 +248,7 @@ def buildAll(String ref, dockerContainer, Boolean contentOnlyBuild) {
 def prearchive(dockerContainer, envName) {
   dockerContainer.inside(DOCKER_ARGS) {
     if (envName == 'vagovdev') {
-      sh "cd /application && NODE_ENV=production yarn build --env.buildtype ${envName} --env.setPublicPath"
+      sh "cd /application && NODE_ENV=production yarn build --buildtype ${envName} --setPublicPath"
     }
     sh "cd /application && node --max-old-space-size=10240 script/prearchive.js --buildtype=${envName}"
   }
