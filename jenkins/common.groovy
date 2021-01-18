05db32fabc1c9d881c007b3359859d464873f941
Tim Wright <timwright12@gmail.com>
2021-01-18 08:52:10 -0500

    attempting full dev build

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -247,7 +247,9 @@ def buildAll(String ref, dockerContainer, Boolean contentOnlyBuild) {
 
 def prearchive(dockerContainer, envName) {
   dockerContainer.inside(DOCKER_ARGS) {
-    sh "cd /application && NODE_ENV=production yarn build:webpack --env.buildtype ${envName} --env.setPublicPath"
+    if (envName == 'vagovdev') {
+      sh "cd /application && NODE_ENV=production yarn build --env.buildtype ${envName} --env.setPublicPath"
+    }
     sh "cd /application && node --max-old-space-size=10240 script/prearchive.js --buildtype=${envName}"
   }
 }
