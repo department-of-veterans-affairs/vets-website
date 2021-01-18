9e1c2f32ebe6da3b4dc7e912ff96b5f854e0fbd6
jhonnyoddball <jhonny.gonzalez@oddball.io>
2021-01-18 14:01:08 -0500

    Testing --use-cms-export DO NOT MERGE

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -39,6 +39,6 @@ done
 # exit code.  In this case, if the build command fails, the tee
 # command won't trick Jenkins into thinking the step passed.
 set -o pipefail
-npm --no-color run build -- --buildtype="$envName" --asset-source="$assetSource" --drupal-address="$drupalAddress" "$pullDrupal" 2>&1 | tee "$buildLog"
+npm --no-color run build -- --buildtype="$envName" --asset-source="$assetSource" --drupal-address="$drupalAddress" "$pullDrupal" --use-cms-export 2>&1 | tee "$buildLog"
 
 exit $?
