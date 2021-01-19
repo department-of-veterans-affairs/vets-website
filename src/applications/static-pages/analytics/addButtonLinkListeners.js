1e4e53cc3f688a5581bf749e7b39ad979bcc06a6
Brian Martin <brian.martin@empiricalpath.com>
2021-01-18 20:28:49 -0500

    Expanded existing static-pages button tracking

--- src/applications/static-pages/analytics/addButtonLinkListeners.js
+++ src/applications/static-pages/analytics/addButtonLinkListeners.js
@@ -1,20 +1,37 @@
 import recordEvent from 'platform/monitoring/record-event';
 
+function getButtonType(classList) {
+  if (classList.contains('usa-button-primary')) {
+    return 'primary';
+  } else if (classList.contains('usa-button-secondary')) {
+    return 'secondary';
+  } else if (classList.contains('usa-button')) {
+    return 'default';
+  }
+
+  return 'unknown';
+}
+
 export default function addButtonLinkListeners() {
   const ignoreReactWidgets = ':not([data-template="paragraphs/react_widget"])';
-  const defaultButtons = 'a.usa-button';
-  const ignorePrimaryButtons = ':not(.usa-button-primary)';
+  const defaultButtons = '.usa-button';
+  const primaryButtons = '.usa-button-primary';
+  const secondaryButtons = '.usa-button-secondary';
   const buttonLinks = [
     ...document.querySelectorAll(
-      `${ignoreReactWidgets} ${defaultButtons}${ignorePrimaryButtons}`,
+      `${ignoreReactWidgets} ${defaultButtons}, ${ignoreReactWidgets} ${primaryButtons}, ${ignoreReactWidgets} ${secondaryButtons}`,
     ),
   ];
 
   buttonLinks.forEach(buttonLink => {
     buttonLink.addEventListener('click', event => {
+      const style = window.getComputedStyle(event.target);
+
       recordEvent({
-        event: 'cta-default-button-click',
-        buttonText: event.target.text,
+        event: 'cta-button-click',
+        'button-type': getButtonType(event.target.classList),
+        'button-click-label': event.target.text,
+        'button-background-color': style.getPropertyValue('background-color'),
       });
     });
   });
