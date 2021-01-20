c5c717a24e06e94c7b795a445362783d41746c62
Mark Dewey <mdewey@governmentcio.com>
2021-01-20 10:35:53 -0500

    updated tests

--- src/applications/health-care-questionnaire/list/tests/unit/questionnaire-item/questionnaire.item.unit.spec.jsx
+++ src/applications/health-care-questionnaire/list/tests/unit/questionnaire-item/questionnaire.item.unit.spec.jsx
@@ -9,10 +9,45 @@ describe('health care questionnaire list - display a questionnaire item', () => 
     const name = 'Magic Kingdom';
     const data = {
       appointment: {
-        facilityName: name,
-        clinic: {},
-        appointmentTime: '2021-02-23T15:00:00Z',
+        id: '195bc02c0518870fc6b1e302cfc326b59',
+        type: 'va_appointments',
+        attributes: {
+          startDate: '2020-08-26T15:00:00Z',
+          sta6aid: '983',
+          clinicId: '848',
+          clinicFriendlyName: 'CHY PC VAR2',
+          facilityId: '983',
+          communityCare: false,
+          patientIcn: '1013124304V115761',
+          vdsAppointments: [
+            {
+              bookingNotes:
+                'Follow-up/Routine: testing reason for visit field availability',
+              appointmentLength: '20',
+              id: '848;20200826.090000',
+              appointmentTime: '2021-02-23T15:00:00Z',
+              clinic: {
+                name: 'CHY PC VAR2',
+                askForCheckIn: false,
+                facilityCode: '983',
+                facility: {
+                  displayName: name,
+                },
+                stopCode: '502',
+              },
+              type: 'REGULAR',
+              currentStatus: 'FUTURE',
+            },
+          ],
+          vvsAppointments: [],
+        },
       },
+      questionnaire: [
+        {
+          id: 'questionnnaire-ABC-123',
+          questionnaireResponse: {},
+        },
+      ],
     };
     const component = mount(<QuestionnaireItem data={data} />);
     expect(component.find('[data-testid="facility-name"]').text()).to.equal(
@@ -30,10 +65,45 @@ describe('health care questionnaire list - display a questionnaire item', () => 
     const name = 'Magic Kingdom';
     const data = {
       appointment: {
-        facilityName: name,
-        clinic: {},
-        appointmentTime: '2021-02-23T15:00:00Z',
+        id: '195bc02c0518870fc6b1e302cfc326b59',
+        type: 'va_appointments',
+        attributes: {
+          startDate: '2020-08-26T15:00:00Z',
+          sta6aid: '983',
+          clinicId: '848',
+          clinicFriendlyName: 'CHY PC VAR2',
+          facilityId: '983',
+          communityCare: false,
+          patientIcn: '1013124304V115761',
+          vdsAppointments: [
+            {
+              bookingNotes:
+                'Follow-up/Routine: testing reason for visit field availability',
+              appointmentLength: '20',
+              id: '848;20200826.090000',
+              appointmentTime: '2021-02-23T15:00:00Z',
+              clinic: {
+                name: 'CHY PC VAR2',
+                askForCheckIn: false,
+                facilityCode: '983',
+                facility: {
+                  displayName: name,
+                },
+                stopCode: '502',
+              },
+              type: 'REGULAR',
+              currentStatus: 'FUTURE',
+            },
+          ],
+          vvsAppointments: [],
+        },
       },
+      questionnaire: [
+        {
+          id: 'questionnnaire-ABC-123',
+          questionnaireResponse: {},
+        },
+      ],
     };
 
     const DueDate = () => <p data-testid="due-date">some data</p>;
@@ -48,10 +118,45 @@ describe('health care questionnaire list - display a questionnaire item', () => 
     const name = 'Magic Kingdom';
     const data = {
       appointment: {
-        facilityName: name,
-        clinic: {},
-        appointmentTime: '2021-02-23T15:00:00Z',
+        id: '195bc02c0518870fc6b1e302cfc326b59',
+        type: 'va_appointments',
+        attributes: {
+          startDate: '2020-08-26T15:00:00Z',
+          sta6aid: '983',
+          clinicId: '848',
+          clinicFriendlyName: 'CHY PC VAR2',
+          facilityId: '983',
+          communityCare: false,
+          patientIcn: '1013124304V115761',
+          vdsAppointments: [
+            {
+              bookingNotes:
+                'Follow-up/Routine: testing reason for visit field availability',
+              appointmentLength: '20',
+              id: '848;20200826.090000',
+              appointmentTime: '2021-02-23T15:00:00Z',
+              clinic: {
+                name: 'CHY PC VAR2',
+                askForCheckIn: false,
+                facilityCode: '983',
+                facility: {
+                  displayName: name,
+                },
+                stopCode: '502',
+              },
+              type: 'REGULAR',
+              currentStatus: 'FUTURE',
+            },
+          ],
+          vvsAppointments: [],
+        },
       },
+      questionnaire: [
+        {
+          id: 'questionnnaire-ABC-123',
+          questionnaireResponse: {},
+        },
+      ],
     };
 
     const Actions = () => <p data-testid="Actions">some data</p>;
@@ -64,20 +169,48 @@ describe('health care questionnaire list - display a questionnaire item', () => 
     component.unmount();
   });
   it('Appointment Type is shown based on clinic', () => {
+    const name = 'Magic Kingdom';
     const data = {
       appointment: {
+        id: '195bc02c0518870fc6b1e302cfc326b59',
+        type: 'va_appointments',
         attributes: {
+          startDate: '2020-08-26T15:00:00Z',
+          sta6aid: '983',
+          clinicId: '848',
+          clinicFriendlyName: 'CHY PC VAR2',
+          facilityId: '983',
+          communityCare: false,
+          patientIcn: '1013124304V115761',
           vdsAppointments: [
             {
+              bookingNotes:
+                'Follow-up/Routine: testing reason for visit field availability',
+              appointmentLength: '20',
+              id: '848;20200826.090000',
+              appointmentTime: '2021-02-23T15:00:00Z',
               clinic: {
+                name: 'CHY PC VAR2',
+                askForCheckIn: false,
+                facilityCode: '983',
+                facility: {
+                  displayName: name,
+                },
                 stopCode: '502',
               },
+              type: 'REGULAR',
+              currentStatus: 'FUTURE',
             },
           ],
           vvsAppointments: [],
         },
       },
-      questionnaire: [],
+      questionnaire: [
+        {
+          id: 'questionnnaire-ABC-123',
+          questionnaireResponse: {},
+        },
+      ],
     };
 
     const Actions = () => <p data-testid="Actions">some data</p>;
