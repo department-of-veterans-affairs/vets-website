718a04a50e3f4f3f90d0f0b3e2441cdf5f891c9e
Jesse Cohn <jesse.cohn@teamadhoc.us>
2021-01-20 11:53:33 -0800

    Added dummy data for unit tests

---
+++ src/applications/personalization/view-representative/tests/helpers.js
@@ -0,0 +1,11 @@
+export const representative = {
+  relationshipType: '',
+  dateRequestAccepted: '2014-17-28',
+  status: 'submitted',
+  representative: {
+    poaCode: 'A01',
+    poaFirstName: 'John',
+    paoLastName: 'Doe',
+    participantID: '987654',
+  },
+};
