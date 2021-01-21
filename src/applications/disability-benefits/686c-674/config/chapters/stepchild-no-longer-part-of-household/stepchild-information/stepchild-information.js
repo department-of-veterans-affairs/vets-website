799ca6f38ba9e1e43f1682fe7f3b5cf4442980be
Jesse Cohn <jesse.cohn@teamadhoc.us>
2021-01-21 13:19:27 -0800

    updated based on PR feedback

--- src/applications/disability-benefits/686c-674/config/chapters/stepchild-no-longer-part-of-household/stepchild-information/stepchild-information.js
+++ src/applications/disability-benefits/686c-674/config/chapters/stepchild-no-longer-part-of-household/stepchild-information/stepchild-information.js
@@ -47,7 +47,7 @@ export const uiSchema = {
       whoDoesTheStepchildLiveWith: {
         'ui:title': 'Who does this stepchild live with?',
         first: {
-          'ui:title': 'First Name of parent or guardian',
+          'ui:title': 'First name of parent or guardian',
           'ui:required': formData =>
             isChapterFieldRequired(
               formData,
@@ -55,10 +55,10 @@ export const uiSchema = {
             ),
         },
         middle: {
-          'ui:title': 'Middle Name of parent or guardian',
+          'ui:title': 'Middle name of parent or guardian',
         },
         last: {
-          'ui:title': 'Last Name of parent or guardian',
+          'ui:title': 'Last name of parent or guardian',
           'ui:required': formData =>
             isChapterFieldRequired(
               formData,
