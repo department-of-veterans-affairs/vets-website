bc589cec26d9e13b4b6c1bdb312fb2f14041172f
Jennie McGibney <jennie.mcgibney@va.gov>
2021-01-25 13:16:52 -0500

    remove flat facility and facility sacramento flag

--- src/applications/vaos/redux/selectors.js
+++ src/applications/vaos/redux/selectors.js
@@ -36,17 +36,12 @@ export const selectFeatureExpressCare = state =>
 export const selectFeatureExpressCareNewRequest = state =>
   toggleValues(state).vaOnlineSchedulingExpressCareNew;
 export const selectFeatureToggleLoading = state => toggleValues(state).loading;
-const selectFeatureFlatFacilityPage = state =>
-  toggleValues(state).vaOnlineSchedulingFlatFacilityPage;
-const selectFeatureFlatFacilityPageSacramento = state =>
-  toggleValues(state).vaOnlineSchedulingFlatFacilityPageSacramento;
+// const selectFeatureFlatFacilityPage = true;
+// const selectFeatureFlatFacilityPageSacramento = true;
 export const selectFeatureProjectCheetah = state =>
   toggleValues(state).vaOnlineSchedulingCheetah;
-export const selectUseFlatFacilityPage = state =>
-  selectFeatureFlatFacilityPage(state) &&
-  !selectIsCernerPatient(state) &&
-  (!selectIsRegisteredToSacramentoVA(state) ||
-    selectFeatureFlatFacilityPageSacramento(state));
+// Use flat facility page for non-Cerner patients
+export const selectUseFlatFacilityPage = state => !selectIsCernerPatient(state);
 
 const selectFeatureProviderSelection = state =>
   toggleValues(state).vaOnlineSchedulingProviderSelection;
