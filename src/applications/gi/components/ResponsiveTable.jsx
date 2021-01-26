1d954aa0c4a2621094b9933444dd444c7e8cef85
ksantiagomorgan6 <kenneth.santiago@morgan6.com>
2021-01-26 10:55:45 -0500

    removed length check

--- src/applications/gi/components/ResponsiveTable.jsx
+++ src/applications/gi/components/ResponsiveTable.jsx
@@ -26,7 +26,7 @@ class ResponsiveTable extends React.Component {
     let mobileHeaderValue = column;
 
     const stringCellData = value =>
-      value.length < 20 || screen.width > 320 ? (
+      screen.width > 320 ? (
         <span className={'vads-u-margin-0'}>{value}</span>
       ) : (
         <span className={'vads-u-margin-0'}>
