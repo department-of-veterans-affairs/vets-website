11498ee299e5d7bb1190e48b99bb5c59ca829f4e
LinnJS <justinslinn727@gmail.com>
2021-01-26 10:57:30 -0500

    Remove content UTs

--- src/applications/caregivers/tests/helpers.unit.spec.js
+++ src/applications/caregivers/tests/helpers.unit.spec.js
@@ -6,7 +6,6 @@ import formConfig from 'applications/caregivers/config/form';
 import requiredOnly from './e2e/fixtures/data/requiredOnly.json';
 import secondaryTwoOnly from './e2e/fixtures/data/secondaryOneOnly.json';
 import oneSecondaryCaregivers from './e2e/fixtures/data/oneSecondaryCaregivers.json';
-import twoSecondaryCaregivers from './e2e/fixtures/data/twoSecondaryCaregivers.json';
 
 describe('Caregivers helpers', () => {
   it('should transform required parties correctly (minimal with primary)', () => {
@@ -126,59 +125,5 @@ describe('Caregivers helpers', () => {
       'gender',
     ]);
     expect(payloadObject?.secondaryTwo).to.equal(undefined);
-  });
-
-  it('should transform all parties correctly', () => {
-    const form = {
-      data: twoSecondaryCaregivers,
-    };
-
-    const transformedData = submitTransform(formConfig, form);
-    const payloadData = JSON.parse(transformedData);
-    const payloadObject = JSON.parse(
-      payloadData.caregiversAssistanceClaim.form,
-    );
-
-    const veteranKeys = Object.keys(payloadObject.veteran);
-    const primaryKeys = Object.keys(payloadObject.primaryCaregiver);
-    const secondaryOneKeys = Object.keys(payloadObject.secondaryCaregiverOne);
-    const secondaryTwoKeys = Object.keys(payloadObject.secondaryCaregiverTwo);
-
-    expect(veteranKeys).to.deep.equal([
-      'lastTreatmentFacility',
-      'plannedClinic',
-      'address',
-      'primaryPhoneNumber',
-      'fullName',
-      'ssnOrTin',
-      'dateOfBirth',
-      'gender',
-    ]);
-    expect(primaryKeys).to.deep.equal([
-      'hasHealthInsurance',
-      'address',
-      'primaryPhoneNumber',
-      'vetRelationship',
-      'fullName',
-      'dateOfBirth',
-      'gender',
-    ]);
-    expect(secondaryOneKeys).to.deep.equal([
-      'address',
-      'primaryPhoneNumber',
-      'email',
-      'vetRelationship',
-      'fullName',
-      'dateOfBirth',
-    ]);
-    expect(secondaryTwoKeys).to.deep.equal([
-      'address',
-      'primaryPhoneNumber',
-      'email',
-      'vetRelationship',
-      'fullName',
-      'dateOfBirth',
-      'gender',
-    ]);
   });
 });
