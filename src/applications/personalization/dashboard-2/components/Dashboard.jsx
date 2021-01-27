614b81b28f7e8fd5fc4a434e8cf0e1254fe224fa
Sandra Hallie <sandra.adamshallie@gmail.com>
2021-01-27 07:45:41 -0700

    Add bradcrumbs and title

--- src/applications/personalization/dashboard-2/components/Dashboard.jsx
+++ src/applications/personalization/dashboard-2/components/Dashboard.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router';
+
+import Breadcrumbs from '@department-of-veterans-affairs/component-library/Breadcrumbs';
 
 import ApplyForBenefits from './apply-for-benefits/ApplyForBenefits';
 import ClaimsAndAppeals from './claims-and-appeals/ClaimsAndAppeals';
@@ -7,6 +10,19 @@ import HealthCare from './health-care/HealthCare';
 const Dashboard = () => {
   return (
     <div className="vads-l-grid-container vads-u-padding-x--0">
+      <Breadcrumbs>
+        <a href="/" key="home">
+          Home
+        </a>
+        <Link to="/" key="dashboard">
+          My VA
+        </Link>
+      </Breadcrumbs>
+
+      <h1 id="dashboard-title" tabIndex="-1">
+        My VA
+      </h1>
+
       <ClaimsAndAppeals />
       <HealthCare />
       <ApplyForBenefits />
