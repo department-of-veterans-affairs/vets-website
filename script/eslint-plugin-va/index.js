757918a5ec641aecf8bd19b229a4377b771b0fa6
rjohnson2011 <72466113+rjohnson2011@users.noreply.github.com>
2021-01-28 12:00:04 -0500

    Add Custom ES Lint Rule to Flag Deprecated Cypress Viewport Method (#15439)
    * Add Cypress viewpoint deprecated file
    
    * Refactors
    
    * Switch to warning not error
    
    * Eslintrc
    
    * Update yarn lock

--- script/eslint-plugin-va/index.js
+++ script/eslint-plugin-va/index.js
@@ -4,7 +4,8 @@ module.exports = {
     'enzyme-unmount': require('./rules/enzyme-unmount.js'),
     'use-resolved-path': require('./rules/use-resolved-path.js'),
     'resolved-path-on-required': require('./rules/resolved-path-on-required.js'),
-    'axe-check-required': require('./rules/axe-e2e-tests'),
+    'axe-check-required': require('./rules/axe-e2e-tests.js'),
+    'cypress-viewport-deprecated': require('./rules/cypress-viewport-deprecated.js'),
   },
   rulesConfig: {
     'proptypes-camel-cased': 2,
@@ -22,5 +23,6 @@ module.exports = {
       },
     ],
     'axe-check-required': 1,
+    'cypress-viewport-deprecated': 1,
   },
 };
