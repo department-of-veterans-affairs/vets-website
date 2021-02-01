6dcad13ff5b7fbc652d5cc4b6ea3fe58433edc76
Tim Wright <timwright12@gmail.com>
2021-02-01 10:11:44 -0500

    prep UI

--- src/site/stages/build/process-cms-exports/transformers/node-regional_health_care_service_des.js
+++ src/site/stages/build/process-cms-exports/transformers/node-regional_health_care_service_des.js
@@ -1,32 +1,41 @@
 /* eslint-disable camelcase */
 const { getDrupalValue, getWysiwygString } = require('./helpers');
 
-// const getFieldFacilityLocationObject = ({
-//   entity,
-//   title,
-//   entityUrl,
-//   field_facility_location,
-// }) =>
-//   typeof title === 'object'
-//     ? {
-//         entity: {
-//           title: getDrupalValue(title),
-//           fieldFacilityLocation: {
-//             entity: {
-//               title: getDrupalValue(field_facility_location[0].title),
-//               entityUrl: field_facility_location[0].entityUrl,
-//             },
-//           },
-//           entityUrl,
-//         },
-//       }
-//     : {
-//         entity: {
-//           title: entity?.title,
-//           fieldFacilityLocation: entity?.fieldFacilityLocation,
-//           entityUrl,
-//         },
-//       };
+const getFieldFacilityLocationObject = ({
+  entity,
+  title,
+  entityUrl,
+  field_facility_location,
+}) => {
+  const fieldFacilityTitle =
+    field_facility_location && field_facility_location[0]?.title
+      ? field_facility_location[0].title
+      : '';
+  const fieldFacilityUrl =
+    field_facility_location && field_facility_location[0]?.entityUrl
+      ? field_facility_location[0].entityUrl
+      : '';
+  return typeof title === 'object'
+    ? {
+        entity: {
+          title: getDrupalValue(title),
+          fieldFacilityLocation: {
+            entity: {
+              title: getDrupalValue(fieldFacilityTitle),
+              entityUrl: fieldFacilityUrl,
+            },
+          },
+          entityUrl,
+        },
+      }
+    : {
+        entity: {
+          title: entity?.title,
+          fieldFacilityLocation: entity?.fieldFacilityLocation,
+          entityUrl,
+        },
+      };
+};
 
 const transform = entity => ({
   entityType: 'node',
@@ -35,9 +44,9 @@ const transform = entity => ({
     processed: getWysiwygString(getDrupalValue(entity.fieldBody)),
   },
   fieldServiceNameAndDescripti: entity.fieldServiceNameAndDescripti[0],
-  // fieldLocalHealthCareService: entity.fieldLocalHealthCareService?.map(i =>
-  //   getFieldFacilityLocationObject(i),
-  // ),
+  fieldLocalHealthCareService: entity.fieldLocalHealthCareService?.map(i =>
+    getFieldFacilityLocationObject(i),
+  ),
 });
 module.exports = {
   filter: [
