a256f9e6911fbe6c5fcb6fa1bdad1123c2167b90
Bill Fienberg <billfienberg@gmail.com>
2021-02-01 20:14:42 -0600

    Add hacky script to get medallia script on allowed staging TeamSite pages

---
+++ src/site/assets/js/medallia/vagovprod.js
@@ -0,0 +1,40 @@
+// Medallia Embed Script
+(function(g) {
+  var isStaging = window.location.host.includes('staging');
+  if (!isStaging) return
+
+  var teamSitePathnames = [
+    /\/ormdi/i,
+    /\/ormdi\/NoFEAR_Select.asp/i,
+    /\/ormdi\/Contact_Us.asp/i,
+    /\/ormdi\/EEOcomplaint.asp/i,
+    /\/ormdi\/HPP.asp/i,
+    /\/ormdi\/Diversity_Inclusion.asp/i,
+    /\/ormdi\/Reasonable_Accommodations1.asp/i,
+    /\/adr/i,
+  ];
+  var pathname = window.location.pathname;
+  var isApprovedPathname = teamSitePathnames.some(x => x.test(pathname))
+  if (!isApprovedPathname) return
+
+  var d = document,
+    am = d.createElement('script'),
+    h = d.head || d.getElementsByTagName('head')[0],
+    fsr = 'fsReady',
+    aex = {
+      src: 'https://resource.digital.voice.va.gov/wdcvoice/5/onsite/embed.js',
+      type: 'text/javascript',
+      async: 'true',
+    };
+  for (var attr in aex) {
+    am.setAttribute(attr, aex[attr]);
+  }
+  h.appendChild(am);
+  g[fsr] ||
+    (g[fsr] = function() {
+      var aT = '__' + fsr + '_stk__';
+      g[aT] = g[aT] || [];
+      g[aT].push(arguments);
+    });
+})(window);
+
