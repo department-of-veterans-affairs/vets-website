10d47c7ad6f20bc8f9efa318d2327d9853fb8f22
Robin Garrison <robin.garrison@adhocteam.us>
2021-02-03 16:35:42 -0600

    Add usa-datefields class

--- src/platform/forms-system/src/js/widgets/DateWidget.jsx
+++ src/platform/forms-system/src/js/widgets/DateWidget.jsx
@@ -88,7 +88,7 @@ export default class DateWidget extends React.Component {
     }
 
     return (
-      <div className="usa-date-of-birth row">
+      <div className="usa-date-of-birth usa-datefields row">
         <div className="form-datefield-month">
           <label className="input-date-label" htmlFor={`${id}Month`}>
             Month
