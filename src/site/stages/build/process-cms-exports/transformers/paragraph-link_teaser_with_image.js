df6cd87b711eb79a2660db104345138687796355
Kelson Adams <kadams@governmentcio.com>
2021-02-04 14:02:17 -0700

    Add paragraph link teaser with image transformer

---
+++ src/site/stages/build/process-cms-exports/transformers/paragraph-link_teaser_with_image.js
@@ -0,0 +1,13 @@
+const { getDrupalValue, createLink } = require('./helpers');
+
+const transform = entity => ({
+  entity: {
+    fieldLinkTeaser: entity.fieldLinkTeaser?.[0],
+    fieldMedia: entity.fieldMedia?.[0],
+  },
+});
+
+module.exports = {
+  filter: ['field_link_teaser', 'field_media'],
+  transform,
+};
