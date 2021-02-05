fe4e8a42509e542552b0bf6455f5b880f9e6808c
Mark Dewey <mdewey@users.noreply.github.com>
2021-02-05 13:00:01 -0500

    Health care questionnaire 19024 error messages (#15932)
    * added error message on the completed page
    
    * added error message to the print button the confiormation page
    
    * added in error message for form
    
    * added test for new print buton
    
    * added print button
    
    * updated error message

--- src/applications/health-care-questionnaire/list/questionnaire-list/components/Shared/Buttons/ViewAndPrint.jsx
+++ src/applications/health-care-questionnaire/list/questionnaire-list/components/Shared/Buttons/ViewAndPrint.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-export default function ViewAndPrint({ displayArrow = true }) {
+export default function ViewAndPrint({
+  displayArrow = true,
+  onClick = () => {},
+}) {
   return (
-    <button className="usa-button va-button view-and-print-button">
+    <button
+      className="usa-button va-button view-and-print-button"
+      onClick={onClick}
+    >
       View and print questions
       {displayArrow && <i className={`fa fa-chevron-right`} />}
     </button>
