45731392296a5d41fd240616b909ad484613e888
Stephen Barrs <stephen.barrs@va.gov>
2021-02-05 13:09:53 -0500

    add GetHelp to introduction screen and update e2e tests

--- src/applications/coronavirus-vaccination/tests/e2e/signup.cypress.spec.js
+++ src/applications/coronavirus-vaccination/tests/e2e/signup.cypress.spec.js
@@ -38,7 +38,9 @@ describe('COVID-19 Vaccination Preparation Form', () => {
       cy.get('div').contains(
         'Your local VA health facility may contact you by phone, email, or text message. If you’re eligible and want to get a vaccine, we encourage you to respond.',
       );
-
+      cy.get('.help-talk').contains(
+        'If you have questions or need help filling out this form, call our MyVA411 main information line at 800-698-2411 (TTY: 711).',
+      );
       cy.axeCheck();
 
       cy.get('.usa-button').contains('Sign in');
