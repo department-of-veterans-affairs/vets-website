992ff8678287881c58c2dd08be5bdb029a8c1067
Jesse Cohn <jesse.cohn@teamadhoc.us>
2021-02-08 11:38:59 -0800

    Refactored to functional component

--- src/applications/personalization/view-dependents/components/ViewDependentsList/ViewDependentsListItem.jsx
+++ src/applications/personalization/view-dependents/components/ViewDependentsList/ViewDependentsListItem.jsx
@@ -1,78 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import classNames from 'classnames';
 
-class ViewDependentsListItem extends Component {
-  state = {
-    open: false,
+function ViewDependentsListItem(props) {
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(prevState => !prevState);
   };
-  handleToggle = () => {
-    this.setState({ open: !this.state.open });
-  };
-  render() {
-    const {
-      manageDependentsToggle,
-      firstName,
-      lastName,
-      relationship,
-      ssn,
-      dateOfBirth,
-    } = this.props;
-    return (
-      <div className="vads-l-row vads-u-background-color--gray-lightest vads-u-margin-top--0 vads-u-margin-bottom--2">
-        <dl
-          className={classNames({
-            'vads-l-row vads-u-padding-x--2': true,
-            'vads-u-margin-bottom--0': manageDependentsToggle === true,
-            'vads-u-margin-bottom--2': manageDependentsToggle === false,
-          })}
-        >
-          <dt className="vads-l-col--12 vads-u-margin--0 vads-u-font-size--lg vads-u-font-weight--bold">
-            {firstName} {lastName}
-          </dt>
 
+  const {
+    manageDependentsToggle,
+    firstName,
+    lastName,
+    relationship,
+    ssn,
+    dateOfBirth,
+  } = props;
+  return (
+    <div className="vads-l-row vads-u-background-color--gray-lightest vads-u-margin-top--0 vads-u-margin-bottom--2">
+      <dl
+        className={classNames({
+          'vads-l-row vads-u-padding-x--2': true,
+          'vads-u-margin-bottom--0': manageDependentsToggle === true,
+          'vads-u-margin-bottom--2': manageDependentsToggle === false,
+        })}
+      >
+        <dt className="vads-l-col--12 vads-u-margin--0 vads-u-font-size--lg vads-u-font-weight--bold">
+          {firstName} {lastName}
+        </dt>
+
+        <dd className="vads-l-col--12 vads-u-margin--0">
+          <dfn className="vads-u-font-weight--bold">Relationship:</dfn>{' '}
+          {relationship}
+        </dd>
+
+        {ssn ? (
           <dd className="vads-l-col--12 vads-u-margin--0">
-            <dfn className="vads-u-font-weight--bold">Relationship:</dfn>{' '}
-            {relationship}
+            <dfn className="vads-u-font-weight--bold">SSN:</dfn> {ssn}
           </dd>
+        ) : null}
 
-          {ssn ? (
-            <dd className="vads-l-col--12 vads-u-margin--0">
-              <dfn className="vads-u-font-weight--bold">SSN:</dfn> {ssn}
-            </dd>
-          ) : null}
-
-          {dateOfBirth ? (
-            <dd className="vads-l-col--12 vads-u-margin--0">
-              <dfn className="vads-u-font-weight--bold">Date of birth: </dfn>
-              {moment(dateOfBirth).format('MMMM D, YYYY')}
-            </dd>
-          ) : null}
-        </dl>
-        {manageDependentsToggle && (
-          <button
-            onClick={this.handleToggle}
-            className="usa-button-secondary vads-u-margin-x--2 vads-u-background-color--white"
-          >
-            Remove this dependent
-          </button>
-        )}
-        <div
-          className={`vads-l-row vads-l-row vads-u-padding-x--2 vads-u-display--${
-            this.state.open ? 'flex' : 'none'
-          }`}
+        {dateOfBirth ? (
+          <dd className="vads-l-col--12 vads-u-margin--0">
+            <dfn className="vads-u-font-weight--bold">Date of birth: </dfn>
+            {moment(dateOfBirth).format('MMMM D, YYYY')}
+          </dd>
+        ) : null}
+      </dl>
+      {manageDependentsToggle && (
+        <button
+          onClick={handleClick}
+          className="usa-button-secondary vads-u-margin-x--2 vads-u-background-color--white"
         >
-          <div className="vads-l-col--8">
-            <p>
-              To remove this dependent from your VA benefits, please enter the
-              information below.
-            </p>
-          </div>
+          Remove this dependent
+        </button>
+      )}
+      <div
+        className={`vads-l-row vads-l-row vads-u-padding-x--2 vads-u-display--${
+          open ? 'flex' : 'none'
+        }`}
+      >
+        <div className="vads-l-col--8">
+          <p>
+            To remove this dependent from your VA benefits, please enter the
+            information below.
+          </p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 ViewDependentsListItem.propTypes = {
