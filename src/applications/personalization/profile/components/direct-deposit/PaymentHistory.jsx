2396037ad83e7b318125a1bf2abd030545de2e7d
Sandra Hallie <sandra.adamshallie@gmail.com>
2021-02-08 08:58:36 -0700

    Minor update

--- src/applications/personalization/profile/components/direct-deposit/PaymentHistory.jsx
+++ src/applications/personalization/profile/components/direct-deposit/PaymentHistory.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import recordEvent from 'platform/monitoring/record-event';
+import recordEvent from '~/platform/monitoring/record-event';
 import ProfileInfoTable from '../ProfileInfoTable';
 
 function PaymentHistory() {
