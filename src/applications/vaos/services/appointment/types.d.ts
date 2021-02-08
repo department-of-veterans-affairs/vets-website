e141a606e5043eb453f8ebe5c62316bbb74b8013
Jeff Balboni <jeff@adhocteam.us>
2021-02-08 17:00:35 -0500

    Type updates

--- src/applications/vaos/services/appointment/types.d.ts
+++ src/applications/vaos/services/appointment/types.d.ts
@@ -8,20 +8,25 @@ interface FHIRType {
     }
   ];
 }
+type LocationReference = `Location/${string}`; 
+type OrganizationReference = `Organization/${string}`; 
+type HealthcareServiceReference = `HealthcareServiceReference/${string}`; 
+type PractitionerReference = `PractitionerReference/${string}`; 
 type ClinicPartipant = {
   actor: {
     reference: string;
     display: string;
   };
 };
+
 type VALocationPartipant = {
   actor: {
-    reference: string;
+    reference: LocationReference;
   };
 };
 type VideoPractitionerParticipant = {
   actor: {
-    reference: string;
+    reference: PractitionerReference;
   };
 };
 type CommunityCarePractitionerParticipant = {
@@ -59,7 +64,6 @@ type ATLASCode = {
   ];
 };
 
-type LocationId = `Location/${string}`; 
 type VideoVisitResource = {
   resourceType: 'HealthcareService';
   id: string;
@@ -69,11 +73,10 @@ type VideoVisitResource = {
     }
   ];
   providedBy: {
-    reference: string;
+    reference: OrganizationReference;
   };
   location?: {
-    // reference: string;
-    reference: LocationId;
+    reference: LocationReference; 
   };
   telecom: [
     {
@@ -90,14 +93,36 @@ type VideoVisitResource = {
 type ContainedResource = VideoVisitResource;
 
 export type Appointment = {
+  resourceType: 'Appointment';
+  // Mapped from the mobile appointment service id or the var-resources request id
   id: string;
+  // Mapped from createdDate field on requests, timezone is unclear
   created?: string;
+  // Mapped from appointmentRequestDetailCode, only for requests
   cancelationReason?: {
+    // veteranMessage field mapped only for requests, used for Express Care only
     text: string;
   };
-  resourceType: 'Appointment';
-  status: string;
-  description: string;
+  // Mapped from vdsAppointment.currentStatus or vvsAppointment.status.code for appoinments
+  // Mapped from status for requests
+  status: 
+    // Used for all community care appointments and any non-cancelled VistA appointments
+    'booked' |
+    // Mapped from cancelled for requests, or the set of cancelled statuses for appointments
+    'cancelled' | 
+    // Mapped from the Submitted status for requests
+    'proposed' |
+    // Mapped from the Escalated status on Express Care requests 
+    'pending' |
+    // Mapped from the Resolved status for Express Care requests
+    'fulfilled' |
+    // Unused
+    'arrived' |
+    // Unused
+    'noshow';
+  // Mapped from vdsAppointment.currentStatus or vvsAppointment.status.code for appoinments
+  // Null for other types of appointments/requests
+  description?: string;
   start?: string;
   minutesDuration: number;
   comment?: string;
