5b2939235444327089e1edae3a8f74d249dc5a80
Erik P. Hansen <eph@erikphansen.com>
2021-02-08 16:26:42 -0800

    application in progress component

--- src/applications/personalization/dashboard-2/components/apply-for-benefits/ApplyForBenefits.jsx
+++ src/applications/personalization/dashboard-2/components/apply-for-benefits/ApplyForBenefits.jsx
@@ -1,7 +1,192 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
 
-const ApplyForBenefits = () => {
-  return <h2>Apply for benefits</h2>;
+import { selectProfile } from '~/platform/user/selectors';
+import {
+  formTitles,
+  formLinks,
+  isSIPEnabledForm,
+  presentableFormIDs,
+  recordDashboardClick,
+  sipFormSorter,
+} from '~/applications/personalization/dashboard/helpers';
+
+const ApplicationInProgress = ({
+  formId,
+  formTitle,
+  continueUrl,
+  expirationDate,
+  lastOpenedDate,
+}) => {
+  return (
+    <div className="vads-u-display--flex vads-l-col--12 medium-screen:vads-l-col--6 large-screen:vads-l-col--4 medium-screen:vads-u-padding-right--3 vads-u-padding-bottom--3">
+      <div className="vads-u-display--flex  vads-u-flex-direction--column vads-u-justify-content--space-between vads-u-align-items--flex-start vads-u-background-color--gray-lightest vads-u-padding--2p5">
+        <div>
+          <div style={{ textTransform: 'uppercase' }}>{formId}</div>
+          <h4 className="vads-u-font-size--h3 vads-u-margin-top--0">
+            {formTitle}
+          </h4>
+          <p>Application expires on: {expirationDate}</p>
+          <p>Last opened on: {lastOpenedDate}</p>
+        </div>
+        <a
+          className="usa-button usa-button-primary"
+          aria-label={`Continue your ${formTitle}`}
+          href={continueUrl}
+          onClick={recordDashboardClick(formId, 'continue-button')}
+        >
+          Continue your application
+        </a>
+      </div>
+    </div>
+  );
 };
 
-export default ApplyForBenefits;
+const ApplyForBenefits = ({ savedForms }) => {
+  return (
+    <div>
+      <h2>Apply for benefits</h2>
+      <h3 className="vads-u-font-size--h4 vads-u-font-family--sans vads-u-margin-bottom--2p5">
+        Applications in progress
+      </h3>
+      {savedForms.length > 0 && (
+        <div className="vads-u-display--flex vads-u-flex-wrap--wrap">
+          {/* for each savedForm, pull off the correct data and pass it to a new ApplicationInProgress component */}
+          {savedForms.map(form => {
+            // blah...
+            const formId = form.form;
+            const formTitle = `Application for ${formTitles[formId]}`;
+            const presentableFormId = presentableFormIDs[formId];
+            const { lastUpdated, expiresAt } = form.metadata;
+            const lastOpenedDate = moment
+              .unix(lastUpdated)
+              .format('MMMM D, YYYY');
+            const expirationDate = moment
+              .unix(expiresAt)
+              .format('MMMM D, YYYY');
+            const continueUrl = `${formLinks[formId]}resume`;
+            return (
+              <ApplicationInProgress
+                key={formId}
+                formId={presentableFormId}
+                formTitle={formTitle}
+                lastOpenedDate={lastOpenedDate}
+                expirationDate={expirationDate}
+                continueUrl={continueUrl}
+              />
+            );
+          })}
+        </div>
+      )}
+      {!savedForms.length && <p>You have no forms in progress</p>}
+    </div>
+  );
+};
+
+// pull the applications off of the user object
+// for each one, create an ApplicationInProgress
+
+const mapStateToProps = state => {
+  const profileState = selectProfile(state);
+  const { savedForms } = profileState;
+  /*
+  const savedForms = [
+    {
+      form: '21P-527EZ',
+      metadata: {
+        version: 3,
+        returnUrl: '/military/reserve-national-guard',
+        savedAt: 1604951152710,
+        expiresAt: 1610135152,
+        lastUpdated: 1604951152,
+        inProgressFormId: 5105,
+      },
+      lastUpdated: 1604951152,
+    },
+    {
+      form: '28-1900',
+      metadata: {
+        version: 0,
+        returnUrl: '/communication-preferences',
+        savedAt: 1611946775267,
+        submission: {
+          status: false,
+          errorMessage: false,
+          id: false,
+          timestamp: false,
+          hasAttemptedSubmit: false,
+        },
+        expiresAt: 1617130775,
+        lastUpdated: 1611946775,
+        inProgressFormId: 9332,
+      },
+      lastUpdated: 1611946775,
+    },
+    {
+      form: '686C-674',
+      metadata: {
+        version: 1,
+        returnUrl: '/net-worth',
+        savedAt: 1607012813063,
+        submission: {
+          status: false,
+          errorMessage: false,
+          id: false,
+          timestamp: false,
+          hasAttemptedSubmit: false,
+        },
+        expiresAt: 1612196813,
+        lastUpdated: 1607012813,
+        inProgressFormId: 5179,
+      },
+      lastUpdated: 1607012813,
+    },
+    {
+      form: '28-8832',
+      metadata: {
+        version: 0,
+        returnUrl: '/claimant-information',
+        savedAt: 1611192804861,
+        submission: {
+          status: false,
+          errorMessage: false,
+          id: false,
+          timestamp: false,
+          hasAttemptedSubmit: false,
+        },
+        expiresAt: 1616376805,
+        lastUpdated: 1611192805,
+        inProgressFormId: 9240,
+      },
+      lastUpdated: 1611192805,
+    },
+    {
+      form: '21-526EZ',
+      metadata: {
+        version: 6,
+        returnUrl: '/review-veteran-details/separation-location',
+        savedAt: 1612535290474,
+        submission: {
+          status: false,
+          errorMessage: false,
+          id: false,
+          timestamp: false,
+          hasAttemptedSubmit: false,
+        },
+        expiresAt: 1644071290,
+        lastUpdated: 1612535290,
+        inProgressFormId: 9374,
+      },
+      lastUpdated: 1612535290,
+    },
+  ];
+   */
+  const verifiedSavedForms = savedForms
+    .filter(isSIPEnabledForm)
+    .sort(sipFormSorter);
+
+  return { savedForms: verifiedSavedForms };
+};
+
+export default connect(mapStateToProps)(ApplyForBenefits);
