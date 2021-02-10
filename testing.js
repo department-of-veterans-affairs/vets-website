c8df922ed9666aa14049606872ce4c2bcf7cb707
Nick Sullivan <nick.sullivan@adhocteam.us>
2021-02-10 15:21:09 -0500

    diff the other direction too

--- testing.js
+++ testing.js
@@ -16,9 +16,28 @@ const diff = oldEntities.filter(old => {
 
   const newbie = newEntities.find(n => n.entityBundle === old.entityBundle && n.entityId === old.entityId);
 
-  console.log('a newbie was found')
+  if (newbie) {
+    console.log('a newbie was found')
+    return !_.isEqual(old, newbie)
+  }
 
-  return !_.isEqual(old, newbie)
+  return old;
 })
 
 console.log(diff)
+
+const otherDiff = newEntities.filter(newbie => {
+  const oldie = oldEntities.find(n => n.entityBundle === newbie.entityBundle && n.entityId === newbie.entityId);
+
+  if (oldie) {
+    console.log('a oldie was found')
+    return !_.isEqual(oldie, newbie)
+  }
+
+  return newbie;
+})
+
+console.log(otherDiff)
+
+console.log(oldEntities.filter(old => JSON.stringify(old) !== '{}').length)
+console.log(newEntities.length)
