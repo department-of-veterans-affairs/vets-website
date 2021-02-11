38a4241fdaea901a0a3bee0b424b5bedecb22789
Craig Wheeler <craig@craigwheeler.net>
2021-02-11 11:34:17 -0500

    update contact

--- src/applications/financial-status-report/components/VeteranInfoBox.jsx
+++ src/applications/financial-status-report/components/VeteranInfoBox.jsx
@@ -40,10 +40,9 @@ const VeteranInfoBox = ({
       <p>
         <strong>Note: </strong>
         If you need to update your personal information, call our VA benefits
-        hotline at{' '}
-        <Telephone contact={'800-827-1000'} pattern={PATTERNS.OUTSIDE_US} /> (
-        <Telephone contact={CONTACTS[711]} pattern={PATTERNS[911]} />) , Monday
-        through Friday, 8:00 a.m. to 9:00 p.m. ET.
+        hotline at <Telephone contact={CONTACTS.VA_BENEFITS} /> (TTY:{' '}
+        <Telephone contact={CONTACTS[711]} pattern={PATTERNS['3_DIGIT']} />) ,
+        Monday through Friday, 8:00 a.m. to 9:00 p.m. ET.
       </p>
     </div>
   );
