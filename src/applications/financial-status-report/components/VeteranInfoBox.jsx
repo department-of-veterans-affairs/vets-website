3d22966b93b62fb07fe3e289bda80d672e9949d6
Craig Wheeler <craig@craigwheeler.net>
2021-02-11 11:10:42 -0500

    updated veteran info page

--- src/applications/financial-status-report/components/VeteranInfoBox.jsx
+++ src/applications/financial-status-report/components/VeteranInfoBox.jsx
@@ -2,6 +2,10 @@ import moment from 'moment';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
+import Telephone, {
+  CONTACTS,
+  PATTERNS,
+} from '@department-of-veterans-affairs/component-library/Telephone';
 
 const VeteranInfoBox = ({
   first,
@@ -36,8 +40,10 @@ const VeteranInfoBox = ({
       <p>
         <strong>Note: </strong>
         If you need to update your personal information, call our VA benefits
-        hotline at <a href="tel:800-827-1000">800-827-1000</a>, Monday through
-        Friday, 8:00 a.m. to 9:00 p.m. ET.
+        hotline at{' '}
+        <Telephone contact={'800-827-1000'} pattern={PATTERNS.OUTSIDE_US} /> (
+        <Telephone contact={CONTACTS[711]} pattern={PATTERNS[911]} />) , Monday
+        through Friday, 8:00 a.m. to 9:00 p.m. ET.
       </p>
     </div>
   );
