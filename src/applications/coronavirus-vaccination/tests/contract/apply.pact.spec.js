14a3badf41a728f3d9d71bc73d429c0fe4573772
Holden Hinkle <holdenhinkle@gmail.com>
2021-02-11 10:21:01 -0500

    add context blocks. change it block descriptions

--- src/applications/coronavirus-vaccination/tests/contract/apply.pact.spec.js
+++ src/applications/coronavirus-vaccination/tests/contract/apply.pact.spec.js
@@ -6,171 +6,179 @@ import unauthenticatedApplicationData from '../cypress/fixtures/data/unauthentic
 
 contractTest('Coronavirus Vaccination', 'VA.gov API', mockApi => {
   describe('GET /registration', () => {
-    it('request for saved submission that exists returns a 200 OK HTTP response and the registration data', async () => {
-      const interaction = {
-        state: 'retrieves previously saved submission data for user',
-        uponReceiving: 'a GET request',
-        withRequest: {
-          method: 'GET',
-          path: '/covid_vaccine/v0/registration',
-          headers: {
-            'X-Key-Inflection': 'camel',
-          },
-        },
-        willRespondWith: {
-          status: 200,
-          headers: {
-            'Content-Type': term({
-              matcher: '^application/json',
-              generate: 'application/json',
-            }),
-          },
-          body: {
-            data: {
-              id: like('BC79619E54A14BFF0D1607952232618112202'),
-              type: 'covid_vaccine_v0_registration_submissions',
-              attributes: like({
-                createdAt: '2020-12-14T13:23:52.929Z',
-                vaccineInterest: 'INTERESTED',
-                zipCode: '10001',
-                zipCodeDetails: 'Yes',
-                phone: '1112223333',
-                email: 'test@example.com',
-                firstName: 'JAMES',
-                lastName: 'BECK',
-                birthDate: '1989-11-11',
-              }),
+    context('saved registration exists', () => {
+      it('returns a 200 OK HTTP response and the registration data', async () => {
+        const interaction = {
+          state: 'retrieves previously saved submission data for user',
+          uponReceiving: 'a GET request',
+          withRequest: {
+            method: 'GET',
+            path: '/covid_vaccine/v0/registration',
+            headers: {
+              'X-Key-Inflection': 'camel',
             },
           },
-        },
-      };
+          willRespondWith: {
+            status: 200,
+            headers: {
+              'Content-Type': term({
+                matcher: '^application/json',
+                generate: 'application/json',
+              }),
+            },
+            body: {
+              data: {
+                id: like('BC79619E54A14BFF0D1607952232618112202'),
+                type: 'covid_vaccine_v0_registration_submissions',
+                attributes: like({
+                  createdAt: '2020-12-14T13:23:52.929Z',
+                  vaccineInterest: 'INTERESTED',
+                  zipCode: '10001',
+                  zipCodeDetails: 'Yes',
+                  phone: '1112223333',
+                  email: 'test@example.com',
+                  firstName: 'JAMES',
+                  lastName: 'BECK',
+                  birthDate: '1989-11-11',
+                }),
+              },
+            },
+          },
+        };
 
-      await mockApi().addInteraction(interaction);
+        await mockApi().addInteraction(interaction);
 
-      await retrievePreviouslySubmittedForm();
+        await retrievePreviouslySubmittedForm();
+      });
     });
 
-    it('request for saved registration that does not exist returns a 404 Not Found HTTP response and errors', async () => {
-      const interaction = {
-        state: 'submission data for user does not exist',
-        uponReceiving: 'a GET request',
-        withRequest: {
-          method: 'GET',
-          path: '/covid_vaccine/v0/registration',
-          headers: {
-            'X-Key-Inflection': 'camel',
+    context('saved registration does not exist', () => {
+      it('returns a 404 Not Found HTTP response and errors', async () => {
+        const interaction = {
+          state: 'submission data for user does not exist',
+          uponReceiving: 'a GET request',
+          withRequest: {
+            method: 'GET',
+            path: '/covid_vaccine/v0/registration',
+            headers: {
+              'X-Key-Inflection': 'camel',
+            },
           },
-        },
-        willRespondWith: {
-          status: 404,
-          headers: {
-            'Content-Type': term({
-              matcher: '^application/json',
-              generate: 'application/json',
-            }),
+          willRespondWith: {
+            status: 404,
+            headers: {
+              'Content-Type': term({
+                matcher: '^application/json',
+                generate: 'application/json',
+              }),
+            },
+            body: {
+              errors: [
+                {
+                  title: 'Record not found',
+                  detail: 'The record identified by  could not be found',
+                  code: '404',
+                  status: '404',
+                },
+              ],
+            },
           },
-          body: {
-            errors: [
-              {
-                title: 'Record not found',
-                detail: 'The record identified by  could not be found',
-                code: '404',
-                status: '404',
-              },
-            ],
-          },
-        },
-      };
+        };
 
-      await mockApi().addInteraction(interaction);
+        await mockApi().addInteraction(interaction);
 
-      try {
-        await retrievePreviouslySubmittedForm();
-      } catch (e) {
-        // Catch the 404 error.
-      }
+        try {
+          await retrievePreviouslySubmittedForm();
+        } catch (e) {
+          // Catch the 404 error.
+        }
+      });
     });
   });
 
   describe('POST /registration', () => {
-    it('authenticated success case: submit valid registration returns a 201 Created HTTP response', async () => {
-      const interaction = {
-        state: 'authenticated user submits registration data',
-        uponReceiving: 'a POST request',
-        withRequest: {
-          method: 'POST',
-          path: '/covid_vaccine/v0/registration',
-          headers: {
-            'X-Key-Inflection': 'camel',
-            'Content-Type': 'application/json',
+    context('authenticated user with valid registration', () => {
+      it('returns a 201 Created HTTP response', async () => {
+        const interaction = {
+          state: 'authenticated user submits registration data',
+          uponReceiving: 'a POST request',
+          withRequest: {
+            method: 'POST',
+            path: '/covid_vaccine/v0/registration',
+            headers: {
+              'X-Key-Inflection': 'camel',
+              'Content-Type': 'application/json',
+            },
+            body: authenticatedApplicationData,
           },
-          body: authenticatedApplicationData,
-        },
-        willRespondWith: {
-          status: 201,
-          headers: {
-            'Content-Type': term({
-              matcher: '^application/json',
-              generate: 'application/json',
-            }),
-          },
-          body: {
-            data: {
-              id: string('BC79619E54A14BFF0D1607952232618112202'),
-              type: 'covid_vaccine_v0_registration_submissions',
-              attributes: like({
-                createdAt: '2020-12-14T13:23:52.929Z',
-                vaccineInterest: 'INTERESTED',
-                zipCode: '10001',
+          willRespondWith: {
+            status: 201,
+            headers: {
+              'Content-Type': term({
+                matcher: '^application/json',
+                generate: 'application/json',
               }),
             },
+            body: {
+              data: {
+                id: string('BC79619E54A14BFF0D1607952232618112202'),
+                type: 'covid_vaccine_v0_registration_submissions',
+                attributes: like({
+                  createdAt: '2020-12-14T13:23:52.929Z',
+                  vaccineInterest: 'INTERESTED',
+                  zipCode: '10001',
+                }),
+              },
+            },
           },
-        },
-      };
+        };
 
-      await mockApi().addInteraction(interaction);
+        await mockApi().addInteraction(interaction);
 
-      await saveForm(authenticatedApplicationData);
+        await saveForm(authenticatedApplicationData);
+      });
     });
 
-    it('unauthenticated success case: submit valid registration will return a 201 Created HTTP response', async () => {
-      const interaction = {
-        state: 'unauthenticated user submits registration data',
-        uponReceiving: 'a POST request',
-        withRequest: {
-          method: 'POST',
-          path: '/covid_vaccine/v0/registration',
-          headers: {
-            'X-Key-Inflection': 'camel',
-            'Content-Type': 'application/json',
+    context('unauthenticated user with valid registration', () => {
+      it('returns a 201 Created HTTP response', async () => {
+        const interaction = {
+          state: 'unauthenticated user submits registration data',
+          uponReceiving: 'a POST request',
+          withRequest: {
+            method: 'POST',
+            path: '/covid_vaccine/v0/registration',
+            headers: {
+              'X-Key-Inflection': 'camel',
+              'Content-Type': 'application/json',
+            },
+            body: unauthenticatedApplicationData,
           },
-          body: unauthenticatedApplicationData,
-        },
-        willRespondWith: {
-          status: 201,
-          headers: {
-            'Content-Type': term({
-              matcher: '^application/json',
-              generate: 'application/json',
-            }),
-          },
-          body: {
-            data: {
-              id: string('BC79619E54A14BFF0D1607952232618112202'),
-              type: 'covid_vaccine_v0_registration_submissions',
-              attributes: like({
-                createdAt: '2020-12-14T13:23:52.929Z',
-                vaccineInterest: 'INTERESTED',
-                zipCode: '10001',
+          willRespondWith: {
+            status: 201,
+            headers: {
+              'Content-Type': term({
+                matcher: '^application/json',
+                generate: 'application/json',
               }),
             },
+            body: {
+              data: {
+                id: string('BC79619E54A14BFF0D1607952232618112202'),
+                type: 'covid_vaccine_v0_registration_submissions',
+                attributes: like({
+                  createdAt: '2020-12-14T13:23:52.929Z',
+                  vaccineInterest: 'INTERESTED',
+                  zipCode: '10001',
+                }),
+              },
+            },
           },
-        },
-      };
+        };
 
-      await mockApi().addInteraction(interaction);
+        await mockApi().addInteraction(interaction);
 
-      await saveForm(unauthenticatedApplicationData);
+        await saveForm(unauthenticatedApplicationData);
+      });
     });
   });
 });
