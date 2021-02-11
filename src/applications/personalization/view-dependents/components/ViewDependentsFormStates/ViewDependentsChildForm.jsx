09c62f922c3f85a4de7f7818b5cd6bcc23754968
Jesse Cohn <jesse.cohn@teamadhoc.us>
2021-02-11 08:41:08 -0800

    Added basic schema form

--- src/applications/personalization/view-dependents/components/ViewDependentsFormStates/ViewDependentsChildForm.jsx
+++ src/applications/personalization/view-dependents/components/ViewDependentsFormStates/ViewDependentsChildForm.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import SchemaForm from 'platform/forms-system/src/js/components/SchemaForm';
 
 function ViewDependentsChildForm() {
-  return <div>This is the child form.</div>;
+  const schema = {
+    type: 'object',
+    properties: {
+      firstName: {
+        type: 'string',
+      },
+    },
+  };
+
+  const uiSchema = {};
+
+  function show() {}
+
+  return (
+    <SchemaForm
+      name="View Dependents Child Form"
+      title="Our form"
+      schema={schema}
+      uiSchema={uiSchema}
+      onSubmit={show}
+    />
+  );
 }
 
 export default ViewDependentsChildForm;
