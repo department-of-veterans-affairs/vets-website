5cce3678ed6fb3c16c016decc5bab08c2e583d81
Matt Roth <roth_matthew@bah.com>
2021-02-12 11:51:59 -0500

    nav bar width accounted for

--- src/applications/gi-sandbox/components/ProfileNavBar.jsx
+++ src/applications/gi-sandbox/components/ProfileNavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { createId } from '../utils/helpers';
+import classNames from 'classnames';
 
 export class ProfileNavBar extends React.Component {
   componentDidMount() {
@@ -11,37 +12,41 @@ export class ProfileNavBar extends React.Component {
     window.removeEventListener('scroll', this.handleScroll, true);
   }
 
+  classes = (stuck = false) => {
+    return classNames('profile-nav-bar', 'nav-bar-desktop-only', {
+      'profile-nav-bar-stuck': stuck,
+    });
+  };
+
   handleScroll = () => {
     const profileNavBar = document.getElementById('profile-nav-bar-desktop');
-    const headingSummary = document.getElementById('profile-nav-container');
+    const headingSummary = document.getElementById('profile-nav-placeholder');
 
     const topOffset = profileNavBar.getBoundingClientRect().top <= 0;
     const bottomOffset = headingSummary.getBoundingClientRect().bottom <= 0;
 
     if (topOffset && bottomOffset) {
-      profileNavBar.className = 'profile-nav-bar-stuck desktop-only';
+      profileNavBar.className = this.classes(true);
     } else {
-      profileNavBar.className = 'profile-nav-bar-free desktop-only';
+      profileNavBar.className = this.classes();
     }
   };
 
   render() {
     return (
-      <div id="profile-nav-container">
-        <div
-          id="profile-nav-bar-desktop"
-          className="profile-nav-bar-free desktop-only"
-        >
+      <>
+        <span id="profile-nav-placeholder" />
+        <div id="profile-nav-bar-desktop" className={this.classes()}>
           {this.props.profileSections.map(section => (
             <span
-              className="vads-u-margin-right--2"
+              className="vads-u-margin-right--1p5"
               key={`${createId(section)}-nav-bar`}
             >
               <a href={`#${createId(section)}`}>{section}</a>
             </span>
           ))}
         </div>
-      </div>
+      </>
     );
   }
 }
