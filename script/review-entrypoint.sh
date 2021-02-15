ce51d10b8bf5b27764ee90e77fc664b3dd0123cc
Tim Wright <timwright12@gmail.com>
2021-02-15 15:29:13 -0500

    Added CMS Export to review instance build

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 yarn install --production=false
 npm run fetch-drupal-cache
-npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+npm run build -- --use-cms-export --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
 npm run heroku-serve -- build/localhost -p 3001
