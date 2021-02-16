f04612b403d24b852221316d6656b873d6687970
zacharymorel <zmorel@governmentcio.com>
2021-02-16 11:35:15 -0500

    Added Error e2e test

--- src/applications/yellow-ribbon/e2e/tests/yellow-ribbon.cypress.spec.js
+++ src/applications/yellow-ribbon/e2e/tests/yellow-ribbon.cypress.spec.js
@@ -7,6 +7,7 @@ const SELECTORS = {
   SEARCH_FORM: '[data-e2e-id="search-form"]',
   SEARCH_RESULTS: '[data-e2e-id="search-results"]',
   SEARCH_RESULT_TITLE: '[data-e2e-id="result-title"]',
+  ERROR_ALERT_BOX: '.usa-alert.usa-alert-error',
 };
 
 function axeTestPage() {
@@ -19,7 +20,7 @@ describe('functionality of Yellow Ribbons', () => {
     if (Cypress.env('CIRCLECI')) this.skip();
   });
 
-  it('search the form and expect dom to have elements', () => {
+  it('search the form and expect dom to have elements on success', () => {
     cy.server();
     cy.route({
       method: 'GET',
@@ -80,5 +81,51 @@ describe('functionality of Yellow Ribbons', () => {
     // Ensure Alert Box Closes
     cy.get(`${SELECTORS.APP} .usa-alert.usa-alert-info button`).click();
     cy.get(`${SELECTORS.APP} .usa-alert.usa-alert-info`).should('not.exist');
+  });
+
+  it('search the form and expect dom to have elements on error', () => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      response: [],
+      status: 500,
+      url:
+        '/v0/gi/yellow_ribbon_programs?city=Austin&name=university&page=1&per_page=10&state=TX',
+    }).as('getSchoolsInYR');
+
+    // navigate to yellow-ribbon and make axe check on browser
+    cy.visit('/education/yellow-ribbon-participating-schools/');
+    axeTestPage();
+
+    // Ensure App is present
+    cy.get(SELECTORS.APP);
+
+    // Ensure form is present
+    cy.get(SELECTORS.SEARCH_FORM);
+
+    // Fill out and submit the form.
+    cy.get(`${SELECTORS.SEARCH_FORM} input[name="yr-search-name"]`).type(
+      'university',
+    );
+    cy.get(`${SELECTORS.SEARCH_FORM} select`).select(`Texas`);
+
+    cy.get(`${SELECTORS.SEARCH_FORM} input[name="yr-search-city"]`).type(
+      'Austin',
+    );
+    cy.get(`${SELECTORS.SEARCH_FORM} button[type="submit"]`).click();
+    cy.wait('@getSchoolsInYR');
+
+    // Ensure ERROR Alert Box exists
+    cy.get(`${SELECTORS.ERROR_ALERT_BOX}`)
+      // Check Headline.
+      .should('contain', 'Something went wrong')
+      // Check contain error message
+      .should(
+        'contain',
+        'We’re sorry. Something went wrong on our end. Please try again later.',
+      );
+
+    // A11y check the search results.
+    axeTestPage();
   });
 });
