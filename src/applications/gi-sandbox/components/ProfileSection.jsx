a972a4535aec563bc4722d07d1560a65b270287a
Matt Roth <roth_matthew@bah.com>
2021-02-16 14:18:39 -0500

    profile section borders

--- src/applications/gi-sandbox/components/ProfileSection.jsx
+++ src/applications/gi-sandbox/components/ProfileSection.jsx
@@ -4,8 +4,12 @@ import { createId } from '../utils/helpers';
 
 export const ProfileSection = ({ name, children }) => (
   <li>
-    <h2 id={createId(name)}>{name}</h2>
-    {children}
+    <h2 id={createId(name)} className="profile-section-header">
+      {name}
+    </h2>
+    <div id={`${createId(name)}-body`} className="profile-section-body">
+      {children}
+    </div>
   </li>
 );
 
