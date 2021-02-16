d08017d0f68c3f327b1960a2dfa7949a5c98ddfc
Tim Wright <timwright12@gmail.com>
2021-02-16 11:43:34 -0500

    added = sign to buildType

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 yarn install --production=false
 npm run fetch-drupal-cache
-npm run build -- --is-review-instance --use-cms-export --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+npm run build -- --is-review-instance --use-cms-export --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
 npm run heroku-serve -- build/localhost -p 3001
