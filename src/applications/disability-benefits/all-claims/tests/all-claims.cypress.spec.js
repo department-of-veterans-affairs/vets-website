3ae5471405a0c18822ba1aee8c3c41dc28d5e58a
Robin Garrison <robin.garrison@adhocteam.us>
2021-02-17 09:20:46 -0600

    Fix unit tests

--- src/applications/disability-benefits/all-claims/tests/all-claims.cypress.spec.js
+++ src/applications/disability-benefits/all-claims/tests/all-claims.cypress.spec.js
@@ -143,11 +143,7 @@ const testConfig = createTestConfig(
       // because fixtures don't evaluate JS.
       cy.intercept('GET', '/v0/intent_to_file', mockItf);
 
-      cy.intercept(
-        'PUT',
-        '/v0/disability_compensation_in_progress_forms/*',
-        mockInProgress,
-      );
+      cy.intercept('PUT', '/v0/in_progress_forms/*', mockInProgress);
 
       cy.intercept(
         'GET',
@@ -181,24 +177,20 @@ const testConfig = createTestConfig(
           ({ 'view:selected': _, ...obj }) => obj,
         );
 
-        cy.intercept(
-          'GET',
-          'v0/disability_compensation_in_progress_forms/21-526EZ',
-          {
-            formData: {
-              veteran: {
-                primaryPhone: '4445551212',
-                emailAddress: 'test2@test1.net',
-              },
-              disabilities: sanitizedRatedDisabilities,
+        cy.intercept('GET', 'v0/in_progress_forms/21-526EZ', {
+          formData: {
+            veteran: {
+              primaryPhone: '4445551212',
+              emailAddress: 'test2@test1.net',
             },
-            metadata: {
-              version: 0,
-              prefill: true,
-              returnUrl: '/veteran-information',
-            },
+            disabilities: sanitizedRatedDisabilities,
           },
-        );
+          metadata: {
+            version: 0,
+            prefill: true,
+            returnUrl: '/veteran-information',
+          },
+        });
       });
     },
 
