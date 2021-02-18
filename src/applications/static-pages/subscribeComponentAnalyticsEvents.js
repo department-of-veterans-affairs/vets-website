ba024dba380c895837d9300fce811e7a99cceb9a
Brian Martin <brian.martin@empiricalpath.com>
2021-02-18 15:44:09 -0500

    Added event handler for component librayr events

---
+++ src/applications/static-pages/subscribeComponentAnalyticsEvents.js
@@ -0,0 +1,52 @@
+import recordEvent from 'platform/monitoring/record-event';
+
+const analyticsEvents = [
+  {
+    componentName: 'Modal',
+    prefix: 'modal',
+    actions: [{ action: 'show', name: 'int-modal-click' }],
+  },
+  {
+    componentName: 'AdditionalInfo',
+    prefix: 'additional-info',
+    actions: [
+      { action: 'expand', name: 'int-additional-info-expand' },
+      { action: 'collapse', name: 'int-additional-info-collapse' },
+    ],
+  },
+];
+
+export default function subscribeComponentAnalyticsEvents() {
+  document.body.addEventListener('component-library-analytics', e => {
+    // Is it a component we are tracking?
+    const component = analyticsEvents.find(
+      c => c.componentName === e.detail.componentName,
+    );
+
+    if (component) {
+      // Is it an action we are tracking?
+      const dataLayer = {};
+
+      if (component.actions) {
+        const action = component.actions.find(
+          ev => ev.action === e.detail.action,
+        );
+
+        if (action && e.detail.details) {
+          for (const key of Object.keys(e.detail.details)) {
+            const newKey = component.prefix
+              ? `${component.prefix}-${key}`
+              : key;
+
+            dataLayer[newKey] = e.detail.details[key];
+          }
+        }
+
+        // Add the event name
+        dataLayer.event = action.name;
+
+        recordEvent(dataLayer);
+      }
+    }
+  });
+}
