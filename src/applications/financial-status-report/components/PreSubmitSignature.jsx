71fb69510c2a584adba63173f7b98b0646a2bb30
Craig Wheeler <craig@craigwheeler.net>
2021-02-20 12:44:07 -0500

    review page

--- src/applications/financial-status-report/components/PreSubmitSignature.jsx
+++ src/applications/financial-status-report/components/PreSubmitSignature.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Checkbox from '@department-of-veterans-affairs/component-library/Checkbox';
 
-const PreSubmitSignature = ({ formData }) => {
-  const [isChecked, setIsChecked] = useState(false);
+const PreSubmitSignature = ({ formData, showError }) => {
+  const [checked, setChecked] = useState(false);
+  const [error, setError] = useState(false);
   const [veteranName, setVeteranName] = useState('');
 
   useEffect(
@@ -17,42 +18,56 @@ const PreSubmitSignature = ({ formData }) => {
     [formData.personalData],
   );
 
+  useEffect(
+    () => {
+      if (showError && !checked) {
+        setError(true);
+      } else {
+        setError(false);
+      }
+    },
+    [checked, showError],
+  );
+
   return (
     <>
       <p>
-        Please review information entered into this request and sign the
-        statement below. Your entries provided below will serve as your
-        electronic signature for the form.
+        Please click on each of the sections above to review the information you
+        entered for this request. Then read and sign the Veteran’s statement of
+        truth. The name you enter will serve as your electronic signature for
+        this request.
       </p>
-      <article className="vads-u-background-color--gray-lightest vads-u-padding-bottom--6 vads-u-padding-x--3 vads-u-padding-top--1px vads-u-margin-bottom--7">
+      <article className="vads-u-background-color--gray-lightest vads-u-padding-bottom--6 vads-u-padding-x--3 vads-u-padding-top--1px">
         <h3>Veteran's statement of truth</h3>
         <p>
-          I certify that the income, assets, and expenses I provided on this
-          form are correct to the best of my knowledge.
+          I’ve reviewed the information I provided in this request, including:
         </p>
-        <p>
-          I certify that my marital and household compensation has been
-          correctly represented.
-        </p>
+        <ul>
+          <li>My marital status and number of dependents</li>
+          <li>My income (and my spouse’s income if included)</li>
+          <li>My household assets and expenses</li>
+          <li>My bankruptcy history</li>
+        </ul>
         <label htmlFor="signatureInput">Veteran's full name</label>
         <input
           name="signatureInput"
           type="text"
           value={veteranName}
-          onChange={event => setVeteranName(event.target.value)}
+          onChange={e => setVeteranName(e.target.value)}
         />
         <Checkbox
-          checked={isChecked}
-          onValueChange={value => setIsChecked(value)}
-          label="I certify the information above is correct and true to the best of my knowledge and belief."
-          errorMessage={'Must certify by checking box'}
+          checked={checked}
+          onValueChange={value => setChecked(value)}
+          label="By checking this box, I certify that the information in this request is true and correct to the best of my knowledge and belief."
+          errorMessage={error && 'Must certify by checking box'}
           required
         />
       </article>
       <p>
-        <strong>Note: </strong> The law provides severe penalties which include
-        fine or imprisonment, or both, for the willful submission of any
-        statement or evidence of a material fact, knowing it to be false.
+        <strong>Note: </strong>
+        It is a crime to knowingly submit false statements or information that
+        could affect our decision on this request. Penalties may include a fine,
+        imprisonment, or both.
       </p>
     </>
   );
