f9e668774b22b1ce9d10757441216fd90dfde0be
Micah Chiang <micahkchiang@gmail.com>
2021-02-22 15:37:02 -0500

    add schemas for remove child workflows

--- src/applications/personalization/view-dependents/components/ViewDependentsList/ViewDependentsListItem.jsx
+++ src/applications/personalization/view-dependents/components/ViewDependentsList/ViewDependentsListItem.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
+import Scroll from 'react-scroll';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import classNames from 'classnames';
+import { focusElement } from 'platform/utilities/ui';
 import ManageDependents from '../../manage-dependents/containers/ManageDependentsApp';
+
+const scroller = Scroll.scroller;
+const scrollToTop = el => {
+  scroller.scrollTo(el, {
+    duration: 500,
+    delay: 0,
+    smooth: true,
+  });
+};
 
 function ViewDependentsListItem(props) {
   const [open, setOpen] = useState(false);
-  const handleClick = () => {
-    setOpen(prevState => !prevState);
-  };
 
   const {
     manageDependentsToggle,
@@ -20,6 +28,17 @@ function ViewDependentsListItem(props) {
     stateKey,
   } = props;
 
+  const handleClick = () => {
+    setOpen(prevState => !prevState);
+    if (open) {
+      const focusEl = document?.querySelectorAll('.mng-dependents-name')?.[
+        stateKey
+      ];
+      focusElement(focusEl);
+      scrollToTop(focusEl);
+    }
+  };
+
   return (
     <div className="vads-l-row vads-u-background-color--gray-lightest vads-u-margin-top--0 vads-u-margin-bottom--2 vads-u-padding-x--2">
       <dl
@@ -29,7 +48,11 @@ function ViewDependentsListItem(props) {
           'vads-u-margin-bottom--2': manageDependentsToggle === false,
         })}
       >
-        <dt className="vads-l-col--12 vads-u-margin--0 vads-u-font-size--lg vads-u-font-weight--bold">
+        <dt
+          // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+          tabIndex="0"
+          className="vads-l-col--12 vads-u-margin--0 vads-u-font-size--lg vads-u-font-weight--bold mng-dependents-name"
+        >
           {firstName} {lastName}
         </dt>
 
@@ -55,6 +78,7 @@ function ViewDependentsListItem(props) {
         <div className="vads-l-col--12 medium-screen:vads-l-col--8">
           {!open && (
             <button
+              type="button"
               onClick={handleClick}
               className="usa-button-secondary vads-u-background-color--white"
             >
