0f57268b3f70e78976a9c5aaf1c48219a27f05ea
narin <n.ratana@gmail.com>
2021-02-22 22:03:21 -0800

    Add cancel modal

--- src/applications/vaos/appointment-list/components/ConfirmedAppointmentDetailsPage/ConfirmedAppointmentDetailsPage.jsx
+++ src/applications/vaos/appointment-list/components/ConfirmedAppointmentDetailsPage/ConfirmedAppointmentDetailsPage.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import LoadingIndicator from '@department-of-veterans-affairs/component-library/LoadingIndicator';
+import AlertBox from '@department-of-veterans-affairs/component-library/AlertBox';
 
+import CancelAppointmentModal from '../cancel/CancelAppointmentModal';
 import AddToCalendar from '../../../components/AddToCalendar';
 import VAFacilityLocation from '../../../components/VAFacilityLocation';
 import moment from '../../../lib/moment-tz';
@@ -16,12 +18,16 @@ import {
   isVideoHome,
   isVideoVAFacility,
 } from '../../../services/appointment';
-import { FETCH_STATUS, PURPOSE_TEXT } from '../../../utils/constants';
+import {
+  APPOINTMENT_STATUS,
+  FETCH_STATUS,
+  PURPOSE_TEXT,
+} from '../../../utils/constants';
 import { scrollAndFocus } from '../../../utils/scrollAndFocus';
 import * as actions from '../../redux/actions';
-import { cancelAppointment } from '../../redux/actions';
 import AppointmentDateTime from './AppointmentDateTime';
 import AppointmentInstructions from './AppointmentInstructions';
+import { getCancelInfo } from '../../redux/selectors';
 import { selectFeatureCancel } from '../../../redux/selectors';
 import VideoVisitSection from './VideoVisitSection';
 import { formatFacilityAddress } from 'applications/vaos/services/location';
@@ -49,8 +55,12 @@ function formatHeader(appointment) {
 }
 
 function ConfirmedAppointmentDetailsPage({
-  appointmentDetails,
+  appointment,
   appointmentDetailsStatus,
+  cancelAppointment,
+  cancelInfo,
+  closeCancelAppointment,
+  confirmCancelAppointment,
   facilityData,
   fetchConfirmedAppointmentDetails,
   confirmedStatus,
@@ -59,8 +69,6 @@ function ConfirmedAppointmentDetailsPage({
   const { id } = useParams();
   const history = useHistory();
 
-  const appointment = appointmentDetails?.[id];
-
   useEffect(() => {
     const status = confirmedStatus === FETCH_STATUS.succeeded;
 
@@ -68,17 +76,29 @@ function ConfirmedAppointmentDetailsPage({
       history.push('/');
     }
 
-    if (!appointment) {
+    if (!appointment || appointment.id !== id) {
       fetchConfirmedAppointmentDetails(id);
     }
 
     scrollAndFocus();
   }, []);
 
+  useEffect(
+    () => {
+      if (
+        !cancelInfo.showCancelModal &&
+        cancelInfo.cancelAppointmentStatus === FETCH_STATUS.succeeded
+      ) {
+        scrollAndFocus();
+      }
+    },
+    [cancelInfo.showCancelModal, cancelInfo.cancelAppointmentStatus],
+  );
+
   if (appointmentDetailsStatus === FETCH_STATUS.loading) {
     return (
       <div className="vads-u-margin-y--8">
-        <LoadingIndicator message="Loading your appointment request...!!!!" />
+        <LoadingIndicator message="Loading your appointment..." />
       </div>
     );
   }
@@ -87,6 +107,7 @@ function ConfirmedAppointmentDetailsPage({
     return null;
   }
 
+  const canceled = appointment.status === APPOINTMENT_STATUS.cancelled;
   const isVideo = isVideoAppointment(appointment);
   const facilityId = isVideo
     ? getVideoAppointmentLocation(appointment)
@@ -114,6 +135,16 @@ function ConfirmedAppointmentDetailsPage({
           facilityId={getVARFacilityId(appointment)}
         />
       </h1>
+
+      {canceled && (
+        <AlertBox
+          status="error"
+          className="vads-u-display--block vads-u-margin-bottom--2"
+          backgroundOnly
+        >
+          This appointment has been canceled
+        </AlertBox>
+      )}
 
       {isVideo && (
         <>
@@ -149,57 +180,63 @@ function ConfirmedAppointmentDetailsPage({
                 </div>
               )}
 
-            <div className="vads-u-margin-top--3 vaos-appts__block-label">
-              <i
-                aria-hidden="true"
-                className="far fa-calendar vads-u-margin-right--1"
-              />
-              <AddToCalendar
-                summary={`${header}`}
-                description={`instructionText`}
-                location={formatFacilityAddress(facility)}
-                duration={appointment.minutesDuration}
-                startDateTime={appointment.start}
-              />
-            </div>
+            {!canceled && (
+              <>
+                <div className="vads-u-margin-top--3 vaos-appts__block-label">
+                  <i
+                    aria-hidden="true"
+                    className="far fa-calendar vads-u-margin-right--1"
+                  />
+                  <AddToCalendar
+                    summary={`${header}`}
+                    description={`instructionText`}
+                    location={formatFacilityAddress(facility)}
+                    duration={appointment.minutesDuration}
+                    startDateTime={appointment.start}
+                  />
+                </div>
 
-            <div className="vads-u-margin-top--2 vaos-appts__block-label">
-              <i
-                aria-hidden="true"
-                className="fas fa-print vads-u-margin-right--1"
-              />
-              <a href="#">Print</a>
-            </div>
+                <div className="vads-u-margin-top--2 vaos-appts__block-label">
+                  <i
+                    aria-hidden="true"
+                    className="fas fa-print vads-u-margin-right--1"
+                  />
+                  <a href="#">Print</a>
+                </div>
 
-            <div className="vads-u-margin-top--2 vaos-appts__block-label">
-              <i
-                aria-hidden="true"
-                className="fas fa-clock vads-u-margin-right--1"
-              />
-              <a href="#">Reschedule</a>
-            </div>
+                <div className="vads-u-margin-top--2 vaos-appts__block-label">
+                  <i
+                    aria-hidden="true"
+                    className="fas fa-clock vads-u-margin-right--1"
+                  />
+                  <a href="#">Reschedule</a>
+                </div>
 
-            {showCancelButton && (
-              <div className="vads-u-margin-top--2 vaos-appts__block-label">
-                <i
-                  aria-hidden="true"
-                  className="fas fa-times vads-u-margin-right--1 vads-u-font-size--lg"
-                />
-                <button
-                  onClick={() => cancelAppointment(appointment)}
-                  aria-label={`Cancel appointment on ${formatAppointmentDate(
-                    moment.parseZone(appointment.start),
-                  )}`}
-                  className="vaos-appts__cancel-btn va-button-link vads-u-margin--0 vads-u-flex--0"
-                >
-                  Cancel appointment
-                  <span className="sr-only">
-                    {' '}
-                    on{' '}
-                    {formatAppointmentDate(moment.parseZone(appointment.start))}
-                  </span>
-                </button>
-              </div>
+                {showCancelButton && (
+                  <div className="vads-u-margin-top--2 vaos-appts__block-label">
+                    <i
+                      aria-hidden="true"
+                      className="fas fa-times vads-u-margin-right--1 vads-u-font-size--lg"
+                    />
+                    <button
+                      onClick={() => cancelAppointment(appointment)}
+                      aria-label={`Cancel appointment on ${formatAppointmentDate(
+                        moment.parseZone(appointment.start),
+                      )}`}
+                      className="vaos-appts__cancel-btn va-button-link vads-u-margin--0 vads-u-flex--0"
+                    >
+                      Cancel appointment
+                      <span className="sr-only">
+                        {' '}
+                        on{' '}
+                        {formatAppointmentDate(
+                          moment.parseZone(appointment.start),
+                        )}
+                      </span>
+                    </button>
+                  </div>
+                )}
+              </>
             )}
           </>
         )}
@@ -209,13 +246,18 @@ function ConfirmedAppointmentDetailsPage({
           « Go back to appointments
         </Link>
       </div>
+      <CancelAppointmentModal
+        {...cancelInfo}
+        onConfirm={confirmCancelAppointment}
+        onClose={closeCancelAppointment}
+      />
     </div>
   );
 }
 
 function mapStateToProps(state) {
   const {
-    appointmentDetails,
+    currentAppointment,
     appointmentDetailsStatus,
     facilityData,
     confirmedStatus,
@@ -223,16 +265,20 @@ function mapStateToProps(state) {
   } = state.appointments;
 
   return {
-    appointmentDetails,
+    appointment: currentAppointment,
     appointmentDetailsStatus,
-    facilityData,
+    cancelInfo: getCancelInfo(state),
     confirmedStatus,
+    facilityData,
     requestMessages,
     showCancelButton: selectFeatureCancel(state),
   };
 }
 
 const mapDispatchToProps = {
+  cancelAppointment: actions.cancelAppointment,
+  closeCancelAppointment: actions.closeCancelAppointment,
+  confirmCancelAppointment: actions.confirmCancelAppointment,
   fetchConfirmedAppointmentDetails: actions.fetchConfirmedAppointmentDetails,
 };
 
