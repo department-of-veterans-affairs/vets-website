35ead7ff8f906fbd9597069485012691eb89dd8e
Brian Martin <brian.martin@empiricalpath.com>
2021-02-24 13:40:18 -0500

    Simplified lookup, removed prefix param

--- src/applications/static-pages/subscribeComponentAnalyticsEvents.js
+++ src/applications/static-pages/subscribeComponentAnalyticsEvents.js
@@ -1,50 +1,30 @@
 import recordEvent from 'platform/monitoring/record-event';
+import { kebabCase } from 'lodash';
 
-const analyticsEvents = [
-  {
-    componentName: 'Modal',
-    prefix: 'modal',
-    actions: [{ action: 'show', name: 'int-modal-click' }],
-  },
-  {
-    componentName: 'AdditionalInfo',
-    prefix: 'additional-info',
-    actions: [
-      { action: 'expand', name: 'int-additional-info-expand' },
-      { action: 'collapse', name: 'int-additional-info-collapse' },
-    ],
-  },
-];
+const analyticsEvents = {
+  Modal: [{ action: 'show', event: 'int-modal-click' }],
+};
 
 export default function subscribeComponentAnalyticsEvents() {
   document.body.addEventListener('component-library-analytics', e => {
     // Is it a component we are tracking?
-    const component = analyticsEvents.find(
-      c => c.componentName === e.detail.componentName,
-    );
+    const component = analyticsEvents[e.detail.componentName];
 
-    // Is it an action we are tracking?
-    if (component?.actions) {
-      const action = component.actions.find(
-        ev => ev.action === e.detail.action,
-      );
+    const action = component.find(ev => ev.action === e.detail.action);
 
-      if (action) {
-        const dataLayer = { event: action.name };
+    if (action) {
+      const dataLayer = { event: action.event };
 
-        // If the event included additional details / context...
-        if (e.detail.details) {
-          for (const key of Object.keys(e.detail.details)) {
-            const newKey = component.prefix
-              ? `${component.prefix}-${key}`
-              : key;
+      // If the event included additional details / context...
+      if (e.detail.details) {
+        for (const key of Object.keys(e.detail.details)) {
+          const newKey = `${kebabCase(e.detail.componentName)}-${key}`;
 
-            dataLayer[newKey] = e.detail.details[key];
-          }
+          dataLayer[newKey] = e.detail.details[key];
         }
-
-        recordEvent(dataLayer);
       }
+
+      recordEvent(dataLayer);
     }
   });
 }
