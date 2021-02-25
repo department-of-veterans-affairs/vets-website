88c71271887649b4695476d7367c9a5500179df0
Oksana Cyrwus <oksanacyrwus@gmail.com>
2021-02-25 09:31:38 -0700

    Add slack group back in.

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -171,7 +171,7 @@ def checkForBrokenLinks(String buildLogPath, String envName, Boolean contentOnly
       // Until slackUploadFile works...
       def brokenLinks = readFile(csvFile)
       def brokenLinksCount = sh(returnStdout: true, script: "wc -l /application/${csvFileName} | cut -d ' ' -f1") as Integer
-      def brokenLinksMessage = "${brokenLinksCount} broken links found in the `${envName}` build on `${env.BRANCH_NAME}`\n${env.RUN_DISPLAY_URL}\n${brokenLinks}".stripMargin()
+      def brokenLinksMessage = "${brokenLinksCount} broken links found in the `${envName}` build on `${env.BRANCH_NAME}`\n@cmshelpdesk\n${env.RUN_DISPLAY_URL}\n${brokenLinks}".stripMargin()
 
       slackSend(
         message: brokenLinksMessage,
