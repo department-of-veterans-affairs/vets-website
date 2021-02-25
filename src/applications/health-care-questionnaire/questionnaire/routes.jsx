25b4539a53de039801ae0054866850decf9419b7
Mark Dewey <mdewey@governmentcio.com>
2021-02-25 14:15:25 -0500

    added getting current questionnaire from session

--- src/applications/health-care-questionnaire/questionnaire/routes.jsx
+++ src/applications/health-care-questionnaire/questionnaire/routes.jsx
@@ -3,12 +3,15 @@ import formConfig from './config/form';
 import QuestionnaireWrapper from './containers/QuestionnaireWrapper.jsx';
 
 import {
-  getCurrentAppointmentId,
   addAppointmentIdToFormId,
+  getCurrentAppointmentId,
+  getCurrentQuestionnaire,
   onFormEnter,
 } from '../shared/utils';
 
 const id = getCurrentAppointmentId(window);
+// eslint-disable-next-line no-unused-vars
+const questionnaire = getCurrentQuestionnaire(window, id);
 formConfig.formId = addAppointmentIdToFormId(id, formConfig.formId);
 
 const route = {
