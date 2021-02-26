ea03a14ac439a462d836fee795bdf064cda6a4b8
Mark Dewey <mdewey@governmentcio.com>
2021-02-26 16:22:38 -0500

    moved list form into share api folder

--- src/applications/health-care-questionnaire/shared/api/index.js
+++ src/applications/health-care-questionnaire/shared/api/index.js
@@ -1,4 +1,5 @@
 import { submit, transformForSubmit } from './submit-questionnaire';
 import { loadAppointment } from './load-appointments';
+import { loadQuestionnaires } from './load-questionnaires';
 
-export { loadAppointment, submit, transformForSubmit };
+export { loadAppointment, submit, transformForSubmit, loadQuestionnaires };
