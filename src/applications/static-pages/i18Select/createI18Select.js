55c30b7e14e152503f1bd956dfaa85e8e028d3c3
Fareez Ahmed <fareezahmed@Fareez-GCIO.local>
2021-02-28 13:13:30 -0800

    comment

--- src/applications/static-pages/i18Select/createI18Select.js
+++ src/applications/static-pages/i18Select/createI18Select.js
@@ -7,16 +7,14 @@ export default function createI18Select(store, widgetType) {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   // we can get the current lang here and pass it as a prop
   if (root) {
-    import(/* webpackChunkName: "view-payment-history-cta" */ './I18Select').then(
-      module => {
-        const I18Select = module.default;
-        ReactDOM.render(
-          <Provider store={store}>
-            <I18Select />
-          </Provider>,
-          root,
-        );
-      },
-    );
+    import('./I18Select').then(module => {
+      const I18Select = module.default;
+      ReactDOM.render(
+        <Provider store={store}>
+          <I18Select />
+        </Provider>,
+        root,
+      );
+    });
   }
 }
