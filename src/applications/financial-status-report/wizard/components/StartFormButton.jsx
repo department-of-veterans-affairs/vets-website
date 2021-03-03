f97c5ddec0bd68861c9513ba92d75001e394eac5
Craig Wheeler <craig@craigwheeler.net>
2021-03-03 11:49:59 -0500

    cleanup

--- src/applications/financial-status-report/wizard/components/StartFormButton.jsx
+++ src/applications/financial-status-report/wizard/components/StartFormButton.jsx
@@ -9,10 +9,7 @@ const StartFormButton = ({ setWizardStatus, label, ariaId }) => {
       href={`${ROOT_URL}/introduction`}
       className="usa-button-primary va-button-primary"
       onClick={event => {
-        // Remove this check, keep the preventDefault once flipper is at 100%
-        if (window.location.pathname.includes(ROOT_URL)) {
-          event.preventDefault();
-        }
+        event.preventDefault();
         setWizardStatus(WIZARD_STATUS_COMPLETE);
       }}
       aria-describedby={ariaId}
