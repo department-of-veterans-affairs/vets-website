9141a98b44629f0be7be7258882bb7a7b9068dd2
Tim Wright <timwright12@gmail.com>
2021-03-03 17:54:25 -0500

    Fix/update prearchive for content only deploy (#16254)
    * rolled back content deploy update

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -202,13 +202,12 @@ def build(String ref, dockerContainer, String assetSource, String envName, Boole
   def drupalAddress = DRUPAL_ADDRESSES.get('vagovprod')
   def drupalCred = DRUPAL_CREDENTIALS.get('vagovprod')
   def drupalMode = useCache ? '' : '--pull-drupal'
-  def setPublicPath = contentOnlyBuild ? '--setPublicPath' : ''
 
   withCredentials([usernamePassword(credentialsId:  "${drupalCred}", usernameVariable: 'DRUPAL_USERNAME', passwordVariable: 'DRUPAL_PASSWORD')]) {
     dockerContainer.inside(DOCKER_ARGS) {
       def buildLogPath = "/application/${envName}-build.log"
 
-      sh "cd /application && jenkins/build.sh --envName ${envName} --assetSource ${assetSource} --drupalAddress ${drupalAddress} ${drupalMode} --buildLog ${buildLogPath} ${setPublicPath} --verbose"
+      sh "cd /application && jenkins/build.sh --envName ${envName} --assetSource ${assetSource} --drupalAddress ${drupalAddress} ${drupalMode} --buildLog ${buildLogPath} --verbose"
 
       if (envName == 'vagovprod') {
         // Find any broken links in the log
@@ -261,16 +260,14 @@ def buildAll(String ref, dockerContainer, Boolean contentOnlyBuild) {
   }
 }
 
-def prearchive(dockerContainer, envName, Boolean contentOnlyBuild) {
+def prearchive(dockerContainer, envName) {
   dockerContainer.inside(DOCKER_ARGS) {
-    if (!contentOnlyBuild) {
-      sh "cd /application && NODE_ENV=production yarn build --buildtype ${envName} --setPublicPath"
-    }
+    sh "cd /application && NODE_ENV=production yarn build --buildtype ${envName} --setPublicPath"
     sh "cd /application && node --max-old-space-size=10240 script/prearchive.js --buildtype=${envName}"
   }
 }
 
-def prearchiveAll(dockerContainer, Boolean contentOnlyBuild) {
+def prearchiveAll(dockerContainer) {
   stage("Prearchive Optimizations") {
     if (shouldBail()) { return }
 
@@ -281,7 +278,7 @@ def prearchiveAll(dockerContainer, Boolean contentOnlyBuild) {
         def envName = VAGOV_BUILDTYPES.get(i)
 
         builds[envName] = {
-          prearchive(dockerContainer, envName, contentOnlyBuild)
+          prearchive(dockerContainer, envName)
         }
       }
 
