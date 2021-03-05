3f30311917ae815eb3bc29cdb6d5d7bc553bf964
linuxlance <LanceGundersen@users.noreply.github.com>
2021-03-05 15:03:45 -0500

    Confirmation page updates
    - added constant for date string
    - added constant for card title and calendar title
    - added directions parmeter to facility address
    - added the add to calendar link
    - updated unit test to reflect changes

--- src/applications/vaos/project-cheetah/components/ConfirmationPage.jsx
+++ src/applications/vaos/project-cheetah/components/ConfirmationPage.jsx
@@ -9,6 +9,8 @@ import { getTimezoneAbbrBySystemId } from '../../utils/timezone.js';
 import { GA_PREFIX } from '../../utils/constants.js';
 import FacilityAddress from '../../components/FacilityAddress.jsx';
 import { selectConfirmationPage } from '../redux/selectors.js';
+import AddToCalendar from 'applications/vaos/components/AddToCalendar';
+import { formatFacilityAddress } from 'applications/vaos/services/location';
 
 const pageTitle = 'Your appointment has been scheduled';
 
@@ -22,33 +24,53 @@ function ConfirmationPage({ data, systemId, facilityDetails }) {
     return <Redirect to="/" />;
   }
 
-  const { date1 } = data;
+  const appointmentType = 'COVID-19 Vaccination';
+  const appointmentDateString =
+    moment(data.date1, 'YYYY-MM-DDTHH:mm:ssZ').format(
+      'dddd, MMMM D, YYYY [at] h:mm a ',
+    ) + getTimezoneAbbrBySystemId(systemId);
 
   return (
     <div>
       <h1>{pageTitle}</h1>
       <AlertBox status="success">
-        <strong>Your appointment is confirmed</strong>
-        <br />
-        Please see your appointment details below.
+        <strong>
+          Your appointment is confirmed. Please see your appointment details
+          below.
+        </strong>
       </AlertBox>
       <div className="vads-u-background-color--gray-lightest vads-u-padding--2 vads-u-margin-top--2">
-        <h2 className="vads-u-margin-y--0 vads-u-font-size--h3">
-          COVID-19 vaccine
+        <span className="vads-u-margin-y--0 vaos-u-text-transform--uppercase">
+          {appointmentType}
+        </span>
+        <br />
+        <h2 className="vads-u-font-size--h3 vads-u-margin-top--1 vads-u-margin-bottom--2">
+          {appointmentDateString}
         </h2>
-        First dose
-        <br />
-        {moment(date1, 'YYYY-MM-DDTHH:mm:ssZ').format(
-          'dddd, MMMM D, YYYY [at] h:mm a ',
-        ) + getTimezoneAbbrBySystemId(systemId)}
-        <br />
+        <strong>
+          <i className="fa fa-check-circle vads-u-color--green" />
+          <span className="vads-u-font-weight--bold vads-u-margin-left--1 vads-u-display--inline-block">
+            Confirmed
+          </span>
+        </strong>
         <br />
         {facilityDetails && (
-          <FacilityAddress
-            name={facilityDetails.name}
-            facility={facilityDetails}
-          />
+          <div className="vads-u-margin-top--2">
+            <FacilityAddress
+              name={facilityDetails.name}
+              facility={facilityDetails}
+              level={3}
+              showDirectionsLink
+            />
+          </div>
         )}
+        <div className="vads-u-margin-top--3 vaos-appts__block-label">
+          <AddToCalendar
+            summary={appointmentType.concat(' Appointment')}
+            location={formatFacilityAddress(facilityDetails)}
+            startDateTime={data.date1}
+          />
+        </div>
       </div>
       <div className="vads-u-margin-y--2">
         <Link
