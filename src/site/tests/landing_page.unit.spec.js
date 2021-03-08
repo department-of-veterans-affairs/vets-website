2b410b552d2473d0ce8f1ceb57616247a8c88d57
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-08 15:20:11 -0500

    write unit tests for temp_layouts/landing_page.drupal.liquid template

---
+++ src/site/tests/landing_page.unit.spec.js
@@ -0,0 +1,28 @@
+
+// import { getByText } from '@testing-library/dom';
+import { expect } from 'chai';
+import { parseFixture, renderHTML } from './support';
+
+describe('intro', () => {
+  describe('no fieldTitleIcon', () => {
+    const data = parseFixture('landing_page');
+
+    it('renders elements with expected values', async () => {
+      const container = await renderHTML(data);
+      expect(container.querySelector('h1').innerHTML).to.equal(data.title);
+      expect(
+        container.querySelector('p').innerHTML).to.equal(data.fieldIntroText);
+      expect(
+        container.querySelector('i.icon-large.white.hub-icon-foo')).to.equal(null);
+    });
+  });
+
+  describe('with fieldTitleIcon', () => {
+    const data = parseFixture('landing_page_with_icon');
+
+    it('renders fieldTitleIcon', async () => {
+      const container = await renderHTML(data);
+      expect(container.querySelector('i.icon-large.white.hub-icon-foo')).not.to.equal(null);
+    });
+  });
+});
