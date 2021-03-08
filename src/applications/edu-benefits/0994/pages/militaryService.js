5749639215a6f2333609b286bd4d5611b4073ff1
Devin McCurdy <mccurdy_devin@bah.com>
2021-03-08 13:35:31 -0500

    Update questions

--- src/applications/edu-benefits/0994/pages/militaryService.js
+++ src/applications/edu-benefits/0994/pages/militaryService.js
@@ -1,11 +1,17 @@
+import moment from 'moment';
 import fullSchema from 'vets-json-schema/dist/22-0994-schema.json';
 import {
-  activeDutyNotice,
   benefitNotice,
+  remainingDaysGreaterThan180Notice,
+  remainingDaysNotGreaterThan180Notice,
   selectedReserveNationalGuardExpectedDutyTitle,
 } from '../content/militaryService';
 
 const { activeDuty, activeDutyDuringVetTec } = fullSchema.properties;
+
+const daysRemaining = formData => {
+  return moment(formData.expectedReleaseDate).diff(moment(), 'days');
+};
 
 export const uiSchema = {
   activeDuty: {
@@ -14,15 +20,56 @@ export const uiSchema = {
     'ui:widget': 'yesNo',
   },
   'view:activeDutyNotice': {
-    'ui:description': activeDutyNotice,
     'ui:options': {
       expandUnder: 'activeDuty',
       expandUnderCondition: true,
     },
   },
+  expectedReleaseDate: {
+    'ui:title': 'Enter the date you expect to be released from active duty.',
+    'ui:widget': 'date',
+    'ui:required': formData => formData.activeDuty,
+    'ui:options': {
+      expandUnder: 'activeDuty',
+      expandUnderCondition: true,
+    },
+  },
+  'view:remainingDaysGreaterThan180Notice': {
+    'ui:title': '',
+    'ui:description': remainingDaysGreaterThan180Notice,
+    'ui:options': {
+      expandUnder: 'activeDuty',
+      expandUnderCondition: (value, formData) => {
+        return (
+          value === true &&
+          formData.expectedReleaseDate &&
+          daysRemaining(formData) > 180
+        );
+      },
+    },
+  },
+  'view:remainingDaysNotGreaterThan180Notice': {
+    'ui:title': '',
+    'ui:description': remainingDaysNotGreaterThan180Notice,
+    expandUnder: 'activeDuty',
+    'ui:options': {
+      expandUnder: 'activeDuty',
+      expandUnderCondition: (value, formData) => {
+        return (
+          value === true &&
+          formData.expectedReleaseDate &&
+          daysRemaining(formData) <= 180
+        );
+      },
+    },
+  },
   activeDutyDuringVetTec: {
     'ui:title': selectedReserveNationalGuardExpectedDutyTitle,
     'ui:widget': 'yesNo',
+    'ui:options': {
+      expandUnder: 'activeDuty',
+      expandUnderCondition: false,
+    },
   },
   'view:benefitNotice': {
     'ui:title': '',
@@ -35,11 +82,24 @@ export const schema = {
   required: ['activeDuty'],
   properties: {
     activeDuty,
+    activeDutyDuringVetTec,
+    expectedReleaseDate: {
+      pattern:
+        '^(\\d{4}|XXXX)-(0[1-9]|1[0-2]|XX)-(0[1-9]|[1-2][0-9]|3[0-1]|XX)$',
+      type: 'string',
+    },
     'view:activeDutyNotice': {
       type: 'object',
       properties: {},
     },
-    activeDutyDuringVetTec,
+    'view:remainingDaysGreaterThan180Notice': {
+      type: 'object',
+      properties: {},
+    },
+    'view:remainingDaysNotGreaterThan180Notice': {
+      type: 'object',
+      properties: {},
+    },
     'view:benefitNotice': {
       type: 'object',
       properties: {},
