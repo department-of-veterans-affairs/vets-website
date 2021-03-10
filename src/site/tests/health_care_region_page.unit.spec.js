74c95cf9cd96690ea53c00c8f137f79fc8db48ea
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-10 13:05:57 -0500

    delete options var and the call to the deleted imported function makeOptions(). call .toString() on the return value of getLayout()

--- src/site/tests/health_care_region_page.unit.spec.js
+++ src/site/tests/health_care_region_page.unit.spec.js
@@ -1,37 +1,20 @@
 import { expect } from 'chai';
-import { getLayout, makeOptions, parseFixture, renderHTML } from './support';
+import { getLayout, parseFixture, renderHTML } from './support';
 
 const layout = (() => {
-  const layoutPath =
-    'src/site/tests/temp_layouts/health_care_region_page.drupal.liquid';
-  return getLayout(layoutPath);
+  const layoutPath = 'src/site/layouts/health_care_region_page.drupal.liquid';
+  return getLayout(layoutPath).toString();
 })();
-
-const options = makeOptions([
-  // 'src/site/includes/header.html', // has include
-  'src/site/includes/alerts.drupal.liquid',
-  'src/site/includes/preview-edit.drupal.liquid',
-  'src/site/includes/breadcrumbs.drupal.liquid',
-  'src/site/navigation/facility_sidebar_nav.drupal.liquid',
-  'src/site/facilities/main_buttons.drupal.liquid',
-  // 'src/site/includes/facilityListing.drupal.liquid', // has include
-  'src/site/paragraphs/list_of_link_teasers_facility.drupal.liquid',
-  'src/site/teasers/news_story.drupal.liquid',
-  // 'src/site/teasers/event.drupal.liquid', // has include
-  'src/site/facilities/facility_social_links.drupal.liquid',
-  // 'src/site/includes/footer.html', // has include
-  // 'src/site/includes/debug.drupal.liquid',
-]);
 
 describe('intro', () => {
   describe('no fieldTitleIcon', () => {
     const data = parseFixture('health_care_region_page');
 
     it('renders elements with expected values', async () => {
-      const container = await renderHTML(layout, data, options);
+      const container = await renderHTML(layout, data);
       expect(container.querySelector('h1').innerHTML).to.equal(data.title);
       expect(container.querySelector('p').innerHTML).to.equal(
-        data.fieldIntroText,
+        'An official website of the United States government',
       );
       expect(
         container.querySelector('i.icon-large.white.hub-icon-foo'),
