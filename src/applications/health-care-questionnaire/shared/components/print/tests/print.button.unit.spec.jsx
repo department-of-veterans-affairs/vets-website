b995ddbdcea4cc69293954b47e3292dcb9b4aa79
Mark Dewey <mdewey@governmentcio.com>
2021-03-10 11:57:46 -0500

    added test for call to action

--- src/applications/health-care-questionnaire/shared/components/print/tests/print.button.unit.spec.jsx
+++ src/applications/health-care-questionnaire/shared/components/print/tests/print.button.unit.spec.jsx
@@ -22,4 +22,22 @@ describe('health care questionnaire list - shows view and print button', () => {
     expect(component.exists('[data-testid="service-down-message"]')).to.be.true;
     component.unmount();
   });
+
+  it('renders error message with call to action after click', () => {
+    const ErrorCallToAction = () => {
+      return <span className="test">testing </span>;
+    };
+    const component = mount(
+      <PrintButton ErrorCallToAction={ErrorCallToAction} />,
+    );
+    expect(component.exists('.va-button')).to.be.true;
+    component
+      .find('.va-button')
+      .at(0)
+      .simulate('click');
+    expect(component.exists('.va-button')).to.be.false;
+    expect(component.exists('.test')).to.be.true;
+    expect(component.exists('[data-testid="service-down-message"]')).to.be.true;
+    component.unmount();
+  });
 });
