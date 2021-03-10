800780da7b4f82ed3a82c8eed49e8e449945968b
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-10 08:23:19 -0500

    create getLayout() and makeOptions() functions

--- src/site/tests/support/index.js
+++ src/site/tests/support/index.js
@@ -6,15 +6,32 @@ import registerFilters from '../../filters/liquid.js';
 
 registerFilters();
 
+const getLayout = layoutPath => {
+  return readFileSync(
+    path.resolve(__dirname, '../../../../', layoutPath),
+    'utf8',
+  ).toString();
+};
+
+const makeOptions = layoutPaths => {
+  const files = {};
+
+  layoutPaths.forEach(layoutPath => {
+    files[layoutPath] = getLayout(layoutPath);
+  });
+
+  return { files: { ...files } };
+};
+
 const parseFixture = file => {
   const jsonPath = path.resolve(__dirname, `../fixtures/${file}.json`);
   const json = readFileSync(jsonPath, 'utf8');
   return JSON.parse(json);
 };
 
-const renderHTML = (layout, data) => {
+const renderHTML = (layout, data, options) => {
   const context = liquid.newContext({ locals: data });
-  const render = liquid.compile(layout);
+  const render = liquid.compile(layout, options);
 
   return new Promise((resolve, reject) =>
     render(context, err => {
@@ -29,4 +46,4 @@ const renderHTML = (layout, data) => {
   );
 };
 
-export { parseFixture, renderHTML };
+export { getLayout, makeOptions, parseFixture, renderHTML };
