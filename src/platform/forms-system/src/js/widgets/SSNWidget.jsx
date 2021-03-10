c21aa1aab963f2ec00e090459144de297b293360
Brooks Johnson <brooks.johnson@pm.me>
2021-03-10 11:07:55 -0800

    Convert to functional component
    I still need to make the onChange call part of the state callback

--- src/platform/forms-system/src/js/widgets/SSNWidget.jsx
+++ src/platform/forms-system/src/js/widgets/SSNWidget.jsx
@@ -1,33 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextWidget from './TextWidget';
 
 /*
  * Handles removing dashes from SSNs by keeping the user input in local state
  * and saving the transformed version instead
  */
-export default class SSNWidget extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { val: props.value };
-  }
-  handleChange = val => {
+export default function SSNWidget(props) {
+  const [val, setVal] = useState(props.value);
+
+  const handleChange = value => {
     // If val is blank or undefined, pass undefined to onChange
     let strippedSSN;
-    if (val) {
-      strippedSSN = val.replace(/[- ]/g, '');
+    if (value) {
+      strippedSSN = value.replace(/[- ]/g, '');
     }
 
-    this.setState({ val }, () => {
-      this.props.onChange(strippedSSN);
-    });
+    setVal(value);
+    props.onChange(strippedSSN);
   };
-  render() {
-    return (
-      <TextWidget
-        {...this.props}
-        value={this.state.val}
-        onChange={this.handleChange}
-      />
-    );
-  }
+
+  return <TextWidget {...props} value={val} onChange={handleChange} />;
 }
