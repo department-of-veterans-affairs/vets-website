59e89b366528eab81af464dac1405c2e932b3495
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2021-03-11 15:42:08 -0500

    Remove temp landing_page template, use real landing_page template

--- src/site/tests/landing_page.unit.spec.js
+++ src/site/tests/landing_page.unit.spec.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { getLayout, parseFixture, renderHTML } from './support';
 
-const layoutPath = 'src/site/tests/temp_layouts/landing_page.drupal.liquid';
+const layoutPath = 'src/site/layouts/landing_page.drupal.liquid';
 const layout = getLayout(layoutPath).toString();
 
 describe('intro', () => {
@@ -11,7 +11,7 @@ describe('intro', () => {
     it('renders elements with expected values', async () => {
       const container = await renderHTML(layoutPath, layout, data);
       expect(container.querySelector('h1').innerHTML).to.equal(data.title);
-      expect(container.querySelector('p').innerHTML).to.equal(
+      expect(container.querySelector('.va-introtext p').innerHTML).to.equal(
         data.fieldIntroText,
       );
       expect(
@@ -25,6 +25,10 @@ describe('intro', () => {
 
     it('renders fieldTitleIcon', async () => {
       const container = await renderHTML(layoutPath, layout, data);
+      expect(container.querySelector('h1').innerHTML).to.equal(data.title);
+      expect(container.querySelector('.va-introtext p').innerHTML).to.equal(
+        data.fieldIntroText,
+      );
       expect(
         container.querySelector('i.icon-large.white.hub-icon-foo'),
       ).not.to.equal(null);
