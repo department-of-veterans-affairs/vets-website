6f64b86bdf875d7994a504295a6baa2b7cf3d460
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-10 19:48:16 -0500

    refactor layout. pass layoutPath as arg to renderHTML()

--- src/site/tests/health_care_region_page.unit.spec.js
+++ src/site/tests/health_care_region_page.unit.spec.js
@@ -1,17 +1,15 @@
 import { expect } from 'chai';
 import { getLayout, parseFixture, renderHTML } from './support';
 
-const layout = (() => {
-  const layoutPath = 'src/site/layouts/health_care_region_page.drupal.liquid';
-  return getLayout(layoutPath).toString();
-})();
+const layoutPath = 'src/site/layouts/health_care_region_page.drupal.liquid';
+const layout = getLayout(layoutPath).toString();
 
 describe('intro', () => {
   describe('no fieldTitleIcon', () => {
     const data = parseFixture('health_care_region_page');
 
     it('renders elements with expected values', async () => {
-      const container = await renderHTML(layout, data);
+      const container = await renderHTML(layoutPath, layout, data);
       expect(container.querySelector('h1').innerHTML).to.equal(data.title);
       expect(container.querySelector('p').innerHTML).to.equal(
         'An official website of the United States government',
--- src/site/tests/landing_page.unit.spec.js
+++ src/site/tests/landing_page.unit.spec.js
@@ -1,17 +1,15 @@
 import { expect } from 'chai';
 import { getLayout, parseFixture, renderHTML } from './support';
 
-const layout = (() => {
-  const layoutPath = 'src/site/tests/temp_layouts/landing_page.drupal.liquid';
-  return getLayout(layoutPath).toString();
-})();
+const layoutPath = 'src/site/tests/temp_layouts/landing_page.drupal.liquid';
+const layout = getLayout(layoutPath).toString();
 
 describe('intro', () => {
   describe('no fieldTitleIcon', () => {
     const data = parseFixture('landing_page');
 
     it('renders elements with expected values', async () => {
-      const container = await renderHTML(layout, data);
+      const container = await renderHTML(layoutPath, layout, data);
       expect(container.querySelector('h1').innerHTML).to.equal(data.title);
       expect(container.querySelector('p').innerHTML).to.equal(
         data.fieldIntroText,
@@ -26,7 +24,7 @@ describe('intro', () => {
     const data = parseFixture('landing_page_with_icon');
 
     it('renders fieldTitleIcon', async () => {
-      const container = await renderHTML(layout, data);
+      const container = await renderHTML(layoutPath, layout, data);
       expect(
         container.querySelector('i.icon-large.white.hub-icon-foo'),
       ).not.to.equal(null);
