337d8c2891a3336fda61470843c729368b432fea
LinnJS <justinslinn727@gmail.com>
2021-03-11 15:30:31 -0500

    Refactor loading spinner to not use callBack

--- src/applications/caregivers/components/PreSubmitInfo/SubmitLoadingIndicator.jsx
+++ src/applications/caregivers/components/PreSubmitInfo/SubmitLoadingIndicator.jsx
@@ -1,29 +1,17 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import LoadingIndicator from '@department-of-veterans-affairs/component-library/LoadingIndicator';
 
 const SubmitLoadingIndicator = ({ submission }) => {
   const [isLoading, setLoading] = useState(false);
   const [timer, setTimer] = useState(0);
-  const [loadingMessage, setLoadingMessage] = useState(null);
+  const [loadingMessage, setLoadingMessage] = useState(
+    'Please wait while we process your application.',
+  );
   const hasAttemptedSubmit = submission.hasAttemptedSubmit;
   const isSubmitPending = submission.status === 'submitPending';
 
-  const getLoadingMessage = useCallback(
-    () => {
-      if (timer >= 0) {
-        setLoadingMessage('Please wait while we process your application.');
-      }
-
-      if (timer >= 15) {
-        setLoadingMessage(
-          'We’re processing your application. This may take up to 1 minute. Please don’t refresh your browser.',
-        );
-      }
-    },
-    [timer],
-  );
-
+  // set loading to true if user has attempted to submit and submit is pending
   useEffect(
     () => {
       if (hasAttemptedSubmit && isSubmitPending) {
@@ -33,6 +21,20 @@ const SubmitLoadingIndicator = ({ submission }) => {
     [hasAttemptedSubmit, isSubmitPending],
   );
 
+  // after 15 seconds set the longWaitMessage in spinner
+  useEffect(
+    () => {
+      const longWaitMessage =
+        'We’re processing your application. This may take up to 1 minute. Please don’t refresh your browser.';
+
+      if (timer >= 15) {
+        setLoadingMessage(longWaitMessage);
+      }
+    },
+    [timer],
+  );
+
+  // count the seconds so we know how long the user has waited
   useEffect(
     () => {
       let interval = null;
@@ -41,24 +43,21 @@ const SubmitLoadingIndicator = ({ submission }) => {
         interval = setInterval(() => {
           setTimer(seconds => seconds + 1);
         }, 1000);
-
-        getLoadingMessage();
       }
 
       return () => {
         clearInterval(interval);
       };
     },
-    [timer, isLoading, getLoadingMessage],
+    [timer, isLoading],
   );
 
   return (
     <>
       {isLoading && (
         <div className="loading-container">
-          <div aria-live="polite" className="vads-u-margin-y--4">
-            <LoadingIndicator />
-            <p>{loadingMessage}</p>
+          <div className="vads-u-margin-y--4">
+            <LoadingIndicator message={loadingMessage} />
           </div>
         </div>
       )}
