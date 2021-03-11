21323024ef4e6e4226faf8a7a6fc393385b10a32
Devin McCurdy <mccurdy_devin@bah.com>
2021-03-11 14:27:09 -0500

    0994 Service History Updates (#16297)

--- src/applications/edu-benefits/0994/tests/pages/militaryService.unit.spec.jsx
+++ src/applications/edu-benefits/0994/tests/pages/militaryService.unit.spec.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { mount } from 'enzyme';
@@ -7,14 +8,38 @@ import { DefinitionTester } from 'platform/testing/unit/schemaform-utils';
 import formConfig from '../../../0994/config/form';
 
 describe('VET TEC military service', () => {
-  const page = formConfig.chapters.militaryService.pages.militaryService;
-  const { schema, uiSchema } = page;
+  const {
+    schema,
+    uiSchema,
+  } = formConfig.chapters.militaryService.pages.militaryService;
+
+  const setDate = (form, momentObj) => {
+    const month = form.find(`select[name="root_expectedReleaseDateMonth"]`);
+    const day = form.find(`select[name="root_expectedReleaseDateDay"]`);
+    const year = form.find(`input[name="root_expectedReleaseDateYear"]`);
+
+    month.simulate('change', {
+      target: { value: (momentObj.month() + 1).toString() },
+    });
+
+    day.simulate('change', {
+      target: { value: momentObj.date().toString() },
+    });
+
+    year.simulate('change', {
+      target: { value: momentObj.year().toString() },
+    });
+  };
 
   it('should render', () => {
     const form = mount(
-      <DefinitionTester schema={schema} uiSchema={uiSchema} />,
+      <DefinitionTester
+        schema={schema}
+        uiSchema={uiSchema}
+        definitions={formConfig.defaultDefinitions}
+      />,
     );
-    expect(form.find('input').length).to.equal(4);
+    expect(form.find('input').length).to.equal(2);
     form.unmount();
   });
 
@@ -24,6 +49,7 @@ describe('VET TEC military service', () => {
       <DefinitionTester
         schema={schema}
         uiSchema={uiSchema}
+        definitions={formConfig.defaultDefinitions}
         onSubmit={onSubmit}
       />,
     );
@@ -40,6 +66,7 @@ describe('VET TEC military service', () => {
       <DefinitionTester
         schema={schema}
         uiSchema={uiSchema}
+        definitions={formConfig.defaultDefinitions}
         onSubmit={onSubmit}
       />,
     );
@@ -47,6 +74,9 @@ describe('VET TEC military service', () => {
     form.find(`input[name="root_activeDuty"][value="Y"]`).simulate('change', {
       target: { Y: 'Y' },
     });
+
+    setDate(form, moment().add(2, 'days'));
+
     form.find('form').simulate('submit');
     expect(form.find(ERR_MSG_CSS_CLASS).length).to.equal(0);
     expect(onSubmit.called).to.be.true;
