f331ab35097a49f6c1d620af2477306243c1ec67
Craig Wheeler <craig@craigwheeler.net>
2021-03-11 11:43:21 -0500

    total monthly income

--- src/applications/financial-status-report/components/FinancialOverview.jsx
+++ src/applications/financial-status-report/components/FinancialOverview.jsx
@@ -1,7 +1,64 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const FinancialOverview = ({ formData: { selectedDebts } }) => {
+const getMonthlyIncome = ({
+  questions,
+  personalData,
+  additionalIncome,
+  socialSecurity,
+  benefits,
+}) => {
+  const { employmentHistory } = personalData;
+
+  let totalArr = [];
+
+  if (questions.vetIsEmployed) {
+    const { grossMonthlyIncome } = employmentHistory.veteran.currentEmployment;
+    totalArr = [...totalArr, grossMonthlyIncome];
+  }
+
+  if (questions.hasAdditionalIncome) {
+    const vetAddl = additionalIncome.additionalIncomeRecords.map(
+      record => record.monthlyIncome,
+    );
+    totalArr = [...totalArr, ...vetAddl];
+  }
+
+  if (questions.spouseIsEmployed) {
+    const { grossMonthlyIncome } = employmentHistory.spouse.currentEmployment;
+    totalArr = [...totalArr, grossMonthlyIncome];
+  }
+
+  if (questions.spouseHasAdditionalIncome) {
+    const spouseAddl = additionalIncome.spouse.additionalIncomeRecords.map(
+      record => record.monthlyIncome,
+    );
+    totalArr = [...totalArr, ...spouseAddl];
+  }
+
+  if (questions.spouseHasBenefits) {
+    const { benefitAmount, educationAmount } = benefits.spouseBenefits;
+    totalArr = [...totalArr, benefitAmount, educationAmount];
+  }
+
+  if (questions.hasSocialSecurity) {
+    const { socialSecurityAmount } = socialSecurity;
+    totalArr = [...totalArr, socialSecurityAmount];
+  }
+
+  if (questions.spouseHasSocialSecurity) {
+    const { socialSecurityAmount } = socialSecurity.spouse;
+    totalArr = [...totalArr, socialSecurityAmount];
+  }
+
+  return totalArr.reduce((acc, income) => acc + income, 0);
+};
+
+const FinancialOverview = ({ formData }) => {
+  const { selectedDebts } = formData;
+
+  const monthlyIncome = getMonthlyIncome(formData);
+
   const index = window.location.href.slice(-1);
   const debt = selectedDebts[index];
   const formatter = new Intl.NumberFormat('en-US', {
@@ -17,7 +74,7 @@ const FinancialOverview = ({ formData: { selectedDebts } }) => {
         <div className="vads-u-margin-bottom--1 overview-container">
           <div>Total monthly income:</div>
           <div>
-            {debt.currentAr && formatter.format(parseFloat(debt.currentAr))}
+            {debt.currentAr && formatter.format(parseFloat(monthlyIncome))}
           </div>
         </div>
         <div className="vads-u-margin-bottom--1 overview-container">
@@ -27,7 +84,7 @@ const FinancialOverview = ({ formData: { selectedDebts } }) => {
           </div>
         </div>
         <div className="vads-u-margin-bottom--0 overview-container">
-          <div>Monthly discretionary income:</div>
+          <div>Income after taxes and expenses:</div>
           <div>
             {debt.currentAr && formatter.format(parseFloat(debt.currentAr))}
           </div>
