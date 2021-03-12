051113334dfaff59335cb68f6be55cd336d1d2af
Jacob Gacek <jacob.gacek@thoughtworks.com>
2021-03-12 14:04:21 -0600

    [department-of-veterans-affairs/va-virtual-agent#59] Adds new page for chatbot
    Co-authored-by: Shefali Nayak <shefali.nayak@thoughtworks.com>

---
+++ src/applications/virtual-agent/containers/App.jsx
@@ -0,0 +1,54 @@
+import React, { useMemo, useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { apiRequest } from 'platform/utilities/api';
+
+window.React = React;
+window.ReactDOM = ReactDOM;
+
+export default function App() {
+  const [isLoaded, setLoaded] = useState(!!window.WebChat);
+
+  if (!isLoaded) {
+    setTimeout(() => {
+      if (window.WebChat) {
+        setLoaded(true);
+      }
+    }, 300);
+    return 'waiting on webchat framework . . .';
+  }
+
+  return <ActualApp />;
+}
+
+function ActualApp() {
+  const { ReactWebChat, createDirectLine, createStore } = window.WebChat;
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    async function getToken() {
+      const res = await apiRequest('/virtual_agent_tokens', {
+        method: 'POST',
+      });
+      setToken(res.token);
+    }
+    getToken();
+  }, []);
+
+  const store = useMemo(() => createStore(), []);
+
+  const directLine = useMemo(
+    () =>
+      createDirectLine({
+        token,
+      }),
+    [token],
+  );
+
+  return (
+    <div className={'vads-l-grid-container'}>
+      <div className={'vads-l-row'} style={{ height: '200px' }}>
+        <ReactWebChat directLine={directLine} store={store} userID="12345" />
+      </div>
+    </div>
+  );
+}
