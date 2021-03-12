0556341ab8da759b8be0799b72c53d0b4b41442c
Katryn Bowman <katy.bowman@adhocteam.us>
2021-03-12 16:38:59 -0500

    add tests for SSNWidget masking

--- src/platform/forms-system/test/js/widgets/SSNWidget.unit.spec.jsx
+++ src/platform/forms-system/test/js/widgets/SSNWidget.unit.spec.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { expect } from 'chai';
 import SkinDeep from 'skin-deep';
+import { render, fireEvent } from '@testing-library/react';
 import sinon from 'sinon';
 
 import SSNWidget from '../../../src/js/widgets/SSNWidget';
+
+const props = {
+  schema: {
+    type: 'text',
+  },
+};
 
 describe('Schemaform <SSNWidget>', () => {
   it('should render', () => {
@@ -34,5 +41,50 @@ describe('Schemaform <SSNWidget>', () => {
     );
     tree.subTree('TextWidget').props.onChange('432549877');
     expect(onChange.calledWith('432549877')).to.be.true;
+  });
+
+  it('should mask all but the last four digits of the SSN onBlur and display with dashes when SSN is entered as all one digit', () => {
+    const { container } = render(<SSNWidget value="456431098" {...props} />);
+    const input = container.querySelector('input');
+    fireEvent.blur(input);
+    expect(input.value).to.equal('●●●-●●-1098');
+  });
+
+  it('should mask all but the last four digits of the SSN onBlur and display with dashes when SSN is entered with dashes', () => {
+    const { container } = render(<SSNWidget value="456-43-1098" {...props} />);
+    const input = container.querySelector('input');
+    fireEvent.blur(input);
+    expect(input.value).to.equal('●●●-●●-1098');
+  });
+
+  it('should mask all but the last four digits of the SSN onBlur and display with dashes when SSN is entered with spaces', () => {
+    const { container } = render(<SSNWidget value="456 43 1098" {...props} />);
+    const input = container.querySelector('input');
+    fireEvent.blur(input);
+    expect(input.value).to.equal('●●●-●●-1098');
+  });
+
+  it('should not mask the SSN onFocus', () => {
+    const { container } = render(<SSNWidget value="456431098" {...props} />);
+    const input = container.querySelector('input');
+    fireEvent.blur(input);
+    fireEvent.focus(input);
+    expect(input.value).to.equal('456431098');
+  });
+
+  it('should display the SSN with dashes when SSN is entered with dashes', () => {
+    const { container } = render(<SSNWidget value="456-43-1098" {...props} />);
+    const input = container.querySelector('input');
+    fireEvent.blur(input);
+    fireEvent.focus(input);
+    expect(input.value).to.equal('456-43-1098');
+  });
+
+  it('should display the SSN with spaces when the SSN is entered with spaces', () => {
+    const { container } = render(<SSNWidget value="456 43 1098" {...props} />);
+    const input = container.querySelector('input');
+    fireEvent.blur(input);
+    fireEvent.focus(input);
+    expect(input.value).to.equal('456 43 1098');
   });
 });
