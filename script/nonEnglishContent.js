fafc29e66ef0c80fa7904cf9647f5980783194be
Fareez Ahmed <fareezahmed@Fareez-GCIO.local>
2021-03-12 14:15:21 -0800

    finish script

--- script/nonEnglishContent.js
+++ script/nonEnglishContent.js
@@ -6,8 +6,13 @@ const SITEMAP_URL = `http://${process.env.WEB_HOST || 'localhost'}:${process.env
   .WEB_PORT || 3001}/sitemap.xml`;
 const SITEMAP_LOC_NS = 'http://www.sitemaps.org/schemas/sitemap/0.9';
 
-const langs = ['-esp', 'espanol', '-tag'];
-
+const langs = ['espanol'];
+const langSuffixes = ['-esp/', '-tag/'];
+// with the new language suffixes
+const filterByLanguageSuffix = url => {
+  return langSuffixes.some(substring => url.endsWith(substring));
+};
+// with the old implementation, full word `espanol` in url
 const filterByLanguage = url => {
   return langs.some(substring => url.includes(substring));
 };
@@ -21,12 +26,18 @@ const parseNonEnglishContent = () => {
       return libxmljs2.parseXmlString(body);
     })
 
-    .then(doc =>
-      doc
-        .find('//xmlns:loc', SITEMAP_LOC_NS)
-        .map(n => n.text())
-        .filter(filterByLanguage),
-    )
+    .then(doc => {
+      return [
+        ...doc
+          .find('//xmlns:loc', SITEMAP_LOC_NS)
+          .map(n => n.text())
+          .filter(filterByLanguage),
+        ...doc
+          .find('//xmlns:loc', SITEMAP_LOC_NS)
+          .map(n => n.text())
+          .filter(filterByLanguageSuffix),
+      ];
+    })
     .then(urls => {
       console.log(urls, 'THE NON ENGLISH CONTENT');
       console.log(urls.length, 'THE NUMBER OF PAGES');
