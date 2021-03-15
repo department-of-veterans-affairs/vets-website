480375bcd1b109d6d261fec05bdb56cfc9ccb227
Sandra Hallie <sandra.adamshallie@gmail.com>
2021-03-15 09:28:22 -0600

    Add unit test

--- src/applications/personalization/dashboard-2/tests/unit/healthcare.unit.spec.jsx
+++ src/applications/personalization/dashboard-2/tests/unit/healthcare.unit.spec.jsx
@@ -5,15 +5,32 @@ import { expect } from 'chai';
 import { HealthCare } from '~/applications/personalization/dashboard-2/components/health-care/HealthCare';
 
 describe('HealthCare', () => {
-  describe('Manage all your prescriptions', () => {
-    const defaultProps = {
-      appointments: [],
-      authenticatedWithSSOe: true,
-      canAccessRx: false,
-      fetchConfirmedFutureAppointments: () => {},
-    };
+  const defaultProps = {
+    appointments: [],
+    authenticatedWithSSOe: true,
+    canAccessRx: false,
+    canAccessMessaging: true,
+    fetchFolder: () => {},
+    fetchConfirmedFutureAppointments: () => {},
+  };
 
+  describe('Prescriptions', () => {
     it('when enrolled in VA health care, should not render when they have never had any prescriptions managed by the VA', () => {
+      const wrapper = mount(<HealthCare {...defaultProps} />);
+      expect(wrapper.find('Prescriptions').exists()).to.be.false;
+      wrapper.unmount();
+    });
+  });
+
+  describe('Messaging', () => {
+    it('should not render when not eligible for messaging', () => {
+      defaultProps.canAccessMessaging = false;
+      const wrapper = mount(<HealthCare {...defaultProps} />);
+      expect(wrapper.find('Prescriptions').exists()).to.be.false;
+      wrapper.unmount();
+    });
+
+    it('should render when eligible for messaging', () => {
       const wrapper = mount(<HealthCare {...defaultProps} />);
       expect(wrapper.find('Prescriptions').exists()).to.be.false;
       wrapper.unmount();
