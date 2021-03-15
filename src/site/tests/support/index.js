b23e6c06899d97aae2f811e9d01e966a1067061c
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-15 09:30:05 -0400

    update comment

--- src/site/tests/support/index.js
+++ src/site/tests/support/index.js
@@ -43,7 +43,7 @@ const updateHTML = files => {
     buildtype: BUILDTYPE,
     entry: true,
     liquidUnitTestingFramework: true,
-    // the following options are expected but the values don't matter
+    // the following options are needed but the values don't matter
     host: 'host',
     port: 3001,
     protocol: 'http',
