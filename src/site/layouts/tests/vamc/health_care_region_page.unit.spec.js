03d4b44c397e15a68a76650d9efee165d776538b
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-16 10:26:31 -0400

    grab html in before block so it can be accessed in multiple examples

--- src/site/layouts/tests/vamc/health_care_region_page.unit.spec.js
+++ src/site/layouts/tests/vamc/health_care_region_page.unit.spec.js
@@ -5,12 +5,16 @@ const layoutPath = 'src/site/layouts/health_care_region_page.drupal.liquid';
 
 describe('intro', () => {
   describe('no fieldTitleIcon', () => {
+    let container;
     const data = parseFixture(
       'src/site/layouts/tests/vamc/fixtures/health_care_region_page.json',
     );
 
+    before(async () => {
+      container = await renderHTML(layoutPath, data);
+    });
+
     it('renders elements with expected values', async () => {
-      const container = await renderHTML(layoutPath, data);
       expect(container.querySelector('h1').innerHTML).to.equal(data.title);
       expect(container.querySelector('p').innerHTML).to.equal(
         'An official website of the United States government',
