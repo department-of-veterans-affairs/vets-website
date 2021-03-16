fbf20997dd982211f4ee56e7c9f1d1c2e322e454
Mark Dewey <mdewey@governmentcio.com>
2021-03-16 13:19:59 -0400

    turned on mock data

--- src/applications/health-care-questionnaire/shared/api/load-questionnaires.js
+++ src/applications/health-care-questionnaire/shared/api/load-questionnaires.js
@@ -1,18 +1,16 @@
 import { apiRequest } from 'platform/utilities/api';
 import environment from 'platform/utilities/environment';
+import { json } from './mock-data/fhir/full.example.data';
 
-const USE_MOCK_DATA = window.Cypress; // || environment.isLocalhost || environment.isStaging;
+const USE_MOCK_DATA =
+  window.Cypress || environment.isLocalhost || environment.isStaging;
 
 const loadQuestionnaires = async () => {
   let promise;
   if (USE_MOCK_DATA) {
     promise = new Promise(resolve => {
       setTimeout(() => {
-        import(/* webpackChunkName: "my-questionnaires-sample-json" */ './mock-data/fhir/data.json').then(
-          module => {
-            resolve(module.default);
-          },
-        );
+        resolve(json);
       }, 1000);
     });
   } else {
