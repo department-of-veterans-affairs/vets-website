7cbcf2fa3ea72363c0666ac03a79d2a9502b5957
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-16 17:23:41 -0400

    destructure violations from results. copy relevent code from the Cypress axeCheck() custom command

--- src/site/tests/support/axe.js
+++ src/site/tests/support/axe.js
@@ -14,17 +14,47 @@ const logViolations = violations => {
   });
 };
 
-export default container => {
+export default (container, options = {}) => {
+  const { _13647Exception } = options;
+
+  /**
+   * Default required ruleset to meet Section 508 compliance.
+   * Do not remove values[] entries. Only add new rulesets like 'best-practices'.
+   *
+   * See https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#axe-core-tags
+   * for available rulesets.
+   */
+  let axeBuilder = {
+    runOnly: {
+      type: 'tag',
+      values: ['section508', 'wcag2a', 'wcag2aa'],
+    },
+    rules: {
+      'color-contrast': {
+        enabled: false,
+      },
+    },
+  };
+
+  /**
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+   */
+  axeBuilder = Object.assign(axeBuilder, options);
+
+  const axeConfig = _13647Exception
+    ? { includedImpacts: ['critical'] }
+    : axeBuilder;
+
   return new Promise((resolve, reject) =>
-    run(container, (error, response) => {
+    run(container, axeConfig, (error, { violations }) => {
       if (error) {
         reject(error);
       } else {
-        if (response.violations) {
-          logViolations(response.violations);
+        if (violations.length) {
+          logViolations(violations);
         }
 
-        resolve(response);
+        resolve(violations);
       }
     }),
   );
