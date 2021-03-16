b0ea419e80502f7de6ad9cb4613c281d19502d3e
Mark Dewey <mdewey@governmentcio.com>
2021-03-16 13:32:05 -0400

    updated name

--- src/applications/health-care-questionnaire/shared/test-data/questionnaire-response/tests/questionnaire.response.factory.unit.spec.js
+++ src/applications/health-care-questionnaire/shared/test-data/questionnaire-response/tests/questionnaire.response.factory.unit.spec.js
@@ -4,9 +4,9 @@ import { QuestionnaireResponseData } from '../factory';
 
 describe('health care questionnaire -- utils -- test data -- data factory -- questionnaire response -- ', () => {
   it('questionnaire response is created with status', () => {
-    const appointment = new QuestionnaireResponseData().withStatus('testing');
+    const qr = new QuestionnaireResponseData().withStatus('testing');
 
-    expect(appointment).to.have.property('status');
-    expect(appointment.status).to.equal('testing');
+    expect(qr).to.have.property('status');
+    expect(qr.status).to.equal('testing');
   });
 });
