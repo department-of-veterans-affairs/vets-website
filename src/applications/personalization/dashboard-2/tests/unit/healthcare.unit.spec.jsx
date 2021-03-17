2575bdfe8c78ad0060db0795eb0381fe4833a589
Sandra Hallie <sandra.adamshallie@gmail.com>
2021-03-17 13:58:06 -0600

    Fix linting

--- src/applications/personalization/dashboard-2/tests/unit/healthcare.unit.spec.jsx
+++ src/applications/personalization/dashboard-2/tests/unit/healthcare.unit.spec.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { expect } from 'chai';
-import { render } from '@testing-library/react';
 import { mockFetch, resetFetch } from '~/platform/testing/unit/helpers';
 import { renderInReduxProvider } from '~/platform/testing/unit/react-testing-library-helpers';
 import reducers from '~/applications/personalization/dashboard/reducers';
