076994cadb53dbf3d734ffd29c7615f7982a5176
Zachary Morel <zacharyjmorel@gmail.com>
2021-03-17 12:02:34 -0400

    DT-20804 Updated Form Label to be more descriptive  (#16395)
    * updated field on form
    
    * fixed sentence to be sentence case

--- src/applications/yellow-ribbon/containers/SearchForm/index.jsx
+++ src/applications/yellow-ribbon/containers/SearchForm/index.jsx
@@ -157,7 +157,7 @@ export class SearchForm extends Component {
           htmlFor="yr-search-name"
           className="vads-u-margin-top--1 vads-u-margin--0"
         >
-          School name
+          Enter full school name
         </label>
         <div className="vads-u-flex--1">
           <input
