de8053303af0c5a3fc7a40afabe89060c7dda202
Micah Chiang <micahkchiang@gmail.com>
2021-03-17 14:07:45 -0400

    add adapter for remaining workflows

--- src/applications/personalization/view-dependents/manage-dependents/utils/index.js
+++ src/applications/personalization/view-dependents/manage-dependents/utils/index.js
@@ -33,6 +33,9 @@ const WIZARD_OPTIONS_KEYS = {
   ANNULMENT: 'reportDivorce',
   VOID: 'reportDivorce',
   DEATH: 'reportDeath',
+  CHILD_MARRIED: 'reportMarriageOfChildUnder18',
+  CHILD_LEFT_SCHOOL: 'reportChild18OrOlderIsNotAttendingSchool',
+  STEPCHILD_LEFT_HOUSEHOLD: 'reportStepchildNotInHousehold',
 };
 
 // maps to values in the 686 report divorce reason
@@ -41,6 +44,12 @@ const DIVORCE_REASONS = {
   DIVORCE: 'Divorce',
   ANNULMENT: 'Other',
   VOID: 'Other',
+};
+
+const DEPENDENT_TYPES = {
+  SPOUSE: 'SPOUSE',
+  CHILD: 'CHILD',
+  DEPENDENT_PARENT: 'DEPENDENT_PARENT',
 };
 
 // mirror the date format forms submit with
@@ -54,10 +63,10 @@ const transformForSubmit = formData => {
   const {
     fullName: { firstName: first, lastName: last },
     date,
-    dateOfBirth,
     ssn,
     location,
   } = formData;
+  const birthDate = formatDateString(formData.dateOfBirth);
   // this goes in all iterations of the payload.
   const universalData = {
     fullName: {
@@ -77,11 +86,78 @@ const transformForSubmit = formData => {
         reasonMarriageEnded: DIVORCE_REASONS[formData.reasonMarriageEnded],
         location,
         ssn,
-        birthDate: formatDateString(dateOfBirth),
+        birthDate,
       };
+    } else if (payload['view:selectable686Options'].reportDeath) {
+      payload.deaths = [
+        {
+          ...universalData,
+          location,
+          ssn,
+          birthDate,
+          dependentType: DEPENDENT_TYPES.SPOUSE,
+        },
+      ];
     }
   }
 
+  // if formData includes reasonForRemoval, user is reporting the removal of a child.
+  if (formData.reasonForRemoval) {
+    payload['view:selectable686Options'] = {
+      [formData.reasonForRemoval]: true,
+    };
+    if (formData.reasonForRemoval === WIZARD_OPTIONS_KEYS.CHILD_MARRIED) {
+      const { fullName, date: dateMarried } = universalData;
+      payload.childMarriage = {
+        fullName,
+        dateMarried,
+        birthDate,
+        ssn,
+      };
+    } else if (
+      formData.reasonForRemoval === WIZARD_OPTIONS_KEYS.CHILD_LEFT_SCHOOL
+    ) {
+      const { fullName, date: dateChildLeftSchool } = universalData;
+      payload.childStoppedAttendingSchool = {
+        fullName,
+        dateChildLeftSchool,
+        birthDate,
+        ssn,
+      };
+    } else if (formData.reasonForRemoval === WIZARD_OPTIONS_KEYS.DEATH) {
+      const { childStatus } = formData;
+      payload.deaths = [
+        {
+          ...universalData,
+          location,
+          ssn,
+          birthDate,
+          dependentType: DEPENDENT_TYPES.CHILD,
+          childStatus,
+        },
+      ];
+    } else if (
+      formData.reasonForRemoval === WIZARD_OPTIONS_KEYS.STEPCHILD_LEFT_HOUSEHOLD
+    ) {
+      const {
+        livingExpensesPaid,
+        whoDoesTheStepchildLiveWith,
+        address,
+      } = formData;
+      const supportingStepchild = livingExpensesPaid !== 'None';
+      payload.stepChildren = [
+        {
+          ...universalData,
+          ssn,
+          birthDate,
+          supportingStepchild,
+          livingExpensesPaid,
+          address,
+          whoDoesTheStepchildLiveWith,
+        },
+      ];
+    }
+  }
   return payload;
 };
 
