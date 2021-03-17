345035b4dbdc42d306a4a20dc682c3a993b674a4
Kelson Adams <kadams@governmentcio.com>
2021-03-17 08:49:09 -0600

    Move staging ordering logic to default ordering logic

--- src/applications/resources-and-support/hooks/useGetSearchResults.js
+++ src/applications/resources-and-support/hooks/useGetSearchResults.js
@@ -46,43 +46,40 @@ export default function useGetSearchResults(articles, query, page) {
 
       let orderedResults = [];
 
-      // Keep legacy sorting on production.
-      if (environment.isProduction()) {
-        orderedResults = orderBy(filteredArticles, 'title');
+      // Begin ordering logic.
+      // =====
+      filteredArticles = filteredArticles?.map(article => ({
+        ...article,
 
-        // Experiment with new sorting on non-prod envs.
-      } else {
-        filteredArticles = filteredArticles?.map(article => ({
-          ...article,
+        // Number of times a keyword is found in the article's title.
+        keywordsCountsTitle: keywords?.reduce(
+          (keywordInstances, keyword) =>
+            keywordInstances +
+            article.title.toLowerCase()?.split(keyword)?.length -
+            1,
+          0,
+        ),
 
-          // Number of times a keyword is found in the article's title.
-          keywordsCountsTitle: keywords?.reduce(
-            (keywordInstances, keyword) =>
-              keywordInstances +
-              article.title.toLowerCase()?.split(keyword)?.length -
-              1,
-            0,
-          ),
+        // Number of times a keyword is found in the article's description.
+        keywordsCountsDescription: keywords?.reduce(
+          (keywordInstances, keyword) =>
+            keywordInstances +
+            article.description.toLowerCase()?.split(keyword)?.length -
+            1,
+          0,
+        ),
+      }));
 
-          // Number of times a keyword is found in the article's description.
-          keywordsCountsDescription: keywords?.reduce(
-            (keywordInstances, keyword) =>
-              keywordInstances +
-              article.description.toLowerCase()?.split(keyword)?.length -
-              1,
-            0,
-          ),
-        }));
-
-        // Sort first by query word instances found in title descending
-        // Sort ties then by query word instances found in description descending
-        // Sort ties then by alphabetical descending
-        orderedResults = orderBy(
-          filteredArticles,
-          ['keywordsCountsTitle', 'keywordsCountsDescription', 'title'],
-          ['desc', 'desc', 'asc'],
-        );
-      }
+      // Sort first by query word instances found in title descending
+      // Sort ties then by query word instances found in description descending
+      // Sort ties then by alphabetical descending
+      orderedResults = orderBy(
+        filteredArticles,
+        ['keywordsCountsTitle', 'keywordsCountsDescription', 'title'],
+        ['desc', 'desc', 'asc'],
+      );
+      // =====
+      // End of ordering logic.
 
       // Track R&S search results.
       recordEvent({
