51a9ef2ee9a3132eea6e7bc662bbc0fbd417ea9f
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-18 17:38:07 -0400

    create smoke test for axe-checks

---
+++ src/site/layouts/tests/axe_smoke_test_with_errors_page/axe_smoke_test_with_errors_page.unit.spec.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { parseFixture, renderHTML } from '~/site/tests/support';
+import axeCheck from '~/site/tests/support/axe';
+
+const layoutPath = 'src/site/layouts/axe_smoke_test_with_errors.drupal.liquid';
+
+describe('intro', () => {
+  describe('no fieldTitleIcon', () => {
+    // let container;
+    const data = parseFixture(
+      'src/site/layouts/tests/axe_smoke_test_with_errors_page/fixtures/axe_smoke_test_with_errors_page.json',
+    );
+
+    // before(async () => {
+    //   container = await renderHTML(layoutPath, data);
+    // });
+
+    it('reports no axe violations', async () => {
+      const container = await renderHTML(layoutPath, data);
+      const violations = await axeCheck(container);
+      expect(violations.length).to.equal(0);
+    });
+  });
+});
