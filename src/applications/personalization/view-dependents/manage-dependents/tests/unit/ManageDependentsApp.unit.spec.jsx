ea8c7a24c461e33080b8d41697cd64efe1574f14
Micah Chiang <micahkchiang@gmail.com>
2021-03-18 17:07:59 -0400

    Initial commit

---
+++ src/applications/personalization/view-dependents/manage-dependents/tests/unit/ManageDependentsApp.unit.spec.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { expect } from 'chai';
+// import userEvent from '@testing-library/user-event';
+import { renderInReduxProvider } from 'platform/testing/unit/react-testing-library-helpers';
+import { removeDependents } from '../../redux/reducers';
+import ManageDependentsApp from '../../containers/ManageDependentsApp';
+import { SCHEMAS } from '../../schemas';
+
+describe('<ManageDependentsApp />', () => {
+  const mockData = {
+    firstName: 'Cindy',
+    lastName: 'See',
+    ssn: '312-243-5634',
+    dateOfBirth: '05-05-1953',
+    relationship: 'Spouse',
+  };
+  it('renders a form', async () => {
+    const initialState = {
+      removeDependents: {
+        dependentsState: {
+          1: {
+            uiSchema: SCHEMAS.Spouse.uiSchema,
+            formSchema: SCHEMAS.Spouse.schema,
+            formData: {},
+          },
+        },
+      },
+    };
+
+    const screen = renderInReduxProvider(
+      <ManageDependentsApp
+        relationship="Spouse"
+        closeFormHandler={() => {}}
+        stateKey={1}
+        userInfo={{
+          fullName: {
+            firstName: mockData.firstName,
+            lastName: mockData.lastName,
+            dateOfBirth: mockData.dateOfBirth,
+            ssn: mockData.ssn,
+          },
+        }}
+      />,
+      {
+        initialState,
+        reducers: removeDependents,
+      },
+    );
+    expect(await screen.findByText('Remove dependent', { selector: 'button' }))
+      .to.exist;
+  });
+});
