b557164ac580193834394d5d3ffffd0b0259eadf
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-18 21:19:30 -0400

    update expected value in assertion. update describe and example descriptions.

--- src/site/layouts/tests/axe_smoke_test_with_errors_page/axe_smoke_test_with_errors_page.unit.spec.js
+++ src/site/layouts/tests/axe_smoke_test_with_errors_page/axe_smoke_test_with_errors_page.unit.spec.js
@@ -4,14 +4,14 @@ import axeCheck from '~/site/tests/support/axe';
 
 const layoutPath = 'src/site/layouts/axe_smoke_test_with_errors.drupal.liquid';
 
-describe('axe check smoke test', () => {
+describe('liquid template axe check smoke test', () => {
   const data = parseFixture(
     'src/site/layouts/tests/axe_smoke_test_with_errors_page/fixtures/axe_smoke_test_with_errors_page.json',
   );
 
-  it('reports no axe violations', async () => {
+  it('reports the expected number of axe violations', async () => {
     const container = await renderHTML(layoutPath, data);
     const violations = await axeCheck(container);
-    expect(violations.length).to.equal(0);
+    expect(violations.length).to.equal(6);
   });
 });
