7fde1b6b487630ab868efd6aa36c2c38296fc8b7
Micah Chiang <micahkchiang@gmail.com>
2021-03-19 13:21:46 -0400

    test form submit

--- src/applications/personalization/view-dependents/manage-dependents/tests/unit/ManageDependentsApp.unit.spec.jsx
+++ src/applications/personalization/view-dependents/manage-dependents/tests/unit/ManageDependentsApp.unit.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import { renderInReduxProvider } from 'platform/testing/unit/react-testing-library-helpers';
 import { removeDependents } from '../../redux/reducers';
 import ManageDependentsApp from '../../containers/ManageDependentsApp';
@@ -11,7 +11,7 @@ describe('<ManageDependentsApp />', () => {
     firstName: 'Cindy',
     lastName: 'See',
     ssn: '312-243-5634',
-    dateOfBirth: '05-05-1953',
+    dateOfBirth: '05/05/1953',
     relationship: 'Spouse',
   };
   it('renders a form', async () => {
@@ -36,9 +36,9 @@ describe('<ManageDependentsApp />', () => {
           fullName: {
             firstName: mockData.firstName,
             lastName: mockData.lastName,
-            dateOfBirth: mockData.dateOfBirth,
-            ssn: mockData.ssn,
           },
+          dateOfBirth: mockData.dateOfBirth,
+          ssn: mockData.ssn,
         }}
       />,
       {
@@ -55,5 +55,84 @@ describe('<ManageDependentsApp />', () => {
     expect(await screen.findByText('Date marriage ended')).to.exist;
     expect(await screen.findByText('State where this happened')).to.exist;
     expect(await screen.findByText('City where this happened')).to.exist;
+  });
+
+  it('fills out the form', async () => {
+    const initialState = {
+      removeDependents: {
+        dependentsState: {
+          1: {
+            uiSchema: SCHEMAS.Spouse.uiSchema,
+            formSchema: SCHEMAS.Spouse.schema,
+            formData: {},
+          },
+        },
+      },
+      user: {
+        profile: {
+          vapContactInfo: {
+            email: {
+              emailAddress: 'someone@famous.com',
+            },
+            homePhone: {
+              areaCode: '555',
+              phoneNumber: '8001212',
+            },
+            residentialAddress: {
+              addressType: 'DOMESTIC',
+              countryName: 'United States',
+              countryCodeIso3: 'USA',
+              addressLine1: '123 Main Blvd',
+              addressLine2: 'Floor 33',
+              addressLine3: 'Suite 55',
+              city: 'Hollywood',
+              stateCode: 'CA',
+              zipCode: '90210',
+            },
+          },
+        },
+      },
+    };
+
+    const screen = renderInReduxProvider(
+      <ManageDependentsApp
+        relationship="Spouse"
+        closeFormHandler={() => {}}
+        stateKey={1}
+        userInfo={{
+          fullName: {
+            firstName: mockData.firstName,
+            lastName: mockData.lastName,
+          },
+          dateOfBirth: mockData.dateOfBirth,
+          ssn: mockData.ssn,
+        }}
+      />,
+      {
+        initialState,
+        reducers: { removeDependents },
+      },
+    );
+    userEvent.click(await screen.findByLabelText('Divorce'));
+    userEvent.selectOptions(await screen.findByLabelText('Month'), ['1']);
+    userEvent.selectOptions(await screen.findByLabelText('Day'), ['25']);
+    userEvent.type(await screen.findByLabelText('Year'), '2003');
+    userEvent.selectOptions(
+      await screen.findByLabelText('State where this happened', {
+        exact: false,
+      }),
+      ['AL'],
+    );
+    userEvent.type(
+      await screen.findByLabelText('City where this happened', {
+        exact: false,
+      }),
+      'Test city',
+    );
+    const submitBtn = screen.findByText('Remove dependent', {
+      selector: 'button',
+    });
+    userEvent.click(await submitBtn);
+    expect(await screen.queryAllByRole('alert')).to.have.lengthOf(0);
   });
 });
