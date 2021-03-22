da5a20fbc8c411612a25ac3499a10286f74329c0
Mark Dewey <mdewey@governmentcio.com>
2021-03-22 11:28:32 -0400

    udpated posting of data

--- src/applications/health-care-questionnaire/shared/api/tests/submit-questionnaire-response/transform.for.submit.unit.spec.js
+++ src/applications/health-care-questionnaire/shared/api/tests/submit-questionnaire-response/transform.for.submit.unit.spec.js
@@ -15,37 +15,99 @@ describe('health care questionnaire -- utils -- transform for submit --', () => 
         ],
         'hidden:appointment': {
           id: 'appointment-123',
-          type: 'va_appointments',
-          attributes: {
-            startDate: '2020-08-26T15:00:00Z',
-            sta6aid: '983',
-            clinicId: '848',
-            clinicFriendlyName: 'CHY PC VAR2',
-            facilityId: '983',
-            communityCare: false,
-            patientIcn: '1013124304V115761',
-            vdsAppointments: [
-              {
-                bookingNotes:
-                  'Follow-up/Routine: testing reason for visit field availability',
-                appointmentLength: '20',
-                id: '848;20200826.090000',
-                appointmentTime: '2021-03-26T15:00:00Z',
-                clinic: {
-                  name: 'CHY PC VAR2',
-                  askForCheckIn: false,
-                  facilityCode: '983',
-                  facility: {
-                    displayName: 'Awesome Facility',
+          status: 'booked',
+          description: 'Scheduled Visit',
+          start: '2021-11-23T08:00:00Z',
+          end: '2021-11-23T08:30:00Z',
+          minutesDuration: 30,
+          created: '2020-11-02',
+          comment: 'Follow-up/Routine:yes i have a reason',
+          participant: [
+            {
+              actor: {
+                reference:
+                  'https://sandbox-api.va.gov/services/fhir/v0/r4/Location/I2-3JYDMXC6RXTU4H25KRVXATSEJQ000000',
+                display: 'TEM MH PSO TRS IND93EH 2',
+              },
+              status: 'accepted',
+            },
+            {
+              actor: {
+                reference:
+                  'https://sandbox-api.va.gov/services/fhir/v0/r4/Patient/1008882029V851792',
+                display: 'Mrs. Sheba703 Harris789',
+              },
+              status: 'accepted',
+            },
+          ],
+          resourceType: 'Appointment',
+        },
+        'hidden:clinic': {
+          id: 'I2-3JYDMXC6RXTU4H25KRVXATSEJQ000000',
+          identifier: [
+            {
+              use: 'usual',
+              type: {
+                coding: [
+                  {
+                    system: 'http://terminology.hl7.org/CodeSystem/v2-0203',
+                    code: 'FI',
+                    display: 'Facility ID',
                   },
-                  stopCode: '502',
+                ],
+                text: 'Facility ID',
+              },
+              system:
+                'https://api.va.gov/services/fhir/v0/r4/NamingSystem/va-facility-identifier',
+              value: 'vha_442',
+            },
+            {
+              system:
+                'https://api.va.gov/services/fhir/v0/r4/NamingSystem/va-clinic-identifier',
+              value: 'vha_442_3049',
+            },
+          ],
+          status: 'active',
+          name: 'TEM MH PSO TRS IND93EH 2',
+          description: 'BLDG 146, RM W02',
+          mode: 'instance',
+          type: [
+            {
+              coding: [
+                {
+                  display: 'Primary Care',
                 },
-                type: 'REGULAR',
-                currentStatus: 'FUTURE',
+              ],
+              text: 'Primary Care',
+            },
+          ],
+          telecom: [
+            {
+              system: 'phone',
+              value: '254-743-2867 x0002',
+            },
+          ],
+          address: {
+            text: '1901 VETERANS MEMORIAL DRIVE TEMPLE TEXAS 76504',
+            line: ['1901 VETERANS MEMORIAL DRIVE'],
+            city: 'TEMPLE',
+            state: 'TEXAS',
+            postalCode: '76504',
+          },
+          physicalType: {
+            coding: [
+              {
+                display: 'BLDG 146, RM W02',
               },
             ],
-            vvsAppointments: [],
+            text: 'BLDG 146, RM W02',
           },
+          managingOrganization: {
+            reference:
+              'https://sandbox-api.va.gov/services/fhir/v0/r4/Organization/I2-AKOTGEFSVKFJOPUKHIVJAH5VQU000000',
+            display: 'CHEYENNE VA MEDICAL',
+          },
+          resourceType: 'Location',
         },
         reasonForVisit: 'reasoning for visit',
         reasonForVisitDescription: 'reasoning for visit description',
@@ -61,6 +123,7 @@ describe('health care questionnaire -- utils -- transform for submit --', () => 
     expect(json.appointment.id).to.equal('appointment-123');
     expect(json).to.have.property('questionnaire');
     expect(json.questionnaire.id).to.equal('questionnaire-123');
+    expect(json.questionnaire.title).to.equal('Primary care questionnaire');
 
     expect(json).to.have.property('item');
     expect(json.item).to.be.an('array');
@@ -98,9 +161,107 @@ describe('health care questionnaire -- utils -- transform for submit --', () => 
     const formConfig = {};
     const form = {
       data: {
-        'hidden:fields': {
-          appointmentId: 'appointment-123',
-          questionnaireId: 'questionnaire-123',
+        'hidden:questionnaire': [
+          {
+            id: 'questionnaire-123',
+            questionnaireResponse: {},
+          },
+        ],
+        'hidden:appointment': {
+          id: 'appointment-123',
+          status: 'booked',
+          description: 'Scheduled Visit',
+          start: '2021-11-23T08:00:00Z',
+          end: '2021-11-23T08:30:00Z',
+          minutesDuration: 30,
+          created: '2020-11-02',
+          comment: 'Follow-up/Routine:yes i have a reason',
+          participant: [
+            {
+              actor: {
+                reference:
+                  'https://sandbox-api.va.gov/services/fhir/v0/r4/Location/I2-3JYDMXC6RXTU4H25KRVXATSEJQ000000',
+                display: 'TEM MH PSO TRS IND93EH 2',
+              },
+              status: 'accepted',
+            },
+            {
+              actor: {
+                reference:
+                  'https://sandbox-api.va.gov/services/fhir/v0/r4/Patient/1008882029V851792',
+                display: 'Mrs. Sheba703 Harris789',
+              },
+              status: 'accepted',
+            },
+          ],
+          resourceType: 'Appointment',
+        },
+        'hidden:clinic': {
+          id: 'I2-3JYDMXC6RXTU4H25KRVXATSEJQ000000',
+          identifier: [
+            {
+              use: 'usual',
+              type: {
+                coding: [
+                  {
+                    system: 'http://terminology.hl7.org/CodeSystem/v2-0203',
+                    code: 'FI',
+                    display: 'Facility ID',
+                  },
+                ],
+                text: 'Facility ID',
+              },
+              system:
+                'https://api.va.gov/services/fhir/v0/r4/NamingSystem/va-facility-identifier',
+              value: 'vha_442',
+            },
+            {
+              system:
+                'https://api.va.gov/services/fhir/v0/r4/NamingSystem/va-clinic-identifier',
+              value: 'vha_442_3049',
+            },
+          ],
+          status: 'active',
+          name: 'TEM MH PSO TRS IND93EH 2',
+          description: 'BLDG 146, RM W02',
+          mode: 'instance',
+          type: [
+            {
+              coding: [
+                {
+                  display: 'Primary Care',
+                },
+              ],
+              text: 'Primary Care',
+            },
+          ],
+          telecom: [
+            {
+              system: 'phone',
+              value: '254-743-2867 x0002',
+            },
+          ],
+          address: {
+            text: '1901 VETERANS MEMORIAL DRIVE TEMPLE TEXAS 76504',
+            line: ['1901 VETERANS MEMORIAL DRIVE'],
+            city: 'TEMPLE',
+            state: 'TEXAS',
+            postalCode: '76504',
+          },
+          physicalType: {
+            coding: [
+              {
+                display: 'BLDG 146, RM W02',
+              },
+            ],
+            text: 'BLDG 146, RM W02',
+          },
+          managingOrganization: {
+            reference:
+              'https://sandbox-api.va.gov/services/fhir/v0/r4/Organization/I2-AKOTGEFSVKFJOPUKHIVJAH5VQU000000',
+            display: 'CHEYENNE VA MEDICAL',
+          },
+          resourceType: 'Location',
         },
         reasonForVisit: 'reasoning for visit',
         reasonForVisitDescription: 'reasoning for visit description',
@@ -124,9 +285,107 @@ describe('health care questionnaire -- utils -- transform for submit --', () => 
     const formConfig = {};
     const form = {
       data: {
-        'hidden:fields': {
-          appointmentId: 'appointment-123',
-          questionnaireId: 'questionnaire-123',
+        'hidden:questionnaire': [
+          {
+            id: 'questionnaire-123',
+            questionnaireResponse: {},
+          },
+        ],
+        'hidden:appointment': {
+          id: 'appointment-123',
+          status: 'booked',
+          description: 'Scheduled Visit',
+          start: '2021-11-23T08:00:00Z',
+          end: '2021-11-23T08:30:00Z',
+          minutesDuration: 30,
+          created: '2020-11-02',
+          comment: 'Follow-up/Routine:yes i have a reason',
+          participant: [
+            {
+              actor: {
+                reference:
+                  'https://sandbox-api.va.gov/services/fhir/v0/r4/Location/I2-3JYDMXC6RXTU4H25KRVXATSEJQ000000',
+                display: 'TEM MH PSO TRS IND93EH 2',
+              },
+              status: 'accepted',
+            },
+            {
+              actor: {
+                reference:
+                  'https://sandbox-api.va.gov/services/fhir/v0/r4/Patient/1008882029V851792',
+                display: 'Mrs. Sheba703 Harris789',
+              },
+              status: 'accepted',
+            },
+          ],
+          resourceType: 'Appointment',
+        },
+        'hidden:clinic': {
+          id: 'I2-3JYDMXC6RXTU4H25KRVXATSEJQ000000',
+          identifier: [
+            {
+              use: 'usual',
+              type: {
+                coding: [
+                  {
+                    system: 'http://terminology.hl7.org/CodeSystem/v2-0203',
+                    code: 'FI',
+                    display: 'Facility ID',
+                  },
+                ],
+                text: 'Facility ID',
+              },
+              system:
+                'https://api.va.gov/services/fhir/v0/r4/NamingSystem/va-facility-identifier',
+              value: 'vha_442',
+            },
+            {
+              system:
+                'https://api.va.gov/services/fhir/v0/r4/NamingSystem/va-clinic-identifier',
+              value: 'vha_442_3049',
+            },
+          ],
+          status: 'active',
+          name: 'TEM MH PSO TRS IND93EH 2',
+          description: 'BLDG 146, RM W02',
+          mode: 'instance',
+          type: [
+            {
+              coding: [
+                {
+                  display: 'Primary Care',
+                },
+              ],
+              text: 'Primary Care',
+            },
+          ],
+          telecom: [
+            {
+              system: 'phone',
+              value: '254-743-2867 x0002',
+            },
+          ],
+          address: {
+            text: '1901 VETERANS MEMORIAL DRIVE TEMPLE TEXAS 76504',
+            line: ['1901 VETERANS MEMORIAL DRIVE'],
+            city: 'TEMPLE',
+            state: 'TEXAS',
+            postalCode: '76504',
+          },
+          physicalType: {
+            coding: [
+              {
+                display: 'BLDG 146, RM W02',
+              },
+            ],
+            text: 'BLDG 146, RM W02',
+          },
+          managingOrganization: {
+            reference:
+              'https://sandbox-api.va.gov/services/fhir/v0/r4/Organization/I2-AKOTGEFSVKFJOPUKHIVJAH5VQU000000',
+            display: 'CHEYENNE VA MEDICAL',
+          },
+          resourceType: 'Location',
         },
         reasonForVisit: 'reasoning for visit',
         reasonForVisitDescription: 'reasoning for visit description',
@@ -162,37 +421,99 @@ describe('health care questionnaire -- utils -- transform for submit --', () => 
         ],
         'hidden:appointment': {
           id: 'appointment-123',
-          type: 'va_appointments',
-          attributes: {
-            startDate: '2020-08-26T15:00:00Z',
-            sta6aid: '983',
-            clinicId: '848',
-            clinicFriendlyName: 'CHY PC VAR2',
-            facilityId: '983',
-            communityCare: false,
-            patientIcn: '1013124304V115761',
-            vdsAppointments: [
-              {
-                bookingNotes:
-                  'Follow-up/Routine: testing reason for visit field availability',
-                appointmentLength: '20',
-                id: '848;20200826.090000',
-                appointmentTime: '2021-03-26T15:00:00Z',
-                clinic: {
-                  name: 'CHY PC VAR2',
-                  askForCheckIn: false,
-                  facilityCode: '983',
-                  facility: {
-                    displayName: 'Awesome Facility',
+          status: 'booked',
+          description: 'Scheduled Visit',
+          start: '2021-11-23T08:00:00Z',
+          end: '2021-11-23T08:30:00Z',
+          minutesDuration: 30,
+          created: '2020-11-02',
+          comment: 'Follow-up/Routine:yes i have a reason',
+          participant: [
+            {
+              actor: {
+                reference:
+                  'https://sandbox-api.va.gov/services/fhir/v0/r4/Location/I2-3JYDMXC6RXTU4H25KRVXATSEJQ000000',
+                display: 'TEM MH PSO TRS IND93EH 2',
+              },
+              status: 'accepted',
+            },
+            {
+              actor: {
+                reference:
+                  'https://sandbox-api.va.gov/services/fhir/v0/r4/Patient/1008882029V851792',
+                display: 'Mrs. Sheba703 Harris789',
+              },
+              status: 'accepted',
+            },
+          ],
+          resourceType: 'Appointment',
+        },
+        'hidden:clinic': {
+          id: 'I2-3JYDMXC6RXTU4H25KRVXATSEJQ000000',
+          identifier: [
+            {
+              use: 'usual',
+              type: {
+                coding: [
+                  {
+                    system: 'http://terminology.hl7.org/CodeSystem/v2-0203',
+                    code: 'FI',
+                    display: 'Facility ID',
                   },
-                  stopCode: '502',
+                ],
+                text: 'Facility ID',
+              },
+              system:
+                'https://api.va.gov/services/fhir/v0/r4/NamingSystem/va-facility-identifier',
+              value: 'vha_442',
+            },
+            {
+              system:
+                'https://api.va.gov/services/fhir/v0/r4/NamingSystem/va-clinic-identifier',
+              value: 'vha_442_3049',
+            },
+          ],
+          status: 'active',
+          name: 'TEM MH PSO TRS IND93EH 2',
+          description: 'BLDG 146, RM W02',
+          mode: 'instance',
+          type: [
+            {
+              coding: [
+                {
+                  display: 'Primary Care',
                 },
-                type: 'REGULAR',
-                currentStatus: 'FUTURE',
+              ],
+              text: 'Primary Care',
+            },
+          ],
+          telecom: [
+            {
+              system: 'phone',
+              value: '254-743-2867 x0002',
+            },
+          ],
+          address: {
+            text: '1901 VETERANS MEMORIAL DRIVE TEMPLE TEXAS 76504',
+            line: ['1901 VETERANS MEMORIAL DRIVE'],
+            city: 'TEMPLE',
+            state: 'TEXAS',
+            postalCode: '76504',
+          },
+          physicalType: {
+            coding: [
+              {
+                display: 'BLDG 146, RM W02',
               },
             ],
-            vvsAppointments: [],
+            text: 'BLDG 146, RM W02',
           },
+          managingOrganization: {
+            reference:
+              'https://sandbox-api.va.gov/services/fhir/v0/r4/Organization/I2-AKOTGEFSVKFJOPUKHIVJAH5VQU000000',
+            display: 'CHEYENNE VA MEDICAL',
+          },
+          resourceType: 'Location',
         },
         reasonForVisit: 'reasoning for visit',
         reasonForVisitDescription: 'reasoning for visit description',
