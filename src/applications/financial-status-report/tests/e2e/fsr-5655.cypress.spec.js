97d68848cda2746d927d00fa8710c6bd7fd97599
Craig Wheeler <craig@craigwheeler.net>
2021-03-22 17:36:13 -0400

    config test

--- src/applications/financial-status-report/tests/e2e/fsr-5655.cypress.spec.js
+++ src/applications/financial-status-report/tests/e2e/fsr-5655.cypress.spec.js
@@ -1,41 +1,30 @@
 import path from 'path';
-
 import testForm from 'platform/testing/e2e/cypress/support/form-tester';
 import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-tester/utilities';
-
 import formConfig from '../../config/form';
 import manifest from '../../manifest.json';
 
 const testConfig = createTestConfig(
   {
     dataPrefix: 'data',
-
-    // Rename and modify the test data as needed.
-    dataSets: ['test-data'],
-
+    dataSets: ['negativeResponse'],
     fixtures: {
       data: path.join(__dirname, 'fixtures', 'data'),
     },
-
+    // setupPerTest: () => {
+    //   // Log in if the form requires an authenticated session.
+    //   cy.login();
+    //   cy.route('POST', formConfig.submitUrl, { status: 200 });
+    // },
     pageHooks: {
-      introduction: ({ afterHook }) => {
-        afterHook(() => {
-          cy.findAllByText(/start/i, { selector: 'button' })
-            .last()
-            .click();
-        });
+      introduction: () => {
+        // Hit the start button
+        cy.findAllByText(/start/i, { selector: 'button' })
+          .first()
+          .click();
       },
     },
-
-    setupPerTest: () => {
-      // Log in if the form requires an authenticated session.
-      // cy.login();
-
-      cy.route('POST', formConfig.submitUrl, { status: 200 });
-    },
-
-    // disable test until FSR is in production
-    skip: true,
+    // skip: true, // disable test until FSR is in production
   },
   manifest,
   formConfig,
