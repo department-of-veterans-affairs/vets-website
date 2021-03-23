b8e5757ed4c10b52100074e4d5a70ba7cf36b9dd
Stephen Barrs <stephen.barrs@va.gov>
2021-03-23 15:42:30 -0400

    intro page updates for 3 buttons

--- src/applications/covid-vaccine/containers/IntroductionPage.jsx
+++ src/applications/covid-vaccine/containers/IntroductionPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import recordEvent from 'platform/monitoring/record-event';
+import { Link } from 'react-router';
+import environment from 'platform/utilities/environment';
 
 import { focusElement } from 'platform/utilities/ui';
 
 import FormTitle from 'platform/forms-system/src/js/components/FormTitle';
-import SaveInProgressIntro from 'platform/forms/save-in-progress/SaveInProgressIntro';
 
 class IntroductionPage extends React.Component {
   componentDidMount() {
@@ -13,66 +15,59 @@ class IntroductionPage extends React.Component {
   render() {
     return (
       <div className="schemaform-intro">
-        <FormTitle title="covid-vaccine-expansion" />
-        <p>Equal to VA Form (covid-vaccine-expansion).</p>
-        <SaveInProgressIntro
-          prefillEnabled={this.props.route.formConfig.prefillEnabled}
-          messages={this.props.route.formConfig.savedFormMessages}
-          pageList={this.props.route.pageList}
-          startText="Start the Application"
-        >
-          Please complete the form to apply for covid vaccine.
-        </SaveInProgressIntro>
-        <h4>Follow the steps below to apply for covid vaccine.</h4>
-        <div className="process schemaform-process">
-          <ol>
-            <li className="process-step list-one">
-              <h5>Prepare</h5>
-              <h6>To fill out this application, you’ll need your:</h6>
-              <ul>
-                <li>Social Security number (required)</li>
-              </ul>
-              <p>
-                <strong>What if I need help filling out my application?</strong>{' '}
-                An accredited representative, like a Veterans Service Officer
-                (VSO), can help you fill out your claim.{' '}
-                <a href="/disability-benefits/apply/help/index.html">
-                  Get help filing your claim
-                </a>
-              </p>
-            </li>
-            <li className="process-step list-two">
-              <h5>Apply</h5>
-              <p>Complete this covid vaccine form.</p>
-              <p>
-                After submitting the form, you’ll get a confirmation message.
-                You can print this for your records.
-              </p>
-            </li>
-            <li className="process-step list-three">
-              <h5>VA Review</h5>
-              <p>
-                We process claims within a week. If more than a week has passed
-                since you submitted your application and you haven’t heard back,
-                please don’t apply again. Call us at.
-              </p>
-            </li>
-            <li className="process-step list-four">
-              <h5>Decision</h5>
-              <p>
-                Once we’ve processed your claim, you’ll get a notice in the mail
-                with our decision.
-              </p>
-            </li>
-          </ol>
-        </div>
-        <SaveInProgressIntro
-          buttonOnly
-          messages={this.props.route.formConfig.savedFormMessages}
-          pageList={this.props.route.pageList}
-          startText="Start the Application"
-        />
-        <div className="omb-info--container" style={{ paddingLeft: '0px' }} />
+        <FormTitle title="Sign up to get a COVID-19 vaccine at VA" />
+        <h2>
+          Are you a Veteran who is enrolled in VA health care or receiving care
+          at VA?
+        </h2>
+        <p>
+          <a
+            className="usa-button"
+            href={encodeURI(
+              `${
+                environment.BASE_URL
+              }/health-care/covid-19-vaccine/stay-informed/form`,
+            )}
+            onClick={() => {
+              recordEvent({
+                event: 'cta-button-click',
+                'button-type': 'default',
+                'button-click-label': 'I have used VA health care before',
+                'button-background-color': '#0071bb',
+              });
+            }}
+          >
+            I have used VA health care before
+          </a>
+          <Link
+            className="usa-button"
+            to="/verify-eligibility"
+            onClick={() => {
+              recordEvent({
+                event: 'cta-button-click',
+                'button-type': 'default',
+                'button-click-label': 'I am new to VA health care',
+                'button-background-color': 'transparent',
+              });
+            }}
+          >
+            I am new to VA health care
+          </Link>
+          <Link
+            className="usa-button"
+            to="/verify-eligibility"
+            onClick={() => {
+              recordEvent({
+                event: 'cta-button-click',
+                'button-type': 'default',
+                'button-click-label': "I'm not sure",
+                'button-background-color': 'transparent',
+              });
+            }}
+          >
+            I'm not sure
+          </Link>
+        </p>
       </div>
     );
   }
