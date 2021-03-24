31c5bf7e4d25237e8a2274d413b6e26d0862c321
Stephen Barrs <stephen.barrs@va.gov>
2021-03-24 14:02:55 -0400

    radio buttons on intro page

--- src/applications/covid-vaccine/containers/IntroductionPage.jsx
+++ src/applications/covid-vaccine/containers/IntroductionPage.jsx
@@ -1,33 +1,88 @@
 import React from 'react';
 import recordEvent from 'platform/monitoring/record-event';
-import { Link } from 'react-router';
-import environment from 'platform/utilities/environment';
 
 import { focusElement } from 'platform/utilities/ui';
 
 import FormTitle from 'platform/forms-system/src/js/components/FormTitle';
 
+const alreadyReceivingCarePath =
+  '/health-care/covid-19-vaccine/stay-informed/form';
+const newlyEligiblePath = '/covid-vaccine/verify-eligibility';
+
 class IntroductionPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentSelection: '', nextUrl: '' };
+  }
   componentDidMount() {
     focusElement('.va-nav-breadcrumbs-list');
+
+    // These do NOT reset the checked status, not sure why
+    document.getElementById('yes').checked = false;
+    document.getElementById('no').checked = false;
+    document.getElementById('imnotsure').checked = false;
   }
 
+  setSelected(selected) {
+    this.setState({
+      currentSelection: selected,
+      nextUrl:
+        selected === 'yes' ? alreadyReceivingCarePath : newlyEligiblePath,
+    });
+  }
   render() {
     return (
       <div className="schemaform-intro">
         <FormTitle title="Sign up to get a COVID-19 vaccine at VA" />
-        <h2>
-          Are you a Veteran who is enrolled in VA health care or receiving care
-          at VA?
-        </h2>
-        <p>
-          <a
+        <fieldset className="fieldset-input">
+          <legend className="legend-label vads-u-font-size--h3">
+            Are you a Veteran who is enrolled in VA health care or receiving
+            care at VA?
+          </legend>
+          <div className="form-radio-buttons">
+            <div className="radio-button">
+              <input
+                type="radio"
+                id="yes"
+                name="enrolledInVaCheck"
+                value="Yes"
+                onChange={() => this.setSelected('yes')}
+                checked={this.state.currentSelection === 'yes'}
+              />
+              <label name="yes-label" htmlFor="yes">
+                Yes
+              </label>
+            </div>
+            <div className="radio-button">
+              <input
+                type="radio"
+                id="no"
+                name="enrolledInVaCheck"
+                value="No"
+                onChange={() => this.setSelected('no')}
+                checked={this.state.currentSelection === 'no'}
+              />
+              <label name="no-label" htmlFor="no">
+                No
+              </label>
+            </div>
+            <div className="radio-button">
+              <input
+                type="radio"
+                id="imnotsure"
+                name="enrolledInVaCheck"
+                value="I'm not sure"
+                onChange={() => this.setSelected('imnotsure')}
+                checked={this.state.currentSelection === 'imnotsure'}
+              />
+              <label name="imnotsure-label" htmlFor="imnotsure">
+                I'm not sure
+              </label>
+            </div>
+          </div>
+          <button
+            type="button"
             className="usa-button"
-            href={encodeURI(
-              `${
-                environment.BASE_URL
-              }/health-care/covid-19-vaccine/stay-informed/form`,
-            )}
             onClick={() => {
               recordEvent({
                 event: 'cta-button-click',
@@ -35,39 +90,12 @@ class IntroductionPage extends React.Component {
                 'button-click-label': 'I have used VA health care before',
                 'button-background-color': '#0071bb',
               });
+              window.location.href = this.state.nextUrl;
             }}
           >
-            I have used VA health care before
-          </a>
-          <Link
-            className="usa-button"
-            to="/verify-eligibility"
-            onClick={() => {
-              recordEvent({
-                event: 'cta-button-click',
-                'button-type': 'default',
-                'button-click-label': 'I am new to VA health care',
-                'button-background-color': 'transparent',
-              });
-            }}
-          >
-            I am new to VA health care
-          </Link>
-          <Link
-            className="usa-button"
-            to="/verify-eligibility"
-            onClick={() => {
-              recordEvent({
-                event: 'cta-button-click',
-                'button-type': 'default',
-                'button-click-label': "I'm not sure",
-                'button-background-color': 'transparent',
-              });
-            }}
-          >
-            I'm not sure
-          </Link>
-        </p>
+            Continue
+          </button>
+        </fieldset>
       </div>
     );
   }
