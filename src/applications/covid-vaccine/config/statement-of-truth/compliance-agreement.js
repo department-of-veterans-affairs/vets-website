1f93ceb4923cc66d10dc61fe0144612eec8ee898
Stephen Barrs <stephen.barrs@va.gov>
2021-03-24 16:14:24 -0400

    dynamic statement of truth

--- src/applications/covid-vaccine/config/statement-of-truth/compliance-agreement.js
+++ src/applications/covid-vaccine/config/statement-of-truth/compliance-agreement.js
@@ -1,27 +1,79 @@
-import { statementOfTruth } from './helpers';
+import {
+  statementOfTruthVeteran,
+  statementOfTruthSpouse,
+  statementOfTruthCaregiver,
+  statementOfTruthChampva,
+} from './helpers';
 
 export const schema = {
   complianceAgreement: {
     type: 'object',
     properties: {
-      iCertify: {
+      veteranCertify: {
+        type: 'boolean',
+      },
+      spouseCertify: {
+        type: 'boolean',
+      },
+      caregiverCertify: {
+        type: 'boolean',
+      },
+      champVaRecipientCertify: {
         type: 'boolean',
       },
     },
   },
 };
 
+// TODO conditional require the one that will be shown.
+
 export const uiSchema = {
   complianceAgreement: {
     'ui:title': 'Statement of truth',
     'ui:description':
       'If you understand and agree with these statements, please check the box:',
-    iCertify: {
+    veteranCertify: {
       'ui:options': {
         hideLabelText: true,
+        hideIf: formData => {
+          return formData.applicatntType !== 'veteran';
+        },
       },
       'ui:widget': 'checkbox',
-      'ui:title': statementOfTruth,
+      'ui:title': statementOfTruthVeteran,
+    },
+    spouseCertify: {
+      'ui:options': {
+        hideLabelText: true,
+        hideIf: formData => {
+          return formData.applicatntType !== 'spouse';
+        },
+      },
+      'ui:widget': 'checkbox',
+      'ui:title': statementOfTruthSpouse,
+    },
+    caregiverCertify: {
+      'ui:options': {
+        hideLabelText: true,
+        hideIf: formData => {
+          return (
+            formData.applicatntType !== 'caregiverEnrolled' &&
+            formData.applicatntType !== 'caregiverOfVeteran'
+          );
+        },
+      },
+      'ui:widget': 'checkbox',
+      'ui:title': statementOfTruthCaregiver,
+    },
+    champVaRecipientCertify: {
+      'ui:options': {
+        hideLabelText: true,
+        hideIf: formData => {
+          return formData.applicatntType !== 'CHAMPVA';
+        },
+      },
+      'ui:widget': 'checkbox',
+      'ui:title': statementOfTruthChampva,
     },
   },
 };
--- src/applications/covid-vaccine/config/statement-of-truth/helpers.js
+++ src/applications/covid-vaccine/config/statement-of-truth/helpers.js
@@ -1,12 +1,45 @@
 import React from 'react';
 
-export const statementOfTruth = (
+export const statementOfTruthVeteran = (
   <>
     <p>
-      I certify that I am a XXXXX. My answer to this question is true and
+      I certify that I am a Veteran. My answer to this question is true and
       correct to the best of my knowledge and belief. I understand that it's a
       crime to give false information that could affect my eligibility to get a
       vaccine through VA. Penalties may include a fine, imprisonment, or both.{' '}
+    </p>
+  </>
+);
+export const statementOfTruthSpouse = (
+  <>
+    <p>
+      I certify that I am a spouse of a Veteran. My answer to this question is
+      true and correct to the best of my knowledge and belief. I understand that
+      it's a crime to give false information that could affect my eligibility to
+      get a vaccine through VA. Penalties may include a fine, imprisonment, or
+      both.{' '}
+    </p>
+  </>
+);
+export const statementOfTruthCaregiver = (
+  <>
+    <p>
+      I certify that I am a caregiver for a Veteran. My answer to this question
+      is true and correct to the best of my knowledge and belief. I understand
+      that it's a crime to give false information that could affect my
+      eligibility to get a vaccine through VA. Penalties may include a fine,
+      imprisonment, or both.{' '}
+    </p>
+  </>
+);
+export const statementOfTruthChampva = (
+  <>
+    <p>
+      I certify that I am a reciepient of CHAMPVA. My answer to this question is
+      true and correct to the best of my knowledge and belief. I understand that
+      it's a crime to give false information that could affect my eligibility to
+      get a vaccine through VA. Penalties may include a fine, imprisonment, or
+      both.{' '}
     </p>
   </>
 );
