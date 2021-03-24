12bec5fa7abd3034b0fbe9f8663406cb1fde0300
Craig Wheeler <craig@craigwheeler.net>
2021-03-24 13:39:19 -0400

    config mock user with debts

--- src/applications/financial-status-report/tests/e2e/fsr-5655.cypress.spec.js
+++ src/applications/financial-status-report/tests/e2e/fsr-5655.cypress.spec.js
@@ -3,13 +3,24 @@ import testForm from 'platform/testing/e2e/cypress/support/form-tester';
 import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-tester/utilities';
 import formConfig from '../../config/form';
 import manifest from '../../manifest.json';
+import mockUser from './fixtures/mocks/mockUser.json';
+import debts from './fixtures/mocks/debts.json';
 
 const testConfig = createTestConfig(
   {
     dataPrefix: 'data',
-    dataSets: ['negativeResponse'],
+    dataSets: ['positiveResponse'],
     fixtures: {
       data: path.join(__dirname, 'fixtures', 'data'),
+    },
+    setupPerTest: () => {
+      cy.login(mockUser);
+      cy.intercept('GET', '/v0/debts', debts);
+
+      // cy.get('@testData').then(testData => {
+      //   cy.intercept('GET', '/v0/in_progress_forms/5655', testData);
+      //   cy.intercept('PUT', 'v0/in_progress_forms/5655', testData);
+      // });
     },
     pageHooks: {
       introduction: () => {
