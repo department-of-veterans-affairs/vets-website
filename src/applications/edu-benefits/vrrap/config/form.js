6a569a9406a8489676319fa195e88d05bf4dc666
Kenneth Santiago <santiago_kenneth@bah.com>
2021-03-25 14:51:25 -0400

    fixed unit test issues

--- src/applications/edu-benefits/vrrap/config/form.js
+++ src/applications/edu-benefits/vrrap/config/form.js
@@ -2,18 +2,30 @@
 
 import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
+import manifest from '../manifest.json';
 
 // const { } = fullSchema.properties;
 
 // const { } = fullSchema.definitions;
 
 const formConfig = {
+  rootUrl: manifest.rootUrl,
   urlPrefix: '/',
   submitUrl: '/v0/api',
   trackingPrefix: 'vrrap-',
   introduction: IntroductionPage,
   confirmation: ConfirmationPage,
   formId: 'VRRAP',
+  saveInProgress: {
+    messages: {
+      inProgress:
+        'Your Veteran Rapid Retraining Assistance Program application is in progress.',
+      expired:
+        'Veteran Rapid Retraining Assistance Program application has expired. If you want to apply for Veteran Rapid Retraining Assistance Program, please start a new application.',
+      saved:
+        'Veteran Rapid Retraining Assistance Program application has been saved.',
+    },
+  },
   version: 0,
   prefillEnabled: true,
   savedFormMessages: {
