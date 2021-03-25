13fe994dcfaace5fef4f511dd7a6a487f3cb9fe4
Jesse Cohn <jesse.cohn@teamadhoc.us>
2021-03-25 10:56:39 -0700

    fixed import

--- src/applications/covid-vaccine/config/pages.js
+++ src/applications/covid-vaccine/config/pages.js
@@ -1,7 +1,7 @@
 import * as attestation from './attestation/attestation';
 import * as notEligible from './not-eligible/not-eligible';
 import * as militaryHistory from './military-history/military-history';
-import * as complianceAgreement from './attestation/compliance-agreement';
+import * as complianceAgreement from './statement-of-truth/compliance-agreement';
 import * as personalInformation from './personal-information/personal-information';
 import * as addressInformation from './address/addressInformation';
 import * as vaLocation from './va-location/va-location';
