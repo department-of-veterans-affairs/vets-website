a46c4250c8a4c9337751d2a795ea7aac9ea5edbc
Sandra Hallie <sandra.adamshallie@gmail.com>
2021-03-25 15:14:45 -0600

    Lint fix

--- src/applications/personalization/dashboard-2/tests/unit/healthcare-appointments.unit.spec.jsx
+++ src/applications/personalization/dashboard-2/tests/unit/healthcare-appointments.unit.spec.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { expect } from 'chai';
-import { mockFetch, resetFetch } from '~/platform/testing/unit/helpers';
 import { renderInReduxProvider } from '~/platform/testing/unit/react-testing-library-helpers';
 import reducers from '~/applications/personalization/dashboard/reducers';
 import HealthCare from '~/applications/personalization/dashboard-2/components/health-care/HealthCare';
