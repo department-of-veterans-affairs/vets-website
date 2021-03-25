69e1e311410c24f5a4ade4206a4066d9a244a399
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-25 16:30:36 -0400

    if provided, add dataName to html filename

--- src/site/tests/support/index.js
+++ src/site/tests/support/index.js
@@ -18,9 +18,10 @@ const getFile = filePath =>
 const getLayout = filePath => getFile(filePath);
 const parseFixture = filePath => JSON.parse(getFile(filePath));
 
-const makeHTMLFileName = name => {
-  const liquidFileName = name.match(/(\w|\d|\.|-)+$/g)[0];
-  return `${liquidFileName.split('.')[0]}.html`;
+const makeHTMLFileName = (layPath, dataName) => {
+  const liquidFileName = layPath.match(/(\w|\d|\.|-)+$/g)[0];
+  const fileName = liquidFileName.split('.')[0];
+  return dataName ? `${fileName}.${dataName}.html` : `${fileName}.html`;
 };
 
 const createDirectory = async () => {
@@ -68,7 +69,7 @@ const updateHTML = files => {
   modifyDom(options)(files, null, done);
 };
 
-const renderHTML = (layoutPath, data) => {
+const renderHTML = (layoutPath, data, dataName) => {
   const layout = getLayout(layoutPath);
   const context = liquid.newContext({ locals: data });
 
@@ -85,7 +86,7 @@ const renderHTML = (layoutPath, data) => {
         reject(err);
       } else {
         const html = context.getBuffer();
-        const htmlFileName = makeHTMLFileName(layoutPath);
+        const htmlFileName = makeHTMLFileName(layoutPath, dataName);
         const files = {
           [htmlFileName]: { contents: html, isDrupalPage: true },
           'generated/file-manifest.json': { contents: JSON.stringify({}) },
