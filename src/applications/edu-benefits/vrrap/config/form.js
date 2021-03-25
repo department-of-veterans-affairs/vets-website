15a3644fe806cbfbd239f6cbb402cfea5baacc56
Kenneth Santiago <santiago_kenneth@bah.com>
2021-03-25 12:06:32 -0400

    vrrap form creation

---
+++ src/applications/edu-benefits/vrrap/config/form.js
@@ -0,0 +1,44 @@
+// import fullSchema from 'vets-json-schema/dist/VRRAP-schema.json';
+
+import IntroductionPage from '../containers/IntroductionPage';
+import ConfirmationPage from '../containers/ConfirmationPage';
+
+// const { } = fullSchema.properties;
+
+// const { } = fullSchema.definitions;
+
+const formConfig = {
+  urlPrefix: '/',
+  submitUrl: '/v0/api',
+  trackingPrefix: 'vrrap-',
+  introduction: IntroductionPage,
+  confirmation: ConfirmationPage,
+  formId: 'VRRAP',
+  version: 0,
+  prefillEnabled: true,
+  savedFormMessages: {
+    notFound: 'Please start over to apply for education benefits.',
+    noAuth:
+      'Please sign in again to continue your application for education benefits.',
+  },
+  title: 'Veteran Rapid Retraining Assistance Program',
+  defaultDefinitions: {},
+  chapters: {
+    chapter1: {
+      title: 'Chapter 1',
+      pages: {
+        page1: {
+          path: 'first-page',
+          title: 'First Page',
+          uiSchema: {},
+          schema: {
+            type: 'object',
+            properties: {},
+          },
+        },
+      },
+    },
+  },
+};
+
+export default formConfig;
