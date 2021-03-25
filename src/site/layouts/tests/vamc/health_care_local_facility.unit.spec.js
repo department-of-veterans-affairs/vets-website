1f43b6ec22d484e60a8270acf6171fb411f5bb6b
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-25 16:02:31 -0400

    remove 3 options to call this.skip()

--- src/site/layouts/tests/vamc/health_care_local_facility.unit.spec.js
+++ src/site/layouts/tests/vamc/health_care_local_facility.unit.spec.js
@@ -4,38 +4,14 @@ import axeCheck from '~/site/tests/support/axe';
 
 const layoutPath = 'src/site/layouts/health_care_local_facility.drupal.liquid';
 
-// part of option 1
-parseFixture.bind(this);
-
 describe('health_care_local_facility', () => {
   // TODO extend tests with more suites
   describe('PhoneNumbers', () => {
     let container;
 
-    // option 1: bind this to parseFixture() (see above);
-    // call this.skip() in parseFixture()
     const data = parseFixture(
       'src/site/layouts/tests/vamc/fixtures/health_care_local_facility_no_phone_mental.json',
     );
-    // end option 1
-
-    // option 2: bind this to parseFixture() here
-    // call this.skip() in parseFixture()
-    // const data = parseFixture(
-    //   'src/site/layouts/tests/vamc/fixtures/health_care_local_facility_no_phone_mental.json',
-    // ).bind(this);
-    // end option 2
-
-    // option 3: don't bind this to parseFixture()
-    // call this.skip() if the return value of parseFixture() is falsey (see below)
-    // const data = parseFixture(
-    //   'src/site/layouts/tests/vamc/fixtures/health_care_local_facility_no_phone_mental.json',
-    // );
-
-    // if (!data) {
-    //   this.skip();
-    // }
-    // end option 3
 
     before(async () => {
       container = await renderHTML(layoutPath, data);
