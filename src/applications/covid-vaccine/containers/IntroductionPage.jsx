1bbdc08bd725aa60f79c964d404b17529eb523ba
Stephen Barrs <stephen.barrs@va.gov>
2021-03-26 09:06:23 -0400

    refactor radios and button to use storybook components

--- src/applications/covid-vaccine/containers/IntroductionPage.jsx
+++ src/applications/covid-vaccine/containers/IntroductionPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import recordEvent from 'platform/monitoring/record-event';
+import RadioButtons from '@department-of-veterans-affairs/component-library/RadioButtons';
+import ProgressButton from '@department-of-veterans-affairs/component-library/ProgressButton';
 
 import { focusElement } from 'platform/utilities/ui';
 
@@ -16,85 +18,49 @@ class IntroductionPage extends React.Component {
   }
   componentDidMount() {
     focusElement('.va-nav-breadcrumbs-list');
-
-    // These do NOT reset the checked status, not sure why
-    document.getElementById('yes').checked = false;
-    document.getElementById('no').checked = false;
-    document.getElementById('imnotsure').checked = false;
   }
 
   setSelected(selected) {
     this.setState({
       currentSelection: selected,
       nextUrl:
-        selected === 'yes' ? alreadyReceivingCarePath : newlyEligiblePath,
+        selected.value === 'Yes' ? alreadyReceivingCarePath : newlyEligiblePath,
     });
   }
+  loadNextPage() {
+    recordEvent({
+      event: 'cta-button-click',
+      'button-type': 'default',
+      'button-click-label': 'I have used VA health care before',
+      'button-background-color': '#0071bb',
+    });
+    window.location.href = this.state.nextUrl;
+  }
+
   render() {
     return (
       <div className="schemaform-intro">
         <FormTitle title="Sign up to get a COVID-19 vaccine at VA" />
-        <fieldset className="fieldset-input">
-          <legend className="legend-label vads-u-font-size--h3">
-            Are you a Veteran who is enrolled in VA health care or receiving
-            care at VA?
-          </legend>
-          <div className="form-radio-buttons">
-            <div className="radio-button">
-              <input
-                type="radio"
-                id="yes"
-                name="enrolledInVaCheck"
-                value="Yes"
-                onChange={() => this.setSelected('yes')}
-                checked={this.state.currentSelection === 'yes'}
-              />
-              <label name="yes-label" htmlFor="yes">
-                Yes
-              </label>
-            </div>
-            <div className="radio-button">
-              <input
-                type="radio"
-                id="no"
-                name="enrolledInVaCheck"
-                value="No"
-                onChange={() => this.setSelected('no')}
-                checked={this.state.currentSelection === 'no'}
-              />
-              <label name="no-label" htmlFor="no">
-                No
-              </label>
-            </div>
-            <div className="radio-button">
-              <input
-                type="radio"
-                id="imnotsure"
-                name="enrolledInVaCheck"
-                value="I'm not sure"
-                onChange={() => this.setSelected('imnotsure')}
-                checked={this.state.currentSelection === 'imnotsure'}
-              />
-              <label name="imnotsure-label" htmlFor="imnotsure">
-                I'm not sure
-              </label>
-            </div>
-          </div>
-          <button
-            type="button"
-            className="usa-button"
-            onClick={() => {
-              recordEvent({
-                event: 'cta-button-click',
-                'button-type': 'default',
-                'button-click-label': 'I have used VA health care before',
-                'button-background-color': '#0071bb',
-              });
-              window.location.href = this.state.nextUrl;
-            }}
-          >
-            Continue
-          </button>
+        <fieldset className="fieldset-input u-vads-margin-top--10">
+          <RadioButtons
+            id="introductionRadios"
+            errorMessage=""
+            onKeyDown={function noRefCheck() {}}
+            onMouseDown={function noRefCheck() {}}
+            onValueChange={val => this.setSelected(val)}
+            options={['Yes', 'No', "I'm not sure"]}
+            value={this.state.currentSelection}
+            label="Are you a Veteran who is enrolled in VA health care or receiving
+            care at VA?"
+            required
+          />
+          <ProgressButton
+            id="continueButton"
+            afterText="»"
+            buttonText="Continue"
+            onButtonClick={() => this.loadNextPage()}
+            disabled={this.state.currentSelection === ''}
+          />
         </fieldset>
       </div>
     );
