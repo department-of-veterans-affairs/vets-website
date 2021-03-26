eb81c743e6fc913a3492895605a6e606129ca2ac
Sandra Hallie <sandra.adamshallie@gmail.com>
2021-03-26 11:49:25 -0600

    Update appointments and IconCTALink layout (#16507)
    * Update appointments and IconCTALink layout
    
    * Css updates
    
    * Update tests
    
    * All tests pass
    
    * Minor changes
    
    * Css updates
    
    * Fix tests
    
    * Update layout
    
    * Remove unneeded test logic
    
    * Lint fix
    
    * Updates per PR review

--- src/applications/personalization/dashboard-2/components/IconCTALink.jsx
+++ src/applications/personalization/dashboard-2/components/IconCTALink.jsx
@@ -10,7 +10,7 @@ const IconCTALink = ({
   boldText,
   newTab,
 }) => {
-  const linkClass = `vads-u-text-decoration--none vads-u-padding-y--2p5 vads-u-padding-x--1 cta-link vads-u-font-weight--${
+  const linkClass = `vads-u-text-decoration--none vads-u-padding-y--2p5 cta-link vads-u-font-weight--${
     boldText ? 'bold' : 'normal'
   }`;
 
@@ -26,18 +26,22 @@ const IconCTALink = ({
       onClick={onClick || undefined}
       className={linkClass}
     >
-      <span className="fa-stack fa-sm vads-u-margin-right--1">
-        <i
-          aria-hidden="true"
-          className="fas fa-circle fa-stack-2x vads-u-color--primary-alt-lightest"
-        />
-        <i aria-hidden="true" className={`fas fa-${icon} fa-stack-1x`} />
+      <span className="vads-u-display--flex">
+        <span className="fa-stack fa-sm vads-u-height--full vads-u-margin-right--1">
+          <i
+            aria-hidden="true"
+            className="fas fa-circle fa-stack-2x vads-u-color--primary-alt-lightest"
+          />
+          <i aria-hidden="true" className={`fas fa-${icon} fa-stack-1x`} />
+        </span>
+        <span>
+          {text}
+          <i
+            aria-hidden="true"
+            className="fas fa-xs fa-chevron-right vads-u-margin-left--1"
+          />
+        </span>
       </span>
-      {text}
-      <i
-        aria-hidden="true"
-        className="fas fa-xs fa-chevron-right vads-u-margin-x--1"
-      />
     </a>
   );
 };
