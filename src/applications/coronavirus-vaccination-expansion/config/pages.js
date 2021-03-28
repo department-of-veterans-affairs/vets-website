1c4cf80e00b4e1bd271c2dde6ac930c73ab9001a
Jesse Cohn <jesse.cohn@adhocteam.us>
2021-03-27 18:10:00 -0700

    Checkbox and location component update (#16545)
    * changed data sent to API to be location ID
    
    * Adjusted pre-submit and removed unneeded page
    
    Co-authored-by: Jesse Cohn <jesse.cohn@teamadhoc.us>

--- src/applications/coronavirus-vaccination-expansion/config/pages.js
+++ src/applications/coronavirus-vaccination-expansion/config/pages.js
@@ -1,6 +1,5 @@
 import * as attestation from './attestation/attestation';
 import * as militaryHistory from './military-history/military-history';
-import * as complianceAgreement from './statement-of-truth/compliance-agreement';
 import * as personalInformation from './personal-information/personal-information';
 import * as addressInformation from './address/addressInformation';
 import * as vaLocation from './va-location/va-location';
@@ -9,7 +8,6 @@ import * as veteranInformation from './veteran-information/veteran-information';
 export {
   attestation,
   militaryHistory,
-  complianceAgreement,
   personalInformation,
   addressInformation,
   vaLocation,
