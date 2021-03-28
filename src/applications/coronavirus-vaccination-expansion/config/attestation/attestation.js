033bfbd25a803a0e013645514f49e7cbfc8de31a
Brooks Johnson <brooks.johnson@pm.me>
2021-03-28 10:09:37 -0700

    Update beginning text
    - Change accordion item header level
    - Add in a h2
    - Shift the text around a bit

--- src/applications/coronavirus-vaccination-expansion/config/attestation/attestation.js
+++ src/applications/coronavirus-vaccination-expansion/config/attestation/attestation.js
@@ -17,13 +17,11 @@ export const uiSchema = {
   attestation: {
     introText: {
       'view:introText': {
-        'ui:description':
-          'We have a limited amount of COVID-19 vaccines. We want to make sure we can offer vaccines to as many Veterans, family members, and caregivers as we can. We can only offer vaccines to people who are eligible under the law. Thank you for helping us to achieve our mission.',
+        'ui:description': eligibilityAccordion,
       },
     },
     applicantType: {
       'ui:title': 'Which of these best describes you?',
-      'ui:description': eligibilityAccordion,
       'ui:widget': 'radio',
       'ui:options': {
         labels: {
