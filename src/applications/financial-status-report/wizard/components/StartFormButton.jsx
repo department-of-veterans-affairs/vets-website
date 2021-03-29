76b80d7e80f6cd5913acdb4282c2b4e87dcdf130
Craig Wheeler <craig@craigwheeler.net>
2021-03-29 13:47:24 -0400

    clean up

--- src/applications/financial-status-report/wizard/components/StartFormButton.jsx
+++ src/applications/financial-status-report/wizard/components/StartFormButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { WIZARD_STATUS_COMPLETE } from 'applications/static-pages/wizard';
-import manifest from '../../manifest.json';
+import { rootUrl } from '../../manifest.json';
 
 const StartFormButton = ({ setWizardStatus, label, ariaId }) => {
   return (
     <a
-      href={`${manifest.rootUrl}/introduction`}
+      href={`${rootUrl}/introduction`}
       className="usa-button-primary va-button-primary"
       onClick={event => {
         event.preventDefault();
