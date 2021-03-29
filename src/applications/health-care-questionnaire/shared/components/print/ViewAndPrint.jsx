920c10bd06870a6bbaf09fe74d1acc5e3e021cfc
Mark Dewey <mdewey@governmentcio.com>
2021-03-29 11:47:20 -0400

    updated QL with screen reader delighters

--- src/applications/health-care-questionnaire/shared/components/print/ViewAndPrint.jsx
+++ src/applications/health-care-questionnaire/shared/components/print/ViewAndPrint.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import moment from 'moment';
 
 export default function ViewAndPrint({
   displayArrow = true,
   onClick = () => {},
+  facilityName,
+  appointmentTime,
 }) {
   return (
     <button
       className="usa-button va-button view-and-print-button"
       onClick={onClick}
+      aria-label={`Download your response to the questionnaire you submitted for your appointment at ${facilityName} that was on ${moment(
+        appointmentTime,
+      ).format('MMMM, D, YYYY')}`}
     >
       View and print questions
       {displayArrow && <i className={`fa fa-chevron-right`} />}
