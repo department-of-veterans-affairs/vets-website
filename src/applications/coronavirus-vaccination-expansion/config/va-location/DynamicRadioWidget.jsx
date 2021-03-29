a583b11bfd5c697eadb5ae22eb7eae99fcb009dc
Jesse Cohn <jesse.cohn@teamadhoc.us>
2021-03-28 17:39:14 -0700

    Updated alert content

--- src/applications/coronavirus-vaccination-expansion/config/va-location/DynamicRadioWidget.jsx
+++ src/applications/coronavirus-vaccination-expansion/config/va-location/DynamicRadioWidget.jsx
@@ -18,6 +18,19 @@ export function DynamicRadioWidget(props) {
   const [error, setError] = useState(false); // app starts with no error
   const [selected, setSelected] = useState(null); // app starts with no error
 
+  const alertContent = (
+    <>
+      <p>
+        We're sorry. We're having trouble finding medical centers for you to
+        choose from right now.
+      </p>
+      <p>
+        We'll match you with the closest medical center based on the address you
+        provided. They’ll contact you when they have a vaccine for you.
+      </p>
+    </>
+  );
+
   useEffect(
     () => {
       // how sure are we that people will always enter 5 digits for their zipcode?
@@ -36,6 +49,7 @@ export function DynamicRadioWidget(props) {
     },
     [props.zipcode],
   );
+
   if (loading === true) {
     locationsList = (
       <LoadingIndicator message="Loading VA medical centers near you..." />
@@ -71,11 +85,7 @@ export function DynamicRadioWidget(props) {
   ) {
     // there are no locations returned or there is an error
     locationsList = (
-      <AlertBox
-        content="based on the information you provided we will identify a facility near your location for vaccination."
-        headline="Alert title"
-        status="info"
-      />
+      <AlertBox content={alertContent} headline="Alert title" status="info" />
     );
   }
   return <>{locationsList}</>;
