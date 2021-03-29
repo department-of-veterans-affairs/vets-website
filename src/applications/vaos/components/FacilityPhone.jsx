ec7f1cb52276fbd47aa647227d7171ba5beee740
Tony Williams <tony.williams@va.gov>
2021-03-29 09:51:44 -0500

    Modified to use Telephone component for TTY number

--- src/applications/vaos/components/FacilityPhone.jsx
+++ src/applications/vaos/components/FacilityPhone.jsx
@@ -8,5 +8,14 @@ export default function FacilityPhone({ contact }) {
 
   const [number, extension] = contact.split('x');
 
-  return <Telephone contact={number} extension={extension} />;
+  return (
+    <>
+      <Telephone contact={number} extension={extension} />
+      <br />
+      <h4 className="vads-u-font-family--sans vads-u-display--inline vads-u-font-size--base">
+        TTY:
+      </h4>{' '}
+      <Telephone contact="711" />
+    </>
+  );
 }
