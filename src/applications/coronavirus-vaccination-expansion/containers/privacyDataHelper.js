ff607190c890b585a6167c0e9b368cd54ee46083
Patrick Bateman <batemapf@gmail.com>
2021-03-29 14:23:39 -0400

    privacy act update

--- src/applications/coronavirus-vaccination-expansion/containers/privacyDataHelper.js
+++ src/applications/coronavirus-vaccination-expansion/containers/privacyDataHelper.js
@@ -6,16 +6,23 @@ export const modalContents = (
     <p>
       <strong>Privacy Act Notice:</strong> Public Law 117-4 authorizes VA to
       provide COVID-19 vaccines to certain eligible Veterans, spouses of
-      eligible Veterans, certain caregivers of Veterans, and certain receipients
+      eligible Veterans, certain caregivers of Veterans, and certain recipients
       of CHAMPVA (Civilian Health and Medical Program of the Department of
-      Veterans Affairs) benefits. This form will be used to collect certain
-      information from interested persons interested in receiving a COVID-19
-      vaccine from VA and determine eligibility for a COVID-19 vaccine
-      administered by VA. If eligibility is determined, a medical may be created
-      if the applicant does not have an existing medical record. The responses
-      you submit are considered confidential (38 U.S.C. 5701). Any information
-      provided by applicants, recipients, and others may be subject to
-      verification through computer matching programs with other agencies.
+      Veterans Affairs) benefits. VA is asking you to provide the information on
+      this form in order for VA to determine your eligibility for vaccination
+      under the law. This form will be used to collect information from certain
+      persons interested in receiving a COVID-19 vaccine from VA and to
+      determine eligibility for a COVID-19 vaccine administered by VA. Providing
+      the requested information is voluntary, but if any or all of the requested
+      information is not provided, it may delay or result in denial of your
+      request for vaccination. If you provide VA your Social Security Number, VA
+      will use it to administer this benefit. Any information provided by
+      applicants, recipients, and others may be subject to verification through
+      computer matching programs with other agencies. VA may disclose the
+      information that you put on the form as permitted by law. VA may make a
+      “routine use” disclosure of the information as outlined in the Privacy Act
+      systems of records notices and in accordance with the VHA Notice of
+      Privacy Practices.
     </p>
   </div>
 );
