c66f39cf4740d08fbd3e33953589d06af65bbcd5
Craig Wheeler <craig@craigwheeler.net>
2021-03-29 11:59:00 -0400

    combine tests

--- src/applications/financial-status-report/tests/e2e/fsr-5655-wizard.cypress.spec.js
+++ src/applications/financial-status-report/tests/e2e/fsr-5655-wizard.cypress.spec.js
@@ -1,5 +1,4 @@
 import { WIZARD_STATUS } from '../../wizard/constants';
-import manifest from '../../manifest.json';
 
 Cypress.config('waitForAnimations', true);
 
@@ -10,23 +9,17 @@ Cypress.Commands.add('checkStorage', (key, expectedValue) => {
 });
 
 describe('Financial Status Report (Wizard)', () => {
-  beforeEach(() => {
+  before(() => {
     window.dataLayer = [];
     sessionStorage.removeItem(WIZARD_STATUS);
-    cy.visit(manifest.rootUrl);
+    cy.visit('/manage-va-debt/request-debt-help-5655');
     cy.injectAxe();
-  });
-
-  it('should show the wizard', () => {
-    const heading = 'Is this the form I need?';
-    cy.url().should('include', manifest.rootUrl);
-    cy.get('.wizard-heading').should('have.text', heading);
-    cy.axeCheck();
   });
 
   it('should navigate the wizard and start the form', () => {
     const title = 'Request help with VA debt (VA Form 5655)';
-    cy.url().should('include', manifest.rootUrl);
+    const heading = 'Is this the form I need?';
+    cy.get('.wizard-heading').should('have.text', heading);
     cy.get('[type="radio"][value="request"]').click();
     cy.get('[type="radio"][value="recipients"]').click();
     cy.get('[type="radio"][value="veteran"]').click();
