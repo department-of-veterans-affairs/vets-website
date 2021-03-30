257e9d7554f831405c72881ecfbdc0ed7f0f1c33
Mark Dewey <mdewey@governmentcio.com>
2021-03-30 09:56:25 -0400

    fixed test

--- src/applications/health-care-questionnaire/list/questionnaire-list/components/Shared/Buttons/tests/answer.button.unit.spec.jsx
+++ src/applications/health-care-questionnaire/list/questionnaire-list/components/Shared/Buttons/tests/answer.button.unit.spec.jsx
@@ -6,7 +6,13 @@ import AnswerQuestions from '../AnswerQuestions';
 
 describe('health care questionnaire list - shows view and print button', () => {
   it('show continue progress', () => {
-    const component = mount(<AnswerQuestions status="in-progress" />);
+    const component = mount(
+      <AnswerQuestions
+        status="in-progress"
+        facilityName="Magic Kingdom"
+        appointmentTime="2021-10-31T17:00:00Z"
+      />,
+    );
     expect(component.exists('[data-testid="button-text"]')).to.be.true;
     expect(component.find('[data-testid="button-text"]').text()).to.equal(
       'Continue questions',
@@ -15,12 +21,17 @@ describe('health care questionnaire list - shows view and print button', () => {
     expect(
       component.find('[data-testid="answer-button"]').prop('aria-label'),
     ).to.equal(
-      'Select to continue your pre-appointment questionnaire for your primary care visit at undefined on March, 29, 2021',
+      'Select to continue your pre-appointment questionnaire for your primary care visit at Magic Kingdom on October, 31, 2021',
     );
     component.unmount();
   });
   it('show start questions', () => {
-    const component = mount(<AnswerQuestions />);
+    const component = mount(
+      <AnswerQuestions
+        facilityName="Magic Kingdom"
+        appointmentTime="2021-10-31T17:00:00Z"
+      />,
+    );
     expect(component.exists('[data-testid="button-text"]')).to.be.true;
     expect(component.find('[data-testid="button-text"]').text()).to.equal(
       'Answer questions',
@@ -29,7 +40,7 @@ describe('health care questionnaire list - shows view and print button', () => {
     expect(
       component.find('[data-testid="answer-button"]').prop('aria-label'),
     ).to.equal(
-      'Select to start your pre-appointment questionnaire for your primary care visit at undefined on March, 29, 2021',
+      'Select to start your pre-appointment questionnaire for your primary care visit at Magic Kingdom on October, 31, 2021',
     );
     component.unmount();
   });
