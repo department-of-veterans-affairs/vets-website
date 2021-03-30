23ca0175bc983581f65f9b7b30b928737aed38a3
Tim Wright <timwright12@gmail.com>
2021-03-30 16:00:58 -0400

    Adding prep work for Review instance updates

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
 set -e
 yarn install --production=false
 npm run fetch-drupal-cache
+[ ! -d "../content-build" ] && echo "Content build does not exist!"
 npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
-npm run heroku-serve -- build/localhost -p 3001
+[ -d "../content-build" ] && cd ../content-build && npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+[ ! -d "../content-build" ] && npm run heroku-serve -- build/localhost -p 3001
+[ -d "../content-build" ] && cd ../content-build && npm run heroku-serve -- build/localhost -p 3002
