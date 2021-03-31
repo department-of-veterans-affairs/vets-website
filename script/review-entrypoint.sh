a6c7a9bb9a55603012e608f8da59f7beb3e4bfb5
Theo Bentum <theo@adhocteam.us>
2021-03-31 15:23:51 -0700

    Update RI script

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
 set -e
 yarn install --production=false
 npm run fetch-drupal-cache
+[ ! -d "../content-build" ] && exit 1
 npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
-npm run heroku-serve -- build/localhost -p 3001
+[ -d "../content-build" ] && cd ../content-build && npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+[ ! -d "../content-build" ] && npm run heroku-serve -- build/localhost -p 3001
+[ -d "../content-build" ] && cd ../content-build && npm run heroku-serve -- build/localhost -p 3002
