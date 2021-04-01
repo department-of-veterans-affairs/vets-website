fc816bf7398fb1605b8fa228cbd5f70cb9c75de8
Theo Bentum <theo@adhocteam.us>
2021-04-01 12:44:27 -0700

    Run review instances normally

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
 set -e
-# yarn install --production=false
-# npm run fetch-drupal-cache
-# npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
-# npm run heroku-serve -- build/localhost -p 3001
+yarn install --production=false
+npm run fetch-drupal-cache
+npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+npm run heroku-serve -- build/localhost -p 3001
 
 if [ -d "../content-build" ]
 then
+  cd ../content-build
   yarn install --production=false
-  npm run fetch-drupal-cache
   npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
-  npm run heroku-serve -- build/localhost -p 3001
-  # cd ../content-build
-  # yarn install --production=false
-  # npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
-  # npm run heroku-serve -- build/localhost -p 3002
+  npm run heroku-serve -- build/localhost -p 3002
 else
   echo "Directory ../content-build does not exists."
 fi
