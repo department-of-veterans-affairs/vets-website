77874f32f7a717a0bbc7c2db5e08da5c5106fd4a
Theo Bentum <theo@adhocteam.us>
2021-04-01 12:16:39 -0700

    Run buld.sh snd serve if content-build directory exists

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -6,14 +6,16 @@ set -e
 # npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
 # npm run heroku-serve -- build/localhost -p 3001
 
-if [ -d "../vets-api" ] 
+if [ -d "../content-build" ]
 then
   yarn install --production=false
   npm run fetch-drupal-cache
   npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
   npm run heroku-serve -- build/localhost -p 3001
-  # cd ../content-build && npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' && npm run heroku-serve -- build/localhost -p 3002
+  # cd ../content-build
+  # yarn install --production=false
+  # npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+  # npm run heroku-serve -- build/localhost -p 3002
 else
   echo "Directory ../content-build does not exists."
-  # npm run heroku-serve -- build/localhost -p 3001
 fi
