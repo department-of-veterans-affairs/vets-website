3c1888894b07f5a8e967b6743e41dc60e000c88f
Theo Bentum <theo@adhocteam.us>
2021-04-01 13:49:44 -0700

    Refactor and use if statement for content build commands

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-echo "Review Entrypoint Script"
 set -e
 yarn install --production=false
 npm run fetch-drupal-cache
-[ ! -d "../content-build" ] && echo "Content build does not exist!"
 npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
-[ -d "../content-build" ] && cd ../content-build && npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
-[ ! -d "../content-build" ] && npm run heroku-serve -- build/localhost -p 3001
-[ -d "../content-build" ] && cd ../content-build && npm run heroku-serve -- build/localhost -p 3002
+npm run heroku-serve -- build/localhost -p 3001
+
+if [ -d "../content-build" ]
+then
+  cd ../content-build
+  yarn install --production=false
+  npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+  npm run heroku-serve -- build/localhost -p 3002
+else
+  echo "Content build does not exist!"
+fi
