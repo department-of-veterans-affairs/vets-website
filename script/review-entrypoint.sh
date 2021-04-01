d4eeb419b43a6a307bf25ccf87ce9dc467bf8374
Theo Bentum <theo@adhocteam.us>
2021-04-01 11:36:59 -0700

    Build if vets-api directory exists

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 set -e
-yarn install --production=false
-npm run fetch-drupal-cache
-npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
-npm run heroku-serve -- build/localhost -p 3001
+# yarn install --production=false
+# npm run fetch-drupal-cache
+# npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+# npm run heroku-serve -- build/localhost -p 3001
 
 if [ -d "../vets-api" ] 
 then
-  # yarn install --production=false
-  # npm run fetch-drupal-cache
-  # npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+  yarn install --production=false
+  npm run fetch-drupal-cache
+  npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+  npm run heroku-serve -- build/localhost -p 3001
   # cd ../content-build && npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' && npm run heroku-serve -- build/localhost -p 3002
 else
   echo "Directory ../content-build does not exists."
