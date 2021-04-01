41f23aadb29af33bdf8a49bd1cb042ed85fad256
Devin McCurdy <mccurdy_devin@bah.com>
2021-04-01 15:48:52 -0400

    Update confirmation page

--- src/applications/edu-benefits/1990s/containers/ConfirmationPage.jsx
+++ src/applications/edu-benefits/1990s/containers/ConfirmationPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import moment from 'moment';
 import { connect } from 'react-redux';
 import Scroll from 'react-scroll';
 
 import { focusElement } from 'platform/utilities/ui';
+
+import { ConfirmationPageContent } from '../../components/ConfirmationPageContent';
 
 const scroller = Scroll.scroller;
 const scrollToTop = () => {
@@ -19,45 +20,18 @@ export class ConfirmationPage extends React.Component {
     focusElement('.schemaform-title > h1');
     scrollToTop();
   }
-
   render() {
-    const { submission, data } = this.props.form;
-    const { response } = submission;
-    const name = data.veteranFullName;
+    const form = this.props.form;
+    const { submission } = form;
 
     return (
-      <div>
-        <h3 className="confirmation-page-title">Claim received</h3>
-        <p>
-          We usually process claims within <strong>a week</strong>.
-        </p>
-        <p>
-          We may contact you for more information or documents.
-          <br />
-          <i>Please print this page for your records.</i>
-        </p>
-        <div className="inset">
-          <h4>
-            Veteran Rapid Retraining Assistance Program Claim{' '}
-            <span className="additional">(Form VRRAP)</span>
-          </h4>
-          {name ? (
-            <span>
-              for {name.first} {name.middle} {name.last} {name.suffix}
-            </span>
-          ) : null}
-
-          {response ? (
-            <ul className="claim-list">
-              <li>
-                <strong>Date received</strong>
-                <br />
-                <span>{moment(response.timestamp).format('MMM D, YYYY')}</span>
-              </li>
-            </ul>
-          ) : null}
-        </div>
-      </div>
+      <ConfirmationPageContent
+        formId="VRRAP"
+        submission={submission}
+        printHeader="Apply for the Veterans Rapid Retraining Assistance Program"
+        formName="Veterans Rapid Retraining Assistance Program"
+        name={form.data.veteranFullName}
+      />
     );
   }
 }
