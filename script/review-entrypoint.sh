1d083daacc6999c4fd761fe10f82fc367bc21522
Theo Bentum <theo@adhocteam.us>
2021-04-01 09:37:30 -0700

    Run RI script normally

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 set -e
-# yarn install --production=false
-# npm run fetch-drupal-cache
-# npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+yarn install --production=false
+npm run fetch-drupal-cache
+npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+npm run heroku-serve -- build/localhost -p 3001
 
 if [ -d "../vets-api" ] 
 then
-  yarn install --production=false
-  npm run fetch-drupal-cache
-  npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+  # yarn install --production=false
+  # npm run fetch-drupal-cache
+  # npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
   # cd ../content-build && npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' && npm run heroku-serve -- build/localhost -p 3002
 else
   echo "Directory ../content-build does not exists."
