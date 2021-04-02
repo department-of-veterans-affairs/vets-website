397c54c9c4dc62f772b502f07a0cb268b2c03be8
Mark Dewey <mdewey@governmentcio.com>
2021-04-02 09:14:46 -0400

    removed testinga artifact

--- src/applications/health-care-questionnaire/questionnaire/tests/e2e/05.reason.for.visit.bug.cypress.spec.js
+++ src/applications/health-care-questionnaire/questionnaire/tests/e2e/05.reason.for.visit.bug.cypress.spec.js
@@ -4,35 +4,33 @@ import featureToggles from './fixtures/mocks/feature-toggles.enabled.json';
 
 import { setSessionStorage } from '../../../shared/test-data/e2e/session.storage.mock';
 
-for (let index = 0; index < 100; index++) {
-  describe('health care questionnaire -- reason for visit --', () => {
-    beforeEach(() => {
-      cy.intercept('GET', '/v0/feature_toggles*', featureToggles);
-      cy.login(basicUser);
-      disableFTUXModals();
-      cy.window().then(window => {
-        const apptId =
-          'I2-3PYJBEU2DIBW5RZT2XI3PASYGM7YYRD5TFQCLHQXK6YBXREQK5VQ0005';
-        setSessionStorage(window, apptId);
-        cy.visit(
-          `/health-care/health-questionnaires/questionnaires/answer-questions?id=${apptId}&skip`,
-        );
-      });
-    });
-
-    it('can be cleared', () => {
-      cy.title().should('contain', 'Health care Questionnaire');
-      cy.get('.schemaform-title>h1').contains(
-        'Answer primary care questionnaire',
+describe('health care questionnaire -- reason for visit --', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/v0/feature_toggles*', featureToggles);
+    cy.login(basicUser);
+    disableFTUXModals();
+    cy.window().then(window => {
+      const apptId =
+        'I2-3PYJBEU2DIBW5RZT2XI3PASYGM7YYRD5TFQCLHQXK6YBXREQK5VQ0005';
+      setSessionStorage(window, apptId);
+      cy.visit(
+        `/health-care/health-questionnaires/questionnaires/answer-questions?id=${apptId}&skip`,
       );
-      cy.get('.va-button').click({ waitForAnimations: true });
-      cy.login(basicUser);
-
-      cy.get('#2-continueButton').click({ waitForAnimations: true });
-      cy.login(basicUser);
-
-      cy.get('#root_reasonForVisitDescription').clear();
-      cy.get('#root_reasonForVisitDescription').should('be.empty');
     });
   });
-}
+
+  it('can be cleared', () => {
+    cy.title().should('contain', 'Health care Questionnaire');
+    cy.get('.schemaform-title>h1').contains(
+      'Answer primary care questionnaire',
+    );
+    cy.get('.va-button').click({ waitForAnimations: true });
+    cy.login(basicUser);
+
+    cy.get('#2-continueButton').click({ waitForAnimations: true });
+    cy.login(basicUser);
+
+    cy.get('#root_reasonForVisitDescription').clear();
+    cy.get('#root_reasonForVisitDescription').should('be.empty');
+  });
+});
