14a40d19bf1f3f63554571d0dfb0195b2dbc2d37
Theo Bentum <theo@adhocteam.us>
2021-04-02 10:46:47 -0700

    test cd into other directories

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 yarn install --production=false
 npm run fetch-drupal-cache
 npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
-npm run heroku-serve -- build/localhost -p 3001
 
 if [ -d "../content-build" ]
 then
@@ -14,4 +13,8 @@ then
   npm run heroku-serve -- build/localhost -p 3002
 else
   echo "Directory ../content-build does not exists."
+  npm run heroku-serve -- build/localhost -p 3001
 fi
+
+cd ../vagov-content
+pwd
