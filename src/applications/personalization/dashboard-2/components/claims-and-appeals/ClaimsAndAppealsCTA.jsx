36bc3f334d77e72b9a5aac4e8ce783c442b9d8db
Erik P. Hansen <eph@erikphansen.com>
2021-04-05 14:44:37 -0700

    remove column layout classes from claims CTA

--- src/applications/personalization/dashboard-2/components/claims-and-appeals/ClaimsAndAppealsCTA.jsx
+++ src/applications/personalization/dashboard-2/components/claims-and-appeals/ClaimsAndAppealsCTA.jsx
@@ -4,13 +4,11 @@ import IconCTALink from '../IconCTALink';
 
 const ClaimsAndAppealsCTA = () => {
   return (
-    <div className="vads-l-col--12 medium-screen:vads-l-col--6 small-desktop-screen:vads-l-col--4 medium-screen:vads-u-padding-right--3">
-      <IconCTALink
-        text="Manage all your claims and appeals"
-        href="claim-or-appeal-status/"
-        icon="clipboard"
-      />
-    </div>
+    <IconCTALink
+      text="Manage all your claims and appeals"
+      href="claim-or-appeal-status/"
+      icon="clipboard"
+    />
   );
 };
 
