dbfb8b18700dd02bedd87cbe59d5403b01e4ad75
linuxlance <LanceGundersen@users.noreply.github.com>
2021-04-05 15:04:28 -0400

    Fix transformer appt check

--- src/applications/vaos/services/appointment/transformers.js
+++ src/applications/vaos/services/appointment/transformers.js
@@ -119,9 +119,10 @@ function getRequestStatus(request, isExpressCare) {
  * @returns {String} Appointment status
  */
 function getConfirmedStatus(appointment, isPast) {
-  const currentStatus = APPOINTMENT_TYPES.ccAppointment
-    ? appointment.vdsAppointments?.[0]?.currentStatus
-    : getVistaStatus(appointment);
+  const currentStatus =
+    getAppointmentType(appointment) === APPOINTMENT_TYPES.ccAppointment
+      ? appointment.vdsAppointments?.[0]?.currentStatus
+      : getVistaStatus(appointment);
 
   if (
     (isPast && PAST_APPOINTMENTS_HIDE_STATUS_SET.has(currentStatus)) ||
