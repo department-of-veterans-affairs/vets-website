8dc331d197b9bc93541641ad81ad1e2a73226c7d
Mark Dewey <mdewey@governmentcio.com>
2021-04-06 15:51:40 -0400

    updated form tracking prefix

--- src/applications/health-care-questionnaire/questionnaire/config/form.js
+++ src/applications/health-care-questionnaire/questionnaire/config/form.js
@@ -24,7 +24,7 @@ const formConfig = {
   rootUrl: manifest.rootUrl,
   urlPrefix: '/',
   submitUrl: `${environment.API_URL}/health_quest/v0/questionnaire_responses`,
-  trackingPrefix: 'hcq',
+  trackingPrefix: 'hcq-',
   introduction: IntroductionPage,
   confirmation: ConfirmationPage,
   downtime: {
