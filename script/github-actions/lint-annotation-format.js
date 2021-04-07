27f3905e6d6cc86d1836368310eabf5c8394f4fb
Oseas Moran <oseas.moran@adhocteam.us>
2021-04-07 14:37:41 -0700

    custom lint formation for GHA annotation

---
+++ script/github-actions/lint-annotation-format.js
@@ -0,0 +1,44 @@
+module.exports = function(results) {
+  const resultsArr = results || [];
+
+  const summary = resultsArr.reduce(
+    function(seq, current) {
+      current.messages.forEach(function(msg) {
+        const logMessage = {
+          filePath: current.filePath,
+          message: msg.message,
+          line: msg.line,
+          column: msg.column,
+        };
+
+        if (msg.severity === 1) {
+          logMessage.type = 'warning';
+          seq.warnings.push(logMessage);
+        }
+        if (msg.severity === 2) {
+          logMessage.type = 'error';
+          seq.errors.push(logMessage);
+        }
+      });
+      return seq;
+    },
+    {
+      errors: [],
+      warnings: [],
+    },
+  );
+
+  if (summary.errors.length > 0 || summary.warnings.length > 0) {
+    const lines = summary.errors
+      .map(function(msg) {
+        return `\n ::${
+          msg.type
+        } file=${msg.filePath},line=${msg.line},col=${msg.column}::${msg.message}`;
+      })
+      .join('\n');
+
+    return `${lines} \n`;
+  }
+
+  return null;
+};
