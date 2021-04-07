3e24ef2a595b03d94f0042d43949d072cee1c985
Tony Wiliiams <tony.williams@va.gov>
2021-04-07 13:10:39 -0500

    18970 use request limit check when determining which express care window to use (#16625)
    * Initial changes.
    
    * More changes.
    
    * Fix unit tests
    
    * Fixed test.
    
    * Added conditional check.
    
    * Modified per code review.
    
    * Removed dead code.
    
    * Fixed typo

--- src/applications/vaos/services/var/index.js
+++ src/applications/vaos/services/var/index.js
@@ -127,10 +127,14 @@ export function checkPastVisits(
   ).then(parseApiObject);
 }
 
-export function getRequestLimits(facilityId, typeOfCareId) {
-  return apiRequestWithMocks(
-    `/vaos/v0/facilities/${facilityId}/limits?type_of_care_id=${typeOfCareId}`,
-  ).then(parseApiObject);
+export function getRequestLimits(facilityIds, typeOfCareId) {
+  let url = `/vaos/v0/facilities/limits?type_of_care_id=${typeOfCareId}&`;
+  if (Array.isArray(facilityIds)) {
+    url += facilityIds.map(id => `facility_ids[]=${id}`).join('&');
+  } else {
+    url += `facility_ids[]=${facilityIds}`;
+  }
+  return apiRequestWithMocks(url).then(parseApiList);
 }
 
 export function getAvailableClinics(facilityId, typeOfCareId, systemId) {
