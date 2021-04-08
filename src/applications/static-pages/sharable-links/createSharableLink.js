0d86ee6280d1c69176d63965640828afbc58e3e2
Fareez Ahmed <fareezahmed@Fareez-GCIO.local>
2021-04-08 15:21:14 -0700

    add in widget to test and rename files

---
+++ src/applications/static-pages/sharable-links/createSharableLink.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+
+export default function createSharableLink(store, widgetType) {
+  const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
+  const dataEntityId = root.getAttribute('entityId');
+  if (root) {
+    import(/* webpackChunkName: "sharableLink" */
+    './sharableLink').then(module => {
+      const SharableLink = module.default;
+      ReactDOM.render(
+        <Provider store={store}>
+          <SharableLink dataEntityId={dataEntityId} />
+        </Provider>,
+        root,
+      );
+    });
+  }
+}
---
+++ src/applications/static-pages/sharable-links/sharableLink.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const SharableLink = ({ dataEntityId }) => {
+  return (
+    <div>
+      <i
+        className="fas fa-link vads-u-color--primary vads-u-margin-left--1 share-link aria-hidden=true"
+        onClick={event => {
+          // copy link to users clipboard
+          const input = document.createElement('input');
+
+          const copyUrl = window.location.href.replace(
+            window.location.hash,
+            '',
+          );
+          input.setAttribute('value', `${copyUrl}#${dataEntityId}`);
+          document.body.appendChild(input);
+          input.select();
+          const result = document.execCommand('copy');
+          document.body.removeChild(input);
+          // eslint-disable-next-line no-console
+          console.log('COPIED THIS TO CLIPBOARD: ', result);
+          event.target.nextSibling.classList.remove('vads-u-display--none');
+          setTimeout(() => {
+            event.target.nextSibling.classList.add('vads-u-display--none');
+          }, 5000);
+        }}
+      />
+      <span
+        className={`link-copy-feedback vads-u-display--none vads-u-margin-left--1 vads-u-color--primary`}
+      >
+        {' '}
+        Link Copied{' '}
+      </span>
+    </div>
+  );
+};
+
+export default SharableLink;
