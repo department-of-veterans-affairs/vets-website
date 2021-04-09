5749f3291e8b9c8b7857df3d2333228c77da1c3f
Gibran Rodriguez <grodriguez@governmentcio.com>
2021-04-08 20:49:13 -0500

    disable cy test run for a try

--- script/run-cypress-tests-docker.sh
+++ script/run-cypress-tests-docker.sh
@@ -4,5 +4,5 @@ if [ "$(find src -name '*.cypress.spec.js' | wc -l)" -eq 0 ]; then
 else
   export CYPRESS_BASE_URL=http://vets-website:3001
   export CYPRESS_CI=$CI
-  yarn cy:run
+  #yarn cy:run
 fi
