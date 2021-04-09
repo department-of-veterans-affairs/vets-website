94712811766bc7681c982f429c8b37d279cb2489
Mark Dewey <mdewey@governmentcio.com>
2021-04-09 14:34:43 -0400

    pasuing for syn

--- src/applications/health-care-questionnaire/shared/api/index.js
+++ src/applications/health-care-questionnaire/shared/api/index.js
@@ -1,15 +1,19 @@
 import { submit, transformForSubmit } from './submit-questionnaire';
 import { loadQuestionnaires } from './load-questionnaires';
 import environment from 'platform/utilities/environment';
+import { loadPdfData } from './load-pdf';
 
 const USE_MOCK_DATA =
   window.Cypress || environment.isLocalhost() || environment.isStaging();
 
 const loadQuestionnairesCurry = () => loadQuestionnaires(USE_MOCK_DATA);
 const submitCurry = (form, formConfig) => submit(false, form, formConfig);
+const pdfCurry = questionnaireResponseId =>
+  loadPdfData(USE_MOCK_DATA, questionnaireResponseId);
 
 export {
   submitCurry as submit,
   transformForSubmit,
   loadQuestionnairesCurry as loadQuestionnaires,
+  pdfCurry as loadPdfData,
 };
--- src/applications/health-care-questionnaire/shared/components/print/PrintButton.jsx
+++ src/applications/health-care-questionnaire/shared/components/print/PrintButton.jsx
@@ -5,28 +5,52 @@ import PrintErrorMessage from './PrintErrorMessage';
 import recordEvent from 'platform/monitoring/record-event';
 import { TRACKING_PREFIX } from '../../constants/analytics';
 
+import { loadPdfData } from '../../api';
+
 export default function PrintButton({
   displayArrow = true,
   ErrorCallToAction = () => <>Please refresh this page or try again later.</>,
   facilityName,
   appointmentTime,
+  questionnaireResponseId,
 }) {
   const [isError, setIsError] = useState(false);
-  const handleClick = () => {
-    recordEvent({
-      event: `${TRACKING_PREFIX}pdf-printed`,
-    });
-    setIsError(true);
+  const [pdfLink, setPdfLink] = useState(null);
+  const handleClick = async () => {
+    try {
+      const resp = await loadPdfData(questionnaireResponseId);
+      const blob = await resp.blob();
+      // console.log({ blob, resp });
+      const url = URL.createObjectURL(new Blob([blob]), {
+        type: 'application/pdf',
+      });
+      // console.log({ url });
+      setPdfLink(url);
+      recordEvent({
+        event: `${TRACKING_PREFIX}pdf-printed-success`,
+      });
+    } catch (error) {
+      // console.log({ error });
+      recordEvent({
+        event: `${TRACKING_PREFIX}pdf-printed-failed`,
+      });
+      setIsError(true);
+    }
   };
   if (isError) {
     return <PrintErrorMessage CallToAction={ErrorCallToAction} />;
   }
   return (
-    <ViewAndPrint
-      displayArrow={displayArrow}
-      onClick={handleClick}
-      facilityName={facilityName}
-      appointmentTime={appointmentTime}
-    />
+    <>
+      <ViewAndPrint
+        displayArrow={displayArrow}
+        onClick={handleClick}
+        facilityName={facilityName}
+        appointmentTime={appointmentTime}
+      />
+      <a href={pdfLink} className="href" download="pdfName">
+        Download now?
+      </a>
+    </>
   );
 }
