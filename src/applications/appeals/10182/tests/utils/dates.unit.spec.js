bf34a18f9e97bf17861ec4dd838249bcd5f8e908
Robin Garrison <robin.garrison@adhocteam.us>
2021-04-09 16:16:49 -0500

    Fix tests

--- src/applications/appeals/10182/tests/utils/dates.unit.spec.js
+++ src/applications/appeals/10182/tests/utils/dates.unit.spec.js
@@ -7,16 +7,17 @@ import { getDate } from '../../utils/dates';
 describe('getDate', () => {
   it('should return a date string based on starting date & offset', () => {
     expect(getDate()).to.equal(format(new Date(), FORMAT_YMD));
-    expect(
-      getDate({ startDate: new Date('2021-02-10'), offset: { years: -1 } }),
-    ).to.contain('2020-02'); // not checking for 10th, because of DST
 
-    const fullDate = getDate({
-      startDate: new Date('2021-02-10'),
-      offset: { years: -1 },
-      pattern: FORMAT_READABLE,
-    });
-    expect(fullDate).to.contains('February');
-    expect(fullDate).to.contains('2020');
+    const startDate = new Date('2021-02-10 00:00:00');
+    expect(getDate({ startDate, offset: { years: -1 } })).to.contain(
+      '2020-02-10',
+    );
+    expect(
+      getDate({
+        startDate,
+        offset: { years: -1 },
+        pattern: FORMAT_READABLE,
+      }),
+    ).to.contains('February 10, 2020');
   });
 });
