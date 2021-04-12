3d6c345bef6b680a97574867c2a680073a726e90
Oseas Moran <oseas.moran@adhocteam.us>
2021-04-12 14:53:20 -0700

    use forEach on data

--- script/github-actions/sasslint-annotation-format.js
+++ script/github-actions/sasslint-annotation-format.js
@@ -1,30 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-const sasslintData = JSON.parse(
+const sassLintData = JSON.parse(
   fs.readFileSync(path.join(__dirname, '../../sasslint-report.json')),
 );
 
-for (let i = 0; i < sasslintData.length; i++) {
-  if (sasslintData[i].warningCount > 0 || sasslintData[i].errorCount > 0) {
-    for (let j = 0; j < sasslintData[i].messages.length; j++) {
-      let output = '';
+sassLintData.forEach(fileMessages => {
+  if (!fileMessages.warningCount && !fileMessages.errorCount) return;
 
-      if (sasslintData[i].messages[j].severity === 0) {
-        output += '::error ';
-      } else {
-        output += '::warning ';
-      }
-
-      output += `file=${sasslintData[i].filePath},line=${
-        sasslintData[i].messages[j].line
-      },col=${sasslintData[i].messages[j].column}::${
-        sasslintData[i].filePath
-      }:${sasslintData[i].messages[j].line}:${
-        sasslintData[i].messages[j].column
-      }:${sasslintData[i].messages[j].message}`;
-
-      console.log(output); // eslint-disable-line no-console
-    }
-  }
-}
+  fileMessages.messages.forEach(fileMessage => {
+    const { severity, column, line, message } = fileMessage;
+    const filePath = fileMessages.filePath;
+    const severityTag = severity === 0 ? '::error' : '::warning';
+    const output = `${severityTag} file=${filePath},line=${line},col=${column}::${filePath}:${line}:${column}:${message}`;
+    console.log(output); // eslint-disable-line no-console
+  });
+});
