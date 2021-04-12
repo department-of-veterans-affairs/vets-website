9b3c6fd967d48767aa65f3e601779f202f6a63b9
Fareez Ahmed <fareezahmed@Fareez-GCIO.local>
2021-04-12 09:25:22 -0700

    save changes

--- src/applications/static-pages/sharable-links/sharableLink.js
+++ src/applications/static-pages/sharable-links/sharableLink.js
@@ -7,20 +7,16 @@ const copyToUsersClipBoard = dataEntityId => {
   input.setAttribute('value', `${copyUrl}#${dataEntityId}`);
   document.body.appendChild(input);
   input.select();
-  const result = document.execCommand('copy');
+  // const result = document.execCommand('copy');
   document.body.removeChild(input);
-  // eslint-disable-next-line no-console
-  console.log(
-    'COPIED THIS TO CLIPBOARD: ',
-    result,
-    copyUrl,
-    dataEntityId,
-    input,
-  );
+  // console.log('COPIED THIS TO CLIPBOARD: ', result);
 };
 
 const SharableLink = ({ dataEntityId }) => {
+  // eslint-disable-next-line no-console
+  console.log(dataEntityId, 'THE DATA ENTITY ID');
   const [feedbackActive, setFeedbackActive] = useState(false);
+  const [copiedText] = useState('Link copied');
   return (
     <span>
       <i
@@ -39,13 +35,13 @@ const SharableLink = ({ dataEntityId }) => {
           setTimeout(() => {
             event.target.nextSibling.classList.add('vads-u-display--none');
             setFeedbackActive(false);
-          }, 500000);
+          }, 10000);
         }}
       />
       <span
         className={`link-copy-feedback vads-u-display--none vads-u-margin-left--0.5`}
       >
-        Link Copied
+        {copiedText}
       </span>
     </span>
   );
