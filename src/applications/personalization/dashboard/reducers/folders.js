e8d94599126c43db5c6dcabdbdefe1088f084292
Sandra Hallie <sandra.adamshallie@gmail.com>
2021-04-13 10:44:00 -0600

    Fix fetching messages loading state

--- src/applications/personalization/dashboard/reducers/folders.js
+++ src/applications/personalization/dashboard/reducers/folders.js
@@ -98,6 +98,7 @@ export default function folders(state = initialState, action) {
             visible: false,
           },
           lastRequestedFolder: action.request,
+          fetching: true,
         },
         newState,
       );
