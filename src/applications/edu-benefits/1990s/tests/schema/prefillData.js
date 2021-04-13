818907a714699b896cf1adf2cc637adc5d57f33e
Matt Roth <roth_matthew@bah.com>
2021-04-13 11:24:15 -0400

    add optional chaining for bank account

---
+++ src/applications/edu-benefits/1990s/tests/schema/prefillData.js
@@ -0,0 +1,79 @@
+export const minPrefillData = {
+  veteranFullName: {
+    first: 'Greg',
+    middle: 'A',
+    last: 'Anderson',
+  },
+  veteranSocialSecurityNumber: '796121200',
+};
+
+export const minTransformedPrefillData = {
+  'view:applicantInformation': {
+    veteranFullName: {
+      first: 'Greg',
+      middle: 'A',
+      last: 'Anderson',
+    },
+    veteranSocialSecurityNumber: '796121200',
+  },
+};
+
+export const maxPrefillData = {
+  veteranFullName: {
+    first: 'Greg',
+    middle: 'A',
+    last: 'Anderson',
+  },
+  veteranSocialSecurityNumber: '796121200',
+  email: 'test@email.com',
+  homePhone: '5551234567',
+  mobilePhone: '5551112222',
+  address: {
+    street: 'MILITARY ADDY 3',
+    street2: 'ADDRESS LINE 2',
+    city: 'DPO',
+    country: 'USA',
+    state: 'MI',
+    postalCode: '22312',
+  },
+  dateOfBirth: '1979-01-01',
+  bankAccount: {
+    accountNumber: '88888888888',
+    accountType: 'checking',
+    routingNumber: '123456789',
+  },
+};
+
+export const transformedMaxPrefillData = {
+  'view:applicantInformation': {
+    veteranFullName: {
+      first: 'Greg',
+      middle: 'A',
+      last: 'Anderson',
+    },
+    veteranSocialSecurityNumber: '796121200',
+    dateOfBirth: '1979-01-01',
+  },
+  'view:contactInformation': {
+    'view:phoneAndEmail': {
+      email: 'test@email.com',
+      alternatePhone: '5551234567',
+      mobilePhone: '5551112222',
+    },
+    address: {
+      street: 'MILITARY ADDY 3',
+      street2: 'ADDRESS LINE 2',
+      city: 'DPO',
+      country: 'USA',
+      state: 'MI',
+      postalCode: '22312',
+    },
+  },
+  'view:directDeposit': {
+    bankAccount: {
+      accountNumber: '88888888888',
+      accountType: 'checking',
+      routingNumber: '123456789',
+    },
+  },
+};
