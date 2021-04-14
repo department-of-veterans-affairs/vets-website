4a90707094f1d587dc6faae8f9ae0be4cef6694c
Rob Garrison <wowmotty@gmail.com>
2021-04-14 12:00:10 -0500

    NOD: Add various pages (#16779)
    * Update intro page content
    
    * Use action links to start form
    
    * Add homeless page
    
    * Add rep pages
    
    * Cleanup
    
    * Add board review page
    
    * Add basic eligible issue page
    
    * Update registry with new path
    
    * Fix unit test

---
+++ src/applications/appeals/10182/content/OptIn.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const OptInTitle = (
+  <strong>
+    I understand that if any issues I’ve selected are from the legacy appeal
+    process, I’m opting them into the new decision review process.
+  </strong>
+);
+
+export const optInDescription = `By checking this box, you’re withdrawing any
+  issue you’ve selected from the legacy appeals process (this is the process for
+  decisions received before February 19, 2019). Instead, the Board will consider
+  your claim for this condition under the new process.`;
+
+export const optInErrorMessage =
+  'Please opt into the new decision review process to proceed';
