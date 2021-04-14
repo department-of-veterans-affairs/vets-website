ca3e10018547d67374ccbf077d46668ab5742861
Mark Dewey <mdewey@governmentcio.com>
2021-04-14 15:54:48 -0400

    added more tests

--- src/applications/health-care-questionnaire/shared/components/print/tests/utils-tests/print.and.open.pdf.unit.spec.jsx
+++ src/applications/health-care-questionnaire/shared/components/print/tests/utils-tests/print.and.open.pdf.unit.spec.jsx
@@ -95,7 +95,71 @@ describe('health care questionnaire -- utils -- print pdf -- browser methods are
       expect(click.called).to.be.true;
     });
   });
-  describe('mobile safari', () => {});
+  describe('mobile safari', () => {
+    it('happy path', () => {
+      const blob = 'hello, there';
+      const createObjectURL = sinon.stub().returns('blob:http://blob.url');
+      const revokeObjectURL = sinon.spy();
+      const click = sinon.spy();
+      const appendChild = sinon.spy();
+      const removeChild = sinon.spy();
+      const newWindow = {
+        location: {
+          href: '',
+        },
+      };
+      const open = sinon.stub().returns(newWindow);
+      const window = {
+        navigator: {
+          userAgent:
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1',
+        },
+        open,
+        document: {
+          body: {
+            appendChild,
+            removeChild,
+          },
+          createElement: () => {
+            return {
+              href: '',
+              target: '',
+              click,
+            };
+          },
+        },
+        URL: {
+          createObjectURL,
+          revokeObjectURL,
+        },
+      };
+
+      openPdfInNewWindow(window, blob);
+
+      // url is created
+      expect(createObjectURL.called).to.be.true;
+      expect(
+        createObjectURL.calledWith(blob, {
+          type: 'application/pdf',
+        }),
+      );
+
+      // that url is revoked
+      expect(revokeObjectURL.called).to.be.true;
+
+      // the a is added/removed from DOM
+      expect(appendChild.called).to.be.true;
+      expect(removeChild.called).to.be.true;
+
+      // that an tag gets created with the details
+      const anchor = appendChild.args[0][0];
+      expect(anchor.href).to.equal('blob:http://blob.url');
+      // that the a is clicked
+      expect(click.called).to.be.true;
+
+      expect(newWindow.location.href).to.equal('blob:http://blob.url');
+    });
+  });
   describe('uses which URL', () => {
     it('window uses URL', () => {});
     it('window uses webkitURL', () => {});
