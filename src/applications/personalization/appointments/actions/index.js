22c75e5201bb03341a92082565cbbb11ca995518
Sandra Hallie <sandra.adamshallie@gmail.com>
2021-04-14 10:56:43 -0600

    Add enddate to appointments query (#16787)

--- src/applications/personalization/appointments/actions/index.js
+++ src/applications/personalization/appointments/actions/index.js
@@ -84,17 +84,23 @@ export function fetchConfirmedFutureAppointments() {
       .startOf('day')
       .toISOString();
 
+    // Maximum number of days you can schedule an appointment in advance in VAOS
+    const endDate = moment()
+      .add(395, 'days')
+      .startOf('day')
+      .toISOString();
+
     try {
       if (environment.isLocalhost() && !window.Cypress) {
         vaAppointments = MOCK_VA_APPOINTMENTS;
         ccAppointments = MOCK_CC_APPOINTMENTS;
       } else {
         vaAppointments = await apiRequest(
-          `/appointments?start_date=${startOfToday}&type=va`,
+          `/appointments?start_date=${startOfToday}&end_date=${endDate}&type=va`,
           { apiVersion: 'vaos/v0' },
         );
         ccAppointments = await apiRequest(
-          `/appointments?start_date=${startOfToday}&type=cc`,
+          `/appointments?start_date=${startOfToday}&end_date=${endDate}&type=cc`,
           { apiVersion: 'vaos/v0' },
         );
       }
