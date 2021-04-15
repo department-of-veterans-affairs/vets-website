378aca5089967112f8c2ad93c1ac141a74b60f57
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2021-04-15 19:22:06 -0400

    Refactor and more logging.

--- src/platform/lambdas/content-cache.js
+++ src/platform/lambdas/content-cache.js
@@ -8,6 +8,9 @@ const getDrupalClient = require('../../site/stages/build/drupal/api');
 const DRUPAL_ADDRESS =
   'http://internal-dsva-vagov-prod-cms-2000800896.us-gov-west-1.elb.amazonaws.com';
 
+const S3_BUCKET = 'vetsgov-website-builds-s3-upload';
+const S3_KEY = 'content-cache/master/pages.json';
+
 exports.handler = async function(event, context) {
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
@@ -36,24 +39,25 @@ exports.handler = async function(event, context) {
     throw new Error('Drupal query returned with errors');
   }
 
+  console.log('Stringifying the data...');
   const pagesString = JSON.stringify(drupalPages, { spaces: 2 });
 
   const s3 = new S3();
-  const bucket = 'vetsgov-website-builds-s3-upload';
-  const key = 'content-cache/master/pages.json';
 
-  const params = {
+  console.log('Uploading the cache...');
+  const request = s3.putObject({
     Body: pagesString,
-    Bucket: bucket,
-    Key: key,
-  };
+    Bucket: S3_BUCKET,
+    Key: S3_KEY,
+  });
 
-  const request = s3.putObject(params);
   let response = null;
 
   try {
     response = await request.promise();
+    console.log('Successfully uploaded the cache!');
   } catch (error) {
+    console.log('Failed to upload the cache.');
     console.error(error);
   }
 
