40c1754ed65d6fb4c0d681fda0259de9b870f712
Theo Bentum <theo@adhocteam.us>
2021-04-15 15:52:25 -0700

    Use application directory in content-build

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -9,7 +9,7 @@ then
   cd ../content-build
   yarn install --production=false
   npm run fetch-drupal-cache
-  npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
+  npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' --apps-directory-name=application
   npm run heroku-serve -- build/localhost -p 3002
 else
   echo "Content build does not exist!"
