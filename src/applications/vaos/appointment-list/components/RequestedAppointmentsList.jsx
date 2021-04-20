005a9e37ed8634831656caa382f742dece6cb879
Jeff Balboni <jeff@adhocteam.us>
2021-04-20 09:58:41 -0400

    Announce view change after type dropdown is used in VAOS (#16748)

--- src/applications/vaos/appointment-list/components/RequestedAppointmentsList.jsx
+++ src/applications/vaos/appointment-list/components/RequestedAppointmentsList.jsx
@@ -14,6 +14,7 @@ import { FETCH_STATUS, GA_PREFIX } from '../../utils/constants';
 import { getVAAppointmentLocationId } from '../../services/appointment';
 import RequestListItem from './AppointmentsPageV2/RequestListItem';
 import NoAppointments from './NoAppointments';
+import { scrollAndFocus } from '../../utils/scrollAndFocus';
 
 function RequestedAppointmentsList({
   showScheduleButton,
@@ -23,43 +24,36 @@ function RequestedAppointmentsList({
   facilityData,
   fetchPendingAppointments,
   startNewAppointmentFlow,
+  hasTypeChanged,
 }) {
   useEffect(
     () => {
       if (pendingStatus === FETCH_STATUS.notStarted) {
         fetchPendingAppointments();
+      } else if (hasTypeChanged && pendingStatus === FETCH_STATUS.succeeded) {
+        scrollAndFocus('#type-dropdown');
+      } else if (hasTypeChanged && pendingStatus === FETCH_STATUS.failed) {
+        scrollAndFocus('h3');
       }
     },
-    [fetchPendingAppointments, pendingStatus],
+    [fetchPendingAppointments, pendingStatus, hasTypeChanged],
   );
 
-  if (pendingStatus === FETCH_STATUS.loading) {
-    return (
-      <div className="vads-u-margin-y--8">
-        <LoadingIndicator message="Loading your appointment requests..." />
-      </div>
-    );
-  } else if (
-    pendingStatus === FETCH_STATUS.succeeded &&
-    pendingAppointments?.length > 0
+  if (
+    pendingStatus === FETCH_STATUS.loading ||
+    pendingStatus === FETCH_STATUS.notStarted
   ) {
     return (
-      // eslint-disable-next-line jsx-a11y/no-redundant-roles
-      <ul
-        role="list"
-        className="vads-u-padding-left--0"
-        data-cy="requested-appointment-list"
-      >
-        {pendingAppointments.map((appt, index) => (
-          <RequestListItem
-            key={index}
-            appointment={appt}
-            facility={facilityData[getVAAppointmentLocationId(appt)]}
-          />
-        ))}
-      </ul>
+      <div className="vads-u-margin-y--8">
+        <LoadingIndicator
+          setFocus={hasTypeChanged}
+          message="Loading your appointment requests..."
+        />
+      </div>
     );
-  } else if (pendingStatus === FETCH_STATUS.failed) {
+  }
+
+  if (pendingStatus === FETCH_STATUS.failed) {
     return (
       <AlertBox status="error" headline="We’re sorry. We’ve run into a problem">
         We’re having trouble getting your appointment requests. Please try again
@@ -69,18 +63,41 @@ function RequestedAppointmentsList({
   }
 
   return (
-    <div className="vads-u-margin-bottom--2 vads-u-background-color--gray-lightest vads-u-padding--2 vads-u-margin-bottom--3">
-      <NoAppointments
-        showScheduleButton={showScheduleButton}
-        isCernerOnlyPatient={isCernerOnlyPatient}
-        startNewAppointmentFlow={() => {
-          recordEvent({
-            event: `${GA_PREFIX}-schedule-appointment-button-clicked`,
-          });
-          startNewAppointmentFlow();
-        }}
-      />
-    </div>
+    <>
+      <div aria-live="assertive" className="sr-only">
+        {hasTypeChanged && 'Showing requested appointments'}
+      </div>
+      {pendingAppointments?.length > 0 && (
+        // eslint-disable-next-line jsx-a11y/no-redundant-roles
+        <ul
+          role="list"
+          className="vads-u-padding-left--0"
+          data-cy="requested-appointment-list"
+        >
+          {pendingAppointments.map((appt, index) => (
+            <RequestListItem
+              key={index}
+              appointment={appt}
+              facility={facilityData[getVAAppointmentLocationId(appt)]}
+            />
+          ))}
+        </ul>
+      )}
+      {pendingAppointments?.length === 0 && (
+        <div className="vads-u-margin-bottom--2 vads-u-background-color--gray-lightest vads-u-padding--2 vads-u-margin-bottom--3">
+          <NoAppointments
+            showScheduleButton={showScheduleButton}
+            isCernerOnlyPatient={isCernerOnlyPatient}
+            startNewAppointmentFlow={() => {
+              recordEvent({
+                event: `${GA_PREFIX}-schedule-appointment-button-clicked`,
+              });
+              startNewAppointmentFlow();
+            }}
+          />
+        </div>
+      )}
+    </>
   );
 }
 
