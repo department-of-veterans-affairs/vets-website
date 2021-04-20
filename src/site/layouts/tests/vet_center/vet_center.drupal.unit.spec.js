1936c062c76a6ab78d72be988fd10c275a3eb34a
Gibran Rodriguez <brangi000@gmail.com>
2021-04-20 18:05:09 -0500

    more unit test

--- src/site/layouts/tests/vet_center/vet_center.drupal.unit.spec.js
+++ src/site/layouts/tests/vet_center/vet_center.drupal.unit.spec.js
@@ -3,6 +3,7 @@ import { parseFixture, renderHTML } from '~/site/tests/support';
 import axeCheck from '~/site/tests/support/axe';
 
 const layoutPath = 'src/site/layouts/vet_center.drupal.liquid';
+import testData from './fixtures/vet_center_escanaba_data';
 
 describe('Vet Center Main Page', () => {
   let container;
@@ -17,5 +18,14 @@ describe('Vet Center Main Page', () => {
   it('reports no axe violations', async () => {
     const violations = await axeCheck(container);
     expect(violations.length).to.equal(0);
+  });
+
+  it('renders header and intro text', () => {
+    expect(container.querySelector('h1').innerHTML).to.equal(
+      testData.entityLabel,
+    );
+    expect(container.querySelector('div.va-introtext > p').innerHTML).to.equal(
+      testData.fieldIntroText,
+    );
   });
 });
