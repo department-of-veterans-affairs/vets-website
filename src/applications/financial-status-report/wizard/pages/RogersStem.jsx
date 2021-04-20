c55bdb03b39ce99ef48690038f82b0da2cfd0a13
Justin Linn <justinslinn727@gmail.com>
2021-04-20 12:20:16 -0400

    Clean up unused files (#16795)
    * Add aria region to all wizard grey boxes via HOC component on FSR

--- src/applications/financial-status-report/wizard/pages/RogersStem.jsx
+++ src/applications/financial-status-report/wizard/pages/RogersStem.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import recordEvent from 'platform/monitoring/record-event';
 import { PAGE_NAMES } from '../constants';
+import DelayedLiveRegion from '../DelayedLiveRegion';
 
 const RogersStem = () => {
   useEffect(() => {
@@ -11,27 +12,32 @@ const RogersStem = () => {
   }, []);
 
   return (
-    <div className="vads-u-background-color--gray-lightest vads-u-padding--2 vads-u-margin-top--2">
-      <p className="vads-u-margin-top--0">
-        Based on the information you provided, this isn’t the form you need.
-      </p>
-      <p>
-        <strong>For help with STEM program debt,</strong> contact us by email or
-        regular mail.
-      </p>
-      <ul>
-        <li>
-          <strong>Email: </strong>
-          <a href="mailto:stem.vbauf@va.gov">STEM.VBAUF@va.gov</a>.
-        </li>
-        <li>
-          <strong>Mail: </strong>
-          <div>VA Regional Processing Office 307</div>
-          <div>P.O. Box 4616</div>
-          <div>Buffalo, NY 14240-4616</div>
-        </li>
-      </ul>
-    </div>
+    <DelayedLiveRegion>
+      <div className="vads-u-background-color--gray-lightest vads-u-padding--2 vads-u-margin-top--2">
+        <h2
+          className="vads-u-margin-top--0 vads-u-font-size--h6 vads-u-font-weight--normal vads-u-font-family--sans"
+          id="wizard-results"
+        >
+          Based on the information you provided, this isn’t the form you need.
+        </h2>
+        <p>
+          <strong>For help with STEM program debt,</strong> contact us by email
+          or regular mail.
+        </p>
+        <ul>
+          <li>
+            <strong>Email: </strong>
+            <a href="mailto:stem.vbauf@va.gov">STEM.VBAUF@va.gov</a>.
+          </li>
+          <li>
+            <strong>Mail: </strong>
+            <div>VA Regional Processing Office 307</div>
+            <div>P.O. Box 4616</div>
+            <div>Buffalo, NY 14240-4616</div>
+          </li>
+        </ul>
+      </div>
+    </DelayedLiveRegion>
   );
 };
 
