3c79be276b41cfc09c3da0c6eea0a43016363d3d
Anita DeWitt <adewitt@thoughtworks.com>
2021-04-21 11:46:05 -0500

    [department-of-veterans-affairs/va-virtual-agent#71] removing close button on error message
    Co-authored-by: Shefali Nayak <shefali.nayak@thoughtworks.com>

--- src/applications/virtual-agent/containers/ChatbotError.jsx
+++ src/applications/virtual-agent/containers/ChatbotError.jsx
@@ -5,8 +5,6 @@ export default function ChatbotError() {
   return (
     <AlertBox
       content="We’re making some updates to the Virtual Agent. We’re sorry it’s not working right now. Please check back soon. If you require immediate assistance please call the VA.gov help desk at 800-698-2411 (TTY: 711)."
-      headline=""
-      onCloseAlert={function noRefCheck() {}}
       status="error"
     />
   );
