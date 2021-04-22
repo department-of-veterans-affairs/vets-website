7cb8bb709cd638026a4dead98fcaeaf91c2959ce
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-04-22 10:33:28 -0500

    Try forcing clicks in form test

--- script/run-cypress-tests-docker.sh
+++ script/run-cypress-tests-docker.sh
@@ -8,7 +8,7 @@ else
   export CYPRESS_CI=$CI
   export CYPRESS_FORMS=$FORMS
   if [ "$FORMS" = "true" ]; then
-    yarn cy:run:forms --config testFiles='**/tests/**/*.cypress.form.spec.js?(x)',video=false
+    yarn cy:run --config testFiles='**/tests/**/*.cypress.form.spec.js?(x)',video=false
   else
    yarn cy:run --config video=false
   fi
