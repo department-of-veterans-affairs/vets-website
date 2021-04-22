38f3390945db29a5929e2411d9e52dc6a6bed944
Matt Roth <roth_matthew@bah.com>
2021-04-22 14:11:44 -0400

    Update utility.js

--- src/applications/gi/reducers/utility.js
+++ src/applications/gi/reducers/utility.js
@@ -1,20 +1,22 @@
 // This file was created to hold redundant or multi-use functions to be used elsewhere in the reducers
 
 export const normalizedInstitutionAttributes = attributes => {
-  const name = attributes.name
-    ? attributes.name.toUpperCase()
-    : attributes.name;
   return {
     ...attributes,
-    name,
+    name: attributes.name?.toUpperCase(),
+    physicalCity: attributes.physicalCity?.toUpperCase(),
+    physicalState: attributes.physicalState?.toUpperCase(),
+    physicalCountry: attributes.physicalCountry?.toUpperCase(),
+    physicalZip: attributes.physicalZip?.toUpperCase(),
   };
 };
 
 export const normalizedProgramAttributes = attributes => {
-  const description =
-    attributes.description && attributes.description.toUpperCase();
   return {
     ...attributes,
-    description,
+    description: attributes.description?.toUpperCase(),
+    city: attributes.city?.toUpperCase(),
+    state: attributes.state?.toUpperCase(),
+    country: attributes.country?.toUpperCase(),
   };
 };
