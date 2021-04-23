2ff2b85569de06a1d0bbeec09faa2c07a4bf7bad
Craig Wheeler <craig@craigwheeler.net>
2021-04-23 14:18:14 -0400

    update selected option

--- src/applications/financial-status-report/components/ResolutionDebtCard.jsx
+++ src/applications/financial-status-report/components/ResolutionDebtCard.jsx
@@ -12,15 +12,29 @@ const formatter = new Intl.NumberFormat('en-US', {
   minimumFractionDigits: 2,
 });
 
-const ExpandedContent = ({ selected, input, setInput }) => {
-  switch (selected.value) {
+const ExpandedContent = ({ debt, setSelected }) => {
+  const update = data => {
+    setSelected(prevState =>
+      prevState.map(item => {
+        if (item.id === debt.id) {
+          return {
+            ...item,
+            offerToPay: data.value,
+          };
+        }
+        return item;
+      }),
+    );
+  };
+
+  switch (debt.value) {
     case 'Extended monthly payments':
       return (
         <TextInput
           additionalClass="input-size-3"
           label="How much can you afford to pay monthly on this debt?"
-          field={input || ''}
-          onValueChange={data => setInput(data.value)}
+          field={{ value: debt.offerToPay || '' }}
+          onValueChange={data => update(data)}
           //   required
         />
       );
@@ -29,8 +43,8 @@ const ExpandedContent = ({ selected, input, setInput }) => {
         <TextInput
           additionalClass="input-size-3"
           label="How much do you offer to pay for this debt with a single payment?"
-          field={input || ''}
-          onValueChange={data => setInput(data.value)}
+          field={{ value: debt.offerToPay || '' }}
+          onValueChange={data => update(data)}
           //   required
         />
       );
@@ -45,24 +59,16 @@ const ExpandedContent = ({ selected, input, setInput }) => {
   }
 };
 
-const ResolutionDebtCard = props => {
-  const { selectedDebts } = props;
-
-  const [input, setInput] = useState('');
-  const [selected, setSelected] = useState({
-    id: null,
-    value: '',
-    offerToPay: 0,
-  });
-
-  const options = ['Waiver', 'Extended monthly payments', 'Compromise'];
-  const label =
+const ResolutionDebtCard = ({ selectedDebts }) => {
+  const [selected, setSelected] = useState(selectedDebts);
+  const radioOptions = ['Waiver', 'Extended monthly payments', 'Compromise'];
+  const radioLabels =
     'Which repayment or relief option would you like for this debt?';
 
   return (
     <>
       <h4 className="vads-u-margin--0">Your selected debts</h4>
-      {selectedDebts.map(debt => {
+      {selected.map(debt => {
         const title = deductionCodes[debt.deductionCode] || debt.benefitType;
         const subTitle =
           debt.currentAr && formatter.format(parseFloat(debt.currentAr));
@@ -79,25 +85,31 @@ const ResolutionDebtCard = props => {
             </p>
             <ExpandingGroup
               key={debt.id}
-              open={selected.id === debt.id}
+              open={debt.value}
               additionalClass="form-expanding-group-active-radio"
             >
               <RadioButtons
                 id={debt.id}
                 name={debt.id}
-                label={label}
-                options={options}
-                value={selected.id === debt.id ? selected : null}
-                onValueChange={val =>
-                  setSelected({ id: debt.id, value: val.value })
-                }
+                label={radioLabels}
+                options={radioOptions}
+                value={{ value: debt.value }}
+                onValueChange={data => {
+                  setSelected(prevState =>
+                    prevState.map(item => {
+                      if (item.id === debt.id) {
+                        return {
+                          ...item,
+                          value: data.value,
+                        };
+                      }
+                      return item;
+                    }),
+                  );
+                }}
                 // required
               />
-              <ExpandedContent
-                selected={selected}
-                input={input}
-                setInput={setInput}
-              />
+              <ExpandedContent debt={debt} setSelected={setSelected} />
             </ExpandingGroup>
           </div>
         );
