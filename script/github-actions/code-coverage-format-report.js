9d0ee3f3dec8ba7ce9d289a25aac2ad17219706c
Oseas Moran <oseas.moran@adhocteam.us>
2021-04-23 10:06:41 -0700

    updated variable

--- script/github-actions/code-coverage-format-report.js
+++ script/github-actions/code-coverage-format-report.js
@@ -11,30 +11,30 @@ const regex = /([^A-Za-z0-9/()%-])+\s/g;
 
 const codeCoverageData = codeCoverageReport.replace(regex, ',').split(',');
 
-let codeCoverageMarkdown = '<table> \n <thead> \n <tr> \n'; // html format
+let codeCoverageHTML = '<table> \n <thead> \n <tr> \n'; // html format
 
 codeCoverageData.forEach((data, index) => {
   if (data === '') {
     // ignore empty data
-    codeCoverageMarkdown += '';
+    codeCoverageHTML += '';
   } else if (index < 5) {
     // create table header
-    codeCoverageMarkdown += `<th> ${data} </th> \n`;
+    codeCoverageHTML += `<th> ${data} </th> \n`;
   } else if (index === 5) {
     // seperate table header from data
-    codeCoverageMarkdown += `<th> ${data} </th> \n </tr> \n </thead> \n <tbody> \n`;
+    codeCoverageHTML += `<th> ${data} </th> \n </tr> \n </thead> \n <tbody> \n`;
   } else if ((index - 1) % 5 === 0) {
     // start of row in table body
-    codeCoverageMarkdown += `<tr> \n <td> ${data} </td> \n`;
+    codeCoverageHTML += `<tr> \n <td> ${data} </td> \n`;
   } else if ((index - 1) % 5 === 4) {
     // end of row in table body
-    codeCoverageMarkdown += `<td> ${data} </td> \n </tr> \n`;
+    codeCoverageHTML += `<td> ${data} </td> \n </tr> \n`;
   } else {
     // row in table body
-    codeCoverageMarkdown += `<td> ${data} </td> \n`;
+    codeCoverageHTML += `<td> ${data} </td> \n`;
   }
 });
 
-codeCoverageMarkdown += `</tbody> \n </table>`; // close html
+codeCoverageHTML += `</tbody> \n </table>`; // close html
 
-console.log(codeCoverageMarkdown); // eslint-disable-line no-console
+console.log(codeCoverageHTML); // eslint-disable-line no-console
