5833ac986f7a1a83bdf028645aa3ce6702c48462
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-04-23 14:26:57 -0500

    Clean up

--- script/run-cypress-tests-docker.js
+++ script/run-cypress-tests-docker.js
@@ -1,21 +1,19 @@
 const { runCommand } = require('./utils');
 const exec = require('child_process').exec;
 
-exec(
-  "find src -name '*.cypress.*.js' | tr '\n' ','",
-  // eslint-disable-next-line no-return-assign
-  function(_err, stdout) {
-    const strings = stdout.split(',');
-    const divider = Math.ceil(strings.length / 4);
-    const tests = strings.slice(
+exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
+  const strings = stdout.split(',');
+  const divider = Math.ceil(strings.length / 4);
+  const tests = strings
+    .slice(
       Number(process.env.STEP) * divider,
       (Number(process.env.STEP) + 1) * divider,
-      )
-      .join(',');
-    runCommand(
-      `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
-        process.env.CI
-      } yarn cy:run --config video=false --spec '${tests}'`,
-    );
-  },
-);
+    )
+    .join(',');
+
+  runCommand(
+    `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
+      process.env.CI
+    } yarn cy:run --config video=false --spec '${tests}'`,
+  );
+});
