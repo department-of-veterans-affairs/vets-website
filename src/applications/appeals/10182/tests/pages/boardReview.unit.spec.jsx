d3f89606810f3fa62dd21167a3424ffe5bea75cc
Robin Garrison <robin.garrison@adhocteam.us>
2021-04-27 15:30:25 -0500

    Add boardReview & contactInfo unit tests

---
+++ src/applications/appeals/10182/tests/pages/boardReview.unit.spec.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
+
+import {
+  DefinitionTester,
+  selectRadio,
+} from 'platform/testing/unit/schemaform-utils.jsx';
+
+import formConfig from '../../config/form';
+
+describe('NOD board review page', () => {
+  const {
+    schema,
+    uiSchema,
+  } = formConfig.chapters.boardReview.pages.boardReviewOption;
+
+  it('should render', () => {
+    const form = mount(
+      <DefinitionTester
+        definitions={{}}
+        schema={schema}
+        uiSchema={uiSchema}
+        data={{}}
+        formData={{}}
+      />,
+    );
+
+    expect(form.find('input').length).to.equal(3);
+    form.unmount();
+  });
+
+  it('should allow submit', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        definitions={{}}
+        schema={schema}
+        uiSchema={uiSchema}
+        data={{}}
+        formData={{}}
+        onSubmit={onSubmit}
+      />,
+    );
+
+    selectRadio(form, 'root_boardReviewOption', 'direct_review');
+    form.find('form').simulate('submit');
+    expect(form.find('.usa-input-error-message').length).to.equal(0);
+    expect(onSubmit.called).to.be.true;
+    form.unmount();
+  });
+});
