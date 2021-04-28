158af7d909fceb74a69466b257bc41253d758037
Elijah Lynn <elijah@elijahlynn.net>
2021-04-28 13:14:44 -0700

    Add comment on where to get support for sandbox URL if issues come up.

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -9,6 +9,7 @@ DRUPAL_ADDRESSES = [
   'vagovstaging': 'http://internal-dsva-vagov-staging-cms-1188006.us-gov-west-1.elb.amazonaws.com',
   'vagovprod'   : 'http://internal-dsva-vagov-prod-cms-2000800896.us-gov-west-1.elb.amazonaws.com',
   // This is a Tugboat URL, rebuilt frequently from PROD CMS. See https://tugboat.vfs.va.gov/6042f35d6a89945fd6399dc3.
+  // If there are issues with this endpoint, please post in #cms-support Slack and tag @CMS DevOps Engineers.
   'sandbox'     : 'https://cms-vets-website-branch-builds-lo9uhqj18nwixunsjadvjsynuni7kk1u.ci.cms.va.gov',
 ]
 
