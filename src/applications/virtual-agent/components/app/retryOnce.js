57072cab4dfa61fdcb2c013ace39ee98d3372c30
Paul Phillips <Paul.Phillips@thoughtworks.com>
2021-04-28 16:20:50 -0500

    Re-enable eslint setting

--- src/applications/virtual-agent/components/app/retryOnce.js
+++ src/applications/virtual-agent/components/app/retryOnce.js
@@ -2,7 +2,6 @@ export default async function retryOnce(retryableFunction) {
   try {
     return await retryableFunction();
   } catch (error) {
-    // eslint-disable-next-line no-return-await
-    return await retryableFunction();
+    return retryableFunction();
   }
 }
