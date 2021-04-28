0f0f48f2c589fa92ca2d33901d598eb1f6c800bc
Tim Wright <timwright12@gmail.com>
2021-04-28 16:30:14 -0400

    added template unit test

--- src/site/layouts/tests/vamc/health_care_local_facility.unit.spec.js
+++ src/site/layouts/tests/vamc/health_care_local_facility.unit.spec.js
@@ -21,14 +21,35 @@ describe('health_care_local_facility', () => {
       expect(violations.length).to.equal(0);
     });
 
-    it('should render phone number', () => {
+    it('should not render .mental-health-clinic-phone', () => {
+      expect(container.querySelector('.mental-health-clinic-phone')).to.be.null;
+    });
+
+    // Default state, main phone number is in dataset
+    it('should render main phone number', () => {
       expect(container.querySelector('.main-phone a').textContent).to.equal(
         data.fieldPhoneNumber,
       );
     });
 
-    it('should not render .mental-health-clinic-phone', () => {
-      expect(container.querySelector('.mental-health-clinic-phone')).to.be.null;
+    // Test return if the fieldPhoneNumber string is empty
+    it('should not render .main-phone if string is empty', async () => {
+      // Clear fieldPhoneNumber to mimic an entry error
+      data.fieldPhoneNumber = '';
+      container = await renderHTML(layoutPath, data, 'fieldPhoneNumberEmpty');
+
+      // Element should not render in the DOM
+      expect(container.querySelector('.main-phone')).to.be.null;
+    });
+
+    // Test return if the fieldPhoneNumber is null
+    it('should not render .main-phone if null', async () => {
+      // Set fieldPhoneNumber to null value
+      data.fieldPhoneNumber = null;
+      container = await renderHTML(layoutPath, data, 'fieldPhoneNumberNull');
+
+      // Element should not render in the DOM
+      expect(container.querySelector('.main-phone')).to.be.null;
     });
   });
 });
