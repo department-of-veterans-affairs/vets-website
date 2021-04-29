aa49526802f2f4506d2a534ea465210305df72cd
Shefali Nayak <shefali.nayak@thoughtworks.com>
2021-04-29 13:04:55 -0400

    adds about this study content

--- src/applications/virtual-agent/components/app/App.jsx
+++ src/applications/virtual-agent/components/app/App.jsx
@@ -60,6 +60,46 @@ export default function Page(props) {
         </div>
         <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--8">
           <h1>About this study</h1>
+
+          <p>
+            Thank you for participating in this study. We know your time is
+            valuable, and your feedback will help us build better products to
+            serve you.
+          </p>
+
+          <p>
+            We are exploring how a virtual agent/chatbot may help you find
+            answers on Va.gov. To help us understand this, we created this
+            prototype for you to test. You can try a question like “Is
+            healthcare covered?” or ask any other question you like. Because
+            this virtual agent is still in development (beta), it will not have
+            answers to all your questions, and cannot do the following:
+          </p>
+
+          <ul>
+            <li>
+              Assess, detect, or provide a medical or mental health diagnosis
+            </li>
+            <li>
+              Provide medical or mental health advice, treatment, or counseling
+            </li>
+            <li>Escalate an emergency</li>
+            <li>Escalate directly to VA.gov support personnel</li>
+            <li>Troubleshoot login issues</li>
+          </ul>
+
+          <p>
+            Please do not enter personal information that someone can use to
+            identify you personally.
+          </p>
+
+          <p>
+            We have created a survey to help you help us build a better bot.
+            When you're ready, [please tell us about your experience] and how we
+            can improve.
+          </p>
+
+          <p>As always, we thank you for your service.</p>
         </div>
       </div>
     </div>
