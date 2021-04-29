3273aad1a12cfa8f8465c8ae3c145e9553e62eab
Paul Phillips <Paul.Phillips@thoughtworks.com>
2021-04-29 14:16:53 -0500

    Swap columns

--- src/applications/virtual-agent/components/app/App.jsx
+++ src/applications/virtual-agent/components/app/App.jsx
@@ -56,11 +56,11 @@ export default function Page(props) {
   return (
     <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
       <div className="vads-l-row vads-u-margin-x--neg2p5">
-        <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--4">
-          <App {...props} />
-        </div>
         <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--8">
           <Disclaimer />
+        </div>
+        <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--4">
+          <App {...props} />
         </div>
       </div>
     </div>
