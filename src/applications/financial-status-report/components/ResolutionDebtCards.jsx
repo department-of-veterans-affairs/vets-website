69ec684da166b915b0f4476546902698ae124dbf
Craig Wheeler <craig@craigwheeler.net>
2021-04-29 17:35:41 -0400

    add telephone contact to checkbox

--- src/applications/financial-status-report/components/ResolutionDebtCards.jsx
+++ src/applications/financial-status-report/components/ResolutionDebtCards.jsx
@@ -6,6 +6,10 @@ import RadioButtons from '@department-of-veterans-affairs/component-library/Radi
 import TextInput from '@department-of-veterans-affairs/component-library/TextInput';
 import ExpandingGroup from '@department-of-veterans-affairs/component-library/ExpandingGroup';
 import Checkbox from '@department-of-veterans-affairs/component-library/Checkbox';
+import Telephone, {
+  CONTACTS,
+  PATTERNS,
+} from '@department-of-veterans-affairs/component-library/Telephone';
 
 const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
@@ -47,13 +51,25 @@ const ExpandedContent = ({ debt, updateDebts, error }) => {
       );
     default:
       return (
-        <Checkbox
-          label="By checking this box, I’m agreeing that I understand how a debt waiver may affect my VA education benefits. If VA grants me a waiver, this will reduce any remaining education benefit entitlement I may have."
-          checked={debt.resolution?.agreeToWaiver || false}
-          onValueChange={value => updateDebts('agreeToWaiver', value, debt)}
-          errorMessage={checkboxError && 'Please provide a response'}
-          required
-        />
+        <>
+          <Checkbox
+            label="By checking this box, I’m agreeing that I understand how a debt waiver may affect my VA education benefits. If VA grants me a waiver, this will reduce any remaining education benefit entitlement I may have."
+            checked={debt.resolution?.agreeToWaiver || false}
+            onValueChange={value => updateDebts('agreeToWaiver', value, debt)}
+            errorMessage={checkboxError && 'Please provide a response'}
+            required
+          />
+          <p>
+            Note: If you have questions about this, call us at{' '}
+            <Telephone contact={CONTACTS.DMC || '800-827-0648'} /> (or{' '}
+            <Telephone
+              contact={CONTACTS.DMC_OVERSEAS || '1-612-713-6415'}
+              pattern={PATTERNS.OUTSIDE_US}
+            />{' '}
+            from overseas). We’re here Monday through Friday, 7:30 a.m. to 7:00
+            p.m. ET.
+          </p>
+        </>
       );
   }
 };
