f1861f51825f73473b4d672269e7f46acbf3fb9f
Shefali Nayak <shefali.nayak@thoughtworks.com>
2021-04-29 14:25:12 -0400

    adds accordions

--- src/applications/virtual-agent/components/app/App.jsx
+++ src/applications/virtual-agent/components/app/App.jsx
@@ -100,6 +100,77 @@ export default function Page(props) {
           </p>
 
           <p>As always, we thank you for your service.</p>
+
+          <va-accordion>
+            <va-accordion-item header="If you need immediate help">
+              <p>
+                If you are experiencing a crisis, please call the Veterans
+                Crisis Line at 800-273-8255 and press 1. To find additional
+                support, go to mentalhealth.va.gov.
+              </p>
+
+              <p>
+                If you are experiencing a medical emergency, please call 911. If
+                you are not sure if you are experiencing a medical emergency,
+                please contact your primary care provider, or go to our
+                [Emergency Medical Care] page to learn more.
+              </p>
+
+              <p>
+                For other VA benefits and services questions, go to the Contact
+                Us page, where you can find phone numbers, FAQs, step-by-step
+                guides, and other resources.
+              </p>
+            </va-accordion-item>
+          </va-accordion>
+          <va-accordion>
+            <va-accordion-item header="What information are we collecting?">
+              <p>
+                VA.gov automatically collects certain information about your
+                visit to VA.gov web pages. We limit the data collected to meet
+                specific business needs and to protect your privacy.
+              </p>
+              <p>
+                Please know that:
+                <ul>
+                  <li>We won't know who you are.</li>
+                  <li>
+                    We will not use this information to identify you personally.
+                  </li>
+                  <li>We will not contact you.</li>
+                </ul>
+              </p>
+              <p>
+                We will retain and analyze conversational logs (what you type to
+                the bot), survey results, and page metrics to answer these types
+                of questions:
+                <ul>
+                  <li>What topics and questions are essential to Veterans?</li>
+                  <li>What questions did the bot answer?</li>
+                  <li>What questions did the bot not answer?</li>
+                  <li>Could this tool be helpful to Veterans? How?</li>
+                  <li>How many Veterans tried to use the bot?</li>
+                  Consolidated information will be aggregated, analyzed
+                  internally, and used to help us build better products.
+                  Findings will not be released publicly.
+                </ul>
+              </p>
+              <p>
+                During this learning phase, please help us protect your privacy
+                and security by not typing any personal information, such as:
+                <ul>
+                  <li>Your name</li>
+                  <li>Address</li>
+                  <li>Social security number</li>
+                </ul>
+                Or any other information that could be used to identify you.
+              </p>
+              <p>
+                For more information on how we protect your privacy, go to our
+                [Privacy, Policies, and legal information] page.
+              </p>
+            </va-accordion-item>
+          </va-accordion>
         </div>
       </div>
     </div>
