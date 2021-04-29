881532f5c94711811da8895c149aab4f5045f615
Paul Phillips <Paul.Phillips@thoughtworks.com>
2021-04-29 14:34:06 -0500

    Remove unnecessary props

--- src/applications/virtual-agent/components/page/Page.jsx
+++ src/applications/virtual-agent/components/page/Page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Disclaimer from './Disclaimer';
 import App from '../app/App';
 
-export default function Page(props) {
+export default function Page() {
   return (
     <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
       <div className="vads-l-row vads-u-margin-x--neg2p5">
@@ -10,7 +10,7 @@ export default function Page(props) {
           <Disclaimer />
         </div>
         <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--4">
-          <App {...props} />
+          <App />
         </div>
       </div>
     </div>
