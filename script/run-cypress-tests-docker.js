42206386b6f113541674d4284785db67c1ef133f
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-04-30 08:59:41 -0500

    Clean up

--- script/run-cypress-tests-docker.js
+++ script/run-cypress-tests-docker.js
@@ -14,6 +14,8 @@ exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
   runCommand(
     `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
       process.env.CI
+    } XDG_CONFIG_HOME=/tmp/cyhome${
+      process.env.STEP
     } yarn cy:run --config video=false --spec '${tests}'`,
   );
 });
