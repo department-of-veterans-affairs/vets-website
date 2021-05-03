23917d0019769cfb4f11fe33b1e5b5bb3d2e61e7
Holden Hinkle <holdenhinkle@gmail.com>
2021-05-03 12:03:41 -0400

    revert Jenkinsfile and run-cypress-tests-docker.js back to Darius' versions

--- script/run-cypress-tests-docker.js
+++ script/run-cypress-tests-docker.js
@@ -1,9 +1,30 @@
 const { runCommand } = require('./utils');
 const exec = require('child_process').exec;
 
+// exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
+//   const strings = stdout.split(',');
+//   const divider = Math.ceil(strings.length / process.env.NUM_STEPS);
+//   const tests = strings
+//     .slice(
+//       Number(process.env.STEP) * divider,
+//       (Number(process.env.STEP) + 1) * divider,
+//     )
+//     .join(',');
+
+//   runCommand(
+//     `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
+//       process.env.CI
+//     } PERCY_TOKEN=${process.env.PERCY_TOKEN} PERCY_PARALLEL_NONCE=${
+//       process.env.PERCY_PARALLEL_NONCE
+//     } PERCY_PARALLEL_TOTAL=${
+//       process.env.NUM_STEPS
+//     } yarn cy:run --config video=false --spec '${tests}'`,
+//   );
+// });
+
 exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
-  const strings = stdout.split(',');
-  const divider = Math.ceil(strings.length / process.env.NUM_STEPS);
+  const strings = stdout.split(',').sort();
+  const divider = Math.ceil(strings.length / 6);
   const tests = strings
     .slice(
       Number(process.env.STEP) * divider,
@@ -14,10 +35,8 @@ exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
   runCommand(
     `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
       process.env.CI
-    } PERCY_TOKEN=${process.env.PERCY_TOKEN} PERCY_PARALLEL_NONCE=${
-      process.env.PERCY_PARALLEL_NONCE
-    } PERCY_PARALLEL_TOTAL=${
-      process.env.NUM_STEPS
+    } XDG_CONFIG_HOME=/tmp/cyhome${
+      process.env.STEP
     } yarn cy:run --config video=false --spec '${tests}'`,
   );
 });
