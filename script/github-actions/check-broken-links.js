e2c3544b30101abb6833dfc9fb8d833f1d13f883
Oseas Moran <oseas.moran@adhocteam.us>
2021-05-04 13:42:06 -0700

    expanded broken-link script w/ more parameters

--- script/github-actions/check-broken-links.js
+++ script/github-actions/check-broken-links.js
@@ -1,22 +1,29 @@
 const fs = require('fs');
+// const path = require('path');
 const commandLineArgs = require('command-line-args');
 
 const COMMAND_LINE_OPTIONS_DEFINITIONS = [
-  { name: 'env', alias: 'e', type: String },
-  { name: 'path', alias: 'p', type: String },
+  { name: 'branch', alias: 'b', type: String },
   { name: 'contentOnlyBuild', alias: 'c', type: Boolean, defaultValue: false },
+  { name: 'envName', alias: 'e', type: String },
+  { name: 'path', alias: 'p', type: String },
+  { name: 'serverUrl', alias: 's', type: String },
 ];
 
 const options = commandLineArgs(COMMAND_LINE_OPTIONS_DEFINITIONS);
-const IS_PROD_BRANCH = options.env.replace('refs/heads/', '') === 'master'; // format: refs/heads/
+const envName = options.envName;
 const contentOnlyBuild = options.contentOnlyBuild;
 // const reportPath = path.join(__dirname, '../../logs/localhost-broken-links.json'); // TODO: Maybe argparse parameter to determine environment
 const reportPath = options.path;
+const SERVER_URL = options.serverUrl;
+const BRANCH_NAME = options.branch.replace('refs/heads/', ''); // format: refs/heads/*
+const IS_PROD_BRANCH = BRANCH_NAME === 'master';
 const maxBrokenLinks = 10;
+
 let color = 'warning';
 
-console.log('branch name', options.env.replace('refs/heads/', '')); // eslint-disable-line no-console
-console.log('rp', reportPath); // eslint-disable-line no-console
+// for testing purposes
+console.log(`SLACK_COLOR=${color} >> $GITHUB_ENV`); // eslint-disable-line no-console
 
 // broken links detected
 if (fs.existsSync(reportPath)) {
@@ -30,20 +37,17 @@ if (fs.existsSync(reportPath)) {
     color = 'danger';
   }
 
-  // TODO: If this is still relavent, fix format
-  // const heading = "@cmshelpdesk ${brokenLinks.brokenLinksCount} broken links found in the `${envName}` build on `${env.BRANCH_NAME}`\n\n${env.RUN_DISPLAY_URL}\n\n"
-  // const message = "${heading}\n${brokenLinks.summary}".stripMargin()
+  const heading = `@cmshelpdesk ${
+    brokenLinks.brokenLinksCount
+  } broken links found in the ${envName} build on ${BRANCH_NAME} \n\n${SERVER_URL}\n\n`;
+  const message = `${heading}\n${brokenLinks.summary}`; // TODO: stripMargin equiv
 
   console.log(`${brokenLinks.brokenLinksCount} broken links found`); // eslint-disable-line no-console
-  // console.log(message); TODO: Fix message format to display
+  console.log(`SLACK_MESSAGE=${message} >> $GITHUB_ENV`); // eslint-disable-line no-console
 
   if (!IS_PROD_BRANCH && !contentOnlyBuild) {
     return;
   }
 
-  // TODO: Send slack
-
-  if (color === 'danger') {
-    console.log(`::set-output name=BROKEN_LINKS::true`); // eslint-disable-line no-console
-  }
+  console.log(`SLACK_COLOR=${color} >> $GITHUB_ENV`); // eslint-disable-line no-console
 }
