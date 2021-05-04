43e3c901a1ad0e175eb85813d9b1fa58a39ef667
Holden Hinkle <holdenhinkle@gmail.com>
2021-05-04 15:59:52 -0400

    set NUMBER_OF_STEPS

--- script/run-cypress-tests-docker.js
+++ script/run-cypress-tests-docker.js
@@ -4,12 +4,12 @@ const exec = require('child_process').exec;
 exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
   /* eslint-disable no-console */
   console.log('PERCY_TOKEN: ', process.env.PERCY_TOKEN);
-  // console.log('PERCY_PARALLEL_NONCE: ', process.env.PERCY_PARALLEL_NONCE);
-  console.log('NUM_STEPS: ', process.env.NUM_STEPS);
+  console.log('PERCY_PARALLEL_NONCE: ', process.env.PERCY_PARALLEL_NONCE);
   /* eslint-enable no-console */
 
+  const NUMBER_OF_STEPS = 6;
   const strings = stdout.split(',').sort();
-  const divider = Math.ceil(strings.length / 6);
+  const divider = Math.ceil(strings.length / NUMBER_OF_STEPS);
   const tests = strings
     .slice(
       Number(process.env.STEP) * divider,
@@ -24,6 +24,6 @@ exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
       process.env.PERCY_TOKEN
     } PERCY_PARALLEL_NONCE=${
       process.env.PERCY_PARALLEL_NONCE
-    } PERCY_PARALLEL_TOTAL=6 yarn cy:run --config video=false --spec '${tests}'`,
+    } PERCY_PARALLEL_TOTAL=${NUMBER_OF_STEPS} yarn cy:run --config video=false --spec '${tests}'`,
   );
 });
