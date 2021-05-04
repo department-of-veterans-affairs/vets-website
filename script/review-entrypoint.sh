3ba4fb4d544a7140202dca7a1a44861c4357264c
Tim Wright <timwright12@gmail.com>
2021-05-04 16:32:56 -0400

    Revert Content Separation

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -e
 yarn install --production=false
+npm run fetch-drupal-cache
 npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
 
 if [ -d "../content-build" ]
 then
   cd ../content-build
   yarn install --production=false
-  npm run fetch-drupal-cache
-  npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' --apps-directory-name=application
+  npm run build -- --buildtype localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
   npm run heroku-serve -- build/localhost -p 3002
 else
   echo "Content build does not exist!"
