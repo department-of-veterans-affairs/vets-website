353c981662c2bd963488bf19ddb552664f3174bb
linuxlance <LanceGundersen@users.noreply.github.com>
2021-05-04 17:18:14 -0400

    Adjust facility number based on covid flow

--- src/applications/vaos/services/location/transformers.js
+++ src/applications/vaos/services/location/transformers.js
@@ -211,6 +211,7 @@ export function transformFacility(facility) {
     managingOrganization: {
       reference: `Organization/${id.substr(0, 3)}`,
     },
+    detailedServices: facility.detailedServices || undefined,
   };
 }
 
