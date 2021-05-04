707070229c3cf7dfff5bc4fd7b009323bdda6f97
Holden Hinkle <holdenhinkle@gmail.com>
2021-05-04 09:35:49 -0400

    wip

--- script/run-cypress-tests-docker.js
+++ script/run-cypress-tests-docker.js
@@ -35,8 +35,12 @@ exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
   runCommand(
     `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
       process.env.CI
-    } XDG_CONFIG_HOME=/tmp/cyhome${
-      process.env.STEP
+    } XDG_CONFIG_HOME=/tmp/cyhome${process.env.STEP} PERCY_TOKEN=${
+      process.env.PERCY_TOKEN
+    } PERCY_PARALLEL_NONCE=${
+      process.env.PERCY_PARALLEL_NONCE
+    } PERCY_PARALLEL_TOTAL=${
+      process.env.NUM_STEPS
     } yarn cy:run --config video=false --spec '${tests}'`,
   );
 });
