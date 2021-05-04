bc41a704f19a3aff73217a9c4f3a40c257df32b6
Holden Hinkle <holdenhinkle@gmail.com>
2021-05-04 14:16:53 -0400

    pass env vars to Percy

--- script/run-cypress-tests-docker.js
+++ script/run-cypress-tests-docker.js
@@ -1,27 +1,6 @@
 const { runCommand } = require('./utils');
 const exec = require('child_process').exec;
 
-// exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
-//   const strings = stdout.split(',');
-//   const divider = Math.ceil(strings.length / process.env.NUM_STEPS);
-//   const tests = strings
-//     .slice(
-//       Number(process.env.STEP) * divider,
-//       (Number(process.env.STEP) + 1) * divider,
-//     )
-//     .join(',');
-
-//   runCommand(
-//     `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
-//       process.env.CI
-//     } PERCY_TOKEN=${process.env.PERCY_TOKEN} PERCY_PARALLEL_NONCE=${
-//       process.env.PERCY_PARALLEL_NONCE
-//     } PERCY_PARALLEL_TOTAL=${
-//       process.env.NUM_STEPS
-//     } yarn cy:run --config video=false --spec '${tests}'`,
-//   );
-// });
-
 exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
   const strings = stdout.split(',').sort();
   const divider = Math.ceil(strings.length / Number(process.env.NUM_STEPS));
@@ -35,8 +14,12 @@ exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
   runCommand(
     `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
       process.env.CI
-    } XDG_CONFIG_HOME=/tmp/cyhome${
-      process.env.STEP
+    } XDG_CONFIG_HOME=/tmp/cyhome${process.env.STEP} PERCY_TOKEN=${
+      process.env.PERCY_TOKEN
+    } PERCY_PARALLEL_NONCE=${
+      process.env.PERCY_PARALLEL_NONCE
+    } PERCY_PARALLEL_TOTAL=${
+      process.env.NUM_STEPS
     } yarn cy:run --config video=false --spec '${tests}'`,
   );
 });
