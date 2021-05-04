dee9291eceb0980a5e609c40aa83400f38008f09
Holden Hinkle <holdenhinkle@gmail.com>
2021-05-04 16:33:17 -0400

    remove logging env vars

--- script/run-cypress-tests-docker.js
+++ script/run-cypress-tests-docker.js
@@ -2,11 +2,6 @@ const { runCommand } = require('./utils');
 const exec = require('child_process').exec;
 
 exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
-  /* eslint-disable no-console */
-  console.log('PERCY_TOKEN: ', process.env.PERCY_TOKEN);
-  console.log('PERCY_PARALLEL_NONCE: ', process.env.PERCY_PARALLEL_NONCE);
-  /* eslint-enable no-console */
-
   const NUMBER_OF_STEPS = 6;
   const strings = stdout.split(',').sort();
   const divider = Math.ceil(strings.length / NUMBER_OF_STEPS);
