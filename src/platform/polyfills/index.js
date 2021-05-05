93378de09e75c9f5a35b260f38f68ee9bac5aa4f
Brooks Johnson <brooks.johnson@pm.me>
2021-05-05 15:30:04 -0700

    Use a better name

--- src/platform/polyfills/index.js
+++ src/platform/polyfills/index.js
@@ -22,7 +22,7 @@ if (navigator.userAgent.includes('Edge/14')) {
   if (typeof window.CustomEvent === 'function') return false;
 
   function CustomEvent(event, params) {
-    const detail = params || {
+    const customParams = params || {
       bubbles: false,
       cancelable: false,
       detail: null,
@@ -30,9 +30,9 @@ if (navigator.userAgent.includes('Edge/14')) {
     const evt = document.createEvent('CustomEvent');
     evt.initCustomEvent(
       event,
-      detail.bubbles,
-      detail.cancelable,
-      detail.detail,
+      customParams.bubbles,
+      customParams.cancelable,
+      customParams.detail,
     );
     return evt;
   }
