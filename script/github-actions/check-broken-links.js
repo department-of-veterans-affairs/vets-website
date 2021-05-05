649e6fab1d7ca7801f05c9ba44932ae3782d2b46
Oseas Moran <oseas.moran@adhocteam.us>
2021-05-05 08:17:24 -0700

    test cancel workflow

--- script/github-actions/check-broken-links.js
+++ script/github-actions/check-broken-links.js
@@ -42,8 +42,19 @@ if (fs.existsSync(reportPath)) {
   console.log(`::set-output name=SLACK_MESSAGE::${message}`); // eslint-disable-line no-console
 
   if (!IS_PROD_BRANCH && !contentOnlyBuild) {
+    // Ignore the results of the broken link checker unless
+    // we are running either on the master branch or during
+    // a Content Release. This way, if there is a broken link,
+    // feature branches aren't affected, so VFS teams can
+    // continue merging.
     return;
   }
+
+  // TODO: Is this still applicable on GHA?
+  // Unset brokenLinks now that we're done with this, because Jenkins may temporarily
+  // freeze (through serialization) this pipeline while the Slack message is being sent.
+  // brokenLinks is an instance of JSONObject, which cannot be serialized by default.
+  // brokenLinks = null;
 
   console.log(`::set-output name=SLACK_COLOR::${color}`); // eslint-disable-line no-console
 }
