8dd08edd9236dd34f61996d121a4dec8d0791868
PaulPhillips-va <73457341+PaulPhillips-va@users.noreply.github.com>
2021-05-06 08:11:24 -0500

    Add aria-labels to phone number links sent by the chatbot (#17091)

--- src/applications/virtual-agent/components/webchat/markdownRenderer.js
+++ src/applications/virtual-agent/components/webchat/markdownRenderer.js
@@ -1,5 +1,6 @@
 import MarkdownIt from 'markdown-it';
 import markdownitLinkAttributes from 'markdown-it-link-attributes';
+import makePhoneNumberAriaLabel from './makePhoneNumberAriaLabel';
 
 const markdownRenderer = MarkdownIt({
   html: true,
@@ -10,5 +11,33 @@ const markdownRenderer = MarkdownIt({
     rel: 'noopener',
   },
 });
+
+// Remember old renderer, if overridden, or proxy to default renderer
+const defaultRender =
+  markdownRenderer.renderer.rules.link_open ||
+  function(tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+const linkOpen = 'link_open';
+markdownRenderer.renderer.rules[linkOpen] = function(
+  tokens,
+  idx,
+  options,
+  env,
+  self,
+) {
+  const [, href] = tokens[idx].attrs.find(([key]) => key === 'href');
+  const [scheme, phoneNumber] = href.split(':');
+
+  if (scheme === 'tel') {
+    const ariaLabel = makePhoneNumberAriaLabel(phoneNumber);
+
+    tokens[idx].attrPush(['aria-label', ariaLabel]); // add new attribute
+  }
+
+  // pass token to default renderer.
+  return defaultRender(tokens, idx, options, env, self);
+};
 
 export default markdownRenderer;
