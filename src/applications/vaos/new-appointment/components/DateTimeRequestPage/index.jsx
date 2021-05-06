45e8441b163eb71d631949a901e8379eaaa587a6
Jennie McGibney <jennie.mcgibney@va.gov>
2021-05-06 18:23:07 -0400

    delete routeToNextAppointmentPage from goForward

--- src/applications/vaos/new-appointment/components/DateTimeRequestPage/index.jsx
+++ src/applications/vaos/new-appointment/components/DateTimeRequestPage/index.jsx
@@ -94,7 +94,6 @@ export default function DateTimeRequestPage() {
             dispatch,
             data,
             history,
-            routeToNextAppointmentPage,
             submitted,
             setSubmitted,
           })
