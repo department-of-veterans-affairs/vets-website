65ea2d82ef7b3816a01ff6e4a4afe474c7e6dbab
Tim Wright <timwright12@gmail.com>
2021-05-06 15:53:18 -0400

    Removed metalsmith action from the build

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -286,16 +286,16 @@ module.exports = (env = {}) => {
   }
 
   // Copy over image assets for when metalsmith is removed
-  // baseConfig.plugins.push(
-  //   new CopyPlugin({
-  //     patterns: [
-  //       {
-  //         from: 'src/site/assets',
-  //         to: path.join(outputPath, '..', ''),
-  //       },
-  //     ],
-  //   }),
-  // );
+  baseConfig.plugins.push(
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/site/assets',
+          to: path.join(outputPath, '..', ''),
+        },
+      ],
+    }),
+  );
 
   // Optionally generate landing pages in the absence of a content build.
   if (buildOptions.scaffold) {
@@ -407,16 +407,16 @@ module.exports = (env = {}) => {
     );
 
     // Copy over image assets to fill in the header and other content.
-    baseConfig.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: 'src/site/assets/img',
-            to: path.join(outputPath, '..', 'img'),
-          },
-        ],
-      }),
-    );
+    // baseConfig.plugins.push(
+    //   new CopyPlugin({
+    //     patterns: [
+    //       {
+    //         from: 'src/site/assets/img',
+    //         to: path.join(outputPath, '..', 'img'),
+    //       },
+    //     ],
+    //   }),
+    // );
 
     // Open the browser to either --env.openTo or one of the root URLs of the
     // apps we're scaffolding
