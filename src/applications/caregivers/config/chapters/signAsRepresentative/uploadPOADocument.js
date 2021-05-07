d79b600fb1c1292cb85e6527aef8fe0fdf1f4bb7
LinnJS <justinslinn727@gmail.com>
2021-05-07 15:18:27 -0400

    Add required field to document upload feature

--- src/applications/caregivers/config/chapters/signAsRepresentative/uploadPOADocument.js
+++ src/applications/caregivers/config/chapters/signAsRepresentative/uploadPOADocument.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import fileUploadUI from 'platform/forms-system/src/js/definitions/file';
 import environment from 'platform/utilities/environment';
 import { representativeFields } from 'applications/caregivers/definitions/constants';
@@ -6,46 +7,59 @@ import recordEvent from 'platform/monitoring/record-event';
 
 export default {
   uiSchema: {
-    'ui:title': '',
     'ui:description': RepresentativeDocumentUploadDescription(),
-    [representativeFields.documentUpload]: fileUploadUI('', {
-      buttonText: 'Upload',
-      classNames: 'poa-document-upload',
-      multiple: false,
-      fileUploadUrl: `${environment.API_URL}/v0/form1010cg/attachments`,
-      fileTypes: ['pdf', 'doc', 'docx', 'jpg', 'jpeg', 'rtf', 'png'],
-      maxSize: 1024 * 1024 * 10,
-      hideLabelText: true,
-      createPayload: (file, formId, password) => {
-        const payload = new FormData();
-        payload.append('attachment[file_data]', file);
-        payload.append('form_id', formId);
+    'ui:title': () => (
+      <legend className="vads-u-font-size--h4">
+        Upload your legal representative documentation
+        <span className="vads-u-margin-left--0p5 vads-u-color--secondary-dark vads-u-font-size--sm vads-u-font-weight--normal">
+          (*Required)
+        </span>
+      </legend>
+    ),
+    [representativeFields.documentUpload]: fileUploadUI(
+      'Upload your legal representative documentation',
+      {
+        buttonText: 'Upload',
+        classNames: 'poa-document-upload',
+        multiple: false,
+        fileUploadUrl: `${environment.API_URL}/v0/form1010cg/attachments`,
+        fileTypes: ['pdf', 'doc', 'docx', 'jpg', 'jpeg', 'rtf', 'png'],
+        maxSize: 1024 * 1024 * 10,
+        hideLabelText: true,
+        createPayload: (file, formId, password) => {
+          const payload = new FormData();
+          payload.append('attachment[file_data]', file);
+          payload.append('form_id', formId);
+          payload.append('name', file.name);
 
-        // password for encrypted PDFs
-        if (password) {
-          payload.append('attachment[password]', password);
-        }
+          // password for encrypted PDFs
+          if (password) {
+            payload.append('attachment[password]', password);
+          }
 
-        return payload;
+          return payload;
+        },
+        parseResponse: (fileInfo, file) => {
+          recordEvent({
+            'caregivers-poa-document-guid': fileInfo.data.attributes.guid,
+            'caregivers-poa-document-confirmation-code': fileInfo.data.id,
+          });
+
+          return {
+            guid: fileInfo.data.attributes.guid,
+            confirmationCode: fileInfo.data.id,
+            name: file.name,
+          };
+        },
+        attachmentName: {
+          'ui:title': 'Document name',
+        },
       },
-      parseResponse: fileInfo => {
-        recordEvent({
-          'caregivers-poa-document-guid': fileInfo.data.attributes.guid,
-          'caregivers-poa-document-confirmation-code': fileInfo.data.id,
-        });
-        return {
-          guid: fileInfo.data.attributes.guid,
-          confirmationCode: fileInfo.data.id,
-          name: 'Legal representative documentation',
-        };
-      },
-      attachmentName: {
-        'ui:title': 'Document name',
-      },
-    }),
+    ),
   },
   schema: {
     type: 'object',
+    required: [representativeFields.documentUpload],
     properties: {
       [representativeFields.documentUpload]: {
         type: 'array',
