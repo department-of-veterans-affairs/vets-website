282b3237e31f59860e47345f46ae146468b25417
Oseas Moran <oseas.moran@adhocteam.us>
2021-05-10 15:29:24 -0700

    testing argument on broken-link script

--- script/github-actions/check-broken-links.js
+++ script/github-actions/check-broken-links.js
@@ -1,46 +1,56 @@
 /* eslint-disable no-console */
 
 const fs = require('fs');
-const commandLineArgs = require('command-line-args');
+// const commandLineArgs = require('command-line-args');
+const args = process.argv.slice(2);
 
-const COMMAND_LINE_OPTIONS_DEFINITIONS = [
-  { name: 'branch', alias: 'b', type: String },
-  { name: 'contentOnlyBuild', alias: 'c', type: Boolean, defaultValue: false },
-  { name: 'envName', alias: 'e', type: String },
-  { name: 'path', alias: 'p', type: String },
-  { name: 'serverUrl', alias: 's', type: String },
-];
+// const COMMAND_LINE_OPTIONS_DEFINITIONS = [
+//   { name: 'branch', alias: 'b', type: String },
+//   { name: 'contentOnlyBuild', alias: 'c', type: Boolean, defaultValue: false },
+//   { name: 'envName', alias: 'e', type: String },
+//   { name: 'path', alias: 'p', type: String },
+//   { name: 'serverUrl', alias: 's', type: String },
+// ];
 
-const options = commandLineArgs(COMMAND_LINE_OPTIONS_DEFINITIONS);
-const envName = options.envName;
-const contentOnlyBuild = options.contentOnlyBuild;
-const reportPath = options.path;
-const SERVER_URL = options.serverUrl;
-const BRANCH_NAME = options.branch.replace('refs/heads/', ''); // format: refs/heads/*
+console.log('args', args);
+
+// const options = commandLineArgs(COMMAND_LINE_OPTIONS_DEFINITIONS);
+const envName = args[0];
+const contentOnlyBuild = !!args[1];
+// const reportPath = options.path;
+// const SERVER_URL = options.serverUrl;
+// const BRANCH_NAME = options.branch.replace('refs/heads/', ''); // format: refs/heads/*
+const reportPath = `./logs/${envName}-broken-links.json`;
+const SERVER_URL = `${process.env.GITHUB_SERVER_URL}/${
+  process.env.GITHUB_REPOSITORY
+}/actions/runs/${process.env.GITHUB_RUN_ID}`;
+const BRANCH_NAME = process.env.GITHUB_HEAD_REF;
 const IS_PROD_BRANCH = BRANCH_NAME === 'master';
 const maxBrokenLinks = 10;
 
-let color = 'warning';
+console.log('branch_name', BRANCH_NAME);
+console.log('server_url', SERVER_URL);
+console.log('CB', contentOnlyBuild);
 
 // broken links detected
 if (fs.existsSync(reportPath)) {
   const brokenLinksReport = fs.readFileSync(reportPath, 'utf8');
   const brokenLinks = JSON.parse(brokenLinksReport);
-
-  if (
+  const shouldFail =
     brokenLinks.isHomepageBroken ||
-    brokenLinks.brokenLinksCount > maxBrokenLinks
-  ) {
-    color = 'danger';
-  }
+    brokenLinks.brokenLinksCount > maxBrokenLinks;
+  const color = shouldFail ? 'danger' : 'warning';
 
   const heading = `@cmshelpdesk ${
     brokenLinks.brokenLinksCount
   } broken links found in the ${envName} build on ${BRANCH_NAME} \n\n${SERVER_URL}\n\n`;
-  const message = `${heading}\n${brokenLinks.summary}`; // TODO: stripMargin equiv
+  const message = `${heading}\n${brokenLinks.summary}`;
 
-  console.log(`${brokenLinks.brokenLinksCount} broken links found`);
-  console.log(`::set-output name=SLACK_MESSAGE::${message}`);
+  console.log(
+    `${brokenLinks.brokenLinksCount} broken links found. \n ${
+      brokenLinks.summary
+    }`,
+  );
 
   if (!IS_PROD_BRANCH && !contentOnlyBuild) {
     // Ignore the results of the broken link checker unless
@@ -51,15 +61,10 @@ if (fs.existsSync(reportPath)) {
     return;
   }
 
-  // TODO: Is this still applicable on GHA?
-  // Unset brokenLinks now that we're done with this, because Jenkins may temporarily
-  // freeze (through serialization) this pipeline while the Slack message is being sent.
-  // brokenLinks is an instance of JSONObject, which cannot be serialized by default.
-  // brokenLinks = null;
-
   if (color === 'danger') {
     throw new Error('Broken links found');
   }
 
+  console.log(`::set-output name=SLACK_MESSAGE::${message}`);
   console.log(`::set-output name=SLACK_COLOR::${color}`);
 }
