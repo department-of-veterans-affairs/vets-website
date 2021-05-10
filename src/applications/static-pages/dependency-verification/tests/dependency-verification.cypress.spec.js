ba5d687c29b6125618222cf12ba0057605704a95
Micah Chiang <micahkchiang@gmail.com>
2021-05-10 14:41:59 -0400

    add mock dependents to tests

--- src/applications/static-pages/dependency-verification/tests/dependency-verification.cypress.spec.js
+++ src/applications/static-pages/dependency-verification/tests/dependency-verification.cypress.spec.js
@@ -1,7 +1,10 @@
 import { rootUrl } from 'applications/personalization/view-dependents/manifest.json';
+import mockDependents from 'applications/personalization/view-dependents/tests/e2e/fixtures/mock-dependents.json';
 import mockDiaries from './fixtures/diaries.json';
 
 import { RETRIEVE_DIARIES } from '../utils';
+
+const DEPENDENTS_ENDPOINT = '/dependents_applications/show';
 
 describe('Dependency Verification', () => {
   beforeEach(() => {
@@ -13,6 +16,10 @@ describe('Dependency Verification', () => {
             name: 'dependency_verification',
             value: true,
           },
+          {
+            name: 'va_view_dependents_access',
+            value: true,
+          },
         ],
       },
     });
@@ -21,6 +28,9 @@ describe('Dependency Verification', () => {
 
   it('should render a dependency verification modal', () => {
     cy.login();
+    cy.intercept('GET', DEPENDENTS_ENDPOINT, mockDependents).as(
+      'mockDependents',
+    );
     cy.intercept('GET', '/v0/dependents_verifications', mockDiaries).as(
       'mockDiaries',
     );
@@ -34,6 +44,9 @@ describe('Dependency Verification', () => {
   });
   it('should display a confirmation message when diaries have been updated', () => {
     cy.login();
+    cy.intercept('GET', DEPENDENTS_ENDPOINT, mockDependents).as(
+      'mockDependents',
+    );
     cy.intercept('GET', '/v0/dependents_verifications', mockDiaries).as(
       'mockDiaries',
     );
@@ -56,6 +69,9 @@ describe('Dependency Verification', () => {
   });
   it('should display alert when diaries update has failed', () => {
     cy.login();
+    cy.intercept('GET', DEPENDENTS_ENDPOINT, mockDependents).as(
+      'mockDependents',
+    );
     cy.intercept('GET', '/v0/dependents_verifications', mockDiaries).as(
       'mockDiaries',
     );
