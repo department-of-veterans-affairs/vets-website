817763026aa99e00fc900e93ae1bac3ee4769c7e
Jeff Balboni <jeff@adhocteam.us>
2021-05-10 10:52:38 -0400

    Disable the right test

--- src/applications/vaos/tests/e2e/appointment-list.cypress.spec.js
+++ src/applications/vaos/tests/e2e/appointment-list.cypress.spec.js
@@ -223,7 +223,7 @@ describe('VAOS appointment list refresh', () => {
         .should('exist');
     });
 
-    it.skip('should navigate to requested appointment details', () => {
+    it('should navigate to requested appointment details', () => {
       cy.get('[data-cy=requested-appointment-list-item]')
         .first()
         .findByText(/Details/i)
@@ -254,7 +254,7 @@ describe('VAOS appointment list refresh', () => {
       cy.axeCheckBestPractice();
     });
 
-    it('should navigate to requested appointment details', () => {
+    it.skip('should navigate to requested appointment details', () => {
       cy.get('[data-cy=appointment-list-item]')
         .first()
         .findByText(/Details/i)
