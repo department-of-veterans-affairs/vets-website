7a69f4692e941ff4ba304fe70107e605cd27f9ac
Micah Chiang <micahkchiang@gmail.com>
2021-05-12 13:27:17 -0400

    updates to registry.json and coe app

--- src/applications/lgy/coe/containers/App.jsx
+++ src/applications/lgy/coe/containers/App.jsx
@@ -3,26 +3,12 @@ import { connect } from 'react-redux';
 import { isLoggedIn } from 'platform/user/selectors';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from '../config/form';
+import { getFormPagePaths } from '../utils';
 
-const FORM_PATHS = [
-  'applicant-information-summary',
-  'applicant-contact-information',
-  'applicant-communication-preference',
-  'service-status',
-  'service-history',
-  'service-decoration',
-  'existing-loan-screener',
-  'loan-intent',
-  'loan-history',
-  'document-upload-summary',
-  'upload-supporting-documents',
-  'review-and-submit',
-  'confirmation',
-];
+const FORM_PATHS = getFormPagePaths(formConfig);
 
 function App({ location, children, loggedIn, router }) {
   const formPath = window.location.pathname.split('/').pop();
-
   if (!loggedIn && FORM_PATHS.includes(formPath)) {
     router.push('/introduction');
   }
