385bad2abeb0c11ef1696c42f37f7240219c645e
Devin McCurdy <mccurdy_devin@bah.com>
2021-05-14 08:27:32 -0400

    Styling fix

--- src/applications/gi-sandbox/containers/NameSearchForm.jsx
+++ src/applications/gi-sandbox/containers/NameSearchForm.jsx
@@ -56,6 +56,7 @@ export function NameSearchForm({
           <div className="medium-screen:vads-l-col--10">
             <KeywordSearch
               version={versionId}
+              className="name-search"
               placeholder="school, employer, or training provider"
               autocomplete={autocomplete}
               onClearAutocompleteSuggestions={clearNameAutocompleteSuggestions}
