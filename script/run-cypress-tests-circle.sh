374f551760b82054b8d6c02fc0b3383b2bb43c30
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-05-17 14:17:13 -0500

    WIP

--- script/run-cypress-tests-circle.sh
+++ script/run-cypress-tests-circle.sh
@@ -13,5 +13,5 @@ else
   reporterArgs="--reporter cypress-multi-reporters --reporter-options \"configFile=config/cypress-reporters.json\""
 
   # Start the web server & run Cypress tests.
-  node src/platform/testing/e2e/test-server.js --buildtype=vagovprod --port=3001
+  node src/platform/testing/e2e/test-server.js --buildtype=vagovprod --port=3001 > /dev/null 2>&1 && yarn cy:run
 fi
