b0ac831eb99603efae0bb07b361ecee1d78d515c
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-05-17 09:08:27 -0500

    WIP

--- src/applications/personalization/dashboard-2/tests/e2e/messaging-error.cypress.spec.js
+++ src/applications/personalization/dashboard-2/tests/e2e/messaging-error.cypress.spec.js
@@ -11,7 +11,7 @@ describe('MyVA Dashboard - Messaging', () => {
     beforeEach(() => {
       mockLocalStorage();
       const mockUser = makeUserObject({
-        messaging: false,
+        messaging: true,
         rx: true,
         isPatient: true,
       });
