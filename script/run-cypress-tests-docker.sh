d870e3c4e34b19aa5f7fd7359694ad2bea4d7019
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-05-17 10:49:30 -0500

    Try different port

---
+++ script/run-cypress-tests-docker.sh
@@ -0,0 +1,8 @@
+if [ "$(find src -name '*.cypress.spec.js' | wc -l)" -eq 0 ]; then
+  echo "No Cypress tests found."
+  exit 0
+else
+  export CYPRESS_BASE_URL=http://vets-website:3002
+  export CYPRESS_CI=$CI
+  yarn cy:run
+fi
