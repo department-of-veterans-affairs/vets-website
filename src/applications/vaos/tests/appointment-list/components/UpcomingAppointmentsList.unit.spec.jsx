b0cae5be4346d5d7b8082dc2f28d524fec798cf4
linuxlance <LanceGundersen@users.noreply.github.com>
2021-05-17 15:31:49 -0400

    [23718] Remove express care from appt list (#17165)
    * Remove express care from appt list
    
    * Filter express care appts
    
    * Add expressCare filter to past appt v2
    
    * Feedback adjustments
    
    * Fix bad filter code
    
    * Feedback adjustments
    
    * Revert feature flag
    
    * Remove facility name

--- src/applications/vaos/tests/appointment-list/components/UpcomingAppointmentsList.unit.spec.jsx
+++ src/applications/vaos/tests/appointment-list/components/UpcomingAppointmentsList.unit.spec.jsx
@@ -13,7 +13,6 @@ import {
   getCCAppointmentMock,
   getVAAppointmentMock,
   getVAFacilityMock,
-  getVARequestMock,
 } from '../../mocks/v0';
 import { mockAppointmentInfo, mockFacilitiesFetch } from '../../mocks/helpers';
 import {
@@ -25,8 +24,6 @@ import UpcomingAppointmentsList from '../../../appointment-list/components/Upcom
 const initialState = {
   featureToggles: {
     vaOnlineSchedulingCancel: true,
-    vaExpressCare: true,
-    vaExpressCareNew: true,
     vaOnlineSchedulingHomepageRefresh: true,
   },
 };
@@ -497,128 +494,6 @@ describe('VAOS <UpcomingAppointmentsList>', () => {
     );
 
     expect(screen.baseElement).to.contain.text('Community care');
-  });
-
-  it('should show express care appointment text', async () => {
-    const startDate = moment.utc();
-    const appointment = getVARequestMock();
-    appointment.attributes = {
-      ...appointment.attributes,
-      status: 'Submitted',
-      date: startDate,
-      optionDate1: startDate,
-      optionTime1: 'AM',
-      purposeOfVisit: 'New Issue',
-      bestTimetoCall: ['Morning'],
-      email: 'patient.test@va.gov',
-      phoneNumber: '5555555566',
-      typeOfCareId: 'CR1',
-      reasonForVisit: 'Back pain',
-      friendlyLocationName: 'Some VA medical center',
-      appointmentType: 'Express Care',
-      comment: 'loss of smell',
-      facility: {
-        ...appointment.attributes.facility,
-        facilityCode: '983GC',
-      },
-    };
-    appointment.id = '1234';
-    mockAppointmentInfo({ requests: [appointment], isHomepageRefresh: true });
-
-    const facility = {
-      id: 'vha_442GC',
-      attributes: {
-        ...getVAFacilityMock().attributes,
-        uniqueId: '442GC',
-        name: 'Cheyenne VA Medical Center',
-        address: {
-          physical: {
-            zip: '82001-5356',
-            city: 'Cheyenne',
-            state: 'WY',
-            address1: '2360 East Pershing Boulevard',
-          },
-        },
-        phone: {
-          main: '307-778-7550',
-        },
-      },
-    };
-    mockFacilitiesFetch('vha_442GC', [facility]);
-
-    const screen = renderWithStoreAndRouter(<UpcomingAppointmentsList />, {
-      initialState,
-      reducers,
-    });
-
-    await screen.findByText(new RegExp(startDate.format('dddd, MMMM D'), 'i'));
-
-    expect(screen.queryByText(/You don’t have any appointments/i)).not.to.exist;
-    expect(screen.baseElement).to.contain.text(
-      'A VA health care provider will follow up with you today.',
-    );
-    expect(screen.baseElement).to.contain.text('Express Care request');
-  });
-
-  it('should show canceled express care appointment text', async () => {
-    const startDate = moment.utc();
-    const appointment = getVARequestMock();
-    appointment.attributes = {
-      ...appointment.attributes,
-      status: 'Cancelled',
-      date: startDate,
-      optionDate1: startDate,
-      optionTime1: 'AM',
-      purposeOfVisit: 'New Issue',
-      bestTimetoCall: ['Morning'],
-      email: 'patient.test@va.gov',
-      phoneNumber: '5555555566',
-      typeOfCareId: 'CR1',
-      reasonForVisit: 'Back pain',
-      friendlyLocationName: 'Some VA medical center',
-      appointmentType: 'Express Care',
-      comment: 'loss of smell',
-      facility: {
-        ...appointment.attributes.facility,
-        facilityCode: '983GC',
-      },
-    };
-    appointment.id = '1234';
-    mockAppointmentInfo({ requests: [appointment], isHomepageRefresh: true });
-
-    const facility = {
-      id: 'vha_442GC',
-      attributes: {
-        ...getVAFacilityMock().attributes,
-        uniqueId: '442GC',
-        name: 'Cheyenne VA Medical Center',
-        address: {
-          physical: {
-            zip: '82001-5356',
-            city: 'Cheyenne',
-            state: 'WY',
-            address1: '2360 East Pershing Boulevard',
-          },
-        },
-        phone: {
-          main: '307-778-7550',
-        },
-      },
-    };
-    mockFacilitiesFetch('vha_442GC', [facility]);
-    const screen = renderWithStoreAndRouter(<UpcomingAppointmentsList />, {
-      initialState,
-      reducers,
-    });
-
-    await screen.findByText(new RegExp(startDate.format('dddd, MMMM D'), 'i'));
-
-    expect(screen.queryByText(/You don’t have any appointments/i)).not.to.exist;
-    expect(screen.baseElement).to.contain.text('Canceled');
-    expect(screen.baseElement).not.to.contain.text(
-      'A VA health care provider will follow up with you today.',
-    );
-    expect(screen.baseElement).to.contain.text('Express Care request');
   });
 
   it('should show phone call appointment text', async () => {
