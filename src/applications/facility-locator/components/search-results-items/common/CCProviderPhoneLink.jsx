777417505ec6949d442f2107d78048479506f6ae
Gaida Escobar <gaidaescobar@gescobar-C02Z18JJLVCG.local>
2021-05-16 21:53:10 -0400

    Refactored LocationPhoneLink.jsx

---
+++ src/applications/facility-locator/components/search-results-items/common/CCProviderPhoneLink.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  LocationType,
+  CLINIC_URGENTCARE_SERVICE,
+  PHARMACY_RETAIL_SERVICE,
+} from '../../../constants';
+
+const CCProviderPhoneLink = ({ location, query, renderPhoneNumber }) => {
+  const { caresitePhone: phone } = location.attributes;
+  const isCCProvider =
+    query &&
+    query.facilityType === LocationType.CC_PROVIDER &&
+    query.serviceType !== CLINIC_URGENTCARE_SERVICE &&
+    query.serviceType !== PHARMACY_RETAIL_SERVICE;
+
+  return (
+    <div>
+      {renderPhoneNumber('Main number', null, phone, true)}
+      {isCCProvider && (
+        <p id={'referral-message'}>
+          If you don’t have a referral, contact your local VA medical center.
+        </p>
+      )}
+    </div>
+  );
+};
+
+CCProviderPhoneLink.propTypes = {
+  location: PropTypes.object,
+  query: PropTypes.object,
+  renderPhoneNumber: PropTypes.func.isRequired,
+};
+
+export default CCProviderPhoneLink;
