12aa8436f1bc7ec8c70d2998c9327f0126d82099
Robin Garrison <robin.garrison@adhocteam.us>
2021-05-17 15:05:38 -0500

    Add issue summary page

---
+++ src/applications/appeals/10182/content/issueSummary.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+import formConfig from '../config/form';
+import { getSelected } from '../utils/helpers';
+import { ShowIssuesList } from '../components/ShowIssuesList';
+
+export const SummaryTitle = ({ formData }) => {
+  const { path } = formConfig.chapters.conditions.pages.contestableIssues;
+  const issues = getSelected(formData);
+
+  return (
+    <p>
+      Below is a list of the issues that you are requesting the Board to review.
+      If an issue is missing, please{' '}
+      <Link
+        aria-label="go back and add any missing disabilities"
+        to={{
+          pathname: path,
+          search: '?redirect',
+        }}
+      >
+        go back and add it
+      </Link>
+      .{ShowIssuesList(issues)}
+    </p>
+  );
+};
