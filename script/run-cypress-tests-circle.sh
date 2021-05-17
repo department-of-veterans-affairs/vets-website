2322b1de2509efdfc714714ee8cd855d181aa5ed
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-05-17 16:56:25 -0500

    Add content build steps

--- script/run-cypress-tests-circle.sh
+++ script/run-cypress-tests-circle.sh
@@ -13,6 +13,5 @@ else
   reporterArgs="--reporter cypress-multi-reporters --reporter-options \"configFile=config/cypress-reporters.json\""
 
   # Start the web server & run Cypress tests.
-  node src/platform/testing/e2e/test-server.js --buildtype=vagovprod --port=3001 > /dev/null 2>&1 &
-  yarn cy:run
+  yarn start-server-and-test "node src/platform/testing/e2e/test-server.js --buildtype=vagovprod --port=3001 > /dev/null 2>&1" :3001 "yarn cy:run $reporterArgs"
 fi
