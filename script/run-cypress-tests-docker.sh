58f3e002403cc21afd9369794c7ccd0405ba03db
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-05-17 11:13:46 -0500

    WIP

--- script/run-cypress-tests-docker.sh
+++ script/run-cypress-tests-docker.sh
@@ -2,7 +2,7 @@ if [ "$(find src -name '*.cypress.spec.js' | wc -l)" -eq 0 ]; then
   echo "No Cypress tests found."
   exit 0
 else
-  export CYPRESS_BASE_URL=http://vets-website:3002
+  export CYPRESS_BASE_URL=http://vets-website:3001
   export CYPRESS_CI=$CI
   yarn cy:run
 fi
