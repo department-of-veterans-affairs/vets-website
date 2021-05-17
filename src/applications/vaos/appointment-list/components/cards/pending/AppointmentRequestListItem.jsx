ce8bbe2f0c906fb4d6453fd36794251d4baa0c71
linuxlance <LanceGundersen@users.noreply.github.com>
2021-05-17 14:22:54 -0400

    Remove facility name

--- src/applications/vaos/appointment-list/components/cards/pending/AppointmentRequestListItem.jsx
+++ src/applications/vaos/appointment-list/components/cards/pending/AppointmentRequestListItem.jsx
@@ -79,7 +79,6 @@ export default function AppointmentRequestListItem({
               appointment={appointment}
             />
           )}
-          {facility?.name}
           {!isCC && (
             <VAFacilityLocation
               facility={facility}
