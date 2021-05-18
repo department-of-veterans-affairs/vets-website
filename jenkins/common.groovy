39d3f010169e4eade714ffd8b35762bb58c18811
Tim Wright <timwright12@gmail.com>
2021-05-18 15:41:04 -0400

    Can we use yarn in CI?

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -103,7 +103,7 @@ def setup() {
 def build(String ref, dockerContainer, String envName) {
   dockerContainer.inside(DOCKER_ARGS) {
     def buildLogPath = "/application/${envName}-build.log"
-    sh "yarn build -- --evn.envName=${envName} --env.buildLog=${buildLogPath} --verbose"
+    sh "cd /application && yarn build -- --evn.envName=${envName} --env.buildLog=${buildLogPath} --verbose"
     // sh "cd /application && jenkins/build.sh --envName ${envName} --buildLog ${buildLogPath} --verbose"
   }
 }
