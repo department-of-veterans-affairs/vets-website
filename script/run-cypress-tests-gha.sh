8db5c3e62721e094077745e220538816917388d9
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-05-18 15:16:46 -0500

    Test to see if artifacts are saved

--- script/run-cypress-tests-gha.sh
+++ script/run-cypress-tests-gha.sh
@@ -9,7 +9,7 @@ else
   export CYPRESS_CI=$CI
 
   # Use mocha-junit-reporter and save results in './test-results'.
-  reporterArgs="--reporter cypress-multi-reporters --reporter-options \"configFile=config/cypress-reporters.json\""
+  reporterArgs="--reporter cypress-multi-reporters --reporter-options \"configFile=config/cypress-reporters.json\" --spec \"src/applications/coronavirus-vaccination/tests/e2e/hideauth.cypress.spec.js,src/applications/coronavirus-vaccination/tests/e2e/unsubscribe.cypress.spec.js\""
 
   # Run Cypress tests.
   yarn cy:run $reporterArgs
