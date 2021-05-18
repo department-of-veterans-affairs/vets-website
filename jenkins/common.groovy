580b9098524ae70afd2bb3414f1168bad457afb3
Tim Wright <timwright12@gmail.com>
2021-05-18 16:47:26 -0400

    restore npm in Jenkins

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -103,8 +103,7 @@ def setup() {
 def build(String ref, dockerContainer, String envName) {
   dockerContainer.inside(DOCKER_ARGS) {
     def buildLogPath = "/application/${envName}-build.log"
-    sh "cd /application && yarn build --env.buildtype=${envName} --env.buildLog=${buildLogPath} --verbose"
-    // sh "cd /application && jenkins/build.sh --envName ${envName} --buildLog ${buildLogPath} --verbose"
+    sh "cd /application && jenkins/build.sh --envName ${envName} --buildLog ${buildLogPath} --verbose"
   }
 }
 
