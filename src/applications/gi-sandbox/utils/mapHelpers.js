cb1010700f4a5ef1e7294c9a4042ccce153c9840
Matt Roth <roth_matthew@bah.com>
2021-05-19 13:58:06 -0400

    cleanup

--- src/applications/gi-sandbox/utils/mapHelpers.js
+++ src/applications/gi-sandbox/utils/mapHelpers.js
@@ -32,3 +32,59 @@ export const radiusFromBoundingBox = fbox => {
 
   return Math.max(radius, MIN_RADIUS);
 };
+
+const BOUNDING_RADIUS = 0.5;
+/**
+ * Calculates a bounding box (±BOUNDING_RADIUS°) centering on the current
+ * address string as typed by the user.
+ *
+ * @param features object from MapBox call
+ * @return {{searchCoords: {lng: *, lat: *}, zoomLevel: number, context: *, bounds: (number|*)[], id: number, position: {latitude: *, longitude: *}, searchArea: null, radius: number, results: [], mapBoxQuery: {placeType: *, placeName: *}}} */
+export const genBBoxFromGeocode = features => {
+  const zip = features[0].context.find(v => v.id.includes('postcode')) || {};
+  const coordinates = features[0].center;
+  const latitude = coordinates[1];
+  const longitude = coordinates[0];
+  const zipCode = zip.text || features[0].place_name;
+  const featureBox = features[0].box;
+
+  let minBounds = [
+    longitude - BOUNDING_RADIUS,
+    latitude - BOUNDING_RADIUS,
+    longitude + BOUNDING_RADIUS,
+    latitude + BOUNDING_RADIUS,
+  ];
+
+  if (featureBox) {
+    minBounds = [
+      Math.min(featureBox[0], longitude - BOUNDING_RADIUS),
+      Math.min(featureBox[1], latitude - BOUNDING_RADIUS),
+      Math.max(featureBox[2], longitude + BOUNDING_RADIUS),
+      Math.max(featureBox[3], latitude + BOUNDING_RADIUS),
+    ];
+  }
+
+  const radius = radiusFromBoundingBox(features);
+
+  return {
+    radius,
+    context: zipCode,
+    id: Date.now(),
+    position: {
+      latitude,
+      longitude,
+    },
+    searchCoords: {
+      lat: features[0].geometry.coordinates[1],
+      lng: features[0].geometry.coordinates[0],
+    },
+    bounds: minBounds,
+    zoomLevel: features[0].id.split('.')[0] === 'region' ? 7 : 9,
+    mapBoxQuery: {
+      placeName: features[0].place_name,
+      placeType: features[0].place_type[0],
+    },
+    searchArea: null,
+    results: [],
+  };
+};
