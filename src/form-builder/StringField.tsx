853c8f3ba6cf0b3302aea29d21fcffcbac8a7a87
Christopher Valarida <chris.valarida@adhocteam.us>
2021-05-21 20:40:50 -0700

    TextInput -> StringField

--- src/form-builder/StringField.tsx
+++ src/form-builder/StringField.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Field } from "formik";
 
 // TODO: Abstract out all field props into a single interface
-const TextInput = (props: { name: string }) => (
+const StringField = (props: { name: string }) => (
   <Field type="string" {...props} />
 );
+
+export default StringField;
