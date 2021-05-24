54d0560284f8302021b026b1b2f3d5353084f512
Gibran Rodriguez <grodriguez@governmentcio.com>
2021-05-24 10:21:30 -0500

    set up vet center near by widget

--- src/applications/static-pages/facilities/createNearByVetCenters.jsx
+++ src/applications/static-pages/facilities/createNearByVetCenters.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-export default async function createVetCentersHours(store) {
+export default async function createNearByVetCenters(store) {
   const nearByVetCentersWidget = document.querySelector(
-    `[data-widget-type="near-by-vet-centers"]`,
+    `[data-widget-type="vet-center-nearby"]`,
   );
 
   if (nearByVetCentersWidget) {
     const { default: NearByVetCenters } = await import('./nearByVetCenters');
     ReactDOM.render(
       <Provider store={store}>
-        <NearByVetCenters hours={window.fieldNearbyVetCenters} />
+        <NearByVetCenters hours={window.nearbyVetCenters} />
       </Provider>,
       nearByVetCentersWidget,
     );
