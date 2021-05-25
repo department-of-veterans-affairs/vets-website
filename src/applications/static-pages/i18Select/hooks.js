0ce8b0a5565df6406d8bc4268237fb038913d4f7
Fareez Ahmed <fareezahmed@Fareez-GCIO.local>
2021-05-25 14:56:31 -0700

    fix

--- src/applications/static-pages/i18Select/hooks.js
+++ src/applications/static-pages/i18Select/hooks.js
@@ -13,7 +13,7 @@ export const setLangAttribute = lang => {
 };
 
 // set lang code as `en` for all links on the current page
-// unless it follows the old IA lang patterns or already has an hreflang attribute
+// unless it follows the old IA lang patterns or already has a lang attribute
 // without overwriting existing onclick events
 // reference: https://stackoverflow.com/questions/891989/javascript-adding-an-onclick-handler-without-overwriting-the-existing-one
 
@@ -22,9 +22,9 @@ export const adaptLinksWithLangCode = setLangAttributeInReduxStore => {
   for (const link of links) {
     link.addEventListener('click', () => {
       setLangAttributeInReduxStore('en');
-
-      if (link.hreflang) {
-        setLangAttributeInReduxStore(link.hreflang);
+      const langAttribute = link.hreflang || link.lang;
+      if (langAttribute) {
+        setLangAttributeInReduxStore(langAttribute);
       }
       // respect the temp IA i18 structure
       if (link.href.endsWith('-esp/')) {
