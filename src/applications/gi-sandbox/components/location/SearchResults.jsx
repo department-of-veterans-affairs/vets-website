9f9354137883a259bf185a976c473c3ab327456d
Matt Roth <roth_matthew@bah.com>
2021-05-25 16:44:29 -0400

    Sandbox location search result cards and filters

--- src/applications/gi-sandbox/components/location/SearchResults.jsx
+++ src/applications/gi-sandbox/components/location/SearchResults.jsx
@@ -1,33 +1,80 @@
 import React from 'react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 import SearchResultCard from '../search/SearchResultCard';
 import TuitionAndHousingEstimates from '../../containers/TuitionAndHousingEstimates';
 import RefineYourSearch from '../../containers/RefineYourSearch';
+import { numberToLetter } from '../../utils/helpers';
 
 export default function SearchResults({ search }) {
+  const { count, results } = search.location;
+  const { location } = search.query;
+
+  const resultCards = results.map((institution, index) => {
+    const { name, city, state, distance } = institution;
+    const miles = Number.parseFloat(distance).toFixed(2);
+    const letter = numberToLetter(index + 1);
+
+    const header = (
+      <>
+        <div className="location-header vads-u-display--flex vads-u-padding-top--1">
+          <span className="location-letter vads-u-font-size--sm">{letter}</span>
+          <span className="vads-u-padding-x--0p5 vads-u-font-size--sm">
+            <strong>{miles} miles</strong>
+          </span>
+          <span className="vads-u-font-size--sm">{`${city}, ${state}`}</span>
+        </div>
+        <div>
+          <h3 className="vads-u-margin-top--2">{name}</h3>
+        </div>
+      </>
+    );
+
+    return (
+      <SearchResultCard
+        institution={institution}
+        key={institution.facilityCode}
+        header={header}
+        location
+      />
+    );
+  });
+
   return (
     <>
-      {search.location.count > 0 && (
-        <div>
-          <div>
-            <TuitionAndHousingEstimates />
-            <RefineYourSearch />
-            <div className="usa-grid vads-u-padding--1">
-              <p>
-                Showing <strong>{search.location.count} search results</strong>{' '}
-                for '<strong>{search.query.location}</strong>'
-              </p>
-              <div className="vads-l-row vads-u-flex-wrap--wrap">
-                {search.location.results.map(institution => (
-                  <SearchResultCard
-                    institution={institution}
-                    key={institution.id}
-                  />
-                ))}
-              </div>
+      <div className={'location-search'}>
+        <div className={'usa-width-one-third'}>
+          {count === null && (
+            <div>
+              Please enter a location (street, city, state, or postal code) then
+              click search above to find institutions.
             </div>
-          </div>
+          )}
+          {location !== '' && (
+            <>
+              <TuitionAndHousingEstimates />
+              <RefineYourSearch />
+              <div className="location-search-results-container usa-grid vads-u-padding--1p5">
+                <p>
+                  Showing <strong>{count} search results</strong> for '
+                  <strong>{location}</strong>'
+                </p>
+                <div
+                  id="location-search-results"
+                  className="location-search-results vads-l-row vads-u-flex-wrap--wrap"
+                >
+                  {resultCards}
+                </div>
+              </div>
+            </>
+          )}
+          {count === 0 && (
+            <div>We didn't find any facilities near the location.</div>
+          )}
         </div>
-      )}
+
+        <div className={'usa-width-two-thirds'} />
+      </div>
     </>
   );
 }
