07ebfbe06270024637ac1d7fad437c3ed357610e
Robin Garrison <robin.garrison@adhocteam.us>
2021-05-26 16:15:02 -0500

    Update additional issue content

--- src/applications/appeals/10182/content/additionalIssues.jsx
+++ src/applications/appeals/10182/content/additionalIssues.jsx
@@ -16,8 +16,15 @@ export const addIssuesIntroLabel =
   'Would you like to add any more issues for review?';
 
 export const AdditionalIssuesLabel = (
-  <span className="vads-u-font-weight--normal vads-u-font-size--base vads-u-font-family--sans">
-    Add an issue and our decision date on this issue. You can find the decision
-    date on your decision notice (the letter you got in the mail from us).
-  </span>
+  <>
+    <span className="vads-u-font-weight--normal vads-u-font-size--base vads-u-font-family--sans">
+      Add an issue and our decision date on this issue. You can find the
+      decision date on your decision notice (the letter you got in the mail from
+      us).
+    </span>
+    <p className="vads-u-font-weight--normal vads-u-font-size--base vads-u-font-family--sans">
+      <strong>Note:</strong> You can only add an issue that you’ve already
+      received a VA decision notice for.
+    </p>
+  </>
 );
