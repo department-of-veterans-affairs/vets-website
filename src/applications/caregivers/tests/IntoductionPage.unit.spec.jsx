df7901098af3a343e1a79d263844697d4fa29d38
dat-gcio <dtran@governmentcio.com>
2021-05-27 10:05:57 -0400

    fix up unit test dependencies and mocks

--- src/applications/caregivers/tests/IntoductionPage.unit.spec.jsx
+++ src/applications/caregivers/tests/IntoductionPage.unit.spec.jsx
@@ -1,28 +1,59 @@
 import React from 'react';
 import { expect } from 'chai';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
 import { IntroductionPage } from '../containers/IntroductionPage';
-import { shallow } from 'enzyme';
 
-const defaultProps = {
-  router: [],
-  route: { formConfig: {} },
-  formData: {},
-  setFormData: () => {},
-  canUpload1010cgPOA: false,
-};
+const getData = ({ canUpload1010cgPOA = true } = {}) => ({
+  mockProps: {
+    router: [],
+    route: { formConfig: {} },
+    formData: {},
+    setFormData: () => {},
+    canUpload1010cgPOA,
+  },
+  mockStore: {
+    getState: () => ({
+      featureToggles: {
+        canUpload1010cgPOA,
+      },
+      scheduledDowntime: {
+        globalDowntime: null,
+        isReady: true,
+        isPending: false,
+        serviceMap: {
+          get() {},
+        },
+        dismissedDowntimeWarnings: [],
+      },
+    }),
+    subscribe: () => {},
+    dispatch: () => {},
+  },
+});
 
 describe('IntroductionPage', () => {
-  it('should not render poa note', () => {
-    const wrapper = shallow(<IntroductionPage {...defaultProps} />);
+  it('should render poa note', () => {
+    const { mockProps, mockStore } = getData({ canUpload1010cgPOA: true });
+    const tree = mount(
+      <Provider store={mockStore}>
+        <IntroductionPage {...mockProps} />,
+      </Provider>,
+    );
 
-    expect(wrapper.find('[data-testid="poa-info-note"]')).to.exist;
-    wrapper.unmount();
+    expect(tree.find('[data-testid="poa-info-note"]').exists()).to.be.true;
+    tree.unmount();
   });
 
-  it('should render poa note', () => {
-    const wrapper = shallow(<IntroductionPage {...defaultProps} />);
+  it('should not render poa note', () => {
+    const { mockProps, mockStore } = getData({ canUpload1010cgPOA: false });
+    const tree = mount(
+      <Provider store={mockStore}>
+        <IntroductionPage {...mockProps} />,
+      </Provider>,
+    );
 
-    expect(wrapper.find('[data-testid="poa-info-note"]')).to.not.exist;
-    wrapper.unmount();
+    expect(tree.find('[data-testid="poa-info-note"]').exists()).to.be.false;
+    tree.unmount();
   });
 });
