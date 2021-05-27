57a7f061f36e992b726e0cb763f25ae8e8df2057
Fareez Ahmed <fareezahmed@Fareez-GCIO.local>
2021-05-27 16:00:51 -0700

    clean up hooks and hrefs

--- src/platform/site-wide/va-footer/components/LanguageSupport.jsx
+++ src/platform/site-wide/va-footer/components/LanguageSupport.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import {
   setLangAttribute,
   adaptLinksWithLangCode,
+  onThisPageHook,
 } from 'applications/static-pages/i18Select/hooks';
 import { FOOTER_EVENTS } from '../helpers';
 import recordEvent from '../../../monitoring/record-event';
@@ -11,22 +12,17 @@ function LanguagesListTemplate({ langSelected }) {
     <ul>
       {[
         {
-          onThisPage: 'En esta página',
           label: 'Español',
-          suffix: '-esp/',
           lang: 'es',
           href: '/asistencia-y-recursos-en-espanol',
         },
         {
-          suffix: '-tag/',
           label: 'Tagalog',
-          onThisPage: 'Sa pahinang ito',
           lang: 'tl',
           href: '/tagalog-wika-mapagkukunan-at-tulong',
         },
         {
           label: 'Other languages',
-          suffix: '/',
           lang: 'en',
           href: '/resources/how-to-get-free-language-assistance-from-va/',
         },
@@ -68,11 +64,12 @@ export default function LanguageSupport({
 
   useEffect(
     () => {
-      if (showLangSupport && languageCode) {
+      if (languageCode) {
         setLangAttribute(languageCode);
+        onThisPageHook(languageCode);
       }
     },
-    [languageCode, showLangSupport],
+    [languageCode],
   );
   if (showLangSupport !== true) return null;
 
