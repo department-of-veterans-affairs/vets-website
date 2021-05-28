b1687fac7096d85aa0f768c861d2fad1198046c7
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2021-05-28 05:24:26 -0400

    Added dataDir and deprecated fixtures in form tester config. Updated docs.

--- src/platform/testing/e2e/cypress/support/form-tester/index.js
+++ src/platform/testing/e2e/cypress/support/form-tester/index.js
@@ -457,15 +457,9 @@ Cypress.Commands.add('fillPage', () => {
  * @property {string} [dataPrefix] - The path prefix for accessing nested
  *     test data. For example, if the test data looks like
  *     { data: { field1: 'value' } }, dataPrefix should be set to 'data'.
- * @property {string[]} dataSets - Array of fixture file paths to test data
- *     relative to the "data" path loaded into fixtures. For example,
- *     if the fixtures object maps the "data" path to "some/folder/path",
- *     which contains a "test.json" file, dataSets can be set to ['test']
- *     to use that file as a data set. A test is generated for each data set
- *     and uses that data to fill out fields during the form flow.
- * @property {Object} fixtures - Paths to files or directories (relative to
- *     project root) to load as fixtures, with object keys as fixture paths.
- *     The "data" fixture path is _required_ to properly set up "dataSets".
+ * @property {string} dataDir - Path to test data directory.
+ * @property {string[]} dataSets - Test data file paths relative to dataDir.
+ *     A test is generated for each data set and uses that data to fill out fields.
  * @property {Object.<function>} [pageHooks] - Functions (hooks) that override
  *     the automatic form filling on specified pages. Each object key is the
  *     URL of the page that triggers the corresponding hook.
@@ -484,8 +478,9 @@ const testForm = testConfig => {
     appName,
     arrayPages = [],
     dataPrefix,
+    dataDir,
     dataSets,
-    fixtures,
+    fixtures, // Deprecated in favor of `dataDir`.
     pageHooks = {},
     rootUrl,
     setup = () => {},
@@ -503,8 +498,8 @@ const testForm = testConfig => {
 
   testSuite(appName, () => {
     before(() => {
-      if (!fixtures.data) {
-        throw new Error('Required data fixture is undefined.');
+      if (!dataDir && !fixtures.data) {
+        throw new Error('Required data directory is undefined.');
       }
 
       setup();
@@ -536,7 +531,7 @@ const testForm = testConfig => {
       testCase(testKey, () => {
         beforeEach(() => {
           cy.wrap(testKey).as('testKey');
-          cy.fixture(`${fixtures.data}/${testKey}`)
+          cy.fixture(`${dataDir || fixtures.data}/${testKey}`)
             .then(extractTestData)
             .as('testData')
             .then(setupPerTest);
