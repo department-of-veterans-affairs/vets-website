476e38a9308be73a4ebbb91b4702093c215d838a
Jeff Balboni <jeff@adhocteam.us>
2021-06-01 09:50:26 -0400

    Move screen reader instructions after Next in VAOS calendar (#17417)

--- src/applications/vaos/components/calendar/CalendarNavigation.jsx
+++ src/applications/vaos/components/calendar/CalendarNavigation.jsx
@@ -27,14 +27,6 @@ const CalendarNavigation = ({
     >
       {momentMonth.format('MMMM YYYY')}
     </h2>
-    <div
-      className="sr-only"
-      id={`vaos-calendar-instructions-${momentMonth.month()}`}
-    >
-      Press the Enter key to expand the day you want to schedule an appointment.
-      Then press the Tab key or form shortcut key to select an appointment time.
-    </div>
-
     <button
       className="vaos-calendar__nav-links-button vads-u-display--flex vads-u-justify-content--flex-end vads-u-align-items--center vads-u-font-weight--normal vads-u-padding--0 vads-u-margin-bottom--0 vads-u-margin-top--0p5  vads-u-color--primary"
       onClick={nextOnClick}
@@ -46,6 +38,13 @@ const CalendarNavigation = ({
       </span>
       <i className="fas fa-chevron-circle-right vads-u-margin-left--1" />
     </button>
+    <div
+      className="sr-only"
+      id={`vaos-calendar-instructions-${momentMonth.month()}`}
+    >
+      Press the Enter key to expand the day you want to schedule an appointment.
+      Then press the Tab key or form shortcut key to select an appointment time.
+    </div>
   </div>
 );
 
