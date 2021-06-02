4ed7c20d67a3829390d0c6c4f048d57487f532c0
Jeff Balboni <jeff@adhocteam.us>
2021-06-02 10:10:58 -0400

    Revert change

--- src/applications/vaos/appointment-list/components/CommunityCareAppointmentDetailsPage.jsx
+++ src/applications/vaos/appointment-list/components/CommunityCareAppointmentDetailsPage.jsx
@@ -161,17 +161,15 @@ export default function CommunityCareAppointmentDetailsPage() {
         </div>
       )}
 
-      {!isPastAppointment && (
-        <div className="vads-u-margin-top--2 vaos-appts__block-label vaos-hide-for-print">
-          <i
-            aria-hidden="true"
-            className="fas fa-print vads-u-margin-right--1 vads-u-color--link-default"
-          />
-          <button className="va-button-link" onClick={() => window.print()}>
-            Print
-          </button>
-        </div>
-      )}
+      <div className="vads-u-margin-top--2 vaos-appts__block-label vaos-hide-for-print">
+        <i
+          aria-hidden="true"
+          className="fas fa-print vads-u-margin-right--1 vads-u-color--link-default"
+        />
+        <button className="va-button-link" onClick={() => window.print()}>
+          Print
+        </button>
+      </div>
 
       {!isPastAppointment && (
         <AlertBox
