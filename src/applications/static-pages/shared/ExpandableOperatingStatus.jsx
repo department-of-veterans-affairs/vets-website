8ee1aea536876f354ece1a1d45f5e1693bcb4b5b
Gibran Rodriguez <grodriguez@governmentcio.com>
2021-06-02 01:23:52 -0500

    Build dynamic widgets

--- src/applications/static-pages/shared/ExpandableOperatingStatus.jsx
+++ src/applications/static-pages/shared/ExpandableOperatingStatus.jsx
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import AdditionalInfo from '@department-of-veterans-affairs/component-library/AdditionalInfo';
+import './sass/_m-operating-status.scss';
 
 const ExpandableOperatingStatus = props => {
+  const [open, setOpen] = useState(false);
+  const handleOnclick = e => {
+    if (!props.extraInfo) return;
+    e.target.classList.toggle('active');
+    const content = e.target.nextElementSibling;
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+      setOpen(false);
+    } else {
+      content.style.display = 'block';
+      setOpen(true);
+    }
+  };
+
+  const iconIndicator = extraInfo => {
+    if (extraInfo && open) return 'fa-angle-down open';
+    if (extraInfo && !open) return ' fa-angle-down close';
+    return null;
+  };
+
   return (
-    <AdditionalInfo triggerText={props.status}>Test test test</AdditionalInfo>
+    <>
+      <button
+        type="button"
+        className={`collapsible ${props.operatingStatusFacility}`}
+        onClick={e => handleOnclick(e)}
+      >
+        <i
+          aria-hidden="true"
+          role="img"
+          className={`fa fa-exclamation-${props.iconType} alert-icon-base`}
+        />
+        <span className={`${props.extraInfo ? 'status-label' : null}`}>
+          {props.statusLabel}
+        </span>
+        <span className="sr-only">Alert: </span>
+        <i
+          aria-hidden="true"
+          role="img"
+          className={`fas ${iconIndicator(
+            props.extraInfo,
+          )}  more-icon vads-u-padding-left--0p5`}
+        />
+      </button>
+      <div className={`content ${props.operatingStatusFacility}`}>
+        <p className="more-info">{props.extraInfo}</p>
+      </div>
+    </>
   );
 };
 
 ExpandableOperatingStatus.propTypes = {
-  status: PropTypes.string,
+  statusLabel: PropTypes.string,
   extraInfo: PropTypes.object,
+  iconType: PropTypes.string,
+  operatingStatusFacility: PropTypes.string,
 };
 
 export default ExpandableOperatingStatus;
