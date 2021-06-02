070eef6f26bc6f69dcf078bf9dd7ee03106ff72a
Mark Dewey <mdewey@governmentcio.com>
2021-06-02 10:26:53 -0400

    updated test

--- src/platform/forms/tests/save-in-progress/SaveInProgressIntro.unit.spec.jsx
+++ src/platform/forms/tests/save-in-progress/SaveInProgressIntro.unit.spec.jsx
@@ -118,7 +118,7 @@ describe('Schemaform <SaveInProgressIntro>', () => {
       />,
     );
 
-    expect(tree.find('.usa-alert').text()).to.contain(
+    expect(tree.find('h1.usa-alert-heading').text()).to.contain(
       'Your application is in progress',
     );
     expect(tree.find('h1')).to.exist;
