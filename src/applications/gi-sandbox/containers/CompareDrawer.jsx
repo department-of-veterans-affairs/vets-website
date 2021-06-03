c35adbed62e74de5f8fde6346a455ee2a13a4c4d
Devin McCurdy <mccurdy_devin@bah.com>
2021-06-03 10:04:31 -0400

    Remove fix

--- src/applications/gi-sandbox/containers/CompareDrawer.jsx
+++ src/applications/gi-sandbox/containers/CompareDrawer.jsx
@@ -42,6 +42,7 @@ export function CompareDrawer({
         onClose={() => setPromptingFacilityCode(null)}
         primaryButton={{
           action: () => {
+            setPromptingFacilityCode(null);
             dispatchRemoveCompareInstitution(promptingFacilityCode);
           },
           text: 'Remove',
