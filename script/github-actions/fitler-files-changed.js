be54c092da8518b7f9a9ed751e2170422ce2fb98
Oseas Moran <oseas.moran@adhocteam.us>
2021-06-03 10:03:25 -0700

    test output format from pull_request

---
+++ script/github-actions/fitler-files-changed.js
@@ -0,0 +1,5 @@
+const args = process.argv.slice(2);
+const FILES = args[0];
+
+console.log('files', FILES); // eslint-disable-line no-console
+console.log('typeof', typeof FILES); // eslint-disable-line no-console
