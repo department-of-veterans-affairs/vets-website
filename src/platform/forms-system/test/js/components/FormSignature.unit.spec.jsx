644c77f96fffc01122296c3abe33f8eea55509e5
Christopher Valarida <chris.valarida@adhocteam.us>
2021-06-04 14:29:47 -0700

    Categorize tests and default required: false

--- src/platform/forms-system/test/js/components/FormSignature.unit.spec.jsx
+++ src/platform/forms-system/test/js/components/FormSignature.unit.spec.jsx
@@ -10,31 +10,73 @@ describe('Forms library - Forms signature component', () => {
     onSectionComplete: () => {},
     setFormData: () => {},
     showError: true,
+    required: false,
   };
 
-  it('should render inputs with default labels', () => {
-    const { getByLabelText } = render(<FormSignature {...signatureProps} />);
-    expect(getByLabelText('Veteran’s full name')).to.exist;
+  describe('signature input', () => {
+    it('should render input with default label', () => {
+      const { getByLabelText } = render(<FormSignature {...signatureProps} />);
+      expect(getByLabelText('Veteran’s full name')).to.exist;
+    });
+
+    it('should render input with custom string label', () => {
+      const { getByLabelText } = render(
+        <FormSignature
+          {...signatureProps}
+          signatureLabel={'Custom text here'}
+        />,
+      );
+      expect(getByLabelText('Custom text here')).to.exist;
+    });
+
+    it('should render input input with custom React element label', () => {
+      const customLabel = <span>Custom text here</span>;
+      const { getByLabelText } = render(
+        <FormSignature {...signatureProps} signatureLabel={customLabel} />,
+      );
+      expect(getByLabelText('Custom text here')).to.exist;
+    });
   });
 
-  it('should render input with custom string label', () => {
-    const { getByLabelText } = render(
-      <FormSignature {...signatureProps} label={'Custom text here'} />,
-    );
-    expect(getByLabelText('Custom text here')).to.exist;
+  describe('certification checkbox', () => {
+    it('should render with default label', () => {
+      const { getByLabelText } = render(<FormSignature {...signatureProps} />);
+      expect(
+        getByLabelText(
+          'I certify the information above is correct and true to the best of my knowledge and belief.',
+        ),
+      ).to.exist;
+    });
+
+    it('should render with custom string label', () => {
+      const { getByLabelText } = render(
+        <FormSignature {...signatureProps} checkboxLabel="LGTM" />,
+      );
+      expect(getByLabelText('LGTM')).to.exist;
+    });
+
+    it('should render with custom React element label', () => {
+      const customLabel = <span>Custom text here</span>;
+      const { getByLabelText } = render(
+        <FormSignature {...signatureProps} checkboxLabel={customLabel} />,
+      );
+      expect(getByLabelText('Custom text here')).to.exist;
+    });
   });
 
-  it('should render input with custom React element label', () => {
-    const customLabel = <span>Custom text here</span>;
-    const { getByLabelText } = render(
-      <FormSignature {...signatureProps} label={customLabel} />,
-    );
-    expect(getByLabelText('Custom text here')).to.exist;
+  describe('behavior', () => {
+    it('should call onSectionComplete when the signature is valid', () => {
+      // "Valid" here means:
+      //   - There are no validation errors
+      //   - The checkbox has been checked
+    });
+
+    it('should call setFormData when the name is entered', () => {});
   });
 
-  it('should call setFormData ...when the name is entered..?', () => {});
-
-  it('should call onSectionComplete when the checkbox is checked', () => {});
-
-  it('should perform data validations if present', () => {});
+  describe('validation', () => {
+    it('should require signature', () => {});
+    it('should require certification checkbox', () => {});
+    it('should perform data validations if present', () => {});
+  });
 });
