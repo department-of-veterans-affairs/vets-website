b7bb402120b48f5cc818895854430e163ff97df1
Oseas Moran <oseas.moran@adhocteam.us>
2021-06-04 07:49:34 -0700

    changed based on feedback

--- script/github-actions/filter-files-changed.js
+++ script/github-actions/filter-files-changed.js
@@ -1,12 +1,4 @@
 const args = process.argv.slice(2);
-const FILES = args[0].slice(1, -1).split(','); // remove unnecessary characters
-let lintFiles = '';
-
-FILES.forEach(file => {
-  const fileExt = file.split('.').pop();
-  if (fileExt === 'js' || fileExt === 'jsx') {
-    lintFiles += `${file} `;
-  }
-});
-
-console.log(`::set-output name=FILES::${lintFiles}`); // eslint-disable-line no-console
+const files = args[0].slice(1, -1).split(','); // remove unnecessary characters
+const filteredFiles = files.filter(file => /.+\.jsx?/.test(file)).join(' ');
+console.log(`::set-output name=FILES::${filteredFiles}`); // eslint-disable-line no-console
