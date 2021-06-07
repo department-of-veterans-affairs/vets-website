fa9649271bfeffaf2b15c32e65e2cd1d0a99c867
Mark Dewey <mdewey@governmentcio.com>
2021-06-07 16:30:48 -0400

    updated happy path

--- src/applications/check-in/pages/Landing.jsx
+++ src/applications/check-in/pages/Landing.jsx
@@ -1,23 +1,9 @@
 import React from 'react';
 import withFeatureFlip from '../containers/withFeatureFlip.jsx';
 
-const Landing = () => {
-  return (
-    <div>
-      <p>
-        This is a place holder page. Most likely will just a page to valid the
-        token before redirect and not show the user anything
-      </p>
-      <p>
-        To try to the flow here are use cases and links
-        <ol>
-          <li>
-            <a href="/check-in/some-token/insurance"> Valid Token</a>
-          </li>
-        </ol>
-      </p>
-    </div>
-  );
+const Landing = ({ router }) => {
+  router.push('/some-token/insurance');
+  return <></>;
 };
 
 export default withFeatureFlip(Landing);
--- src/applications/check-in/tests/happy-path/base.happy.path.cypress.spec.js
+++ src/applications/check-in/tests/happy-path/base.happy.path.cypress.spec.js
@@ -4,8 +4,11 @@ describe('Check In Experience -- ', () => {
   beforeEach(() => {
     cy.intercept('GET', '/v0/feature_toggles*', features);
   });
-  it.skip('Does the button check us in', () => {
-    cy.visit('/check-in/some-token/details');
+  it('Does the button check us in', () => {
+    cy.visit('/check-in/some-token');
+    cy.get('h1').contains('insurance information');
+    cy.get('#errorable-radio-buttons-1-1').click();
+    cy.get('.usa-button').click();
     cy.get('h1').contains('Appointment details');
     cy.get('.usa-button').click();
     cy.get('h1').contains("You're now checked in");
