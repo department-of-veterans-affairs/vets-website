681262d4ec19f2d333404a11f8022c86481ff3b9
Asha Gross <hello@ashagross.com>
2021-06-07 12:34:17 -0400

    Change text to alert user opens in new tab

--- src/applications/appeals/10182/components/ContactInformation.jsx
+++ src/applications/appeals/10182/components/ContactInformation.jsx
@@ -24,11 +24,11 @@ export const ContactInfoDescription = ({ veteran }) => {
         updates or information about your Board Appeal request to this address.
       </p>
       <p className="vads-u-margin-top--1p5">
-        You can{' '}
+        You can update this contact information in your{' '}
         <a href={PROFILE_URL} target="_blank" rel="noopener noreferrer">
-          update this contact information in your VA.gov profile
-        </a>
-        .
+          VA.gov
+        </a>{' '}
+        profile (opens in new tab).
       </p>
       {(!phoneType || !street) && (
         <p className="vads-u-margin-top--1p5">
