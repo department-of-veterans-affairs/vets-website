db347668dbbba02619872c55569eafe2b931567c
Oseas Moran <oseas.moran@adhocteam.us>
2021-06-07 10:19:25 -0700

    updated variables to say stylelint

--- script/github-actions/stylelint-annotation-format.js
+++ script/github-actions/stylelint-annotation-format.js
@@ -2,16 +2,16 @@
 const fs = require('fs');
 const path = require('path');
 
-const sassLintReport = path.join(__dirname, '../../stylelint-report.json');
+const styleLintReport = path.join(__dirname, '../../stylelint-report.json');
 
-if (!fs.existsSync(sassLintReport)) {
-  console.log('No sass-lint report found. Skipping.');
+if (!fs.existsSync(styleLintReport)) {
+  console.log('No stylelint report found. Skipping.');
   return;
 }
 
-const sassLintData = JSON.parse(fs.readFileSync(sassLintReport));
+const styleLintData = JSON.parse(fs.readFileSync(styleLintReport));
 
-sassLintData.forEach(fileMessages => {
+styleLintData.forEach(fileMessages => {
   if (!fileMessages.errored) return;
 
   for (const [key, value] of Object.entries(fileMessages)) {
