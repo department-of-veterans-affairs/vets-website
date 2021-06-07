ef0dcfcb542c544679e0c5ac71c6372c1515195b
Mark Dewey <mdewey@governmentcio.com>
2021-06-07 16:37:38 -0400

    added axe checks

--- src/applications/check-in/tests/go-to-staff/need.to.update.cypress.spec.js
+++ src/applications/check-in/tests/go-to-staff/need.to.update.cypress.spec.js
@@ -7,10 +7,15 @@ describe('Check In Experience -- happy path', () => {
   it('needs to update information', () => {
     cy.visit('/check-in/some-token');
     cy.get('h1').contains('insurance information');
+    cy.injectAxe();
+    cy.axeCheck();
+
     cy.get('#errorable-radio-buttons-1-0').click();
     cy.get('.usa-button').click();
     cy.get('.hydrated > h3').contains(
       'Please see a staff member to complete check-in.',
     );
+    cy.injectAxe();
+    cy.axeCheck();
   });
 });
--- src/applications/check-in/tests/happy-path/base.happy.path.cypress.spec.js
+++ src/applications/check-in/tests/happy-path/base.happy.path.cypress.spec.js
@@ -1,16 +1,22 @@
 import features from '../mocks/enabled.json';
 
-describe('Check In Experience -- happy path', () => {
+describe('Check In Experience -- ', () => {
   beforeEach(() => {
     cy.intercept('GET', '/v0/feature_toggles*', features);
   });
-  it('Does the button check us in', () => {
+  it('happy path', () => {
     cy.visit('/check-in/some-token');
     cy.get('h1').contains('insurance information');
+    cy.injectAxe();
+    cy.axeCheck();
     cy.get('#errorable-radio-buttons-1-1').click();
     cy.get('.usa-button').click();
     cy.get('h1').contains('Appointment details');
+    cy.injectAxe();
+    cy.axeCheck();
     cy.get('.usa-button').click();
     cy.get('h1').contains("You're now checked in");
+    cy.injectAxe();
+    cy.axeCheck();
   });
 });
