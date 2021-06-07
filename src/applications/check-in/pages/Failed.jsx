6cb625c0ed346c61718ff2a4bfdd858668709280
Mark Dewey <mdewey@governmentcio.com>
2021-06-07 10:32:25 -0400

    added pages

---
+++ src/applications/check-in/pages/Failed.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import withFeatureFlip from '../containers/withFeatureFlip.jsx';
+
+const Failed = () => {
+  return <div>Go see staff member</div>;
+};
+
+export default withFeatureFlip(Failed);
