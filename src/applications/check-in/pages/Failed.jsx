cdcb47a79b8813bad7cc6da75a848e0090f9334b
Mark Dewey <mdewey@governmentcio.com>
2021-06-07 15:32:13 -0400

    added failed content

--- src/applications/check-in/pages/Failed.jsx
+++ src/applications/check-in/pages/Failed.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import withFeatureFlip from '../containers/withFeatureFlip.jsx';
 
 const Failed = () => {
-  return <div>Go see staff member</div>;
+  return (
+    <div className="vads-u-padding--3 vads-u-padding-y--7">
+      <va-alert status="error">
+        <h3 slot="headline">Please see a staff member to complete check-in.</h3>
+      </va-alert>
+    </div>
+  );
 };
 
 export default withFeatureFlip(Failed);
--- src/applications/check-in/pages/Landing.jsx
+++ src/applications/check-in/pages/Landing.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import withFeatureFlip from '../containers/withFeatureFlip.jsx';
 
 const Landing = () => {
-  return <div>Landing?</div>;
+  return (
+    <div>
+      <p>
+        This is a place holder page. Most likely will just a page to valid the
+        token before redirect and not show the user anything
+      </p>
+      <p>
+        To try to the flow here are use cases and links
+        <ol>
+          <li>
+            <a href="/check-in/some-token/insurance"> Valid Token</a>
+          </li>
+        </ol>
+      </p>
+    </div>
+  );
 };
 
 export default withFeatureFlip(Landing);
