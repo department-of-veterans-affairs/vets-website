b1b5c0e21df3a69c3ddad317d975901b2001ecc8
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-06-07 10:48:06 -0500

    WIP

---
+++ script/run-cypress-tests-gha.js
@@ -0,0 +1,19 @@
+const { runCommand } = require('./utils');
+const exec = require('child_process').exec;
+
+exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
+  const strings = stdout.split(',').sort();
+  const divider = Math.ceil(strings.length / 6);
+  const tests = strings
+    .slice(
+      Number(process.env.STEP) * divider,
+      (Number(process.env.STEP) + 1) * divider,
+    )
+    .join(',');
+
+  runCommand(
+    `CYPRESS_CI=${
+      process.env.CI
+    } yarn cy:run  --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json --spec '${tests}'`,
+  );
+});
