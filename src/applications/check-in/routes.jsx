d2e885ee47381e75ea6db44ef48c2a3ce9384e8f
Mark Dewey <mdewey@governmentcio.com>
2021-06-07 10:15:39 -0400

    added the routes

--- src/applications/check-in/routes.jsx
+++ src/applications/check-in/routes.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import LandingPage from './pages/LandingPage';
-// import ConfirmationPage from './pages/ConfirmationPage';
-
 const createRoutesWithStore = () => {
   return (
     <Switch>
-      <Route path="/:token" component={LandingPage} />
+      <Route path="/:token" component={() => <>token</>} />
+      <Route path="/:token/insurance" component={() => <>insurance</>} />
+      <Route path="/:token/details" component={() => <>details</>} />
+      <Route path="/:token/confirmed" component={() => <>confirmed</>} />
+      <Route path="/:token/failed" component={() => <>failed</>} />
     </Switch>
   );
 };
