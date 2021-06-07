e5c28f9e80f048b246f948b425d2253f82bf9f5c
Mark Dewey <mdewey@governmentcio.com>
2021-06-07 15:11:59 -0400

    added insurance redirect and content

--- src/applications/check-in/pages/Insurance.jsx
+++ src/applications/check-in/pages/Insurance.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router';
+
+import RadioButtons from '@department-of-veterans-affairs/component-library/RadioButtons';
+
 import withFeatureFlip from '../containers/withFeatureFlip.jsx';
 
-const Insurance = () => {
-  return <div>Insurance question goes here</div>;
+const Insurance = props => {
+  const { router } = props;
+  const [needToUpdate, setNeedToUpdate] = useState();
+
+  const handleContinueClick = () => {
+    if (needToUpdate === 'Yes') {
+      const url = '/some-token/failed';
+      router.push(url);
+    } else if (needToUpdate === 'No') {
+      const url = '/some-token/details';
+      router.push(url);
+    }
+  };
+
+  return (
+    <div className={'vads-u-margin--1p5'}>
+      <h1>
+        Do you need to update your contact, next of kin, or insurance
+        information?
+      </h1>
+      <div
+        style={{
+          paddingLeft: '1em',
+        }}
+      >
+        <RadioButtons
+          onValueChange={e => {
+            setNeedToUpdate(e.value);
+          }}
+          options={['Yes', 'No']}
+          value={{ value: needToUpdate }}
+        />
+      </div>
+      <button
+        type="button"
+        className="usa-button"
+        onClick={handleContinueClick}
+      >
+        Continue
+      </button>
+    </div>
+  );
 };
 
-export default withFeatureFlip(Insurance);
+export default withRouter(withFeatureFlip(Insurance));
