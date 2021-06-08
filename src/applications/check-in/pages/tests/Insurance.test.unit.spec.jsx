bb11445928c61bbd0c3892eda5377050f13290da
Mark Dewey <mdewey@governmentcio.com>
2021-06-08 15:42:52 -0400

    made the test simplier

--- src/applications/check-in/pages/tests/Insurance.test.unit.spec.jsx
+++ src/applications/check-in/pages/tests/Insurance.test.unit.spec.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
-import sinon from 'sinon';
 
 import Insurance from '../Insurance';
 
@@ -11,25 +10,6 @@ describe('health care check in -- Insurance component -- ', () => {
 
     expect(component.find('h1').exists()).to.be.true;
 
-    component.unmount();
-  });
-  it('button click calls router', () => {
-    const push = sinon.spy();
-    const mockRouter = {
-      push,
-      params: {
-        token: 'token-123',
-      },
-    };
-
-    const component = mount(<Insurance router={mockRouter} />);
-
-    const checkInButton = component.find('[data-testid="check-in-button"]');
-    expect(checkInButton.exists()).to.be.true;
-    expect(checkInButton.props()).to.have.property('onClick');
-    checkInButton.props().onClick();
-    expect(push.called).to.be.true;
-    expect(push.calledWith('/token-123/confirmed'));
     component.unmount();
   });
 });
