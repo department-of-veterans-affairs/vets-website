a16d6448e4a8525fefd5078c0a100b3fab1fbf0c
Mark Dewey <mdewey@governmentcio.com>
2021-06-09 16:09:36 -0400

    updated e2e tests

--- src/applications/check-in/tests/dynamic-routing/base.happy.path.cypress.spec.js
+++ src/applications/check-in/tests/dynamic-routing/base.happy.path.cypress.spec.js
@@ -4,34 +4,32 @@ describe('Check In Experience -- ', () => {
   beforeEach(() => {
     cy.intercept('GET', '/v0/feature_toggles*', features);
   });
-  it('dynamic routing keeps token -- check in', () => {
+  it('dynamic routing keeps token -- check in -- happy path', () => {
     const token = 'SOME-AWESOME-UUID';
     cy.visit(`/check-in/${token}`);
-    cy.get('h1').contains('insurance information');
+    cy.get('h1').contains('insurance');
     cy.url().should('contain', token);
     cy.url().should('contain', 'insurance');
-    cy.get('#errorable-radio-buttons-1-1').click();
-    cy.get('.usa-button').click();
 
-    cy.get('h1').contains('Appointment details');
+    cy.get('.vads-u-margin--3 > :nth-child(3)').click();
+    cy.get('h1').contains('Your appointment');
     cy.url().should('contain', token);
     cy.url().should('contain', 'details');
+
     cy.get('.usa-button').click();
-    cy.get('h1').contains("You're now checked in");
+    cy.get('h1').contains('Thank you for checking in');
     cy.url().should('contain', token);
     cy.url().should('contain', 'confirmed');
   });
   it('dynamic routing keeps token -- check in', () => {
     const token = 'SOME-AWESOME-UUID';
     cy.visit(`/check-in/${token}`);
-    cy.get('h1').contains('insurance information');
+    cy.get('h1').contains('insurance');
     cy.url().should('contain', token);
     cy.url().should('contain', 'insurance');
-    cy.get('#errorable-radio-buttons-1-0').click();
-    cy.get('.usa-button').click();
-    cy.get('.hydrated > h3').contains(
-      'Please see a staff member to complete check-in.',
-    );
+
+    cy.get('.vads-u-margin--3 > :nth-child(2)').click();
+    cy.get('h1').contains('staff member');
     cy.url().should('contain', token);
     cy.url().should('contain', 'failed');
   });
