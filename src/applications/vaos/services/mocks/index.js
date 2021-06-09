999055d2b77f72ca923466498a2f3d33368842a4
Jeff Balboni <jeff@adhocteam.us>
2021-06-09 11:14:34 -0400

    Revert cerner change

--- src/applications/vaos/services/mocks/index.js
+++ src/applications/vaos/services/mocks/index.js
@@ -228,7 +228,7 @@ const responses = {
             },
             {
               facility_id: '984',
-              is_cerner: true,
+              is_cerner: false,
             },
           ],
         },
