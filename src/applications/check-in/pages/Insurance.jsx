05ec5af0150d3c824422128ffca7527b851e1747
Mark Dewey <mdewey@governmentcio.com>
2021-06-09 15:47:57 -0400

    removed  radio buttons

--- src/applications/check-in/pages/Insurance.jsx
+++ src/applications/check-in/pages/Insurance.jsx
@@ -1,43 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import RadioButtons from '@department-of-veterans-affairs/component-library/RadioButtons';
 import Telephone from '@department-of-veterans-affairs/component-library/Telephone';
 
 import { goToNextPageWithToken } from '../utils/navigation';
 
 const Insurance = props => {
   const { router } = props;
-  const [needToUpdate, setNeedToUpdate] = useState();
 
   const contactNumber = '555-867-5309';
-
-  const handleContinueClick = () => {
-    if (needToUpdate === 'Yes') {
-      goToNextPageWithToken(router, 'failed');
-    } else if (needToUpdate === 'No') {
-      goToNextPageWithToken(router, 'details');
-    }
-  };
 
   return (
     <div className={'vads-u-margin--3'}>
       <h1>Need to update your insurance, contact, or other information?</h1>
-      <div className="vads-u-margin-bottom--3">
-        <RadioButtons
-          onValueChange={e => {
-            setNeedToUpdate(e.value);
-          }}
-          options={['Yes', 'No']}
-          value={{ value: needToUpdate }}
-        />
-      </div>
       <button
-        type="button"
-        className="usa-button"
-        onClick={handleContinueClick}
+        className="usa-button-secondary usa-button-big"
+        onClick={() => goToNextPageWithToken(router, 'failed')}
       >
-        Continue
+        Yes
       </button>
+      <button
+        className="usa-button-secondary usa-button-big"
+        onClick={() => goToNextPageWithToken(router, 'details')}
+      >
+        No
+      </button>
+
       <footer className="row">
         <h2 className="help-heading vads-u-font-size--lg">Need help?</h2>
         <p>
