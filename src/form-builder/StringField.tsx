3868e831fa2a3699a43eb540d9a3d8829eae46ba
Christopher Valarida <chris.valarida@adhocteam.us>
2021-06-11 12:32:58 -0700

    Linting

--- src/form-builder/StringField.tsx
+++ src/form-builder/StringField.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Field } from "formik";
+import React from 'react';
+import { Field } from 'formik';
 
 // TODO: Abstract out all field props into a single interface
 const StringField = (props: { name: string }) => (
