bd3f6183a47a2b699a49323d1396cd502b51fd7c
Kevin Lee <kevin.lee@adhocteam.us>
2021-06-11 09:20:06 -0700

    WIP appointment list tests for VAOS service

--- src/applications/vaos/tests/appointment-list/components/RequestedAppointmentsList.unit.spec.jsx
+++ src/applications/vaos/tests/appointment-list/components/RequestedAppointmentsList.unit.spec.jsx
@@ -6,6 +6,7 @@ import environment from 'platform/utilities/environment';
 import { mockFetch, setFetchJSONFailure } from 'platform/testing/unit/helpers';
 import reducers from '../../../redux/reducer';
 import { getVAFacilityMock, getVARequestMock } from '../../mocks/v0';
+import { getVAOSRequestMock } from '../../mocks/v2';
 import { mockAppointmentInfo, mockFacilitiesFetch } from '../../mocks/helpers';
 import {
   renderWithStoreAndRouter,
@@ -17,6 +18,14 @@ const initialState = {
   featureToggles: {
     vaOnlineSchedulingCancel: true,
     vaOnlineSchedulingHomepageRefresh: true,
+  },
+};
+
+const initialStateWithVAOSService = {
+  featureToggles: {
+    vaOnlineSchedulingCancel: true,
+    vaOnlineSchedulingHomepageRefresh: true,
+    vaOnlineSchedulingVAOSServiceRequests: true,
   },
 };
 
@@ -194,6 +203,222 @@ describe('VAOS <RequestedAppointmentsList>', () => {
   it('should not show resolved requests', async () => {
     const startDate = moment.utc();
     const appointment = getVARequestMock();
+    appointment.attributes = {
+      ...appointment.attributes,
+      status: 'Resolved',
+      optionDate1: startDate,
+      optionTime1: 'AM',
+      purposeOfVisit: 'New Issue',
+      bestTimetoCall: ['Morning'],
+      email: 'patient.test@va.gov',
+      phoneNumber: '5555555566',
+      typeOfCareId: '323',
+      reasonForVisit: 'Back pain',
+      friendlyLocationName: 'Some VA medical center',
+      appointmentType: 'Primary Care',
+      comment: 'loss of smell',
+      facility: {
+        ...appointment.attributes.facility,
+        facilityCode: '983GC',
+      },
+    };
+    appointment.id = '1234';
+    mockAppointmentInfo({ requests: [appointment], isHomepageRefresh: true });
+
+    const screen = renderWithStoreAndRouter(<RequestedAppointmentsList />, {
+      initialState,
+      reducers,
+    });
+
+    expect(await screen.findByText(/You don’t have any appointments/i)).to
+      .exist;
+  });
+});
+
+describe('VAOS <RequestedAppointmentsList> with the VAOS service', () => {
+  beforeEach(() => {
+    mockFetch();
+    MockDate.set(getTimezoneTestDate());
+  });
+  afterEach(() => {
+    MockDate.reset();
+  });
+  it('should show va request', async () => {
+    const startDate = moment.utc();
+    const appointment = getVAOSRequestMock();
+    appointment.id = '1234';
+    appointment.attributes = {
+      comment: 'A message from the patient',
+      contact: {
+        telecom: [
+          { type: 'phone', value: '2125551212' },
+          { type: 'email', value: 'veteranemailtest@va.gov' },
+        ],
+      },
+      kind: 'clinic',
+      locationId: '983GC',
+      id: '1234',
+      preferredTimesForPhoneCall: ['Morning'],
+      reason: 'Routine Follow-up',
+      requestedPeriods: [
+        {
+          start: moment(startDate)
+            .add(3, 'days')
+            .format('YYYY-MM-DDTHH:mm:ss[Z]'),
+        },
+        {
+          start: moment(startDate)
+            .add(4, 'days')
+            .format('YYYY-MM-DDTHH:mm:ss[Z]'),
+        },
+      ],
+      serviceType: '323',
+      start: null,
+      status: 'proposed',
+    };
+    mockAppointmentInfo({ requests: [appointment], isHomepageRefresh: true });
+
+    const facility = {
+      id: 'vha_442GC',
+      attributes: {
+        ...getVAFacilityMock().attributes,
+        uniqueId: '442GC',
+        name: 'Cheyenne VA Medical Center',
+        address: {
+          physical: {
+            zip: '82001-5356',
+            city: 'Cheyenne',
+            state: 'WY',
+            address1: '2360 East Pershing Boulevard',
+          },
+        },
+        phone: {
+          main: '307-778-7550',
+        },
+      },
+    };
+    mockFacilitiesFetch('vha_442GC', [facility]);
+
+    const screen = renderWithStoreAndRouter(<RequestedAppointmentsList />, {
+      initialStateWithVAOSService,
+      reducers,
+    });
+
+    expect(await screen.findByText('Primary care')).to.be.ok;
+    expect(await screen.findByText(facility.attributes.name)).to.be.ok;
+    expect(screen.queryByText(/You don’t have any appointments/i)).not.to.exist;
+  });
+
+  it('should show cc request', async () => {
+    const startDate = moment.utc();
+    const appointment = getVAOSRequestMock();
+    appointment.attributes = {
+      ...appointment.attributes,
+      status: 'Submitted',
+      optionDate1: startDate,
+      optionTime1: 'AM',
+      purposeOfVisit: 'New Issue',
+      bestTimetoCall: ['Morning'],
+      email: 'patient.test@va.gov',
+      phoneNumber: '5555555566',
+      typeOfCareId: 'CCAUDHEAR',
+      reasonForVisit: 'Back pain',
+      friendlyLocationName: 'Some VA medical center',
+      appointmentType: 'Audiology (hearing aid support)',
+      comment: 'loss of smell',
+      ccAppointmentRequest: {
+        preferredProviders: [],
+      },
+    };
+    appointment.id = '1234';
+    mockAppointmentInfo({ requests: [appointment], isHomepageRefresh: true });
+
+    const screen = renderWithStoreAndRouter(<RequestedAppointmentsList />, {
+      initialStateWithVAOSService,
+      reducers,
+    });
+
+    expect(await screen.findByText('Audiology (hearing aid support)')).to.be.ok;
+    expect(screen.baseElement).to.contain.text('Community care');
+    expect(screen.queryByText(/You don’t have any appointments/i)).not.to.exist;
+  });
+
+  it('should show cc request and provider facility name if available', async () => {
+    const startDate = moment.utc();
+    const appointment = getVAOSRequestMock();
+    appointment.attributes = {
+      ...appointment.attributes,
+      status: 'Submitted',
+      optionDate1: startDate,
+      optionTime1: 'AM',
+      purposeOfVisit: 'New Issue',
+      bestTimetoCall: ['Morning'],
+      email: 'patient.test@va.gov',
+      phoneNumber: '5555555566',
+      typeOfCareId: 'CCAUDHEAR',
+      reasonForVisit: 'Back pain',
+      friendlyLocationName: 'Some VA medical center',
+      appointmentType: 'Audiology (hearing aid support)',
+      comment: 'loss of smell',
+      ccAppointmentRequest: {
+        preferredProviders: [
+          {
+            firstName: 'Test',
+            lastName: 'User',
+            practiceName: 'Scripps Health Clinic',
+            address: {
+              zipCode: '01060',
+            },
+            preferredOrder: 0,
+            providerZipCode: '01060',
+            objectType: 'Provider',
+            link: [],
+          },
+        ],
+      },
+    };
+    appointment.id = '1234';
+    mockAppointmentInfo({ requests: [appointment], isHomepageRefresh: true });
+
+    const screen = renderWithStoreAndRouter(<RequestedAppointmentsList />, {
+      initialStateWithVAOSService,
+      reducers,
+    });
+
+    expect(await screen.findByText('Audiology (hearing aid support)')).to.be.ok;
+    expect(screen.baseElement).to.contain.text('Scripps Health Clinic');
+    expect(screen.baseElement).not.to.contain.text('Community care');
+    expect(screen.queryByText(/You don’t have any appointments/i)).not.to.exist;
+  });
+
+  it('should show error message when request fails', async () => {
+    mockAppointmentInfo({});
+    setFetchJSONFailure(
+      global.fetch.withArgs(
+        `${
+          environment.API_URL
+        }/vaos/v0/appointment_requests?start_date=${moment()
+          .add(-120, 'days')
+          .format('YYYY-MM-DD')}&end_date=${moment().format('YYYY-MM-DD')}`,
+      ),
+      { errors: [] },
+    );
+
+    const screen = renderWithStoreAndRouter(<RequestedAppointmentsList />, {
+      initialState,
+      reducers,
+    });
+
+    expect(
+      await screen.findByText(
+        /We’re having trouble getting your appointment requests/i,
+      ),
+    ).to.be.ok;
+  });
+
+  it('should not show resolved requests', async () => {
+    const startDate = moment.utc();
+    const appointment = getVAOSRequestMock();
     appointment.attributes = {
       ...appointment.attributes,
       status: 'Resolved',
