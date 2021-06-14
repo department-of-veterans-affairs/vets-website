2f5ad71a0ba6d63fb019156fb6c283295bd6d1c7
Brooks Johnson <brooks.johnson@pm.me>
2021-06-14 13:46:06 -0700

    Use more modules

--- script/component-migration/index.js
+++ script/component-migration/index.js
@@ -2,6 +2,7 @@
 const commandLineArgs = require('command-line-args');
 const fs = require('fs');
 const glob = require('glob');
+const replacements = require('./transformers');
 
 const optionDefinitions = [
   { name: 'component', alias: 'c' },
@@ -42,69 +43,6 @@ function translateProps(componentString, propMap) {
 
   return translatedComp;
 }
-
-/*
- **********************************
- *    Component transformers      *
- **********************************
- */
-function alertBoxReplacement() {
-  const moveToNamedSlot = componentString => {
-    let translatedHeadline = componentString.toString();
-    const level = componentString.match(/level=["{](\d)["}]/)?.[1] || 3;
-    /* eslint-disable-next-line no-useless-escape */
-    const headline = componentString.match(/headline=["{]([\w\s\.]+)["}]/)?.[1];
-    translatedHeadline = translatedHeadline.replace(/level=["{](\d)["}]/, '');
-    translatedHeadline = translatedHeadline.replace(
-      /headline=["{](.)+["}]/,
-      '',
-    );
-
-    const childrenStart = translatedHeadline.indexOf('>') + 1;
-    translatedHeadline = `
-      ${translatedHeadline.slice(0, childrenStart)}
-      <h${level} slot="headline">${headline}</h${level}>
-      ${translatedHeadline.slice(childrenStart)}
-      `;
-    return translatedHeadline;
-  };
-
-  const moveChildren = (componentString, propName) => {
-    let translatedChildren = componentString.toString();
-    const tag = [...componentString.match(/<(va-[a-z-]+)\s/)][1];
-    const children = componentString.match(
-      new RegExp(`${propName}={(.+)}`, 's'),
-    )?.[1];
-
-    if (!children) return componentString;
-
-    translatedChildren = translatedChildren.replace(
-      `></${tag}`,
-      `>\n${children}\n</${tag}`,
-    );
-
-    translatedChildren = translatedChildren.replace(children, '');
-    translatedChildren = translatedChildren.replace(`${propName}={}\n`, '');
-    return translatedChildren;
-  };
-
-  return [
-    'va-alert',
-    {
-      headline: moveToNamedSlot,
-      content: moveChildren,
-      // children: moveChildren,
-      // level: null,
-      isVisible: 'visible',
-    },
-  ];
-}
-
-const replacements = {
-  AlertBox: alertBoxReplacement,
-};
-
-/* End of component transformers */
 
 const filenames = glob.sync(`${options.dir}/**/*.jsx`);
 
---
+++ script/component-migration/transformers/alertbox.js
@@ -0,0 +1,53 @@
+function alertBoxReplacement() {
+  const moveToNamedSlot = componentString => {
+    let translatedHeadline = componentString.toString();
+    const level = componentString.match(/level=["{](\d)["}]/)?.[1] || 3;
+    /* eslint-disable-next-line no-useless-escape */
+    const headline = componentString.match(/headline=["{]([\w\s\.]+)["}]/)?.[1];
+    translatedHeadline = translatedHeadline.replace(/level=["{](\d)["}]/, '');
+    translatedHeadline = translatedHeadline.replace(
+      /headline=["{](.)+["}]/,
+      '',
+    );
+
+    const childrenStart = translatedHeadline.indexOf('>') + 1;
+    translatedHeadline = `
+      ${translatedHeadline.slice(0, childrenStart)}
+      <h${level} slot="headline">${headline}</h${level}>
+      ${translatedHeadline.slice(childrenStart)}
+      `;
+    return translatedHeadline;
+  };
+
+  const moveChildren = (componentString, propName) => {
+    let translatedChildren = componentString.toString();
+    const tag = [...componentString.match(/<(va-[a-z-]+)\s/)][1];
+    const children = componentString.match(
+      new RegExp(`${propName}={(.+)}`, 's'),
+    )?.[1];
+
+    if (!children) return componentString;
+
+    translatedChildren = translatedChildren.replace(
+      `></${tag}`,
+      `>\n${children}\n</${tag}`,
+    );
+
+    translatedChildren = translatedChildren.replace(children, '');
+    translatedChildren = translatedChildren.replace(`${propName}={}\n`, '');
+    return translatedChildren;
+  };
+
+  return [
+    'va-alert',
+    {
+      headline: moveToNamedSlot,
+      content: moveChildren,
+      // children: moveChildren,
+      // level: null,
+      isVisible: 'visible',
+    },
+  ];
+}
+
+module.exports = { alertBoxReplacement };
