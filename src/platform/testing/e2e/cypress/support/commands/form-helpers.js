1eef8495c7305414fb218550d5b7c0226940a9ac
Cbonade <cbonade@DESKTOP-MI88P0N.localdomain>
2021-06-14 12:22:18 -0400

    Rewrite education benefits opt out test in Cypress, add helpers to fill out form that mimic nightwatch helpers and test data required for this form

---
+++ src/platform/testing/e2e/cypress/support/commands/form-helpers.js
@@ -0,0 +1,33 @@
+Cypress.Commands.add('overrideVetsGovApi', url => {
+  const current = window.VetsGov || {};
+  window.VetsGov = Object.assign({}, current, {
+    api: {
+      // eslint-disable-next-line object-shorthand
+      url: url,
+    },
+  });
+  return window.VetsGov;
+});
+
+Cypress.Commands.add('fill', (selector, value) => {
+  if (typeof value !== 'undefined') {
+    cy.get(selector)
+      .clear()
+      .type(value);
+  }
+});
+
+Cypress.Commands.add('fillName', (baseName, name) => {
+  cy.get(`input[name="${baseName}_first"]`)
+    .clear()
+    .type(name.first);
+  cy.get(`input[name="${baseName}_middle"]`)
+    .clear()
+    .type(name.middle);
+  cy.get(`input[name="${baseName}_last"]`)
+    .clear()
+    .type(name.last);
+  if (name.suffix) {
+    cy.get(`input[name="${baseName}]_suffix`).select(name.suffix);
+  }
+});
