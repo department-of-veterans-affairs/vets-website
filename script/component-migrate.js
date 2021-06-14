ac9cce480a867ae644bf50a99b343abed8d68f2f
Brooks Johnson <brooks.johnson@pm.me>
2021-06-14 12:34:53 -0700

    Move headline down into children

--- script/component-migrate.js
+++ script/component-migrate.js
@@ -49,6 +49,26 @@ function translateProps(componentString, propMap) {
  **********************************
  */
 function alertBoxReplacement() {
+  const moveToNamedSlot = componentString => {
+    let translatedHeadline = componentString.toString();
+    const level = componentString.match(/level=["{](\d)["}]/)?.[1] || 3;
+    /* eslint-disable-next-line no-useless-escape */
+    const headline = componentString.match(/headline=["{]([\w\s\.]+)["}]/)?.[1];
+    translatedHeadline = translatedHeadline.replace(/level=["{](\d)["}]/, '');
+    translatedHeadline = translatedHeadline.replace(
+      /headline=["{](.)+["}]/,
+      '',
+    );
+
+    const childrenStart = translatedHeadline.indexOf('>') + 1;
+    translatedHeadline = `
+      ${translatedHeadline.slice(0, childrenStart)}
+      <h${level} slot="headline">${headline}</h${level}>
+      ${translatedHeadline.slice(childrenStart)}
+      `;
+    return translatedHeadline;
+  };
+
   const moveChildren = (componentString, propName) => {
     let translatedChildren = componentString.toString();
     const tag = [...componentString.match(/<(va-[a-z-]+)\s/)][1];
@@ -71,7 +91,9 @@ function alertBoxReplacement() {
   return [
     'va-alert',
     {
+      headline: moveToNamedSlot,
       content: moveChildren,
+      // children: moveChildren,
       // level: null,
       isVisible: 'visible',
     },
