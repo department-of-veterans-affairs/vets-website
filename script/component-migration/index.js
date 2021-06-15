768caabc91da62f7dbde908ce2861d1f857f2b0e
Brooks Johnson <brooks.johnson@pm.me>
2021-06-15 12:20:46 -0700

    Add more logging

--- script/component-migration/index.js
+++ script/component-migration/index.js
@@ -86,6 +86,9 @@ FILENAMES.forEach(fname => {
     const cmpRegex = new RegExp(`(<${newTag}.+?</${newTag}>)`, 'gsm');
     const components = Array.from(newTags.matchAll(cmpRegex), m => m[0]);
 
+    console.log(
+      chalk.magenta(`${options.component} found ${components.length} times.`),
+    );
     let migratedFile = newTags;
     components.forEach(component => {
       console.log(chalk.yellow.bold('Remapping component props:'));
@@ -105,6 +108,7 @@ FILENAMES.forEach(fname => {
       console.log(chalk.green(`File written: ${fname}`));
     });
 
+    console.log();
     return 0;
   });
 });
