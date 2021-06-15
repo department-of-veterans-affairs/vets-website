f51b01ff0de62477bea09f29ba7c3855f4e519d2
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-06-15 13:09:49 -0500

    Remove forced failures

--- src/applications/health-care-questionnaire/questionnaire/tests/e2e/04.appointment.id.is.present.cypress.spec.js
+++ src/applications/health-care-questionnaire/questionnaire/tests/e2e/04.appointment.id.is.present.cypress.spec.js
@@ -23,7 +23,7 @@ describe('health care questionnaire -- appointment id is required --', () => {
   it('should show questionnaire with id in url', () => {
     cy.title().should('contain', 'Questionnaire');
     cy.get('.schemaform-title>h1').contains(
-      'Answer primsssary care questionnaire',
+      'Answer primary care questionnaire',
     );
   });
 });
