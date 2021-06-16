4c8b51259c97815b0baa00e00eae4e502296f66b
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2021-06-16 06:25:37 -0400

    Replaced Cypress Webpack preprocessor with esbuild.

--- src/platform/testing/e2e/cypress/plugins/index.js
+++ src/platform/testing/e2e/cypress/plugins/index.js
@@ -1,27 +1,82 @@
 const fs = require('fs');
-const webpackPreprocessor = require('@cypress/webpack-preprocessor');
+const path = require('path');
+
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const alias = require('esbuild-plugin-alias');
+
+// const webpackPreprocessor = require('@cypress/webpack-preprocessor');
+
+const root = path.resolve(__dirname, '../../../../../src');
 
 module.exports = on => {
-  const ENV = 'localhost';
+  // const ENV = 'localhost';
 
   // Import our own Webpack config.
-  require('../../../../../../config/webpack.config.js')(ENV).then(
-    webpackConfig => {
-      const options = {
-        webpackOptions: {
-          ...webpackConfig,
+  // require('../../../../../../config/webpack.config.js')(ENV).then(
+  //   webpackConfig => {
+  //     const options = {
+  //       webpackOptions: {
+  //         ...webpackConfig,
 
-          // Expose some Node globals.
-          node: {
-            __dirname: true,
-            __filename: true,
-          },
-        },
-      };
+  //         // Expose some Node globals.
+  //         node: {
+  //           __dirname: true,
+  //           __filename: true,
+  //         },
+  //       },
+  //     };
 
-      on('file:preprocessor', webpackPreprocessor(options));
+  //     on('file:preprocessor', webpackPreprocessor(options));
+  //   },
+  // );
+
+  const nodeModulesRegex = /^(?:.*[\\/])?node_modules(?:[\\/].*)?$/;
+  const relativePathRegex = /.*\/vets-website\/(.+)/;
+
+  const dirnamePlugin = {
+    name: 'dirname',
+
+    setup(build) {
+      build.onLoad({ filter: /.js?$/ }, ({ path: filePath }) => {
+        if (!nodeModulesRegex.test(filePath)) {
+          const [, relativePath] = filePath.match(relativePathRegex);
+          if (!relativePath) return null;
+
+          const dirname = path.dirname(relativePath);
+          const injectedDefinition = `const __dirname = '${dirname}';`;
+          const fileContents = fs.readFileSync(filePath, 'utf8');
+          return {
+            contents: `${injectedDefinition}\n\n${fileContents}`,
+            loader: 'jsx',
+          };
+        }
+
+        return null;
+      });
     },
-  );
+  };
+
+  const bundler = createBundler({
+    define: {
+      __BUILDTYPE__: '"localhost"',
+      __API__: null,
+      // global: '"window"',
+    },
+    loader: {
+      '.js': 'jsx',
+    },
+    nodePaths: [root],
+    plugins: [
+      alias({
+        '~': root,
+        '@@vap-svc': path.join(root, 'platform/user/profile/vap-svc'),
+        '@@profile': path.resolve(root, 'applications/personalization/profile'),
+      }),
+      dirnamePlugin,
+    ],
+  });
+
+  on('file:preprocessor', bundler);
 
   on('after:spec', (spec, results) => {
     if (results.stats.failures === 0 && results.video) {
