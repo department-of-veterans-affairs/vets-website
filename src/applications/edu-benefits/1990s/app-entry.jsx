d994c11b0bba19416b8954e6d5b9e7ca2cff550a
Devin McCurdy <mccurdy_devin@bah.com>
2021-06-16 15:45:16 -0400

    Update analytics

--- src/applications/edu-benefits/1990s/app-entry.jsx
+++ src/applications/edu-benefits/1990s/app-entry.jsx
@@ -6,7 +6,7 @@ import startApp from 'platform/startup';
 import routes from './routes';
 import reducer from './reducers';
 import manifest from './manifest.json';
-import { analyticsEvents } from './analytics-events';
+import { analyticsEvents } from './analytics/analytics-events';
 
 startApp({
   url: manifest.rootUrl,
