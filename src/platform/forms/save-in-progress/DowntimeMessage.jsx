4f5f558e798fc194ebf3f1d571106ac0ed612003
Brooks Johnson <brooks.johnson@pm.me>
2021-06-17 18:03:14 -0700

    Commit results after running component-migration

--- src/platform/forms/save-in-progress/DowntimeMessage.jsx
+++ src/platform/forms/save-in-progress/DowntimeMessage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AlertBox from '@department-of-veterans-affairs/component-library/AlertBox';
+
 import classNames from 'classnames';
 import { APP_TYPE_DEFAULT } from '../../forms-system/src/js/constants';
 
@@ -11,14 +11,9 @@ export default function DowntimeMessage({
   const endTime = downtime.endTime;
   const appType = formConfig?.customText?.appType || APP_TYPE_DEFAULT;
   return (
-    <AlertBox
-      className={classNames({
-        'schemaform-downtime-after-steps': isAfterSteps,
-      })}
-      headline={`This ${appType} is down for maintenance.`}
-      isVisible
-      status="warning"
-    >
+    <va-alert visible status="warning">
+      <h3 slot="headline">`This ${appType} is down for maintenance.`</h3>
+
       {endTime ? (
         <p>
           We’re making some updates to this {appType}. We’re sorry it’s not
@@ -31,6 +26,6 @@ export default function DowntimeMessage({
           working right now. Please check back soon.
         </p>
       )}
-    </AlertBox>
+    </va-alert>
   );
 }
