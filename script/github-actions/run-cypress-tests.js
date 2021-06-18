50efb41af282afee7172bc308ef46ee515ecf231
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2021-06-18 00:32:53 -0400

    Differentiate server and Cypress ports.

--- script/github-actions/run-cypress-tests.js
+++ script/github-actions/run-cypress-tests.js
@@ -2,6 +2,8 @@ const path = require('path');
 const glob = require('glob');
 const { runCommandSync } = require('../utils');
 
+const CYPRESS_PORT = Number(process.env.SERVER_PORT) + 1;
+
 const searchPath = path.join(__dirname, '../../src/**/*.cypress.spec.js');
 const tests = glob.sync(searchPath);
 const divider = Math.ceil(tests.length / 6);
@@ -14,6 +16,6 @@ const batch = tests
 
 runCommandSync(
   `CYPRESS_CI=true yarn cy:run --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --config baseUrl=http://localhost:${
-    process.env.CYPRESS_PORT
-  } --port ${process.env.CYPRESS_PORT} --spec '${batch}'`,
+    process.env.SERVER_PORT
+  } --port ${CYPRESS_PORT} --spec '${batch}'`,
 );
