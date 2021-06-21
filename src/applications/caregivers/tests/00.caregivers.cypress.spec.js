eb3731ca5d3fc85c3888cb62caf42127127732d9
Cbonade <cbonade@DESKTOP-MI88P0N.localdomain>
2021-06-21 10:36:30 -0400

    Rework disabled condition to more reliably achieve the desired result

--- src/applications/caregivers/tests/00.caregivers.cypress.spec.js
+++ src/applications/caregivers/tests/00.caregivers.cypress.spec.js
@@ -4,14 +4,15 @@ import environments from 'site/constants/environments';
 
 describe('Caregivers test', () => {
   it('Loads the page body', () => {
-    if (
-      !(
-        manifest.e2eTestsDisabled &&
-        process.env.BUILDTYPE !== environments.LOCALHOST
-      )
-    )
+    const disabled =
+      manifest.e2eTestsDisabled &&
+      process.env.BUILDTYPE !== environments.LOCALHOST;
+    // TODO: Remove this when CI builds temporary landing pages to run e2e tests
+    // Carried forward the above comment from the Nightwatch test.  This is disabled in manifest.json so it will be skipped in testing for the time being.
+    if (!disabled) {
       cy.visit('/caregivers');
-    cy.get('body', { timeout: Timeouts.normal }).should('be.visible');
-    cy.injectAxeThenAxeCheck();
+      cy.get('body', { timeout: Timeouts.normal }).should('be.visible');
+      cy.injectAxeThenAxeCheck();
+    }
   });
 });
