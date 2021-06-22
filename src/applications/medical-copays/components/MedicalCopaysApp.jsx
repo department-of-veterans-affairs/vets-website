aa4dd7bdce26cedf61870f295116045af55a2bbc
Craig Wheeler <craig@craigwheeler.net>
2021-06-22 09:49:17 -0400

    clean up

--- src/applications/medical-copays/components/MedicalCopaysApp.jsx
+++ src/applications/medical-copays/components/MedicalCopaysApp.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { mcpFeatureToggle } from '../utils/helpers';
-import LoadingIndicator from '@department-of-veterans-affairs/component-library/LoadingIndicator';
 
 const MedicalCopaysApp = ({ children }) => {
   const showMCP = useSelector(state => mcpFeatureToggle(state));
@@ -11,18 +10,9 @@ const MedicalCopaysApp = ({ children }) => {
   }
 
   return (
-    <>
-      <div className="vads-l-grid-container large-screen:vads-u-padding-x--0 vads-u-margin-bottom--5">
-        {showMCP ? (
-          children
-        ) : (
-          <LoadingIndicator
-            setFocus
-            message="Please wait while we load the application for you."
-          />
-        )}
-      </div>
-    </>
+    <div className="vads-l-grid-container large-screen:vads-u-padding-x--0 vads-u-margin-bottom--5">
+      {children}
+    </div>
   );
 };
 
