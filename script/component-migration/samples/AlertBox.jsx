b7fa6a6bd4511370abad60b42a65f679a44d34cd
Brooks Johnson <brooks.johnson@pm.me>
2021-06-22 10:26:22 -0700

    Add a headline prop to sample

--- script/component-migration/samples/AlertBox.jsx
+++ script/component-migration/samples/AlertBox.jsx
@@ -4,6 +4,9 @@ import AlertBox from '@department-of-veterans-affairs/component-library/AlertBox
 export function Sample() {
   const myStatus = 'info';
   const someContent = 'Testing Testing';
+  const myHeadline = 'Pay attention!';
 
-  return <AlertBox status={myStatus} headline="4" content={someContent} />;
+  return (
+    <AlertBox status={myStatus} headline={myHeadline} content={someContent} />
+  );
 }
