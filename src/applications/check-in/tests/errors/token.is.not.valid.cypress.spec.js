fcb944be8fc5a1121ff0e878fdb57435d8ff543a
Mark Dewey <mdewey@governmentcio.com>
2021-06-24 13:27:53 -0400

    updated the skip

--- src/applications/check-in/tests/errors/token.is.not.valid.cypress.spec.js
+++ src/applications/check-in/tests/errors/token.is.not.valid.cypress.spec.js
@@ -3,7 +3,7 @@ import mockCheckIn from '../../api/local-mock-api/mocks/check.in.response';
 import mockValidate from '../../api/local-mock-api/mocks/validate.responses';
 
 describe('Check In Experience -- ', () => {
-  beforeEach(() => {
+  beforeEach(function() {
     if (Cypress.env('CI')) this.skip();
     cy.intercept('GET', '/v0/patient_check_in/*', req => {
       req.reply(mockValidate.createMockFailedResponse({}));
--- src/applications/check-in/tests/feature-flip/disabled.cypress.spec.js
+++ src/applications/check-in/tests/feature-flip/disabled.cypress.spec.js
@@ -1,7 +1,7 @@
 import features from '../mocks/enabled.json';
 
 describe('Check In Experience -- ', () => {
-  beforeEach(() => {
+  beforeEach(function() {
     if (Cypress.env('CI')) this.skip();
     cy.intercept('GET', '/v0/feature_toggles*', features);
   });
