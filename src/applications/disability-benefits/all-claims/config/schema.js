7cdd149943c6ff597bb63e66a742902b72f49ac0
Robin Garrison <robin.garrison@adhocteam.us>
2021-06-25 08:56:01 -0500

    Remove VA treatment end dates

--- src/applications/disability-benefits/all-claims/config/schema.js
+++ src/applications/disability-benefits/all-claims/config/schema.js
@@ -708,7 +708,7 @@ const schema = {
             pattern: "^([a-zA-Z0-9\\-'.#]([a-zA-Z0-9\\-'.# ])?)+$",
           },
           treatmentDateRange: {
-            $ref: '#/definitions/dateRangeFromRequired',
+            $ref: '#/definitions/dateRange',
           },
           treatmentCenterAddress: {
             $ref: '#/definitions/vaTreatmentCenterAddress',
