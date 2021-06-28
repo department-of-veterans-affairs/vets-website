0db9b73d8936908a976918bdf57a36cad02e3744
Christopher Valarida <chris.valarida@adhocteam.us>
2021-06-28 15:17:08 -0700

    Intersect FieldHookConfig
    Details given in the comment. :shrug:

--- src/form-builder/types.ts
+++ src/form-builder/types.ts
@@ -1,6 +1,9 @@
-import { FieldAttributes } from 'formik';
+import { FieldHookConfig } from 'formik';
 
-// The <T> is the value type of the field
-export declare type FormulateFieldProps<T> = FieldAttributes<T> & {
+// The <T> is the value type of the field.
+//
+// This probably should intersect with FieldAttributes, but that one intersects
+// with T which gives me an error.
+export type FormulateFieldProps<T> = FieldHookConfig<T> & {
   label: string;
 };
