6bd8ebe0ee6859d7de838d1340ca1ca4195f2ce3
Kelson Adams <kadams@governmentcio.com>
2021-06-29 11:07:45 -0600

    Fix linting

--- src/applications/proxy-rewrite/redirects/index.js
+++ src/applications/proxy-rewrite/redirects/index.js
@@ -32,6 +32,7 @@ const redirectIfNecessary = currentWindow => {
 
   // Redirect if it's an exact match first.
   if (absoluteRedirectMatch) {
+    // eslint-disable-next-line no-param-reassign
     currentWindow.location.href = `${environment.BASE_URL}${
       absoluteRedirectMatch.dest
     }`;
@@ -56,7 +57,6 @@ const redirectIfNecessary = currentWindow => {
     currentWindow.location.href = `${environment.BASE_URL}${
       catchAllRedirectMatch.dest
     }`;
-    return;
   }
 };
 
