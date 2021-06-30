0ef98588fa5addfef5a68d4e0d857cd8fa9cd1d7
Erik P. Hansen <eph@erikphansen.com>
2021-06-30 09:57:02 -0700

    cleanup

--- src/applications/personalization/profile/models/CommunicationGroup.unit.spec.js
+++ src/applications/personalization/profile/models/CommunicationGroup.unit.spec.js
@@ -5,23 +5,6 @@ import CommunicationChannel from './CommunicationChannel';
 
 describe('CommunicationGroup model', () => {
   describe('constructor', () => {
-    it('takes an id and an array of CommunicationChannel objects', () => {
-      /* eslint-disable no-new */
-      new CommunicationGroup({
-        id: 1,
-        communicationChannels: [
-          new CommunicationChannel({ id: 1, parentItemId: 1 }),
-          new CommunicationChannel({ id: 2, parentItemId: 1 }),
-          new CommunicationChannel({
-            id: 1,
-            parentItemId: 2,
-            isAllowed: true,
-            permissionId: 123,
-          }),
-        ],
-      });
-      /* eslint-enable no-new */
-    });
     it('throws an error if not given an id', () => {
       expect(() => {
         // eslint-disable-next-line no-new
@@ -37,8 +20,9 @@ describe('CommunicationGroup model', () => {
   });
 
   describe('getUpdatedChannels()', () => {
-    it('returns an empty array when the groups are identical', () => {
-      const baseGroup = new CommunicationGroup({
+    let baseGroup;
+    beforeEach(() => {
+      baseGroup = new CommunicationGroup({
         id: 1,
         communicationChannels: [
           new CommunicationChannel({ id: 1, parentItemId: 1 }),
@@ -51,24 +35,13 @@ describe('CommunicationGroup model', () => {
           }),
         ],
       });
+    });
+    it('returns an empty array when the groups are identical', () => {
       const wipGroup = baseGroup.clone();
       const updatedChannels = baseGroup.getUpdatedChannels(wipGroup);
       expect(updatedChannels).to.deep.equal([]);
     });
     it("returns an empty array when the group's ids do not match", () => {
-      const baseGroup = new CommunicationGroup({
-        id: 1,
-        communicationChannels: [
-          new CommunicationChannel({ id: 1, parentItemId: 1 }),
-          new CommunicationChannel({ id: 2, parentItemId: 1 }),
-          new CommunicationChannel({
-            id: 1,
-            parentItemId: 2,
-            isAllowed: true,
-            permissionId: 123,
-          }),
-        ],
-      });
       const wipGroup = new CommunicationGroup({
         id: 2,
         communicationChannels: [
@@ -85,25 +58,14 @@ describe('CommunicationGroup model', () => {
       const updatedChannels = baseGroup.getUpdatedChannels(wipGroup);
       expect(updatedChannels).to.deep.equal([]);
     });
-    it("returns the argument's CommunicationChannels that are different", () => {
-      const baseGroup = new CommunicationGroup({
-        id: 1,
-        communicationChannels: [
-          new CommunicationChannel({ id: 1, parentItemId: 1 }),
-          new CommunicationChannel({ id: 2, parentItemId: 1 }),
-          new CommunicationChannel({
-            id: 1,
-            parentItemId: 2,
-            isAllowed: true,
-            permissionId: 123,
-          }),
-        ],
-      });
+    it("returns the argument group's CommunicationChannels that are different", () => {
       const wipGroup = baseGroup.clone();
       wipGroup.channels[0].setIsAllowed(true);
       wipGroup.channels[2].setIsAllowed(false);
       const updatedChannels = baseGroup.getUpdatedChannels(wipGroup);
       expect(updatedChannels.length).to.equal(2);
+      expect(updatedChannels[0].isAllowed).to.be.true;
+      expect(updatedChannels[1].isAllowed).to.be.false;
     });
   });
 
