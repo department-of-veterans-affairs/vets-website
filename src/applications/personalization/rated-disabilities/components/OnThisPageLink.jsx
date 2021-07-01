4196c7ad2759b1a12c64898dfc73fdb47a1ae070
Jesse Cohn <jesse.cohn@teamadhoc.us>
2021-07-01 12:22:58 -0700

    Made visual upates to Rated Disabilities page

---
+++ src/applications/personalization/rated-disabilities/components/OnThisPageLink.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const OnThisPageLink = props => {
+  return (
+    <a className="arrow-down-link" href={props.link}>
+      <p>
+        <span className="icon-with-info">
+          <i className="fa fa-arrow-down vads-u-padding-right--1" />
+          {props.text}
+        </span>
+      </p>
+    </a>
+  );
+};
