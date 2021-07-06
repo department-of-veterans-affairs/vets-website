e7cbcfd063b9ac61008dcbb2c434139136bd2965
Devin McCurdy <mccurdy_devin@bah.com>
2021-07-06 10:49:15 -0400

    aria hidden for icons

--- src/applications/gi-sandbox/components/IconWithInfo.jsx
+++ src/applications/gi-sandbox/components/IconWithInfo.jsx
@@ -4,7 +4,7 @@ export default function IconWithInfo({ icon, children, present }) {
   if (!present) return null;
   return (
     <p className="icon-with-info">
-      <i className={`fa fa-${icon}`} />
+      <i className={`fa fa-${icon}`} aria-hidden="true" />
       &nbsp;
       {children}
     </p>
--- src/applications/gi-sandbox/components/profile/JumpLink.jsx
+++ src/applications/gi-sandbox/components/profile/JumpLink.jsx
@@ -4,7 +4,7 @@ export default function JumpLink({ label, jumpToId }) {
   return (
     <a className="jump-link arrow-down-link" href={`#${jumpToId}`}>
       <p>
-        <i className={`fa fa-arrow-down`} />
+        <i className={`fa fa-arrow-down`} aria-hidden="true" />
         <span>{label}</span>
       </p>
     </a>
