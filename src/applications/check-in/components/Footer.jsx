deed5aaa0101189ad7ad1f6cb733d0c7c0d96b6e
Mark Dewey <mdewey@governmentcio.com>
2021-07-06 10:16:03 -0400

    added footer unit test

--- src/applications/check-in/components/Footer.jsx
+++ src/applications/check-in/components/Footer.jsx
@@ -6,7 +6,9 @@ import Telephone from '@department-of-veterans-affairs/component-library/Telepho
 function Footer({ contactNumber, header = 'Need Help?' }) {
   return (
     <footer className="row">
-      <h2 className="help-heading vads-u-font-size--lg">{header}</h2>
+      <h2 data-testid="heading" className="help-heading vads-u-font-size--lg">
+        {header}
+      </h2>
       <p>
         Ask a staff member or call us at <Telephone contact={contactNumber} />.
       </p>
