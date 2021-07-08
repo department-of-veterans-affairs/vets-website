b303155b5841a859d9cb4e2165aa30aeb679d928
Mark Dewey <mdewey@users.noreply.github.com>
2021-07-08 09:58:03 -0400

    Check in ia feedback 1 (#17789)
    * updated routes, need to fix
    
    * added route constants, updated the first see-staff flow
    
    * refined routing
    
    * Added common redirect methodl; updated checkin screen
    
    * updated todo
    
    * removed old router coder
    
    * updated navigation tests
    
    * housekeeping
    
    * Updated e2e tests
    
    * paused
    
    * added error page
    
    * upadted error message
    
    * more error updates
    
    * updated todo list
    
    * added test for withRequiredData
    
    * removed todo list

--- src/applications/check-in/pages/UpdateInformationQuestion.jsx
+++ src/applications/check-in/pages/UpdateInformationQuestion.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import { goToNextPageWithToken } from '../utils/navigation';
+import { URLS, goToNextPage } from '../utils/navigation';
 import BackToHome from '../components/BackToHome';
 import Footer from '../components/Footer';
 
-const Insurance = props => {
+const UpdateInformationQuestion = props => {
   const { router } = props;
-
   return (
     <div className="vads-l-grid-container vads-u-padding-y--5">
       <fieldset>
@@ -18,14 +17,14 @@ const Insurance = props => {
         <button
           data-testid="yes-button"
           className="usa-button-secondary usa-button-big"
-          onClick={() => goToNextPageWithToken(router, 'failed')}
+          onClick={() => goToNextPage(router, URLS.SEE_STAFF)}
         >
           Yes
         </button>
         <button
           data-testid="no-button"
           className="usa-button-secondary usa-button-big"
-          onClick={() => goToNextPageWithToken(router, 'details')}
+          onClick={() => goToNextPage(router, URLS.DETAILS)}
         >
           No
         </button>
@@ -37,4 +36,4 @@ const Insurance = props => {
   );
 };
 
-export default Insurance;
+export default UpdateInformationQuestion;
