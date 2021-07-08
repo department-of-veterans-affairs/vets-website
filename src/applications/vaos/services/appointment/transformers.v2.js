e90caaba04d05b895a0773246dd0ac5feb79234c
Jennie McGibney <jennie.mcgibney@va.gov>
2021-07-08 17:04:37 -0400

    transforms VAOS service to VA appt detail page

--- src/applications/vaos/services/appointment/transformers.v2.js
+++ src/applications/vaos/services/appointment/transformers.v2.js
@@ -1,3 +1,4 @@
+import moment from '../../lib/moment-tz';
 import {
   TYPES_OF_CARE,
   APPOINTMENT_TYPES,
@@ -7,6 +8,8 @@ import {
   TYPES_OF_SLEEP_CARE,
   AUDIOLOGY_TYPES_OF_CARE,
 } from '../../utils/constants';
+
+import { getTimezoneBySystemId } from '../../utils/timezone';
 
 function getAppointmentType(appt) {
   if (appt.kind === 'cc' && appt.start) {
@@ -36,16 +39,75 @@ function getTypeOfCareById(id) {
 /**
  * Gets the type of visit that matches our array of visit constant
  *
- * @param {Object} id VAR appointment object
+ * @param {Object} id VAOS Service appointment object
  * @returns {String} type of visit string
  */
 function getTypeOfVisit(id) {
   return TYPE_OF_VISIT.find(type => type.id === id)?.name;
 }
 
+/**
+ * Finds the datetime of the appointment depending on the appointment type
+ * and returns it as a moment object
+ *
+ * @param {Object} appt VAOS Service appointment object
+ * @returns {Object} Returns appointment datetime as moment object
+ */
+function getMomentConfirmedDate(appt) {
+  if (appt.kind === 'cc' && appt.timeZone) {
+    const zoneSplit = appt.timeZone.split(' ');
+    const offset = zoneSplit.length > 1 ? zoneSplit[0] : '+0:00';
+    return moment
+      .utc(appt.appointmentTime, 'MM/DD/YYYY HH:mm:ss')
+      .utcOffset(offset);
+  }
+
+  const timezone = getTimezoneBySystemId(appt.locationId)?.timezone;
+
+  return timezone ? moment(appt.start).tz(timezone) : moment(appt.start);
+}
+
+/**
+ *  Determines whether current time is less than appointment time
+ *  +60 min or +240 min in the case of video
+ * @param {*} appt VAOS Service appointment object
+ */
+export function isPastAppointment(appt) {
+  const isVideo = appt.kind === 'telehealth';
+  const threshold = isVideo ? 240 : 60;
+  const apptDateTime = moment(getMomentConfirmedDate(appt));
+  return apptDateTime.add(threshold, 'minutes').isBefore(moment());
+}
+
+/**
+ * Gets the atlas location and sitecode
+ *
+ * @param {Object} appt VAOS Service appointment object
+ * @returns {String} returns format data
+ */
+function getAtlasLocation(appt) {
+  const atlas = appt.telehealth.atlas;
+  return {
+    id: atlas.siteCode,
+    address: {
+      line: [atlas.address.streetAddress],
+      city: atlas.address.city,
+      state: atlas.address.state,
+      postalCode: atlas.address.zipCode,
+    },
+    position: {
+      longitude: atlas.address.longitude,
+      latitude: atlas.address.latitude,
+    },
+  };
+}
+
 export function transformVAOSAppointment(appt) {
   const isCC = appt.kind === 'cc';
   const isVideo = appt.kind === 'telehealth';
+  const isAtlas = appt.telehealth?.vvsKind === 'ADHOC';
+  const isPast = isPastAppointment(appt);
+  const providers = appt.practitioners;
 
   return {
     resourceType: 'Appointment',
@@ -83,9 +145,23 @@ export function transformVAOSAppointment(appt) {
     },
     preferredTimesForPhoneCall: appt.preferredTimesForPhoneCall,
     comment: appt.comment,
-    // TODO missing full video data
+    // TODO missing video data: status
     videoData: {
       isVideo,
+      facilityId: appt.locationId,
+      kind: appt.telehealth?.vvsKind,
+      url: appt.telehealth?.url,
+      duration: appt.minutesDuration,
+      providers: (providers || []).map(provider => ({
+        name: {
+          firstName: provider.firstName,
+          lastName: provider.lastName,
+        },
+        display: `${provider.firstName} ${provider.lastName}`,
+      })),
+      isAtlas,
+      atlasLocation: isAtlas ? getAtlasLocation(appt) : null,
+      atlasConfirmationCode: appt.telehealth?.atlas?.confirmationCode,
     },
     communityCareProvider:
       appt.start && appt.kind === 'cc' ? { id: appt.practitioners[0] } : null,
@@ -94,12 +170,13 @@ export function transformVAOSAppointment(appt) {
     practitioners: appt.practitioners,
     vaos: {
       isVideo,
+      isPastAppointment: isPast,
       appointmentType: getAppointmentType(appt),
       isCommunityCare: isCC,
       isExpressCare: false,
       requestVisitType: getTypeOfVisit(appt.kind),
       apiData: appt,
-      // TODO missing data: isPastAppointment, isCOVIDVaccine, timeZone
+      // TODO missing data: isCOVIDVaccine, timeZone
       isPhoneAppointment: appt.kind === 'phone',
     },
   };
