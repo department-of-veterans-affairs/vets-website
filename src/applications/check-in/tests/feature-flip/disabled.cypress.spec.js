40c31831171c3e0cdc603fd0b27768d145e1ac0a
Mark Dewey <mdewey@governmentcio.com>
2021-07-09 11:39:32 -0400

    fixed mocked data for cy press test

--- src/applications/check-in/tests/feature-flip/disabled.cypress.spec.js
+++ src/applications/check-in/tests/feature-flip/disabled.cypress.spec.js
@@ -1,11 +1,11 @@
-import features from '../mocks/enabled.json';
+import features from '../mocks/disabled.json';
 
 describe('Check In Experience -- ', () => {
   beforeEach(function() {
     if (Cypress.env('CI')) this.skip();
     cy.intercept('GET', '/v0/feature_toggles*', features);
   });
-  it.skip('feature is disabled', () => {
+  it('feature is disabled', () => {
     const featureRoute =
       '/health-care/appointment-check-in/?id=46bebc0a-b99c-464f-a5c5-560bc9eae287';
     cy.visit(featureRoute);
