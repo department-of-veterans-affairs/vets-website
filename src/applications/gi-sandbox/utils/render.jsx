ac135d82828657e80bf78819804cfbf97176fc57
Matt Roth <roth_matthew@bah.com>
2021-07-09 09:36:43 -0400

    switch vetteclogo to component

--- src/applications/gi-sandbox/utils/render.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const renderVetTecLogo = classNames => (
-  <img
-    className={classNames}
-    src={'/img/logo/vet-tec-logo.png'}
-    alt="Vet Tec Logo"
-  />
-);
