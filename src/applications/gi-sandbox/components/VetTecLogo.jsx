a200ff92e17b195e9292ed44cae89b5e520621ab
Matt Roth <roth_matthew@bah.com>
2021-07-09 16:10:36 -0400

    Sandbox switch to components for LearnMoreLabel and VetTecLogo (#17816)
    * switch to component
    
    * switch vetteclogo to component
    
    * Update LearnMoreLabel.jsx
    
    * cleanup
    
    * bug fixes

---
+++ src/applications/gi-sandbox/components/VetTecLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function VetTecLogo({ classNames }) {
+  return (
+    <img
+      className={classNames}
+      src={'/img/logo/vet-tec-logo.png'}
+      alt="Vet Tec Logo"
+    />
+  );
+}
