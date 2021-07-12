db0c94267e746aff608dc0066b0f8d3405d536fa
Jesse Cohn <jesse.cohn@teamadhoc.us>
2021-07-12 10:21:17 -0700

    Added aria hidden tag

--- src/applications/personalization/rated-disabilities/components/OnThisPageLink.jsx
+++ src/applications/personalization/rated-disabilities/components/OnThisPageLink.jsx
@@ -5,7 +5,10 @@ export const OnThisPageLink = props => {
     <a className="arrow-down-link" href={props.link}>
       <p>
         <span className="icon-with-info">
-          <i className="fa fa-arrow-down vads-u-padding-right--1" />
+          <i
+            className="fa fa-arrow-down vads-u-padding-right--1"
+            aria-hidden="true"
+          />
           {props.text}
         </span>
       </p>
