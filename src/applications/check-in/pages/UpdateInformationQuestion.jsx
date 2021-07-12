0306dbe8789eff27213c41da595f365b6a7f2b69
Mark Dewey <mdewey@users.noreply.github.com>
2021-07-12 12:00:53 -0400

    Check in focus management (#17820)
    * added some focus management and aria labels
    
    * fixed mocked data for cy press test

--- src/applications/check-in/pages/UpdateInformationQuestion.jsx
+++ src/applications/check-in/pages/UpdateInformationQuestion.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { URLS, goToNextPage } from '../utils/navigation';
 import BackToHome from '../components/BackToHome';
 import Footer from '../components/Footer';
+import { focusElement } from 'platform/utilities/ui';
 
 const UpdateInformationQuestion = props => {
+  useEffect(() => {
+    focusElement('h1');
+  }, []);
   const { router } = props;
   return (
     <div className="vads-l-grid-container vads-u-padding-y--5">
       <fieldset>
         <legend>
-          <h1 tabIndex="-1">
+          <h1 tabIndex="-1" className="question">
             Need to update your insurance, contact, or other information?
           </h1>
         </legend>
