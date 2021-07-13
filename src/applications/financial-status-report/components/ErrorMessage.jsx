63df42c23cd3cf0fb39105c32d34b582cfe91b4a
Justin Linn <justinslinn727@gmail.com>
2021-07-13 11:38:15 -0400

    Fix error component props

--- src/applications/financial-status-report/components/ErrorMessage.jsx
+++ src/applications/financial-status-report/components/ErrorMessage.jsx
@@ -2,42 +2,39 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 export const SubmissionError = () => (
-  <va-alert
-    status="error"
-    headline="We’re sorry. Your request wasn’t submitted."
-    content={
-      <>
-        <p>
-          Your request for financial help wasn’t submitted because something
-          went wrong on our end. We’re working to fix the problem, but it may
-          take us a while.
-        </p>
-        <h5 className="vads-u-margin-top--0">What you can do</h5>
-        <p className="vads-u-margin-top--0">
-          We've saved your progress. Please try to submit your request again
-          tomorrow.
-        </p>
-      </>
-    }
-  />
+  <va-alert status="error">
+    <h3 slot="headline">We’re sorry. Your request wasn’t submitted.</h3>
+
+    <p className="vads-u-font-size--base vads-u-font-family--sans">
+      Your request for financial help wasn’t submitted because something went
+      wrong on our end. We’re working to fix the problem, but it may take us a
+      while.
+    </p>
+
+    <h5 className="vads-u-margin-top--0 vads-u-font-size--base vads-u-font-family--sans">
+      What you can do
+    </h5>
+
+    <p className="vads-u-margin-top--0 vads-u-font-size--base vads-u-font-family--sans">
+      We've saved your progress. Please try to submit your request again
+      tomorrow.
+    </p>
+  </va-alert>
 );
 
 const ServerError = () => (
-  <div className="row vads-u-margin-bottom--5">
-    <va-alert
-      data-testid="server-error"
-      status="error"
-      headline="We're sorry. Something went wrong on our end."
-      content={
-        <>
-          <p>
-            You're unable to submit a Financial Status Report (VA Form 5655)
-            because something went wrong on our end. Please try again later.
-          </p>
-        </>
-      }
-    />
-  </div>
+  <va-alert
+    class="row vads-u-margin-bottom--5"
+    data-testid="server-error"
+    status="error"
+  >
+    <h3 slot="headline">We're sorry. Something went wrong on our end.</h3>
+
+    <p className="vads-u-font-size--base vads-u-font-family--sans">
+      You're unable to submit a Financial Status Report (VA Form 5655) because
+      something went wrong on our end. Please try again later.
+    </p>
+  </va-alert>
 );
 
 const ErrorMessage = () => {
