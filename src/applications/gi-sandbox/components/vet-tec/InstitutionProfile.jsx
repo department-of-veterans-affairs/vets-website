de0014faf4fb3d4944cec2807021abc1054d4641
Kenneth Santiago <santiago_kenneth@bah.com>
2021-07-14 13:03:28 -0400

    Added vettec profile Page framework

--- src/applications/gi-sandbox/components/vet-tec/InstitutionProfile.jsx
+++ src/applications/gi-sandbox/components/vet-tec/InstitutionProfile.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import _ from 'lodash';
 
-import AccordionItem from '../AccordionItem';
-import VetTecAdditionalInformation from './VetTecAdditionalInformation';
-import VetTecApplicationProcess from './VetTecApplicationProcess';
 import VetTecApprovedProgramsList from './VetTecApprovedProgramsList';
 import ContactInformation from '../profile/ContactInformation';
-import classNames from 'classnames';
 
 import VetTecVeteranPrograms from './VetTecVeteranPrograms';
 import VetTecEstimateYourBenefits from '../../containers/VetTecEstimateYourBenefits';
 import ProfilePageHeader from '../../containers/ProfilePageHeader';
-import VetTecLogo from '../VetTecLogo';
-
-const profileLogo =
-  'vads-u-display--block medium-screen:vads-u-display--none vettec-logo-container';
+import JumpLink from '../profile/JumpLink';
+import ProfileSection from '../profile/ProfileSection';
+import CautionaryInformation from '../profile/CautionaryInformation';
 
 const InstitutionProfile = ({
   institution,
@@ -29,46 +24,91 @@ const InstitutionProfile = ({
 
   return (
     <div>
-      {
-        <div className={profileLogo}>
-          <VetTecLogo classNames={classNames('vettec-logo-profile')} />
+      <div className="usa-grid vads-u-padding--0 vads-u-margin-bottom--4">
+        <div className="usa-width-three-fourths">
+          <ProfilePageHeader institution={institution} />
         </div>
-      }
-      <ProfilePageHeader institution={institution} />
-      <ul className="profile-accordion-list">
-        <AccordionItem button="Estimate your benefits">
-          <VetTecEstimateYourBenefits
-            institution={institution}
-            showModal={showModal}
-            selectedProgram={program}
-            preSelectedProgram={preSelectedProgram}
+
+        <div className="usa-width-one-fourth">
+          <h2 className="vads-u-padding-top--2">On this page</h2>
+          <JumpLink
+            label="Calculate your benefits"
+            jumpToId="calculate-your-benefits"
           />
-        </AccordionItem>
-        <AccordionItem button="Approved programs">
-          <VetTecApprovedProgramsList
-            programs={institution.programs}
-            selectedProgram={program}
+          <JumpLink label="Approved programs" jumpToId="approved-programs" />
+          <JumpLink
+            label="Getting started with VET TEC"
+            jumpToId="getting-started-with-benefits"
           />
-        </AccordionItem>
-        <AccordionItem button="Veteran programs">
-          <VetTecVeteranPrograms
-            institution={institution}
-            onShowModal={showModal}
+          <JumpLink
+            label="Cautionary information"
+            jumpToId="cautionary-information"
           />
-        </AccordionItem>
-        <AccordionItem button="Application process">
-          <VetTecApplicationProcess institution={institution} />
-        </AccordionItem>
-        <AccordionItem button="Contact details">
-          <ContactInformation institution={institution} />
-        </AccordionItem>
-        <AccordionItem button="Additional information">
-          <VetTecAdditionalInformation
-            institution={institution}
-            showModal={showModal}
+          <JumpLink label="Veteran programs" jumpToId="veteran-programs" />
+          <JumpLink
+            label="Get started with CareerScope"
+            jumpToId="get-started-with-career-scope"
           />
-        </AccordionItem>
-      </ul>
+          <JumpLink
+            label="Contact Information"
+            jumpToId="contact-information"
+          />
+        </div>
+      </div>
+      <ProfileSection
+        label="Calculate your benefits"
+        id="calculate-your-benefits"
+      >
+        <VetTecEstimateYourBenefits
+          institution={institution}
+          showModal={showModal}
+          selectedProgram={program}
+          preSelectedProgram={preSelectedProgram}
+        />
+      </ProfileSection>
+      <ProfileSection label="Approved programs" id="approved-programs">
+        <VetTecApprovedProgramsList
+          programs={institution.programs}
+          selectedProgram={program}
+        />
+      </ProfileSection>
+      <ProfileSection
+        label="Getting started with VET TEC"
+        id="getting-started-with-benefits"
+      />
+      <ProfileSection
+        label="Cautionary information"
+        id="cautionary-information"
+      >
+        <CautionaryInformation
+          institution={institution}
+          onShowModal={showModal}
+        />
+      </ProfileSection>
+      <ProfileSection label="Veteran programs" id="veteran-programs">
+        <VetTecVeteranPrograms
+          institution={institution}
+          onShowModal={showModal}
+        />
+      </ProfileSection>
+      <ProfileSection
+        label="Get started with CareerScope"
+        id="get-started-with-career-scope"
+      >
+        CareerScope&#174; take career and educational planning to a new level.
+        The proven career assessment and reporting system from the Vocational
+        Research Insitute is a powerful, yet easy-to-use program. <br />
+        <a
+          href="https://va.careerscope.net/gibill"
+          rel="noopener noreferrer"
+          className="vads-c-action-link--blue"
+        >
+          Get started with CareerScope
+        </a>
+      </ProfileSection>{' '}
+      <ProfileSection label="Contact Information" id="contact-information">
+        <ContactInformation institution={institution} showModal={showModal} />
+      </ProfileSection>
     </div>
   );
 };
