28e584e1a53122d904a33ba09472d829bd945d47
Christopher Valarida <chris.valarida@adhocteam.us>
2021-07-14 15:23:08 -0700

    Add required type

--- src/form-builder/types.ts
+++ src/form-builder/types.ts
@@ -3,4 +3,9 @@ import { FieldHookConfig } from 'formik';
 export type FieldProps<V> = FieldHookConfig<V> & {
   label: string;
   id?: string;
+  /**
+   * If `required` is true, the default message will be used. If `required` is a
+   * string, it will be used as the error message.
+   */
+  required?: boolean | string;
 };
