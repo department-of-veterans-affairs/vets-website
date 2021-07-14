c2f3146a2b03fcc172d6a421059025018a1f7641
Matt Roth <roth_matthew@bah.com>
2021-07-14 11:40:28 -0400

    CT Redesign: Create "Veteran programs and support" component for profile page #26834 (#17819)
    * Create VeteranProgramsAndSupport.jsx
    
    * Update InstitutionProfile.jsx
    
    * lotso f thing
    
    * ariaLbels and modals

---
+++ src/applications/gi-sandbox/components/content/modals/MilitaryTuitionAssistanceModalContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function MilitaryTuitionAssistanceModalContent() {
+  return (
+    <>
+      <h3>Military Tuition Assistance (TA)</h3>
+      <p>
+        Are you receiving any military tuition assistance this year? If so, how
+        much? The Post-9/11 GI Bill pays the net-cost of your education after
+        scholarships or financial aid amounts are applied. This includes amounts
+        already paid by military tuition assistance.
+      </p>
+    </>
+  );
+}
