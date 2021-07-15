5289a9317831bfce3905c66ba5f65cd957c3815b
Cbonade <cbonade@DESKTOP-MI88P0N.localdomain>
2021-07-15 10:21:19 -0400

    test out logging text file

--- src/platform/forms/tests/save-in-progress/01-sip-autosave.cypress.spec.js
+++ src/platform/forms/tests/save-in-progress/01-sip-autosave.cypress.spec.js
@@ -1,5 +1,5 @@
 // import moment from 'moment';
-// import Timeouts from 'platform/testing/e2e/timeouts';
+import Timeouts from 'platform/testing/e2e/timeouts';
 
 import mockUser from '../fixtures/mocks/mockUser';
 import mock1010Get from '../fixtures/mocks/mock1010Get';
@@ -20,9 +20,50 @@ describe('SIP Autosave Test', () => {
     cy.intercept('GET', '/v0/user', mockUser).as('mockUser');
     cy.intercept('GET', '/v0/in_progress_forms/1010ez', mock1010Get);
     cy.intercept('PUT', '/v0/in_progress_forms/1010ez', mock1010Put);
+    cy.login();
 
     cy.visit('/health-care/apply/application');
-    cy.login();
     cy.get('body').should('be.visible');
+    cy.intercept('GET', '/v0/health_care_applications/enrollment_status', {
+      applicationDate: '2018-01-24T00:00:00.000-06:00',
+      enrollmentDate: '2018-01-24T00:00:00.000-06:00',
+      preferredFacility: '987 - CHEY6',
+      parsedStatus: 'none_of_the_above',
+    });
+    cy.title().should('contain', 'Apply for Health Care | Veterans Affairs');
+    cy.get('.main .usa-button-primary', { timeout: Timeouts.slow }).should(
+      'be.visible',
+    );
+    cy.injectAxeThenAxeCheck();
+    cy.get('.main .usa-button-primary')
+      .first()
+      .click();
+
+    cy.url().should('not.contain', '/introduction');
+    cy.url().should('contain', '/veteran-information/birth-information');
+
+    cy.get('.schemaform-sip-save-link', { timeout: Timeouts.normal }).should(
+      'be.visible',
+    );
+    cy.get('#root_veteranSocialSecurityNumber').should(
+      'have.attr',
+      'value',
+      '123445544',
+    );
+    cy.fill(
+      'input[name="root_view:placeOfBirth_cityOfBirth"]',
+      'Northhampton, MA',
+    );
+    cy.get('.saved-success-container', { timeout: Timeouts.normal }).should(
+      'be.visible',
+    );
+    cy.get('.main .usa-button-primary').click();
+    cy.get('.schemaform-sip-save-link', { timeout: Timeouts.normal }).should(
+      'be.visible',
+    );
+    cy.intercept('PUT', '/v0/in_progress_forms/1010ez', {
+      statusCode: 500,
+      body: {},
+    });
   });
 });
