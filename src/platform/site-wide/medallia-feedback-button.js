d451091cc9e75948e94e4c71a1bc9594b11a8fe9
Holden Hinkle <holdenhinkle@gmail.com>
2021-07-19 17:25:43 -0400

    refactor and break up comments

--- src/platform/site-wide/medallia-feedback-button.js
+++ src/platform/site-wide/medallia-feedback-button.js
@@ -1,22 +1,25 @@
-/**
- * Moves #kampyleButtonContainer (the container for the Feedback button
- * powered by Medallia) from the end of <body> to the end of <main> so
- * that users can tab to it more quickly.
- * Before, users had to tab through all the links in the footer to get
- * to the button.
- * When the button is clicked on, #nebula_div_btn is removed from the DOM.
- * When the form is closed, it gets added back.
- * This script also adds a listener for when #nebula_div_btn is added
- * back and sets the focus on it again.
- */
-
 window.addEventListener('DOMContentLoaded', () => {
+  /**
+   * Moves #kampyleButtonContainer (the container for the Feedback button
+   * powered by Medallia) from the end of <body> to the end of <main> so
+   * that users can tab to it more quickly.
+   * Before, users had to tab through all the links in the footer to get
+   * to the button.
+   */
   const main = document.querySelector('main');
   const feedbackButtonContainer = document.getElementById(
     'kampyleButtonContainer',
   );
+  feedbackButtonContainer.remove();
+  main.append(feedbackButtonContainer);
 
-  const detectButtonAdded = function(mutationsList) {
+  /**
+   * When the button is clicked on, #nebula_div_btn is removed from the DOM.
+   * When the form is closed, it gets added back.
+   * An observer detects when #nebula_div_btn is added back and sets the
+   * focus on it again.
+   */
+  const observer = new MutationObserver(mutationsList => {
     mutationsList.forEach(mutation => {
       if (mutation.addedNodes.length > 0) {
         mutation.addedNodes.forEach(node => {
@@ -26,12 +29,7 @@ window.addEventListener('DOMContentLoaded', () => {
         });
       }
     });
-  };
-
-  feedbackButtonContainer.remove();
-  main.append(feedbackButtonContainer);
-
-  const observer = new MutationObserver(detectButtonAdded);
+  });
   const observeConfig = { attributes: false, childList: true };
   observer.observe(feedbackButtonContainer, observeConfig);
 });
