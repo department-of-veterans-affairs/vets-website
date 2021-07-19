e9ccf87bcf1c16293421127171ff6ec12d0255a8
Erik P. Hansen <eph@erikphansen.com>
2021-07-19 07:50:25 -0700

    Use deep links in payment info update errors

--- src/applications/personalization/profile/components/direct-deposit/PaymentInformationEditError.jsx
+++ src/applications/personalization/profile/components/direct-deposit/PaymentInformationEditError.jsx
@@ -87,8 +87,10 @@ function UpdateAddressError() {
     <p>
       We’re sorry. We couldn’t update your direct deposit bank information
       because your mailing address is missing or invalid. Please go back to{' '}
-      <a href="/profile/personal-information">your profile</a> and fill in this
-      required information.
+      <a href="/profile/personal-information#edit-mailing-address">
+        your profile
+      </a>{' '}
+      and fill in this required information.
     </p>
   );
 }
@@ -98,8 +100,11 @@ function UpdatePhoneNumberError({ phoneNumberType = 'home' }) {
     <p>
       We’re sorry. We couldn’t update your direct deposit bank information
       because your {phoneNumberType} phone number is missing or invalid. Please
-      go back to <a href="/profile/personal-information">your profile</a> and
-      fill in this required information.
+      go back to{' '}
+      <a href="/profile/personal-information#edit-home-phone-number">
+        your profile
+      </a>{' '}
+      and fill in this required information.
     </p>
   );
 }
