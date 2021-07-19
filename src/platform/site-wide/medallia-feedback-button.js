f9162733d1f3e4fa655f5efc8a6b8c28981945a5
Holden Hinkle <holdenhinkle@gmail.com>
2021-07-19 16:48:41 -0400

    focus button when form is closed

--- src/platform/site-wide/medallia-feedback-button.js
+++ src/platform/site-wide/medallia-feedback-button.js
@@ -4,6 +4,22 @@ window.addEventListener('DOMContentLoaded', () => {
     'kampyleButtonContainer',
   );
 
+  const detectButtonAdded = function(mutationsList) {
+    mutationsList.forEach(mutation => {
+      if (mutation.addedNodes.length > 0) {
+        mutation.addedNodes.forEach(node => {
+          if (node.id === 'nebula_div_btn') {
+            document.getElementById('nebula_div_btn').focus();
+          }
+        });
+      }
+    });
+  };
+
   feedbackButtonContainer.remove();
   main.append(feedbackButtonContainer);
+
+  const observer = new MutationObserver(detectButtonAdded);
+  const observeConfig = { attributes: false, childList: true };
+  observer.observe(feedbackButtonContainer, observeConfig);
 });
