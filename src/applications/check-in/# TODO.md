b8df5781044da5ac595e419985660c465072d8af
Mark Dewey <mdewey@governmentcio.com>
2021-07-19 11:00:34 -0400

    updated check in pagea

--- src/applications/check-in/# TODO.md
+++ src/applications/check-in/# TODO.md
@@ -4,6 +4,4 @@
 - [ ] Update logic for failed API response
 - [ ] Update analytics to use error messages
 - [ ] Update any e2e/unit tests
-- [ ] Update the footer?
-
-  - be sure to add UUID (Token) to state
+- [ ] Content updates
--- src/applications/check-in/api/local-mock-api/mocks/validate.responses.js
+++ src/applications/check-in/api/local-mock-api/mocks/validate.responses.js
@@ -1,7 +1,7 @@
 const createMockSuccessResponse = _data => {
   return {
     data: {
-      startTime: '2021-07-19T13:56:31',
+      startTime: '2021-08-19T13:56:31',
       facility: 'LOMA LINDA VA CLINIC',
       clinicPhoneNumber: '5551234567',
       clinicFriendlyName: 'TEST CLINIC',
