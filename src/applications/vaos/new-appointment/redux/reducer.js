1607fb96874d32e32f81015bfcd8f16f25aae9e8
Kevin Lee <LeKeve@users.noreply.github.com>
2021-07-20 10:24:59 -0700

    Fix variant test sorting facilities bugs (#17921)
    * Add missing address note and update location of blocked geolocation alert
    
    * Update facility sort by location and hide facilities list on error
    
    * Reset browser location check on page rerender
    
    * Restrict sort options if address missing

--- src/applications/vaos/new-appointment/redux/reducer.js
+++ src/applications/vaos/new-appointment/redux/reducer.js
@@ -353,12 +353,19 @@ export default function formReducer(state = initialState, action) {
         childFacilitiesStatus: FETCH_STATUS.succeeded,
         facilityPageSortMethod: sortMethod,
         showEligibilityModal: false,
+        requestLocationStatus: FETCH_STATUS.notStarted,
       };
     }
     case FORM_REQUEST_CURRENT_LOCATION: {
       return {
         ...state,
         requestLocationStatus: FETCH_STATUS.loading,
+      };
+    }
+    case FORM_REQUEST_CURRENT_LOCATION_FAILED: {
+      return {
+        ...state,
+        requestLocationStatus: FETCH_STATUS.failed,
       };
     }
     case FORM_PAGE_CC_FACILITY_SORT_METHOD_UPDATED: {
@@ -395,7 +402,6 @@ export default function formReducer(state = initialState, action) {
       let facilities = state.facilities[typeOfCareId];
       let newSchema = state.pages.vaFacilityV2;
       let requestLocationStatus = state.requestLocationStatus;
-
       if (location && facilities?.length) {
         const { coords } = location;
         const { latitude, longitude } = coords;
@@ -420,6 +426,8 @@ export default function formReducer(state = initialState, action) {
         }
 
         requestLocationStatus = FETCH_STATUS.succeeded;
+      } else {
+        requestLocationStatus = FETCH_STATUS.notStarted;
       }
 
       if (sortMethod === FACILITY_SORT_METHODS.alphabetical) {
@@ -462,12 +470,6 @@ export default function formReducer(state = initialState, action) {
         childFacilitiesStatus: FETCH_STATUS.succeeded,
         facilityPageSortMethod: sortMethod,
         requestLocationStatus,
-      };
-    }
-    case FORM_REQUEST_CURRENT_LOCATION_FAILED: {
-      return {
-        ...state,
-        requestLocationStatus: FETCH_STATUS.failed,
       };
     }
     case FORM_PAGE_FACILITY_V2_OPEN_FAILED: {
