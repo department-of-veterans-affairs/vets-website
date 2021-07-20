eaf8f5b26bf2463424cd39db7915cb3ccb2024b0
Matt Roth <roth_matthew@bah.com>
2021-07-20 13:05:28 -0400

    prevent default jump link behavior

--- src/applications/gi-sandbox/components/profile/JumpLink.jsx
+++ src/applications/gi-sandbox/components/profile/JumpLink.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { scroller } from 'react-scroll';
+import { getScrollOptions } from 'platform/utilities/ui';
 
 export default function JumpLink({ label, jumpToId }) {
+  const jumpLinkClicked = e => {
+    e.preventDefault();
+    scroller.scrollTo(jumpToId, getScrollOptions());
+  };
+
   return (
-    <a className="jump-link arrow-down-link" href={`#${jumpToId}`}>
+    <a
+      className="jump-link arrow-down-link"
+      href={`#${jumpToId}`}
+      onClick={jumpLinkClicked}
+    >
       <p>
         <i className={`fa fa-arrow-down`} aria-hidden="true" />
         <span>{label}</span>
