1f31a4494a9d3782a6d36686b03d8b604fc6bcdc
Matt Roth <roth_matthew@bah.com>
2021-07-20 09:28:32 -0400

    on bottom right of page

--- src/applications/gi-sandbox/components/BackToTop.jsx
+++ src/applications/gi-sandbox/components/BackToTop.jsx
@@ -6,18 +6,20 @@ const scroll = Scroll.animateScroll;
 
 export default function BackToTop() {
   return (
-    <div id="back-to-top-container">
-      <div className="usa-content">
-        <button
-          type="button"
-          className="usa-button va-top-button-transition-in"
-          onClick={() => scroll.scrollToTop(getScrollOptions())}
-        >
-          <span>
-            <i aria-hidden="true" className="fas fa-arrow-up" role="img" />
-          </span>
-          <span>Back to top</span>
-        </button>
+    <div className="back-to-top">
+      <div className="back-to-top-container">
+        <div className="usa-content">
+          <button
+            type="button"
+            className="usa-button va-top-button-transition-in"
+            onClick={() => scroll.scrollToTop(getScrollOptions())}
+          >
+            <span>
+              <i aria-hidden="true" className="fas fa-arrow-up" role="img" />
+            </span>
+            <span>Back to top</span>
+          </button>
+        </div>
       </div>
     </div>
   );
