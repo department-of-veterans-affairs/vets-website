4d8772f13032f67b9b520448f1cd49a3bfbcd022
Erik P. Hansen <eph@erikphansen.com>
2021-07-21 11:40:15 -0700

    Build NotificationSettings section

--- src/applications/personalization/profile/components/notification-settings/MissingContactInfoAlert.jsx
+++ src/applications/personalization/profile/components/notification-settings/MissingContactInfoAlert.jsx
@@ -1,45 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import AlertBox from '@department-of-veterans-affairs/component-library/AlertBox';
 
-import { PROFILE_PATHS } from '../../constants';
+import AddContactInfoLink from './AddContactInfoLink';
+
+const missingEmailAddressContent = (
+  <p>
+    To get notifications by email, first{' '}
+    <AddContactInfoLink missingInfo={AddContactInfoLink.EMAIL} />.
+  </p>
+);
+const missingMobilePhoneContent = (
+  <p>
+    To get notifications by SMS text message, first{' '}
+    <AddContactInfoLink missingInfo={AddContactInfoLink.MOBILE} />.
+  </p>
+);
+const missingAllContactInfoContent = (
+  <p>
+    We don‘t have an email address or mobile phone number for you. To manage
+    notification settings, please{' '}
+    <AddContactInfoLink missingInfo={AddContactInfoLink.ALL} />.
+  </p>
+);
 
 const MissingContactInfoAlert = ({
   missingMobilePhone,
   missingEmailAddress,
 }) => {
-  const missingEmailAddressContent = (
-    <p>
-      To get notifications by email, first{' '}
-      <Link to={`${PROFILE_PATHS.PERSONAL_INFORMATION}#edit-email-address`}>
-        add your email address to your profile
-      </Link>
-      .
-    </p>
-  );
-  const missingMobilePhoneContent = (
-    <p>
-      To get notifications by SMS text message, first{' '}
-      <Link
-        to={`${PROFILE_PATHS.PERSONAL_INFORMATION}#edit-mobile-phone-number`}
-      >
-        add your mobile phone number to your profile
-      </Link>
-      .
-    </p>
-  );
-  const missingAllContactInfoContent = (
-    <p>
-      We don‘t have an email address or mobile phone number for you. To manage
-      notification settings, please{' '}
-      <Link to={`${PROFILE_PATHS.PERSONAL_INFORMATION}#mobile-phone-number`}>
-        update your contact information
-      </Link>
-      .
-    </p>
-  );
-
   const alertContents = React.useMemo(
     () => {
       if (missingEmailAddress && missingMobilePhone) {
@@ -52,13 +40,7 @@ const MissingContactInfoAlert = ({
         return null;
       }
     },
-    [
-      missingEmailAddress,
-      missingMobilePhone,
-      missingAllContactInfoContent,
-      missingEmailAddressContent,
-      missingMobilePhoneContent,
-    ],
+    [missingEmailAddress, missingMobilePhone],
   );
 
   if (alertContents) {
