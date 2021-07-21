a9b5f60472b4543c278c6b05f098a8a1512460cb
Holden Hinkle <holdenhinkle@gmail.com>
2021-07-21 11:37:16 -0400

    update comments

--- src/platform/site-wide/medallia-feedback-button.js
+++ src/platform/site-wide/medallia-feedback-button.js
@@ -1,6 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
   /**
-   * Update comments
+   * Moves #kampyleButtonContainer (the container for the Feedback button
+   * powered by Medallia) from the end of <body> to the end of <main> so
+   * that users can tab to it more quickly.
+   * Before, users had to tab through all the links in the footer to get
+   * to the button.
    */
   const body = document.querySelector('body');
   const main = document.querySelector('main');
@@ -8,7 +12,8 @@ window.addEventListener('DOMContentLoaded', () => {
   let feedbackButtonContainer;
 
   /**
-   * Update comments
+   * Set an observer to detect when #kampyleButtonContainer is added to
+   * the DOM, then move it.
    */
   const bodyObserver = new MutationObserver(bodyMutationsList => {
     bodyMutationsList.forEach(bodyMutation => {
@@ -22,7 +27,10 @@ window.addEventListener('DOMContentLoaded', () => {
             main.append(feedbackButtonContainer);
 
             /**
-             * Update comments
+             * When the Feedback button is clicked on, #nebula_div_btn is removed from the DOM.
+             * When the form is closed, it is added back.
+             * Set an observer to detect when #nebula_div_btn is added back then set the
+             * focus on it again.
              */
             const buttonContainerObserver = new MutationObserver(
               buttonContainerMutationsList => {
