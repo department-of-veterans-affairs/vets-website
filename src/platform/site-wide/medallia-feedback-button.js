3844dcbd5e93119fe6acc53bcbf6f114d95299b1
Holden Hinkle <holdenhinkle@gmail.com>
2021-07-21 11:24:14 -0400

    first listen for #kampyleButtonContainer being added to DOM

--- src/platform/site-wide/medallia-feedback-button.js
+++ src/platform/site-wide/medallia-feedback-button.js
@@ -1,35 +1,52 @@
 window.addEventListener('DOMContentLoaded', () => {
   /**
-   * Moves #kampyleButtonContainer (the container for the Feedback button
-   * powered by Medallia) from the end of <body> to the end of <main> so
-   * that users can tab to it more quickly.
-   * Before, users had to tab through all the links in the footer to get
-   * to the button.
+   * Update comments
    */
+  const body = document.querySelector('body');
   const main = document.querySelector('main');
-  const feedbackButtonContainer = document.getElementById(
-    'kampyleButtonContainer',
-  );
-  feedbackButtonContainer.remove();
-  main.append(feedbackButtonContainer);
+  const observeConfig = { attributes: false, childList: true };
+  let feedbackButtonContainer;
 
   /**
-   * When the button is clicked on, #nebula_div_btn is removed from the DOM.
-   * When the form is closed, it gets added back.
-   * An observer detects when #nebula_div_btn is added back and sets the
-   * focus on it again.
+   * Update comments
    */
-  const observer = new MutationObserver(mutationsList => {
-    mutationsList.forEach(mutation => {
-      if (mutation.addedNodes.length > 0) {
-        mutation.addedNodes.forEach(node => {
-          if (node.id === 'nebula_div_btn') {
-            document.getElementById('nebula_div_btn').focus();
+  const bodyObserver = new MutationObserver(bodyMutationsList => {
+    bodyMutationsList.forEach(bodyMutation => {
+      if (bodyMutation.addedNodes.length > 0) {
+        bodyMutation.addedNodes.forEach(bodyNode => {
+          if (bodyNode.id === 'kampyleButtonContainer') {
+            feedbackButtonContainer = document.getElementById(
+              'kampyleButtonContainer',
+            );
+            feedbackButtonContainer.remove();
+            main.append(feedbackButtonContainer);
+
+            /**
+             * Update comments
+             */
+            const buttonContainerObserver = new MutationObserver(
+              buttonContainerMutationsList => {
+                buttonContainerMutationsList.forEach(containerMutation => {
+                  if (containerMutation.addedNodes.length > 0) {
+                    containerMutation.addedNodes.forEach(containerNode => {
+                      if (containerNode.id === 'nebula_div_btn') {
+                        document.getElementById('nebula_div_btn').focus();
+                      }
+                    });
+                  }
+                });
+              },
+            );
+
+            buttonContainerObserver.observe(
+              feedbackButtonContainer,
+              observeConfig,
+            );
           }
         });
       }
     });
   });
-  const observeConfig = { attributes: false, childList: true };
-  observer.observe(feedbackButtonContainer, observeConfig);
+
+  bodyObserver.observe(body, observeConfig);
 });
