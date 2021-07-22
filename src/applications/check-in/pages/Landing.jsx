b57e9d4cb338c3c584a17f712dceefab12e9a3c3
Mark Dewey <mdewey@governmentcio.com>
2021-07-22 11:59:53 -0400

    better check

--- src/applications/check-in/pages/Landing.jsx
+++ src/applications/check-in/pages/Landing.jsx
@@ -30,7 +30,7 @@ const Landing = props => {
         validateToken(token)
           .then(json => {
             const { data } = json;
-            if (data.error) {
+            if (data.error || data.errors) {
               const error = data.error || data.errors;
               recordEvent({
                 event: createAnalyticsSlug('uuid-validate-api-call-failed'),
