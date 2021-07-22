f9e329077a877d66f97a5ca40ed83a5e5e239fa3
ashagross <hello@ashagross.com>
2021-07-22 14:24:49 -0400

    Remove fragment & curly brackets

--- src/applications/appeals/10182/content/WorkInProgressMessage.jsx
+++ src/applications/appeals/10182/content/WorkInProgressMessage.jsx
@@ -9,23 +9,19 @@ export const showWorkInProgress = ({ title, subTitle }) => (
     <div className="usa-width-two-thirds medium-8 columns">
       <FormTitle title={title} subTitle={subTitle} />
       <va-alert status="info">
-        {
-          <>
-            <h3 slot="headline">We’re still working on this feature</h3>
-            <p className="vads-u-font-size--base">
-              We’re rolling out the Board Appeals (Notice of Disagreement) form
-              in stages. It’s not quite ready yet. Please check back again soon.
-            </p>
-            <p>
-              <a
-                href={NOD_INFO_URL}
-                className="u-vads-display--block u-vads-margin-top--2"
-              >
-                Return to Board Appeals page
-              </a>
-            </p>
-          </>
-        }
+        <h3 slot="headline">We’re still working on this feature</h3>
+        <p className="vads-u-font-size--base">
+          We’re rolling out the Board Appeals (Notice of Disagreement) form in
+          stages. It’s not quite ready yet. Please check back again soon.
+        </p>
+        <p>
+          <a
+            href={NOD_INFO_URL}
+            className="u-vads-display--block u-vads-margin-top--2"
+          >
+            Return to Board Appeals page
+          </a>
+        </p>
       </va-alert>
     </div>
   </div>
