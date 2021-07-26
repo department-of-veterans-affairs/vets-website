ad4955990f33777a9e995d871301909faa6f3b29
Kelson Adams <kadams@governmentcio.com>
2021-07-25 22:22:57 -0600

    Delete legacy helpers file

--- src/platform/site-wide/cta-widget/tests/helpers.unit.spec.js
+++ src/platform/site-wide/cta-widget/tests/helpers.unit.spec.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 
 import { eauthEnvironmentPrefixes } from 'platform/utilities/sso/constants';
-import { toolUrl, widgetTypes } from '../helpers';
+import { CTA_WIDGET_TYPES, ctaWidgetsLookup } from '../ctaWidgets';
 import environment from '../../../utilities/environment';
 
 describe('CTA helpers', () => {
@@ -9,74 +9,117 @@ describe('CTA helpers', () => {
     const useSSO = true;
 
     it('Download my data', () => {
-      expect(toolUrl(widgetTypes.HEALTH_RECORDS, useSSO).url).to.equal(
+      expect(
+        ctaWidgetsLookup?.[
+          CTA_WIDGET_TYPES?.HEALTH_RECORDS
+        ]?.deriveToolUrlDetails(useSSO)?.url,
+      ).to.equal(
         `https://${
           eauthEnvironmentPrefixes[environment.BUILDTYPE]
         }eauth.va.gov/mhv-portal-web/eauth?deeplinking=download_my_data`,
       );
     });
     it('Prescription Refill', () => {
-      expect(toolUrl(widgetTypes.RX, useSSO).url).to.equal(
+      expect(
+        ctaWidgetsLookup?.[CTA_WIDGET_TYPES?.RX]?.deriveToolUrlDetails(useSSO)
+          ?.url,
+      ).to.equal(
         `https://${
           eauthEnvironmentPrefixes[environment.BUILDTYPE]
         }eauth.va.gov/mhv-portal-web/eauth?deeplinking=prescription_refill`,
       );
     });
     it('Secure Messaging', () => {
-      expect(toolUrl(widgetTypes.MESSAGING, useSSO).url).to.equal(
+      expect(
+        ctaWidgetsLookup?.[CTA_WIDGET_TYPES?.MESSAGING]?.deriveToolUrlDetails(
+          useSSO,
+        )?.url,
+      ).to.equal(
         `https://${
           eauthEnvironmentPrefixes[environment.BUILDTYPE]
         }eauth.va.gov/mhv-portal-web/eauth?deeplinking=secure_messaging`,
       );
     });
     it('Appointments', () => {
-      expect(toolUrl(widgetTypes.VIEW_APPOINTMENTS, useSSO).url).to.equal(
+      expect(
+        ctaWidgetsLookup?.[
+          CTA_WIDGET_TYPES?.VIEW_APPOINTMENTS
+        ]?.deriveToolUrlDetails(useSSO)?.url,
+      ).to.equal(
         `https://${
           eauthEnvironmentPrefixes[environment.BUILDTYPE]
         }eauth.va.gov/mhv-portal-web/eauth?deeplinking=appointments`,
       );
-      expect(toolUrl(widgetTypes.SCHEDULE_APPOINTMENTS, useSSO).url).to.equal(
+      expect(
+        ctaWidgetsLookup?.[
+          CTA_WIDGET_TYPES?.SCHEDULE_APPOINTMENTS
+        ]?.deriveToolUrlDetails(useSSO)?.url,
+      ).to.equal(
         `https://${
           eauthEnvironmentPrefixes[environment.BUILDTYPE]
         }eauth.va.gov/mhv-portal-web/eauth?deeplinking=appointments`,
       );
     });
     it('Lab Tests', () => {
-      expect(toolUrl(widgetTypes.LAB_AND_TEST_RESULTS, useSSO).url).to.equal(
+      expect(
+        ctaWidgetsLookup?.[
+          CTA_WIDGET_TYPES?.LAB_AND_TEST_RESULTS
+        ]?.deriveToolUrlDetails(useSSO)?.url,
+      ).to.equal(
         `https://${
           eauthEnvironmentPrefixes[environment.BUILDTYPE]
         }eauth.va.gov/mhv-portal-web/eauth`,
       );
     });
   });
+
   describe('A signed-in non-SSO user', () => {
     it('Download my data', () => {
-      expect(toolUrl(widgetTypes.HEALTH_RECORDS).url).to.equal(
+      expect(
+        ctaWidgetsLookup?.[
+          CTA_WIDGET_TYPES?.HEALTH_RECORDS
+        ]?.deriveToolUrlDetails()?.url,
+      ).to.equal(
         'https://mhv-syst.myhealth.va.gov/mhv-portal-web/download-my-data',
       );
     });
     it('Prescription Refill', () => {
-      expect(toolUrl(widgetTypes.RX).url).to.equal(
+      expect(
+        ctaWidgetsLookup?.[CTA_WIDGET_TYPES?.RX]?.deriveToolUrlDetails()?.url,
+      ).to.equal(
         'https://mhv-syst.myhealth.va.gov/mhv-portal-web/web/myhealthevet/refill-prescriptions',
       );
     });
     it('Secure Messaging', () => {
-      expect(toolUrl(widgetTypes.MESSAGING).url).to.equal(
+      expect(
+        ctaWidgetsLookup?.[CTA_WIDGET_TYPES?.MESSAGING]?.deriveToolUrlDetails()
+          ?.url,
+      ).to.equal(
         'https://mhv-syst.myhealth.va.gov/mhv-portal-web/secure-messaging',
       );
     });
     it('Appointments', () => {
-      expect(toolUrl(widgetTypes.VIEW_APPOINTMENTS).url).to.equal(
+      expect(
+        ctaWidgetsLookup?.[
+          CTA_WIDGET_TYPES?.VIEW_APPOINTMENTS
+        ]?.deriveToolUrlDetails()?.url,
+      ).to.equal(
         'https://mhv-syst.myhealth.va.gov/mhv-portal-web/appointments',
       );
-      expect(toolUrl(widgetTypes.SCHEDULE_APPOINTMENTS).url).to.equal(
+      expect(
+        ctaWidgetsLookup?.[
+          CTA_WIDGET_TYPES?.SCHEDULE_APPOINTMENTS
+        ]?.deriveToolUrlDetails()?.url,
+      ).to.equal(
         'https://mhv-syst.myhealth.va.gov/mhv-portal-web/appointments',
       );
     });
     it('Lab Tests', () => {
-      expect(toolUrl(widgetTypes.LAB_AND_TEST_RESULTS).url).to.equal(
-        'https://mhv-syst.myhealth.va.gov/mhv-portal-web/labs-tests',
-      );
+      expect(
+        ctaWidgetsLookup?.[
+          CTA_WIDGET_TYPES?.LAB_AND_TEST_RESULTS
+        ]?.deriveToolUrlDetails()?.url,
+      ).to.equal('https://mhv-syst.myhealth.va.gov/mhv-portal-web/labs-tests');
     });
   });
 });
