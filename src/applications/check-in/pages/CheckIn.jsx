c2c475942e00a38c18a3fbe908a8849e92ff931d
Mark Dewey <mdewey@governmentcio.com>
2021-07-26 17:08:13 -0400

    updated url

--- src/applications/check-in/pages/CheckIn.jsx
+++ src/applications/check-in/pages/CheckIn.jsx
@@ -49,7 +49,7 @@ const CheckIn = props => {
           event: createAnalyticsSlug('api-checking-in-user-failed'),
           error,
         });
-        goToNextPage(router, URLS.SEE_STAFF);
+        goToNextPage(router, URLS.ERROR);
       }
     } catch (error) {
       recordEvent({
