63ce78e8e24d12058b64095e20d002cd5b66ac83
Kenneth Santiago <santiago_kenneth@bah.com>
2021-07-28 14:59:12 -0400

    updated CSS on profile page

--- src/applications/gi-sandbox/containers/ProfilePageHeader.jsx
+++ src/applications/gi-sandbox/containers/ProfilePageHeader.jsx
@@ -306,8 +306,8 @@ const ProfilePageHeader = ({
       {!expanded && !vetTecProvider && renderIconSection()}
       {!expanded && vetTecProvider && renderVetTecIconSection()}
 
-      <div className="card-bottom-cell vads-u-flex--1 vads-u-margin--0 vads-u-border-top--4px vads-u-border-color--white ">
-        <div className="vads-u-padding--0 vads-u-margin-top--neg2 vads-u-margin-bottom--0p5">
+      <div className="card-bottom-cell vads-u-flex--1 vads-u-margin--0 vads-u-border-top--4px vads-u-border-color--white vads-u-width--full">
+        <div className="vads-u-padding-bottom--1 vads-u-margin-top--neg2 vads-u-margin-bottom--0p5 vads-u-width--full compare-checkbox">
           <Checkbox
             label="Compare"
             checked={compareChecked}
