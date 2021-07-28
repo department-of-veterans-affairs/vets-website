f49fd0a4fa7a2c23450ec9adb781e84d046870f6
Cbonade <cbonade@DESKTOP-MI88P0N.localdomain>
2021-07-28 14:32:02 -0400

    Verify correct error messaging happens when attempting to test cross origin

---
+++ src/platform/site-wide/user-nav/tests/user-nav.cypress.spec.js
@@ -0,0 +1,30 @@
+import Timeouts from 'platform/testing/e2e/timeouts';
+
+const selectors = {
+  menu: '#login-root button[aria-controls="account-menu"]',
+  signOut: '#account-menu ul li:nth-child(4) a',
+};
+
+describe('User Nav Test', () => {
+  it('Displays the proper elements on the nav', () => {
+    cy.login();
+    cy.visit('/my-va');
+    cy.title().should('contain', 'My VA | Veterans Affairs');
+    Cypress.on('uncaught:exception', err => {
+      expect(err.message).to.include('Network request failed');
+    });
+
+    cy.get(selectors.menu, { timeout: Timeouts.slow })
+      .should('be.visible')
+      .then(dropDownMenu => {
+        cy.wrap(dropDownMenu).click();
+        cy.get(selectors.signOut, { timeout: Timeouts.slow })
+          .should('be.visible')
+          .and('contain', 'Sign Out')
+          .then(signOutButton => {
+            cy.wrap(signOutButton).click();
+            cy.url().should('contain', 'chrome-error://chromewebdata/');
+          });
+      });
+  });
+});
