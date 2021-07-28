72d4db74f28b9b5fbb89b3cac7eb9494a1b5b9a2
Jacob Gacek <jacob.gacek@thoughtworks.com>
2021-07-28 10:32:33 -0500

    [department-of-veterans-affairs/va-virtual-agent#000] Test commit
    Co-authored-by: Kha Huynh <kha.huynh@thoughtworks.com>

--- src/applications/virtual-agent/components/chatbot-error/ChatbotError.jsx
+++ src/applications/virtual-agent/components/chatbot-error/ChatbotError.jsx
@@ -5,10 +5,7 @@ const errorMessage = (
     We’re making some updates to the Virtual Agent. We’re sorry it’s not working
     right now. Please check back soon. If you require immediate assistance
     please call the VA.gov help desk at{' '}
-    <a href="tel:800-698-2411" aria-label="8 0 0. 6 9 8. 2 4 1 1.">
-      800-698-2411
-    </a>{' '}
-    (
+    <a href="tel:800-698-2411">800-698-2411</a> (
     <a href="tel:711" aria-label="TTY: 7 1 1.">
       TTY: 711
     </a>
