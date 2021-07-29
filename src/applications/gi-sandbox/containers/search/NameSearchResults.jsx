ac47aabab65933d58ef9902e70fba39db9f97a91
Matt Roth <roth_matthew@bah.com>
2021-07-29 12:17:08 -0400

    bad css changes

--- src/applications/gi-sandbox/containers/search/NameSearchResults.jsx
+++ src/applications/gi-sandbox/containers/search/NameSearchResults.jsx
@@ -65,7 +65,7 @@ export function NameSearchResults({
                 <FilterYourResults smallScreen={smallScreen} />
               </div>
             )}
-            <div className="column  search-results-content">
+            <div className="column small-12 medium-8">
               {inProgress && (
                 <LoadingIndicator message="Loading search results..." />
               )}
