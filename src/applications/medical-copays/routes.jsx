55fbf65202dd9f3dc3cca1dd48795b667fbb08a9
Craig Wheeler <craig@craigwheeler.net>
2021-07-29 10:22:50 -0400

    add redirect to root

--- src/applications/medical-copays/routes.jsx
+++ src/applications/medical-copays/routes.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import MedicalCopaysApp from './components/MedicalCopaysApp.jsx';
 import OverviewPage from './components/OverviewPage';
 import DetailPage from './components/DetailPage';
 
 const Routes = () => (
   <MedicalCopaysApp>
+    <Redirect exact from="/" to="/copays" />
     <Route component={OverviewPage} exact path="/copays" />
     <Route component={DetailPage} exact path="/copay-detail" />
   </MedicalCopaysApp>
