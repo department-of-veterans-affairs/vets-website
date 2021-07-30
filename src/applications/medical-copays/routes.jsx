6a3dba24ed1ee88c4ce3b0871a03614cea227fc7
Craig Wheeler <craig@craigwheeler.net>
2021-07-30 11:01:12 -0400

    27357: [MCP] copays page footer (#18069)

--- src/applications/medical-copays/routes.jsx
+++ src/applications/medical-copays/routes.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import MedicalCopaysApp from './components/MedicalCopaysApp.jsx';
-import OverviewPage from './components/OverviewPage';
-import DetailPage from './components/DetailPage';
+import { Route, Redirect } from 'react-router-dom';
+import MedicalCopaysApp from './containers/MedicalCopaysApp.jsx';
+import OverviewPage from './containers/OverviewPage';
+import DetailPage from './containers/DetailPage';
 
 const Routes = () => (
   <MedicalCopaysApp>
-    <Route component={OverviewPage} exact path="/copays" />
-    <Route component={DetailPage} exact path="/copay-detail" />
+    <Route exact path="/">
+      <Redirect to="/copays" />
+    </Route>
+    <Route component={OverviewPage} path="/copays" />
+    <Route component={DetailPage} path="/copay-detail" />
   </MedicalCopaysApp>
 );
 
