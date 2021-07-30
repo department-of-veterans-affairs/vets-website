bc30b8844034dc927c7f86d02164c0cf04f7f726
Craig Wheeler <craig@craigwheeler.net>
2021-07-30 09:40:43 -0400

    cleanup

--- src/applications/medical-copays/components/GetHelp.jsx
+++ src/applications/medical-copays/components/GetHelp.jsx
@@ -32,19 +32,19 @@ const GetHelp = () => {
           <h4>James A. Haley Veterans’ Hospital</h4>
         </dt>
         <dd>
-          <strong>Main number:</strong> <Telephone contact={'8139722000'} />
+          <strong>Main number:</strong> <Telephone contact={'813-972-2000'} />
         </dd>
         <dt>
           <h4>San Diego VA Medical Center</h4>
         </dt>
         <dd>
-          <strong>Main number:</strong> <Telephone contact={'8585528585'} />
+          <strong>Main number:</strong> <Telephone contact={'858-552-8585'} />
         </dd>
         <dt>
           <h4>Philadelphia VA Medical Center</h4>
         </dt>
         <dd>
-          <strong>Main number:</strong> <Telephone contact={'2158235800'} />
+          <strong>Main number:</strong> <Telephone contact={'215-823-5800'} />
         </dd>
       </dl>
     </div>
