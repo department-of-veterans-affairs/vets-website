db171938412cd8e8cfa1514bde26279793e094df
Christopher Valarida <chris.valarida@adhocteam.us>
2021-08-02 13:46:30 -0700

    Update JSDoc for getPages

--- src/platform/forms-system/src/js/validate-config/get-pages.js
+++ src/platform/forms-system/src/js/validate-config/get-pages.js
@@ -2,12 +2,6 @@
  * A helper function to easily iterate over each page in a form config and
  * preserve the path for debugging output.
  *
- * Returns an array of tuples containing the stringified path for debug logs the
- * page config, and the chapter config. E.g.
- * [
- *   ["chapterName.pageName", { path: "path-to-page", ... }, { title: 'Chapter Title' }]
- * ]
- *
  * @typedef {Object} PageSpec
  * @property {string} chapterName
  * @property {string} pageName
@@ -15,7 +9,7 @@
  * @property {Object} chapterConfig
  *
  * @param {object} formConfig - The formConfig
- * @return {PageSpec}
+ * @return {PageSpec[]}
  */
 const getPages = formConfig => {
   const pageConfigs = [];
