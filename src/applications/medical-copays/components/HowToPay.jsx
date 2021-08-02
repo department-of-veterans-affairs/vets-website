11ecc429c13d4ecd5a0b8dc6598757f610f86426
Craig Wheeler <craig@craigwheeler.net>
2021-08-02 15:33:43 -0400

    init va-accordian

---
+++ src/applications/medical-copays/components/HowToPay.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export const HowToPay = () => (
+  <>
+    <h2>How do I pay my VA copay bill?</h2>
+    <h5>You can pay your bill in any of these 4 ways:</h5>
+    <div className="vads-u-margin-top--4">
+      <va-accordion>
+        <va-accordion-item header="Option 1: Pay online">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
+          voluptatem numquam ipsum praesentium consequuntur recusandae modi
+          sequi expedita odio, perferendis distinctio facere odit a eligendi
+          esse saepe assumenda libero iste.
+        </va-accordion-item>
+        <va-accordion-item header="Option 2: Pay by phone">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
+          voluptatem numquam ipsum praesentium consequuntur recusandae modi
+          sequi expedita odio, perferendis distinctio facere odit a eligendi
+          esse saepe assumenda libero iste.
+        </va-accordion-item>
+        <va-accordion-item header="Option 3: Pay by mail">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
+          voluptatem numquam ipsum praesentium consequuntur recusandae modi
+          sequi expedita odio, perferendis distinctio facere odit a eligendi
+          esse saepe assumenda libero iste.
+        </va-accordion-item>
+        <va-accordion-item header="Option 4: Pay in person">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
+          voluptatem numquam ipsum praesentium consequuntur recusandae modi
+          sequi expedita odio, perferendis distinctio facere odit a eligendi
+          esse saepe assumenda libero iste.
+        </va-accordion-item>
+      </va-accordion>
+    </div>
+  </>
+);
+
+export default HowToPay;
