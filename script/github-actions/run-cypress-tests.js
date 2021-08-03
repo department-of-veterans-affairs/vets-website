94b005fa67005adb9c44aa8395e4592568b0abde
Holden Hinkle <holdenhinkle@gmail.com>
2021-08-03 15:45:47 -0400

    wip

--- script/github-actions/run-cypress-tests.js
+++ script/github-actions/run-cypress-tests.js
@@ -6,22 +6,52 @@ const { integrationFolder, testFiles } = require('../../config/cypress.json');
 // eslint-disable-next-line no-console
 console.log('Changed files: ', process.env.CHANGED_FILE_PATHS);
 const pathsOfChangedFiles = process.env.CHANGED_FILE_PATHS.split('\n');
-
 // eslint-disable-next-line no-console
 console.log('pathsOfChangedFiles: ', pathsOfChangedFiles);
 
-const pattern = path.join(__dirname, '../..', integrationFolder, testFiles);
-const tests = glob.sync(pattern);
-const divider = Math.ceil(tests.length / 8);
-const batch = tests
-  .slice(
-    Number(process.env.STEP) * divider,
-    (Number(process.env.STEP) + 1) * divider,
-  )
-  .join(',');
+// let batch;
+
+// function allPathsEndWith(str) {
+//   for (let i = 0; i < pathsOfChangedFiles.length; i += 1) {
+//     if (!pathsOfChangedFiles.endsWith(str)) return false;
+//   }
+
+//   return true;
+// }
+
+// function allPathsStartWith(str) {
+//   for (let i = 0; i < pathsOfChangedFiles.length; i += 1) {
+//     if (!pathsOfChangedFiles.startsWith(str)) return false;
+//   }
+
+//   return true;
+// }
+
+// function selectedTests() {}
+
+function allTests() {
+  const pattern = path.join(__dirname, '../..', integrationFolder, testFiles);
+  const tests = glob.sync(pattern);
+  const divider = Math.ceil(tests.length / 8);
+
+  return tests
+    .slice(
+      Number(process.env.STEP) * divider,
+      (Number(process.env.STEP) + 1) * divider,
+    )
+    .join(',');
+}
+
+// if (allPathsEndWith('.md')) {
+//   batch = '';
+// } else if (allPathsStartWith('/src/applications')) {
+//   batch = selectedTests();
+// } else {
+//   batch = allTests();
+// }
 
 const status = runCommandSync(
-  `yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --spec '${batch}'`,
+  `yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --spec '${allTests()}'`,
 );
 
 process.exit(status);
