1dffda261511e6e6a319fcdab269bef5721618ce
Holden Hinkle <holdenhinkle@gmail.com>
2021-08-04 09:06:16 -0400

    refactor run-cypress-tests.js for test selection

--- script/github-actions/run-cypress-tests.js
+++ script/github-actions/run-cypress-tests.js
@@ -3,37 +3,9 @@ const glob = require('glob');
 const { runCommandSync } = require('../utils');
 const { integrationFolder, testFiles } = require('../../config/cypress.json');
 
-// eslint-disable-next-line no-console
-console.log('Changed files: ', process.env.CHANGED_FILE_PATHS);
 const pathsOfChangedFiles = process.env.CHANGED_FILE_PATHS.split(' ');
-// eslint-disable-next-line no-console
-console.log('pathsOfChangedFiles: ', pathsOfChangedFiles);
 
-// let batch;
-
-// function allPathsEndWith(str) {
-//   for (let i = 0; i < pathsOfChangedFiles.length; i += 1) {
-//     if (!pathsOfChangedFiles.endsWith(str)) return false;
-//   }
-
-//   return true;
-// }
-
-// function allPathsStartWith(str) {
-//   for (let i = 0; i < pathsOfChangedFiles.length; i += 1) {
-//     if (!pathsOfChangedFiles.startsWith(str)) return false;
-//   }
-
-//   return true;
-// }
-
-// function selectedTests() {}
-
-function allTests() {
-  const pattern = path.join(__dirname, '../..', integrationFolder, testFiles);
-  const tests = glob.sync(pattern);
-  const divider = Math.ceil(tests.length / 8);
-
+function getSliceOfTests(tests, divider) {
   return tests
     .slice(
       Number(process.env.STEP) * divider,
@@ -42,16 +14,62 @@ function allTests() {
     .join(',');
 }
 
-// if (allPathsEndWith('.md')) {
-//   batch = '';
-// } else if (allPathsStartWith('/src/applications')) {
-//   batch = selectedTests();
-// } else {
-//   batch = allTests();
-// }
+function selectedTests() {
+  const tests = [];
+  const applicationNames = pathsOfChangedFiles.map(filePath => {
+    return filePath.split('/')[2];
+  });
+
+  [...new Set(applicationNames)].forEach(name => {
+    const pattern = path.join(
+      __dirname,
+      '../..',
+      `src/applictions/${name}`,
+      'tests/**/*.cypress.spec.js?(x)',
+    );
+
+    tests.push(...glob.sync(pattern));
+  });
+
+  const divider = Math.ceil(tests.length / 8);
+  return getSliceOfTests(tests, divider);
+}
+
+function allTests() {
+  const pattern = path.join(__dirname, '../..', integrationFolder, testFiles);
+  const tests = glob.sync(pattern);
+  const divider = Math.ceil(tests.length / 8);
+  return getSliceOfTests(tests, divider);
+}
+
+let allMdFiles = true;
+let allSrcApplicationFiles = true;
+let batch;
+
+for (let i = 0; i < pathsOfChangedFiles.length; i += 1) {
+  if (!pathsOfChangedFiles[i].startsWith('src/applications')) {
+    allSrcApplicationFiles = false;
+  }
+
+  if (!pathsOfChangedFiles[i].endsWith('.md')) {
+    allMdFiles = false;
+  }
+
+  if (allMdFiles === false && allSrcApplicationFiles === false) {
+    break;
+  }
+}
+
+if (allMdFiles) {
+  batch = '';
+} else if (allSrcApplicationFiles) {
+  batch = selectedTests();
+} else {
+  batch = allTests();
+}
 
 const status = runCommandSync(
-  `yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --spec '${allTests()}'`,
+  `yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --spec '${batch}'`,
 );
 
 process.exit(status);
