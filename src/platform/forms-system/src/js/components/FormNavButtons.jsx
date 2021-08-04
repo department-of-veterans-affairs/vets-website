87c0e60d8ba5d1b87e15871f6e86fe0798fe421b
Christopher Valarida <chris.valarida@adhocteam.us>
2021-08-04 15:06:30 -0700

    FormNavButtons documentation

--- src/platform/forms-system/src/js/components/FormNavButtons.jsx
+++ src/platform/forms-system/src/js/components/FormNavButtons.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import ProgressButton from './ProgressButton';
 
+/**
+ * Render the form navigation buttons for the normal form page flow.
+ *
+ * If `goBack` is not present, the back button will not appear. If
+ * `FormNavButtons` are rendered inside a form (such as
+ * ~/platform/forms/formulate-integration/Form), use `submitToContinue` and pass
+ * the `goForward` function to the form's `onSubmit` instead. Doing this will
+ * navigate the user to the next page only if validation is successful.
+ */
 const FormNavButtons = ({ goBack, goForward, submitToContinue }) => (
   <div className="row form-progress-buttons schemaform-buttons">
     <div className="small-6 medium-5 columns">
@@ -24,4 +34,11 @@ const FormNavButtons = ({ goBack, goForward, submitToContinue }) => (
     </div>
   </div>
 );
+
+FormNavButtons.propTypes = {
+  goBack: propTypes.func,
+  goForward: propTypes.func,
+  submitToContinue: propTypes.bool,
+};
+
 export default FormNavButtons;
