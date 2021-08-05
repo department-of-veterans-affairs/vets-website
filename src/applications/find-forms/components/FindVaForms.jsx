1d500338117d7e1e8641b9d640216cfdfa2417d9
Kelson Adams <kelsonic@users.noreply.github.com>
2021-08-05 10:21:38 -0700

    Add role='list' (#18082)
    * Add role='list'
    
    * Add {/* eslint-disable-next-line jsx-a11y/no-redundant-roles */} where needed
    
    * Fix linting errors
    
    Co-authored-by: Kelson Adams <kadams@governmentcio.com>

--- src/applications/find-forms/components/FindVaForms.jsx
+++ src/applications/find-forms/components/FindVaForms.jsx
@@ -23,9 +23,10 @@ export default () => (
       claim and applying for the GI Bill or VA health care. We&apos;ll walk you
       through the process step-by-step.
     </p>
+    {/* eslint-disable-next-line jsx-a11y/no-redundant-roles */}
     <ul
-      role="list"
       className="usa-grid usa-grid-full vads-u-margin-top--3 vads-u-margin-bottom--4 vads-u-display--flex vads-u-flex-direction--column medium-screen:vads-u-flex-direction--row"
+      role="list"
     >
       <li className="featured-content-list-item vads-u-background-color--primary-alt-lightest  vads-u-padding-y--1p5 vads-u-padding-x--1p5 vads-u-margin-bottom--2 medium-screen:vads-u-margin-bottom--0 vads-u-display--flex vads-u-flex-direction--column">
         <h3 className="vads-u-font-size--base vads-u-margin--0">
