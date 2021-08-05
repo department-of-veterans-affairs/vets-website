56b82b623e4580d467c4dd8cfb94a58f40f39a3b
Holden Hinkle <holdenhinkle@gmail.com>
2021-08-05 10:54:04 -0400

    only compute batch and then execute run command

--- script/github-actions/run-cypress-tests.js
+++ script/github-actions/run-cypress-tests.js
@@ -1,81 +1,11 @@
-const path = require('path');
-const glob = require('glob');
 const { runCommandSync } = require('../utils');
-const { integrationFolder, testFiles } = require('../../config/cypress.json');
 
-// const pathsOfChangedFiles = process.env.CHANGED_FILE_PATHS.split(' '); // commented out for testing purposes
-// the following is for testing purposes
-const filepaths = process.env.CHANGED_FILE_PATHS.split(' ');
-const pathsOfChangedFiles = filepaths.filter(filepath => {
-  return (
-    filepath !== '.github/workflows/continuous-integration.yml' &&
-    filepath !== 'script/github-actions/run-cypress-tests.js'
-  );
-});
-
-function getSliceOfTests(tests, divider) {
-  return tests
-    .slice(
-      Number(process.env.STEP) * divider,
-      (Number(process.env.STEP) + 1) * divider,
-    )
-    .join(',');
-}
-
-function selectedTests() {
-  const tests = [];
-  const applicationNames = pathsOfChangedFiles.map(filePath => {
-    return filePath.split('/')[2];
-  });
-
-  [...new Set(applicationNames)].forEach(name => {
-    const pattern = path.join(
-      __dirname,
-      '../..',
-      `src/applications/${name}`,
-      'tests/**/*.cypress.spec.js?(x)',
-    );
-
-    const appTests = glob.sync(pattern);
-    tests.push(...appTests);
-  });
-
-  const divider = Math.ceil(tests.length / 8);
-  return getSliceOfTests(tests, divider);
-}
-
-function allTests() {
-  const pattern = path.join(__dirname, '../..', integrationFolder, testFiles);
-  const tests = glob.sync(pattern);
-  const divider = Math.ceil(tests.length / 8);
-  return getSliceOfTests(tests, divider);
-}
-
-let allMdFiles = true;
-let allSrcApplicationsFiles = true;
-let batch;
-
-for (let i = 0; i < pathsOfChangedFiles.length; i += 1) {
-  if (!pathsOfChangedFiles[i].startsWith('src/applications')) {
-    allSrcApplicationsFiles = false;
-  }
-
-  if (!pathsOfChangedFiles[i].endsWith('.md')) {
-    allMdFiles = false;
-  }
-
-  if (allMdFiles === false && allSrcApplicationsFiles === false) {
-    break;
-  }
-}
-
-if (allMdFiles) {
-  process.exit(); // no need to run any tests
-} else if (allSrcApplicationsFiles) {
-  batch = selectedTests();
-} else {
-  batch = allTests();
-}
+const tests = process.env.TESTS;
+const step = process.env.STEP;
+const divider = Math.ceil(tests.length / process.env.NUM_CONTAINERS);
+const batch = tests
+  .slice(Number(step) * divider, (Number(step) + 1) * divider)
+  .join(',');
 
 const status = runCommandSync(
   `yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --spec '${batch}'`,
