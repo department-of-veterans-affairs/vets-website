460f9543364b765efee5d143b4c2b09ced16e121
Holden Hinkle <holdenhinkle@gmail.com>
2021-08-05 20:46:58 -0400

    fix paths to test file

--- script/github-actions/run-cypress-tests.js
+++ script/github-actions/run-cypress-tests.js
@@ -2,28 +2,10 @@ const { runCommandSync } = require('../utils');
 
 const tests = JSON.parse(process.env.TESTS);
 const step = Number(process.env.STEP);
-
-// eslint-disable-next-line no-console
-console.log('tests: ', tests);
-
-// eslint-disable-next-line no-console
-console.log('typeof tests: ', typeof tests);
-
-// eslint-disable-next-line no-console
-console.log('tests.length: ', tests.length);
-
-// eslint-disable-next-line no-console
-console.log('step: ', step);
-
-// eslint-disable-next-line no-console
-console.log('NUM_CONTAINERS: ', Number(process.env.NUM_CONTAINERS));
-
 const divider = Math.ceil(tests.length / Number(process.env.NUM_CONTAINERS));
 
-// eslint-disable-next-line no-console
-console.log('divider: ', divider);
-
 const batch = tests
+  .map(test => test.replace('/home/runner/work', '/__w'))
   .slice(Number(step) * divider, (Number(step) + 1) * divider)
   .join(',');
 
