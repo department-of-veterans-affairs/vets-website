9416ac6ae8a5db9e52523ec99c2658ba9cb05c0f
Holden Hinkle <holdenhinkle@gmail.com>
2021-08-05 21:43:23 -0400

    filter out paths of files needed to implement test selection so it can be tested

--- script/github-actions/select-cypress-tests.js
+++ script/github-actions/select-cypress-tests.js
@@ -3,7 +3,17 @@ const path = require('path');
 const glob = require('glob');
 const { integrationFolder, testFiles } = require('../../config/cypress.json');
 
-const pathsOfChangedFiles = process.env.CHANGED_FILE_PATHS.split(' ');
+// const pathsOfChangedFiles = process.env.CHANGED_FILE_PATHS.split(' ');
+const filepaths = process.env.CHANGED_FILE_PATHS.split(' ');
+const pathsOfChangedFiles = filepaths.filter(filepath => {
+  return (
+    filepath !== 'package.json' &&
+    filepath !== 'yarn.lock' &&
+    filepath !== 'script/github-actions/run-cypress-tests.js' &&
+    filepath !== 'script/github-actions/select-cypress-tests.js' &&
+    filepath !== '.github/workflows/continuous-integration.yml'
+  );
+});
 
 function selectedTests() {
   const tests = [];
