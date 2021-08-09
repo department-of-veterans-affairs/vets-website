0ed1502eb64b26313fb9bb28d393f7b6c660c08f
Holden Hinkle <holdenhinkle@gmail.com>
2021-08-09 12:17:08 -0400

    testrail test #210290 - b

--- src/applications/gi/components/content/AboutThisTool.jsx
+++ src/applications/gi/components/content/AboutThisTool.jsx
@@ -16,7 +16,7 @@ function AboutThisTool() {
           className="about-this-tool-mobile"
           href="https://www.benefits.va.gov/GIBILL/docs/job_aids/ComparisonToolData.xlsx"
         >
-          Download Data on All Schools (Excel)
+          Download Data on All Schools (in Excel)
         </a>
       </div>
     </div>
