d9185e46f70ce13e521732113698d90c6642a12b
Devin McCurdy <mccurdy_devin@bah.com>
2021-08-09 14:24:47 -0400

    Updates

---
+++ src/applications/gi-sandbox/components/CompareHeader.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import appendQuery from 'append-query';
+import classNames from 'classnames';
+import Checkbox from './Checkbox';
+import SchoolClassification from './SchoolClassification';
+import { Link } from 'react-router-dom';
+
+export default function({
+  institutionCount,
+  institutions,
+  setPromptingFacilityCode,
+  setShowDifferences,
+  showDifferences,
+  smallScreen,
+}) {
+  const smallWrap = cards => {
+    return smallScreen ? <div className="card-wrapper">{cards}</div> : cards;
+  };
+
+  const empties = [];
+  for (let i = 0; i < 3 - institutionCount; i++) {
+    empties.push(
+      <div key={i} className="small-screen:vads-l-col--3">
+        <div className="compare-header empty-header" />
+        <div className="compare-action">
+          <Link to={'/search'}>Return to search to add</Link>
+        </div>
+      </div>,
+    );
+  }
+
+  return (
+    <div
+      className={classNames({
+        'vads-l-row': !smallScreen,
+      })}
+    >
+      <div className="small-screen:vads-l-col--3 compare-controls non-scroll-parent">
+        <div className="non-scroll-label">
+          <div className="compare-header vads-u-padding-right--1">
+            <div className="compare-page-description-label">
+              School comparison:
+            </div>
+            View school information side by side to compare schools
+          </div>
+          <div className="compare-action">
+            <Checkbox
+              checked={showDifferences}
+              label={
+                <span>
+                  <i className={`fas fa-asterisk`} /> Highlight differences
+                </span>
+              }
+              name="highlight-differences"
+              className="vads-u-display--inline-block"
+              onChange={e => setShowDifferences(e.target.checked)}
+            />
+          </div>
+        </div>
+      </div>
+      {smallWrap(
+        institutions.map((institution, index) => {
+          return (
+            <div
+              className="small-screen:vads-l-col--3 institution-card"
+              key={index}
+            >
+              <div className="compare-header institution-header">
+                <div>
+                  <SchoolClassification institution={institution} />
+                  <div className="header-fields">
+                    <div className="institution-name">
+                      {smallScreen && institution.name}
+                      {!smallScreen && (
+                        <Link
+                          to={appendQuery(
+                            `/profile/${institution.facilityCode}`,
+                          )}
+                        >
+                          {institution.name}
+                        </Link>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="compare-action">
+                <button
+                  type="button"
+                  className="va-button-link learn-more-button"
+                  onClick={() => {
+                    setPromptingFacilityCode(institution.facilityCode);
+                  }}
+                >
+                  Remove
+                </button>
+              </div>
+            </div>
+          );
+        }),
+      )}
+      {!smallScreen && empties}
+    </div>
+  );
+}
