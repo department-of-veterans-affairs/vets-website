47f81567a71e8f12e8f3568180bc297dd2a6e1ef
Jeff Balboni <jeff@adhocteam.us>
2021-08-09 15:23:52 -0400

    Direct schedule through VAOS service endpoint (#18137)

--- src/applications/vaos/services/mocks/index.js
+++ src/applications/vaos/services/mocks/index.js
@@ -31,8 +31,11 @@ const schedulingConfigurationsCC = require('./v2/scheduling_configurations_cc.js
 const schedulingConfigurations = require('./v2/scheduling_configurations.json');
 const appointmentSlotsV2 = require('./v2/slots.json');
 const clinicsV2 = require('./v2/clinics.json');
+const confirmedV2 = require('./v2/confirmed.json');
 
 varSlots.data[0].attributes.appointmentTimeSlot = generateMockSlots();
+const mockAppts = [];
+let currentMockId = 1;
 
 const responses = {
   'GET /vaos/v0/appointments': (req, res) => {
@@ -185,14 +188,16 @@ const responses = {
   },
   'PUT /vaos/v0/preferences': { data: { attributes: {} } },
   'POST /vaos/v2/appointments': (req, res) => {
-    return res.json({
-      data: {
-        id: '32152',
-        attributes: {
-          ...req.body,
-        },
+    const submittedAppt = {
+      id: `mock${currentMockId}`,
+      attributes: {
+        ...req.body,
+        start: req.body.slot ? req.body.slot.start : null,
       },
-    });
+    };
+    currentMockId++;
+    mockAppts.push(submittedAppt);
+    return res.json({ data: submittedAppt });
   },
   'PUT /vaos/v2/appointments/:id': (req, res) => {
     // TODO: also check through confirmed mocks, when those exist
@@ -214,14 +219,14 @@ const responses = {
     if (req.query.statuses?.includes('proposed')) {
       return res.json(requestsV2);
     } else if (req.query.statuses?.includes('booked')) {
-      return res.json(require('./v2/confirmed.json'));
+      return res.json(confirmedV2);
     }
 
     return res.json({ data: [] });
   },
   'GET /vaos/v2/appointments/:id': (req, res) => {
     const appointments = {
-      data: requestsV2.data.concat(require('./v2/confirmed.json').data),
+      data: requestsV2.data.concat(confirmedV2).concat(mockAppts),
     };
     return res.json({
       data: appointments.data.find(appt => appt.id === req.params.id),
