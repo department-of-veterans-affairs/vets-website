2fd81cdb0a0df484ec2a118e99313766311f1fb0
Taylor Mitchell <tmitchell@gcio.com>
2021-08-10 12:00:10 -0400

    getting closer to the radio button styles per spec.

--- src/applications/personalization/profile/helpers.js
+++ src/applications/personalization/profile/helpers.js
@@ -55,3 +55,15 @@ export const transformServiceHistoryEntryIntoTableRow = entry => {
     ),
   };
 };
+
+/**
+ * Represents an input field value.
+ *
+ * Input fields need to have a `dirty` state that represents whether or not a user has touched it.
+ * Without this state, it is extremely hard (impossible?) to write UI with required fields where
+ * the initial empty state does not get marked as a distracting error.
+ */
+export function makeField(value, optionalDirty) {
+  const dirty = optionalDirty === undefined ? false : optionalDirty;
+  return { value, dirty };
+}
