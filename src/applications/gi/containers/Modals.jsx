c14a52bb417f17a05930bbe86dcb2d98788ababd
Kenneth Santiago <santiago_kenneth@bah.com>
2021-08-11 10:54:35 -0400

    Added no opner

--- src/applications/gi/containers/Modals.jsx
+++ src/applications/gi/containers/Modals.jsx
@@ -1044,7 +1044,7 @@ export class Modals extends React.Component {
             Visit this school's website to{' '}
             <a
               href={inStateTuitionInformation}
-              rel="no opener noreferrer"
+              rel="noreferrer noopener"
               target="_blank"
             >
               see any in-state tuition requirements.
