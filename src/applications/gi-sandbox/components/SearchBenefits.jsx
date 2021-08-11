ffa16c5d31a26d8d3abfa90165b068e2f91f4b6c
Kenneth Santiago <santiago_kenneth@bah.com>
2021-08-11 14:01:56 -0400

    updated logic

--- src/applications/gi-sandbox/components/SearchBenefits.jsx
+++ src/applications/gi-sandbox/components/SearchBenefits.jsx
@@ -91,25 +91,24 @@ const SearchBenefits = ({
         onChange={e => setGiBillChapter(e.target.value)}
       />
 
-      {(militaryStatus === 'active duty' && giBillChapter === '33a') ||
-        (militaryStatus === 'active duty' &&
-          giBillChapter === '33b' && (
-            <div className="military-status-info warning form-group">
-              <i className="fa fa-warning" />
-              <a
-                title="Post 9/11 GI Bill"
-                href="http://www.benefits.va.gov/gibill/post911_gibill.asp"
-                id="anch_378"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Post 9/11 GI Bill
-              </a>{' '}
-              recipients serving on Active Duty (or transferee spouses of a
-              service member on active duty) are not eligible to receive a
-              monthly housing allowance.
-            </div>
-          ))}
+      {militaryStatus === 'active duty' &&
+        chapter33Check && (
+          <div className="military-status-info warning form-group">
+            <i className="fa fa-warning" />
+            <a
+              title="Post 9/11 GI Bill"
+              href="http://www.benefits.va.gov/gibill/post911_gibill.asp"
+              id="anch_378"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Post 9/11 GI Bill
+            </a>{' '}
+            recipients serving on Active Duty (or transferee spouses of a
+            service member on active duty) are not eligible to receive a monthly
+            housing allowance.
+          </div>
+        )}
 
       {giBillChapter === '31' && (
         <div className="military-status-info info form-group">
--- src/applications/gi-sandbox/components/profile/BenefitsForm.jsx
+++ src/applications/gi-sandbox/components/profile/BenefitsForm.jsx
@@ -135,26 +135,24 @@ export class BenefitsForm extends React.Component {
             onFocus={this.props.handleInputFocus}
           />
           <div>
-            {(this.props.militaryStatus === 'active duty' &&
-              this.props.giBillChapter === '33a') ||
-              (this.props.militaryStatus === 'active duty' &&
-                this.props.giBillChapter === '33b' && (
-                  <div className="military-status-info warning form-group">
-                    <i className="fa fa-warning" />
-                    <a
-                      title="Post 9/11 GI Bill"
-                      href="http://www.benefits.va.gov/gibill/post911_gibill.asp"
-                      id="anch_378"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Post 9/11 GI Bill
-                    </a>{' '}
-                    recipients serving on Active Duty (or transferee spouses of
-                    a service member on active duty) are not eligible to receive
-                    a monthly housing allowance.
-                  </div>
-                ))}
+            {this.props.militaryStatus === 'active duty' &&
+              chapter33Check && (
+                <div className="military-status-info warning form-group">
+                  <i className="fa fa-warning" />
+                  <a
+                    title="Post 9/11 GI Bill"
+                    href="http://www.benefits.va.gov/gibill/post911_gibill.asp"
+                    id="anch_378"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Post 9/11 GI Bill
+                  </a>{' '}
+                  recipients serving on Active Duty (or transferee spouses of a
+                  service member on active duty) are not eligible to receive a
+                  monthly housing allowance.
+                </div>
+              )}
             {this.props.giBillChapter === '31' && (
               <div className="military-status-info info form-group">
                 <i className="fa fa-info-circle" />
