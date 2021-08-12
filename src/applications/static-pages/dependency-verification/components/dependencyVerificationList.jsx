f0f00c6d2c5bf33830489a8407bbd8b169e471db
Micah Chiang <micahkchiang@gmail.com>
2021-08-12 14:19:03 -0500

    remove filter for not on award

--- src/applications/static-pages/dependency-verification/components/dependencyVerificationList.jsx
+++ src/applications/static-pages/dependency-verification/components/dependencyVerificationList.jsx
@@ -1,32 +1,23 @@
 import React from 'react';
 
-const NOTONAWARD = 'NAWDDEP';
-
 const DependencyVerificationList = ({ dependents }) => {
   return (
     <div className="vads-u-margin-top--4">
-      {dependents
-        .filter(dependent => {
-          return (
-            dependent.dependencyStatusType &&
-            dependent.dependencyStatusType !== NOTONAWARD
-          );
-        })
-        .map((dependent, index) => {
-          return (
-            <div
-              key={index}
-              className="vads-l-row vads-u-background-color--gray-lightest vads-u-margin-top--0 vads-u-margin-bottom--2 vads-u-padding--2"
-            >
-              <h3 className="vads-u-margin--0 vads-u-font-size--h5">
-                {dependent.fullName}
-              </h3>
-              <p className="vads-l-col--12 vads-u-margin-bottom--0">
-                {dependent.dependencyStatusTypeDescription}
-              </p>
-            </div>
-          );
-        })}
+      {dependents.map((dependent, index) => {
+        return (
+          <div
+            key={index}
+            className="vads-l-row vads-u-background-color--gray-lightest vads-u-margin-top--0 vads-u-margin-bottom--2 vads-u-padding--2"
+          >
+            <h3 className="vads-u-margin--0 vads-u-font-size--h5">
+              {dependent.fullName}
+            </h3>
+            <p className="vads-l-col--12 vads-u-margin-bottom--0">
+              {dependent.dependencyStatusTypeDescription}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 };
