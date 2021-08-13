45ddbf4e315bbe2acd7e40ce9c6ba958f0b70229
Craig Wheeler <craig@craigwheeler.net>
2021-08-13 13:23:16 -0400

    refactor mock data

--- src/applications/debt-letters/tests/e2e/diary-codes-next-step.cypress.spec.js
+++ src/applications/debt-letters/tests/e2e/diary-codes-next-step.cypress.spec.js
@@ -26,11 +26,6 @@ describe('Diary Codes - Next Steps', () => {
     cy.get('.usa-button')
       .first()
       .click();
-    // TODO: test is passing because it finds the default case for next step here
-    // https://github.com/department-of-veterans-affairs/vets-website/blob/dbbf4dfdc89a682e62aad01f1773021a28209cdc/src/applications/debt-letters/const/diary-codes/index.js#L783
-    // Need to revisit once work is completed on passing debts by id here and renders the correct next step
-    // https://app.zenhub.com/workspaces/vsa---debt-607736a6c8b7e2001084e3ab/issues/department-of-veterans-affairs/va.gov-team/27789
-    // cy.get('[data-testid="diary-code-080-nextstep"]');
-    cy.get('[data-testid="diary-code-default-nextstep"]');
+    cy.get('[data-testid="diary-code-100-nextstep"]');
   });
 });
