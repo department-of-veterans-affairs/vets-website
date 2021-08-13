37cd1c3a8c9e155b0786ecb40cbed965ed004bb4
fernando.boza <fernando.boza@accenturefederal.com>
2021-08-13 14:05:33 -0400

    created MailingAddressViewField.jsx for Address form

---
+++ src/applications/my-education-benefits/components/MailingAddressViewField.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function MailingAddressViewField({ formData }) {
+  const { country, street, street2, city, state, postalCode } = formData;
+  // console.log(formData)
+  return (
+    <>
+      {country}
+      <br />
+      {street} {street2 && `${street2}`}
+      <br />
+      {city}, {state} {postalCode}
+    </>
+  );
+}
