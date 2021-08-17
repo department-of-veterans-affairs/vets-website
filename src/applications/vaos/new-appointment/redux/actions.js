21c26b637e9f0cc8cdd7dfe67ee800e271ca3bf0
Jeff Balboni <jeff@adhocteam.us>
2021-08-17 16:46:08 -0400

    Pass criteria and facility information to eligibility message on VAOS clinic page (#18278)

--- src/applications/vaos/new-appointment/redux/actions.js
+++ src/applications/vaos/new-appointment/redux/actions.js
@@ -111,7 +111,6 @@ export const FORM_ELIGIBILITY_CHECKS_SUCCEEDED =
   'newAppointment/FORM_ELIGIBILITY_CHECKS_SUCCEEDED';
 export const FORM_ELIGIBILITY_CHECKS_FAILED =
   'newAppointment/FORM_ELIGIBILITY_CHECKS_FAILED';
-export const FORM_CLINIC_PAGE_OPENED = 'newAppointment/FORM_CLINIC_PAGE_OPENED';
 export const FORM_CLINIC_PAGE_OPENED_SUCCEEDED =
   'newAppointment/FORM_CLINIC_PAGE_OPENED_SUCCEEDED';
 export const START_DIRECT_SCHEDULE_FLOW =
@@ -538,21 +537,11 @@ export function updateReasonForAppointmentData(page, uiSchema, data) {
 }
 
 export function openClinicPage(page, uiSchema, schema) {
-  return async (dispatch, getState) => {
-    dispatch({
-      type: FORM_CLINIC_PAGE_OPENED,
-    });
-
-    const formData = getFormData(getState());
-    // Remove parse function when converting this call to FHIR service
-    await dispatch(fetchFacilityDetails(formData.vaFacility));
-
-    dispatch({
-      type: FORM_CLINIC_PAGE_OPENED_SUCCEEDED,
-      page,
-      uiSchema,
-      schema,
-    });
+  return {
+    type: FORM_CLINIC_PAGE_OPENED_SUCCEEDED,
+    page,
+    uiSchema,
+    schema,
   };
 }
 
