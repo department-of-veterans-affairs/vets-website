da51346b78ca2c3610252d679694bf786dcb24b5
Devin McCurdy <mccurdy_devin@bah.com>
2021-08-18 12:54:16 -0400

    CT Sandbox - Compare Page Updates (#18265)

--- src/applications/gi-sandbox/containers/ComparePage.jsx
+++ src/applications/gi-sandbox/containers/ComparePage.jsx
@@ -49,7 +49,7 @@ export function ComparePage({
 }) {
   const [showDifferences, setShowDifferences] = useState(false);
   const [promptingFacilityCode, setPromptingFacilityCode] = useState(null);
-  const [headerClass, setHeaderClass] = useState(null);
+  const [headerFixed, setHeaderFixed] = useState(false);
   const [scrollTo, setScrollTo] = useState(null);
   const [initialTop, setInitialTop] = useState(null);
   const [currentXScroll, setCurrentXScroll] = useState(0);
@@ -62,8 +62,6 @@ export function ComparePage({
   const { version } = preview;
   const institutionCount = loaded.length;
   const history = useHistory();
-  const isSticky = headerClass === 'sticky';
-  const isLimited = headerClass === 'limited';
   const hasScrollTo = scrollTo !== null;
 
   useEffect(
@@ -83,7 +81,7 @@ export function ComparePage({
           behavior: 'smooth',
         });
 
-        if (isSticky) {
+        if (headerFixed) {
           scrollHeaderRef.current.scroll({
             left: scrollTo,
             behavior: 'smooth',
@@ -105,7 +103,7 @@ export function ComparePage({
     return () => window.removeEventListener('resize', checkSize);
   }, []);
 
-  const toggleSticky = useCallback(
+  const handleScroll = useCallback(
     () => {
       if (!initialTop && headerRef.current && headerRef.current.offsetTop) {
         setInitialTop(headerRef.current.offsetTop);
@@ -113,34 +111,34 @@ export function ComparePage({
 
       if (initialTop) {
         const offset = window.pageYOffset;
-        if (offset > initialTop && !isSticky && !isLimited) {
-          setHeaderClass('sticky');
+        const placeholder = document.querySelector('.placeholder.open');
+        const footer = document.getElementById('footerNav');
+
+        const visibleFooterHeight = footer
+          ? window.innerHeight - footer.getBoundingClientRect().top
+          : 0;
+        if (placeholder) {
+          placeholder.style.height = headerRef.current.getBoundingClientRect().height;
+        }
+        if (offset > initialTop && !headerFixed) {
+          setHeaderFixed(true);
           scrollHeaderRef.current.scroll({
             left: scrollPageRef.current.scrollLeft,
           });
-        } else if (offset < initialTop && isSticky && !isLimited) {
-          setHeaderClass(null);
-        } else if (
-          isSticky &&
-          headerRef.current.getBoundingClientRect().bottom >=
-            scrollPageRef.current.getBoundingClientRect().bottom
-        ) {
-          // setHeaderClass('limited');
-        } else if (
-          isLimited &&
-          headerRef.current.getBoundingClientRect().bottom <
-            scrollPageRef.current.getBoundingClientRect().bottom
-        ) {
-          // setHeaderClass('sticky');
+        } else if (offset < initialTop && headerFixed) {
+          setHeaderFixed(false);
+        } else if (headerFixed) {
+          headerRef.current.style.top =
+            visibleFooterHeight > 0 ? -visibleFooterHeight : 0;
         }
       }
     },
-    [scrollHeaderRef, scrollPageRef, headerClass, initialTop],
+    [scrollHeaderRef, scrollPageRef, headerFixed, initialTop],
   );
 
   const handleBodyScrollReact = () => {
     if (
-      isSticky &&
+      headerFixed &&
       !hasScrollTo &&
       scrollHeaderRef.current.scrollLeft !== scrollPageRef.current.scrollLeft
     ) {
@@ -156,7 +154,7 @@ export function ComparePage({
 
   const handleHeaderScrollReact = () => {
     if (
-      isSticky &&
+      headerFixed &&
       !hasScrollTo &&
       scrollHeaderRef.current.scrollLeft !== scrollPageRef.current.scrollLeft
     ) {
@@ -176,12 +174,12 @@ export function ComparePage({
 
   useLayoutEffect(
     () => {
-      window.addEventListener('scroll', toggleSticky);
+      window.addEventListener('scroll', handleScroll);
       return () => {
-        window.removeEventListener('scroll', toggleSticky);
+        window.removeEventListener('scroll', handleScroll);
       };
     },
-    [toggleSticky],
+    [handleScroll],
   );
 
   if (error) {
@@ -232,9 +230,14 @@ export function ComparePage({
       )}
       <div className="content-wrapper">
         <div
+          className={classNames('placeholder', {
+            open: headerFixed,
+          })}
+        />
+        <div
           id="compareHeader"
           className={classNames({
-            [headerClass]: isSticky || isLimited,
+            fixed: headerFixed,
           })}
           ref={headerRef}
         >
