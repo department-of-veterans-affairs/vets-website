4fc69ba859294a992a70191f05e9e5baa18745e3
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2021-08-18 11:05:19 -0400

    Remove hard-coded feature flag

--- src/applications/facility-locator/utils/featureFlagSelectors.js
+++ src/applications/facility-locator/utils/featureFlagSelectors.js
@@ -1,9 +1,8 @@
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
 import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
 
-export const facilitiesPpmsSuppressAll = () => true;
-// TODO: remove hard-wired value and replace with this to test the feature flag in staging:
-// toggleValues(state)[FEATURE_FLAG_NAMES.facilitiesPpmsSuppressAll];
+export const facilitiesPpmsSuppressAll = state =>
+  toggleValues(state)[FEATURE_FLAG_NAMES.facilitiesPpmsSuppressAll];
 
 export const facilitiesPpmsSuppressPharmacies = state =>
   toggleValues(state)[FEATURE_FLAG_NAMES.facilitiesPpmsSuppressPharmacies];
