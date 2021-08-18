ba9ebbd5889dbfb515765f0e05776406196aff51
Craig Wheeler <craig@craigwheeler.net>
2021-08-18 12:30:39 -0400

    render mock data

--- src/applications/medical-copays/components/BalanceCard.jsx
+++ src/applications/medical-copays/components/BalanceCard.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { currency } from '../utils/helpers';
 
-const BalanceCard = () => (
+const renderText = amount => {
+  return amount
+    ? 'Check details and resolve this bill'
+    : 'Check your balance details';
+};
+
+const BalanceCard = ({ amount, facility, city, dueDate }) => (
   <div className="vads-u-background-color--gray-lightest vads-u-padding--3 vads-u-margin-bottom--2">
     <h3 className="card-balance vads-u-margin-top--0">
-      <a href="#">$300.00</a>
+      <a href="#">{currency(amount)}</a>
     </h3>
     <p className="card-heading vads-u-margin-top--0">
-      Copay balance for James A. Haley Veterans’ Hospital - Tampa
+      Copay balance for {facility} - {city}
     </p>
-    <div className="card-content">
-      <i aria-hidden="true" role="img" className="fa fa-exclamation-triangle" />
-      <p>
-        Pay your full balance or request financial help before
-        <strong> July 9, 2021 </strong>
-        to avoid late charges, interest, or collection actions.
-      </p>
-    </div>
+    {!!amount && (
+      <div className="card-content">
+        <i
+          aria-hidden="true"
+          role="img"
+          className="fa fa-exclamation-triangle"
+        />
+        <p>
+          Pay your full balance or request financial help before
+          <strong> {dueDate} </strong>
+          to avoid late charges, interest, or collection actions.
+        </p>
+      </div>
+    )}
     <Link className="vads-u-font-size--sm" to="/balance-details">
-      <strong>Check details and resolve this bill</strong>
+      <strong>{renderText(amount)}</strong>
       <i
         className="fa fa-chevron-right vads-u-margin-left--1"
         aria-hidden="true"
