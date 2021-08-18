d73f351986fdb6df541295d50c425e721c5c1746
Holden Hinkle <holdenhinkle@gmail.com>
2021-08-18 14:13:34 -0400

    create and write graph

--- script/github-actions/create-cross-app-import-graph.js
+++ script/github-actions/create-cross-app-import-graph.js
@@ -8,13 +8,64 @@ const imports = findImports(files, {
   relativeImports: true,
   packageImports: false,
 });
+const graph = {};
 
-try {
-  fs.writeFileSync(
-    path.resolve(__dirname, '../../config/cross_app_import_graph.json'),
-    JSON.stringify(imports),
-  );
-} catch (err) {
-  // eslint-disable-next-line no-console
-  console.error(err);
+function getAppName(file) {
+  return file.split('/')[2];
 }
+
+function dedupeGraph() {
+  Object.keys(graph).forEach(app => {
+    graph[app] = [...new Set(graph[app])];
+  });
+}
+
+function writeGraph() {
+  try {
+    fs.writeFileSync(
+      path.resolve(__dirname, '../../config/cross_app_import_graph.json'),
+      JSON.stringify(graph, null, 2),
+    );
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+}
+
+function buildGraph() {
+  Object.keys(imports).forEach(file => {
+    const appName = getAppName(file);
+    const filePathAsArray = file.split('/');
+
+    if (!graph[appName]) graph[appName] = [appName];
+
+    imports[file].forEach(importRelPath => {
+      if (importRelPath.startsWith('../')) {
+        const numDirsUp = importRelPath.split('/').filter(str => str === '..')
+          .length;
+        const importPath = importRelPath.replace(
+          '../'.repeat(numDirsUp),
+          `${filePathAsArray
+            .slice(0, filePathAsArray.length - 1 - numDirsUp)
+            .join('/')}/`,
+        );
+
+        if (
+          importPath.startsWith('src/applications') &&
+          !importPath.startsWith(`src/applications/${appName}`)
+        ) {
+          const importAppName = getAppName(importPath);
+
+          if (!graph[importAppName]) graph[importAppName] = [importAppName];
+
+          graph[appName].push(importAppName);
+          graph[importAppName].push(appName);
+        }
+      }
+    });
+  });
+}
+
+buildGraph();
+dedupeGraph();
+writeGraph();
