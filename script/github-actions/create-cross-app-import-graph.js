86063a57d2125a0edf50219ee6205ec2090a4280
Holden Hinkle <holdenhinkle@gmail.com>
2021-08-19 09:09:16 -0400

    import and log DIFF_RESULTS. comment out the rest

--- script/github-actions/create-cross-app-import-graph.js
+++ script/github-actions/create-cross-app-import-graph.js
@@ -1,71 +1,75 @@
-const fs = require('fs');
-const path = require('path');
-const findImports = require('find-imports');
+// const fs = require('fs');
+// const path = require('path');
+// const findImports = require('find-imports');
 
-const files = ['src/applications/**/*.*'];
-const imports = findImports(files, {
-  absoluteImports: true,
-  relativeImports: true,
-  packageImports: false,
-});
-const graph = {};
+const diff = process.env.DIFF_RESULTS;
+// eslint-disable-next-line no-console
+console.log('Diff: ', diff);
 
-function getAppName(file) {
-  return file.split('/')[2];
-}
+// const files = ['src/applications/**/*.*'];
+// const imports = findImports(files, {
+//   absoluteImports: true,
+//   relativeImports: true,
+//   packageImports: false,
+// });
+// const graph = {};
 
-function dedupeGraph() {
-  Object.keys(graph).forEach(app => {
-    graph[app] = [...new Set(graph[app])];
-  });
-}
+// function getAppName(file) {
+//   return file.split('/')[2];
+// }
 
-function writeGraph() {
-  try {
-    fs.writeFileSync(
-      path.resolve(__dirname, '../../config/cross_app_import_graph.json'),
-      JSON.stringify(graph, null, 2),
-    );
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.error(err);
-  }
-}
+// function dedupeGraph() {
+//   Object.keys(graph).forEach(app => {
+//     graph[app] = [...new Set(graph[app])];
+//   });
+// }
 
-function buildGraph() {
-  Object.keys(imports).forEach(file => {
-    const appName = getAppName(file);
-    const filePathAsArray = file.split('/');
+// function writeGraph() {
+//   try {
+//     fs.writeFileSync(
+//       path.resolve(__dirname, '../../config/cross_app_import_graph.json'),
+//       JSON.stringify(graph, null, 2),
+//     );
+//   } catch (err) {
+//     // eslint-disable-next-line no-console
+//     console.error(err);
+//   }
+// }
 
-    if (!graph[appName]) graph[appName] = [appName];
+// function buildGraph() {
+//   Object.keys(imports).forEach(file => {
+//     const appName = getAppName(file);
+//     const filePathAsArray = file.split('/');
 
-    imports[file].forEach(importRelPath => {
-      if (importRelPath.startsWith('../')) {
-        const numDirsUp = importRelPath.split('/').filter(str => str === '..')
-          .length;
-        const importPath = importRelPath.replace(
-          '../'.repeat(numDirsUp),
-          `${filePathAsArray
-            .slice(0, filePathAsArray.length - 1 - numDirsUp)
-            .join('/')}/`,
-        );
+//     if (!graph[appName]) graph[appName] = [appName];
 
-        if (
-          importPath.startsWith('src/applications') &&
-          !importPath.startsWith(`src/applications/${appName}`)
-        ) {
-          const importAppName = getAppName(importPath);
+//     imports[file].forEach(importRelPath => {
+//       if (importRelPath.startsWith('../')) {
+//         const numDirsUp = importRelPath.split('/').filter(str => str === '..')
+//           .length;
+//         const importPath = importRelPath.replace(
+//           '../'.repeat(numDirsUp),
+//           `${filePathAsArray
+//             .slice(0, filePathAsArray.length - 1 - numDirsUp)
+//             .join('/')}/`,
+//         );
 
-          if (!graph[importAppName]) graph[importAppName] = [importAppName];
+//         if (
+//           importPath.startsWith('src/applications') &&
+//           !importPath.startsWith(`src/applications/${appName}`)
+//         ) {
+//           const importAppName = getAppName(importPath);
 
-          graph[appName].push(importAppName);
-          graph[importAppName].push(appName);
-        }
-      }
-    });
-  });
-}
+//           if (!graph[importAppName]) graph[importAppName] = [importAppName];
 
-buildGraph();
-dedupeGraph();
-writeGraph();
+//           graph[appName].push(importAppName);
+//           graph[importAppName].push(appName);
+//         }
+//       }
+//     });
+//   });
+// }
+
+// buildGraph();
+// dedupeGraph();
+// writeGraph();
