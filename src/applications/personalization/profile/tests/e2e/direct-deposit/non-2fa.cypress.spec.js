efbecba0c45cdca00874ff9ccb832ae0268712a8
Erik P. Hansen <eph@erikphansen.com>
2021-08-21 08:43:16 -0700

    improve existing DD non-2FA Cypress tests
    make sure the DD APIs are not even called

--- src/applications/personalization/profile/tests/e2e/direct-deposit/non-2fa.cypress.spec.js
+++ src/applications/personalization/profile/tests/e2e/direct-deposit/non-2fa.cypress.spec.js
@@ -15,14 +15,29 @@ function confirmSetUp2FAAlertIsShown() {
 }
 
 describe.skip('Direct Deposit', () => {
+  let getDD4CNPBankInfoStub;
+  let getDD4EDUBankInfoStub;
   beforeEach(() => {
+    getDD4CNPBankInfoStub = cy.stub();
+    getDD4EDUBankInfoStub = cy.stub();
     cy.login();
   });
   it('should show a single "set up 2-factor authentication" alert to non-2FA users', () => {
     cy.intercept('GET', 'v0/user', mockUser);
+    cy.intercept('GET', 'v0/ppiu/payment_information', () => {
+      getDD4CNPBankInfoStub();
+    });
+    cy.intercept('GET', 'v0/profile/ch33_bank_accounts', () => {
+      getDD4EDUBankInfoStub();
+    });
     cy.visit(PROFILE_PATHS.DIRECT_DEPOSIT);
 
     // confirm the 2FA alert is shown
     confirmSetUp2FAAlertIsShown();
+
+    cy.should(() => {
+      expect(getDD4CNPBankInfoStub).not.to.be.called;
+      expect(getDD4EDUBankInfoStub).not.to.be.called;
+    });
   });
 });
