379d7d836f4bb75d8246ef587bf41611d64e6d16
Erik P. Hansen <eph@erikphansen.com>
2021-08-21 10:59:47 -0700

    DRY out the ID.me verified Cypress tests

--- src/applications/personalization/profile/tests/e2e/direct-deposit/non-verified-idme.cypress.spec.js
+++ src/applications/personalization/profile/tests/e2e/direct-deposit/non-verified-idme.cypress.spec.js
@@ -1,25 +1,50 @@
 import { PROFILE_PATHS } from '@@profile/constants';
+import { mockGETEndpoints } from '@@profile/tests/e2e/helpers';
 
 import { makeUserObject } from '~/applications/personalization/common/helpers';
 
+let getDD4CNPBankInfoStub;
+let getDD4EDUBankInfoStub;
+
 function notIDme2FAAlertShown() {
-  cy.findByRole('button', { name: /set up 2-factor authentication/i }).should(
-    'exist',
-  );
+  cy.findByRole('link', {
+    name: /create a verified account through ID\.me/i,
+  }).should('exist');
   cy.findByText(
-    /You’ll need to set up 2-factor authentication before you can edit your direct deposit information/i,
+    /You’ll need to verify your account to edit direct deposit information/i,
   )
     .should('exist')
     .closest('.usa-alert-continue')
     .should('exist');
 }
 
+function directDepositAPIsNotCalled() {
+  cy.should(() => {
+    expect(getDD4CNPBankInfoStub).not.to.be.called;
+    expect(getDD4EDUBankInfoStub).not.to.be.called;
+  });
+}
+
 describe.skip('Direct Deposit', () => {
-  let getDD4CNPBankInfoStub;
-  let getDD4EDUBankInfoStub;
   beforeEach(() => {
+    // explicitly mock all required APIs to be 500s to speed up the tests
+    mockGETEndpoints([
+      'v0/profile/personal_information',
+      'v0/profile/service_history',
+      'v0/profile/full_name',
+      'v0/profile/status',
+      'v0/mhv_account',
+      'v0/feature_toggles*',
+      '/v0/disability_compensation_form/rating_info',
+    ]);
     getDD4CNPBankInfoStub = cy.stub();
     getDD4EDUBankInfoStub = cy.stub();
+    cy.intercept('GET', 'v0/ppiu/payment_information', () => {
+      getDD4CNPBankInfoStub();
+    });
+    cy.intercept('GET', 'v0/profile/ch33_bank_accounts', () => {
+      getDD4EDUBankInfoStub();
+    });
     cy.login();
   });
   context('when user is a non-2FA ID.me user', () => {
@@ -30,22 +55,13 @@ describe.skip('Direct Deposit', () => {
         multifactor: false,
       });
       cy.intercept('GET', 'v0/user', mockUser);
-      cy.intercept('GET', 'v0/ppiu/payment_information', () => {
-        getDD4CNPBankInfoStub();
-      });
-      cy.intercept('GET', 'v0/profile/ch33_bank_accounts', () => {
-        getDD4EDUBankInfoStub();
-      });
     });
     it('should show a single "verify your account" alert and not call direct deposit APIs', () => {
       cy.visit(PROFILE_PATHS.DIRECT_DEPOSIT);
 
       notIDme2FAAlertShown();
 
-      cy.should(() => {
-        expect(getDD4CNPBankInfoStub).not.to.be.called;
-        expect(getDD4EDUBankInfoStub).not.to.be.called;
-      });
+      directDepositAPIsNotCalled();
     });
   });
   context('when user has 2FA set up but signed in with DSLogon', () => {
@@ -56,22 +72,13 @@ describe.skip('Direct Deposit', () => {
         multifactor: true,
       });
       cy.intercept('GET', 'v0/user', mockUser);
-      cy.intercept('GET', 'v0/ppiu/payment_information', () => {
-        getDD4CNPBankInfoStub();
-      });
-      cy.intercept('GET', 'v0/profile/ch33_bank_accounts', () => {
-        getDD4EDUBankInfoStub();
-      });
     });
     it('should show a single "verify your account" alert and not call direct deposit APIs', () => {
       cy.visit(PROFILE_PATHS.DIRECT_DEPOSIT);
 
       notIDme2FAAlertShown();
 
-      cy.should(() => {
-        expect(getDD4CNPBankInfoStub).not.to.be.called;
-        expect(getDD4EDUBankInfoStub).not.to.be.called;
-      });
+      directDepositAPIsNotCalled();
     });
   });
   context(
@@ -84,22 +91,13 @@ describe.skip('Direct Deposit', () => {
           multifactor: true,
         });
         cy.intercept('GET', 'v0/user', mockUser);
-        cy.intercept('GET', 'v0/ppiu/payment_information', () => {
-          getDD4CNPBankInfoStub();
-        });
-        cy.intercept('GET', 'v0/profile/ch33_bank_accounts', () => {
-          getDD4EDUBankInfoStub();
-        });
       });
       it('should show a single "verify your account" alert and not call direct deposit APIs', () => {
         cy.visit(PROFILE_PATHS.DIRECT_DEPOSIT);
 
         notIDme2FAAlertShown();
 
-        cy.should(() => {
-          expect(getDD4CNPBankInfoStub).not.to.be.called;
-          expect(getDD4EDUBankInfoStub).not.to.be.called;
-        });
+        directDepositAPIsNotCalled();
       });
     },
   );
