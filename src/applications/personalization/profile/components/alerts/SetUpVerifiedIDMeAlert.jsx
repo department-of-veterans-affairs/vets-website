719ec91e737d52618f4a0906502abf44e347ac9e
Erik P. Hansen <eph@erikphansen.com>
2021-08-23 07:19:10 -0700

    Show non-ID.me users an alert on the Direct Deposit page in lower envs (#18347)
    * move makeUserObject and add new options
    
    * improve existing DD non-2FA Cypress tests
    
    make sure the DD APIs are not even called
    
    * use nullish coalescing operator in makeUserObject()
    
    * start showing Direct Deposit section to any non-ID.me users
    
    * show "set up 2FA" to anyone other than ID.me + 2FA users
    
    * expand the Cypress tests to make sure non-ID.me users see the alert
    
    * DRY out the ID.me verified Cypress tests
    
    * Use Telephone component in FraudVictimAlert
    
    * Update the SetUp2FAAlert
    
    no longer needs to know about SSOe
    
    * rename alert component
    
    * nit fix

---
+++ src/applications/personalization/profile/components/alerts/SetUpVerifiedIDMeAlert.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import AlertBox from '@department-of-veterans-affairs/component-library/AlertBox';
+import Telephone, {
+  CONTACTS,
+  PATTERNS,
+} from '@department-of-veterans-affairs/component-library/Telephone';
+
+import recordEvent from '~/platform/monitoring/record-event';
+
+const SetUpVerifiedIDMeAlert = () => {
+  return (
+    <AlertBox
+      className="vads-u-margin-bottom--2"
+      headline="You’ll need to verify your account to edit direct deposit information online."
+      level={2}
+      content={
+        <>
+          <p>
+            We require this to help protect your bank account information and
+            prevent fraud.
+          </p>
+          <h3 className="vads-u-font-size--h4">What you can do</h3>
+          <p>
+            If you have questions or concerns about your direct deposit, call us
+            at <Telephone contact={CONTACTS.VA_BENEFITS} /> (TTY:{' '}
+            <Telephone contact={CONTACTS['711']} pattern={PATTERNS['911']} />
+            ). We’re here Monday through Friday, 8:00 a.m. to 9:00 p.m. ET.
+          </p>
+          <p>
+            You can also{' '}
+            <a
+              href="https://www.id.me/registration/new"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={() => {
+                recordEvent({ event: 'multifactor-link-clicked' });
+              }}
+            >
+              create a verified account through ID.me
+            </a>
+            , and then use it to log into VA.gov and update your direct deposit
+            information.
+          </p>
+        </>
+      }
+      status="continue"
+      isVisible
+    />
+  );
+};
+
+export default SetUpVerifiedIDMeAlert;
