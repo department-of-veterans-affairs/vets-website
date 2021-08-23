7ad1c753b65d37da2f4ecc9834febb9872ea8763
fernando.boza <fernando.boza@accenturefederal.com>
2021-08-23 11:27:30 -0400

    wrapped regex in try catch for numbers without dashes

--- src/applications/my-education-benefits/components/PhoneViewField.jsx
+++ src/applications/my-education-benefits/components/PhoneViewField.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export default function PhoneViewField({ formData }) {
-  const phone = formData.phone.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+  let phone;
+  try {
+    phone = formData.phone.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+  } catch (err) {
+    phone = formData.phone;
+  }
   return <>{phone}</>;
 }
