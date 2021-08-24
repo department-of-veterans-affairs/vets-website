f43561c81f8f2a9b095a0e5b61d1534f7898a216
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2021-08-24 10:56:10 -0400

    Add a delay before rendering the "We couldn't find that" error message

--- src/applications/facility-locator/components/MessagePromptDiv.jsx
+++ src/applications/facility-locator/components/MessagePromptDiv.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const MessagePromptDiv = props => {
-  return (
-    <div className="dropdown" role="alert" id={props.id}>
-      <div className="vads-u-margin--1p5">{props.message}</div>
-    </div>
+const MessagePromptDiv = ({ id, message, waitBeforeShow = 0 }) => {
+  const [isShown, setIsShown] = useState(false);
+
+  useEffect(
+    () => {
+      setTimeout(() => {
+        setIsShown(true);
+      }, waitBeforeShow);
+    },
+    [waitBeforeShow],
   );
+
+  return isShown ? (
+    <div className="dropdown" role="alert" id={id}>
+      <div className="vads-u-margin--1p5">{message}</div>
+    </div>
+  ) : null;
 };
 
 MessagePromptDiv.propTypes = {
