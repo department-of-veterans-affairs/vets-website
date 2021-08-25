a1216b8e67dedf118939d4911d7ae6a47465edb3
Theo Bentum <theo@adhocteam.us>
2021-08-24 17:33:35 -0700

    Remove set

--- src/platform/forms-system/src/js/definitions/address.js
+++ src/platform/forms-system/src/js/definitions/address.js
@@ -176,12 +176,8 @@ export function uiSchema(
       if (stateList) {
         // We have a list and it’s different, so we need to make schema updates
         if (addressSchema.properties.state.enum !== stateList) {
-          const withEnum = set(
-            'state.enum',
-            stateList,
-            schemaUpdate.properties,
-          );
-          schemaUpdate.properties = set('state.enumNames', labelList, withEnum);
+          schemaUpdate.properties.state.enum = stateList;
+          schemaUpdate.properties.state.enumNames = labelList;
 
           // all the countries with state lists require the state field, so add that if necessary
           if (
