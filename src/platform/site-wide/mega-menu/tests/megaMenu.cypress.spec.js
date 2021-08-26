56ba677bd0910349fa6aee53adbc2441ae967b2b
Ryan Leahy <ryan.leahy@adhocteam.us>
2021-08-26 15:28:56 -0400

    Remove feature toggles mock from cypress tests

--- src/platform/site-wide/mega-menu/tests/megaMenu.cypress.spec.js
+++ src/platform/site-wide/mega-menu/tests/megaMenu.cypress.spec.js
@@ -1,5 +1,4 @@
 // Relative imports.
-import mockFeatureToggles from './fixtures/mocks/feature-toggles.json';
 import { mockUser } from '@@profile/tests/fixtures/users/user.js';
 
 const testFirstMenuSection = isMobile => {
@@ -74,7 +73,6 @@ const testDesktopMenuSections = () => {
 describe('Mega Menu', () => {
   context('on desktop', () => {
     beforeEach(() => {
-      cy.intercept('GET', '/v0/feature_toggles?*', mockFeatureToggles);
       cy.viewport(1280, 720);
     });
 
@@ -114,17 +112,12 @@ describe('Mega Menu', () => {
 
   context('on mobile', () => {
     beforeEach(() => {
-      cy.intercept('GET', '/v0/feature_toggles?*', mockFeatureToggles).as(
-        'getFeatureToggles',
-      );
       cy.viewport('iphone-4');
     });
 
     it('looks as expected unauthenticated', () => {
       // Visit the homepage.
       cy.visit('/');
-
-      cy.wait('@getFeatureToggles');
 
       // Test the menu sections.
       testMobileMenuSections();
@@ -140,8 +133,6 @@ describe('Mega Menu', () => {
 
       // Visit the homepage.
       cy.visit('/');
-
-      cy.wait('@getFeatureToggles');
 
       // Test the menu sections.
       testMobileMenuSections();
