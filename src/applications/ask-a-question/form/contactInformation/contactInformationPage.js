e273bdcaf0119e3e0706c8dd142d2063f2659542
Holden Hinkle <holdenhinkle@gmail.com>
2021-08-27 16:02:50 -0400

    add back x import

--- src/applications/ask-a-question/form/contactInformation/contactInformationPage.js
+++ src/applications/ask-a-question/form/contactInformation/contactInformationPage.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import phoneUI from 'platform/forms-system/src/js/definitions/phone';
 import emailUI from 'platform/forms-system/src/js/definitions/email';
+import { confirmationEmailUI } from 'applications/caregivers/definitions/UIDefinitions/sharedUI';
 import fullNameUI from './fullName/fullName';
 
 import fullSchema from '../0873-schema.json';
@@ -10,6 +11,7 @@ import {
   phoneNumberError,
   phoneTitle,
   preferredContactMethodTitle,
+  verifyEmailAddressError,
 } from '../../constants/labels';
 
 const { email, phone } = fullSchema.definitions;
@@ -36,6 +38,14 @@ const contactInformationPage = {
         required: emailAddressError,
       },
     }),
+    [formFields.verifyEmail]: _.merge(
+      confirmationEmailUI('', formFields.email),
+      {
+        'ui:errorMessages': {
+          required: verifyEmailAddressError,
+        },
+      },
+    ),
     [formFields.phone]: _.merge(phoneUI(phoneTitle), {
       'ui:required': (formData, _index) =>
         formData.preferredContactMethod === 'phone',
