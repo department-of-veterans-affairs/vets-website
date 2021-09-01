eb44bda8aa9e5e31bcddcdcbc8c1d7a905aade69
nbuckafs <nathaniel.buck@accenturefederal.com>
2021-09-01 14:19:25 -0400

    Change Form Field Name to Fix Autosave Issue

--- src/applications/my-education-benefits/config/form.js
+++ src/applications/my-education-benefits/config/form.js
@@ -86,7 +86,7 @@ const formFields = {
   contactMethodRdoBtnList: 'contactMethodRdoBtnList',
   notificationTypes: 'notificationTypes',
   militaryCommissionReceived: 'militaryCommissionReceived',
-  isSrROTCCommissioned: 'srROTCCommissioned',
+  isSrRotcCommissioned: 'isSrRotcCommissioned',
   hasDoDLoanPaymentPeriod: 'hasDoDLoanPaymentPeriod',
 };
 
@@ -756,7 +756,7 @@ const formConfig = {
                 'Did you receive a commission from a federally-sponsored U.S. military service academy?',
               'ui:widget': 'yesNo',
             },
-            [formFields.isSrROTCCommissioned]: {
+            [formFields.isSrRotcCommissioned]: {
               'ui:title': 'Were you commissioned as a result of Senior ROTC?',
               'ui:widget': 'yesNo',
             },
@@ -804,7 +804,7 @@ const formConfig = {
             type: 'object',
             required: [
               formFields.militaryCommissionReceived,
-              formFields.isSrROTCCommissioned,
+              formFields.isSrRotcCommissioned,
               formFields.hasDoDLoanPaymentPeriod,
             ],
             properties: {
@@ -812,7 +812,7 @@ const formConfig = {
                 type: 'boolean',
                 properties: {},
               },
-              [formFields.isSrROTCCommissioned]: {
+              [formFields.isSrRotcCommissioned]: {
                 type: 'boolean',
                 properties: {},
               },
