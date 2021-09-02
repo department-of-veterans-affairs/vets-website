3768fe2cc6a91face9a9d1feee839300569b7106
Holden Hinkle <holdenhinkle@gmail.com>
2021-09-02 12:58:53 -0400

    add percy vars to cypress run command

--- script/github-actions/run-cypress-tests.js
+++ script/github-actions/run-cypress-tests.js
@@ -2,7 +2,8 @@ const { runCommandSync } = require('../utils');
 
 const tests = JSON.parse(process.env.TESTS);
 const step = Number(process.env.STEP);
-const divider = Math.ceil(tests.length / Number(process.env.NUM_CONTAINERS));
+const numContainers = Number(process.env.NUM_CONTAINERS);
+const divider = Math.ceil(tests.length / numContainers);
 
 const batch = tests
   .map(test => test.replace('/home/runner/work', '/__w'))
@@ -10,7 +11,9 @@ const batch = tests
   .join(',');
 
 const status = runCommandSync(
-  `yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --spec '${batch}'`,
+  `PERCY_TOKEN=${process.env.PERCY_TOKEN} PERCY_PARALLEL_NONCE=${
+    process.env.PERCY_PARALLEL_NONCE
+  } PERCY_PARALLEL_TOTAL=${numContainers} yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --spec '${batch}'`,
 );
 
 process.exit(status);
