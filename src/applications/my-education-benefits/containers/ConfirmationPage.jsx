e7a9cf89ea18a9ea96b8b0f379df4ba821fde975
nbuckafs <nathaniel.buck@accenturefederal.com>
2021-09-03 14:31:37 -0400

    Add Approved Page

--- src/applications/my-education-benefits/containers/ConfirmationPage.jsx
+++ src/applications/my-education-benefits/containers/ConfirmationPage.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Scroll from 'react-scroll';
 
 import { focusElement } from 'platform/utilities/ui';
+import AdditionalInfo from '@department-of-veterans-affairs/component-library/AdditionalInfo';
 import FormFooter from '../components/FormFooter';
 
 const scroller = Scroll.scroller;
@@ -14,6 +15,67 @@ const scrollToTop = () => {
     smooth: true,
   });
 };
+
+const approvedPage = (
+  <div className="meb-confirmation-page meb-confirmation-page_denied">
+    <h1>You have been approved</h1>
+    <div className="feature">
+      <h3>
+        Congratulations! We reviewed your application and have determined that
+        you are entitled to educational benefits under the{' '}
+        <strong>Post-9/11 GI Bill</strong>.
+      </h3>
+      <p>
+        Your Certificate of Eligibility is now available. A physical copy will
+        also be mailed to your mailing address.
+      </p>
+      <button type="button" className="usa-button">
+        Download your Certificate of Eligibility
+      </button>
+      <a href="#">View an explanation of your benefits</a>
+    </div>
+    <h2>What happens next?</h2>
+    <ul>
+      <li>
+        Download a copy of your <a href="#">Certificate of Eligibility</a>.
+      </li>
+      <li>
+        Use our <a href="#">GI Bill Comparison Tool</a> to help you decide which
+        education program and school is best for you.
+      </li>
+      <li>
+        Once you’ve selected a school or program, you may bring your Certificate
+        of Eligibility to your School Certifying Official to provide proof of
+        eligibility.
+      </li>
+      <li>
+        Review and/or update your direct deposit information on your{' '}
+        <a href="#">VA.gov profile</a>.
+      </li>
+      <li>
+        Learn more about VA benefits and programs through the{' '}
+        <a href="#">Building Your Future with the GI Bill Series</a>.
+      </li>
+    </ul>
+
+    <AdditionalInfo triggerText="What is a Certificate of Eligibility?">
+      <p>
+        A Certificate of Eligibility is an official document from the U.S.
+        Department of Veterans Affairs that details your GI Bill benefit status.
+        You may provide this official document to your educational institution
+        to prove your eligibility status.
+      </p>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        Understanding your Certificate of Eligibility
+      </a>
+    </AdditionalInfo>
+
+    <button className="usa-button-secondary">Download your application</button>
+    <a href="#">Go to your My VA dashboard</a>
+
+    <FormFooter />
+  </div>
+);
 
 const deniedPage = (
   <div className="meb-confirmation-page meb-confirmation-page_denied">
@@ -67,11 +129,11 @@ export class ConfirmationPage extends React.Component {
     const { response } = submission;
     const name = data.veteranFullName;
 
-    const confirmationResult = 'denied';
+    const confirmationResult = 'approved';
 
     switch (confirmationResult) {
       case 'approved': {
-        return <h1>Approved</h1>;
+        return approvedPage;
       }
       case 'denied': {
         return deniedPage;
