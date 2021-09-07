99443ca3c3f3ea94acae74cf04437cd68bbddf8d
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-09-07 16:57:11 -0500

    Run longest-running Cypress test in its own Docker container on Jenkins

--- script/run-cypress-tests-docker.js
+++ script/run-cypress-tests-docker.js
@@ -3,6 +3,12 @@ const exec = require('child_process').exec;
 
 exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
   const strings = stdout.split(',').sort();
+  const index = strings.indexOf(
+    'src/applications/disability-benefits/all-claims/tests/all-claims.cypress.spec.js',
+  );
+  if (index > -1) {
+    strings.splice(index, 1);
+  }
   const divider = Math.ceil(strings.length / 6);
   const tests = strings
     .slice(
@@ -11,11 +17,21 @@ exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
     )
     .join(',');
 
-  runCommand(
-    `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
-      process.env.CI
-    } XDG_CONFIG_HOME=/tmp/cyhome${
-      process.env.STEP
-    } yarn cy:run --config video=false --spec '${tests}'`,
-  );
+  if (process.env.STEP === 5) {
+    runCommand(
+      `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
+        process.env.CI
+      } XDG_CONFIG_HOME=/tmp/cyhome${
+        process.env.STEP
+      } yarn cy:run --config video=false --spec 'src/applications/disability-benefits/all-claims/tests/all-claims.cypress.spec.js'`,
+    );
+  } else {
+    runCommand(
+      `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
+        process.env.CI
+      } XDG_CONFIG_HOME=/tmp/cyhome${
+        process.env.STEP
+      } yarn cy:run --config video=false --spec '${tests}'`,
+    );
+  }
 });
