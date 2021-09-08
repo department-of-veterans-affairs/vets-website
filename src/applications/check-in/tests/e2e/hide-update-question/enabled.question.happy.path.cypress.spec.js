300d0cc800e530b1c830a34b193cedfd14526a37
Mark Dewey <mdewey@governmentcio.com>
2021-09-08 15:23:03 -0400

    added e2e

---
+++ src/applications/check-in/tests/e2e/hide-update-question/enabled.question.happy.path.cypress.spec.js
@@ -0,0 +1,41 @@
+import { createFeatureToggles } from '../../../api/local-mock-api/mocks/feature.toggles';
+
+import mockCheckIn from '../../../api/local-mock-api/mocks/check.in.response';
+import mockValidate from '../../../api/local-mock-api/mocks/validate.responses';
+
+describe('Check In Experience -- ', () => {
+  beforeEach(function() {
+    cy.intercept('GET', '/check_in/v0/patient_check_ins//*', req => {
+      req.reply(mockValidate.createMockSuccessResponse({}));
+    });
+    cy.intercept('POST', '/check_in/v0/patient_check_ins/', req => {
+      req.reply(mockCheckIn.createMockSuccessResponse({}));
+    });
+    cy.intercept(
+      'GET',
+      '/v0/feature_toggles*',
+      createFeatureToggles(true, false, false, true),
+    );
+  });
+  afterEach(() => {
+    cy.window().then(window => {
+      window.sessionStorage.clear();
+    });
+  });
+  it('C5746 - Happy path', () => {
+    const featureRoute =
+      '/health-care/appointment-check-in/?id=46bebc0a-b99c-464f-a5c5-560bc9eae287';
+    cy.visit(featureRoute);
+    cy.get('legend > h2').contains('information');
+    cy.injectAxe();
+    cy.axeCheck();
+    cy.get('[data-testid="no-button"]').click();
+    cy.get('h1').contains('Your appointment');
+    cy.injectAxe();
+    cy.axeCheck();
+    cy.get('.usa-button').click();
+    cy.get('va-alert > h1').contains('checked in');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+});
