4e8d664ebdcf98b04279e5a35df3b53ff2a12d40
Holden Hinkle <holdenhinkle@gmail.com>
2021-09-08 15:08:13 -0400

    clean up

--- script/github-actions/select-cypress-tests.js
+++ script/github-actions/select-cypress-tests.js
@@ -232,19 +232,8 @@ function exportVariables(tests) {
 }
 
 function run() {
-  const filepaths = process.env.CHANGED_FILE_PATHS.split(' ');
-  const pathsOfChangedFiles = filepaths.filter(filepath => {
-    // Ignore the cross-app import graph file
-    return (
-      filepath !== '.github/workflows/continuous-integration.yml' &&
-      filepath !== 'config/cross_app_import_graph.json' &&
-      filepath !== 'script/github-actions/create-cross-app-import-graph.js' &&
-      filepath !== 'script/github-actions/select-cypress-tests.js'
-    );
-  });
+  const pathsOfChangedFiles = process.env.CHANGED_FILE_PATHS.split(' ');
   const graph = dedupeGraph(buildGraph());
-  // eslint-disable-next-line no-console
-  console.log('Graph: ', graph);
   const tests = selectTests(graph, pathsOfChangedFiles);
   exportVariables(tests);
 }
