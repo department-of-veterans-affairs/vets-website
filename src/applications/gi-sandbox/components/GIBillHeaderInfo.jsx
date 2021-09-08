55314fbf44cb3d7a8019aff90868b0fbb2bb709d
Joe <elder_joseph@bah.com>
2021-09-08 11:05:58 -0400

    29362: Update styling

--- src/applications/gi-sandbox/components/GIBillHeaderInfo.jsx
+++ src/applications/gi-sandbox/components/GIBillHeaderInfo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function GIBillHeaderInfo() {
   return (
     <div className="tool-description">
-      <h1>GI Bill® Comparison Tool</h1>
+      <h1 className="main-title">GI Bill® Comparison Tool</h1>
       <p className="vads-u-font-size--h3 vads-u-color--gray-dark">
         Learn about and compare your GI Bill benefits at approved schools,
         employers, and VET TEC providers.
