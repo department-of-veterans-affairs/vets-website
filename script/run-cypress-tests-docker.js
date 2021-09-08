030e03978bd219640ab3573d62379cb487223059
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-09-08 08:36:43 -0500

    Clean up

--- script/run-cypress-tests-docker.js
+++ script/run-cypress-tests-docker.js
@@ -2,36 +2,34 @@ const { runCommand } = require('./utils');
 const exec = require('child_process').exec;
 
 exec("find src -name '*.cypress.*.js' | tr '\n' ','", function(_err, stdout) {
+  const stepNumber = Number(process.env.STEP);
+  const longestTest =
+    'src/applications/disability-benefits/all-claims/tests/all-claims.cypress.spec.js';
   const strings = stdout.split(',').sort();
-  const index = strings.indexOf(
-    'src/applications/disability-benefits/all-claims/tests/all-claims.cypress.spec.js',
-  );
+  const index = strings.indexOf(longestTest);
+
   if (index > -1) {
     strings.splice(index, 1);
   }
   const divider = Math.ceil(strings.length / 6);
-  const tests = strings
-    .slice(
-      Number(process.env.STEP) * divider,
-      (Number(process.env.STEP) + 1) * divider,
-    )
-    .join(',');
 
-  if (Number(process.env.STEP) === 5) {
-    runCommand(
-      `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
-        process.env.CI
-      } XDG_CONFIG_HOME=/tmp/cyhome${
-        process.env.STEP
-      } yarn cy:run --config video=false --spec 'src/applications/disability-benefits/all-claims/tests/all-claims.cypress.spec.js'`,
-    );
+  let tests;
+  if (stepNumber === 5) {
+    tests = longestTest;
   } else {
-    runCommand(
-      `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
-        process.env.CI
-      } XDG_CONFIG_HOME=/tmp/cyhome${
-        process.env.STEP
-      } yarn cy:run --config video=false --spec '${tests}'`,
-    );
+    tests = strings
+      .slice(
+        Number(process.env.STEP) * divider,
+        (Number(process.env.STEP) + 1) * divider,
+      )
+      .join(',');
   }
+
+  runCommand(
+    `CYPRESS_BASE_URL=http://vets-website:3001 CYPRESS_CI=${
+      process.env.CI
+    } XDG_CONFIG_HOME=/tmp/cyhome${
+      process.env.STEP
+    } yarn cy:run --config video=false --spec '${tests}'`,
+  );
 });
