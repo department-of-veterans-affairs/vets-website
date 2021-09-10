28ef91a9b9fdd9e18181a9d3be1f53fdc9b70a65
Craig Wheeler <craig@craigwheeler.net>
2021-09-10 11:27:19 -0400

    29764: TypeError: Cannot read property 'date' of undefined (#18643)

--- src/applications/debt-letters/components/OnThisPageLinks.jsx
+++ src/applications/debt-letters/components/OnThisPageLinks.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
 
-export const OnThisPageLinks = ({ isDetailsPage }) => {
-  return (
-    <div>
-      <h2 className="vads-u-font-size--h3">On this page</h2>
-      <div className="vads-u-font-family--sans vads-u-display--flex vads-u-flex-direction--column">
-        {!isDetailsPage && (
-          <a href="#currentDebts" className="vads-u-margin-y--2">
-            <i
-              aria-hidden="true"
-              role="img"
-              className="fas fa-arrow-down vads-u-padding-right--1 vads-u-font-size--sm"
-            />
-            Current debts
-          </a>
-        )}
-        {isDetailsPage && (
-          <a href="#debtLetterHistory" className="vads-u-margin-y--2">
-            <i
-              aria-hidden="true"
-              role="img"
-              className="fas fa-arrow-down vads-u-padding-right--1 vads-u-font-size--sm"
-            />
-            Debt letter history
-          </a>
-        )}
+export const OnThisPageLinks = ({ isDetailsPage, hasHistory }) => (
+  <div>
+    <h2 className="vads-u-font-size--h3">On this page</h2>
+    <div className="vads-u-font-family--sans vads-u-display--flex vads-u-flex-direction--column">
+      {!isDetailsPage && (
+        <a href="#currentDebts" className="vads-u-margin-y--2">
+          <i
+            aria-hidden="true"
+            role="img"
+            className="fas fa-arrow-down vads-u-padding-right--1 vads-u-font-size--sm"
+          />
+          Current debts
+        </a>
+      )}
+      {hasHistory && (
+        <a href="#debtLetterHistory" className="vads-u-margin-y--2">
+          <i
+            aria-hidden="true"
+            role="img"
+            className="fas fa-arrow-down vads-u-padding-right--1 vads-u-font-size--sm"
+          />
+          Debt letter history
+        </a>
+      )}
+      {(!isDetailsPage || hasHistory) && (
         <a href="#downloadDebtLetters" className="vads-u-margin-y--0">
           <i
             aria-hidden="true"
@@ -33,31 +33,31 @@ export const OnThisPageLinks = ({ isDetailsPage }) => {
           />
           Download debt letters
         </a>
-        <a href="#howDoIPay" className="vads-u-margin-y--2">
-          <i
-            aria-hidden="true"
-            role="img"
-            className="fas fa-arrow-down vads-u-padding-right--1 vads-u-font-size--sm"
-          />
-          How do I pay my VA debt?
-        </a>
-        <a href="#howDoIGetHelp" className="vads-u-margin-y--0">
-          <i
-            aria-hidden="true"
-            role="img"
-            className="fas fa-arrow-down vads-u-padding-right--1 vads-u-font-size--sm"
-          />
-          How do I get financial help?
-        </a>
-        <a href="#howDoIDispute" className="vads-u-margin-y--2">
-          <i
-            aria-hidden="true"
-            role="img"
-            className="fas fa-arrow-down vads-u-padding-right--1 vads-u-font-size--sm"
-          />
-          How do I dispute a debt?
-        </a>
-      </div>
+      )}
+      <a href="#howDoIPay" className="vads-u-margin-y--2">
+        <i
+          aria-hidden="true"
+          role="img"
+          className="fas fa-arrow-down vads-u-padding-right--1 vads-u-font-size--sm"
+        />
+        How do I pay my VA debt?
+      </a>
+      <a href="#howDoIGetHelp" className="vads-u-margin-y--0">
+        <i
+          aria-hidden="true"
+          role="img"
+          className="fas fa-arrow-down vads-u-padding-right--1 vads-u-font-size--sm"
+        />
+        How do I get financial help?
+      </a>
+      <a href="#howDoIDispute" className="vads-u-margin-y--2">
+        <i
+          aria-hidden="true"
+          role="img"
+          className="fas fa-arrow-down vads-u-padding-right--1 vads-u-font-size--sm"
+        />
+        How do I dispute a debt?
+      </a>
     </div>
-  );
-};
+  </div>
+);
