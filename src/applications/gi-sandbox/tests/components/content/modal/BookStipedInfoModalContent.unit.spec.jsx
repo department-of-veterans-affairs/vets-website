dc52487bc9d7db97b46ddbd121d0f160296bd0d8
Kenneth Santiago <santiago_kenneth@bah.com>
2021-09-13 15:21:37 -0400

    resolved path

--- src/applications/gi-sandbox/tests/components/content/modal/BookStipedInfoModalContent.unit.spec.jsx
+++ src/applications/gi-sandbox/tests/components/content/modal/BookStipedInfoModalContent.unit.spec.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
-import BookStipedInfoModalContent from '../../../../components/content/modals/BookStipedInfoModalContent';
+import BookStipedInfoModalContent from '../../../../components/content/modals/BookStipendInfoModalContent';
 
 describe('BookStipedInfoModalContent modal', () => {
   it('should render', () => {
