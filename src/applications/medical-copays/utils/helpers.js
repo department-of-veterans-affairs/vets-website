19309be1978af1eb4b4c230e94546f9c3e37a1c7
Craig Wheeler <craig@craigwheeler.net>
2021-09-14 12:05:56 -0400

    replaced payment table with download statements (#18694)

--- src/applications/medical-copays/utils/helpers.js
+++ src/applications/medical-copays/utils/helpers.js
@@ -2,9 +2,8 @@ import React from 'react';
 import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
 
-export const mcpFeatureToggle = state => {
-  return toggleValues(state)[FEATURE_FLAG_NAMES.showMedicalCopays];
-};
+export const mcpFeatureToggle = state =>
+  toggleValues(state)[FEATURE_FLAG_NAMES.showMedicalCopays];
 
 export const currency = amount => {
   const formatter = new Intl.NumberFormat('en-US', {
@@ -15,10 +14,9 @@ export const currency = amount => {
   return formatter.format(parseFloat(amount));
 };
 
-export const formatTableData = tableData => {
-  return tableData.map(row => ({
+export const formatTableData = tableData =>
+  tableData.map(row => ({
     date: row.date,
     desc: <strong>{row.desc}</strong>,
     amount: currency(row.amount),
   }));
-};
