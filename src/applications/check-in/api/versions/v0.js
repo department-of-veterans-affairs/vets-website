0c5253aec53f0af391bf09931a9c1483a74b49ba
Mark Dewey <mdewey@users.noreply.github.com>
2021-09-14 17:27:39 -0400

    Check in 29477 api integration (#18670)
    * added some things
    
    * API calls are in place ?
    
    * added new permission update action
    
    * added final new API
    
    * updated redux actions
    
    * Updated checkin page to call API to get data
    
    * Addedd some basic logic for mock api
    
    * added call to v1 endpoint when enabled
    
    * removed extra import
    
    * updated todo list
    
    * Refactored Check In to Display & Logic
    
    * updated todo list
    
    * updated api export name
    
    * updated unit tests
    
    * updated to new structure
    
    * refactored responses to creation file
    
    * mocked our endpoints for e2e tests
    
    * refined server structure
    
    * updated todo list
    
    * added action test
    
    * updated todo list
    
    * updated api interaction
    
    * removed todo list
    
    * refactored the API versions in better structure

---
+++ src/applications/check-in/api/versions/v0.js
@@ -0,0 +1,48 @@
+import { apiRequest } from 'platform/utilities/api';
+import environment from 'platform/utilities/environment';
+import { makeApiCall } from '../../utils/api';
+
+const validateToken = async token => {
+  const url = '/check_in/v0/patient_check_ins/';
+  const json = await makeApiCall(
+    apiRequest(`${environment.API_URL}${url}${token}`),
+    'lorota-token-validation',
+    token,
+  );
+  return {
+    data: json.data,
+  };
+};
+
+const checkInUser = async ({ token }) => {
+  const url = '/check_in/v0/patient_check_ins/';
+  const headers = { 'Content-Type': 'application/json' };
+  const data = {
+    patientCheckIns: {
+      id: token,
+    },
+  };
+  const body = JSON.stringify(data);
+  const settings = {
+    headers,
+    body,
+    method: 'POST',
+    mode: 'cors',
+  };
+
+  const json = await makeApiCall(
+    apiRequest(`${environment.API_URL}${url}`, settings),
+    'check-in-user',
+    token,
+  );
+  return {
+    ...json,
+  };
+};
+
+const v0 = {
+  validateToken,
+  checkInUser,
+};
+
+export { v0 };
