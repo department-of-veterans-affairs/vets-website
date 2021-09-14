1474a92e543f0bfae91336cc4be0fba2fa2106f8
Kelson Adams <kelsonic@users.noreply.github.com>
2021-09-14 10:04:58 -0600

    Kelsonic 28848 contact us widget (#18689)
    * Organize imports
    
    * Organize
    
    * Create ask-va react widget
    
    * Remove unused isLOA2 selector
    
    * Add hidden prop
    
    * Update src/applications/static-pages/ask-va/components/App/index.unit.spec.js
    
    Co-authored-by: Ryan Leahy <Rleahy22@users.noreply.github.com>
    
    Co-authored-by: Kelson Adams <kadams@governmentcio.com>
    Co-authored-by: Ryan Leahy <Rleahy22@users.noreply.github.com>

--- src/applications/static-pages/static-pages-entry.js
+++ src/applications/static-pages/static-pages-entry.js
@@ -1,93 +1,79 @@
-import 'platform/polyfills';
+// Node modules.
 import LazyLoad from 'vanilla-lazyload/dist/lazyload';
 import * as Sentry from '@sentry/browser';
-
-import createCommonStore from 'platform/startup/store';
-import startSitewideComponents from 'platform/site-wide';
-import { VA_FORM_IDS } from 'platform/forms/constants';
-
+// Relative imports.
 import './analytics';
+import './sass/static-pages.scss';
+import 'platform/polyfills';
 import alertsBuildShow from './widget-creators/alerts-dismiss-view';
-import icsCreate from './widget-creators/ics-generator';
-import createFacilityPage from './facilities/createFacilityPage';
-import createVetCentersHours from './facilities/createVetCentersHours';
-import createNearByVetCenters from './facilities/vet-center/createNearByVetCenters';
-import createExpandableOperatingStatus from './facilities/vet-center/createExpandableOperatingStatus';
-
-import widgetTypes from './widgetTypes';
-import subscribeAdditionalInfoEvents from './subscription-creators/subscribeAdditionalInfoEvents';
-import subscribeAccordionEvents from './subscription-creators/subscribeAccordionEvents';
-import createApplicationStatus from './widget-creators/createApplicationStatus';
-import createCallToActionWidget from './widget-creators/createCallToActionWidget';
-import createMyVALoginWidget from './widget-creators/createMyVALoginWidget';
-import createDisabilityFormWizard from '../disability-benefits/wizard/createWizard';
-import createDisabilityRatingCalculator from '../disability-benefits/disability-rating-calculator/createCalculator';
-import createEducationApplicationStatus from '../edu-benefits/components/createEducationApplicationStatus';
-import createOptOutApplicationStatus from '../edu-benefits/components/createOptOutApplicationStatus';
-import createFindVaForms, {
-  findVaFormsWidgetReducer,
-} from '../find-forms/createFindVaForms';
-import createFindVaFormsInvalidPdfAlert from '../find-forms/widgets/createInvalidPdfAlert';
-import createHigherLevelReviewApplicationStatus from 'applications/disability-benefits/996/components/createHLRApplicationStatus';
-import createPost911GiBillStatusWidget, {
-  post911GIBillStatusReducer,
-} from '../post-911-gib-status/createPost911GiBillStatusWidget';
-
 import form686CTA from './view-modify-dependent/686-cta/form686CTA';
-
+import icsCreate from './widget-creators/ics-generator';
+import openShareLink from './widget-creators/social-share-links';
+import startSitewideComponents from 'platform/site-wide';
+import subscribeAccordionEvents from './subscription-creators/subscribeAccordionEvents';
+import subscribeAdditionalInfoEvents from './subscription-creators/subscribeAdditionalInfoEvents';
+import widgetTypes from './widgetTypes';
+import { VA_FORM_IDS } from 'platform/forms/constants';
 // Health Care | Manage Benefits widgets.
 import createGetMedicalRecordsPage from './health-care-manage-benefits/get-medical-records-page';
 import createRefillTrackPrescriptionsPage from './health-care-manage-benefits/refill-track-prescriptions-page';
 import createScheduleViewVAAppointmentsPage from './health-care-manage-benefits/schedule-view-va-appointments-page';
 import createSecureMessagingPage from './health-care-manage-benefits/secure-messaging-page';
 import createViewTestAndLabResultsPage from './health-care-manage-benefits/view-test-and-lab-results-page';
-
-// No-react styles.
-import './sass/static-pages.scss';
-
-// Social share links behavior
-import openShareLink from './widget-creators/social-share-links';
-
-// Resources and support widgets
-import createResourcesAndSupportSearchWidget from './widget-creators/resources-and-support-search';
-
-// Health care facility widgets
-import createFacilityListWidget from './facilities/facilityList';
+// Health care facility widgets.
 import createBasicFacilityListWidget from './facilities/basicFacilityList';
-import facilityReducer from './facilities/reducers';
-import createOtherFacilityListWidget from './facilities/otherFacilityList';
-import createChapter36CTA from './vre-chapter36/createChapter36CTA';
 import createChapter31CTA from './vre-chapter31/createChapter31CTA';
+import createChapter36CTA from './vre-chapter36/createChapter36CTA';
+import createFacilityListWidget from './facilities/facilityList';
+import createI18Select from './i18Select/createI18Select';
+import createOtherFacilityListWidget from './facilities/otherFacilityList';
 import createViewDependentsCTA from './view-modify-dependents/view-dependents-cta/createViewDependentsCTA';
 import createViewPaymentHistoryCTA from './view-payment-history/createViewPaymentHistoryCTA';
-import createI18Select from './i18Select/createI18Select';
-
-// School resources widgets
+import facilityReducer from './facilities/reducers';
+// Other widgets.
+import createAskVAWidget from './ask-va';
+import createApplicationStatus from './widget-creators/createApplicationStatus';
+import createCOEAccess from './coe-access/createCOEAccess';
+import createCallToActionWidget from './widget-creators/createCallToActionWidget';
+import createCommonStore from 'platform/startup/store';
+import createCoronavirusChatbot from '../coronavirus-chatbot/createCoronavirusChatbot';
+import createCovidVaccineUpdatesWidget from './covid-vaccine-updates-cta/createCovidVaccineUpdatesWidget';
+import createDependencyVerification from './dependency-verification/createDependencyVerification';
+import createDisabilityFormWizard from '../disability-benefits/wizard/createWizard';
+import createDisabilityRatingCalculator from '../disability-benefits/disability-rating-calculator/createCalculator';
+import createEducationApplicationStatus from '../edu-benefits/components/createEducationApplicationStatus';
+import createExpandableOperatingStatus from './facilities/vet-center/createExpandableOperatingStatus';
+import createFacilityPage from './facilities/createFacilityPage';
+import createFindVaForms, {
+  findVaFormsWidgetReducer,
+} from '../find-forms/createFindVaForms';
+import createFindVaFormsInvalidPdfAlert from '../find-forms/widgets/createInvalidPdfAlert';
+import createHigherLevelReviewApplicationStatus from 'applications/disability-benefits/996/components/createHLRApplicationStatus';
+import createManageVADebtCTA from './manage-va-debt/createManageVADebtCTA';
+import createMyVALoginWidget from './widget-creators/createMyVALoginWidget';
+import createNearByVetCenters from './facilities/vet-center/createNearByVetCenters';
+import createOptOutApplicationStatus from '../edu-benefits/components/createOptOutApplicationStatus';
+import createPost911GiBillStatusWidget, {
+  post911GIBillStatusReducer,
+} from '../post-911-gib-status/createPost911GiBillStatusWidget';
+import createResourcesAndSupportSearchWidget from './widget-creators/resources-and-support-search';
+import createThirdPartyApps, {
+  thirdPartyAppsReducer,
+} from '../third-party-app-directory/createThirdPartyApps';
+import createVetCentersHours from './facilities/createVetCentersHours';
+import dependencyVerificationReducer from './dependency-verification/reducers/index';
 import {
   createScoEventsWidget,
   createScoAnnouncementsWidget,
 } from './school-resources/SchoolResources';
-import createCoronavirusChatbot from '../coronavirus-chatbot/createCoronavirusChatbot';
-import createCovidVaccineUpdatesWidget from './covid-vaccine-updates-cta/createCovidVaccineUpdatesWidget';
-
-import createThirdPartyApps, {
-  thirdPartyAppsReducer,
-} from '../third-party-app-directory/createThirdPartyApps';
-
-import createDependencyVerification from './dependency-verification/createDependencyVerification';
-import dependencyVerificationReducer from './dependency-verification/reducers/index';
-
-import createCOEAccess from './coe-access/createCOEAccess';
-
-// Debt Resolution | Manage VA Debt Widgets
-import createManageVADebtCTA from './manage-va-debt/createManageVADebtCTA';
 
 // Set the app name header when using the apiRequest helper
 window.appName = 'static-pages';
 
-// Set further errors to have the appropriate source tag
+// Set errors to have the appropriate source tag.
 Sentry.configureScope(scope => scope.setTag('source', 'static-pages'));
 
+// Create the Redux store.
 const store = createCommonStore({
   ...facilityReducer,
   ...findVaFormsWidgetReducer,
@@ -96,19 +82,20 @@ const store = createCommonStore({
   ...dependencyVerificationReducer,
 });
 
+// Add the site-wide source tag to the sentry scope.
 Sentry.withScope(scope => {
   scope.setTag('source', 'site-wide');
   startSitewideComponents(store);
 });
 
+// Before create-widget tasks.
 subscribeAdditionalInfoEvents();
-
 subscribeAccordionEvents();
-
 alertsBuildShow();
 icsCreate();
 openShareLink();
 
+// Create widgets.
 createApplicationStatus(store, {
   formId: VA_FORM_IDS.FORM_21P_527EZ,
   applyHeading: 'How do I apply?',
@@ -117,7 +104,6 @@ createApplicationStatus(store, {
   applyText: 'Apply for Veterans Pension benefits',
   widgetType: widgetTypes.PENSION_APP_STATUS,
 });
-
 createApplicationStatus(store, {
   formId: VA_FORM_IDS.FORM_10_10EZ,
   applyHeading: 'How do I apply?',
@@ -126,18 +112,13 @@ createApplicationStatus(store, {
   applyText: 'Apply for health care benefits',
   widgetType: widgetTypes.HEALTH_CARE_APP_STATUS,
 });
-
 createCallToActionWidget(store, widgetTypes.CTA);
-
 createEducationApplicationStatus(store, widgetTypes.EDUCATION_APP_STATUS);
-
 createOptOutApplicationStatus(store, widgetTypes.OPT_OUT_APP_STATUS);
-
 createHigherLevelReviewApplicationStatus(
   store,
   widgetTypes.HIGHER_LEVEL_REVIEW_APP_STATUS,
 );
-
 createApplicationStatus(store, {
   formId: VA_FORM_IDS.FORM_21P_530,
   applyHeading: 'How do I apply?',
@@ -145,18 +126,15 @@ createApplicationStatus(store, {
   applyText: 'Apply for burial benefits',
   widgetType: widgetTypes.BURIALS_APP_STATUS,
 });
-
 createDisabilityFormWizard(store, widgetTypes.DISABILITY_APP_STATUS);
 createDisabilityRatingCalculator(
   store,
   widgetTypes.DISABILITY_RATING_CALCULATOR,
 );
-
 createResourcesAndSupportSearchWidget(
   store,
   widgetTypes.RESOURCES_AND_SUPPORT_SEARCH,
 );
-
 createVetCentersHours(store);
 createExpandableOperatingStatus(store);
 createNearByVetCenters(store);
@@ -164,13 +142,9 @@ createFacilityListWidget();
 createOtherFacilityListWidget();
 createFacilityPage(store);
 createBasicFacilityListWidget();
-
 createScoEventsWidget();
 createScoAnnouncementsWidget();
-
-// App Directory third party applications widget
 createThirdPartyApps(store, widgetTypes.THIRD_PARTY_APP_DIRECTORY);
-
 createFindVaForms(store, widgetTypes.FIND_VA_FORMS);
 createFindVaFormsInvalidPdfAlert(
   store,
@@ -180,14 +154,11 @@ createPost911GiBillStatusWidget(
   store,
   widgetTypes.POST_911_GI_BILL_STATUS_WIDGET,
 );
-
 createCoronavirusChatbot(store, widgetTypes.CORONAVIRUS_CHATBOT);
 createCovidVaccineUpdatesWidget(store, widgetTypes.COVID_VACCINE_UPDATES_CTA);
-
 createViewDependentsCTA(store, widgetTypes.VIEW_DEPENDENTS_CTA);
 form686CTA(store, widgetTypes.FORM_686_CTA);
-
-// Create Health Care | Manage Benefits widgets.
+createAskVAWidget(store, widgetTypes.ASK_VA);
 createGetMedicalRecordsPage(store, widgetTypes.GET_MEDICAL_RECORDS_PAGE);
 createRefillTrackPrescriptionsPage(
   store,
@@ -202,19 +173,15 @@ createViewTestAndLabResultsPage(
   store,
   widgetTypes.VIEW_TEST_AND_LAB_RESULTS_PAGE,
 );
-
 createChapter36CTA(store, widgetTypes.CHAPTER_36_CTA);
 createChapter31CTA(store, widgetTypes.CHAPTER_31_CTA);
 createViewPaymentHistoryCTA(store, widgetTypes.VIEW_PAYMENT_HISTORY);
 createI18Select(store, widgetTypes.I_18_SELECT);
-
 createDependencyVerification(store, widgetTypes.DEPENDENCY_VERIFICATION);
-
 createCOEAccess(store, widgetTypes.COE_ACCESS);
-
 createManageVADebtCTA(store, widgetTypes.MANAGE_VA_DEBT_CTA);
 
-// homepage widgets
+// Create the My VA Login widget only on the homepage.
 if (location.pathname === '/') {
   createMyVALoginWidget(store);
 }
