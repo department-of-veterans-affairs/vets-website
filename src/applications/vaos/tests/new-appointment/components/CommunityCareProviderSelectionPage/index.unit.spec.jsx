073911cfa121b9264a1d77217553077f136272c4
Jennie McGibney <jennie.mcgibney@va.gov>
2021-09-14 10:29:23 -0400

    unit test

--- src/applications/vaos/tests/new-appointment/components/CommunityCareProviderSelectionPage/index.unit.spec.jsx
+++ src/applications/vaos/tests/new-appointment/components/CommunityCareProviderSelectionPage/index.unit.spec.jsx
@@ -680,6 +680,7 @@ describe('VAOS <CommunityCareProviderSelectionPage>', () => {
   it('should not display closest city question when using iterations toggle', async () => {
     // Given a user with two supported sites
     // And the CC iterations toggle is on
+    // And type of care is selected
     const store = await setCommunityCareFlow({
       toggles: {
         vaOnlineSchedulingCCIterations: true,
@@ -689,6 +690,7 @@ describe('VAOS <CommunityCareProviderSelectionPage>', () => {
         { id: '984', address: { city: 'Belgrade', state: 'MT' } },
       ],
     });
+    await setTypeOfCare(store, /primary care/i);
 
     // When the page is displayed
     const screen = renderWithStoreAndRouter(
@@ -697,9 +699,18 @@ describe('VAOS <CommunityCareProviderSelectionPage>', () => {
         store,
       },
     );
-    await screen.findByText(/We’ll call you to confirm your provider choice/);
 
-    // Then the closest city/state question is not shown
+    // Then the heading will display type of provider requested
+    expect(
+      await screen.findByRole('heading', {
+        level: 1,
+        name: /Request a Primary care provider/i,
+      }),
+    ).to.be.ok;
+    expect(screen.getByText(/We’ll call you to confirm your provider choice/))
+      .to.be.ok;
+
+    // And the closest city/state question is not shown
     expect(screen.queryByLabelText('Bozeman, MT')).not.to.exist;
     expect(screen.queryByLabelText('Belgrade, MT')).not.to.exist;
     expect(screen.queryByText(/closest city and state/i)).not.to.exist;
