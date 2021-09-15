7abc201d2171c045b5699c6aac891ee5d399a574
Cesar Santiago <cesaresantiago@users.noreply.github.com>
2021-09-15 16:52:18 -0400

    Give unique formId to the My Education Benefits App (#18716)
    * - The formId in our app has the same formId that the current 1990 app. This results in the autosave data being associated with our app and also with the current 1990 app. The formId must be unique so the saved form data is unique to our app.
    
    * - Change formId to match other ids in vets-json-schema
    
    * - Push update to latest json schema
    
    * - Push update to latest json schema with fix for missing dist file.

--- src/applications/my-education-benefits/config/form.js
+++ src/applications/my-education-benefits/config/form.js
@@ -267,13 +267,15 @@ const formConfig = {
   trackingPrefix: 'my-education-benefits-',
   introduction: IntroductionPage,
   confirmation: ConfirmationPage,
-  formId: '22-1990',
+  formId: '22-1990-MEB',
   saveInProgress: {
-    // messages: {
-    //   inProgress: 'Your my education benefits application (22-1990) is in progress.',
-    //   expired: 'Your saved my education benefits application (22-1990) has expired. If you want to apply for my education benefits, please start a new application.',
-    //   saved: 'Your my education benefits application has been saved.',
-    // },
+    messages: {
+      inProgress:
+        'Your my education benefits application (22-1990) is in progress.',
+      expired:
+        'Your saved my education benefits application (22-1990) has expired. If you want to apply for my education benefits, please start a new application.',
+      saved: 'Your my education benefits application has been saved.',
+    },
   },
   version: 0,
   prefillEnabled: true,
