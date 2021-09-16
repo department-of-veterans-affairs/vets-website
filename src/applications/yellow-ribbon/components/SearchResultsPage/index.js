82b5b1994fb8f3fe33ce56091e59639589002b0c
zacharymorel <zmorel@governmentcio.com>
2021-09-16 14:28:56 -0400

    refactor placement of nodes

--- src/applications/yellow-ribbon/components/SearchResultsPage/index.js
+++ src/applications/yellow-ribbon/components/SearchResultsPage/index.js
@@ -59,9 +59,34 @@ export const SearchResultsPage = ({
         <SearchForm />
       </div>
 
-      {/* Search Results */}
       <div className="vads-l-col--12 vads-u-padding-left--0 medium-screen:vads-l-col--9 medium-screen:vads-u-padding-left--5">
+        {/* Search Results */}
         <SearchResults />
+
+        {/* Helpful Links */}
+        <div
+          className="vads-u-display--none medium-screen:vads-u-display--flex vads-u-flex-direction--column vads-u-padding-x--2 vads-u-padding-y--2 vads-u-background-color--gray-light-alt vads-u-margin-top--2p5"
+          data-e2e-id="yellow-ribbon--helpful-links"
+        >
+          <h3 className="vads-u-margin--0 vads-u-padding-bottom--1p5 vads-u-border-bottom--1px vads-u-border-color--gray-light">
+            Helpful links
+          </h3>
+          <p className="vads-u-margin-bottom--1 vads-u-font-weight--bold">
+            <a href="/education/eligibility/">
+              Find out if you’re eligible for the Post-9/11 GI Bill
+            </a>
+          </p>
+          <p className="vads-u-margin-bottom--1 vads-u-margin-top--1 vads-u-font-weight--bold">
+            <a href="/education/about-gi-bill-benefits/post-9-11/yellow-ribbon-program/">
+              Find out if you qualify for the Yellow Ribbon Program
+            </a>
+          </p>
+          <p className="vads-u-margin-top--1 vads-u-font-weight--bold">
+            <a href="/education/how-to-apply/">
+              Apply for Post-9/11 GI Bill benefits
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   </>
