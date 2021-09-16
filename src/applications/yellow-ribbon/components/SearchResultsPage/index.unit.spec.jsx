baf659a08c3dd608cc2c9c4783ff194cb1e88934
Zachary Morel <zacharyjmorel@gmail.com>
2021-09-16 15:20:08 -0400

    DT-9374 Yellow Ribbon Helpful links update (#18747)
    * Moved Helpful Links over to new spot under results for Screen reader order
    
    * Updating e2e and cypress tests
    
    * refactor placement of nodes
    
    * Moving tests to new file over

--- src/applications/yellow-ribbon/components/SearchResultsPage/index.unit.spec.jsx
+++ src/applications/yellow-ribbon/components/SearchResultsPage/index.unit.spec.jsx
@@ -12,6 +12,13 @@ describe('Search Results Page <SearchResultsPage>', () => {
 
     // Expect there to be:
     expect(text).to.include('Yellow Ribbon school search results');
+    expect(tree.text()).to.include('Helpful links');
+    expect(
+      tree.find('[data-e2e-id="yellow-ribbon--helpful-links"]'),
+    ).to.have.lengthOf(1);
+    expect(
+      tree.find('[data-e2e-id="yellow-ribbon--helpful-links"] a'),
+    ).to.have.lengthOf(3);
 
     // Expect there NOT to be:
     expect(text).to.not.include('Find a Yellow Ribbon school');
