82251eb2b572ff9b3f0de6caf7164413d5e20d26
Craig Wheeler <craig@craigwheeler.net>
2021-09-16 11:02:50 -0400

    29874: deductionCode is undefined (#18742)

--- src/applications/financial-status-report/components/ResolutionDebtCards.jsx
+++ src/applications/financial-status-report/components/ResolutionDebtCards.jsx
@@ -4,19 +4,13 @@ import RadioButtons from '@department-of-veterans-affairs/component-library/Radi
 import TextInput from '@department-of-veterans-affairs/component-library/TextInput';
 import ExpandingGroup from '@department-of-veterans-affairs/component-library/ExpandingGroup';
 import Checkbox from '@department-of-veterans-affairs/component-library/Checkbox';
+import { setData } from 'platform/forms-system/src/js/actions';
+import { deductionCodes } from '../../debt-letters/const/deduction-codes';
+import { currency } from '../utils/helpers';
 import Telephone, {
   CONTACTS,
   PATTERNS,
 } from '@department-of-veterans-affairs/component-library/Telephone';
-
-import { setData } from 'platform/forms-system/src/js/actions';
-import { deductionCodes } from '../../debt-letters/const/deduction-codes';
-
-const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 2,
-});
 
 const ExpandedContent = ({
   index,
@@ -130,7 +124,7 @@ const ResolutionDebtCards = ({
         const radioError = submitted && !debt.resolution?.resolutionType;
         const title = deductionCodes[debt.deductionCode] || debt.benefitType;
         const subTitle =
-          debt.currentAr && formatter.format(parseFloat(debt.currentAr));
+          debt.currentAr && currency.format(parseFloat(debt.currentAr));
 
         return (
           <div
