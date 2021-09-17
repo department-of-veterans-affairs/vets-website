4e4c40ec72c25f3970cc03a8a47a28912c18d051
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2021-09-17 13:59:58 -0400

    2 passing tests!

--- src/applications/static-pages/tests/facilities/NearbyVetCenters.unit.spec.jsx
+++ src/applications/static-pages/tests/facilities/NearbyVetCenters.unit.spec.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
 import NearbyVetCenters from '../../facilities/vet-center/NearByVetCenters';
@@ -6,18 +7,37 @@ import NearbyVetCenters from '../../facilities/vet-center/NearByVetCenters';
 const createFakeStore = state => {
   return {
     getState: () => state,
+    subscribe: () => {},
+    dispatch: () => {},
   };
 };
 
 describe('NearbyVetCenters', () => {
   it('should render spinner while loading', () => {
     const state = {
-      facilitiesLoading: true,
+      facility: { loading: true },
     };
     const fakeStore = createFakeStore(state);
-    const wrapper = mount(<NearbyVetCenters store={fakeStore} />);
-    // console.log(wrapper.debug());
+    const wrapper = mount(
+      <Provider store={fakeStore}>
+        <NearbyVetCenters />
+      </Provider>,
+    );
     expect(wrapper.find('LoadingIndicator').exists()).to.be.true;
+    wrapper.unmount();
+  });
+
+  it('should not render spinner if not loading', () => {
+    const state = {
+      facility: { loading: false },
+    };
+    const fakeStore = createFakeStore(state);
+    const wrapper = mount(
+      <Provider store={fakeStore}>
+        <NearbyVetCenters />
+      </Provider>,
+    );
+    expect(wrapper.find('LoadingIndicator').exists()).to.be.false;
     wrapper.unmount();
   });
 });
