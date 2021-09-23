aa9db7732b1bf6a122945902569de261b503e34b
Patrick <patrickryll@yahoo.com>
2021-09-23 15:32:48 -0400

    split gender and  marital info in separate pages

--- src/applications/hca/components/DependentView.jsx
+++ src/applications/hca/components/DependentView.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 export default function DependentView({ formData }) {
   return (
     <div>
+      gggg
       <strong>
         {formData.fullName.first} {formData.fullName.last}
       </strong>
+      llll
       <br />
       {formData.dependentRelation}
     </div>
