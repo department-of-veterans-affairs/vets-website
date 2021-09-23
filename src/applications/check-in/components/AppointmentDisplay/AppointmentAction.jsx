4e0d215c37d31fb6b5418c667c5b44ab32630bf0
Mark Dewey <mdewey@governmentcio.com>
2021-09-23 12:47:52 -0400

    added tests for appointment action

--- src/applications/check-in/components/AppointmentDisplay/AppointmentAction.jsx
+++ src/applications/check-in/components/AppointmentDisplay/AppointmentAction.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import { api } from '../../api';
 
 import { goToNextPage, URLS } from '../../utils/navigation';
-
+import { STATUSES, areEqual } from '../../utils/appointment/status';
 import recordEvent from 'platform/monitoring/record-event';
+import format from 'date-fns/format';
 
 export default function AppointmentAction(props) {
-  const { isLowAuthEnabled, token, router } = props;
+  const { appointment, isLowAuthEnabled, token, router } = props;
 
   const [isCheckingIn, setIsCheckingIn] = useState(false);
 
@@ -35,16 +36,73 @@ export default function AppointmentAction(props) {
     }
   };
 
-  return (
-    <button
-      type="button"
-      className="usa-button usa-button-big vads-u-font-size--md"
-      onClick={onClick}
-      data-testid="check-in-button"
-      disabled={isCheckingIn}
-      aria-label="Check in now for your appointment"
-    >
-      {isCheckingIn ? <>Loading...</> : <>Check in now</>}
-    </button>
-  );
+  if (appointment.status) {
+    if (areEqual(appointment.status, STATUSES.ELIGIBLE)) {
+      return (
+        <button
+          type="button"
+          className="usa-button usa-button-big vads-u-font-size--md"
+          onClick={onClick}
+          data-testid="check-in-button"
+          disabled={isCheckingIn}
+          aria-label="Check in now for your appointment"
+        >
+          {isCheckingIn ? <>Loading...</> : <>Check in now</>}
+        </button>
+      );
+    } else if (areEqual(appointment.status, STATUSES.INELIGIBLE_BAD_STATUS)) {
+      return (
+        <p data-testid="ineligible-bad-status-message">
+          This appointment isn’t eligible for online check-in. Check-in with a
+          staff member.
+        </p>
+      );
+    } else if (areEqual(appointment.status, STATUSES.INELIGIBLE_TOO_EARLY)) {
+      const appointmentDateTime = new Date(appointment.startTime);
+      const appointmentTime = format(appointmentDateTime, 'h:mm aaaa');
+      return (
+        <p data-testid="too-early-message">
+          You can check in starting at this time: {appointmentTime}
+        </p>
+      );
+    } else if (areEqual(appointment.status, STATUSES.INELIGIBLE_TOO_LATE)) {
+      return (
+        <p data-testid="too-late-message">
+          Your appointment started more than 10 minutes ago. We can't check you
+          in online. Ask a staff member for help.
+        </p>
+      );
+    } else if (
+      areEqual(appointment.status, STATUSES.INELIGIBLE_UNSUPPORTED_LOCATION)
+    ) {
+      return (
+        <p data-testid="unsupported-location-message">
+          This appointment isn’t eligible for online check-in. Check-in with a
+          staff member.
+        </p>
+      );
+    } else if (
+      areEqual(appointment.status, STATUSES.INELIGIBLE_UNKNOWN_REASON)
+    ) {
+      return (
+        <p data-testid="unknown-reason-message">
+          This appointment isn’t eligible for online check-in. Check-in with a
+          staff member.
+        </p>
+      );
+    }
+  } else {
+    return (
+      <button
+        type="button"
+        className="usa-button usa-button-big vads-u-font-size--md"
+        onClick={onClick}
+        data-testid="check-in-button"
+        disabled={isCheckingIn}
+        aria-label="Check in now for your appointment"
+      >
+        {isCheckingIn ? <>Loading...</> : <>Check in now</>}
+      </button>
+    );
+  }
 }
