5495cc3c1118a19b9ade27b7e0694d1cd9b97e41
Craig Wheeler <craig@craigwheeler.net>
2021-09-23 13:19:11 -0400

    handle multiple facilities

--- src/applications/medical-copays/components/Balances.jsx
+++ src/applications/medical-copays/components/Balances.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import BalanceCard from './BalanceCard';
 
-export const Balances = ({ statementData }) => (
-  <>
+export const Balances = ({ statementData }) => {
+  const single = <h2>What you owe to your facility</h2>;
+  const multiple = (
     <h2>What you owe to your {statementData?.length} facilities</h2>
-    {statementData?.map(balance => (
-      <BalanceCard
-        id={balance.id}
-        key={balance.id}
-        amount={balance.pHAmtDue}
-        facility={balance.station.facilitYDesc}
-        city={balance.station.city}
-        dueDate={balance.pSStatementDate}
-      />
-    ))}
-  </>
-);
+  );
+
+  return (
+    <>
+      {statementData?.length === 1 ? single : multiple}
+      {statementData?.map(balance => (
+        <BalanceCard
+          id={balance.id}
+          key={balance.id}
+          amount={balance.pHAmtDue}
+          facility={balance.station.facilitYDesc}
+          city={balance.station.city}
+          dueDate={balance.pSStatementDate}
+        />
+      ))}
+    </>
+  );
+};
 
 export default Balances;
