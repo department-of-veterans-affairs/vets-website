461625bf2c44e07d230b91d4cdf1fb4e323d1409
Micah Chiang <micahkchiang@gmail.com>
2021-09-23 16:10:35 -0500

    dynamic check list

---
+++ src/applications/lgy/coe/config/chapters/documents/UploadRequirements.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+// Abbreviation keys:
+// ADSM - Active Duty Service Member
+// NADNA - Non Active Duty Never Activated
+// DNANA - Discharged National Guard Never Activated
+// DRNA - Discharged Reserves Never Activaed
+const DOCUMENT_REQUIREMENTS = {
+  VETERAN: ['A copy of your discharge or separation papers (DD214)'],
+  ADSM: ['Statement of Service'],
+  NADNA: [
+    'Statement of Service AND',
+    'Creditable number of years served or',
+    'Retirement Points Statement',
+  ],
+  DNANA: [
+    'Separation and Report of Service (NGB Form 22) for each period of National Guard service AND',
+    'Retirement Points Statement (NGB Form 23) AND',
+    'Proof of character of service',
+  ],
+  DRNA: ['Latest annual retirement points AND', 'Proof of honorable service'],
+};
+
+const UploadRequirements = ({ formData }) => {
+  const { identity } = formData;
+
+  return (
+    <div>
+      <h2 className="vads-u-font-size--h4">Upload your documents</h2>
+      {identity && (
+        <p className="vads-u-font-weight--bold">Please upload the following</p>
+      )}
+      <ul>
+        {identity &&
+          DOCUMENT_REQUIREMENTS[identity].map((req, index) => (
+            <li key={index}>{req}</li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  formData: state?.form?.data,
+});
+
+export default connect(
+  mapStateToProps,
+  null,
+)(UploadRequirements);
--- src/applications/lgy/coe/config/chapters/documents/fileUpload.js
+++ src/applications/lgy/coe/config/chapters/documents/fileUpload.js
@@ -2,65 +2,81 @@ import React from 'react';
 import environment from 'platform/utilities/environment';
 import fileUploadUI from 'platform/forms-system/src/js/definitions/file';
 import { documentUpload } from '../../schemaImports';
+import UploadRequirements from './UploadRequirements';
 
 const MAX_SIZE = 50 * 1024 * 1024;
 const MAX_PDF_SIZE = 150 * 1024 * 1024;
 
-const FileUploadDescription = (
-  <div>
-    <p>
-      You can upload your document in a .pdf, .jpg, .jpeg, .png, .gif, .bmp, or
-      .txt file format. You’ll first need to scan a copy of your document onto
-      your computer or mobile phone. You can then upload the document from
-      there.
-    </p>
-    <p>Guidelines for uploading a file:</p>
-    <ul>
-      <li>
-        File types you can upload: .pdf, .jpg, .jpeg, .png, .gif, .bmp, or .txt
-      </li>
-      <li>Maximum non-PDF file size: 50MB</li>
-      <li>Maximum PDF file size: 150MB</li>
-    </ul>
-    <p>
-      A 1MB file equals about 500 pages of text. A photo or scan is usually
-      about 6MB. Large files can take longer to upload with a slow internet
-      connection.
-    </p>
-  </div>
-);
+const DocumentUploadDescription = () => {
+  return (
+    <div>
+      <p>
+        You can upload your document in a .pdf, .jpg, .jpeg, .png, .gif, .bmp,
+        or .txt file format. You’ll first need to scan a copy of your document
+        onto your computer or mobile phone. You can then upload the document
+        from there.
+      </p>
+      <p>Guidelines for uploading a file:</p>
+      <ul>
+        <li>
+          File types you can upload: .pdf, .jpg, .jpeg, .png, .gif, .bmp, or
+          .txt
+        </li>
+        <li>Maximum non-PDF file size: 50MB</li>
+        <li>Maximum PDF file size: 150MB</li>
+      </ul>
+      <p>
+        A 1MB file equals about 500 pages of text. A photo or scan is usually
+        about 6MB. Large files can take longer to upload with a slow internet
+        connection.
+      </p>
+    </div>
+  );
+};
 
 export const schema = {
   type: 'object',
   properties: {
-    'view:fileUploadDescription': {
+    'view:documentRequirements': {
       type: 'object',
       properties: {},
     },
     ...documentUpload.properties,
+    'view:documentUploadDescription': {
+      type: 'object',
+      properties: {},
+    },
   },
 };
 
 export const uiSchema = {
-  'view:fileUploadDescription': {
-    'ui:title': 'Upload service documentation',
-    'ui:description': FileUploadDescription,
+  'view:documentRequirements': {
+    'ui:field': UploadRequirements,
     'ui:options': {
       classNames: 'schemaform-block-override',
     },
   },
   fileType: {
     'ui:title': 'What kind of document are you uploading?',
-    'ui:required': formData => formData?.willUploadDocs,
+    'ui:required': () => true,
   },
   files: {
-    'ui:required': formData => formData?.willUploadDocs,
-    ...fileUploadUI('Uploaded files', {
+    'ui:required': () => true,
+    ...fileUploadUI('Your uploaded documents', {
       buttonText: 'Upload a document',
+      hideLabelText: true,
       fileUploadUrl: `${environment.API_URL}/v0/claim_attachments`,
       fileTypes: ['pdf', 'jpg', 'jpeg', 'png', 'gif', 'bmp', 'txt'],
       maxSize: MAX_SIZE,
       maxPdfSize: MAX_PDF_SIZE,
     }),
+  },
+  'view:documentUploadDescription': {
+    'ui:title': () => (
+      <legend className="schemaform-block-title">
+        Having problems uploading your documents?
+      </legend>
+    ),
+    'ui:description': DocumentUploadDescription,
   },
 };
