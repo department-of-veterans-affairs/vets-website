e947cba41f8e76aa5e478e61cdb2549f24248983
zacharymorel <zmorel@governmentcio.com>
2021-09-27 10:09:23 -0400

    removing unused, old feature code

--- src/applications/find-forms/components/SearchResult.jsx
+++ src/applications/find-forms/components/SearchResult.jsx
@@ -121,14 +121,7 @@ const deriveRelatedTo = ({
   return null;
 };
 
-const SearchResult = ({
-  currentPosition,
-  form,
-  formMetaInfo,
-  showPDFInfoVersionOne,
-  showPDFInfoVersionTwo,
-  showPDFInfoVersionThree,
-}) => {
+const SearchResult = ({ form, formMetaInfo, showPDFInfoVersionThree }) => {
   // Escape early if we don't have the necessary form attributes.
   if (!form?.attributes) {
     return null;
@@ -169,13 +162,11 @@ const SearchResult = ({
   return (
     <li>
       <FormTitle
-        currentPosition={currentPosition}
         id={id}
         formUrl={formDetailsUrl}
         lang={language}
         title={title}
         recordGAEvent={recordGAEvent}
-        showPDFInfoVersionTwo={showPDFInfoVersionTwo}
       />
       <div className="vads-u-margin-y--1 vsa-from-last-updated">
         <span className="vads-u-font-weight--bold">Form last updated:</span>{' '}
@@ -210,35 +201,6 @@ const SearchResult = ({
           </a>
         </div>
       ) : null}
-      {showPDFInfoVersionOne && (
-        <div className="find-forms-alert-message vads-u-margin-bottom--2 vads-u-background-color--primary-alt-lightest vads-u-display--flex vads-u-padding-y--4 vads-u-padding-right--7 vads-u-padding-left--3 vads-u-width--full">
-          <i aria-hidden="true" role="img" />
-          <span className="sr-only">Alert: </span>
-          <div>
-            <p className="vads-u-margin-top--0">
-              We recommend that you download PDF forms and open them in Adobe
-              Acrobat Reader
-            </p>
-            <a href="https://www.va.gov/resources/how-to-download-and-open-a-vagov-pdf-form/">
-              Get instructions for opening the form in Acrobat Reader
-            </a>
-          </div>
-        </div>
-      )}
-      {showPDFInfoVersionThree && (
-        <div className="vads-u-margin-bottom--1 vads-u-margin-top--6">
-          <span className="vads-u-margin-top--0 vads-u-margin-right--0p5 vads-u-color--gray-medium">
-            You’ll need to download this form and open it in Adobe Acrobat
-            Reader.
-          </span>
-          <a
-            className="vads-u-display--inline "
-            href="https://www.va.gov/resources/how-to-download-and-open-a-vagov-pdf-form/"
-          >
-            Read More
-          </a>
-        </div>
-      )}
       <div className="vads-u-margin-bottom--5">
         <a
           className="find-forms-max-content vads-u-text-decoration--none"
@@ -265,12 +227,9 @@ const SearchResult = ({
 };
 
 SearchResult.propTypes = {
-  currentPosition: PropTypes.number,
   form: customPropTypes.Form.isRequired,
   formMetaInfo: customPropTypes.FormMetaInfo,
   showPDFInfoVersionOne: PropTypes.bool,
-  showPDFInfoVersionTwo: PropTypes.bool,
-  showPDFInfoVersionThree: PropTypes.bool,
 };
 
 export default SearchResult;
