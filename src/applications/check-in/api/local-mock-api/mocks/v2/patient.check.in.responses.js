2b912655818d8fcb57b07afcc286836a7ab5e2be
Mark Dewey <mdewey@governmentcio.com>
2021-09-30 14:16:34 -0400

    updated the mock response

--- src/applications/check-in/api/local-mock-api/mocks/v2/patient.check.in.responses.js
+++ src/applications/check-in/api/local-mock-api/mocks/v2/patient.check.in.responses.js
@@ -1,20 +1,22 @@
 const createMockSuccessResponse = (data, hasBeenValidated) => {
   const rv = {
     id: data.id || '46bebc0a-b99c-464f-a5c5-560bc9eae287',
-    payload: [
-      {
-        facility: 'LOMA LINDA VA CLINIC',
-        clinicPhoneNumber: '5551234567',
-        clinicFriendlyName: 'TEST CLINIC',
-        clinicName: 'LOM ACC CLINIC TEST',
-        appointmentIEN: 'some-ien',
-      },
-    ],
+    payload: {
+      appointments: [
+        {
+          facility: 'LOMA LINDA VA CLINIC',
+          clinicPhoneNumber: '5551234567',
+          clinicFriendlyName: 'TEST CLINIC',
+          clinicName: 'LOM ACC CLINIC TEST',
+          appointmentIEN: 'some-ien',
+        },
+      ],
+    },
   };
   if (hasBeenValidated) {
-    rv.payload[0].startTime = '2021-08-19T13:56:31';
-    rv.payload[0].status = 'ELIGIBLE';
-    rv.payload[0].facilityId = 'ABC_123';
+    rv.payload.appointments[0].startTime = '2021-08-19T13:56:31';
+    rv.payload.appointments[0].status = 'ELIGIBLE';
+    rv.payload.appointments[0].facilityId = 'ABC_123';
   }
   return rv;
 };
@@ -51,17 +53,19 @@ const createMultipleAppointments = (
 ) => {
   const rv = {
     id: token || '46bebc0a-b99c-464f-a5c5-560bc9eae287',
-    payload: [
-      createAppointment(
-        'INELIGIBLE_TOO_LATE',
-        'ABC_123',
-        `some-ien-L`,
-        `TEST CLINIC-L`,
-      ),
-    ],
+    payload: {
+      appointments: [
+        createAppointment(
+          'INELIGIBLE_TOO_LATE',
+          'ABC_123',
+          `some-ien-L`,
+          `TEST CLINIC-L`,
+        ),
+      ],
+    },
   };
   for (let i = 0; i < numberOfCheckInAbledAppointments; i++) {
-    rv.payload.push(
+    rv.payload.appointments.push(
       createAppointment(
         'ELIGIBLE',
         'ABC_123',
@@ -70,7 +74,7 @@ const createMultipleAppointments = (
       ),
     );
   }
-  rv.payload.push(
+  rv.payload.appointments.push(
     createAppointment(
       'INELIGIBLE_TOO_EARLY',
       'ABC_123',
