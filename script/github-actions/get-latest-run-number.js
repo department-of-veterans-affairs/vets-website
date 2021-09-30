3416d9fdfe7ce029516f7f52fa183424903860f4
Nick Sprinkle <ndsprinkle@gmail.com>
2021-09-30 14:17:47 -0400

    Remove SHA testing and check run_number via octokit

---
+++ script/github-actions/get-latest-run-number.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+/* eslint-disable camelcase */
+
+const { Octokit } = require('@octokit/rest');
+
+const { GITHUB_TOKEN: auth, GITHUB_REPOSITORY } = process.env;
+const [owner, repo] = GITHUB_REPOSITORY.split('/');
+
+const octokit = new Octokit({ auth });
+
+const params = {
+  owner,
+  repo,
+  workflow_id: 'continuous-integration.yml',
+  branch: 'master',
+};
+
+return octokit.rest.actions
+  .listWorkflowRuns(params)
+  .then(response => {
+    if (response.status !== 200) {
+      throw new Error(
+        `Response ${response.status} from ${response.url}. Aborting.`,
+      );
+    }
+    return response.data;
+  })
+  .then(({ workflow_runs }) => {
+    if (workflow_runs.length === 0) {
+      throw new Error('No workflows found. Aborting.');
+    }
+
+    console.log(
+      `::set-output name=latest_run_number::${workflow_runs[0].run_number}`,
+    );
+
+    return workflow_runs[0].run_number;
+  });
