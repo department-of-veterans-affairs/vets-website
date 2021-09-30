e55fbc5f2c06e346b42742d7d42fc7eef87199c6
AFSMWK2BERWR9I0 <tony.valdez@accenturefederal.com>
2021-09-30 07:53:53 -0600

    Go to your profile link text fix

--- src/applications/my-education-benefits/config/form.js
+++ src/applications/my-education-benefits/config/form.js
@@ -532,9 +532,8 @@ const formConfig = {
                   </p>
                   <p>
                     <a href="/profile/personal-information">
-                      go to your profile page
+                      Go to your profile
                     </a>
-                    .
                   </p>
                 </>
               ),
