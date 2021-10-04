aa764b4351d4b51271dcf6bdf09633fae28478f7
zacharymorel <zmorel@governmentcio.com>
2021-10-04 13:13:34 -0400

    Refactor widgets now that it has expanded

---
+++ src/applications/find-forms/widgets/createFindVaFormsPDFDownloadHelper/DownloadPDFGuidance.js
@@ -0,0 +1,65 @@
+// Dependencies.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+// relative imports
+import DownloadPDFModal from './DownloadPDFModal';
+import InvalidFormDownload from './InvalidFormAlert';
+import { onDownloadLinkClick, sentryLogger } from './index';
+import { showPDFModal } from '../../helpers/selectors';
+
+// DownloadPDFGuidance sole purpose is to render the react widget depending on params (PDF is valid or not)
+const DownloadPDFGuidance = ({
+  downloadUrl,
+  form,
+  formNumber,
+  formPdfIsValid,
+  formPdfUrlIsValid,
+  link,
+  netWorkRequestError,
+  store,
+}) => {
+  const div = document.createElement('div');
+  const parentEl = link.parentNode;
+
+  if (formPdfIsValid && formPdfUrlIsValid && !netWorkRequestError) {
+    // feature flag
+    if (showPDFModal(store.getState())) {
+      ReactDOM.render(
+        <Provider store={store}>
+          <DownloadPDFModal formNumber={formNumber} url={downloadUrl} />
+        </Provider>,
+        div,
+      );
+      parentEl.insertBefore(div, link);
+    } else {
+      // if the Feature Flag for the Modal is turned off
+      link.removeEventListener('click', onDownloadLinkClick);
+      link.click();
+    }
+  } else {
+    let errorMessage = 'Find Forms - Form Detail - invalid PDF accessed';
+
+    if (netWorkRequestError)
+      errorMessage =
+        'Find Forms - Form Detail - onDownloadLinkClick function error';
+
+    if (!formPdfIsValid && !formPdfUrlIsValid)
+      errorMessage =
+        'Find Forms - Form Detail - invalid PDF accessed & invalid PDF link';
+
+    if (formPdfIsValid && !formPdfUrlIsValid)
+      errorMessage = 'Find Forms - Form Detail - invalid PDF link';
+
+    sentryLogger(form, formNumber, downloadUrl, errorMessage);
+
+    const alertBox = <InvalidFormDownload downloadUrl={downloadUrl} />;
+
+    ReactDOM.render(alertBox, div);
+    parentEl.insertBefore(div, link);
+    parentEl.removeChild(link);
+  }
+};
+
+export default DownloadPDFGuidance;
---
+++ src/applications/find-forms/widgets/createFindVaFormsPDFDownloadHelper/DownloadPDFModal.jsx
@@ -0,0 +1,76 @@
+// Dependencies.
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Modal from '@department-of-veterans-affairs/component-library/Modal';
+
+// DownloadPDFModal is state wrapper + modal for PDF guidance upon PDf being valid
+const DownloadPDFModal = ({ formNumber, url }) => {
+  const [modalState, setModalState] = useState({
+    isOpen: true,
+    pdfSelected: formNumber,
+    pdfUrl: url,
+  });
+
+  const toggleModalState = () => setModalState({ isOpen: !modalState.isOpen });
+
+  // modal state variables
+  const { isOpen, pdfSelected, pdfUrl } = modalState;
+  return (
+    <div
+      className="pdf-alert-modal"
+      style={{
+        pointerEvents: 'all',
+      }}
+    >
+      <Modal
+        onClose={() => toggleModalState()}
+        secondaryButton={{
+          action: () => {
+            toggleModalState();
+          },
+          text: 'Close',
+        }}
+        title="Adobe Reader DC Required"
+        visible={isOpen}
+      >
+        <>
+          <p className="vads-u-display--block vads-u-margin-bottom--3">
+            <span>
+              All PDF forms do not function fully in a web browser or other PDF
+              viewer. Please download the form and use Adobe Acrobat Reader DC
+              to fill out. For specific instructions about working with PDFs
+            </span>{' '}
+            <a href="https://www.va.gov/resources/how-to-download-and-open-a-vagov-pdf-form">
+              please read out Resources and Support Article
+            </a>
+          </p>
+          <a
+            className="vads-u-display--block vads-u-margin-bottom--1p5"
+            href="https://get.adobe.com/reader/"
+            rel="noopener noreferrer"
+          >
+            <span>Get Acrobat Reader DC</span>
+          </a>
+          <a
+            href={pdfUrl}
+            className="vads-u-display--block vads-u-margin-bottom--3"
+          >
+            <i
+              aria-hidden="true"
+              className="fas fa-download fa-lg vads-u-margin-right--1"
+              role="presentation"
+            />
+            <span>Download VA Form {pdfSelected}</span>
+          </a>
+        </>
+      </Modal>
+    </div>
+  );
+};
+
+DownloadPDFModal.propTypes = {
+  formNumber: PropTypes.string,
+  url: PropTypes.string,
+};
+
+export default DownloadPDFModal;
