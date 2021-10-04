bc6c88b91037871396ac8b1814f2ff505e0bc8fc
Mark Dewey <mdewey@governmentcio.com>
2021-10-04 15:23:14 -0400

    created new flip generator

--- src/applications/check-in/api/local-mock-api/mocks/feature.toggles.js
+++ src/applications/check-in/api/local-mock-api/mocks/feature.toggles.js
@@ -29,4 +29,37 @@ const createFeatureToggles = (
   };
 };
 
-module.exports = { createFeatureToggles };
+const generateFeatureToggles = toggles => {
+  const {
+    checkInExperienceEnabled = true,
+    checkInExperienceLowAuthenticationEnabled = true,
+    checkInExperienceMultipleAppointmentSupport = true,
+    checkInExperienceUpdateInformationPageEnabled = true,
+  } = toggles;
+
+  return {
+    data: {
+      type: 'feature_toggles',
+      features: [
+        {
+          name: 'check_in_experience_enabled',
+          value: checkInExperienceEnabled,
+        },
+        {
+          name: 'check_in_experience_update_information_page_enabled',
+          value: checkInExperienceUpdateInformationPageEnabled,
+        },
+        {
+          name: 'check_in_experience_low_authentication_enabled',
+          value: checkInExperienceLowAuthenticationEnabled,
+        },
+        {
+          name: 'check_in_experience_multiple_appointment_support',
+          value: checkInExperienceMultipleAppointmentSupport,
+        },
+      ],
+    },
+  };
+};
+
+module.exports = { createFeatureToggles, generateFeatureToggles };
