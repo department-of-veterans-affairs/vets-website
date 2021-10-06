7978cad76bbc2809af2fb84328f26b1a5d59733b
Mark Dewey <mdewey@governmentcio.com>
2021-10-06 16:08:03 -0400

    removed v0 from more tests

--- src/applications/check-in/tests/e2e/phase-3-multiple-appointments/errors/malformed.token.cypress.spec.js
+++ src/applications/check-in/tests/e2e/phase-3-multiple-appointments/errors/malformed.token.cypress.spec.js
@@ -1,16 +1,7 @@
 import { generateFeatureToggles } from '../../../../api/local-mock-api/mocks/feature.toggles';
-import mockSessions from '../../../../api/local-mock-api/mocks/v2/sessions.responses';
 
 describe('Check In Experience -- ', () => {
   beforeEach(function() {
-    cy.intercept('GET', '/check_in/v0/patient_check_ins//*', req => {
-      req.reply({
-        statusCode: 200,
-        body: mockSessions.createMockFailedResponse({}),
-        delay: 10, // milliseconds
-      });
-    });
-
     cy.intercept('GET', '/v0/feature_toggles*', generateFeatureToggles());
   });
   afterEach(() => {
