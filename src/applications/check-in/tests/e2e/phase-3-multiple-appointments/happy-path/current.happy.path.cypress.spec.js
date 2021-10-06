691b4f6b75436ae642f8d5ecdca02c7dd5b3560c
Mark Dewey <mdewey@governmentcio.com>
2021-10-06 16:05:52 -0400

    fixed feature toggle tests

--- src/applications/check-in/tests/e2e/phase-3-multiple-appointments/happy-path/current.happy.path.cypress.spec.js
+++ src/applications/check-in/tests/e2e/phase-3-multiple-appointments/happy-path/current.happy.path.cypress.spec.js
@@ -43,7 +43,7 @@ describe('Check In Experience -- ', () => {
         window.sessionStorage.clear();
       });
     });
-    it('Current Happy path', () => {
+    it('phase 3 - happy path', () => {
       const featureRoute =
         '/health-care/appointment-check-in/?id=46bebc0a-b99c-464f-a5c5-560bc9eae287';
       cy.visit(featureRoute);
