60822114154ff1f2d21c07dd2a3538e1f70fffa7
Brian Seek <brian.seek@civicactions.com>
2021-10-07 09:52:29 -0700

    Added e2e tests for each of the appointment status types.

---
+++ src/applications/check-in/tests/e2e/phase-3-multiple-appointments/appointment-status-types/appointment.unknown.reason.status.cypress.spec.js
@@ -0,0 +1,66 @@
+import { createFeatureToggles } from '../../../../api/local-mock-api/mocks/feature.toggles';
+
+import mockSession from '../../../../api/local-mock-api/mocks/v2/sessions.responses';
+import mockPatientCheckIns from '../../../../api/local-mock-api/mocks/v2/patient.check.in.responses';
+
+import Timeouts from 'platform/testing/e2e/timeouts';
+
+describe('Check In Experience -- ', () => {
+  describe('phase 3 -- ', () => {
+    beforeEach(function() {
+      cy.intercept('GET', '/check_in/v2/sessions/*', req => {
+        req.reply(
+          mockSession.createMockSuccessResponse('some-token', 'read.basic'),
+        );
+      });
+      cy.intercept('POST', '/check_in/v2/sessions', req => {
+        req.reply(
+          mockSession.createMockSuccessResponse('some-token', 'read.full'),
+        );
+      });
+      cy.intercept('GET', '/check_in/v2/patient_check_ins/*', req => {
+        const unknownStatus = mockPatientCheckIns.createAppointment();
+        unknownStatus.eligibility = 'INELIGIBLE_UNKNOWN_REASON';
+        const response = {
+          payload: {
+            appointments: [unknownStatus],
+          },
+        };
+        req.reply(response);
+      });
+      cy.intercept(
+        'GET',
+        '/v0/feature_toggles*',
+        createFeatureToggles(true, true, true, false),
+      );
+    });
+    afterEach(() => {
+      cy.window().then(window => {
+        window.sessionStorage.clear();
+      });
+    });
+    it('Appointment shows unknown reason status', () => {
+      const featureRoute =
+        '/health-care/appointment-check-in/?id=46bebc0a-b99c-464f-a5c5-560bc9eae287';
+      cy.visit(featureRoute);
+      cy.get('h1').contains('Check in at VA');
+      cy.injectAxe();
+      cy.axeCheck();
+      cy.get('[label="Your last name"]')
+        .shadow()
+        .find('input')
+        .type('Smith');
+      cy.get('[label="Last 4 digits of your Social Security number"]')
+        .shadow()
+        .find('input')
+        .type('4837');
+      cy.get('[data-testid=check-in-button]').click();
+      cy.get('.appointment-list > li p', { timeout: Timeouts.slow }).should(
+        'contain',
+        'This appointment isn’t eligible for online check-in. Check-in with a staff member.',
+      );
+      cy.injectAxe();
+      cy.axeCheck();
+    });
+  });
+});
