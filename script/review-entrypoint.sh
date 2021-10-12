fc1858069220af4984eafb8efc55cf02240a40df
Tim Wright <timwright12@gmail.com>
2021-10-12 08:10:16 -0400

    Removed update to review-entrypoint

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Build vets-website
 set -e
-yarn install --production=false --prefer-offline
+yarn install --production=false
 npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
 
 # Build content-build and serve site
