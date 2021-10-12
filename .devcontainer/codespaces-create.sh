cdedb221e5b44716f9ebd9c2ea74bb2f32e861a6
Tim Wright <timwright12@gmail.com>
2021-10-12 13:14:29 +0000

    updated readme

--- .devcontainer/codespaces-create.sh
+++ .devcontainer/codespaces-create.sh
@@ -12,7 +12,7 @@ yarn install-repos
 
 # Build vets-website
 set -e
-cd ../vets-website && yarn install --production=false --prefer-offline && yarn build -- --buildtype=localhost --api=https://staging-api.va.gov --host="${CODESPACE_NAME}-3002.githubpreview.dev/" --port=3002
+cd ../vets-website && yarn install --production=false --prefer-offline && yarn build -- --buildtype=localhost --api=https://staging-api.va.gov --host="${CODESPACE_NAME}-3001.githubpreview.dev/" --port=3001
 
 # Build content-build and serve site
 cd ../content-build && yarn install --production=false --prefer-offline && yarn fetch-drupal-cache && yarn build -- --buildtype=localhost --api=https://staging-api.va.gov --host="${CODESPACE_NAME}-3002.githubpreview.dev/" --port=3002 --apps-directory-name=vets-website
