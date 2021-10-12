224dec8769be54dba5cbdcab4bb0b6797f43eddf
Adrian Rollett <acrollet@gmail.com>
2021-10-12 10:53:59 -0600

    Add UX tests for last4 field

---
+++ src/applications/check-in/tests/e2e/phase-3-multiple-appointments/extra-validation/last4.field.is.limited.to.4.characters.cypress.spec.js
@@ -0,0 +1,56 @@
+import { generateFeatureToggles } from '../../../../api/local-mock-api/mocks/feature.toggles';
+
+import mockCheckIn from '../../../../api/local-mock-api/mocks/v2/check.in.responses';
+import mockPatientCheckIns from '../../../../api/local-mock-api/mocks/v2/patient.check.in.responses';
+import mockSession from '../../../../api/local-mock-api/mocks/v2/sessions.responses';
+
+describe('Check In Experience -- ', () => {
+  describe('phase 3 -- ', () => {
+    beforeEach(function() {
+      cy.intercept('GET', '/check_in/v2/sessions/*', req => {
+        req.reply(
+          mockSession.createMockSuccessResponse('some-token', 'read.basic'),
+        );
+      });
+      cy.intercept('GET', '/check_in/v2/patient_check_ins/*', req => {
+        req.reply(mockPatientCheckIns.createMockSuccessResponse({}, false));
+      });
+      cy.intercept('POST', '/check_in/v2/patient_check_ins/', req => {
+        req.reply(mockCheckIn.createMockSuccessResponse({}));
+      });
+      cy.intercept(
+        'GET',
+        '/v0/feature_toggles*',
+        generateFeatureToggles({
+          checkInExperienceMultipleAppointmentSupport: true,
+          checkInExperienceUpdateInformationPageEnabled: true,
+        }),
+      );
+    });
+    afterEach(() => {
+      cy.window().then(window => {
+        window.sessionStorage.clear();
+      });
+    });
+    it('only allows typing 4 characters', () => {
+      const featureRoute =
+        '/health-care/appointment-check-in/?id=46bebc0a-b99c-464f-a5c5-560bc9eae287';
+      cy.visit(featureRoute);
+      cy.get('h1').contains('Check in at VA');
+      cy.get('[label="Last 4 digits of your Social Security number"]')
+        .shadow()
+        .find('input')
+        .type('12345');
+      cy.get('[label="Last 4 digits of your Social Security number"]')
+        .shadow()
+        .find('input')
+        .should('be.visible')
+        .and('have.value', '1234');
+      cy.get('[label="Last 4 digits of your Social Security number"]')
+        .shadow()
+        .find('small')
+        .should('be.visible')
+        .and('have.text', '(Max. 4 characters)');
+    });
+  });
+});
