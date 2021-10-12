8f957a8f081c09bed6ea3ce23d77d4f090932de8
Rob Garrison <wowmotty@gmail.com>
2021-10-12 10:38:15 -0500

    Hide mobile alert in prod (#19078)

--- src/applications/claims-status/components/MobileAppMessage.jsx
+++ src/applications/claims-status/components/MobileAppMessage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import environment from 'platform/utilities/environment';
 
 const appleStoreUrl =
   'https://apps.apple.com/app/apple-store/id1559609596?pt=545860&ct=gov.va.claimstatus&mt=8';
@@ -22,6 +23,11 @@ export default function MobileAppMessage({ mockUserAgent }) {
   const [isHidden, setIsHidden] = useState(
     (sessionStorage.getItem(STORAGE_KEY) || '') !== '',
   );
+
+  // Hide in production until content review is complete
+  if (environment.isProduction()) {
+    return null;
+  }
 
   const userAgent =
     mockUserAgent || navigator.userAgent || navigator.vendor || window.opera;
