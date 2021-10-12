6253626f0d7bdae2076f4f24c220079b8891b801
Adam Whitlock <adam.whitlock@adhocteam.us>
2021-10-12 11:14:58 -0600

    add unit tests getNonActiveLinkUrls

--- src/applications/static-pages/i18Select/tests/getNonActiveLinkUrls.spec.js
+++ src/applications/static-pages/i18Select/tests/getNonActiveLinkUrls.spec.js
@@ -5,6 +5,30 @@ describe('getNonActiveLinkUrls util', () => {
   it('should return non-english urls for "en" language code', () => {
     const result = getNonActiveLinkUrls('en');
 
+    result.forEach(url => {
+      expect(/(esp|tag)/i.test(url)).to.equal(true);
+    });
+
+    expect(result.length).to.equal(8);
+  });
+
+  it('should return correct urls for "es" language code', () => {
+    const result = getNonActiveLinkUrls('es');
+
+    result.forEach(url => {
+      expect(/(esp)/i.test(url)).to.equal(false);
+    });
+
+    expect(result.length).to.equal(8);
+  });
+
+  it('should return correct urls for "tl" language code', () => {
+    const result = getNonActiveLinkUrls('tl');
+
+    result.forEach(url => {
+      expect(/(tag)/i.test(url)).to.equal(false);
+    });
+
     expect(result.length).to.equal(8);
   });
 });
