fa2f9cd908aaaaf7e0160abaf1434ae21f72924f
Alex Garcia <67602137+AGarcia-Clarity@users.noreply.github.com>
2021-10-14 11:36:17 -0400

    refactor sso feature flags (#19039)
    * refactor sso feature flags
    
    * updates caregiver json

--- src/platform/site-wide/ebenefits/tests/selectors.unit.spec.js
+++ src/platform/site-wide/ebenefits/tests/selectors.unit.spec.js
@@ -1,17 +1,21 @@
 import { expect } from 'chai';
 import * as selectors from '../selectors';
 
+const ssoeEbenefitsLinks = 'ssoe_ebenefits_links';
+
 describe('ebenefits selectors', () => {
   describe('shouldUseProxyUrl', () => {
     let state = {};
     beforeEach(() => {
       state = {
         featureToggles: {
-          ssoeEbenefitsLinks: false,
+          [ssoeEbenefitsLinks]: false,
         },
         user: {
           profile: {
-            session: {},
+            session: {
+              ssoe: false,
+            },
           },
         },
       };
@@ -19,19 +23,19 @@ describe('ebenefits selectors', () => {
 
     it('renders false when user is logged out', () => {
       expect(selectors.shouldUseProxyUrl(state)).to.equal(false);
-      state.featureToggles.ssoeEbenefitsLinks = true;
+      state.featureToggles[ssoeEbenefitsLinks] = true;
       expect(selectors.shouldUseProxyUrl(state)).to.equal(false);
     });
     it('renders false when feature flags are off', () => {
       state.user.profile.session.ssoe = true;
       expect(selectors.shouldUseProxyUrl(state)).to.equal(false);
-      state.featureToggles.ssoeEbenefitsLinks = false;
+      state.featureToggles[ssoeEbenefitsLinks] = false;
       expect(selectors.shouldUseProxyUrl(state)).to.equal(false);
     });
     it('renders true when user is logged in and feature flags are on', () => {
       expect(selectors.shouldUseProxyUrl(state)).to.equal(false);
       state.user.profile.session.ssoe = true;
-      state.featureToggles.ssoeEbenefitsLinks = true;
+      state.featureToggles[ssoeEbenefitsLinks] = true;
       expect(selectors.shouldUseProxyUrl(state)).to.equal(true);
     });
   });
