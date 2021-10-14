5ee225dbf2b4da695128100a4040f76ee09eeeb5
Brian Seek <brian.seek@civicactions.com>
2021-10-14 11:49:43 -0700

    passing checkin data in prop for demographics unit test.

--- src/applications/check-in/pages/tests/Demographics.unit.spec.jsx
+++ src/applications/check-in/pages/tests/Demographics.unit.spec.jsx
@@ -23,10 +23,30 @@ describe('check in', () => {
       };
       store = mockStore(initState);
     });
+    const demographics = {
+      mailingAddress: {
+        address1: '123 Turtle Trail',
+        city: 'Treetopper',
+        state: 'Tennessee',
+        zip: '101010',
+      },
+      homeAddress: {
+        address1: '445 Fine Finch Fairway',
+        address2: 'Apt 201',
+        city: 'Fairfence',
+        state: 'Florida',
+        zip: '445545',
+      },
+      homePhone: '5552223333',
+      mobilePhone: '5553334444',
+      workPhone: '5554445555',
+      emailAddress: 'kermit.frog@sesameenterprises.us',
+    };
+
     it('renders', () => {
       const component = render(
         <Provider store={store}>
-          <Demographics />
+          <Demographics checkInData={{ demographics }} />
         </Provider>,
       );
 
@@ -37,7 +57,7 @@ describe('check in', () => {
     it('passes axeCheck', () => {
       axeCheck(
         <Provider store={store}>
-          <Demographics />
+          <Demographics checkInData={{ demographics }} />
         </Provider>,
       );
     });
