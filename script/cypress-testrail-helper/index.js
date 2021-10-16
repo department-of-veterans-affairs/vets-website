97ed24025696e2e47b1a92db49809f7353b4cc18
Tze-chiu Lei <tze@adhocteam.us>
2021-10-15 19:57:43 -0700

    Update cypress-testrail-reporter: Add util-methods and update config/flow.

--- script/cypress-testrail-helper/index.js
+++ script/cypress-testrail-helper/index.js
@@ -7,32 +7,54 @@ const fs = require('fs');
 const utils = require('./lib/utils');
 
 const appArgs = process.argv.slice(2);
-const myConfigPath = './script/cypress-testrail-helper/my-config.json'; // Project-root-relative path [for fs].
+const myConfigPath = './script/cypress-testrail-helper/my-config.json'; // Project-root-relative path [for fs-call].
 
 // Runtime variables.
-let myConfig = undefined; // Config's generated & saved on first launch.
-let projectSwitching = false; // Flag for changing config's saved Project & Suite IDs.
+let myConfig, specFile;
 
 // Cypress run-specific config and run function
 const configRunCySpec = appConfig => {
   console.log(chalk.yellow('CREATING CYPRESS RUN-SPECIFIC CONFIG-FILE NOW...'));
   utils
-    .getSetCypressConfig(appConfig)
-    .then(specPath => {
-      console.log(chalk.green('CYPRESS RUN-SPECIFIC CONFIG-FILE CREATED!'));
-      console.log(chalk.green(`NOW RUNNING CYPRESS SPEC: ${specPath}`));
+    .globSpecFiles()
+    .then(files => {
       console.log(
-        chalk.green(
-          'CLI-prompt will return after Cypress child-process exits.\nTHANKS FOR USING CYPRESS-TESTRAIL-HELPER!',
-        ),
+        chalk.green('[configRunCySpec] spec file(s) returned:', files),
       );
 
-      // Run Cypress spec
-      utils.runCySpec(specPath);
-      utils.parseSpecTrInfo(specPath);
+      filesLength = files.length;
+      console.log(
+        chalk.green('[configRunCySpec] files array-length:', filesLength),
+      );
+
+      if (filesLength === 0) {
+        console.log(chalk.red('NO FILES FOUND!  Please try again.'));
+        configRunCySpec(myConfig);
+      } else if (filesLength === 1) {
+        specFile = files[0];
+      } else {
+        console.log('Multiple files found!');
+        utils
+          .chooseSpecFile(files)
+          .then(file => {
+            specFile = file;
+          })
+          .catch(err => {
+            throw new Error(err);
+          });
+      }
+
+      utils
+        .getSpecTrInfo(specFile)
+        .then(parsedTrInfo => {
+          console.log('trInfo returned:', parsedTrInfo);
+        })
+        .catch(err => {
+          console.log(chalk.red(`getSpecTrInfo failed! ${err}`));
+        });
     })
     .catch(err => {
-      console.error(chalk.red(`ERROR: ${err}`));
+      console.log(chalk.red(`configRunCySpec failed! ${err}`));
       process.exitCode = 1;
     });
 };
@@ -51,11 +73,6 @@ console.log(
 // console.log('process.argv:', process.argv);
 // console.log('appArgs:', appArgs);
 
-// SET PROJECT-SWITCHING FLAG.
-projectSwitching =
-  (appArgs.includes('--switch-project') || appArgs.includes('--sp')) &&
-  fs.existsSync(myConfigPath);
-
 // CHECK FOR CONFIG-FILE, GET/SET CONFIG(S), THEN RUN CYPRESS SPEC.
 // IF no config-file exists, create one.
 // IF config-file does exist, create run-specific Cypress config-file.
@@ -71,39 +88,19 @@ switch (fs.existsSync(myConfigPath)) {
       ),
     );
 
-    if (projectSwitching) {
-      console.log(chalk.yellow('SWITCHING PROJECT...'));
-      utils
-        .getSetAppProjectConfig(myConfig)
-        .then(() => {
-          console.log(chalk.green('APP CONFIG-FILE UPDATED!'));
-          configRunCySpec(myConfig);
-        })
-        .catch(err => {
-          console.error(chalk.red(`'ERROR: ${err}`));
-        });
-    } else {
-      configRunCySpec(myConfig);
-    }
+    configRunCySpec(myConfig);
     break;
   case false:
     console.log(chalk.yellow('APP CONFIG-FILE MISSING -- CREATING ONE NOW...'));
     utils
-      .getSetAppConfig(myConfig)
+      .setMyConfig(myConfig)
       .then(() => {
-        console.log(
-          chalk.green(
-            'APP CONFIG-FILE SUCCESSFULLY CREATED!\nCREATING CYPRESS RUN-SPECIFIC CONFIG-FILE NOW...',
-          ),
-        );
+        myConfig = require('./my-config.json');
+        console.log(chalk.green('APP CONFIG-FILE SUCCESSFULLY CREATED!'));
         configRunCySpec(myConfig);
       })
       .catch(err => {
-        console.error(
-          chalk.red(
-            `'ERROR: ${err}\n[See the app README.md file for instructions.]'`,
-          ),
-        );
+        console.error(chalk.red(`ERROR: ${err}`));
       });
     break;
   default:
--- script/cypress-testrail-helper/lib/utils.js
+++ script/cypress-testrail-helper/lib/utils.js
@@ -7,23 +7,107 @@ const { parse } = require('comment-parser');
 
 const cyConfig = require('../../../config/cypress-testrail.json');
 const inquirer = require('./app-inquirer');
+const { glob } = require('glob');
 
 module.exports = {
-  async getSetAppConfig(myConfig) {
-    const answers = await inquirer.askTestRailConfigOptions();
-
-    // eslint-disable-next-line no-unused-vars, no-param-reassign
-    myConfig = answers;
+  async setMyConfig() {
+    const answers = await inquirer.askMyConfigOptions();
 
     return new Promise((myResolve, myReject) => {
       try {
         fs.writeFileSync(
           './script/cypress-testrail-helper/my-config.json',
-          JSON.stringify({ ...answers, trIncludeAllInTestRun: false }),
+          JSON.stringify({ ...answers }),
         );
-        myResolve('succeeded');
+        myResolve('setMyConfig succeeded');
       } catch (err) {
-        myReject('failed');
+        myReject('setMyConfig failed');
+      }
+    });
+  },
+  async globSpecFiles() {
+    let input, filename, specGlob;
+    const answers = await inquirer.askSpecFilename();
+
+    // clean input & set filename
+    input = answers.specFilename;
+    if (input.includes('/')) {
+      // remove path in from of filename
+      input = input.substr(input.lastIndexOf('/') + 1);
+    }
+    if (input.includes('.cypress.')) {
+      // remove dot-extensions
+      input = input.substring(0, input.indexOf('.cypress'));
+    }
+    filename = input;
+
+    return new Promise((myResolve, myReject) => {
+      try {
+        specGlob = 'src/applications/**/' + filename + '.cypress.spec.js?(x)';
+        console.log('specGlob: ', specGlob);
+        glob(specGlob, {}, (err, files) => {
+          if (err) {
+            throw new Error(err);
+          }
+          myResolve(files);
+        });
+      } catch (err) {
+        myReject(`globSpecFiles failed: ${err}`);
+      }
+    });
+  },
+  async chooseSpecFile(files) {
+    const answers = await inquirer.askSpecFileChoice(files);
+
+    console.log('answers.specFile: ', answers.specFile);
+
+    return new Promise((myResolve, myReject) => {
+      try {
+        myResolve(answers.specFile);
+      } catch (err) {
+        myReject(`chooseSpecFile failed: ${err}`);
+      }
+    });
+  },
+  async getSpecTrInfo(specFile) {
+    const parseComments = async data => {
+      try {
+        const parsedComments = await parse(data);
+        return parsedComments;
+      } catch (e) {
+        console.error(chalk.red(`ERROR: ${e}`));
+      }
+    };
+
+    return new Promise((myResolve, myReject) => {
+      try {
+        fs.readFile(specFile, 'utf8', async (err, data) => {
+          const parsedTrInfo = {};
+          let specComments, trCommentBlock, trCommentTags;
+
+          if (err) throw new Error(err);
+
+          specComments = await parseComments(data);
+          trCommentBlock = specComments.filter(b =>
+            b.description.toLowerCase().includes('testrail-integrated'),
+          )[0];
+          trCommentTags = trCommentBlock.tags.filter(
+            t => t.tag.toLowerCase() === 'testrailinfo',
+          );
+          trCommentTags.forEach(t => {
+            const tagName = t.name;
+            const tagDescription = t.description;
+
+            parsedTrInfo[tagName] =
+              tagName === 'runName'
+                ? tagDescription
+                : parseInt(tagDescription, 10);
+          });
+
+          myResolve(parsedTrInfo);
+        });
+      } catch (err) {
+        myReject(`readFile failed! ${err}`);
       }
     });
   },
@@ -98,8 +182,8 @@ module.exports = {
   },
 
   async getSetAppProjectConfig(myConfig) {
-    let projectAnswers = undefined;
-    let myNewConfig = undefined;
+    let projectAnswers;
+    let myNewConfig;
 
     projectAnswers = await inquirer.askTestRailProjectOptions();
     // eslint-disable-next-line no-unused-vars, no-param-reassign, no-multi-assign
@@ -131,34 +215,51 @@ module.exports = {
       }
     };
 
-    fs.readFile(specPath, 'utf8', async (err, data) => {
-      const trInfo = {};
-      let specComments, trCommentBlock, trCommentTags;
+    // delete any leading slash
+    specPath = specPath.replace(/^\//, '');
+    // prepend 'src/applications/**/' if missing
+    if (!specPath.includes('src/applications/')) {
+      if (specPath.indexOf('**/') === 0) {
+        specPath = 'src/applications/' + specPath;
+      } else {
+        specPath = 'src/applications/**/' + specPath;
+      }
+      // console.log('specPath (prepended):', specPath);
+    }
+    // append file-extension pattern if missing
+    if (!specPath.includes('.cypress.spec.js')) {
+      specPath += '.cypress.spec.js?(x)';
+    }
+    console.log('specPath (modified):', specPath);
 
-      if (err) throw err;
+    // fs.readFile(specPath, 'utf8', async (err, data) => {
+    //   const trInfo = {};
+    //   let specComments, trCommentBlock, trCommentTags;
 
-      specComments = await parseComments(data);
-      // console.log('specComments:', specComments);
+    //   if (err) throw err;
 
-      trCommentBlock = specComments.filter(b =>
-        b.description.toLowerCase().includes('testrail-integrated'),
-      )[0];
-      // console.log('trCommentBlock:', trCommentBlock);
+    //   specComments = await parseComments(data);
+    //   // console.log('specComments:', specComments);
 
-      trCommentTags = trCommentBlock.tags.filter(
-        t => t.tag.toLowerCase() === 'testrailinfo',
-      );
-      // console.log('trCommentTags:', trCommentTags);
+    //   trCommentBlock = specComments.filter(b =>
+    //     b.description.toLowerCase().includes('testrail-integrated'),
+    //   )[0];
+    //   // console.log('trCommentBlock:', trCommentBlock);
 
-      trCommentTags.forEach(t => {
-        const tagName = t.name;
-        const tagDescription = t.description;
+    //   trCommentTags = trCommentBlock.tags.filter(
+    //     t => t.tag.toLowerCase() === 'testrailinfo',
+    //   );
+    //   // console.log('trCommentTags:', trCommentTags);
 
-        trInfo[tagName] =
-          tagName === 'runName' ? tagDescription : parseInt(tagDescription, 10);
-      });
-      console.log('trInfo:', trInfo);
-    });
+    //   trCommentTags.forEach(t => {
+    //     const tagName = t.name;
+    //     const tagDescription = t.description;
+
+    //     trInfo[tagName] =
+    //       tagName === 'runName' ? tagDescription : parseInt(tagDescription, 10);
+    //   });
+    //   console.log('trInfo:', trInfo);
+    // });
 
     console.log('\nreadFile called...');
   },
