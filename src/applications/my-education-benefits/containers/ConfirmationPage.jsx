5024fb9e281b6d61baef350acb4e548be7d404a8
Kelson Adams <kelsonic@users.noreply.github.com>
2021-10-18 19:18:25 +0200

    Make changes to copy + links for contact-us (#19114)
    * Make changes to copy + links for contact-us
    
    * Remove periods
    
    Co-authored-by: Kelson Adams <kadams@governmentcio.com>

--- src/applications/my-education-benefits/containers/ConfirmationPage.jsx
+++ src/applications/my-education-benefits/containers/ConfirmationPage.jsx
@@ -212,8 +212,7 @@ const pendingPage = (
     <ul>
       <li>
         If you need to submit documentation to VA, such as service records,
-        please send this through our{' '}
-        <a href="https://gibill.custhelp.va.gov/app/ask ">Ask VA feature</a>.
+        please send this through <a href="/contact-us">Ask VA</a>.
       </li>
       <li>
         Review and/or update your direct deposit information on{' '}
