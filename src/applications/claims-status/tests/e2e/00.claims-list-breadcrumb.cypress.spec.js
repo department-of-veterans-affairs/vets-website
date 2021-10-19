4f13c1f9c8b7c308fa12f840b249e9b8b4bca3d9
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-10-19 10:32:17 -0500

    Refactor Claims E2E tests to use page objects (#19098)
    * Refactor Claims E2E tests to use page objects
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Refactor last few tests

--- src/applications/claims-status/tests/e2e/00.claims-list-breadcrumb.cypress.spec.js
+++ src/applications/claims-status/tests/e2e/00.claims-list-breadcrumb.cypress.spec.js
@@ -1,50 +1,11 @@
-const Timeouts = require('platform/testing/e2e/timeouts.js');
-
+import TrackClaimsPage from './page-objects/TrackClaimsPage';
 import claimsList from './fixtures/mocks/claims-list.json';
 
 describe('Breadcrumb Test', () => {
   it('Verifies breadcrumb functionality', () => {
-    cy.intercept('GET', '/v0/evss_claims_async', claimsList);
-    cy.login();
-    cy.visit('/track-claims');
-    cy.title().should('eq', 'Track Claims: VA.gov');
-    cy.get('.claim-list-item-container', { timeout: Timeouts.slow }).should(
-      'be.visible',
-    );
-    cy.get('.va-nav-breadcrumbs').should('be.visible');
-    cy.get('.va-nav-breadcrumbs-list').should('be.visible');
-    cy.get('a[aria-current="page"').should('be.visible');
-    cy.injectAxeThenAxeCheck();
-
-    cy.get(
-      '.va-nav-breadcrumbs-list li:nth-of-type(2) a[aria-current="page"]',
-    ).should('exist');
-    cy.get(
-      '.va-nav-breadcrumbs-list li:nth-of-type(2) a[aria-current="page"]',
-    ).should('contain', 'Check your claims and appeals');
-    cy.get(
-      '.va-nav-breadcrumbs-list li:nth-of-type(2) a[aria-current="page"]',
-    ).should('have.css', 'pointer-events', 'none');
-
-    cy.viewportPreset('va-top-mobile-1');
-
-    cy.get('.va-nav-breadcrumbs-list').should('be.visible');
-
-    cy.get('.va-nav-breadcrumbs-list li:not(:nth-last-child(2))').should(
-      'have.css',
-      'display',
-      'none',
-    );
-    cy.get('.va-nav-breadcrumbs-list li:nth-last-child(2)').should(
-      'contain',
-      'Home',
-    );
-    cy.get('.va-nav-breadcrumbs-list li:nth-last-child(2)').should(
-      'have.css',
-      'display',
-      'inline-block',
-    );
-
-    cy.viewport(1024, 768);
+    const trackClaimsPage = new TrackClaimsPage();
+    trackClaimsPage.loadPage(claimsList);
+    trackClaimsPage.checkBreadcrumbs();
+    trackClaimsPage.checkBreadcrumbsMobile();
   });
 });
