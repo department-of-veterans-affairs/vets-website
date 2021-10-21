97c6456fe73f3f964466193e31efd5a6271f68c8
Patrick <patrickryll@yahoo.com>
2021-10-21 14:56:20 -0400

    removed eslint toggle offs

--- src/platform/forms-system/src/js/state/index.js
+++ src/platform/forms-system/src/js/state/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-console */
 import { createInitialState } from '../state/helpers';
 import reducers from '../state/reducers';
 
@@ -10,15 +8,9 @@ export default function createSchemaFormReducer(
 ) {
   return (state = initialState, action) => {
     const reducer = formReducers[action.type];
-    // console.log(reducer, `--> reducer`);
     if (reducer) {
-      // console.group/* Collapsed */('if(reducer)');
-      //   console.log(state, `--> state`);
-      //   console.log(action, `--> action`);
-      // console.groupEnd();
       return reducer(state, action);
     }
-    // console.log(state, `--> state`);
     return state;
   };
 }
