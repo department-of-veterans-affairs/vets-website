8bd09200ba0b4157e4b8463d5f4992623a949528
Taylor Mitchell <tmitchell@gcio.com>
2021-10-21 12:17:27 -0400

    Not sure how these were changed, but reverting back

--- src/applications/personalization/profile/tests/e2e/address-validation/confirm-address.cypress.spec.js
+++ src/applications/personalization/profile/tests/e2e/address-validation/confirm-address.cypress.spec.js
@@ -1,22 +1,35 @@
-import AddressPage from './page-objects/AddressPage';
+import { setUp } from '@@profile/tests/e2e/address-validation/setup';
 
 describe('Personal and contact information', () => {
   describe('when entering a valid address that needs confirmation', () => {
     it('should successfully update on Desktop', () => {
-      const formFields = {
-        address: '36310 Coronado Dr',
-        city: 'Fremont',
-        state: 'CA',
-        zipCode: '94536',
-      };
-      const addressPage = new AddressPage();
-      addressPage.loadPage('confirm-address');
-      addressPage.fillAddressForm(formFields);
+      setUp('confirm-address');
+
+      cy.findByLabelText(/^street address \(/i)
+        .clear()
+        .type('36310 Coronado Dr');
+
+      cy.findByLabelText(/^street address line 2/i).clear();
+      cy.findByLabelText(/^street address line 3/i).clear();
+
+      cy.findByLabelText(/City/i)
+        .clear()
+        .type('Fremont');
+
+      cy.findByLabelText(/^State/).select('CA');
+
+      cy.findByLabelText(/Zip code/i)
+        .clear()
+        .type('94536');
+
       cy.findByTestId('save-edit-button').click({
         force: true,
       });
-      addressPage.saveForm();
-      addressPage.confirmAddressMessage(formFields);
+
+      cy.findByTestId('mailingAddress')
+        .should('contain', '36310 Coronado Dr')
+        .and('contain', 'Please confirm your address');
+
       cy.findByTestId('confirm-address-button').click({
         force: true,
       });
