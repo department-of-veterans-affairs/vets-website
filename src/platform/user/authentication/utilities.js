e2c0cd7e04c8534c0c87df47b3b775f8cd93ad79
Alexander Garcia <agarcia@clarityinnovates.com>
2021-10-26 09:57:06 -0400

    re-adds postLogin query

--- src/platform/user/authentication/utilities.js
+++ src/platform/user/authentication/utilities.js
@@ -127,7 +127,7 @@ export function redirect(redirectUrl, clickedEvent) {
   if (loginAppUrlRE.test(window.location.pathname)) {
     const { application: app } = getQueryParams();
     rUrl = {
-      mhv: `${redirectUrl}?skip_dupe=mhv&redirect=${returnUrl}`,
+      mhv: `${redirectUrl}?skip_dupe=mhv&redirect=${returnUrl}&postLogin=true`,
       myvahealth: `${redirectUrl}`,
     }[app];
     recordEvent({ event: `${authnSettings.REDIRECT_EVENT}-${app}-inbound` });
