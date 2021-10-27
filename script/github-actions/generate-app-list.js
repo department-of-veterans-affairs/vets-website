05ac0318649010bcbb4dc01f3c5589020b2d1729
Cbonade <cbonade@DESKTOP-MI88P0N.localdomain>
2021-10-27 15:51:05 -0400

    harness the existing manifest list function and simplify script

--- script/github-actions/generate-app-list.js
+++ script/github-actions/generate-app-list.js
@@ -1,19 +1,15 @@
-const fs = require('fs');
-const glob = require('glob');
 const core = require('@actions/core');
 
-function findAllManifests() {
-  const applicationList = [];
-  glob('src/applications/**/manifest.json', function(err, files) {
-    files.forEach(file => {
-      const manifestData = JSON.parse(fs.readFileSync(file));
-      applicationList.push({
-        name: manifestData.appName,
-        slug: manifestData.entryName,
-      });
-    });
-    core.exportVariable('APPLICATION_LIST', applicationList);
+const { getAppManifests } = require('../../config/manifest-helpers');
+
+function exportAppList() {
+  const applicationList = getAppManifests().map(app => {
+    return {
+      name: app.appName,
+      slug: app.entryName,
+    };
   });
+  core.exportVariable('APPLICATION_LIST', applicationList);
 }
 
-findAllManifests();
+exportAppList();
