ecd6831e4e23341e73f0671f44a4214c27b5fd87
Kelson Adams <kadams@governmentcio.com>
2021-10-27 08:32:42 -0600

    Create new header component

---
+++ src/platform/site-wide/header/components/App/index.unit.spec.js
@@ -0,0 +1,28 @@
+// Node modules.
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+// Relative imports.
+import { App } from '.';
+
+describe('Header <App>', () => {
+  it('renders legacy header when showHeaderV2 is falsey', () => {
+    const wrapper = shallow(<App show />);
+    expect(wrapper.find(`Header`)).be.have.lengthOf(0);
+    wrapper.unmount();
+  });
+
+  it('renders legacy header when showHeaderV2 is truthy and our width is more than 768px', () => {
+    window.innerWidth = 768;
+    const wrapper = shallow(<App show showHeaderV2 />);
+    expect(wrapper.find(`Header`)).be.have.lengthOf(0);
+    wrapper.unmount();
+  });
+
+  it('renders header v2 when showHeaderV2 is truthy and our width is less than 768px', () => {
+    window.innerWidth = 767;
+    const wrapper = shallow(<App show showHeaderV2 />);
+    expect(wrapper.find(`Header`)).be.have.lengthOf(1);
+    wrapper.unmount();
+  });
+});
