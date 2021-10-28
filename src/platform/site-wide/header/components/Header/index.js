d316b836c3a779d1c037703c8424c13ee9dbb6ee
Kelson Adams <kadams@governmentcio.com>
2021-10-27 20:15:39 -0600

    Add header v2 boilerplate + styling

--- src/platform/site-wide/header/components/Header/index.js
+++ src/platform/site-wide/header/components/Header/index.js
@@ -1,10 +1,54 @@
 // Node modules.
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+// Relative imports.
+import './styles.scss';
+import OfficialGovtWebsite from '../OfficialGovtWebsite';
+import VeteranCrisisLine from '../VeteranCrisisLine';
+import { toggleFormSignInModal } from '../../../user-nav/actions';
 
 // This is the Header v2 component, it will be built out in a follow-up PR.
-export const Header = () => {
-  return <header className="header">New header</header>;
+export const Header = ({ showNavLogin, showMegaMenu }) => {
+  return (
+    <header className="vads-u-display--flex vads-u-flex-direction--column vads-u-margin--0 vads-u-padding--0">
+      {/* Official government website banner */}
+      <OfficialGovtWebsite />
+
+      {/* Veteran crisis line */}
+      <VeteranCrisisLine />
+
+      <div className="vads-u-background-color--primary-darkest vads-u-display--flex vads-u-align-items--center vads-u-justify-content--space-between vads-u-padding-y--0p5 vads-u-padding-left--1p5 vads-u-padding-right--1">
+        {/* Logo */}
+        <a href="/" className="header-logo">
+          <img src="/img/header-logo-v2.png" alt="Go to VA.gov" />
+        </a>
+
+        <div className="vads-u-display--flex vads-u-flex-direction--row vads-u-align-items--center">
+          {/* Sign in button */}
+          <button
+            className="header-sign-in-button va-button-link vads-u-color--white vads-u-text-decoration--none"
+            onClick={toggleFormSignInModal()}
+            type="button"
+          >
+            Sign in
+          </button>
+
+          {/* Mobile menu button */}
+          <button
+            className="header-menu-button usa-button vads-u-background-color--white vads-u-color--link-default vads-u-padding-y--1 vads-u-padding-x--1p5 vads-u-margin--0 vads-u-margin-left--2"
+            type="button"
+          >
+            Menu
+            <i
+              aria-hidden="true"
+              className="fa fa-bars vads-u-margin-left--1 vads-u-font-size--sm"
+            />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 Header.propTypes = {
@@ -13,4 +57,11 @@ Header.propTypes = {
   showNavLogin: PropTypes.bool.isRequired,
 };
 
-export default Header;
+const mapDispatchToProps = dispatch => ({
+  toggleFormSignInModal: () => dispatch(toggleFormSignInModal()),
+});
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(Header);
---
+++ src/platform/site-wide/header/components/OfficialGovtWebsite/index.js
@@ -0,0 +1,73 @@
+// Node modules.
+import React, { useState } from 'react';
+// Relative imports.
+import './styles.scss';
+
+export const OfficialGovtWebsite = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div className="vads-u-display--flex vads-u-flex-direction--column">
+      {/* Banner */}
+      <div className="vads-u-background-color--gray-lightest vads-u-display--flex vads-u-align-items--center vads-u-justify-content--center vads-u-text-align--center vads-u-padding--0p5">
+        <img
+          alt="U.S. flag"
+          className="header-us-flag vads-u-margin-right--1"
+          src="/img/tiny-usa-flag.png"
+        />
+        <button
+          aria-controls="official-govt-site-explanation"
+          aria-expanded={expanded ? 'true' : 'false'}
+          className="expand-official-govt-explanation va-button-link vads-u-text-decoration--none"
+          onKeyDown={event => event.keyCode === 13 && setExpanded(!expanded)}
+          onMouseUp={() => setExpanded(!expanded)}
+        >
+          An official website of the United States government.
+          <i
+            aria-hidden="true"
+            className="fa fa-chevron-down vads-u-margin-left--0p5"
+          />
+        </button>
+      </div>
+
+      {/* Expanded section */}
+      {expanded && (
+        <div
+          aria-hidden={expanded ? 'false' : 'true'}
+          className="vads-u-background-color--gray-lightest vads-u-display--flex vads-u-flex-direction--column vads-u-padding--0p5 vads-u-padding-bottom--2"
+          id="official-govt-site-explanation"
+        >
+          <div className="vads-u-display--flex vads-u-flex-direction--row vads-u-align-items--flex-start">
+            <img
+              alt="Dot gov"
+              className="usa-banner-icon usa-media_block-img"
+              src="/img/icon-dot-gov.svg"
+            />
+            <p>
+              <strong>The .gov means it’s official.</strong>
+              <br />
+              Federal government websites often end in .gov or .mil. Before
+              sharing sensitive information, make sure you’re on a federal
+              government site.
+            </p>
+          </div>
+          <div className="vads-u-display--flex vads-u-flex-direction--row vads-u-align-items--flex-start vads-u-margin-top--2">
+            <img
+              alt="SSL"
+              className="usa-banner-icon usa-media_block-img"
+              src="/img/icon-https.svg"
+            />
+            <p>
+              <strong>The site is secure.</strong>
+              <br /> The <strong>https://</strong> ensures that you’re
+              connecting to the official website and that any information you
+              provide is encrypted and sent securely.
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OfficialGovtWebsite;
