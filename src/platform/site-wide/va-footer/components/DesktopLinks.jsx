2c0b08568049e8ba1a2ce2ec5ced3de1fdb3b8c7
Trent McDole <trent.mcdole@gcio.com>
2021-10-28 11:49:01 -0500

    added new header to footer

--- src/platform/site-wide/va-footer/components/DesktopLinks.jsx
+++ src/platform/site-wide/va-footer/components/DesktopLinks.jsx
@@ -5,44 +5,55 @@ import { FOOTER_COLUMNS, FOOTER_EVENTS } from '../helpers';
 
 export default function DesktopLinks({ links, visible }) {
   return (
-    <div
-      aria-hidden={visible ? 'false' : 'true'}
-      className="usa-grid-full flex-container usa-grid-flex-mobile va-footer-content"
-    >
-      <div className="va-footer-linkgroup">
-        <h2 className="va-footer-linkgroup-title">
-          Veteran programs and services
-        </h2>
-        {links[FOOTER_COLUMNS.PROGRAMS]}
+    <>
+      <h2
+        className="heading-level-2"
+        id="footer-heading"
+        style={{ marginTop: '.5em', textAlign: 'center' }}
+      >
+        Footer
+      </h2>
+      <div
+        aria-hidden={visible ? 'false' : 'true'}
+        className="usa-grid-full flex-container usa-grid-flex-mobile va-footer-content"
+      >
+        <div className="va-footer-linkgroup">
+          <h2 className="va-footer-linkgroup-title">
+            Veteran programs and services
+          </h2>
+          {links[FOOTER_COLUMNS.PROGRAMS]}
+        </div>
+        <div className="va-footer-linkgroup" id="footer-services">
+          <h2 className="va-footer-linkgroup-title">More VA resources</h2>
+          {links[FOOTER_COLUMNS.RESOURCES]}
+        </div>
+        <div className="va-footer-linkgroup" id="footer-popular">
+          <h2 className="va-footer-linkgroup-title">Get VA updates</h2>
+          {links[FOOTER_COLUMNS.CONNECT]}
+        </div>
+        <div className="va-footer-linkgroup" id="veteran-crisis">
+          <h2 className="va-footer-linkgroup-title">
+            In crisis? Talk to someone now
+          </h2>
+          <ul className="va-footer-links">
+            <li>
+              <button
+                onClick={() =>
+                  recordEvent({ event: FOOTER_EVENTS.CRISIS_LINE })
+                }
+                className="va-button-link va-overlay-trigger"
+                data-show="#modal-crisisline"
+              >
+                Veterans Crisis Line
+              </button>
+            </li>
+          </ul>
+          <h2 className="va-footer-linkgroup-title vads-u-margin-top--2 vads-u-padding-bottom--1">
+            Get answers
+          </h2>
+          {links[FOOTER_COLUMNS.CONTACT]}
+        </div>
       </div>
-      <div className="va-footer-linkgroup" id="footer-services">
-        <h2 className="va-footer-linkgroup-title">More VA resources</h2>
-        {links[FOOTER_COLUMNS.RESOURCES]}
-      </div>
-      <div className="va-footer-linkgroup" id="footer-popular">
-        <h2 className="va-footer-linkgroup-title">Get VA updates</h2>
-        {links[FOOTER_COLUMNS.CONNECT]}
-      </div>
-      <div className="va-footer-linkgroup" id="veteran-crisis">
-        <h2 className="va-footer-linkgroup-title">
-          In crisis? Talk to someone now
-        </h2>
-        <ul className="va-footer-links">
-          <li>
-            <button
-              onClick={() => recordEvent({ event: FOOTER_EVENTS.CRISIS_LINE })}
-              className="va-button-link va-overlay-trigger"
-              data-show="#modal-crisisline"
-            >
-              Veterans Crisis Line
-            </button>
-          </li>
-        </ul>
-        <h2 className="va-footer-linkgroup-title vads-u-margin-top--2 vads-u-padding-bottom--1">
-          Get answers
-        </h2>
-        {links[FOOTER_COLUMNS.CONTACT]}
-      </div>
-    </div>
+    </>
   );
 }
