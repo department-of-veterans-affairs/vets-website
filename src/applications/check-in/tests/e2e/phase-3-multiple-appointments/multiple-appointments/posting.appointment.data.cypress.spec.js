0565b76122dd57d63131edb5f011324173aa2993
Mark Dewey <mdewey@users.noreply.github.com>
2021-10-28 09:55:35 -0400

    Check in 31494 multiple appt toggle depricated (#19207)
    * removed toggle from the app
    
    * removed from supporting files
    
    * removed from mock apis
    
    * updated e2e tests
    
    * removed depricated files
    
    * removed unneeded test
    
    * Remove variable re-introduced in merge
    
    * removed v1 api references
    
    Co-authored-by: Adrian Rollett <acrollet@gmail.com>

--- src/applications/check-in/tests/e2e/phase-3-multiple-appointments/multiple-appointments/posting.appointment.data.cypress.spec.js
+++ src/applications/check-in/tests/e2e/phase-3-multiple-appointments/multiple-appointments/posting.appointment.data.cypress.spec.js
@@ -34,7 +34,6 @@ describe('Check In Experience -- ', () => {
         'GET',
         '/v0/feature_toggles*',
         generateFeatureToggles({
-          checkInExperienceMultipleAppointmentSupport: true,
           checkInExperienceUpdateInformationPageEnabled: false,
         }),
       );
