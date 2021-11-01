96cd71dcfb4b64b3bacdc5e4113a278daead5b94
jerekshoe <jerek.shoemaker@adhocteam.us>
2021-11-01 11:45:51 -0500

    Undo changes to IntroductionPage

--- src/applications/lgy/coe/containers/IntroductionPage.jsx
+++ src/applications/lgy/coe/containers/IntroductionPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { focusElement } from 'platform/utilities/ui';
 import { isLoggedIn } from 'platform/user/selectors';
@@ -11,33 +11,14 @@ import { CALLSTATUS, COE_ELIGIBILITY_STATUS } from '../constants';
 const IntroductionPage = props => {
   let content;
 
-  const [showSteps, setShowSteps] = useState(false);
-
   useEffect(() => {
     focusElement('.va-nav-breadcrumbs-list');
   });
   // Set the content to be the loading indicator
   content = <LoadingIndicator message="Loading your application..." />;
 
-  const onContentClick = () => {
-    setShowSteps(true);
-  };
-
   // Once the coe call is done, render the rest of the content
   const coeCallEnded = [CALLSTATUS.failed, CALLSTATUS.success, CALLSTATUS.skip];
-
-  // We don't want to show logged in content for these statuses
-  const pendingStatuses = [
-    COE_ELIGIBILITY_STATUS.pending,
-    COE_ELIGIBILITY_STATUS.pendingUpload,
-  ];
-
-  const hideLoggedInStatuses = [
-    COE_ELIGIBILITY_STATUS.denied,
-    ...pendingStatuses,
-  ];
-
-  console.log(showSteps); // eslint-disable-line no-console
 
   if (!props.loggedIn && coeCallEnded.includes(props.status)) {
     content = notLoggedInContent(props);
@@ -46,10 +27,7 @@ const IntroductionPage = props => {
     content = (
       <div>
         <COEIntroPageBox coe={props.coe} status={props.status} />
-        {pendingStatuses.includes(props.coe.status) && (
-          <a onClick={onContentClick}>Request a VA home loan COE again</a>
-        )}
-        {(!hideLoggedInStatuses.includes(props.coe.status) || showSteps) && (
+        {props.coe.status !== COE_ELIGIBILITY_STATUS.denied && (
           <LoggedInContent parentProps={props} />
         )}
       </div>
@@ -60,9 +38,9 @@ const IntroductionPage = props => {
 };
 
 const mapStateToProps = state => ({
+  status: state.certificateOfEligibility.generateAutoCoeStatus,
   coe: state.certificateOfEligibility.coe,
   loggedIn: isLoggedIn(state),
-  status: state.certificateOfEligibility.generateAutoCoeStatus,
 });
 
 export default connect(mapStateToProps)(IntroductionPage);
