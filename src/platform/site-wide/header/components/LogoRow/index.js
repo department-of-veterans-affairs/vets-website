e31cfae279ce9264c903277f8a66f5138dc3db6a
Kelson Adams <kadams@governmentcio.com>
2021-11-01 13:32:08 -0600

    Make Sign in button work

--- src/platform/site-wide/header/components/LogoRow/index.js
+++ src/platform/site-wide/header/components/LogoRow/index.js
@@ -1,35 +1,34 @@
 // Node modules.
 import React from 'react';
+// Relative imports.
+import UserNav from '../../../user-nav/containers/Main';
 
-export const LogoRow = () => (
-  <div className="vads-u-background-color--primary-darkest vads-u-display--flex vads-u-align-items--center vads-u-justify-content--space-between vads-u-padding-y--0p5 vads-u-padding-left--1p5 vads-u-padding-right--1">
-    {/* Logo */}
-    <a href="/" className="header-logo">
-      <img src="/img/header-logo-v2.png" alt="Go to VA.gov" />
-    </a>
+export const LogoRow = () => {
+  return (
+    <div className="header-logo-row vads-u-background-color--primary-darkest vads-u-display--flex vads-u-align-items--center vads-u-justify-content--space-between vads-u-padding-y--0p5 vads-u-padding-left--1p5 vads-u-padding-right--1">
+      {/* Logo */}
+      <a href="/" className="header-logo">
+        <img alt="Go to VA.gov" src="/img/header-logo-v2.png" />
+      </a>
 
-    <div className="vads-u-display--flex vads-u-flex-direction--row vads-u-align-items--center">
-      {/* Sign in button */}
-      <button
-        className="header-sign-in-button va-button-link vads-u-color--white vads-u-text-decoration--none"
-        type="button"
-      >
-        Sign in
-      </button>
+      <div className="vads-u-display--flex vads-u-flex-direction--row vads-u-align-items--center">
+        {/* Sign in button */}
+        <UserNav isHeaderV2 />
 
-      {/* Mobile menu button */}
-      <button
-        className="header-menu-button usa-button vads-u-background-color--white vads-u-color--link-default vads-u-padding-y--1 vads-u-padding-x--1p5 vads-u-margin--0 vads-u-margin-left--2"
-        type="button"
-      >
-        Menu
-        <i
-          aria-hidden="true"
-          className="fa fa-bars vads-u-margin-left--1 vads-u-font-size--sm"
-        />
-      </button>
+        {/* Mobile menu button */}
+        <button
+          className="header-menu-button usa-button vads-u-background-color--white vads-u-color--link-default vads-u-padding-y--1 vads-u-padding-x--1p5 vads-u-margin--0 vads-u-margin-left--2"
+          type="button"
+        >
+          Menu
+          <i
+            aria-hidden="true"
+            className="fa fa-bars vads-u-margin-left--1 vads-u-font-size--sm"
+          />
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default LogoRow;
