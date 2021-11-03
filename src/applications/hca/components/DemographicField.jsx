43d99800d78f97efa384ece583a43ce6e92fefbc
Patrick <patrickryll@yahoo.com>
2021-11-03 15:04:52 -0400

    remove linting toggles

--- src/applications/hca/components/DemographicField.jsx
+++ src/applications/hca/components/DemographicField.jsx
@@ -1,21 +1,17 @@
-/* eslint-disable no-console */
 import React from 'react';
 
 export default function DemographicField(props) {
-  console.log(props, '--> props');
   const { formContext, uiSchema, schema, formData } = props;
   const ObjectField = props.registry.fields.ObjectField;
 
   if (!formContext.reviewMode) {
-    console.log('if block');
     return <ObjectField {...props} />;
   }
 
   const categories = Object.keys(schema.properties).filter(
     prop => formData[prop],
   );
-  // eslint-disable-next-line no-console
-  console.log(categories);
+
   return (
     <>
       <div className="review-row">
