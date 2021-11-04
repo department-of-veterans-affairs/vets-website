0c9b50634e34d84b78d206c3696e65d15ffb09f7
Brian Seek <brian.seek@civicactions.com>
2021-11-04 10:08:19 -0700

    Created commands to minimize repeated cypress code. (#19278)
    * Created commands to minimize repeated cypress code.
    
    * Added a fetch for appointments to NOK tests.
    
    * Added versioning to checkin cypress commands.
    
    * Added visit with UUID command to replace repeated UUID path.
    
    * Moved default UUID to originate from patient checkin response mock.

--- src/applications/check-in/tests/e2e/phase-3-multiple-appointments/errors/server.404.on.validate.cypress.spec.js
+++ src/applications/check-in/tests/e2e/phase-3-multiple-appointments/errors/server.404.on.validate.cypress.spec.js
@@ -1,5 +1,6 @@
 import { generateFeatureToggles } from '../../../../api/local-mock-api/mocks/feature.toggles';
 import mockSession from '../../../../api/local-mock-api/mocks/v2/sessions.responses';
+import '../../support/commands';
 
 describe('Check In Experience -- ', () => {
   beforeEach(function() {
@@ -28,9 +29,7 @@ describe('Check In Experience -- ', () => {
     });
   });
   it('C5732 - Validate - 404 error', () => {
-    const featureRoute =
-      '/health-care/appointment-check-in/?id=46bebc0a-b99c-464f-a5c5-560bc9eae287';
-    cy.visit(featureRoute);
+    cy.visitWithUUID();
     cy.url().should('match', /error/);
     cy.get('h1').contains('We couldn’t check you in');
   });
