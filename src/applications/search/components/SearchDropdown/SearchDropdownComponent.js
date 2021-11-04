db210ef596ef2c6628c27ac8bee0c671a308dc6f
Sam Suddath <srsuddath@gmail.com>
2021-11-04 12:31:27 -0400

    refactor styles and add className props

--- src/applications/search/components/SearchDropdown/SearchDropdownComponent.js
+++ src/applications/search/components/SearchDropdown/SearchDropdownComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './SearchDropdownStyles.scss';
 
 import IconSearch from '@department-of-veterans-affairs/component-library/IconSearch';
 
