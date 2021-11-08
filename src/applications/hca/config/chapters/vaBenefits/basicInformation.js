e773a536618d82f44c7cc7742a7107efbfe28834
Craig Wheeler <craig@craigwheeler.net>
2021-11-08 10:02:58 -0500

    initial commit

--- src/applications/hca/config/chapters/vaBenefits/basicInformation.js
+++ src/applications/hca/config/chapters/vaBenefits/basicInformation.js
@@ -1,23 +1,56 @@
-import fullSchemaHca from 'vets-json-schema/dist/10-10EZ-schema.json';
+import React from 'react';
+import AdditionalInfo from '@department-of-veterans-affairs/component-library/AdditionalInfo';
 import PrefillMessage from 'platform/forms/save-in-progress/PrefillMessage';
 
-const { vaCompensationType } = fullSchemaHca.properties;
+const CompInfo = (
+  <div className="vads-u-margin-top--2 vads-u-margin-bottom--5">
+    <AdditionalInfo triggerText="Why we ask for this information">
+      <p>We use this information to help us decide these 4 things:</p>
+      <ul>
+        <li className="vads-u-margin-left--3 vads-u-margin-bottom--2 bullet-disc">
+          If you can fill out a shorter application, <strong>and</strong>
+        </li>
+        <li className="vads-u-margin-left--3 vads-u-margin-bottom--2 bullet-disc">
+          What types of VA health care benefits you’re eligible for,
+          <strong className="vads-u-margin-left--0p5">and</strong>
+        </li>
+        <li className="vads-u-margin-left--3 vads-u-margin-bottom--2 bullet-disc">
+          How soon we enroll you in VA health care, <strong>and</strong>
+        </li>
+        <li className="vads-u-margin-left--3 bullet-disc">
+          How much (if anything) you’ll have to pay toward the cost of your care
+        </li>
+      </ul>
+      <p>
+        We give veterans with service-connected disabilities the highest
+        priority.
+      </p>
+    </AdditionalInfo>
+  </div>
+);
 
 export default {
   uiSchema: {
-    'ui:title': 'Current compensation',
+    'ui:title': 'Current compensation from VA',
     'ui:description': PrefillMessage,
+    'view:compDesc': {
+      'ui:description':
+        'VA disability compensation (pay) provides monthly payments to Veterans with service-connected disabilities. You may get this benefit if you got sick or injured, or had a condition that got worse, because of your active-duty service. We assign a disability rating based on the severity of your disability.',
+      'ui:options': {
+        classNames: 'vads-u-margin-bottom--4',
+      },
+    },
     vaCompensationType: {
-      'ui:title': 'Which type of VA compensation do you currently receive?',
+      'ui:title': 'Do you receive VA disability compensation?',
+      'ui:description': CompInfo,
       'ui:widget': 'radio',
       'ui:options': {
         labels: {
           lowDisability:
-            'Service-connected disability pay for a 10%, 20%, 30%, or 40% disability rating',
+            'Yes, for service-connected disability rating of up to 40%',
           highDisability:
-            'Service-connected disability pay for a 50% or higher disability rating',
-          pension: 'VA pension',
-          none: 'I don’t receive any VA pay',
+            'Yes, for a service-connected disability rating of 50% or higher',
+          none: 'No',
         },
       },
     },
@@ -26,7 +59,14 @@ export default {
     type: 'object',
     required: ['vaCompensationType'],
     properties: {
-      vaCompensationType,
+      'view:compDesc': {
+        type: 'object',
+        properties: {},
+      },
+      vaCompensationType: {
+        type: 'string',
+        enum: ['lowDisability', 'highDisability', 'none'],
+      },
     },
   },
 };
