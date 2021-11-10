6c69ffc11f3d09a1938d4a2c403cd37765792367
Darius D <ddzien2@gmail.com>
2021-11-10 08:57:58 -0600

    Revert "Rebalance Cypress parallelization on GitHub Actions (#19348)"
    This reverts commit 88a9f5748d2a2bf2dbde0cec02e819e055c69938.

--- script/github-actions/run-cypress-tests.js
+++ script/github-actions/run-cypress-tests.js
@@ -2,18 +2,15 @@ const { runCommandSync } = require('../utils');
 
 const tests = JSON.parse(process.env.TESTS);
 const step = Number(process.env.STEP);
-const divider = Math.ceil(tests.length / 8);
+const divider = Math.ceil(tests.length / Number(process.env.NUM_CONTAINERS));
 
 const batch = tests
   .map(test => test.replace('/home/runner/work', '/__w'))
   .slice(step * divider, (step + 1) * divider)
   .join(',');
 
-if (batch !== '') {
-  const status = runCommandSync(
-    `yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --spec '${batch}'`,
-  );
-  process.exit(status);
-} else {
-  process.exit(0);
-}
+const status = runCommandSync(
+  `yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --spec '${batch}'`,
+);
+
+process.exit(status);
