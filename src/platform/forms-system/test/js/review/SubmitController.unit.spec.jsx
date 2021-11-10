b6998eafc0f2a8ec430a5c8370eabcb114f96992
AFSMWX9K3EV6YHQ <adam.freemer@afsmwx9k3ev6yhq.localdomain>
2021-11-10 10:29:02 -0800

    Update SubmitController unit spec with test data

--- src/platform/forms-system/test/js/review/SubmitController.unit.spec.jsx
+++ src/platform/forms-system/test/js/review/SubmitController.unit.spec.jsx
@@ -24,6 +24,9 @@ const createformReducer = (options = {}) =>
 // Return fresh objects from templates for use with individual tests
 // Default setup: Valid (but empty) form, privacy agreement not set
 const createFormConfig = options => ({
+  ariaDescribedBy: {
+    submitSection: '22-0994-submit-application',
+  },
   urlPrefix: '/',
   trackingPrefix: 'test-',
   prefillEnabled: true,
