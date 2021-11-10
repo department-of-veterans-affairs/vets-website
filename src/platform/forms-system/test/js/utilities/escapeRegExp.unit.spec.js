88744bd5a27266d60e29bcc4b671b821c58484ee
Robin Garrison <robin.garrison@adhocteam.us>
2021-11-10 14:39:26 -0600

    Remove disabling eslint comments

--- src/platform/forms-system/test/js/utilities/escapeRegExp.unit.spec.js
+++ src/platform/forms-system/test/js/utilities/escapeRegExp.unit.spec.js
@@ -5,13 +5,11 @@ import escapeRegExp from 'platform/forms-system/src/js/utilities/data/escapeRegE
 describe('escapeRegExp', () => {
   it('should escape regular expression special characters', () => {
     const raw = '[]{}()*+?.^$|';
-    // eslint-disable-next-line no-useless-escape
     const result = '\\[\\]\\{\\}\\(\\)\\*\\+\\?\\.\\^\\$\\|';
     expect(escapeRegExp(raw)).to.equal(result);
   });
   it('should escape regular expression escaped characters', () => {
     const raw = '\\s and \\';
-    // eslint-disable-next-line no-useless-escape
     const result = '\\\\s and \\\\';
     expect(escapeRegExp(raw)).to.equal(result);
   });
