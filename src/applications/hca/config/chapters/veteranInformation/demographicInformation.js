fd0fe97219608679ba1c613f58177860b179bb22
Justin Linn <justinslinn727@gmail.com>
2021-11-12 10:51:55 -0500

    Make demographicInfo match mock

--- src/applications/hca/config/chapters/veteranInformation/demographicInformation.js
+++ src/applications/hca/config/chapters/veteranInformation/demographicInformation.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import fullSchemaHca from 'vets-json-schema/dist/10-10EZ-schema.json';
 import PrefillMessage from 'platform/forms/save-in-progress/PrefillMessage';
 
@@ -12,13 +13,31 @@ const {
   isWhite,
 } = fullSchemaHca.properties;
 
+const DemographicInfoDescription = props => {
+  return (
+    <>
+      <PrefillMessage {...props} />
+
+      <div>
+        <p className="vads-u-margin-bottom--1">
+          What is your race, ethnicity, or origin? (Please check all that
+          apply.)
+        </p>
+
+        <p className="vads-u-color--gray-medium vads-u-margin-top--0 vads-u-margin-bottom--0">
+          Information is gathered for statistical purposes only.
+        </p>
+      </div>
+    </>
+  );
+};
+
 export default {
   uiSchema: {
-    'ui:description': PrefillMessage,
+    'ui:description': DemographicInfoDescription,
     'view:demographicCategories': {
       'ui:field': DemographicField,
-      'ui:title': 'Which categories best describe you?',
-      'ui:description': 'You may check more than one.',
+      'ui:title': ' ',
       isSpanishHispanicLatino: {
         'ui:title': 'Spanish, Hispanic, or Latino',
       },
