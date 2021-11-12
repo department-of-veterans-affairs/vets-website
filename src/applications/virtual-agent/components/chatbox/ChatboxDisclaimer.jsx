25e9b8aeadb65c9c98b74660b4971619b41654df
Joseph Hall <joe.hall@thoughtworks.com>
2021-11-12 16:03:32 -0500

    [department-of-veterans-affairs/va-virtual-agent#295] matches dialog styling to that of the VA design library
    Co-authored-by: Jacob Gacek <jacob.gacek@thoughtworks.com>

--- src/applications/virtual-agent/components/chatbox/ChatboxDisclaimer.jsx
+++ src/applications/virtual-agent/components/chatbox/ChatboxDisclaimer.jsx
@@ -5,28 +5,32 @@ import { ACCEPTED } from '../../actions';
 export const ChatboxDisclaimer = () => {
   const dispatch = useDispatch();
   return (
-    <div data-testid={'disclaimer'} style={{ height: '550px', width: '100%' }}>
-      <ul>
-        <li>
-          This virtual agent is still in development and cannot help with
-          personal, medical or mental health emergencies. Thank you for
-          understanding.
-        </li>
-        <li>
-          We keep a record of all virtual agent conversations, so we ask that
-          you do not enter personal information that can be used to identify
-          you.
-        </li>
-      </ul>
-      <button
-        id="btnAcceptDisclaimer"
-        data-testid="btnAcceptDisclaimer"
-        className={'usa-button-primary'}
-        onClick={() => dispatch({ type: ACCEPTED })}
-      >
-        Accept
-      </button>
-    </div>
+    <va-alert status="info">
+      <h3 slot="headline">Disclaimer</h3>
+
+      <div data-testid={'disclaimer'} style={{ width: '100%' }}>
+        <ul>
+          <li>
+            This virtual agent is still in development and cannot help with
+            personal, medical or mental health emergencies. Thank you for
+            understanding.
+          </li>
+          <li>
+            We keep a record of all virtual agent conversations, so we ask that
+            you do not enter personal information that can be used to identify
+            you.
+          </li>
+        </ul>
+        <button
+          id="btnAcceptDisclaimer"
+          data-testid="btnAcceptDisclaimer"
+          className={'usa-button-primary'}
+          onClick={() => dispatch({ type: ACCEPTED })}
+        >
+          Accept
+        </button>
+      </div>
+    </va-alert>
   );
 };
 
