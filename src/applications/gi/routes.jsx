868366e57276791edc05ebf10a940bb04d15881f
Sebastian S.A. Cooper <sebastian@kvortex.com>
2021-11-12 12:04:28 -0600

    Rerouting and faking a flag
    Setting up a ternary expression that routes to the sandbox version of CT
    
    (Sandbox version not quite showing up yet even if routed but fake flag works and only shows one or the other when appropriate)

--- src/applications/gi/routes.jsx
+++ src/applications/gi/routes.jsx
@@ -1,33 +1,65 @@
-import GiBillApp from './containers/GiBillApp';
-import LandingPage from './containers/LandingPage';
-import SearchPage from './containers/SearchPage';
-import VetTecSearchPage from './containers/VetTecSearchPage';
-import ProfilePage from './containers/ProfilePage';
-import { Route, Switch } from 'react-router-dom';
+// Legacy Pages
+import LegacyGiBillApp from './containers/GiBillApp';
+import LegacyLandingPage from './containers/LandingPage';
+import LegacySearchPage from './containers/SearchPage';
+import LegacyVetTecSearchPage from './containers/VetTecSearchPage';
+import LegacyProfilePage from './containers/ProfilePage';
+
+// Redesigned Pages
+import GiBillApp from '../gi-sandbox/containers/GiBillApp';
+import SearchPage from '../gi-sandbox/containers/SearchPage';
+import ComparePage from '../gi-sandbox/containers/ComparePage';
+import ProfilePage from '../gi-sandbox/containers/ProfilePage';
+
+// Routing
+import { Route, Switch, Redirect } from 'react-router-dom';
 import React from 'react';
 
+const fakeFlag = true;
+
 export const buildRoutes = () => {
-  return (
+  return fakeFlag ? (
     <GiBillApp>
+      <Switch>
+        <Redirect
+          from="/profile/:facilityCode"
+          to="/institution/:facilityCode"
+        />
+        <Route
+          path="/institution/:facilityCode"
+          render={({ match }) => <ProfilePage match={match} />}
+        />
+        <Route
+          path="/compare"
+          render={({ match }) => <ComparePage match={match} />}
+        />
+        <Route path="/" render={({ match }) => <SearchPage match={match} />} />
+      </Switch>
+    </GiBillApp>
+  ) : (
+    <LegacyGiBillApp>
       <Switch>
         <Route
           path="/search"
-          render={({ match }) => <SearchPage match={match} />}
+          render={({ match }) => <LegacySearchPage match={match} />}
         />
         <Route
           path="/program-search"
-          render={({ match }) => <VetTecSearchPage match={match} />}
+          render={({ match }) => <LegacyVetTecSearchPage match={match} />}
         />
         <Route
           path="/profile/:facilityCode/:preSelectedProgram"
-          render={({ match }) => <ProfilePage match={match} />}
+          render={({ match }) => <LegacyProfilePage match={match} />}
         />
         <Route
           path="/profile/:facilityCode"
-          render={({ match }) => <ProfilePage match={match} />}
+          render={({ match }) => <LegacyProfilePage match={match} />}
         />
-        <Route path="/" render={({ match }) => <LandingPage match={match} />} />
+        <Route
+          path="/"
+          render={({ match }) => <LegacyLandingPage match={match} />}
+        />
       </Switch>
-    </GiBillApp>
+    </LegacyGiBillApp>
   );
 };
