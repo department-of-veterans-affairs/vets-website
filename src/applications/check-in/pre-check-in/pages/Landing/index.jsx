adbd5262a3c25729389db001d4dac9110b5e7c56
Mark Dewey <mdewey@governmentcio.com>
2021-11-16 10:40:49 -0500

    (feat) updated to use a configured array

--- src/applications/check-in/pre-check-in/pages/Landing/index.jsx
+++ src/applications/check-in/pre-check-in/pages/Landing/index.jsx
@@ -2,21 +2,24 @@ import React, { useEffect } from 'react';
 
 import { useDispatch } from 'react-redux';
 
+import { PRE_CHECK_IN_FORM_PAGES } from '../../utils/navigation';
+
 export default function Index(props) {
   const dispatch = useDispatch();
+
   useEffect(
     () => {
       const { router } = props;
-
+      const firstPage = PRE_CHECK_IN_FORM_PAGES[0].url;
       dispatch({
         type: 'INIT_FORM',
         payload: {
-          pages: ['verify', 'introduction', 'contact-information'],
-          currentPage: 'verify',
+          pages: PRE_CHECK_IN_FORM_PAGES.map(page => page.url),
+          currentPage: firstPage,
         },
       });
 
-      router.push('verify');
+      router.push(firstPage);
     },
     [props, dispatch],
   );
--- src/applications/check-in/pre-check-in/utils/navigation/index.js
+++ src/applications/check-in/pre-check-in/utils/navigation/index.js
@@ -9,4 +9,27 @@ const URLS = Object.freeze({
   VERIFY: 'verify',
 });
 
-export { URLS };
+const PRE_CHECK_IN_FORM_PAGES = Object.freeze([
+  {
+    url: URLS.VERIFY,
+    order: 0,
+  },
+  {
+    url: URLS.INTRODUCTION,
+    order: 1,
+  },
+  {
+    url: URLS.DEMOGRAPHICS,
+    order: 2,
+  },
+  {
+    url: URLS.NEXT_OF_KIN,
+    order: 4,
+  },
+  {
+    url: URLS.CONFIRMATION,
+    order: 5,
+  },
+]);
+
+export { URLS, PRE_CHECK_IN_FORM_PAGES };
