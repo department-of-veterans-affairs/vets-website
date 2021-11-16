6e40d9817cc776774f80480ef28f024ae08a63dd
Tim Wright <timwright12@gmail.com>
2021-11-16 15:30:17 -0500

    Move bam cypress test (#19423)
    * Moved HCA Cypress Test
    
    * Moved HCA Cypress Test
    
    * merge in feature/32795-move-hca-cypress-test
    
    * Moved BAM Cypress Test

--- src/applications/burials/tests/e2e/application-status.cypress.spec.js
+++ src/applications/burials/tests/e2e/application-status.cypress.spec.js
@@ -1,17 +1,9 @@
 import mockUser from './fixtures/mocks/mockUser';
 
-describe('Application Status Test', () => {
+describe('Application Status Test: Burials and Memorials', () => {
   it('Achieves the correct result per URL', () => {
     cy.login();
     cy.intercept('GET', '/v0/user', mockUser);
-    cy.testStatus(
-      '/health-care/how-to-apply/',
-      '/health-care/apply/application/resume',
-    );
-    cy.testStatus(
-      '/health-care/eligibility',
-      '/health-care/apply/application/resume',
-    );
     cy.testStatus(
       '/burials-memorials/veterans-burial-allowance/',
       '/burials-and-memorials/application/530/resume',
