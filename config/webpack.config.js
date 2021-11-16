09ebd1757605f0e4fff37373d5d1fdb63cf63277
jhonnyoddball <jhonny.gonzalez@oddball.io>
2021-11-16 08:23:19 -0500

    change terser parallel option

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -385,7 +385,7 @@ module.exports = async (env = {}) => {
             },
             warnings: false,
           },
-          parallel: 3,
+          parallel: true,
         }),
       ],
       splitChunks: {
