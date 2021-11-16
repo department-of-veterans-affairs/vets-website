9bb8c2b19286a90f9276adb04a99140842c38c25
Justin Linn <justinslinn727@gmail.com>
2021-11-16 16:34:36 -0500

    [25411]: Add Self-Identifying Gender Identity (SIGI) to 10-10EZ (#19337)
    * Wire in HCA schema changes to veteranGender page
    
    * Fix veteran-gender labels
    
    * Add feature flip on SIGI Veteran sex page
    
    * Change checkboxes to radio options
    
    * Add custom review component
    
    * Fix birthSex order
    
    * Add feature_toggles mock to HCA cypress test as well as optional data to maximal test
    
    * Fix design feedback
    
    * Update schema to master branch on vets-json-schema

--- src/applications/hca/HealthCareApp.jsx
+++ src/applications/hca/HealthCareApp.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-
+import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
+import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
+import { setData } from 'platform/forms-system/src/js/actions';
 import formConfig from './config/form';
 
-const HealthCareEntry = ({ location, children }) => {
+const HealthCareEntry = ({
+  location,
+  children,
+  caregiverSIGIEnabled = false,
+  setFormData,
+  formData,
+}) => {
+  const getFlipperData = useCallback(
+    () => {
+      if (caregiverSIGIEnabled !== undefined) {
+        setFormData({
+          ...formData,
+          'view:caregiverSIGIEnabled': caregiverSIGIEnabled,
+        });
+      }
+    },
+    [caregiverSIGIEnabled],
+  );
+
+  useEffect(
+    () => {
+      getFlipperData();
+    },
+
+    [getFlipperData, caregiverSIGIEnabled],
+  );
+
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
@@ -12,4 +42,24 @@ const HealthCareEntry = ({ location, children }) => {
   );
 };
 
-export default HealthCareEntry;
+const mapStateToProps = state => ({
+  formData: state.form.data,
+  caregiverSIGIEnabled: toggleValues(state)[
+    FEATURE_FLAG_NAMES.caregiverSIGIEnabled
+  ],
+});
+
+const mapDispatchToProps = {
+  setFormData: setData,
+};
+
+HealthCareEntry.propTypes = {
+  caregiverSIGIEnabled: PropTypes.bool,
+  setFormData: PropTypes.func,
+  formData: PropTypes.object,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(HealthCareEntry);
