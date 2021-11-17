f54faff68d9004c7547e201dbd7e2c2ee12291c1
Brooks Johnson <2008881+bkjohnson@users.noreply.github.com>
2021-11-17 15:55:49 -0800

    Add transformer for loading indicator migration (#19295)
    * Add initial transformer
    
    * Add new transform to index
    
    * Kebab case the enableAnalytics prop

--- script/component-migration/transformers/index.js
+++ script/component-migration/transformers/index.js
@@ -1,5 +1,8 @@
 const alertBoxReplacement = require('./alertbox').alertBoxReplacement;
+const loadingIndicatorReplacement = require('./loadingindicator')
+  .loadingIndicatorReplacement;
 
 module.exports = {
   AlertBox: alertBoxReplacement,
+  LoadingIndicator: loadingIndicatorReplacement,
 };
