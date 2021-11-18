6af0c3375da7431ccb45b0fd61630f4499b5064e
Tony Williams <tony.williams@va.gov>
2021-11-18 13:05:10 -0600

    Modified per code review

--- src/applications/vaos/covid-19-vaccine/components/PlanAheadPage.jsx
+++ src/applications/vaos/covid-19-vaccine/components/PlanAheadPage.jsx
@@ -10,6 +10,7 @@ import { GA_PREFIX } from '../../utils/constants';
 import { scrollAndFocus } from '../../utils/scrollAndFocus';
 import FormButtons from '../../components/FormButtons';
 import { selectPageChangeInProgress } from '../redux/selectors';
+import NewTabAnchor from '../../components/NewTabAnchor';
 
 const pageKey = 'planAhead';
 const pageTitle = 'COVID-19 vaccine appointment';
@@ -36,16 +37,22 @@ export default function PlanAheadPage() {
           If you’re eligible for a booster shot or additional dose, contact your
           VA health facility.
         </li>
-        <li>
-          If you’re eligible for a booster shot or additional dose, contact your
-          VA health facility.
-        </li>
       </ul>
 
       <p>
         Want to get your vaccine without an appointment?
         <br />
-        Find out how to get your vaccine at a VA walk-in clinic
+        <NewTabAnchor
+          href="/health-care/covid-19-vaccine"
+          className="vads-u-margin-top--2"
+          onClick={() => {
+            recordEvent({
+              event: `${GA_PREFIX}-COVID-19-vaccines-at-VA-link-clicked`,
+            });
+          }}
+        >
+          Find out how to get your vaccine at a VA walk-in clinic
+        </NewTabAnchor>
       </p>
       <FormButtons
         pageChangeInProgress={pageChangeInProgress}
