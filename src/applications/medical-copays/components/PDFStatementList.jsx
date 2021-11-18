04d1583782cc87822899d971a429c0381193af35
Justin Linn <justinslinn727@gmail.com>
2021-11-18 12:58:07 -0500

    Filter PDF statements by facility

--- src/applications/medical-copays/components/PDFStatementList.jsx
+++ src/applications/medical-copays/components/PDFStatementList.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 import DownloadStatements from './DownloadStatement';
 
 const PDFStatementList = () => {
-  const statements = useSelector(({ mcp }) => mcp.statements);
+  const { pathname } = useLocation();
+  const selectedId = pathname.replace('/balance-details/', '');
   const userFullName = useSelector(({ user }) => user.profile.userFullName);
+  const statements = useSelector(({ mcp }) => mcp.statements);
 
+  // get selected statement
+  const [selectedCopay] = statements?.filter(({ id }) => id === selectedId);
+  // get facility  number on selected statement
+  const facilityNumber = selectedCopay?.pSFacilityNum;
+  // filter out all statements that are not related to this facility
+  const facilityCopays = statements?.filter(
+    ({ pSFacilityNum }) => pSFacilityNum === facilityNumber,
+  );
   const fullName = userFullName.middle
     ? `${userFullName.first} ${userFullName.middle} ${userFullName.last}`
     : `${userFullName.first} ${userFullName.last}`;
@@ -19,7 +30,7 @@ const PDFStatementList = () => {
         months.
       </p>
 
-      {statements.map(statement => (
+      {facilityCopays.map(statement => (
         <DownloadStatements
           key={statement.id}
           statementId={statement.id}
