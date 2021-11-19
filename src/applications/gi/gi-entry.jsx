360dddc508478fb12ad9d5decda7ec336c1d7ba9
Sebastian S.A. Cooper <sebastian@kvortex.com>
2021-11-19 09:28:29 -0600

    Put feature flag back in
    gi_sandbox_comparision_tool_toggle is again the determiner of whether the sandbox redesigned is served or not.

--- src/applications/gi/gi-entry.jsx
+++ src/applications/gi/gi-entry.jsx
@@ -1,5 +1,5 @@
 import 'platform/polyfills';
-// import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
+import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
 import './sass/gi.scss';
 import '../gi-sandbox/sass/gi.scss';
 import startApp from 'platform/startup/router';
@@ -10,8 +10,7 @@ import manifest from './manifest.json';
 import { buildRoutes as redesignedRoutes } from '../gi-sandbox/routes';
 import redesignedReducer from '../gi-sandbox/reducers';
 
-// const redesignFlag = toggleValues(0).gi_sandbox_comparision_tool_toggle;
-const redesignFlag = true;
+const redesignFlag = toggleValues(0).gi_sandbox_comparision_tool_toggle;
 
 startApp(
   redesignFlag
