cb894e92021d733a3216bff1beebca673728c297
Sebastian S.A. Cooper <sebastian@kvortex.com>
2021-11-22 12:51:35 -0600

    changed feature flag to correct name
    was using the  api name (underscored) instead of the referenceable name (camelCase).

--- src/applications/gi/gi-entry.jsx
+++ src/applications/gi/gi-entry.jsx
@@ -10,7 +10,7 @@ import manifest from './manifest.json';
 import { buildRoutes as buildRedesignedRoutes } from '../gi-sandbox/routes';
 import redesignedReducer from '../gi-sandbox/reducers';
 
-const redesignFlag = toggleValues(0).gi_sandbox_comparision_tool_toggle;
+const redesignFlag = toggleValues(0).giSandboxComparisonToolToggle;
 const page = redesignFlag
   ? { reducer: redesignedReducer, routes: buildRedesignedRoutes() }
   : { reducer, routes: buildRoutes() };
