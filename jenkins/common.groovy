1e0d9e91ff4a688df56cf0545a8bac05e327d67e
Tim Wright <timwright12@gmail.com>
2021-11-23 10:22:50 -0500

    added --scaffold to Jenkins for e2e

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -103,7 +103,7 @@ def build(String ref, dockerContainer, String envName) {
   dockerContainer.inside(DOCKER_ARGS) {
     def buildLogPath = "/application/${envName}-build.log"
 
-    sh "cd /application && jenkins/build.sh --envName ${envName} --buildLog ${buildLogPath} --verbose"
+    sh "cd /application && jenkins/build.sh --envName ${envName} --buildLog ${buildLogPath} --verbose --scaffold"
   }
 }
 
