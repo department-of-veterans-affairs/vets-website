45a8211b5c6d97196a90111de92d88031d12d33e
Theo Bentum <theo@adhocteam.us>
2021-11-23 14:02:02 -0800

    Add test for url option

--- script/github-actions/get-changed-apps.unit.spec.js
+++ script/github-actions/get-changed-apps.unit.spec.js
@@ -92,6 +92,14 @@ describe('getChangedAppsString', () => {
     expect(appString).to.equal('src/applications/app1,src/applications/app2');
   });
 
+  it('should return a comma-delimited string of app URLs when the url output type is specified', () => {
+    const config = { allow: ['app1', 'app2'] };
+    const changedFiles = ['src/applications/app1', 'src/applications/app2'];
+
+    const appString = getChangedAppsString(changedFiles, config, 'url');
+    expect(appString).to.equal('/app1,/app2');
+  });
+
   it('should not duplicate entry names when multiple files in an app are modified', () => {
     const config = { allow: ['app1'] };
     const changedFiles = [
