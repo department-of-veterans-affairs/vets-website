cb946ee233def3dc1310431bb9b966d93dbca2fa
Tim Wright <timwright12@gmail.com>
2021-11-23 11:16:47 -0500

    move archive before test and change archive path

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -122,7 +122,7 @@ def archive(dockerContainer, String ref, String envName) {
       // }
       if(envName == 'vagovprod') {
         sh "tar -C /application/build/${envName} -cf /application/build/${envName}.tar.bz2 ."
-        sh "aws s3 cp /application/build/${envName}.tar.bz2 s3://vetsgov-website-builds-s3-upload/${ref}/${envName}.tar.bz2 --acl public-read --region us-gov-west-1 --quiet"
+        sh "aws s3 cp /application/build/${envName}.tar.bz2 s3://vetsgov-website-builds-s3-upload/jenkins/${ref}/${envName}.tar.bz2 --acl public-read --region us-gov-west-1 --quiet"
       }
     }
   }
