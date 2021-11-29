f93e3e92acd64d0a48831cdd000ba97a915965e4
Mark Dewey <mdewey@governmentcio.com>
2021-11-29 16:33:55 -0500

    (feature) added loading  of data on introduction page

--- src/applications/check-in/pre-check-in/pages/Introduction/index.jsx
+++ src/applications/check-in/pre-check-in/pages/Introduction/index.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import IntroductionDisplay from './IntroductionDisplay';
+
+import { api } from '../../api';
 
 // @TODO Remove appointments once mock API merged in. Add cypress test for intro.
 const Introduction = props => {
   const { router } = props;
-  return <IntroductionDisplay router={router} />;
+  const [isLoading, setIsLoading] = useState(false);
+  // select token from redux store
+
+  useEffect(() => {
+    // show loading screen
+    setIsLoading(true);
+    //  call get data from API
+    api.v2.getPreCheckInData().then(() => {
+      //  set data to state
+      // hide loading screen
+      setIsLoading(false);
+    });
+  }, []);
+  if (isLoading) {
+    return <va-loading-indicator message="Loading your appointment details" />;
+  } else {
+    return <IntroductionDisplay router={router} />;
+  }
 };
 
 Introduction.propTypes = {
