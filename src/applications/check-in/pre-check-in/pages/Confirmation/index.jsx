97d52809571c1f15aeecf52d9449a3321ed21aa9
dcgoodwin2112 <dcgoodwin2112@gmail.com>
2021-11-30 09:19:28 -0500

    Add confimaiton page content

--- src/applications/check-in/pre-check-in/pages/Confirmation/index.jsx
+++ src/applications/check-in/pre-check-in/pages/Confirmation/index.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import BackToHome from '../../components/BackToHome';
+import React, { useEffect } from 'react';
+import { focusElement } from 'platform/utilities/ui';
 
-export default function index() {
+const Confirmation = () => {
+  useEffect(() => {
+    focusElement('h1');
+  }, []);
   return (
-    <>
-      <h1>Confirmation</h1>
-      <BackToHome />
-    </>
+    <div className="vads-l-grid-container vads-u-padding-bottom--5 vads-u-padding-top--2">
+      <h1>You’ve completed pre check-in</h1>
+      <p className="vads-u-font-family--serif">Placeholder</p>
+      {/* TODO APPOINTMENT LIST */}
+      {/* TODO GO TO APPOINTMENT LINKS */}
+      <p>Please bring your insurance cards with you to your appointment.</p>
+      <h3>What if I have questions about my appointment?</h3>
+      <p>Call your VA health care team:</p>
+      {/* TODO FACILITY PHONE LINKS */}
+    </div>
   );
-}
+};
+
+export default Confirmation;
