dba4d9e6c2131ce610231c0972fb99b622c296e5
dcgoodwin2112 <dcgoodwin2112@gmail.com>
2021-12-01 17:39:36 -0500

    Remove MOCK data and integrate with Redux

--- src/applications/check-in/pre-check-in/pages/Confirmation/index.jsx
+++ src/applications/check-in/pre-check-in/pages/Confirmation/index.jsx
@@ -1,64 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+
 import { focusElement } from 'platform/utilities/ui';
-import AppointmentBlock from '../../components/AppointmentBlock';
 import Telephone from '@department-of-veterans-affairs/component-library/Telephone';
 
-/**
- * MOCK DATA
- */
-
-const mockTime = new Date();
-const appointments = [
-  {
-    facility: 'LOMA LINDA VA CLINIC',
-    clinicPhoneNumber: '5551234567',
-    clinicFriendlyName: 'TEST CLINIC',
-    clinicName: 'LOM ACC CLINIC TEST',
-    appointmentIen: 'some-ien',
-    startTime: mockTime,
-    eligibility: 'ELIGIBLE',
-    facilityId: 'some-facility',
-    checkInWindowStart: mockTime,
-    checkInWindowEnd: mockTime,
-    checkedInTime: '',
-  },
-  {
-    facility: 'LOMA LINDA VA CLINIC',
-    clinicPhoneNumber: '5551234567',
-    clinicFriendlyName: 'TEST CLINIC',
-    clinicName: 'LOM ACC CLINIC TEST',
-    appointmentIen: 'some-other-ien',
-    startTime: mockTime,
-    eligibility: 'ELIGIBLE',
-    facilityId: 'some-facility',
-    checkInWindowStart: mockTime,
-    checkInWindowEnd: mockTime,
-    checkedInTime: '',
-  },
-];
-
-const updates = {
-  demographicsUpToDate: true,
-  nextOfKinUpToDate: false,
-};
-
-const hasUpdates = () => {
-  return Object.values(updates).includes(true);
-};
-
-/**
- * END MOCK DATA
- */
+import AppointmentBlock from '../../components/AppointmentBlock';
+import { makeSelectVeteranData, makeSelectForm } from '../../selectors';
 
 const Confirmation = () => {
   useEffect(() => {
     focusElement('h1');
   }, []);
+  const selectVeteranData = useMemo(makeSelectVeteranData, []);
+  const selectFormData = useMemo(makeSelectForm, []);
+  const { appointments } = useSelector(selectVeteranData);
+  const { data: formData } = useSelector(selectFormData);
+  const hasUpdates = Object.values(formData).includes('no');
   return (
     <div className="vads-l-grid-container vads-u-padding-bottom--3 vads-u-padding-top--3">
       <h1>You’ve completed pre check-in</h1>
       <AppointmentBlock appointments={appointments} />
-      {hasUpdates ? (
+      {hasUpdates === true ? (
         <va-alert background-only status="info">
           {/** TODO INFO ICON */}
           <div>
@@ -69,12 +31,12 @@ const Confirmation = () => {
       ) : (
         <></>
       )}
-      <p className={hasUpdates ? `vads-u-padding-left--2` : ``}>
+      <p className={hasUpdates === true ? `vads-u-padding-left--2` : ``}>
         <a href="https://va.gov/health-care/schedule-view-va-appointments/appointments/">
           Go to your appointment
         </a>
       </p>
-      <p className={hasUpdates ? `vads-u-padding-left--2` : ``}>
+      <p className={hasUpdates === true ? `vads-u-padding-left--2` : ``}>
         Please bring your insurance cards with you to your appointment.
       </p>
       <h3>What if I have questions about my appointment?</h3>
