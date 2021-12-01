21ab43c8791db20c476764f877d9f3aefd71c14b
Tony Williams <tony.williams@va.gov>
2021-12-01 10:01:39 -0600

    WIP Fixed alert message for urgent care when there are slots and no slots

--- src/applications/vaos/new-appointment/components/DateTimeSelectPage/WaitTimeAlert.jsx
+++ src/applications/vaos/new-appointment/components/DateTimeSelectPage/WaitTimeAlert.jsx
@@ -11,13 +11,62 @@ import NewTabAnchor from '../../../components/NewTabAnchor';
 import InfoAlert from '../../../components/InfoAlert';
 import { getTimezoneByFacilityId } from '../../../utils/timezone';
 
+function UrgentCareMessage() {
+  return (
+    <>
+      <p className="vads-u-font-weight--bold">
+        If you have an urgent medical need or need care right away:
+      </p>
+      <ul>
+        <li>
+          Call <Telephone contact="911" />,{' '}
+          <span className="vads-u-font-weight--bold">or</span>
+        </li>
+        <li>
+          Call the Veterans Crisis hotline at{' '}
+          <Telephone contact={CONTACTS.CRISIS_LINE} /> and select 1,{' '}
+          <span className="vads-u-font-weight--bold">or</span>
+        </li>
+        <li>Go to your nearest emergency room or VA medical center</li>
+      </ul>
+    </>
+  );
+}
+
+function ActionButtons(props) {
+  const { eligibleForRequests, facilityId, onClickRequest } = props;
+  return (
+    <div className="vads-u-display--flex vads-u-margin-top--2 vads-u-align-items--center">
+      {eligibleForRequests && (
+        <>
+          <Link to={newAppointmentFlow.requestDateTime.url}>
+            <button
+              className="usa-button-secondary vads-u-margin-x--0"
+              onClick={onClickRequest}
+            >
+              Request an earlier appointment
+            </button>
+          </Link>
+          <span className="vads-u-display--inline-block vads-u-margin-x--2p5 vads-u-font-weight--bold">
+            OR
+          </span>
+        </>
+      )}
+      <NewTabAnchor
+        href={`/find-locations/facility/vha_${getRealFacilityId(facilityId)}`}
+      >
+        Contact your local VA medical center
+      </NewTabAnchor>
+    </div>
+  );
+}
+
 export const WaitTimeAlert = ({
   eligibleForRequests,
   facilityId,
   nextAvailableApptDate,
   onClickRequest,
   preferredDate,
-  timezone,
 }) => {
   const today = moment();
   const momentPreferredDate = moment(preferredDate);
@@ -33,7 +82,7 @@ export const WaitTimeAlert = ({
   if (momentPreferredDate.isValid() && momentNextAvailableDate.isValid()) {
     const showUrgentCareMessage = today.isSame(momentPreferredDate, 'day');
 
-    if (showUrgentCareMessage) {
+    if (showUrgentCareMessage && nextAvailableApptDate) {
       return (
         <InfoAlert
           headline="Your appointment time"
@@ -44,7 +93,7 @@ export const WaitTimeAlert = ({
             <p>
               The earliest we can schedule your appointment is{' '}
               {momentNextAvailableDate.format('MMMM D, YYYY')} at{' '}
-              {momentNextAvailableDate.format('h:mm a')} {timezone}.
+              {momentNextAvailableDate.format('h:mm a')}
             </p>
             <p>If this date doesn’t work, you can:</p>
             <ul>
@@ -60,45 +109,37 @@ export const WaitTimeAlert = ({
               </li>
               <li>Call your local VA medical center</li>
             </ul>
-            <p className="vads-u-font-weight--bold">
-              If you have an urgent medical need or need care right away:
-            </p>
+            <UrgentCareMessage />
+            <ActionButtons
+              eligibleForRequests={eligibleForRequests}
+              facilityId={facilityId}
+              onClickRequest={onClickRequest}
+            />
+          </>
+        </InfoAlert>
+      );
+    } else {
+      return (
+        <InfoAlert
+          headline="We couldn’t find an appointment for your selected date"
+          status={showUrgentCareMessage ? 'warning' : 'info'}
+          level={2}
+        >
+          <>
+            <p>To schedule this appointment, you can:</p>
             <ul>
               <li>
-                Call <Telephone contact="911" />,{' '}
+                Call your local VA medical center,{' '}
                 <span className="vads-u-font-weight--bold">or</span>
               </li>
-              <li>
-                Call the Veterans Crisis hotline at{' '}
-                <Telephone contact={CONTACTS.CRISIS_LINE} /> and select 1,{' '}
-                <span className="vads-u-font-weight--bold">or</span>
-              </li>
-              <li>Go to your nearest emergency room or VA medical center</li>
+              <li>Submit a request for another date</li>
             </ul>
-            <div className="vads-u-display--flex vads-u-margin-top--2 vads-u-align-items--center">
-              {eligibleForRequests && (
-                <>
-                  <Link to={newAppointmentFlow.requestDateTime.url}>
-                    <button
-                      className="usa-button-secondary vads-u-margin-x--0"
-                      onClick={onClickRequest}
-                    >
-                      Request an earlier appointment
-                    </button>
-                  </Link>
-                  <span className="vads-u-display--inline-block vads-u-margin-x--2p5 vads-u-font-weight--bold">
-                    OR
-                  </span>
-                </>
-              )}
-              <NewTabAnchor
-                href={`/find-locations/facility/vha_${getRealFacilityId(
-                  facilityId,
-                )}`}
-              >
-                Contact your local VA medical center
-              </NewTabAnchor>
-            </div>
+            <UrgentCareMessage />
+            <ActionButtons
+              eligibleForRequests={eligibleForRequests}
+              facilityId={facilityId}
+              onClickRequest={onClickRequest}
+            />
           </>
         </InfoAlert>
       );
