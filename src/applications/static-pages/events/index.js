ef98c97444c1ae0002ecc495a06a25654feaca8e
Kelson Adams <kadams@governmentcio.com>
2021-12-01 11:25:00 -0700

    Do not lazy load in header v2 for faster rendering time

--- src/applications/static-pages/events/index.js
+++ src/applications/static-pages/events/index.js
@@ -2,25 +2,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+// Related imports.
+import App from './components/App';
 
 export default (store, widgetType) => {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
+
   if (root) {
-    import(/* webpackChunkName: "ask-va" */
-    './components/App').then(module => {
-      const App = module.default;
+    // Derive the props to pass to the widget.
+    const pastEvents = window?.pastEvents?.entities || [];
+    const allEventTeasers = window?.allEventTeasers?.entities || [];
+    const rawEvents = [...pastEvents, ...allEventTeasers];
 
-      // Derive the props to pass to the widget.
-      const pastEvents = window?.pastEvents?.entities || [];
-      const allEventTeasers = window?.allEventTeasers?.entities || [];
-      const rawEvents = [...pastEvents, ...allEventTeasers];
-
-      ReactDOM.render(
-        <Provider store={store}>
-          <App rawEvents={rawEvents} />
-        </Provider>,
-        root,
-      );
-    });
+    ReactDOM.render(
+      <Provider store={store}>
+        <App rawEvents={rawEvents} />
+      </Provider>,
+      root,
+    );
   }
 };
