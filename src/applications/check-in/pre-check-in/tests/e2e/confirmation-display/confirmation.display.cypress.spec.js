dd1cf20f85e7cb12c17ae07edd6e291af34312cc
Dan Goodwin <dcgoodwin2112@gmail.com>
2021-12-02 14:40:40 -0500

    Check in/feat/32777/pre check in confirm page (#19570)
    * Add confimaiton page content
    
    * Update confirm page
    
    * Remove MOCK data and integrate with Redux
    
    * Add confirmation testing
    
    * Add e2e tests for confimation page
    
    * Clean up ternary statements, Update page routing in e2e tests. Remove empty test function

---
+++ src/applications/check-in/pre-check-in/tests/e2e/confirmation-display/confirmation.display.cypress.spec.js
@@ -0,0 +1,53 @@
+import { generateFeatureToggles } from '../../../api/local-mock-api/mocks/feature.toggles';
+import '../support/commands';
+
+import validateVeteran from '../pages/ValidateVeteran';
+import introduction from '../pages/Introduction';
+import Demographics from '../pages/Demographics';
+import NextOfKin from '../pages/NextOfKin';
+import Confirmation from '../pages/Confirmation';
+import apiInitializer from '../support/ApiInitializer';
+
+describe('Pre-Check In Experience', () => {
+  describe('Next of kin Page', () => {
+    beforeEach(function() {
+      cy.intercept(
+        'GET',
+        '/v0/feature_toggles*',
+        generateFeatureToggles({
+          checkInExperienceUpdateInformationPageEnabled: true,
+        }),
+      );
+      apiInitializer.initializeSessionGet.withSuccessfulNewSession();
+
+      apiInitializer.initializeSessionPost.withSuccess();
+      apiInitializer.initializePreCheckInDataGet.withSuccess();
+
+      cy.visitPreCheckInWithUUID();
+      validateVeteran.validateVeteran();
+      validateVeteran.attemptToGoToNextPage();
+      introduction.validatePageLoaded();
+      introduction.attemptToGoToNextPage();
+      Demographics.validatePageLoaded();
+      Demographics.attemptToGoToNextPage();
+      NextOfKin.validatePageLoaded();
+    });
+    afterEach(() => {
+      cy.window().then(window => {
+        window.sessionStorage.clear();
+      });
+    });
+    it('Confirmation page content loads', () => {
+      NextOfKin.attemptToGoToNextPage();
+      Confirmation.validatePageContent();
+    });
+    it('Staff update alert message is not visible', () => {
+      NextOfKin.attemptToGoToNextPage();
+      Confirmation.validateConfirmNoUpdates();
+    });
+    it('Staff update alert message is visible', () => {
+      NextOfKin.attemptToGoToNextPage('no');
+      Confirmation.validateConfirmWithUpdates();
+    });
+  });
+});
