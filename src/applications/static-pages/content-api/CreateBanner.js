ac78f780b575789b5eb881cb206c06fc7db85077
Aaron <aaron.ponce@agile6.com>
2021-12-03 10:31:18 -0800

    VACMS-6863 move the path reference to Create Banner and remove return in Banner

--- src/applications/static-pages/content-api/CreateBanner.js
+++ src/applications/static-pages/content-api/CreateBanner.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CAPI_DOMAIN, BANNER_SOURCE } from './constants/';
+import { CAPI_DOMAIN } from './constants/';
 import { apiRequest } from 'platform/utilities/api';
+
+const path =
+  '/jsonapi/resource/banner-alerts?item-path=/family-member-benefits';
 
 export default async function createBanner(widgetType) {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
-  // If root is not found, do nothing.
   if (!root) {
     return;
   }
   try {
     const module = await import('./components/Banner');
-    const response = await apiRequest(`${CAPI_DOMAIN}${BANNER_SOURCE}`, null);
+    const response = await apiRequest(`${CAPI_DOMAIN}${path}`, null);
     const { data } = await response.json();
     const parsedData = data.map(banner => banner.attributes);
     const Banner = module.default;
