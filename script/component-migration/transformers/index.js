c55ddd6316863b3d1dee5edc9c69f8d10003f2c2
Preston Davies <pdavies88@gmail.com>
2021-12-03 11:04:41 -0500

    32862 Migration Script for Segemented Progress Bar

--- script/component-migration/transformers/index.js
+++ script/component-migration/transformers/index.js
@@ -1,8 +1,11 @@
 const alertBoxReplacement = require('./alertbox').alertBoxReplacement;
 const loadingIndicatorReplacement = require('./loadingindicator')
   .loadingIndicatorReplacement;
+const segmentedProgressBarReplacement = require('./segmentedprogressbar')
+  .segmentedProgressBarReplacement;
 
 module.exports = {
   AlertBox: alertBoxReplacement,
   LoadingIndicator: loadingIndicatorReplacement,
+  SegmentedProgressBar: segmentedProgressBarReplacement,
 };
