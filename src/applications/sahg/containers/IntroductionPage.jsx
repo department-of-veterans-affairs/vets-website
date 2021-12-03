4ea74df83073a0dafd1dd9bba383f9b59c51e51c
jerekshoe <jerek.shoemaker@adhocteam.us>
2021-12-03 12:38:21 -0600

    Updating OMB info

--- src/applications/sahg/containers/IntroductionPage.jsx
+++ src/applications/sahg/containers/IntroductionPage.jsx
@@ -75,7 +75,7 @@ class IntroductionPage extends React.Component {
           startText="Start the Application"
         />
         <div className="omb-info--container" style={{ paddingLeft: '0px' }}>
-          <OMBInfo resBurden={15} ombNumber="26-4555" expDate="12/31/2022" />
+          <OMBInfo resBurden={10} ombNumber="2900-0132" expDate="06/30/2024" />
         </div>
       </div>
     );
