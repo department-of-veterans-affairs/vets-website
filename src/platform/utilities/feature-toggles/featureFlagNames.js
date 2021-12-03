eca65bd76f88f6f11d03476f65ecd9713bca22c3
Jennie McGibney <jennie.mcgibney@va.gov>
2021-12-03 13:28:58 -0500

    add health apt toggle flag

--- src/platform/utilities/feature-toggles/featureFlagNames.js
+++ src/platform/utilities/feature-toggles/featureFlagNames.js
@@ -126,6 +126,7 @@ export default Object.freeze({
     'va_online_scheduling_facilities_service_v2',
   vaOnlineSchedulingVariantTesting: 'va_online_scheduling_variant_testing',
   vaOnlineSchedulingCCIterations: 'va_online_scheduling_cc_iterations',
+  vaOnlineSchedulingPocHealthApt: 'va_online_scheduling_poc_health_apt',
   vaViewDependentsAccess: 'va_view_dependents_access',
   yellowRibbonEnhancements: 'yellow_ribbon_mvp_enhancement',
   showEduBenefits1990EZWizard: 'show_edu_benefits_1990EZ_Wizard',
