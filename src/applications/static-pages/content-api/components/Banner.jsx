d5964ca2ebe22d304539a0541d508fbbb6f42406
Aaron <aaron.ponce@agile6.com>
2021-12-03 01:31:53 -0800

    VACMS-6863 content api banner cleanup and initial unit test

---
+++ src/applications/static-pages/content-api/components/Banner.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Banner = ({ banners }) => {
+  if (!banners) return 'banner not found';
+  return (
+    <>
+      {banners.map(
+        ({ heading, dismissible, alert_type: alertType, text }, index) => {
+          const bodyContent = () => {
+            return { __html: text };
+          };
+          return (
+            <va-alert
+              key={index}
+              visible
+              full-width
+              closeable={dismissible === 'dismiss' ? `true` : `false`}
+              status={alertType}
+            >
+              <h3 slot="headline">{heading}</h3>
+              {/* eslint-disable-next-line react/no-danger */}
+              <div dangerouslySetInnerHTML={bodyContent()} />
+            </va-alert>
+          );
+        },
+      )}
+    </>
+  );
+};
+
+export default Banner;
---
+++ src/applications/static-pages/content-api/constants/index.js
@@ -0,0 +1,4 @@
+export const CAPI_DOMAIN =
+  'https://pr6811-d2ogupvg2fodkdfjyl95sjzwzinh2zev.ci.cms.va.gov';
+export const BANNER_SOURCE =
+  '/jsonapi/resource/banner-alerts?item-path=/family-member-benefits';
