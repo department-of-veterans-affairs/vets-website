187fa2f42b991aa5962314afa7947a54f39bc9ba
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-12-03 08:52:50 -0600

    WIP

--- script/eslint-plugin-va/rules/axe-e2e-tests.js
+++ script/eslint-plugin-va/rules/axe-e2e-tests.js
@@ -35,3 +35,54 @@ const rule = {
 };
 
 module.exports = rule;
+
+// const MESSAGE =
+//   'E2E tests must include at least one axeCheck call. Documentation for adding checks and understanding errors can be found here: https://github.com/department-of-veterans-affairs/va.gov-team/blob/master/platform/engineering/frontend/eslint/axe-check-required.md';
+
+// const rule = {
+//   meta: {
+//     type: 'suggestion',
+//     docs: {
+//       description: MESSAGE,
+//       category: 'best practices',
+//       recommended: false,
+//     },
+//   },
+//   create: context => {
+//     function isTestBlock(callExpressionNode) {
+//       const { type, name } = callExpressionNode.callee;
+//       return type === 'Identifier' && name === 'it';
+//     }
+
+//     // let axeCheckCount = 0;
+
+//     // const checkAxeCountForReport = node => {
+//     //   if (axeCheckCount === 0) {
+//     //     const message = `${MESSAGE}`;
+//     //     context.report({
+//     //       node,
+//     //       message,
+//     //     });
+//     //   }
+//     // };
+
+//     return {
+//       Identifier(node) {
+//         if (node.name === 'cy') {
+//           const ancestors = context.getAncestors();
+//           const testBlocks = ancestors
+//             .filter(n => n.type === 'CallExpression')
+//             .filter(isTestBlock);
+
+//           if (testBlocks.length >= 1) {
+//             testBlocks.forEach(node => {
+//               context.report({ node, messageId: 'unexpected' })
+//             });
+//           }
+//         }
+//       },
+//     };
+//   },
+// };
+
+// module.exports = rule;
