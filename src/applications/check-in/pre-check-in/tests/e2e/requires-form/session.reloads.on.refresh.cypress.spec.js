61b18cbd97c53d54f3cf09cc9e42a6b420a502d7
Mark Dewey <mdewey@users.noreply.github.com>
2021-12-03 14:46:55 -0500

    (feature) added checks for forms structure in redux on page load (#19594)
    * (feature) added checks for forms structure in redux on page load
    
    * (test) added missing url

---
+++ src/applications/check-in/pre-check-in/tests/e2e/requires-form/session.reloads.on.refresh.cypress.spec.js
@@ -0,0 +1,39 @@
+import { generateFeatureToggles } from '../../../api/local-mock-api/mocks/feature.toggles';
+import '../support/commands';
+
+import validateVeteran from '../pages/ValidateVeteran';
+import introduction from '../pages/Introduction';
+
+import apiInitializer from '../support/ApiInitializer';
+
+describe('Pre Check In Experience', () => {
+  describe('requires form', () => {
+    beforeEach(function() {
+      cy.intercept('GET', '/v0/feature_toggles*', generateFeatureToggles({}));
+      apiInitializer.initializeSessionGet.withSuccessfulNewSession();
+
+      apiInitializer.initializeSessionPost.withSuccess();
+
+      apiInitializer.initializePreCheckInDataGet.withSuccess();
+    });
+    afterEach(() => {
+      cy.window().then(window => {
+        window.sessionStorage.clear();
+      });
+    });
+    it('On page reload, on verify, this should redirect to the landing page', () => {
+      cy.visitPreCheckInWithUUID();
+      validateVeteran.validatePageLoaded();
+
+      cy.reload();
+      // redirected back to landing page to reload the data
+      cy.url().should('match', /id=0429dda5-4165-46be-9ed1-1e652a8dfd83/);
+
+      validateVeteran.validatePageLoaded();
+      validateVeteran.validateVeteran();
+      validateVeteran.attemptToGoToNextPage();
+
+      introduction.validatePageLoaded();
+    });
+  });
+});
