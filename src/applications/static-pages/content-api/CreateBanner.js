6c4dd8fdb38b42eeabb44eaa8d1596190d24899f
Aaron <aaron.ponce@agile6.com>
2021-12-06 13:05:21 -0800

    VACMS-6863-banner poc cleanup and add store prop back into createBanner

--- src/applications/static-pages/content-api/CreateBanner.js
+++ src/applications/static-pages/content-api/CreateBanner.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CAPI_DOMAIN } from './constants/';
+import { CAPI_DOMAIN, BANNER_PATH } from './constants/';
 import { apiRequest } from 'platform/utilities/api';
 
-const path =
-  '/jsonapi/resource/banner-alerts?item-path=/family-member-benefits';
-
-export default async function createBanner(widgetType) {
+export default async function createBanner(store, widgetType) {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   if (!root) {
     return;
   }
   try {
-    const module = await import('./components/Banner');
-    const response = await apiRequest(`${CAPI_DOMAIN}${path}`, null);
+    const response = await apiRequest(`${CAPI_DOMAIN}${BANNER_PATH}`, null);
     const { data } = await response.json();
-    const parsedData = data.map(banner => banner.attributes);
-    const Banner = module.default;
-    if (root) {
-      ReactDOM.render(<Banner banners={parsedData} />, root);
-    }
+    const { default: Banner } = await import('./components/Banner');
+    const banners = data.map(banner => banner.attributes);
+    ReactDOM.render(<Banner banners={banners} store={store} />, root);
   } catch (error) {
     throw new Error(error);
   }
