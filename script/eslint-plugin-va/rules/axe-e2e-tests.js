03c19aaad2974cd8849e40277941433222ac6ae0
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-12-06 13:57:21 -0600

    Create ESLint rule to check for a11y checks in Cypress tests

--- script/eslint-plugin-va/rules/axe-e2e-tests.js
+++ script/eslint-plugin-va/rules/axe-e2e-tests.js
@@ -1,37 +1,28 @@
 const MESSAGE =
-  'e2e tests must include at least one axeCheck call. Documentation for adding checks and understanding errors can be found here: https://github.com/department-of-veterans-affairs/va.gov-team/blob/master/platform/engineering/frontend/eslint/axe-check-required.md';
+  'Cypress E2E tests must include at least one axeCheck call. Documentation for adding checks and understanding errors can be found here: https://depo-platform-documentation.scrollhelp.site/developer-docs/A11y-Testing.1935409178.html';
 
-const rule = {
+module.exports = {
   meta: {
-    type: 'suggestion',
     docs: {
-      description: MESSAGE,
-      category: 'best practices',
+      description: 'Cypress E2E tests must include at least one axeCheck call.',
+      category: 'Best Practices',
       recommended: false,
     },
+    messages: {
+      unexpected: 'Cypress E2E tests must include at least one axeCheck call.',
+    },
   },
-  create: context => {
-    let axeCheckCount = 0;
 
-    const checkAxeCountForReport = node => {
-      if (axeCheckCount === 0) {
-        const message = `${MESSAGE}`;
-        context.report({
-          node,
-          message,
-        });
-      }
-    };
-
+  create(context) {
     return {
-      MemberExpression: node => {
-        if (node.property.name === 'axeCheck') {
-          axeCheckCount++;
+      Identifier(node) {
+        if (node.name === 'it') {
+          const testBody = context.getSourceCode().text;
+          if (!testBody.toLowerCase().includes('axecheck')) {
+            context.report({ node, message: MESSAGE });
+          }
         }
       },
-      'Program:exit': checkAxeCountForReport,
     };
   },
 };
-
-module.exports = rule;
