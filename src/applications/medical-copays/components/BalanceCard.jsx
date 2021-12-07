9778b4f59cba6c9b0dcf8b251338d85df1b5d8c1
Justin Linn <justinslinn727@gmail.com>
2021-12-07 12:58:04 -0500

    Remove link on zero balance

--- src/applications/medical-copays/components/BalanceCard.jsx
+++ src/applications/medical-copays/components/BalanceCard.jsx
@@ -2,60 +2,66 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { currency, calcDueDate } from '../utils/helpers';
 
-const BalanceCard = ({ id, amount, facility, city, date }) => (
-  <div
-    className="vads-u-background-color--gray-lightest vads-u-padding--3 vads-u-margin-bottom--2"
-    data-testid={`balance-card-${id}`}
-  >
-    <h3
-      aria-describedby="copay-balance"
-      className="card-balance vads-u-margin-top--0"
-      data-testid={`amount-${id}`}
+const BalanceCard = ({ id, amount, facility, city, date }) => {
+  return (
+    <div
+      className="vads-u-background-color--gray-lightest vads-u-padding--3 vads-u-margin-bottom--2"
+      data-testid={`balance-card-${id}`}
     >
-      {currency(amount)}
-    </h3>
-    <p
-      id="copay-balance"
-      className="card-heading vads-u-margin-top--0"
-      data-testid={`facility-city-${id}`}
-    >
-      Copay balance for {facility} - {city}
-    </p>
+      <h3
+        aria-describedby="copay-balance"
+        className="card-balance vads-u-margin-top--0"
+        data-testid={`amount-${id}`}
+      >
+        {currency(amount)}
+      </h3>
+      <p
+        id="copay-balance"
+        className="card-heading vads-u-margin-top--0"
+        data-testid={`facility-city-${id}`}
+      >
+        Copay balance for {facility} - {city}
+      </p>
 
-    {!!amount && (
-      <div className="card-content">
-        <i
-          aria-hidden="true"
-          role="img"
-          className="fa fa-exclamation-triangle"
-        />
-        <p>
-          Pay your full balance or request financial help before
-          <strong
-            className="vads-u-margin-x--0p5"
-            data-testid={`due-date-${id}`}
-          >
-            {calcDueDate(date, 30)}
+      {!!amount && (
+        <div className="card-content">
+          <i
+            aria-hidden="true"
+            role="img"
+            className="fa fa-exclamation-triangle"
+          />
+          <p>
+            Pay your full balance or request financial help before
+            <strong
+              className="vads-u-margin-x--0p5"
+              data-testid={`due-date-${id}`}
+            >
+              {calcDueDate(date, 30)}
+            </strong>
+            to avoid late charges, interest, or collection actions.
+          </p>
+        </div>
+      )}
+
+      {/* TODO: need to come back to this and do a check for zero balance with and without statements */}
+      {/* if no statement no link if has statements put link */}
+      {amount ? (
+        <Link
+          className="vads-u-font-size--sm"
+          to={`/balance-details/${id}`}
+          data-testid={`detail-link-${id}`}
+        >
+          <strong>
+            {amount ? 'Check details and resolve this bill' : 'Check details'}
           </strong>
-          to avoid late charges, interest, or collection actions.
-        </p>
-      </div>
-    )}
-
-    <Link
-      className="vads-u-font-size--sm"
-      to={`/balance-details/${id}`}
-      data-testid={`detail-link-${id}`}
-    >
-      <strong>
-        {amount ? 'Check details and resolve this bill' : 'Check details'}
-      </strong>
-      <i
-        className="fa fa-chevron-right vads-u-margin-left--1"
-        aria-hidden="true"
-      />
-    </Link>
-  </div>
-);
+          <i
+            className="fa fa-chevron-right vads-u-margin-left--1"
+            aria-hidden="true"
+          />
+        </Link>
+      ) : null}
+    </div>
+  );
+};
 
 export default BalanceCard;
--- src/applications/medical-copays/components/Balances.jsx
+++ src/applications/medical-copays/components/Balances.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BalanceCard from './BalanceCard';
+import { getMedicalCenterNameByID } from 'platform/utilities/medical-centers/medical-centers';
 
 export const Balances = ({ statementData }) => {
   const single = <h2>What you owe to your facility</h2>;
@@ -10,16 +11,23 @@ export const Balances = ({ statementData }) => {
   return (
     <>
       {statementData?.length === 1 ? single : multiple}
-      {statementData?.map(balance => (
-        <BalanceCard
-          id={balance.id}
-          key={balance.id}
-          amount={balance.pHAmtDue}
-          facility={balance.station.facilityName}
-          city={balance.station.city}
-          date={balance.pSStatementDate}
-        />
-      ))}
+
+      {statementData?.map((balance, idx) => {
+        const facilityName =
+          balance.station.facilityName ||
+          getMedicalCenterNameByID(balance.station.facilitYNum);
+
+        return (
+          <BalanceCard
+            id={balance.id}
+            amount={balance.pHAmtDue}
+            date={balance.pSStatementDate}
+            city={balance.station.city}
+            facility={facilityName}
+            key={balance.id ? balance.id : `${idx}-${balance.facilitYNum}`}
+          />
+        );
+      })}
     </>
   );
 };
