e86c3dd537c5f76e6c3668b785614c07c0e92bbe
Andres Rivera <andres.rivera.toro@gmail.com>
2021-12-14 15:37:21 -0500

    refactor: update regex and replace

--- script/component-migration/transformers/additionalinfo.js
+++ script/component-migration/transformers/additionalinfo.js
@@ -1,19 +1,25 @@
 function additionalInfoReplacement() {
-  // eslint-disable-next-line no-unused-vars
+  /**
+   * Remove prop from component if it exists.
+   *
+   * @param {string} componentString A string containing an occurrence of a component
+   * @param {string} propName The name of the prop to be removed
+   */
   const removeProp = (componentString, propName) => {
-    const component = componentString;
-    const regex = new RegExp(`(?<!>)(${propName}=["{](.)+?["}])`, 'ms');
-    component.replace(regex, '');
-    return component;
+    const regex = new RegExp(`(${propName}=["{](.)+?["}])`);
+    const propExists = componentString
+      .substring(0, componentString.indexOf('>'))
+      .includes(propName);
+    return propExists ? componentString.replace(regex, '') : componentString;
   };
 
   return [
     'va-additional-info',
     {
-      // className: removeProp,
+      className: removeProp,
       triggerText: 'trigger',
       disableAnalytics: 'disable-analytics',
-      // onClick: removeProp,
+      onClick: removeProp,
     },
   ];
 }
