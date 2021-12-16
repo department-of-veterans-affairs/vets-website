66fc295aa39e0f6c791bec0c02a5358f76a693c0
Darius Dzien <dariusz.dzien@adhocteam.us>
2021-12-16 09:31:51 -0600

    Enables Cypress viewport ESLint rule

--- script/eslint-plugin-va/index.js
+++ script/eslint-plugin-va/index.js
@@ -6,6 +6,7 @@ module.exports = {
     'resolved-path-on-required': require('./rules/resolved-path-on-required.js'),
     'axe-check-required': require('./rules/axe-e2e-tests.js'),
     'correct-apostrophe': require('./rules/correct-apostrophe'),
+    'cypress-viewport-deprecated': require('./rules/cypress-viewport-deprecated.js'),
   },
   rulesConfig: {
     'proptypes-camel-cased': 2,
