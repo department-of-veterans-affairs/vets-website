adc3549b3d1604223f204c2b40889758f778fcc4
David <dsrr74@gmail.com>
2021-12-20 10:49:23 -0600

    replaced svg file, updated file path to be svg from png

--- src/applications/edu-benefits/10203/content/directDeposit.jsx
+++ src/applications/edu-benefits/10203/content/directDeposit.jsx
@@ -11,7 +11,7 @@ export const directDepositDescription = () => {
         Scholarship, we’ll need to know where to deposit the funds.
       </p>
       <img
-        src="/img/direct-deposit-check-guide.png"
+        src="/img/direct-deposit-check-guide.svg"
         alt="On a personal check, find your bank’s 9-digit routing number listed along the bottom-left edge, and your account number listed beside that."
       />
     </div>
