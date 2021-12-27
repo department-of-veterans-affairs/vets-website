c06ad5a8016a2905411efc2a22a6f09c63e994ed
Dan Brooking <dan.brooking@accenturefederal.com>
2021-12-27 11:03:56 -0500

    Move checkbox above label for SSN and fix spacing.

--- src/applications/edu-benefits/5490/Form5490App.jsx
+++ src/applications/edu-benefits/5490/Form5490App.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from './config/form';
 
+import './sass/5490.scss';
+
 export default function Form5490Entry({ location, children }) {
+  React.useEffect(() => {
+    const checkbox = document.getElementById('root_view:noSSN');
+    const input = document.getElementById('root_relativeSocialSecurityNumber');
+    const disableSsnField = event => {
+      if (event.target.checked) {
+        input.setAttribute('disabled', true);
+      } else {
+        input.removeAttribute('disabled');
+      }
+    };
+
+    checkbox.addEventListener('change', disableSsnField);
+    return () => checkbox.removeEventListener('change', disableSsnField);
+  }, []);
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
