bb6ac4d574274cb57bf3888076f461c54386594b
Dan Brooking <dan.brooking@accenturefederal.com>
2021-12-30 13:15:47 -0500

    Only add event listener on correct page.

--- src/applications/edu-benefits/5490/Form5490App.jsx
+++ src/applications/edu-benefits/5490/Form5490App.jsx
@@ -6,20 +6,27 @@ import formConfig from './config/form';
 import './sass/5490.scss';
 
 export default function Form5490Entry({ location, children }) {
-  React.useEffect(() => {
-    const checkbox = document.getElementById('root_view:noSSN');
-    const input = document.getElementById('root_relativeSocialSecurityNumber');
-    const disableSsnField = event => {
-      if (event.target.checked) {
-        input.setAttribute('disabled', true);
-      } else {
-        input.removeAttribute('disabled');
-      }
-    };
+  React.useEffect(
+    () => {
+      const checkbox = document.getElementById('root_view:noSSN');
+      const input = document.getElementById(
+        'root_relativeSocialSecurityNumber',
+      );
+      const disableSsnField = event => {
+        if (event.target.checked) {
+          input.setAttribute('disabled', true);
+        } else {
+          input.removeAttribute('disabled');
+        }
+      };
 
-    checkbox.addEventListener('change', disableSsnField);
-    return () => checkbox.removeEventListener('change', disableSsnField);
-  }, []);
+      if (location.pathname === '/applicant/information') {
+        checkbox.addEventListener('change', disableSsnField);
+      }
+      return () => checkbox?.removeEventListener('change', disableSsnField);
+    },
+    [location.pathname],
+  );
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
