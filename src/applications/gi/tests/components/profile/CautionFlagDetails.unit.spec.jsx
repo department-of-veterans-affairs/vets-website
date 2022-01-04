68296c4e39ea3f166696b2bff88221f1a15713b7
dan-brooking <90346049+dan-brooking@users.noreply.github.com>
2022-01-04 15:11:47 -0500

    Swapping gi and gi-sandbox. (#19798)
    * Swapping gi and gi-sandbox.
    
    * Updating unit test import.
    
    * Sandbox was using GI utility.js so copying it over.
    
    * Fixing import for mobile menu spec.

--- src/applications/gi/tests/components/profile/CautionFlagDetails.unit.spec.jsx
+++ src/applications/gi/tests/components/profile/CautionFlagDetails.unit.spec.jsx
@@ -1,105 +1,27 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import CautionFlagDetails from '../../../components/profile/CautionFlagDetails';
 
 describe('<CautionFlagDetails>', () => {
-  it('does not render without caution flags', () => {
-    const wrapper = shallow(<CautionFlagDetails cautionFlags={[]} />);
-    expect(wrapper.html()).to.be.null;
-    wrapper.unmount();
+  it('should render', () => {
+    const tree = shallow(
+      <CautionFlagDetails
+        cautionFlags={[
+          {
+            title: 'Test',
+          },
+        ]}
+      />,
+    );
+    expect(tree.type()).to.not.equal(null);
+    tree.unmount();
   });
 
-  it('displays correct amount of flags', () => {
-    const cautionFlags = [
-      { title: 'Test flag a', id: 1 },
-      { title: 'Test flag b', id: 2 },
-    ];
-
-    const wrapper = mount(<CautionFlagDetails cautionFlags={cautionFlags} />);
-    expect(wrapper.find('div.flagDetail').length).to.equal(cautionFlags.length);
-    wrapper.unmount();
-  });
-
-  it('sorts caution flags by title', () => {
-    const cautionFlags = [
-      { title: 'Test flag z', id: 1 },
-      { title: 'Test flag a', id: 2 },
-    ];
-
-    const wrapper = mount(<CautionFlagDetails cautionFlags={cautionFlags} />);
-    expect(
-      wrapper
-        .find('.usa-alert-heading')
-        .at(0)
-        .text(),
-    ).to.equal(cautionFlags[1].title);
-    wrapper.unmount();
-  });
-
-  it('displays caution flag data correctly', () => {
-    const cautionFlag = {
-      id: 1,
-      title: 'Test flag',
-      description: 'description',
-      linkText: 'LINK',
-      linkUrl: 'https://va.gov',
-    };
-
-    const wrapper = mount(<CautionFlagDetails cautionFlags={[cautionFlag]} />);
-    const html = wrapper.html();
-    expect(wrapper.find('a').length).to.equal(1);
-    expect(html).to.contain(cautionFlag.title);
-    expect(html).to.contain(cautionFlag.description);
-    expect(html).to.contain(cautionFlag.linkText);
-    expect(html).to.contain(cautionFlag.linkUrl);
-    wrapper.unmount();
-  });
-
-  it('does not display link without link text and url', () => {
-    const cautionFlags = [
-      {
-        title: 'Test flag a',
-        id: 1,
-      },
-    ];
-
-    const wrapper = mount(<CautionFlagDetails cautionFlags={cautionFlags} />);
-    expect(wrapper.find('a').length).to.equal(0);
-    wrapper.unmount();
-  });
-
-  it('displays link text without url as text', () => {
-    const cautionFlag = {
-      title: 'Test flag a',
-      linkText: 'LINK',
-      id: 1,
-    };
-
-    const wrapper = mount(<CautionFlagDetails cautionFlags={[cautionFlag]} />);
-    expect(wrapper.find('a').length).to.equal(0);
-    expect(wrapper.html()).to.contain(cautionFlag.linkText);
-    wrapper.unmount();
-  });
-
-  it('tracks link click', () => {
-    const cautionFlag = {
-      id: 1,
-      title: 'Test flag',
-      linkText: 'LINK',
-      linkUrl: 'https://va.gov',
-    };
-
-    const wrapper = mount(<CautionFlagDetails cautionFlags={[cautionFlag]} />);
-
-    wrapper
-      .find('a')
-      .at(0)
-      .simulate('click');
-
-    expect(global.window.dataLayer[0].alertBoxHeading).to.eq(cautionFlag.title);
-
-    wrapper.unmount();
+  it('should not render without caution flags', () => {
+    const tree = shallow(<CautionFlagDetails cautionFlags={[]} />);
+    expect(tree.type()).to.equal(null);
+    tree.unmount();
   });
 });
