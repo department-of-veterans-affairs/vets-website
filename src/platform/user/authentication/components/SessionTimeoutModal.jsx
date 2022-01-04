4ab41ccc3e16951057336a75c8ce74241fda87e0
Alexander Garcia <agarcia@clarityinnovates.com>
2022-01-04 16:15:53 -0500

    remove moment dependencies

--- src/platform/user/authentication/components/SessionTimeoutModal.jsx
+++ src/platform/user/authentication/components/SessionTimeoutModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import getSeconds from 'date-fns/getSeconds';
 
 import Modal from '@department-of-veterans-affairs/component-library/Modal';
 
@@ -44,7 +45,9 @@ class SessionTimeoutModal extends React.Component {
     const expirationTime = new Date(expirationDate).getTime();
     if (isNaN(expirationTime)) return;
 
-    const countdown = Math.floor((expirationTime - Date.now()) / 1000);
+    const countdown = Math.floor(
+      getSeconds(expirationDate) - getSeconds(new Date()),
+    );
     if (countdown < 0) this.expireSession();
     else if (countdown <= MODAL_DURATION) this.setState({ countdown });
   };
