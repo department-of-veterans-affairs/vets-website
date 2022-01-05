07c7990ff56d06f19ac8e765efe8b029160920c7
Robin Garrison <robin.garrison@adhocteam.us>
2022-01-05 12:33:27 -0600

    Replace AdditionalInfo with web component

--- src/applications/disability-benefits/all-claims/content/separationLocation.jsx
+++ src/applications/disability-benefits/all-claims/content/separationLocation.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import AdditionalInfo from '@department-of-veterans-affairs/component-library/AdditionalInfo';
 
 export const SeparationLocationTitle = 'Place of anticipated separation';
 
 export const SeparationLocationDescription = (
   <>
     <br />
-    <AdditionalInfo triggerText="What's this?">
+    <va-additional-info trigger="What's this?">
       <>
         <p>
           Your place of anticipated separation is where you expect to separate
@@ -18,10 +17,10 @@ export const SeparationLocationDescription = (
           your exact separation date, to help process your claim.
         </p>
         <b>
-          If you can't find your exact location from the list, please select
+          If you can’t find your exact location from the list, please select
           your nearest regional facility.
         </b>
       </>
-    </AdditionalInfo>
+    </va-additional-info>
   </>
 );
