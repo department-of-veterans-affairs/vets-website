5fe280bf9be262f99b8ef97fe835b115220cdba0
tmitchellgcio <84803567+tmitchellgcio@users.noreply.github.com>
2022-01-07 15:53:34 -0500

    feature toggle for debt and payments section. (#19772)
    * feature toggle for debt and payments section. This is only a prototype for now.
    
    * Spelling error
    
    * fix feature toggle test
    
    * make dismissible alert actually dismissible
    
    * hook up to real data and adjust based on the new mocks
    
    * fix changed route
    
    * fix tests
    
    * use utility classes when possible
    
    * removes the need for cross team feature toggle mock
    
    * removes the need for cross team feature toggle mock unit test

--- src/applications/personalization/dashboard/reducers/index.js
+++ src/applications/personalization/dashboard/reducers/index.js
@@ -5,12 +5,16 @@ import folders from './folders';
 import unreadCount from './unreadCount';
 import appointments from '~/applications/personalization/appointments/reducers';
 import profile from '@@profile/reducers';
+import { debtsReducer } from '~/applications/debt-letters/reducers';
+import payments from '~/applications/disability-benefits/view-payments/reducers';
 
 import { combineReducers } from 'redux';
 
 export default {
   ...claimsAppeals,
   ...profile,
+  allPayments: payments.allPayments,
+  fsr: debtsReducer,
   health: combineReducers({
     appointments,
     rx: combineReducers({
