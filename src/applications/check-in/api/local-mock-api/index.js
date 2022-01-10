ee131d92850ab1152f11ac33c2abf7d1fd0c210d
Mark Dewey <mdewey@governmentcio.com>
2022-01-10 16:14:09 -0500

    (chore) fixed mock api

--- src/applications/check-in/api/local-mock-api/index.js
+++ src/applications/check-in/api/local-mock-api/index.js
@@ -16,7 +16,7 @@ const responses = {
     checkInExperienceEnabled: true,
     preCheckInEnabled: true,
     checkInExperienceUpdateInformationPageEnabled: false,
-    emergencyContactEnabled: false,
+    emergencyContactEnabled: true,
   }),
   // v2
   'GET /check_in/v2/sessions/:uuid': (req, res) => {
