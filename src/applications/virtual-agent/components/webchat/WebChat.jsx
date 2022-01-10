0ecf3229758ca2e41a5a52618ae1ff32896a9f81
Kha Huynh <kha.huynh@thoughtworks.com>
2022-01-10 14:21:52 -0500

    [#331] WIP

--- src/applications/virtual-agent/components/webchat/WebChat.jsx
+++ src/applications/virtual-agent/components/webchat/WebChat.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import MarkdownRenderer from './markdownRenderer';
 import GreetUser from './makeBotGreetUser';
 import environment from 'platform/utilities/environment';
@@ -15,19 +15,37 @@ const WebChat = ({ token, WebChatFramework, apiSession }) => {
     _.upperFirst(_.toLower(state.user.profile.userFullName.first)),
   );
 
+  const [updated, setUpdated] = useState(false);
+
   const store = useMemo(
+    () => {
+      console.log('up-dated value is: ', updated);
+      if (!updated) {
+        console.log('in store hook');
+        createStore(
+          {},
+          GreetUser.makeBotGreetUser(
+            csrfToken,
+            apiSession,
+            '76a5-2601-241-8f80-54b0-8549-f157-2895-500b.ngrok.io',
+            environment.BASE_URL,
+            userFirstName === '' ? 'noFirstNameFound' : userFirstName,
+          ),
+        );
+      }
+    },
+    [createStore, updated],
+  );
+
+  const directLine = useMemo(
     () =>
-      createStore(
-        {},
-        GreetUser.makeBotGreetUser(
-          csrfToken,
-          apiSession,
-          '76a5-2601-241-8f80-54b0-8549-f157-2895-500b.ngrok.io',
-          environment.BASE_URL,
-          userFirstName === '' ? 'noFirstNameFound' : userFirstName,
-        ),
-      ),
-    [createStore],
+      createDirectLine({
+        token,
+        domain:
+          'https://northamerica.directline.botframework.com/v3/directline',
+      }),
+    [token, createDirectLine],
+    console.log('in direct line hook'),
   );
 
   const IDLE_TIMEOUT = 30000;
@@ -38,6 +56,7 @@ const WebChat = ({ token, WebChatFramework, apiSession }) => {
   const updateBot = useCallback(
     () => {
       (function() {
+        setUpdated(true);
         console.log('updating bot');
         console.log('old store: ', botStore.getState());
 
@@ -49,27 +68,33 @@ const WebChat = ({ token, WebChatFramework, apiSession }) => {
               apiSession,
               '76a5-2601-241-8f80-54b0-8549-f157-2895-500b.ngrok.io',
               'fabulous-friday',
-              userFirstName === '' ? 'noFirstNameFound' : userFirstName,
+              'Kha',
             ),
           ),
         );
+
+        setBotDirectLine(
+          createDirectLine({
+            token,
+            domain:
+              'https://northamerica.directline.botframework.com/v3/directline',
+          }),
+        );
         console.log('new store: ', botStore.getState());
+        //console.log('new directline: ', botDirectLine);
       })();
     },
     [setTimer, setBotStore],
   );
 
-  useTimeoutAt(updateBot, timer);
+  // useEffect(
+  //   () => {
+  //     console.log(botStore.getState());
+  //   },
+  //   [botStore],
+  // );
 
-  const directLine = useMemo(
-    () =>
-      createDirectLine({
-        token,
-        domain:
-          'https://northamerica.directline.botframework.com/v3/directline',
-      }),
-    [token, createDirectLine],
-  );
+  useTimeoutAt(updateBot, timer);
 
   const styleOptions = {
     hideUploadButton: true,
@@ -95,14 +120,16 @@ const WebChat = ({ token, WebChatFramework, apiSession }) => {
   };
 
   return (
-    <div data-testid={'webchat'} style={{ height: '550px', width: '100%' }}>
-      <ReactWebChat
-        styleOptions={styleOptions}
-        directLine={directLine}
-        store={botStore}
-        renderMarkdown={renderMarkdown}
-      />
-    </div>
+    <>
+      <div data-testid={'webchat'} style={{ height: '550px', width: '100%' }}>
+        <ReactWebChat
+          styleOptions={styleOptions}
+          directLine={botDirectLine}
+          store={botStore}
+          renderMarkdown={renderMarkdown}
+        />
+      </div>
+    </>
   );
 };
 
