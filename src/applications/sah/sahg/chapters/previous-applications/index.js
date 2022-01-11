c10e5b442bb762d370b231c289f192e157b4efac
jerekshoe <jerek.shoemaker@adhocteam.us>
2022-01-11 14:20:32 -0600

    Adding in chapters/pages for SAHG

---
+++ src/applications/sah/sahg/chapters/previous-applications/index.js
@@ -0,0 +1,4 @@
+import * as appliedForHomeImprovement from './home-improvement-structural-alteration';
+import * as appliedForSpeciallyAdaptedHousing from './specially-adapted-housing';
+
+export { appliedForHomeImprovement, appliedForSpeciallyAdaptedHousing };
