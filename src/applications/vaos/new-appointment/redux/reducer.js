0ad930eb2c66315203ffeca351c768094fd0848d
Tony Williams <tony.williams@va.gov>
2022-01-11 16:45:10 -0600

    Fixed failing unit test case

--- src/applications/vaos/new-appointment/redux/reducer.js
+++ src/applications/vaos/new-appointment/redux/reducer.js
@@ -755,6 +755,7 @@ export default function formReducer(state = initialState, action) {
       if (state.ccEnabledSystems?.length === 1) {
         formData = {
           ...formData,
+          communityCareSystemId: state.ccEnabledSystems[0].id,
         };
       }
       const { data, schema } = setupFormData(
