8968caeb8a4a8fa52e8acc56c9bb0eadb52e6371
jerekshoe <jerek.shoemaker@adhocteam.us>
2022-01-13 12:24:07 -0600

    Using form fields for personal information

--- src/applications/lgy/coe/form/config/chapters/applicant/applicantInformation.js
+++ src/applications/lgy/coe/form/config/chapters/applicant/applicantInformation.js
@@ -1,12 +1,43 @@
-import { applicantInformation } from '../../schemaImports';
+import fullNameUI from 'platform/forms/definitions/fullName';
+import currentOrPastDateUI from 'platform/forms-system/src/js/definitions/currentOrPastDate';
 
-import ApplicantInformationSummary from './ApplicantInformationSummary';
-
-export const schema = applicantInformation;
+export const schema = {
+  type: 'object',
+  properties: {
+    fullName: {
+      type: 'object',
+      properties: {
+        first: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 30,
+        },
+        middle: {
+          type: 'string',
+        },
+        last: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 30,
+        },
+        suffix: {
+          type: 'string',
+          enum: ['Jr.', 'Sr.', 'II', 'III', 'IV'],
+        },
+      },
+      required: ['first', 'last'],
+    },
+    dateOfBirth: {
+      type: 'string',
+      title: 'Date of birth',
+      pattern:
+        '^(\\d{4}|XXXX)-(0[1-9]|1[0-2]|XX)-(0[1-9]|[1-2][0-9]|3[0-1]|XX)$',
+    },
+  },
+  required: ['dateOfBirth'],
+};
 
 export const uiSchema = {
-  'ui:description': ApplicantInformationSummary,
-  'ui:options': {
-    hideOnReview: true,
-  },
+  fullName: fullNameUI,
+  dateOfBirth: currentOrPastDateUI('Date of birth'),
 };
