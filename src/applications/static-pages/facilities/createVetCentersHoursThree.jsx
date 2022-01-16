5af4eaff2ba43364342951178ae18a34adc6abcf
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2022-01-16 17:07:25 -0600

    34316 Add Cap hours functionality. (#19918)

--- src/applications/static-pages/facilities/createVetCentersHoursThree.jsx
+++ src/applications/static-pages/facilities/createVetCentersHoursThree.jsx
@@ -5,14 +5,16 @@ import { Provider } from 'react-redux';
 import widgetTypes from '../widgetTypes';
 import { standardizeDateTime } from './facilityUtilities';
 
-export default async function createVetCentersHours(store) {
+export default async function createVetCentersHoursThree(store) {
   const vetCentersHoursWidget = document.querySelector(
-    `[data-widget-type="${widgetTypes.VET_CENTER_HOURS}"]`,
+    `[data-widget-type="${widgetTypes.VET_CENTER_HOURS_THREE}"]`,
   );
 
   if (vetCentersHoursWidget) {
     const { default: VetCenterHours } = await import('./vetCentersHours');
-    const vetCenterHoursArray = standardizeDateTime(window.vetCenterHours);
+    const vetCenterHoursArray = standardizeDateTime(
+      window.vetCenterHoursKey_03,
+    );
     ReactDOM.render(
       <Provider store={store}>
         <VetCenterHours hours={vetCenterHoursArray} />
