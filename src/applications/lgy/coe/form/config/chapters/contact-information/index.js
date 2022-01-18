9272a7ed6fcdc6624833d0d1919075402c7c035f
Jerek Shoemaker <jerek.shoemaker@adhocteam.us>
2022-01-18 13:58:56 -0600

    [COE] Update form pages to align with mockups (#19903)
    * Updating COE form pages to align with mockups

---
+++ src/applications/lgy/coe/form/config/chapters/contact-information/index.js
@@ -0,0 +1,4 @@
+import * as additionalInformation from '../contact-information/additional-information';
+import * as mailingAddress from '../contact-information/mailing-address';
+
+export { additionalInformation, mailingAddress };
