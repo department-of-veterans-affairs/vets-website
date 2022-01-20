e5560d4fa9c93745f3f3bd173d691955b2ba8662
dcgoodwin2112 <dcgoodwin2112@gmail.com>
2022-01-20 10:12:05 -0500

    update error message on error pages to show max retries message

--- src/applications/check-in/day-of/pages/Error.jsx
+++ src/applications/check-in/day-of/pages/Error.jsx
@@ -4,10 +4,20 @@ import ErrorMessage from '../../components/ErrorMessage';
 import BackToHome from '../components/BackToHome';
 import Footer from '../components/Footer';
 
+import { useSessionStorage } from '../../hooks/useSessionStorage';
+
 const Error = () => {
+  const { getValidateAttempts } = useSessionStorage(false);
+  const { isMaxValidateAttempts } = getValidateAttempts(window);
+  const maxValidateMessage =
+    "We're sorry. We couldn't match your information to our records. Please try again or call us at 800-698-2411 (TTY: 711) for help signing in.";
   return (
     <div className="vads-l-grid-container vads-u-padding-y--5 ">
-      <ErrorMessage />
+      {isMaxValidateAttempts ? (
+        <ErrorMessage message={maxValidateMessage} />
+      ) : (
+        <ErrorMessage />
+      )}
       <Footer />
       <BackToHome />
     </div>
