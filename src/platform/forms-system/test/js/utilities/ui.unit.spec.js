5a91deac33a0e364dd8c13f4cb08cebbbbb341b8
Sebastian S.A. Cooper <sebastian@kvortex.com>
2022-01-20 11:05:37 -0600

    Changed ui test back to original state

--- src/platform/forms-system/test/js/utilities/ui.unit.spec.js
+++ src/platform/forms-system/test/js/utilities/ui.unit.spec.js
@@ -135,7 +135,7 @@ describe('getFocuableElements', () => {
     const dom = findDOMNode(tree);
     global.document = dom;
     const focusableElements = getFocusableElements(dom);
-    expect(focusableElements.length).to.be.above(0);
+    expect(focusableElements.length).to.eq(7);
   });
 
   it('should return an empty array from non-focusable elements', () => {
