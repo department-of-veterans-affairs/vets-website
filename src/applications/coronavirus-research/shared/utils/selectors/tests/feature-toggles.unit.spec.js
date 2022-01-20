db932d914bbe81239d0738e45400a316964df445
Stephen Barrs <stephen.barrs@va.gov>
2022-01-20 09:20:22 -0500

    update feature toggle unit test

--- src/applications/coronavirus-research/shared/utils/selectors/tests/feature-toggles.unit.spec.js
+++ src/applications/coronavirus-research/shared/utils/selectors/tests/feature-toggles.unit.spec.js
@@ -7,8 +7,8 @@ describe('covid-vaccine-trials-feature-toggles', () => {
     const state = {
       featureToggles: {
         /* eslint-disable camelcase */
-        covid_vaccine_registration: true,
-        check_in_experience_pre_check_in_enabled: true,
+        covidVolunteerIntakeEnabled: true,
+        covidVolunteerUpdateEnabled: true,
         loading: false,
       },
     };
@@ -18,8 +18,8 @@ describe('covid-vaccine-trials-feature-toggles', () => {
         const selectFeatureToggles = makeSelectFeatureToggles();
         expect(selectFeatureToggles(state)).to.eql({
           isLoadingFeatureFlags: false,
-          isCovidVaccineTrialsIntakeEnabled: true,
-          isPreCheckInEnabled: true,
+          covidVolunteerIntakeEnabled: true,
+          covidVolunteerUpdateEnabled: true,
         });
       });
     });
