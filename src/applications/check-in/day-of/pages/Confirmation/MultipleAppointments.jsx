3f404b12ba0732045cc42e7af07a5c73cf05c511
Brian Seek <brian.seek@civicactions.com>
2022-01-21 09:07:46 -0800

    Moved BackToAppointments component.

--- src/applications/check-in/day-of/pages/Confirmation/MultipleAppointments.jsx
+++ src/applications/check-in/day-of/pages/Confirmation/MultipleAppointments.jsx
@@ -6,7 +6,7 @@ import { VaAlert } from 'web-components/react-bindings';
 import { focusElement } from 'platform/utilities/ui';
 
 import BackToHome from '../../components/BackToHome';
-import BackToAppointments from '../../components/BackToAppointments';
+import BackToAppointments from '../../../components/BackToAppointments';
 import Footer from '../../components/Footer';
 import AppointmentLocation from '../../components/AppointmentDisplay/AppointmentLocation';
 import TravelPayReimbursementLink from '../../../components/TravelPayReimbursementLink';
