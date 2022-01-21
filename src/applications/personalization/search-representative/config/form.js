bff5cb3e6bedf0174f2eca3eb0fec60b58ee9441
jerekshoe <jerek.shoemaker@adhocteam.us>
2022-01-21 14:06:15 -0600

    Hiding authenticated page for now

--- src/applications/personalization/search-representative/config/form.js
+++ src/applications/personalization/search-representative/config/form.js
@@ -1,6 +1,3 @@
-import { isLoggedIn } from 'platform/user/selectors';
-
-import { store } from '../app-entry';
 import manifest from '../manifest.json';
 
 import GetFormHelp from '../components/GetFormHelp';
@@ -83,25 +80,12 @@ const formConfig = {
           path: 'personal-information-auth',
           title: personalInformation.title,
           ...personalInformation.authenticated,
-          depends: () => {
-            if (store) {
-              return isLoggedIn(store.getState());
-            }
-
-            return false;
-          },
+          depends: () => false,
         },
         personalInformationNoAuth: {
-          path: 'personal-information-unauth',
+          path: 'personal-information',
           title: personalInformation.title,
           ...personalInformation.unauthenticated,
-          depends: () => {
-            if (store) {
-              return !isLoggedIn(store.getState());
-            }
-
-            return true;
-          },
         },
       },
     },
