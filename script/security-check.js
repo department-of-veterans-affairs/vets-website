3e0c39ec4ae878cc9a7c98f5bc57cbeed04ae026
Theo Bentum <theo@adhocteam.us>
2022-01-22 17:42:50 -0800

    Add nanoid to exception list

--- script/security-check.js
+++ script/security-check.js
@@ -11,6 +11,7 @@ const packageJSON = require('../package.json');
 const exceptionSet = new Set([
   'https://npmjs.com/advisories/996',
   'https://npmjs.com/advisories/1488',
+  'https://github.com/advisories/GHSA-qrpm-p2h7-hrv2',
 ]);
 
 const severitySet = new Set(['high', 'critical', 'moderate']);
