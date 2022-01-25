1aad341ca5bdeb665440702d73da13840041d702
fer.boza <fernando.boza@accenturefederal.com>
2022-01-25 14:36:43 -0500

    updated title for Additional consideration

--- src/applications/my-education-benefits/config/form.js
+++ src/applications/my-education-benefits/config/form.js
@@ -1234,7 +1234,7 @@ const formConfig = {
       },
     },
     additionalConsiderationsChapter: {
-      title: 'Additional Considerations',
+      title: 'Additional considerations',
       pages: {
         [formPages.additionalConsiderations.activeDutyKicker.name]: {
           ...AdditionalConsiderationTemplate(
