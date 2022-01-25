bb970c7e2d87ca66987f39e88a5a6b50609a7923
Taylor Mitchell <tmitchell@governmentcio.com>
2022-01-25 17:19:30 -0500

    fix tests

--- src/applications/personalization/dashboard/tests/unit/healthcare-messaging.unit.spec.jsx
+++ src/applications/personalization/dashboard/tests/unit/healthcare-messaging.unit.spec.jsx
@@ -54,9 +54,7 @@ describe('HealthCare component', () => {
         reducers,
       });
       expect(view.queryByRole('progressbar')).not.to.exist;
-      expect(
-        await view.findByRole('link', { name: /you have 3 unread messages/i }),
-      ).to.exist;
+      expect(await view.findByText(/you have 3 unread messages/i)).to.exist;
     });
 
     it('should render the unread messages count with 1 message', async () => {
@@ -65,9 +63,7 @@ describe('HealthCare component', () => {
         initialState,
         reducers,
       });
-      expect(
-        await view.findByRole('link', { name: /you have 1 unread message/i }),
-      ).to.exist;
+      expect(await view.findByText(/you have 1 unread message/i)).to.exist;
     });
 
     it('should render the unread messages count with 0 messages', async () => {
@@ -76,9 +72,7 @@ describe('HealthCare component', () => {
         initialState,
         reducers,
       });
-      expect(
-        await view.findByRole('link', { name: /you have 0 unread messages/i }),
-      ).to.exist;
+      expect(await view.findByText(/you have 0 unread messages/i)).to.exist;
     });
   });
 
