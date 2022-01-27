225851d5739abe967ff9d8d2bd8f6c74e9aee15b
Preston Davies <pdavies88@gmail.com>
2022-01-27 14:07:32 -0500

    34752/Cypress-Command-expandAccordions-ShadowDOM

--- src/platform/testing/e2e/cypress/support/commands/expandAccordions.js
+++ src/platform/testing/e2e/cypress/support/commands/expandAccordions.js
@@ -1,15 +1,35 @@
 /**
  * Expands all accordions and AdditionalInfo components.
+ * Web Components that require Shadow DOM broken out from React Components
  */
 Cypress.Commands.add('expandAccordions', () => {
   Cypress.log();
 
   cy.get('main').then($main => {
-    const accordions = $main.find('button[aria-expanded=false]');
-    if (accordions.length) {
-      cy.wrap(accordions).each(button => {
-        cy.wrap(button).click({ force: true });
-      });
+    if ($main.find('va-accordion-item').length > 0) {
+      // va-accordion-item Web Component Expand
+      cy.get('va-accordion-item')
+        .shadow()
+        .find('button[aria-expanded=false]')
+        .each(button => {
+          cy.wrap(button).click({ force: true });
+        });
+    } else if ($main.find('va-additional-info').length > 0) {
+      // va-additional-info Web Component Expand
+      cy.get('va-additional-info')
+        .shadow()
+        .find('button[aria-expanded=false]')
+        .each(button => {
+          cy.wrap(button).click({ force: true });
+        });
+    } else {
+      // AdditionalInfo and Accordion React Components Expand
+      const accordions = $main.find('button[aria-expanded=false]');
+      if (accordions.length) {
+        cy.wrap(accordions).each(button => {
+          cy.wrap(button).click({ force: true });
+        });
+      }
     }
   });
 });
