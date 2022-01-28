3578c53f071f64e98c3e90de317b650d2af61eb8
jerekshoe <jerek.shoemaker@adhocteam.us>
2022-01-28 15:38:14 -0600

    Saving Progress

--- src/applications/lgy/coe/form/components/introduction/AuthenticatedContent.jsx
+++ src/applications/lgy/coe/form/components/introduction/AuthenticatedContent.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import OMBInfo from '@department-of-veterans-affairs/component-library/OMBInfo';
-
-import SaveInProgressIntro from 'platform/forms/save-in-progress/SaveInProgressIntro';
 
 import SubwayMap from './SubwayMap';
 
 const AuthenticatedContent = props => (
   <>
-    <SubwayMap />
-    <div className="vads-u-margin-bottom--4">
-      <SaveInProgressIntro
-        buttonOnly
-        testActionLink
-        prefillEnabled={props.parentProps.route.formConfig.prefillEnabled}
-        messages={props.parentProps.route.formConfig.savedFormMessages}
-        formConfig={props.parentProps.route.formConfig}
-        pageList={props.parentProps.route.pageList}
-        downtime={props.parentProps.route.formConfig.downtime}
-        startText="Request a Certificate of Eligibility"
-        headingLevel={2}
-      />
-    </div>
-    <OMBInfo expDate="11/30/2022" ombNumber="2900-0086" resBurden={15} />
+    <SubwayMap {...props} />
   </>
 );
 
--- src/applications/lgy/coe/form/components/introduction/SubwayMap.jsx
+++ src/applications/lgy/coe/form/components/introduction/SubwayMap.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import OMBInfo from '@department-of-veterans-affairs/component-library/OMBInfo';
 
-const SubwayMap = () => (
+import SaveInProgressIntro from 'platform/forms/save-in-progress/SaveInProgressIntro';
+
+const SubwayMap = props => (
   <div className="process schemaform-process">
     <ol>
       <li className="process-step list-one">
@@ -85,6 +88,20 @@ const SubwayMap = () => (
         </va-additional-info>
       </li>
     </ol>
+    <div className="vads-u-margin-bottom--4">
+      <SaveInProgressIntro
+        buttonOnly
+        testActionLink
+        prefillEnabled={props.parentProps.route.formConfig.prefillEnabled}
+        messages={props.parentProps.route.formConfig.savedFormMessages}
+        formConfig={props.parentProps.route.formConfig}
+        pageList={props.parentProps.route.pageList}
+        downtime={props.parentProps.route.formConfig.downtime}
+        startText="Request a Certificate of Eligibility"
+        headingLevel={2}
+      />
+    </div>
+    <OMBInfo expDate="11/30/2022" ombNumber="2900-0086" resBurden={15} />
   </div>
 );
 
