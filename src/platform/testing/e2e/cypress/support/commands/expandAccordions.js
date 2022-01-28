097aa7ffea6d9967b3a136bd92f24a05aa6a3ddf
Preston Davies <pdavies88@gmail.com>
2022-01-28 09:16:00 -0500

    chore: update syntax of comments

--- src/platform/testing/e2e/cypress/support/commands/expandAccordions.js
+++ src/platform/testing/e2e/cypress/support/commands/expandAccordions.js
@@ -43,7 +43,8 @@ Cypress.Commands.add('expandAccordions', () => {
           }
         });
     }
-    // Check if AdditionalInfo and Accordion React Components exist and not already expanded
+    // Check if AdditionalInfo and/or Accordion React Components exist
+    // Check that the component is not already expanded
     if ($main.find('button[aria-expanded=false]').length > 0) {
       cy.get('button[aria-expanded=false]').each(button => {
         // Click to open Accordion or Additional Info
