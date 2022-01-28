065aaf027049ff2fd1a0d8cb98e45bfdc836f0e9
Ian Harrison <ian.harrison@adhocteam.us>
2022-01-28 09:58:01 -0600

    remove lazy loading in site wide components causing bug in sign in page

--- src/platform/site-wide/mobile-menu-button/index.js
+++ src/platform/site-wide/mobile-menu-button/index.js
@@ -9,16 +9,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import startReactApp from '../../startup/react';
+import Main from './containers/Main';
 
 /**
  * Sets up the login widget with the given store at login-root
  *
  * @param {Redux.Store} store The common store used on the site
  */
-export default async function startMobileMenuButton(store) {
-  const {
-    default: Main,
-  } = await import(/* mobile-menu-button-widget */ './containers/Main');
+export default function startMobileMenuButton(store) {
   startReactApp(
     <Provider store={store}>
       <Main />
