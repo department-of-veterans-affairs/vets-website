f2488dc5066711548b06a2bf0a5ef2edcd95ae4b
BJ McCotter <bj.mccotter@adhocteam.us>
2022-02-01 09:00:54 -0600

    On blur back button (#20048)
    * added onMouseDown preventDefault to bypass onBlur
    
    * added tests and defaultProp for preventOnBlur
    
    * added test to ensure mousedown doesnt effect click

--- src/platform/forms-system/test/js/components/ProgressButton.unit.spec.jsx
+++ src/platform/forms-system/test/js/components/ProgressButton.unit.spec.jsx
@@ -3,6 +3,7 @@ import ReactTestUtils from 'react-dom/test-utils';
 import { shallow } from 'enzyme';
 import chaiAsPromised from 'chai-as-promised';
 import chai, { expect } from 'chai';
+import sinon from 'sinon';
 
 import { axeCheck } from '../../config/helpers';
 import ProgressButton from '../../../src/js/components/ProgressButton.jsx';
@@ -46,6 +47,73 @@ describe('<ProgressButton>', () => {
     ReactTestUtils.Simulate.click(button);
 
     return expect(updatePromise).to.eventually.eql(true);
+  });
+
+  it('calls handle() on click even if mouseDown happens', () => {
+    let progressButton;
+    const spy = sinon.spy();
+
+    const updatePromise = new Promise((resolve, _reject) => {
+      progressButton = ReactTestUtils.renderIntoDocument(
+        <ProgressButton
+          buttonText="Button text"
+          buttonClass="usa-button-primary"
+          disabled={false}
+          onButtonClick={() => {
+            resolve(true);
+          }}
+          preventOnBlur={spy}
+        />,
+      );
+    });
+
+    const button = ReactTestUtils.findRenderedDOMComponentWithTag(
+      progressButton,
+      'button',
+    );
+
+    ReactTestUtils.Simulate.mouseDown(button);
+    ReactTestUtils.Simulate.click(button);
+
+    expect(spy.calledOnce).to.be.true;
+    return expect(updatePromise).to.eventually.eql(true);
+  });
+
+  it('calls preventDefault() on mouseDown event when providing prop', () => {
+    const spy = sinon.spy();
+
+    const progressButton = ReactTestUtils.renderIntoDocument(
+      <ProgressButton
+        buttonText="Button text"
+        buttonClass="usa-button-primary"
+        disabled={false}
+        preventOnBlur={spy}
+      />,
+    );
+
+    const button = ReactTestUtils.findRenderedDOMComponentWithTag(
+      progressButton,
+      'button',
+    );
+
+    ReactTestUtils.Simulate.mouseDown(button);
+
+    expect(spy.calledOnce).to.be.true;
+  });
+
+  it('calls preventDefault() on mouseDown event with defaultProperty', () => {
+    const wrapper = shallow(
+      <ProgressButton
+        buttonText="Button text"
+        buttonClass="usa-button-primary"
+        disabled={false}
+      />,
+    );
+
+    expect(
+      wrapper.find('button').simulate('mouseDown', { preventDefault() {} }),
+    );
+    wrapper.unmount();
   });
 
   it('should add aria-hidden button icons', () => {
