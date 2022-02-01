3560e1cadf1659e375aa3a65066425d0a116fb9c
Craig Wheeler <craig@craigwheeler.net>
2022-02-01 13:39:31 -0500

    fix axe check in cy tests

--- src/applications/debt-letters/tests/e2e/debt-letters.cypress.spec.js
+++ src/applications/debt-letters/tests/e2e/debt-letters.cypress.spec.js
@@ -9,7 +9,6 @@ describe.skip('Debt Letters', () => {
     cy.intercept('GET', '/v0/debts', mockDebts);
     cy.visit('/manage-va-debt/your-debt/');
     cy.injectAxe();
-    cy.axeCheck();
   });
 
   it('C1033 displays the current debts section and navigates to debt details', () => {
@@ -19,6 +18,7 @@ describe.skip('Debt Letters', () => {
       .first()
       .click();
     cy.get('#debtLetterHistory').contains('Debt letter history');
+    cy.axeCheck();
   });
 
   it('C1034 displays download debt letters', () => {
@@ -27,20 +27,24 @@ describe.skip('Debt Letters', () => {
       selector: 'a',
     }).click();
     cy.get('#downloadDebtLetters').contains('Download debt letters');
+    cy.axeCheck();
   });
 
   it('C1035 displays how do I pay my VA debt?', () => {
     cy.findByText(/How do I pay my VA debt/i, { selector: 'a' }).click();
     cy.get('#howDoIPay').contains('How do I pay my VA debt?');
+    cy.axeCheck();
   });
 
   it('C1036 displays how do I get financial help?', () => {
     cy.findByText(/How do I get financial help/i, { selector: 'a' }).click();
     cy.get('#howDoIGetHelp').contains('How do I get financial help?');
+    cy.axeCheck();
   });
 
   it('C1037 displays how do I dispute a debt?', () => {
     cy.findByText(/How do I dispute a debt?/i, { selector: 'a' }).click();
     cy.get('#howDoIDispute').contains('How do I dispute a debt?');
+    cy.axeCheck();
   });
 });
