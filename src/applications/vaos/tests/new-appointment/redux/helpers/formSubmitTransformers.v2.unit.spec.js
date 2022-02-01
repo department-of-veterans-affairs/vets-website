46a7bff98f4cce0893381c706b2e0b8cf5be1c49
Bradley Gross <bradley_gross@hotmail.com>
2022-02-01 10:05:27 -0800

    added reasonCode.text field (#20081)

--- src/applications/vaos/tests/new-appointment/redux/helpers/formSubmitTransformers.v2.unit.spec.js
+++ src/applications/vaos/tests/new-appointment/redux/helpers/formSubmitTransformers.v2.unit.spec.js
@@ -1,60 +1,48 @@
 import { expect } from 'chai';
 
-import { transformFormToVAOSAppointment } from '../../../../new-appointment/redux/helpers/formSubmitTransformers.v2';
-import { VHA_FHIR_ID } from '../../../../utils/constants';
+import {
+  transformFormToVAOSAppointment,
+  transformFormToVAOSVARequest,
+} from '../../../../new-appointment/redux/helpers/formSubmitTransformers.v2';
+import { FLOW_TYPES, VHA_FHIR_ID } from '../../../../utils/constants';
 
 describe('VAOS V2 data transformation', () => {
-  it('remove slot.start and slot.end from new appointment object', () => {
-    const state = {
-      user: {
-        profile: {
-          vapContactInfo: {},
+  describe('VA booked', () => {
+    it('remove slot.start and slot.end from new appointment object', () => {
+      const state = {
+        user: {
+          profile: {
+            vapContactInfo: {},
+          },
         },
-      },
-      newAppointment: {
-        data: {
-          phoneNumber: '5035551234',
-          bestTimeToCall: {
-            morning: true,
-          },
-          email: 'test@va.gov',
-          reasonForAppointment: 'routine-follow-up',
-          reasonAdditionalInfo: 'asdfasdf',
-          selectedDates: ['2019-11-22T09:30:00'],
-          preferredDate: '2019-12-02',
-          clinicId: '983_308',
-          vaParent: '983',
-          vaFacility: '983',
-          facilityType: 'vamc',
-          typeOfCareId: '323',
-        },
-        availableSlots: [
-          {
-            start: '2019-12-22T09:30:00',
-            end: '2019-12-22T10:00:00',
-          },
-          {
-            start: '2019-11-22T09:30:00',
-            end: '2019-11-22T10:00:00',
-          },
-        ],
-        parentFacilities: [
-          {
-            id: '983',
-            identifier: [
-              {
-                system: VHA_FHIR_ID,
-                value: '983',
-              },
-            ],
-            address: {
-              city: 'Cheyenne',
-              state: 'WY',
+        newAppointment: {
+          data: {
+            phoneNumber: '5035551234',
+            bestTimeToCall: {
+              morning: true,
             },
+            email: 'test@va.gov',
+            reasonForAppointment: 'routine-follow-up',
+            reasonAdditionalInfo: 'asdfasdf',
+            selectedDates: ['2019-11-22T09:30:00'],
+            preferredDate: '2019-12-02',
+            clinicId: '983_308',
+            vaParent: '983',
+            vaFacility: '983',
+            facilityType: 'vamc',
+            typeOfCareId: '323',
           },
-        ],
-        facilities: {
-          '323': [
+          availableSlots: [
+            {
+              start: '2019-12-22T09:30:00',
+              end: '2019-12-22T10:00:00',
+            },
+            {
+              start: '2019-11-22T09:30:00',
+              end: '2019-11-22T10:00:00',
+            },
+          ],
+          parentFacilities: [
             {
               id: '983',
               identifier: [
@@ -63,59 +51,158 @@ describe('VAOS V2 data transformation', () => {
                   value: '983',
                 },
               ],
-              name: 'Cheyenne VA Medical Center',
               address: {
                 city: 'Cheyenne',
                 state: 'WY',
               },
-              legacyVAR: {
-                institutionTimezone: 'America/Denver',
+            },
+          ],
+          facilities: {
+            '323': [
+              {
+                id: '983',
+                identifier: [
+                  {
+                    system: VHA_FHIR_ID,
+                    value: '983',
+                  },
+                ],
+                name: 'Cheyenne VA Medical Center',
+                address: {
+                  city: 'Cheyenne',
+                  state: 'WY',
+                },
+                legacyVAR: {
+                  institutionTimezone: 'America/Denver',
+                },
               },
-            },
+            ],
+          },
+          clinics: {
+            '983_323': [
+              {
+                id: '983_308',
+                serviceName: 'Green Team Clinic1',
+                stationId: '983',
+                stationName: 'CHYSHR-Cheyenne VA Medical Center',
+              },
+            ],
+          },
+        },
+        featureToggles: {},
+      };
+
+      const stateWithSlotId = {
+        ...state,
+        newAppointment: {
+          ...state.newAppointment,
+          availableSlots: [
+            { ...state.newAppointment.availableSlots[0], id: 'test' },
+            { ...state.newAppointment.availableSlots[1], id: 'test2' },
           ],
         },
-        clinics: {
-          '983_323': [
+      };
+
+      const data = transformFormToVAOSAppointment(stateWithSlotId);
+      expect(data).to.deep.equal({
+        kind: 'clinic',
+        status: 'booked',
+        clinic: '308',
+        slot: {
+          id: 'test2',
+        },
+        extension: { desiredDate: '2019-12-02T00:00:00+00:00' },
+        locationId: '983',
+        comment: 'Follow-up/Routine: asdfasdf',
+        contact: {
+          telecom: [
+            { type: 'phone', value: '5035551234' },
+            { type: 'email', value: 'test@va.gov' },
+          ],
+        },
+      });
+    });
+  });
+
+  describe('VA request', () => {
+    it('request body is valid', () => {
+      const state = {
+        newAppointment: {
+          data: {
+            phoneNumber: '5035551234',
+            bestTimeToCall: {
+              morning: true,
+            },
+            email: 'test@va.gov',
+            visitType: 'clinic',
+            reasonForAppointment: 'routine-follow-up',
+            reasonAdditionalInfo: 'Testing',
+            selectedDates: ['2019-11-20T12:00:00.000'],
+            vaParent: '983',
+            vaFacility: '983GB',
+            facilityType: 'vamc',
+            typeOfCareId: 'SLEEP',
+            typeOfSleepCareId: '349',
+          },
+          parentFacilities: [
             {
-              id: '983_308',
-              serviceName: 'Green Team Clinic1',
-              stationId: '983',
-              stationName: 'CHYSHR-Cheyenne VA Medical Center',
+              id: '983',
+              identifier: [
+                {
+                  system: VHA_FHIR_ID,
+                  value: '983',
+                },
+              ],
             },
           ],
+          facilities: {
+            '349': [
+              {
+                id: '983GB',
+                identifier: [
+                  {
+                    system: VHA_FHIR_ID,
+                    value: '983GB',
+                  },
+                ],
+                name: 'Cheyenne VA Medical Center',
+                address: {
+                  city: 'Cheyenne',
+                  state: 'WY',
+                },
+                legacyVAR: {
+                  institutionTimezone: 'America/Denver',
+                },
+              },
+            ],
+          },
+          flowType: FLOW_TYPES.REQUEST,
         },
-      },
-      featureToggles: {},
-    };
+        featureToggles: {},
+      };
 
-    const stateWithSlotId = {
-      ...state,
-      newAppointment: {
-        ...state.newAppointment,
-        availableSlots: [
-          { ...state.newAppointment.availableSlots[0], id: 'test' },
-          { ...state.newAppointment.availableSlots[1], id: 'test2' },
+      const data = transformFormToVAOSVARequest(state);
+      expect(data).to.deep.equal({
+        kind: 'clinic',
+        status: 'proposed',
+        locationId: '983GB',
+        serviceType: 'cpap',
+        reasonCode: {
+          coding: [{ code: 'Routine Follow-up' }],
+          text: { type: 'Routine Follow-up' },
+        },
+        comment: 'Testing',
+        contact: {
+          telecom: [
+            { type: 'phone', value: '5035551234' },
+            { type: 'email', value: 'test@va.gov' },
+          ],
+        },
+        requestedPeriods: [
+          { start: '2019-11-20T12:00:00Z', end: '2019-11-20T23:59:00Z' },
         ],
-      },
-    };
-
-    const data = transformFormToVAOSAppointment(stateWithSlotId);
-    expect(data).to.deep.equal({
-      kind: 'clinic',
-      status: 'booked',
-      clinic: '308',
-      slot: {
-        id: 'test2',
-      },
-      extension: { desiredDate: '2019-12-02T00:00:00+00:00' },
-      locationId: '983',
-      comment: 'Follow-up/Routine: asdfasdf',
-      contact: {
-        telecom: [
-          { type: 'phone', value: '5035551234' },
-          { type: 'email', value: 'test@va.gov' },
-        ],
-      },
+        preferredTimesForPhoneCall: ['Morning'],
+      });
     });
   });
 });
