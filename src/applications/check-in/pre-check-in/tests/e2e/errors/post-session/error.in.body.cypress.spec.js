1a729bd6e22a0683a3f3b6f36edbc6748f49d2f4
Cbonade <cbonade@DESKTOP-MI88P0N.localdomain>
2022-02-04 15:39:48 -0500

    WIP

--- src/applications/check-in/pre-check-in/tests/e2e/errors/post-session/error.in.body.cypress.spec.js
+++ src/applications/check-in/pre-check-in/tests/e2e/errors/post-session/error.in.body.cypress.spec.js
@@ -4,37 +4,35 @@ import ApiInitializer from '../../../../../api/local-mock-api/e2e/ApiInitializer
 import ValidateVeteran from '../../../../../tests/e2e/pages/ValidateVeteran';
 import Error from '../../pages/Error';
 
-describe('Pre-Check In Experience ', () => {
-  describe('Error handling', () => {
-    describe('POST /check_in/v2/sessions/', () => {
-      describe('error in the body', () => {
-        beforeEach(() => {
-          const {
-            initializeFeatureToggle,
-            initializeSessionGet,
-            initializeSessionPost,
-          } = ApiInitializer;
-          initializeFeatureToggle.withCurrentFeatures();
-          initializeSessionGet.withSuccessfulNewSession();
+describe('Pre-Check In Experience Error handling', () => {
+  describe('POST /check_in/v2/sessions/', () => {
+    describe('error in the body', () => {
+      beforeEach(() => {
+        const {
+          initializeFeatureToggle,
+          initializeSessionGet,
+          initializeSessionPost,
+        } = ApiInitializer;
+        initializeFeatureToggle.withCurrentFeatures();
+        initializeSessionGet.withSuccessfulNewSession();
 
-          initializeSessionPost.withFailure(200);
+        initializeSessionPost.withFailure(200);
+      });
+      afterEach(() => {
+        cy.window().then(window => {
+          window.sessionStorage.clear();
         });
-        afterEach(() => {
-          cy.window().then(window => {
-            window.sessionStorage.clear();
-          });
-        });
-        it('attempt to sign in with an error', () => {
-          cy.visitPreCheckInWithUUID();
-          // page: Validate
-          ValidateVeteran.validatePage.preCheckIn();
-          ValidateVeteran.validateVeteran();
-          cy.injectAxeThenAxeCheck();
+      });
+      it('attempt to sign in with an error', () => {
+        cy.visitPreCheckInWithUUID();
+        // page: Validate
+        ValidateVeteran.validatePage.preCheckIn();
+        ValidateVeteran.validateVeteran();
+        cy.injectAxeThenAxeCheck();
 
-          ValidateVeteran.attemptToGoToNextPage();
+        ValidateVeteran.attemptToGoToNextPage();
 
-          Error.validatePageLoaded();
-        });
+        Error.validatePageLoaded();
       });
     });
   });
