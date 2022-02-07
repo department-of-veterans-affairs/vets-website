8ad531a4adb477727a86b7a1005c7ea71cac3c3f
Brian Seek <brian.seek@civicactions.com>
2022-02-07 08:21:34 -0800

    Fix flaky test (#20138)
    * Added validate for next page before doing refresh.
    
    * Added data fetch to allow introduction page to render.

--- src/applications/check-in/pre-check-in/tests/e2e/session/session.reloads.on.refresh.cypress.spec.js
+++ src/applications/check-in/pre-check-in/tests/e2e/session/session.reloads.on.refresh.cypress.spec.js
@@ -2,6 +2,7 @@ import '../../../../tests/e2e/commands';
 
 import ApiInitializer from '../../../../api/local-mock-api/e2e/ApiInitializer';
 import ValidateVeteran from '../../../../tests/e2e/pages/ValidateVeteran';
+import Introduction from '../pages/Introduction';
 
 describe('Pre Check In Experience', () => {
   describe('session', () => {
@@ -10,22 +11,25 @@ describe('Pre Check In Experience', () => {
         initializeFeatureToggle,
         initializeSessionGet,
         initializeSessionPost,
+        initializePreCheckInDataGet,
       } = ApiInitializer;
       initializeFeatureToggle.withCurrentFeatures();
       initializeSessionGet.withSuccessfulNewSession();
 
       initializeSessionPost.withSuccess();
+      initializePreCheckInDataGet.withSuccess();
     });
     afterEach(() => {
       cy.window().then(window => {
         window.sessionStorage.clear();
       });
     });
-    it.skip('On page reload, the data should be pull from session storage and redirected to landing screen with data loaded', () => {
+    it('On page reload, the data should be pull from session storage and redirected to landing screen with data loaded', () => {
       cy.visitPreCheckInWithUUID();
       ValidateVeteran.validatePageLoaded();
       ValidateVeteran.validateVeteran();
       ValidateVeteran.attemptToGoToNextPage();
+      Introduction.validatePageLoaded();
 
       cy.window().then(window => {
         const data = window.sessionStorage.getItem(
