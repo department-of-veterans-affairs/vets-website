f3a799623018a7355d8417902a95e6e0767f4574
Cbonade <cbonade@DESKTOP-MI88P0N.localdomain>
2022-02-07 09:37:05 -0500

    WIP

--- src/applications/check-in/pre-check-in/tests/e2e/errors/post-session/error.in.body.cypress.spec.js
+++ src/applications/check-in/pre-check-in/tests/e2e/errors/post-session/error.in.body.cypress.spec.js
@@ -5,64 +5,65 @@ import ValidateVeteran from '../../../../../tests/e2e/pages/ValidateVeteran';
 import Error from '../../pages/Error';
 
 describe('Pre-Check In Experience ', () => {
-  describe('Error handling', () => {
-    describe('POST /check_in/v2/sessions/ with error in the body', () => {
-      beforeEach(() => {
-        const {
-          initializeFeatureToggle,
-          initializeSessionGet,
-          initializeSessionPost,
-        } = ApiInitializer;
-        initializeFeatureToggle.withCurrentFeatures();
-        initializeSessionGet.withSuccessfulNewSession();
+  describe('Error handling - POST /check_in/v2/sessions/ - error in the body', () => {
+    beforeEach(() => {
+      const {
+        initializeFeatureToggle,
+        initializeSessionGet,
+        initializeSessionPost,
+      } = ApiInitializer;
+      initializeFeatureToggle.withCurrentFeatures();
+      initializeSessionGet.withSuccessfulNewSession();
 
-        initializeSessionPost.withFailure(200);
+      initializeSessionPost.withFailure(200);
+    });
+    afterEach(() => {
+      cy.window().then(window => {
+        window.sessionStorage.clear();
       });
-      afterEach(() => {
-        cy.window().then(window => {
-          window.sessionStorage.clear();
-        });
-      });
-      it('attempt to sign in with an error', () => {
-        cy.visitPreCheckInWithUUID();
-        // page: Validate
-        ValidateVeteran.validatePage.preCheckIn();
-        ValidateVeteran.validateVeteran();
-        cy.injectAxeThenAxeCheck();
+    });
+    it('attempt to sign in with an error', () => {
+      cy.visitPreCheckInWithUUID();
+      // page: Validate
+      ValidateVeteran.validatePage.preCheckIn();
+      ValidateVeteran.validateVeteran();
+      cy.injectAxeThenAxeCheck();
 
-        ValidateVeteran.attemptToGoToNextPage();
+      ValidateVeteran.attemptToGoToNextPage();
 
-        Error.validatePageLoaded();
-      });
-      // describe('error in the body', () => {
-      //   beforeEach(() => {
-      //     const {
-      //       initializeFeatureToggle,
-      //       initializeSessionGet,
-      //       initializeSessionPost,
-      //     } = ApiInitializer;
-      //     initializeFeatureToggle.withCurrentFeatures();
-      //     initializeSessionGet.withSuccessfulNewSession();
-
-      //     initializeSessionPost.withFailure(200);
-      //   });
-      //   afterEach(() => {
-      //     cy.window().then(window => {
-      //       window.sessionStorage.clear();
-      //     });
-      //   });
-      //   it('attempt to sign in with an error', () => {
-      //     cy.visitPreCheckInWithUUID();
-      //     // page: Validate
-      //     ValidateVeteran.validatePage.preCheckIn();
-      //     ValidateVeteran.validateVeteran();
-      //     cy.injectAxeThenAxeCheck();
-
-      //     ValidateVeteran.attemptToGoToNextPage();
-
-      //     Error.validatePageLoaded();
-      //   });
-      // });
+      Error.validatePageLoaded();
     });
   });
+  // describe('POST /check_in/v2/sessions/', () => {
+  //   describe('error in the body', () => {
+  //     beforeEach(() => {
+  //       const {
+  //         initializeFeatureToggle,
+  //         initializeSessionGet,
+  //         initializeSessionPost,
+  //       } = ApiInitializer;
+  //       initializeFeatureToggle.withCurrentFeatures();
+  //       initializeSessionGet.withSuccessfulNewSession();
+
+  //       initializeSessionPost.withFailure(200);
+  //     });
+  //     afterEach(() => {
+  //       cy.window().then(window => {
+  //         window.sessionStorage.clear();
+  //       });
+  //     });
+  //     it('attempt to sign in with an error', () => {
+  //       cy.visitPreCheckInWithUUID();
+  //       // page: Validate
+  //       ValidateVeteran.validatePage.preCheckIn();
+  //       ValidateVeteran.validateVeteran();
+  //       cy.injectAxeThenAxeCheck();
+
+  //       ValidateVeteran.attemptToGoToNextPage();
+
+  //       Error.validatePageLoaded();
+  //     });
+  //   });
+  // });
+  // });
 });
