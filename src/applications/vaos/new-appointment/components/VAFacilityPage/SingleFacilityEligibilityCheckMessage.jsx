02b450e495167be2dcf0945e10a7ba64b6365c84
Tony Williams <tony.williams@va.gov>
2022-02-07 18:58:37 -0600

    Update urls to pending

--- src/applications/vaos/new-appointment/components/VAFacilityPage/SingleFacilityEligibilityCheckMessage.jsx
+++ src/applications/vaos/new-appointment/components/VAFacilityPage/SingleFacilityEligibilityCheckMessage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import State from '../../../components/State';
 import InfoAlert from '../../../components/InfoAlert';
 import getEligibilityMessage from './getEligibilityMessage';
+import { selectFeatureStatusImprovement } from '../../../redux/selectors';
 
 export default function SingleFacilityEligibilityCheckMessage({
   facility,
@@ -10,10 +12,15 @@ export default function SingleFacilityEligibilityCheckMessage({
 }) {
   const title =
     'We found one facility that accepts online scheduling for this care';
+  const featureStatusImprovement = useSelector(state =>
+    selectFeatureStatusImprovement(state),
+  );
+
   const { content } = getEligibilityMessage({
     eligibility,
     typeOfCare,
     facilityDetails: facility,
+    featureStatusImprovement,
   });
 
   return (
