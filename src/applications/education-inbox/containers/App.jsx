03599d3e10776a22b76ef9327e7b7a544edd2a93
fer.boza <fernando.boza@accenturefederal.com>
2022-02-10 11:41:48 -0500

    brought back the same designs that reflects the mockup, specifically the footer

--- src/applications/education-inbox/containers/App.jsx
+++ src/applications/education-inbox/containers/App.jsx
@@ -83,32 +83,30 @@ const App = ({ toggleLoginModal, user }) => {
 
               <div style={{ marginBottom: '6.4rem' }}>
                 <h2>What information will I be able to see?</h2>
-
+                <va-alert
+                  background-only
+                  close-btn-aria-label="Close notification"
+                  show-icon
+                  status="info"
+                  visible
+                >
+                  <div>
+                    At this time, we’re only able to show decision letters that
+                    you received after <b>Month Day, 2022</b>. If you’re looking
+                    for an older decision letter,{' '}
+                    <a href="/">contact us using Ask VA</a>.
+                  </div>
+                </va-alert>
                 <p>
                   <b>In your VA education inbox, you’ll find:</b>
                 </p>
                 <ul>
                   <li>
                     Important VA documents, including your Certificate of
-                    Eligibility or Denial Letter if you’ve applied for an
-                    education benefit
+                    Eligibility or Denial Letter if you’ve applied for the
+                    Post-9/11 GI Bill
                   </li>
                 </ul>
-
-                <p>
-                  <b>Note:</b> At this time, we’re only able to show Post-9/11
-                  Gl Bill decision letters that you received after{' '}
-                  <b>Month Day, 2022</b>.
-                </p>
-
-                <p>
-                  If you have questions about your education benefits, call our
-                  Education Call Center at{' '}
-                  <a href="tel:+18884424551;ext=711">888-442-4551 (711)</a>.{' '}
-                  We’re here Monday through Friday, 8:00 a.m. to 7:00 p.m. ET.
-                  If you’re outside the U.S., call us at{' '}
-                  <a href="tel:001-918-781-5678">001-918-781-5678</a>.
-                </p>
               </div>
             </article>
           </div>
