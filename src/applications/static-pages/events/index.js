341a36c00085a2f079d182980c16f8b8946ea3c1
Sam Suddath <47081947+srsuddath@users.noreply.github.com>
2022-02-11 10:11:21 -0500

    remove lazy load for footer and events (#20182)
    * remove lazy load for footer and events
    
    * remove feature toggle from header
    
    * remove lazy load and feature toggle
    
    * re-add sort
    
    * remove unneccesary change
    
    * update tests
    
    * update comments

--- src/applications/static-pages/events/index.js
+++ src/applications/static-pages/events/index.js
@@ -2,22 +2,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+// Related imports.
+import App from './components/App';
 
-export default async (store, widgetType) => {
+export default (store, widgetType) => {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
 
   if (root) {
     // Derive the props to pass to the widget.
     const pastEvents = window?.pastEvents?.entities || [];
     const allEventTeasers = window?.allEventTeasers?.entities || [];
+
     const rawEvents = [...pastEvents, ...allEventTeasers]?.sort(
       (event1, event2) =>
         event1?.fieldDatetimeRangeTimezone?.value -
         event2?.fieldDatetimeRangeTimezone?.value,
     );
-    const {
-      App,
-    } = await import(/* webpackChunkName: "events" */ './components/App');
+
     ReactDOM.render(
       <Provider store={store}>
         <App rawEvents={rawEvents} />
