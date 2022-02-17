a6c9eef5798114ec2c479069b5d8fb1ef56df8d8
Kyle Woolley <iamkylewoolley@gmail.com>
2022-02-17 10:13:17 -0600

    36452/36402 - Identity USiP Logic for OCC/Flagship Mobile Apps (#20249)
    * initial mobile logic for param passthrough
    
    * tests for mobile app usip logic
    
    * add verified to vets-api route when needed
    
    * update constants structure

--- src/platform/user/authentication/constants.js
+++ src/platform/user/authentication/constants.js
@@ -46,6 +46,8 @@ export const EXTERNAL_APPS = {
   MHV: CSP_IDS.MHV,
   MY_VA_HEALTH: 'myvahealth',
   EBENEFITS: 'ebenefits',
+  VA_FLAGSHIP_MOBILE: 'vamobile',
+  VA_OCC_MOBILE: 'vaoccmobile',
 };
 
 export const eAuthURL = `https://${
@@ -59,6 +61,10 @@ export const EXTERNAL_LINKS = {
   },
   MHV: `${eAuthURL}/mhv-portal-web/eauth`,
   EBENEFITS: `${eAuthURL}/ebenefits`,
+  VA_FLAGSHIP_MOBILE: `https://${
+    eauthEnvironmentPrefixes[environment.BUILDTYPE]
+  }fed.eauth.va.gov/oauthe/sps/oauth/oauth20/authorize`,
+  VA_OCC_MOBILE: `${eAuthURL}/MAP/users/v2/landing`,
 };
 
 export const EXTERNAL_REDIRECTS = {
@@ -67,6 +73,8 @@ export const EXTERNAL_REDIRECTS = {
     : EXTERNAL_LINKS.MY_VA_HEALTH.STAGING,
   [EXTERNAL_APPS.MHV]: EXTERNAL_LINKS.MHV,
   [EXTERNAL_APPS.EBENEFITS]: EXTERNAL_LINKS.EBENEFITS,
+  [EXTERNAL_APPS.VA_FLAGSHIP_MOBILE]: EXTERNAL_LINKS.VA_FLAGSHIP_MOBILE,
+  [EXTERNAL_APPS.VA_OCC_MOBILE]: EXTERNAL_LINKS.VA_OCC_MOBILE,
 };
 
 export const VAGOV_TRACKING_IDS = ['UA-50123418-16', 'UA-50123418-17'];
