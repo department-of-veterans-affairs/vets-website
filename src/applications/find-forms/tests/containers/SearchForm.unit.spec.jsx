56ae90cf9e6ebd19dd69df35d662a7578a445610
Tyler Simoni <tyler.simoni.8@gmail.com>
2022-02-17 22:54:37 -0500

    Adding new tests for updated error display

--- src/applications/find-forms/tests/containers/SearchForm.unit.spec.jsx
+++ src/applications/find-forms/tests/containers/SearchForm.unit.spec.jsx
@@ -65,4 +65,21 @@ describe('Find VA Forms <SearchForm>', () => {
 
     tree.unmount();
   });
+
+  // Testing for Error message
+  it('does not display an error on initial page load with no URL query', () => {});
+
+  it('displays an error if input is empty and search is clicked', () => {});
+
+  it('displays an error if input is size one and search is clicked', () => {});
+
+  it('does not display an error if input is greater than one character and search is clicked', () => {});
+
+  it('does not display an error on initial page load with an empty URL query', () => {});
+
+  it('displays an error on initial page load with a URL query of length one', () => {});
+
+  it('does not display an error on initial page load with a URL query of length greater than one', () => {});
+
+  it('removes the error once a valid query has been entered into the input', () => {});
 });
