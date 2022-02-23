2b6662266bf89c5fbca7fc2686d37d900b717ceb
Tim Wright <timwright12@gmail.com>
2022-02-23 08:30:49 -0500

    Rearranged dependencies to remove prod flag

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -2,12 +2,12 @@
 
 # Build vets-website
 set -e
-yarn install --production=false
+yarn install
 npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
 
 # Build content-build and serve site
 cd ../content-build
-yarn install --production=false
+yarn install
 npm run fetch-drupal-cache
 npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' --apps-directory-name=application
 npm run heroku-serve -- build/localhost -p 3002
