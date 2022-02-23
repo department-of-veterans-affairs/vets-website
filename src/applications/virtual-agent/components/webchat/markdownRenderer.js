f4d12d5607296337afd0b9476e5e8242f073cbfb
Jacob Gacek <jacob.gacek@thoughtworks.com>
2022-02-23 15:58:51 -0600

    [department-of-veterans-affairs/va-virtual-agent#369] Uses event listener to know when users click on chatbot link Co-authored-by: Joe Hall <joe.hall@thoughtworks.com>

--- src/applications/virtual-agent/components/webchat/markdownRenderer.js
+++ src/applications/virtual-agent/components/webchat/markdownRenderer.js
@@ -1,5 +1,6 @@
 import MarkdownIt from 'markdown-it';
 import markdownitLinkAttributes from 'markdown-it-link-attributes';
+import recordEvent from 'platform/monitoring/record-event';
 import makePhoneNumberAriaLabel from './makePhoneNumberAriaLabel';
 
 const markdownRenderer = MarkdownIt({
@@ -12,16 +13,13 @@ const markdownRenderer = MarkdownIt({
   },
 });
 
-const script = document.createElement('script');
-script.nonce = '**CSP_NONCE**';
-script.type = 'text/javascript';
-script.text =
-  'function recordLinkClick(data) {\n' +
-  '  console.log("in the script");\n' +
-  '  window.dataLayer && window.dataLayer.push(data);\n' +
-  '  return false;\n' +
-  '};';
-document.body.appendChild(script);
+document.addEventListener('click', e => {
+  const origin = e.target.closest('a');
+
+  if (origin && origin.id && origin.id === 'chatbotLink') {
+    recordEvent({ event: 'chatbot-resource-link-click' });
+  }
+});
 
 // Remember old renderer, if overridden, or proxy to default renderer
 const defaultRender =
@@ -47,10 +45,7 @@ markdownRenderer.renderer.rules[linkOpen] = function(
     tokens[idx].attrPush(['aria-label', ariaLabel]); // add new attribute
   }
 
-  tokens[idx].attrPush([
-    'onclick',
-    'recordEvent({event: "chatbot-resource-link-click"})',
-  ]);
+  tokens[idx].attrPush(['id', 'chatbotLink']);
 
   // pass token to default renderer.
   return defaultRender(tokens, idx, options, env, self);
