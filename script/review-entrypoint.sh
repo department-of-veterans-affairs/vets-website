cac571e0e94b44a0f2bca40edb0823284ad928b6
Tim Wright <timwright12@gmail.com>
2022-02-25 13:37:18 -0500

    update yarn version in docker

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -9,6 +9,6 @@ npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' -
 cd ../content-build
 YARN_CHECKSUM_BEHAVIOR=ignore yarn install --immutable
 npm run fetch-drupal-cache
-npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' --apps-directory-name=application
+npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' --apps-directory-name=application --no-drupal-proxy
 npm run heroku-serve -- build/localhost -p 3002
 
