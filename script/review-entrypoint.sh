5b06212b1acc0c0aa0eba941833a3741ac77304c
Tim Wright <timwright12@gmail.com>
2022-02-25 14:03:42 -0500

    added env file update to review instances

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -7,7 +7,7 @@ npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' -
 
 # Build content-build and serve site
 cd ../content-build
-YARN_CHECKSUM_BEHAVIOR=ignore yarn install --immutable
+cp .env.example .env && YARN_CHECKSUM_BEHAVIOR=ignore yarn install --immutable
 npm run fetch-drupal-cache
 npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' --apps-directory-name=application --no-drupal-proxy
 npm run heroku-serve -- build/localhost -p 3002
