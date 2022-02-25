77c1443209acfabd5b4c577d75785d6a009ca418
Tim Wright <timwright12@gmail.com>
2022-02-25 12:28:43 -0500

    YARN_CHECKSUM_BEHAVIOR=update

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -2,12 +2,12 @@
 
 # Build vets-website
 set -e
-yarn install --immutable
+YARN_CHECKSUM_BEHAVIOR=ignore yarn install --immutable
 npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}'
 
 # Build content-build and serve site
 cd ../content-build
-yarn install --immutable
+YARN_CHECKSUM_BEHAVIOR=ignore yarn install --immutable
 npm run fetch-drupal-cache
 npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' --apps-directory-name=application
 npm run heroku-serve -- build/localhost -p 3002
