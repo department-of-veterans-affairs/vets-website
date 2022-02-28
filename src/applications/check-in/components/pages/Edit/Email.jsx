91c965cac236e74f502c38459dd4afd58a03c86e
Mark Dewey <mdewey@governmentcio.com>
2022-02-28 16:24:49 -0500

    (feature) added dummy page for edit

--- src/applications/check-in/components/pages/Edit/Email.jsx
+++ src/applications/check-in/components/pages/Edit/Email.jsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
 
-export default function Email() {
-  return <div>Email</div>;
+import { useSelector } from 'react-redux';
+
+import { useFormRouting } from '../../../hooks/useFormRouting';
+import { makeSelectEditContext } from '../../../selectors';
+import CancelButton from './shared/CancelButton';
+
+export default function Email(props) {
+  const { router } = props;
+  const { jumpToPage } = useFormRouting(router);
+  const selectEditContext = useMemo(makeSelectEditContext, []);
+  const { editing } = useSelector(selectEditContext);
+  const { editingPage, key, originatingUrl, value } = editing;
+  return (
+    <div>
+      <h1>Update your email</h1>
+      <p>editing Page {editingPage}</p>
+      <p>key {key}</p>
+      <p>originatingUrl {originatingUrl}</p>
+      <p>value {value}</p>
+      <CancelButton jumpToPage={jumpToPage} backPage={originatingUrl} />
+    </div>
+  );
 }
+
+Email.propTypes = {
+  router: PropTypes.object,
+};
