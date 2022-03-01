cbbc804882eb18d519321dce76924173617cb8fd
Tony Williams <tony.williams@va.gov>
2022-03-01 14:10:05 -0600

    Fix e2e test.

--- src/applications/vaos/tests/e2e/community-care.cypress.spec.js
+++ src/applications/vaos/tests/e2e/community-care.cypress.spec.js
@@ -19,6 +19,14 @@ describe('VAOS community care flow', () => {
       'health-care/schedule-view-va-appointments/appointments/new-appointment/',
     );
 
+    // Wait until the app has been bootstraped. Could have used 'cy.wait' but
+    // this is considered an 'anti-pattern'. Tried to wait for the loading
+    // indicator but multiple indicators are displayed. So, we are waiting for
+    // the appointment cards to be displayed.
+    cy.findAllByRole('link', { name: /Details for appointment/ }).should(
+      'exist',
+    );
+
     // we should re-direct to home page
     cy.get('h2', { timeout: Timeouts.slow })
       .should('be.visible')
@@ -226,6 +234,14 @@ describe('VAOS community care flow', () => {
     initCommunityCareMock();
     cy.visit(
       'health-care/schedule-view-va-appointments/appointments/new-appointment/',
+    );
+
+    // Wait until the app has been bootstraped. Could have used 'cy.wait' but
+    // this is considered an 'anti-pattern'. Tried to wait for the loading
+    // indicator but multiple indicators are displayed. So, we are waiting for
+    // the appointment cards to be displayed.
+    cy.findAllByRole('link', { name: /Details for appointment/ }).should(
+      'exist',
     );
 
     // we should re-direct to home page
@@ -694,6 +710,12 @@ describe('VAOS community care flow using VAOS service', () => {
       expect(request.requestedPeriods[0].start).to.equal(date);
       expect(request.practitioners).to.deep.eq([
         {
+          address: {
+            city: 'WASHINGTON',
+            line: ['1012 14TH ST NW STE 700'],
+            postalCode: '20005-3477',
+            state: 'DC',
+          },
           identifier: [
             {
               system: 'http://hl7.org/fhir/sid/us-npi',
