9f74e2defb4378bc7fe5cf9d343f9f3ebe184bee
dcgoodwin2112 <dcgoodwin2112@gmail.com>
2022-03-02 06:24:05 -0500

    Remove demographics flag API call from Check-in page.

--- src/applications/check-in/day-of/pages/CheckIn/index.jsx
+++ src/applications/check-in/day-of/pages/CheckIn/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState, useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { api } from '../../../api';
@@ -6,18 +6,15 @@ import { useFormRouting } from '../../../hooks/useFormRouting';
 import { receivedMultipleAppointmentDetails } from '../../../actions/day-of';
 
 import DisplayMultipleAppointments from './DisplayMultipleAppointments';
-import { URLS } from '../../../utils/navigation';
 
 import {
   makeSelectVeteranData,
   makeSelectCurrentContext,
-  makeSelectForm,
 } from '../../../selectors';
 
 const CheckIn = props => {
-  const { isDayOfDemographicsFlagsEnabled } = props;
   const { router } = props;
-  const { goToErrorPage, jumpToPage } = useFormRouting(router);
+  const { goToErrorPage } = useFormRouting(router);
   const selectVeteranData = useMemo(makeSelectVeteranData, []);
   const { appointments } = useSelector(selectVeteranData);
   const selectCurrentContext = useMemo(makeSelectCurrentContext, []);
@@ -27,51 +24,6 @@ const CheckIn = props => {
 
   const { token } = context;
   const dispatch = useDispatch();
-
-  const selectForm = useMemo(makeSelectForm, []);
-  const { data } = useSelector(selectForm);
-  // These will be undefined if pages are skipped in day-of. Default to 'yes'
-  const {
-    demographicsUpToDate = 'yes',
-    emergencyContactUpToDate = 'yes',
-    nextOfKinUpToDate = 'yes',
-  } = data;
-
-  const [demographicsFlagsSent, setDemographicsFlagsSent] = useState(false);
-
-  useEffect(
-    () => {
-      if (!isDayOfDemographicsFlagsEnabled) return;
-      const demographicsData = {
-        uuid: token,
-        demographicsUpToDate: demographicsUpToDate === 'yes',
-        emergencyContactUpToDate: emergencyContactUpToDate === 'yes',
-        nextOfKinUpToDate: nextOfKinUpToDate === 'yes',
-      };
-      try {
-        api.v2.patchDayOfDemographicsData(demographicsData).then(resp => {
-          if (resp.data.error || resp.data.errors) {
-            goToErrorPage();
-          } else if (Object.values(demographicsData).includes(false)) {
-            jumpToPage(URLS.SEE_STAFF);
-          } else {
-            setDemographicsFlagsSent(true);
-          }
-        });
-      } catch (error) {
-        goToErrorPage();
-      }
-    },
-    [
-      demographicsUpToDate,
-      emergencyContactUpToDate,
-      goToErrorPage,
-      isDayOfDemographicsFlagsEnabled,
-      jumpToPage,
-      nextOfKinUpToDate,
-      token,
-    ],
-  );
 
   const getMultipleAppointments = useCallback(
     () => {
@@ -96,17 +48,12 @@ const CheckIn = props => {
     [dispatch, context, goToErrorPage, token],
   );
 
-  if (isDayOfDemographicsFlagsEnabled) {
-    if (!appointment || !demographicsFlagsSent) {
-      return (
-        <va-loading-indicator message="Loading your appointments for today" />
-      );
-    }
-  } else if (!appointment) {
+  if (!appointment) {
     return (
       <va-loading-indicator message="Loading your appointments for today" />
     );
   }
+
   return (
     <DisplayMultipleAppointments
       router={router}
