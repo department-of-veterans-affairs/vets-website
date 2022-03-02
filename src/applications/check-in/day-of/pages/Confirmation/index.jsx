a97907cfb59af9715ce7edf4e945e91797c4ff44
dcgoodwin2112 <dcgoodwin2112@gmail.com>
2022-03-02 06:25:09 -0500

    Add demographics API call to Check-in Confirmation page.

--- src/applications/check-in/day-of/pages/Confirmation/index.jsx
+++ src/applications/check-in/day-of/pages/Confirmation/index.jsx
@@ -1,13 +1,24 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { api } from '../../../api';
 import MultipleAppointment from './MultipleAppointments';
-
 import { triggerRefresh } from '../../../actions/day-of';
-
 import { makeSelectConfirmationData } from '../../../selectors';
 
-const Confirmation = () => {
+import { useDemographicsFlags } from '../../../hooks/useDemographicsFlags';
+import { useFormRouting } from '../../../hooks/useFormRouting';
+
+const Confirmation = props => {
+  const { router } = props;
+  const { isDayOfDemographicsFlagsEnabled } = props;
+  const { goToErrorPage } = useFormRouting(router);
+  const {
+    demographicsData,
+    demographicsFlagsSent,
+    setDemographicsFlagsSent,
+  } = useDemographicsFlags(true);
   const dispatch = useDispatch();
   const refreshAppointments = useCallback(
     () => {
@@ -21,6 +32,30 @@ const Confirmation = () => {
     selectConfirmationData,
   );
 
+  useEffect(
+    () => {
+      if (!isDayOfDemographicsFlagsEnabled || demographicsFlagsSent) return;
+      try {
+        api.v2.patchDayOfDemographicsData(demographicsData).then(resp => {
+          if (resp.data.error || resp.data.errors) {
+            goToErrorPage();
+          } else {
+            setDemographicsFlagsSent(true);
+          }
+        });
+      } catch (error) {
+        goToErrorPage();
+      }
+    },
+    [
+      demographicsData,
+      demographicsFlagsSent,
+      goToErrorPage,
+      isDayOfDemographicsFlagsEnabled,
+      setDemographicsFlagsSent,
+    ],
+  );
+
   return (
     <MultipleAppointment
       selectedAppointment={selectedAppointment}
@@ -28,6 +63,11 @@ const Confirmation = () => {
       triggerRefresh={refreshAppointments}
     />
   );
+};
+
+Confirmation.propTypes = {
+  isDayOfDemographicsFlagsEnabled: PropTypes.bool,
+  router: PropTypes.object,
 };
 
 export default Confirmation;
