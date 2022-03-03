14fece11d99d81688a566dc9f35c89bcb4343dd0
Andrew Rodiek <andrew.rodiek@gmail.com>
2022-03-03 11:17:23 -0500

    30744: [MDOT] AlertBox to va-alert (#20403)

--- src/applications/disability-benefits/2346/components/ErrorMessage.jsx
+++ src/applications/disability-benefits/2346/components/ErrorMessage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import AlertBox from '@department-of-veterans-affairs/component-library/AlertBox';
 import moment from 'moment';
 
 const ErrorMessage = ({ errorCode, nextAvailabilityDate }) => {
@@ -8,13 +7,11 @@ const ErrorMessage = ({ errorCode, nextAvailabilityDate }) => {
   switch (errorCode) {
     case 'MDOT_SUPPLIES_INELIGIBLE':
       content = (
-        <AlertBox
-          status="warning"
-          headline="You can't reorder your items at this time"
-        >
+        <va-alert status="warning">
+          <h3 slot="headline">You can’t reorder your items at this time</h3>
           <div className="vads-u-display--flex vads-u-flex-direction--column">
             <span>
-              Our records show that your items aren't available for reorder
+              Our records show that your items aren’t available for reorder
               until {moment(nextAvailabilityDate).format('MMMM DD, YYYY')}. You
               can only order items once every 5 months.
             </span>
@@ -24,19 +21,17 @@ const ErrorMessage = ({ errorCode, nextAvailabilityDate }) => {
               <a href="mailto:dalc.css@va.gov">dalc.css@va.gov</a>.
             </span>
           </div>
-        </AlertBox>
+        </va-alert>
       );
       break;
     case 'MDOT_INVALID':
       content = (
-        <AlertBox
-          status="warning"
-          headline="We can't find your records in our system"
-        >
+        <va-alert status="warning">
+          <h3 slot="headline">We can’t find your records in our system</h3>
           <div className="vads-u-display--flex vads-u-flex-direction--column">
             <span>
-              You can't order hearing aid batteries or accessories at this time
-              because we can't find your records in our system or we're missing
+              You can’t order hearing aid batteries or accessories at this time
+              because we can’t find your records in our system or we’re missing
               some information needed for you to order.
             </span>
 
@@ -52,19 +47,17 @@ const ErrorMessage = ({ errorCode, nextAvailabilityDate }) => {
               </a>
             </span>
           </div>
-        </AlertBox>
+        </va-alert>
       );
       break;
     case 'MDOT_SUPPLIES_NOT_FOUND':
       content = (
-        <AlertBox
-          status="warning"
-          headline="You can't reorder your items at this time"
-        >
+        <va-alert status="warning">
+          <h3 slot="headline">You can’t reorder your items at this time</h3>
           <div className="vads-u-display--flex vads-u-flex-direction--column">
             <span>
-              You can't order hearing aid batteries or accessories online at
-              this time because you haven't placed an order within the past two
+              You can’t order hearing aid batteries or accessories online at
+              this time because you haven’t placed an order within the past two
               years
             </span>
 
@@ -74,36 +67,34 @@ const ErrorMessage = ({ errorCode, nextAvailabilityDate }) => {
               <a href="mailto:dalc.css@va.gov">dalc.css@va.gov</a>.
             </span>
           </div>
-        </AlertBox>
+        </va-alert>
       );
       break;
     case 'MDOT_DECEASED':
       content = (
-        <AlertBox
-          status="warning"
-          headline="Our records show that this Veteran is deceased"
-        >
+        <va-alert status="warning">
+          <h3 slot="headline">
+            Our records show that this Veteran is deceased
+          </h3>
           <div className="vads-u-display--flex vads-u-flex-direction--column">
-            <span>We can't fulfill an order for this Veteran</span>
+            <span>We can’t fulfill an order for this Veteran</span>
             <span className="vads-u-margin-top--1">
               If this information is incorrect, please call Veterans Benefits
               Assistance at <a href="tel:800-827-1000">800-827-1000</a>, Monday
               through Friday, 8:00 a.m. to 9:00 p.m. E.T.
             </span>
           </div>
-        </AlertBox>
+        </va-alert>
       );
       break;
     case 'MDOT_SERVICE_UNAVAILABLE':
     case 'MDOT_SERVER_ERROR':
       content = (
-        <AlertBox
-          status="error"
-          headline="We're sorry. Something went wrong on our end."
-        >
+        <va-alert status="error">
+          <h3 slot="headline">We’re sorry. Something went wrong on our end.</h3>
           <div className="mdot-server-error-alert">
             <p>
-              You can't place an order for hearing aid batteries and accessories
+              You can’t place an order for hearing aid batteries and accessories
               because something went wrong on our end.
             </p>
             <p className="vads-u-font-weight--bold vads-u-margin-y--1 vads-u-font-family--serif">
@@ -118,7 +109,7 @@ const ErrorMessage = ({ errorCode, nextAvailabilityDate }) => {
               or email <a href="mailto:dalc.css@va.gov">dalc.css@va.gov</a>.
             </p>
           </div>
-        </AlertBox>
+        </va-alert>
       );
       break;
     default:
