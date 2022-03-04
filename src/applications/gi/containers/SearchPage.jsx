5695f085392d50b48eb2aec0946aab4712ffe15b
David <dsrr74@gmail.com>
2022-03-04 15:19:16 -0600

    removed extra import environment

--- src/applications/gi/containers/SearchPage.jsx
+++ src/applications/gi/containers/SearchPage.jsx
@@ -8,7 +8,6 @@ import { useHistory } from 'react-router-dom';
 import classNames from 'classnames';
 import environment from 'platform/utilities/environment';
 import recordEvent from 'platform/monitoring/record-event';
-import environment from 'platform/utilities/environment';
 import SearchTabs from '../components/search/SearchTabs';
 import { TABS } from '../constants';
 import NameSearchResults from './search/NameSearchResults';
