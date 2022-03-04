6faf6bb377fbf40415ffd26dc559f5290789fc02
Brian Seek <brian.seek@civicactions.com>
2022-03-04 15:45:45 -0800

    Added provide to NOK display tests.

--- src/applications/check-in/components/pages/nextOfKin/NextOfKinDisplay.unit.spec.jsx
+++ src/applications/check-in/components/pages/nextOfKin/NextOfKinDisplay.unit.spec.jsx
@@ -2,17 +2,42 @@ import React from 'react';
 import { expect } from 'chai';
 import { render, fireEvent } from '@testing-library/react';
 import sinon from 'sinon';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 import { axeCheck } from 'platform/forms-system/test/config/helpers';
 import NextOfKinDisplay from './NextOfKinDisplay';
 
 describe('pre-check-in experience', () => {
   describe('shared components', () => {
     describe('NextOfKinDisplay', () => {
+      let store;
+      beforeEach(() => {
+        const middleware = [];
+        const mockStore = configureStore(middleware);
+        const initState = {
+          checkInData: {
+            context: {
+              token: '',
+              editing: {},
+            },
+            form: {},
+          },
+        };
+        store = mockStore(initState);
+      });
       it('passes axeCheck', () => {
-        axeCheck(<NextOfKinDisplay />);
+        axeCheck(
+          <Provider store={store}>
+            <NextOfKinDisplay />
+          </Provider>,
+        );
       });
       it('renders with default values', () => {
-        const { getByText } = render(<NextOfKinDisplay />);
+        const { getByText } = render(
+          <Provider store={store}>
+            <NextOfKinDisplay />
+          </Provider>,
+        );
 
         expect(getByText('Is this your current next of kin information?')).to
           .exist;
@@ -20,17 +45,26 @@ describe('pre-check-in experience', () => {
       it('renders the footer if footer is supplied', () => {
         const { getByText } = render(
           // eslint-disable-next-line react/jsx-no-bind
-          <NextOfKinDisplay Footer={() => <div>foo</div>} />,
+          <Provider store={store}>
+            <NextOfKinDisplay Footer={() => <div>foo</div>} />
+          </Provider>,
         );
         expect(getByText('foo')).to.exist;
       });
       it('renders custom header', () => {
-        const { getByText } = render(<NextOfKinDisplay header="foo" />);
-
+        const { getByText } = render(
+          <Provider store={store}>
+            <NextOfKinDisplay header="foo" />
+          </Provider>,
+        );
         expect(getByText('foo')).to.exist;
       });
       it('renders custom subtitle', () => {
-        const { getByText } = render(<NextOfKinDisplay subtitle="foo" />);
+        const { getByText } = render(
+          <Provider store={store}>
+            <NextOfKinDisplay subtitle="foo" />
+          </Provider>,
+        );
         expect(getByText('foo')).to.exist;
       });
       it('renders next of kin labels', () => {
@@ -52,7 +86,9 @@ describe('pre-check-in experience', () => {
           },
         };
         const { getByText } = render(
-          <NextOfKinDisplay nextOfKin={nextOfKinData} />,
+          <Provider store={store}>
+            <NextOfKinDisplay nextOfKin={nextOfKinData} />
+          </Provider>,
         );
         expect(getByText('Name')).to.exist;
         expect(getByText('Relationship')).to.exist;
@@ -79,7 +115,9 @@ describe('pre-check-in experience', () => {
           },
         };
         const { getByText } = render(
-          <NextOfKinDisplay nextOfKin={nextOfKinData} />,
+          <Provider store={store}>
+            <NextOfKinDisplay nextOfKin={nextOfKinData} />
+          </Provider>,
         );
         expect(getByText('VETERAN,JONAH')).to.exist;
         expect(getByText('BROTHER')).to.exist;
@@ -91,24 +129,40 @@ describe('pre-check-in experience', () => {
       });
       it('fires the yes function', () => {
         const yesClick = sinon.spy();
-        const screen = render(<NextOfKinDisplay yesAction={yesClick} />);
+        const screen = render(
+          <Provider store={store}>
+            <NextOfKinDisplay yesAction={yesClick} />
+          </Provider>,
+        );
         fireEvent.click(screen.getByTestId('yes-button'));
         expect(yesClick.calledOnce).to.be.true;
       });
       it('fires the no function', () => {
         const noClick = sinon.spy();
-        const screen = render(<NextOfKinDisplay noAction={noClick} />);
+        const screen = render(
+          <Provider store={store}>
+            <NextOfKinDisplay noAction={noClick} />
+          </Provider>,
+        );
         fireEvent.click(screen.getByTestId('no-button'));
         expect(noClick.calledOnce).to.be.true;
       });
       it('renders the loading message', () => {
-        const screen = render(<NextOfKinDisplay isSendingData />);
+        const screen = render(
+          <Provider store={store}>
+            <NextOfKinDisplay isSendingData />
+          </Provider>,
+        );
         expect(screen.queryByTestId('no-button')).to.not.exist;
         expect(screen.queryByTestId('yes-button')).to.not.exist;
         expect(screen.getByTestId('loading-message')).to.exist;
       });
       it('renders the buttons', () => {
-        const screen = render(<NextOfKinDisplay isSendingData={false} />);
+        const screen = render(
+          <Provider store={store}>
+            <NextOfKinDisplay isSendingData={false} />
+          </Provider>,
+        );
         expect(screen.getByTestId('no-button')).to.exist;
         expect(screen.getByTestId('yes-button')).to.exist;
       });
