f52863e1a4e475e4f3883b7025254db205277ceb
Lisa Zapson <lisazapson@ymail.com>
2022-03-07 14:58:25 -0500

    formated full name

--- src/applications/hca/components/PersonalAuthenticatedInformation.jsx
+++ src/applications/hca/components/PersonalAuthenticatedInformation.jsx
@@ -3,10 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import moment from 'moment';
 
-import Telephone, {
-  CONTACTS,
-  PATTERNS,
-} from '@department-of-veterans-affairs/component-library/Telephone';
+import { CONTACTS } from '@department-of-veterans-affairs/component-library/Telephone';
 
 import FormNavButtons from '~/platform/forms-system/src/js/components/FormNavButtons';
 
@@ -44,7 +41,7 @@ const PersonalAuthenticatedInformation = ({
                 <p className="vads-u-margin--1px">
                   <strong>
                     {' '}
-                    {first} {middle} {last} {suffix}
+                    {first || ''} {middle || ''} {last || ''} {suffix || ''}
                   </strong>
                 </p>
                 <p className="vads-u-margin--1px">
@@ -59,16 +56,13 @@ const PersonalAuthenticatedInformation = ({
               <strong>Note: </strong>
               If you need to update your personal information, call our VA
               benefits hotline at
-              <Telephone
-                contact={CONTACTS.VA_BENEFITS}
-                className="vads-u-margin-x--0p5"
-              />
+              <span className="vads-u-padding-x--0p5">
+                <va-telephone contact={CONTACTS.VA_BENEFITS} />
+              </span>
               (TTY:
-              <Telephone
-                contact={CONTACTS[711]}
-                pattern={PATTERNS['3_DIGIT']}
-                className="vads-u-margin-left--0p5"
-              />
+              <span className="vads-u-padding-left--0p5">
+                <va-telephone contact={CONTACTS[711]} />
+              </span>
               ), Monday through Friday, 8:00 a.m. to 9:00 p.m. ET.
             </p>
             <p>
