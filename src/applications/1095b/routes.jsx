cb1101a7fe478e938fefc3e9958a3cb5a7bdc88e
Nadya Primak <nadyaprimak@Nadyas-MacBook-Pro.local>
2022-03-08 17:29:01 -0500

    remove 1095b app (using widget)

--- src/applications/1095b/routes.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Route } from 'react-router';
-import App from './containers/App.jsx';
-
-const routes = <Route path="/" component={App} />;
-
-export default routes;
