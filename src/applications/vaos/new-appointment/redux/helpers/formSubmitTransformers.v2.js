1c19e67b0cc0db68ed40c6056a9787812cd3ca13
Tony Williams <tony.williams@va.gov>
2022-03-08 11:57:21 -0600

    Modified per code review

--- src/applications/vaos/new-appointment/redux/helpers/formSubmitTransformers.v2.js
+++ src/applications/vaos/new-appointment/redux/helpers/formSubmitTransformers.v2.js
@@ -43,7 +43,7 @@ export function transformFormToVAOSCCRequest(state) {
 
   if (
     residentialAddress &&
-    residentialAddress.addressType !== 'MILITARY OVERSEAS'
+    residentialAddress.addressType !== 'OVERSEAS MILITARY'
   ) {
     preferredLocation = {
       city: residentialAddress.city,
