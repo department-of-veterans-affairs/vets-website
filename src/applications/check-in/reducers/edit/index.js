71ed829dc21ffe8bf4c32a4ab6ee9c02b497ce9d
Mark Dewey <mdewey@users.noreply.github.com>
2022-03-08 14:04:51 -0500

    Check in/feature/37620/edit emails (#20415)
    * (feature) added required and emailt validation; added email UI
    
    * (feature) sorta rough updated saving updated values to email
    
    * (feature) added API calls to the demographics page
    
    * (feature) sending new data only if its updated
    
    * (test) added redux tests
    
    * (feature) added check to use existing data when possible
    
    * (test) added update button tests
    
    * (test) added test to show updated information instead of old info

--- src/applications/check-in/reducers/edit/index.js
+++ src/applications/check-in/reducers/edit/index.js
@@ -1,3 +1,5 @@
+import { EDITING_PAGE_NAMES } from '../../utils/appConstants';
+
 const setEditingContext = (state, action) => {
   return {
     ...state,
@@ -14,4 +16,26 @@ const clearEditingContext = state => {
   return next;
 };
 
-export { setEditingContext, clearEditingContext };
+const setPendingEditedData = (state, action) => {
+  const { fieldsToUpdate, editingPage } = action.payload;
+
+  if (editingPage === EDITING_PAGE_NAMES.DEMOGRAPHICS) {
+    const { demographics } = state.veteranData;
+    const { pendingEdits } = state.context;
+    const currentData = pendingEdits?.demographics || demographics;
+    const nextDemo = { ...currentData, ...fieldsToUpdate };
+
+    delete nextDemo.emergencyContact;
+    delete nextDemo.nextOfKin1;
+    return {
+      ...state,
+      context: {
+        ...state.context,
+        pendingEdits: { demographics: nextDemo },
+      },
+    };
+  }
+  return { ...state };
+};
+
+export { setEditingContext, clearEditingContext, setPendingEditedData };
