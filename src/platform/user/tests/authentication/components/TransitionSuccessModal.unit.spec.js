f39de1d516071ef4a98c88c1bb90d09009d86c10
Alexander Garcia <agarcia@clarityinnovates.com>
2022-03-08 14:35:22 -0500

    adds TransitionSuccessModal unit test

--- src/platform/user/tests/authentication/components/TransitionSuccessModal.unit.spec.js
+++ src/platform/user/tests/authentication/components/TransitionSuccessModal.unit.spec.js
@@ -1,13 +1,35 @@
-// import React from 'react';
-// import { expect } from 'chai';
-// import { mount } from 'enzyme';
-// import sinon from 'sinon';
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+import TransitionSuccessModal from '../../../authentication/components/account-transition/TransitionSuccessModal';
 
-// import * as authUtilities from '../../../authentication/utilities';
-// import LoginActions from '../../../authentication/components/LoginActions';
-// import { CSP_IDS } from '../../../authentication/constants';
+describe('', () => {
+  let wrapper;
+  const props = { visible: false, onClose: sinon.spy() };
 
-// describe('', () => {
-//   it('should display when `` is set to true');
-//   it('should')
-// });
+  beforeEach(() => {
+    wrapper = shallow(<TransitionSuccessModal {...props} />);
+  });
+  it('should display when `visible` is set to true', () => {
+    expect(wrapper.prop('visible')).to.be.false;
+    wrapper.setProps({ visible: true });
+    expect(wrapper.prop('visible')).to.be.true;
+    wrapper.unmount();
+  });
+
+  it('should include `Congratulations` in the modal', () => {
+    expect(wrapper.find('.container').text()).to.include('Congratulations');
+    wrapper.unmount();
+  });
+
+  it('should close the modal when primaryButton action is called', () => {
+    wrapper = mount(<TransitionSuccessModal {...props} />);
+    const mutatedProps = { visible: true, onClose: sinon.spy() };
+    wrapper.setProps(mutatedProps);
+
+    wrapper.find('.usa-button').simulate('click');
+    expect(wrapper.prop('onClose').calledOnce).to.be.true;
+    wrapper.unmount();
+  });
+});
