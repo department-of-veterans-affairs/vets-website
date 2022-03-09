db0b4f2f50432f0035849292f8fe0bdb333c8a28
Theo Bentum <theo@adhocteam.us>
2022-03-08 16:58:16 -0800

    Update directory

--- script/github-actions/remove-global-assets.sh
+++ script/github-actions/remove-global-assets.sh
@@ -15,7 +15,7 @@ filesToSync="$ENTRIES,$webpackChunkNames"
 echo "File names to sync: $filesToSync"
 
 # Delete non app assets
-mkdir assets/filtered/
-rsync -a --delete $(eval echo "--include={,$filesToSync}.*") --exclude='*' $WORKDIR/ assets/filtered/
-rsync -a --delete assets/filtered/ $WORKDIR/
-rm -rf assets/filtered/
+mkdir assets/
+rsync -a --delete $(eval echo "--include={,$filesToSync}.*") --exclude='*' $WORKDIR/ assets/
+rsync -a --delete assets/ $WORKDIR/
+rm -rf assets/
