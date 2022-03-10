f1e0184beefadbcc2eb7aadfff4b18e12650c307
Andrew Rodiek <andrew.rodiek@gmail.com>
2022-03-10 09:55:42 -0500

    Added page  still WIP

---
+++ src/applications/medical-copays/containers/HTMLStatementPage.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import scrollToTop from 'platform/utilities/ui/scrollToTop';
+import PropTypes from 'prop-types';
+import Modals from '../components/Modals';
+import { OnThisPage } from '../components/OnThisPage';
+import { formatDate } from '../utils/helpers';
+
+const HTMLStatementPage = ({ match }) => {
+  const selectedId = match.params.id;
+  // const [alert, setAlert] = useState('status');
+  const statements = useSelector(({ mcp }) => mcp.statements) ?? [];
+  const [selectedCopay] = statements?.filter(({ id }) => id === selectedId);
+  const statementDate = formatDate(selectedCopay?.pSStatementDate);
+  const title = `${statementDate} statement`;
+  const prevPage = `Copay bill for ${selectedCopay?.station.facilityName}`;
+
+  useEffect(() => {
+    scrollToTop();
+  }, []);
+
+  return (
+    <>
+      <va-breadcrumbs className="vads-u-font-family--sans no-wrap">
+        <a href="/">Home</a>
+        <a href="/health-care">Health care</a>
+        <a href="/health-care/pay-copay-bill">Pay your VA copay bill</a>
+        <a href="/health-care/pay-copay-bill/your-current-balances">
+          {prevPage}
+        </a>
+        <a href="/health-care/pay-copay-bill/your-current-balances/balance-details">
+          {title}
+        </a>
+      </va-breadcrumbs>
+      <h1 data-testid="detail-page-title">{title}</h1>
+      <p className="vads-u-font-size--h3 vads-u-margin-top--0 vads-u-margin-bottom--5">
+        {`${selectedCopay?.station.facilityName}`}
+      </p>
+      <OnThisPage />
+      <h2>Account Summary</h2>
+      <h2>Statement charges</h2>
+      <h2>Statement addresses</h2>
+      <h2>What if I have questions about my statement?</h2>
+      <Modals title="Notice of rights and responsibilities">
+        <Modals.Rights />
+      </Modals>
+      <Link className="vads-u-font-size--sm" to="/">
+        <i
+          className="fa fa-chevron-left vads-u-margin-right--1"
+          aria-hidden="true"
+        />
+        <strong>Return to facility details</strong>
+      </Link>
+    </>
+  );
+};
+
+HTMLStatementPage.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+};
+
+export default HTMLStatementPage;
--- src/applications/medical-copays/routes.jsx
+++ src/applications/medical-copays/routes.jsx
@@ -3,12 +3,16 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 import MedicalCopaysApp from './containers/MedicalCopaysApp';
 import OverviewPage from './containers/OverviewPage';
 import DetailPage from './containers/DetailPage';
+import HTMLStatementPage from './containers/HTMLStatementPage';
 
 const Routes = () => (
   <MedicalCopaysApp>
     <Switch>
       <Route exact path="/" component={OverviewPage} />
       <Route path="/balance-details/:id" component={DetailPage} />
+      <Route path="/statement">
+        <HTMLStatementPage />
+      </Route>
       <Route>
         <Redirect to="/" />
       </Route>
