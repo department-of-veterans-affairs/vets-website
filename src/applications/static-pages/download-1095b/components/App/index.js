6bef59a0d326f5eeb7d7849a56b8ed8d4bb739bf
Nadya Primak <nadyaprimak@Nadyas-MacBook-Pro.local>
2022-03-11 15:58:49 -0500

    remove static content

--- src/applications/static-pages/download-1095b/components/App/index.js
+++ src/applications/static-pages/download-1095b/components/App/index.js
@@ -10,11 +10,6 @@ import ServiceProvidersText, {
 } from 'platform/user/authentication/components/ServiceProvidersText';
 
 export const App = ({ loggedIn, toggleLoginModal }) => {
-  // show is feature toggle, see medical-copays-cta for example
-  // if (!show) {
-  //   return null;
-  // }
-
   const loggedInComponent = (
     <va-alert close-btn-aria-label="Close notification" status="info" visible>
       <h3 slot="headline">Download your 1095-B</h3>
@@ -63,66 +58,16 @@ export const App = ({ loggedIn, toggleLoginModal }) => {
       </button>
     </va-alert>
   );
-  return (
-    <>
-      <h1 className="vads-u-font-size--h1">
-        Download your 1095-B tax document
-      </h1>
-      <p className="va-introtext">
-        The 1095-B is an IRS tax document that shows the period that you had
-        health coverage through the VA for the tax year. Due to changes in the{' '}
-        <a href="http://wwww.link.com">Affordable Care Act</a>, this document is
-        no longer required to file your federal taxes; however some states may
-        still require proof of health coverage.
-      </p>
-      <p className="vads-u-margin-bottom--5">
-        <strong>Note:</strong> Some states may still require proof of health
-        coverage. States requiring proof of coverage as of January 2022:
-        Massachusetts, New Jersey, Vermont, California, Rhode Island and
-        District of Columbia (Washington D.C.)
-      </p>
-      {loggedIn ? loggedInComponent : loggedOutComponent}
-      <h3 className="vads-u-font-size--h3 vads-u-border-bottom--3px vads-u-border-color--primary vads-u-margin-top--5">
-        Need help?
-      </h3>
-      <p className="vads-u-margin-top--3">
-        <span className="vads-u-font-weight--bold">
-          If your address or other information is incorrect or needs to be
-          updated on your 1095-B
-        </span>
-        . Call the enrollment center toll-free at{' '}
-        <a href="http://wwww.link.com">1-877-222-VETS (8387)</a> Monday through
-        Friday, 8:00 am until 8:00 pm (EST). Changes may take up to 10 business
-        days to process and for you to receive your updated document.
-      </p>
-      <p className="vads-u-margin-top--3 vads-u-margin-bottom--5">
-        <span className="vads-u-font-weight--bold">
-          If you’re having trouble viewing your 1095-B.
-        </span>{' '}
-        To view your 1095-B after download, you may need the latest version of
-        Adobe Acrobat Reader. It’s free to download.{' '}
-        <a href="http://wwww.link.com">
-          Get Acrobat Reader for free from Adobe
-        </a>
-        .
-      </p>
-      <div className="last-updated usa-content vads-u-padding-x--1 large-screen:vads-u-padding-x--0">
-        Last updated: December 12, 2021
-      </div>
-    </>
-  );
+  return <>{loggedIn ? loggedInComponent : loggedOutComponent}</>;
 };
 
 App.propTypes = {
-  // From mapStateToProps.
   loggedIn: PropTypes.bool,
-  // show: PropTypes.bool,
-  // From mapDispatchToProps.
   toggleLoginModal: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
-  loggedIn: state?.user?.login?.currentlyLoggedIn || false,
+  loggedIn: state?.user?.login?.currentlyLoggedIn || null,
 });
 
 const mapDispatchToProps = dispatch => ({
