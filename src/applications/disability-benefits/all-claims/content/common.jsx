9830f806e65ae4b40ff7a400a8a92bc75f596738
Robin Garrison <robin.garrison@adhocteam.us>
2022-03-14 12:38:55 -0500

    Update to va-telephone & fix lint issues

--- src/applications/disability-benefits/all-claims/content/common.jsx
+++ src/applications/disability-benefits/all-claims/content/common.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import Telephone, {
-  CONTACTS,
-} from '@department-of-veterans-affairs/component-library/Telephone';
+import { CONTACTS } from '@department-of-veterans-affairs/component-library/Telephone';
 
 export const editNote = name => (
   <p>
     <strong>Note:</strong> If you need to update your {name}, please call
-    Veterans Benefits Assistance at <Telephone contact={CONTACTS.VA_BENEFITS} />
-    , Monday through Friday, 8:00 a.m. to 9:00 p.m. ET.
+    Veterans Benefits Assistance at{' '}
+    <va-telephone contact={CONTACTS.VA_BENEFITS} />, Monday through Friday, 8:00
+    a.m. to 9:00 p.m. ET.
   </p>
 );
 
@@ -23,7 +22,7 @@ export const ContactCrisis = () => (
     <ul>
       <li>
         Call the Veterans Crisis Line at{' '}
-        <Telephone contact={CONTACTS.CRISIS_LINE} /> and press 1,{' '}
+        <va-telephone contact={CONTACTS.CRISIS_LINE} /> and press 1,{' '}
         <strong>or</strong>
       </li>
       <li>
@@ -34,12 +33,16 @@ export const ContactCrisis = () => (
         to start a confidential chat online, <strong>or</strong>
       </li>
       <li>
-        Send a text message to <a href="sms:838255">838255</a>.
+        Send a{' '}
+        <a href="sms:838255" aria-label="8 3 8 2 5 5.">
+          text to 838255
+        </a>
+        .
       </li>
     </ul>
     <p>
       If you have hearing loss, please call TTY at{' '}
-      <Telephone contact={CONTACTS.CRISIS_TTY} />.
+      <va-telephone contact={CONTACTS.CRISIS_TTY} />.
     </p>
   </>
 );
