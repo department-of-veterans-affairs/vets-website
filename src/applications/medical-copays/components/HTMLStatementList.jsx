538093aa97485ccd50b6b2b4dcd2f73b0cb9048d
Andrew Rodiek <andrew.rodiek@gmail.com>
2022-03-14 09:39:04 -0400

    Broke out link component, cleaned up statement ID retrival

--- src/applications/medical-copays/components/HTMLStatementList.jsx
+++ src/applications/medical-copays/components/HTMLStatementList.jsx
@@ -1,26 +1,49 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useLocation, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 import { sortStatementsByDate } from '../utils/helpers';
 
-const HTMLStatementList = () => {
-  const { pathname } = useLocation();
-  const selectedId = pathname.replace('/balance-details/', '');
+const HTMLStatementLink = ({ id, statementDate }) => {
+  const formattedStatementDate = date => {
+    return moment(date, 'MM-DD-YYYY').format('MMMM D, YYYY');
+  };
+
+  return (
+    <Link
+      className="vads-u-font-size--sm"
+      to={`/balance-details/${id}/statement-view`}
+      data-testid="statement"
+    >
+      <span aria-hidden="true">
+        {formattedStatementDate(statementDate)} statement{' '}
+      </span>
+      <span className="sr-only">
+        Download {formattedStatementDate(statementDate)} dated medical copay
+        statement
+      </span>
+    </Link>
+  );
+};
+
+HTMLStatementLink.propTypes = {
+  id: PropTypes.string,
+  statementDate: PropTypes.string,
+};
+
+const HTMLStatementList = ({ selectedId }) => {
   const statements = useSelector(({ mcp }) => mcp.statements) ?? [];
   // get selected statement
-  const [selectedCopay] = statements?.filter(({ id }) => id === selectedId);
+  const [selectedCopay] = statements.filter(({ id }) => id === selectedId);
   // get facility  number on selected statement
-  const facilityNumber = selectedCopay?.pSFacilityNum;
+  const facilityNumber = selectedCopay.pSFacilityNum;
   // filter out all statements that are not related to this facility
-  const facilityCopays = statements?.filter(
+  const facilityCopays = statements.filter(
     ({ pSFacilityNum }) => pSFacilityNum === facilityNumber,
   );
 
   const sortedFacilityCopays = sortStatementsByDate(facilityCopays);
-  const formattedStatementDate = date => {
-    return moment(date, 'MM-DD-YYYY').format('MMMM D, YYYY');
-  };
 
   return (
     <section data-testid="download-statements">
@@ -31,25 +54,18 @@ const HTMLStatementList = () => {
       </p>
 
       {sortedFacilityCopays.map(statement => (
-        <>
-          <Link
-            className="vads-u-font-size--sm"
-            to={`/balance-details/${statement.id}/statement-view`}
-            data-testid="statement"
-            key={statement.id}
-          >
-            <span aria-hidden="true">
-              {formattedStatementDate(statement.pSStatementDate)} statement{' '}
-            </span>
-            <span className="sr-only">
-              Download {formattedStatementDate(statement.pSStatementDate)} dated
-              medical copay statement
-            </span>
-          </Link>
-        </>
+        <HTMLStatementLink
+          id={statement.id}
+          statementDate={statement.pSStatementDate}
+          key={statement.id}
+        />
       ))}
     </section>
   );
+};
+
+HTMLStatementList.propTypes = {
+  selectedId: PropTypes.string,
 };
 
 export default HTMLStatementList;
