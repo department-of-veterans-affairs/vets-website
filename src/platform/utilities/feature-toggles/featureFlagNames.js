5ccc4b5ea3e7e9d82942500419c85d9068ca7bdb
Kyle Woolley <iamkylewoolley@gmail.com>
2022-03-14 13:00:41 -0500

    [DON'T MERGE] 37930: Make Login.gov visible by default and hide via flipper (#20452)
    * make login gov visible by default
    
    * add login.gov to string test case

--- src/platform/utilities/feature-toggles/featureFlagNames.js
+++ src/platform/utilities/feature-toggles/featureFlagNames.js
@@ -60,6 +60,7 @@ export default Object.freeze({
   hlrv2: 'hlr_v2',
   loginGov: 'login_gov',
   loginGovCreateAccount: 'login_gov_create_account',
+  loginGovDisabled: 'login_gov_disabled',
   loginGovMHV: 'login_gov_mhv',
   loginGovMyVAHealth: 'login_gov_myvahealth',
   loopPages: 'loop_pages',
