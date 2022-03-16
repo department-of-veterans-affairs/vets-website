eda8545bbc79cfa1969ac4d9c7ca3daccaaae60e
OptionSelect <brandyn.sullins@gmail.com>
2022-03-16 09:31:30 -0400

    Fixed OnThisPage

--- src/applications/medical-copays/containers/HTMLStatementPage.jsx
+++ src/applications/medical-copays/containers/HTMLStatementPage.jsx
@@ -23,50 +23,52 @@ const HTMLStatementPage = ({ match }) => {
 
   return (
     <>
-      <va-breadcrumbs className="vads-u-font-family--sans no-wrap">
-        <a href="/">Home</a>
-        <a href="/health-care">Health care</a>
-        <a href="/health-care/pay-copay-bill">Pay your VA copay bill</a>
-        <a
-          href={`/health-care/pay-copay-bill/your-current-balances/balance-details/${selectedId}`}
+      <article>
+        <va-breadcrumbs className="vads-u-font-family--sans no-wrap">
+          <a href="/">Home</a>
+          <a href="/health-care">Health care</a>
+          <a href="/health-care/pay-copay-bill">Pay your VA copay bill</a>
+          <a
+            href={`/health-care/pay-copay-bill/your-current-balances/balance-details/${selectedId}`}
+          >
+            {prevPage}
+          </a>
+          <a href={`/balance-details/${selectedId}/statement-view`}>{title}</a>
+        </va-breadcrumbs>
+        <h1 data-testid="statement-page-title">{title}</h1>
+        <p
+          className="vads-u-font-size--h3 vads-u-margin-top--0 vads-u-margin-bottom--5"
+          data-testid="facility-name"
         >
-          {prevPage}
-        </a>
-        <a href={`/balance-details/${selectedId}/statement-view`}>{title}</a>
-      </va-breadcrumbs>
-      <h1 data-testid="statement-page-title">{title}</h1>
-      <p
-        className="vads-u-font-size--h3 vads-u-margin-top--0 vads-u-margin-bottom--5"
-        data-testid="facility-name"
-      >
-        {`${selectedCopay?.station.facilityName}`}
-      </p>
-      <Link
-        className="vads-u-font-size--sm"
-        to={`/balance-details/${selectedId}`}
-      >
-        <i
-          className="fa fa-chevron-left vads-u-margin-right--1"
-          aria-hidden="true"
-        />
-        <strong>Return to facility details</strong>
-      </Link>
-      <va-on-this-page className="vads-u-margin-top--0" />
-      <StatementAddresses copay={selectedCopay} />
-      <h2>What if I have questions about my statement?</h2>
-      <Modals title="Notice of rights and responsibilities">
-        <Modals.Rights />
-      </Modals>
-      <Link
-        className="vads-u-font-size--sm"
-        to={`/balance-details/${selectedId}`}
-      >
-        <i
-          className="fa fa-chevron-left vads-u-margin-right--1"
-          aria-hidden="true"
-        />
-        <strong>Return to facility details</strong>
-      </Link>
+          {`${selectedCopay?.station.facilityName}`}
+        </p>
+        <Link
+          className="vads-u-font-size--sm"
+          to={`/balance-details/${selectedId}`}
+        >
+          <i
+            className="fa fa-chevron-left vads-u-margin-right--1"
+            aria-hidden="true"
+          />
+          <strong>Return to facility details</strong>
+        </Link>
+        <va-on-this-page className="vads-u-margin-top--0" />
+        <StatementAddresses id="statement-addresses" copay={selectedCopay} />
+        <h2>What if I have questions about my statement?</h2>
+        <Modals title="Notice of rights and responsibilities">
+          <Modals.Rights />
+        </Modals>
+        <Link
+          className="vads-u-font-size--sm"
+          to={`/balance-details/${selectedId}`}
+        >
+          <i
+            className="fa fa-chevron-left vads-u-margin-right--1"
+            aria-hidden="true"
+          />
+          <strong>Return to facility details</strong>
+        </Link>
+      </article>
     </>
   );
 };
