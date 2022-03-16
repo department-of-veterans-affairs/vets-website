57f5cb3132f953545d1e33dbc399a0b0032025f3
Darius Dzien <dariusz.dzien@adhocteam.us>
2022-03-16 16:02:21 -0500

    Fix if condition

--- src/platform/testing/e2e/cypress/plugins/index.js
+++ src/platform/testing/e2e/cypress/plugins/index.js
@@ -12,7 +12,7 @@ const tableConfig = {
 };
 
 module.exports = async (on, config) => {
-  if (process.env.CODE_COVERAGE === true) {
+  if (process.env.CODE_COVERAGE === 'true') {
     require('@cypress/code-coverage/task')(on, config);
     on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
   }
