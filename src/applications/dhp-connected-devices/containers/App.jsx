fed972fa46f02fe8a8cd213d71fcf5e9fb7470df
Rebecca Green <rebecca.green@thoughtworks.com>
2022-03-17 16:39:27 -0400

    WIP replaces div with SignIn component, starts adding tests
    Co-authored-by: Rebecca Green <rebecca.green@thoughtworks.com>
    Co-authored-by: Travis McKinstry <travis.mckinstry@thoughtworks.com>

--- src/applications/dhp-connected-devices/containers/App.jsx
+++ src/applications/dhp-connected-devices/containers/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import SignIn from '../../static-pages/cta-widget/components/messages/SignIn';
 
-export default function App() {
+const mapStateToProps = state => ({
+  isLoggedIn: state?.user?.login?.currentlyLoggedIn,
+});
+
+function App(isLoggedIn) {
+  function buttonHandler() {
+    return isLoggedIn;
+  }
   return (
     <div className="usa-grid-full margin">
       <div className="usa-width-three-fourths">
@@ -23,23 +32,11 @@ export default function App() {
         <div className="schemaform-title">
           <h2>Connected devices</h2>
         </div>
-        <va-alert
-          close-btn-aria-label="Close notification"
-          status="continue"
-          visible
-        >
-          <h3 slot="headline">Please sign in to connect a device</h3>
-          <div>
-            Sign in with your existing ID.me, DS Logon, or My HealtheVet
-            account. If you don't have any of these accounts, you can create a
-            free ID.me account now.
-          </div>
-          <div>
-            <button type="button" className="usa-button">
-              Sign in or create an account
-            </button>
-          </div>
-        </va-alert>
+        <SignIn
+          primaryButtonHandler={buttonHandler}
+          serviceDescription="connect a device"
+          testId="sign-in"
+        />
         <div className="schemaform-title">
           <h2>Frequently asked questions</h2>
         </div>
@@ -61,3 +58,5 @@ export default function App() {
     </div>
   );
 }
+
+export default connect(mapStateToProps)(App);
---
+++ src/applications/dhp-connected-devices/tests/App.spec.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import App from '../containers/App';
+
+describe('auth flow', () => {
+  it('shows sign in button when vet is not logged in', () => {
+    const fakeStore = {
+      getState: () => ({
+        user: {
+          login: {
+            currentlyLoggedIn: true,
+          },
+        },
+      }),
+      subscribe: () => {},
+      dispatch: () => {},
+    };
+    const app = shallow(<App store={fakeStore} />);
+    expect(app.find({ testId: 'sign-in' })).to.exist;
+    app.unmount();
+  });
+  // it('shows sign in button when vet is logged in', () => {
+  //   const fakeStore = {
+  //     getState: () => ({
+  //       user: {
+  //         login: {
+  //           currentlyLoggedIn: false,
+  //         },
+  //       },
+  //     }),
+  //     subscribe: () => {},
+  //     dispatch: () => {},
+  //   };
+  //   const app = shallow(<App store={fakeStore} />);
+  //   expect(app.find({ testId: 'sign-in' })).to.not.exist;
+  //   app.unmount();
+  // });
+});
