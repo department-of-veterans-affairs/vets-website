e815c7dd40edcb91f384e4b1f3684d537d26d7a2
Adam Whitlock <adam.whitlock@adhocteam.us>
2022-03-18 13:11:00 -0600

    add basic e2e test

--- src/applications/personalization/profile/tests/e2e/personal-and-contact-info/setup.js
+++ src/applications/personalization/profile/tests/e2e/personal-and-contact-info/setup.js
@@ -4,18 +4,18 @@ import mockUser from '@@profile/tests/fixtures/users/user-36.json';
 import receivedTransaction from '@@profile/tests/fixtures/transactions/received-transaction.json';
 import finishedTransaction from '@@profile/tests/fixtures/transactions/finished-transaction.json';
 import noChangesTransaction from '@@profile/tests/fixtures/transactions/no-changes-transaction.json';
-import { createAddressValidationResponse } from './addressValidation';
-import { createUserResponse } from './user';
+import { createAddressValidationResponse } from '../address-validation/addressValidation';
+import { createUserResponse } from '../address-validation/user';
 import disableFTUXModals from '~/platform/user/tests/disableFTUXModals';
 
-export const setUp = type => {
+export const setupHomeAddressModalCheck = type => {
   disableFTUXModals();
 
   cy.login(mockUser);
   cy.visit(PROFILE_PATHS.PERSONAL_INFORMATION);
   cy.injectAxe();
 
-  cy.findByRole('button', { name: /edit mailing address/i }).click({
+  cy.findByRole('button', { name: /edit home address/i }).click({
     force: true,
   });
 
