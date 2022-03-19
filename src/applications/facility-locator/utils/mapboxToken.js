1d2af67c7b9079a548ee6a06fc0e696135f62f49
Theo Bentum <theo@adhocteam.us>
2022-03-18 18:14:14 -0700

    Test top-level await

--- src/applications/facility-locator/utils/mapboxToken.js
+++ src/applications/facility-locator/utils/mapboxToken.js
@@ -1,2 +1,6 @@
+/* eslint-disable no-console */
+const asyncMsg = await Promise.resolve('WORKS!');
+console.log(asyncMsg); // "WORKS!"
+
 export const mapboxToken =
   'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2l2Y3VlNWp5MDBoNjJvbHZ2a3R4bnN2cyJ9.2LoUhwRmz2OiCtRirnc6Pw';
