06ccbee099b76c89ea9832abc77195f6eed0e5e7
Robin Garrison <robin.garrison@adhocteam.us>
2022-03-23 14:33:17 -0500

    Update unit test

--- src/applications/disability-benefits/all-claims/tests/content/ancillaryFormsWizardSummary.unit.spec.jsx
+++ src/applications/disability-benefits/all-claims/tests/content/ancillaryFormsWizardSummary.unit.spec.jsx
@@ -8,20 +8,26 @@ describe('526 All Claims -- Ancillary forms wizard summary content', () => {
   it('should render only the appropriate panels', () => {
     const formData = {
       'view:modifyingHome': true,
-      // view:modifyingCar omitted
+      'view:modifyingCar': true,
       'view:aidAndAttendance': true,
-      'view:unemployable': false,
+      'view:unemployable': true,
     };
     const tree = shallow(<SummaryDescription formData={formData} />);
 
-    const collapsiblePanels = tree.find('CollapsiblePanel');
-    expect(collapsiblePanels.length).to.equal(2);
-    expect(collapsiblePanels.first().props().panelName).to.equal(
-      'Adapted housing assistance',
-    );
-    expect(collapsiblePanels.last().props().panelName).to.equal(
-      'Aid and Attendance',
-    );
+    const accordion = tree.find('va-accordion-item');
+    expect(accordion.length).to.equal(4);
+    expect(
+      accordion
+        .first()
+        .find('h3')
+        .text(),
+    ).to.equal('Adapted housing assistance');
+    expect(
+      accordion
+        .last()
+        .find('h3')
+        .text(),
+    ).to.equal('Individual Unemployability');
     tree.unmount();
   });
 });
