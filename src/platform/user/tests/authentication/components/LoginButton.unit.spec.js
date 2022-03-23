6fd7dc2ed1d750e95da296b7b6312705d598f14a
Alexander Garcia <agarcia@clarityinnovates.com>
2022-03-23 10:18:54 -0400

    finishes LoginButton unit test

---
+++ src/platform/user/tests/authentication/components/LoginButton.unit.spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { CSP_IDS, CSP_CONTENT } from 'platform/user/authentication/constants';
+import { expect } from 'chai';
+import { render } from '@testing-library/react';
+import LoginButton from 'platform/user/authentication/components/LoginButton';
+
+const csps = Object.values(CSP_IDS).filter(csp => csp !== 'myhealthevet');
+
+describe('LoginButton', () => {
+  it('should not render with a `csp`', () => {
+    const screen = render(<LoginButton />);
+    expect(screen.queryByRole('button')).to.be.null;
+  });
+  csps.forEach(csp => {
+    it(`should render correctly for ${csp}`, () => {
+      const screen = render(<LoginButton csp={csp} />);
+
+      expect(screen.queryByRole('button')).to.have.attr('data-csp', csp);
+      expect(screen.queryByRole('button')).to.have.attr(
+        'aria-label',
+        `Sign in with ${CSP_CONTENT[csp].COPY}`,
+      );
+    });
+  });
+});
