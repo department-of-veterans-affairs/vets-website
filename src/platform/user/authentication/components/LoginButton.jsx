6a91fab67b00ddbf3344ab2196d278de14f0d3fa
Alexander Garcia <agarcia@clarityinnovates.com>
2022-03-24 10:30:16 -0400

    updates LoginButton component to set default onClick prop

--- src/platform/user/authentication/components/LoginButton.jsx
+++ src/platform/user/authentication/components/LoginButton.jsx
@@ -3,14 +3,14 @@ import recordEvent from 'platform/monitoring/record-event';
 import * as authUtilities from 'platform/user/authentication/utilities';
 import { CSP_CONTENT } from '../constants';
 
-export function loginHandler(loginType) {
+function loginHandler(loginType) {
   recordEvent({ event: `login-attempted-${loginType}` });
   authUtilities.login({
     policy: loginType,
   });
 }
 
-export default function LoginButton({ csp }) {
+export default function LoginButton({ csp, onClick = loginHandler }) {
   if (!csp) return null;
   return (
     <button
@@ -18,7 +18,7 @@ export default function LoginButton({ csp }) {
       aria-label={`Sign in with ${CSP_CONTENT[csp].COPY}`}
       className={`usa-button ${csp}-button vads-u-margin-y--1p5 vads-u-padding-y--2`}
       data-csp={csp}
-      onClick={() => loginHandler(csp)}
+      onClick={() => onClick(csp)}
     >
       {CSP_CONTENT[csp].LOGO}
     </button>
--- src/platform/user/tests/authentication/components/LoginButton.unit.spec.js
+++ src/platform/user/tests/authentication/components/LoginButton.unit.spec.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { CSP_IDS, CSP_CONTENT } from 'platform/user/authentication/constants';
 import { expect } from 'chai';
+import { shallow } from 'enzyme';
 import { render } from '@testing-library/react';
+import sinon from 'sinon';
 import LoginButton from 'platform/user/authentication/components/LoginButton';
 
 const csps = Object.values(CSP_IDS).filter(csp => csp !== 'myhealthevet');
@@ -21,5 +23,15 @@ describe('LoginButton', () => {
         `Sign in with ${CSP_CONTENT[csp].COPY}`,
       );
     });
+  });
+  it('should call the `loginHandler` function on click', () => {
+    const loginHandler = sinon.spy();
+    const wrapper = shallow(
+      <LoginButton csp="dslogon" onClick={loginHandler} />,
+    );
+
+    wrapper.find('button').simulate('click');
+    expect(loginHandler.called).to.be.true;
+    wrapper.unmount();
   });
 });
