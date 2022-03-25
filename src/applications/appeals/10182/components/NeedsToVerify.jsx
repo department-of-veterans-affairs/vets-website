3ef2f099562538b0887ce783b28b8bc42b1ce80c
Robin Garrison <robin.garrison@adhocteam.us>
2022-03-25 08:48:28 -0500

    Update verify identity alert content

--- src/applications/appeals/10182/components/NeedsToVerify.jsx
+++ src/applications/appeals/10182/components/NeedsToVerify.jsx
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 
 const NeedsToVerify = ({ pathname }) => (
   <va-alert status="warning">
-    We want to keep your information safe with the highest level of security.
-    Please{' '}
-    <a href={`/verify?next=${pathname}`} className="verify-link">
-      verify your identity
-    </a>{' '}
-    to access this form.
+    <h2 slot="headline">We need you to verify your identity</h2>
+    You’ll be able to complete this form after your identity is confirmed. This
+    helps us keep your information safe during the application process.
+    <p>
+      <a href={`/verify?next=${pathname}`} className="verify-link">
+        Verify your identity
+      </a>
+    </p>
   </va-alert>
 );
 
