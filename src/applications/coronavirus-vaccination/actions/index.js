5ac70b2b8cbd2ec7ff27c426e2eee60cdd83e71d
Theo Bentum <theo@adhocteam.us>
2022-03-28 13:23:15 -0700

    Revert grouped app testing changes

--- src/applications/coronavirus-vaccination/actions/index.js
+++ src/applications/coronavirus-vaccination/actions/index.js
@@ -1,6 +1,5 @@
 export const FORM_DATA_UPDATED = 'coronavirusVaccination/FORM_DATA_UPDATED';
 
-// Trigger changed app build
 export function updateFormData(formSchema, uiSchema, formData) {
   return {
     type: FORM_DATA_UPDATED,
