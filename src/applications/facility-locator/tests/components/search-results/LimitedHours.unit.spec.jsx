e7b74c9ae0b3c3fe6a752e67c2b15fcd199d0fa0
Mark Dewey <mdewey@governmentcio.com>
2022-03-29 11:51:49 -0400

    (test) added test for tabindex

--- src/applications/facility-locator/tests/components/search-results/LimitedHours.unit.spec.jsx
+++ src/applications/facility-locator/tests/components/search-results/LimitedHours.unit.spec.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import { expect } from 'chai';
 import { axeCheck } from 'platform/forms-system/test/config/helpers';
 import LimitedHours from '../../../components/search-results-items/LimitedHours';
 
@@ -6,6 +8,12 @@ describe('facility-locator', () => {
   describe('LimitedHours', () => {
     it('passes axeCheck', () => {
       axeCheck(<LimitedHours />);
+    });
+    it('should have tabIdex on the div', () => {
+      const { container } = render(<LimitedHours />);
+      expect(container.querySelector('div').getAttribute('tabindex')).to.equal(
+        '0',
+      );
     });
   });
 });
