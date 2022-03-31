8bd996daa8cec49ba6fd55f27eb819e676cbbf5d
Rebecca Green <rebecca.green@thoughtworks.com>
2022-03-31 13:47:47 -0400

    Adds successful and failure device connection alert components
    Co-authored-by: Rebecca Green <rebecca.green@thoughtworks.com>
    Co-authored-by: Travis McKinstry <travis.mckinstry@thoughtworks.com>

---
+++ src/applications/dhp-connected-devices/components/DeviceConnectionAlerts.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const DeviceConnectionSucceededAlert = () => {
+  return (
+    <va-alert
+      close-btn-aria-label="Close notification"
+      status="success"
+      visible
+    >
+      <h3 slot="headline">Device Connected</h3>
+      <div>Your device is now connected.</div>
+    </va-alert>
+  );
+};
+
+export const DeviceConnectionFailedAlert = () => {
+  return (
+    <va-alert close-btn-aria-label="Close notification" status="error" visible>
+      <h3 slot="headline">We couldn’t connect your device</h3>
+      <div>
+        We were not able to connect to your device now. Please try again.
+      </div>
+    </va-alert>
+  );
+};
