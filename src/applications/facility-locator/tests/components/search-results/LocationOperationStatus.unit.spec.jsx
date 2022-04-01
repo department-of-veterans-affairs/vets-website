8234ac2af9d4cc566aaa728cfb3c5cb6e3f8b3bc
Mark Dewey <mdewey@governmentcio.com>
2022-04-01 14:05:44 -0400

    (test) updated unit tests

--- src/applications/facility-locator/tests/components/search-results/LocationOperationStatus.unit.spec.jsx
+++ src/applications/facility-locator/tests/components/search-results/LocationOperationStatus.unit.spec.jsx
@@ -6,16 +6,38 @@ import LocationOperationStatus from '../../../components/search-results-items/co
 
 describe('facility-locator', () => {
   describe('LocationOperationStatus', () => {
+    it('should have tabIndex on the div', () => {
+      const operatingStatus = { code: 'CLOSED' };
+      const { container } = render(
+        <LocationOperationStatus operatingStatus={operatingStatus} />,
+      );
+      expect(container.querySelector('div').getAttribute('tabindex')).to.equal(
+        '0',
+      );
+    });
+
     it('check in button passes axeCheck', () => {
       const operatingStatus = { code: 'CLOSED' };
       axeCheck(<LocationOperationStatus operatingStatus={operatingStatus} />);
     });
-    it('should render nothing for status LIMITED', () => {
-      const operatingStatus = { code: 'LIMITED' };
+
+    it('should render nothing operation status for  NORMAL', () => {
+      const operatingStatus = { code: 'NORMAL' };
       const { container } = render(
         <LocationOperationStatus operatingStatus={operatingStatus} />,
       );
-      expect(container.firstChild).to.equal(null);
+      expect(container.firstChild).to.be.null;
+    });
+    it('should render nothing operation status for no status', () => {
+      const { container } = render(<LocationOperationStatus />);
+      expect(container.firstChild).to.be.null;
+    });
+    it('should render nothing for status LIMITED', () => {
+      const operatingStatus = { code: 'LIMITED' };
+      const { getByText } = render(
+        <LocationOperationStatus operatingStatus={operatingStatus} />,
+      );
+      expect(getByText('Limited services and hours')).to.be.ok;
     });
     it('should render the operation status for  CLOSED', () => {
       const operatingStatus = { code: 'CLOSED' };
