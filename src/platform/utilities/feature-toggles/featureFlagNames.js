8d9f941c2789f9ceb508c965b1bd6dc7020e2778
Brian Seek <brian.seek@civicactions.com>
2022-04-04 11:37:42 -0700

    Added new feature flag name for edit messaging.

--- src/platform/utilities/feature-toggles/featureFlagNames.js
+++ src/platform/utilities/feature-toggles/featureFlagNames.js
@@ -22,6 +22,7 @@ export default Object.freeze({
   checkInExperienceTranslationPreCheckInEnabled:'check_in_experience_translation_pre_check_in_enabled',
   checkInExperienceDayOfDemographicsFlagsEnabled: 'check_in_experience_day_of_demographics_flags_enabled',
   checkInExperienceLorotaSecurityUpdatesEnabled: 'check_in_experience_lorota_security_updates_enabled',
+  checkInExperienceEditMessagingEnabled: 'check_in_experience_edit_messaging_enabled',
   coeAccess: 'coe_access',
   combinedDebtPortalAccess: 'combined_debt_portal_access',
   covidVaccineSchedulingFrontend: 'covid_vaccine_scheduling_frontend',
