c4ebf4c682be361a49fef6be569ba13b8dae3c9c
BJ McCotter <bj.mccotter@adhocteam.us>
2022-04-05 11:40:42 -0500

    attempting to auto generate docs with a new test field

---
+++ src/form-builder/TestField.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * This is a test field that will return a nice message
+ *
+ * @param none
+ * @returns String that is friendly and nice
+ */
+const TestField = (): JSX.Element => {
+  return <h1>Hello There</h1>;
+};
+
+export default TestField;
