2936d1869be7a76f1a9574bb8ff3755411ad0c1a
Ryan Smith <ryan.smith@adhocteam.us>
2022-04-12 15:09:21 -0400

    Code cleanup

--- src/applications/ask-a-question/tests/cypress/keyboard-navigation.cypress.spec.js
+++ src/applications/ask-a-question/tests/cypress/keyboard-navigation.cypress.spec.js
@@ -3,140 +3,140 @@ context('Keyboard Navigation', () => {
     cy.visit('education/submit-school-feedback/introduction');
     cy.injectAxeThenAxeCheck();
 
-    cy.tabToElem('.no-wrap');
+    cy.tabToElement('.no-wrap');
 
-    cy.tabToElem('.va-button-link.schemaform-start-button', false);
+    cy.tabToElement('.va-button-link.schemaform-start-button', false);
 
     cy.realPress('Space');
 
     /* */
-    cy.tabToElem('[name="root_onBehalfOf"]');
+    cy.tabToElement('[name="root_onBehalfOf"]');
 
     cy.chooseRadio('Myself');
 
-    cy.tabToElem('#2-continueButton');
+    cy.tabToElement('#2-continueButton');
 
     cy.realPress('Space');
 
     /* */
-    cy.tabToElem('#root_fullName_prefix');
+    cy.tabToElement('#root_fullName_prefix');
 
     cy.chooseSelectOptionByTyping('Mrs.');
 
-    cy.tabToElem('#root_fullName_first');
+    cy.tabToElement('#root_fullName_first');
 
     cy.typeInFocused('Noah');
 
-    cy.tabToElem('#root_fullName_middle');
+    cy.tabToElement('#root_fullName_middle');
 
     cy.typeInFocused('Michael');
 
-    cy.tabToElem('#root_fullName_last');
+    cy.tabToElement('#root_fullName_last');
 
     cy.typeInFocused('Gelman');
 
-    cy.tabToElem('#root_fullName_suffix');
+    cy.tabToElement('#root_fullName_suffix');
 
     cy.chooseSelectOptionByTyping('Sr.');
 
-    cy.tabToElem('#root_serviceAffiliation');
+    cy.tabToElement('#root_serviceAffiliation');
 
     cy.chooseSelectOptionByTyping('Servicemember');
 
-    cy.tabToElem('#2-continueButton');
+    cy.tabToElement('#2-continueButton');
 
     cy.realPress('Space');
 
     /* */
-    cy.tabToElem('#root_serviceBranch');
+    cy.tabToElement('#root_serviceBranch');
 
     cy.chooseSelectOptionByTyping('Air Force');
 
-    cy.tabToElem('#root_serviceDateRange_fromMonth');
+    cy.tabToElement('#root_serviceDateRange_fromMonth');
 
     cy.chooseSelectOptionByTyping('March');
 
-    cy.tabToElem('#root_serviceDateRange_fromDay');
+    cy.tabToElement('#root_serviceDateRange_fromDay');
 
     cy.chooseSelectOptionByTyping('1');
 
-    cy.tabToElem('#root_serviceDateRange_fromYear');
+    cy.tabToElement('#root_serviceDateRange_fromYear');
 
     cy.typeInFocused('1990');
 
-    cy.tabToElem('#root_serviceDateRange_toMonth');
+    cy.tabToElement('#root_serviceDateRange_toMonth');
 
     cy.chooseSelectOptionByTyping('May');
 
-    cy.tabToElem('#root_serviceDateRange_toDay');
+    cy.tabToElement('#root_serviceDateRange_toDay');
 
     cy.chooseSelectOptionByTyping('4');
 
-    cy.tabToElem('#root_serviceDateRange_toYear');
+    cy.tabToElement('#root_serviceDateRange_toYear');
 
     cy.typeInFocused('2020');
 
-    cy.tabToElem('#2-continueButton');
+    cy.tabToElement('#2-continueButton');
 
     cy.realPress('Space');
 
     /* */
-    cy.tabToElem('#root_address_country');
+    cy.tabToElement('#root_address_country');
 
     cy.chooseSelectOptionByTyping('United States');
 
-    cy.tabToElem('#root_address_street');
+    cy.tabToElement('#root_address_street');
 
     cy.typeInFocused('12345 Test Ave.');
 
-    cy.tabToElem('#root_address_street2');
+    cy.tabToElement('#root_address_street2');
 
     cy.typeInFocused('Apt 3');
 
-    cy.tabToElem('#root_address_city');
+    cy.tabToElement('#root_address_city');
 
     cy.typeInFocused('Los Angeles');
 
-    cy.tabToElem('#root_address_state');
+    cy.tabToElement('#root_address_state');
 
     cy.chooseSelectOptionByTyping('California');
 
-    cy.tabToElem('#root_address_postalCode');
+    cy.tabToElement('#root_address_postalCode');
 
     cy.typeInFocused('90210');
 
-    cy.tabToElem('#root_applicantEmail');
+    cy.tabToElement('#root_applicantEmail');
 
     cy.typeInFocused('noahIsCool@gmail.com');
 
-    cy.tabToElem('[type="email"]');
+    cy.tabToElement('[type="email"]');
 
     cy.typeInFocused('noahIsCool@gmail.com');
 
-    cy.tabToElem('#root_phone');
+    cy.tabToElement('#root_phone');
 
     cy.typeInFocused('(555) 867-5309');
 
-    cy.tabToElem('#2-continueButton');
+    cy.tabToElement('#2-continueButton');
 
     cy.realPress('Space');
 
     /* */
-    cy.tabToElem('#root_educationDetails_programs_chapter35');
+    cy.tabToElement('#root_educationDetails_programs_chapter35');
 
     cy.realPress('Space');
 
-    cy.tabToElem(
+    cy.tabToElement(
       '[name="root_educationDetails_assistance_view:assistance_ta"]',
     );
 
     cy.realPress('Space');
 
-    cy.tabToElem('[name="root_educationDetails_assistance_view:ffa_ffa"]');
+    cy.tabToElement('[name="root_educationDetails_assistance_view:ffa_ffa"]');
 
     cy.realPress('Space');
 
-    cy.tabToElem('#2-continueButton');
+    cy.tabToElement('#2-continueButton');
 
     cy.realPress('Space');
   });
--- src/platform/testing/e2e/cypress/support/commands/keyboardNavigation.js
+++ src/platform/testing/e2e/cypress/support/commands/keyboardNavigation.js
@@ -1,39 +1,12 @@
 import 'cypress-real-events';
 
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
-// This is the timeout duration between each event. You can overwrite this in your test if you
-// want it to move faster or slower
+// This is the timeout duration between each event. You can overwrite this in your test if you want it to move faster or slower.
 // eslint-disable-next-line prefer-const
 let timeoutDuration = 0;
 
-// This Command is used by other commands to select form values.
-// You can call this yourself if you want to move focus to a specific radio or select value
-// while the radio or select field is focused. The value is the value of the input/option
+/**
+ * This command is used by other commands to select form values. You can call this yourself if you want to move focus to a specific radio value while the radio field is focused. The value is the value of the input/option.
+ */
 Cypress.Commands.add('findOption', value => {
   cy.get(':focus').then($el => {
     if ($el.val() !== value) {
@@ -43,20 +16,24 @@ Cypress.Commands.add('findOption', value => {
   });
 });
 
-Cypress.Commands.add('findSelectOptionByTyping', value => {
+/**
+ * This command is used by other commands to select form values by typing in the desired characters. This is done instead of arrow keys because doing this on Mac is currently not possible. You can call this yourself if you want to move focus to a specific select value while the select field is focused. The text is the text that shows to the user on the select option.
+ */
+Cypress.Commands.add('findSelectOptionByTyping', text => {
   cy.get(':focus :selected').then($el => {
-    if ($el.text() !== value) {
-      for (const character of value) {
+    if ($el.text() !== text) {
+      for (const character of text) {
         cy.realPress(character);
       }
 
-      cy.findSelectOptionByTyping(value);
+      cy.findSelectOptionByTyping(text);
     }
   });
 });
 
-// This Command is used to select a specific radio input when one of the radios in a group
-// is focused. The value is the value of the input
+/**
+ * This command is used to select a specific radio input when one of the radios in a group is focused. The value is the value of the input.
+ */
 Cypress.Commands.add('chooseRadio', value => {
   if (typeof value === 'undefined') {
     return;
@@ -70,35 +47,38 @@ Cypress.Commands.add('chooseRadio', value => {
   });
 });
 
-// This Command is used to select a specific select option when a select box is focused.
-// The value is the value of the input
-Cypress.Commands.add('chooseSelectOptionByTyping', value => {
-  if (typeof value === 'undefined') {
+/**
+ * This command is used to select a specific select option when a select box is focused. The value is the value of the input
+ */
+Cypress.Commands.add('chooseSelectOptionByTyping', text => {
+  if (typeof text === 'undefined') {
     return;
   }
 
-  cy.findSelectOptionByTyping(value);
+  cy.findSelectOptionByTyping(text);
 });
 
-Cypress.Commands.add('chooseSelectOptionByArrow', value => {
-  if (typeof value === 'undefined') {
-    return;
-  }
-
-  cy.findSelectOption(value);
-});
-
-// This Command types in the focused input or textarea
+/**
+ * This command types in the focused input or textarea.
+ * */
 Cypress.Commands.add('typeInFocused', text => {
   cy.get(':focus').type(text, { delay: timeoutDuration });
 });
 
-// This command tabs to the element on the page based on the passed in selector
-// Selector is the selector
-// Forward is a boolean that will tab for or backward towards the element
-// checkIfExists is for when this Command recursively calls itself, you don't need to call this
+/**
+ * @callback moveToElement
+ * @param {String} selector - The CSS element selector.
+ * @param {Boolean} [forward=true] - A boolean that will tab for or backward towards the element.
+ * @param {Boolean} [checkIfExists=true] - A boolean for when this command recursively calls itself, you don't need to call this
+ */
+
+/**
+ * This command tabs to the element on the page based on the passed in selector.
+ * @param {String} name - Name of the new command.
+ * @param {moveToElement} callbackFunction - The callback that handles moving to the element.
+ */
 Cypress.Commands.add(
-  'tabToElem',
+  'tabToElement',
   (selector, forward = true, checkIfExists = true) => {
     if (checkIfExists) {
       cy.get(selector).should('exist');
@@ -109,7 +89,7 @@ Cypress.Commands.add(
     cy.realPress(key, { pressDelay: timeoutDuration }).then(() => {
       cy.get(':focus').then($el => {
         if (!$el.is(selector)) {
-          cy.tabToElem(selector, forward, false);
+          cy.tabToElement(selector, forward, false);
         }
       });
     });
