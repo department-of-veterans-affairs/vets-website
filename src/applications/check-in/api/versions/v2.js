7f52484c0b4c91dee661a07c6262857760dd2a01
Brian Seek <brian.seek@civicactions.com>
2022-04-14 17:32:19 -0700

    Updates to API to use same fetch for both ssn and dob.

--- src/applications/check-in/api/versions/v2.js
+++ src/applications/check-in/api/versions/v2.js
@@ -15,10 +15,17 @@ const v2 = {
       ...json,
     };
   },
-  postSession: async ({ lastName, last4, token, checkInType = '' }) => {
+  postSession: async ({
+    lastName,
+    last4,
+    dob,
+    token,
+    checkInType = '',
+    isLorotaSecurityUpdatesEnabled = false,
+  }) => {
     const url = '/check_in/v2/sessions/';
     const headers = { 'Content-Type': 'application/json' };
-    const data = {
+    let data = {
       session: {
         uuid: token,
         last4: last4.trim(),
@@ -26,35 +33,17 @@ const v2 = {
         checkInType,
       },
     };
-    const body = JSON.stringify(data);
-    const settings = {
-      headers,
-      body,
-      method: 'POST',
-      mode: 'cors',
-    };
+    if (isLorotaSecurityUpdatesEnabled) {
+      data = {
+        session: {
+          uuid: token,
+          dob,
+          lastName: lastName.trim(),
+          checkInType,
+        },
+      };
+    }
 
-    const json = await makeApiCallWithSentry(
-      apiRequest(`${environment.API_URL}${url}`, settings),
-      'validating-user',
-      token,
-    );
-    return {
-      ...json,
-    };
-  },
-
-  postSessionDOB: async ({ lastName, dob, token, checkInType = '' }) => {
-    const url = '/check_in/v2/sessions/';
-    const headers = { 'Content-Type': 'application/json' };
-    const data = {
-      session: {
-        uuid: token,
-        dob,
-        lastName: lastName.trim(),
-        checkInType,
-      },
-    };
     const body = JSON.stringify(data);
     const settings = {
       headers,
