3b34b8532b3f303eddc582fb228aed75642a10d9
Kha Huynh <kha.huynh@thoughtworks.com>
2022-04-15 10:46:16 -0400

    [department-of-veterans-affairs/va-virtual-agent#458] Add integration test to check if chatbot is accessible and interactable
    Co-authored-by: Addy Sharma <aditya.sharma@thoughtworks.com>

--- src/applications/virtual-agent/tests/virtual-agent.cypress.spec.js
+++ src/applications/virtual-agent/tests/virtual-agent.cypress.spec.js
@@ -1,15 +1,51 @@
 import manifest from '../manifest.json';
 
 describe(manifest.appName, () => {
-  // Skip tests in CI until the app is released.
-  // Remove this block when the app has a content page in production.
-  before(function() {
-    if (Cypress.env('CI')) this.skip();
-  });
+  it('check if user is able to see the chat bot', () => {
+    const startChatButton = '[data-testid="btnAcceptDisclaimer"]';
+    const welcomeBubble = '.webchat__bubble--show-nub p';
+    const expectedWelcomeText = 'Welcome to the VA virtual agent.';
+    const textField = '.webchat__send-box-text-box__input';
+    const actualResponse =
+      'You may be eligible for education benefits under this program if you meet these requirements.';
+    const noButton = '.webchat__suggested-actions__flow-box';
 
-  it('is accessible', () => {
     cy.visit(manifest.rootUrl)
       .injectAxe()
       .axeCheck();
+
+    cy.get(startChatButton, { timeout: 60000 });
+    cy.get(startChatButton).should('be.visible');
+
+    cy.get(startChatButton).click();
+    cy.get(welcomeBubble, { timeout: 60000 });
+    cy.get(welcomeBubble)
+      .invoke('text')
+      .should('eq', expectedWelcomeText);
+
+    cy.get(textField)
+      .type('MGIB')
+      .type('{enter}');
+    cy.contains('span', 'Yes', { timeout: 60000 });
+    cy.contains('p', 'You may be eligible for education')
+      .invoke('text')
+      .should('include', actualResponse);
+
+    // When user selects Yes
+    cy.contains('span', 'Yes').click();
+    cy.get('[alt="4"]', { timeout: 60000 }).click();
+    cy.contains('span', 'No, thanks').click();
+
+    // When user selects No
+    cy.get(textField)
+      .type('Covid Vaccine')
+      .type('{enter}');
+    cy.get(noButton, { timeout: 60000 })
+      .contains('span', 'No')
+      .click();
+    cy.contains('span', 'Speak with an agent', { timeout: 60000 }).should(
+      'be.visible',
+    );
+    cy.contains('span', 'Try again').click();
   });
 });
