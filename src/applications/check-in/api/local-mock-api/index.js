e720482dda9a574b13cc05b8a29c805ffb1487c9
Brian Seek <brian.seek@civicactions.com>
2022-04-20 08:58:02 -0700

    Turned off LoRota security updates feature in mock.

--- src/applications/check-in/api/local-mock-api/index.js
+++ src/applications/check-in/api/local-mock-api/index.js
@@ -21,7 +21,6 @@ const responses = {
     checkInExperienceEnabled: true,
     preCheckInEnabled: true,
     checkInExperienceUpdateInformationPageEnabled: false,
-    checkInExperienceLorotaSecurityUpdatesEnabled: true,
   }),
   // v2
   'GET /check_in/v2/sessions/:uuid': (req, res) => {
