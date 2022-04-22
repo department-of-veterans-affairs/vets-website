3404e84b795cb30b2fe9068ae89b2336a585c92d
Alexander Garcia <agarcia@clarityinnovates.com>
2022-04-22 12:55:46 -0400

    fixes AutoSSO unit test

--- src/platform/site-wide/user-nav/containers/AutoSSO.jsx
+++ src/platform/site-wide/user-nav/containers/AutoSSO.jsx
@@ -2,7 +2,6 @@ import { connect } from 'react-redux';
 
 import { checkKeepAlive } from 'platform/user/authentication/actions';
 import {
-  ssoeInbound,
   hasCheckedKeepAlive,
   ssoeTransactionId,
 } from 'platform/user/authentication/selectors';
@@ -16,7 +15,6 @@ import { removeLoginAttempted } from 'platform/utilities/sso/loginAttempted';
 
 function AutoSSO(props) {
   const {
-    useInboundSSOe,
     hasCalledKeepAlive,
     transactionId,
     loggedIn,
@@ -28,21 +26,19 @@ function AutoSSO(props) {
     removeLoginAttempted();
   }
 
-  const badPaths = ['auth/login/callback', 'logout'];
-  const isValidPath = !badPaths.some(path =>
+  const isValidPath = !['auth/login/callback', 'logout'].some(path =>
     window.location.pathname.includes(path),
   );
 
   if (
     // avoid race condition where hasSession hasn't been set
     isValidPath &&
-    useInboundSSOe &&
     !profileLoading &&
     !hasCalledKeepAlive
   ) {
-    checkAutoSession(loggedIn, transactionId, profile).then(() => {
-      props.checkKeepAlive();
-    });
+    checkAutoSession(loggedIn, transactionId, profile).then(
+      props.checkKeepAlive(),
+    );
   }
 
   return null;
@@ -54,7 +50,6 @@ const mapStateToProps = state => ({
   hasCalledKeepAlive: hasCheckedKeepAlive(state),
   profileLoading: isProfileLoading(state),
   loggedIn: isLoggedIn(state),
-  useInboundSSOe: ssoeInbound(state),
 });
 
 const mapDispatchToProps = {
