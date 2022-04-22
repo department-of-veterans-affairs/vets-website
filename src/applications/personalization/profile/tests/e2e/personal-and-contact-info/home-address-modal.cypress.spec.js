918387d566dc32098bebabd7ad6211aa6bcf1967
Adam Whitlock <adam.whitlock@adhocteam.us>
2022-04-21 23:28:11 -0600

    (test) dont skip previously flaky tests

--- src/applications/personalization/profile/tests/e2e/personal-and-contact-info/home-address-modal.cypress.spec.js
+++ src/applications/personalization/profile/tests/e2e/personal-and-contact-info/home-address-modal.cypress.spec.js
@@ -25,7 +25,7 @@ describe('Home address update modal', () => {
     cy.injectAxeThenAxeCheck();
   });
 
-  it.skip('should show update prompt modal and save successfully', () => {
+  it('should show update prompt modal and save successfully', () => {
     const formFields = {
       address: '36320 Coronado Dr',
       city: 'Fremont',
@@ -80,7 +80,7 @@ describe('Home address update modal', () => {
     cy.injectAxeThenAxeCheck();
   });
 
-  it.skip('should show update prompt modal and show error when updating fails', () => {
+  it('should show update prompt modal and show error when updating fails', () => {
     const formFields = {
       address: '36320 Coronado Dr',
       city: 'Fremont',
