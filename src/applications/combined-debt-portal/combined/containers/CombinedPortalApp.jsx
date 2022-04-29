74391652665572b8f27d596ae8eee4a04540c4dc
Andrew Rodiek <andrew.rodiek@gmail.com>
2022-04-29 15:26:16 -0400

    Included some checks to show loading while data is being fetched

--- src/applications/combined-debt-portal/combined/containers/CombinedPortalApp.jsx
+++ src/applications/combined-debt-portal/combined/containers/CombinedPortalApp.jsx
@@ -5,6 +5,7 @@ import {
   externalServices,
 } from 'platform/monitoring/DowntimeNotification';
 import PropTypes from 'prop-types';
+import { isProfileLoading } from 'platform/user/selectors';
 import { fetchDebtLetters } from '../actions/debts';
 import { getStatements } from '../actions/copays';
 import {
@@ -27,11 +28,30 @@ const CombinedPortalApp = ({ children }) => {
   const isCombinedPortalActive = useSelector(state =>
     combinedPortalAccess(state),
   );
-  const isLoadingFlags = useSelector(state => selectLoadingFeatureFlags(state));
 
-  if (isLoadingFlags) {
+  // Generic loading flags
+  const isLoadingFlags = useSelector(state => selectLoadingFeatureFlags(state));
+  const profileLoading = useSelector(state => isProfileLoading(state));
+  // Debt and Bill loading flags
+  const { debtLetters, mcp } = useSelector(
+    ({ combinedPortal }) => combinedPortal,
+  );
+  const { pending } = mcp;
+  const { isPending, isPendingVBMS, isProfileUpdating } = debtLetters;
+
+  // Hold off on loading until everyone is ready
+  if (
+    isLoadingFlags ||
+    profileLoading ||
+    pending ||
+    isPending ||
+    isPendingVBMS ||
+    isProfileUpdating
+  ) {
     return <LoadingSpinner />;
   }
+
+  // TODO: we'll probably need to have a unauth redirect checking 'userLoggedIn'
   if (!isCombinedPortalActive) {
     window.location.replace('/');
     return <></>;
