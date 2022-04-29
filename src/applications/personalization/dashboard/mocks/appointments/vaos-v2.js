4015531d5ecdad20560de0d3d92b1149969f018d
Mark Dewey <mdewey@governmentcio.com>
2022-04-29 09:21:02 -0400

    (paused)

--- src/applications/personalization/dashboard/mocks/appointments/vaos-v2.js
+++ src/applications/personalization/dashboard/mocks/appointments/vaos-v2.js
@@ -1,6 +1,71 @@
 const format = require('date-fns/format');
 const add = require('date-fns/add');
 
+export function createMockAppointmentByVersion({
+  id = null,
+  email = null,
+  phone = null,
+  _currentStatus = null,
+  _version = 2,
+  _clinicFriendlyName = null,
+  _clinicName = null,
+  communityCareProvider = null,
+  _timezone = null,
+  // ...fields
+} = {}) {
+  return {
+    id,
+    type: 'appointments',
+    attributes: {
+      id,
+      cancelationReason: null,
+      clinic: null,
+      comment: null,
+      contact: {
+        telecom: [
+          { type: 'phone', value: phone },
+          { type: 'email', value: email },
+        ],
+      },
+      description: null,
+      end: null,
+      kind: null,
+      locationId: null,
+      minutesDuration: null,
+      patientInstruction: 'Video Visit Preparation plus extra data',
+      practitioners: communityCareProvider
+        ? [
+            {
+              identifier: [
+                {
+                  system: 'http://hl7.org/fhir/sid/us-npi',
+                  value: communityCareProvider.uniqueId,
+                },
+              ],
+            },
+          ]
+        : null,
+      preferredTimesForPhoneCall: null,
+      priority: null,
+      reason: null,
+      requestedPeriods: null,
+      serviceType: null,
+      slot: null,
+      // start:
+      //   fields.kind === 'cc' && !fields.requestedPeriods?.length
+      //     ? moment(fields.start)
+      //         .utc()
+      //         .format()
+      //     : null,
+      status: null,
+      telehealth: null,
+      extension: {
+        ccLocation: communityCareProvider,
+      },
+      // ...fieldsWithoutProps,
+    },
+  };
+}
 const createVaosAppointment = ({ startsInDays = 1 }) => {
   const now = add(new Date(), { days: startsInDays });
 
