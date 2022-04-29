a7102c29d2a87bf353816c9785a8bc952fd14ede
Holden Hinkle <holden@holdenhinkle.com>
2022-04-29 17:02:41 -0400

    wip - github service

--- script/github-actions/daily-product-dependency-scan/github/index.js
+++ script/github-actions/daily-product-dependency-scan/github/index.js
@@ -1,27 +1,52 @@
-/* eslint-disable class-methods-use-this */
+/* eslint-disable no-console */
 const { Octokit } = require('octokit');
 const { createAppAuth } = require('@octokit/auth-app');
 
 class GitHub {
   constructor() {
-    this.client = this.createClient();
+    this.createClient();
     this.authenticate();
   }
 
   createClient() {
-    return new Octokit({
+    this.client = new Octokit({
       authStrategy: createAppAuth,
       auth: {
-        appId: 1,
-        privateKey: '-----BEGIN PRIVATE KEY-----\n...',
+        appId: process.env.PRODUCT_DIRECTORY_APP_ID,
+        privateKey: process.env.PRODUCT_DIRECTORY_PRIVATE_KEY.replace(
+          /\\n/gm,
+          '\n',
+        ),
         installationId: 123,
       },
     });
   }
 
   async authenticate() {
-    await this.client.rest.apps.getAuthenticated();
+    try {
+      await this.client.rest.apps.getAuthenticated();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async getProductDirectory() {
+    try {
+      const response = await this.client.rest.repos.getContent({
+        owner: 'holdenhinkle',
+        repo: 'product-directory',
+        path:
+          'https://github.com/holdenhinkle/product-directory/blob/main/product-directory.csv',
+      });
+
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
 
-module.exports = GitHub;
+const gitHubService = new GitHub();
+gitHubService.getProductDirectory();
+
+// module.exports = GitHub;
