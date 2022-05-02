694038c918b9de676c830de9df4f1fec326e3d28
Andrew Rodiek <andrew.rodiek@gmail.com>
2022-05-02 13:21:01 -0400

    added unit tests fixed naming and moment issues

---
+++ src/applications/combined-debt-portal/combined/tests/unit/balance-helper.unit.spec.js
@@ -0,0 +1,52 @@
+import { expect } from 'chai';
+import {
+  calculateTotalDebts,
+  getLatestDebt,
+  calculateTotalBills,
+  getLatestBills,
+} from '../../components/Balances';
+// TODO: Update referece after refactoring
+import mockDebt from '../../../../debt-letters/tests/e2e/fixtures/mocks/debts.json';
+import mockBill from '../../utils/mock-users/mockStatements.json';
+
+describe('combined debt portal helpers', () => {
+  describe('calculateTotalDebts helper: ', () => {
+    it('should calculate total debt', () => {
+      expect(calculateTotalDebts(mockDebt.debts)).to.equal(3305.4);
+    });
+
+    it('should return 0 if no valid data', () => {
+      expect(calculateTotalDebts(null)).to.equal(0);
+    });
+  });
+
+  describe('getLatestDebt helper: ', () => {
+    it('should return latest debt', () => {
+      expect(getLatestDebt(mockDebt.debts)).to.equal('12/19/2018');
+    });
+
+    it('should return null if no valid data', () => {
+      expect(getLatestDebt(null)).to.equal(null);
+    });
+  });
+
+  describe('calculateTotalBills helper: ', () => {
+    it('should calculate total bills', () => {
+      expect(calculateTotalBills(mockBill)).to.equal(87);
+    });
+
+    it('should return 0 if no valid data', () => {
+      expect(calculateTotalBills(null)).to.equal(0);
+    });
+  });
+
+  describe('getLatestBills helper: ', () => {
+    it('should return latest bills', () => {
+      expect(getLatestBills(mockBill)).to.equal('12/05/2021');
+    });
+
+    it('should return latest bills', () => {
+      expect(getLatestBills(null)).to.equal(null);
+    });
+  });
+});
