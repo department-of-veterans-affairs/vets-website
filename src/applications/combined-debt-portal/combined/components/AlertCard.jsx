9afa827479a123db51580aaaaf76185e957b2e45
Andrew Rodiek <andrew.rodiek@gmail.com>
2022-05-04 09:40:28 -0400

    feedback update, moved to date-fns, using enum style renders & cleanup

--- src/applications/combined-debt-portal/combined/components/AlertCard.jsx
+++ src/applications/combined-debt-portal/combined/components/AlertCard.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { APP_TYPES } from '../utils/helpers';
 
-const AlertCard = ({ isDebt }) => {
+const AlertCard = ({ appType }) => {
   return (
     <>
       <va-alert
         class="row vads-u-margin-bottom--5"
         status="error"
-        data-testid={`balance-card-alert-${isDebt ? 'debt' : 'copay'}`}
+        data-testid={`balance-card-alert-${
+          appType === APP_TYPES.DEBT ? 'debt' : 'copay'
+        }`}
       >
         <h2 slot="headline" className="vads-u-font-size--h3">
-          We can’t access your {`${isDebt ? 'debt' : 'copay'}`} records right
-          now
+          We can’t access your{' '}
+          {`${appType === APP_TYPES.DEBT ? 'debt' : 'copay'}`} records right now
         </h2>
         <p className="vads-u-font-size--base vads-u-font-family--sans">
-          We’re sorry. Information about {`${isDebt ? 'debts' : 'copays'}`} you
-          might have is unavailable because something went wrong on our end.
-          Please check back soon.
+          We’re sorry. Information about{' '}
+          {`${appType === APP_TYPES.DEBT ? 'debts' : 'copays'}`} you might have
+          is unavailable because something went wrong on our end. Please check
+          back soon.
         </p>
         <p>
           If you continue having trouble viewing information about your{' '}
-          {`${isDebt ? 'debts' : 'copays'}`}, email us at{' '}
+          {`${appType === APP_TYPES.DEBT ? 'debts' : 'copays'}`}, email us at{' '}
           <a href="mailto:dmcops.vbaspl@va.gov">dmcops.vbaspl@va.gov</a>.
         </p>
       </va-alert>
@@ -28,7 +32,7 @@ const AlertCard = ({ isDebt }) => {
   );
 };
 AlertCard.propTypes = {
-  isDebt: PropTypes.bool,
+  appType: PropTypes.string,
 };
 
 export default AlertCard;
--- src/applications/combined-debt-portal/combined/utils/helpers.js
+++ src/applications/combined-debt-portal/combined/utils/helpers.js
@@ -1,6 +1,16 @@
 import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
-import moment from 'moment';
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
+import { format } from 'date-fns';
+
+export const APP_TYPES = Object.freeze({
+  DEBT: 'DEBT',
+  COPAY: 'COPAY',
+});
+
+export const ALERT_TYPES = Object.freeze({
+  ZERO: 'ZERO',
+  ERROR: 'ERROR',
+});
 
 export const combinedPortalAccess = state =>
   toggleValues(state)[FEATURE_FLAG_NAMES.combinedDebtPortalAccess];
@@ -9,7 +19,7 @@ export const selectLoadingFeatureFlags = state =>
   state?.featureToggles?.loading;
 
 export const formatDate = date => {
-  return moment(date, 'MM-DD-YYYY').format('MMMM D, YYYY');
+  return format(new Date(date), 'MMMM d, yyyy');
 };
 
 export const currency = amount => {
