7444ded0196c63b26b707548cc9af33807b850d9
Holden Hinkle <holden@holdenhinkle.com>
2022-05-04 18:04:51 -0400

    fix mock manifests

---
+++ script/github-actions/daily-product-dependency-scan/tests/mocks/applications/app-2/app-2a/wizard/pages/veteran/02-noDisabilityRating.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { CAREERS_EMPLOYMENT_ROOT_URL } from 'applications/vre/28-1900/constants';
+import { veteranPathPageNames } from '../pageList';
+import { recordNotificationEvent, fireLinkClickEvent } from '../helpers';
+
+const NoDisabilityRating = () => {
+  recordNotificationEvent('ineligibility - does not have disability rating');
+  return (
+    <div
+      className="feature vads-u-background-color--gray-lightest"
+      aria-live="polite"
+      aria-atomic="true"
+    >
+      <p id="ineligible-disability-notice">
+        To be eligible for VR&E benefits, you must have a service-connected
+        disability and an employment handicap in which your disability limits
+        your ability to get a job.
+      </p>
+      <a
+        onClick={e => fireLinkClickEvent(e)}
+        href={CAREERS_EMPLOYMENT_ROOT_URL}
+        aria-describedby="ineligible-disability-notice"
+      >
+        Find out about VA educational and career counseling
+      </a>
+    </div>
+  );
+};
+
+export default {
+  name: veteranPathPageNames.noDisabilityRating,
+  component: NoDisabilityRating,
+};
