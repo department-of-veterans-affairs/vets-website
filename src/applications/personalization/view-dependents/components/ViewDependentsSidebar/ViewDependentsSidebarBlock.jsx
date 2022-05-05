bfd08c2116d975b6ead8e42352d439b2e5374d5d
Rob Garrison <robin.garrison@adhocteam.us>
2022-05-05 08:43:38 -0500

    686: fix headers (#20966)

--- src/applications/personalization/view-dependents/components/ViewDependentsSidebar/ViewDependentsSidebarBlock.jsx
+++ src/applications/personalization/view-dependents/components/ViewDependentsSidebar/ViewDependentsSidebarBlock.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ViewDependentsSidebarBlock = props => (
   <div className="vads-u-margin-bottom--5">
-    <h3 className="vads-u-margin-top--0 vads-u-padding-bottom--1p5 vads-u-border-bottom--3px vads-u-border-color--primary">
+    <h2 className="vads-u-font-size--h3 vads-u-margin-top--0 vads-u-padding-bottom--1p5 vads-u-border-bottom--3px vads-u-border-color--primary">
       {props.heading}
-    </h3>
+    </h2>
     {props.content}
   </div>
 );
