4ba7416bf2dad1a0e0f6db908f1b4943f0239630
Holden Hinkle <holden@holdenhinkle.com>
2022-05-06 16:15:26 -0400

    update dependency field names

--- script/github-actions/daily-product-dependency-scan/csv/constants.js
+++ script/github-actions/daily-product-dependency-scan/csv/constants.js
@@ -1,6 +1,6 @@
 const constants = {
-  PACKAGE_DEPENDENCIES: 'Package Dependencies',
-  CROSS_PRODUCT_DEPENDENCIES: 'Cross Product Dependencies',
+  PACKAGE_DEPENDENCIES: 'dependencies',
+  CROSS_PRODUCT_DEPENDENCIES: 'cross_product_imports',
 };
 
 module.exports = constants;
