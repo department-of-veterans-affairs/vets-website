8889f03e06ee1ead61767bea7252645046c0dfbd
Holden Hinkle <holden@holdenhinkle.com>
2022-05-06 14:38:36 -0400

    debug - check package imports

--- script/github-actions/daily-product-dependency-scan/tests/index.unit.spec.js
+++ script/github-actions/daily-product-dependency-scan/tests/index.unit.spec.js
@@ -3,13 +3,13 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 
 // eslint-disable-next-line import/no-useless-path-segments
-const main = require('../../daily-product-dependency-scan');
-const GitHub = require('../github');
-const octokitResponses = require('./mocks/octokit-responses');
-const Csv = require('../csv');
-const Headings = require('../csv/headings');
-const Rows = require('../csv/rows');
-const { removeCarriageReturn, transformCsvToScsv } = require('../csv/helpers');
+// const main = require('../../daily-product-dependency-scan');
+// const GitHub = require('../github');
+// const octokitResponses = require('./mocks/octokit-responses');
+// const Csv = require('../csv');
+// const Headings = require('../csv/headings');
+// const Rows = require('../csv/rows');
+// const { removeCarriageReturn, transformCsvToScsv } = require('../csv/helpers');
 
 // describe('daily-product-dependency-scan', () => {
 //   context('success, dependency changes ARE detected', () => {
