10a8ab561cf57d2753e1e2b4bb1c11b6256b7199
Holden Hinkle <holden@holdenhinkle.com>
2022-05-06 16:21:13 -0400

    wip

--- script/github-actions/daily-product-dependency-scan/csv/constants.js
+++ script/github-actions/daily-product-dependency-scan/csv/constants.js
@@ -1,6 +1,12 @@
 const constants = {
-  PACKAGE_DEPENDENCIES: 'dependencies',
-  CROSS_PRODUCT_DEPENDENCIES: 'cross_product_imports',
+  PACKAGE_DEPENDENCIES: 'Package Dependencies',
+  CROSS_PRODUCT_DEPENDENCIES: 'Cross Product Dependencies',
 };
+
+// TODO replace the above with this
+// const constants = {
+//   PACKAGE_DEPENDENCIES: 'dependencies',
+//   CROSS_PRODUCT_DEPENDENCIES: 'cross_product_imports',
+// };
 
 module.exports = constants;
