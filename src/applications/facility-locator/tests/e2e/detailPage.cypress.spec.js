c4b7fe737f25c52faa182daee6e4ae0a989b486b
Ryan Koch <ryan.koch.0213@gmail.com>
2022-05-09 12:41:38 -0500

    debug

--- src/applications/facility-locator/tests/e2e/detailPage.cypress.spec.js
+++ src/applications/facility-locator/tests/e2e/detailPage.cypress.spec.js
@@ -1,6 +1,5 @@
 import mockFacilityDataV1 from '../../constants/mock-facility-v1.json';
 import mockGeocodingData from '../../constants/mock-geocoding-data.json';
-import { mapboxToken } from '../../utils/mapboxToken';
 
 describe('Detail Page', () => {
   beforeEach(() => {
@@ -13,8 +12,6 @@ describe('Detail Page', () => {
   });
 
   it('renders static map images on detail page', () => {
-    // eslint-disable-next-line no-console
-    console.log('MAPBOX TOKEN: ', mapboxToken);
     // from https://stackoverflow.com/questions/51246606/test-loading-of-image-in-cypress
     cy.visit('/find-locations/facility/vha_688GA');
     cy.get('[alt="Static map"]')
