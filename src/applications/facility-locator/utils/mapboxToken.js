103235fe1e859851f260b9e2b24be094400277cc
Ryan Koch <ryan.koch.0213@gmail.com>
2022-05-09 14:58:25 -0500

    debug

--- src/applications/facility-locator/utils/mapboxToken.js
+++ src/applications/facility-locator/utils/mapboxToken.js
@@ -1,3 +1,6 @@
+// export const mapboxToken =
+//   process.env.MAPBOX_TOKEN ||
+//   'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2wyZjNwM3dxMDZ4YjNjbzVwbTZ5aWQ1dyJ9.D8TZ1a4WobqcdYLWntXV_w';
+
 export const mapboxToken =
-  process.env.MAPBOX_TOKEN ||
-  'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2wyZjNwM3dxMDZ4YjNjbzVwbTZ5aWQ1dyJ9.D8TZ1a4WobqcdYLWntXV_w';
+  'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2l2Y3VlNWp5MDBoNjJvbHZ2a3R4bnN2cyJ9.2LoUhwRmz2OiCtRirnc6Pw';
--- src/applications/gi/utils/mapboxToken.js
+++ src/applications/gi/utils/mapboxToken.js
@@ -1,6 +1,6 @@
-// export const mapboxToken =
-//   'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2tvbHI5aDQ2MDAxaTJvcW5xYThremM1MyJ9.pEZjlJcYPfCNwoUEnMnagg';
-
 export const mapboxToken =
-  process.env.MAPBOX_TOKEN ||
-  'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2wyZjNwM3dxMDZ4YjNjbzVwbTZ5aWQ1dyJ9.D8TZ1a4WobqcdYLWntXV_w';
+  'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2tvbHI5aDQ2MDAxaTJvcW5xYThremM1MyJ9.pEZjlJcYPfCNwoUEnMnagg';
+
+// export const mapboxToken =
+//   process.env.MAPBOX_TOKEN ||
+//   'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2wyZjNwM3dxMDZ4YjNjbzVwbTZ5aWQ1dyJ9.D8TZ1a4WobqcdYLWntXV_w';
