ec56961c314a2cd10ccf772a49588788df79e7b8
Ryan Koch <ryan.koch.0213@gmail.com>
2022-05-10 11:27:39 -0500

    debug

--- src/applications/facility-locator/utils/mapboxToken.js
+++ src/applications/facility-locator/utils/mapboxToken.js
@@ -1,6 +1,6 @@
-// export const mapboxToken =
-//   process.env.MAPBOX_TOKEN ||
-//   'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2wyZjNwM3dxMDZ4YjNjbzVwbTZ5aWQ1dyJ9.D8TZ1a4WobqcdYLWntXV_w';
-
 export const mapboxToken =
-  'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2l2Y3VlNWp5MDBoNjJvbHZ2a3R4bnN2cyJ9.2LoUhwRmz2OiCtRirnc6Pw';
+  process.env.MAPBOX_TOKEN ||
+  'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2wyZjNwM3dxMDZ4YjNjbzVwbTZ5aWQ1dyJ9.D8TZ1a4WobqcdYLWntXV_w';
+
+// export const mapboxToken =
+//   'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2l2Y3VlNWp5MDBoNjJvbHZ2a3R4bnN2cyJ9.2LoUhwRmz2OiCtRirnc6Pw';
