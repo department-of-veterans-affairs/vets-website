712eea89d5dac85203b71841668978d7f0447e5d
Ryan Koch <ryan.koch.0213@gmail.com>
2022-05-10 13:14:14 -0500

    debug

--- src/applications/facility-locator/tests/e2e/serverErrors.cypress.spec.js
+++ src/applications/facility-locator/tests/e2e/serverErrors.cypress.spec.js
@@ -1,3 +1,5 @@
+import { mapboxToken } from '../../utils/mapboxToken';
+
 describe('Facility Locator error handling', () => {
   beforeEach(() => {
     cy.intercept('GET', '/v0/feature_toggles?*', []);
@@ -12,7 +14,7 @@ describe('Facility Locator error handling', () => {
     cy.visit('/find-locations');
   });
 
-  it('should show an error if the API returns a non-200 response', () => {
+  it(`MAPBOX TOKEN: ${mapboxToken}  ------  should show an error if the API returns a non-200 response`, () => {
     cy.get('#street-city-state-zip').type('Austin, TX');
     cy.get('#facility-type-dropdown').select('VA health');
     cy.get('#service-type-dropdown').select('Primary care');
