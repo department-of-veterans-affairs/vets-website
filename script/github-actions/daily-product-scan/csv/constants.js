56500301eb68ecbc6c3837208814d959c4c08886
Holden Hinkle <holden@holdenhinkle.com>
2022-05-11 13:10:40 -0400

    update csv constants and headings

--- script/github-actions/daily-product-scan/csv/constants.js
+++ script/github-actions/daily-product-scan/csv/constants.js
@@ -1,6 +1,9 @@
 const constants = {
   PACKAGE_DEPENDENCIES: 'package_dependencies',
   CROSS_PRODUCT_DEPENDENCIES: 'cross_product_dependencies',
+  HAS_UNIT_TESTS: 'has_unit_tests',
+  HAS_E2E_TESTS: 'has_e2e_tests',
+  HAS_CONTRACT_TESTS: 'has_contract_tests',
 };
 
 module.exports = constants;
--- script/github-actions/daily-product-scan/csv/headings.js
+++ script/github-actions/daily-product-scan/csv/headings.js
@@ -3,25 +3,30 @@ const constants = require('./constants');
 class Headings {
   constructor({ csvLine }) {
     this.all = csvLine.split(';');
-    [
-      this.packageDependencyIndex,
-      this.crossProductDependencyIndex,
-    ] = this.getDependencyHeadingIndices();
+    this.setFieldHeadingIndices();
   }
 
-  getDependencyHeadingIndices() {
-    let packageDependencyIndex;
-    let crossProductDependencyIndex;
-
+  setFieldHeadingIndices() {
     this.all.forEach((heading, index) => {
-      if (heading === constants.PACKAGE_DEPENDENCIES) {
-        packageDependencyIndex = index;
-      } else if (heading === constants.CROSS_PRODUCT_DEPENDENCIES) {
-        crossProductDependencyIndex = index;
+      switch (heading) {
+        case constants.PACKAGE_DEPENDENCIES:
+          this.packageDependencyIndex = index;
+          break;
+        case constants.CROSS_PRODUCT_DEPENDENCIES:
+          this.crossProductDependencyIndex = index;
+          break;
+        case constants.HAS_UNIT_TESTS:
+          this.hasUnitTests = index;
+          break;
+        case constants.HAS_E2E_TESTS:
+          this.hasE2eTests = index;
+          break;
+        case constants.HAS_CONTRACT_TESTS:
+          this.hasContractTests = index;
+          break;
+        default:
       }
     });
-
-    return [packageDependencyIndex, crossProductDependencyIndex];
   }
 }
 
