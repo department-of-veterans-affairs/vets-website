a411a419a8d4a691d10234fbf7bc74dee380de53
Curt Bonade <cbonade@Curts-MBP.attlocal.net>
2022-05-11 13:48:17 -0400

    WIP

--- src/applications/check-in/components/tests/PreCheckinConfirmation.test.unit.spec.jsx
+++ src/applications/check-in/components/tests/PreCheckinConfirmation.test.unit.spec.jsx
@@ -9,122 +9,118 @@ import PreCheckinConfirmation from '../PreCheckinConfirmation';
 describe('pre-check-in', () => {
   beforeEach(() => {
     let store;
-    describe('Confirmation page', () => {
-      describe('appointment without friendly name', () => {
-        const appointments = [
-          {
-            facility: 'LOMA LINDA VA CLINIC',
-            clinicPhoneNumber: '5551234567',
-            clinicFriendlyName: '',
-            clinicName: 'LOM ACC CLINIC TEST',
-            appointmentIen: 'some-ien',
-            startTime: '2021-11-30T17:12:10.694Z',
-            eligibility: 'ELIGIBLE',
-            facilityId: 'some-facility',
-            checkInWindowStart: '2021-11-30T17:12:10.694Z',
-            checkInWindowEnd: '2021-11-30T17:12:10.694Z',
-            checkedInTime: '',
-          },
-        ];
-        it('renders loading screen', () => {
-          const wrapper = shallow(
-            <PreCheckinConfirmation
-              appointments={appointments}
-              hasUpdates={false}
-              isLoading
-            />,
-          );
-          expect(wrapper.find('va-loading-indicator').length).to.equal(1);
-          wrapper.unmount();
-        });
-        it('renders page with clinic name', () => {
-          const screen = render(
-            <PreCheckinConfirmation
-              appointments={appointments}
-              hasUpdates={false}
-              isLoading={false}
-            />,
-          );
-          expect(screen.getAllByText('LOM ACC CLINIC TEST')).to.have.lengthOf(
-            1,
-          );
-        });
+    describe('Confirmation page - appointment without friendly name', () => {
+      const appointments = [
+        {
+          facility: 'LOMA LINDA VA CLINIC',
+          clinicPhoneNumber: '5551234567',
+          clinicFriendlyName: '',
+          clinicName: 'LOM ACC CLINIC TEST',
+          appointmentIen: 'some-ien',
+          startTime: '2021-11-30T17:12:10.694Z',
+          eligibility: 'ELIGIBLE',
+          facilityId: 'some-facility',
+          checkInWindowStart: '2021-11-30T17:12:10.694Z',
+          checkInWindowEnd: '2021-11-30T17:12:10.694Z',
+          checkedInTime: '',
+        },
+      ];
+      it('renders loading screen', () => {
+        const wrapper = shallow(
+          <PreCheckinConfirmation
+            appointments={appointments}
+            hasUpdates={false}
+            isLoading
+          />,
+        );
+        expect(wrapper.find('va-loading-indicator').length).to.equal(1);
+        wrapper.unmount();
       });
-      describe('appointments with friendly name', () => {
-        const appointments = [
-          {
-            facility: 'LOMA LINDA VA CLINIC',
-            clinicPhoneNumber: '5551234567',
-            clinicFriendlyName: 'TEST CLINIC',
-            clinicName: 'LOM ACC CLINIC TEST',
-            appointmentIen: 'some-ien',
-            startTime: '2021-11-30T17:12:10.694Z',
-            eligibility: 'ELIGIBLE',
-            facilityId: 'some-facility',
-            checkInWindowStart: '2021-11-30T17:12:10.694Z',
-            checkInWindowEnd: '2021-11-30T17:12:10.694Z',
-            checkedInTime: '',
-          },
-          {
-            facility: 'LOMA LINDA VA CLINIC',
-            clinicPhoneNumber: '5551234567',
-            clinicFriendlyName: 'TEST CLINIC',
-            clinicName: 'LOM ACC CLINIC TEST',
-            appointmentIen: 'some-ien',
-            startTime: '2021-11-30T17:12:10.694Z',
-            eligibility: 'ELIGIBLE',
-            facilityId: 'some-facility',
-            checkInWindowStart: '2021-11-30T17:12:10.694Z',
-            checkInWindowEnd: '2021-11-30T17:12:10.694Z',
-            checkedInTime: '',
-          },
-          {
-            facility: 'LOMA LINDA VA CLINIC',
-            clinicPhoneNumber: '5551234567',
-            clinicFriendlyName: 'TEST CLINIC',
-            clinicName: 'LOM ACC CLINIC TEST',
-            appointmentIen: 'some-other-ien',
-            startTime: '2021-11-30T17:12:10.694Z',
-            eligibility: 'ELIGIBLE',
-            facilityId: 'some-facility',
-            checkInWindowStart: '2021-11-30T17:12:10.694Z',
-            checkInWindowEnd: '2021-11-30T17:12:10.694Z',
-            checkedInTime: '',
-          },
-        ];
-        it('renders page - no updates', () => {
-          const screen = render(
-            <Provider store={store}>
-              <PreCheckinConfirmation
-                appointments={appointments}
-                hasUpdates={false}
-                isLoading={false}
-              />
-              ,
-            </Provider>,
-          );
-          expect(screen.getByTestId('confirmation-wrapper')).to.exist;
-          expect(screen.queryByTestId('confirmation-update-alert')).to.be.null;
-        });
-        it('renders page with clinic friendly name', () => {
-          const screen = render(
+      it('renders page with clinic name', () => {
+        const screen = render(
+          <PreCheckinConfirmation
+            appointments={appointments}
+            hasUpdates={false}
+            isLoading={false}
+          />,
+        );
+        expect(screen.getAllByText('LOM ACC CLINIC TEST')).to.have.lengthOf(1);
+      });
+    });
+    describe('Confirmation page - appointments with friendly name', () => {
+      const appointments = [
+        {
+          facility: 'LOMA LINDA VA CLINIC',
+          clinicPhoneNumber: '5551234567',
+          clinicFriendlyName: 'TEST CLINIC',
+          clinicName: 'LOM ACC CLINIC TEST',
+          appointmentIen: 'some-ien',
+          startTime: '2021-11-30T17:12:10.694Z',
+          eligibility: 'ELIGIBLE',
+          facilityId: 'some-facility',
+          checkInWindowStart: '2021-11-30T17:12:10.694Z',
+          checkInWindowEnd: '2021-11-30T17:12:10.694Z',
+          checkedInTime: '',
+        },
+        {
+          facility: 'LOMA LINDA VA CLINIC',
+          clinicPhoneNumber: '5551234567',
+          clinicFriendlyName: 'TEST CLINIC',
+          clinicName: 'LOM ACC CLINIC TEST',
+          appointmentIen: 'some-ien',
+          startTime: '2021-11-30T17:12:10.694Z',
+          eligibility: 'ELIGIBLE',
+          facilityId: 'some-facility',
+          checkInWindowStart: '2021-11-30T17:12:10.694Z',
+          checkInWindowEnd: '2021-11-30T17:12:10.694Z',
+          checkedInTime: '',
+        },
+        {
+          facility: 'LOMA LINDA VA CLINIC',
+          clinicPhoneNumber: '5551234567',
+          clinicFriendlyName: 'TEST CLINIC',
+          clinicName: 'LOM ACC CLINIC TEST',
+          appointmentIen: 'some-other-ien',
+          startTime: '2021-11-30T17:12:10.694Z',
+          eligibility: 'ELIGIBLE',
+          facilityId: 'some-facility',
+          checkInWindowStart: '2021-11-30T17:12:10.694Z',
+          checkInWindowEnd: '2021-11-30T17:12:10.694Z',
+          checkedInTime: '',
+        },
+      ];
+      it('renders page - no updates', () => {
+        const screen = render(
+          <Provider store={store}>
             <PreCheckinConfirmation
               appointments={appointments}
               hasUpdates={false}
               isLoading={false}
-            />,
-          );
-          expect(screen.getAllByText('TEST CLINIC')).to.have.lengthOf(3);
-        });
-        it('page passes axeCheck', () => {
-          axeCheck(
-            <PreCheckinConfirmation
-              appointments={appointments}
-              hasUpdates={false}
-              isLoading={false}
-            />,
-          );
-        });
+            />
+            ,
+          </Provider>,
+        );
+        expect(screen.getByTestId('confirmation-wrapper')).to.exist;
+        expect(screen.queryByTestId('confirmation-update-alert')).to.be.null;
+      });
+      it('renders page with clinic friendly name', () => {
+        const screen = render(
+          <PreCheckinConfirmation
+            appointments={appointments}
+            hasUpdates={false}
+            isLoading={false}
+          />,
+        );
+        expect(screen.getAllByText('TEST CLINIC')).to.have.lengthOf(3);
+      });
+      it('page passes axeCheck', () => {
+        axeCheck(
+          <PreCheckinConfirmation
+            appointments={appointments}
+            hasUpdates={false}
+            isLoading={false}
+          />,
+        );
       });
     });
   });
