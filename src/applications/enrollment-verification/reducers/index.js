ee608e32304ce5bcfd919ca36295c945ebe0bb3b
nbuckafs <nathaniel.buck@accenturefederal.com>
2022-05-12 14:29:57 -0400

    Map Enrollment Verification Object for Submission

--- src/applications/enrollment-verification/reducers/index.js
+++ src/applications/enrollment-verification/reducers/index.js
@@ -14,268 +14,62 @@ export default {
       case FETCH_POST_911_GI_BILL_ELIGIBILITY_FAILURE:
         return {
           ...state,
-          enrollmentVerification:
-            action?.response?.data?.attributes ||
-            {
-              // claimantId: 600000000,
-              // lastCertifiedThroughDate: '2021-10-01',
-              // paymentOnHold: false,
-              // enrollmentVerifications: [
-              //   {
-              //     verificationMonth: 'October 2021',
-              //     certifiedBeginDate: '2021-10-01',
-              //     certifiedEndDate: '2021-10-31',
-              //     certifiedThroughDate: null,
-              //     certificationMethod: null,
-              //     enrollments: [
-              //       {
-              //         facilityName: 'UNIVERSITY OF HAWAII AT HILO',
-              //         beginDate: '2021-08-01',
-              //         endDate: '2021-10-01',
-              //         totalCreditHours: 22.0,
-              //       },
-              //     ],
-              //     verificationResponse: 'NR',
-              //     createdDate: null,
-              //   },
-              //   {
-              //     verificationMonth: 'September 2021',
-              //     certifiedBeginDate: '2021-09-01',
-              //     certifiedEndDate: '2021-09-30',
-              //     certifiedThroughDate: null,
-              //     certificationMethod: null,
-              //     enrollments: [
-              //       {
-              //         facilityName: 'UNIVERSITY OF HAWAII AT HILO',
-              //         beginDate: '2021-08-01',
-              //         endDate: '2021-10-01',
-              //         totalCreditHours: 22.0,
-              //       },
-              //     ],
-              //     verificationResponse: 'NR',
-              //     createdDate: null,
-              //   },
-              //   {
-              //     verificationMonth: 'August 2021',
-              //     certifiedBeginDate: '2021-08-01',
-              //     certifiedEndDate: '2021-08-31',
-              //     certifiedThroughDate: null,
-              //     certificationMethod: null,
-              //     enrollments: [
-              //       {
-              //         facilityName: 'UNIVERSITY OF HAWAII AT HILO',
-              //         beginDate: '2021-08-01',
-              //         endDate: '2021-10-01',
-              //         totalCreditHours: 22.0,
-              //       },
-              //     ],
-              //     verificationResponse: 'NR',
-              //     createdDate: null,
-              //   },
-              // ],
-            },
-
-          // {
-          //   paymentStatus: PAYMENT_STATUS.ONGOING,
-          //   nextVerificationDate: '2022-03-01',
-          //   months: [
+          enrollmentVerification: action?.response?.data?.attributes,
+          // enrollmentVerification: action?.response?.data?.attributes || {
+          //   claimantId: 600000000,
+          //   lastCertifiedThroughDate: '2021-10-01',
+          //   paymentOnHold: false,
+          //   enrollmentVerifications: [
           //     {
-          //       month: '2022-02',
-          //       verified: false,
+          //       verificationMonth: 'October 2021',
+          //       certifiedBeginDate: '2021-10-01',
+          //       certifiedEndDate: '2021-10-31',
+          //       certifiedThroughDate: null,
+          //       certificationMethod: null,
           //       enrollments: [
           //         {
-          //           institution: 'Wake Forest University School of Business',
-          //           creditHours: 15,
-          //           startDate: '2022-02-01',
-          //           endDate: '2022-02-28',
+          //           facilityName: 'UNIVERSITY OF HAWAII AT HILO',
+          //           beginDate: '2021-08-01',
+          //           endDate: '2021-10-01',
+          //           totalCreditHours: 22.0,
           //         },
           //       ],
+          //       verificationResponse: 'NR',
+          //       createdDate: null,
           //     },
           //     {
-          //       month: '2022-01',
-          //       verified: false,
+          //       verificationMonth: 'September 2021',
+          //       certifiedBeginDate: '2021-09-01',
+          //       certifiedEndDate: '2021-09-30',
+          //       certifiedThroughDate: null,
+          //       certificationMethod: null,
           //       enrollments: [
           //         {
-          //           institution: 'Wake Forest University School of Business',
-          //           creditHours: 15,
-          //           startDate: '2022-01-01',
-          //           endDate: '2022-01-31',
+          //           facilityName: 'UNIVERSITY OF HAWAII AT HILO',
+          //           beginDate: '2021-08-01',
+          //           endDate: '2021-10-01',
+          //           totalCreditHours: 22.0,
           //         },
           //       ],
+          //       verificationResponse: 'NR',
+          //       createdDate: null,
           //     },
           //     {
-          //       month: '2021-12',
-          //       verified: false,
+          //       verificationMonth: 'August 2021',
+          //       certifiedBeginDate: '2021-08-01',
+          //       certifiedEndDate: '2021-08-31',
+          //       certifiedThroughDate: null,
+          //       certificationMethod: null,
           //       enrollments: [
           //         {
-          //           institution: 'Wake Forest University School of Business',
-          //           creditHours: 10,
-          //           startDate: '2021-12-01',
-          //           endDate: '2021-12-31',
-          //         },
-          //         {
-          //           institution: 'Adirondack Community College',
-          //           creditHours: 4,
-          //           startDate: '2021-12-01',
-          //           endDate: '2021-12-31',
+          //           facilityName: 'UNIVERSITY OF HAWAII AT HILO',
+          //           beginDate: '2021-08-01',
+          //           endDate: '2021-10-01',
+          //           totalCreditHours: 22.0,
           //         },
           //       ],
-          //     },
-          //     {
-          //       month: '2021-11',
-          //       verified: true,
-          //       enrollments: [
-          //         {
-          //           institution: 'Wake Forest University School of Business',
-          //           creditHours: 10,
-          //           startDate: '2021-11-01',
-          //           endDate: '2021-11-30',
-          //         },
-          //         {
-          //           institution: 'Adirondack Community College',
-          //           creditHours: 4,
-          //           startDate: '2021-11-01',
-          //           endDate: '2021-11-30',
-          //         },
-          //       ],
-          //     },
-          //     {
-          //       month: '2021-10',
-          //       verified: true,
-          //       enrollments: [
-          //         {
-          //           institution: 'Wake Forest University School of Business',
-          //           creditHours: 10,
-          //           startDate: '2021-10-01',
-          //           endDate: '2021-10-31',
-          //         },
-          //         {
-          //           institution: 'Adirondack Community College',
-          //           creditHours: 4,
-          //           startDate: '2021-10-01',
-          //           endDate: '2021-10-31',
-          //         },
-          //       ],
-          //     },
-          //     {
-          //       month: '2021-09',
-          //       verified: true,
-          //       enrollments: [
-          //         {
-          //           institution: 'Wake Forest University School of Business',
-          //           creditHours: 10,
-          //           startDate: '2021-09-01',
-          //           endDate: '2021-9-30',
-          //         },
-          //         {
-          //           institution: 'Adirondack Community College',
-          //           creditHours: 4,
-          //           startDate: '2021-10-01',
-          //           endDate: '2021-10-31',
-          //         },
-          //       ],
-          //     },
-          //     {
-          //       month: '2021-08',
-          //       verified: true,
-          //       enrollments: [
-          //         {
-          //           institution: 'Wake Forest University School of Business',
-          //           creditHours: 10,
-          //           startDate: '2021-10-01',
-          //           endDate: '2021-10-31',
-          //         },
-          //         {
-          //           institution: 'Adirondack Community College',
-          //           creditHours: 4,
-          //           startDate: '2021-8-01',
-          //           endDate: '2021-8-31',
-          //         },
-          //       ],
-          //     },
-          //     {
-          //       month: '2021-07',
-          //       verified: true,
-          //       enrollments: [
-          //         {
-          //           institution: 'Adirondack Community College',
-          //           creditHours: 4,
-          //           startDate: '2021-07-01',
-          //           endDate: '2021-07-31',
-          //         },
-          //       ],
-          //     },
-          //     {
-          //       month: '2021-06',
-          //       verified: true,
-          //       enrollments: [
-          //         {
-          //           institution: 'Adirondack Community College',
-          //           creditHours: 4,
-          //           startDate: '2021-06-01',
-          //           endDate: '2021-06-30',
-          //         },
-          //       ],
-          //     },
-          //     {
-          //       month: '2021-05',
-          //       verified: true,
-          //       enrollments: [
-          //         {
-          //           institution: 'Adirondack Community College',
-          //           creditHours: 4,
-          //           startDate: '2021-05-01',
-          //           endDate: '2021-05-31',
-          //         },
-          //       ],
-          //     },
-          //     {
-          //       month: '2021-04',
-          //       verified: true,
-          //       enrollments: [
-          //         {
-          //           institution: 'Wake Forest University School of Business',
-          //           creditHours: 15,
-          //           startDate: '2021-04-01',
-          //           endDate: '2021-04-30',
-          //         },
-          //       ],
-          //     },
-          //     {
-          //       month: '2021-03',
-          //       verified: true,
-          //       enrollments: [
-          //         {
-          //           institution: 'Wake Forest University School of Business',
-          //           creditHours: 15,
-          //           startDate: '2021-03-01',
-          //           endDate: '2021-03-31',
-          //         },
-          //       ],
-          //     },
-          //     {
-          //       month: '2021-02',
-          //       verified: true,
-          //       enrollments: [
-          //         {
-          //           institution: 'Wake Forest University School of Business',
-          //           creditHours: 15,
-          //           startDate: '2021-02-01',
-          //           endDate: '2021-02-28',
-          //         },
-          //       ],
-          //     },
-          //     {
-          //       month: '2021-01',
-          //       verified: true,
-          //       enrollments: [
-          //         {
-          //           institution: 'Wake Forest University School of Business',
-          //           creditHours: 15,
-          //           startDate: '2021-01-01',
-          //           endDate: '2021-01-31',
-          //         },
-          //       ],
+          //       verificationResponse: 'NR',
+          //       createdDate: null,
           //     },
           //   ],
           // },
