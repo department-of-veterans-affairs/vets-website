df9bbae4d37689b854434edaead28e3d7cd9b841
Max Antonucci <maxx1128@gmail.com>
2022-05-13 16:45:44 -0400

    VAVFA-8986: WIP add COVID status to facility search

--- src/applications/facility-locator/components/search-results-items/common/LocationOperationStatus.jsx
+++ src/applications/facility-locator/components/search-results-items/common/LocationOperationStatus.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { OperatingStatus } from '../../../constants';
 
+// import VaAlertExpandable from '@department-of-veterans-affairs/component-library/CollapsiblePanel';
+// import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
+// import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
+
 const LocationOperationStatus = ({ operatingStatus }) => {
   if (!operatingStatus || operatingStatus.code === 'NORMAL') {
     return <></>;
   }
+
+  // console.log(toggleValues(state)[FEATURE_FLAG_NAMES.showExpandableVamcAlert]);
+  const showExpandableVamcAlert = true;
 
   const display = {
     [OperatingStatus.CLOSED]: {
@@ -29,19 +36,40 @@ const LocationOperationStatus = ({ operatingStatus }) => {
   const { operationStatusTitle, alertClass } = display[operatingStatus.code];
 
   return (
-    <va-alert
-      background-only
-      show-icon
-      status={alertClass}
-      visible
-      data-testid={`${operatingStatus.code.toLowerCase()}-message`}
-      class="vads-u-margin-y--2"
-    >
-      <div tabIndex={0}>
-        <span className="sr-only">Alert: </span>
-        {operationStatusTitle}
-      </div>
-    </va-alert>
+    <div>
+      <va-alert
+        background-only
+        show-icon
+        status={alertClass}
+        visible
+        data-testid={`${operatingStatus.code.toLowerCase()}-message`}
+        class="vads-u-margin-y--2"
+      >
+        <div tabIndex={0}>
+          <span className="sr-only">Alert: </span>
+          {operationStatusTitle}
+        </div>
+      </va-alert>
+
+      {operatingStatus.supplementalStatus.map(status => {
+        if (status.id.includes('COVID') && showExpandableVamcAlert) {
+          return (
+            <va-alert
+              background-only
+              show-icon
+              status={alertClass}
+              visible
+              data-testid={`${operatingStatus.code.toLowerCase()}-message`}
+              class="vads-u-margin-y--2"
+            >
+              <div tabIndex={0}>{status.label}</div>
+            </va-alert>
+          );
+        }
+
+        return null;
+      })}
+    </div>
   );
 };
 
