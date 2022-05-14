5f4769655c96d05f3854d8b741a5a241fe1ec2c0
Ethan Teague <metrowebz@gmail.com>
2022-05-14 13:47:57 -0400

    VAVFA-8986: Add flipper flag grab to facility locator items.

--- src/applications/facility-locator/components/search-results-items/common/LocationOperationStatus.jsx
+++ src/applications/facility-locator/components/search-results-items/common/LocationOperationStatus.jsx
@@ -1,18 +1,38 @@
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 import React from 'react';
 import PropTypes from 'prop-types';
+import createCommonStore from 'platform/startup/store';
+import { connectFeatureToggle } from 'platform/utilities/feature-toggles';
+import { FeatureToggleReducer } from 'platform/site-wide/feature-toggles/reducers';
+import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
 import { OperatingStatus } from '../../../constants';
 
-// import VaAlertExpandable from '@department-of-veterans-affairs/component-library/CollapsiblePanel';
-// import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
-// import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
+const store = createCommonStore({
+  FeatureToggleReducer,
+});
 
 const LocationOperationStatus = ({ operatingStatus }) => {
+  connectFeatureToggle(store.dispatch);
+  store.subscribe(() => {
+    const flags = toggleValues(store.getState());
+    if (
+      flags?.showExpandableVamcAlert === 'undefined' ||
+      flags?.showExpandableVamcAlert === false
+    ) {
+      const expandableAlerts = document.querySelectorAll(
+        // whatever the class / target is named
+        '.va-alert-expandable',
+      );
+      expandableAlerts.forEach(element => {
+        element.classList.add('vads-u-display--none');
+      });
+    }
+  });
+
   if (!operatingStatus || operatingStatus.code === 'NORMAL') {
     return <></>;
   }
 
-  // console.log(toggleValues(state)[FEATURE_FLAG_NAMES.showExpandableVamcAlert]);
   const showExpandableVamcAlert = true;
 
   const display = {
