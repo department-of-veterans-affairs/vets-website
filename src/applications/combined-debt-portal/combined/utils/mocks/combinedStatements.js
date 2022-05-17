a424df972e21f8d35953613d2d148ce2f16634a3
OptionSelect <brandyn.sullins@gmail.com>
2022-05-17 10:56:15 -0400

    Combined statements now spit out correctly ilocalhost 3000 as well as am able to navigate visually on 3001

--- src/applications/combined-debt-portal/combined/utils/mocks/combinedStatements.js
+++ src/applications/combined-debt-portal/combined/utils/mocks/combinedStatements.js
@@ -1,5 +1,6 @@
 const mockStatements = require('./mockStatements.json');
+const mockDebts = require('./mockDebts.json');
 
-const happyPath = { data: mockStatements };
+const happyPath = { data: [{ mockStatements }, { mockDebts }] };
 
 module.exports = { happyPath };
--- src/applications/combined-debt-portal/combined/utils/mocks/mockServer.js
+++ src/applications/combined-debt-portal/combined/utils/mocks/mockServer.js
@@ -1,11 +1,11 @@
 const delay = require('mocker-api/lib/delay');
 const commonResponses = require('../../../../../platform/testing/local-dev-mock-api/common');
-const mockStatements = require('./combinedStatements');
+const happyPath = require('./combinedStatements');
 
 /* eslint-disable camelcase */
 const responses = {
   ...commonResponses,
-  'GET v0/medical_copays': mockStatements,
+  'GET /v0/medical_copays': happyPath,
 };
 
 module.exports = delay(responses, 2000);
