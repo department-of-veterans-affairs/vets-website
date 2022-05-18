5e380961ff39bcfb667380d1f5bbe0e5b54923ec
Allison Lu <allisonlu820@gmail.com>
2022-05-18 10:03:51 -0400

    VACMS-8362: - removed mock suggestions, trying to use Typeahead API instead - added documentation

--- src/applications/static-pages/homepage/HomepageSearch.jsx
+++ src/applications/static-pages/homepage/HomepageSearch.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import { VaSearchInput } from '@department-of-veterans-affairs/web-components/react-bindings';
 import PropTypes from 'prop-types';
-import { replaceWithStagingDomain } from '../../../platform/utilities/environment/stagingDomains';
-// import recordEvent from '../../../platform/monitoring/record-event';
+import * as Sentry from '@sentry/browser';
 
+import { replaceWithStagingDomain } from '../../../platform/utilities/environment/stagingDomains';
+import { apiRequest } from '../../../platform/utilities/api';
+
+/**
+ * Homepage redesign
+ * Search component that appears in the Common Tasks section
+ *
+ * uses the new Search Input component from the VA Design System
+ */
 const HomepageSearch = ({ value, suggestions }) => {
-  // const [userInput, setUserInput] = useState(value);
+  const [userInput, setUserInput] = useState(value);
   const [latestSuggestions, setLatestSuggestions] = useState(suggestions);
 
   /**
@@ -13,28 +21,56 @@ const HomepageSearch = ({ value, suggestions }) => {
    * Provides suggestions for the following values: for, form, forms
    * All other values will return an empty array
    */
-  const mockSuggestions = [
-    'foreign study',
-    'forever gi bill',
-    'form',
-    'form finder',
-    'form search',
-    'forms',
-  ];
+  // const mockSuggestions = [
+  //   'foreign study',
+  //   'forever gi bill',
+  //   'form',
+  //   'form finder',
+  //   'form search',
+  //   'forms',
+  // ];
+
+  // TA2.0
+  const fetchDropDownSuggestions = async inputValue => {
+    // encode user input for query to suggestions url
+    const encodedInput = encodeURIComponent(inputValue);
+
+    // fetch suggestions
+    try {
+      const apiRequestOptions = {
+        method: 'GET',
+      };
+      const fetchedSuggestions = await apiRequest(
+        `/search_typeahead?query=${encodedInput}`,
+        apiRequestOptions,
+      );
+
+      if (fetchedSuggestions.length !== 0) {
+        return fetchedSuggestions.sort((a, b) => {
+          return a.length - b.length;
+        });
+      }
+      return [];
+      // if we fail to fetch suggestions
+    } catch (error) {
+      if (error?.error?.code === 'OVER_RATE_LIMIT') {
+        Sentry.captureException(
+          new Error(`"OVER_RATE_LIMIT" - Search Typeahead`),
+        );
+      }
+      Sentry.captureException(error);
+    }
+    return [];
+  };
 
   const handleInput = e => {
     if (e.target.value.length < 3) return;
     setTimeout(
-      () =>
-        setLatestSuggestions(
-          mockSuggestions.filter(suggestion =>
-            suggestion.includes(e.target.value),
-          ),
-        ),
+      () => setLatestSuggestions(fetchDropDownSuggestions(e.target.value)),
       500,
     );
 
-    // setUserInput(e.target.value);
+    setUserInput(e.target.value);
   };
 
   const handleSubmit = e => {
@@ -56,7 +92,7 @@ const HomepageSearch = ({ value, suggestions }) => {
       </label>
 
       <VaSearchInput
-        value={value}
+        value={userInput}
         onInput={handleInput}
         onSubmit={handleSubmit}
         suggestions={latestSuggestions}
