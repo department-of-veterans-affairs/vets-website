c793a06787598c04aa2e3e24c12459599bb922e8
Alex Garcia <67602137+AGarcia-Clarity@users.noreply.github.com>
2022-05-19 17:02:31 -0400

    Removes date from MHV Inherited Proofing (#21107)
    * updates date
    
    * double checks spacing; removes loggedin check

--- src/applications/mhv-inherited-proofing/container/TransitionContainer.jsx
+++ src/applications/mhv-inherited-proofing/container/TransitionContainer.jsx
@@ -1,15 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { isLoggedIn } from 'platform/user/selectors';
-import { signInServiceName } from 'platform/user/authentication/selectors';
 
 export default function TransitionContainer({ children }) {
-  const canView = useSelector(isLoggedIn);
-  const isSignedInWithMHV = useSelector(signInServiceName);
-
-  if (!canView || !['myhealthevet', 'mhv'].includes(isSignedInWithMHV)) {
-    window.location = '/';
-  }
-
   return <div className="transfer-account">{children}</div>;
 }
