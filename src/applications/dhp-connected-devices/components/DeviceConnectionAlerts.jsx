66a0f52783f25677f872f927fa1b99755050299e
Rebecca Green <rebecca.green@thoughtworks.com>
2022-05-19 15:48:31 -0400

    [DHP-228] Adds generic device connection alert

--- src/applications/dhp-connected-devices/components/DeviceConnectionAlerts.jsx
+++ src/applications/dhp-connected-devices/components/DeviceConnectionAlerts.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const DeviceConnectionSucceededAlert = () => {
   return (
@@ -31,4 +32,31 @@ export const DeviceConnectionFailedAlert = () => {
       </va-alert>
     </div>
   );
+};
+
+export const DeviceConnectionAlert = ({
+  testId,
+  status,
+  headline,
+  description,
+}) => {
+  return (
+    <div data-testid={testId}>
+      <va-alert
+        close-btn-aria-label="Close notification"
+        status={status}
+        visible
+      >
+        <h3 slot="headline">{headline}</h3>
+        <div>{description}</div>
+      </va-alert>
+    </div>
+  );
+};
+
+DeviceConnectionAlert.propTypes = {
+  description: PropTypes.string.isRequired,
+  headline: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  testId: PropTypes.string.isRequired,
 };
--- src/applications/dhp-connected-devices/tests/components/DeviceConnectionAlerts.spec.jsx
+++ src/applications/dhp-connected-devices/tests/components/DeviceConnectionAlerts.spec.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import { expect } from 'chai';
-import { renderInReduxProvider } from 'platform/testing/unit/react-testing-library-helpers';
+import { render } from '@testing-library/react';
 import {
   DeviceConnectionSucceededAlert,
   DeviceConnectionFailedAlert,
+  DeviceConnectionAlert,
 } from '../../components/DeviceConnectionAlerts';
 
 describe('Successful device connection alert', () => {
   it('shows a success message', () => {
-    const screen = renderInReduxProvider(<DeviceConnectionSucceededAlert />);
-    expect(screen.getByText(/Device Connected/)).to.exist;
+    const { getByTestId } = render(<DeviceConnectionSucceededAlert />);
+    expect(getByTestId('success-alert')).to.exist;
   });
 });
 
 describe('Failed device connect alert', () => {
   it('shows a failure message', () => {
-    const screen = renderInReduxProvider(<DeviceConnectionFailedAlert />);
-    expect(screen.getByText(/We couldn't connect your device/)).to.exist;
+    const { getByTestId } = render(<DeviceConnectionFailedAlert />);
+    expect(getByTestId('failure-alert')).to.exist;
+  });
+});
+
+describe('Device connection alerts', () => {
+  it('should show a success alert when given success params', () => {
+    const { getByTestId, getByText } = render(
+      <DeviceConnectionAlert
+        testId="success-alert"
+        status="success"
+        headline="Device connected"
+        description="Your device is now connected"
+      />,
+    );
+    expect(getByTestId('success-alert')).to.exist;
+    expect(getByText('Device connected')).to.exist;
+    expect(getByText('Your device is now connected')).to.exist;
   });
 });
