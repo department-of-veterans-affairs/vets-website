1cd4d60c0f05a5ad653b7322b225f43c138c89c8
nbuckafs <nathaniel.buck@accenturefederal.com>
2022-05-19 15:44:18 -0400

    Filter Future Enrollment Verifications

--- src/applications/enrollment-verification/reducers/index.js
+++ src/applications/enrollment-verification/reducers/index.js
@@ -7,6 +7,82 @@ import {
 
 const initialState = {};
 
+// const testEv = {
+//   claimantId: 600000000,
+//   lastCertifiedThroughDate: '2021-10-01',
+//   paymentOnHold: false,
+//   enrollmentVerifications: [
+//     {
+//       verificationMonth: 'May 2022',
+//       certifiedBeginDate: '2022-05-01',
+//       certifiedEndDate: '2022-05-19',
+//       certifiedThroughDate: null,
+//       certificationMethod: null,
+//       enrollments: [
+//         {
+//           facilityName: 'UNIVERSITY OF HAWAII AT HILO',
+//           beginDate: '2022-05-01',
+//           endDate: '2022-05-31',
+//           totalCreditHours: 22.0,
+//         },
+//       ],
+//       verificationResponse: 'NR',
+//       createdDate: null,
+//     },
+//     {
+//       verificationMonth: 'October 2021',
+//       certifiedBeginDate: '2021-10-01',
+//       certifiedEndDate: '2021-10-31',
+//       certifiedThroughDate: null,
+//       certificationMethod: null,
+//       enrollments: [
+//         {
+//           facilityName: 'UNIVERSITY OF HAWAII AT HILO',
+//           beginDate: '2021-08-01',
+//           endDate: '2021-10-01',
+//           totalCreditHours: 22.0,
+//         },
+//       ],
+//       verificationResponse: 'NR',
+//       createdDate: null,
+//     },
+//     {
+//       verificationMonth: 'September 2021',
+//       certifiedBeginDate: '2021-09-01',
+//       certifiedEndDate: '2021-09-30',
+//       certifiedThroughDate: null,
+//       certificationMethod: null,
+//       enrollments: [
+//         {
+//           facilityName: 'UNIVERSITY OF HAWAII AT HILO',
+//           beginDate: '2021-08-01',
+//           endDate: '2021-10-01',
+//           totalCreditHours: 22.0,
+//         },
+//       ],
+//       verificationResponse: 'NR',
+//       createdDate: null,
+//     },
+//     {
+//       verificationMonth: 'August 2021',
+//       certifiedBeginDate: '2021-08-01',
+//       certifiedEndDate: '2021-08-31',
+//       certifiedThroughDate: null,
+//       certificationMethod: null,
+//       enrollments: [
+//         {
+//           facilityName: 'UNIVERSITY OF HAWAII AT HILO',
+//           beginDate: '2021-08-01',
+//           endDate: '2021-10-01',
+//           totalCreditHours: 22.0,
+//         },
+//       ],
+//       verificationResponse: 'NR',
+//       createdDate: null,
+//     },
+//   ],
+// };
+
 export default {
   data: (state = initialState, action) => {
     switch (action.type) {
@@ -14,65 +90,32 @@ export default {
       case FETCH_POST_911_GI_BILL_ELIGIBILITY_FAILURE:
         return {
           ...state,
-          enrollmentVerification: action?.response?.data?.attributes,
-          // enrollmentVerification: action?.response?.data?.attributes || {
-          //   claimantId: 600000000,
-          //   lastCertifiedThroughDate: '2021-10-01',
-          //   paymentOnHold: false,
-          //   enrollmentVerifications: [
-          //     {
-          //       verificationMonth: 'October 2021',
-          //       certifiedBeginDate: '2021-10-01',
-          //       certifiedEndDate: '2021-10-31',
-          //       certifiedThroughDate: null,
-          //       certificationMethod: null,
-          //       enrollments: [
-          //         {
-          //           facilityName: 'UNIVERSITY OF HAWAII AT HILO',
-          //           beginDate: '2021-08-01',
-          //           endDate: '2021-10-01',
-          //           totalCreditHours: 22.0,
-          //         },
-          //       ],
-          //       verificationResponse: 'NR',
-          //       createdDate: null,
+          enrollmentVerification: {
+            ...action?.response?.data?.attributes,
+            enrollmentVerifications: action?.response?.data?.attributes?.enrollmentVerifications?.filter(
+              ev =>
+                ev.certifiedEndDate < new Date().toISOString().split('T')[0],
+            ),
+          },
+
+          // Test data:
+          // enrollmentVerification: action?.response?.data?.attributes
+          //   ? {
+          //       ...action?.response?.data?.attributes,
+          //       enrollmentVerifications: action?.response?.data?.attributes?.enrollmentVerifications?.filter(
+          //         ev =>
+          //           ev.certifiedEndDate <
+          //           new Date().toISOString().split('T')[0],
+          //       ),
+          //     }
+          //   : {
+          //       ...testEv,
+          //       enrollmentVerifications: testEv?.enrollmentVerifications?.filter(
+          //         ev =>
+          //           ev.certifiedEndDate <
+          //           new Date().toISOString().split('T')[0],
+          //       ),
           //     },
-          //     {
-          //       verificationMonth: 'September 2021',
-          //       certifiedBeginDate: '2021-09-01',
-          //       certifiedEndDate: '2021-09-30',
-          //       certifiedThroughDate: null,
-          //       certificationMethod: null,
-          //       enrollments: [
-          //         {
-          //           facilityName: 'UNIVERSITY OF HAWAII AT HILO',
-          //           beginDate: '2021-08-01',
-          //           endDate: '2021-10-01',
-          //           totalCreditHours: 22.0,
-          //         },
-          //       ],
-          //       verificationResponse: 'NR',
-          //       createdDate: null,
-          //     },
-          //     {
-          //       verificationMonth: 'August 2021',
-          //       certifiedBeginDate: '2021-08-01',
-          //       certifiedEndDate: '2021-08-31',
-          //       certifiedThroughDate: null,
-          //       certificationMethod: null,
-          //       enrollments: [
-          //         {
-          //           facilityName: 'UNIVERSITY OF HAWAII AT HILO',
-          //           beginDate: '2021-08-01',
-          //           endDate: '2021-10-01',
-          //           totalCreditHours: 22.0,
-          //         },
-          //       ],
-          //       verificationResponse: 'NR',
-          //       createdDate: null,
-          //     },
-          //   ],
-          // },
         };
       case UPDATE_VERIFICATION_STATUS_MONTHS:
         return {
