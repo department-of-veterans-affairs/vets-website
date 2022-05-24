368126c2aefc0f3bfafa7b62f48f396d53aabf90
Mark Dewey <mdewey@governmentcio.com>
2022-05-24 14:44:51 -0400

    (feature) added the alerts

---
+++ src/applications/personalization/profile/components/alerts/bad-address/index.jsx
@@ -0,0 +1,5 @@
+import full from './full';
+import secondary from './secondary';
+import small from './small';
+
+export { full, secondary, small };
---
+++ src/applications/personalization/profile/components/alerts/bad-address/small.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function small() {
+  return (
+    <va-alert status="warning" background-only show-icon>
+      <h2 slot="headline">Review your mailing address</h2>
+      <p>Review your Address</p>
+    </va-alert>
+  );
+}
