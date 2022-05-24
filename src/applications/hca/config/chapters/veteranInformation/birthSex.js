699605ce1d374a5f022edb386f32bd102771aefe
Lisa Zapson <lisazapson@ymail.com>
2022-05-24 10:29:12 -0400

    41333 temporarily hide info component

--- src/applications/hca/config/chapters/veteranInformation/birthSex.js
+++ src/applications/hca/config/chapters/veteranInformation/birthSex.js
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { genderLabels } from 'platform/static-data/labels';
 import fullSchemaHca from 'vets-json-schema/dist/10-10EZ-schema.json';
 
@@ -14,34 +14,34 @@ import {
 
 const { gender } = fullSchemaHca.properties;
 
-const BirthSexDescription = () => {
-  return (
-    <div className="vads-u-margin-y--2">
-      <va-additional-info trigger="Why we ask for this information">
-        <p>
-          Population data shows that a person’s sex can affect things like their
-          health risks and the way their body responds to medications. Knowing
-          your sex assigned at birth, along with other factors, helps your
-          health care team use data to:
-        </p>
+// const BirthSexDescription = () => {
+//   return (
+//     <div className="vads-u-margin-y--2">
+//       <va-additional-info trigger="Why we ask for this information">
+//         <p>
+//           Population data shows that a person’s sex can affect things like their
+//           health risks and the way their body responds to medications. Knowing
+//           your sex assigned at birth, along with other factors, helps your
+//           health care team use data to:
+//         </p>
 
-        <ul>
-          <li>Interpret your lab results</li>
+//         <ul>
+//           <li>Interpret your lab results</li>
 
-          <li>Prescribe the right dose of medications</li>
+//           <li>Prescribe the right dose of medications</li>
 
-          <li>Recommend health prevention screenings</li>
-        </ul>
+//           <li>Recommend health prevention screenings</li>
+//         </ul>
 
-        <p>
-          We also collect this information to better understand our Veteran
-          community. This helps us make sure that we’re serving the needs of all
-          Veterans.
-        </p>
-      </va-additional-info>
-    </div>
-  );
-};
+//         <p>
+//           We also collect this information to better understand our Veteran
+//           community. This helps us make sure that we’re serving the needs of all
+//           Veterans.
+//         </p>
+//       </va-additional-info>
+//     </div>
+//   );
+// };
 
 export default {
   uiSchema: {
@@ -62,7 +62,7 @@ export default {
     gender: {
       'ui:title': 'What sex were you assigned at birth?',
       'ui:reviewField': CustomReviewField,
-      'ui:description': BirthSexDescription,
+      // 'ui:description': BirthSexDescription,
       'ui:widget': 'radio',
       'ui:options': {
         labels: genderLabels,
