aa25c99f0b4393820337c74ccf0f1ff5c3c7137d
Robin Garrison <robin.garrison@adhocteam.us>
2022-05-24 09:25:35 -0500

    Update NOD keyboard-only tests

--- src/applications/appeals/10182/tests/10182-keyboard-only.cypress.spec.js
+++ src/applications/appeals/10182/tests/10182-keyboard-only.cypress.spec.js
@@ -1,84 +1,117 @@
+import path from 'path';
+
 import formConfig from '../config/form';
 import { fixDecisionDates } from './nod.cypress.helpers';
 import mockFeatureToggles from './fixtures/mocks/feature-toggles.json';
 import mockInProgress from './fixtures/mocks/in-progress-forms.json';
 import mockSubmit from './fixtures/mocks/application-submit.json';
 import mockUser from './fixtures/mocks/user.json';
-import mockData from './fixtures/data/maximal-test.json';
-
-// Modified from Cypress docs
-// https://glebbahmutov.com/cypress-examples/6.5.0/recipes/form-input-by-label.html#reusable-function
-Cypress.Commands.add('tabToInputWithLabel', text => {
-  cy.contains('label', text)
-    .invoke('attr', 'for')
-    .then(id => {
-      cy.tabToElement(`#${id}`);
-    });
-});
 
 describe.skip('Notice of Disagreement keyboard only navigation', () => {
-  it('navigates through a maximal form', () => {
+  before(() => {
+    cy.fixture(path.join(__dirname, 'fixtures/data/minimal-test.json')).as(
+      'testData',
+    );
     cy.intercept('GET', '/v0/feature_toggles?*', mockFeatureToggles);
     cy.intercept('PUT', 'v0/in_progress_forms/10182', mockInProgress);
     cy.intercept('POST', formConfig.submitUrl, mockSubmit);
-    cy.intercept('GET', 'v0/notice_of_disagreements/contestable_issues', {
-      data: fixDecisionDates(mockData.data.contestableIssues),
-    });
-
     cy.login(mockUser);
-    cy.visit('/decision-reviews/board-appeal/request-board-appeal-form-10182');
-    cy.injectAxeThenAxeCheck();
+  });
 
-    // Intro page
-    cy.tabToStartForm();
+  it('navigates through a maximal form', () => {
+    cy.get('@testData').then(({ data }) => {
+      const { chapters } = formConfig;
+      cy.intercept('GET', 'v0/notice_of_disagreements/contestable_issues', {
+        data: fixDecisionDates(data.contestableIssues),
+      });
+      cy.visit(
+        '/decision-reviews/board-appeal/request-board-appeal-form-10182',
+      );
+      cy.injectAxeThenAxeCheck();
 
-    // Veteran details
-    cy.tabToContinueForm();
+      // Intro page
+      cy.tabToStartForm();
 
-    // Homelessness radios
-    cy.tabToElement('[name="root_homeless"]');
-    cy.chooseRadio('N');
-    cy.tabToContinueForm();
+      // Veteran details
+      cy.url().should(
+        'include',
+        chapters.infoPages.pages.veteranInformation.path,
+      );
+      cy.tabToContinueForm();
 
-    // Contact info
-    cy.tabToContinueForm();
+      // Homelessness radios
+      cy.url().should('include', chapters.infoPages.pages.homeless.path);
+      cy.tabToElement('[name="root_homeless"]');
+      cy.chooseRadio('N');
+      cy.tabToContinueForm();
 
-    // Filing deadlines
-    cy.tabToContinueForm();
+      // Contact info
+      cy.url().should(
+        'include',
+        chapters.infoPages.pages.confirmContactInformation.path,
+      );
+      cy.tabToContinueForm();
 
-    // Issues for review (sorted by random decision date) - only selecting one,
-    // or more complex code is needed to find if the next checkbox is before or
-    // after the first
-    cy.tabToInputWithLabel('tinnitus');
-    cy.realPress('Space');
-    cy.tabToContinueForm();
+      // Filing deadlines
+      cy.url().should(
+        'include',
+        chapters.conditions.pages.filingDeadlines.path,
+      );
+      cy.tabToContinueForm();
 
-    // area of disagreement for tinnitus
-    cy.tabToInputWithLabel('service connection');
-    cy.realPress('Space');
-    cy.tabToElement('#root_otherEntry');
-    cy.typeInFocused('Few words');
-    cy.tabToContinueForm();
+      // Issues for review (sorted by random decision date) - only selecting one,
+      // or more complex code is needed to find if the next checkbox is before or
+      // after the first
+      cy.url().should(
+        'include',
+        chapters.conditions.pages.contestableIssues.path,
+      );
+      cy.tabToInputWithLabel('tinnitus');
+      cy.realPress('Space');
+      cy.tabToContinueForm();
 
-    // Issue summary
-    cy.tabToContinueForm();
+      // area of disagreement for tinnitus
+      cy.url().should(
+        'include',
+        chapters.conditions.pages.areaOfDisagreementFollowUp.path.replace(
+          ':index',
+          '',
+        ),
+      );
+      cy.tabToInputWithLabel('service connection');
+      cy.realPress('Space');
+      cy.tabToElement('#root_otherEntry');
+      cy.typeInFocused('Few words');
+      cy.tabToContinueForm();
 
-    // Board review option
-    cy.tabToElement('[name="root_boardReviewOption"]');
-    cy.chooseRadio('hearing');
-    cy.tabToContinueForm();
+      // Issue summary
+      cy.url().should('include', chapters.conditions.pages.issueSummary.path);
+      cy.tabToContinueForm();
 
-    // Hearing type
-    cy.tabToElement('[name="root_hearingTypePreference"]');
-    cy.chooseRadio('video_conference');
-    cy.tabToContinueForm();
+      // Board review option
+      cy.url().should(
+        'include',
+        chapters.boardReview.pages.boardReviewOption.path,
+      );
+      cy.tabToElement('[name="root_boardReviewOption"]');
+      cy.chooseRadio('hearing');
+      cy.tabToContinueForm();
 
-    // Review & submit page
-    cy.tabToElement('[name="privacyAgreementAccepted"]');
-    cy.realPress('Space');
-    cy.tabToSubmitForm();
+      // Hearing type
+      cy.url().should('include', chapters.boardReview.pages.hearingType.path);
+      cy.tabToElement('[name="root_hearingTypePreference"]');
+      cy.chooseRadio('video_conference');
+      cy.tabToContinueForm();
 
-    // Confirmation page print button
-    cy.get('button.screen-only').should('exist');
+      // Review & submit page
+      cy.url().should('include', 'review-and-submit');
+      cy.tabToElement('[name="privacyAgreementAccepted"]');
+      cy.realPress('Space');
+      cy.tabToSubmitForm();
+
+      // Check confirmation page print button
+      cy.url().should('include', 'confirmation');
+      cy.get('button.screen-only').should('exist');
+    });
   });
 });
