8c813add6635f4e7c2004eece297e04f01e5b2ec
Rebecca Green <54145406+reblg@users.noreply.github.com>
2022-05-24 18:05:26 -0400

    DHP - Disconnect a device (#21141)
    * [#228] Adds disconnect modal to connected devices section
    
    Co-authored-by: Michael Chaffee <michael.chaffee@thoughtworks.com>
    Co-authored-by: Rebecca Green <rebecca.green@thoughtworks.com>
    
    * [DHP-228] Moves disconnect modal logic to get triggered from DeviceDisconnectionCard component. Moves DisconnectionModal to its own file
    
    * [DHP-228] Adds functionality for closeEvent and secondaryButtonClick on DisconnectModal
    
    Co-authored-by: Michael Chaffee <michael.chaffee@thoughtworks.com>
    Co-authored-by: Rebecca Green <rebecca.green@thoughtworks.com>
    
    * [DHP-228] Adds href update in DeviceDisconnectionCard to begin disconnect sequence in backend
    
    Co-authored-by: Michael Chaffee <michael.chaffee@thoughtworks.com>
    Co-authored-by: Rebecca Green <rebecca.green@thoughtworks.com>
    
    * [DHP-228] changes assign to href update in DeviceDisconnectionCard
    
    Co-authored-by: Michael Chaffee <michael.chaffee@thoughtworks.com>
    Co-authored-by: Rebecca Green <rebecca.green@thoughtworks.com>
    
    * [DHP-228] Adds generic device connection alert
    
    * [DHP-228] Adds disconnection success and failure alerts
    
    * [DHP-228] Refactors alerts to use constants and fixes flaky tests
    
    * [DHP-228] Removes DeviceConnectionFailed and DeviceConnectionSuccess alerts and uses DeviceConnectionAlert
    
    * [DHP-228] Removes unused function uses closeModal instead of state variable in DeviceDisconnectionCard
    
    Co-authored-by: Michael Chaffee <michael.chaffee@thoughtworks.com>

---
+++ src/applications/dhp-connected-devices/constants/alerts.js
@@ -0,0 +1,4 @@
+export const CONNECTION_SUCCESSFUL_STATUS = 'success';
+export const CONNECTION_FAILED_STATUS = 'error';
+export const DISCONNECTION_SUCCESSFUL_STATUS = 'disconnect-success';
+export const DISCONNECTION_FAILED_STATUS = 'disconnect-error';
