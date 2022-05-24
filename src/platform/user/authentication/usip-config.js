db27c0e6d9331568e38b995f4be7c35bd58ad915
Alexander Garcia <agarcia@clarityinnovates.com>
2022-05-24 14:48:03 -0400

    separates configs for prod, dev, staging, etc.

--- src/platform/user/authentication/usip-config.js
+++ src/platform/user/authentication/usip-config.js
@@ -1,91 +1,13 @@
-import { EXTERNAL_APPS, EXTERNAL_REDIRECTS } from './constants';
+import ENVIRONMENTS from 'site/constants/environments';
+import environment from 'platform/utilities/environment';
 
-const defaultSignInProviders = {
-  logingov: true,
-  idme: true,
-  mhv: true,
-  dslogon: true,
-};
-
-const defaultSignUpProviders = {
-  logingov: true,
-  idme: true,
-};
-
-const defaultMobileQueryParams = {
-  allowOAuth: true,
-  allowPostLogin: false,
-  allowRedirect: false,
-};
+import DEV from './config/dev.config';
+import STAGING from './config/staging.config';
+import PROD from './config/prod.config';
 
 export const externalApplicationsConfig = {
-  default: {
-    allowedSignInProviders: { ...defaultSignInProviders },
-    allowedSignUpProviders: { ...defaultSignUpProviders },
-    isMobile: false,
-    queryParams: {
-      allowOAuth: false,
-      allowPostLogin: true,
-      allowRedirect: false,
-    },
-    OAuthEnabled: false,
-    requiresVerification: false,
-  },
-  [EXTERNAL_APPS.MHV]: {
-    allowedSignInProviders: { ...defaultSignInProviders },
-    allowedSignUpProviders: { ...defaultSignUpProviders },
-    isMobile: false,
-    queryParams: {
-      allowOAuth: false,
-      allowPostLogin: true,
-      allowRedirect: true,
-    },
-    OAuthEnabled: false,
-    requiresVerification: false,
-    externalRedirectUrl: EXTERNAL_REDIRECTS[EXTERNAL_APPS.MHV],
-  },
-  [EXTERNAL_APPS.MY_VA_HEALTH]: {
-    allowedSignInProviders: { ...defaultSignInProviders },
-    allowedSignUpProviders: { ...defaultSignUpProviders },
-    isMobile: false,
-    queryParams: {
-      allowOAuth: false,
-      allowPostLogin: false,
-      allowRedirect: false,
-    },
-    OAuthEnabled: false,
-    requiresVerification: false,
-    externalRedirectUrl: EXTERNAL_REDIRECTS[EXTERNAL_APPS.MY_VA_HEALTH],
-  },
-  [EXTERNAL_APPS.EBENEFITS]: {
-    allowedSignInProviders: { ...defaultSignInProviders },
-    allowedSignUpProviders: { ...defaultSignUpProviders },
-    isMobile: false,
-    queryParams: {
-      allowOAuth: false,
-      allowPostLogin: false,
-      allowRedirect: false,
-    },
-    OAuthEnabled: false,
-    requiresVerification: false,
-    externalRedirectUrl: EXTERNAL_REDIRECTS[EXTERNAL_APPS.EBENEFITS],
-  },
-  [EXTERNAL_APPS.VA_FLAGSHIP_MOBILE]: {
-    allowedSignInProviders: { ...defaultSignInProviders },
-    allowedSignUpProviders: { ...defaultSignUpProviders },
-    isMobile: true,
-    queryParams: { ...defaultMobileQueryParams },
-    OAuthEnabled: true,
-    requiresVerification: true,
-    externalRedirectUrl: EXTERNAL_REDIRECTS[EXTERNAL_APPS.VA_FLAGSHIP_MOBILE],
-  },
-  [EXTERNAL_APPS.VA_OCC_MOBILE]: {
-    allowedSignInProviders: { ...defaultSignInProviders },
-    allowedSignUpProviders: { ...defaultSignUpProviders },
-    isMobile: true,
-    queryParams: { ...defaultMobileQueryParams },
-    OAuthEnabled: true,
-    requiresVerification: true,
-    externalRedirectUrl: EXTERNAL_REDIRECTS[EXTERNAL_APPS.VA_OCC_MOBILE],
-  },
-};
+  [ENVIRONMENTS.VAGOVPROD]: PROD,
+  [ENVIRONMENTS.VAGOVSTAGING]: STAGING,
+  [ENVIRONMENTS.VAGOVDEV]: DEV,
+  [ENVIRONMENTS.LOCALHOST]: DEV,
+}[environment.BUILDTYPE];
