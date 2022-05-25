3f46bb95bac2c25920193b71fbe6a65fbd45c8c6
Mark Dewey <mdewey@users.noreply.github.com>
2022-05-25 10:47:16 -0400

    Profile/feature/39342/bad actor indicator - mocks, alerts and toggle (#21168)
    * (feature) added feature toggles
    
    * (mocks) added mocks for bad address response
    
    * (feature) added the alerts
    
    * (refactor) better names
    
    * (feature) fixed copy

--- src/applications/personalization/profile/mocks/server.js
+++ src/applications/personalization/profile/mocks/server.js
@@ -21,7 +21,7 @@ const serviceHistory = require('./service-history');
 
 /* eslint-disable camelcase */
 const responses = {
-  'GET /v0/user': user.user72Success,
+  'GET /v0/user': user.badAddress,
   'GET /v0/profile/status': status,
   'OPTIONS /v0/maintenance_windows': 'OK',
   'GET /v0/maintenance_windows': { data: [] },
