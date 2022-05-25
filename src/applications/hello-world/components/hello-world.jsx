686155041f5271caec2b46e64c9d2d14f8b5f30e
Keya Sengupta <keya.sengupta@thoughtworks.com>
2022-05-25 07:00:10 -0400

    Converted class to function

--- src/applications/hello-world/components/hello-world.jsx
+++ src/applications/hello-world/components/hello-world.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-export class HelloWorld extends React.Component {
-  async componentDidMount() {}
-
-  render() {
-    return (
-      <div>
-        <div className="message">Hello World</div>
-      </div>
-    );
-  }
+export function HelloWorld() {
+  return (
+    <div>
+      <div className="message">Hello World</div>
+      <></>
+    </div>
+  );
 }
