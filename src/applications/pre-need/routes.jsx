dd0874112c3fb767d3beda2e102fc17293b5c346
BJ McCotter <bj.mccotter@adhocteam.us>
2022-05-25 10:11:14 -0500

    removing landing pages after moving RoutedSavable

--- src/applications/pre-need/routes.jsx
+++ src/applications/pre-need/routes.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-
+import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import { createRoutesWithSaveInProgress } from 'platform/forms/save-in-progress/helpers';
-
 import formConfig from './config/form';
-import PreNeedApp from './PreNeedApp';
 
 const routeObjects = createRoutesWithSaveInProgress(formConfig);
 
 const route = (
-  <Switch>
-    <Route exact path="/">
-      <PreNeedApp>
+  <RoutedSavableApp formConfig={formConfig} currentLocation={window.location}>
+    <Switch>
+      <Route exact path="/">
         <Redirect to="/introduction" />
-      </PreNeedApp>
-    </Route>
-    {routeObjects}
-  </Switch>
+      </Route>
+      {routeObjects}
+    </Switch>
+  </RoutedSavableApp>
 );
 
 export default route;
