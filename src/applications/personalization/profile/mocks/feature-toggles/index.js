f806d93d44a6f257562a21b8bfa5feac6a260dfb
Mark Dewey <mdewey@governmentcio.com>
2022-05-25 12:40:55 -0400

    (tests) added e2e tests to verify toggle logic

--- src/applications/personalization/profile/mocks/feature-toggles/index.js
+++ src/applications/personalization/profile/mocks/feature-toggles/index.js
@@ -11,7 +11,7 @@ const generateFeatureToggles = (toggles = {}) => {
     profileShowProfile2 = false,
     profileShowPronounsAndSexualOrientation = false,
     profileShowReceiveTextNotifications = true,
-    profileAlwaysShowDirectDepositDisplay = true,
+    profileAlwaysShowDirectDepositDisplay = false,
   } = toggles;
 
   return {
