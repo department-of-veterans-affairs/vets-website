86b0302071a9378a7a6bc74a839c55b2c91224ce
BJ McCotter <bjmccotter7192@gmail.com>
2022-05-24 20:57:45 -0500

    moving RoutedSavableApp closer to router

--- src/applications/ask-a-question/App.jsx
+++ src/applications/ask-a-question/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { createRoutesWithSaveInProgress } from 'platform/forms/save-in-progress/helpers';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from './form/form';
+
+const routeObjects = createRoutesWithSaveInProgress(formConfig);
 
 export default function App({ children }) {
   const location = useLocation();
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
+      {routeObjects}
     </RoutedSavableApp>
   );
 }
--- src/applications/ask-a-question/routes.jsx
+++ src/applications/ask-a-question/routes.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import { createRoutesWithSaveInProgress } from 'platform/forms/save-in-progress/helpers';
 import formConfig from './form/form';
-import App from './App';
 
 const routeObjects = createRoutesWithSaveInProgress(formConfig);
 
 const route = (
-  <Switch>
-    <Route exact path="/">
-      <App>
+  <RoutedSavableApp formConfig={formConfig} currentLocation={window.location}>
+    <Switch>
+      <Route exact path="/">
         <Redirect to="/introduction" />
-      </App>
-    </Route>
-    {routeObjects}
-  </Switch>
+      </Route>
+      {routeObjects}
+    </Switch>
+  </RoutedSavableApp>
 );
 
 export default route;
