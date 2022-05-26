fdd615d422ea541c167a90162f892e74569b28f7
Brandyn Sullins <brandyn.sullins@gmail.com>
2022-05-26 16:30:03 -0400

    39378 - [CDP] Debt 2.0 - Update Card Components to fit Requirements (#21198)
    * Halfway through cards, some refactor of main component as well to deal with behavioral display logic
    
    * Finish initial pass through, now validating & testing
    
    * Fix date sorting to grab most recent for due by as well as extra line break for formatting
    
    * Remove unused testid for now
    
    * Use va-telephone and remove custom css

--- src/applications/combined-debt-portal/debt-letters/components/DebtDetailsCard.jsx
+++ src/applications/combined-debt-portal/debt-letters/components/DebtDetailsCard.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import head from 'lodash/head';
+import { head } from 'lodash';
 import PropTypes from 'prop-types';
 // import { deductionCodes } from '../const/deduction-codes';
 // import { setActiveDebt } from '../../combined/actions/debts';
+import { format } from 'date-fns';
 import { getDebtDetailsCardContent } from '../const/diary-codes/debtDetailsCardContent';
 import { currency } from '../utils/page';
 
 const DebtLetterCard = ({ debt }) => {
   // TODO: currently we do not have a debtID so we need to make one by combining fileNumber and diaryCode
-  const mostRecentHistory = head(debt?.debtHistory);
-
+  const dates = debt?.debtHistory.map(m => new Date(m.date));
+  const sortedHistory = dates.sort((a, b) => Date.parse(b) - Date.parse(a));
+  const mostRecentDate = format(head(sortedHistory), 'MM/dd/yyyy');
   const convertedAr = currency.format(parseFloat(debt.currentAr));
 
   const debtCardContent = getDebtDetailsCardContent(
     debt,
-    mostRecentHistory?.date,
+    mostRecentDate,
     convertedAr,
   );
 
@@ -24,45 +26,50 @@ const DebtLetterCard = ({ debt }) => {
       class="vads-u-margin-bottom--1"
       disable-analytics="false"
       full-width="false"
+      show-icon={debtCardContent.showIcon}
       status={debtCardContent.status}
       visible="true"
     >
       <h3 className="vads-u-margin--0">{debtCardContent.headerText}</h3>
 
       <div>
-        <p className="vads-u-margin-y--2 vads-u-font-size--md vads-u-font-family--sans">
+        <div className="vads-u-margin-y--2 vads-u-font-size--md vads-u-font-family--sans">
           {debtCardContent.bodyText}
-        </p>
+        </div>
       </div>
 
       {debtCardContent.showLinks && (
         <div className="vads-u-margin-y--2">
-          <div>
-            <a
-              className="vads-u-font-size--md vads-u-font-weight--bold"
-              aria-label="Make a payment"
-              href="https://www.pay.va.gov/"
-            >
-              <i
-                aria-hidden="true"
-                className="fas fa-chevron-circle-right fa-2x vads-u-margin-right--1"
-              />
-              Make a payment
-            </a>
-          </div>
-          <div>
-            <a
-              className="vads-u-font-size--md vads-u-font-weight--bold"
-              aria-label="Request help with your debt"
-              href="/manage-va-debt/request-debt-help-form-5655"
-            >
-              <i
-                className="fas fa-chevron-circle-right fa-2x vads-u-margin-right--1"
-                aria-hidden="true"
-              />
-              Request help with your debt
-            </a>
-          </div>
+          {debtCardContent.showMakePayment && (
+            <div>
+              <a
+                className="vads-u-font-size--md vads-u-font-weight--bold"
+                aria-label="Make a payment"
+                href="https://www.pay.va.gov/"
+              >
+                <i
+                  aria-hidden="true"
+                  className="fas fa-chevron-circle-right fa-2x vads-u-margin-right--1"
+                />
+                Make a payment
+              </a>
+            </div>
+          )}
+          {debtCardContent.showRequestHelp && (
+            <div>
+              <a
+                className="vads-u-font-size--md vads-u-font-weight--bold"
+                aria-label="Request help with your debt"
+                href="/manage-va-debt/request-debt-help-form-5655"
+              >
+                <i
+                  className="fas fa-chevron-circle-right fa-2x vads-u-margin-right--1"
+                  aria-hidden="true"
+                />
+                Request help with your debt
+              </a>
+            </div>
+          )}
         </div>
       )}
     </va-alert>
