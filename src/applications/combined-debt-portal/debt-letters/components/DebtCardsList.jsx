6270916dc7ed3b7b0ff1f255f57fb470a7aa5546
Andrew Rodiek <andrew.rodiek@gmail.com>
2022-05-27 10:35:10 -0400

    Updated Debt Summary information, other minor fixes, include test helper

--- src/applications/combined-debt-portal/debt-letters/components/DebtCardsList.jsx
+++ src/applications/combined-debt-portal/debt-letters/components/DebtCardsList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { PATTERNS } from '@department-of-veterans-affairs/component-library/Telephone';
 import DebtSummaryCard from './DebtSummaryCard';
 import { ErrorMessage, DowntimeMessage } from './Alerts';
 // TODO: OtherVA Update
