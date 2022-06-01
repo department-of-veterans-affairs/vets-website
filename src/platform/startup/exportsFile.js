8caddb3e6684a93d5e58539c77c4d0efde742cc7
Ray Messina <rmessina10@gmail.com>
2022-06-01 00:45:10 -0500

    add exportsFile.js; all exports listed o exports file;  registers all exports;

---
+++ src/platform/startup/exportsFile.js
@@ -0,0 +1,11 @@
+// duplicated vat name, startApp, in platform, adjusted
+export { default as startAppRouter } from './index'; // used outside platform
+export { default as createCommonStore } from './store'; // used outside platform
+
+// duplicated vat name, startApp, in platform, adjusted
+export { default as startAppRouterDom } from './router'; // used outside platform
+
+// tentative
+export { default as createAnalyticsMiddleware } from './analytics-middleware';
+export { default as startReactApp } from './react';
+export { default as setUpCommonFunctionality } from './setup';
