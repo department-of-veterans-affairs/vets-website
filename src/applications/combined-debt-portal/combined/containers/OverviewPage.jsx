8ea33f05b26e83b16f6093719eb79ab9c02f500f
Andrew Rodiek <andrew.rodiek@gmail.com>
2022-06-01 13:16:22 -0400

    Fixed Alerts and breadcrumbs to match mocks on CDP Overview page (#21228)

--- src/applications/combined-debt-portal/combined/containers/OverviewPage.jsx
+++ src/applications/combined-debt-portal/combined/containers/OverviewPage.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect } from 'react';
 import scrollToTop from 'platform/utilities/ui/scrollToTop';
+import { useSelector } from 'react-redux';
 import Balances from '../components/Balances';
+import ComboAlerts from '../components/ComboAlerts';
+import { ALERT_TYPES } from '../utils/helpers';
+import {
+  calculateTotalDebts,
+  calculateTotalBills,
+} from '../utils/balance-helpers';
 
 const OverviewPage = () => {
   const title = 'Your VA debt and bill';
@@ -9,11 +16,28 @@ const OverviewPage = () => {
     scrollToTop();
   }, []);
 
+  const { debtLetters, mcp } = useSelector(
+    ({ combinedPortal }) => combinedPortal,
+  );
+
+  // Get errors
+  const billError = mcp.error;
+  const debtError = debtLetters.errors?.length > 0;
+  const bothError = billError && debtError;
+
+  // get totals
+  const { debts } = debtLetters;
+  const totalDebts = calculateTotalDebts(debts);
+  const bills = mcp.statements;
+  const totalBills = calculateTotalBills(bills);
+  const bothZero = totalDebts === 0 && totalBills === 0;
+
   return (
     <>
       <va-breadcrumbs className="vads-u-font-family--sans no-wrap">
         <a href="/">Home</a>
-        <a href="/debt-and-bills">Your VA debt and bills</a>
+        <a href="/manage-debt-and-bills">Review and manage VA debt and bills</a>
+        <a href="/manage-debt-and-bills/summary">Your VA debt and bills</a>
       </va-breadcrumbs>
       <h1 data-testid="overview-page-title">{title}</h1>
       <p className="vads-u-font-size--lg vads-u-font-family--serif">
@@ -22,23 +46,31 @@ const OverviewPage = () => {
         charges from VA health care facilities. Find out how to make payments or
         request financial help.
       </p>
-      <h2>Debt and bill overview</h2>
-      <Balances />
-      <h2>What to do if you have questions about your debt and bills</h2>
-      <h3>Questions about benefit debt</h3>
-      <p>
-        Call the Debt Management Center (DMC) at{' '}
-        <va-telephone contact="800-827-0648" /> (TTY:{' '}
-        <va-telephone contact="711" />
-        ). We’re here Monday through Friday, 7:30 a.m. to 7:00 p.m. ET.
-      </p>
-      <h3>Questions about medical copayment bills</h3>
-      <p>
-        Call the VA Health Resource Center at{' '}
-        <va-telephone contact="866-400-1238" /> (TTY:{' '}
-        <va-telephone contact="711" />
-        ). We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
-      </p>
+      {bothError || bothZero ? (
+        <ComboAlerts
+          alertType={bothError ? ALERT_TYPES.ERROR : ALERT_TYPES.ZERO}
+        />
+      ) : (
+        <>
+          <h2>Debt and bill overview</h2>
+          <Balances />
+          <h2>What to do if you have questions about your debt and bills</h2>
+          <h3>Questions about benefit debt</h3>
+          <p>
+            Call the Debt Management Center (DMC) at{' '}
+            <va-telephone contact="800-827-0648" /> (TTY:{' '}
+            <va-telephone contact="711" />
+            ). We’re here Monday through Friday, 7:30 a.m. to 7:00 p.m. ET.
+          </p>
+          <h3>Questions about medical copayment bills</h3>
+          <p>
+            Call the VA Health Resource Center at{' '}
+            <va-telephone contact="866-400-1238" /> (TTY:{' '}
+            <va-telephone contact="711" />
+            ). We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
+          </p>
+        </>
+      )}
     </>
   );
 };
