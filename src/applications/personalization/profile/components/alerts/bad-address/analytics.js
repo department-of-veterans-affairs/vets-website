4c67cd29f91e0336c49cbd009fe16d519775c146
Mark Dewey <mdewey@governmentcio.com>
2022-06-02 09:53:13 -0400

    (feature) refined analytics slug

--- src/applications/personalization/profile/components/alerts/bad-address/analytics.js
+++ src/applications/personalization/profile/components/alerts/bad-address/analytics.js
@@ -2,7 +2,7 @@ import recordEvent from 'platform/monitoring/record-event';
 
 const recordBadAddressEvent = ({ location = 'unknown' }) => {
   recordEvent({
-    event: `bad_address_alert_shown_${location}`,
+    event: `profile-bad-address-${location}-alert-shown-`,
   });
 };
 
