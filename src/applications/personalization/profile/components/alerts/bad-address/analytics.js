7d6b92f01ce79e2afdf59173ecaae0b7e4f58f24
Mark Dewey <mdewey@governmentcio.com>
2022-06-02 11:20:11 -0400

    (chore) removed trailing character

--- src/applications/personalization/profile/components/alerts/bad-address/analytics.js
+++ src/applications/personalization/profile/components/alerts/bad-address/analytics.js
@@ -2,7 +2,7 @@ import recordEvent from 'platform/monitoring/record-event';
 
 const recordBadAddressEvent = ({ location = 'unknown' }) => {
   recordEvent({
-    event: `profile-bad-address-${location}-alert-shown-`,
+    event: `profile-bad-address-${location}-alert-shown`,
   });
 };
 
