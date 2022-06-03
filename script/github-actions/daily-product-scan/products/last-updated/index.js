70a655daa7618350a5848b4167feda03c3e7ce51
Holden Hinkle <holden@holdenhinkle.com>
2022-06-02 22:50:36 -0400

    set last updated for each product

--- script/github-actions/daily-product-scan/products/last-updated/index.js
+++ script/github-actions/daily-product-scan/products/last-updated/index.js
@@ -12,15 +12,16 @@ class LastUpdated {
   }
 
   setLastUpdated() {
-    Object.keys(this.products).forEach(async (productId, i) => {
-      if (i === 0) {
-        const product = this.products[productId];
-        const { pathToCode } = product;
-        product.lastUpdated = await this.getLastDateUpdated({
-          path: pathToCode,
-        });
-        // console.log(product.lastUpdated);
-      }
+    Object.keys(this.products).forEach(async productId => {
+      const product = this.products[productId];
+      const { pathToCode } = product;
+      product.lastUpdated = await this.getLastDateUpdated({
+        path: pathToCode,
+      });
+
+      // console.log(productId);
+      // console.log(product.lastUpdated);
+      // console.log('');
     });
   }
 
@@ -53,140 +54,11 @@ class LastUpdated {
         }
       }
     }
+    // console.log('page', page);
+    // console.log('commitDates.length', commitDates.length);
 
-    // console.log('commitDates', commitDates);
-    // console.log('number of commits', commitDates.length);
-    // return this.getMostRecentDate({ dates: commitDates });
+    return this.getMostRecentDate({ dates: commitDates });
   }
-
-  // getCommits({ path, page }) {
-  //   this.octokit
-  //     .request('GET /repos/{owner}/{repo}/commits', {
-  //       owner: constants.owner,
-  //       repo: constants.repo,
-  //       // eslint-disable-next-line camelcase
-  //       per_page: constants.perPage,
-  //       path,
-  //       page,
-  //     })
-  //     .then(response => {
-  //       console.log("I'M IN RESPONSE");
-  //       return response;
-  //     })
-  //     .catch(_ => {
-  //       console.log("I'M IN CATCH");
-  //       return { status: 400 };
-  //     });
-  // }
-
-  // async getLastDateUpdated({ path }) {
-  //   const commitDates = [];
-  //   let moreResultsExist = true;
-  //   let page = 1;
-
-  //   while (moreResultsExist) {
-  //     const { status, headers, data } = this.getCommits({ path, page });
-
-  //     if (status === 200) {
-  //       data.forEach(d => commitDates.push(d.commit.committer.date));
-
-  //       if (headers.link) {
-  //         page += 1;
-  //       } else {
-  //         moreResultsExist = false;
-  //       }
-  //     }
-  //   }
-
-  //   console.log('commitDates', commitDates);
-  //   console.log('number of commits', commitDates.length);
-  //   return this.getMostRecentDate({ dates: commitDates });
-  // }
-
-  // getCommits({ path, page }) {
-  //   this.octokit
-  //     .request('GET /repos/{owner}/{repo}/commits', {
-  //       owner: constants.owner,
-  //       repo: constants.repo,
-  //       // eslint-disable-next-line camelcase
-  //       per_page: constants.perPage,
-  //       path,
-  //       page,
-  //     })
-  //     .then(response => {
-  //       console.log("I'M IN RESPONSE");
-  //       return response;
-  //     })
-  //     .catch(_ => {
-  //       console.log("I'M IN CATCH");
-  //       return { status: 400 };
-  //     });
-  // }
-
-  // blarg
-
-  // getCommits({ path, page }) {
-  //   const request = new Promise((res, rej) => {
-  //     async function get() {
-  //       try {
-  //         const response = await this.octokit.request(
-  //           'GET /repos/{owner}/{repo}/commits',
-  //           {
-  //             owner: constants.owner,
-  //             repo: constants.repo,
-  //             // eslint-disable-next-line camelcase
-  //             per_page: constants.perPage,
-  //             path,
-  //             page,
-  //           },
-  //         );
-
-  //         res(response);
-  //       } catch {
-  //         rej();
-  //       }
-  //     }
-
-  //     get();
-  //   });
-
-  //   request
-  //     .then(res => {
-  //       return res;
-  //     })
-  //     .catch(_ => {
-  //       return { status: 400 };
-  //     });
-  // }
-
-  // async getCommits({ path, page }) {
-  //   const request = new Promise((res, rej) => {
-  //     try {
-  //       const response = this.octokit.request(
-  //         'GET /repos/{owner}/{repo}/commits',
-  //         {
-  //           owner: constants.owner,
-  //           repo: constants.repo,
-  //           // eslint-disable-next-line camelcase
-  //           per_page: constants.perPage,
-  //           path,
-  //           page,
-  //         },
-  //       );
-  //       res(response);
-  //     } catch (_) {
-  //       rej();
-  //     }
-  //   });
-
-  //   request
-  //     .then(res => {
-  //       return res;
-  //     })
-  //     .catch(_ => {
-  //       return { status: 400 };
-  //     });
-  // }
 
   getMostRecentDate({ dates }) {
     return dates
