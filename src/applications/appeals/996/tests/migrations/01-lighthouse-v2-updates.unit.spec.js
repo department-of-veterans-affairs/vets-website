d0bc09985b7d7aa688b5771834acb39a02acd3d3
Robin Garrison <robin.garrison@adhocteam.us>
2022-06-08 10:53:49 -0500

    Fix unit tests

--- src/applications/appeals/996/tests/migrations/01-lighthouse-v2-updates.unit.spec.js
+++ src/applications/appeals/996/tests/migrations/01-lighthouse-v2-updates.unit.spec.js
@@ -7,10 +7,10 @@ import saveInProgress from '../fixtures/data/save-in-progress-v1.json';
 import transformed01 from '../fixtures/data/migrated/01-migrated-v1-to-v2.json';
 
 describe('HLR v2 migration', () => {
-  it('should return v2 migrated data', () => {
+  it('should return migrated v2 data', () => {
     expect(version2Updates(saveInProgress)).to.deep.equal(transformed01);
   });
-  it('should return v2 migrated data', () => {
+  it('should return force migrated v2 data', () => {
     expect(forceV2Migration(saveInProgress.formData)).to.deep.equal(
       transformed01.formData,
     );
