09d81bd81b60fbeacde3c5f86a2465fb848c95e3
Yoni Doron-Peters <jonathan.d@thoughtworks.com>
2022-06-13 16:37:30 -0600

    DHP-301 Introduce a wrapper component around <App /> to check if Fitbit feature is enabled. If not, render the PageNotFound component

--- src/applications/dhp-connected-devices/containers/App.jsx
+++ src/applications/dhp-connected-devices/containers/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { connect, useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import UnauthenticatedPageContent from '../components/UnauthenticatedPageContent';
 import { AuthenticatedPageContent } from '../components/AuthenticatedPageContent';
 import { FrequentlyAskedQuestions } from '../components/FrequentlyAskedQuestions';
-import { dhpConnectedDevicesFitbitFeature } from '../utils/helpers';
-import { PageNotFound } from '../components/PageNotFound';
 
-export const App = ({ showConnectedDevicesPage }) => {
+export default function App() {
   const isLoading = useSelector(state => state?.user?.profile?.loading);
   const isLoggedIn = useSelector(state => state?.user.login.currentlyLoggedIn);
   const pageContent = isLoggedIn ? (
@@ -18,44 +15,27 @@ export const App = ({ showConnectedDevicesPage }) => {
   const content = isLoading ? <va-loading-indicator set-focus /> : pageContent;
 
   return (
-    <>
-      {showConnectedDevicesPage ? (
-        <div className="usa-grid-full margin landing-page">
-          <div className="usa-width-three-fourths">
-            <article className="usa-content">
-              <div className="schemaform-title">
-                <h1>Connect your health devices to share data</h1>
-              </div>
-              <div className="va-introtext">
-                <p>
-                  Connecting a device will share your device health data with
-                  VA. This data is automatically shared with your care team.
-                </p>
-              </div>
-              <p>
-                <strong>Note:</strong> Your shared data will not be monitored by
-                your VA care team. If you have concerns about any specific
-                shared data, you must contact your care team directly.
-              </p>
-              {content}
-              <FrequentlyAskedQuestions />
-            </article>
+    <div className="usa-grid-full margin landing-page">
+      <div className="usa-width-three-fourths">
+        <article className="usa-content">
+          <div className="schemaform-title">
+            <h1>Connect your health devices to share data</h1>
           </div>
-        </div>
-      ) : (
-        <PageNotFound />
-      )}
-    </>
+          <div className="va-introtext">
+            <p>
+              Connecting a device will share your device health data with VA.
+              This data is automatically shared with your care team.
+            </p>
+          </div>
+          <p>
+            <strong>Note:</strong> Your shared data will not be monitored by
+            your VA care team. If you have concerns about any specific shared
+            data, you must contact your care team directly.
+          </p>
+          {content}
+          <FrequentlyAskedQuestions />
+        </article>
+      </div>
+    </div>
   );
-};
-
-App.propTypes = {
-  showConnectedDevicesPage: PropTypes.bool,
-};
-
-const mapStateToProps = state => {
-  const showConnectedDevicesPage = dhpConnectedDevicesFitbitFeature(state);
-  return { showConnectedDevicesPage };
-};
-
-export default connect(mapStateToProps)(App);
+}
