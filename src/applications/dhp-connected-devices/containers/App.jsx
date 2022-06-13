9dd470b12bb045bae97101658abe623ea6cdd8aa
Rebecca Green <rebecca.green@thoughtworks.com>
2022-06-13 17:22:43 -0400

    [DHP-301] Adds PageNotFound component and logic to app to show it when the feature toggle is off
    Co-authored-by: Rebecca Green <rebecca.green@thoughtworks.com>
    Co-authored-by: Yoni Doron-Peters <jonathan.d@thoughtworks.com>

--- src/applications/dhp-connected-devices/containers/App.jsx
+++ src/applications/dhp-connected-devices/containers/App.jsx
@@ -5,6 +5,7 @@ import UnauthenticatedPageContent from '../components/UnauthenticatedPageContent
 import { AuthenticatedPageContent } from '../components/AuthenticatedPageContent';
 import { FrequentlyAskedQuestions } from '../components/FrequentlyAskedQuestions';
 import { dhpConnectedDevicesFitbitFeature } from '../utils/helpers';
+import { PageNotFound } from '../components/PageNotFound';
 
 export const App = ({ showConnectedDevicesPage }) => {
   const isLoading = useSelector(state => state?.user?.profile?.loading);
@@ -18,30 +19,32 @@ export const App = ({ showConnectedDevicesPage }) => {
 
   return (
     <>
-      {/* TODO render not found page if showConnectedDevicesPage is false */}
-      {showConnectedDevicesPage}
-      <div className="usa-grid-full margin landing-page">
-        <div className="usa-width-three-fourths">
-          <article className="usa-content">
-            <div className="schemaform-title">
-              <h1>Connect your health devices to share data</h1>
-            </div>
-            <div className="va-introtext">
+      {showConnectedDevicesPage ? (
+        <div className="usa-grid-full margin landing-page">
+          <div className="usa-width-three-fourths">
+            <article className="usa-content">
+              <div className="schemaform-title">
+                <h1>Connect your health devices to share data</h1>
+              </div>
+              <div className="va-introtext">
+                <p>
+                  Connecting a device will share your device health data with
+                  VA. This data is automatically shared with your care team.
+                </p>
+              </div>
               <p>
-                Connecting a device will share your device health data with VA.
-                This data is automatically shared with your care team.
+                <strong>Note:</strong> Your shared data will not be monitored by
+                your VA care team. If you have concerns about any specific
+                shared data, you must contact your care team directly.
               </p>
-            </div>
-            <p>
-              <strong>Note:</strong> Your shared data will not be monitored by
-              your VA care team. If you have concerns about any specific shared
-              data, you must contact your care team directly.
-            </p>
-            {content}
-            <FrequentlyAskedQuestions />
-          </article>
+              {content}
+              <FrequentlyAskedQuestions />
+            </article>
+          </div>
         </div>
-      </div>
+      ) : (
+        <PageNotFound />
+      )}
     </>
   );
 };
