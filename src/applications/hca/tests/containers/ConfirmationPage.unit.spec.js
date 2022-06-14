0808da6be90aeeca81062712981b44f27dd0c1ac
Matt Long <longmd@me.com>
2022-06-14 17:30:05 -0400

    Refactor confirmation page and update testing

---
+++ src/applications/hca/tests/containers/ConfirmationPage.unit.spec.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import { expect } from 'chai';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+import localStorage from 'platform/utilities/storage/localStorage';
+import ConfirmationPage from '../../containers/ConfirmationPage';
+
+describe('HCA ConfirmationPage', () => {
+  const initState = {
+    form: {
+      submission: false,
+      data: {
+        veteranFullName: {
+          first: 'Jack',
+          middle: 'William',
+          last: 'Smith',
+        },
+      },
+    },
+    user: {
+      profile: {
+        userFullName: {
+          first: 'John',
+          middle: 'Marjorie',
+          last: 'Smith',
+          suffix: 'Sr.',
+        },
+      },
+    },
+  };
+  const middleware = [];
+  const mockStore = configureStore(middleware);
+
+  it('should render', () => {
+    const store = mockStore(initState);
+    const view = render(
+      <Provider store={store}>
+        <ConfirmationPage />
+      </Provider>,
+    );
+    expect(view.container.querySelector('.hca-confirmation-page')).to.exist;
+    expect(
+      view.container.querySelector('.schemaform-confirmation-claim-header'),
+    ).to.contain.text('Thank you for submitting your application');
+    expect(
+      view.container.querySelectorAll('.confirmation-guidance-message').length,
+    ).to.equal(3);
+  });
+
+  it('should render without response properties', () => {
+    const store = mockStore(initState);
+    const view = render(
+      <Provider store={store}>
+        <ConfirmationPage />
+      </Provider>,
+    );
+    expect(view.container.querySelector('.hca-claim-list')).to.not.exist;
+  });
+
+  it('should render with response properties', () => {
+    const mockFormData = {
+      submission: {
+        response: {
+          timestamp: '2010-01-01',
+          formSubmissionId: '3702390024',
+        },
+      },
+    };
+    const store = mockStore({
+      ...initState,
+      form: { ...initState.form, ...mockFormData },
+    });
+    const view = render(
+      <Provider store={store}>
+        <ConfirmationPage />
+      </Provider>,
+    );
+    expect(view.container.querySelector('.hca-claim-list')).to.exist;
+    expect(
+      view.container.querySelector('.hca-claim-list > dd'),
+    ).to.contain.text('Jan. 1, 2010');
+  });
+
+  it('should render without email message', () => {
+    const store = mockStore(initState);
+    const view = render(
+      <Provider store={store}>
+        <ConfirmationPage />
+      </Provider>,
+    );
+    expect(view.container.querySelector('.hca-email-message')).to.not.exist;
+  });
+
+  it('should render email message', () => {
+    const mockFormData = { email: 'jack.smith@gmail.com' };
+    const store = mockStore({
+      ...initState,
+      form: {
+        ...initState.form,
+        data: { ...initState.form.data, ...mockFormData },
+      },
+    });
+    const view = render(
+      <Provider store={store}>
+        <ConfirmationPage />
+      </Provider>,
+    );
+    expect(view.container.querySelector('.hca-email-message')).to.exist;
+  });
+
+  it('should render veteran’s name from form data', () => {
+    const store = mockStore(initState);
+    const view = render(
+      <Provider store={store}>
+        <ConfirmationPage />
+      </Provider>,
+    );
+    expect(
+      view.container.querySelector('.hca-veteran-fullname'),
+    ).to.contain.text('Jack William Smith');
+  });
+
+  it('should render veteran’s name from profile', () => {
+    localStorage.setItem('hasSession', true);
+    const store = mockStore(initState);
+    const view = render(
+      <Provider store={store}>
+        <ConfirmationPage />
+      </Provider>,
+    );
+    expect(
+      view.container.querySelector('.hca-veteran-fullname'),
+    ).to.contain.text('John Marjorie Smith Sr.');
+    localStorage.removeItem('hasSession');
+  });
+});
