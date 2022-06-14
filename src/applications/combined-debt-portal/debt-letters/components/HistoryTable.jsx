369c7bd20219c300afb646599a1105925af511f5
Andrew Rodiek <andrew.rodiek@gmail.com>
2022-06-14 10:46:18 -0400

    Updated CDP general and debt-letters styling
    * Separated breadcrumbs from content to help with awkward wrappnig
    * Removed a bunch of superfluous styling tags, and emptied out the css of unused styles
    * Fixed history table to match mock (no side borders)
    * Fixed debt details summary card layouts
      * Icon was not centered on first line, text was wrapping underneath on smaller screens
      * Link text was awkwardly wrapping on smaller screens, it's been adjusted
    * Updated Details card to use 'action-link' platform styling
    * Updated Loading spinner to just use va-loading-indicator

--- src/applications/combined-debt-portal/debt-letters/components/HistoryTable.jsx
+++ src/applications/combined-debt-portal/debt-letters/components/HistoryTable.jsx
@@ -3,32 +3,34 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import { renderLetterHistory } from '../const/diary-codes';
 
-const HistoryTable = ({ history }) => (
-  <table className="vads-u-margin-y--4">
-    <thead>
-      <tr>
-        <th className="vads-u-font-weight--bold" scope="col">
-          Date
-        </th>
-        <th className="vads-u-font-weight--bold" scope="col">
-          Letter
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      {history.map((debt, index) => (
-        <tr key={`${debt.date}-${index}`}>
-          <td>{moment(debt.date, 'MM-DD-YYYY').format('MMMM D, YYYY')}</td>
-          <td>
-            <div className="vads-u-margin-top--0">
-              {renderLetterHistory(debt.letterCode)}
-            </div>
-          </td>
+const HistoryTable = ({ history }) => {
+  return (
+    <table className="debt-letters-table vads-u-margin-y--4">
+      <thead>
+        <tr>
+          <th className="vads-u-font-weight--bold" scope="col">
+            Date
+          </th>
+          <th className="vads-u-font-weight--bold" scope="col">
+            Letter
+          </th>
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
+      </thead>
+      <tbody>
+        {history.map((debt, index) => (
+          <tr key={`${debt.date}-${index}`}>
+            <td>{moment(debt.date, 'MM-DD-YYYY').format('MMMM D, YYYY')}</td>
+            <td>
+              <div className="vads-u-margin-top--0">
+                {renderLetterHistory(debt.letterCode)}
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 HistoryTable.propTypes = {
   history: PropTypes.arrayOf(
