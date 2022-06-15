24820bf68450082ae42a77f19ee4d0869368e5d6
Bethany Dunfield <brdunfield@gmail.com>
2022-06-15 14:38:28 -0600

    Move duplicated mock appointments to an import (#21351)

---
+++ src/applications/check-in/tests/unit/mocks/mock-appointments.js
@@ -0,0 +1,59 @@
+const singleAppointment = [
+  {
+    facility: 'LOMA LINDA VA CLINIC',
+    clinicPhoneNumber: '5551234567',
+    clinicFriendlyName: 'TEST CLINIC',
+    clinicName: 'LOM ACC CLINIC TEST',
+    appointmentIen: 'some-ien',
+    startTime: '2022-01-03T14:56:04.788Z',
+    eligibility: 'ELIGIBLE',
+    facilityId: 'some-facility',
+    checkInWindowStart: '2022-01-03T14:56:04.788Z',
+    checkInWindowEnd: '2022-01-03T14:56:04.788Z',
+    checkedInTime: '',
+  },
+];
+
+const multipleAppointments = [
+  {
+    facility: 'LOMA LINDA VA CLINIC',
+    clinicPhoneNumber: '5551234567',
+    clinicFriendlyName: 'TEST CLINIC',
+    clinicName: 'LOM ACC CLINIC TEST',
+    appointmentIen: 'some-ien',
+    startTime: '2021-11-30T17:12:10.694Z',
+    eligibility: 'ELIGIBLE',
+    facilityId: 'some-facility',
+    checkInWindowStart: '2021-11-30T17:12:10.694Z',
+    checkInWindowEnd: '2021-11-30T17:12:10.694Z',
+    checkedInTime: '',
+  },
+  {
+    facility: 'LOMA LINDA VA CLINIC',
+    clinicPhoneNumber: '5551234567',
+    clinicFriendlyName: 'TEST CLINIC',
+    clinicName: 'LOM ACC CLINIC TEST',
+    appointmentIen: 'some-ien',
+    startTime: '2021-11-30T17:12:10.694Z',
+    eligibility: 'ELIGIBLE',
+    facilityId: 'some-facility',
+    checkInWindowStart: '2021-11-30T17:12:10.694Z',
+    checkInWindowEnd: '2021-11-30T17:12:10.694Z',
+    checkedInTime: '',
+  },
+  {
+    facility: 'LOMA LINDA VA CLINIC',
+    clinicPhoneNumber: '5551234567',
+    clinicFriendlyName: 'TEST CLINIC',
+    clinicName: 'LOM ACC CLINIC TEST',
+    appointmentIen: 'some-other-ien',
+    startTime: '2021-11-30T17:12:10.694Z',
+    eligibility: 'ELIGIBLE',
+    facilityId: 'some-facility',
+    checkInWindowStart: '2021-11-30T17:12:10.694Z',
+    checkInWindowEnd: '2021-11-30T17:12:10.694Z',
+    checkedInTime: '',
+  },
+];
+
+export { singleAppointment, multipleAppointments };
