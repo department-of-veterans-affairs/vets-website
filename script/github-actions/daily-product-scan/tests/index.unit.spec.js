47134c2c5b1ec1b24a3eb703e1c5fe4353a0c073
Theo Bentum <theo@adhocteam.us>
2022-06-15 14:34:13 -0700

    Revert daily-product-scan test skip

--- script/github-actions/daily-product-scan/tests/index.unit.spec.js
+++ script/github-actions/daily-product-scan/tests/index.unit.spec.js
@@ -6,7 +6,7 @@ const main = require('../main');
 const GitHubClient = require('../github-client');
 const octokitResponses = require('./mocks/octokit-responses');
 
-describe.skip('daily-product-scan', () => {
+describe('daily-product-scan', () => {
   context('success, changes ARE detected', () => {
     let status;
     let message;
