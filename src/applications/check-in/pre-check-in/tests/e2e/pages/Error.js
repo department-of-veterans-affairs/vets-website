b8ffd1cb78dd907ebca7489594610a48cc69d73a
Bethany Dunfield <bethany.dunfield@civicactions.com>
2022-06-17 09:11:32 -0600

    one more

--- src/applications/check-in/pre-check-in/tests/e2e/pages/Error.js
+++ src/applications/check-in/pre-check-in/tests/e2e/pages/Error.js
@@ -3,7 +3,7 @@ import Timeouts from 'platform/testing/e2e/timeouts';
 class Error {
   validatePageLoaded = (lastValidateAttempt = false) => {
     const messageText = lastValidateAttempt
-      ? "We're sorry. We couldn't match your information to our records. Please call us at 800-698-2411 (TTY:711) for help signing in."
+      ? 'We’re sorry. We couldn’t match your information to our records.'
       : 'We’re sorry. Something went wrong on our end. Please try again.';
     cy.get('h1', { timeout: Timeouts.slow })
       .should('be.visible')
