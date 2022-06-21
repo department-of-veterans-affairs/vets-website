17ecae7909f51e58e465fabf2cb33eaaa5a24ac1
Adam Whitlock <adam.whitlock@adhocteam.us>
2022-06-21 11:54:36 -0600

    (test) fix profile unit test for no DD toggle

--- src/applications/personalization/profile/tests/components/Profile.unit.spec.js
+++ src/applications/personalization/profile/tests/components/Profile.unit.spec.js
@@ -285,7 +285,6 @@ describe('mapStateToProps', () => {
       'shouldFetchCNPDirectDepositInformation',
       'shouldFetchEDUDirectDepositInformation',
       'shouldFetchTotalDisabilityRating',
-      'shouldShowDirectDeposit',
       'isDowntimeWarningDismissed',
       'shouldShowProfileLGBTQEnhancements',
     ];
