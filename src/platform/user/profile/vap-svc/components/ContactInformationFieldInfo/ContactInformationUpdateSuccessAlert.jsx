975eb36b2d0f9d0f88a06206738a72c85b3e4283
Mark Dewey <mdewey@users.noreply.github.com>
2022-06-27 08:56:33 -0400

    (feature) Updated focus control after editing of field (#21431)
    * (paused) mvp of selector
    
    * (feature) added focus to nearest edit element successful update
    
    * (refactor) updated alert box
    
    * (chore) fixed linting errors
    
    * (mocks) added phone number mocks
    
    * (test) added simple tests for focus control
    
    * (test) fixed a e2e test
    
    * (test) fixed a e2e test

--- src/platform/user/profile/vap-svc/components/ContactInformationFieldInfo/ContactInformationUpdateSuccessAlert.jsx
+++ src/platform/user/profile/vap-svc/components/ContactInformationFieldInfo/ContactInformationUpdateSuccessAlert.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useLastLocation } from 'react-router-last-location';
-
-import AlertBox from '@department-of-veterans-affairs/component-library/AlertBox';
-
 import { PROFILE_PATHS } from '@@profile/constants';
 import { FIELD_NAMES } from '@@vap-svc/constants';
 import { selectVAPContactInfoField } from '@@vap-svc/selectors';
+
+import { focusElement } from '~/platform/utilities/ui';
 
 const ContactInformationUpdateSuccessAlert = ({ fieldName }) => {
   const fieldData = useSelector(state => {
     return selectVAPContactInfoField(state, fieldName);
   });
   const referrer = useLastLocation();
+  const id = `${fieldName}-alert`;
+  const useLink =
+    !!fieldData &&
+    referrer?.pathname === PROFILE_PATHS.NOTIFICATION_SETTINGS &&
+    fieldName === FIELD_NAMES.MOBILE_PHONE;
+  useEffect(
+    () => {
+      const editButton = document
+        .querySelector(`[data-field-name=${fieldName}]`)
+        .querySelector("[data-action='edit']");
+      if (editButton) {
+        focusElement(editButton);
+      }
+    },
+
+    [fieldName, id, useLink],
+  );
   const message = React.useMemo(
     () => {
-      if (
-        !!fieldData &&
-        referrer?.pathname === PROFILE_PATHS.NOTIFICATION_SETTINGS &&
-        fieldName === FIELD_NAMES.MOBILE_PHONE
-      ) {
+      if (useLink) {
         return (
           <>
             Update saved. Now you can{' '}
@@ -33,22 +46,31 @@ const ContactInformationUpdateSuccessAlert = ({ fieldName }) => {
       }
       return 'Update saved.';
     },
-    [fieldName, fieldData, referrer],
+    [useLink],
   );
 
   return (
-    <AlertBox backgroundOnly status="success" className="vads-u-margin-y--1">
-      <div className="vads-u-display--flex">
-        <i
-          aria-hidden="true"
-          className="fa fa-check-circle vads-u-padding-top--0p5 vads-u-margin-right--1"
-        />
-        <p className="vads-u-margin-y--0" role="alert" aria-live="polite">
-          {message}
-        </p>
-      </div>
-    </AlertBox>
+    <>
+      <va-alert status="success" background-only class="vads-u-margin-y--1">
+        <div
+          className="vads-u-display--flex vads-u-margin-left--neg1p5"
+          id={id}
+        >
+          <i
+            aria-hidden="true"
+            className="fa fa-check-circle vads-u-padding-top--0p5 vads-u-margin-right--1"
+          />
+          <p className="vads-u-margin-y--0" role="alert" aria-live="polite">
+            {message}
+          </p>
+        </div>
+      </va-alert>
+    </>
   );
+};
+
+ContactInformationUpdateSuccessAlert.propTypes = {
+  fieldName: PropTypes.string.isRequired,
 };
 
 export default ContactInformationUpdateSuccessAlert;
