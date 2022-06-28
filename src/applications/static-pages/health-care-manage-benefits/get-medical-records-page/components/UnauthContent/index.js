bff47770eaa0b7d73cf2aee7745eabbcd2af2ccd
Allison Lu <allisonlu820@gmail.com>
2022-06-28 16:05:06 -0400

    VACMS-9387: - updated FAQ content for unauthenticated/non-Cerner users

--- src/applications/static-pages/health-care-manage-benefits/get-medical-records-page/components/UnauthContent/index.js
+++ src/applications/static-pages/health-care-manage-benefits/get-medical-records-page/components/UnauthContent/index.js
@@ -5,7 +5,6 @@ import Telephone, {
 } from '@department-of-veterans-affairs/component-library/Telephone';
 // Relative imports.
 import CallToActionWidget from 'applications/static-pages/cta-widget';
-import ServiceProvidersList from 'platform/user/authentication/components/ServiceProvidersList';
 
 const UnauthContent = () => (
   <>
@@ -20,21 +19,11 @@ const UnauthContent = () => (
         <a href="#vhie">The Veterans Health Information Exchange (VHIE)</a>
       </li>
     </ul>
-    <h2 id="va-blue-button">VA Blue Button</h2>
+    <h2 id="va-blue-button">
+      Use VA Blue Button to manage your records online
+    </h2>
     <CallToActionWidget appId="health-records" setFocus={false} />
-    <h3>
-      What is VA Blue Button, and how can it help me manage my health care?
-    </h3>
-    <p>
-      VA Blue Button is a feature of the My HealtheVet health management portal.
-      It lets you review, print, save, download, and share information from your
-      VA medical record and personal health record. With this tool, you can
-      better manage your health needs and communicate with your health care
-      team.
-    </p>
-    <p>
-      <strong>With VA Blue Button, you can:</strong>
-    </p>
+    <h3>What you can do when you sign in</h3>
     <ul>
       <li>
         Download a customized Blue Button report with information from your VA
@@ -43,8 +32,8 @@ const UnauthContent = () => (
       </li>
       <li>
         Download a Health Summary that includes specific information from your
-        VA medical records (like your known allergies, medications, and recent
-        lab results)
+        VA medical records (like your known allergies, medicines, and recent lab
+        results)
       </li>
       <li>
         Build your own personal health record that includes information like
@@ -59,96 +48,190 @@ const UnauthContent = () => (
         yourself with your VA health care team through secure messaging
       </li>
     </ul>
-    <h3>Am I eligible to use all the features of VA Blue Button?</h3>
+    <h3>Who can manage VA medical records online</h3>
     <p>
-      You can use all the features of this tool if you meet all of the
-      requirements listed below.
+      You can use all the features of VA Blue Button if you meet all of these
+      requirements.
     </p>
     <p>
-      <strong>Both of these must be true. You’re:</strong>
+      <strong>All of these must be true:</strong>
     </p>
     <ul>
       <li>
-        Enrolled in VA health care, <strong>and</strong>
+        You’re enrolled in VA health care, <strong>and</strong>
       </li>
-      <li>Registered as a patient in a VA health facility</li>
+      <li>
+        You’re registered as a patient in a VA health facility,
+        <strong>and</strong>
+      </li>
+      <li>
+        You have a verified <strong>Login.gov</strong> or <strong>ID.me</strong>
+        account or a Premium <strong>DS Logon</strong> or
+        <strong>My HealtheVet</strong> account
+      </li>
     </ul>
     <p>
       <a href="/health-care/how-to-apply">
         Find out how to apply for VA health care
       </a>
     </p>
-    <p>
-      <strong>And you must have one of these free accounts:</strong>
-    </p>
-    <ServiceProvidersList />
-    <a
-      href="https://www.myhealth.va.gov/mhv-portal-web/my-healthevet-offers-three-account-types"
-      rel="noreferrer noopener"
+    <h3>Questions about managing your VA medical records</h3>
+    <va-accordion
+      disable-analytics={{
+        value: 'false',
+      }}
+      section-heading={{
+        value: 'null',
+      }}
     >
-      Learn about the 2 different My HealtheVet account types
-    </a>
-    <h3>Once I’m signed in, how do I access my medical records?</h3>
-
-    <p>
-      Go to your welcome page dashboard, and click on{' '}
-      <strong>Health Records</strong>. You’ll go to a new page.
-    </p>
-    <p>From here, you can choose to access your VA:</p>
-    <ul>
-      <li>Blue Button report</li>
-      <li>Health summary</li>
-      <li>Medical images and reports</li>
-    </ul>
-    <h4>If you’d like to add information to your personal health record</h4>
-    <p>
-      Go to the blue navigation menu at the top of the page, and click on{' '}
-      <strong>Track Health</strong>. You’ll go to a new page where you can
-      choose to record information like your vital signs, health history, goals,
-      and food and exercise efforts.
-    </p>
-    <h3>Will my personal health information be protected?</h3>
-    <p>
-      Yes. This is a secure website. We follow strict security policies and
-      practices to protect your personal health information.
-    </p>
-    <p>
-      If you print or download anything from the website, you’ll need to take
-      responsibility for protecting that information.
-    </p>
-    <h3>What if I have more questions?</h3>
-    <p>You can:</p>
-    <ul>
-      <li>
-        Read the FAQs pages on the My HealtheVet web portal
-        <br />
-        <a href="https://www.myhealth.va.gov/mhv-portal-web/web/myhealthevet/faqs#bbtop">
-          VA Blue Button FAQs
+      <va-accordion-item id="first">
+        <h4 slot="headline">
+          Once I’m signed in, how do I access my medical records?
+        </h4>
+        <p>
+          Go to your welcome page dashboard. Then select{' '}
+          <strong>Health Records</strong>. You’ll go to a new page.
+        </p>
+        <p>From here, you can choose to access these items:</p>
+        <ul>
+          <li>Your VA Blue Button report</li>
+          <li>Your VA health summary</li>
+          <li>Your VA medical images and reports</li>
+        </ul>
+      </va-accordion-item>
+      <va-accordion-item
+        header="How can I add information to my personal health record?"
+        id="second"
+      >
+        <p>
+          Go to the main navigation menu at the top of the page, and click on{' '}
+          <strong>Track Health</strong>. You’ll go to a new page.
+        </p>
+        <p>
+          From here, you can choose to record different types of information
+          like these:
+        </p>
+        <ul>
+          <li>Vital signs</li>
+          <li>Health history</li>
+          <li>Health goals</li>
+          <li>Food and exercise efforts</li>
+        </ul>
+      </va-accordion-item>
+      <va-accordion-item
+        header="Can I get notifications when my medical images and reports are ready?"
+        id="third"
+      >
+        <p>
+          Yes. You can sign up to get notifications by email on the{' '}
+          <strong>My HealtheVet</strong> website. You can get notifications with
+          a Basic or Premium <strong>My HealtheVet</strong> account.
+        </p>
+        <p>
+          <strong>If you’re signing up for a My HealtheVet account</strong>, go
+          to "Notifications and Settings" on the registration page. Select{' '}
+          <strong>On</strong>
+          for "VA medical images and report available notification.
+        </p>
+        <a href="https://www.myhealth.va.gov/mhv-portal-web/user-registration/">
+          Sign up for an account on the My HealtheVet website
         </a>
-        <br />
-        <a href="https://www.myhealth.va.gov/mhv-portal-web/faqs#CCD">
-          VA health summary FAQs
+        <p>
+          <strong>If you already have a My HealtheVet account</strong>, go to
+          your profile page to update your notification settings.
+        </p>
+        <a href="https://www.myhealth.va.gov/mhv-portal-web/web/myhealthevet/user-login?redirect=/mhv-portal-web/user-registration/user-login">
+          Sign in to your account on the My HealtheVet website
         </a>
-        <br />
-        <a href="https://www.myhealth.va.gov/mhv-portal-web/faqs#VAMIR">
-          VA medical images and reports FAQs
+      </va-accordion-item>
+      <va-accordion-item
+        header="What if some of my medical records are missing online?"
+        id="fourth"
+      >
+        <p>
+          You can request a complete copy of your medical records from your VA
+          health facility or the Department of Defense (DoD), depending on where
+          you received care.
+        </p>
+        <a href="/resources/how-to-get-your-medical-records-from-your-va-health-facility">
+          Learn how to request medical records from your VA health facility
         </a>
-      </li>
-      <li>
-        Call the My HealtheVet help desk at{' '}
-        <a href="tel:18773270022" aria-label="8 7 7. 3 2 7. 0 0 2 2.">
-          877-327-0022
-        </a>{' '}
-        (TTY: <Telephone contact={CONTACTS.HELP_TTY} />
-        ). We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
-      </li>
-      <li>
-        Or{' '}
-        <a href="https://www.myhealth.va.gov/mhv-portal-web/web/myhealthevet/contact-mhv">
-          contact us online
+        <a href="/resources/how-to-get-your-medical-records-from-the-department-of-defense-dod">
+          Learn how to request medical records from DoD
         </a>
-      </li>
-    </ul>
+      </va-accordion-item>
+      <va-accordion-item
+        header="Will VA protect my personal health information?"
+        id="fifth"
+      >
+        <p>
+          Yes. This is a secure website. We follow strict security policies and
+          practices to protect your personal health information.
+        </p>
+        <p>
+          If you print or download anything from the website, you’ll need to
+          take responsibility for protecting that information.
+        </p>
+      </va-accordion-item>
+      <va-accordion-item
+        header="How does VA share my health information with providers outside VA?"
+        id="sixth"
+      >
+        <p>
+          The Veterans Health Information Exchange (VHIE) program lets us
+          securely share your health information with participating community
+          care providers in our network. VHIE also lets us share your
+          information with the Department of Defense.
+        </p>
+        <p>
+          VHIE gives your health care providers a more complete understanding of
+          your health record. This more complete understanding can help your
+          providers make more informed treatment decisions. We share your health
+          information with participating community providers only when they’re
+          treating you.
+        </p>
+        <p>
+          If you don’t want us to share your information through VHIE, you can
+          opt out at any time.
+        </p>
+        <a href="/resources/the-veterans-health-information-exchange">
+          Learn more about VHIE
+        </a>
+      </va-accordion-item>
+      <va-accordion-item header="What if I have more questions?" id="seventh">
+        <p>You can get more information in any of these ways:</p>
+        <ul>
+          <li>
+            Read the FAQs pages on the My HealtheVet web portal
+            <br />
+            <a href="https://www.myhealth.va.gov/mhv-portal-web/web/myhealthevet/faqs-blue-button">
+              VA Blue Button FAQs
+            </a>
+            <br />
+            <a href="https://www.myhealth.va.gov/mhv-portal-web/web/myhealthevet/faqs-health-summary">
+              VA health summary FAQs
+            </a>
+            <br />
+            <a href="https://www.myhealth.va.gov/mhv-portal-web/web/myhealthevet/what-are-va-medical-images-and-reports-faqs">
+              VA medical images and reports FAQs
+            </a>
+          </li>
+          <li>
+            Call the My HealtheVet help desk at{' '}
+            <a href="tel:18773270022" aria-label="8 7 7. 3 2 7. 0 0 2 2.">
+              877-327-0022
+            </a>{' '}
+            (TTY: <Telephone contact={CONTACTS.HELP_TTY} />
+            ). We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
+          </li>
+          <li>
+            <a href="https://www.myhealth.va.gov/contact-us">
+              Contact us online through My HealtheVet
+            </a>
+          </li>
+        </ul>
+      </va-accordion-item>
+    </va-accordion>
 
     <h2 id="vhie">The Veterans Health Information Exchange</h2>
     <p>
