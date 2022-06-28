26301af963ce138a6403510129524f559d15896a
Tony Williams <tony.williams@va.gov>
2022-06-28 08:50:20 -0500

    fix unit test

--- src/applications/vaos/tests/mocks/v2.js
+++ src/applications/vaos/tests/mocks/v2.js
@@ -73,7 +73,7 @@ export function getVAOSAppointmentMock() {
       practitioners: [],
       preferredTimesForPhoneCall: [],
       priority: null,
-      reason: null,
+      reasonCode: null,
       requestedPeriods: null,
       serviceType: null,
       slot: null,
