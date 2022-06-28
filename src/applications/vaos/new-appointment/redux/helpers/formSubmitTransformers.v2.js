3dc8695e4d838b6e5ab6ee26e5de68403145398c
Tony Williams <tony.williams@va.gov>
2022-06-28 10:54:23 -0500

    fix to convert requested date to facility timezone

--- src/applications/vaos/new-appointment/redux/helpers/formSubmitTransformers.v2.js
+++ src/applications/vaos/new-appointment/redux/helpers/formSubmitTransformers.v2.js
@@ -1,6 +1,6 @@
-import moment from 'moment';
 import titleCase from 'platform/utilities/data/titleCase';
 import { selectVAPResidentialAddress } from 'platform/user/selectors';
+import moment from '../../../lib/moment-tz';
 import {
   LANGUAGES,
   PURPOSE_TEXT,
@@ -14,6 +14,7 @@ import {
   getChosenSlot,
 } from '../selectors';
 import { getClinicId } from '../../../services/healthcare-service';
+import { getTimezoneByFacilityId } from '../../../utils/timezone';
 
 export function transformFormToVAOSCCRequest(state) {
   const data = getFormData(state);
@@ -61,7 +62,7 @@ export function transformFormToVAOSCCRequest(state) {
   }
 
   const typeOfCare = getTypeOfCare(data);
-
+  const facilityTimezone = getTimezoneByFacilityId(data.communityCareSystemId);
   return {
     kind: 'cc',
     status: 'proposed',
@@ -81,12 +82,16 @@ export function transformFormToVAOSCCRequest(state) {
       ],
     },
     requestedPeriods: data.selectedDates.map(date => ({
-      start: moment.utc(date).format('YYYY-MM-DDTHH:mm:ss'),
+      start: moment
+        .tz(date, facilityTimezone)
+        .utc()
+        .format(),
       end: moment
-        .utc(date)
+        .tz(date, facilityTimezone)
+        .utc()
         .add(12, 'hours')
         .subtract(1, 'minute')
-        .format('YYYY-MM-DDTHH:mm:ss'),
+        .format(),
     })),
     // These four fields aren't in the current schema, but probably should be
     preferredTimesForPhoneCall: Object.entries(data.bestTimeToCall)
