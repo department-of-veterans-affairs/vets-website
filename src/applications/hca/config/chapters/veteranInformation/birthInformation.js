60030a5908c90cfb147ddece1e815ff23b0110f6
Matt Long <longmd@me.com>
2022-06-29 10:13:03 -0400

    Remove country of birth field

--- src/applications/hca/config/chapters/veteranInformation/birthInformation.js
+++ src/applications/hca/config/chapters/veteranInformation/birthInformation.js
@@ -1,11 +1,9 @@
 import fullSchemaHca from 'vets-json-schema/dist/10-10EZ-schema.json';
-import { countries, states } from 'platform/forms/address';
-
 import AuthenticatedShortFormAlert from '../../../components/FormAlerts/AuthenticatedShortFormAlert';
 import { BirthInfoDescription } from '../../../components/FormDescriptions';
 import { HIGH_DISABILITY, emptyObjectSchema } from '../../../helpers';
 
-const { cityOfBirth } = fullSchemaHca.properties;
+const { cityOfBirth, stateOfBirth } = fullSchemaHca.properties;
 
 export default {
   uiSchema: {
@@ -23,29 +21,11 @@ export default {
     'view:placeOfBirth': {
       'ui:title': 'Place of birth',
       'ui:description': BirthInfoDescription,
-      countryOfBirth: {
-        'ui:title': 'Country',
-      },
       cityOfBirth: {
         'ui:title': 'City',
       },
       stateOfBirth: {
         'ui:title': 'State/Province/Region',
-        'ui:options': {
-          hideIf: formData => {
-            const { countryOfBirth } = formData['view:placeOfBirth'];
-            return !states[countryOfBirth]?.length;
-          },
-          updateSchema: formData => {
-            const { countryOfBirth } = formData['view:placeOfBirth'];
-            return states[countryOfBirth]?.length
-              ? {
-                  enum: states[countryOfBirth].map(object => object.value),
-                  enumNames: states[countryOfBirth].map(object => object.label),
-                }
-              : { enum: [], enumNames: [] };
-          },
-        },
       },
     },
   },
@@ -56,18 +36,8 @@ export default {
       'view:placeOfBirth': {
         type: 'object',
         properties: {
-          countryOfBirth: {
-            default: 'USA',
-            type: 'string',
-            enum: countries.map(object => object.value),
-            enumNames: countries.map(object => object.label),
-          },
           cityOfBirth,
-          stateOfBirth: {
-            type: 'string',
-            enum: states.USA.map(object => object.value),
-            enumNames: states.USA.map(object => object.label),
-          },
+          stateOfBirth,
         },
       },
     },
