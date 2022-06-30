3e16b4e94dc6d361e7db7b691b42ec11ef4c534f
Tony Williams <tony.williams@va.gov>
2022-06-29 19:44:30 -0500

    initial changes

--- src/applications/vaos/new-appointment/redux/helpers/formSubmitTransformers.v2.js
+++ src/applications/vaos/new-appointment/redux/helpers/formSubmitTransformers.v2.js
@@ -15,6 +15,25 @@ import {
 } from '../selectors';
 import { getClinicId } from '../../../services/healthcare-service';
 
+function getReasonCode(data) {
+  const code = PURPOSE_TEXT_V2.filter(purpose => purpose.id !== 'other').find(
+    purpose => purpose.id === data.reasonForAppointment,
+  )?.serviceName;
+
+  return {
+    // If the user selects one of the three preset radio selections
+    // ("Routine Follow-up", "New Problem", or "Medication Concern"), then that values goes
+    // in reasonCode.coding[0].code.
+    coding: code ? [{ code }] : undefined,
+    // Per Brad - All comments should be sent in the reasonCode.text field and should should be
+    // truncated to 100 char for both VA appointment types only. CC appointments will continue
+    // to be truncated to 250 char
+    text: data.reasonAdditionalInfo
+      ? data.reasonAdditionalInfo.slice(0, 100)
+      : null,
+  };
+}
+
 export function transformFormToVAOSCCRequest(state) {
   const data = getFormData(state);
   const provider = data.communityCareProvider;
@@ -68,6 +87,7 @@ export function transformFormToVAOSCCRequest(state) {
     locationId: data.communityCareSystemId,
     serviceType: typeOfCare.idV2 || typeOfCare.ccId,
     comment: data.reasonAdditionalInfo,
+    reasonCode: getReasonCode(data),
     contact: {
       telecom: [
         {
@@ -103,9 +123,6 @@ export function transformFormToVAOSCCRequest(state) {
 export function transformFormToVAOSVARequest(state) {
   const data = getFormData(state);
   const typeOfCare = getTypeOfCare(data);
-  const code = PURPOSE_TEXT_V2.find(
-    purpose => purpose.id === data.reasonForAppointment,
-  )?.serviceName;
 
   return {
     kind: data.visitType,
@@ -113,20 +130,7 @@ export function transformFormToVAOSVARequest(state) {
     locationId: data.vaFacility,
     // This may need to change when we get the new service type ids
     serviceType: typeOfCare.idV2,
-    reasonCode:
-      code === 'Other'
-        ? {
-            coding: [],
-            text: data.reasonAdditionalInfo.slice(0, 100),
-          }
-        : {
-            coding: [
-              {
-                code,
-              },
-            ],
-            text: code,
-          },
+    reasonCode: getReasonCode(data),
     comment: data.reasonAdditionalInfo,
     contact: {
       telecom: [
@@ -183,5 +187,6 @@ export function transformFormToVAOSAppointment(state) {
     locationId: data.vaFacility,
     // removing this for now, it's preventing QA from testing, will re-introduce when the team figures out how we're handling the comment field
     comment: getUserMessage(data),
+    reasonCode: getReasonCode(data),
   };
 }
--- src/applications/vaos/services/appointment/transformers.v2.js
+++ src/applications/vaos/services/appointment/transformers.v2.js
@@ -166,6 +166,17 @@ export function transformVAOSAppointment(appt) {
   if (appt.location && appt.location.attributes) {
     facilityData = transformFacilityV2(appt.location.attributes);
   }
+  let comment = null;
+  const { coding } = appt.reasonCode;
+  const { text } = appt.reasonCode;
+  if (coding && coding[0]?.code && appt.reasonCode.text) {
+    comment = `${coding[0].code}: ${text}`;
+  } else if (coding && coding[0].code) {
+    comment = coding[0].code;
+  } else {
+    comment = text;
+  }
+
   return {
     resourceType: 'Appointment',
     id: appt.id,
@@ -188,7 +199,7 @@ export function transformVAOSAppointment(appt) {
     comment:
       isVideo && !!appt.patientInstruction
         ? getPatientInstruction(appt)
-        : appt.comment || null,
+        : comment,
     videoData,
     communityCareProvider:
       isCC && !isRequest
