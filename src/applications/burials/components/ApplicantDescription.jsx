6df4475ab2e2eec57abd441febf0620cda0e0b5e
Jerek Shoemaker <jerek.shoemaker@adhocteam.us>
2022-07-01 14:41:36 -0500

    Burials | Update claimant information description (#21552)
    * Updating fullNameUI descriptions to better describe the information needed
    
    * Updating fullNameUI for veteran's previous names
    
    * Trigger CI
    
    * Updating with finalized content

--- src/applications/burials/components/ApplicantDescription.jsx
+++ src/applications/burials/components/ApplicantDescription.jsx
@@ -6,8 +6,8 @@ import PrefillMessage from 'platform/forms/save-in-progress/PrefillMessage';
 const ApplicantDescription = ({ formContext }) => (
   <div>
     <p>
-      As the person claiming burial benefits, you need to provide some key
-      information. You aren’t required to fill in all fields, but we can review
+      As the person claiming burial benefits, you’ll need to provide some
+      information. You don’t need to fill in all the fields, but we can review
       your application faster if you provide more information.
     </p>
     <PrefillMessage formContext={formContext} />
