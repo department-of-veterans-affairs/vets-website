a21456f60a9bdbb0fc7946bf14a5da445edf78f0
Brian Seek <brian.seek@civicactions.com>
2022-07-07 14:39:38 -0700

    Removed expiration date. (#21605)

--- src/applications/check-in/pre-check-in/pages/Introduction/IntroductionDisplay.jsx
+++ src/applications/check-in/pre-check-in/pages/Introduction/IntroductionDisplay.jsx
@@ -2,7 +2,6 @@ import React, { useMemo, useState, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
-import { subDays } from 'date-fns';
 
 import VaModal from '@department-of-veterans-affairs/component-library/Modal';
 
@@ -62,7 +61,6 @@ const IntroductionDisplay = props => {
       ),
     },
   ];
-  const appointmentsDateTime = new Date(appointments[0].startTime);
   const privacyStatement = (
     <div>
       <h3>{t('privacy-act-statement')}</h3>
@@ -148,8 +146,6 @@ const IntroductionDisplay = props => {
         ''
       )}
       <div className="vads-u-margin-top--4">
-        {t('expiration-date', { date: subDays(appointmentsDateTime, 1) })}
-        <br />
         <a
           href="#privacy-modal"
           onClick={useCallback(
