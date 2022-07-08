1be0a944b0ea3747572fbfb8d51a142334f67356
Bethany Dunfield <bethany.dunfield@civicactions.com>
2022-07-08 12:41:41 -0600

    fix cypress test

--- src/applications/check-in/day-of/tests/e2e/pages/Error.js
+++ src/applications/check-in/day-of/tests/e2e/pages/Error.js
@@ -6,16 +6,21 @@ const messages = {
       'We’re sorry. Something went wrong on our end. Check in with a staff member.',
     es:
       'Lo sentimos. Algo no funcionó de nuestra parte. Regístrese con un miembro del personal.',
+    tl:
+      'Paumanhin. May nangyaring mali sa aming panig. Mag-check in sa isang staff member.',
   },
   messageTextLastValidateAttempt: {
     en:
-      'We’re sorry. We couldn’t match your information to our records. Please ask a staff member for help.',
+      'We’re sorry. We couldån’t match your information to our records. Please ask a staff member for help.',
     es:
       'Lo sentimos. No encontramos información en nuestros archivos que corresponda a esta información. Pídale asistencia a un miembro del personal.',
+    tl:
+      'Paumanhin. Hindi namin maitugma ang iyong impormasyon sa aming mga rekord. Mangyaring humingi ng tulong sa isang staff member.',
   },
   couldntCheckIn: {
     en: 'We couldn’t check you in',
     es: 'No pudimos completar el registro.',
+    tl: 'Hindi ka namin mai-check in',
   },
 };
 class Error {
