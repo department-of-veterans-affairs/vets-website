d519ef758882b119b41928106c1a90f3f7fc33c0
Brian Seek <brian.seek@civicactions.com>
2022-07-11 11:34:37 -0700

    Moved event recorder to ExternalLink component.

--- src/applications/check-in/components/ExternalLink.jsx
+++ src/applications/check-in/components/ExternalLink.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
+import recordEvent from 'platform/monitoring/record-event';
 
-function ExternalLink({ children, href, hrefLang, onClick = null }) {
+import { createAnalyticsSlug } from '../utils/analytics';
+
+function ExternalLink({ children, href, hrefLang, eventId = null }) {
   const { t, i18n } = useTranslation();
+
+  const handleClick = useCallback(
+    () => {
+      recordEvent({
+        event: createAnalyticsSlug(eventId),
+      });
+    },
+    [eventId],
+  );
+
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-    <a {...{ href, hrefLang, onClick }}>
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
+    <a {...{ href, hrefLang }} onClick={eventId ? handleClick : null}>
       {children}
       {!i18n?.language.startsWith(hrefLang) ? (
         <> ({t(`in-${hrefLang}`)})</>
@@ -17,9 +30,9 @@ function ExternalLink({ children, href, hrefLang, onClick = null }) {
 
 ExternalLink.propTypes = {
   children: PropTypes.node,
+  eventId: PropTypes.string,
   href: PropTypes.string,
   hrefLang: PropTypes.string,
-  onClick: PropTypes.func,
 };
 
 export default ExternalLink;
