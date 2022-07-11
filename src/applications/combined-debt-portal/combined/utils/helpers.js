3d6e6b94a1e147ea3504b499ca92f4f3bc6ef1fe
Andrew Rodiek <andrew.rodiek@gmail.com>
2022-07-11 15:53:13 -0400

    [CDP] Updating setfocus for each page (#21614)

--- src/applications/combined-debt-portal/combined/utils/helpers.js
+++ src/applications/combined-debt-portal/combined/utils/helpers.js
@@ -110,3 +110,14 @@ export const transform = data => {
     };
   });
 };
+
+export const setPageFocus = selector => {
+  const el = document.querySelector(selector);
+  if (el) {
+    el.setAttribute('tabIndex', -1);
+    el.focus();
+  } else {
+    document.querySelector('#main h1').setAttribute('tabIndex', -1);
+    document.querySelector('#main h1').focus();
+  }
+};
