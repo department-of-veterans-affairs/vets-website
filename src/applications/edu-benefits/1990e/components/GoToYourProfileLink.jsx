466bec26e009f8eb0dbe09c0cec0459bfa4628b0
Cesar Santiago <cesaresantiago@users.noreply.github.com>
2022-07-12 18:36:47 -0400

    Revert "Revert "Revert Toe Changes to 1990E Form (#21611)" (#21638)" (#21650)
    This reverts commit 1482e578178255d82c171d6dcaffbdadb687ea06.

--- src/applications/edu-benefits/1990e/components/GoToYourProfileLink.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { YOUR_PROFILE_URL } from '../constants';
-
-export default function GoToYourProfileLink() {
-  return <a href={YOUR_PROFILE_URL}>Go to your profile</a>;
-}
