5dc74211fe315b303cdc4d5e9f0e9221bb6eadf1
Ryan Shaw <ryan.shaw@adhocteam.us>
2022-07-22 12:01:40 -0700

    Changed detail button to link element and added e.preventDefault
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/appointment-list/components/AppointmentsPageV2/AppointmentListItem.jsx
+++ src/applications/vaos/appointment-list/components/AppointmentsPageV2/AppointmentListItem.jsx
@@ -162,14 +162,14 @@ export default function AppointmentListItem({ appointment, facility }) {
           )}
         </div>
         <div className="vads-u-flex--auto vads-u-padding-top--0p5 medium-screen:vads-u-padding-top--0 vaos-hide-for-print">
-          <button
-            type="button"
-            role="link"
-            className="va-button-link vaos-appts__focus--hide-outline"
+          <Link
+            className="vaos-appts__focus--hide-outline"
             aria-label={label}
+            to={link}
+            onClick={e => e.preventDefault()}
           >
             Details
-          </button>
+          </Link>
 
           <i
             aria-hidden="true"
