363a1399de68cbeba31963e04f95cd58366ecdad
OptionSelect <brandyn.sullins@gmail.com>
2022-07-26 15:17:22 -0400

    Now conditionally rendering new and old experience based on bool, working with Andrew to use feature toggle for this

--- src/applications/financial-status-report/pages/assets/recreationalVehicles/records.js
+++ src/applications/financial-status-report/pages/assets/recreationalVehicles/records.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import ItemLoop from '../../../components/ItemLoop';
+import CardDetailsView from '../../../components/CardDetailsView';
+import CustomReviewField from '../../../components/CustomReviewField';
 import { validateCurrency } from '../../../utils/validations';
+import Typeahead from '../../../components/Typeahead';
+import {
+  formatOptions,
+  recreationalVehicleTypes,
+} from '../../../constants/typeaheadOptions';
+
+const showCombinedFSRExperience = true;
 
 const RecVehicleInfo = (
   <va-additional-info trigger="What if I don’t know the estimated value of my trailer, camper, or boat?">
@@ -14,53 +24,144 @@ const RecVehicleInfo = (
   </va-additional-info>
 );
 
-export const uiSchema = {
-  'ui:title': () => (
-    <>
-      <legend className="schemaform-block-title">
-        Your trailers, campers, and boats
-      </legend>
-    </>
-  ),
-  assets: {
-    recVehicleAmount: {
-      'ui:title':
-        'What is the estimated value of your trailers, campers, and boats?',
-      'ui:options': {
-        classNames: 'schemaform-currency-input',
-        widgetClassNames: 'input-size-4 vads-u-margin-bottom--3',
-      },
-      'ui:required': () => true,
-      'ui:errorMessages': {
-        required: 'Please enter the estimated value.',
-      },
-      'ui:validations': [validateCurrency],
-    },
-  },
-  'view:components': {
-    'view:recVehicleInfo': {
-      'ui:description': RecVehicleInfo,
-    },
-  },
-};
-
-export const schema = {
-  type: 'object',
-  properties: {
-    assets: {
-      type: 'object',
-      properties: {
-        recVehicleAmount: { type: 'string' },
-      },
-    },
-    'view:components': {
-      type: 'object',
-      properties: {
-        'view:recVehicleInfo': {
-          type: 'object',
-          properties: {},
+export const uiSchema = showCombinedFSRExperience
+  ? {
+      'ui:title': () => (
+        <>
+          <legend className="schemaform-block-title">
+            Your trailers, campers, and boats
+          </legend>
+        </>
+      ),
+      assets: {
+        recVehicleAmount: {
+          'ui:title':
+            'What is the estimated value of your trailers, campers, and boats?',
+          'ui:options': {
+            classNames: 'schemaform-currency-input',
+            widgetClassNames: 'input-size-4 vads-u-margin-bottom--3',
+          },
+          'ui:required': () => true,
+          'ui:errorMessages': {
+            required: 'Please enter the estimated value.',
+          },
+          'ui:validations': [validateCurrency],
         },
       },
-    },
-  },
-};
+      'view:components': {
+        'view:recVehicleInfo': {
+          'ui:description': RecVehicleInfo,
+        },
+      },
+    }
+  : {
+      'ui:title': () => (
+        <>
+          <legend className="schemaform-block-title">
+            Your trailers, campers, and boats
+          </legend>
+          <p className="vads-u-padding-top--2">
+            Enter each of your trailers, campers, and boats separately below.
+          </p>
+        </>
+      ),
+      assets: {
+        recVehicles: {
+          'ui:field': ItemLoop,
+          'ui:options': {
+            viewField: CardDetailsView,
+            doNotScroll: true,
+            itemName: 'trailer, camper, or boat',
+            keepInPageOnReview: true,
+          },
+          items: {
+            recVehicleType: {
+              'ui:title': 'Type of vehicle',
+              'ui:field': Typeahead,
+              'ui:reviewField': CustomReviewField,
+              'ui:options': {
+                idPrefix: 'rec_vehicles',
+                classNames:
+                  'input-size-6 vads-u-margin-top--3 vads-u-margin-bottom--3',
+                getOptions: () => formatOptions(recreationalVehicleTypes),
+              },
+              'ui:errorMessages': {
+                required: 'Please enter the type of vehicle.',
+              },
+            },
+            recVehicleAmount: {
+              'ui:title': 'Estimated value',
+              'ui:options': {
+                classNames: 'schemaform-currency-input',
+                widgetClassNames: 'input-size-4 vads-u-margin-bottom--3',
+              },
+              'ui:errorMessages': {
+                required: 'Please enter the estimated value.',
+              },
+              'ui:validations': [validateCurrency],
+            },
+          },
+        },
+      },
+      'view:components': {
+        'view:recVehicleInfo': {
+          'ui:description': RecVehicleInfo,
+        },
+      },
+    };
+
+export const schema = showCombinedFSRExperience
+  ? {
+      type: 'object',
+      properties: {
+        assets: {
+          type: 'object',
+          properties: {
+            recVehicleAmount: { type: 'string' },
+          },
+        },
+        'view:components': {
+          type: 'object',
+          properties: {
+            'view:recVehicleInfo': {
+              type: 'object',
+              properties: {},
+            },
+          },
+        },
+      },
+    }
+  : {
+      type: 'object',
+      properties: {
+        assets: {
+          type: 'object',
+          properties: {
+            recVehicles: {
+              type: 'array',
+              items: {
+                type: 'object',
+                required: ['recVehicleType', 'recVehicleAmount'],
+                properties: {
+                  recVehicleType: {
+                    type: 'string',
+                  },
+                  recVehicleAmount: {
+                    type: 'string',
+                  },
+                },
+              },
+            },
+          },
+        },
+        'view:components': {
+          type: 'object',
+          properties: {
+            'view:recVehicleInfo': {
+              type: 'object',
+              properties: {},
+            },
+          },
+        },
+      },
+    };
