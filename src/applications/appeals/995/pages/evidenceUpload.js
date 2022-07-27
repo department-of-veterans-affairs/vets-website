dad35c4aa2d17fa97ec63a2573290fc507e0f2c4
Robin Garrison <robin.garrison@adhocteam.us>
2022-07-27 15:12:05 -0500

    Add other uploads page

--- src/applications/appeals/995/pages/evidenceUpload.js
+++ src/applications/appeals/995/pages/evidenceUpload.js
@@ -1,8 +1,39 @@
+import {
+  UploadDescription,
+  evidenceOtherText,
+} from '../content/evidenceUpload';
+import { ancillaryFormUploadUi } from '../utils/upload';
+import { ATTACHMENTS_OTHER } from '../constants';
+
 export default {
-  uiSchema: {},
+  uiSchema: {
+    additionalDocuments: {
+      ...ancillaryFormUploadUi(evidenceOtherText),
+      'ui:description': UploadDescription,
+    },
+  },
 
   schema: {
     type: 'object',
-    properties: {},
+    required: ['additionalDocuments'],
+    properties: {
+      additionalDocuments: {
+        type: 'array',
+        items: {
+          type: 'object',
+          required: ['name', 'attachmentId'],
+          properties: {
+            name: {
+              type: 'string',
+            },
+            attachmentId: {
+              type: 'string',
+              enum: Object.keys(ATTACHMENTS_OTHER),
+              enumNames: Object.values(ATTACHMENTS_OTHER),
+            },
+          },
+        },
+      },
+    },
   },
 };
