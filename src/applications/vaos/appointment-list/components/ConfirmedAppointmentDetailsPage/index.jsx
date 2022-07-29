5e8c22bd815aa0a8a5b53543f9a855f889bba58f
Ryan Shaw <ryan.shaw@adhocteam.us>
2022-07-29 06:58:01 -0700

    Add useEffect dependency
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/appointment-list/components/ConfirmedAppointmentDetailsPage/index.jsx
+++ src/applications/vaos/appointment-list/components/ConfirmedAppointmentDetailsPage/index.jsx
@@ -82,7 +82,7 @@ export default function ConfirmedAppointmentDetailsPage() {
         scrollAndFocus();
       }
     },
-    [appointmentDetailsStatus],
+    [appointmentDetailsStatus, appointment],
   );
 
   if (
