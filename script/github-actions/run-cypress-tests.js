6a9a94eae0739aedbbd3ccf507195b5f9ccc1790
Darius Dzien <dariusz.dzien@adhocteam.us>
2022-07-29 14:54:57 -0500

    WIP

--- script/github-actions/run-cypress-tests.js
+++ script/github-actions/run-cypress-tests.js
@@ -11,11 +11,7 @@ const batch = tests
   .slice(step * divider, (step + 1) * divider)
   .join(',');
 
-if (batch === '') {
-  process.exit(0);
-}
-
-if (tests.includes('all-claims.cypress.spec.js') && step === 11) {
+if (tests.join(',').includes('all-claims.cypress.spec.js') && step === 11) {
   const status = runCommandSync(
     `CYPRESS_EVERY_NTH_FRAME=1 yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --spec /__w/vets-website/vets-website/src/applications/disability-benefits/all-claims/tests/all-claims.cypress.spec.js --env app_url=${appUrl}`,
   );
@@ -32,7 +28,11 @@ if (batch.includes('all-claims.cypress.spec.js') && step !== 11) {
   process.exit(status);
 }
 
-const status = runCommandSync(
-  `CYPRESS_EVERY_NTH_FRAME=1 yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --spec '${batch}' --env app_url=${appUrl}`,
-);
-process.exit(status);
+if (batch !== '') {
+  const status = runCommandSync(
+    `CYPRESS_EVERY_NTH_FRAME=1 yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.json" --spec '${batch}' --env app_url=${appUrl}`,
+  );
+  process.exit(status);
+} else {
+  process.exit(0);
+}
