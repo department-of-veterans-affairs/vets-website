48eb29e234faab7b229f86ed1c65a2a840523b68
fer.boza <fernando.boza@accenturefederal.com>
2022-08-01 11:00:35 -0400

    Refactored to use ConfirmationResponses to sort the different outcomes.

--- src/applications/fry-dea/containers/ConfirmationPage.jsx
+++ src/applications/fry-dea/containers/ConfirmationPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import moment from 'moment';
 import { connect } from 'react-redux';
 
 import scrollToTop from 'platform/utilities/ui/scrollToTop';
 import { focusElement } from 'platform/utilities/ui';
+import PropTypes from 'prop-types';
+import { UnderReview } from '../components/ConfirmationResponses';
+
+function createConfirmationPage(form) {
+  const { submission, data } = form;
+  const { response } = submission;
+  let name = data.veteranFullName;
+  name = {
+    first: 'John',
+    middle: 'J',
+    last: 'Doe',
+    suffix: 'Sr',
+  };
+  return UnderReview(response, name);
+}
 
 export class ConfirmationPage extends React.Component {
   componentDidMount() {
@@ -12,45 +26,7 @@ export class ConfirmationPage extends React.Component {
   }
 
   render() {
-    const { submission, data } = this.props.form;
-    const { response } = submission;
-    const name = data.veteranFullName;
-
-    return (
-      <div>
-        <h3 className="confirmation-page-title">Claim received</h3>
-        <p>
-          We usually process claims within <strong>a week</strong>.
-        </p>
-        <p>
-          We may contact you for more information or documents.
-          <br />
-          <i>Please print this page for your records.</i>
-        </p>
-        <div className="inset">
-          <h4>
-            &#39;Fry/DEA — VA Education Benefits For Survivors And
-            Dependents&#39; Claim{' '}
-            <span className="additional">(Form 22-5490)</span>
-          </h4>
-          {name ? (
-            <span>
-              for {name.first} {name.middle} {name.last} {name.suffix}
-            </span>
-          ) : null}
-
-          {response ? (
-            <ul className="claim-list">
-              <li>
-                <strong>Date received</strong>
-                <br />
-                <span>{moment(response.timestamp).format('MMM D, YYYY')}</span>
-              </li>
-            </ul>
-          ) : null}
-        </div>
-      </div>
-    );
+    return createConfirmationPage(this.props.form);
   }
 }
 
@@ -59,5 +35,10 @@ function mapStateToProps(state) {
     form: state.form,
   };
 }
+
+// Comment this to work, need it for eslint for now.
+ConfirmationPage.prototype = {
+  form: PropTypes.object,
+};
 
 export default connect(mapStateToProps)(ConfirmationPage);
