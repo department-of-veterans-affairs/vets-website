d142aaf284d6d1e71bb284d396fa5d36509ffe66
Rodriguez Renteria <ROBERTO.RODRIGUEZRENTERIA@va.gov>
2022-08-02 13:19:58 -0400

    erased comment

--- src/applications/mhv/secure-messaging/tests/components/header.unit.spec.jsx
+++ src/applications/mhv/secure-messaging/tests/components/header.unit.spec.jsx
@@ -1,7 +1,6 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import Header from '../../components/Header';
-// import App from '../../containers/App';
 
 describe('Header', () => {
   it('renders without errors', () => {
