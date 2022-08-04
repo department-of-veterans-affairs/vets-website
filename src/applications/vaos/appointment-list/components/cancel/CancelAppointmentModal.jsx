baf79970724a4ee4eda09c9ece8019b62c12a8b0
Ryan Shaw <ryan.shaw@adhocteam.us>
2022-08-04 06:51:10 -0700

    Removed cancel modal conditionals and added proptypes

--- src/applications/vaos/appointment-list/components/cancel/CancelAppointmentModal.jsx
+++ src/applications/vaos/appointment-list/components/cancel/CancelAppointmentModal.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import CancelVideoAppointmentModal from './CancelVideoAppointmentModal';
-import CancelCommunityCareAppointmentModal from './CancelCommunityCareAppointmentModal';
 import CancelAppointmentFailedModal from './CancelAppointmentFailedModal';
 import CancelAppointmentSucceededModal from './CancelAppointmentSucceededModal';
 import CancelAppointmentConfirmationModal from './CancelAppointmentConfirmationModal';
-import CancelCernerAppointmentModal from './CancelCernerAppointmentModal';
 
-import {
-  FETCH_STATUS,
-  APPOINTMENT_TYPES,
-  APPOINTMENT_STATUS,
-} from '../../../utils/constants';
-import CancelCOVIDVaccineModal from './CancelCOVIDVaccineModal';
+import { FETCH_STATUS, APPOINTMENT_STATUS } from '../../../utils/constants';
 
 export default function CancelAppointmentModal(props) {
   const {
@@ -23,45 +16,10 @@ export default function CancelAppointmentModal(props) {
     onClose,
     onConfirm,
     facility,
-    isCerner,
   } = props;
 
   if (!showCancelModal) {
     return null;
-  }
-
-  if (appointmentToCancel.vaos.isCOVIDVaccine) {
-    return <CancelCOVIDVaccineModal onClose={onClose} facility={facility} />;
-  }
-
-  if (
-    appointmentToCancel.vaos?.isVideo &&
-    appointmentToCancel.status === APPOINTMENT_STATUS.booked
-  ) {
-    return (
-      <CancelVideoAppointmentModal onClose={onClose} facility={facility} />
-    );
-  }
-
-  if (
-    appointmentToCancel.vaos?.appointmentType ===
-    APPOINTMENT_TYPES.ccAppointment
-  ) {
-    return (
-      <CancelCommunityCareAppointmentModal
-        onClose={onClose}
-        appointment={appointmentToCancel}
-      />
-    );
-  }
-
-  if (isCerner) {
-    return (
-      <CancelCernerAppointmentModal
-        onClose={onClose}
-        status={cancelAppointmentStatus}
-      />
-    );
   }
 
   if (cancelAppointmentStatus === FETCH_STATUS.failed) {
@@ -102,3 +60,13 @@ export default function CancelAppointmentModal(props) {
 
   return null;
 }
+
+CancelAppointmentModal.propTypes = {
+  appointmentToCancel: PropTypes.object.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+  cancelAppointmentStatus: PropTypes.string,
+  cancelAppointmentStatusVaos400: PropTypes.string,
+  facility: PropTypes.string,
+  showCancelModal: PropTypes.bool,
+};
