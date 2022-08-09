cce500228cfea70f07e6c3528dc583a3a7b0a302
Saritha Rayannagari <saritha.rayannagari@accenturefederal.com>
2022-08-09 11:02:57 -0400

    resolved review comments

--- src/applications/fry-dea/components/ConfirmationResponses.jsx
+++ src/applications/fry-dea/components/ConfirmationResponses.jsx
@@ -14,8 +14,8 @@ const NeedHelpComponent = () => {
 
 const ConfirmationReceiptBox = ({ name }) => {
   return (
-    <div className="inset vads-u-margin-top--3 vads-u-padding-y--3">
-      <h3 className="vads-u-margin-top--0p5">
+    <div className="inset">
+      <h3 className="vads-u-font-size--h3">
         Application for VA education benefits (Form 22-5490)
       </h3>
       {name ? (
@@ -163,24 +163,32 @@ export function Approved(name) {
           status="success"
           visible
         >
-          <h2 id="track-your-status-on-mobile" slot="headline">
+          <h2 slot="headline">
             Congratulations, you’ve been approved for Survivors’ and Dependents’
             Educational Assistance
           </h2>
           <div>
-            <p className="vads-u-margin-y--0">
+            <p className="vads-u-margin-y--0 vads-u-margin-bottom--3">
               We reviewed your application and have determined that you’re
               entitled to educational benefits under Survivors’ and Dependents’
               Educational Assistance (DEA, Chapter 35). Your decision letter is
               now available. A physical copy will also be mailed to your mailing
               address.
             </p>
-            <va-link
-              download
-              filetype="PDF"
+            <a
               href="https://www.va.gov"
-              text="Download your decision letter"
-            />
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <i
+                className="fa fa-download vads-u-margin-right--1"
+                aria-hidden="true"
+              />
+              <span className="vads-u-margin-left--0p25 vads-u-font-weight--bold">
+                Download your decision letter (PDF)
+              </span>
+              .
+            </a>
           </div>
         </va-alert>
         <ConfirmationReceiptBox name={name} />
@@ -190,23 +198,21 @@ export function Approved(name) {
         <ul>
           <li>
             Download a copy of your decision letter. This can also be found at{' '}
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://va.gov/vso/"
-            >
-              Download your VA education letters.
+            <a rel="noopener noreferrer" target="_blank" href="/vso/">
+              Download your VA education letters
             </a>
+            .
           </li>
           <li>
             <a
               rel="noopener noreferrer"
               target="_blank"
-              href="https://www.va.gov/education/gi-bill-comparison-tool/"
+              href="/education/gi-bill-comparison-tool/"
             >
               Use our GI Bill Comparison Tool to help you decide which education
-              program and school are best for you.
+              program and school are best for you
             </a>
+            .
           </li>
           <li>
             Once you’ve selected a school or program, you may bring your
@@ -217,11 +223,12 @@ export function Approved(name) {
             <a
               rel="noopener noreferrer"
               target="_blank"
-              href="https://www.va.gov/?next=%2Fprofile"
+              href="/?next=%2Fprofile"
             >
               Review and/or update your direct deposit information on your
-              VA.gov profile.
+              VA.gov profile
             </a>
+            .
           </li>
           <li>
             <a
@@ -230,8 +237,9 @@ export function Approved(name) {
               href="https://benefits.va.gov/GIBILL/docs/GIBguideseries/ChooseYourEducationPathway.pdf"
             >
               Learn more about VA benefits and programs through the Building
-              Your Future with the GI Bill Series.
+              Your Future with the GI Bill Series
             </a>
+            .
           </li>
         </ul>
       </div>
