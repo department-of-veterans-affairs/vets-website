3fc8312afb9b40f96681364b07a27afdf50cd86d
Rodriguez Renteria <ROBERTO.RODRIGUEZRENTERIA@va.gov>
2022-08-09 13:02:39 -0400

    Finished styling the menu, just need it to not close when mobile breakpoint is hit

--- src/applications/mhv/secure-messaging/routes.jsx
+++ src/applications/mhv/secure-messaging/routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import App from './containers/App';
 import Compose from './containers/Compose';
 import MessageReply from './containers/MessageReply.jsx';
@@ -9,6 +10,7 @@ const routes = (
     <Route path="/" component={App} />
     <Route path="/compose" component={Compose} />
     <Route path="/reply" component={MessageReply} />
+    <Route path="/nav" component={Navigation} />
   </Switch>
 );
 
