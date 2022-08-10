d65e3af7961af309ef111a188d48cd1161355a32
nbuckafs <nathaniel.buck@accenturefederal.com>
2022-08-10 13:09:41 -0400

    Update Enrollment Verification URLs

--- src/applications/enrollment-verification/constants/index.js
+++ src/applications/enrollment-verification/constants/index.js
@@ -1,12 +1,11 @@
 import manifest from '../manifest.json';
 
 export const BASE_URL = `${manifest.rootUrl}/`;
-export const REVIEW_ENROLLMENTS_URL_SEGMENT =
-  'post-911-gi-bill-enrollment-verifications';
+export const REVIEW_ENROLLMENTS_URL_SEGMENT = 'enrollment-verifications';
 export const REVIEW_ENROLLMENTS_URL = `${BASE_URL}${REVIEW_ENROLLMENTS_URL_SEGMENT}/`;
 export const REVIEW_ENROLLMENTS_RELATIVE_URL = `/${REVIEW_ENROLLMENTS_URL_SEGMENT}/`;
 
-export const VERIFY_ENROLLMENTS_URL_SEGMENT = 'verify-all-enrollments';
+export const VERIFY_ENROLLMENTS_URL_SEGMENT = 'verify';
 export const VERIFY_ENROLLMENTS_URL = `${REVIEW_ENROLLMENTS_URL}${VERIFY_ENROLLMENTS_URL_SEGMENT}/`;
 export const VERIFY_ENROLLMENTS_RELATIVE_URL = `/${REVIEW_ENROLLMENTS_URL_SEGMENT}/${VERIFY_ENROLLMENTS_URL_SEGMENT}/`;
 
