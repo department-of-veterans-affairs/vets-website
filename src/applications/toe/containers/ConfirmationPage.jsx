d53aa2ab0ded783a9b1bc483d173658ab58f0774
nbuckafs <nathaniel.buck@accenturefederal.com>
2022-08-11 15:47:02 -0400

    Copy and Update Confirmation Pages

--- src/applications/toe/containers/ConfirmationPage.jsx
+++ src/applications/toe/containers/ConfirmationPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+// import ApprovedConfirmation from '../components/confirmation/ApprovedConfirmation';
+// import DeniedConfirmation from '../components/confirmation/DeniedConfirmation';
+import UnderReviewConfirmation from '../components/confirmation/UnderReviewConfirmation';
 
-export const ConfirmationPage = () => {
-  return <h1>ToE Confirmation Page</h1>;
-};
+// export const ConfirmationPage = () => <ApprovedConfirmation />;
+// export const ConfirmationPage = () => <DeniedConfirmation />;
+export const ConfirmationPage = () => <UnderReviewConfirmation />;
 
 export default connect()(ConfirmationPage);
