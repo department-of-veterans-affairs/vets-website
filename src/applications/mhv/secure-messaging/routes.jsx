20fdcebd2cb97f99b67ad2ae7e97540bfff2eb39
Rodriguez Renteria <ROBERTO.RODRIGUEZRENTERIA@va.gov>
2022-08-11 13:08:36 -0400

    Removed navigation from routes

--- src/applications/mhv/secure-messaging/routes.jsx
+++ src/applications/mhv/secure-messaging/routes.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
 import App from './containers/App';
 import Compose from './containers/Compose';
 import MessageReply from './containers/MessageReply.jsx';
@@ -10,7 +9,6 @@ const routes = (
     <Route path="/" component={App} />
     <Route path="/compose" component={Compose} />
     <Route path="/reply" component={MessageReply} />
-    <Route path="/nav" component={Navigation} />
   </Switch>
 );
 
