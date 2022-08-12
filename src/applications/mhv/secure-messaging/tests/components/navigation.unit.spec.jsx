d0df9d5763a65fe10efea1ffeabe43fcab2e2064
Rodriguez Renteria <ROBERTO.RODRIGUEZRENTERIA@va.gov>
2022-08-12 12:05:20 -0400

    changed the unit test to check for nav bar header

--- src/applications/mhv/secure-messaging/tests/components/navigation.unit.spec.jsx
+++ src/applications/mhv/secure-messaging/tests/components/navigation.unit.spec.jsx
@@ -1,9 +1,11 @@
-import { render } from '@testing-library/react';
+import { renderInReduxProvider } from 'platform/testing/unit/react-testing-library-helpers';
 import React from 'react';
 import Navigation from '../../components/Navigation';
 
 describe('Navigation mobile view', () => {
-  it('renders without errors', () => {
-    render(<Navigation />);
+  it('renders without crashing', () => {
+    const screen = renderInReduxProvider(<Navigation />);
+
+    screen.getByText('My Health');
   });
 });
