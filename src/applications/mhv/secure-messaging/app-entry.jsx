039129df77098bffe654959e396e3cea71b5bda0
Mike Moyer <michael.moyer@va.gov>
2022-08-12 10:19:42 -0400

    [#MHV-33533] Added conditionally displayed auth/unauth landing pages

--- src/applications/mhv/secure-messaging/app-entry.jsx
+++ src/applications/mhv/secure-messaging/app-entry.jsx
@@ -2,13 +2,13 @@ import 'platform/polyfills';
 import './sass/secure-messaging.scss';
 
 import startApp from 'platform/startup';
-
 import routes from './routes';
 import reducer from './reducers';
 import manifest from './manifest.json';
 
 startApp({
   url: manifest.rootUrl,
+  entryName: manifest.entryName,
   reducer,
   routes,
 });
---
+++ src/applications/mhv/secure-messaging/containers/LandingPageAuth.jsx
@@ -0,0 +1,73 @@
+/*
+The main application container. Responsibilities:
+- Gating the application behind a login prompt (<RequiredLoginView />)
+- Rendering various application states based on api call status
+
+Assumptions that may need to be addressed:
+- Assumes the only service required is MHV_AC
+- Currently doesn't include a DowntimeNotification, but may want to given its external dependencies.
+- Assumes the api call invoked by props.getAllMessages returns ALL messages. If pagination is handled by the server,
+then additional functionality will need to be added to account for this.
+*/
+
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+// import backendServices from 'platform/user/profile/constants/backendServices';
+// import RequiredLoginView from 'platform/user/authorization/components/RequiredLoginView';
+
+import { getAllMessages } from '../actions';
+import Header from '../components/Header';
+import InboxListView from '../components/InboxListView';
+
+const LandingPageAuth = () => {
+  const dispatch = useDispatch();
+  // const user = useSelector(state => state?.user);
+  const { isLoading, messages, error } = useSelector(
+    state => state?.allMessages,
+  );
+
+  // fire api call to retreive messages
+  useEffect(() => {
+    dispatch(getAllMessages());
+  }, []);
+
+  let content;
+  if (isLoading) {
+    content = (
+      <va-loading-indicator
+        message="Loading your secure messages..."
+        setFocus
+      />
+    );
+  } else if (error) {
+    content = (
+      <va-alert status="error" visible>
+        <h2 slot="headline">We’re sorry. Something went wrong on our end</h2>
+        <p>
+          You can’t view your secure messages because something went wrong on
+          our end. Please check back soon.
+        </p>
+      </va-alert>
+    );
+  } else {
+    content = (
+      <>
+        <Header />
+        <InboxListView messages={messages} />
+      </>
+    );
+  }
+
+  return (
+    // <RequiredLoginView
+    //   user={props.user}
+    //   serviceRequired={backendServices.MHV_AC}
+    // >
+    <div className="vads-l-grid-container">
+      <div className="vads-l-row">{content}</div>
+    </div>
+    // </RequiredLoginView>
+  );
+};
+
+export default LandingPageAuth;
