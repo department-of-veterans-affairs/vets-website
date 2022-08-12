c7470ebafc198e6abc46e1850fe62eda67f962e8
OptionSelect <brandyn.sullins@gmail.com>
2022-08-12 12:21:43 -0400

    Resolution options now check abehave with relative accuracy. working on styling etc

---
+++ src/applications/financial-status-report/components/ResolutionOptions.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { setData } from 'platform/forms-system/src/js/actions';
+import { deductionCodes } from '../../debt-letters/const/deduction-codes';
+
+const ResolutionOptions = ({ formContext }) => {
+  const dispatch = useDispatch();
+  const formData = useSelector(state => state.form.data);
+
+  const { selectedDebtsAndCopays = [] } = formData;
+  const currentDebt = selectedDebtsAndCopays[formContext.pagePerItemIndex];
+  const { deductionCode, benefitType } = currentDebt;
+
+  const onChange = ({ target }) => {
+    return dispatch(
+      setData({
+        ...formData,
+        selectedDebtsAndCopays: [
+          ...selectedDebtsAndCopays.slice(0, formContext.pagePerItemIndex),
+          {
+            ...currentDebt,
+            resolutionOption: target.value,
+          },
+          ...selectedDebtsAndCopays.slice(formContext.pagePerItemIndex + 1),
+        ],
+      }),
+    );
+  };
+
+  return (
+    <div>
+      <h3>
+        Debt {parseInt(formContext.pagePerItemIndex, 10) + 1} of{' '}
+        {selectedDebtsAndCopays.length}:{' '}
+        {deductionCodes[deductionCode] || benefitType}
+      </h3>
+      <div>
+        <input
+          type="radio"
+          checked={currentDebt.resolutionOption === 'waiver'}
+          name="resolution-option-waiver"
+          id="radio-waiver"
+          value="waiver"
+          onChange={onChange}
+        />
+        <label htmlFor="radio-waiver">
+          <span className="vads-u-display--block vads-u-font-weight--bold">
+            Debt forgiveness (waiver)
+          </span>
+          <span className="vads-u-display--block vads-u-font-size--sm">
+            If we accept your request, we will stop collection on and forgive
+            (or "waive") the debt.
+          </span>
+        </label>
+        <input
+          type="radio"
+          checked={currentDebt.resolutionOption === 'monthly'}
+          name="resolution-option-monthly"
+          id="radio-monthly"
+          value="monthly"
+          onChange={onChange}
+        />
+        <label htmlFor="radio-monthly">
+          <span className="vads-u-display--block vads-u-font-weight--bold">
+            Extended monthly payments
+          </span>
+          <span className="vads-u-display--block vads-u-font-size--sm">
+            If we accept your request, you can make smaller monthly payments for
+            up to 5 years with either monthly offsets or a monthly payment plan.
+          </span>
+        </label>
+        <input
+          type="radio"
+          checked={currentDebt.resolutionOption === 'compromise'}
+          name="resolution-option-compromise"
+          id="radio-compromise"
+          value="compromise"
+          onChange={onChange}
+        />
+        <label htmlFor="radio-compromise">
+          <span className="vads-u-display--block vads-u-font-weight--bold">
+            Compromise
+          </span>
+          <span className="vads-u-display--block vads-u-font-size--sm">
+            If you’re unable to pay the debt in full or make smaller monthly
+            payments, we can consider a smaller, one-time payment to resolve
+            your debt.
+          </span>
+        </label>
+      </div>
+    </div>
+  );
+};
+
+ResolutionOptions.propTypes = {
+  formContext: PropTypes.shape({
+    pagePerItemIndex: PropTypes.string.isRequired,
+  }),
+};
+
+export default ResolutionOptions;
--- src/applications/financial-status-report/pages/resolution/resolutionOption.js
+++ src/applications/financial-status-report/pages/resolution/resolutionOption.js
@@ -1,54 +1,11 @@
-import React from 'react';
+import ResolutionOptions from '../../components/ResolutionOptions';
 
 export const uiSchema = {
   selectedDebtsAndCopays: {
     items: {
-      'ui:title': 'DEBT X of Y: [DEBT NAME HERE]',
-      resolutionOption: {
-        'ui:title': (
-          <p>
-            Which repayment or relief option would you like for your{' '}
-            <strong>[DOLLAR] for [PLACE]</strong> debt for Post-9/11 GI Bill
-            tuition and fees?
-          </p>
-        ),
-        'ui:widget': 'radio',
-        'ui:options': {
-          labels: {
-            waiver: (
-              <span>
-                <p>
-                  <strong>Debt forgiveness (waiver)</strong>
-                  If we accept your request, we will stop collection on and
-                  forgive (or “waive”) the debt.
-                </p>
-              </span>
-            ),
-            payments: (
-              <span>
-                <p>
-                  <strong>Extended monthly payments</strong>
-                  If we accept your request, you can make smaller monthly
-                  payments for up to 5 years with either monthly offsets or a
-                  monthly payment plan.
-                </p>
-              </span>
-            ),
-            compromise: (
-              <span>
-                <p>
-                  <strong>Compromise</strong>
-                  If you’re unable to either pay the debt in full or make
-                  smaller monthly payments, we can consider a smaller, one-time
-                  payment to resolve your debt.
-                </p>
-              </span>
-            ),
-          },
-        },
-        'ui:errorMessages': {
-          required: 'You must select one of the following.',
-        },
+      'ui:title': ' ',
+      resolutionOptions: {
+        'ui:widget': ResolutionOptions,
       },
     },
   },
@@ -61,11 +18,10 @@ export const schema = {
       type: 'array',
       items: {
         type: 'object',
-        required: ['resolutionOption'],
+        required: ['resolutionOptions'],
         properties: {
-          resolutionOption: {
+          resolutionOptions: {
             type: 'string',
-            enum: ['waiver', 'payments', 'compromise'],
           },
         },
       },
