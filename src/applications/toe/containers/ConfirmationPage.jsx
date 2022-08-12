e388875e57b51eda5a0d931ab50d96a878077387
nbuckafs <nathaniel.buck@accenturefederal.com>
2022-08-12 16:02:08 -0400

    Add Confirmation Loading Page

--- src/applications/toe/containers/ConfirmationPage.jsx
+++ src/applications/toe/containers/ConfirmationPage.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import ApprovedConfirmation from '../components/confirmation/ApprovedConfirmation';
-// import DeniedConfirmation from '../components/confirmation/DeniedConfirmation';
+import PropTypes from 'prop-types';
+
+import ApprovedConfirmation from '../components/confirmation/ApprovedConfirmation';
+import DeniedConfirmation from '../components/confirmation/DeniedConfirmation';
 import UnderReviewConfirmation from '../components/confirmation/UnderReviewConfirmation';
 
-// export const ConfirmationPage = () => <ApprovedConfirmation />;
-// export const ConfirmationPage = () => <DeniedConfirmation />;
-export const ConfirmationPage = () => <UnderReviewConfirmation />;
+function ConfirmationPage({ confirmationResult }) {
+  switch (confirmationResult) {
+    case 'APPROVED': {
+      return <ApprovedConfirmation />;
+    }
+    case 'DENIED': {
+      return <DeniedConfirmation />;
+    }
+    case 'IN_PROGRESS':
+    case 'ERROR': {
+      return <UnderReviewConfirmation />;
+    }
+    default: {
+      return (
+        <va-loading-indicator
+          class="vads-u-margin-y--5"
+          label="Loading"
+          message="Loading your results..."
+          set-focus
+        />
+      );
+    }
+  }
+}
 
+ConfirmationPage.propTypes = {
+  confirmationResult: PropTypes.string,
+};
+
+// const mapStateToProps = state => ({
+//   claimStatus: state.data?.claimStatus,
+// });
+
+// export default connect(mapStateToProps)(ConfirmationPage);
 export default connect()(ConfirmationPage);
