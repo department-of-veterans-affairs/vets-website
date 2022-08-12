eaaa3e17bb7f2f46126e040aba7ce8b25df355fe
Robin Garrison <robin.garrison@adhocteam.us>
2022-08-12 16:39:20 -0500

    Restore 526 wizard

--- src/applications/disability-benefits/all-claims/routes.jsx
+++ src/applications/disability-benefits/all-claims/routes.jsx
@@ -2,16 +2,16 @@ import { createRoutesWithSaveInProgress } from 'platform/forms/save-in-progress/
 
 import Form526EZApp from './Form526EZApp';
 import formConfig from './config/form';
-// import WizardContainer from './containers/WizardContainer';
+import WizardContainer from './containers/WizardContainer';
 
 // 526 SubTask is incomplete - each nested page will need content and an h2,
 // otherwise working - see platform/forms/tests/sub-task/bdd-526.unit.spec.jsx
-import SubTaskContainer from './subtask/SubTaskContainer';
+// import SubTaskContainer from './subtask/SubTaskContainer';
 
 const routes = [
   {
     path: '/start',
-    component: SubTaskContainer,
+    component: WizardContainer, // SubTaskContainer,
   },
   {
     path: '/',
