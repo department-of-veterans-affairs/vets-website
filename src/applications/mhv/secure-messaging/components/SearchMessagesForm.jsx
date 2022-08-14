981275cad328af36ecd61d7e86866202af43d422
mattwrightva <matthew.wright8@va.gov>
2022-08-13 23:40:43 -0600

    MHV-33594: Search messages mobile and desktop view complete

--- src/applications/mhv/secure-messaging/components/SearchMessagesForm.jsx
+++ src/applications/mhv/secure-messaging/components/SearchMessagesForm.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const SearchMessagesForm = () => {
+const SearchMessagesForm = props => {
+  const { advancedSearchOpen } = props;
+
   return (
     <form className="search-messages-form">
       <va-text-input
@@ -8,7 +11,7 @@ const SearchMessagesForm = () => {
         name="keyword"
         onBlur={function noRefCheck() {}}
         onInput={function noRefCheck() {}}
-        class="searchKeyword"
+        class="textField"
       />
 
       <va-select
@@ -17,7 +20,7 @@ const SearchMessagesForm = () => {
         label="Search in"
         name="searchIn"
         value=""
-        class="searchFolder"
+        class="selectField"
       >
         <option value="all">All message folders</option>
         <option value="compose">compose</option>
@@ -26,12 +29,84 @@ const SearchMessagesForm = () => {
         <option value="deleted">deleted</option>
       </va-select>
 
-      <button type="button" className="search-messages-button">
-        <i className="fas fa-search" />
-        <span className="search-messages-button-text">Search</span>
-      </button>
+      {advancedSearchOpen && (
+        <section className="advanced-search-fields">
+          <va-text-input
+            label="Message ID"
+            name="messageId"
+            onBlur={function noRefCheck() {}}
+            onInput={function noRefCheck() {}}
+            class="textField"
+          />
+          <va-text-input
+            label="From"
+            name="from"
+            onBlur={function noRefCheck() {}}
+            onInput={function noRefCheck() {}}
+            class="textField"
+          />
+          <va-text-input
+            label="Subject"
+            name="subject"
+            onBlur={function noRefCheck() {}}
+            onInput={function noRefCheck() {}}
+            class="textField"
+          />
+          <va-select
+            // eslint-disable-next-line jsx-a11y/aria-props
+            aria-live-region-text="You selected"
+            label="Category"
+            name="category"
+            value=""
+            class="selectField"
+          >
+            <option value=" "> </option>
+            <option value="all">one</option>
+            <option value="compose">two</option>
+            <option value="drafts">three</option>
+          </va-select>
+
+          <va-select
+            // eslint-disable-next-line jsx-a11y/aria-props
+            aria-live-region-text="You selected"
+            label="Date range"
+            name="dateRange"
+            class="selectField"
+          >
+            <option value="all">Any</option>
+            <option value="all">Past week</option>
+            <option value="compose">Past month</option>
+            <option value="drafts">Past year</option>
+            <option value="drafts">All time</option>
+          </va-select>
+
+          <va-checkbox
+            label="&#x1F4CE; Includes attachment"
+            name="includesAttachment"
+            class="includes-attachment"
+          />
+        </section>
+      )}
+
+      {advancedSearchOpen ? (
+        <div className="advanced-search-actions">
+          <button type="submit">Advanced Search</button>
+          <button type="button" className="reset">
+            Reset Search
+          </button>
+        </div>
+      ) : (
+        <button type="submit" className="search-messages-button">
+          <i className="fas fa-search" />
+          <span className="search-messages-button-text">Search</span>
+        </button>
+      )}
     </form>
   );
+};
+
+SearchMessagesForm.propTypes = {
+  advancedSearchOpen: PropTypes.bool,
 };
 
 export default SearchMessagesForm;
--- src/applications/mhv/secure-messaging/containers/SearchMessages.jsx
+++ src/applications/mhv/secure-messaging/containers/SearchMessages.jsx
@@ -4,14 +4,35 @@ import Breadcrumbs from '../components/Breadcrumbs';
 import SearchForm from '../components/SearchMessagesForm';
 
 const Search = () => {
+  const queryParams = new URLSearchParams(window.location.search);
+  const advancedSearchOpen =
+    queryParams.get('advanced') && queryParams.get('advanced') === 'true';
+
+  const toggleAdvancedSearchHandler = () => {
+    window.location.search = 'advanced=true';
+  };
+
   return (
-    <div className="vads-l-grid-container">
+    <div className="vads-l-grid-container search-messages">
       <Breadcrumbs link="/search" pageName="Search" />
       <SectionGuideButton sectionName="Messages" />
-      <h1 className="page-title">Search Messages</h1>
-      <SearchForm />
-      <div className="advanced-search-option">
-        <a href="/advanced-search">Or try the advanced search.</a>
+
+      <div className="page-content">
+        <h1 className="page-title">
+          {advancedSearchOpen ? 'Advanced Search' : 'Search Messages'}
+        </h1>
+
+        <SearchForm advancedSearchOpen={advancedSearchOpen} />
+
+        {!advancedSearchOpen && (
+          <button
+            type="button"
+            className="link-button advanced-search-toggle"
+            onClick={toggleAdvancedSearchHandler}
+          >
+            Or try the advanced search.
+          </button>
+        )}
       </div>
     </div>
   );
