0b669d66f3f98cc77dd9510c40c1a06a16170342
Adam Freemer <adam.freemer@accenturefederal.com>
2022-08-15 09:33:50 -0700

    Update date

--- src/applications/edu-benefits/feedback-tool/containers/IntroductionPage.jsx
+++ src/applications/edu-benefits/feedback-tool/containers/IntroductionPage.jsx
@@ -106,7 +106,7 @@ class IntroductionPage extends React.Component {
             <OMBInfo
               resBurden={15}
               ombNumber="2900-0797"
-              expDate="01/31/2022"
+              expDate="08/31/2024"
             />
           ) : (
             <OMBInfo resBurden={15} ombNumber="2900-0797" expDate="08/31/2024">
