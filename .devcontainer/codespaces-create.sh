a9a3e4a7a2404b70041b2244904b812f45ff27fc
Tim Wright <timwright12@gmail.com>
2022-08-15 11:07:21 -0400

    Added vets-api into the mix

--- .devcontainer/codespaces-create.sh
+++ .devcontainer/codespaces-create.sh
@@ -10,16 +10,22 @@ nvm use --delete-prefix
 printf "\n\n##### Downloading content repo #####\n"
 yarn install-repos
 
+# Set up vets-api
+printf "\n\n##### Installing vets-api #####\n"
+set -e
+cd ../vets-api && make up
+
+
 # Build vets-website
 printf "\n\n##### Installing vets-website #####\n"
 set -e
-cd ../vets-website && yarn cache clean && yarn install --production=false --prefer-offline && yarn build -- --buildtype=localhost --api=https://dev-api.va.gov --host="${CODESPACE_NAME}-3001.githubpreview.dev/" --port=3001
+cd ../vets-website && yarn cache clean && yarn install --production=false --prefer-offline && yarn build -- --buildtype=localhost --host="${CODESPACE_NAME}-3001.githubpreview.dev/" --port=3001
 
 if [[ "${VETS_WEBSITE_BUILD_CONTENT}" != "NO" ]]
 then
   # Build content-build and serve site
   printf "\n\n##### Installing content-build #####\n"
-  cd ../content-build && cp .env.example .env && yarn cache clean && yarn install --production=false --prefer-offline && yarn fetch-drupal-cache && yarn build -- --buildtype=localhost --api=https://dev-api.va.gov --host="${CODESPACE_NAME}-3002.githubpreview.dev/" --port=3002 --apps-directory-name=vets-website && && npx http-server ./build/localhost --port 3002
+  cd ../content-build && cp .env.example .env && yarn cache clean && yarn install --production=false --prefer-offline && yarn fetch-drupal-cache && yarn build -- --buildtype=localhost --host="${CODESPACE_NAME}-3002.githubpreview.dev/" --port=3002 --apps-directory-name=vets-website && npx http-server ./build/localhost --port 3002
 fi
 
 printf "\n\n##### Your codespace has been created! #####\n"
