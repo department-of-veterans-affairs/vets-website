d3655a4c9cf83907185c5aa7f33fea01c2b26ca7
Chris Alexander <chris.alexander@adhocteam.us>
2022-08-18 13:57:06 -0600

    Test allowlist app importing from non-allowlist app

--- src/applications/burials/components/ApplicantDescription.jsx
+++ src/applications/burials/components/ApplicantDescription.jsx
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 
 import PrefillMessage from 'platform/forms/save-in-progress/PrefillMessage';
 
+import stuff from '../../appeals/10182/components/AddIssue';
+
 const ApplicantDescription = ({ formContext }) => (
   <div>
+    {stuff}
     <p>
       As the person claiming burial benefits, you’ll need to provide some
       information. You don’t need to fill in all the fields, but we can review
