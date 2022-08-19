55ca573f3c4a45ab6239bab7d2857d23839bac86
Alex Garcia <67602137+AGarcia-Clarity@users.noreply.github.com>
2022-08-19 14:45:10 -0700

    Verify bug app (#21942)
    * fixes bug allowing verified users to verify again
    
    * updates unit tests accordingly
    
    * updates EmailAddressNotification component + test
    
    * updates the VerifyApp

--- src/applications/verify/containers/VerifyApp.jsx
+++ src/applications/verify/containers/VerifyApp.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import URLSearchParams from 'url-search-params';
 import PropTypes from 'prop-types';
+import { selectProfile } from 'platform/user/selectors';
 
-import LoginGovSVG from 'platform/user/authentication/components/LoginGovSVG';
 import LoadingIndicator from '@department-of-veterans-affairs/component-library/LoadingIndicator';
 import recordEvent from 'platform/monitoring/record-event';
 
@@ -13,33 +12,10 @@ import { SERVICE_PROVIDERS } from 'platform/user/authentication/constants';
 import { focusElement } from '~/platform/utilities/ui';
 import { VerifyButton } from '../components/verifyButton';
 
+const selectCSP = selectedPolicy =>
+  Object.values(SERVICE_PROVIDERS).find(csp => csp.policy === selectedPolicy);
+
 export class VerifyApp extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.renderOpts = [
-      {
-        copy: 'Login.gov',
-        renderImage: <LoginGovSVG />,
-        className: `logingov-button`,
-        policy: 'logingov',
-      },
-      {
-        copy: 'ID.me',
-        renderImage: (
-          <img
-            role="presentation"
-            aria-hidden="true"
-            alt="ID.me"
-            src="/img/signin/idme-icon-white.svg"
-          />
-        ),
-        className: `idme-button`,
-        policy: 'idme',
-      },
-    ];
-  }
-
   componentDidMount() {
     if (!hasSession()) {
       window.location.replace('/');
@@ -49,82 +25,59 @@ export class VerifyApp extends React.Component {
   }
 
   componentDidUpdate(prevProps) {
-    const { verified } = this.props.profile;
-    const shouldCheckAccount = prevProps.profile.verified !== verified;
-    if (shouldCheckAccount) {
-      this.checkAccountAccess();
+    if (this.props.profile.verified) {
+      window.location.replace('/');
     }
+
     if (!this.props.profile.loading && prevProps.profile.loading) {
       focusElement('h1');
     }
   }
 
-  checkAccountAccess() {
-    if (this.props.profile.verified) {
-      const nextParams = new URLSearchParams(window.location.search);
-      const nextPath = nextParams.get('next');
-      if (nextPath && nextPath !== 'loginModal') {
-        window.location.replace(nextPath || '/');
-      }
-    }
-  }
-
-  verifyButton(signInMethod) {
-    const { idme, logingov } = SERVICE_PROVIDERS;
-    if ([idme.label, logingov.label].includes(signInMethod)) {
-      const selectCSP = signInCSP =>
-        this.renderOpts.find(csp => csp.copy === signInCSP);
-      const verifyButtonProps = selectCSP(signInMethod);
-      return <VerifyButton {...verifyButtonProps} />;
-    }
-    return this.renderOpts.map(props => (
-      <VerifyButton key={props.policy} {...props} />
-    ));
-  }
-
   render() {
     const { profile } = this.props;
-
-    const signInMethod = profile.loading
-      ? null
-      : SERVICE_PROVIDERS[profile.signIn.serviceName].label;
 
     if (profile.loading) {
       return <LoadingIndicator message="Loading the application..." />;
     }
+
+    const { idme, logingov } = SERVICE_PROVIDERS;
+    const signInMethod = !profile.loading && profile.signIn.serviceName;
+
     return (
-      <main className="verify">
+      <section className="verify">
         <div className="container">
           <div className="row">
-            <div className="columns small-12">
-              <div>
-                <h1>Verify your identity</h1>
-                <va-alert visible status="success">
-                  You signed in with {signInMethod}
-                </va-alert>
-                <p>
-                  We'll need to verify your identity so that you can securely
-                  access and manage your benefits.
-                  <br />
-                  <a
-                    href="/resources/privacy-and-security-on-vagov/#why-do-i-need-to-verify-my-ide"
-                    target="_blank"
-                  >
-                    Why does VA.gov verify identity?
-                  </a>
-                </p>
-                <p>
-                  This one-time process will take{' '}
-                  <strong>5 - 10 minutes</strong> to complete.
-                </p>
-                {this.verifyButton(signInMethod)}
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="columns small-12">
+            <div className="columns small-12 fed-warning--v2">
+              <h1>Verify your identity</h1>
+              <va-alert visible status="success">
+                You signed in with {SERVICE_PROVIDERS[signInMethod].label}
+              </va-alert>
+              <p>
+                We'll need to verify your identity so that you can securely
+                access and manage your benefits.
+                <br />
+                <a
+                  href="/resources/privacy-and-security-on-vagov/#why-do-i-need-to-verify-my-ide"
+                  target="_blank"
+                >
+                  Why does VA.gov verify identity?
+                </a>
+              </p>
+              <p>
+                This one-time process will take <strong>5 - 10 minutes</strong>{' '}
+                to complete.
+              </p>
+              {[idme.policy, logingov.policy].includes(signInMethod) ? (
+                <VerifyButton {...selectCSP(signInMethod)} />
+              ) : (
+                <>
+                  <VerifyButton {...selectCSP(logingov.policy)} />
+                  <VerifyButton {...selectCSP(idme.policy)} />
+                </>
+              )}
               <div className="help-info">
-                <h4>Having trouble verifying your identity?</h4>
+                <h2>Having trouble verifying your identity?</h2>
                 <p>
                   <a href="/resources/signing-in-to-vagov/" target="_blank">
                     Get answers to frequently asked questions
@@ -137,22 +90,17 @@ export class VerifyApp extends React.Component {
             </div>
           </div>
         </div>
-      </main>
+      </section>
     );
   }
 }
 
-const mapStateToProps = state => {
-  const userState = state.user;
-  return {
-    login: userState.login,
-    profile: userState.profile,
-  };
-};
+const mapStateToProps = state => ({
+  profile: selectProfile(state),
+});
 
 export default connect(mapStateToProps)(VerifyApp);
 
 VerifyApp.propTypes = {
   profile: PropTypes.object.isRequired,
-  login: PropTypes.object,
 };
