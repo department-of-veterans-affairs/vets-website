f687e4ed92d8698956d987af1024818483178b46
Mike Moyer <michael.moyer@va.gov>
2022-08-19 17:37:06 -0400

    Removed an outdated unit test

--- src/applications/mhv/secure-messaging/tests/components/header.unit.spec.jsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import Header from '../../components/Header';
-
-describe('Header', () => {
-  it('renders without errors', () => {
-    render(<Header />);
-  });
-});
