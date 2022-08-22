3227767fa5cc71bfe7b8d69e2c02d853025f2851
Tim Wright <timwright12@gmail.com>
2022-08-22 16:29:45 -0400

    Revert "Removed feature toggle calls from header and nav (#21989)"
    This reverts commit 3519a577917d031a9ea762189dd56011fe5821ff.

--- src/platform/site-wide/user-nav/index.js
+++ src/platform/site-wide/user-nav/index.js
@@ -13,6 +13,7 @@ import 'applications/search/components/SearchDropdown/SearchDropdownStyles.scss'
 import './sass/user-nav.scss';
 import startReactApp from '../../startup/react';
 import Main from './containers/Main';
+import { connectFeatureToggle } from 'platform/utilities/feature-toggles';
 
 /**
  * Sets up the login widget with the given store at login-root
@@ -20,6 +21,8 @@ import Main from './containers/Main';
  * @param {Redux.Store} store The common store used on the site
  */
 export default function startUserNavWidget(store) {
+  connectFeatureToggle(store.dispatch);
+
   startReactApp(
     <Provider store={store}>
       <Main />
