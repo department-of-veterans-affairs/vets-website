3e0dad3b1f18cc75cef21fe796374b1e6c1d5f36
Adam Whitlock <adam.whitlock@adhocteam.us>
2022-08-23 21:01:24 -0600

    Hide Payments notification pref via FF (#21973)
    * (feat) hide Payments notification pref via FF
    
    * (test) e2e tests FF payment notification settings
    
    * (test) update fixture for E2E testing
    
    * (test) update E2E with axe checks
    
    * (test) update unit tests for payment notifications
    
    * (chore) adding prop types for notifications

--- src/platform/utilities/feature-toggles/featureFlagNames.js
+++ src/platform/utilities/feature-toggles/featureFlagNames.js
@@ -83,6 +83,7 @@ export default Object.freeze({
   profileShowAddressChangeModal: 'profile_show_address_change_modal',
   profileShowBadAddressIndicator: 'profile_show_bad_address_indicator',
   profileShowPronounsAndSexualOrientation: 'profile_show_pronouns_and_sexual_orientation',
+  profileShowPaymentsNotificationSetting: 'profile_show_payments_notification_setting',
   profileUseVaosV2Api:'profile_use_vaos_v2_api',
   profileUseVAFSC: 'profile_use_vafsc',
   pwEhrCtaDrupalSourceOfTruth: 'pw_ehr_cta_drupal_source_of_truth',
