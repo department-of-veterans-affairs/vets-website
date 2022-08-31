11547180d4d5d29395c5ca1f238c24385c940c6a
Bethany Dunfield <bethany.dunfield@civicactions.com>
2022-08-31 12:23:20 -0600

    Make changes based on state dept feedback

--- src/applications/check-in/pre-check-in/pages/Introduction/IntroductionDisplay.jsx
+++ src/applications/check-in/pre-check-in/pages/Introduction/IntroductionDisplay.jsx
@@ -102,7 +102,7 @@ const IntroductionDisplay = props => {
             'if-you-need-to-talk-to-someone-right-away-or-need-emergency-care',
           )}
         </span>{' '}
-        call <va-telephone contact="911" />,{' '}
+        {t('call')} <va-telephone contact="911" />,{' '}
         <span className="vads-u-font-weight--bold">or</span>{' '}
         {t('call-the-veterans-crisis-hotline-at')}{' '}
         <va-telephone contact="988" /> {t('and-select-1')}
