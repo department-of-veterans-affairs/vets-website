e6a4cc05b7079b29bf4f59ae5c85cc2da9dc5cf8
OptionSelect <brandyn.sullins@gmail.com>
2022-09-01 13:50:04 -0400

    Dynamic rendering fixed based on page logic with Mark. Issue with display at point in time was misunderstanding position of ExpandingGroup

--- src/applications/financial-status-report/components/ResolutionOptions.jsx
+++ src/applications/financial-status-report/components/ResolutionOptions.jsx
@@ -78,9 +78,10 @@ const ResolutionOptions = ({ formContext }) => {
           <p>{resolutionErrorMessage}</p>
         </span>
       )}
-      <ExpandingGroup open={currentDebt.resolutionOption === 'waiver'}>
-        <div>
-          {isEditing && (
+      {!isEditing && <>SELECTED SPANS</>}
+      {isEditing && (
+        <ExpandingGroup open={currentDebt.resolutionOption === 'waiver'}>
+          <div>
             <input
               type="radio"
               checked={currentDebt.resolutionOption === 'waiver'}
@@ -90,19 +91,17 @@ const ResolutionOptions = ({ formContext }) => {
               className="vads-u-width--auto"
               onChange={onResolutionChange}
             />
-          )}
-          <label htmlFor="radio-waiver" className="vads-u-margin--0">
-            <span className="vads-u-display--block vads-u-font-weight--bold vads-u-margin-left--3 vads-u-margin-top--neg2p5">
-              Debt forgiveness (waiver)
-            </span>
-            <span className="vads-u-display--block vads-u-font-size--sm vads-u-margin-left--3">
-              If we accept your request, we will stop collection on and forgive
-              (or "waive") the debt.
-            </span>
-          </label>
-          {currentDebt.debtType !== 'COPAY' && (
-            <div>
-              {isEditing && (
+            <label htmlFor="radio-waiver" className="vads-u-margin--0">
+              <span className="vads-u-display--block vads-u-font-weight--bold vads-u-margin-left--3 vads-u-margin-top--neg2p5">
+                Debt forgiveness (waiver)
+              </span>
+              <span className="vads-u-display--block vads-u-font-size--sm vads-u-margin-left--3">
+                If we accept your request, we will stop collection on and
+                forgive (or "waive") the debt.
+              </span>
+            </label>
+            {currentDebt.debtType !== 'COPAY' && (
+              <div>
                 <input
                   type="radio"
                   checked={currentDebt.resolutionOption === 'monthly'}
@@ -112,20 +111,18 @@ const ResolutionOptions = ({ formContext }) => {
                   className="vads-u-width--auto"
                   onChange={onResolutionChange}
                 />
-              )}
-              <label htmlFor="radio-monthly">
-                <span className="vads-u-display--block vads-u-font-weight--bold vads-u-margin-left--3 vads-u-margin-top--neg2p5">
-                  Extended monthly payments
-                </span>
-                <span className="vads-u-display--block vads-u-font-size--sm vads-u-margin-left--3">
-                  If we accept your request, you can make smaller monthly
-                  payments for up to 5 years with either monthly offsets or a
-                  monthly payment plan.
-                </span>
-              </label>
-            </div>
-          )}
-          {isEditing && (
+                <label htmlFor="radio-monthly">
+                  <span className="vads-u-display--block vads-u-font-weight--bold vads-u-margin-left--3 vads-u-margin-top--neg2p5">
+                    Extended monthly payments
+                  </span>
+                  <span className="vads-u-display--block vads-u-font-size--sm vads-u-margin-left--3">
+                    If we accept your request, you can make smaller monthly
+                    payments for up to 5 years with either monthly offsets or a
+                    monthly payment plan.
+                  </span>
+                </label>
+              </div>
+            )}
             <input
               type="radio"
               checked={currentDebt.resolutionOption === 'compromise'}
@@ -135,29 +132,27 @@ const ResolutionOptions = ({ formContext }) => {
               className="vads-u-width--auto"
               onChange={onResolutionChange}
             />
-          )}
-          <label htmlFor="radio-compromise">
-            <span className="vads-u-display--block vads-u-font-weight--bold vads-u-margin-left--3 vads-u-margin-top--neg2p5">
-              Compromise
-            </span>
-            <span className="vads-u-display--block vads-u-font-size--sm vads-u-margin-left--3">
-              If you’re unable to pay the debt in full or make smaller monthly
-              payments, we can consider a smaller, one-time payment to resolve
-              your debt.
-            </span>
-          </label>
-        </div>
-        <div className={checkboxError ? 'error-line' : 'vads-u-margin-y--3'}>
-          {checkboxError && (
-            <span
-              className="vads-u-font-weight--bold vads-u-color--secondary-dark"
-              role="alert"
-            >
-              <span className="sr-only">Error</span>
-              <p>{checkboxErrorMessage}</p>
-            </span>
-          )}
-          {isEditing && (
+            <label htmlFor="radio-compromise">
+              <span className="vads-u-display--block vads-u-font-weight--bold vads-u-margin-left--3 vads-u-margin-top--neg2p5">
+                Compromise
+              </span>
+              <span className="vads-u-display--block vads-u-font-size--sm vads-u-margin-left--3">
+                If you’re unable to pay the debt in full or make smaller monthly
+                payments, we can consider a smaller, one-time payment to resolve
+                your debt.
+              </span>
+            </label>
+          </div>
+          <div className={checkboxError ? 'error-line' : 'vads-u-margin-y--3'}>
+            {checkboxError && (
+              <span
+                className="vads-u-font-weight--bold vads-u-color--secondary-dark"
+                role="alert"
+              >
+                <span className="sr-only">Error</span>
+                <p>{checkboxErrorMessage}</p>
+              </span>
+            )}
             <input
               name="request-help-with-copay"
               id={currentDebt.id}
@@ -166,18 +161,18 @@ const ResolutionOptions = ({ formContext }) => {
               className="vads-u-width--auto"
               onChange={onWaiverChecked}
             />
-          )}
-          <label className="vads-u-margin--0" htmlFor={currentDebt.id}>
-            <div className="vads-u-margin-left--4 vads-u-margin-top--neg3">
-              <p className="vads-u-margin--0">
-                By checking this box, I’m agreeing that I understand that
-                forgiveness of education debt will reduce any remaining
-                education benefit I may have.
-              </p>
-            </div>
-          </label>
-        </div>
-      </ExpandingGroup>
+            <label className="vads-u-margin--0" htmlFor={currentDebt.id}>
+              <div className="vads-u-margin-left--4 vads-u-margin-top--neg3">
+                <p className="vads-u-margin--0">
+                  By checking this box, I’m agreeing that I understand that
+                  forgiveness of education debt will reduce any remaining
+                  education benefit I may have.
+                </p>
+              </div>
+            </label>
+          </div>
+        </ExpandingGroup>
+      )}
     </div>
   );
 };
