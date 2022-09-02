d7bbe698c7b8b0a28f56eaebbc06383d3225d0fc
Mike Haley <mike.f.haley@gmail.com>
2022-09-02 08:29:49 -0400

    Create new VFS app: My app

---
+++ src/applications/my-app/routes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route } from 'react-router';
+import App from './containers/App.jsx';
+
+const routes = (
+  <Route path="/" component={App}>
+  </Route>
+);
+
+export default routes;
