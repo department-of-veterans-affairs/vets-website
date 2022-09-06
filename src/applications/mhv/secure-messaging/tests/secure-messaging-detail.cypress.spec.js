f2b297a08269ef58ff6f1f9e380a8f0b843351ec
Aaron Read <Aaron.Read@va.gov>
2022-09-06 14:09:55 -0500

    Update secure-messaging-detail.cypress.spec.js

--- src/applications/mhv/secure-messaging/tests/secure-messaging-detail.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/secure-messaging-detail.cypress.spec.js
@@ -12,28 +12,16 @@ beforeEach(() => {
 
 describe('Compose Message Test', function() {
   it('is test fine accessible', () => {
-    cy.visit('/secure-messages/');
+    cy.visit('my-health/secure-messages/');
     cy.injectAxe().axeCheck();
     cy.intercept('/v0/messaging/health/messages/*', mockMessage).as('message');
-    cy.wait('@message');
+    cy.wait('@featureToggle');
     // https://staging-api.va.gov/v0/messaging/health/messages/2339331
     // class="composeSelect hydrated"
-    cy.contains('General Enqury').click();
-    cy.wait('@featureToggle');
+    cy.contains('General Inquiry').click();
     cy.wait('@message');
+
+    // cy.wait('@message');
     // cy.get('[class="composeInput hydrated"]').get('[value="Doctor A"]').click();
-    cy.get('[class="composeSelect hydrated"]')
-      .shadow()
-      .find('[id="select"]')
-      .select('Doctor A');
-    cy.get('[id="categoryCovid"]').click();
-    cy.get('[class="composeInput hydrated"]')
-      .shadow()
-      .find('[id="inputField"]')
-      .type('Test Subject');
-    cy.get('[id="message"]').type('message Test');
-    cy.get('[class="send-button-bottom-text"]')
-      .contains('Send')
-      .click();
   });
 });
