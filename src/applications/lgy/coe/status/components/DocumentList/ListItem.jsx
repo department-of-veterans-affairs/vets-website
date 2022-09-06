af60328b03004d8d198f68f8eb16f59aa307eb9c
Robin Garrison <robin.garrison@adhocteam.us>
2022-09-06 09:49:38 -0500

    Add COE status unit tests

--- src/applications/lgy/coe/status/components/DocumentList/ListItem.jsx
+++ src/applications/lgy/coe/status/components/DocumentList/ListItem.jsx
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 import DownloadLink from '../../../shared/components/DownloadLink';
 
 const ListItem = ({ downloadUrl, downloadLinkLabel, sentDate, title }) => (
-  <div className="vads-u-border-bottom--1px vads-u-border-color--gray-lighter vads-u-margin-top--2p5 vads-u-padding-bottom--4">
+  <div className="coe-list-item vads-u-border-bottom--1px vads-u-border-color--gray-lighter vads-u-margin-top--2p5 vads-u-padding-bottom--4">
     <h3 className="vads-u-font-family--serif vads-u-margin--0">{title}</h3>
-    <p className="vads-u-margin-y--1p5">Date Sent: {sentDate}</p>
+    <p className="vads-u-margin-y--1p5">Date sent: {sentDate}</p>
     <DownloadLink href={downloadUrl} label={downloadLinkLabel} />
   </div>
 );
