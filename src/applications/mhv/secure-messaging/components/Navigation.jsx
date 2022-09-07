67775b601b1af30d97dacd6362f82e0a6cad14be
oleksii-morgun-va <87077843+oleksii-morgun-va@users.noreply.github.com>
2022-09-07 14:02:03 -0400

    [#MHV-35962] fix navigation menu + bar
    returning old code that wasn't merged in correctly after the initial merge from mhv-sm-main to main branch

--- src/applications/mhv/secure-messaging/components/Navigation.jsx
+++ src/applications/mhv/secure-messaging/components/Navigation.jsx
@@ -1,29 +1,87 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Navigation = () => (
-  <div className="side-menu-nav">
-    <ul>
-      <li>
-        <div className="nav-title">
-          <i className="medkit-icon fas fa-medkit" />
-          <h4>My Health</h4>
+const Navigation = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const [isNavigationOpen, setIsNavigationOpen] = useState(false);
+  function openNavigation() {
+    setIsNavigationOpen(true);
+  }
+
+  function closeNavigation() {
+    setIsNavigationOpen(false);
+  }
+
+  function checkScreenSize() {
+    if (window.innerWidth <= 481 && setIsMobile !== false) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+      setIsNavigationOpen(false);
+    }
+  }
+
+  function openNavigationBurgerButton() {
+    return isMobile ? (
+      <div className="va-btn-sidebarnav-trigger">
+        <div className="button-background" />
+        <div className="button-wrapper">
           <button
+            aria-controls="va-detailpage-sidebar"
+            onClick={openNavigation}
             type="button"
-            className="va-btn-close-icon"
-            aria-label="Close-this-menu"
-            aria-expanded="true"
-            aria-controls="a1"
-          />
+          >
+            <strong>In the Messages section</strong>
+            <i className="fas fa-bars" aria-hidden />
+          </button>
         </div>
-        <ul className="sidebar">
-          <li>
-            <div id="a1" className="side-nav-content" aria-hidden="false">
-              <ul className="usa-sidenav-list">
-                <li>
-                  <a className="list-parent" href="/my-health/secure-messages">
-                    Messages
-                  </a>
-                  <ul>
+      </div>
+    ) : (
+      <></>
+    );
+  }
+
+  useEffect(
+    () => {
+      checkScreenSize();
+    },
+    [isMobile],
+  );
+
+  window.addEventListener('resize', checkScreenSize);
+
+  return (
+    <>
+      {openNavigationBurgerButton()}
+      {(isNavigationOpen && isMobile) || isMobile === false ? (
+        <div className="sidebar-navigation">
+          <div className="sidebar-navigation-header">
+            <i className="medkit-icon fas fa-medkit" />
+            <h4>My Health</h4>
+            <button
+              className={
+                isMobile === true ? 'va-btn-close-icon' : 'no-close-btn'
+              }
+              aria-label="Close-this-menu"
+              aria-expanded="true"
+              aria-controls="a1"
+              onClick={closeNavigation}
+              type="button"
+            />
+          </div>
+          <div id="a1" className="sidebar-navigation-list" aria-hidden="false">
+            <ul className="usa-sidenav-list">
+              <li>
+                <a href="/my-health/secure-messages">Pharmacy</a>
+              </li>
+              <li>
+                <a href="/my-health/secure-messages">Appointments</a>
+              </li>
+              <li className="sidebar-navigation-messages-list">
+                <div className="sidebar-navigation-messages-list-header">
+                  <a href="/my-health/secure-messages">Messages</a>
+                </div>
+                <div className="sidebar-navigation-messages-list-menu">
+                  <ul className="usa-sidenav-list">
                     <li>
                       <a href="/my-health/secure-messages">Compose</a>
                     </li>
@@ -40,20 +98,36 @@ const Navigation = () => (
                       <a href="/my-health/secure-messages">Deleted</a>
                     </li>
                     <li>
-                      <a href="/my-health/secure-messages">Search Messages</a>
+                      <a href="/my-health/secure-messages">Search messages</a>
                     </li>
                     <li>
                       <a href="/my-health/secure-messages">Messages FAQ</a>
                     </li>
                   </ul>
-                </li>
-              </ul>
-            </div>
-          </li>
-        </ul>
-      </li>
-    </ul>
-  </div>
-);
+                </div>
+              </li>
+              <li>
+                <a href="/my-health/secure-messages">Medical records</a>
+              </li>
+              <li>
+                <a href="/my-health/secure-messages">VA health care benefits</a>
+              </li>
+              <li>
+                <a href="/my-health/secure-messages">
+                  Copay bills and travel pay
+                </a>
+              </li>
+              <li>
+                <a href="/my-health/secure-messages">Health resources</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
 
 export default Navigation;
