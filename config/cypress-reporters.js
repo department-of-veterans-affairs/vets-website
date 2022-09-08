c3d88f784cd14b1e8a894a448b3b0cf0cfbfaccf
Curt Bonade <cbonade@Curts-MBP.attlocal.net>
2022-09-08 15:24:28 -0400

    WIP

--- config/cypress-reporters.js
+++ config/cypress-reporters.js
@@ -1,18 +1,8 @@
-/* eslint-disable no-bitwise */
-function uuidv4() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-    (
-      c ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-    ).toString(16),
-  );
-}
-
 module.exports = {
   reporterEnabled: 'mochawesome',
   mochawesomeReporterOptions: {
     reportDir: 'cypress/results',
-    reportFileName: `${uuidv4()}`,
+    reportFileName: `${process.env.STEP}_${new Date()}`,
     overwrite: false,
     html: false,
     json: true,
