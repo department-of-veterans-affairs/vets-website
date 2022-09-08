953ab4cd3586c8a4fbf0d4872e988bea086dc6d8
Robin Garrison <robin.garrison@adhocteam.us>
2022-09-08 09:05:43 -0500

    Restore all reverted files

--- src/applications/lgy/coe/status/validations.js
+++ src/applications/lgy/coe/status/validations.js
@@ -1,15 +1,4 @@
 import { FILE_TYPES } from './constants';
 
-export const isNotBlank = value => {
-  return value !== '';
-};
-
 export const isValidFileType = file =>
   FILE_TYPES.some(type => file.name.toLowerCase().endsWith(type));
-
-export const validateIfDirty = (field, validator) => {
-  if (field.dirty) {
-    return validator(field.value);
-  }
-  return true;
-};
