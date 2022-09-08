48f11043679f33f9f9bfe31f19ee9f07a577dae2
Curt Bonade <cbonade@Curts-MBP.attlocal.net>
2022-09-08 11:41:51 -0400

    WIP

--- script/github-actions/select-cypress-tests.js
+++ script/github-actions/select-cypress-tests.js
@@ -253,15 +253,15 @@ function run() {
   const disallowedTests = ALLOW_LIST.filter(spec => spec.allowed === false).map(
     spec => spec.spec_path,
   );
-  let tests = selectTests(graph, pathsOfChangedFiles).filter(testPath => {
+  const tests = selectTests(graph, pathsOfChangedFiles).filter(testPath => {
     return !disallowedTests.includes(
       testPath.substring(testPath.indexOf('src/')),
     );
   });
 
-  tests = [
-    '/home/runner/work/vets-website/vets-website/src/applications/appeals/10182/tests/10182-keyboard-only.cypress.spec.js',
-  ];
+  // tests = [
+  //   '/home/runner/work/vets-website/vets-website/src/applications/appeals/10182/tests/10182-keyboard-only.cypress.spec.js',
+  // ];
 
   // console.log('tests: ', tests);
   exportVariables(tests);
