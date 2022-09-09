a6f79488ffd8215d45438c0e9b85285518431f98
Holden Hinkle <holden@holdenhinkle.com>
2022-09-09 12:05:14 -0400

    create new job in ci workflow to stress test e2e tests

--- script/github-actions/select-cypress-tests.js
+++ script/github-actions/select-cypress-tests.js
@@ -259,7 +259,6 @@ function run() {
 
   // groups of tests based on test selection and filtering the groups from the allow list
   const testsSelectedByTestSelection = selectTests(graph, CHANGED_FILE_PATHS);
-
   const newTests = testsSelectedByTestSelection.filter(
     test => !allAllowedTestPaths.includes(test.substring(test.indexOf('src/'))),
   );
