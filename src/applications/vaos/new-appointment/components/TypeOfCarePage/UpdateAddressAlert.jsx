e36a3129e3e9c1a8901980bbf6069772e00d64ed
Simi Adebowale <47654119+simiadebowale@users.noreply.github.com>
2022-09-12 19:19:29 -0400

    added prop validation

--- src/applications/vaos/new-appointment/components/TypeOfCarePage/UpdateAddressAlert.jsx
+++ src/applications/vaos/new-appointment/components/TypeOfCarePage/UpdateAddressAlert.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import NewTabAnchor from '../../../components/NewTabAnchor';
 import InfoAlert from '../../../components/InfoAlert';
 
 export default function UpdateAddressAlert({ onClickUpdateAddress }) {
   const headline =
     'To use some of the tool’s features, you need a home address on file';
+  const style = {
+    width: 'fit-content',
+    webkitWidth: 'fit-content',
+  };
 
   return (
     <InfoAlert
@@ -16,13 +21,18 @@ export default function UpdateAddressAlert({ onClickUpdateAddress }) {
         To update your address, go to your VA.gov profile. Please allow some
         time for your address update to process through our system. <br />
         <NewTabAnchor
+          style={style}
           href="/change-address/#how-do-i-change-my-address-in-"
           className="usa-button usa-button-primary vads-u-margin-top--4"
           onClick={() => onClickUpdateAddress(headline)}
         >
-          Update your address
+          Update your address!
         </NewTabAnchor>
       </p>
     </InfoAlert>
   );
 }
+
+UpdateAddressAlert.propTypes = {
+  onClickUpdateAddress: PropTypes.func,
+};
