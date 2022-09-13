a81c2225612cc3c7366f8acfd9b45bc2a65fb978
Nathaniel Buck <nathaniel.buck@accenturefederal.com>
2022-09-13 10:43:19 -0400

    My Education Benefits—Fix Intro Page Styling (#22141)

--- src/applications/my-education-benefits/selectors/selectors.js
+++ src/applications/my-education-benefits/selectors/selectors.js
@@ -12,6 +12,7 @@ export const getAppData = state => ({
   isEligibilityCallComplete: state.data?.eligibilityFetchComplete,
   isLOA1: isLOA1Selector(state),
   isLOA3: isLOA3Selector(state),
+  isLoggedIn: state?.user?.login?.currentlyLoggedIn,
   savedForms: state?.user?.profile?.savedForms,
   showUnverifiedUserAlert: !!toggleValues(state)[
     FEATURE_FLAG_NAMES.showMebUnverifiedUserAlert
