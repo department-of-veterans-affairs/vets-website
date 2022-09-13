bc13c402af40580332d12b3c40b4c45f6e9a2f4b
oleksii-morgun-va <87077843+oleksii-morgun-va@users.noreply.github.com>
2022-09-13 09:53:22 -0400

    [#MHV-35951] use va-telephone component on phone number links

--- src/applications/mhv/secure-messaging/components/EmergencyNote.jsx
+++ src/applications/mhv/secure-messaging/components/EmergencyNote.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
 
 const EmergencyNote = () => (
-  // <p className="vads-u-font-size--h3 vads-u-font-weight--normal vads-u-margin-top--0">
-
-  <p className="medium-screen:vads-u-margin-top--0">
-    <strong>Note:</strong> Call <a href="tel:911">911</a> if you have a medical
-    emergency. If you’re in crisis and need to talk with someone now, call the{' '}
-    <a href="tel:2027370522">Veterans Crisis Line</a>. To speak with a VA health
-    care team member right away, contact your local VA call center.
-  </p>
+  <section className="emergency-note">
+    <p className="medium-screen:vads-u-margin-top--0">
+      <strong>Note:</strong> Call <va-telephone contact="911" /> if you have a
+      medical emergency. If you’re in crisis and need to talk with someone now,
+      call the Veterans Crisis Line{' '}
+      <va-telephone contact={CONTACTS.CRISIS_LINE} />. To speak with a VA health
+      care team member right away, contact your local VA call center.
+    </p>
+  </section>
 );
 
 export default EmergencyNote;
