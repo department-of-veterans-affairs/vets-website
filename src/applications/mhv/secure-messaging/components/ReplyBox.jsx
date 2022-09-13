d25177444dee4cdbb905b1ca554fc193aa389a20
oleksii-morgun-va <87077843+oleksii-morgun-va@users.noreply.github.com>
2022-09-13 14:49:29 -0400

    adding how to attach file to reply

--- src/applications/mhv/secure-messaging/components/ReplyBox.jsx
+++ src/applications/mhv/secure-messaging/components/ReplyBox.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VaAdditionalInfo } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import HowToAttachFiles from './HowToAttachFiles';
 
 const ReplyBox = () => (
   <div className=" message-box vads-u-padding--0p25">
@@ -68,52 +68,7 @@ const ReplyBox = () => (
       </div>
 
       <div className="message-body">
-        <VaAdditionalInfo
-          trigger="How to attach a file"
-          disable-analytics={false}
-          disable-border={false}
-        >
-          <ul>
-            <li>
-              Select the 'Attach files' link on the message you are editing
-            </li>
-            <li>Select the file you would like to attach</li>
-            <li>Select the 'Attach' button</li>
-          </ul>
-          <p>
-            <strong>Attachment Requirements:</strong>
-          </p>
-          <ul>
-            <li>You may attach up to 4 files</li>
-            <li>
-              Files supported: doc, docx, gif, jpg, pdf, png, rtf, txt, xls,
-              xlsx
-            </li>
-            <li>
-              File size for a single attachment cannot exceed 6MB and the total
-              size of all attachments cannot exceed 10MB
-            </li>
-          </ul>
-          <p>
-            <strong>
-              Note: If you are unable to attach a document to a Secure Message:
-            </strong>
-          </p>
-          <ul>
-            <li>
-              Confirm that your document meets the requirements for size and
-              type
-            </li>
-            <li>
-              Check your browser settings to be sure JavaScript is enabled
-            </li>
-            <li>Use a browser such as Chrome or Firefox</li>
-            <li>
-              If your problem persists, please contact the My HealtheVet{' '}
-              <a href="tel:1-877-327-0022">Help Desk</a>
-            </li>
-          </ul>
-        </VaAdditionalInfo>
+        <HowToAttachFiles />
       </div>
 
       <div className="small-screen:vads-u-display--flex small-screen:vads-u-flex-direction--row-reverse small-screen:vads-u-justify-content--space-between">
