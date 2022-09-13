6c07190ee775ea7c5c7e80cebab632e2c4620135
oleksii-morgun-va <87077843+oleksii-morgun-va@users.noreply.github.com>
2022-09-13 16:05:20 -0400

    adding Veterans Crisis Line modal

--- src/applications/mhv/secure-messaging/components/EmergencyNote.jsx
+++ src/applications/mhv/secure-messaging/components/EmergencyNote.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
+import recordEvent from 'platform/monitoring/record-event';
+import { FOOTER_EVENTS } from 'platform/site-wide/va-footer/helpers';
 
 const EmergencyNote = () => (
   <section className="emergency-note">
     <p className="medium-screen:vads-u-margin-top--0">
       <strong>Note:</strong> Call <va-telephone contact="911" /> if you have a
       medical emergency. If you’re in crisis and need to talk with someone now,
-      call the Veterans Crisis Line{' '}
-      <va-telephone contact={CONTACTS.CRISIS_LINE} />. To speak with a VA health
-      care team member right away, contact your local VA call center.
+      call the{' '}
+      <button
+        onClick={() => recordEvent({ event: FOOTER_EVENTS.CRISIS_LINE })}
+        className="va-button-link va-overlay-trigger"
+        data-show="#modal-crisisline"
+        type="button"
+      >
+        Veterans Crisis Line
+      </button>
+      . To speak with a VA health care team member right away, contact your
+      local VA call center.
     </p>
   </section>
 );
