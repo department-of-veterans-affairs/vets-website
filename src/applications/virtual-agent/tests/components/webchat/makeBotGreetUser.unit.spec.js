d8a313ad5dccf217c453409bbef1554864706bfd
helene-mccarron <helene.mccarron@thoughtworks.com>
2022-09-15 16:13:14 -0500

    [#575] improve makebotgreetuser test coverage
        Co-authored-by: Aaron Young <aaron.young@thoughtworks.com>
        Co-authored-by: Helene Mccarron <helene.mccarron@thoughtworks.com>
    
    Write DIRECT_LINE/CONNECT_FULFILLED test

--- src/applications/virtual-agent/tests/components/webchat/makeBotGreetUser.unit.spec.js
+++ src/applications/virtual-agent/tests/components/webchat/makeBotGreetUser.unit.spec.js
@@ -1,10 +1,56 @@
-import {
-  processActionSendMessage,
-  processActionConnectFulfilled,
-} from '../../../components/webchat/makeBotGreetUser';
+// import {
+//   processActionSendMessage,
+//   processActionConnectFulfilled,
+//   processActionIncomingActivity,
+// } from '../../../components/webchat/makeBotGreetUser';
+import { expect } from 'chai';
+// import sinon from 'sinon';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import GreetUser from '../../../components/webchat/makeBotGreetUser';
 
 describe.only('makeBotGreetUser actions', () => {
-  it('should correctly handle "DIRECT_LINE/CONNECT_FULFILLED"', () => {});
+  // mock store
+  const middlewares = [thunk];
+  const mockStore = configureMockStore(middlewares);
+  let store;
+
+  beforeEach(() => {
+    store = mockStore(
+      {},
+      GreetUser.makeBotGreetUser(
+        'fakeCsrfToken',
+        'fakeApiSession',
+        'http://apiURL',
+        'http://baseURL',
+        'noFirstNameFound',
+        'fakeUserUuid',
+        true,
+      ),
+    );
+    // authActivityHandlerSpy.reset();
+    // messageActivityHandlerSpy.reset();
+    // sessionStorage.removeItem(IN_AUTH_EXP);
+    // sessionStorage.removeItem(LOGGED_IN_FLOW);
+  });
+
+  it('should correctly handle "DIRECT_LINE/CONNECT_FULFILLED"', () => {
+    // fire dispatch against mock store
+    store.dispatch({ type: 'DIRECT_LINE/CONNECT_FULFILLED' });
+
+    // check that the right action in the mock store has been invoked
+    const actions = store.getActions();
+    expect(actions.length).to.equal(1);
+    console.log(actions[0]);
+    expect(actions[0].payload.activity).to.own.include({
+      name: 'startConversation',
+      type: 'event',
+    });
+  });
+
+  // it('should correctly handle "DIRECT_LINE/CONNECT_FULFILLED" and is not "LOGGED_IN_FLOW"', () => {});
 
   it('should correctly handle "DIRECT_LINE/POST_ACTIVITY"', () => {});
+
+  it('should correctly handle "DIRECT_LINE/SEND_MESSAGE"', () => {});
 });
