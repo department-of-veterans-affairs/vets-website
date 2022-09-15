ae6272dfa92412a919fab4c96a53e8d97526b8ad
Theo Bentum <theo@adhocteam.us>
2022-09-14 23:43:20 -0700

    Make post create command more readable

--- .devcontainer/codespaces-create.sh
+++ .devcontainer/codespaces-create.sh
@@ -2,7 +2,7 @@
 
 # Build vets-website
 # printf "\n\n##### Installing vets-website #####\n"
-# set -e
+set -e
 # yarn cache clean && yarn install --production=false --prefer-offline && yarn build -- --host="${CODESPACE_NAME}-3001.githubpreview.dev/" --env api=${CODESPACE_NAME}-3000.githubpreview.dev/
 
 printf "\n\n##### Starting vets-api #####\n"
