c1a5aeb1c30b14c8941ddc2ad8ae9d245d1fb7d8
Theo Bentum <theo@adhocteam.us>
2022-09-15 15:20:16 -0700

    Remove saving log to file

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,10 +7,10 @@ set -e
 
 setup_vets_api() {
   # Copy settings file to vets-api directory
-  cp .devcontainer/vets-api/settings.local.yml ../vets-api/config/settings.local.yml
+  cp ./vets-website/.devcontainer/vets-api/settings.local.yml ./vets-api/config/settings.local.yml
 
   # Change directory to vets-api
-  cd ../vets-api
+  cd ./vets-api
 
   # Add codespace name to `virtual_hosts` in settings file
   sed -i "/^virtual_hosts*/a - $CODESPACE_NAME-3000.githubpreview.dev" config/settings.local.yml
