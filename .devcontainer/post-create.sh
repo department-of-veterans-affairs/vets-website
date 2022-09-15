d0ce357323bf1f95009039a1732347f216f08d56
Theo Bentum <theo@adhocteam.us>
2022-09-15 13:04:21 -0700

    Setup vets-api in function

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,21 +5,24 @@
 set -e
 # yarn cache clean && yarn install --production=false --prefer-offline && yarn build -- --host="${CODESPACE_NAME}-3001.githubpreview.dev/" --env api=${CODESPACE_NAME}-3000.githubpreview.dev/
 
+setup_vets_api() {
+  # Copy settings file to vets-api directory
+  cp .devcontainer/vets-api/settings.local.yml ../vets-api/config/settings.local.yml
+
+  # Change directory to vets-api
+  cd ../vets-api
+
+  # Add codespace name to `virtual_hosts` in settings file
+  sed -i "/^virtual_hosts*/a - $CODESPACE_NAME-3000.githubpreview.dev" config/settings.local.yml
+
+  # Setup key & cert for localhost authentication to ID.me
+  mkdir config/certs
+  touch config/certs/vetsgov-localhost.crt
+  touch config/certs/vetsgov-localhost.key
+
+  # Start vets-api server and associated services
+  make up
+}
+
 printf "\n\n##### Starting vets-api #####\n"
-
-# Copy settings file to vets-api directory
-cp .devcontainer/vets-api/settings.local.yml ../vets-api/config/settings.local.yml
-
-# Change directory to vets-api
-cd ../vets-api
-
-# Add codespace name to `virtual_hosts` in settings file
-sed -i "/^virtual_hosts*/a - $CODESPACE_NAME-3000.githubpreview.dev" config/settings.local.yml
-
-# Setup key & cert for localhost authentication to ID.me
-mkdir config/certs
-touch config/certs/vetsgov-localhost.crt
-touch config/certs/vetsgov-localhost.key
-
-# Start vets-api server and associated services
-make up
+setup_vets_api
