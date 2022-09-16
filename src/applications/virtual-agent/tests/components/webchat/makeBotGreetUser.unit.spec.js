d0db507e6237f426f41a27117df0a36a3d08cca6
Aaron Young <aaron.young@thoughtworks.com>
2022-09-16 11:27:15 -0400

    [#585] Pass pairing
       Co-authored-by: Aaron Young <aaron.young@thoughtworks.com>
       Co-authored-by: Helene Mccarron <helene.mccarron@thoughtworks.com>
    
    Description goes here

--- src/applications/virtual-agent/tests/components/webchat/makeBotGreetUser.unit.spec.js
+++ src/applications/virtual-agent/tests/components/webchat/makeBotGreetUser.unit.spec.js
@@ -13,60 +13,41 @@ describe.only('makeBotGreetUser actions', () => {
   // mock store
   const middlewares = [thunk];
   const mockStore = configureMockStore(middlewares);
-  let store;
+  
   let fakeNext;
   beforeEach(() => {
     fakeNext = sinon.stub();
-    store = mockStore(
-      {},
-      GreetUser.makeBotGreetUser(
-        'fakeCsrfToken',
-        'fakeApiSession',
-        'http://apiURL',
-        'http://baseURL',
-        'noFirstNameFound',
-        'fakeUserUuid',
-        true,
-      ),
-    );
     // fake next
     // authActivityHandlerSpy.reset();
     // messageActivityHandlerSpy.reset();
     // sessionStorage.removeItem(IN_AUTH_EXP);
     // sessionStorage.removeItem(LOGGED_IN_FLOW);
   });
-
-  it('should correctly handle "DIRECT_LINE/CONNECT_FULFILLED"', () => {
+  
+  it('should correctly handle "DIRECT_LINE/CONNECT_FULFILLED" with auth true', () => {
     // 1. test correct dispatches - done
+    // 2. test values of the new actions
     // 2. TODO test invocation of next
     // 3. TODO test return of the reducer
-
+    
+    const store = mockStore({}, GreetUser.makeBotGreetUser('fakeCsrfToken', 'fakeApiSession', 'http://apiURL', 'http://baseURL', 'noFirstNameFound', 'fakeUserUuid', true));
     // TODO discover result of the "next" function
     // invoke greetUser with proper curried values
     const action = { type: 'DIRECT_LINE/CONNECT_FULFILLED' };
-    GreetUser.makeBotGreetUser(
-      'csrfToken',
-      'apiSession',
-      'apiURL',
-      'baseURL',
-      'userFirstName',
-      'userUuid',
-      'requireAuth',
-    )(store)(fakeNext)(action);
+    GreetUser.makeBotGreetUser('csrfToken', 'apiSession', 'apiURL', 'baseURL', 'userFirstName', 'userUuid', 'requireAuth')(store)(fakeNext)(action);
     // fake the session storage using stubs
 
     const actions = store.getActions();
     expect(actions.length).to.equal(2);
     expect(fakeNext.callCount).to.equal(1);
-
-    // expect(actions[0].payload.activity).to.own.include({
-    //   name: 'startConversation',
-    //   type: 'event',
-    // });
+    
   });
 
   // it('should correctly handle "DIRECT_LINE/CONNECT_FULFILLED" and is not "LOGGED_IN_FLOW"', () => {});
+  it('should correctly handle "DIRECT_LINE/CONNECT_FULFILLED" with auth false', () => {
+    const store = mockStore({}, GreetUser.makeBotGreetUser('fakeCsrfToken', 'fakeApiSession', 'http://apiURL', 'http://baseURL', 'noFirstNameFound', 'fakeUserUuid', false));
 
+  };
   it('should correctly handle "DIRECT_LINE/POST_ACTIVITY"', () => {});
 
   it('should correctly handle "DIRECT_LINE/SEND_MESSAGE"', () => {});
