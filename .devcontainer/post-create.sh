8af1aa71a54cbb63f286ae624ad3069876bf2859
Theo Bentum <theo@adhocteam.us>
2022-09-15 23:46:40 -0700

    Uncomment script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,23 +6,21 @@ set -e
 # yarn cache clean && yarn install --production=false --prefer-offline && yarn build -- --host="${CODESPACE_NAME}-3001.githubpreview.dev/" --env api=${CODESPACE_NAME}-3000.githubpreview.dev/
 
 setup_vets_api() {
-  echo $USER
   # Copy settings file to vets-api directory
   cp .devcontainer/vets-api/settings.local.yml ../vets-api/config/settings.local.yml
 
   # Change directory to vets-api
   cd ../vets-api
-  pwd
 
-  # # Add codespace name to `virtual_hosts` in settings file
-  # sed -i "/^virtual_hosts*/a - $CODESPACE_NAME-3000.githubpreview.dev" config/settings.local.yml
+  # Add codespace name to `virtual_hosts` in settings file
+  sed -i "/^virtual_hosts*/a - $CODESPACE_NAME-3000.githubpreview.dev" config/settings.local.yml
 
-  # # Setup key & cert for localhost authentication to ID.me
-  # mkdir config/certs
-  # touch config/certs/vetsgov-localhost.crt
-  # touch config/certs/vetsgov-localhost.key
+  # Setup key & cert for localhost authentication to ID.me
+  mkdir config/certs
+  touch config/certs/vetsgov-localhost.crt
+  touch config/certs/vetsgov-localhost.key
 
-  # # Start vets-api server and associated services
+  # Start vets-api server and associated services
   # make up
 }
 
