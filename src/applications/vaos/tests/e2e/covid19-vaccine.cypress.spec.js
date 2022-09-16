af9319ccf0287a75ee43f0e24ed4d2ec7a61ffb5
Tony Williams <tony.williams@va.gov>
2022-09-16 13:16:54 -0500

    fix flaky test

--- src/applications/vaos/tests/e2e/covid19-vaccine.cypress.spec.js
+++ src/applications/vaos/tests/e2e/covid19-vaccine.cypress.spec.js
@@ -152,105 +152,105 @@ describe('VAOS COVID-19 vaccine appointment flow', () => {
     cy.findByText('Clinic:');
   });
 
-  //   it('should show facility contact page on second dose selection', () => {
-  //     cy.visit('health-care/schedule-view-va-appointments/appointments');
-  //     cy.injectAxe();
+  it('should show facility contact page on second dose selection', () => {
+    cy.visit('health-care/schedule-view-va-appointments/appointments');
+    cy.injectAxe();
 
-  //     cy.wait([
-  //       '@v0:get:appointments:va',
-  //       '@v0:get:appointments:cc',
-  //       '@v0:get:appointment:requests',
-  // '@v1:get:facilities',
-  //     ]);
+    cy.wait([
+      '@v0:get:appointments:va',
+      '@v0:get:appointments:cc',
+      '@v0:get:appointment:requests',
+      '@v1:get:facilities',
+    ]);
 
-  //     cy.axeCheckBestPractice();
+    cy.axeCheckBestPractice();
 
-  //     // Start flow
-  //     cy.findByText('Start scheduling', { waitForAnimations: true }).click({
-  //       waitForAnimations: true,
-  //     });
+    // Start flow
+    cy.findByText('Start scheduling', { waitForAnimations: true }).click({
+      waitForAnimations: true,
+    });
 
-  //     // Select COVID-19 vaccine appointment type
-  //     cy.get('input[value="covid"]')
-  //       .focus()
-  //       .check();
+    // Select COVID-19 vaccine appointment type
+    cy.get('input[value="covid"]')
+      .focus()
+      .check();
 
-  //     // Start vaccine flow
-  //     cy.findByText(/Continue/).click();
+    // Start vaccine flow
+    cy.findByText(/Continue/).click();
 
-  //     // Plan ahead page
-  //     cy.url().should('include', '/new-covid-19-vaccine-appointment');
-  //     cy.axeCheckBestPractice();
-  //     cy.contains('button', 'Continue')
-  //       .should('not.be.disabled')
-  //       .focus()
-  //       .click();
+    // Plan ahead page
+    cy.url().should('include', '/new-covid-19-vaccine-appointment');
+    cy.axeCheckBestPractice();
+    cy.contains('button', 'Continue')
+      .should('not.be.disabled')
+      .focus()
+      .click();
 
-  //     // Screener page
-  //     cy.url().should('include', '/confirm-doses-received');
-  //     cy.axeCheckBestPractice();
-  //     cy.get('#root_hasReceivedDoseYes')
-  //       .focus()
-  //       .click();
-  //     cy.findByText(/Continue/).click();
+    // Screener page
+    cy.url().should('include', '/confirm-doses-received');
+    cy.axeCheckBestPractice();
+    cy.get('#root_hasReceivedDoseYes')
+      .focus()
+      .click();
+    cy.findByText(/Continue/).click();
 
-  //     // Contact Facility Page
-  //     cy.url().should('include', '/contact-facility');
-  //     cy.findByText(/Continue/i).should('not.exist');
-  //   });
-  // });
+    // Contact Facility Page
+    cy.url().should('include', '/contact-facility');
+    cy.findByText(/Continue/i).should('not.exist');
+  });
+});
 
-  // describe('VAOS COVID-19 vaccine appointment flow - unavailable', () => {
-  //   beforeEach(() => {
-  //     vaosSetup();
+describe('VAOS COVID-19 vaccine appointment flow - unavailable', () => {
+  beforeEach(() => {
+    vaosSetup();
 
-  //     mockAppointmentRequestsApi();
-  //     mockAppointmentsApi({ apiVersion: 0 });
-  //     mockClinicApi({ facilityId: '983', apiVersion: 0 });
-  //     mockDirectBookingEligibilityCriteriaApi({ unableToScheduleCovid: true });
-  //     mockDirectScheduleSlotsApi(); // TODO: rename mockAppointmentSlots
-  //     mockFacilitiesApi({ apiVersion: 1 });
-  //     mockFacilityApi({ id: '983', apiVersion: 1 });
-  //     mockFeatureToggles();
-  //     mockLoginApi();
-  //     mockRequestEligibilityCriteriaApi();
-  //   });
+    mockAppointmentRequestsApi();
+    mockAppointmentsApi({ apiVersion: 0 });
+    mockClinicApi({ facilityId: '983', apiVersion: 0 });
+    mockDirectBookingEligibilityCriteriaApi({ unableToScheduleCovid: true });
+    mockDirectScheduleSlotsApi(); // TODO: rename mockAppointmentSlots
+    mockFacilitiesApi({ apiVersion: 1 });
+    mockFacilityApi({ id: '983', apiVersion: 1 });
+    mockFeatureToggles();
+    mockLoginApi();
+    mockRequestEligibilityCriteriaApi();
+  });
 
-  //   it('should show facility contact page when vaccine schedule is not available', () => {
-  //     cy.visit('health-care/schedule-view-va-appointments/appointments');
-  //     cy.injectAxe();
+  it('should show facility contact page when vaccine schedule is not available', () => {
+    cy.visit('health-care/schedule-view-va-appointments/appointments');
+    cy.injectAxe();
 
-  //     cy.wait([
-  //       '@v0:get:appointments:va',
-  //       '@v0:get:appointments:cc',
-  //       '@v0:get:appointment:requests',
-  //     ]);
+    cy.wait([
+      '@v0:get:appointments:va',
+      '@v0:get:appointments:cc',
+      '@v0:get:appointment:requests',
+    ]);
 
-  //     cy.axeCheckBestPractice();
+    cy.axeCheckBestPractice();
 
-  //     // Start flow
-  //     cy.findByText('Start scheduling', { waitForAnimations: true }).click({
-  //       waitForAnimations: true,
-  //     });
+    // Start flow
+    cy.findByText('Start scheduling', { waitForAnimations: true }).click({
+      waitForAnimations: true,
+    });
 
-  //     // Select COVID-19 vaccine appointment type
-  //     cy.get('input[value="covid"]')
-  //       .focus()
-  //       .check();
+    // Select COVID-19 vaccine appointment type
+    cy.get('input[value="covid"]')
+      .focus()
+      .check();
 
-  //     // Start vaccine flow
-  //     cy.findByText(/Continue/).click();
+    // Start vaccine flow
+    cy.findByText(/Continue/).click();
 
-  //     // Contact Facility Page
-  //     cy.url().should('include', '/contact-facility');
-  //     cy.wait([
-  //       '@v0:get:request_eligibility_criteria',
-  //       '@v0:get:direct_booking_eligibility_criteria',
-  //       '@v1:get:facilities',
-  //     ]).then(() => {
-  //       cy.findByText('Your facilities');
-  //       cy.axeCheckBestPractice();
-  //       cy.findByText(/Continue/i).should('not.exist');
-  //     });
-  //   });
+    // Contact Facility Page
+    cy.url().should('include', '/contact-facility');
+    cy.wait([
+      '@v0:get:request_eligibility_criteria',
+      '@v0:get:direct_booking_eligibility_criteria',
+      '@v1:get:facilities',
+    ]).then(() => {
+      cy.findByText('Your facilities');
+      cy.axeCheckBestPractice();
+      cy.findByText(/Continue/i).should('not.exist');
+    });
+  });
 });
