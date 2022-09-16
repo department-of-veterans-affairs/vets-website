db6f988e9bc5ae6f217247b3bb5171df99f1eb9a
KolbyVA <108146636+KolbyVA@users.noreply.github.com>
2022-09-16 09:49:01 -0500

    Mhv 33534 delete message mock (#22170)
    * [MHV-33534] Updated delete to closer match move
    
    * [MHV-33534] added aria-hidden tags

--- src/applications/mhv/secure-messaging/components/MessageActionsButtons.jsx
+++ src/applications/mhv/secure-messaging/components/MessageActionsButtons.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { VaModal } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAllFolders, moveMessageToFolder } from '../actions';
+import { getAllFolders, moveMessageToFolder, deleteMessage } from '../actions';
 
 const MessageActionButtons = props => {
   const dispatch = useDispatch();
@@ -35,6 +35,10 @@ const MessageActionButtons = props => {
       dispatch(moveMessageToFolder(props.id, selectedFolder));
     }
     closeMoveModal();
+  };
+
+  const handleDeleteMessage = () => {
+    dispatch(deleteMessage(props.id));
   };
 
   const moveToFolderModal = () => {
@@ -89,8 +93,12 @@ const MessageActionButtons = props => {
         <span className="message-action-button-text">Print</span>
       </button>
 
-      <button type="button" className="message-action-button">
-        <i className="fas fa-trash-alt" aria-hidden="true" />
+      <button
+        type="button"
+        className="message-action-button"
+        onClick={handleDeleteMessage}
+      >
+        <i className="fas fa-trash-alt" aria-hidden />
         <span className="message-action-button-text">Delete</span>
       </button>
 
@@ -99,7 +107,7 @@ const MessageActionButtons = props => {
         className="message-action-button"
         onClick={openMoveModal}
       >
-        <i className="fas fa-folder" aria-hidden="true" />
+        <i className="fas fa-folder" aria-hidden />
         <span className="message-action-button-text">Move</span>
       </button>
       {isModalVisible ? moveToFolderModal() : null}
