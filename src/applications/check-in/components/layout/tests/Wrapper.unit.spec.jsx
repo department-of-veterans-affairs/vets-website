b18ce2fb9354100962e7b81616765d395e39e702
Brian Seek <brian.seek@civicactions.com>
2022-09-19 09:18:45 -0700

    Added downtime test for wrapper.

--- src/applications/check-in/components/layout/tests/Wrapper.unit.spec.jsx
+++ src/applications/check-in/components/layout/tests/Wrapper.unit.spec.jsx
@@ -5,6 +5,7 @@ import { axeCheck } from 'platform/forms-system/test/config/helpers';
 import { expect } from 'chai';
 import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
+import { addDays, subDays, format } from 'date-fns';
 import i18n from '../../../utils/i18n/i18n';
 import { scheduledDowntimeState } from '../../../tests/unit/utils/initState';
 import Wrapper from '../Wrapper';
@@ -18,12 +19,11 @@ describe('Wrapper component', () => {
         pages: [],
       },
     },
-    ...scheduledDowntimeState,
   };
   const middleware = [];
   const mockStore = configureStore(middleware);
   beforeEach(() => {
-    store = mockStore(initState);
+    store = mockStore({ ...initState, ...scheduledDowntimeState });
   });
   it('Wrapper passes axeCheck', () => {
     axeCheck(
@@ -36,16 +36,48 @@ describe('Wrapper component', () => {
       </Provider>,
     );
   });
-  it('renders the passed in page title', () => {
+  it('renders the passed in page title and child', () => {
     const { getByText } = render(
       <Provider store={store}>
         <I18nextProvider i18n={i18n}>
           <Wrapper pageTitle="Test Title">
-            <p>test</p>
+            <p>test body</p>
           </Wrapper>
         </I18nextProvider>
       </Provider>,
     );
     expect(getByText('Test Title')).to.exist;
+    expect(getByText('test body')).to.exist;
+  });
+  it('renders the downtime component', () => {
+    const downDowntimeState = {
+      scheduledDowntime: {
+        globalDowntime: {
+          attributes: {
+            externalService: 'pcie',
+            startTime: format(subDays(new Date(), 1), "yyyy-LL-dd'T'HH:mm:ss"),
+            endTime: format(addDays(new Date(), 1), "yyyy-LL-dd'T'HH:mm:ss"),
+          },
+        },
+        isReady: true,
+        isPending: false,
+        serviceMap: {
+          get() {},
+        },
+        dismissedDowntimeWarnings: [],
+      },
+    };
+    const downTimeStore = mockStore({ ...downDowntimeState, ...initState });
+    const { getByText } = render(
+      <Provider store={downTimeStore}>
+        <I18nextProvider i18n={i18n}>
+          <Wrapper pageTitle="Test Title">
+            <p>test body</p>
+          </Wrapper>
+        </I18nextProvider>
+      </Provider>,
+    );
+    expect(getByText('Test Title')).to.exist;
+    expect(getByText('This tool is down for maintenance')).to.exist;
   });
 });
