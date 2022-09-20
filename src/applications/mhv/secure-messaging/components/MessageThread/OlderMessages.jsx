23838b95b220f4e80189ffcc7efee119dd16f274
oleksii-morgun-va <87077843+oleksii-morgun-va@users.noreply.github.com>
2022-09-20 14:49:23 -0400

    [#MHV-33539] handle load more messages button

--- src/applications/mhv/secure-messaging/components/MessageThread/OlderMessages.jsx
+++ src/applications/mhv/secure-messaging/components/MessageThread/OlderMessages.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getThread } from '../../actions';
 import HorizontalRule from '../shared/HorizontalRule';
@@ -8,6 +8,7 @@ const OlderMessages = () => {
   const dispatch = useDispatch();
   const message = useSelector(state => state.message.message);
   const messageThread = useSelector(state => state.message.messages);
+  const [viewCount, setViewCount] = useState(5);
 
   useEffect(
     () => {
@@ -18,16 +19,32 @@ const OlderMessages = () => {
     [message, dispatch],
   );
 
+  const handleLoadMoreMessages = () => {
+    setViewCount(viewCount + 5);
+  };
+
   return (
-    messageThread?.length > 0 && (
+    messageThread?.length > 0 &&
+    viewCount && (
       <div className="older-messages vads-u-margin-y--3 vads-u-padding-left--0p5">
         <h3 className="vads-u-font-weight--bold">
           Messages in this conversation
         </h3>
         <HorizontalRule />
+
         {messageThread.map((m, i) => {
-          return <MessageThreadItem key={i} message={m} />;
+          return i < viewCount && <MessageThreadItem key={i} message={m} />;
         })}
+
+        {viewCount < messageThread?.length && (
+          <div className="vads-u-margin-top--1 vads-l-row vads-u-justify-content--center">
+            <va-button
+              secondary
+              text="Load more messages"
+              onClick={handleLoadMoreMessages}
+            />
+          </div>
+        )}
       </div>
     )
   );
