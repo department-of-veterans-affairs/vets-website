c03c17743641cfa8427b0810ae3be48ee14c21b0
Holden Hinkle <holden@holdenhinkle.com>
2022-09-21 12:03:32 -0400

    force test to fail

--- src/platform/forms/tests/save-in-progress/01-sip-review.cypress.spec.js
+++ src/platform/forms/tests/save-in-progress/01-sip-review.cypress.spec.js
@@ -57,7 +57,7 @@ describe('SIP Review Test', () => {
         cy.wrap(link).click();
       });
 
-    cy.get('.usa-alert-error', { timeout: Timeouts.slow });
+    cy.get('.usa-alert-errrror', { timeout: Timeouts.slow });
 
     cy.url().should('contain', 'review-and-submit');
     cy.get('.usa-alert-error').should(
