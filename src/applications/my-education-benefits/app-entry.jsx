09c7fdddef1a1f05319a5b5a22d1cff9170e10ea
fer.boza <fernando.boza@accenturefederal.com>
2022-09-26 12:30:03 -0400

    reverted app.entry.jsx

--- src/applications/my-education-benefits/app-entry.jsx
+++ src/applications/my-education-benefits/app-entry.jsx
@@ -2,10 +2,6 @@ import 'platform/polyfills';
 import './sass/my-education-benefits.scss';
 
 import startApp from 'platform/startup';
-
-import { connect } from 'react-redux';
-import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
-import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
 import routes from './routes';
 import reducer from './reducers';
 import manifest from './manifest.json';
@@ -15,14 +11,3 @@ startApp({
   reducer,
   routes,
 });
-
-const mapStateToProps = state => {
-  const showMEBMailingAddressForeign = !!toggleValues(state)[
-    FEATURE_FLAG_NAMES.showMEBMailingAddressForeign
-  ];
-  return {
-    showMEBMailingAddressForeign,
-  };
-};
-
-export default connect(mapStateToProps)(startApp);
