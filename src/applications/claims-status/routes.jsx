b3b9f15f6f574c870dc4f831ef3409c10a3a3a0b
jerekshoe <jerek.shoemaker@adhocteam.us>
2022-10-06 12:55:40 -0500

    Cleaning up reducers

--- src/applications/claims-status/routes.jsx
+++ src/applications/claims-status/routes.jsx
@@ -5,13 +5,11 @@ import YourClaimsPageV2 from './containers/YourClaimsPageV2';
 import ClaimPage from './containers/ClaimPage';
 import ClaimStatusPage from './containers/ClaimStatusPage';
 import StemClaimStatusPage from './containers/StemClaimStatusPage';
-// import AppealStatusPage from './containers/AppealStatusPage';
 import FilesPage from './containers/FilesPage';
 import DetailsPage from './containers/DetailsPage';
 import AskVAPage from './containers/AskVAPage';
 import DocumentRequestPage from './containers/DocumentRequestPage';
 import ClaimEstimationPage from './containers/ClaimEstimationPage';
-// import AppealLayout from './components/AppealLayout';
 import AppealsV2StatusPage from './containers/AppealsV2StatusPage';
 import AppealsV2DetailPage from './containers/AppealsV2DetailPage';
 import AppealInfo from './containers/AppealInfo';
@@ -23,17 +21,6 @@ const routes = [
     to="/your-claims"
   />,
   <Route component={YourClaimsPageV2} key="/your-claims" path="/your-claims" />,
-  /*
-  <Route
-    component={AppealLayout}
-    key="/appeals"
-    path="/appeals">
-    <Route
-      component={AppealStatusPage}
-      key=":id/status"
-      path=":id/status"/>,
-  </Route>,
-  */
   <Route component={AppealInfo} key="/appeals/:id" path="/appeals/:id">
     <IndexRedirect to="status" />
     <Route component={AppealsV2StatusPage} key="status" path="status" />
