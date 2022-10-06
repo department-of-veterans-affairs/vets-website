00bcdd23e84c4d0cdbb402a6651c935521d27978
nbuckafs <nathaniel.buck@accenturefederal.com>
2022-10-06 16:14:59 -0400

    Require Postal Code for Foreign Addresses

--- src/applications/my-education-benefits/config/form.js
+++ src/applications/my-education-benefits/config/form.js
@@ -827,12 +827,6 @@ const formConfig = {
                   'ui:errorMessages': {
                     required: 'Zip code must be 5 digits',
                   },
-                  'ui:required': formData =>
-                    !formData.showMebDgi40Features ||
-                    (formData.showMebDgi40Features &&
-                      (formData['view:mailingAddress']?.livesOnMilitaryBase ||
-                        formData['view:mailingAddress']?.address?.country ===
-                          'USA')),
                   'ui:options': {
                     replaceSchema: formData => {
                       if (
