f9cbe2f63bb5f9f84dc9c092533263d92d073545
Josh Mills <61624970+jtmst@users.noreply.github.com>
2022-10-06 10:07:11 -0500

    adjusted header level for virtual agent faq (#22316)
    * adjusted header level for virtual agent faq
    
    * adjusted header level for virtual agent faq
    
    * estlint phone number component fix

--- src/applications/virtual-agent/components/page/Disclaimer.jsx
+++ src/applications/virtual-agent/components/page/Disclaimer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @department-of-veterans-affairs/prefer-telephone-component */
 /* eslint-disable @department-of-veterans-affairs/telephone-contact-3-or-10-digits */
 import React from 'react';
 
@@ -82,7 +83,8 @@ export default function Disclaimer() {
       <h3>More about our chatbot</h3>
 
       <va-accordion>
-        <va-accordion-item header="What to expect when using our chatbot">
+        <va-accordion-item>
+          <h4 slot="headline">What to expect when using our chatbot</h4>
           <p>
             Our chatbot is a resource to help you quickly find information about
             VA benefits and services. You won’t communicate with an actual
@@ -135,7 +137,8 @@ export default function Disclaimer() {
           </p>
         </va-accordion-item>
 
-        <va-accordion-item header="How to use our chatbot with a screen reader">
+        <va-accordion-item>
+          <h4 slot="headline">How to use our chatbot with a screen reader</h4>
           <p>
             If you’re blind or have low vision, follow these steps to use our
             chatbot on a desktop computer with a screen reader:
@@ -181,7 +184,10 @@ export default function Disclaimer() {
           </p>
         </va-accordion-item>
 
-        <va-accordion-item header="What information we collect when you use the chatbot">
+        <va-accordion-item>
+          <h4 slot="headline">
+            What information we collect when you use the chatbot
+          </h4>
           <p>
             We use certain information you’ve provided to build better tools for
             Veterans, service members, and their families.
