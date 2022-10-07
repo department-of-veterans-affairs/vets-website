ec9f76a8922722de5836d55cca5672fcb117e754
jerekshoe <jerek.shoemaker@adhocteam.us>
2022-10-07 16:11:39 -0500

    Updating name used in selector

--- src/applications/claims-status/selectors/index.js
+++ src/applications/claims-status/selectors/index.js
@@ -3,4 +3,4 @@ import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNa
 
 export const isLoadingFeatures = state => toggleValues(state).loading;
 export const showClaimLettersFeature = state =>
-  toggleValues(state)[FEATURE_FLAG_NAMES.claimLettersEnabled];
+  toggleValues(state)[FEATURE_FLAG_NAMES.claimLettersAccess];
