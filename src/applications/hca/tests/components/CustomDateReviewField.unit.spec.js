2663ea3a7b8537b45bafa44a5f234b98cde23bc6
Lisa Zapson <lisazapson@ymail.com>
2022-10-11 16:02:05 -0400

    made uiSchema generic

--- src/applications/hca/tests/components/CustomDateReviewField.unit.spec.js
+++ src/applications/hca/tests/components/CustomDateReviewField.unit.spec.js
@@ -3,23 +3,21 @@ import { render } from '@testing-library/react';
 import { expect } from 'chai';
 
 import CustomDateReviewField from '../../components/FormReview/CustomDateReviewField';
-import medicarePartAEffectiveDate from '../../config/chapters/insuranceInformation/medicarePartAEffectiveDate';
 
 describe('hca <CustomDateReviewField>', () => {
   it('should render the field title and field date', () => {
+    const uiSchema = {
+      'ui:title': 'Review Field Title',
+    };
     const formData = new Date('2020-5-15');
-    const { uiSchema } = medicarePartAEffectiveDate;
 
     const { getByText } = render(
       <CustomDateReviewField>
-        <React.Fragment
-          uiSchema={uiSchema.medicarePartAEffectiveDate}
-          formData={formData}
-        />
+        <React.Fragment uiSchema={uiSchema} formData={formData} />
       </CustomDateReviewField>,
     );
 
-    expect(getByText(/what is your medicare part A effective date/i)).to.exist;
+    expect(getByText(/review field title/i)).to.exist;
     expect(getByText('05/15/2020')).to.exist;
   });
 });
