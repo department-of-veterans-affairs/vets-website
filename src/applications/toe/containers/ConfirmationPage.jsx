71fa85d393a1d12b2bcfeb4fc15ff7a184a540c9
fer.boza <fernando.boza@accenturefederal.com>
2022-10-12 09:37:41 -0400

    updated CLAIM_STATUS actions and ConfirmationPage with claimStatus redux object

--- src/applications/toe/containers/ConfirmationPage.jsx
+++ src/applications/toe/containers/ConfirmationPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -6,7 +6,34 @@ import ApprovedConfirmation from '../components/confirmation/ApprovedConfirmatio
 import DeniedConfirmation from '../components/confirmation/DeniedConfirmation';
 import UnderReviewConfirmation from '../components/confirmation/UnderReviewConfirmation';
 
-function ConfirmationPage({ confirmationResult }) {
+import { fetchClaimStatus } from '../actions';
+
+function ConfirmationPage({
+  confirmationResult,
+  getClaimStatus,
+  claimStatus,
+  user,
+}) {
+  const [fetchedClaimStatus, setFetchedClaimStatus] = useState(false);
+  useEffect(
+    () => {
+      if (!fetchedClaimStatus) {
+        setFetchedClaimStatus(true);
+        getClaimStatus();
+      }
+    },
+    [
+      fetchedClaimStatus,
+      getClaimStatus,
+      user?.login?.currentlyLoggedIn,
+      confirmationResult,
+    ],
+  );
+
+  if (claimStatus) {
+    // console.log(claimStatus);
+  }
+
   switch (confirmationResult) {
     case 'APPROVED': {
       return <ApprovedConfirmation />;
@@ -32,12 +59,20 @@ function ConfirmationPage({ confirmationResult }) {
 }
 
 ConfirmationPage.propTypes = {
+  claimStatus: PropTypes.object,
   confirmationResult: PropTypes.string,
+  getClaimStatus: PropTypes.func,
+  user: PropTypes.object,
 };
 
-// const mapStateToProps = state => ({
-//   claimStatus: state.data?.claimStatus,
-// });
+const mapStateToProps = state => {
+  // console.log(state);
+  return {
+    claimStatus: state.data?.claimStatus,
+    getClaimStatus: fetchClaimStatus,
+    user: state.user,
+  };
+};
 
-// export default connect(mapStateToProps)(ConfirmationPage);
-export default connect()(ConfirmationPage);
+export default connect(mapStateToProps)(ConfirmationPage);
+// export default connect()(ConfirmationPage);
