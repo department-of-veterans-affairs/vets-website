4dcb64c57534accdcad27cd55e200b02005e89b2
Victoria Saleem <victoria.saleem@va.gov>
2022-10-12 14:12:32 -0400

    [Mhv 33641] Messages FAQ (unauth + auth) component (#22396)
    * [MHV 33641] Added UnAuth MessageFAQ components
    
    -Created reusable UnAuthBanner and UnAuthSendMessageSection for unauthenticated users
    
    * [MHV-33641] Created MessageFAQ Container and FAQ sub-component
    
    * [MHV-33641] Added MessageFAQ component with unauth render logic
    
    -added logic for unAuthBanner and UnAuthSendMessageSection components, renders only when user is unauthenticated
    -updated routes, nav, and breadcrumb for MessageFAQ
    
    * [MHV-33641] Updated FAQ component
    
    -uncommented-out FAQ component
    
    * [MHV-33641] Updated routes
    
    -Added /faq to path const in Navigation.jsx
    -Updated routes to /faq in breadcrumb and routes.jsx

---
+++ src/applications/mhv/secure-messaging/components/shared/UnAuthSendMessageSection.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const UnAuthSendMessageSection = () => {
+  return (
+    <>
+      <h2>To send messages, you must be a VA patient</h2>
+      <p>
+        And your VA provider must agree to communicate with you through secure
+        messaging.
+      </p>
+      <p>
+        VA health care covers primary care and specialist appointments, as well
+        as services like home health and geriatric (elder) care. And VA offers
+        family and caregiver health benefits.
+      </p>
+      <p className="vads-u-margin-top--3px">
+        <a
+          className="vads-c-action-link--blue apply-for-health-care-link"
+          href="/my-health/secure-messages"
+        >
+          Apply for health care
+        </a>
+      </p>
+      <p>
+        <a href="/my-health/secure-messages">Learn about VA health benefits</a>
+      </p>
+    </>
+  );
+};
+
+export default UnAuthSendMessageSection;
