bf938bfabdd852b9ce9c2aea8ba250e2eebe81e1
jtmst <josh.troy.mills@gmail.com>
2022-10-12 12:02:38 -0400

    lint fix

--- src/applications/proxy-rewrite/proxy-rewrite-entry.jsx
+++ src/applications/proxy-rewrite/proxy-rewrite-entry.jsx
@@ -167,7 +167,9 @@ function activateInjectedAssets() {
       }
 
       throw new Error(
-        `vets_headerFooter_error: Failed to fetch header and footer menu data: ${resp.statusText}`,
+        `vets_headerFooter_error: Failed to fetch header and footer menu data: ${
+          resp.statusText
+        }`,
       );
     })
     .then(headerFooterData => {
