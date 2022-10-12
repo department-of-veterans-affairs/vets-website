7faad17702287fedd615098e37e7753829f5e6c4
VicSaleem <59583325+vsaleem@users.noreply.github.com>
2022-10-12 10:57:02 -0400

    [MHV-33641] Created MessageFAQ Container and FAQ sub-component

---
+++ src/applications/mhv/secure-messaging/containers/MessageFAQs.jsx
@@ -0,0 +1,18 @@
+/** 
+MessageFAQs Container
+@author Vic Saleem 
+*/
+
+import React from 'react';
+// import MessageFAQ from '../components/MessageFAQ';
+
+const MessageFAQs = () => {
+  // const { isLoggedIn } = props;
+  return (
+    <div className="vads-l-grid-container vads-u-margin-top--2 message-faq-container">
+      {/* <MessageFAQ isLoggedIn={isLoggedIn} /> */}
+    </div>
+  );
+};
+
+export default MessageFAQs;
