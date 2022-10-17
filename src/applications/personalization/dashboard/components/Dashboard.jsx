2fad397589fd893ff7853f56e040226460f98aa2
Adam Whitlock <adam.whitlock627@gmail.com>
2022-10-17 09:09:45 -0600

    Profile/31403/update identity verification alert (#22410)
    * (fix) moive mock user back to standard data response
    
    * (chore) update copy
    
    * (chore) clean up handlers, update alert content and styling
    
    * (test) update e2e/u tests, update prop for myVa comp usage
    
    * (test) unskip mobile test, to see if it will pass CI
    
    * (chore) add missing proptypes
    
    * (fix) remove comp from debugging with john

--- src/applications/personalization/dashboard/components/Dashboard.jsx
+++ src/applications/personalization/dashboard/components/Dashboard.jsx
@@ -249,10 +249,7 @@ const Dashboard = ({
               {props.showValidateIdentityAlert ? (
                 <div className="vads-l-row">
                   <div className="vads-l-col--12 medium-screen:vads-l-col--8 medium-screen:vads-u-padding-right--3">
-                    <IdentityNotVerified
-                      alertHeadline="Verify your identity to access more VA.gov tools and features"
-                      level={2}
-                    />
+                    <IdentityNotVerified headline="Verify your identity to access more VA.gov tools and features" />
                   </div>
                 </div>
               ) : null}
