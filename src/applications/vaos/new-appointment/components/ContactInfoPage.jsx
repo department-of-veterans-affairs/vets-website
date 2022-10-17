c6ec17e432398937177367485ef350e0b02921d2
Tony Williams <tony.williams@va.gov>
2022-10-17 17:18:42 -0500

    fix to hide best time to call when acheron is on

--- src/applications/vaos/new-appointment/components/ContactInfoPage.jsx
+++ src/applications/vaos/new-appointment/components/ContactInfoPage.jsx
@@ -25,6 +25,7 @@ import {
 import NewTabAnchor from '../../components/NewTabAnchor';
 import useFormState from '../../hooks/useFormState';
 import { FLOW_TYPES, GA_PREFIX } from '../../utils/constants';
+import { selectFeatureAcheronService } from '../../redux/selectors';
 
 const initialSchema = {
   type: 'object',
@@ -99,6 +100,9 @@ export default function ContactInfoPage() {
   const homePhone = useSelector(selectVAPHomePhoneString);
   const mobilePhone = useSelector(selectVAPMobilePhoneString);
   const flowType = useSelector(getFlowType);
+  const featureAcheronService = useSelector(state =>
+    selectFeatureAcheronService(state),
+  );
 
   useEffect(() => {
     document.title = `${pageTitle} | Veterans Affairs`;
@@ -136,11 +140,13 @@ export default function ContactInfoPage() {
     bestTimeToCall: {
       'ui:title': 'What are the best times for us to call you?',
       'ui:validations':
-        flowType === FLOW_TYPES.REQUEST ? [validateBooleanGroup] : [],
+        !featureAcheronService && flowType === FLOW_TYPES.REQUEST
+          ? [validateBooleanGroup]
+          : [],
       'ui:options': {
         showFieldLabel: true,
         classNames: 'vaos-form__checkboxgroup',
-        hideIf: () => flowType === FLOW_TYPES.DIRECT,
+        hideIf: () => featureAcheronService || flowType === FLOW_TYPES.DIRECT,
       },
       morning: {
         'ui:title': 'Morning (8:00 a.m. – noon)',
