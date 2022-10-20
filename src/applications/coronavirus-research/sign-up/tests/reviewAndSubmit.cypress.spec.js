b20975a1cbccb713bbc5f4685c568e86de64f57e
Theo Bentum <theo@adhocteam.us>
2022-10-19 20:36:06 -0700

    Refactor coronavirus-research test

--- src/applications/coronavirus-research/sign-up/tests/reviewAndSubmit.cypress.spec.js
+++ src/applications/coronavirus-research/sign-up/tests/reviewAndSubmit.cypress.spec.js
@@ -3,17 +3,15 @@ import featureTogglesEnabled from './fixtures/toggle-covid-feature.json';
 
 describe('COVID-19 Research Form', () => {
   describe('when entering valid information and submitting the sign up form', () => {
-    before(() => {
+    beforeEach(() => {
       cy.intercept('GET', '/v0/feature_toggles*', featureTogglesEnabled).as(
         'feature',
       );
+      cy.visit('coronavirus-research/volunteer/sign-up');
     });
 
-    before(() => {
-      cy.visit('coronavirus-research/volunteer/sign-up');
-      cy.injectAxe();
-    });
     it('should load form page', () => {
+      cy.injectAxe();
       cy.url().should('include', 'coronavirus-research/volunteer/sign-up');
       cy.axeCheck();
       cy.get('h1').contains(
