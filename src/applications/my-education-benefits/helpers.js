f5370ec2791c439f49bd3e2a05eb6557e74fabfd
fer.boza <fernando.boza@accenturefederal.com>
2022-10-21 17:52:06 -0400

    Discovered that the ReviewCard passes new data even when cancelling the update method, causing the obfuscate to crash, added helper function

--- src/applications/my-education-benefits/helpers.js
+++ src/applications/my-education-benefits/helpers.js
@@ -47,6 +47,10 @@ export function titleCase(str) {
   return str[0].toUpperCase() + str.slice(1).toLowerCase();
 }
 export function obfuscate(str, numVisibleChars = 4, obfuscateChar = '●') {
+  if (!str) {
+    return '010';
+  }
+
   if (str.length <= numVisibleChars) {
     return str;
   }
