c02409b3c4956787167a5036296ca14e596305a3
John Woolschlager <wullaski@gmail.com>
2022-10-21 16:17:35 -0400

    Change to just one appointment

--- src/applications/check-in/day-of/tests/e2e/travel-vehicle-display/travel-vehicle.display.cypress.spec.js
+++ src/applications/check-in/day-of/tests/e2e/travel-vehicle-display/travel-vehicle.display.cypress.spec.js
@@ -10,6 +10,7 @@ import TravelPages from '../../../../tests/e2e/pages/TravelPages';
 describe('Check In Experience', () => {
   describe('travel vehicle display', () => {
     beforeEach(() => {
+      const appointments = [{ startTime: '2021-08-19T03:00:00' }];
       const {
         initializeFeatureToggle,
         initializeSessionGet,
@@ -24,6 +25,7 @@ describe('Check In Experience', () => {
       initializeDemographicsPatch.withSuccess();
       initializeCheckInDataGet.withSuccess({
         numberOfCheckInAbledAppointments: 1,
+        appointments,
       });
       initializeCheckInDataPost.withSuccess();
       cy.visitWithUUID();
