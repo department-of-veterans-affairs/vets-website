3043d15056b790137a49bd09d9864be87e91753d
fer.boza <fernando.boza@accenturefederal.com>
2022-10-25 22:09:25 -0400

    Updated TOE formConfig with footerContent and GetHelp component paragraph text.

--- src/applications/toe/components/GetHelp.jsx
+++ src/applications/toe/components/GetHelp.jsx
@@ -5,15 +5,10 @@ export default function GetHelp() {
     <>
       <p className="help-talk">
         If you need help with your application or have questions about
-        enrollment or eligibility, call our Education Call Center at{' '}
-        <a href="tel:888-442-4551">888-442-4551 (711)</a>. We're here Monday
-        through Friday, 8:00 a.m. to 7:00 p.m. ET. If you're outside the U.S.,
-        call us at <a href="tel:001-918-781-5678">001-918-781-5678</a>.
-      </p>
-      <p>
-        If you have technical difficulties using this online application, call
-        our MyVA411 main information line at{' '}
-        <a href="tel:800-698-2411">800-698-2411 (711)</a>. We're here 24/7.
+        enrollment or eligibility, submit a request with{' '}
+        <a href="/contact-us"> Ask VA. </a> If you have technical difficulties
+        using this online application, call our MyVA411 main information line at{' '}
+        <va-telephone contact="8006982411" />. We're here 24/7.
       </p>
     </>
   );
