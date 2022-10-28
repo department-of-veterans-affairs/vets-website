f41c1a40a7066ea3c30436da3e4adf781374886c
fer.boza <fernando.boza@accenturefederal.com>
2022-10-28 15:02:42 -0400

    updated extension for va-telephone

--- src/applications/toe/components/GetHelp.jsx
+++ src/applications/toe/components/GetHelp.jsx
@@ -12,7 +12,7 @@ export default function GetHelp() {
         </a>{' '}
         If you have technical difficulties using this online application, call
         our MyVA411 main information line at{' '}
-        <va-telephone contact="8006982411" />. We're here 24/7.
+        <va-telephone contact="8006982411" extension="711" />. We're here 24/7.
       </p>
     </>
   );
