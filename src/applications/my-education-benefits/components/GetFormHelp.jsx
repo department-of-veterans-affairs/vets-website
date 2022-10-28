ef501d00a8086efc65c764d8b4173c60068dbb3a
fer.boza <fernando.boza@accenturefederal.com>
2022-10-28 15:42:00 -0400

    updated va-telephone for 711

--- src/applications/my-education-benefits/components/GetFormHelp.jsx
+++ src/applications/my-education-benefits/components/GetFormHelp.jsx
@@ -17,8 +17,9 @@ function GetFormHelp({ showMebDgi40Features }) {
           </a>{' '}
           If you have technical difficulties using this online application, call
           our MyVA411 main information line at{' '}
-          <va-telephone contact="8006982411" extension="711" />. We're here
-          24/7.
+          <va-telephone contact="8006982411" /> (TTY:{' '}
+          <va-telephone contact="711" />
+          ). We're here 24/7.
         </>
       ) : (
         <>
@@ -30,8 +31,9 @@ function GetFormHelp({ showMebDgi40Features }) {
           <br />
           If you have technical difficulties using this online application, call
           our MyVA411 main information line at{' '}
-          <va-telephone contact="8006982411" extension="711" />. We're here
-          24/7.
+          <va-telephone contact="8006982411" /> (TTY:{' '}
+          <va-telephone contact="711" />
+          ). We're here 24/7.
         </>
       )}
     </p>
