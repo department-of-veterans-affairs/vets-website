7f3bf7f1e359bdaf24c44fa5c54e38c266605966
Jarred Simon <jarred.simon@va.gov>
2022-10-28 09:13:45 -0500

    removed reference to National Call to Service from additional test cases

---
+++ src/applications/edu-benefits/1990n/tests/ReadMe.txt
@@ -0,0 +1,6 @@
+All test are commented out
+Online form 22-1990n will no longer be available to fill out Online
+PDF version of 22-1990n will be available for download
+Left code for online form 22-1990n in repo incase education wants to add the online form back
+
+Date: Oct 2022
--- src/applications/edu-benefits/1990n/tests/containers/IntroductionPage.unit.spec.jsx
+++ src/applications/edu-benefits/1990n/tests/containers/IntroductionPage.unit.spec.jsx
@@ -1,63 +1,63 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
-import { IntroductionPage } from 'applications/edu-benefits/1990n/containers/IntroductionPage';
-import {
-  WIZARD_STATUS_COMPLETE,
-  getWizardStatus,
-} from 'applications/static-pages/wizard';
+// import React from 'react';
+// import { expect } from 'chai';
+// import { shallow } from 'enzyme';
+// import { IntroductionPage } from 'applications/edu-benefits/1990n/containers/IntroductionPage';
+// import {
+//   WIZARD_STATUS_COMPLETE,
+//   getWizardStatus,
+// } from 'applications/static-pages/wizard';
 
-describe('the Edu-Benefit 1990E Introduction Page', () => {
-  it('should show the wizard if showWizard is set to true', () => {
-    const fakeStore = {
-      getState: () => ({
-        showWizard: true,
-        route: { formConfig: {} },
-      }),
-      subscribe: () => {},
-      dispatch: () => {},
-    };
+// describe('the Edu-Benefit 1990E Introduction Page', () => {
+//   it('should show the wizard if showWizard is set to true', () => {
+//     const fakeStore = {
+//       getState: () => ({
+//         showWizard: true,
+//         route: { formConfig: {} },
+//       }),
+//       subscribe: () => {},
+//       dispatch: () => {},
+//     };
 
-    const wrapper = shallow(<IntroductionPage {...fakeStore.getState()} />);
-    expect(wrapper.exists('WizardContainer')).to.equal(true);
-    expect(wrapper.exists('.subway-map')).to.equal(false);
-    wrapper.unmount();
-  });
+//     const wrapper = shallow(<IntroductionPage {...fakeStore.getState()} />);
+//     expect(wrapper.exists('WizardContainer')).to.equal(true);
+//     expect(wrapper.exists('.subway-map')).to.equal(false);
+//     wrapper.unmount();
+//   });
 
-  it('should show the subway map if showWizard is set to false', () => {
-    const fakeStore = {
-      getState: () => ({
-        showWizard: false,
-        route: { formConfig: {} },
-      }),
-      subscribe: () => {},
-      dispatch: () => {},
-    };
+//   it('should show the subway map if showWizard is set to false', () => {
+//     const fakeStore = {
+//       getState: () => ({
+//         showWizard: false,
+//         route: { formConfig: {} },
+//       }),
+//       subscribe: () => {},
+//       dispatch: () => {},
+//     };
 
-    const wrapper = shallow(<IntroductionPage {...fakeStore.getState()} />);
-    expect(wrapper.exists('WizardContainer')).to.equal(false);
-    expect(wrapper.exists('.subway-map')).to.equal(true);
-    wrapper.unmount();
-  });
+//     const wrapper = shallow(<IntroductionPage {...fakeStore.getState()} />);
+//     expect(wrapper.exists('WizardContainer')).to.equal(false);
+//     expect(wrapper.exists('.subway-map')).to.equal(true);
+//     wrapper.unmount();
+//   });
 
-  it('should show the subway map if the wizard was completed', () => {
-    const fakeStore = {
-      getState: () => ({
-        showWizard: true,
-        route: { formConfig: {} },
-      }),
-      subscribe: () => {},
-      dispatch: () => {},
-    };
+//   it('should show the subway map if the wizard was completed', () => {
+//     const fakeStore = {
+//       getState: () => ({
+//         showWizard: true,
+//         route: { formConfig: {} },
+//       }),
+//       subscribe: () => {},
+//       dispatch: () => {},
+//     };
 
-    const wrapper = shallow(<IntroductionPage {...fakeStore.getState()} />);
-    const instance = wrapper.instance();
-    instance.setWizardStatus(WIZARD_STATUS_COMPLETE);
-    const status = getWizardStatus().then(() => {
-      expect(status).to.equal(WIZARD_STATUS_COMPLETE);
-    });
-    expect(wrapper.exists('WizardContainer')).to.equal(false);
-    expect(wrapper.exists('.subway-map')).to.equal(true);
-    wrapper.unmount();
-  });
-});
+//     const wrapper = shallow(<IntroductionPage {...fakeStore.getState()} />);
+//     const instance = wrapper.instance();
+//     instance.setWizardStatus(WIZARD_STATUS_COMPLETE);
+//     const status = getWizardStatus().then(() => {
+//       expect(status).to.equal(WIZARD_STATUS_COMPLETE);
+//     });
+//     expect(wrapper.exists('WizardContainer')).to.equal(false);
+//     expect(wrapper.exists('.subway-map')).to.equal(true);
+//     wrapper.unmount();
+//   });
+// });
