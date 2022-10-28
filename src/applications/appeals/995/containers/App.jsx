40690e8d3208a506fc490f113defe42dcab4c574
Robin Garrison <robin.garrison@adhocteam.us>
2022-10-28 14:16:02 -0500

    App shows WIP alert when feature disabled

--- src/applications/appeals/995/containers/App.jsx
+++ src/applications/appeals/995/containers/App.jsx
@@ -7,6 +7,8 @@ import { selectProfile, isLoggedIn } from 'platform/user/selectors';
 
 import { setData } from 'platform/forms-system/src/js/actions';
 import { getStoredSubTask } from 'platform/forms/sub-task';
+import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
+import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
 
 import {
   getContestableIssues as getContestableIssuesAction,
@@ -16,7 +18,9 @@ import {
 import formConfig from '../config/form';
 import { issuesNeedUpdating, processContestableIssues } from '../utils/helpers';
 
-export const Form0995App = ({
+import { WIP } from '../components/WIP';
+
+export const App = ({
   loggedIn,
   location,
   children,
@@ -28,58 +32,65 @@ export const Form0995App = ({
   getContestableIssues,
   contestableIssues = {},
   legacyCount,
+  isLoadingFeatures,
+  show995,
 }) => {
   const { email = {}, homePhone = {}, mobilePhone = {}, mailingAddress = {} } =
     profile?.vapContactInfo || {};
 
   // Make sure we're only loading issues once - see
   // https://github.com/department-of-veterans-affairs/va.gov-team/issues/33931
-  const [isLoadingIssues, setIsLoadingIssues] = useState(false);
+  const [isLoadingFeaturesIssues, setIsLoadingFeaturesIssues] = useState(false);
 
   const subTaskBenefitType =
     formData?.benefitType || getStoredSubTask()?.benefitType;
 
   useEffect(
     () => {
-      // form data is reset after logging in and from the save-in-progress data,
-      // so get it from the session storage
-      if (!formData.benefitType && subTaskBenefitType) {
-        setFormData({
-          ...formData,
-          benefitType: subTaskBenefitType,
-        });
-      } else if (loggedIn && formData.benefitType) {
-        const { veteran = {} } = formData || {};
-        if (!isLoadingIssues && (contestableIssues?.status || '') === '') {
-          // load benefit type contestable issues
-          setIsLoadingIssues(true);
-          getContestableIssues({ benefitType: formData.benefitType });
-        } else if (
-          email?.emailAddress !== veteran.email ||
-          homePhone?.updatedAt !== veteran.homePhone?.updatedAt ||
-          mobilePhone?.updatedAt !== veteran.mobilePhone?.updatedAt ||
-          mailingAddress?.updatedAt !== veteran.address?.updatedAt ||
-          issuesNeedUpdating(
-            contestableIssues?.issues,
-            formData?.contestedIssues,
-          ) ||
-          contestableIssues.legacyCount !== formData.legacyCount
-        ) {
-          // resetStoredSubTask();
+      if (show995) {
+        // form data is reset after logging in and from the save-in-progress data,
+        // so get it from the session storage
+        if (!formData.benefitType && subTaskBenefitType) {
           setFormData({
             ...formData,
-            veteran: {
-              ...veteran,
-              address: mailingAddress,
-              mobilePhone,
-              homePhone,
-              email: email?.emailAddress,
-            },
-            contestedIssues: processContestableIssues(
-              contestableIssues?.issues,
-            ),
-            legacyCount: contestableIssues?.legacyCount,
+            benefitType: subTaskBenefitType,
           });
+        } else if (loggedIn && formData.benefitType) {
+          const { veteran = {} } = formData || {};
+          if (
+            !isLoadingFeaturesIssues &&
+            (contestableIssues?.status || '') === ''
+          ) {
+            // load benefit type contestable issues
+            setIsLoadingFeaturesIssues(true);
+            getContestableIssues({ benefitType: formData.benefitType });
+          } else if (
+            email?.emailAddress !== veteran.email ||
+            homePhone?.updatedAt !== veteran.homePhone?.updatedAt ||
+            mobilePhone?.updatedAt !== veteran.mobilePhone?.updatedAt ||
+            mailingAddress?.updatedAt !== veteran.address?.updatedAt ||
+            issuesNeedUpdating(
+              contestableIssues?.issues,
+              formData?.contestedIssues,
+            ) ||
+            contestableIssues.legacyCount !== formData.legacyCount
+          ) {
+            // resetStoredSubTask();
+            setFormData({
+              ...formData,
+              veteran: {
+                ...veteran,
+                address: mailingAddress,
+                mobilePhone,
+                homePhone,
+                email: email?.emailAddress,
+              },
+              contestedIssues: processContestableIssues(
+                contestableIssues?.issues,
+              ),
+              legacyCount: contestableIssues?.legacyCount,
+            });
+          }
         }
       }
     },
@@ -89,14 +100,21 @@ export const Form0995App = ({
       formData,
       getContestableIssues,
       homePhone,
-      isLoadingIssues,
+      isLoadingFeaturesIssues,
       legacyCount,
       loggedIn,
       mailingAddress,
       mobilePhone,
       setFormData,
       subTaskBenefitType,
+      show995,
     ],
+  );
+
+  const wrapInH1 = content => (
+    <h1 className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--normal">
+      {content}
+    </h1>
   );
 
   let content = (
@@ -105,25 +123,28 @@ export const Form0995App = ({
     </RoutedSavableApp>
   );
 
+  if (isLoadingFeatures) {
+    return wrapInH1(<va-loading-indicator message="Loading application..." />);
+  }
+  if (!show995) {
+    return <WIP />;
+  }
+
   if (!subTaskBenefitType) {
     router.push('/start');
-    content = (
-      <h1 className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--normal">
-        <va-loading-indicator message="Please wait while we restart the application for you." />
-      </h1>
+    content = wrapInH1(
+      <va-loading-indicator message="Please wait while we restart the application for you." />,
     );
   } else if (
     loggedIn &&
     ((contestableIssues?.status || '') === '' ||
       contestableIssues?.status === FETCH_CONTESTABLE_ISSUES_INIT)
   ) {
-    content = (
-      <h1 className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--normal">
-        <va-loading-indicator
-          set-focus
-          message="Loading your previous decisions..."
-        />
-      </h1>
+    content = wrapInH1(
+      <va-loading-indicator
+        set-focus
+        message="Loading your previous decisions..."
+      />,
     );
   }
 
@@ -134,7 +155,7 @@ export const Form0995App = ({
   );
 };
 
-Form0995App.propTypes = {
+App.propTypes = {
   getContestableIssues: PropTypes.func.isRequired,
   setFormData: PropTypes.func.isRequired,
   children: PropTypes.any,
@@ -147,6 +168,7 @@ Form0995App.propTypes = {
     legacyCount: PropTypes.number,
     informalConferenceRep: PropTypes.shape({}),
   }),
+  isLoadingFeatures: PropTypes.bool,
   legacyCount: PropTypes.number,
   location: PropTypes.shape({
     pathname: PropTypes.string,
@@ -159,6 +181,7 @@ Form0995App.propTypes = {
     push: PropTypes.func,
   }),
   savedForms: PropTypes.array,
+  show995: PropTypes.bool,
 };
 
 const mapStateToProps = state => ({
@@ -168,6 +191,8 @@ const mapStateToProps = state => ({
   savedForms: state.user?.profile?.savedForms || [],
   contestableIssues: state.contestableIssues || {},
   legacyCount: state.legacyCount || 0,
+  isLoadingFeatures: toggleValues(state).loading,
+  show995: toggleValues(state)[FEATURE_FLAG_NAMES.supplementalClaim] || false,
 });
 
 const mapDispatchToProps = {
@@ -178,4 +203,4 @@ const mapDispatchToProps = {
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(Form0995App);
+)(App);
