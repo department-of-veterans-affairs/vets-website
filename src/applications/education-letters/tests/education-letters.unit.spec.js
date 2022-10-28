eeaac3ef55b660d131200d80fe727486386460dd
fer.boza <fernando.boza@accenturefederal.com>
2022-10-28 11:36:42 -0400

    created and working on education-letters.unit.spec.js

---
+++ src/applications/education-letters/tests/education-letters.unit.spec.js
@@ -0,0 +1,5 @@
+// import React from 'react';
+// import reducer from '../reducers';
+// import { expect } from 'chai';
+// import { renderWithStoreAndRouter } from 'platform/testing/unit/react-testing-library-helpers';
+// import InboxPage from '../containers/InboxPage'
