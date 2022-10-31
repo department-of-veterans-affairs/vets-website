36b233dc2130cd8a2ea52eca0b5d5e62952cf61a
Andrew Rodiek <andrew.rodiek@gmail.com>
2022-10-31 11:25:48 -0400

    [eFSR] 48432 - Monetary Asset Enhancements (#22519)

---
+++ src/applications/financial-status-report/components/monetary/MonetaryCheckList.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setData } from 'platform/forms-system/src/js/actions';
+import { monetaryAssets as monetaryAssetList } from '../../constants/checkboxSelections';
+
+const MonetaryCheckList = () => {
+  const dispatch = useDispatch();
+  const formData = useSelector(state => state.form.data);
+
+  const { assets } = formData;
+  const { monetaryAssets = [] } = assets;
+
+  const onChange = ({ target }) => {
+    const { value } = target;
+    return monetaryAssets.some(source => source.name === value)
+      ? dispatch(
+          setData({
+            ...formData,
+            assets: {
+              ...assets,
+              monetaryAssets: monetaryAssets.filter(
+                source => source.name !== value,
+              ),
+            },
+          }),
+        )
+      : dispatch(
+          setData({
+            ...formData,
+            assets: {
+              ...assets,
+              monetaryAssets: [...monetaryAssets, { name: value, amount: '' }],
+            },
+          }),
+        );
+  };
+
+  const isBoxChecked = option => {
+    return monetaryAssets.some(asset => asset.name === option);
+  };
+
+  return (
+    <div className="checkbox-list">
+      {monetaryAssetList?.map((option, key) => (
+        <div key={option + key} className="checkbox-list-item">
+          <input
+            type="checkbox"
+            id={option + key}
+            name={option}
+            value={option}
+            checked={isBoxChecked(option)}
+            onChange={onChange}
+          />
+          <label className="vads-u-margin-y--2" htmlFor={option + key}>
+            {option}
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MonetaryCheckList;
