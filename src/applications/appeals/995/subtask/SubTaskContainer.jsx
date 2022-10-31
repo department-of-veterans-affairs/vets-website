e747b80168716e132ecc103b5367838f65d6201f
Robin Garrison <robin.garrison@adhocteam.us>
2022-10-31 10:38:58 -0500

    Update subtask content

--- src/applications/appeals/995/subtask/SubTaskContainer.jsx
+++ src/applications/appeals/995/subtask/SubTaskContainer.jsx
@@ -10,6 +10,13 @@ export const SubTaskContainer = () => (
   <article className="row">
     <div className="usa-width-two-thirds medium-8 columns vads-u-margin-bottom--2">
       <SubTask pages={pages} />
+      <p>
+        If you don’t think this is the right form for you, find out about other
+        decision review options.
+      </p>
+      <a href="/resources/choosing-a-decision-review-option/">
+        Learn about choosing a decision review option
+      </a>
     </div>
     <FormFooter formConfig={formConfig} />
   </article>
