1b04e1262f82e605942553f99785d29473953c37
Robin Garrison <robin.garrison@adhocteam.us>
2022-10-31 15:37:52 -0500

    Add subtask tests

---
+++ src/applications/appeals/995/tests/995-subtask.cypress.spec.js
@@ -0,0 +1,61 @@
+import { resetStoredSubTask } from 'platform/forms/sub-task';
+
+import { BASE_URL } from '../constants';
+
+describe('995 subtask', () => {
+  beforeEach(() => {
+    window.dataLayer = [];
+    cy.intercept('GET', '/v0/feature_toggles?*', {
+      data: {
+        type: 'feature_toggles',
+        features: [],
+      },
+    });
+
+    resetStoredSubTask();
+    cy.visit(`${BASE_URL}/start`);
+  });
+
+  it('should show error when nothing selected', () => {
+    cy.location('pathname').should('eq', `${BASE_URL}/start`);
+    cy.injectAxeThenAxeCheck();
+
+    cy.findByText(/continue/i, { selector: 'va-button' }).click();
+    cy.get('va-radio')
+      .shadow()
+      .find('#error-message')
+      .contains('You must choose a claim type');
+
+    cy.location('pathname').should('eq', `${BASE_URL}/start`);
+  });
+
+  it('should go to intro page when compensation is selected', () => {
+    cy.location('pathname').should('eq', `${BASE_URL}/start`);
+    cy.injectAxeThenAxeCheck();
+
+    cy.selectRadio('benefitType', 'compensation');
+    cy.findByText(/continue/i, { selector: 'va-button' }).click();
+
+    cy.location('pathname').should('eq', `${BASE_URL}/introduction`);
+  });
+
+  it('should go to non-compensation type page when another type is selected', () => {
+    cy.location('pathname').should('eq', `${BASE_URL}/start`);
+    cy.injectAxeThenAxeCheck();
+
+    cy.selectRadio('benefitType', 'other');
+    cy.findByText(/continue/i, { selector: 'va-button' }).click();
+
+    cy.location('pathname').should('eq', `${BASE_URL}/start`);
+    cy.get('h1').contains('Claim isn’t for a disability');
+    cy.findByText('Find the address for mailing your form', { selector: 'a' })
+      .should('have.attr', 'href')
+      .and(
+        'contain',
+        '/decision-reviews/supplemental-claim#find-addresses-for-other-benef-8804',
+      );
+    cy.findByText('Download VA Form 20-0995', { selector: 'a' })
+      .should('have.attr', 'href')
+      .and('contain', 'https://www.vba.va.gov/pubs/forms/VBA-20-0995-ARE.pdf');
+  });
+});
