da526461e83db8737e2d939b80b1d642302b7b3a
OptionSelect <brandyn.sullins@gmail.com>
2022-11-01 15:17:57 -0400

    Summary card base added

---
+++ src/applications/financial-status-report/pages/income/employmentEnhanced/EmploymentHistoryWidget.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router';
+import EmploymentHistorySummaryCard from '../../../components/EmploymentHistorySummaryCard';
+
+const EmploymentHistoryWidget = items => {
+  const formData = useSelector(state => state.form.data);
+  const employmentHistory =
+    formData.personalData.employmentHistory.veteran.employmentRecords || [];
+
+  return (
+    <>
+      <div className="vads-u-margin-top--3" data-testid="debt-list">
+        {employmentHistory.map((job, index) => (
+          <EmploymentHistorySummaryCard
+            key={`${index}-${job.employername}`}
+            job={job}
+          />
+        ))}
+      </div>
+      <Link
+        className="add-additional-job"
+        to={{
+          pathname: '/enhanced-employment-records',
+          search: `?index=${items.length}`,
+        }}
+      >
+        Add additional job
+      </Link>
+    </>
+  );
+};
+
+export default EmploymentHistoryWidget;
