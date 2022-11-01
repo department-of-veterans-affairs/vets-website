a4b529307a40783c580509b2254776f9db523406
fer.boza <fernando.boza@accenturefederal.com>
2022-11-01 14:53:20 -0400

    refactored with new ACTIONS and REDUCERS for TOE, and migrating filtering logic to InboxPage.jsx

--- src/applications/education-letters/actions/index.js
+++ src/applications/education-letters/actions/index.js
@@ -1,40 +1,37 @@
 import { apiRequest } from 'platform/utilities/api';
 import environment from 'platform/utilities/environment';
 
-export const MEB_CLAIM_STATUS_ENDPOINT = `${
-  environment.API_URL
-}/meb_api/v0/claim_status`;
-export const TOE_CLAIM_STATUS_ENDPOINT = `${
-  environment.API_URL
-}/meb_api/v0/forms_claim_status`;
+// export const MEB_CLAIM_STATUS_ENDPOINT = `${environment.API_URL}/meb_api/v0/claim_status`;
+// export const TOE_CLAIM_STATUS_ENDPOINT = `${environment.API_URL}/meb_api/v0/forms_claim_status`;
 
-export const FETCH_CLAIM_STATUS = 'FETCH_CLAIM_STATUS';
-export const FETCH_CLAIM_STATUS_SUCCESS = 'FETCH_CLAIM_STATUS_SUCCESS';
-export const FETCH_CLAIM_STATUS_FAILED = 'FETCH_CLAIM_STATUS_FAILED';
+const CLAIM_ENDPOINTS = {
+  MEB: `${environment.API_URL}/meb_api/v0/claim_status`,
+  TOE: `${environment.API_URL}/meb_api/v0/forms_claim_status`,
+};
 
-const fetchAPIs = (endpoints, dispatch) =>
-  endpoints.map(endpoint =>
-    apiRequest(endpoint)
+export const MEB_FETCH_CLAIM_STATUS = 'MEB_FETCH_CLAIM_STATUS';
+export const MEB_FETCH_CLAIM_STATUS_SUCCESS = 'MEB_FETCH_CLAIM_STATUS_SUCCESS';
+export const MEB_FETCH_CLAIM_STATUS_FAILED = 'MEB_FETCH_CLAIM_STATUS_FAILED';
+
+export const TOE_FETCH_CLAIM_STATUS = 'TOE_FETCH_CLAIM_STATUS';
+export const TOE_FETCH_CLAIM_STATUS_SUCCESS = 'TOE_FETCH_CLAIM_STATUS_SUCCESS';
+export const TOE_FETCH_CLAIM_STATUS_FAILED = 'TOE_FETCH_CLAIM_STATUS_FAILED';
+
+export function fetchClaimStatus(statusType) {
+  return dispatch => {
+    dispatch({ type: `${statusType}_FETCH_CLAIM_STATUS` });
+    return apiRequest(CLAIM_ENDPOINTS[statusType])
       .then(response => {
         dispatch({
-          type: FETCH_CLAIM_STATUS_SUCCESS,
+          type: `${statusType}_FETCH_CLAIM_STATUS_SUCCESS`,
           response,
         });
       })
       .catch(errors => {
         dispatch({
-          type: FETCH_CLAIM_STATUS_FAILED,
+          type: `${statusType}_FETCH_CLAIM_STATUS_FAILED`,
           errors,
         });
-      }),
-  );
-
-export function fetchClaimStatus() {
-  return async dispatch => {
-    dispatch({ type: FETCH_CLAIM_STATUS });
-    return fetchAPIs(
-      [MEB_CLAIM_STATUS_ENDPOINT, TOE_CLAIM_STATUS_ENDPOINT],
-      dispatch,
-    );
+      });
   };
 }
--- src/applications/education-letters/reducers/index.js
+++ src/applications/education-letters/reducers/index.js
@@ -1,38 +1,46 @@
 import {
-  FETCH_CLAIM_STATUS,
-  FETCH_CLAIM_STATUS_FAILED,
-  FETCH_CLAIM_STATUS_SUCCESS,
+  MEB_FETCH_CLAIM_STATUS,
+  MEB_FETCH_CLAIM_STATUS_FAILED,
+  MEB_FETCH_CLAIM_STATUS_SUCCESS,
+  TOE_FETCH_CLAIM_STATUS,
+  TOE_FETCH_CLAIM_STATUS_FAILED,
+  TOE_FETCH_CLAIM_STATUS_SUCCESS,
 } from '../actions';
 
 const initialState = {};
 
-const getLatestClaim = action => {
-  return {
-    ...action?.response?.data?.attributes.reduce(
-      (currentDate, nextDate) =>
-        currentDate?.claimStatus?.receivedDate >
-        nextDate?.claimStatus?.receivedDate
-          ? currentDate
-          : nextDate,
-    ),
-  };
-};
-
 export default {
   data: (state = initialState, action) => {
     switch (action.type) {
-      case FETCH_CLAIM_STATUS:
+      case MEB_FETCH_CLAIM_STATUS:
         return {
           ...state,
-          claimStatusFetchInProgress: true,
+          MEBClaimStatusFetchInProgress: true,
         };
-      case FETCH_CLAIM_STATUS_SUCCESS:
-      case FETCH_CLAIM_STATUS_FAILED:
+      case MEB_FETCH_CLAIM_STATUS_SUCCESS:
+      case MEB_FETCH_CLAIM_STATUS_FAILED:
         return {
           ...state,
-          claimStatusFetchComplete: true,
-          claimStatusFetchInProgress: false,
-          claimStatus: getLatestClaim(action),
+          MEBClaimStatusFetchComplete: true,
+          MEBClaimStatusFetchInProgress: false,
+          MEBClaimStatus: {
+            ...action?.response?.data?.attributes,
+          },
+        };
+      case TOE_FETCH_CLAIM_STATUS:
+        return {
+          ...state,
+          TOEClaimStatusFetchInProgress: true,
+        };
+      case TOE_FETCH_CLAIM_STATUS_SUCCESS:
+      case TOE_FETCH_CLAIM_STATUS_FAILED:
+        return {
+          ...state,
+          TOEClaimStatusFetchComplete: true,
+          TOEClaimStatusFetchInProgress: false,
+          TOEClaimStatus: {
+            ...action?.response?.data?.attributes,
+          },
         };
       default:
         return state;
