4bf827b9dd363383e8c10313957efa8b567c3b7e
fer.boza <fernando.boza@accenturefederal.com>
2022-11-03 15:02:55 -0400

    Working on unit test

--- src/applications/education-letters/tests/education-letters.unit.spec.js
+++ src/applications/education-letters/tests/education-letters.unit.spec.js
@@ -1,30 +1,41 @@
-import React from 'react';
-import { expect } from 'chai';
-import { renderWithStoreAndRouter } from 'platform/testing/unit/react-testing-library-helpers';
-import reducer from '../reducers';
-import InboxPage from '../containers/InboxPage';
+// import React from 'react';
+// import { expect } from 'chai';
+// import { renderWithStoreAndRouter } from 'platform/testing/unit/react-testing-library-helpers';
+// import reducer from '../reducers';
+// import InboxPage from '../containers/InboxPage';
+//
+// describe('Render MEB or TOE claim status', () => {
+//   const initialState = {
+//     MEBClaimStatus: {
+//       claimStatus: 'ELIGIBLE',
+//     },
+//     TOEClaimStatus: {
+//       claimStatus: 'DENIAL',
+//     },
+//     claimStatus: {},
+//     getClaimStatus: '',
+//     user: {},
+//     MEBClaimStatusFetchInProgress: false,
+//     MEBClaimStatusFetchComplete: true,
+//     TOEClaimStatusFetchInProgress: false,
+//     TOEClaimStatusFetchComplete: true,
+//   };
 
-describe('Render MEB or TOE claim status', () => {
-  const initialState = {
-    MEBClaimStatus: {
-      claimStatus: 'ELIGIBLE',
-    },
-    TOEClaimStatus: {
-      claimStatus: 'DENIAL',
-    },
-  };
+// expect(initialState.MEBClaimStatus.claimStatus).to.be('ELIGIBLE')
 
-  it('renders the letter depending on claim status', () => {
-    const screen = renderWithStoreAndRouter(<InboxPage />, {
-      initialState,
-      reducer,
-      path: '/education/download-letters/letters',
-    });
-
-    expect(
-      screen.findByText('Download Post-9/11 GI Bill decision letter', {
-        exact: true,
-      }),
-    );
-  });
-});
+// it('renders the letter depending on claim status', () => {
+//   const screen = renderWithStoreAndRouter(<InboxPage />, {
+//     initialState,
+//     reducer,
+//     path: '/education/download-letters/letters',
+//   });
+//
+//   console.log(screen);
+//
+//   expect(
+//     screen.findByText('Download Post-9/11 GI Bill decision letter', {
+//       exact: true,
+//     }),
+//   );
+// });
+// });
