ea1968198fe65ef359174540c26dcb93382fc595
Jesse Anderson <jesse.c.anderson@icloud.com>
2022-11-04 14:56:25 -0400

    reverting a change that broke building the form

--- src/applications/1010d/app-entry.jsx
+++ src/applications/1010d/app-entry.jsx
@@ -1,7 +1,7 @@
-import '@department-of-veterans-affairs/platform-polyfills';
+import 'platform/polyfills';
 import './sass/1010d.scss';
 
-import startApp from '@department-of-veterans-affairs/platform-startup';
+import startApp from 'platform/startup';
 
 import routes from './routes';
 import reducer from './reducers';
