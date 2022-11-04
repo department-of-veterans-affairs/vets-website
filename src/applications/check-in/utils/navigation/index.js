5810c919537b87084dc5abb49c5982a885d1e00d
Brian Seek <brian.seek@civicactions.com>
2022-11-04 14:54:16 -0700

    Moved allow list to app constants and added better comment on check.

--- src/applications/check-in/utils/navigation/index.js
+++ src/applications/check-in/utils/navigation/index.js
@@ -1,11 +1,10 @@
 import { differenceInCalendarDays } from 'date-fns';
+import { travelAllowList } from '../appConstants';
 
 const isWithInDays = (days, pageLastUpdated) => {
   const daysAgo = differenceInCalendarDays(Date.now(), pageLastUpdated);
   return daysAgo <= days;
 };
-
-const travelAllowList = ['0001'];
 
 const updateFormPages = (
   patientDemographicsStatus,
@@ -62,6 +61,7 @@ const updateFormPages = (
   ];
 
   // Skip travel pay if not enabled, if veteran has more than one appointment for the day, or station if not in the allow list.
+  // The allowlist currently only looks at the first appointment in the array, if we support multiple appointments later, this will need to get updated to a loop.
   if (
     !isTravelReimbursementEnabled ||
     appointments.length > 1 ||
