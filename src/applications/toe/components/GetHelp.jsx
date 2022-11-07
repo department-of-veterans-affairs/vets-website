4db53fdac2a9eab94d609d38c12ec4475c67d036
fer.boza <fernando.boza@accenturefederal.com>
2022-11-07 16:53:08 -0500

    Updating AskVA line in GetHelp.jsx

--- src/applications/toe/components/GetHelp.jsx
+++ src/applications/toe/components/GetHelp.jsx
@@ -6,10 +6,12 @@ export default function GetHelp() {
       <p className="help-talk">
         If you need help with your application or have questions about
         enrollment or eligibility, submit a request with{' '}
-        <a target="_blank" href="/contact-us">
+        <a target="_blank" href="https://ask.va.gov/" rel="noreferrer">
           {' '}
           Ask VA.
         </a>{' '}
+        <br />
+        <br />
         If you have technical difficulties using this online application, call
         our MyVA411 main information line at{' '}
         <va-telephone contact="8006982411" /> (TTY:{' '}
