39aa7ddd964eb6f0d1ca4f024e5f5c3e43cc0050
Robert <roberto.rodriguez@bylight.com>
2022-11-07 12:05:15 -0500

    [MHV 33588] changed the sort values to constants

--- src/applications/mhv/secure-messaging/components/MessageList/MessageList.jsx
+++ src/applications/mhv/secure-messaging/components/MessageList/MessageList.jsx
@@ -24,6 +24,12 @@ import {
 import { useLocation } from 'react-router-dom';
 import InboxListItem from './MessageListItem';
 
+const DESCENDING = 'desc';
+const ASCENDING = 'asc';
+const SENDER_ALPHA_ASCENDING = 'sender-alpha-asc';
+const SENDER_ALPHA_DESCENDING = 'sender-alpha-desc';
+const RECEPIENT_ALPHA_ASCENDING = 'recepient-alpha-asc';
+const RECEPIENT_ALPHA_DESCENDING = 'recepient-alpha-desc';
 // Arbitrarily set because the VaPagination component has a required prop for this.
 // This value dictates how many pages are displayed in a pagination component
 const MAX_PAGE_LIST_LENGTH = 5;
@@ -61,27 +67,27 @@ const MessageList = props => {
   const sortMessages = useCallback(
     data => {
       let sorted;
-      if (sortOrder === 'desc') {
+      if (sortOrder === DESCENDING) {
         sorted = data.sort((a, b) => {
           return b.sentDate > a.sentDate ? 1 : -1;
         });
-      } else if (sortOrder === 'asc') {
+      } else if (sortOrder === ASCENDING) {
         sorted = data.sort((a, b) => {
           return a.sentDate > b.sentDate ? 1 : -1;
         });
-      } else if (sortOrder === 'alpha-asc') {
+      } else if (sortOrder === SENDER_ALPHA_ASCENDING) {
         sorted = data.sort((a, b) => {
           return a.senderName > b.senderName ? 1 : -1;
         });
-      } else if (sortOrder === 'alpha-desc') {
+      } else if (sortOrder === SENDER_ALPHA_DESCENDING) {
         sorted = data.sort((a, b) => {
           return a.senderName < b.senderName ? 1 : -1;
         });
-      } else if (sortOrder === 'recepient-alpha-asc') {
+      } else if (sortOrder === RECEPIENT_ALPHA_ASCENDING) {
         sorted = data.sort((a, b) => {
           return a.recipientName > b.recipientName ? 1 : -1;
         });
-      } else if (sortOrder === 'recepient-alpha-desc') {
+      } else if (sortOrder === RECEPIENT_ALPHA_DESCENDING) {
         sorted = data.sort((a, b) => {
           return a.recipientName < b.recipientName ? 1 : -1;
         });
@@ -137,20 +143,23 @@ const MessageList = props => {
             handleOnSelect(e);
           }}
         >
-          <option value="desc">Newest to oldest</option>
-          <option value="asc">Oldest to newest</option>
-
+          <option value={DESCENDING}>Newest to oldest</option>
+          <option value={ASCENDING}>Oldest to newest</option>
           {location.pathname !== '/sent' && location.pathname !== '/drafts' ? (
             <>
-              <option value="alpha-asc">A to Z - Sender’s name</option>
-              <option value="alpha-desc">Z to A - Sender’s name</option>
+              <option value={SENDER_ALPHA_ASCENDING}>
+                A to Z - Sender’s name
+              </option>
+              <option value={SENDER_ALPHA_DESCENDING}>
+                Z to A - Sender’s name
+              </option>
             </>
           ) : (
             <>
-              <option value="recepient-alpha-asc">
+              <option value={RECEPIENT_ALPHA_ASCENDING}>
                 A to Z - Recipient’s name
               </option>
-              <option value="recepient-alpha-desc">
+              <option value={RECEPIENT_ALPHA_DESCENDING}>
                 Z to A - Recipient’s name
               </option>
             </>
