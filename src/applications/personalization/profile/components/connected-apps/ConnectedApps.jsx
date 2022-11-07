bb75f290891ac765df55ac0758479ae64050d0ac
Adam Whitlock <adam.whitlock@adhocteam.us>
2022-11-07 12:36:16 -0700

    (chore) migrate loading comp, fix import linting warn

--- src/applications/personalization/profile/components/connected-apps/ConnectedApps.jsx
+++ src/applications/personalization/profile/components/connected-apps/ConnectedApps.jsx
@@ -2,15 +2,16 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { isEmpty } from 'lodash';
-import LoadingIndicator from '@department-of-veterans-affairs/component-library/LoadingIndicator';
+
+import { VaLoadingIndicator } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 
 import {
   deleteConnectedApp,
   dismissDeletedAppAlert,
   loadConnectedApps,
 } from '@@profile/components/connected-apps/actions';
-import recordEvent from 'platform/monitoring/record-event';
-import { focusElement } from 'platform/utilities/ui';
+import { recordEvent } from '@department-of-veterans-affairs/platform-monitoring/exports';
+import { focusElement } from '@department-of-veterans-affairs/platform-utilities/ui';
 import Headline from '../ProfileSectionHeadline';
 import { AppDeletedAlert } from './AppDeletedAlert';
 import { ConnectedApp } from './ConnectedApp';
@@ -146,7 +147,10 @@ export class ConnectedApps extends Component {
         )}
 
         {loading && (
-          <LoadingIndicator setFocus message="Loading your connected apps..." />
+          <VaLoadingIndicator
+            setFocus
+            message="Loading your connected apps..."
+          />
         )}
         {!isEmpty(disconnectErrorApps) &&
           disconnectErrorApps.map(app => (
