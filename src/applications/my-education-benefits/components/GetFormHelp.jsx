578bde5db70b7280beec6dca898577009df664ea
fer.boza <fernando.boza@accenturefederal.com>
2022-11-07 16:23:54 -0500

    Updating AskVA link and added a link break in GetFormHelp.jsx

--- src/applications/my-education-benefits/components/GetFormHelp.jsx
+++ src/applications/my-education-benefits/components/GetFormHelp.jsx
@@ -11,10 +11,11 @@ function GetFormHelp({ showMebDgi40Features }) {
         <>
           If you need help with your application or have questions about
           enrollment or eligibility, submit a request with{' '}
-          <a target="_blank" href="/contact-us">
+          <a target="_blank" href="https://ask.va.gov/" rel="noreferrer">
             {' '}
             Ask VA.{' '}
           </a>{' '}
+          <br />
           If you have technical difficulties using this online application, call
           our MyVA411 main information line at{' '}
           <va-telephone contact="8006982411" /> (TTY:{' '}
