67660821f46f8e81fb71943e182e06f03bad52cf
Brian Seek <brian.seek@civicactions.com>
2022-11-07 09:09:16 -0800

    Check in/47829/persist data (#22518)
    * Refactored to AppWrapper component and added to day of route building.
    
    * Removed AppInit
    
    * Added util for URL check.
    
    * Added test for nav util.
    
    * Updated unit tests.
    
    * Added e2e tests.
    
    * Added unit test for setForm.
    
    * Updated weird/broken refresh test.
    
    * Set refresh to false always in useEffect.
    
    * Also hold render on isLoading.
    
    * Refactored useGetCheckInData to use an object as vars.
    
    * AppWrapper linting issues.
    
    * var name change.
    
    * Build URL with appendQuery.
    
    * Update var name.
    
    * Fixed variable issue in useCheckinData call
    
    * Updated reload cypress tests to use new framework.
    
    * Moved error useEffect up and added gate for setRefreshData to prevent premature rendering of children.
    
    * Added cypress tests for confirmation page relaod.
    
    * Added missing items from dependancy arrays.
    
    * Added control for when to resubmit demographics and not do it on reload.
    
    * Swapped setStale and setLoadign order to prevent duplicate api calls.
    
    * Updated dep arrays for yes/no callbacks

--- src/applications/check-in/utils/navigation/index.js
+++ src/applications/check-in/utils/navigation/index.js
@@ -86,4 +86,17 @@ const URLS = Object.freeze({
   TRAVEL_MILEAGE: 'travel-mileage',
 });
 
-export { updateFormPages, URLS };
+const isAnInternalPage = location => {
+  let valid = false;
+  const url = location.split('/');
+  // Analyze the part of the URL where internal pages are.
+  const pageFromUrl = url[3];
+  if (pageFromUrl && Object.values(URLS).includes(pageFromUrl)) {
+    // Any page except verify is valid.
+    valid = pageFromUrl !== 'verify';
+  }
+
+  return valid;
+};
+
+export { updateFormPages, URLS, isAnInternalPage };
