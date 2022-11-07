3dc08e46511362cb049b41829d928f1a907accac
jtmst <josh.troy.mills@gmail.com>
2022-11-07 15:12:24 -0500

    adjust search unit test

--- src/platform/site-wide/header/components/Search/index.unit.spec.js
+++ src/platform/site-wide/header/components/Search/index.unit.spec.js
@@ -11,10 +11,7 @@ describe('Header <Search>', () => {
     const wrapper = shallow(<Search />);
 
     // Assertions.
-    expect(wrapper.find('form')).to.have.length(1);
-    expect(wrapper.find('label')).to.have.length(1);
-    expect(wrapper.find('input[type="text"]')).to.have.length(1);
-    expect(wrapper.find('button[type="submit"]')).to.have.length(1);
+    expect(wrapper.find('SearchDropdownComponent')).to.have.length(1);
 
     // Clean up.
     wrapper.unmount();
