f76f11b0320c272d91f24974371a7999f4b29ac7
Robin Garrison <robin.garrison@adhocteam.us>
2022-11-08 10:59:25 -0600

    Update opt-in page content

---
+++ src/applications/appeals/995/content/optIn.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const optInDescription = (
+  <div>
+    <h3>Opt-in to the new decision review process</h3>
+    <p>
+      If you’re filing a Supplemental Claim for an issue in an initial claim
+      that we decided before February 19, 2019, you’ll need to opt in to the new
+      decision review process. When you do this, we’ll move your issue from the
+      old appeals process to the new decision review process. You’re likely to
+      get a faster decision on your claim when you switch to the new process.
+    </p>
+  </div>
+);
+
+export const optInLabel = (
+  <strong>
+    I understand that I'm opting in to the new decision review process. This
+    means any isues I want reviewed from the old appeals process will move into
+    the new decision review process.
+  </strong>
+);
