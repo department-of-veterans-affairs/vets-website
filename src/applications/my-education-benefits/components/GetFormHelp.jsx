38af3887ea67748441bdaf0f9f1e16100c7f9bd8
fer.boza <fernando.boza@accenturefederal.com>
2022-11-08 14:38:43 -0500

    worked on text formatting and spacing.

--- src/applications/my-education-benefits/components/GetFormHelp.jsx
+++ src/applications/my-education-benefits/components/GetFormHelp.jsx
@@ -15,13 +15,13 @@ function GetFormHelp({ showMebDgi40Features }) {
             {' '}
             Ask VA.{' '}
           </a>{' '}
-          <br />
-          <br />
-          If you have technical difficulties using this online application, call
-          our MyVA411 main information line at{' '}
-          <va-telephone contact="8006982411" /> (TTY:{' '}
-          <va-telephone contact="711" />
-          ). We're here 24/7.
+          <p>
+            If you have technical difficulties using this online application,
+            call our MyVA411 main information line at{' '}
+            <va-telephone contact="8006982411" /> (TTY:{' '}
+            <va-telephone contact="711" />
+            ). We're here 24/7.
+          </p>
         </>
       ) : (
         <>
