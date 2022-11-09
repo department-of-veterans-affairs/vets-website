b1e9b314a14062ac39e1fcf6322d5dd050e12f9e
jerekshoe <jerek.shoemaker@adhocteam.us>
2022-11-09 12:11:23 -0600

    Fixing some accessibility issues

--- src/applications/claims-status/components/ClaimLetterList.jsx
+++ src/applications/claims-status/components/ClaimLetterList.jsx
@@ -3,11 +3,16 @@ import PropTypes from 'prop-types';
 
 import ClaimLetterListItem from './ClaimLetterListItem';
 
-const ClaimLetterList = ({ letters }) => {
-  return letters.map(letter => (
+const renderLetters = letters =>
+  letters.map(letter => (
     <ClaimLetterListItem key={letter.documentId} letter={letter} />
   ));
-};
+
+const ClaimLetterList = ({ letters }) => (
+  <ol className="vads-u-margin--0 vads-u-padding--0">
+    {renderLetters(letters)}
+  </ol>
+);
 
 ClaimLetterList.propTypes = {
   letters: PropTypes.array,
