49149f1b13c00d9117692a762cc1a013302061a0
jtmst <josh.troy.mills@gmail.com>
2022-11-09 10:52:12 -0500

    unit test revert

--- src/platform/site-wide/header/components/Search/index.unit.spec.js
+++ src/platform/site-wide/header/components/Search/index.unit.spec.js
@@ -11,7 +11,10 @@ describe('Header <Search>', () => {
     const wrapper = shallow(<Search />);
 
     // Assertions.
-    expect(wrapper.find('SearchDropdownComponent')).to.have.length(1);
+    expect(wrapper.find('form')).to.have.length(1);
+    expect(wrapper.find('label')).to.have.length(1);
+    expect(wrapper.find('input[type="text"]')).to.have.length(1);
+    expect(wrapper.find('button[type="submit"]')).to.have.length(1);
 
     // Clean up.
     wrapper.unmount();
