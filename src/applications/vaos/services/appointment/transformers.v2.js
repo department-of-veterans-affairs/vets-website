f21cb83bb283a016f1f78ffc0d7b526f8eddb115
Simi Adebowale <47654119+simiadebowale@users.noreply.github.com>
2022-11-08 22:04:10 -0500

    fix pending appts

--- src/applications/vaos/services/appointment/transformers.v2.js
+++ src/applications/vaos/services/appointment/transformers.v2.js
@@ -88,18 +88,13 @@ export function getAppointmentInfoFromComments(comments, key) {
   return data;
 }
 function getAppointmentType(appt) {
-  const commentsPreferredDate = getAppointmentInfoFromComments(
-    appt.reasonCode?.text,
-    'preferredDate',
-  );
-  const reqPeriods = commentsPreferredDate || appt.requestedPeriods;
   if (appt.kind === 'cc' && appt.start) {
     return APPOINTMENT_TYPES.ccAppointment;
   }
   if (appt.kind === 'cc' && appt.requestedPeriods?.length) {
     return APPOINTMENT_TYPES.ccRequest;
   }
-  if (appt.kind !== 'cc' && reqPeriods?.length) {
+  if (appt.kind !== 'cc' && appt.requestedPeriods?.length) {
     return APPOINTMENT_TYPES.request;
   }
 
