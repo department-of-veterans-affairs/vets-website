7ac92f833c9c3979baae6b43a2fac75d076c94fb
Robin Garrison <robin.garrison@adhocteam.us>
2022-11-09 11:19:26 -0600

    Revert evidence upload API change

--- src/applications/appeals/995/constants.js
+++ src/applications/appeals/995/constants.js
@@ -29,10 +29,10 @@ export const BENEFIT_OFFICES_URL = `${SC_INFO_URL}#find-addresses-for-other-bene
 export const CONTESTABLE_ISSUES_API =
   '/supplemental_claims/contestable_issues/';
 
-// Evidence upload API
+// Evidence upload API - same endpoint as NOD
 export const EVIDENCE_UPLOAD_API = `${
   environment.API_URL
-}/v1/decision_review_evidence?sub_type=SC`;
+}/v0/decision_review_evidence`;
 
 // key for contestedIssues to indicate that the user selected the issue
 export const SELECTED = 'view:selected';
