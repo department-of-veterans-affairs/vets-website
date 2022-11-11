51cc30c43e9b639e9d40ac36ae8173a479005a10
Brooks Johnson <brooks.johnson@pm.me>
2022-11-11 13:36:01 -0800

    Define remaining web components used in this app

--- src/applications/check-in/utils/defineWebComponents.js
+++ src/applications/check-in/utils/defineWebComponents.js
@@ -1,4 +1,18 @@
 import '@department-of-veterans-affairs/component-library/dist/main.css';
-import { defineCustomElementVaTelephone } from '@department-of-veterans-affairs/component-library/dist/components';
+import {
+  defineCustomElementVaTelephone,
+  defineCustomElementVaLoadingIndicator,
+  defineCustomElementVaAlert,
+  defineCustomElementVaAccordion,
+  defineCustomElementVaAccordionItem,
+  defineCustomElementVaTextInput,
+  defineCustomElementVaMemorableDate,
+} from '@department-of-veterans-affairs/component-library/dist/components';
 
 defineCustomElementVaTelephone();
+defineCustomElementVaLoadingIndicator();
+defineCustomElementVaAlert();
+defineCustomElementVaMemorableDate();
+defineCustomElementVaTextInput();
+defineCustomElementVaAccordion();
+defineCustomElementVaAccordionItem();
