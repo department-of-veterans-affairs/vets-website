88f9ecba3b2576b0b0603d4b91c25fabdf4c0270
John Woolschlager <wullaski@gmail.com>
2022-11-14 15:43:26 -0500

    Add TravelPayAlert BTSSS error handling component (#22608)
    * Roughed out a new travel pay alert component
    
    * Add cypress tests
    
    * Add unit test
    
    * Update mock data to match api
    
    * Update error codes in component
    
    * Update context for TestComponent tests
    
    * Add better date handling
    
    * Set default for error code
    
    * Update for finalized wireframes

--- src/applications/check-in/api/local-mock-api/mocks/v2/btsss/post.js
+++ src/applications/check-in/api/local-mock-api/mocks/v2/btsss/post.js
@@ -1,9 +1,30 @@
 const createMockSuccessResponse = _data => {
-  return { data: { claimId: 'BT10934849' }, status: 200 };
+  return {
+    data: {
+      value: {
+        claimNumber: 'TC202207000011666',
+      },
+      formatters: [],
+      contentTypes: [],
+      declaredType: null,
+      statusCode: 200,
+    },
+    status: 200,
+  };
 };
 
-const createMockFailedResponse = _data => {
-  return { data: { error: true } };
+const createMockFailedResponse = errorType => {
+  return {
+    data: {
+      error: true,
+      code: errorType,
+      message: '10/16/2020 : Error message',
+    },
+    status: 400,
+  };
 };
 
-module.exports = { createMockSuccessResponse, createMockFailedResponse };
+module.exports = {
+  createMockSuccessResponse,
+  createMockFailedResponse,
+};
