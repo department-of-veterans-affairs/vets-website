4c0e778bb1bb9b761b971b6f164d6429b17e33d8
Nathaniel Buck <nathaniel.buck@gmail.com>
2022-11-14 16:25:50 -0500

    Cleanup Help Markup (#22664)

--- src/applications/my-education-benefits/components/GetFormHelp.jsx
+++ src/applications/my-education-benefits/components/GetFormHelp.jsx
@@ -5,16 +5,17 @@ import { getAppData } from '../selectors/selectors';
 
 function GetFormHelp({ showMebDgi40Features }) {
   return (
-    <p className="help-talk">
+    <div className="help-talk">
       {showMebDgi40Features ? (
         <>
-          If you need help with your application or have questions about
-          enrollment or eligibility, submit a request with{' '}
-          <a target="_blank" href="https://ask.va.gov/" rel="noreferrer">
-            {' '}
-            Ask VA.{' '}
-          </a>{' '}
-          <p>
+          <p className="vads-u-margin-top--0">
+            If you need help with your application or have questions about
+            enrollment or eligibility, submit a request with{' '}
+            <a target="_blank" href="https://ask.va.gov/" rel="noreferrer">
+              Ask VA.
+            </a>
+          </p>
+          <p className="vads-u-margin-bottom--0">
             If you have technical difficulties using this online application,
             call our MyVA411 main information line at{' '}
             <va-telephone contact="8006982411" /> (TTY:{' '}
@@ -24,20 +25,23 @@ function GetFormHelp({ showMebDgi40Features }) {
         </>
       ) : (
         <>
-          If you need help with your application or have questions about
-          enrollment or eligibility, call our Education Call Center at{' '}
-          <va-telephone contact="8884424551" />. We're here Monday through
-          Friday, 8:00 a.m. to 7:00 p.m. ET. If you're outside the U.S., call us
-          at <va-telephone international contact="9187815678" />.<br />
-          <br />
-          If you have technical difficulties using this online application, call
-          our MyVA411 main information line at{' '}
-          <va-telephone contact="8006982411" /> (TTY:{' '}
-          <va-telephone contact="711" />
-          ). We're here 24/7.
+          <p className="vads-u-margin-top--0">
+            If you need help with your application or have questions about
+            enrollment or eligibility, call our Education Call Center at{' '}
+            <va-telephone contact="8884424551" />. We're here Monday through
+            Friday, 8:00 a.m. to 7:00 p.m. ET. If you're outside the U.S., call
+            us at <va-telephone international contact="9187815678" />.
+          </p>
+          <p className="vads-u-margin-bottom--0">
+            If you have technical difficulties using this online application,
+            call our MyVA411 main information line at{' '}
+            <va-telephone contact="8006982411" /> (TTY:{' '}
+            <va-telephone contact="711" />
+            ). We're here 24/7.
+          </p>
         </>
       )}
-    </p>
+    </div>
   );
 }
 
