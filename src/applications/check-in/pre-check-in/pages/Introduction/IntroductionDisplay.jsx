c7e0607198f579af281cc4b35cc9f553bb665f0d
Brian Seek <brian.seek@civicactions.com>
2022-11-15 10:10:45 -0800

    Refactored privacy statement from var to JSX children.

--- src/applications/check-in/pre-check-in/pages/Introduction/IntroductionDisplay.jsx
+++ src/applications/check-in/pre-check-in/pages/Introduction/IntroductionDisplay.jsx
@@ -59,13 +59,7 @@ const IntroductionDisplay = props => {
       ),
     },
   ];
-  const privacyStatement = (
-    <p>
-      {t(
-        'we-ask-you-to-provide-the-information-in-this-questionnaire-to-help-with-your-medical-care-under-law-38-u-s-c-chapter-17-its-your-choice-if-you-want-to-provide-this-information-if-you-choose-not-to-provide-this-information-it-may-make-it-harder-for-us-to-prepare-for-your-visit-but-it-wont-have-any-effect-on-your-eligibility-for-any-va-benefits-or-services-we-may-use-and-share-the-information-you-provide-in-this-questionnaire-in-the-ways-were-allowed-to-by-law-we-may-make-a-routine-use-disclosure-of-the-information-as-outlined-in-the-privacy-act-system-of-records-notice-in-24va10a7-patient-medical-record-va-and-following-the-veterans-health-administration-vha-notice-of-privacy-practices',
-      )}
-    </p>
-  );
+
   const StartButton = () => (
     <div
       className="vads-u-margin-bottom--4 vads-u-display--block"
@@ -147,7 +141,11 @@ const IntroductionDisplay = props => {
         visible={privacyActModalOpen}
         initialFocusSelector="button"
       >
-        {privacyStatement}
+        <p>
+          {t(
+            'we-ask-you-to-provide-the-information-in-this-questionnaire-to-help-with-your-medical-care-under-law-38-u-s-c-chapter-17-its-your-choice-if-you-want-to-provide-this-information-if-you-choose-not-to-provide-this-information-it-may-make-it-harder-for-us-to-prepare-for-your-visit-but-it-wont-have-any-effect-on-your-eligibility-for-any-va-benefits-or-services-we-may-use-and-share-the-information-you-provide-in-this-questionnaire-in-the-ways-were-allowed-to-by-law-we-may-make-a-routine-use-disclosure-of-the-information-as-outlined-in-the-privacy-act-system-of-records-notice-in-24va10a7-patient-medical-record-va-and-following-the-veterans-health-administration-vha-notice-of-privacy-practices',
+          )}
+        </p>
       </VaModal>
     </Wrapper>
   );
