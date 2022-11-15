243154209d66e9d859665c559b2027f995df66fc
Alexander Garcia <agarcia@clarityinnovates.com>
2022-11-15 16:35:11 -0500

    fix attempt for 22-1995 e2e test

--- src/applications/edu-benefits/1995/tests/e2e/edu-1995.cypress.spec.js
+++ src/applications/edu-benefits/1995/tests/e2e/edu-1995.cypress.spec.js
@@ -28,7 +28,13 @@ const form = createTestConfig(
         },
       });
       cy.get('@testData').then(testData => {
-        cy.intercept('GET', '/v0/in_progress_forms/22-1995', testData);
+        cy.intercept('GET', '/v0/in_progress_forms/22-1995', req => {
+          req.reply({
+            formData: testData,
+            metadata: { returnUrl: '/applicant/information' },
+          });
+        });
+        cy.intercept('PUT', '/v0/in_progress_forms/22-1995', testData);
       });
     },
     pageHooks: {
