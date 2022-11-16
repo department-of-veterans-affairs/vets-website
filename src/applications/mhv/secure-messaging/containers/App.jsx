d5cdd3a10ba6f0746a8a12488b9185858e818b04
Robert <roberto.rodriguez@bylight.com>
2022-11-16 12:25:59 -0500

    [MHV 36601] refactored the routes component to lead to the app component so that the app component can handle all the logic behind authorization, then I created another component for authorized routes

--- src/applications/mhv/secure-messaging/containers/App.jsx
+++ src/applications/mhv/secure-messaging/containers/App.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import LandingPageAuth from './LandingPageAuth';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import useInterval from '../hooks/use-interval';
+import AuthorizedRoutes from './AuthorizedRoutes';
 import LandingPageUnauth from './LandingPageUnauth';
+import MessageFAQs from './MessageFAQs';
+import SmBreadcrumbs from '../components/shared/SmBreadcrumbs';
+import Navigation from '../components/Navigation';
 
 const App = () => {
-  // const isLoggedIn = useSelector(state => state?.user.login.currentlyLoggedIn);
-  const isLoggedIn = true;
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    useSelector(state => state?.user.login.currentlyLoggedIn),
+  );
+  useInterval(() => {
+    setIsLoggedIn(!isLoggedIn);
+  }, 9000);
 
-  return isLoggedIn ? <LandingPageAuth /> : <LandingPageUnauth />;
+  const handleClick = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
+
+  return (
+    <div className="vads-l-grid-container">
+      <div className="vads-l-row breadcrumbs">
+        <SmBreadcrumbs />
+      </div>
+      <div className="secure-messaging-container vads-u-display--flex">
+        <div className="vads-u-flex--auto">
+          <Navigation />
+        </div>
+
+        <div className="vads-u-flex--fill">
+          <button type="button" onClick={handleClick}>
+            {isLoggedIn ? <>log out</> : <>log in</>}
+          </button>
+          <Switch>
+            <Route path="/faq" key="MessageFAQ">
+              <MessageFAQs isLoggedIn={isLoggedIn} />
+            </Route>
+            {isLoggedIn ? (
+              <AuthorizedRoutes isLoggedIn={isLoggedIn} />
+            ) : (
+              <LandingPageUnauth />
+            )}
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
