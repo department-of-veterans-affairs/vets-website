c99573ef0a7f1234d8c3af252eb824c5da368b70
tmitchellgcio <84803567+tmitchellgcio@users.noreply.github.com>
2022-11-16 11:38:07 -0500

    check access for copays and claims and rating (#22687)
    * check access for copays and claims and rating
    
    * check if we can access military history
    
    * add claims to test users
    
    * add claims to test users

--- src/applications/personalization/dashboard/components/Dashboard.jsx
+++ src/applications/personalization/dashboard/components/Dashboard.jsx
@@ -102,7 +102,9 @@ DashboardHeader.propTypes = {
 };
 
 const Dashboard = ({
+  canAccessMilitaryHistory,
   canAccessPaymentHistory,
+  canAccessRatingInfo,
   fetchFullName,
   fetchMilitaryInformation,
   fetchTotalDisabilityRating,
@@ -151,11 +153,17 @@ const Dashboard = ({
     () => {
       if (isLOA3) {
         fetchFullName();
-        fetchMilitaryInformation();
-        fetchTotalDisabilityRating();
+        if (canAccessMilitaryHistory) {
+          fetchMilitaryInformation();
+        }
+        if (canAccessRatingInfo) {
+          fetchTotalDisabilityRating();
+        }
       }
     },
     [
+      canAccessMilitaryHistory,
+      canAccessRatingInfo,
       isLOA3,
       fetchFullName,
       fetchMilitaryInformation,
@@ -338,6 +346,8 @@ const mapStateToProps = state => {
     isLOA3 &&
     isVAPatient;
   const canAccessPaymentHistory = canAccess(state)[API_NAMES.PAYMENT_HISTORY];
+  const canAccessRatingInfo = canAccess(state)[API_NAMES.RATING_INFO];
+  const canAccessMilitaryHistory = canAccess(state)[API_NAMES.MILITARY_HISTORY];
   const showBenefitPaymentsAndDebt =
     !showMPIConnectionError && !showNotInMPIError && isLOA3;
   const showBenefitPaymentsAndDebtV2 =
@@ -352,7 +362,9 @@ const mapStateToProps = state => {
     !showMPIConnectionError && !showNotInMPIError && isLOA3;
 
   return {
+    canAccessMilitaryHistory,
     canAccessPaymentHistory,
+    canAccessRatingInfo,
     isLOA3,
     showLoader,
     showValidateIdentityAlert,
@@ -377,6 +389,7 @@ const mapStateToProps = state => {
 
 Dashboard.propTypes = {
   canAccessPaymentHistory: PropTypes.bool,
+  canAccessRatingInfo: PropTypes.bool,
   fetchFullName: PropTypes.func,
   fetchMilitaryInformation: PropTypes.func,
   fetchTotalDisabilityRating: PropTypes.func,
--- src/applications/personalization/dashboard/utils/apiNames.js
+++ src/applications/personalization/dashboard/utils/apiNames.js
@@ -1,3 +1,11 @@
 export default Object.freeze({
   PAYMENT_HISTORY: 'paymentHistory',
+  CH33_BANK_ACCOUNTS: 'ch33BankAccounts',
+  COMMUNICATION_PREFERENCES: 'communicationPreferences',
+  CONNECTED_APPS: 'connectedApps',
+  MILITARY_HISTORY: 'militaryHistory',
+  PERSONAL_INFORMATION: 'personalInformation',
+  RATING_INFO: 'ratingInfo',
+  APPEALS: 'appeals',
+  MEDICAL_COPAYS: 'medicalCopays',
 });
