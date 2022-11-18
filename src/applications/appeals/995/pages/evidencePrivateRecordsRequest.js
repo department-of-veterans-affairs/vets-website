5062eb4909748acf274eac0e2fe5471c60012f8e
Robin Garrison <robin.garrison@adhocteam.us>
2022-11-18 08:44:53 -0600

    Cleanup private record request

--- src/applications/appeals/995/pages/evidencePrivateRecordsRequest.js
+++ src/applications/appeals/995/pages/evidencePrivateRecordsRequest.js
@@ -1,108 +1,13 @@
-import {
-  privateRecordsChoiceHelp,
-  patientAcknowledgmentTitle,
-  patientAcknowledgmentText,
-} from '../content/evidencePrivateRecordsRequest';
+import { EVIDENCE_PRIVATE } from '../constants';
 
 export default {
-  uiSchema: {
-    'ui:description':
-      'Now we’ll ask you about your private medical records for your claimed disability.',
-    'view:aboutPrivateMedicalRecords': {
-      'ui:title': 'About private medical records',
-      'ui:description': `If you have your private medical records, you can upload them to your
-        application. If you want us to get them for you, you’ll need to
-        authorize their release.`,
-    },
-    'view:uploadPrivateRecordsChoice': {
-      'view:hasPrivateRecordsToUpload': {
-        'ui:title': 'Do you want to upload your private medical records?',
-        'ui:widget': 'yesNo',
-        'ui:options': {
-          labels: {
-            Y: 'Yes',
-            N: 'No, please get my records from my doctor.',
-          },
-          widgetProps: {
-            N: {
-              'aria-describedby':
-                'root_view:patientAcknowledgement_view:acknowledgement',
-            },
-          },
-        },
-      },
-      'view:privateRecordsChoiceHelp': {
-        'ui:description': privateRecordsChoiceHelp,
-      },
-    },
-    'view:patientAcknowledgement': {
-      'ui:title': patientAcknowledgmentTitle,
-      'ui:options': {
-        expandUnder: 'view:uploadPrivateRecordsChoice',
-        expandUnderCondition: data =>
-          data?.['view:hasPrivateRecordsToUpload'] === false,
-        showFieldLabel: false,
-        // forceDivWrapper: true,
-      },
-      'view:acknowledgement': {
-        'ui:title': 'I acknowledge and authorize this release of information',
-        'ui:options': {
-          useDlWrap: true,
-        },
-      },
-      'view:helpText': {
-        'ui:title': ' ',
-        'ui:description': patientAcknowledgmentText,
-        'ui:options': {
-          forceDivWrapper: true,
-        },
-      },
-      'ui:validations': [
-        (errors, item) => {
-          if (!item['view:acknowledgement']) {
-            errors.addError('You must accept the acknowledgment');
-          }
-        },
-      ],
-    },
-  },
+  uiSchema: {},
 
   schema: {
     type: 'object',
     properties: {
-      'view:uploadPrivateRecordsChoice': {
-        required: ['view:hasPrivateRecordsToUpload'],
-        type: 'object',
-        properties: {
-          'view:aboutPrivateMedicalRecords': {
-            type: 'object',
-            properties: {},
-          },
-          'view:hasPrivateRecordsToUpload': {
-            type: 'boolean',
-          },
-          'view:privateRecordsChoiceHelp': {
-            type: 'object',
-            properties: {},
-          },
-        },
-      },
-      // Note: this property is misspelled. Should be
-      // `view:patientAcknowledgment` but isn't worth writing a migration for at
-      // this time since it's only used on this page.
-      'view:patientAcknowledgement': {
-        type: 'object',
-        required: ['view:acknowledgement'],
-        properties: {
-          'view:acknowledgement': {
-            type: 'boolean',
-            default: true,
-          },
-          'view:helpText': {
-            type: 'object',
-            properties: {},
-          },
-        },
+      [EVIDENCE_PRIVATE]: {
+        type: 'boolean',
       },
     },
   },
