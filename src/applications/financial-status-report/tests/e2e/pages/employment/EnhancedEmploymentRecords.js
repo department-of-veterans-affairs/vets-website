5c3c120e5ae9240796802a97957613c604b61dd2
OptionSelect <brandyn.sullins@gmail.com>
2022-11-28 15:47:15 -0500

    Adding validation tests

--- src/applications/financial-status-report/tests/e2e/pages/employment/EnhancedEmploymentRecords.js
+++ src/applications/financial-status-report/tests/e2e/pages/employment/EnhancedEmploymentRecords.js
@@ -19,6 +19,52 @@ class EnhancedEmploymentRecords {
       .click();
   };
 
+  failValidationEmployerFill = employer => {
+    cy.findByLabelText(/Type of work/).select(employer.type);
+    cy.findByLabelText(/Type of work/).select('');
+    cy.get('#errorable-select-1-error-message').should('exist');
+    cy.findByLabelText(/Type of work/).select(employer.type);
+    cy.get('#errorable-select-1-error-message').should('not.exist');
+
+    cy.fillDate('from', `2300-1`);
+    cy.get(':nth-child(2) > .hydrated')
+      .shadow()
+      .find('#error-message')
+      .should('exist')
+      .should('contain', 'start date.');
+
+    if (employer.isCurrent) {
+      cy.get(`input[name="current-employment"]`).check();
+
+      cy.get(':nth-child(2) > .hydrated')
+        .shadow()
+        .find('#error-message')
+        .should('exist')
+        .should('contain', '1900');
+
+      // Not overwriting values?
+      cy.fillDate(
+        'from',
+        `${employer.from.split('-')[0]}-${employer.from.split('-')[1]}`,
+      );
+    } else {
+      cy.fillDate(
+        'to',
+        `${employer.to.split('-')[0]}-${employer.to.split('-')[1]}`,
+      );
+    }
+
+    cy.get(`input[name="employerName"]`).type(employer.employerName);
+    cy.get(`input[name="employerName"]`).clear();
+    cy.get('#errorable-text-input-1-error-message')
+      .should('exist')
+      .should('contain', 'employer name');
+    cy.get(`input[name="employerName"]`).type(employer.employerName);
+    cy.findAllByText(/Continue/i, { selector: 'button' })
+      .first()
+      .click();
+  };
+
   addEmployer = () => {
     cy.get('.add-item-button').click();
   };
