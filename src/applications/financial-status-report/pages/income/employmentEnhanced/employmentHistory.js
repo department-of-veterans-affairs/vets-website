198999ec0956e98007e001c553ce5bc03a0a82c0
Brandyn Sullins <brandyn.sullins@gmail.com>
2022-11-29 16:30:26 -0500

    46674 - [efSR] - Enhancement - Continuous Add - Employment History  (#22793)
    * AddIssue example now implemented properly, begin conversion.
    
    * Updating with Andrew, making progress, have array list we think just figuring how to use
    
    * Remove unnecessary files
    
    * wip, checklist partially in place
    
    * Payroll deduction checklist added partially
    
    * Added placeholder logic for custom job title section on checklist and working on inputs
    
    * Title now goes to both pages correctly, still working on inputs, had to revert some changes because they broke
    
    * Update validation for true currency value based on other forms found in repo
    
    * Employment now works properly, fixed validation on gross income, begin work on history and looping
    
    * Fixed using wrong employer array for name
    
    * Fix error validation firing on gross montlhly input
    
    * Working with Andrew to fix payroll checklist and inputs
    
    * WORKING STATE
    
    * Summary card base added
    
    * Working with Mark on index work, edit is now in place
    
    * Rewrote enhanced file with Mark to use react and custom page logic.
    
    * Add working and now edit mostly working as well, big progress
    
    * Add and edit now work, begin work on conditionalizing other pages and syncing them up with index
    
    * Add edit now works, button in place for edit for now, begin work on final two page conversions before validation and testing
    
    * Now edit aware of state of deductions. Of note have to save a as button for edit for linter but it breaks behavior
    
    * Add edit now works, state saved on all pages, begin work on validation and testing
    
    * Replaced link with radios and now go back and forth properly. Begin work on test and validation
    
    * Fix text for radios
    
    * Updating tests for EFSR, test passes but doesnt fully complete sections yet
    
    * test now fills in all of our new fields
    
    * Adding validation tests
    
    * Update e2e tests for merge, need to fix unit to merge then update tests in later PR
    
    * Remove duplicate code block
    
    * Switched back to unit maximal and updated strings to test
    
    * Undoing the test changes to get back to clean slate of efsr unit maximal usage
    
    * Fix broken tests with Andrew
    
    * Repasting yarn lock from old commit because it changed

---
+++ src/applications/financial-status-report/pages/income/employmentEnhanced/employmentHistory.js
@@ -0,0 +1,23 @@
+import EmploymentHistoryWidget from './EmploymentHistoryWidget';
+
+export const uiSchema = {
+  'ui:title': 'Employment history',
+  employmentHistory: {
+    'ui:title': ' ',
+    'ui:field': 'StringField', // this is necessary, but shows type errors
+    'ui:widget': EmploymentHistoryWidget,
+  },
+};
+
+export const schema = {
+  type: 'object',
+  properties: {
+    employmentHistory: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {}, // we won't render any form elements here
+      },
+    },
+  },
+};
