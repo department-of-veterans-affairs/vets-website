befe7e2ffa64faff354064f5b9b4703e00083b0c
Bryan Paronto <bryan@kindsys.us>
2022-11-29 17:55:45 -0600

    11707 Refactor e2e tests.

--- src/applications/edu-benefits/tests/edu-apply-wizard.cypress.spec.js
+++ src/applications/edu-benefits/tests/edu-apply-wizard.cypress.spec.js
@@ -1,132 +1,162 @@
+function selectApplyForNewBenefit() {
+  cy.get('.wizard-button')
+    .click()
+    .get('label[for="newBenefit-0"]')
+    .should('be.visible');
+
+  cy.axeCheck();
+
+  cy.get('input[id="newBenefit-0"]').click();
+}
+
+function selectUpdateProgramOfStudy() {
+  cy.get('.wizard-button')
+    .click()
+    .get('label[for="newBenefit-1"]')
+    .should('be.visible');
+
+  cy.axeCheck();
+
+  cy.get('input[id="newBenefit-1"]').click();
+}
+
+function selectOwnEduBenefit() {
+  cy.get('label[for="transferredEduBenefits-0"]').should('be.visible');
+  cy.get('#transferredEduBenefits-0').click();
+}
+
+function selectTransferredEduBenefit() {
+  cy.get('label[for="transferredEduBenefits-1"]').should('be.visible');
+  cy.get('#transferredEduBenefits-1').click();
+}
+
+function selectFryScholarshipOrDEABenefit() {
+  cy.get('label[for="transferredEduBenefits-2"]').should('be.visible');
+  cy.get('#transferredEduBenefits-2').click();
+}
+
+function selectOwnServiceBasedBenefit() {
+  cy.get('label[for="serviceBenefitBasedOn-0"]').should('be.visible');
+  cy.get('#serviceBenefitBasedOn-0').click();
+}
+
+function selectOthersServiceBasedBenefit() {
+  cy.get('label[for="serviceBenefitBasedOn-1"]').should('be.visible');
+  cy.get('#serviceBenefitBasedOn-1').click();
+}
+
+function selectTransferredBenefits() {
+  cy.get('label[for="sponsorTransferredBenefits-0"]').should('be.visible');
+  cy.get('#sponsorTransferredBenefits-0')
+    .click()
+    .get('#apply-now-link')
+    .should('be.visible');
+}
+
+function selectNonTransferredBenefits() {
+  cy.get('label[for="sponsorTransferredBenefits-1"]').should('be.visible');
+  cy.get('#sponsorTransferredBenefits-1').click();
+}
+
+function selectDeceasedDisabledMIASponsor() {
+  cy.get('label[for="sponsorDeceasedDisabledMIA-0"]').should('be.visible');
+  cy.get('#sponsorDeceasedDisabledMIA-0').click();
+}
+
+function selectNonDeceasedDisabledMIASponsor() {
+  cy.get('label[for="sponsorDeceasedDisabledMIA-1"]').should('be.visible');
+  cy.get('#sponsorDeceasedDisabledMIA-1').click();
+}
+
+function assertWarningIsShown() {
+  cy.get('main .usa-alert-warning').should('be.visible');
+}
+
+function assertApplyButtonShowsForForm(form) {
+  cy.get('#apply-now-link')
+    .should('have.attr', 'href')
+    .and(
+      'contain',
+      `/education/apply-for-education-benefits/application/${form}`,
+    );
+}
+
+function selectVETTECCourses() {
+  cy.get('label[for="vetTecBenefit-0"]').should('be.visible');
+  cy.get('#vetTecBenefit-0').click();
+}
+
+function selectNonVETTECCourses() {
+  cy.get('label[for="vetTecBenefit-1"]').should('be.visible');
+  cy.get('#vetTecBenefit-1').click();
+}
+
 describe('Education Application Wizard', () => {
-  it('Goes through the education wizard', () => {
-    // Ensure education apply-wizard page renders.
-    // Open education apply wizard
+  // Ensure education apply-wizard page renders.
+  beforeEach(() => {
     cy.visit(`/education/how-to-apply/`)
       .get('body')
       .should('be.visible');
     cy.injectAxeThenAxeCheck();
+  });
 
-    cy.get('.wizard-button')
-      .click()
-      .get('label[for="newBenefit-0"]')
-      .should('be.visible');
-    cy.axeCheck();
-
-    // Create a new application
-    cy.get('input[id="newBenefit-0"]')
-      .click()
-      .get('label[for="serviceBenefitBasedOn-1"]')
-      .should('be.visible');
-
-    // Select veteran
-    // cy.get('input[id="serviceBenefitBasedOn-0"]')
-    //   .click()
-    //   .get('label[for="nationalCallToService-0"]')
-    //   .should('be.visible');
-
-    // Select national call to service
-    // cy.get('#nationalCallToService-0')
-    //   .click()
-    //   .get('#apply-now-link')
-    //   .should('be.visible');
-
-    // cy.get('#apply-now-link')
-    //   .should('have.attr', 'href')
-    //   .and(
-    //     'contain',
-    //     '/education/apply-for-education-benefits/application/1990N',
-    //   );
-
-    // cy.get('main .usa-alert-warning').should('be.visible');
-
-    // Select non-veteran
-    cy.get('#serviceBenefitBasedOn-1').click();
-
-    cy.get('#apply-now-link').should('not.exist');
-
-    cy.get('main .usa-alert-warning').should('not.exist');
-
-    cy.get('label[for="sponsorTransferredBenefits-0"]').should('be.visible');
-
-    cy.injectAxeThenAxeCheck();
-
-    // Select dependent
-    cy.get('#sponsorTransferredBenefits-0')
-      .click()
-      .get('#apply-now-link')
-      .should('be.visible');
-
+  it('flow for New Benefits, Own Service, w/ VET TEC', () => {
+    selectApplyForNewBenefit();
+    selectOwnServiceBasedBenefit();
+    selectVETTECCourses();
     cy.get('#apply-now-link')
       .should('have.attr', 'href')
       .and(
         'contain',
-        '/education/apply-for-education-benefits/application/1990E',
+        '/education/about-gi-bill-benefits/how-to-use-benefits/vettec-high-tech-program/apply-for-vettec-form-22-0994',
       );
+  });
 
-    // Select non-dependant
-    cy.get('#sponsorTransferredBenefits-1').click();
-    cy.get('#apply-now-link').should('not.exist');
+  it('flow for New Benefits, Own Service, w/o VET TEC', () => {
+    selectApplyForNewBenefit();
+    selectOwnServiceBasedBenefit();
+    selectNonVETTECCourses();
+    assertApplyButtonShowsForForm('1990');
+  });
 
-    cy.get('#sponsorTransferredBenefits-1')
-      .get('label[for="sponsorDeceasedDisabledMIA-0"]')
-      .should('be.visible');
-    cy.axeCheck();
+  it('flow for New Benefits, Others Service w/ Transferred Benefits', () => {
+    selectApplyForNewBenefit();
+    selectOthersServiceBasedBenefit();
+    selectTransferredBenefits();
+    assertApplyButtonShowsForForm('1990E');
+  });
 
-    // Select transfer
-    cy.get('#sponsorDeceasedDisabledMIA-0')
-      .click()
-      .get('#apply-now-link')
-      .should('be.visible');
+  it('flow for New Benefits, Others Service w/o Transferred Benefits and w/ deceased, disabled, MIA/POW sponsor', () => {
+    selectApplyForNewBenefit();
+    selectOthersServiceBasedBenefit();
+    selectNonTransferredBenefits();
+    selectDeceasedDisabledMIASponsor();
+    assertApplyButtonShowsForForm('5490');
+  });
 
-    cy.get('#apply-now-link')
-      .should('have.attr', 'href')
-      .and(
-        'contain',
-        '/education/apply-for-education-benefits/application/5490',
-      );
+  it('flow for New Benefits, Others Service w/o Transferred Benefits and w/o deceased, disabled, MIA/POW sponsor', () => {
+    selectApplyForNewBenefit();
+    selectOthersServiceBasedBenefit();
+    selectNonTransferredBenefits();
+    selectNonDeceasedDisabledMIASponsor();
+    assertWarningIsShown();
+  });
 
-    // Select non-transfer
-    cy.get('#sponsorDeceasedDisabledMIA-1').click();
-    cy.get('main .usa-alert-warning').should('be.visible');
+  it('flow for Updating Program of Study, Using own Benefits', () => {
+    selectUpdateProgramOfStudy();
+    selectOwnEduBenefit();
+    assertApplyButtonShowsForForm('1995');
+  });
 
-    // Update an existing application
-    cy.get('#newBenefit-1')
-      .click()
-      .get('label[for="transferredEduBenefits-0"]')
-      .should('be.visible');
+  it('flow for Updating Program of Study, Using transferred Benefits', () => {
+    selectUpdateProgramOfStudy();
+    selectTransferredEduBenefit();
+    assertApplyButtonShowsForForm('1995');
+  });
 
-    cy.get('#apply-now-link').should('not.exist');
-
-    // Select dependent
-    cy.get('#transferredEduBenefits-2')
-      .click()
-      .get('#apply-now-link')
-      .should('be.visible');
-    cy.get('main .usa-alert-warning').should('not.exist');
-
-    cy.get('#apply-now-link')
-      .should('have.attr', 'href')
-      .and(
-        'contain',
-        '/education/apply-for-education-benefits/application/5495',
-      );
-    cy.get('#transferredEduBenefits-0')
-      .click()
-      .get('#apply-now-link')
-      .should('be.visible');
-
-    cy.get('#apply-now-link')
-      .should('have.attr', 'href')
-      .and(
-        'contain',
-        '/education/apply-for-education-benefits/application/1995',
-      );
-    cy.get('#apply-now-link').click();
-
-    cy.url().should(
-      'contain',
-      '/education/apply-for-education-benefits/application/1995/introduction',
-    );
+  it('flow for Updating Program of Study, Using Fry Scholarship', () => {
+    selectUpdateProgramOfStudy();
+    selectFryScholarshipOrDEABenefit();
+    assertApplyButtonShowsForForm('5495');
   });
 });
