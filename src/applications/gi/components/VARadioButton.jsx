c9bb6d840c31e6527fe76e53dd63103ba3184484
Napoleon Kernessant <95312667+GcioNapoleon@users.noreply.github.com>
2022-11-29 10:52:53 -0600

    Remove initial value for the VARadioButton component.

--- src/applications/gi/components/VARadioButton.jsx
+++ src/applications/gi/components/VARadioButton.jsx
@@ -14,33 +14,22 @@ import {
  * @param {Function} onRadioOptionSelected - updates subtask options
  * @returns {JSX.Element}
  */
-const VARadioButton = ({
-  radioLabel,
-  initialValue,
-  options,
-  onRadioOptionSelected,
-}) => (
+const VARadioButton = ({ radioLabel, options, onRadioOptionSelected }) => (
   <VaRadio
     enable-analytics
     label={radioLabel}
     onRadioOptionSelected={onRadioOptionSelected}
   >
     {options.map(({ value, label }) => (
-      <VaRadioOption
-        key={value}
-        id={value}
-        value={value}
-        label={label}
-        checked={value === initialValue}
-      />
+      <VaRadioOption key={value} id={value} value={value} label={label} />
     ))}
   </VaRadio>
 );
 
 VARadioButton.propTypes = {
   initialValue: PropTypes.string.isRequired,
-  onRadioOptionSelected: PropTypes.func.isRequired,
   radioLabel: PropTypes.string.isRequired,
+  onRadioOptionSelected: PropTypes.func.isRequired,
   options: PropTypes.arrayOf(
     PropTypes.shape({
       value: PropTypes.string,
