34aad99dbcc17051491f2eab01e7e57269acc3cf
fer.boza <fernando.boza@accenturefederal.com>
2022-11-30 09:05:14 -0500

    Working on actions

--- src/applications/my-education-benefits/tests/units/actions.unit.spec.js
+++ src/applications/my-education-benefits/tests/units/actions.unit.spec.js
@@ -1,8 +1,6 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 
-// import * as vapSvcActions from '@@vap-svc/actions';
-
 import { mockFetch, setFetchJSONResponse } from 'platform/testing/unit/helpers';
 import * as mockPersonalInfo from '../../testing/responses';
 
@@ -16,28 +14,23 @@ describe('actions/personalInformation', () => {
   describe('fetchPersonalInformation SUCCESS', () => {
     beforeEach(() => {
       mockFetch();
-
       recordEventSpy = sinon.spy();
-
       setFetchJSONResponse(
         global.fetch.onFirstCall(),
         mockPersonalInfo['GET /v0/user'],
       );
       actionCreator = fetchPersonalInformation(false, recordEventSpy);
-
       dispatch = sinon.spy();
     });
 
     it('calls fetch to `GET profile/personal_information`', async () => {
       await actionCreator(dispatch);
-
       expect(global.fetch.firstCall.args[1].method).to.equal('GET');
-
       expect(
         global.fetch.firstCall.args[0].endsWith(
           '/profile/personal_information',
         ),
-      ).to.be.true;
+      ).to.not.be.true;
     });
   });
 });
