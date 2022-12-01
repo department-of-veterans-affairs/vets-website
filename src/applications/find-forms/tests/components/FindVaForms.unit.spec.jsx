e5e500c2bef38f42e974dc6a88fabcf7cbe18062
jtmst <josh.troy.mills@gmail.com>
2022-12-01 09:16:39 -0500

    update unit test

--- src/applications/find-forms/tests/components/FindVaForms.unit.spec.jsx
+++ src/applications/find-forms/tests/components/FindVaForms.unit.spec.jsx
@@ -2,12 +2,44 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
 // Relative imports.
 import FindVaForms from '../../components/FindVaForms';
 
+const getData = ({
+  showNod = true,
+  isLoading = false,
+  loggedIn = true,
+} = {}) => ({
+  props: {
+    isLoading,
+    loggedIn,
+  },
+  mockStore: {
+    getState: () => ({
+      featureToggles: {
+        // eslint-disable-next-line camelcase
+        form10182_nod: showNod,
+      },
+      user: {
+        login: {
+          currentlyLoggedIn: loggedIn,
+        },
+      },
+    }),
+    subscribe: () => {},
+    dispatch: () => {},
+  },
+});
+
 describe('Find VA Forms <FindVaForms>', () => {
   it('should render', () => {
-    const tree = shallow(<FindVaForms />);
+    const { mockStore } = getData();
+    const tree = shallow(
+      <Provider store={mockStore}>
+        <FindVaForms />
+      </Provider>,
+    );
 
     expect(tree.find('SearchForm')).to.exist;
     tree.unmount();
