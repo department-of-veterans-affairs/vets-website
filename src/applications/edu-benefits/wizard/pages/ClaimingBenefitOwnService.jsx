91f5a54b71717458b9a73b1d867c67478afcf4f1
Jarred Simon <jarred.simon@va.gov>
2022-12-04 19:48:40 -0600

    VFEP-252 #comment deprecated radio buttons replaced with new web-component radio buttons

--- src/applications/edu-benefits/wizard/pages/ClaimingBenefitOwnService.jsx
+++ src/applications/edu-benefits/wizard/pages/ClaimingBenefitOwnService.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { pageNames } from './pageList';
-import RadioButtons from '@department-of-veterans-affairs/component-library/RadioButtons';
 import { formIdSuffixes } from 'applications/static-pages/wizard/';
+import { pageNames } from './pageList';
+import VARadioButton from '../../../gi/components/VARadioButton';
 
 const claimingBenefitOwnServiceOptions = [
   { label: 'Yes', value: 'yes' },
@@ -10,17 +10,15 @@ const claimingBenefitOwnServiceOptions = [
 const ClaimingBenefitOwnService = ({
   setPageState,
   getPageStateFromPageName,
-  state = {},
   setReferredBenefit,
 }) => (
   <div>
-    <RadioButtons
-      name={`${pageNames.claimingBenefitOwnService}`}
-      label="Are you a Veteran or service member claiming a benefit based on your own service?"
-      id={`${pageNames.claimingBenefitOwnService}`}
-      additionalFieldsetClass="wizard-fieldset"
+    <VARadioButton
+      radioLabel="Are you a Veteran or service member claiming a benefit based on your own service?"
+      initialValue=""
       options={claimingBenefitOwnServiceOptions}
-      onValueChange={({ value }) => {
+      onVaValueChange={event => {
+        const { value } = event.detail;
         const newBenefitAnswer = getPageStateFromPageName(pageNames.newBenefit)
           ?.selected;
         const sponsorDeceasedAnswer = getPageStateFromPageName(
@@ -29,16 +27,14 @@ const ClaimingBenefitOwnService = ({
         const transferredBenefitsAnswer = getPageStateFromPageName(
           pageNames.transferredBenefits,
         )?.selected;
-        const nationalCallToServiceAnswer = getPageStateFromPageName(
-          pageNames.nationalCallToService,
-        )?.selected;
         if (
           newBenefitAnswer === 'new' &&
           value === 'no' &&
           transferredBenefitsAnswer === 'no'
         ) {
           return setPageState({ selected: value }, pageNames.sponsorDeceased);
-        } else if (
+        }
+        if (
           newBenefitAnswer === 'new' &&
           value === 'no' &&
           sponsorDeceasedAnswer === 'yes'
@@ -46,24 +42,15 @@ const ClaimingBenefitOwnService = ({
           const { FORM_ID_5490 } = formIdSuffixes;
           setReferredBenefit(FORM_ID_5490);
           return setPageState({ selected: value }, pageNames.applyNow);
-        } else if (
-          newBenefitAnswer === 'new' &&
-          nationalCallToServiceAnswer === 'no' &&
-          value === 'yes'
-        ) {
-          return setPageState({ selected: value }, pageNames.vetTec);
-        } else if (newBenefitAnswer === 'new' && value === 'yes') {
-          return setPageState(
-            { selected: value },
-            pageNames.nationalCallToService,
-          );
-        } else if (newBenefitAnswer === 'new' && value === 'no') {
-          return setPageState({ selected: value }, pageNames.sponsorDeceased);
-        } else {
-          return setPageState({ selected: value });
         }
+        if (newBenefitAnswer === 'new' && value === 'yes') {
+          return setPageState({ selected: value }, pageNames.vetTec);
+        }
+        if (newBenefitAnswer === 'new' && value === 'no') {
+          return setPageState({ selected: value }, pageNames.sponsorDeceased);
+        }
+        return setPageState({ selected: value });
       }}
-      value={{ value: state.selected }}
     />
   </div>
 );
