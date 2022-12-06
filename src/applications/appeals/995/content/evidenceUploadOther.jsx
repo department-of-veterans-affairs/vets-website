c433034088e517b99545ae6d82cabf37245f226d
rocio-desantiago <rocio@coforma.io>
2022-12-05 23:15:59 -0600

    add page for new-and-relevant-evidencee

---
+++ src/applications/appeals/995/content/evidenceUploadOther.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const evidenceUploadOtherTitle =
+  'Do you want to upload your records or other documents to support your claim?';
+
+export const evidenceUploadOtherInfo = (
+  <va-additional-info
+    trigger="Types of supporting evidence"
+    class="vads-u-margin-top--4"
+  >
+    <p>
+      Lay statements or other. A lay statement is a written statement from
+      family, friends, or coworkers to help support your claim. Lay statements
+      are also called “buddy statements.” In most cases, you’ll only need your
+      medical records to support your disability claim. But some claims—such as
+      those for Posttraumatic Stress Disorder or military sexual trauma—could
+      benefit from a lay or buddy statement.
+    </p>
+  </va-additional-info>
+);
