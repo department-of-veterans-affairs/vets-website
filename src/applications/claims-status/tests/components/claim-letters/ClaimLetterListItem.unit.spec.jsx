4dbb87d8b7da06a3109f5726ea5b59e652d7c34c
jerekshoe <jerek.shoemaker@adhocteam.us>
2022-12-07 14:38:09 -0600

    Adding unit tests

--- src/applications/claims-status/tests/components/claim-letters/ClaimLetterListItem.unit.spec.jsx
+++ src/applications/claims-status/tests/components/claim-letters/ClaimLetterListItem.unit.spec.jsx
@@ -2,25 +2,43 @@ import React from 'react';
 import { expect } from 'chai';
 import { render } from '@testing-library/react';
 
-import { $ } from 'platform/forms-system/src/js/utilities/ui';
-
 import ClaimLetterListItem from '../../../components/ClaimLetterListItem';
 
-describe('<ClaimLetterListItem', () => {
-  const mockLetter = {
-    documentId: '{27832B64-2D88-4DEE-9F6F-DF80E4CAAA87}',
-    receivedAt: '2022-09-22',
-    typeDescription: 'Notification Letter (e.g. VA 20-8993, VA 21-0290, PCGL)',
-  };
+const mockLetter = {
+  documentId: '{27832B64-2D88-4DEE-9F6F-DF80E4CAAA87}',
+  receivedAt: '2022-09-22',
+  docType: '184',
+  typeDescription: 'Notification Letter (e.g. VA 20-8993, VA 21-0290, PCGL)',
+};
 
+const mockLetterWithoutDocType = { ...mockLetter };
+delete mockLetterWithoutDocType.docType;
+
+describe('<ClaimLetterListItem>', () => {
   it('should render', () => {
     const { container } = render(<ClaimLetterListItem letter={mockLetter} />);
 
-    expect($('h2', container).textContent).to.equal(
-      'Letter dated September 22, 2022',
+    expect(container).to.exist;
+  });
+
+  it('should have the correct title', () => {
+    const screen = render(<ClaimLetterListItem letter={mockLetter} />);
+
+    const title = screen.getByText(/Letter dated/i);
+    expect(title.textContent).to.eq('Letter dated September 22, 2022');
+  });
+
+  it('should have the correct description', () => {
+    const screen = render(<ClaimLetterListItem letter={mockLetter} />);
+
+    expect(screen.getByText(/Notification Letter/i)).to.exist;
+  });
+
+  it('should use the default description when no `docType` is provided', () => {
+    const screen = render(
+      <ClaimLetterListItem letter={mockLetterWithoutDocType} />,
     );
-    expect($('h2 + div', container).textContent).to.equal(
-      'Notification Letter (e.g. VA 20-8993, VA 21-0290, PCGL)',
-    );
+
+    expect(screen.getByText(/Notification Letter/i)).to.exist;
   });
 });
