1a1552c7707ffbcdd7ffb134e112cc79c91f6cfd
jerekshoe <jerek.shoemaker@adhocteam.us>
2022-12-10 16:32:28 -0600

    Using withRouter HOC

--- src/applications/claims-status/containers/AskVAPage.jsx
+++ src/applications/claims-status/containers/AskVAPage.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { Link, useNavigate, useParams } from 'react-router-dom-v5-compat';
+import { Link } from 'react-router-dom-v5-compat';
 import PropTypes from 'prop-types';
 import Checkbox from '@department-of-veterans-affairs/component-library/Checkbox';
 
@@ -8,112 +8,120 @@ import { submitRequest, getClaimDetail } from '../actions';
 import AskVAQuestions from '../components/AskVAQuestions';
 import ClaimsBreadcrumbs from '../components/ClaimsBreadcrumbs';
 import { setUpPage } from '../utils/page';
+import withRouter from '../utils/withRouter';
 
-export const AskVAPage = ({
-  decisionRequested,
-  decisionRequestError,
-  loadingDecisionRequest,
-}) => {
-  const [submittedDocs, setSubmittedDocs] = useState(false);
-  const navigate = useNavigate();
-  const params = useParams();
-
-  useEffect(() => {
-    document.title = 'Ask for your Claim Decision';
-    setUpPage();
-  }, []);
-
-  useEffect(
-    () => {
-      if (decisionRequested) {
-        getClaimDetail(params.id);
-      }
-    },
-    [decisionRequested, params.id],
-  );
-
-  const goToStatusPage = () => {
-    navigate(`../status`);
-  };
-
-  const submitDisabled =
-    !submittedDocs || loadingDecisionRequest || decisionRequestError;
-
-  let buttonMsg = 'Submit';
-  if (loadingDecisionRequest) {
-    buttonMsg = 'Submitting...';
-  } else if (decisionRequestError !== null) {
-    buttonMsg = 'Something went wrong...';
+class AskVAPage extends React.Component {
+  constructor() {
+    super();
+    this.goToStatusPage = this.goToStatusPage.bind(this);
+    this.setSubmittedDocs = this.setSubmittedDocs.bind(this);
+    this.state = { submittedDocs: false };
   }
 
-  return (
-    <div className="vads-l-grid-container large-screen:vads-u-padding-x--0  vads-u-margin-bottom--7">
-      <div className="vads-l-row vads-u-margin-x--neg1p5 medium-screen:vads-u-margin-x--neg2p5">
-        <div className="vads-l-col--12">
-          <ClaimsBreadcrumbs>
-            <Link to="../status">Status details</Link>
-            <Link to="../ask-va-to-decide">Ask for your claim decision</Link>
-          </ClaimsBreadcrumbs>
-        </div>
-      </div>
-      <div className="vads-l-row vads-u-margin-x--neg2p5">
-        <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--8">
-          <div>
-            <h1>Ask for your claim decision</h1>
-            <p className="first-of-type">
-              We sent you a letter in the mail asking for more evidence to
-              support your claim. We’ll wait 30 days for your evidence. If you
-              don’t have anything more you want to submit, let us know and we’ll
-              go ahead and make a decision on your claim.
-            </p>
-            <p>Taking the full 30 days won’t affect:</p>
-            <ul>
-              <li>Whether you get VA benefits</li>
-              <li>The payment amount</li>
-              <li>
-                Whether you get our help to gather evidence to support your
-                claim
-              </li>
-              <li>The date benefits will begin if we approve your claim</li>
-            </ul>
-            <div className="usa-alert usa-alert-info background-color-only claims-alert">
-              <Checkbox
-                className="claims-alert-checkbox"
-                checked={submittedDocs}
-                onValueChange={update => setSubmittedDocs(update)}
-                label="I have submitted all evidence that will support my claim and I’m not going to turn in any more information. I would like VA to make a decision on my claim based on the information already provided."
-              />
-            </div>
-            <button
-              disabled={submitDisabled}
-              type="button"
-              className={
-                submitDisabled
-                  ? 'usa-button-primary usa-button-disabled'
-                  : 'usa-button-primary'
-              }
-              onClick={() => submitRequest(params.id)}
-            >
-              {buttonMsg}
-            </button>
-            {!loadingDecisionRequest ? (
-              <button
-                className="usa-button-secondary"
-                onClick={goToStatusPage}
-                type="button"
-              >
-                Not yet–I still have more evidence to submit
-              </button>
-            ) : null}
+  componentDidMount() {
+    document.title = 'Ask for your Claim Decision';
+    setUpPage();
+  }
+
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillReceiveProps(props) {
+    if (props.decisionRequested) {
+      props.getClaimDetail(this.props.params.id);
+      this.goToStatusPage();
+    }
+  }
+
+  setSubmittedDocs(val) {
+    this.setState({ submittedDocs: val });
+  }
+
+  goToStatusPage() {
+    this.props.navigate(`../status`);
+  }
+
+  render() {
+    const { loadingDecisionRequest, decisionRequestError } = this.props;
+    const submitDisabled =
+      !this.state.submittedDocs ||
+      loadingDecisionRequest ||
+      decisionRequestError;
+
+    let buttonMsg = 'Submit';
+    if (loadingDecisionRequest) {
+      buttonMsg = 'Submitting...';
+    } else if (decisionRequestError !== null) {
+      buttonMsg = 'Something went wrong...';
+    }
+
+    return (
+      <div className="vads-l-grid-container large-screen:vads-u-padding-x--0  vads-u-margin-bottom--7">
+        <div className="vads-l-row vads-u-margin-x--neg1p5 medium-screen:vads-u-margin-x--neg2p5">
+          <div className="vads-l-col--12">
+            <ClaimsBreadcrumbs>
+              <Link to="../status">Status details</Link>
+              <Link to="../ask-va-to-decide">Ask for your claim decision</Link>
+            </ClaimsBreadcrumbs>
           </div>
         </div>
-        <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--4 help-sidebar">
-          <AskVAQuestions />
+        <div className="vads-l-row vads-u-margin-x--neg2p5">
+          <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--8">
+            <div>
+              <h1>Ask for your claim decision</h1>
+              <p className="first-of-type">
+                We sent you a letter in the mail asking for more evidence to
+                support your claim. We’ll wait 30 days for your evidence. If you
+                don’t have anything more you want to submit, let us know and
+                we’ll go ahead and make a decision on your claim.
+              </p>
+              <p>Taking the full 30 days won’t affect:</p>
+              <ul>
+                <li>Whether you get VA benefits</li>
+                <li>The payment amount</li>
+                <li>
+                  Whether you get our help to gather evidence to support your
+                  claim
+                </li>
+                <li>The date benefits will begin if we approve your claim</li>
+              </ul>
+              <div className="usa-alert usa-alert-info background-color-only claims-alert">
+                <Checkbox
+                  className="claims-alert-checkbox"
+                  checked={this.state.submittedDocs}
+                  onValueChange={update => this.setSubmittedDocs(update)}
+                  label="I have submitted all evidence that will support my claim and I’m not going to turn in any more information. I would like VA to make a decision on my claim based on the information already provided."
+                />
+              </div>
+              <button
+                disabled={submitDisabled}
+                type="button"
+                className={
+                  submitDisabled
+                    ? 'usa-button-primary usa-button-disabled'
+                    : 'usa-button-primary'
+                }
+                onClick={() => this.props.submitRequest(this.props.params.id)}
+              >
+                {buttonMsg}
+              </button>
+              {!loadingDecisionRequest ? (
+                <button
+                  className="usa-button-secondary"
+                  onClick={this.goToStatusPage}
+                  type="button"
+                >
+                  Not yet–I still have more evidence to submit
+                </button>
+              ) : null}
+            </div>
+          </div>
+          <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--4 help-sidebar">
+            <AskVAQuestions />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 function mapStateToProps(state) {
   const claimsState = state.disability.status;
@@ -129,15 +137,21 @@ const mapDispatchToProps = {
   getClaimDetail,
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(AskVAPage);
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(AskVAPage),
+);
 
 AskVAPage.propTypes = {
   decisionRequestError: PropTypes.string,
   decisionRequested: PropTypes.bool,
   getClaimDetail: PropTypes.func,
   loadingDecisionRequest: PropTypes.bool,
+  navigate: PropTypes.func,
+  params: PropTypes.object,
   submitRequest: PropTypes.func,
 };
+
+export { AskVAPage };
