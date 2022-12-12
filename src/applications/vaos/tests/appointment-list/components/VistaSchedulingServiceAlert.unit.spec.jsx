eaefdbb44b97d74cc930f7e8f6c718a70f46edd5
Ryan Shaw <ryan.shaw@adhocteam.us>
2022-12-12 15:28:31 -0800

    Updated helpers.v2.js to handle failures array

--- src/applications/vaos/tests/appointment-list/components/VistaSchedulingServiceAlert.unit.spec.jsx
+++ src/applications/vaos/tests/appointment-list/components/VistaSchedulingServiceAlert.unit.spec.jsx
@@ -98,7 +98,67 @@ describe('VAOS <AppointmentsPage>', () => {
     });
 
     it('should not display VistaSchedulingServiceAlert if there is no failure returned', async () => {
-      return true;
+      const appointmentTime = moment().add(1, 'days');
+      const start = moment()
+        .subtract(30, 'days')
+        .format('YYYY-MM-DD');
+      const end = moment()
+        .add(395, 'days')
+        .format('YYYY-MM-DD');
+
+      const data = {
+        id: '01aa456cc',
+        kind: 'cc',
+        practitioners: [
+          {
+            identifier: [{ system: null, value: '123' }],
+            name: {
+              family: 'Medical Care',
+              given: ['Atlantic'],
+            },
+          },
+        ],
+        description: 'community care appointment',
+        comment: 'test comment',
+        start: appointmentTime,
+        communityCareProvider: {
+          providerName: 'Atlantic Medical Care',
+        },
+        serviceType: 'audiology',
+        reasonCode: {
+          text: 'test comment',
+        },
+      };
+      const appointment = createMockAppointmentByVersion({
+        version: 2,
+        ...data,
+      });
+
+      mockVAOSAppointmentsFetch({
+        start,
+        end,
+        requests: [appointment],
+        statuses: ['booked', 'arrived', 'fulfilled', 'cancelled'],
+        backendServiceFailures: true,
+      });
+
+      const screen = renderWithStoreAndRouter(<AppointmentList />, {
+        initialState: {
+          featureToggles: {
+            ...initialState.featureToggles,
+            vaOnlineSchedulingVAOSV2Next: true,
+          },
+        },
+      });
+
+      await waitFor(() => {
+        expect(global.document.title).to.equal(
+          `Your appointments | VA online scheduling | Veterans Affairs`,
+        );
+      });
+
+      expect(screen.queryByText("We can't display in-person VA appointments"))
+        .not.to.exist;
     });
   });
 
