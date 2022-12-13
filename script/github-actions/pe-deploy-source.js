58a1f534285e8235430d3bf87dc97aa729dbd759
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2022-12-13 12:34:55 -0500

    Test event repository name

--- script/github-actions/pe-deploy-source.js
+++ script/github-actions/pe-deploy-source.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core');
 
-const sourceEvent = JSON.parse(process.env.SOURCE_EVENT);
+const sourceEvent = process.env.SOURCE_EVENT || null;
 
 /* eslint-disable no-console */
 console.log(sourceEvent);
 
-if (sourceEvent === 'push') {
+if (sourceEvent) {
   core.exportVariable('SOURCE_REPO', 'vets-website');
 } else {
   core.exportVariable('SOURCE_REPO', 'content-build');
