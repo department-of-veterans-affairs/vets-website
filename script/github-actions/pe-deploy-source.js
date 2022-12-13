39c159a73824283569a19cc1705e86366b101676
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2022-12-13 16:22:56 -0500

    WIP

--- script/github-actions/pe-deploy-source.js
+++ script/github-actions/pe-deploy-source.js
@@ -3,18 +3,14 @@ const core = require('@actions/core');
 const sourceEvent = process.env.SOURCE_EVENT;
 const sourceRef = process.env.SOURCE_REF;
 const workflowRef = process.env.WORKFLOW_BRANCH;
-/* eslint-disable no-console */
 
-console.log(sourceEvent);
 if (
   sourceEvent === 'repository_dispatch' ||
   sourceEvent === 'workflow_dispatch'
 ) {
-  console.log('content');
   core.exportVariable('SOURCE_REPO', 'content-build');
   core.exportVariable('SOURCE_REF', sourceRef);
 } else {
-  console.log('vets');
   core.exportVariable('SOURCE_REPO', 'vets-website');
   core.exportVariable('SOURCE_REF', workflowRef);
 }
