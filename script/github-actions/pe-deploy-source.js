e305f6d855498b946452fb8b3f2569b8bb59a705
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2022-12-13 15:31:48 -0500

    WIP

--- script/github-actions/pe-deploy-source.js
+++ script/github-actions/pe-deploy-source.js
@@ -1,3 +1,10 @@
-/* eslint-disable no-console */
+const core = require('@actions/core');
 
-console.log(process.env.SOURCE_EVENT);
+const sourceEvent = process.env.SOURCE_EVENT;
+
+/* eslint-disable no-console */
+if (sourceEvent === 'repository_dispatch' || 'workflow_dispatch') {
+  core.exportVariable('SOURCE_REPO', 'content-build');
+} else {
+  core.exportVariable('SOURCE_REPO', 'vets-website');
+}
