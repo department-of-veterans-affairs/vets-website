f8f606e463b58dd356e5e9dc87eebd9b2891e562
cbonade <34250637+CBonade@users.noreply.github.com>
2022-12-13 08:42:43 -0500

    Update pe-deploy-source.js

--- script/github-actions/pe-deploy-source.js
+++ script/github-actions/pe-deploy-source.js
@@ -2,8 +2,6 @@ const core = require('@actions/core');
 
 const sourceEvent = process.env.SOURCE_EVENT;
 
-/* eslint-disable no-console */
-
 if (sourceEvent === 'push') {
   core.exportVariable('SOURCE_REPO', 'vets-website');
 } else {
