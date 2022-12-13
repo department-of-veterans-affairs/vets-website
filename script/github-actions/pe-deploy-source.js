8c9fb0758b9a8f006f70a63ca0fe325c210a640d
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2022-12-13 11:42:30 -0500

    WIP

--- script/github-actions/pe-deploy-source.js
+++ script/github-actions/pe-deploy-source.js
@@ -2,6 +2,9 @@ const core = require('@actions/core');
 
 const sourceEvent = JSON.parse(process.env.SOURCE_EVENT);
 
+/* eslint-disable no-console */
+console.log(sourceEvent);
+
 if (sourceEvent === 'push') {
   core.exportVariable('SOURCE_REPO', 'vets-website');
 } else {
