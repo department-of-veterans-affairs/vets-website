9160e74c841f494161f5ad5a1fddccfc6c4963ee
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2022-12-13 16:27:08 -0500

    WIP

--- script/github-actions/pe-deploy-source.js
+++ script/github-actions/pe-deploy-source.js
@@ -1,14 +1,15 @@
 const core = require('@actions/core');
 
 const sourceEvent = process.env.SOURCE_EVENT;
-const sourceRef = process.env.SOURCE_REF;
+const sourceRepo = process.env.SOURCE_REPO;
+const sourceRef = process.env.SOURCE_BRANCH;
 const workflowRef = process.env.WORKFLOW_BRANCH;
 
 if (
   sourceEvent === 'repository_dispatch' ||
   sourceEvent === 'workflow_dispatch'
 ) {
-  core.exportVariable('SOURCE_REPO', 'content-build');
+  core.exportVariable('SOURCE_REPO', sourceRepo);
   core.exportVariable('SOURCE_REF', sourceRef);
 } else {
   core.exportVariable('SOURCE_REPO', 'vets-website');
