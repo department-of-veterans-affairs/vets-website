d182026e669c34094464ead95585890bec35a5d9
Adam Whitlock <adam.whitlock@adhocteam.us>
2022-12-15 12:38:52 -0700

    (test) fix unit test for mil info

--- src/applications/personalization/profile/tests/components/MilitaryInformation.unit.spec.jsx
+++ src/applications/personalization/profile/tests/components/MilitaryInformation.unit.spec.jsx
@@ -215,7 +215,9 @@ describe('MilitaryInformation', () => {
         initialState,
       });
 
-      expect(view.getByTestId('service-is-down-banner')).to.exist;
+      expect(view.getByTestId('not-a-veteran-alert')).to.exist;
+      expect(view.getByText(/If you think this is an error, call us at/i)).to
+        .exist;
     });
   });
   describe('when the military service history is empty', () => {
@@ -273,11 +275,7 @@ describe('MilitaryInformation', () => {
         initialState,
       });
 
-      expect(
-        view.getByText(
-          'We’re sorry. Something went wrong on our end. Please refresh this page or try again later.',
-        ),
-      ).to.exist;
+      expect(view.getByTestId('service-is-down-banner')).to.exist;
     });
   });
 });
