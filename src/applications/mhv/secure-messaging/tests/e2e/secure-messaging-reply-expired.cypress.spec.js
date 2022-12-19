1ddb0173209c202637355c76a796ca7e2ec063fb
swathichelika <swathi.chelika@bylight.com>
2022-12-19 10:42:28 -0600

    added one function to script

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-reply-expired.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-reply-expired.cypress.spec.js
@@ -1,4 +1,3 @@
-import { cy } from 'date-fns/locale';
 import manifest from '../../manifest.json';
 import PatientMessagesLandingPage from './pages/PatientMessagesLandingPage';
 
@@ -7,6 +6,22 @@ describe(manifest.appName, () => {
     const landingPage = new PatientMessagesLandingPage();
     landingPage.login();
     landingPage.loadPage();
+    landingPage.loadMessageDetails(
+      landingPage.getExpired46DayOldMessage().attributes.messageId,
+      landingPage.getExpired46DayOldMessage().attributes.subject,
+      landingPage.getExpired46DayOldMessage().attributes.sentDate,
+    );
+    messageDetailsPage.loadReplyPage(
+      landingPage.getExpired46DayOldMessage().attributes.messageId,
+      landingPage.getExpired46DayOldMessage().attributes.subject,
+      landingPage.getExpired46DayOldMessage().attributes.sentDate,
+    );
+    cy.get('[data-testid="message-body-field"]')
+      .shadow()
+      .find('[name="message-body"]')
+      .type('Test message body');
+    cy.injectAxe();
+    cy.axeCheck();
     cy.get('va-select#sort-order-dropdown')
       .shadow()
       .find('[id="select"]')
@@ -23,6 +38,7 @@ describe(manifest.appName, () => {
       'have.text',
       "Please select 'Compose' to create a new message.",
     );
+
     cy.injectAxe();
     cy.axeCheck();
   });
