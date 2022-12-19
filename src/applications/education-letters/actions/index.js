16824a70783650c8c1842acc6e647cedae672ec7
fer.boza <fernando.boza@accenturefederal.com>
2022-12-19 11:51:38 -0500

    refactored to single endpoint claims status

--- src/applications/education-letters/actions/index.js
+++ src/applications/education-letters/actions/index.js
@@ -1,35 +1,27 @@
 import { apiRequest } from 'platform/utilities/api';
 import environment from 'platform/utilities/environment';
 
-// export const MEB_CLAIM_STATUS_ENDPOINT = `${environment.API_URL}/meb_api/v0/claim_status`;
-// export const TOE_CLAIM_STATUS_ENDPOINT = `${environment.API_URL}/meb_api/v0/forms_claim_status`;
+export const CLAIM_STATUS_ENDPOINT = `${
+  environment.API_URL
+}/meb_api/v0/letter_availability`;
 
-const CLAIM_ENDPOINTS = {
-  MEB: `${environment.API_URL}/meb_api/v0/get_latest_claim_status`,
-  TOE: `${environment.API_URL}/meb_api/v0/forms_claim_status`,
-};
+export const FETCH_CLAIM_STATUS = 'FETCH_CLAIM_STATUS';
+export const FETCH_CLAIM_STATUS_SUCCESS = 'FETCH_CLAIM_STATUS_SUCCESS';
+export const FETCH_CLAIM_STATUS_FAILED = 'FETCH_CLAIM_STATUS_FAILED';
 
-export const MEB_FETCH_CLAIM_STATUS = 'MEB_FETCH_CLAIM_STATUS';
-export const MEB_FETCH_CLAIM_STATUS_SUCCESS = 'MEB_FETCH_CLAIM_STATUS_SUCCESS';
-export const MEB_FETCH_CLAIM_STATUS_FAILED = 'MEB_FETCH_CLAIM_STATUS_FAILED';
-
-export const TOE_FETCH_CLAIM_STATUS = 'TOE_FETCH_CLAIM_STATUS';
-export const TOE_FETCH_CLAIM_STATUS_SUCCESS = 'TOE_FETCH_CLAIM_STATUS_SUCCESS';
-export const TOE_FETCH_CLAIM_STATUS_FAILED = 'TOE_FETCH_CLAIM_STATUS_FAILED';
-
-export function fetchClaimStatus(statusType) {
-  return dispatch => {
-    dispatch({ type: `${statusType}_FETCH_CLAIM_STATUS` });
-    return apiRequest(CLAIM_ENDPOINTS[statusType])
+export function fetchClaimStatus() {
+  return async dispatch => {
+    dispatch({ type: FETCH_CLAIM_STATUS });
+    return apiRequest(CLAIM_STATUS_ENDPOINT)
       .then(response => {
         dispatch({
-          type: `${statusType}_FETCH_CLAIM_STATUS_SUCCESS`,
+          type: FETCH_CLAIM_STATUS_SUCCESS,
           response,
         });
       })
       .catch(errors => {
         dispatch({
-          type: `${statusType}_FETCH_CLAIM_STATUS_FAILED`,
+          type: FETCH_CLAIM_STATUS_FAILED,
           errors,
         });
       });
