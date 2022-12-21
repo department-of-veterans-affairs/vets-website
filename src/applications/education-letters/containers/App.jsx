13687aede80332702e1915d5b7d6398a2aee85f2
fer.boza <fernando.boza@accenturefederal.com>
2022-12-21 09:32:14 -0500

    Linked up Education letters and EnrollmentVerificationLogIn signin alert and route to ctaWidgets

--- src/applications/education-letters/containers/App.jsx
+++ src/applications/education-letters/containers/App.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import FormTitle from 'platform/forms-system/src/js/components/FormTitle';
 import { toggleLoginModal as toggleLoginModalAction } from 'platform/site-wide/user-nav/actions';
 import PropTypes from 'prop-types';
+import CallToActionWidget from 'applications/static-pages/cta-widget';
 import Layout from '../components/Layout';
 
 const App = ({ toggleLoginModal, user }) => {
@@ -28,33 +29,39 @@ const App = ({ toggleLoginModal, user }) => {
     }
 
     return (
-      <va-alert
-        close-btn-aria-label="Close notification"
-        status="continue"
-        visible
-      >
-        <p
-          className="vads-u-font-size--h3 vads-u-font-weight--bold vads-u-font-family--serif"
-          slot="headline"
+      <CallToActionWidget appId="education-letters">
+        <va-alert
+          close-btn-aria-label="Close notification"
+          status="continue"
+          visible
         >
-          Please sign in to check your VA education letter.
-        </p>
-        <div>
-          Sign in with your existing ID.me or Login.gov account. If you don’t
-          have an account, you can create a free{' '}
-          <a href="https://www.id.me/" target="_blank" rel="noreferrer">
-            ID.me account
-          </a>{' '}
-          or{' '}
-          <a href="https://secure.login.gov/" target="_blank" rel="noreferrer">
-            Login.gov account
-          </a>{' '}
-          now.
-        </div>
-        <button className="va-button" type="button" onClick={toggleLogin}>
-          Sign in or create an account
-        </button>
-      </va-alert>
+          <p
+            className="vads-u-font-size--h3 vads-u-font-weight--bold vads-u-font-family--serif"
+            slot="headline"
+          >
+            Please sign in to check your VA education letter.
+          </p>
+          <div>
+            Sign in with your existing ID.me or Login.gov account. If you don’t
+            have an account, you can create a free{' '}
+            <a href="https://www.id.me/" target="_blank" rel="noreferrer">
+              ID.me account
+            </a>{' '}
+            or{' '}
+            <a
+              href="https://secure.login.gov/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Login.gov account
+            </a>{' '}
+            now.
+          </div>
+          <button className="va-button" type="button" onClick={toggleLogin}>
+            Sign in or create an account
+          </button>
+        </va-alert>
+      </CallToActionWidget>
     );
   }
 
--- src/applications/enrollment-verification/components/EnrollmentVerificationLogIn.jsx
+++ src/applications/enrollment-verification/components/EnrollmentVerificationLogIn.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import CallToActionWidget from 'applications/static-pages/cta-widget';
 
 import { toggleLoginModal as toggleLoginModalAction } from 'platform/site-wide/user-nav/actions';
 
@@ -9,34 +10,36 @@ export function EnrollmentVerificationLogin({ toggleLoginModal }) {
     toggleLoginModal,
   ]);
   return (
-    <va-alert status="continue" visible>
-      <h1
-        className="vads-u-font-size--h1 vads-u-font-weight--bold"
-        slot="headline"
-      >
-        Please sign in to verify your enrollment
-      </h1>
-      <p>
-        Sign in with your existing <strong>ID.me</strong> or{' '}
-        <strong>Login.gov</strong> account. If you don’t have an account, you
-        can create a free{' '}
-        <a href="https://api.id.me/" target="_blank" rel="noreferrer">
-          ID.me
-        </a>{' '}
-        account or{' '}
-        <a href="https://secure.login.gov/" target="_blank" rel="noreferrer">
-          Login.gov
-        </a>{' '}
-        account now.
-      </p>
-      <button
-        type="button"
-        className="usa-button-primary va-button-primary"
-        onClick={onSignInClicked}
-      >
-        Sign in or create an account
-      </button>
-    </va-alert>
+    <CallToActionWidget appId="enrollment-verification">
+      <va-alert status="continue" visible>
+        <h1
+          className="vads-u-font-size--h1 vads-u-font-weight--bold"
+          slot="headline"
+        >
+          Please sign in to verify your enrollment
+        </h1>
+        <p>
+          Sign in with your existing <strong>ID.me</strong> or{' '}
+          <strong>Login.gov</strong> account. If you don’t have an account, you
+          can create a free{' '}
+          <a href="https://api.id.me/" target="_blank" rel="noreferrer">
+            ID.me
+          </a>{' '}
+          account or{' '}
+          <a href="https://secure.login.gov/" target="_blank" rel="noreferrer">
+            Login.gov
+          </a>{' '}
+          account now.
+        </p>
+        <button
+          type="button"
+          className="usa-button-primary va-button-primary"
+          onClick={onSignInClicked}
+        >
+          Sign in or create an account
+        </button>
+      </va-alert>
+    </CallToActionWidget>
   );
 }
 
