c4146a5d03c65603b5275b3c27ff59ac205fdf86
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2022-12-22 14:50:40 -0600

    Update Basic Search Folders Test

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-basic-search.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-basic-search.cypress.spec.js
@@ -4,6 +4,8 @@ import mockMessages from './fixtures/drafts-search-results.json';
 import mockDraftsFolder from './fixtures/folder-drafts-metadata.json';
 import mockSentFolder from './fixtures/folder-sent-metadata.json';
 import PatientBasicSearchPage from './pages/PatientBasicSearchPage';
+import mockDeletedFolder from './fixtures/folder-deleted-metadata.json';
+import mockCustomFolder from './fixtures/folder-custom-metadata.json';
 
 describe(manifest.appName, () => {
   it('Basic Search Axe Check', () => {
@@ -100,7 +102,7 @@ describe(manifest.appName, () => {
       'GET',
       '/my_health/v1/messaging/folders/-1',
       mockSentFolder,
-    ).as('basicSearchRequestDraftsMeta');
+    ).as('basicSearchRequestSentMeta');
     cy.intercept(
       'GET',
       '/my_health/v1/messaging/folders/-1/messages?per_page=-1',
@@ -115,6 +117,68 @@ describe(manifest.appName, () => {
     // basicSearchPage.selectMessagesFolder('Sent');
     basicSearchPage.submitSearch();
     cy.wait('@basicSearchRequestSentFolder');
+    basicSearchPage.verifyHighlightedText('test');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+
+  it('Basic Search Deleted Folder Check', () => {
+    const landingPage = new PatientMessagesLandingPage();
+    const basicSearchPage = new PatientBasicSearchPage();
+    landingPage.login();
+    landingPage.loadPage();
+    basicSearchPage.clickSearchMessage();
+
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/-3',
+      mockDeletedFolder,
+    ).as('basicSearchRequestDeletedMeta');
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/-3/messages?per_page=-1',
+      mockMessages,
+    ).as('basicSearchRequestDeletedFolder');
+
+    basicSearchPage.getInputFieldText('test');
+    cy.get('[data-testid="folder-dropdown"]')
+      .shadow()
+      .find('select')
+      .select('Deleted', { force: true });
+
+    basicSearchPage.submitSearch();
+    cy.wait('@basicSearchRequestDeletedFolder');
+    basicSearchPage.verifyHighlightedText('test');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+
+  it('Basic Search Custom Folder Check', () => {
+    const landingPage = new PatientMessagesLandingPage();
+    const basicSearchPage = new PatientBasicSearchPage();
+    landingPage.login();
+    landingPage.loadPage();
+    basicSearchPage.clickSearchMessage();
+
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/7038175',
+      mockCustomFolder,
+    ).as('basicSearchRequestCustomMeta');
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/7038175/messages?per_page=-1',
+      mockMessages,
+    ).as('basicSearchRequestCustomFolder');
+
+    basicSearchPage.getInputFieldText('test');
+    cy.get('[data-testid="folder-dropdown"]')
+      .shadow()
+      .find('select')
+      .select('TEST2', { force: true });
+
+    basicSearchPage.submitSearch();
+    cy.wait('@basicSearchRequestCustomFolder');
     basicSearchPage.verifyHighlightedText('test');
     cy.injectAxe();
     cy.axeCheck();
