77809369c9e58627cd724425a46d426ee079ff66
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2022-12-22 08:40:09 -0600

    Update Basic Search Sent Folder
    Added scripts to include inbox and Sent folder basic search.

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-basic-search.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-basic-search.cypress.spec.js
@@ -2,6 +2,7 @@ import manifest from '../../manifest.json';
 import PatientMessagesLandingPage from './pages/PatientMessagesLandingPage';
 import mockMessages from './fixtures/drafts-search-results.json';
 import mockDraftsFolder from './fixtures/folder-drafts-metadata.json';
+import mockSentFolder from './fixtures/folder-sent-metadata.json';
 
 describe(manifest.appName, () => {
   it('Basic Search Axe Check', () => {
@@ -20,6 +21,37 @@ describe(manifest.appName, () => {
     cy.injectAxe();
     cy.axeCheck();
   });
+
+  it('Basic Search Inbox Check', () => {
+    const landingPage = new PatientMessagesLandingPage();
+    landingPage.login();
+    landingPage.loadPage();
+    cy.get('[data-testid="search-messages-sidebar"]').click();
+    cy.injectAxe();
+    cy.axeCheck();
+    cy.intercept(
+      'POST',
+      '/my_health/v1/messaging/folders/*/messages*',
+      mockMessages,
+    ).as('basicSearchInboxRequest');
+
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/0/messages?per_page=-1',
+      mockMessages,
+    ).as('basicSearchInboxRequest');
+    cy.get('[data-testid="keyword-text-input"]')
+      .shadow()
+      .find('[id="inputField"]')
+      .type('test');
+
+    cy.get('[data-testid="basic-search-submit"]').click();
+    cy.wait('@basicSearchInboxRequest');
+    cy.get('[data-testid="highlighted-text"]').should('contain', 'test');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+
   it('Basic Search Drafts Check', () => {
     const landingPage = new PatientMessagesLandingPage();
     landingPage.login();
@@ -49,6 +81,39 @@ describe(manifest.appName, () => {
     cy.get('[data-testid="basic-search-submit"]').click({ force: true });
     cy.wait('@basicSearchRequestDrafts');
     cy.get('[data-testid="highlighted-text"]').should('contain', 'test');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+
+  it('Basic Search Sent Folder Check', () => {
+    const landingPage = new PatientMessagesLandingPage();
+    landingPage.login();
+    landingPage.loadPage();
+    cy.get('[data-testid="search-messages-sidebar"]').click();
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/-1',
+      mockSentFolder,
+    ).as('basicSearchRequestDraftsMeta');
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/-1/messages?per_page=-1',
+      mockMessages,
+    ).as('basicSearchRequestSentFolder');
+
+    cy.get('[data-testid="keyword-text-input"]')
+      .shadow()
+      .find('[id="inputField"]')
+      .type('test');
+
+    cy.get('[data-testid="folder-dropdown"]')
+      .shadow()
+      .find('select')
+      .select('Sent', { force: true });
+
+    cy.get('[data-testid="basic-search-submit"]').click({ force: true });
+    // cy.wait('@basicSearchRequestSentFolder');
+    // cy.get('[data-testid="highlighted-text"]').should('contain', 'test');
     cy.injectAxe();
     cy.axeCheck();
   });
