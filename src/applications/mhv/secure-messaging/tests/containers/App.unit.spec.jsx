9984dc17e4a28626f274da051de15d853eecc452
Robert <roberto.rodriguez@bylight.com>
2022-12-27 17:02:18 -0500

    [MHV 39911] finished testing for feature flag, however there is an issue when the flag is set to true

--- src/applications/mhv/secure-messaging/tests/containers/App.unit.spec.jsx
+++ src/applications/mhv/secure-messaging/tests/containers/App.unit.spec.jsx
@@ -1,1 +1,52 @@
-describe('App', () => {});
+import React from 'react';
+import { expect } from 'chai';
+import { renderWithStoreAndRouter } from 'platform/testing/unit/react-testing-library-helpers';
+import App from '../../containers/App';
+
+describe('App', () => {
+  it('feature flag set to undefined', () => {
+    const initialState = {
+      featureToggles: {
+        // eslint-disable-next-line camelcase
+        mhv_secure_messaging_to_va_gov_release: undefined,
+      },
+    };
+    const screen = renderWithStoreAndRouter(<App />, {
+      initialState,
+      path: `/`,
+    });
+    expect(screen.getByTestId('feature-flag-undefined'));
+  });
+  it('feature flag set to false', () => {
+    const initialState = {
+      featureToggles: {
+        // eslint-disable-next-line camelcase
+        mhv_secure_messaging_to_va_gov_release: false,
+      },
+    };
+    const screen = renderWithStoreAndRouter(<App />, {
+      initialState,
+      path: `/`,
+    });
+    expect(screen.getByTestId('feature-flag-false'));
+  });
+  it('feature flag set to true', () => {
+    const initialState = {
+      featureToggles: {
+        sm: {},
+        user: {
+          login: {
+            currentlyLoggedIn: true,
+          },
+        },
+        // eslint-disable-next-line camelcase
+        mhv_secure_messaging_to_va_gov_release: true,
+      },
+    };
+    const screen = renderWithStoreAndRouter(<App />, {
+      initialState,
+      path: `/`,
+    });
+    expect(screen.getByTestId('feature-flag-true'));
+  });
+});
