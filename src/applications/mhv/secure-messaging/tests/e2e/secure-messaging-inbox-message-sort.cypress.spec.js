bd23016fdca300e7c692d6d6721df209f263e738
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2022-12-27 17:12:40 -0600

    Add Tests to Sort Newest to Oldest Messages
    Test script to validate the sort feature - Newest to Oldest inbox messages.

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-inbox-message-sort.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-inbox-message-sort.cypress.spec.js
@@ -1,32 +1,33 @@
 import manifest from '../../manifest.json';
 import PatientMessagesLandingPage from './pages/PatientMessagesLandingPage';
-import mockMessages from '../fixtures/messages-response.json';
 
 describe(manifest.appName, () => {
   it('Sort Inbox Messages from Newest to Oldest', () => {
     const landingPage = new PatientMessagesLandingPage();
     landingPage.login();
     landingPage.loadPage();
-    cy.get('[data-testid="search-messages-sidebar"]').click();
-    cy.injectAxe();
-    cy.axeCheck();
-    cy.intercept(
-      'POST',
-      '/my_health/v1/messaging/folders/*/messages*',
-      mockMessages,
-    ).as('basicSearchRequest');
-
-    cy.get('[data-testid="keyword-text-input"]')
-      .shadow()
-      .find('[id="inputField"]')
-      .type('inquiry');
-
-    cy.get('[data-testid="basic-search-submit"]').click();
+    cy.get('.sidebar-navigation-messages-list-header > a');
     cy.get('#sort-order-dropdown')
       .shadow()
       .find('select')
-      .select('desc', { force: true })
+
       .should('contain', 'Newest');
+    cy.get('.message-list-sort > va-button.hydrated').click();
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+
+  it('Sort Inbox Messages from Oldest to Newest', () => {
+    const landingPage = new PatientMessagesLandingPage();
+    landingPage.login();
+    landingPage.loadPage();
+    cy.get('.sidebar-navigation-messages-list-header > a');
+    cy.get('#sort-order-dropdown')
+      .shadow()
+      .find('select')
+      .select('asc', { force: true })
+      .should('contain', 'newest');
+    cy.get('.message-list-sort > va-button.hydrated').click();
     cy.injectAxe();
     cy.axeCheck();
   });
