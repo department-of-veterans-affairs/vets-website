dd2795cf733c588b963c03abad8089ef3dc846b0
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2022-12-28 10:33:00 -0500

    [#MHV-39898] adding RequiredLoginView

--- src/applications/mhv/secure-messaging/containers/App.jsx
+++ src/applications/mhv/secure-messaging/containers/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import {
-  logOut,
-  updateLoggedInStatus,
-} from '@department-of-veterans-affairs/platform-user/authentication/actions';
+  selectUser,
+  isLoggedIn,
+} from '@department-of-veterans-affairs/platform-user/selectors';
+import backendServices from '@department-of-veterans-affairs/platform-user/profile/backendServices';
+import { RequiredLoginView } from '@department-of-veterans-affairs/platform-user/RequiredLoginView';
+import environment from '@department-of-veterans-affairs/platform-utilities/environment';
 import FEATURE_FLAG_NAMES from '@department-of-veterans-affairs/platform-utilities/featureFlagNames';
 import AuthorizedRoutes from './AuthorizedRoutes';
 import LandingPageUnauth from './LandingPageUnauth';
@@ -13,76 +16,55 @@ import SmBreadcrumbs from '../components/shared/SmBreadcrumbs';
 import Navigation from '../components/Navigation';
 
 const App = () => {
-  const dispatch = useDispatch();
-  const isLoggedIn = useSelector(state => state?.user.login.currentlyLoggedIn);
+  const user = useSelector(selectUser);
+  const loginState = useSelector(isLoggedIn);
   const mhvSecureMessagingToVaGovRelease = useSelector(
     state =>
       state.featureToggles[FEATURE_FLAG_NAMES.mhvSecureMessagingToVaGovRelease],
   );
 
-  const handleClick = () => {
-    if (isLoggedIn) {
-      dispatch(logOut());
-    } else dispatch(updateLoggedInStatus(!isLoggedIn));
-  };
-
   return (
-    <div className="vads-l-grid-container">
-      {/* if the feature flag is undefined, show the loading indicator */}
-      {mhvSecureMessagingToVaGovRelease === undefined && (
-        <va-loading-indicator
-          message="Loading your secure messages..."
-          setFocus
-          data-testid="loading-indicator"
-        />
-      )}
-      {mhvSecureMessagingToVaGovRelease && (
-        <>
-          <div className="vads-l-row breadcrumbs">
+    <RequiredLoginView
+      user={user}
+      serviceRequired={[backendServices.MESSAGING]}
+      verify={!environment.isLocalhost()}
+    >
+      <div className="vads-l-grid-container">
+        {/* if the feature flag is undefined, show the loading indicator */}
+        {mhvSecureMessagingToVaGovRelease === undefined && (
+          <va-loading-indicator
+            message="Loading your secure messages..."
+            setFocus
+            data-testid="loading-indicator"
+          />
+        )}
+        {mhvSecureMessagingToVaGovRelease && (
+          <>
             <SmBreadcrumbs />
-          </div>
-          <div className="secure-messaging-container vads-u-display--flex">
-            <div className="vads-u-flex--auto">
-              <Navigation />
-            </div>
 
-            <div className="vads-u-flex--fill">
-              {/* toggle log in and out state without using va.gov sign in, to show the toggle button, comment out the visibility:hidden style */}
-              <button
-                style={{
-                  visibility: 'hidden',
-                  'z-index': '2',
-                  width: '100px',
-                  position: 'absolute',
-                  top: '0',
-                  left: '0',
-                }}
-                type="button"
-                onClick={handleClick}
-              >
-                {isLoggedIn ? <>log out</> : <>log in</>}
-              </button>
+            <div className="secure-messaging-container vads-u-display--flex">
+              <Navigation />
               <Switch>
                 <Route path="/faq" key="MessageFAQ">
-                  <MessageFAQs isLoggedIn={isLoggedIn} />
+                  <MessageFAQs isLoggedIn={loginState} />
                 </Route>
-                {isLoggedIn ? (
-                  <AuthorizedRoutes isLoggedIn={isLoggedIn} />
+                {loginState ? (
+                  <AuthorizedRoutes isLoggedIn={loginState} />
                 ) : (
                   <LandingPageUnauth />
                 )}
               </Switch>
             </div>
-          </div>
-        </>
-      )}
-      {mhvSecureMessagingToVaGovRelease === false && (
-        <>
-          <h1>Secure Messaging</h1>
-          <p className="va-introtext vads-u-margin-top--1">Coming soon...</p>
-        </>
-      )}
-    </div>
+          </>
+        )}
+        {mhvSecureMessagingToVaGovRelease === false && (
+          <>
+            <h1>Secure Messaging</h1>
+            <p className="va-introtext vads-u-margin-top--1">Coming soon...</p>
+          </>
+        )}
+      </div>
+    </RequiredLoginView>
   );
 };
 
