305bbdfc5db945118270c53ffbe9736312b16ff2
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2022-12-28 12:04:09 -0500

    [#MHV-39899] unread msgs card

---
+++ src/applications/mhv/secure-messaging/components/Dashboard/UnreadMessages.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UnreadMessages = () => {
+  return (
+    <div className="unread-messages">
+      <h2>15 Unread Messages</h2>
+      <p>Insert information text here</p>
+      <va-button text="View Inbox" />
+    </div>
+  );
+};
+
+export default UnreadMessages;
