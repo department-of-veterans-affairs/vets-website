6ff35aef851cfd0f44a1769877ac018fbdc600b9
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2022-12-28 09:26:52 -0600

    Update Inbox Messages Sort Test
    Added scripts to sort messages by selecting options from the drop down menu.

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-inbox-message-sort.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-inbox-message-sort.cypress.spec.js
@@ -2,31 +2,44 @@ import manifest from '../../manifest.json';
 import PatientMessagesLandingPage from './pages/PatientMessagesLandingPage';
 
 describe(manifest.appName, () => {
-  it('Sort Inbox Messages from Newest to Oldest', () => {
+  beforeEach(() => {
     const landingPage = new PatientMessagesLandingPage();
     landingPage.login();
     landingPage.loadPage();
     cy.get('.sidebar-navigation-messages-list-header > a');
+  });
+  it('Sort Inbox Messages from Newest to Oldest', () => {
     cy.get('#sort-order-dropdown')
       .shadow()
       .find('select')
 
       .should('contain', 'Newest');
-    cy.get('.message-list-sort > va-button.hydrated').click();
-    cy.injectAxe();
-    cy.axeCheck();
   });
 
   it('Sort Inbox Messages from Oldest to Newest', () => {
-    const landingPage = new PatientMessagesLandingPage();
-    landingPage.login();
-    landingPage.loadPage();
-    cy.get('.sidebar-navigation-messages-list-header > a');
     cy.get('#sort-order-dropdown')
       .shadow()
       .find('select')
       .select('asc', { force: true })
       .should('contain', 'newest');
+  });
+  it('Sort Inbox Messages from A to Z', () => {
+    cy.get('#sort-order-dropdown')
+      .shadow()
+      .find('select')
+      .select('sender-alpha-asc', { force: true })
+      .should('contain', 'A to Z');
+  });
+
+  it('Sort Inbox Messages from Z to A', () => {
+    cy.get('#sort-order-dropdown')
+      .shadow()
+      .find('select')
+      .select('sender-alpha-desc', { force: true })
+      .should('contain', 'Z to A');
+  });
+
+  afterEach(() => {
     cy.get('.message-list-sort > va-button.hydrated').click();
     cy.injectAxe();
     cy.axeCheck();
