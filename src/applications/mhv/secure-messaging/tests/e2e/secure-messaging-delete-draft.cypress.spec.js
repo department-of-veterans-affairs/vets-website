f2e6b56a23268df17a030f1d4869c4f28a62132d
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2022-12-30 08:59:29 -0600

    Test Script for Validating Delete Draft
    Added test to validate the delete draft functionality.

---
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-delete-draft.cypress.spec.js
@@ -0,0 +1,59 @@
+import PatientMessagesLandingPage from './pages/PatientMessagesLandingPage';
+import PatientComposePage from './pages/PatientComposePage';
+import manifest from '../../manifest.json';
+import mockDraftFolderMetaResponse from './fixtures/folder-drafts-metadata.json';
+import mockDraftMessages from './fixtures/drafts-response.json';
+import mockDraftResponse from './fixtures/message-draft-response.json';
+
+describe(manifest.appName, () => {
+  it('Axe Check Save Draft', () => {
+    const landingPage = new PatientMessagesLandingPage();
+    const composePage = new PatientComposePage();
+    landingPage.login();
+    landingPage.loadPage(false);
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/-2',
+      mockDraftFolderMetaResponse,
+    ).as('draftsFolderMetaResponse');
+    cy.get('[data-testid="drafts-sidebar"]').click();
+    cy.injectAxe();
+    cy.axeCheck();
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/-2/messages**',
+      mockDraftMessages,
+    ).as('draftsResponse');
+    cy.wait('@draftsFolderMetaResponse');
+    cy.wait('@draftsResponse');
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/messages/7208913',
+      mockDraftResponse,
+    ).as('draftMessageResponse');
+    cy.contains('test').click();
+    // cy.wait('@draftMessageResponse');
+    cy.injectAxe();
+    cy.axeCheck();
+    cy.get('[data-testid="message-subject-field"]')
+      .shadow()
+      .find('[name="message-subject"]')
+      .type('Testing Drafts');
+    cy.get('[data-testid="message-body-field"]')
+      .shadow()
+      .find('[name="message-body"]')
+      .type('Test message body');
+    composePage.saveDraft();
+
+    cy.get('[data-testid="drafts-sidebar"]').click();
+    cy.get(':nth-child(3) > .message-subject-link').click();
+    cy.get('[data-testid="discard-draft-button"]').click({ force: true });
+
+    cy.get('[data-testid="discard-draft-modal"] > p').should('be.visible');
+    cy.get('[data-testid="discard-draft-modal"]')
+      .shadow()
+      .find('[type ="button"]', { force: true })
+      .contains('Discard draft')
+      .click({ force: true });
+  });
+});
