4f3866fe93989446153562d3fd33d53cc1d7a52c
Brian Seek <brian.seek@civicactions.com>
2023-01-03 14:24:35 -0800

    css update and unit test for appointmentBlockVaos

---
+++ src/applications/check-in/components/tests/AppointmentBlockVaos.unit.spec.jsx
@@ -0,0 +1,231 @@
+import React from 'react';
+import { expect } from 'chai';
+import { render } from '@testing-library/react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../../utils/i18n/i18n';
+import AppointmentBlock from '../AppointmentBlock';
+
+const appointments = [
+  {
+    facility: 'LOMA LINDA VA CLINIC',
+    clinicPhoneNumber: '5551234567',
+    clinicFriendlyName: 'TEST CLINIC',
+    clinicName: 'LOM ACC CLINIC TEST',
+    appointmentIen: 'some-ien',
+    startTime: '2021-11-16T21:39:36',
+    doctorName: 'Dr. Green',
+    clinicStopCodeName: 'Primary care',
+    kind: 'clinic',
+  },
+  {
+    facility: 'LOMA LINDA VA CLINIC',
+    clinicPhoneNumber: '5551234567',
+    clinicFriendlyName: '',
+    clinicName: 'LOM ACC CLINIC TEST',
+    appointmentIen: 'some-ien2',
+    startTime: '2021-11-16T23:00:00',
+    kind: 'clinic',
+  },
+];
+describe('pre-check-in', () => {
+  describe('AppointmentBlock', () => {
+    describe('In person appointment context', () => {
+      it('Renders appointment type label', () => {
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock appointments={appointments} page="confirmation" />
+          </I18nextProvider>,
+        );
+        expect(screen.getAllByTestId('appointment-type-label')[0]).to.have.text(
+          'In person',
+        );
+      });
+      it('Renders appointment day for multiple appointments', () => {
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock appointments={appointments} page="intro" />
+          </I18nextProvider>,
+        );
+        expect(screen.getByTestId('appointment-day-location')).to.have.text(
+          'Your appointments are on November 16, 2021.',
+        );
+        expect(screen.getAllByTestId('appointment-list-item').length).to.equal(
+          2,
+        );
+      });
+      it('Renders appointment day and facility for single appointment', () => {
+        const updateAppointments = [...appointments];
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock
+              appointments={[updateAppointments.shift()]}
+              page="intro"
+            />
+          </I18nextProvider>,
+        );
+        expect(screen.getByTestId('appointment-day-location')).to.have.text(
+          'Your appointment is on November 16, 2021.',
+        );
+
+        expect(screen.getAllByTestId('appointment-list-item').length).to.equal(
+          1,
+        );
+      });
+      it('Renders appointment facility, time, and clinic', () => {
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock appointments={appointments} page="intro" />
+          </I18nextProvider>,
+        );
+        expect(
+          screen
+            .getAllByTestId('appointment-list-item')[0]
+            .querySelector('[data-testid="facility-name"]'),
+        ).to.have.text('LOMA LINDA VA CLINIC');
+        expect(
+          screen
+            .getAllByTestId('appointment-list-item')[0]
+            .querySelector('[data-testid="appointment-time"]'),
+        ).to.have.text('9:39 p.m.');
+        expect(
+          screen
+            .getAllByTestId('appointment-list-item')[0]
+            .querySelector('[data-testid="appointment-clinic"]'),
+        ).to.have.text('TEST CLINIC');
+      });
+      it('Renders clinicName if no clinicFriendlyName', () => {
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock appointments={appointments} page="intro" />
+          </I18nextProvider>,
+        );
+        expect(
+          screen
+            .getAllByTestId('appointment-list-item')[1]
+            .querySelector('[data-testid="appointment-clinic"]'),
+        ).to.have.text('LOM ACC CLINIC TEST');
+      });
+      it('should render the appointment location for in person appointments when available', () => {
+        const locationAppointments = JSON.parse(JSON.stringify(appointments));
+        locationAppointments[0].clinicLocation = 'Test location';
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock appointments={locationAppointments} />
+          </I18nextProvider>,
+        );
+        expect(
+          screen
+            .getAllByTestId('appointment-list-item')[0]
+            .querySelector('[data-testid="clinic-location"]'),
+        ).to.have.text('Test location');
+        expect(
+          screen
+            .getAllByTestId('appointment-list-item')[1]
+            .querySelector('[data-testid="clinic-location"]'),
+        ).to.not.exist;
+      });
+    });
+    describe('Phone appointment context', () => {
+      const phoneAppointments = JSON.parse(JSON.stringify(appointments));
+      phoneAppointments[0].kind = 'phone';
+      phoneAppointments[1].kind = 'phone';
+      it('Renders appointment type label', () => {
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock
+              appointments={phoneAppointments}
+              page="confirmation"
+            />
+          </I18nextProvider>,
+        );
+        expect(screen.getAllByTestId('appointment-type-label')[0]).to.have.text(
+          'Phone Call',
+        );
+      });
+      it('Renders appointment time with no clinic for phone appointments', () => {
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock
+              appointments={phoneAppointments}
+              page="confirmation"
+            />
+          </I18nextProvider>,
+        );
+        expect(screen.getByTestId('appointment-day-location')).to.have.text(
+          'Your appointments are on November 16, 2021.',
+        );
+      });
+      it('Renders phone message with appointment on confirmation page', () => {
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock
+              appointments={phoneAppointments}
+              page="confirmation"
+            />
+          </I18nextProvider>,
+        );
+        expect(screen.getAllByTestId('appointment-message')[0]).to.have.text(
+          'Your provider will call you at your appointment time. You may need to wait about 15 minutes for their call. Thanks for your patience.',
+        );
+      });
+      it('Renders phone message with appointment on intro page', () => {
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock appointments={phoneAppointments} page="intro" />
+          </I18nextProvider>,
+        );
+        expect(screen.getAllByTestId('appointment-message')[0]).to.have.text(
+          'Your provider will call you.',
+        );
+      });
+      it('Does not render facility name', () => {
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock appointments={phoneAppointments} page="intro" />
+          </I18nextProvider>,
+        );
+        expect(screen.queryByTestId('facility-name')).to.not.exist;
+      });
+      it('should render the type of care when available or default', () => {
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock appointments={appointments} />
+          </I18nextProvider>,
+        );
+        expect(
+          screen
+            .getAllByTestId('appointment-list-item')[0]
+            .querySelector('[data-testid="type-of-care"]'),
+        ).to.have.text('Primary care');
+        expect(
+          screen
+            .getAllByTestId('appointment-list-item')[1]
+            .querySelector('[data-testid="type-of-care"]'),
+        ).to.have.text('VA Appointment');
+      });
+      it('should not render the appointment location for phone appointments even when available', () => {
+        const phoneLocationAppointments = JSON.parse(
+          JSON.stringify(appointments),
+        );
+        phoneLocationAppointments[0].clinicLocation = 'Test location';
+        phoneLocationAppointments[0].kind = 'phone';
+        phoneLocationAppointments[1].kind = 'phone';
+        const screen = render(
+          <I18nextProvider i18n={i18n}>
+            <AppointmentBlock appointments={phoneLocationAppointments} />
+          </I18nextProvider>,
+        );
+        expect(
+          screen
+            .getAllByTestId('appointment-list-item')[0]
+            .querySelector('[data-testid="clinic-location"]'),
+        ).to.not.exist;
+        expect(
+          screen
+            .getAllByTestId('appointment-list-item')[1]
+            .querySelector('[data-testid="clinic-location"]'),
+        ).to.not.exist;
+      });
+    });
+  });
+});
