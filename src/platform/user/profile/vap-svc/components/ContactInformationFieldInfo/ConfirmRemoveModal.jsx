fb0995488ad3c94300c140a413d350fb82f388cb
Adam Whitlock <adam.whitlock@adhocteam.us>
2023-01-03 12:50:47 -0700

    (fix) migrate to VaModal, update btn text

--- src/platform/user/profile/vap-svc/components/ContactInformationFieldInfo/ConfirmRemoveModal.jsx
+++ src/platform/user/profile/vap-svc/components/ContactInformationFieldInfo/ConfirmRemoveModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Modal from '@department-of-veterans-affairs/component-library/Modal';
+import { VaModal } from '@department-of-veterans-affairs/web-components/react-bindings';
 import LoadingButton from 'platform/site-wide/loading-button/LoadingButton';
 import { FIELD_NAMES } from 'platform/user/profile/vap-svc/constants';
 import VAPServiceEditModalErrorMessage from 'platform/user/profile/vap-svc/components/base/VAPServiceEditModalErrorMessage';
@@ -17,12 +17,12 @@ const ConfirmRemoveModal = ({
   error,
 }) => {
   return (
-    <Modal
-      title="Are you sure?"
-      cssClass="overflow-auto"
+    <VaModal
+      modalTitle="Are you sure?"
+      className="overflow-auto"
       status="warning"
       visible={isVisible}
-      onClose={onHide}
+      onCloseEvent={onHide}
     >
       <div>
         This will remove your {title.toLowerCase()} across these VA benefits and
@@ -80,11 +80,11 @@ const ConfirmRemoveModal = ({
             className="usa-button-secondary"
             onClick={cancelAction}
           >
-            Cancel
+            No, cancel this change
           </button>
         )}
       </div>
-    </Modal>
+    </VaModal>
   );
 };
 
