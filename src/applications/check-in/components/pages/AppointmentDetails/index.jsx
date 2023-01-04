ba23945be7576e8573f3b07062f5e1d94f89da32
Brian Seek <brian.seek@civicactions.com>
2023-01-04 11:24:38 -0800

    Updates around reload of details.

--- src/applications/check-in/components/pages/AppointmentDetails/index.jsx
+++ src/applications/check-in/components/pages/AppointmentDetails/index.jsx
@@ -23,134 +23,150 @@ const AppointmentDetails = props => {
   const { activeAppointment } = useSelector(selectActiveAppointment);
   const [appointment, setAppointment] = useState([]);
 
-  const appointmentDay = new Date(appointment.startTime);
+  const appointmentDay = new Date(appointment?.startTime);
   const isPhoneAppointment = appointment?.kind === 'phone';
 
   useEffect(
     () => {
       if (activeAppointment) {
-        setAppointment(
-          appointments.find(
-            appointmentItem =>
-              appointmentItem.appointmentIen === activeAppointment,
-          ),
+        const activeAppointmentDetails = appointments.find(
+          appointmentItem =>
+            appointmentItem.appointmentIen === activeAppointment,
         );
-      } else {
-        jumpToPage('complete');
+        if (activeAppointmentDetails) {
+          setAppointment(
+            appointments.find(
+              appointmentItem =>
+                appointmentItem.appointmentIen === activeAppointment,
+            ),
+          );
+          return;
+        }
       }
+      // Go back to complete page if no activeAppointment or not in list.
+      jumpToPage('complete');
     },
     [activeAppointment, appointments, jumpToPage],
   );
 
-  const clinic = appointment.clinicFriendlyName
-    ? appointment.clinicFriendlyName
-    : appointment.clinicName;
+  const clinic = appointment?.clinicFriendlyName
+    ? appointment?.clinicFriendlyName
+    : appointment?.clinicName;
+
   return (
     <>
-      <BackButton
-        router={router}
-        action={goToPreviousPage}
-        text={t('back-to-appointments')}
-      />
-      <Wrapper classNames="appointment-details-page" withBackButton>
-        <div className="appointment-details--container vads-u-margin-top--2 vads-u-border--2px vads-u-border-color--gray-lighter vads-u-padding-x--2 vads-u-padding-top--4 vads-u-padding-bottom--2">
-          <div className="appointment-details--icon">
-            <i
-              aria-label="Appointment type"
-              className={`fas ${
-                appointment?.kind === 'phone' ? 'fa-phone' : 'fa-building'
-              }`}
-              aria-hidden="true"
-            />
-          </div>
-          <h1
-            tabIndex="-1"
-            data-testid="header"
-            className="vads-u-font-size--h3"
-          >
-            {`${isPhoneAppointment ? t('phone') : t('in-person')} ${t(
-              'appointment',
-            )}`}
-          </h1>
-          {isPhoneAppointment ? (
-            <p
-              data-testid="phone-appointment-subtitle"
-              className="vads-u-margin--0"
-            >
-              {t('your-provider-will-call-you-at-your-appointment-time')}
-            </p>
-          ) : (
-            <p
-              data-testid="in-person-appointment-subtitle"
-              className="vads-u-margin--0"
-            >
-              {t(
-                'please-bring-your-insurance-cards-with-you-to-your-appointment',
-              )}
-            </p>
-          )}
-          <div data-testid="appointment-details--when">
-            <h2 className="vads-u-font-size--sm">{t('when')}</h2>
-            <div data-testid="appointment-details--date-value">
-              {isValid(appointmentDay) &&
-                t('appointment-day', { date: appointmentDay })}
-            </div>
-          </div>
-          <div data-testid="appointment-details--what">
-            <h2 className="vads-u-font-size--sm">{t('what')}</h2>
-            <div data-testid="appointment-details--appointment-value">
-              {appointment.clinicStopCodeName ?? t('VA-appointment')}
-            </div>
-          </div>
-          {appointment.doctorName && (
-            <div data-testid="appointment-details--provider">
-              <h2 className="vads-u-font-size--sm">{t('Provider')}</h2>
-              <div data-testid="appointment-details--provider-value">
-                {appointment.doctorName}
-              </div>
-            </div>
-          )}
-          <div data-testid="appointment-details--where">
-            <h2 className="vads-u-font-size--sm">
-              {isPhoneAppointment ? t('clinic') : t('where-to-attend')}
-            </h2>
-            {/* TODO add address for in person appointments */}
-            <div data-testid="appointment-details--clinic-value">
-              {isPhoneAppointment ? '' : t('clinic')}: {clinic}
-            </div>
-            {isPhoneAppointment ? (
-              ''
-            ) : (
-              <div data-testid="appointment-details--location-value">
-                {`${t('location')}: ${appointment.clinicLocation}`}
-              </div>
-            )}
-          </div>
-          {appointment.clinicPhoneNumber && (
-            <div data-testid="appointment-details--phone">
-              <h2 className="vads-u-font-size--sm">{t('phone')}</h2>
-              <div data-testid="appointment-details--phone-value">
+      {appointment ? (
+        <>
+          <BackButton
+            router={router}
+            action={goToPreviousPage}
+            text={t('back-to-appointments')}
+          />
+          <Wrapper classNames="appointment-details-page" withBackButton>
+            <div className="appointment-details--container vads-u-margin-top--2 vads-u-border--2px vads-u-border-color--gray-lighter vads-u-padding-x--2 vads-u-padding-top--4 vads-u-padding-bottom--2">
+              <div className="appointment-details--icon">
                 <i
-                  aria-label="phone"
-                  className="fas fa-phone vads-u-color--link-default vads-u-margin-right--1"
+                  aria-label="Appointment type"
+                  className={`fas ${
+                    appointment?.kind === 'phone' ? 'fa-phone' : 'fa-building'
+                  }`}
                   aria-hidden="true"
                 />
-                <va-telephone contact={appointment.clinicPhoneNumber}>
-                  {appointment.clinicPhoneNumber}
-                </va-telephone>
               </div>
-            </div>
-          )}
-          {appointment.reasonForVisit && (
-            <div data-testid="appointment-details--reason">
-              <h2 className="vads-u-font-size--sm">{t('reason-for-visit')}</h2>
-              <div data-testid="appointment-details--reason-value">
-                {appointment.reasonForVisit}
+              <h1
+                tabIndex="-1"
+                data-testid="header"
+                className="vads-u-font-size--h3"
+              >
+                {`${isPhoneAppointment ? t('phone') : t('in-person')} ${t(
+                  'appointment',
+                )}`}
+              </h1>
+              {isPhoneAppointment ? (
+                <p
+                  data-testid="phone-appointment-subtitle"
+                  className="vads-u-margin--0"
+                >
+                  {t('your-provider-will-call-you-at-your-appointment-time')}
+                </p>
+              ) : (
+                <p
+                  data-testid="in-person-appointment-subtitle"
+                  className="vads-u-margin--0"
+                >
+                  {t(
+                    'please-bring-your-insurance-cards-with-you-to-your-appointment',
+                  )}
+                </p>
+              )}
+              <div data-testid="appointment-details--when">
+                <h2 className="vads-u-font-size--sm">{t('when')}</h2>
+                <div data-testid="appointment-details--date-value">
+                  {isValid(appointmentDay) &&
+                    t('appointment-day', { date: appointmentDay })}
+                </div>
               </div>
+              <div data-testid="appointment-details--what">
+                <h2 className="vads-u-font-size--sm">{t('what')}</h2>
+                <div data-testid="appointment-details--appointment-value">
+                  {appointment.clinicStopCodeName ?? t('VA-appointment')}
+                </div>
+              </div>
+              {appointment.doctorName && (
+                <div data-testid="appointment-details--provider">
+                  <h2 className="vads-u-font-size--sm">{t('Provider')}</h2>
+                  <div data-testid="appointment-details--provider-value">
+                    {appointment.doctorName}
+                  </div>
+                </div>
+              )}
+              <div data-testid="appointment-details--where">
+                <h2 className="vads-u-font-size--sm">
+                  {isPhoneAppointment ? t('clinic') : t('where-to-attend')}
+                </h2>
+                {/* TODO add address for in person appointments */}
+                <div data-testid="appointment-details--clinic-value">
+                  {isPhoneAppointment ? '' : t('clinic')}: {clinic}
+                </div>
+                {isPhoneAppointment ? (
+                  ''
+                ) : (
+                  <div data-testid="appointment-details--location-value">
+                    {`${t('location')}: ${appointment.clinicLocation}`}
+                  </div>
+                )}
+              </div>
+              {appointment.clinicPhoneNumber && (
+                <div data-testid="appointment-details--phone">
+                  <h2 className="vads-u-font-size--sm">{t('phone')}</h2>
+                  <div data-testid="appointment-details--phone-value">
+                    <i
+                      aria-label="phone"
+                      className="fas fa-phone vads-u-color--link-default vads-u-margin-right--1"
+                      aria-hidden="true"
+                    />
+                    <va-telephone contact={appointment.clinicPhoneNumber}>
+                      {appointment.clinicPhoneNumber}
+                    </va-telephone>
+                  </div>
+                </div>
+              )}
+              {appointment.reasonForVisit && (
+                <div data-testid="appointment-details--reason">
+                  <h2 className="vads-u-font-size--sm">
+                    {t('reason-for-visit')}
+                  </h2>
+                  <div data-testid="appointment-details--reason-value">
+                    {appointment.reasonForVisit}
+                  </div>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </Wrapper>
+          </Wrapper>
+        </>
+      ) : (
+        <va-loading-indicator message={t('loading')} />
+      )}
     </>
   );
 };
