388d4655e4a5f5d521e31e8e5f6bef006fb5ddcb
Brian Seek <brian.seek@civicactions.com>
2023-01-06 13:43:11 -0800

    Added unit test for list item component.

--- src/applications/check-in/components/AppointmentListItem.jsx
+++ src/applications/check-in/components/AppointmentListItem.jsx
@@ -16,20 +16,32 @@ const AppointmentListItem = props => {
       data-testid="appointment-list-item"
     >
       <div className="check-in--appointment-summary vads-u-margin-bottom--2 vads-u-margin-top--2">
-        <div className="vads-u-font-size--h2 vads-u-font-family--serif">
+        <div
+          data-testid="appointment-time"
+          className="vads-u-font-size--h2 vads-u-font-family--serif"
+        >
           {t('date-time', { date: appointmentDateTime })}
         </div>
-        <div className="vads-u-font-weight--bold">
+        <div
+          data-testid="appointment-type-and-provider"
+          className="vads-u-font-weight--bold"
+        >
           {appointment.clinicStopCodeName ?? t('VA-appointment')}
           {appointment.doctorName
             ? ` ${t('with')} ${appointment.doctorName}`
             : ''}
         </div>
         <div>
-          <div className="vads-u-margin-right--1 check-in--label">
+          <div
+            data-testid="appointment-kind-icon"
+            className="vads-u-margin-right--1 check-in--label"
+          >
             {appointmentIcon(appointment)}
           </div>
-          <div className="vads-u-display--inline">
+          <div
+            data-testid="appointment-kind-and-location"
+            className="vads-u-display--inline"
+          >
             {appointment?.kind === 'phone' ? (
               t('phone')
             ) : (
