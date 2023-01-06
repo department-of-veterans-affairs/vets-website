34786ff82bcddd858b489f8a0b1e72b43cecebaa
ewaz-farhang <ewaz.farhang@accenturefederal.com>
2023-01-06 10:45:32 -0500

    Letters

---
+++ src/applications/education-letters/tests/e2e/00-letters-unauthenticated-all-fields.cypress.spec.js
@@ -0,0 +1,44 @@
+/// <reference types='cypress' />
+
+describe('All Field, texts and links should be validated on letters app', () => {
+  it('All texts are present for the letters page unauthenticated', () => {
+    cy.visit('http://localhost:3001/education/download-letters/');
+
+    cy.injectAxeThenAxeCheck();
+    cy.url().should('include', '/education/download-letters/');
+
+    cy.get('.va-introtext').should(
+      'have.text',
+      'If you’re a Veteran and you recently received your VA education decision letter, you can download it now.',
+    );
+    cy.findByTestId('form-title').should(
+      'have.text',
+      'Download your VA education letter',
+    );
+    cy.findByText('Who can download VA education letters?').should(
+      'be.visible',
+    );
+    cy.findByText(
+      'You can download your education letter if you’re a Veteran and you meet both of the requirements listed here. At this time, family members and dependents can’t get their education letters online.',
+    ).should('be.visible');
+  });
+
+  it('All texts are present for the letters page authenticated but no letter', () => {
+    cy.login();
+    cy.visit('http://localhost:3001/education/download-letters/');
+    cy.injectAxeThenAxeCheck();
+    cy.url().should('include', '/education/download-letters/');
+
+    cy.get('.va-introtext').should(
+      'have.text',
+      'Download important documents about your education benefits here, including your decision letters. ',
+    );
+    cy.findByTestId('form-title').should('have.text', 'VA education inbox');
+    cy.findByText(
+      'Your letter is not available to you through this tool',
+    ).should('be.visible');
+    cy.findByText(
+      'The letter displayed will be based on your most recent claim submission. If your decision was prior to August 20, 2022 – or you’re a family member or dependent – your decision letter will not be listed here. You can contact us through Ask VA to request a copy of your letter. Request your VA education letter through',
+    ).should('be.visible');
+  });
+});
