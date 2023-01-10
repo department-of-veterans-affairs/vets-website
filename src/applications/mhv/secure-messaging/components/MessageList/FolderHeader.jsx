8552734bf25dea84575d91d331c20b75100c56f9
Mike Moyer <michael.moyer@va.gov>
2023-01-10 13:30:20 -0500

    [#MHV-38775] Refactored Compose button

--- src/applications/mhv/secure-messaging/components/MessageList/FolderHeader.jsx
+++ src/applications/mhv/secure-messaging/components/MessageList/FolderHeader.jsx
@@ -60,15 +60,7 @@ const FolderHeader = props => {
       {folder.folderId === Folders.INBOX.id && (
         <>
           <EmergencyNote dropDownFlag />
-          <p className="vads-u-margin-top--0p5 vads-u-margin-bottom--2p5">
-            <Link
-              className="vads-c-action-link--blue compose-message-link"
-              to="/compose"
-              data-testid="compose-message-link"
-            >
-              Compose message
-            </Link>
-          </p>
+          <ComposeMessageButton />
         </>
       )}
       <ManageFolderButtons />
