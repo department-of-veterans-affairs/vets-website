ee1eeca8323ed42eabbabc68066235d1f80c8041
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2023-01-10 12:44:33 -0600

    update test to search special characters
    Fixed the cy.wait() for api intercept. Network error is fixed.

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-specialcharacter-search.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-specialcharacter-search.cypress.spec.js
@@ -30,33 +30,24 @@ describe(manifest.appName, () => {
     ).as('basicSearchRequestSentFolder');
     cy.intercept(
       'GET',
-      '/my_health/v1/messaging/messages/2585370/thread',
+      '/my_health/v1/messaging/messages/2585370',
       mockMessages,
     ).as('specialCharSearch');
-
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/messages/2585370/thread',
+      mockMessages,
+    ).as('specialCharSearchMessage');
     cy.get('[data-testid="sent-sidebar"]').click();
 
-    basicSearchPage.typeSearchInputFieldText('special message%$#*');
+    basicSearchPage.typeSearchInputFieldText('message%$#*');
 
     basicSearchPage.submitSearch();
     cy.wait('@basicSearchRequestSentFolder');
-    // basicSearchPage.verifyHighlightedText('special');
+
     cy.injectAxe();
     cy.axeCheck();
-    // cy.get('[data-testid="highlighted-text"]').click({ force: true });
-
-    // cy.wait('@specialCharSearch');
-    // cy.on('uncaught:exception', (err, runnable) => {
-    //     // returning false here prevents Cypress from
-    //     // failing the test
-    //     return false;
-
-    // });
-
-    cy.get('.message-subject-link > :nth-child(1) > span').click({
-      waitforanimations: true,
-    });
-    // cy.get('[class="message-subject-link vads-u-margin-y--0p5"]').click({ force: true });
-    // cy.get('.message-subject-link').click({ force: true });
+    cy.get('.message-subject-link', { timeout: 5000 }).click({ force: true });
+    cy.wait('@specialCharSearchMessage');
   });
 });
