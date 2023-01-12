9623fbfd3e788ccf9c7facb0986045cd38e2e77b
jerekshoe <jerek.shoemaker@adhocteam.us>
2023-01-12 15:27:48 -0600

    Adding clickURL to event

--- src/applications/claims-status/components/ClaimLetterListItem.jsx
+++ src/applications/claims-status/components/ClaimLetterListItem.jsx
@@ -36,8 +36,9 @@ const filename = 'ClaimLetter.pdf';
 const downloadHandler = () => {
   recordEvent({
     event: 'claim-letters-download',
-    eventLabel: `${environment.API_URL}/v0/claim_letters/:id`,
+    eventLabel: `${environment.API_URL}/v0/claim_letters/:id.pdf`,
     clickText: 'Download letter (PDF)',
+    clickURL: `${environment.API_URL}/v0/claim_letters/:id.pdf`,
   });
 };
 
