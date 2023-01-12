03a67b3079d1bf01f1c6699b9951e7a9e63ae8de
RajiVenkatesh18 <114511630+RajiVenkatesh18@users.noreply.github.com>
2023-01-12 13:56:10 -0600

    Mhv 39776 compose special characters (#23116)
    * Validate SM Compose Message with Special Characters
    
    Test scripts added to validate special character in compose messages.
    
    * update compose message special character
    
    Added scripts to validate message with special character is sent successfully.
    
    * updates to compose message special character
    
    Made changes to include search text - test with Special characters and modify data for mock Messages in the test.
    
    * updates to compose special characters
    
    added scripts to search for message that has special characters.
    
    * updates to compose special characters
    
    added json file to include special character message info
    
    * update test to search special characters
    
    Added scripts to search for special characters in the subject
    
    * update test to search special characters
    
    added scripts to search for special characters in the subject and message body.
    
    * update test to search special characters
    
    Fixed the cy.wait() for api intercept. Network error is fixed.
    
    * Refactor to use LoadMessageDetails method
    
    * Fixes to loadMessageDetails
    
    * update special character search in message body
    
    Added scripts to find special character in the message body.
    
    * Update secure-messaging-specialcharacter-search.cypress.spec.js
    
    * update test to  search for special characters
    
    * Update secure-messaging-specialcharacter-search.cypress.spec.js
    
    * update search special characters test
    
    deleted the unnecessary test scripts
    
    * Update secure-messaging-basic-search.cypress.spec.js
    
    Co-authored-by: raji.venkatesh <raji.venkatesh@accomsolutions.com>
    Co-authored-by: Aaron Read <Aaron.Read@va.gov>

---
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-specialcharacter-search.cypress.spec.js
@@ -0,0 +1,43 @@
+import SecureMessagingSite from './site/SecureMessagingSite';
+import PatientBasicSearchPage from './pages/PatientBasicSearchPage';
+import PatientInboxPage from './pages/PatientInboxPage';
+import mockMessages from './fixtures/drafts-search-results.json';
+import mockSpeciaCharMessage from './fixtures/message-response-specialchars.json';
+import mockSentFolder from './fixtures/folder-sent-metadata.json';
+
+describe('Secure Messaging - Search Special Characters', () => {
+  const basicSearchPage = new PatientBasicSearchPage();
+
+  const landingPage = new PatientInboxPage();
+  const site = new SecureMessagingSite();
+
+  it('search for special characters', () => {
+    site.login();
+    landingPage.loadPage();
+    cy.injectAxe();
+    cy.axeCheck();
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/-1',
+      mockSentFolder,
+    ).as('basicSearchRequestSentMeta');
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/-1/messages?per_page=-1',
+      mockMessages,
+    ).as('basicSearchRequestSentFolder');
+    cy.get('[data-testid="sent-sidebar"]').click();
+
+    basicSearchPage.typeSearchInputFieldText('message%$#*');
+
+    basicSearchPage.submitSearch();
+    cy.wait('@basicSearchRequestSentFolder');
+
+    mockSpeciaCharMessage.data.attributes.messageId = '2585370';
+    mockSpeciaCharMessage.data.attributes.body = 'special %$#';
+    landingPage.loadMessageDetailsWithData(mockSpeciaCharMessage);
+    cy.get('pre').should('contain', 'special %$#');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+});
