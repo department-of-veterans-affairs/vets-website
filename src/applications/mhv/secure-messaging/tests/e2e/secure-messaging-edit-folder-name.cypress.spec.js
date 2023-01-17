e69abc681133c43e35716458281f3fdfdb31a582
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2023-01-17 10:29:39 -0600

    Validate Edit Folder Name Button
    Added test scripts to verify edit folder name button in the custom folder.

---
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-edit-folder-name.cypress.spec.js
@@ -0,0 +1,40 @@
+import SecureMessagingSite from './site/SecureMessagingSite';
+import PatientInboxPage from './pages/PatientInboxPage';
+import customFolderMessage from './fixtures/messages-response.json';
+import customFolder from './fixtures/folder-custom-metadata.json';
+
+describe('Secure Messaging Custom Folder Delete Error Message Validation', () => {
+  it('Axe Check Custom Folder List', () => {
+    const landingPage = new PatientInboxPage();
+    const site = new SecureMessagingSite();
+    site.login();
+
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/7038175',
+      customFolder,
+    ).as('test2Folder');
+    landingPage.loadPage();
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/7038175/messages?per_page=-1&useCache=false',
+      customFolderMessage,
+    ).as('customFolder');
+
+    cy.get('[data-testid="my-folders-sidebar"]').click();
+
+    // cy.wait('@customFolder');
+    // cy.wait('@test2Folder');
+    cy.contains('TEST2').click();
+
+    // cy.injectAxe();
+    // cy.axeCheck();
+
+    cy.get('.left-button').click({ force: true });
+    cy.get('[name="new-folder-name"]')
+      .shadow()
+      .find('[id="inputField"]')
+      .type('Testing');
+    cy.get('[visible=""] > [secondary=""]').click();
+  });
+});
