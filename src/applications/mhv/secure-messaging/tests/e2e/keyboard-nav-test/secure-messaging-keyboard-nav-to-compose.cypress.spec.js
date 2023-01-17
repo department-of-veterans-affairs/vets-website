029a5954d74d722cd8790bfc005a81cfa09d8707
Aaron Read <Aaron.Read@va.gov>
2023-01-17 14:31:49 -0600

    Initial commit

---
+++ src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-compose.cypress.spec.js
@@ -0,0 +1,23 @@
+import SecureMessagingWelcomePage from '../pages/SecureMessagingWelcomePage';
+import SecureMessagingSite from '../sm_site/SecureMessagingSite';
+
+describe('Secure Messaging Compose', () => {
+  const landingPage = new SecureMessagingWelcomePage();
+  const site = new SecureMessagingSite();
+  beforeEach(() => {
+    site.login();
+    landingPage.loadPage();
+  });
+  it('Tab to Message Body', () => {
+    cy.tabToElement('[data-testid="compose-message-link"]').pushEnter();
+    cy.injectAxe();
+    cy.axeCheck();
+    cy.tabToElement('[data-testid="message-body-field"] ').should('exist');
+  });
+  it('Tab to Save Draft Button', () => {
+    cy.get('[data-testid="compose-message-link"]').click();
+    cy.injectAxe();
+    cy.axeCheck();
+    cy.tabToElement('[data-testid="Save-Draft-Button"]').should('exist');
+  });
+});
