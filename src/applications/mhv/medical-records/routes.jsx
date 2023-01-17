61bfcd68d2490a2c6ad8e945abdf694be66bf60a
KolbyVA <kolby.balch@va.gov>
2023-01-17 09:35:29 -0600

    [#MHV-40466] added breadcrumbs to MR

--- src/applications/mhv/medical-records/routes.jsx
+++ src/applications/mhv/medical-records/routes.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import MrBreadcrumbs from './containers/MrBreadcrumbs';
 import App from './containers/App';
 
 const routes = (
-  <Switch>
-    <Route path="/" key="App">
-      <App />
-    </Route>
-  </Switch>
+  <div className="vads-l-grid-container">
+    <MrBreadcrumbs />
+    <Switch>
+      <Route path="/" key="App">
+        <App />
+      </Route>
+    </Switch>
+  </div>
 );
 
 export default routes;
---
+++ src/applications/mhv/medical-records/util/actionTypes.js
@@ -0,0 +1,5 @@
+export const Actions = {
+  Breadcrumbs: {
+    SET_BREAD_CRUMBS: 'SM_SET_BREAD_CRUMBS',
+  },
+};
