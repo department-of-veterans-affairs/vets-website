b339d150f90158cd3ae1c3e7be8f0c42d07e9c9e
Kolby Balch <108146636+KolbyVA@users.noreply.github.com>
2023-01-18 13:21:36 -0600

    [#MHV-40466] added breadcrumbs to MR (#23154)
    * [#MHV-40466] added breadcrumbs to MR
    
    * [#MHV-40466] changes SmBreadcrumbs to MrBreadcrumbs

--- src/applications/mhv/medical-records/routes.jsx
+++ src/applications/mhv/medical-records/routes.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import MrBreadcrumbs from './containers/MrBreadcrumbs';
 import App from './containers/App';
 import VaccineDetails from './containers/VaccineDetails';
 
 const routes = (
-  <Switch>
-    <Route exact path="/" key="App">
-      <App />
-    </Route>
-    <Route exact path="/vaccine" key="vaccine">
-      <VaccineDetails />
-    </Route>
-  </Switch>
+  <div className="vads-l-grid-container">
+    <MrBreadcrumbs />
+    <Switch>
+      <Route exact path="/" key="App">
+        <App />
+      </Route>
+      <Route exact path="/vaccine" key="vaccine">
+        <VaccineDetails />
+      </Route>
+    </Switch>
+  </div>
 );
 
 export default routes;
