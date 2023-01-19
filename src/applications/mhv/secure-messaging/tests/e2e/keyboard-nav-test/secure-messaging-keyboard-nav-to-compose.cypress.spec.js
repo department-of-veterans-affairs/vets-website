5a315dbe2207b6d1f7813ced644186a2c17e192e
Aaron Read <Aaron.Read@va.gov>
2023-01-19 10:06:09 -0600

    Add nav Tests and refactor

--- src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-compose.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-compose.cypress.spec.js
@@ -1,30 +1,16 @@
-import SecureMessagingWelcomePage from '../pages/SecureMessagingWelcomePage';
 import SecureMessagingSite from '../sm_site/SecureMessagingSite';
 
-describe('Secure Messaging Compose', () => {
-  const landingPage = new SecureMessagingWelcomePage();
+describe('Secure Messaging Keyboard Nav To Compose', () => {
   const site = new SecureMessagingSite();
   beforeEach(() => {
     site.login();
-    landingPage.loadPage();
+    site.loadPage();
   });
-  it.skip('Keyboard Nav from Welcome Page to Compose', () => {
-    cy.tabToElement('[data-testid="compose-message-link"]').type(
-      'Cypress.io{enter}',
-    );
+  it('Keyboard Nav from Welcome Page to Compose', () => {
+    cy.tabToElement('[data-testid="compose-message-link"]');
+    cy.realPress(['Enter']);
     cy.injectAxe();
     cy.axeCheck();
-    cy.tabToElement('[data-testid="message-body-field"] ').type(
-      'Cypress.io{enter}',
-    );
-  });
-  it('Keyboard Nav from Welcome Page to Inbox', () => {
-    cy.tabToElement('[data-testid="inbox-sidebar"]');
-    // .type(
-    //  'Cypress.io{enter}',
-    // );
-    cy.injectAxe();
-    // cy.axeCheck();
-    // cy.tabToElement('[data-testid="Save-Draft-Button"]').should('exist');
+    cy.tabToElement('[data-testid="message-body-field"] ');
   });
 });
