748d4084430072138260da8a07845103e5d4c495
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2023-01-19 08:42:32 -0600

    Validate Compose Clickable URL
    Added test scripts to verify clickable URL in the message body.

---
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-compose-clickable-URL.cypress.spec.js
@@ -0,0 +1,51 @@
+import SecureMessagingSite from './sm_site/SecureMessagingSite';
+import PatientBasicSearchPage from './pages/PatientBasicSearchPage';
+import PatientInboxPage from './pages/PatientInboxPage';
+import mockMessages from './fixtures/drafts-search-results.json';
+import mockSentFolder from './fixtures/folder-sent-metadata.json';
+import mockURLMessage from './fixtures/message-clickableUrl-response.json';
+
+describe('Secure Messaging - Compose with Clickable URL', () => {
+  const basicSearchPage = new PatientBasicSearchPage();
+
+  const landingPage = new PatientInboxPage();
+  const site = new SecureMessagingSite();
+
+  it('search for clickable URL', () => {
+    site.login();
+    landingPage.loadPage();
+    cy.injectAxe();
+    cy.axeCheck();
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/-1',
+      mockSentFolder,
+    ).as('basicSearchRequestSentMeta');
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/-1/messages?per_page=-1',
+      mockMessages,
+    ).as('basicSearchRequestSentFolder');
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/messages/2585368',
+      mockURLMessage,
+    ).as('urlMessage');
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/messages/2585368/thread',
+      mockURLMessage,
+    ).as('urlMessageThread');
+    cy.get('[data-testid="sent-sidebar"]').click();
+    basicSearchPage.typeSearchInputFieldText('clickable url');
+    basicSearchPage.submitSearch();
+
+    cy.wait('@basicSearchRequestSentFolder');
+
+    cy.contains('clickable url test').click();
+    cy.get('pre > a').click();
+
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+});
