e839524aaa38d0d5bf2c34f708dce008230d3ed9
nbuckafs <nathaniel.buck@accenturefederal.com>
2023-01-20 16:48:25 -0500

    Add Unit Tests for Utility Functions

--- src/applications/enrollment-verification/tests/unit/helpers.unit.spec.js
+++ src/applications/enrollment-verification/tests/unit/helpers.unit.spec.js
@@ -1,6 +1,11 @@
 import { expect } from 'chai';
 import { PAYMENT_PAUSED_DAY_OF_MONTH } from '../../constants';
-import { monthlyPaymentsPaused } from '../../helpers';
+import {
+  convertNumberToStringWithMinimumDigits,
+  formatNumericalDate,
+  formatReadableMonthYear,
+  monthlyPaymentsPaused,
+} from '../../helpers';
 // import { prefillTransformerV1, prefillTransformerV2 } from '../../helpers';
 // import { claimantInfo } from '../fixtures/data/prefill-transformer-test-data';
 
@@ -10,6 +15,38 @@ describe('helpers', () => {
   //   beforeEach(() => {
   //     mockClaimantInfo = JSON.parse(JSON.stringify(claimantInfo));
   //   });
+  describe('month formatters', () => {
+    it('formatNumericalDate', () => {
+      expect(formatNumericalDate('2019-04-10')).to.eql('4/10/2019');
+      expect(formatNumericalDate('2020-02-29')).to.eql('2/29/2020');
+      expect(formatNumericalDate('')).to.eql('');
+    });
+
+    it('formatReadableMonthYear', () => {
+      expect(formatReadableMonthYear('2021-01-14')).to.eql('January 2021');
+      expect(formatReadableMonthYear('2000-12-31')).to.eql('December 2000');
+    });
+  });
+
+  describe('utility functions', () => {
+    it('convertNumberToStringWithMinimumDigits', () => {
+      // expect(convertNumberToStringWithMinimumDigits(5, 0)).to.eql('5');
+      expect(convertNumberToStringWithMinimumDigits(5, 1)).to.eql('5');
+      expect(convertNumberToStringWithMinimumDigits(5, 2)).to.eql('05');
+      expect(convertNumberToStringWithMinimumDigits(5, 3)).to.eql('005');
+      expect(convertNumberToStringWithMinimumDigits(2022, 2)).to.eql('2022');
+      expect(convertNumberToStringWithMinimumDigits(2022, 5)).to.eql('02022');
+      expect(convertNumberToStringWithMinimumDigits(3.14159, 1)).to.eql(
+        '3.142',
+      );
+      expect(convertNumberToStringWithMinimumDigits(3.14159, 2)).to.eql(
+        '03.142',
+      );
+      expect(convertNumberToStringWithMinimumDigits(3.14159, 3)).to.eql(
+        '003.142',
+      );
+    });
+  });
 
   describe('paused payments calculation', () => {
     it('payments are not paused when the last certified through date is in the future', () => {
