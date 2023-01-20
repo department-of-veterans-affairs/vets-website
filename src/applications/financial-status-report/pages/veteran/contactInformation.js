26340be7b217f08a7d3166f0c17a7468f6bb76f7
Andrew Rodiek <andrew.rodiek@gmail.com>
2023-01-20 09:53:10 -0500

    Adding new contact info validation

--- src/applications/financial-status-report/pages/veteran/contactInformation.js
+++ src/applications/financial-status-report/pages/veteran/contactInformation.js
@@ -1,11 +1,11 @@
 import ContactInfoDescription from '../../components/ContactInformation';
-// import { contactInfoValidation } from '../validations';
+import { contactInfoValidation } from '../../utils/validations';
 
 export const uiSchema = {
   'ui:title': ' ',
   'ui:description': ContactInfoDescription,
   'ui:required': () => true,
-  // 'ui:validations': [contactInfoValidation],
+  'ui:validations': [contactInfoValidation],
   'ui:options': {
     hideOnReview: true,
     forceDivWrapper: true,
