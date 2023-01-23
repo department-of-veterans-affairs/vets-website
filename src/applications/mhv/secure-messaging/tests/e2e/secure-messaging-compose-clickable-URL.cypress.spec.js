f54bbd319691dce01b41911532cdb483c5743c8c
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2023-01-23 09:32:10 -0600

    Updates to ClickableURL test
    Added method load MessageDetailsWithData()

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-compose-clickable-URL.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-compose-clickable-URL.cypress.spec.js
@@ -3,7 +3,8 @@ import PatientBasicSearchPage from './pages/PatientBasicSearchPage';
 import PatientInboxPage from './pages/PatientInboxPage';
 import mockMessages from './fixtures/drafts-search-results.json';
 import mockSentFolder from './fixtures/folder-sent-metadata.json';
-import mockURLMessage from './fixtures/message-clickableUrl-response.json';
+// import mockURLMessage from './fixtures/message-clickableUrl-response.json';
+import mockSpeciaCharMessage from './fixtures/message-response-specialchars.json';
 
 describe('Secure Messaging - Compose with Clickable URL', () => {
   const basicSearchPage = new PatientBasicSearchPage();
@@ -26,23 +27,28 @@ describe('Secure Messaging - Compose with Clickable URL', () => {
       '/my_health/v1/messaging/folders/-1/messages?per_page=-1',
       mockMessages,
     ).as('basicSearchRequestSentFolder');
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/messages/2585368',
-      mockURLMessage,
-    ).as('urlMessage');
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/messages/2585368/thread',
-      mockURLMessage,
-    ).as('urlMessageThread');
+    // cy.intercept(
+    //   'GET',
+    //   '/my_health/v1/messaging/messages/2585368',
+    //   mockURLMessage,
+    // ).as('urlMessage');
+    // cy.intercept(
+    //   'GET',
+    //   '/my_health/v1/messaging/messages/2585368/thread',
+    //   mockURLMessage,
+    // ).as('urlMessageThread');
     cy.get('[data-testid="sent-sidebar"]').click();
-    basicSearchPage.typeSearchInputFieldText('clickable url');
+
+    basicSearchPage.typeSearchInputFieldText('message%$#*');
+
     basicSearchPage.submitSearch();
 
     cy.wait('@basicSearchRequestSentFolder');
-
-    cy.contains('clickable url test').click();
+    mockSpeciaCharMessage.data.attributes.messageId = '2585370';
+    mockSpeciaCharMessage.data.attributes.body =
+      'clickable URL  https://www.va.gov/';
+    landingPage.loadMessageDetailsWithData(mockSpeciaCharMessage);
+    // cy.contains('clickable url test').click();
     cy.get('pre > a').click();
 
     cy.injectAxe();
