e4532dd05bd2024b54d7c3ba6e5629e893a28e77
bradbergeron-us <bradley.bergeron@accenturefederal.com>
2023-01-25 16:13:08 -0500

    fixes title wording to lowercase

--- src/applications/education-letters/tests/e2e/00-letters-all-fields.cypress.spec.js
+++ src/applications/education-letters/tests/e2e/00-letters-all-fields.cypress.spec.js
@@ -60,7 +60,7 @@ describe('All Field, texts and links should be validated on letters app', () => 
       'have.text',
       'Your VA education letter',
     );
-    cy.findByText('Your decision Letter').should('be.visible');
+    cy.findByText('Your decision letter').should('be.visible');
 
     cy.get('a[href*="/meb_api/v0/claim_letter"]').should('be.visible');
     cy.get('a[href*="/meb_api/v0/claim_letter"]').should(
