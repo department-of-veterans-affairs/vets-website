5044844f5814e6836b56c80a0b9dd72184b00959
Andrew Steele <andrew.steele@adhocteam.us>
2023-01-27 13:26:05 -0500

    Initial commit of the DS Playground app

---
+++ src/applications/ds-playground/app-entry.jsx
@@ -0,0 +1,16 @@
+import '@department-of-veterans-affairs/platform-polyfills';
+import './sass/ds-playground.scss';
+
+import startApp from '@department-of-veterans-affairs/platform-startup';
+
+import routes from './routes';
+import reducer from './reducers';
+import manifest from './manifest.json';
+
+startApp({
+  url: manifest.rootUrl,
+  reducer,
+  routes,
+  component: null,
+  analyticsEvents: [],
+});
