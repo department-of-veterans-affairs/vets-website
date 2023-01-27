34f1ea8686d7bb21de42891b63fac656b7705866
jerekshoe <jerek.shoemaker@adhocteam.us>
2023-01-27 13:49:41 -0600

    Verifying that date object is valid before passing to date-fns format method

--- src/applications/claims-status/components/ClosedClaimMessage.jsx
+++ src/applications/claims-status/components/ClosedClaimMessage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { format } from 'date-fns';
+import { format, isValid } from 'date-fns';
 import moment from 'moment';
 import { orderBy } from 'lodash';
 import PropTypes from 'prop-types';
@@ -77,7 +77,11 @@ const getCloseDate = claim => claim.attributes.phaseChangeDate;
 
 const getFileDate = claim => claim.attributes.dateFiled;
 
-const formatDate = date => format(new Date(date), 'MMMM d, yyyy');
+const formatDate = date => {
+  const dateObj = new Date(date);
+
+  return isValid(dateObj) ? format(dateObj, 'MMMM d, yyyy') : '';
+};
 
 export default function ClosedClaimMessage({ claims, onClose }) {
   const closedClaims = getRecentlyClosedClaims(claims);
