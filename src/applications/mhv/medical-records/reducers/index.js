cc3ba8efb4ba5b6d2ceda590b48fdcb6dae2b106
Kolby Balch <108146636+KolbyVA@users.noreply.github.com>
2023-01-30 16:50:17 -0600

    Mhv 41053 mr redux setup (#23237)
    * [#MHV-41053] initial setup of redux store for mhv/MR
    
    * [#MHV-41053] added all current fields to redux
    
    * [#MHV-41053] fixed stuff for pr
    
    * [#MHV-41053] Unit test changes
    
    * [#MHV-41053] Fixes the failing unit tests

--- src/applications/mhv/medical-records/reducers/index.js
+++ src/applications/mhv/medical-records/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
 import { breadcrumbsReducer } from './breadcrumbs';
+import { vaccineReducer } from './vaccine';
 
 const rootReducer = {
   mr: combineReducers({
     breadcrumbs: breadcrumbsReducer,
+    vaccines: vaccineReducer,
   }),
 };
 
