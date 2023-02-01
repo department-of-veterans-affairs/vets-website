fd9bfd35852890ab6a12e0f5781f06f8c53f14f1
Fernando Boza <17830527+FernandoBoza@users.noreply.github.com>
2023-02-01 15:30:05 -0500

    Rm and commented out LoginWidget spec test from main. (#23272)
    Co-authored-by: fer.boza <fernando.boza@accenturefederal.com>

--- src/applications/education-letters/tests/units/components/LoginWidget.unit.spec.js
+++ src/applications/education-letters/tests/units/components/LoginWidget.unit.spec.js
@@ -1,40 +1,43 @@
-import { expect } from 'chai';
-import React from 'react';
-import { mount } from 'enzyme';
-import { Provider } from 'react-redux';
-import configureMockStore from 'redux-mock-store';
-import LoginWidget from '../../../components/LoginWidget';
+// import { expect } from 'chai';
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import { Provider } from 'react-redux';
+// import configureMockStore from 'redux-mock-store';
+// import LoginWidget from '../../../components/LoginWidget';
 
-describe.skip('Render Sign up/ login UI', () => {
-  const mockStore = configureMockStore();
+// describe.skip('Render Sign up/ login UI', () => {
+//   const mockStore = configureMockStore();
+//   const initialState = {
+//     toggleLoginModal: () => {},
+//     user: {
+//       login: {
+//         currentlyLoggedIn: false,
+//         hasCheckedKeepAlive: true,
+//       },
+//     },
+//   };
+//
+//   it('should show h3 font sized header', () => {
+//     const store = mockStore(initialState);
+//     const wrapper = shallow(<LoginWidget {...initialState} />);
+//
+//     console.log(wrapper);
+//     expect(wrapper).to.exist;
+//
+//     wrapper.unmount();
+//   });
 
-  it('should show h3 font sized header', () => {
-    const store = mockStore({});
-    const wrapper = mount(
-      <Provider store={store}>
-        <LoginWidget />
-      </Provider>,
-    );
-
-    expect(wrapper.text()).to.include(
-      'Sign in to download your VA education decision letter',
-    );
-    expect(wrapper.text()).to.not.include('Login to download');
-
-    wrapper.unmount();
-  });
-
-  it('should show sign in button', () => {
-    const store = mockStore({});
-    const wrapper = mount(
-      <Provider store={store}>
-        <LoginWidget />
-      </Provider>,
-    );
-
-    expect(wrapper.text()).to.include('Sign in or create an account');
-    expect(wrapper.text()).to.not.include('Login or create an account');
-
-    wrapper.unmount();
-  });
-});
+// it('should show sign in button', () => {
+//   const store = mockStore({});
+//   const wrapper = mount(
+//     <Provider store={store}>
+//       <LoginWidget />
+//     </Provider>,
+//   );
+//
+//   expect(wrapper.text()).to.include('Sign in or create an account');
+//   expect(wrapper.text()).to.not.include('Login or create an account');
+//
+//   wrapper.unmount();
+// });
+// });
