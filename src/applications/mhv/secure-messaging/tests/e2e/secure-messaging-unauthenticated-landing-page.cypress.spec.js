07514297df0e623861e4f854edc754e33071f444
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2023-02-02 16:25:41 -0600

    Added Test Fixes
    Included axeCheck and authenticated user log in.

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-unauthenticated-landing-page.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-unauthenticated-landing-page.cypress.spec.js
@@ -3,8 +3,16 @@ import SecureMessagingSite from './sm_site/SecureMessagingSite';
 describe('Secure Messaging Compose', () => {
   it('can send message', () => {
     const site = new SecureMessagingSite();
-    site.login(false);
+    // site.login(false);
+    // site.loadPageUnauthenticated();
 
-    cy.url().should('contain', '/health-care/secure-messaging');
+    // cy.url().should('contain', '/health-care/secure-messaging');
+
+    site.login();
+
+    site.loadPage();
+
+    cy.injectAxe();
+    cy.axeCheck();
   });
 });
