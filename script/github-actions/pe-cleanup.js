c0167282e52e4a37af93e62a64aa0c63c0aa56da
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2023-02-03 16:45:14 -0500

    WIP

--- script/github-actions/pe-cleanup.js
+++ script/github-actions/pe-cleanup.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
+const core = require('@actions/core');
 
-/* eslint-disable no-console */
 const valuesFiles = fs
   .readdirSync('./manifests/apps/preview-environment/dev/environment-values/')
-  .filter(file => file.includes(process.env.DELETED_BRANCH));
+  .filter(file =>
+    file.includes(
+      `${process.env.CURRENT_REPOSITORY}-${process.env.DELETED_BRANCH}`,
+    ),
+  );
 
-console.log(valuesFiles);
+if (valuesFiles.length > 0) {
+  core.exportVariable('FILES_TO_DELETE', true);
+}
 
 valuesFiles.forEach(file => {
   try {
@@ -13,15 +19,7 @@ valuesFiles.forEach(file => {
       `./manifests/apps/preview-environment/dev/environment-values/${file}`,
     );
   } catch (error) {
+    /* eslint-disable no-console */
     console.log(error);
   }
 });
-const valuesFilesAfterDelete = fs.readdirSync(
-  './manifests/apps/preview-environment/dev/environment-values/',
-);
-
-console.log(
-  valuesFilesAfterDelete.filter(file =>
-    file.includes(process.env.DELETED_BRANCH),
-  ),
-);
