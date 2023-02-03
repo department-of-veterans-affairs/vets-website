a1a6003ad69f3325d5ce6268d38e15c1a652b48e
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2023-02-03 13:20:30 -0500

    WIP

--- script/github-actions/pe-cleanup.js
+++ script/github-actions/pe-cleanup.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 /* eslint-disable no-console */
 const valuesFiles = fs.readdirSync(
-  '.manifests/apps/preview-environment/dev/environment-values/',
+  './manifests/apps/preview-environment/dev/environment-values/',
 );
 
 console.log(valuesFiles);
