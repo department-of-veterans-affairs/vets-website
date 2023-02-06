4400828d2257d3dd88a8bee31cd63ce08820ae0a
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2023-02-06 14:50:15 -0500

    WIP

--- script/github-actions/pe-cleanup.js
+++ script/github-actions/pe-cleanup.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
 const core = require('@actions/core');
+/* eslint-disable no-console */
 
-const valuesFiles = fs
-  .readdirSync('./manifests/apps/preview-environment/dev/environment-values/')
-  .filter(file =>
-    file.includes(
-      `${process.env.CURRENT_REPOSITORY}-${process.env.DELETED_BRANCH}`,
-    ),
-  );
+const daysSinceUpdate = dateUpdated => {
+  const diff = new Date(dateUpdated) - new Date();
+  return diff / (1000 * 60 * 60 * 24);
+};
 
-if (valuesFiles.length > 0) {
+const deleteFiles = valuesFiles => {
   core.exportVariable('FILES_TO_DELETE', true);
   valuesFiles.forEach(file => {
     try {
@@ -21,6 +19,34 @@ if (valuesFiles.length > 0) {
       console.log(error);
     }
   });
-} else {
-  core.exportVariable('FILES_TO_DELETE', false);
+};
+
+if (process.env.TRIGGERING_EVENT === 'delete') {
+  const valuesFiles = fs
+    .readdirSync('./manifests/apps/preview-environment/dev/environment-values/')
+    .filter(file =>
+      file.includes(
+        `${process.env.CURRENT_REPOSITORY}-${process.env.DELETED_BRANCH}`,
+      ),
+    );
+
+  if (valuesFiles.length > 0) {
+    deleteFiles(valuesFiles);
+  } else {
+    core.exportVariable('FILES_TO_DELETE', false);
+  }
+}
+
+if (
+  process.env.TRIGGERING_EVENT === 'schedule' ||
+  process.env.TRIGGERING_EVENT === 'push'
+) {
+  const valuesFiles = fs
+    .readdirSync('./manifests/apps/preview-environment/dev/environment-values/')
+    .filter(file => daysSinceUpdate(file.last_updated) >= 7);
+  console.log(valuesFiles);
+  //   deleteFiles(valuesFiles);
+  // } else {
+  //   core.exportVariable('FILES_TO_DELETE', false);
+  // }
 }
