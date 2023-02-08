38f1240d02ad7fe5e8560e85ef35ebca699c2002
Daniel Cloud <daniel.cloud@adhocteam.us>
2023-02-08 17:37:00 -0500

    Rough out layout. Many things yet to do

--- src/applications/mhv/landing-page/containers/App.jsx
+++ src/applications/mhv/landing-page/containers/App.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const NavCard = ({ title, links }) => {
+  const listItems = links.map(l => (
+    <li key={l.href}>
+      <va-link active href={l.href} text={l.text} />
+    </li>
+  ));
+  return (
+    <div className="vads-u-height--full vads-u-padding-x--3 vads-u-padding-y--2 vads-u-background-color--gray-lightest">
+      <h2 className="vads-u-margin-top--1">{title}</h2>
+      <nav className="mhv-u-list-style--none">{listItems}</nav>
+    </div>
+  );
+};
 
 const App = () => {
+  const demoLinks = [
+    { href: '#fixme', text: 'A link' },
+    { href: '#fixme', text: 'A link, too' },
+    {
+      href: '#fixme',
+      text: 'A much longer link that might wrap to a new line',
+    },
+  ];
+
+  const demoLinks2 = demoLinks.concat([
+    { href: '#fixmetoo', text: 'Making a slightly longer list' },
+  ]);
   return (
     <>
       <va-breadcrumbs className="vads-u-font-family--sans no-wrap">
@@ -9,29 +36,51 @@ const App = () => {
       </va-breadcrumbs>
       <main>
         <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
-          <div className="vads-l-row vads-u-margin-x--neg2p5">
-            <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--4 large-screen:vads-l-col--3">
-              <div
-                className="vads-u-padding-x--2 vads-u-padding-y--7 vads-u-background-color--primary-alt-lightest"
-                style={{ height: '240px' }}
-              >
-                Usually this is the sidebar
-              </div>
+          <div className="vads-l-row vads-u-margin-bottom--3">
+            <div className="vads-l-col medium-screen:vads-l-col--6">
+              <h1>My Health</h1>
+              <p>
+                One place to manage your health care &#x2E3A; and your health
+              </p>
             </div>
-            <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--8 large-screen:vads-l-col--9">
-              <div
-                className="vads-u-padding-x--2 vads-u-padding-y--7 vads-u-background-color--primary-alt-lightest"
-                style={{ height: '240px' }}
-              >
-                <h1 className="vads-u-margin-bottom--1p5">MHV Landing page</h1>
-                Usually this is main content
-              </div>
+            <div className="vads-u-display--none medium-screen:vads-u-display--block vads-l-col--6">
+              <img
+                src="/img/my-health/logo/logo.png"
+                className="vagov-logo vads-u-max-width--100 vads-u-margin-bottom--4"
+                alt="My HealtheVet Logo"
+              />
+            </div>
+          </div>
+          <div className="vads-l-row vads-u-justify-content--space-between vads-u-margin-bottom--0 medium-screen:vads-u-margin-bottom--2">
+            <div className="vads-l-col--12 medium-screen:vads-l-col mhv-u-grid-gap vads-u-margin-bottom--2 medium-screen:vads-u-margin-bottom--0">
+              <NavCard title="A Section" links={demoLinks} />
+            </div>
+            <div className="vads-l-col--12 medium-screen:vads-l-col mhv-u-grid-gap vads-u-margin-bottom--2 medium-screen:vads-u-margin-bottom--0">
+              <NavCard title="Another Section" links={demoLinks2} />
+            </div>
+          </div>
+          <div className="vads-l-row vads-u-justify-content--space-between vads-u-margin-bottom--0 medium-screen:vads-u-margin-bottom--2">
+            <div className="vads-l-col--12 medium-screen:vads-l-col mhv-u-grid-gap vads-u-margin-bottom--2 medium-screen:vads-u-margin-bottom--0">
+              <NavCard title="A third Section" links={demoLinks2} />
+            </div>
+            <div className="vads-l-col--12 medium-screen:vads-l-col mhv-u-grid-gap vads-u-margin-bottom--2 medium-screen:vads-u-margin-bottom--0">
+              <NavCard
+                title="The Fourth Section that has a long title"
+                links={demoLinks}
+              />
             </div>
           </div>
         </div>
       </main>
     </>
   );
+};
+
+NavCard.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({ text: PropTypes.string, href: PropTypes.string }),
+  ),
+  title: PropTypes.string,
 };
 
 export default App;
