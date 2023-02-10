eadcd3d329f2950dca94a058883037fb69868e9c
Tze-chiu Lei <tclei2009@gmail.com>
2023-02-09 20:13:54 -0700

    Refactor 26-4555 form page 3

--- src/applications/simple-forms/26-4555/pages/contactInformation1.js
+++ src/applications/simple-forms/26-4555/pages/contactInformation1.js
@@ -1,15 +1,22 @@
-import * as address from 'platform/forms-system/src/js/definitions/address';
+import { intersection } from 'lodash';
 
+import * as address from 'platform/forms-system/src/js/definitions/address';
+import { veteranFields } from '../definitions/constants';
 import fullSchema from '../26-4555-schema.json';
 
+const { required } = fullSchema.properties[veteranFields.parentObject];
+const pageFields = [veteranFields.address];
 const contactInformation1 = {
   uiSchema: {
-    address: address.uiSchema('Mailing address'),
+    // can't use ('Mailing address', true) here to turn on line 3
+    // check if usAddress or profileAddress definitions work better for that
+    [veteranFields.address]: address.uiSchema('Mailing address'),
   },
   schema: {
     type: 'object',
+    required: intersection(required, pageFields),
     properties: {
-      address: address.schema(fullSchema, true),
+      [veteranFields.address]: address.schema(fullSchema, true),
     },
   },
 };
