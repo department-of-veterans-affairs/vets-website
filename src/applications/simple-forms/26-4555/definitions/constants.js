4a0dcc50f49fae0ddf9d5c3193c70524e17f683f
Tze-chiu Lei <tclei2009@gmail.com>
2023-02-09 19:05:52 -0700

    Add constants to 26-4555 form-app

---
+++ src/applications/simple-forms/26-4555/definitions/constants.js
@@ -0,0 +1,11 @@
+export const veteranFields = {
+  parentObject: 'veteran',
+  fullName: 'fullName',
+  dateOfBirth: 'dateOfBirth',
+  ssn: 'ssn',
+  vaFileNumber: 'vaFileNumber',
+  address: 'address',
+  homePhone: 'homePhone',
+  mobilePhone: 'mobilePhone',
+  email: 'email',
+};
