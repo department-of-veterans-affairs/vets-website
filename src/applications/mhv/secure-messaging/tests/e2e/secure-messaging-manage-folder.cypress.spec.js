c8568f435ee6172fc78492c027437dcf1870e7d2
Aaron Read <Aaron.Read@va.gov>
2023-02-13 15:52:51 -0600

    create with error

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-manage-folder.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-manage-folder.cypress.spec.js
@@ -12,6 +12,30 @@ describe('Secure Messaging Manage Folder AXE check', () => {
       .shadow()
       .find('[type="button"]')
       .click();
+    cy.get('[name="folder-name"]')
+      .shadow()
+      .find('[name="folder-name"]')
+      .type('create folder test');
+    cy.intercept('POST', '/my_health/v1/messaging/folder', {
+      statusCode: 400,
+      body: {
+        alertType: 'error',
+        header: 'err.title',
+        content: 'err.detail',
+        response: {
+          header: 'err.title',
+          content: 'err.detail',
+        },
+      },
+    }).as('folder');
+    cy.get('[text="Create"]')
+      .shadow()
+      .find('[type="button"]')
+      .click();
+    cy.get('[class="vads-u-margin-y--0"]').should(
+      'have.text',
+      'Folder could not be created. Try again later. If this problem persists, contact the help desk.',
+    );
     cy.injectAxe();
     cy.axeCheck();
   });
