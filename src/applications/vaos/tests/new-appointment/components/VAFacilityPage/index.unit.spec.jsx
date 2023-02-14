d4081ffd92090b55abe6c24a866de7f2ddc8fcee
Ryan Shaw <ryan.shaw@adhocteam.us>
2023-02-14 08:36:20 -0800

    47132 vaos va link/appointment list (#23128)
    * Linting fixes
    
    * Swap out Link for va-link
    
    * Updated tests
    
    * Skip v0 tests
    
    * Skip v0 tests that were breaking
    
    * Replace links
    
    * Replaced links
    
    * Fix links
    
    * Fix links
    
    * Fix new appointment link
    
    * Fix new appointment link
    
    * Fix proptypes
    
    * Temporarily disabled failing tests
    
    * Fix unit tests
    
    * Re-enabled skipped unit tests
    * Adjusted generated link for appointment detail page
    
    * Temporarily disable failing unit tests
    
    * Add userEvent import to unit tests
    
    * Fix unit test
    
    * Fix unit tests and links
    
    * Fix unit tests
    
    * Fix unit tests
    
    * Fix unit tests
    
    * Fix unit test
    
    * Temporarily skip breaking cypress tests
    
    * Change VaLink to va-link
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Fix and enable e2e tests
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Fix link issue
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Skips broken t3est
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Refactor to use shared service
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Fixes link
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    ---------
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/tests/new-appointment/components/VAFacilityPage/index.unit.spec.jsx
+++ src/applications/vaos/tests/new-appointment/components/VAFacilityPage/index.unit.spec.jsx
@@ -710,7 +710,7 @@ describe('VAOS <VAFacilityPage>', () => {
         .to.be.null;
     });
 
-    it.skip('should display correct facilities after changing type of care', async () => {
+    it('should display correct facilities after changing type of care', async () => {
       const facilityIdsForTwoTypesOfCare = ['983', '983GC', '983QA', '984'];
       mockParentSites(parentSiteIds, [parentSite983, parentSite984]);
       mockDirectBookingEligibilityCriteria(
