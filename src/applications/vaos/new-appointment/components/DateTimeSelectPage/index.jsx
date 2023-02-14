3525ebd9d018ed1103ddb2a7e0aff2a097872e41
Ryan Shaw <ryan.shaw@adhocteam.us>
2023-02-14 10:37:15 -0800

    Removes duplicate prop-types
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/new-appointment/components/DateTimeSelectPage/index.jsx
+++ src/applications/vaos/new-appointment/components/DateTimeSelectPage/index.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
 
-import PropTypes from 'prop-types';
 import InfoAlert from '../../../components/InfoAlert';
 
 import {
