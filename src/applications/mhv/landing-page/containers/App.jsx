2abfdf48352753c9846c3d452f8136ea37dc7e4c
Daniel Cloud <daniel.cloud@adhocteam.us>
2023-02-15 09:53:10 -0500

    Refactor to make files have fewer lines of code

--- src/applications/mhv/landing-page/containers/App.jsx
+++ src/applications/mhv/landing-page/containers/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import LandingNav from '../components/LandingNav';
+import CardLayout from '../components/CardLayout';
+import HeaderLayout from '../components/HeaderLayout';
 import HubLinks from '../components/HubLinks';
+import demodata from '../demodata.json';
 
 const App = () => {
   return (
     <div className="vads-u-margin-y--5">
-      <LandingNav />
+      <main>
+        <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
+          <HeaderLayout />
+          <CardLayout data={demodata.cards} />
+        </div>
+      </main>
       <HubLinks />
     </div>
   );
