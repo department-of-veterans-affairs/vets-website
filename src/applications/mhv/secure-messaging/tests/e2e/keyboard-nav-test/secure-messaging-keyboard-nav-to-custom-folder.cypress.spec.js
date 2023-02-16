6a8db13a316190bbac5b6f928caac905acbf147a
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2023-02-16 13:11:05 -0600

    updates to keyboard nav to custom folder
    Test fixes to keyboard nav to inbox

--- src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-custom-folder.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-custom-folder.cypress.spec.js
@@ -1,17 +1,17 @@
 import SecureMessagingSite from '../sm_site/SecureMessagingSite';
-import PatientKeywordSearchPage from '../pages/PatientKeywordSearchPage';
+import PatientInboxPage from '../pages/PatientInboxPage';
 
 describe('Secure Messaging access folder on landing page Keyboard Nav', () => {
   const site = new SecureMessagingSite();
-  const landingPage = new PatientKeywordSearchPage();
+  const landingPage = new PatientInboxPage();
   beforeEach(() => {
     site.login();
     landingPage.loadPage();
-  });
-  it('tab to inbox folder', () => {
-    cy.get('[data-testid="Inbox"] > a').realClick();
     cy.injectAxe();
     cy.axeCheck();
+  });
+  it('tab to inbox folder', () => {
+    cy.get('[data-testid="inbox-sidebar"]').realClick();
     cy.tabToElement('[data-testid="keyword-search-input"]').should('exist');
   });
 });
