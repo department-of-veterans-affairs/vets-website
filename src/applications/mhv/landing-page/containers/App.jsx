0cb9b83b66d6f7d9abce9c844214b6a70603eb75
Mark Dewey <mtdewey55@gmail.com>
2023-02-16 16:36:21 -0500

    (paused) PoC of app

--- src/applications/mhv/landing-page/containers/App.jsx
+++ src/applications/mhv/landing-page/containers/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+// import { useSelector } from 'react-redux';
 import CardLayout from '../components/CardLayout';
 import HeaderLayout from '../components/HeaderLayout';
 import HubLinks from '../components/HubLinks';
 import demodata from '../demodata.json';
 
+// import { isLandingPageEnabledForUser } from '../utilities/feature-toggles';
+
 const App = () => {
+  // const { featureToggles, user } = useSelector(state => state);
+  // const appEnabled = isLandingPageEnabledForUser(
+  //   featureToggles,
+  //   user?.profile?.signIn?.serviceName,
+  // );
+  // console.log({ appEnabled });
   return (
     <div className="vads-u-margin-y--5">
       <main>
