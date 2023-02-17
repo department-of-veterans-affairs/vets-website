2d90c0833b82d9512fe2fa375fc328f09d38c6ed
jerekshoe <jerek.shoemaker@adhocteam.us>
2023-02-17 13:54:20 -0600

    Removing extra cy.intercept

--- src/applications/claims-status/tests/e2e/page-objects/TrackClaimsPage.js
+++ src/applications/claims-status/tests/e2e/page-objects/TrackClaimsPage.js
@@ -25,10 +25,6 @@ class TrackClaimsPage {
     cy.intercept('GET', '/v0/evss_claims_async', claimsList);
     cy.login();
 
-    // START lighthouse_migration
-    cy.intercept('GET', '/v0/feature_toggles?*', featureToggleDisabled);
-    // END lighthouse_migration
-
     cy.visit('/track-claims');
     cy.title().should(
       'eq',
