c17066a9821e6de4da373812ae3b1a0c7cf30c95
John Woolschlager <wullaski@gmail.com>
2023-02-17 12:11:45 -0500

    Add specific error message for uuid not found

--- src/applications/check-in/day-of/pages/Error.jsx
+++ src/applications/check-in/day-of/pages/Error.jsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
 
 import { makeSelectError } from '../../selectors';
 import Wrapper from '../../components/layout/Wrapper';
@@ -10,22 +10,54 @@ const Error = () => {
   const selectError = useMemo(makeSelectError, []);
   const { error } = useSelector(selectError);
 
-  const message =
-    error === 'max-validation'
-      ? t('were-sorry-we-couldnt-match-your-information-please-ask-for-help')
-      : t(
-          'were-sorry-something-went-wrong-on-our-end-check-in-with-a-staff-member',
-        );
+  let messageText = '';
+  let alertType = '';
+  let header = '';
+
+  switch (error) {
+    case 'max-validation':
+      alertType = 'error';
+      header = t('we-couldnt-check-you-in');
+      messageText = t(
+        'were-sorry-we-couldnt-match-your-information-please-ask-for-help',
+      );
+      break;
+    case 'uuid-not-found':
+      // Shown when POST sessions returns 404.
+      alertType = 'info';
+      header = t('were-sorry-this-link-has-expired');
+      messageText = (
+        <Trans
+          i18nKey="trying-to-check-in-for-an-appointment--info-message"
+          components={[
+            <span key="bold" className="vads-u-font-weight--bold" />,
+            <va-telephone
+              key="53079"
+              data-testid="error-message-sms"
+              contact="53079"
+              sms
+            />,
+          ]}
+        />
+      );
+      break;
+    default:
+      alertType = 'info';
+      header = t('we-couldnt-check-you-in');
+      messageText = t(
+        'were-sorry-something-went-wrong-on-our-end-check-in-with-a-staff-member',
+      );
+  }
 
   return (
-    <Wrapper pageTitle={t('we-couldnt-check-you-in')}>
+    <Wrapper pageTitle={header}>
       <va-alert
         background-only
         show-icon
-        status={error === 'max-validation' ? 'error' : 'info'}
+        status={alertType}
         data-testid="error-message"
       >
-        <div>{message}</div>
+        <div>{messageText}</div>
       </va-alert>
     </Wrapper>
   );
