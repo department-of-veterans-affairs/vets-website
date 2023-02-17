49c6dd8d8680b85e0c87e8bc035a57e6046515aa
Mark Dewey <mtdewey55@gmail.com>
2023-02-17 10:25:39 -0500

    (feature) added logic around feature toggles enabled

--- src/applications/mhv/landing-page/containers/App.jsx
+++ src/applications/mhv/landing-page/containers/App.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
-import CardLayout from '../components/CardLayout';
-import HeaderLayout from '../components/HeaderLayout';
-import HubLinks from '../components/HubLinks';
-import demodata from '../demodata.json';
+import { useSelector } from 'react-redux';
+import { mhvUrl } from '@department-of-veterans-affairs/platform-site-wide/utilities';
+import LandingPage from '../components/LandingPage';
 
-// import { isLandingPageEnabledForUser } from '../utilities/feature-toggles';
+import { isLandingPageEnabledForUser } from '../utilities/feature-toggles';
 
 const App = () => {
-  // const { featureToggles, user } = useSelector(state => state);
-  // const appEnabled = isLandingPageEnabledForUser(
-  //   featureToggles,
-  //   user?.profile?.signIn?.serviceName,
-  // );
-  // console.log({ appEnabled });
-  return (
-    <div className="vads-u-margin-y--5">
-      <main>
-        <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
-          <HeaderLayout />
-          <CardLayout data={demodata.cards} />
-        </div>
-      </main>
-      <HubLinks />
-    </div>
+  const { featureToggles, user } = useSelector(state => state);
+  const appEnabled = isLandingPageEnabledForUser(
+    featureToggles,
+    user?.profile?.signIn?.serviceName,
   );
+  if (featureToggles.loading) return <va-loading-indicator />;
+  if (!appEnabled) {
+    const url = mhvUrl(true, 'home');
+    window.location.replace(url);
+  }
+  return <LandingPage />;
 };
 
 export default App;
