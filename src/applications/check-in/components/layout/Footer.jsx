280a5c8e00842ca12c8f9c1afd49e71134c64e5d
John Woolschlager <wullaski@gmail.com>
2023-02-20 17:22:48 -0500

    Remove comment

--- src/applications/check-in/components/layout/Footer.jsx
+++ src/applications/check-in/components/layout/Footer.jsx
@@ -45,9 +45,7 @@ const Footer = ({ router, isPreCheckIn }) => {
             {t('and-select-0-were-here-24-7')}
           </p>
           <p>
-            {t('if-you-have-hearing-loss-call')}{' '}
-            {/* Not using the va-telephone component due to issues with 711 link. To re-evaluate after component is fixed. */}
-            <va-telephone contact="711" />
+            {t('if-you-have-hearing-loss-call')} <va-telephone contact="711" />
           </p>
         </div>
       ) : (
