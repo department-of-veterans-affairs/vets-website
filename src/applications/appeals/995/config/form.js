af153057dc90ba795cee07d2f5ebcc2590f2bc6c
Robin Garrison <robin.garrison@adhocteam.us>
2023-02-21 08:57:41 -0600

    Update phone header text

--- src/applications/appeals/995/config/form.js
+++ src/applications/appeals/995/config/form.js
@@ -123,7 +123,7 @@ const formConfig = {
           schema: contactInfo.schema,
         },
         editHomePhone: {
-          title: 'Edit phone number',
+          title: 'Edit home phone number',
           path: 'edit-home-phone',
           CustomPage: EditHomePhone,
           CustomPageReview: EditHomePhone,
@@ -132,7 +132,7 @@ const formConfig = {
           schema: blankSchema,
         },
         editMobilePhone: {
-          title: 'Edit phone number',
+          title: 'Edit mobile phone number',
           path: 'edit-mobile-phone',
           CustomPage: EditMobilePhone,
           CustomPageReview: EditMobilePhone,
