6a687e568f547c5f5cd3b19ba9654238d73b9fc9
Tze-chiu Lei <tclei2009@gmail.com>
2023-02-21 10:18:21 -0700

    Revert "Delete previous version of 26-4555 form-app"
    This reverts commit df79e7491af55db138b4603f27eb29045347562a.

---
+++ src/applications/sah/sahg/chapters/previous-applications/specially-adapted-housing/previous-application.js
@@ -0,0 +1,8 @@
+export const title = '';
+
+export const schema = {
+  type: 'object',
+  properties: {},
+};
+
+export const uiSchema = {};
---
+++ src/applications/sah/sahg/helpers.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const directDepositWarning = (
+  <div className="pension-dd-warning">
+    The Department of Treasury requires all federal benefit payments be made by
+    electronic funds transfer (EFT), also called direct deposit. If you don’t
+    have a bank account, you must get your payment through Direct Express Debit
+    MasterCard. To request a Direct Express Debit MasterCard you must apply at{' '}
+    <a
+      href="http://www.usdirectexpress.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="www.usdirectexpress.com (opens in new window)"
+    >
+      www.usdirectexpress.com
+    </a>{' '}
+    or by telephone at <a href="tel:8003331795">1-800-333-1795</a>. If you chose
+    not to enroll, you must contact representatives handling waiver requests for
+    the Department of Treasury at
+    <a href="tel:8882242950">1-888-224-2950</a>. They will address any questions
+    or concerns you may have and encourage your participation in EFT.
+  </div>
+);
