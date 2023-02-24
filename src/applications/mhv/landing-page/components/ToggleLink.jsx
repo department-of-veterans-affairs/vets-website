c00f1382e4598bed893d79a8ed708d6c7edf5c2c
Daniel Cloud <daniel.cloud@adhocteam.us>
2023-02-24 16:29:57 -0500

    ToggleLink will respect feature flags

--- src/applications/mhv/landing-page/components/ToggleLink.jsx
+++ src/applications/mhv/landing-page/components/ToggleLink.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+
+const hasOwn = (object, prop) =>
+  Object.prototype.hasOwnProperty.call(object, prop);
 
 const ToggleLink = ({ link }) => {
-  const { text } = link;
-  // TODO: Incorporate logic for feature toggle check
-  const href = link.href ? link.href : link.oldHref;
+  const { featureToggles } = useSelector(state => state);
+  const { text, oldHref, href: newHref, toggle } = link;
+  let href = newHref || oldHref;
+  // If the link's toggle matches a feature toggle
+  // check if the toggle is on. If so, show new href. Otherwise show old href
+  if (hasOwn(featureToggles, toggle)) {
+    const showNewHref = featureToggles[toggle] === true;
+    href = showNewHref ? link.href : link.oldHref;
+  }
   return (
     <a className="mhv-c-navlink" href={href}>
       {text}
--- src/applications/mhv/landing-page/components/tests/ToggleLink.unit.spec.jsx
+++ src/applications/mhv/landing-page/components/tests/ToggleLink.unit.spec.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
+
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+
 import ToggleLink from '../ToggleLink';
+
+const generateInitialState = ({
+  mhvLinkOneEnabled = false,
+  mhvLinkTwoEnabled = false,
+}) => {
+  return {
+    featureToggles: {
+      // eslint-disable-next-line camelcase
+      mhv_link_one_enabled: mhvLinkOneEnabled,
+      // eslint-disable-next-line camelcase
+      mhv_link_two_enabled: mhvLinkTwoEnabled,
+    },
+  };
+};
 
 describe('ToggleLink', () => {
   it('renders the new href, when available', async () => {
@@ -11,7 +29,17 @@ describe('ToggleLink', () => {
       text: 'This is a link',
       toggle: '',
     };
-    const screen = render(<ToggleLink link={link} />);
+
+    const middleware = [];
+    const mockStore = configureStore(middleware);
+    const initialState = generateInitialState({ mhvLinkOneEnabled: false });
+    const store = mockStore(initialState);
+
+    const screen = render(
+      <Provider store={store}>
+        <ToggleLink link={link} />
+      </Provider>,
+    );
     const anchorLink = await screen.findByText('This is a link');
     expect(anchorLink).to.exist;
     expect(anchorLink.href.endsWith(link.href)).to.be.true;
@@ -24,13 +52,69 @@ describe('ToggleLink', () => {
       text: 'This is a link',
       toggle: '',
     };
-    const screen = render(<ToggleLink link={link} />);
+
+    const middleware = [];
+    const mockStore = configureStore(middleware);
+    const initialState = generateInitialState({ mhvLinkOneEnabled: false });
+    const store = mockStore(initialState);
+
+    const screen = render(
+      <Provider store={store}>
+        <ToggleLink link={link} />
+      </Provider>,
+    );
     const anchorLink = await screen.findByText('This is a link');
     expect(anchorLink).to.exist;
     expect(anchorLink.href.endsWith(link.oldHref)).to.be.true;
   });
 
   // TODO: Test feature toggle affecting which link is displayed
-  it('renders the old href if the feature toggle is off');
-  it('renders the new href if the feature toggle is on');
+  it('renders the old href if the feature toggle is off', async () => {
+    const link = {
+      href: '/new',
+      oldHref: '/bar',
+      text: 'This is a link',
+      toggle: 'mhv_link_one_enabled',
+    };
+
+    const middleware = [];
+    const mockStore = configureStore(middleware);
+    const initialState = generateInitialState({
+      mhvLinkOneEnabled: false,
+      mhvLinkTwoEnabled: true,
+    });
+    const store = mockStore(initialState);
+
+    const screen = render(
+      <Provider store={store}>
+        <ToggleLink link={link} />
+      </Provider>,
+    );
+    const anchorLink = await screen.findByText('This is a link');
+    expect(anchorLink).to.exist;
+    expect(anchorLink.href.endsWith(link.oldHref)).to.be.true;
+  });
+
+  it('renders the new href if the feature toggle is on', async () => {
+    const link = {
+      href: '/new',
+      oldHref: '/bar',
+      text: 'This is a link',
+      toggle: 'mhv_link_one_enabled',
+    };
+
+    const middleware = [];
+    const mockStore = configureStore(middleware);
+    const initialState = generateInitialState({ mhvLinkOneEnabled: true });
+    const store = mockStore(initialState);
+
+    const screen = render(
+      <Provider store={store}>
+        <ToggleLink link={link} />
+      </Provider>,
+    );
+    const anchorLink = await screen.findByText('This is a link');
+    expect(anchorLink).to.exist;
+    expect(anchorLink.href.endsWith(link.href)).to.be.true;
+  });
 });
