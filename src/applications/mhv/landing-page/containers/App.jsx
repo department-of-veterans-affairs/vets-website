6435298dcb62c757dfc4a00f9382f64d22fcf724
Mark Dewey <mdewey@users.noreply.github.com>
2023-02-28 10:13:23 -0500

    (feature) added Required Log in View (#23469)
    * (feature) added Required Log in View
    
    * (test) added unit test
    
    * (test) removed data-testid from tests.

--- src/applications/mhv/landing-page/containers/App.jsx
+++ src/applications/mhv/landing-page/containers/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { mhvUrl } from '@department-of-veterans-affairs/platform-site-wide/utilities';
+import backendServices from '@department-of-veterans-affairs/platform-user/profile/backendServices';
+import { RequiredLoginView } from '@department-of-veterans-affairs/platform-user/RequiredLoginView';
+
 import LandingPage from '../components/LandingPage';
 
 import { isLandingPageEnabledForUser } from '../utilities/feature-toggles';
@@ -11,15 +14,21 @@ const App = () => {
     featureToggles,
     user?.profile?.signIn?.serviceName,
   );
-
   if (featureToggles.loading || user.profile.loading)
-    return <va-loading-indicator data-testid="loading-indicator" />;
-  if (!appEnabled) {
+    return <va-loading-indicator />;
+  if (!appEnabled && user?.login?.currentlyLoggedIn) {
     const url = mhvUrl(true, 'home');
     window.location.replace(url);
     return <></>;
   }
-  return <LandingPage />;
+  return (
+    <RequiredLoginView
+      user={user}
+      serviceRequired={[backendServices.USER_PROFILE]}
+    >
+      <LandingPage />
+    </RequiredLoginView>
+  );
 };
 
 export default App;
