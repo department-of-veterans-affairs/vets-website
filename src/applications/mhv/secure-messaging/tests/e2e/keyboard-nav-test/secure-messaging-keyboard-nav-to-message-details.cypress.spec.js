39998d0156f9acb0e557f96125a4519e1f5030ef
haniasma <121971510+haniasma@users.noreply.github.com>
2023-02-28 16:30:10 -0500

    navigate to print button
    add some codes

--- src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-message-details.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-message-details.cypress.spec.js
@@ -1,32 +1,25 @@
+import SecureMessagingSite from '../sm_site/SecureMessagingSite';
 import PatientInboxPage from '../pages/PatientInboxPage';
 
-describe('Navigate to Message Details ', () => {
-  const landingPage = new PatientInboxPage();
+describe('Secure Messaging Keyboard Nav to Message Details', () => {
+  const site = new SecureMessagingSite();
   beforeEach(() => {
-    landingPage.login();
+    site.login();
+    site.loadPage();
+  });
+  it('Keyboard Nav from Welcome Page to Message Details', () => {
+    const landingPage = new PatientInboxPage();
+    cy.realPress(['Tab']);
+    cy.tabToElement('a[href*="/my-health/secure-messages/inbox"]');
+    cy.realPress(['Enter']);
     landingPage.loadPage();
-    landingPage.loadMessageDetails(
-      landingPage.getNewMessage().attributes.messageId,
-      landingPage.getNewMessage().attributes.subject,
-      landingPage.getNewMessage().attributes.sentDate,
-    );
-  });
-  it('navigate Print Button', () => {
-    landingPage.navigatePrintCancelButton();
+    const message = landingPage.getLoadedMessages().data.at(1);
+    landingPage.loadMessageDetailsByTabbingAndEnterKey(message);
+    cy.tabToElement('[class="usa-button-secondary"]');
+    cy.get('[data-testid="print-button"]').contains('Print');
+    cy.get('[data-testid="trash-button-text"]').contains('Trash');
+    cy.get('[data-testid="move-button-text"]');
     cy.injectAxe();
     cy.axeCheck();
-    cy.tabToElement('[class="usa-button-secondary"]').should('exist');
-  });
-  it('navigate Trash Button', () => {
-    landingPage.navigateTrash();
-    cy.injectAxe();
-    cy.axeCheck();
-    cy.tabToElement('[class="usa-button-secondary"]').should('exist');
-  });
-  it('navigate Reply Button', () => {
-    landingPage.navigateReply();
-    cy.injectAxe();
-    cy.axeCheck();
-    cy.tabToElement('[data-testid="message-body-field"]').should('exist');
   });
 });
