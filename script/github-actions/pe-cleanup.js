faedce0c7a1330805779bcadc99faf0e7bcd9555
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2023-03-02 16:52:28 -0500

    WIP

--- script/github-actions/pe-cleanup.js
+++ script/github-actions/pe-cleanup.js
@@ -1,59 +1,75 @@
 const fs = require('fs');
-const core = require('@actions/core');
+// const core = require('@actions/core');
 const yaml = require('js-yaml');
 
 /* eslint-disable no-console */
 
-const daysSinceUpdate = dateUpdated => {
-  const diff = new Date() - new Date(dateUpdated);
-  return diff / (1000 * 60 * 60 * 24);
-};
+// const daysSinceUpdate = dateUpdated => {
+//   const diff = new Date() - new Date(dateUpdated);
+//   return diff / (1000 * 60 * 60 * 24);
+// };
 
-const deleteFiles = valuesFiles => {
-  core.exportVariable('FILES_TO_DELETE', true);
-  valuesFiles.forEach(file => {
-    try {
-      fs.unlinkSync(
-        `./manifests/apps/preview-environment/dev/environment-values/${file}`,
-      );
-      console.log(`${file} removed`);
-    } catch (error) {
-      console.log(error);
-    }
-  });
-};
+// const deleteFiles = valuesFiles => {
+//   core.exportVariable('FILES_TO_DELETE', true);
+// const envFileContents = yaml.load(
+//   fs.readFileSync(
+//     './manifests/apps/preview-environment/dev/argocd-apps/values.yaml',
+//   ),
+// );
+// console.log(envFileContents);
+// valuesFiles.forEach(file => {
+//   if (file != 'template-values.yaml') {
+//     try {
+//       fs.unlinkSync(
+//         `./manifests/apps/preview-environment/dev/environment-values/${file}`,
+//       );
+//       console.log(`${file} removed`);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// });
+// };
 
 if (process.env.TRIGGERING_EVENT === 'delete') {
-  const valuesFiles = fs
-    .readdirSync('./manifests/apps/preview-environment/dev/environment-values/')
-    .filter(file =>
-      file.includes(
-        `${process.env.CURRENT_REPOSITORY}-${process.env.DELETED_BRANCH}`,
-      ),
-    );
-
-  if (valuesFiles.length > 0) {
-    deleteFiles(valuesFiles);
-  } else {
-    core.exportVariable('FILES_TO_DELETE', false);
-  }
+  // const valuesFiles = fs
+  //   .readdirSync('./manifests/apps/preview-environment/dev/environment-values/')
+  //   .filter(file =>
+  //     file.includes(
+  //       `${process.env.CURRENT_REPOSITORY}-${process.env.DELETED_BRANCH}`,
+  //     ),
+  //   );
+  // if (valuesFiles.length > 0) {
+  //   deleteFiles(valuesFiles);
+  // } else {
+  //   core.exportVariable('FILES_TO_DELETE', false);
+  // }
 }
 
-if (process.env.TRIGGERING_EVENT === 'schedule') {
-  const valuesFiles = fs
-    .readdirSync('./manifests/apps/preview-environment/dev/environment-values/')
-    .filter(file => {
-      const fileContents = yaml.load(
-        fs.readFileSync(
-          `./manifests/apps/preview-environment/dev/environment-values/${file}`,
-          'utf8',
-        ),
-      ).podAnnotations.last_updated;
-      return !fileContents || daysSinceUpdate(fileContents) >= 7;
-    });
-  if (valuesFiles.length > 0) {
-    deleteFiles(valuesFiles);
-  } else {
-    core.exportVariable('FILES_TO_DELETE', false);
-  }
+if (
+  process.env.TRIGGERING_EVENT === 'schedule' ||
+  process.env.TRIGGERING_EVENT === 'push'
+) {
+  const envFileContents = yaml.load(
+    fs.readFileSync(
+      './manifests/apps/preview-environment/dev/argocd-apps/values.yaml',
+    ),
+  );
+  console.log(envFileContents);
+  // const valuesFiles = fs
+  //   .readdirSync('./manifests/apps/preview-environment/dev/pe-env/')
+  //   .filter(file => {
+  //     const fileContents = yaml.load(
+  //       fs.readFileSync(
+  //         `./manifests/apps/preview-environment/dev/environment-values/${file}`,
+  //         'utf8',
+  //       ),
+  //     ).podAnnotations.last_updated;
+  //     return !fileContents || daysSinceUpdate(fileContents) >= 7;
+  //   });
+  // if (valuesFiles.length > 0) {
+  //   deleteFiles(valuesFiles);
+  // } else {
+  //   core.exportVariable('FILES_TO_DELETE', false);
+  // }
 }
