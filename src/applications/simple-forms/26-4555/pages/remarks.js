dad92d7289ed2d5c2cc5d354d2f026a856f36ec9
Robert Hasselle <robert.hasselle@oddball.io>
2023-03-02 09:15:58 -0600

    Add minimal and maximal cypress tests

--- src/applications/simple-forms/26-4555/pages/remarks.js
+++ src/applications/simple-forms/26-4555/pages/remarks.js
@@ -3,15 +3,19 @@ import React from 'react';
 export default {
   uiSchema: {
     remarks: {
-      'ui:title': ' ',
+      'ui:title': (
+        <p>
+          Please describe any service-connected conditions you may have due to
+          your military service. If you have a VA Decision Rating, please
+          include that as well.
+        </p>
+      ),
       'ui:description': (
         <>
-          <p>
-            Please describe any service-connected conditions you may have due to
-            your military service. If you have a VA Decision Rating, please
-            include that as well.
-          </p>
-          <va-additional-info trigger="Why do I need to provide this information?">
+          <va-additional-info
+            aria-label="Remarks"
+            trigger="Why do I need to provide this information?"
+          >
             <p>
               A service-connected condition is a disability related to an injury
               or disease that developed during or was aggravated while on active
