4cf820a38eaa39e21a9c488f5d15d88ed83e365d
Evan Ubiera <evan.ubiera@yum.com>
2023-03-03 15:57:52 -0500

    [#857] Starting floating chatbox implementation

--- src/applications/virtual-agent/components/page/Page.jsx
+++ src/applications/virtual-agent/components/page/Page.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { connect, useSelector } from 'react-redux';
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
-import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
+import FEATURE_FLAG_NAMES from '@department-of-veterans-affairs/platform-utilities/featureFlagNames';
 import Disclaimer from './Disclaimer';
-import Chatbox from '../chatbox/Chatbox';
+import ChatBox from '../chatbox/Chatbox';
+import FloatingChatBox from '../floating-chatbox/FloatingChatBox';
 
-export default function Page() {
+function Page() {
   const canShowFloatingChatbot = useSelector(
     state =>
       toggleValues(state)[FEATURE_FLAG_NAMES.virtualAgentFloatingChatbot],
   );
+
   return (
     <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
       <div className="vads-l-row vads-u-margin-x--neg2p5 vads-u-margin-y--4">
@@ -16,9 +19,23 @@ export default function Page() {
           <Disclaimer />
         </div>
         <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--7 small-desktop-screen:vads-l-col--5">
-          <Chatbox />
+          {`can show floating chatbox value:${canShowFloatingChatbot}`}
+          <ChatBox />
+        </div>
+        <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--7 small-desktop-screen:vads-l-col--5">
+          {canShowFloatingChatbot && <FloatingChatBox />}
         </div>
       </div>
     </div>
   );
 }
+
+function mapStateToProps(state) {
+  return {
+    canShowFloatingChatbot: FEATURE_FLAG_NAMES.virtualAgentFloatingChatbot(
+      state,
+    ),
+  };
+}
+
+export default connect(mapStateToProps)(Page);
