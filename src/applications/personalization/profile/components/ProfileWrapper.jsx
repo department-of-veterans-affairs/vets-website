0d54485cb77932b90ed6b21aaeb86fb45dbf4e94
Adam Whitlock <adam.whitlock@adhocteam.us>
2023-03-04 12:24:08 -0700

    test: fix unit, ui: update wrappers

--- src/applications/personalization/profile/components/ProfileWrapper.jsx
+++ src/applications/personalization/profile/components/ProfileWrapper.jsx
@@ -11,6 +11,7 @@ import ProfileSubNav from './ProfileSubNav';
 import ProfileMobileSubNav from './ProfileMobileSubNav';
 import { useFeatureToggle } from '../../hooks/useFeatureToggle';
 import { PROFILE_PATHS } from '../constants';
+import { EditWrapper } from './edit/EditWrapper';
 
 const getLayoutAndRoutes = (routes, useEditingPage, currentPathname) => {
   const filteredRoutes = [...routes].filter(
@@ -88,16 +89,7 @@ const ProfileWrapper = ({
         </>
       )}
 
-      {layout === 'edit' && (
-        <div className="vads-l-grid-container vads-u-padding-x--0">
-          <div className="vads-l-row">
-            <div className="vads-l-col--12 vads-u-padding-bottom--4 vads-u-padding-x--1 medium-screen:vads-l-col--9 medium-screen:vads-u-padding-x--2 medium-screen:vads-u-padding-bottom--6 small-desktop-screen:vads-l-col--8">
-              {/* children will be passed in from React Router one level up */}
-              {children}
-            </div>
-          </div>
-        </div>
-      )}
+      {layout === 'edit' && <EditWrapper>{children}</EditWrapper>}
     </>
   );
 };
