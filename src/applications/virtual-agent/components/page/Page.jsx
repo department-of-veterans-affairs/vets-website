c917deff4e978905f9c7cb151e32cf5dc0e7eaa8
Evan Ubiera <evan.ubiera@yum.com>
2023-03-06 12:56:06 -0500

    [#857] Importing feature name lists and rendering prototype chatbot skeletal work in place

--- src/applications/virtual-agent/components/page/Page.jsx
+++ src/applications/virtual-agent/components/page/Page.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { connect, useSelector } from 'react-redux';
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
-import FEATURE_FLAG_NAMES from '@department-of-veterans-affairs/platform-utilities/featureFlagNames';
+import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
 import Disclaimer from './Disclaimer';
 import ChatBox from '../chatbox/Chatbox';
 import FloatingChatBox from '../floating-chatbox/FloatingChatBox';
 
-function Page() {
-  const canShowFloatingChatbot = useSelector(
-    state =>
-      toggleValues(state)[FEATURE_FLAG_NAMES.virtualAgentFloatingChatbot],
-  );
+const virtualAgentShowFloatingChatbot = state =>
+  toggleValues(state)[FEATURE_FLAG_NAMES.virtualAgentShowFloatingChatbot];
 
+function Page() {
   return (
     <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
       <div className="vads-l-row vads-u-margin-x--neg2p5 vads-u-margin-y--4">
@@ -19,23 +17,18 @@ function Page() {
           <Disclaimer />
         </div>
         <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--7 small-desktop-screen:vads-l-col--5">
-          {`can show floating chatbox value:${canShowFloatingChatbot}`}
-          <ChatBox />
-        </div>
-        <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--7 small-desktop-screen:vads-l-col--5">
-          {canShowFloatingChatbot && <FloatingChatBox />}
+          {FEATURE_FLAG_NAMES.virtualAgentShowFloatingChatbot && (
+            <FloatingChatBox />
+          )}
+          {!FEATURE_FLAG_NAMES.virtualAgentShowFloatingChatbot && <ChatBox />}
         </div>
       </div>
     </div>
   );
 }
 
-function mapStateToProps(state) {
-  return {
-    canShowFloatingChatbot: FEATURE_FLAG_NAMES.virtualAgentFloatingChatbot(
-      state,
-    ),
-  };
-}
+const mapStateToProps = state => ({
+  virtualAgentShowFloatingChatbot: virtualAgentShowFloatingChatbot(state),
+});
 
 export default connect(mapStateToProps)(Page);
