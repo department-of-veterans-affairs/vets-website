b33f2816ce7201a41b8d9b1b212e2e3d5ecb5aa8
Evan Ubiera <evan.ubiera@yum.com>
2023-03-06 16:20:06 -0500

    [#857] Adding POC jump link

--- src/applications/virtual-agent/components/page/Page.jsx
+++ src/applications/virtual-agent/components/page/Page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect, useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
 import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
 import Disclaimer from './Disclaimer';
@@ -12,11 +12,25 @@ const virtualAgentShowFloatingChatbot = state =>
 function Page() {
   return (
     <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
+      {FEATURE_FLAG_NAMES.virtualAgentShowFloatingChatbot && (
+        <div>
+          <a
+            // className="show-on-focus"
+            href="#chatbot"
+            // onClick="focusContent(event)"
+          >
+            Go to Chatbot
+          </a>
+        </div>
+      )}
       <div className="vads-l-row vads-u-margin-x--neg2p5 vads-u-margin-y--4">
         <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--5 small-desktop-screen:vads-l-col--7">
           <Disclaimer />
         </div>
-        <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--7 small-desktop-screen:vads-l-col--5">
+        <div
+          id="chatbot"
+          className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--7 small-desktop-screen:vads-l-col--5"
+        >
           {FEATURE_FLAG_NAMES.virtualAgentShowFloatingChatbot && (
             <FloatingChatBox />
           )}
