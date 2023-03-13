953cf0ae0414e8fb3d52b6866b28931d102d435b
Andrew Rodiek <andrew.rodiek@gmail.com>
2023-03-13 10:22:06 -0400

    updating mini summary card to match new design

--- src/applications/financial-status-report/components/shared/MiniSummaryCard.jsx
+++ src/applications/financial-status-report/components/shared/MiniSummaryCard.jsx
@@ -20,44 +20,42 @@ export const MiniSummaryCard = ({
 }) => {
   return (
     <div
-      className="vads-u-border--1px vads-u-margin-y--2 vads-u-padding--0 vads-l-grid-container"
+      className="vads-u-border--1px vads-u-margin-y--2 vads-u-padding--0"
       data-testid="mini-summary-card"
     >
-      <div className="vads-l-row">
-        <div className="vads-u-padding--2 vads-l-col--9">
-          <h4 className="vads-u-margin-y--0 vads-u-font-size--h3">{heading}</h4>
-          {body}
-        </div>
-        <div className="medium-screen:vads-l-row small-desktop-screen:vads-l-col--3 summary-card-button-container">
-          <Link
-            aria-label={`Edit ${heading}`}
-            to={editDesination}
-            className="vads-u-padding--1"
+      <div className="vads-u-padding--2 vads-u-display--flex vads-u-flex-direction--column">
+        <h4 className="vads-u-margin-y--0 vads-u-font-size--h3">{heading}</h4>
+        {body}
+      </div>
+      <div className="vads-l-row vads-u-justify-content--space-between vads-u-align-items--center">
+        <Link
+          aria-label={`Edit ${heading}`}
+          to={editDesination}
+          className="vads-u-padding-y--1 vads-u-padding-x--2"
+        >
+          <span className="vads-u-font-size--h3">
+            <strong>Edit</strong>
+            <i
+              aria-hidden="true"
+              role="img"
+              className="fas fa-chevron-right vads-u-padding-left--0p5"
+            />
+          </span>
+        </Link>
+        {showDelete && (
+          <button
+            type="button"
+            aria-label={`Delete ${heading}`}
+            className="usa-button summary-card-delete-button vads-u-margin--1"
+            onClick={onDelete}
           >
-            <span className="vads-u-font-size--h3">
-              Edit
-              <i
-                aria-hidden="true"
-                role="img"
-                className="fas fa-chevron-right vads-u-padding-left--0p5"
-              />
-            </span>
-          </Link>
-          {showDelete && (
-            <button
-              type="button"
-              aria-label={`Delete ${heading}`}
-              className="usa-button summary-card-delete-button vads-u-margin--1"
-              onClick={onDelete}
-            >
-              <i
-                aria-hidden="true"
-                className="fas fa-trash-alt vads-u-padding-right--0p5"
-              />
-              <span>DELETE</span>
-            </button>
-          )}
-        </div>
+            <i
+              aria-hidden="true"
+              className="fas fa-trash-alt vads-u-padding-right--0p5"
+            />
+            <span>DELETE</span>
+          </button>
+        )}
       </div>
     </div>
   );
