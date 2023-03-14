a3a3181ca7d573a47bedc5562f428b609a4d9c9e
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2023-03-14 17:17:38 -0400

    [#MHV-42347] role button for voice control on Win

--- src/applications/mhv/secure-messaging/components/MessageThread/MessageThreadMeta.jsx
+++ src/applications/mhv/secure-messaging/components/MessageThread/MessageThreadMeta.jsx
@@ -8,7 +8,7 @@ const MessageThreadMeta = props => {
   const expandButton = () => {
     return (
       <div className="vads-u-flex--auto">
-        <div className="vads-u-font-weight--bold" tabIndex={-1}>
+        <div className="vads-u-font-weight--bold" role="button">
           {expanded ? (
             <>
               <span>close</span>
