0cae0a8ab000d3a3d65fdaa2f17859e7ae262b42
robertbylight <roberto.rodriguez@bylight.com>
2023-03-17 08:38:47 -0700

    [MHV 39023] created threadlist components and implemented sorting, having trouble trying to render the max page number.

---
+++ src/applications/mhv/secure-messaging/components/ThreadList/ThreadsList.jsx
@@ -0,0 +1,248 @@
+/*
+This component handles:
+- displaying a list of 10 threads per page
+- sorting messages
+
+Assumptions that may need to be addressed:
+- 
+
+Outstanding work:
+- individual message links go nowhere. Another component would need to be made 
+to display message details. Another react route would need to be set up to handle this view, 
+probably needing to accept a URL parameter
+*/
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { VaSelect } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import { useLocation } from 'react-router-dom';
+import recordEvent from 'platform/monitoring/record-event';
+import { useDispatch } from 'react-redux';
+import ThreadListItem from './ThreadListItem';
+import { getListOfThreads } from '../../actions/threads';
+// import { getThreadList } from '../../api/SmApi';
+
+const DESCENDING = 'DESC';
+const ASCENDING = 'ASC';
+const SORT_BY_SENDER = 'SENDER_NAME';
+const SORT_BY_RECEPIENT = 'RECIPIENT_NAME';
+const SORT_BY_SENT_DATE = 'SENT_DATE';
+const SORT_BY_DRAFT_DATE = 'DRAFT_DATE';
+const SENDER_ALPHA_ASCENDING = 'sender-alpha-asc';
+const SENDER_ALPHA_DESCENDING = 'sender-alpha-desc';
+const RECEPIENT_ALPHA_ASCENDING = 'recepient-alpha-asc';
+const RECEPIENT_ALPHA_DESCENDING = 'recepient-alpha-desc';
+
+// Arbitrarily set because the VaPagination component has a required prop for this.
+// This value dictates how many pages are displayed in a pagination component
+// const MAX_PAGE_LIST_LENGTH = 5;
+let sortOrderSelection;
+const ThreadsList = props => {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const { folder, threadList, keyword, folderId } = props;
+  const pageSize = 10;
+  const totalEntries = threadList?.length;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortOrder, setSortOrder] = useState(DESCENDING);
+  const [sortBy, setSortBy] = useState(SORT_BY_SENDER);
+  // const [totalThreads, setTotalThreads] = useState()
+
+  // const paginatedMessages = useRef([]);
+
+  // split messages into pages
+
+  // get display numbers
+  const fromToNums = (page, total) => {
+    const from = (page - 1) * pageSize + 1;
+    const to = Math.min(page * pageSize, total);
+
+    return [from, to];
+  };
+  // sort messages
+  const handleThreadSortSelection = sortOption => {
+    switch (sortOption) {
+      case ASCENDING:
+        setSortOrder(ASCENDING);
+        if (location.pathname === '/drafts') {
+          setSortBy(SORT_BY_DRAFT_DATE);
+          break;
+        }
+
+        setSortBy(SORT_BY_SENT_DATE);
+        break;
+      case DESCENDING:
+        setSortOrder(DESCENDING);
+        if (location.pathname === '/drafts') {
+          setSortBy(SORT_BY_DRAFT_DATE);
+          break;
+        }
+        setSortBy(SORT_BY_SENT_DATE);
+        break;
+      case SENDER_ALPHA_ASCENDING:
+        setSortOrder(ASCENDING);
+        setSortBy(SORT_BY_SENDER);
+        break;
+      case SENDER_ALPHA_DESCENDING:
+        setSortOrder(DESCENDING);
+        setSortBy(SORT_BY_SENDER);
+        break;
+      case RECEPIENT_ALPHA_ASCENDING:
+        setSortOrder(ASCENDING);
+        setSortBy(SORT_BY_RECEPIENT);
+        break;
+      case RECEPIENT_ALPHA_DESCENDING:
+        setSortOrder(DESCENDING);
+        setSortBy(SORT_BY_RECEPIENT);
+        break;
+      default:
+        setSortOrder(ASCENDING);
+        setSortBy(SORT_BY_SENT_DATE);
+    }
+  };
+
+  // run once on component mount to set initial message and page data
+  // useEffect(
+  //   () => {
+  //     if (threadlist?.length) {
+
+  //       setCurrentMessages(threadlist);
+  //     }
+  //   },
+  //   [currentPage, messages, paginateData, sortMessages],
+  // );
+
+  // update pagination values on...page change
+  // const onPageChange = page => {
+  //   setCurrentMessages(paginatedMessages.current[page - 1]);
+  //   setCurrentPage(page);
+  // };
+
+  // handle message sorting on sort button click
+  const handleMessageSort = () => {
+    dispatch(getListOfThreads(folderId, pageSize, 1, sortBy, sortOrder));
+    // paginatedMessages.current = paginateData(sortMessages(messages));
+    // setCurrentMessages(paginatedMessages.current[0]);
+    setCurrentPage(1);
+    setSortOrder(sortOrderSelection);
+  };
+
+  const handleOnSelect = e => {
+    sortOrderSelection = e.detail.value;
+    handleThreadSortSelection(e.detail.value);
+  };
+
+  //   async function getTotalThreads() {
+  //     let threadListTotal = 0;
+  //     let fetchingThreads = true;
+  //     let pageNumber=1;
+
+  //     while (fetchingThreads) {
+  //       const result = await getThreadList(folderId, 100, pageNumber)
+  //       threadListTotal += result.data.length
+  //       if (result.data.length < 100) {
+  //         fetchingThreads = false;
+  //       }
+  //       pageNumber+=1;
+  //   }
+  //   // console.log("threadlisttotal: ", threadListTotal)
+  //   setTotalThreads(threadListTotal)
+  //   return threadListTotal
+  // }
+
+  // const totalNumberOfThreads = useMemo(
+  //   () => {
+  //     const threadListTotal = getTotalThreads().result
+  //     return threadListTotal
+  //   },
+  //   [],
+  // );
+
+  // getTotalThreads()
+
+  const displayNums = fromToNums(currentPage, threadList?.length);
+
+  return (
+    <div className="message-list vads-l-row vads-u-flex-direction--column">
+      <div className="message-list-sort">
+        <VaSelect
+          id="sort-order-dropdown"
+          label="Sort by"
+          name="sort-order"
+          value={sortOrderSelection}
+          onVaSelect={e => {
+            handleOnSelect(e);
+          }}
+        >
+          <option value={DESCENDING}>Newest to oldest</option>
+          <option value={ASCENDING}>Oldest to newest</option>
+          {location.pathname !== '/sent' && location.pathname !== '/drafts' ? (
+            <>
+              <option value={SENDER_ALPHA_ASCENDING}>
+                A to Z - Sender’s name
+              </option>
+              <option value={SENDER_ALPHA_DESCENDING}>
+                Z to A - Sender’s name
+              </option>
+            </>
+          ) : (
+            <>
+              <option value={RECEPIENT_ALPHA_ASCENDING}>
+                A to Z - Recipient’s name
+              </option>
+              <option value={RECEPIENT_ALPHA_DESCENDING}>
+                Z to A - Recipient’s name
+              </option>
+            </>
+          )}
+        </VaSelect>
+
+        <va-button
+          type="button"
+          text="Sort"
+          label="Sort"
+          data-testid="sort-button"
+          onClick={() => {
+            handleMessageSort();
+            recordEvent({
+              event: 'cta-button-click',
+              'button-type': 'primary',
+              'button-click-label': 'Sort messages',
+            });
+          }}
+        />
+      </div>
+      <div className="vads-u-padding-y--1 vads-l-row vads-u-margin-top--2 vads-u-border-top--1px vads-u-border-bottom--1px vads-u-border-color--gray-light">
+        Displaying {displayNums[0]}
+        &#8211;
+        {displayNums[1]} of {totalEntries} conversations
+      </div>
+      {threadList?.length > 0 &&
+        threadList.map((thread, idx) => (
+          <ThreadListItem
+            key={`${thread.messageId}+${idx}`}
+            messageId={thread.messageId}
+            senderName={thread.senderName}
+            sentDate={thread.sentDate}
+            subject={thread.subject}
+            readReceipt={thread.readReceipt}
+            attachment={thread.attachment}
+            recipientName={thread.recipientName}
+            keyword={keyword}
+            category={thread.category}
+            activeFolder={folder}
+            triageGroupName={thread.triageGroupName}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default ThreadsList;
+
+ThreadsList.propTypes = {
+  folder: PropTypes.object,
+  isSearch: PropTypes.bool,
+  keyword: PropTypes.string,
+  threadList: PropTypes.array,
+};
---
+++ src/applications/mhv/secure-messaging/containers/FolderThreadListView.jsx
@@ -0,0 +1,198 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLocation, useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { DefaultFolders as Folders, Alerts } from '../util/constants';
+import useInterval from '../hooks/use-interval';
+import FolderHeader from '../components/MessageList/FolderHeader';
+import { clearFolder, retrieveFolder } from '../actions/folders';
+import AlertBackgroundBox from '../components/shared/AlertBackgroundBox';
+import { closeAlert } from '../actions/alerts';
+import ThreadsList from '../components/ThreadList/ThreadsList';
+import {
+  getListOfThreads,
+  clearListOfThreads,
+  getTotalListOfThreads,
+} from '../actions/threads';
+
+const FolderThreadListView = props => {
+  const { testing } = props;
+  const dispatch = useDispatch();
+  const [folderId, setFolderId] = useState(null);
+  const [totalThreadsCount, setTotalThreadsCount] = useState(null);
+
+  const error = null;
+  const threads = useSelector(state => state.sm.threads?.threadList);
+  const threadListTotalCount = useSelector(
+    state => state.sm.threas?.threadListTotalCount,
+  );
+  const folder = useSelector(state => state.sm.folders.folder);
+  const location = useLocation();
+  const params = useParams();
+
+  // console.log("totalThreadsCount: ",totalThreadsCount)
+
+  useEffect(
+    () => {
+      if (folderId !== null) {
+        dispatch(retrieveFolder(folderId)).then(() => {
+          dispatch(getListOfThreads(folderId, 10, 1, 'SENDER_NAME', 'ASC'));
+          dispatch(getTotalListOfThreads(folderId)).then(() => {
+            setTotalThreadsCount(threadListTotalCount);
+          });
+        });
+      }
+      // on component unmount, clear out threads reducer to prevent from
+      // previous threads results flashing when navigating between messages
+      return () => dispatch(clearListOfThreads());
+    },
+    [folderId, dispatch, threadListTotalCount],
+  );
+
+  useEffect(
+    () => {
+      if (folderId !== null) {
+        dispatch(retrieveFolder(folderId)).then(() => {
+          dispatch(getTotalListOfThreads(folderId));
+        });
+      }
+      // on component unmount, clear out threads reducer to prevent from
+      // previous threads results flashing when navigating between messages
+      return () => dispatch(clearListOfThreads());
+    },
+    [folderId, dispatch],
+  );
+
+  // useEffect(() => {
+  //   if(threadListTotalCount !== undefined) {
+  //     console.log("inside threadlistTotalCount")
+  //     setTotalThreadsCount(threadListTotalCount)
+  //   }
+  // },[threadListTotalCount])
+
+  useEffect(
+    () => {
+      // clear out folder reducer to prevent from previous folder data flashing
+      // when navigating between folders
+      if (!testing) dispatch(clearFolder());
+      if (location.pathname.includes('/folder')) {
+        setFolderId(params.folderId);
+      } else {
+        switch (location.pathname) {
+          case '/inbox':
+            setFolderId(Folders.INBOX.id);
+            break;
+          case '/sent':
+            setFolderId(Folders.SENT.id);
+            break;
+          case '/drafts':
+            setFolderId(Folders.DRAFTS.id);
+            break;
+          case '/trash':
+            setFolderId(Folders.DELETED.id);
+            break;
+          default:
+            break;
+        }
+      }
+    },
+    [dispatch, location.pathname, params.folderId],
+  );
+
+  // clear out alerts if user navigates away from this component
+  useEffect(
+    () => {
+      return () => {
+        if (location.pathname) {
+          dispatch(closeAlert());
+        }
+      };
+    },
+    [location.pathname, dispatch],
+  );
+
+  useInterval(() => {
+    if (folderId) {
+      dispatch(getListOfThreads(), true);
+    }
+  }, 60000);
+
+  const loadingIndicator = () => {
+    return (
+      <va-loading-indicator
+        message="Loading your secure messages..."
+        setFocus
+        data-testid="loading-indicator"
+      />
+    );
+  };
+
+  const content = () => {
+    if (threads === undefined) {
+      return loadingIndicator();
+    }
+    if (threads.length === 0) {
+      return (
+        <>
+          <div className="vads-u-padding-y--1p5 vads-l-row vads-u-margin-top--2 vads-u-border-top--1px vads-u-border-bottom--1px vads-u-border-color--gray-light">
+            Displaying 0 of 0 messages
+          </div>
+          <div className="vads-u-margin-top--3">
+            <va-alert
+              background-only="true"
+              status="info"
+              className="vads-u-margin-bottom--1 va-alert"
+              data-testid="alert-no-messages"
+            >
+              <p className="vads-u-margin-y--0">{Alerts.Message.NO_MESSAGES}</p>
+            </va-alert>
+          </div>
+        </>
+      );
+    }
+    if (error) {
+      return (
+        <va-alert status="error" visible>
+          <h2 slot="headline">We’re sorry. Something went wrong on our end</h2>
+          <p>
+            You can’t view your secure messages because something went wrong on
+            our end. Please check back soon.
+          </p>
+        </va-alert>
+      );
+    }
+    if (threads.length > 0) {
+      // console.log(threadListTotalCount)
+      return (
+        <ThreadsList
+          threadList={threads}
+          folder={folder}
+          folderId={folderId}
+          totalThreadscount={totalThreadsCount}
+        />
+      );
+    }
+    return '';
+  };
+
+  return (
+    <div className="vads-l-grid-container vads-u-padding--0">
+      <div className="main-content">
+        <AlertBackgroundBox closeable />
+        {folder?.folderId === undefined && loadingIndicator()}
+        {folder?.folderId !== undefined && (
+          <>
+            <FolderHeader folder={folder} />
+            {content()}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+FolderThreadListView.propTypes = {
+  testing: PropTypes.any,
+};
+
+export default FolderThreadListView;
