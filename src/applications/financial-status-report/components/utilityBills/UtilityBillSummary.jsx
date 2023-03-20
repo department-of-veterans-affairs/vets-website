d52fc6d001114817a879ff7ec8d9e4e8cea38ada
Andrew Rodiek <andrew.rodiek@gmail.com>
2023-03-20 12:27:09 -0400

    Added new utility bill pages

---
+++ src/applications/financial-status-report/components/utilityBills/UtilityBillSummary.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Link } from 'react-router';
+import PropTypes from 'prop-types';
+import FormNavButtons from 'platform/forms-system/src/js/components/FormNavButtons';
+import {
+  EmptyMiniSummaryCard,
+  MiniSummaryCard,
+} from '../shared/MiniSummaryCard';
+import { currency as currencyFormatter } from '../../utils/helpers';
+
+const UtilityBillSummary = ({
+  data,
+  goBack,
+  goToPath,
+  setFormData,
+  contentBeforeButtons,
+  contentAfterButtons,
+}) => {
+  const { utilityRecords = [] } = data;
+
+  const onDelete = deleteIndex => {
+    setFormData({
+      ...data,
+      utilityRecords: utilityRecords.filter(
+        (source, index) => index !== deleteIndex,
+      ),
+    });
+  };
+
+  const goForward = () => {
+    goToPath('/expenses-explainer');
+  };
+
+  const cardBody = text => (
+    <p className="vads-u-margin-y--2 vads-u-color--gray">{text}</p>
+  );
+  const emptyPrompt = `Select the ‘add additional utility bills‘ link to add another utility bill. Select the continue button to move on to the next question.`;
+
+  return (
+    <form>
+      <fieldset>
+        <legend
+          id="added-utility-bills-summary"
+          className="vads-u-font-family--serif"
+          name="addedUtilityBillsSummary"
+        >
+          You have added these utility bills
+        </legend>
+        <div className="vads-l-grid-container--full">
+          {!utilityRecords.length ? (
+            <EmptyMiniSummaryCard content={emptyPrompt} />
+          ) : (
+            utilityRecords.map((utility, index) => (
+              <MiniSummaryCard
+                body={cardBody(`Value: ${currencyFormatter(utility.amount)}`)}
+                editDesination={{
+                  pathname: '/add-utility-bill',
+                  search: `?index=${index}`,
+                }}
+                heading={utility.name}
+                key={utility.name + utility.amount}
+                onDelete={() => onDelete(index)}
+                showDelete
+              />
+            ))
+          )}
+          <Link
+            className="vads-c-action-link--green"
+            to={{
+              pathname: '/add-utility-bill',
+              search: `?index=${utilityRecords.length}`,
+            }}
+          >
+            Add additional utility bills
+          </Link>
+          {contentBeforeButtons}
+          <FormNavButtons
+            goBack={goBack}
+            goForward={goForward}
+            submitToContinue
+          />
+          {contentAfterButtons}
+        </div>
+      </fieldset>
+    </form>
+  );
+};
+
+UtilityBillSummary.propTypes = {
+  contentAfterButtons: PropTypes.object,
+  contentBeforeButtons: PropTypes.object,
+  data: PropTypes.shape({
+    utilityRecords: PropTypes.array,
+  }),
+  goBack: PropTypes.func,
+  goToPath: PropTypes.func,
+  setFormData: PropTypes.func,
+  testingIndex: PropTypes.number,
+  updatePage: PropTypes.func,
+  onReviewPage: PropTypes.bool,
+};
+
+export default UtilityBillSummary;
---
+++ src/applications/financial-status-report/pages/expenses/utilities/utilityBillsEnhanced.js
@@ -0,0 +1,55 @@
+import UtilityBillChecklist from '../../../components/utilityBills/UtilityBillChecklist';
+import UtilityBillInputList from '../../../components/utilityBills/UtilityBillInputList';
+import { validateCurrencyArray } from '../../../utils/validations';
+
+export const utilityBillChecklist = {
+  uiSchema: {
+    'ui:title': 'Your monthly utility bills',
+    utilityBills: {
+      'ui:title': 'Which of the following utilities do you pay for?',
+      'ui:widget': UtilityBillChecklist,
+      'ui:options': {
+        hideOnReview: true,
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      utilityBills: {
+        type: 'boolean',
+      },
+    },
+  },
+};
+
+export const utilityBillValues = {
+  uiSchema: {
+    'ui:title': '',
+    utilityBills: {
+      'ui:title': 'utilityBillValues',
+      'ui:field': UtilityBillInputList,
+      'ui:validations': [validateCurrencyArray],
+      items: {
+        name: {
+          'ui:title': 'Name of utility bill',
+        },
+        amount: {
+          'ui:title': 'Utility bill amount',
+        },
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      utilityBills: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {},
+        },
+      },
+    },
+  },
+};
