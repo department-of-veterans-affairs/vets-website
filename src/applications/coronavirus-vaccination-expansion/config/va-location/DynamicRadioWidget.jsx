8eafa95e7e57a5b47844406b2960122cb3520f49
Adam Whitlock <adam.whitlock@adhocteam.us>
2023-03-21 11:45:50 -0600

    Update DynamicRadioWidget.jsx

--- src/applications/coronavirus-vaccination-expansion/config/va-location/DynamicRadioWidget.jsx
+++ src/applications/coronavirus-vaccination-expansion/config/va-location/DynamicRadioWidget.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import environment from 'platform/utilities/environment';
+import LoadingIndicator from '@department-of-veterans-affairs/component-library/LoadingIndicator';
+import AlertBox from '@department-of-veterans-affairs/component-library/AlertBox';
 import {
   VaRadio,
   VaRadioOption,
@@ -11,18 +13,25 @@ import { apiRequest } from 'platform/utilities/api';
 const apiUrl = `${environment.API_URL}/covid_vaccine/v0/facilities/`;
 
 export function DynamicRadioWidget(props) {
+  // console.log(props);
   const { onChange } = props;
   let locationsList = null;
   let upperContent = null;
   const [locations, setLocations] = useState([]);
-  const [loading, isLoading] = useState(false); // app starts in a not loading state
+  const [loading, isLoading] = useState(true); // app starts in a loading state
   const [error, setError] = useState(false); // app starts with no error
-  const [selected, setSelected] = useState(null); // app starts with no selection
+  const [selected, setSelected] = useState(null); // app starts with no error
 
   const alertContent = (
     <>
-      We’re sorry. We’re having trouble finding medical centers for you to
-      choose from right now.
+      <p>
+        We're sorry. We're having trouble finding medical centers for you to
+        choose from right now.
+      </p>
+      <p>
+        We'll match you with the closest medical center based on the address you
+        provided. They’ll contact you when they have a vaccine for you.
+      </p>
     </>
   );
 
@@ -30,7 +39,6 @@ export function DynamicRadioWidget(props) {
     () => {
       // how sure are we that people will always enter 5 digits for their zipcode?
       if (props.zipcode && props.zipcode.length === 5) {
-        isLoading(true);
         setSelected(null);
         onChange();
         apiRequest(`${apiUrl}${props.zipcode}`, {})
@@ -50,20 +58,24 @@ export function DynamicRadioWidget(props) {
     [props.zipcode],
   );
 
-  if (props.zipcode === undefined) return null;
-
   if (loading === true) {
     locationsList = (
-      <va-loading-indicator message="Loading VA medical centers" />
+      <LoadingIndicator message="Loading VA medical centers near you..." />
     );
   } else if (locations.length > 0 && loading === false) {
     upperContent = (
       <>
-        These are the VA medical centers closest to the Zip code you entered.
-        Select the medical center you mainly go to for care.
+        <p>
+          These are the VA medical centers closest to where you live. Select the
+          medical center you'd like to go to get a COVID-19 vaccine.
+        </p>
+        <p>
+          <strong>Note</strong>: If you get a vaccine that requires 2 doses to
+          be fully effective, you'll need to return to the same VA medical
+          center to get your second dose.
+        </p>
       </>
     );
-
     locationsList = (
       <VaRadio
         label="Select your medical center"
@@ -91,10 +103,11 @@ export function DynamicRadioWidget(props) {
   ) {
     // there are no locations returned or there is an error
     locationsList = (
-      <va-alert status="info">
-        <h3 slot="headline">We can't share your closest medical centers</h3>
-        <p className="vads-u-font-size--base">{alertContent}</p>
-      </va-alert>
+      <AlertBox
+        content={alertContent}
+        headline="We can't share your closest medical centers"
+        status="info"
+      />
     );
   }
   return (
