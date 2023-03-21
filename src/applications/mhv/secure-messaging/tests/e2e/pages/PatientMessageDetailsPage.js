808f9de7f7c33bd1e13c109fc94b6f6d504a9b0e
Aaron Read <Aaron.Read@va.gov>
2023-03-21 16:23:18 -0500

    Update PatientMessageDetailsPage.js

--- src/applications/mhv/secure-messaging/tests/e2e/pages/PatientMessageDetailsPage.js
+++ src/applications/mhv/secure-messaging/tests/e2e/pages/PatientMessageDetailsPage.js
@@ -80,7 +80,11 @@ class PatientMessageDetailsPage {
       this.currentThread,
     ).as('full-thread');
 
-    cy.contains(mockParentMessageDetails.data.attributes.subject).click();
+    cy.contains(
+      `${mockParentMessageDetails.data.attributes.category}: ${
+        mockParentMessageDetails.data.attributes.subject
+      }`,
+    ).click();
     cy.wait('@message1');
   };
 
