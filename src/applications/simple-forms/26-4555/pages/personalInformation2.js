81eb2adfa4ab8925ade85585e50d42bf1e31b13e
Nick Sprinkle <ndsprinkle@gmail.com>
2023-03-23 11:03:14 -0400

    Add prefill messaging to appropriate pages

--- src/applications/simple-forms/26-4555/pages/personalInformation2.js
+++ src/applications/simple-forms/26-4555/pages/personalInformation2.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { intersection, pick } from 'lodash';
 
+import PrefillMessage from 'platform/forms/save-in-progress/PrefillMessage';
 import ssnUI from 'platform/forms-system/src/js/definitions/ssn';
 import fullSchema from 'vets-json-schema/dist/26-4555-schema.json';
 import { veteranFields } from '../definitions/constants';
@@ -12,12 +13,13 @@ const pageFields = [veteranFields.ssn, veteranFields.vaFileNumber];
 
 export default {
   uiSchema: {
-    'ui:title': (
-      <h3 className="vads-u-color--gray-dark vads-u-margin-y--0">
-        Identification information
-      </h3>
-    ),
+    'ui:description': PrefillMessage,
     [veteranFields.parentObject]: {
+      'ui:title': (
+        <h3 className="vads-u-color--gray-dark vads-u-margin-y--0">
+          Identification information
+        </h3>
+      ),
       [veteranFields.ssn]: ssnUI,
       [veteranFields.vaFileNumber]: {
         'ui:title': 'VA file number (if you have one)',
