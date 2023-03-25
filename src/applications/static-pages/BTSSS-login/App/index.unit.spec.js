dcc64615d74d0829bdcafa0bdc29e9cf3dbd6554
Daniel Sasser <daniel.sasser@agile6.com>
2023-03-24 19:26:47 -0700

    VACMS-12591: Updates unit test to use updated props.

--- src/applications/static-pages/BTSSS-login/App/index.unit.spec.js
+++ src/applications/static-pages/BTSSS-login/App/index.unit.spec.js
@@ -10,21 +10,25 @@ import UnauthContext from '../UnauthContext';
 
 describe('BTSSS Widget', () => {
   it('renders what we expect when unauthenticated', () => {
-    const wrapper = shallow(<App currentlyLoggedIn={false} BTSSSLoginWidget />);
+    const wrapper = shallow(
+      <App currentlyLoggedIn={false} showBtsssLoginWidget />,
+    );
     expect(wrapper.find(UnauthContext)).to.have.lengthOf(1);
     expect(wrapper.find(AuthContext)).to.have.lengthOf(0);
     wrapper.unmount();
   });
 
   it('renders what we expect when authenticated', () => {
-    const wrapper = shallow(<App currentlyLoggedIn BTSSSLoginWidget />);
+    const wrapper = shallow(<App currentlyLoggedIn showBtsssLoginWidget />);
     expect(wrapper.find(UnauthContext)).to.have.lengthOf(0);
     expect(wrapper.find(AuthContext)).to.have.lengthOf(1);
     wrapper.unmount();
   });
 
   it('Should not render either when feature flag is falsey', () => {
-    const wrapper = shallow(<App currentlyLoggedIn BTSSSLoginWidget={false} />);
+    const wrapper = shallow(
+      <App currentlyLoggedIn showBtsssLoginWidget={false} />,
+    );
     expect(wrapper.find(UnauthContext)).to.have.lengthOf(0);
     expect(wrapper.find(AuthContext)).to.have.lengthOf(0);
     wrapper.unmount();
