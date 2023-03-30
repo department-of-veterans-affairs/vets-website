43406cf0b891d3422e9ac04f1cc0700b5d0fd783
Daniel Cloud <daniel.cloud@adhocteam.us>
2023-03-30 10:45:21 -0400

    Revert "Remove unknown authenticity token, maybe unused by govdelivery"
    This reverts commit 5d3797de0b05bfb26f6e3715b1b42a9fde408c83.

--- src/applications/mhv/landing-page/components/NewsletterSignup.jsx
+++ src/applications/mhv/landing-page/components/NewsletterSignup.jsx
@@ -13,6 +13,11 @@ const NewsletterSignup = () => {
         <div className="vads-l-col medium-screen:vads-l-col--4">
           <form method="POST" action={mhvNewsletterURL}>
             <input type="hidden" value="¿" name="utf8" />
+            <input
+              type="hidden"
+              value="zrD8Ri9HkK5xMUmPd/64OtM13GRi0R9lmtOiprc1K4N/I6+cXT+0y+g1HmXpYPAt0ZT4lSgWuQk99qqiAsy/ZQ=="
+              name="authenticity_token"
+            />
             <input type="hidden" value="USVHA_60" name="topic_id" />
             <label htmlFor="email">Your email address</label>
             <input
