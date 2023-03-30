d9a0ae33d453cb88b409070209bf3ad4daada520
Aaron Read <Aaron.Read@va.gov>
2023-03-30 10:14:19 -0500

    Create secure-messaging-compose-error-message-keyboard-nav.cypress.spec.js

---
+++ src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-compose-error-message-keyboard-nav.cypress.spec.js
@@ -0,0 +1,26 @@
+import SecureMessagingSite from '../sm_site/SecureMessagingSite';
+import PatientInboxPage from '../pages/PatientInboxPage';
+import PatientComposePage from '../pages/PatientComposePage';
+
+describe('Secure Messaging Compose Errors Keyboard Nav', () => {
+  it('can send message', () => {
+    const landingPage = new PatientInboxPage();
+    const composePage = new PatientComposePage();
+    const site = new SecureMessagingSite();
+    site.login();
+    landingPage.loadInboxMessages();
+    cy.get('[data-testid="compose-message-link"]').click();
+    cy.injectAxe();
+    cy.axeCheck();
+    composePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
+    cy.get('[name="COVID"]').click();
+    cy.get('[data-testid="attach-file-input"]').selectFile(
+      'src/applications/mhv/secure-messaging/tests/e2e/fixtures/test_image.jpg',
+      { force: true },
+    );
+    composePage.getMessageSubjectField().type('Test Subject');
+    // composePage.getMessageBodyField().type('Test message body');
+    // composePage.sendMessage();
+    composePage.pushSendMessageWithKeyboardPress();
+  });
+});
