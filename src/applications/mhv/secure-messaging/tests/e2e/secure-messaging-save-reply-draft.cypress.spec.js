ed79a883d96bc392d9f16efa38b67331b1b6d343
Aaron Read <Aaron.Read@va.gov>
2023-04-03 13:08:08 -0500

    more fixes

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-save-reply-draft.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-save-reply-draft.cypress.spec.js
@@ -14,6 +14,8 @@ describe('Secure Messaging Reply', () => {
     const site = new SecureMessagingSite();
     site.login();
     const messageDetails = landingPage.getNewMessageDetails();
+    const messageDetailsBody = messageDetails.data.attributes.body;
+
     landingPage.loadInboxMessages(mockMessages, messageDetails);
     messageDetailsPage.loadMessageDetails(messageDetails);
     messageDetailsPage.loadReplyPageDetails(messageDetails);
@@ -23,20 +25,29 @@ describe('Secure Messaging Reply', () => {
     cy.injectAxe();
     cy.axeCheck();
 
-    replyPage.saveReplyDraft(
-      landingPage.getNewMessage().attributes.messageId,
-      landingPage.getNewMessage().attributes.senderId,
-      landingPage.getNewMessage().attributes.category,
-      landingPage.getNewMessage().attributes.subject,
-      testMessageBody,
+    replyPage.saveReplyDraft(messageDetails, testMessageBody);
+    cy.log(
+      `the message details after saveReplyDraft ${JSON.stringify(
+        messageDetails,
+      )}`,
     );
-
+    cy.log(
+      `the message details before assert${JSON.stringify(messageDetails)}`,
+    );
+    cy.log(`message details  Body${messageDetailsBody}`);
+    cy.log(
+      `messageDetails.data.attributes.body = ${
+        messageDetails.data.attributes.body
+      }`,
+    );
     messageDetailsPage.ReplyToMessageTO(messageDetails);
     messageDetailsPage.ReplyToMessagesenderName(messageDetails);
     messageDetailsPage.ReplyToMessagerecipientName(messageDetails);
     messageDetailsPage.ReplyToMessageDate(messageDetails);
     messageDetailsPage.ReplyToMessageId(messageDetails);
-    messageDetailsPage.ReplyToMessagebody(messageDetails);
+
+    messageDetails.data.attributes.body = messageDetailsBody;
+    messageDetailsPage.ReplyToMessagebody(messageDetailsBody);
 
     replyPage.sendReplyDraft(
       landingPage.getNewMessage().attributes.messageId,
