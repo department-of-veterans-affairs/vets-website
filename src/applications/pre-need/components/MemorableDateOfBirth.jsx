62fb6e6e58e60fc7bb2acefc41632b0cbb36767d
Connor <Fewin_Connor@bah.com>
2023-04-03 10:09:24 -0400

    MBMS-39191 trying to set data to redux

--- src/applications/pre-need/components/MemorableDateOfBirth.jsx
+++ src/applications/pre-need/components/MemorableDateOfBirth.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { VaMemorableDate } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import set from 'platform/utilities/data/set';
+import get from 'platform/utilities/data/get';
 import { selectMemorableDateOfBirth } from '../redux-selectors';
 
 const MemorableDateOfBirthField = props => {
-  const { showDateOfBirth } = props;
+  const { showDateOfBirth, formData } = props;
+
+  // console.log(formData);
+  const updateField = event => {
+    const claimant = set(
+      'application.claimant.dateOfBirth',
+      event.target.value,
+      formData,
+    );
+    // console.log(claimant);
+    return set('application.claimant', claimant, formData);
+  };
 
   if (showDateOfBirth) {
     return (
       <>
-        <VaMemorableDate />
+        <VaMemorableDate
+          value={get('application.claimant.dateOfBirth', formData)}
+          onDateBlur={updateField}
+          onDateChange={updateField}
+        />
       </>
     );
   }
@@ -19,6 +36,7 @@ const MemorableDateOfBirthField = props => {
 function mapStateToProps(state) {
   return {
     showDateOfBirth: selectMemorableDateOfBirth(state),
+    formData: state.form?.data,
   };
 }
 
