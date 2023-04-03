169277d0cb0781e7f272486baa3cf009f50eed0f
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2023-04-03 19:07:43 -0400

    Update formHelpers.js

--- src/applications/mhv/secure-messaging/util/formHelpers.js
+++ src/applications/mhv/secure-messaging/util/formHelpers.js
@@ -1,11 +1,13 @@
 import { focusElement } from '@department-of-veterans-affairs/platform-utilities/ui';
 
 export const focusOnErrorField = () => {
-  focusElement(
-    document
-      .querySelectorAll('[error]:not([error=""]')[0]
-      .shadowRoot.querySelector(
-        '[aria-describedby="error-message"], #error-message, #input-error-message',
-      ),
-  );
+  const errors = document.querySelectorAll('[error]:not([error=""]');
+  const firstError =
+    errors.length > 0 &&
+    errors[0]?.shadowRoot?.querySelector(
+      '[aria-describedby="error-message"], #error-message, #input-error-message',
+    );
+  if (firstError) {
+    focusElement(firstError);
+  }
 };
