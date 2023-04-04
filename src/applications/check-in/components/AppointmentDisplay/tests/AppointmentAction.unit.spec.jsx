8664eb795675d407331d470dbd8ce3fd59c1baa7
Brian Seek <brian.seek@civicactions.com>
2023-04-03 17:06:54 -0700

    First batch of updated unit tests.

--- src/applications/check-in/components/AppointmentDisplay/tests/AppointmentAction.unit.spec.jsx
+++ src/applications/check-in/components/AppointmentDisplay/tests/AppointmentAction.unit.spec.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { expect } from 'chai';
 import { render } from '@testing-library/react';
-import configureStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
-import { I18nextProvider } from 'react-i18next';
 import MockDate from 'mockdate';
 import AppointmentAction from '../AppointmentAction';
-import i18n from '../../../utils/i18n/i18n';
 
+import CheckInProvider from '../../../tests/unit/utils/CheckInProvider';
 import { ELIGIBILITY } from '../../../utils/appointment/eligibility';
 
 describe('check-in', () => {
@@ -16,34 +13,15 @@ describe('check-in', () => {
   });
 
   describe('AppointmentAction', () => {
-    let store;
-    beforeEach(() => {
-      const middleware = [];
-      const mockStore = configureStore(middleware);
-      const initState = {
-        checkInData: {
-          context: {
-            token: 'test-token',
-          },
-          form: {
-            pages: ['first-page', 'second-page', 'third-page', 'fourth-page'],
-          },
-        },
-      };
-      store = mockStore(initState);
-    });
-
     it('should render the check in button for ELIGIBLE appointments status', () => {
       const action = render(
-        <Provider store={store}>
-          <I18nextProvider i18n={i18n}>
-            <AppointmentAction
-              appointment={{
-                eligibility: ELIGIBILITY.ELIGIBLE,
-              }}
-            />
-          </I18nextProvider>
-        </Provider>,
+        <CheckInProvider>
+          <AppointmentAction
+            appointment={{
+              eligibility: ELIGIBILITY.ELIGIBLE,
+            }}
+          />
+        </CheckInProvider>,
       );
 
       expect(action.getByTestId('check-in-button')).to.exist;
@@ -55,16 +33,14 @@ describe('check-in', () => {
     it('should render the check in button for appointments with ELIGIBLE status that expire in more than 10 seconds', () => {
       MockDate.set('2018-01-01T12:14:49-04:00');
       const action = render(
-        <Provider store={store}>
-          <I18nextProvider i18n={i18n}>
-            <AppointmentAction
-              appointment={{
-                checkInWindowEnd: '2018-01-01T12:15:00-04:00',
-                eligibility: ELIGIBILITY.ELIGIBLE,
-              }}
-            />
-          </I18nextProvider>
-        </Provider>,
+        <CheckInProvider>
+          <AppointmentAction
+            appointment={{
+              checkInWindowEnd: '2018-01-01T12:15:00-04:00',
+              eligibility: ELIGIBILITY.ELIGIBLE,
+            }}
+          />
+        </CheckInProvider>,
       );
 
       expect(action.getByTestId('check-in-button')).to.exist;
@@ -75,16 +51,14 @@ describe('check-in', () => {
     it('should render the check in button for appointments with ELIGIBLE status in an earlier timezone', () => {
       MockDate.set('2018-01-01T13:25:00-04:00');
       const action = render(
-        <Provider store={store}>
-          <I18nextProvider i18n={i18n}>
-            <AppointmentAction
-              appointment={{
-                checkInWindowEnd: '2018-01-01T12:45:00.106-05:00',
-                eligibility: ELIGIBILITY.ELIGIBLE,
-              }}
-            />
-          </I18nextProvider>
-        </Provider>,
+        <CheckInProvider>
+          <AppointmentAction
+            appointment={{
+              checkInWindowEnd: '2018-01-01T12:45:00.106-05:00',
+              eligibility: ELIGIBILITY.ELIGIBLE,
+            }}
+          />
+        </CheckInProvider>,
       );
 
       expect(action.getByTestId('check-in-button')).to.exist;
