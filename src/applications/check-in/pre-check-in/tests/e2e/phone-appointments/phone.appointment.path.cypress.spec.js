4cb16cf155c44f8a89aae7b7a72b7ad3dcb22b7c
John Woolschlager <wullaski@gmail.com>
2023-04-04 12:37:02 -0400

    Remove skip from cypress test

--- src/applications/check-in/pre-check-in/tests/e2e/phone-appointments/phone.appointment.path.cypress.spec.js
+++ src/applications/check-in/pre-check-in/tests/e2e/phone-appointments/phone.appointment.path.cypress.spec.js
@@ -35,7 +35,7 @@ describe('Pre-Check In Experience ', () => {
       window.sessionStorage.clear();
     });
   });
-  it.skip('Happy Path yes to demographics', () => {
+  it('Happy Path yes to demographics', () => {
     cy.visitPreCheckInWithUUID('258d753c-262a-4ab2-b618-64b645884daf');
     // page: Validate
     ValidateVeteran.validatePage.preCheckIn();
