d3bac5634648b49e616d431f692adb1dac9fc871
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2023-04-04 14:30:54 -0400

    MHV-42674, 42130 Interstitial page flow and Patient Safety Message (#23765)
    * [#MHV-42130] patient safety note on compose and reply
    
    * [#MHV-42674] Interstitial page flow on compose new message, on continuing to edit compose or reply draft, on new reply, on thread details
    
    * crisis line modal focus on interstitial page
    
    * interstitial page - button dynamic wording on compose,  draft and reply
    
    * [MHV-43155] focus on h1 Interstitial page
    
    ---------
    
    Co-authored-by: Aaron Read <Aaron.Read@va.gov>

--- src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-compose-keyboard-nav-send.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-compose-keyboard-nav-send.cypress.spec.js
@@ -9,7 +9,7 @@ describe('Secure Messaging Compose', () => {
     const site = new SecureMessagingSite();
     site.login();
     landingPage.loadInboxMessages();
-    cy.get('[data-testid="compose-message-link"]').click();
+    landingPage.loadComposeMessagePage();
     cy.injectAxe();
     cy.axeCheck();
     composePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
