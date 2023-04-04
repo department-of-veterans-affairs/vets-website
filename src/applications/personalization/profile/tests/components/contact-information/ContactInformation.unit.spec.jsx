f1ca1415ad113d279542a3f213a30af0c3116a0a
Adam Whitlock <adam.whitlock627@gmail.com>
2023-04-04 12:26:40 -0600

    test: update contact info

--- src/applications/personalization/profile/tests/components/contact-information/ContactInformation.unit.spec.jsx
+++ src/applications/personalization/profile/tests/components/contact-information/ContactInformation.unit.spec.jsx
@@ -8,8 +8,63 @@ import ContactInformation from '@@profile/components/contact-information/Contact
 
 import {
   createBasicInitialState,
+  createFeatureTogglesState,
   renderWithProfileReducers,
 } from '../../unit-test-helpers';
+import { Toggler } from '~/platform/utilities/feature-toggles';
+
+const testContactInfo = toggleValue => {
+  const ui = (
+    <MemoryRouter>
+      <ContactInformation />
+    </MemoryRouter>
+  );
+  const initialState = {
+    ...createBasicInitialState(),
+    ...createFeatureTogglesState({
+      [Toggler.TOGGLE_NAMES.profileUseInfoCard]: toggleValue || false,
+    }),
+  };
+  initialState.user.profile.vapContactInfo.email.emailAddress =
+    'alongusername@gmail.com';
+
+  const {
+    residentialAddress,
+    mailingAddress,
+  } = initialState.user.profile.vapContactInfo;
+
+  const view = renderWithProfileReducers(ui, { initialState });
+
+  expect(view.getByText(residentialAddress.addressLine1, { exact: false })).to
+    .exist;
+  expect(
+    view.getByText(
+      `${residentialAddress.city}, ${residentialAddress.stateCode} ${
+        residentialAddress.zipCode
+      }`,
+      { exact: false },
+    ),
+  ).to.exist;
+
+  expect(view.getByText(mailingAddress.addressLine1, { exact: false })).to
+    .exist;
+  expect(
+    view.getByText(
+      `${mailingAddress.city}, ${mailingAddress.stateCode} ${
+        mailingAddress.zipCode
+      }`,
+      { exact: false },
+    ),
+  ).to.exist;
+
+  // It's too cumbersome to convert the raw phone number data into what is
+  // displayed so I'm using strings here.
+  expect(view.getByText('555-555-5559', { exact: false })).to.exist;
+  expect(view.getByText('804-205-5544, ext. 17747')).to.exist;
+  expect(view.getByText('214-718-2112', { exact: false })).to.exist;
+
+  expect(view.getByText(/alongusername/)).to.exist;
+};
 
 describe('ContactInformation', () => {
   let server;
@@ -24,54 +79,16 @@ describe('ContactInformation', () => {
     server.close();
   });
 
-  const ui = (
-    <MemoryRouter>
-      <ContactInformation />
-    </MemoryRouter>
+  context(
+    'correct contact info based on what exists in the Redux state',
+    () => {
+      it('renders when profileUseInfoCard toggle is off', () => {
+        testContactInfo(false);
+      });
+
+      it('renders when profileUseInfoCard toggle is on', () => {
+        testContactInfo(true);
+      });
+    },
   );
-
-  let initialState;
-
-  it('should render the correct contact based on what exists in the Redux state', () => {
-    initialState = createBasicInitialState();
-    initialState.user.profile.vapContactInfo.email.emailAddress =
-      'alongusername@gmail.com';
-
-    const {
-      residentialAddress,
-      mailingAddress,
-    } = initialState.user.profile.vapContactInfo;
-
-    const view = renderWithProfileReducers(ui, { initialState });
-
-    expect(view.getByText(residentialAddress.addressLine1, { exact: false })).to
-      .exist;
-    expect(
-      view.getByText(
-        `${residentialAddress.city}, ${residentialAddress.stateCode} ${
-          residentialAddress.zipCode
-        }`,
-        { exact: false },
-      ),
-    ).to.exist;
-
-    expect(view.getByText(mailingAddress.addressLine1, { exact: false })).to
-      .exist;
-    expect(
-      view.getByText(
-        `${mailingAddress.city}, ${mailingAddress.stateCode} ${
-          mailingAddress.zipCode
-        }`,
-        { exact: false },
-      ),
-    ).to.exist;
-
-    // It's too cumbersome to convert the raw phone number data into what is
-    // displayed so I'm using strings here.
-    expect(view.getByText('555-555-5559', { exact: false })).to.exist;
-    expect(view.getByText('804-205-5544, ext. 17747')).to.exist;
-    expect(view.getByText('214-718-2112', { exact: false })).to.exist;
-
-    expect(view.getByText(/alongusername/)).to.exist;
-  });
 });
