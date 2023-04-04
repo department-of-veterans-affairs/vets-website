22e5137dc609ee4375ceb51183fe7bb8ec63203b
Nick Sprinkle <ndsprinkle@gmail.com>
2023-04-04 15:08:31 -0400

    Update web component field directory name, component field names, and ui: property name

--- src/platform/forms-system/src/js/web-component-fields/VaTextInputField.jsx
+++ src/platform/forms-system/src/js/web-component-fields/VaTextInputField.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { VaTextInput } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 
-export default function FormFieldVaTextInput(props) {
+export default function VaTextInputField(props) {
   const {
     textDescription,
     showLabel,
