1831ba671ac3944dc0c5356835d6cb5d0ae37e5a
Evan Ubiera <evan.ubiera@yum.com>
2023-04-05 17:13:24 -0400

    [#857] Testing review instance and toggles

--- src/applications/virtual-agent/components/page/Page.jsx
+++ src/applications/virtual-agent/components/page/Page.jsx
@@ -21,20 +21,20 @@ const showBot = botPosition => {
   }
 };
 
-// const renderChatBox = () => {
-//   return (
-//     <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
-//       <div className="vads-l-row vads-u-margin-x--neg2p5 vads-u-margin-y--4">
-//         <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--5 small-desktop-screen:vads-l-col--7">
-//           <Disclaimer />
-//         </div>
-//         <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--7 small-desktop-screen:vads-l-col--5">
-//           <ChatBox />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const renderChatBox = () => {
+  return (
+    <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
+      <div className="vads-l-row vads-u-margin-x--neg2p5 vads-u-margin-y--4">
+        <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--5 small-desktop-screen:vads-l-col--7">
+          <Disclaimer />
+        </div>
+        <div className="vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--7 small-desktop-screen:vads-l-col--5">
+          <ChatBox />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const renderStickyBot = () => {
   return (
@@ -99,34 +99,34 @@ const renderStickyBot = () => {
 };
 
 function Page({ virtualAgentShowFloatingChatbot = null }) {
-  // const [chosenBot, setChosenBot] = useState(null);
+  const [chosenBot, setChosenBot] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  // const bot = '';
+  let bot = '';
 
   useEffect(
     () => {
       setIsLoading(false);
-      // if (virtualAgentShowFloatingChatbot) {
-      //   setChosenBot('sticky');
-      // } else {
-      //   setChosenBot('default');
-      // }
+      if (virtualAgentShowFloatingChatbot) {
+        setChosenBot('sticky');
+      } else {
+        setChosenBot('default');
+      }
     },
     [virtualAgentShowFloatingChatbot],
   );
 
-  // if (chosenBot === 'sticky') {
-  //   bot =
-  // } else if (chosenBot === 'default') {
-  //   bot = renderChatBox();
-  // } else {
-  //   bot = '';
-  // }
+  if (chosenBot === 'sticky') {
+    bot = renderStickyBot();
+  } else if (chosenBot === 'default') {
+    bot = renderChatBox();
+  } else {
+    bot = '';
+  }
 
   if (isLoading) {
     return <LoadingIndicator />;
   }
-  return renderStickyBot();
+  return bot;
 }
 
 Page.propTypes = {
