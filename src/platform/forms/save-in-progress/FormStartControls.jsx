440b19078581556996b8c1b90313dd1d1acc8473
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-04-06 15:53:51 -0500

    Remove chevron

--- src/platform/forms/save-in-progress/FormStartControls.jsx
+++ src/platform/forms/save-in-progress/FormStartControls.jsx
@@ -151,7 +151,7 @@ class FormStartControls extends React.Component {
       <div>
         <VaButton
           onClick={this.handleLoadPrefill}
-          text={`${startText} »`}
+          text={startText}
           label={ariaLabel}
         />
       </div>
