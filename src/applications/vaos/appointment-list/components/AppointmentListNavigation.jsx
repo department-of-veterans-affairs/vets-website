dc537b8b8c2dc71bfdd679bc4adfbb1c105c16c2
Tony Williams <tony.williams@va.gov>
2023-04-06 13:57:13 -0500

    convert navigation buttons to links - 508

--- src/applications/vaos/appointment-list/components/AppointmentListNavigation.jsx
+++ src/applications/vaos/appointment-list/components/AppointmentListNavigation.jsx
@@ -1,38 +1,12 @@
 import React from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useLocation, NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import recordEvent from 'platform/monitoring/record-event';
 import { selectFeatureStatusImprovement } from '../../redux/selectors';
 import { GA_PREFIX } from '../../utils/constants';
 
-function handleClick({ history, callback }) {
-  return event => {
-    if (event.target.id === 'upcoming') {
-      history.push('/');
-      callback(true);
-    }
-
-    if (event.target.id === 'pending') {
-      history.push('/pending');
-      callback(true);
-      recordEvent({
-        event: `${GA_PREFIX}-status-pending-link-clicked`,
-      });
-    }
-
-    if (event.target.id === 'past') {
-      recordEvent({
-        event: `${GA_PREFIX}-status-past-link-clicked`,
-      });
-      history.push('/past');
-      callback(true);
-    }
-  };
-}
-
 export default function AppointmentListNavigation({ count, callback }) {
-  const history = useHistory();
   const location = useLocation();
   const featureStatusImprovement = useSelector(state =>
     selectFeatureStatusImprovement(state),
@@ -46,55 +20,53 @@ export default function AppointmentListNavigation({ count, callback }) {
       >
         <ul>
           <li>
-            <button
+            <NavLink
               id="upcoming"
-              type="button"
-              className="va-button-link"
-              onClick={handleClick({
-                history,
-                callback,
-              })}
+              to="/"
+              onClick={() => callback(true)}
               // eslint-disable-next-line jsx-a11y/aria-proptypes
               aria-current={`${
-                location.pathname.endsWith('/') ? 'page' : null
+                location.pathname.endsWith('/') ? 'true' : false
               }`}
             >
               Upcoming
-            </button>
+            </NavLink>
           </li>
           <li>
-            <button
+            <NavLink
               id="pending"
-              type="button"
-              className="va-button-link"
-              onClick={handleClick({
-                history,
-                callback,
-              })}
+              to="/pending"
+              onClick={() => {
+                callback(true);
+                recordEvent({
+                  event: `${GA_PREFIX}-status-pending-link-clicked`,
+                });
+              }}
               // eslint-disable-next-line jsx-a11y/aria-proptypes
               aria-current={`${
-                location.pathname.endsWith('pending') ? 'page' : null
+                location.pathname.endsWith('pending') ? 'true' : false
               }`}
             >
               {`Pending (${count})`}
-            </button>
+            </NavLink>
           </li>
           <li>
-            <button
+            <NavLink
               id="past"
-              type="button"
-              className="va-button-link"
-              onClick={handleClick({
-                history,
-                callback,
-              })}
+              to="/past"
+              onClick={() => {
+                callback(true);
+                recordEvent({
+                  event: `${GA_PREFIX}-status-past-link-clicked`,
+                });
+              }}
               // eslint-disable-next-line jsx-a11y/aria-proptypes
               aria-current={`${
-                location.pathname.endsWith('past') ? 'page' : null
+                location.pathname.endsWith('past') ? 'true' : false
               }`}
             >
               Past
-            </button>
+            </NavLink>
           </li>
         </ul>
       </nav>
