9e1b5977b5982b58f0e2e55110f39e919bc8a6c4
Robert Hasselle <robert.hasselle@oddball.io>
2023-04-07 10:49:44 -0500

    update text input consumer pages

--- src/applications/simple-forms/mock-simple-forms-patterns/config/form.js
+++ src/applications/simple-forms/mock-simple-forms-patterns/config/form.js
@@ -5,10 +5,12 @@ import ConfirmationPage from '../containers/ConfirmationPage';
 
 // pages
 import textInput from '../pages/textInput';
-import checkbox from '../pages/checkbox';
-import radio from '../pages/radio';
-import select from '../pages/select';
-import date from '../pages/date';
+import textInputWidgets1 from '../pages/textInputWidgets1';
+import textInputFullName from '../pages/textInputFullName';
+// import checkbox from '../pages/checkbox';
+// import radio from '../pages/radio';
+// import select from '../pages/select';
+// import date from '../pages/date';
 
 const formConfig = {
   rootUrl: manifest.rootUrl,
@@ -37,53 +39,67 @@ const formConfig = {
           title: 'Text Input',
           uiSchema: textInput.uiSchema,
           schema: textInput.schema,
+          initialData: textInput.initialData,
+        },
+        textInputWidgets1: {
+          path: 'text-input-widgets1',
+          title: 'Text Input Widgets 1',
+          uiSchema: textInputWidgets1.uiSchema,
+          schema: textInputWidgets1.schema,
+        },
+        textInputFullName: {
+          path: 'text-input-full-name',
+          title: 'Text Input Full Name',
+          uiSchema: textInputFullName.uiSchema,
+          schema: textInputFullName.schema,
+          initialData: textInputFullName.initialData,
         },
       },
     },
-    checkbox: {
-      title: 'Checkbox',
-      pages: {
-        checkbox: {
-          path: 'checkbox',
-          title: 'Checkbox',
-          uiSchema: checkbox.uiSchema,
-          schema: checkbox.schema,
-        },
-      },
-    },
-    select: {
-      title: 'Select',
-      pages: {
-        checkbox: {
-          path: 'select',
-          title: 'Select',
-          uiSchema: select.uiSchema,
-          schema: select.schema,
-        },
-      },
-    },
-    radio: {
-      title: 'Radio',
-      pages: {
-        checkbox: {
-          path: 'radio',
-          title: 'Radio',
-          uiSchema: radio.uiSchema,
-          schema: radio.schema,
-        },
-      },
-    },
-    date: {
-      title: 'Date',
-      pages: {
-        checkbox: {
-          path: 'date',
-          title: 'Date',
-          uiSchema: date.uiSchema,
-          schema: date.schema,
-        },
-      },
-    },
+    // checkbox: {
+    //   title: 'Checkbox',
+    //   pages: {
+    //     checkbox: {
+    //       path: 'checkbox',
+    //       title: 'Checkbox',
+    //       uiSchema: checkbox.uiSchema,
+    //       schema: checkbox.schema,
+    //     },
+    //   },
+    // },
+    // select: {
+    //   title: 'Select',
+    //   pages: {
+    //     checkbox: {
+    //       path: 'select',
+    //       title: 'Select',
+    //       uiSchema: select.uiSchema,
+    //       schema: select.schema,
+    //     },
+    //   },
+    // },
+    // radio: {
+    //   title: 'Radio',
+    //   pages: {
+    //     checkbox: {
+    //       path: 'radio',
+    //       title: 'Radio',
+    //       uiSchema: radio.uiSchema,
+    //       schema: radio.schema,
+    //     },
+    //   },
+    // },
+    // date: {
+    //   title: 'Date',
+    //   pages: {
+    //     checkbox: {
+    //       path: 'date',
+    //       title: 'Date',
+    //       uiSchema: date.uiSchema,
+    //       schema: date.schema,
+    //     },
+    //   },
+    // },
   },
 };
 
---
+++ src/applications/simple-forms/mock-simple-forms-patterns/pages/textInputWidgets1.js
@@ -0,0 +1,68 @@
+import VaTextInputField from 'platform/forms-system/src/js/web-component-fields/VaTextInputField';
+import {
+  emailUI,
+  ssnUI,
+} from 'applications/caregivers/definitions/UIDefinitions/sharedUI';
+import phoneUI from 'platform/forms-system/src/js/definitions/phone';
+
+export default {
+  uiSchema: {
+    emailOld: {
+      ...emailUI(),
+      'ui:title': 'TextWidget - emailUI',
+    },
+    emailNew: {
+      ...emailUI(),
+      'ui:title': 'VaTextInputField - emailUI',
+      'ui:webComponentField': VaTextInputField,
+    },
+    phoneOld: phoneUI('TextWidget - phoneUI'),
+    phoneNew: {
+      ...phoneUI('VaTextInputField - phoneUI'),
+      'ui:webComponentField': VaTextInputField,
+    },
+    ssnOld: {
+      ...ssnUI(),
+      'ui:title': 'TextWidget - ssnUI',
+    },
+    ssnNew: {
+      ...ssnUI(),
+      'ui:title': 'VaTextInputField - ssnUI',
+      'ui:webComponentField': VaTextInputField,
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      emailOld: {
+        type: 'string',
+        pattern: '^\\S+@\\S+$',
+        minLength: 3,
+        maxLength: 10,
+      },
+      emailNew: {
+        type: 'string',
+        pattern: '^\\S+@\\S+$',
+        minLength: 3,
+        maxLength: 10,
+      },
+      phoneOld: {
+        type: 'string',
+        minLength: 10,
+      },
+      phoneNew: {
+        type: 'string',
+        minLength: 10,
+      },
+      ssnOld: {
+        type: 'string',
+        pattern: '^[0-9]{9}$',
+      },
+      ssnNew: {
+        type: 'string',
+        pattern: '^[0-9]{9}$',
+      },
+    },
+    required: ['requiredOld', 'requiredNew'],
+  },
+};
