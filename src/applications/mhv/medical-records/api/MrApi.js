172aa26469c7fdfc2a052ebbe0bd181bc3912f81
Kolby Balch <108146636+KolbyVA@users.noreply.github.com>
2023-04-07 11:32:19 -0500

    Mhv 42370 conditions test 2 (#23880)
    * [#MHV-42370] Health Conditions List Page
    
    * [#MHV-42370] Added recordType constants
    
    * [#MHV-42370] Fixed Unit tests
    
    * [#MHV-42370] styling fix

--- src/applications/mhv/medical-records/api/MrApi.js
+++ src/applications/mhv/medical-records/api/MrApi.js
@@ -3,6 +3,7 @@ import { apiRequest } from 'platform/utilities/api';
 import labsAndTests from '../tests/fixtures/labsAndTests.json';
 import vaccines from '../tests/fixtures/vaccines.json';
 import vitals from '../tests/fixtures/vitals.json';
+import conditions from '../tests/fixtures/conditions.json';
 
 const apiBasePath = `${environment.API_URL}/my_health/v1`;
 
@@ -35,6 +36,23 @@ export const mockGetVitalsList = () => {
   return new Promise(resolve => {
     setTimeout(() => {
       resolve(vitals);
+    }, 1000);
+  });
+};
+
+export const mockGetConditionsList = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(conditions);
+    }, 1000);
+  });
+};
+
+export const mockGetCondition = id => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const condition = conditions.find(cond => +cond.id === +id);
+      resolve(condition);
     }, 1000);
   });
 };
