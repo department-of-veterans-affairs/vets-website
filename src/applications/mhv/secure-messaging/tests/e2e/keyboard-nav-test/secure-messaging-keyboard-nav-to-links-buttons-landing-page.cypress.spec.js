3fe6966a37bee3cd3b6a11cc81e4c93fe607c97f
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2023-04-10 14:41:11 -0500

    Test Fixes
    Test fixes added to update the should assertion.

--- src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-links-buttons-landing-page.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-links-buttons-landing-page.cypress.spec.js
@@ -11,14 +11,14 @@ describe('Secure Messaging Verify Links and Buttons Keyboard Nav', () => {
     cy.tabToElement('.sidebar-navigation-messages-list-header > a');
     cy.realPress(['Enter']);
     cy.tabToElement('[data-testid="compose-message-link"]').should(
-      'be.focused',
+      'have.focus',
     );
-    cy.tabToElement('.vads-c-action-link--blue').should('be.focused');
+    cy.tabToElement('.vads-c-action-link--blue').should('have.focus');
     cy.tabToElement('.welcome-message > :nth-child(4) > a').should(
-      'be.focused',
+      'have.focus',
     );
     cy.tabToElement('.vads-u-padding-right--1 > .hydrated').should(
-      'be.focused',
+      'have.focus',
     );
     cy.injectAxe();
     cy.axeCheck();
