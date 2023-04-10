fa811a7fed91b68a8c76fe7108476f8df9fa64d1
Tony Wiliiams <tony.williams@va.gov>
2023-04-10 15:14:18 -0500

    Update src/applications/vaos/appointment-list/components/AppointmentsPageV2/RequestAppointmentLayout.jsx
    Co-authored-by: jenniemc <54327023+jenniemc@users.noreply.github.com>

--- src/applications/vaos/appointment-list/components/AppointmentsPageV2/RequestAppointmentLayout.jsx
+++ src/applications/vaos/appointment-list/components/AppointmentsPageV2/RequestAppointmentLayout.jsx
@@ -50,7 +50,7 @@ export default function RequestAppointmentLayout({ appointment }) {
           >
             <AppointmentRow className="vaos-appts__column-gap--3 small-screen:vads-u-flex-direction--row">
               <AppointmentColumn size="1" className="vads-u-flex--4">
-                <AppointmentRow className="vaos-appts__column-gap--3 xsmall-screen:vads-u-flex-direction--column small-screen:vads-u-flex-direction--row">
+                <AppointmentRow className="vaos-appts__column-gap--3 vaos-appts__display--table vaos-appts__text--truncate xsmall-screen:vads-u-flex-direction--column small-screen:vads-u-flex-direction--row">
                   <AppointmentColumn
                     padding="0"
                     size="1"
