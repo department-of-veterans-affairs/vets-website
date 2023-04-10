ac4338353afae1f6e3cf33358bf82a70dd4687a3
Robert Hasselle <robert.hasselle@oddball.io>
2023-04-10 14:30:49 -0500

    Add SSN + related IDs web components

---
+++ src/applications/simple-forms/mock-simple-forms-patterns/pages/ssnPattern.js
@@ -0,0 +1,26 @@
+import {
+  ssnUI,
+  ssnSchema,
+  vaFileNumberSchema,
+  vaFileNumberUI,
+  serviceNumberUI,
+  serviceNumberSchema,
+} from 'platform/forms-system/src/js/web-component-schemas/personalIdSchemas';
+
+export default {
+  uiSchema: {
+    ssn: ssnUI(),
+    vaFileNumber: vaFileNumberUI(),
+    serviceNumber: serviceNumberUI(),
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      ssn: ssnSchema(),
+      vaFileNumber: vaFileNumberSchema(),
+      serviceNumber: serviceNumberSchema(),
+    },
+    required: ['ssn'],
+  },
+  initialData: {},
+};
--- src/platform/forms-system/src/js/web-component-fields/VaTextInputField.jsx
+++ src/platform/forms-system/src/js/web-component-fields/VaTextInputField.jsx
@@ -1,55 +1,8 @@
 import React from 'react';
 import { VaTextInput } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import vaTextInputFieldMapping from './vaTextInputFieldMapping';
 
 export default function VaTextInputField(props) {
-  const {
-    description,
-    textDescription,
-    DescriptionField,
-    label,
-    required,
-    error,
-    uiOptions,
-    index,
-    childrenProps,
-  } = props;
-
-  let inputType = uiOptions?.inputType;
-  if (!inputType) {
-    inputType = ['number', 'integer'].includes(childrenProps.schema.type)
-      ? 'number'
-      : 'text';
-  }
-
-  return (
-    <VaTextInput
-      name={childrenProps.idSchema.$id}
-      label={label}
-      autocomplete={uiOptions?.autocomplete}
-      required={required}
-      error={error}
-      maxlength={childrenProps.schema.maxLength}
-      minlength={childrenProps.schema.minLength}
-      value={
-        typeof childrenProps.formData === 'undefined'
-          ? ''
-          : childrenProps.formData
-      }
-      inputmode={uiOptions?.inputmode}
-      type={inputType}
-      hint={uiOptions?.hint}
-      onInput={event =>
-        childrenProps.onChange(
-          event.target.value ? event.target.value : undefined,
-        )
-      }
-      onBlur={() => childrenProps.onBlur(childrenProps.idSchema.$id)}
-    >
-      {textDescription && <p>{textDescription}</p>}
-      {DescriptionField && (
-        <DescriptionField options={uiOptions} index={index} />
-      )}
-      {!textDescription && !DescriptionField && description}
-    </VaTextInput>
-  );
+  const mappedProps = vaTextInputFieldMapping(props);
+  return <VaTextInput {...mappedProps} />;
 }
