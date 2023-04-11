1aac8087781ae9b04c9b086aa59ea49f8c4ab805
Allison Lu <allisonlu820@gmail.com>
2023-04-11 17:24:47 -0400

    initial TestNotification component w new styles

--- src/applications/personalization/dashboard/components/notifications/TestNotification.jsx
+++ src/applications/personalization/dashboard/components/notifications/TestNotification.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import moment from 'moment';
 import recordEvent from '~/platform/monitoring/record-event';
 import CTALink from '../CTALink';
+import DashboardWidgetWrapper from '../DashboardWidgetWrapper';
+import { dismissNotificationById } from '../../actions/notifications';
 
 import './TestNotification.scss';
 
@@ -12,36 +17,84 @@ function handleNotification() {
   });
 }
 
-const TestNotification = () => {
-  return (
-    <>
-      <div
-        className="onsite-notification vads-u-background-color--white vads-u-padding--2"
-        role="alert"
-      >
-        <div className="vads-u-margin-top--0 vads-u-display--flex">
-          <i
-            aria-hidden="true"
-            className="fas fa-exclamation-circle vads-u-color--secondary-darkest vads-u-font-size--xl
-            vads-u-margin-right--1"
-          />
-          <div className="body" role="presentation">
-            <h4 className="vads-u-margin-y--0">You have new debt. </h4>
-            <div className="">Tuesday, March 4, 2023</div>
+export const TestNotification = ({ notification, dismissNotification }) => {
+  const [visible, setVisible] = useState(true);
 
-            <CTALink
-              ariaLabel=""
-              className="vads-u-margin-top--1 vads-u-font-weight--bold"
-              text="Manage your VA debt"
-              href="/manage-va-debt/your-debt"
-              onClick={handleNotification}
-              showArrow
+  const createdAtFormatted = moment(notification.attributes.createdAt).format(
+    'dddd, MMM DD, YYYY',
+  );
+
+  const closeNotification = () => {
+    setVisible(prev => !prev);
+    dismissNotification(notification.id);
+  };
+
+  return (
+    <DashboardWidgetWrapper>
+      {visible && (
+        <div
+          className="onsite-notification vads-u-background-color--white vads-u-padding--2 vads-u-margin-bottom--2p5"
+          role="alert"
+        >
+          <div className="vads-u-margin-top--0 vads-u-display--flex">
+            <i
+              aria-hidden="true"
+              className="fas fa-exclamation-circle vads-u-color--secondary-darkest vads-u-font-size--xl
+            vads-u-margin-right--1"
             />
+            <div className="body" role="presentation">
+              <h4 className="vads-u-margin-y--0">You have new debt. </h4>
+              <div className="">{createdAtFormatted}</div>
+
+              <CTALink
+                ariaLabel=""
+                className="vads-u-margin-top--1 vads-u-font-weight--bold"
+                text="Manage your VA debt"
+                href="/manage-va-debt/your-debt"
+                onClick={handleNotification}
+                showArrow
+              />
+            </div>
+            <button
+              className="onsite-notification-close"
+              aria-label="Close notification"
+              type="button"
+              onClick={closeNotification}
+            >
+              <i
+                aria-hidden="true"
+                className="fas fa-times-circle vads-u-margin-right--1"
+                role="presentation"
+              />
+              <span>CLOSE</span>
+            </button>
           </div>
         </div>
-      </div>
-    </>
+      )}
+    </DashboardWidgetWrapper>
   );
 };
 
-export default TestNotification;
+TestNotification.propTypes = {
+  dismissNotification: PropTypes.func.isRequired,
+  notification: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    attributes: PropTypes.shape({
+      createdAt: PropTypes.string.isRequired,
+      dismissed: PropTypes.bool.isRequired,
+      templateId: PropTypes.string.isRequired,
+      updatedAt: PropTypes.string,
+      vaProfileId: PropTypes.string.isRequired,
+    }),
+  }),
+};
+
+const mapDispatchToProps = {
+  dismissNotification: dismissNotificationById,
+};
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(TestNotification);
