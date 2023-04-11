02d37e5885fe2d1b036e6bc1c3541c825b2b3c1e
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2023-04-11 16:07:40 -0400

    MHV-43744, 43741 SM to VA.gov: a11y Feedback (Major): Focus on Save, a11y Feedback (Critical): Folder Input field (#23845)
    * [MHV-43744] focus on error input field
    
    * [MHV-43741] proper labels for va-text-input
    
    * [MHV-43741] print handle error on input
    
    * [MHV-43744] force focus on repeat validation failure
    
    * [MHV-43744] Create Folder force focus on repeat validation failure
    
    ---------
    
    Co-authored-by: Aaron Read <Aaron.Read@va.gov>

--- src/applications/mhv/secure-messaging/components/ManageFolderButtons.jsx
+++ src/applications/mhv/secure-messaging/components/ManageFolderButtons.jsx
@@ -2,9 +2,10 @@ import {
   VaModal,
   VaTextInput,
 } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useHistory, useLocation, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { focusElement } from '@department-of-veterans-affairs/platform-utilities/ui';
 import { navigateToFoldersPage } from '../util/helpers';
 import {
   delFolder,
@@ -29,6 +30,7 @@ const ManageFolderButtons = () => {
   const [deleteModal, setDeleteModal] = useState(false);
   const [renameModal, setRenameModal] = useState(false);
   const [folderName, setFolderName] = useState('');
+  const folderNameInput = useRef();
   let folderMatch = null;
 
   useEffect(
@@ -38,6 +40,14 @@ const ManageFolderButtons = () => {
       }
     },
     [dispatch, location.pathname, params.folderId],
+  );
+
+  useEffect(
+    () => {
+      if (nameWarning.length)
+        focusElement(folderNameInput.current.shadowRoot.querySelector('input'));
+    },
+    [nameWarning],
   );
 
   const openDelModal = () => {
@@ -72,9 +82,10 @@ const ManageFolderButtons = () => {
     setRenameModal(false);
   };
 
-  const confirmRenameFolder = () => {
+  const confirmRenameFolder = async () => {
     folderMatch = null;
     folderMatch = folders.filter(testFolder => testFolder.name === folderName);
+    await setNameWarning(''); // Clear any previous warnings, so that the warning state can be updated and refocuses back to input if on repeat Save clicks.
     if (folderName === '' || folderName.match(/^[\s]+$/)) {
       setNameWarning(ErrorMessages.ManageFolders.FOLDER_NAME_REQUIRED);
     } else if (folderMatch.length > 0) {
@@ -159,14 +170,16 @@ const ManageFolderButtons = () => {
         modalTitle={`Editing: ${folder.name}`}
         onCloseEvent={closeRenameModal}
       >
-        <p className="vads-u-margin--0">
-          {Alerts.Folder.CREATE_FOLDER_MODAL_LABEL}
-        </p>
         <VaTextInput
+          ref={folderNameInput}
+          label={Alerts.Folder.CREATE_FOLDER_MODAL_LABEL}
           value={folderName}
           className="input"
           error={nameWarning}
-          onInput={e => setFolderName(e.target.value)}
+          onInput={e => {
+            setFolderName(e.target.value);
+            setNameWarning(e.target.value ? '' : 'Folder name cannot be blank');
+          }}
           maxlength="50"
           name="new-folder-name"
         />
