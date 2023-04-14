b784d5d7a17356e41420014f935689d1b00e3e17
haniasma <121971510+haniasma@users.noreply.github.com>
2023-04-14 10:28:48 -0400

    Create secure-messaging-keyboard-nav-to-trash-button.cypress.spec.js

---
+++ src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-trash-button.cypress.spec.js
@@ -0,0 +1,44 @@
+import SecureMessagingSite from '../sm_site/SecureMessagingSite';
+import PatientInboxPage from '../pages/PatientInboxPage';
+import customFolderMessage from '../fixtures/messages-response.json';
+import customFolder from '../fixtures/folder-custom-metadata.json';
+
+describe('Navigate to Trash button', () => {
+  it('confirm button the in custem folder', () => {
+    const landingPage = new PatientInboxPage();
+    const site = new SecureMessagingSite();
+    site.login();
+
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/7038175',
+      customFolder,
+    ).as('test2Folder');
+    landingPage.loadInboxMessages();
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/7038175/threads?pageSize=100&pageNumber=1&sortField=SENT_DATE&sortOrder=DESC',
+      customFolderMessage,
+    ).as('customFolder');
+
+    cy.get('[data-testid="my-folders-sidebar"]').click();
+    cy.contains('TEST2').click();
+    cy.wait('@customFolder');
+    cy.wait('@test2Folder');
+    cy.tabToElement('[data-testid="remove-folder-button"]');
+    cy.get('[data-testid="remove-folder-button"]').should('have.focus');
+    cy.realPress(['Enter']);
+    cy.get('[class="modal hydrated"]')
+      .shadow()
+      .find('button');
+    cy.get('[visible=""] > p');
+    cy.intercept('DELETE', `/my_health/v1/messaging/folders/7038175`, {
+      statusCode: 204,
+    }).as('deleteFolder');
+    cy.tabToElement('[text="Remove"]');
+    cy.realPress(['Enter']);
+    cy.get('.vads-u-margin-bottom--1').should('have.focus');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+});
