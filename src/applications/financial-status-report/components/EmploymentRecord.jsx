9b7a80983c6bec52699b95d5d1701738fd40ff3f
Aaron Ponce <aaron.ponce@va.gov>
2023-04-13 17:39:31 -0700

    fix linting

--- src/applications/financial-status-report/components/EmploymentRecord.jsx
+++ src/applications/financial-status-report/components/EmploymentRecord.jsx
@@ -209,4 +209,7 @@ const mapDispatchToProps = {
   setFormData: setData,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(EmploymentRecord);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(EmploymentRecord);
