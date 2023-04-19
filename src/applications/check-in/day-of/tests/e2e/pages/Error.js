a9a18c22943be00b9425d1a3b53e547758d21715
John Woolschlager <wullaski@gmail.com>
2023-04-19 10:08:28 -0400

    Fix tests

--- src/applications/check-in/day-of/tests/e2e/pages/Error.js
+++ src/applications/check-in/day-of/tests/e2e/pages/Error.js
@@ -2,7 +2,7 @@ import Timeouts from 'platform/testing/e2e/timeouts';
 
 const titles = {
   default: {
-    en: 'We couldn’t check you in',
+    en: 'We couldn’t check you in.',
     es: 'No pudimos completar el registro.',
     tl: 'Hindi ka namin mai-check in',
   },
