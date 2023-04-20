feedb2d62d096cd36ff25d346b67cb8ee4cbf48f
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2023-04-20 10:03:55 -0400

    MHV-41394 SM to VA.gov: Multiple draft messages get created for Reply flow (#23944)
    * [MHV-41394] fix a defect on duplicate reply draft save

--- src/applications/mhv/secure-messaging/components/ComposeForm/ReplyForm.jsx
+++ src/applications/mhv/secure-messaging/components/ComposeForm/ReplyForm.jsx
@@ -46,7 +46,7 @@ const ReplyForm = props => {
 
   const isSaving = useSelector(state => state.sm.draftDetails.isSaving);
   const history = useHistory();
-  let draft;
+  const [draft, setDraft] = useState(null);
 
   const debouncedSubject = useDebounce(subject, draftAutoSaveTimeout);
   const debouncedMessageBody = useDebounce(messageBody, draftAutoSaveTimeout);
@@ -61,6 +61,9 @@ const ReplyForm = props => {
         setSubject(replyMessage.subject);
         setMessageBody('');
         setCategory(replyMessage.category);
+      }
+      if (draftToEdit) {
+        setDraft(draftToEdit);
       }
     },
     [replyMessage, draftToEdit],
@@ -158,10 +161,14 @@ const ReplyForm = props => {
     );
   };
 
-  if (draftToEdit && !formPopulated) {
-    draft = draftToEdit;
-    populateForm();
-  }
+  useEffect(
+    () => {
+      if (draft && !formPopulated) {
+        populateForm();
+      }
+    },
+    [draft],
+  );
 
   const setMessageTitle = () => {
     const casedCategory =
@@ -234,7 +241,7 @@ const ReplyForm = props => {
     if (!draftId) {
       dispatch(saveReplyDraft(replyMessage.messageId, formData, type)).then(
         newDraft => {
-          draft = newDraft;
+          setDraft(newDraft);
           setNewDraftId(newDraft.messageId);
         },
       );
