340a02743cf441c613665a029c039a94b2da876a
Garrett <57802560+gmrabian@users.noreply.github.com>
2023-04-20 13:01:29 -0400

    just in case

--- src/applications/simple-forms/21-4142/config/prefill-transformer.js
+++ src/applications/simple-forms/21-4142/config/prefill-transformer.js
@@ -3,12 +3,12 @@ export default function prefillTransformer(pages, formData, metadata) {
     pages,
     formData: {
       veteran: {
-        fullName: formData.veteran.fullName,
-        dateOfBirth: formData.veteran.dateOfBirth,
-        ssn: formData.veteran.ssn,
-        address: formData.veteran.address,
-        homePhone: formData.veteran.homePhone,
-        email: formData.veteran.email,
+        fullName: formData?.veteran?.fullName,
+        dateOfBirth: formData?.veteran?.dateOfBirth,
+        ssn: formData?.veteran?.ssn,
+        address: formData?.veteran?.address,
+        homePhone: formData?.veteran?.homePhone,
+        email: formData?.veteran?.email,
       },
     },
     metadata,
