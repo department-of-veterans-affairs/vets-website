37a0ded04ab59551f78edcabc6145fcdbbb20979
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2023-04-24 11:07:03 -0400

    test full workflow

--- script/github-actions/pe-deploy-source.js
+++ script/github-actions/pe-deploy-source.js
@@ -2,8 +2,7 @@ const core = require('@actions/core');
 
 const sourceEvent = process.env.SOURCE_EVENT;
 const sourceRepo = process.env.SOURCE_REPO;
-const sourceRef = process.env.SOURCE_BRANCH;
-const workflowRef = process.env.WORKFLOW_BRANCH;
+const sourceRef = process.env.SOURCE_REF;
 // const sourceDeployment = process.env.SOURCE_DEPLOYMENT;
 // const workflowDeployment = process.env.WORKFLOW_DEPLOYMENT;
 
@@ -12,18 +11,14 @@ if (
   sourceEvent === 'workflow_dispatch'
 ) {
   core.exportVariable('SOURCE_REPO', sourceRepo);
-  core.exportVariable('SOURCE_REF', sourceRef);
-  core.exportVariable(
-    'SOURCE_REF_SANITIZED',
-    sourceRef.replace(/[^a-zA-Z0-9-_]/g, ''),
-  );
+
   // core.exportVariable('DEPLOYMENT_ID', sourceDeployment);
 } else {
   core.exportVariable('SOURCE_REPO', 'vets-website');
-  core.exportVariable('SOURCE_REF', workflowRef);
-  core.exportVariable(
-    'SOURCE_REF_SANITIZED',
-    workflowRef.replace(/[^a-zA-Z0-9-_]/g, '-'),
-  );
   // core.exportVariable('DEPLOYMENT_ID', workflowDeployment);
 }
+
+core.exportVariable(
+  'SOURCE_REF_SANITIZED',
+  sourceRef.replace(/[^a-zA-Z0-9-_]/g, ''),
+);
