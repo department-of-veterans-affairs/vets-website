00697d799bdd06294980922f4a3173450f7afe99
Robin Garrison <robin.garrison@adhocteam.us>
2023-04-26 13:13:20 -0500

    Add contact info validation for review & submit

--- src/applications/appeals/995/pages/contactInformation.js
+++ src/applications/appeals/995/pages/contactInformation.js
@@ -1,9 +1,32 @@
+import formSchema from '../config/form-0995-schema.json';
+
+const { address, phone } = formSchema.definitions;
+
 const contactInfo = {
   uiSchema: {},
 
   schema: {
     type: 'object',
-    properties: {},
+    properties: {
+      veteran: {
+        type: 'object',
+        properties: {
+          address: {
+            ...address,
+            // the schema has countryCodeISO2 and zipCode5
+            required: ['addressLine1', 'city', 'countryCodeIso2', 'zipCode'],
+          },
+          homePhone: phone,
+          mobilePhone: phone,
+          email: {
+            type: 'string',
+            format: 'email',
+            minLength: 6,
+            maxLength: 255,
+          },
+        },
+      },
+    },
   },
 };
 
