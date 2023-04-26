41640fcf42af3f99226d4e54535d9a395b0817c9
Garrett Rabian <garrett@coforma.io>
2023-04-26 15:55:58 -0400

    simplify component now that facility name is required

--- src/applications/simple-forms/21-4142/components/RecordField.jsx
+++ src/applications/simple-forms/21-4142/components/RecordField.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
 export default function RecordField({ formData }) {
-  const providerFacilityName = formData?.providerFacilityName;
-  const { street: providerFacilityStreet } = formData?.providerFacilityAddress;
-
   return (
     <div>
-      <strong>{providerFacilityName ?? providerFacilityStreet}</strong>
+      <strong>{formData.providerFacilityName}</strong>
     </div>
   );
 }
