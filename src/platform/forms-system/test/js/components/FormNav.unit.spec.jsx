5c88f33d5118a8138ab8825b6841e91165441505
Tze-chiu Lei <tze@adhocteam.us>
2023-04-26 16:39:43 -0700

    Cherry-pick FormNav fix from VFEP-51 feature-branch

--- src/platform/forms-system/test/js/components/FormNav.unit.spec.jsx
+++ src/platform/forms-system/test/js/components/FormNav.unit.spec.jsx
@@ -65,7 +65,7 @@ describe('Schemaform FormNav', () => {
     );
 
     expect(tree.getByTestId('navFormHeader').textContent).to.contain(
-      'Step 1 of 3: Testing',
+      'Step 1 of 4: Testing',
     );
   });
   it('should optionally hide current chapter progress-bar & stepText', () => {
@@ -121,7 +121,7 @@ describe('Schemaform FormNav', () => {
     );
 
     expect(tree.getByTestId('navFormHeader')).to.contain.text(
-      'Step 1 of 2: Testing',
+      'Step 1 of 3: Testing',
     );
   });
 
