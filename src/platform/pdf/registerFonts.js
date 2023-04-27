04688961b6e3c0b2198e9f1b78e28d8516140fef
Adrian Rollett <acrollet@gmail.com>
2023-04-27 12:34:58 -0600

    Improve font fetching

--- src/platform/pdf/registerFonts.js
+++ src/platform/pdf/registerFonts.js
@@ -6,15 +6,16 @@ const knownFonts = {
 };
 
 export default async function registerFonts(doc, fonts) {
-  for (const font of fonts) {
+  const fontPromises = fonts.map(async font => {
     if (!knownFonts[font]) return;
 
-    // Fixme await in loop.
     const request = await fetch(`/generated/${knownFonts[font]}`);
     const binaryFont = await request.arrayBuffer();
     const encodedFont = Buffer.from(binaryFont).toString('base64');
     fs.writeFileSync(knownFonts[font], encodedFont);
 
     doc.registerFont(font, knownFonts[font]);
-  }
+  });
+
+  await Promise.all(fontPromises);
 }
