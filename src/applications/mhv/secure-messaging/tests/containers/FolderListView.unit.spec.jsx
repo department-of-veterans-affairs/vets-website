aac213f20a2d0a7e4184ba4f5fdfff682acbf3fa
Vic Saleem <victoria.saleem@va.gov>
2023-04-28 03:57:44 -0500

    [MHV-43740] Commented-out unusedunit test: FolderListView; see notes

--- src/applications/mhv/secure-messaging/tests/containers/FolderListView.unit.spec.jsx
+++ src/applications/mhv/secure-messaging/tests/containers/FolderListView.unit.spec.jsx
@@ -1,62 +1,70 @@
-import React from 'react';
-import { renderWithStoreAndRouter } from '@department-of-veterans-affairs/platform-testing/react-testing-library-helpers';
-import { expect } from 'chai';
-import messages from '../fixtures/search-response.json';
-import folder from '../fixtures/folder-inbox-metadata.json';
-import reducer from '../../reducers';
-import FolderListView from '../../containers/FolderListView';
+/**
+ * @Note : This component is currently not used in SM application.
+ *         Consider deleting this file if there are no plans to use
+ *         this component in the future, or refactor this file to test
+ *         for the 'FilterBox' component.
+ */
 
-describe('FolderListView', () => {
-  const initialState = {
-    sm: {
-      folders: { folder },
-      messages: {
-        messageList: messages,
-      },
-      search: {
-        keyword: '',
-      },
-    },
-  };
+// import React from 'react';
+// import { renderWithStoreAndRouter } from '@department-of-veterans-affairs/platform-testing/react-testing-library-helpers';
+// import { expect } from 'chai';
+// import messages from '../fixtures/search-response.json';
+// import folder from '../fixtures/folder-inbox-metadata.json';
+// import reducer from '../../reducers';
+// import FolderListView from '../../containers/FolderListView';
 
-  const setup = (state = initialState) => {
-    return renderWithStoreAndRouter(<FolderListView testing />, {
-      initialState: state,
-      reducers: reducer,
-      path: `/`,
-    });
-  };
+// describe('FolderListView', () => {
+//   const initialState = {
+//     sm: {
+//       folders: { folder },
+//       messages: {
+//         messageList: messages,
+//       },
+//       search: {
+//         keyword: '',
+//       },
+//     },
+//   };
 
-  it('renders without errors', () => {
-    const screen = setup();
-    expect(screen.getByText('Inbox', { exact: true, selector: 'h1' })).to.exist;
-  });
+//   const setup = (state = initialState) => {
+//     return renderWithStoreAndRouter(<FolderListView testing />, {
+//       initialState: state,
+//       reducers: reducer,
+//       path: `/`,
+//     });
+//   };
 
-  it('displays loading indicator when messages are not yet loaded', () => {
-    const state = {
-      sm: {
-        folders: { folder: undefined },
-        messages: {
-          messageList: undefined,
-        },
-        search: {
-          keyword: '',
-        },
-      },
-    };
-    const screen = setup(state);
-    const loadingIndicator = screen.getByTestId('loading-indicator');
-    expect(loadingIndicator).to.exist;
-  });
+//   it('renders without errors', () => {
+//     const screen = setup();
+//     expect(screen.getByText('Inbox', { exact: true, selector: 'h1' })).to.exist;
+//   });
 
-  it('displays the name of folder to be searched', async () => {
-    const screen = setup();
-    const folderStatementStart = await screen.getByTestId(
-      'search-message-folder-input-label',
-    );
+//   it('displays loading indicator when messages are not yet loaded', () => {
+//     const state = {
+//       sm: {
+//         folders: { folder: undefined },
+//         messages: {
+//           messageList: undefined,
+//         },
+//         search: {
+//           keyword: '',
+//         },
+//       },
+//     };
+//     const screen = setup(state);
+//     const loadingIndicator = screen.getByTestId('loading-indicator');
+//     expect(loadingIndicator).to.exist;
+//   });
 
-    expect(folderStatementStart.textContent).to.contain(
-      `Search your ${folder.name} messages folder`,
-    );
-  });
-});
+//   // Make updates to this test when FilterBox component is merged
+//   it('displays the name of folder to be searched', async () => {
+//     const screen = setup();
+//     const folderStatementStart = await screen.getByTestId(
+//       'search-message-folder-input-label',
+//     );
+
+//     expect(folderStatementStart.textContent).to.contain(
+//       `Search your ${folder.name} messages folder`,
+//     );
+//   });
+// });
