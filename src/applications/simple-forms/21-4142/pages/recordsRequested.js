c84536093722500b78104fcd9c122d4590e52e82
Garrett Rabian <garrett@coforma.io>
2023-04-28 14:01:36 -0400

    reset to cleaner state, axe issue

--- src/applications/simple-forms/21-4142/pages/recordsRequested.js
+++ src/applications/simple-forms/21-4142/pages/recordsRequested.js
@@ -5,34 +5,6 @@ import fullSchema from 'vets-json-schema/dist/21-4142-schema.json';
 import { providerFacilityFields } from '../definitions/constants';
 
 import RecordField from '../components/RecordField';
-import { RecordReviewField } from '../components/RecordReviewField';
-
-const addressUiSchema = address.uiSchema(null, false, () => true);
-const { country, street, street2, city, state, postalCode } = addressUiSchema;
-const countryReviewPattern = {
-  ...country,
-  'ui:reviewField': RecordReviewField,
-};
-const streetReviewPattern = {
-  ...street,
-  'ui:reviewField': RecordReviewField,
-};
-const street2ReviewPattern = {
-  ...street2,
-  'ui:reviewField': RecordReviewField,
-};
-const cityReviewPattern = {
-  ...city,
-  'ui:reviewField': RecordReviewField,
-};
-const stateReviewPattern = {
-  ...state,
-  'ui:reviewField': RecordReviewField,
-};
-const postalCodeReviewPattern = {
-  ...postalCode,
-  'ui:reviewField': RecordReviewField,
-};
 
 export default {
   uiSchema: {
@@ -49,10 +21,9 @@ export default {
     ),
     [providerFacilityFields.parentObject]: {
       'ui:options': {
+        itemName: 'Treatment record',
         viewField: RecordField,
         keepInPageOnReview: true,
-        // Added to make h5 in review list go away – see ReadOnlyArrayField.jsx
-        customTitle: ' ',
       },
       items: {
         'ui:order': [
@@ -64,34 +35,20 @@ export default {
         [providerFacilityFields.providerFacilityName]: {
           'ui:title': 'Name of private provider or hospital',
           'ui:required': () => true,
-          'ui:reviewField': RecordReviewField,
         },
-        [providerFacilityFields.providerFacilityAddress]: {
-          ...addressUiSchema,
-          country: countryReviewPattern,
-          street: streetReviewPattern,
-          street2: street2ReviewPattern,
-          city: cityReviewPattern,
-          state: stateReviewPattern,
-          postalCode: postalCodeReviewPattern,
-          // ...address.uiSchema(null, false, () => true),
-          // 'ui:reviewField': RecordReviewField,
-        },
+        [providerFacilityFields.providerFacilityAddress]: address.uiSchema(
+          null,
+          false,
+          () => true,
+        ),
         [providerFacilityFields.conditionsTreated]: {
           'ui:title':
             'List the conditions the patient was treated for at this facility',
           'ui:widget': 'textarea',
-          'ui:reviewField': RecordReviewField,
         },
         [providerFacilityFields.treatmentDateRange]: {
-          from: {
-            ...dateUI('First treatment date (you can estimate)'),
-            'ui:reviewField': RecordReviewField,
-          },
-          to: {
-            ...dateUI('Last treatment date (you can estimate)'),
-            'ui:reviewField': RecordReviewField,
-          },
+          from: dateUI('First treatment date (you can estimate)'),
+          to: dateUI('Last treatment date (you can estimate)'),
         },
       },
     },
