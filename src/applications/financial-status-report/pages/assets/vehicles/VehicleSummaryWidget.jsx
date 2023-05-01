37bc1d2f31a8ceadc83fa7e1b0be500ffab585dd
Joseph Lee <joseph@cityfriends.tech>
2023-05-01 16:06:59 -0700

    Made vehicle summary title consistent  with rest of the pages and fixed Real Estae hint text spacing.

--- src/applications/financial-status-report/pages/assets/vehicles/VehicleSummaryWidget.jsx
+++ src/applications/financial-status-report/pages/assets/vehicles/VehicleSummaryWidget.jsx
@@ -59,7 +59,7 @@ const VehicleSummaryWidget = ({
 
   return (
     <form onSubmit={handlers.onSubmit}>
-      <legend className="schemaform-block-title">
+      <legend className="schemaform-block-title vads-u-margin-top--5">
         Your cars or other vehicles
       </legend>
       <div className="vads-u-margin-top--3" data-testid="debt-list">
