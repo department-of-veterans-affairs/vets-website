f125b84696e2c031e755d0facc37391996b5f1b6
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2023-05-02 13:34:50 -0400

    handle focus to thread count on pagination

--- src/applications/mhv/secure-messaging/components/ThreadList/ThreadsList.jsx
+++ src/applications/mhv/secure-messaging/components/ThreadList/ThreadsList.jsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import ThreadListItem from './ThreadListItem';
 
 const ThreadsList = props => {
   const { folder, threadList, keyword, pageNum, threadsPerPage } = props;
 
-  const totalEntries = threadList?.length;
+  const [displayNums, setDisplayNums] = useState({
+    from: 0,
+    to: 0,
+    label: '',
+  }); // [from, to]
   const totalThreads = threadList[0]?.threadPageSize;
-  // get display numbers
-  const fromToNums = (page, total) => {
-    const from = (page - 1) * threadsPerPage + 1;
-    const to = Math.min(page * threadsPerPage, total);
-    return { from, to };
-  };
 
-  const displayNums = fromToNums(pageNum, totalEntries);
+  const fromToNums = useMemo(
+    () => {
+      const from = (pageNum - 1) * threadsPerPage + 1;
+      const to = Math.min(pageNum * threadsPerPage, totalThreads);
+      return { from, to };
+    },
+    [pageNum, threadsPerPage, totalThreads],
+  );
+
+  useEffect(
+    () => {
+      // get display numbers
+      if (fromToNums && totalThreads) {
+        const label = `Displaying ${fromToNums.from} - ${
+          fromToNums.to
+        } of ${totalThreads} conversations`;
+        setDisplayNums({ ...fromToNums, label });
+      }
+    },
+    [fromToNums, totalThreads],
+  );
 
   return (
     <div className="thread-list vads-l-row vads-u-flex-direction--column">
-      <div className="vads-u-padding-y--1 vads-l-row vads-u-margin-top--2 vads-u-border-top--1px vads-u-border-bottom--1px vads-u-border-color--gray-light">
-        Displaying {displayNums.from}
-        &#8211;
-        {displayNums.to} of {totalThreads} conversations
+      <div
+        aria-label={displayNums.label.replace('-', 'to')}
+        data-testid="displaying-number-of-threads"
+        className="vads-u-padding-y--1 vads-l-row vads-u-margin-top--2 vads-u-border-top--1px vads-u-border-bottom--1px vads-u-border-color--gray-light"
+      >
+        {displayNums.label}
       </div>
       {threadList?.length > 0 &&
         threadList.map((thread, idx) => (
