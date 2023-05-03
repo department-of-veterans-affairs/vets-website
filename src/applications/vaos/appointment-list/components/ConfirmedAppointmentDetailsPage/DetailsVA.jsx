54c64254fec5e53bf304fcaa2b2aa8a335f2241b
Simi Adebowale <47654119+simiadebowale@users.noreply.github.com>
2023-05-03 15:18:03 -0400

    update appts details for c& p appts

--- src/applications/vaos/appointment-list/components/ConfirmedAppointmentDetailsPage/DetailsVA.jsx
+++ src/applications/vaos/appointment-list/components/ConfirmedAppointmentDetailsPage/DetailsVA.jsx
@@ -13,6 +13,7 @@ import VAInstructions from './VAInstructions';
 import NoOnlineCancelAlert from './NoOnlineCancelAlert';
 import PhoneInstructions from './PhoneInstructions';
 import { selectTypeOfCareName } from '../../redux/selectors';
+import { APPOINTMENT_STATUS } from '../../../utils/constants';
 
 function formatHeader(appointment) {
   if (appointment.vaos.isCOVIDVaccine) {
@@ -26,14 +27,15 @@ function formatHeader(appointment) {
 
 export default function DetailsVA({ appointment, facilityData }) {
   const locationId = getVAAppointmentLocationId(appointment);
-
   const facility = facilityData?.[locationId];
   const isCovid = appointment.vaos.isCOVIDVaccine;
   const header = formatHeader(appointment);
   const isPhone = appointment.vaos.isPhoneAppointment;
+  const { isPastAppointment } = appointment.vaos;
+  const canceled = appointment.status === APPOINTMENT_STATUS.cancelled;
 
   const typeOfCareName = selectTypeOfCareName(appointment);
-  const isCAndP = typeOfCareName === 'Compensation and pension exam';
+  const isCompAndPen = typeOfCareName === 'Compensation and pension exam';
 
   // v0 does not return a stopCode for covid as serviceType, instead we check for isCovid
   // remove the check for isCovid when we migrate entirely to v2
@@ -42,19 +44,19 @@ export default function DetailsVA({ appointment, facilityData }) {
     return (
       !!typeOfCare && (
         <>
-          {isCAndP ? (
+          {isCompAndPen ? (
             <>
               <h2 className="vads-u-font-size--base vads-u-font-family--sans vads-u-margin-bottom--0 vads-u-display--inline-block">
                 {typeOfCare}
               </h2>
-              <div className="vads-u-display--inline" />
-              <p className="vads-u-font-size--base vads-u-font-family--sans">
-                This appointment is for rating purposes only and will not
-                include treatment.
-                <br />
-                If you have any medical evidence to support your claim, please
-                bring copies to your exam.
-              </p>
+              {!isPastAppointment &&
+                !canceled && (
+                  <p className="vads-l-col--12 medium-screen:vads-l-col--8">
+                    This appointment is for rating purposes only and will not
+                    include treatment. If you have any medical evidence to
+                    claim, please bring copies to your exam.
+                  </p>
+                )}
             </>
           ) : (
             <>
