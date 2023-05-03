7aa54da24dfe0b61b4aa30ebc6e4cc6a3547be54
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2023-05-03 16:35:45 -0500

    Validate Move Message from Custom Folder
    Added scripts to load custom folder messages

--- src/applications/mhv/secure-messaging/tests/e2e/pages/FolderManagementPage.js
+++ src/applications/mhv/secure-messaging/tests/e2e/pages/FolderManagementPage.js
@@ -1,4 +1,9 @@
+import mockCustomResponse from '../fixtures/custom-response.json';
+import defaultMockThread from '../fixtures/thread-response.json';
+
 class FolderManagementPage {
+  currentThread = defaultMockThread;
+
   createANewFolderButton = () => {
     return cy
       .get('[text="Create new folder"]')
@@ -44,6 +49,92 @@ class FolderManagementPage {
 
     cy.contains(folderName).click();
     cy.wait('@customFolderMessages');
+  };
+
+  currentThread = defaultMockThread;
+
+  loadCustomFolderMessageDetails = (
+    mockParentMessageDetails,
+    mockThread = defaultMockThread,
+    previousMessageIndex = 1,
+    mockPreviousMessageDetails = mockCustomResponse,
+  ) => {
+    this.currentThread = mockThread;
+
+    this.currentThread.data.at(0).id =
+      mockParentMessageDetails.data.attributes.messageId;
+    this.currentThread.data.at(0).attributes.messageId =
+      mockParentMessageDetails.data.attributes.messageId;
+    this.currentThread.data.at(0).attributes.subject =
+      mockParentMessageDetails.data.attributes.subject;
+    this.currentThread.data.at(0).attributes.body =
+      mockParentMessageDetails.data.attributes.body;
+    this.currentThread.data.at(0).attributes.category =
+      mockParentMessageDetails.data.attributes.category;
+    this.currentThread.data.at(0).attributes.recipientId =
+      mockParentMessageDetails.data.attributes.recipientId;
+    this.currentThread.data.at(0).attributes.senderName =
+      mockParentMessageDetails.data.attributes.senderName;
+    this.currentThread.data.at(0).attributes.recipientName =
+      mockParentMessageDetails.data.attributes.recipientName;
+
+    cy.log(
+      `loading parent message details.${
+        this.currentThread.data.at(0).attributes.messageId
+      }`,
+    );
+    if (this.currentThread.data.lenghth > 1) {
+      this.currentThread.data.at(previousMessageIndex).attributes.sentDate =
+        mockPreviousMessageDetails.data.attributes.sentDate;
+      this.currentThread.data.at(previousMessageIndex).id =
+        mockPreviousMessageDetails.data.attributes.messageId;
+      this.currentThread.data.at(previousMessageIndex).attributes.messageId =
+        mockPreviousMessageDetails.data.attributes.messageId;
+      this.currentThread.data.at(previousMessageIndex).attributes.subject =
+        mockPreviousMessageDetails.data.attributes.subject;
+      this.currentThread.data.at(previousMessageIndex).attributes.body =
+        mockPreviousMessageDetails.data.attributes.body;
+      this.currentThread.data.at(previousMessageIndex).attributes.category =
+        mockPreviousMessageDetails.data.attributes.category;
+      this.currentThread.data.at(previousMessageIndex).attributes.recipientId =
+        mockPreviousMessageDetails.data.attributes.recipientId;
+      this.currentThread.data.at(previousMessageIndex).attributes.senderName =
+        mockPreviousMessageDetails.data.attributes.senderName;
+      this.currentThread.data.at(
+        previousMessageIndex,
+      ).attributes.recipientName =
+        mockPreviousMessageDetails.data.attributes.recipientName;
+      this.currentThread.data.at(
+        previousMessageIndex,
+      ).attributes.triageGroupName =
+        mockPreviousMessageDetails.data.attributes.triageGroupName;
+    }
+    cy.log(
+      `message thread  = ${JSON.stringify(
+        mockParentMessageDetails.data.attributes.messageId,
+      )}`,
+    );
+    cy.intercept(
+      'GET',
+      `/my_health/v1/messaging/messages/${
+        this.currentThread.data.at(0).attributes.messageId
+      }`,
+      mockParentMessageDetails,
+    ).as('message1');
+
+    cy.intercept(
+      'GET',
+      `/my_health/v1/messaging/messages/${
+        mockParentMessageDetails.data.attributes.messageId
+      }/thread`,
+      this.currentThread,
+    ).as('full-thread');
+
+    cy.contains(mockParentMessageDetails.data.attributes.subject).click();
+    cy.injectAxe();
+    cy.axeCheck();
+    cy.wait('@message1');
+    // cy.wait('@full-thread');
   };
 
   folderConfirmation = () => {
