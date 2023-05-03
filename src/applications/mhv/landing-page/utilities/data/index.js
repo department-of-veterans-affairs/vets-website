e81e0d92f9e4971befa056f4a8b733303fa7b982
Vic Saleem <victoria.saleem@va.gov>
2023-05-03 15:23:12 -0500

    [MHV-43740] Reverted 'Start a new message' back to 'Compose message'; need feedback from va-cto-health-products, vfs-mhv-secure-messaging, and vfs-mhv-integration

--- src/applications/mhv/landing-page/utilities/data/index.js
+++ src/applications/mhv/landing-page/utilities/data/index.js
@@ -46,7 +46,7 @@ const resolveLandingPageLinks = (authdWithSSOe = false, featureToggles) => {
       {
         href: null,
         oldHref: mhvUrl(authdWithSSOe, 'compose-message'),
-        text: 'Start a new message',
+        text: 'Compose message',
         toggle: null,
       },
       {
