5a90a42fc6ebeb02fca541fe698eaa4ab1d87782
Andrew Rodiek <andrew.rodiek@gmail.com>
2023-05-04 12:06:05 -0400

    Removed unused home number functions

--- src/applications/financial-status-report/utils/contactInformation.js
+++ src/applications/financial-status-report/utils/contactInformation.js
@@ -69,13 +69,8 @@ export const getFormattedPhone = phone => {
 };
 
 // schema allows 1 digit area code & 1 digit phone number
-export const hasHomePhone = formData =>
-  getPhoneString(formData?.veteran?.homePhone).length >= 2;
 export const hasMobilePhone = formData =>
   getPhoneString(formData?.veteran?.mobilePhone).length >= 2;
-
-export const hasHomeAndMobilePhone = formData =>
-  hasHomePhone(formData) && hasMobilePhone(formData);
 
 /**
  * Set sessionStorage of last edited contact field. We could have used
