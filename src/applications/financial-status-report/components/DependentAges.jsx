695c091b7d117d3c1d8bd765fd25301901a0e5d2
Aaron Ponce <aaron.ponce@va.gov>
2023-05-04 11:07:43 -0700

    fix linting

--- src/applications/financial-status-report/components/DependentAges.jsx
+++ src/applications/financial-status-report/components/DependentAges.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { VaNumberInput } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import { setData } from 'platform/forms-system/src/js/actions';
+import { setData } from '@department-of-veterans-affairs/platform-forms-system/actions';
 import { DEPENDENT_AGE_LABELS } from '../constants/dependentLabels';
 import { validateIsNumber } from '../utils/validations';
 import ButtonGroup from './shared/ButtonGroup';
@@ -50,7 +50,7 @@ const DependentAges = ({ goToPath, isReviewMode = false }) => {
         }
       }
     },
-    [stateDependents, dispatch, formData, hasDependents, isReviewMode],
+    [dispatch, hasDependents, isReviewMode, formData, stateDependents],
   );
 
   useEffect(
