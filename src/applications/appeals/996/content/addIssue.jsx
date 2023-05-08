fd8ee08d7529fbc87afee2c9dc9503607883851f
Rob Garrison <wowmotty@gmail.com>
2023-05-08 14:14:38 -0500

    HLR | Refactor code (#24145)
    * Update intro page components
    
    * Change add link to action link
    
    * Replace va-date with va-memorable-date
    
    * Add path constants
    
    * Allow single digit month & day in va-memorable-date
    
    * Update unit tests
    
    * Update e2e tests
    
    * Remove line break
    
    * Fix e2e tests

--- src/applications/appeals/996/content/addIssue.jsx
+++ src/applications/appeals/996/content/addIssue.jsx
@@ -28,9 +28,5 @@ export const issueNameHintText = (
 );
 
 export const dateOfDecisionLabel = 'Date of decision';
-export const dateOfDecisionHintText = (
-  <p className="vads-u-font-weight--normal label-description">
-    You can find the decision date on your decision notice (the letter you got
-    in the mail from us).
-  </p>
-);
+export const dateOfDecisionHintText =
+  'You can find the decision date on your decision notice (the letter you got in the mail from us).';
