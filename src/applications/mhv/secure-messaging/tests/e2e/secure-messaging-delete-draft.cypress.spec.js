dc4ca06d06759c211bb85e3e8b08d670ae8ece00
haniasma <121971510+haniasma@users.noreply.github.com>
2023-05-08 15:14:34 -0400

    Replicate the sort message

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-delete-draft.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-delete-draft.cypress.spec.js
@@ -26,3 +26,55 @@ describe('Secure Messaging Delete Draft', () => {
     cy.axeCheck();
   });
 });
+
+describe(' Draft page Message Sort', () => {
+  beforeEach(() => {
+    const landingPage = new PatientInboxPage();
+    const site = new SecureMessagingSite();
+    site.login();
+    cy.reload(true);
+    landingPage.loadInboxMessages();
+    cy.get('.sidebar-navigation-messages-list-header > a');
+  });
+  it('Sort Inbox Messages from Newest to Oldest', () => {
+    cy.get('#sort-order-dropdown')
+      .shadow()
+      .find('select')
+      .should('contain', 'Newest');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+
+  it('Sort Inbox Messages from Oldest to Newest', () => {
+    cy.get('#sort-order-dropdown')
+      .shadow()
+      .find('select')
+      .select('ASC', { force: true })
+      .should('contain', 'newest');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+  it('Sort Inbox Messages from A to Z', () => {
+    cy.get('#sort-order-dropdown')
+      .shadow()
+      .find('select')
+      .select('sender-alpha-asc', { force: true })
+      .should('contain', 'A to Z');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+
+  it('Sort Inbox Messages from Z to A', () => {
+    cy.get('#sort-order-dropdown')
+      .shadow()
+      .find('select')
+      .select('sender-alpha-desc', { force: true })
+      .should('contain', 'Z to A');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+
+  afterEach(() => {
+    cy.get('[data-testid="sort-button"]').click({ force: true });
+  });
+});
