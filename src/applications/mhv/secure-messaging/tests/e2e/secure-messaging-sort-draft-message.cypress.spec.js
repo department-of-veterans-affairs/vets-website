8dc2c699ab0d586877c276e49c023d491c879114
haniasma <121971510+haniasma@users.noreply.github.com>
2023-05-10 12:43:43 -0400

    updated

---
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-sort-draft-message.cypress.spec.js
@@ -0,0 +1,66 @@
+import mockDraftMessages from './fixtures/drafts-response.json';
+import mockDraftResponse from './fixtures/message-draft-response.json';
+import PatientInboxPage from './pages/PatientInboxPage';
+import PatientInterstitialPage from './pages/PatientInterstitialPage';
+import PatientMessageDraftsPage from './pages/PatientMessageDraftsPage';
+import SecureMessagingSite from './sm_site/SecureMessagingSite';
+import mockThreadResponse from './fixtures/single-draft-response.json';
+
+describe(' Draft page Message Sort', () => {
+  beforeEach(() => {
+    const inboxPage = new PatientInboxPage();
+    const draftsPage = new PatientMessageDraftsPage();
+    const site = new SecureMessagingSite();
+    const patientInterstitialPage = new PatientInterstitialPage();
+    site.login();
+    inboxPage.loadInboxMessages();
+    cy.reload(true);
+    draftsPage.loadDraftMessages(mockDraftMessages, mockDraftResponse);
+    draftsPage.loadMessageDetails(mockDraftResponse, mockThreadResponse);
+    patientInterstitialPage.getContinueButton().click();
+    draftsPage.clickDeleteButton();
+    draftsPage.confirmDeleteDraft(mockDraftResponse);
+    inboxPage.verifyDeleteConfirmMessage();
+    cy.get('.sidebar-navigation-messages-list-header > a');
+  });
+  it('Sort Inbox Messages from Newest to Oldest', () => {
+    cy.get('#sort-order-dropdown')
+      .shadow()
+      .find('select')
+      .should('contain', 'Newest to oldest');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+
+  it('Sort Inbox Messages from Oldest to Newest', () => {
+    cy.get('#sort-order-dropdown')
+      .shadow()
+      .find('select')
+      .select('ASC', { force: true })
+      .should('contain', 'newest');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+  it('Sort Inbox Messages from A to Z', () => {
+    cy.get('#sort-order-dropdown')
+      .shadow()
+      .find('select')
+      .select('ASC', { force: true })
+      .should('contain', 'A to Z');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+
+  it('Sort Inbox Messages from Z to A', () => {
+    cy.get('#sort-order-dropdown')
+      .shadow()
+      .find('select')
+      .should('contain', 'Z to A');
+    cy.injectAxe();
+    cy.axeCheck();
+  });
+
+  afterEach(() => {
+    cy.get('[data-testid="sort-button"]').click({ force: true });
+  });
+});
