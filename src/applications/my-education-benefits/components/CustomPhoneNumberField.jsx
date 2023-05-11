f33f5fb43c71edee4c16a39f4e42c25a7b58a749
s-caso <sebastian.caso@accenturefederal.com>
2023-05-11 08:06:40 -0400

    add custom phone component

---
+++ src/applications/my-education-benefits/components/CustomPhoneNumberField.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import PhoneNumberWidget from 'platform/forms-system/src/js/widgets/PhoneNumberWidget';
+
+import { fetchDuplicateContactInfo, updateGlobalPhoneNumber } from '../actions';
+
+function CustomPhoneNumberField(props) {
+  function handleChange(event) {
+    props.fetchDuplicateContactInfo(event);
+    props.updateGlobalPhoneNumber(event);
+  }
+
+  return (
+    <>
+      <PhoneNumberWidget
+        {...props}
+        onChange={handleChange}
+        value={props.phoneNumber}
+      />
+    </>
+  );
+}
+
+CustomPhoneNumberField.propTypes = {
+  fetchDuplicateContactInfo: PropTypes.func,
+  updateGlobalEmail: PropTypes.func,
+  phoneNumber: PropTypes.string,
+};
+
+const mapStateToProps = state => ({
+  phoneNumber: state?.form?.data?.mobilePhone,
+  duplicatePhone: state?.data?.duplicatePhone,
+});
+
+const mapDispatchToProps = {
+  fetchDuplicateContactInfo,
+  updateGlobalPhoneNumber,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(CustomPhoneNumberField);
