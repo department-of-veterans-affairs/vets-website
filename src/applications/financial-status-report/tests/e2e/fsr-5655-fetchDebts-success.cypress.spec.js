7f059a1386fc63c8961977327e6b5e5f71befe93
Andrew Rodiek <andrew.rodiek@gmail.com>
2023-05-11 15:59:59 -0400

    (testing) cleanup - remove wizard nav

--- src/applications/financial-status-report/tests/e2e/fsr-5655-fetchDebts-success.cypress.spec.js
+++ src/applications/financial-status-report/tests/e2e/fsr-5655-fetchDebts-success.cypress.spec.js
@@ -1,6 +1,9 @@
+import { WIZARD_STATUS_COMPLETE } from 'applications/static-pages/wizard';
 import manifest from '../../manifest.json';
-import { navigateToDebtSelection } from './fixtures/helpers';
 import mockUser from './fixtures/mocks/mockUser.json';
+import saveInProgressData from './fixtures/mocks/saveInProgress.json';
+
+import { WIZARD_STATUS } from '../../wizard/constants';
 
 describe('Fetch Debts Successfully', () => {
   before(() => {
@@ -70,30 +73,8 @@ describe('Fetch Debts Successfully', () => {
       ],
     });
 
-    cy.intercept('GET', '/v0/in_progress_forms/5655', {
-      formData: {
-        personalIdentification: { ssn: '4437', fileNumber: '4437' },
-        personalData: {
-          veteranFullName: { first: 'Mark', last: 'Webb', suffix: 'Jr.' },
-          address: {
-            street: '123 Faker Street',
-            city: 'Bogusville',
-            state: 'GA',
-            country: 'USA',
-            postalCode: '30058',
-          },
-          telephoneNumber: '4445551212',
-          emailAddress: 'test2@test1.net',
-          dateOfBirth: '1950-10-04',
-        },
-        income: [{ veteranOrSpouse: 'VETERAN' }],
-      },
-      metadata: {
-        version: 0,
-        prefill: true,
-        returnUrl: '/veteran-information',
-      },
-    });
+    cy.intercept('GET', '/v0/in_progress_forms/5655', saveInProgressData);
+    sessionStorage.setItem(WIZARD_STATUS, WIZARD_STATUS_COMPLETE);
 
     cy.visit(manifest.rootUrl);
   });
@@ -108,7 +89,15 @@ describe('Fetch Debts Successfully', () => {
     });
   });
   it('Successful API Response', () => {
-    navigateToDebtSelection();
+    // navigateToDebtSelection();
+
+    cy.get('a.vads-c-action-link--green')
+      .first()
+      .click();
+
+    cy.findAllByText(/continue/i, { selector: 'button' })
+      .first()
+      .click();
 
     cy.get('.debt-card').should('have.length', 2);
 
