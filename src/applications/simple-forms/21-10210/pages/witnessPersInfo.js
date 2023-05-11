55047cb0c90181a57c016797d9f282c30428030e
Tze-chiu Lei <tze@adhocteam.us>
2023-05-10 19:21:28 -0700

    Make 21-10210 witness-relationship fields conditionally required

--- src/applications/simple-forms/21-10210/pages/witnessPersInfo.js
+++ src/applications/simple-forms/21-10210/pages/witnessPersInfo.js
@@ -9,25 +9,58 @@ export default {
     witnessRelationshipToVeteran: {
       'ui:description': (
         <p className="vads-u-margin-bottom--0 vads-u-margin-top--4">
-          What is your relationship to the Veteran?
+          What is your relationship to the Veteran?{' '}
+          <span className="form-required-span">(* Required)</span>
           <br />
           Check all that apply.
         </p>
       ),
-      'ui:help': 'Check all the apply',
+      // checkboxes' '(* Required)' spans hidden via styling
       'served-with': {
         'ui:title': 'Served with Veteran',
+        'ui:required': formData =>
+          !formData.witnessRelationshipToVeteran['family-or-friend'] &&
+          !formData.witnessRelationshipToVeteran['coworker-or-supervisor'] &&
+          !formData.witnessOtherRelationshipToVeteran,
+        'ui:errorMessages': {
+          required:
+            'Please select at least one option, or provide an unlisted relationship in textbox below',
+        },
       },
       'family-or-friend': {
         'ui:title': 'Family/Friend of Veteran',
+        'ui:required': formData =>
+          !formData.witnessRelationshipToVeteran['served-with'] &&
+          !formData.witnessRelationshipToVeteran['coworker-or-supervisor'] &&
+          !formData.witnessOtherRelationshipToVeteran,
+        'ui:errorMessages': {
+          required:
+            'Please select at least one option, or provide an unlisted relationship in textbox below',
+        },
       },
       'coworker-or-supervisor': {
         'ui:title': 'Coworker/Supervisor of Veteran',
+        'ui:required': formData =>
+          !formData.witnessRelationshipToVeteran['served-with'] &&
+          !formData.witnessRelationshipToVeteran['family-or-friend'] &&
+          !formData.witnessOtherRelationshipToVeteran,
+        'ui:errorMessages': {
+          required:
+            'Please select at least one option, or provide an unlisted relationship in textbox below',
+        },
       },
     },
     witnessOtherRelationshipToVeteran: {
       'ui:title':
         'If your relationship with the Veteran is not listed, you can write it here (255 characters maximum)',
+      'ui:required': formData =>
+        !formData.witnessRelationshipToVeteran['served-with'] &&
+        !formData.witnessRelationshipToVeteran['family-or-friend'] &&
+        !formData.witnessRelationshipToVeteran['coworker-or-supervisor'],
+      'ui:errorMessages': {
+        required:
+          'Please provide an unlisted relationship here, or select at least one option above',
+      },
     },
   },
   schema: {
