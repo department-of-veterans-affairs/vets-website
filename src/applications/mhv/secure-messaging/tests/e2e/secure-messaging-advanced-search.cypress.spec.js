e281b5e48ff93f37d58d780dc7c4b51002f503be
fazilqa <fazil.mammadov@bylight.com>
2023-05-12 11:26:32 -0700

    minor updates - 2

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-advanced-search.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-advanced-search.cypress.spec.js
@@ -8,46 +8,50 @@ import draftResponce from './fixtures/drafts-response.json';
 import PatientMessageDraftsPage from './pages/PatientMessageDraftsPage';
 
 describe(manifest.appName, () => {
-  it('Advanced Search Axe Check', () => {
-    const draftAdvancedSearch = new PatientMessageDraftsPage();
-    const site = new SecureMessagingSite();
-    site.login();
-    const landingPage = new PatientInboxPage();
-    landingPage.loadInboxMessages();
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/folders/-2',
-      mockDraftsFolder,
-    ).as('basicSearchRequestDraftsMeta');
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/folders/-2/threads?**',
-      draftResponce,
-    );
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/folders/-2/messages?per_page=-1',
-      mockMessages,
-    ).as('basicSearchRequestDrafts');
-    cy.get('[data-testid="drafts-sidebar"]').click();
-    cy.intercept(
-      'POST',
-      '/my_health/v1/messaging/folders/*/search',
-      mockDraftsSearchMessages,
-    ).as('advancedSearchRequest');
-    draftAdvancedSearch.openAdvancedSearch();
-    draftAdvancedSearch.selectAdvancedSearchCategory();
-    draftAdvancedSearch.submitSearchButton();
-
+  describe('Advanced search in Drafts', () => {
+    before(() => {
+      const draftAdvancedSearch = new PatientMessageDraftsPage();
+      const site = new SecureMessagingSite();
+      site.login();
+      const landingPage = new PatientInboxPage();
+      landingPage.loadInboxMessages();
+      cy.intercept(
+        'GET',
+        '/my_health/v1/messaging/folders/-2',
+        mockDraftsFolder,
+      ).as('basicSearchRequestDraftsMeta');
+      cy.intercept(
+        'GET',
+        '/my_health/v1/messaging/folders/-2/threads?**',
+        draftResponce,
+      );
+      cy.intercept(
+        'GET',
+        '/my_health/v1/messaging/folders/-2/messages?per_page=-1',
+        mockMessages,
+      ).as('basicSearchRequestDrafts');
+      cy.get('[data-testid="drafts-sidebar"]').click();
+      cy.intercept(
+        'POST',
+        '/my_health/v1/messaging/folders/*/search',
+        mockDraftsSearchMessages,
+      ).as('advancedSearchRequest');
+      draftAdvancedSearch.openAdvancedSearch();
+      draftAdvancedSearch.selectAdvancedSearchCategory();
+      draftAdvancedSearch.submitSearchButton();
+    });
     // Following assertion could be turned to the POM style
-
-    cy.get('[data-testid="message-list-item"]')
-      .should('contain', 'COVID')
-      .and('have.length', mockDraftsSearchMessages.data.length);
-    cy.get('[data-testid="search-message-folder-input-label"]')
-      .should('contain', '4')
-      .and('contain', 'Category: "covid"');
-    cy.injectAxe();
-    cy.axeCheck();
+    it('Check all messages contain the serached category', () => {
+      cy.get('[data-testid="message-list-item"]')
+        .should('contain', 'COVID')
+        .and('have.length', mockDraftsSearchMessages.data.length);
+      it('Check the search message label', function() {
+        cy.get('[data-testid="search-message-folder-input-label"]')
+          .should('contain', '4')
+          .and('contain', 'Category: "covid"');
+        cy.injectAxe();
+        cy.axeCheck();
+      });
+    });
   });
 });
