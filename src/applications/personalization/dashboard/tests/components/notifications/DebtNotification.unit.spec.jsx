bc554019122e1cc257afbf5e9271e46115dbb73e
Derrick Ellerbie <derrick.ellerbie@agile6.com>
2023-05-16 15:38:05 -0700

    Unit spec for My VA Notifications component

--- src/applications/personalization/dashboard/tests/components/notifications/DebtNotification.unit.spec.jsx
+++ src/applications/personalization/dashboard/tests/components/notifications/DebtNotification.unit.spec.jsx
@@ -10,10 +10,10 @@ describe('<DebtNotification />', () => {
       id: '12345',
       type: 'abc',
       attributes: {
-        createdAt: '2013-07-16T19:00:00Z',
+        createdAt: '2023-05-14T12:00:00Z',
         dismissed: false,
         templateId: 'abc-xyz',
-        updatedAt: '2013-07-16T19:00:00Z',
+        updatedAt: '2023-05-14T12:00:00Z',
         vaProfileId: '1',
       },
     };
@@ -24,10 +24,9 @@ describe('<DebtNotification />', () => {
       />,
     );
 
-    expect(tree.getByTestId('dashboard-notification-alert')).to.not.be.null;
-    expect(tree.getByText('You have new debt.')).to.not.be.null;
-    expect(tree.getByText('Manage your VA debt')).to.not.be.null;
-    expect(tree.getByText('Tuesday, July 16, 2013')).to.not.be.null;
-    tree.unmount();
+    expect(tree.getByTestId('dashboard-notification-alert')).to.exist;
+    expect(tree.getByText('You have new debt.')).to.exist;
+    expect(tree.getByText('Manage your VA debt')).to.exist;
+    expect(tree.getByText('Sunday, May 14, 2023')).to.exist;
   });
 });
