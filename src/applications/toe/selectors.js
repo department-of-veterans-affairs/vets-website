a9669ff84f020d79f8a6b58cfc52f2f6e24e8ec7
bradbergeron-us <bradley.bergeron@accenturefederal.com>
2023-05-16 13:56:58 -0400

    Updates Headline text on introduction page component behind showMebEnhancements06 feature flag on TOE App

--- src/applications/toe/selectors.js
+++ src/applications/toe/selectors.js
@@ -17,5 +17,9 @@ export const getAppData = state => ({
   showMebEnhancements: !!toggleValues(state)[
     FEATURE_FLAG_NAMES.showMebEnhancements
   ],
+  // Add the new feature flag: showMebEnhancements
+  showMebEnhancements06: !!toggleValues(state)[
+    FEATURE_FLAG_NAMES.showMebEnhancements
+  ],
   user: state.user || {},
 });
