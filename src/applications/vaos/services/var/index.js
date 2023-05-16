c96c7b3c870aa4cd8e14db5788073b85019685d9
Ryan Shaw <ryan.shaw@adhocteam.us>
2023-05-16 15:28:31 -0700

    Comments out getCommunityCare
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/services/var/index.js
+++ src/applications/vaos/services/var/index.js
@@ -102,11 +102,11 @@ export function getFacilitiesBySystemAndTypeOfCare(
   ).then(parseApiList);
 }
 
-export function getCommunityCare(typeOfCare) {
-  return apiRequestWithUrl(
-    `/vaos/v0/community_care/eligibility/${typeOfCare}`,
-  ).then(parseApiObject);
-}
+// export function getCommunityCare(typeOfCare) {
+//   return apiRequestWithUrl(
+//     `/vaos/v0/community_care/eligibility/${typeOfCare}`,
+//   ).then(parseApiObject);
+// }
 
 export function checkPastVisits(
   systemId,
