d5125a4f3e8ba5088cdd203af73c122acf2fc26e
fazilqa <fazil.mammadov@bylight.com>
2023-05-16 09:08:05 -0700

    minor updates - 3

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-advanced-search.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-advanced-search.cypress.spec.js
@@ -1,7 +1,7 @@
 import manifest from '../../manifest.json';
 import SecureMessagingSite from './sm_site/SecureMessagingSite';
 import PatientInboxPage from './pages/PatientInboxPage';
-import mockMessages from './fixtures/drafts-search-results.json';
+// import mockMessages from './fixtures/drafts-search-results.json';
 import mockDraftsSearchMessages from './fixtures/drafts-search-COVID-results.json';
 import mockDraftsFolder from './fixtures/folder-drafts-metadata.json';
 import draftResponce from './fixtures/drafts-response.json';
@@ -19,23 +19,23 @@ describe(manifest.appName, () => {
         'GET',
         '/my_health/v1/messaging/folders/-2',
         mockDraftsFolder,
-      ).as('basicSearchRequestDraftsMeta');
+      );
       cy.intercept(
         'GET',
         '/my_health/v1/messaging/folders/-2/threads?**',
         draftResponce,
       );
-      cy.intercept(
-        'GET',
-        '/my_health/v1/messaging/folders/-2/messages?per_page=-1',
-        mockMessages,
-      ).as('basicSearchRequestDrafts');
+      // cy.intercept(
+      //   'GET',
+      //   '/my_health/v1/messaging/folders/-2/messages?per_page=-1',
+      //   mockMessages,
+      // ).as('basicSearchRequestDrafts');
       cy.get('[data-testid="drafts-sidebar"]').click();
       cy.intercept(
         'POST',
         '/my_health/v1/messaging/folders/*/search',
         mockDraftsSearchMessages,
-      ).as('advancedSearchRequest');
+      );
       draftAdvancedSearch.openAdvancedSearch();
       draftAdvancedSearch.selectAdvancedSearchCategory();
       draftAdvancedSearch.submitSearchButton();
