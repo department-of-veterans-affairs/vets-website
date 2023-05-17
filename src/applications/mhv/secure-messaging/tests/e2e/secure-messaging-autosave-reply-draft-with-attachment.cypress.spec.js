a1698081d84ed24893905c42d0f645d74194c021
haniasma <121971510+haniasma@users.noreply.github.com>
2023-05-17 12:25:32 -0400

    fixed

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-autosave-reply-draft-with-attachment.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-autosave-reply-draft-with-attachment.cypress.spec.js
@@ -1,62 +1,37 @@
 import SecureMessagingSite from './sm_site/SecureMessagingSite';
-import mockDraftMessages from './fixtures/drafts-response.json';
-import mockDraftResponse from './fixtures/message-draft-response.json';
-import mockThreadResponse from './fixtures/single-draft-response.json';
+import PatientMessageDetailsPage from './pages/PatientMessageDetailsPage';
 import PatientInboxPage from './pages/PatientInboxPage';
 import PatientInterstitialPage from './pages/PatientInterstitialPage';
-import PatientComposePage from './pages/PatientComposePage';
-import PatientMessageDraftsPage from './pages/PatientMessageDraftsPage';
-import PatientMessageDetailsPage from './pages/PatientMessageDetailsPage';
+import PatientReplyPage from './pages/PatientReplyPage';
 import mockMessages from './fixtures/messages-response.json';
+import PatientComposePage from './pages/PatientComposePage';
 
 describe('autosave Reply draft with attachment', () => {
-  it('Autosave Reply Draft', () => {
+  it(' Autosave Reply Draft', () => {
     const landingPage = new PatientInboxPage();
-    const composePage = new PatientComposePage();
-    const site = new SecureMessagingSite();
-    const inboxPage = new PatientInboxPage();
-    const draftsPage = new PatientMessageDraftsPage();
     const messageDetailsPage = new PatientMessageDetailsPage();
-
     const patientInterstitialPage = new PatientInterstitialPage();
+    const replyPage = new PatientReplyPage();
+    const site = new SecureMessagingSite();
+    const composePage = new PatientComposePage();
     site.login();
-    inboxPage.loadInboxMessages();
     const messageDetails = landingPage.getNewMessageDetails();
     landingPage.loadInboxMessages(mockMessages, messageDetails);
     messageDetailsPage.loadMessageDetails(messageDetails);
     messageDetailsPage.loadReplyPageDetails(messageDetails);
-    draftsPage.loadDraftMessages(mockDraftMessages, mockDraftResponse);
-    cy.reload();
-    draftsPage.loadMessageDetails(mockDraftResponse, mockThreadResponse);
-    cy.reload();
     patientInterstitialPage.getContinueButton().click();
-    composePage
-      .getMessageBodyField()
-      .type('Testing Autosave Drafts with Attachments');
-    cy.realPress(['Enter']);
-    composePage.attachMessageFromFile('sample_docx.docx');
-
-    mockDraftResponse.data.attributes.body =
-      'ststASertTesting Autosave Drafts with Attachments\nTesting Autosave Drafts with Attachments\n';
-    cy.intercept(
-      'PUT',
-      `/my_health/v1/messaging/message_drafts/${
-        mockDraftResponse.data.attributes.messageId
-      }`,
-      mockDraftResponse,
-    ).as('saveDraftwithAttachment');
-    cy.wait('@saveDraftwithAttachment', { timeout: 8500 });
-    cy.get('@saveDraftwithAttachment')
-      .its('request.body')
-      .should('deep.equal', {
-        recipientId: mockDraftResponse.data.attributes.recipientId,
-        category: mockDraftResponse.data.attributes.category,
-        body:
-          'ststASertTesting Autosave Drafts with Attachments\nTesting Autosave Drafts with Attachments\n',
-        subject: mockDraftResponse.data.attributes.subject,
-      });
-    cy.contains('Your message was saved');
+    const testMessageBody = 'Test message body';
+    replyPage.getMessageBodyField().type(testMessageBody, { force: true });
     cy.injectAxe();
     cy.axeCheck();
+    composePage.attachMessageFromFile('test_video.mp4');
+    composePage.verifyAttachmentErrorMessage(
+      "We can't attach this file type. Try attaching a DOC, JPG, PDF, PNG, RTF, TXT, or XLS.",
+    );
+    composePage.attachMessageFromFile('empty.txt');
+    composePage.verifyAttachmentErrorMessage(
+      'Your file is empty. Try attaching a different file.',
+    );
+    replyPage.saveReplyDraft(messageDetails, testMessageBody);
   });
 });
