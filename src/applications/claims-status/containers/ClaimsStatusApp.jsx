249f51d851c4ed2c3d05f9832a8df0ccd59450b3
jerekshoe <jerek.shoemaker@adhocteam.us>
2023-05-22 13:11:56 -0500

    Adding feature toggle override

--- src/applications/claims-status/containers/ClaimsStatusApp.jsx
+++ src/applications/claims-status/containers/ClaimsStatusApp.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
 
 import backendServices from 'platform/user/profile/constants/backendServices';
 import { RequiredLoginView } from 'platform/user/authorization/components/RequiredLoginView';
+import environment from 'platform/utilities/environment';
 
 import { setLastPage } from '../actions';
 import ClaimsAppealsUnavailable from '../components/ClaimsAppealsUnavailable';
+import VaButtonGroupSegmented from '../components/VaButtonGroupSegmented';
 import { isLoadingFeatures } from '../selectors';
 
 // This needs to be a React component for RequiredLoginView to pass down
@@ -16,6 +18,52 @@ function AppContent({ children, featureFlagsLoading, isDataAvailable }) {
   const canUseApp =
     isDataAvailable === true || typeof isDataAvailable === 'undefined';
   const isAppReady = canUseApp && !featureFlagsLoading;
+  const defaultFlipperOverride =
+    sessionStorage.getItem('cstFlipperOverride') || 'featureToggle';
+  const defaultShowFlipperOverrideState =
+    sessionStorage.getItem('showFlipperOverride') === 'true' || false;
+  const [selectedOption, setSelectedOption] = useState(defaultFlipperOverride);
+  const [showFlipperOverride, setShowFlipperOverride] = useState(
+    defaultShowFlipperOverrideState,
+  );
+
+  const buttons = [
+    { label: 'EVSS', value: 'evss' },
+    { label: 'Lighthouse', value: 'lighthouse' },
+    { label: 'Feature toggle', value: 'featureToggle' },
+  ];
+
+  const canShowFlipperOverride = () => {
+    const canShow = !environment.isProduction();
+    const shouldShow = showFlipperOverride;
+    return canShow && shouldShow;
+  };
+
+  const handleKeydown = event => {
+    // `metaKey` is associated with the 'Command' key on macOS
+    const ctrlPressed = event.ctrlKey || event.metaKey;
+
+    if (ctrlPressed && event.shiftKey && event.key === 'k') {
+      setShowFlipperOverride(prevState => {
+        const nextState = !prevState;
+        sessionStorage.setItem('showFlipperOverride', nextState);
+        return nextState;
+      });
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeydown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeydown);
+    };
+  }, []);
+
+  const onOptionClick = option => {
+    setSelectedOption(option);
+    sessionStorage.setItem('cstFlipperOverride', option);
+  };
 
   if (!isAppReady) {
     return (
@@ -31,7 +79,21 @@ function AppContent({ children, featureFlagsLoading, isDataAvailable }) {
   return (
     <div className="claims-status-content">
       {!canUseApp && <ClaimsAppealsUnavailable />}
-      {isAppReady && <>{children}</>}
+      {isAppReady && (
+        <>
+          <div className="row">
+            <div className="usa-width-two-thirds medium-8 columns" />
+            {canShowFlipperOverride() && (
+              <VaButtonGroupSegmented
+                options={buttons}
+                selected={selectedOption}
+                onOptionClick={onOptionClick}
+              />
+            )}
+          </div>
+          {children}
+        </>
+      )}
     </div>
   );
 }
