ce68fe476ba47784b189e670b73b6189d35930f2
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2023-05-22 08:40:42 -0400

    WIP

--- src/platform/utilities/preview-environment/start.sh
+++ src/platform/utilities/preview-environment/start.sh
@@ -27,6 +27,14 @@ git clone --depth 1 https://github.com/department-of-veterans-affairs/content-bu
 
 git clone -b pe-custom-api --single-branch https://github.com/department-of-veterans-affairs/vets-website.git
 
+echo "set yarn to allow self-signed cert for install"
+yarn config set "strict-ssl" false
+
+# Build and watch vets-website
+echo "Install, build, and watch vets-website"
+cd vets-website
+yarn install
+yarn watch --env buildtype=localhost --env api=http://vets-api-web:3004 &
 
 echo "Download dev content-build to website dir"
 
@@ -46,14 +54,7 @@ mkdir -p content-build/.cache/localhost/drupal
 echo "untar the build into content-build/.cache/localhost/drupal"
 tar -xf vagovdev_dd03cdd3eb98417b247b1a61d54651a1.tar.bz2 -C content-build/.cache/localhost/drupal
 
-echo "set yarn to allow self-signed cert for install"
-yarn config set "strict-ssl" false
 
-# Build and watch vets-website
-echo "Install, build, and watch vets-website"
-cd vets-website
-yarn install
-yarn watch --env buildtype=localhost --env api=http://vets-api-web:3004 &
 
 # Serve the content-build
 echo "Install and serve content-build"
