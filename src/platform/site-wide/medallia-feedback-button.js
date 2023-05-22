23bacb756b978d0ff7ca7372a35d1fc5c44780c2
aubreyarcangel <aarcangel@gmail.com>
2023-05-22 13:51:19 -0400

    detecting modal-live-form

--- src/platform/site-wide/medallia-feedback-button.js
+++ src/platform/site-wide/medallia-feedback-button.js
@@ -1,60 +1,105 @@
+// window.addEventListener('DOMContentLoaded', () => {
+//   /**
+//    * Moves #kampyleButtonContainer (the container for the Feedback button
+//    * powered by Medallia) from the end of <body> to the end of <main> so
+//    * that users can tab to it more quickly.
+//    * Before, users had to tab through all the links in the footer to get
+//    * to the button.
+//    */
+//   const body = document.querySelector('body');
+//   const main = document.querySelector('main');
+//   const observeConfig = { attributes: false, childList: true };
+//   let feedbackButtonContainer;
+
+//   /**
+//    * Set an observer to detect when #kampyleButtonContainer is added to
+//    * the DOM, then move it.
+//    */
+//   const bodyObserver = new MutationObserver(bodyMutationsList => {
+//     bodyMutationsList.forEach(bodyMutation => {
+//       if (bodyMutation.addedNodes.length > 0) {
+//         bodyMutation.addedNodes.forEach(bodyNode => {
+//           if (bodyNode.id === 'kampyleButtonContainer') {
+//             feedbackButtonContainer = document.getElementById(
+//               'kampyleButtonContainer',
+//             );
+//             feedbackButtonContainer.remove();
+//             main.append(feedbackButtonContainer);
+
+//             /**
+//              * When the Feedback button is clicked on, #nebula_div_btn is removed from the DOM.
+//              * When the form is closed, it is added back.
+//              * Set an observer to detect when #nebula_div_btn is added back then set the
+//              * focus on it again.
+//              */
+//             const buttonContainerObserver = new MutationObserver(
+//               buttonContainerMutationsList => {
+//                 buttonContainerMutationsList.forEach(containerMutation => {
+//                   if (containerMutation.addedNodes.length > 0) {
+//                     containerMutation.addedNodes.forEach(containerNode => {
+//                       if (containerNode.id === 'nebula_div_btn') {
+//                         document.getElementById('nebula_div_btn').focus();
+//                       }
+//                     });
+//                   }
+//                 });
+//               },
+//             );
+
+//             buttonContainerObserver.observe(
+//               feedbackButtonContainer,
+//               observeConfig,
+//             );
+//           }
+//         });
+//       }
+//     });
+//   });
+
+//   bodyObserver.observe(body, observeConfig);
+// });
+
 window.addEventListener('DOMContentLoaded', () => {
-  /**
-   * Moves #kampyleButtonContainer (the container for the Feedback button
-   * powered by Medallia) from the end of <body> to the end of <main> so
-   * that users can tab to it more quickly.
-   * Before, users had to tab through all the links in the footer to get
-   * to the button.
-   */
-  const body = document.querySelector('body');
-  const main = document.querySelector('main');
-  const observeConfig = { attributes: false, childList: true };
-  let feedbackButtonContainer;
+  function addAriaLabel(spanElement) {
+    // Get the text of the span element.
+    const text = spanElement.innerHTML;
 
-  /**
-   * Set an observer to detect when #kampyleButtonContainer is added to
-   * the DOM, then move it.
-   */
-  const bodyObserver = new MutationObserver(bodyMutationsList => {
-    bodyMutationsList.forEach(bodyMutation => {
-      if (bodyMutation.addedNodes.length > 0) {
-        bodyMutation.addedNodes.forEach(bodyNode => {
-          if (bodyNode.id === 'kampyleButtonContainer') {
-            feedbackButtonContainer = document.getElementById(
-              'kampyleButtonContainer',
-            );
-            feedbackButtonContainer.remove();
-            main.append(feedbackButtonContainer);
+    // Find the index of the text "988.".
+    const index = text.indexOf('988.');
 
-            /**
-             * When the Feedback button is clicked on, #nebula_div_btn is removed from the DOM.
-             * When the form is closed, it is added back.
-             * Set an observer to detect when #nebula_div_btn is added back then set the
-             * focus on it again.
-             */
-            const buttonContainerObserver = new MutationObserver(
-              buttonContainerMutationsList => {
-                buttonContainerMutationsList.forEach(containerMutation => {
-                  if (containerMutation.addedNodes.length > 0) {
-                    containerMutation.addedNodes.forEach(containerNode => {
-                      if (containerNode.id === 'nebula_div_btn') {
-                        document.getElementById('nebula_div_btn').focus();
-                      }
-                    });
-                  }
-                });
-              },
+    // If the text "988." is found, add an aria label to the span element.
+    if (index > -1) {
+      const ariaLabel = '9 8 8';
+      const spanText = `${text.slice(
+        0,
+        index,
+      )}<span aria-label="${ariaLabel}">988.</span>${text.slice(index + 5)}`;
+      // eslint-disable-next-line no-param-reassign
+      spanElement.innerHTML = spanText;
+    }
+  }
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      if (mutation.addedNodes.length > 0) {
+        const addedNode = mutation.addedNodes[0];
+        if (addedNode.id === 'MDigitalLightboxWrapper') {
+          const medalliaForm = addedNode.querySelector('.modal-live-form');
+          if (medalliaForm) {
+            const vclNumber = document.querySelector(
+              '#liveForm > div > div.live-form-content > div.modal-live-form.ng-scope > div > div > div:nth-child(1) > div > div > p > ul > li:nth-child(1) > span',
             );
-
-            buttonContainerObserver.observe(
-              feedbackButtonContainer,
-              observeConfig,
-            );
+            // eslint-disable-next-line no-console
+            console.log('Medallia form found!');
+            addAriaLabel(vclNumber);
+            observer.disconnect();
           }
-        });
+        }
       }
     });
   });
 
-  bodyObserver.observe(body, observeConfig);
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
 });
