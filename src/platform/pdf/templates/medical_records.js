764b9fd37688730e1ce5dd79289ebc53474cb1ef
Adrian Rollett <acrollet@gmail.com>
2023-05-22 17:01:22 -0600

    Improve readability of generation method

--- src/platform/pdf/templates/medical_records.js
+++ src/platform/pdf/templates/medical_records.js
@@ -8,26 +8,7 @@ import {
 } from './utils';
 import registerFonts from '../registerFonts';
 
-const generate = async data => {
-  const doc = createAccessibleDoc(data.title);
-
-  await registerFonts(doc, [
-    'Bitter-Bold',
-    'Bitter-Regular',
-    'SourceSansPro-Bold',
-    'SourceSansPro-Regular',
-  ]);
-
-  doc.addPage({
-    margins: {
-      top: 40,
-      bottom: 40,
-      left: 20,
-      right: 20,
-    },
-  });
-
-  // Introduction section.
+const generateIntroductionContent = async (doc, data) => {
   const introductionContent = [
     doc.struct('H1', () => {
       doc
@@ -54,128 +35,131 @@ const generate = async data => {
     introductionContent,
   );
   doc.addStructure(introduction);
+};
 
-  // Details section.
-  if (data.details) {
-    const details = doc.struct('Sect', {
-      title: 'Details',
-    });
-    doc.addStructure(details);
-    if (data.details.header) {
-      details.add(
-        doc.struct('H2', () => {
-          doc
-            .font('Bitter-Bold')
-            .fontSize(24)
-            .text(data.details.header, { x: 30, paragraphGap: 16 });
-        }),
-      );
-    }
-    const detailsItemsCount = data.details.items.length;
-    if (detailsItemsCount > 0) {
-      data.details.items.forEach(item => {
-        const paragraphOptions = { lineGap: 0 };
-        let titleText = item.title;
-        if (item.inline === true) {
-          paragraphOptions.continued = true;
-          titleText += ': ';
-        } else {
-          titleText += ' ';
-        }
-        details.add(
-          doc.struct('P', () => {
-            doc
-              .font('SourceSansPro-Bold')
-              .fontSize(16)
-              .text(titleText, 30, doc.y, paragraphOptions);
-            doc
-              .font('SourceSansPro-Regular')
-              .fontSize(16)
-              .text(`${item.value}`);
-          }),
-        );
-      });
-    }
-    details.end();
+const generateDetailsContent = async (doc, data) => {
+  const details = doc.struct('Sect', {
+    title: 'Details',
+  });
+  doc.addStructure(details);
+  if (data.details.header) {
+    details.add(
+      doc.struct('H2', () => {
+        doc
+          .font('Bitter-Bold')
+          .fontSize(24)
+          .text(data.details.header, { x: 30, paragraphGap: 16 });
+      }),
+    );
   }
-
-  // Results section.
-  if (data.results) {
-    const results = doc.struct('Sect', {
-      title: 'Results',
-    });
-    doc.addStructure(results);
-    if (data.results.header) {
-      results.add(
-        doc.struct('H2', () => {
-          doc
-            .font('Bitter-Bold')
-            .fontSize(24)
-            .text(data.results.header, 34, doc.y, { paragraphGap: 20 });
-        }),
-      );
-    }
-    let initialBlock = true;
-    data.results.items.forEach((item, idx) => {
-      // Insert a pagebreak if the next block will not fit on the current page,
-      // taking the footer height into account.
-      const blockHeight = getTestResultBlockHeight(doc, item, initialBlock);
-      if (doc.y + blockHeight > 750) {
-        initialBlock = true;
-        doc.addPage();
-      } else if (idx > 0) {
-        initialBlock = false;
-        results.add(
-          doc.struct('NonStruct', () => {
-            addHorizontalRule(doc, 30, 0.5);
-          }),
-        );
+  const detailsItemsCount = data.details.items.length;
+  if (detailsItemsCount > 0) {
+    data.details.items.forEach(item => {
+      const paragraphOptions = { lineGap: 0 };
+      let titleText = item.title;
+      if (item.inline === true) {
+        paragraphOptions.continued = true;
+        titleText += ': ';
+      } else {
+        titleText += ' ';
       }
-
-      results.add(
-        doc.struct('H3', () => {
+      details.add(
+        doc.struct('P', () => {
           doc
             .font('SourceSansPro-Bold')
-            .fontSize(18)
-            .text(item.header, 34, doc.y, { paragraphGap: 10 });
+            .fontSize(16)
+            .text(titleText, 30, doc.y, paragraphOptions);
+          doc
+            .font('SourceSansPro-Regular')
+            .fontSize(16)
+            .text(`${item.value}`);
         }),
       );
-
-      item.items.forEach(resultItem => {
-        const paragraphOptions = { lineGap: 0 };
-        let titleText = resultItem.title;
-        if (resultItem.inline === true) {
-          paragraphOptions.continued = true;
-          titleText += ': ';
-        } else {
-          titleText += ' ';
-        }
-        results.add(
-          doc.struct('P', () => {
-            doc
-              .font('SourceSansPro-Bold')
-              .fontSize(16)
-              .text(titleText, 44, doc.y, paragraphOptions);
-            doc
-              .font('SourceSansPro-Regular')
-              .fontSize(16)
-              .text(`${resultItem.value}`);
-          }),
-        );
-      });
     });
-    results.end();
   }
+  details.end();
+};
 
-  // Add header & footer.
+const generateResultsContent = async (doc, data) => {
+  const results = doc.struct('Sect', {
+    title: 'Results',
+  });
+  doc.addStructure(results);
+  if (data.results.header) {
+    results.add(
+      doc.struct('H2', () => {
+        doc
+          .font('Bitter-Bold')
+          .fontSize(24)
+          .text(data.results.header, 34, doc.y, { paragraphGap: 20 });
+      }),
+    );
+  }
+  let initialBlock = true;
+  data.results.items.forEach((item, idx) => {
+    // Insert a pagebreak if the next block will not fit on the current page,
+    // taking the footer height into account.
+    const blockHeight = getTestResultBlockHeight(doc, item, initialBlock);
+    if (doc.y + blockHeight > 750) {
+      initialBlock = true;
+      doc.addPage();
+    } else if (idx > 0) {
+      initialBlock = false;
+      results.add(
+        doc.struct('NonStruct', () => {
+          addHorizontalRule(doc, 30, 0.5);
+        }),
+      );
+    }
+
+    results.add(
+      doc.struct('H3', () => {
+        doc
+          .font('SourceSansPro-Bold')
+          .fontSize(18)
+          .text(item.header, 34, doc.y, { paragraphGap: 10 });
+      }),
+    );
+
+    item.items.forEach(resultItem => {
+      const paragraphOptions = { lineGap: 0 };
+      let titleText = resultItem.title;
+      if (resultItem.inline === true) {
+        paragraphOptions.continued = true;
+        titleText += ': ';
+      } else {
+        titleText += ' ';
+      }
+      results.add(
+        doc.struct('P', () => {
+          doc
+            .font('SourceSansPro-Bold')
+            .fontSize(16)
+            .text(titleText, 44, doc.y, paragraphOptions);
+          doc
+            .font('SourceSansPro-Regular')
+            .fontSize(16)
+            .text(`${resultItem.value}`);
+        }),
+      );
+    });
+  });
+  results.end();
+};
+
+const generateHeaderAndFooterContent = async (doc, data) => {
   const pages = doc.bufferedPageRange();
   for (let i = 0; i < pages.count; i += 1) {
     doc.switchToPage(i);
 
-    // Adjust page margins.
-    doc.page.margins.top = 0;
-    doc.page.margins.bottom = 0;
-    doc.page.margins.right = 16;
+    // Adjust page margins so that we can write in the header/footer area.
+    // eslint-disable-next-line no-param-reassign
+    doc.page.margins = {
+      top: 0,
+      bottom: 0,
+      left: 20,
+      right: 16,
+    };
 
     const header = doc.struct('Artifact', {
       type: 'Pagination',
@@ -227,6 +211,41 @@ const generate = async data => {
     footer.end();
     doc.addStructure(footer);
   }
+};
+
+const generate = async data => {
+  const doc = createAccessibleDoc(data.title);
+
+  await registerFonts(doc, [
+    'Bitter-Bold',
+    'Bitter-Regular',
+    'SourceSansPro-Bold',
+    'SourceSansPro-Regular',
+  ]);
+
+  doc.addPage({
+    margins: {
+      top: 40,
+      bottom: 40,
+      left: 20,
+      right: 20,
+    },
+  });
+
+  // Add content synchronously to ensure that reading order
+  // is left intact for screen reader users.
+
+  await generateIntroductionContent(doc, data);
+
+  if (data.details) {
+    await generateDetailsContent(doc, data);
+  }
+
+  if (data.results) {
+    await generateResultsContent(doc, data);
+  }
+
+  await generateHeaderAndFooterContent(doc, data);
 
   doc.flushPages();
   return doc;
