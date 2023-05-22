28c2b31e388d35046e7eb9b915a5d4d0b2cacb09
Ray Messina <rmessina10@gmail.com>
2023-05-22 03:27:07 -0500

    refactors platform/landing-pages/exportsFile

--- src/platform/landing-pages/exportsFile.js
+++ src/platform/landing-pages/exportsFile.js
@@ -1,7 +1,13 @@
-const devTemplate = require('./dev-template.ejs');
-const facilitySidebar = require('./facility-sidebar.json');
-const headerFooter = require('./header-footer-data.json');
+import facilitySidebar from './facility-sidebar.json';
+import headerFooter from './header-footer-data.json';
+import devTemplate from './dev-template.ejs';
 
-exports.devTemplate = devTemplate;
-exports.facilitySidebar = facilitySidebar;
-exports.headerFooter = headerFooter;
+// const devTemplate = require('./dev-template.ejs');
+// const facilitySidebar = require('./facility-sidebar.json');
+// const headerFooter = require('./header-footer-data.json');
+
+// exports.devTemplate = devTemplate;
+// exports.facilitySidebar = facilitySidebar;
+// exports.headerFooter = headerFooter;
+
+export { facilitySidebar, headerFooter, devTemplate };
