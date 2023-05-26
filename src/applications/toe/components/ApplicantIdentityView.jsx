08fd7fb967bc70353243d5067787f5a3c7a04f0b
Brad Bergeron <bradley.bergeron@accenturefederal.com>
2023-05-26 11:07:36 -0400

    SA-61736 TOE - VA Profile Integration - Identity Trait Fields In Custom View (#24195)
    * Wires up the new Identity view component for the Application Information Chapter of the form
    
    * Adds date format helper, updates schema and prop types, updates helpers to match prop types
    
    * Passes the formData Prop to the ApplicantIdentityView component.
    
    * Updates FormatDateString function and adds unit tests for ApplicantIdentityView component
    
    * Updates display of date to include Date of birth, opens links in new tab, updates test
    
    * Removes duplicate paragraph in component
    
    * Updates to form.js and helpers for after view component is connected to redux
    
    * Intermediate Commit testing feature flag hide logic
    
    * Updates unit test to pass data with mock store
    
    * Updates mapStateToProps to map from profile instead of formFields
    
    * Adds custom mapping for feature toggle in mapStateToProps
    
    * Update InitialState in unit test to reflect updated State mapping of Props
    
    * Reorganizes Imports in form.js
    
    * fixes Typo
    
    * Reorganizes imports
    
    * Removes extra whitespace in form.js
    
    * Fixes linting errors in test file

---
+++ src/applications/toe/components/ApplicantIdentityView.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { YOUR_PROFILE_URL, CHANGE_YOUR_NAME } from '../constants';
+
+function ordinalSuffix(num) {
+  const suffixes = ['th', 'st', 'nd', 'rd'];
+  const mod100 = num % 100;
+  return (
+    num + (suffixes[(mod100 - 20) % 10] || suffixes[mod100] || suffixes[0])
+  );
+}
+
+function formatDateString(dateString) {
+  const dateObj = new Date(dateString);
+  const year = dateObj.getUTCFullYear();
+  const month = dateObj.toLocaleString('en-US', {
+    month: 'long',
+    timeZone: 'UTC',
+  });
+  const day = ordinalSuffix(dateObj.getUTCDate());
+  return `${month} ${day}, ${year}`;
+}
+const ApplicantIdentityView = ({ userFullName, dateOfBirth }) => {
+  if (!userFullName || !dateOfBirth) {
+    return <></>;
+  }
+
+  const formattedDateOfBirth = formatDateString(dateOfBirth);
+  return (
+    <>
+      <h3>Review your personal information</h3>
+      <p>
+        We have this personal information on file for you. Any updates you make
+        will change the information for your education benefits only. If you
+        want to update your personal information for other VA benefits,{' '}
+        <a href={YOUR_PROFILE_URL} target="_blank" rel="noopener noreferrer">
+          update your information on your profile
+        </a>
+        .
+      </p>
+      <p>
+        <strong>Note:</strong> If you want to request that we change your name
+        or date of birth, you will need to send additional information. Learn
+        more on how to{' '}
+        <a href={CHANGE_YOUR_NAME} target="_blank" rel="noopener noreferrer">
+          change your legal name
+        </a>{' '}
+        on file with VA.
+      </p>
+      <h4>Your Personal Information</h4>
+      <p className="va-address-block">
+        {userFullName.first} {userFullName.middle} {userFullName.last}
+        <br />
+        Date of birth: {formattedDateOfBirth}
+      </p>
+    </>
+  );
+};
+
+ApplicantIdentityView.propTypes = {
+  dateOfBirth: PropTypes.string,
+  userFullName: PropTypes.shape({
+    first: PropTypes.string,
+    middle: PropTypes.string,
+    last: PropTypes.string,
+  }),
+};
+
+const mapStateToProps = state => ({
+  userFullName: state.user.profile?.userFullName,
+  dateOfBirth: state.user.profile?.dob,
+});
+
+export default connect(mapStateToProps)(ApplicantIdentityView);
