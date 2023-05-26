ea0620e459989c8c853fb56c2ab4048e4bf9504f
Adam Whitlock <adam.whitlock@adhocteam.us>
2023-05-26 11:54:54 -0600

    chore: clean up import CONTACTS

--- src/applications/personalization/profile/components/military-information/MilitaryInformation.jsx
+++ src/applications/personalization/profile/components/military-information/MilitaryInformation.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { some } from 'lodash';
 import { connect } from 'react-redux';
 
-import { CONTACTS } from '@department-of-veterans-affairs/component-library/Telephone';
+import { CONTACTS } from '@department-of-veterans-affairs/component-library';
 
 import DowntimeNotification, {
   externalServices,
