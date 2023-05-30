e1945cdd5b4ae289b83db8194333b95eab4d45a0
aubreyarcangel <aarcangel@gmail.com>
2023-05-30 16:43:06 -0400

    trying to find modal form

--- src/platform/site-wide/medallia-feedback-button.js
+++ src/platform/site-wide/medallia-feedback-button.js
@@ -59,52 +59,74 @@
 //   bodyObserver.observe(body, observeConfig);
 // });
 
+// window.addEventListener('DOMContentLoaded', () => {
+//   function addAriaLabel(spanElement) {
+//     // Get the text of the span element.
+//     const text = spanElement.innerHTML;
+
+//     // Find the index of the text "988.".
+//     const index = text.indexOf('988.');
+
+//     // If the text "988." is found, add an aria label to the span element.
+//     if (index > -1) {
+//       const ariaLabel = '9 8 8';
+//       const spanText = `${text.slice(
+//         0,
+//         index,
+//       )}<span aria-label="${ariaLabel}">988.</span>${text.slice(index + 5)}`;
+//       // eslint-disable-next-line no-param-reassign
+//       spanElement.innerHTML = spanText;
+//     }
+//   }
+//   const observer = new MutationObserver(mutations => {
+//     mutations.forEach(mutation => {
+//       if (mutation.addedNodes.length > 0) {
+//         const addedNode = mutation.addedNodes[0];
+//         if (addedNode.id === 'MDigitalLightboxWrapper') {
+//           // eslint-disable-next-line no-console
+//           console.log('MDigitalLightboxWrapper found!');
+//           const medalliaForm = addedNode.querySelector('.modal-live-form');
+//           if (medalliaForm) {
+//             const vclNumber = document
+//               .querySelector('.modal-live-form')
+//               .querySelector('.pageRepeater')
+//               .querySelector('.neb-component')
+//               .querySelector('ul')
+//               .querySelector('li:nth-child(1)')
+//               .querySelector('span');
+//             // eslint-disable-next-line no-console
+//             console.log('Medallia form found!');
+//             addAriaLabel(vclNumber);
+//             observer.disconnect();
+//           } else {
+//             // eslint-disable-next-line no-console
+//             console.log('Medallia form found!');
+//           }
+//         }
+//       }
+//     });
+//   });
+
+//   observer.observe(document.body, {
+//     childList: true,
+//     subtree: true,
+//   });
+// });
+
 window.addEventListener('DOMContentLoaded', () => {
-  function addAriaLabel(spanElement) {
-    // Get the text of the span element.
-    const text = spanElement.innerHTML;
-
-    // Find the index of the text "988.".
-    const index = text.indexOf('988.');
-
-    // If the text "988." is found, add an aria label to the span element.
-    if (index > -1) {
-      const ariaLabel = '9 8 8';
-      const spanText = `${text.slice(
-        0,
-        index,
-      )}<span aria-label="${ariaLabel}">988.</span>${text.slice(index + 5)}`;
-      // eslint-disable-next-line no-param-reassign
-      spanElement.innerHTML = spanText;
+  const mutationObserver = new MutationObserver(mutations => {
+    // Find the first div with classname .modal-live-form
+    const medalliaForm = mutations[0].addedNodes.find(node =>
+      node.classList.contains('modal-live-form'),
+    );
+    if (medalliaForm) {
+      // eslint-disable-next-line no-console
+      console.log('modal-live-form found!');
     }
-  }
-  const observer = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-      if (mutation.addedNodes.length > 0) {
-        const addedNode = mutation.addedNodes[0];
-        if (addedNode.id === 'MDigitalLightboxWrapper') {
-          // eslint-disable-next-line no-console
-          console.log('MDigitalLightboxWrapper found!');
-          const medalliaForm = addedNode.querySelector('.modal-live-form');
-          if (medalliaForm) {
-            const vclNumber = document
-              .querySelector('.modal-live-form')
-              .querySelector('.pageRepeater')
-              .querySelector('.neb-component')
-              .querySelector('ul')
-              .querySelector('li:nth-child(1)')
-              .querySelector('span');
-            // eslint-disable-next-line no-console
-            console.log('Medallia form found!');
-            addAriaLabel(vclNumber);
-            observer.disconnect();
-          }
-        }
-      }
-    });
   });
 
-  observer.observe(document.body, {
+  // Set the observer to watch for changes to the body
+  mutationObserver.observe(document.body, {
     childList: true,
     subtree: true,
   });
