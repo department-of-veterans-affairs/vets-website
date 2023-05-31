79a51151d7b9b9134e72f51fced1d539932bd4ab
Joseph Lee <joseph@cityfriends.tech>
2023-05-31 15:19:13 -0700

    Removed unnecessary fieldest and cahnged legend to h3

--- src/applications/financial-status-report/components/SpouseGrossMonthlyIncomeInput.jsx
+++ src/applications/financial-status-report/components/SpouseGrossMonthlyIncomeInput.jsx
@@ -124,40 +124,37 @@ const SpouseGrossMonthlyIncomeInput = props => {
 
   return (
     <form onSubmit={updateFormData}>
-      <fieldset className="vads-u-margin-y--2">
-        <legend className="schemaform-block-title">
-          Your spouse’s job at {employerName}
-        </legend>
-        <p className="vads-u-margin-bottom--0">
-          What’s your spouse’s gross <strong>monthly</strong> income at this
-          job?{' '}
-          <span className="required vads-u-color--secondary-dark">
-            (*Required)
-          </span>
-        </p>
-        <p className="formfield-subtitle">
-          You’ll find this in your spouse’s pay stub. It’s the amount of your
-          spouse’s pay before taxes and deductions.
-        </p>
-        <div className="input input-size-3">
-          <va-number-input
-            inputmode="numeric"
-            currency
-            id="gross-monthly-income"
-            data-testid="gross-monthly-income"
-            name="gross-monthly-income"
-            onInput={setNewGrossMonthlyIncome}
-            type="text"
-            value={grossMonthlyIncome.value}
-            required
-            error={
-              incomeError && grossMonthlyIncome.dirty
-                ? `Please enter a valid number.`
-                : ''
-            }
-          />
-        </div>
-      </fieldset>
+      <h3 className="schemaform-block-title vads-u-margin-top--6">
+        Your spouse’s job at {employerName}
+      </h3>
+      <p className="vads-u-margin-bottom--0">
+        What’s your spouse’s gross <strong>monthly</strong> income at this job?{' '}
+        <span className="required vads-u-color--secondary-dark">
+          (*Required)
+        </span>
+      </p>
+      <p className="formfield-subtitle">
+        You’ll find this in your spouse’s pay stub. It’s the amount of your
+        spouse’s pay before taxes and deductions.
+      </p>
+      <div className="input input-size-3">
+        <va-number-input
+          inputmode="numeric"
+          currency
+          id="gross-monthly-income"
+          data-testid="gross-monthly-income"
+          name="gross-monthly-income"
+          onInput={setNewGrossMonthlyIncome}
+          type="text"
+          value={grossMonthlyIncome.value}
+          required
+          error={
+            incomeError && grossMonthlyIncome.dirty
+              ? `Please enter a valid number.`
+              : ''
+          }
+        />
+      </div>
       {onReviewPage ? updateButton : navButtons}
     </form>
   );
