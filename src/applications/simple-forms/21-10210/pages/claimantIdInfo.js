41443f40d9718c451aac32bd7b70ade1cbb73bae
Tze-chiu Lei <tze@adhocteam.us>
2023-06-01 16:20:15 -0700

    Make 21-10210 VA-File & VA-Ins-File number labels static

--- src/applications/simple-forms/21-10210/pages/claimantIdInfo.js
+++ src/applications/simple-forms/21-10210/pages/claimantIdInfo.js
@@ -1,39 +1,21 @@
 import definitions from 'vets-json-schema/dist/definitions.json';
 import ssnUI from 'platform/forms-system/src/js/definitions/ssn';
-import { CLAIM_OWNERSHIPS } from '../definitions/constants';
 
 /** @type {PageSchema} */
 export default {
   uiSchema: {
     claimantSSN: ssnUI,
     claimantVaFileNumber: {
+      'ui:title': 'VA file number',
       'ui:errorMessages': {
         pattern:
           "Please enter a valid VA file number.  All should have 7-9 digits; some may start with a 'C'",
       },
-      'ui:options': {
-        updateSchema: formData => {
-          const title =
-            formData.claimOwnership === CLAIM_OWNERSHIPS.SELF
-              ? 'VA file number (if you have one)'
-              : 'VA file number (if Claimant has one)';
-          return { title };
-        },
-      },
     },
     claimantVaInsuranceFileNumber: {
-      'ui:title': 'VA Insurance File Number (if you have one)',
+      'ui:title': 'VA Insurance File Number',
       'ui:errorMessages': {
         maxLength: 'Please enter a number with fewer than 20 digits.',
-      },
-      'ui:options': {
-        updateSchema: formData => {
-          const title =
-            formData.claimOwnership === CLAIM_OWNERSHIPS.SELF
-              ? 'VA Insurance File number (if you have one)'
-              : 'VA Insurance File number (if Claimant has one)';
-          return { title };
-        },
       },
     },
   },
--- src/applications/simple-forms/21-10210/pages/vetIdInfo.js
+++ src/applications/simple-forms/21-10210/pages/vetIdInfo.js
@@ -1,38 +1,21 @@
 import definitions from 'vets-json-schema/dist/definitions.json';
 import ssnUI from 'platform/forms-system/src/js/definitions/ssn';
-import { CLAIM_OWNERSHIPS } from '../definitions/constants';
 
 /** @type {PageSchema} */
 export default {
   uiSchema: {
     veteranSSN: ssnUI,
     veteranVaFileNumber: {
+      'ui:title': 'VA file number',
       'ui:errorMessages': {
         pattern:
           "Please enter a valid VA file number.  All should have 7-9 digits; some may start with a 'C'",
       },
-      'ui:options': {
-        updateSchema: formData => {
-          const title =
-            formData.claimOwnership === CLAIM_OWNERSHIPS.SELF
-              ? 'VA file number (if you have one)'
-              : 'VA file number (if Veteran has one)';
-          return { title };
-        },
-      },
     },
     veteranVaInsuranceFileNumber: {
+      'ui:title': 'VA Insurance File number',
       'ui:errorMessages': {
         maxLength: 'Please enter a number with fewer than 20 digits.',
-      },
-      'ui:options': {
-        updateSchema: formData => {
-          const title =
-            formData.claimOwnership === CLAIM_OWNERSHIPS.SELF
-              ? 'VA Insurance File number (if you have one)'
-              : 'VA Insurance File number (if Veteran has one)';
-          return { title };
-        },
       },
     },
   },
