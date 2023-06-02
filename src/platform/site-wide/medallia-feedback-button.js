31d7c91e3b1491f3ad3262efd5c689b14ca74cf6
aubreyarcangel <aarcangel@gmail.com>
2023-06-02 12:29:37 -0400

    observing aria-label on kampyleFormModal

--- src/platform/site-wide/medallia-feedback-button.js
+++ src/platform/site-wide/medallia-feedback-button.js
@@ -259,46 +259,41 @@
 //   });
 // });
 window.addEventListener('DOMContentLoaded', () => {
-  function findModalLiveForm(element) {
-    if (element.classList && element.classList.contains('modal-live-form')) {
-      return element;
-    }
-
-    const childElements = Array.from(element.children);
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < childElements.length; i++) {
-      const childElement = childElements[i];
-      const modalLiveForm = findModalLiveForm(childElement);
-      if (modalLiveForm) {
-        return modalLiveForm;
-      }
-    }
-
-    return null;
-  }
-
+  // Create a new MutationObserver to watch for the target element's insertion into the DOM
   const observer = new MutationObserver(mutationsList => {
-    // eslint-disable-next-line consistent-return
-    mutationsList.forEach(mutation => {
+    for (const mutation of mutationsList) {
       if (mutation.type === 'childList') {
-        const addedNodes = Array.from(mutation.addedNodes);
-        for (let i = 0; i < addedNodes.length; i++) {
-          const addedNode = addedNodes[i];
-          if (
-            addedNode.nodeType === Node.ELEMENT_NODE &&
-            addedNode.id === 'MDigitalLightboxWrapper'
-          ) {
-            console.log('MDigitalLightboxWrapper found!');
-            const modalLiveForm = findModalLiveForm(addedNode);
-            if (modalLiveForm) {
-              console.log('.modal-live-form found:', modalLiveForm);
-              return modalLiveForm;
-            }
-          }
+        // Check if the target element has been added to the DOM
+        const targetElement = document.getElementById('kampyleFormModal');
+        if (targetElement) {
+          // Once the target element is present, create a new MutationObserver to watch for changes in its attributes
+          const attributeObserver = new MutationObserver(
+            attributeMutationsList => {
+              for (const attributeMutation of attributeMutationsList) {
+                if (
+                  attributeMutation.type === 'attributes' &&
+                  attributeMutation.attributeName === 'aria-label'
+                ) {
+                  // Check if the aria-label attribute has the value "Feedback Survey"
+                  const ariaLabel = targetElement.getAttribute('aria-label');
+                  if (ariaLabel === 'Feedback Survey') {
+                    console.log('feedback survey is here');
+                  }
+                }
+              }
+            },
+          );
+
+          // Start observing changes to the target element's attributes
+          attributeObserver.observe(targetElement, { attributes: true });
+
+          // Disconnect the observer once the target element is found
+          observer.disconnect();
         }
       }
-    });
+    }
   });
 
+  // Start observing changes in the DOM
   observer.observe(document.body, { childList: true, subtree: true });
 });
