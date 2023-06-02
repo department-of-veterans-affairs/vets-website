7c9973abca9f171a3f947894bb79e050cb3f34e8
aubreyarcangel <aarcangel@gmail.com>
2023-06-02 14:40:47 -0400

    added setTimeout

--- src/platform/site-wide/medallia-feedback-button.js
+++ src/platform/site-wide/medallia-feedback-button.js
@@ -259,6 +259,9 @@
 //   });
 // });
 window.addEventListener('DOMContentLoaded', () => {
+  function addAriaLabel(element, label) {
+    element.setAttribute('aria-label', label);
+  }
   // Create a new MutationObserver to watch for the target element's insertion into the DOM
   const observer = new MutationObserver(mutationsList => {
     for (const mutation of mutationsList) {
@@ -278,6 +281,12 @@ window.addEventListener('DOMContentLoaded', () => {
                   const ariaLabel = targetElement.getAttribute('aria-label');
                   if (ariaLabel === 'Feedback Survey') {
                     console.log('feedback survey is here');
+                    setTimeout(() => {
+                      const vclNumber = document.querySelector(
+                        '#liveForm > div > div.live-form-content > div.modal-live-form.ng-scope > div > div > div:nth-child(1) > div > div > p > ul > li:nth-child(1) > span > strong:nth-child(2) > a',
+                      );
+                      addAriaLabel(vclNumber, '9 9 8');
+                    }, 3000);
                   }
                 }
               }
