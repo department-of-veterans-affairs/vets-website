4627b7c6d467a06a8a3bd57236eb055a361133d2
Tze-chiu Lei <tze@adhocteam.us>
2023-06-05 10:46:37 -0700

    Refactor 21-10210 test-data

--- src/applications/simple-forms/21-10210/config/form.js
+++ src/applications/simple-forms/21-10210/config/form.js
@@ -29,7 +29,8 @@ import transformForSubmit from './submit-transformer';
 
 // mock-data import for local development
 // import the appropriate file [flow?.json] for the flow you're working on, or
-// noStmtInfo.json for all flows [select claimOwnership & claimantType via UI]
+// noStmtInfo.json for all flows [manually select claimOwnership, claimantType,
+// & witnessRelationshipWithClaimant via UI]
 import testData from '../tests/e2e/fixtures/data/noStmtInfo.json';
 
 const mockData = testData.data;
