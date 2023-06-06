640d032e02ecc1789f3a8197afdb0db5e40e0185
haniasma <121971510+haniasma@users.noreply.github.com>
2023-06-06 11:12:04 -0400

    Update secure-messaging-advanced-search-keyboard-nav.cypress.spec.js

--- src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-advanced-search-keyboard-nav.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-advanced-search-keyboard-nav.cypress.spec.js
@@ -7,7 +7,7 @@ import mockSearchMessages from '../fixtures/search-COVID-results.json';
 
 describe(manifest.appName, () => {
   describe('Advanced search in Inbox keyboard nav', () => {
-    beforeEach(() => {
+    it('Check all inbox messages contain the searched category', () => {
       const site = new SecureMessagingSite();
       const landingPage = new PatientInboxPage();
       site.login();
@@ -20,11 +20,14 @@ describe(manifest.appName, () => {
       landingPage.NavigateAdvancedSearch();
       landingPage.NavigateAdvancedSearchCategory();
       landingPage.KeyboardNavsubmitSearchButton();
+      cy.injectAxe();
+      cy.axeCheck();
     });
   });
+
   describe('Advanced search in Drafts keyboard nav', () => {
-    beforeEach(() => {
-      const site = new SecureMessagingSite();
+    const site = new SecureMessagingSite();
+    it('check draft message advance search', () => {
       const landingPage = new PatientInboxPage();
       site.login();
       landingPage.loadInboxMessages();
@@ -47,20 +50,21 @@ describe(manifest.appName, () => {
       landingPage.NavigateAdvancedSearch();
       landingPage.selectAdvancedSearchCategory();
       landingPage.KeyboardNavsubmitSearchButton();
-    });
-    it('Check all draft messages contain the searched category', () => {
-      cy.get('[data-testid="message-list-item"]')
-        .should('contain', 'COVID')
-        .and('have.length', mockSearchMessages.data.length);
-      cy.injectAxe();
-      cy.axeCheck();
-    });
-    it('Check the search message label', () => {
-      cy.get('[data-testid="search-message-folder-input-label"]')
-        .should('contain', '4')
-        .and('contain', 'Category: "covid"');
-      cy.injectAxe();
-      cy.axeCheck();
+
+      it('Check all draft messages contain the searched category', () => {
+        cy.get('[data-testid="message-list-item"]')
+          .should('contain', 'COVID')
+          .and('have.length', mockSearchMessages.data.length);
+        cy.injectAxe();
+        cy.axeCheck();
+      });
+      it('Check the search message label', () => {
+        cy.get('[data-testid="search-message-folder-input-label"]')
+          .should('contain', '4')
+          .and('contain', 'Category: "covid"');
+        cy.injectAxe();
+        cy.axeCheck();
+      });
     });
   });
 });
