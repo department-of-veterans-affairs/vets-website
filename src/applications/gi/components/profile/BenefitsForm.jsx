ae699935dd38c21a647379ac6181efa5a547fde3
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2023-06-13 09:49:34 -0500

    create a branch for all 1990 form changes.

--- src/applications/gi/components/profile/BenefitsForm.jsx
+++ src/applications/gi/components/profile/BenefitsForm.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import EbenefitsLink from 'platform/site-wide/ebenefits/containers/EbenefitsLink';
 import { ariaLabels } from '../../constants';
