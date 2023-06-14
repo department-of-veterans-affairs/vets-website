2a5ef58d802a534ecd3fbee375e87ce11a6cbb80
Joseph Hall <joe.hall@thoughtworks.com>
2023-06-14 15:56:01 -0400

    [department-of-veterans-affairs/ask-va/#2] add page to  vets-website

---
+++ src/applications/ask-va/components/page/Page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import React, { useEffect, useState } from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+import Disclaimer from './Disclaimer';
+
+export default function Page() {
+  return <Disclaimer />;
+}
---
+++ src/applications/ask-va/reducers/index.js
@@ -0,0 +1,6 @@
+const initialState = {
+  // whatever initial state you have
+  foo: true,
+};
+
+export default function askVa(state = initialState, action) {}
