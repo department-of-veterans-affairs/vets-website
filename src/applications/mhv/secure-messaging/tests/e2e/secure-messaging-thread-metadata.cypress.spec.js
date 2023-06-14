1ffcef35895f85621ca6520b7a3f8505b4f3f463
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2023-06-14 14:32:39 -0700

    First step: load inbox messages

---
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-thread-metadata.cypress.spec.js
@@ -0,0 +1,29 @@
+import SecureMessagingSite from './sm_site/SecureMessagingSite';
+import PatientInboxPage from './pages/PatientInboxPage';
+import inboxMessages from './fixtures/messages-response.json';
+// import mockMessageDetails from './fixtures/message-response.json';
+// import PatientMessageDetailsPage from './pages/PatientMessageDetailsPage';
+
+describe('Secure Messaging Message Details AXE Check', () => {
+  it('Axe Check Message Details Page', () => {
+    const landingPage = new PatientInboxPage();
+    // const detailsPage = new PatientMessageDetailsPage();
+    const site = new SecureMessagingSite();
+    site.login();
+    // const messageDetails = mockMessageDetails;
+    const date = new Date();
+    date.setDate(date.getDate() - 2);
+    // messageDetails.data.attributes.sentDate = date.toISOString();
+    // cy.log(`New Message Details ==== ${JSON.stringify(messageDetails)}`);
+    landingPage.loadInboxMessages(inboxMessages); // messageDetails
+    // detailsPage.loadMessageDetails(messageDetails);
+    cy.injectAxe();
+    cy.axeCheck('main', {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
+  });
+});
