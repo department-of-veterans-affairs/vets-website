4cf444aea3af5dec5362dfc8477ba03e256a4d1a
Alex <AlexanderSherrow@gmail.com>
2023-06-14 10:19:42 -0400

    MBMS-38574 Updated Privacy Statement

--- src/platform/forms/components/OMBInfoModalContent/BurialModalContent.jsx
+++ src/platform/forms/components/OMBInfoModalContent/BurialModalContent.jsx
@@ -3,15 +3,40 @@ import React from 'react';
 export default function HealthcareModalContent({ resBurden, ombNumber }) {
   return (
     <>
-      <h3>Paperwork Reduction Act and Privacy Act information</h3>
+      <h3>Privacy Act Statement</h3>
       <p>
-        The Privacy Act of 1974, the Paperwork Reduction Act of 1980, and the
-        Paperwork Reduction act of 1995 require that when we ask you for
-        information we must first tell you our legal right to ask for the
-        information, why we are asking for it, and how it will be used. We must
-        also tell you whether your response is voluntary, required to obtain a
-        benefit, or mandatory under the law and what could happen if we don’t
-        receive the information.
+        <strong>Respondent Burden:</strong> This information is collected in
+        accordance with section 3507 of the Paperwork Reduction Act of 1995.
+        Accordingly, we may not conduct or sponsor, and you are not required to
+        respond to a collection of information unless it displays a valid OMB
+        number. We anticipate the time expended by individuals who complete this
+        form will average 20 minutes per response, including the time to review
+        instructions, search existing data sources, gather the necessary data,
+        and complete and review the collection of information. Your response is
+        voluntary and not required to obtain or retain benefits to which you may
+        be entitled. Send comments concerning the accuracy of this burden
+        estimate, including suggestions for reducing this burden or any other
+        aspect of this collection of information to the VA Clearance Officer
+        (005R1B), 810 Vermont Avenue, NW, Washington, DC 20420. Please DO NOT
+        send claims for, or correspondence regarding benefits to this address.
+        <br />
+        <br />
+        <strong>Privacy Act Notice:</strong> Title 38 U.S.C. 2402 authorizes the
+        solicitation of this information. VA considers the responses you submit
+        confidential (38 U.S.C. 5701). VA may only disclose this information
+        outside the VA if the disclosure is authorized under the Privacy Act,
+        including the routine uses identified in the VA system of records,
+        175VA41A, published in the Federal Register. VA considers the requested
+        information relevant and necessary to determine maximum benefits under
+        the law. The purpose for which the records are used will include, but
+        will not be limited to the provision of VA burial and memorial benefits;
+        provision of information about VA burial and memorial benefits,
+        including specific claims; determination of eligibility for burial in a
+        VA national cemetery; disclosure of military service information upon
+        request from VA funded State and Tribal Veterans cemeteries;
+        coordination of committal services and interment upon request of
+        families, funeral homes, and others of eligible decedents at VA national
+        cemeteries.
       </p>
 
       <h4>VA’s legal right to ask for the information (authority)</h4>
