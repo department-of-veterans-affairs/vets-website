6055a255495aa33806b5ca0772e2c47758ff3c47
Adam Whitlock <adam.whitlock@adhocteam.us>
2023-06-15 10:32:06 -0600

    feat: use dynamic update link target for edit page

--- src/applications/personalization/profile/components/notification-settings/ContactInfoOnFile.jsx
+++ src/applications/personalization/profile/components/notification-settings/ContactInfoOnFile.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom';
 import { getContactInfoDeepLinkURL } from '@@profile/helpers';
 import { FIELD_NAMES } from '@@vap-svc/constants';
 import { VaTelephone } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import { useContactInfoDeepLink } from '../../hooks';
+import { PROFILE_PATHS } from '../../constants';
 
 const ContactInfoOnFile = ({ emailAddress, mobilePhoneNumber }) => {
+  const { generateContactInfoLink } = useContactInfoDeepLink();
   return (
     <>
       <p className="vads-u-margin-bottom--0">
@@ -32,7 +35,13 @@ const ContactInfoOnFile = ({ emailAddress, mobilePhoneNumber }) => {
               notClickable
             />{' '}
             <Link
-              to={getContactInfoDeepLinkURL(FIELD_NAMES.MOBILE_PHONE, true)}
+              to={generateContactInfoLink({
+                fieldName: FIELD_NAMES.MOBILE_PHONE,
+                focusOnEditButton: true,
+                returnPath: encodeURIComponent(
+                  PROFILE_PATHS.NOTIFICATION_SETTINGS,
+                ),
+              })}
               className="small-screen:vads-u-display--block medium-screen:vads-u-display--inline"
               aria-label="mobile number"
             >
