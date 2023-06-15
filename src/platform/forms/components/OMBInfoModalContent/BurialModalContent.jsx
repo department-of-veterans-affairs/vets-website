eaa163bf092147552c8620cb3e9b95426854d74b
Alex <AlexanderSherrow@gmail.com>
2023-06-15 10:42:11 -0400

    MBMS-38574 fix

--- src/platform/forms/components/OMBInfoModalContent/BurialModalContent.jsx
+++ src/platform/forms/components/OMBInfoModalContent/BurialModalContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-export default function HealthcareModalContent() {
+// eslint-disable-next-line no-unused-vars
+export default function HealthcareModalContent({ resBurden, ombNumber }) {
   return (
     <>
       <p>
