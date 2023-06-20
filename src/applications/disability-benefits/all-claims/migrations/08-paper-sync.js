73ee89bbb019bf4daacd48f7e46c0a960b229c61
Christine <127446042+christinec-fftc@users.noreply.github.com>
2023-06-20 14:37:33 -0700

    59798 page migration for housing-situation, terminally-ill and fully-developed-claim pages

---
+++ src/applications/disability-benefits/all-claims/migrations/08-paper-sync.js
@@ -0,0 +1,35 @@
+/**
+ * Syncing 526 online form with paper form
+ *  Move housing-situation and terminally-ill pages from additionalInformation to veteranDetails chapter which
+ *    is from the last chapter to the first. If user has progressed beyond the first couple pages and hasn't
+ *    filled either of these out yet, redirect to them.
+ *  Fully developed claim page was removed. If they left off here, redirect to review and submit page.
+ */
+export default function reorderHousingIllnessRemoveFdc(savedData) {
+  const { returnUrl } = savedData.metadata;
+
+  if (
+    returnUrl === '/veteran-information' ||
+    returnUrl === '/contact-information'
+  )
+    return savedData;
+
+  const { formData } = savedData;
+  if (formData.homelessOrAtRisk === undefined) {
+    return Object.assign({}, savedData, {
+      metadata: { returnUrl: '/housing-situation-1' },
+    });
+  }
+
+  if (formData.isTerminallyIll === undefined) {
+    return Object.assign({}, savedData, {
+      metadata: { returnUrl: '/terminally-ill-1' },
+    });
+  }
+
+  if (returnUrl === '/fully-developed-claim')
+    return Object.assign({}, savedData, {
+      metadata: { returnUrl: '/review-and-submit' },
+    });
+  return savedData;
+}
