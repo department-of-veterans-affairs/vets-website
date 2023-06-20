749a6dd4a7566e4d917fc9f4b5376cbdebac8d50
Robert Hasselle <robert.hasselle@oddball.io>
2023-06-20 15:10:28 -0500

    update comment about real events

--- src/applications/simple-forms/shared/tests/e2e/helpers.js
+++ src/applications/simple-forms/shared/tests/e2e/helpers.js
@@ -85,8 +85,9 @@ export const fillDateWebComponentPattern = (fieldName, value) => {
       // There is a bug only on Chromium based browsers where
       // VaMemorableDate text input fields will think they are
       // disabled if you blur focus of the window while the test
-      // is running. realPress and realType prevent this issue,
-      // but these are only applicable to Chromium based browsers.
+      // is running. realPress and realType solve this issue,
+      // but these are only available for Chromium based browsers.
+      // See cypress-real-events npmjs for more info.
       cy.get(`va-memorable-date[name="root_${fieldName}"]`)
         .shadow()
         .find('va-select.usa-form-group--month-select')
