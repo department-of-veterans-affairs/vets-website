c660224954ebb62205e8538b4aac26ea8e1377e5
Peter Hill <pjhill@FlyingDutchman>
2023-06-22 12:22:36 -0400

    remove trailing slash from hardcoded API URL

--- src/platform/utilities/preview-environment/start.sh
+++ src/platform/utilities/preview-environment/start.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 echo "Creating 'website' directory to hold front end sibling directories"
-mkdir -p website
-cd website
+mkdir -p website/slow
+cd website/slow
 
 # Clone vagov-content
-echo "Cloning vagov-content"
+echo "Starting vagov-content"
 git clone --depth 1 https://github.com/department-of-veterans-affairs/vagov-content
 
 # Clone vets-gov-json schema
-echo "Cloning vets-json-schema"
+echo "Starting vets-json-schema"
 git clone --depth 1 https://github.com/department-of-veterans-affairs/vets-json-schema.git
 
 # Clone veteran-facing-services-tools
-echo "Cloning veteran-facing-services-tools"
+echo "Starting veteran-facing-services-tools"
 git clone --depth 1 https://github.com/department-of-veterans-affairs/veteran-facing-services-tools
 
 # Clone content-build
-echo "Cloning content-build"
+echo "Starting content-build"
 git clone --depth 1 https://github.com/department-of-veterans-affairs/content-build.git
 
 # Clone vets-website
 if [ -z ${SOURCE_REF} ] ;
 then
-    echo "SOURCE_REF is NULL; cloning vets-website main" ;
+    echo "SOURCE_REF is NULL; starting vets-website main" ;
     git clone --depth 1 https://github.com/department-of-veterans-affairs/vets-website.git ;
 else
-    echo "SOURCE_REF is: " $SOURCE_REF " using workflow env var" ;
-    git clone -b ${SOURCE_REF} --single-branch https://github.com/department-of-veterans-affairs/vets-website.git
+    echo "SOURCE_REF is: " $SOURCE_REF " using workflow env var and starting vets-website" ;
+    git clone -b ${SOURCE_REF} --single-branch https://github.com/department-of-veterans-affairs/vets-website.git ;
 fi
 
 echo "set yarn to allow self-signed cert for install"
@@ -36,7 +36,7 @@ yarn config set "strict-ssl" false
 echo "Install, build, and watch vets-website"
 cd vets-website
 yarn install
-yarn watch --env api="http://vets-website-rework-pe-fronten-dev-platform-api.vfs.va.gov/" &
+yarn watch --env api="http://vets-website-rework-pe-fronten-dev-platform-api.vfs.va.gov" &
 
 # Serve the content-build
 echo "Install and serve content-build"
