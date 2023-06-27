ecf9d6d2ce8e8fa29ac8fbc04625ac7875f40358
Adrian Rollett <acrollet@gmail.com>
2023-06-27 20:09:51 +0000

    Revert syntax in cypress before hook

--- src/applications/avs/app-entry.jsx
+++ src/applications/avs/app-entry.jsx
@@ -1,13 +1,13 @@
 import '@department-of-veterans-affairs/platform-polyfills';
 import './sass/avs.scss';
 
-import startApp from '@department-of-veterans-affairs/platform-startup/index';
+import { startAppFromIndex } from '@department-of-veterans-affairs/platform-startup/exports';
 
 import routes from './routes';
 import reducer from './reducers';
 import manifest from './manifest.json';
 
-startApp({
+startAppFromIndex({
   url: manifest.rootUrl,
   reducer,
   routes,
