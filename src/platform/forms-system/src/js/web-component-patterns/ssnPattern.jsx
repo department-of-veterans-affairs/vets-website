588b3093887e5d4813aec62700f48d589aae80bf
Robert Hasselle <robert.hasselle@oddball.io>
2023-06-28 15:12:26 -0500

    cleanup mock form

--- src/platform/forms-system/src/js/web-component-patterns/ssnPattern.jsx
+++ src/platform/forms-system/src/js/web-component-patterns/ssnPattern.jsx
@@ -6,7 +6,6 @@ import VaTextInputField from '../web-component-fields/VaTextInputField';
 
 const SSN_DEFAULT_TITLE = 'Social Security number';
 const VA_FILE_NUMBER_DEFAULT_TITLE = 'VA file number (if applicable)';
-const SERVICE_NUMBER_DEFAULT_TITLE = 'Service number (if applicable)';
 
 /**
  * Web component uiSchema for SSN
@@ -55,42 +54,10 @@ const vaFileNumberUI = title => {
 const vaFileNumberSchema = commonDefinitions.vaFileNumber;
 
 /**
- * @param {string} [title]
- * @returns {UISchemaOptions}
- */
-const serviceNumberUI = title => {
-  return {
-    'ui:title': title ?? SERVICE_NUMBER_DEFAULT_TITLE,
-    'ui:webComponentField': VaTextInputField,
-    'ui:errorMessages': {
-      pattern:
-        'Your Veteran Service Number must start with 0, 1, or 2 letters followed by 5 to 8 digits',
-    },
-    'ui:options': {
-      hideEmptyValueInReview: true,
-    },
-  };
-};
-
-/**
- * @returns `commonDefinitions.veteranServiceNumber`
- */
-const serviceNumberSchema = commonDefinitions.veteranServiceNumber;
-
-/**
  */
 const ssnOrVaFileNumberUI = () => ({
   socialSecurityNumber: ssnUI(),
   vaFileNumber: vaFileNumberUI(),
-});
-
-/**
- *
- */
-const ssnOrVaFileNumberOrServiceNumberUI = () => ({
-  socialSecurityNumber: ssnUI(),
-  vaFileNumber: vaFileNumberUI(),
-  serviceNumber: serviceNumberUI(),
 });
 
 const ssnOrVaFileNumberSchema = {
@@ -98,21 +65,11 @@ const ssnOrVaFileNumberSchema = {
   vaFileNumber: vaFileNumberSchema,
 };
 
-const ssnOrVaFileNumberOrServiceNumberSchema = {
-  socialSecurityNumber: ssnSchema,
-  vaFileNumber: vaFileNumberSchema,
-  serviceNumber: serviceNumberSchema,
-};
-
 export {
   ssnUI,
   ssnSchema,
   vaFileNumberUI,
   vaFileNumberSchema,
-  serviceNumberUI,
-  serviceNumberSchema,
   ssnOrVaFileNumberUI,
-  ssnOrVaFileNumberOrServiceNumberUI,
   ssnOrVaFileNumberSchema,
-  ssnOrVaFileNumberOrServiceNumberSchema,
 };
