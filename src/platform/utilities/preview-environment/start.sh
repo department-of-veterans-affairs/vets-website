3d5503836bda2d8c1276699c0003e77f075c547f
Peter Hill <pjhill@FlyingDutchman>
2023-06-28 13:32:04 -0400

    back to using EFS and watching vw

--- src/platform/utilities/preview-environment/start.sh
+++ src/platform/utilities/preview-environment/start.sh
@@ -1,130 +1,68 @@
 #!/bin/sh
-echo "Creating 'website' directory to hold front end sibling directories"
-mkdir -p website
-cd website
+echo "Navigate into /app/website/slow"
+cd website/slow
+echo "Generating timestamp"
+TIMESTAMP=$(date +%s)
+echo "Timestamp value: " $TIMESTAMP
+echo "Moving: " $SOURCE_REF " to " $SOURCE_REF"-"$TIMESTAMP
+mv -vf $SOURCE_REF $SOURCE_REF"-"$TIMESTAMP
+echo "Removing previous build in background"
+rm -rf $SOURCE_REF"-"$TIMESTAMP &
+echo "Creating directory: " $SOURCE_REF
+mkdir $SOURCE_REF
+echo "Navigate into: " $SOURCE_REF
+cd $SOURCE_REF
 
 # Clone vagov-content
-echo "Cloning vagov-content"
+echo "Starting vagov-content"
 git clone --depth 1 https://github.com/department-of-veterans-affairs/vagov-content
 
 # Clone vets-gov-json schema
-echo "Cloning vets-json-schema"
+echo "Starting vets-json-schema"
 git clone --depth 1 https://github.com/department-of-veterans-affairs/vets-json-schema.git
 
 # Clone veteran-facing-services-tools
-echo "Cloning veteran-facing-services-tools"
+echo "Starting veteran-facing-services-tools"
 git clone --depth 1 https://github.com/department-of-veterans-affairs/veteran-facing-services-tools
 
 # Clone content-build
-echo "Cloning content-build"
+echo "Starting content-build"
 git clone --depth 1 https://github.com/department-of-veterans-affairs/content-build.git
 
 # Clone vets-website
 if [ -z ${SOURCE_REF} ] ;
 then
-    echo "SOURCE_REF is NULL; cloning vets-website main" ;
+    echo "SOURCE_REF is NULL; starting vets-website main" ;
     git clone --depth 1 https://github.com/department-of-veterans-affairs/vets-website.git ;
 else
-    echo "SOURCE_REF is: " $SOURCE_REF " using workflow env var" ;
-    git clone -b ${SOURCE_REF} --single-branch https://github.com/department-of-veterans-affairs/vets-website.git
+    echo "SOURCE_REF is: " $SOURCE_REF " using workflow env var and starting vets-website" ;
+    git clone -b ${SOURCE_REF} --single-branch https://github.com/department-of-veterans-affairs/vets-website.git ;
 fi
-
-echo "set yarn to allow self-signed cert for install"
-yarn config set "strict-ssl" false
 
 # Build and watch vets-website
 echo "Install, build, and watch vets-website"
 cd vets-website
-yarn install
-yarn watch --env api="http://vets-website-rework-pe-fronten-dev-platform-api.vfs.va.gov/" &
+echo "Installing"
+yarn install #--production=false
+echo "Waiting for yarn install to finish before proceeding"
+wait
+echo "Building"
+yarn build --env api="http://vets-website-rework-pe-fronten-dev-platform-api.vfs.va.gov"
+wait
+yarn watch --env api="http://vets-website-rework-pe-fronten-dev-platform-api.vfs.va.gov" &
 
 # Serve the content-build
 echo "Install and serve content-build"
 cd ../content-build
 yarn install
+wait
 echo "Copy environment file template into place"
 cp .env.example .env
 echo "Fetch drupal cache as a discrete task so as to avoid SOCKS issues"
 yarn fetch-drupal-cache
+wait
 echo "Build using previously cached assets"
 yarn build --use-cached-assets
+wait
 echo "Serve up content-build"
 yarn serve
-
-# #!/bin/sh
-# echo "Navigate into /app/website/slow"
-# cd website/slow
-# echo "Generating timestamp"
-# TIMESTAMP=$(date +%s)
-# echo "Timestamp value: " $TIMESTAMP
-# echo "Moving: " $SOURCE_REF " to " $SOURCE_REF"-"$TIMESTAMP
-# mv -vf $SOURCE_REF $SOURCE_REF"-"$TIMESTAMP
-# echo "Removing previous build in background"
-# rm -rf $SOURCE_REF"-"$TIMESTAMP &
-# echo "Creating directory: " $SOURCE_REF
-# mkdir $SOURCE_REF
-# echo "Navigate into: " $SOURCE_REF
-# cd $SOURCE_REF
-
-# # Clone vagov-content
-# echo "Starting vagov-content"
-# git clone --depth 1 https://github.com/department-of-veterans-affairs/vagov-content
-
-# # Clone vets-gov-json schema
-# echo "Starting vets-json-schema"
-# git clone --depth 1 https://github.com/department-of-veterans-affairs/vets-json-schema.git
-
-# # Clone veteran-facing-services-tools
-# echo "Starting veteran-facing-services-tools"
-# git clone --depth 1 https://github.com/department-of-veterans-affairs/veteran-facing-services-tools
-
-# # Clone content-build
-# echo "Starting content-build"
-# git clone --depth 1 https://github.com/department-of-veterans-affairs/content-build.git
-
-# # Clone vets-website
-# if [ -z ${SOURCE_REF} ] ;
-# then
-#     echo "SOURCE_REF is NULL; starting vets-website main" ;
-#     git clone --depth 1 https://github.com/department-of-veterans-affairs/vets-website.git ;
-# else
-#     echo "SOURCE_REF is: " $SOURCE_REF " using workflow env var and starting vets-website" ;
-#     git clone -b ${SOURCE_REF} --single-branch https://github.com/department-of-veterans-affairs/vets-website.git ;
-# fi
-
-# echo "Listing contents website staging folder"
-# ls -l
-
-# echo "set yarn to allow self-signed cert for install"
-# yarn config set "strict-ssl" false -g
-# echo "set puppeteer to not download"
-# PUPPETEER_SKIP_DOWNLOAD='true'
-
-# # Build and watch vets-website
-# echo "Install, build, and watch vets-website"
-# cd vets-website
-# echo "Installing"
-# yarn install #--production=false
-# echo "Waiting for yarn install to finish before proceeding"
-# wait
-# echo "Building"
-# yarn build --env api="http://vets-website-rework-pe-fronten-dev-platform-api.vfs.va.gov"
-# wait
-# #echo "Watch"
-# #yarn watch --env api="http://vets-website-rework-pe-fronten-dev-platform-api.vfs.va.gov" &
-
-# # Serve the content-build
-# echo "Install and serve content-build"
-# cd ../content-build
-# yarn install
-# wait
-# echo "Copy environment file template into place"
-# cp .env.example .env
-# echo "Fetch drupal cache as a discrete task so as to avoid SOCKS issues"
-# yarn fetch-drupal-cache
-# wait
-# echo "Build using previously cached assets"
-# yarn build --use-cached-assets
-# wait
-# echo "Serve up content-build"
-# yarn serve
