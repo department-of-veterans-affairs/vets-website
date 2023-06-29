2c6657b72c5231dd465dac6cf8d2e91911195d43
fazilqa <fazil.mammadov@bylight.com>
2023-06-29 14:15:39 -0700

    create FolderLoadPage.js

--- src/applications/mhv/secure-messaging/tests/e2e/pages/FolderLoadPage.js
+++ src/applications/mhv/secure-messaging/tests/e2e/pages/FolderLoadPage.js
@@ -1,8 +1,6 @@
 import mockMessages from '../fixtures/messages-response.json';
 import mockCategories from '../fixtures/categories-response.json';
 import mockFolders from '../fixtures/folder-response.json';
-import mockInboxFolder from '../fixtures/folder-inbox-response.json';
-import mockRecipients from '../fixtures/recipients-response.json';
 import mockToggles from '../fixtures/toggles-response.json';
 
 class FolderLoadPage {
@@ -23,21 +21,113 @@ class FolderLoadPage {
       '/my_health/v1/messaging/folders/0/threads?*',
       mockMessages,
     ).as('inboxMessages');
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/folders/0*',
-      mockInboxFolder,
-    ).as('inboxFolderMetaData');
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/recipients?useCache=false',
-      mockRecipients,
-    ).as('recipients');
+    cy.intercept('GET', '/my_health/v1/messaging/folders/0*', {
+      data: mockFolders.data[0],
+    }).as('inboxFolderMetaData');
     cy.visit('my-health/secure-messages/inbox/', {
       onBeforeLoad: win => {
         cy.stub(win, 'print');
       },
     });
+
+    cy.wait('@folders');
+    cy.wait('@featureToggle');
+    cy.wait('@mockUser');
+    cy.wait('@inboxMessages');
+  };
+
+  loadDraftMessages = () => {
+    cy.intercept('GET', '/v0/feature_toggles?*', mockToggles).as(
+      'featureToggle',
+    );
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/messages/categories',
+      mockCategories,
+    ).as('categories');
+    cy.intercept('GET', '/my_health/v1/messaging/folders*', mockFolders).as(
+      'folders',
+    );
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/0/threads?*',
+      mockMessages,
+    ).as('inboxMessages');
+    cy.intercept('GET', '/my_health/v1/messaging/folders/0*', {
+      data: mockFolders.data[1],
+    }).as('inboxFolderMetaData');
+    cy.visit('my-health/secure-messages/inbox/', {
+      onBeforeLoad: win => {
+        cy.stub(win, 'print');
+      },
+    });
+    cy.get('[data-testid="drafts-sidebar"]').click();
+
+    cy.wait('@folders');
+    cy.wait('@featureToggle');
+    cy.wait('@mockUser');
+    cy.wait('@inboxMessages');
+  };
+
+  loadSentMessages = () => {
+    cy.intercept('GET', '/v0/feature_toggles?*', mockToggles).as(
+      'featureToggle',
+    );
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/messages/categories',
+      mockCategories,
+    ).as('categories');
+    cy.intercept('GET', '/my_health/v1/messaging/folders*', mockFolders).as(
+      'folders',
+    );
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/0/threads?*',
+      mockMessages,
+    ).as('inboxMessages');
+    cy.intercept('GET', '/my_health/v1/messaging/folders/0*', {
+      data: mockFolders.data[2],
+    }).as('inboxFolderMetaData');
+    cy.visit('my-health/secure-messages/inbox/', {
+      onBeforeLoad: win => {
+        cy.stub(win, 'print');
+      },
+    });
+    cy.get('[data-testid="sent-sidebar"]').click();
+
+    cy.wait('@folders');
+    cy.wait('@featureToggle');
+    cy.wait('@mockUser');
+    cy.wait('@inboxMessages');
+  };
+
+  loadDeletedMessages = () => {
+    cy.intercept('GET', '/v0/feature_toggles?*', mockToggles).as(
+      'featureToggle',
+    );
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/messages/categories',
+      mockCategories,
+    ).as('categories');
+    cy.intercept('GET', '/my_health/v1/messaging/folders*', mockFolders).as(
+      'folders',
+    );
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/0/threads?*',
+      mockMessages,
+    ).as('inboxMessages');
+    cy.intercept('GET', '/my_health/v1/messaging/folders/0*', {
+      data: mockFolders.data[3],
+    }).as('inboxFolderMetaData');
+    cy.visit('my-health/secure-messages/inbox/', {
+      onBeforeLoad: win => {
+        cy.stub(win, 'print');
+      },
+    });
+    cy.get('[data-testid="sent-sidebar"]').click();
 
     cy.wait('@folders');
     cy.wait('@featureToggle');
--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-folders.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-folders.cypress.spec.js
@@ -3,7 +3,7 @@ import SecureMessagingSite from './sm_site/SecureMessagingSite';
 import FolderLoadPage from './pages/FolderLoadPage';
 
 describe(manifest.appName, () => {
-  describe('Load Inbox messages', () => {
+  describe.skip('Load Inbox messages', () => {
     before(() => {
       const site = new SecureMessagingSite();
       const inboxPage = new FolderLoadPage();
@@ -27,9 +27,51 @@ describe(manifest.appName, () => {
       const site = new SecureMessagingSite();
       const inboxPage = new FolderLoadPage();
       site.login();
-      inboxPage.loadInboxMessages();
+      inboxPage.loadDraftMessages();
     });
     it('Check the header', () => {
+      cy.get('[data-testid="folder-header"]').should('have.text', 'Drafts');
+      cy.injectAxe();
+      cy.axeCheck('main', {
+        rules: {
+          'aria-required-children': {
+            enabled: false,
+          },
+        },
+      });
+    });
+  });
+  describe.skip('Load Sent messages', () => {
+    before(() => {
+      const site = new SecureMessagingSite();
+      const inboxPage = new FolderLoadPage();
+      site.login();
+      inboxPage.loadSentMessages();
+    });
+    it('Check the header', () => {
+      cy.get('[data-testid="folder-header"]').should(
+        'have.text',
+        'Sent messages',
+      );
+      cy.injectAxe();
+      cy.axeCheck('main', {
+        rules: {
+          'aria-required-children': {
+            enabled: false,
+          },
+        },
+      });
+    });
+  });
+  describe('Load Trash messages', () => {
+    before(() => {
+      const site = new SecureMessagingSite();
+      const inboxPage = new FolderLoadPage();
+      site.login();
+      inboxPage.loadDeletedMessages();
+    });
+    it('Check the header', () => {
+      cy.get('[data-testid="folder-header"]').should('have.text', 'Trash');
       cy.injectAxe();
       cy.axeCheck('main', {
         rules: {
