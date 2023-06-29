954de36e8cc07d8f0a5d74afd30a831ec63c8b83
Vic Saleem <victoria.saleem@va.gov>
2023-06-29 16:07:16 -0500

    [MHV-45980] Updated unit tests to remove falsey results

--- src/applications/mhv/secure-messaging/tests/components/Modals/RemoveAttachmentsModal.unit.spec.jsx
+++ src/applications/mhv/secure-messaging/tests/components/Modals/RemoveAttachmentsModal.unit.spec.jsx
@@ -8,9 +8,24 @@ describe('Remove Message Modal component', () => {
   it('should render without errors', () => {
     const screen = render(<RemoveAttachmentsModal visible />);
 
-    expect(screen.findByText(Prompts.Attachment.REMOVE_ATTACHMENT_TITLE));
-    expect(screen.findByText(Prompts.Attachment.REMOVE_ATTACHMENT_CONTENT));
-    expect(screen.findByText('Remove'));
-    expect(screen.findByText('Cancel'));
+    expect(screen.getByTestId('remove-attachment-modal')).to.exist;
+    expect(screen.getByText(Prompts.Attachment.REMOVE_ATTACHMENT_CONTENT)).to
+      .exist;
+    expect(screen.getByTestId('remove-attachment-modal')).to.have.attribute(
+      'modal-title',
+      `${Prompts.Attachment.REMOVE_ATTACHMENT_TITLE}`,
+    );
+    expect(screen.getByTestId('remove-attachment-modal')).to.have.attribute(
+      'primary-button-text',
+      'Remove',
+    );
+    expect(screen.getByTestId('remove-attachment-modal')).to.have.attribute(
+      'secondary-button-text',
+      'Cancel',
+    );
+    expect(screen.getByTestId('remove-attachment-modal')).to.have.attribute(
+      'status',
+      'warning',
+    );
   });
 });
