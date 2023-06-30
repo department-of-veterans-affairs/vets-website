2e8a29be7e793e088cd66d6637cee46082b0d7dd
Elwood Gary <46933023+elwoodva@users.noreply.github.com>
2023-06-30 12:37:30 -0400

    [MHV-41651] Checking for SM119 and SM151 temporarily

--- src/applications/mhv/secure-messaging/actions/messages.js
+++ src/applications/mhv/secure-messaging/actions/messages.js
@@ -264,7 +264,11 @@ export const sendMessage = (message, attachments) => async dispatch => {
       ),
     );
   } catch (e) {
-    if (e.errors && e.errors[0].code === Constants.Errors.Code.BLOCKED_USER) {
+    if (
+      e.errors &&
+      (e.errors[0].code === Constants.Errors.Code.BLOCKED_USER ||
+        e.errors[0].code === Constants.Errors.code.BLOCKED_USER2)
+    ) {
       dispatch(
         addAlert(
           Constants.ALERT_TYPE_ERROR,
