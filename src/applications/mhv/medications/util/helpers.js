1d64c3bcd47a30f32e2af2dcd6ce725ea1f5ed63
robertbylight <roberto.rodriguez@bylight.com>
2023-06-30 10:47:57 -0400

    [MHV-45008] added pdf helper function and pdf generation logic to landing page

--- src/applications/mhv/medications/util/helpers.js
+++ src/applications/mhv/medications/util/helpers.js
@@ -1,4 +1,5 @@
 import moment from 'moment-timezone';
+import { generatePdf } from '@department-of-veterans-affairs/platform-pdf/exports';
 
 /**
  * @param {*} timestamp
@@ -10,4 +11,22 @@ export const dateFormat = (timestamp, format = null) => {
   return moment
     .tz(timestamp, timeZone)
     .format(format || 'MMMM D, YYYY, h:mm a z');
+};
+
+/**
+ * @param {String} templateName must be an already created template found on platform/pdf/templates
+ * @param {String} generatedFileName pdf is saved under this name
+ * @param {Object} pdfData json object formatted according to a pdf template guideline set by platform
+ */
+export const generateMedicationsPDF = async (
+  templateName,
+  generatedFileName,
+  pdfData,
+) => {
+  try {
+    await generatePdf(templateName, generatedFileName, pdfData);
+  } catch (error) {
+    // TODO: Once UCD gives a flow on how to present to the  user when something goes wrong with the pdf generation
+    // Error logging/presentation goes here...
+  }
 };
