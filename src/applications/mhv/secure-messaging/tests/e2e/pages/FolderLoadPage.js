90400532f4bd807438620c739d27f60418e4bb93
fazilqa <fazil.mammadov@bylight.com>
2023-07-03 09:27:39 -0700

    create folderSetup method

--- src/applications/mhv/secure-messaging/tests/e2e/pages/FolderLoadPage.js
+++ src/applications/mhv/secure-messaging/tests/e2e/pages/FolderLoadPage.js
@@ -4,7 +4,7 @@ import mockFolders from '../fixtures/folder-response.json';
 import mockToggles from '../fixtures/toggles-response.json';
 
 class FolderLoadPage {
-  loadInboxMessages = () => {
+  foldersSetup = () => {
     cy.intercept('GET', '/v0/feature_toggles?*', mockToggles).as(
       'featureToggle',
     );
@@ -21,14 +21,18 @@ class FolderLoadPage {
       '/my_health/v1/messaging/folders/0/threads?*',
       mockMessages,
     ).as('inboxMessages');
-    cy.intercept('GET', '/my_health/v1/messaging/folders/0*', {
-      data: mockFolders.data[0],
-    }).as('inboxFolderMetaData');
     cy.visit('my-health/secure-messages/inbox/', {
       onBeforeLoad: win => {
         cy.stub(win, 'print');
       },
     });
+  };
+
+  loadInboxMessages = () => {
+    this.foldersSetup();
+    cy.intercept('GET', '/my_health/v1/messaging/folders/0*', {
+      data: mockFolders.data[0],
+    }).as('inboxFolderMetaData');
 
     cy.wait('@folders');
     cy.wait('@featureToggle');
@@ -37,30 +41,11 @@ class FolderLoadPage {
   };
 
   loadDraftMessages = () => {
-    cy.intercept('GET', '/v0/feature_toggles?*', mockToggles).as(
-      'featureToggle',
-    );
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/messages/categories',
-      mockCategories,
-    ).as('categories');
-    cy.intercept('GET', '/my_health/v1/messaging/folders*', mockFolders).as(
-      'folders',
-    );
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/folders/0/threads?*',
-      mockMessages,
-    ).as('inboxMessages');
+    this.foldersSetup();
     cy.intercept('GET', '/my_health/v1/messaging/folders/0*', {
       data: mockFolders.data[1],
     }).as('inboxFolderMetaData');
-    cy.visit('my-health/secure-messages/inbox/', {
-      onBeforeLoad: win => {
-        cy.stub(win, 'print');
-      },
-    });
+
     cy.get('[data-testid="drafts-sidebar"]').click();
 
     cy.wait('@folders');
@@ -70,30 +55,11 @@ class FolderLoadPage {
   };
 
   loadSentMessages = () => {
-    cy.intercept('GET', '/v0/feature_toggles?*', mockToggles).as(
-      'featureToggle',
-    );
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/messages/categories',
-      mockCategories,
-    ).as('categories');
-    cy.intercept('GET', '/my_health/v1/messaging/folders*', mockFolders).as(
-      'folders',
-    );
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/folders/0/threads?*',
-      mockMessages,
-    ).as('inboxMessages');
+    this.foldersSetup();
     cy.intercept('GET', '/my_health/v1/messaging/folders/0*', {
       data: mockFolders.data[2],
     }).as('inboxFolderMetaData');
-    cy.visit('my-health/secure-messages/inbox/', {
-      onBeforeLoad: win => {
-        cy.stub(win, 'print');
-      },
-    });
+
     cy.get('[data-testid="sent-sidebar"]').click();
 
     cy.wait('@folders');
@@ -103,30 +69,11 @@ class FolderLoadPage {
   };
 
   loadDeletedMessages = () => {
-    cy.intercept('GET', '/v0/feature_toggles?*', mockToggles).as(
-      'featureToggle',
-    );
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/messages/categories',
-      mockCategories,
-    ).as('categories');
-    cy.intercept('GET', '/my_health/v1/messaging/folders*', mockFolders).as(
-      'folders',
-    );
-    cy.intercept(
-      'GET',
-      '/my_health/v1/messaging/folders/0/threads?*',
-      mockMessages,
-    ).as('inboxMessages');
+    this.foldersSetup();
     cy.intercept('GET', '/my_health/v1/messaging/folders/0*', {
       data: mockFolders.data[3],
     }).as('inboxFolderMetaData');
-    cy.visit('my-health/secure-messages/inbox/', {
-      onBeforeLoad: win => {
-        cy.stub(win, 'print');
-      },
-    });
+
     cy.get('[data-testid="sent-sidebar"]').click();
 
     cy.wait('@folders');
