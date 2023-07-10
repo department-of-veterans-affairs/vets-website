65933af395acf98fa8df061d85b7f0df75b20c55
Randi Mays <randimays1024@gmail.com>
2023-07-10 09:25:49 -0500

    VACMS-13751 Income Limits: implement final URLs (#24775)

--- src/applications/income-limits/tests/income-limits.cypress.spec.js
+++ src/applications/income-limits/tests/income-limits.cypress.spec.js
@@ -13,29 +13,35 @@ xdescribe('Income Limits', () => {
     it('navigates through the flow successfully forward and backward', () => {
       cy.visit('/health-care/income-limits');
 
+      cy.url().should('contain', '/health-care/income-limits/introduction');
+
       // Home
       h.verifyElement(h.CURRENT_LINK);
       cy.injectAxeThenAxeCheck();
       h.clickCurrent();
 
       // Zip code
+      cy.url().should('contain', '/health-care/income-limits/zip');
       h.verifyElement(h.ZIPINPUT);
       cy.injectAxeThenAxeCheck();
       h.typeInInput(h.ZIPINPUT, h.ZIP);
       h.clickContinue();
 
       // Dependents
+      cy.url().should('contain', '/health-care/income-limits/dependents');
       h.verifyElement(h.DEPINPUT);
       cy.injectAxeThenAxeCheck();
       h.typeInInput(h.DEPINPUT, h.DEPENDENTS);
       h.clickContinue();
 
       // Review
+      cy.url().should('contain', '/health-care/income-limits/review');
       h.verifyElement(h.REVIEWPAGE);
       cy.injectAxeThenAxeCheck();
       h.clickContinue();
 
       // Results
+      cy.url().should('contain', '/health-care/income-limits/results');
       h.verifyElement(h.RESULTSPAGE);
       cy.injectAxeThenAxeCheck();
 
