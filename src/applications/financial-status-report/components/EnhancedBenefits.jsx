4010b8510e6313b571282f89d4c88a69bc0a9263
Joseph Lee <joseph@cityfriends.tech>
2023-07-10 12:45:58 -0700

    Efsr mini summary card drop shadow (#24797)
    * Converted mini summary cards to use [va-card](https://design.va.gov/storybook/?path=/docs/components-va-card--default) component
    
    * Fixed vertical spacing for values inside mini summary cards.

--- src/applications/financial-status-report/components/EnhancedBenefits.jsx
+++ src/applications/financial-status-report/components/EnhancedBenefits.jsx
@@ -17,10 +17,10 @@ const HotlineInfo = () => {
 
 const BenefitCard = React.memo(({ received }) => {
   return (
-    <div className="vads-u-margin-bottom--1 vads-u-margin-top--1">
+    <p className="vads-u-margin-bottom--1 vads-u-margin-top--1">
       <span>Amount received last month: </span>
       <b>{currency(received)}</b>
-    </div>
+    </p>
   );
 });
 
