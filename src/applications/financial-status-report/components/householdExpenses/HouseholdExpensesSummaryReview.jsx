474065186a9f1412849077e4d70d8eb945387a76
OptionSelect <brandyn.sullins@gmail.com>
2023-07-10 09:31:25 -0400

    Add household expenses readonly section to review page

---
+++ src/applications/financial-status-report/components/householdExpenses/HouseholdExpensesSummaryReview.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { currency as currencyFormatter } from '../../utils/helpers';
+
+const HouseholdExpensesSummaryReview = ({ data }) => {
+  const { expenseRecords = [] } = data.expenses;
+
+  return (
+    <div>
+      <h4>Monthly housing expenses</h4>
+      <dl className="review">
+        {expenseRecords.map((expense, index) => {
+          return (
+            <div
+              className="review-row"
+              key={expense.name + expense.amount + index}
+            >
+              <dt>{expense.name}</dt>
+              <dd>{currencyFormatter(expense.amount)}</dd>
+            </div>
+          );
+        })}
+      </dl>
+    </div>
+  );
+};
+
+HouseholdExpensesSummaryReview.propTypes = {
+  data: PropTypes.shape({
+    expenses: PropTypes.shape({ expenseRecords: PropTypes.array }),
+  }),
+  title: PropTypes.string,
+};
+
+export default HouseholdExpensesSummaryReview;
