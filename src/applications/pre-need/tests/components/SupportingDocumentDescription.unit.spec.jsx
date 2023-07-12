0909ce1cb72d704f1fe0f8ad68a5f2d2268454da
Kenneth Santiago <santiago_kenneth@bah.com>
2023-07-12 10:59:31 -0500

    MBMS-42629 updated unit test

--- src/applications/pre-need/tests/components/SupportingDocumentDescription.unit.spec.jsx
+++ src/applications/pre-need/tests/components/SupportingDocumentDescription.unit.spec.jsx
@@ -1,75 +1,67 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
 import SupportingDocumentsDescription from '../../components/SupportingDocumentsDescription';
+import * as helpers from '../../utils/helpers';
 
-describe('<SupportingDocumentsDescription>', () => {
-  it('should render', () => {
-    const tree = shallow(<SupportingDocumentsDescription />);
+describe('SupportingDocumentsDescription', () => {
+  let isVeteranStub;
 
-    expect(tree.find('va-additional-info').length).to.equal(2);
-    tree.unmount();
+  beforeEach(() => {
+    isVeteranStub = sinon.stub(helpers, 'isVeteran');
   });
-  it('should render service member text', () => {
-    const tree = mount(
-      <SupportingDocumentsDescription
-        formData={{
-          application: {
-            claimant: {
-              relationshipToVet: '1',
-            },
-          },
-        }}
-      />,
+
+  afterEach(() => {
+    isVeteranStub.restore();
+  });
+
+  it('renders the correct description if isVeteran returns true', () => {
+    isVeteranStub.returns(true);
+
+    const formData = {
+      relationshipToVet: '1',
+    };
+    const wrapper = shallow(
+      <SupportingDocumentsDescription formData={formData} />,
     );
 
-    const info = tree
-      .find('va-additional-info')
-      .first()
-      .html();
-    expect(info).to.contain('your DD214');
-    tree.unmount();
+    const veteranDescription = wrapper.find('p').at(2);
+
+    expect(veteranDescription.text()).to.contain(
+      'your  DD214 or other separation papers.',
+    );
+    wrapper.unmount();
   });
-  it('should render sponsor text', () => {
-    const tree = mount(
-      <SupportingDocumentsDescription
-        formData={{
-          application: {
-            claimant: {
-              relationshipToVet: '2',
-            },
-          },
-        }}
-      />,
+
+  it('renders the correct description if isVeteran returns false', () => {
+    isVeteranStub.returns(false);
+
+    const formData = {
+      relationshipToVet: '2',
+    };
+    const wrapper = shallow(
+      <SupportingDocumentsDescription formData={formData} />,
     );
 
-    const info = tree
-      .find('va-additional-info')
-      .first()
-      .html();
-    expect(info).to.contain('sponsor’s DD214');
-    tree.unmount();
-  });
-  it('should render child text', () => {
-    const tree = mount(
-      <SupportingDocumentsDescription
-        formData={{
-          application: {
-            claimant: {
-              relationshipToVet: '3',
-            },
-          },
-        }}
-      />,
-    );
+    const nonVeteranDescription = wrapper.find('p').at(2);
 
-    const info = tree
-      .find('va-additional-info')
-      .first()
-      .html();
-    expect(info).to.contain('sponsor’s DD214');
-    expect(info).to.contain('need to provide supporting documents');
-    tree.unmount();
+    expect(nonVeteranDescription.text()).to.contain(
+      "your sponsor's DD214 or other separation papers.",
+    );
+    wrapper.unmount();
+  });
+
+  it('renders the correct mailing address', () => {
+    const wrapper = shallow(
+      <SupportingDocumentsDescription formData={{ relationshipToVet: '2' }} />,
+    );
+    const mailingAddress = wrapper.find('p').at(6);
+
+    expect(mailingAddress.text()).to.contain('NCA Intake Center');
+    expect(mailingAddress.text()).to.contain('P.O. Box 5237');
+    expect(mailingAddress.text()).to.contain('Janesville, WI 53547');
+    wrapper.unmount();
   });
 });
