55eea7c2ec6739e24c08d90ef11f10bd803adc7d
Richard Davis <richard.davis@agile6.com>
2023-07-12 12:43:24 -0400

    Moves 'Learn more...' link

--- src/applications/mhv/landing-page/utilities/data/index.js
+++ src/applications/mhv/landing-page/utilities/data/index.js
@@ -182,6 +182,10 @@ const resolveLandingPageLinks = (authdWithSSOe = false, featureToggles) => {
   const moreResourcesLinks = resolveLinkCollection(
     [
       {
+        href: '/resources/my-healthevet-on-vagov-what-to-know',
+        text: 'Learn more about My HealtheVet on VA.gov',
+      },
+      {
         href: '/resources/the-pact-act-and-your-va-benefits/',
         text: 'The PACT Act and your benefits',
       },
