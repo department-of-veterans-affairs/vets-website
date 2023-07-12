bef97083f74faa8a5ed5928726d34d19fae0eefc
Evan Smith <evan.smith@oddball.io>
2023-07-12 10:30:51 -0400

    update va_benefits number, update tty component

--- src/applications/disability-benefits/686c-674/components/GetFormHelp.jsx
+++ src/applications/disability-benefits/686c-674/components/GetFormHelp.jsx
@@ -4,8 +4,8 @@ import { CONTACTS } from '@department-of-veterans-affairs/component-library/cont
 const GetFormHelp = () => (
   <p className="help-talk">
     Need help filling out the form or have questions about eligibility? Please
-    call VA Benefits and Services at <va-telephone contact="8008271000" />.
-    <br />
+    call VA Benefits and Services at{' '}
+    <va-telephone contact={CONTACTS.VA_BENEFITS} />.<br />
     <br />
     If you have hearing loss, call TTY: <va-telephone contact={CONTACTS[711]} />
     .
--- src/applications/disability-benefits/686c-674/config/helpers.js
+++ src/applications/disability-benefits/686c-674/config/helpers.js
@@ -52,8 +52,8 @@ export const ServerErrorAlert = (
     </p>
     <p className="vads-u-font-size--base">
       If you get this error again, please call the VA.gov help desk at{' '}
-      <va-telephone contact={CONTACTS.VA_311} /> (TTY:{' '}
-      <va-telephone contact={CONTACTS['711']} />
+      <va-telephone contact={CONTACTS.VA_311} /> (
+      <va-telephone contact={CONTACTS[711]} tty />
       ). We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
     </p>
   </>
