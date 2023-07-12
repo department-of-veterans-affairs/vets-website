1b9ca5f91751aa9aa74b0a8f8a7c0aa57f930e49
OptionSelect <brandyn.sullins@gmail.com>
2023-07-12 10:44:05 -0400

    Worked with Andrew to refactor unnecessary extra components

--- src/applications/financial-status-report/components/otherIncome/OtherIncomeSummaryReview.jsx
+++ src/applications/financial-status-report/components/otherIncome/OtherIncomeSummaryReview.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import OtherIncomeReviewSection from './OtherIncomeReviewSection';
+import { currency as currencyFormatter } from '../../utils/helpers';
 
-const OtherIncomeSummaryReview = ({ data }) => {
+const OtherIncomeSummaryReview = ({ data, title }) => {
   const { addlIncRecords = [] } = data.additionalIncome;
+  const { spAddlIncome = [] } = data.additionalIncome.spouse;
 
-  return <div>{OtherIncomeReviewSection(addlIncRecords, false)}</div>;
+  const isSpouse = title.toLowerCase().includes('spouse');
+
+  const recordArray = isSpouse ? spAddlIncome : addlIncRecords;
+
+  return (
+    <>
+      <h4>{isSpouse ? 'Spouse’s additional income' : 'Additional income'}</h4>
+      <dl className="review">
+        {recordArray.map((income, index) => {
+          return (
+            <div
+              className="review-row"
+              key={income.name + income.amount + index}
+            >
+              <dt>{income.name}</dt>
+              <dd>{currencyFormatter(income.amount)}</dd>
+            </div>
+          );
+        })}
+      </dl>
+    </>
+  );
 };
 
 OtherIncomeSummaryReview.propTypes = {
   data: PropTypes.shape({
     additionalIncome: PropTypes.shape({
       addlIncRecords: PropTypes.array,
+      spouse: PropTypes.shape({
+        spAddlIncome: PropTypes.array,
+      }),
     }),
   }),
   title: PropTypes.string,
