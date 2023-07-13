98ac6641d1093a80591c4cac157e29f00bd5d545
OptionSelect <brandyn.sullins@gmail.com>
2023-07-13 15:25:37 -0400

    Fix amount overdue conditional display

--- src/applications/financial-status-report/components/householdExpenses/CreditCardBillsSummaryReview.jsx
+++ src/applications/financial-status-report/components/householdExpenses/CreditCardBillsSummaryReview.jsx
@@ -2,6 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { currency as currencyFormatter } from '../../utils/helpers';
 
+const renderUnpaidBalance = (bill, index) => {
+  return (
+    <div className="review-row" key={bill.amountDueMonthly + index}>
+      <dt>Unpaid balance</dt>
+      <dd>{currencyFormatter(bill.unpaidBalance)}</dd>
+    </div>
+  );
+};
+
+const renderAmountOverdue = (bill, index) => {
+  if (bill.amountPastDue === '0') return null;
+
+  return (
+    <div className="review-row" key={bill.amountDueMonthly + index + 1}>
+      <dt>Amount overdue</dt>
+      <dd>{currencyFormatter(bill.amountPastDue)}</dd>
+    </div>
+  );
+};
+
+const renderMinMonthlyPayment = (bill, index) => {
+  return (
+    <div className="review-row" key={bill.amountDueMonthly + index + 2}>
+      <dt>Minimum monthly payment</dt>
+      <dd>{currencyFormatter(bill.amountDueMonthly)}</dd>
+    </div>
+  );
+};
+
 const CreditCardBillsSummaryReview = ({ data }) => {
   const { creditCardBills = [] } = data.expenses;
 
@@ -12,17 +41,9 @@ const CreditCardBillsSummaryReview = ({ data }) => {
           return (
             <>
               <h5>Credit card bill {index + 1}</h5>
-              <div className="review-row" key={bill.amountDueMonthly + index}>
-                <dt>Unpaid balance</dt>
-                <dd>{currencyFormatter(bill.unpaidBalance)}</dd>
-              </div>
-              <div
-                className="review-row"
-                key={bill.amountDueMonthly + index + 1}
-              >
-                <dt>Minimum monthly payment amount</dt>
-                <dd>{currencyFormatter(bill.amountDueMonthly)}</dd>
-              </div>
+              {renderUnpaidBalance(bill, index)}
+              {renderAmountOverdue(bill, index)}
+              {renderMinMonthlyPayment(bill, index)}
             </>
           );
         })}
