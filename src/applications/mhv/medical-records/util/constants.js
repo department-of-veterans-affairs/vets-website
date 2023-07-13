d53bd0d2d2632f7f14c7bb66dfe8a453270cec78
Mike Moyer <87040148+mmoyer-va@users.noreply.github.com>
2023-07-13 09:37:15 -0400

    MHV-42063 Update MR labs & tests data and reducer to match MHV data format (#24830)
    * [#MHV-42063] Update labs & tests reducer and test data
    
    * [#MHV-42063] Moved if/then block to a lookup table
    
    * [#MHV-42063] Updated unit test to match new fixture data

--- src/applications/mhv/medical-records/util/constants.js
+++ src/applications/mhv/medical-records/util/constants.js
@@ -16,6 +16,76 @@ export const labTypes = {
   OTHER: 'other',
 };
 
+export const LoincCodes = {
+  MICROBIOLOGY: '79381-0',
+  PATHOLOGY: '60567-5',
+  EKG: '11524-6',
+  RADIOLOGY: '18748-4',
+};
+
+export const FhirResourceTypes = {
+  BUNDLE: 'Bundle',
+  DIAGNOSTIC_REPORT: 'DiagnosticReport',
+  DOCUMENT_REFERENCE: 'DocumentReference',
+};
+
+/**
+ * Interpretation code map based on https://terminology.hl7.org/3.1.0/CodeSystem-v3-ObservationInterpretation.html
+ */
+export const interpretationMap = {
+  CAR: 'Carrier',
+  CARRIER: 'Carrier',
+  '<': 'Off scale low',
+  '>': 'Off scale high',
+  A: 'Abnormal',
+  AA: 'Critical abnormal',
+  AC: 'Anti-complementary substances present',
+  B: 'Better',
+  D: 'Significant change down',
+  DET: 'Detected',
+  E: 'Equivocal',
+  EX: 'outside threshold',
+  EXP: 'Expected',
+  H: 'High',
+  'H*': 'Critical high',
+  HH: 'Critical high',
+  HU: 'Significantly high',
+  'H>': 'Significantly high',
+  HM: 'Hold for Medical Review',
+  HX: 'above high threshold',
+  I: 'Intermediate',
+  IE: 'Insufficient evidence',
+  IND: 'Indeterminate',
+  L: 'Low',
+  'L*': 'Critical low',
+  LL: 'Critical low',
+  LU: 'Significantly low',
+  'L<': 'Significantly low',
+  LX: 'below low threshold',
+  MS: 'moderately susceptible',
+  N: 'Normal',
+  NCL: 'No CLSI defined breakpoint',
+  ND: 'Not detected',
+  NEG: 'Negative',
+  NR: 'Non-reactive',
+  NS: 'Non-susceptible',
+  OBX: 'Interpretation qualifiers in separate OBX segments',
+  POS: 'Positive',
+  QCF: 'Quality control failure',
+  R: 'Resistant',
+  RR: 'Reactive',
+  S: 'Susceptible',
+  SDD: 'Susceptible-dose dependent',
+  'SYN-R': 'Synergy - resistant',
+  'SYN-S': 'Synergy - susceptible',
+  TOX: 'Cytotoxic substance present',
+  U: 'Significant change up',
+  UNE: 'Unexpected',
+  VS: 'very susceptible',
+  W: 'Worse',
+  WR: 'Weakly reactive',
+};
+
 export const emptyField = 'None noted';
 
 export const testing = false;
