3f112bcb04850dadd7a29361a64c6224d1a8c5b2
Tze-chiu Lei <tclei2009@gmail.com>
2023-07-13 17:32:16 -0700

    Update 21-0845 authorizer-contact-information page

--- src/applications/simple-forms/21-0845/pages/authorizerContactInfo.js
+++ src/applications/simple-forms/21-0845/pages/authorizerContactInfo.js
@@ -13,19 +13,33 @@ export default {
     },
     authorizerEmail: {
       'ui:title': 'Email address',
+      'ui:widget': 'email',
       'ui:errorMessages': {
-        required: 'Please enter an email address.',
         format:
           'Enter a valid email address using the format email@domain.com. Your email address can only have letters, numbers, the @ symbol and a period, with no spaces.',
+      },
+    },
+    authorizerEmailConsent: {
+      'ui:title':
+        'I agree to receive electronic correspondence from VA in regards to my claim.',
+      'ui:required': formData => formData.authorizerEmail,
+      'ui:errorMessages': {
+        required: 'Please agree to receive electronic correspondence from VA.',
+      },
+      'ui:options': {
+        hideLabelText: true,
       },
     },
   },
   schema: {
     type: 'object',
-    required: ['authorizerPhone', 'authorizerEmail'],
+    required: ['authorizerPhone'],
     properties: {
       authorizerPhone: definitions.phone,
       authorizerEmail: definitions.email,
+      authorizerEmailConsent: {
+        type: 'boolean',
+      },
     },
   },
 };
