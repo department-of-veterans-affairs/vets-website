501ba6db864420222e0d2d39afd0616c2c9b628b
Tze-chiu Lei <tclei2009@gmail.com>
2023-07-14 18:16:53 -0700

    Add 21-0845 veteranDateOfBirth description and custom-validation

--- src/applications/simple-forms/21-0845/pages/veteranPersonalInfo.js
+++ src/applications/simple-forms/21-0845/pages/veteranPersonalInfo.js
@@ -1,5 +1,8 @@
+import React from 'react';
+
 import definitions from 'vets-json-schema/dist/definitions.json';
 import fullNameUI from 'platform/forms-system/src/js/definitions/fullName';
+import { validateDateOfBirth } from 'platform/forms/validations';
 import { schema } from '../../shared/definitions/pdfFullNameNoSuffix';
 
 /** @type {PageSchema} */
@@ -8,7 +11,14 @@ export default {
     veteranFullName: fullNameUI,
     veteranDateOfBirth: {
       'ui:title': 'Date of birth',
+      'ui:description': (
+        <p className="dob-description">For example: January 19 2000</p>
+      ),
       'ui:widget': 'date',
+      'ui:validations': [validateDateOfBirth],
+      'ui:errorMessages': {
+        pattern: 'Please provide a valid date of birth',
+      },
     },
   },
   schema: {
