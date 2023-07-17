3de2d11ae344e04330e9ab7912f006a8c4708661
Adrian Rollett <adrian.rollett@va.gov>
2023-07-17 17:03:26 -0600

    Add first pass on orders to your treatment plan

--- src/applications/avs/components/YourTreatmentPlan.jsx
+++ src/applications/avs/components/YourTreatmentPlan.jsx
@@ -1,12 +1,141 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { ORDER_TYPES } from '../utils/constants';
+
+const getOrderItems = (type, orders) => {
+  return orders.filter(order => order.type === type.label);
+};
+
+const consultations = avs => {
+  if (avs.data.orders?.length > 0) {
+    const items = getOrderItems(ORDER_TYPES.CONSULT, avs.data.orders);
+    const orderListItems = items.map((item, idx) => (
+      <li key={idx}>{item.text}</li>
+    ));
+
+    return (
+      <div>
+        <h4>Consultations</h4>
+        <p>
+          You will be contacted by mail or telephone for the following referral:
+        </p>
+        {orderListItems && <ul>{orderListItems}</ul>}
+      </div>
+    );
+  }
+
+  return null;
+};
+
+const imaging = avs => {
+  if (avs.data.orders?.length > 0) {
+    const items = getOrderItems(ORDER_TYPES.IMAGING, avs.data.orders);
+    const orderListItems = items.map((item, idx) => (
+      <li key={idx}>{item.text}</li>
+    ));
+
+    return (
+      <div>
+        <h4>Imaging</h4>
+        <p>
+          For CT and MRI scans, you will be contacted by Imaging Service for
+          your appointment. For X-Rays and Ultrasounds, please report to Imaging
+          Service during normmal working hours to complete your exam. If a
+          specific preparation is required, the Technologist will inform you.
+        </p>
+        {orderListItems && <ul>{orderListItems}</ul>}
+      </div>
+    );
+  }
+
+  return null;
+};
+
+const labTests = avs => {
+  if (avs.data.orders?.length > 0) {
+    const items = getOrderItems(ORDER_TYPES.LAB, avs.data.orders);
+    const orderListItems = items.map((item, idx) => (
+      <li key={idx}>
+        {item.text} ({item.date})
+      </li>
+    ));
+
+    return (
+      <div>
+        <h4>
+          Lab tests: Patients no longer need to fast for most blood draws. Check
+          with your provider whether fasting is required before completing your
+          Laboratory draws.
+        </h4>
+        <p>
+          Please report to the lab for the following blood tests on the date
+          listed for each test:
+        </p>
+        {orderListItems && <ul>{orderListItems}</ul>}
+      </div>
+    );
+  }
+
+  return null;
+};
+
+const medsAndSupplies = avs => {
+  if (avs.data.orders?.length > 0) {
+    const items = getOrderItems(ORDER_TYPES.MED, avs.data.orders);
+    const orderListItems = items.map((item, idx) => (
+      <li key={idx}>{item.text}</li>
+    ));
+
+    return (
+      <div>
+        <h4>Medications & supplies</h4>
+        <p>
+          Note: this section <strong>only</strong> lists{' '}
+          <strong>changes</strong> to your medication regimen. Please see your
+          complete medication list under My Ongoing Care below.
+        </p>
+        {/* TODO: headings for med change types */}
+        {orderListItems && <ul>{orderListItems}</ul>}
+      </div>
+    );
+  }
+
+  return null;
+};
+
+const otherOrders = avs => {
+  if (avs.data.orders?.length > 0) {
+    const items = getOrderItems(ORDER_TYPES.OTHER, avs.data.orders);
+    const orderListItems = items.map((item, idx) => (
+      <li key={idx}>{item.text}</li>
+    ));
+
+    return (
+      <div>
+        <h4>Other orders</h4>
+        {orderListItems && <ul>{orderListItems}</ul>}
+      </div>
+    );
+  }
+
+  return null;
+};
+
 const YourTreatmentPlan = props => {
   const { avs } = props;
-  // eslint-disable-next-line no-console
-  console.log(avs);
 
-  return <p>Treatment plan.</p>;
+  return (
+    <div>
+      <h3>New orders from this appointment</h3>
+      {/* TODO: is this the correct dataset? (Today vs. all) */}
+      {consultations(avs)}
+      {imaging(avs)}
+      {labTests(avs)}
+      {medsAndSupplies(avs)}
+      {otherOrders(avs)}
+    </div>
+  );
 };
 
 export default YourTreatmentPlan;
---
+++ src/applications/avs/utils/constants.js
@@ -0,0 +1,24 @@
+const ORDER_TYPES = {
+  CONSULT: {
+    key: 'CONSULT',
+    label: 'Consultations',
+  },
+  IMAGING: {
+    key: 'IMAGING',
+    label: 'Imaging',
+  },
+  LAB: {
+    key: 'LAB',
+    label: 'Lab Tests',
+  },
+  MED: {
+    key: 'MED',
+    label: 'Medications',
+  },
+  OTHER: {
+    key: 'OTHER',
+    label: 'Other Orders',
+  },
+};
+
+export { ORDER_TYPES };
