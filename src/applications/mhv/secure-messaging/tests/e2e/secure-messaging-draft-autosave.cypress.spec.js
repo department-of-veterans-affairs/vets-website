30530a49414290639d566df5b486ef82f0c01514
fazilqa <fazil.mammadov@bylight.com>
2023-07-18 08:35:47 -0700

    update draft-autosave test

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-draft-autosave.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-draft-autosave.cypress.spec.js
@@ -1,7 +1,7 @@
 import manifest from '../../manifest.json';
 import SecureMessagingSite from './sm_site/SecureMessagingSite';
 import PatientInboxPage from './pages/PatientInboxPage';
-import mockAutosaveDraftResponse from './fixtures/autosafe-draft-response.json';
+import mockAutoSaveDraftResponse from './fixtures/autosafe-draft-response.json';
 
 describe(manifest.appName, () => {
   describe('Advanced search in Drafts', () => {
@@ -15,21 +15,21 @@ describe(manifest.appName, () => {
       cy.intercept(
         'POST',
         '/my_health/v1/messaging/message_drafts',
-        mockAutosaveDraftResponse,
+        mockAutoSaveDraftResponse,
       ).as('autoSave');
-      cy.wait('@autoSave').then(xhr => {
-        cy.log(JSON.stringify(xhr.response.body));
-      });
-    });
-    it('Check all draft messages contain the searched category', () => {
       cy.intercept(
         'PUT',
         `/my_health/v1/messaging/message_drafts/${
-          mockAutosaveDraftResponse.data.attributes.messageId
+          mockAutoSaveDraftResponse.data.attributes.messageId
         }`,
-        mockAutosaveDraftResponse,
+        mockAutoSaveDraftResponse,
       ).as('autoSaveDetailed');
+      cy.wait('@autoSave').then(xhr => {
+        cy.log(JSON.stringify(xhr.response.body));
+      });
       cy.wait('@autoSaveDetailed');
+    });
+    it('Check all draft messages contain the searched category', () => {
       cy.get('.last-save-time').should('contain', 'Your message was saved');
       cy.injectAxe();
       cy.axeCheck('main', {
