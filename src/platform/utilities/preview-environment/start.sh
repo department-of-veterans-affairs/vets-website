cccfb2393d69edad093b0d00f62380318cd7196b
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2023-07-18 14:50:54 -0400

    Try building only vets-website in EFS

--- src/platform/utilities/preview-environment/start.sh
+++ src/platform/utilities/preview-environment/start.sh
@@ -2,17 +2,17 @@
 # Use the mounted EFS volume to retrieve instance repos, install, and build
 echo "Navigate into /app/website/slow"
 cd website/slow
-echo "Generating timestamp"
-TIMESTAMP=$(date +%s)
-echo "Timestamp value: " $TIMESTAMP
-echo "Moving: " $SOURCE_REF " to " $SOURCE_REF"-"$TIMESTAMP
-mv -vf $SOURCE_REF $SOURCE_REF"-"$TIMESTAMP
-echo "Removing previous build in background"
-rm -rf $SOURCE_REF"-"$TIMESTAMP &
-echo "Creating directory: " $SOURCE_REF
-mkdir $SOURCE_REF
-echo "Navigate into: " $SOURCE_REF
-cd $SOURCE_REF
+# echo "Generating timestamp"
+# TIMESTAMP=$(date +%s)
+# echo "Timestamp value: " $TIMESTAMP
+# echo "Moving: " $SOURCE_REF " to " $SOURCE_REF"-"$TIMESTAMP
+# mv -vf $SOURCE_REF $SOURCE_REF"-"$TIMESTAMP
+# echo "Removing previous build in background"
+# rm -rf $SOURCE_REF"-"$TIMESTAMP &
+# echo "Creating directory: " $SOURCE_REF
+# mkdir $SOURCE_REF
+# echo "Navigate into: " $SOURCE_REF
+# cd $SOURCE_REF
 # Commenting out directory creation for 
 # mkdir -p website
 # cd website
@@ -29,9 +29,9 @@ git clone --depth 1 https://github.com/department-of-veterans-affairs/vets-json-
 echo "Starting veteran-facing-services-tools"
 git clone --depth 1 https://github.com/department-of-veterans-affairs/veteran-facing-services-tools
 
-# Clone content-build
-echo "Starting content-build"
-git clone --depth 1 https://github.com/department-of-veterans-affairs/content-build.git
+# # Clone content-build
+# echo "Starting content-build"
+# git clone --depth 1 https://github.com/department-of-veterans-affairs/content-build.git
 
 # Clone vets-website
 if [ -z ${SOURCE_REF} ] ;
@@ -47,27 +47,27 @@ fi
 echo "Install, build, and watch vets-website"
 cd vets-website
 echo "Installing"
-yarn install --verbose #--production=false 
+yarn install #--production=false 
 echo "Waiting for yarn install to finish before proceeding"
 wait
 echo "Building"
-yarn build --env api="http://vets-website-rework-pe-fronten-dev-platform-api.vfs.va.gov" --verbose
-wait
-yarn watch --env api="http://vets-website-rework-pe-fronten-dev-platform-api.vfs.va.gov" --verbose &
+yarn build --env api="http://vets-website-rework-pe-fronten-dev-platform-api.vfs.va.gov"
+# wait
+# yarn watch --env api="http://vets-website-rework-pe-fronten-dev-platform-api.vfs.va.gov" &
 
-# Serve the content-build
-echo "Install and serve content-build"
-cd ../content-build
-git config --global --add safe.directory "*"
-yarn install --verbose
-wait
-echo "Copy environment file template into place"
-cp .env.example .env
-echo "Fetch drupal cache as a discrete task so as to avoid SOCKS issues"
-yarn fetch-drupal-cache
-wait
-echo "Build using previously cached assets"
-yarn build --use-cached-assets --verbose
-wait
-echo "Serve up content-build"
-yarn serve --verbose
+# # Serve the content-build
+# echo "Install and serve content-build"
+# cd ../content-build
+# git config --global --add safe.directory "*"
+# yarn install
+# wait
+# echo "Copy environment file template into place"
+# cp .env.example .env
+# echo "Fetch drupal cache as a discrete task so as to avoid SOCKS issues"
+# yarn fetch-drupal-cache
+# wait
+# echo "Build using previously cached assets"
+# yarn build --use-cached-assets --api="http://vets-website-rework-pe-fronten-dev-platform-api.vfs.va.gov"
+# wait"
+# echo "Serve up content-build"
+# yarn serve 
