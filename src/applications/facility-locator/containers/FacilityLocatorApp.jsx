6f08f32b0b6a601f8edbddf65d3e27647386e412
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-07-19 12:41:09 -0500

    Design system component demo

--- src/applications/facility-locator/containers/FacilityLocatorApp.jsx
+++ src/applications/facility-locator/containers/FacilityLocatorApp.jsx
@@ -8,6 +8,13 @@ import DowntimeNotification, {
 import { validateIdString } from '../utils/helpers';
 
 class FacilityLocatorApp extends React.Component {
+  componentDidMount() {
+    const modal = document.querySelector('#my-modal');
+    modal.addEventListener('closeEvent', () => {
+      modal.visible = false;
+    });
+  }
+
   renderBreadcrumbs(location, selectedResult) {
     // Map and name props for the search query object
     const {
@@ -66,12 +73,200 @@ class FacilityLocatorApp extends React.Component {
   render() {
     const { location, selectedResult } = this.props;
 
+    const handleModalOpen = () => {
+      document.querySelector('#my-modal').visible = true;
+    };
+
     return (
       <div>
         <va-breadcrumbs>
           {this.renderBreadcrumbs(location, selectedResult)}
         </va-breadcrumbs>
         <div className="row">
+          <h3>va-notification</h3>
+
+          <va-notification
+            closeBtnAriaLabel="Close notification"
+            closeable
+            date-time="Wednesday, May 11 at 1:13pm"
+            has-border
+            headline="Notification heading"
+            headline-level="3"
+            href="#"
+            symbol="none"
+            text="Manage your notifications"
+            visible
+          >
+            <p>Notification body</p>
+          </va-notification>
+
+          <hr />
+
+          <h3>va-checkbox</h3>
+
+          <va-checkbox
+            description={null}
+            error={null}
+            hint={null}
+            label="This is a cleverly-labelled checkbox"
+            enable-analytics
+          />
+
+          <hr />
+
+          <h3>va-checkbox-group</h3>
+
+          <va-checkbox-group
+            error={null}
+            hint={null}
+            label="This is a label"
+            enable-analytics
+          >
+            <va-checkbox label="Option one" name="example" value="1" />
+            <va-checkbox label="Option two" name="example" value="2" />
+          </va-checkbox-group>
+
+          <hr />
+
+          <h3>va-date</h3>
+
+          <va-date
+            label="What’s the date or anticipated date of your release from active duty?"
+            name="discharge-date"
+            enable-analytics
+          />
+
+          <hr />
+
+          <h3>va-memorable-date</h3>
+
+          <va-memorable-date
+            hint=""
+            label="Date of birth"
+            name="test"
+            enable-analytics
+          />
+
+          <hr />
+
+          <h3>va-link</h3>
+
+          <va-link
+            href="#"
+            text="Contact a local Veterans Service Organization (VSO)"
+          />
+
+          <hr />
+
+          <h3>va-loading-indicator</h3>
+
+          <va-loading-indicator
+            label="Loading"
+            message="Loading your application..."
+          />
+
+          <hr />
+
+          <h3>va-modal</h3>
+
+          <va-button
+            text="Click here to open modal"
+            onClick={handleModalOpen}
+          />
+          <va-modal
+            id="my-modal"
+            modal-title="Modal title goes here"
+            status="info"
+            primary-button-text="Primary button"
+            secondary-button-text="Secondary button"
+          >
+            <p>
+              A modal may pass any React nodes as children to be displayed
+              within it.
+            </p>
+            <p>
+              By default, a modal may have up to one primary button, and up to
+              one secondary button. Check the console log when clicking the
+              buttons to see the events fired.
+            </p>
+            <p>If more buttons are required, pass them in as children.</p>
+          </va-modal>
+
+          <hr />
+
+          <h3>va-number-input</h3>
+
+          <va-number-input
+            hint={null}
+            inputmode="numeric"
+            label="My input"
+            name="my-input"
+            value={0}
+          />
+
+          <hr />
+
+          <h3>va-select</h3>
+
+          <va-select
+            hint={null}
+            label="Branch of Service"
+            name="branch"
+            value="army"
+            enable-analytics
+          >
+            <option value="navy">Navy</option>
+            <option value="army">Army</option>
+            <option value="marines">Marines</option>
+            <option value="air-force">Air Force</option>
+            <option value="coastguard">Coastguard</option>
+          </va-select>
+
+          <hr />
+
+          <h3>va-radio</h3>
+
+          <va-radio
+            error={null}
+            hint=""
+            label="This is a label"
+            label-header-level=""
+            enable-analytics
+          >
+            <va-radio-option
+              enable-analytics
+              label="Option one"
+              name="example"
+              value="1"
+            />
+            <va-radio-option
+              label="Option two with an extra long label, so we can get it to wrap"
+              name="example"
+              value="2"
+              enable-analytics
+            />
+          </va-radio>
+
+          <hr />
+
+          <h3>va-pagination</h3>
+
+          <va-pagination page={3} pages={15} showLastPage enable-analytics />
+
+          <hr />
+
+          <h3>va-progress-bar</h3>
+
+          <va-progress-bar enable-analytics percent={100} />
+
+          <hr />
+
+          <h3>va-segmented-progress-bar</h3>
+
+          <va-segmented-progress-bar current={6} total={6} enable-analytics />
+
+          <hr />
+
           <DowntimeNotification
             appTitle="facility locator tool"
             dependencies={[externalServices.arcgis]}
