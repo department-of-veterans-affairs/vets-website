6152b0047735d0f1548102a0207a3f53381ad8aa
Nick Sprinkle <ndsprinkle@gmail.com>
2023-07-20 10:53:23 -0400

    Rework veteran title logic into a const

--- src/platform/forms-system/src/js/web-component-patterns/relationshipToVeteranPattern.jsx
+++ src/platform/forms-system/src/js/web-component-patterns/relationshipToVeteranPattern.jsx
@@ -2,21 +2,21 @@ import { radioUI, radioSchema } from './radioPattern';
 import VaTextInputField from '../web-component-fields/VaTextInputField';
 
 export const relationshipToVeteranUI = personTitle => {
+  const person = personTitle ?? 'veteran';
+
   return {
     relationshipToVeteran: radioUI({
-      title: `What’s your relationship to the ${personTitle ?? 'veteran'}?`,
+      title: `What’s your relationship to the ${person}?`,
       labels: {
-        spouse: `I’m the ${personTitle ?? 'veteran'}’s spouse`,
-        child: `I’m the ${personTitle ?? 'veteran'}’s child`,
-        parent: `I’m the ${personTitle ?? 'veteran'}’s parent`,
-        executor: `I’m the ${personTitle ??
-          'veteran'}’s executor or administrator of estate`,
+        spouse: `I’m the ${person}’s spouse`,
+        child: `I’m the ${person}’s child`,
+        parent: `I’m the ${person}’s parent`,
+        executor: `I’m the ${person}’s executor or administrator of estate`,
         other: 'We don’t have a relationship that’s listed here',
       },
     }),
     otherRelationshipToVeteran: {
-      'ui:title': `Since your relationship with the ${personTitle ??
-        'veteran'} was not listed, please describe it here`,
+      'ui:title': `Since your relationship with the ${person} was not listed, please describe it here`,
       'ui:webComponentField': VaTextInputField,
       'ui:options': {
         expandUnder: 'relationshipToVeteran',
