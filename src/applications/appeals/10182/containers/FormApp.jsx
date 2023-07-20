0788c3b4c4d8656f945ff5a4b3bb0944f64cd2cc
Robin Garrison <robin.garrison@adhocteam.us>
2023-07-20 10:18:45 -0500

    Init RUM

--- src/applications/appeals/10182/containers/FormApp.jsx
+++ src/applications/appeals/10182/containers/FormApp.jsx
@@ -6,6 +6,7 @@ import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import { selectProfile, isLoggedIn } from 'platform/user/selectors';
 import { setData } from 'platform/forms-system/src/js/actions';
 
+import { useBrowserMonitoring } from '../hooks/useBrowserMonitoring';
 import formConfig from '../config/form';
 import {
   noticeOfDisagreementFeature,
@@ -93,6 +94,9 @@ export const FormApp = ({
   if (showNod === false) {
     content = showWorkInProgress(formConfig);
   }
+
+  // Add Datadog UX monitoring to the application
+  useBrowserMonitoring();
 
   return (
     <article id="form-10182" data-location={`${location?.pathname?.slice(1)}`}>
