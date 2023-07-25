4d5ce26baee33de0bb8b811ef4efce7701ef41da
Richard Davis <richard.davis@agile6.com>
2023-07-25 09:36:38 -0400

    updates

--- src/applications/static-pages/priority-group-alert/index.js
+++ src/applications/static-pages/priority-group-alert/index.js
@@ -6,7 +6,8 @@ const createPriorityGroupAlertWidget = async (store, widgetType) => {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   if (!root) return;
 
-  const App = await import(/* webpackChunkName: "priority-group-alert" */ './components/App');
+  const App = await import(/* webpackChunkName: "priority-group-alert" */ './components/App')
+    .default;
   ReactDOM.render(
     <Provider store={store}>
       <App />
