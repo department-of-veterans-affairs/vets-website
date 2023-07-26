c1f482cdf48049dbce7eb9fac52908341f6d09a8
Micah <micah.frazier82@gmail.com>
2023-07-26 15:09:12 -0600

    eslint error blocking commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
+    // eslint-disable-next-line no-unused-vars
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
