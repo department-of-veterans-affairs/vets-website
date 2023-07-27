332e424ac53af8c3c866e542a8cec98e12d623f9
Robin Garrison <robin.garrison@adhocteam.us>
2023-07-27 12:58:37 -0500

    Hide contact info fields in datadog recordings

--- src/platform/user/profile/vap-svc/components/AddressField/AddressView.jsx
+++ src/platform/user/profile/vap-svc/components/AddressField/AddressView.jsx
@@ -6,17 +6,10 @@ export default function AddressView({ data: address }) {
   const { street, cityStateZip, country } = formatAddress(address);
 
   return (
-    <div>
-      {street}
-      <br />
-      {cityStateZip}
-
-      {country && (
-        <>
-          <br />
-          {country}
-        </>
-      )}
-    </div>
+    <>
+      <div className="dd-privacy-hidden">{street}</div>
+      <div className="dd-privacy-hidden">{cityStateZip}</div>
+      {country && <div className="dd-privacy-hidden">{country}</div>}
+    </>
   );
 }
