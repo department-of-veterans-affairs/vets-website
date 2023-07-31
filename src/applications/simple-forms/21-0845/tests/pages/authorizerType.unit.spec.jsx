f2537dde5aa9187d3440d2b4a544ebd47269f6f7
Tze-chiu Lei <tclei2009@gmail.com>
2023-07-31 16:56:03 -0700

    Add hideFormNavProgress test to 21-0845 authorizerType unit-spec

--- src/applications/simple-forms/21-0845/tests/pages/authorizerType.unit.spec.jsx
+++ src/applications/simple-forms/21-0845/tests/pages/authorizerType.unit.spec.jsx
@@ -1,10 +1,17 @@
+import React from 'react';
+import { expect } from 'chai';
+import { render } from '@testing-library/react';
+import { DefinitionTester } from 'platform/testing/unit/schemaform-utils';
 import {
   testNumberOfErrorsOnSubmit,
   testNumberOfFields,
 } from '../../../shared/tests/pages/pageTests.spec';
 import formConfig from '../../config/form';
+import authTypeVet from '../e2e/fixtures/data/authTypeVet.json';
 
 const {
+  arrayPath,
+  defaultDefinitions,
   schema,
   uiSchema,
 } = formConfig.chapters.authorizerTypeChapter.pages.authTypePage;
@@ -28,3 +35,21 @@ testNumberOfErrorsOnSubmit(
   expectedNumberOfErrors,
   pageTitle,
 );
+
+describe(`${pageTitle} - hideFormNavProgress`, () => {
+  it('renders page with header & progressbar hidden', () => {
+    const screen = render(
+      <DefinitionTester
+        arrayPath={arrayPath}
+        pagePerItemIndex={0}
+        definitions={defaultDefinitions}
+        schema={schema}
+        data={authTypeVet.data}
+        formData={authTypeVet.data}
+        uiSchema={uiSchema}
+      />,
+    );
+    expect(screen.queryAllByRole('progressbar')).to.be.empty;
+    expect(screen.queryAllByTestId('navFormHeader')).to.be.empty;
+  });
+});
