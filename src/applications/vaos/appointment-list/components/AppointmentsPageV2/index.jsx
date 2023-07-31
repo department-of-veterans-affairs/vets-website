7477b8c97a7cc0c7960722f18483438797c2b525
Ryan Shaw <ryan.shaw@adhocteam.us>
2023-07-31 16:35:22 -0700

    Removes unused conditionals and feature flags from the appointment list page
    This commit removes some unused conidtionals based on feature flags that are no longer in use. The following feature flags are no longer in use:
    
    - vaOnlineSchedulingAppointmentList
    - vaOnlineSchedulingStatusImprovement

--- src/applications/vaos/appointment-list/components/AppointmentsPageV2/index.jsx
+++ src/applications/vaos/appointment-list/components/AppointmentsPageV2/index.jsx
@@ -1,22 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Switch, Route, useHistory, useLocation } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
 import DowntimeNotification, {
   externalServices,
 } from 'platform/monitoring/DowntimeNotification';
 import PropTypes from 'prop-types';
-import {
-  selectFeatureStatusImprovement,
-  selectFeatureAppointmentList,
-  selectFeaturePrintList,
-} from '../../../redux/selectors';
-import RequestedAppointmentsList from '../RequestedAppointmentsList';
+import { selectFeaturePrintList } from '../../../redux/selectors';
 import UpcomingAppointmentsList from '../UpcomingAppointmentsList';
 import PastAppointmentsListV2 from '../PastAppointmentsListV2';
 import CanceledAppointmentsList from '../CanceledAppointmentsList';
 import WarningNotification from '../../../components/WarningNotification';
-import Select from '../../../components/Select';
 import ScheduleNewAppointment from '../ScheduleNewAppointment';
 import PageLayout from '../PageLayout';
 import { selectPendingAppointments } from '../../redux/selectors';
@@ -31,13 +25,6 @@ const DROPDOWN_VALUES = {
   past: 'past',
   canceled: 'canceled',
 };
-
-const options = [
-  { label: 'Upcoming', value: DROPDOWN_VALUES.upcoming },
-  { label: 'Requested', value: DROPDOWN_VALUES.requested },
-  { label: 'Past', value: DROPDOWN_VALUES.past },
-  { label: 'Canceled', value: DROPDOWN_VALUES.canceled },
-];
 
 function getDropdownValueFromLocation(pathname) {
   if (pathname.endsWith(DROPDOWN_VALUES.requested)) {
@@ -65,22 +52,6 @@ function getDropdownValueFromLocation(pathname) {
     dropdownValue: DROPDOWN_VALUES.upcoming,
     subPageTitle: 'Your appointments',
     subHeading: 'Your appointments',
-  };
-}
-
-function handleDropdownChange(history, setHasTypeChanged) {
-  return e => {
-    const { value } = e.detail;
-    if (value === DROPDOWN_VALUES.upcoming) {
-      history.push('/');
-    } else if (value === DROPDOWN_VALUES.requested) {
-      history.push('/requested');
-    } else if (value === DROPDOWN_VALUES.past) {
-      history.push('/past');
-    } else if (value === DROPDOWN_VALUES.canceled) {
-      history.push('/canceled');
-    }
-    setHasTypeChanged(true);
   };
 }
 
@@ -119,55 +90,32 @@ export default function AppointmentsPageV2() {
   const [hasTypeChanged, setHasTypeChanged] = useState(false);
   let [pageTitle] = useState('VA online scheduling');
 
-  const featureAppointmentList = useSelector(state =>
-    selectFeatureAppointmentList(state),
-  );
-  const featureStatusImprovement = useSelector(state =>
-    selectFeatureStatusImprovement(state),
-  );
-
   const pendingAppointments = useSelector(state =>
     selectPendingAppointments(state),
   );
   const isPrintList = useSelector(state => selectFeaturePrintList(state));
-  const {
-    dropdownValue,
-    subPageTitle,
-    subHeading,
-  } = getDropdownValueFromLocation(location.pathname);
+  const { subPageTitle } = getDropdownValueFromLocation(location.pathname);
 
   let prefix = 'Your';
   const isPending = location.pathname.endsWith('/pending');
   const isPast = location.pathname.endsWith('/past');
 
-  if (featureStatusImprovement) {
-    if (isPending) {
-      prefix = 'Pending';
-      pageTitle = `${prefix} appointments`;
-    } else if (isPast) {
-      prefix = 'Past';
-      pageTitle = `${prefix} appointments`;
-    } else {
-      pageTitle = 'Appointments';
-    }
+  if (isPending) {
+    prefix = 'Pending';
+    pageTitle = `${prefix} appointments`;
+  } else if (isPast) {
+    prefix = 'Past';
+    pageTitle = `${prefix} appointments`;
+  } else {
+    pageTitle = 'Appointments';
   }
+
   useEffect(
     () => {
-      if (featureStatusImprovement) {
-        document.title = `${pageTitle} | VA online scheduling | Veterans Affairs`;
-        scrollAndFocus('h1');
-      } else {
-        document.title = `${subPageTitle} | ${pageTitle} | Veterans Affairs`;
-        scrollAndFocus('h1');
-      }
+      document.title = `${pageTitle} | VA online scheduling | Veterans Affairs`;
+      scrollAndFocus('h1');
     },
-    [
-      subPageTitle,
-      featureStatusImprovement,
-      location.pathname,
-      prefix,
-      pageTitle,
-    ],
+    [subPageTitle, location.pathname, prefix, pageTitle],
   );
 
   const [count, setCount] = useState(0);
@@ -186,16 +134,9 @@ export default function AppointmentsPageV2() {
     [pendingAppointments],
   );
 
-  const history = useHistory();
+  const paragraphText =
+    'These appointment requests haven’t been scheduled yet.';
 
-  let paragraphText =
-    'Below is your list of appointment requests that haven’t been scheduled yet.';
-  if (featureAppointmentList) {
-    paragraphText = 'These appointment requests haven’t been scheduled yet.';
-  } else if (featureStatusImprovement) {
-    paragraphText =
-      'Your appointment requests that haven’t been scheduled yet.';
-  }
   return (
     <PageLayout showBreadcrumbs showNeedHelp>
       <h1 className={getSpacing({ isPrintList, isPast, isPending })}>
@@ -216,31 +157,12 @@ export default function AppointmentsPageV2() {
       />
       <ScheduleNewAppointment />
       <AppointmentListNavigation count={count} callback={setHasTypeChanged} />
-      {!featureStatusImprovement && (
-        <>
-          <h2 className="vads-u-margin-y--3">{subHeading}</h2>
-          {/* Commenting out for now. See https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/718 */}
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <Select
-            options={options}
-            onChange={handleDropdownChange(history, setHasTypeChanged)}
-            id="type-dropdown"
-            value={dropdownValue}
-            label="Show by status"
-          />
-        </>
-      )}
       <Switch>
         <Route exact path="/">
           <UpcomingAppointmentsList hasTypeChanged={hasTypeChanged} />
         </Route>
-        <Route path={featureStatusImprovement ? '/pending' : '/requested'}>
-          {featureStatusImprovement && (
-            <RequestedAppointmentsListGroup hasTypeChanged={hasTypeChanged} />
-          )}
-          {!featureStatusImprovement && (
-            <RequestedAppointmentsList hasTypeChanged={hasTypeChanged} />
-          )}
+        <Route path="/pending">
+          <RequestedAppointmentsListGroup hasTypeChanged={hasTypeChanged} />
         </Route>
         <Route path="/past">
           <PastAppointmentsListV2 hasTypeChanged={hasTypeChanged} />
