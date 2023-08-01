04f66ffa4abcd8a0c9eff29aee72c95398e040ef
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2023-08-01 16:30:48 -0400

    Test

--- script/github-actions/check-e2e-status.js
+++ script/github-actions/check-e2e-status.js
@@ -1,9 +1,9 @@
 const core = require('@actions/core');
 
-const { NOT_STRESS_TEST } = process.env;
+const TESTS_TO_STRESS_TEST = JSON.parse(process.env.TESTS_TO_STRESS_TEST);
 const E2E_BLOCKED_PATHS = JSON.parse(process.env.E2E_BLOCKED_PATHS);
 
-if (E2E_BLOCKED_PATHS.length > 0 && NOT_STRESS_TEST) {
+if (E2E_BLOCKED_PATHS.length > 0 && TESTS_TO_STRESS_TEST.length === 0) {
   core.setFailed(
     `One or more directories contain tests that have been disabled for a total exceeding 90 days. Merging is blocked until all warnings are cleared. The paths in question are: ${E2E_BLOCKED_PATHS}`,
   );
