e1ca39254adf9e43be011d9d2cfd95ed114d7d07
Adam Antonioli <adam.antonioli@va.gov>
2023-08-01 14:50:59 -0400

    MBMS-43474 change content on pre feed form introduction

--- src/applications/pre-need/components/IntroductionPage.jsx
+++ src/applications/pre-need/components/IntroductionPage.jsx
@@ -43,8 +43,7 @@ class IntroductionPage extends React.Component {
           />
         )}
         <h2 className="vads-u-font-size--h3">
-          Follow the steps below to apply for a pre-need eligibility
-          determination
+          Follow the steps below to get started
         </h2>
         <div className="process schemaform-process">
           <ol>
@@ -53,6 +52,14 @@ class IntroductionPage extends React.Component {
               <h4>When you apply, be sure to have these on hand:</h4>
               <ul>
                 <li>
+                  Personal information (including Social Security number and
+                  date of birth).
+                </li>
+                <li>
+                  Military history or the military history of the service member
+                  or Veteran who’s sponsoring your application.
+                </li>
+                <li>
                   The name of the VA national cemetery where you’d prefer to be
                   buried.
                   <br />
@@ -60,77 +67,36 @@ class IntroductionPage extends React.Component {
                     Find a VA national cemetery
                   </a>
                 </li>
-                <li>
-                  Service history or the service history of the service member
-                  or Veteran who’s sponsoring your application. This includes
-                  your or your sponsor’s:
-                  <ul>
-                    <li>
-                      Social Security number (and Military Service number if
-                      it’s different than the Social Security number)
-                    </li>
-                    <li>VA claim number (if you know it)</li>
-                    <li>Date and place of birth</li>
-                    <li>
-                      Date of death (if the service member or Veteran has died)
-                    </li>
-                    <li>Military status and history</li>
-                  </ul>
-                </li>
               </ul>
-              <h4>You’ll need to upload:</h4>
+              <h4>
+                To help us process your application faster, you can upload:
+              </h4>
               <p>
                 A copy of your or your sponsor’s DD214 or other separation
-                documents
+                files.
               </p>
-              <h4>
-                Additional information and documents needed for certain
-                applicants:
-              </h4>
+              <h4>Other information for certain applicants you can provide:</h4>
               <ul>
                 <li className="vads-u-margin-y--2">
                   <strong>
                     If you’re applying as the legally married spouse or
-                    surviving spouse or the unmarried adult child of a service
-                    member or Veteran
+                    surviving spouse of a service member or Veteran
                   </strong>
                   , you’ll need your personal information (including Social
                   Security number and date of birth).
                 </li>
                 <li>
                   <strong>If you’re applying on behalf of someone else</strong>,
-                  you’ll need to upload supporting documents or an affidavit (a
-                  written statement of facts confirmed by an oath or
-                  affirmation) showing that you’re:
-                  <ul>
-                    <li>
-                      The applicant’s court-appointed representative,{' '}
-                      <strong>or</strong>
-                    </li>
-                    <li>
-                      The applicant’s caregiver (including a spouse or other
-                      relative), <strong>or</strong>
-                    </li>
-                    <li>
-                      An attorney or agent acting on behalf of the applicant
-                      under a durable power of attorney, <strong>or</strong>
-                    </li>
-                    <li>
-                      The manager or principal officer of an institution in
-                      which the applicant is being cared for
-                    </li>
-                  </ul>
+                  you can upload supporting files or an affidavit showing you
+                  can apply on their behalf.
                 </li>
                 <li>
                   <strong>
-                    If you're applying for an unmarried adult child
+                    If you’re applying for an unmarried adult child of a service
+                    member or Veteran
                   </strong>
-                  , you’ll need to upload supporting documents showing:
-                  <ul>
-                    <li>Medical evidence of a disability</li>
-                    <li>Start date of a disability</li>
-                    <li>The child's age when diagnosed with a disability</li>
-                  </ul>
+                  , you can upload supporting files with information about the
+                  adult child’s disability.
                 </li>
               </ul>
               <h4>What if I need help filling out my application?</h4>
@@ -145,10 +111,10 @@ class IntroductionPage extends React.Component {
             </li>
             <li className="process-step list-two">
               <h3>Apply</h3>
-              <p>Complete this pre-need eligibility determination form.</p>
               <p>
-                After submitting the form, you’ll get a confirmation message.
-                You can print this for your records.
+                Complete the pre-need eligibility determination form. After
+                submitting the form, you’ll get a confirmation message that you
+                can print for your records.
               </p>
               <div style={{ marginBottom: '-25px' }}>
                 <SaveInProgressIntro
@@ -162,13 +128,15 @@ class IntroductionPage extends React.Component {
             </li>
             <li className="process-step list-three">
               <h3>VA review</h3>
-              <p>We’ll let you know by mail if we need more information.</p>
+              <p>
+                We’ll let you know by phone or mail if we need more information.
+              </p>
             </li>
             <li className="process-step list-four">
               <h3>Decision</h3>
               <p>
-                After we process your claim, you'll get a notice in the mail
-                about the decision.
+                You’ll get a notice in the mail about the decision after we
+                process your claim.
               </p>
             </li>
           </ol>
