1043d74674228f859066f835e262a4d4fed64756
Tze-chiu Lei <tclei2009@gmail.com>
2023-07-31 17:49:35 -0700

    Add custom-label test to 21-0845 personAddress unit-spec

--- src/applications/simple-forms/21-0845/tests/pages/personAddress.unit.spec.jsx
+++ src/applications/simple-forms/21-0845/tests/pages/personAddress.unit.spec.jsx
@@ -1,10 +1,21 @@
+import React from 'react';
+
+import { expect } from 'chai';
+import { render } from '@testing-library/react';
+
+import { DefinitionTester } from 'platform/testing/unit/schemaform-utils';
+import { cloneDeep } from 'lodash';
 import {
   testNumberOfErrorsOnSubmit,
   testNumberOfFields,
 } from '../../../shared/tests/pages/pageTests.spec';
 import formConfig from '../../config/form';
+import authTypeVet from '../e2e/fixtures/data/authTypeVet.json';
 
+const data = cloneDeep(authTypeVet.data);
+data.thirdPartyType = 'person';
 const {
+  defaultDefinitions,
   schema,
   uiSchema,
 } = formConfig.chapters.disclosureInfoChapter.pages.personAddressPage;
@@ -28,3 +39,20 @@ testNumberOfErrorsOnSubmit(
   expectedNumberOfErrors,
   pageTitle,
 );
+
+describe(`${pageTitle} - custom-street2-label`, () => {
+  it('renders street2 custom-label', () => {
+    const screen = render(
+      <DefinitionTester
+        definitions={defaultDefinitions}
+        schema={schema}
+        data={data}
+        formData={data}
+        uiSchema={uiSchema}
+      />,
+    );
+
+    expect(screen.queryAllByText('Street address line 2')).to.have.lengthOf(0);
+    expect(screen.queryAllByText('Apt./Unit number')).to.have.lengthOf(1);
+  });
+});
