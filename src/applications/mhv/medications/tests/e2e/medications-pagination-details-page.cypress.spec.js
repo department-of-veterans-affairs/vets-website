f605698480539b58559dcd175d267642bd8bca4d
haniasma <121971510+haniasma@users.noreply.github.com>
2023-08-03 12:25:23 -0400

    Update medications-pagination-details-page.cypress.spec.js

--- src/applications/mhv/medications/tests/e2e/medications-pagination-details-page.cypress.spec.js
+++ src/applications/mhv/medications/tests/e2e/medications-pagination-details-page.cypress.spec.js
@@ -1,6 +1,9 @@
 import MedicationsSite from './med_site/MedicationsSite';
 import mockRxPageOne from './fixtures/prescriptions.json';
 import mockRxPageTwo from './fixtures/presciptions-page-2.json';
+import MedicationsDetailsPage from './pages/MedicationsDetailsPage';
+import MedicationsListPage from './pages/MedicationsListPage';
+import mockPrescriptionDetails from './fixtures/prescription-details.json';
 
 describe('Medications Landing Page', () => {
   it('visits Medications landing Page', () => {
@@ -23,6 +26,35 @@ describe('Medications Landing Page', () => {
     site.verifyPaginationPrescirptionsDisplayed(21, 21, threadLength);
     site.loadVAPaginationPreviousPrescriptions(1, mockRxPageOne, 20);
     site.verifyPaginationPrescirptionsDisplayed(1, 20, threadLength);
+    cy.get(
+      '.vads-l-row > :nth-child(1) > .rx-card-detials > .link-to-details > [data-testid="medications-history-details-link"]',
+    ).click();
+    cy.injectAxe();
+    cy.axeCheck('main', {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+        'link-name': {
+          enabled: false,
+        },
+      },
+    });
+  });
+});
+
+describe('verify navigation to medication details Page', () => {
+  it('verify Medications details Page', () => {
+    const site = new MedicationsSite();
+    const listPage = new MedicationsListPage();
+    const detailsPage = new MedicationsDetailsPage();
+    cy.visit('my-health/medications/');
+    site.login();
+    listPage.clickGotoMedicationsLink();
+    detailsPage.clickMedicationHistoryAndDetailsLink(mockPrescriptionDetails);
+    detailsPage.clickWhatToKnowAboutMedicationsDropDown();
+    detailsPage.verifyTextInsideDropDownOnDetailsPage();
+
     cy.injectAxe();
     cy.axeCheck('main', {
       rules: {
