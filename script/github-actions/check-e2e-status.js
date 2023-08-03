410d0cc1c2762c0083534ace7ebbf7abeff465b6
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2023-08-03 13:00:42 -0400

    Move logic around

--- script/github-actions/check-e2e-status.js
+++ script/github-actions/check-e2e-status.js
@@ -1,11 +1,18 @@
-const core = require('@actions/core');
+/* eslint-disable no-console */
 
-const TESTS_TO_STRESS_TEST = JSON.parse(process.env.TESTS_TO_STRESS_TEST);
+// const core = require('@actions/core');
+
 const E2E_BLOCKED_PATHS = JSON.parse(process.env.E2E_BLOCKED_PATHS);
+const ALLOW_LIST = JSON.parse(process.env.ALLOW_LIST);
 
-if (E2E_BLOCKED_PATHS.length > 0 && TESTS_TO_STRESS_TEST.length === 0) {
-  core.setFailed(
-    `One or more directories contain tests that have been disabled for a total exceeding 90 days. Merging is blocked until all warnings are cleared. The paths in question are: ${E2E_BLOCKED_PATHS}`,
-  );
-  process.exit(1);
-}
+console.log('blocked paths', E2E_BLOCKED_PATHS);
+console.log('allow list: ', ALLOW_LIST);
+
+// const containsWarningsPastLimit = ALLOW_LIST.some(entry => entry.);
+// if (E2E_BLOCKED_PATHS.length > 0) {
+//   const specsBeingUpdated = E2E_BLOCKED_PATHS.map(spec => spec.split('/').pop);
+//   const specsBeingStressTested = core.setFailed(
+//     `One or more directories contain tests that have been disabled for a total exceeding 90 days. Merging is blocked until all warnings are cleared. The paths in question are: ${E2E_BLOCKED_PATHS}`,
+//   );
+//   process.exit(1);
+// }
