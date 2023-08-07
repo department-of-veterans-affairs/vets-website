6ff453d71719a7b6491deebf5a6bc234c678ebb5
Eric Tillberg <Thrillberg@users.noreply.github.com>
2023-08-07 10:52:33 -0400

    updates per feedback

--- src/applications/simple-forms/21-0972/definitions/constants.js
+++ src/applications/simple-forms/21-0972/definitions/constants.js
@@ -5,30 +5,30 @@ export const claimantIdentificationKeys = [
   'CHILD',
 ];
 
-export const claimantIdentificationOptions = {
+export const claimantIdentificationOptions = Object.freeze({
   [claimantIdentificationKeys[0]]: 'I’m signing for a Veteran.',
   [claimantIdentificationKeys[1]]: 'I’m signing for a spouse of a Veteran.',
   [claimantIdentificationKeys[2]]: 'I’m signing for a parent of a Veteran.',
   [claimantIdentificationKeys[3]]: 'I’m signing for a child of a Veteran.',
-};
+});
 
-export const claimantIdentificationDisplayOptions = {
+export const claimantIdentificationDisplayOptions = Object.freeze({
   [claimantIdentificationKeys[0]]: 'Veteran',
   [claimantIdentificationKeys[1]]: 'Veteran’s spouse',
   [claimantIdentificationKeys[2]]: 'Veteran’s parent',
   [claimantIdentificationKeys[3]]: 'Veteran’s child',
-};
+});
 
-export const preparerQualificationsOptions = {
+export const preparerQualificationsOptions = Object.freeze({
   COURT_APPOINTED_REP: 'Court-appointed representative',
   ATTORNEY: 'Attorney in fact or agent',
   CAREGIVER: 'Caregiver',
   MANAGER: 'Manager or Principal Officer',
-};
+});
 
-export const preparerSigningReasonOptions = {
+export const preparerSigningReasonOptions = Object.freeze({
   UNDER18: 'They’re under 18 years old.',
   MENTALLY_INCAPABLE:
     'They have an illness, injury, or other health condition that prevents them from being able to make decisions for themselves or provide the information needed to complete forms.',
   PHYSICALLY_INCAPABLE: 'They can’t physically sign the forms.',
-};
+});
