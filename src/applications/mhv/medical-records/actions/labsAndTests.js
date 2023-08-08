0101287858480b06a1edce39e3259b020bc3c75b
wright0776 <matwright2010@gmail.com>
2023-08-08 09:22:50 -0600

    MHV-45441: Chem/hem mhv data hookup work done

--- src/applications/mhv/medical-records/actions/labsAndTests.js
+++ src/applications/mhv/medical-records/actions/labsAndTests.js
@@ -2,8 +2,22 @@ import { Actions } from '../util/actionTypes';
 import { getLabsAndTests, getLabOrTest } from '../api/MrApi';
 
 export const getLabsAndTestsList = () => async dispatch => {
-  const response = await getLabsAndTests();
-  dispatch({ type: Actions.LabsAndTests.GET_LIST, response });
+  try {
+    const response = await getLabsAndTests();
+    dispatch({ type: Actions.LabsAndTests.GET_LIST, response });
+  } catch (error) {
+    // console.error(error);
+    // const err = error.errors[0];
+    // dispatch({
+    //   type: Actions.Alerts.ADD_ALERT,
+    //   payload: {
+    //     alertType: 'error',
+    //     header: err.title,
+    //     content: err.detail,
+    //     response: err,
+    //   },
+    // });
+  }
 };
 
 export const getlabsAndTestsDetails = labId => async dispatch => {
