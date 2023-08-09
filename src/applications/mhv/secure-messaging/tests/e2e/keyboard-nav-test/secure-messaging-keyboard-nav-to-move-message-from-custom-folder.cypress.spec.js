9995337be888b04550ac1dcf5911f32566270496
fazilqa <fazil.mammadov@bylight.com>
2023-08-09 10:28:02 -0700

    add tests to existing spec

--- src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-move-message-from-custom-folder.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-move-message-from-custom-folder.cypress.spec.js
@@ -5,6 +5,9 @@ import mockCustomFolderResponse from '../fixtures/folder-custom-metadata.json';
 import mockCustomMessagesResponse from '../fixtures/message-custom-response.json';
 import mockFoldersResponse from '../fixtures/folder-response.json';
 import mockCustomDetails from '../fixtures/custom-response.json';
+import PatientMessageDetailsPage from '../pages/PatientMessageDetailsPage';
+import mockMessages from '../fixtures/messages-response.json';
+import mockMessagewithAttachment from '../fixtures/message-response-withattachments.json';
 
 describe('Secure Messaging Keyboard Nav Move Message from CustomFolder', () => {
   it('move message', () => {
@@ -35,5 +38,50 @@ describe('Secure Messaging Keyboard Nav Move Message from CustomFolder', () => {
         },
       },
     });
+  });
+});
+
+describe('Move message to another folder ', () => {
+  it('Keyboard Navigation to Print Button', () => {
+    const landingPage = new PatientInboxPage();
+    const messageDetailsPage = new PatientMessageDetailsPage();
+    const site = new SecureMessagingSite();
+    site.login();
+    landingPage.loadInboxMessages(mockMessages, mockMessagewithAttachment);
+    messageDetailsPage.loadMessageDetails(mockMessagewithAttachment);
+
+    cy.intercept(
+      'PATCH',
+      'my_health/v1/messaging/threads/7176615/move?folder_id=-3',
+      {},
+    );
+
+    cy.get('[data-testid="move-button-text"]').click();
+
+    cy.get('va-radio[class="form-radio-buttons hydrated"]')
+      .find('[for="radiobutton-Deleted"]')
+      .should('exist')
+      .click();
+
+    cy.get('[type="button"]')
+      .contains('Confirm')
+      .should('exist')
+      .click();
+
+    cy.injectAxe();
+    cy.axeCheck('main', {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+        'color-contrast': {
+          enabled: false,
+        },
+      },
+    });
+
+    cy.get('[close-btn-aria-label="Close notification"]')
+      .should('exist')
+      .and('have.text', 'Message conversation was successfully moved.');
   });
 });
