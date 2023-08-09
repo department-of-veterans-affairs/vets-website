1fd7721f9ff7b115a53d1405845d64e1dbb00fef
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2023-08-09 16:35:07 -0400

    styling tweaks

--- src/applications/mhv/secure-messaging/components/ComposeForm/ComposeFormActionButtons.jsx
+++ src/applications/mhv/secure-messaging/components/ComposeForm/ComposeFormActionButtons.jsx
@@ -16,7 +16,7 @@ const ComposeFormActionButtons = ({
           text="Send"
           label="Send"
           id="send-button"
-          class={`vads-u-padding-x--0p5 vads-u-width--full vads-u-margin-bottom--2 
+          class={`small-screen:vads-u-padding-right--0p5  vads-u-width--full vads-u-margin-bottom--2 
                       small-screen:vads-u-flex--1 vads-u-margin-top--0 vads-u-margin-right--0`}
           data-testid="Send-Button"
           onClick={onSend}
@@ -26,7 +26,7 @@ const ComposeFormActionButtons = ({
       <button
         type="button"
         id="save-draft-button"
-        className={`usa-button usa-button-secondary save-draft-button vads-u-width--full vads-u-margin-left--0p5
+        className={`usa-button usa-button-secondary save-draft-button vads-u-width--full small-screen:vads-u-margin-left--0p5
                           xsmall-screen:vads-u-flex--1 vads-u-margin-top--0 xsmall-screen:vads-u-margin-right--1 
                           vads-u-margin-right--0`}
         data-testid="Save-Draft-Button"
