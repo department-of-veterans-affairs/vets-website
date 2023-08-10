55f82a34138d407c2d5dfc75b907041ca08561ea
John Woolschlager <wullaski@gmail.com>
2023-08-10 16:29:04 -0400

    Add feature flag to names (#25151)
    * Add feature flag to names
    
    * Added unified landing page selector, mocks, and test
    
    * Update the readme with feature toggle stuff
    
    * Fix test

--- src/applications/check-in/utils/selectors/feature-toggles.js
+++ src/applications/check-in/utils/selectors/feature-toggles.js
@@ -27,6 +27,9 @@ const selectFeatureToggles = createSelector(
     isPreCheckInActionLinkTopPlacementEnabled: toggleValues(state)[
       FEATURE_FLAG_NAMES.checkInExperiencePreCheckInActionLinkTopPlacement
     ],
+    isUnifiedLandingPageEnabled: toggleValues(state)[
+      FEATURE_FLAG_NAMES.checkInExperienceUnifiedLandingPage
+    ],
   }),
   toggles => toggles,
 );
