5b647092e2c659e829c07623ba7bf1c065562116
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2023-08-11 09:42:43 -0400

    Test restructure of check

--- script/github-actions/check-e2e-status.js
+++ script/github-actions/check-e2e-status.js
@@ -3,17 +3,51 @@
 
 const core = require('@actions/core');
 
-const E2E_BLOCKED_PATHS = JSON.parse(process.env.E2E_BLOCKED_PATHS);
 const ALLOW_LIST = JSON.parse(process.env.ALLOW_LIST);
+// const CHANGED_FILE_PATHS = process.env.CHANGED_FILE_PATHS
+//   ? process.env.CHANGED_FILE_PATHS.split(' ')
+//   : [];
+const CHANGED_FILE_PATHS = 'src/applications/financial-status-report/utils/transform.js'.split(
+  ' ',
+);
+console.log('CHANGED_FILE_PATHS', CHANGED_FILE_PATHS);
+
+function getDaysSinceDate(diff) {
+  const daysSinceDate =
+    (new Date().getTime() - new Date(diff).getTime()) / (1000 * 3600 * 24);
+  return daysSinceDate < 1
+    ? Math.ceil(daysSinceDate)
+    : Math.round(daysSinceDate);
+}
+
+const allDisallowedTestsWithWarnings = ALLOW_LIST.filter(
+  spec => spec.allowed === false && getDaysSinceDate(spec.warned_at) > 60,
+).map(spec => spec.spec_path);
+console.log('allDisallowedTestsWithWarnings', allDisallowedTestsWithWarnings);
+
+const appsAdjusted = CHANGED_FILE_PATHS.map(specPath =>
+  specPath
+    .split('/')
+    .slice(specPath.indexOf('src'), 3)
+    .join('/'),
+);
+console.log('appsAdjusted', appsAdjusted);
+
+const blockedPathsWithCodeChanges = allDisallowedTestsWithWarnings.filter(
+  entry => appsAdjusted.some(appPath => entry.includes(appPath)),
+);
+console.log('blockedPathsWithCodeCHanges', blockedPathsWithCodeChanges);
 
 const warningsExistPastLimit = ALLOW_LIST.some(
   entry =>
-    E2E_BLOCKED_PATHS.indexOf(entry.spec_path) > -1 && entry.allowed === false,
+    blockedPathsWithCodeChanges.indexOf(entry.spec_path) > -1 &&
+    entry.allowed === false,
 );
+console.log('warningsExistPastLimit', warningsExistPastLimit);
 
 if (warningsExistPastLimit) {
   core.setFailed(
-    `One or more directories contain tests that have been disabled for a total exceeding 90 days. In the future, merging will be blocked until all warnings are cleared. The paths in question are: ${E2E_BLOCKED_PATHS}`,
+    `One or more directories contain tests that have been disabled for a total exceeding 90 days. In the future, merging will be blocked until all warnings are cleared. The paths in question are: ${blockedPathsWithCodeChanges}`,
   );
   process.exit(1);
 }
