a85b37cbdb922acb0de4dd02ff23f6a8fe1b432d
gerard-sullivan <132001256+gerard-sullivan@users.noreply.github.com>
2023-08-11 08:01:01 -0700

    Update LandingPage.jsx
    eslint

--- src/applications/mhv/medications/containers/LandingPage.jsx
+++ src/applications/mhv/medications/containers/LandingPage.jsx
@@ -34,7 +34,9 @@ const LandingPage = () => {
               Email your feedback and questions to us at <FeedbackEmail />.
             </p>
             <p>
-              <strong>Note:</strong> You still have access to the pharmacy tool on the My HealtheVet website. You can go back to that site at any time.{' '}
+              <strong>Note:</strong>You still have access to the pharmacy tool 
+              on the My HealtheVet website. 
+              You can go back to that site at any time.{' '}
               <a
                 href={mhvUrl(isAuthenticatedWithSSOe(fullState), 'pharmacy')}
                 target="_blank"
