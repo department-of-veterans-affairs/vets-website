97eac09a16d5e1b85cf10054e28981cb3154ec2f
Eric Tillberg <Thrillberg@users.noreply.github.com>
2023-08-15 11:52:20 -0400

    Remove conditional WIP page

--- src/applications/simple-forms/21P-0847/containers/App.jsx
+++ src/applications/simple-forms/21P-0847/containers/App.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
 
-import { connect } from 'react-redux';
-
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
-import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
 import formConfig from '../config/form';
-import { WIP } from '../../shared/components/WIP';
-import { workInProgressContent } from '../definitions/constants';
 
-export function App({ location, children, show21P0847 }) {
-  if (!show21P0847) {
-    return <WIP content={workInProgressContent} />;
-  }
-
+export default function App({ location, children }) {
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
     </RoutedSavableApp>
   );
 }
-
-const mapStateToProps = state => ({
-  show21P0847: toggleValues(state)[FEATURE_FLAG_NAMES.form21P0847] || false,
-});
-
-export default connect(mapStateToProps)(App);
--- src/applications/simple-forms/21P-0847/definitions/constants.js
+++
@@ -1,6 +0,0 @@
-export const workInProgressContent = {
-  description:
-    'We’re rolling out the request to be a substitute for a deceased claimant (VA Form 21P-0847) in stages. It’s not quite ready yet. Please check back again soon.',
-  redirectLink: '/',
-  redirectText: 'Return to VA home page',
-};
