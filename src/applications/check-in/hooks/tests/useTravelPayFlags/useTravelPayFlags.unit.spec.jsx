638ddce5cc9493d01a94234b4638cfa10402bc41
Gaurav Gupta <ggupta@kindsys.us>
2023-08-18 09:39:42 -0700

    63318 fix test

--- src/applications/check-in/hooks/tests/useTravelPayFlags/useTravelPayFlags.unit.spec.jsx
+++ src/applications/check-in/hooks/tests/useTravelPayFlags/useTravelPayFlags.unit.spec.jsx
@@ -104,7 +104,7 @@ describe('check-in', () => {
         const middleware = [];
         const mockStore = configureStore(middleware);
         const initState = {
-          features: {
+          featureToggles: {
             // eslint-disable-next-line camelcase
             check_in_experience_travel_logic: true,
           },
