61464e44a0384e251ffc8c47cd123212e6ff7744
Adam Whitlock <adam.whitlock@adhocteam.us>
2023-08-18 14:05:34 -0600

    feat: allow saving with no changes to redirect to notifs

--- src/applications/personalization/profile/mocks/server.js
+++ src/applications/personalization/profile/mocks/server.js
@@ -55,7 +55,7 @@ const genericErrors = {
 const responses = {
   'GET /v0/user': (_req, res) => {
     // example user data cases
-    // return res.json(user.loa3User72); // default user (success)
+    return res.json(user.loa3User72); // default user (success)
     // return res.json(user.loa1User); // user with loa1
     // return res.json(user.badAddress); // user with bad address
     // return res.json(user.loa3User); // user with loa3
@@ -63,7 +63,7 @@ const responses = {
     // return res.json(user.externalServiceError); // external service error
     // return res.json(user.loa3UserWithNoMobilePhone); // user with no mobile phone number
     // return res.json(user.loa3UserWithNoEmail); // user with no email address
-    return res.json(user.loa3UserWithNoEmailOrMobilePhone); // user without email or mobile phone
+    // return res.json(user.loa3UserWithNoEmailOrMobilePhone); // user without email or mobile phone
   },
   'GET /v0/profile/status': status.success,
   'OPTIONS /v0/maintenance_windows': 'OK',
