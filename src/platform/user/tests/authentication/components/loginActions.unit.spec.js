d6b54962e6f8d388411d3dd4be24d81fc7bb902f
moanu404 <anu@themostudios.com>
2023-08-21 10:50:37 -0700

    test and some changes

--- src/platform/user/tests/authentication/components/loginActions.unit.spec.js
+++ src/platform/user/tests/authentication/components/loginActions.unit.spec.js
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 import { mount } from 'enzyme';
 import sinon from 'sinon';
 
+import { render } from '@testing-library/react';
 import * as authUtilities from '../../../authentication/utilities';
 import LoginActions from '../../../authentication/components/LoginActions';
 import { CSP_IDS } from '../../../authentication/constants';
@@ -16,6 +17,17 @@ describe('login DOM ', () => {
 
   afterEach(() => {
     sandbox.restore();
+  });
+
+  it('text alert modal is viewable to users', () => {
+    const view = render(<LoginActions />);
+    expect(view.getByText(`Sign in with your Login.gov or ID.me account`)).to
+      .exist;
+    expect(
+      view.getByText(
+        `Soon all VA websites will follow a new, more secure sign-in process. You’ll need to sign in using your Login.gov or ID.me account. So you’re ready for the change, try signing in now with Login.gov or ID.me.`,
+      ),
+    ).to.exist;
   });
 
   it('login buttons should properly call login method', () => {
@@ -62,7 +74,7 @@ describe('login DOM ', () => {
       const loginButtons = mount(<LoginActions externalApplication={csp} />);
       expect(loginButtons.find('button').length).to.eql(4);
       expect(loginButtons.find('#create-account').exists()).to.be.true;
-      expect(loginButtons.find('a').length).to.eql(2);
+      expect(loginButtons.find('a').length).to.eql(3);
       loginButtons.unmount();
     });
   });
