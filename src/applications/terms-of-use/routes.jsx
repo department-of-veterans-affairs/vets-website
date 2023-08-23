90d43f4259e1a31089f20e3adb29e12178de6e2d
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2023-08-23 13:31:08 -0400

    updates: adds decline page routing + empty Declined component

--- src/applications/terms-of-use/routes.jsx
+++ src/applications/terms-of-use/routes.jsx
@@ -1,8 +1,13 @@
 import TermsOfUse from './containers/TermsOfUse';
+import Declined from './components/Declined';
 
 export default [
   {
     path: '/',
     component: TermsOfUse,
+  },
+  {
+    path: '/declined',
+    component: Declined,
   },
 ];
