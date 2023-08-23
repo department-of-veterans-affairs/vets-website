99bd346d19e1d7df5ac92eb61a93c3bc5a9b764d
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2023-08-23 16:12:34 -0400

    updates: fixes capitalization in Declined + fixes SubmitSignInForm

--- src/applications/terms-of-use/components/Declined.jsx
+++ src/applications/terms-of-use/components/Declined.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleLoginModal } from 'platform/site-wide/user-nav/actions';
+import SubmitSignInForm from 'platform/static-data/SubmitSignInForm';
 
 export default function Placeholder() {
   const dispatch = useDispatch();
@@ -13,7 +14,7 @@ export default function Placeholder() {
       <div className="usa-content">
         <h1>We’ve signed you out of VA.gov</h1>
         <va-alert visible status="error">
-          You didn’t accept VA online services Terms of Use, so we signed you
+          You didn’t accept VA online services terms of use, so we signed you
           out of the site.
         </va-alert>
         <h2>What you can do:</h2>
@@ -22,9 +23,9 @@ export default function Placeholder() {
           We’ll take you back to the terms of use.{' '}
         </p>
         <p>
-          <strong>Note:</strong> You can still get VA benefits without using VA
-          online services. If you need help or have questions, call us at
-          800-698-2411 (TTY: 711). We’re here 24/7.
+          <strong>Note:</strong> You can still get VA benefits in-person without
+          using VA online services. If you need help or have questions,{' '}
+          <SubmitSignInForm /> We’re here 24/7.
         </p>
         <button type="button" onClick={openSignInModal}>
           Sign in
