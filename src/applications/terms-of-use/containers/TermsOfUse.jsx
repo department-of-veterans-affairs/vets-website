3aea473edc6716fb9ae9428a5ebac52df551f795
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2023-08-23 13:36:17 -0400

    updates: fixes TermsOfUse to match figma design

--- src/applications/terms-of-use/containers/TermsOfUse.jsx
+++ src/applications/terms-of-use/containers/TermsOfUse.jsx
@@ -1,5 +1,132 @@
 import React from 'react';
+import SubmitSignInForm from 'platform/static-data/SubmitSignInForm';
+
+const pr = new Intl.PluralRules('en-US', { type: 'ordinal' });
+
+const suffixes = new Map([
+  ['one', 'st'],
+  ['two', 'nd'],
+  ['few', 'rd'],
+  ['other', 'th'],
+]);
+const formatOrdinals = n => {
+  const rule = pr.select(n);
+  const suffix = suffixes.get(rule);
+  return `${n}${suffix}`;
+};
 
 export default function TermsOfUse() {
-  return <div>terms-of-use</div>;
+  return (
+    <>
+      <va-breadcrumbs label="Terms of use breadcrumbs">
+        <a href="/">Home</a>
+        <a href="#terms-of-use">VA online services terms of use</a>
+      </va-breadcrumbs>
+      <section
+        className="vads-l-grid-container vads-u-padding-y--5 vads-u-padding-x--0"
+        id="terms-of-use"
+      >
+        <div className="usa-content">
+          <h1>VA online services terms of use</h1>
+          <p className="va-introtext">
+            To sign in to VA.gov and most other VA online services, you’ll need
+            to accept the terms of use. We recently updated the terms. Read the
+            updated terms on this page. Then confirm if you accept or not.
+          </p>
+          <div id="terms-of-use-metadata">
+            <p>
+              Version: 1<br />
+              Last updated: {new Date().toLocaleDateString()}
+            </p>
+            <p>
+              If you want to save or print the terms, you can download a copy
+              now.
+            </p>
+            <va-link
+              href="#download"
+              download
+              fileType="PDF"
+              pages={6}
+              text="Download VA online services terms of use"
+            />
+          </div>
+          <div id="terms-of-use-content">
+            <h2>Terms of Use</h2>
+            <p>
+              The Department of Veterans Affairs (VA) owns and manages VA.gov
+              and the My HealtheVet health management portal. VA.gov allows you
+              to use online tools that display parts of your personal health
+              information. This health information is only displayed on VA.gov
+              &mdash; the information is stored on VA protected federal computer
+              systems and networks. VA supports the secure storage and
+              transmission of all information on VA.gov.
+            </p>
+            <div>
+              <va-accordion bordered>
+                {Array.from({
+                  length: 7,
+                }).map((_, i) => (
+                  <va-accordion-item
+                    header={`${formatOrdinals(i + 1)}`}
+                    level={4}
+                    key={i}
+                  >
+                    This is some long summary that needs to see some information
+                    for {formatOrdinals(i + 1)} one
+                  </va-accordion-item>
+                ))}
+              </va-accordion>
+            </div>
+          </div>
+          <div id="terms-of-use-alert">
+            <h2>Getting VA benefits and services if you don’t accept</h2>
+            <p>
+              Your decision decline these terms won’t affect your eligibility
+              for VA health care and benefits in any way. You can still get VA
+              health care and benefits without using online services. If you
+              need help or have questions, <SubmitSignInForm /> We’re here 24/7.
+            </p>
+            <va-alert status="warning" visible>
+              <h3 slot="headline">What will happen if you decline?</h3>
+              <p>
+                If you decline these terms, we'll automatically sign you out and
+                take you back to the VA.gov homepage. And you won't be able to
+                sign in to use tools on these VA websites:
+              </p>
+              <ul>
+                <li>VA.gov</li>
+                <li>My HealtheVet</li>
+                <li>My VA Health</li>
+              </ul>
+              <p>
+                This means you won’t be able to do these types of things using
+                VA online services:
+              </p>
+              <ul>
+                <li>Apply for some benefits</li>
+                <li>Check your claim status</li>
+                <li>Send messages to your VA health care providers</li>
+                <li>Refill your prescriptions</li>
+                <li>Update your personal information</li>
+              </ul>
+            </va-alert>
+          </div>
+          <div id="terms-of-use-actions">
+            <h2>Do you accept these Terms of Use?</h2>
+            <va-button
+              text="Accept"
+              onClick={() => {}}
+              ariaLabel="I accept to VA online serivices terms of use"
+            />
+            <va-button
+              text="Decline"
+              secondary
+              ariaLabel="I decline to VA online serivices terms of use"
+              onClick={() => {}}
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
