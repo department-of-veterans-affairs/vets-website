80441d8d5f00a92c927e9fc5a2b8416fbad53dd6
Tyler Fink <tfink419@gmail.com>
2023-08-23 09:47:15 -0600

    Fix spacing for explanation

--- src/applications/disability-benefits/686c-674/config/chapters/add-a-child/child-place-of-birth/notSelfSufficientDescription.jsx
+++ src/applications/disability-benefits/686c-674/config/chapters/add-a-child/child-place-of-birth/notSelfSufficientDescription.jsx
@@ -6,28 +6,28 @@ export const notSelfSufficientDescription = (
       <p className="vads-u-margin-bottom--4">
         We consider a child to be <strong>unable to support themself</strong> if
         both of these are true:
+        <ul className="vads-u-margin-bottom--4">
+          <li>
+            The child has a diagnosed physical or mental disability that makes
+            them unable to support themself, <strong>and</strong>
+          </li>
+          <li>The child is permanently disabled</li>
+        </ul>
       </p>
-      <ul className="vads-u-margin-bottom--4">
-        <li>
-          The child has a diagnosed physical or mental disability that makes
-          them unable to support themself, <strong>and</strong>
-        </li>
-        <li>The child is permanently disabled</li>
-      </ul>
       <p>
         To add this child as a dependent, you’ll need to provide this evidence:
+        <ul>
+          <li className="vads-u-margin-bottom--2">
+            Medical evidence that shows a permanent physical or mental
+            disability, <strong>and</strong>
+          </li>
+          <li>
+            A statement from your child’s doctor that shows the type{' '}
+            <strong>and</strong> severity of the child’s physical or mental
+            condition
+          </li>
+        </ul>
       </p>
-      <ul>
-        <li className="vads-u-margin-bottom--2">
-          Medical evidence that shows a permanent physical or mental disability,{' '}
-          <strong>and</strong>
-        </li>
-        <li>
-          A statement from your child’s doctor that shows the type{' '}
-          <strong>and</strong> severity of the child’s physical or mental
-          condition
-        </li>
-      </ul>
     </va-additional-info>
   </div>
 );
