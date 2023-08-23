4c379e0000628b3cf6e164af002f7b6da183f524
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2023-08-23 16:33:22 -0400

    updates: transforms routes.jsx => *.js + adds initial e2e testing

--- src/applications/terms-of-use/tests/00.terms-of-use.cypress.spec.js
+++ src/applications/terms-of-use/tests/00.terms-of-use.cypress.spec.js
@@ -1,8 +1,14 @@
 import manifest from '../manifest.json';
+import routes from '../routes';
 
-describe('Terms and Conditions test', () => {
-  it('Page loads and passes accessibility check', () => {
-    cy.visit(manifest.rootUrl);
+describe('Terms of use application', () => {
+  it('loads the homepage (/terms-of-use) and passes accessibility check', () => {
+    cy.visit(`${manifest.rootUrl}${routes[0].path}`);
+    cy.get('body').should('be.visible');
+    cy.injectAxeThenAxeCheck();
+  });
+  it('loads the declined page (/terms-of-use/declined) and passes accessibility check', () => {
+    cy.visit(`${manifest.rootUrl}${routes[1].path}`);
     cy.get('body').should('be.visible');
     cy.injectAxeThenAxeCheck();
   });
