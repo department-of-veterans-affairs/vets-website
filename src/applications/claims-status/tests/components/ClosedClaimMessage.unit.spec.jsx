1b2f81dfd5ed43b2e800fba3cbe2bb99124fd836
Jerek Shoemaker <jerek.shoemaker@adhocteam.us>
2023-08-25 14:05:24 -0500

    [CST] Fixing bug that displays incorrect dates for closed appeals (#25392)
    * Fixing bug that displays incorrect dates for closed appeals
    
    * Updating moment calls to use null if date variable is undefined

--- src/applications/claims-status/tests/components/ClosedClaimMessage.unit.spec.jsx
+++ src/applications/claims-status/tests/components/ClosedClaimMessage.unit.spec.jsx
@@ -7,6 +7,75 @@ import moment from 'moment';
 import ClosedClaimMessage from '../../components/ClosedClaimMessage';
 
 describe('<ClosedClaimMessage>', () => {
+  context('Appeals', () => {
+    it('should render closed appeals within 60 days', () => {
+      const closeDate = moment().add(-59, 'days');
+
+      const claims = [
+        {
+          id: 1,
+          type: 'appeal',
+          attributes: {
+            active: false,
+            events: [
+              {
+                type: 'claim_decision',
+                date: '2020-01-01',
+              },
+              {
+                type: 'bva_decision',
+                date: closeDate.format('YYYY-MM-DD'),
+              },
+            ],
+          },
+        },
+      ];
+
+      const screen = render(<ClosedClaimMessage claims={claims} />);
+
+      // Check that the component was rendered
+      expect(screen.getByText('Recently closed:')).to.exist;
+
+      // Check that the dates match up with what we would expect
+      expect(
+        screen.getByText('Your Compensation Appeal Received January 1, 2020'),
+      ).to.exist;
+
+      const closeDateText = closeDate.format('MMMM D, YYYY');
+      expect(screen.getByText(`has been closed as of ${closeDateText}`)).to
+        .exist;
+    });
+
+    it('should not render closed claims at 60 days', () => {
+      const closeDate = moment().add(-60, 'days');
+
+      const claims = [
+        {
+          id: 2,
+          type: 'appeal',
+          attributes: {
+            active: false,
+            events: [
+              {
+                type: 'claim_decision',
+                date: '2021-05-10',
+              },
+              {
+                type: 'bva_decision',
+                date: closeDate.format('YYYY-MM-DD'),
+              },
+            ],
+          },
+        },
+      ];
+
+      const screen = render(<ClosedClaimMessage claims={claims} />);
+
+      // Check that the component was rendered
+      expect(screen.queryByText('Recently closed:')).to.not.exist;
+    });
+  });
+
   context('EVSS claims', () => {
     it('should render closed claims within 30 days', () => {
       const claims = [
@@ -95,7 +164,7 @@ describe('<ClosedClaimMessage>', () => {
       const claims = [
         {
           id: 1,
-          type: 'claims',
+          type: 'claim',
           attributes: {
             claimDate: '2023-01-01',
             closeDate: moment()
@@ -113,7 +182,7 @@ describe('<ClosedClaimMessage>', () => {
       const claims = [
         {
           id: 1,
-          type: 'claims',
+          type: 'claim',
           attributes: {
             claimDate: '2023-01-01',
             closeDate: null,
