e4aec36143c2a9e10d77e9ddb2a5d061c9b23b17
Aaron Read <Aaron.Read@va.gov>
2023-08-25 15:38:31 -0500

    Mhv 47985 manage your medications accordion landing page (#25384)
    * Validate Manage Your Medications Accordion
    
    Added test scripts to verify accordion functionality on medications landing page
    
    * Test Fixes for Accordions on Landing Page
    
    Added scripts to validate all accordion components on medications landing page
    
    ---------
    
    Co-authored-by: raji.venkatesh <114511630+RajiVenkatesh18@users.noreply.github.com>

---
+++ src/applications/mhv/medications/tests/e2e/medications-validate-accordions-on-landing-page.cypress.spec.js
@@ -0,0 +1,30 @@
+import MedicationsSite from './med_site/MedicationsSite';
+import MedicationsLandingPage from './pages/MedicationsLandingPage';
+
+describe('Medications Accordions on Medications Landing Page', () => {
+  it('visits accordions on landing page', () => {
+    const site = new MedicationsSite();
+    const landingPage = new MedicationsLandingPage();
+    cy.visit('my-health/medications/');
+    site.login();
+    landingPage.clickExpandAccordionsOnMedicationsLandingPage();
+    landingPage.verifyListMedicationsAndSuppliesAccordionDropDown();
+    landingPage.verifyWhatTypeOfPrescriptionsAccordionDropDown();
+    landingPage.verifyPrescriptionRefillRequestInformationAccordionDropDown();
+    landingPage.verifyMoreQuestionsAccordionDropDown();
+    landingPage.verifyHowtoRenewPrescriptionsAccordionDropDown();
+    landingPage.verifyHowToConfirmOrUpdateMailingAddressAccordionDropDown();
+    landingPage.verifyHowToReviewAllergiesAndReactionsAccordionDropDown();
+    cy.injectAxe();
+    cy.axeCheck('main', {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+        'link-name': {
+          enabled: false,
+        },
+      },
+    });
+  });
+});
