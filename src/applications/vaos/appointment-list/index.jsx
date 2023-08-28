2bae949e36f04dd7d9fa2596cb5cc8aab8a3b2ed
Ryan Shaw <ryan.shaw@adhocteam.us>
2023-08-28 11:16:55 -0700

    Adds new routes
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/appointment-list/index.jsx
+++ src/applications/vaos/appointment-list/index.jsx
@@ -1,29 +1,52 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import AppointmentsPageV2 from './components/AppointmentsPageV2/index';
 import RequestedAppointmentDetailsPage from './components/RequestedAppointmentDetailsPage';
 import ConfirmedAppointmentDetailsPage from './components/ConfirmedAppointmentDetailsPage';
-// import CommunityCareAppointmentDetailsPage from './components/CommunityCareAppointmentDetailsPage';
 import useManualScrollRestoration from '../hooks/useManualScrollRestoration';
+import { selectFeatureBreadcrumbUrlUpdate } from '../redux/selectors';
 
 function AppointmentListSection() {
   useManualScrollRestoration();
+
+  const featureBreadcrumbUrlUpdate = useSelector(state =>
+    selectFeatureBreadcrumbUrlUpdate(state),
+  );
+
   return (
-    <Switch>
-      <Route
-        path="/:pastOrPending?/cc/:id"
-        component={ConfirmedAppointmentDetailsPage}
-      />
-      <Route
-        path="/:pastOrPending?/va/:id"
-        component={ConfirmedAppointmentDetailsPage}
-      />
-      <Route
-        path="/:pastOrPending?/requests/:id"
-        component={RequestedAppointmentDetailsPage}
-      />
-      <Route path="/" component={AppointmentsPageV2} />
-    </Switch>
+    <>
+      {!featureBreadcrumbUrlUpdate && (
+        <Switch>
+          <Route
+            path="/:pastOrPending?/cc/:id"
+            component={ConfirmedAppointmentDetailsPage}
+          />
+          <Route
+            path="/:pastOrPending?/va/:id"
+            component={ConfirmedAppointmentDetailsPage}
+          />
+          <Route
+            path="/:pastOrPending?/requests/:id"
+            component={RequestedAppointmentDetailsPage}
+          />
+          <Route path="/" component={AppointmentsPageV2} />
+        </Switch>
+      )}
+      {featureBreadcrumbUrlUpdate && (
+        <Switch>
+          <Route
+            path="/:pastOrPending?/:id"
+            component={ConfirmedAppointmentDetailsPage}
+          />
+          <Route
+            path="/:pastOrPending?/requests/:id"
+            component={RequestedAppointmentDetailsPage}
+          />
+          <Route path="/" component={AppointmentsPageV2} />
+        </Switch>
+      )}
+    </>
   );
 }
 
