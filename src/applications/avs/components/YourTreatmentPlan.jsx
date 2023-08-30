2fa3cc608a817d31c3d962e340d89e6b7147663b
Adrian Rollett <adrian.rollett@va.gov>
2023-08-30 11:20:10 -0600

    Add unit tests for Your Treatment Plan

--- src/applications/avs/components/YourTreatmentPlan.jsx
+++ src/applications/avs/components/YourTreatmentPlan.jsx
@@ -4,123 +4,118 @@ import PropTypes from 'prop-types';
 import { ORDER_TYPES } from '../utils/constants';
 
 const getOrderItems = (type, orders) => {
-  return orders.filter(order => order.type === type.label);
+  return orders?.filter(order => order.type === type.label);
 };
 
 const getOrderListItemsByType = (type, orders) => {
   const items = getOrderItems(type, orders);
-  return items.map((item, idx) => <li key={idx}>{item.text}</li>);
+  return items?.map((item, idx) => <li key={idx}>{item.text}</li>) || [];
 };
 
 const consultations = avs => {
-  if (avs.orders?.length > 0) {
-    const orderListItems = getOrderListItemsByType(
-      ORDER_TYPES.CONSULT,
-      avs.orders,
+  const orderListItems = getOrderListItemsByType(
+    ORDER_TYPES.CONSULT,
+    avs.orders,
+  );
+  if (orderListItems.length) {
+    return (
+      <div>
+        <h4>Consultations</h4>
+        <p>
+          You will be contacted by mail or telephone for the following referral:
+        </p>
+        <ul className="bulleted-list" data-testid="consultations">
+          {orderListItems}
+        </ul>
+      </div>
     );
-    if (orderListItems.length) {
-      return (
-        <div>
-          <h4>Consultations</h4>
-          <p>
-            You will be contacted by mail or telephone for the following
-            referral:
-          </p>
-          <ul className="bulleted-list">{orderListItems}</ul>
-        </div>
-      );
-    }
   }
 
   return null;
 };
 
 const imaging = avs => {
-  if (avs.orders?.length > 0) {
-    const orderListItems = getOrderListItemsByType(
-      ORDER_TYPES.IMAGING,
-      avs.orders,
+  const orderListItems = getOrderListItemsByType(
+    ORDER_TYPES.IMAGING,
+    avs.orders,
+  );
+  if (orderListItems.length) {
+    return (
+      <div>
+        <h4>Imaging</h4>
+        <p>
+          For CT and MRI scans, you will be contacted by Imaging Service for
+          your appointment. For X-Rays and Ultrasounds, please report to Imaging
+          Service during normmal working hours to complete your exam. If a
+          specific preparation is required, the Technologist will inform you.
+        </p>
+        <ul className="bulleted-list" data-testid="imaging">
+          {orderListItems}
+        </ul>
+      </div>
     );
-    if (orderListItems.length) {
-      return (
-        <div>
-          <h4>Imaging</h4>
-          <p>
-            For CT and MRI scans, you will be contacted by Imaging Service for
-            your appointment. For X-Rays and Ultrasounds, please report to
-            Imaging Service during normmal working hours to complete your exam.
-            If a specific preparation is required, the Technologist will inform
-            you.
-          </p>
-          <ul className="bulleted-list">{orderListItems}</ul>
-        </div>
-      );
-    }
   }
 
   return null;
 };
 
 const labTests = avs => {
-  if (avs.orders?.length > 0) {
-    const orderListItems = getOrderListItemsByType(ORDER_TYPES.LAB, avs.orders);
-    if (orderListItems.length) {
-      return (
-        <div>
-          <h4>
-            Lab tests: Patients no longer need to fast for most blood draws.
-            Check with your provider whether fasting is required before
-            completing your Laboratory draws.
-          </h4>
-          <p>
-            Please report to the lab for the following blood tests on the date
-            listed for each test:
-          </p>
-          <ul className="bulleted-list">{orderListItems}</ul>
-        </div>
-      );
-    }
+  const orderListItems = getOrderListItemsByType(ORDER_TYPES.LAB, avs.orders);
+  if (orderListItems.length) {
+    return (
+      <div>
+        <h4>
+          Lab tests: Patients no longer need to fast for most blood draws. Check
+          with your provider whether fasting is required before completing your
+          Laboratory draws.
+        </h4>
+        <p>
+          Please report to the lab for the following blood tests on the date
+          listed for each test:
+        </p>
+        <ul className="bulleted-list" data-testid="lab-tests">
+          {orderListItems}
+        </ul>
+      </div>
+    );
   }
 
   return null;
 };
 
 const medsAndSupplies = avs => {
-  if (avs.orders?.length > 0) {
-    const orderListItems = getOrderListItemsByType(ORDER_TYPES.MED, avs.orders);
-    if (orderListItems.length) {
-      return (
-        <div>
-          <h4>Medications & supplies</h4>
-          <p>
-            Note: this section <strong>only</strong> lists{' '}
-            <strong>changes</strong> to your medication regimen. Please see your
-            complete medication list under My Ongoing Care below.
-          </p>
-          {/* TODO: headings for med change types */}
-          <ul className="bulleted-list">{orderListItems}</ul>
-        </div>
-      );
-    }
+  const orderListItems = getOrderListItemsByType(ORDER_TYPES.MED, avs.orders);
+  if (orderListItems.length) {
+    return (
+      <div>
+        <h4>Medications & supplies</h4>
+        <p>
+          Note: this section <strong>only</strong> lists{' '}
+          <strong>changes</strong> to your medication regimen. Please see your
+          complete medication list under My Ongoing Care below.
+        </p>
+        {/* TODO: headings for med change types */}
+        <ul className="bulleted-list" data-testid="medications-and-supplies">
+          {orderListItems}
+        </ul>
+      </div>
+    );
   }
 
   return null;
 };
 
 const otherOrders = avs => {
-  if (avs.orders?.length > 0) {
-    const orderListItems = getOrderListItemsByType(
-      ORDER_TYPES.OTHER,
-      avs.orders,
+  const orderListItems = getOrderListItemsByType(ORDER_TYPES.OTHER, avs.orders);
+  if (orderListItems.length) {
+    return (
+      <div>
+        <h4>Other orders</h4>
+        <ul className="bulleted-list" data-testid="other-orders">
+          {orderListItems}
+        </ul>
+      </div>
     );
-    if (orderListItems.length) {
-      return (
-        <div>
-          <h4>Other orders</h4>
-          <ul className="bulleted-list">{orderListItems}</ul>
-        </div>
-      );
-    }
   }
 
   return null;
@@ -132,8 +127,12 @@ const patientInstructions = avs => {
       <div>
         <h4>Other instructions</h4>
         {/* TODO: better way to do this */}
-        {/* eslint-disable-next-line react/no-danger */}
-        <p dangerouslySetInnerHTML={{ __html: avs.patientInstructions }} />
+        {/* eslint-disable react/no-danger */}
+        <p
+          data-testid="patient-instructions"
+          dangerouslySetInnerHTML={{ __html: avs.patientInstructions }}
+        />
+        {/* eslint-enable react/no-danger */}
       </div>
     );
   }
---
+++ src/applications/avs/tests/components/YourTreatmentPlan.unit.spec.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { expect } from 'chai';
+import { render } from '@testing-library/react';
+import { replacementFunctions } from '@department-of-veterans-affairs/platform-utilities';
+
+import YourTreatmentPlan from '../../components/YourTreatmentPlan';
+
+import mockAvs from '../fixtures/9A7AF40B2BC2471EA116891839113252.json';
+
+const avsData = mockAvs.data.attributes;
+
+describe('Avs: Your Treatment Plan', () => {
+  it('correctly renders all data', async () => {
+    const avs = replacementFunctions.cloneDeep(avsData);
+    const props = { avs };
+    const screen = render(<YourTreatmentPlan {...props} />);
+    expect(screen.getByTestId('consultations').firstChild).to.have.text(
+      'Test Consultation',
+    );
+    expect(screen.getByTestId('imaging').firstChild).to.have.text(
+      'Test Imaging',
+    );
+    expect(screen.getByTestId('lab-tests').children[1]).to.have.text(
+      'HEMOGLOBIN A1C (LAB) BLOOD-LAVENDER$',
+    );
+    expect(
+      screen.getByTestId('medications-and-supplies').firstChild,
+    ).to.have.text('Test Medication');
+    expect(screen.getByTestId('other-orders').children[1]).to.contain.text(
+      'PACT ALERT BRAVO\nConcern:',
+    );
+    expect(screen.getByTestId('patient-instructions')).to.contain.text(
+      'Recommend acetaminophen 500 mg',
+    );
+  });
+
+  it('sections without data are hidden', async () => {
+    const avs = replacementFunctions.cloneDeep(avsData);
+    delete avs.orders;
+    delete avs.patientInstructions;
+    const props = { avs };
+    const screen = render(<YourTreatmentPlan {...props} />);
+    expect(screen.queryByTestId('consultations')).to.not.exist;
+    expect(screen.queryByTestId('imaging')).to.not.exist;
+    expect(screen.queryByTestId('lab-tests')).to.not.exist;
+    expect(screen.queryByTestId('medications-and-supplies')).to.not.exist;
+    expect(screen.queryByTestId('other-orders')).to.not.exist;
+    expect(screen.queryByTestId('patient-instructions')).to.not.exist;
+  });
+});
