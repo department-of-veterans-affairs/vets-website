083988a4e435e675cc1fb450f321ec38f8329746
Matt Long <longmd@me.com>
2023-08-31 10:15:01 -0400

    Remove feature platform declaration

--- src/platform/utilities/feature-toggles/featureFlagNames.js
+++ src/platform/utilities/feature-toggles/featureFlagNames.js
@@ -84,7 +84,6 @@ export default Object.freeze({
   hcaBrowserMonitoringEnabled: 'hca_browser_monitoring_enabled',
   hcaEnrollmentStatusOverrideEnabled: 'hca_enrollment_status_override_enabled',
   hcaHouseholdV2Enabled: 'hca_household_v2_enabled',
-  hcaRemoveIdFieldsEnabled: 'hca_remove_id_fields_enabled',
   hcaSigiEnabled: 'hca_sigi_enabled',
   hcaUseFacilitiesApi: 'hca_use_facilities_API',
   loopPages: 'loop_pages',
