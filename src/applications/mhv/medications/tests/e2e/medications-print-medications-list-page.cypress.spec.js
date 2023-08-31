e5ab5722ac546f3742d589705984dde3c0b54795
Aaron Read <Aaron.Read@va.gov>
2023-08-31 12:46:20 -0500

    Test Fixes for URL updates (#25470)
    Added fixes to reflect changes in the URL for the pages
    
    Co-authored-by: raji.venkatesh <114511630+RajiVenkatesh18@users.noreply.github.com>

--- src/applications/mhv/medications/tests/e2e/medications-print-medications-list-page.cypress.spec.js
+++ src/applications/mhv/medications/tests/e2e/medications-print-medications-list-page.cypress.spec.js
@@ -2,12 +2,23 @@ import MedicationsSite from './med_site/MedicationsSite';
 import MedicationsListPage from './pages/MedicationsListPage';
 
 describe('Medications List Page Print List', () => {
-  it.skip('visits Print Medications List', () => {
+  it('visits Print Medications List', () => {
     const site = new MedicationsSite();
     const listPage = new MedicationsListPage();
     cy.visit('my-health/about-medications/');
     site.login();
     listPage.clickGotoMedicationsLink();
+    cy.injectAxe();
+    cy.axeCheck('main', {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+        'link-name': {
+          enabled: false,
+        },
+      },
+    });
     listPage.clickPrintOrDownloadThisListDropDown();
     listPage.verifyPrintMedicationsListEnabledOnListPage();
   });
