6d11bf964185d7d633a3907e342733d6c680e3e9
robertbylight <roberto.rodriguez@bylight.com>
2023-08-31 16:04:48 -0400

    [MHV-49115] cleaned up jsx.

--- src/applications/mhv/medications/containers/RxBreadcrumbs.jsx
+++ src/applications/mhv/medications/containers/RxBreadcrumbs.jsx
@@ -4,6 +4,12 @@ import { useSelector } from 'react-redux';
 import Breadcrumbs from '@department-of-veterans-affairs/component-library/Breadcrumbs';
 // import { replaceWithStagingDomain } from '~/platform/utilities/environment/stagingDomains';
 
+const alignToLeft = `xsmall-screen:vads-u-margin-left--neg1 
+small-screen:vads-u-margin-left--neg1 
+medium-screen:vads-u-margin-left--neg2
+small-desktop-screen:vads-u-margin-left--neg2
+large-screen:vads-u-margin-left--0`;
+
 const RxBreadcrumbs = () => {
   const crumbs = useSelector(state => state.rx.breadcrumbs.list);
   const currentPath = useSelector(state => state.rx.breadcrumbs.location);
@@ -15,14 +21,7 @@ const RxBreadcrumbs = () => {
           <div>
             {/* per exisiting issue found here https://github.com/department-of-veterans-affairs/vets-design-system-documentation/issues/1296 */}
             {/* eslint-disable-next-line @department-of-veterans-affairs/prefer-web-component-library */}
-            <Breadcrumbs
-              label="Breadcrumb"
-              className={`xsmall-screen:vads-u-margin-left--neg1 
-                          small-screen:vads-u-margin-left--neg1 
-                          medium-screen:vads-u-margin-left--neg2
-                          small-desktop-screen:vads-u-margin-left--neg2
-                          large-screen:vads-u-margin-left--0`}
-            >
+            <Breadcrumbs label="Breadcrumb" className={alignToLeft}>
               {crumbs.map((crumb, idx) => (
                 <a href={crumb.url} key={idx}>
                   {crumb.label}
