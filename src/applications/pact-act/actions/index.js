3a3395e2b3b946e8e1d7869cf4d78b4e86e5a971
Randi Mays <rmays@kindsys.us>
2023-08-31 14:56:35 -0500

    VACMS-14931 PACT Act initial questions setup

--- src/applications/pact-act/actions/index.js
+++ src/applications/pact-act/actions/index.js
@@ -1,8 +1,34 @@
-import { PAW_VIEWED_RESULTS_PAGE_1 } from '../constants';
+import {
+  PAW_UPDATE_BURN_PIT_2_1,
+  PAW_UPDATE_SERVICE_PERIOD,
+  PAW_VIEWED_INTRO_PAGE,
+  PAW_VIEWED_RESULTS_PAGE_1,
+} from '../constants';
+
+export const updateIntroPageViewed = value => {
+  return {
+    type: PAW_VIEWED_INTRO_PAGE,
+    payload: value,
+  };
+};
 
 export const updateResultsPage1Viewed = value => {
   return {
     type: PAW_VIEWED_RESULTS_PAGE_1,
+    payload: value,
+  };
+};
+
+export const updateServicePeriod = value => {
+  return {
+    type: PAW_UPDATE_SERVICE_PERIOD,
+    payload: value,
+  };
+};
+
+export const updateBurnPit21 = value => {
+  return {
+    type: PAW_UPDATE_BURN_PIT_2_1,
     payload: value,
   };
 };
