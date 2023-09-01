e3cb7389e6a4285259a1aa3a8d761f577ffe07d1
Richard Davis <richard.davis@agile6.com>
2023-09-01 11:56:20 -0400

    initial form

--- src/applications/associated-persons/containers/App.jsx
+++ src/applications/associated-persons/containers/App.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import { suffixes } from '@department-of-veterans-affairs/platform-static-data';
+import { countries, states } from 'vets-json-schema/dist/constants.json';
 
-export default function App({ children }) {
-  return <div>{children}</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <va-text-input label="First name" name="givenName" />
+      <va-text-input label="Middle name" name="middleName" />
+      <va-text-input label="Last name" name="familyName" />
+      <va-select label="Suffix" name="suffix">
+        <option value=""></option>
+        {suffixes.map(suffix => <option value={suffix}>{suffix}</option>)}
+      </va-select>
+      {/*
+      <va-select label="Country" name="country">
+        {countries.map(({ label, value }) => <option value={value}>{label}</option>)}
+      </va-select>
+       */}
+      <va-text-input label="Street address" name="addressLine1" />
+      <va-text-input label="Street address (line 2)" name="addressLine2" />
+      <va-text-input label="Street address (line 3)" name="addressLine3" />
+      <va-text-input label="City" name="city" />
+      <va-select label="State" name="state">
+        {states.USA.map(({ label, value }) => <option value={value}>{label}</option>)}
+      </va-select>
+      <va-text-input label="Zip code" name="zipCode" />
+      <va-text-input label="Phone number" name="primaryPhone" />
+    </div>
+  )
+};
+
+export default App;
