c8b1e221afcd3dc952a6b7000b18edc506e8a2d8
data-doge <eugenestapler@gmail.com>
2023-08-31 23:25:40 -0400

    Add comment.

--- src/applications/appeals/10182/utils/issues.js
+++ src/applications/appeals/10182/utils/issues.js
@@ -1,11 +1,21 @@
 import { getEligibleContestableIssues } from './submit';
 
+// This function checks whether contestable issue data loaded from the API (`loaded`)
+// is different from the contestable issue data already present in `formData`
+// (`formIssues`). We use it to determine whether we need to update `formData`.
+// with contestable issue data from the API. This function is adapted from the
+// `issuesNeedUpdating` function defined in `src/applications/appeals/shared/utils/issues.js`.
+// The only difference is that we are calling `getEligibleContestableIssues`
+// instead of `processContestableIssues`. This ensures that we are comparing
+// filtered data from the API with filtered data from `formData`.
 export const issuesNeedUpdating = (
   loaded = [],
   formIssues = [],
   options = {},
 ) => {
   const { showPart3 } = options;
+  // `getEligibleContestableIssues` filters out deferred issues, and issues
+  // with a decision date older than a year.
   const loadedIssues = getEligibleContestableIssues(loaded, { showPart3 });
   const existingIssues = getEligibleContestableIssues(formIssues, {
     showPart3,
