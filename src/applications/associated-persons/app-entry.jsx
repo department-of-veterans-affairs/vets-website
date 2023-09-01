02ace7fb622cab85d8b7297399849994bc4de633
Richard Davis <richard.davis@agile6.com>
2023-09-01 10:38:13 -0400

    Initial NOK/EC app

---
+++ src/applications/associated-persons/app-entry.jsx
@@ -0,0 +1,14 @@
+import '@department-of-veterans-affairs/platform-polyfills';
+import './sass/associated-persons.scss';
+import { startAppFromIndex as startApp } from '@department-of-veterans-affairs/platform-startup/exports';
+
+import routes from './routes';
+import reducer from './reducers';
+import manifest from './manifest.json';
+
+startApp({
+  entryName: manifest.entryName,
+  reducer,
+  routes,
+  url: manifest.rootUrl,
+});
---
+++ src/applications/associated-persons/routes.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import App from './containers/App';
+
+const routes = <Route exact path="/" component={App} />;
+
+export default routes;
