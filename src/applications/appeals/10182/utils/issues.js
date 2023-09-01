ce90d022b0fbf80fb117a320dbfb012c94d54d5f
data-doge <eugenestapler@gmail.com>
2023-08-31 23:55:33 -0400

    Rename variables in NOD-specific `issuesNeedUpdating` for clarity.

--- src/applications/appeals/10182/utils/issues.js
+++ src/applications/appeals/10182/utils/issues.js
@@ -5,25 +5,28 @@ import { getEligibleContestableIssues } from './submit';
 // (`formIssues`). We use it to determine whether we need to update `formData`.
 // with contestable issue data from the API. This function is adapted from the
 // `issuesNeedUpdating` function defined in `src/applications/appeals/shared/utils/issues.js`.
-// The only difference is that we are calling `getEligibleContestableIssues`
-// instead of `processContestableIssues`. This ensures that we are comparing
-// filtered data from the API with filtered data from `formData`.
+// The only difference (other than variable naming) is that we are calling
+// `getEligibleContestableIssues` instead of `processContestableIssues`.
+// This ensures that we are comparing filtered data from the API with filtered
+// data from `formData`.
 export const issuesNeedUpdating = (
-  loaded = [],
-  formIssues = [],
+  loadedIssues = [],
+  existingIssues = [],
   options = {},
 ) => {
   const { showPart3 } = options;
   // `getEligibleContestableIssues` filters out deferred issues, and issues
   // with a decision date older than a year.
-  const loadedIssues = getEligibleContestableIssues(loaded, { showPart3 });
-  const existingIssues = getEligibleContestableIssues(formIssues, {
+  const filteredLoadedIssues = getEligibleContestableIssues(loadedIssues, {
     showPart3,
   });
-  return loadedIssues.length !== existingIssues.length
+  const filteredExistingIssues = getEligibleContestableIssues(existingIssues, {
+    showPart3,
+  });
+  return filteredLoadedIssues.length !== filteredExistingIssues.length
     ? true
-    : !loadedIssues.every(({ attributes }, index) => {
-        const existing = existingIssues[index]?.attributes || {};
+    : !filteredLoadedIssues.every(({ attributes }, index) => {
+        const existing = filteredExistingIssues[index]?.attributes || {};
         return (
           attributes.ratingIssueSubjectText ===
             existing.ratingIssueSubjectText &&
