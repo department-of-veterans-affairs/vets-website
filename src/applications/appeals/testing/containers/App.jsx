11b5469c5ad7d61fc60dfac3b9aaf4626b7e8120
Robin Garrison <robin.garrison@adhocteam.us>
2023-09-05 08:42:53 -0500

    Fix testing wrapper

--- src/applications/appeals/testing/containers/App.jsx
+++ src/applications/appeals/testing/containers/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link } from 'react-router';
 import PropTypes from 'prop-types';
 
-import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-
 import formConfig from '../config/form';
 
 import { setupPages } from '../utils/taskListPages';
@@ -29,79 +27,67 @@ export default function App(props) {
       id="appeals-testing"
       data-location={`${location?.pathname?.slice(1)}`}
     >
-      <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+      {!noHeader.includes(currentPath) && (
         <>
-          {!noHeader.includes(currentPath) && (
-            <>
-              <div className="vads-u-background-color--primary-alt-lightest vads-u-padding--2">
-                <div className="row">
-                  <div className="usa-width-two-thirds medium-8 columns">
-                    <strong>{formConfig.title} (VA Form 10182)</strong>
-                  </div>
+          <div className="vads-u-background-color--primary-alt-lightest vads-u-padding--2">
+            <div className="row">
+              <div className="usa-width-two-thirds medium-8 columns">
+                <strong>{formConfig.title} (VA Form 10182)</strong>
+              </div>
+            </div>
+          </div>
+          <div className="vads-u-background-color--primary-alt-lightest vads-u-padding--2">
+            <div className="row">
+              <div className="usa-width-two-thirds medium-8 columns">
+                <div className="vads-u-display--flex">
+                  <div>&#x2190;&#x00a0;</div>
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                  <a
+                    href="#"
+                    className="vads-u-padding-right--1p5"
+                    onClick={goBack}
+                  >
+                    Back
+                  </a>
+                  <span role="presentation" className="vads-u-padding-x--1p5">
+                    |
+                  </span>
+                  <Link
+                    to={{ pathname: '/task-list', search: '?redirect' }}
+                    className="vads-u-padding-x--1p5"
+                  >
+                    Task List
+                  </Link>
+                  <span role="presentation" className="vads-u-padding-x--1p5">
+                    |
+                  </span>
+                  <a
+                    href="/decision-reviews/appeals-testing/"
+                    className="vads-u-padding-x--1p5"
+                  >
+                    Exit form
+                  </a>
                 </div>
               </div>
-              <div className="vads-u-background-color--primary-alt-lightest vads-u-padding--2">
-                <div className="row">
-                  <div className="usa-width-two-thirds medium-8 columns">
-                    <div className="vads-u-display--flex">
-                      <div>&#x2190;&#x00a0;</div>
-                      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                      <a
-                        href="#"
-                        className="vads-u-padding-right--1p5"
-                        onClick={goBack}
-                      >
-                        Back
-                      </a>
-                      <span
-                        role="presentation"
-                        className="vads-u-padding-x--1p5"
-                      >
-                        |
-                      </span>
-                      <Link
-                        to={{ pathname: '/task-list', search: '?redirect' }}
-                        className="vads-u-padding-x--1p5"
-                      >
-                        Task List
-                      </Link>
-                      <span
-                        role="presentation"
-                        className="vads-u-padding-x--1p5"
-                      >
-                        |
-                      </span>
-                      <a
-                        href="/decision-reviews/appeals-testing/"
-                        className="vads-u-padding-x--1p5"
-                      >
-                        Exit form
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="usa-width-two-thirds medium-8 columns vads-u-margin-top--2">
-                  <va-segmented-progress-bar
-                    current={chapterIndex + 1}
-                    heading-text={chapterTitles[chapterIndex]}
-                    label="Label is here"
-                    labels={chapterTitles.join(';')}
-                    total={chapterTitles.length}
-                    uswds
-                  />
-                </div>
-              </div>
-            </>
-          )}
+            </div>
+          </div>
           <div className="row">
-            <div className="usa-width-two-thirds medium-8 columns">
-              {children}
+            <div className="usa-width-two-thirds medium-8 columns vads-u-margin-top--2">
+              <va-segmented-progress-bar
+                current={chapterIndex + 1}
+                heading-text={chapterTitles[chapterIndex]}
+                label="Label is here"
+                labels={chapterTitles.join(';')}
+                total={chapterTitles.length}
+                uswds
+              />
             </div>
           </div>
         </>
-      </RoutedSavableApp>
+      )}
+      <div className="row">
+        <div className="usa-width-two-thirds medium-8 columns">{children}</div>
+      </div>
     </article>
   );
 }
