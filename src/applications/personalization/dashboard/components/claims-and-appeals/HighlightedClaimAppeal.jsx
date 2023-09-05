d42f675562abb35f8a46b1d89f3f466bd0c92512
Derrick Ellerbie <dellerbie@users.noreply.github.com>
2023-09-05 16:48:27 -0400

    My VA: remove dashboard v1 components (#25418)
    * My VA: remove dashboard v1 components
    
    * My VA: rename dashboard v2 components
    
    * My VA: rename dashboard v2 testids and classnames

--- src/applications/personalization/dashboard/components/claims-and-appeals/HighlightedClaimAppeal.jsx
+++ src/applications/personalization/dashboard/components/claims-and-appeals/HighlightedClaimAppeal.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-
-import { appealTypes } from '../../claims-and-appeals-helpers';
+import PropTypes from 'prop-types';
+import { appealTypes } from '../../utils/appeals-v2-helpers';
 
 import Claim from './Claim';
 import Appeal from './Appeal';
 
-const HighlightedClaimAppeal = ({ claimOrAppeal, name }) => {
+const HighlightedClaimAppeal = ({
+  claimOrAppeal,
+  name,
+  useLighthouseClaims = false,
+}) => {
   if (!claimOrAppeal) {
     return <p>You have no claims or appeals updates in the last 30 days.</p>;
   }
   if (appealTypes.includes(claimOrAppeal.type)) {
     return <Appeal appeal={claimOrAppeal} name={name} />;
   }
-  return <Claim claim={claimOrAppeal} />;
+  return (
+    <Claim claim={claimOrAppeal} useLighthouseClaims={useLighthouseClaims} />
+  );
 };
+
+HighlightedClaimAppeal.propTypes = {
+  claimOrAppeal: PropTypes.object.isRequired,
+  name: PropTypes.string,
+  useLighthouseClaims: PropTypes.bool,
+};
+
 export default HighlightedClaimAppeal;
