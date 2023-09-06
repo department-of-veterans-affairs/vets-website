8a265727767b5ee01ffe31446f557a59899e9e8e
tynyttie <ty_barnes_jobs@yahoo.com>
2023-09-06 12:00:25 -0400

    Remove expect(recordDate.length).to.eq(2) and place with expect(recordDate).to.exist

--- src/applications/mhv/medical-records/tests/components/CareSummariesAndNotesListItem.unit.spec.jsx
+++ src/applications/mhv/medical-records/tests/components/CareSummariesAndNotesListItem.unit.spec.jsx
@@ -48,7 +48,7 @@ describe('CareSummariesAndNotesListItem', () => {
   it('should contain the date of the record', () => {
     const screen = setup();
     const recordDate = screen.getAllByText('August', { exact: false });
-    expect(recordDate).to.eq(2);
+    expect(recordDate).to.exist;
   });
 
   it('should contain a link to view record details', () => {
