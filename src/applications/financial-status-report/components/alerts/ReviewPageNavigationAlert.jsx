1396383c4f0eff1e9cf2831e5505be72a90d3c99
Andrew Rodiek <andrew.rodiek@gmail.com>
2023-09-06 11:41:57 -0400

    Adding alert for navigating back into the form

---
+++ src/applications/financial-status-report/components/alerts/ReviewPageNavigationAlert.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+import { setData } from 'platform/forms-system/src/js/actions';
+
+const ReviewPageNavigationAlert = ({ data, title }) => {
+  const dispatch = useDispatch();
+  const onReviewClick = () => {
+    dispatch(
+      setData({
+        ...data,
+        reviewNavigation: false,
+      }),
+    );
+  };
+
+  return (
+    <va-alert
+      class="vads-u-margin-y--1"
+      status="info"
+      data-testid="review-page-navigation-alert"
+    >
+      <h4 slot="headline">Editing {title}</h4>
+      <p className="vads-u-font-size--base vads-u-font-family--sans">
+        You are currently editing the {title} section. Complete the entire
+        sectionto return to the review page.
+      </p>
+      <Link
+        id="review-and-submit"
+        to="/review-and-submit"
+        aria-label="return to the review page"
+        onClick={onReviewClick}
+      >
+        Back to review page
+      </Link>
+    </va-alert>
+  );
+};
+
+ReviewPageNavigationAlert.propTypes = {
+  data: PropTypes.object,
+  setFlag: PropTypes.func,
+  title: PropTypes.string,
+};
+
+export default ReviewPageNavigationAlert;
