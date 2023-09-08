500831563365bc3129d482c180d039486c54b131
Richard Davis <richard.davis@agile6.com>
2023-09-08 11:56:25 -0400

    borders

--- src/applications/associated-persons/components/PersonalDetails.jsx
+++ src/applications/associated-persons/components/PersonalDetails.jsx
@@ -7,7 +7,12 @@ const PersonalDetails = props => {
 
   return (
     <>
-
+      <h2>Next of kin information</h2>
+      <p>
+        Next of kin is who you'd like to represent you, your wishes for care,
+        medical documentation, and benefits, if needed. You next of kin is often
+        your closest living relative, like your spouse, child, parent, or sibling.
+      </p>
       {/* <Edit {...props} /> */}
       <Show {...props} />
     </>
--- src/applications/associated-persons/components/Show.jsx
+++ src/applications/associated-persons/components/Show.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Edit from './Edit';
 
 const headings = {
-  'nextOfKinPrimary': 'Is this your current next of kin information?',
+  'nextOfKinPrimary': 'Next of kin',
   'nextOfKinOther': 'Next-of-Kin, Other',
   'emergencyContactPrimary': 'Emergency Contact, Primary',
   'emergencyContactOther': 'Emergency Contact, Other'
@@ -12,34 +12,70 @@ const Show = props => {
   let { name, relationship, address, phoneNumber, variant } = props;
 
   return (
-    <>
-      <h2>{headings[variant]}</h2>
-      <hr />
+    <section className="vads-u-margin-top--2 vads-u-margin-bottom--6">
+      <h2
+        className="heading vads-u-background-color--gray-lightest vads-u-border-color--gray-lighter vads-u-color--gray-darkest vads-u-border-top--1px vads-u-border-left--1px vads-u-border-right--1px vads-u-margin--0 vads-u-padding-x--2 vads-u-padding-y--1p5 vads-u-font-size--h3 medium-screen:vads-u-padding-x--4 medium-screen:vads-u-padding-y--2"
+      >
+        {headings[variant]}
+      </h2>
 
-      <p>
-        <b>Name</b><br />
+      <div
+        className="row vads-u-border-color--gray-lighter vads-u-color-gray-dark vads-u-display--flex vads-u-flex-direction--column vads-u-padding-x--2 vads-u-padding-y--1p5 medium-screen:vads-u-padding--4 vads-u-border--1px"
+      >
+        <h3
+          className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--bold vads-u-margin--0 vads-u-width--auto"
+          >
+            Name
+        </h3>
         { name }
-      </p>
-      <hr />
+      </div>
 
-      <p>
-        <b>Relationship</b><br />
+      <div
+        className="row vads-u-border-color--gray-lighter vads-u-color-gray-dark vads-u-display--flex vads-u-flex-direction--column vads-u-padding-x--2 vads-u-padding-y--1p5 medium-screen:vads-u-padding--4 vads-u-border-top--0px vads-u-border-left--1px vads-u-border-right--1px vads-u-border-bottom--1px"
+      >
+        <h3
+          className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--bold vads-u-margin--0 vads-u-width--auto"
+          >
+            Relationship
+        </h3>
         { relationship }
-      </p>
-      <hr />
+      </div>
 
-      <p>
-        <b>Address</b><br />
+      <div
+        className="row vads-u-border-color--gray-lighter vads-u-color-gray-dark vads-u-display--flex vads-u-flex-direction--column vads-u-padding-x--2 vads-u-padding-y--1p5 medium-screen:vads-u-padding--4 vads-u-border-top--0px vads-u-border-left--1px vads-u-border-right--1px vads-u-border-bottom--1px"
+      >
+        <h3
+          className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--bold vads-u-margin--0 vads-u-width--auto"
+          >
+            Address
+        </h3>
         { address }
-      </p>
-      <hr />
+      </div>
 
-      <p>
-        <b>Phone Number</b><br />
+      <div
+        className="row vads-u-border-color--gray-lighter vads-u-color-gray-dark vads-u-display--flex vads-u-flex-direction--column vads-u-padding-x--2 vads-u-padding-y--1p5 medium-screen:vads-u-padding--4 vads-u-border-top--0px vads-u-border-left--1px vads-u-border-right--1px vads-u-border-bottom--1px"
+      >
+        <h3
+          className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--bold vads-u-margin--0 vads-u-width--auto"
+          >
+            Phone
+        </h3>
         { phoneNumber }
-      </p>
-      <hr />
-    </>
+      </div>
+
+      <div
+        className="row vads-u-border-color--gray-lighter vads-u-color-gray-dark vads-u-display--flex vads-u-flex-direction--column vads-u-padding-x--2 vads-u-padding-y--1p5 medium-screen:vads-u-padding--4 vads-u-border-top--0px vads-u-border-left--1px vads-u-border-right--1px vads-u-border-bottom--1px"
+      >
+        <h3
+          className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--bold vads-u-margin--0 vads-u-width--auto"
+          >
+            Work phone
+        </h3>
+        { phoneNumber }
+
+        <va-button text="Edit" />
+      </div>
+    </section>
   )
 };
 
