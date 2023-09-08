aa818b2f0099679d64184c663dcdb7054ddf8d5b
Kyle Cardwell <kyle.cardwell@acommsolutions.com>
2023-09-08 15:11:58 -0600

    commit to retrigger tests

--- src/applications/mhv/secure-messaging/components/ComposeForm/ComposeForm.jsx
+++ src/applications/mhv/secure-messaging/components/ComposeForm/ComposeForm.jsx
@@ -204,7 +204,7 @@ const ComposeForm = props => {
     return recipientsList.findIndex(item => +item.id === +recipientId) > -1;
   };
 
-  // Populates form fields with recipients and categories
+  //  Populates form fields with recipients and categories
   const populateForm = () => {
     if (!recipientExists(draft.recipientId)) {
       const newRecipient = {
