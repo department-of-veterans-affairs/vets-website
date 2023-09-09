d16609bef2675ea92c7271e8e3d45f1a19f8e48e
Tze-chiu Lei <tclei2009@gmail.com>
2023-09-08 18:46:00 -0700

    Refactor 40-0247 Veteran-Gender-Identity page

--- src/applications/simple-forms/40-0247/pages/veteranPersonalInfoGenderIdentity.js
+++ src/applications/simple-forms/40-0247/pages/veteranPersonalInfoGenderIdentity.js
@@ -3,8 +3,14 @@ import React from 'react';
 /** @type {PageSchema} */
 export default {
   uiSchema: {
-    'ui:title': <h3>Select the Veteran's gender identity</h3>,
-    'ui:description': 'Information is gathered for statistical purposes only.',
+    'ui:title': (
+      <>
+        <h3>Select the Veteran’s gender identity</h3>
+        <p className="vads-u-font-family--sans vads-u-font-weight--normal vads-u-font-size--base">
+          Information is gathered for statistical purposes only.
+        </p>
+      </>
+    ),
     veteranGenderIdentity: {
       'ui:title': 'Gender identity',
       'ui:widget': 'radio',
