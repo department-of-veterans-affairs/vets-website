804585b41c19bc71dcfe9fa0c4000d7131830820
Samuel Cormier <sam.cormier@coforma.io>
2023-09-12 07:39:44 -0700

    removed redundant margin class from paragraph

--- src/applications/claims-status/components/WIP.jsx
+++ src/applications/claims-status/components/WIP.jsx
@@ -6,7 +6,7 @@ const WIP = () => (
       Decision letters aren’t available to download right now.
     </h2>
     <div>
-      <p className="vads-u-margin-y--0 vads-u-margin-bottom--0">
+      <p className="vads-u-margin-y--0">
         We’re fixing some problems with this tool. Check back later. If you need
         information about your decision letters now, call us at{' '}
         <va-telephone contact="8008271000" /> (TTY: 711). We’re here Monday
