d2edd331a044585fb88429b9a0dee61ad82b1abb
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-09-12 10:28:40 -0500

    Update contact value

--- src/applications/personalization/profile/components/direct-deposit/alerts/NotEligible.jsx
+++ src/applications/personalization/profile/components/direct-deposit/alerts/NotEligible.jsx
@@ -23,7 +23,7 @@ const NotEligible = props => {
       <p className="vads-u-margin-top--0">
         {contactMessage}, call us at{' '}
         <va-telephone contact={CONTACTS.VA_BENEFITS} /> (
-        <va-telephone contact={711} tty />
+        <va-telephone contact="711" tty />
         ). We’re here Monday through Friday, 8:00 a.m. to 9:00 p.m. ET.
       </p>
       <p>
