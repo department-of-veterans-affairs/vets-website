cdcb8ad2e4798623a71f4889c670bac1e8744240
Tze-chiu Lei <tclei2009@gmail.com>
2023-09-12 19:09:54 -0700

    Refactor 20-0247 AddlCertsReqViewField component markup

--- src/applications/simple-forms/40-0247/components/AddlCertsReqViewField.js
+++ src/applications/simple-forms/40-0247/components/AddlCertsReqViewField.js
@@ -2,29 +2,31 @@ import React from 'react';
 
 export default function AddlCertsReqViewField({ formData }) {
   return (
-    <div className="vads-u-padding--2">
-      <div>
+    <>
+      <div className="vads-u-padding--2">
         <h3 className="vads-u-margin-top--0 vads-u-font-size--h5">
           Additional request
         </h3>
-        <dt className="vads-u-font-weight--bold">Address:</dt>
-        <dd>
+        <p className="vads-u-font-weight--bold vads-u-margin-y--0p5">
+          Address:
+        </p>
+        <p className="vads-u-margin-y--0">
           {formData.address.street}
           <br />
           {!!formData.address.street2 && (
             <>
-              <span>formData.street2</span>
+              <span>{formData.street2}</span>
               <br />
             </>
           )}
           {formData.address.city}, {formData.address.state}{' '}
           {formData.address.postalCode}
-        </dd>
+        </p>
+        <p className="vads-u-font-weight--bold vads-u-margin-y--0p5">
+          Quantity:
+        </p>
+        <p className="vads-u-margin-y--0">{formData.quantity}</p>
       </div>
-      <dt className="vads-u-margin-top-2 vads-u-font-weight--bold">
-        Quantity:
-      </dt>
-      <dd>{formData.quantity}</dd>
-    </div>
+    </>
   );
 }
