64a5e08d32b496ba13fb55bce576383f3582282a
Simi Adebowale <47654119+simiadebowale@users.noreply.github.com>
2023-09-14 17:52:05 -0400

    Update index.js

--- src/applications/vaos/services/appointment/index.js
+++ src/applications/vaos/services/appointment/index.js
@@ -878,9 +878,9 @@ export function getLink({
           appointment.id
         }`;
   }
-  return `${
-    featureStatusImprovement && isPastAppointment ? 'past' : 'upcoming'
-  }/${appointment.id}`;
+  return `${featureStatusImprovement && isPastAppointment ? 'past' : ''}/${
+    appointment.id
+  }`;
 }
 
 export function getPractitionerName(appointment) {
