ef70fbebb2edde89a16ad4cceccae3bd886c89e0
Dakota Larson <dakota.larson@oddball.io>
2023-09-13 17:08:44 -0700

    Revert content changes

--- src/applications/vre/28-8832/containers/IntroductionPage.jsx
+++ src/applications/vre/28-8832/containers/IntroductionPage.jsx
@@ -20,7 +20,7 @@ const IntroductionPage = props => {
     <div className="schemaform-intro">
       <FormTitle title="Apply for Personalized Career Planning and Guidance" />
       <p>
-        Equal to VA Form 28-8832 (Education/Vocational Counseling Application).
+        Equal to VA Form 25-8832 (Education/Vocational Counseling Application).
       </p>
       <SaveInProgressIntro
         prefillEnabled={props.route.formConfig.prefillEnabled}
@@ -32,7 +32,7 @@ const IntroductionPage = props => {
         headingLevel={2}
       >
         <p>
-          Please complete the 25-8832 form to apply for Planning and career
+          Please complete the 28-8832 form to apply for Planning and career
           guidance.
         </p>
       </SaveInProgressIntro>
