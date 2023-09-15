927eee61895b581a07f9db1a8fff0b21e5581e4a
Tze-chiu Lei <tclei2009@gmail.com>
2023-09-14 18:12:56 -0700

    Update 40-0247 Vet Supporting Docs page

--- src/applications/simple-forms/40-0247/pages/veteranSupportDocs.js
+++ src/applications/simple-forms/40-0247/pages/veteranSupportDocs.js
@@ -7,51 +7,31 @@ import { FileField } from 'platform/forms-system/src/js/fields/FileField';
 /** @type {PageSchema} */
 export default {
   uiSchema: {
-    'ui:title': (
-      <span className="custom-label">Military/Discharge Documents</span>
-    ),
     'ui:description': (
       <>
         <p>
-          To receive a Presidential Memorial Certificate, specific active duty
-          service requirements must be met. Military discharge documents aren’t
-          required, but they are preferred since they help speed up the
-          application review.
-        </p>
-        <h4>Enlisted Service Members</h4>
-        <ul>
-          <li>
-            For those who served after September 7, 1980: A minimum of 24 months
-            of consecutive active service is required.
-          </li>
-          <li>
-            For those who served before September 7, 1980: At least 1 day of
-            active service is required.
-          </li>
-        </ul>
-        <h4>Enlisted Officers</h4>
-        <ul>
-          <li>
-            For those who served after October 16, 1981: A minimum of 24 months
-            of consecutive active service is necessary.
-          </li>
-          <li>
-            For those who served before October 16, 1981: At least 1 day of
-            active service is required.
-          </li>
-        </ul>
-        <p>
-          <strong>Note</strong>: Active duty for training purposes does not
-          count towards the active duty service requirement.
+          We don’t require that you submit anything with this form. But to speed
+          up the process, we encourage you to submit military records or
+          discharge documents if they’re available.
         </p>
         <p>
-          The preferred discharge document to establish eligibility for the
-          certificate is Form DD214.
+          To be eligible for a Presidential Memorial Certificate, the deceased
+          Veteran or Reservist must meet eligibility requirements for burial in
+          a VA national cemetery.
+        </p>
+        <p>Not sure if the Veteran or Reservist is eligible?</p>
+        <p>
+          <a href="/burials-memorials/eligibility/">
+            Check eligibility requirements for burial in a VA national cemetary
+          </a>
+        </p>
+        <p className="vads-u-margin-bottom--4">
+          We prefer that you upload the Veteran’s or Reservist’s DD214.
         </p>
       </>
     ),
     veteranSupportingDocuments: {
-      'ui:title': 'Upload files',
+      'ui:title': 'Upload documents',
       'ui:field': FileField,
       'ui:options': {
         hideLabelText: false,
