01ec7607878c4a4c6fcff90b1678a15b305e6b96
Kenneth Santiago <santiago_kenneth@bah.com>
2023-09-15 09:23:36 -0500

    MBMS-50233 Fixed linting issues

--- src/applications/pre-need/PreNeedApp.jsx
+++ src/applications/pre-need/PreNeedApp.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-
+import recordEvent from 'platform/monitoring/record-event';
 import formConfig from './config/form';
 
-export default function PreNeedApp({ location, children }) {
+export default function PreNeedApp({ loading, location, children }) {
+  // find all yes/no check boxes and attach analytics events
+  useEffect(
+    () => {
+      if (!loading) {
+        const radios = document.querySelectorAll('input[type="radio"]');
+        for (const radio of radios) {
+          radio.onclick = e => {
+            const label = e.target.nextElementSibling.innerText;
+            recordEvent({
+              event: 'int-radio-button-option-click',
+              'radio-button-label': label,
+              'radio-button-optionLabel': e.target.value,
+              'radio-button-required': e.target.required,
+            });
+          };
+        }
+      }
+    },
+    [loading, location],
+  );
   return (
     <article id="pre-need" data-location={`${location?.pathname?.slice(1)}`}>
       <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
@@ -16,6 +36,7 @@ export default function PreNeedApp({ location, children }) {
 }
 
 PreNeedApp.propTypes = {
+  loading: PropTypes.bool.isRequired,
   children: PropTypes.object,
   location: PropTypes.object,
 };
