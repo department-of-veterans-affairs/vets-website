199068a704547391df6dd5cc8631a0306bb61d97
Jami Gibbs <jami.gibbs@adhocteam.us>
2023-09-18 14:02:37 -0500

    remove telephone eslint disable

--- src/applications/virtual-agent/components/page/Disclaimer.jsx
+++ src/applications/virtual-agent/components/page/Disclaimer.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable @department-of-veterans-affairs/prefer-telephone-component */
-/* eslint-disable @department-of-veterans-affairs/telephone-contact-digits */
 import React from 'react';
 import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
 
