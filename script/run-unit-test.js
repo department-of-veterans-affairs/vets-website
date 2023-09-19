edb73acedfbaa6b877b62c77f0664766c55418c0
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2023-09-19 12:46:29 -0400

    Revert last changes

--- script/run-unit-test.js
+++ script/run-unit-test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const commandLineArgs = require('command-line-args');
-// const glob = require('glob');
+const glob = require('glob');
 const printUnitTestHelp = require('./run-unit-test-help');
 const { runCommand } = require('./utils');
 // For usage instructions see https://github.com/department-of-veterans-affairs/vets-website#unit-tests
@@ -26,52 +26,50 @@ const COMMAND_LINE_OPTIONS_DEFINITIONS = [
 ];
 
 const options = commandLineArgs(COMMAND_LINE_OPTIONS_DEFINITIONS);
-// let coverageInclude = '';
+let coverageInclude = '';
 
-// if (
-//   options['app-folder'] &&
-//   options.path[0] === defaultPath &&
-//   options.path.length === 1
-// ) {
-//   options.path[0] = options.path[0].replace(
-//     `/${specDirs}/`,
-//     `/src/applications/${options['app-folder']}/`,
-//   );
+if (
+  options['app-folder'] &&
+  options.path[0] === defaultPath &&
+  options.path.length === 1
+) {
+  options.path[0] = options.path[0].replace(
+    `/${specDirs}/`,
+    `/src/applications/${options['app-folder']}/`,
+  );
 
-//   const unitTestList = glob.sync(options.path[0]);
-//   if (!unitTestList.length) {
-//     console.log('There are no unit tests in the app folder.');
-//     process.exit(0);
-//   }
+  const unitTestList = glob.sync(options.path[0]);
+  if (!unitTestList.length) {
+    console.log('There are no unit tests in the app folder.');
+    process.exit(0);
+  }
 
-//   coverageInclude = `--include 'src/applications/${options['app-folder']}/**'`;
-// }
+  coverageInclude = `--include 'src/applications/${options['app-folder']}/**'`;
+}
 
-// const reporterOption = options.reporter ? `--reporter ${options.reporter}` : '';
+const reporterOption = options.reporter ? `--reporter ${options.reporter}` : '';
 
 if (options.help) {
   printUnitTestHelp();
   process.exit(0);
 }
 
-// const mochaPath = `BABEL_ENV=test NODE_ENV=test mocha ${reporterOption}`;
-// const coverageReporter = options['coverage-html']
-//   ? '--reporter=html mocha --retries 5'
-//   : '--reporter=json-summary mocha --reporter mocha-multi-reporters --reporter-options configFile=config/mocha-multi-reporter.js --no-color --retries 5';
-// const coveragePath = `NODE_ENV=test nyc --all ${coverageInclude} ${coverageReporter}`;
-// const testRunner = options.coverage ? coveragePath : mochaPath;
-// const configFile = options.config ? options.config : 'config/mocha.json';
-// let testsToVerify = null;
-// console.log(process.env.TESTS_TO_VERIFY);
-// if (process.env.TESTS_TO_VERIFY) {
-//   testsToVerify = JSON.parse(process.env.TESTS_TO_VERIFY).join(' ');
-// }
+const mochaPath = `BABEL_ENV=test NODE_ENV=test mocha ${reporterOption}`;
+const coverageReporter = options['coverage-html']
+  ? '--reporter=html mocha --retries 5'
+  : '--reporter=json-summary mocha --reporter mocha-multi-reporters --reporter-options configFile=config/mocha-multi-reporter.js --no-color --retries 5';
+const coveragePath = `NODE_ENV=test nyc --all ${coverageInclude} ${coverageReporter}`;
+const testRunner = options.coverage ? coveragePath : mochaPath;
+const configFile = options.config ? options.config : 'config/mocha.json';
+let testsToVerify = null;
+console.log(process.env.TESTS_TO_VERIFY);
+if (process.env.TESTS_TO_VERIFY) {
+  testsToVerify = JSON.parse(process.env.TESTS_TO_VERIFY).join(' ');
+}
 
-// const command = `LOG_LEVEL=${options[
-//   'log-level'
-// ].toLowerCase()} ${testRunner} --max-old-space-size=4096 --config ${configFile} --recursive ${testsToVerify ||
-//   options.path.map(p => `'${p}'`).join(' ')}`;
-const command = `mocha src/applications/appeals/995/tests/components/EvidenceVaRecords.unit.spec.jsx src/applications/check-in/hooks/tests/useSendTravelPayClaim/useSendTravelPayClaim.unit.spec.jsx
-`;
+const command = `LOG_LEVEL=${options[
+  'log-level'
+].toLowerCase()} ${testRunner} --max-old-space-size=4096 --config ${configFile} --recursive ${testsToVerify ||
+  options.path.map(p => `'${p}'`).join(' ')}`;
 console.log('command: ', command);
 runCommand(command);
