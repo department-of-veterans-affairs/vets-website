5aa0c3e34d06b48c15373dfa6018e85eba3a3a02
Alexander Garcia <67602137+asg5704@users.noreply.github.com>
2023-09-19 15:34:08 -0400

    Terms of Use - Bug fixes (#25698)
    * update: fixes terms-of-use to remove postLogin=true query for MHV
    
    * fix: autoSSO + tests to add terms-of-use pathname
    
    * fix: ToU component + tests
    
    * fix: isLoggedIn state

--- src/applications/terms-of-use/tests/TermsOfUse.unit.spec.jsx
+++ src/applications/terms-of-use/tests/TermsOfUse.unit.spec.jsx
@@ -6,13 +6,8 @@ import { expect } from 'chai';
 import { $, $$ } from 'platform/forms-system/src/js/utilities/ui';
 import TermsOfUse, { parseRedirectUrl } from '../containers/TermsOfUse';
 
-const store = ({ isLoggedIn = false, termsOfUseEnabled = true } = {}) => ({
+const store = ({ termsOfUseEnabled = true } = {}) => ({
   getState: () => ({
-    user: {
-      login: {
-        currentlyLoggedIn: isLoggedIn,
-      },
-    },
     featureToggles: {
       // eslint-disable-next-line camelcase
       terms_of_use: termsOfUseEnabled,
@@ -37,7 +32,7 @@ describe('TermsOfUse', () => {
     expect($('va-accordion', container)).to.exist;
     expect($('va-alert', container)).to.exist;
   });
-  it('should display content if not signed in', () => {
+  it('should display content regardless of loggedIn state', () => {
     const mockStore = store();
     const { container } = render(
       <Provider store={mockStore}>
@@ -47,7 +42,7 @@ describe('TermsOfUse', () => {
     expect($$('va-button', container).length).to.eql(2);
   });
   it('should NOT display Accept or Decline buttons termsOfUse Flipper is disabled', () => {
-    const mockStore = store({ isLoggedIn: true, termsOfUseEnabled: false });
+    const mockStore = store({ termsOfUseEnabled: false });
     const { container } = render(
       <Provider store={mockStore}>
         <TermsOfUse />
@@ -59,20 +54,17 @@ describe('TermsOfUse', () => {
 });
 
 describe('parseRedirectUrl', () => {
-  it('should return the proper url', () => {
-    expect(
-      parseRedirectUrl(
-        'https%3A%2F%2Fdev.va.gov%2Fauth%2Flogin%2Fcallback%2F%3Ftype%3Didme',
-      ),
-    ).to.eql('https://dev.va.gov/auth/login/callback/?type=idme');
-    expect(
-      parseRedirectUrl('https://staging-patientportal.myhealth.va.gov'),
-    ).to.eql('https://staging-patientportal.myhealth.va.gov');
-    expect(
-      parseRedirectUrl('https://int.eauth.va.gov/mhv-portal-web/eauth'),
-    ).to.eql('https://int.eauth.va.gov/mhv-portal-web/eauth');
-    expect(parseRedirectUrl('https://google.com?q=https://va.gov')).to.eql(
-      'https://dev.va.gov',
-    );
+  const testUrls = {
+    [`https%3A%2F%2Fdev.va.gov%2Fauth%2Flogin%2Fcallback%2F%3Ftype%3Didme`]: `https://dev.va.gov/auth/login/callback/?type=idme`,
+    [`https://staging-patientportal.myhealth.va.gov`]: `https://staging-patientportal.myhealth.va.gov`,
+    [`https://staging-patientportal.myhealth.va.gov/?authenticated=true`]: `https://staging-patientportal.myhealth.va.gov/?authenticated=true`,
+    [`https://int.eauth.va.gov/mhv-portal-web/eauth&postLogin=true`]: `https://int.eauth.va.gov/mhv-portal-web/eauth`,
+    [`https://int.eauth.va.gov/mhv-portal-web/eauth?deeplinking=home&postLogin=true`]: `https://int.eauth.va.gov/mhv-portal-web/eauth?deeplinking=home&postLogin=true`,
+    [`https://google.com?q=https://va.gov`]: `https://dev.va.gov`,
+  };
+  Object.entries(testUrls).forEach(([parsedUrl, formattedUrl]) => {
+    it('should return the proper url', () => {
+      expect(parseRedirectUrl(parsedUrl)).to.eql(formattedUrl);
+    });
   });
 });
