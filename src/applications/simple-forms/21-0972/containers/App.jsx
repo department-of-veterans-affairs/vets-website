01e59d2a08cf4ca0c1ba5a07bb4eb7721cf77f44
Tze-chiu Lei <tclei2009@gmail.com>
2023-09-20 16:38:13 -0700

    Add feature-flag functionality to 21-0972

--- src/applications/simple-forms/21-0972/containers/App.jsx
+++ src/applications/simple-forms/21-0972/containers/App.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 
-import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-import formConfig from '../config/form';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default function App({ location, children }) {
+import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
+import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
+import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+
+import formConfig from '../config/form';
+import { WIP } from '../../shared/components/WIP';
+import { workInProgressContent } from '../definitions/constants';
+
+function App({ location, children, show210972 }) {
+  // console.info('[App] show210972:', show210972);
+  if (!show210972) {
+    return <WIP content={workInProgressContent} />;
+  }
+
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
     </RoutedSavableApp>
   );
 }
+
+const mapStateToProps = state => ({
+  show210972: toggleValues(state)[FEATURE_FLAG_NAMES.form210972] || false,
+});
+
+App.propTypes = {
+  children: PropTypes.element.isRequired,
+  location: PropTypes.object.isRequired,
+  show210972: PropTypes.bool,
+};
+
+export default connect(mapStateToProps)(App);
