4713899d575734d240e5540f52f58fcc08bfd96e
Justin Lattimore <jlattimoreweb@gmail.com>
2023-09-20 10:34:20 -0500

    removed changes from trickle over commit

--- src/applications/mhv/medical-records/components/RecordList/RecordListItem.jsx
+++ src/applications/mhv/medical-records/components/RecordList/RecordListItem.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
 import VaccinesListItem from './VaccinesListItem';
 import VitalListItem from './VitalListItem';
 import ConditionListItem from './ConditionListItem';
@@ -11,14 +10,6 @@ import AllergyListItem from './AllergyListItem';
 
 const RecordListItem = props => {
   const { record, type } = props;
-
-  const history = useHistory();
-
-  useEffect(() => {
-    return history.listen(() => {
-      window.location.reload();
-    });
-  }, []);
 
   switch (type) {
     case recordType.LABS_AND_TESTS:
