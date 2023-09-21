475f72dc280b205b1163825e5aa01c3814a238a9
fazilqa <fazil.mammadov@bylight.com>
2023-09-21 13:24:13 -0700

    add single thread load

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-compose-message-signature.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-compose-message-signature.cypress.spec.js
@@ -1,16 +1,39 @@
 import SecureMessagingSite from './sm_site/SecureMessagingSite';
 import PatientInboxPage from './pages/PatientInboxPage';
-import { AXE_CONTEXT } from './utils/constants';
+import mockSingleThread from './fixtures/inboxResponse/single-thread-response.json';
+import mockMessageDetails from './fixtures/messages-response.json';
+import { AXE_CONTEXT, Paths } from './utils/constants';
 
-describe('Secure Messaging Compose', () => {
-  it('can send message', () => {
-    const landingPage = new PatientInboxPage();
-    const site = new SecureMessagingSite();
+describe('verify signature', () => {
+  const landingPage = new PatientInboxPage();
+  const site = new SecureMessagingSite();
+  beforeEach(() => {
     site.login();
     landingPage.loadInboxMessages();
+  });
+  it.skip('signature added on composing', () => {
     landingPage.navigateToComposePage();
-
     landingPage.verifySignature();
+
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
+  });
+
+  it('signature added on replying', () => {
+    // add folders intercept
+    cy.intercept('GET', `${Paths.SM_API_BASE}/messages/`, mockSingleThread);
+    // add messages/number/thread intercept
+
+    cy.get('[data-testid="thread-list-item"]')
+      .first()
+      .find(`#message-link-${mockMessageDetails.data[0].attributes.messageId}`)
+      .click();
 
     cy.injectAxe();
     cy.axeCheck(AXE_CONTEXT, {
