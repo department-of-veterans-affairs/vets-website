11aa2b3932cf46216fc977acb9b4657f742e5414
Matt Long <longmd@me.com>
2023-09-25 11:53:31 -0400

    Add approved content

--- src/applications/static-pages/hca-performance-warning/components/App/index.unit.spec.js
+++ src/applications/static-pages/hca-performance-warning/components/App/index.unit.spec.js
@@ -6,7 +6,7 @@ import { App } from '.';
 describe('HCA Performance Warning <App>', () => {
   describe('when the feature toggle is falsy', () => {
     it('should not render the warning', () => {
-      const wrapper = shallow(<App />);
+      const wrapper = shallow(<App show={false} />);
       expect(wrapper.find('va-alert')).to.have.lengthOf(0);
       wrapper.unmount();
     });
--- src/applications/static-pages/hca-performance-warning/index.jsx
+++ src/applications/static-pages/hca-performance-warning/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { connectFeatureToggle } from 'platform/utilities/feature-toggles';
 
 export default (store, widgetType) => {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
@@ -8,6 +9,8 @@ export default (store, widgetType) => {
     import(/* webpackChunkName: "hca-performance-warning" */
     './components/App').then(module => {
       const App = module.default;
+      connectFeatureToggle(store.dispatch);
+
       ReactDOM.render(
         <Provider store={store}>
           <App />
