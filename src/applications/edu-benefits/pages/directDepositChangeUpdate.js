25b792ddf673bef70c1fc2d82a39cd31dcd98c0c
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2023-09-25 14:24:43 -0500

    VFEP-68 Add Direct deposit description assistance when I don't have a bank account is selected.

--- src/applications/edu-benefits/pages/directDepositChangeUpdate.js
+++ src/applications/edu-benefits/pages/directDepositChangeUpdate.js
@@ -17,30 +17,44 @@ const gaBankInfoHelpText = () => {
   });
 };
 
+const directDepositDescriptionAssistance = (
+  <div className="vads-u-margin-top--2 vads-u-margin-bottom--2">
+    <p>
+      Direct deposit information is not mandatory at this time. However,
+      benefits cannot be awarded without this information per U.S. Treasury
+      regulation 31 C.F.R. § 208.3.
+    </p>
+    <p>For assistance call 1-888-442-4551 (1-888-GIBILL-1)</p>
+  </div>
+);
+
 const bankInfoHelpText = (
-  <va-additional-info
-    trigger="What if I don’t have a bank account?"
-    onClick={gaBankInfoHelpText}
-  >
-    <span>
-      <p>
-        The{' '}
-        <a href="https://veteransbenefitsbanking.org/">
-          Veterans Benefits Banking Program (VBBP)
-        </a>{' '}
-        provides a list of Veteran-friendly banks and credit unions. They’ll
-        work with you to set up an account, or help you qualify for an account,
-        so you can use direct deposit. To get started, call one of the
-        participating banks or credit unions listed on the VBBP website. Be sure
-        to mention the Veterans Benefits Banking Program.
-      </p>
-      <p>
-        Note: Federal regulation, found in 31 C.F.R. § 208.3 provides that,
-        subject to section 208.4, “all Federal payments made by an agency shall
-        be made by electronic funds transfer” (EFT).
-      </p>
-    </span>
-  </va-additional-info>
+  <>
+    {directDepositDescriptionAssistance}
+    <va-additional-info
+      trigger="What if I don’t have a bank account?"
+      onClick={gaBankInfoHelpText}
+    >
+      <span>
+        <p>
+          The{' '}
+          <a href="https://veteransbenefitsbanking.org/">
+            Veterans Benefits Banking Program (VBBP)
+          </a>{' '}
+          provides a list of Veteran-friendly banks and credit unions. They’ll
+          work with you to set up an account, or help you qualify for an
+          account, so you can use direct deposit. To get started, call one of
+          the participating banks or credit unions listed on the VBBP website.
+          Be sure to mention the Veterans Benefits Banking Program.
+        </p>
+        <p>
+          Note: Federal regulation, found in 31 C.F.R. § 208.3 provides that,
+          subject to section 208.4, “all Federal payments made by an agency
+          shall be made by electronic funds transfer” (EFT).
+        </p>
+      </span>
+    </va-additional-info>
+  </>
 );
 
 const directDepositDescription = (
@@ -48,8 +62,7 @@ const directDepositDescription = (
     <p>
       Direct deposit information is not mandatory at this time. However,
       benefits cannot be awarded without this information per U.S. Treasury
-      regulation 31 C.F.R. § 208.3. For assistance call 1-888-442-4551
-      (1-888-GIBILL-1)
+      regulation 31 C.F.R. § 208.3.
     </p>
     <img
       src="/img/direct-deposit-check-guide.svg"
