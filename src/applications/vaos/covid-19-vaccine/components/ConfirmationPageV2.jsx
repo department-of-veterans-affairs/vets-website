502931758ff5d59335c547f3fedce131d7c4574c
Tony Williams <tony.williams@va.gov>
2023-09-26 19:13:35 -0500

    update routes for request flow

--- src/applications/vaos/covid-19-vaccine/components/ConfirmationPageV2.jsx
+++ src/applications/vaos/covid-19-vaccine/components/ConfirmationPageV2.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { Redirect, useHistory } from 'react-router-dom';
-import { connect, useDispatch } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-import recordEvent from 'platform/monitoring/record-event';
+import { recordEvent } from '@department-of-veterans-affairs/platform-monitoring/exports';
 import moment from '../../lib/moment-tz';
 import { scrollAndFocus } from '../../utils/scrollAndFocus';
 import { getTimezoneByFacilityId } from '../../utils/timezone';
@@ -17,16 +17,17 @@ import {
 } from '../../services/location';
 import AppointmentDate from '../../new-appointment/components/ReviewPage/AppointmentDate';
 import { startNewAppointmentFlow } from '../redux/actions';
+import getNewAppointmentFlow from '../../new-appointment/newAppointmentFlow';
 
 const pageTitle = 'We’ve scheduled your appointment';
 
-function handleClick(history, dispatch) {
+function handleClick(history, dispatch, typeOfCare) {
   return () => {
     recordEvent({
       event: `${GA_PREFIX}-schedule-appointment-button-clicked`,
     });
     dispatch(startNewAppointmentFlow());
-    history.push(`/new-appointment`);
+    history.push(typeOfCare.url);
   };
 }
 
@@ -39,6 +40,7 @@ function ConfirmationPageV2({
 }) {
   const history = useHistory();
   const dispatch = useDispatch();
+  const { root, typeOfCare } = useSelector(getNewAppointmentFlow);
 
   useEffect(() => {
     document.title = `${pageTitle} | Veterans Affairs`;
@@ -68,7 +70,7 @@ function ConfirmationPageV2({
         <br />
         <div className="vads-u-margin-y--1">
           <va-link
-            href="/health-care/schedule-view-va-appointments/appointments/"
+            href={root.url}
             onClick={() => {
               recordEvent({
                 event: `${GA_PREFIX}-view-your-appointments-button-clicked`,
@@ -82,7 +84,7 @@ function ConfirmationPageV2({
           <va-link
             text="Schedule a new appointment"
             data-testid="schedule-appointment-link"
-            onClick={handleClick(history, dispatch)}
+            onClick={handleClick(history, dispatch, typeOfCare)}
           />
         </div>
       </InfoAlert>
