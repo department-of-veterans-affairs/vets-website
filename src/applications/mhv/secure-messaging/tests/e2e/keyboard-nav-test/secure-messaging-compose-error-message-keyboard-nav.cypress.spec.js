30d090c441bf86fd0d1d4c16c1a1634688260505
fazilqa <fazil.mammadov@bylight.com>
2023-09-27 13:41:52 -0700

    fix compose-error-message-keyboard-nav tests

--- src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-compose-error-message-keyboard-nav.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-compose-error-message-keyboard-nav.cypress.spec.js
@@ -3,91 +3,88 @@ import PatientInboxPage from '../pages/PatientInboxPage';
 import PatientComposePage from '../pages/PatientComposePage';
 import { AXE_CONTEXT, Locators } from '../utils/constants';
 
-for (let i = 0; i < 30; i += 1) {
-  describe('Secure Messaging Compose Errors Keyboard Nav', () => {
-    const landingPage = new PatientInboxPage();
-    const composePage = new PatientComposePage();
-    const site = new SecureMessagingSite();
-    beforeEach(() => {
-      site.login();
-      landingPage.loadInboxMessages();
-      landingPage.navigateToComposePage();
-    });
+describe('Secure Messaging Compose Errors Keyboard Nav', () => {
+  const landingPage = new PatientInboxPage();
+  const composePage = new PatientComposePage();
+  const site = new SecureMessagingSite();
+  beforeEach(() => {
+    site.login();
+    landingPage.loadInboxMessages();
+    landingPage.navigateToComposePage();
+  });
 
-    it('focus on error message for no provider', () => {
-      composePage.selectCategory();
+  it('focus on error message for no provider', () => {
+    composePage.selectCategory();
 
-      composePage.getMessageSubjectField().type('Test Subject');
-      composePage
-        .getMessageBodyField()
-        .type('Test Message Body', { force: true });
-      composePage.pushSendMessageWithKeyboardPress();
-      composePage.verifyFocusOnErrorMessageToSelectRecipient();
-      cy.injectAxe();
-      cy.axeCheck(AXE_CONTEXT, {
-        rules: {
-          'aria-required-children': {
-            enabled: false,
-          },
+    composePage.getMessageSubjectField().type('Test Subject');
+    composePage
+      .getMessageBodyField()
+      .type('Test Message Body', { force: true });
+    composePage.pushSendMessageWithKeyboardPress();
+    composePage.verifyFocusOnErrorMessageToSelectRecipient();
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
         },
-      });
-    });
-    it('focus on error message for empty category', () => {
-      composePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
-
-      composePage.getMessageSubjectField().type('Test Subject');
-      composePage
-        .getMessageBodyField()
-        .type('Test Message Body', { force: true });
-      composePage.pushSendMessageWithKeyboardPress();
-      composePage.verifyFocusOnErrorMessageToSelectCategory();
-      cy.injectAxe();
-      cy.axeCheck(AXE_CONTEXT, {
-        rules: {
-          'aria-required-children': {
-            enabled: false,
-          },
-        },
-      });
-    });
-    it('focus on error message for empty message subject', () => {
-      composePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
-      composePage.selectCategory();
-      composePage
-        .getMessageBodyField()
-        .type('Test Message Body', { force: true });
-      composePage.pushSendMessageWithKeyboardPress();
-      composePage.verifyFocusOnErrorEmptyMessageSubject();
-      cy.injectAxe();
-      cy.axeCheck(AXE_CONTEXT, {
-        rules: {
-          'aria-required-children': {
-            enabled: false,
-          },
-        },
-      });
-    });
-    it('focus on error message for empty message body', () => {
-      composePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
-      composePage.selectCategory();
-      composePage
-        .getMessageSubjectField()
-        .type('Test Subject', { force: true });
-
-      // lines below not working due to incorrect focusing ???
-      // composePage.pushSendMessageWithKeyboardPress();
-      // composePage.verifyFocusOnErrorEmptyMessageBody();
-
-      cy.get(Locators.BUTTONS.SEND).click();
-      composePage.verifyErrorEmptyMessageBody();
-      cy.injectAxe();
-      cy.axeCheck(AXE_CONTEXT, {
-        rules: {
-          'aria-required-children': {
-            enabled: false,
-          },
-        },
-      });
+      },
     });
   });
-}
+  it('focus on error message for empty category', () => {
+    composePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
+
+    composePage.getMessageSubjectField().type('Test Subject');
+    composePage
+      .getMessageBodyField()
+      .type('Test Message Body', { force: true });
+    composePage.pushSendMessageWithKeyboardPress();
+    composePage.verifyFocusOnErrorMessageToSelectCategory();
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
+  });
+  it('focus on error message for empty message subject', () => {
+    composePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
+    composePage.selectCategory();
+    composePage
+      .getMessageBodyField()
+      .type('Test Message Body', { force: true });
+    composePage.pushSendMessageWithKeyboardPress();
+    composePage.verifyFocusOnErrorEmptyMessageSubject();
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
+  });
+  it('focus on error message for empty message body', () => {
+    composePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
+    composePage.selectCategory();
+    composePage.getMessageSubjectField().type('Test Subject', { force: true });
+
+    // lines below not working due to incorrect focusing ???
+    // composePage.pushSendMessageWithKeyboardPress();
+    // composePage.verifyFocusOnErrorEmptyMessageBody();
+
+    cy.get(Locators.BUTTONS.SEND).click();
+    composePage.verifyErrorEmptyMessageBody();
+
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
+  });
+});
