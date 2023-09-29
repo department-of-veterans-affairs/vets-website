18cb9e8e731ef78cddf4bc92a66d822be4e99d55
Jennie McGibney <jennie.mcgibney@va.gov>
2023-09-28 17:10:38 -0700

    Update text in requested status alert

--- src/applications/vaos/appointment-list/components/RequestedStatusAlert.jsx
+++ src/applications/vaos/appointment-list/components/RequestedStatusAlert.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useLocation, useHistory } from 'react-router-dom';
 import recordEvent from 'platform/monitoring/record-event';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import InfoAlert from '../../components/InfoAlert';
 import {
   APPOINTMENT_STATUS,
@@ -10,14 +10,19 @@ import {
   GA_PREFIX,
 } from '../../utils/constants';
 import { startNewAppointmentFlow } from '../redux/actions';
+import { selectFeatureBreadcrumbUrlUpdate } from '../../redux/selectors';
 
-function handleClick(history, dispatch) {
+function handleClick(history, dispatch, featureBreadcrumbUrlUpdate) {
   return () => {
     recordEvent({
-      event: `${GA_PREFIX}-schedule-another-appointment-button-clicked`,
+      event: `${GA_PREFIX}-schedule-appointment-button-clicked`,
     });
     dispatch(startNewAppointmentFlow());
-    history.push(`/new-appointment`);
+    history.push(
+      featureBreadcrumbUrlUpdate
+        ? '/schedule/type-of-care'
+        : `/new-appointment`,
+    );
   };
 }
 
@@ -25,6 +30,9 @@ export default function RequestedStatusAlert({ appointment, facility }) {
   const history = useHistory();
   const dispatch = useDispatch();
   const { search } = useLocation();
+  const featureBreadcrumbUrlUpdate = useSelector(
+    selectFeatureBreadcrumbUrlUpdate,
+  );
 
   const queryParams = new URLSearchParams(search);
   const showConfirmMsg = queryParams.get('confirmMsg');
@@ -56,21 +64,29 @@ export default function RequestedStatusAlert({ appointment, facility }) {
             <br />
             <div className=" vads-u-margin-top--1">
               <va-link
-                href="/health-care/schedule-view-va-appointments/appointments/"
+                href={
+                  featureBreadcrumbUrlUpdate
+                    ? `/my-health/appointments`
+                    : '/health-care/schedule-view-va-appointments/appointments/'
+                }
                 onClick={() =>
                   recordEvent({
                     event: `${GA_PREFIX}-view-your-appointments-button-clicked`,
                   })
                 }
-                text="View your appointments"
-                data-testid="view-appointments-link"
+                text="Review your appointments"
+                data-testid="review-appointments-link"
               />
             </div>
             <div className=" vads-u-margin-top--1">
               <va-link
-                onClick={handleClick(history, dispatch)}
-                text="New appointment"
-                data-testid="new-appointment-link"
+                onClick={handleClick(
+                  history,
+                  dispatch,
+                  featureBreadcrumbUrlUpdate,
+                )}
+                text="Schedule a new appointment"
+                data-testid="schedule-appointment-link"
               />
             </div>
           </>
