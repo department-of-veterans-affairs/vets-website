2c6e587148ed37465af104916bfdaf849a3aaa1a
Vitalii Menshutin <133991282+vmenshutin-bylight@users.noreply.github.com>
2023-09-29 12:02:25 -0400

    MHV-47089: Medications List PDF (#25847)
    * MHV-47089: Medications List Page PDF

--- src/applications/mhv/medications/actions/prescriptions.js
+++ src/applications/mhv/medications/actions/prescriptions.js
@@ -1,5 +1,6 @@
 import { Actions } from '../util/actionTypes';
 import { getPrescription, getPrescriptionList, fillRx } from '../api/rxApi';
+import { getAllergies } from '../../medical-records/api/MrApi';
 
 export const setSortedRxList = rxList => async dispatch => {
   dispatch({ type: Actions.Prescriptions.SET_SORTED_LIST, rxList });
@@ -9,6 +10,17 @@ export const getPrescriptionsList = () => async dispatch => {
   try {
     const response = await getPrescriptionList();
     dispatch({ type: Actions.Prescriptions.GET_LIST, response });
+    return null;
+  } catch (error) {
+    return error;
+  }
+};
+
+// TODO: consider re-using this action from medical-records
+export const getAllergiesList = () => async dispatch => {
+  try {
+    const response = await getAllergies();
+    dispatch({ type: Actions.Allergies.GET_LIST, response });
     return null;
   } catch (error) {
     return error;
