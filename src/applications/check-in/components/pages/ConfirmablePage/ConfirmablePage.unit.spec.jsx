5183310d768236d74ffad8a18c815a2c3cbed344
John Woolschlager <wullaski@gmail.com>
2023-10-03 15:13:42 -0400

    Add unit test

--- src/applications/check-in/components/pages/ConfirmablePage/ConfirmablePage.unit.spec.jsx
+++ src/applications/check-in/components/pages/ConfirmablePage/ConfirmablePage.unit.spec.jsx
@@ -67,6 +67,23 @@ describe('pre-check-in experience', () => {
         expect(getByText('foo-title')).to.exist;
         expect(getByText('Not available')).to.exist;
       });
+      it('renders help text if provided', () => {
+        const helpText = <div data-testid="help-text">FOO</div>;
+        const { getByTestId } = render(
+          <CheckInProvider>
+            <ConfirmablePage helpText={helpText} />
+          </CheckInProvider>,
+        );
+        expect(getByTestId('help-text')).to.exist;
+      });
+      it('does not render help text if not provided', () => {
+        const { queryByTestId } = render(
+          <CheckInProvider>
+            <ConfirmablePage />
+          </CheckInProvider>,
+        );
+        expect(queryByTestId('help-text')).to.not.exist;
+      });
       it('renders additional info if provided', () => {
         const additionalInfo = <div data-testid="additional-info">FOO</div>;
         const { getByTestId } = render(
