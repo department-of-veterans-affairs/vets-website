4c07c8ed0ea28f63742c128fc2a93987ca53f6ec
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2023-10-03 12:27:50 -0400

    wording adjustment

--- script/github-actions/verify-merge-eligibility.js
+++ script/github-actions/verify-merge-eligibility.js
@@ -7,7 +7,7 @@ const testsBlockingMerge = process.env.TESTS_BLOCKING_MERGE
 if (testsBlockingMerge.length > 0) {
   core.setFailed(
     `*MERGE BLOCK WARNING* This PR has test specs that have been disabled to to flakiness. \n 
-     Beginning on Nov 6th, 2023, merging will be blocked to PRs in products that have flaky Unit/E2E tests associated with them. Please resolve these tests BY 11/5/23 in order to avoid merge blocking.\n
+     Beginning on Nov 6th, 2023, merging will be blocked for PRs in products that have flaky Unit/E2E tests associated with them. Please resolve these tests BY 11/5/23 in order to avoid merge blocking.\n
      More information is available at: https://depo-platform-documentation.scrollhelp.site/developer-docs/test-stability-review.\n
     
      The file paths causing this status are: \n ${testsBlockingMerge.join(
