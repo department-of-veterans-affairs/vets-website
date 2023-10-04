3ebc8ff50d4757be7c8a7263dcef702586098642
Richard Davis <richard.davis@agile6.com>
2023-10-04 16:49:51 -0400

    update spec

--- src/applications/personalization/profile/components/personal-health-care-contacts/Contact.unit.spec.jsx
+++ src/applications/personalization/profile/components/personal-health-care-contacts/Contact.unit.spec.jsx
@@ -4,6 +4,7 @@ import { render } from '@testing-library/react';
 import Contact from './Contact';
 
 const initialProps = {
+  contactType: 'Primary Next of Kin',
   prefix: 'Mrs.',
   givenName: 'Rachel',
   middleName: 'Walker',
@@ -37,6 +38,13 @@ describe('Contact Component', () => {
 
   it('renders name and phone number if addressLine1 is not present', () => {
     const { container } = setup({ addressLine1: '' });
+    expect(container.textContent).to.contain('Mrs. Rachel Walker Revere');
+    expect(container.textContent).to.contain('617-555-1111');
+    expect(container.textContent).not.to.contain('Boston, MA 02113');
+  });
+
+  it('renders name and phone number when contactType is an emergency contact', () => {
+    const { container } = setup({ contactType: 'Emergency Contact' });
     expect(container.textContent).to.contain('Mrs. Rachel Walker Revere');
     expect(container.textContent).to.contain('617-555-1111');
     expect(container.textContent).not.to.contain('Boston, MA 02113');
