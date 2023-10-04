f9fb12e5d395c09731702223320217e24f38b7d4
Alex <AlexanderSherrow@gmail.com>
2023-10-04 16:10:18 -0400

    MBMS-50233 added analytics for pre-need

--- src/applications/pre-need/PreNeedApp.jsx
+++ src/applications/pre-need/PreNeedApp.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-
+import recordEvent from 'platform/monitoring/record-event';
 import formConfig from './config/form';
 
-export default function PreNeedApp({ location, children }) {
+export default function PreNeedApp({ loading, location, children }) {
+  // find all yes/no check boxes and attach analytics events
+  useEffect(
+    () => {
+      const radios = document.querySelectorAll('input[type="radio"], va-radio');
+
+      for (const radio of radios) {
+        radio.onclick = e => {
+          const name = e.target.attributes.name.value;
+          let optionalLabel = e.target.nextElementSibling.innerHTML;
+          // conditional to remove PII on page 5/6 of 6/7
+          if (
+            name ===
+            'root_application_applicant_applicantRelationshipToClaimant'
+          ) {
+            if (optionalLabel === 'Someone else, such as a preparer')
+              optionalLabel = 'Authorized Agent/Rep';
+            else optionalLabel = 'Self';
+          }
+          const currentEvent = {
+            event: 'int-radio-option-click',
+            'radio-button-label': name,
+            'radio-button-optionLabel': optionalLabel,
+            'radio-button-required': true,
+          };
+          const priorEvent = window.dataLayer[window.dataLayer.length - 1];
+          // if prior event is identical to current event it must be a duplicate.
+          if (
+            !priorEvent ||
+            JSON.stringify(currentEvent) !== JSON.stringify(priorEvent)
+          )
+            recordEvent({
+              event: 'int-radio-option-click',
+              'radio-button-label': name,
+              'radio-button-optionLabel': optionalLabel,
+              'radio-button-required': true,
+            });
+        };
+      }
+    },
+    [loading, location],
+  );
   return (
     <article id="pre-need" data-location={`${location?.pathname?.slice(1)}`}>
       <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
