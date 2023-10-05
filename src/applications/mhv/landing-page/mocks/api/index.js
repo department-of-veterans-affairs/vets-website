b40c8182864a2a91c500300691dbf518dd5fee5f
Patrick Bateman <batemapf@gmail.com>
2023-10-05 10:18:37 -0400

    clean up mocks

--- src/applications/mhv/landing-page/mocks/api/index.js
+++ src/applications/mhv/landing-page/mocks/api/index.js
@@ -5,14 +5,12 @@ const commonResponses = require('../../../../../platform/testing/local-dev-mock-
 
 const featureToggles = require('./feature-toggles/index');
 const user = require('./user/index');
-const contentBuild = require('./content-build/index');
 const folders = require('./mhv-api/messaging/folders/index');
 
 const responses = {
   ...commonResponses,
   'GET /v0/user': user.defaultUser,
   'GET /v0/feature_toggles': featureToggles.generateFeatureToggles({}),
-  'GET /data/cms/vamc-ehr.json': contentBuild.vamcEhr,
   'GET /my_health/v1/messaging/folders': folders.allFoldersWithUnreadMessages,
 };
 
