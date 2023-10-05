7a03c45c59d9b478464ae80d5b527972c25077f5
tynyttie <ty_barnes_jobs@yahoo.com>
2023-10-05 11:53:35 -0400

    MHV-49829 Updated-PDF-Verbaige (#26003)
    * Refactor pages to utilize helper.js for PDF footer. Also remove details button from Vaccines page, Conditions page, and Vitals page.
    
    * Restored vitals to oringinal state. Update unit test and removed details link.
    
    * Added H3 component to ComditionListItem. Plus, restored unit test in VitalListItem to previous code
    
    * Added data-dd-privacy=maskn to H3 tag in VaccinesListItem.jsx and ConditionListItem.jsx
    
    ---------
    
    Co-authored-by: Ty Barnes <tybarnes@Tys-MacBook-Pro.local>

--- src/applications/mhv/medical-records/components/RecordList/ConditionListItem.jsx
+++ src/applications/mhv/medical-records/components/RecordList/ConditionListItem.jsx
@@ -14,34 +14,25 @@ const ConditionListItem = props => {
           className="record-list-item vads-u-padding--3 vads-u-border-color--gray-light vads-u-border--0 vads-u-background-color--gray-lightest card"
           data-testid="record-list-item"
         >
+          <h3 className="vads-u-font-size--h4 vads-u-margin--0 vads-u-line-height--4 no-print">
+            <Link
+              to={`/conditions/${record.id}`}
+              className="vads-u-margin--0"
+              data-dd-privacy="mask"
+              aria-label={`${record.name} on ${formattedDate}`}
+            >
+              {record.name}
+            </Link>
+          </h3>
           <h3
-            className="vads-u-font-size--h4 vads-u-margin--0 vads-u-line-height--4"
-            aria-label={`${record.name} ${formattedDate}`}
+            className="vads-u-font-size--h4 vads-u-line-height--4 print-only"
             data-dd-privacy="mask"
+            aria-label={`${record.name} ${formattedDate}`}
           >
             {record.name}
           </h3>
 
-          <p className="vads-u-margin--0">
-            Date entered: <span data-dd-privacy="mask">{formattedDate}</span>
-          </p>
-          <Link
-            to={`/conditions/${record.id}`}
-            className="vads-u-margin--0"
-            aria-describedby={`details-button-description-${record.id}`}
-          >
-            <strong>Details</strong>
-            <i
-              className="fas fa-angle-right details-link-icon"
-              aria-hidden="true"
-            />
-            <span
-              id={`details-button-description-${record.id}`}
-              className="sr-only"
-            >
-              {record.name} {formattedDate}
-            </span>
-          </Link>
+          <p className="vads-u-margin--0">Date entered: {formattedDate}</p>
         </div>
       );
     }
