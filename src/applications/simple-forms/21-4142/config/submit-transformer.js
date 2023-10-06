137473063bfac8823bb485f05d21f1c76c446a2c
Robert Hasselle <robert.hasselle@oddball.io>
2023-10-06 14:24:22 -0500

    update text

--- src/applications/simple-forms/21-4142/config/submit-transformer.js
+++ src/applications/simple-forms/21-4142/config/submit-transformer.js
@@ -30,8 +30,9 @@ export default function transformForSubmit(formConfig, form) {
     transformedData.preparerIdentification.relationshipToVeteran ===
     alternateSigner
   ) {
+    // This text will put in the PDF, and we want it as short as possible
     transformedData.preparerIdentification.relationshipToVeteran =
-      'Alternate Signer';
+      'Alternate signer';
   }
 
   return JSON.stringify(transformedData);
