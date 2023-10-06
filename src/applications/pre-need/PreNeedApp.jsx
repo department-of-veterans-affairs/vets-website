861e11c17da6fea83cfcdea031bcf180978b8bc1
Alex <AlexanderSherrow@gmail.com>
2023-10-06 10:07:00 -0400

    MBMS-50233 added state analytics logic

--- src/applications/pre-need/PreNeedApp.jsx
+++ src/applications/pre-need/PreNeedApp.jsx
@@ -3,20 +3,74 @@ import PropTypes from 'prop-types';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import recordEvent from 'platform/monitoring/record-event';
+import { useSelector } from 'react-redux';
 import formConfig from './config/form';
+import { isVeteran } from './utils/helpers';
+
+const veteranLabels = [
+  [
+    'root_application_claimant_relationshipToVet',
+    'Relationship to service member',
+  ],
+  ['root_application_veteran_gender', 'Sex'],
+  ['root_application_veteran_maritalStatus', 'Marital status'],
+  [
+    'root_application_veteran_view:hasServiceName',
+    'Did you serve under another name?',
+  ],
+  [
+    'root_application_hasCurrentlyBuried',
+    'Is there anyone currently buried in a VA national cemetery under your eligibility?',
+  ],
+  [
+    'root_application_applicant_applicantRelationshipToClaimant',
+    'Who is filling out this application?',
+  ],
+  ['root_application_veteran_isDeceased', 'Has the sponsor died?'],
+];
+
+const nonVeteranLabels = [
+  [
+    'root_application_claimant_relationshipToVet',
+    'Relationship to service member',
+  ],
+  ['root_application_veteran_gender', 'Sponsor’s sex'],
+  ['root_application_veteran_maritalStatus', 'Sponsor’s marital status'],
+  [
+    'root_application_veteran_view:hasServiceName',
+    'Did the sponsor serve under another name?',
+  ],
+  [
+    'root_application_hasCurrentlyBuried',
+    'Is there anyone currently buried in a VA national cemetery under the sponsor’s eligibility?',
+  ],
+  [
+    'root_application_applicant_applicantRelationshipToClaimant',
+    'Who is filling out this application?',
+  ],
+  ['root_application_veteran_isDeceased', 'Has the sponsor died?'],
+];
+
+const veteranLabelMap = new Map(veteranLabels);
+const nonVeteranMap = new Map(nonVeteranLabels);
 
 export default function PreNeedApp({ loading, location, children }) {
+  const selectorData = useSelector(state => state.form || {});
   // find all yes/no check boxes and attach analytics events
   useEffect(
     () => {
+      const hasVeteran = isVeteran(selectorData.data);
       const radios = document.querySelectorAll('input[type="radio"]');
       for (const radio of radios) {
         radio.onclick = e => {
-          const title = e.target.attributes.name.value;
+          let title = e.target.attributes.name.value;
+          if (hasVeteran && veteranLabelMap.has(title))
+            title = veteranLabelMap.get(title);
+          else if (nonVeteranMap.has(title)) title = nonVeteranMap.get(title);
           let optionLabel = e.target.nextElementSibling.innerHTML;
           // conditional to remove PII on page 5/6 of 6/7
           if (
-            title ===
+            e.target.attributes.name.value ===
             'root_application_applicant_applicantRelationshipToClaimant'
           ) {
             if (optionLabel === 'Someone else, such as a preparer')
@@ -39,7 +93,7 @@ export default function PreNeedApp({ loading, location, children }) {
         };
       }
     },
-    [loading, location],
+    [loading, location, selectorData],
   );
   return (
     <article id="pre-need" data-location={`${location?.pathname?.slice(1)}`}>
