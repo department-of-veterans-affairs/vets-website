16876b04df673c1d1d6aa8872c4815dad4b8bbf5
Holden Hinkle <holden@holdenhinkle.com>
2023-10-06 11:22:11 -0400

    update App export

--- src/applications/representative/containers/App.jsx
+++ src/applications/representative/containers/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-import formConfig from '../config/form';
-
-export default function App({ location, children }) {
-  return (
-    <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-      {children}
-    </RoutedSavableApp>
-  );
+export default function App({ children }) {
+  return <div>{children}</div>;
 }
+
+// import React from 'react';
+
+// import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+// import formConfig from '../config/form';
+
+// export default function App({ location, children }) {
+//   return (
+//     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+//       {children}
+//     </RoutedSavableApp>
+//   );
+// }
