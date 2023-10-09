cc52718002a6ea323ea69bb603204c53f499239f
Brian Seek <brian.seek@civicactions.com>
2023-10-09 14:59:51 -0700

    reduced duplicated code in test.

--- src/applications/check-in/utils/pilotFeatures/pilotFeatures.unit.spec.js
+++ src/applications/check-in/utils/pilotFeatures/pilotFeatures.unit.spec.js
@@ -3,38 +3,51 @@ import { isInPilot } from './index';
 
 describe('check in utils', () => {
   describe('isInPilotFeature', () => {
+    const featuredAppointment = {
+      stationNo: '0001',
+      clinicIen: '0001',
+    };
+    const nonFeaturedStation = {
+      stationNo: '0002',
+      clinicIen: '0001',
+    };
+    const nonFeaturedClinic = {
+      stationNo: '0001',
+      clinicIen: '0002',
+    };
     it('should return false if feature not defined', () => {
-      const appointment = {
-        stationNo: '0001',
-        clinicIen: '0001',
-      };
-      expect(isInPilot({ appointment, pilotFeature: 'notAPilot' })).to.be.false;
+      expect(
+        isInPilot({
+          appointment: featuredAppointment,
+          pilotFeature: 'notAPilot',
+        }),
+      ).to.be.false;
     });
     it('should return false if station not in list', () => {
-      const appointment = {
-        stationNo: '0002',
-        clinicIen: '0001',
-      };
-      expect(isInPilot({ appointment, pilotFeature: 'fileTravelClaim' })).to.be
-        .false;
+      expect(
+        isInPilot({
+          appointment: nonFeaturedStation,
+          pilotFeature: 'fileTravelClaim',
+        }),
+      ).to.be.false;
     });
     it('should return false if clinic not in list', () => {
-      const appointment = {
-        stationNo: '0001',
-        clinicIen: '0002',
-      };
-      expect(isInPilot({ appointment, pilotFeature: 'fileTravelClaim' })).to.be
-        .false;
+      expect(
+        isInPilot({
+          appointment: nonFeaturedClinic,
+          pilotFeature: 'fileTravelClaim',
+        }),
+      ).to.be.false;
     });
     it('should return true if clinic, and station match', () => {
-      const appointment = {
-        stationNo: '0001',
-        clinicIen: '0001',
-      };
-      expect(isInPilot({ appointment, pilotFeature: 'fileTravelClaim' })).to.be
-        .true;
+      expect(
+        isInPilot({
+          appointment: featuredAppointment,
+          pilotFeature: 'fileTravelClaim',
+        }),
+      ).to.be.true;
     });
-    it('should return true only station listed', () => {
+    it('should return tru if only station is listed', () => {
       const appointment = {
         stationNo: '500',
         clinicIen: '3003',
