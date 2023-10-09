676e60c8ee8e5b1f93650e43b7e02e279d4a522b
Adam Whitlock <adam.whitlock@adhocteam.us>
2023-10-09 15:50:42 -0600

    fix: remove hca cross app imports

--- src/applications/personalization/view-dependents/components/ViewDependentsList/ViewDependentsList.jsx
+++ src/applications/personalization/view-dependents/components/ViewDependentsList/ViewDependentsList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import ViewDependentsListItem from '../ViewDependentsList/ViewDependentsListItem';
+import ViewDependentsListItem from './ViewDependentsListItem';
 
 const RemoveDependentSuccessMessage = () => (
   <p
