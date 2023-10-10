260370c6dc51edbf0be40626abd4f39b070e5ef3
Ty Barnes <tybarnes@Tys-MacBook-Pro.local>
2023-10-10 07:55:31 -0400

    Changed H2 tag of -Date entere- to P tag

--- src/applications/mhv/medical-records/containers/AllergyDetails.jsx
+++ src/applications/mhv/medical-records/containers/AllergyDetails.jsx
@@ -165,7 +165,7 @@ const AllergyDetails = () => {
           </h1>
           <div className="condition-subheader vads-u-margin-bottom--4">
             <div className="time-header">
-              <h2
+              <p
                 className="vads-u-font-size--base vads-u-font-family--sans"
                 id="allergy-date"
               >
@@ -176,7 +176,7 @@ const AllergyDetails = () => {
                 >
                   {allergy.date}
                 </span>
-              </h2>
+              </p>
             </div>
             <PrintDownload
               download={generateAllergyPdf}
