86286fba8f46fbc769fc8a2352f342b16126f0d7
Aaron Read <Aaron.Read@va.gov>
2023-10-10 10:45:30 -0500

    Update medications-validate-information-for-transferred-prescription.cypress.spec.js

--- src/applications/mhv/medications/tests/e2e/medications-validate-information-for-transferred-prescription.cypress.spec.js
+++ src/applications/mhv/medications/tests/e2e/medications-validate-information-for-transferred-prescription.cypress.spec.js
@@ -2,7 +2,7 @@ import MedicationsSite from './med_site/MedicationsSite';
 import MedicationsListPage from './pages/MedicationsListPage';
 
 describe('Medications List Page Information based on Medication Status', () => {
-  it('verify information on list view for transferred rx', () => {
+  it('verify information on list view for transferred prescriptions', () => {
     const site = new MedicationsSite();
     const listPage = new MedicationsListPage();
     cy.visit('my-health/about-medications/');
