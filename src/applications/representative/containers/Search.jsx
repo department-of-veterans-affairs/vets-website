5c21335d0446d4843e8222c7ae5a6e48fbf8d4c3
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2023-10-10 11:03:06 -0400

    Building out Search container

--- src/applications/representative/containers/Search.jsx
+++ src/applications/representative/containers/Search.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-
-import FormFooter from '@department-of-veterans-affairs/platform-forms/FormFooter';
-import formConfig from '../config/form';
+import React, { useState } from 'react';
+// import { VaCheckboxGroup } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import SearchControls from '../components/search/SearchControls';
+import ResultsList from '../components/search/ResultsList';
 
 export default function Search() {
+  const [showResults, setShowResults] = useState(false);
+
+  const handleSearch = e => {
+    e.preventDefault();
+    setShowResults(!showResults);
+  };
+
   return (
     <>
-      <h1>Search</h1>
-      <FormFooter formConfig={formConfig} />
+      <div className="representative-search">
+        <div className="title-section">
+          <h1>Find a Local Representative</h1>
+        </div>
+
+        <SearchControls handleSearch={handleSearch} />
+        {showResults && <ResultsList onSelect />}
+      </div>
     </>
   );
 }
