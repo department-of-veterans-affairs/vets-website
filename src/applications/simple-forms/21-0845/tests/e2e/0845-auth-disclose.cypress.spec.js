a127cc0e5881ca93b938f33c0b0e37b2f39e2389
Robert Hasselle <robert.hasselle@oddball.io>
2023-10-11 15:47:00 -0500

    update e2e tests - 0845 + 0972

--- src/applications/simple-forms/21-0845/tests/e2e/0845-auth-disclose.cypress.spec.js
+++ src/applications/simple-forms/21-0845/tests/e2e/0845-auth-disclose.cypress.spec.js
@@ -9,14 +9,56 @@ import featureToggles from '../../../shared/tests/e2e/fixtures/mocks/feature-tog
 import { AUTHORIZER_TYPES } from '../../definitions/constants';
 import { reviewAndSubmitPageFlow } from '../../../shared/tests/e2e/helpers';
 
+const awaitFocusSelectorThenTest = pagePath => {
+  return ({ afterHook }) => {
+    cy.injectAxeThenAxeCheck();
+    afterHook(() => {
+      const header =
+        pagePath === 'authorizer-type'
+          ? '#root_authorizerType-label'
+          : '#nav-form-header';
+      cy.get(header).should('be.visible');
+      cy.fillPage();
+      cy.axeCheck();
+      cy.findByText(/continue/i, { selector: 'button' }).click();
+    });
+  };
+};
+
+const pagePaths = [
+  'authorizer-type',
+  'authorizer-personal-information',
+  'authorizer-address',
+  'authorizer-contact-information',
+  'veteran-personal-information',
+  'veteran-identification-information',
+  'disclosure-information-third-party-type',
+  'disclosure-information-person-name',
+  'disclosure-information-person-address',
+  'disclosure-information-organization-name',
+  'disclosure-information-organization-representative',
+  'disclosure-information-organization-address',
+  'disclosure-information-scope',
+  'disclosure-information-limited-information',
+  'disclosure-information-release-duration',
+  'disclosure-information-release-and-date',
+  'security-information-question',
+  'security-information-answer',
+];
+
+const pageTestConfigs = pagePaths.reduce((obj, pagePath) => {
+  return {
+    ...obj,
+    [pagePath]: awaitFocusSelectorThenTest(pagePath),
+  };
+}, {});
+
 const testConfig = createTestConfig(
   {
     dataPrefix: 'data',
-
+    useWebComponentFields: true,
     dataDir: path.join(__dirname, 'fixtures', 'data'),
-
     dataSets: ['authTypeVet', 'authTypeNonVet'],
-
     pageHooks: {
       introduction: ({ afterHook }) => {
         afterHook(() => {
@@ -25,6 +67,7 @@ const testConfig = createTestConfig(
           }).click();
         });
       },
+      ...pageTestConfigs,
       'review-and-submit': ({ afterHook }) => {
         afterHook(() => {
           cy.get('@testData').then(data => {
@@ -42,13 +85,14 @@ const testConfig = createTestConfig(
       Cypress.config({ waitForAnimations: true, defaultCommandTimeout: 8000 });
       // Log in if the form requires an authenticated session.
       // cy.login();
-      cy.intercept('POST', formConfig.submitUrl, { statusCode: 200 });
+      cy.intercept('POST', formConfig.submitUrl, {
+        body: {
+          statusCode: 200,
+        },
+      });
       cy.intercept('GET', '/v0/feature_toggles*', featureToggles);
     },
-
-    // Skip tests in CI until the form is released.
-    // Remove this setting when the form has a content page in production.
-    skip: Cypress.env('CI'),
+    skip: false,
   },
   manifest,
   formConfig,
