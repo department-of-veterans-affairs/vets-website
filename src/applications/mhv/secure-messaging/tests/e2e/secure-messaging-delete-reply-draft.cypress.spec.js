b8eafa4a38c4624bd5a5e304919e86d3859ff3cd
Aaron Read <Aaron.Read@va.gov>
2023-10-12 12:09:57 -0500

    Update secure-messaging-delete-reply-draft.cypress.spec.js

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-delete-reply-draft.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-delete-reply-draft.cypress.spec.js
@@ -7,37 +7,36 @@ import PatientMessageDraftsPage from './pages/PatientMessageDraftsPage';
 import mockMessages from './fixtures/messages-response.json';
 import { AXE_CONTEXT } from './utils/constants';
 
-for (let i = 0; i < 200; i += 1) {
-  describe('Secure Messaging Reply', () => {
-    it('Axe Check Message Reply', () => {
-      const draftsPage = new PatientMessageDraftsPage();
-      const landingPage = new PatientInboxPage();
-      const messageDetailsPage = new PatientMessageDetailsPage();
-      const patientInterstitialPage = new PatientInterstitialPage();
-      const replyPage = new PatientReplyPage();
-      const site = new SecureMessagingSite();
-      site.login();
-      const messageDetails = landingPage.getNewMessageDetails();
+describe('Secure Messaging Reply', () => {
+  it('Axe Check Message Reply', () => {
+    const draftsPage = new PatientMessageDraftsPage();
+    const landingPage = new PatientInboxPage();
+    const messageDetailsPage = new PatientMessageDetailsPage();
+    const patientInterstitialPage = new PatientInterstitialPage();
+    const replyPage = new PatientReplyPage();
+    const site = new SecureMessagingSite();
+    site.login();
+    const messageDetails = landingPage.getNewMessageDetails();
 
-      landingPage.loadInboxMessages(mockMessages, messageDetails);
-      messageDetailsPage.loadMessageDetails(messageDetails);
-      messageDetailsPage.loadReplyPageDetails(messageDetails);
-      patientInterstitialPage.getContinueButton().click();
-      const testMessageBody = 'Test body';
-      replyPage.getMessageBodyField().click();
-      replyPage.getMessageBodyField().type(testMessageBody, { force: true });
-      cy.realPress(['Enter']).then(() => {
-        replyPage.saveReplyDraft(
+    landingPage.loadInboxMessages(mockMessages, messageDetails);
+    messageDetailsPage.loadMessageDetails(messageDetails);
+    messageDetailsPage.loadReplyPageDetails(messageDetails);
+    patientInterstitialPage.getContinueButton().click();
+    const testMessageBody = 'Test body';
+    replyPage.getMessageBodyField().click();
+    replyPage.getMessageBodyField().type(testMessageBody, { force: true });
+    cy.realPress(['Enter']).then(() => {
+      replyPage.saveReplyDraft(
+        messageDetails,
+        `\n\n\nName\nTitleTest${testMessageBody}`,
+      );
+      cy.log(
+        `the message details after saveReplyDraft ${JSON.stringify(
           messageDetails,
-          `\n\n\nName\nTitleTest${testMessageBody}`,
-        );
-        cy.log(
-          `the message details after saveReplyDraft ${JSON.stringify(
-            messageDetails,
-          )}`,
-        );
-      });
-      /*
+        )}`,
+      );
+    });
+    /*
       * This verification is redundant with secure-messaging-save-reply-draft
       messageDetailsPage.ReplyToMessageTO(messageDetails);
       messageDetailsPage.ReplyToMessagesenderName(messageDetails);
@@ -47,18 +46,17 @@ for (let i = 0; i < 200; i += 1) {
       messageDetails.data.attributes.body = messageDetailsBody;
       messageDetailsPage.ReplyToMessageBody(messageDetailsBody);
       */
-      draftsPage.clickDeleteButton();
-      draftsPage.confirmDeleteReplyDraftWithEnterKey(messageDetails);
-      draftsPage.verifyDeleteConfirmationMessage();
+    draftsPage.clickDeleteButton();
+    draftsPage.confirmDeleteReplyDraftWithEnterKey(messageDetails);
+    draftsPage.verifyDeleteConfirmationMessage();
 
-      cy.injectAxe();
-      cy.axeCheck(AXE_CONTEXT, {
-        rules: {
-          'aria-required-children': {
-            enabled: false,
-          },
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
         },
-      });
+      },
     });
   });
-}
+});
