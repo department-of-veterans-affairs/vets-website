87aad5d51345119659362d458fc3823d16c3fe7d
Holden Hinkle <holden@holdenhinkle.com>
2023-10-12 15:54:52 -0400

    update form breadcrumb urls

--- src/applications/representative/components/breadcrumbs/form/index.jsx
+++ src/applications/representative/components/breadcrumbs/form/index.jsx
@@ -4,10 +4,8 @@ function FormBreadcrumbs() {
   return (
     <va-breadcrumbs className="vads-u-font-family--sans no-wrap">
       <a href="/">Home</a>
-      <a href="/representative/form">Find a Local Representative</a>
-      <a href="/view-change-representative/search/introduction">
-        Appoint a Representative
-      </a>
+      <a href="/representative">Find a Representative</a>
+      <a href="/representative/form">Appoint a Representative</a>
     </va-breadcrumbs>
   );
 }
