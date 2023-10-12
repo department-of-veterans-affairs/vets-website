475e9c0e7d144125243db0f2f3336bca20799ec1
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2023-10-12 12:35:14 -0400

    Reordering jsx output

--- src/applications/representative/components/search/SearchControls.jsx
+++ src/applications/representative/components/search/SearchControls.jsx
@@ -9,11 +9,13 @@ const SearchControls = ({ handleSearch }) => {
       <div className="search-controls-container">
         <form id="facility-search-controls" onSubmit={e => handleSearch(e)}>
           <div className="columns">
-            <LocationInput />
             <div id="search-controls-bottom-row">
               <RepTypeSelector />
-              <input id="facility-search" type="submit" value="Search" />
             </div>
+            <LocationInput />
+            <button id="repreentative-search" type="submit" value="Search">
+              <i className="fas fa-search" /> Search
+            </button>
           </div>
         </form>
       </div>
