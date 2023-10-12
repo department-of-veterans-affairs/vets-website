0b35e84f9e647abdbfef55e4f696796419e1e0fc
Aaron Read <Aaron.Read@va.gov>
2023-10-12 12:11:28 -0500

    Update secure-messaging-inbox-user-navigation.cypress.spec.js

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-inbox-user-navigation.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-inbox-user-navigation.cypress.spec.js
@@ -4,45 +4,41 @@ import PatientComposePage from './pages/PatientComposePage';
 import mockSignature from './fixtures/signature-response.json';
 import { AXE_CONTEXT } from './utils/constants';
 
-for (let i = 0; i < 200; i += 1) {
-  describe('Secure Messaging Compose', () => {
-    it('can send message', () => {
-      const landingPage = new PatientInboxPage();
-      const composePage = new PatientComposePage();
-      const site = new SecureMessagingSite();
-      site.login();
-      landingPage.loadInboxMessages();
+describe('Secure Messaging Compose', () => {
+  it('can send message', () => {
+    const landingPage = new PatientInboxPage();
+    const composePage = new PatientComposePage();
+    const site = new SecureMessagingSite();
+    site.login();
+    landingPage.loadInboxMessages();
 
-      cy.intercept(
-        'GET',
-        '/my_health/v1/messaging/messages/signature',
-        mockSignature,
-      ).as('signature');
-      landingPage.navigateToComposePage();
-      cy.injectAxe();
-      cy.axeCheck(AXE_CONTEXT, {
-        rules: {
-          'aria-required-children': {
-            enabled: false,
-          },
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/messages/signature',
+      mockSignature,
+    ).as('signature');
+    landingPage.navigateToComposePage();
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
         },
-      });
-      composePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4', {
-        force: true,
-      });
-      cy.get('[name="COVID"]')
-        .first()
-        .click();
-      composePage.attachMessageFromFile('test_image.jpg', { force: true });
-      composePage
-        .getMessageSubjectField()
-        .type('Test Subject', { force: true });
-      composePage
-        .getMessageBodyField()
-        .type('Test message body', { force: true });
-      composePage.pushSendMessageWithKeyboardPress();
-      composePage.verifySendMessageConfirmationMessageText();
-      // landingPage.verifyInboxHeader('Inbox');
+      },
     });
+    composePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4', {
+      force: true,
+    });
+    cy.get('[name="COVID"]')
+      .first()
+      .click();
+    composePage.attachMessageFromFile('test_image.jpg', { force: true });
+    composePage.getMessageSubjectField().type('Test Subject', { force: true });
+    composePage
+      .getMessageBodyField()
+      .type('Test message body', { force: true });
+    composePage.pushSendMessageWithKeyboardPress();
+    composePage.verifySendMessageConfirmationMessageText();
+    // landingPage.verifyInboxHeader('Inbox');
   });
-}
+});
