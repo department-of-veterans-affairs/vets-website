b20c9a7bde89f9945b92b4cdc24c94df071dce54
fazilqa <fazil.mammadov@bylight.com>
2023-10-12 09:36:03 -0700

    remove cycling

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-navigate-away-from-replying-message.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-navigate-away-from-replying-message.cypress.spec.js
@@ -2,43 +2,41 @@ import SecureMessagingSite from './sm_site/SecureMessagingSite';
 import PatientInboxPage from './pages/PatientInboxPage';
 import { AXE_CONTEXT, Locators } from './utils/constants';
 
-for (let i = 0; i < 200; i += 1) {
-  describe('verify signature', () => {
-    const landingPage = new PatientInboxPage();
-    const site = new SecureMessagingSite();
+describe('verify signature', () => {
+  const landingPage = new PatientInboxPage();
+  const site = new SecureMessagingSite();
 
-    it('signature added on replying', () => {
-      site.login();
-      landingPage.loadInboxMessages();
-      landingPage.replyToMessage();
+  it('signature added on replying', () => {
+    site.login();
+    landingPage.loadInboxMessages();
+    landingPage.replyToMessage();
 
-      cy.get('#textarea').type('testMessageBody', {
-        waitForAnimations: true,
-        force: true,
-      });
+    cy.get('#textarea').type('testMessageBody', {
+      waitForAnimations: true,
+      force: true,
+    });
 
-      cy.get(Locators.FOLDERS.INBOX).click();
+    cy.get(Locators.FOLDERS.INBOX).click();
 
-      cy.get('[data-testid="reply-form"]')
-        .find('h1')
-        .should('have.text', "We can't save this message yet");
+    cy.get('[data-testid="reply-form"]')
+      .find('h1')
+      .should('have.text', "We can't save this message yet");
 
-      cy.get('[data-testid="reply-form"]')
-        .find('#modal-primary-button')
-        .should('have.text', 'Continue editing');
+    cy.get('[data-testid="reply-form"]')
+      .find('#modal-primary-button')
+      .should('have.text', 'Continue editing');
 
-      cy.get('[data-testid="reply-form"]')
-        .find('#modal-secondary-button')
-        .should('have.text', 'Delete draft');
+    cy.get('[data-testid="reply-form"]')
+      .find('#modal-secondary-button')
+      .should('have.text', 'Delete draft');
 
-      cy.injectAxe();
-      cy.axeCheck(AXE_CONTEXT, {
-        rules: {
-          'aria-required-children': {
-            enabled: false,
-          },
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
         },
-      });
+      },
     });
   });
-}
+});
