80502684687912a5c09a29b8c248eedada725df0
Tze-chiu Lei <tclei2009@gmail.com>
2023-10-12 09:55:07 -0700

    Use titleUI for 40-0247 Applicant Address page H3

--- src/applications/simple-forms/40-0247/pages/applicantAddress.js
+++ src/applications/simple-forms/40-0247/pages/applicantAddress.js
@@ -1,6 +1,5 @@
-import React from 'react';
-
 import {
+  titleUI,
   addressNoMilitarySchema,
   addressNoMilitaryUI,
 } from 'platform/forms-system/src/js/web-component-patterns';
@@ -8,16 +7,9 @@ import {
 /** @type {PageSchema} */
 export default {
   uiSchema: {
-    'ui:title': (
-      <>
-        <h3 className="vads-u-margin-y--0">
-          Tell us where we should send the certificate.
-        </h3>
-        <p className="vads-u-margin-y--0 vads-u-font-family--sans vads-u-font-weight--normal vads-u-font-size--base">
-          If you want us to send copies to another address, you can add an
-          additional address later in this form.
-        </p>
-      </>
+    ...titleUI(
+      'Tell us where we should send the certificate.',
+      'If you want us to send copies to another address, you can add an additional address later in this form.',
     ),
     applicantAddress: addressNoMilitaryUI({
       labels: {
