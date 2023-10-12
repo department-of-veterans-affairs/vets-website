e76a3fd95db46438f89ad47c1c4716ee30f390e2
Aaron Read <Aaron.Read@va.gov>
2023-10-11 20:10:05 -0500

    Update secure-messaging-draft-autosave.cypress.spec.js

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-draft-autosave.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-draft-autosave.cypress.spec.js
@@ -6,55 +6,53 @@ import { AXE_CONTEXT } from './utils/constants';
 import { draftAutoSaveTimeout } from '../../util/constants';
 import PatientComposePage from './pages/PatientComposePage';
 
-for (let i = 0; i < 200; i += 1) {
-  describe(manifest.appName, () => {
-    const composePage = new PatientComposePage();
-    describe('Verify draft auto save', () => {
-      beforeEach(() => {
-        const site = new SecureMessagingSite();
-        const landingPage = new PatientInboxPage();
+describe(manifest.appName, () => {
+  const composePage = new PatientComposePage();
+  describe('Verify draft auto save', () => {
+    beforeEach(() => {
+      const site = new SecureMessagingSite();
+      const landingPage = new PatientInboxPage();
 
-        site.login();
-        landingPage.loadInboxMessages();
-        landingPage.navigateToComposePage();
-        landingPage.composeMessage();
-        cy.intercept(
-          'POST',
-          '/my_health/v1/messaging/message_drafts',
-          mockAutoSaveDraftResponse,
-        ).as('autoSave');
-        cy.intercept(
-          'PUT',
-          `/my_health/v1/messaging/message_drafts/${
-            mockAutoSaveDraftResponse.data.attributes.messageId
-          }`,
-          mockAutoSaveDraftResponse,
-        ).as('autoSaveDetailed');
-        cy.wait('@autoSave', { timeout: draftAutoSaveTimeout + 1000 }).then(
-          xhr => {
-            cy.log(JSON.stringify(xhr.response.body));
-            cy.get('[data-testid="message-subject-field"]')
-              .shadow()
-              .find('#inputField')
-              .type('testSubject2');
-            cy.wait('@autoSaveDetailed', {
-              timeout: draftAutoSaveTimeout + 1000,
-            });
+      site.login();
+      landingPage.loadInboxMessages();
+      landingPage.navigateToComposePage();
+      landingPage.composeMessage();
+      cy.intercept(
+        'POST',
+        '/my_health/v1/messaging/message_drafts',
+        mockAutoSaveDraftResponse,
+      ).as('autoSave');
+      cy.intercept(
+        'PUT',
+        `/my_health/v1/messaging/message_drafts/${
+          mockAutoSaveDraftResponse.data.attributes.messageId
+        }`,
+        mockAutoSaveDraftResponse,
+      ).as('autoSaveDetailed');
+      cy.wait('@autoSave', { timeout: draftAutoSaveTimeout + 1000 }).then(
+        xhr => {
+          cy.log(JSON.stringify(xhr.response.body));
+          cy.get('[data-testid="message-subject-field"]')
+            .shadow()
+            .find('#inputField')
+            .type('testSubject2');
+          cy.wait('@autoSaveDetailed', {
+            timeout: draftAutoSaveTimeout + 1000,
+          });
+        },
+      );
+    });
+    it('verify notification message', () => {
+      cy.get('.last-save-time').should('contain', 'Your message was saved');
+      cy.injectAxe();
+      cy.axeCheck(AXE_CONTEXT, {
+        rules: {
+          'aria-required-children': {
+            enabled: false,
           },
-        );
+        },
       });
-      it('verify notification message', () => {
-        cy.get('.last-save-time').should('contain', 'Your message was saved');
-        cy.injectAxe();
-        cy.axeCheck(AXE_CONTEXT, {
-          rules: {
-            'aria-required-children': {
-              enabled: false,
-            },
-          },
-        });
-        composePage.sendMessage();
-      });
+      composePage.sendMessage();
     });
   });
-}
+});
