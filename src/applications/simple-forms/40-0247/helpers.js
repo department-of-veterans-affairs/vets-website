c47ab28f482f49a4351c23585bcc13c0176b9fc2
Tze-chiu Lei <tze@adhocteam.us>
2023-10-13 09:57:15 -0700

    [DRAFT] 40-0247 - Add date-of-death custom-validation to Vet Personal Info page (#26168)
    * Add date-of-death custom-validation to 40-0247 Vet Pers Info page
    
    * Refactor 40-0247 data-of-death custom-validation

--- src/applications/simple-forms/40-0247/helpers.js
+++ src/applications/simple-forms/40-0247/helpers.js
@@ -1,5 +1,19 @@
+import moment from 'moment';
+
 export function getInitialData({ mockData, environment }) {
   return !!mockData && environment.isLocalhost() && !window.Cypress
     ? mockData
     : undefined;
+}
+
+export function dateOfDeathValidation(errors, fields) {
+  const { veteranDateOfBirth, veteranDateOfDeath } = fields;
+  const dob = moment(veteranDateOfBirth);
+  const dod = moment(veteranDateOfDeath);
+
+  if (dod.isBefore(dob)) {
+    errors.veteranDateOfDeath.addError(
+      'Provide a date that is after the date of birth',
+    );
+  }
 }
