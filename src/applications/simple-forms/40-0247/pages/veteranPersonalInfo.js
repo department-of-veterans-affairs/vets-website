9693c21a1ac7a033ddaf84db7014a274c95bc429
Tze-chiu Lei <tclei2009@gmail.com>
2023-10-12 19:44:06 -0700

    Add date-of-death custom-validation to 40-0247 Vet Pers Info page

--- src/applications/simple-forms/40-0247/pages/veteranPersonalInfo.js
+++ src/applications/simple-forms/40-0247/pages/veteranPersonalInfo.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import moment from 'moment';
+
 import {
   dateOfBirthUI,
   dateOfDeathUI,
@@ -20,6 +22,22 @@ export default {
     veteranFullName: fullNameNoSuffixUI(),
     veteranDateOfBirth: dateOfBirthUI(),
     veteranDateOfDeath: dateOfDeathUI(),
+    // can't move this validation to Forms-Library web-component-patterns
+    // since one pattern can't detect presence of the other
+    'ui:validations': [
+      // date of death should be after date of birth
+      (errors, fields) => {
+        const { veteranDateOfBirth, veteranDateOfDeath } = fields;
+        const dob = moment(veteranDateOfBirth);
+        const dod = moment(veteranDateOfDeath);
+
+        if (dod.isBefore(dob)) {
+          errors.veteranDateOfDeath.addError(
+            'Provide a date that is after the date of birth',
+          );
+        }
+      },
+    ],
   },
   schema: {
     type: 'object',
