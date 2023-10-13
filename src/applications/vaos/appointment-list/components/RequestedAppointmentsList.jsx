35d3404bd3bced1d97010f144655b12481fd2110
Ryan Shaw <ryan.shaw@adhocteam.us>
2023-10-13 13:52:35 -0700

    Revert "Fixes import related linting error"
    This reverts commit 30e8bb09a709fc750dcfe539ea5fbfe548c4986d.

--- src/applications/vaos/appointment-list/components/RequestedAppointmentsList.jsx
+++ src/applications/vaos/appointment-list/components/RequestedAppointmentsList.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import recordEvent from '@department-of-veterans-affairs/platform-monitoring';
+
+import recordEvent from 'platform/monitoring/record-event';
 import {
   fetchPendingAppointments,
   startNewAppointmentFlow,
