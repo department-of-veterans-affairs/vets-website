198e6e157f93c1bd0bccf461b9b4a8f7941d4cc5
Adam Antonioli <adam.antonioli@va.gov>
2023-10-16 12:35:54 -0400

    MBMS-47181 pre need form text changes, reordering confirmation screen info

--- src/applications/pre-need/components/Footer.jsx
+++ src/applications/pre-need/components/Footer.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import FormFooter from 'platform/forms/components/FormFooter';
 
 const Footer = ({ formConfig, currentLocation }) => (
   <div className="row">
     {currentLocation.pathname === '/introduction'}
-    <FormFooter formConfig={formConfig} currentLocation={currentLocation} />
+    <FormFooter formConfig={formConfig} />
   </div>
 );
+
+function FormFooter({ formConfig }) {
+  const GetFormHelp = formConfig.getHelp;
+
+  if (!GetFormHelp) {
+    return null;
+  }
+
+  return (
+    <div className="row">
+      <div className="usa-width-two-thirds medium-8 columns">
+        <div className="help-footer-box">
+          <h2 className="help-heading">Need help?</h2>
+          <div>
+            <GetFormHelp formConfig={formConfig} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Footer;
