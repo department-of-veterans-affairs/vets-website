412a899673e2aee3851d9466569f8064eaf93e46
Holden Hinkle <holden@holdenhinkle.com>
2023-10-16 21:29:56 -0400

    add Branch of Service checkbox options to 'Id Information' page

--- src/applications/representative/config/chapters/personal-information/pages/unauth-id-info.js
+++ src/applications/representative/config/chapters/personal-information/pages/unauth-id-info.js
@@ -2,6 +2,37 @@ import ssnUI from '@department-of-veterans-affairs/platform-forms-system/ssn';
 
 export const title = 'Veteran’s Identification Information';
 
+const branchOfServiceOptions = {
+  army: 'Army',
+  airForce: 'Air Force',
+  coastGuard: 'Coast Guard',
+  marineCorps: 'Marine Corps',
+  navy: 'Navy',
+  spaceForce: 'Space Force',
+  other: 'Other',
+};
+
+const branchOfServiceSchema = Object.keys(branchOfServiceOptions).reduce(
+  (accumulator, key) => {
+    accumulator[key] = { type: 'boolean', default: false };
+    return accumulator;
+  },
+  {},
+);
+
+const branchOfServiceUiSchema = Object.keys(branchOfServiceOptions).reduce(
+  (accumulator, key) => {
+    accumulator[key] = {
+      'ui:widget': 'checkbox',
+      'ui:options': {
+        hideLabelText: true,
+      },
+    };
+    return accumulator;
+  },
+  {},
+);
+
 export const schema = {
   type: 'object',
   title,
@@ -18,12 +49,20 @@ export const schema = {
       type: 'string',
       title: 'Service number',
     },
+    branchOfService: {
+      type: 'object',
+      properties: branchOfServiceSchema,
+    },
   },
-  required: ['ssn'],
+  required: ['ssn', 'branchOfService'],
 };
 
 export const uiSchema = {
   ssn: ssnUI,
   vaFileNumber: {},
   serviceNumber: {},
+  branchOfService: {
+    'ui:description': 'Branch of Service',
+    ...branchOfServiceUiSchema,
+  },
 };
