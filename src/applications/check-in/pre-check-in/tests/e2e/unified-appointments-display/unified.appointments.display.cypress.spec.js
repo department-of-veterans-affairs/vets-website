16838be130fc15051abfab247263a8a8a45ec3e2
John Woolschlager <wullaski@gmail.com>
2023-10-17 11:55:13 -0400

    Add cypress tests for pre check in

---
+++ src/applications/check-in/pre-check-in/tests/e2e/unified-appointments-display/unified.appointments.display.cypress.spec.js
@@ -0,0 +1,71 @@
+import '../../../../tests/e2e/commands';
+
+import ApiInitializer from '../../../../api/local-mock-api/e2e/ApiInitializer';
+import ValidateVeteran from '../../../../tests/e2e/pages/ValidateVeteran';
+import AppointmentsPage from '../../../../tests/e2e/pages/AppointmentsPage';
+import AppointmentDetails from '../../../../tests/e2e/pages/AppointmentDetails';
+
+describe('Check In Experience -- ', () => {
+  describe('Appointment display -- ', () => {
+    beforeEach(() => {
+      const {
+        initializeFeatureToggle,
+        initializeSessionGet,
+        initializeSessionPost,
+        initializePreCheckInDataGet,
+        initializeCheckInDataPost,
+        initializeDemographicsPatch,
+      } = ApiInitializer;
+      initializeFeatureToggle.withCurrentFeatures();
+      initializeSessionGet.withSuccessfulNewSession();
+      initializeSessionPost.withSuccess();
+      initializeDemographicsPatch.withSuccess();
+      initializePreCheckInDataGet.withSuccess();
+      initializeCheckInDataPost.withSuccess();
+
+      cy.visitPreCheckInWithUUID();
+      ValidateVeteran.validatePage.preCheckIn();
+    });
+    afterEach(() => {
+      cy.window().then(window => {
+        window.sessionStorage.clear();
+      });
+    });
+    it('Upcoming appointments are displayed in a sorted manner', () => {
+      ValidateVeteran.validateVeteran();
+      ValidateVeteran.attemptToGoToNextPage();
+      AppointmentsPage.validatePageLoaded();
+      AppointmentsPage.validateUpcomingAppointmentsList();
+      cy.injectAxeThenAxeCheck();
+    });
+
+    it('What to do next displays a pre check in card for valid appointments', () => {
+      ValidateVeteran.validateVeteran();
+      ValidateVeteran.attemptToGoToNextPage();
+      AppointmentsPage.validateWhatNextHeader();
+      AppointmentsPage.validateWhatNextCardTitle.preCheckIn();
+      AppointmentsPage.validateWhatNextCardDetailsLink();
+      AppointmentsPage.validateWhatNextCardActionLink.preCheckIn();
+      cy.injectAxeThenAxeCheck();
+    });
+
+    it('should navigate to appointment details', () => {
+      const multipleAppointments = [
+        { startTime: '2021-08-19T17:00:00' },
+        {
+          startTime: '2021-08-19T16:00:00',
+          eligibility: 'INELIGIBLE',
+        },
+        { startTime: '2021-08-19T15:00:00' },
+      ];
+      ApiInitializer.initializeCheckInDataGet.withSuccess({
+        appointments: multipleAppointments,
+      });
+      ValidateVeteran.validateVeteran();
+      ValidateVeteran.attemptToGoToNextPage();
+      AppointmentsPage.clickDetails();
+      AppointmentDetails.validatePageLoadedInPerson();
+      cy.injectAxeThenAxeCheck();
+    });
+  });
+});
