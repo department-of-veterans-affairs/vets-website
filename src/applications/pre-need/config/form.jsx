20a8d74bd9e71f7244b1ea277f4357aa69d791a4
Adam Antonioli <adam.antonioli@va.gov>
2023-10-18 11:17:54 -0400

    MBMS-47183 add prod flag for pre need sponsor contact mailing address page field labels text change

--- src/applications/pre-need/config/form.jsx
+++ src/applications/pre-need/config/form.jsx
@@ -631,26 +631,37 @@ const formConfig = {
               uiSchema: {
                 application: {
                   veteran: {
-                    address: merge(
-                      {},
-                      address.uiSchema('Sponsor’s mailing address'),
-                      {
-                        street: {
-                          'ui:title': 'Street address',
-                        },
-                        street2: {
-                          'ui:title': 'Street address line 2',
-                        },
-                        state: {
-                          'ui:title': sponsorMailingAddressStateTitleWrapper,
-                          'ui:options': {
-                            hideIf: formData =>
-                              !sponsorMailingAddressHasState(formData) &&
-                              !environment.isProduction(),
+                    address: !environment.isProduction()
+                      ? merge(
+                          {},
+                          address.uiSchema('Sponsor’s mailing address'),
+                          {
+                            street: {
+                              'ui:title': 'Street address',
+                            },
+                            street2: {
+                              'ui:title': 'Street address line 2',
+                            },
+                            state: {
+                              'ui:title': sponsorMailingAddressStateTitleWrapper,
+                              'ui:options': {
+                                hideIf: formData =>
+                                  !sponsorMailingAddressHasState(formData) &&
+                                  !environment.isProduction(),
+                              },
+                            },
                           },
-                        },
-                      },
-                    ),
+                        )
+                      : merge({}, address.uiSchema('Sponsor’s address'), {
+                          state: {
+                            'ui:title': sponsorMailingAddressStateTitleWrapper,
+                            'ui:options': {
+                              hideIf: formData =>
+                                !sponsorMailingAddressHasState(formData) &&
+                                !environment.isProduction(),
+                            },
+                          },
+                        }),
                   },
                 },
               },
