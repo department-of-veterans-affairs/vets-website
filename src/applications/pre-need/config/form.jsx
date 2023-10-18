bea3298679ac38afab9b4e40066bd50233a00cbd
Adam Antonioli <adam.antonioli@va.gov>
2023-10-18 11:02:34 -0400

    MBMS-47183 change pre need sponsor contact mailing address page field labels text

--- src/applications/pre-need/config/form.jsx
+++ src/applications/pre-need/config/form.jsx
@@ -631,16 +631,26 @@ const formConfig = {
               uiSchema: {
                 application: {
                   veteran: {
-                    address: merge({}, address.uiSchema('Sponsor’s address'), {
-                      state: {
-                        'ui:title': sponsorMailingAddressStateTitleWrapper,
-                        'ui:options': {
-                          hideIf: formData =>
-                            !sponsorMailingAddressHasState(formData) &&
-                            !environment.isProduction(),
+                    address: merge(
+                      {},
+                      address.uiSchema('Sponsor’s mailing address'),
+                      {
+                        street: {
+                          'ui:title': 'Street address',
+                        },
+                        street2: {
+                          'ui:title': 'Street address line 2',
+                        },
+                        state: {
+                          'ui:title': sponsorMailingAddressStateTitleWrapper,
+                          'ui:options': {
+                            hideIf: formData =>
+                              !sponsorMailingAddressHasState(formData) &&
+                              !environment.isProduction(),
+                          },
                         },
                       },
-                    }),
+                    ),
                   },
                 },
               },
