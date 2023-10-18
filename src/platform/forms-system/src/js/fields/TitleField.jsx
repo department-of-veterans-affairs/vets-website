273df4f15a4afb6836cc255683ff6aa65930b793
Tze-chiu Lei <tclei2009@gmail.com>
2023-10-17 18:32:14 -0700

    Refactor TitleField to conditionally render title Render strings as-is, but change element's <h3> to <h4> on Review page

--- src/platform/forms-system/src/js/fields/TitleField.jsx
+++ src/platform/forms-system/src/js/fields/TitleField.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
+
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-export default function TitleField({ id, title }) {
+export default function TitleField({ id, title, formContext }) {
   const isRoot = id === 'root__title';
-
-  const classes = classNames('schemaform-block-title', {
+  const classes = classNames('schemaform-block-title testing', {
     'schemaform-block-subtitle': !isRoot,
   });
+  const { onReviewPage } = formContext;
+  let reviewTitle;
 
-  const isEmptyTitle = typeof title === 'string' && title.trim() === '';
+  const isStringTitle = typeof title === 'string';
+  const isEmptyStringTitle = isStringTitle && title.trim() === '';
 
-  return isEmptyTitle ? null : (
+  // Handle title scenarios:
+  // If title is a React element with an <h3>,
+  // it should be made <h4> for the Review page.
+  if (isStringTitle) {
+    reviewTitle = !isEmptyStringTitle ? title : null;
+  } else if (!onReviewPage) {
+    reviewTitle = title;
+  } else if (title.type !== 'h3') {
+    reviewTitle = title;
+  } else {
+    reviewTitle = { ...title, type: 'h4' };
+  }
+
+  return (
     <legend className={classes} id={id}>
-      {title}
+      {reviewTitle}
     </legend>
   );
 }
+
+TitleField.defaultProps = {
+  formContext: {
+    onReviewPage: false,
+  },
+};
+
+TitleField.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
+  formContext: PropTypes.shape({
+    onReviewPage: PropTypes.bool,
+  }),
+};
--- src/platform/forms-system/test/js/fields/TitleField.unit.spec.jsx
+++ src/platform/forms-system/test/js/fields/TitleField.unit.spec.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+
 import { expect } from 'chai';
 import SkinDeep from 'skin-deep';
+import { render } from '@testing-library/react';
 
 import TitleField from '../../../src/js/fields/TitleField';
 
@@ -21,5 +23,58 @@ describe('Schemaform <TitleField>', () => {
   it('should not render anything if no title is provided', () => {
     const tree = SkinDeep.shallowRender(<TitleField id="root__title" />);
     expect(tree.subTree('TitleField')).to.be.false;
+  });
+
+  it('should render string title as-is', () => {
+    const titleOnFormPage = render(
+      <TitleField
+        id="root__title"
+        title="foo"
+        formContext={{ onReviewPage: false }}
+      />,
+    );
+    const titleOnReviewPage = render(
+      <TitleField
+        id="root__title"
+        title="foo"
+        formContext={{ onReviewPage: true }}
+      />,
+    );
+
+    expect(titleOnFormPage.findByText('foo', { selector: 'legend' })).to.exist;
+    expect(titleOnReviewPage.findByText('foo', { selector: 'legend' })).to
+      .exist;
+  });
+
+  it('should render React-element title as-is on form page', async () => {
+    const Foo = <div>Foo</div>;
+    const titleOnFormPage = await render(
+      <TitleField
+        id="root__title"
+        title={Foo}
+        formContext={{ onReviewPage: false }}
+      />,
+    );
+
+    expect(titleOnFormPage.findByText('Foo', { selector: 'legend > div' })).to
+      .exist;
+  });
+
+  it('should render React-element title with h4 on review page', async () => {
+    const Foo = <h3>Foo</h3>;
+    const titleOnReviewPage = await render(
+      <TitleField
+        id="root__title"
+        title={Foo}
+        f
+        ormContext={{ onReviewPage: true }}
+      />,
+    );
+
+    expect(
+      titleOnReviewPage.findByText('Foo', {
+        selector: 'legend > h4',
+      }),
+    ).to.exist;
   });
 });
