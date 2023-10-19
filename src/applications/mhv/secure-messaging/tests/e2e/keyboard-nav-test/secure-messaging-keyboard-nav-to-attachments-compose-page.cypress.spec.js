daf48f09856dc3fff90050e51d6e6ca6e8895337
raji.venkatesh <114511630+RajiVenkatesh18@users.noreply.github.com>
2023-10-19 10:13:23 -0500

    updates to inbox folder test
    removed the for loop

--- src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-attachments-compose-page.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-keyboard-nav-to-attachments-compose-page.cypress.spec.js
@@ -4,38 +4,36 @@ import PatientComposePage from '../pages/PatientComposePage';
 import { AXE_CONTEXT } from '../utils/constants';
 import requestBody from '../fixtures/message-compose-request-body.json';
 
-for (let i = 0; i < 20; i += 1) {
-  describe('Secure Messaging Keyboard Nav to Attachment', () => {
-    it('Keyboard Nav to Focus on Attachment', () => {
-      const landingPage = new PatientInboxPage();
-      const composePage = new PatientComposePage();
-      const site = new SecureMessagingSite();
-      site.login();
-      landingPage.loadInboxMessages();
-      landingPage.navigateToComposePage();
-      composePage.selectRecipient(requestBody.recipientId);
-      composePage.selectCategory(
-        `${requestBody.category}${requestBody.category}input`,
-      );
-      // cy.tabToElement('#OTHEROTHERinput');
-      // cy.realPress(['Enter']);
-      composePage
-        .getMessageSubjectField()
-        .type(`${requestBody.subject}`, { force: true });
-      composePage
-        .getMessageBodyField()
-        .type(`${requestBody.body}`, { force: true });
-      composePage.attachMessageFromFile('test_image.jpg');
-      composePage.verifyFocusonMessageAttachment();
-      cy.injectAxe();
-      cy.axeCheck(AXE_CONTEXT, {
-        rules: {
-          'aria-required-children': {
-            enabled: false,
-          },
+describe('Secure Messaging Keyboard Nav to Attachment', () => {
+  it('Keyboard Nav to Focus on Attachment', () => {
+    const landingPage = new PatientInboxPage();
+    const composePage = new PatientComposePage();
+    const site = new SecureMessagingSite();
+    site.login();
+    landingPage.loadInboxMessages();
+    landingPage.navigateToComposePage();
+    composePage.selectRecipient(requestBody.recipientId);
+    composePage.selectCategory(
+      `${requestBody.category}${requestBody.category}input`,
+    );
+    // cy.tabToElement('#OTHEROTHERinput');
+    // cy.realPress(['Enter']);
+    composePage
+      .getMessageSubjectField()
+      .type(`${requestBody.subject}`, { force: true });
+    composePage
+      .getMessageBodyField()
+      .type(`${requestBody.body}`, { force: true });
+    composePage.attachMessageFromFile('test_image.jpg');
+    composePage.verifyFocusonMessageAttachment();
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
         },
-      });
-      composePage.sendMessage();
+      },
     });
+    composePage.sendMessage();
   });
-}
+});
