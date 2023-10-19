29bb211ba08044eb99d4bf159db55c6ac48abb47
Tze-chiu Lei <tclei2009@gmail.com>
2023-10-18 19:48:55 -0700

    Refactor TitleField Modify title React-fragment manipulation code to handle nested/multiple children

--- src/platform/forms-system/src/js/fields/TitleField.jsx
+++ src/platform/forms-system/src/js/fields/TitleField.jsx
@@ -13,6 +13,36 @@ export default function TitleField({ id, title, formContext }) {
 
   const isStringTitle = typeof title === 'string';
   const isEmptyStringTitle = isStringTitle && title.trim() === '';
+  const getReviewTitle = element => {
+    // returns a conditionally-modify clone of title React-element,
+    // with any header tags lowered by one level for Review page.
+    if (Array.isArray(element)) {
+      return element.map((child, index) => (
+        <React.Fragment key={index}>{getReviewTitle(child)}</React.Fragment>
+      ));
+      // eslint-disable-next-line no-else-return
+    } else if (typeof element === 'object' && element !== null) {
+      const { type, props } = element;
+      const newProps = { ...props };
+      let newType;
+
+      if (typeof type === 'string') {
+        if (type.match(/^h[1-5]$/)) {
+          newType = `h${parseInt(type.charAt(1), 10) + 1}`;
+        } else if (type === 'h6') {
+          newType = 'p';
+        } else {
+          newType = type;
+        }
+      } else {
+        newType = type;
+      }
+      const newChildren = getReviewTitle(props.children);
+      newProps.children = newChildren;
+      return React.createElement(newType, newProps);
+    }
+    return element;
+  };
 
   // Handle title scenarios:
   // If title is a React element with an <h3>,
@@ -21,10 +51,10 @@ export default function TitleField({ id, title, formContext }) {
     reviewTitle = !isEmptyStringTitle ? title : null;
   } else if (!onReviewPage) {
     reviewTitle = title;
-  } else if (title.type !== 'h3') {
-    reviewTitle = title;
   } else {
-    reviewTitle = { ...title, type: 'h4' };
+    // title is a React fragment and on Review page, so
+    // any header tags should be lowered by one level.
+    reviewTitle = getReviewTitle(title);
   }
 
   return (
