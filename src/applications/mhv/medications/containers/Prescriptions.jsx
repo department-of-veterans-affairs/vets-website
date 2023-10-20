0a76410ccfc9b3f137ba241a2e2cd3783532d678
raji.venkatesh <114511630+RajiVenkatesh18@users.noreply.github.com>
2023-10-20 11:06:20 -0500

    validate empty medications alert message
    Added scripts to verify the alert message for empty medications list on medications landing page

--- src/applications/mhv/medications/containers/Prescriptions.jsx
+++ src/applications/mhv/medications/containers/Prescriptions.jsx
@@ -83,9 +83,9 @@ const Prescriptions = () => {
           </va-alert>
         )}
         {paginatedPrescriptionsList?.length <= 0 && (
-          <va-alert status="info" uswds>
+          <va-alert status="info" data-testid="empty-list-alert" uswds>
             <div>
-              <h4 className="vads-u-margin-top--0">
+              <h4 className="vads-u-margin-top--0" data-testid="alert-message">
                 You don’t have any medications in your medications list
               </h4>
               <strong>Note</strong>: This list doesn’t include older
--- src/applications/mhv/medications/tests/e2e/pages/MedicationsLandingPage.js
+++ src/applications/mhv/medications/tests/e2e/pages/MedicationsLandingPage.js
@@ -68,5 +68,12 @@ class MedicationsLandingPage {
       .should('be.visible')
       .and('contain', 'About medications');
   };
+
+  verifyEmptyMedicationsListMessageAlertOnLandingPage = () => {
+    cy.get('[data-testid="empty-list-alert"] >div ').should(
+      'have.text',
+      'You don’t have any medications in your medications listNote: This list doesn’t include older prescriptions that have been inactive for more than 180 days. To find these older prescriptions, go to your VA Blue Button report on the My HealtheVet website. Go to VA Blue Button® on the My HealtheVet website',
+    );
+  };
 }
 export default MedicationsLandingPage;
