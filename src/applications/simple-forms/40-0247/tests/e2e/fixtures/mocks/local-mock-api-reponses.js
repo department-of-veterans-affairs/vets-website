fb4d20724f80c4816eb11c625c05b4aff2d5a971
Tze-chiu Lei <tclei2009@gmail.com>
2023-10-20 18:14:23 -0700

    Update 40-0247 local-mock-api Change file-upload URL

--- src/applications/simple-forms/40-0247/tests/e2e/fixtures/mocks/local-mock-api-reponses.js
+++ src/applications/simple-forms/40-0247/tests/e2e/fixtures/mocks/local-mock-api-reponses.js
@@ -6,7 +6,7 @@ const mockSubmission = require('./submission.json');
 
 const responses = {
   ...commonResponses,
-  'POST /v0/simple-forms/40-0247/files': mockUpload,
+  'POST /simple_forms_api/v1/simple_forms/submit_supporting_documents': mockUpload,
   'POST /simple_forms_api/v1/simple_forms': mockSubmission,
 };
 
