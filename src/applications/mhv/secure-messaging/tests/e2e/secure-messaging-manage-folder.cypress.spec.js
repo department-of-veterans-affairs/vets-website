5df764eaa2ebfef3c5f912f1d267fee8305aeb76
fazilqa <fazil.mammadov@bylight.com>
2023-10-20 17:00:39 -0700

    remove cycle from manage-folder test

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-manage-folder.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-manage-folder.cypress.spec.js
@@ -6,67 +6,64 @@ import mockFolders from './fixtures/generalResponses/folders.json';
 import PatientMessageCustomFolderPage from './pages/PatientMessageCustomFolderPage';
 import { AXE_CONTEXT } from './utils/constants';
 
-for (let i = 0; i < 200; i += 1) {
-  describe('manage folders', () => {
-    describe('custom folder created message', () => {
-      const folderPage = new FolderManagementPage();
-      const landingPage = new PatientInboxPage();
-      const site = new SecureMessagingSite();
-      const newFolder = `folder${Date.now()}`;
+describe('manage folders', () => {
+  describe('custom folder created message', () => {
+    const folderPage = new FolderManagementPage();
+    const landingPage = new PatientInboxPage();
+    const site = new SecureMessagingSite();
+    const newFolder = `folder${Date.now()}`;
 
-      before(() => {
-        site.login();
-        landingPage.loadInboxMessages();
-        PatientMessageCustomFolderPage.loadFoldersList();
-      });
-
-      it('verify folder created', () => {
-        cy.injectAxe();
-        cy.axeCheck(AXE_CONTEXT, {
-          rules: {
-            'aria-required-children': {
-              enabled: false,
-            },
-          },
-        });
-
-        PatientMessageCustomFolderPage.createCustomFolder(newFolder);
-
-        folderPage.verifyCreateFolderSuccessMessage();
-      });
+    before(() => {
+      site.login();
+      landingPage.loadInboxMessages();
+      PatientMessageCustomFolderPage.loadFoldersList();
     });
 
-    describe('custom folder deleted message', () => {
-      const folderPage = new FolderManagementPage();
-      const landingPage = new PatientInboxPage();
-      const site = new SecureMessagingSite();
-      const folderName = createdFolderResponse.data.attributes.name;
-      const { folderId } = createdFolderResponse.data.attributes;
-
-      before(() => {
-        site.login();
-        landingPage.loadInboxMessages();
-        PatientMessageCustomFolderPage.loadFoldersList(mockFolders);
-      });
-
-      it('verify folder deleted', () => {
-        cy.injectAxe();
-        cy.axeCheck(AXE_CONTEXT, {
-          rules: {
-            'aria-required-children': {
-              enabled: false,
-            },
+    it('verify folder created', () => {
+      cy.injectAxe();
+      cy.axeCheck(AXE_CONTEXT, {
+        rules: {
+          'aria-required-children': {
+            enabled: false,
           },
-        });
-
-        PatientMessageCustomFolderPage.loadSingleFolderWithNoMessages(
-          folderId,
-          folderName,
-        );
-        mockFolders.data.pop(createdFolderResponse.data);
-        folderPage.deleteFolder(folderId);
-        folderPage.verifyDeleteSuccessMessage();
+        },
       });
+
+      PatientMessageCustomFolderPage.createCustomFolder(newFolder);
+
+      folderPage.verifyCreateFolderSuccessMessage();
     });
   });
-}
+
+  describe('custom folder deleted message', () => {
+    const folderPage = new FolderManagementPage();
+    const landingPage = new PatientInboxPage();
+    const site = new SecureMessagingSite();
+    const folderName = createdFolderResponse.data.attributes.name;
+    const { folderId } = createdFolderResponse.data.attributes;
+
+    before(() => {
+      site.login();
+      landingPage.loadInboxMessages();
+      PatientMessageCustomFolderPage.loadFoldersList(mockFolders);
+    });
+
+    it('verify folder deleted', () => {
+      cy.injectAxe();
+      cy.axeCheck(AXE_CONTEXT, {
+        rules: {
+          'aria-required-children': {
+            enabled: false,
+          },
+        },
+      });
+
+      PatientMessageCustomFolderPage.loadSingleFolderWithNoMessages(
+        folderId,
+        folderName,
+      );
+      folderPage.deleteFolder(folderId);
+      folderPage.verifyDeleteSuccessMessage();
+    });
+  });
+});
