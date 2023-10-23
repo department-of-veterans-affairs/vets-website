59dc3cadf5a67364ba930767ad4da43d3ac14f2e
“Anu <anu@themostudio.com>
2023-10-23 13:52:47 -0700

    additional files

---
+++ src/applications/ipp/containers/LocationSelector.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default function LocationSelector({ onPageChange }) {
+  return (
+    <body className="page">
+      <div className="container">
+        <div className="white-box">
+          <br />
+          <h1>Choose a nearby site</h1>
+          <div className="text">
+            <p>You have chosen to verify at the following location:</p>
+            <p>
+              Baltimore VA Medical Center
+              <br />
+              10 Greene Street
+              <br />
+              Baltimore, MD 21201-1524
+            </p>
+            <p>
+              Please confirm that this is a location you will be able to visit
+              in the next 10 days before continuing.
+            </p>
+            <div className="wrapper">
+              <button
+                type="button"
+                className="usa-button mo-full-width-btn"
+                onClick={() => onPageChange(3)}
+              >
+                Continue
+              </button>
+            </div>
+            <div className="wrapper">
+              <button
+                type="button"
+                className="usa-button usa-button--outline mo-full-width-btn"
+                onClick={() => onPageChange(1)}
+              >
+                Back
+              </button>
+            </div>
+            <br />
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+}
