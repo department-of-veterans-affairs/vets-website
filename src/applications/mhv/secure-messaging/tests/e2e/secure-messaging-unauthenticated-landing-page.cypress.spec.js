5e43d954a22e0708cace530d529b6c23bec734a0
fazilqa <fazil.mammadov@bylight.com>
2023-10-23 15:20:59 -0700

    fix unauthenticated-landing-page

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-unauthenticated-landing-page.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-unauthenticated-landing-page.cypress.spec.js
@@ -2,27 +2,29 @@ import SecureMessagingSite from './sm_site/SecureMessagingSite';
 import PatientInboxPage from './pages/PatientInboxPage';
 import { AXE_CONTEXT } from './utils/constants';
 
-describe.skip('Secure Messaging Compose', () => {
-  it.skip('can send message', () => {
-    const site = new SecureMessagingSite();
-    const patientInboxPage = new PatientInboxPage();
-    site.login(false);
-    site.loadPageUnauthenticated();
+for (let i = 0; i < 200; i += 1) {
+  describe('Secure Messaging Compose', () => {
+    it('can send message', () => {
+      const site = new SecureMessagingSite();
+      const patientInboxPage = new PatientInboxPage();
+      site.login(false);
+      site.loadPageUnauthenticated();
 
-    cy.url().should('contain', '/health-care/secure-messaging');
+      cy.url().should('contain', '/my-health/secure-messages');
 
-    site.login();
+      site.login();
 
-    patientInboxPage.loadInboxMessages();
-    cy.get('[data-testid="inbox-sidebar"] > a').click();
+      patientInboxPage.loadInboxMessages();
+      cy.get('[data-testid="inbox-sidebar"] > a').click();
 
-    cy.injectAxe();
-    cy.axeCheck(AXE_CONTEXT, {
-      rules: {
-        'aria-required-children': {
-          enabled: false,
+      cy.injectAxe();
+      cy.axeCheck(AXE_CONTEXT, {
+        rules: {
+          'aria-required-children': {
+            enabled: false,
+          },
         },
-      },
+      });
     });
   });
-});
+}
