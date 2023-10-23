89cfa4494811d055e58b45b7377c491ca70ae331
Carlos Felix <cfelix@agile6.com>
2023-10-23 12:09:26 -0400

    Fixed per review comments.

--- src/applications/mhv/landing-page/components/NoHealthAlert.jsx
+++ src/applications/mhv/landing-page/components/NoHealthAlert.jsx
@@ -12,9 +12,9 @@ const NoHealthAlert = () => {
       <div>
         <p className="vads-u-margin-y--0">
           Only patients who have received care at a VA facility can use VA.gov
-          health tools. If you’ve received care at a VA medical center, clinic,
-          or Vet center, please call that facility to find out if you’re in
-          their records.
+          health tools. If you believe you have registered or received care at a
+          VA medical center, clinic, or Vet center, please call that facility to
+          find out if you’re in their records.
         </p>
         <p className="vads-u-margin-bottom--0">
           <a href="/find-locations">Find a facility phone number</a>
