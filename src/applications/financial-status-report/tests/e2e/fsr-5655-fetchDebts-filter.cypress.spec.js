d818c3c7375c78c7068a8be9b4593a6050ac543f
Andrew Rodiek <andrew.rodiek@gmail.com>
2023-10-23 16:07:02 -0400

    updating additional flaky ttest

--- src/applications/financial-status-report/tests/e2e/fsr-5655-fetchDebts-filter.cypress.spec.js
+++ src/applications/financial-status-report/tests/e2e/fsr-5655-fetchDebts-filter.cypress.spec.js
@@ -3,7 +3,9 @@ import manifest from '../../manifest.json';
 import mockUser from './fixtures/mocks/mockUser.json';
 import { WIZARD_STATUS } from '../../wizard/constants';
 
-describe('Fetch Debts Successfully and Filter Out Invalid Debt', () => {
+describe(`Fetch Debts Successfully and Filter Out Invalid Debt`, () => {
+  Cypress.config({ requestTimeout: 10000 });
+
   before(() => {
     cy.intercept('GET', '/v0/feature_toggles*', {
       data: {
@@ -12,10 +14,7 @@ describe('Fetch Debts Successfully and Filter Out Invalid Debt', () => {
           { name: 'show_financial_status_report', value: true },
         ],
       },
-    });
-
-    cy.intercept('GET', '/v0/maintenance_windows', []);
-    cy.login(mockUser);
+    }).as('features');
 
     cy.intercept('GET', '/v0/debts*', {
       hasDependentDebts: false,
@@ -124,27 +123,25 @@ describe('Fetch Debts Successfully and Filter Out Invalid Debt', () => {
 
     sessionStorage.setItem(WIZARD_STATUS, WIZARD_STATUS_COMPLETE);
 
+    cy.login(mockUser);
+    cy.intercept('GET', '/v0/user?*', mockUser);
+    cy.intercept('GET', '/v0/maintenance_windows', []);
     cy.visit(manifest.rootUrl);
-  });
-  beforeEach(() => {
-    cy.window().then(win => {
-      win.sessionStorage.clear();
-    });
-  });
-  afterEach(() => {
-    cy.window().then(window => {
-      window.sessionStorage.clear();
-    });
+    cy.wait('@features');
   });
 
   it('Successful API Response', () => {
     cy.get('a.vads-c-action-link--green')
       .first()
-      .click();
+      .click({ waitForAnimations: true });
 
+    cy.location('pathname').should(
+      'eq',
+      `/manage-va-debt/request-debt-help-form-5655/veteran-information`,
+    );
     cy.findAllByText(/continue/i, { selector: 'button' })
       .first()
-      .click();
+      .click({ waitForAnimations: true });
 
     cy.get('[data-testid="debt-selection-checkbox"]').should('have.length', 2);
 
