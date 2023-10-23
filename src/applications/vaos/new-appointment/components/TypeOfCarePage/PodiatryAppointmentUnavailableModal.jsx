abb10597e464c68a9feace8fa774f51c1dc8930b
Tony Williams <tony.williams@va.gov>
2023-10-23 13:44:40 -0500

    fix 508 error

--- src/applications/vaos/new-appointment/components/TypeOfCarePage/PodiatryAppointmentUnavailableModal.jsx
+++ src/applications/vaos/new-appointment/components/TypeOfCarePage/PodiatryAppointmentUnavailableModal.jsx
@@ -16,6 +16,7 @@ const PodiatryAppointmentUnavailableModal = ({ onClose, showModal }) => {
       visible
       onCloseEvent={onClose}
       modalTitle={title}
+      aria-label={title}
       role="alertdialog"
     >
       You’re not eligible to request a community care Podiatry appointment
