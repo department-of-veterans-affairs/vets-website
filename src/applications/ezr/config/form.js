dac3f12649caf751e700dfabc2325a6f851c9c2d
Matt Long <longmd@me.com>
2023-10-24 13:43:08 -0400

    Populate Household Information form pages for 10-10EZR (#26297)

--- src/applications/ezr/config/form.js
+++ src/applications/ezr/config/form.js
@@ -7,6 +7,7 @@ import { externalServices } from 'platform/monitoring/DowntimeNotification';
 import manifest from '../manifest.json';
 import content from '../locales/en/content.json';
 import { SHARED_PATHS } from '../utils/constants';
+import { includeSpousalInformation } from '../utils/helpers/household';
 import { prefillTransformer } from '../utils/helpers/prefill-transformer';
 import { submitTransformer } from '../utils/helpers/submit-transformer';
 import IntroductionPage from '../containers/IntroductionPage';
@@ -25,6 +26,20 @@ import veteranMailingAddress from './chapters/veteranInformation/mailingAddress'
 import veteranHomeAddress from './chapters/veteranInformation/homeAddress';
 import veteranContantInformation from './chapters/veteranInformation/contactInformation';
 
+// chapter 2 - Household Information
+import maritalStatus from './chapters/householdInformation/maritalStatus';
+import spousePersonalInformation from './chapters/householdInformation/spousePersonalInformation';
+import spouseAdditionalInformation from './chapters/householdInformation/spouseAdditionalInformation';
+import spouseFinancialSupport from './chapters/householdInformation/spouseFinancialSupport';
+import spouseContactInformation from './chapters/householdInformation/spouseContactInformation';
+import dependentSummary from './chapters/householdInformation/dependentSummary';
+import veteranAnnualIncome from './chapters/householdInformation/veteranAnnualIncome';
+import spouseAnnualIncome from './chapters/householdInformation/spouseAnnualIncome';
+import deductibleExpenses from './chapters/householdInformation/deductibleExpenses';
+import DependentSummaryPage from '../components/FormPages/DependentSummary';
+import DependentInformationPage from '../components/FormPages/DependentInformation';
+import DependentsReviewPage from '../components/FormReview/DependentsReviewPage';
+
 // chapter 3 - Insurance Information
 import medicaidEligibility from './chapters/insuranceInformation/medicaid';
 import medicarePartAEnrollment from './chapters/insuranceInformation/medicare';
@@ -35,7 +50,10 @@ import InsurancePolicyInformationPage from '../components/FormPages/InsurancePol
 import InsurancePolicyReviewPage from '../components/FormReview/InsurancePolicyReviewPage';
 
 // declare shared paths for custom form page navigation
-const { insurance: INSURANCE_PATHS } = SHARED_PATHS;
+const {
+  insurance: INSURANCE_PATHS,
+  dependents: DEPENDENT_PATHS,
+} = SHARED_PATHS;
 
 // declare form config object
 const formConfig = {
@@ -142,6 +160,90 @@ const formConfig = {
           initialData: {},
           uiSchema: veteranContantInformation.uiSchema,
           schema: veteranContantInformation.schema,
+        },
+      },
+    },
+    householdInformation: {
+      title: 'Household financial information',
+      pages: {
+        maritalStatus: {
+          path: 'household-information/marital-status',
+          title: 'Marital status',
+          initialData: {},
+          uiSchema: maritalStatus.uiSchema,
+          schema: maritalStatus.schema,
+        },
+        spousePersonalInformation: {
+          path: 'household-information/spouse-personal-information',
+          title: 'Spouse\u2019s personal information',
+          initialData: {},
+          depends: formData => includeSpousalInformation(formData),
+          uiSchema: spousePersonalInformation.uiSchema,
+          schema: spousePersonalInformation.schema,
+        },
+        spouseAdditionalInformation: {
+          path: 'household-information/spouse-additional-information',
+          title: 'Spouse\u2019s additional information',
+          initialData: {},
+          depends: formData => includeSpousalInformation(formData),
+          uiSchema: spouseAdditionalInformation.uiSchema,
+          schema: spouseAdditionalInformation.schema,
+        },
+        spouseFinancialSupport: {
+          path: 'household-information/spouse-financial-support',
+          title: 'Spouse\u2019s financial support',
+          depends: formData =>
+            includeSpousalInformation(formData) && !formData.cohabitedLastYear,
+          uiSchema: spouseFinancialSupport.uiSchema,
+          schema: spouseFinancialSupport.schema,
+        },
+        spouseContactInformation: {
+          path: 'household-information/spouse-contact-information',
+          title: 'Spouse\u2019s address and phone number',
+          initialData: {},
+          depends: formData =>
+            includeSpousalInformation(formData) && !formData.sameAddress,
+          uiSchema: spouseContactInformation.uiSchema,
+          schema: spouseContactInformation.schema,
+        },
+        dependentSummary: {
+          path: DEPENDENT_PATHS.summary,
+          title: 'Dependents',
+          CustomPage: DependentSummaryPage,
+          CustomPageReview: DependentsReviewPage,
+          uiSchema: dependentSummary.uiSchema,
+          schema: dependentSummary.schema,
+        },
+        dependentInformation: {
+          path: DEPENDENT_PATHS.info,
+          title: 'Dependent information',
+          depends: formData => !formData['view:skipDependentInfo'],
+          CustomPage: DependentInformationPage,
+          CustomPageReview: null,
+          uiSchema: {},
+          schema: { type: 'object', properties: {} },
+        },
+        veteranAnnualIncome: {
+          path: 'household-information/veteran-annual-income',
+          title: 'Your annual income',
+          initialData: {},
+          uiSchema: veteranAnnualIncome.uiSchema,
+          schema: veteranAnnualIncome.schema,
+        },
+        spouseAnnualIncome: {
+          path: 'household-information/spouse-annual-income',
+          title: 'Spouse\u2019s annual income',
+          initialData: {},
+          depends: formData => includeSpousalInformation(formData),
+          uiSchema: spouseAnnualIncome.uiSchema,
+          schema: spouseAnnualIncome.schema,
+        },
+        deductibleExpenses: {
+          path: 'household-information/deductible-expenses',
+          title: 'Deductible expenses',
+          initialData: {},
+          uiSchema: deductibleExpenses.uiSchema,
+          schema: deductibleExpenses.schema,
         },
       },
     },
