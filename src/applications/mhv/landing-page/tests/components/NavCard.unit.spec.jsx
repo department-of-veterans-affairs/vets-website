91fc78a5d06fe7c7c3b8d0cdbbd53461609f9e0d
Patrick Bateman <batemapf@gmail.com>
2023-10-24 15:57:55 -0400

    update/fix unit tests

--- src/applications/mhv/landing-page/tests/components/NavCard.unit.spec.jsx
+++ src/applications/mhv/landing-page/tests/components/NavCard.unit.spec.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import { expect } from 'chai';
 import { render } from '@testing-library/react';
-import { resolveLandingPageLinks } from '../../utilities/data';
+import {
+  resolveLandingPageLinks,
+  resolveUnreadMessageAriaLabel,
+} from '../../utilities/data';
 import NavCard from '../../components/NavCard';
 
 describe('unread message indicator', () => {
-  function renderCards(unreadMessageCount) {
+  function renderCards({ unreadMessageCount }) {
+    const unreadMessageAriaLabel = resolveUnreadMessageAriaLabel(
+      unreadMessageCount,
+    );
     const { cards } = resolveLandingPageLinks(
       undefined,
       { featureToggles: {} },
       unreadMessageCount,
+      unreadMessageAriaLabel,
     );
 
     return render(
       cards.map(c => (
-        <NavCard key={c.title} icon={c.icon} title={c.title} links={c.links} />
+        <NavCard
+          key={c.title}
+          icon={c.icon}
+          title={c.title}
+          links={c.links}
+          aria-label={c.ariaLabel}
+        />
       )),
     );
   }
@@ -23,7 +36,9 @@ describe('unread message indicator', () => {
 
   it('includes unread messages message when greater than 0', () => {
     const unreadMessageCount = 4;
-    const { getByText } = renderCards(unreadMessageCount);
+    const { getByText } = renderCards({
+      unreadMessageCount,
+    });
 
     const inboxSpan = getByText('Inbox');
     const message = inboxSpan.parentNode.getAttribute('aria-label');
@@ -34,16 +49,24 @@ describe('unread message indicator', () => {
 
   it('does not include unread messages message when message count is 0', () => {
     const unreadMessageCount = 0;
-    const { queryByRole } = renderCards(unreadMessageCount);
-
+    const { queryByRole, getByText } = renderCards({
+      unreadMessageCount,
+    });
     const indicator = queryByRole('status');
 
     expect(indicator).not.to.exist;
+
+    const inboxSpan = getByText('Inbox');
+    const message = inboxSpan.parentNode.getAttribute('aria-label');
+
+    expect(message).to.not.exist;
   });
 
   it('renders if message count is undefined', () => {
     const unreadMessageCount = undefined;
-    const { getByRole } = renderCards(unreadMessageCount);
+    const { getByRole } = renderCards({
+      unreadMessageCount,
+    });
 
     const link = getByRole('link', { name: /inbox/i });
 
@@ -52,7 +75,9 @@ describe('unread message indicator', () => {
 
   it('does not include unread messages message when message count is undefined', () => {
     const unreadMessageCount = undefined;
-    const { queryByRole } = renderCards(unreadMessageCount);
+    const { queryByRole } = renderCards({
+      unreadMessageCount,
+    });
 
     const indicator = queryByRole('status');
 
