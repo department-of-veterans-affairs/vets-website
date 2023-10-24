0463cefdde7de8bd7a8e905eb63bb9fa41952017
Adam Antonioli <adam.antonioli@va.gov>
2023-10-24 11:52:44 -0400

    MBMS-47040 add prod flags to pre need applicant information pages changes

--- src/applications/pre-need/config/pages/nonVeteranApplicantDetails.jsx
+++ src/applications/pre-need/config/pages/nonVeteranApplicantDetails.jsx
@@ -4,6 +4,9 @@ import applicantDescription from 'platform/forms/components/ApplicantDescription
 import currentOrPastDateUI from 'platform/forms-system/src/js/definitions/currentOrPastDate';
 
 import { merge, pick } from 'lodash';
+
+import environment from 'platform/utilities/environment';
+
 import {
   applicantDetailsDescription,
   applicantDetailsSubHeader,
@@ -13,44 +16,76 @@ import {
 
 const { claimant } = fullSchemaPreNeed.properties.application.properties;
 
-export const uiSchema = {
-  'ui:description': applicantDescription,
-  application: {
-    'ui:title': applicantDetailsSubHeader,
-    claimant: {
-      'view:applicantDetailsDescription': {
-        'ui:description': applicantDetailsDescription,
-        'ui:options': {
-          displayEmptyObjectOnReview: true,
+export const uiSchema = !environment.isProduction()
+  ? {
+      'ui:description': applicantDescription,
+      application: {
+        'ui:title': applicantDetailsSubHeader,
+        claimant: {
+          'view:applicantDetailsDescription': {
+            'ui:description': applicantDetailsDescription,
+            'ui:options': {
+              displayEmptyObjectOnReview: true,
+            },
+          },
+          name: fullMaidenNameUI,
+          ssn: ssnDashesUI,
+          dateOfBirth: currentOrPastDateUI('Date of birth'),
         },
       },
-      name: fullMaidenNameUI,
-      ssn: ssnDashesUI,
-      dateOfBirth: currentOrPastDateUI('Date of birth'),
-    },
-  },
-};
-export const schema = {
-  type: 'object',
-  properties: {
-    application: {
+    }
+  : {
+      'ui:description': applicantDescription,
+      application: {
+        'ui:title': applicantDetailsSubHeader,
+        claimant: {
+          name: fullMaidenNameUI,
+          ssn: ssnDashesUI,
+          dateOfBirth: currentOrPastDateUI('Date of birth'),
+        },
+      },
+    };
+export const schema = !environment.isProduction()
+  ? {
       type: 'object',
       properties: {
-        claimant: {
+        application: {
           type: 'object',
-          required: ['name', 'ssn', 'dateOfBirth'],
-          properties: merge(
-            {},
-            {
-              'view:applicantDetailsDescription': {
-                type: 'object',
-                properties: {},
-              },
+          properties: {
+            claimant: {
+              type: 'object',
+              required: ['name', 'ssn', 'dateOfBirth'],
+              properties: merge(
+                {},
+                {
+                  'view:applicantDetailsDescription': {
+                    type: 'object',
+                    properties: {},
+                  },
+                },
+                pick(claimant.properties, ['name', 'ssn', 'dateOfBirth']),
+              ),
             },
-            pick(claimant.properties, ['name', 'ssn', 'dateOfBirth']),
-          ),
+          },
         },
       },
-    },
-  },
-};
+    }
+  : {
+      type: 'object',
+      properties: {
+        application: {
+          type: 'object',
+          properties: {
+            claimant: {
+              type: 'object',
+              required: ['name', 'ssn', 'dateOfBirth'],
+              properties: pick(claimant.properties, [
+                'name',
+                'ssn',
+                'dateOfBirth',
+              ]),
+            },
+          },
+        },
+      },
+    };
