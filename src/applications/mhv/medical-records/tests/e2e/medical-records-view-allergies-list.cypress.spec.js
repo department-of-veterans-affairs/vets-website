f1a3da9e0e477c4e13ecc1dbb26081a2a4690d91
Aaron Read <Aaron.Read@va.gov>
2023-10-24 15:22:33 -0500

    details and list tests

--- src/applications/mhv/medical-records/tests/e2e/medical-records-view-allergies-list.cypress.spec.js
+++ src/applications/mhv/medical-records/tests/e2e/medical-records-view-allergies-list.cypress.spec.js
@@ -1,31 +1,18 @@
+import AllergiesListPage from './pages/AllergiesListPage';
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
 import allergies from './fixtures/allergies.json';
-// import { cy } from 'date-fns/locale';
 
 describe('Medical Records View Allergies', () => {
   it('Visits Medical Records View Allergies List', () => {
     const site = new MedicalRecordsSite();
-
-    cy.intercept(
-      'GET',
-      '/my_health/v1/medical_records/allergies',
-      allergies,
-    ).as('allergies');
-
-    // cy.visit('my-health/medical-records/allergies');
     site.login();
     cy.visit('my-health/medical-records');
 
-    cy.get('[href="/my-health/medical-records/allergies"]').click();
+    AllergiesListPage.clickGotoAllergiesLink(allergies);
 
     cy.get('[data-testid="print-records-button"]')
       .should('be.visible')
       .click({ force: true });
-
-    // cy.get('[data-testid="print-allergies-list-1"').should('be.visible');
-
-    // cy.get('[data-testid="download-allergies-list-2"]').should('be.visible');
-    // Axe check
     cy.injectAxe();
     cy.axeCheck('main', {
       rules: {
