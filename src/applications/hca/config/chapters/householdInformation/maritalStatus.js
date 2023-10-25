5f981a450a805294acfa8f47d5a53e5f12f3f323
Joshua Drumm <joshua.drumm@agile6.com>
2023-10-25 12:30:32 -0400

    Reverted marital status change

--- src/applications/hca/config/chapters/householdInformation/maritalStatus.js
+++ src/applications/hca/config/chapters/householdInformation/maritalStatus.js
@@ -4,7 +4,7 @@ import PrefillMessage from 'platform/forms/save-in-progress/PrefillMessage';
 import { MaritalStatusDescription } from '../../../components/FormDescriptions';
 import CustomReviewField from '../../../components/FormReview/CustomReviewField';
 
-const { maritalStatus } = fullSchemaHca.properties;
+const { maritalStatus } = fullSchemaHca.definitions;
 
 export default {
   uiSchema: {
