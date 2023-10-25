684bf739b0b98c8868e31c0badc36b70854db738
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2023-10-25 17:33:30 -0400

    Moving "Appoint a Rep" work into temporary folder

---
+++ src/applications/representative/Appoint-a-rep work/reducers/index.js
@@ -0,0 +1,8 @@
+import { createSaveInProgressFormReducer } from '@department-of-veterans-affairs/platform-forms/reducers';
+import formConfig from '../config/form';
+
+const rootReducer = {
+  form: createSaveInProgressFormReducer(formConfig),
+};
+
+export default rootReducer;
