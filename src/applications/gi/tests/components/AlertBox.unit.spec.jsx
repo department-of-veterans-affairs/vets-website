20c15e950ff27eccbb3af3ed364fb714bd24db1c
fatmakhan0395 <Fatma.khan@va.gov>
2023-10-25 23:08:44 -0700

    adding text coverage for gi/components folder

--- src/applications/gi/tests/components/AlertBox.unit.spec.jsx
+++ src/applications/gi/tests/components/AlertBox.unit.spec.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { expect } from 'chai';
 import { shallow } from 'enzyme';
-
+import { expect } from 'chai';
 import AlertBox from '../../components/AlertBox';
 
 describe('<AlertBox>', () => {
-  it('should render', () => {
-    const tree = shallow(<AlertBox />);
-    expect(tree.type()).to.not.equal(null);
-    tree.unmount();
+  it('it should render', () => {
+    const currentProps = {
+      isVisible: true,
+      content: 'Previous content',
+      status: 'active',
+    };
+
+    const nextProps = {
+      isVisible: false,
+      content: 'New content',
+      status: 'inactive',
+    };
+
+    const wrapper = shallow(<AlertBox {...currentProps} />);
+    expect(wrapper.instance().shouldComponentUpdate(nextProps)).to.be.true;
+    wrapper.setProps(nextProps);
+    expect(wrapper.instance().shouldComponentUpdate(nextProps)).to.be.false;
+    wrapper.unmount();
   });
 });
--- src/applications/gi/tests/components/SearchBenefits.unit.spec.jsx
+++ src/applications/gi/tests/components/SearchBenefits.unit.spec.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-
+import sinon from 'sinon';
 import SearchBenefits from '../../components/SearchBenefits';
 
-describe('<SearchBenefits/>', () => {
-  it('should render', () => {
-    const wrapper = shallow(<SearchBenefits />);
-    expect(wrapper.html()).to.not.be.undefined;
+describe('SearchBenefits', () => {
+  it('it should handle change correctly', () => {
+    const setGiBillChapter = sinon.spy();
+    const setMilitaryStatus = sinon.spy();
+    const wrapper = shallow(
+      <SearchBenefits
+        giBillChapter=""
+        militaryStatus=""
+        setGiBillChapter={setGiBillChapter}
+        setMilitaryStatus={setMilitaryStatus}
+      />,
+    );
+    const event = { target: { name: 'militaryStatus', value: 'spouse' } };
+    wrapper.find('[name="militaryStatus"]').simulate('change', event);
+    expect(setMilitaryStatus.calledWith('spouse')).to.be.true;
+    expect(setGiBillChapter.calledWith('33a')).to.be.true;
     wrapper.unmount();
   });
 });
