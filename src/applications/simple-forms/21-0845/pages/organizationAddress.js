0f3ad7d632b4231c05ed10bb56c16f6451c3ef83
Tze-chiu Lei <tclei2009@gmail.com>
2023-10-26 14:40:36 -0700

    Remove code-comments

--- src/applications/simple-forms/21-0845/pages/organizationAddress.js
+++ src/applications/simple-forms/21-0845/pages/organizationAddress.js
@@ -33,27 +33,3 @@ export default {
     required: ['organizationAddress'],
   },
 };
-
-/** @type {PageSchema} */
-// export default {
-//   uiSchema: {
-//     'ui:title': <h3 className="custom-header">Organization’s address</h3>,
-//     organizationAddress: uiSchema({
-//       root: '',
-//       country: 'Organization’s country',
-//       street: 'Organization’s street address',
-//       street2: 'Apartment or unit number',
-//     }),
-//   },
-//   schema: {
-//     type: 'object',
-//     required: ['organizationAddress'],
-//     properties: {
-//       organizationAddress: schema({ definitions }, true, 'address', {
-//         street: 30,
-//         street2: 5,
-//         city: 18,
-//       }),
-//     },
-//   },
-// };
