6d2593b91c792b9104c77aeeef9d8b8f12c7f4ab
Carlos <carlos.felix@agile6.com>
2023-10-26 09:11:16 -0400

    Updates from design

--- src/applications/mhv/landing-page/components/NoHealthAlert.jsx
+++ src/applications/mhv/landing-page/components/NoHealthAlert.jsx
@@ -2,25 +2,23 @@ import React from 'react';
 
 const NoHealthAlert = () => {
   return (
-    <va-alert
-      close-btn-aria-label="Close notification"
-      status="info"
-      visible
-      data-testid="no-health-message"
-    >
-      <h2 slot="headline">We cannot display your health information.</h2>
+    <va-alert status="warning" visible data-testid="no-health-message">
+      <h2 slot="headline">We unable to display your health information.</h2>
       <div>
         <p className="vads-u-margin-y--0">
-          Only patients who have received care at a VA facility can use VA.gov
-          health tools. If you believe you have registered or received care at a
-          VA medical center, clinic, or Vet center, please call that facility to
+          Only patients who have registered at a VA facility can use VA.gov
+          health tools.
+        </p>
+        <p className="vads-u-margin-y--0">
+          If you believe you have registered or received care at a VA medical
+          center, clinic, or Vet center, please call that facility to find out
           find out if you’re in their records.
         </p>
         <p className="vads-u-margin-bottom--0">
-          <a href="/find-locations">Find a facility phone number</a>
+          <a href="/find-locations">Lookup a facility’s phone number</a>
         </p>
         <p className="vads-u-margin-bottom--0">
-          <a href="/health-care">Apply here for healthcare</a>
+          <a href="/health-care">Learn more about VA health care</a>
         </p>
       </div>
     </va-alert>
