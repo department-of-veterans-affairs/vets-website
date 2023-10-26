6cfd49d5081db2ce4e367708c3b6d7a9d2218935
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2023-10-26 17:38:43 -0400

    Display changes to SearchResult component

--- src/applications/representative/components/search/SearchResult.jsx
+++ src/applications/representative/components/search/SearchResult.jsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SearchResult = ({ name, type, address, phone, handleRedirect }) => {
+const SearchResult = ({
+  organization,
+  // type,
+  addressLine1,
+  addressLine2,
+  phone,
+  distance,
+}) => {
   return (
     <>
       <div className="representative-result">
-        <div className="primary">
-          <b>{name}</b> - {type}
+        <div>
+          <strong>{distance} Mi</strong>
         </div>
-        <div>{address}</div>
+        <div>
+          <strong>{organization}</strong>
+        </div>
+        <div>
+          {addressLine1}, {addressLine2}
+        </div>
         <div className="primary">{phone}</div>
-        <va-button onClick={e => handleRedirect(e)} text="Select" />
-        <div className="va-h-ruled" />
+        <div className="primary">Get directions on Google Maps</div>
+        {/* <div className="va-h-ruled" /> */}
+        <hr styling={{ borderTop: '3px solid black' }} />
       </div>
     </>
   );
 };
 
 SearchResult.propTypes = {
-  address: PropTypes.string.isRequired,
-  handleRedirect: PropTypes.func.isRequired,
-  name: PropTypes.string.isRequired,
+  addressLine1: PropTypes.string.isRequired,
+  addressLine2: PropTypes.string.isRequired,
+  // handleRedirect: PropTypes.func.isRequired,
+  organization: PropTypes.string.isRequired,
   phone: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
+  distance: PropTypes.number.isRequired,
 };
 
 export default SearchResult;
