25005660703bd9db6d5fca1a796462e0016560ec
Brad Bergeron <bergeron.bradley@gmail.com>
2023-10-26 16:24:09 -0400

    Adds formatting to the phone nuumbers viewable in contact information review panel

--- src/applications/my-education-benefits/components/ContactInformationReviewPanel.jsx
+++ src/applications/my-education-benefits/components/ContactInformationReviewPanel.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { formFields } from '../constants';
+
+export function formatPhoneNumber(phoneNumber) {
+  // Ensure we're dealing with a string of numbers.
+  const cleanDigits = String(phoneNumber).replace(/\D/g, '');
+  // Check if the phone number is not the correct length.
+  if (cleanDigits.length !== 10) {
+    // If it's not 10 digits, we don't want to format it,
+    // just return the original number.
+    return phoneNumber;
+  }
+  // At this point, we know the number is 10 digits long.
+  // Extract the area code and prefix.
+  const areaCode = cleanDigits.substring(0, 3);
+  const prefix = cleanDigits.substring(3, 6);
+  const line = cleanDigits.substring(6);
+  // Format to the desired format.
+  return `(${areaCode}) ${prefix}-${line}`;
+}
 
 const ContactInformationReviewPanel = ({ data, editPage, title }) => {
   const EMAIL_ADDRESS = data?.email?.email ?? null;
   const PHONE_NUMBERS = data['view:phoneNumbers'];
   const MOBILE_PHONE = PHONE_NUMBERS?.mobilePhoneNumber?.phone ?? null;
   const HOME_PHONE = PHONE_NUMBERS?.phoneNumber?.phone ?? null;
+
+  const formattedMobilePhone = MOBILE_PHONE
+    ? formatPhoneNumber(MOBILE_PHONE)
+    : null;
+  const formattedHomePhone = HOME_PHONE ? formatPhoneNumber(HOME_PHONE) : null;
 
   return (
     <>
@@ -27,11 +49,11 @@ const ContactInformationReviewPanel = ({ data, editPage, title }) => {
         <dl className="review">
           <div className="review-row">
             <dt>Mobile phone number</dt>
-            <dd>{MOBILE_PHONE}</dd>
+            <dd>{formattedMobilePhone}</dd>
           </div>
           <div className="review-row">
             <dt>Home phone number</dt>
-            <dd>{HOME_PHONE}</dd>
+            <dd>{formattedHomePhone}</dd>
           </div>
           <div className="review-row">
             <dt>Email</dt>
