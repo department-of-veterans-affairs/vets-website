e1521fc55189936115a2e1621a6cad62f23ab617
Aaron Read <Aaron.Read@va.gov>
2023-10-26 12:23:09 -0500

    Update medications-validate-accordions-on-landing-page.cypress.spec.js

--- src/applications/mhv/medications/tests/e2e/medications-validate-accordions-on-landing-page.cypress.spec.js
+++ src/applications/mhv/medications/tests/e2e/medications-validate-accordions-on-landing-page.cypress.spec.js
@@ -1,21 +1,23 @@
 import MedicationsSite from './med_site/MedicationsSite';
-import MedicationsLandingPage from './pages/MedicationsLandingPage';
+// import MedicationsLandingPage from './pages/MedicationsLandingPage';
 
 describe('Medications Accordions on Medications Landing Page', () => {
-  it.skip('visits accordions on landing page', () => {
+  it('visits accordions on landing page', () => {
     const site = new MedicationsSite();
-    const landingPage = new MedicationsLandingPage();
+    // const landingPage = new MedicationsLandingPage();
     site.login();
     cy.visit('my-health/about-medications/');
 
-    landingPage.clickExpandAccordionsOnMedicationsLandingPage();
-    landingPage.verifyListMedicationsAndSuppliesAccordionDropDown();
+    // landingPage.clickExpandAccordionsOnMedicationsLandingPage();
+    cy.get('[data-testid="accordion-dropdown"]').click();
+    /* landingPage.verifyListMedicationsAndSuppliesAccordionDropDown();
     landingPage.verifyWhatTypeOfPrescriptionsAccordionDropDown();
     landingPage.verifyPrescriptionRefillRequestInformationAccordionDropDown();
     landingPage.verifyMoreQuestionsAccordionDropDown();
     landingPage.verifyHowtoRenewPrescriptionsAccordionDropDown();
     landingPage.verifyHowToConfirmOrUpdateMailingAddressAccordionDropDown();
     landingPage.verifyHowToReviewAllergiesAndReactionsAccordionDropDown();
+    */
     cy.injectAxe();
     cy.axeCheck('main', {
       rules: {
