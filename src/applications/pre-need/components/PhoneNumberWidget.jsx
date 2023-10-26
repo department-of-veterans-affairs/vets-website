f7323a99463fd77fd5b48f2bbc65ad39948a06fd
Alex <AlexanderSherrow@gmail.com>
2023-10-26 09:57:57 -0400

    MBMS-51116 testing removing function

--- src/applications/pre-need/components/PhoneNumberWidget.jsx
+++ src/applications/pre-need/components/PhoneNumberWidget.jsx
@@ -9,11 +9,12 @@ import { PreparerPhoneNumberDescription } from '../utils/helpers';
 export default class PhoneNumberWidget extends React.Component {
   state = { val: this.props.value, firstUpdate: true };
 
-  componentDidUpdate(prevProps) {
-    if (this.state.firstUpdate && this.props.value !== prevProps.value) {
-      this.handleChange(this.props.value);
-    }
-  }
+  // componentDidUpdate(prevProps) {
+  //   console.log('hi');
+  //   if (this.state.firstUpdate && this.props.value !== prevProps.value) {
+  //     this.handleChange(this.props.value);
+  //   }
+  // }
 
   handleChange = val => {
     let stripped;
