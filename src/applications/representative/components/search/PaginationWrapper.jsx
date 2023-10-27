6e3745e696b1e32c79b02b0e4251c928bc5e88d4
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2023-10-26 22:07:22 -0400

    Removed pagination logic to force display for demoing purposes

--- src/applications/representative/components/search/PaginationWrapper.jsx
+++ src/applications/representative/components/search/PaginationWrapper.jsx
@@ -1,37 +1,19 @@
+import React from 'react';
 import { VaPagination } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import React, { Component } from 'react';
 
-export class PaginationWrapper extends Component {
-  shouldComponentUpdate = nextProps =>
-    nextProps.results !== this.props.results ||
-    nextProps.inProgress !== this.props.inProgress;
-
-  render() {
-    const {
-      shouldRender,
-      handlePageSelect,
-      currentPage,
-      totalPages,
-      results,
-    } = this.props;
-
-    if (
-      shouldRender &&
-      currentPage &&
-      totalPages > 1 &&
-      results &&
-      results.length > 0
-    ) {
-      return (
-        <VaPagination
-          onPageSelect={handlePageSelect}
-          page={currentPage}
-          pages={totalPages}
-        />
-      );
-    }
-    return null;
-  }
-}
+const PaginationWrapper = ({
+  handlePageSelect,
+  currentPage,
+  totalPages,
+  // searchResults,
+}) => {
+  return (
+    <VaPagination
+      onPageSelect={handlePageSelect}
+      page={currentPage}
+      pages={totalPages}
+    />
+  );
+};
 
 export default PaginationWrapper;
