83013310ec5d2bf62b94325b4dce55ede4152b67
Carlos <carlos.felix@agile6.com>
2023-10-27 10:00:38 -0400

    Added viewport sizes

--- src/applications/mhv/landing-page/tests/e2e/restrict-by-facilities-list/facilities.content.cypress.spec.js
+++ src/applications/mhv/landing-page/tests/e2e/restrict-by-facilities-list/facilities.content.cypress.spec.js
@@ -4,6 +4,8 @@ import ApiInitializer from '../utilities/ApiInitializer';
 import LandingPage from '../pages/LandingPage';
 import { resolveLandingPageLinks } from '../../../utilities/data';
 
+const viewportSizes = ['va-top-desktop-1', 'va-top-mobile-1'];
+
 const noHealthDataHeading = /You do not have access to My HealtheVet tools/i;
 
 // Validate a card has a heading and the correct number of links in it.
@@ -28,62 +30,67 @@ function validateLinkGroup(cardHeadline, numLinks) {
 
 describe(appName, () => {
   describe('Display content based on patient facilities', () => {
-    beforeEach(() => {
-      cy.intercept('GET', '/data/cms/vamc-ehr.json', vamcEhr).as('vamcEhr');
-      ApiInitializer.initializeFeatureToggle.withCurrentFeatures();
-    });
-
-    it('No health info for patients with no facilities', () => {
-      const pageLinks = resolveLandingPageLinks(false, [], 0, false);
-
-      LandingPage.visitPage({ facilities: [] });
-      LandingPage.validatePageLoaded();
-      LandingPage.validateURL();
-      cy.injectAxeThenAxeCheck();
-
-      // Test that the no health data message is present
-      cy.findByRole('heading', {
-        name: noHealthDataHeading,
-      }).should.exist;
-
-      // Test the cards are not visible
-      pageLinks.cards.forEach(card => {
-        cy.findByRole('heading', { name: card.title }).should('not.exist');
-      });
-      // Test the hubs are visible
-      pageLinks.hubs.forEach(hub => {
-        validateLinkGroup(hub.title, hub.links.length);
+    viewportSizes.forEach(size => {
+      beforeEach(() => {
+        cy.intercept('GET', '/data/cms/vamc-ehr.json', vamcEhr).as('vamcEhr');
+        ApiInitializer.initializeFeatureToggle.withCurrentFeatures();
       });
 
-      // Test for the conditional heading for VA health benefits
-      cy.findByRole('heading', { name: /VA health benefits/i }).should.exist;
-    });
+      it(`No health info for patients with no facilities on ${size} screen`, () => {
+        cy.viewportPreset(size);
+        const pageLinks = resolveLandingPageLinks(false, [], 0, false);
 
-    it('landing page is enabled for patients with facilities', () => {
-      const pageLinks = resolveLandingPageLinks(false, [], 0, true);
+        LandingPage.visitPage({ facilities: [] });
+        LandingPage.validatePageLoaded();
+        LandingPage.validateURL();
+        cy.injectAxeThenAxeCheck();
 
-      LandingPage.visitPage({
-        facilities: [{ facilityId: '123', isCerner: false }],
+        // Test that the no health data message is present
+        cy.findByRole('heading', {
+          name: noHealthDataHeading,
+        }).should.exist;
+
+        // Test the cards are not visible
+        pageLinks.cards.forEach(card => {
+          cy.findByRole('heading', { name: card.title }).should('not.exist');
+        });
+        // Test the hubs are visible
+        pageLinks.hubs.forEach(hub => {
+          validateLinkGroup(hub.title, hub.links.length);
+        });
+
+        // Test for the conditional heading for VA health benefits
+        cy.findByRole('heading', { name: /VA health benefits/i }).should.exist;
       });
-      LandingPage.validatePageLoaded();
-      LandingPage.validateURL();
-      cy.injectAxeThenAxeCheck();
 
-      // Validate the cards and hubs
-      pageLinks.cards.forEach(card => {
-        validateLinkGroup(card.title, card.links.length);
+      it(`landing page is enabled for patients with facilities on ${size} screen`, () => {
+        cy.viewportPreset(size);
+        const pageLinks = resolveLandingPageLinks(false, [], 0, true);
+
+        LandingPage.visitPage({
+          facilities: [{ facilityId: '123', isCerner: false }],
+        });
+        LandingPage.validatePageLoaded();
+        LandingPage.validateURL();
+        cy.injectAxeThenAxeCheck();
+
+        // Validate the cards and hubs
+        pageLinks.cards.forEach(card => {
+          validateLinkGroup(card.title, card.links.length);
+        });
+        pageLinks.hubs.forEach(hub => {
+          validateLinkGroup(hub.title, hub.links.length);
+        });
+
+        // Test for the conditional heading for VA health benefits
+        cy.findByRole('heading', { name: /My VA health benefits/i }).should
+          .exist;
+
+        // Test that the no health data message is NOT present
+        cy.findByRole('heading', {
+          name: noHealthDataHeading,
+        }).should('not.exist');
       });
-      pageLinks.hubs.forEach(hub => {
-        validateLinkGroup(hub.title, hub.links.length);
-      });
-
-      // Test for the conditional heading for VA health benefits
-      cy.findByRole('heading', { name: /My VA health benefits/i }).should.exist;
-
-      // Test that the no health data message is NOT present
-      cy.findByRole('heading', {
-        name: noHealthDataHeading,
-      }).should('not.exist');
     });
   });
 });
