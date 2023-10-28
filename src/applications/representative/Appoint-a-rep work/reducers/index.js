826662bfdf316c044158dc6a7dffcf3910002f5d
Colin O'Sullivan <88686663+cjosu@users.noreply.github.com>
2023-10-28 10:02:58 -0700

    Removing unused Appoint-a-Rep references

--- src/applications/representative/Appoint-a-rep work/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// import { createSaveInProgressFormReducer } from '@department-of-veterans-affairs/platform-forms/reducers';
-// import formConfig from '../config/form';
-
-// const rootReducer = {
-//   form: createSaveInProgressFormReducer(formConfig),
-// };
-
-// export default rootReducer;
