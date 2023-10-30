04bd0ed03280c00c94f6b6271b3638dab5b63bb4
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2023-10-30 10:12:09 -0400

    MHV-50175 MHV SM "Start new message" link formatting (#26377)
    * [#MHV-50175] start new message link formatting
    
    * [#MHV-50175] unit test coverage
    
    * [#MHV-50175] cy test
    
    * [#MHV-50175] interstitial page unit tests

--- src/applications/mhv/secure-messaging/containers/InterstitialPage.jsx
+++ src/applications/mhv/secure-messaging/containers/InterstitialPage.jsx
@@ -43,6 +43,20 @@ const InterstitialPage = props => {
           Your care team may take up to <strong>3 business days</strong> to
           reply.
         </p>
+        {
+          // linter advises to use button instead. However, per designs, action link is required
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a
+            data-testid="continue-button"
+            className="vads-c-action-link--green vads-u-margin-bottom--2 small-screen:vads-u-margin-bottom--1 link"
+            tabIndex={0}
+            role="button"
+            onKeyPress={handleKeyPress}
+            onClick={props.acknowledge}
+          >
+            {continueButtonText}
+          </a>
+        }
         <p>
           If you need help sooner, use one of these urgent communication
           options:
@@ -67,20 +81,6 @@ const InterstitialPage = props => {
             </p>
           </li>
         </ul>
-        {
-          // linter advises to use button instead. However, per designs, action link is required
-          // eslint-disable-next-line jsx-a11y/anchor-is-valid
-          <a
-            className="vads-c-action-link--green vads-u-margin-top--1 link"
-            tabIndex={0}
-            role="button"
-            onKeyPress={handleKeyPress}
-            onClick={props.acknowledge}
-            data-testid="continue-button"
-          >
-            {continueButtonText}
-          </a>
-        }
       </div>
     </div>
   );
