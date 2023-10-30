57fbf6e1710ebc4118c4696c4c31b22ea415e193
mattwrightva <107576133+mattwrightva@users.noreply.github.com>
2023-10-30 10:07:58 -0600

    MHV-48204: labs and tests error handling complete (#26408)
    Co-authored-by: wright0776 <matwright2010@gmail.com>

--- src/applications/mhv/medical-records/actions/labsAndTests.js
+++ src/applications/mhv/medical-records/actions/labsAndTests.js
@@ -1,23 +1,14 @@
 import { Actions } from '../util/actionTypes';
 import { getLabsAndTests, getLabOrTest } from '../api/MrApi';
+import * as Constants from '../util/constants';
+import { addAlert } from './alerts';
 
 export const getLabsAndTestsList = () => async dispatch => {
   try {
     const response = await getLabsAndTests();
     dispatch({ type: Actions.LabsAndTests.GET_LIST, response });
   } catch (error) {
-    // TODO: add error handling
-    // console.error(error);
-    // const err = error.errors[0];
-    // dispatch({
-    //   type: Actions.Alerts.ADD_ALERT,
-    //   payload: {
-    //     alertType: 'error',
-    //     header: err.title,
-    //     content: err.detail,
-    //     response: err,
-    //   },
-    // });
+    dispatch(addAlert(Constants.ALERT_TYPE_ERROR));
   }
 };
 
@@ -26,18 +17,7 @@ export const getlabsAndTestsDetails = labId => async dispatch => {
     const response = await getLabOrTest(labId);
     dispatch({ type: Actions.LabsAndTests.GET, response });
   } catch (error) {
-    // console.error('error: ', error);
-    // TODO: implement error handling
-    // const err = error.errors[0];
-    // dispatch({
-    //   type: Actions.Alerts.ADD_ALERT,
-    //   payload: {
-    //     alertType: 'error',
-    //     header: err.title,
-    //     content: err.detail,
-    //     response: err,
-    //   },
-    // });
+    dispatch(addAlert(Constants.ALERT_TYPE_ERROR));
   }
 };
 
