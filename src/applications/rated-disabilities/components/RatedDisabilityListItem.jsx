7aa955ee9c775178f218073e7ee55ff9d45e0338
jerekshoe <jerek.shoemaker@adhocteam.us>
2023-10-30 14:59:18 -0500

    Fixing issue with dfn

--- src/applications/rated-disabilities/components/RatedDisabilityListItem.jsx
+++ src/applications/rated-disabilities/components/RatedDisabilityListItem.jsx
@@ -21,12 +21,7 @@ const RatedDisabilityListItem = ({ ratedDisability }) => {
       <h3 className="vads-u-margin-y--0">{headingText}</h3>
       {effectiveDate !== null && (
         <div className="vads-u-margin-top--2">
-          <dd className="vads-u-display--block vads-u-margin--0">
-            <dfn className="vads-u-display--inline-block vads-u-font-weight--bold vads-u-margin-right--0p5">
-              Effective date:
-            </dfn>
-            {effectiveDate.format('MM/DD/YYYY')}
-          </dd>
+          <strong>Effective date:</strong> {effectiveDate.format('MM/DD/YYYY')}
         </div>
       )}
     </va-card>
