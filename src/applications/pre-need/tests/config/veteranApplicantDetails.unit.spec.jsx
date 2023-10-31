080a6588fd57f8811058929781fa67d8bcc3f03a
Adam Antonioli <adam.antonioli@va.gov>
2023-10-31 09:48:08 -0400

    MBMS-47040 update unit tests for pre need applicant information screen

--- src/applications/pre-need/tests/config/veteranApplicantDetails.unit.spec.jsx
+++ src/applications/pre-need/tests/config/veteranApplicantDetails.unit.spec.jsx
@@ -84,12 +84,10 @@ describe('Pre-need applicant veteran applicant details', () => {
     );
 
     expect(form.find('va-additional-info').length).to.equal(1);
-    expect(form.find('div#info').props().className).to.equal('closed');
-
     click(form, 'va-additional-info');
-
-    expect(form.find('va-additional-info').length).to.equal(1);
-    expect(form.find('div#info').props().className).to.equal('open');
+    expect(form.find('va-additional-info').html()).to.include(
+      '<va-additional-info trigger="What if the applicant is not a service member or Veteran?"><ul><li>A <strong>spouse</strong> is a person who is or was legally married to a service member or Veteran. A <strong>surviving spouse</strong> is someone who was legally married to the service member or Veteran at the time of their death and includes a surviving spouse who remarried.</li><li>An <strong>unmarried adult child</strong> is an individual who became physically or mentally disabled permanently and incapable of self-support before the age of 21, or before 23 years of age if pursuing a full-time course of instruction at an approved educational institution.</li><li>For <strong>other</strong> applicants such as the parent of a service member, we’ll ask questions about the service member (the sponsor) to determine eligibility for burial in a VA national cemetery.</li></ul></va-additional-info>',
+    );
     form.unmount();
   });
 });
