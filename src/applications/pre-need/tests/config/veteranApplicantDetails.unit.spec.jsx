6054e2ff35f919fc42525b1a614ea74749c99131
Adam Antonioli <adam.antonioli@va.gov>
2023-10-31 10:47:22 -0400

    MBMS-47040 update unit tests for pre need applicant information screen

--- src/applications/pre-need/tests/config/veteranApplicantDetails.unit.spec.jsx
+++ src/applications/pre-need/tests/config/veteranApplicantDetails.unit.spec.jsx
@@ -28,7 +28,7 @@ describe('Pre-need applicant veteran applicant details', () => {
     expect(form.find('select').length).to.equal(3);
     expect(form.find('va-additional-info').length).to.equal(1);
     expect(form.find('va-additional-info').html()).to.include(
-      '<va-additional-info trigger="What if the applicant is not a service member or Veteran?"><ul><li>A <strong>spouse</strong> is a person who is or was legally married to a service member or Veteran. A <strong>surviving spouse</strong> is someone who was legally married to the service member or Veteran at the time of their death and includes a surviving spouse who remarried.</li><li>An <strong>unmarried adult child</strong> is an individual who became physically or mentally disabled permanently and incapable of self-support before the age of 21, or before 23 years of age if pursuing a full-time course of instruction at an approved educational institution.</li><li>For <strong>other</strong> applicants such as the parent of a service member, we’ll ask questions about the service member (the sponsor) to determine eligibility for burial in a VA national cemetery.</li></ul></va-additional-info>',
+      '<va-additional-info trigger="Are you filling out this application on behalf of someone else?"><p>If you’re filling out the form on behalf of someone else, you’ll need to provide their details below. As the preparer, we’ll ask for your own details later.</p></va-additional-info>',
     );
     form.unmount();
   });
