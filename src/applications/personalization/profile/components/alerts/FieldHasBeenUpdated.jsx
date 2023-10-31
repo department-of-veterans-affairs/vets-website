1c948ec2e0b65a3e6b99f1e8c02bad58d383d4c8
Adam Whitlock <adam.whitlock@adhocteam.us>
2023-10-31 10:32:53 -0600

    fix: + alert role, clear history state on render

--- src/applications/personalization/profile/components/alerts/FieldHasBeenUpdated.jsx
+++ src/applications/personalization/profile/components/alerts/FieldHasBeenUpdated.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useLocation } from 'react-router-dom';
 
 export const FieldHasBeenUpdated = () => {
+  const location = useLocation();
   const locationState = useLocation().state;
   const { fieldInfo = null } = locationState || {};
+
+  // on chrome if you don't clear out browser history state, it will show the
+  // alert after refresh. this useEffect clears the state after alert is shown
+  useEffect(() => {
+    if (location.state?.fieldInfo) {
+      window.history.replaceState(null, '');
+    }
+  }, []);
 
   const text = `We saved your ${fieldInfo?.title?.toLowerCase() ||
     `information`} to your profile.`;
@@ -15,6 +24,7 @@ export const FieldHasBeenUpdated = () => {
       background-only
       status="success"
       show-icon
+      role="alert"
     >
       <p className="vads-u-margin-y--0">{text}</p>
     </va-alert>
