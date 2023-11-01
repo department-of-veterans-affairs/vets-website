2387f1c99bcf882dc7451290021346b869e1d4bd
fazilqa <fazil.mammadov@bylight.com>
2023-11-01 14:46:37 -0700

    remove unnecessary spec add edit folder without name test

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-edit-folder-name.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-edit-folder-name.cypress.spec.js
@@ -4,24 +4,61 @@ import { AXE_CONTEXT } from './utils/constants';
 import PatientMessageCustomFolderPage from './pages/PatientMessageCustomFolderPage';
 
 describe('Secure Messaging Custom Folder Edit Folder Name Message Validation', () => {
-  it('Axe Check Custom Folder List', () => {
+  it('verify edit folder name buttons', () => {
     const landingPage = new PatientInboxPage();
     const site = new SecureMessagingSite();
     site.login();
     landingPage.loadInboxMessages();
     PatientMessageCustomFolderPage.loadFoldersList();
     PatientMessageCustomFolderPage.loadMessages();
-    cy.get('[data-testid="edit-folder-button"]').click({ force: true });
+
+    cy.get('[data-testid="edit-folder-button"]')
+      .should('be.visible')
+      .click({ force: true });
     cy.get('[name="new-folder-name"]')
+      .should('be.visible')
       .shadow()
       .find('[id="inputField"]')
       .type('Testing');
-    cy.get('[visible=""] > [secondary=""]').click();
-    cy.focused({ timeout: 5000 }).should(
-      'have.attr',
-      'data-testid',
-      'edit-folder-button',
-    );
+
+    cy.get('[text="Save"]')
+      .should('be.visible')
+      .click();
+
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+        'color-contrast': {
+          enabled: false,
+        },
+      },
+    });
+  });
+
+  it('verify edit folder name error', () => {
+    const landingPage = new PatientInboxPage();
+    const site = new SecureMessagingSite();
+    site.login();
+    landingPage.loadInboxMessages();
+    PatientMessageCustomFolderPage.loadFoldersList();
+    PatientMessageCustomFolderPage.loadMessages();
+
+    cy.get('[data-testid="edit-folder-button"]')
+      .should('be.visible')
+      .click({ force: true });
+
+    cy.get('[text="Save"]')
+      .should('be.visible')
+      .click();
+
+    cy.get('[label="Folder name"]')
+      .shadow()
+      .find('#input-error-message')
+      .should('be.visible')
+      .and('include.text', 'Folder name cannot be blank');
 
     cy.injectAxe();
     cy.axeCheck(AXE_CONTEXT, {
