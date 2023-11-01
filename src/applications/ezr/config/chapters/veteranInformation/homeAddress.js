3846a23f1c13607e6a296ceb1cd1043c76630281
Matt Long <longmd@me.com>
2023-11-01 18:05:59 -0400

    Allow for Military base address for 10-10EZR form (#26511)

--- src/applications/ezr/config/chapters/veteranInformation/homeAddress.js
+++ src/applications/ezr/config/chapters/veteranInformation/homeAddress.js
@@ -18,13 +18,13 @@ export default {
   uiSchema: {
     ...descriptionUI(PrefillMessage, { hideOnReview: true }),
     'view:pageTitle': inlineTitleUI(content['vet-home-address-title']),
-    veteranHomeAddress: addressUI({ omit: ['isMilitary'] }),
+    veteranHomeAddress: addressUI(),
   },
   schema: {
     type: 'object',
     properties: {
       'view:pageTitle': inlineTitleSchema,
-      veteranHomeAddress: merge({}, addressSchema({ omit: ['isMilitary'] }), {
+      veteranHomeAddress: merge({}, addressSchema(), {
         properties: schemaOverride,
       }),
     },
