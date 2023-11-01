2687023c314a0fbf8790ddb7e414611687fdc2bd
fatmakhan0395 <Fatma.khan@va.gov>
2023-10-31 22:30:13 -0700

    vfep-916 tests for profile folder

--- src/applications/gi/tests/components/profile/CalculateYourBenefitsForm.unit.spec.jsx
+++ src/applications/gi/tests/components/profile/CalculateYourBenefitsForm.unit.spec.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-
 import CalculateYourBenefitsForm from '../../../components/profile/CalculateYourBenefitsForm';
 
 describe('<CalculateYourBenefitsForm>', () => {
@@ -16,4 +15,18 @@ describe('<CalculateYourBenefitsForm>', () => {
     expect(tree.type()).to.not.equal(null);
     tree.unmount();
   });
+
+  // it('should render', () => {
+  //   const tree = shallow(
+  //     <CalculateYourBenefitsForm
+  //       eligibility={{}}
+  //       inputs={{}}
+  //       displayedInputs={{}}
+  //     />,
+  //   );
+  //   console.log(tree.instance(), "tree")
+  //   tree.find('AccordionItem').first().simulate('click');
+  //   expect(tree.instance().toggleExpanded.calledWith('aboutYourSchool', true)).to.be.true;
+  //   tree.unmount();
+  // });
 });
