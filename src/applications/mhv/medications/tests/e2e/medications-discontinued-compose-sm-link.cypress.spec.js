493847de2f320ffed4600297e1fe727c44b47c11
Aaron Read <Aaron.Read@va.gov>
2023-11-02 14:54:21 -0500

    Revert "Update medications-discontinued-compose-sm-link.cypress.spec.js"
    This reverts commit 859c0236851f53c55584ed21dd99acf2edc53dcf.

--- src/applications/mhv/medications/tests/e2e/medications-discontinued-compose-sm-link.cypress.spec.js
+++ src/applications/mhv/medications/tests/e2e/medications-discontinued-compose-sm-link.cypress.spec.js
@@ -1,4 +1,3 @@
-import environment from '@department-of-veterans-affairs/platform-utilities/environment';
 import MedicationsSite from './med_site/MedicationsSite';
 import MedicationsListPage from './pages/MedicationsListPage';
 
@@ -21,22 +20,8 @@ describe('Medications List Page DropDown -- discontinued SM Compose Link', () =>
       },
     });
     listPage.clickGotoMedicationsLink(true);
-    if (environment.BASE_URL.includes('localhost')) {
-      cy.log('running localhost');
-      cy.get('[data-testid="discontinued-compose-message-link"]')
-        .invoke('attr', 'href')
-        .should(
-          'contain',
-          'mhv-syst.myhealth.va.gov/mhv-portal-web/secure-messaging',
-        );
-    } else {
-      cy.log('running prod');
-      cy.get('[data-testid="discontinued-compose-message-link"]')
-        .invoke('attr', 'href')
-        .should(
-          'contain',
-          'www.myhealth.va.gov/mhv-portal-web/secure-messaging',
-        );
-    }
+    cy.get('[data-testid="discontinued-compose-message-link"]')
+      .invoke('attr', 'href')
+      .should('contain', 'myhealth.va.gov/mhv-portal-web/secure-messaging');
   });
 });
