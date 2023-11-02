4cc2627f23b0d462655e7ba0de89065105ddb0ca
Jennie McGibney <jennie.mcgibney@va.gov>
2023-11-01 20:04:34 -0500

    Fix flaky e2e test

--- src/applications/vaos/tests/e2e/page-objects/AppointmentList/AppointmentListPageObject.js
+++ src/applications/vaos/tests/e2e/page-objects/AppointmentList/AppointmentListPageObject.js
@@ -7,9 +7,12 @@ export class AppointmentListPageObject extends PageObject {
   }
 
   selectListItem() {
-    cy.findByTestId('appointment-list-item')
-      .first()
-      .click({ waitForAnimations: true });
+    // get the first appointment list item
+    cy.get(
+      '[data-testid="appointment-list-item"] > :nth-child(1) > :nth-child(1) > :nth-child(1)',
+    )
+      .should('exist')
+      .click();
 
     // cy.axeCheckBestPractice();
 
@@ -19,10 +22,10 @@ export class AppointmentListPageObject extends PageObject {
   validate() {
     // Wait for appointments to load
     cy.wait(['@v2:get:appointments']);
-
-    cy.get('h2')
+    // get the header month
+    cy.get('[data-testid="appointment-list-header"]')
       .should('be.visible')
-      .and('contain', 'Appointments');
+      .and('contain', 'Appointments in');
 
     cy.findByText(/You don.t have any upcoming appointments/i).should(
       'not.exist',
