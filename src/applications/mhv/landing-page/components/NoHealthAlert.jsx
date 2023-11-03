08c0139dd14235e8a297668239686204fd499b36
Carlos <carlos.felix@agile6.com>
2023-11-03 16:50:24 -0400

    Updated link spacing

--- src/applications/mhv/landing-page/components/NoHealthAlert.jsx
+++ src/applications/mhv/landing-page/components/NoHealthAlert.jsx
@@ -19,7 +19,7 @@ const NoHealthAlert = () => {
           If you’ve received care at a VA health facility, call the facility and
           ask if you’re registered.
         </p>
-        <p className="vads-u-margin-bottom--0">
+        <p>
           <a href="/find-locations/?&facilityType=health">
             Find your nearest VA health facility
           </a>
@@ -27,7 +27,7 @@ const NoHealthAlert = () => {
         <p className="vads-u-margin-y--0">
           If you’re not enrolled in VA health care, you can apply now.
         </p>
-        <p className="vads-u-margin-bottom--0">
+        <p>
           <a href="/health-care/how-to-apply/">
             Find out how to apply for VA health care
           </a>
