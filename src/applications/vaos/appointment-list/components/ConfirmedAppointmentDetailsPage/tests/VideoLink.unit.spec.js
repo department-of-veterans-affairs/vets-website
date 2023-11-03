2955f1791f35b8b2c33e787a9fa52dad92017a83
Simi Adebowale <47654119+simiadebowale@users.noreply.github.com>
2023-11-03 15:45:04 -0400

    added additional test coverage

--- src/applications/vaos/appointment-list/components/ConfirmedAppointmentDetailsPage/tests/VideoLink.unit.spec.js
+++ src/applications/vaos/appointment-list/components/ConfirmedAppointmentDetailsPage/tests/VideoLink.unit.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import moment from 'moment';
 import VideoLink from '../VideoLink';
 
@@ -18,6 +18,11 @@ describe('VideoVisitInstructions', () => {
     expect(wrapper.container.querySelector('.usa-button')).to.exist;
     expect(wrapper.container.querySelector('.usa-button-disabled')).to.not
       .exist;
+    // fireEvent.click returns false if event is cancelable, and at least one of the event
+    // handlers which received event called Event.preventDefault(). Otherwise true.
+    // We do not expect preventDefault() to be called if the link is active.
+    expect(fireEvent.click(wrapper.container.querySelector('.usa-button'))).to
+      .be.true;
   });
   it('renders disabled join appoinment link 35 minutes prior to start time', () => {
     const appointment = {
@@ -44,5 +49,24 @@ describe('VideoVisitInstructions', () => {
     expect(wrapper.queryByText('Join appointment')).to.exist;
     expect(wrapper.container.querySelector('.usa-button')).to.exist;
     expect(wrapper.container.querySelector('.usa-button-disabled')).to.exist;
+  });
+  it('call preventDefault function', () => {
+    const appointment = {
+      start: moment().add(35, 'm'),
+      videoData: {
+        url: 'test.com',
+      },
+    };
+    const props = { appointment };
+    const wrapper = render(<VideoLink {...props} />);
+    expect(wrapper.queryByText('Join appointment')).to.exist;
+    expect(wrapper.container.querySelector('.usa-button')).to.exist;
+    expect(wrapper.container.querySelector('.usa-button-disabled')).to.exist;
+
+    // fireEvent.click returns false if event is cancelable, and at least one of the event
+    // handlers which received event called Event.preventDefault(). Otherwise true.
+    // We expect preventDefault() to be called if the link is disabled.
+    expect(fireEvent.click(wrapper.container.querySelector('.usa-button'))).to
+      .be.false;
   });
 });
