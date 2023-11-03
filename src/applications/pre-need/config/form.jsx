096306ab7af4b48318d89193ffbfe63fd65e55f4
624883 <Connor.Fewin@va.gov>
2023-11-03 11:45:05 -0400

    Initial Commit, setup sponsor demographic and seperate sponsor pages

--- src/applications/pre-need/config/form.jsx
+++ src/applications/pre-need/config/form.jsx
@@ -18,6 +18,8 @@ import emailUI from '../definitions/email';
 import * as applicantMilitaryHistory from './pages/applicantMilitaryHistory';
 import * as applicantMilitaryName from './pages/applicantMilitaryName';
 import * as applicantMilitaryNameInformation from './pages/applicantMilitaryNameInformation';
+import * as sponsorDetails from './pages/sponserDetails';
+import * as sponsorDemographics from './pages/sponserDemographics';
 import * as sponsorMilitaryHistory from './pages/sponsorMilitaryHistory';
 import * as sponsorMilitaryName from './pages/sponsorMilitaryName';
 import * as sponsorMilitaryNameInformation from './pages/sponsorMilitaryNameInformation';
@@ -332,137 +334,163 @@ const formConfig = {
     },
     sponsorInformation: {
       title: 'Sponsor information',
-      pages: {
-        sponsorInformation: {
-          path: 'sponsor-information',
-          depends: formData => !isVeteran(formData),
-          uiSchema: {
-            'ui:description': applicantDescription,
-            application: {
-              veteran: merge({}, veteranUI, {
-                currentName: merge({}, fullNameUI, {
-                  first: {
-                    'ui:title': 'Sponsor’s first name',
-                  },
-                  last: {
-                    'ui:title': 'Sponsor’s last name',
-                  },
-                  middle: {
-                    'ui:title': 'Sponsor’s middle name',
-                  },
-                  suffix: {
-                    'ui:title': 'Sponsor’s suffix',
-                  },
-                  maiden: {
-                    'ui:title': 'Sponsor’s maiden name',
-                  },
-                  'ui:order': ['first', 'middle', 'last', 'suffix', 'maiden'],
-                }),
-                militaryServiceNumber: {
-                  'ui:title':
-                    'Sponsor’s Military Service number (if they have one that’s different than their Social Security number)',
-                  'ui:errorMessages': {
-                    pattern:
-                      'Sponsor’s Military Service number must be between 4 to 9 characters',
-                  },
-                },
-                vaClaimNumber: {
-                  'ui:title': 'Sponsor’s VA claim number (if known)',
-                  'ui:errorMessages': {
-                    pattern: 'Sponsor’s VA claim number must be 8 or 9 digits',
-                  },
-                },
-                ssn: {
-                  ...ssnDashesUI,
-                  'ui:title': 'Sponsor’s Social Security number',
-                },
-                dateOfBirth: currentOrPastDateUI('Sponsor’s date of birth'),
-                placeOfBirth: {
-                  'ui:title':
-                    "Sponsor's place of birth (City, State, or Territory)",
-                },
-                gender: {
-                  'ui:title':
-                    "Sponsor's sex (information will be used for statistical purposes only)",
-                },
-                race: {
-                  'ui:title':
-                    'Which categories best describe your sponsor? (You may check more than one)',
-                },
-                maritalStatus: {
-                  'ui:title': 'Sponsor’s marital status',
-                },
-                militaryStatus: {
-                  'ui:title':
-                    'Sponsor’s current military status (You can add more service history information later in this application)',
-                  'ui:options': {
-                    nestedContent: {
-                      X: sponsorMilitaryStatusDescription,
+      /* 
+       * Prod flag this high up because its screen division. Make sure content 
+       * changes are in correct spots and not in a code chunk that is getting deleted. 
+       */
+      pages: environment.isProduction()
+        ? {
+            sponsorInformation: {
+              path: 'sponsor-information',
+              depends: formData => !isVeteran(formData),
+              uiSchema: {
+                'ui:description': applicantDescription,
+                application: {
+                  veteran: merge({}, veteranUI, {
+                    currentName: merge({}, fullNameUI, {
+                      first: {
+                        'ui:title': 'Sponsor’s first name',
+                      },
+                      last: {
+                        'ui:title': 'Sponsor’s last name',
+                      },
+                      middle: {
+                        'ui:title': 'Sponsor’s middle name',
+                      },
+                      suffix: {
+                        'ui:title': 'Sponsor’s suffix',
+                      },
+                      maiden: {
+                        'ui:title': 'Sponsor’s maiden name',
+                      },
+                      'ui:order': [
+                        'first',
+                        'middle',
+                        'last',
+                        'suffix',
+                        'maiden',
+                      ],
+                    }),
+                    militaryServiceNumber: {
+                      'ui:title':
+                        'Sponsor’s Military Service number (if they have one that’s different than their Social Security number)',
+                      'ui:errorMessages': {
+                        pattern:
+                          'Sponsor’s Military Service number must be between 4 to 9 characters',
+                      },
                     },
-                  },
-                },
-                isDeceased: {
-                  'ui:title': 'Has the sponsor died?',
-                  'ui:widget': 'radio',
-                  'ui:options': {
-                    labels: {
-                      yes: 'Yes',
-                      no: 'No',
-                      unsure: 'I don’t know',
+                    vaClaimNumber: {
+                      'ui:title': 'Sponsor’s VA claim number (if known)',
+                      'ui:errorMessages': {
+                        pattern:
+                          'Sponsor’s VA claim number must be 8 or 9 digits',
+                      },
                     },
-                  },
-                },
-                dateOfDeath: merge(
-                  {},
-                  currentOrPastDateUI('Sponsor’s date of death'),
-                  {
-                    'ui:options': {
-                      expandUnder: 'isDeceased',
-                      expandUnderCondition: 'yes',
+                    ssn: {
+                      ...ssnDashesUI,
+                      'ui:title': 'Sponsor’s Social Security number',
                     },
-                  },
-                ),
-                'ui:validations': [validateSponsorDeathDate],
-              }),
-            },
-          },
-          schema: {
-            type: 'object',
-            properties: {
-              application: {
+                    dateOfBirth: currentOrPastDateUI('Sponsor’s date of birth'),
+                    placeOfBirth: {
+                      'ui:title':
+                        "Sponsor's place of birth (City, State, or Territory)",
+                    },
+                    gender: {
+                      'ui:title':
+                        "Sponsor's sex (information will be used for statistical purposes only)",
+                    },
+                    race: {
+                      'ui:title':
+                        'Which categories best describe your sponsor? (You may check more than one)',
+                    },
+                    maritalStatus: {
+                      'ui:title': 'Sponsor’s marital status',
+                    },
+                    militaryStatus: {
+                      'ui:title':
+                        'Sponsor’s current military status (You can add more service history information later in this application)',
+                      'ui:options': {
+                        nestedContent: {
+                          X: sponsorMilitaryStatusDescription,
+                        },
+                      },
+                    },
+                    isDeceased: {
+                      'ui:title': 'Has the sponsor died?',
+                      'ui:widget': 'radio',
+                      'ui:options': {
+                        labels: {
+                          yes: 'Yes',
+                          no: 'No',
+                          unsure: 'I don’t know',
+                        },
+                      },
+                    },
+                    dateOfDeath: merge(
+                      {},
+                      currentOrPastDateUI('Sponsor’s date of death'),
+                      {
+                        'ui:options': {
+                          expandUnder: 'isDeceased',
+                          expandUnderCondition: 'yes',
+                        },
+                      },
+                    ),
+                    'ui:validations': [validateSponsorDeathDate],
+                  }),
+                },
+              },
+              schema: {
                 type: 'object',
                 properties: {
-                  veteran: {
+                  application: {
                     type: 'object',
-                    required: [
-                      'ssn',
-                      'gender',
-                      'maritalStatus',
-                      'militaryStatus',
-                      'isDeceased',
-                      'race',
-                    ],
-                    properties: pick(veteran.properties, [
-                      'currentName',
-                      'ssn',
-                      'dateOfBirth',
-                      'militaryServiceNumber',
-                      'vaClaimNumber',
-                      'placeOfBirth',
-                      'gender',
-                      'race',
-                      'maritalStatus',
-                      'militaryStatus',
-                      'isDeceased',
-                      'dateOfDeath',
-                    ]),
+                    properties: {
+                      veteran: {
+                        type: 'object',
+                        required: [
+                          'ssn',
+                          'gender',
+                          'maritalStatus',
+                          'militaryStatus',
+                          'isDeceased',
+                          'race',
+                        ],
+                        properties: pick(veteran.properties, [
+                          'currentName',
+                          'ssn',
+                          'dateOfBirth',
+                          'militaryServiceNumber',
+                          'vaClaimNumber',
+                          'placeOfBirth',
+                          'gender',
+                          'race',
+                          'maritalStatus',
+                          'militaryStatus',
+                          'isDeceased',
+                          'dateOfDeath',
+                        ]),
+                      },
+                    },
                   },
                 },
               },
             },
+          }
+        : {
+            sponserDetails: {
+              path: 'sponsor-detials',
+              depends: formData => !isVeteran(formData),
+              uiSchema: sponsorDetails.uiSchema,
+              schema: sponsorDetails.schema,
+            },
+            sponserDemographics: {
+              path: 'sponsor-demographics',
+              depends: formData => !isVeteran(formData),
+              uiSchema: sponsorDemographics.uiSchema,
+              schema: sponsorDemographics.schema,
+            },
           },
-        },
-      },
     },
     militaryHistory: {
       title: 'Military history',
---
+++ src/applications/pre-need/config/pages/sponserDemographics.jsx
@@ -0,0 +1,44 @@
+import fullSchemaPreNeed from 'vets-json-schema/dist/40-10007-schema.json';
+
+import { merge, pick } from 'lodash';
+import { veteranUI } from '../../utils/helpers';
+
+const { veteran } = fullSchemaPreNeed.properties.application.properties;
+
+export const uiSchema = {
+  application: {
+    veteran: merge({}, veteranUI, {
+      gender: {
+        'ui:title':
+          "Sponsor's sex (information will be used for statistical purposes only)",
+      },
+      race: {
+        'ui:title':
+          'Which categories best describe your sponsor? (You may check more than one)',
+      },
+      maritalStatus: {
+        'ui:title': 'Sponsor’s marital status',
+      },
+    }),
+  },
+};
+
+export const schema = {
+  type: 'object',
+  properties: {
+    application: {
+      type: 'object',
+      properties: {
+        veteran: {
+          type: 'object',
+          required: ['gender', 'race', 'maritalStatus'],
+          properties: pick(veteran.properties, [
+            'gender',
+            'race',
+            'maritalStatus',
+          ]),
+        },
+      },
+    },
+  },
+};
