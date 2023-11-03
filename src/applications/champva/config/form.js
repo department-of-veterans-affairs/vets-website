f893f0aeffc12371479de8c632303419a076b144
Michael Clement <michael.clement@adhocteam.us>
2023-11-03 15:26:03 -0500

    initial setup + a few pages

---
+++ src/applications/champva/config/form.js
@@ -0,0 +1,117 @@
+// import fullSchema from 'vets-json-schema/dist/10-10D-schema.json';
+
+import {
+  fullNameSchema,
+  fullNameUI,
+  ssnSchema,
+  ssnUI,
+  vaFileNumberSchema,
+  vaFileNumberUI,
+  addressSchema,
+  addressUI,
+} from 'platform/forms-system/src/js/web-component-patterns';
+
+import manifest from '../manifest.json';
+import IntroductionPage from '../containers/IntroductionPage';
+import ConfirmationPage from '../containers/ConfirmationPage';
+
+// const { } = fullSchema.properties;
+
+// const { } = fullSchema.definitions;
+
+/** @type {PageSchema} */
+const formConfig = {
+  rootUrl: manifest.rootUrl,
+  urlPrefix: '/',
+  // submitUrl: '/v0/api',
+  submit: () =>
+    Promise.resolve({ attributes: { confirmationNumber: '123123123' } }),
+  trackingPrefix: 'champva-10-10d-',
+  introduction: IntroductionPage,
+  confirmation: ConfirmationPage,
+  formId: '10-10D',
+  saveInProgress: {
+    // messages: {
+    //   inProgress: 'Your CHAMPVA benefits application (10-10D) is in progress.',
+    //   expired: 'Your saved CHAMPVA benefits application (10-10D) has expired. If you want to apply for CHAMPVA benefits, please start a new application.',
+    //   saved: 'Your CHAMPVA benefits application has been saved.',
+    // },
+  },
+  version: 0,
+  prefillEnabled: true,
+  savedFormMessages: {
+    notFound: 'Please start over to apply for CHAMPVA benefits.',
+    noAuth:
+      'Please sign in again to continue your application for CHAMPVA benefits.',
+  },
+  title: 'Application for CHAMPVA Benefits',
+  defaultDefinitions: {},
+  chapters: {
+    chapter1: {
+      title: 'Sponsor Information',
+      pages: {
+        page1: {
+          path: 'name',
+          title: 'Name',
+          uiSchema: {
+            veteransFullName: fullNameUI(),
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              veteransFullName: fullNameSchema,
+            },
+          },
+        },
+        page2: {
+          path: 'sponsor-ssn',
+          title: 'Social Security Number',
+          uiSchema: {
+            ssn: ssnUI(),
+          },
+          schema: {
+            type: 'object',
+            required: ['ssn'],
+            properties: {
+              ssn: ssnSchema,
+            },
+          },
+        },
+        page3: {
+          path: 'va-file-number',
+          title: 'VA File Number',
+          uiSchema: {
+            vaFileNumber: vaFileNumberUI(),
+          },
+          schema: {
+            type: 'object',
+            required: ['vaFileNumber'],
+            properties: {
+              vaFileNumber: vaFileNumberSchema,
+            },
+          },
+        },
+        page4: {
+          path: 'sponsor-address',
+          title: 'Address',
+          uiSchema: {
+            sponsorAddress: addressUI({
+              labels: {
+                militaryCheckbox:
+                  'Sponsor lives on a U.S. military base outside the U.S.',
+              },
+            }),
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              sponsorAddress: addressSchema(),
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+export default formConfig;
