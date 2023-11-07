8e2b3c9e79d8eae52a41d8e04bc524ca03bc4e9e
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2023-11-07 11:15:58 -0500

    Adding error display with conditional styling on neighboring elements

--- src/applications/representative/components/search/SearchControls.jsx
+++ src/applications/representative/components/search/SearchControls.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { focusElement } from 'platform/utilities/ui';
+import classNames from 'classnames';
 import { VaModal } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import RepTypeSelector from './RepTypeSelector';
 
@@ -106,27 +107,37 @@ const SearchControls = props => {
       />
       <form id="representative-search-controls" onSubmit={e => onSubmit(e)}>
         <div className="usa-width-two-thirds">
-          <h3 style={{ marginBottom: '1em' }}>Search for a representative</h3>
-          <div className="location-input-container">
+          <h3 className="vads-u-margin-bottom--0">
+            Search for a representative
+          </h3>
+          <div
+            className={classNames('location-input-container', {
+              'usa-input-error': showError,
+            })}
+          >
             <div className="location-input-header">
               <label
                 htmlFor="street-city-state-zip"
                 id="street-city-state-zip-label"
-                className="vads-u-margin-top--2"
+                className={showError ? null : 'vads-u-margin-bottom--1p5'}
+                style={showError ? null : { marginTop: '1em' }}
               >
                 City, state or postal code{' '}
                 <span className="form-required-span">(*Required)</span>
               </label>
             </div>
             {showError && (
-              <span className="usa-input-error-message" role="alert">
+              <span
+                className="usa-input-error-message"
+                style={{ whiteSpace: 'nowrap' }}
+                role="alert"
+              >
                 <span className="sr-only">Error</span>
-                Please fill in a city, state, or postal code.
+                Please fill in a city, state or postal code.
               </span>
             )}
             <input
               id="street-city-state-zip"
-              className="vads-u-margin-top--2"
               ref={locationInputFieldRef}
               name="street-city-state-zip"
               type="text"
@@ -135,7 +146,11 @@ const SearchControls = props => {
               value={locationInputString}
               title="Your location: Street, City, State or Postal code"
             />
-            <div className="use-my-location-button-container">
+            <div
+              className={classNames('use-my-location-button-container', {
+                'use-my-location-button-container-error': showError,
+              })}
+            >
               {geolocationInProgress ? (
                 <div className="finding-your-location-loading">
                   <i
