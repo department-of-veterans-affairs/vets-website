0d8760da3bfec2db45938fa319c4b0a2d5edd042
harshil1793 <harshil1793@gmail.com>
2023-11-07 10:36:53 -0500

    fix: fixed test case for debt letter.

--- src/applications/debt-letters/tests/e2e/diary-codes-content.cypress.spec.js
+++ src/applications/debt-letters/tests/e2e/diary-codes-content.cypress.spec.js
@@ -15,9 +15,16 @@ describe('Diary Codes', () => {
     cy.get('[data-testid="diary-codes-status"]').contains(
       'Status: We referred this debt to the U.S. Department of the Treasury.',
     );
-    cy.get('[data-testid="diary-code-080-next-step"]').contains(
-      'Next step: Call the U.S. Department of the Treasury’s Debt Management Center at888-826-3127, 8:30 a.m. to 6:30 p.m. ET. ',
-    );
+    cy.get('[data-testid="diary-code-080-next-step"]')
+      .should(
+        'contain',
+        'Next step: Call the U.S. Department of the Treasury’s Debt Management Center at',
+      )
+      .and(
+        'contain',
+        '8:30 a.m. to 6:30 p.m. ET. Don’t send us payment directly.',
+      );
+
     cy.axeCheck();
   });
 
