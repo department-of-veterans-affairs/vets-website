48eeac066a504a0427cfc859e922856088194d4d
Alex <AlexanderSherrow@gmail.com>
2023-11-08 09:56:16 -0500

    MBMS-51921 Added prod flag

--- src/platform/forms-system/src/js/review/ObjectField.jsx
+++ src/platform/forms-system/src/js/review/ObjectField.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { flow, groupBy } from 'lodash';
+import environment from 'platform/utilities/environment';
 
 import {
   getDefaultFormState,
@@ -197,7 +198,14 @@ class ObjectField extends React.Component {
             {defaultEditButton()}
           </div>
         )}
-        <Tag className="review">{renderedProperties}</Tag>
+        {!formContext?.hideHeaderRow &&
+          environment.isProduction() && (
+            <Tag className="review">{renderedProperties}</Tag>
+          )}
+        {!formContext?.hideHeaderRow &&
+          !environment.isProduction() && (
+            <Tag className="review-auto-margin">{renderedProperties}</Tag>
+          )}
       </>
     ) : (
       <>{renderedProperties}</>
