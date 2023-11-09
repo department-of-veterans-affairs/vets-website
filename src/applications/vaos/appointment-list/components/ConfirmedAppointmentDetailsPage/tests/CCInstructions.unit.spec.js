7296d6d90b5f3dfb4ab1940b647edc760937dcfb
jcy <julia.young@va.gov>
2023-11-09 14:17:23 -0500

    fixes

--- src/applications/vaos/appointment-list/components/ConfirmedAppointmentDetailsPage/tests/CCInstructions.unit.spec.js
+++ src/applications/vaos/appointment-list/components/ConfirmedAppointmentDetailsPage/tests/CCInstructions.unit.spec.js
@@ -4,26 +4,11 @@ import { renderWithStoreAndRouter } from '~/platform/testing/unit/react-testing-
 import { Toggler } from '~/platform/utilities/feature-toggles';
 import CCInstructions from '../CCInstructions';
 
-const appointmentData = {
-  start: '2024-07-19T08:00:00-07:00',
-  location: {
-    vistaId: '983',
-    clinicId: '848',
-    stationId: '983',
-    clinicName: 'CHY PC VAR2',
-  },
-};
-
 describe('CCInstructions component', () => {
-  const initialState = {
-    featureToggles: {
-      [Toggler.TOGGLE_NAMES.vaOnlineSchedulingDescriptiveBackLink]: true,
-    },
-  };
+  const initialState = {};
 
   it('should render comment in CCInstructions', async () => {
     const appointment = {
-      ...appointmentData,
       comment: 'Follow-up/Routine: I have a headache',
     };
 
@@ -47,7 +32,6 @@ describe('CCInstructions component', () => {
 
   it('should not render comment in CCInstructions', async () => {
     const appointment = {
-      ...appointmentData,
       comment: '',
     };
 
@@ -59,6 +43,11 @@ describe('CCInstructions component', () => {
     );
 
     // CCInstructions with no comment
-    expect(await wrapper).to.exist;
+    expect(
+      await wrapper.queryByRole('heading', {
+        level: 2,
+        name: 'Special instructions',
+      }),
+    ).to.be.null;
   });
 });
