806561cde66727be683abc69077b716d90c16774
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2023-11-09 14:18:27 -0700

    Remove platform schemaform-utils unit tests

--- src/applications/pensions/containers/UnauthenticatedWarningAlert.jsx
+++ src/applications/pensions/containers/UnauthenticatedWarningAlert.jsx
@@ -23,7 +23,7 @@ const UnauthenticatedWarningAlert = () => {
         This application is 7 steps long and it contains several substeps per
         step. We advise you{' '}
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#" onClick={showLoginModal}>
+        <a href="#" className="usa-link" onClick={showLoginModal}>
           sign in to save your progress
         </a>
         .
