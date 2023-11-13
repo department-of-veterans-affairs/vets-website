2bcbd8aac7055e6a6802f3d0e8d0de7e19c8a59f
Justin Lattimore <jlattimoreweb@gmail.com>
2023-11-13 14:09:45 -0600

    Removed Fragment from InfoAlert

--- src/applications/mhv/medical-records/components/shared/InfoAlert.jsx
+++ src/applications/mhv/medical-records/components/shared/InfoAlert.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { mhvUrl } from '~/platform/site-wide/mhv/utilities';
 import { isAuthenticatedWithSSOe } from '~/platform/user/authentication/selectors';
