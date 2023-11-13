a81c0da7405d6525492fe91a276b8c3b1eb55587
fatmakhan0395 <Fatma.khan@va.gov>
2023-11-13 07:53:36 -0800

    vfep-973 adding more tests coverage for branches gi

--- src/applications/gi/tests/containers/GiBillApp.unit.spec.jsx
+++ src/applications/gi/tests/containers/GiBillApp.unit.spec.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { expect } from 'chai';
 import { waitFor } from '@testing-library/react';
 import { mockConstants, renderWithStoreAndRouter } from '../helpers';
 
@@ -6,14 +7,32 @@ import GiBillApp from '../../containers/GiBillApp';
 
 describe('<GiBillApp>', () => {
   it('should render', async () => {
-    renderWithStoreAndRouter(<GiBillApp />, {
+    const screen = renderWithStoreAndRouter(<GiBillApp />, {
       initialState: {
         constants: mockConstants(),
       },
     });
-
     await waitFor(() => {
-      // expect(screen.getByRole('application')).to.be.ok;
+      expect(screen.getByRole('application')).to.be.ok;
+    });
+  });
+
+  it('should render after running shouldExitPreviewMode()', async () => {
+    const screen = renderWithStoreAndRouter(
+      <GiBillApp TESTVERSION TESTQUERYPARAMS={false} />,
+      {
+        initialState: {
+          preview: {
+            display: true,
+            version: {
+              id: null,
+            },
+          },
+        },
+      },
+    );
+    await waitFor(() => {
+      expect(screen.getByRole('application')).to.be.ok;
     });
   });
 });
