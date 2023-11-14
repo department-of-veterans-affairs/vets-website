4b9d95d82502c88cb19272f4c5c78107342ad0ee
John Woolschlager <wullaski@gmail.com>
2023-11-14 09:55:32 -0500

    Check in/62111/completion content link changes (#26674)
    * Remove the test button
    
    * Show travel reimbursement link for any number of appointments
    
    * Change title

--- src/applications/check-in/day-of/pages/Confirmation/index.jsx
+++ src/applications/check-in/day-of/pages/Confirmation/index.jsx
@@ -63,7 +63,6 @@ const Confirmation = props => {
       {appointment && (
         <CheckInConfirmation
           selectedAppointment={appointment}
-          appointments={appointments}
           triggerRefresh={refreshAppointments}
           router={router}
         />
