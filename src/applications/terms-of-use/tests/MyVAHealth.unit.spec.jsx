0488c479562e84bbb826a3b831d62651f9a6ad8b
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2023-11-14 16:47:56 -0500

    tests: fix MyVAHealth unit tests

--- src/applications/terms-of-use/tests/MyVAHealth.unit.spec.jsx
+++ src/applications/terms-of-use/tests/MyVAHealth.unit.spec.jsx
@@ -10,67 +10,73 @@ import MyVAHealth from '../components/MyVAHealth';
 
 const oldLocation = global.window.location;
 
-describe('MyVAHealth', () => {
-  const ssoeTarget = `https://staging-patientportal.myhealth.va.gov`;
-  let server;
-  let pushState;
-  let go;
+Array.from({ length: 200 }).forEach(() => {
+  describe('MyVAHealth', () => {
+    const ssoeTarget = `https://staging-patientportal.myhealth.va.gov`;
+    let server;
+    let pushState;
+    let go;
 
-  before(() => {
-    server = setupServer();
-    server.listen();
-  });
-
-  beforeEach(() => {
-    pushState = sinon.spy(global.window.history, 'pushState');
-    go = sinon.spy(global.window.history, 'go');
-  });
-
-  afterEach(() => {
-    server.resetHandlers();
-    global.window.location = oldLocation;
-  });
-
-  after(() => server.close());
-
-  it('should render', () => {
-    const { container } = render(<MyVAHealth />);
-
-    const loadingIndicator = $('va-loading-indicator', container);
-    expect(loadingIndicator).to.not.be.null;
-  });
-
-  it('should redirect to failure page with code=110 when api returns an error', async () => {
-    global.window.location = `https://dev.va.gov/terms-of-use/myvahealth/?ssoeTarget=${ssoeTarget}`;
-
-    server.use(
-      rest.put(
-        `https://dev-api.va.gov/v0/terms_of_use_agreements/update_provisioning`,
-        (_, res, ctx) => res(ctx.status(400), ctx.json({ errors: [] })),
-      ),
-    );
-
-    render(<MyVAHealth />);
-    await waitFor(() => {
-      expect(pushState.called).to.be.true;
-      expect(go.calledWith('/auth/login/callback/?auth=fail&code=110'));
+    before(() => {
+      server = setupServer();
+      server.listen();
     });
-  });
 
-  it('should redirect formatted redirect url when api returns 200', async () => {
-    global.window.location = `https://dev.va.gov/terms-of-use/myvahealth/?ssoeTarget=${ssoeTarget}`;
+    beforeEach(() => {
+      pushState = sinon.spy(global.window.history, 'pushState');
+      go = sinon.spy(global.window.history, 'go');
+    });
 
-    server.use(
-      rest.put(
-        `https://dev-api.va.gov/v0/terms_of_use_agreements/update_provisioning`,
-        (_, res, ctx) => res(ctx.status(200), ctx.json({ provisioned: true })),
-      ),
-    );
+    afterEach(() => {
+      server.resetHandlers();
+      global.window.location = oldLocation;
+    });
 
-    render(<MyVAHealth />);
+    after(() => server.close());
 
-    await waitFor(() => {
-      expect(global.window.location).to.eql(ssoeTarget);
+    it('should render', () => {
+      const { container } = render(<MyVAHealth />);
+
+      const loadingIndicator = $('va-loading-indicator', container);
+      expect(loadingIndicator).to.not.be.null;
+    });
+
+    it('should redirect to failure page with code=110 when api returns an error', async () => {
+      global.window.location = `https://dev.va.gov/terms-of-use/myvahealth/?ssoeTarget=${ssoeTarget}`;
+
+      server.use(
+        rest.put(
+          `https://dev-api.va.gov/v0/terms_of_use_agreements/update_provisioning`,
+          (_, res, ctx) => res(ctx.status(400), ctx.json({ errors: [] })),
+        ),
+      );
+
+      render(<MyVAHealth />);
+      await waitFor(
+        () => {
+          expect(pushState.called).to.be.true;
+          expect(go.calledWith('/auth/login/callback/?auth=fail&code=110'));
+        },
+        { timeout: 3500 },
+      );
+    });
+
+    it('should redirect formatted redirect url when api returns 200', async () => {
+      global.window.location = `https://dev.va.gov/terms-of-use/myvahealth/?ssoeTarget=${ssoeTarget}`;
+
+      server.use(
+        rest.put(
+          `https://dev-api.va.gov/v0/terms_of_use_agreements/update_provisioning`,
+          (_, res, ctx) =>
+            res(ctx.status(200), ctx.json({ provisioned: true })),
+        ),
+      );
+
+      render(<MyVAHealth />);
+
+      await waitFor(() => {
+        expect(global.window.location).to.eql(ssoeTarget);
+      });
     });
   });
 });
