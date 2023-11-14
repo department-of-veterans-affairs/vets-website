5385f86b91c3b4e4758ebebe0c28a748a56045db
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2023-11-14 09:54:27 -0700

    Remove combatSince911

--- src/applications/pensions/pages/generalHistory.js
+++ src/applications/pensions/pages/generalHistory.js
@@ -1,16 +1,9 @@
 import fullSchemaPensions from 'vets-json-schema/dist/21P-527EZ-schema.json';
-import moment from 'moment';
-import { createSelector } from 'reselect';
-import { isFullDate } from 'platform/forms/validations';
 
-import fullNameUI from 'platform/forms/definitions/fullName';
 import FullNameField from 'platform/forms-system/src/js/fields/FullNameField';
+import { fullNameUI } from 'platform/forms-system/src/js/web-component-patterns';
 
-const {
-  combatSince911,
-  placeOfSeparation,
-  previousNames,
-} = fullSchemaPensions.properties;
+const { placeOfSeparation, previousNames } = fullSchemaPensions.properties;
 
 /** @type {PageSchema} */
 export default {
@@ -26,33 +19,12 @@ export default {
         viewField: FullNameField,
         reviewTitle: 'Previous names',
       },
-      items: fullNameUI,
+      items: fullNameUI(),
     },
     placeOfSeparation: {
       'ui:title':
         'Place of last or anticipated separation (city and state or foreign country)',
     },
-    combatSince911: (() => {
-      const rangeExcludes911 = createSelector(
-        form => form.servicePeriods,
-        periods =>
-          (periods || []).every(
-            period =>
-              !period.activeServiceDateRange ||
-              !isFullDate(period.activeServiceDateRange.to) ||
-              !moment('2001-09-11').isBefore(period.activeServiceDateRange.to),
-          ),
-      );
-
-      return {
-        'ui:title': 'Did you serve in a combat zone after 9/11/2001?',
-        'ui:widget': 'yesNo',
-        'ui:required': formData => !rangeExcludes911(formData),
-        'ui:options': {
-          hideIf: rangeExcludes911,
-        },
-      };
-    })(),
   },
   schema: {
     type: 'object',
@@ -66,7 +38,6 @@ export default {
         minItems: 1,
       },
       placeOfSeparation,
-      combatSince911,
     },
   },
 };
