4c9b8c9fe138ab7be0db5d4e628505de8423819e
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2023-11-15 16:49:44 -0500

    Unit test for searchResult reducer

---
+++ src/applications/representative-search/tests/reducers/searchResult.unit.spec.js
@@ -0,0 +1,96 @@
+import { expect } from 'chai';
+
+import { FETCH_REPRESENTATIVES } from '../../utils/actionTypes';
+import { SearchResultReducer } from '../../reducers/searchResult';
+
+const INITIAL_STATE = {
+  results: [],
+  selectedResult: null,
+  pagination: {},
+};
+
+describe('representatives reducer', () => {
+  it('should handle fetching a list of representatives', () => {
+    const state = SearchResultReducer(INITIAL_STATE, {
+      type: FETCH_REPRESENTATIVES,
+      payload: {
+        data: [{ name: 'selectedResult1' }, { name: 'selectedResult2' }],
+        meta: {
+          pagination: {
+            currentPage: 1,
+          },
+        },
+      },
+    });
+
+    expect(state.results.length).to.eql(2);
+    expect(state.pagination.currentPage).to.eql(1);
+  });
+
+  it('should clear error after a successful search', () => {
+    const state = SearchResultReducer(
+      { ...INITIAL_STATE, error: true },
+      {
+        type: FETCH_REPRESENTATIVES,
+        payload: {
+          data: [{ name: 'selectedResult1' }, { name: 'selectedResult2' }],
+          meta: {
+            pagination: {
+              currentPage: 1,
+            },
+          },
+        },
+      },
+    );
+
+    expect(state.error).to.be.null;
+  });
+
+  it('should handle fetching state to build a search query object', () => {
+    const state = SearchResultReducer(INITIAL_STATE, {
+      type: FETCH_REPRESENTATIVES,
+      payload: {
+        data: [
+          {
+            attributes: {
+              name: 'Test representative',
+              representativeType: 'Test rep type',
+              lat: 40.7365270700001,
+              long: -73.97761421,
+              address: {
+                physical: {
+                  zip: '10010',
+                  city: 'New York',
+                  state: 'NY',
+                  address1: '123 East 33rd Street',
+                },
+              },
+            },
+          },
+        ],
+        meta: {
+          pagination: {
+            currentPage: 1,
+          },
+        },
+      },
+    });
+
+    expect(state.results.length).to.eql(1);
+    expect(state.results[0].attributes.name).to.eql('Test representative');
+    expect(state.results[0].attributes.representativeType).to.eql(
+      'Test rep type',
+    );
+    expect(state.results[0].attributes.lat).to.eql(40.7365270700001);
+    expect(state.results[0].attributes.long).to.eql(-73.97761421);
+    expect(state.results[0].attributes.address.physical.zip).to.eql('10010');
+    expect(state.results[0].attributes.address.physical.city).to.eql(
+      'New York',
+    );
+    expect(state.results[0].attributes.address.physical.state).to.eql('NY');
+    expect(state.results[0].attributes.address.physical.address1).to.eql(
+      '123 East 33rd Street',
+    );
+    expect(state.pagination.currentPage).to.eql(1);
+  });
+});
