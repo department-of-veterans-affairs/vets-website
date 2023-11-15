6b07b05b9707c6cb20e0a985b6146f0bbad187e8
Alex <AlexanderSherrow@gmail.com>
2023-11-15 12:14:50 -0500

    MBMS-52691 updated tests for new ui option

--- src/platform/forms-system/test/js/fields/TitleField.unit.spec.jsx
+++ src/platform/forms-system/test/js/fields/TitleField.unit.spec.jsx
@@ -11,6 +11,19 @@ describe('Schemaform <TitleField>', () => {
     );
     expect(tree.subTree('legend')).not.to.be.false;
   });
+
+  it('should render h3 for root', () => {
+    const useHeaderStyling = true;
+    const tree = SkinDeep.shallowRender(
+      <TitleField
+        id="root__title"
+        title="foo"
+        useHeaderStyling={useHeaderStyling}
+      />,
+    );
+    expect(tree.subTree('h3')).not.to.be.false;
+  });
+
   it('should render subtitle for non-root', () => {
     const Foo = <div>Foo</div>;
     const tree = SkinDeep.shallowRender(
