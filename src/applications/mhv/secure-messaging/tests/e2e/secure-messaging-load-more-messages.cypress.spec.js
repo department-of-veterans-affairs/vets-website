b333516f791a81105083493adf05fd62b36bf61d
fazilqa <fazil.mammadov@bylight.com>
2023-11-16 13:31:30 -0800

    update loadSingleThread method

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-load-more-messages.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-load-more-messages.cypress.spec.js
@@ -1,38 +1,18 @@
 import SecureMessagingSite from './sm_site/SecureMessagingSite';
 import PatientInboxPage from './pages/PatientInboxPage';
-import { AXE_CONTEXT, Paths } from './utils/constants';
-import mockMessages from './fixtures/messages-response.json';
-import mockFolders from './fixtures/folder-response.json';
+import { AXE_CONTEXT } from './utils/constants';
 import mockThread from './fixtures/thread-response.json';
 import mockFirstMessage from './fixtures/first-message-from-thread-response.json';
 
 describe('Secure Messaging Inbox Message Sort', () => {
+  const landingPage = new PatientInboxPage();
+  const site = new SecureMessagingSite();
   beforeEach(() => {
-    const landingPage = new PatientInboxPage();
-    const site = new SecureMessagingSite();
     site.login();
     landingPage.loadInboxMessages();
   });
   it('load more messages', () => {
-    cy.intercept(
-      'GET',
-      `${Paths.SM_API_BASE + Paths.FOLDERS}*`,
-      mockFolders,
-    ).as('folders');
-    cy.intercept(
-      'GET',
-      `${Paths.SM_API_EXTENDED}/${
-        mockMessages.data[0].attributes.messageId
-      }/thread`,
-      mockThread,
-    ).as('full-thread');
-    cy.intercept(
-      'GET',
-      `${Paths.SM_API_EXTENDED}/${mockThread.data[0].attributes.messageId}`,
-      mockFirstMessage,
-    ).as('fist-message-in-thread');
-
-    cy.contains(mockMessages.data[0].attributes.subject).click();
+    landingPage.loadSingleThread();
 
     cy.focused().should(
       'contain.text',
