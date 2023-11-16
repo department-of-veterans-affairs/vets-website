f0d867a6d3e903c0b39e587214ade3ca5a3f5406
Tony Gravell <anthony.gravell@bylight.com>
2023-11-16 11:44:28 -0500

    updated unit test

--- src/applications/mhv/medications/components/MedicationsList/MedicationsListCard.jsx
+++ src/applications/mhv/medications/components/MedicationsList/MedicationsListCard.jsx
@@ -45,9 +45,9 @@ const MedicationsListCard = props => {
             id="status"
             className="vads-u-margin-top--1p5 vads-u-font-weight--bold"
           >
-            {!rx.dispStatus.includes('Refill in Process')
+            {rx.dispStatus !== 'Active: Refill in Process'
               ? rx.dispStatus
-              : 'Refill in process'}
+              : 'Active: Refill in process'}
           </div>
         )}
         {rx && <ExtraDetails {...rx} />}
