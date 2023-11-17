ec5c3a43c499b8b81fd268f19104e6a5d10b4731
fazilqa <fazil.mammadov@bylight.com>
2023-11-17 12:09:46 -0800

    delete loop

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-load-more-messages.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-load-more-messages.cypress.spec.js
@@ -4,47 +4,45 @@ import { AXE_CONTEXT } from './utils/constants';
 import mockThread from './fixtures/thread-response.json';
 import mockFirstMessage from './fixtures/first-message-from-thread-response.json';
 
-for (let i = 0; i < 200; i += 1) {
-  describe('Secure Messaging Inbox Message Sort', () => {
-    const landingPage = new PatientInboxPage();
-    const site = new SecureMessagingSite();
-    beforeEach(() => {
-      site.login();
-      landingPage.loadInboxMessages();
-    });
-    it('load more messages', () => {
-      landingPage.loadSingleThread();
+describe('Secure Messaging Inbox Message Sort', () => {
+  const landingPage = new PatientInboxPage();
+  const site = new SecureMessagingSite();
+  beforeEach(() => {
+    site.login();
+    landingPage.loadInboxMessages();
+  });
+  it('load more messages', () => {
+    landingPage.loadSingleThread();
 
-      cy.focused().should(
-        'contain.text',
-        mockFirstMessage.data.attributes.subject,
-      );
-      cy.get('va-accordion-item')
-        .should('have.length', 5)
-        .as('quantity-before-expanding');
+    cy.focused().should(
+      'contain.text',
+      mockFirstMessage.data.attributes.subject,
+    );
+    cy.get('va-accordion-item')
+      .should('have.length', 5)
+      .as('quantity-before-expanding');
 
-      cy.contains('more messages').click();
+    cy.contains('more messages').click();
 
-      cy.get('va-accordion-item')
-        .should('have.length', 10)
-        .as('quantity-after-first-expanding');
+    cy.get('va-accordion-item')
+      .should('have.length', 10)
+      .as('quantity-after-first-expanding');
 
-      cy.contains('more messages').click();
+    cy.contains('more messages').click();
 
-      cy.get('va-accordion-item')
-        .should('have.length', mockThread.data.length)
-        .as('quantity-after-last-expanding');
+    cy.get('va-accordion-item')
+      .should('have.length', mockThread.data.length)
+      .as('quantity-after-last-expanding');
 
-      cy.contains('more messages').should('not.exist');
+    cy.contains('more messages').should('not.exist');
 
-      cy.injectAxe();
-      cy.axeCheck(AXE_CONTEXT, {
-        rules: {
-          'aria-required-children': {
-            enabled: false,
-          },
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
         },
-      });
+      },
     });
   });
-}
+});
