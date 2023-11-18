70721e52bfd017c8a96f4ea871cb3f5644b795b7
Tze-chiu Lei <tclei2009@gmail.com>
2023-11-18 14:51:09 -0700

    Add placeOfBirth field to 20-10206 pers-info page

--- src/applications/simple-forms/20-10206/pages/personalInfo.js
+++ src/applications/simple-forms/20-10206/pages/personalInfo.js
@@ -3,21 +3,40 @@ import {
   fullNameUI,
   dateOfBirthSchema,
   dateOfBirthUI,
+  inlineTitleSchema,
+  inlineTitleUI,
   titleUI,
 } from 'platform/forms-system/src/js/web-component-patterns';
+import VaTextInputField from 'platform/forms-system/src/js/web-component-fields/VaTextInputField';
 
 /** @type {PageSchema} */
 export default {
   uiSchema: {
     ...titleUI('Your name and date of birth'),
     fullName: fullNameUI(),
+    'view:birthTitle': inlineTitleUI('Your date and place of birth'),
     dateOfBirth: dateOfBirthUI(),
+    placeOfBirth: {
+      'ui:title': 'Place of birth',
+      'ui:webComponentField': VaTextInputField,
+      'ui:options': {
+        hint: 'City and state or foreign country',
+      },
+      'ui:errorMessages': {
+        required: 'Please enter your place of birth',
+      },
+    },
   },
   schema: {
     type: 'object',
     properties: {
       fullName: fullNameSchema,
+      'view:birthTitle': inlineTitleSchema,
       dateOfBirth: dateOfBirthSchema,
+      placeOfBirth: {
+        type: 'string',
+      },
     },
+    required: ['fullName', 'dateOfBirth', 'placeOfBirth'],
   },
 };
