f1fd40524972b1c17f52cd77a5e37a5cb79f8e36
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2023-11-20 15:52:21 -0500

    Cypress test - mobile

---
+++ src/applications/representative-search/tests/e2e/mobile.cypress.spec.js
@@ -0,0 +1,58 @@
+import mockRepresentativeData from '../../constants/mock-representative-data.json';
+import mockGeocodingData from '../../constants/mock-geocoding-data.json';
+
+Cypress.Commands.add('checkSearch', () => {
+  cy.axeCheck();
+
+  // Search
+  cy.get('input[name="City, State or Postal code"]', { timeout: 5000 })
+    .should('exist')
+    .should('not.be.disabled')
+    .clear({ force: true });
+
+  cy.get('#street-city-state-zip')
+    .find('input[type="text"]')
+    .type('Austin, TX');
+  cy.get('#representative-search').click();
+
+  // Search title
+  cy.get('#search-results-subheader', { timeout: 5000 }).should('exist');
+
+  // Result list
+  cy.get('.representative-results-list').should('exist');
+});
+
+describe('Mobile', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/v0/feature_toggles?*', []);
+    cy.intercept('GET', '/v0/maintenance_windows', []);
+    cy.intercept(
+      'GET',
+      '/services/veteran/v0/accredited_representatives',
+      mockRepresentativeData,
+    );
+    cy.intercept('GET', '/geocoding/**/*', mockGeocodingData);
+  });
+
+  it('should render in mobile layouts', () => {
+    cy.visit('/get-help-from-accredited-representative/find-rep/');
+    cy.axeCheck();
+    cy.injectAxe();
+
+    // iPhone X
+    cy.viewport(400, 812);
+    cy.checkSearch();
+
+    // iPhone 6/7/8 plus
+    cy.viewport(414, 736);
+    cy.checkSearch();
+
+    // Pixel 2
+    cy.viewport(411, 731);
+    cy.checkSearch();
+
+    // Galaxy S5/Moto
+    cy.viewport(360, 640);
+    cy.checkSearch();
+  });
+});
