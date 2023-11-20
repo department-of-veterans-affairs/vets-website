4d390ff53d5076b0b40a4572f8654231893dd8db
jcy <julia.young@va.gov>
2023-11-20 15:11:07 -0500

    Hook error

--- src/applications/vaos/appointment-list/components/AppointmentsPageV2/tests/AppointmentCard.unit.spec.js
+++ src/applications/vaos/appointment-list/components/AppointmentsPageV2/tests/AppointmentCard.unit.spec.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { expect } from 'chai';
+import { focusElement } from 'platform/utilities/ui';
 import { renderWithStoreAndRouter } from '~/platform/testing/unit/react-testing-library-helpers';
 import AppointmentCard from '../AppointmentCard';
 import { Facility } from '../../../../tests/mocks/unit-test-helpers';
+import { SPACE_BAR } from '../../../../utils/constants';
+import { getVAAppointmentLocationId } from '../../../../services/appointment';
+import { useHistory } from 'react-router-dom';
 
 const appointmentData = {
   start: '2024-07-19T12:00:00Z',
@@ -17,7 +21,25 @@ const appointmentData = {
     clinicName: 'CHY PC VAR2',
   },
 };
+
 const facilityData = new Facility();
+function handleClick({ history, link, idClickable }) {
+  return () => {
+    if (!window.getSelection().toString()) {
+      focusElement(`#${idClickable}`);
+      history.push(link);
+    }
+  };
+}
+
+function handleKeyDown({ history, link, idClickable }) {
+  return event => {
+    if (!window.getSelection().toString() && event.keyCode === SPACE_BAR) {
+      focusElement(`#${idClickable}`);
+      history.push(link);
+    }
+  };
+}
 
 describe('AppointmentCard component', () => {
   const initialState = { featureToggles: {} };
@@ -36,10 +58,24 @@ describe('AppointmentCard component', () => {
       },
     };
 
-    const props = { appointment, facilityData };
-    const wrapper = renderWithStoreAndRouter(<AppointmentCard {...props} />, {
-      initialState,
-    });
+    const history = useHistory();
+    const facilityId = getVAAppointmentLocationId(appointment);
+    const link = <a href="#">Test Link</a>;
+    const idClickable = `id-${appointment.id.replace('.', '\\.')}`;
+
+    // const props = { appointment, facilityData };
+    const wrapper = renderWithStoreAndRouter(
+      <AppointmentCard
+        appointment={appointment}
+        facility={facilityData[facilityId]}
+        link={link}
+        handleClick={() => handleClick({ history, link, idClickable })}
+        handleKeyDown={() => handleKeyDown({ history, link, idClickable })}
+      />,
+      {
+        initialState,
+      },
+    );
 
     expect(
       await wrapper.queryByRole('heading', {
