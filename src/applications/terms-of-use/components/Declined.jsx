fbd16891af33e79551b3f1302c8a4c01f4a34fb7
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2023-11-20 11:40:04 -0500

    updates tou declined + unit tests

--- src/applications/terms-of-use/components/Declined.jsx
+++ src/applications/terms-of-use/components/Declined.jsx
@@ -1,16 +1,29 @@
 import React, { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
+import { COOKIES, CLIENT_IDS } from 'platform/utilities/oauth/constants';
 import { toggleLoginModal } from 'platform/site-wide/user-nav/actions';
 import SubmitSignInForm from 'platform/static-data/SubmitSignInForm';
+import { touStyles } from '../helpers';
 
 export default function Declined() {
+  const shouldRedirectToMobile =
+    sessionStorage.getItem(COOKIES.CI) === CLIENT_IDS.VAMOBILE;
   const dispatch = useDispatch();
-  const openSignInModal = useCallback(() => dispatch(toggleLoginModal(true)), [
-    dispatch,
-  ]);
+  const openSignInModal = useCallback(
+    () => {
+      if (shouldRedirectToMobile) {
+        sessionStorage.removeItem(COOKIES.CI);
+        window.location = `vamobile://login-terms-rejected`;
+      } else {
+        dispatch(toggleLoginModal(true));
+      }
+    },
+    [dispatch, shouldRedirectToMobile],
+  );
 
   return (
     <div className="vads-l-grid-container vads-u-padding-y--5">
+      {shouldRedirectToMobile && <style>{touStyles}</style>}
       <div className="usa-content">
         <h1>We’ve signed you out</h1>
         <p>
@@ -28,9 +41,7 @@ export default function Declined() {
           accept the terms, sign in again. We’ll take you back to the terms of
           use. Then you can continue using VA online services.
         </p>
-        <button type="button" onClick={openSignInModal}>
-          Sign in
-        </button>
+        <va-button text="Sign in" onClick={openSignInModal} />
       </div>
     </div>
   );
