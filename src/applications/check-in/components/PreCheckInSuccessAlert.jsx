87544142d49f41189c96ca8285a5745b3ae7149d
Brian Seek <brian.seek@civicactions.com>
2023-11-20 13:20:52 -0800

    dismissible alert (#26783)
    * utilized additionalContext to store alert state
    
    * Added tests and tweaks to support tests.

--- src/applications/check-in/components/PreCheckInSuccessAlert.jsx
+++ src/applications/check-in/components/PreCheckInSuccessAlert.jsx
@@ -1,18 +1,24 @@
 import React, { useMemo } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { VaAlert } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { useSendPreCheckInData } from '../hooks/useSendPreCheckInData';
-
-import { makeSelectVeteranData } from '../selectors';
+import { additionalContext } from '../actions/day-of';
+import { makeSelectVeteranData, makeSelectCurrentContext } from '../selectors';
 import { hasPhoneAppointments } from '../utils/appointment';
 
 const PreCheckInSuccessAlert = () => {
   const selectVeteranData = useMemo(makeSelectVeteranData, []);
   const { appointments } = useSelector(selectVeteranData);
   const { t } = useTranslation();
+  const dispatch = useDispatch();
 
+  const hideAlert = () => {
+    dispatch(additionalContext({ showPreCheckInSuccess: false }));
+  };
   const { isLoading } = useSendPreCheckInData();
-
+  const selectCurrentContext = useMemo(makeSelectCurrentContext, []);
+  const { showPreCheckInSuccess } = useSelector(selectCurrentContext);
   const successMessage = hasPhoneAppointments(appointments)
     ? t('your-provider-will-call-you-at-your-appointment-time')
     : t('you-can-check-in-with-your-smartphone', {
@@ -29,18 +35,20 @@ const PreCheckInSuccessAlert = () => {
   }
 
   return (
-    <section data-testid="pre-check-in-success-alert">
-      <va-alert
+    <section>
+      <VaAlert
         close-btn-aria-label="Close notification"
         closeable
         status="success"
-        visible
+        visible={showPreCheckInSuccess !== false}
+        onCloseEvent={hideAlert}
+        data-testid="pre-check-in-success-alert"
       >
         <h2 slot="headline">{t('your-information-is-up-to-date')}</h2>
         <p data-testid="success-message" className="vads-u-margin-y--0">
           {successMessage}
         </p>
-      </va-alert>
+      </VaAlert>
     </section>
   );
 };
