04727440c7ce143ff5d816f5eefdacad364ccc07
Robin Garrison <robin.garrison@adhocteam.us>
2023-11-21 08:06:11 -0600

    Fix unit test

--- src/applications/appeals/996/validations/issues.js
+++ src/applications/appeals/996/validations/issues.js
@@ -1,8 +1,8 @@
-import { errorMessages } from '../constants';
+import { issueErrorMessages } from '../content/addIssue';
 import { MAX_LENGTH } from '../../shared/constants';
 
 export const maxNameLength = (error, data) => {
   if (data.length > MAX_LENGTH.ISSUE_NAME) {
-    error.addError(errorMessages.maxLength(MAX_LENGTH.ISSUE_NAME));
+    error.addError(issueErrorMessages.maxLength);
   }
 };
