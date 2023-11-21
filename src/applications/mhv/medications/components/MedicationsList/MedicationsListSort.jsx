0630be1e8ce8f391d9eb666eecf60c3383f6779e
kolbyVA <kolby.balch@va.gov>
2023-11-21 15:12:56 -0600

    [#mhv-51441] returns user to first page on sorting

--- src/applications/mhv/medications/components/MedicationsList/MedicationsListSort.jsx
+++ src/applications/mhv/medications/components/MedicationsList/MedicationsListSort.jsx
@@ -1,10 +1,13 @@
 import { VaSelect } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { waitForRenderThenFocus } from '@department-of-veterans-affairs/platform-utilities/ui';
+import { useHistory } from 'react-router-dom';
 import { rxListSortingOptions } from '../../util/constants';
 
 const MedicationsListSort = props => {
   const { value, sortRxList } = props;
+  const history = useHistory();
   const [sortListOption, setSortListOption] = useState(value);
 
   const rxSortingOptions = Object.keys(rxListSortingOptions);
@@ -33,6 +36,12 @@ const MedicationsListSort = props => {
           className="vads-u-line-height--4"
           onClick={() => {
             sortRxList(sortListOption);
+            history.push(`/1`);
+            waitForRenderThenFocus(
+              "[data-testid='page-total-info']",
+              document,
+              500,
+            );
           }}
         >
           Sort
