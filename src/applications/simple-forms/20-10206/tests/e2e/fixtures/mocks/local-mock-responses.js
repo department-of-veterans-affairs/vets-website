e44d987e016341f6d1c6a56d8b5e1eb0ecb192d0
Tze-chiu Lei <tclei2009@gmail.com>
2023-11-22 11:07:59 -0700

    Fix 20-10206 prefill/sip local mocks

--- src/applications/simple-forms/20-10206/tests/e2e/fixtures/mocks/local-mock-responses.js
+++ src/applications/simple-forms/20-10206/tests/e2e/fixtures/mocks/local-mock-responses.js
@@ -1,6 +1,8 @@
 /* eslint-disable camelcase */
 // use this file to mock api responses for local development
 // yarn mock-api --responses ./src/applications/simple-forms/21-0845/tests/e2e/fixtures/mocks/local-mock-responses.js
+const mockInProgressForms = require('./in-progress-forms.json');
+const mockPrefill = require('./prefill.json');
 const mockSubmit = require('../../../../../shared/tests/e2e/fixtures/mocks/application-submit.json');
 
 const responses = {
@@ -114,6 +116,9 @@ const responses = {
       ],
     },
   },
+
+  'GET /v0/in_progress_forms/20-10206': mockPrefill,
+  'PUT /v0/in_progress_forms/20-10206': mockInProgressForms,
 
   'POST /simple_forms_api/v1/simple_forms': mockSubmit,
 };
