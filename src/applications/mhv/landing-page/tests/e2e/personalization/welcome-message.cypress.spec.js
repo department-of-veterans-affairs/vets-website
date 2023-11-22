0a67fb3aabb4b7e0ecb1ddc317ba23654c1a06cc
Richard Davis <richard.davis@agile6.com>
2023-11-22 14:42:37 -0500

    updates e2e spec

--- src/applications/mhv/landing-page/tests/e2e/personalization/welcome-message.cypress.spec.js
+++ src/applications/mhv/landing-page/tests/e2e/personalization/welcome-message.cypress.spec.js
@@ -1,15 +1,22 @@
 import { appName, rootUrl } from '../../../manifest.json';
-import ApiInitializer from '../utilities/ApiInitializer';
-
+import { generateFeatureToggles } from '../../../mocks/api/feature-toggles';
 import user from '../../fixtures/user.json';
+import vamcEhr from '../../fixtures/vamc-ehr.json';
+import ApiInitializer from '../utilities/ApiInitializer';
 
 describe(`${appName} -- Welcome message`, () => {
   beforeEach(() => {
-    ApiInitializer.initializeFeatureToggle.withAllFeatures();
+    cy.intercept('GET', '/data/cms/vamc-ehr.json', vamcEhr).as('vamcEhr');
+    cy.intercept('GET', '/v0/feature_toggles*', generateFeatureToggles()).as(
+      'featureToggles',
+    );
+    const redirectUrl = 'https://**.va.gov/mhv-portal**/**';
+    cy.intercept('GET', redirectUrl, '').as('mhvRedirect');
+    ApiInitializer.initializeMessageData.withUnreadMessages();
   });
 
   describe('default user', () => {
-    it('renders the Welcome component', () => {
+    it('renders a Welcome message', () => {
       cy.login(user);
       cy.visit(rootUrl);
       cy.findByRole('heading', { level: 2, name: /^Welcome/ });
