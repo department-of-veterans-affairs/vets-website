d111005edd7dd15653eae707a6cd59d11c5d4cdc
Richard Davis <richard.davis@agile6.com>
2023-11-28 15:28:07 -0500

    use more specific test assertion

--- src/applications/mhv/landing-page/tests/components/LandingPage.unit.spec.jsx
+++ src/applications/mhv/landing-page/tests/components/LandingPage.unit.spec.jsx
@@ -33,8 +33,8 @@ describe('LandingPage component', () => {
 
   it('shows the Welcome component, when enabled', () => {
     const initialState = stateFn({ mhv_landing_page_personalization: true });
-    const { getByText } = setup({ initialState });
-    getByText(/Welcome/);
+    const { getByRole } = setup({ initialState });
+    getByRole('heading', { level: 2, name: /Welcome/ });
   });
 
   it('shows an alert when user has no facilities (aka no health data)', () => {
