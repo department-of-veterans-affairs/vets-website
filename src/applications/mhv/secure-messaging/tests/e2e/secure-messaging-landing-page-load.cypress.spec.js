d5c1da85853d34760bf4b4fef82c493f4779e6ec
fazilqa <fazil.mammadov@bylight.com>
2023-11-28 13:18:00 -0800

    add axecheck

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-landing-page-load.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-landing-page-load.cypress.spec.js
@@ -21,25 +21,65 @@ describe('SM main page', () => {
   });
 
   it('verify headers', () => {
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
     SecureMessagingLandingPage.verifyHeader();
     SecureMessagingLandingPage.verifyUnreadMessagesNote();
   });
 
   it('verify main topics', () => {
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
     SecureMessagingLandingPage.verifyWelcomeMessage();
     SecureMessagingLandingPage.verifyFaqMessage();
   });
 
   it('verify faq accordions', () => {
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
     SecureMessagingLandingPage.verifyFaqAccordions();
   });
 
   it('verify "Start a new message" link', () => {
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
     cy.get(Locators.LINKS.CREATE_NEW_MESSAGE).click();
     cy.location('pathname').should('contain', 'new-message');
   });
 
   it('verify "Go to the inbox" link', () => {
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
     cy.get(Locators.LINKS.GO_TO_INBOX).click({ force: true });
     cy.location('pathname').should('contain', 'inbox');
   });
@@ -50,8 +90,16 @@ describe('SM main page without API calls', () => {
     const site = new SecureMessagingSite();
     site.login();
     cy.visit('my-health/secure-messages/');
+    cy.injectAxe();
 
     cy.get(Locators.LINKS.GO_TO_INBOX).should('be.visible');
     cy.get(Locators.LINKS.CREATE_NEW_MESSAGE).should('be.visible');
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
   });
 });
