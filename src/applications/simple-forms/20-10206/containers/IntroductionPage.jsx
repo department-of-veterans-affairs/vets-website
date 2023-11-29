3ccb65957713db34100dc8c5a6c902a3d8a804d4
Tze-chiu Lei <tclei2009@gmail.com>
2023-11-28 19:54:07 -0700

    Add 20-10206 Introduction page conditional content

--- src/applications/simple-forms/20-10206/containers/IntroductionPage.jsx
+++ src/applications/simple-forms/20-10206/containers/IntroductionPage.jsx
@@ -2,15 +2,9 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
+import { isLOA3, isLoggedIn } from 'platform/user/selectors';
+import { connect } from 'react-redux';
 import { IntroductionPageView } from '../../shared/components/IntroductionPageView';
-
-const content = {
-  formTitle: 'Request personal records',
-  formSubTitle:
-    'Freedom of Information Act (FOIA) and Privacy Act (PA) Request (VA Form 20-10206)',
-  authStartFormText: 'Start filling out your request',
-  unauthStartText: 'Sign in to start filling out your request',
-};
 
 const ombInfo = {
   resBurden: '5',
@@ -18,125 +12,193 @@ const ombInfo = {
   expDate: '08/31/2026',
 };
 
-const childContent = (
-  <>
-    <p>
-      Use this form to access personal military, compensation, pension, or
-      benefit records.
-    </p>
-    <h2>What to know before submitting your request</h2>
-    <p>
-      This form is for requesting access to personal VA records{' '}
-      <strong>only</strong>. If you’re seeking general information about VA
-      policies or programs, or if you’re a third-party or power of attorney
-      request on behalf of someone else,{' '}
-      <a
-        href="https://www.va.gov/FOIA/index.asp"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        submit a FOIA request (opens in new tab)
-      </a>
-      .
-    </p>
-    <va-additional-info trigger="What is a Privacy Act (PA) request?">
-      <div>
-        <p>
-          The Privacy Act provides a citizen of the United States or an alien
-          lawfully admitted for permanent residence the right to request access
-          or amend records on themself from a System of Records (SORs). Examples
-          of PA records are personal Claims Files (C-File), educational loan,
-          and beneficiary records.
-        </p>
-        <p>
-          <a
-            href="https://department.va.gov/privacy/privacy-act-requests/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn more about filing PA requests (opens in new tab)
-          </a>
-        </p>
-      </div>
-    </va-additional-info>
-    <h2>Who can submit this request?</h2>
-    <p>
-      If you’re looking for your own records, you have the option to sign in to
-      your account to complete and submit the request online. Your
-      identification will need to be verified, so we encourage you to use a{' '}
-      <strong>Login.gov</strong> or <strong>ID.me</strong> account.
-    </p>
-    <p>
-      <a href="https://www.va.gov/resources/verifying-your-identity-on-vagov/">
-        Learn more about verified accounts
-      </a>
-    </p>
-    <div className="vads-u-margin-y--2">
-      <va-alert status="warning" visible>
-        <div className="vads-u-margin-y--0">
-          Only use the online form if you’re requesting your own records
-        </div>
-      </va-alert>
-    </div>
-    <div className="vads-u-margin-y--2">
-      <va-additional-info trigger="How do I request personal records for someone else?">
-        <div>
-          <p>
-            If you’re asking for someone else’s records, submit a FOIA request
-            (opens in new tab). You’ll need to have proper authorization on
-            record for your request to be processed:
-          </p>
-          <p>A third-party representative</p>
-          <ul>
-            <li>
-              A third-party may be a family member or other assigned person who
-              is not a power of attorney, agent, or fiduciary.{' '}
-              <a href="https://www.va.gov/FOIA/index.asp">
-                VA Form 21-0845 Authorization to Disclose Personal Information
-                to a Third-Party (link opens in new tab)
-              </a>{' '}
-              must be on record to fill out this form on behalf of a Veteran or
-              non-Veteran claimant.
-            </li>
-          </ul>
-          <p>A power of attorney</p>
-          <ul>
-            <li>
-              A power of attorney must have{' '}
-              <a
-                href="https://www.va.gov/find-forms/about-form-21-22/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                VA Form 21-22 Appointment of Veterans Service Organization as
-                Claimant’s Representative (opens in new tab)
-              </a>
-              , or{' '}
-              <a
-                href="https://www.va.gov/find-forms/about-form-21-22a/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                VA Form 21-22a Appointment of Individual as Claimant’s
-                Representative (link opens in new tab)
-              </a>{' '}
-              on record to fill out this form on behalf of a Veteran or
-              non-Veteran claimant.
-            </li>
-          </ul>
-        </div>
-      </va-additional-info>
-    </div>
-  </>
-);
+export const IntroductionPage = ({ route, userIdVerified, userLoggedIn }) => {
+  const content = {
+    formTitle: 'Request personal records',
+    formSubTitle:
+      'Freedom of Information Act (FOIA) and Privacy Act (PA) Request (VA Form 20-10206)',
+    authStartFormText: 'Start filling out your request',
+    hideSipIntro: userLoggedIn && !userIdVerified,
+  };
 
-export const IntroductionPage = ({ route }) => {
+  const childContent = (
+    <>
+      <p>
+        Use this form to access personal military, compensation, pension, or
+        benefit records.
+      </p>
+      <h2>What to know before submitting your request</h2>
+      <p>
+        If you want to request your records through this online form, you’ll
+        need to sign in to your account. We’ll need to verify your identity, so
+        we encourage you to use a Login.gov or ID.me account.
+      </p>
+      <p>
+        <strong>Note</strong>: You need to be a U.S. citizen or a legal
+        permanent resident to access your records under the Privacy Act (PA).
+      </p>
+      <p>
+        <a
+          href="https://www.va.gov/FOIA/index.asp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          submit a FOIA request (opens in new tab)
+        </a>
+        .
+      </p>
+      <p>Or you can submit a PDF version of this form.</p>
+      <h2>What type of information can I request?</h2>
+      <p>You can request any of these kinds of personal records:</p>
+      <p className="vads-u-font-weight--bold">
+        Compensation and pension records
+      </p>
+      <ul>
+        <li>
+          Certificate of Release or Discharge from Active Duty (DD Form 214)
+        </li>
+        <li>Claims file (C-file)</li>
+        <li>Disability examinations (C&P exams)</li>
+        <li>Official military personnel file (OMPF)</li>
+        <li>Pension benefit documents</li>
+        <li>Service or military treatment</li>
+        <li>Other compensation and pension records</li>
+      </ul>
+      <p className="vads-u-font-weight--bold">Benefit records</p>
+      <ul>
+        <li>Education benefit</li>
+        <li>Fiduciary services</li>
+        <li>Financial records</li>
+        <li>Home loan benefit</li>
+        <li>Life insurance benefit</li>
+        <li>Vocational rehabilitation and employment</li>
+        <li>Other benefit record</li>
+      </ul>
+      <div className="vads-u-margin-top--4">
+        <va-alert status="warning" uswds visible>
+          <div className="vads-u-margin-y--0">
+            Only use the online form if you’re requesting your own records
+          </div>
+        </va-alert>
+      </div>
+      <div className="vads-u-margin-y--4">
+        <va-additional-info trigger="How do I request personal records for someone else?">
+          <div>
+            <p>
+              If you’re asking for someone else’s records,{' '}
+              <a
+                href="https://www.va.gov/FOIA/index.asp"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                submit a FOIA request (opens in new tab)
+              </a>
+              . You’ll need to have proper authorization on record for your
+              request to be processed.
+            </p>
+            <ul>
+              <li>
+                If you’re a third-party representative (a family member or other
+                assigned person who is not a power of attorney, agent, or
+                fiduciary) requesting VA records for someone else, we must have
+                an authorization form on record (VA Form 21-0845) for us to
+                release their information.
+                <a
+                  href="https://www.va.gov/find-forms/about-form-21-22/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="vads-u-display--block vads-u-margin-top--2"
+                >
+                  Go to VA Form 21-22 Appointment of Veterans Service
+                  Organization as Claimant’s Representative (opens in new tab)
+                </a>
+              </li>
+              <li className="vads-u-margin-top--2">
+                If you’re a power of attorney requesting VA records for someone
+                else, we must have an official record that you were appointed as
+                their representative (VA Form 21-22 or VA Form 21-22a).
+                <a
+                  href="https://www.va.gov/find-forms/about-form-21-22/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="vads-u-display--block vads-u-margin-top--2"
+                >
+                  Go to VA Form 21-22 Appointment of Veterans Service
+                  Organization as Claimant’s Representative (opens in new tab)
+                </a>
+                <a
+                  href="https://www.va.gov/find-forms/about-form-21-22a/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="vads-u-display--block vads-u-margin-top--2"
+                >
+                  Go to VA Form 21-22a Appointment of Individual as Claimant’s
+                  Representative (opens in new tab)
+                </a>
+              </li>
+            </ul>
+          </div>
+        </va-additional-info>
+      </div>
+      {userLoggedIn &&
+        !userIdVerified && (
+          <div className="id-not-verified-content vads-u-margin-top--4">
+            <va-alert
+              class="vads-u-margin-bottom--1"
+              close-btn-aria-label="Close notification"
+              disable-analytics="false"
+              full-width="false"
+              slim
+              status="error"
+              uswds
+              visible="true"
+            >
+              <h3 slot="headline">We need to verify your identity</h3>
+              <p className="vads-u-font-size--base">
+                We’re sorry, but you’ll need to verify your identity before you
+                can continue with this request online.
+              </p>
+              <p>
+                To manage certain tasks and information on VA.gov, like
+                requesting personal records or changing your direct deposit
+                information, you’ll need to create a <strong>Login.gov</strong>{' '}
+                or <strong>ID.me</strong> account and verify your identity.
+              </p>
+              <p>
+                <a
+                  href="https://www.va.gov/resources/verifying-your-identity-on-vagov/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="vads-c-action-link--green"
+                >
+                  Verify your identity (opens in new tab)
+                </a>
+              </p>
+            </va-alert>
+            <p className="vads-u-margin-top--3">
+              If you don’t want to verify your identity right now, you can still
+              download and complete the PDF version of this request.
+            </p>
+            <p className="vads-u-margin-y--3">
+              <va-link
+                download
+                href="https://www.vba.va.gov/pubs/forms/VBA-20-10206-ARE.pdf"
+                text="Get VA Form 20-10206 to download"
+              />
+            </p>
+          </div>
+        )}
+    </>
+  );
+
   return (
     <IntroductionPageView
       route={route}
       content={content}
       ombInfo={ombInfo}
       childContent={childContent}
+      userIdVerified={userIdVerified}
+      userLoggedIn={userLoggedIn}
     />
   );
 };
@@ -149,6 +211,13 @@ IntroductionPage.propTypes = {
     }).isRequired,
     pageList: PropTypes.arrayOf(PropTypes.object).isRequired,
   }).isRequired,
+  userIdVerified: PropTypes.bool,
+  userLoggedIn: PropTypes.bool,
 };
 
-export default IntroductionPage;
+const mapStateToProps = state => ({
+  userIdVerified: isLOA3(state),
+  userLoggedIn: isLoggedIn(state),
+});
+
+export default connect(mapStateToProps)(IntroductionPage);
