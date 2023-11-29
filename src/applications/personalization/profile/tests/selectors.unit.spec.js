b67964074e4dda6c2be68656a5daf44f64a06a79
Adam Whitlock <adam.whitlock@adhocteam.us>
2023-11-28 18:12:53 -0700

    test: update selector unit tests

--- src/applications/personalization/profile/tests/selectors.unit.spec.js
+++ src/applications/personalization/profile/tests/selectors.unit.spec.js
@@ -106,31 +106,20 @@ describe('profile selectors', () => {
       state = {
         vaProfile: {
           cnpPaymentInformation: {
-            paymentAddress: {
-              addressOne: '123 Main',
-              city: 'San Francisco',
-              stateCode: 'CA',
+            controlInformation: {
+              canUpdateDirectDeposit: true,
             },
           },
         },
       };
     });
-    it('returns `true` if there is a street, city, and state set on the payment info payment address', () => {
+    it('returns `true` if control info canUpdateDirectDeposit is true', () => {
       expect(selectors.cnpDirectDepositIsEligible(state)).to.be.true;
     });
-    it('returns `false` if the street address is missing', () => {
-      state.vaProfile.cnpPaymentInformation.paymentAddress.addressOne = '';
+    it('returns `false` if control info canUpdateDirectDeposit is false', () => {
+      state.vaProfile.cnpPaymentInformation.controlInformation.canUpdateDirectDeposit = false;
       expect(selectors.cnpDirectDepositIsEligible(state)).to.be.false;
     });
-    it('returns `false` if the city is missing', () => {
-      state.vaProfile.cnpPaymentInformation.paymentAddress.city = '';
-      expect(selectors.cnpDirectDepositIsEligible(state)).to.be.false;
-    });
-    it('returns `false` if the state is missing', () => {
-      state.vaProfile.cnpPaymentInformation.paymentAddress.stateCode = '';
-      expect(selectors.cnpDirectDepositIsEligible(state)).to.be.false;
-    });
-
     it('returns `false` when the payment info endpoint failed to get data', () => {
       state = {
         vaProfile: {
@@ -140,29 +129,6 @@ describe('profile selectors', () => {
         },
       };
       expect(selectors.cnpDirectDepositIsEligible(state)).to.be.false;
-    });
-
-    describe('when useLighthouseFormat is true', () => {
-      beforeEach(() => {
-        state = {
-          vaProfile: {
-            cnpPaymentInformation: {
-              controlInformation: {
-                canUpdateDirectDeposit: true,
-              },
-            },
-          },
-        };
-      });
-
-      it('returns `true` if control info canUpdateDirectDeposit is true', () => {
-        expect(selectors.cnpDirectDepositIsEligible(state, true)).to.be.true;
-      });
-
-      it('returns `false` if control info canUpdateDirectDeposit is false', () => {
-        state.vaProfile.cnpPaymentInformation.controlInformation.canUpdateDirectDeposit = false;
-        expect(selectors.cnpDirectDepositIsEligible(state, true)).to.be.false;
-      });
     });
   });
 
