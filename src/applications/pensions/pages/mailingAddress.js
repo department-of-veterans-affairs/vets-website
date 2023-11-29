91ca0222f861d9defb37e90c7ab5a3d958762562
Tai Wilkin <tai.wilkin@coforma.io>
2023-11-29 11:32:09 -0500

    Add military base checkbox

--- src/applications/pensions/pages/mailingAddress.js
+++ src/applications/pensions/pages/mailingAddress.js
@@ -1,25 +1,25 @@
-import set from 'platform/utilities/data/set';
-
-import fullSchemaPensions from 'vets-json-schema/dist/21P-527EZ-schema.json';
-import * as address from 'platform/forms/definitions/address';
-
-import { validateCentralMailPostalCode } from '../validation';
+import {
+  addressSchema,
+  addressUI,
+} from 'platform/forms-system/src/js/web-component-patterns';
 
 /** @type {PageSchema} */
 export default {
   uiSchema: {
     'ui:title': 'Mailing address',
-    veteranAddress: set(
-      'ui:validations[1]',
-      validateCentralMailPostalCode,
-      address.uiSchema('Mailing address'),
-    ),
+    veteranAddress: addressUI('Mailing address'),
   },
   schema: {
     type: 'object',
     required: ['veteranAddress'],
     properties: {
-      veteranAddress: address.schema(fullSchemaPensions, true),
+      veteranAddress: addressSchema({
+        labels: {
+          militaryCheckbox:
+            'I receive mail outside of the United States on a U.S. military base',
+        },
+        omit: ['street3'],
+      }),
     },
   },
 };
