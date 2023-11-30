0738b3f562e52dee7c97b92e37e71859eff8c9e6
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2023-11-30 09:47:06 -0700

    Use 'ssnOrVaFileNumberUI' component pattern for veteran id (#26848)
    * Add veteran id
    
    * Remove required field
    
    * Update unit test
    
    ---------
    
    Co-authored-by: Matthew Knight <matt.knight@coforma.io>

--- src/applications/pensions/pages/applicantInformation.js
+++ src/applications/pensions/pages/applicantInformation.js
@@ -1,20 +1,13 @@
-import fullSchemaPensions from 'vets-json-schema/dist/21P-527EZ-schema.json';
-
 import {
   dateOfBirthUI,
   dateOfBirthSchema,
   fullNameUI,
   fullNameSchema,
-  ssnUI,
-  ssnSchema,
-  yesNoUI,
-  yesNoSchema,
+  ssnOrVaFileNumberUI,
+  ssnOrVaFileNumberSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
-import VaTextInputField from 'platform/forms-system/src/js/web-component-fields/VaTextInputField';
 import applicantDescription from 'platform/forms/components/ApplicantDescription';
 import UnauthenticatedWarningAlert from '../containers/UnauthenticatedWarningAlert';
-
-const { vaFileNumber } = fullSchemaPensions.properties;
 
 /** @type {PageSchema} */
 export default {
@@ -24,42 +17,19 @@ export default {
       'ui:description': UnauthenticatedWarningAlert,
     },
     veteranFullName: fullNameUI(),
-    veteranSocialSecurityNumber: ssnUI(),
-    vaClaimsHistory: yesNoUI({
-      title: 'Have you filed any type of VA claim before?',
-      uswds: true,
-      classNames: 'vads-u-margin-bottom--2',
-    }),
-    vaFileNumber: {
-      'ui:title': 'VA file number',
-      'ui:webComponentField': VaTextInputField,
-      'ui:options': {
-        classNames: 'vads-u-margin-bottom--2',
-        hint: 'Enter your VA file number if it doesn’t match your SSN',
-        hideIf: formData => formData.vaClaimsHistory !== true,
-      },
-      'ui:errorMessages': {
-        pattern: 'Your VA file number must be 8 or 9 digits',
-      },
-    },
+    veteranId: ssnOrVaFileNumberUI(),
     veteranDateOfBirth: dateOfBirthUI(),
   },
   schema: {
     type: 'object',
-    required: [
-      'veteranFullName',
-      'veteranSocialSecurityNumber',
-      'veteranDateOfBirth',
-    ],
+    required: ['veteranFullName', 'veteranDateOfBirth'],
     properties: {
       'view:warningAlert': {
         type: 'object',
         properties: {},
       },
       veteranFullName: fullNameSchema,
-      veteranSocialSecurityNumber: ssnSchema,
-      vaClaimsHistory: yesNoSchema,
-      vaFileNumber,
+      veteranId: ssnOrVaFileNumberSchema,
       veteranDateOfBirth: dateOfBirthSchema,
     },
   },
