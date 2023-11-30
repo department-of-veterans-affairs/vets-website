4768589af5f8dfade547e27a85e82eb9010d4b6f
624883 <Connor.Fewin@va.gov>
2023-11-30 15:38:34 -0500

    Got it working with pre-need eligibility form

--- src/applications/pre-need/config/pages/ContactInfo.jsx
+++ src/applications/pre-need/config/pages/ContactInfo.jsx
@@ -1,54 +1,72 @@
-import React, { useEffect, useRef } from 'react';
-
+import React from 'react';
+import { connect, useDispatch } from 'react-redux';
+import { setData } from 'platform/forms-system/src/js/actions';
+import set from 'platform/utilities/data/set';
 import { FIELD_NAMES } from '@@vap-svc/constants';
 import InitializeVAPServiceID from '@@vap-svc/containers/InitializeVAPServiceID';
 import ProfileInformationFieldController from '@@vap-svc/components/ProfileInformationFieldController';
-import { focusElement } from 'platform/utilities/ui';
 
-const ContactInfo = () => {
-  const field = 'MAILING_ADDRESS';
-  const title = 'Hello Mate';
-
-  const headerRef = useRef(null);
-
-  useEffect(
-    () => {
-      if (headerRef?.current) {
-        focusElement(headerRef?.current);
-      }
-    },
-    [headerRef],
-  );
-
+const ContactInfo = ({ formData, vapAddress, goToPath }) => {
+  const dispatch = useDispatch();
   const handlers = {
     onSubmit: event => {
       // This prevents this nested form submit event from passing to the
       // outer form and causing a page advance
       event.stopPropagation();
+
+      const address = {
+        city: vapAddress.value?.city,
+        state: vapAddress.value?.stateCode,
+        country: vapAddress.value?.countryCodeIso3,
+        postalCode: vapAddress.value?.zipCode,
+        street: vapAddress.value?.addressLine1,
+        street2: vapAddress.value?.addressLine2,
+      };
+
+      const updatedFormData = set(
+        'application.applicant[view:applicantInfo].mailingAddress',
+        address,
+        { ...formData }, // make a copy of the original formData
+      );
+      dispatch(setData(updatedFormData));
     },
-    cancel: () => {},
-    success: () => {},
+    cancel: () => {
+      goToPath('/supporting-documents');
+    },
+    success: () => {
+      goToPath('/preparer');
+    },
   };
 
   return (
     <div
       className="va-profile-wrapper"
       onSubmit={handlers.onSubmit}
-      id={`edit-${FIELD_NAMES[field]}`}
+      id={`edit-${FIELD_NAMES.MAILING_ADDRESS}`}
     >
       <InitializeVAPServiceID>
-        <h3 ref={headerRef}>{title}</h3>
+        <h3>Hello Mate</h3>
         <ProfileInformationFieldController
           forceEditView
-          fieldName={FIELD_NAMES[field]}
           isDeleteDisabled
+          fieldName={FIELD_NAMES.MAILING_ADDRESS}
+          showEditView={false}
+          showValidationView={false}
           cancelCallback={handlers.cancel}
           successCallback={handlers.success}
-          saveButtonText="Update"
+          saveButtonText="Continue"
+          cancelButtonText="Back"
         />
       </InitializeVAPServiceID>
     </div>
   );
 };
 
-export default ContactInfo;
+const mapStateToProps = state => {
+  return {
+    formData: state.form?.data,
+    vapAddress: state.vapService?.formFields?.mailingAddress,
+  };
+};
+
+export default connect(mapStateToProps)(ContactInfo);
