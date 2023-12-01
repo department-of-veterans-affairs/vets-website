ee8ea9373da413e43911bc1b9abc3aff7ac6b84c
Jeana <jeana.clark@adhocteam.us>
2023-12-01 17:16:11 -0600

    Updated Name, address, phone fields to match sketch

--- src/applications/simple-forms/20-10206/pages/personalInfo.js
+++ src/applications/simple-forms/20-10206/pages/personalInfo.js
@@ -1,19 +1,27 @@
+import { cloneDeep } from 'lodash';
+
+import { fullNameNoSuffixUI } from 'platform/forms-system/src/js/web-component-patterns/fullNamePattern.js';
+
 import {
-  fullNameSchema,
-  fullNameUI,
   dateOfBirthSchema,
   dateOfBirthUI,
   inlineTitleSchema,
   inlineTitleUI,
   titleUI,
 } from 'platform/forms-system/src/js/web-component-patterns';
+
 import VaTextInputField from 'platform/forms-system/src/js/web-component-fields/VaTextInputField';
+import { pdfFullNameNoSuffixSchema } from '../../shared/definitions/pdfFullNameNoSuffix';
+
+const fullNameUI = cloneDeep(fullNameNoSuffixUI());
+
+fullNameUI.middle['ui:title'] = 'Middle initial';
 
 /** @type {PageSchema} */
 export default {
   uiSchema: {
     ...titleUI('Your name'),
-    fullName: fullNameUI(),
+    fullName: fullNameUI,
     'view:birthTitle': inlineTitleUI('Your date and place of birth'),
     dateOfBirth: dateOfBirthUI(),
     placeOfBirth: {
@@ -30,7 +38,9 @@ export default {
   schema: {
     type: 'object',
     properties: {
-      fullName: fullNameSchema,
+      fullName: pdfFullNameNoSuffixSchema({
+        pdfMaxLengths: { first: 12, middle: 1, last: 18 },
+      }),
       'view:birthTitle': inlineTitleSchema,
       dateOfBirth: dateOfBirthSchema,
       placeOfBirth: {
