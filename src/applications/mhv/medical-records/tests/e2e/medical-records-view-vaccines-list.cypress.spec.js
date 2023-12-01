1b67d945f70bdcdefd2c535ffaa4f2077aa1079f
Aaron Read <Aaron.Read@va.gov>
2023-12-01 16:14:29 -0600

    updates to vaccines testing for staging gaps (#26950)
    * updates to vaccines testing for staging gaps
    
    * file name change

--- src/applications/mhv/medical-records/tests/e2e/medical-records-view-vaccines-list.cypress.spec.js
+++ src/applications/mhv/medical-records/tests/e2e/medical-records-view-vaccines-list.cypress.spec.js
@@ -1,11 +1,12 @@
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
+import VaccinesListPage from './pages/VaccinesListPage';
 
-describe('Medical Records View Immunizations', () => {
-  it('Visits Medical Records View Immunization List', () => {
+describe('Medical Records View Vaccines', () => {
+  it('Visits Medical Records View Vaccine List', () => {
     const site = new MedicalRecordsSite();
     site.login();
-    cy.visit('my-health/medical-records/vaccines');
-
+    cy.visit('my-health/medical-records/');
+    VaccinesListPage.clickGotoVaccinesLink();
     // click on the vaccines link
 
     // Axe check
---
+++ src/applications/mhv/medical-records/tests/e2e/pages/VaccineDetailsPage.js
@@ -0,0 +1,33 @@
+import moment from 'moment';
+import defaultVaccineDetail from '../fixtures/vaccines/vaccine-8261.json';
+
+class VaccinesDetailsPage {
+  verifyVaccineName = (VaccinesDetails = defaultVaccineDetail) => {
+    cy.get('[data-testid="vaccine-name"]').contains(
+      VaccinesDetails.vaccineCode.text,
+    );
+  };
+
+  verifyVaccineDate = (VaccinesDetails = defaultVaccineDetail) => {
+    cy.get('[data-testid="header-time"]').contains(
+      moment(VaccinesDetails.occurrenceDateTime).format('MMMM D, YYYY'),
+    );
+  };
+
+  verifyVaccineLocation = (VaccinesDetails = defaultVaccineDetail) => {
+    cy.get('[data-testid="vaccine-location"]').contains(
+      VaccinesDetails.contained[0].name,
+    );
+  };
+
+  verifyVaccineNotes = (
+    VaccinesDetails = defaultVaccineDetail,
+    noteIndex = 0,
+  ) => {
+    cy.get('[data-testid="list-item"]')
+      .eq(noteIndex)
+      .contains(VaccinesDetails.note[noteIndex].text);
+  };
+}
+
+export default new VaccinesDetailsPage();
