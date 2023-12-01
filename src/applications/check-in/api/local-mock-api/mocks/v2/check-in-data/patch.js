2bd92b8a88604edd19b0bba77bc941b00b2941cf
John Woolschlager <wullaski@gmail.com>
2023-12-01 10:30:36 -0500

    Check in/68865/remove appointment details list view (#26849)
    * init
    
    * Check in/62227/create upcoming appointments component (#25866)
    
    * init
    
    * Add actions, reducers and selectors for upcoming appointments data
    
    * Add method and mocks to fetch appointments
    
    * Add hook for getting upcoming appointments
    
    * Add components for the list and test
    
    * Add multiple months and days to mocks
    
    * Add sorting
    
    * Add reload
    
    * Fix sort
    
    * style components
    
    * Switch to an array structure for more reliable sorting
    
    * Add error state for upcomingAppointments component
    
    * Fix appointments page test
    
    * Add unit tests for UpcomingAppointments component
    
    * Add test for upcomingAppointmentsList component
    
    * Add test for upcomingAppointmentsListItem component
    
    * Add cypress tests for upcoming appointments
    
    * Fix test
    
    * Change loading messages
    
    * Fix test
    
    * Add i18n for clinic
    
    * Add app to dependency array
    
    * Add dependency
    
    * Add key
    
    * Use i18n utils for date strings
    
    * Fix unit test
    
    * Add translated error message
    
    * Add dependency
    
    ---------
    
    Co-authored-by: Brian Seek <brian.seek@civicactions.com>
    
    * Check in/62138/add what to do next component (#26211)
    
    * init
    
    * Add what to do next component
    
    * Add action item WIP
    
    * Finish up component logic and structure
    
    * Dry upcoming appointments
    
    * Add unit tests for action item display component
    
    * Add what to do next unit test
    
    * Remove eyebrow
    
    * Add e2e tests for day of appointments landing
    
    * Add action link component and unit test
    
    * Test for appointment details click
    
    * Add cypress tests for pre check in
    
    * Add better aria labels and translations
    
    * Remove reloadable
    
    * Add create appointment to better test pre check in card title
    
    * Add pre check in title method
    
    * Fix cypress tests
    
    * Fix unit tests
    
    * Fix unit test
    
    * Deleted a test
    
    * Add button to pass old tests
    
    * Skipping some tests
    
    * Add timeout
    
    * Add cy intercept for mock upcoming appointment data
    
    * Add type button
    
    * remove hard coded string
    
    * change test name for clarity
    
    ---------
    
    Co-authored-by: Brian Seek <brian.seek@civicactions.com>
    
    * Check in/66619/refresh upcoming apts (#26220)
    
    * init
    
    * refactor to keep page from re-fetching data without a relaod.
    
    * removed un-needed param
    
    * Updated tests.
    
    * added test for full hook covereage and removed extra at
    
    * Skipped tests that need un-related refactors.
    
    * Added more test skips.
    
    * data loading refactor for both upcoming and vet data
    
    * Update unit tests.
    
    * removing tests for unified for now :)
    
    These tests will be re-written in upcoming tickets before the feature gets merged to main.
    
    * removed cy screenshots
    
    * Add appointment id to form data on action item click
    
    * Remove details view
    
    * change back to appointments link to go back to appointments list view
    
    * Change demographics questions to go to complete page with id
    
    * Fix typo
    
    * Add api calls to complete index
    
    * Remove duplicate method
    
    * Fix unit test
    
    * Delete spaces
    
    * Update and use demographics patch mock response
    
    * Changed use state location and values to prevent calling endpoint twice
    
    * Fixed imports
    
    * WIP add tests for confirmation page
    
    * Add more tests for confirmation component
    
    ---------
    
    Co-authored-by: Brian Seek <brian.seek@civicactions.com>

--- src/applications/check-in/api/local-mock-api/mocks/v2/check-in-data/patch.js
+++ src/applications/check-in/api/local-mock-api/mocks/v2/check-in-data/patch.js
@@ -1,19 +1,21 @@
 const createMockSuccessResponse = _data => {
   return {
     data: {
-      attributes: {
-        id: 1,
-        patientDfn: '123',
-        demographicsNeedsUpdate: false,
-        demographicsConfirmedAt: '2022-01-22T12:00:00.000-05:00',
-        nextOfKinNeedsUpdate: false,
-        nextOfKinConfirmedAt: '2022-02-03T12:00:00.000-05:00',
-        emergencyContactNeedsUpdate: true,
-        emergencyContactConfirmedAt: '2022-01-27T12:00:00.000-05:00',
-        insuranceVerificationNeeded: null,
+      data: {
+        attributes: {
+          id: 1,
+          patientDfn: '123',
+          demographicsNeedsUpdate: false,
+          demographicsConfirmedAt: '2022-01-22T12:00:00.000-05:00',
+          nextOfKinNeedsUpdate: false,
+          nextOfKinConfirmedAt: '2022-02-03T12:00:00.000-05:00',
+          emergencyContactNeedsUpdate: false,
+          emergencyContactConfirmedAt: '2022-01-27T12:00:00.000-05:00',
+          insuranceVerificationNeeded: null,
+        },
+        id: '123',
+        type: 'PatientDemographicsStatusResponse',
       },
-      id: '123',
-      type: 'PatientDemographicsStatusResponse',
     },
   };
 };
--- src/applications/check-in/day-of/pages/NextOfKin.jsx
+++ src/applications/check-in/day-of/pages/NextOfKin.jsx
@@ -8,7 +8,7 @@ import BackButton from '../../components/BackButton';
 import { seeStaffMessageUpdated } from '../../actions/day-of';
 import { recordAnswer } from '../../actions/universal';
 import NextOfKinDisplay from '../../components/pages/nextOfKin/NextOfKinDisplay';
-import { makeSelectVeteranData } from '../../selectors';
+import { makeSelectVeteranData, makeSelectForm } from '../../selectors';
 import { useStorage } from '../../hooks/useStorage';
 import { URLS } from '../../utils/navigation';
 import { APP_NAMES } from '../../utils/appConstants';
@@ -18,14 +18,19 @@ const NextOfKin = props => {
   const { t } = useTranslation();
   const selectVeteranData = useMemo(makeSelectVeteranData, []);
   const { demographics } = useSelector(selectVeteranData);
+  const selectForm = useMemo(makeSelectForm, []);
+  const { data } = useSelector(selectForm);
   const { nextOfKin1: nextOfKin } = demographics;
   const {
     jumpToPage,
     goToNextPage,
     goToPreviousPage,
     getPreviousPageFromRouter,
+    getNextPageFromRouter,
   } = useFormRouting(router);
   const { setShouldSendDemographicsFlags } = useStorage(APP_NAMES.CHECK_IN);
+
+  const nextPage = getNextPageFromRouter();
 
   const seeStaffMessage = t(
     'our-staff-can-help-you-update-your-next-of-kin-information',
@@ -42,9 +47,19 @@ const NextOfKin = props => {
     () => {
       dispatch(recordAnswer({ nextOfKinUpToDate: 'yes' }));
       setShouldSendDemographicsFlags(window, true);
+      if (nextPage === 'complete') {
+        jumpToPage(`complete/${data.activeAppointmentId}`);
+      }
       goToNextPage();
     },
-    [dispatch, goToNextPage, setShouldSendDemographicsFlags],
+    [
+      dispatch,
+      goToNextPage,
+      setShouldSendDemographicsFlags,
+      nextPage,
+      jumpToPage,
+      data.activeAppointmentId,
+    ],
   );
 
   const noClick = useCallback(
