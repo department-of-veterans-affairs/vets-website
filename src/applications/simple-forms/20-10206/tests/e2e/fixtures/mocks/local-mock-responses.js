66645dedc99b20d6f6b0dbb1ff3ac3109a0caecc
Tze-chiu Lei <tze@adhocteam.us>
2023-12-04 15:45:11 -0700

    20-10206 - Build front-end (#26863)
    * Future-proof 20-10206 tracking-prefix
    
    * Build 20-10206 veteran pages
    
    * Refactor 20-10206 pages and constants
    
    * Rename 20-10206 page files
    
    * Refactor 20-10216 test data
    
    * Add 20-10206 non-citizen id-info page
    
    * Rename 20-10206 citizen-pers-info page file
    
    * Refactor 20-10206 pers-info page
    
    * Rename 20-10206 citizen-addr page
    
    * Refactor 20-10206 addr page
    
    * Rename 20-10206 citizen phone-email
    page
    
    * Refactor 20-10206 phone-email page
    
    * Enable 20-10206 v3SegmentedProgressBar
    
    * Refactor 20-10206 page-titles
    
    * Add placeOfBirth field to 20-10206 pers-info page
    
    * Add 20-10206 record-selections page
    
    * Build 20-10206 intro page
    
    * Fix 20-10206 prefill/sip local mocks
    
    * Add 20-10206 disability-exam-details chapter & page
    
    * Add 20-10206 financial-record-details page
    
    * Add 20-10206 life-insurance-benefit-details page
    
    * Add 20-10206 other-compensation-and-pension-details page
    
    * Add 20-10206 other-benefit-details page
    
    * Add 20-10206 conditional id-verification page
    
    * Restore 20-10206 user-id-verified mock-state
    
    * Build 20-10206 Confirmation page
    
    * Add 20-10206 Review-page config and actual submitUrl
    
    * Fix 20-10206 Confirmation page submission-response handling
    
    * Fix 20-10206 Benefit-record-details page-config
    
    * Add 20-10206 submit-transformer
    
    * Remove 20-10206 old styling rule
    
    * Add 20-10206 Introduction page conditional content
    
    * Fix 20-10206 Introduction page content/styling
    
    * Fix 20-10206 Introduction page content further
    
    * Clarify/explain 20-10206 User-identity-verification logic/branching
    
    * Add hideUnauthedStartLink prop to formConfig type-definition and forms unit-test
    
    * Add 20-10206 prefill-transformer
    
    * Initial text edits to PA request form (#26938)
    
    * Updated Name, address, phone fields to match sketch (#26952)
    
    ---------
    
    Co-authored-by: Jeana <117098918+jeana-adhoc@users.noreply.github.com>

--- src/applications/simple-forms/20-10206/tests/e2e/fixtures/mocks/local-mock-responses.js
+++ src/applications/simple-forms/20-10206/tests/e2e/fixtures/mocks/local-mock-responses.js
@@ -1,7 +1,9 @@
 /* eslint-disable camelcase */
 // use this file to mock api responses for local development
 // yarn mock-api --responses ./src/applications/simple-forms/21-0845/tests/e2e/fixtures/mocks/local-mock-responses.js
-const mockSubmit = require('../../../../shared/tests/e2e/fixtures/mocks/application-submit.json');
+const mockSipPut = require('./sipPut.json');
+const mockSipGet = require('./sipGet.json');
+const mockSubmit = require('../../../../../shared/tests/e2e/fixtures/mocks/application-submit.json');
 
 const responses = {
   'GET /v0/user': {
@@ -11,29 +13,59 @@ const responses = {
           sign_in: {
             service_name: 'idme',
           },
-          email: 'fake@fake.com',
-          loa: { current: 3 },
+          email: 'john.preparer@example.com',
+          loa: { current: 3 }, // change to 2 to mock User not identity-verified
           first_name: 'John',
           middle_name: '',
-          last_name: 'Veteran',
+          last_name: 'Preparer',
           gender: 'F',
           birth_date: '1985-01-01',
-          verified: true,
+          verified: true, // change to false to mock User not identity-verified
         },
         veteran_status: {
           status: 'OK',
           is_veteran: true,
           served_in_military: true,
         },
-        in_progress_forms: [],
+        in_progress_forms: [
+          // uncomment object below to see signed-in SIP-alert
+          // {
+          //   form: '20-10206',
+          //   metadata: {
+          //     savedAt: 1700155167499,
+          //     submission: {
+          //       status: false,
+          //       errorMessage: false,
+          //       id: false,
+          //       timestamp: 1700155167494,
+          //       hasAttemptedSubmit: true,
+          //     },
+          //     createdAt: 1700155105,
+          //     expiresAt: 1705339168,
+          //     lastUpdated: 1700155168,
+          //     inProgressFormId: 27056,
+          //   },
+          //   lastUpdated: 1700155168,
+          // },
+        ],
         prefills_available: ['20-10206'],
-        services: [],
+        services: [
+          'facilities',
+          'hca',
+          'edu-benefits',
+          'evss-claims',
+          'form526',
+          'user-profile',
+          'health-records',
+          'rx',
+          'messaging',
+        ],
         va_profile: {
           status: 'OK',
-          birth_date: '19511118',
-          family_name: 'Veteran',
+          birth_date: '19850101',
+          family_name: 'Preparer',
           gender: 'M',
-          given_names: ['John', 'D'],
+          given_names: ['John', ''],
           active_status: 'active',
           facilities: [
             {
@@ -58,6 +90,7 @@ const responses = {
     data: {
       type: 'feature_toggles',
       features: [
+        { name: 'form210845', value: true },
         { name: 'showExpandableVamcAlert', value: true },
         { name: 'facilityLocatorShowCommunityCares', value: true },
         { name: 'profile_show_profile_2.0', value: false },
@@ -83,6 +116,9 @@ const responses = {
       ],
     },
   },
+
+  'GET /v0/in_progress_forms/20-10206': mockSipGet,
+  'PUT /v0/in_progress_forms/20-10206': mockSipPut,
 
   'POST /simple_forms_api/v1/simple_forms': mockSubmit,
 };
