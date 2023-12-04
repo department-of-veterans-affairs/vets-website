2e8c074f46d5de4771510be84ed07c8f49f7538b
Luke Short <lukeshortemail@gmail.com>
2023-12-04 10:27:11 -0800

    wip use build flag for disabilities list revised list staging review

--- src/applications/disability-benefits/all-claims/content/disabilityLabels.js
+++ src/applications/disability-benefits/all-claims/content/disabilityLabels.js
@@ -1,5 +1,9 @@
+import environment from '@department-of-veterans-affairs/platform-utilities/environment';
 import disabilityLabelsReduced from './disabilityLabelsReduced';
 
+const disabilityLabelsRevised = {};
+
 export const getDisabilityLabels = () => {
+  if (environment.isStaging()) return disabilityLabelsRevised;
   return disabilityLabelsReduced;
 };
