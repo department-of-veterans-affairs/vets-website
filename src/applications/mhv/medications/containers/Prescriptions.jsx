f553dc210926f31b01e3d2eaaa9c4ebcb93baad9
gerard-sullivan <132001256+gerard-sullivan@users.noreply.github.com>
2023-12-04 09:51:24 -0800

    Update 52136

--- src/applications/mhv/medications/containers/Prescriptions.jsx
+++ src/applications/mhv/medications/containers/Prescriptions.jsx
@@ -61,7 +61,10 @@ const Prescriptions = props => {
 
   const sortRxList = sortOption => {
     setPdfGenerateStatus(PDF_GENERATE_STATUS.NotStarted);
-    setSelectedSortOption(sortOption);
+    if (sortOption !== selectedSortOption) {
+      setSelectedSortOption(sortOption);
+      setLoading(true);
+    }
     sessionStorage.setItem(SESSION_SELECTED_SORT_OPTION, sortOption);
     focusElement(document.getElementById('showingRx'));
   };
@@ -88,7 +91,9 @@ const Prescriptions = props => {
 
   useEffect(
     () => {
-      setLoading(true);
+      if (!paginatedPrescriptionsList) {
+        setLoading(true);
+      }
       if (!page) history.replace('/1');
       dispatch(
         getPrescriptionsPaginatedSortedList(
