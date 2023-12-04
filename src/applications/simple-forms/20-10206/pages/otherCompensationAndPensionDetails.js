209a97c030173a69907e8411e8cd7505b45cbb4f
Tze-chiu Lei <tze@adhocteam.us>
2023-12-04 15:45:11 -0700

    20-10206 - Build front-end (#26863)
    * Future-proof 20-10206 tracking-prefix
    
    * Build 20-10206 veteran pages
    
    * Refactor 20-10206 pages and constants
    
    * Rename 20-10206 page files
    
    * Refactor 20-10216 test data
    
    * Add 20-10206 non-citizen id-info page
    
    * Rename 20-10206 citizen-pers-info page file
    
    * Refactor 20-10206 pers-info page
    
    * Rename 20-10206 citizen-addr page
    
    * Refactor 20-10206 addr page
    
    * Rename 20-10206 citizen phone-email
    page
    
    * Refactor 20-10206 phone-email page
    
    * Enable 20-10206 v3SegmentedProgressBar
    
    * Refactor 20-10206 page-titles
    
    * Add placeOfBirth field to 20-10206 pers-info page
    
    * Add 20-10206 record-selections page
    
    * Build 20-10206 intro page
    
    * Fix 20-10206 prefill/sip local mocks
    
    * Add 20-10206 disability-exam-details chapter & page
    
    * Add 20-10206 financial-record-details page
    
    * Add 20-10206 life-insurance-benefit-details page
    
    * Add 20-10206 other-compensation-and-pension-details page
    
    * Add 20-10206 other-benefit-details page
    
    * Add 20-10206 conditional id-verification page
    
    * Restore 20-10206 user-id-verified mock-state
    
    * Build 20-10206 Confirmation page
    
    * Add 20-10206 Review-page config and actual submitUrl
    
    * Fix 20-10206 Confirmation page submission-response handling
    
    * Fix 20-10206 Benefit-record-details page-config
    
    * Add 20-10206 submit-transformer
    
    * Remove 20-10206 old styling rule
    
    * Add 20-10206 Introduction page conditional content
    
    * Fix 20-10206 Introduction page content/styling
    
    * Fix 20-10206 Introduction page content further
    
    * Clarify/explain 20-10206 User-identity-verification logic/branching
    
    * Add hideUnauthedStartLink prop to formConfig type-definition and forms unit-test
    
    * Add 20-10206 prefill-transformer
    
    * Initial text edits to PA request form (#26938)
    
    * Updated Name, address, phone fields to match sketch (#26952)
    
    ---------
    
    Co-authored-by: Jeana <117098918+jeana-adhoc@users.noreply.github.com>

---
+++ src/applications/simple-forms/20-10206/pages/otherCompensationAndPensionDetails.js
@@ -0,0 +1,30 @@
+import VaTextInputField from 'platform/forms-system/src/js/web-component-fields/VaTextInputField';
+import { titleUI } from 'platform/forms-system/src/js/web-component-patterns';
+
+/** @type {PageSchema} */
+export default {
+  uiSchema: {
+    ...titleUI(
+      'Other compensation and/or pension record details',
+      'You requested access to another type of record that wasn’t listed. Any extra information you can share will help us find your records.',
+    ),
+    otherCompAndPenDetails: {
+      'ui:title':
+        'Describe the compensation and/or pension records you’d like to request',
+      'ui:webComponentField': VaTextInputField,
+      'ui:errorMessages': {
+        required:
+          'Please describe the compensation and/or pension records you’re requesting.',
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      otherCompAndPenDetails: {
+        type: 'string',
+      },
+    },
+    required: ['otherCompAndPenDetails'],
+  },
+};
