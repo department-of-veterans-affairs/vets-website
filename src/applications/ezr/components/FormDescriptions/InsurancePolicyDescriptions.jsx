c98c4c20367e60c92ee10335e0f79f2589e14501
Matt Long <longmd@me.com>
2023-12-05 14:22:54 -0500

    Init commit

--- src/applications/ezr/components/FormDescriptions/InsurancePolicyDescriptions.jsx
+++ src/applications/ezr/components/FormDescriptions/InsurancePolicyDescriptions.jsx
@@ -5,7 +5,7 @@ export const InsurancePolicyOrDescription = (
 );
 
 export const PolicyOrGroupDescription = (
-  <div className="schemaform-block-title schemaform-block-subtitle vads-u-margin-top--6 vads-u-margin-bottom--2 vads-u-color--base">
+  <div className="vads-u-margin-top--6 vads-u-margin-bottom--2 vads-u-color--base vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--bold">
     Provide either your insurance policy number or group code.{' '}
     <span className="schemaform-required-span vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--normal">
       (*Required)
