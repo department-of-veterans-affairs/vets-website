701c14695b392785ff0a638671fd00be3666bbee
John Woolschlager <wullaski@gmail.com>
2023-12-05 17:10:01 -0500

    Check in/70319/unified e2e dayof group1 (#26976)
    * Add cypress test for authentication failure
    
    * Add comma for #70340
    
    * Fix for yes click when travel is disabled
    
    * Change current features to have travel enabled
    
    * Add demographics tests
    
    * Add cy intercepts
    
    * Rename files

---
+++ src/applications/check-in/day-of/tests/e2e/demographics.does.not.need.to.confirm.cypress.spec.js
@@ -0,0 +1,67 @@
+import '../../../tests/e2e/commands';
+import ApiInitializer from '../../../api/local-mock-api/e2e/ApiInitializer';
+import ValidateVeteran from '../../../tests/e2e/pages/ValidateVeteran';
+import AppointmentsPage from '../../../tests/e2e/pages/AppointmentsPage';
+import TravelPages from '../../../tests/e2e/pages/TravelPages';
+import Confirmation from './pages/Confirmation';
+
+const dateFns = require('date-fns');
+
+describe('Check In Experience | Day Of |', () => {
+  describe("Patient who does't need to confirm demographics", () => {
+    beforeEach(() => {
+      const {
+        initializeFeatureToggle,
+        initializeSessionGet,
+        initializeSessionPost,
+        initializeUpcomingAppointmentsDataGet,
+        initializeCheckInDataGet,
+        initializeCheckInDataPost,
+      } = ApiInitializer;
+      initializeFeatureToggle.withCurrentFeatures();
+      initializeSessionGet.withSuccessfulNewSession();
+      initializeSessionPost.withValidation();
+      initializeUpcomingAppointmentsDataGet.withSuccess();
+      initializeCheckInDataGet.withSuccess({
+        demographicsNeedsUpdate: false,
+        demographicsConfirmedAt: dateFns.format(
+          new Date(),
+          "yyyy-LL-dd'T'HH:mm:ss",
+        ),
+        nextOfKinNeedsUpdate: false,
+        nextOfKinConfirmedAt: dateFns.format(
+          new Date(),
+          "yyyy-LL-dd'T'HH:mm:ss",
+        ),
+        emergencyContactNeedsUpdate: false,
+        emergencyContactConfirmedAt: dateFns.format(
+          new Date(),
+          "yyyy-LL-dd'T'HH:mm:ss",
+        ),
+      });
+      initializeCheckInDataPost.withSuccess();
+      cy.visitWithUUID();
+    });
+    afterEach(() => {
+      cy.window().then(window => {
+        window.sessionStorage.clear();
+      });
+    });
+    it('should complete check in if answer is no to travel', () => {
+      ValidateVeteran.validateVeteran();
+      cy.injectAxeThenAxeCheck();
+      ValidateVeteran.attemptToGoToNextPage();
+
+      AppointmentsPage.validatePageLoaded();
+      cy.injectAxeThenAxeCheck();
+      AppointmentsPage.attemptCheckIn();
+
+      TravelPages.validatePageLoaded();
+      cy.injectAxeThenAxeCheck();
+      TravelPages.attemptToGoToNextPage('no');
+
+      Confirmation.validatePageLoaded();
+      cy.injectAxeThenAxeCheck();
+    });
+  });
+});
