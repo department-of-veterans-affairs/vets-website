72be91c7b190e31275c3ea5ebda779fcb2e8567c
John Woolschlager <wullaski@gmail.com>
2023-12-05 17:10:01 -0500

    Check in/70319/unified e2e dayof group1 (#26976)
    * Add cypress test for authentication failure
    
    * Add comma for #70340
    
    * Fix for yes click when travel is disabled
    
    * Change current features to have travel enabled
    
    * Add demographics tests
    
    * Add cy intercepts
    
    * Rename files

---
+++ src/applications/check-in/day-of/tests/e2e/demographics.needs.to.confirm.cypress.spec.js
@@ -0,0 +1,82 @@
+import '../../../tests/e2e/commands';
+
+import ApiInitializer from '../../../api/local-mock-api/e2e/ApiInitializer';
+import ValidateVeteran from '../../../tests/e2e/pages/ValidateVeteran';
+import AppointmentsPage from '../../../tests/e2e/pages/AppointmentsPage';
+import Demographics from '../../../tests/e2e/pages/Demographics';
+import EmergencyContact from '../../../tests/e2e/pages/EmergencyContact';
+import NextOfKin from '../../../tests/e2e/pages/NextOfKin';
+import TravelPages from '../../../tests/e2e/pages/TravelPages';
+import SeeStaff from './pages/SeeStaff';
+import Confirmation from './pages/Confirmation';
+
+describe('Check In Experience | Day Of |', () => {
+  describe('Patient who needs to confirm demographics', () => {
+    beforeEach(() => {
+      const {
+        initializeFeatureToggle,
+        initializeSessionGet,
+        initializeSessionPost,
+        initializeUpcomingAppointmentsDataGet,
+        initializeCheckInDataGet,
+        initializeCheckInDataPost,
+      } = ApiInitializer;
+      initializeFeatureToggle.withCurrentFeatures();
+      initializeSessionGet.withSuccessfulNewSession();
+      initializeSessionPost.withValidation();
+      initializeUpcomingAppointmentsDataGet.withSuccess();
+      initializeCheckInDataGet.withSuccess();
+      initializeCheckInDataPost.withSuccess();
+      cy.visitWithUUID();
+    });
+    afterEach(() => {
+      cy.window().then(window => {
+        window.sessionStorage.clear();
+      });
+    });
+    it('should complete check in if answer is yes to all demographics questions', () => {
+      ValidateVeteran.validateVeteran();
+      cy.injectAxeThenAxeCheck();
+      ValidateVeteran.attemptToGoToNextPage();
+
+      AppointmentsPage.validatePageLoaded();
+      cy.injectAxeThenAxeCheck();
+      AppointmentsPage.attemptCheckIn();
+
+      Demographics.validatePageLoaded();
+      cy.injectAxeThenAxeCheck();
+      Demographics.attemptToGoToNextPage();
+
+      EmergencyContact.validatePageLoaded();
+      cy.injectAxeThenAxeCheck();
+      EmergencyContact.attemptToGoToNextPage();
+
+      NextOfKin.validatePage.dayOf();
+      cy.injectAxeThenAxeCheck();
+      NextOfKin.attemptToGoToNextPage();
+
+      TravelPages.validatePageLoaded();
+      cy.injectAxeThenAxeCheck();
+      TravelPages.attemptToGoToNextPage('no');
+
+      Confirmation.validatePageLoaded();
+      cy.injectAxeThenAxeCheck();
+    });
+    it('should direct patient to front desk if answer is no to demographics question', () => {
+      ValidateVeteran.validateVeteran();
+      cy.injectAxeThenAxeCheck();
+      ValidateVeteran.attemptToGoToNextPage();
+
+      AppointmentsPage.validatePageLoaded();
+      cy.injectAxeThenAxeCheck();
+      AppointmentsPage.attemptCheckIn();
+
+      Demographics.validatePageLoaded();
+      cy.injectAxeThenAxeCheck();
+      Demographics.attemptToGoToNextPage('no');
+
+      SeeStaff.validatePageLoaded();
+      cy.injectAxeThenAxeCheck();
+    });
+  });
+});
