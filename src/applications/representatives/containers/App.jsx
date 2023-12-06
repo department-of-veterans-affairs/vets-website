a0c9ed31b5f1792be5b961ea12befb78c0aaf392
Oren Mittman <nihil2501@gmail.com>
2023-12-06 14:28:11 -0500

    (spike-rep-app) require login at root

--- src/applications/representatives/containers/App.jsx
+++ src/applications/representatives/containers/App.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default function App({ children }) {
-  return <div>{children}</div>;
+import { RequiredLoginView } from '@department-of-veterans-affairs/platform-user/RequiredLoginView';
+
+function App({ children, user }) {
+  return (
+    <RequiredLoginView
+      // TODO: Determine the significance of this flag for us.
+      verify
+      // TODO: Determine which services we require.
+      serviceRequired={[]}
+      user={user}
+    >
+      <div>{children}</div>
+    </RequiredLoginView>
+  );
 }
+
+App.propTypes = {
+  children: PropTypes.object,
+  user: PropTypes.object,
+};
+
+function mapStateToProps({ user }) {
+  return { user };
+}
+
+export default connect(mapStateToProps)(App);
+export { App };
