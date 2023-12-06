a82233dc94fad0a485f1cc1a50e918cdd8dc3db7
Adrian Rollett <adrian.rollett@va.gov>
2023-12-06 09:34:41 -0700

    Fix title capitalization

--- src/applications/avs/containers/Avs.jsx
+++ src/applications/avs/containers/Avs.jsx
@@ -86,7 +86,7 @@ const Avs = props => {
         serviceRequired={[backendServices.USER_PROFILE]}
       >
         <BreadCrumb />
-        <h1>After-visit Summary</h1>
+        <h1>After-visit summary</h1>
 
         <va-accordion>
           <va-accordion-item
