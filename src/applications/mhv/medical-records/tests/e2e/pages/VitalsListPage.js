5d120a2488bd42155530ec2077a8ba4bd600d170
Anal Gomes <103195615+agomes82@users.noreply.github.com>
2023-12-07 17:06:42 -0500

    MHV-51575 Add TXT Download functionality for Vitals (#27045)
    * MHV-51575 Add TXT Download functionality for Vitals
    
    MHV-51575 Add TXT Download functionality for Vitals
    
    * MHV-51575 Add TXT Download functionality for Vitals
    
    Vitals page separate Click method

--- src/applications/mhv/medical-records/tests/e2e/pages/VitalsListPage.js
+++ src/applications/mhv/medical-records/tests/e2e/pages/VitalsListPage.js
@@ -25,6 +25,35 @@ class VitalsListPage {
       .eq(_VitalsIndex)
       .click();
   };
+
+  verifyPrintOrDownload = () => {
+    cy.get('[data-testid="print-records-button"]').should('be.visible');
+  };
+
+  clickPrintOrDownload = () => {
+    cy.get('[data-testid="print-records-button"]').click();
+  };
+
+  verifyPrintButton = () => {
+    // should display print button for a list "Print this list"
+    cy.get('[data-testid="printButton-0"]').should('be.visible');
+  };
+
+  verifyDownloadPDF = () => {
+    // should display a download pdf file button "Download PDF of this page"
+    cy.get('[data-testid="printButton-1"]').should('be.visible');
+  };
+
+  verifyDownloadTextFile = () => {
+    // should display a download text file button "Download list as a text file"
+    cy.get('[data-testid="printButton-2"]').should('be.visible');
+    // cy.get('[data-testid="printButton-2').click();
+  };
+
+  clickDownloadPDFFile = () => {
+    // should display a download text file button "Download list as a text file"
+    cy.get('[data-testid="printButton-1"]').click();
+  };
 }
 
 export default new VitalsListPage();
