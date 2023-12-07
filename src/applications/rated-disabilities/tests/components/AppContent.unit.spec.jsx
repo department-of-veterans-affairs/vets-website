008d42e3ad05c80041c80b44b2b0dabb4b06f986
jerekshoe <jerek.shoemaker@adhocteam.us>
2023-12-07 12:50:42 -0600

    Saving progress

--- src/applications/rated-disabilities/tests/components/AppContent.unit.spec.jsx
+++ src/applications/rated-disabilities/tests/components/AppContent.unit.spec.jsx
@@ -1,4 +1,28 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
 
 import AppContent from '../../components/AppContent';
+
+const badStatusUser = {
+  profile: {
+    status: 'NOT_FOUND',
+    verified: true,
+  },
+};
+
+const unverifiedUser = {
+  profile: {
+    status: 'OK',
+    verified: false,
+  },
+},
+
+
+describe('<AppContent>', () => {
+  context('when the users profile is not as expected', () => {
+    it('should render an error when they are not verified', () => {
+      const screen = render(<AppContent user={unverifiedUser} />)
+    });
+  });
+});
