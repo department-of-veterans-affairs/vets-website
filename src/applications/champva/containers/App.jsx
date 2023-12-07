a923f8377805a325261f7d5dff8d95762aa1507e
Michael Clement <michael.clement@adhocteam.us>
2023-12-07 10:11:17 -0600

    added breadcrumb

--- src/applications/champva/containers/App.jsx
+++ src/applications/champva/containers/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+import { VaBreadcrumbs } from '@department-of-veterans-affairs/web-components/react-bindings';
 import formConfig from '../config/form';
 
 export default function App({ location, children }) {
+  const renderBreadcrumbs = () => {
+    return [
+      {
+        href: '/',
+        label: 'Home',
+      },
+      {
+        href: '/application-for-champva-benefits',
+        label: 'Apply for CHAMPVA',
+      },
+    ];
+  };
   return (
-    <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-      {children}
-    </RoutedSavableApp>
+    <>
+      <div className="row">
+        <VaBreadcrumbs breadcrumbList={renderBreadcrumbs()} uswds />
+      </div>
+      <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+        {children}
+      </RoutedSavableApp>
+    </>
   );
 }
