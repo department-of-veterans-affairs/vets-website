aa46d287fbddaf3f7dc9afe7cf328fcdaa904f95
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2023-12-07 07:41:07 -0700

    Remove unit tests

--- src/applications/pensions/tests/pages/currentSpouse.unit.spec.jsx
+++ src/applications/pensions/tests/pages/currentSpouse.unit.spec.jsx
@@ -4,7 +4,6 @@ import {
 } from './pageTests.spec';
 import formConfig from '../../config/form';
 import currentSpouse from '../../pages/currentSpouse';
-import currentSpouseMonthlySupport from '../../pages/currentSpouseMonthlySupport';
 
 describe('current spouse full name page', () => {
   it('should render web components in form correctly', () => {
@@ -23,29 +22,6 @@ describe('current spouse full name page', () => {
       formConfig,
       currentSpouse.schema,
       currentSpouse.uiSchema,
-      expectedNumberOfErrors,
-      pageTitle,
-    );
-  });
-});
-
-describe('current spouse monthly support page', () => {
-  it('should render web components in form correctly', () => {
-    const pageTitle = 'current spouse monthly support';
-    const expectedNumberOfFields = 1;
-    testNumberOfWebComponentFields(
-      formConfig,
-      currentSpouseMonthlySupport.schema,
-      currentSpouseMonthlySupport.uiSchema,
-      expectedNumberOfFields,
-      pageTitle,
-    );
-
-    const expectedNumberOfErrors = 1;
-    testNumberOfErrorsOnSubmitForWebComponents(
-      formConfig,
-      currentSpouseMonthlySupport.schema,
-      currentSpouseMonthlySupport.uiSchema,
       expectedNumberOfErrors,
       pageTitle,
     );
