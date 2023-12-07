47d0f1300c04329a196d6552826cbcabfe7cca04
Daniel Cloud <daniel.cloud@adhocteam.us>
2023-12-07 15:57:56 -0500

    Collapse medications e2e tests into one file

--- src/applications/mhv-landing-page/tests/medications/e2e/medications-landing-page.cypress.spec.js
+++ src/applications/mhv-landing-page/tests/medications/e2e/medications-landing-page.cypress.spec.js
@@ -1,15 +1,24 @@
 import vamcEhr from '../../fixtures/vamc-ehr.json';
-import featureToggles from '../../fixtures/feature-toggles.json';
+
+const medicationFeatureToggles = (enabled = true) => ({
+  data: {
+    type: 'feature_toggles',
+    features: [
+      {
+        name: 'mhv_medications_to_va_gov_release',
+        value: enabled,
+      },
+    ],
+  },
+});
 
 describe('Medications Landing Page', () => {
   beforeEach(() => {
     cy.intercept('GET', '/data/cms/vamc-ehr.json', vamcEhr).as('vamcEhr');
-    cy.intercept('GET', '/v0/feature_toggles*', featureToggles).as(
-      'featureToggles',
-    );
   });
 
   it('visits Medications landing Page', () => {
+    cy.intercept('GET', '/v0/feature_toggles*', medicationFeatureToggles(true));
     cy.login();
     cy.visit('my-health/about-medications/');
 
@@ -21,5 +30,21 @@ describe('Medications Landing Page', () => {
         },
       },
     });
+  });
+
+  it('redirects from Medications landing Page when feature toggle is off', () => {
+    const redirectPath = '/health-care/refill-track-prescriptions';
+    cy.intercept('GET', redirectPath, '').as('refillRedirect');
+    cy.intercept(
+      'GET',
+      '/v0/feature_toggles*',
+      medicationFeatureToggles(false),
+    );
+
+    // Login when feature disabled, expect redirect
+    cy.login();
+    cy.visit('my-health/about-medications/');
+
+    cy.wait('@refillRedirect');
   });
 });
