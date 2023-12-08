9d4887c72d7ddc69fca82aabe102653fcb56c7a2
Aaron Ponce <aaron.m.ponce@gmail.com>
2023-12-07 16:28:13 -0800

    Add dependents text

--- src/applications/financial-status-report/components/householdIncome/SpouseTransitionExplainer.jsx
+++ src/applications/financial-status-report/components/householdIncome/SpouseTransitionExplainer.jsx
@@ -12,7 +12,7 @@ const SpouseTransitionExplainer = ({
     <ExplainerComponent
       headline="You added a spouse"
       strongMessage="You will now be asked additional questions about your spouse’s
-      income and employment."
+      income and employment. Your spouse is considered a dependent. On the dependents page, include your spouse as a dependent."
       detailMessage="  After you answer these questions, you can continue back to the
       review page."
       contentBeforeButtons={contentBeforeButtons}
