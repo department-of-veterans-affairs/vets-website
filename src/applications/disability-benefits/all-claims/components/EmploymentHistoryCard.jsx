c966cc7b791057b8b9edcd5be0e90737ac35c09b
Christine <127446042+christinec-fftc@users.noreply.github.com>
2023-12-11 09:39:10 -0800

    69814 add unit test coverage and docs for components

--- src/applications/disability-benefits/all-claims/components/EmploymentHistoryCard.jsx
+++ src/applications/disability-benefits/all-claims/components/EmploymentHistoryCard.jsx
@@ -1,8 +1,9 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import { formatDateRange } from '../utils';
 
-export default ({ formData }) => {
+export default function EmploymentHistoryCard({ formData }) {
   const { name, dates } = formData;
 
   return (
@@ -12,4 +13,11 @@ export default ({ formData }) => {
       {formatDateRange(dates)}
     </p>
   );
+}
+
+EmploymentHistoryCard.propTypes = {
+  formData: PropTypes.shape({
+    dates: PropTypes.object,
+    name: PropTypes.string,
+  }),
 };
--- src/applications/disability-benefits/all-claims/components/PeriodOfConfinement.jsx
+++ src/applications/disability-benefits/all-claims/components/PeriodOfConfinement.jsx
@@ -1,6 +1,11 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { formatDateRange } from '../utils';
 
 export default function PeriodOfConfinement({ formData = {} }) {
   return <div>{formatDateRange(formData)}</div>;
 }
+
+PeriodOfConfinement.propTypes = {
+  formData: PropTypes.object,
+};
