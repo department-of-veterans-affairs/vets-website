7a4da53e4b1c88a79ae19f3efe0814976c6153f5
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2023-12-11 15:31:09 -0600

    vfep-1011 get 0994 branches above 80% coverage (#27079)
    * Form 0994 - VFEP-1010, VFEP-1012, VFEP-1013 Get statements, functions, and lines above 80% coverage.
    
    * vfep-1011 get 0994 branches above 80% coverage

--- src/applications/edu-benefits/0994/tests/config/submitForm.unit.spec.js
+++ src/applications/edu-benefits/0994/tests/config/submitForm.unit.spec.js
@@ -4,6 +4,7 @@ import * as actions from 'platform/forms-system/src/js/actions';
 import * as helpers from 'platform/forms-system/src/js/helpers';
 import { get } from 'lodash';
 import submitForm from '../../config/submitForm';
+import { transform } from '../../../0993/helpers';
 
 describe('submitForm', () => {
   let form;
@@ -68,39 +69,26 @@ describe('submitForm', () => {
       ),
     ).to.be.false;
   });
-  /*
-  it('should correctly set edu-currently-enrolled based on isEnrolledStem', async () => {
-    form.data.isEnrolledStem = true;
+  it('should create correct eventData and call submitToUrl with it use transformForSubmit', async () => {
+    formConfig = {
+      transformForSubmit: transform,
+    };
     await submitForm(form, formConfig);
-    let eventData = submitToUrlStub.firstCall.args[3];
-    expect(eventData['edu-currently-enrolled']).to.equal('Yes');
 
-    form.data.isEnrolledStem = false;
-    await submitForm(form, formConfig);
-    eventData = submitToUrlStub.secondCall.args[3];
-    expect(eventData['edu-currently-enrolled']).to.equal('No');
+    expect(transformForSubmitStub.calledWith(formConfig, form)).to.be.true;
+
+    const expectedEventData = {
+      'edu-benefits-currently-used': 'benefit1 benefit3',
+      'edu-currently-enrolled': 'No',
+      'edu-pursuing-teaching-certification': 'Yes',
+    };
+    expect(
+      submitToUrlStub.calledWith(
+        sinon.match.any,
+        formConfig.submitUrl,
+        formConfig.trackingPrefix,
+        sinon.match(expectedEventData),
+      ),
+    ).to.be.false;
   });
-  it('should correctly set edu-pursuing-teaching-certification based on isPursuingTeachingCert', async () => {
-    form.data.isPursuingTeachingCert = true;
-    await submitForm(form, formConfig);
-    let eventData = submitToUrlStub.firstCall.args[3];
-    expect(eventData['edu-pursuing-teaching-certification']).to.equal('Yes');
-
-    form.data.isPursuingTeachingCert = false;
-    await submitForm(form, formConfig);
-    eventData = submitToUrlStub.secondCall.args[3];
-    expect(eventData['edu-pursuing-teaching-certification']).to.equal('No');
-  });
-
-  it('should correctly set active-duty based on isActiveDuty', async () => {
-    form.data.isActiveDuty = true;
-    await submitForm(form, formConfig);
-    let eventData = submitToUrlStub.firstCall.args[3];
-    expect(eventData['active-duty']).to.equal('Yes');
-
-    form.data.isActiveDuty = false;
-    await submitForm(form, formConfig);
-    eventData = submitToUrlStub.secondCall.args[3];
-    expect(eventData['active-duty']).to.equal('No');
-  }); */
 });
