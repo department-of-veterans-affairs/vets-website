c03da17865602a99d709c8158459d72069b9d3aa
Adam Antonioli <adam.antonioli@va.gov>
2023-12-11 09:27:42 -0500

    MBMS-53586 fix unit test for pre-need integration form preparer pages

--- src/applications/pre-need-integration/tests/config/preparerContactDetails.unit.spec.jsx
+++ src/applications/pre-need-integration/tests/config/preparerContactDetails.unit.spec.jsx
@@ -50,7 +50,7 @@ describe('Pre-need preparer info', () => {
       'USA',
     );
 
-    expect($$('input', formDOM).length).to.equal(5);
+    expect($$('input', formDOM).length).to.equal(6);
     expect($$('select', formDOM).length).to.equal(2);
   });
 
@@ -74,7 +74,7 @@ describe('Pre-need preparer info', () => {
       'MEX',
     );
 
-    expect($$('input', formDOM).length).to.equal(5);
+    expect($$('input', formDOM).length).to.equal(6);
     expect($$('select', formDOM).length).to.equal(1);
   });
 });
