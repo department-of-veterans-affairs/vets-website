76f8fa29289680a2bd6eeed195c9e8259688525f
Aaron Read <Aaron.Read@va.gov>
2023-12-12 17:09:09 -0600

    Update medical-records-view-vitals-details.cypress.spec.js

--- src/applications/mhv/medical-records/tests/e2e/medical-records-view-vitals-details.cypress.spec.js
+++ src/applications/mhv/medical-records/tests/e2e/medical-records-view-vitals-details.cypress.spec.js
@@ -69,7 +69,7 @@ describe('Medical Records Vitals Details Page', () => {
 
   it('Vitals Details Weight', () => {
     // Click Vitals Page Weight Link
-    VitalsDetailsPage.clickBloodPressureLink(1);
+    VitalsDetailsPage.clickBloodPressureLink(2);
     // Verify Vital Date
     VitalsDetailsPage.verifyVitalDate('August 9, 1999');
     // Verify Vital Result
@@ -90,7 +90,7 @@ describe('Medical Records Vitals Details Page', () => {
   });
   it('Vitals Details Pain', () => {
     // Click Vitals Page Pain Link
-    VitalsDetailsPage.clickBloodPressureLink(1);
+    VitalsDetailsPage.clickBloodPressureLink(3);
     // Verify Vital Date
     VitalsDetailsPage.verifyVitalDate('September 14, 2000');
     // Verify Vital Result
