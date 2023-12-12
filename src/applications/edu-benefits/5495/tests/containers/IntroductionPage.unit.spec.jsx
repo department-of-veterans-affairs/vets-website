ab2660edae341044b0b3ef4a9b260baac3abc3ce
fatmakhan0395 <145523589+fatmakhan0395@users.noreply.github.com>
2023-12-11 20:13:40 -0800

    vfep-909 statements and functions are above 80% test coverage for form 5495 (#27037)

--- src/applications/edu-benefits/5495/tests/containers/IntroductionPage.unit.spec.jsx
+++ src/applications/edu-benefits/5495/tests/containers/IntroductionPage.unit.spec.jsx
@@ -1,39 +1,31 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import { IntroductionPage } from 'applications/edu-benefits/5495/containers/IntroductionPage';
+import {
+  IntroductionPage,
+  mapStateToProps,
+} from 'applications/edu-benefits/5495/containers/IntroductionPage';
 import {
   WIZARD_STATUS_COMPLETE,
   getWizardStatus,
 } from 'applications/static-pages/wizard';
 
 describe('the Edu-Benefit 5495 Introduction Page', () => {
+  const fakeStore = {
+    getState: () => ({
+      showWizard: true,
+      route: { formConfig: {} },
+    }),
+    subscribe: () => {},
+    dispatch: () => {},
+  };
   it('should show the subway map if showWizard is set to false', () => {
-    const fakeStore = {
-      getState: () => ({
-        showWizard: false,
-        route: { formConfig: {} },
-      }),
-      subscribe: () => {},
-      dispatch: () => {},
-    };
-
     const wrapper = shallow(<IntroductionPage {...fakeStore.getState()} />);
     expect(wrapper.exists('WizardContainer')).to.equal(false);
     expect(wrapper.exists('.subway-map')).to.equal(true);
     wrapper.unmount();
   });
-
   it('should show the subway map if the wizard was completed', () => {
-    const fakeStore = {
-      getState: () => ({
-        showWizard: true,
-        route: { formConfig: {} },
-      }),
-      subscribe: () => {},
-      dispatch: () => {},
-    };
-
     const wrapper = shallow(<IntroductionPage {...fakeStore.getState()} />);
     const instance = wrapper.instance();
     instance.setWizardStatus(WIZARD_STATUS_COMPLETE);
@@ -43,5 +35,28 @@ describe('the Edu-Benefit 5495 Introduction Page', () => {
     expect(wrapper.exists('WizardContainer')).to.equal(false);
     expect(wrapper.exists('.subway-map')).to.equal(true);
     wrapper.unmount();
+  });
+  it('returns null when showWizard is undefined', () => {
+    const newProps = {
+      getState: () => ({
+        route: { formConfig: {} },
+      }),
+      subscribe: () => {},
+      dispatch: () => {},
+    };
+    const wrapper = shallow(<IntroductionPage {...newProps.getState()} />);
+    expect(wrapper.isEmptyRender()).to.be.true;
+    wrapper.unmount();
+  });
+  it('correctly maps showWizard from state', () => {
+    const state = {
+      form: {
+        showWizard: true,
+      },
+    };
+    const result = mapStateToProps(state);
+    expect(result).to.deep.equal({
+      showWizard: undefined,
+    });
   });
 });
