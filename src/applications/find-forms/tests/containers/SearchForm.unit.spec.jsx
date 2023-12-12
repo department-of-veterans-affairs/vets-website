e97cb454f73552ecb70517d1abef266a4ba2d74c
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2023-12-12 17:42:19 -0500

    fixes it #2

--- src/applications/find-forms/tests/containers/SearchForm.unit.spec.jsx
+++ src/applications/find-forms/tests/containers/SearchForm.unit.spec.jsx
@@ -11,40 +11,34 @@ describe('Find VA Forms <SearchForm>', () => {
     global.window.location = oldLocation;
   });
 
-  context('when a search query is added', () => {
-    it('should fetch data on mount', async () => {
-      global.window.location = { search: '?q=health' };
-      const spy1 = sinon.spy();
-      const { queryByTestId } = render(<SearchForm fetchForms={spy1} />);
+  it('should fetch data on mount when a search query is added', async () => {
+    global.window.location = { search: '?q=health' };
+    const spy1 = sinon.spy();
+    const { queryByTestId } = render(<SearchForm fetchForms={spy1} />);
+    await waitFor(() => {
       expect(spy1.called).to.be.true;
       expect(spy1.calledWith('health')).to.be.true;
-      await waitFor(() => {
-        expect(queryByTestId(/find-form-error-body/i)).to.be.null;
-      });
+      expect(queryByTestId(/find-form-error-body/i)).to.be.null;
     });
   });
 
-  context('when there is no search query added', () => {
-    it('should not fetch data', async () => {
-      global.window.location = { search: '?q=' };
-      const spy2 = sinon.spy();
-      const { queryByTestId } = render(<SearchForm fetchForms={spy2} />);
+  it('should not fetch data when there is no search query', async () => {
+    global.window.location = { search: '?q=' };
+    const spy2 = sinon.spy();
+    const { queryByTestId } = render(<SearchForm fetchForms={spy2} />);
+    await waitFor(() => {
+      expect(queryByTestId(/find-form-error-body/i)).to.be.null;
       expect(spy2.called).to.be.false;
-      await waitFor(() => {
-        expect(queryByTestId(/find-form-error-body/i)).to.be.null;
-      });
     });
   });
 
-  context('when there is a one-character query added', () => {
-    it('should not fetch data and show an error', async () => {
-      global.window.location = { search: '?q=a' };
-      const spy3 = sinon.spy();
-      const { queryByTestId } = render(<SearchForm fetchForms={spy3} />);
+  it('should not fetch data and show an error when there is only 1 character search query', async () => {
+    global.window.location = { search: '?q=a' };
+    const spy3 = sinon.spy();
+    const { queryByTestId } = render(<SearchForm fetchForms={spy3} />);
+    await waitFor(() => {
       expect(spy3.called).to.be.false;
-      await waitFor(() => {
-        expect(queryByTestId(/find-form-error-body/i)).not.to.be.null;
-      });
+      expect(queryByTestId(/find-form-error-body/i)).not.to.be.null;
     });
   });
 });
