0bd23b6f4accea5e432ac7fb9258a298dcbcbb46
fazilqa <fazil.mammadov@bylight.com>
2023-12-12 13:35:50 -0800

    upd: change draft messageBody

--- src/applications/mhv/secure-messaging/tests/e2e/draft-page-test/secure-messaging-multiple-drafts-resave.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/draft-page-test/secure-messaging-multiple-drafts-resave.cypress.spec.js
@@ -2,9 +2,9 @@ import SecureMessagingSite from '../sm_site/SecureMessagingSite';
 import PatientInboxPage from '../pages/PatientInboxPage';
 import { AXE_CONTEXT } from '../utils/constants';
 import PatientMessageDraftsPage from '../pages/PatientMessageDraftsPage';
-// import mockMultiDraftsResponse from '../fixtures/draftsResponse/multi-draft-response.json';
+import mockMultiDraftsResponse from '../fixtures/draftsResponse/multi-draft-response.json';
 
-describe('handle multiple drafts in one thread', () => {
+describe('re-save multiple drafts in one thread', () => {
   const site = new SecureMessagingSite();
   const landingPage = new PatientInboxPage();
   const draftPage = new PatientMessageDraftsPage();
@@ -12,7 +12,7 @@ describe('handle multiple drafts in one thread', () => {
   it('verify first draft could be re-saved', () => {
     site.login();
     landingPage.loadInboxMessages();
-    draftPage.loadMultiDraftThread();
+    draftPage.loadMultiDraftThread(mockMultiDraftsResponse);
 
     cy.injectAxe();
     cy.axeCheck(AXE_CONTEXT, {
@@ -22,6 +22,21 @@ describe('handle multiple drafts in one thread', () => {
         },
       },
     });
+
+    // cy.get('[id="reply-message-body"]').shadow().find('#textarea', ).should('be.enabled', {timeout: 10000}).type('newText',{ waitForAnimations: true })
+
+    cy.get('[id="reply-message-body"]')
+      .shadow()
+      .find('#textarea')
+      .should('be.enabled')
+      .click({ waitForAnimations: true });
+    // cy.wait(10000)
+    cy.get('[id="reply-message-body"]')
+      .shadow()
+      .find('#textarea')
+      .type('newText', { waitForAnimations: true });
+
+    // cy.get('#textarea').should('be.enabled').type('newText', {waitForAnimations: true})
   });
 
   it('verify second draft could be re-saved', () => {
