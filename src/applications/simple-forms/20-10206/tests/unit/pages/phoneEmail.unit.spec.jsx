00ed9ef6f045051151d55d320a8a3ef2c72be67b
Tze-chiu Lei <tze@adhocteam.us>
2023-12-13 09:25:26 -0700

    Restore 20-10206 unit-tests (#27110)

---
+++ src/applications/simple-forms/20-10206/tests/unit/pages/phoneEmail.unit.spec.jsx
@@ -0,0 +1,30 @@
+import {
+  testNumberOfErrorsOnSubmitForWebComponents,
+  testNumberOfWebComponentFields,
+} from '../../../../shared/tests/pages/pageTests.spec';
+import formConfig from '../../../config/form';
+
+const {
+  schema,
+  uiSchema,
+} = formConfig.chapters.contactInformationChapter.pages.phoneEmailPage;
+
+const pageTitle = 'Your contact information';
+
+const expectedNumberOfFields = 2;
+testNumberOfWebComponentFields(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfFields,
+  pageTitle,
+);
+
+const expectedNumberOfErrors = 1;
+testNumberOfErrorsOnSubmitForWebComponents(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfErrors,
+  pageTitle,
+);
