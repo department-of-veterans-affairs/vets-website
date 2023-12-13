e6d81ea16fced7825c2a3c2368dc342c676fe592
samcoforma <105806934+samcoforma@users.noreply.github.com>
2023-12-13 12:16:35 -0800

    Updated claim contention list design and functionality (#27073)
    * updated claim contention list design and functionality
    
    * added unit test for ClaimContentionList
    
    * updated ClaimDetailLayout unit test to remove contention list assertions covered by another unit test

--- src/applications/claims-status/tests/components/ClaimDetailLayout.unit.spec.jsx
+++ src/applications/claims-status/tests/components/ClaimDetailLayout.unit.spec.jsx
@@ -38,44 +38,6 @@ describe('<ClaimDetailLayout>', () => {
     expect(tree.everySubTree('ClaimsUnavailable')).to.have.lengthOf(1);
   });
 
-  it('should render contention list', () => {
-    const claim = {
-      attributes: {
-        contentions: [{ name: 'Condition 1' }, { name: 'Condition 2' }],
-      },
-    };
-
-    const tree = SkinDeep.shallowRender(<ClaimDetailLayout claim={claim} />);
-
-    expect(tree.subTree('.claim-contentions').text()).to.contain(
-      'Condition 1, Condition 2',
-    );
-  });
-
-  it('should render see all link if long contention list', () => {
-    const claim = {
-      id: 5,
-      attributes: {
-        contentions: [
-          { name: 'Condition 1' },
-          { name: 'Condition 2' },
-          { name: 'Condition 3' },
-          { name: 'Condition 4' },
-          { name: 'Condition 5' },
-        ],
-      },
-    };
-
-    const tree = SkinDeep.shallowRender(<ClaimDetailLayout claim={claim} />);
-
-    expect(tree.subTree('.claim-contentions').text()).to.contain(
-      'Condition 1, Condition 2, Condition 3',
-    );
-    expect(
-      tree.subTree('.claim-contentions').subTree('Link').props.to,
-    ).to.equal('your-claims/5/details');
-  });
-
   it('should render when the claim was submitted', () => {
     const claim = {
       attributes: {
@@ -92,20 +54,6 @@ describe('<ClaimDetailLayout>', () => {
 
     expect(screen.getByRole('heading', { level: 1 })).to.contain.text(
       'Submitted on November 23, 2023',
-    );
-  });
-
-  it('should render not available if no contention list', () => {
-    const claim = {
-      attributes: {
-        contentions: [],
-      },
-    };
-
-    const tree = SkinDeep.shallowRender(<ClaimDetailLayout claim={claim} />);
-
-    expect(tree.subTree('.claim-contentions').text()).to.contain(
-      'Not available',
     );
   });
 
