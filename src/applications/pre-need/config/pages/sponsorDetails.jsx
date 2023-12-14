c1118dd7ec4c9998aedb8c3cf7fa224ccf68348a
ConnorJeff <122034971+ConnorJeff@users.noreply.github.com>
2023-12-14 14:30:07 -0500

    Removed Prod Flag from MBMS-45330 (#27116)
    * Initial Commit. Removed Prod Flag from MBMS-47184
    
    * Removed Prod Flags from MBMS-47182
    
    * Missed last prod flag

--- src/applications/pre-need/config/pages/sponsorDetails.jsx
+++ src/applications/pre-need/config/pages/sponsorDetails.jsx
@@ -1,10 +1,7 @@
 import fullSchemaPreNeed from 'vets-json-schema/dist/40-10007-schema.json';
 
 import { merge, pick } from 'lodash';
-import applicantDescription from 'platform/forms/components/ApplicantDescription';
 import currentOrPastDateUI from 'platform/forms-system/src/js/definitions/currentOrPastDate';
-
-import environment from 'platform/utilities/environment';
 
 import fullNameUI from 'platform/forms/definitions/fullName';
 import {
@@ -16,130 +13,72 @@ import {
 
 const { veteran } = fullSchemaPreNeed.properties.application.properties;
 
-// prod flag for MBMS-47182
-export const uiSchema = !environment.isProduction()
-  ? {
-      'ui:title': sponsorDetailsSubHeader,
-      application: {
-        veteran: merge({}, veteranUI, {
-          'view:sponsorDetailsDescription': {
-            'ui:description': sponsorDetailsDescription,
-            'ui:options': {
-              displayEmptyObjectOnReview: true,
-            },
-          },
-          currentName: merge({}, fullNameUI, {
-            first: {
-              'ui:title': 'Sponsor’s first name',
-            },
-            last: {
-              'ui:title': 'Sponsor’s last name',
-            },
-            middle: {
-              'ui:title': 'Sponsor’s middle name',
-            },
-            suffix: {
-              'ui:title': 'Sponsor’s suffix',
-            },
-            maiden: {
-              'ui:title': 'Sponsor’s maiden name',
-            },
-            'ui:order': ['first', 'middle', 'last', 'suffix', 'maiden'],
-          }),
-          ssn: {
-            ...ssnDashesUI,
-            'ui:title': 'Sponsor’s Social Security number',
-          },
-          dateOfBirth: currentOrPastDateUI('Sponsor’s date of birth'),
-          placeOfBirth: {
-            'ui:title': "Sponsor's place of birth (City, State, or Territory)",
-          },
-        }),
+export const uiSchema = {
+  'ui:title': sponsorDetailsSubHeader,
+  application: {
+    veteran: merge({}, veteranUI, {
+      'view:sponsorDetailsDescription': {
+        'ui:description': sponsorDetailsDescription,
+        'ui:options': {
+          displayEmptyObjectOnReview: true,
+        },
       },
-    }
-  : {
-      'ui:title': sponsorDetailsSubHeader,
-      'ui:description': applicantDescription,
-      application: {
-        veteran: merge({}, veteranUI, {
-          currentName: merge({}, fullNameUI, {
-            first: {
-              'ui:title': 'Sponsor’s first name',
-            },
-            last: {
-              'ui:title': 'Sponsor’s last name',
-            },
-            middle: {
-              'ui:title': 'Sponsor’s middle name',
-            },
-            suffix: {
-              'ui:title': 'Sponsor’s suffix',
-            },
-            maiden: {
-              'ui:title': 'Sponsor’s maiden name',
-            },
-            'ui:order': ['first', 'middle', 'last', 'suffix', 'maiden'],
-          }),
-          ssn: {
-            ...ssnDashesUI,
-            'ui:title': 'Sponsor’s Social Security number',
-          },
-          dateOfBirth: currentOrPastDateUI('Sponsor’s date of birth'),
-          placeOfBirth: {
-            'ui:title': "Sponsor's place of birth (City, State, or Territory)",
-          },
-        }),
+      currentName: merge({}, fullNameUI, {
+        first: {
+          'ui:title': 'Sponsor’s first name',
+        },
+        last: {
+          'ui:title': 'Sponsor’s last name',
+        },
+        middle: {
+          'ui:title': 'Sponsor’s middle name',
+        },
+        suffix: {
+          'ui:title': 'Sponsor’s suffix',
+        },
+        maiden: {
+          'ui:title': 'Sponsor’s maiden name',
+        },
+        'ui:order': ['first', 'middle', 'last', 'suffix', 'maiden'],
+      }),
+      ssn: {
+        ...ssnDashesUI,
+        'ui:title': 'Sponsor’s Social Security number',
       },
-    };
+      dateOfBirth: currentOrPastDateUI('Sponsor’s date of birth'),
+      placeOfBirth: {
+        'ui:title': "Sponsor's place of birth (City, State, or Territory)",
+      },
+    }),
+  },
+};
 
-// prod flag for MBMS-47182
-export const schema = !environment.isProduction()
-  ? {
+export const schema = {
+  type: 'object',
+  properties: {
+    application: {
       type: 'object',
       properties: {
-        application: {
+        veteran: {
           type: 'object',
-          properties: {
-            veteran: {
-              type: 'object',
-              required: ['ssn'],
-              properties: merge(
-                {},
-                {
-                  'view:sponsorDetailsDescription': {
-                    type: 'object',
-                    properties: {},
-                  },
-                },
-                pick(veteran.properties, [
-                  'currentName',
-                  'ssn',
-                  'dateOfBirth',
-                  'placeOfBirth',
-                ]),
-              ),
+          required: ['ssn'],
+          properties: merge(
+            {},
+            {
+              'view:sponsorDetailsDescription': {
+                type: 'object',
+                properties: {},
+              },
             },
-          },
+            pick(veteran.properties, [
+              'currentName',
+              'ssn',
+              'dateOfBirth',
+              'placeOfBirth',
+            ]),
+          ),
         },
       },
-    }
-  : {
-      type: 'object',
-      properties: {
-        application: {
-          type: 'object',
-          properties: {
-            veteran: {
-              type: 'object',
-              required: ['ssn'],
-              properties: pick(veteran.properties, [
-                'currentName',
-                'ssn',
-                'dateOfBirth',
-                'placeOfBirth',
-              ]),
-            },
-          },
-        },
-      },
-    };
+    },
+  },
+};
