27036b3af2daaea5f6ea270645c583261d2cd404
fazilqa <fazil.mammadov@bylight.com>
2023-12-14 13:09:48 -0800

    add: blocked-recipients-response.json

--- src/applications/mhv/secure-messaging/tests/e2e/triage-team-tests/secure-messaging-blocked-from-particular-group.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/triage-team-tests/secure-messaging-blocked-from-particular-group.cypress.spec.js
@@ -1,14 +1,23 @@
 import SecureMessagingSite from '../sm_site/SecureMessagingSite';
 import PatientInboxPage from '../pages/PatientInboxPage';
 import { AXE_CONTEXT } from '../utils/constants';
-// import mockBlockedRecipients from '../fixtures/recipientsResponse/blocked-recipients-response.json';
+import mockMessages from '../fixtures/messages-response.json';
+import mockSingleMessage from '../fixtures/inboxResponse/single-message-response.json';
+import mockBlockedRecipients from '../fixtures/recipientsResponse/blocked-recipients-response.json';
+import blockedThread from '../fixtures/recipientsResponse/thread-with-blocked-group-response.json';
 
 describe('Secure Messaging Compose', () => {
   it('can send message', () => {
     const landingPage = new PatientInboxPage();
     const site = new SecureMessagingSite();
     site.login();
-    landingPage.loadInboxMessages();
+    landingPage.loadInboxMessages(
+      mockMessages,
+      mockSingleMessage,
+      mockBlockedRecipients,
+    );
+
+    landingPage.loadSingleThread(blockedThread);
 
     cy.injectAxe();
     cy.axeCheck(AXE_CONTEXT, {
