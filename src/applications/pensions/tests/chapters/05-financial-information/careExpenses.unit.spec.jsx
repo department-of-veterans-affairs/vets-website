160272f542653663dd5ebd79854fbabe66ba076a
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2023-12-15 12:48:19 -0700

    Pension mvp 71727 Add care expenses (#27164)
    * Add care expenses
    
    * Remove rogue description

--- src/applications/pensions/tests/chapters/05-financial-information/careExpenses.unit.spec.jsx
+++ src/applications/pensions/tests/chapters/05-financial-information/careExpenses.unit.spec.jsx
@@ -1,6 +1,8 @@
 import {
-  testNumberOfErrorsOnSubmitForWebComponents,
   testNumberOfWebComponentFields,
+  testNumberOfErrorsOnSubmitForWebComponents,
+  testNumberOfErrorsOnSubmit,
+  testNumberOfFields,
 } from '../pageTests.spec';
 import formConfig from '../../../config/form';
 import careExpenses from '../../../config/chapters/05-financial-information/careExpenses';
@@ -9,8 +11,8 @@ const { schema, uiSchema } = careExpenses;
 
 describe('Unreimbursed care expenses pension page', () => {
   const pageTitle = 'Care expenses';
-  const expectedNumberOfFields = 1;
-  testNumberOfWebComponentFields(
+  const expectedNumberOfFields = 11;
+  testNumberOfFields(
     formConfig,
     schema,
     uiSchema,
@@ -18,12 +20,30 @@ describe('Unreimbursed care expenses pension page', () => {
     pageTitle,
   );
 
-  const expectedNumberOfErrors = 1;
-  testNumberOfErrorsOnSubmitForWebComponents(
+  const expectedNumberOfErrors = 3;
+  testNumberOfErrorsOnSubmit(
     formConfig,
     schema,
     uiSchema,
     expectedNumberOfErrors,
+    pageTitle,
+  );
+
+  const expectedNumberOfWebComponentFields = 3;
+  testNumberOfWebComponentFields(
+    formConfig,
+    schema,
+    uiSchema,
+    expectedNumberOfWebComponentFields,
+    pageTitle,
+  );
+
+  const expectedNumberOfErrorsForWebComponents = 3;
+  testNumberOfErrorsOnSubmitForWebComponents(
+    formConfig,
+    schema,
+    uiSchema,
+    expectedNumberOfErrorsForWebComponents,
     pageTitle,
   );
 });
