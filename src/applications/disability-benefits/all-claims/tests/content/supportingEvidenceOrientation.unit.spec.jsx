a7354c2d38aab373cfd792d0d3aa0424f241d79b
tyler-spangler6 <142833783+tyler-spangler6@users.noreply.github.com>
2023-12-15 09:48:48 -0700

    Add tests for supportingEvidenceOrientation.jsx (#27066)
    * added tests for supportingEvidenceOrientation.jsx
    
    * converted tests to react testing library
    
    * update test name in it() fields
    
    ---------
    
    Co-authored-by: Tyler Spangler <tylerspangler@Tyler-Spangler.local>

---
+++ src/applications/disability-benefits/all-claims/tests/content/supportingEvidenceOrientation.unit.spec.jsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+import { supportingEvidenceOrientation } from '../../content/supportingEvidenceOrientation';
+
+describe('supportingEvidenceOrientation', () => {
+  it('renders increase message when claiming only an increase', () => {
+    const formData = {
+      'view:claimType': {
+        'view:claimingNew': false,
+        'view:claimingIncrease': true,
+      },
+    };
+    const result = render(supportingEvidenceOrientation({ formData }));
+    const expectedString = 'Your rated service-connected disabilities';
+    result.getByText(expectedString);
+  });
+
+  it('renders new message when claiming a new condition', () => {
+    const formData = {
+      'view:claimType': {
+        'view:claimingNew': true,
+        'view:claimingIncrease': false,
+      },
+    };
+    const expectedString =
+      'Your new service-connected disabilities or conditions';
+    const result = render(supportingEvidenceOrientation({ formData }));
+    result.getByText(expectedString);
+  });
+});
