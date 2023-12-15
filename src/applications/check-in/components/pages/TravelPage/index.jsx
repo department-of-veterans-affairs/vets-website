5401bfaa683f243f0b4765c7be961cbcff34381c
Brian Seek <brian.seek@civicactions.com>
2023-12-15 11:15:58 -0800

    Check in/71432/unified review page (#27135)
    * adjsutments for unified flow.
    
    * Removed re-introduced tests, and added new e2e tests and text for unified.
    
    * Removed details that got back in.

--- src/applications/check-in/components/pages/TravelPage/index.jsx
+++ src/applications/check-in/components/pages/TravelPage/index.jsx
@@ -2,7 +2,6 @@ import React, { useLayoutEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-// eslint-disable-next-line import/no-unresolved
 import { recordEvent } from '@department-of-veterans-affairs/platform-monitoring/exports';
 
 import { recordAnswer } from '../../../actions/universal';
@@ -14,7 +13,6 @@ import {
   makeSelectApp,
   makeSelectForm,
 } from '../../../selectors';
-import { URLS } from '../../../utils/navigation';
 
 import BackButton from '../../BackButton';
 import Wrapper from '../../layout/Wrapper';
@@ -41,7 +39,6 @@ const TravelPage = ({
     goToPreviousPage,
     jumpToPage,
     getPreviousPageFromRouter,
-    // getNextPageFromRouter,
   } = useFormRouting(router);
 
   const selectCurrentContext = useMemo(makeSelectCurrentContext, []);
@@ -68,15 +65,9 @@ const TravelPage = ({
     if (answer === 'no' && noFunction) {
       noFunction();
     } else if (answer === 'no') {
-      jumpToPage(URLS.DETAILS);
+      jumpToPage(`complete/${data.activeAppointmentId}`);
     } else if (yesFunction) {
       yesFunction();
-      // =======
-      //     if (answer === 'no') {
-      //       jumpToPage(`complete/${data.activeAppointmentId}`);
-      //     } else if (answer === 'yes' && nextPage === 'complete') {
-      //       jumpToPage(`complete/${data.activeAppointmentId}`);
-      // >>>>>>> cf620ce629 (Check in/68865/remove appointment details list view (#26849))
     } else {
       goToNextPage();
     }
