1bcd0fe0c10eee2b8824ccf19e4de01788fd1af5
Michael Clement <michael.clement@adhocteam.us>
2023-12-15 11:50:02 -0600

    removed unnecessary feedback btn and fixed omb burden

--- src/applications/champva/components/GetFormHelp.jsx
+++ src/applications/champva/components/GetFormHelp.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
 import { VaTelephone } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import { VaButton } from '@department-of-veterans-affairs/web-components/react-bindings';
 
 function GetFormHelp() {
   return (
@@ -14,12 +13,6 @@ function GetFormHelp() {
         If you have hearing loss, call TTY:&nbsp;
         <VaTelephone contact={CONTACTS['711']} />.
       </p>
-      <br />
-      <hr className="help-heading vads-u-border-color--gray-lightest" />
-      {/* TODO: Just use a link without button? */}
-      <div className="vads-u-display--flex vads-u-justify-content--flex-end vads-u-flex-direction--row vads-u-width--full vads-u-text-decoration--none">
-        <VaButton onClick={() => {}} text="Feedback" />
-      </div>
     </div>
   );
 }
