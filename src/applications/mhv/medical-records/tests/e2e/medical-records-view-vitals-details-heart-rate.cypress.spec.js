9f9efec2e2f4b937c4c1c7fba619fa2efe3d0059
Anal Gomes <103195615+agomes82@users.noreply.github.com>
2023-12-18 12:11:54 -0500

    Mhv 52020 staging review validation for vitals (#27167)
    * MHV-52385 Vitals Details Page E2E Test
    
    MHV-52385 Vitals Details Page E2E Test
    
    * MHV-52020 Staging Review Validation for Vitals
    
    MHV-52020 Staging Review Validation for Vitals
    
    * Update medical-records-view-vitals-details.cypress.spec.js
    
    * MHV-52020 Staging Review Validation for Vitals
    
    MHV-52020 Staging Review Validation for Vitals
    
    * fixing naming conventions
    
    * Update medical-records-view-vitals-details.cypress.spec.js
    
    * separate heart rate out
    
    * Update medical-records-download-txt-functionality-vitals.cypress.spec.js
    
    * Update medical-records-view-vitals-details-heart-rate.cypress.spec.js
    
    * Update medical-records-view-vitals-details-heart-rate.cypress.spec.js
    
    ---------
    
    Co-authored-by: Aaron Read <Aaron.Read@va.gov>

---
+++ src/applications/mhv/medical-records/tests/e2e/medical-records-view-vitals-details-heart-rate.cypress.spec.js
@@ -0,0 +1,31 @@
+import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
+import VitalsDetailsPage from './pages/VitalsDetailsPage';
+
+describe.skip('Medical Records Vitals Details Page', () => {
+  const site = new MedicalRecordsSite();
+
+  beforeEach(() => {
+    site.login();
+    cy.visit('my-health/medical-records/vitals');
+  });
+
+  it('Vitals Details Heart Rate', () => {
+    // Click Vitals Page Heart Rate  Link
+    VitalsDetailsPage.clickHeartRateLink(1);
+    // Verify Vital Date
+    VitalsDetailsPage.verifyVitalDate('December 25, 2004 12:00 a.m.');
+    // Verify Vital Result
+    VitalsDetailsPage.verifyVitalResult('185 /min');
+    // Verify Vital Details Location
+    VitalsDetailsPage.verifyVitalLocation('None noted');
+    // Verify Vital Details  Provider Notes
+    VitalsDetailsPage.verifyVitalProviderNotes('a bit fast');
+    // Axe check
+    cy.injectAxe();
+    cy.axeCheck('main');
+  });
+
+  afterEach(() => {
+    VitalsDetailsPage.clickBreadCrumbsLink(0);
+  });
+});
