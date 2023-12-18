4c19507b882d036444162fe8359579533540abe5
Matthew Kerns <matthewkerns@Matthew-Kerns.lan>
2023-12-18 16:34:50 -0500

    Added new verbiage

---
+++ src/applications/ezr/components/FormDescriptions/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const DependentEducationExpensesDescription = (
+  <>
+    <span className="vads-u-display--block vads-u-color--gray-medium">
+      Only enter an amount if they had gross income to report to the IRS in
+      2022. This income is the minimum amount of gross income the IRS requires
+      to file a federal income tax return.
+    </span>
+  </>
+);
