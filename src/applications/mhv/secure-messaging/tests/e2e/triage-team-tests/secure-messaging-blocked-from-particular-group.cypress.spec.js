47c0825e3376bef3495010ecfe60fa46a17a9efa
fazilqa <fazil.mammadov@bylight.com>
2023-12-18 11:41:45 -0800

    upd: add more assertions

--- src/applications/mhv/secure-messaging/tests/e2e/triage-team-tests/secure-messaging-blocked-from-particular-group.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/triage-team-tests/secure-messaging-blocked-from-particular-group.cypress.spec.js
@@ -1,30 +1,26 @@
 import SecureMessagingSite from '../sm_site/SecureMessagingSite';
 import PatientInboxPage from '../pages/PatientInboxPage';
-import { AXE_CONTEXT } from '../utils/constants';
+import { AXE_CONTEXT, Locators } from '../utils/constants';
 import mockMessages from '../fixtures/messages-response.json';
 import mockSingleMessage from '../fixtures/inboxResponse/single-message-response.json';
 import mockBlockedRecipients from '../fixtures/recipientsResponse/blocked-recipients-response.json';
 import blockedThread from '../fixtures/recipientsResponse/thread-with-blocked-group-response.json';
 
-describe('Secure Messaging Compose', () => {
-  // const featureFlag = {
-  //     name: 'mhv_secure_messaging_blocked_triage_group_1_0',
-  //     value: true,
-  //   }
-  it('can send message', () => {
+describe('Blocked Triage Group', () => {
+  beforeEach(() => {
     const landingPage = new PatientInboxPage();
     const site = new SecureMessagingSite();
     site.login();
 
     landingPage.loadInboxMessages(
-      // featureFlag,
       mockMessages,
       mockSingleMessage,
       mockBlockedRecipients,
     );
 
     landingPage.loadSingleThread(blockedThread);
-
+  });
+  it('alert message', () => {
     cy.injectAxe();
     cy.axeCheck(AXE_CONTEXT, {
       rules: {
@@ -37,5 +33,29 @@ describe('Secure Messaging Compose', () => {
     cy.get('[class="alert-expandable-title"]')
       .should('be.visible')
       .and('include.text', `You can't send messages to`);
+
+    cy.get('[data-testid="blocked-triage-group-alert"]')
+      .shadow()
+      .find('#alert-body')
+      .should('have.class', 'closed');
+  });
+
+  it('alert message expandable', () => {
+    cy.get('[data-testid="blocked-triage-group-alert"]').click({
+      waitForAnimations: true,
+    });
+
+    cy.get('[data-testid="blocked-triage-group-alert"]')
+      .shadow()
+      .find('#alert-body')
+      .should('have.class', 'open');
+
+    cy.get('[data-testid="blocked-triage-group-alert"]')
+      .find('a')
+      .should('have.attr', 'href', '/find-locations/');
+  });
+
+  it('reply btn does not exist', () => {
+    cy.get(Locators.BUTTONS.REPLY).should('not.exist');
   });
 });
