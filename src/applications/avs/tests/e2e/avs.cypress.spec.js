783f49a4171c4d6669b16475835989b25e6ba1f0
Adrian Rollett <adrian.rollett@va.gov>
2023-12-19 15:06:58 -0700

    Add axe check

--- src/applications/avs/tests/e2e/avs.cypress.spec.js
+++ src/applications/avs/tests/e2e/avs.cypress.spec.js
@@ -1,16 +1,19 @@
 import manifest from '../../manifest.json';
 
-describe(manifest.appName, () => {
-  // Skip tests in CI until the app is released.
-  // Remove this block when the app has a content page in production.
-  // eslint-disable-next-line func-names
-  before(function() {
-    if (Cypress.env('CI')) this.skip();
+const avsId = '9A7AF40B2BC2471EA116891839113252';
+const testUrl = `${manifest.rootUrl}/${avsId}`;
+
+import avsData from '../fixtures/9A7AF40B2BC2471EA116891839113252.json';
+
+describe('After-visit Summary', () => {
+  beforeEach(() => {
+    cy.intercept('GET', `/avs/v0/avs/*`, avsData);
+    cy.login();
   });
 
   it('is accessible', () => {
-    cy.visit(manifest.rootUrl);
-    cy.get('h1').contains('AVS');
+    cy.visit(testUrl);
+    cy.get('h1').contains('After-visit summary');
     cy.injectAxeThenAxeCheck();
   });
 });
