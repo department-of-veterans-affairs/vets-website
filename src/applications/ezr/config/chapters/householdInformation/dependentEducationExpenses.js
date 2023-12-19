d4d85085cd064188b255c4af923221990bb1bb64
Joshua Drumm <34111449+JoshingYou1@users.noreply.github.com>
2023-12-19 14:24:07 -0500

    [#71795] [10-10EZR] Update Expenses Questions Flow and Verbiage (#27205)
    * Refactored dependent code
    
    * Fixed failing tests and adjusted some code
    
    * Added content changes from Matt K.'s branch

--- src/applications/ezr/config/chapters/householdInformation/dependentEducationExpenses.js
+++ src/applications/ezr/config/chapters/householdInformation/dependentEducationExpenses.js
@@ -8,6 +8,7 @@ import { replaceStrValues } from '../../../utils/helpers/general';
 import { validateCurrency } from '../../../utils/validation';
 import { LAST_YEAR } from '../../../utils/constants';
 import content from '../../../locales/en/content.json';
+import DependentEducationExpensesDescription from '../../../components/FormDescriptions/DependentEducationExpensesDescription';
 
 const {
   dependents: { items: dependent },
@@ -25,6 +26,7 @@ export default {
     dependentEducationExpenses: {
       ...currencyUI(content['household-dependent-education-expenses-label']),
       'ui:validations': [validateCurrency],
+      'ui:description': DependentEducationExpensesDescription,
     },
   },
   schema: {
