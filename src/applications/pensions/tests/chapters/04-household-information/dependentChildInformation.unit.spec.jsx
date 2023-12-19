ba8cac413a5869084130670d2199be3f348ecb92
Wayne Weibel <wayne.weibel@adhocteam.us>
2023-12-19 16:51:29 -0500

    test wip

--- src/applications/pensions/tests/chapters/04-household-information/dependentChildInformation.unit.spec.jsx
+++ src/applications/pensions/tests/chapters/04-household-information/dependentChildInformation.unit.spec.jsx
@@ -1,29 +1,163 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import ReactTestUtils from 'react-dom/test-utils';
+import moment from 'moment';
+
 import {
-  testNumberOfErrorsOnSubmitForWebComponents,
-  testNumberOfWebComponentFields,
-} from '../pageTests.spec';
+  DefinitionTester,
+  getFormDOM,
+} from '@department-of-veterans-affairs/platform-testing/schemaform-utils';
 import formConfig from '../../../config/form';
-import dependentChildInformation from '../../../config/chapters/04-household-information/dependentChildInformation';
 
-const { schema, uiSchema } = dependentChildInformation;
+const {
+  schema,
+  uiSchema,
+  arrayPath,
+  title,
+} = formConfig.chapters.householdInformation.pages.dependentChildInformation;
 
-describe('pension dependent child info page', () => {
-  const pageTitle = 'dependent child information';
-  const expectedNumberOfFields = 4;
-  testNumberOfWebComponentFields(
-    formConfig,
-    schema,
-    uiSchema,
-    expectedNumberOfFields,
-    pageTitle,
-  );
+const dependentData = {
+  'view:hasDependents': true,
+  dependents: [
+    {
+      fullName: {
+        first: 'Jane',
+        last: 'Doe',
+      },
+      childDateOfBirth: moment()
+        .subtract(19, 'years')
+        .toISOString(),
+    },
+  ],
+};
 
-  const expectedNumberOfErrors = 4;
-  testNumberOfErrorsOnSubmitForWebComponents(
-    formConfig,
-    schema,
-    uiSchema,
-    expectedNumberOfErrors,
-    pageTitle,
-  );
+describe('Child information page', () => {
+  // it('should render', () => {
+  //   const form = ReactTestUtils.renderIntoDocument(
+  //     <DefinitionTester
+  //       arrayPath={arrayPath}
+  //       pagePerItemIndex={0}
+  //       definitions={formConfig.defaultDefinitions}
+  //       schema={schema}
+  //       data={dependentData}
+  //       uiSchema={uiSchema}
+  //     />,
+  //   );
+  //   const formDOM = getFormDOM(form);
+
+  //   expect(formDOM.querySelectorAll('input, select, textarea').length).to.equal(
+  //     8,
+  //   );
+  // });
+
+  // it('should show errors when required fields are empty', () => {
+  //   const onSubmit = sinon.spy();
+  //   const form = ReactTestUtils.renderIntoDocument(
+  //     <DefinitionTester
+  //       arrayPath={arrayPath}
+  //       pagePerItemIndex={0}
+  //       definitions={formConfig.defaultDefinitions}
+  //       schema={schema}
+  //       onSubmit={onSubmit}
+  //       data={dependentData}
+  //       uiSchema={uiSchema}
+  //     />,
+  //   );
+  //   const formDOM = getFormDOM(form);
+  //   formDOM.submitForm(form);
+  //   expect(formDOM.querySelectorAll('.usa-input-error').length).to.equal(4);
+  //   expect(onSubmit.called).not.to.be.true;
+  // });
+
+  // it('should not require ssn if noSSN is checked', () => {
+  //   const onSubmit = sinon.spy();
+  //   const form = ReactTestUtils.renderIntoDocument(
+  //     <DefinitionTester
+  //       arrayPath={arrayPath}
+  //       pagePerItemIndex={0}
+  //       definitions={formConfig.defaultDefinitions}
+  //       schema={schema}
+  //       onSubmit={onSubmit}
+  //       data={dependentData}
+  //       uiSchema={uiSchema}
+  //     />,
+  //   );
+  //   const formDOM = getFormDOM(form);
+  //   formDOM.setCheckbox('#root_view\\:noSSN', true);
+  //   formDOM.submitForm(form);
+  //   const errors = formDOM.querySelectorAll('.usa-input-error-label');
+
+  //   expect(errors.length).to.equal(3);
+  //   expect(onSubmit.called).not.to.be.true;
+  // });
+
+  // it('should submit with valid data', () => {
+  //   const onSubmit = sinon.spy();
+  //   const form = ReactTestUtils.renderIntoDocument(
+  //     <DefinitionTester
+  //       arrayPath={arrayPath}
+  //       pagePerItemIndex={0}
+  //       definitions={formConfig.defaultDefinitions}
+  //       schema={schema}
+  //       data={dependentData}
+  //       onSubmit={onSubmit}
+  //       uiSchema={uiSchema}
+  //     />,
+  //   );
+
+  //   const formDOM = getFormDOM(form);
+
+  //   formDOM.fillData('#root_childPlaceOfBirth', 'sf');
+  //   formDOM.fillData('#root_childSocialSecurityNumber', '123123123');
+  //   formDOM.fillData('#root_childRelationship_0', 'biological');
+  //   formDOM.fillData('#root_previouslyMarriedNo', 'Y');
+
+  //   formDOM.submitForm(form);
+  //   expect(onSubmit.called).to.be.true;
+  // });
+
+  it('should ask if the child is in school', () => {
+    const onSubmit = sinon.spy();
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+        arrayPath={arrayPath}
+        pagePerItemIndex={0}
+        definitions={formConfig.defaultDefinitions}
+        schema={schema}
+        data={dependentData}
+        onSubmit={onSubmit}
+        uiSchema={uiSchema}
+      />,
+    );
+
+    const formDOM = getFormDOM(form);
+    expect(formDOM.querySelector('#root_attendingCollegeYes')).to.not.be.null;
+  });
+
+  it('should ask if the child is disabled', () => {
+    const onSubmit = sinon.spy();
+    const form = ReactTestUtils.renderIntoDocument(
+      <DefinitionTester
+        arrayPath={arrayPath}
+        pagePerItemIndex={0}
+        definitions={formConfig.defaultDefinitions}
+        schema={schema}
+        data={dependentData}
+        onSubmit={onSubmit}
+        uiSchema={uiSchema}
+      />,
+    );
+
+    const formDOM = getFormDOM(form);
+    expect(formDOM.querySelector('#root_disabledYes')).to.not.be.null;
+  });
+
+  it('should set the title to the dependents name if available', () => {
+    const pageTitle = title;
+    expect(pageTitle(dependentData.dependents[0])).to.eql(
+      'Jane Doe information',
+    );
+    expect(pageTitle({ fullName: {} })).to.eql('  information');
+  });
 });
