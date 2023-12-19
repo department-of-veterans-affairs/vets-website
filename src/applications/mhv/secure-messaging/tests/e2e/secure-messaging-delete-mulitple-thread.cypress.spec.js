9429912fc45e62947d0b60db790425aa84d28e4f
haniasma <121971510+haniasma@users.noreply.github.com>
2023-12-19 08:39:43 -0500

    Create secure-messaging-delete-mulitple-thread.cypress.spec.js

---
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-delete-mulitple-thread.cypress.spec.js
@@ -0,0 +1,68 @@
+import SecureMessagingSite from './sm_site/SecureMessagingSite';
+import PatientInboxPage from './pages/PatientInboxPage';
+import { AXE_CONTEXT } from './utils/constants';
+import PatientMessageDraftsPage from './pages/PatientMessageDraftsPage';
+import mockMultiDraftsResponse from './fixtures/draftsResponse/multi-draft-response.json';
+
+describe('delete multiple drafts thread', () => {
+  const site = new SecureMessagingSite();
+  const landingPage = new PatientInboxPage();
+  const draftPage = new PatientMessageDraftsPage();
+
+  beforeEach(() => {
+    site.login();
+    landingPage.loadInboxMessages();
+    draftPage.loadMultiDraftThread();
+  });
+
+  it('verify multiple draft deletion', () => {
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
+    draftPage.deleteDraftMessage(
+      mockMultiDraftsResponse,
+      mockMultiDraftsResponse.data[0].attributes.messageId,
+    );
+    draftPage.verifyDeleteConfirmationMessage();
+  });
+
+  it('verify drafts detailed vew', () => {
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
+
+    cy.get('[data-testid="message-body-field"]')
+      .should('have.attr', 'value')
+      .and('eq', mockMultiDraftsResponse.data[0].attributes.body);
+
+    cy.get('[text="Edit draft 1"]').click();
+    cy.get('[data-testid="message-body-field"]')
+      .should('have.attr', 'value')
+      .and('eq', mockMultiDraftsResponse.data[1].attributes.body);
+
+    cy.get('.message-body-draft-preview').should(
+      'have.text',
+      `${mockMultiDraftsResponse.data[0].attributes.body}`,
+    );
+
+    cy.get('[text="Edit draft 2"]').click();
+    cy.get('[data-testid="message-body-field"]')
+      .should('have.attr', 'value')
+      .and('eq', mockMultiDraftsResponse.data[0].attributes.body);
+
+    cy.get('.message-body-draft-preview').should(
+      'have.text',
+      `${mockMultiDraftsResponse.data[1].attributes.body}`,
+    );
+  });
+});
