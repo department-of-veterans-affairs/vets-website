c854aca6f9615cf875bd24c64841c05842cec21e
Zack Youngren <zack@youngren.io>
2023-12-20 14:36:55 -0500

    71855: 21P-527EZ Step 6 Copy Changes
    https://github.com/department-of-veterans-affairs/va.gov-team/issues/71855

---
+++ src/applications/pensions/config/chapters/06-additional-information/fasterClaimProcessing.js
@@ -0,0 +1,90 @@
+import React from 'react';
+
+import fullSchemaPensions from 'vets-json-schema/dist/21P-527EZ-schema.json';
+
+const { noRapidProcessing } = fullSchemaPensions.properties;
+
+const description = (
+  <div className="vads-u-margin-bottom--1">
+    <p>
+      If you’ve uploaded all your supporting documents, you may be able to get a
+      faster decision on your claim. We call this the Fully Developed Claims
+      (FDC) program.
+    </p>
+
+    <a href="https://www.va.gov/disability/how-to-file-claim/evidence-needed/fully-developed-claims/">
+      Learn more about fully developed claims
+    </a>
+  </div>
+);
+
+export const fdcWarning = (
+  <div className="usa-alert usa-alert-info background-color-only">
+    <div className="usa-alert-body">
+      <div className="usa-alert-text">
+        Your application will be submitted as a fully developed claim.
+      </div>
+    </div>
+  </div>
+);
+
+export const noFDCWarning = (
+  <div className="usa-alert usa-alert-info background-color-only">
+    <div className="usa-alert-body">
+      <div className="usa-alert-text">
+        Your application doesn’t qualify for the Fully Developed Claim (FDC)
+        program. We’ll review your claim through the standard claim process.
+        Please turn in any information to support your claim as soon as you can
+        to the address provided after you finish the application.
+      </div>
+    </div>
+  </div>
+);
+
+export default {
+  uiSchema: {
+    'ui:title': 'Faster claim processing (optional)',
+    'ui:description': description,
+    noRapidProcessing: {
+      'ui:title':
+        'Do you want to use the Fully Developed Claims program to apply?',
+      'ui:widget': 'yesNo',
+      'ui:options': {
+        yesNoReverse: true,
+        labels: {
+          Y:
+            'Yes. I’ve uploaded all my supporting documents for my pension application.',
+          N: 'No. I have other supporting documents to submit later.',
+        },
+      },
+    },
+    fdcWarning: {
+      'ui:description': fdcWarning,
+      'ui:options': {
+        expandUnder: 'noRapidProcessing',
+        expandUnderCondition: false,
+      },
+    },
+    noFDCWarning: {
+      'ui:description': noFDCWarning,
+      'ui:options': {
+        expandUnder: 'noRapidProcessing',
+        expandUnderCondition: true,
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      noRapidProcessing,
+      fdcWarning: {
+        type: 'object',
+        properties: {},
+      },
+      noFDCWarning: {
+        type: 'object',
+        properties: {},
+      },
+    },
+  },
+};
