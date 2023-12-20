26a46de79a77b15d49cab1127d69dfc9c433d8c4
jerekshoe <jerek.shoemaker@adhocteam.us>
2023-12-20 14:28:05 -0600

    Adding unit tests for the RatedDisabilityView component

--- src/applications/rated-disabilities/components/Learn.jsx
+++ src/applications/rated-disabilities/components/Learn.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+export const heading = 'Learn about VA disability ratings';
+
 export default function Learn() {
   return (
     <div className="vads-u-margin-top--4">
@@ -7,7 +9,7 @@ export default function Learn() {
         id="learn"
         className="vads-u-padding-bottom--1p5 vads-u-border-bottom--3px vads-u-border-color--primary vads-u-font-size--h3"
       >
-        Learn about VA disability ratings
+        {heading}
       </h2>
       <p>
         To learn how we determined your VA combined disability rating, use our
