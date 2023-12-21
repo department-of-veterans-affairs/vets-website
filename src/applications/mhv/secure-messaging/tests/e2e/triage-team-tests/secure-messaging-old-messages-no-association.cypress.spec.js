503833843f890835f3bf5a84c2fc59a680dea95f
fazilqa <fazil.mammadov@bylight.com>
2023-12-21 12:31:10 -0800

    upd: split to specs

---
+++ src/applications/mhv/secure-messaging/tests/e2e/triage-team-tests/secure-messaging-old-messages-no-association.cypress.spec.js
@@ -0,0 +1,162 @@
+import SecureMessagingSite from '../sm_site/SecureMessagingSite';
+import PatientInboxPage from '../pages/PatientInboxPage';
+import { AXE_CONTEXT, Locators } from '../utils/constants';
+import mockMessages from '../fixtures/messages-response.json';
+import mockSingleMessage from '../fixtures/inboxResponse/single-message-response.json';
+import mockRecipients from '../fixtures/recipients-response.json';
+import mockThread from '../fixtures/thread-response.json';
+
+describe('Verify old messages - No association with particular Triage Group', () => {
+  const site = new SecureMessagingSite();
+  const landingPage = new PatientInboxPage();
+
+  const updatedData = mockRecipients.data.slice(1);
+  const updatedMeta = { ...mockRecipients.meta, associatedTriageGroups: 6 };
+  const removedFirstRecipientsList = {
+    data: updatedData,
+    meta: updatedMeta,
+  };
+
+  it('detailed view - older than 45 day', () => {
+    const currentDate = new Date();
+    const fortyFiveDaysAgo = new Date();
+    fortyFiveDaysAgo.setDate(currentDate.getDate() - 46);
+
+    const oldThreadWithNoAssociatedTG = {
+      ...mockThread,
+      data: [
+        {
+          ...mockThread.data[0],
+          attributes: {
+            ...mockThread.data[0].attributes,
+            sentDate: fortyFiveDaysAgo.toISOString(),
+            recipientName: mockRecipients.data[0].attributes.name,
+            triageGroupName: mockRecipients.data[0].attributes.name,
+            recipientId: mockRecipients.data[0].attributes.triageTeamId,
+          },
+        },
+        ...mockThread.data,
+      ],
+    };
+
+    site.login();
+
+    landingPage.loadInboxMessages(
+      mockMessages,
+      mockSingleMessage,
+      removedFirstRecipientsList,
+    );
+    landingPage.loadSingleThread(oldThreadWithNoAssociatedTG, fortyFiveDaysAgo);
+
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
+
+    cy.get('[class="alert-expandable-title"]')
+      .should('be.visible')
+      .and(
+        'include.text',
+        `You can't send messages to ${mockRecipients.data[0].attributes.name}`,
+      );
+
+    cy.get('[data-testid="blocked-triage-group-alert"]')
+      .shadow()
+      .find('#alert-body')
+      .should('have.class', 'closed');
+
+    cy.get('[data-testid="blocked-triage-group-alert"]').click({
+      waitForAnimations: true,
+    });
+
+    cy.get('[data-testid="blocked-triage-group-alert"]')
+      .shadow()
+      .find('#alert-body')
+      .should('have.class', 'open');
+
+    cy.get('[data-testid="blocked-triage-group-alert"]')
+      .find('a')
+      .should('have.attr', 'href', '/find-locations/');
+
+    cy.get(Locators.BUTTONS.REPLY).should('not.exist');
+  });
+
+  it('existing draft - older than 45 days', () => {
+    const currentDate = new Date();
+    const fortyFiveDaysAgo = new Date();
+
+    fortyFiveDaysAgo.setDate(currentDate.getDate() - 46);
+    const mockThreadWithOldDraft = {
+      ...mockThread,
+      data: [
+        {
+          ...mockThread.data[0],
+          attributes: {
+            ...mockThread.data[0].attributes,
+            draftDate: fortyFiveDaysAgo.toISOString(),
+            recipientName: mockRecipients.data[0].attributes.name,
+            triageGroupName: mockRecipients.data[0].attributes.name,
+            recipientId: mockRecipients.data[0].attributes.triageTeamId,
+            sentDate: null,
+          },
+        },
+        ...mockThread.data,
+      ],
+    };
+
+    site.login();
+
+    landingPage.loadInboxMessages(
+      mockMessages,
+      mockSingleMessage,
+      removedFirstRecipientsList,
+    );
+
+    landingPage.loadSingleThread(
+      mockThreadWithOldDraft,
+      fortyFiveDaysAgo,
+      fortyFiveDaysAgo,
+    );
+
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
+
+    cy.get('[class="alert-expandable-title"]')
+      .should('be.visible')
+      .and(
+        'include.text',
+        `You can't send messages to ${mockRecipients.data[0].attributes.name}`,
+      );
+
+    cy.get('[data-testid="blocked-triage-group-alert"]')
+      .shadow()
+      .find('#alert-body')
+      .should('have.class', 'closed');
+
+    cy.get('[data-testid="blocked-triage-group-alert"]').click({
+      waitForAnimations: true,
+    });
+
+    cy.get('[data-testid="blocked-triage-group-alert"]')
+      .shadow()
+      .find('#alert-body')
+      .should('have.class', 'open');
+
+    cy.get('[data-testid="blocked-triage-group-alert"]')
+      .find('a')
+      .should('have.attr', 'href', '/find-locations/');
+
+    cy.get(Locators.BUTTONS.REPLY).should('not.exist');
+    cy.get(Locators.BUTTONS.SAVE_DRAFT).should('not.exist');
+  });
+});
---
+++ src/applications/mhv/secure-messaging/tests/e2e/triage-team-tests/secure-messaging-thread-no-association.cypress.spec.js
@@ -0,0 +1,108 @@
+import SecureMessagingSite from '../sm_site/SecureMessagingSite';
+import PatientInboxPage from '../pages/PatientInboxPage';
+import { AXE_CONTEXT, Locators } from '../utils/constants';
+import mockMessages from '../fixtures/messages-response.json';
+import mockSingleMessage from '../fixtures/inboxResponse/single-message-response.json';
+import mockRecipients from '../fixtures/recipients-response.json';
+import mockThread from '../fixtures/thread-response.json';
+
+describe('Verify thread - No association with particular Triage Group', () => {
+  const site = new SecureMessagingSite();
+  const landingPage = new PatientInboxPage();
+
+  const updatedData = mockRecipients.data.slice(1);
+  const updatedMeta = { ...mockRecipients.meta, associatedTriageGroups: 6 };
+  const removedFirstRecipientsList = {
+    data: updatedData,
+    meta: updatedMeta,
+  };
+
+  it('inbox view', () => {
+    site.login();
+
+    landingPage.loadInboxMessages(
+      mockMessages,
+      mockSingleMessage,
+      removedFirstRecipientsList,
+    );
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
+
+    cy.get(Locators.LINKS.CREATE_NEW_MESSAGE).click({
+      waitForAnimations: true,
+    });
+    cy.get(Locators.BUTTONS.CONTINUE).click({ waitForAnimations: true });
+    cy.get('#select').should(
+      'not.contain',
+      mockRecipients.data[0].attributes.name,
+    );
+  });
+
+  it('detailed view', () => {
+    const threadWithNoAssociatedTG = {
+      ...mockThread,
+      data: [
+        {
+          ...mockThread.data[0],
+          attributes: {
+            ...mockThread.data[0].attributes,
+            recipientName: mockRecipients.data[0].attributes.name,
+            triageGroupName: mockRecipients.data[0].attributes.name,
+            recipientId: mockRecipients.data[0].attributes.triageTeamId,
+          },
+        },
+        ...mockThread.data,
+      ],
+    };
+    site.login();
+
+    landingPage.loadInboxMessages(
+      mockMessages,
+      mockSingleMessage,
+      removedFirstRecipientsList,
+    );
+    landingPage.loadSingleThread(threadWithNoAssociatedTG);
+
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+      },
+    });
+
+    cy.get('[class="alert-expandable-title"]')
+      .should('be.visible')
+      .and(
+        'include.text',
+        `You can't send messages to ${mockRecipients.data[0].attributes.name}`,
+      );
+
+    cy.get('[data-testid="blocked-triage-group-alert"]')
+      .shadow()
+      .find('#alert-body')
+      .should('have.class', 'closed');
+
+    cy.get('[data-testid="blocked-triage-group-alert"]').click({
+      waitForAnimations: true,
+    });
+
+    cy.get('[data-testid="blocked-triage-group-alert"]')
+      .shadow()
+      .find('#alert-body')
+      .should('have.class', 'open');
+
+    cy.get('[data-testid="blocked-triage-group-alert"]')
+      .find('a')
+      .should('have.attr', 'href', '/find-locations/');
+
+    cy.get(Locators.BUTTONS.REPLY).should('not.exist');
+  });
+});
