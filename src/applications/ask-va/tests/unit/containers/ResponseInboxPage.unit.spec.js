b0c402c9fedf0382408cd6b81d38394700646f5e
Edwin Otero <30818791+ediiotero@users.noreply.github.com>
2023-12-22 15:08:57 -0500

    ASK-VA 450/Response Inbox Pg (#27200)
    * Updated response inbox page and tests
    
    * removed old response inbox page file

--- src/applications/ask-va/tests/unit/containers/ResponseInboxPage.unit.spec.js
+++ src/applications/ask-va/tests/unit/containers/ResponseInboxPage.unit.spec.js
@@ -1,11 +1,12 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render, waitFor } from '@testing-library/react';
 import { expect } from 'chai';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-
 import environment from '@department-of-veterans-affairs/platform-utilities/environment';
+import { RESPONSE_PAGE } from '../../../constants';
 import ResponseInboxPage from '../../../containers/ResponseInboxPage';
 
 describe('<ResponseInboxPage>', () => {
@@ -26,12 +27,27 @@ describe('<ResponseInboxPage>', () => {
                   inquiryNumber: '123',
                   processingStatus: 'complete',
                   question: 'Red or blue?',
-                  reply: {
-                    data: {
-                      attributes: {
-                        reply: 'Blue',
-                      },
+                  lastUpdate: '08/07/23',
+                  attachments: [
+                    {
+                      id: '012345',
+                      name: 'File_ABC.pdf',
                     },
+                  ],
+                  reply: {
+                    data: [
+                      {
+                        id: '456',
+                        modifiedon: '08/10/23',
+                        status_reason: 'Completed/Sent',
+                        message_type: '722310000: Reply to VA',
+                        enable_reply: true,
+                        attributes: {
+                          reply: 'Blue',
+                          attachmentNames: [],
+                        },
+                      },
+                    ],
                   },
                 },
               },
@@ -81,11 +97,34 @@ describe('<ResponseInboxPage>', () => {
         </Provider>,
       );
 
+      // TODO: update tests to look for data from api call. Currently page is rendered with mock data in component.
+      const h2List = [
+        RESPONSE_PAGE.YOUR_QUESTION,
+        RESPONSE_PAGE.ATTACHMENTS,
+        RESPONSE_PAGE.INBOX,
+        'Need help?',
+      ];
+      const h3List = [
+        RESPONSE_PAGE.SEND_REPLY,
+        RESPONSE_PAGE.UPLOAD_YOUR_FILES,
+      ];
+
       await waitFor(() => {
         expect(view.container.querySelector('h1')).to.contain.text(
-          'Response Inbox',
+          RESPONSE_PAGE.QUESTION_DETAILS,
         );
-        expect(view.container.querySelector('em')).to.contain.text('complete');
+        expect(view.container.querySelectorAll('h2')).to.contain.text(
+          RESPONSE_PAGE.YOUR_QUESTION,
+        );
+        expect(view.container.querySelectorAll('h2')).to.have.length(4);
+        h2List.forEach(h2 =>
+          expect(view.container.querySelectorAll('h2')).to.contain.text(h2),
+        );
+
+        expect(view.container.querySelectorAll('h3')).to.have.length(2);
+        h3List.forEach(h3 =>
+          expect(view.container.querySelectorAll('h3')).to.contain.text(h3),
+        );
       });
     });
   });
