5af3a5b658b1abededccf9eb93503c08cd62761e
Hemesh Patel <hemesh.v.patel@gmail.com>
2023-12-22 07:49:27 -0600

    remove PageFieldSummary from non first pages

--- src/applications/ask-va/config/chapters/yourQuestion/reasonContacting.js
+++ src/applications/ask-va/config/chapters/yourQuestion/reasonContacting.js
@@ -1,30 +1,47 @@
 import FormElementTitle from '../../../components/FormElementTitle';
-import PageFieldSummary from '../../../components/PageFieldSummary';
 import { CHAPTER_2 } from '../../../constants';
+import { radioUI } from '../../schema-helpers/radioHelper';
 
 const reasonContactPage = {
   uiSchema: {
     'ui:description': FormElementTitle({ title: CHAPTER_2.PAGE_2.TITLE }),
-    'ui:objectViewField': PageFieldSummary,
     question: {
-      'ui:title': CHAPTER_2.PAGE_2.QUESTION_1,
-      'ui:widget': 'radio',
-      'ui:options': {
-        labels: {
-          question: 'I have a question',
-          nice: 'I want to say something nice',
-          complaint: 'I have a complaint about a service',
-          suggestion: 'I have a suggestion',
-          townHall: 'I attended a Town Hall and now I have a question',
-          somethingElse: 'I want to say something else',
-        },
+      personalRelationship: radioUI({
+        title: CHAPTER_2.PAGE_2.QUESTION_1,
+      }),
+      labels: {
+        question: 'I have a question',
+        nice: 'I want to say something nice',
+        complaint: 'I have a complaint about a service',
+        suggestion: 'I have a suggestion',
+        townHall: 'I attended a Town Hall and now I have a question',
+        somethingElse: 'I want to say something else',
       },
+      // 'ui:title': CHAPTER_2.PAGE_2.QUESTION_1,
+      // 'ui:widget': 'radio',
+      // 'ui:options': {
+      //   labels: {
+      //     question: 'I have a question',
+      //     nice: 'I want to say something nice',
+      //     complaint: 'I have a complaint about a service',
+      //     suggestion: 'I have a suggestion',
+      //     townHall: 'I attended a Town Hall and now I have a question',
+      //     somethingElse: 'I want to say something else',
+      //   },
+      // },
     },
   },
   schema: {
     type: 'object',
     required: [],
     properties: {
+      // question: radioSchema(Object.keys( {
+      //   question: 'I have a question',
+      //   nice: 'I want to say something nice',
+      //   complaint: 'I have a complaint about a service',
+      //   suggestion: 'I have a suggestion',
+      //   townHall: 'I attended a Town Hall and now I have a question',
+      //   somethingElse: 'I want to say something else')})
       question: {
         type: 'string',
         enum: [
--- src/applications/ask-va/config/chapters/yourQuestion/yourQuestion.js
+++ src/applications/ask-va/config/chapters/yourQuestion/yourQuestion.js
@@ -1,11 +1,9 @@
 import FormElementTitle from '../../../components/FormElementTitle';
-import PageFieldSummary from '../../../components/PageFieldSummary';
 import { CHAPTER_2 } from '../../../constants';
 
 const yourQuestionPage = {
   uiSchema: {
     'ui:description': FormElementTitle({ title: CHAPTER_2.PAGE_3.TITLE }),
-    'ui:objectViewField': PageFieldSummary,
     question: {
       'ui:title': CHAPTER_2.PAGE_3.QUESTION_1,
       'ui:widget': 'textarea',
