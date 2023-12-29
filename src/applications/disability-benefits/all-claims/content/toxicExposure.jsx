efd21d0fb40726f353292729d5b64340608d8a03
Christine <127446042+christinec-fftc@users.noreply.github.com>
2023-12-29 15:14:15 -0800

    72301 Add toxic exposure conditions page and remove intro page

--- src/applications/disability-benefits/all-claims/content/toxicExposure.jsx
+++ src/applications/disability-benefits/all-claims/content/toxicExposure.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
 
-export const introQuestion = (
-  <b>Would you like to answer questions related to toxic exposure?</b>
-);
-
-export const introDescription = (
-  <>
+export const conditionsTitle =
+  'Are any of your new conditions related to toxic exposure during your military service? Check any that are related.';
+export const conditionsDescription = (
+  <va-additional-info
+    class="vads-u-margin-bottom--4"
+    trigger="What is toxic exposure?"
+  >
     <p>
       Toxic exposures include exposures to substances like Agent Orange, burn
       pits, radiation, asbestos, or contaminated water.
     </p>
+    <br role="presentation" />
     <p>
-      We’ll use the information you provide to help us locate your records and
-      make decisions on your claim.
+      <va-link
+        href="https://www.va.gov/resources/the-pact-act-and-your-va-benefits/"
+        text="Learn more about toxic exposure"
+      />
     </p>
-    <p>
-      If you’ve answered these questions in a disability compensation claim in
-      the past, you don’t need to answer them again.
-    </p>
-  </>
-);
-
-export const moreInfoDescription = (
-  <a
-    href="https://www.va.gov/disability/eligibility/hazardous-materials-exposure/"
-    target="_blank"
-    rel="noreferrer"
-  >
-    Learn more about toxic exposure (opens in new tab)
-  </a>
+  </va-additional-info>
 );
