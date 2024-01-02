33c88cd10eb69e85c9d1b13887541a390763f6ae
Sean Midgley <sean.midgley.dev@gmail.com>
2024-01-02 09:36:50 -0800

    more clean up

---
+++ src/applications/burials/utils/migrations.js
@@ -0,0 +1,28 @@
+import { isValidCentralMailPostalCode } from 'platform/forms/address/validations';
+
+export default [
+  // 0 > 1, move user back to address page if zip code is bad
+  ({ formData, metadata }) => {
+    let newMetadata = metadata;
+
+    if (
+      formData.claimantAddress &&
+      !isValidCentralMailPostalCode(formData.claimantAddress)
+    ) {
+      newMetadata = { ...metadata, returnUrl: '/claimant-contact-information' };
+    }
+
+    return { formData, metadata: newMetadata };
+  },
+  // 1 > 2, move user back to file number if incorrect
+  ({ formData, metadata }) => {
+    const fileNumbeRegex = /^\d{8,9}$/;
+    let newMetadata = metadata;
+
+    if (formData.vaFileNumber && !fileNumbeRegex.test(formData.vaFileNumber)) {
+      newMetadata = { ...metadata, returnUrl: '/veteran-information' };
+    }
+
+    return { formData, metadata: newMetadata };
+  },
+];
