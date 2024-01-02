7e773fa4b1dd96aa8c33e0731e0f6be1933c7baa
Aaron Read <Aaron.Read@va.gov>
2024-01-02 14:44:20 -0600

    Update medical-records-download-txt-allergies.cypress.spec.js

--- src/applications/mhv/medical-records/tests/e2e/medical-records-download-txt-allergies.cypress.spec.js
+++ src/applications/mhv/medical-records/tests/e2e/medical-records-download-txt-allergies.cypress.spec.js
@@ -4,35 +4,33 @@ import allergy from './fixtures/allergy.json';
 import allergies from './fixtures/allergies.json';
 import AllergiesListPage from './pages/AllergiesListPage';
 
-for (let i = 0; i < 200; i += 1) {
-  describe('Medical Records View Allergies', () => {
-    const site = new MedicalRecordsSite();
+describe('Medical Records View Allergies', () => {
+  const site = new MedicalRecordsSite();
 
-    before(() => {
-      site.login();
-      cy.visit('my-health/medical-records');
-      // Given Navigate to Allergy Page
-      cy.reload();
-      AllergiesListPage.clickGotoAllergiesLink(allergies);
-      AllergyDetailsPage.clickAllergyDetailsLink('NUTS', 7006, allergy);
-    });
-
-    it('Toggle Menu button Print or download on Details Page ', () => {
-      // should display a toggle menu button
-      AllergyDetailsPage.verifyPrintOrDownload();
-
-      // should display print button for a list "Print this list"
-      AllergyDetailsPage.clickPrintOrDownload();
-      AllergyDetailsPage.verifyPrintButton();
-
-      // should display a download pdf file button "Download PDF of this page"
-      AllergyDetailsPage.verifyDownloadPDF();
-
-      // should display a download text file button "Download list as a text file"
-      AllergyDetailsPage.verifyDownloadTextFile();
-
-      cy.injectAxe();
-      cy.axeCheck('main');
-    });
+  before(() => {
+    site.login();
+    cy.visit('my-health/medical-records');
+    // Given Navigate to Allergy Page
+    cy.reload();
+    AllergiesListPage.clickGotoAllergiesLink(allergies);
+    AllergyDetailsPage.clickAllergyDetailsLink('NUTS', 7006, allergy);
   });
-}
+
+  it('Toggle Menu button Print or download on Details Page ', () => {
+    // should display a toggle menu button
+    AllergyDetailsPage.verifyPrintOrDownload();
+
+    // should display print button for a list "Print this list"
+    AllergyDetailsPage.clickPrintOrDownload();
+    AllergyDetailsPage.verifyPrintButton();
+
+    // should display a download pdf file button "Download PDF of this page"
+    AllergyDetailsPage.verifyDownloadPDF();
+
+    // should display a download text file button "Download list as a text file"
+    AllergyDetailsPage.verifyDownloadTextFile();
+
+    cy.injectAxe();
+    cy.axeCheck('main');
+  });
+});
