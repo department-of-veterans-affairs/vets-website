3ddf963cc86a3329bda995a68a56f032d3c81fcb
Brian Seek <brian.seek@civicactions.com>
2024-01-02 08:05:47 -0800

    GA event bug for 45 minute check-in message (#27282)
    * set 45 text if setStarted not called
    
    * removed double page type in no event

--- src/applications/check-in/components/pages/TravelPage/index.jsx
+++ src/applications/check-in/components/pages/TravelPage/index.jsx
@@ -45,7 +45,7 @@ const TravelPage = ({
     recordEvent({
       event: createAnalyticsSlug(
         `${answer}-to-${pageType}${
-          setECheckinStartedCalled ? '-45MR' : ''
+          setECheckinStartedCalled ? '' : '-45MR'
         }-clicked`,
         'nav',
       ),
