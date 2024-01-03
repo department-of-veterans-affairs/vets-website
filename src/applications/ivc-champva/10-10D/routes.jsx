7f88ffecb29b05a8a2f1d218a8236935f2572cef
Michael Clement <michael.clement@adhocteam.us>
2024-01-03 15:05:37 -0600

    26884 scaffold new 1010d form app (#26908)
    * ran yeoman form generator
    
    * form id added to missingschema array
    
    * moved to new parent folder
    
    * added message on intro page
    
    ---------
    
    Co-authored-by: Tze-chiu Lei <tze@adhocteam.us>

---
+++ src/applications/ivc-champva/10-10D/routes.jsx
@@ -0,0 +1,13 @@
+import { createRoutesWithSaveInProgress } from 'platform/forms/save-in-progress/helpers';
+import formConfig from './config/form.js';
+import App from './containers/App.jsx';
+
+const route = {
+  path: '/',
+  component: App,
+  indexRoute: { onEnter: (nextState, replace) => replace('/introduction') },
+
+  childRoutes: createRoutesWithSaveInProgress(formConfig),
+};
+
+export default route;
