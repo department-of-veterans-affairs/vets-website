4f69081bbd71505560707b8e256ef852f2bade07
Jami Gibbs <jami.gibbs@adhocteam.us>
2024-01-03 16:41:46 -0600

    update test

--- src/applications/education-letters/tests/e2e/00-letters-all-fields.cypress.spec.js
+++ src/applications/education-letters/tests/e2e/00-letters-all-fields.cypress.spec.js
@@ -6,24 +6,24 @@ import { mockClaimStatus } from '../fixtures/mockClaimStatusEligible';
 describe('All Field, texts and links should be validated on letters app', () => {
   it('All texts are present for the letters page unauthenticated', () => {
     cy.visit('/education/download-letters/');
-
-    cy.injectAxeThenAxeCheck();
     cy.url().should('include', '/education/download-letters/');
 
     cy.get('.va-introtext').should(
-      'have.text',
-      'If you’re a Veteran and you recently received your VA education decision letter, you can download it now.',
+      'include.text',
+      'If we made a decision on your application for Post-9/11 GI Bill benefits, you may be able to download your education decision letter now.',
     );
-    cy.findByTestId('form-title').should(
-      'have.text',
-      'Download your VA education letter',
-    );
-    cy.findByText('Who can download VA education letters?').should(
+
+    cy.get('h1').should('include.text', 'Download your VA education letter');
+
+    cy.findByText('Who can download an education decision letter?').should(
       'be.visible',
     );
+
     cy.findByText(
-      'You can download your education letter if you’re a Veteran and you meet both of the requirements listed here. At this time, family members and dependents can’t get their education letters online.',
+      'You can download your decision letter if you’re a Veteran and you meet both of the requirements listed here.',
     ).should('be.visible');
+
+    cy.injectAxeThenAxeCheck();
   });
 
   it('All texts are present for the letters page authenticated but no letter', () => {
@@ -36,23 +36,21 @@ describe('All Field, texts and links should be validated on letters app', () => 
     cy.visit('/education/download-letters/');
 
     cy.get('a[href*="/education/download-letters/letters"').click();
-    cy.injectAxeThenAxeCheck();
     cy.url().should('include', '/education/download-letters/letters');
 
     cy.get('.va-introtext').should(
-      'have.text',
-      'If you’re a Veteran and you recently received your VA education decision letter, you can download it now.',
+      'include.text',
+      'Check this page for your education decision letter for Post-9/11 GI Bill benefits.',
     );
-    cy.findByTestId('form-title').should(
-      'have.text',
-      'Download your VA education letter',
+    cy.get('a.decision-letter-download').should(
+      'include.text',
+      'Download your education decision letter',
     );
-    cy.findByText('Your decision letter isn’t available online').should(
+    cy.findByText('What if my education decision letter isn’t here?').should(
       'be.visible',
     );
-    cy.findByText(
-      'Your letter won’t be here if 1 of these situations is true for you:',
-    ).should('be.visible');
+
+    cy.injectAxeThenAxeCheck();
   });
 
   it('All texts are present for the letters page authenticated with letter', () => {
@@ -64,14 +62,9 @@ describe('All Field, texts and links should be validated on letters app', () => 
     cy.login(mebUser);
 
     cy.visit('/education/download-letters/letters');
-    cy.injectAxeThenAxeCheck();
     cy.url().should('include', '/education/download-letters/letters');
-    cy.findByTestId('form-title').should(
-      'have.text',
-      'Download your VA education decision letter',
-    );
+    cy.get('h1').should('include.text', 'Your VA education letter');
 
-    cy.get('a[href*="/meb_api/v0/claim_letter"]').should('be.visible');
-    cy.findByText('How do I download and open a letter?').should('be.visible');
+    cy.injectAxeThenAxeCheck();
   });
 });
