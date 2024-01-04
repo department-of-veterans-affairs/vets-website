e5d7e5993452772ce52db0636bbd2e4d10f17b7c
Micah Chiang <micahkchiang@gmail.com>
2024-01-04 10:21:35 -0800

    update url and text
    Signed-off-by: Micah Chiang <micahkchiang@gmail.com>

--- src/applications/education-letters/tests/e2e/00-letters-all-fields.cypress.spec.js
+++ src/applications/education-letters/tests/e2e/00-letters-all-fields.cypress.spec.js
@@ -5,7 +5,7 @@ import { mockClaimStatus } from '../fixtures/mockClaimStatusEligible';
 
 describe('All Field, texts and links should be validated on letters app', () => {
   it('All texts are present for the letters page unauthenticated', () => {
-    cy.visit('/education/download-letters/');
+    cy.visit('/education/download-letters/letters');
 
     cy.injectAxeThenAxeCheck();
     cy.url().should('include', '/education/download-letters/');
@@ -33,7 +33,7 @@ describe('All Field, texts and links should be validated on letters app', () => 
       '/meb_api/v0/claim_status?latest=true',
       mockClaimStatus,
     ).as('mockClaimStatus');
-    cy.visit('/education/download-letters/');
+    cy.visit('/education/download-letters/letters');
 
     cy.get('a[href*="/education/download-letters/letters"').click();
     cy.injectAxeThenAxeCheck();
@@ -68,7 +68,7 @@ describe('All Field, texts and links should be validated on letters app', () => 
     cy.url().should('include', '/education/download-letters/letters');
     cy.findByTestId('form-title').should(
       'have.text',
-      'Download your VA education decision letter',
+      'Your VA education letter',
     );
 
     cy.get('a[href*="/meb_api/v0/claim_letter"]').should('be.visible');
