235df4f2d9951bc5753f309f2c23f19eb02e1cd2
Peri-Ann McLaren <141954992+pmclaren19@users.noreply.github.com>
2024-01-04 08:04:30 -0700

    added new toggle called lettersCheckDiscrepancies and updated applications/letters/selectors/index.js to include the toggle (#27311)

--- src/applications/letters/selectors/index.js
+++ src/applications/letters/selectors/index.js
@@ -1,5 +1,5 @@
-import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
-import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
+import { toggleValues } from '@department-of-veterans-affairs/platform-site-wide/selectors';
+import FEATURE_FLAG_NAMES from '@department-of-veterans-affairs/platform-utilities/featureFlagNames';
 
 export const isLoadingFeatures = state => toggleValues(state).loading;
 
@@ -8,3 +8,7 @@ export const isLoadingFeatures = state => toggleValues(state).loading;
 // 'bcas_letters_use_lighthouse'
 export const lettersUseLighthouse = state =>
   toggleValues(state)[FEATURE_FLAG_NAMES.bcasLettersUseLighthouse];
+
+// 'letters_check_discrepancies'
+export const lettersCheckDiscrepancies = state =>
+  toggleValues(state)[FEATURE_FLAG_NAMES.lettersCheckDiscrepancies];
