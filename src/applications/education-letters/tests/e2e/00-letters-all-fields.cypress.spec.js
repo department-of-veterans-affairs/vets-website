ed30b957a1b617211eaea542b4bc2291fd475428
Jami Gibbs <jami.gibbs@adhocteam.us>
2024-01-04 10:29:39 -0600

    update education-benefits test

--- src/applications/education-letters/tests/e2e/00-letters-all-fields.cypress.spec.js
+++ src/applications/education-letters/tests/e2e/00-letters-all-fields.cypress.spec.js
@@ -5,35 +5,34 @@ import { mockClaimStatus } from '../fixtures/mockClaimStatusEligible';
 
 describe('All Field, texts and links should be validated on letters app', () => {
   it('All texts are present for the letters page unauthenticated', () => {
-    cy.visit('http://localhost:3001/education/download-letters/letters');
+    cy.visit('http://localhost:3002/education/download-letters/');
+
+    cy.injectAxeThenAxeCheck();
     cy.url().should('include', '/education/download-letters/');
 
-    // cy.get('.va-introtext').should(
-    //   'include.text',
-    //   'If we made a decision on your application for Post-9/11 GI Bill benefits, you may be able to download your education decision letter now.',
-    // );
+    cy.get('.va-introtext').should(
+      'include.text',
+      'If we made a decision on your application for Post-9/11 GI Bill benefits, you may be able to download your education decision letter now.',
+    );
+    cy.get('h1').should('include.text', 'Download your VA education letter');
 
-    // cy.get('h1').should('include.text', 'Download your VA education letter');
+    cy.findByText('Who can download an education decision letter?').should(
+      'be.visible',
+    );
 
-    // cy.findByText('Who can download an education decision letter?').should(
-    //   'be.visible',
-    // );
-
-    // cy.findByText(
-    //   'You can download your decision letter if you’re a Veteran and you meet both of the requirements listed here.',
-    // ).should('be.visible');
-
-    // cy.injectAxeThenAxeCheck();
+    cy.findByText(
+      'You can download your decision letter if you’re a Veteran and you meet both of the requirements listed here.',
+    ).should('be.visible');
   });
 
   it('All texts are present for the letters page authenticated but no letter', () => {
-    cy.login(mebUser);
     cy.intercept(
       'GET',
       '/meb_api/v0/claim_status?latest=true',
       mockClaimStatus,
     ).as('mockClaimStatus');
-    cy.visit('http://localhost:3001/education/download-letters/');
+    cy.login(mebUser);
+    cy.visit('http://localhost:3002/education/download-letters/');
 
     cy.get('a[href*="/education/download-letters/letters"').click();
     cy.url().should('include', '/education/download-letters/letters');
