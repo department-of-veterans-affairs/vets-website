fc1712ded49e54c1c619e52d1dc13eae4dd5733f
Rob Garrison <wowmotty@gmail.com>
2024-01-05 14:24:38 -0600

    Add formConfig & pageConfig onFormExit callback (#27349)
    * Add formConfig & pageConfig onFormExit callback
    
    * Fix e2e test failure

--- src/platform/forms-system/test/js/components/ContactInfo.unit.spec.jsx
+++ src/platform/forms-system/test/js/components/ContactInfo.unit.spec.jsx
@@ -12,6 +12,7 @@ import {
   getContent,
   setReturnState,
   clearReturnState,
+  getReturnState,
 } from '../../../src/js/utilities/data/profile';
 
 const getData = ({
@@ -444,5 +445,36 @@ describe('<ContactInfo>', () => {
     expect($$('h3', container).length).to.equal(0);
     expect($$('h4', container).length).to.equal(1);
     expect($$('h5', container).length).to.equal(4);
+  });
+
+  it('should call go forward callback', async () => {
+    setReturnState('testing123');
+    const forwardSpy = sinon.spy();
+    const data = getData({ forwardSpy });
+    const { container } = render(
+      <Provider store={mockStore}>
+        <ContactInfo {...data} />
+      </Provider>,
+    );
+
+    fireEvent.click($('.usa-button-primary', container));
+    expect(forwardSpy.called).to.be.true;
+    // return state gets cleared on continuing past the page
+    await expect(getReturnState()).to.equal('');
+  });
+  it('should call updatePage callback', async () => {
+    const updateSpy = sinon.spy();
+    const data = getData({ updateSpy, onReviewPage: true });
+    const { container } = render(
+      <Provider store={mockStore}>
+        <ContactInfo {...data} />
+      </Provider>,
+    );
+
+    fireEvent.click($('va-button', container));
+    expect(updateSpy.called).to.be.true;
+    // return state is set to 'true,' on update so the form returns back to the
+    // review & submit page
+    await expect(getReturnState()).to.contain('true');
   });
 });
