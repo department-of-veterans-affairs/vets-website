13002f95e2e8161e68be5d5cf993514a65bd1a9b
Dan Hinze <hinzed1127@gmail.com>
2024-01-05 10:59:36 -0600

    Remove forms cruft

--- src/applications/travel-pay/config/form.js
+++ src/applications/travel-pay/config/form.js
@@ -1,18 +1,3 @@
-// In a real app this would not be imported directly; instead the schema you
-// imported above would import and use these common definitions:
-import commonDefinitions from 'vets-json-schema/dist/definitions.json';
-
-// Example of an imported schema:
-// import fullSchema from '../10-3542-schema.json';
-// In a real app this would be imported from `vets-json-schema`:
-// import fullSchema from 'vets-json-schema/dist/10-3542-schema.json';
-import fullNameUI from '@department-of-veterans-affairs/platform-forms-system/fullName';
-import ssnUI from '@department-of-veterans-affairs/platform-forms-system/ssn';
-import phoneUI from '@department-of-veterans-affairs/platform-forms-system/phone';
-import * as address from '@department-of-veterans-affairs/platform-forms-system/address';
-
-// import fullSchema from 'vets-json-schema/dist/10-3542-schema.json';
-
 import manifest from '../manifest.json';
 
 import IntroductionPage from '../containers/IntroductionPage';
@@ -21,12 +6,6 @@ import ConfirmationPage from '../containers/ConfirmationPage';
 // const { } = fullSchema.properties;
 
 // const { } = fullSchema.definitions;
-
-// pages
-import directDeposit from '../pages/directDeposit';
-import serviceHistory from '../pages/serviceHistory';
-
-const { fullName, ssn, date, dateRange, usaPhone } = commonDefinitions;
 
 const formConfig = {
   rootUrl: manifest.rootUrl,
@@ -53,83 +32,19 @@ const formConfig = {
       'Please sign in again to continue your application for beneficiary travel claim.',
   },
   title: 'Complex Form',
-  defaultDefinitions: {
-    fullName,
-    ssn,
-    date,
-    dateRange,
-    usaPhone,
-  },
+  defaultDefinitions: {},
   chapters: {
-    applicantInformationChapter: {
-      title: 'Applicant Information',
+    chapter1: {
+      title: 'Chapter 1',
       pages: {
-        applicantInformation: {
-          path: 'applicant-information',
-          title: 'Applicant Information',
-          uiSchema: {
-            fullName: fullNameUI,
-            ssn: ssnUI,
-          },
+        page1: {
+          path: 'first-page',
+          title: 'First Page',
+          uiSchema: {},
           schema: {
             type: 'object',
-            required: ['fullName'],
-            properties: {
-              fullName,
-              ssn,
-            },
+            properties: {},
           },
-        },
-      },
-    },
-    serviceHistoryChapter: {
-      title: 'Service History',
-      pages: {
-        serviceHistory: {
-          path: 'service-history',
-          title: 'Service History',
-          uiSchema: serviceHistory.uiSchema,
-          schema: serviceHistory.schema,
-        },
-      },
-    },
-    additionalInformationChapter: {
-      title: 'Additional Information',
-      pages: {
-        contactInformation: {
-          path: 'contact-information',
-          title: 'Contact Information',
-          uiSchema: {
-            address: address.uiSchema('Mailing address'),
-            email: {
-              'ui:title': 'Primary email',
-            },
-            altEmail: {
-              'ui:title': 'Secondary email',
-            },
-            phoneNumber: phoneUI('Daytime phone'),
-          },
-          schema: {
-            type: 'object',
-            properties: {
-              // address: address.schema(fullSchema, true),
-              email: {
-                type: 'string',
-                format: 'email',
-              },
-              altEmail: {
-                type: 'string',
-                format: 'email',
-              },
-              phoneNumber: usaPhone,
-            },
-          },
-        },
-        directDeposit: {
-          path: 'direct-deposit',
-          title: 'Direct Deposit',
-          uiSchema: directDeposit.uiSchema,
-          schema: directDeposit.schema,
         },
       },
     },
