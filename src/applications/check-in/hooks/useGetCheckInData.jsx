16798f8757bb2538786d3b0b0ecdc4a01bc611dd
John Woolschlager <wullaski@gmail.com>
2024-01-05 13:55:06 -0500

    Remove unused import

--- src/applications/check-in/hooks/useGetCheckInData.jsx
+++ src/applications/check-in/hooks/useGetCheckInData.jsx
@@ -25,7 +25,6 @@ import {
 } from '../utils/appointment';
 import { useFormRouting } from './useFormRouting';
 import { useStorage } from './useStorage';
-import { URLS } from '../utils/navigation';
 
 import { useUpdateError } from './useUpdateError';
 
