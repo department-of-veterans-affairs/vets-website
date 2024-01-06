b3df596329356f90090cb899e75ea5a5608c89aa
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2024-01-05 18:19:22 -0600

    VFEP 'Community focus' update text

--- src/applications/gi/containers/FilterYourResults.jsx
+++ src/applications/gi/containers/FilterYourResults.jsx
@@ -338,8 +338,9 @@ export function FilterYourResults({
         class="vads-u-margin-y--4"
         label={
           <div className="vads-u-margin-left--neg0p25">
-            Specialized mission (i.e., Single-gender, Religious affiliation,
-            HBCU)
+            {environment.isProduction()
+              ? 'Specialized mission (i.e., Single-gender, Religious affiliation, HBCU)'
+              : 'Community focus (i.e., Single-gender, Religious affiliation, HBCU)'}
           </div>
         }
         onChange={onChangeCheckbox}
