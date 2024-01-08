b39cc3da0901df02f2e7b552f54977ba2b4e336d
Matt Long <longmd@me.com>
2024-01-08 11:53:50 -0500

    Add analytics to alert component

--- src/applications/ezr/components/FormAlerts/PreferredFacilityAlert.jsx
+++ src/applications/ezr/components/FormAlerts/PreferredFacilityAlert.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import recordEvent from 'platform/monitoring/record-event';
 
 const PreferredFacilityAlert = () => {
+  // use logging to track the volume of users who receive this alert
+  useEffect(() => {
+    return () => {
+      recordEvent({
+        event: 'ezr-missing-preferred-facility',
+      });
+    };
+  }, []);
+
   return (
     <va-alert status="error" data-testid="ezr-preferred-facility-alert" uswds>
       <h3 slot="headline">
@@ -15,7 +25,7 @@ const PreferredFacilityAlert = () => {
           You can update your health benefits information by phone, by mail, or
           in person.
         </p>
-        <a href="/health-care/update-health-information/">
+        <a href="/health-care/update-health-information/#how-do-i-update-my-information">
           Learn more about how to update your health benefits information
         </a>
       </div>
