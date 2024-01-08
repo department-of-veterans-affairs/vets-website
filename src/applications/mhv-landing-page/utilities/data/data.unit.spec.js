38a3c283fceeefe417b989be35fcbcc096a0d4a6
Carlos Felix <63804190+carlosfelix2@users.noreply.github.com>
2024-01-08 10:58:25 -0500

    71809 MHV on VA.gov unread message indicator only uses inbox and custom folders (#27344)

--- src/applications/mhv-landing-page/utilities/data/data.unit.spec.js
+++ src/applications/mhv-landing-page/utilities/data/data.unit.spec.js
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 import { expect } from 'chai';
-import { resolveToggleLink } from './index';
+import { resolveToggleLink, countUnreadMessages } from './index';
 import manifest from '../../manifest.json';
 
 const initializeFeatureToggles = ({
@@ -63,6 +63,94 @@ describe(manifest.appName, () => {
         const resolvedLink = resolveToggleLink(link, toggles);
 
         expect(resolvedLink.href).to.equal('/new');
+      });
+    });
+
+    describe('countUnreadMessages', () => {
+      it('unread count only uses inbox and custom folders', () => {
+        // Note the system folder IDs are constant and set by the API
+        const folderIds = {
+          inbox: 0,
+          sent: -1,
+          drafts: -2,
+          deleted: -3,
+        };
+
+        const folder = (id, unreadCount) => {
+          return {
+            id,
+            type: 'folders',
+            attributes: {
+              folderId: id,
+              unreadCount,
+              count: 10, // Helps check the count is not used
+              systemFolder: id <= 0,
+            },
+          };
+        };
+
+        // All empty folders
+        let count = countUnreadMessages({
+          data: [
+            folder(folderIds.inbox, 0),
+            folder(folderIds.sent, 0),
+            folder(folderIds.drafts, 0),
+            folder(folderIds.deleted, 0),
+            folder(100, 0),
+          ],
+        });
+        expect(count).to.equal(0);
+
+        // Inbox has unread messages
+        count = countUnreadMessages({
+          data: [
+            folder(folderIds.inbox, 1),
+            folder(folderIds.sent, 0),
+            folder(folderIds.drafts, 0),
+            folder(folderIds.deleted, 0),
+            folder(100, 0),
+          ],
+        });
+        expect(count).to.equal(1);
+
+        // Custom folder has unread messages
+        count = countUnreadMessages({
+          data: [
+            folder(folderIds.inbox, 0),
+            folder(folderIds.sent, 0),
+            folder(folderIds.drafts, 0),
+            folder(folderIds.deleted, 0),
+            folder(100, 0),
+            folder(101, 1),
+          ],
+        });
+        expect(count).to.equal(1);
+
+        // No unread received messages for the user
+        count = countUnreadMessages({
+          data: [
+            folder(folderIds.inbox, 0),
+            folder(folderIds.sent, 1),
+            folder(folderIds.drafts, 1),
+            folder(folderIds.deleted, 1),
+            folder(100, 0),
+            folder(101, 0),
+          ],
+        });
+        expect(count).to.equal(0);
+
+        // Multi count
+        count = countUnreadMessages({
+          data: [
+            folder(folderIds.inbox, 5),
+            folder(folderIds.sent, 1),
+            folder(folderIds.drafts, 1),
+            folder(folderIds.deleted, 1),
+            folder(100, 2),
+            folder(101, 3),
+          ],
+        });
+        expect(count).to.equal(10);
       });
     });
   });
