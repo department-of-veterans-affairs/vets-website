9d575d9aa36672b8540a748f5248a584822e7e40
Tai Wilkin <tai.wilkin@coforma.io>
2024-01-08 11:14:22 -0500

    Expand Pension unit test coverage

--- src/applications/pensions/config/form.js
+++ src/applications/pensions/config/form.js
@@ -160,11 +160,63 @@ export function isUnder65(formData, currentDate) {
   );
 }
 
-function showSpouseAddress(form) {
+export function showSpouseAddress(form) {
   return (
     form.maritalStatus === 'Separated' ||
     get(['view:liveWithSpouse'], form) === false
   );
+}
+
+export function isSeparated(formData) {
+  return formData.maritalStatus === 'Separated';
+}
+
+export function currentSpouseHasFormerMarriages(formData) {
+  return formData.currentSpouseMaritalHistory === 'Yes';
+}
+
+export function hasNoSocialSecurityDisability(formData) {
+  return formData.socialSecurityDisability !== true;
+}
+
+export function isInNursingHome(formData) {
+  return formData.nursingHome !== false;
+}
+
+export function medicaidDoesNotCoverNursingHome(formData) {
+  return formData.medicaidCoverage !== true;
+}
+
+export function isHomeAcreageMoreThanTwo(formData) {
+  return formData.homeAcreageMoreThanTwo === true;
+}
+
+export function ownsHome(formData) {
+  return formData.homeOwnership === true;
+}
+
+export function hasVaTreatmentHistory(formData) {
+  return formData.vaTreatmentHistory !== false;
+}
+
+export function hasFederalTreatmentHistory(formData) {
+  return formData.federalTreatmentHistory !== false;
+}
+
+export function isEmployedUnder65(formData) {
+  return formData.currentEmployment !== false && isUnder65(formData);
+}
+
+export function isUnemployedUnder65(formData) {
+  return formData.currentEmployment !== true && isUnder65(formData);
+}
+
+export function doesReceiveIncome(formData) {
+  return formData.receivesIncome !== false;
+}
+
+export function doesHaveCareExpenses(formData) {
+  return formData.hasMedicalExpenses;
 }
 
 function isCurrentMarriage(form, index) {
@@ -310,9 +362,7 @@ const formConfig = {
         medicalCondition: {
           title: 'Medical condition',
           path: 'medical/history/condition',
-          depends: formData => {
-            return formData.socialSecurityDisability !== true;
-          },
+          depends: hasNoSocialSecurityDisability,
           uiSchema: medicalCondition.uiSchema,
           schema: medicalCondition.schema,
         },
@@ -325,18 +375,14 @@ const formConfig = {
         medicaidCoverage: {
           title: 'Medicaid coverage',
           path: 'medical/history/nursing/medicaid',
-          depends: formData => {
-            return formData.nursingHome !== false;
-          },
+          depends: isInNursingHome,
           uiSchema: medicaidCoverage.uiSchema,
           schema: medicaidCoverage.schema,
         },
         medicaidStatus: {
           title: 'Medicaid application status',
           path: 'medical/history/nursing/medicaid/status',
-          depends: formData => {
-            return formData.medicaidCoverage !== true;
-          },
+          depends: medicaidDoesNotCoverNursingHome,
           uiSchema: medicaidStatus.uiSchema,
           schema: medicaidStatus.schema,
         },
@@ -355,9 +401,7 @@ const formConfig = {
         vaMedicalCenters: {
           title: 'VA medical centers',
           path: 'medical/history/va-treatment/medical-centers',
-          depends: formData => {
-            return formData.vaTreatmentHistory !== false;
-          },
+          depends: hasVaTreatmentHistory,
           uiSchema: vaMedicalCenters.uiSchema,
           schema: vaMedicalCenters.schema,
         },
@@ -370,9 +414,7 @@ const formConfig = {
         federalMedicalCenters: {
           title: 'Federal medical facilities',
           path: 'medical/history/federal-treatment/medical-centers',
-          depends: formData => {
-            return formData.federalTreatmentHistory !== false;
-          },
+          depends: hasFederalTreatmentHistory,
           uiSchema: federalMedicalCenters.uiSchema,
           schema: federalMedicalCenters.schema,
         },
@@ -386,18 +428,14 @@ const formConfig = {
         currentEmploymentHistory: {
           title: 'Current employment',
           path: 'employment/current/history',
-          depends: formData => {
-            return formData.currentEmployment !== false && isUnder65(formData);
-          },
+          depends: isEmployedUnder65,
           uiSchema: currentEmployers.uiSchema,
           schema: currentEmployers.schema,
         },
         previousEmploymentHistory: {
           title: 'Previous employment',
           path: 'employment/previous/history',
-          depends: formData => {
-            return formData.currentEmployment !== true && isUnder65(formData);
-          },
+          depends: isUnemployedUnder65,
           uiSchema: previousEmployers.uiSchema,
           schema: previousEmployers.schema,
         },
@@ -634,25 +672,21 @@ const formConfig = {
         reasonForCurrentSeparation: {
           title: 'Reason for separation',
           path: 'household/marital-status/separated',
-          depends: formData => {
-            return formData.maritalStatus === 'Separated';
-          },
+          depends: isSeparated,
           uiSchema: reasonForCurrentSeparation.uiSchema,
           schema: reasonForCurrentSeparation.schema,
         },
         currentSpouseAddress: {
           title: 'Spouse address',
           path: 'household/marital-status/separated/spouse-address',
-          depends: form => showSpouseAddress(form),
+          depends: showSpouseAddress,
           uiSchema: currentSpouseAddress.uiSchema,
           schema: currentSpouseAddress.schema,
         },
         currentSpouseMonthlySupport: {
           title: 'Financial support for your spouse',
           path: 'household/marital-status/separated/spouse-monthly-support',
-          depends: formData => {
-            return formData.maritalStatus === 'Separated';
-          },
+          depends: isSeparated,
           uiSchema: currentSpouseMonthlySupport.uiSchema,
           schema: currentSpouseMonthlySupport.schema,
         },
@@ -666,7 +700,7 @@ const formConfig = {
         spouseMarriageHistory: {
           title: 'Spouse’s former marriages',
           path: 'household/marital-status/spouse-marriages',
-          depends: formData => formData.currentSpouseMaritalHistory === 'Yes',
+          depends: currentSpouseHasFormerMarriages,
           uiSchema: currentSpouseFormerMarriages.uiSchema,
           schema: currentSpouseFormerMarriages.schema,
         },
@@ -817,18 +851,14 @@ const formConfig = {
         homeAcreageMoreThanTwo: {
           title: 'Home acreage size',
           path: 'financial/home-ownership/acres',
-          depends: formData => {
-            return formData.homeOwnership === true;
-          },
+          depends: ownsHome,
           uiSchema: homeAcreageMoreThanTwo.uiSchema,
           schema: homeAcreageMoreThanTwo.schema,
         },
         homeAcreageValue: {
           title: 'Home acreage value',
           path: 'financial/home-ownership/acres/value',
-          depends: formData => {
-            return formData.homeAcreageMoreThanTwo === true;
-          },
+          depends: isHomeAcreageMoreThanTwo,
           uiSchema: {},
           schema: { type: 'object', properties: {} },
           CustomPage: HomeAcreageValueInput,
@@ -837,9 +867,7 @@ const formConfig = {
         landMarketable: {
           title: 'Land marketable',
           path: 'financial/land-marketable',
-          depends: formData => {
-            return formData.homeAcreageMoreThanTwo === true;
-          },
+          depends: isHomeAcreageMoreThanTwo,
           uiSchema: landMarketable.uiSchema,
           schema: landMarketable.schema,
         },
@@ -852,7 +880,7 @@ const formConfig = {
         incomeSources: {
           title: 'Gross monthly income',
           path: 'financial/income-sources',
-          depends: formData => formData.receivesIncome !== false,
+          depends: doesReceiveIncome,
           uiSchema: incomeSources.uiSchema,
           schema: incomeSources.schema,
         },
@@ -865,7 +893,7 @@ const formConfig = {
         careExpenses: {
           path: 'financial/care-expenses/add',
           title: 'Unreimbursed care expenses',
-          depends: formData => formData.hasCareExpenses,
+          depends: doesHaveCareExpenses,
           uiSchema: careExpenses.uiSchema,
           schema: careExpenses.schema,
         },
@@ -878,7 +906,7 @@ const formConfig = {
         medicalExpenses: {
           path: 'financial/medical-expenses/add',
           title: 'Medical expenses',
-          depends: formData => formData.hasMedicalExpenses,
+          depends: hasMedicalExpenses,
           uiSchema: medicalExpenses.uiSchema,
           schema: medicalExpenses.schema,
         },
