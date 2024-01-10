cebd333dce984de497bf2f60920fd76968647395
Anal Gomes <103195615+agomes82@users.noreply.github.com>
2024-01-09 19:48:18 -0500

    MHV-52713 Add TXT Download functionality for All Domain
    Separate file for List Page Print Button and Detail Page Print Button for Allergy and Vaccine

--- src/applications/mhv/medical-records/tests/e2e/medical-records-details-download-txt-allergies.cypress.spec.js
+++ src/applications/mhv/medical-records/tests/e2e/medical-records-details-download-txt-allergies.cypress.spec.js
@@ -1,4 +1,6 @@
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
+import AllergyDetailsPage from './pages/AllergyDetailsPage';
+import allergy from './fixtures/allergy.json';
 import allergies from './fixtures/allergies.json';
 import AllergiesListPage from './pages/AllergiesListPage';
 
@@ -13,16 +15,18 @@ describe('Medical Records View Allergies Print or download ', () => {
   it('Allergies Toggle Menu button Print or download on Details Page ', () => {
     // Given Navigate to Allergies List Page
     AllergiesListPage.clickGotoAllergiesLink(allergies);
-    // Verify List Page  PrintDownload button
-    AllergiesListPage.verifyPrintOrDownload('Print or download');
-    // Click List Page PrintDownload button
-    AllergiesListPage.clickPrintOrDownload();
-    // List Page should display print button for a list "Print this list"
-    AllergiesListPage.verifyPrintButton();
+    // Given Navigate to Allergies Details Link
+    AllergyDetailsPage.clickAllergyDetailsLink('NUTS', 7006, allergy);
+    // Verify Details Page PrintDownload button
+    AllergyDetailsPage.verifyPrintOrDownload('Print or download');
+    // Click Details Page PrintDownload button
+    AllergyDetailsPage.clickPrintOrDownload();
+    // Details Page should display print button for a list "Print this list"
+    AllergyDetailsPage.verifyPrintButton();
     // should display a download pdf file button "Download PDF of this page"
-    AllergiesListPage.verifyDownloadPDF();
+    AllergyDetailsPage.verifyDownloadPDF();
     // should display a download text file button "Download a text file (.txt) of this list"
-    AllergiesListPage.verifyDownloadTextFile();
+    AllergyDetailsPage.verifyDownloadTextFile();
 
     cy.injectAxe();
     cy.axeCheck('main');
