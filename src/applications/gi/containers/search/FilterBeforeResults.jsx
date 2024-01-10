f517373474a15c0f947600dbbda700bdb37703f1
fatmakhan0395 <Fatma.khan@va.gov>
2024-01-09 21:09:43 -0800

    added more tests to cover GI test coverage above 80%, as well as resolves flaky tests

--- src/applications/gi/containers/search/FilterBeforeResults.jsx
+++ src/applications/gi/containers/search/FilterBeforeResults.jsx
@@ -10,6 +10,7 @@ import JumpLink from '../../components/profile/JumpLink';
 import LearnMoreLabel from '../../components/LearnMoreLabel';
 import AccordionItem from '../../components/AccordionItem';
 import Dropdown from '../../components/Dropdown';
+import { isProductionOfTestProdEnv } from '../../../edu-benefits/1995/helpers';
 import {
   getStateNameForCode,
   sortOptionsByStateName,
@@ -356,21 +357,21 @@ export function FilterBeforeResults({
       {
         name: 'specialMissionHbcu',
         checked: specialMissionHbcu,
-        optionLabel: environment.isProduction()
+        optionLabel: isProductionOfTestProdEnv()
           ? 'Historically Black college or university'
           : 'Historically Black Colleges and Universities',
       },
       {
         name: 'specialMissionMenonly',
         checked: specialMissionMenonly,
-        optionLabel: environment.isProduction()
+        optionLabel: isProductionOfTestProdEnv()
           ? 'Men-only'
           : 'Men’s colleges and universities',
       },
       {
         name: 'specialMissionWomenonly',
         checked: specialMissionWomenonly,
-        optionLabel: environment.isProduction()
+        optionLabel: isProductionOfTestProdEnv()
           ? 'Women-only'
           : 'Women’s colleges and universities',
         // optionLabel: 'Women-only',
@@ -378,49 +379,49 @@ export function FilterBeforeResults({
       {
         name: 'specialMissionRelaffil',
         checked: specialMissionRelaffil,
-        optionLabel: environment.isProduction()
+        optionLabel: isProductionOfTestProdEnv()
           ? 'Religious affiliation'
           : 'Religiously affiliated institutions',
       },
       {
         name: 'specialMissionHSI',
         checked: specialMissionHSI,
-        optionLabel: environment.isProduction()
+        optionLabel: isProductionOfTestProdEnv()
           ? 'Hispanic-serving institutions'
           : 'Hispanic-Serving Institutions',
       },
       {
         name: 'specialMissionNANTI',
         checked: specialMissionNANTI,
-        optionLabel: environment.isProduction()
+        optionLabel: isProductionOfTestProdEnv()
           ? 'Native American-serving institutions'
           : 'Native American-Serving Nontribal Institutions',
       },
       {
         name: 'specialMissionANNHI',
         checked: specialMissionANNHI,
-        optionLabel: environment.isProduction()
+        optionLabel: isProductionOfTestProdEnv()
           ? 'Alaska Native-serving institutions'
           : 'Alaska Native-Serving Institutions',
       },
       {
         name: 'specialMissionAANAPII',
         checked: specialMissionAANAPII,
-        optionLabel: environment.isProduction()
+        optionLabel: isProductionOfTestProdEnv()
           ? 'Asian American Native American Pacific Islander-serving institutions'
           : 'Asian American and Native American Pacific Islander-Serving Institutions',
       },
       {
         name: 'specialMissionPBI',
         checked: specialMissionPBI,
-        optionLabel: environment.isProduction()
+        optionLabel: isProductionOfTestProdEnv()
           ? 'Predominantly Black institutions'
           : 'Predominantly Black Institutions',
       },
       {
         name: 'specialMissionTRIBAL',
         checked: specialMissionTRIBAL,
-        optionLabel: environment.isProduction()
+        optionLabel: isProductionOfTestProdEnv()
           ? 'Tribal college and university'
           : 'Tribal Colleges and Universities',
       },
