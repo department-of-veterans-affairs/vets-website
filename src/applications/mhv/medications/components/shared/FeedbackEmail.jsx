25b8c6d60818e45c41bdfefbd5625e99f8a9ac9b
Kolby Balch <108146636+KolbyVA@users.noreply.github.com>
2024-01-10 11:53:33 -0600

    [#mhv-52496] adds allergies to print view (#27379)

--- src/applications/mhv/medications/components/shared/FeedbackEmail.jsx
+++ src/applications/mhv/medications/components/shared/FeedbackEmail.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const FeedbackEmail = () => {
-  return <a href="mailto: vamhvfeedback@va.gov">vamhvfeedback@va.gov</a>;
+const FeedbackEmail = ({ forPrint = false }) => {
+  if (forPrint)
+    return (
+      <span className="print-only vads-u-display--inline-block">
+        vamhvfeedback@va.gov
+      </span>
+    );
+
+  return (
+    <a className="no-print" href="mailto: vamhvfeedback@va.gov">
+      vamhvfeedback@va.gov
+    </a>
+  );
 };
 
 export default FeedbackEmail;
+
+FeedbackEmail.propTypes = {
+  forPrint: PropTypes.bool,
+};
