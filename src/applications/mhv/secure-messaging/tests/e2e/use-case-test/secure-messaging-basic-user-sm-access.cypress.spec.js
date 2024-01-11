25427ed84bfe2e9edaa3bec531e724b68575c733
fazilqa <fazil.mammadov@bylight.com>
2024-01-11 10:18:38 -0800

    add: basic user-sm-access test

--- src/applications/mhv/secure-messaging/tests/e2e/use-case-test/secure-messaging-basic-user-sm-access.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/use-case-test/secure-messaging-basic-user-sm-access.cypress.spec.js
@@ -1,23 +1,54 @@
 import SecureMessagingSite from '../sm_site/SecureMessagingSite';
 import mockBasicUser from '../fixtures/userResponse/basic-user.json';
-
-import { AXE_CONTEXT } from '../utils/constants';
-import SecureMessagingLandingPage from '../pages/SecureMessagingLandingPage';
+import mockGeneralMessages from '../fixtures/generalResponses/generalMessages.json';
+// import { AXE_CONTEXT } from '../utils/constants';
+// import SecureMessagingLandingPage from '../pages/SecureMessagingLandingPage';
+import mockFeatureToggles from '../fixtures/generalResponses/featureToggles.json';
+import mockGeneralFolder from '../fixtures/generalResponses/generalFolder.json';
 
 describe('Secure Messaging Basic User', () => {
   it('verify user has not access to secure-messaging', () => {
     const site = new SecureMessagingSite();
     site.login(true, mockBasicUser);
-    SecureMessagingLandingPage.loadMainPage(mockBasicUser);
-    cy.injectAxe();
-    cy.axeCheck(AXE_CONTEXT, {
-      rules: {
-        'aria-required-children': {
-          enabled: false,
-        },
+
+    // in case if it could not be pushed w/o axe check assertion
+
+    // SecureMessagingLandingPage.loadMainPage();
+    //
+    // cy.injectAxe();
+    // cy.axeCheck(AXE_CONTEXT, {
+    //   rules: {
+    //     'aria-required-children': {
+    //       enabled: false,
+    //     },
+    //   },
+    // });
+
+    cy.intercept('GET', '/v0/feature_toggles?*', mockFeatureToggles).as(
+      'featureToggles',
+    );
+    cy.intercept('GET', '/v0/user', mockBasicUser).as('user');
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/0*',
+      mockGeneralFolder,
+    ).as('generalFolder');
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/0/messages*',
+      mockGeneralMessages,
+    ).as('generalMessages');
+
+    cy.visit('my-health/secure-messages/', {
+      onBeforeLoad: win => {
+        cy.stub(win, 'print');
       },
     });
 
-    cy.location('pathname').should('eq', '/health-care/secure-messages/');
+    cy.location('pathname').should('eq', '/health-care/secure-messaging');
+    cy.get('body').should(
+      'contain.text',
+      'Cannot GET /health-care/secure-messaging',
+    );
   });
 });
