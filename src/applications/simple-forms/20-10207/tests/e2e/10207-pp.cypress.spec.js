ab2179ceb9582dfbd986908022341f0f8198165d
Tze-chiu Lei <tclei2009@gmail.com>
2024-01-10 18:54:33 -0700

    Add/refactor 20-10207 e2e-specs

--- src/applications/simple-forms/20-10207/tests/e2e/10207-pp.cypress.spec.js
+++ src/applications/simple-forms/20-10207/tests/e2e/10207-pp.cypress.spec.js
@@ -5,12 +5,34 @@ import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-test
 
 import formConfig from '../../config/form';
 import manifest from '../../manifest.json';
+import featureToggles from './fixtures/mocks/featureToggles.json';
+import user from './fixtures/mocks/user.json';
+
+// mock logged in LOA3 user
+const userLOA3 = {
+  ...user,
+  data: {
+    ...user.data,
+    attributes: {
+      ...user.data.attributes,
+      login: {
+        currentlyLoggedIn: true,
+      },
+      profile: {
+        ...user.data.attributes.profile,
+        loa: {
+          current: 3,
+        },
+      },
+    },
+  },
+};
 
 const testConfig = createTestConfig(
   {
     dataPrefix: 'data',
 
-    dataDir: path.join(__dirname, 'data'),
+    dataDir: path.join(__dirname, 'fixtures', 'data'),
 
     // Rename and modify the test data as needed.
     dataSets: ['test-data'],
@@ -18,7 +40,7 @@ const testConfig = createTestConfig(
     pageHooks: {
       introduction: ({ afterHook }) => {
         afterHook(() => {
-          cy.findAllByText(/start/i, { selector: 'button' })
+          cy.findAllByText(/^Start your request/, { selector: 'a' })
             .last()
             .click();
         });
@@ -26,10 +48,9 @@ const testConfig = createTestConfig(
     },
 
     setupPerTest: () => {
-      // Log in if the form requires an authenticated session.
-      // cy.login();
-
-      cy.route('POST', formConfig.submitUrl, { status: 200 });
+      cy.intercept('/v0/api', { status: 200 });
+      cy.intercept('/v0/feature_toggles', featureToggles);
+      cy.login(userLOA3);
     },
 
     // Skip tests in CI until the form is released.
