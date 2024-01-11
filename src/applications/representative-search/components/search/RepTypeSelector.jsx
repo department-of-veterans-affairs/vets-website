ac008dfb4d9e0d35ddae7e0a5f76a3b55ea1e9e3
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-01-11 18:04:24 -0500

    Updated input labels per new design. Removed accordion component.

--- src/applications/representative-search/components/search/RepTypeSelector.jsx
+++ src/applications/representative-search/components/search/RepTypeSelector.jsx
@@ -15,6 +15,7 @@ const RepTypeSelector = ({ onChange, representativeType }) => {
           error={null}
           hint=""
           required
+          label="Type of accredited representative"
           label-header-level=""
           onVaValueChange={handleRadioButtonSelect}
         >
@@ -27,7 +28,7 @@ const RepTypeSelector = ({ onChange, representativeType }) => {
             vaValueChange={handleRadioButtonSelect}
           />
           <va-radio-option
-            label="Attorney"
+            label="Accredited attorney"
             name="group"
             value="attorney"
             checked={representativeType === 'attorney'}
@@ -45,53 +46,6 @@ const RepTypeSelector = ({ onChange, representativeType }) => {
             />
           )}
         </VaRadio>
-
-        <div style={{ marginTop: '2em' }}>
-          <va-accordion
-            disable-analytics={{
-              value: 'false',
-            }}
-            section-heading={{
-              value: 'null',
-            }}
-            uswds
-            open-single
-          >
-            <va-accordion-item id="first">
-              <h3 slot="headline" style={{ fontSize: '16px' }}>
-                How can each type of accredited representative help me?
-              </h3>
-              <p>
-                <strong>Veteran Service Officer (VSO)</strong> representatives
-                can help you gather evidence and file your claims, decision
-                reviews, and appeals. They can also communicate with VA about
-                your case on your behalf. Examples of VSOs include the American
-                Legion, County Veteran Service Offices, Disabled American
-                Veterans, and Veterans of Foreign Wars.
-              </p>
-              <p>
-                VSO representatives have completed training and passed tests
-                about VA claims and benefits. They provide services to Veterans
-                and their families at no cost.
-              </p>
-              <p>
-                <strong>Attorneys</strong> usually work on decision reviews and
-                appeals, including cases that require legal knowledge. They
-                don’t have to take a test about VA claims and benefits, but they
-                must be a member in good standing of the bar association.
-                Attorneys can charge fees for their services.
-              </p>
-              {claimsAgentIsEnabled && (
-                <p>
-                  <strong>Claims agents</strong> usually work on decision
-                  reviews and appeals. They’re independent professionals who
-                  have passed a test about VA claims and benefits. Claims agents
-                  can charge fees for their services.
-                </p>
-              )}
-            </va-accordion-item>
-          </va-accordion>
-        </div>
       </div>
     </>
   );
