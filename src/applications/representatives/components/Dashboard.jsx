f0838f6986b1f53a91a114a3dd0455eeeefb46b9
cohnjesse <cohnjesse@gmail.com>
2024-01-11 13:13:29 -0500

    Made style edits to get more in line with mockup

--- src/applications/representatives/components/Dashboard.jsx
+++ src/applications/representatives/components/Dashboard.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 
 const Dashboard = () => (
-  <div id="rep-dashboard" className="vads-u-margin--4">
-    <div className="left vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--4 large-screen:vads-l-col--3 vads-u-background-color--gray-lightest vads-u-margin-top--5">
-      Side Navigation / Secondary Tasks
-    </div>
-    <div className="right vads-m-col--8 vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--8 large-screen:vads-l-col--9">
-      <div className="dash-header vads-u-font-size--h3">Rep VA Dashboard</div>
-      <div className="nav dash-box vads-u-background-color--gray-lightest">
-        Navigation
-      </div>
-      <div className="notif dash-box vads-u-background-color--gray-lightest">
-        Notifications
-      </div>
-      <div className="primary dash-box vads-u-background-color--gray-lightest">
-        Primary Tasks
-      </div>
-      <div
-        className="etc dash-box vads-u-background-color--gray-lightest"
-        vads-u-background-color--gray-lightest
-      />
+  <div className="dash-container">
+    <h1 className="dash-header vads-u-font-size--h2 vads-u-margin-bottom--4">
+      Rep VA Dashboard
+    </h1>
+    <div className="vads-u-display--flex vads-u-justify-content--center">
+      <aside className="left vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--4 large-screen:vads-l-col--3 vads-u-background-color--gray-lightest" />
+      <article className="vads-m-col--8 vads-l-col--12 vads-u-padding-left--5 medium-screen:vads-l-col--8 large-screen:vads-l-col--5">
+        <div className="nav dash-box vads-u-background-color--gray-lightest vads-u-margin-bottom--2" />
+        <div className="notif dash-box vads-u-background-color--gray-lightest vads-u-margin-bottom--2" />
+        <div className="primary dash-box vads-u-background-color--gray-lightest vads-u-margin-bottom--2" />
+        <div
+          className="etc dash-box vads-u-background-color--gray-lightest"
+          vads-u-background-color--gray-lightest
+        />
+      </article>
     </div>
   </div>
 );
