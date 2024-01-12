b686817245aad3c4a5e67ef8dfd8a644a869b0e8
Michael Clement <michael.clement@adhocteam.us>
2024-01-12 10:05:14 -0600

    27448 add initial applicant information section to ivc champva form 1010d (#27462)
    * renamed section complete component
    
    * added initial fields to applicant flow

--- src/applications/ivc-champva/10-10D/components/SectionCompleteAlert.jsx
+++ src/applications/ivc-champva/10-10D/components/SectionCompleteAlert.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { VaAlert } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 
-export default function ApplicantField() {
+export default function SectionCompleteAlert() {
   return (
     <VaAlert status="success" visible uswds>
       <h2>Section Complete</h2>
