479f8a7c8520ca01a6003cb1034f01e353bb8d6b
Vitalii Menshutin <Vitalii.Menshutin@bylight.com>
2024-01-12 00:57:29 -0500

    MHV-51130: RX TXT files download

--- src/applications/mhv/medications/components/shared/PrintDownload.jsx
+++ src/applications/mhv/medications/components/shared/PrintDownload.jsx
@@ -2,28 +2,33 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import FeedbackEmail from './FeedbackEmail';
 
+export const DOWNLOAD_FORMAT = {
+  PDF: 'PDF',
+  TXT: 'TXT',
+};
+
 const PrintDownload = props => {
   const { download, isSuccess, list } = props;
   const [isError, setIsError] = useState(false);
 
-  // Variables required for dropdown button group should we go back to it
-  // const [menuOpen, setMenuOpen] = useState(false);
-  // let toggleMenuButtonClasses =
-  //   'toggle-menu-button vads-u-justify-content--space-between';
-  // let menuOptionsClasses = 'menu-options';
-  // let menuIconClasses =
-  //   'fas fa-angle-down vads-u-color--primary vads-u-margin-left--0p5';
-  // if (menuOpen) {
-  //   toggleMenuButtonClasses +=
-  //     'toggle-menu-button-open vads-u-justify-content--space-between';
-  //   menuOptionsClasses += ' menu-options-open';
-  //   menuIconClasses =
-  //     'fas fa-angle-up vads-u-color--primary vads-u-margin-left--0p5';
-  // }
-  const handleDownloadPDF = async () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  let toggleMenuButtonClasses =
+    'toggle-menu-button vads-u-justify-content--space-between';
+  let menuOptionsClasses = 'menu-options';
+  let menuIconClasses =
+    'fas fa-angle-down vads-u-color--primary vads-u-margin-left--0p5';
+  if (menuOpen) {
+    toggleMenuButtonClasses +=
+      'toggle-menu-button-open vads-u-justify-content--space-between';
+    menuOptionsClasses += ' menu-options-open';
+    menuIconClasses =
+      'fas fa-angle-up vads-u-color--primary vads-u-margin-left--0p5';
+  }
+
+  const handleDownload = async format => {
     try {
-      await download();
       setIsError(false);
+      await download(format);
     } catch {
       setIsError(true);
     }
@@ -51,55 +56,39 @@ const PrintDownload = props => {
           </va-alert>
         </div>
       )}
-      <button
-        type="button"
-        className="link-button vads-u-margin-bottom--3"
-        onClick={handleDownloadPDF}
-        data-testid="download-pdf-button"
-      >
-        <i
-          className="fas fa-download vads-u-margin-right--0p5"
-          aria-hidden="true"
-        />
-        {list
-          ? 'Download your medication list as a PDF'
-          : 'Download this page as a PDF'}
-      </button>
-
-      {/* Code for dropdown print/download button should we go back to it
-       <div className="print-download vads-u-margin-y--2 no-print">
-         <button
-           type="button"
-           className={toggleMenuButtonClasses}
-           onClick={() => setMenuOpen(!menuOpen)}
-           data-testid="print-records-button"
-           aria-expanded={menuOpen}
-         >
-           <span>Print or download this {list ? 'list' : 'page'}</span>
-           <i className={menuIconClasses} aria-hidden="true" />
-         </button>
-         <ul className={menuOptionsClasses}>
-           <li>
-             <button
-               type="button"
-               data-testid="print-button"
-               onClick={window.print}
-             >
-               Print {list && 'list'}
-             </button>
-           </li>
-           <li>
-             <button
-               type="button"
-               data-testid="download-pdf-button"
-               onClick={download}
-             >
-               Download {list && 'list '}
-               as PDF
-             </button>
-           </li>
-         </ul>
-       </div> */}
+      <div className="print-download vads-u-margin-y--2 no-print">
+        <button
+          type="button"
+          className={toggleMenuButtonClasses}
+          onClick={() => setMenuOpen(!menuOpen)}
+          data-testid="print-records-button"
+          aria-expanded={menuOpen}
+        >
+          <span>Print or download this {list ? 'list' : 'page'}</span>
+          <i className={menuIconClasses} aria-hidden="true" />
+        </button>
+        <ul className={menuOptionsClasses}>
+          <li>
+            <button
+              type="button"
+              data-testid="download-pdf-button"
+              onClick={() => handleDownload(DOWNLOAD_FORMAT.PDF)}
+            >
+              Download {list && 'list '}
+              as PDF
+            </button>
+          </li>
+          <li>
+            <button
+              type="button"
+              data-testid="download-txt-button"
+              onClick={() => handleDownload(DOWNLOAD_FORMAT.TXT)}
+            >
+              Download {list ? 'list' : 'page'} as a text file
+            </button>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
