f90e891e5306ea7f22896868f22e5716c18f5390
Anal Gomes <103195615+agomes82@users.noreply.github.com>
2024-01-12 17:51:37 -0500

    MHV-52163 Automate test for understanding your results dropdown
    Chem/Hem, Microbiology, Pathology, and Radiology.

--- src/applications/mhv/medical-records/tests/e2e/pages/ChemHemDetailsPage.js
+++ src/applications/mhv/medical-records/tests/e2e/pages/ChemHemDetailsPage.js
@@ -129,6 +129,40 @@ class ChemHemDetailsPage {
     // should display a download text file button "Download list as a text file"
     cy.get('[data-testid="printButton-1"]').click();
   };
+
+  verifyEpnadUnderstandResultsBtton = () => {
+    // Expand "help to be able to understand results"
+    cy.get('[data-testid="understanding-result"]').should('be.visible');
+  };
+
+  clickExpnadUnderstandResultsBtton = () => {
+    // Expand "help to be able to understand results"
+    cy.get('[data-testid="understanding-result"]').click();
+  };
+
+  verifyResultAlert1 = resultAlert1 => {
+    // should display  "If your results are outside the reference range"
+    cy.get('[data-testid="result-Alert-1"]').should('be.visible');
+    cy.get('[data-testid="result-Alert-1"]').contains(resultAlert1);
+  };
+
+  verifyResultAlert2 = resultAlert2 => {
+    // should display  "Your provider will review your results. If you need to do anything, your provider will contact you."
+    cy.get('[data-testid="result-Alert-2"]').should('be.visible');
+    cy.get('[data-testid="result-Alert-2"]').contains(resultAlert2);
+  };
+
+  verifyResultAlert3 = resultAlert3 => {
+    // should display  "If you have any questions, send a message to the care team that ordered this test"
+    cy.get('[data-testid="result-Alert-3"]').should('be.visible');
+    cy.get('[data-testid="result-Alert-3"]').contains(resultAlert3);
+  };
+
+  verifyComposeMessageLink = composeMessageLink => {
+    // verify compose a message on the My Healthvet website
+    cy.get('[data-testid="compose-message-Link"]').should('be.visible');
+    cy.get('[data-testid="compose-message-Link"]').contains(composeMessageLink);
+  };
 }
 
 export default new ChemHemDetailsPage();
