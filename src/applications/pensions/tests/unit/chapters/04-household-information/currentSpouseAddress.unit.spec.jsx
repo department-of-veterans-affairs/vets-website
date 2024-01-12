b8b8db80a674e6db8bfef14784d74b710a3b3f37
Tai Wilkin-Corraggio <tai.wilkin@coforma.io>
2024-01-12 13:02:38 -0500

    Pensions: Update copy for dependent children (#27464)
    * Update dependent children copy
    
    * Fix broken tests

--- src/applications/pensions/tests/unit/chapters/04-household-information/currentSpouseAddress.unit.spec.jsx
+++ src/applications/pensions/tests/unit/chapters/04-household-information/currentSpouseAddress.unit.spec.jsx
@@ -6,24 +6,22 @@ import formConfig from '../../../../config/form';
 import currentSpouseAddress from '../../../../config/chapters/04-household-information/currentSpouseAddress';
 
 describe('current spouse address page', () => {
-  it('should render web components in form correctly', () => {
-    const pageTitle = 'current spouse address';
-    const expectedNumberOfFields = 6;
-    testNumberOfWebComponentFields(
-      formConfig,
-      currentSpouseAddress.schema,
-      currentSpouseAddress.uiSchema,
-      expectedNumberOfFields,
-      pageTitle,
-    );
+  const pageTitle = 'current spouse address';
+  const expectedNumberOfFields = 6;
+  testNumberOfWebComponentFields(
+    formConfig,
+    currentSpouseAddress.schema,
+    currentSpouseAddress.uiSchema,
+    expectedNumberOfFields,
+    pageTitle,
+  );
 
-    const expectedNumberOfErrors = 4;
-    testNumberOfErrorsOnSubmitForWebComponents(
-      formConfig,
-      currentSpouseAddress.schema,
-      currentSpouseAddress.uiSchema,
-      expectedNumberOfErrors,
-      pageTitle,
-    );
-  });
+  const expectedNumberOfErrors = 4;
+  testNumberOfErrorsOnSubmitForWebComponents(
+    formConfig,
+    currentSpouseAddress.schema,
+    currentSpouseAddress.uiSchema,
+    expectedNumberOfErrors,
+    pageTitle,
+  );
 });
