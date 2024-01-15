1fd0c657310a74b0ded7f18d289ad59b29eec505
Tze-chiu Lei <tclei2009@gmail.com>
2024-01-14 18:40:23 -0700

    Update Cypress-local-reports script

--- script/run-local-cy-with-reports.js
+++ script/run-local-cy-with-reports.js
@@ -1,11 +1,17 @@
 /* eslint-disable no-console */
+/* run-local-cy-with-reports.js
+  * Script to run Cypress tests locally with mochawesome reports
+  * yarn cy:run:localreport --spec "your-spec-files-global-pattern"
+  */
 const fs = require('fs');
 const path = require('path');
 const rimraf = require('rimraf');
 const cheerio = require('cheerio');
 const { execSync } = require('child_process');
+const chalk = require('chalk');
+const cyLocalReportsHtml = require('./cyLocalReportsHtml');
 
-// Get the --spec parameter from the command line arguments
+// Get --spec parameter from command line arguments
 let spec = '';
 const specArgIndex = process.argv.findIndex(arg => arg === '--spec');
 if (specArgIndex !== -1 && process.argv[specArgIndex + 1]) {
@@ -14,86 +20,87 @@ if (specArgIndex !== -1 && process.argv[specArgIndex + 1]) {
   const specArg = process.argv.find(arg => arg.startsWith('--spec='));
   spec = specArg ? specArg.split('=')[1] : '';
 }
-console.log(`Running Cypress tests with --spec: ${spec}`);
+console.log(chalk.yellow(`RUNNING CYPRESS WITH LOCAL REPORTS --spec: ${spec}`));
 
-// Delete all previously generated test-results
-// Set folder paths relative to this script
+// Delete all output from previous test-runs
 try {
-  console.log('Deleting old report output files');
   rimraf.sync('cypress/*');
   rimraf.sync('mochawesome-report/*');
+  console.log(chalk.green('Old test-output files deleted.'));
 } catch (error) {
-  console.error('Error deleting files:', error);
+  console.error(chalk.red('Error deleting old test-output files:', error));
 }
 
 // Run Cypress tests
-execSync(
-  `npx cypress run --config-file config/cypress-local-with-report.config.js ${
-    spec ? `--spec "${spec}"` : ''
-  }`,
-  {
-    stdio: 'inherit',
-  },
-);
+try {
+  execSync(
+    `npx cypress run --config-file config/cypress-local-with-report.config.js ${
+      spec ? `--spec "${spec}"` : ''
+    }`,
+    {
+      stdio: 'inherit',
+    },
+  );
+} catch (error) {
+  console.error(chalk.red('Cypress tests failed:', error));
+}
 
-// Rename all generated HTML report-files with suite-titles
-const htmlFiles1 = fs
-  .readdirSync('mochawesome-report')
-  .filter(file => !file.startsWith('index') && file.endsWith('.html'));
-
+// Rename generated HTML report-files with spec filenames
+let htmlFiles1;
+let data;
+try {
+  htmlFiles1 = fs
+    .readdirSync('mochawesome-report')
+    .filter(file => !file.startsWith('index') && file.endsWith('.html'));
+  console.log(chalk.green(`Report-files found: ${htmlFiles1}`));
+} catch (error) {
+  console.error(chalk.red('Error reading report-files:', error));
+}
 htmlFiles1.forEach(file => {
   const filePath = path.join('mochawesome-report', file);
-  const content = fs.readFileSync(filePath, 'utf8');
-  const $ = cheerio.load(content);
-  const dataRaw = $('body').attr('data-raw');
+  let content;
+  let dataRaw;
+  let $;
+  let fileName;
 
-  // Extract suite title from body tag's data-raw attribute
-  let data;
   try {
-    data = JSON.parse(decodeURIComponent(dataRaw));
+    content = fs.readFileSync(filePath, 'utf8');
+    $ = cheerio.load(content);
+    dataRaw = $('body').attr('data-raw');
+    data = JSON.parse(decodeURIComponent(encodeURIComponent(dataRaw)));
+    fileName = data.results[0].file
+      .split('/')
+      .pop()
+      .replace(/\.cypress\.spec\.js(x)?$/, '');
+    if (fileName) {
+      fs.renameSync(
+        filePath,
+        path.join('mochawesome-report', `${fileName}.html`),
+      );
+      console.log(chalk.green(`Report-file ${file} renamed: ${fileName}.html`));
+    }
   } catch (error) {
-    console.error(`Error parsing JSON from data-row in file ${file}:`, error);
-    return;
-  }
-
-  const suiteName = data.results[0].suites[0].title.replace(/ /g, '_');
-  console.log(`suiteName: ${suiteName}`);
-
-  if (suiteName) {
-    fs.renameSync(
-      filePath,
-      path.join('mochawesome-report', `${suiteName}.html`),
-    );
+    console.error(chalk.red(`Error renaming report-files:`, error));
   }
 });
 
-// Get a list of all HTML files in the mochawesome-report directory
-const htmlFiles2 = fs
-  .readdirSync('mochawesome-report')
-  .filter(file => !file.startsWith('index') && file.endsWith('.html'));
-console.log(`Renamed HTML report-files: ${htmlFiles2}`);
-
-// Generate an index.html with links to above HTML files
-const htmlContent = `
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Cypress test-results</title>
-</head>
-<body>
-  <h1>Cypress test-results</h1>
-  ${htmlFiles2
-    .map(
-      file =>
-        `<p><a href="${file}">${file
-          .replace(/_/g, ' ')
-          .replace('.html', '')}</a></p>`,
-    )
-    .join('\n')}
-</body>
-</html>
-`;
-
-// Write the HTML content to an index.html file
-fs.writeFileSync(path.join('mochawesome-report', 'index.html'), htmlContent);
+// Generate index.html
+try {
+  const htmlFiles2 = fs
+    .readdirSync('mochawesome-report')
+    .filter(file => !file.startsWith('index') && file.endsWith('.html'));
+  const htmlContent = cyLocalReportsHtml(
+    htmlFiles2,
+    // passes,
+    // failures,
+    // skipped,
+    // pending,
+  );
+  fs.writeFileSync(path.join('mochawesome-report', 'index.html'), htmlContent);
+  console.log(
+    chalk.green(`Index generated for renamed report-files: ${htmlFiles2}`),
+  );
+} catch (error) {
+  console.error(chalk.red('Error generating index:', error));
+}
 /* eslint-enable no-console */
