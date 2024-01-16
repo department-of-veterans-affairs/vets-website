90115d7ea364ed40c17674d86c01bb8b1270a067
Tze-chiu Lei <tze@adhocteam.us>
2024-01-16 10:38:48 -0700

    20-10207 - Build Introduction page LOA3 & write associated tests (#27501)
    * Add 20-10207 form & Intro-page unit tests
    
    * Build 20-10207 Intro-page
    
    * Add/refactor 20-10207 e2e-specs
    
    * CI-skip 20-10207 Intro-page e2e-spec
    
    * Remove unneeded code from 20-10207 form-config unit-spec
    
    * Clean-up/fix 20-10206 unit-tests
    
    * Add WIP comment to 20-10207 Intro-page unit-spec
    
    * Update 20-10207 Intro-page unit-spec
    
    * Fix 20-10207 Intro-page unit-test

---
+++ src/applications/simple-forms/20-10207/config/constants.js
@@ -0,0 +1,2 @@
+export const TITLE = 'Request priority processing of an existing claim';
+export const SUBTITLE = 'Priority processing request (VA form 20-10207)';
---
+++ src/applications/simple-forms/20-10207/tests/e2e/10207-pp.cypress.spec.js
@@ -0,0 +1,64 @@
+import path from 'path';
+
+import testForm from 'platform/testing/e2e/cypress/support/form-tester';
+import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-tester/utilities';
+
+import formConfig from '../../config/form';
+import manifest from '../../manifest.json';
+import featureToggles from './fixtures/mocks/featureToggles.json';
+import user from './fixtures/mocks/user.json';
+
+// mock logged in LOA3 user
+const userLOA3 = {
+  ...user,
+  data: {
+    ...user.data,
+    attributes: {
+      ...user.data.attributes,
+      login: {
+        currentlyLoggedIn: true,
+      },
+      profile: {
+        ...user.data.attributes.profile,
+        loa: {
+          current: 3,
+        },
+      },
+    },
+  },
+};
+
+const testConfig = createTestConfig(
+  {
+    dataPrefix: 'data',
+
+    dataDir: path.join(__dirname, 'fixtures', 'data'),
+
+    // Rename and modify the test data as needed.
+    dataSets: ['test-data'],
+
+    pageHooks: {
+      introduction: ({ afterHook }) => {
+        afterHook(() => {
+          cy.findAllByText(/^Start your request/, { selector: 'a' })
+            .last()
+            .click();
+        });
+      },
+    },
+
+    setupPerTest: () => {
+      cy.intercept('/v0/api', { status: 200 });
+      cy.intercept('/v0/feature_toggles', featureToggles);
+      cy.login(userLOA3);
+    },
+
+    // Skip tests in CI until the form is released.
+    // Remove this setting when the form has a content page in production.
+    skip: Cypress.env('CI'),
+  },
+  manifest,
+  formConfig,
+);
+
+testForm(testConfig);
