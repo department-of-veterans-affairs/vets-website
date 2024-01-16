c5fd44bc7c75d9286e15568ae52e533908b05c66
Vic Saleem <victoria.saleem@va.gov>
2024-01-16 11:04:02 -0600

    [MHV-44454] Added printThread component for print only

---
+++ src/applications/mhv/secure-messaging/components/MessageThread/MessageThreadForPrint.jsx
@@ -0,0 +1,44 @@
+/*
+Printable full message thread expanded
+*/
+
+import React from 'react';
+import PropType from 'prop-types';
+import { VaAccordion } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import MessageThreadItem from './MessageThreadItem';
+
+const MessageThreadForPrint = props => {
+  const { messageHistory, printThread } = props;
+
+  return (
+    <>
+      <h2 className="messages-in-conversation vads-u-font-weight--bold vads-u-margin-bottom--0p5 vads-u-font-size--lg">
+        {messageHistory?.length > 0 &&
+          `${messageHistory?.length} Message${
+            messageHistory?.length > 1 ? 's' : ''
+          } in this conversation`}
+      </h2>
+      <VaAccordion bordered>
+        {messageHistory?.map(m => {
+          return (
+            <>
+              <MessageThreadItem
+                printThread={printThread}
+                key={m.messageId}
+                message={m}
+                preloaded={m.preloaded}
+                expanded
+              />
+            </>
+          );
+        })}
+      </VaAccordion>
+    </>
+  );
+};
+MessageThreadForPrint.propTypes = {
+  isDraftThread: PropType.bool,
+  messageHistory: PropType.array,
+  printThread: PropType.bool,
+};
+export default MessageThreadForPrint;
