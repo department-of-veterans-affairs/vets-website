6e65e271e21a1eedf81e22bbf3580794323d0f34
William Phelps <williamphelps13@gmail.com>
2024-01-16 19:59:28 -0700

    72959 Add and integrate representatives portal feature toggle

--- src/platform/utilities/feature-toggles/featureFlagNames.js
+++ src/platform/utilities/feature-toggles/featureFlagNames.js
@@ -180,6 +180,7 @@ export default Object.freeze({
   ratedDisabilitiesDetectDiscrepancies:
     'rated_disabilities_detect_discrepancies',
   ratedDisabilitiesUseLighthouse: 'rated_disabilities_use_lighthouse',
+  'representativesPortalFrontend': 'representatives_portal_frontend',
   scBrowserMonitoringEnabled: 'sc_browser_monitoring_enabled',
   searchRepresentative: 'search_representative',
   searchDropdownComponentEnabled: 'search_dropdown_component_enabled',
