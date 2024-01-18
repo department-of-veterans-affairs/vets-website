291de7dc48fcea15087047c6d2c906fbb1796550
Michael Clement <michael.clement@adhocteam.us>
2024-01-18 16:16:10 -0600

    reverting import because it broke cypress test

--- src/applications/ivc-champva/10-10D/config/form.js
+++ src/applications/ivc-champva/10-10D/config/form.js
@@ -25,7 +25,7 @@ import {
   radioUI,
   titleSchema,
   inlineTitleUI,
-} from '@department-of-veterans-affairs/platform-forms-system/web-component-patterns';
+} from 'platform/forms-system/src/js/web-component-patterns';
 import get from '@department-of-veterans-affairs/platform-forms-system/get';
 import fileUploadUI from '@department-of-veterans-affairs/platform-forms-system/definitions/file';
 
