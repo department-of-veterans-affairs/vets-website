09aff8607637d370a6ca398033d4b96e510600cb
Chris Kim <chriskim@Chriss-MacBook-Pro-2.local>
2024-01-17 16:58:04 -0800

    init commit for removal of dynamic breadcrumbs

--- src/applications/pact-act/components/Breadcrumbs.jsx
+++ src/applications/pact-act/components/Breadcrumbs.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { VaBreadcrumbs } from '@department-of-veterans-affairs/web-components/react-bindings';
 import { QUESTION_MAP } from '../constants/question-data-map';
 
-const Breadcrumbs = ({ currentPage }) => {
+const Breadcrumbs = () => {
   return (
     <VaBreadcrumbs
       class="pact-act-breadcrumbs vads-u-margin-left--1p5"
@@ -16,7 +16,7 @@ const Breadcrumbs = ({ currentPage }) => {
         },
         {
           href: '/pact-act-eligibility',
-          label: QUESTION_MAP?.[currentPage] || 'PACT Act',
+          label: QUESTION_MAP.HOME,
         },
       ]}
     />
