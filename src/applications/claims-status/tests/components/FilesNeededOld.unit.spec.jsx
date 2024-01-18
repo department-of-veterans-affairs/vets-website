9049ac3d9529151a5ed7a8943172800a6e9b27fe
Peri McLaren <peri.mclaren@adhocteam.us>
2024-01-18 15:47:08 -0700

    added tests for FilesNeeded and FilesNeededOld

---
+++ src/applications/claims-status/tests/components/FilesNeededOld.unit.spec.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import SkinDeep from 'skin-deep';
+import { expect } from 'chai';
+import FilesNeededOld from '../../components/FilesNeededOld';
+
+describe('<FilesNeededOld>', () => {
+  it('should render alert with item data', () => {
+    const id = 1;
+    const item = {
+      displayName: 'Request 1',
+      description: 'This is a alert',
+      suspenseDate: '2024-12-01',
+    };
+    const tree = SkinDeep.shallowRender(<FilesNeededOld id={id} item={item} />);
+
+    expect(tree.everySubTree('.file-request-title')[0].text()).to.equal(
+      item.displayName,
+    );
+    expect(tree.everySubTree('.submission-description')[0].text()).to.equal(
+      item.description,
+    );
+  });
+});
