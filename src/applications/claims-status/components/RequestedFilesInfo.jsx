5e93455eae09437aa9d9b3b249ba1c78803d7310
Peri McLaren <peri.mclaren@adhocteam.us>
2024-01-18 13:30:08 -0700

    Update RequestedFilesInfo with the component FilesNeededOld

--- src/applications/claims-status/components/RequestedFilesInfo.jsx
+++ src/applications/claims-status/components/RequestedFilesInfo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 
 import AdditionalEvidencePage from '../containers/AdditionalEvidencePage';
 import { getTrackedItemId, truncateDescription } from '../utils/helpers';
-import DueDateOld from './DueDateOld';
+import FilesNeededOld from './FilesNeededOld';
 
 export default function RequestedFilesInfo({ id, filesNeeded, optionalFiles }) {
   return (
@@ -19,30 +19,7 @@ export default function RequestedFilesInfo({ id, filesNeeded, optionalFiles }) {
         ) : null}
 
         {filesNeeded.map(item => (
-          <div
-            className="file-request-list-item usa-alert usa-alert-warning background-color-only alert-with-details"
-            key={getTrackedItemId(item)}
-          >
-            <div className="item-container">
-              <h3 className="file-request-title">{item.displayName}</h3>
-              <p className="submission-description">
-                {truncateDescription(item.description)}
-              </p>
-              <DueDateOld date={item.suspenseDate} />
-            </div>
-            <div className="button-container">
-              <Link
-                aria-label={`View Details for ${item.displayName}`}
-                title={`View Details for ${item.displayName}`}
-                className="usa-button usa-button-secondary view-details-button"
-                to={`your-claims/${id}/document-request/${getTrackedItemId(
-                  item,
-                )}`}
-              >
-                View Details
-              </Link>
-            </div>
-          </div>
+          <FilesNeededOld key={getTrackedItemId(item)} id={id} item={item} />
         ))}
 
         {optionalFiles.map(item => (
