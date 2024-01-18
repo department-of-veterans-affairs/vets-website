23928ec7089f9eb8b82b449bf76220e0a3832c29
rocio-desantiago <rocio.de-santiago@coforma.io>
2024-01-18 07:41:19 -0800

    NOD | Prototype Confirmation content updates (#27546)
    * Add information to view appeal page
    
    * Add information under inset box for the Confirmation Page
    
    * update content for GetFormHelp component
    
    ---------
    
    Co-authored-by: Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
    Co-authored-by: Rob Garrison <wowmotty@gmail.com>

--- src/applications/appeals/10182/content/GetFormHelp.jsx
+++ src/applications/appeals/10182/content/GetFormHelp.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
 
-import { srSubstitute } from 'platform/forms-system/src/js/utilities/ui/mask-string';
-
 const GetFormHelp = () => (
   <>
     <p className="help-talk">
-      If you have questions or need help filling out this form, call our{' '}
-      {srSubstitute('MYVA411', 'My V. A. 4 1 1.')} main information line at{' '}
-      <va-telephone contact={CONTACTS.HELP_DESK} /> and select 0. We’re here{' '}
-      {srSubstitute('24/7', '24 hours a day, 7 days a week')}.
+      If you have questions please call us at{' '}
+      <va-telephone contact={CONTACTS.VA_BENEFITS} />. We’re here Monday through
+      Friday, 8:00 a.m. to 9:00 p.m ET.{' '}
     </p>
     <p className="u-vads-margin-bottom--0">
-      If you have hearing loss, call{' '}
-      <va-telephone contact={CONTACTS['711']} tty />.
+      If you have hearing loss, call TTY:{' '}
+      <va-telephone contact={CONTACTS[711]} />.
     </p>
   </>
 );
