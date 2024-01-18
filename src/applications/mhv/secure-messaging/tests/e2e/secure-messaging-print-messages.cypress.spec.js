a28deb7113080f58e677dca3119a931f35ae0217
Aaron Read <Aaron.Read@va.gov>
2024-01-18 12:53:16 -0600

    Update secure-messaging-print-messages.cypress.spec.js

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-print-messages.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-print-messages.cypress.spec.js
@@ -24,22 +24,22 @@ describe('Secure Messaging - Print Functionality', () => {
   });
 
   it('print messages', () => {
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT, {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+        'color-contrast': {
+          enabled: false,
+        },
+      },
+    });
     cy.get(Locators.BUTTONS.PRINT).should('be.visible');
     cy.get(Locators.BUTTONS.PRINT).click();
     cy.window().then(win => {
-      expect(win.print).to.be.calledTwice;
-
-      cy.injectAxe();
-      cy.axeCheck(AXE_CONTEXT, {
-        rules: {
-          'aria-required-children': {
-            enabled: false,
-          },
-          'color-contrast': {
-            enabled: false,
-          },
-        },
-      });
+      cy.get(Locators.BUTTONS.PRINT).should('be.visible');
+      expect(win.print).to.be.calledOnce;
     });
   });
 });
