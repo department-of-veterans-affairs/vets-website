0377a2a96bc59f64c43a2c23ce64f8dbe9cf8f02
Anal Gomes <103195615+agomes82@users.noreply.github.com>
2024-01-19 16:12:33 -0500

    Add TXT Download for consolidated report (all domains/Blue Button)
    Update feature toggles

--- src/applications/mhv/medical-records/tests/e2e/mr_site/MedicalRecordsSite.js
+++ src/applications/mhv/medical-records/tests/e2e/mr_site/MedicalRecordsSite.js
@@ -50,7 +50,37 @@ class MedicalRecordsSite {
       cy.login();
       window.localStorage.setItem('isLoggedIn', true);
       cy.intercept('GET', '/v0/user', mockNonMRuser).as('mockUser');
-      cy.intercept('GET', '/v0/feature_toggles?*', {}).as('featureToggle');
+      cy.intercept('GET', '/v0/feature_toggles?*', {
+        data: {
+          type: 'feature_toggles',
+          features: [
+            {
+              name: 'mhvMedicalRecordsToVAGovRelease',
+              value: true,
+            },
+            {
+              name: 'mhv_medical_records_to_va_gov_release',
+              value: true,
+            },
+            {
+              name: 'mhvMedicalRecordsDisplayDomains',
+              value: true,
+            },
+            {
+              name: 'mhv_medical_records_display_domains',
+              value: true,
+            },
+            {
+              name: 'mhv_medical_records_allow_txt_downloads',
+              value: true,
+            },
+            {
+              name: 'mhv_medical_records_display_vaccines',
+              value: true,
+            },
+          ],
+        },
+      }).as('featureToggle');
     }
   };
 
