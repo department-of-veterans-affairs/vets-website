e41702dd2ec76b03444bb9f9447520e8db88ac92
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2024-01-19 11:35:12 -0500

    [#MHV-53646] prevent allrecipients call on unauthorized view

--- src/applications/mhv/secure-messaging/containers/App.jsx
+++ src/applications/mhv/secure-messaging/containers/App.jsx
@@ -42,16 +42,10 @@ const App = () => {
     () => {
       if (user.login.currentlyLoggedIn) {
         dispatch(userFacilities);
+        dispatch(getAllTriageTeamRecipients());
       }
     },
     [userFacilities, user.login.currentlyLoggedIn, dispatch],
-  );
-
-  useEffect(
-    () => {
-      dispatch(getAllTriageTeamRecipients());
-    },
-    [dispatch],
   );
 
   const datadogRumConfig = {
