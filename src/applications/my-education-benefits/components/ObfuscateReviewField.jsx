f676030dfff213f3b59e7189dd59876b8deb67fd
Brad Bergeron <bergeron.bradley@gmail.com>
2024-01-19 13:46:03 -0500

    Updates Prop validation in obfuscate review field component

--- src/applications/my-education-benefits/components/ObfuscateReviewField.jsx
+++ src/applications/my-education-benefits/components/ObfuscateReviewField.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { obfuscate } from '../helpers';
 
-export default function ObfuscateReviewField({ children, uiSchema }) {
+function ObfuscateReviewField({ children, uiSchema }) {
   return (
     <div className="review-row">
       <dt>{uiSchema['ui:title']}</dt>
@@ -9,3 +10,11 @@ export default function ObfuscateReviewField({ children, uiSchema }) {
     </div>
   );
 }
+
+ObfuscateReviewField.propTypes = {
+  children: PropTypes.node.isRequired,
+  uiSchema: PropTypes.shape({
+    'ui:title': PropTypes.string.isRequired,
+  }).isRequired,
+};
+export default ObfuscateReviewField;
