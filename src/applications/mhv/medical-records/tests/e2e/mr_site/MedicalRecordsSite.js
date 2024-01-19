40ca9ecee7d7aa75ee599b11c1890dc1a6eda8eb
Anal Gomes <103195615+agomes82@users.noreply.github.com>
2024-01-19 11:02:27 -0500

    Add TXT Download for consolidated report (all domains/Blue Button)

--- src/applications/mhv/medical-records/tests/e2e/mr_site/MedicalRecordsSite.js
+++ src/applications/mhv/medical-records/tests/e2e/mr_site/MedicalRecordsSite.js
@@ -69,6 +69,27 @@ class MedicalRecordsSite {
     }
   };
 
+  verifyDownloadedTxtFile = (_prefixString, _clickMoment, _searchText) => {
+    if (Cypress.browser.isHeadless) {
+      cy.log('browser is headless');
+      const downloadsFolder = Cypress.config('downloadsFolder');
+      const txtPath1 = `${downloadsFolder}/${_prefixString}-${_clickMoment
+        .add(1, 'seconds')
+        .format('M-D-YYYY_hhmmssa')}.txt`;
+      const txtPath2 = `${downloadsFolder}/${_prefixString}-${_clickMoment
+        .add(1, 'seconds')
+        .format('M-D-YYYY_hhmmssa')}.txt`;
+      const txtPath3 = `${downloadsFolder}/${_prefixString}-${_clickMoment
+        .add(1, 'seconds')
+        .format('M-D-YYYY_hhmmssa')}.txt`;
+      this.internalReadFileMaybe(txtPath1, _searchText);
+      this.internalReadFileMaybe(txtPath2, _searchText);
+      this.internalReadFileMaybe(txtPath3, _searchText);
+    } else {
+      cy.log('browser is not headless');
+    }
+  };
+
   internalReadFileMaybe = (fileName, searchText) => {
     cy.task('log', `attempting to find file = ${fileName}`);
     cy.task('readFileMaybe', fileName).then(textOrNull => {
