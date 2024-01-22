f1c113f4d97f470b28581ec1ab2444190aefbd14
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2024-01-22 09:20:21 -0500

    Try logging out full file path

--- src/platform/testing/unit/mocha-setup.js
+++ src/platform/testing/unit/mocha-setup.js
@@ -148,8 +148,8 @@ function setupJSDom() {
 
 setupJSDom();
 const checkAllowList = testContext => {
+  console.log(testContext.currentTest.file);
   const file = testContext.currentTest.file.indexOf('src');
-  console.log(file);
   if (DISALLOWED_SPECS.indexOf(file) > -1) {
     /* eslint-disable-next-line no-console */
     console.log('Test skipped due to flakiness: ', file);
