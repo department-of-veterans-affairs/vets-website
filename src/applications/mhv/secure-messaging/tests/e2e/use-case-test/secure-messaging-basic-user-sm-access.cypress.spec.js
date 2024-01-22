5855dd7f48b7d71c9cd6a6da4a66733c5327761b
fazilqa <fazil.mammadov@bylight.com>
2024-01-22 11:41:06 -0800

    add: non-va user SM access testtest

--- src/applications/mhv/secure-messaging/tests/e2e/use-case-test/secure-messaging-basic-user-sm-access.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/use-case-test/secure-messaging-basic-user-sm-access.cypress.spec.js
@@ -1,18 +1,52 @@
 import SecureMessagingSite from '../sm_site/SecureMessagingSite';
-import mockBasicUser from '../fixtures/userResponse/basic-user.json';
+import mockUser from '../fixtures/userResponse/user.json';
 import mockGeneralMessages from '../fixtures/generalResponses/generalMessages.json';
 import mockFeatureToggles from '../fixtures/generalResponses/featureToggles.json';
 import mockGeneralFolder from '../fixtures/generalResponses/generalFolder.json';
 
 describe('Secure Messaging Basic User', () => {
-  it('verify user has not access to secure-messaging', () => {
+  it('verify basic user has not access to secure-messaging', () => {
+    const basicUser = JSON.parse(JSON.stringify(mockUser));
+    basicUser.data.attributes.services = basicUser.data.attributes.services.filter(
+      service => service !== 'messaging',
+    );
     const site = new SecureMessagingSite();
-    site.login(true, mockBasicUser);
+    site.login(true, basicUser);
 
     cy.intercept('GET', '/v0/feature_toggles?*', mockFeatureToggles).as(
       'featureToggles',
     );
-    cy.intercept('GET', '/v0/user', mockBasicUser).as('user');
+    cy.intercept('GET', '/v0/user', basicUser).as('user');
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/0*',
+      mockGeneralFolder,
+    ).as('generalFolder');
+    cy.intercept(
+      'GET',
+      '/my_health/v1/messaging/folders/0/messages*',
+      mockGeneralMessages,
+    ).as('generalMessages');
+
+    cy.visit('my-health/secure-messages/', {
+      onBeforeLoad: win => {
+        cy.stub(win, 'print');
+      },
+    });
+
+    cy.location('pathname').should('eq', '/health-care/secure-messaging');
+  });
+
+  it('verify non-va premium user has not access to secure-messaging', () => {
+    const nonVAUser = { ...mockUser };
+    nonVAUser.data.attributes.vaProfile.vaPatient = false;
+    const site = new SecureMessagingSite();
+    site.login(true, nonVAUser);
+
+    cy.intercept('GET', '/v0/feature_toggles?*', mockFeatureToggles).as(
+      'featureToggles',
+    );
+    cy.intercept('GET', '/v0/user', nonVAUser).as('user');
     cy.intercept(
       'GET',
       '/my_health/v1/messaging/folders/0*',
