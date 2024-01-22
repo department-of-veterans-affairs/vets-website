4885c2e329c08cba432f9949c6c7babacd3f3911
Adam Antonioli <adam.antonioli@va.gov>
2024-01-22 09:41:47 -0500

    MBMS-54134 fix unit test of applicant information screen of pre need integration form

--- src/applications/pre-need-integration/tests/config/applicantRelationshipToVet.unit.spec.jsx
+++ src/applications/pre-need-integration/tests/config/applicantRelationshipToVet.unit.spec.jsx
@@ -12,8 +12,9 @@ import formConfig from '../../config/form';
 describe('Pre-need applicant relationship to vet', () => {
   const {
     schema,
-    uiSchema,
   } = formConfig.chapters.applicantInformation.pages.applicantRelationshipToVet;
+
+  const uiSchema = formConfig.chapters.applicantInformation.pages.applicantRelationshipToVet.uiSchema();
 
   it('should render', () => {
     const form = mount(
@@ -24,7 +25,7 @@ describe('Pre-need applicant relationship to vet', () => {
       />,
     );
 
-    expect(form.find('input').length).to.equal(4);
+    expect(form.find('input').length).to.equal(8);
     expect(form.find('va-additional-info').length).to.equal(1);
     expect(form.find('va-additional-info').html()).to.include(
       '<va-additional-info trigger="What if the applicant is not a service member or Veteran?"><ul><li>A <strong>spouse</strong> is a person who is or was legally married to a service member or Veteran. A <strong>surviving spouse</strong> is someone who was legally married to the service member or Veteran at the time of their death and includes a surviving spouse who remarried.</li><li>An <strong>unmarried adult child</strong> is an individual who became physically or mentally disabled permanently and incapable of self-support before the age of 21, or before 23 years of age if pursuing a full-time course of instruction at an approved educational institution.</li><li>For <strong>other</strong> applicants such as the parent of a service member, we’ll ask questions about the service member (the sponsor) to determine eligibility for burial in a VA national cemetery.</li></ul></va-additional-info>',
