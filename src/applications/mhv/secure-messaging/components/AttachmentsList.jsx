783508911178f3b6f836be64fa5db1746dea3749
Vic Saleem <victoria.saleem@va.gov>
2024-01-22 04:07:16 -0600

    [MHV-44454] Refactored AssignmentList to use attachment ids for individual
    - this was an axe check finding by the automation team; each message thread contains it's own 'RemoveAttachmentModal' and 'RemoveAttachmentButtons' because they live within the AssignmentList component initially. To fix this issue, each id and data-testid needed their own id identifier.

--- src/applications/mhv/secure-messaging/components/AttachmentsList.jsx
+++ src/applications/mhv/secure-messaging/components/AttachmentsList.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import recordEvent from 'platform/monitoring/record-event';
 import { focusElement } from '@department-of-veterans-affairs/platform-utilities/ui';
 import { VaAlert } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import { handleRemoveAttachmentButtonId, getSize } from '../util/helpers';
 import RemoveAttachmentModal from './Modals/RemoveAttachmentModal';
 import HowToAttachFiles from './HowToAttachFiles';
 
@@ -23,16 +24,6 @@ const AttachmentsList = props => {
   const [recentlyRemovedFile, setRecentlyRemovedFile] = useState(false);
   const attachFileAlertRef = useRef();
   const [focusedElement, setFocusedElement] = useState(null);
-
-  const getSize = num => {
-    if (num > 999999) {
-      return `${(num / 1000000).toFixed(1)} MB`;
-    }
-    if (num > 999) {
-      return `${Math.floor(num / 1000)} KB`;
-    }
-    return `${num} B`;
-  };
 
   useEffect(
     () => {
@@ -151,115 +142,118 @@ const AttachmentsList = props => {
             </button>
           </VaAlert>
         )}
-
-      <ul className="attachments-list">
-        {!!attachments.length &&
-          attachments.map(file => (
-            <li key={file.name + file.size}>
-              {editingEnabled && (
-                <div className="editable-attachment vads-u-display--flex vads-u-flex-direction--row">
-                  <span
-                    data-dd-privacy="mask"
-                    ref={attachmentReference}
-                    className="vads-u-flex--1"
-                    role="alert"
-                    aria-live="polite"
-                    aria-label={
-                      recentlyRemovedFile
-                        ? null
-                        : `${file.name}, ${getSize(
-                            file.size || file.attachmentSize,
-                          )}, button available: Remove ${file.name}`
-                    }
-                  >
-                    <i
-                      className="fas fa-paperclip"
-                      alt="Attachment icon"
-                      aria-hidden="true"
-                    />
-                    <span>{file.name} </span>(
-                    {getSize(file.size || file.attachmentSize)})
-                  </span>
-                  <button
-                    type="button"
-                    onClick={() => {
-                      setIsModalVisible(true);
-                      setFileToRemove(file);
-                    }}
-                    aria-label={`remove ${file.name}`}
-                    data-testid="remove-attachment-button"
-                    data-dd-action-name="Remove Attachment Button"
-                    className="remove-attachment-button vads-u-flex--auto vads-u-margin-right--1p5 vads-u-padding-y--2"
-                  >
-                    <span className="remove-attachment-icon vads-u-padding-right--3" />
-                    REMOVE
-                  </button>
-                </div>
-              )}
-              {!editingEnabled && (
-                <>
-                  <a
-                    className="attachment"
-                    href={file.link}
-                    target="_blank"
-                    rel="noreferrer"
-                    onClick={() => {
-                      recordEvent({
-                        event: 'cta-button-click',
-                        'button-type': 'link',
-                        'button-click-label': 'Attachment link',
-                      });
-                    }}
-                  >
-                    <i
-                      aria-labelledby="has-attachment"
-                      className="fas fa-paperclip"
-                      aria-hidden="true"
-                      alt="Attachment icon"
-                    />
+      {!!attachments.length &&
+        attachments.map(file => (
+          <span key={file.name + file.size}>
+            <ul className="attachments-list">
+              <li>
+                {editingEnabled && (
+                  <div className="editable-attachment vads-u-display--flex vads-u-flex-direction--row">
                     <span
-                      id="has-attachment"
-                      data-testid="attachment-name"
-                      ref={attachmentReference}
                       data-dd-privacy="mask"
+                      ref={attachmentReference}
+                      className="vads-u-flex--1"
+                      role="alert"
+                      aria-live="polite"
+                      aria-label={
+                        recentlyRemovedFile
+                          ? null
+                          : `${file.name}, ${getSize(
+                              file.size || file.attachmentSize,
+                            )}, button available: Remove ${file.name}`
+                      }
                     >
-                      {file.name}
+                      <i
+                        className="fas fa-paperclip"
+                        alt="Attachment icon"
+                        aria-hidden="true"
+                      />
+                      <span>{file.name} </span>(
+                      {getSize(file.size || file.attachmentSize)})
                     </span>
-                    ({getSize(file.size || file.attachmentSize)})
-                    <span className="sr-only">Has attachment</span>
-                  </a>
-                </>
-              )}
-            </li>
-          ))}
-      </ul>
-      <RemoveAttachmentModal
-        visible={isModalVisible}
-        onClose={() => {
-          setIsModalVisible(false);
-          setIsAttachmentRemoved(false);
-        }}
-        onDelete={() => {
-          setNavigationError();
-          setIsModalVisible(false);
-          removeAttachment(fileToRemove);
-        }}
-        data-testid="remove-attachment-modal"
-      />
-      {isAttachmentRemoved ? (
-        <>
-          <div
-            ref={attachmentReference}
-            role="status"
-            aria-live="polite"
-            className="sr-only"
-            id="attachment-removed-successfully"
-            data-dd-privacy="mask"
-          >
-            {`File ${removedAttachmentName} successfully removed. Attach file, button.`}
-          </div>
-        </>
-      ) : null}
+                    <button
+                      type="button"
+                      onClick={() => {
+                        setIsModalVisible(true);
+                        setFileToRemove(file);
+                      }}
+                      aria-label={`remove ${file.name}`}
+                      data-testid={handleRemoveAttachmentButtonId(file)}
+                      data-dd-action-name="Remove Attachment Button"
+                      className="remove-attachment-button vads-u-flex--auto vads-u-margin-right--1p5 vads-u-padding-y--2"
+                    >
+                      <span className="remove-attachment-icon vads-u-padding-right--3" />
+                      REMOVE
+                    </button>
+                  </div>
+                )}
+                {!editingEnabled && (
+                  <>
+                    <a
+                      data-testid="attachment-link-metadata"
+                      className="attachment"
+                      href={file.link}
+                      target="_blank"
+                      rel="noreferrer"
+                      onClick={() => {
+                        recordEvent({
+                          event: 'cta-button-click',
+                          'button-type': 'link',
+                          'button-click-label': 'Attachment link',
+                        });
+                      }}
+                    >
+                      <i
+                        aria-labelledby={`has-attachment-${file.messageId}`}
+                        className="fas fa-paperclip"
+                        aria-hidden="true"
+                        alt="Attachment icon"
+                      />
+                      <span
+                        id={`has-attachment-${file.messageId}`}
+                        data-testid={`attachment-name-${file.messageId}`}
+                        ref={attachmentReference}
+                        data-dd-privacy="mask"
+                      >
+                        {file.name}
+                      </span>
+                      ({getSize(file.size || file.attachmentSize)})
+                      <span className="sr-only">Has attachment</span>
+                    </a>
+                  </>
+                )}
+              </li>
+            </ul>
+            <RemoveAttachmentModal
+              visible={isModalVisible}
+              onClose={() => {
+                setIsModalVisible(false);
+                setIsAttachmentRemoved(false);
+              }}
+              onDelete={() => {
+                setNavigationError();
+                setIsModalVisible(false);
+                removeAttachment(fileToRemove);
+              }}
+              data-testid="remove-attachment-modal"
+              file={file}
+            />
+            {isAttachmentRemoved ? (
+              <>
+                <div
+                  ref={attachmentReference}
+                  role="status"
+                  aria-live="polite"
+                  className="sr-only"
+                  id="attachment-removed-successfully"
+                  data-dd-privacy="mask"
+                >
+                  {`File ${removedAttachmentName} successfully removed. Attach file, button.`}
+                </div>
+              </>
+            ) : null}
+          </span>
+        ))}
     </div>
   );
 };
