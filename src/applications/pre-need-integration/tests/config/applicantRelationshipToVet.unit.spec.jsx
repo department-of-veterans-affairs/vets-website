edd5a9c2ae04fa7b361d118d81fc7356a55c6885
Adam Antonioli <adam.antonioli@va.gov>
2024-01-22 11:40:42 -0500

    MBMS-54134 fix unit test of applicant information screen of pre need integration form

--- src/applications/pre-need-integration/tests/config/applicantRelationshipToVet.unit.spec.jsx
+++ src/applications/pre-need-integration/tests/config/applicantRelationshipToVet.unit.spec.jsx
@@ -14,7 +14,9 @@ describe('Pre-need applicant relationship to vet', () => {
     schema,
   } = formConfig.chapters.applicantInformation.pages.applicantRelationshipToVet;
 
-  const uiSchema = formConfig.chapters.applicantInformation.pages.applicantRelationshipToVet.uiSchema();
+  const {
+    uiSchema,
+  } = formConfig.chapters.applicantInformation.pages.applicantRelationshipToVet.uiSchema();
 
   it('should render', () => {
     const form = mount(
