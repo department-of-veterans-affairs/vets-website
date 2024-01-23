40342f0fa4b2f45575e397c8e9755b3d56545401
Carlos <carlos.felix@agile6.com>
2024-01-23 11:04:41 -0500

    Enabled redirect app tests

--- src/applications/health-care-supply-reordering-redirect/tests/e2e/redirect.cypress.spec.js
+++ src/applications/health-care-supply-reordering-redirect/tests/e2e/redirect.cypress.spec.js
@@ -3,7 +3,7 @@ import { appName, rootUrl } from '../../manifest.json';
 describe(`${appName} -- Redirect`, () => {
   // eslint-disable-next-line @department-of-veterans-affairs/axe-check-required
   // Need to skip the test until the content-build registry is updated to run the app.
-  it.skip('Redirect from the root URL', () => {
+  it('Redirect from the root URL', () => {
     cy.visit(rootUrl);
     cy.url().should(
       'contain',
@@ -17,7 +17,7 @@ describe(`${appName} -- Redirect`, () => {
 
   // eslint-disable-next-line @department-of-veterans-affairs/axe-check-required
   // Need to skip the test until the content-build registry is updated to run the app.
-  it.skip('Redirect from a child URL', () => {
+  it('Redirect from a child URL', () => {
     cy.visit(`${rootUrl}/introduction`);
     cy.url().should(
       'contain',
