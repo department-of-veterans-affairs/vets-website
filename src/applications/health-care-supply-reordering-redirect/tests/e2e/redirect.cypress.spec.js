8000fb8cd2e7b9392e94ecf72c18a8ad631b3f16
Carlos <carlos.felix@agile6.com>
2024-01-23 11:09:11 -0500

    Removed comments

--- src/applications/health-care-supply-reordering-redirect/tests/e2e/redirect.cypress.spec.js
+++ src/applications/health-care-supply-reordering-redirect/tests/e2e/redirect.cypress.spec.js
@@ -2,7 +2,6 @@ import { appName, rootUrl } from '../../manifest.json';
 
 describe(`${appName} -- Redirect`, () => {
   // eslint-disable-next-line @department-of-veterans-affairs/axe-check-required
-  // Need to skip the test until the content-build registry is updated to run the app.
   it('Redirect from the root URL', () => {
     cy.visit(rootUrl);
     cy.url().should(
@@ -16,7 +15,6 @@ describe(`${appName} -- Redirect`, () => {
   });
 
   // eslint-disable-next-line @department-of-veterans-affairs/axe-check-required
-  // Need to skip the test until the content-build registry is updated to run the app.
   it('Redirect from a child URL', () => {
     cy.visit(`${rootUrl}/introduction`);
     cy.url().should(
