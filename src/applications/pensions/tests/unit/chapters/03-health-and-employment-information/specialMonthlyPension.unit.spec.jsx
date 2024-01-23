e73d64d5d5b82c27877e5c2e57e47ae33251a106
Zack Youngren <zack@youngren.io>
2024-01-23 10:40:23 -0500

    fix test

--- src/applications/pensions/tests/unit/chapters/03-health-and-employment-information/specialMonthlyPension.unit.spec.jsx
+++ src/applications/pensions/tests/unit/chapters/03-health-and-employment-information/specialMonthlyPension.unit.spec.jsx
@@ -1,6 +1,6 @@
 import {
-  testNumberOfErrorsOnSubmitForWebComponents,
-  testNumberOfWebComponentFields,
+  testNumberOfErrorsOnSubmit,
+  testNumberOfFields,
 } from '../pageTests.spec';
 import formConfig from '../../../../config/form';
 import specialMonthlyPension from '../../../../config/chapters/03-health-and-employment-information/specialMonthlyPension';
@@ -9,11 +9,8 @@ const { schema, uiSchema } = specialMonthlyPension;
 
 describe('pension special monthly pension page', () => {
   const pageTitle = 'special monthly pension';
-  // Using an old component since the new web component can't take a react
-  // object as a title and that's what we need to do in order to get the helper
-  // text to be properly read by a screen reader
-  const expectedNumberOfFields = 0;
-  testNumberOfWebComponentFields(
+  const expectedNumberOfFields = 2;
+  testNumberOfFields(
     formConfig,
     schema,
     uiSchema,
@@ -21,8 +18,8 @@ describe('pension special monthly pension page', () => {
     pageTitle,
   );
 
-  const expectedNumberOfErrors = 0;
-  testNumberOfErrorsOnSubmitForWebComponents(
+  const expectedNumberOfErrors = 1;
+  testNumberOfErrorsOnSubmit(
     formConfig,
     schema,
     uiSchema,
