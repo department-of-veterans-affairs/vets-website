09d4190203edc223d26d4b50df3dcaf63751e6a8
Tze-chiu Lei <tclei2009@gmail.com>
2024-01-22 19:17:48 -0700

    Build 20-10207 Personal-info-page

--- src/applications/simple-forms/20-10207/helpers.js
+++ src/applications/simple-forms/20-10207/helpers.js
@@ -1,3 +1,19 @@
+import { PREPARER_TYPES } from './config/constants';
+
 export function getMockData(mockData, isLocalhost) {
   return !!mockData && isLocalhost() && !window.Cypress ? mockData : undefined;
+}
+
+export function getPersonalInformationChapterTitle(formData) {
+  const { preparerType } = formData;
+  const { THIRD_PARTY_NON_VETERAN, THIRD_PARTY_VETERAN } = PREPARER_TYPES;
+
+  switch (preparerType) {
+    case THIRD_PARTY_VETERAN:
+      return 'Veteran’s personal information';
+    case THIRD_PARTY_NON_VETERAN:
+      return 'Claimant’s personal information';
+    default:
+      return 'Your personal information';
+  }
 }
---
+++ src/applications/simple-forms/20-10207/pages/personalInfoThirdPartyNonVeteran.js
@@ -0,0 +1,35 @@
+import { cloneDeep } from 'lodash';
+
+import { fullNameNoSuffixUI } from 'platform/forms-system/src/js/web-component-patterns/fullNamePattern.js';
+
+import {
+  dateOfBirthSchema,
+  dateOfBirthUI,
+  titleUI,
+} from 'platform/forms-system/src/js/web-component-patterns';
+
+import { pdfFullNameNoSuffixSchema } from '../../shared/definitions/pdfFullNameNoSuffix';
+
+const fullNameUI = cloneDeep(fullNameNoSuffixUI());
+
+fullNameUI.middle['ui:title'] = 'Middle initial';
+
+/** @type {PageSchema} */
+export default {
+  uiSchema: {
+    ...titleUI('Claimant’s name and date of birth'),
+    fullName: fullNameUI,
+    dateOfBirth: dateOfBirthUI(),
+    updateSchemaAndData: () => {},
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      fullName: pdfFullNameNoSuffixSchema({
+        pdfMaxLengths: { first: 12, middle: 1, last: 18 },
+      }),
+      dateOfBirth: dateOfBirthSchema,
+    },
+    required: ['fullName', 'dateOfBirth'],
+  },
+};
