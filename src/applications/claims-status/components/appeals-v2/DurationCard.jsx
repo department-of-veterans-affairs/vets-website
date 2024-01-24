d0fab04cac35c2e3b2c22c9df59653414444a53f
Jami Gibbs <jami.gibbs@adhocteam.us>
2024-01-24 11:40:24 -0600

    add aria-hidden to icon element

--- src/applications/claims-status/components/appeals-v2/DurationCard.jsx
+++ src/applications/claims-status/components/appeals-v2/DurationCard.jsx
@@ -12,7 +12,10 @@ const DurationCard = ({ durationText, cardDescription, isDisabled = true }) => {
   return (
     <va-card background class="duration-card">
       <div className="number vads-u-font-family--serif 40px vads-u-font-size--h2 vads-u-font-weight--bold vads-u-line-height--1 vads-u-padding-bottom--2 vads-u-border-bottom--1px vads-u-border-color--gray-light">
-        <i className="fas fa-clock vads-u-margin-right--1p5" alt="clock icon" />
+        <i
+          className="fas fa-clock vads-u-margin-right--1p5"
+          aria-hidden="true"
+        />
         {durationText}
       </div>
       <div className="description vads-u-font-size--base vads-u-font-weight--normal vads-u-padding-top--0p5">
