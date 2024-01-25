37c45e718a64ea508bdfca4f3d4f53900423e159
Rob Garrison <wowmotty@gmail.com>
2024-01-25 10:34:00 -0600

    Review & submit chapters | highlight page errors (#27620)
    * Fix review & submit page error highlighting
    
    * Add unit tests
    
    * Fix unit tests
    
    * Fix unit tests
    
    * Remove unnecessary property

--- src/platform/forms-system/src/js/review/ReviewCollapsibleChapter.jsx
+++ src/platform/forms-system/src/js/review/ReviewCollapsibleChapter.jsx
@@ -164,8 +164,12 @@ class ReviewCollapsibleChapter extends React.Component {
       fullPageKey = page.pageKey;
     }
 
+    const hasError = props.form.formErrors?.errors?.some(
+      err => fullPageKey === err.pageKey,
+    );
+
     const classes = classNames('form-review-panel-page', {
-      'schemaform-review-page-error': !viewedPages.has(fullPageKey),
+      'schemaform-review-page-error': hasError || !viewedPages.has(fullPageKey),
       // Remove bottom margin when the div content is empty
       'vads-u-margin-bottom--0': !pageSchema && arrayFields.length === 0,
     });
@@ -327,6 +331,7 @@ class ReviewCollapsibleChapter extends React.Component {
         />
       );
     }
+
     return (
       <page.CustomPageReview
         key={`${page.pageKey}Review${page.index ?? ''}`}
