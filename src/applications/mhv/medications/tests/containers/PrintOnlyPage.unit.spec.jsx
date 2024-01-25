0ca4b1b86eb4bfb3a85c8ade45f639deebb23d4f
agravell047 <146013972+agravell047@users.noreply.github.com>
2024-01-25 09:19:53 -0500

    Mhv-51129-Update-Print-View-Tests (#27430)
    * updated print content
    
    * text change
    
    * added tests

--- src/applications/mhv/medications/tests/containers/PrintOnlyPage.unit.spec.jsx
+++ src/applications/mhv/medications/tests/containers/PrintOnlyPage.unit.spec.jsx
@@ -2,16 +2,21 @@ import { expect } from 'chai';
 import React from 'react';
 import { renderWithStoreAndRouter } from '@department-of-veterans-affairs/platform-testing/react-testing-library-helpers';
 import reducers from '../../reducers';
-import PrescriptionsPrintOnly from '../../containers/PrescriptionsPrintOnly';
+import PrintOnlyPage from '../../containers/PrintOnlyPage';
 
 describe('Medications List Print Page', () => {
   const setup = (params = {}) => {
-    return renderWithStoreAndRouter(<PrescriptionsPrintOnly />, {
-      initialState: {},
-      reducers,
-      path: '/1',
-      ...params,
-    });
+    return renderWithStoreAndRouter(
+      <PrintOnlyPage>
+        <span>This is the page content.</span>
+      </PrintOnlyPage>,
+      {
+        initialState: {},
+        reducers,
+        path: '/1',
+        ...params,
+      },
+    );
   };
 
   it('renders without errors', () => {
@@ -19,20 +24,23 @@ describe('Medications List Print Page', () => {
     expect(screen);
   });
 
+  it('renders content as body of table', () => {
+    setup();
+    const text = document.querySelector('tbody').textContent;
+    expect(text).to.contain('This is the page content.');
+  });
+
   it('Medications | Veterans Affairs', () => {
     const screen = setup();
     const rxName = screen.findByText('Medications | Veterans Affairs');
     expect(rxName).to.exist;
   });
+
   it('display user name and dob', () => {
     const screen = setup();
     const name = 'Doe, John R., Jr.';
     const dob = 'March 15, 1982';
     expect(screen.findByText(name)).to.exist;
     expect(screen.findByText(`Date of birth: ${dob}`)).to.exist;
-  });
-  it('does not render for paths other than medication list and details', () => {
-    const screen = setup({ path: '/foo' });
-    expect(screen.queryByTestId('name-date-of-birth')).to.not.exist;
   });
 });
