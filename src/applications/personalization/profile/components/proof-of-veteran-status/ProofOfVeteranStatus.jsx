ff97738519cf7aac29dd426d88739be853b8c7c4
Liz Townsend <liz.townsend@adhocteam.us>
2024-01-26 08:32:34 -0600

    add eligibility rules, add necessary data to mocks

--- src/applications/personalization/profile/components/proof-of-veteran-status/ProofOfVeteranStatus.jsx
+++ src/applications/personalization/profile/components/proof-of-veteran-status/ProofOfVeteranStatus.jsx
@@ -7,6 +7,7 @@ import { generatePdf } from '~/platform/pdf';
 import { formatFullName } from '../../../common/helpers';
 import recordEvent from '~/platform/monitoring/record-event';
 import { PROFILE_PATHS } from '../../constants';
+import { DISCHARGE_CODE_MAP } from './constants';
 
 const ProofOfVeteranStatus = ({
   dob,
@@ -28,6 +29,13 @@ const ProofOfVeteranStatus = ({
   const isMobile =
     (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) ||
     /android/i.test(userAgent);
+
+  const eligibilityMap = militaryInformation.serviceHistory.serviceHistory.map(
+    service =>
+      service.characterOfDischargeCode
+        ? DISCHARGE_CODE_MAP[service.characterOfDischargeCode].indicator
+        : 'N',
+  );
 
   const pdfData = {
     title: `Veteran status card for ${formatFullName({
@@ -60,49 +68,54 @@ const ProofOfVeteranStatus = ({
   };
 
   return (
-    <div id="proof-of-veteran-status">
-      <h2 className="vads-u-font-size--h3 vads-u-margin-top--4 vads-u-margin-bottom--1p5">
-        Proof of Veteran status
-      </h2>
-      <p>
-        You can use your Veteran status card to get discounts offered to
-        Veterans at many restaurants, hotels, stores, and other businesses.
-      </p>
-      <p>
-        <strong>Note: </strong>
-        This card doesn’t entitle you to any VA benefits.
-      </p>
-      <div className="vads-u-margin--1 vads-u-margin-y--2">
-        <img
-          width="400rem"
-          src="/img/content/posts/proof-of-veteran-status-card-example.png"
-          alt="sample proof of veteran status card featuring name, date of birth, disability rating and period of service"
-        />
-      </div>
-      <div className="vads-u-font-size--md">
-        <va-link
-          download
-          filetype="PDF"
-          // exception to eslint: the url is a dynamically generated blob url
-          // eslint-disable-next-line no-script-url
-          href="javascript:void(0)"
-          text="Download Veteran status card"
-          onClick={createPdf}
-        />
-      </div>
-      <div className="vads-u-margin-y--4">
-        <MobileAppCallout
-          headingText="Get proof of Veteran Status on your mobile device"
-          bodyText={
-            <>
-              You can use our mobile app to get proof of Veteran status. To get
-              started, download the <strong> VA: Health and Benefits </strong>{' '}
-              mobile app.
-            </>
-          }
-        />
-      </div>
-    </div>
+    <>
+      {militaryInformation.serviceHistory.serviceHistory.length > 0 &&
+      eligibilityMap.includes('Y') ? (
+        <div id="proof-of-veteran-status">
+          <h2 className="vads-u-font-size--h3 vads-u-margin-top--4 vads-u-margin-bottom--1p5">
+            Proof of Veteran status
+          </h2>
+          <p>
+            You can use your Veteran status card to get discounts offered to
+            Veterans at many restaurants, hotels, stores, and other businesses.
+          </p>
+          <p>
+            <strong>Note: </strong>
+            This card doesn’t entitle you to any VA benefits.
+          </p>
+          <div className="vads-u-margin--1 vads-u-margin-y--2">
+            <img
+              width="400rem"
+              src="/img/content/posts/proof-of-veteran-status-card-example.png"
+              alt="sample proof of veteran status card featuring name, date of birth, disability rating and period of service"
+            />
+          </div>
+          <div className="vads-u-font-size--md">
+            <va-link
+              download
+              filetype="PDF"
+              // exception to eslint: the url is a dynamically generated blob url
+              // eslint-disable-next-line no-script-url
+              href="javascript:void(0)"
+              text="Download Veteran status card"
+              onClick={createPdf}
+            />
+          </div>
+          <div className="vads-u-margin-y--4">
+            <MobileAppCallout
+              headingText="Get proof of Veteran Status on your mobile device"
+              bodyText={
+                <>
+                  You can use our mobile app to get proof of Veteran status. To
+                  get started, download the{' '}
+                  <strong> VA: Health and Benefits </strong> mobile app.
+                </>
+              }
+            />
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
