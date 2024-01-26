9cd30014fcd603bbcc4707bf5a1228d29613959e
Tze-chiu Lei <tclei2009@gmail.com>
2024-01-26 12:53:42 -0700

    Refactor 20-10207 Living-situation custom-validation

--- src/applications/simple-forms/20-10207/helpers.js
+++ src/applications/simple-forms/20-10207/helpers.js
@@ -52,12 +52,24 @@ export function validateLivingSituation(errors, fields) {
   const selectedSituations = Object.keys(fields.livingSituation).filter(
     key => fields.livingSituation[key],
   );
+  let preparerString = '';
+  switch (fields.preparerType) {
+    case PREPARER_TYPES.THIRD_PARTY_VETERAN:
+      preparerString = 'the Veteran';
+      break;
+    case PREPARER_TYPES.THIRD_PARTY_NON_VETERAN:
+      preparerString = 'the Claimant';
+      break;
+    default:
+      preparerString = 'you';
+      break;
+  }
 
   // We're just checking to make sure no other option's selected along with NONE here
   // schema's required prop already handles required error-message
   if (selectedSituations.length > 1 && selectedSituations.includes('NONE')) {
     errors.livingSituation.addError(
-      'If none of these situations apply to you, unselect the other options you selected.',
+      `If none of these situations apply to ${preparerString}, unselect the other options you selected.`,
     );
   }
 }
