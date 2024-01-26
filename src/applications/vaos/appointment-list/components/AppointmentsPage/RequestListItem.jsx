07e368bece0bfe36f35955b4da11fd5668b29fa5
Ryan Shaw <ryan.shaw@adhocteam.us>
2024-01-25 17:02:41 -0800

    Fixes linting error
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/appointment-list/components/AppointmentsPage/RequestListItem.jsx
+++ src/applications/vaos/appointment-list/components/AppointmentsPage/RequestListItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import moment from 'moment';
-import { focusElement } from 'platform/utilities/ui';
+import { focusElement } from '@department-of-veterans-affairs/platform-utilities/ui';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { sentenceCase } from '../../../utils/formatters';
