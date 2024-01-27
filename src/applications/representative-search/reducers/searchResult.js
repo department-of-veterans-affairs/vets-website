aaa858134daf6019300ef602b2c5e967414f9af9
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-01-26 19:14:52 -0500

    Removed error updates from old reducers

--- src/applications/representative-search/reducers/searchResult.js
+++ src/applications/representative-search/reducers/searchResult.js
@@ -1,31 +1,28 @@
 import {
   CLEAR_SEARCH_RESULTS,
   SEARCH_COMPLETE,
-  SEARCH_FAILED,
+  // SEARCH_FAILED,
   FETCH_REPRESENTATIVES,
   REPORT_STARTED,
   REPORT_COMPLETE,
-  REPORT_FAILED,
+  // REPORT_FAILED,
   REPORT_ITEMS_UPDATED,
 } from '../utils/actionTypes';
 
 const INITIAL_STATE = {
   searchResults: [],
   reportedResults: [],
-  reportError: false,
   pagination: {},
 };
 
 import { appendReportsFromLocalStorage } from '../utils/helpers';
 
 export const SearchResultReducer = (state = INITIAL_STATE, action) => {
-  // const searchResultsWithReports = ;
   switch (action.type) {
     case FETCH_REPRESENTATIVES:
     case SEARCH_COMPLETE:
       return {
         ...state,
-        error: null,
         searchResults: appendReportsFromLocalStorage(action.payload.data),
         pagination: action.payload.meta.pagination,
         resultTime: action.payload.meta.resultTime,
@@ -34,40 +31,28 @@ export const SearchResultReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         ...action.payload,
-        error: false,
         reportSubmissionInProgress: true,
       };
     case REPORT_COMPLETE:
       return {
         ...state,
-        error: null,
         reportSubmissionInProgress: false,
         searchResults: appendReportsFromLocalStorage([...state.searchResults]),
       };
     case REPORT_ITEMS_UPDATED:
       return {
         ...state,
-        error: null,
         reportSubmissionInProgress: false,
         reportedResults: action.payload,
       };
-    case SEARCH_FAILED:
-      if (action.error) {
-        return {
-          ...INITIAL_STATE,
-          error: action.error,
-        };
-      }
-      return INITIAL_STATE;
-    case REPORT_FAILED:
-      if (action.error) {
-        return {
-          ...state,
-          reportError: true,
-        };
-      }
-      return state;
-
+    // case SEARCH_FAILED:
+    //   if (action.error) {
+    //     return {
+    //       ...INITIAL_STATE,
+    //       isErrorFetchRepresentatives: action.error,
+    //     };
+    //   }
+    //   return INITIAL_STATE;
     case CLEAR_SEARCH_RESULTS:
       return INITIAL_STATE;
     default:
