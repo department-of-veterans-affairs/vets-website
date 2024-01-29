bcc6c63e317ab4973054e29a216a125020b534aa
Liz Townsend <liz.townsend@adhocteam.us>
2024-01-29 11:20:28 -0600

    update location of sample card image

--- src/applications/personalization/profile/components/proof-of-veteran-status/ProofOfVeteranStatus.jsx
+++ src/applications/personalization/profile/components/proof-of-veteran-status/ProofOfVeteranStatus.jsx
@@ -94,7 +94,7 @@ const ProofOfVeteranStatus = ({
           <div className="vads-u-margin--1 vads-u-margin-y--2">
             <img
               width="400rem"
-              src="/img/content/posts/proof-of-veteran-status-card-example.png"
+              src="/img/proof-of-veteran-status-card-example.png"
               alt="sample proof of veteran status card featuring name, date of birth, disability rating and period of service"
             />
           </div>
