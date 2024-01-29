49d4c01ad894efa40a0c85f03872749777ea51a3
Peri-Ann McLaren <141954992+pmclaren19@users.noreply.github.com>
2024-01-29 15:31:06 -0700

    [CST] - Revamp additional evidence section on files tab (#27673)
    * Rename AdditionalEvidence file and test file to AdditionalEvidenceOld
    
    * created files called FilesOptional and FilesOptionaOld and created tests
    
    * updates
    
    * update FilesOptional for css changes
    
    * add files needed to additionalevidence
    
    * add tests
    
    * update tests for AdditionalEvidencePageOld and AdditionalEvidencePage
    
    * update filesoptional html
    
    * update imports
    
    * update test files
    
    * Update tests
    
    * update tests
    
    * update test to have a stub

--- src/applications/claims-status/components/FilesOptional.jsx
+++ src/applications/claims-status/components/FilesOptional.jsx
@@ -2,39 +2,38 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router';
 import { getTrackedItemId, truncateDescription } from '../utils/helpers';
-import DueDate from './DueDate';
 
-function FilesNeeded({ id, item }) {
+function FilesOptional({ id, item }) {
   return (
     <va-alert
-      class="primary-alert vads-u-margin-bottom--2"
-      status="warning"
+      class="optional-alert vads-u-margin-bottom--2"
+      status="default"
       uswds
     >
       <h4 slot="headline" className="alert-title">
         {item.displayName}
       </h4>
-      <DueDate date={item.suspenseDate} />
       <p className="alert-description">
-        {truncateDescription(item.description, 200)}
+        {truncateDescription(item.description)}
       </p>
-      <div className="link-action-container">
+      <div className="call-to-action vads-u-padding-top--2">
+        You don't have to do anything, but if you have this information you
+        can&nbsp;
         <Link
-          aria-label={`View details for ${item.displayName}`}
-          title={`View details for ${item.displayName}`}
-          className="vads-c-action-link--blue"
+          aria-label={`Add information for ${item.displayName}`}
+          className="add-your-claims-link"
           to={`your-claims/${id}/document-request/${getTrackedItemId(item)}`}
         >
-          View details
+          add it here.
         </Link>
       </div>
     </va-alert>
   );
 }
 
-FilesNeeded.propTypes = {
+FilesOptional.propTypes = {
   id: PropTypes.string.isRequired,
   item: PropTypes.object.isRequired,
 };
 
-export default FilesNeeded;
+export default FilesOptional;
