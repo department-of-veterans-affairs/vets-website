0624d7048741bd482506e0caff02ed64c3b52bc1
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-01-29 09:52:18 -0800

    Find a Rep 2.0: API, redux work for reporting inaccurate data (#27653)
    * Added reportResult post method to RepresentativeFinderApi class
    
    * Created submitRepresentativeReport action
    
    * Passed relevant props to SearchResult component
    
    * SearchResult component is now controlled + assembles body of post request for submitting reports.
    
    Also reverted to VaCheckboxGroup because va-checkbox-group events weren't firing properly.
    
    * Added new actionTypes for report submission
    
    * Added new conditions to results reducer (report submission starting, completing, failing)
    
    * Removed camelcase disable. Added and sorted prop types.
    
    * Repaired unit tests. Added unit test for failed report request.
    
    * Repaired unit test by updating test's initial state to reflect new reducer structure.
    
    * Added proptype
    
    * Linter warning - sorted proptypes alphabetically (onSubmit/callback types go last)
    
    * Sorted prop type alphabetically (linter warning)
    
    * Callback prop types msut be listed after all other proptypes
    
    * Removed incorrectly placed submitRepresentativeReport proptype
    
    * BaseURL now depends on environment + config settings. Refactored getApi to generate url/settings for both GET and POST methods.
    
    * Stringified request body
    
    * Removed duplicate url path from requestUrl constructor. Removed trailing slash from baseUrl constructor.
    
    * Repaired broken unit tests following api refactor.
    
    * Redux report action for updating redux state from local storage (on pageload)
    
    * Updated submitRepresentativeReport action + moved file. Checks for existing reports + passes new report to post request + updates local storage.
    
    * Moved logic assembling the POST request body to the api method.
    
    * Added address concatenating logic (for passing value to the backend). Added conditional rendering logic for the report modal. Removed api logic assembly POST request.
    
    * Updated searchResult query for new actions. Wrote helper method for updating redux state to be synchronized with what's present in local storage.
    
    * Added propTypes to SearchResult component
    
    * Created generic "clearError" action which passes an errorType to the reducers (rather than having multiple actions for multiple errors)
    
    * Replaced clearGeocodeError with generic clearError
    
    * Added ErrorTypes enum
    
    * Replace clearGeocodeError with clearError. Renamed searchError as isErrorFetchRepresentatives.
    
    * Created dedicated errors reducer
    
    * Removed error updates from old reducers
    
    * Temporarily removed unit tests following redux refactor
    
    * Repaired unit tests. Temporarily removed some unit tests.
    
    * Added proptypes
    
    * Added export to INITIAL_STATE for importing to unit tests
    
    * Set 'GET' as the default api method directly in params.

--- src/applications/representative-search/config.js
+++ src/applications/representative-search/config.js
@@ -2,27 +2,11 @@ import environment from '@department-of-veterans-affairs/platform-utilities/envi
 import compact from 'lodash/compact';
 import { RepresentativeType } from './constants';
 import manifest from './manifest.json';
-// import { facilityLocatorLatLongOnly } from './utils/featureFlagSelectors';
 
 /* eslint-disable camelcase */
 
-const apiSettings = {
-  // credentials: 'include',
-  mode: 'cors',
-  headers: {
-    'X-Key-Inflection': 'camel',
-    'Sec-Fetch-Mode': 'cors',
-
-    // Pull app name directly from manifest since this config is defined
-    // before startApp, and using window.appName here would result in
-    // undefined for all requests that use this config.
-    'Source-App-Name': manifest.entryName,
-  },
-};
-
 export const sortOptions = {
   distance_asc: 'Distance (closest to farthest)',
-  // distance_desc: 'Distance (farthest to closest)',
   first_name_asc: 'First Name (A - Z)',
   first_name_desc: 'First Name (Z - A)',
   last_name_asc: 'Last Name (A - Z)',
@@ -36,26 +20,39 @@ export const useStagingDataLocally = true;
 
 export const claimsAgentIsEnabled = false;
 
-export const getAPI = repType => {
-  return {
-    baseUrl:
-      useStagingDataLocally && environment.BASE_URL === 'http://localhost:3001'
-        ? `https://staging-api.va.gov/services/veteran/v0/${
-            repType === 'VSO' ? 'vso' : 'other'
-          }_accredited_representatives`
-        : `${environment.API_URL}/services/veteran/v0/${
-            repType === 'VSO' ? 'vso' : 'other'
-          }_accredited_representatives`,
-    url:
-      useStagingDataLocally && environment.BASE_URL === 'http://localhost:3001'
-        ? `https://staging-api.va.gov/services/veteran/v0/${
-            repType === 'VSO' ? 'vso' : 'other'
-          }_accredited_representatives`
-        : `${environment.API_URL}/services/veteran/v0/${
-            repType === 'VSO' ? 'vso' : 'other'
-          }_accredited_representatives`,
-    settings: apiSettings,
+const baseUrl =
+  useStagingDataLocally && environment.BASE_URL === 'http://localhost:3001'
+    ? `https://staging-api.va.gov/services/veteran/v0`
+    : `${environment.API_URL}/services/veteran/v0`;
+
+/**
+ * Build requestUrl and settings for api calls
+ *  * @param endpoint {String} eg '/vso_accredited_representatives'
+ *  * @param method {String} 'GET' or 'POST' (optional - defaults to 'GET')
+ *  * @param requestBody {String} optional
+ * @returns {requestUrl, apiSettings}
+ */
+export const getApi = (endpoint, method = 'GET', requestBody) => {
+  const requestUrl = `${baseUrl}${endpoint}`;
+
+  const apiSettings = {
+    mode: 'cors',
+    method,
+
+    headers: {
+      'X-Key-Inflection': 'camel',
+      'Sec-Fetch-Mode': 'cors',
+      'Content-Type': 'application/json',
+
+      // Pull app name directly from manifest since this config is defined
+      // before startApp, and using window.appName here would result in
+      // undefined for all requests that use this config.
+      'Source-App-Name': manifest.entryName,
+    },
+    body: JSON.stringify(requestBody) || null,
   };
+
+  return { requestUrl, apiSettings };
 };
 
 /**
@@ -72,10 +69,6 @@ export const resolveParamsWithUrl = ({
   sort,
   type = 'VSO',
 }) => {
-  const api = getAPI(type);
-
-  const { url } = api;
-
   const params = [
     address ? `address=${address}` : null,
     lat ? `lat=${lat}` : null,
@@ -87,13 +80,8 @@ export const resolveParamsWithUrl = ({
     type ? `type=${type}` : null,
   ];
 
-  return {
-    url,
-    params: compact([...params]).join('&'),
-  };
+  return `?${compact([...params]).join('&')}`;
 };
-// Please use sentence case for all of these
-// except 'Vet Centers' and acronyms like IDES.
 
 export const representativeTypes = {
   [RepresentativeType.VETERAN_SERVICE_OFFICER]: 'VSO',
