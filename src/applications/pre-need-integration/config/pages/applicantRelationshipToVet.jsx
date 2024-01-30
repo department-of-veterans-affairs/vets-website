3a6d3db7cf3ecde9407e905fbfaeba24db749553
Adam Antonioli <132934480+adam-antonioli@users.noreply.github.com>
2024-01-30 15:59:58 -0500

    MBMS-54134: Self-Service: Pre-Need Integration: Applicant Information: Relationship to Veteran Updates - Includes Adding New Pre-Need Integration Form Schema Changes (#27581)
    * MBMS-54134 change text on applicant information screen of pre need integration form
    
    * MBMS-54134 change text on applicant information screen of pre need integration form
    
    * MBMS-54134 fix unit test of applicant information screen of pre need integration form
    
    * MBMS-54134 fix unit test of applicant information screen of pre need integration form
    
    * MBMS-54134 fix unit test of applicant information screen of pre need integration form
    
    * MBMS-54134 fix unit test of applicant information screen of pre need integration form
    
    * MBMS-54134 fix unit test of applicant information screen of pre need integration form
    
    * MBMS-54134 fix relationship to vet helper functions of pre need integration form
    
    * MBMS-54134 fix unit test of applicant information screen of pre need integration form
    
    * MBMS-54134 fix unit test of applicant information screen of pre need integration form
    
    * MBMS-54134 Worked on unit tests
    
    * MBMS-54134 change schema values for relationshipToVet on applicant information screen of pre need integration form
    
    * MBMS-54134 replacing schema with new integration schema for pre need integration form
    
    * MBMS-54134 update schema
    
    * MBMS-54134 update relationShipToVet useage values to new schema values in pre need integration form
    
    * MBMS-54134 fix unit test for pre need integration form
    
    ---------
    
    Co-authored-by: 624883 <Connor.Fewin@va.gov>
    Co-authored-by: ConnorJeff <122034971+ConnorJeff@users.noreply.github.com>

--- src/applications/pre-need-integration/config/pages/applicantRelationshipToVet.jsx
+++ src/applications/pre-need-integration/config/pages/applicantRelationshipToVet.jsx
@@ -1,121 +1,60 @@
-import fullSchemaPreNeed from 'vets-json-schema/dist/40-10007-schema.json';
+import fullSchemaPreNeed from 'vets-json-schema/dist/40-10007-INTEGRATION-schema.json';
 
 import { merge, pick } from 'lodash';
 
-import environment from 'platform/utilities/environment';
-
 import {
   applicantInformationDescription,
-  ApplicantDescriptionWrapper,
-  childRelationshipDescription,
-  otherRelationshipDescription,
-  spouseRelationshipDescription,
-  veteranRelationshipDescription,
+  relationshipToVetDescription,
+  relationshipToVetOptions,
 } from '../../utils/helpers';
 
 const { claimant } = fullSchemaPreNeed.properties.application.properties;
 
-export const uiSchema = !environment.isProduction()
-  ? {
-      application: {
-        claimant: {
-          'ui:title': ' ',
-          relationshipToVet: {
-            'ui:title':
-              'What is the applicant’s relationship to the service member or Veteran?',
-            'ui:widget': 'radio',
-            'ui:options': {
-              labels: {
-                1: 'Applicant is the service member or Veteran',
-                2: 'Spouse or surviving spouse',
-                3: 'Unmarried adult child',
-                4: 'Other',
-              },
-              widgetProps: {
-                1: { 'aria-describedby': 'veteran-relationship' },
-                2: { 'aria-describedby': 'spouse-relationship' },
-                3: { 'aria-describedby': 'child-relationship' },
-                4: { 'aria-describedby': 'other-relationship' },
-              },
-            },
-          },
-          'view:applicantInformationDescription': {
-            'ui:description': applicantInformationDescription,
-            'ui:options': {
-              displayEmptyObjectOnReview: true,
-            },
+export function uiSchema(
+  description = relationshipToVetDescription,
+  title = relationshipToVetDescription,
+  options = relationshipToVetOptions,
+) {
+  return {
+    application: {
+      claimant: {
+        'ui:description': description,
+        relationshipToVet: {
+          'ui:title': title,
+          'ui:options': options,
+        },
+        'view:applicantInformationDescription': {
+          'ui:description': applicantInformationDescription,
+          'ui:options': {
+            displayEmptyObjectOnReview: true,
           },
         },
       },
-    }
-  : {
-      'ui:description': ApplicantDescriptionWrapper,
-      application: {
-        claimant: {
-          relationshipToVet: {
-            'ui:title': 'Relationship to service member',
-            'ui:widget': 'radio',
-            'ui:options': {
-              labels: {
-                1: 'I am the service member/Veteran',
-                2: 'Spouse or surviving spouse',
-                3: 'Unmarried adult child',
-                4: 'Other',
-              },
-              widgetProps: {
-                1: { 'aria-describedby': 'veteran-relationship' },
-                2: { 'aria-describedby': 'spouse-relationship' },
-                3: { 'aria-describedby': 'child-relationship' },
-                4: { 'aria-describedby': 'other-relationship' },
-              },
-              nestedContent: {
-                1: veteranRelationshipDescription,
-                2: spouseRelationshipDescription,
-                3: childRelationshipDescription,
-                4: otherRelationshipDescription,
-              },
-            },
-          },
-        },
-      },
-    };
-export const schema = !environment.isProduction()
-  ? {
+    },
+  };
+}
+
+export const schema = {
+  type: 'object',
+  properties: {
+    application: {
       type: 'object',
       properties: {
-        application: {
+        claimant: {
           type: 'object',
-          properties: {
-            claimant: {
-              type: 'object',
-              required: ['relationshipToVet'],
-              properties: merge(
-                {},
-                pick(claimant.properties, ['relationshipToVet']),
-                {
-                  'view:applicantInformationDescription': {
-                    type: 'object',
-                    properties: {},
-                  },
-                },
-              ),
+          required: ['relationshipToVet'],
+          properties: merge(
+            {},
+            pick(claimant.properties, ['relationshipToVet']),
+            {
+              'view:applicantInformationDescription': {
+                type: 'object',
+                properties: {},
+              },
             },
-          },
+          ),
         },
       },
-    }
-  : {
-      type: 'object',
-      properties: {
-        application: {
-          type: 'object',
-          properties: {
-            claimant: {
-              type: 'object',
-              required: ['relationshipToVet'],
-              properties: pick(claimant.properties, ['relationshipToVet']),
-            },
-          },
-        },
-      },
-    };
+    },
+  },
+};
--- src/applications/pre-need-integration/config/pages/sponsorMilitaryHistory.jsx
+++ src/applications/pre-need-integration/config/pages/sponsorMilitaryHistory.jsx
@@ -1,6 +1,6 @@
 import { merge } from 'lodash';
 
-import fullSchemaPreNeed from 'vets-json-schema/dist/40-10007-schema.json';
+import fullSchemaPreNeed from 'vets-json-schema/dist/40-10007-INTEGRATION-schema.json';
 
 import dateRangeUI from 'platform/forms-system/src/js/definitions/dateRange';
 
