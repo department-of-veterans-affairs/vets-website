4d188ec563f7e285a4a063c5991a4539c331fcc2
Alex Morgun <87077843+oleksii-morgun-va@users.noreply.github.com>
2024-01-30 14:02:42 -0500

    [#MHV-53639] hiding behind feature flag

--- src/applications/mhv/secure-messaging/components/Alerts/CernerTransitioningFacilityAlert.jsx
+++ src/applications/mhv/secure-messaging/components/Alerts/CernerTransitioningFacilityAlert.jsx
@@ -1,8 +1,13 @@
 import React, { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import FEATURE_FLAG_NAMES from '@department-of-veterans-affairs/platform-utilities/featureFlagNames';
 
 const CernerTransitioningFacilityAlert = props => {
   const { recipients } = props;
+  const cernerTransition556T30 = useSelector(
+    state => state.featureToggles[FEATURE_FLAG_NAMES.cernerTransition556T30],
+  );
 
   const isTranstioningFacility = useMemo(
     () => {
@@ -16,6 +21,7 @@ const CernerTransitioningFacilityAlert = props => {
   );
 
   return (
+    cernerTransition556T30 &&
     isTranstioningFacility && (
       <va-alert status="warning">
         <h2 slot="headline">Cerner Transition Alert</h2>
