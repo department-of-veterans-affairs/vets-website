07180024b8f79c565311f12ad66a5facd84e20e2
Daniel Cloud <daniel.cloud@adhocteam.us>
2024-02-01 17:57:14 -0500

    Add basic unit test

---
+++ src/platform/mhv/downtime/tests/containers/MHVDowntime.unit.spec.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable camelcase */
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { externalServiceStatus } from '@department-of-veterans-affairs/platform-monitoring/exports';
+
+import MHVDowntime from '../../containers/MHVDowntime';
+
+describe('MHVDowntime', () => {
+  it('renders MHVDown when a service is down', () => {
+    const now = new Date();
+    const later = new Date(now).setHours(now.getHours() + 4);
+
+    const mockServiceProps = {
+      endTime: later,
+      startTime: now,
+      externalService: 'mhv_sm',
+    };
+    const mockProps = {
+      children: ' ',
+      status: externalServiceStatus.down,
+      ...mockServiceProps,
+    };
+    const { getByRole } = render(<MHVDowntime {...mockProps} />);
+    getByRole('heading', { level: 3, name: /Secure Messaging is down/ });
+  });
+
+  it('renders MHVDowntimeApproaching when a service is going down within an hour', () => {
+    // Create a starting datetime 30 minutes into the future, though `status` is what really controls what renders
+    const soon = new Date(Date.now());
+    soon.setMinutes(soon.getMinutes() + 30);
+    const later = new Date(soon).setHours(soon.getHours() + 4);
+
+    const mockServiceProps = {
+      endTime: later,
+      startTime: soon,
+      externalService: 'mhv_sm',
+    };
+    const mockProps = {
+      children: ' ',
+      status: externalServiceStatus.downtimeApproaching,
+      ...mockServiceProps,
+    };
+    const { getByRole } = render(<MHVDowntime {...mockProps} />);
+    getByRole('heading', { level: 3, name: /Secure Messaging will be down/ });
+  });
+});
