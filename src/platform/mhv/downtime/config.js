5eb85a703a1f303241d223d93947fa3c66cd4270
Daniel Cloud <daniel.cloud@adhocteam.us>
2024-02-01 15:41:00 -0500

    Add mhvServiceLabels for apps

--- src/platform/mhv/downtime/config.js
+++ src/platform/mhv/downtime/config.js
@@ -1,8 +1,24 @@
+/*
+ * Map service labels as passed in from platform-monitoring's `externalService` variable
+ * Service labels come from vets-api as snake_case values.
+ */
+
+/* @constant
+ * @type {string}
+ */
 const defaultLabel = 'My HealtheVet';
+
+/*
+ * @readonly
+ * @enum {string}
+ */
 /* eslint-disable camelcase */
 const mhvServiceLabels = {
   mhv: 'MHV Login',
+  mhv_meds: 'Medications',
+  mhv_mr: 'Medical Records',
   mhv_platform: defaultLabel,
+  mhv_sm: 'Secure Messaging',
 };
 /* eslint-enable camelcase */
 
