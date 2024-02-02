102dae71aa5778555fe2670051384eecdbf81030
Aaron Read <Aaron.Read@va.gov>
2024-02-02 13:54:40 -0600

    add download txt all test

---
+++ src/applications/mhv/medical-records/tests/e2e/medical-records-download-txt-functionality-all.cypress.spec.js
@@ -0,0 +1,31 @@
+import moment from 'moment-timezone';
+import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
+
+describe('Medical Records Download All TXT Functionality', () => {
+  it('Medical Records Download All TXTX Functionality', () => {
+    const site = new MedicalRecordsSite();
+    site.login();
+    // Given Navigate to Radiology Page
+    cy.visit('my-health/medical-records/download-all');
+
+    cy.get('[data-testid="download-blue-button-txt"]').click();
+    // cy.readFile(`${Cypress.config('downloadsFolder')}/radiology_report.pdf`);
+    site.verifyDownloadedPdfFile(
+      'VA-Blue-Button-report-Safari-Mhvtp',
+      moment(),
+      '',
+    );
+    // Axe check
+    cy.injectAxe();
+    cy.axeCheck('main', {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+        'link-name': {
+          enabled: false,
+        },
+      },
+    });
+  });
+});
