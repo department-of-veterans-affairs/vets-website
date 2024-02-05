73288e4f5498fb97ae95c1573d464396ce0ce086
Wafi Mohamed <wafi.mohamed@va.gov>
2024-02-05 15:15:29 -0500

    removed disables eslint VYE-213]

--- src/applications/verify-your-enrollment/components/ChangeOfAddressForm.jsx
+++ src/applications/verify-your-enrollment/components/ChangeOfAddressForm.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable @department-of-veterans-affairs/use-workspace-imports */
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import {
--- src/applications/verify-your-enrollment/components/PeriodsToVerify.jsx
+++ src/applications/verify-your-enrollment/components/PeriodsToVerify.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable @department-of-veterans-affairs/prefer-button-component */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
