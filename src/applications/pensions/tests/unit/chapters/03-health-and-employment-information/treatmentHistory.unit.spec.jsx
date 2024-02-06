4957b87acc78ae49c1b1090f4c0987e7984ea652
Tai Wilkin-Corraggio <tai.wilkin@coforma.io>
2024-02-06 12:19:48 -0500

    Add Pension home acreage review (#27746)
    * Add Pension home acreage review
    
    * Update button and style
    
    * Fix non-running test

--- src/applications/pensions/tests/unit/chapters/03-health-and-employment-information/treatmentHistory.unit.spec.jsx
+++ src/applications/pensions/tests/unit/chapters/03-health-and-employment-information/treatmentHistory.unit.spec.jsx
@@ -7,48 +7,44 @@ import treatmentHistory from '../../../../config/chapters/03-health-and-employme
 import generateMedicalCentersSchemas from '../../../../config/chapters/03-health-and-employment-information/medicalCenters';
 
 describe('pension treatment history page', () => {
-  it('should render web components in form correctly', () => {
-    const pageTitle = 'treatment history';
-    const expectedNumberOfFields = 1;
-    testNumberOfWebComponentFields(
-      formConfig,
-      treatmentHistory.schema,
-      treatmentHistory.uiSchema,
-      expectedNumberOfFields,
-      pageTitle,
-    );
+  const pageTitle = 'treatment history';
+  const expectedNumberOfFields = 1;
+  testNumberOfWebComponentFields(
+    formConfig,
+    treatmentHistory.schema,
+    treatmentHistory.uiSchema,
+    expectedNumberOfFields,
+    pageTitle,
+  );
 
-    const expectedNumberOfErrors = 1;
-    testNumberOfErrorsOnSubmitForWebComponents(
-      formConfig,
-      treatmentHistory.schema,
-      treatmentHistory.uiSchema,
-      expectedNumberOfErrors,
-      pageTitle,
-    );
-  });
+  const expectedNumberOfErrors = 1;
+  testNumberOfErrorsOnSubmitForWebComponents(
+    formConfig,
+    treatmentHistory.schema,
+    treatmentHistory.uiSchema,
+    expectedNumberOfErrors,
+    pageTitle,
+  );
 });
 
 describe('pension add medical centers page', () => {
-  it('should render web components in form correctly', () => {
-    const medicalCenters = generateMedicalCentersSchemas();
-    const pageTitle = 'medical centers';
-    const expectedNumberOfFields = 1;
-    testNumberOfWebComponentFields(
-      formConfig,
-      medicalCenters.schema,
-      medicalCenters.uiSchema,
-      expectedNumberOfFields,
-      pageTitle,
-    );
+  const medicalCenters = generateMedicalCentersSchemas();
+  const pageTitle = 'medical centers';
+  const expectedNumberOfFields = 1;
+  testNumberOfWebComponentFields(
+    formConfig,
+    medicalCenters.schema,
+    medicalCenters.uiSchema,
+    expectedNumberOfFields,
+    pageTitle,
+  );
 
-    const expectedNumberOfErrors = 1;
-    testNumberOfErrorsOnSubmitForWebComponents(
-      formConfig,
-      medicalCenters.schema,
-      medicalCenters.uiSchema,
-      expectedNumberOfErrors,
-      pageTitle,
-    );
-  });
+  const expectedNumberOfErrors = 1;
+  testNumberOfErrorsOnSubmitForWebComponents(
+    formConfig,
+    medicalCenters.schema,
+    medicalCenters.uiSchema,
+    expectedNumberOfErrors,
+    pageTitle,
+  );
 });
