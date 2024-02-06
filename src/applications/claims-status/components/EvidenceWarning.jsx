de388e4d8bb6e1b44d18b39d99276613a071f624
Peri-Ann McLaren <141954992+pmclaren19@users.noreply.github.com>
2024-02-06 12:14:25 -0700

    [CST] Update files that have va components without the uswds prop for claim-status (#27815)
    * update files for v3 components having uswds="false"
    
    * Update files for V3

--- src/applications/claims-status/components/EvidenceWarning.jsx
+++ src/applications/claims-status/components/EvidenceWarning.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function EvidenceWarning() {
   return (
-    <va-alert className="claims-alert" status="warning">
+    <va-alert className="claims-alert" status="warning" uswds="false">
       <h3 slot="headline">
         Please only submit additional evidence that supports this claim
       </h3>
