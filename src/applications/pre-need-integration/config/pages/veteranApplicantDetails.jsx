e9ad062dc21e5639485dff0af7454e307cce3d9c
Adam Antonioli <adam.antonioli@va.gov>
2024-02-07 09:35:27 -0500

    MBMS-43492 change fields on pre need integration veteran applicant details

--- src/applications/pre-need-integration/config/pages/veteranApplicantDetails.jsx
+++ src/applications/pre-need-integration/config/pages/veteranApplicantDetails.jsx
@@ -1,11 +1,8 @@
 import fullSchemaPreNeed from 'vets-json-schema/dist/40-10007-INTEGRATION-schema.json';
 
-import applicantDescription from 'platform/forms/components/ApplicantDescription';
 import currentOrPastDateUI from 'platform/forms-system/src/js/definitions/currentOrPastDate';
 
 import { merge, pick } from 'lodash';
-
-import environment from 'platform/utilities/environment';
 
 import {
   applicantDetailsDescription,
@@ -19,95 +16,56 @@ const {
   veteran,
 } = fullSchemaPreNeed.properties.application.properties;
 
-export const uiSchema = !environment.isProduction()
-  ? {
-      application: {
-        'ui:title': applicantDetailsSubHeader,
-        claimant: {
-          'view:applicantDetailsDescription': {
-            'ui:description': applicantDetailsDescription,
-            'ui:options': {
-              displayEmptyObjectOnReview: true,
-            },
-          },
-          name: fullMaidenNameUI,
-          ssn: ssnDashesUI,
-          dateOfBirth: currentOrPastDateUI('Date of birth'),
-        },
-        veteran: {
-          placeOfBirth: {
-            'ui:title': 'Place of birth (city, state, territory)',
-          },
+export const uiSchema = {
+  application: {
+    'ui:title': applicantDetailsSubHeader,
+    claimant: {
+      'view:applicantDetailsDescription': {
+        'ui:description': applicantDetailsDescription,
+        'ui:options': {
+          displayEmptyObjectOnReview: true,
         },
       },
-    }
-  : {
-      'ui:description': applicantDescription,
-      application: {
-        'ui:title': applicantDetailsSubHeader,
-        claimant: {
-          name: fullMaidenNameUI,
-          ssn: ssnDashesUI,
-          dateOfBirth: currentOrPastDateUI('Date of birth'),
-        },
-        veteran: {
-          placeOfBirth: {
-            'ui:title': 'Place of birth (city, state, territory)',
-          },
-        },
+      name: fullMaidenNameUI,
+      ssn: ssnDashesUI,
+      dateOfBirth: currentOrPastDateUI('Date of birth'),
+    },
+    veteran: {
+      birthCity: {
+        'ui:title': 'Your birth city or county',
       },
-    };
-export const schema = !environment.isProduction()
-  ? {
+      birthState: {
+        'ui:title': 'Your birth state or territory',
+      },
+    },
+  },
+};
+export const schema = {
+  type: 'object',
+  properties: {
+    application: {
       type: 'object',
       properties: {
-        application: {
+        claimant: {
           type: 'object',
-          properties: {
-            claimant: {
-              type: 'object',
-              required: ['name', 'ssn', 'dateOfBirth'],
-              properties: merge(
-                {},
-                {
-                  'view:applicantDetailsDescription': {
-                    type: 'object',
-                    properties: {},
-                  },
-                },
-                pick(claimant.properties, ['name', 'ssn', 'dateOfBirth']),
-              ),
+          required: ['name', 'ssn', 'dateOfBirth'],
+          properties: merge(
+            {},
+            {
+              'view:applicantDetailsDescription': {
+                type: 'object',
+                properties: {},
+              },
             },
-            veteran: {
-              type: 'object',
-              required: ['placeOfBirth'],
-              properties: pick(veteran.properties, ['placeOfBirth']),
-            },
-          },
+            pick(claimant.properties, ['name', 'ssn', 'dateOfBirth']),
+          ),
+        },
+        veteran: {
+          type: 'object',
+          required: ['birthCity', 'birthState'],
+          properties: pick(veteran.properties, ['placeOfBirth']),
         },
       },
-    }
-  : {
-      type: 'object',
-      properties: {
-        application: {
-          type: 'object',
-          properties: {
-            claimant: {
-              type: 'object',
-              required: ['name', 'ssn', 'dateOfBirth'],
-              properties: pick(claimant.properties, [
-                'name',
-                'ssn',
-                'dateOfBirth',
-              ]),
-            },
-            veteran: {
-              type: 'object',
-              required: ['placeOfBirth'],
-              properties: pick(veteran.properties, ['placeOfBirth']),
-            },
-          },
-        },
-      },
-    };
+    },
+  },
+};
