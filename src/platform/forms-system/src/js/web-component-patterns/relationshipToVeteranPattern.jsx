98d9a408496367efecc7fa7cff683b636f54c4df
Michael Clement <michael.clement@adhocteam.us>
2024-02-07 17:38:50 -0600

    Add new relationship field to main UI pattern

--- src/platform/forms-system/src/js/web-component-patterns/relationshipToVeteranPattern.jsx
+++ src/platform/forms-system/src/js/web-component-patterns/relationshipToVeteranPattern.jsx
@@ -19,33 +19,43 @@ import VaTextInputField from '../web-component-fields/VaTextInputField';
  *
  * @param {string | {
  *   personTitle?: string,
+ *   relativeTitle?: string,
  *   labelHeaderLevel?: UISchemaOptions['ui:options']['labelHeaderLevel'],
  * }} options - a string representing the person type, or an object with options
  * @returns {UISchemaOptions}
  */
 
 export const relationshipToVeteranUI = options => {
-  const { personTitle, labelHeaderLevel } =
+  const { personTitle, relativeTitle, labelHeaderLevel } =
     typeof options === 'object' ? options : { personTitle: options };
   const person = personTitle ?? 'Veteran';
 
+  const relativeBeingVerb = `${`${
+    relativeTitle ? `${relativeTitle} is` : 'I’m'
+  }`}`;
+  const relativePossessive = `${`${
+    relativeTitle ? `${relativeTitle}’s` : 'your'
+  }`}`;
+
   return {
     relationshipToVeteran: radioUI({
-      title: `What’s your relationship to the ${person}?`,
+      title: `What’s ${relativePossessive} relationship to the ${person}?`,
       labels: {
-        spouse: `I’m the ${person}’s spouse`,
-        child: `I’m the ${person}’s child`,
-        parent: `I’m the ${person}’s parent`,
-        executor: `I’m the ${person}’s executor or administrator of estate`,
-        other: 'We don’t have a relationship that’s listed here',
+        spouse: `${relativeBeingVerb} the ${person}’s spouse`,
+        child: `${relativeBeingVerb} the ${person}’s child`,
+        parent: `${relativeBeingVerb} the ${person}’s parent`,
+        executor: `${relativeBeingVerb} the ${person}’s executor or administrator of estate`,
+        other: `${`${
+          relativeTitle ? `${relativeTitle} doesn’t` : 'We don’t'
+        }`} have a relationship that’s listed here`,
       },
       errorMessages: {
-        required: `Please select your relationship to the ${person}`,
+        required: `Please enter ${relativePossessive} relationship to the ${person}`,
       },
       labelHeaderLevel: labelHeaderLevel ?? '3',
     }),
     otherRelationshipToVeteran: {
-      'ui:title': `Since your relationship with the ${person} was not listed, please describe it here`,
+      'ui:title': `Since ${relativePossessive} relationship with the ${person} was not listed, please describe it here`,
       'ui:webComponentField': VaTextInputField,
       'ui:options': {
         expandUnder: 'relationshipToVeteran',
@@ -53,7 +63,7 @@ export const relationshipToVeteranUI = options => {
         expandedContentFocus: true,
       },
       'ui:errorMessages': {
-        required: `Please enter your relationship to the ${person}`,
+        required: `Please enter ${relativePossessive} relationship to the ${person}`,
       },
     },
     'ui:options': {
