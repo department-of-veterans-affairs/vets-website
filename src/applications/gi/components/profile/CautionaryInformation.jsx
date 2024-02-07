00093935aab821e2d06df62ed18a5d6c5f008728
fatmakhan0395 <Fatma.khan@va.gov>
2024-02-06 17:12:59 -0800

    added cautionary info verbiage

--- src/applications/gi/components/profile/CautionaryInformation.jsx
+++ src/applications/gi/components/profile/CautionaryInformation.jsx
@@ -73,6 +73,20 @@ export function CautionaryInformation({ institution, showModal }) {
 
     return (
       <div>
+        <h3>Cautionary warnings and school closings</h3>
+        <p>
+          If any cautionary warnings have been issued, they will appear below.
+          VA applies caution flags when we, or another federal agency, have
+          increased regulatory or legal scrutiny of an educational program. We
+          recommend students consider these warnings before enrolling in flagged
+          programs.
+        </p>
+        <p>
+          In addition, if the VA receives notice that a school or campus
+          location will be closing, it will appear below. Once the closing date
+          passes, we remove the institution from the Comparison Tool during the
+          next system update.
+        </p>
         <h3 tabIndex="-1" id="viewWarnings">
           Alerts from VA and other federal agencies
         </h3>
@@ -146,6 +160,7 @@ export function CautionaryInformation({ institution, showModal }) {
   return (
     <div className="cautionary-information small-screen-font">
       {renderCautionFlags()}
+      <hr />
 
       <div className="student-complaints">
         <h3 className="small-screen-font">Student feedback</h3>
