bb6523cc1b63ce3659fd1349c3d825c8d3baf920
fatmakhan0395 <Fatma.khan@va.gov>
2024-02-07 07:18:15 -0800

    added feature flags

--- src/applications/gi/components/profile/CautionaryInformation.jsx
+++ src/applications/gi/components/profile/CautionaryInformation.jsx
@@ -4,6 +4,7 @@ import CautionFlagDetails from './CautionFlagDetails';
 import SchoolClosingDetails from './SchoolClosingDetails';
 import LearnMoreLabel from '../LearnMoreLabel';
 import { ariaLabels, complaintData } from '../../constants';
+import { isProductionOfTestProdEnv } from '../../utils/helpers';
 
 export function CautionaryInformation({ institution, showModal }) {
   const {
@@ -73,20 +74,24 @@ export function CautionaryInformation({ institution, showModal }) {
 
     return (
       <div>
-        <h3>Cautionary warnings and school closings</h3>
-        <p>
-          If any cautionary warnings have been issued, they will appear below.
-          VA applies caution flags when we, or another federal agency, have
-          increased regulatory or legal scrutiny of an educational program. We
-          recommend students consider these warnings before enrolling in flagged
-          programs.
-        </p>
-        <p>
-          In addition, if the VA receives notice that a school or campus
-          location will be closing, it will appear below. Once the closing date
-          passes, we remove the institution from the Comparison Tool during the
-          next system update.
-        </p>
+        {!isProductionOfTestProdEnv() && (
+          <div>
+            <h3>Cautionary warnings and school closings</h3>
+            <p>
+              If any cautionary warnings have been issued, they will appear
+              below. VA applies caution flags when we, or another federal
+              agency, have increased regulatory or legal scrutiny of an
+              educational program. We recommend students consider these warnings
+              before enrolling in flagged programs.
+            </p>
+            <p>
+              In addition, if the VA receives notice that a school or campus
+              location will be closing, it will appear below. Once the closing
+              date passes, we remove the institution from the Comparison Tool
+              during the next system update.
+            </p>
+          </div>
+        )}
         <h3 tabIndex="-1" id="viewWarnings">
           Alerts from VA and other federal agencies
         </h3>
