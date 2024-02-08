9d2d5e74393f24c1b6b429372eb006bb745384d8
Peri McLaren <peri.mclaren@adhocteam.us>
2024-02-08 14:14:35 -0700

    add whatweredoing section and tests

---
+++ src/applications/claims-status/components/WhatWereDoing.jsx
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router';
+
+const statusMap = {
+  CLAIM_RECEIVED: 'Step 1 of 5: Claim received',
+  INITIAL_REVIEW: 'Step 2 of 5: Initial review',
+  EVIDENCE_GATHERING_REVIEW_DECISION:
+    'Step 3 of 5: Evidence gathering, review, and decision',
+  PREPARATION_FOR_NOTIFICATION: 'Step 4 of 5: Preparation for notification',
+  COMPLETE: 'Step 5 of 5: Closed',
+};
+
+function getStatusDescription(status) {
+  return statusMap[status];
+}
+
+function WhatWereDoing({ claim }) {
+  const { status } = claim.attributes;
+
+  const humanStatus = getStatusDescription(status);
+
+  return (
+    <div className="what-were-doing-container">
+      <h3 className="vads-u-margin-bottom--3">What we’re doing</h3>
+      <va-card uswds="false">
+        <h4 className="vads-u-margin-top--0 vads-u-margin-bottom--1">
+          {humanStatus}
+        </h4>
+        <p className="vads-u-margin-top--0p5 vads-u-margin-bottom--0p5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam.
+        </p>
+        <Link
+          aria-label="Overview of the process"
+          className="overview-of-process-link"
+          to={`your-claims/${claim.id}/overview`}
+        >
+          <span>
+            <strong>Overview of the process</strong>
+            <i
+              aria-hidden="true"
+              role="img"
+              className="fas fa-chevron-right vads-u-padding-left--0p5"
+            />
+          </span>
+        </Link>
+      </va-card>
+    </div>
+  );
+}
+
+WhatWereDoing.propTypes = {
+  claim: PropTypes.object,
+  status: PropTypes.bool,
+};
+
+export default WhatWereDoing;
---
+++ src/applications/claims-status/tests/components/WhatWereDoing.unit.spec.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { expect } from 'chai';
+import { $ } from '@department-of-veterans-affairs/platform-forms-system/ui';
+
+import WhatWereDoing from '../../components/WhatWereDoing';
+
+const statusMap = {
+  CLAIM_RECEIVED: 'Step 1 of 5: Claim received',
+  INITIAL_REVIEW: 'Step 2 of 5: Initial review',
+  EVIDENCE_GATHERING_REVIEW_DECISION:
+    'Step 3 of 5: Evidence gathering, review, and decision',
+  PREPARATION_FOR_NOTIFICATION: 'Step 4 of 5: Preparation for notification',
+  COMPLETE: 'Step 5 of 5: Closed',
+};
+
+function getStatusDescription(status) {
+  return statusMap[status];
+}
+
+describe('<WhatWereDoing>', () => {
+  it('should render a WhatWereDoing section', () => {
+    const claim = {
+      id: '1',
+      attributes: {
+        supportingDocuments: [],
+        claimDate: '2023-01-01',
+        closeDate: null,
+        documentsNeeded: true,
+        decisionLetterSent: false,
+        status: 'INITIAL_REVIEW',
+        claimPhaseDates: {
+          currentPhaseBack: false,
+          phaseChangeDate: '2015-01-01',
+          latestPhaseType: 'INITIAL_REVIEW',
+          previousPhases: {
+            phase1CompleteDate: '2023-02-08',
+            phase2CompleteDate: '2023-02-08',
+          },
+        },
+      },
+    };
+    const { container, getByText } = render(<WhatWereDoing claim={claim} />);
+    expect($('.what-were-doing-container', container)).to.exist;
+    getByText(getStatusDescription(claim.attributes.status));
+  });
+});
