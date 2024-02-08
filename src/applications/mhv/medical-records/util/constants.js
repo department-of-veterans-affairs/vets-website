4f34917a290dcd9b7e484dd4ddc402b53a36e4d1
tynyttie <ty_barnes_jobs@yahoo.com>
2024-02-08 11:08:06 -0500

    Changed page tab title to match content title.

--- src/applications/mhv/medical-records/util/constants.js
+++ src/applications/mhv/medical-records/util/constants.js
@@ -143,7 +143,7 @@ export const pageTitles = {
   CARE_SUMMARIES_AND_NOTES_PAGE_TITLE:
     'Care Summaries And Notes - Medical Records | Veterans Affairs',
   VACCINES_PAGE_TITLE: 'Vaccines - Medical Records | Veterans Affairs',
-  ALLERGIES_PAGE_TITLE: 'Allergies - Medical Records | Veterans Affairs',
+  ALLERGIES_PAGE_TITLE: 'Allergies and reactions',
   HEALTH_CONDITIONS_PAGE_TITLE:
     'Health Conditions - Medical Records | Veterans Affairs',
   VITALS_PAGE_TITLE: 'Vitals - Medical Records | Veterans Affairs',
