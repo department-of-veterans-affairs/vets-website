f5267b8f13e74436534b0709cc4ac6cc17d6b848
Michael Clement <michael.clement@adhocteam.us>
2024-02-09 16:52:10 -0600

    Update filesize calculation func flow

--- src/applications/ivc-champva/10-10D/helpers/utilities.js
+++ src/applications/ivc-champva/10-10D/helpers/utilities.js
@@ -1,9 +1,9 @@
 export const getFileSize = num => {
   if (num > 999999) {
     return `${(num / 1000000).toFixed(1)} MB`;
+  } else if (num > 999) {
+    return `${(num / 1000).toFixed(1)} KB`;
+  } else {
+    return `${num} B`;
   }
-  if (num > 999) {
-    return `${Math.floor(num / 1000)} KB`;
-  }
-  return `${num} B`;
 };
