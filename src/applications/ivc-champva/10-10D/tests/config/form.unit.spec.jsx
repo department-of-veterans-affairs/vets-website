1868cab883a303a9459d4afbdfc3cfc520843b03
Michael Clement <michael.clement@adhocteam.us>
2024-02-09 16:49:43 -0600

    adjust test for filesize

--- src/applications/ivc-champva/10-10D/tests/config/form.unit.spec.jsx
+++ src/applications/ivc-champva/10-10D/tests/config/form.unit.spec.jsx
@@ -102,13 +102,13 @@ describe('FileFieldCustom remove button', () => {
 
 describe('File sizes', () => {
   it('should be in bytes for values < 999', () => {
-    expect(getFileSize(998) === '998 B').to.be.true;
+    expect(getFileSize(998)).to.equal('998 B');
   });
   it('should be in KB for values between a thousand and a million', () => {
-    expect(getFileSize(1024) === '1 KB').to.be.true;
+    expect(getFileSize(1024)).to.equal('1 KB');
   });
   it('should be in MB for values greater than a million', () => {
-    expect(getFileSize(2000000) === '2.0 MB').to.be.true;
+    expect(getFileSize(2000000)).to.equal('2.0 MB');
   });
 });
 
