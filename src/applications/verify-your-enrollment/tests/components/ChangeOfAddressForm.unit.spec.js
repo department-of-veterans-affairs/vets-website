74917041b05ed0093bd390c7efe69ab0273724fa
Wafi Mohamed <wafi.mohamed@va.gov>
2024-02-12 13:57:49 -0500

    fixed flicky tests VYE-222

--- src/applications/verify-your-enrollment/tests/components/ChangeOfAddressForm.unit.spec.js
+++ src/applications/verify-your-enrollment/tests/components/ChangeOfAddressForm.unit.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import SchemaForm from '@department-of-veterans-affairs/platform-forms-system/SchemaForm';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { mount, shallow } from 'enzyme';
 import ChangeOfAddressForm from '../../components/ChangeOfAddressForm';
 
@@ -31,61 +31,80 @@ describe('when <ChangeOfAddressForm/> renders', () => {
     const wrapper = render(<ChangeOfAddressForm {...dummyProps} />);
     expect(wrapper).to.be.not.null;
   });
-  it('calls useEffect on initial render and sets initial state', () => {
+  it('calls useEffect on initial render and sets initial state', async () => {
     const newAddressFormData = { 'view:livesOnMilitaryBaseInfo': true };
     const { wrapper } = setupRenderComp();
     wrapper.setProps({ addressFormData: newAddressFormData });
-    expect(wrapper.find('SchemaForm').prop('data')).to.eql(newAddressFormData);
+    await waitFor(() => {
+      expect(wrapper.find('SchemaForm').prop('data')).to.eql(
+        newAddressFormData,
+      );
+    });
     wrapper.unmount();
   });
-  it('renders SchemaForm with the correct props', () => {
+  it('renders SchemaForm with the correct props', async () => {
     const { wrapper } = setupRenderComp();
     const schemaForm = wrapper.find(SchemaForm);
-    expect(schemaForm.exists()).to.be.true;
-    expect(schemaForm.prop('data')).to.eql(mockAddressFormData);
+    await waitFor(() => {
+      expect(schemaForm.exists()).to.be.true;
+      expect(schemaForm.prop('data')).to.eql(mockAddressFormData);
+    });
+
     wrapper.unmount();
   });
-  it('renders an error message when city input is invalid', () => {
+  it('renders an error message when city input is invalid', async () => {
     const { wrapper } = setupRenderComp();
     wrapper.find('form').simulate('submit');
     const cityErrorMsg = wrapper.find('.usa-input-error').at(2);
-    expect(cityErrorMsg.text()).includes('City is required');
+    await waitFor(() => {
+      expect(cityErrorMsg.text()).includes('City is required');
+    });
     wrapper.unmount();
   });
-  it('renders an error message when state input is invalid', () => {
+  it('renders an error message when state input is invalid', async () => {
     const { wrapper } = setupRenderComp();
     wrapper.find('form').simulate('submit');
     const staterrorMsg = wrapper.find('.usa-input-error').last();
-    expect(staterrorMsg.text()).includes('State is required');
+    await waitFor(() => {
+      expect(staterrorMsg.text()).includes('State is required');
+    });
     wrapper.unmount();
   });
-  it('sets ui:title to on APO/FPO/DPO when view:livesOnMilitaryBase is true', () => {
+  it('sets ui:title to on APO/FPO/DPO when view:livesOnMilitaryBase is true', async () => {
     const addressFormData = { 'view:livesOnMilitaryBase': true };
     const wrapper = shallow(
       <ChangeOfAddressForm addressFormData={addressFormData} />,
     );
     wrapper.setProps({ addressFormData });
-    expect(wrapper.find('addressUISchema')).to.not.equal('APO/FPO/DPO');
+    await waitFor(() => {
+      expect(wrapper.find('addressUISchema')).to.not.equal('APO/FPO/DPO');
+    });
+
     wrapper.unmount();
   });
-  it('should return true for ui:required', () => {
+  it('should return true for ui:required', async () => {
     const addressFormData = {
       'view:livesOnMilitaryBase': true,
       countryCodeIso3: 'USA',
     };
     const { wrapper } = setupRenderComp();
     wrapper.find('form').simulate('submit');
-    expect(mockFormSubmit.called).to.be.false;
+    await waitFor(() => {
+      expect(mockFormSubmit.called).to.be.false;
+    });
+
     const newStateFormData = {
       ...addressFormData,
       stateCode: 'CA',
     };
     wrapper.setProps({ addressFormData: newStateFormData });
     wrapper.find('form').simulate('submit');
-    expect(mockFormSubmit.calledOnce).to.be.false;
+    await waitFor(() => {
+      expect(mockFormSubmit.calledOnce).to.be.false;
+    });
     wrapper.unmount();
   });
-  it('removes stateCode as a required field when certain conditions are met', () => {
+  it('removes stateCode as a required field when certain conditions are met', async () => {
     const { wrapper } = setupRenderComp();
     let addressFormData = {
       countryCodeIso3: 'USA',
@@ -100,8 +119,12 @@ describe('when <ChangeOfAddressForm/> renders', () => {
     };
     wrapper.setProps({ addressFormData });
     wrapper.find('form').simulate('submit');
-    const stateCodeField = wrapper.find('input[name="stateCode"]');
-    expect(stateCodeField.exists()).to.be.false;
+
+    await waitFor(() => {
+      const stateCodeField = wrapper.find('input[name="stateCode"]');
+      expect(stateCodeField.exists()).to.be.false;
+    });
+
     wrapper.unmount();
   });
 });
