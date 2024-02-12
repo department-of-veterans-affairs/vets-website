87f9a84812d14ba06642489c8d113a0c66dafdcd
William Phelps <williamphelps13@gmail.com>
2024-02-11 22:28:45 -0400

    Add LoginViewWrapper to Dashboard to add Breadcrumb and Sidenav; Align styles with Platform layouts

--- src/applications/representatives/containers/Dashboard.jsx
+++ src/applications/representatives/containers/Dashboard.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Link } from 'react-router';
 
-// import { RequiredLoginView } from '@department-of-veterans-affairs/platform-user/RequiredLoginView';
+import LoginViewWrapper from './LoginViewWrapper';
 
-const Dashboard = () => (
-  // <RequiredLoginView verify serviceRequired={[]} user={user}>
-  <div className="dash-container">
-    <h1 className="dash-header vads-u-font-size--h2 vads-u-margin-bottom--4">
-      Representative Dashboard
-    </h1>
-    <div className="vads-u-display--flex vads-u-justify-content--center">
-      <aside className="left vads-l-col--12 vads-u-padding-x--2p5 medium-screen:vads-l-col--4 large-screen:vads-l-col--3 vads-u-background-color--gray-lightest" />
-      <article className="vads-m-col--8 vads-l-col--12 vads-u-padding-left--5 medium-screen:vads-l-col--8 large-screen:vads-l-col--5">
-        <div className="nav dash-box vads-u-background-color--gray-lightest vads-u-margin-bottom--2" />
-        <div className="notif dash-box vads-u-background-color--gray-lightest vads-u-margin-bottom--2" />
-        <div className="primary dash-box vads-u-background-color--gray-lightest vads-u-margin-bottom--2" />
-        <div className="etc dash-box vads-u-background-color--gray-lightest" />
-      </article>
-    </div>
-  </div>
-  // </RequiredLoginView>
-);
-
-Dashboard.propTypes = {
-  user: PropTypes.object,
+const Dashboard = () => {
+  const breadcrumbs = [
+    { link: '/', label: 'Home' },
+    { link: '/dashboard', label: 'Dashboard' },
+  ];
+  return (
+    <LoginViewWrapper breadcrumbs={breadcrumbs}>
+      <h1>Accredited Representative Portal</h1>
+      <Link
+        to="/poa-requests"
+        className="vads-c-action-link--green vads-u-margin-bottom--2"
+      >
+        Manage power of attorney requests
+      </Link>
+      <div className="placeholder-container">
+        <div className="notif vads-u-background-color--gray-lightest vads-u-margin-bottom--2" />
+        <div className="primary vads-u-background-color--gray-lightest vads-u-margin-bottom--2" />
+        <div className="etc vads-u-background-color--gray-lightest" />
+      </div>
+    </LoginViewWrapper>
+  );
 };
 
 export default Dashboard;
