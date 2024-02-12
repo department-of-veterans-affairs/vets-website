2011c89acf7b99ab9147f161256e9bc12aadfc97
Tony Williams <tony.williams@va.gov>
2024-02-11 20:21:51 -0600

    fix failing tests

--- src/applications/vaos/tests/e2e/page-objects/AppointmentList/AppointmentListPageObject.js
+++ src/applications/vaos/tests/e2e/page-objects/AppointmentList/AppointmentListPageObject.js
@@ -3,8 +3,7 @@ import PageObject from '../PageObject';
 export class AppointmentListPageObject extends PageObject {
   assertAppointmentList({ numberOfAppointments = 0 } = {}) {
     // Wait for appointments to load
-    cy.wait(['@v2:get:appointments']);
-    cy.wait(['@v2:get:appointments']);
+    cy.wait(['@v2:get:appointments', '@v2:get:appointments']);
 
     cy.findAllByTestId('appointment-list-item').should($li => {
       expect($li).to.have.length(numberOfAppointments);
