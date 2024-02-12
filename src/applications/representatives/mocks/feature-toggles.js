be1bd7b707663548c11f6122275399d4b5dfe596
William Phelps <williamphelps13@gmail.com>
2024-02-11 22:30:48 -0400

    Add feature toggles mock for e2e testing

---
+++ src/applications/representatives/mocks/feature-toggles.js
@@ -0,0 +1,17 @@
+const generateFeatureToggles = (toggles = {}) => {
+  const { representativesPortalFrontend = false } = toggles;
+
+  return {
+    data: {
+      type: 'feature_toggles',
+      features: [
+        {
+          name: 'representatives_portal_frontend',
+          value: representativesPortalFrontend,
+        },
+      ],
+    },
+  };
+};
+
+module.exports = { generateFeatureToggles };
