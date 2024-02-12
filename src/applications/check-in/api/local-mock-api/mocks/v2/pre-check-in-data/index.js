4cdab37820f79aad1a6a53ef7ccbab2a9fdf340a
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2024-02-12 09:28:49 -0500

    temporarily remove check-in directory to test unit tests

--- src/applications/check-in/api/local-mock-api/mocks/v2/pre-check-in-data/index.js
+++
@@ -1,7 +0,0 @@
-const get = require('./get');
-const post = require('./post');
-
-module.exports = {
-  get: { ...get },
-  post: { ...post },
-};
--- src/applications/check-in/api/local-mock-api/mocks/v2/pre-check-in-data/post.js
+++
@@ -1,5 +0,0 @@
-const createMockSuccessResponse = _data => {
-  return { data: 'Pre Check In successful', status: 200 };
-};
-
-module.exports = { createMockSuccessResponse };
