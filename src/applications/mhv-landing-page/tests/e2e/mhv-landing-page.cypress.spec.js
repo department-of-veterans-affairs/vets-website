f84fbfdffb68236b8f9ba432f96394ae861088cc
Carlos <carlos.felix@agile6.com>
2024-02-13 11:21:34 -0500

    Added e2e test

--- src/applications/mhv-landing-page/tests/e2e/mhv-landing-page.cypress.spec.js
+++ src/applications/mhv-landing-page/tests/e2e/mhv-landing-page.cypress.spec.js
@@ -1,9 +1,10 @@
+import { notFoundHeading } from '@department-of-veterans-affairs/platform-site-wide/PageNotFound';
 import manifest from '../../manifest.json';
 
 describe(manifest.appName, () => {
   it('Visit bad URL', () => {
     cy.visit(`${manifest.rootUrl}/dummy`);
     cy.injectAxeThenAxeCheck();
-    cy.findByRole('heading', { name: /we can’t find that page/ }).should.exist;
+    cy.findByRole('heading', { name: notFoundHeading }).should.exist;
   });
 });
