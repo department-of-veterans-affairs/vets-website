bc52a07d183c3df861f8637b2051406e27ec0644
Wafi Mohamed <wafi.mohamed@va.gov>
2024-02-13 12:51:44 -0500

    added unit test for missing components VYE-222

--- src/applications/verify-your-enrollment/tests/actions/index.unit.spec.js
+++ src/applications/verify-your-enrollment/tests/actions/index.unit.spec.js
@@ -1,7 +1,16 @@
 import { expect } from 'chai';
+import * as apiModule from '@department-of-veterans-affairs/platform-utilities/api';
 import sinon from 'sinon';
 import { waitFor } from '@testing-library/dom';
-import { getData, GET_DATA, GET_DATA_SUCCESS } from '../../actions';
+import {
+  getData,
+  GET_DATA,
+  GET_DATA_SUCCESS,
+  FETCH_PERSONAL_INFO,
+  FETCH_PERSONAL_INFO_SUCCESS,
+  FETCH_PERSONAL_INFO_FAILED,
+  fetchPersonalInfo,
+} from '../../actions';
 
 const mockData = { user: 'user' };
 describe('getData, creator', () => {
@@ -23,6 +32,34 @@ describe('getData, creator', () => {
     clock.tick(1000);
     await waitFor(() => {
       expect(mockDispatch.calledWith(seconfAction)).to.be.false;
+    });
+  });
+  describe('fetchPersonalInfo', () => {
+    let dispatch;
+    let apiRequestStub;
+    beforeEach(() => {
+      dispatch = sinon.stub();
+      apiRequestStub = sinon.stub(apiModule, 'apiRequest');
+    });
+    afterEach(() => {
+      apiRequestStub.restore();
+    });
+    it('should FETCH_PERSONAL_INFO and FETCH_PERSONAL_INFO_SUCCESS when api call is successful', async () => {
+      const response = { data: 'test data' };
+      apiRequestStub.resolves(response);
+      await fetchPersonalInfo()(dispatch);
+      expect(dispatch.calledWith({ type: FETCH_PERSONAL_INFO })).to.be.true;
+      expect(
+        dispatch.calledWith({ type: FETCH_PERSONAL_INFO_SUCCESS, response }),
+      ).to.be.true;
+    });
+    it('should FETCH_PERSONAL_INFO and FETCH_PERSONAL_INFO_FAILED when api call is successful', async () => {
+      const errors = { erros: 'some error' };
+      apiRequestStub.rejects(errors);
+      await fetchPersonalInfo()(dispatch);
+      expect(dispatch.calledWith({ type: FETCH_PERSONAL_INFO })).to.be.true;
+      expect(dispatch.calledWith({ type: FETCH_PERSONAL_INFO_FAILED, errors }))
+        .to.be.true;
     });
   });
 });
