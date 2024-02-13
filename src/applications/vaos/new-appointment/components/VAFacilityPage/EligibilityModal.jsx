61c107e236aab3113f5a548beb0406713702b568
Ryan Shaw <ryan.shaw@adhocteam.us>
2024-02-12 21:50:54 -0800

    [55085] removes new appointment use of va online scheduling status improvement flag (#27709)
    * Removes vaOnlineSchedulingStatusImprovement from new appointment flow
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Removes vaOnlineSchedulingStatusImprovement from VAFacilityPage in new appointments flow
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Formatting
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Updates unit test
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    ---------
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/new-appointment/components/VAFacilityPage/EligibilityModal.jsx
+++ src/applications/vaos/new-appointment/components/VAFacilityPage/EligibilityModal.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { VaModal } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import PropTypes from 'prop-types';
 import { FETCH_STATUS } from '../../../utils/constants';
 import getEligibilityMessage from './getEligibilityMessage';
-import { selectFeatureStatusImprovement } from '../../../redux/selectors';
 
 export default function EligibilityModal({
   onClose,
@@ -12,14 +10,9 @@ export default function EligibilityModal({
   facilityDetails,
   typeOfCare,
 }) {
-  const featureStatusImprovement = useSelector(state =>
-    selectFeatureStatusImprovement(state),
-  );
-
   const { title, content } = getEligibilityMessage({
     eligibility,
     facilityDetails,
-    featureStatusImprovement,
     typeOfCare,
     includeFacilityContactInfo: true,
   });
