bcd793208fc3af404508aa1b3288f6a86be2ba24
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-02-13 01:04:58 -0500

    modifying user mock data to see if that causes the failed test pt 2

--- src/applications/mhv-landing-page/mocks/api/user/index.js
+++ src/applications/mhv-landing-page/mocks/api/user/index.js
@@ -192,6 +192,45 @@ const generateUser = ({ serviceProvider = 'idme', facilities, loa = 3 }) => {
 
 const noFacilityUser = generateUserWithFacilities({ facilities: [] });
 
+const mhvTermsNotAcceptedUser = {
+  ...defaultUser,
+  data: {
+    ...defaultUser.data,
+    attributes: {
+      ...defaultUser.data.attributes,
+      profile: {
+        ...defaultUser.data.attributes.profile,
+        mhvAccount: {
+          accountLevel: null,
+          accountState: null,
+          errors: null,
+          loading: false,
+          termsAndConditionsAccepted: false,
+        },
+      },
+    },
+  },
+};
+
+const generateUserWithMHVTermsAccepted = termsAccepted => {
+  return {
+    ...defaultUser,
+    data: {
+      ...defaultUser.data,
+      attributes: {
+        ...defaultUser.data.attributes,
+        profile: {
+          ...defaultUser.data.attributes.profile,
+          mhvAccount: {
+            ...defaultUser.data.attributes.profile.mhvAccount,
+            termsAndConditionsAccepted: termsAccepted,
+          },
+        },
+      },
+    },
+  };
+};
+
 module.exports = {
   defaultUser,
   cernerUser,
@@ -199,4 +238,6 @@ module.exports = {
   generateUser,
   generateUserWithServiceProvider,
   generateUserWithFacilities,
+  mhvTermsNotAcceptedUser,
+  generateUserWithMHVTermsAccepted,
 };
