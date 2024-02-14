e801116fdde6484d7dc2cf1b1339922ed6a23c90
Jennie McGibney <jennie.mcgibney@va.gov>
2024-02-13 21:41:10 -0600

    Fix margin

--- src/applications/vaos/components/Breadcrumbs.jsx
+++ src/applications/vaos/components/Breadcrumbs.jsx
@@ -108,7 +108,7 @@ export default function VAOSBreadcrumbs({ children }) {
       <VaBreadcrumbs
         role="navigation"
         aria-label="Breadcrumbs"
-        class="vaos-hide-for-print"
+        class="vaos-hide-for-print xsmall-screen:vads-u-margin-bottom--0 small-screen:vads-u-margin-bottom--1 medium-screen:vads-u-margin-bottom--2"
         breadcrumbList={getBreadcrumbList()}
         uswds
       >
