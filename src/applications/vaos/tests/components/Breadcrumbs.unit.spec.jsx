90f145702d2cd30ce10dcfaa2d2c8892420a1fe7
Jennie McGibney <jennie.mcgibney@va.gov>
2024-02-14 21:31:47 -0600

    Fix unit test

--- src/applications/vaos/tests/components/Breadcrumbs.unit.spec.jsx
+++ src/applications/vaos/tests/components/Breadcrumbs.unit.spec.jsx
@@ -4,19 +4,20 @@ import { renderWithStoreAndRouter } from '../mocks/setup';
 import Breadcrumbs from '../../components/Breadcrumbs';
 
 describe('VAOS <Breadcrumbs>', () => {
+  const initialState = {
+    featureToggles: {
+      vaOnlineSchedulingBreadcrumbUrlUpdate: true,
+    },
+  };
+
   it('should display Pending appointments as last crumb', () => {
     // Given the path is on pending
     const url = 'my-health/appointments/pending';
-    const initialState = {};
-
     // when web component  is rendered
-    const screen = renderWithStoreAndRouter(
-      <Breadcrumbs breadcrumbList={Breadcrumbs} />,
-      {
-        initialState,
-        path: url,
-      },
-    );
+    const screen = renderWithStoreAndRouter(<Breadcrumbs />, {
+      initialState,
+      path: url,
+    });
     const navigation = screen.getByRole('navigation', { name: 'Breadcrumbs' });
     expect(navigation).to.exist;
     const crumb =
@@ -28,59 +29,46 @@ describe('VAOS <Breadcrumbs>', () => {
   it('should display text in the breadcrumb when path is type-of-care', () => {
     // Given the path is on type of care
     const url = '/schedule/type-of-care';
-    const initialState = {};
     // when web component is rendered
-    const screen = renderWithStoreAndRouter(
-      <Breadcrumbs breadcrumbList={Breadcrumbs} />,
-      {
-        initialState,
-        path: url,
-      },
-    );
+    const screen = renderWithStoreAndRouter(<Breadcrumbs />, {
+      initialState,
+      path: url,
+    });
     const navigation = screen.getByRole('navigation', { name: 'Breadcrumbs' });
     expect(navigation).to.exist;
     const crumb =
       navigation.breadcrumbList[navigation.breadcrumbList.length - 1].label;
     // it will display the last crumb
-    expect(crumb[0]).to.equal('Choose the type of care you need');
+    expect(crumb).to.equal('Choose the type of care you need');
   });
 
   it('should display Review in the breadcrumb when path is review page', () => {
     // Given the path is on review
     const url = '/schedule/review';
-    const initialState = {};
     // when web component is rendered
-    const screen = renderWithStoreAndRouter(
-      <Breadcrumbs breadcrumbList={Breadcrumbs} />,
-      {
-        initialState,
-        path: url,
-      },
-    );
+    const screen = renderWithStoreAndRouter(<Breadcrumbs />, {
+      initialState,
+      path: url,
+    });
     const navigation = screen.getByRole('navigation', { name: 'Breadcrumbs' });
     expect(navigation).to.exist;
     const crumb =
       navigation.breadcrumbList[navigation.breadcrumbList.length - 1].label;
     // it will display the last crumb
-    expect(crumb[0]).to.equal('Review your appointment details');
+    expect(crumb).to.equal('Review your appointment details');
   });
 
   it('should display covid in the breadcrumb when path is covid-vaccine', () => {
     const url = '/schedule/covid-vaccine/';
-    const initialState = {};
-
-    const screen = renderWithStoreAndRouter(
-      <Breadcrumbs breadcrumbList={Breadcrumbs} />,
-      {
-        initialState,
-        path: url,
-      },
-    );
+    const screen = renderWithStoreAndRouter(<Breadcrumbs />, {
+      initialState,
+      path: url,
+    });
 
     const navigation = screen.getByRole('navigation', { name: 'Breadcrumbs' });
     expect(navigation).to.exist;
     const crumb =
       navigation.breadcrumbList[navigation.breadcrumbList.length - 1].label;
-    expect(crumb[0]).to.equal('COVID-19 vaccine appointment');
+    expect(crumb).to.equal('COVID-19 vaccine appointment');
   });
 });
