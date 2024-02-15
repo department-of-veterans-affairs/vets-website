bdbc13c150046e3e9f2037eef4bdf8a393d7a5e7
Matthew Wright <matwright2010@gmail.com>
2024-02-15 06:41:06 -0700

    MHV-52721: name fixed

--- src/applications/mhv/shared/hooks/usePrintTitle.jsx
+++ src/applications/mhv/shared/hooks/usePrintTitle.jsx
@@ -10,11 +10,9 @@ const usePrintTitle = (
   useEffect(
     () => {
       const { first, last, suffix } = userDetails;
-      const name = first
-        ? `${first} ${last} ${suffix}`.replace(/undefined/g, '').trim()
-        : 'John Doe Jr.';
-      let pageTitle = `${baseTitle} | ${name}`;
-      if (dob) pageTitle += ` | ${dateFormat(new Date(dob))}`;
+      let name = `${first} ${last}`.replace(/undefined/g, '').trim();
+      if (suffix) name += ` ${suffix}`;
+      const pageTitle = `${baseTitle} | ${name} | ${dateFormat(new Date(dob))}`;
 
       const beforePrintHandler = () => {
         updatePageTitle(pageTitle);
