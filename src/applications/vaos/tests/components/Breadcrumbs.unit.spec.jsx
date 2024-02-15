fc8476aa2ba3274e9ff0699fa7c92dfc464d5abb
Ryan Shaw <ryan.shaw@adhocteam.us>
2024-02-15 12:58:45 -0800

    Removes use of vaOnlineSchedulingBreadcrumbUrlUpdate feature flag within appointments breadcrumb component (#27973)
    * Removes selectFeatureBreadcrumbUrlUpdate from breadcrumb component
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Updates unit test
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Removes unused unit tests for vaOnlineSchedulingBreadcrumbUrlUpdate feature flag
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Removes unused unit tests for vaOnlineSchedulingBreadcrumbUrlUpdate feature flag
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Updates unit test
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Updates unit test
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    * Updates unit test
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>
    
    ---------
    
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/tests/components/Breadcrumbs.unit.spec.jsx
+++ src/applications/vaos/tests/components/Breadcrumbs.unit.spec.jsx
@@ -4,26 +4,7 @@ import { renderWithStoreAndRouter } from '../mocks/setup';
 import Breadcrumbs from '../../components/Breadcrumbs';
 
 describe('VAOS <Breadcrumbs>', () => {
-  it('should display the text within the breadcrumb', () => {
-    const initialState = {
-      featureToggles: { vaOnlineSchedulingBreadcrumbUrlUpdate: false },
-    }; // eslint-disable-next-line camelcase
-
-    const screen = renderWithStoreAndRouter(
-      <Breadcrumbs>
-        <a href="https://www.va.gov/">test</a>
-      </Breadcrumbs>,
-      { initialState },
-    );
-
-    expect(screen.getByText(/Health care/i)).to.be.ok;
-    expect(screen.getByText(/Schedule and manage health appointments/i)).to.be
-      .ok;
-    expect(screen.getByRole('link', { name: 'Appointments' })).to.be.ok;
-    expect(screen.getByText(/test/i)).to.be.ok;
-  });
-
-  it('should display new text within the breadcrumb when breadcrumb  toggle is on', () => {
+  it('should display new text within the breadcrumb', () => {
     const initialState = {
       featureToggles: { vaOnlineSchedulingBreadcrumbUrlUpdate: true },
     }; // eslint-disable-next-line camelcase
