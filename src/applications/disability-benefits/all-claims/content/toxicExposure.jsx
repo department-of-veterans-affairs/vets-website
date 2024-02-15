f330863cf825d842e63529a9f5660f836d7b6753
Christine <127446042+christinec-fftc@users.noreply.github.com>
2024-02-15 11:19:08 -0800

    73360 Move from checkbox widgets to web components

--- src/applications/disability-benefits/all-claims/content/toxicExposure.jsx
+++ src/applications/disability-benefits/all-claims/content/toxicExposure.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import get from '@department-of-veterans-affairs/platform-forms-system/get';
-import { isClaimingNew, showToxicExposurePages } from '../utils';
+import { checkboxGroupSchema } from '@department-of-veterans-affairs/platform-forms-system/web-component-patterns';
+import {
+  capitalizeEachWord,
+  isClaimingNew,
+  showToxicExposurePages,
+  sippableId,
+} from '../utils';
+import { NULL_CONDITION_STRING } from '../constants';
 
 /**
  * Checks if toggle is enabled and user is claiming at least one new condition for toxic exposure
@@ -18,7 +25,7 @@ export const conditionsQuestion =
   'Are any of your new conditions related to toxic exposure during your military service? Check any that are related.';
 export const conditionsDescription = (
   <va-additional-info
-    class="vads-u-margin-bottom--4"
+    class="vads-u-margin-top--2"
     trigger="What is toxic exposure?"
   >
     <div>
@@ -42,3 +49,80 @@ export const conditionsDescription = (
 export const gulfWar1990PageTitle = 'Service locations after August 2, 1990';
 export const gulfWar1990Question =
   'Did you serve in any of these Gulf War locations on or after August 2, 1990? Check any locations where you served.';
+
+/**
+ * Builds the Schema based on user entered condition names
+ * 
+ * Example output:
+{
+    type: 'object',
+    properties: {
+      anemia: {
+        type: 'boolean'
+      },
+      tinnitusringingorhissinginears: {
+        type: 'boolean'
+      },
+      none: {
+        type: 'boolean'
+      }
+    }
+  }
+}
+ *
+ * @param {*} formData Full formData for the form
+ * @returns {object} Object with id's for each condition
+ */
+export const makeTEConditionsSchema = formData => {
+  const options = (formData?.newDisabilities || []).map(disability =>
+    sippableId(disability.condition),
+  );
+
+  options.push('none');
+
+  return checkboxGroupSchema(options);
+};
+
+/**
+ * Builds the UI Schema based on user entered condition names.
+ *
+ * Example output:
+ *  {
+ *   anemia: {
+ *     'ui:title': 'Anemia',
+ *   },
+ *   tinnitusringingorhissinginears: {
+ *     'ui:title': 'Tinnitus (Ringing Or Hissing In Ears)',
+ *   },
+ *   none: {
+ *     'ui:title': 'I am not claiming any conditions related to toxic exposure',
+ *   },
+ * };
+ * @param {*} formData Full formData for the form
+ * @returns {object} Object with id and title for each condition
+ */
+export const makeTEConditionsUISchema = formData => {
+  const { newDisabilities } = formData;
+  const options = {};
+
+  if (newDisabilities) {
+    newDisabilities.forEach(disability => {
+      const { condition } = disability;
+
+      const capitalizedDisabilityName =
+        typeof condition === 'string'
+          ? capitalizeEachWord(condition)
+          : NULL_CONDITION_STRING;
+
+      options[sippableId(condition)] = {
+        'ui:title': capitalizedDisabilityName,
+      };
+    });
+  }
+
+  options.none = {
+    'ui:title': 'I am not claiming any conditions related to toxic exposure',
+  };
+
+  return options;
+};
