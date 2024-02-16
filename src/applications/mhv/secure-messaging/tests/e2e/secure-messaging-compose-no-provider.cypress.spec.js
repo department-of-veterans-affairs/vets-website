a31c573a3916949b10efcbb2b1e8291278a61398
fazilqa <127263470+fazilqa@users.noreply.github.com>
2024-02-16 14:32:40 -0800

    54797 remove blocked tg feature flag (#28042)
    * upd: remove blocked TG feature flag, fixed filed test
    
    * upd: remove created 'cy:sm:run' from package.json

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-compose-no-provider.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-compose-no-provider.cypress.spec.js
@@ -1,35 +1,31 @@
 import PatientInboxPage from './pages/PatientInboxPage';
 import SecureMessagingSite from './sm_site/SecureMessagingSite';
-import mockDraftMessage from '../fixtures/message-draft-response.json';
-import PatientComposePage from './pages/PatientComposePage';
-import PatientInterstitialPage from './pages/PatientInterstitialPage';
-import { AXE_CONTEXT, Paths } from './utils/constants';
+import { AXE_CONTEXT } from './utils/constants';
 
 describe('Secure Messaging Compose with No Provider', () => {
-  it('can send message', () => {
+  it('can not send message', () => {
     const landingPage = new PatientInboxPage();
-    const composePage = new PatientComposePage();
     const site = new SecureMessagingSite();
     site.login();
     landingPage.loadPageForNoProvider();
-    cy.get('[data-testid="compose-message-link"]').click({ force: true });
-    PatientInterstitialPage.getContinueButton().click({ force: true });
 
-    composePage.selectRecipient('');
-    composePage
-      .getCategory('COVID')
-      .first()
-      .click();
-    composePage.getMessageSubjectField().type('Test Subject');
-    composePage
-      .getMessageBodyField()
-      .type('Test message body', { force: true });
+    cy.get('[data-testid="blocked-triage-group-alert"]>h2').should(
+      'contain',
+      "You can't send messages to your care teams right now",
+    );
+    cy.contains(
+      'p',
+      'If you need to contact your care teams, call your VA health facility.',
+    ).should('be.visible');
 
-    cy.intercept('POST', Paths.SM_API_EXTENDED, mockDraftMessage).as('message');
-    cy.get('[data-testid="Send-Button"]')
-      .contains('Send')
-      .click();
-    composePage.verifySelectRecipientErrorMessage();
+    cy.get('[data-testid="blocked-triage-group-alert"] > div > a').should(
+      'have.attr',
+      'href',
+      '/find-locations/',
+    );
+
+    cy.get('[data-testid="compose-message-link"]').should('not.exist');
+
     cy.injectAxe();
     cy.axeCheck(AXE_CONTEXT, {
       rules: {
