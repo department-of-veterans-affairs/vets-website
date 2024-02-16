74002968a707fade707424f8308d96d339f35f7e
Tony Williams <tony.williams@va.gov>
2024-02-16 14:00:34 -0600

    change file extention to .js

---
+++ src/applications/vaos/tests/covid-19-vaccine/components/NewBookingPage.unit.spec.js
@@ -0,0 +1,172 @@
+import React from 'react';
+import moment from 'moment';
+import { expect } from 'chai';
+import {
+  mockFetch,
+  setFetchJSONResponse,
+} from '@department-of-veterans-affairs/platform-testing/helpers';
+import environment from '@department-of-veterans-affairs/platform-utilities/environment';
+import { createTestStore, renderWithStoreAndRouter } from '../../mocks/setup';
+import { NewBookingSection } from '../../../covid-19-vaccine';
+import { mockFacilitiesFetchByVersion } from '../../mocks/fetch';
+import { createMockFacilityByVersion } from '../../mocks/data';
+import { mockSchedulingConfigurations } from '../../mocks/helpers.v2';
+import { getSchedulingConfigurationMock } from '../../mocks/v2';
+
+const initialState = {
+  featureToggles: {
+    vaOnlineSchedulingDirect: true,
+  },
+  user: {
+    profile: {
+      facilities: [
+        { facilityId: '983', isCerner: false },
+        { facilityId: '984', isCerner: false },
+      ],
+    },
+  },
+};
+
+describe('VAOS vaccine flow <NewBookingSection>', () => {
+  beforeEach(() => {
+    mockFetch();
+  });
+
+  it('should not redirect the user to the Contact Facility page when facilities are available', async () => {
+    const store = createTestStore({
+      ...initialState,
+    });
+
+    mockFacilitiesFetchByVersion({
+      children: true,
+      facilities: [
+        createMockFacilityByVersion({
+          id: '983',
+          name: 'A facility',
+        }),
+        createMockFacilityByVersion({
+          id: '984',
+          name: 'B facility',
+        }),
+      ],
+    });
+    mockSchedulingConfigurations([
+      getSchedulingConfigurationMock({
+        id: '983',
+        typeOfCareId: 'covid',
+        requestEnabled: true,
+        directEnabled: true,
+      }),
+      getSchedulingConfigurationMock({
+        id: '984',
+        typeOfCareId: 'primaryCare',
+        requestEnabled: true,
+      }),
+    ]);
+
+    const screen = renderWithStoreAndRouter(<NewBookingSection />, {
+      store,
+    });
+
+    await screen.findByRole('heading', {
+      level: 1,
+      name: 'COVID-19 vaccine appointment',
+    });
+  });
+
+  it('should redirect the user to the Contact Facility page when facilities are not available', async () => {
+    const store = createTestStore({
+      ...initialState,
+    });
+
+    mockFacilitiesFetchByVersion({
+      children: true,
+      facilities: [
+        createMockFacilityByVersion({
+          id: '983',
+          name: 'Facility that is enabled',
+          lat: 39.1362562,
+          long: -83.1804804,
+          address: {
+            city: 'Bozeman',
+            state: 'MT',
+          },
+          phone: '5555555555x1234',
+        }),
+        createMockFacilityByVersion({
+          id: '984',
+          name: 'Facility 2',
+        }),
+      ],
+    });
+    mockSchedulingConfigurations([
+      getSchedulingConfigurationMock({
+        id: '983',
+        typeOfCareId: 'covid',
+        requestEnabled: true,
+      }),
+      getSchedulingConfigurationMock({
+        id: '984',
+        typeOfCareId: 'primaryCare',
+        requestEnabled: true,
+      }),
+    ]);
+
+    const screen = renderWithStoreAndRouter(<NewBookingSection />, {
+      store,
+    });
+
+    expect(
+      await screen.findByText(/Contact one of your registered VA facilities/i),
+    ).to.be.ok;
+  });
+
+  it('should render warning message', async () => {
+    setFetchJSONResponse(
+      global.fetch.withArgs(`${environment.API_URL}/v0/maintenance_windows/`),
+      {
+        data: [
+          {
+            id: '139',
+            type: 'maintenance_windows',
+            attributes: {
+              externalService: 'vaosWarning',
+              description: 'My description',
+              startTime: moment.utc().subtract('1', 'days'),
+              endTime: moment.utc().add('1', 'days'),
+            },
+          },
+        ],
+      },
+    );
+    const store = createTestStore(initialState);
+    const screen = renderWithStoreAndRouter(<NewBookingSection />, {
+      store,
+      basename: '/new-covid-19-vaccine-appointment',
+    });
+
+    expect(
+      await screen.findByRole('heading', {
+        level: 3,
+        name: /You may have trouble using the VA appointments tool right now/,
+      }),
+    ).to.exist;
+  });
+
+  it('should show error when facility availability check fails', async () => {
+    const store = createTestStore({
+      ...initialState,
+    });
+
+    const screen = renderWithStoreAndRouter(<NewBookingSection />, {
+      store,
+    });
+
+    expect(
+      await screen.findByRole('heading', {
+        level: 1,
+        name: /We’re sorry. We’ve run into a problem/,
+      }),
+    ).to.exist;
+  });
+});
---
+++ src/applications/vaos/tests/covid-19-vaccine/components/SecondDosePage.unit.spec.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import moment from 'moment';
+import { expect } from 'chai';
+import userEvent from '@testing-library/user-event';
+
+import { waitFor } from '@testing-library/dom';
+import { createTestStore, renderWithStoreAndRouter } from '../../mocks/setup';
+
+import SecondDosePage from '../../../covid-19-vaccine/components/SecondDosePage';
+
+const initialState = {
+  featureToggles: {
+    vaOnlineSchedulingCancel: true,
+  },
+};
+
+describe('VAOS vaccine flow <SecondDosePage>', () => {
+  const start = moment();
+  const store = createTestStore({
+    ...initialState,
+    covid19Vaccine: {
+      newBooking: {
+        previousPages: {},
+        data: {
+          vaFacility: 'var983',
+          clinicId: '455',
+          date1: [start.format()],
+        },
+        availableSlots: [
+          {
+            start: start.format(),
+            end: start
+              .clone()
+              .add(30, 'minutes')
+              .format(),
+          },
+        ],
+        clinics: {},
+        facilities: [
+          {
+            id: 'var983',
+            name: 'Cheyenne VA Medical Center',
+          },
+        ],
+      },
+    },
+  });
+
+  it('should show second vaccine dose information', async () => {
+    const screen = renderWithStoreAndRouter(<SecondDosePage />, {
+      store,
+    });
+
+    expect(
+      await screen.getByText(/When to plan for a second dose/i),
+    ).to.have.tagName('h1');
+
+    expect(
+      screen.getByText(
+        new RegExp(`If you get your first dose of a 2-dose vaccine on`, 'i'),
+      ),
+    ).to.be.ok;
+    expect(
+      screen.getByText(
+        new RegExp(`${start.clone().format('dddd, MMMM DD, YYYY')}`, 'i'),
+      ),
+    ).to.be.ok;
+    expect(
+      screen.getByText(
+        new RegExp(
+          `, here’s when to plan to come back for your second dose`,
+          'i',
+        ),
+      ),
+    ).to.be.ok;
+    expect(screen.getByText('Moderna')).to.have.tagName('h2');
+    expect(screen.getByText('Pfizer')).to.have.tagName('h2');
+    expect(screen.getByText('Johnson & Johnson')).to.have.tagName('h2');
+    expect(
+      screen.getByText(
+        new RegExp(
+          `after ${start
+            .clone()
+            .add(21, 'days')
+            .format('dddd, MMMM DD, YYYY')}`,
+          'i',
+        ),
+      ),
+    ).to.be.ok;
+    expect(
+      screen.getByText(
+        new RegExp(
+          `after ${start
+            .clone()
+            .add(28, 'days')
+            .format('dddd, MMMM DD, YYYY')}`,
+          'i',
+        ),
+      ),
+    ).to.be.ok;
+  });
+
+  it('should continue to the correct page once continue to clicked', async () => {
+    const screen = renderWithStoreAndRouter(<SecondDosePage />, {
+      store,
+    });
+
+    const button = await screen.findByText(/Continue/i);
+
+    userEvent.click(button);
+
+    expect(screen.history.push.called).to.be.true;
+
+    await waitFor(() =>
+      expect(screen.history.push.firstCall.args[0]).to.equal(
+        '/new-covid-19-vaccine-appointment/contact-info',
+      ),
+    );
+  });
+});
