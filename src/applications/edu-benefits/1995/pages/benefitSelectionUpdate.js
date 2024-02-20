cff1a4e9a09fe42fe77e9b0ea0f65081f3c66a57
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2024-02-20 14:09:55 -0600

    VFEP-951 Add conditional benefitAppliedFor when changAnotherBenefit is Yes. (#28012)
    * VFEP-951 Add conditional benefitAppliedFor when changAnotherBenefit is Yes.
    
    * VFEP-951 update package.json to point to latest head of vets-json-schema

--- src/applications/edu-benefits/1995/pages/benefitSelectionUpdate.js
+++ src/applications/edu-benefits/1995/pages/benefitSelectionUpdate.js
@@ -2,14 +2,23 @@ import fullSchema from 'vets-json-schema/dist/22-1995-schema.json';
 
 import { benefitsLabelsUpdate } from '../../utils/labels';
 
-const { benefitUpdate, changeAnotherBenefit } = fullSchema.properties;
+const {
+  benefitUpdate,
+  changeAnotherBenefit,
+  benefitAppliedFor,
+} = fullSchema.properties;
 
 const displayBenefit = {
   ...benefitUpdate,
   enum: [...benefitUpdate.enum],
 };
 
-/* 
+const displayNewBenefit = {
+  ...benefitAppliedFor,
+  enum: [...benefitAppliedFor.enum],
+};
+
+/*
   the schema has post9/11 listed as chapter33Post911
   and fry scholarship listed as chapter33FryScholarship
   In order to use the benefitsLabelsUpdate import we rename
@@ -17,14 +26,16 @@ const displayBenefit = {
   and chapter33FryScholarship to fryScholarship
 
   Once the applicant submits the form, the function
-  fryScholarshipTransform() located in the 
+  fryScholarshipTransform() located in the
   submit-transform.js file runs.
   This changes back chapter33 to chapter33Post911
   and changes fryScholarship to chapter33FryScholarship
-  to align with the values listed in the JSON Schema 
+  to align with the values listed in the JSON Schema
 */
 displayBenefit.enum.splice(0, 1, 'chapter33');
 displayBenefit.enum.splice(1, 1, 'fryScholarship');
+displayNewBenefit.enum.splice(0, 1, 'chapter33');
+displayNewBenefit.enum.splice(1, 1, 'fryScholarship');
 
 export const uiSchema = {
   benefitUpdate: {
@@ -38,15 +49,12 @@ export const uiSchema = {
     'ui:title': 'Do you want to change to another benefit?',
     'ui:widget': 'radio',
   },
-};
-
-export const oldUiSchema = {
-  benefitUpdate: {
+  benefitAppliedFor: {
     'ui:widget': 'radio',
-    'ui:title':
-      'Which benefit are you currently using or have you used most recently?',
+    'ui:title': 'Which benefit do you want to change to?',
     'ui:options': {
       labels: benefitsLabelsUpdate,
+      hideIf: formData => formData.changeAnotherBenefit !== 'Yes',
     },
   },
 };
@@ -56,5 +64,6 @@ export const schema = {
   properties: {
     benefitUpdate: displayBenefit,
     changeAnotherBenefit,
+    benefitAppliedFor: displayNewBenefit,
   },
 };
