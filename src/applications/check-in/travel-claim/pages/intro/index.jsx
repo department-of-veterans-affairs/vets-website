875feaf81d5cb9b8adee6005e062f145b6571f2e
Brian Seek <brian.seek@civicactions.com>
2024-02-20 15:36:20 -0800

    first pass at intro page.

--- src/applications/check-in/travel-claim/pages/intro/index.jsx
+++ src/applications/check-in/travel-claim/pages/intro/index.jsx
@@ -1,26 +1,36 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
+import { recordEvent } from '@department-of-veterans-affairs/platform-monitoring/exports';
 
 import TravelIntroDisplay from '../../../components/pages/TravelIntro/TravelIntroDisplay';
-
+import { createAnalyticsSlug } from '../../../utils/analytics';
 import { useFormRouting } from '../../../hooks/useFormRouting';
 
 const TravelIntro = props => {
   const { router } = props;
   const { t } = useTranslation();
 
-  const { goToNextPage } = useFormRouting(router);
-
-  const buttonClick = useCallback(
-    () => {
-      goToNextPage();
+  const { jumpToPage } = useFormRouting(router);
+  // @TODO logic need to figure out if we should go to multi-facility page or single
+  const fileClaimClick = useCallback(
+    e => {
+      if (e?.key && e.key !== ' ') {
+        return;
+      }
+      recordEvent({
+        event: createAnalyticsSlug('file-travel-clicked', 'nav'),
+      });
+      e.preventDefault();
+      jumpToPage('select-appointment');
     },
-    [goToNextPage],
+    [jumpToPage],
   );
-
   return (
-    <TravelIntroDisplay header={t('fpo-header')} buttonClick={buttonClick} />
+    <TravelIntroDisplay
+      header={t('file-a-travel-reimbursement-claim')}
+      fileClaimClick={fileClaimClick}
+    />
   );
 };
 
