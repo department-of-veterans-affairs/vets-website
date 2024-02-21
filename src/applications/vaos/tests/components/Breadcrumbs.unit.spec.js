3456eed42744dd6af00e6b068fb2105a345e4e58
Jennie McGibney <jennie.mcgibney@va.gov>
2024-02-21 14:27:08 -0600

    fix unit test

--- src/applications/vaos/tests/components/Breadcrumbs.unit.spec.js
+++ src/applications/vaos/tests/components/Breadcrumbs.unit.spec.js
@@ -22,8 +22,8 @@ describe('VAOS Component: Breadcrumbs', () => {
     expect(crumb).to.equal('Pending appointments');
   });
 
-  it('should display text in the breadcrumb when path is type-of-care', () => {
-    const url = 'my-health/appointments/schedule/type-of-care';
+  it('should display Past appointments as last crumb', () => {
+    const url = 'my-health/appointments/past';
     const screen = renderWithStoreAndRouter(<Breadcrumbs />, {
       initialState,
       path: url,
@@ -32,33 +32,6 @@ describe('VAOS Component: Breadcrumbs', () => {
     expect(navigation).to.exist;
     const crumb =
       navigation.breadcrumbList[navigation.breadcrumbList.length - 1].label;
-    expect(crumb).to.equal('Choose the type of care you need');
-  });
-
-  it('should display Review in the breadcrumb when path is review page', () => {
-    const url = 'my-health/appointments/schedule/review';
-    const screen = renderWithStoreAndRouter(<Breadcrumbs />, {
-      initialState,
-      path: url,
-    });
-    const navigation = screen.getByRole('navigation', { name: 'Breadcrumbs' });
-    expect(navigation).to.exist;
-    const crumb =
-      navigation.breadcrumbList[navigation.breadcrumbList.length - 1].label;
-    expect(crumb).to.equal('Review your appointment details');
-  });
-
-  it('should display covid in the breadcrumb when path is covid-vaccine', () => {
-    const url = 'my-health/appointments/schedule/covid-vaccine/';
-    const screen = renderWithStoreAndRouter(<Breadcrumbs />, {
-      initialState,
-      path: url,
-    });
-
-    const navigation = screen.getByRole('navigation', { name: 'Breadcrumbs' });
-    expect(navigation).to.exist;
-    const crumb =
-      navigation.breadcrumbList[navigation.breadcrumbList.length - 1].label;
-    expect(crumb).to.equal('COVID-19 vaccine appointment');
+    expect(crumb).to.equal('Past appointments');
   });
 });
