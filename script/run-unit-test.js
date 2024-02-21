a46467b54d432c5b93a7cd60f074f5b8b2269b0c
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2024-02-21 14:21:39 -0500

    Try different matrix strategy

--- script/run-unit-test.js
+++ script/run-unit-test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const commandLineArgs = require('command-line-args');
 const glob = require('glob');
+const path = require('path');
 const printUnitTestHelp = require('./run-unit-test-help');
 const { runCommand } = require('./utils');
 // For usage instructions see https://github.com/department-of-veterans-affairs/vets-website#unit-tests
@@ -8,7 +9,7 @@ const { runCommand } = require('./utils');
 const specDirs = '{src,script}';
 const defaultPath = `./${specDirs}/**/*.unit.spec.js?(x)`;
 const numContainers = process.env.NUM_CONTAINERS || 10;
-const matrixStep = process.env.STEP || 1;
+const matrixStep = process.env.STEP || 0;
 
 const COMMAND_LINE_OPTIONS_DEFINITIONS = [
   { name: 'log-level', type: String, defaultValue: 'log' },
@@ -26,8 +27,20 @@ const COMMAND_LINE_OPTIONS_DEFINITIONS = [
     defaultValue: [defaultPath],
   },
 ];
-
 const allUnitTests = glob.sync(defaultPath);
+const allUnitTestDirs = Array.from(
+  new Set(
+    allUnitTests.map(spec =>
+      JSON.stringify(
+        path
+          .dirname(spec)
+          .split('/')
+          .slice(1, 4),
+      ),
+    ),
+  ),
+).filter(spec => spec !== undefined);
+
 function splitArray(array, chunks) {
   const [...arrayCopy] = array;
   const arrayChunks = [];
@@ -78,22 +91,31 @@ if (process.env.TESTS_TO_VERIFY) {
 }
 
 const splitUnitTests = splitArray(
-  allUnitTests,
+  allUnitTestDirs,
   Math.ceil(allUnitTests.length / numContainers),
 );
-console.log('app folder selected: ', options['app-folder']);
-const testsToRun = options['app-folder']
-  ? `--recursive ${options.path.map(p => `'${p}'`).join(' ')}`
-  : splitUnitTests[matrixStep].join(' ');
-const command = `LOG_LEVEL=${options[
-  'log-level'
-].toLowerCase()} ${testRunner} --max-old-space-size=8192 --config ${configFile} ${testsToVerify ||
-  testsToRun} `;
+console.log(splitUnitTests[matrixStep].length);
+for (const dir of splitUnitTests[matrixStep]) {
+  const testsToRun = options['app-folder']
+    ? `--recursive ${options.path.map(p => `'${p}'`).join(' ')}`
+    : `--recursive ${options.path[0].replace(
+        `/${specDirs}/`,
+        `/${JSON.parse(dir).join('/')}/`,
+      )}`;
+
+  const command = `LOG_LEVEL=${options[
+    'log-level'
+  ].toLowerCase()} ${testRunner} --max-old-space-size=8192 --config ${configFile} ${testsToVerify ||
+    testsToRun} `;
+  runCommand(command);
+}
+// const command = `LOG_LEVEL=${options[
+//   'log-level'
+// ].toLowerCase()} ${testRunner} --max-old-space-size=8192 --config ${configFile} ${testsToVerify ||
+//   testsToRun} `;
 // const command = `LOG_LEVEL=${options[
 //   'log-level'
 // ].toLowerCase()} ${testRunner} --max-old-space-size=8192 --config ${configFile} ${testsToVerify ||
 //   `--recursive ${options.path.map(p => `'${p}'`).join(' ')}`} `;
 
-console.log(command);
-
-runCommand(command);
+// console.log(command);
