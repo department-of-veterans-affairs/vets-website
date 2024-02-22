1bb569edfe4acb75302029f846e0c0be2506c934
cohnjesse <cohnjesse@gmail.com>
2024-02-22 13:07:22 -0500

    Added permissions page

---
+++ src/applications/representatives/containers/PermissionsPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import LoginViewWrapper from './LoginViewWrapper';
+
+import { POAbreadcrumbs } from '../common/breadcrumbs';
+
+const PermissionsPage = () => {
+  return (
+    <LoginViewWrapper breadcrumbs={POAbreadcrumbs}>
+      <h1>Permissions</h1>
+      <va-button onClick={function noRefCheck() {}} text="Add" secondary />
+      <va-button
+        onClick={function noRefCheck() {}}
+        text="Upload CSV"
+        secondary
+      />
+      <div className="placeholder-container">
+        <div className="vads-u-background-color--gray-lightest vads-u-margin-bottom--2 vads-u-height--viewport" />
+      </div>
+    </LoginViewWrapper>
+  );
+};
+
+export default PermissionsPage;
