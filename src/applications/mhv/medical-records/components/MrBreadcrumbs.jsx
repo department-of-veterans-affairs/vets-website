b2536ab41ffa55fc2da99eecdcd496ce63bfb875
Mike Moyer <michael.moyer@va.gov>
2024-02-22 09:53:14 -0500

    MHV-53637 Updated breadcrumbs to match UCD designs

--- src/applications/mhv/medical-records/components/MrBreadcrumbs.jsx
+++ src/applications/mhv/medical-records/components/MrBreadcrumbs.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { VaBreadcrumbs } from '@department-of-veterans-affairs/web-components/react-bindings';
-// import { replaceWithStagingDomain } from '~/platform/utilities/environment/stagingDomains';
+import { Link } from 'react-router-dom';
 
 const MrBreadcrumbs = () => {
   const crumbs = useSelector(state => state.mr.breadcrumbs.list);
-  const currentPath = useSelector(state => state.mr.breadcrumbs.location);
 
   return (
     <>
       {crumbs.length > 0 && crumbs[0]?.url ? (
         <div
-          className="vads-l-row breadcrumbs-container no-print"
+          className="vads-l-row vads-u-padding-y--1 breadcrumbs-container no-print"
           data-testid="breadcrumbs"
         >
-          <VaBreadcrumbs label="Breadcrumb" mobileFirstProp>
-            {crumbs.map((crumb, idx) => (
-              <a href={crumb.url} key={idx}>
-                Back to {crumb.label}
-              </a>
-            ))}
-            <a href={currentPath?.url}>{currentPath?.label}</a>
-          </VaBreadcrumbs>
+          <va-breadcrumbs label="Breadcrumb">
+            <div>
+              <span className="breadcrumb-angle vads-u-padding-right--1">
+                {'\u2039'}{' '}
+              </span>
+              <Link to={crumbs[0].url?.toLowerCase()}>
+                Back to {crumbs[0].label?.toLowerCase()}
+              </Link>
+            </div>
+          </va-breadcrumbs>
         </div>
       ) : (
         <div
