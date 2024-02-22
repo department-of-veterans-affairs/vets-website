d87f5f28fde979dbf799deccf4c168f999d61163
Carlos <carlos.felix@agile6.com>
2024-02-22 15:37:56 -0500

    Added e2e test

--- src/applications/mhv/medications/tests/e2e/medications-landing-page.cypress.spec.js
+++ src/applications/mhv/medications/tests/e2e/medications-landing-page.cypress.spec.js
@@ -1,5 +1,7 @@
+import { notFoundHeading } from '@department-of-veterans-affairs/platform-site-wide/PageNotFound';
 import MedicationsSite from './med_site/MedicationsSite';
 import MedicationsLandingPage from './pages/MedicationsLandingPage';
+import { rootUrl } from '../../manifest.json';
 
 describe('Medications Landing Page', () => {
   it('visits Medications landing Page', () => {
@@ -10,5 +12,14 @@ describe('Medications Landing Page', () => {
     landingPage.visitLandingPageURL();
     cy.injectAxe();
     cy.axeCheck('main');
+  });
+
+  it('Visit unsupported URL and get a page not found', () => {
+    const site = new MedicationsSite();
+    site.login();
+
+    cy.visit(`${rootUrl}/dummy/dummy`);
+    cy.injectAxeThenAxeCheck();
+    cy.findByRole('heading', { name: notFoundHeading }).should.exist;
   });
 });
