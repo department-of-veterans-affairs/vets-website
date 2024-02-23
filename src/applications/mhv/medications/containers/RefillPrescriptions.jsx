980600b5080a662332059e7fb06f3a66cecc0871
agravell047 <anthony.gravell@bylight.com>
2024-02-23 17:14:19 -0500

    added pagination

--- src/applications/mhv/medications/containers/RefillPrescriptions.jsx
+++ src/applications/mhv/medications/containers/RefillPrescriptions.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { VaButton } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import {
+  VaButton,
+  VaPagination,
+} from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import PropTypes from 'prop-types';
 import { updatePageTitle } from '../../shared/util/helpers';
 import { dateFormat } from '../util/helpers';
@@ -77,6 +80,27 @@ const RefillPrescriptions = ({ refillList = [], isLoadingList = true }) => {
     );
     dispatch(setPrescriptionDetails(rx));
   };
+
+  // Pagination
+  const MAX_PAGE_LIST_LENGTH = 20;
+  const [pagination, setPagination] = useState({
+    currentPage: 1,
+    totalPages: Math.ceil(fullRefillList.length / MAX_PAGE_LIST_LENGTH),
+  });
+
+  const onPageChange = page => {
+    setPagination(prevState => ({
+      ...prevState,
+      currentPage: page,
+    }));
+  };
+
+  const startIdx = (pagination.currentPage - 1) * MAX_PAGE_LIST_LENGTH;
+  const endIdx = pagination.currentPage * MAX_PAGE_LIST_LENGTH;
+  const paginatedRenewablePrescriptions = renewablePrescriptionsList.slice(
+    startIdx,
+    endIdx,
+  );
 
   useEffect(
     () => {
@@ -258,32 +282,48 @@ const RefillPrescriptions = ({ refillList = [], isLoadingList = true }) => {
           </p>
           <div className="no-print rx-page-total-info vads-u-border-bottom--2px vads-u-border-color--gray-lighter" />
           <div>
-            {renewablePrescriptionsList.slice().map((prescription, idx) => (
-              <div key={idx}>
-                <h4 className="vads-u-margin-top--5 vads-u-margin-bottom--0">
-                  <Link
-                    data-testid={`medication-details-page-link-${idx}`}
-                    to={`/prescription/${prescription.prescriptionId}`}
-                    onClick={() => onRxLinkClick(prescription)}
-                  >
-                    {prescription.prescriptionName}
-                  </Link>
-                </h4>
-                <p className="vads-u-margin-top--0">
-                  Prescription number: {prescription.prescriptionNumber}
-                  <br />
-                  <span data-testid={`renew-last-filled-${idx}`}>
-                    Last filled on:{' '}
-                    {dateFormat(
-                      prescription.rxRfRecords?.[0]?.[1]?.find(
-                        record => record.dispensedDate,
-                      )?.dispensedDate || prescription.dispensedDate,
-                      'MMMM D, YYYY',
-                    )}
-                  </span>
-                </p>
-              </div>
-            ))}
+            {paginatedRenewablePrescriptions
+              .slice()
+              .map((prescription, idx) => (
+                <div key={idx}>
+                  <h4 className="vads-u-margin-top--5 vads-u-margin-bottom--0">
+                    <Link
+                      data-testid={`medication-details-page-link-${idx}`}
+                      to={`/prescription/${prescription.prescriptionId}`}
+                      onClick={() => onRxLinkClick(prescription)}
+                    >
+                      {prescription.prescriptionName}
+                    </Link>
+                  </h4>
+                  <p className="vads-u-margin-top--0">
+                    Prescription number: {prescription.prescriptionNumber}
+                    <br />
+                    <span data-testid={`renew-last-filled-${idx}`}>
+                      Last filled on:{' '}
+                      {dateFormat(
+                        prescription.rxRfRecords?.[0]?.[1]?.find(
+                          record => record.dispensedDate,
+                        )?.dispensedDate || prescription.dispensedDate,
+                        'MMMM D, YYYY',
+                      )}
+                    </span>
+                  </p>
+                </div>
+              ))}
+            <div className="refill-pagination-container">
+              {fullRefillList.length > 20 && (
+                <VaPagination
+                  max-page-list-length={MAX_PAGE_LIST_LENGTH}
+                  id="pagination"
+                  className="vads-u-justify-content--center no-print"
+                  onPageSelect={e => onPageChange(e.detail.page)}
+                  page={pagination.currentPage}
+                  pages={pagination.totalPages}
+                  unbounded
+                  uswds
+                />
+              )}
+            </div>
           </div>
         </div>
       </div>
