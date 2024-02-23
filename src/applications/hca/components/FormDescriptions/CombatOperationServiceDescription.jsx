e822749a17a228905a2f2e54e90c16d2b3d15e5d
Matt Long <longmd@me.com>
2024-02-23 15:10:50 -0500

    Cleanup description components

--- src/applications/hca/components/FormDescriptions/CombatOperationServiceDescription.jsx
+++ src/applications/hca/components/FormDescriptions/CombatOperationServiceDescription.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 const CombatOperationServiceDescription = (
-  <>
-    <ul>
-      <li>Enduring Freedom</li>
-      <li>Freedom’s Sentinel</li>
-      <li>Iraqi Freedom</li>
-      <li>New Dawn</li>
-      <li>Inherent Resolve</li>
-      <li>Resolute Support Mission</li>
-    </ul>
-  </>
+  <ul>
+    <li>Enduring Freedom</li>
+    <li>Freedom’s Sentinel</li>
+    <li>Iraqi Freedom</li>
+    <li>New Dawn</li>
+    <li>Inherent Resolve</li>
+    <li>Resolute Support Mission</li>
+  </ul>
 );
 
 export default CombatOperationServiceDescription;
