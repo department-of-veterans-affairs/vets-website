44c7dce22a0233a74af7fb3cc7dc20b466fbf81e
Brian Seek <brian.seek@civicactions.com>
2024-02-23 08:16:50 -0800

    updated second action-link to secondary (#28136)
    * updated second action-link to secondary
    
    * more content changes.
    
    * removed TODO

--- src/applications/check-in/travel-claim/pages/travel-intro/index.jsx
+++ src/applications/check-in/travel-claim/pages/travel-intro/index.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { useTranslation, Trans } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { recordEvent } from '@department-of-veterans-affairs/platform-monitoring/exports';
 
 import Wrapper from '../../../components/layout/Wrapper';
@@ -17,7 +17,6 @@ const TravelIntro = props => {
   const { t } = useTranslation();
 
   const { jumpToPage } = useFormRouting(router);
-  // @TODO logic need to figure out if we should go to multi-facility page or single
 
   const nextPage = hasMultipleFacilities(appointments)
     ? 'select-appointment'
@@ -42,12 +41,7 @@ const TravelIntro = props => {
         classNames="travel-page"
       >
         <p className="vads-u-margin-bottom--0">
-          <Trans
-            i18nKey="you-can-use-this-tool-file-claim"
-            components={[
-              <span key="bold" className="vads-u-font-weight--bold" />,
-            ]}
-          />
+          {t('you-can-use-this-tool-file-claim')}
         </p>
         <va-process-list uswds>
           <va-process-list-item>
@@ -81,12 +75,10 @@ const TravelIntro = props => {
               {t('file-mileage-only-claim')}
             </a>
             <p>
-              {t(
-                'if-claiming-other-expenses-file-online-or-mail-email-fax-in-person',
-              )}
+              {t('if-claiming-other-expenses-file-online-or-mail-or-in-person')}
             </p>
             <a
-              className="vads-c-action-link--green"
+              className="vads-c-action-link--blue"
               href="https://www.va.gov/health-care/get-reimbursed-for-travel-pay/"
             >
               {t('learn-how-file-claims-other-expenses')}
