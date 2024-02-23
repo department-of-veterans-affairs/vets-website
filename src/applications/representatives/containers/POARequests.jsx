ef8b8f2b9c8b5b664667d095b41ea5007e91e096
William Phelps <williamphelps13@gmail.com>
2024-02-22 21:52:29 -0700

    Remove search for now and replace placeholder container with Content component

--- src/applications/representatives/containers/POARequests.jsx
+++ src/applications/representatives/containers/POARequests.jsx
@@ -1,41 +1,26 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+
+import POARequestsContent from '../components/POARequestsContent/POARequestsContent';
 import LoginViewWrapper from './LoginViewWrapper';
 
-const POARequests = ({ POApermissions = true }) => {
-  const breadcrumbs = [
-    { link: '/', label: 'Home' },
-    { link: '/dashboard', label: 'Dashboard' },
-    { link: '/poa-requests', label: 'POA requests' },
-  ];
+const breadcrumbs = [
+  { link: '/', label: 'Home' },
+  { link: '/dashboard', label: 'Dashboard' },
+  { link: '/poa-requests', label: 'POA requests' },
+];
+
+const POARequests = ({ poaPermissions = true }) => {
   return (
-    <LoginViewWrapper breadcrumbs={breadcrumbs} POApermissions={POApermissions}>
+    <LoginViewWrapper breadcrumbs={breadcrumbs} poaPermissions={poaPermissions}>
       <h1>Power of attorney requests</h1>
-      <label
-        htmlFor="poa-requests-search"
-        id="poa-requests-search-label"
-        className="vads-u-margin-top--0 vads-u-margin-bottom--1p5"
-      >
-        Search
-        <va-text-input
-          id="poa-requests-search"
-          aria-labelledby="poa-requests-search-label"
-          name="preferred-poa-requests-search"
-          required
-        />
-      </label>
-      <div className="placeholder-container">
-        <div className="nav vads-u-background-color--gray-lightest vads-u-margin-bottom--2" />
-        <div className="notif vads-u-background-color--gray-lightest vads-u-margin-bottom--2" />
-        <div className="primary vads-u-background-color--gray-lightest vads-u-margin-bottom--2" />
-        <div className="etc vads-u-background-color--gray-lightest" />
-      </div>
+      <POARequestsContent />
     </LoginViewWrapper>
   );
 };
 
 POARequests.propTypes = {
-  POApermissions: PropTypes.bool,
+  poaPermissions: PropTypes.bool,
 };
 
 export default POARequests;
