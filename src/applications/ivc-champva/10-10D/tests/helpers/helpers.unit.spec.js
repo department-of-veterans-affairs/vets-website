8823abfece86137c80a3338043ace5e9e50cf395
Michael Clement <michael.clement@adhocteam.us>
2024-02-23 14:20:21 -0600

    added unit tests for helper funcs

--- src/applications/ivc-champva/10-10D/tests/helpers/helpers.unit.spec.js
+++ src/applications/ivc-champva/10-10D/tests/helpers/helpers.unit.spec.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import { applicantWording } from '../../helpers/wordingCustomization';
+import { getAgeInYears, isInRange, getParts } from '../../helpers/utilities';
 
 describe('applicantWording helper', () => {
   it('should concatenate first and last names', () => {
@@ -8,5 +9,33 @@ describe('applicantWording helper', () => {
         applicantName: { first: 'firstname', last: 'lastname' },
       }),
     ).to.equal("firstname lastname's ");
+  });
+});
+
+describe('getAgeInYears helper', () => {
+  const year = Number(
+    new Date()
+      .getFullYear()
+      .toString()
+      .slice(-2),
+  );
+
+  it('should return the proper age in years', () => {
+    expect(getAgeInYears('2000-01-01')).to.equal(year);
+  });
+});
+
+describe('isInRange helper', () => {
+  it('should return true if number in range', () => {
+    expect(isInRange(22, 18, 23)).to.be.true;
+  });
+  it('should return false if number not in range', () => {
+    expect(isInRange(25, 18, 23)).to.be.false;
+  });
+});
+
+describe('getParts helper', () => {
+  it('should clean up presentation of medicare part text', () => {
+    expect(getParts('partA, partB, partD')).to.equal('Part A, Part B');
   });
 });
