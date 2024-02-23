e5a56389e6de39a51ccd93c4289f379ac2e7c597
Michael Clement <michael.clement@adhocteam.us>
2024-02-23 12:58:00 -0600

    updated submit transformer to remove undefineds

--- src/applications/ivc-champva/10-10D/config/submitTransformer.js
+++ src/applications/ivc-champva/10-10D/config/submitTransformer.js
@@ -34,6 +34,11 @@ function transformApplicants(applicants) {
         app?.applicantMedicareCardBack,
         app?.applicantOHICardFront,
         app?.applicantOHICardBack,
+        app?.applicantBirthCertOrSocialSecCard,
+        app?.applicantMedicarePartAPartBCard,
+        app?.applicantMedicarePartDCard,
+        app?.applicantOhiCard,
+        app?.applicant107959c,
       ],
       address: app.applicantAddress ?? '',
       gender: app.applicantGender ?? '',
@@ -105,9 +110,9 @@ export default function transformForSubmit(formConfig, form) {
     }
   });
 
-  dataPostTransform.supporting_docs = dataPostTransform.supporting_docs.concat(
-    supDocs,
-  );
+  dataPostTransform.supporting_docs = dataPostTransform.supporting_docs
+    .concat(supDocs)
+    .filter(el => el); // remove undefineds
 
   // eslint-disable-next-line dot-notation
   dataPostTransform.veteran.address['postal_code'] =
