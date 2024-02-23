1c763fc43dee45e3a524994b4e6f1c24222d6948
William Phelps <williamphelps13@gmail.com>
2024-02-22 21:39:04 -0700

    Remove POA requests link since can now use Sidenav/View all; Fix pascal prop

--- src/applications/representatives/containers/Dashboard.jsx
+++ src/applications/representatives/containers/Dashboard.jsx
@@ -1,46 +1,20 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Link } from 'react-router';
 
-import PoaRequestsWidget from '../components/PoaRequestsWidget/PoaRequestsWidget';
+import POARequestsWidget from '../components/POARequestsWidget/POARequestsWidget';
+import { mockPOARequests } from '../mocks/mockPOARequests';
 
-// import { RequiredLoginView } from '@department-of-veterans-affairs/platform-user/RequiredLoginView';
-
-const dummyPoaRequestData = [
-  {
-    name: 'John Smith',
-    id: 12345,
-    date: '24 JAN 2024 09:00AM',
-  },
-  {
-    name: 'Madaline Rouge',
-    id: 12345,
-    date: '25 JAN 2024 09:00AM',
-  },
-  {
-    name: 'Arnold R. Ford',
-    id: 12345,
-    date: '30 JAN 2024 10:00AM',
-  },
+const breadcrumbs = [
+  { link: '/', label: 'Home' },
+  { link: '/dashboard', label: 'Dashboard' },
 ];
 
 import LoginViewWrapper from './LoginViewWrapper';
 
-const Dashboard = ({ POApermissions = true }) => {
-  const breadcrumbs = [
-    { link: '/', label: 'Home' },
-    { link: '/dashboard', label: 'Dashboard' },
-  ];
-
+const Dashboard = ({ poaPermissions = true }) => {
   return (
-    <LoginViewWrapper breadcrumbs={breadcrumbs} POApermissions={POApermissions}>
+    <LoginViewWrapper breadcrumbs={breadcrumbs} poaPermissions={poaPermissions}>
       <h1>Accredited Representative Portal</h1>
-      <Link
-        to="/poa-requests"
-        className="vads-c-action-link--green vads-u-margin-bottom--2"
-      >
-        Manage power of attorney requests
-      </Link>
       <div className="placeholder-container">
         <div className="dash-container">
           <div className="vads-u-display--flex">
@@ -50,7 +24,7 @@ const Dashboard = ({ POApermissions = true }) => {
               </div>
               <div className="vads-u-display--flex vads-u-flex-direction--row">
                 <div className="vads-l-col--9">
-                  <PoaRequestsWidget poaRequests={dummyPoaRequestData} />
+                  <POARequestsWidget poaRequests={mockPOARequests} />
                 </div>
                 <div className="vads-l-col--3 vads-u-padding-left--2">
                   <div className="primary dash-box vads-u-background-color--white vads-u-margin-bottom--2 rounded-corners" />
@@ -66,7 +40,7 @@ const Dashboard = ({ POApermissions = true }) => {
 };
 
 Dashboard.propTypes = {
-  POApermissions: PropTypes.bool,
+  poaPermissions: PropTypes.bool,
 };
 
 export default Dashboard;
