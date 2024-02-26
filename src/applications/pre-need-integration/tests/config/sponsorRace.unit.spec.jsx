7ee6959cfc0be35d7962e2b342aa43d428eb4f0a
Jordan Boardman <155088633+jordanBoardmanCodes@users.noreply.github.com>
2024-02-26 10:38:16 -0500

    MBMS-54135/Self-Service-Pre-Need-Integration-Non-Veteran-Flow-Sponsor-Info-Screen-Division/Screen-Hiding (#28098)
    * upload everything from previous pr
    
    * fixed tests for sponsorDemographics and sponsorRace
    
    * MBMS-54135 uncommented certain lines needed for vets-json-schema changes incoming
    
    * MBMS-54135 fixed ui:title in ethnicity and race value in helpers.js
    
    * added new file upload properties (#28113)
    
    * Arf 74382 notifications list (#28077)
    
    * Added notifications component
    
    * Reformatted page layout to better match mockup
    
    * Addressed PR feedback
    
    * Addressing more PR feedback
    
    * updated link
    
    * VFEP is 400% zoom should not generate an error. (#28114)
    
    * MBMS-54135 resolved merge conflicts
    
    * MBMS-54135 fixed ' issues in helpers.js
    
    * MBMS-54135 commented out a link to match Adam's side
    
    * MBMS-54135 final fix before merge
    
    * MBMS-54135 deleted unneeded commented out code
    
    ---------
    
    Co-authored-by: Michael Clement <michael.clement@adhocteam.us>
    Co-authored-by: Jesse Cohn <jesse.cohn@adhocteam.us>
    Co-authored-by: Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>

--- src/applications/pre-need-integration/tests/config/sponsorRace.unit.spec.jsx
+++ src/applications/pre-need-integration/tests/config/sponsorRace.unit.spec.jsx
@@ -5,18 +5,17 @@ import { mount } from 'enzyme';
 
 import {
   DefinitionTester,
-  selectRadio,
+  fillData,
 } from 'platform/testing/unit/schemaform-utils.jsx';
 import formConfig from '../../config/form';
 
-describe('Pre-need sponsor demographics', () => {
+describe('Pre-need applicant demographics', () => {
   const {
     schema,
     uiSchema,
-  } = formConfig.chapters.sponsorInformation.pages.sponsorDemographics;
+  } = formConfig.chapters.sponsorInformation.pages.sponsorRace;
 
-  // to be implemented in MBMS-54135:
-  it.skip('should render', () => {
+  it('should render', () => {
     const form = mount(
       <DefinitionTester
         schema={schema}
@@ -25,12 +24,12 @@ describe('Pre-need sponsor demographics', () => {
       />,
     );
 
-    expect(form.find('input').length).to.equal(9);
+    expect(form.find('input').length).to.equal(4);
+    expect(form.find('va-checkbox').length).to.equal(7);
     form.unmount();
   });
 
-  // to be implemented in MBMS-54135:
-  it.skip('should not submit empty form', () => {
+  it('should not submit empty form', () => {
     const onSubmit = sinon.spy();
     const form = mount(
       <DefinitionTester
@@ -43,13 +42,13 @@ describe('Pre-need sponsor demographics', () => {
 
     form.find('form').simulate('submit');
 
-    expect(form.find('.usa-input-error').length).to.equal(2);
+    expect(form.find('.usa-input-error').length).to.equal(1);
+    expect(form.find('va-checkbox-group[error]').length).to.equal(1);
     expect(onSubmit.called).to.be.false;
     form.unmount();
   });
 
-  // to be implemented in MBMS-54135:
-  it.skip('should submit with required information', () => {
+  it('should check required boxes', () => {
     const onSubmit = sinon.spy();
     const form = mount(
       <DefinitionTester
@@ -59,12 +58,17 @@ describe('Pre-need sponsor demographics', () => {
         uiSchema={uiSchema}
       />,
     );
+    fillData(form, 'input#root_application_veteran_ethnicity_2', 'unknown');
+    form
+      .find('va-checkbox[name*="root_application_veteran_race_isAsian"]')
+      .simulate('change', { target: { checked: true } });
 
-    selectRadio(form, 'root_application_veteran_gender', 'Female');
-    selectRadio(form, 'root_application_veteran_maritalStatus', 'Single');
-    form.find('form').simulate('submit');
+    expect(
+      form
+        .find('va-checkbox[name*="root_application_veteran_race_isAsian"]')
+        .exists(),
+    ).to.be.true;
 
-    expect(onSubmit.called).to.be.true;
     form.unmount();
   });
 });
