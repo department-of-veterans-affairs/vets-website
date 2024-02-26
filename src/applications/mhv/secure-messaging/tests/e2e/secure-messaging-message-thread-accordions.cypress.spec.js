4dba19971508ed043648513275801a53bdbbd413
fazilqa <fazil.mammadov@bylight.com>
2024-02-26 15:54:30 -0800

    upd: thread-accordion.cypress.spec.js

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-message-thread-accordions.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-message-thread-accordions.cypress.spec.js
@@ -11,49 +11,22 @@ describe('Secure Messaging Thread Details', () => {
     el => el.attributes.messageId,
   );
 
-  // TODO find the problem - when click on first message focus will back to header
-  // TODO find the alternate for 'cy.wait()'
   it('verify expanded messages focus', () => {
     site.login();
     landingPage.loadInboxMessages();
     landingPage.loadSingleThread(mockThreadResponse, date);
-    // cy.wait(500);
 
-    cy.then(() => {
-      for (let i = 1; i < messageIdList.length; i += 1) {
-        cy.get(`[data-testid="expand-message-button-${messageIdList[i]}"]`)
-          .shadow()
-          .find('[part="accordion-header"]')
-          .click()
-          .then(el => {
-            cy.wrap(el)
-              .should('have.css', 'outline-color', 'rgb(61, 69, 81)')
-              .and('be.enabled');
-          });
-        cy.focused().then(el => {
-          if (cy.wrap(el).contains('h1')) {
-            cy.get(`[data-testid="expand-message-button-${messageIdList[i]}"]`)
-              .shadow()
-              .find('[part="accordion-header"]')
-              .click();
-          } else {
-            cy.get(`[data-testid="expand-message-button-${messageIdList[i]}"]`)
-              .shadow()
-              .find('[part="accordion-header"]')
-              .should('have.attr', 'aria-expanded', 'true')
-              .and('have.focus');
-          }
-        });
-        // cy.focused().then(el => {
-        //   console.log(el);
-        // });
-        // cy.get(`[data-testid="expand-message-button-${messageIdList[i]}"]`)
-        //   .shadow()
-        //   .find('[part="accordion-header"]')
-        //   .should('have.attr', 'aria-expanded', 'true')
-        //   .and('have.focus');
-      }
-    });
+    for (let i = 1; i < messageIdList.length; i += 1) {
+      cy.get(`[data-testid="expand-message-button-${messageIdList[i]}"]`)
+        .shadow()
+        .find('[part="accordion-header"]')
+        .click();
+
+      cy.get(`[data-testid="expand-message-button-${messageIdList[i]}"]`)
+        .shadow()
+        .find('[part="accordion-header"]')
+        .should('have.attr', 'aria-expanded', 'true');
+    }
 
     cy.injectAxe();
     cy.axeCheck(AXE_CONTEXT);
