1d2842718bab90a1c4150ec8a3fb322adaf23530
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2024-02-26 08:46:19 -0700

    Pensions json schema key value updates (#28096)
    * Update data sets
    
    * Update key value radio patterns
    
    * Update json schema version
    
    * Add additional child in overflow test
    
    * Fix edu-benefits error and skip failing tests
    
    ---------
    
    Co-authored-by: Dan Lim <danlim715@gmail.com>

--- src/applications/pensions/tests/unit/components/DisclosureTitle.unit.spec.jsx
+++ src/applications/pensions/tests/unit/components/DisclosureTitle.unit.spec.jsx
@@ -26,7 +26,7 @@ describe('Pensions DisclosureTitle', () => {
       <DisclosureTitle
         id="id"
         formData={{
-          maritalStatus: 'Married',
+          maritalStatus: 'MARRIED',
           marriages: [
             {
               spouseFullName: {
