9971ea59e906feffd132f00670f9b1528fc8e8c9
Peri McLaren <peri.mclaren@adhocteam.us>
2024-02-26 12:19:33 -0700

    update components to be v3

--- src/applications/rated-disabilities/components/TotalRatingStates.jsx
+++ src/applications/rated-disabilities/components/TotalRatingStates.jsx
@@ -13,15 +13,15 @@ export const errorMessage = () => (
     </p>
     <p className="vads-u-font-size--base">
       If you get this error again, please call the VA.gov help desk at{' '}
-      <va-telephone contact={CONTACTS.VA_311} uswds="false" /> (
-      <va-telephone contact={CONTACTS['711']} tty uswds="false" />
+      <va-telephone contact={CONTACTS.VA_311} /> (
+      <va-telephone contact={CONTACTS['711']} tty />
       ). We’re here Monday though Friday, 8:00 a.m. to 8:00 p.m. ET.
     </p>
   </va-alert>
 );
 
 export const missingTotalMessage = () => (
-  <va-alert status="info" uswds="false">
+  <va-alert status="info" uswds="true">
     <h2 slot="headline" className="vads-u-margin-y--0 vads-u-font-size--h3">
       We don’t have a combined disability rating on file for you
     </h2>
@@ -33,7 +33,6 @@ export const missingTotalMessage = () => (
     <va-link
       href="/disability/how-to-file-claim"
       text="Learn how to file a claim for disability compensation"
-      uswds="false"
     />
   </va-alert>
 );
@@ -42,7 +41,7 @@ export const totalRatingMessage = totalDisabilityRating => {
   const heading = `Your combined disability rating is ${totalDisabilityRating}%`;
 
   return (
-    <va-featured-content uswds="false">
+    <va-summary-box>
       <h3 slot="headline">{heading}</h3>
       <p>
         This rating doesn’t include any conditions from claims that we’re still
@@ -61,6 +60,6 @@ export const totalRatingMessage = totalDisabilityRating => {
       >
         Check the status of your claims, decision reviews, or appeals online
       </a>
-    </va-featured-content>
+    </va-summary-box>
   );
 };
