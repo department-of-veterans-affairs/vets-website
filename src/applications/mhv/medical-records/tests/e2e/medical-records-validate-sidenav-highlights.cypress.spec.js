30f9071c507cae490302193f82073f5af9fd702d
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2024-02-26 11:56:17 -0800

    check allergies list/details page for sidenav highlight

--- src/applications/mhv/medical-records/tests/e2e/medical-records-validate-sidenav-highlights.cypress.spec.js
+++ src/applications/mhv/medical-records/tests/e2e/medical-records-validate-sidenav-highlights.cypress.spec.js
@@ -3,11 +3,6 @@ import allergies from './fixtures/allergies.json';
 import allergy from './fixtures/allergy.json';
 import AllergiesListPage from './pages/AllergiesListPage';
 import AllergyDetailsPage from './pages/AllergyDetailsPage';
-import VaccinesListPage from './pages/VaccinesListPage';
-import VaccineDetailsPage from './pages/VaccineDetailsPage';
-import NotesListPage from './pages/NotesListPage';
-import NotesDetailsPage from './pages/NotesDetailsPage';
-import defaultVaccines from './fixtures/vaccines/vaccines.json';
 
 describe('Medical Records Validate Sidenav Highlights', () => {
   it('Visits Medical Records View Details', () => {
@@ -15,25 +10,13 @@ describe('Medical Records Validate Sidenav Highlights', () => {
     site.login();
     cy.visit('my-health/medical-records');
 
-    // navigate to allergies details page
+    // navigate to allergies list page
     AllergiesListPage.clickGotoAllergiesLink(allergies);
+    AllergyDetailsPage.verifySidenavHighlightAllergies();
+    // navigate to allergies details page
     AllergyDetailsPage.clickAllergyDetailsLink('NUTS', 7006, allergy);
     cy.get('@allergyDetails.all').should('have.length', 0);
-    // Verify allergies sidenav is highlighted
     AllergyDetailsPage.verifySidenavHighlightAllergies();
-
-    // navigate to vaccines details page
-    cy.visit('my-health/medical-records');
-    VaccinesListPage.clickGotoVaccinesLink(defaultVaccines);
-    VaccinesListPage.clickVaccinesDetailsLink(0, defaultVaccines.entry[0]);
-    // verify vaccines sidenav is highlighted
-    VaccineDetailsPage.verifySidenavHighlightVaccines();
-
-    // navigate to care summaries and notes details page
-    // cy.visit('my-health/medical-records');
-    NotesListPage.clickGotoNotesLink();
-    NotesDetailsPage.clickProgressNoteLink(4);
-    NotesDetailsPage.verifySidenavHighlightNotes();
     cy.injectAxe();
     cy.axeCheck('main');
   });
