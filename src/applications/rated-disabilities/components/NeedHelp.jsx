b55fbbb03d5ef8f2ce241188d2958ccdb2fd41c0
jerekshoe <jerek.shoemaker@adhocteam.us>
2024-02-27 16:10:00 -0600

    Adding back uswds='false'

--- src/applications/rated-disabilities/components/NeedHelp.jsx
+++ src/applications/rated-disabilities/components/NeedHelp.jsx
@@ -6,9 +6,11 @@ export default function NeedHelp() {
     <va-need-help class="vads-u-margin-y--3" uswds="false">
       <div slot="content">
         <p>
-          You can call us at <va-telephone contact={CONTACTS.VA_BENEFITS} />.
-          We’re here Monday through Friday, 8:00 a.m to 9:00 p.m. ET. If you
-          have hearing loss, call <va-telephone contact={CONTACTS['711']} tty />
+          You can call us at{' '}
+          <va-telephone contact={CONTACTS.VA_BENEFITS} uswds="false" />. We’re
+          here Monday through Friday, 8:00 a.m to 9:00 p.m. ET. If you have
+          hearing loss, call{' '}
+          <va-telephone contact={CONTACTS['711']} tty uswds="false" />
         </p>
       </div>
     </va-need-help>
