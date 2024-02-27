a3c07fe0c485a78a1d7fe12bf85159623b7ef561
robertbylight <roberto.rodriguez@bylight.com>
2024-02-27 18:54:30 -0500

    [MHV-54541] more unit test.

--- src/applications/mhv/medications/tests/containers/PrescriptionsPrintOnly.unit.spec.jsx
+++ src/applications/mhv/medications/tests/containers/PrescriptionsPrintOnly.unit.spec.jsx
@@ -1,20 +1,34 @@
 import { expect } from 'chai';
 import React from 'react';
 import { renderWithStoreAndRouter } from '@department-of-veterans-affairs/platform-testing/react-testing-library-helpers';
+import { mockApiRequest } from '@department-of-veterans-affairs/platform-testing/helpers';
 import reducers from '../../reducers';
 import PrescriptionsPrintOnly from '../../containers/PrescriptionsPrintOnly';
+import { allergiesList } from '../fixtures/allergiesList.json';
+import { prescriptionsList } from '../fixtures/prescriptionsList.json';
+import { rxListSortingOptions } from '../../util/constants';
 
 describe('Medications List Print Page', () => {
   const setup = (params = {}) => {
     return renderWithStoreAndRouter(<PrescriptionsPrintOnly />, {
-      initialState: {},
+      initialState: {
+        rx: {
+          prescriptions: {
+            selectedSortOption: rxListSortingOptions.alphabeticalOrder,
+          },
+          allergies: {
+            allergiesList: { allergiesList },
+          },
+        },
+      },
       reducers,
-      path: '/1',
+      path: '/?page=1',
       ...params,
     });
   };
 
   it('renders without errors', () => {
+    mockApiRequest(prescriptionsList);
     const screen = setup();
     expect(screen);
   });
@@ -27,5 +41,14 @@ describe('Medications List Print Page', () => {
   it('does not render for paths other than medication list and details', () => {
     const screen = setup({ path: '/foo' });
     expect(screen.queryByTestId('name-date-of-birth')).to.not.exist;
+  });
+  it('displays "This is a list of prescriptions and other..." message', () => {
+    mockApiRequest(prescriptionsList);
+    const screen = setup();
+    expect(
+      screen.findByText(
+        'This is a list of prescriptions and other medications in your VA medical records. When you download medication records, we also include a list of allergies and reactions in your VA medical records.',
+      ),
+    ).to.exist;
   });
 });
