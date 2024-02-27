542f46c2363558913b2d39a038ef6403f745f46a
Michael Clement <michael.clement@adhocteam.us>
2024-02-27 15:08:35 -0600

    condensed tests and removed unnecessary async

--- src/applications/ivc-champva/10-10D/tests/components/CustomRelationshipPattern.unit.spec.js
+++ src/applications/ivc-champva/10-10D/tests/components/CustomRelationshipPattern.unit.spec.js
@@ -2,25 +2,19 @@ import { expect } from 'chai';
 import { relationshipToVeteranUI } from '../../components/CustomRelationshipPattern';
 
 describe('Custom relationship pattern', () => {
-  it('should default to veteran', async () => {
+  it('should default to veteran', () => {
     const relationship = relationshipToVeteranUI();
     expect(relationship.relationshipToVeteran?.['ui:title']).to.contain(
       'Veteran',
     );
   });
-});
-
-describe('Custom relationship pattern', () => {
-  it('should use functions for titles', async () => {
+  it('should use functions for titles', () => {
     const relationship = relationshipToVeteranUI({
       relativeTitle: () => '',
     });
     expect(relationship.relationshipToVeteran['ui:title']).to.exist;
   });
-});
-
-describe('Custom relationship pattern', () => {
-  it('should allow past-tense wording', async () => {
+  it('should allow past-tense wording', () => {
     const relationship = relationshipToVeteranUI({ tense: 'past' });
     expect(relationship.relationshipToVeteran?.['ui:title']).to.contain(
       'What’s your relationship',
