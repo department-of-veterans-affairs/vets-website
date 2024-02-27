5803f797bdf8416869ceee03a648f761213b1695
Adam Whitlock <adam.whitlock@adhocteam.us>
2024-02-26 23:37:00 -0700

    test: e2e tests for cred retirement alerts

---
+++ src/applications/personalization/profile/tests/e2e/account-security/credential-retirement-alert.cypress.spec.js
@@ -0,0 +1,66 @@
+import { PROFILE_PATHS } from '../../../constants';
+
+import fullName from '../../fixtures/full-name-success.json';
+import personalInformation from '../../fixtures/personal-information-success-enhanced.json';
+import serviceHistory from '../../fixtures/service-history-success.json';
+import { generateFeatureToggles } from '../../../mocks/endpoints/feature-toggles';
+import { dsLogonUser, mvhUser, loa1User } from '../../../mocks/endpoints/user';
+import { findVaLinkByText } from '../../../../common/e2eHelpers';
+
+context('credential retirement alerts on account security', () => {
+  beforeEach(() => {
+    cy.intercept('v0/profile/full_name', fullName);
+    cy.intercept('v0/profile/personal_information', personalInformation);
+    cy.intercept('v0/profile/service_history', () => serviceHistory);
+    cy.intercept(
+      'GET',
+      '/v0/feature_toggles*',
+      generateFeatureToggles({
+        profileShowCredentialRetirementMessaging: true,
+      }),
+    );
+  });
+
+  it('should show credential retirement alert content for DS Logon account', () => {
+    cy.login(dsLogonUser);
+
+    cy.visit(PROFILE_PATHS.ACCOUNT_SECURITY);
+
+    cy.findByText(
+      `you’ll no longer be able to sign in with your DS Logon username and password`,
+      { exact: false },
+    );
+
+    findVaLinkByText('Learn how to create an account').should('exist');
+
+    cy.injectAxeThenAxeCheck();
+  });
+
+  it('should show credential retirement alert content for MHV account', () => {
+    cy.login(mvhUser);
+
+    cy.visit(PROFILE_PATHS.ACCOUNT_SECURITY);
+
+    cy.findByText(
+      `you’ll no longer be able to sign in with your My HealtheVet username and password`,
+      { exact: false },
+    );
+
+    findVaLinkByText('Learn how to create an account').should('exist');
+
+    cy.injectAxeThenAxeCheck();
+  });
+
+  it('should show credential retirement alert content for LOA1 users', () => {
+    cy.login(loa1User);
+
+    cy.visit(PROFILE_PATHS.ACCOUNT_SECURITY);
+
+    cy.findByText(
+      `Verify your identity with Login.gov or ID.me to manage your profile information`,
+      { exact: false },
+    );
+
+    cy.injectAxeThenAxeCheck();
+  });
+});
