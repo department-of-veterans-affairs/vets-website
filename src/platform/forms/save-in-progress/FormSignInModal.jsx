c86f9fafebd1c5ccbc87c781c065a309ac7f3c5a
Rob Garrison <wowmotty@gmail.com>
2024-02-27 10:15:48 -0600

    Revert sign in modal changes (#28209)
    * Revert sign in modal changes
    
    * Revert unit tests

--- src/platform/forms/save-in-progress/FormSignInModal.jsx
+++ src/platform/forms/save-in-progress/FormSignInModal.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { VaModal } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import Modal from '@department-of-veterans-affairs/component-library/Modal';
 
 import recordEvent from '../../monitoring/record-event';
 import { APP_TYPE_DEFAULT } from '../../forms-system/src/js/constants';
@@ -19,29 +19,36 @@ class FormSignInModal extends React.Component {
   };
 
   render() {
+    const primaryButton = {
+      action: this.handleClose,
+      text: 'Finish applying',
+    };
+
+    const secondaryButton = {
+      action: this.handleSignIn,
+      text: 'Sign in and start over',
+    };
     const { formConfig } = this.props;
     const appType = formConfig?.customText?.appType || APP_TYPE_DEFAULT;
 
     return (
-      <VaModal
+      <Modal
         id="form-sign-in-modal"
-        primaryButtonText="Finish applying"
-        secondaryButtonText="Sign in and start over"
-        onPrimaryButtonClick={this.handleClose}
-        onSecondaryButtonClick={this.handleSignIn}
+        primaryButton={primaryButton}
+        secondaryButton={secondaryButton}
         visible={this.props.visible}
-        forcedModal
-        onCloseEvent={this.props.onClose}
+        focusSelector="button"
+        hideCloseButton
+        onClose={this.props.onClose}
         status="warning"
-        modalTitle="If you sign in now, you’ll lose any information you’ve filled in"
-        uswds
+        title="If you sign in now, you’ll lose any information you’ve filled in"
       >
         <p>
           Since you didn’t sign in before you started, we can’t save your
           in-progress {appType}.
         </p>
         <p>If you sign in now, you’ll need to start over.</p>
-      </VaModal>
+      </Modal>
     );
   }
 }
--- src/platform/forms/tests/save-in-progress/FormSignInModal.unit.spec.jsx
+++ src/platform/forms/tests/save-in-progress/FormSignInModal.unit.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import sinon from 'sinon';
 
 import FormSignInModal from '../../save-in-progress/FormSignInModal';
@@ -27,24 +27,26 @@ describe('<FormSignInModal>', () => {
   });
 
   it('should render', () => {
-    const { container } = render(<FormSignInModal {...props} />);
-    expect(container.querySelector('va-modal')).to.exist;
+    const wrapper = shallow(<FormSignInModal {...props} />);
+    expect(wrapper).to.exist;
+    wrapper.unmount();
   });
 
   it('should close as a primary action', () => {
-    const { container } = render(<FormSignInModal {...props} />);
-    container.querySelector('va-modal').__events.primaryButtonClick();
+    const wrapper = shallow(<FormSignInModal {...props} />);
+    wrapper.prop('primaryButton').action();
     expect(props.onClose.calledOnce).to.be.true;
     expect(
       global.window.dataLayer.some(
         ({ event }) => event === 'no-login-finish-form',
       ),
     ).to.be.true;
+    wrapper.unmount();
   });
 
   it('should start the sign-in process as a secondary action', () => {
-    const { container } = render(<FormSignInModal {...props} />);
-    container.querySelector('va-modal').__events.secondaryButtonClick();
+    const wrapper = shallow(<FormSignInModal {...props} />);
+    wrapper.prop('secondaryButton').action();
     expect(props.onClose.calledOnce).to.be.true;
     expect(props.onSignIn.calledOnce).to.be.true;
     expect(
@@ -52,5 +54,6 @@ describe('<FormSignInModal>', () => {
         ({ event }) => event === 'login-link-restart-form',
       ),
     ).to.be.true;
+    wrapper.unmount();
   });
 });
