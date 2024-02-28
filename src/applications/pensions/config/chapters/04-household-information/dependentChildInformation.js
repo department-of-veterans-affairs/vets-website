c2e504fa8f5f4874adbc8daf77cfbe18d3c98e31
Wayne Weibel <wayne.weibel@adhocteam.us>
2024-02-28 17:00:50 -0500

    Pension 76776 Dependent information deprecated field

--- src/applications/pensions/config/chapters/04-household-information/dependentChildInformation.js
+++ src/applications/pensions/config/chapters/04-household-information/dependentChildInformation.js
@@ -1,4 +1,3 @@
-import get from 'platform/utilities/data/get';
 import merge from 'lodash/merge';
 import moment from 'moment';
 
@@ -11,7 +10,13 @@ import {
   yesNoUI,
 } from 'platform/forms-system/src/js/web-component-patterns';
 
-import fullSchemaPensions from 'vets-json-schema/dist/21P-527EZ-schema.json';
+import {
+  VaCheckboxField,
+  VaTextInputField,
+} from 'platform/forms-system/src/js/web-component-fields';
+
+import get from 'platform/utilities/data/get';
+
 import createHouseholdMemberTitle from '../../../components/DisclosureTitle';
 
 import { dependentSeriouslyDisabledDescription } from '../../../helpers';
@@ -19,8 +24,6 @@ import {
   DisabilityDocsAlert,
   SchoolAttendanceAlert,
 } from '../../../components/FormAlerts';
-
-const { dependents } = fullSchemaPensions.properties;
 
 const childRelationshipOptions = {
   BIOLOGICAL: "They're my biological child",
@@ -56,6 +59,12 @@ export default {
         'ui:title': createHouseholdMemberTitle('fullName', 'Information'),
         childPlaceOfBirth: {
           'ui:title': 'Place of birth (city and state or foreign country)',
+          'ui:webComponentField': VaTextInputField,
+          'ui:options': {
+            charcount: true,
+            inert: true,
+            enableAnalytics: true,
+          },
         },
         childSocialSecurityNumber: merge({}, ssnUI(), {
           'ui:required': (formData, index) =>
@@ -63,6 +72,7 @@ export default {
         }),
         'view:noSSN': {
           'ui:title': "Doesn't have a Social Security number",
+          'ui:webComponentField': VaCheckboxField,
         },
         childRelationship: radioUI({
           title: "What's your relationship?",
@@ -142,9 +152,9 @@ export default {
             'previouslyMarried',
           ],
           properties: {
-            childPlaceOfBirth: dependents.items.properties.childPlaceOfBirth,
+            childPlaceOfBirth: { type: 'string' },
             childSocialSecurityNumber: ssnSchema,
-            'view:noSSN': { type: 'boolean' },
+            'view:noSSN': { type: 'boolean', enum: [true] },
             childRelationship: radioSchema(
               Object.keys(childRelationshipOptions),
             ),
