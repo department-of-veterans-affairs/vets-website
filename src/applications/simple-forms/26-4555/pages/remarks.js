10c7c5ee8d7c44f221cea9cd7aea4bdeb8429e74
Robert Hasselle <123402053+rhasselle-oddball@users.noreply.github.com>
2024-02-28 14:35:13 -0600

    Update 26-4555 to use web components + updates (#28220)
    * Update form 4555 to use web components
    * Add selectUI and selectSchema
    * Update 4555 to match new design changes

--- src/applications/simple-forms/26-4555/pages/remarks.js
+++ src/applications/simple-forms/26-4555/pages/remarks.js
@@ -1,53 +1,38 @@
 import React from 'react';
+import {
+  checkboxGroupSchema,
+  checkboxGroupUI,
+  titleUI,
+} from 'platform/forms-system/src/js/web-component-patterns';
+import VaTextAreaField from 'platform/forms-system/src/js/web-component-fields/VaTextareaField';
 
 /** @type {PageSchema} */
 export default {
   uiSchema: {
-    'ui:title': (
-      <h3 className="vads-u-color--gray-dark vads-u-margin-y--0">
-        Service-connected conditions
-      </h3>
-    ),
-    remarks: {
-      'ui:title': (
-        <p className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--normal">
-          Do you have any of these conditions that were caused&mdash;or made
-          worse&mdash;by your service?
-        </p>
-      ),
-      respiratoryIllness: {
-        'ui:title': 'A severe respiratory (breathing-related) illness',
-      },
-      blindness: {
-        'ui:title':
-          'Blindness in both eyes (with 20/200 visual acuity or less)',
-      },
-      lossOfHands: {
-        'ui:title': 'Loss or loss of use of both hands',
-      },
-      lossOfLimbs: {
-        'ui:title': 'Loss or loss of use of more than one limb',
-      },
-      lossOfLegs: {
-        'ui:title':
+    ...titleUI('Service-connected conditions'),
+    remarks: checkboxGroupUI({
+      title:
+        'Do you have any of these conditions that were caused—or made worse—by your service?',
+      required: false,
+      labels: {
+        respiratoryIllness: 'A severe respiratory (breathing-related) illness',
+        blindness: 'Blindness in both eyes (with 20/200 visual acuity or less)',
+        lossOfHands: 'Loss or loss of use of both hands',
+        lossOfLimbs: 'Loss or loss of use of more than one limb',
+        lossOfLegs:
           'Loss or loss of use of a lower leg along with the residuals (lasting effects) of an organic (natural) disease or injury',
-      },
-      lossOfExtremity: {
-        'ui:title':
+        lossOfExtremity:
           'The loss, or loss of use, of one lower extremity (foot or leg) after September 11, 2001, which makes it so you can’t balance or walk without the help of braces, crutches, canes, or a wheelchair',
+        burns: 'Severe burns',
       },
-      burns: {
-        'ui:title': 'Severe burns',
-      },
-      otherConditions: {
-        'ui:title':
-          'If your conditions aren’t listed, you can write them here:',
-        'ui:widget': 'textarea',
-      },
+    }),
+    otherConditions: {
+      'ui:title': 'If your conditions aren’t listed, you can write them here:',
+      'ui:webComponentField': VaTextAreaField,
     },
     'view:additionalInformation': {
       'ui:description': (
-        <va-additional-info trigger="Why we ask for this information?">
+        <va-additional-info uswds trigger="Why we ask for this information?">
           <p>
             We use the information you provide to help decide if you&rsquo;re
             eligible for a grant. To be eligible, you must have a qualifying
@@ -61,34 +46,17 @@ export default {
   schema: {
     type: 'object',
     properties: {
-      remarks: {
-        type: 'object',
-        properties: {
-          respiratoryIllness: {
-            type: 'boolean',
-          },
-          blindness: {
-            type: 'boolean',
-          },
-          lossOfHands: {
-            type: 'boolean',
-          },
-          lossOfLimbs: {
-            type: 'boolean',
-          },
-          lossOfLegs: {
-            type: 'boolean',
-          },
-          lossOfExtremity: {
-            type: 'boolean',
-          },
-          burns: {
-            type: 'boolean',
-          },
-          otherConditions: {
-            type: 'string',
-          },
-        },
+      remarks: checkboxGroupSchema([
+        'respiratoryIllness',
+        'blindness',
+        'lossOfHands',
+        'lossOfLimbs',
+        'lossOfLegs',
+        'lossOfExtremity',
+        'burns',
+      ]),
+      otherConditions: {
+        type: 'string',
       },
       'view:additionalInformation': {
         type: 'object',
