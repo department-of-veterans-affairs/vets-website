35ba2b78c375f9976cd1fd14da24c5b85daeb2ea
Midge-dev <sean.midgley.dev@gmail.com>
2024-02-28 13:16:46 -0800

    new form version

--- src/applications/burials-v2/config/form.js
+++ src/applications/burials-v2/config/form.js
@@ -70,7 +70,7 @@ const formConfig = {
       saved: 'Your burial benefits application has been saved.',
     },
   },
-  version: 2,
+  version: 3,
   migrations,
   prefillEnabled: true,
   downtime: {
