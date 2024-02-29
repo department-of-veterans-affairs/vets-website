3176051745e1cb4a50ac27714f9a7da459be2300
Robin Garrison <robin.garrison@adhocteam.us>
2024-02-29 08:50:59 -0600

    Remove unused nod feature toggle

--- src/platform/utilities/feature-toggles/featureFlagNames.js
+++ src/platform/utilities/feature-toggles/featureFlagNames.js
@@ -98,7 +98,6 @@ export default Object.freeze({
   financialStatusReportReviewPageNavigation:
     'financial_status_report_review_page_navigation',
   findARepresentativeEnableFrontend: 'find_a_representative_enable_frontend',
-  form10182Nod: 'form10182_nod',
   form2010206: 'form2010206',
   form2110210: 'form2110210',
   form210845: 'form210845',
