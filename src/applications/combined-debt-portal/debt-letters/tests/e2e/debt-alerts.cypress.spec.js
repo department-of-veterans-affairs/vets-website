939f272e0bfc8368a4f7a6118fb94c4000307daa
Kevin Suarez <kjsuarez94@gmail.com>
2024-02-29 13:04:04 -0500

    67409 Pass in debtLettersShowLettersVBMS flag state to fetchDebtLetters (#28208)
    * pass in debtLettersShowLettersVBMS flag state to fetchDebtLetters
    
    * add debtLettersActive to CombinedPortalApp dependency array
    
    * properly intercept feature toggles

--- src/applications/combined-debt-portal/debt-letters/tests/e2e/debt-alerts.cypress.spec.js
+++ src/applications/combined-debt-portal/debt-letters/tests/e2e/debt-alerts.cypress.spec.js
@@ -1,8 +1,19 @@
 import mockDebts from './fixtures/mocks/debts.json';
-import mockFeatureToggles from './fixtures/mocks/feature-toggles.json';
 import mockUser from './fixtures/mocks/mock-user.json';
 import mockCopays from '../../../medical-copays/tests/e2e/fixtures/mocks/copays.json';
 import mockDebtVBMS from './fixtures/mocks/debtsVBMS.json';
+
+beforeEach(() => {
+  cy.intercept('GET', '/v0/feature_toggles*', {
+    data: {
+      features: [
+        { name: 'debt_letters_show_letters', value: true },
+        { name: 'combined_debt_portal_access', value: true },
+        { name: 'debt_letters_show_letters_vbms', value: true },
+      ],
+    },
+  });
+});
 
 describe('Debt Letters - CDP Alerts', () => {
   const mockZeroDebt = {
@@ -15,14 +26,11 @@ describe('Debt Letters - CDP Alerts', () => {
   // Has both VBA and VHA balances
   it('displays the current debts section and Your Other VA section', () => {
     cy.login(mockUser);
-    cy.intercept('GET', '/v0/feature_toggles*', mockFeatureToggles).as(
-      'features',
-    );
     cy.intercept('GET', '/v0/debts', mockDebts).as('debts');
     cy.intercept('GET', '/v0/debt_letters', mockDebtVBMS).as('debtVBMS');
     cy.intercept('GET', '/v0/medical_copays', mockCopays).as('copays');
     cy.visit('/manage-va-debt/summary/debt-balances');
-    cy.wait(['@copays', '@debts', '@debtVBMS', '@features']);
+    cy.wait(['@copays', '@debts', '@debtVBMS']);
     // Page load
     cy.findByTestId('summary-page-title').should('exist');
     // No large alert
@@ -35,16 +43,13 @@ describe('Debt Letters - CDP Alerts', () => {
   // Has VBA balances and VHA endpoint 404s
   it('displays the current debts section and alert in Your Other VA section', () => {
     cy.login(mockUser);
-    cy.intercept('GET', '/v0/feature_toggles*', mockFeatureToggles).as(
-      'features',
-    );
     cy.intercept('GET', '/v0/debts', mockDebts).as('debts');
     cy.intercept('GET', '/v0/debt_letters', mockDebtVBMS).as('debtVBMS');
     cy.intercept('GET', '/v0/medical_copays', req => {
       req.reply(404, { errors: ['error'] });
     }).as('copays');
     cy.visit('/manage-va-debt/summary/debt-balances');
-    cy.wait(['@copays', '@debts', '@debtVBMS', '@features']);
+    cy.wait(['@copays', '@debts', '@debtVBMS']);
     // Page load
     cy.findByTestId('summary-page-title').should('exist');
     // No large alert
@@ -57,9 +62,6 @@ describe('Debt Letters - CDP Alerts', () => {
   // VHA && VBA endpoints 404
   it('should display alert error message for VBA & VHA 404 responses', () => {
     cy.login(mockUser);
-    cy.intercept('GET', '/v0/feature_toggles*', mockFeatureToggles).as(
-      'features',
-    );
     cy.intercept('GET', '/v0/debts', req => {
       req.reply(404, { errors: ['error'] });
     }).as('debts');
@@ -67,7 +69,7 @@ describe('Debt Letters - CDP Alerts', () => {
       req.reply(404, { errors: ['error'] });
     }).as('copays');
     cy.visit('/manage-va-debt/summary/debt-balances');
-    cy.wait(['@copays', '@debts', '@features']);
+    cy.wait(['@copays', '@debts']);
     // Page load
     cy.findByTestId('summary-page-title').should('exist');
     cy.findByTestId('current-va-debt-list').should('not.exist');
@@ -82,15 +84,12 @@ describe('Debt Letters - CDP Alerts', () => {
   // // VBA endpoint 404s & VHA has balance
   it('should display alert error message for VBA 404 response with Your Other VA section', () => {
     cy.login(mockUser);
-    cy.intercept('GET', '/v0/feature_toggles*', mockFeatureToggles).as(
-      'features',
-    );
     cy.intercept('GET', '/v0/debts', req => {
       req.reply(404, { errors: ['error'] });
     }).as('debts');
     cy.intercept('GET', '/v0/medical_copays', mockCopays).as('copays');
     cy.visit('/manage-va-debt/summary/debt-balances');
-    cy.wait(['@copays', '@debts', '@features']);
+    cy.wait(['@copays', '@debts']);
     // Page load
     cy.findByTestId('summary-page-title').should('exist');
     cy.findByTestId('current-va-debt-list').should('not.exist');
@@ -103,15 +102,12 @@ describe('Debt Letters - CDP Alerts', () => {
   // // VBA 404 & VHA 0 balance
   it('should display alert error message for VBA 404 response without Your Other VA section', () => {
     cy.login(mockUser);
-    cy.intercept('GET', '/v0/feature_toggles*', mockFeatureToggles).as(
-      'features',
-    );
     cy.intercept('GET', '/v0/debts', req => {
       req.reply(404, {});
     }).as('debts');
     cy.intercept('GET', '/v0/medical_copays', mockZeroCopay).as('copays');
     cy.visit('/manage-va-debt/summary/debt-balances');
-    cy.wait(['@copays', '@debts', '@features']);
+    cy.wait(['@copays', '@debts']);
     // Page load
     cy.findByTestId('summary-page-title').should('exist');
     cy.findByTestId('current-va-debt-list').should('not.exist');
@@ -124,13 +120,10 @@ describe('Debt Letters - CDP Alerts', () => {
   // // VBA 0 balance & VHA balance
   it('should display alert info message for 0 VBA balance with Your Other VA secion', () => {
     cy.login(mockUser);
-    cy.intercept('GET', '/v0/feature_toggles*', mockFeatureToggles).as(
-      'features',
-    );
     cy.intercept('GET', '/v0/debts', mockZeroDebt).as('debts');
     cy.intercept('GET', '/v0/medical_copays', mockCopays).as('copays');
     cy.visit('/manage-va-debt/summary/debt-balances');
-    cy.wait(['@copays', '@debts', '@features']);
+    cy.wait(['@copays', '@debts']);
     // Page load
     cy.findByTestId('summary-page-title').should('exist');
     cy.findByTestId('current-va-debt-list').should('not.exist');
@@ -143,13 +136,10 @@ describe('Debt Letters - CDP Alerts', () => {
   // // VBA & VHA 0 balance
   it('should display alert info message for 0 VBA balance without Your Other VA secion', () => {
     cy.login(mockUser);
-    cy.intercept('GET', '/v0/feature_toggles*', mockFeatureToggles).as(
-      'features',
-    );
     cy.intercept('GET', '/v0/debts', mockZeroDebt).as('debts');
     cy.intercept('GET', '/v0/medical_copays', mockZeroCopay).as('copays');
     cy.visit('/manage-va-debt/summary/debt-balances');
-    cy.wait(['@copays', '@debts', '@features']);
+    cy.wait(['@copays', '@debts']);
     // Page Load
     cy.findByTestId('summary-page-title').should('exist');
     cy.findByTestId('current-va-debt-list').should('not.exist');
