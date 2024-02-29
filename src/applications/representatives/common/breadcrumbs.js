91bb16d4899de8c19d08ac387d20f7ebc3b4a1df
cohnjesse <cohnjesse@gmail.com>
2024-02-29 12:04:46 -0500

    added breadcrumbs test

--- src/applications/representatives/common/breadcrumbs.js
+++ src/applications/representatives/common/breadcrumbs.js
@@ -1,6 +1,29 @@
-export const POAbreadcrumbs = [
-  { link: '/', label: 'Home' },
-  { link: '/dashboard', label: 'Dashboard' },
-  { link: '/poa-requests', label: 'POA requests' },
-  { link: '/permissions', label: 'Permissions' },
-];
+export const POAbreadcrumbs = page => {
+  let breadcrumbs = [];
+
+  switch (page) {
+    case 'dashboard':
+      breadcrumbs = [
+        { link: '/', label: 'Home' },
+        { link: '/dashboard', label: 'Dashboard' },
+      ];
+      break;
+    case 'permissions':
+      breadcrumbs = [
+        { link: '/', label: 'Home' },
+        { link: '/permissions', label: 'Permissions' },
+      ];
+      break;
+    case 'poa-requests':
+      breadcrumbs = [
+        { link: '/', label: 'Home' },
+        { link: '/poa-requests', label: 'POA requests' },
+      ];
+      break;
+    default:
+      breadcrumbs = [{ link: '/', label: 'Home' }];
+      break;
+  }
+
+  return breadcrumbs;
+};
