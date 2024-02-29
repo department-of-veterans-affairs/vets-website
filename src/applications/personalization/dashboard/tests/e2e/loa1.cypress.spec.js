4f8a6616b79af9b6c7cf8d4f896d938756f67cce
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-02-28 22:03:36 -0500

    update tests

--- src/applications/personalization/dashboard/tests/e2e/loa1.cypress.spec.js
+++ src/applications/personalization/dashboard/tests/e2e/loa1.cypress.spec.js
@@ -20,8 +20,12 @@ const loa1DashboardTest = (mobile, stubs) => {
   }
 
   // make sure that the "Verify" alert is shown
-  cy.findByText(/Verify your identity to access/i).should('exist');
-  cy.findByText(/we need to make sure you’re you/i).should('exist');
+  cy.findByText(/We need you to verify your identity for this account/i).should(
+    'exist',
+  );
+  cy.findByText(/will ask you for certain personal information/i).should(
+    'exist',
+  );
   cy.findByRole('link', { name: 'Verify your identity' }).should(
     'have.attr',
     'href',
