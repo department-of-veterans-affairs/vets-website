a8c2b96eb23eb1c768b926e38daf6dcea71aa6b2
Eli Selkin <eli.j.selkin@gmail.com>
2024-02-29 18:09:16 -0800

    cleanup

--- src/applications/static-pages/facilities/vet-center/components/VAFacilityAddress.jsx
+++ src/applications/static-pages/facilities/vet-center/components/VAFacilityAddress.jsx
@@ -2,33 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import GoogleMapsLink from './GoogleMapsLink';
 
-function VAFacilityAddress(props) {
-  const addressDirections = `${props.vaFacility.fieldAddress.addressLine1}, ${
-    props.vaFacility.fieldAddress.locality
-  }, ${props.vaFacility.fieldAddress.administrativeArea}, ${
-    props.vaFacility.fieldAddress.postalCode
+const buildAddressLine = vaFacility => {
+  return `${vaFacility.fieldAddress.addressLine1}, ${
+    vaFacility.fieldAddress.locality
+  }, ${vaFacility.fieldAddress.administrativeArea}, ${
+    vaFacility.fieldAddress.postalCode
   }`;
+};
+function VAFacilityAddress(props) {
+  const addressDirections = buildAddressLine(props.vaFacility);
+  const { fieldAddress, title } = props.vaFacility;
   return (
-    <>
-      <div className="vads-u-margin-bottom--1">
-        <address className="vads-u-margin-bottom--0">
-          <div>{props.vaFacility.fieldAddress.organization}</div>
-          <div>{props.vaFacility.fieldAddress.addressLine1}</div>
-          <div>
-            {props.vaFacility.fieldAddress.locality}
-            {', '}
-            {props.vaFacility.fieldAddress.administrativeArea}{' '}
-            {props.vaFacility.fieldAddress.postalCode}
-          </div>
-        </address>
+    <div className="vads-u-margin-bottom--1">
+      <address className="vads-u-margin-bottom--0">
+        <div>{fieldAddress.organization}</div>
+        <div>{fieldAddress.addressLine1}</div>
         <div>
-          <GoogleMapsLink
-            addressDirections={addressDirections}
-            title={props.vaFacility.title}
-          />
+          {`${fieldAddress.locality}, ${fieldAddress.administrativeArea} ${
+            fieldAddress.postalCode
+          }`}
         </div>
+      </address>
+      <div>
+        <GoogleMapsLink addressDirections={addressDirections} title={title} />
       </div>
-    </>
+    </div>
   );
 }
 VAFacilityAddress.propTypes = {
