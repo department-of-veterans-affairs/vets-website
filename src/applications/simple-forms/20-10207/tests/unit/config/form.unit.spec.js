b35791e01d199b722c2763f73a9e99fb2af40112
Tze-chiu Lei <tclei2009@gmail.com>
2024-02-29 19:20:39 -0700

    WIP Add 20-10207 form unit-tests

--- src/applications/simple-forms/20-10207/tests/unit/config/form.unit.spec.js
+++ src/applications/simple-forms/20-10207/tests/unit/config/form.unit.spec.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import formConfig from '../../../config/form';
+import { PREPARER_TYPES } from '../../../config/constants';
 
 describe('formConfig', () => {
   it('should be an object', () => {
@@ -11,11 +12,160 @@ describe('formConfig', () => {
     expect(formConfig.rootUrl).to.be.a('string');
   });
 
-  // Add more tests for other properties...
-
   describe('chapters', () => {
     it('should be an object', () => {
       expect(formConfig.chapters).to.be.an('object');
+    });
+  });
+
+  describe('page.depends functions', () => {
+    describe('preparerTypeChapter page.depends', () => {
+      describe('thirdPartyVeteranIdentityPage.depends', () => {
+        const {
+          depends,
+        } = formConfig.chapters.preparerTypeChapter.pages.thirdPartyVeteranIdentityPage;
+
+        it('returns FALSE if preparerType is veteran', () => {
+          expect(depends({ preparerType: PREPARER_TYPES.VETERAN })).to.be.false;
+        });
+
+        it('returns TRUE if preparerType is third-party-veteran', () => {
+          expect(
+            depends({
+              preparerType: PREPARER_TYPES.THIRD_PARTY_VETERAN,
+            }),
+          ).to.be.true;
+        });
+      });
+
+      describe('thirdPartyNonVeteranIdentityPage.depends', () => {
+        const {
+          depends,
+        } = formConfig.chapters.preparerTypeChapter.pages.thirdPartyNonVeteranIdentityPage;
+
+        it('returns FALSE if preparerType is non-veteran', () => {
+          expect(depends({ preparerType: PREPARER_TYPES.NON_VETERAN })).to.be
+            .false;
+        });
+
+        it('returns TRUE if preparerType is third-party-non-veteran', () => {
+          expect(
+            depends({
+              preparerType: PREPARER_TYPES.THIRD_PARTY_NON_VETERAN,
+            }),
+          ).to.be.true;
+        });
+      });
+    });
+
+    describe('personalInformationChapter page.depends', () => {
+      describe('veteranNameAndDateOfBirthPageA.depends', () => {
+        const {
+          depends,
+        } = formConfig.chapters.personalInformationChapter.pages.veteranNameAndDateOfBirthPageA;
+
+        it('returns TRUE if preparerType is veteran or third-party-veteran', () => {
+          expect(
+            depends({
+              preparerType: PREPARER_TYPES.VETERAN,
+            }),
+          ).to.be.true;
+          expect(
+            depends({
+              preparerType: PREPARER_TYPES.THIRD_PARTY_VETERAN,
+            }),
+          ).to.be.true;
+        });
+
+        it('returns FALSE if preparerType is non-veteran or third-party-non-veteran', () => {
+          expect(depends({ preparerType: PREPARER_TYPES.NON_VETERAN })).to.be
+            .false;
+          expect(
+            depends({ preparerType: PREPARER_TYPES.THIRD_PARTY__NON_VETERAN }),
+          ).to.be.false;
+        });
+      });
+
+      describe('nonVeteranNameAndDateOfBirthPage.depends', () => {
+        const {
+          depends,
+        } = formConfig.chapters.personalInformationChapter.pages.nonVeteranNameAndDateOfBirthPage;
+
+        it('returns FALSE if preparerType is veteran or third-party-veteran', () => {
+          expect(
+            depends({
+              preparerType: PREPARER_TYPES.VETERAN,
+            }),
+          ).to.be.false;
+          expect(
+            depends({
+              preparerType: PREPARER_TYPES.THIRD_PARTY_VETERAN,
+            }),
+          ).to.be.false;
+        });
+
+        it('returns TRUE if preparerType is non-veteran or third-party-non-veteran', () => {
+          expect(depends({ preparerType: PREPARER_TYPES.NON_VETERAN })).to.be
+            .true;
+          expect(
+            depends({ preparerType: PREPARER_TYPES.THIRD_PARTY_NON_VETERAN }),
+          ).to.be.true;
+        });
+      });
+
+      describe('veteranIdentificationInformationPageA.depends', () => {
+        const {
+          depends,
+        } = formConfig.chapters.personalInformationChapter.pages.veteranIdentificationInformationPageA;
+
+        it('returns TRUE if preparerType is veteran or third-party-veteran', () => {
+          expect(
+            depends({
+              preparerType: PREPARER_TYPES.VETERAN,
+            }),
+          ).to.be.true;
+          expect(
+            depends({
+              preparerType: PREPARER_TYPES.THIRD_PARTY_VETERAN,
+            }),
+          ).to.be.true;
+        });
+
+        it('returns FALSE if preparerType is non-veteran or third-party-non-veteran', () => {
+          expect(depends({ preparerType: PREPARER_TYPES.NON_VETERAN })).to.be
+            .false;
+          expect(
+            depends({ preparerType: PREPARER_TYPES.THIRD_PARTY_NON_VETERAN }),
+          ).to.be.false;
+        });
+      });
+
+      describe('nonVeteranIdentificationInformationPage.depends', () => {
+        const {
+          depends,
+        } = formConfig.chapters.personalInformationChapter.pages.nonVeteranIdentificationInformationPage;
+
+        it('returns FALSE if preparerType is veteran or third-party-veteran', () => {
+          expect(
+            depends({
+              preparerType: PREPARER_TYPES.VETERAN,
+            }),
+          ).to.be.false;
+          expect(
+            depends({
+              preparerType: PREPARER_TYPES.THIRD_PARTY_VETERAN,
+            }),
+          ).to.be.false;
+        });
+
+        it('returns TRUE if preparerType is non-veteran or third-party-non-veteran', () => {
+          expect(depends({ preparerType: PREPARER_TYPES.NON_VETERAN })).to.be
+            .true;
+          expect(
+            depends({ preparerType: PREPARER_TYPES.THIRD_PARTY_NON_VETERAN }),
+          ).to.be.true;
+        });
+      });
     });
   });
 });
