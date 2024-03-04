446494e0fe23acf73a637762734120ec2da47dcc
tynyttie <ty_barnes_jobs@yahoo.com>
2024-03-04 14:45:05 -0500

    MHV-53672 Ensure-SideNav-Highlights-For-Details-Pages (#28102)

--- src/applications/mhv/medical-records/tests/util/helpers.unit.spec.jsx
+++ src/applications/mhv/medical-records/tests/util/helpers.unit.spec.jsx
@@ -9,6 +9,7 @@ import {
   processList,
   extractContainedResource,
   dispatchDetails,
+  getActiveLinksStyle,
 } from '../../util/helpers';
 
 describe('Name formatter', () => {
@@ -194,5 +195,67 @@ describe('dispatchDetails function', () => {
       type: 'GET',
       response: { id: '1', data: 'Sample Data' },
     }); // Check if dispatch was called with the expected arguments
+  });
+});
+
+describe('getActiveLinksStyle', () => {
+  it('returns "is-active" when linkPath and currentPath are both "/"', () => {
+    const linkPath = '/';
+    const currentPath = '/';
+    const expectedStyle = 'is-active';
+
+    const style = getActiveLinksStyle(linkPath, currentPath);
+
+    expect(style).to.equal(expectedStyle);
+  });
+
+  it('returns "is-active" when linkPath and currentPath have the same second segment', () => {
+    const linkPath = '/example/1';
+    const currentPath = '/example/1/some-page';
+    const expectedStyle = 'is-active';
+
+    const style = getActiveLinksStyle(linkPath, currentPath);
+
+    expect(style).to.equal(expectedStyle);
+  });
+
+  it('returns "is-active" when linkPath and currentPath have the same second segment for labs-and-tests', () => {
+    const linkPath = '/labs-and-tests/some-page';
+    const currentPath = '/labs-and-tests/some-page';
+    const expectedStyle = 'is-active';
+
+    const style = getActiveLinksStyle(linkPath, currentPath);
+
+    expect(style).to.equal(expectedStyle);
+  });
+
+  it('returns "is-active" when linkPath and currentPath have the same second segment for three-part paths', () => {
+    const linkPath = '/example/1';
+    const currentPath = '/example/1/some-page';
+    const expectedStyle = 'is-active';
+
+    const style = getActiveLinksStyle(linkPath, currentPath);
+
+    expect(style).to.equal(expectedStyle);
+  });
+
+  it('returns an empty string when linkPath and currentPath have different second segments', () => {
+    const linkPath = '/example/1';
+    const currentPath = '/other-example/1/some-page';
+    const expectedStyle = '';
+
+    const style = getActiveLinksStyle(linkPath, currentPath);
+
+    expect(style).to.equal(expectedStyle);
+  });
+
+  it('returns an empty string when linkPath and currentPath have no second segment', () => {
+    const linkPath = '/example';
+    const currentPath = '/other-example';
+    const expectedStyle = '';
+
+    const style = getActiveLinksStyle(linkPath, currentPath);
+
+    expect(style).to.equal(expectedStyle);
   });
 });
