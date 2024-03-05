69abb6af95e70c27615479aa466167c95efc1d84
jerekshoe <jerek.shoemaker@adhocteam.us>
2024-03-05 12:58:38 -0600

    Updating e2e tests to work with Lighthouse version of claim details instead of EVSS

--- src/applications/claims-status/tests/e2e/01.claim-status.cypress.spec.js
+++ src/applications/claims-status/tests/e2e/01.claim-status.cypress.spec.js
@@ -1,17 +1,12 @@
 import TrackClaimsPage from './page-objects/TrackClaimsPage';
 import claimsList from './fixtures/mocks/lighthouse/claims-list.json';
-import claimDetail from './fixtures/mocks/lighthouse/claim-detail.json';
+import claimDetails from './fixtures/mocks/lighthouse/claim-detail-closed.json';
 
 describe('Claims status test', () => {
   it('Shows the correct status for the claim - C30820', () => {
     const trackClaimsPage = new TrackClaimsPage();
-    trackClaimsPage.loadPage(claimsList, claimDetail);
+    trackClaimsPage.loadPage(claimsList, claimDetails);
     trackClaimsPage.verifyInProgressClaim(false);
-    trackClaimsPage.verifyCompletedSteps(5);
-    trackClaimsPage.verifyClosedClaim();
-    trackClaimsPage.axeCheckClaimDetails();
-    trackClaimsPage.verifyItemsNeedAttention(2);
-    cy.expandAccordions();
     cy.axeCheck();
   });
 });
