4ee68f1c3a3c1c8f4b61c30fa8bad275e6e86c71
Robert Hasselle <robert.hasselle@oddball.io>
2024-03-05 11:27:15 -0600

    list and loop cards refactor and show updated alert

--- src/applications/simple-forms/mock-simple-forms-patterns/arrayBuilder/helpers.js
+++ src/applications/simple-forms/mock-simple-forms-patterns/arrayBuilder/helpers.js
@@ -51,3 +51,54 @@ export function onNavForwardKeepUrlParams({ goNextPath, urlParams }) {
 export function onNavBackKeepUrlParams({ goPreviousPath, urlParams }) {
   goPreviousPath(urlParams);
 }
+
+/**
+ * Gets the query params from the URL and parses them as an object
+ *
+ * @param {string} [queryString] optional e.g. `?add=true`
+ * @returns {object} e.g. `{ add: 'true' }`
+ */
+export function parseQueryParams(queryString = window.location?.search) {
+  return queryString?.split('?')?.reduce((acc, query) => {
+    const [key, value] = query.split('=');
+    return { ...acc, [key]: value };
+  }, {});
+}
+
+/**
+ * Creates a path with a `add` query param
+ * @param {Object} props
+ * @param {string} props.basePath e.g. `/path-summary`
+ * @param {string | number} props.index
+ * @returns {string} e.g. `/path-summary/0?add=true`
+ */
+export function createArrayBuilderItemAddPath({ basePath, index }) {
+  return `${basePath}/${index}?add=true`;
+}
+
+/**
+ * Creates a path with a `edit` query param
+ * @param {Object} props
+ * @param {string} props.basePath e.g. `/path-summary`
+ * @param {string | number} props.index
+ * @returns {string} e.g. `/path-summary/0?edit=true`
+ */
+export function createArrayBuilderItemEditPath({ basePath, index }) {
+  return `${basePath}/${index}?edit=true`;
+}
+
+/**
+ * Creates a path with a `updated` query param
+ * @param {Object} props
+ * @param {string} props.basePath e.g. `/path-summary`
+ * @param {string} props.nounSingular e.g. `employer`
+ * @param {string | number} props.index
+ * @returns {string} e.g. `/path-summary?updated=employer-0`
+ */
+export function createArrayBuilderSummaryUpdatedPath({
+  basePath,
+  nounSingular,
+  index,
+}) {
+  return `${basePath}?updated=${nounSingular}-${index}`;
+}
