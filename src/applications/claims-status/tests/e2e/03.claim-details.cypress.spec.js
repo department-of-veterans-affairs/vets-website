7742edf189370155ee27786aec80d52cb64dd204
Jerek Shoemaker <jerek.shoemaker@adhocteam.us>
2024-03-06 11:33:07 -0600

    [CST] Remove EVSS code from DocumentRequestPage component (#28351)
    * Removing EVSS code from the `DocumentRequestPage` component
    
    * Updating e2e tests to work with Lighthouse version of claim details instead of EVSS

--- src/applications/claims-status/tests/e2e/03.claim-details.cypress.spec.js
+++ src/applications/claims-status/tests/e2e/03.claim-details.cypress.spec.js
@@ -1,18 +1,11 @@
 import TrackClaimsPage from './page-objects/TrackClaimsPage';
 import claimsList from './fixtures/mocks/lighthouse/claims-list.json';
-
-let mockDetails = {};
-
-beforeEach(() => {
-  cy.initClaimDetailMocks(false, true, false, 8).then(data => {
-    mockDetails = data;
-  });
-});
+import claimDetail from './fixtures/mocks/lighthouse/claim-detail.json';
 
 describe('Claim Details Test', () => {
   it('Shows the correct details - C30825', () => {
     const trackClaimsPage = new TrackClaimsPage();
-    trackClaimsPage.loadPage(claimsList, mockDetails);
+    trackClaimsPage.loadPage(claimsList, claimDetail);
     trackClaimsPage.checkClaimsContent();
     trackClaimsPage.claimDetailsTab();
     trackClaimsPage.verifyClaimDetails();
