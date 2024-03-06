e3488300b15e0ca2fd289472f2009ecf933ee78c
Tony Williams <tony.williams@va.gov>
2024-03-06 09:50:20 -0600

    fix failing tests

--- src/applications/vaos/tests/e2e/page-objects/ConfirmationPageObject.js
+++ src/applications/vaos/tests/e2e/page-objects/ConfirmationPageObject.js
@@ -3,6 +3,7 @@ import PageObject from './PageObject';
 export class ConfirmationPageObject extends PageObject {
   assertUrl({ isDirect = true } = {}) {
     cy.url().should('include', isDirect ? '/mock1' : '/pending');
+    cy.wait('@v2:get:appointments');
     cy.axeCheckBestPractice();
 
     return this;
