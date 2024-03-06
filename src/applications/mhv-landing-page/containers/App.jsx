cc37b386f02dbb7b3f425223a90f797e0d82dddb
James Zaccardo <james.zaccardo@adhocteam.us>
2024-03-06 15:06:28 -0500

    Update mhv-landing-page to USWDS v3 components (#28323)
    * test v3 components
    
    * add role to no alert

--- src/applications/mhv-landing-page/containers/App.jsx
+++ src/applications/mhv-landing-page/containers/App.jsx
@@ -107,7 +107,7 @@ const App = () => {
         <va-loading-indicator
           data-testid="mhv-landing-page-loading"
           message="Please wait..."
-          uswds="false"
+          uswds
         />
       </div>
     );
