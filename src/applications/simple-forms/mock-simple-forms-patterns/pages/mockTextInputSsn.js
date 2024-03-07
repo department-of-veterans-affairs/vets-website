f394ba970be6cff49487adf15007ede24027b69f
Matt Long <longmd@me.com>
2024-03-07 14:16:24 -0500

    Remove unnecessary VA File Number input

--- src/applications/simple-forms/mock-simple-forms-patterns/pages/mockTextInputSsn.js
+++ src/applications/simple-forms/mock-simple-forms-patterns/pages/mockTextInputSsn.js
@@ -2,30 +2,17 @@ import {
   ssnOrVaFileNumberUI,
   ssnOrVaFileNumberSchema,
   titleUI,
-  inlineTitleUI,
-  inlineTitleSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
 
 /** @type {PageSchema} */
 export default {
   uiSchema: {
-    ...titleUI('RJSF'),
-    vaFileNumber: {
-      'ui:title': 'VA file number',
-      'ui:errorMessages': {
-        pattern: 'Your VA file number must be 8 or 9 digits',
-      },
-    },
-    'view:wcv3Title': inlineTitleUI('Web component v3'),
+    ...titleUI('Web component v3'),
     wcv3SsnNew: ssnOrVaFileNumberUI(),
   },
   schema: {
     type: 'object',
     properties: {
-      vaFileNumber: {
-        $ref: '#/definitions/vaFileNumber',
-      },
-      'view:wcv3Title': inlineTitleSchema,
       wcv3SsnNew: ssnOrVaFileNumberSchema,
     },
     required: ['wcv3SsnNew'],
--- src/applications/simple-forms/mock-simple-forms-patterns/tests/pages/mockTextInputSsn.unit.spec.jsx
+++ src/applications/simple-forms/mock-simple-forms-patterns/tests/pages/mockTextInputSsn.unit.spec.jsx
@@ -1,7 +1,5 @@
 import {
-  testNumberOfErrorsOnSubmit,
   testNumberOfErrorsOnSubmitForWebComponents,
-  testNumberOfFields,
   testNumberOfWebComponentFields,
 } from '../../../shared/tests/pages/pageTests.spec';
 import formConfig from '../../config/form';
@@ -25,23 +23,5 @@ testNumberOfErrorsOnSubmitForWebComponents(
   schema,
   uiSchema,
   expectedNumberOfWebComponentErrors,
-  pageTitle,
-);
-
-const expectedNumberOfFields = 1;
-testNumberOfFields(
-  formConfig,
-  schema,
-  uiSchema,
-  expectedNumberOfFields,
-  pageTitle,
-);
-
-const expectedNumberOfErrors = 0;
-testNumberOfErrorsOnSubmit(
-  formConfig,
-  schema,
-  uiSchema,
-  expectedNumberOfErrors,
   pageTitle,
 );
