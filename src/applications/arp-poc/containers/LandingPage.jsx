13c917232be7b4dc253a6a1dd5ffcbfed2a60d66
Oren Mittman <nihil2501@gmail.com>
2024-03-07 02:03:50 -0500

    ARF-77596 add USiP link to ARP POC landing page

--- src/applications/arp-poc/containers/LandingPage.jsx
+++ src/applications/arp-poc/containers/LandingPage.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import { SIGN_IN_URL } from '../constants';
+
 export default function LandingPage() {
-  return <div>Landing page</div>;
+  return (
+    <>
+      <div>Landing page</div>
+
+      <a href={SIGN_IN_URL}>Sign in or create an account</a>
+    </>
+  );
 }
