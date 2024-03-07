8e539eef12a5c19b3d1f4d2e8a283d4b6c87555c
Lee Delarm <lee.delarm@agile6.com>
2024-03-07 15:52:16 -0500

    Adding travel agreement tests

---
+++ src/applications/check-in/travel-claim/pages/travel-agreement/index.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+
+import Wrapper from '../../../components/layout/Wrapper';
+import BackButton from '../../../components/BackButton';
+import { useFormRouting } from '../../../hooks/useFormRouting';
+
+const TravelAgreement = props => {
+  const { router } = props;
+  const { t } = useTranslation();
+
+  const { goToPreviousPage } = useFormRouting(router);
+  const finishReview = () => {
+    goToPreviousPage();
+  };
+
+  return (
+    <>
+      <BackButton
+        router={router}
+        action={goToPreviousPage}
+        prevUrl="#back"
+        text={t('back-to-last-screen')}
+      />
+      <Wrapper
+        pageTitle="Travel Agreement"
+        classNames="travel-page"
+        withBackButton
+      >
+        <ul data-testid="agreement-list-items">
+          <li>{t('i-have-incurred-a-cost-in-relation')}</li>
+          <li>
+            {t('i-have-neither-obtained-transportion-at-government-expense')}
+          </li>
+          <li>{t('i-have-not-received-other-transportion-resources')}</li>
+          <li>{t('i-am-the-only-person-claiming-for-the-travel-listed')}</li>
+          <li>{t('i-have-not-previously-received-payment')}</li>
+        </ul>
+        <a
+          data-testid="file-claim-link"
+          href="#travel-review"
+          onKeyDown={finishReview}
+          onClick={finishReview}
+          className="vads-u-font-weight--bold"
+        >
+          {t('finish-reviewing-your-travel-claim')}
+        </a>
+      </Wrapper>
+    </>
+  );
+};
+
+TravelAgreement.propTypes = {
+  router: PropTypes.object,
+};
+
+export default TravelAgreement;
---
+++ src/applications/check-in/travel-claim/pages/travel-agreement/travelAgreement.unit.spec.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import CheckInProvider from '../../../tests/unit/utils/CheckInProvider';
+import TravelAgreement from './index'; // adjust the path as needed
+import * as useFormRoutingModule from '../../../hooks/useFormRouting';
+
+describe('TravelAgreement page ', () => {
+  const store = {
+    formPages: ['travel-agreement', 'travel-mileage'],
+    app: 'travelClaim',
+  };
+  const sandbox = sinon.createSandbox();
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('renders without crashing', () => {
+    const component = render(
+      <CheckInProvider
+        store={store}
+        router={{
+          currentPage: '/travel-agreement',
+        }}
+      >
+        <TravelAgreement />
+      </CheckInProvider>,
+    );
+
+    expect(component).to.exist;
+  });
+
+  it('renders agreement list text', () => {
+    const push = sinon.spy();
+    const component = render(
+      <CheckInProvider
+        store={store}
+        router={{
+          push,
+          currentPage: '/travel-agreement',
+        }}
+      >
+        <TravelAgreement />
+      </CheckInProvider>,
+    );
+
+    expect(component.getByTestId('agreement-list-items')).to.exist;
+  });
+
+  it('calls goToPreviousPage when file-claim-link is clicked', () => {
+    const goToPreviousPageSpy = sinon.spy();
+    sandbox.stub(useFormRoutingModule, 'useFormRouting').returns({
+      goToPreviousPage: goToPreviousPageSpy,
+      getCurrentPageFromRouter: () => {},
+      getPreviousPageFromRouter: () => {},
+    });
+
+    const component = render(
+      <CheckInProvider store={store}>
+        <TravelAgreement
+          router={{
+            currentPage: '/travel-agreement',
+          }}
+        />
+      </CheckInProvider>,
+    );
+
+    expect(component.getByTestId('file-claim-link')).to.exist;
+    fireEvent.click(component.getByTestId('file-claim-link'));
+    expect(goToPreviousPageSpy.calledOnce).to.be.true;
+    sandbox.restore();
+  });
+
+  it('renders link with correct href', () => {
+    const component = render(
+      <CheckInProvider
+        store={store}
+        router={{
+          currentPage: '/travel-agreement',
+        }}
+      >
+        <TravelAgreement />
+      </CheckInProvider>,
+    );
+
+    const link = component.getByTestId('file-claim-link');
+    expect(link.getAttribute('href')).to.equal('#travel-review');
+  });
+});
