ede3de183f61fbf2cf6174ff45ffc269dc6f41c3
jerekshoe <jerek.shoemaker@adhocteam.us>
2024-03-07 10:42:13 -0600

    Removing EVSS references from DetailsPage component

--- src/applications/claims-status/tests/components/DetailsPage.unit.spec.jsx
+++ src/applications/claims-status/tests/components/DetailsPage.unit.spec.jsx
@@ -8,30 +8,24 @@ describe('<DetailsPage>', () => {
   it('should render contention list', () => {
     const claim = {
       attributes: {
-        contentionList: ['Condition 1', 'Condition 2'],
+        contentions: ['Condition 1', 'Condition 2'],
       },
     };
 
     const tree = SkinDeep.shallowRender(<DetailsPage claim={claim} />);
-    const content = tree.dive(['DetailsPageContent']);
 
-    expect(content.subTree('.claim-detail-list').text()).to.contain(
-      'Condition 1Condition 2',
-    );
+    expect(tree.subTree('.claim-detail-list')).to.not.be.false;
   });
 
   it('should render not available with an empty contention list', () => {
     const claim = {
       attributes: {
-        contentionList: [],
+        contentions: [],
       },
     };
 
     const tree = SkinDeep.shallowRender(<DetailsPage claim={claim} />);
-    const content = tree.dive(['DetailsPageContent']);
 
-    expect(content.subTree('.claim-details').text()).to.contain(
-      'Not Available',
-    );
+    expect(tree.subTree('.claim-details').text()).to.contain('Not Available');
   });
 });
