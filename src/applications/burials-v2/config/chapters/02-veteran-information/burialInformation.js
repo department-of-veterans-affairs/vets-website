11fee6319d012c6e6b732230969d0f6fbdbfe00a
Midge-dev <sean.midgley.dev@gmail.com>
2024-03-11 10:40:47 -0700

    remove burial date warning

--- src/applications/burials-v2/config/chapters/02-veteran-information/burialInformation.js
+++ src/applications/burials-v2/config/chapters/02-veteran-information/burialInformation.js
@@ -1,12 +1,6 @@
 import fullSchemaBurials from 'vets-json-schema/dist/21P-530V2-schema.json';
-import { isFullDate } from '@department-of-veterans-affairs/platform-forms/validations';
 import { dateOfDeathUI } from '@department-of-veterans-affairs/platform-forms-system/web-component-patterns';
-import { validateBurialAndDeathDates } from '../../../utils/validation';
-import {
-  isEligibleNonService,
-  generateTitle,
-  BurialDateWarning,
-} from '../../../utils/helpers';
+import { generateTitle } from '../../../utils/helpers';
 
 const { deathDate, burialDate } = fullSchemaBurials.properties;
 
@@ -17,21 +11,6 @@ export default {
     burialDate: dateOfDeathUI(
       'Date of burial (includes cremation or interment)',
     ),
-    'view:burialDateWarning': {
-      'ui:description': BurialDateWarning,
-      'ui:options': {
-        hideIf: formData => {
-          // If they haven’t entered a complete year, don’t jump the gun and show the warning
-          if (formData.burialDate && !isFullDate(formData.burialDate)) {
-            return true;
-          }
-
-          // Show the warning if the burial date was more than 2 years ago
-          return isEligibleNonService(formData.burialDate);
-        },
-      },
-    },
-    'ui:validations': [validateBurialAndDeathDates],
   },
   schema: {
     type: 'object',
