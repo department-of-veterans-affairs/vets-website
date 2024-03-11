fd3b411e8052957e230ebe14f17257067393b7d8
Matt Long <longmd@me.com>
2024-03-11 11:10:13 -0400

    Init commit

--- src/applications/hca/components/FormFooter/index.jsx
+++ src/applications/hca/components/FormFooter/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
-import { shouldHideFormFooter } from '../../utils/selectors';
 
 const FormFooter = ({ formConfig, currentLocation }) => {
   const GetHelp = formConfig.getHelp;
-  const hideFooter = useSelector(shouldHideFormFooter);
   const trimmedPathname = currentLocation.pathname.replace(/\/$/, '');
   const isConfirmationPage = trimmedPathname.endsWith('confirmation');
 
-  return !isConfirmationPage && !hideFooter ? (
+  return !isConfirmationPage ? (
     <div className="row">
       <div className="usa-width-two-thirds medium-8 columns">
         <div className="help-footer-box">
