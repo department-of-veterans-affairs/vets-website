be26d2078afc5282bd3a6f015bfb19ec0add596d
Micah Chiang <micahkchiang@gmail.com>
2024-03-11 21:24:30 -0700

    breadcrumbs
    Signed-off-by: Micah Chiang <micahkchiang@gmail.com>

--- src/applications/disability-benefits/2346/containers/App.jsx
+++ src/applications/disability-benefits/2346/containers/App.jsx
@@ -65,7 +65,7 @@ class App extends Component {
       <>
         {!featureToggles.loading && (
           <div className="large-screen:vads-u-padding-left--0 vads-u-padding-left--2">
-            <VaBreadcrumbs label="Breadcrumb">
+            <VaBreadcrumbs uswds="false" label="Breadcrumb">
               <a href="/">Home</a>
               {/* this will get updated when this route is added */}
               <a href="/health-care">Health care</a>
