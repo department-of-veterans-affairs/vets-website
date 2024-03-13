84d2ce0c7c60aa1b26c07ea5476e8ee2bb6eff4f
Robert Hasselle <123402053+rhasselle-oddball@users.noreply.github.com>
2024-03-13 11:13:43 -0500

    More refactors on array builder pattern (#28526)

--- src/applications/simple-forms/mock-simple-forms-patterns/arrayBuilder/helpers.js
+++ src/applications/simple-forms/mock-simple-forms-patterns/arrayBuilder/helpers.js
@@ -53,19 +53,6 @@ export function onNavBackKeepUrlParams({ goPreviousPath, urlParams }) {
 }
 
 /**
- * Gets the query params from the URL and parses them as an object
- *
- * @param {string} [queryString] optional e.g. `?add=true`
- * @returns {object} e.g. `{ add: 'true' }`
- */
-export function parseQueryParams(queryString = window.location?.search) {
-  return queryString?.split('?')?.reduce((acc, query) => {
-    const [key, value] = query.split('=');
-    return { ...acc, [key]: value };
-  }, {});
-}
-
-/**
  * Creates a path with a `add` query param
  * @param {Object} props
  * @param {string} props.basePath e.g. `/path-summary`
@@ -81,10 +68,11 @@ export function createArrayBuilderItemAddPath({ basePath, index }) {
  * @param {Object} props
  * @param {string} props.basePath e.g. `/path-summary`
  * @param {string | number} props.index
+ * @param {boolean} [props.isReview] if coming from the review page
  * @returns {string} e.g. `/path-summary/0?edit=true`
  */
-export function createArrayBuilderItemEditPath({ basePath, index }) {
-  return `${basePath}/${index}?edit=true`;
+export function createArrayBuilderItemEditPath({ basePath, index, isReview }) {
+  return `${basePath}/${index}?edit=true${isReview ? '&review=true' : ''}`;
 }
 
 /**
@@ -95,7 +83,7 @@ export function createArrayBuilderItemEditPath({ basePath, index }) {
  * @param {string | number} props.index
  * @returns {string} e.g. `/path-summary?updated=employer-0`
  */
-export function createArrayBuilderSummaryUpdatedPath({
+export function createArrayBuilderUpdatedPath({
   basePath,
   nounSingular,
   index,
