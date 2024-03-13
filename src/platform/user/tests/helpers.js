163313aff01232e28c6010118541ecdaf23e51e5
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2024-03-13 14:34:51 -0400

    adds examples so people who want to perform a11y checks know how to

--- src/platform/user/tests/helpers.js
+++ src/platform/user/tests/helpers.js
@@ -2,10 +2,23 @@ import { render } from '@testing-library/react';
 
 /**
  *
- * @param {*} component A React component or HTML
- * @param {*} options
- * @param {*} options.isWrapped Defaults to 'false', only use when the componet is wrapped with react-testing-library
- * @returns
+ * Examples:
+ * ```
+ * // If normal React component (aka not wrapped by RTL or RTL helpers)
+ * it('should be accessible for normal React component', async () => {
+ *    const component = renderComponentForA11y(<ComponentName />)
+ *    await expect(component).to.be.accessible();
+ * })
+ *
+ * // If using a wrapped component (wrapped by RTL, or our helpers that require the router and/or redux)
+ * it('should be accessible for RTL helper component', async () => {
+ *    const { container } = renderWithStoreAndRouter(<ProfileAlert />, {
+ *      path: PROFILE_PATHS.PROFILE_ROOT,
+ *    });
+ *    const component = renderComponentForA11y(container, { isWrapped: true });
+ *    await expect(component).to.be.accessible();
+ * })
+ * ```
  */
 export function renderComponentForA11y(component, { isWrapped = false } = {}) {
   const parentNode = document.createElement('div');
