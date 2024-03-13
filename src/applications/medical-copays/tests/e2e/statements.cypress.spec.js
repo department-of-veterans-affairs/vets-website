7647e1563d2f4beb276913436a8007e9a0e7f5b9
Micah Chiang <micahkchiang@gmail.com>
2024-03-12 17:00:04 -0700

    remove trailing slash
    Signed-off-by: Micah Chiang <micahkchiang@gmail.com>

--- src/applications/medical-copays/tests/e2e/statements.cypress.spec.js
+++ src/applications/medical-copays/tests/e2e/statements.cypress.spec.js
@@ -18,7 +18,7 @@ describe('Medical Copays', () => {
     cy.intercept('GET', '/v0/feature_toggles*', mockFeatureToggles);
     cy.intercept('GET', '/v0/medical_copays', mockCopays);
     cy.intercept('GET', '/v0/debts', mockDebt);
-    cy.visit('/health-care/pay-copay-bill/your-current-balances/');
+    cy.visit('/health-care/pay-copay-bill/your-current-balances');
     cy.findByTestId('overview-page-title').should('exist');
     cy.injectAxe();
   });
