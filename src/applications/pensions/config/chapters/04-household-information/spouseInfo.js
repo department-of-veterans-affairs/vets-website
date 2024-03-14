f9b0eb1d5f17f2b69c7fffd7a4f70bb9b2adfcf1
Wayne Weibel <wayne.weibel@adhocteam.us>
2024-03-14 10:09:45 -0400

    Pension 76768 Spouse info - update deprecated fields

---
+++ src/applications/pensions/config/chapters/04-household-information/spouseInfo.js
@@ -0,0 +1,92 @@
+import merge from 'lodash/merge';
+
+import currentOrPastDateUI from 'platform/forms-system/src/js/definitions/currentOrPastDate';
+import {
+  ssnUI,
+  titleUI,
+} from 'platform/forms-system/src/js/web-component-patterns';
+import fullSchemaPensions from 'vets-json-schema/dist/21P-527EZ-schema.json';
+
+import { createSpouseLabelSelector } from '../../../helpers';
+
+import createHouseholdMemberTitle from '../../../components/DisclosureTitle';
+
+const {
+  spouseDateOfBirth,
+  spouseSocialSecurityNumber,
+  spouseVaFileNumber,
+  liveWithSpouse,
+  spouseIsVeteran,
+} = fullSchemaPensions.properties;
+
+/** @type {PageSchema} */
+export default {
+  uiSchema: {
+    ...titleUI(createHouseholdMemberTitle('spouseFullName', 'information')),
+    spouseDateOfBirth: merge({}, currentOrPastDateUI(''), {
+      'ui:options': {
+        updateSchema: createSpouseLabelSelector(
+          spouseName =>
+            `${spouseName.first} ${spouseName.last}’s date of birth`,
+        ),
+      },
+    }),
+    spouseSocialSecurityNumber: merge({}, ssnUI, {
+      'ui:title': '',
+      'ui:options': {
+        updateSchema: createSpouseLabelSelector(
+          spouseName =>
+            `${spouseName.first} ${spouseName.last}’s Social Security number`,
+        ),
+      },
+    }),
+    spouseIsVeteran: {
+      'ui:widget': 'yesNo',
+      'ui:options': {
+        updateSchema: createSpouseLabelSelector(
+          spouseName =>
+            `Is ${spouseName.first} ${spouseName.last} also a Veteran?`,
+        ),
+        yesNoReverse: true,
+        labels: {
+          Y: 'No',
+          N: 'Yes',
+        },
+      },
+    },
+    spouseVaFileNumber: {
+      'ui:title': 'Enter their VA file number if it does not match their SSN',
+      'ui:options': {
+        expandUnder: 'spouseIsVeteran',
+      },
+      'ui:errorMessages': {
+        pattern: 'Your VA file number must be 8 or 9 digits',
+      },
+    },
+    'view:liveWithSpouse': {
+      'ui:widget': 'yesNo',
+      'ui:options': {
+        updateSchema: createSpouseLabelSelector(
+          spouseName =>
+            `Do you live with ${spouseName.first} ${spouseName.last}?`,
+        ),
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    required: [
+      'spouseDateOfBirth',
+      'spouseSocialSecurityNumber',
+      'spouseIsVeteran',
+      'view:liveWithSpouse',
+    ],
+    properties: {
+      spouseDateOfBirth,
+      spouseSocialSecurityNumber,
+      spouseIsVeteran,
+      spouseVaFileNumber,
+      'view:liveWithSpouse': liveWithSpouse,
+    },
+  },
+};
