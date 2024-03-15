e389047fb39fe4830ec9a13935c7b64c63341304
Vitalii Menshutin <Vitalii.Menshutin@bylight.com>
2024-03-15 12:25:49 -0400

    MHV-55428: Styling of Downtime Notifications Error Message [Meds]

--- src/applications/mhv/medications/containers/App.jsx
+++ src/applications/mhv/medications/containers/App.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useMemo, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import FEATURE_FLAG_NAMES from '@department-of-veterans-affairs/platform-utilities/featureFlagNames';
 import { RequiredLoginView } from '@department-of-veterans-affairs/platform-user/RequiredLoginView';
 import { selectUser } from '@department-of-veterans-affairs/platform-user/selectors';
 import backendServices from '@department-of-veterans-affairs/platform-user/profile/backendServices';
+import { renderMHVDowntime } from '@department-of-veterans-affairs/mhv/exports';
 import {
   DowntimeNotification,
   externalServices,
+  externalServiceStatus,
 } from '@department-of-veterans-affairs/platform-monitoring/DowntimeNotification';
+import { getScheduledDowntime } from 'platform/monitoring/DowntimeNotification/actions';
 import { useDatadogRum } from '../../shared/hooks/useDatadogRum';
 import { medicationsUrls } from '../util/constants';
 
 const App = ({ children }) => {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
   const { featureTogglesLoading, appEnabled } = useSelector(
     state => {
@@ -24,6 +28,31 @@ const App = ({ children }) => {
     },
     state => state.featureToggles,
   );
+
+  const scheduledDowntimes = useSelector(
+    state => state.scheduledDowntime?.serviceMap || [],
+  );
+
+  const mhvMedsDown = useMemo(
+    () => {
+      if (scheduledDowntimes.size > 0) {
+        return (
+          scheduledDowntimes?.get(externalServices.mhvMeds)?.status ||
+          scheduledDowntimes?.get(externalServices.mhvPlatform)?.status
+        );
+      }
+      return 'downtime status: ok';
+    },
+    [scheduledDowntimes],
+  );
+
+  useEffect(
+    () => {
+      dispatch(getScheduledDowntime());
+    },
+    [dispatch],
+  );
+
   const datadogRumConfig = {
     applicationId: '2b875bc2-034a-445b-868c-d43bec8928d1',
     clientToken: 'pubb9b9c833770797060110a821283a0892',
@@ -64,12 +93,21 @@ const App = ({ children }) => {
       user={user}
       serviceRequired={[backendServices.USER_PROFILE]}
     >
-      <DowntimeNotification
-        appTitle="Medications"
-        dependencies={[externalServices.mhvPlatform, externalServices.mhvMeds]}
-      >
-        {children}
-      </DowntimeNotification>
+      {mhvMedsDown === externalServiceStatus.down ? (
+        <>
+          <h1 className="vads-u-padding-top--4">Medications</h1>
+          <DowntimeNotification
+            appTitle="Medications"
+            dependencies={[
+              externalServices.mhvPlatform,
+              externalServices.mhvMeds,
+            ]}
+            render={renderMHVDowntime}
+          />
+        </>
+      ) : (
+        children
+      )}
     </RequiredLoginView>
   );
 };
