480284c09217b81af85ce6effde21f381bf72d6a
Dan Hinze <hinzed1127@gmail.com>
2024-03-15 15:58:02 -0500

    Update breadcrumbs component to newer version

--- src/applications/travel-pay/components/Breadcrumbs.jsx
+++ src/applications/travel-pay/components/Breadcrumbs.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { VaBreadcrumbs } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 
 export default function BreadCrumbs() {
   return (
-    <va-breadcrumbs label="Breadcrumb">
-      <a href="/">Home</a>
-      <a href="/health-care">Health Care</a>
-      <a href="/health-care/file-travel-claim">File Travel Claim</a>
-    </va-breadcrumbs>
+    <VaBreadcrumbs
+      breadcrumbList={[
+        {
+          href: '/',
+          // the `home-veterans-affairs` prop defaults to this
+          // label, but including it here to be explicit.
+          label: 'VA.gov home',
+        },
+        {
+          href: '/health-care',
+          label: 'Health Care',
+        },
+        {
+          href: '/health-care/file-travel-claim',
+          label: 'Travel Claims',
+        },
+      ]}
+      label="Breadcrumb"
+      uswds
+      wrapping
+    />
   );
 }
