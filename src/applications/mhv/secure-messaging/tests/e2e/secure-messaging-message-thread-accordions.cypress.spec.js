d87023bd6d4c6ee1ae7f7ba6c050e37e1b1be7f2
fazilqa <fazil.mammadov@bylight.com>
2024-03-18 09:15:02 -0700

    upd: update assertions

--- src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-message-thread-accordions.cypress.spec.js
+++ src/applications/mhv/secure-messaging/tests/e2e/secure-messaging-message-thread-accordions.cypress.spec.js
@@ -16,12 +16,16 @@ describe('Secure Messaging Thread Details', () => {
     landingPage.loadInboxMessages();
     landingPage.loadSingleThread(mockThreadResponse, date);
 
-    // cy.pause();
-    for (let i = 1; i < messageIdList.length; i += 1) {
-      cy.get(
-        `[data-testid="expand-message-button-${messageIdList[i]}"]`,
-      ).click({ force: true });
+    cy.get(`[data-testid="expand-message-button-${messageIdList[0]}"]`)
+      .shadow()
+      .find('button')
+      .should('have.attr', 'aria-expanded', 'true');
 
+    for (let i = 1; i < messageIdList.length; i += 1) {
+      cy.get(`[data-testid="expand-message-button-${messageIdList[i]}"]`)
+        .shadow()
+        .find('button')
+        .click({ force: true });
       cy.get(`[data-testid="expand-message-button-${messageIdList[i]}"]`)
         .shadow()
         .find('button')
