a9892871f73b249a29950192588d83aba09c6b40
Lee Delarm <lee.delarm@agile6.com>
2024-03-19 16:45:59 -0400

    Updated tests

--- src/applications/check-in/day-of/pages/ArrivedAtFacility.jsx
+++ src/applications/check-in/day-of/pages/ArrivedAtFacility.jsx
@@ -6,7 +6,7 @@ import { recordEvent } from '@department-of-veterans-affairs/platform-monitoring
 import { useFormRouting } from '../../hooks/useFormRouting';
 import { recordAnswer } from '../../actions/universal';
 import { createAnalyticsSlug } from '../../utils/analytics';
-import { makeSelectCurrentContext, makeSelectApp } from '../../selectors';
+import { makeSelectCurrentContext } from '../../selectors';
 
 import Wrapper from '../../components/layout/Wrapper';
 
@@ -16,50 +16,34 @@ const ArrivedAtFacility = props => {
   const { router } = props;
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const { goToNextPage, getCurrentPageFromRouter } = useFormRouting(router);
-  const currentPage = getCurrentPageFromRouter();
+  const { goToNextPage } = useFormRouting(router);
 
   const pageType = 'arrived-at-facility';
   const testID = 'arrived-page';
-  const bodyText = (
-    <>
-      <p>{t('we-ask-this-question-because-staff-can-call-you')}</p>
-    </>
-  );
 
   const selectCurrentContext = useMemo(makeSelectCurrentContext, []);
   const { setECheckinStartedCalled } = useSelector(selectCurrentContext);
-  const selectApp = useMemo(makeSelectApp, []);
-  const { app } = useSelector(selectApp);
 
   const onClick = event => {
     const answer = event.target.attributes.value.value;
     recordEvent({
       event: createAnalyticsSlug(
         `${answer}-to-${pageType}${
-          setECheckinStartedCalled || app !== APP_NAMES.CHECK_IN ? '' : '-45MR'
+          setECheckinStartedCalled ? '' : '-45MR'
         }-clicked`,
         'nav',
-        app,
+        APP_NAMES.CHECK_IN,
       ),
     });
   };
 
   const yesAction = event => {
-    recordEvent({
-      event: createAnalyticsSlug('arrived-at-facility-yes'),
-      fromPage: currentPage,
-    });
     dispatch(recordAnswer({ [pageType]: 'yes' }));
     onClick(event);
     goToNextPage();
   };
 
   const noAction = event => {
-    recordEvent({
-      event: createAnalyticsSlug('arrived-at-facility-no'),
-      fromPage: currentPage,
-    });
     dispatch(recordAnswer({ [pageType]: 'no' }));
     onClick(event);
     goToNextPage();
@@ -69,13 +53,12 @@ const ArrivedAtFacility = props => {
     <>
       <Wrapper
         pageTitle={t('have-you-arrived-at-your-va-health-facility')}
+        eyebrow={t('check-in')}
         testID={testID}
       >
-        {bodyText && (
-          <div data-testid="body-text" className="vads-u-margin-bottom--3">
-            {bodyText}
-          </div>
-        )}
+        <div data-testid="body-text" className="vads-u-margin-bottom--3">
+          <p>{t('we-ask-this-question-because-staff-can-call-you')}</p>
+        </div>
         <div className="vads-u-display--flex vads-u-flex-direction--column vads-u-align-itmes--stretch small-screen:vads-u-flex-direction--row">
           <va-button
             uswds
