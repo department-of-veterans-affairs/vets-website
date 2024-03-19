27b5ce16c29596ccbc77d9cd34c2cd7b233ab78a
William Phelps <williamphelps13@gmail.com>
2024-03-19 13:47:37 -0600

    Revert entryFile and entryName and corresponding url paths

--- src/applications/representatives/tests/components/common/Breadcrumbs.unit.spec.js
+++ src/applications/representatives/tests/components/common/Breadcrumbs.unit.spec.js
@@ -7,7 +7,7 @@ import Breadcrumbs from '../../../components/common/Breadcrumbs';
 describe('breadcrumbs', () => {
   it('creates breadcrumbs for the dashboard', () => {
     const { getByText } = render(
-      <Breadcrumbs pathname="/representative/dashboard" />,
+      <Breadcrumbs pathname="/representatives/dashboard" />,
     );
     expect(getByText('Home')).to.exist;
     expect(getByText('Dashboard')).to.exist;
@@ -15,7 +15,7 @@ describe('breadcrumbs', () => {
 
   it('creates breadcrumbs for the permissions page', () => {
     const { getByText } = render(
-      <Breadcrumbs pathname="/representative/permissions" />,
+      <Breadcrumbs pathname="/representatives/permissions" />,
     );
     expect(getByText('Home')).to.exist;
     expect(getByText('Permissions')).to.exist;
@@ -23,14 +23,14 @@ describe('breadcrumbs', () => {
 
   it('creates breadcrumbs for the POA Requests page', () => {
     const { getByText } = render(
-      <Breadcrumbs pathname="/representative/poa-requests" />,
+      <Breadcrumbs pathname="/representatives/poa-requests" />,
     );
     expect(getByText('Home')).to.exist;
     expect(getByText('POA requests')).to.exist;
   });
 
   it('creates breadcrumbs for the home page by default', () => {
-    const { getByText } = render(<Breadcrumbs pathname="representative" />);
+    const { getByText } = render(<Breadcrumbs pathname="representatives" />);
     expect(getByText('Home')).to.exist;
   });
 });
