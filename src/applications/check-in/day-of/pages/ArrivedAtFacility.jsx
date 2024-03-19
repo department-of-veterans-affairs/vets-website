b9f24fa8f4cde9194c642f38c4afdba89adccbe2
Lee Delarm <lee.delarm@agile6.com>
2024-03-19 11:00:11 -0400

    Adding recordevent

--- src/applications/check-in/day-of/pages/ArrivedAtFacility.jsx
+++ src/applications/check-in/day-of/pages/ArrivedAtFacility.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { recordEvent } from '@department-of-veterans-affairs/platform-monitoring/exports';
 import { useFormRouting } from '../../hooks/useFormRouting';
 import { recordAnswer } from '../../actions/universal';
+import { createAnalyticsSlug } from '../../utils/analytics';
 
 import ArrivedPage from '../../components/pages/ArrivedPage';
 
@@ -11,7 +13,8 @@ const ArrivedAtFacility = props => {
   const { router } = props;
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const { goToNextPage } = useFormRouting(router);
+  const { goToNextPage, getCurrentPageFromRouter } = useFormRouting(router);
+  const currentPage = getCurrentPageFromRouter();
 
   const bodyText = (
     <>
@@ -20,11 +23,19 @@ const ArrivedAtFacility = props => {
   );
 
   const yesAction = () => {
+    recordEvent({
+      event: createAnalyticsSlug('check-in-arrived-at-facility-yes'),
+      fromPage: currentPage,
+    });
     dispatch(recordAnswer({ 'arrived-at-facility': 'yes' }));
     goToNextPage();
   };
 
   const noAction = () => {
+    recordEvent({
+      event: createAnalyticsSlug('check-in-arrived-at-facility-no'),
+      fromPage: currentPage,
+    });
     dispatch(recordAnswer({ 'arrived-at-facility': 'no' }));
     goToNextPage();
   };
