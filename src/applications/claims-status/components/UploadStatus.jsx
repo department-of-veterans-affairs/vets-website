85c4a74db775a0be1c2c2f50cf38bb940efae7d6
Peri McLaren <peri.mclaren@adhocteam.us>
2024-03-20 16:30:41 -0600

    undo changes

--- src/applications/claims-status/components/UploadStatus.jsx
+++ src/applications/claims-status/components/UploadStatus.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function UploadStatus({ files, progress, onCancel }) {
+export default function UploadStatus({ files, onCancel, progress }) {
   const handleClick = evt => {
     evt.preventDefault();
     onCancel();
