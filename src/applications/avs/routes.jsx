fc277a58a363e8bd19ebe34091ee5cdc1251a86a
James Zaccardo <jameszaccardo@gmail.com>
2024-03-21 02:33:18 -0400

    add prop type for auth guard

--- src/applications/avs/routes.jsx
+++ src/applications/avs/routes.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import PageNotFound from '@department-of-veterans-affairs/platform-site-wide/PageNotFound';
 import { isLOA1 } from '~/platform/user/selectors';
-import { useSelector } from 'react-redux';
-
 import ErrorBoundary from './components/ErrorBoundary';
-
 import Avs from './containers/Avs';
 
 // Redirect an unverified user to the My Health landing page
-
 const AuthGuard = ({ children }) => {
   const isUnverified = useSelector(isLOA1);
   if (isUnverified) {
     return <Redirect to="/my-health" />;
   }
   return children;
+};
+
+AuthGuard.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 const ErrorBoundaryWrapper = props => (
