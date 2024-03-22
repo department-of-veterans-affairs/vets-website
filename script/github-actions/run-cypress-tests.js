88e2fa5e9307cd68663862d02a871e150a1b1f14
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2024-03-22 13:25:11 -0400

    test: get longest test to run

--- script/github-actions/run-cypress-tests.js
+++ script/github-actions/run-cypress-tests.js
@@ -9,13 +9,14 @@ const appUrl = process.env.APP_URLS.split(',')[0];
 // If it has been selected, it is run in its own container in the last parallel container.
 let divider;
 let longestTestIsPresent = false;
-const longestTest = /long-ptsd.cypress.spec.js/g;
+const longestTestFilename = `long-ptsd.cypress.spec.js`;
+const longestTestPattern = new RegExp(longestTestFilename, 'g');
 const lastStep = numContainers - 1;
 
-if (tests.some(test => test.match(longestTest))) {
+if (tests.some(test => test.match(longestTestPattern))) {
   longestTestIsPresent = true;
   divider = Math.ceil(tests.length / (numContainers - 1));
-  tests = tests.filter(test => !test.match(longestTest));
+  tests = tests.filter(test => !test.match(longestTestPattern));
 } else {
   divider = Math.ceil(tests.length / numContainers);
 }
@@ -30,7 +31,7 @@ let status = null;
 
 if (longestTestIsPresent && step === lastStep) {
   status = runCommandSync(
-    `CYPRESS_EVERY_NTH_FRAME=1 yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.js" --spec 'src/applications/**/all-claims.cypress.spec.js' --env app_url=${appUrl}`,
+    `CYPRESS_EVERY_NTH_FRAME=1 yarn cy:run --browser chrome --headless --reporter cypress-multi-reporters --reporter-options "configFile=config/cypress-reporters.js" --spec 'src/applications/**/${longestTestFilename}' --env app_url=${appUrl}`,
   );
 } else if (batch !== '') {
   status = runCommandSync(
