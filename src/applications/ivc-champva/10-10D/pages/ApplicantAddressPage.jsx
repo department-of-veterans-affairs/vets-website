8456f0dae7eb404f2235c1c65cee8d8bd28d975e
Michael Clement <michael.clement@adhocteam.us>
2024-03-22 10:00:31 -0500

    polish

--- src/applications/ivc-champva/10-10D/pages/ApplicantAddressPage.jsx
+++ src/applications/ivc-champva/10-10D/pages/ApplicantAddressPage.jsx
@@ -7,9 +7,9 @@ import { titleUI } from 'platform/forms-system/src/js/web-component-patterns';
 import FormNavButtons from 'platform/forms-system/src/js/components/FormNavButtons';
 import PropTypes from 'prop-types';
 
-import { applicantWording } from '../helpers/wordingCustomization';
-
 export function ApplicantAddressCopyPage({
+  contentBeforeButtons,
+  contentAfterButtons,
   data,
   setFormData,
   goBack,
@@ -19,20 +19,22 @@ export function ApplicantAddressCopyPage({
   onReviewPage,
 }) {
   const currentApp = data?.applicants?.[pagePerItemIndex];
-  const [checkValue, setCheckValue] = useState(currentApp?.sharedAddress);
+  const [radioValue, setRadioValue] = useState(currentApp.hasSharedAddress);
   const [selectValue, setSelectValue] = useState(currentApp?.addressOriginator);
   const [address, setAddress] = useState(currentApp?.applicantAddress);
 
-  // TODO: Do we want/need this functionality?
-  // If we copied an address but then edited on next screen,
-  // add an indicator if the user ever comes back to this screen.
+  // TODO: Do we want/need this functionality? Commenting out for now.
+  // If we copied an address but then edited on next screen, track that
+  // info if the user ever comes back to this screen and we want to notify.
+  /*
   const [hasEditedAddress, setHasEditedAddress] = useState(
     currentApp?.addressOriginator &&
       JSON.stringify(currentApp?.applicantAddress) !==
         currentApp?.addressOriginator,
   );
+  */
 
-  const [checkError, setCheckError] = useState(undefined);
+  const [radioError, setRadioError] = useState(undefined);
   const [selectError, setSelectError] = useState(undefined);
   const [dirty, setDirty] = useState(false);
   const navButtons = <FormNavButtons goBack={goBack} submitToContinue />;
@@ -52,67 +54,63 @@ export function ApplicantAddressCopyPage({
   ];
 
   function isValidOrigin(person) {
-    // Make sure that our <select> only shows options
-    // that are:
+    // Make sure that our <select> only shows options that:
     // 1. Have a valid address we can copy
-    // 2. NOT the current applicant
+    // 2. Are NOT the current applicant
     return (
       person?.applicantAddress?.country !== undefined && person !== currentApp
     );
   }
 
-  function selectUpdate(event) {
-    const { target = {} } = event;
-    const value = event.detail?.value || target.value || '';
-    let parsedAddress;
-    try {
-      parsedAddress = JSON.parse(value);
-    } catch (e) {
-      // TODO: this just means they chose the default <select> value of "-- select --"
-      // Don't really see a reason to log this, but we might want to do something... TBD
-    }
-    if (parsedAddress) {
-      setAddress(parsedAddress);
-      setSelectValue(value);
-    }
-  }
   const handlers = {
     validate() {
-      // TODO: fill this out
       let isValid = true;
-      if (checkValue === undefined) {
-        setCheckError('This field is required');
+      if (radioValue === undefined) {
+        setRadioError('This field is required');
         isValid = false;
-      } else if (checkValue && selectValue === undefined) {
+      } else if (radioValue && selectValue === undefined) {
+        setRadioError(null);
         setSelectError('This field is required');
         isValid = false;
       } else {
-        setCheckError(null);
+        setRadioError(null);
         setSelectError(null);
       }
       return isValid;
     },
-    onBlur(args) {
-      console.log(args.target.value);
-      // TODO: fill this out
-      setDirty(true);
-      // handlers.validate();
-    },
     radioUpdate: ({ detail }) => {
+      setRadioValue(detail.value === 'true'); // convert from string to bool
       setDirty(true);
-      setCheckValue(detail.value === 'true'); // convert from string to bool
-      // handlers.validate();
     },
-
+    selectUpdate: event => {
+      const { target = {} } = event;
+      const value = event.detail?.value || target.value || '';
+      let parsedAddress;
+      try {
+        parsedAddress = JSON.parse(value);
+      } catch (e) {
+        // We selected "-Select-" or somehow otherwise fouled up.
+        // Clear any previously set value
+        setAddress(undefined);
+        setSelectValue(undefined);
+      }
+      if (parsedAddress) {
+        setAddress(parsedAddress);
+        setSelectValue(value);
+      }
+      setDirty(true);
+    },
     onGoForward: event => {
       event.preventDefault();
       if (!handlers.validate()) return;
-      const testVal = { ...data };
-      const testApp = testVal.applicants[pagePerItemIndex];
-      testApp.sharedAddress = checkValue;
-      testApp.addressOriginator = selectValue;
-      testApp.applicantAddress = address;
-      setFormData(testVal);
+      const tmpVal = { ...data };
+      const tmpApp = tmpVal.applicants[pagePerItemIndex];
+      tmpApp.hasSharedAddress = radioValue;
+      if (radioValue) {
+        tmpApp.addressOriginator = selectValue;
+        tmpApp.applicantAddress = address;
+      }
+      setFormData(tmpVal);
       if (onReviewPage) updatePage();
       goForward(data);
     },
@@ -120,11 +118,10 @@ export function ApplicantAddressCopyPage({
 
   useEffect(
     () => {
-      console.log(hasEditedAddress);
       if (dirty) handlers.validate();
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [data, checkValue, selectValue],
+    [data, radioValue, selectValue],
   );
   return (
     <>
@@ -136,10 +133,9 @@ export function ApplicantAddressCopyPage({
           label="Do you share an address with another applicant?"
           hint="If yes, you will need to tell us whose address you share."
           required
-          error={checkError}
-          onBlur={handlers.onBlur}
+          error={radioError}
           onVaValueChange={handlers.radioUpdate}
-          value={checkValue}
+          value={radioValue}
         >
           {options.map(option => (
             <va-radio-option
@@ -147,28 +143,29 @@ export function ApplicantAddressCopyPage({
               name="pre-address-info"
               label={option.label}
               value={option.value}
-              checked={checkValue === option.value}
+              checked={radioValue === option.value}
               uswds
               aria-describedby={
-                checkValue === option.value ? option.value : null
+                radioValue === option.value ? option.value : null
               }
             />
           ))}
         </VaRadio>
-        {checkValue && (
+        {radioValue && (
           <div
             className={
-              checkValue ? 'form-expanding-group form-expanding-group-open' : ''
+              radioValue ? 'form-expanding-group form-expanding-group-open' : ''
             }
           >
             <div className="form-expanding-group-inner-enter-done">
               <div className="schemaform-expandUnder-indent">
                 <VaSelect
-                  onVaSelect={selectUpdate}
+                  onVaSelect={handlers.selectUpdate}
                   error={selectError}
-                  onBlur={handlers.onBlur}
                   required
                   value={selectValue}
+                  label="Select the user with whom you share an address"
+                  name="shared-address-select"
                 >
                   {data.applicants
                     .filter(person => isValidOrigin(person))
@@ -188,13 +185,19 @@ export function ApplicantAddressCopyPage({
             </div>
           </div>
         )}
-        {onReviewPage ? updateButton : navButtons}
+        <div className="vads-u-margin-top--4">
+          {contentBeforeButtons}
+          {onReviewPage ? updateButton : navButtons}
+          {contentAfterButtons}
+        </div>
       </form>
     </>
   );
 }
 
 ApplicantAddressCopyPage.propTypes = {
+  contentAfterButtons: PropTypes.element,
+  contentBeforeButtons: PropTypes.element,
   data: PropTypes.object,
   genOp: PropTypes.func,
   goBack: PropTypes.func,
