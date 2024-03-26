234262df2f881506b1fce68442ce819479109f92
Midge-dev <sean.midgley.dev@gmail.com>
2024-03-26 15:46:42 -0700

    adds phoneSchema and emailSchema

--- src/applications/burials-v2/config/chapters/01-claimant-information/contactInformation.js
+++ src/applications/burials-v2/config/chapters/01-claimant-information/contactInformation.js
@@ -1,37 +1,28 @@
 import {
   phoneUI,
   emailUI,
+  phoneSchema,
+  emailSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
-import { VaTextInput } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import fullSchemaBurials from 'vets-json-schema/dist/21P-530V2-schema.json';
 import { generateTitle } from '../../../utils/helpers';
-
-const {
-  claimantEmail,
-  claimantPhone,
-  claimantIntPhone,
-} = fullSchemaBurials.properties;
 
 export default {
   uiSchema: {
     'ui:title': generateTitle('Contact information'),
     claimantEmail: {
       ...emailUI(),
-      'ui:webComponentField': VaTextInput,
       'ui:options': {
         uswds: true,
       },
     },
     claimantPhone: {
       ...phoneUI('Your phone number'),
-      'ui:webComponentField': VaTextInput,
       'ui:options': {
         uswds: true,
       },
     },
     claimantIntPhone: {
       ...phoneUI('Your international phone number'),
-      'ui:webComponentField': VaTextInput,
       'ui:options': {
         uswds: true,
       },
@@ -40,9 +31,9 @@ export default {
   schema: {
     type: 'object',
     properties: {
-      claimantEmail,
-      claimantPhone,
-      claimantIntPhone,
+      claimantEmail: emailSchema,
+      claimantPhone: phoneSchema,
+      claimantIntPhone: phoneSchema,
     },
   },
 };
