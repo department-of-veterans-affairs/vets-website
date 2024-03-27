559085caf984c4110caa96be02ae4e91278c75bb
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-03-27 15:50:57 -0400

    use mhvAccountState

--- src/platform/mhv/util/route-guard/index.js
+++ src/platform/mhv/util/route-guard/index.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-// import { isLOA1 } from '~/platform/user/selectors';
-// import { hasHealthData } from '~/applications/mhv-landing-page/selectors';
 
 /**
- * MyHealthAccessGuard component that will redirect the user to the my-health page if they are LOA1 or are not registerd with My HealtheVet
+ * MyHealthAccessGuard component that will redirect the user to the my-health page if they do not have an MHV account
  *
  * @param {Object} children The children to render
  * @returns {Node} The children to render if the user is authenticated
  */
 
 export const MyHealthAccessGuard = ({ children }) => {
-  // const isUnverified = useSelector(isLOA1);
-  // const userHasHealthData = useSelector(hasHealthData);
   const hasMHVAccountState = state => {
     return ['OK', 'MULTIPLE'].includes(state?.user?.profile?.mhvAccountState);
   };
-  if (hasMHVAccountState) {
+  if (!hasMHVAccountState) {
     return <Redirect to="/my-health" />;
   }
   return children;
