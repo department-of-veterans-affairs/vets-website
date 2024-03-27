65dd2bb284dc1daf5d1fea9ec5b1e3d3ba684e38
tynyttie <ty_barnes_jobs@yahoo.com>
2024-03-27 19:56:58 -0400

    Updated medical-records-view-conditions-details.cypress.spec with proper test objects.

--- src/applications/mhv-medical-records/tests/e2e/medical-records-view-conditions-details.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-view-conditions-details.cypress.spec.js
@@ -9,12 +9,10 @@ describe('Medical Records View Conditions', () => {
     cy.visit('my-health/medical-records/conditions');
 
     ConditionsListPage.verifyConditionsPageTitle();
-    ConditionsListPage.clickConditionsDetailsLink(0);
-    ConditionDetailsPage.verifyConditionStatus('abc123');
-    ConditionDetailsPage.verifyProvider('Dr. John');
-    ConditionDetailsPage.verifyLocation("chiropractor's office");
-    ConditionDetailsPage.verifySnomed('Back pain (SCT 161891005)');
-    ConditionDetailsPage.verifyProviderNotes('A note');
+    ConditionsListPage.clickConditionsDetailsLink(1);
+    ConditionDetailsPage.verifyProvider('SMITH,JOHN');
+    ConditionDetailsPage.verifyLocation('SLC10.FO-BAYPINES.MED.VA.GOV');
+    ConditionDetailsPage.verifyProviderNotes("JOHN's test");
     // Axe check
     cy.injectAxe();
     cy.axeCheck('main');
