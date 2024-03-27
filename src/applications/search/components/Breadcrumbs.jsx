cb6a048308a56285405904836ec5403650fcee4d
Randi Mays <rmays@kindsys.us>
2024-03-27 14:51:32 -0500

    update breadcrumbs

--- src/applications/search/components/Breadcrumbs.jsx
+++ src/applications/search/components/Breadcrumbs.jsx
@@ -1,43 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
 import { VaBreadcrumbs } from '@department-of-veterans-affairs/web-components/react-bindings';
 import { focusElement } from 'platform/utilities/ui';
 
-class Breadcrumbs extends React.Component {
-  static defaultProps = {
-    breadcrumbId: 'search-breadcrumbs',
-  };
+const Breadcrumbs = () => {
+  useEffect(() => {
+    focusElement('#search-breadcrumbs');
+  }, []);
 
-  componentDidMount() {
-    focusElement(`#${this.props.breadcrumbId}`);
-  }
-
-  render() {
-    return (
-      <div className="row">
-        <VaBreadcrumbs
-          class="vads-u-margin-left--1p5"
-          id={this.props.breadcrumbId}
-          label="Breadcrumbs"
-          uswds
-          breadcrumbList={[
-            {
-              href: '/',
-              label: 'Home',
-            },
-            {
-              href: '/search',
-              label: 'Search VA.gov',
-            },
-          ]}
-        />
-      </div>
-    );
-  }
-}
-
-Breadcrumbs.propTypes = {
-  breadcrumbId: PropTypes.string.isRequired,
+  return (
+    <div className="row">
+      <VaBreadcrumbs
+        class="vads-u-margin-left--1p5"
+        id="search-breadcrumbs"
+        label="Breadcrumbs"
+        uswds
+        breadcrumbList={[
+          {
+            href: '/',
+            label: 'Home',
+          },
+          {
+            href: '/search',
+            label: 'Search VA.gov',
+          },
+        ]}
+      />
+    </div>
+  );
 };
 
 export default Breadcrumbs;
