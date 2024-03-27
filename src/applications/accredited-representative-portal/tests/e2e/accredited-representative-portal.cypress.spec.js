39d2a399750f2202c13ff3de8c70ade41be74a2d
William Phelps <williamphelps13@gmail.com>
2024-03-27 09:20:45 -0600

    Remove 3x describe block nesting, enhance readability, and add 'and back' testing

--- src/applications/accredited-representative-portal/tests/e2e/accredited-representative-portal.cypress.spec.js
+++ src/applications/accredited-representative-portal/tests/e2e/accredited-representative-portal.cypress.spec.js
@@ -1,4 +1,4 @@
-const togglePortal = value => {
+const featureIsEnabled = value => {
   beforeEach(() => {
     cy.intercept('GET', '/v0/feature_toggles*', {
       data: {
@@ -11,70 +11,141 @@ const togglePortal = value => {
 };
 
 describe('Accredited Representative Portal', () => {
-  describe('feature toggling in production', () => {
-    // During CI, the environment is production, so we can test our global
-    // feature toggling behavior there. But when running this test locally, the
-    // environment is localhost, so we can't test our global feature toggling
-    // behavior there without doing some more complex test setup.
-    before(function skipOutsideCI() {
+  beforeEach(() => {
+    cy.visit('/representative');
+  });
+
+  describe('Feature toggling', () => {
+    it('does not allow navigation to the Portal when feature is not enabled', () => {
+      // During CI, the environment is production, so we can test our global
+      // feature toggling behavior there. But when running this test locally, the
+      // environment is localhost, so we can't test our global feature toggling
+      // behavior there without doing some more complex test setup.
       if (!Cypress.env('CI')) {
         this.skip();
       }
+      featureIsEnabled(false);
+
+      cy.injectAxe();
+      cy.axeCheck();
+
+      cy.location('pathname').should('equal', '/');
     });
 
-    describe('when feature is toggled off', () => {
-      togglePortal(false);
+    // In CI, the feature toggle applies so we need to toggle it on to test
+    // behavior past the guard. On localhost, the feature toggle doesn't apply so
+    // toggling the feature on is redundant.
 
-      it('does redirect to root', () => {
-        cy.visit('/representative');
-        cy.injectAxe();
-        cy.axeCheck();
+    it('allows navigation to the Portal when feature is enabled', () => {
+      featureIsEnabled(true);
 
-        cy.location('pathname').should('equal', '/');
-      });
+      cy.injectAxe();
+      cy.axeCheck();
+      cy.location('pathname').should('equal', '/representative/');
     });
   });
 
-  // In CI, the feature toggle applies so we need to toggle it on to test
-  // behavior past the guard. On localhost, the feature toggle doesn't apply so
-  // toggling the feature on is redundant.
-  togglePortal(true);
+  describe('Navigation from Landing Page to pages and back', () => {
+    beforeEach(() => {
+      featureIsEnabled(true);
+    });
 
-  it('does not redirect to root', () => {
-    cy.visit('/representative');
-    cy.injectAxe();
-    cy.axeCheck();
+    it('allows navigation from the Landing Page to unified sign-in page', () => {
+      cy.injectAxe();
+      cy.axeCheck();
 
-    cy.location('pathname').should('equal', '/representative/');
-  });
+      cy.get('[data-testid=landing-page-sign-in-link]')
+        .contains('Sign in or create an account')
+        .click();
+      cy.location('pathname').should('equal', '/sign-in/');
+    });
 
-  it('allows navigation from landing page to unified sign-in page', () => {
-    cy.visit('/representative');
-    cy.injectAxe();
-    cy.axeCheck();
+    it('allows navigation from the Landing Page to the Dashboard Page and back', () => {
+      cy.injectAxe();
+      cy.axeCheck();
 
-    cy.contains('Sign in or create an account').click();
-    cy.url().should('include', '/sign-in/?application=arp&oauth=true');
-  });
+      cy.get('[data-testid=landing-page-heading]').should(
+        'have.text',
+        'Welcome to the Accredited Representative Portal',
+      );
+      cy.get('[data-testid=landing-page-bypass-sign-in-link]').click();
 
-  it('allows navigation from landing page to dashboard to poa requests', () => {
-    cy.visit('/representative');
-    cy.injectAxe();
-    cy.axeCheck();
+      cy.location('pathname').should('equal', '/representative/dashboard');
+      cy.axeCheck();
 
-    cy.contains('Welcome to the Accredited Representative Portal');
-    cy.contains('Until sign in is added use this to see dashboard').click();
+      cy.get('[data-testid=dashboard-heading]').should(
+        'have.text',
+        'Accredited Representative Portal',
+      );
 
-    cy.url().should('include', '/representative/dashboard');
-    cy.axeCheck();
+      cy.get('[data-testid=breadcrumbs-home]').click();
+      cy.get('[data-testid=landing-page-heading]').should(
+        'have.text',
+        'Welcome to the Accredited Representative Portal',
+      );
+    });
 
-    cy.contains('Accredited Representative Portal');
-    cy.contains('View all').click();
+    it('allows navigation from the Landing Page to the Dashboard Page to the POA Requests Page and back', () => {
+      cy.injectAxe();
+      cy.axeCheck();
 
-    cy.url().should('include', '/representative/poa-requests');
-    cy.axeCheck();
+      cy.get('[data-testid=landing-page-heading]').should(
+        'have.text',
+        'Welcome to the Accredited Representative Portal',
+      );
+      cy.get('[data-testid=landing-page-bypass-sign-in-link]').click();
 
-    cy.contains('Power of attorney requests');
-    cy.get('[data-testid=poa-requests-table]').should('exist');
+      cy.get('[data-testid=dashboard-heading]').should(
+        'have.text',
+        'Accredited Representative Portal',
+      );
+      cy.get('[data-testid=poa-requests-widget-view-all-link]').click();
+
+      cy.location('pathname').should('equal', '/representative/poa-requests');
+      cy.axeCheck();
+
+      cy.get('[data-testid=poa-requests-heading]').should(
+        'have.text',
+        'Power of attorney requests',
+      );
+      cy.get('[data-testid=poa-requests-table]').should('exist');
+
+      cy.get('[data-testid=breadcrumbs-home]').click();
+      cy.get('[data-testid=landing-page-heading]').should(
+        'have.text',
+        'Welcome to the Accredited Representative Portal',
+      );
+    });
+
+    it('allows navigation from the Landing Page to the Dashboard Page to the Permissions Page and back', () => {
+      cy.injectAxe();
+      cy.axeCheck();
+
+      cy.get('[data-testid=landing-page-heading]').should(
+        'have.text',
+        'Welcome to the Accredited Representative Portal',
+      );
+      cy.get('[data-testid=landing-page-bypass-sign-in-link]').click();
+
+      cy.get('[data-testid=dashboard-heading]').should(
+        'have.text',
+        'Accredited Representative Portal',
+      );
+      cy.get('[data-testid=sidenav-permissions-item]').click();
+
+      cy.location('pathname').should('equal', '/representative/permissions');
+      cy.axeCheck();
+
+      cy.get('[data-testid=permissions-page-heading]').should(
+        'have.text',
+        'Permissions',
+      );
+
+      cy.get('[data-testid=breadcrumbs-home]').click();
+      cy.get('[data-testid=landing-page-heading]').should(
+        'have.text',
+        'Welcome to the Accredited Representative Portal',
+      );
+    });
   });
 });
