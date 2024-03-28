279786315a4449608db927558041ec3c1148e2f1
Dakota Larson <dakota.larson@oddball.io>
2024-03-27 17:24:25 -0700

    update content

--- src/applications/static-pages/burials-v2-how-do-i-apply-widget/components/App/index.unit.spec.js
+++ src/applications/static-pages/burials-v2-how-do-i-apply-widget/components/App/index.unit.spec.js
@@ -26,75 +26,41 @@ const store = ({ burialFormV2 = false } = {}) => ({
 });
 
 describe('Burials v2', () => {
-  it('renders documents with feature flag disabled', () => {
+  it('renders v1 apply link with feature flag disabled', () => {
     const mockStore = store({ burialFormV2: false });
     const { container } = render(
       <Provider store={mockStore}>
         <App />
       </Provider>,
     );
-    expect(container.textContent).to.contain(
-      `What documents do I need to send with my application?`,
-    );
+    expect(
+      container.querySelector(
+        'a[href="/burials-and-memorials/application/530/introduction"]',
+      ),
+    ).to.exist;
+    expect(
+      container.querySelector(
+        'a[href="/burials-and-memorials-v2/application/530/introduction"]',
+      ),
+    ).to.not.exist;
   });
 
-  it('renders documents with feature flag enabled', () => {
+  it('renders v2 apply link with feature enabled', () => {
     const mockStore = store({ burialFormV2: true });
     const { container } = render(
       <Provider store={mockStore}>
         <App />
       </Provider>,
     );
-    expect(container.textContent).to.contain(
-      `What documents do I need to submit with my application?`,
-    );
-  });
-
-  it('renders time limit content with feature flag disabled', () => {
-    const mockStore = store({ burialFormV2: false });
-    const { container } = render(
-      <Provider store={mockStore}>
-        <App />
-      </Provider>,
-    );
-    expect(container.textContent).to.contain(
-      `You must file a claim for a non-service-connected burial allowance within 2 years after the Veteran’s burial. If a Veteran’s discharge was changed after death from dishonorable to another status, you must file for an allowance claim within 2 years after the discharge update.`,
-    );
-  });
-
-  it('renders time limit content with feature flag enabled', () => {
-    const mockStore = store({ burialFormV2: true });
-    const { container } = render(
-      <Provider store={mockStore}>
-        <App />
-      </Provider>,
-    );
-    expect(container.textContent).to.contain(
-      `If you’re claiming a burial allowance for a non-service-connected death or unclaimed remains you must file a claim within 2 years after the Veteran’s burial.`,
-    );
-  });
-
-  it('renders mail directions content with feature flag disabled', () => {
-    const mockStore = store({ burialFormV2: false });
-    const { container } = render(
-      <Provider store={mockStore}>
-        <App />
-      </Provider>,
-    );
-    expect(container.textContent).to.contain(
-      `Mail the application and copies of supporting documents to your nearest VA regional office.`,
-    );
-  });
-
-  it('renders mail directions content with feature flag enabled', () => {
-    const mockStore = store({ burialFormV2: true });
-    const { container } = render(
-      <Provider store={mockStore}>
-        <App />
-      </Provider>,
-    );
-    expect(container.textContent).to.contain(
-      `Mail the application and copies of supporting documents to this address:`,
-    );
+    expect(
+      container.querySelector(
+        'a[href="/burials-and-memorials-v2/application/530/introduction"]',
+      ),
+    ).to.exist;
+    expect(
+      container.querySelector(
+        'a[href="/burials-and-memorials/application/530/introduction"]',
+      ),
+    ).to.not.exist;
   });
 });
