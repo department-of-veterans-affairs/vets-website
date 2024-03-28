ca00bf72790834f8c70dc7d9a4891333c102bc43
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2024-03-28 13:43:29 -0400

    test e2e refetch for errors

--- script/github-actions/double-check-allow-list.js
+++ script/github-actions/double-check-allow-list.js
@@ -1,16 +1,26 @@
 /* eslint-disable no-console */
 const fs = require('fs');
 const path = require('path');
+const core = require('@actions/core');
 
-const TESTS = process.env.TESTS || [];
-const { TESTS_PROPERTY } = process.env;
+const TESTS =
+  process.env.TESTS.map(test => test.slice(test.indexOf('src'))) || [];
+const TESTS_PROPERTY = process.env.TESTS_PROPERTY || TESTS;
 
 const ALLOW_LIST =
   process.env.TEST_TYPE &&
-  fs.existsSync(path.resolve(`${process.env.TEST_TYPE}_allow_list.json`))
+  fs.existsSync(
+    path.resolve(
+      `qa-standards-dashboard-data/${process.env.TEST_TYPE}_allow_list.json`,
+    ),
+  )
     ? JSON.parse(
         fs.readFileSync(
-          path.resolve(`${process.env.TEST_TYPE}_allow_list.json`),
+          path.resolve(
+            `qa-standards-dashboard-data/${
+              process.env.TEST_TYPE
+            }_allow_list.json`,
+          ),
         ),
       )
     : [];
@@ -19,12 +29,20 @@ console.log('TESTS', TESTS);
 console.log('TESTS_PROPERTY', TESTS_PROPERTY);
 console.log('ALLOW_LIST', ALLOW_LIST);
 
-const disallowedTests = ALLOW_LIST.filter(test => test.allowed === false);
+const disallowedTests = ALLOW_LIST.filter(test => test.allowed === false).map(
+  test => test.spec_path,
+);
 
 console.log('disallowed tests: ', disallowedTests);
 
-const newDisallowedTests = disallowedTests.filter(test =>
-  TESTS.includes(test.spec_path),
-);
+const newDisallowedTests = disallowedTests.filter(test => TESTS.includes(test));
 
-console.log('new disallowed tests: ', newDisallowedTests);
+if (disallowedTests.length > 0) {
+  console.log('new disallowed tests: ', newDisallowedTests);
+
+  const newTests = TESTS.filter(test => disallowedTests.indexOf(test) === -1);
+
+  console.log('new tests: ', newTests);
+
+  core.exportVariable(TESTS_PROPERTY, newTests);
+}
