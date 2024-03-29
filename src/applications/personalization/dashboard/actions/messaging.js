bf6d8c457579858f11e8ac3281ab155f57f56bfc
Allison Lu <allisonlu820@gmail.com>
2024-03-29 15:37:52 -0400

    move messaging action types from constants to actions file

--- src/applications/personalization/dashboard/actions/messaging.js
+++ src/applications/personalization/dashboard/actions/messaging.js
@@ -5,17 +5,27 @@ import {
 } from '../utils/helpers';
 import environment from '~/platform/utilities/environment';
 import { apiRequest } from '~/platform/utilities/api';
-import {
-  FETCH_FOLDER_FAILURE,
-  FETCH_FOLDER_SUCCESS,
-  LOADING_FOLDER,
-  FETCH_RECIPIENTS_SUCCESS,
-  FETCH_RECIPIENTS_FAILURE,
-  LOADING_RECIPIENTS,
-  LOADING_UNREAD_MESSAGES_COUNT,
-  FETCH_UNREAD_MESSAGES_COUNT_SUCCESS,
-  FETCH_UNREAD_MESSAGES_COUNT_ERROR,
-} from '../utils/constants';
+
+// Loading action types
+export const LOADING_FOLDER = 'LOADING_FOLDER';
+export const LOADING_RECIPIENTS = 'LOADING_RECIPIENTS';
+export const LOADING_UNREAD_MESSAGES_COUNT = 'LOADING_UNREAD_MESSAGES_COUNT';
+
+// Compose action types
+export const FETCH_RECIPIENTS_FAILURE = 'FETCH_RECIPIENTS_FAILURE';
+export const FETCH_RECIPIENTS_SUCCESS = 'FETCH_RECIPIENTS_SUCCESS';
+
+// Folders action types
+export const FETCH_FOLDER_FAILURE = 'FETCH_FOLDER_FAILURE';
+export const FETCH_FOLDER_SUCCESS = 'FETCH_FOLDER_SUCCESS';
+
+// Unread messages actions
+export const FETCH_UNREAD_MESSAGES_COUNT_SUCCESS =
+  'FETCH_UNREAD_MESSAGES_COUNT_SUCCESS';
+export const FETCH_UNREAD_MESSAGES_COUNT_ERROR =
+  'FETCH_UNREAD_MESSAGES_COUNT_ERROR';
+
+export const ITEMS_PER_PAGE = 10;
 
 const baseUrl = `${environment.API_URL}/my_health/v1/messaging`;
 
