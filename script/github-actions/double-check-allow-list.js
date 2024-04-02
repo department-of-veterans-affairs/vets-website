f02b610523b7fb01dc003f42dee83ec6cb9dcb36
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2024-04-02 14:14:50 -0400

    remove all logging and prep for merge

--- script/github-actions/double-check-allow-list.js
+++ script/github-actions/double-check-allow-list.js
@@ -26,43 +26,18 @@ const ALLOW_LIST =
       )
     : [];
 
-console.log('TESTS', TESTS);
-
 const disallowedTests = ALLOW_LIST.filter(test => test.allowed === false).map(
   test => test.spec_path,
 );
-console.log(disallowedTests);
 
-if (process.env.TEST_TYPE === 'e2e') {
-  disallowedTests.push(
-    'src/applications/check-in/pre-check-in/tests/e2e/routing/browser.back.button.cypress.spec.js',
+if (process.env.TEST_TYPE === 'e2e' && disallowedTests.length > 0) {
+  const newTests = TESTS.filter(
+    test =>
+      !disallowedTests.some(disallowedTest => test.includes(disallowedTest)),
   );
-
-  if (disallowedTests.length > 0) {
-    console.log('new disallowed tests: ', disallowedTests);
-
-    const newTests = TESTS.filter(
-      test =>
-        !disallowedTests.some(disallowedTest => test.includes(disallowedTest)),
-    );
-    console.log(
-      'tests index of disallowed spec:',
-      TESTS.indexOf(
-        TESTS.filter(test =>
-          test.includes(
-            'src/applications/pre-check-in/tests/e2e/errors/post-pre-check-in/non.200.status.code.cypress.spec.js',
-          ),
-        ),
-      ),
-    );
-    console.log('new tests length:', newTests.length);
-    console.log('new tests: ', newTests);
-
-    core.exportVariable(TESTS_PROPERTY, newTests);
-  }
+  core.exportVariable(TESTS_PROPERTY, newTests);
 }
 
 if (process.env.TEST_TYPE === 'unit_test') {
-  disallowedTests.push('src/applications/test-app/test-spec.unit.spec.js');
   core.exportVariable(TESTS_PROPERTY, disallowedTests);
 }
