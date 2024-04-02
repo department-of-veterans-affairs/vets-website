6426d1fd9354fac55fe026aeeaa3e5464b605469
Kyle Cardwell <kyle.cardwell@acommsolutions.com>
2024-04-02 10:48:26 -0600

    move message to folder modal and buttons

--- src/applications/mhv/secure-messaging/components/MessageActionButtons/MoveMessageToFolderBtn.jsx
+++ src/applications/mhv/secure-messaging/components/MessageActionButtons/MoveMessageToFolderBtn.jsx
@@ -110,10 +110,8 @@ const MoveMessageToFolderBtn = props => {
         data-testid="message-actions-buttons-modal"
       >
         <VaModal
-          uswds={false}
           id="move-to-modal"
           data-testid="move-to-modal"
-          large
           modalTitle="Move to:"
           onCloseEvent={closeModal}
           visible={isMoveModalVisible}
@@ -166,13 +164,12 @@ const MoveMessageToFolderBtn = props => {
           <p /> {/* to create extra margin between radio and action buttons */}
           {/* For creating a new folder and moving the thread */}
           <va-button
-            uswds={false}
+            class="vads-u-margin-right--1 small-screen:vads-u-margin-right--0"
             text="Confirm"
             onClick={handleConfirmMoveFolderTo}
             data-dd-action-name="Confirm Move to Button"
           />
           <va-button
-            uswds={false}
             secondary
             text="Cancel"
             onClick={closeModal}
