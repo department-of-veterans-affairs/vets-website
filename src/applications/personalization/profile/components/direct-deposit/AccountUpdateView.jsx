e5527335a5dc03451562dc5346cf26ccfc27acd5
Allison Lu <allisonlu820@gmail.com>
2024-04-03 16:01:17 -0400

    created view for additional info dropdown, reorganized schema property definitions

--- src/applications/personalization/profile/components/direct-deposit/AccountUpdateView.jsx
+++ src/applications/personalization/profile/components/direct-deposit/AccountUpdateView.jsx
@@ -1,64 +1,84 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import merge from 'lodash/merge';
 
 import SchemaForm from '~/platform/forms-system/src/js/components/SchemaForm';
 import { ACCOUNT_TYPES_OPTIONS } from '../../constants';
+import { BankNumberFaq } from './BankNumberFaq';
 
-export function makeFormProperties(prefix) {
-  return {
-    accountType: `${prefix}AccountType`,
-    routingNumber: `${prefix}RoutingNumber`,
-    accountNumber: `${prefix}AccountNumber`,
-  };
-}
-
-function makeSchemas(prefix) {
-  const properties = makeFormProperties(prefix);
-  const schema = {
+function makeSchemas() {
+  const bankAccountProperties = {
     type: 'object',
     properties: {
-      [properties.accountType]: {
+      accountType: {
         type: 'string',
         enum: Object.values(ACCOUNT_TYPES_OPTIONS),
       },
-      [properties.routingNumber]: {
+      routingNumber: {
         type: 'string',
         pattern: '^\\d{9}$',
       },
-      [properties.accountNumber]: {
+      accountNumber: {
         type: 'string',
         pattern: '^\\d{1,17}$',
       },
+      'view:directDepositInfo': {
+        type: 'object',
+        properties: {},
+      },
+    },
+  };
+  const schema = {
+    type: 'object',
+    properties: {
+      bankAccount: bankAccountProperties,
     },
     required: [
-      properties.accountType,
-      properties.accountNumber,
-      properties.routingNumber,
+      bankAccountProperties.properties.accountType,
+      bankAccountProperties.properties.accountNumber,
+      bankAccountProperties.properties.routingNumber,
     ],
   };
 
   const uiSchema = {
-    [properties.accountType]: {
-      'ui:widget': 'radio',
-      'ui:title': 'Account type',
-      'ui:errorMessages': {
-        required: 'Please select the type that best describes your account',
+    'ui:description':
+      'Please provide your bank’s routing number as well as your current account and type.',
+    bankAccount: merge(
+      {},
+      {
+        accountType: {
+          'ui:widget': 'radio',
+          'ui:title': 'Account type',
+          // 'ui:options': {
+          //   labels: ACCOUNT_TYPES_OPTIONS,
+          // },
+          'ui:errorMessages': {
+            required: 'Please select the type that best describes your account',
+          },
+        },
+        routingNumber: {
+          'ui:title': 'Routing number',
+          // 'ui:validations': [validateRoutingNumber],
+          'ui:errorMessages': {
+            pattern: 'Please enter your bank’s 9-digit routing number',
+            required: 'Please enter your bank’s 9-digit routing number',
+          },
+        },
+        accountNumber: {
+          'ui:title': 'Account number (No more than 17 digits)',
+          'ui:errorMessages': {
+            pattern: 'Please enter your account number',
+            required: 'Please enter your account number',
+          },
+        },
+        'view:directDepositInfo': {
+          'ui:description': BankNumberFaq,
+          'ui:options': {
+            hideOnReview: true,
+          },
+        },
       },
-    },
-    [properties.routingNumber]: {
-      'ui:title': 'Routing number',
-      'ui:errorMessages': {
-        pattern: 'Please enter your bank’s 9-digit routing number',
-        required: 'Please enter your bank’s 9-digit routing number',
-      },
-    },
-    [properties.accountNumber]: {
-      'ui:title': 'Account number (This should be no more than 17 digits)',
-      'ui:errorMessages': {
-        pattern: 'Please enter your account number',
-        required: 'Please enter your account number',
-      },
-    },
+    ),
   };
 
   return { schema, uiSchema };
@@ -70,22 +90,25 @@ export const AccountUpdateView = ({
   formData,
   formSubmit,
 }) => {
-  const { schema, uiSchema } = makeSchemas('directDeposit');
+  const { schema, uiSchema } = makeSchemas();
 
   return (
-    <SchemaForm
-      addNameAttribute
-      name="Direct Deposit Information"
-      // title is required by the SchemaForm and used internally
-      title="Direct Deposit Information"
-      schema={schema}
-      uiSchema={uiSchema}
-      data={formData}
-      onChange={formChange}
-      onSubmit={formSubmit}
-    >
-      {children}
-    </SchemaForm>
+    <>
+      <strong>Account</strong>
+      <SchemaForm
+        addNameAttribute
+        name="Direct Deposit Information"
+        // title is required by the SchemaForm and used internally
+        title="Direct Deposit Information"
+        schema={schema}
+        uiSchema={uiSchema}
+        data={formData}
+        onChange={formChange}
+        onSubmit={formSubmit}
+      >
+        {children}
+      </SchemaForm>
+    </>
   );
 };
 
