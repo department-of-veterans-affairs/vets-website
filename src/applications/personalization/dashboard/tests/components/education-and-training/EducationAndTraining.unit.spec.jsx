cd332619c401def6f0635c4a0b48ea1b967b1cb3
Daniel Miller <daniel.miller@agile6.com>
2024-04-03 12:38:50 -0700

    fix tests

--- src/applications/personalization/dashboard/tests/components/education-and-training/EducationAndTraining.unit.spec.jsx
+++ src/applications/personalization/dashboard/tests/components/education-and-training/EducationAndTraining.unit.spec.jsx
@@ -18,18 +18,22 @@ describe('<EducationAndTraining />', () => {
 
   it('renders expected LOA1 content', () => {
     const tree = render(<EducationAndTraining isLOA1 />);
-    expect(tree.getByTestId('compare-gi-benefits-from-cta')).to.not.exist;
-    expect(tree.getByTestId('check-gi-bill-benefits-from-cta')).to.not.exist;
+    expect(tree.queryByTestId('compare-gi-benefits-from-cta')).to.not.exist;
+    expect(tree.queryByTestId('check-gi-bill-benefits-from-cta')).to.not.exist;
   });
 
   it('fires monitoring events on CTA clicks', async () => {
-    const user = userEvent.setup();
     const events = [];
+    const EXPECTED = Array(3)
+      .fill('nav-linkslist')
+      .toString();
     const recordEvent = e => events.push(e.event);
     const tree = render(<EducationAndTraining rE={recordEvent} />);
-    await user.click(tree.getByTestId('apply-education-benefits-from-cta'));
-    await user.click(tree.getByTestId('compare-gi-benefits-from-cta'));
-    await user.click(tree.getByTestId('check-gi-bill-benefits-from-cta'));
-    expect(events.toString()).to.equal(Array(3).fill('nav-linkslist'));
+    await userEvent.click(
+      tree.getByTestId('apply-education-benefits-from-cta'),
+    );
+    await userEvent.click(tree.getByTestId('compare-gi-benefits-from-cta'));
+    await userEvent.click(tree.getByTestId('check-gi-bill-benefits-from-cta'));
+    expect(events.toString()).to.equal(EXPECTED);
   });
 });
