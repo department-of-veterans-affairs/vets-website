2f6d30b22a04e0042d22477fd26b32f7f9b2cf70
Adam King <adam.king0126@gmail.com>
2024-04-03 10:29:58 -0500

    one more linter issue resolution

--- src/applications/edu-benefits/10203/actions/post-911-gib-status.js
+++ src/applications/edu-benefits/10203/actions/post-911-gib-status.js
@@ -1,4 +1,4 @@
-import { apiRequest } from 'platform/utilities/api';
+import { apiRequest } from '@department-of-veterans-affairs/platform-utilities/api';
 import { get } from 'lodash';
 
 const GET_REMAINING_ENTITLEMENT_SUCCESS = 'GET_REMAINING_ENTITLEMENT_SUCCESS';
