5f1c514f6d17a5d34e10e7dfb898a38284f65383
Michael Clement <michael.clement@adhocteam.us>
2024-04-05 12:15:45 -0500

    78841e Applicant medicare flow (sub PR 1/X) (#28964)
    * updated test data for upcoming form fields
    
    * added conditional paths file
    
    * updated constants file
    
    * added custom hint to custom relationship page

--- src/applications/ivc-champva/10-7959C/config/constants.js
+++ src/applications/ivc-champva/10-7959C/config/constants.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export const MAX_APPLICANTS = 3;
 
 // TODO: pull this out of here (and 1010d) and elevate to the shared dir
@@ -18,3 +20,33 @@ export const applicantListSchema = (requireds, propertyList) => {
     },
   };
 };
+
+// These proptypes are used with the custom pages that use
+// ApplicantRelationshipPage
+export const pageProps = {
+  data: PropTypes.object,
+  goBack: PropTypes.func,
+  goForward: PropTypes.func,
+  pagePerItemIndex: PropTypes.string || PropTypes.number,
+  setFormData: PropTypes.func,
+  updatePage: PropTypes.func,
+  onReviewPage: PropTypes.bool,
+};
+export const reviewPageProps = {
+  data: PropTypes.object,
+  editPage: PropTypes.func,
+  pagePerItemIndex: PropTypes.number,
+  props: PropTypes.object,
+  title: PropTypes.func,
+};
+
+export const yesNoOptions = [
+  {
+    label: 'Yes',
+    value: 'yes',
+  },
+  {
+    label: 'No',
+    value: 'no',
+  },
+];
