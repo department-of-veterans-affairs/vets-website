e2cef14539396fce983183a15e82ec0089ddecb4
Gabriel Zurita <gabezurita@gmail.com>
2024-04-05 18:48:31 -0600

    arf.78633 update ARP poaRequests.js to match rails controller See PR: https://github.com/department-of-veterans-affairs/vets-api/pull/16218/files

--- src/applications/accredited-representative-portal/actions/poaRequests.js
+++ src/applications/accredited-representative-portal/actions/poaRequests.js
@@ -1,22 +1,22 @@
 import { apiRequest } from '@department-of-veterans-affairs/platform-utilities/api';
 
-const handlePOARequest = async (poaId, action) => {
+const handlePOARequest = async (procId, action) => {
   try {
-    const resource = `/accredited_representative_portal/v0/power_of_attorney_requests/${poaId}/${action}`;
+    const resource = `/accredited_representative_portal/v0/power_of_attorney_requests/${procId}/${action}`;
     return await apiRequest(resource, { method: 'POST' });
   } catch (error) {
     return error;
   }
 };
 
-export const getPOARequestsByCode = async poaCode => {
+export const getPOARequestsByCodes = async poaCodes => {
   try {
-    const resource = `/accredited_representative_portal/v0/power_of_attorney_requests?poaCode=${poaCode}`;
+    const resource = `/accredited_representative_portal/v0/power_of_attorney_requests?poa_codes=${poaCodes}`;
     return await apiRequest(resource);
   } catch (error) {
     return error;
   }
 };
 
-export const acceptPOARequest = poaId => handlePOARequest(poaId, 'accept');
-export const declinePOARequest = poaId => handlePOARequest(poaId, 'decline');
+export const acceptPOARequest = procId => handlePOARequest(procId, 'accept');
+export const declinePOARequest = procId => handlePOARequest(procId, 'decline');
