59d977efcec4c46b44f735b252435827524e6c8d
robertbylight <roberto.rodriguez@bylight.com>
2024-04-08 14:23:12 -0400

    [MHV-56761] changed the breadcrumbs to only have one crumb rather than the whole history of breadcrumbs.

--- src/applications/mhv/medications/containers/RxBreadcrumbs.jsx
+++ src/applications/mhv/medications/containers/RxBreadcrumbs.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { VaBreadcrumbs } from '@department-of-veterans-affairs/web-components/react-bindings';
 // import { replaceWithStagingDomain } from '~/platform/utilities/environment/stagingDomains';
 import { useLocation } from 'react-router-dom';
 import { medicationsUrls } from '../util/constants';
-
-const alignToLeft = `va-nav-breadcrumbs xsmall-screen:vads-u-margin-left--neg1
-small-screen:vads-u-margin-left--neg1
-medium-screen:vads-u-margin-left--neg2
-small-desktop-screen:vads-u-margin-left--neg2
-large-screen:vads-u-margin-left--0 `;
 
 const RxBreadcrumbs = () => {
   const location = useLocation();
   const crumbs = useSelector(state => state.rx.breadcrumbs.list);
   const currentPath = useSelector(state => state.rx.breadcrumbs.location);
   const allCrumbs = [...crumbs, currentPath];
+  const oneLevelDeepCrumb = allCrumbs?.length - 2;
   return (
     <>
       {!medicationsUrls.MEDICATIONS_ABOUT.endsWith(location.pathname) &&
@@ -23,17 +17,18 @@ const RxBreadcrumbs = () => {
         allCrumbs.length > 0 &&
         allCrumbs[0]?.url && (
           <div className="no-print">
-            <VaBreadcrumbs
-              uswds
+            <div
+              className="vads-l-row vads-u-padding-y--3"
               label="Breadcrumb"
               data-testid="rx-breadcrumb"
-              home-veterans-affairs="false"
-              breadcrumbList={allCrumbs.map(crumb => ({
-                href: crumb.url,
-                label: crumb.label,
-              }))}
-              className={`${alignToLeft} va-breadcrumbs-li vads-u-padding-bottom--0 vads-u-padding-top--4 vads-u-margin-bottom--neg1p5`}
-            />
+            >
+              <span className="breadcrumb-angle vads-u-padding-right--1">
+                {'\u2039'}{' '}
+              </span>
+              <a href={crumbs[oneLevelDeepCrumb].url}>
+                Back to {crumbs[oneLevelDeepCrumb].label}
+              </a>
+            </div>
           </div>
         )}
     </>
