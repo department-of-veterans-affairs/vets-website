46a5c60912825d1d1341b2e0daf12cfe6867067c
Peri McLaren <peri.mclaren@adhocteam.us>
2024-04-08 14:18:08 -0600

    update tests

--- src/applications/claims-status/tests/components/ClaimsStatusApp.unit.spec.jsx
+++ src/applications/claims-status/tests/components/ClaimsStatusApp.unit.spec.jsx
@@ -4,23 +4,25 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import { MemoryRouter, Routes, Route } from 'react-router-dom-v5-compat';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import backendServices from '@department-of-veterans-affairs/platform-user/profile/backendServices';
 import { AppContent, ClaimsStatusApp } from '../../containers/ClaimsStatusApp';
 
 describe('<AppContent>', () => {
   it('should render loading indicator if feature toggles are not available', () => {
-    const screen = render(
+    const { getByTestId, queryByTestId } = render(
       <AppContent featureFlagsLoading>
         <div data-testid="children" />
       </AppContent>,
     );
 
-    expect(screen.getByTestId('feature-flags-loading')).to.exist;
-    expect(screen.queryByTestId('children')).to.not.exist;
+    expect(getByTestId('feature-flags-loading')).to.exist;
+    expect(queryByTestId('children')).to.not.exist;
   });
 
   it('should render nested route if feature toggles are available', () => {
-    const screen = render(
+    const { getByTestId, queryByTestId } = render(
       <MemoryRouter>
         <Routes>
           <Route element={<AppContent featureFlagsLoading={false} />}>
@@ -30,11 +32,20 @@ describe('<AppContent>', () => {
       </MemoryRouter>,
     );
 
-    expect(screen.queryByTestId('feature-flags-loading')).to.not.exist;
-    expect(screen.queryByTestId('children')).to.exist;
+    expect(queryByTestId('feature-flags-loading')).to.not.exist;
+    expect(getByTestId('children')).to.exist;
   });
 
-  it.skip('should render ClaimsStatusApp', () => {
+  it('should render ClaimsStatusApp', () => {
+    const store = createStore(() => ({
+      scheduledDowntime: {
+        globalDowntime: null,
+        isReady: true,
+        isPending: false,
+        serviceMap: new Map(),
+        dismissedDowntimeWarnings: [],
+      },
+    }));
     const props = {
       dispatchSetLastPage: sinon.spy(),
       featureFlagsLoading: false,
@@ -46,21 +57,29 @@ describe('<AppContent>', () => {
             backendServices.APPEALS_STATUS,
             backendServices.LIGHTHOUSE,
           ],
+          verified: true,
         },
       },
     };
-    const screen = render(
+
+    const element = (
+      <Provider store={store}>
+        <ClaimsStatusApp {...props} />
+      </Provider>
+    );
+
+    const { getByTestId, queryByTestId } = render(
       <MemoryRouter>
         <Routes>
-          <Route element={<ClaimsStatusApp {...props} />}>
+          <Route element={element}>
             <Route index element={<div data-testid="children" />} />
           </Route>
         </Routes>
       </MemoryRouter>,
     );
 
-    expect(screen.queryByTestId('feature-flags-loading')).to.not.exist;
-    expect(screen.queryByTestId('children')).to.exist;
-    expect(props.dispatchSetLastPage.called).to.be.false;
+    expect(queryByTestId('feature-flags-loading')).to.not.exist;
+    expect(getByTestId('children')).to.exist;
+    expect(props.dispatchSetLastPage.called).to.be.true;
   });
 });
