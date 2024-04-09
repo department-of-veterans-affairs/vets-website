1ace992354f3a343893fb299341488c021106b02
Matt Kerns <131277002+mkerns1@users.noreply.github.com>
2024-04-09 09:18:53 -0400

    Updated web components and corresponding tests/e2e test (#29055)

--- src/applications/ezr/config/chapters/militaryService/agentOrangeExposure.js
+++ src/applications/ezr/config/chapters/militaryService/agentOrangeExposure.js
@@ -1,22 +1,22 @@
-import ezrSchema from 'vets-json-schema/dist/10-10EZR-schema.json';
+import {
+  yesNoUI,
+  yesNoSchema,
+} from 'platform/forms-system/src/js/web-component-patterns';
 import AgentOrangeExposureDescription from '../../../components/FormDescriptions/AgentOrangeExposureDescription';
 import content from '../../../locales/en/content.json';
-
-const { exposedToAgentOrange } = ezrSchema.properties;
 
 export default {
   uiSchema: {
     'ui:title': content['military-service-agent-orange-locations-title'],
-    exposedToAgentOrange: {
-      'ui:title': content['military-service-agent-orange-exposed-title'],
-      'ui:description': AgentOrangeExposureDescription,
-      'ui:widget': 'yesNo',
-    },
+    exposedToAgentOrange: yesNoUI({
+      title: content['military-service-agent-orange-exposed-title'],
+      description: AgentOrangeExposureDescription,
+    }),
   },
   schema: {
     type: 'object',
     properties: {
-      exposedToAgentOrange,
+      exposedToAgentOrange: yesNoSchema,
     },
   },
 };
