8bd0b9c3e75191c8f62a10986b38d082aecf9c33
OptionSelect <brandyn.sullins@gmail.com>
2024-04-09 09:36:49 -0400

    Fix API alias for cypress, still working on mock response

--- src/applications/financial-status-report/tests/e2e/fsr-5655-streamlined-waiver-long.cypress.spec.js
+++ src/applications/financial-status-report/tests/e2e/fsr-5655-streamlined-waiver-long.cypress.spec.js
@@ -64,6 +64,14 @@ const testConfig = createTestConfig(
       cy.intercept('GET', '/v0/debts', debts);
       cy.intercept('GET', '/v0/medical_copays', copays);
 
+      const calculatedMonthlyExpenseResponse = { body: 120 };
+
+      cy.intercept(
+        'POST',
+        '/debts_api/v0/calculate_monthly_expenses',
+        calculatedMonthlyExpenseResponse,
+      );
+
       cy.intercept('POST', formConfig.submitUrl, {
         statusCode: 200,
         body: {
