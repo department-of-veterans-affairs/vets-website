2457e0d8ef39863e58c6dd545a9a5091d2f7b801
Jerek Shoemaker <jerek.shoemaker@adhocteam.us>
2024-04-09 12:32:11 -0500

    Removing VAButtonGroupSegemented.jsx (#29062)

--- src/applications/claims-status/containers/ClaimsStatusApp.jsx
+++ src/applications/claims-status/containers/ClaimsStatusApp.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Outlet, useLocation } from 'react-router-dom-v5-compat';
 import PropTypes from 'prop-types';
@@ -8,24 +8,10 @@ import DowntimeNotification, {
 } from '@department-of-veterans-affairs/platform-monitoring/DowntimeNotification';
 import backendServices from '@department-of-veterans-affairs/platform-user/profile/backendServices';
 import { RequiredLoginView } from '@department-of-veterans-affairs/platform-user/RequiredLoginView';
-import environment from '@department-of-veterans-affairs/platform-utilities/environment';
 
 import { setLastPage } from '../actions';
 import ClaimsAppealsUnavailable from '../components/ClaimsAppealsUnavailable';
-import VaButtonGroupSegmented from '../components/VaButtonGroupSegmented';
 import { isLoadingFeatures } from '../selectors';
-
-const flipperOverrideModes = [
-  { label: 'EVSS', value: 'evss' },
-  { label: 'Lighthouse', value: 'lighthouse' },
-  { label: 'Feature toggle', value: 'featureToggle' },
-];
-
-const shouldShowFlipperOverride = showFlipperOverride => {
-  const canShow = !environment.isProduction();
-  const shouldShow = showFlipperOverride;
-  return canShow && shouldShow;
-};
 
 // This needs to be a React component for RequiredLoginView to pass down
 // the isDataAvailable prop, which is only passed on failure.
@@ -33,48 +19,6 @@ function AppContent({ featureFlagsLoading, isDataAvailable }) {
   const canUseApp =
     isDataAvailable === true || typeof isDataAvailable === 'undefined';
   const isAppReady = canUseApp && !featureFlagsLoading;
-
-  // Mode to use when overriding feature toggle
-  // Options are: evss, lighthouse, featureToggle
-  const defaultFlipperOverrideMode =
-    sessionStorage.getItem('cstFlipperOverrideMode') || 'featureToggle';
-  const [flipperOverrideMode, setFlipperOverrideMode] = useState(
-    defaultFlipperOverrideMode,
-  );
-
-  // Whether flipper override controls should be shown or not
-  // Can be toggled with key combination: Ctrl + Shift + k
-  const defaultShowFlipperOverrideState =
-    sessionStorage.getItem('showFlipperOverride') === 'true' || false;
-  const [showFlipperOverride, setShowFlipperOverride] = useState(
-    defaultShowFlipperOverrideState,
-  );
-
-  useEffect(() => {
-    const handleKeydown = event => {
-      // `metaKey` is associated with the 'Command' key on macOS
-      const ctrlPressed = event.ctrlKey || event.metaKey;
-
-      if (ctrlPressed && event.shiftKey && event.key.toLowerCase() === 'k') {
-        setShowFlipperOverride(prevState => {
-          const nextState = !prevState;
-          sessionStorage.setItem('showFlipperOverride', nextState);
-          return nextState;
-        });
-      }
-    };
-
-    document.addEventListener('keydown', handleKeydown);
-
-    return () => {
-      document.removeEventListener('keydown', handleKeydown);
-    };
-  }, []);
-
-  const onOptionClick = option => {
-    setFlipperOverrideMode(option);
-    sessionStorage.setItem('cstFlipperOverrideMode', option);
-  };
 
   if (!isAppReady) {
     return (
@@ -87,29 +31,10 @@ function AppContent({ featureFlagsLoading, isDataAvailable }) {
     );
   }
 
-  const showFlipperOverrideControls = shouldShowFlipperOverride(
-    showFlipperOverride,
-  );
-
   return (
     <div className="claims-status-content">
       {!canUseApp && <ClaimsAppealsUnavailable />}
-      {isAppReady && (
-        <>
-          <div className="row">
-            <div className="usa-width-two-thirds medium-8 columns">
-              {showFlipperOverrideControls && (
-                <VaButtonGroupSegmented
-                  options={flipperOverrideModes}
-                  selected={flipperOverrideMode}
-                  onOptionClick={onOptionClick}
-                />
-              )}
-            </div>
-          </div>
-          <Outlet />
-        </>
-      )}
+      {isAppReady && <Outlet />}
     </div>
   );
 }
