499f3129223402250d56a58f2e08e538d431329f
harshil1793 <harshil1793@gmail.com>
2024-04-09 14:26:07 -0400

    cypress facility search element width fix

--- src/applications/facility-locator/tests/e2e/mobile.cypress.spec.js
+++ src/applications/facility-locator/tests/e2e/mobile.cypress.spec.js
@@ -113,7 +113,7 @@ describe('Mobile', () => {
     cy.viewport(1007, 1000);
     cy.axeCheck();
     cy.get('#facility-search').then($element => {
-      expect($element.width()).closeTo(899, 2);
+      expect($element.width()).closeTo(884, 2);
     });
     cy.get('.desktop-map-container').should('exist');
     cy.get('.react-tabs').should('not.exist');
@@ -122,7 +122,7 @@ describe('Mobile', () => {
     cy.viewport(768, 1000);
     cy.axeCheck();
     cy.get('#facility-search').then($element => {
-      expect($element.width()).closeTo(660, 2);
+      expect($element.width()).closeTo(652, 2);
     });
     cy.get('.desktop-map-container').should('exist');
     cy.get('.react-tabs').should('not.exist');
@@ -131,7 +131,7 @@ describe('Mobile', () => {
     cy.viewport(481, 1000);
     cy.axeCheck();
     cy.get('#facility-search').then($element => {
-      expect($element.width()).closeTo(397, 2);
+      expect($element.width()).closeTo(389, 2);
     });
     cy.get('.desktop-map-container').should('not.exist');
     cy.get('.react-tabs').should('exist');
