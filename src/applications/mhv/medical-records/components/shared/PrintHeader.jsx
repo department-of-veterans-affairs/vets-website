ee03a59b58c66440be2b05d7c4103429e828fee2
Matthew Wright <matwright2010@gmail.com>
2024-04-10 10:25:45 -0600

    MHV-56871: Crisis line print header adjustments

--- src/applications/mhv/medical-records/components/shared/PrintHeader.jsx
+++ src/applications/mhv/medical-records/components/shared/PrintHeader.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const PrintHeader = () => {
   return (
-    <div className="print-only print-header vads-u-margin-bottom--4">
+    <div className="print-only print-header vads-u-margin-bottom--4 vads-u-margin-top--2">
       <div
         id="crisis-line-print"
-        className="vads-u-border--2px vads-u-padding--0p5 vads-u-text-align--center print-only"
+        className="vads-u-border--1px vads-u-padding--0p5 vads-u-text-align--center print-only vads-u-font-size--sm"
       >
         If you’re ever in crisis and need to talk to someone right away, call
         the Veterans Crisis Line at{' '}
