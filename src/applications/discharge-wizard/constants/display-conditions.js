8b19587e3f3b29b7e4b5f1827e0029874140c675
Chris Kim <chriskim@Chriss-MacBook-Pro-2.local>
2024-04-11 09:36:38 -0700

    update conditions

--- src/applications/discharge-wizard/constants/display-conditions.js
+++ src/applications/discharge-wizard/constants/display-conditions.js
@@ -1,8 +1,18 @@
+import { range } from 'lodash';
 import { RESPONSES } from './question-data-map';
 
 const get15YearsPast = () => `${new Date().getFullYear() - 15}`;
-
+const currentYear = new Date().getFullYear();
+const yearResponses = range(currentYear - 1992).map(i => {
+  const year = currentYear - i;
+  return year.toString();
+});
 const {
+  ARMY,
+  NAVY,
+  AIR_FORCE,
+  COAST_GUARD,
+  MARINE_CORPS,
   REASON_1,
   REASON_2,
   REASON_3,
@@ -23,32 +33,32 @@ const {
 export const DISPLAY_CONDITIONS = Object.freeze({
   SERVICE_BRANCH: {},
   DISCHARGE_YEAR: {
-    SERVICE_BRANCH: [],
+    SERVICE_BRANCH: [ARMY, NAVY, AIR_FORCE, COAST_GUARD, MARINE_CORPS],
   },
   DISCHARGE_MONTH: {
-    SERVICE_BRANCH: [],
+    SERVICE_BRANCH: [ARMY, NAVY, AIR_FORCE, COAST_GUARD, MARINE_CORPS],
     DISCHARGE_YEAR: [get15YearsPast()],
   },
   REASON: {
-    SERVICE_BRANCH: [],
-    DISCHARGE_YEAR: [],
+    SERVICE_BRANCH: [ARMY, NAVY, AIR_FORCE, COAST_GUARD, MARINE_CORPS],
+    DISCHARGE_YEAR: yearResponses,
     DISCHARGE_MONTH: [],
   },
   DISCHARGE_TYPE: {
-    SERVICE_BRANCH: [],
-    DISCHARGE_YEAR: [],
+    SERVICE_BRANCH: [ARMY, NAVY, AIR_FORCE, COAST_GUARD, MARINE_CORPS],
+    DISCHARGE_YEAR: yearResponses,
     DISCHARGE_MONTH: [],
     REASON: [REASON_3],
   },
   INTENTION: {
-    SERVICE_BRANCH: [],
-    DISCHARGE_YEAR: [],
+    SERVICE_BRANCH: [ARMY, NAVY, AIR_FORCE, COAST_GUARD, MARINE_CORPS],
+    DISCHARGE_YEAR: yearResponses,
     DISCHARGE_MONTH: [],
     REASON: [REASON_1, REASON_2, REASON_3, REASON_4, REASON_6, REASON_7],
   },
   COURT_MARTIAL: {
-    SERVICE_BRANCH: [],
-    DISCHARGE_YEAR: [],
+    SERVICE_BRANCH: [ARMY, NAVY, AIR_FORCE, COAST_GUARD, MARINE_CORPS],
+    DISCHARGE_YEAR: yearResponses,
     DISCHARGE_MONTH: [],
     REASON: [
       REASON_5,
