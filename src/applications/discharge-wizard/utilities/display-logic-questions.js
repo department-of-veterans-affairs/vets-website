12d770216ded7b8b0003c64f79969b4d953e6379
Chris Kim <chriskim@Chriss-MacBook-Pro-2.local>
2024-04-10 23:21:52 -0700

    old code removal

--- src/applications/discharge-wizard/utilities/display-logic-questions.js
+++ src/applications/discharge-wizard/utilities/display-logic-questions.js
@@ -1,38 +1,5 @@
-import { ROUTES } from '../constants';
 import { DISPLAY_CONDITIONS } from '../constants/display-conditions';
 import { SHORT_NAME_MAP } from '../constants/question-data-map';
-import { determineYearRoute, determineReasonRoute } from './shared';
-
-export const nextQuestionRoute = (currentQuestion, answer) => {
-  let nextRoute;
-
-  switch (currentQuestion) {
-    case SHORT_NAME_MAP.SERVICE_BRANCH:
-      nextRoute = SHORT_NAME_MAP.DISCHARGE_YEAR;
-      break;
-    case SHORT_NAME_MAP.DISCHARGE_YEAR:
-      nextRoute = determineYearRoute(answer);
-      break;
-    case SHORT_NAME_MAP.DISCHARGE_MONTH:
-      nextRoute = SHORT_NAME_MAP.REASON;
-      break;
-    case SHORT_NAME_MAP.REASON:
-      nextRoute = determineReasonRoute(answer);
-      break;
-    case SHORT_NAME_MAP.DISCHARGE_TYPE:
-      nextRoute = SHORT_NAME_MAP.INTENTION;
-      break;
-    case SHORT_NAME_MAP.INTENTION:
-      nextRoute = SHORT_NAME_MAP.COURT_MARTIAL;
-      break;
-    case SHORT_NAME_MAP.COURT_MARTIAL:
-      nextRoute = SHORT_NAME_MAP.PREVIOUS_APPLICATION;
-      break;
-    default:
-      return ROUTES.RESULTS;
-  }
-  return nextRoute;
-};
 
 /** ================================================================
  * Make a roadmap (in display order) of SHORT_NAMEs that display in the flow
