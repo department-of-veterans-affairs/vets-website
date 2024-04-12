3f1bc61e1b4fd5f16879fb7fb2d39cfac1f3b57b
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-04-12 10:49:39 -0400

    eslint formatting

--- src/applications/representative-search/containers/SearchPage.jsx
+++ src/applications/representative-search/containers/SearchPage.jsx
@@ -370,7 +370,6 @@ const SearchPage = props => {
           </div>
         )}
 
-
         <SearchControls
           geolocateUser={props.geolocateUser}
           currentQuery={props.currentQuery}
