cd5ee429e577967e78f02650e3239a1fd8d3b095
Annie Tran <annie.tran@coforma.io>
2024-04-12 12:02:31 -0500

    use date-fns in shared unit tests

--- src/applications/appeals/shared/tests/utils/dates.unit.spec.js
+++ src/applications/appeals/shared/tests/utils/dates.unit.spec.js
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
-import moment from 'moment';
+import { parse } from 'date-fns';
 
-import { FORMAT_YMD, FORMAT_READABLE } from '../../constants';
-import { parseDate, getDate } from '../../utils/dates';
+import { FORMAT_FULL_DATE, FORMAT_YMD_DATE_FNS } from '../../constants';
+import { parseDate } from '../../utils/dates';
 
 describe('parseDate', () => {
   it('should return null for invalid dates', () => {
@@ -12,51 +12,34 @@ describe('parseDate', () => {
     expect(parseDate('2024-13-32')).to.be.null;
     expect(parseDate('2024-13-32T00:00:00.000-06:00')).to.be.null;
     expect(parseDate('1712854521628')).to.be.null;
+    expect(parseDate('2000-1-1')).to.be.null; // Non-ISO8601 format
+    expect(parseDate('02-03-2024')).to.be.null; // Non-ISO8601 format
   });
   it('should return a formatted date string', () => {
-    expect(parseDate(2024)).to.eq('December 31, 1969');
-    expect(parseDate(1712854521628)).to.eq('April 11, 2024');
-    expect(parseDate('2024-02-03')).to.eq('February 3, 2024');
+    expect(parseDate(2024, FORMAT_FULL_DATE)).to.eq('December 31, 1969');
+    expect(parseDate(1712854521628, FORMAT_FULL_DATE)).to.eq('April 11, 2024');
+    expect(parseDate('2024-02-03', FORMAT_FULL_DATE)).to.eq('February 3, 2024');
     // one off date example if you don't include the time
-    expect(parseDate(new Date('2024-05-06T00:00:00.000'))).to.eq('May 6, 2024');
-    expect(parseDate('2023-06-17T12:34:56.000-06:00')).to.eq('June 17, 2023');
+    expect(
+      parseDate(new Date('2024-05-06T00:00:00.000'), FORMAT_FULL_DATE),
+    ).to.eq('May 6, 2024');
+    expect(parseDate('2023-06-17T12:34:56.000-06:00', FORMAT_FULL_DATE)).to.eq(
+      'June 17, 2023',
+    );
+    const dateFnsParsedDate = parse(
+      '2000-1-1',
+      FORMAT_YMD_DATE_FNS,
+      new Date(),
+    );
+    expect(parseDate(dateFnsParsedDate, FORMAT_FULL_DATE)).to.eq(
+      'January 1, 2000',
+    );
   });
   it('should return a formatted date string', () => {
-    const shortDate = 'MM-dd-yyyy';
-    expect(parseDate(2024, shortDate)).to.eq('12-31-1969');
-    expect(parseDate(1712854521628, shortDate)).to.eq('04-11-2024');
-    expect(parseDate('2024-02-03', shortDate)).to.eq('02-03-2024');
+    expect(parseDate(2024)).to.eq('1969-12-31');
+    expect(parseDate(1712854521628)).to.eq('2024-04-11');
     // one off date example if you don't include the time
-    expect(parseDate(new Date('2024-05-06T00:00:00.000'), shortDate)).to.eq(
-      '05-06-2024',
-    );
-    expect(parseDate('2023-06-17T12:34:56.000-06:00', shortDate)).to.eq(
-      '06-17-2023',
-    );
-  });
-});
-
-describe.skip('getDate', () => {
-  const dateString = '2021-02-10 00:00:00';
-  const date = new Date(dateString);
-
-  it('should return a date string from date object', () => {
-    expect(getDate()).to.equal(moment().format(FORMAT_YMD));
-  });
-  it('should return a date string from date string & offset', () => {
-    expect(getDate({ date, offset: { days: 3 } })).to.contain('2021-02-13');
-  });
-  it('should return a date string pattern based on date & offset', () => {
-    expect(getDate({ date, offset: { years: -1 } })).to.contain('2020-02-10');
-    expect(
-      getDate({
-        date,
-        offset: { years: -1 },
-        pattern: FORMAT_READABLE,
-      }),
-    ).to.contain('February 10, 2020');
-    expect(
-      getDate({ date: '2021-02-10 00:00:00', pattern: FORMAT_READABLE }),
-    ).to.contain('February 10, 2021');
+    expect(parseDate(new Date('2024-05-06T00:00:00.000'))).to.eq('2024-05-06');
+    expect(parseDate('2023-06-17T12:34:56.000-06:00')).to.eq('2023-06-17');
   });
 });
