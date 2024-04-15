9476d9964436c29b58273b8436b5cfafa6d1db30
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-04-15 11:56:07 -0400

    Added null checks for render methods

--- src/applications/static-pages/representative-status/components/App/States/Auth.jsx
+++ src/applications/static-pages/representative-status/components/App/States/Auth.jsx
@@ -31,144 +31,154 @@ export const Auth = ({ DynamicHeader, DynamicSubheader }) => {
   }
 
   const renderAuthNoRep = () => {
-    return (
-      <>
-        <div className="auth-header-icon">
-          <va-icon
-            icon="account_circle"
-            size={4}
-            srtext="Your representative"
-          />{' '}
-        </div>
-        <div className="auth-no-rep-text">
-          <DynamicHeader className="auth-no-rep-header">
-            You don’t have an accredited representative
-          </DynamicHeader>
-          <div className="auth-no-rep-body">
-            <va-link
-              href="https://va.gov/va-accredited-representative-faqs"
-              text="Learn about accredited representatives"
-            />
+    if (representative) {
+      return (
+        <>
+          <div className="auth-header-icon">
+            <va-icon
+              icon="account_circle"
+              size={4}
+              srtext="Your representative"
+            />{' '}
           </div>
-        </div>
-      </>
-    );
-  };
-  const renderAuthRep = () => {
-    return (
-      <>
-        <div className="auth-header-icon">
-          <va-icon
-            icon="account_circle"
-            size={4}
-            srtext="Your representative"
-          />{' '}
-        </div>
-        <div className="auth-rep-text">
-          <DynamicHeader className="auth-rep-header">
-            Your accredited representative
-          </DynamicHeader>
-          <DynamicSubheader className="auth-rep-subheader">
-            {representative.name}
-          </DynamicSubheader>
-
-          <div className="auth-rep-body">
-            {representative.concatAddress && (
-              <div className="contact-info vads-u-margin-top--1p5">
-                <div className="contact-icon">
-                  <va-icon
-                    icon="location_on"
-                    size={2}
-                    srtext="Representative address"
-                  />
-                </div>
-
-                <div className="address-link">
-                  <a
-                    href={`https://maps.google.com?daddr=${
-                      representative.concatAddress
-                    }`}
-                    tabIndex="0"
-                    target="_blank"
-                    rel="noreferrer"
-                    aria-label={`${
-                      representative.concatAddress
-                    } (opens in a new tab)`}
-                  >
-                    {representative.addressLine1}{' '}
-                    {representative.addressLine2 ? (
-                      <>
-                        <br /> {representative.addressLine2}
-                      </>
-                    ) : null}{' '}
-                    <br />
-                    {representative.city}, {representative.stateCode}{' '}
-                    {representative.zipCode}
-                  </a>
-                </div>
-              </div>
-            )}
-            {representative.email && (
-              <div className="contact-info vads-u-margin-top--1p5">
-                <div className="contact-icon">
-                  <va-icon icon="mail" size={2} srtext="Representative email" />
-                </div>
-                <a href={`mailto:${representative.email}`}>
-                  {representative.email}
-                </a>
-              </div>
-            )}
-            {representative.contact && (
-              <div className="contact-info vads-u-margin-top--1p5">
-                <div className="contact-icon">
-                  <va-icon
-                    icon="phone"
-                    size={2}
-                    srtext="Representative phone"
-                  />
-                </div>
-                <va-telephone
-                  contact={representative.contact}
-                  extension={representative.extension}
-                  disable-analytics
-                />
-              </div>
-            )}
-
-            {(representative.contact || representative.email) && (
-              <div className="contact-info vads-u-margin-top--1p5">
-                <va-link
-                  download
-                  filetype="VCF"
-                  href={representative.vcard}
-                  text="Download your accredited representative's contact information"
-                />
-              </div>
-            )}
-            <div className="contact-info vads-u-margin-top--1p5">
-              <div className="contact-icon">
-                <va-icon
-                  icon="search"
-                  size={2}
-                  srtext="Learn about accredited representatives"
-                />
-              </div>
+          <div className="auth-no-rep-text">
+            <DynamicHeader className="auth-no-rep-header">
+              You don’t have an accredited representative
+            </DynamicHeader>
+            <div className="auth-no-rep-body">
               <va-link
                 href="https://va.gov/va-accredited-representative-faqs"
                 text="Learn about accredited representatives"
               />
             </div>
           </div>
-        </div>
-      </>
-    );
+        </>
+      );
+    }
+    return <></>;
+  };
+  const renderAuthRep = () => {
+    if (representative) {
+      return (
+        <>
+          <div className="auth-header-icon">
+            <va-icon
+              icon="account_circle"
+              size={4}
+              srtext="Your representative"
+            />{' '}
+          </div>
+          <div className="auth-rep-text">
+            <DynamicHeader className="auth-rep-header">
+              Your accredited representative
+            </DynamicHeader>
+            <DynamicSubheader className="auth-rep-subheader">
+              {representative.name}
+            </DynamicSubheader>
+
+            <div className="auth-rep-body">
+              {representative.concatAddress && (
+                <div className="contact-info vads-u-margin-top--1p5">
+                  <div className="contact-icon">
+                    <va-icon
+                      icon="location_on"
+                      size={2}
+                      srtext="Representative address"
+                    />
+                  </div>
+
+                  <div className="address-link">
+                    <a
+                      href={`https://maps.google.com?daddr=${
+                        representative.concatAddress
+                      }`}
+                      tabIndex="0"
+                      target="_blank"
+                      rel="noreferrer"
+                      aria-label={`${
+                        representative.concatAddress
+                      } (opens in a new tab)`}
+                    >
+                      {representative.addressLine1}{' '}
+                      {representative.addressLine2 ? (
+                        <>
+                          <br /> {representative.addressLine2}
+                        </>
+                      ) : null}{' '}
+                      <br />
+                      {representative.city}, {representative.stateCode}{' '}
+                      {representative.zipCode}
+                    </a>
+                  </div>
+                </div>
+              )}
+              {representative.email && (
+                <div className="contact-info vads-u-margin-top--1p5">
+                  <div className="contact-icon">
+                    <va-icon
+                      icon="mail"
+                      size={2}
+                      srtext="Representative email"
+                    />
+                  </div>
+                  <a href={`mailto:${representative.email}`}>
+                    {representative.email}
+                  </a>
+                </div>
+              )}
+              {representative.contact && (
+                <div className="contact-info vads-u-margin-top--1p5">
+                  <div className="contact-icon">
+                    <va-icon
+                      icon="phone"
+                      size={2}
+                      srtext="Representative phone"
+                    />
+                  </div>
+                  <va-telephone
+                    contact={representative.contact}
+                    extension={representative.extension}
+                    disable-analytics
+                  />
+                </div>
+              )}
+
+              {(representative.contact || representative.email) && (
+                <div className="contact-info vads-u-margin-top--1p5">
+                  <va-link
+                    download
+                    filetype="VCF"
+                    href={representative.vcard}
+                    text="Download your accredited representative's contact information"
+                  />
+                </div>
+              )}
+              <div className="contact-info vads-u-margin-top--1p5">
+                <div className="contact-icon">
+                  <va-icon
+                    icon="search"
+                    size={2}
+                    srtext="Learn about accredited representatives"
+                  />
+                </div>
+                <va-link
+                  href="https://va.gov/va-accredited-representative-faqs"
+                  text="Learn about accredited representatives"
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    }
+    return <></>;
   };
 
   return (
     <>
       <va-card>
         <div className="auth-card">
-          {representative.id ? renderAuthRep() : renderAuthNoRep()}
+          {representative?.id ? renderAuthRep() : renderAuthNoRep()}
         </div>
       </va-card>
     </>
