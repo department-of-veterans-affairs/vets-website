28c53d92077fadec1ea54cc0584325de0009fba2
Peri McLaren <peri.mclaren@adhocteam.us>
2024-04-17 10:02:42 -0600

    update tests

--- src/applications/claims-status/tests/components/DueDate.unit.spec.jsx
+++ src/applications/claims-status/tests/components/DueDate.unit.spec.jsx
@@ -1,95 +1,104 @@
 import React from 'react';
-import SkinDeep from 'skin-deep';
 import { expect } from 'chai';
 import moment from 'moment';
-
+import { $ } from '@department-of-veterans-affairs/platform-forms-system/ui';
 import { buildDateFormatter } from '../../utils/helpers';
+import { renderWithRouter } from '../utils';
+
 import DueDate from '../../components/DueDate';
 
 const formatDate = buildDateFormatter();
 
 describe('<DueDate>', () => {
   it('should render past due class when theres more than a years difference', () => {
-    const date = moment()
-      .subtract(15, 'month')
-      .format('YYYY-MM-DD');
-    const tree = SkinDeep.shallowRender(<DueDate date={date} />);
-    const formattedClaimDate = formatDate(date);
-    const dueDateHeader = `Needed from you by ${formattedClaimDate} - Due a year ago`;
-
-    expect(tree.everySubTree('.past-due')).not.to.be.empty;
-    expect(tree.everySubTree('.due-date-header')[0].text()).to.equal(
-      dueDateHeader,
+    const date = moment().subtract(15, 'month');
+    const dateString = date.format('YYYY-MM-DD');
+    const { container, getByText } = renderWithRouter(
+      <DueDate date={dateString} />,
     );
+    const formattedClaimDate = formatDate(dateString);
+
+    getByText(
+      `Needed from you by ${formattedClaimDate} - Due ${date.fromNow()}`,
+    );
+    expect($('.past-due', container)).to.exist;
   });
 
   it('should render past due class when theres more than a months difference', () => {
-    const date = moment()
-      .subtract(4, 'month')
-      .format('YYYY-MM-DD');
-    const tree = SkinDeep.shallowRender(<DueDate date={date} />);
-    const formattedClaimDate = formatDate(date);
-    const dueDateHeader = `Needed from you by ${formattedClaimDate} - Due 4 months ago`;
-
-    expect(tree.everySubTree('.past-due')).not.to.be.empty;
-    expect(tree.everySubTree('.due-date-header')[0].text()).to.equal(
-      dueDateHeader,
+    const date = moment().subtract(4, 'month');
+    const dateString = date.format('YYYY-MM-DD');
+    const { container, getByText } = renderWithRouter(
+      <DueDate date={dateString} />,
     );
+    const formattedClaimDate = formatDate(dateString);
+
+    getByText(
+      `Needed from you by ${formattedClaimDate} - Due ${date.fromNow()}`,
+    );
+    expect($('.past-due', container)).to.exist;
   });
 
   it('should render past due class when theres more than a days difference', () => {
-    const date = moment()
-      .subtract(3, 'day')
-      .format('YYYY-MM-DD');
-    const tree = SkinDeep.shallowRender(<DueDate date={date} />);
-    const formattedClaimDate = formatDate(date);
-    const dueDateHeader = `Needed from you by ${formattedClaimDate} - Due 3 days ago`;
-
-    expect(tree.everySubTree('.past-due')).not.to.be.empty;
-    expect(tree.everySubTree('.due-date-header')[0].text()).to.equal(
-      dueDateHeader,
+    const date = moment().subtract(3, 'day');
+    const dateString = date.format('YYYY-MM-DD');
+    const { container, getByText } = renderWithRouter(
+      <DueDate date={dateString} />,
     );
+    const formattedClaimDate = formatDate(dateString);
+
+    getByText(
+      `Needed from you by ${formattedClaimDate} - Due ${date.fromNow()}`,
+    );
+    expect($('.past-due', container)).to.exist;
+  });
+
+  it('should render past due class when theres more than a few hours difference', () => {
+    const date = moment().subtract(1, 'day');
+    const dateString = date.format('YYYY-MM-DD');
+    const { container, getByText } = renderWithRouter(
+      <DueDate date={dateString} />,
+    );
+    const formattedClaimDate = formatDate(dateString);
+
+    getByText(
+      `Needed from you by ${formattedClaimDate} - Due ${date.fromNow()}`,
+    );
+    expect($('.past-due', container)).to.exist;
   });
 
   it('should render file due class when more than a days difference', () => {
-    const date = moment()
-      .add(3, 'day')
-      .format('YYYY-MM-DD');
-    const tree = SkinDeep.shallowRender(<DueDate date={date} />);
-    const formattedClaimDate = formatDate(date);
-    const dueDateHeader = `Needed from you by ${formattedClaimDate}`;
-
-    expect(tree.everySubTree('.due-file')).not.to.be.empty;
-    expect(tree.everySubTree('.due-date-header')[0].text()).to.equal(
-      dueDateHeader,
+    const date = moment().add(3, 'day');
+    const dateString = date.format('YYYY-MM-DD');
+    const { container, getByText } = renderWithRouter(
+      <DueDate date={dateString} />,
     );
+    const formattedClaimDate = formatDate(dateString);
+
+    getByText(`Needed from you by ${formattedClaimDate}`);
+    expect($('.due-file', container)).to.exist;
   });
 
   it('should render file due class when more than a months difference', () => {
-    const date = moment()
-      .add(10, 'month')
-      .format('YYYY-MM-DD');
-    const tree = SkinDeep.shallowRender(<DueDate date={date} />);
-    const formattedClaimDate = formatDate(date);
-    const dueDateHeader = `Needed from you by ${formattedClaimDate}`;
-
-    expect(tree.everySubTree('.due-file')).not.to.be.empty;
-    expect(tree.everySubTree('.due-date-header')[0].text()).to.equal(
-      dueDateHeader,
+    const date = moment().add(10, 'month');
+    const dateString = date.format('YYYY-MM-DD');
+    const { container, getByText } = renderWithRouter(
+      <DueDate date={dateString} />,
     );
+    const formattedClaimDate = formatDate(dateString);
+
+    getByText(`Needed from you by ${formattedClaimDate}`);
+    expect($('.due-file', container)).to.exist;
   });
 
   it('should render file due class when more than a years difference', () => {
-    const date = moment()
-      .add(15, 'month')
-      .format('YYYY-MM-DD');
-    const tree = SkinDeep.shallowRender(<DueDate date={date} />);
-    const formattedClaimDate = formatDate(date);
-    const dueDateHeader = `Needed from you by ${formattedClaimDate}`;
-
-    expect(tree.everySubTree('.due-file')).not.to.be.empty;
-    expect(tree.everySubTree('.due-date-header')[0].text()).to.equal(
-      dueDateHeader,
+    const date = moment().add(15, 'month');
+    const dateString = date.format('YYYY-MM-DD');
+    const { container, getByText } = renderWithRouter(
+      <DueDate date={dateString} />,
     );
+    const formattedClaimDate = formatDate(dateString);
+
+    getByText(`Needed from you by ${formattedClaimDate}`);
+    expect($('.due-file', container)).to.exist;
   });
 });
