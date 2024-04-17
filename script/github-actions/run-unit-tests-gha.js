1313a741430efd0ceda5f6b36283746632e4786f
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2024-04-17 15:43:53 -0400

    Try running stress test chunked by app

--- script/github-actions/run-unit-tests-gha.js
+++ script/github-actions/run-unit-tests-gha.js
@@ -8,8 +8,8 @@ const { runCommand } = require('../utils');
 
 const specDirs = '{src,script}';
 const defaultPath = `./${specDirs}/**/*.unit.spec.js?(x)`;
-// const numContainers = process.env.NUM_CONTAINERS || 1;
-// const matrixStep = process.env.STEP || 0;
+const numContainers = process.env.NUM_CONTAINERS || 1;
+const matrixStep = process.env.STEP || 0;
 
 const COMMAND_LINE_OPTIONS_DEFINITIONS = [
   { name: 'log-level', type: String, defaultValue: 'log' },
@@ -41,14 +41,14 @@ const allUnitTestDirs = Array.from(
   ),
 ).filter(spec => spec !== undefined);
 
-// function splitArray(array, chunks) {
-//   const [...arrayCopy] = array;
-//   const arrayChunks = [];
-//   while (arrayCopy.length) {
-//     arrayChunks.push(arrayCopy.splice(0, chunks));
-//   }
-//   return arrayChunks;
-// }
+function splitArray(array, chunks) {
+  const [...arrayCopy] = array;
+  const arrayChunks = [];
+  while (arrayCopy.length) {
+    arrayChunks.push(arrayCopy.splice(0, chunks));
+  }
+  return arrayChunks;
+}
 const options = commandLineArgs(COMMAND_LINE_OPTIONS_DEFINITIONS);
 let coverageInclude = '';
 
@@ -85,13 +85,13 @@ if (process.env.TESTS_TO_VERIFY) {
   testsToVerify = JSON.parse(process.env.TESTS_TO_VERIFY).join(' ');
 }
 
-// const splitUnitTests = splitArray(
-//   allUnitTestDirs,
-//   Math.ceil(allUnitTestDirs.length / numContainers),
-// );
+const splitUnitTests = splitArray(
+  allUnitTestDirs,
+  Math.ceil(allUnitTestDirs.length / numContainers),
+);
 const appsToRun = options['app-folder']
   ? [options['app-folder']]
-  : allUnitTestDirs;
+  : splitUnitTests[matrixStep];
 
 if (testsToVerify === null) {
   if (appsToRun && appsToRun.length > 0) {
@@ -125,9 +125,26 @@ if (testsToVerify === null) {
     core.exportVariable('NO_APPS_TO_RUN', true);
   }
 } else {
+  const appsToVerify = process.env.APPS_TO_VERIFY.filter(app =>
+    app.startsWith('src/applications'),
+  ).map(app => app.split('/')[2]);
+  for (const app of appsToVerify) {
+    const testsToRun = testsToVerify.filter(test =>
+      test.includes(`src/applications/${app}`),
+    );
+    if (testsToRun !== '') {
+      const command = `LOG_LEVEL=${options[
+        'log-level'
+      ].toLowerCase()} ${testRunner} --max-old-space-size=8192 --config ${configFile} ${testsToRun.replace(
+        /,/g,
+        ' ',
+      )} `;
+      // Case: Unit Tests are available for the selected app to run and will run here for the one app only.
+      runCommand(command);
+    } else {
+      // Case: Unit Tests are runnning, but the app with changed code in this case has no unit tests as a part of it.
+      console.log('This app has no tests to run');
+    }
+  }
   // Case: Unit Tests are needed to be Stress Tested. Selected tests are all run in one container so each container runs the full suite.
-  const command = `LOG_LEVEL=${options[
-    'log-level'
-  ].toLowerCase()} ${testRunner} --max-old-space-size=8192 --config ${configFile} ${testsToVerify}`;
-  runCommand(command);
 }
