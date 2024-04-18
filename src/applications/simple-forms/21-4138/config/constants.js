bbdfbe179f3129a4983528f6804bbb61d4bf0ff6
Jacob Penner <jacob.penner@adhocteam.us>
2024-04-18 09:20:58 -0500

    first iteration of IntroductionPage

--- src/applications/simple-forms/21-4138/config/constants.js
+++ src/applications/simple-forms/21-4138/config/constants.js
@@ -1,6 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
-export const TITLE = '21-4138 Statement in Support of a Claim';
-export const SUBTITLE =
-  'Equal to VA Form 21-4138 (21-4138 Statement in Support of a Claim)';
+export const TITLE = 'Submit a statement to support a claim';
+export const SUBTITLE = 'Statement in Support of Claim (VA Form 21-4138)';
+
+export const workInProgressContent = {
+  description:
+    'We’re rolling out Submit a Statement to Support a Claim (VA Form 21-4138) in stages. It’s not quite ready yet. Please check back again soon.',
+  redirectLink: '/',
+  redirectText: 'Return to VA home page',
+};
