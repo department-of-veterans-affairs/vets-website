a60791444d1a0562a22e3b275661e7e5feb8a592
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-04-19 18:52:59 -0400

    fixing failing tests window.history

--- src/platform/mhv/hooks/useMyHealthAccessGuard.jsx
+++ src/platform/mhv/hooks/useMyHealthAccessGuard.jsx
@@ -1,26 +1,18 @@
-import React from 'react';
 import { useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 
 /**
  * Route guard hook that will redirect the user to the /my-health landing page if the user does not have an MHV account
- * @returns {JSX.Element|null}
+ * @returns {null}
  */
-
 export const useMyHealthAccessGuard = () => {
   const mhvAccountState = useSelector(
     state => state?.user?.profile?.mhvAccountState,
   );
 
-  if (mhvAccountState === 'NONE') {
-    return (
-      <Redirect
-        to={{
-          pathname: '/my-health/',
-          state: { from: window.location.pathname },
-        }}
-      />
-    );
+  if (mhvAccountState !== 'NONE') {
+    window.history.replaceState({}, '', '/my-health/');
+    window.location.reload();
+    return null;
   }
 
   return null;
