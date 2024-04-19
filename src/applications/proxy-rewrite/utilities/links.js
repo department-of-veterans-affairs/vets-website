96b561fb06f144ba0d0e2f4f280c664cb1eed796
Randi Mays <rmays@kindsys.us>
2024-04-19 11:39:02 -0500

    Fix code scanning issue with domains

--- src/applications/proxy-rewrite/utilities/links.js
+++ src/applications/proxy-rewrite/utilities/links.js
@@ -1,23 +1,33 @@
 export const updateLinkDomain = href => {
   const local1 = 'http://localhost:3001';
   const local2 = 'http://localhost:3002';
-  const staging = 'https://staging.va.gov';
-  const prod = 'https://va.gov';
+  const stagingWithoutWWW = 'https://staging.va.gov';
+  const stagingWithWWW = 'https://www.staging.va.gov';
+  const prodWithoutWWW = 'https://va.gov';
+  const prodWithWWW = 'https://www.va.gov';
 
-  if (href.includes(local1)) {
-    return href.replace(local1, prod);
+  if (href.startsWith(local1)) {
+    return href.replace(local1, prodWithWWW);
   }
 
-  if (href.includes(local2)) {
-    return href.replace(local2, prod);
+  if (href.startsWith(local2)) {
+    return href.replace(local2, prodWithWWW);
   }
 
-  if (href.includes(staging)) {
-    return href.replace(staging, prod);
+  if (href.startsWith(stagingWithoutWWW)) {
+    return href.replace(stagingWithoutWWW, prodWithWWW);
+  }
+
+  if (href.startsWith(stagingWithWWW)) {
+    return href.replace(stagingWithWWW, prodWithWWW);
+  }
+
+  if (href.startsWith(prodWithoutWWW)) {
+    return href.replace(prodWithoutWWW, prodWithWWW);
   }
 
   if (href.charAt(0) === '/') {
-    return `${prod}${href}`;
+    return `${prodWithWWW}${href}`;
   }
 
   return href;
