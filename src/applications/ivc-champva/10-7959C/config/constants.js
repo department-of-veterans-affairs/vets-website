cd3c891ea9fd80fb0023048dc24e3de3265b05d7
Michael Clement <michael.clement@adhocteam.us>
2024-04-19 14:31:11 -0500

    80072a medicare file uploads (pt 1/X) (#29277)
    * pulled
    
    * moved func to shared dir
    
    * transformer
    
    * moved stuff around, added file upload to 7959c

--- src/applications/ivc-champva/10-7959C/config/constants.js
+++ src/applications/ivc-champva/10-7959C/config/constants.js
@@ -50,3 +50,10 @@ export const yesNoOptions = [
     value: 'no',
   },
 ];
+
+/* List of required files - not enforced by the form because we want
+users to be able to opt into mailing these documents. This object 
+performs double duty by also providing a map to presentable names. */
+export const requiredFiles = {
+  applicantMedicareIneligibleProof: 'Proof of Medicare Ineligibility',
+};
