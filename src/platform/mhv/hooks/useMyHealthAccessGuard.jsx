e2193ed7fb43ad14b7ccd7f8c47ab369c0349a4a
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-04-21 00:10:37 -0400

    remove use of useHistory

--- src/platform/mhv/hooks/useMyHealthAccessGuard.jsx
+++ src/platform/mhv/hooks/useMyHealthAccessGuard.jsx
@@ -1,29 +1,25 @@
-/* eslint-disable no-console */
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 
 export const useMyHealthAccessGuard = () => {
-  const history = useHistory();
   const mhvAccountState = useSelector(
     state => state?.user?.profile?.mhvAccountState,
   );
 
   useEffect(
     () => {
-      if (mhvAccountState === 'NONE') {
-        console.log('Redirecting to /my-health');
+      if (mhvAccountState !== 'NONE') {
+        const currentPath = window.location.pathname;
+        // Check if the path starts with '/my-health/' and has more characters beyond '/my-health/'
         if (
-          !window.location.pathname.startsWith('/my-health/') ||
-          window.location.pathname.length > '/my-health/'.length
+          currentPath.startsWith('/my-health/') &&
+          currentPath !== '/my-health'
         ) {
-          history.replace('/my-health');
+          window.location.replace('/my-health');
         }
-      } else {
-        console.log('Access granted to current path');
       }
     },
-    [mhvAccountState, history],
+    [mhvAccountState],
   );
 
   return null;
