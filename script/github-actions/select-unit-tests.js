dc5899400b20bdff1380935986b5b6b178804e25
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2024-04-22 15:13:36 -0400

    fix array dedupe placement

--- script/github-actions/select-unit-tests.js
+++ script/github-actions/select-unit-tests.js
@@ -14,13 +14,11 @@ const DISALLOWED_SPECS = ALLOW_LIST.filter(spec => spec.allowed === false).map(
 );
 
 const CHANGED_APPS = CHANGED_FILES
-  ? new Set(
-      ...CHANGED_FILES.split(' ').map(filePath =>
-        filePath
-          .split('/')
-          .slice(0, 3)
-          .join('/'),
-      ),
+  ? Array.from(new Set(...CHANGED_FILES.split(' '))).map(filePath =>
+      filePath
+        .split('/')
+        .slice(0, 3)
+        .join('/'),
     )
   : [];
 
