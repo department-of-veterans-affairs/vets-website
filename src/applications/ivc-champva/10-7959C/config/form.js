e5374d39b50b98d1defc5f61a43fd8c00d057996
Michael Clement <michael.clement@adhocteam.us>
2024-04-22 15:39:30 -0500

    set up prefill on 10-7959c

--- src/applications/ivc-champva/10-7959C/config/form.js
+++ src/applications/ivc-champva/10-7959C/config/form.js
@@ -3,9 +3,9 @@ import get from 'platform/utilities/data/get';
 import manifest from '../manifest.json';
 import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
-import transformForSubmit from './submitTransformer';
 import { applicantWording, getAgeInYears } from '../../shared/utilities';
 import FileFieldWrapped from '../components/FileUploadWrapper';
+import { prefillTransformer } from './prefillTransformer';
 
 import {
   certifierRole,
@@ -124,8 +124,12 @@ const formConfig = {
   introduction: IntroductionPage,
   confirmation: ConfirmationPage,
   v3SegmentedProgressBar: true,
+  showReviewErrors: !environment.isProduction(),
   formId: '10-7959C',
-  transformForSubmit,
+  dev: {
+    showNavLinks: false,
+    collapsibleNavLinks: true,
+  },
   saveInProgress: {
     messages: {
       inProgress:
@@ -138,6 +142,7 @@ const formConfig = {
   },
   version: 0,
   prefillEnabled: true,
+  prefillTransformer,
   savedFormMessages: {
     notFound:
       'Please start over to apply for CHAMPVA other health insurance certification.',
