924418bda73da5fa91b8c501575c40a3ea8102b1
Tai Wilkin <tai.wilkin@coforma.io>
2024-04-22 09:34:10 -0400

    Update ListItemView

--- src/applications/pensions/components/ListItemView.jsx
+++ src/applications/pensions/components/ListItemView.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useFeatureToggle } from 'platform/utilities/feature-toggles';
 
-const ListItemView = ({ title }) => (
-  <h3 className="vads-u-font-size--h5 vads-u-margin-y--1">{title}</h3>
-);
+const ListItemView = ({ title }) => {
+  const { useToggleValue, TOGGLE_NAMES } = useFeatureToggle();
+  const pensionMultiresponseStyles = useToggleValue(
+    TOGGLE_NAMES.pensionMultiresponseStyles,
+  );
+  return pensionMultiresponseStyles ? (
+    <div className="vads-u-padding--2">
+      <strong>{title}</strong>
+      <br />
+    </div>
+  ) : (
+    <h3 className="vads-u-font-size--h5 vads-u-margin-y--1">{title}</h3>
+  );
+};
 
 ListItemView.propTypes = {
   title: PropTypes.string.isRequired,
