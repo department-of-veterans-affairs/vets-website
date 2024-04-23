54137c69744d549c71cf9d0490772d07fe108d8b
Richard Davis <richard.davis@agile6.com>
2024-04-23 10:22:02 -0400

    Adds GA for loa1 and non-VA patient conditions

--- src/applications/mhv-landing-page/mocks/api/index.js
+++ src/applications/mhv-landing-page/mocks/api/index.js
@@ -5,16 +5,17 @@ const commonResponses = require('../../../../platform/testing/local-dev-mock-api
 
 const featureToggles = require('./feature-toggles/index');
 const user = require('./user/index');
+// const userWithNoFacilities = require('../../tests/fixtures/user.no-facilities.json');
 const folders = require('./mhv-api/messaging/folders/index');
 const vamcEhr = require('../../tests/fixtures/vamc-ehr.json');
 const personalInformation = require('../../tests/fixtures/personal-information.json');
 
 const responses = {
   ...commonResponses,
-  'GET /v0/user': user.defaultUser,
+  'GET /v0/user': user,
   'GET /v0/feature_toggles': featureToggles.generateFeatureToggles({
     mhvLandingPageEnabled: true,
-    mhvLandingPagePersonalization: true,
+    mhvLandingPagePersonalization: false,
   }),
   'GET /my_health/v1/messaging/folders': folders.allFoldersWithUnreadMessages,
   'GET /data/cms/vamc-ehr.json': vamcEhr,
