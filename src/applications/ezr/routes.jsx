6a4de10f6405003b3d65298300f377b24d91cb7b
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-04-23 17:38:54 -0400

    add mhv route guard to EZR

--- src/applications/ezr/routes.jsx
+++ src/applications/ezr/routes.jsx
@@ -1,14 +1,28 @@
 import { createRoutesWithSaveInProgress } from '@department-of-veterans-affairs/platform-forms/save-in-progress/helpers';
+import { useMyHealthAccessGuard } from '~/platform/mhv/hooks/useMyHealthAccessGuard';
 import formConfig from './config/form';
 import App from './containers/App';
 
+const AccessGuardWrapper = ({ children }) => {
+  const redirectToMyHealth = useMyHealthAccessGuard();
+  if (redirectToMyHealth) {
+    return null;
+  }
+  return children;
+};
+
 const route = {
   path: '/',
-  component: App,
+  component: AccessGuardWrapper,
   indexRoute: {
     onEnter: (nextState, replace) => replace('/introduction'),
   },
-  childRoutes: createRoutesWithSaveInProgress(formConfig),
+  childRoutes: [
+    {
+      component: App,
+      childRoutes: createRoutesWithSaveInProgress(formConfig),
+    },
+  ],
 };
 
 export default route;
