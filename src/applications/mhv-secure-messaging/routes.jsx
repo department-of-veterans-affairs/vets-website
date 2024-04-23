92a9a28a8cfa8093c83e94d55b1bfcaaee5c5d5c
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-04-23 17:58:27 -0400

    add mhv route guard to SM

--- src/applications/mhv-secure-messaging/routes.jsx
+++ src/applications/mhv-secure-messaging/routes.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { useMyHealthAccessGuard } from '~/platform/mhv/hooks/useMyHealthAccessGuard';
 import App from './containers/App';
+
+const AccessGuardWrapper = ({ children }) => {
+  const redirectToMyHealth = useMyHealthAccessGuard();
+  if (redirectToMyHealth) {
+    return redirectToMyHealth;
+  }
+  return children;
+};
 
 const routes = (
   <Switch>
-    <Route path="/" key="App">
-      <App />
-    </Route>
+    <AccessGuardWrapper>
+      <Route path="/" key="App">
+        <App />
+      </Route>
+    </AccessGuardWrapper>
   </Switch>
 );
 
