31ae470026f2c725e1644e38de060d808eb2f64d
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-04-23 18:37:10 -0400

    add mhv route guard to supply reodering

--- src/applications/health-care-supply-reordering/routes.jsx
+++ src/applications/health-care-supply-reordering/routes.jsx
@@ -1,13 +1,28 @@
 import { createRoutesWithSaveInProgress } from 'platform/forms/save-in-progress/helpers';
+import { useMyHealthAccessGuard } from '~/platform/mhv/hooks/useMyHealthAccessGuard';
 import formConfig from './config/form';
-import App from './containers/App.jsx';
+import App from './containers/App';
+
+const AccessGuardWrapper = ({ children }) => {
+  const redirectToMyHealth = useMyHealthAccessGuard();
+  if (redirectToMyHealth) {
+    return null;
+  }
+  return children;
+};
 
 const route = {
   path: '/',
-  component: App,
-  indexRoute: { onEnter: (nextState, replace) => replace('/introduction') },
-
-  childRoutes: createRoutesWithSaveInProgress(formConfig),
+  component: AccessGuardWrapper,
+  indexRoute: {
+    onEnter: (nextState, replace) => replace('/introduction'),
+  },
+  childRoutes: [
+    {
+      component: App,
+      childRoutes: createRoutesWithSaveInProgress(formConfig),
+    },
+  ],
 };
 
 export default route;
