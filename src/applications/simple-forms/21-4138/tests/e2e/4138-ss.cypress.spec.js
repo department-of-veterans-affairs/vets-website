20e79d47865ca1f4688731118bc4fe38f361af7d
Jacob Penner <jacob.penner@adhocteam.us>
2024-04-24 11:22:49 -0400

    update e2e coverage for 21-4138 form

--- src/applications/simple-forms/21-4138/tests/e2e/4138-ss.cypress.spec.js
+++ src/applications/simple-forms/21-4138/tests/e2e/4138-ss.cypress.spec.js
@@ -7,6 +7,9 @@ import formConfig from '../../config/form';
 import manifest from '../../manifest.json';
 import featureToggles from './fixtures/mocks/featureToggles.json';
 import user from './fixtures/mocks/user.json';
+import sipPut from './fixtures/mocks/sip-put.json';
+import sipGet from './fixtures/mocks/sip-get.json';
+import mockSubmit from '../../../shared/tests/e2e/fixtures/mocks/application-submit.json';
 
 // mock logged in LOA3 user
 const userLOA3 = {
@@ -30,20 +33,22 @@ const userLOA3 = {
 
 const testConfig = createTestConfig(
   {
+    useWebComponentFields: true,
+
     dataPrefix: 'data',
-
     dataDir: path.join(__dirname, 'fixtures', 'data'),
-
-    // Rename and modify the test data as needed.
-    dataSets: ['test-data'],
+    dataSets: ['veteran'],
 
     pageHooks: {
       introduction: ({ afterHook }) => {
         afterHook(() => {
-          cy.findAllByText(/^Start your statement/, { selector: 'a' })
+          cy.findAllByText(/^Start/, { selector: 'a[href="#start"]' })
             .last()
             .click();
         });
+      },
+      'living-situation': ({ afterHook }) => {
+        afterHook(() => {});
       },
     },
 
@@ -56,6 +61,9 @@ const testConfig = createTestConfig(
     setupPerTest: () => {
       cy.intercept('/v0/api', { status: 200 });
       cy.intercept('/v0/feature_toggles', featureToggles);
+      cy.intercept('PUT', '/v0/in_progress_forms/21-4138', sipPut);
+      cy.intercept('GET', '/v0/in_progress_forms/21-4138', sipGet);
+      cy.intercept(formConfig.submitUrl, mockSubmit);
       cy.login(userLOA3);
     },
 
