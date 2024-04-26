148a7a49c4c3b75ce059743740f6c925e507ca68
fazilqa <fazil.mammadov@bylight.com>
2024-04-26 10:20:31 -0700

    upd: code cleanup

--- src/applications/mhv-secure-messaging/tests/e2e/secure-messaging-save-reply-draft.cypress.spec.js
+++ src/applications/mhv-secure-messaging/tests/e2e/secure-messaging-save-reply-draft.cypress.spec.js
@@ -7,57 +7,55 @@ import mockMessages from './fixtures/messages-response.json';
 import mockSingleThread from './fixtures/thread-response.json';
 import { AXE_CONTEXT, Data, Locators } from './utils/constants';
 
-for (let i = 0; i < 200; i += 1) {
-  describe('Secure Messaging Reply', () => {
-    it('Axe Check Message Reply', () => {
-      // declare pages & constants
-      const landingPage = new PatientInboxPage();
-      const site = new SecureMessagingSite();
-      const draftPage = new PatientMessageDraftsPage();
-      const messageDetailsPage = new PatientMessageDetailsPage();
+describe('Secure Messaging Reply', () => {
+  it('Axe Check Message Reply', () => {
+    // declare pages & constants
+    const landingPage = new PatientInboxPage();
+    const site = new SecureMessagingSite();
+    const draftPage = new PatientMessageDraftsPage();
+    const messageDetailsPage = new PatientMessageDetailsPage();
 
-      const bodyText = ' Updated body text';
-      const singleMessage = { data: mockSingleThread.data[0] };
-      singleMessage.data.attributes.body = bodyText;
+    const bodyText = ' Updated body text';
+    const singleMessage = { data: mockSingleThread.data[0] };
+    singleMessage.data.attributes.body = bodyText;
 
-      // load single thread
-      site.login();
-      landingPage.loadInboxMessages(mockMessages);
-      landingPage.loadSingleThread(mockSingleThread);
+    // load single thread
+    site.login();
+    landingPage.loadInboxMessages(mockMessages);
+    landingPage.loadSingleThread(mockSingleThread);
 
-      // click reply btn
-      messageDetailsPage.clickReplyButton(mockSingleThread);
+    // click reply btn
+    messageDetailsPage.clickReplyButton(mockSingleThread);
 
-      // change message
-      PatientReplyPage.getMessageBodyField().type(bodyText, {
-        force: true,
-      });
-
-      // save changed message as a draft
-      draftPage.saveNewDraftMessage(mockSingleThread, singleMessage);
-
-      // assert message saved
-      cy.get(Locators.ALERTS.SAVE_DRAFT).should(
-        'include.text',
-        Data.MESSAGE_WAS_SAVED,
-      );
-
-      // verify reply topic
-      messageDetailsPage.replyToMessageTo(singleMessage);
-
-      // verify saved draft details
-      messageDetailsPage.replyToMessageSenderName(singleMessage); // TODO skipped for flakiness - verify by 200x pattern
-
-      messageDetailsPage.replyToMessageRecipientName(singleMessage);
-
-      messageDetailsPage.replyToMessageDate(singleMessage);
-
-      messageDetailsPage.replyToMessageId(singleMessage);
-
-      messageDetailsPage.replyToMessageBody(singleMessage); // TODO skipped for flakiness - verify by 200x pattern
-
-      cy.injectAxe();
-      cy.axeCheck(AXE_CONTEXT);
+    // change message
+    PatientReplyPage.getMessageBodyField().type(bodyText, {
+      force: true,
     });
+
+    // save changed message as a draft
+    draftPage.saveNewDraftMessage(mockSingleThread, singleMessage);
+
+    // assert message saved
+    cy.get(Locators.ALERTS.SAVE_DRAFT).should(
+      'include.text',
+      Data.MESSAGE_WAS_SAVED,
+    );
+
+    // verify reply topic
+    messageDetailsPage.replyToMessageTo(singleMessage);
+
+    // verify saved draft details
+    messageDetailsPage.replyToMessageSenderName(singleMessage); // TODO skipped for flakiness - verify by 200x pattern
+
+    messageDetailsPage.replyToMessageRecipientName(singleMessage);
+
+    messageDetailsPage.replyToMessageDate(singleMessage);
+
+    messageDetailsPage.replyToMessageId(singleMessage);
+
+    messageDetailsPage.replyToMessageBody(singleMessage); // TODO skipped for flakiness - verify by 200x pattern
+
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT);
   });
-}
+});
