ac5266ad140a705c447f4049a0a100d1e4506b00
Andrew Rodiek <andrew.rodiek@gmail.com>
2024-04-26 09:51:49 -0400

    [FSR] 79756 - Cleaning up components from v3 upgrade (#29292)

--- src/applications/financial-status-report/tests/e2e/fsr-5655-alert-cards-combined.cypress.spec.js
+++ src/applications/financial-status-report/tests/e2e/fsr-5655-alert-cards-combined.cypress.spec.js
@@ -19,7 +19,7 @@ import saveInProgressData from './fixtures/mocks/saveInProgress.json';
 // TODO: Skipping this test due to the Chromium Renderer crash issue.
 // Track the progress of the fix in issue #63283
 
-describe('Enhanced FSR debt and copay alerts', () => {
+describe.skip('Enhanced FSR debt and copay alerts', () => {
   afterEach(() => {
     cy.window().then(win => {
       win.sessionStorage.clear();
