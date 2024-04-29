2953f757d22edfdd47b51248bd368b44ce44e278
Ryan Shaw <ryan.shaw@adhocteam.us>
2024-04-28 19:02:14 -0700

    WIP: updated tests
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/tests/e2e/fixtures/MockAppointmentResponse.js
+++ src/applications/vaos/tests/e2e/fixtures/MockAppointmentResponse.js
@@ -45,14 +45,15 @@ export default class MockAppointmentResponse {
   } = {}) {
     const requestedPeriods = [];
     let timestamp = moment();
+    // let timestamp = moment().utc();
 
     if (localStartTime && localStartTime instanceof moment)
       timestamp = localStartTime;
 
     if (status === APPOINTMENT_STATUS.proposed) {
       requestedPeriods.push({
-        start: timestamp.format('YYYY-MM-DDTHH:mm:ss.000Z'),
-        end: timestamp.format('YYYY-MM-DDTHH:mm:ss.000Z'),
+        start: timestamp.utc().format('YYYY-MM-DDTHH:mm:ss.000Z'),
+        end: timestamp.utc().format('YYYY-MM-DDTHH:mm:ss.000Z'),
       });
     }
 
