700d68b40fb1f77dfc371a4d27883ef4ba7535c4
cohnjesse <cohnjesse@gmail.com>
2024-04-30 15:59:51 -0400

    Adjusted test

--- src/applications/accredited-representative-portal/tests/components/common/Header/MobileHeader/MobileHeader.unit.spec.js
+++ src/applications/accredited-representative-portal/tests/components/common/Header/MobileHeader/MobileHeader.unit.spec.js
@@ -2,15 +2,31 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
 import { MemoryRouter } from 'react-router-dom-v5-compat';
+import { renderInReduxProvider } from '@department-of-veterans-affairs/platform-testing/react-testing-library-helpers';
 
 import MobileHeader from '../../../../../components/common/Header/MobileHeader/MobileHeader';
 
 describe('MobileHeader', () => {
+  const initialState = {
+    featureToggles: {
+      // eslint-disable-next-line camelcase
+      accredited_representative_portal_pilot: true,
+      loading: false,
+    },
+    user: {
+      login: {
+        currentlyLoggedIn: false,
+      },
+    },
+  };
   const getMobileHeader = () =>
-    render(
+  renderInReduxProvider(
       <MemoryRouter>
         <MobileHeader />
       </MemoryRouter>,
+      {
+        initialState,
+      },
     );
 
   it('renders header on mobile', () => {
