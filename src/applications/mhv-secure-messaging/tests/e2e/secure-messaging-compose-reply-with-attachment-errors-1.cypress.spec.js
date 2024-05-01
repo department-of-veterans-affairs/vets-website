b79775b53e53a7aed0c3e2e33c6580dfe698dc37
fazilqa <fazil.mammadov@bylight.com>
2024-05-01 14:14:01 -0700

    upd: new import in compose page

--- src/applications/mhv-secure-messaging/tests/e2e/secure-messaging-compose-reply-with-attachment-errors-1.cypress.spec.js
+++ src/applications/mhv-secure-messaging/tests/e2e/secure-messaging-compose-reply-with-attachment-errors-1.cypress.spec.js
@@ -6,32 +6,32 @@ import { AXE_CONTEXT, Data } from './utils/constants';
 describe('Start a new message With Attacments and Errors', () => {
   it('start a new message with attachment', () => {
     const landingPage = new PatientInboxPage();
-    const composePage = new PatientComposePage();
     const site = new SecureMessagingSite();
     site.login();
     landingPage.loadInboxMessages();
     landingPage.navigateToComposePage();
     cy.injectAxe();
     cy.axeCheck(AXE_CONTEXT, {});
-    composePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
-    composePage.selectCategory('COVID');
-    composePage.attachMessageFromFile(Data.TEST_VIDEO);
-    composePage.verifyAttachmentErrorMessage(
+    PatientComposePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
+    PatientComposePage.selectCategory('COVID');
+    PatientComposePage.attachMessageFromFile(Data.TEST_VIDEO);
+    PatientComposePage.verifyAttachmentErrorMessage(
       "We can't attach this file type. Try attaching a DOC, DOCX, GIF, JPG, PDF, PNG, RTF, TXT, XLS XLSX, JPEG, JFIF, PJPEG, or PJP.",
     );
-    composePage.attachMessageFromFile('empty.txt');
-    composePage.verifyAttachmentErrorMessage(
+    PatientComposePage.attachMessageFromFile('empty.txt');
+    PatientComposePage.verifyAttachmentErrorMessage(
       'Your file is empty. Try attaching a different file.',
     );
-    composePage.attachMessageFromFile('test_ext.TXT');
-    composePage.removeAttachedFile();
+    PatientComposePage.attachMessageFromFile('test_ext.TXT');
+    PatientComposePage.removeAttachedFile();
 
-    composePage.getMessageSubjectField().type(Data.TEST_SUBJECT);
-    composePage
-      .getMessageBodyField()
-      .type(Data.TEST_MESSAGE_BODY, { force: true, waitforanimations: true });
-    composePage.sendMessage();
-    composePage.verifySendMessageConfirmationMessageText();
-    composePage.verifySendMessageConfirmationMessageHasFocus();
+    PatientComposePage.getMessageSubjectField().type(Data.TEST_SUBJECT);
+    PatientComposePage.getMessageBodyField().type(Data.TEST_MESSAGE_BODY, {
+      force: true,
+      waitforanimations: true,
+    });
+    PatientComposePage.sendMessage();
+    PatientComposePage.verifySendMessageConfirmationMessageText();
+    PatientComposePage.verifySendMessageConfirmationMessageHasFocus();
   });
 });
