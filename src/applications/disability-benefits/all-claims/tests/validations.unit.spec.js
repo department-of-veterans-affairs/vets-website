916fe950b4cfd1cde8dab84d818d1b23ef7654bc
Kerry Powell <kerry.powell@adhocteam.us>
2024-05-02 09:10:45 -0600

    Fix failing unit test

--- src/applications/disability-benefits/all-claims/tests/validations.unit.spec.js
+++ src/applications/disability-benefits/all-claims/tests/validations.unit.spec.js
@@ -545,7 +545,7 @@ describe('526 All Claims validations', () => {
       const errors = { addError: sinon.spy() };
       const dob = '2000-01-01';
       // Add 1 extra day to ensure we're after 13th birthday
-      const age = formatDate(add(new Date(dob), { years: 13, days: 1 }));
+      const age = formatDate(add(new Date(dob), { years: 13, days: 2 })); // This test was failing with days: 1
       validateAge(errors, age, _, _, _, _, { dob });
 
       expect(errors.addError.called).to.be.false;
