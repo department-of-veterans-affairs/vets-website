440c83b01f516cd412773858c837b3e8bb8dcb47
Rachel <rachele.eiting@copilot.llc>
2024-05-03 11:20:00 -0400

    updated to match new backend

--- src/applications/ivc-champva/10-7959f-1/config/prefillTransformer.js
+++ src/applications/ivc-champva/10-7959f-1/config/prefillTransformer.js
@@ -1,26 +1,16 @@
-export const prefillTransformer = (pages, formData, metadata) => {
+const prefillTransformer = (pages, metadata, formData) => {
   return {
     pages,
     formData: {
-      veteran: {
-        dateOfBirth: formData.veteranDOB,
-        fullName: formData.fullName,
-        first: formData.fullName.first,
-        middle: formData.fullName.middle,
-        last: formData.fullName.last,
-      },
-      physicalAddress: {
-        street: formData.physicalAddress.street,
-        street2: formData.physicalAddress.street2,
-        city: formData.physicalAddress.city,
-        state: formData.physicalAddress.state,
-        country: formData.physicalAddress.country,
-        postalCode: formData.physicalAddress.postalCode,
-      },
+      veteranFullName: formData.fullName,
+      veteranAddress: formData.mailingAddress,
+      veteranDateOfBirth: formData.veteranDOB,
       veteranSocialSecurityNumber: formData.ssn,
-      phoneNumber: formData.phoneNumber,
-      emailAddress: formData.emailAddress,
+      veteranPhoneNumber: formData.phoneNumber,
+      veteranEmailAddress: formData.emailAddress,
     },
     metadata,
   };
 };
+
+export default prefillTransformer;
