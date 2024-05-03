67a6f67c4663c2c8e867a77f1072db9bafa3a620
Jacob Penner <jacob.penner@adhocteam.us>
2024-05-03 15:25:27 -0400

    fix typo

--- src/applications/static-pages/simple-forms/21-4138/entry.js
+++ src/applications/static-pages/simple-forms/21-4138/entry.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 export default function create214138Access(store, widgetType) {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   if (root) {
-    import(/* webpackChunkName: "form-20-10207" */ './App.js').then(module => {
+    import(/* webpackChunkName: "form-21-4138" */ './App.js').then(module => {
       const App = module.default;
       ReactDOM.render(
         <Provider store={store}>
