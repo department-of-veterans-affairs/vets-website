18a0c056993fc26bf53b0f68a9e85aac778e089f
William Phelps <williamphelps13@gmail.com>
2024-05-06 15:39:59 -0600

    Remove routes

--- src/applications/accredited-representative-portal/routes.jsx
+++ src/applications/accredited-representative-portal/routes.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom-v5-compat';
 
 import App from './containers/App';
-import Dashboard from './containers/Dashboard';
 import LandingPage from './containers/LandingPage';
 import POARequestsPage from './containers/POARequestsPage';
-import PermissionsPage from './containers/PermissionsPage';
 import SignedInViewLayout from './containers/SignedInViewLayout';
 
 const routes = (
@@ -13,9 +11,7 @@ const routes = (
     <Route element={<App />}>
       <Route path="/" element={<LandingPage />} />
       <Route element={<SignedInViewLayout />}>
-        <Route path="dashboard" element={<Dashboard />} />
         <Route path="poa-requests" element={<POARequestsPage />} />
-        <Route path="permissions" element={<PermissionsPage />} />
       </Route>
     </Route>
   </Routes>
