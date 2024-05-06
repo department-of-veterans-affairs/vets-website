cba66e9aaf087314025b06a0b56c6f901438e018
Michael Clement <michael.clement@adhocteam.us>
2024-05-06 16:31:44 -0500

    added some title/depends testing

--- src/applications/ivc-champva/10-7959C/tests/config/form.unit.spec.jsx
+++ src/applications/ivc-champva/10-7959C/tests/config/form.unit.spec.jsx
@@ -1,7 +1,49 @@
-import { testNumberOfWebComponentFields } from '../../../shared/tests/pages/pageTests.spec';
+import { expect } from 'chai';
 
+import { testNumberOfWebComponentFields } from '../../../shared/tests/pages/pageTests.spec';
 import formConfig from '../../config/form';
 import mockData from '../fixtures/data/test-data.json';
+
+// Call the depends() function for any page that relies on it
+describe('dependent page logic', () => {
+  it('should be called', () => {
+    let depCount = 0;
+
+    Object.keys(formConfig.chapters).forEach(ch => {
+      Object.keys(formConfig.chapters[`${ch}`].pages).forEach(pg => {
+        const { depends } = formConfig.chapters[`${ch}`].pages[`${pg}`];
+        if (depends) {
+          depends({ data: '' });
+          depCount += 1;
+        }
+      });
+    });
+
+    expect(depCount > 0).to.be.true;
+  });
+});
+
+// Call the title fn for any page that has a computed title
+// NOTE: if mockData.data doesn't have all the properties accessed by your
+// titles, this test will fail and point you to the title with the property
+// that is missing.
+describe('title text logic', () => {
+  it('should be called', () => {
+    let titleCount = 0;
+
+    Object.keys(formConfig.chapters).forEach(ch => {
+      Object.keys(formConfig.chapters[`${ch}`].pages).forEach(pg => {
+        const { title } = formConfig.chapters[`${ch}`].pages[`${pg}`];
+        if (typeof title === 'function') {
+          title({ data: mockData.data });
+          titleCount += 1;
+        }
+      });
+    });
+
+    expect(titleCount > 0).to.be.true;
+  });
+});
 
 testNumberOfWebComponentFields(
   formConfig,
@@ -9,6 +51,15 @@ testNumberOfWebComponentFields(
   formConfig.chapters.certifierInformation.pages.role.uiSchema,
   1,
   'Certifier information',
+  { ...mockData.data },
+);
+
+testNumberOfWebComponentFields(
+  formConfig,
+  formConfig.chapters.certifierInformation.pages.name.schema,
+  formConfig.chapters.certifierInformation.pages.name.uiSchema,
+  4,
+  'Certifier name',
   { ...mockData.data },
 );
 
@@ -38,3 +89,39 @@ testNumberOfWebComponentFields(
   'Certifier relationship',
   { ...mockData.data },
 );
+
+testNumberOfWebComponentFields(
+  formConfig,
+  formConfig.chapters.certifierInformation.pages.relationship.schema,
+  formConfig.chapters.certifierInformation.pages.relationship.uiSchema,
+  2,
+  'Certifier relationship',
+  { ...mockData.data },
+);
+
+describe('Applicant Name/DOB page title', () => {
+  it('should start with "Your" if role is "applicant"', () => {
+    const {
+      title,
+    } = formConfig.chapters.applicantInformation.pages.applicantNameDob;
+    const res = title({ ...mockData.data, certifierRole: 'applicant' });
+    expect(res.startsWith('Your')).to.be.true;
+  });
+});
+
+describe('Medicare ineligibility screen depends function', () => {
+  // Get date 65 yrs ago in format MM-DD-YYYY (to match what form would produce)
+  const date65YrAgo = new Date();
+  date65YrAgo.setFullYear(date65YrAgo.getFullYear() - 65);
+
+  it('should return true if applicant is ineligible and over 65', () => {
+    const depRes = formConfig.chapters.medicareInformation.pages.medicareIneligible.depends(
+      {
+        applicantMedicareStatusContinued: 'ineligible',
+        applicantDOB: date65YrAgo,
+      },
+    );
+
+    expect(depRes).to.be.true;
+  });
+});
