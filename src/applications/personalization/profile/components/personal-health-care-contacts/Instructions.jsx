3d0a33b8ec60219c8e67712a2d04ed3e99c0ede4
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-05-07 05:50:45 -0400

    adjusting for tests

--- src/applications/personalization/profile/components/personal-health-care-contacts/Instructions.jsx
+++ src/applications/personalization/profile/components/personal-health-care-contacts/Instructions.jsx
@@ -8,7 +8,7 @@ const Instructions = ({ testId, contactType }) => {
     <div data-testid={testId}>
       To add {isEmergency ? 'an' : 'a'} {contactType}, call your VA health
       facility.{' '}
-      <href testId={testId} to="https://www.va.gov/find-locations/">
+      <href data-testid={testId} to="https://www.va.gov/find-locations/">
         Find your health facility’s phone number
       </href>
       .
