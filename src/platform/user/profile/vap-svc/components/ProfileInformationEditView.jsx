d065902b1b1e36f24a5b16b38822deb7606de67b
Adam Whitlock <adam.whitlock@adhocteam.us>
2024-05-07 11:14:49 -0600

    fix: cancel button width adjusted, use slim alert for success

--- src/platform/user/profile/vap-svc/components/ProfileInformationEditView.jsx
+++ src/platform/user/profile/vap-svc/components/ProfileInformationEditView.jsx
@@ -346,7 +346,7 @@ export class ProfileInformationEditView extends Component {
                     <va-button
                       data-testid="cancel-edit-button"
                       secondary
-                      class="vads-u-margin--0 vads-u-margin-top--0 vads-u-width--full"
+                      class="vads-u-margin--0 vads-u-margin-top--0 vads-u-width--full small-screen:vads-u-width--auto"
                       onClick={onCancel}
                       text={cancelButtonText || 'Cancel'}
                     />
