584ed50ed1177fd3932fa85b47de9263b6456690
John Woolschlager <wullaski@gmail.com>
2024-05-08 13:52:43 -0400

    Check in/71995/modify to use vaos data (#29585)
    * Add mock appointment
    
    * Set up mock response
    
    * Change endpoint for upcoming appointments and mocks to match api
    
    * Change hook to recieve api response object
    
    * Convert appointments to match vista appointment object so we can resuse existing shared components and utils
    
    * Modify getAppointmentId to use the VAOS appointment id if it exists and add findUpcomingAppointment method to display the correct VAOS appointment on the appointment details view
    
    * Fix test
    
    * Fix cy intercept
    
    * Skip test for now
    
    * Fix cypress test
    
    * Skipping test
    
    * Add tests and use locationId

--- src/applications/check-in/actions/universal/index.js
+++ src/applications/check-in/actions/universal/index.js
@@ -50,16 +50,3 @@ export const recievedUpcomingAppointments = payload => {
     },
   };
 };
-
-export const RECEIVED_UPCOMING_APPOINTMENTS = 'RECEIVED_UPCOMING_APPOINTMENTS';
-
-export const recievedUpcomingAppointments = payload => {
-  const data = { upcomingAppointments: [...payload] };
-
-  return {
-    type: RECEIVED_UPCOMING_APPOINTMENTS,
-    payload: {
-      ...data,
-    },
-  };
-};
