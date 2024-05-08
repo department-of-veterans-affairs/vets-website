38d7eda954de02d34f76aa9cdda8eee4e133dd58
OptionSelect <brandyn.sullins@gmail.com>
2024-05-08 15:01:16 -0400

    Implement expenses endpoints for streamlined determination.

--- src/applications/financial-status-report/utils/calculateExpenses.js
+++ src/applications/financial-status-report/utils/calculateExpenses.js
@@ -1,3 +1,6 @@
+import { apiRequest } from 'platform/utilities/api';
+import environment from '@department-of-veterans-affairs/platform-utilities/environment';
+import * as Sentry from '@sentry/browser';
 import { sumValues, safeNumber } from './helpers';
 
 // Constants representing the type of expense
@@ -145,4 +148,32 @@ export const getAllExpenses = formData => {
       ...creditCardBills,
     ],
   };
+};
+
+// /v0/calculate_monthly_expenses
+export const getMonthlyExpensesAPI = async formData => {
+  const body = JSON.stringify(formData);
+  try {
+    const url = `${
+      environment.API_URL
+    }/debts_api/v0/calculate_monthly_expenses`;
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Key-Inflection': 'camel',
+        'Source-App-Name': window.appName,
+      },
+      body,
+      mode: 'cors',
+    };
+
+    return await apiRequest(url, options);
+  } catch (error) {
+    Sentry.withScope(scope => {
+      scope.setExtra('error', error);
+      Sentry.captureMessage(`calculate_monthly_expenses failed: ${error}`);
+    });
+    return null;
+  }
 };
