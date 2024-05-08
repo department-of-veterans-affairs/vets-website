180b60058b0eda72a8acd63fd37dade55c013a40
Daniel Cloud <daniel.cloud@adhocteam.us>
2024-05-08 14:01:40 -0400

    The `item` objects are a collection of props, so we can expand it on the MhvSecondaryNavItem

--- src/platform/mhv/secondary-nav/components/MhvSecondaryNavMenu.jsx
+++ src/platform/mhv/secondary-nav/components/MhvSecondaryNavMenu.jsx
@@ -31,11 +31,7 @@ const MhvSecondaryNavMenu = ({ items }) => {
     const key = item.title.toLowerCase().replaceAll(' ', '_');
     return (
       <MhvSecondaryNavItem
-        title={item.title}
-        href={item.href}
-        icon={item.icon}
-        abbreviation={item.abbreviation}
-        actionName={item.actionName}
+        {...item}
         isActive={activeItem === item}
         key={key}
         isHeader={index === 0}
