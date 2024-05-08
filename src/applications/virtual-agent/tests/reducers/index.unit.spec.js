6b43944b59d1c96b80122767128ba2659a767042
Samreen Zarroug <samreen.zarroug@thoughtworks.com>
2024-05-08 16:55:42 -0500

    Add tests for reducers

---
+++ src/applications/virtual-agent/tests/reducers/index.unit.spec.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+
+import reducers, { ACCEPTED } from '../../reducers';
+
+describe('index', () => {
+  const state = { termsAccepted: false };
+
+  describe('reducer', () => {
+    it('should return termsAccepted as true when action type is ACCEPTED', () => {
+      const result = reducers.virtualAgentData(state, {
+        type: ACCEPTED,
+      });
+
+      expect(result).to.deep.equal({ termsAccepted: true });
+    });
+    it('should return the state when the action type is not ACCEPTED', () => {
+      const result = reducers.virtualAgentData(state, { type: 'OTHER' });
+
+      expect(result).to.deep.equal({ termsAccepted: false });
+    });
+  });
+});
