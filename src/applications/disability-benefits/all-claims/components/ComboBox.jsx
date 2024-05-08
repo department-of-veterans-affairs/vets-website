1b72d3cb77b15758db908e2fa660b13c090b5e70
Luke Short <lukeshortemail@gmail.com>
2024-05-08 09:16:08 -0700

    add shell for combobox for revisedAddDisabilities page

---
+++ src/applications/disability-benefits/all-claims/components/ComboBox.jsx
@@ -0,0 +1,45 @@
+// Description: ComboBox component for the disability benefits form.
+import { VaTextInput } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import React from 'react';
+
+export class ComboBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // bump: false,
+      // searchTerm: '',
+      // value: value || '',
+      input: '',
+    };
+    //     //this.inputRef = React.createRef();
+  }
+
+  handleSearchChange = e => {
+    // const { bump } = this.state;
+    // const updatedTerm = e.target.value;
+    const newTextValue = e.target.value;
+    this.setState({
+      // searchTerm: updatedTerm,
+      // bump: !bump,
+      input: newTextValue,
+    });
+    this.props.onChange(newTextValue);
+  };
+
+  render() {
+    // const { searchTerm } = this.state;
+    return (
+      <div className="autosuggest-container">
+        <VaTextInput
+          hint={null}
+          // label="What new condition do you want to claim?"
+          label=""
+          message-aria-describedby="What new condition do you want to claim?"
+          name="combobox-input"
+          onInput={this.handleSearchChange}
+          onChange={this.handleSearchChange}
+        />
+      </div>
+    );
+  }
+}
