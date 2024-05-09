825a796a2f93a96918042487d9a66db516df12b3
Rachel <rachele.eiting@copilot.llc>
2024-05-09 09:20:39 -0400

    changed key name

--- src/applications/ivc-champva/10-7959f-1/config/submitTransformer.js
+++ src/applications/ivc-champva/10-7959f-1/config/submitTransformer.js
@@ -30,7 +30,7 @@ export default function transformForSubmit(formConfig, form) {
     },
     // statement_of_truth_signature: transformedData.fullName,
     current_date: new Date().toJSON().slice(0, 10),
-    primaryCustomer: {
+    primaryContactInfo: {
       name: {
         first: transformedData.firstName,
         last: transformedData.lastName,
