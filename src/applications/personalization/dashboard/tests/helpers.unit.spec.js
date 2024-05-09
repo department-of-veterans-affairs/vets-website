92203df068bae45e5ac58bbd829c3811574a38a3
Allison Lu <allisonlu820@gmail.com>
2024-05-09 14:58:09 -0400

    pivoting direction - putting objects back that i previously removed - renamed functions using the new object with V2 for easy removal later\

--- src/applications/personalization/dashboard/tests/helpers.unit.spec.js
+++ src/applications/personalization/dashboard/tests/helpers.unit.spec.js
@@ -1,8 +1,20 @@
 import { expect } from 'chai';
 
-import { MY_VA_SIP_FORMS } from '~/platform/forms/constants';
+import {
+  FORM_DESCRIPTIONS,
+  SIP_ENABLED_FORMS,
+  MY_VA_SIP_FORMS,
+} from '~/platform/forms/constants';
 
 describe('profile helpers:', () => {
+  describe('FORM_DESCRIPTIONS', () => {
+    it('should have description information for each verified form', () => {
+      SIP_ENABLED_FORMS.forEach(form => {
+        expect(FORM_DESCRIPTIONS[form]).to.exist;
+      });
+    });
+  });
+
   describe('MY_VA_SIP_FORMS', () => {
     it('should have description information for each verified form', () => {
       MY_VA_SIP_FORMS.forEach(form => {
