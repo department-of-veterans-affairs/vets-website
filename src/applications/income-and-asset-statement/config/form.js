d751bda3e340f5a42b5ff8fb3f42e22eb810377d
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2024-05-09 11:27:25 -0600

    Generate new form

---
+++ src/applications/income-and-asset-statement/config/form.js
@@ -0,0 +1,55 @@
+// import fullSchema from 'vets-json-schema/dist/21P-0969-schema.json';
+
+import manifest from '../manifest.json';
+
+import IntroductionPage from '../containers/IntroductionPage';
+import ConfirmationPage from '../containers/ConfirmationPage';
+
+// const { } = fullSchema.properties;
+
+// const { } = fullSchema.definitions;
+
+const formConfig = {
+  rootUrl: manifest.rootUrl,
+  urlPrefix: '/',
+  // submitUrl: '/v0/api',
+  submit: () =>
+    Promise.resolve({ attributes: { confirmationNumber: '123123123' } }),
+  trackingPrefix: 'income-and-asset-statement-',
+  introduction: IntroductionPage,
+  confirmation: ConfirmationPage,
+  formId: '21P-0969',
+  saveInProgress: {
+    // messages: {
+    //   inProgress: 'Your benefits application (21P-0969) is in progress.',
+    //   expired: 'Your saved benefits application (21P-0969) has expired. If you want to apply for benefits, please start a new application.',
+    //   saved: 'Your benefits application has been saved.',
+    // },
+  },
+  version: 0,
+  prefillEnabled: true,
+  savedFormMessages: {
+    notFound: 'Please start over to apply for benefits.',
+    noAuth: 'Please sign in again to continue your application for benefits.',
+  },
+  title: '21P-0969 Income and Asset Statement Form',
+  defaultDefinitions: {},
+  chapters: {
+    chapter1: {
+      title: 'Chapter 1',
+      pages: {
+        page1: {
+          path: 'first-page',
+          title: 'First Page',
+          uiSchema: {},
+          schema: {
+            type: 'object',
+            properties: {},
+          },
+        },
+      },
+    },
+  },
+};
+
+export default formConfig;
