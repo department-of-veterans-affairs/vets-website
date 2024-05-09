5664f0cc184bfa889c697541ec180f5eb0b7953b
Michael Clement <michael.clement@adhocteam.us>
2024-05-09 14:05:18 -0500

    added e2e test

--- src/applications/ivc-champva/10-7959C/tests/10-7959C.cypress.spec.js
+++ src/applications/ivc-champva/10-7959C/tests/10-7959C.cypress.spec.js
@@ -6,11 +6,15 @@ import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-test
 import formConfig from '../config/form';
 import manifest from '../manifest.json';
 
+import {
+  reviewAndSubmitPageFlow,
+  fillAddressWebComponentPattern,
+} from '../../shared/tests/helpers';
+
 const testConfig = createTestConfig(
   {
     dataPrefix: 'data',
-
-    dataDir: path.join(__dirname, 'data'),
+    dataDir: path.join(__dirname, 'e2e', 'fixtures', 'data'),
 
     // Rename and modify the test data as needed.
     dataSets: ['test-data'],
@@ -18,20 +22,62 @@ const testConfig = createTestConfig(
     pageHooks: {
       introduction: ({ afterHook }) => {
         afterHook(() => {
-          cy.findAllByText(/start/i, { selector: 'button' })
-            .last()
+          cy.findAllByText(/start/i, { selector: 'a' })
+            .first()
             .click();
         });
       },
+      'your-information/address': ({ afterHook }) => {
+        cy.injectAxeThenAxeCheck();
+        afterHook(() => {
+          cy.get('@testData').then(data => {
+            fillAddressWebComponentPattern(
+              'certifierAddress',
+              data.certifierAddress,
+            );
+            cy.axeCheck();
+            cy.findByText(/continue/i, { selector: 'button' }).click();
+          });
+        });
+      },
+      'applicant-information/address': ({ afterHook }) => {
+        cy.injectAxeThenAxeCheck();
+        afterHook(() => {
+          cy.get('@testData').then(data => {
+            fillAddressWebComponentPattern(
+              'applicantAddress',
+              data.applicantAddress,
+            );
+            cy.axeCheck();
+            cy.findByText(/continue/i, { selector: 'button' }).click();
+          });
+        });
+      },
+      'consent-mail': ({ afterHook }) => {
+        cy.injectAxeThenAxeCheck();
+        afterHook(() => {
+          cy.get('@testData').then(data => {
+            cy.selectVaCheckbox(
+              `consent-checkbox`,
+              data.consentToMailMissingRequiredFiles,
+            );
+            cy.axeCheck();
+            cy.findByText(/continue/i, { selector: 'button' }).click();
+          });
+        });
+      },
+      'review-and-submit': ({ afterHook }) => {
+        afterHook(() => {
+          cy.get('@testData').then(data => {
+            reviewAndSubmitPageFlow(data.certifierName);
+          });
+        });
+      },
     },
-
     setupPerTest: () => {
-      // Log in if the form requires an authenticated session.
-      // cy.login();
-
-      cy.route('POST', formConfig.submitUrl, { status: 200 });
+      cy.intercept('POST', formConfig.submitUrl, { status: 200 });
+      cy.config('includeShadowDom', true);
     },
-
     // Skip tests in CI until the form is released.
     // Remove this setting when the form has a content page in production.
     skip: Cypress.env('CI'),
