08e30dee887b9b06fd9a0b2dc4644d04bcf477e5
Lee Delarm <lee.delarm@agile6.com>
2024-05-10 14:04:50 -0400

    Update css

--- src/applications/vaos/referral-appointments/ReviewApproved.jsx
+++ src/applications/vaos/referral-appointments/ReviewApproved.jsx
@@ -5,24 +5,31 @@ import ReferralAppLink from './components/ReferralAppLink';
 export default function ReviewApproved() {
   return (
     <PageLayout showBreadcrumbs showNeedHelp>
+      <div className="title-text-eyebrow">New Appointment</div>
       <h1>Review your approved referral and start scheduling an appointment</h1>
-      <p data-testid="subtitle" className="vads-u-font-family--serif">
-        Your community care referral has been approved and can now be schedule.
+      <p data-testid="subtitle">
+        Your community care referral has been approved and can now be scheduled.
       </p>
+      <hr className="vads-u-margin-y--2" />
       <div className="vads-u-font-weight--bold">What</div>
       <div>Family Medicine</div>
+      <hr className="vads-u-margin-y--2" />
       <div className="vads-u-font-weight--bold">Preferred Facility</div>
       <div>none</div>
+      <hr className="vads-u-margin-y--2" />
       <div className="vads-u-font-weight--bold">Preferred location</div>
       <div>none</div>
+      <hr className="vads-u-margin-y--2" />
       <div className="vads-u-font-weight--bold">Preferred provider</div>
       <div>none</div>
+      <hr className="vads-u-margin-y--2" />
       <div className="vads-u-font-weight--bold">
         Details you shared with your provider
       </div>
       <div>Back hurts</div>
-      <div className="vads-u-font-weight--bold">
-        Details you shared with your provider
+      <hr className="vads-u-margin-y--2" />
+      <div className="vads-u-font-weight--bold vads-u-margin-bottom--2">
+        Details about your referral
       </div>
       <div>
         <span className="vads-u-font-weight--bold vads-u-font-size--h5 vads-u-margin-bottom--2">
@@ -54,9 +61,10 @@ export default function ReviewApproved() {
         </span>{' '}
         None
       </div>
+      <hr className="vads-u-margin-y--2" />
       <div>
         <ReferralAppLink
-          linkText="Schedule community care appointment"
+          linkText="Start scheduling your community care appointment"
           linkPath="/choose-community-care-appointment"
         />
       </div>
