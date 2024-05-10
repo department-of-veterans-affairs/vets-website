271252478966d8a834ed4c7b0451e8d5749d6f79
Luke Short <lukeshortemail@gmail.com>
2024-05-10 12:57:51 -0700

    add comments to ComboBox component

--- src/applications/disability-benefits/all-claims/components/ComboBox.jsx
+++ src/applications/disability-benefits/all-claims/components/ComboBox.jsx
@@ -1,7 +1,11 @@
-// Description: ComboBox component for the disability benefits form.
 import { VaTextInput } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import React from 'react';
 
+// This is a combobox component that is used in the revisedAddDisabilities page.
+// Originally, the addDisabilities page used the AutosuggestField component from the platform-forms-system package.
+// A new component was created to make suggestions to the veteran more understandable when selecting a new condition to claim.
+// Search functions for use with this component are located in:
+// src/platform/forms-system/src/js/utilities/addDisabilitiesStringSearch.js
 export class ComboBox extends React.Component {
   constructor(props) {
     super(props);
