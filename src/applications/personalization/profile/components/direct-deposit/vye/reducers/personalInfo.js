1213d111f324413bc2b3e2d01d448aade1d79dff
Wafi Mohamed <wafi.mohamed@va.gov>
2024-05-13 13:37:54 -0400

    re-connecting front-end with the backend

--- src/applications/personalization/profile/components/direct-deposit/vye/reducers/personalInfo.js
+++ src/applications/personalization/profile/components/direct-deposit/vye/reducers/personalInfo.js
@@ -2,8 +2,6 @@ import {
   FETCH_PERSONAL_INFO,
   FETCH_PERSONAL_INFO_FAILED,
   FETCH_PERSONAL_INFO_SUCCESS,
-  UPDATE_PENDING_VERIFICATIONS,
-  UPDATE_VERIFICATIONS,
 } from '../actions';
 
 const initialState = {
@@ -33,19 +31,6 @@ const personalInfo = (state = initialState, action) => {
         personalInfo: action.response,
         isLoading: false,
       };
-    case UPDATE_PENDING_VERIFICATIONS:
-      return {
-        ...state,
-        personalInfo: {
-          ...(state.personalInfo || {}),
-          'vye::UserInfo': {
-            ...((state.personalInfo && state.personalInfo['vye::UserInfo']) ||
-              defaultUserInfo['vye::UserInfo']),
-            pendingVerifications: action.payload,
-          },
-        },
-      };
-    case UPDATE_VERIFICATIONS:
       if (!state.personalInfo) {
         return {
           ...state,
