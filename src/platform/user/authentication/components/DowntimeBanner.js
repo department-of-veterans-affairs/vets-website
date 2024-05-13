be3d40793ab14c5b893411f3f2e4cc6d2de91eef
Caitlin <78328496+CaitHawk@users.noreply.github.com>
2024-05-13 08:32:34 -0700

    fixes linting issue

--- src/platform/user/authentication/components/DowntimeBanner.js
+++ src/platform/user/authentication/components/DowntimeBanner.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import React, { useEffect, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getBackendStatuses as getBackendStatusAction } from 'platform/monitoring/external-services/actions';
@@ -23,7 +22,8 @@ export default function DowntimeBanners() {
   }, []); // only on load
 
   // mimics the mvi service error if we don't get an OK response from vets-api
-  const isApiDown = () => !isLocalhost && !loading && (!statuses || statuses?.length === 0);
+  const isApiDown = () =>
+    !isLocalhost && !loading && (!statuses || statuses?.length === 0);
 
   return (
     <div className="downtime-notification row">
