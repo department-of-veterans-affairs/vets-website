a4a6a60a51637c34a7464493315db5e802810235
Richard Davis <richard.davis@agile6.com>
2024-05-13 14:40:08 -0400

    Update feature toggles to reflect production values

--- src/applications/mhv-landing-page/mocks/api/index.js
+++ src/applications/mhv-landing-page/mocks/api/index.js
@@ -20,7 +20,7 @@ const responses = {
     mhvLandingPagePersonalization: false,
     mhvLandingPageEnableVaGovHealthToolsLinks: false,
     mhvSecondaryNavigationEnabled: false,
-    mhvTransitionalMedicalRecordsLandingPage: true,
+    mhvTransitionalMedicalRecordsLandingPage: false,
   }),
   'GET /my_health/v1/messaging/folders': folders.allFoldersWithUnreadMessages,
   'GET /v0/profile/personal_information': personalInformation,
