5a8e280f4645e5eb09f418b1e53f3ac48597097b
Jacob Penner <jacob.penner@adhocteam.us>
2024-05-14 11:25:26 -0400

    update logic to correctly check against checkbox values

--- src/applications/simple-forms/21-4138/config/constants.js
+++ src/applications/simple-forms/21-4138/config/constants.js
@@ -56,6 +56,48 @@ export const DECISION_REVIEW_TYPE_DESCRIPTIONS = Object.freeze({
     'You can also submit new evidence with certain types of Board Appeals.',
 });
 
+export const LIVING_SITUATIONS = Object.freeze({
+  OVERNIGHT:
+    'I live or sleep overnight in a place that isn’t meant for regular sleeping. This includes a car, park, abandoned building, bus station, train station, airport, or camping ground.',
+  SHELTER:
+    'I live in a shelter (including a hotel or motel) that’s meant for temporary stays.',
+  FRIEND_OR_FAMILY:
+    'I’m staying with a friend or family member, because I can’t get my own home right now.',
+  LEAVING_SHELTER:
+    'In the next 30 days, I will have to leave a facility, like a homeless shelter.',
+  LOSING_HOME:
+    'In the next 30 days, I will lose my home. (Note: This could include a house, apartment, trailer, or other living space that you own, rent, or live in without paying rent. Or it could include a living space that you share with others. It could also include rooms in hotels or motels.)',
+  OTHER_RISK: 'I have another housing risk not listed here.',
+  NONE: 'None of these situations apply to me.',
+});
+
+export const OTHER_REASONS_REQUIRED = Object.freeze({
+  FINANCIAL_HARDSHIP:
+    'I’m experiencing extreme financial hardship (such as loss of your job or sudden decrease in income).',
+  ALS:
+    'I have ALS (amyotrophic lateral sclerosis), also known as Lou Gehrig’s disease.',
+  TERMINAL_ILLNESS: 'I have a terminal illness.',
+  VSI_SI:
+    'I have a status from the Defense Department of Very Seriously Injured or Ill (VSI) or Seriously Injured or Ill (SI).',
+  OVER_85: 'I’m age 85 or older.',
+  FORMER_POW: 'I’m a former prisoner of war (POW).',
+  MEDAL_AWARD: 'I’m a Medal of Honor or Purple Heart award recipient.',
+  NONE: 'None of these situations apply to me.',
+});
+
+export const OTHER_REASONS_OPTIONAL = Object.freeze({
+  FINANCIAL_HARDSHIP:
+    'I’m experiencing extreme financial hardship (such as loss of your job or sudden decrease in income).',
+  ALS:
+    'I have ALS (amyotrophic lateral sclerosis), also known as Lou Gehrig’s disease.',
+  TERMINAL_ILLNESS: 'I have a terminal illness.',
+  VSI_SI:
+    'I have a status from the Defense Department of Very Seriously Injured or Ill (VSI) or Seriously Injured or Ill (SI).',
+  OVER_85: 'I’m age 85 or older.',
+  FORMER_POW: 'I’m a former prisoner of war (POW).',
+  MEDAL_AWARD: 'I’m a Medal of Honor or Purple Heart award recipient.',
+});
+
 const PrimaryActionLink = ({ href, children }) => (
   <div className="arrow" style={{ maxWidth: '75%' }}>
     <div className="vads-u-background-color--primary vads-u-padding--1">
@@ -254,21 +296,6 @@ export const NOD_BA_HANDOFF = Object.freeze(
   </div>,
 );
 
-export const LIVING_SITUATIONS = Object.freeze({
-  OVERNIGHT:
-    'I live or sleep overnight in a place that isn’t meant for regular sleeping. This includes a car, park, abandoned building, bus station, train station, airport, or camping ground.',
-  SHELTER:
-    'I live in a shelter (including a hotel or motel) that’s meant for temporary stays.',
-  FRIEND_OR_FAMILY:
-    'I’m staying with a friend or family member, because I can’t get my own home right now.',
-  LEAVING_SHELTER:
-    'In the next 30 days, I will have to leave a facility, like a homeless shelter.',
-  LOSING_HOME:
-    'In the next 30 days, I will lose my home. (Note: This could include a house, apartment, trailer, or other living space that you own, rent, or live in without paying rent. Or it could include a living space that you share with others. It could also include rooms in hotels or motels.)',
-  OTHER_RISK: 'I have another housing risk not listed here.',
-  NONE: 'None of these situations apply to me.',
-});
-
 export const ADDITIONAL_INFO_OTHER_HOUSING_RISKS = Object.freeze(
   <va-additional-info
     trigger="What to know before sharing details about other housing risks"
@@ -291,33 +318,6 @@ export const ADDITIONAL_INFO_OTHER_HOUSING_RISKS = Object.freeze(
     </div>
   </va-additional-info>,
 );
-
-export const OTHER_REASONS_REQUIRED = Object.freeze({
-  FINANCIAL_HARDSHIP:
-    'I’m experiencing extreme financial hardship (such as loss of your job or sudden decrease in income).',
-  ALS:
-    'I have ALS (amyotrophic lateral sclerosis), also known as Lou Gehrig’s disease.',
-  TERMINAL_ILLNESS: 'I have a terminal illness.',
-  VSI_SI:
-    'I have a status from the Defense Department of Very Seriously Injured or Ill (VSI) or Seriously Injured or Ill (SI).',
-  OVER_85: 'I’m age 85 or older.',
-  FORMER_POW: 'I’m a former prisoner of war (POW).',
-  MEDAL_AWARD: 'I’m a Medal of Honor or Purple Heart award recipient.',
-  NONE: 'None of these situations apply to me.',
-});
-
-export const OTHER_REASONS_OPTIONAL = Object.freeze({
-  FINANCIAL_HARDSHIP:
-    'I’m experiencing extreme financial hardship (such as loss of your job or sudden decrease in income).',
-  ALS:
-    'I have ALS (amyotrophic lateral sclerosis), also known as Lou Gehrig’s disease.',
-  TERMINAL_ILLNESS: 'I have a terminal illness.',
-  VSI_SI:
-    'I have a status from the Defense Department of Very Seriously Injured or Ill (VSI) or Seriously Injured or Ill (SI).',
-  OVER_85: 'I’m age 85 or older.',
-  FORMER_POW: 'I’m a former prisoner of war (POW).',
-  MEDAL_AWARD: 'I’m a Medal of Honor or Purple Heart award recipient.',
-});
 
 export const PRIORITY_PROCESSING_NOT_QUALIFIED = Object.freeze(
   <div>
