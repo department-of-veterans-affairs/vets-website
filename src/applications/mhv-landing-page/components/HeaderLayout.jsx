e1eafcfae61c4a485216ffef37be96227e9364a3
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-05-14 18:16:44 -0400

    add Message Try-me phase on landing page

--- src/applications/mhv-landing-page/components/HeaderLayout.jsx
+++ src/applications/mhv-landing-page/components/HeaderLayout.jsx
@@ -11,18 +11,59 @@ const HeaderLayout = ({ showWelcomeMessage = false }) => {
           'vads-u-display--flex',
           'vads-u-justify-content--space-between',
           'vads-u-margin-bottom--2',
-          'vads-u-align-items--center',
+          'vads-u-align-items--flex-start',
         )}
       >
         <div className="vads-l-col medium-screen:vads-l-col--8">
           <h1>My HealtheVet</h1>
           <div className="va-introtext">
             <p>
-              <a href="/resources/my-healthevet-on-vagov-what-to-know">
-                Learn more about My HealtheVet on VA.gov,
-              </a>
-              &nbsp;where you can manage your VA health care and your health.
+              Welcome to the new home for My HealtheVet. Now you can manage your
+              health care needs in the same place where you manage your other VA
+              benefits and services—right here on VA.gov.
             </p>
+            <p>
+              If you’re not ready to try the new My HealtheVet, you can use the
+              previous version anytime.{' '}
+              <a href="https://www.myhealth.va.gov/mhv-portal-web/home">
+                Go back to the previous version of My HealtheVet
+              </a>
+            </p>
+          </div>
+          <div>
+            <va-alert-expandable
+              status="info"
+              trigger="Learn more about My HealtheVet on VA.gov "
+            >
+              <div>
+                <p>
+                  <strong>What you can do now on VA.gov:</strong>
+                </p>
+                <ul>
+                  <li>Schedule, cancel, and manage some health appointments</li>
+                  <li>Send secure messages to your health care team</li>
+                  <li>
+                    Refill your prescriptions and get a list of all your
+                    medications
+                  </li>
+                </ul>
+                <p>
+                  <strong>What’s coming soon:</strong>
+                </p>
+                <ul>
+                  <li>Find, print, and download your medical records</li>
+                  <li>Get your lab and test results</li>
+                </ul>
+                <p>
+                  We’re working to bring your medical records to VA.gov. For
+                  now, you can download your records using the previous version
+                  of My HealtheVet.{' '}
+                  <a href="https://www.myhealth.va.gov/mhv-portal-web/home">
+                    Go back to the previous version of My HealtheVet
+                  </a>
+                </p>
+              </div>
+            </va-alert-expandable>
           </div>
         </div>
         <div
@@ -31,6 +72,7 @@ const HeaderLayout = ({ showWelcomeMessage = false }) => {
             'medium-screen:vads-u-display--block',
             'vads-l-col--4',
             'vads-u-text-align--right',
+            'vads-u-margin-y--2p5',
           )}
         >
           <img
