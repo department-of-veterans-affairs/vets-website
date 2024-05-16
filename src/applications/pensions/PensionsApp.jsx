4b7bb57bc756e81e793fc45d93530a727f48accc
Tai Wilkin <tai.wilkin@coforma.io>
2024-05-16 10:18:29 -0400

    Move multiresponse styles to sass, stop using sessionStorage

--- src/applications/pensions/PensionsApp.jsx
+++ src/applications/pensions/PensionsApp.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -6,21 +6,10 @@ import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import { useFeatureToggle } from 'platform/utilities/feature-toggles';
 import formConfig from './config/form';
 import { NoFormPage } from './components/NoFormPage';
-import {
-  addMultiresponseStyles,
-  removeMultiresponseStyles,
-} from './multiresponseStyles';
-import {
-  removeSessionMultiresponseStyles,
-  setSessionMultiresponseStyles,
-} from './helpers';
 
 export default function PensionEntry({ location, children }) {
   const { useToggleValue, TOGGLE_NAMES } = useFeatureToggle();
   const pensionFormEnabled = useToggleValue(TOGGLE_NAMES.pensionFormEnabled);
-  const pensionMultiresponseStyles = useToggleValue(
-    TOGGLE_NAMES.pensionMultiresponseStyles,
-  );
   const isLoadingFeatures = useSelector(
     state => state?.featureToggles?.loading,
   );
@@ -30,23 +19,6 @@ export default function PensionEntry({ location, children }) {
   if (redirectToHowToPage === true) {
     window.location.href = '/pension/survivors-pension/';
   }
-
-  useEffect(
-    () => {
-      if (isLoadingFeatures === false) {
-        if (pensionMultiresponseStyles) {
-          // TODO: Move styles to pensions.scss when we remove the flipper
-          addMultiresponseStyles();
-          // TODO: Remove sessionStorage calls when removing the flipper
-          setSessionMultiresponseStyles();
-        } else {
-          removeMultiresponseStyles();
-          removeSessionMultiresponseStyles();
-        }
-      }
-    },
-    [isLoadingFeatures, pensionMultiresponseStyles],
-  );
 
   if (isLoadingFeatures !== false || redirectToHowToPage) {
     return <va-loading-indicator message="Loading application..." />;
