42b736851d5219f52940ca908ddadc4911a777c0
Tyler Spangler <tylerspangler@Tyler-Spangler.local>
2024-05-17 12:39:12 -0600

    removed class for headings

--- src/applications/disability-benefits/all-claims/content/addDisabilitiesRevised.jsx
+++ src/applications/disability-benefits/all-claims/content/addDisabilitiesRevised.jsx
@@ -3,21 +3,17 @@ import { Link } from 'react-router';
 
 export const addDisabilitiesInstructions = (
   <>
-    <h2 className="schemaform-block-title">
-      Tell us the new conditions you want to claim
-    </h2>
+    <h2>Tell us the new conditions you want to claim</h2>
     <p>
       Enter the name of your condition. Then, select your condition from the
       list of possible matches.
     </p>
-    <h3 className="schemaform-block-subtitle">
-      What if my condition isn’t listed?
-    </h3>
+    <h3>What if my condition isn’t listed?</h3>
     <p>
       You can claim a condition that isn’t listed. Enter your condition,
       diagnosis, or short description of you symptoms.
     </p>
-    <h3 className="schemaform-block-subtitle">Examples of conditions</h3>
+    <h3>Examples of conditions</h3>
     <ul className="usa-list">
       <li>Tinnitus (ringing or hissing in ears)</li>
       <li>PTSD (post-traumatic stress disorder), combat-related</li>
