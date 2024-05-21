78d9682272ecc396c83a2b197a3a8db6040b11da
Adrian Rollett <acrollet@gmail.com>
2024-05-21 22:54:25 +0000

    Account for missing addresses in prefill

--- src/applications/health-care-supply-reordering/config/prefill-transformer.js
+++ src/applications/health-care-supply-reordering/config/prefill-transformer.js
@@ -2,14 +2,21 @@ import { nameToValue } from '../utils/country-mapping';
 
 export default function prefillTransformer(pages, formData, metadata) {
   const newFormData = formData;
-  newFormData.permanentAddress.country = nameToValue(
-    formData.permanentAddress.country,
-  );
-  newFormData.temporaryAddress.country =
-    nameToValue(formData.temporaryAddress.country) ?? 'USA';
-  newFormData.temporaryAddress.city = formData.temporaryAddress.city ?? '';
-  newFormData.temporaryAddress.state = formData.temporaryAddress.state ?? '';
-  newFormData.temporaryAddress.street = formData.temporaryAddress.street ?? '';
+  if (newFormData.permanentAddress?.country) {
+    newFormData.permanentAddress.country = nameToValue(
+      formData.permanentAddress.country,
+    );
+  }
+  if (newFormData.temporaryAddress?.country) {
+    newFormData.temporaryAddress.country =
+      nameToValue(formData.temporaryAddress.country) ?? 'USA';
+  }
+  if (newFormData.temporaryAddress) {
+    newFormData.temporaryAddress.city = formData.temporaryAddress.city ?? '';
+    newFormData.temporaryAddress.state = formData.temporaryAddress.state ?? '';
+    newFormData.temporaryAddress.street =
+      formData.temporaryAddress.street ?? '';
+  }
   return {
     pages,
     formData: newFormData,
