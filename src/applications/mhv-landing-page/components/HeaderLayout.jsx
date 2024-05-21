d18f80bd380289c59a186b2dac23e87e4e06cd2b
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-05-21 19:24:24 -0400

    add New label and put content behind feature flag

--- src/applications/mhv-landing-page/components/HeaderLayout.jsx
+++ src/applications/mhv-landing-page/components/HeaderLayout.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
+import { useFeatureToggle } from '~/platform/utilities/feature-toggles';
 import WelcomeContainer from '../containers/WelcomeContainer';
 
 const HeaderLayout = ({ showWelcomeMessage = false }) => {
+  const { useToggleValue, TOGGLE_NAMES } = useFeatureToggle();
+  // toggle name aka feature flag name is mhvLandingPageEnableVaGovHealthToolsLinks
+  const showHealthToolsLinks = useToggleValue(
+    TOGGLE_NAMES.mhvLandingPageEnableVaGovHealthToolsLinks,
+  );
+
   return (
     <>
       <div
@@ -15,55 +22,81 @@ const HeaderLayout = ({ showWelcomeMessage = false }) => {
         )}
       >
         <div className="vads-l-col medium-screen:vads-l-col--8">
-          <h1>My HealtheVet</h1>
-          <div className="va-introtext">
-            <p>
-              Welcome to the new home for My HealtheVet. Now you can manage your
-              health care needs in the same place where you manage your other VA
-              benefits and services—right here on VA.gov.
-            </p>
-            <p>
-              If you’re not ready to try the new My HealtheVet, you can use the
-              previous version anytime.{' '}
-              <a href="https://www.myhealth.va.gov/mhv-portal-web/home">
-                Go back to the previous version of My HealtheVet
-              </a>
-            </p>
+          <div className="vads-l-col">
+            <div className="vads-l-row">
+              <div className="vads-l-col-6">
+                <h1>My HealtheVet</h1>
+              </div>
+              <div className="vads-l-col-2 vads-u-margin-left--2 vads-u-margin-top--2">
+                {showHealthToolsLinks && (
+                  <span className="usa-label vads-u-background-color--cool-blue">
+                    New
+                  </span>
+                )}
+              </div>
+            </div>
           </div>
-          <div>
-            <va-alert-expandable
-              status="info"
-              trigger="Learn more about My HealtheVet on VA.gov "
-            >
-              <div>
+          <div className="va-introtext">
+            {showHealthToolsLinks ? (
+              <>
                 <p>
-                  <strong>What you can do now on VA.gov:</strong>
+                  Welcome to the new home for My HealtheVet. Now you can manage
+                  your health care needs in the same place where you manage your
+                  other VA benefits and services—right here on VA.gov.
                 </p>
-                <ul>
-                  <li>Schedule, cancel, and manage some health appointments</li>
-                  <li>Send secure messages to your health care team</li>
-                  <li>
-                    Refill your prescriptions and get a list of all your
-                    medications
-                  </li>
-                </ul>
                 <p>
-                  <strong>What’s coming soon:</strong>
-                </p>
-                <ul>
-                  <li>Find, print, and download your medical records</li>
-                  <li>Get your lab and test results</li>
-                </ul>
-                <p>
-                  We’re working to bring your medical records to VA.gov. For
-                  now, you can download your records using the previous version
-                  of My HealtheVet.{' '}
+                  If you’re not ready to try the new My HealtheVet, you can use
+                  the previous version anytime.{' '}
                   <a href="https://www.myhealth.va.gov/mhv-portal-web/home">
                     Go back to the previous version of My HealtheVet
                   </a>
                 </p>
-              </div>
-            </va-alert-expandable>
+                <div>
+                  <va-alert-expandable
+                    status="info"
+                    trigger="Learn more about My HealtheVet on VA.gov "
+                  >
+                    <div>
+                      <p>
+                        <strong>What you can do now on VA.gov:</strong>
+                      </p>
+                      <ul>
+                        <li>
+                          Schedule, cancel, and manage some health appointments
+                        </li>
+                        <li>Send secure messages to your health care team</li>
+                        <li>
+                          Refill your prescriptions and get a list of all your
+                          medications
+                        </li>
+                      </ul>
+                      <p>
+                        <strong>What’s coming soon:</strong>
+                      </p>
+                      <ul>
+                        <li>Find, print, and download your medical records</li>
+                        <li>Get your lab and test results</li>
+                      </ul>
+                      <p>
+                        We’re working to bring your medical records to VA.gov.
+                        For now, you can download your records using the
+                        previous version of My HealtheVet.{' '}
+                        <a href="https://www.myhealth.va.gov/mhv-portal-web/home">
+                          Go back to the previous version of My HealtheVet
+                        </a>
+                      </p>
+                    </div>
+                  </va-alert-expandable>
+                </div>
+              </>
+            ) : (
+              <p>
+                <a href="/resources/my-healthevet-on-vagov-what-to-know">
+                  Learn more about My HealtheVet on VA.gov,
+                </a>
+                &nbsp;where you can manage your VA health care and your health.
+              </p>
+            )}
           </div>
         </div>
         <div
