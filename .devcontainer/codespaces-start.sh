e0b51cccdcb2eabdd570883afacd1694ea5e4711
Alex Morgun <87077843+oleksii-morgun@users.noreply.github.com>
2024-05-22 17:07:50 +0000

    api config parameter

--- .devcontainer/codespaces-start.sh
+++ .devcontainer/codespaces-start.sh
@@ -7,4 +7,4 @@ yarn mock-api --responses src/applications/mhv-secure-messaging/api/mocks &
 
 # Start vets-website
 printf "\n\n##### Starting vets-website #####\n"
-cd ../vets-website && yarn watch --env entry=mhv-secure-messaging
+cd ../vets-website && yarn watch --env entry=mhv-secure-messaging api=https://${CODESPACE_NAME}-3000.app.github.dev public=https://${CODESPACE_NAME}-3001.app.github.dev
