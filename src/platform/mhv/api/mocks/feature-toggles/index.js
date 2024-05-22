27098e4b5c5880da3aca32d0f3fc143adbbb04e7
Alex Morgun <87077843+oleksii-morgun@users.noreply.github.com>
2024-05-22 20:56:50 +0000

    consolidating mocks

---
+++ src/platform/mhv/api/mocks/feature-toggles/index.js
@@ -0,0 +1,39 @@
+const generateFeatureToggles = (toggles = {}) => {
+  const {
+    mhvLandingPageEnabled = true,
+    mhvLandingPagePersonalization = true,
+    mhvMedicationsToVaGovRelease = true,
+    mhvMedicationsDisplayRefillContent = true,
+    mhvSecureMessagingToVAGovRelease = true,
+  } = toggles;
+
+  return {
+    data: {
+      type: 'feature_toggles',
+      features: [
+        {
+          name: 'mhv_landing_page_enabled',
+          value: mhvLandingPageEnabled,
+        },
+        {
+          name: 'mhv_landing_page_personalization',
+          value: mhvLandingPagePersonalization,
+        },
+        {
+          name: 'mhv_medications_to_va_gov_release',
+          value: mhvMedicationsToVaGovRelease,
+        },
+        {
+          name: 'mhv_medications_display_refill_content',
+          value: mhvMedicationsDisplayRefillContent,
+        },
+        {
+          name: 'mhv_secure_messaging_to_va_gov_release',
+          value: mhvSecureMessagingToVAGovRelease,
+        },
+      ],
+    },
+  };
+};
+
+module.exports = { generateFeatureToggles };
