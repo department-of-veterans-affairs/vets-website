6a507c41dddd8f6c45f9b6cb9f119a7012f4cf72
Alex Morgun <87077843+oleksii-morgun@users.noreply.github.com>
2024-05-22 20:57:15 +0000

    change mocks destination path

--- .devcontainer/codespaces-start.sh
+++ .devcontainer/codespaces-start.sh
@@ -2,9 +2,9 @@
 
 # Start mock services
 printf "\n\n##### Starting mock services #####\n"
-yarn mock-api --responses src/applications/mhv-secure-messaging/api/mocks &
+yarn mock-api --responses src/platform/mhv/api/mocks &
 
 
 # Start vets-website
 printf "\n\n##### Starting vets-website #####\n"
-cd ../vets-website && yarn watch --env entry=mhv-secure-messaging api=https://${CODESPACE_NAME}-3000.app.github.dev public=https://${CODESPACE_NAME}-3001.app.github.dev
+cd ../vets-website && yarn watch --env entry=mhv-secure-messaging,medications api=https://${CODESPACE_NAME}-3000.app.github.dev public=https://${CODESPACE_NAME}-3001.app.github.dev
