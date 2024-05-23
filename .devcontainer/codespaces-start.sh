be62f9b98b7c91462ce2d79312418935bb571f36
Alex Morgun <oleksii.morgun@va.gov>
2024-05-23 14:53:00 -0400

    exclude content-build, yarn watch entire vets-website

--- .devcontainer/codespaces-start.sh
+++ .devcontainer/codespaces-start.sh
@@ -7,4 +7,4 @@ yarn mock-api --responses src/platform/mhv/api/mocks &
 
 # Start vets-website
 printf "\n\n##### Starting vets-website #####\n"
-cd ../vets-website && yarn watch --env entry=mhv-secure-messaging,medications api=https://${CODESPACE_NAME}-3000.app.github.dev public=https://${CODESPACE_NAME}-3001.app.github.dev
+cd ../vets-website && yarn watch --env api=https://${CODESPACE_NAME}-3000.app.github.dev public=https://${CODESPACE_NAME}-3001.app.github.dev
