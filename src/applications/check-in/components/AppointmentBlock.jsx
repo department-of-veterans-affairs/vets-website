243585f1cab48bd24fc2840d6bf2bd2efb1022d5
Brian Seek <brian.seek@civicactions.com>
2024-05-24 13:32:23 -0700

    removed timestamp and related tests from pre-check-in.

--- src/applications/check-in/components/AppointmentBlock.jsx
+++ src/applications/check-in/components/AppointmentBlock.jsx
@@ -9,7 +9,6 @@ import { createAnalyticsSlug } from '../utils/analytics';
 import AppointmentListItem from './AppointmentDisplay/AppointmentListItem';
 import { makeSelectApp } from '../selectors';
 import { useFormRouting } from '../hooks/useFormRouting';
-import { APP_NAMES } from '../utils/appConstants';
 import {
   getAppointmentId,
   sortAppointmentsByStartTime,
@@ -20,7 +19,6 @@ const AppointmentBlock = props => {
   const selectApp = useMemo(makeSelectApp, []);
   const { app } = useSelector(selectApp);
   const { t } = useTranslation();
-  const appointmentsDateTime = new Date(appointments[0].startTime);
 
   const { jumpToPage } = useFormRouting(router);
 
@@ -36,22 +34,9 @@ const AppointmentBlock = props => {
 
   return (
     <div>
-      {app === APP_NAMES.PRE_CHECK_IN ? (
-        <p
-          className="vads-u-font-family--serif"
-          data-testid="appointment-day-location"
-        >
-          {t('your-appointments-on-day', {
-            count: appointments.length,
-            day: appointmentsDateTime,
-          })}
-        </p>
-      ) : (
-        <p className="vads-u-font-family--serif" data-testid="date-text">
-          {t('here-are-your-appointments-for-today', { date: new Date() })}
-        </p>
-      )}
-
+      <p className="vads-u-font-family--serif" data-testid="date-text">
+        {t('here-are-your-appointments-for-today', { date: new Date() })}
+      </p>
       <ul
         className="vads-u-border-top--1px vads-u-margin-bottom--4 check-in--appointment-list appointment-list"
         data-testid="appointment-list"
