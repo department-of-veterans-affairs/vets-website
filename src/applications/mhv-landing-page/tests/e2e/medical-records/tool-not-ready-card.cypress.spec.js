9111c3ef61a181ba83e938fb4f9560a65f5bace2
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-05-24 12:26:59 -0400

    fix failing test condition for link that appears more than once pt 2

--- src/applications/mhv-landing-page/tests/e2e/medical-records/tool-not-ready-card.cypress.spec.js
+++ src/applications/mhv-landing-page/tests/e2e/medical-records/tool-not-ready-card.cypress.spec.js
@@ -13,12 +13,14 @@ describe(`${appName} -- transitional Medical records card **enabled**`, () => {
   });
 
   // eslint-disable-next-line @department-of-veterans-affairs/axe-check-required
-  cy.findByRole('heading', { level: 2, name: heading })
-    .parent()
-    .within(() => {
-      cy.findByText(/^The new version of this tool isn't ready yet./);
-      cy.findByRole('link', { name: /^Go back to the previous version/ });
-    });
+  it('renders the medical records card correctly', () => {
+    cy.findByRole('heading', { level: 2, name: heading })
+      .parent()
+      .within(() => {
+        cy.findByText(/^The new version of this tool isn't ready yet./);
+        cy.findByRole('link', { name: /^Go back to the previous version/ });
+      });
+  });
 
   it('passes automated accessibility (a11y) checks', () => {
     cy.injectAxeThenAxeCheck();
