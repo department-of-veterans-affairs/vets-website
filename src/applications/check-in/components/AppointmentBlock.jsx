ef5c4cd12cf542551d4598de36da036dd2eeac92
Brian Seek <brian.seek@civicactions.com>
2024-05-24 13:37:31 -0700

    added app check back for day-of content.

--- src/applications/check-in/components/AppointmentBlock.jsx
+++ src/applications/check-in/components/AppointmentBlock.jsx
@@ -9,6 +9,7 @@ import { createAnalyticsSlug } from '../utils/analytics';
 import AppointmentListItem from './AppointmentDisplay/AppointmentListItem';
 import { makeSelectApp } from '../selectors';
 import { useFormRouting } from '../hooks/useFormRouting';
+import { APP_NAMES } from '../utils/appConstants';
 import {
   getAppointmentId,
   sortAppointmentsByStartTime,
@@ -34,9 +35,11 @@ const AppointmentBlock = props => {
 
   return (
     <div>
-      <p className="vads-u-font-family--serif" data-testid="date-text">
-        {t('here-are-your-appointments-for-today', { date: new Date() })}
-      </p>
+      {app === APP_NAMES.CHECK_IN && (
+        <p className="vads-u-font-family--serif" data-testid="date-text">
+          {t('here-are-your-appointments-for-today', { date: new Date() })}
+        </p>
+      )}
       <ul
         className="vads-u-border-top--1px vads-u-margin-bottom--4 check-in--appointment-list appointment-list"
         data-testid="appointment-list"
