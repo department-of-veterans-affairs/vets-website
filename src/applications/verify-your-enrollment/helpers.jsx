4d5f86f45ebdd4febf01cf3770574018e5d6e586
Jarred Simon <jarred.simon@va.gov>
2024-05-28 15:33:29 -0500

    updated getDateRanges in helper.jsx

--- src/applications/verify-your-enrollment/helpers.jsx
+++ src/applications/verify-your-enrollment/helpers.jsx
@@ -214,9 +214,9 @@ export const getDateRangesBetween = (date1, date2) => {
 
     if (currentMonth === 12) {
       currentMonth = 1;
-      currentYear++;
+      currentYear += 1;
     } else {
-      currentMonth++;
+      currentMonth += 1;
     }
   }
 
