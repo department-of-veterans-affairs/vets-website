5bef25da7eb436ed2bc0b84889f4ac83c4c02540
Adrian Rollett <acrollet@gmail.com>
2024-05-28 20:40:07 +0000

    Improve country name handling

--- src/applications/health-care-supply-reordering/tests/utils/country-mapping.unit.spec.jsx
+++ src/applications/health-care-supply-reordering/tests/utils/country-mapping.unit.spec.jsx
@@ -2,13 +2,31 @@ import { expect } from 'chai';
 
 import constants from 'vets-json-schema/dist/constants.json';
 
-import { nameToValue, valueToName } from '../../utils/country-mapping';
+import {
+  caseInsensitiveComparison,
+  nameToValue,
+  valueToName,
+} from '../../utils/country-mapping';
 
 describe('country-mapping', () => {
   describe('nameToValue', () => {
     it('returns the value for a known country name', () => {
       const countryName = constants.countries[0].label;
       const expectedValue = constants.countries[0].value;
+
+      expect(nameToValue(countryName)).to.equal(expectedValue);
+    });
+
+    it('returns the value for a known country name (case insensitive)', () => {
+      const countryName = constants.countries[0].label.toUpperCase();
+      const expectedValue = constants.countries[0].value;
+
+      expect(nameToValue(countryName)).to.equal(expectedValue);
+    });
+
+    it('maps from DLC APO Country format', () => {
+      const countryName = 'ARMED FORCES AF,EU,ME,CA';
+      const expectedValue = 'USA';
 
       expect(nameToValue(countryName)).to.equal(expectedValue);
     });
@@ -28,10 +46,47 @@ describe('country-mapping', () => {
       expect(valueToName(countryValue)).to.equal(expectedName);
     });
 
+    it('returns the name for a known country value (case insensitive)', () => {
+      const countryValue = constants.countries[0].value.toUpperCase();
+      const expectedName = constants.countries[0].label;
+
+      expect(valueToName(countryValue)).to.equal(expectedName);
+    });
+
     it('returns the input if the country value is unknown', () => {
       const countryValue = 'Unknown Value';
 
       expect(valueToName(countryValue)).to.equal(countryValue);
+    });
+  });
+
+  describe('caseInsensitiveComparison', () => {
+    it('returns true for case-insensitive match', () => {
+      const a = 'United States';
+      const b = 'united states';
+
+      expect(caseInsensitiveComparison(a, b)).to.be.true;
+    });
+
+    it('returns false for non-matching strings', () => {
+      const a = 'United States';
+      const b = 'Canada';
+
+      expect(caseInsensitiveComparison(a, b)).to.be.false;
+    });
+
+    it('returns true for non-string identical values', () => {
+      const a = 123;
+      const b = 123;
+
+      expect(caseInsensitiveComparison(a, b)).to.be.true;
+    });
+
+    it('returns false for non-string different values', () => {
+      const a = 123;
+      const b = 456;
+
+      expect(caseInsensitiveComparison(a, b)).to.be.false;
     });
   });
 });
--- src/applications/health-care-supply-reordering/utils/country-mapping.js
+++ src/applications/health-care-supply-reordering/utils/country-mapping.js
@@ -1,17 +1,30 @@
 import constants from 'vets-json-schema/dist/constants.json';
 
-const nameToValue = countryName => {
-  const country = constants.countries.find(
-    countryMapping => countryMapping.label === countryName,
+const caseInsensitiveComparison = (a, b) => {
+  return typeof a === 'string' && typeof b === 'string'
+    ? a.localeCompare(b, undefined, { sensitivity: 'accent' }) === 0
+    : a === b;
+};
+
+const nameToValue = name => {
+  let countryName = name.trim();
+
+  // Convert from DLC APO format.
+  if (countryName.startsWith('ARMED FORCES')) {
+    countryName = 'United States';
+  }
+
+  const country = constants.countries.find(countryMapping =>
+    caseInsensitiveComparison(countryMapping.label, countryName),
   );
   return country ? country.value : countryName;
 };
 
 const valueToName = countryValue => {
-  const country = constants.countries.find(
-    countryMapping => countryMapping.value === countryValue,
+  const country = constants.countries.find(countryMapping =>
+    caseInsensitiveComparison(countryMapping.value, countryValue),
   );
   return country ? country.label : countryValue;
 };
 
-export { nameToValue, valueToName };
+export { caseInsensitiveComparison, nameToValue, valueToName };
