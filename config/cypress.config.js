4fdecde84a734684af9faf598e4706a1153f966b
Richard Davis <richard.davis@agile6.com>
2024-05-29 17:36:25 -0400

    Updates cypress headless configuration

--- config/cypress.config.js
+++ config/cypress.config.js
@@ -184,6 +184,30 @@ const cypressConfig = {
   },
   e2e: {
     setupNodeEvents(on, config) {
+      // https://docs.cypress.io/api/plugins/browser-launch-api#Set-screen-size-when-running-headless
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome' && browser.isHeadless) {
+          // fullPage screenshot size is 1400x1200 on non-retina screens
+          // and 2800x2400 on retina screens
+          launchOptions.args.push('--window-size=1400,1200');
+
+          // force screen to be non-retina (1400x1200 size)
+          launchOptions.args.push('--force-device-scale-factor=1');
+
+          // force screen to be retina (2800x2400 size)
+          // launchOptions.args.push('--force-device-scale-factor=2')
+        }
+
+        if (browser.name === 'firefox' && browser.isHeadless) {
+          // menubars take up height on the screen
+          // so fullPage screenshot size is 1400x1126
+          launchOptions.args.push('--width=1400');
+          launchOptions.args.push('--height=1200');
+        }
+
+        return launchOptions;
+      });
+
       return require('../src/platform/testing/e2e/cypress/plugins/index')(
         on,
         config,
