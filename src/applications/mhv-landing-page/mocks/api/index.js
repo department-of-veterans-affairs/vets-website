1afcb67a1c0e1f38ed7d69cc86dbe2b44e7c2544
Richard Davis <richard.davis@agile6.com>
2024-05-29 12:53:46 -0400

    Updates

--- src/applications/mhv-landing-page/mocks/api/index.js
+++ src/applications/mhv-landing-page/mocks/api/index.js
@@ -33,7 +33,6 @@ const responses = (selectedMockType = MOCK_TYPES.VERIFIED_USER) => {
     if (selectedMockType === MOCK_TYPES.VERIFIED_USER_ALL_FEATURES) {
       return featureToggles.generateFeatureToggles({
         mhvLandingPageEnabled: true,
-        mhvLandingPagePersonalization: true,
         mhvLandingPageEnableVaGovHealthToolsLinks: true,
         mhvSecondaryNavigationEnabled: true,
         mhvTransitionalMedicalRecordsLandingPage: true,
@@ -63,4 +62,4 @@ const responses = (selectedMockType = MOCK_TYPES.VERIFIED_USER) => {
 
 // Change the mock type for different type of mocked content.
 // Please keep this mock to always return MOCK_TYPES.VERIFIED_USER to keep features like in production.
-module.exports = delay(responses(MOCK_TYPES.VERIFIED_USER), 1000);
+module.exports = delay(responses(MOCK_TYPES.VERIFIED_USER_ALL_FEATURES), 1000);
