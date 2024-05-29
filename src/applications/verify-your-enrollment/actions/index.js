416b30ebee6397357790e07bb116eee66e7caceb
Wafi Mohamed <wafi.mohamed@va.gov>
2024-05-28 22:55:19 -0400

    #VYE-415 #Comment removed Veterans name from both forms (Direct Deposit and from Mailing Address)

--- src/applications/verify-your-enrollment/actions/index.js
+++ src/applications/verify-your-enrollment/actions/index.js
@@ -1,5 +1,6 @@
 import { apiRequest } from '@department-of-veterans-affairs/platform-utilities/api';
 import environment from '@department-of-veterans-affairs/platform-utilities/environment';
+import { UPDATED_USER_MOCK_DATA } from '../constants/mockData';
 // Action Types
 export const UPDATE_PENDING_VERIFICATIONS = 'UPDATE_PENDING_VERIFICATIONS';
 export const UPDATE_VERIFICATIONS = 'UPDATE_VERIFICATIONS';
@@ -59,9 +60,17 @@ export const updateVerifications = verifications => ({
   type: UPDATE_VERIFICATIONS,
   payload: verifications,
 });
-const customHeaders = {
-  'Content-Type': 'application/json',
-  'X-Key-Inflection': 'camel',
+
+export const getData = () => {
+  return disptach => {
+    disptach({ type: GET_DATA }); // TODO: replace with real API call when is ready
+    setTimeout(() => {
+      disptach({
+        type: GET_DATA_SUCCESS,
+        response: UPDATED_USER_MOCK_DATA,
+      });
+    }, 1000);
+  };
 };
 export const fetchPersonalInfo = () => {
   return async dispatch => {
@@ -85,7 +94,10 @@ export const fetchPersonalInfo = () => {
       });
   };
 };
-
+const customHeaders = {
+  'Content-Type': 'application/json',
+  'X-Key-Inflection': 'camel',
+};
 export function postMailingAddress(mailingAddress) {
   return async dispatch => {
     dispatch({ type: UPDATE_ADDRESS });
@@ -118,6 +130,7 @@ export const updateBankInfo = bankInfo => {
         body: JSON.stringify(bankInfo),
         headers: customHeaders,
       });
+
       dispatch({
         type: UPDATE_BANK_INFO_SUCCESS,
         response,
