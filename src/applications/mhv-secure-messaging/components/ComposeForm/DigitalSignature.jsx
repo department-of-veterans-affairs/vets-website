df0a7a490a6823597c05701adc553f4c069058f7
Alex Morgun <oleksii.morgun@va.gov>
2024-05-29 09:30:11 -0400

    [#MHV-57322] add digital signature comonent

---
+++ src/applications/mhv-secure-messaging/components/ComposeForm/DigitalSignature.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DigitalSignature = () => {
+  return (
+    <va-card background>
+      <h2 className="vads-u-font-size--h3 vads-u-margin-top--1">
+        Digital signature
+      </h2>
+      <p />
+
+      <va-text-input label="Your full name" required />
+    </va-card>
+  );
+};
+
+export default DigitalSignature;
