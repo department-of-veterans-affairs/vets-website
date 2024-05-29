0eab7c422ce1d48a0ba4ae39f0a818c7a3e094f5
Richard Davis <richard.davis@agile6.com>
2024-05-29 17:36:13 -0400

    Updates spec

--- src/applications/mhv-landing-page/tests/e2e/mhv-landing-page-screenshots.cypress.spec.js
+++ src/applications/mhv-landing-page/tests/e2e/mhv-landing-page-screenshots.cypress.spec.js
@@ -4,10 +4,10 @@ import LandingPage from './pages/LandingPage';
 
 // Screenshots save to 'vets-website/cypress/screenshots'
 
-const viewports = ['va-top-mobile-1', 'va-top-desktop-1'];
+const viewports = ['va-top-mobile-1', 'va-top-desktop-2'];
 
 Cypress.Commands.add('saveScreenshot', filename => {
-  cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
+  cy.wait(500); // eslint-disable-line cypress/no-unnecessary-waiting
   // set scroll behavior to default
   cy.get('html, body').invoke(
     'attr',
@@ -17,30 +17,30 @@ Cypress.Commands.add('saveScreenshot', filename => {
   cy.screenshot(filename, { overwrite: true });
 });
 
-viewports.forEach(size => {
-  describe(`${appName} -- ${size} screenshots`, () => {
+viewports.forEach(viewport => {
+  describe(`${appName} -- ${viewport} screenshots`, () => {
     beforeEach(() => {
       ApiInitializer.initializeFeatureToggle.withAllFeatures();
       ApiInitializer.initializeMessageData.withNoUnreadMessages();
       cy.intercept('/data/cms/vamc-ehr.json', {});
-      cy.viewportPreset(size);
+      cy.viewportPreset(viewport);
     });
 
     it("displays 'Identity not verified' alert", () => {
       LandingPage.visit({ verified: false });
-      cy.saveScreenshot(`my-health--alert--identity-not-verified--${size}`);
+      cy.saveScreenshot(`my-health--alert--identity-not-verified--${viewport}`);
       cy.injectAxeThenAxeCheck();
     });
 
     it("displays 'You don't have access' alert", () => {
       LandingPage.visit({ registered: false });
-      cy.saveScreenshot(`my-health--alert--you-dont-have-access--${size}`);
+      cy.saveScreenshot(`my-health--alert--you-dont-have-access--${viewport}`);
       cy.injectAxeThenAxeCheck();
     });
 
     it('renders', () => {
       LandingPage.visit();
-      cy.saveScreenshot(`my-health--${size}`);
+      cy.saveScreenshot(`my-health--${viewport}`);
       cy.injectAxeThenAxeCheck();
     });
   });
