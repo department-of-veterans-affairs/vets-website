13b4a11a84724bef2f38fd022793ba5219cbfab6
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2024-05-29 14:33:37 -0400

    #VYE-411 #Comment removed Veterans name from both forms (Direct Depos… (#30042)
    * #VYE-411 #Comment removed Veterans name from both forms (Direct Deposit and from Mailing Address)
    
    * #VYE-411 #Commnet Fixed failing cypress tests

--- src/applications/verify-your-enrollment/tests/e2e/enrollment-verification.cypress.spec.js
+++ src/applications/verify-your-enrollment/tests/e2e/enrollment-verification.cypress.spec.js
@@ -114,4 +114,37 @@ describe('Enrollment Verification Page Tests', () => {
     cy.get('a[aria-label="page 1, first page"]').click();
     cy.get('[id="vye-pagination-page-status-text"]').should('be.focused');
   });
+  it("Should return This page isn't available right now if there is 500 error ", () => {
+    cy.injectAxeThenAxeCheck();
+    cy.intercept('GET', '/vye/v1', {
+      statusCode: 500,
+      body: {
+        errors: [
+          {
+            title: 'Internal server error',
+            detail: 'Internal server error',
+            code: '500',
+            status: '500',
+          },
+        ],
+      },
+    }).as('getServerError');
+    cy.visit('/education/verify-school-enrollment/mgib-enrollments/', {
+      onBeforeLoad: win => {
+        /* eslint no-param-reassign: "error" */
+        win.isProduction = true;
+      },
+    });
+
+    cy.wait('@getServerError').then(interception => {
+      expect(interception.response.statusCode).to.equal(500);
+      expect(interception.response.body.errors[0].title).to.equal(
+        'Internal server error',
+      );
+    });
+    cy.get('[slot="headline"]').should(
+      'contain',
+      "This page isn't available right now.",
+    );
+  });
 });
