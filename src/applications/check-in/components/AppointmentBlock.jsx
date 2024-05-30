2819806b0472b896801a691fe32d3c1da2fe3108
John Woolschlager <wullaski@gmail.com>
2024-05-30 15:54:22 -0400

    Use h2 for pre check in complete

--- src/applications/check-in/components/AppointmentBlock.jsx
+++ src/applications/check-in/components/AppointmentBlock.jsx
@@ -36,9 +36,14 @@ const AppointmentBlock = props => {
   return (
     <div>
       {app === APP_NAMES.CHECK_IN && (
-        <p className="vads-u-font-family--serif" data-testid="date-text">
+        <h2 className="vads-u-font-family--serif" data-testid="date-text">
           {t('here-are-your-appointments-for-today', { date: new Date() })}
-        </p>
+        </h2>
+      )}
+      {app === APP_NAMES.PRE_CHECK_IN && (
+        <h2 className="vads-u-margin-top--0" data-testid="appointment-text">
+          {t('your-appointments', { count: sortedAppointments.length })}
+        </h2>
       )}
       <ul
         className="vads-u-border-top--1px vads-u-margin-bottom--4 check-in--appointment-list appointment-list"
