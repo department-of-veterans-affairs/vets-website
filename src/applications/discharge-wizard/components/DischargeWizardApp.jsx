21e719abd127ee8ed02336be0dec87224b2cfa9f
Randi Mays <rmays@kindsys.us>
2024-05-30 11:48:51 -0500

    VACMS-18155 DUW content updates for Air Force flow

--- src/applications/discharge-wizard/components/DischargeWizardApp.jsx
+++ src/applications/discharge-wizard/components/DischargeWizardApp.jsx
@@ -7,20 +7,20 @@ import BreadcrumbsV2 from './v2/BreadcrumbsV2';
 export default function DischargeWizardApp({ children }) {
   const isProd = environment.isProduction();
 
-  // if (isProd) {
+  if (isProd) {
     return (
       <div className="discharge-wizard row vads-u-padding-x--1 large-screen:vads-u-padding-x--0">
         <Breadcrumbs />
         {children}
       </div>
     );
-  // }
-  // return (
-  //   <div className="row discharge-wizard-v2 vads-u-padding-bottom--8">
-  //     <BreadcrumbsV2 />
-  //     <div className="columns">{children}</div>
-  //   </div>
-  // );
+  }
+  return (
+    <div className="row discharge-wizard-v2 vads-u-padding-bottom--8">
+      <BreadcrumbsV2 />
+      <div className="columns">{children}</div>
+    </div>
+  );
 }
 DischargeWizardApp.propTypes = {
   children: PropTypes.any,
