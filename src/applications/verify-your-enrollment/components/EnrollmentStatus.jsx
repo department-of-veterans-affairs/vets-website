5216a7031da462c94014c81ef44c677af3737dc8
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2024-05-30 15:06:19 -0400

    #VYE-417 #Comments added functionality for a new users also added unit test and cypress test (#30086)

--- src/applications/verify-your-enrollment/components/EnrollmentStatus.jsx
+++ src/applications/verify-your-enrollment/components/EnrollmentStatus.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const EnrollmentStatus = ({ start, end, total, hasEnrollment }) => (
-  <p
-    id="vye-pagination-page-status-text"
-    className="focus-element-on-pagination"
-    aria-label={`Showing ${start}${
-      end > 0 ? ` - ${end}` : ''
-    } of ${total} monthly enrollments listed by most recent`}
-    aria-hidden="false"
-  >
-    {`Showing ${start}${
-      end > 0 ? ` - ${end}` : ''
-    } of ${total} monthly enrollments listed by most recent`}
-    {hasEnrollment && (
-      <span className="vads-u-font-weight--bold vads-u-display--block vads-u-margin-top--2">
-        You currently have no enrollments.
-      </span>
-    )}
-  </p>
-);
+export const EnrollmentStatus = ({ start, end, total, hasEnrollment }) => {
+  const showingEnrollment = `Showing ${start}${
+    end > 0 ? ` - ${end}` : ''
+  } of ${total} monthly enrollments listed by most recent`;
+  return (
+    <p
+      id="vye-pagination-page-status-text"
+      className="focus-element-on-pagination"
+      aria-label={showingEnrollment}
+      aria-hidden="false"
+    >
+      {showingEnrollment}
+
+      {hasEnrollment && (
+        <span className="vads-u-font-weight--bold vads-u-display--block vads-u-margin-top--2">
+          You currently have no enrollments.
+        </span>
+      )}
+    </p>
+  );
+};
 
 EnrollmentStatus.propTypes = {
   end: PropTypes.number,
