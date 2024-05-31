478cf5cd9f45a696be9e5e3e2860dc8328d68e8a
Alex Morgun <87077843+oleksii-morgun@users.noreply.github.com>
2024-05-31 15:55:50 +0000

    startCommand condition for mhv mock service

--- .devcontainer/codespaces-start.sh
+++ .devcontainer/codespaces-start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Start mock services
-printf "\n\n##### Starting mock services #####\n"
-yarn mock-api --responses src/platform/mhv/api/mocks &
+if [[ "${VETS_WEBSITE_MHV_MOCK_SERVICE}" == "YES" ]]
+then
+    # Start mock services
+    printf "\n\n##### Starting mock services #####\n"
+    yarn mock-api --responses src/platform/mhv/api/mocks &
 
+    # Start vets-website
+    printf "\n\n##### Starting vets-website #####\n"
+    cd ../vets-website && yarn watch --env api=https://${CODESPACE_NAME}-3000.app.github.dev public=https://${CODESPACE_NAME}-3001.app.github.dev
+fi
 
-# Start vets-website
-printf "\n\n##### Starting vets-website #####\n"
-cd ../vets-website && yarn watch --env api=https://${CODESPACE_NAME}-3000.app.github.dev public=https://${CODESPACE_NAME}-3001.app.github.dev
