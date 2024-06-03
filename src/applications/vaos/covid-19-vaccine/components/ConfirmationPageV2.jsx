218f8dd2f70c91ea4690add08604cdb3588dc357
Jun Tao Luo <john.luo@agile6.com>
2024-06-03 12:19:06 -0400

    Add tab-navigation for all va-links in VAOS

--- src/applications/vaos/covid-19-vaccine/components/ConfirmationPageV2.jsx
+++ src/applications/vaos/covid-19-vaccine/components/ConfirmationPageV2.jsx
@@ -93,6 +93,7 @@ function ConfirmationPageV2({
             text="Schedule a new appointment"
             data-testid="schedule-appointment-link"
             onClick={handleClick(history, dispatch, typeOfCare)}
+            tabindex="0"
           />
         </div>
       </InfoAlert>
