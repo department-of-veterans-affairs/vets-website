a274b77461cde53bf2beea22388534a90d542521
William Phelps <williamphelps13@gmail.com>
2024-06-03 09:54:47 -0600

    Convert ConfirmationPage to FC and update content

--- src/applications/accreditation/21a/containers/ConfirmationPage.jsx
+++ src/applications/accreditation/21a/containers/ConfirmationPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { format, isValid } from 'date-fns';
 import { connect } from 'react-redux';
@@ -6,89 +6,89 @@ import { connect } from 'react-redux';
 import scrollToTop from 'platform/utilities/ui/scrollToTop';
 import { focusElement } from 'platform/utilities/ui';
 
-export class ConfirmationPage extends React.Component {
-  componentDidMount() {
+const ConfirmationPage = ({ form }) => {
+  const { submission, formId, data } = form;
+  const { fullName } = data;
+  const submitDate = new Date(submission?.timestamp);
+
+  useEffect(() => {
     focusElement('h2');
     scrollToTop('topScrollElement');
-  }
+  }, []);
 
-  render() {
-    const { form } = this.props;
-    const { submission, formId, data } = form;
-
-    const { fullName } = data;
-
-    const submitDate = new Date(submission?.timestamp);
-
-    return (
-      <div>
-        <div className="print-only">
-          <img
-            src="https://www.va.gov/img/design/logo/logo-black-and-white.png"
-            alt="VA logo"
-            width="300"
-          />
-          <h2>Application for Mock Form</h2>
-        </div>
-        <h2 className="vads-u-font-size--h3">
-          Your application has been submitted
-        </h2>
-        <p>We may contact you for more information or documents.</p>
-        <p className="screen-only">Please print this page for your records.</p>
-        <div className="inset">
-          <h3 className="vads-u-margin-top--0 vads-u-font-size--h4">
-            Apply to become a VA accredited attorney or claims agent Claim{' '}
-            <span className="vads-u-font-weight--normal">(Form {formId})</span>
-          </h3>
-          {fullName ? (
-            <span>
-              for {fullName.first} {fullName.middle} {fullName.last}
-              {fullName.suffix ? `, ${fullName.suffix}` : null}
-            </span>
-          ) : null}
-
-          {isValid(submitDate) ? (
-            <p>
-              <strong>Date submitted</strong>
-              <br />
-              <span>{format(submitDate, 'MMMM d, yyyy')}</span>
-            </p>
-          ) : null}
-          <button
-            type="button"
-            className="usa-button screen-only"
-            onClick={window.print}
-          >
-            Print this for your records
-          </button>
-        </div>
+  return (
+    <div>
+      <div className="print-only">
+        <img
+          src="https://www.va.gov/img/design/logo/logo-black-and-white.png"
+          alt="VA logo"
+          width="300"
+        />
       </div>
-    );
-  }
-}
+      <va-alert status="success" visible>
+        <h2 slot="headline">
+          Thank you for completing your application to become a VA accredited
+          attorney or claims agent (Form {formId})
+        </h2>
+        <p className="vads-u-margin-y--0">
+          Once we’ve successfully received your application, we’ll contact you
+          to tell you what happens next in the application process.
+        </p>
+      </va-alert>
+      <div className="inset">
+        <h3 className="vads-u-margin-top--0 vads-u-font-size--h4">
+          Your application information
+        </h3>
+        {fullName && (
+          <p>
+            <strong>For</strong>
+            <br />
+            {fullName.first} {fullName.middle} {fullName.last}
+            {fullName.suffix && `, ${fullName.suffix}`}
+          </p>
+        )}
+        {isValid(submitDate) && (
+          <p>
+            <strong>Date you applied</strong>
+            <br />
+            {format(submitDate, 'MMMM d, yyyy')}
+          </p>
+        )}
+        <p>
+          <strong>Confirmation for your records</strong>
+          <br />
+          You can print this confirmation for your records.
+        </p>
+        <va-button
+          uswds
+          class="screen-only vads-u-margin-top--1"
+          text="Print this page"
+          onClick={() => window.print()}
+        />
+      </div>
+    </div>
+  );
+};
 
 ConfirmationPage.propTypes = {
   form: PropTypes.shape({
     data: PropTypes.shape({
-      fullName: {
+      fullName: PropTypes.shape({
         first: PropTypes.string,
         middle: PropTypes.string,
         last: PropTypes.string,
         suffix: PropTypes.string,
-      },
+      }),
     }),
     formId: PropTypes.string,
     submission: PropTypes.shape({
       timestamp: PropTypes.string,
     }),
   }),
-  name: PropTypes.string,
 };
 
-function mapStateToProps(state) {
-  return {
-    form: state.form,
-  };
-}
+const mapStateToProps = state => ({
+  form: state.form,
+});
 
 export default connect(mapStateToProps)(ConfirmationPage);
