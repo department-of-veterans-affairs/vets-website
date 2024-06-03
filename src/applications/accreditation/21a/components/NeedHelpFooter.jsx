85afd9de4be0b76113a90f90420dff6398ec405b
William Phelps <williamphelps13@gmail.com>
2024-06-03 09:57:50 -0600

    Add propTypes and NeedHelpFooter to App (placeholder)

---
+++ src/applications/accreditation/21a/components/NeedHelpFooter.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NeedHelpFooter = () => (
+  <div>
+    <h2 className="vads-u-padding-bottom--0p5 vads-u-font-size--h3 vads-u-border-bottom--2px vads-u-border-color--primary">
+      Need help?
+    </h2>
+    <p>
+      <strong>If you have trouble using this online form,</strong> email VA
+      accreditation at{' '}
+      <a href="mailto:ogcaccreditationmailbox@va.gov">
+        ogcaccreditationmailbox@va.gov
+      </a>
+    </p>
+  </div>
+);
+
+export default NeedHelpFooter;
--- src/applications/accreditation/21a/containers/App.jsx
+++ src/applications/accreditation/21a/containers/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+import NeedHelpFooter from '../components/NeedHelpFooter';
 import formConfig from '../config/form';
 
-export default function App({ location, children }) {
+const App = ({ location, children }) => {
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
+      <NeedHelpFooter />
     </RoutedSavableApp>
   );
-}
+};
+
+App.propTypes = {
+  children: PropTypes.node.isRequired,
+  location: PropTypes.object.isRequired,
+};
+
+export default App;
