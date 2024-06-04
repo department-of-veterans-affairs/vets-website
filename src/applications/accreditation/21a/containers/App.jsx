b6063ade5fcb2b2c7f2e65bacd21a5ecb2d1b014
William Phelps <williamphelps13@gmail.com>
2024-06-03 22:24:43 -0600

    Remove NeedHelpFooter to align with more common pattern: FormFooter and GetFormHelp

--- src/applications/accreditation/21a/containers/App.jsx
+++ src/applications/accreditation/21a/containers/App.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-import NeedHelpFooter from '../components/NeedHelpFooter';
+
 import formConfig from '../config/form';
 
 const App = ({ location, children }) => {
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
-      <NeedHelpFooter />
     </RoutedSavableApp>
   );
 };
