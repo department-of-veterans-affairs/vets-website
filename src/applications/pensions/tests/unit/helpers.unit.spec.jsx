f9560f8bd940b8beddb8458c8d439ebb8543fc06
Scott <51915366+scottsdevelopment@users.noreply.github.com>
2024-06-05 09:24:07 -0400

    Remove pension polling on submission. (#30141)

--- src/applications/pensions/tests/unit/helpers.unit.spec.jsx
+++ src/applications/pensions/tests/unit/helpers.unit.spec.jsx
@@ -1,10 +1,7 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 
-import {
-  mockFetch,
-  setFetchJSONResponse as setFetchResponse,
-} from 'platform/testing/unit/helpers';
+import { mockFetch } from 'platform/testing/unit/helpers';
 import { transformForSubmit } from 'platform/forms-system/src/js/helpers';
 import { formatCurrency, isHomeAcreageMoreThanTwo } from '../../helpers';
 import {
@@ -36,83 +33,6 @@ describe('Pensions helpers', () => {
         },
         err => {
           expect(err.message).to.equal('fake error');
-        },
-      );
-    });
-    it('should resolve if polling state is success', () => {
-      mockFetch();
-      setFetchResponse(global.fetch.onFirstCall(), {
-        data: {
-          attributes: {
-            guid: 'test',
-          },
-        },
-      });
-      setFetchResponse(global.fetch.onSecondCall(), {
-        data: {
-          attributes: {
-            state: 'pending',
-          },
-        },
-      });
-      const response = {};
-      setFetchResponse(global.fetch.onThirdCall(), {
-        data: {
-          attributes: {
-            state: 'success',
-            response,
-          },
-        },
-      });
-      const formConfig = {
-        chapters: {},
-      };
-      const form = {
-        data: {},
-      };
-
-      return submit(form, formConfig).then(res => {
-        expect(res).to.deep.equal(response);
-      });
-    });
-    it('should reject if polling state is failed', () => {
-      mockFetch();
-      setFetchResponse(global.fetch.onFirstCall(), {
-        data: {
-          attributes: {
-            guid: 'test',
-          },
-        },
-      });
-      setFetchResponse(global.fetch.onSecondCall(), {
-        data: {
-          attributes: {
-            state: 'pending',
-          },
-        },
-      });
-      setFetchResponse(global.fetch.onThirdCall(), {
-        data: {
-          attributes: {
-            state: 'failed',
-          },
-        },
-      });
-      const formConfig = {
-        chapters: {},
-      };
-      const form = {
-        data: {},
-      };
-
-      return submit(form, formConfig).then(
-        () => {
-          expect.fail();
-        },
-        err => {
-          expect(err.message).to.equal(
-            'vets_server_error_pensions: status failed',
-          );
         },
       );
     });
