abca0894d8022881fccdfdbe60d126c91b605e08
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2024-06-05 00:01:10 -0400

    address fixed

--- src/applications/personalization/profile/components/direct-deposit/vye/helper.js
+++ src/applications/personalization/profile/components/direct-deposit/vye/helper.js
@@ -99,12 +99,7 @@ export const addressLabel = address => {
 
   return (
     <span>
-      {line1 && (
-        <>
-          {line1}
-          <br />
-        </>
-      )}
+      {line1 && <>{line1}</>}
       {line2 && (
         <>
           {line2}
