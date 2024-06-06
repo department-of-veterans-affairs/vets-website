7970f561399e8ba1327631b3c9ef5c87f69e2aea
Robin Garrison <robin.garrison@adhocteam.us>
2024-06-06 11:50:57 -0500

    Update non-VA edivence page content

--- src/applications/appeals/995/content/evidencePrivateRecordsRequest.jsx
+++ src/applications/appeals/995/content/evidencePrivateRecordsRequest.jsx
@@ -1,19 +1,12 @@
-import React from 'react';
-
 export const privateRecordsRequestTitle =
-  'Would you like us to request your private medical records?';
+  'Would you like us to request your non-VA medical records?';
 
-export const privateRecordsRequestInfo = (
-  <va-additional-info
-    trigger="How do I know if I have private medical records"
-    class="vads-u-margin-top--4"
-    uswds
-  >
-    <p>
-      You have private medical records if you were treated by a private doctor,
-      including a Veteran’s Choice doctor. We’ll need those records to make a
-      decision on your claim. A Disability Benefits Questionnaire is an example
-      of a private medical record.
-    </p>
-  </va-additional-info>
-);
+export const privateRecordsRadioDescription = {
+  yes:
+    'Next we’ll ask you questions from VA Forms 21-4142 and 21-4142a Authorize the release of non-VA medical records to VA',
+  no:
+    'You can upload private medical records later in the form or we may ask you for the 4142 and 4142a when we review your claim',
+};
+
+export const privateRecordsRequestInfo =
+  'You have non-VA medical records if you were treated by a private doctor, including a Veteran’s Choice doctor. We’ll need those records to make a decision on your claim. A Disability Benefits Questionnaire is an example of a private medical record.';
