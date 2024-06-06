e77f2c162938b27089b469a966f7c95e7313f435
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-06-06 13:31:21 -0400

    Added authorizeMedicalSelect content + page

---
+++ src/applications/representative-appoint/content/authorizeMedicalSelect.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const authorizeMedicalSelect = () => (
+  <>
+    <h3>Authorization to access certain medical records</h3>
+    <p>
+      You’ve authorized this accredited Veterans Service Organization (VSO) to
+      access some of your medical records.
+    </p>
+  </>
+);
---
+++ src/applications/representative-appoint/pages/authorizeMedicalSelect.js
@@ -0,0 +1,52 @@
+import VaCheckboxField from 'platform/forms-system/src/js/web-component-fields/VaCheckboxField';
+import { validateBooleanGroup } from 'platform/forms-system/src/js/validation';
+import { authorizeMedicalSelect } from '../content/authorizeMedicalSelect';
+
+export default {
+  uiSchema: {
+    'view:authorizeMedicalSelect': {
+      'ui:description': authorizeMedicalSelect,
+    },
+    'view:authorizeRecordsCheckbox': {
+      'ui:title': 'Select the types of records they can access',
+      'ui:options': {
+        showFieldLabel: true,
+      },
+      'ui:validations': [validateBooleanGroup],
+      'ui:errorMessages': {
+        atLeastOne: 'Please select at least one type',
+        required: 'Please select at least one type',
+      },
+      'view:alcoholRecords': {
+        'ui:webComponentField': VaCheckboxField,
+        'ui:title': 'Alcoholism and alcohol abuse records',
+      },
+      'view:drugAbuseRecords': {
+        'ui:webComponentField': VaCheckboxField,
+        'ui:title': 'Drug abuse records',
+      },
+      'view:HIVRecords': {
+        'ui:webComponentField': VaCheckboxField,
+        'ui:title': 'HIV (human immunodeficiency virus) records',
+      },
+      'view:sickleCellRecords': {
+        'ui:webComponentField': VaCheckboxField,
+        'ui:title': 'Sickle cell anemia records',
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      'view:authorizeRecordsCheckbox': {
+        type: 'object',
+        properties: {
+          'view:alcoholRecords': { type: 'boolean' },
+          'view:drugAbuseRecords': { type: 'boolean' },
+          'view:HIVRecords': { type: 'boolean' },
+          'view:sickleCellRecords': { type: 'boolean' },
+        },
+      },
+    },
+  },
+};
