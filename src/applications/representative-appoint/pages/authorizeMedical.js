37c2d25350594912d371a662c550744d8c5e30ea
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-06-07 10:55:52 -0400

    Removed export default

--- src/applications/representative-appoint/pages/authorizeMedical.js
+++ src/applications/representative-appoint/pages/authorizeMedical.js
@@ -4,58 +4,57 @@ import {
 } from '../content/authorizeMedical';
 import { saveYourApplication } from '../content/saveYourApplication';
 
-export default {
-  uiSchema: {
-    'view:saveYourApplication': {
-      'ui:description': saveYourApplication,
-    },
-    'view:authorizeMedical': {
-      'ui:description': authorizeMedical,
-    },
-    authorizationRadio: {
-      'ui:title': `Do you authorize this accredited VSO to access your medical records?`,
-      'ui:widget': 'radio',
-      'ui:options': {
-        widgetProps: {
-          'All records': { 'data-info': 'all_records' },
-          'Some records': { 'data-info': 'some_records' },
-          'No records': { 'data-info': 'no_records' },
-        },
-        selectedProps: {
-          'All records': { 'aria-describedby': 'all_records' },
-          'Some records': { 'aria-describedby': 'some_records' },
-          'No records': { 'aria-describedby': 'no_records' },
-        },
+export const uiSchema = {
+  'view:saveYourApplication': {
+    'ui:description': saveYourApplication,
+  },
+  'view:authorizeMedical': {
+    'ui:description': authorizeMedical,
+  },
+  authorizationRadio: {
+    'ui:title': `Do you authorize this accredited VSO to access your medical records?`,
+    'ui:widget': 'radio',
+    'ui:options': {
+      widgetProps: {
+        'All records': { 'data-info': 'all_records' },
+        'Some records': { 'data-info': 'some_records' },
+        'No records': { 'data-info': 'no_records' },
       },
-    },
-
-    'view:authorizationNote': {
-      'ui:description': authorizationNote,
+      selectedProps: {
+        'All records': { 'aria-describedby': 'all_records' },
+        'Some records': { 'aria-describedby': 'some_records' },
+        'No records': { 'aria-describedby': 'no_records' },
+      },
     },
   },
-  schema: {
-    type: 'object',
-    properties: {
-      'view:saveYourApplication': {
-        type: 'object',
-        properties: {},
-      },
-      'view:authorizeMedical': {
-        type: 'object',
-        properties: {},
-      },
-      authorizationRadio: {
-        type: 'string',
-        enum: [
-          'Yes, they can access all of these types of records',
-          'Yes, but they can only access some of these types of records',
-          `No, they can't access any of these types of records`,
-        ],
-      },
-      'view:authorizationNote': {
-        type: 'object',
-        properties: {},
-      },
+
+  'view:authorizationNote': {
+    'ui:description': authorizationNote,
+  },
+};
+
+export const schema = {
+  type: 'object',
+  properties: {
+    'view:saveYourApplication': {
+      type: 'object',
+      properties: {},
+    },
+    'view:authorizeMedical': {
+      type: 'object',
+      properties: {},
+    },
+    authorizationRadio: {
+      type: 'string',
+      enum: [
+        'Yes, they can access all of these types of records',
+        'Yes, but they can only access some of these types of records',
+        `No, they can't access any of these types of records`,
+      ],
+    },
+    'view:authorizationNote': {
+      type: 'object',
+      properties: {},
     },
   },
 };
