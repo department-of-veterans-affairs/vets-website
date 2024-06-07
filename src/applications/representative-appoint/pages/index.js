b983705ed495dbf571d4fa7017f5c7c79c76da1a
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-06-07 11:08:05 -0400

    Removed export default

--- src/applications/representative-appoint/pages/index.js
+++ src/applications/representative-appoint/pages/index.js
@@ -1,4 +1,4 @@
 import * as authorizeMedical from './authorizeMedical';
 import * as authorizeMedicalSelect from './authorizeMedicalSelect';
 
-export default { authorizeMedical, authorizeMedicalSelect };
+export { authorizeMedical, authorizeMedicalSelect };
