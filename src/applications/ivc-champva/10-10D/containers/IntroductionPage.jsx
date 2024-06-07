f0cb6602742deb88db75f4b8782d8ea47cb418e2
Michael Clement <michael.clement@adhocteam.us>
2024-06-07 09:17:15 -0500

    removed redundant getHelp and added margin

--- src/applications/ivc-champva/10-10D/containers/IntroductionPage.jsx
+++ src/applications/ivc-champva/10-10D/containers/IntroductionPage.jsx
@@ -6,7 +6,6 @@ import { focusElement } from '@department-of-veterans-affairs/platform-forms-sys
 import FormTitle from '@department-of-veterans-affairs/platform-forms-system/FormTitle';
 import SaveInProgressIntro from 'platform/forms/save-in-progress/SaveInProgressIntro'; // '@' import not working
 import { VaAlert } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import GetFormHelp from '../../shared/components/GetFormHelp';
 
 const IntroductionPage = props => {
   const { route, isLoggedIn } = props;
@@ -140,8 +139,6 @@ const IntroductionPage = props => {
         omb-number="2900-0219"
         exp-date="10/31/2024"
       />
-
-      <GetFormHelp />
     </article>
   );
 };
