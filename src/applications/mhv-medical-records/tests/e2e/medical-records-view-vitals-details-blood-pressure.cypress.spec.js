ba9dbbd6405e159c6e5215041df057c83f858a29
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2024-06-07 10:07:59 -0700

    Update medical-records-view-vitals-details-blood-pressure.cypress.spec.js

--- src/applications/mhv-medical-records/tests/e2e/medical-records-view-vitals-details-blood-pressure.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-view-vitals-details-blood-pressure.cypress.spec.js
@@ -1,6 +1,32 @@
+// import moment from 'moment';
+// import 'moment/dist/locale/en';
+// import moment.locale('en');
+// import 'moment/min/locales';
+import moment from 'moment/min/moment-with-locales';
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
 import VitalsListPage from './pages/VitalsListPage';
 import VitalsDetailsPage from './pages/VitalsDetailsPage';
+import defaultVitals from '../fixtures/vitals.json';
+
+moment.updateLocale('en', {
+  meridiem: {
+    am: 'a.m.',
+    AM: 'A.M.',
+    pm: 'p.m.',
+    PM: 'P.M.',
+  },
+});
+
+// moment.updateLocale('en', {
+//   // Specify the callback function for
+//   // customizing the values
+//   meridiem: function (hour, minute, isLowercase) {
+//         if (hour >= 12)
+//             return isLowercase ? 'p.m.' : 'P.M.';
+//         else
+//             return isLowercase ? 'a.m.' : 'A.M.';
+//     }
+// });
 
 describe('Medical Records Vitals Details Page', () => {
   const site = new MedicalRecordsSite();
@@ -15,14 +41,56 @@ describe('Medical Records Vitals Details Page', () => {
     // click vitals page Blood Pressure Link
     VitalsListPage.clickLinkByRecordListItemIndex(0);
 
+    // // verify first reading
+    // VitalsDetailsPage.verifyVitalReadingByIndex(
+    //   0,
+    //   'October', // 27, 2023, 7:00 a.m. PDT
+    //   '130/70',
+    //   'ADTP BURNETT',
+    //   'None noted',
+    // );
+
+    // moment.updateLocale('en', {
+    //   meridiem: function(hour, minute, isLowerCase) {
+    //     if (hour < 12) {
+    //       return 'a.m.';
+    //     } else {
+    //       return 'p.m.';
+    //     }
+    //   }
+    // });
+
     // verify first reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       0,
-      'October', // 27, 2023, 7:00 a.m. PDT
+      // moment(defaultVitals.entry[0].resource.effectiveDateTime).format(
+      //   'MMMM D, YYYY hh:mm A',
+      // ),
+      // moment
+      //   .parseZone(defaultVitals.entry[0].resource.effectiveDateTime)
+      //   .format('l LT'),
+      moment
+        .parseZone(defaultVitals.entry[0].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, hh:mm a'),
+      // .format('MMMM D, YYYY, hh:mm a'),
       '130/70',
       'ADTP BURNETT',
       'None noted',
     );
+
+    // // verify first reading
+    // VitalsDetailsPage.verifyVitalReadingByIndex(
+    //   0,
+    //   // moment(defaultVitals.entry[0].resource.effectiveDateTime).format(
+    //   //   'MMMM D, YYYY hh:mm A',
+    //   // ),
+    //   moment
+    //     .parseZone(defaultVitals.entry[0].resource.effectiveDateTime)
+    //     .format('LLL'), // .format('l LT'),
+    //   '130/70',
+    //   'ADTP BURNETT',
+    //   'None noted',
+    // );
 
     // verify second reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
