bc07aa054ef16a4076a4b8b2348ef249c30a97b3
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2024-06-07 17:13:59 -0700

    skip download-all page tests, test link to MHV

--- src/applications/mhv-medical-records/tests/e2e/pages/MedicalRecordsLandingPage.js
+++ src/applications/mhv-medical-records/tests/e2e/pages/MedicalRecordsLandingPage.js
@@ -6,5 +6,16 @@ class MedicalRecordsLandingPage {
   verifyPageTitle = () => {
     cy.get('[data-testid="mr-landing-page-title"]').contains('Medical records');
   };
+
+  verifyDownloadOnMhvLink = linkText => {
+    cy.get('[data-testid="go-to-mhv-download-records"]').should('be.visible');
+    cy.get('[data-testid="go-to-mhv-download-records"]')
+      .contains(linkText)
+      .invoke('attr', 'href')
+      .should(
+        'contain',
+        'mhv-syst.myhealth.va.gov/mhv-portal-web/download-my-data',
+      );
+  };
 }
 export default new MedicalRecordsLandingPage();
