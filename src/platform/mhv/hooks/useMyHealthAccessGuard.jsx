329e283e1770aa006519fc58f81ad254e0760a40
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-06-10 04:23:31 -0400

    clean up implementation with vaPatient

--- src/platform/mhv/hooks/useMyHealthAccessGuard.jsx
+++ src/platform/mhv/hooks/useMyHealthAccessGuard.jsx
@@ -1,45 +1,16 @@
 import { useSelector } from 'react-redux';
-// import { isLOA3 } from '@department-of-veterans-affairs/platform-user/selectors';
-import { selectProfile } from '~/platform/user/selectors';
+import { isVAPatient } from '~/platform/user/selectors';
 
-export const hasFacilityData = state => {
-  const facilities = selectProfile(state)?.facilities || [];
-  return facilities.length > 0;
-};
-
-// if facility count is 0, null, or undefined, redirect to /my-health
+/**
+ * Route guard hook that will redirect the user to the /my-health landing page if isVAPatient is false.
+ */
 
 export const useMyHealthAccessGuard = () => {
-  const hasFacility = useSelector(hasFacilityData);
+  const isVAPatientUser = useSelector(isVAPatient);
 
-  if (!hasFacility) {
+  if (!isVAPatientUser) {
     return window.location.replace('/my-health');
   }
 
   return null;
 };
-// export const useMyHealthAccessGuard = () => {
-//   const mhvAccountState = useSelector(
-//     state => state?.user?.profile?.mhvAccountState,
-//   );
-
-//   if (mhvAccountState === 'NONE') {
-//     return window.location.replace('/my-health');
-//   }
-
-//   return null;
-// };
-
-/**
- * Route guard hook that will redirect the user to the /my-health landing page if isLOA3 is false.
- */
-
-// export const useMyHealthAccessGuard = () => {
-//   const isLOA3User = useSelector(isLOA3);
-
-//   if (!isLOA3User) {
-//     return window.location.replace('/my-health');
-//   }
-
-//   return null;
-// };
