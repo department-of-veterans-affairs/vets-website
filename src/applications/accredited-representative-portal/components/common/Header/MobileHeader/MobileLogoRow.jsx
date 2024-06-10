2c165632e0d3d430ec0e48b5dd51da64d61d6ea8
Gabriel Zurita <gabezurita@gmail.com>
2024-06-10 17:24:34 -0600

    #80240 Integrate Sign-in into ARP (#30146)
    * arf.80240 integrate sign-in functionality for ARP
    
    Integrates Accredited Representative Portal (ARP) sign-in functionality using the React Router and Redux libraries. Key changes include:
    
    - Implement an AuthRouterProvider proof of concept https://github.com/department-of-veterans-affairs/vets-website/pull/28355/files
    - Populate a user in client state at root component load
      - use accredited_representative_portal/v0/user
    - Update header user navigation component
      - Renders loading, logged-in/out states with sign-in/out links
      - Uses /v0/representative_user endpoint to populate client state
    - Gate dashboard access based on signed-in status
      - conditionally renders based loading, not signed-in, and signed-in states
    - Integrate feature toggles directly into StartReactApp within ARP
    
    The sign-in integration allows accredited representatives to authenticate and access the portal. The user's logged-in state is tracked in the Redux store. The header reflects the current auth state and provides sign-in/sign-out links.
    
    * arf.80240 include credentials in arp/v0/user GET request
    See thread: https://github.com/department-of-veterans-affairs/vets-website/pull/30146/files#r1626227086
    
    * arf.80240 correctly handle arp/user controller response
    NOTE: accredited_representative_portal/v0/user returns a flat profile object.
    see: https://github.com/department-of-veterans-affairs/vets-api/blob/master/modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/representative_users_controller.rb
    TODO: consider nesting the ARP user profile data in a profile object to match: https://github.com/department-of-veterans-affairs/vets-api/blob/master/app/controllers/v0/users_controller.rb
    
    * arf.80240 make PR review feedback changes
    
    Replace deprecated substr() with substring().
    
    Align imports with default exports.
    
    Reorganize import statements following a common frontend JavaScript order.
    
    Use ES6 arrow functions for consistency.
    
    Handle user fetch error messages correctly.
    
    Use defaultProps for LandingPageWelcome component.
    
    Improve Chai tests by using eq for precision.
    
    Reorganize import statements following a common frontend JavaScript order.
    
    * arf.80240 make PR feedback changes
    	modified:   src/applications/accredited-representative-portal/actions/user.js
      - remove unnecessary response header info: https://github.com/department-of-veterans-affairs/vets-website/pull/30146#discussion_r1630042622
      - add error to redux dispatch: https://github.com/department-of-veterans-affairs/vets-website/pull/30146#discussion_r1630463423
    	modified:   src/applications/accredited-representative-portal/components/LandingPageWelcome/LandingPageWelcome.jsx
      - remove defaultProps: https://github.com/department-of-veterans-affairs/vets-website/pull/30146#discussion_r1630468458
    	modified:   src/applications/accredited-representative-portal/tests/components/common/Header/MobileHeader/MobileLogoRow.unit.spec.js
      - align with the other imports of default exports in this PR and the app: https://github.com/department-of-veterans-affairs/vets-website/pull/30146#discussion_r1630317398
    
    * arf.80240 ARP#fetchUser address code climate complexity comment
    modified:   src/applications/accredited-representative-portal/actions/user.js
    Function fetchUser has a Cognitive Complexity of 6 (exceeds 5 allowed).

--- src/applications/accredited-representative-portal/components/common/Header/MobileHeader/MobileLogoRow.jsx
+++ src/applications/accredited-representative-portal/components/common/Header/MobileHeader/MobileLogoRow.jsx
@@ -23,7 +23,11 @@ export const MobileLogoRow = () => {
         <div className="vads-u-display--flex vads-u-flex-direction--row vads-u-align-items--center">
           <div className="profile-nav-container">
             <div className="hidden-header vads-u-display--flex vads-u-align-items--center">
-              <UserNav isMobile />
+              <div className="sign-in-nav">
+                <div className="sign-in-links">
+                  <UserNav isMobile />
+                </div>
+              </div>
             </div>
           </div>
           {/* eslint-disable-next-line @department-of-veterans-affairs/prefer-button-component, react/button-has-type */}
