048d054ffe864c79f6bee7ae27816c200202d362
Jacob Penner <jacob.penner@adhocteam.us>
2024-06-10 10:57:45 -0400

    add tests for statement page

---
+++ src/applications/simple-forms/21-4138/tests/unit/pages/statement.unit.spec.js
@@ -0,0 +1,56 @@
+import {
+  testNumberOfErrorsOnSubmit,
+  testNumberOfErrorsOnSubmitForWebComponents,
+  testNumberOfFields,
+  testNumberOfWebComponentFields,
+} from '../../../../shared/tests/pages/pageTests.spec';
+import formConfig from '../../../config/form';
+
+const {
+  schema,
+  uiSchema,
+} = formConfig.chapters.statementChapter.pages.statementPage;
+
+const pageTitle = 'Provide your statement';
+
+const data = {};
+
+const expectedNumberOfWebComponentFields = 1;
+testNumberOfWebComponentFields(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfWebComponentFields,
+  pageTitle,
+  data,
+);
+
+const expectedNumberOfWebComponentErrors = 1;
+testNumberOfErrorsOnSubmitForWebComponents(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfWebComponentErrors,
+  pageTitle,
+  data,
+);
+
+const expectedNumberOfFields = 0;
+testNumberOfFields(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfFields,
+  pageTitle,
+  data,
+);
+
+const expectedNumberOfErrors = 0;
+testNumberOfErrorsOnSubmit(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfErrors,
+  pageTitle,
+  data,
+);
