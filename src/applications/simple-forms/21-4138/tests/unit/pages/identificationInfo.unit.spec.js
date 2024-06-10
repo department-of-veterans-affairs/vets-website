d54b24aeb30fb16739d750d28f8c9ccefc3476c5
Jacob Penner <jacob.penner@adhocteam.us>
2024-06-10 09:45:40 -0400

    add tests for identification info page

---
+++ src/applications/simple-forms/21-4138/tests/unit/pages/identificationInfo.unit.spec.js
@@ -0,0 +1,56 @@
+import {
+  testNumberOfErrorsOnSubmit,
+  testNumberOfErrorsOnSubmitForWebComponents,
+  testNumberOfFields,
+  testNumberOfWebComponentFields,
+} from '../../../../shared/tests/pages/pageTests.spec';
+import formConfig from '../../../config/form';
+
+const {
+  schema,
+  uiSchema,
+} = formConfig.chapters.identificationChapter.pages.identificationInformationPage;
+
+const pageTitle = 'Identification information';
+
+const data = {};
+
+const expectedNumberOfWebComponentFields = 2;
+testNumberOfWebComponentFields(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfWebComponentFields,
+  pageTitle,
+  data,
+);
+
+const expectedNumberOfWebComponentErrors = 1;
+testNumberOfErrorsOnSubmitForWebComponents(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfWebComponentErrors,
+  pageTitle,
+  data,
+);
+
+const expectedNumberOfFields = 0;
+testNumberOfFields(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfFields,
+  pageTitle,
+  data,
+);
+
+const expectedNumberOfErrors = 0;
+testNumberOfErrorsOnSubmit(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfErrors,
+  pageTitle,
+  data,
+);
