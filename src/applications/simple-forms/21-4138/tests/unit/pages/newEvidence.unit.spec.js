c4c416221a96f4cc8a5491cfa50c95f1c024a450
Jacob Penner <jacob.penner@adhocteam.us>
2024-06-10 10:00:05 -0400

    add tests for new evidence page

---
+++ src/applications/simple-forms/21-4138/tests/unit/pages/newEvidence.unit.spec.js
@@ -0,0 +1,56 @@
+import {
+  testNumberOfErrorsOnSubmit,
+  testNumberOfErrorsOnSubmitForWebComponents,
+  testNumberOfFields,
+  testNumberOfWebComponentFields,
+} from '../../../../shared/tests/pages/pageTests.spec';
+import formConfig from '../../../config/form';
+
+const {
+  schema,
+  uiSchema,
+} = formConfig.chapters.statementTypeChapter.pages.claimStatusToolPage;
+
+const pageTitle = "There's a better way to request your personal records";
+
+const data = {};
+
+const expectedNumberOfWebComponentFields = 0;
+testNumberOfWebComponentFields(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfWebComponentFields,
+  pageTitle,
+  data,
+);
+
+const expectedNumberOfWebComponentErrors = 0;
+testNumberOfErrorsOnSubmitForWebComponents(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfWebComponentErrors,
+  pageTitle,
+  data,
+);
+
+const expectedNumberOfFields = 0;
+testNumberOfFields(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfFields,
+  pageTitle,
+  data,
+);
+
+const expectedNumberOfErrors = 0;
+testNumberOfErrorsOnSubmit(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfErrors,
+  pageTitle,
+  data,
+);
