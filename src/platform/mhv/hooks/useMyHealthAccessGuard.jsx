6f790ccf9917f41c62203d96f425f8e18d292f0a
Jim Zaccardo <james.zaccardo@adhocteam.us>
2024-06-10 04:13:35 -0400

    switch logic to use facility count

--- src/platform/mhv/hooks/useMyHealthAccessGuard.jsx
+++ src/platform/mhv/hooks/useMyHealthAccessGuard.jsx
@@ -1,5 +1,11 @@
-import { useSelector } from 'react-redux';
-import { isLOA3 } from '@department-of-veterans-affairs/platform-user/selectors';
+// import { useSelector } from 'react-redux';
+// import { isLOA3 } from '@department-of-veterans-affairs/platform-user/selectors';
+import { selectProfile } from '~/platform/user/selectors';
+
+export const hasHealthData = state => {
+  const facilities = selectProfile(state)?.facilities || [];
+  return facilities.length > 0;
+};
 
 // export const useMyHealthAccessGuard = () => {
 //   const mhvAccountState = useSelector(
@@ -17,12 +23,12 @@ import { isLOA3 } from '@department-of-veterans-affairs/platform-user/selectors'
  * Route guard hook that will redirect the user to the /my-health landing page if isLOA3 is false.
  */
 
-export const useMyHealthAccessGuard = () => {
-  const isLOA3User = useSelector(isLOA3);
+// export const useMyHealthAccessGuard = () => {
+//   const isLOA3User = useSelector(isLOA3);
 
-  if (!isLOA3User) {
-    return window.location.replace('/my-health');
-  }
+//   if (!isLOA3User) {
+//     return window.location.replace('/my-health');
+//   }
 
-  return null;
-};
+//   return null;
+// };
