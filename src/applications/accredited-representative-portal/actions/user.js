cc6d1c7c76362fa045a8e6e7818297fb3568ab76
Gabriel Zurita <gabezurita@gmail.com>
2024-06-10 13:59:23 -0600

    arf.80240 ARP#fetchUser address code climate complexity comment modified:   src/applications/accredited-representative-portal/actions/user.js Function fetchUser has a Cognitive Complexity of 6 (exceeds 5 allowed).

--- src/applications/accredited-representative-portal/actions/user.js
+++ src/applications/accredited-representative-portal/actions/user.js
@@ -5,30 +5,33 @@ export const FETCH_USER = 'FETCH_USER';
 export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS';
 export const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE';
 
-export const fetchUser = () => async dispatch => {
-  dispatch({ type: FETCH_USER });
-  try {
-    const response = await apiRequest(
-      `${environment.API_URL}/accredited_representative_portal/v0/user`,
-    );
+const fetchUserStart = () => ({ type: FETCH_USER });
+const fetchUserSuccess = profile => ({
+  type: FETCH_USER_SUCCESS,
+  payload: profile,
+});
+const fetchUserFailure = error => ({ type: FETCH_USER_FAILURE, error });
 
+const requestUserProfile = () => {
+  return apiRequest(
+    `${environment.API_URL}/accredited_representative_portal/v0/user`,
+  );
+};
+
+export const fetchUser = () => async dispatch => {
+  dispatch(fetchUserStart());
+
+  try {
     // NOTE: accredited_representative_portal/v0/user returns a flat profile object.
     // see: https://github.com/department-of-veterans-affairs/vets-api/blob/master/modules/accredited_representative_portal/app/controllers/accredited_representative_portal/v0/representative_users_controller.rb
     // TODO: consider nesting the ARP user profile data in a profile object to match: https://github.com/department-of-veterans-affairs/vets-api/blob/master/app/controllers/v0/users_controller.rb
-    const profile = response;
-
+    const profile = await requestUserProfile();
     if (profile) {
-      dispatch({
-        type: FETCH_USER_SUCCESS,
-        payload: profile,
-      });
+      dispatch(fetchUserSuccess(profile));
     } else {
-      dispatch({ type: FETCH_USER_FAILURE, error: 'Profile not found' });
+      dispatch(fetchUserFailure('Profile not found'));
     }
   } catch (error) {
-    dispatch({
-      type: FETCH_USER_FAILURE,
-      error: error.message || 'Unknown error',
-    });
+    dispatch(fetchUserFailure(error.message || 'Unknown error'));
   }
 };
