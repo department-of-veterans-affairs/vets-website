0acf15cc5da2e67222ec25eb48e348653325fded
Jacob Penner <jacob.penner@adhocteam.us>
2024-06-10 09:37:53 -0400

    add test skeletons, add tests for decision review

---
+++ src/applications/simple-forms/21-4138/tests/unit/pages/decisionReview.unit.spec.js
@@ -0,0 +1,56 @@
+import {
+  testNumberOfErrorsOnSubmit,
+  testNumberOfErrorsOnSubmitForWebComponents,
+  testNumberOfFields,
+  testNumberOfWebComponentFields,
+} from '../../../../shared/tests/pages/pageTests.spec';
+import formConfig from '../../../config/form';
+
+const {
+  schema,
+  uiSchema,
+} = formConfig.chapters.statementTypeChapter.pages.decisionReviewPage;
+
+const pageTitle = 'What to know before you request a decision review';
+
+const data = {};
+
+const expectedNumberOfWebComponentFields = 1;
+testNumberOfWebComponentFields(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfWebComponentFields,
+  pageTitle,
+  data,
+);
+
+const expectedNumberOfWebComponentErrors = 1;
+testNumberOfErrorsOnSubmitForWebComponents(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfWebComponentErrors,
+  pageTitle,
+  data,
+);
+
+const expectedNumberOfFields = 0;
+testNumberOfFields(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfFields,
+  pageTitle,
+  data,
+);
+
+const expectedNumberOfErrors = 0;
+testNumberOfErrorsOnSubmit(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfErrors,
+  pageTitle,
+  data,
+);
