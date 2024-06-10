41088f256a30e163fdf21ec53d64fd17b039fe02
Michael Clement <michael.clement@adhocteam.us>
2024-06-10 17:10:58 -0500

    updated text on confirmation page

--- src/applications/ivc-champva/10-7959C/components/MissingFileConsentPage.jsx
+++ src/applications/ivc-champva/10-7959C/components/MissingFileConsentPage.jsx
@@ -8,8 +8,33 @@ import {
 import MissingFileOverview, {
   MissingFileConsentPagePropTypes,
 } from '../../shared/components/fileUploads/MissingFileOverview';
+import { nameWording } from '../helpers/utilities';
+
+/**
+ * Adds applicant's name to front of all user-friendly file descriptions and
+ * adds primary/secondary health insurance company names to associated file descriptions.
+ * E.g., turns "Primary health insurance card" into "Jimmy's Blue Cross card"
+ *
+ * @param {object} formData standard form data object
+ * @param {object} fileNames object mapping file upload property keys to user-friendly names
+ * @returns object mapping file upload property keys to user-friendly names
+ */
+export function prefixFileNames(formData, fileNames) {
+  const prefixed = {};
+  const appName = nameWording(formData, true, false, true);
+  const pi = 'Primary health insurance';
+  const si = 'Secondary health insurance';
+  Object.keys(fileNames).forEach(key => {
+    prefixed[key] = `${appName} ${fileNames[key]}`
+      .replace(pi, formData?.applicantPrimaryProvider ?? pi)
+      .replace(si, formData?.applicantSecondaryProvider ?? si);
+  });
+  return prefixed;
+}
 
 export function MissingFileConsentPage(props) {
+  const fileNames = prefixFileNames(props.data, requiredFiles);
+
   const OverviewComp = MissingFileOverview({
     contentAfterButtons: props.contentAfterButtons,
     data: props.data,
@@ -20,7 +45,7 @@ export function MissingFileConsentPage(props) {
     heading: <></>,
     showMail: true,
     showConsent: true,
-    fileNameMap: { ...requiredFiles },
+    fileNameMap: fileNames,
     requiredFiles,
     nonListNameKey: 'applicantName',
     mailingAddress: officeAddress,
