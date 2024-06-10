afde0ef891aa9aadfa742fef9f82aa3d2afe040a
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-06-10 17:34:49 -0400

    Added authorizeAddress screen

--- src/applications/representative-appoint/pages/index.js
+++ src/applications/representative-appoint/pages/index.js
@@ -1,4 +1,5 @@
 import * as authorizeMedical from './authorizeMedical';
 import * as authorizeMedicalSelect from './authorizeMedicalSelect';
+import * as authorizeAddress from './authorizeAddress';
 
-export { authorizeMedical, authorizeMedicalSelect };
+export { authorizeMedical, authorizeMedicalSelect, authorizeAddress };
