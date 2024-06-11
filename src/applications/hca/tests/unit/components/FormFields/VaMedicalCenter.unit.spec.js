378eabb7952499089c2fdf6ad1d4fd004e665621
Mitch Saltykov <mitch.saltykov@agile6.com>
2024-06-11 17:05:50 -0400

    Sort facilities alphabetically by name

--- src/applications/hca/tests/unit/components/FormFields/VaMedicalCenter.unit.spec.js
+++ src/applications/hca/tests/unit/components/FormFields/VaMedicalCenter.unit.spec.js
@@ -15,7 +15,7 @@ describe('hca <VaMedicalCenter>', () => {
     {
       id: 'vha_528A4',
       uniqueId: '528A4',
-      name: 'Batavia VA Medical Center',
+      name: 'ZBatavia VA Medical Center',
     },
     {
       id: 'vha_528A5',
@@ -126,6 +126,7 @@ describe('hca <VaMedicalCenter>', () => {
     });
 
     it('should not render the loading indicator', async () => {
+      mockApiRequest(mockData);
       const { container } = render(
         <Provider store={mockStore}>
           <VaMedicalCenter {...props} />
@@ -138,6 +139,7 @@ describe('hca <VaMedicalCenter>', () => {
     });
 
     it('should not render the select input', async () => {
+      mockApiRequest(mockData);
       const { container } = render(
         <Provider store={mockStore}>
           <VaMedicalCenter {...props} />
@@ -182,7 +184,8 @@ describe('hca <VaMedicalCenter>', () => {
         const alert = container.querySelector('va-alert');
 
         expect(options).to.have.lengthOf(mockData.length);
-        expect(options[0]).to.have.attr('value', mockData[0].uniqueId);
+        expect(options[0]).to.have.attr('value', mockData[1].uniqueId);
+        expect(options[1]).to.have.attr('value', mockData[0].uniqueId);
         expect(alert).to.not.exist;
       });
     });
