7fb24dce9a837cb501e0cdc8a8a98d3928e653ce
Jacob Penner <jacob.penner@adhocteam.us>
2024-06-11 12:06:48 -0400

    update pages with UI tweaks

--- src/applications/simple-forms/form-upload/containers/SubmitPage.jsx
+++ src/applications/simple-forms/form-upload/containers/SubmitPage.jsx
@@ -1,25 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
 import {
-  VaBreadcrumbs,
   VaButton,
   VaIcon,
-  VaSegmentedProgressBar,
 } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { useHistory, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { capitalize } from 'lodash';
 import environment from '@department-of-veterans-affairs/platform-utilities/environment';
 import { apiRequest } from '~/platform/utilities/api';
-import {
-  mask,
-  getBreadcrumbList,
-  getFileSize,
-  getFormNumber,
-  getFormUploadContent,
-  handleRouteChange,
-  submitForm,
-} from '../helpers';
+import { mask, getFileSize, getFormNumber, submitForm } from '../helpers';
+import FormPage from './FormPage';
 
 const inProgressApi = formId => {
   const apiUrl = '/v0/in_progress_forms/';
@@ -30,8 +21,6 @@ const SubmitPage = () => {
   const history = useHistory();
   const location = useLocation();
   const formNumber = getFormNumber(location);
-  const formUploadContent = getFormUploadContent(formNumber);
-  const breadcrumbList = getBreadcrumbList(formNumber);
 
   const fullName = useSelector(state => state?.user?.profile?.userFullName);
   const { state } = location;
@@ -58,23 +47,8 @@ const SubmitPage = () => {
   }, []);
 
   return (
-    <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
-      <VaBreadcrumbs
-        breadcrumbList={breadcrumbList}
-        onRouteChange={handleRouteChange}
-      />
-      <h1>{`Upload VA Form ${formNumber}`}</h1>
-      <p>{formUploadContent}</p>
-      <div>
-        <VaSegmentedProgressBar
-          current={3}
-          total={3}
-          headerLevel={2}
-          labels="Upload your file;Review your information;Submit your form"
-        />
-      </div>
-      <div className="vads-u-display--flex vads-u-border-bottom--2px vads-u-justify-content--space-between vads-u-align-items--center">
-        <h3>Uploaded file</h3>
+    <FormPage currentLocation={3} pageTitle="Uploaded file">
+      <div className="vads-u-display--flex vads-u-border-bottom--2px vads-u-justify-content--space-between vads-u-align-items--center vads-u-margin-top--0">
         <a href={`/form-upload/${formNumber}/upload`}>Change file</a>
       </div>
       <div>
@@ -108,20 +82,10 @@ const SubmitPage = () => {
         9:00pm ET.
       </p>
       <span>
-        <VaButton secondary text="<< Back" onClick={history.goBack} />
+        <VaButton secondary back onClick={history.goBack} />
         <VaButton primary text="Submit form" onClick={submitHandler} />
       </span>
-      <div className="need-help-footer">
-        <h2 className="vads-u-padding-bottom--0p5 vads-u-font-size--h3 vads-u-border-bottom--2px vads-u-border-color--primary">
-          Need help?
-        </h2>
-        <p>
-          You can call us at <va-telephone contact="8772228387" /> (
-          <va-telephone contact="8008778339" tty />
-          ). We’re here Monday through Friday, 8:00 a.m to 9:00 p.m ET.
-        </p>
-      </div>
-    </div>
+    </FormPage>
   );
 };
 
