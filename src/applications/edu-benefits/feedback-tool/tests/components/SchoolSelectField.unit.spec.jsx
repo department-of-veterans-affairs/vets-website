3e032dc6beeca5fc3f323d6eafbabaa308d02dc9
Chad Chotikasatien <chad.chotikasatien@va.gov.com>
2024-06-11 19:30:40 -0700

    #vfep-1489 #comment Updated unit test to check for truth value when va checkbox clicked

--- src/applications/edu-benefits/feedback-tool/tests/components/SchoolSelectField.unit.spec.jsx
+++ src/applications/edu-benefits/feedback-tool/tests/components/SchoolSelectField.unit.spec.jsx
@@ -285,15 +285,10 @@ describe('<SchoolSelectField>', () => {
       />,
     );
 
-    const clickEvent = new MouseEvent('click', {
-      bubbles: true,
-      cancelable: true,
-    });
-
     const checkbox = container.getElementsByTagName('va-checkbox')[0];
-    checkbox.__events.vaChange(clickEvent);
+    checkbox.__events.vaChange({ detail: { checked: true } });
     expect(onChange.firstCall.args[0]).to.eql({
-      'view:manualSchoolEntryChecked': undefined,
+      'view:manualSchoolEntryChecked': true,
     });
   });
 
