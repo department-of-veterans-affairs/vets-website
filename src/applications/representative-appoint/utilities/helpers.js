5650a38526cac4a12818320d505f6984625ea1af
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-06-13 17:04:40 -0400

    Refactored checkbox group to make use of checkboxGroupSchema

--- src/applications/representative-appoint/utilities/helpers.js
+++ src/applications/representative-appoint/utilities/helpers.js
@@ -1,5 +1,19 @@
+import { checkboxGroupSchema } from 'platform/forms-system/src/js/web-component-patterns';
+
 export const representativeTypeMap = {
   Attorney: 'attorney',
   'Claims Agent': 'claims agent',
   'Veterans Service Officer (VSO)': 'Veterans Service Officer (VSO)',
+};
+
+export const checkboxGroupSchemaWithReviewLabels = keys => {
+  const schema = checkboxGroupSchema(keys);
+  keys.forEach(key => {
+    schema.properties[key] = {
+      ...schema.properties[key],
+      enum: [true, false],
+      enumNames: ['Selected', 'Not selected'],
+    };
+  });
+  return schema;
 };
