33a11529a535e52407c7281eb52d491a04270c02
Eli Selkin <eli.j.selkin@gmail.com>
2024-06-13 09:46:22 -0700

    update API

--- src/applications/static-pages/health-care-manage-benefits/components/CernerCallToAction/index.js
+++ src/applications/static-pages/health-care-manage-benefits/components/CernerCallToAction/index.js
@@ -4,19 +4,18 @@ import PropTypes from 'prop-types';
 import { isEmpty, map, replace } from 'lodash';
 import * as Sentry from '@sentry/browser';
 // Relative imports.
-import environment from 'platform/utilities/environment';
 import recordEvent from 'platform/monitoring/record-event';
 import { apiRequest } from 'platform/utilities/api';
 import { getButtonType } from 'applications/static-pages/analytics/addButtonLinkListeners';
 import { getVamcSystemNameFromVhaId } from 'platform/site-wide/drupal-static-data/source-files/vamc-ehr/utils';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
+import { toggleValues } from '~/platform/site-wide/feature-toggles/selectors';
 import {
   cernerFacilitiesPropType,
   ehrDataByVhaIdPropType,
   otherFacilitiesPropType,
 } from '../../propTypes';
-import { toggleValues } from '~/platform/site-wide/feature-toggles/selectors';
 import widgetTypes from '../../../widgetTypes';
 
 function ListItem({ facilities, ehrDataByVhaId }) {
@@ -69,9 +68,15 @@ export class CernerCallToAction extends Component {
 
     try {
       // Fetch facilities and store them in state.
-      const response = await apiRequest(
-        `${environment.API_URL}/v1/facilities/va?ids=${facilityIDs.join(',')}`,
-      );
+      const response = await apiRequest('/va', {
+        apiVersion: 'facilities_api/v2',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ids: facilityIDs.join(',') }),
+      });
+
       this.setState({ facilities: response?.data, fetching: false });
 
       // Log any API errors.
