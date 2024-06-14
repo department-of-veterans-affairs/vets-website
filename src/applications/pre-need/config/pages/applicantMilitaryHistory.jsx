4381e2c4bd107429a2a0fb5a16a8eae59fab1be3
jordanBoardmanCodes <thomas.boardman@va.gov>
2024-06-14 16:28:44 -0400

    MBMS-43503 initial commit

--- src/applications/pre-need/config/pages/applicantMilitaryHistory.jsx
+++ src/applications/pre-need/config/pages/applicantMilitaryHistory.jsx
@@ -1,17 +1,25 @@
 import fullSchemaPreNeed from 'vets-json-schema/dist/40-10007-schema.json';
 
+import { merge } from 'lodash';
 import { serviceRecordsUI } from '../../utils/helpers';
+import { validateSponsorDeathDate } from '../../validation';
 
 const { veteran } = fullSchemaPreNeed.properties.application.properties;
 
 export const uiSchema = {
   application: {
-    veteran: {
-      serviceRecords: serviceRecordsUI,
-    },
-    'ui:options': {
-      customTitle: ' ',
-    },
+    // ORIGINAL STUFF BELOW
+    // veteran: {
+    //   serviceRecords: serviceRecordsUI,
+    // },
+    // 'ui:options': {
+    //   customTitle: ' ',
+    // },
+
+    // TRYING TO MAKE THIS WORK SIMILARLY TO SPONSORDEATHDATE SINCE IT PASSES IN THE UI:VALIDATIONS WHICH MAKES SENSE.
+    veteran: merge({}, serviceRecordsUI, {
+      'ui:validations': [validateSponsorDeathDate],
+    }),
   },
 };
 export const schema = {
