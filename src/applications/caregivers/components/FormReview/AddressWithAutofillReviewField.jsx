3a69dcd75bdecbd52065544b8ab7e5c76b3d537a
Matt Long <longmd@me.com>
2024-06-14 12:37:48 -0400

    Update address with autofill reviewfield

--- src/applications/caregivers/components/FormReview/AddressWithAutofillReviewField.jsx
+++ src/applications/caregivers/components/FormReview/AddressWithAutofillReviewField.jsx
@@ -1,42 +1,86 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { states } from 'platform/forms/address';
+import { replaceStrValues } from '../../utils/helpers';
+import content from '../../locales/en/content.json';
 
 export const AddressWithAutofillReviewField = ({ formData, inputLabel }) => {
   const { street, street2, city, state, postalCode, county } = formData;
   const stateLabel = states.USA.find(s => s.value === state)?.label;
+  const streetAddressLabel = replaceStrValues(
+    content['form-address-street-label'],
+    inputLabel,
+  );
 
   return (
     <>
       {formData['view:autofill'] && (
         <div className="review-row" data-testid="cg-address-autofill">
-          <dt>Use the same address as the Veteran</dt>
-          <dd>Yes</dd>
+          <dt>{content['form-address-same-as-label']}</dt>
+          <dd className="dd-privacy-hidden" data-dd-action-name="data value">
+            Yes
+          </dd>
         </div>
       )}
       <div className="review-row">
-        <dt>{inputLabel} street address</dt>
-        <dd data-testid="cg-address-street">{street}</dd>
+        <dt>{streetAddressLabel}</dt>
+        <dd
+          className="dd-privacy-hidden"
+          data-dd-action-name="data value"
+          data-testid="cg-address-street"
+        >
+          {street}
+        </dd>
       </div>
       <div className="review-row">
-        <dt>Street address line 2</dt>
-        <dd data-testid="cg-address-street2">{street2}</dd>
+        <dt>{content['form-address-street2-label']}</dt>
+        <dd
+          className="dd-privacy-hidden"
+          data-dd-action-name="data value"
+          data-testid="cg-address-street2"
+        >
+          {street2}
+        </dd>
       </div>
       <div className="review-row">
-        <dt>City</dt>
-        <dd data-testid="cg-address-city">{city}</dd>
+        <dt>{content['form-address-city-label']}</dt>
+        <dd
+          className="dd-privacy-hidden"
+          data-dd-action-name="data value"
+          data-testid="cg-address-city"
+        >
+          {city}
+        </dd>
       </div>
       <div className="review-row">
-        <dt>State</dt>
-        <dd data-testid="cg-address-state">{stateLabel}</dd>
+        <dt>{content['form-address-state-label']}</dt>
+        <dd
+          className="dd-privacy-hidden"
+          data-dd-action-name="data value"
+          data-testid="cg-address-state"
+        >
+          {stateLabel}
+        </dd>
       </div>
       <div className="review-row">
-        <dt>Postal code</dt>
-        <dd data-testid="cg-address-postalcode">{postalCode}</dd>
+        <dt>{content['form-address-postalCode-label']}</dt>
+        <dd
+          className="dd-privacy-hidden"
+          data-dd-action-name="data value"
+          data-testid="cg-address-postalcode"
+        >
+          {postalCode}
+        </dd>
       </div>
       <div className="review-row">
-        <dt>County</dt>
-        <dd data-testid="cg-address-county">{county}</dd>
+        <dt>{content['form-address-county-label']}</dt>
+        <dd
+          className="dd-privacy-hidden"
+          data-dd-action-name="data value"
+          data-testid="cg-address-county"
+        >
+          {county}
+        </dd>
       </div>
     </>
   );
