ddfbd44b1e22bf3e84de40e33724fed6e09fc9b8
Brad Bergeron <bergeron.bradley@gmail.com>
2024-06-14 14:18:14 -0400

    Adds extra linebreak on description list

--- src/applications/my-education-benefits/config/form.js
+++ src/applications/my-education-benefits/config/form.js
@@ -1604,6 +1604,7 @@ const formConfig = {
               'ui:description': (
                 <div>
                   <br />
+                  <br />
                   <ul>
                     <li>
                       You can select a date up to one year in the past. We may
