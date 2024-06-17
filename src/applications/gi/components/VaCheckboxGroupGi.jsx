2c7c7124c7eb1b964245f3b207c96c862c13de84
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2024-06-17 16:53:58 -0500

    VFEP-1203 SMF - Replace checkboxes to V3 - Front-end

---
+++ src/applications/gi/components/VaCheckboxGroupGi.jsx
@@ -0,0 +1,98 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import _ from 'lodash';
+import { VaCheckbox } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import { handleScrollOnInputFocus } from '../utils/helpers';
+
+const VACheckboxGroupGi = ({
+  errorMessage,
+  label,
+  onChange,
+  onFocus,
+  options,
+  row = false,
+  colNum = null,
+  labelMargin = '1p5',
+  className,
+  // focusOnFirstInput,
+  // setIsCleared,
+}) => {
+  const inputId = _.uniqueId('checkbox-group-');
+
+  // If Row is true then style checkboxes in a row within a container
+  // with 16px margin to the left and right of the label
+  const checkBoxStyleGrid = row ? 'vads-l-grid-container' : '';
+  // const checkBoxStyleRow = row ? 'vads-l-row' : '';
+  const checkBoxStyleColOption =
+    colNum != null ? `vads-l-col--${colNum}` : 'vads-l-col';
+  const checkBoxStyleCol = row ? checkBoxStyleColOption : '';
+  const checkBoxLabelMargin = row ? `vads-u-margin-right--${labelMargin}` : '';
+
+  const renderOptions = () => {
+    const displayOptions = Array.isArray(options) ? options : [];
+    return displayOptions.map((option, index) => {
+      const { checked, optionLabel, name, learnMore } = option;
+
+      // eslint-disable-next-line no-console
+      console.log(`------------optionLabel=${optionLabel}`);
+      return (
+        <div key={index} className={`${checkBoxStyleCol} ${className}`}>
+          <VaCheckbox
+            key={optionLabel}
+            data-index={index}
+            label={optionLabel}
+            label-header-level="3"
+            checked={checked}
+            onFocus={() => onFocus(`${inputId}-${index}`)}
+            onVaChange={target => {
+              onChange(target, name);
+            }}
+            class="vads-l-col--1p5 va-checkbox-horizontal"
+            uswds
+          />
+          <label
+            className={`gi-checkbox-label ${checkBoxLabelMargin}`}
+            aria-hidden
+          >
+            {learnMore}
+          </label>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div className={errorMessage ? 'usa-input-error' : ''}>
+      <fieldset>
+        <div className={checkBoxStyleGrid}>
+          <span id={`${inputId}-legend`} className="gibct-legend">
+            {label}
+          </span>
+          {renderOptions()}
+        </div>
+      </fieldset>
+    </div>
+  );
+};
+
+VACheckboxGroupGi.propTypes = {
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  options: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.shape({
+        label: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+        checked: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+        name: PropTypes.oneOfType([PropTypes.string, PropTypes.string]),
+      }),
+    ]),
+  ).isRequired,
+  onChange: PropTypes.func.isRequired,
+  onFocus: PropTypes.func,
+  row: PropTypes.bool,
+};
+
+VACheckboxGroupGi.defaultProps = {
+  onFocus: handleScrollOnInputFocus,
+};
+
+export default VACheckboxGroupGi;
