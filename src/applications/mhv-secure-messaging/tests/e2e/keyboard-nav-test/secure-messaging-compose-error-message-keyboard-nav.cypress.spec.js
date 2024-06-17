0d11aad6e139be692887565882a9da2b461e13f6
pjhill <52456633+pjhill@users.noreply.github.com>
2024-06-17 15:51:43 -0400

    merge tests in from main

--- src/applications/mhv-secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-compose-error-message-keyboard-nav.cypress.spec.js
+++ src/applications/mhv-secure-messaging/tests/e2e/keyboard-nav-test/secure-messaging-compose-error-message-keyboard-nav.cypress.spec.js
@@ -1,10 +1,10 @@
 import SecureMessagingSite from '../sm_site/SecureMessagingSite';
 import PatientInboxPage from '../pages/PatientInboxPage';
 import PatientComposePage from '../pages/PatientComposePage';
+import FolderLoadPage from '../pages/FolderLoadPage';
 import { AXE_CONTEXT, Data } from '../utils/constants';
 
-describe.skip('Secure Messaging Compose Errors Keyboard Nav', () => {
-  // const composePage = new PatientComposePage();
+describe('Secure Messaging Compose Errors Keyboard Nav', () => {
   const site = new SecureMessagingSite();
   beforeEach(() => {
     site.login();
@@ -20,33 +20,21 @@ describe.skip('Secure Messaging Compose Errors Keyboard Nav', () => {
     });
     PatientComposePage.pushSendMessageWithKeyboardPress();
     cy.injectAxe();
-    cy.axeCheck(AXE_CONTEXT, {
-      rules: {
-        'aria-required-children': {
-          enabled: false,
-        },
-      },
-    });
-    // PatientComposePage.verifyFocusOnErrorMessage(Data.PLEASE_SELECT_RECIPIENT);
+    cy.axeCheck(AXE_CONTEXT);
+    PatientComposePage.verifyFocusOnErrorMessage(Data.PLEASE_SELECT_RECIPIENT);
     PatientComposePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
-    PatientComposePage.selectSideBarMenuOption('Inbox');
+    FolderLoadPage.backToInbox();
     PatientComposePage.clickOnDeleteDraftButton();
   });
 
   it('focus on error message for empty category', () => {
     PatientComposePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
     PatientComposePage.pushSendMessageWithKeyboardPress();
-    // PatientComposePage.verifyFocusOnErrorMessage(Data.PLEASE_SELECT_CATEGORY);
+    PatientComposePage.verifyFocusOnErrorMessage(Data.PLEASE_SELECT_CATEGORY);
     cy.injectAxe();
-    cy.axeCheck(AXE_CONTEXT, {
-      rules: {
-        'aria-required-children': {
-          enabled: false,
-        },
-      },
-    });
+    cy.axeCheck(AXE_CONTEXT);
     PatientComposePage.selectCategory();
-    PatientComposePage.selectSideBarMenuOption('Inbox');
+    FolderLoadPage.backToInbox();
     PatientComposePage.clickOnDeleteDraftButton();
   });
 
@@ -54,22 +42,17 @@ describe.skip('Secure Messaging Compose Errors Keyboard Nav', () => {
     PatientComposePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
     PatientComposePage.selectCategory();
     PatientComposePage.pushSendMessageWithKeyboardPress();
-    // PatientComposePage.verifyFocusOnErrorMessage(Data.SUBJECT_CANNOT_BLANK);
+    PatientComposePage.verifyFocusOnErrorMessage(Data.SUBJECT_CANNOT_BLANK);
     cy.injectAxe();
-    cy.axeCheck(AXE_CONTEXT, {
-      rules: {
-        'aria-required-children': {
-          enabled: false,
-        },
-      },
-    });
+    cy.axeCheck(AXE_CONTEXT);
     PatientComposePage.getMessageSubjectField().type(
       Data.TEST_MESSAGE_SUBJECT,
       { force: true },
     );
-    PatientComposePage.selectSideBarMenuOption('Inbox');
+    FolderLoadPage.backToInbox();
     PatientComposePage.clickOnDeleteDraftButton();
   });
+
   it('focus on error message for empty message body', () => {
     PatientComposePage.selectRecipient('CAMRY_PCMM RELATIONSHIP_05092022_SLC4');
     PatientComposePage.selectCategory();
@@ -77,17 +60,11 @@ describe.skip('Secure Messaging Compose Errors Keyboard Nav', () => {
       force: true,
     });
     PatientComposePage.pushSendMessageWithKeyboardPress();
-    // PatientComposePage.verifyFocusOnErrorMessage(Data.BODY_CANNOT_BLANK);
+    PatientComposePage.verifyFocusOnErrorMessage(Data.BODY_CANNOT_BLANK);
     cy.injectAxe();
-    cy.axeCheck(AXE_CONTEXT, {
-      rules: {
-        'aria-required-children': {
-          enabled: false,
-        },
-      },
-    });
+    cy.axeCheck(AXE_CONTEXT);
     PatientComposePage.getMessageBodyField().type(Data.TEST_MESSAGE_BODY);
-    PatientComposePage.selectSideBarMenuOption('Inbox');
+    FolderLoadPage.backToInbox();
     PatientComposePage.clickOnDeleteDraftButton();
   });
 });
