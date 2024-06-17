a1463b3d2c99c263c623742fc47b53e6e1e35494
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2024-06-17 12:31:15 -0500

    VFEP-1502 Form 22-1990 Remove benefits to relinquish under Benefits eligibility - Front-end (#30333)
    * VFEP-1502 remove benefits relinquished.
    
    * VFEP-1502 Code cleanup. Remove tests that are no longer needed.
    
    * VFEP-1502 code cleanup.

--- src/applications/edu-benefits/1990/containers/ConfirmationPage.jsx
+++ src/applications/edu-benefits/1990/containers/ConfirmationPage.jsx
@@ -1,11 +1,9 @@
-import get from 'platform/utilities/data/get';
 import React from 'react';
 import { connect } from 'react-redux';
 
 import { focusElement } from 'platform/utilities/ui';
 import scrollToTop from 'platform/utilities/ui/scrollToTop';
 import { getListOfBenefits } from '../../utils/helpers';
-import { benefitsRelinquishmentLabels } from '../helpers';
 import { ConfirmationPageContent } from '../../components/ConfirmationPageContent';
 
 class ConfirmationPage extends React.Component {
@@ -28,18 +26,14 @@ class ConfirmationPage extends React.Component {
   }
 
   render() {
-    const form = this.props.form;
+    const { form } = this.props;
     const { submission, formId } = form;
     const benefits = form.data['view:selectedBenefits'];
-    const benefitsRelinquished = get(
-      'data.view:benefitsRelinquishedContainer.benefitsRelinquished',
-      form,
-    );
 
     return (
       <ConfirmationPageContent
         claimInfoListItems={[
-          <li key={'benefit'}>
+          <li key="benefit">
             <strong>Benefit claimed</strong>
             <br />
             {this.makeList(getListOfBenefits(benefits))}
@@ -48,9 +42,6 @@ class ConfirmationPage extends React.Component {
                 <span>
                   <i>Relinquished:</i>
                 </span>
-                {this.makeList([
-                  benefitsRelinquishmentLabels[benefitsRelinquished],
-                ])}
               </div>
             )}
           </li>,
