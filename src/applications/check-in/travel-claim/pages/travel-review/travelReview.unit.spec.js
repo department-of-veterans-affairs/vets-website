397603a28fb17840d2727e584df47b8c94929e6d
Brian Seek <brian.seek@civicactions.com>
2024-06-18 15:18:50 -0700

    updated unit test.

--- src/applications/check-in/travel-claim/pages/travel-review/travelReview.unit.spec.js
+++ src/applications/check-in/travel-claim/pages/travel-review/travelReview.unit.spec.js
@@ -19,6 +19,7 @@ describe('travel-review', () => {
         startTime: '2021-08-19T13:56:31Z',
         clinicStopCodeName: 'Endoscopy',
         timezone: 'America/Los_Angeles',
+        facility: 'Test Facility',
       },
     };
     it('renders page details', () => {
@@ -29,7 +30,7 @@ describe('travel-review', () => {
       );
       expect(component.getByTestId('review-body')).to.exist;
       expect(component.getByTestId('claim-info')).to.contain.text(
-        'Mileage-only reimbursement for your  Endoscopy appointment on August 19, 2021, 6:56 a.m.',
+        'Mileage-only reimbursement for your appointment at Test Facility on August 19, 2021, 6:56 a.m.',
       );
     });
   });
