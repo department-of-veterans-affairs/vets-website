9dd82ff93a43db63996365b679bbaec1420989ca
Matthew Kerns <matthewkerns@Matthew-Kerns.local>
2024-06-18 09:24:35 -0400

    Added paragraph to SIP Alert

--- src/applications/ezr/components/IntroductionPage/SaveInProgressInfo.jsx
+++ src/applications/ezr/components/IntroductionPage/SaveInProgressInfo.jsx
@@ -9,6 +9,7 @@ import EnrollmentStatusAlert from '../FormAlerts/EnrollmentStatusAlert';
 import VerifiedPrefillAlert from '../FormAlerts/VerifiedPrefillAlert';
 import PreferredFacilityAlert from '../FormAlerts/PreferredFacilityAlert';
 import FinancialMeansTestWarning from '../FormAlerts/FinancialStatusWarning';
+import UpdatedFormAlertDescription from '../FormDescriptions/UpdatedFormAlertDescription';
 import content from '../../locales/en/content.json';
 
 const SaveInProgressInfo = ({ formConfig, pageList }) => {
@@ -42,7 +43,11 @@ const SaveInProgressInfo = ({ formConfig, pageList }) => {
     devOnly: { forceShowFormControls: true },
   };
 
-  const sipIntro = <SaveInProgressIntro {...sipProps} />;
+  const sipIntro = (
+    <SaveInProgressIntro {...sipProps}>
+      <UpdatedFormAlertDescription />
+    </SaveInProgressIntro>
+  );
 
   // set the correct alert to render based on enrollment status
   const LoggedInAlertToRender = () => {
