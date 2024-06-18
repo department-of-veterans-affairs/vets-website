542349182cc13d1be82eae72fc23b1719af5a2e8
pjhill <52456633+pjhill@users.noreply.github.com>
2024-06-18 11:24:01 -0400

    fix typo in timeout

--- src/applications/edu-benefits/feedback-tool/tests/00.feedback-tool.cypress.spec.js
+++ src/applications/edu-benefits/feedback-tool/tests/00.feedback-tool.cypress.spec.js
@@ -92,7 +92,7 @@ describe('Feedback Tool Test', () => {
 
     cy.get(
       'input[name="root_educationDetails_school_view:manualSchoolEntry_name"]',
-      { timout: Timeouts.slow },
+      { timeout: Timeouts.slow },
     ).should('be.visible');
     cy.fill(
       'input[name="root_educationDetails_school_view:manualSchoolEntry_name"]',
