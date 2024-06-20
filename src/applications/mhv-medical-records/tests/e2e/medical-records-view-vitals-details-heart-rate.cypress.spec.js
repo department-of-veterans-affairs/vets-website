78e7a1a815d5b40b04ce5ff613164c46cb7cef06
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2024-06-20 11:44:55 -0700

    Update medical-records-view-vitals-details-heart-rate.cypress.spec.js

--- src/applications/mhv-medical-records/tests/e2e/medical-records-view-vitals-details-heart-rate.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-view-vitals-details-heart-rate.cypress.spec.js
@@ -1,6 +1,8 @@
+import moment from 'moment';
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
 import VitalsListPage from './pages/VitalsListPage';
 import VitalsDetailsPage from './pages/VitalsDetailsPage';
+import defaultVitals from '../fixtures/vitals.json';
 
 describe('Medical Records Vitals Details Page', () => {
   const site = new MedicalRecordsSite();
@@ -18,36 +20,62 @@ describe('Medical Records Vitals Details Page', () => {
     // verify first reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       0,
-      'October',
-      '70 beats per minute',
-      'ADTP BURNETT',
+      // 'October',
+      moment
+        .parseZone(defaultVitals.entry[4].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '70 beats per minute',
+      `${defaultVitals.entry[4].resource.valueQuantity.value} beats per minute`,
+      // 'ADTP BURNETT', // location
+      defaultVitals.entry[4].resource.contained[0].name,
       'None noted',
     );
 
     // verify second reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       1,
-      'August',
-      '85 beats per minute',
-      '23 HOUR OBSERVATION',
+      // 'August',
+      moment
+        .parseZone(defaultVitals.entry[14].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '85 beats per minute',
+      `${
+        defaultVitals.entry[14].resource.valueQuantity.value
+      } beats per minute`,
+      // '23 HOUR OBSERVATION', // location
+      defaultVitals.entry[14].resource.contained[0].name,
       'None noted',
     );
 
     // verify third reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       2,
-      'August',
-      '90 beats per minute',
-      'ADMISSIONS (LOC)',
+      // 'August',
+      moment
+        .parseZone(defaultVitals.entry[24].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '90 beats per minute',
+      `${
+        defaultVitals.entry[24].resource.valueQuantity.value
+      } beats per minute`,
+      // 'ADMISSIONS (LOC)', // location
+      defaultVitals.entry[24].resource.contained[0].name,
       'None noted',
     );
 
     // verify fourth reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       3,
-      'May',
-      '60 beats per minute',
-      'ADTP SCREENING',
+      // 'May',
+      moment
+        .parseZone(defaultVitals.entry[34].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '60 beats per minute',
+      `${
+        defaultVitals.entry[34].resource.valueQuantity.value
+      } beats per minute`,
+      // 'ADTP SCREENING', // location
+      defaultVitals.entry[34].resource.contained[0].name,
       'None noted',
     );
     // Axe check
