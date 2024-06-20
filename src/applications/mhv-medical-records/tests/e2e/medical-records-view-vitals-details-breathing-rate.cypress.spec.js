2bfbab9b87cb21d96cba7268200be155c992d59a
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2024-06-20 12:31:26 -0700

    Update medical-records-view-vitals-details-breathing-rate.cypress.spec.js

--- src/applications/mhv-medical-records/tests/e2e/medical-records-view-vitals-details-breathing-rate.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-view-vitals-details-breathing-rate.cypress.spec.js
@@ -1,6 +1,8 @@
+import moment from 'moment';
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
 import VitalsListPage from './pages/VitalsListPage';
 import VitalsDetailsPage from './pages/VitalsDetailsPage';
+import defaultVitals from '../fixtures/vitals.json';
 
 describe('Medical Records Vitals Details Page', () => {
   const site = new MedicalRecordsSite();
@@ -18,36 +20,64 @@ describe('Medical Records Vitals Details Page', () => {
     // verify first reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       0,
-      'October',
-      '15 breaths per minute',
-      'ADTP BURNETT',
+      // 'October',
+      moment
+        .parseZone(defaultVitals.entry[7].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '15 breaths per minute',
+      `${
+        defaultVitals.entry[7].resource.valueQuantity.value
+      } breaths per minute`,
+      // 'ADTP BURNETT', // location
+      defaultVitals.entry[4].resource.contained[0].name,
       'None noted',
     );
 
     // verify second reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       1,
-      'August',
-      '15 breaths per minute',
-      '23 HOUR OBSERVATION',
+      // 'August',
+      moment
+        .parseZone(defaultVitals.entry[17].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '15 breaths per minute',
+      `${
+        defaultVitals.entry[17].resource.valueQuantity.value
+      } breaths per minute`,
+      // '23 HOUR OBSERVATION', // location
+      defaultVitals.entry[17].resource.contained[0].name,
       'None noted',
     );
 
     // verify third reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       2,
-      'August',
-      '12 breaths per minute',
-      'ADMISSIONS (LOC)',
+      // 'August',
+      moment
+        .parseZone(defaultVitals.entry[27].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '12 breaths per minute',
+      `${
+        defaultVitals.entry[27].resource.valueQuantity.value
+      } breaths per minute`,
+      // 'ADMISSIONS (LOC)', // location
+      defaultVitals.entry[27].resource.contained[0].name,
       'None noted',
     );
 
     // verify fourth reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       3,
-      'May',
-      '12 breaths per minute',
-      'ADTP SCREENING',
+      // 'May',
+      moment
+        .parseZone(defaultVitals.entry[37].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '12 breaths per minute',
+      `${
+        defaultVitals.entry[37].resource.valueQuantity.value
+      } breaths per minute`,
+      // 'ADTP SCREENING', // location
+      defaultVitals.entry[37].resource.contained[0].name,
       'None noted',
     );
 
