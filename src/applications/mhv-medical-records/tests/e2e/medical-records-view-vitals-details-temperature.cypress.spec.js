2d73f12b0a9e0dd15e618349331e146e0eda3341
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2024-06-20 14:03:56 -0700

    Update medical-records-view-vitals-details-temperature.cypress.spec.js

--- src/applications/mhv-medical-records/tests/e2e/medical-records-view-vitals-details-temperature.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-view-vitals-details-temperature.cypress.spec.js
@@ -1,6 +1,8 @@
+import moment from 'moment';
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
 import VitalsListPage from './pages/VitalsListPage';
 import VitalsDetailsPage from './pages/VitalsDetailsPage';
+import defaultVitals from '../fixtures/vitals.json';
 
 describe('Medical Records Vitals Details Page', () => {
   const site = new MedicalRecordsSite();
@@ -18,36 +20,56 @@ describe('Medical Records Vitals Details Page', () => {
     // verify first reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       0,
-      'October',
-      '99 °F',
-      'ADTP BURNETT',
+      // 'October',
+      moment
+        .parseZone(defaultVitals.entry[8].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '99 °F',
+      `${defaultVitals.entry[8].resource.valueQuantity.value} °F`,
+      // 'ADTP BURNETT', // location
+      defaultVitals.entry[8].resource.contained[0].name,
       'None noted',
     );
 
     // verify second reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       1,
-      'August',
-      '98.5 °F',
-      '23 HOUR OBSERVATION',
+      // 'August',
+      moment
+        .parseZone(defaultVitals.entry[18].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '98.5 °F',
+      `${defaultVitals.entry[18].resource.valueQuantity.value} °F`,
+      // '23 HOUR OBSERVATION', // location
+      defaultVitals.entry[18].resource.contained[0].name,
       'None noted',
     );
 
     // verify third reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       2,
-      'August',
-      '98.5 °F',
-      'ADMISSIONS (LOC)',
+      // 'August',
+      moment
+        .parseZone(defaultVitals.entry[28].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '98.5 °F',
+      `${defaultVitals.entry[28].resource.valueQuantity.value} °F`,
+      // 'ADMISSIONS (LOC)', // location
+      defaultVitals.entry[28].resource.contained[0].name,
       'None noted',
     );
 
     // verify fourth reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       3,
-      'May',
-      '98.5 °F',
-      'ADTP SCREENING',
+      // 'May',
+      moment
+        .parseZone(defaultVitals.entry[38].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '98.5 °F',
+      `${defaultVitals.entry[38].resource.valueQuantity.value} °F`,
+      // 'ADTP SCREENING', // location
+      defaultVitals.entry[38].resource.contained[0].name,
       'None noted',
     );
 
