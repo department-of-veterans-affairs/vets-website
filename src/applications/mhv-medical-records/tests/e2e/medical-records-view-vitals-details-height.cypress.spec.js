1b4b47b1bde03bfc779aaf398ef602a64f8693e8
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2024-06-20 14:44:22 -0700

    updated vitals test for weight and height

--- src/applications/mhv-medical-records/tests/e2e/medical-records-view-vitals-details-height.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-view-vitals-details-height.cypress.spec.js
@@ -1,6 +1,8 @@
+import moment from 'moment';
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
 import VitalsListPage from './pages/VitalsListPage';
 import VitalsDetailsPage from './pages/VitalsDetailsPage';
+import defaultVitals from '../fixtures/vitals.json';
 
 describe('Medical Records Vitals Details Page', () => {
   const site = new MedicalRecordsSite();
@@ -18,36 +20,56 @@ describe('Medical Records Vitals Details Page', () => {
     // verify first reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       0,
-      'October',
-      '70 inches',
-      'ADTP BURNETT',
+      // 'October',
+      moment
+        .parseZone(defaultVitals.entry[3].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '70 inches',
+      `${defaultVitals.entry[3].resource.valueQuantity.value} inches`,
+      // 'ADTP BURNETT', // location
+      defaultVitals.entry[3].resource.contained[0].name,
       'None noted',
     );
 
     // verify second reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       1,
-      'August',
-      '72 inches',
-      '23 HOUR OBSERVATION',
+      // 'August',
+      moment
+        .parseZone(defaultVitals.entry[13].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '72 inches',
+      `${defaultVitals.entry[13].resource.valueQuantity.value} inches`,
+      // '23 HOUR OBSERVATION', // location
+      defaultVitals.entry[13].resource.contained[0].name,
       'None noted',
     );
 
     // verify third reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       2,
-      'August',
-      '70 inches',
-      'ADMISSIONS (LOC)',
+      // 'August',
+      moment
+        .parseZone(defaultVitals.entry[23].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '70 inches',
+      `${defaultVitals.entry[23].resource.valueQuantity.value} inches`,
+      // 'ADMISSIONS (LOC)', // location
+      defaultVitals.entry[23].resource.contained[0].name,
       'None noted',
     );
 
     // verify fourth reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       3,
-      'May',
-      '70 inches',
-      'ADTP SCREENING',
+      // 'May',
+      moment
+        .parseZone(defaultVitals.entry[33].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '70 inches',
+      `${defaultVitals.entry[33].resource.valueQuantity.value} inches`,
+      // 'ADTP SCREENING', // location
+      defaultVitals.entry[33].resource.contained[0].name,
       'None noted',
     );
 
--- src/applications/mhv-medical-records/tests/e2e/medical-records-view-vitals-details-weight.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-view-vitals-details-weight.cypress.spec.js
@@ -1,6 +1,8 @@
+import moment from 'moment';
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
 import VitalsListPage from './pages/VitalsListPage';
 import VitalsDetailsPage from './pages/VitalsDetailsPage';
+import defaultVitals from '../fixtures/vitals.json';
 
 describe('Medical Records Vitals Details Page', () => {
   const site = new MedicalRecordsSite();
@@ -18,36 +20,56 @@ describe('Medical Records Vitals Details Page', () => {
     // verify first reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       0,
-      'October',
-      '185 pounds',
-      'ADTP BURNETT',
+      // 'October',
+      moment
+        .parseZone(defaultVitals.entry[9].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '185 pounds',
+      `${defaultVitals.entry[9].resource.valueQuantity.value} pounds`,
+      // 'ADTP BURNETT', // location
+      defaultVitals.entry[9].resource.contained[0].name,
       'None noted',
     );
 
     // verify second reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       1,
-      'August',
-      '200 pounds',
-      '23 HOUR OBSERVATION',
+      // 'August',
+      moment
+        .parseZone(defaultVitals.entry[19].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '200 pounds',
+      `${defaultVitals.entry[19].resource.valueQuantity.value} pounds`,
+      // '23 HOUR OBSERVATION', // location
+      defaultVitals.entry[19].resource.contained[0].name,
       'None noted',
     );
 
     // verify third reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       2,
-      'August',
-      '190 pounds',
-      'ADMISSIONS (LOC)',
+      // 'August',
+      moment
+        .parseZone(defaultVitals.entry[29].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '190 pounds',
+      `${defaultVitals.entry[29].resource.valueQuantity.value} pounds`,
+      // 'ADMISSIONS (LOC)', // location
+      defaultVitals.entry[29].resource.contained[0].name,
       'None noted',
     );
 
     // verify fourth reading
     VitalsDetailsPage.verifyVitalReadingByIndex(
       3,
-      'May',
-      '185 pounds',
-      'ADTP SCREENING',
+      // 'May',
+      moment
+        .parseZone(defaultVitals.entry[39].resource.effectiveDateTime)
+        .format('MMMM D, YYYY, h:mm'),
+      // '185 pounds',
+      `${defaultVitals.entry[39].resource.valueQuantity.value} pounds`,
+      // 'ADTP SCREENING', // location
+      defaultVitals.entry[39].resource.contained[0].name,
       'None noted',
     );
 
