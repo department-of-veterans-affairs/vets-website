5ed88e198a9d760d1fdfbece9bc44bd5deca6ca7
William Phelps <williamphelps13@gmail.com>
2024-06-24 13:57:29 -0700

    Update Your practicing information page to have aligned title and path

--- src/applications/accreditation/21a/pages/05-law-practice-information-chapter/jurisdictionsPages.js
+++ src/applications/accreditation/21a/pages/05-law-practice-information-chapter/jurisdictionsPages.js
@@ -85,8 +85,8 @@ const jurisdictionsPages = arrayBuilderPages(
   arrayBuilderOptions,
   pageBuilder => ({
     jurisdictions: pageBuilder.introPage({
-      title: 'Jurisdictions',
-      path: 'jurisdictions',
+      title: 'Your practicing information',
+      path: 'your-practicing-information',
       uiSchema: introPage.uiSchema,
       schema: introPage.schema,
     }),
