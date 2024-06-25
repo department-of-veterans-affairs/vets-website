2876e8d5f7a007fee9016cc5103ec218519e2d89
Eric Tillberg <Thrillberg@users.noreply.github.com>
2024-06-25 13:45:01 -0400

    Fixes the back link on confirmation page of form upload flow

--- src/applications/simple-forms/form-upload/containers/ConfirmationPage.jsx
+++ src/applications/simple-forms/form-upload/containers/ConfirmationPage.jsx
@@ -64,9 +64,26 @@ const ConfirmationPage = () => {
           text="Print this page"
         />
       </div>
-      <a className="vads-c-action-link--green vads-u-margin-y--2" href="/">
-        Go back to VA.gov
-      </a>
+      <div
+        className="action-bar-arrow vads-u-margin-bottom--4"
+        style={{ maxWidth: '60%' }}
+      >
+        <div className="vads-u-background-color--primary vads-u-padding--1">
+          <a className="vads-c-action-link--white" href="/">
+            Go back to VA.gov
+          </a>
+        </div>
+      </div>
+      <div className="need-help-footer">
+        <h2 className="vads-u-padding-bottom--0p5 vads-u-font-size--h3 vads-u-border-bottom--2px vads-u-border-color--primary">
+          Need help?
+        </h2>
+        <p>
+          You can call us at <va-telephone contact="8772228387" /> (
+          <va-telephone contact="8008778339" tty />
+          ). We’re here Monday through Friday, 8:00 a.m to 9:00 p.m ET.
+        </p>
+      </div>
     </div>
   );
 };
