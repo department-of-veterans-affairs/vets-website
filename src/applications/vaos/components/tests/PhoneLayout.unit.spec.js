3cb82228bc8885f7a19cd3bae529db13fad820cf
Ryan Shaw <ryan.shaw@adhocteam.us>
2024-06-24 22:32:02 -0700

    Updates phone layout unit test
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/components/tests/PhoneLayout.unit.spec.js
+++ src/applications/vaos/components/tests/PhoneLayout.unit.spec.js
@@ -181,7 +181,7 @@ describe('VAOS Component: PhoneLayout', () => {
 
       expect(screen.getByText(/Clinic:/i));
       expect(screen.getByText(/Clinic 1/i));
-      expect(screen.getByText(/Clinic phone:/i));
+      expect(screen.getByText(/Phone:/i));
       expect(
         screen.container.querySelector('va-telephone[contact="307-778-7550"]'),
       ).to.be.ok;
