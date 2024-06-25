9abb3bf67018100a911a802461a4dbc5e4302006
Ryan Shaw <ryan.shaw@adhocteam.us>
2024-06-25 08:25:15 -0700

    Fixes phone heading in claim exam layout unit test
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/components/tests/ClaimExamLayout.unit.spec.js
+++ src/applications/vaos/components/tests/ClaimExamLayout.unit.spec.js
@@ -94,7 +94,7 @@ describe('VAOS Component: ClaimExamLayout', () => {
 
       expect(screen.getByText(/Clinic:/i));
       expect(screen.getByText(/Clinic 1/i));
-      expect(screen.getByText(/Clinic phone:/i));
+      expect(screen.getByText(/Phone:/i));
       expect(
         screen.container.querySelector('va-telephone[contact="307-778-7550"]'),
       ).to.be.ok;
