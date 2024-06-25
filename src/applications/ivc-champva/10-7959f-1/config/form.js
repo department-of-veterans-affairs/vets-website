54d749c132f3ccc6e41b7002f9db74546b0b020b
Rachel <rachele.eiting@copilot.llc>
2024-06-25 10:17:26 -0400

    removed unneeded comma

--- src/applications/ivc-champva/10-7959f-1/config/form.js
+++ src/applications/ivc-champva/10-7959f-1/config/form.js
@@ -236,7 +236,7 @@ const formConfig = {
           uiSchema: {
             ...titleUI(
               'Phone and email address',
-              'For foreign numbers, add the country code, so we can reach you if there are questions about this form.',
+              'For foreign numbers, add the country code so we can reach you if there are questions about this form.',
             ),
             messageAriaDescribedby:
               'Please include this information so that we can contact you with questions or updates.',
