4d3c7ccded892845f3f9db45ef96ea580b123d13
Ryan Smith <ryan.smith@adhocteam.us>
2024-06-26 16:00:12 -0400

    Update test

--- src/platform/site-wide/user-nav/tests/components/PageNotFound.unit.spec.jsx
+++ src/platform/site-wide/user-nav/tests/components/PageNotFound.unit.spec.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 import { expect } from 'chai';
 import sinon from 'sinon';
-import PageNotFound, {
-  notFoundHeading,
-  notFoundTitle,
-} from '../../components/PageNotFound';
+
+import { $ } from '@department-of-veterans-affairs/platform-forms-system/ui';
+
+import PageNotFound, { notFoundTitle } from '../../components/PageNotFound';
 
 describe('PageNotFound Component', () => {
   it('renders', async () => {
     const recordEvent = sinon.spy();
     const props = { recordEvent };
-    const { getByRole } = render(<PageNotFound {...props} />);
-    const heading = getByRole('heading', { name: notFoundHeading });
+    // const { getByRole } = render(<PageNotFound {...props} />);
+    // const heading = getByRole('heading', { name: notFoundHeading });
+    const { container } = render(<PageNotFound {...props} />);
+    const heading = $('h1', container);
     expect(heading).to.exist;
     expect(document.activeElement).to.eq(heading);
     expect(document.title).to.eql(notFoundTitle);
