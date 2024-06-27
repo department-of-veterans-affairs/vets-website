dae695af8a5b808284d3497647c4fc67ccb9a441
Peri McLaren <peri.mclaren@adhocteam.us>
2024-06-27 15:55:23 -0600

    update recent activity and helpers

--- src/applications/claims-status/components/claim-status-tab/RecentActivity.jsx
+++ src/applications/claims-status/components/claim-status-tab/RecentActivity.jsx
@@ -3,76 +3,172 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom-v5-compat';
 import { VaPagination } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import moment from 'moment';
+import { useFeatureToggle } from '~/platform/utilities/feature-toggles';
 
 import { ITEMS_PER_PAGE } from '../../constants';
-import { buildDateFormatter } from '../../utils/helpers';
-
-const getOldestDocumentDate = item => {
-  const arrDocumentDates = item.documents.map(document => document.uploadDate);
-  return arrDocumentDates.sort()[0]; // Tried to do Math.min() here and it was erroring out
-};
-
-const getTrackedItemDateFromStatus = item => {
-  switch (item.status) {
-    case 'NEEDED_FROM_YOU':
-    case 'NEEDED_FROM_OTHERS':
-      return item.requestedDate;
-    case 'NO_LONGER_REQUIRED':
-      return item.closedDate;
-    case 'SUBMITTED_AWAITING_REVIEW':
-      return getOldestDocumentDate(item);
-    case 'INITIAL_REVIEW_COMPLETE':
-    case 'ACCEPTED':
-      return item.receivedDate;
-    default:
-      return item.requestedDate;
-  }
-};
-
-const getTrackedItemDescription = item => {
-  switch (item.status) {
-    case 'NEEDED_FROM_YOU':
-    case 'NEEDED_FROM_OTHERS':
-      return `We opened a request for "${item.displayName}"`;
-    case 'NO_LONGER_REQUIRED':
-      return `We closed a request for "${item.displayName}"`;
-    case 'SUBMITTED_AWAITING_REVIEW':
-      return `We received your document(s) for "${item.displayName}"`;
-    case 'INITIAL_REVIEW_COMPLETE':
-    case 'ACCEPTED':
-      return `We completed a review for "${item.displayName}"`;
-    default:
-      return 'There was an update to this item';
-  }
-};
-
-const generateTrackedItems = claim => {
-  const { trackedItems } = claim.attributes;
-
-  return trackedItems.map(item => ({
-    id: item.id,
-    date: getTrackedItemDateFromStatus(item),
-    description: getTrackedItemDescription(item),
-    displayName: item.displayName,
-    status: item.status,
-    type: 'tracked_item',
-  }));
-};
-
-const getSortedItems = claim => {
-  // Get items from trackedItems and claimPhaseDates
-  const trackedItems = generateTrackedItems(claim);
-  const phaseItems = [];
-  const items = [...trackedItems, ...phaseItems];
-
-  return items.sort((item1, item2) => {
-    return moment(item2.date) - moment(item1.date);
-  });
-};
+import {
+  buildDateFormatter,
+  getItemDate,
+  getPhaseItemText,
+  getStatusMap,
+} from '../../utils/helpers';
 
 export default function RecentActivity({ claim }) {
+  const { TOGGLE_NAMES, useToggleValue } = useFeatureToggle();
+  const cstClaimPhasesEnabled = useToggleValue(TOGGLE_NAMES.cstClaimPhases);
+
+  const getOldestDocumentDate = item => {
+    const arrDocumentDates = item.documents.map(
+      document => document.uploadDate,
+    );
+    return arrDocumentDates.sort()[0]; // Tried to do Math.min() here and it was erroring out
+  };
+
+  const getTrackedItemDateFromStatus = item => {
+    switch (item.status) {
+      case 'NEEDED_FROM_YOU':
+      case 'NEEDED_FROM_OTHERS':
+        return item.requestedDate;
+      case 'NO_LONGER_REQUIRED':
+        return item.closedDate;
+      case 'SUBMITTED_AWAITING_REVIEW':
+        return getOldestDocumentDate(item);
+      case 'INITIAL_REVIEW_COMPLETE':
+      case 'ACCEPTED':
+        return item.receivedDate;
+      default:
+        return item.requestedDate;
+    }
+  };
+
+  const getTrackedItemDescription = item => {
+    switch (item.status) {
+      case 'NEEDED_FROM_YOU':
+      case 'NEEDED_FROM_OTHERS':
+        return `We opened a request for "${item.displayName}"`;
+      case 'NO_LONGER_REQUIRED':
+        return `We closed a request for "${item.displayName}"`;
+      case 'SUBMITTED_AWAITING_REVIEW':
+        return `We received your document(s) for "${item.displayName}"`;
+      case 'INITIAL_REVIEW_COMPLETE':
+      case 'ACCEPTED':
+        return `We completed a review for "${item.displayName}"`;
+      default:
+        return 'There was an update to this item';
+    }
+  };
+
+  const getPhaseItemDescription = (currentPhaseBack, phase) => {
+    const phaseItemText = getPhaseItemText(phase, cstClaimPhasesEnabled);
+    switch (phase) {
+      case 1:
+      case 8:
+        return phaseItemText;
+      case 2:
+      case 7:
+        return `Your claim moved into ${phaseItemText}`;
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+        if (currentPhaseBack) {
+          return `Your claim moved back to ${phaseItemText}`;
+        }
+        return `Your claim moved into ${phaseItemText}`;
+      default:
+        return `Your claim moved into ${phaseItemText}`;
+    }
+  };
+
+  const generateTrackedItems = () => {
+    const { trackedItems } = claim.attributes;
+
+    return trackedItems.map(item => ({
+      id: item.id,
+      date: getTrackedItemDateFromStatus(item),
+      description: getTrackedItemDescription(item),
+      displayName: item.displayName,
+      status: item.status,
+      type: 'tracked_item',
+    }));
+  };
+
+  const isEventOrPrimaryPhase = event => {
+    if (event.type === 'phase_entered') {
+      return event.phase <= 3 || event.phase >= 7;
+    }
+
+    return !!getItemDate(event);
+  };
+
+  const getPhaseFromStatus = latestStatus =>
+    [...getStatusMap().keys()].indexOf(latestStatus.toUpperCase()) + 1;
+
+  const isCurrentOrPastPhase = (event, currentPhase) => {
+    return event.phase <= currentPhase;
+  };
+
+  const generatePhaseItems = () => {
+    const {
+      currentPhaseBack,
+      previousPhases,
+    } = claim.attributes.claimPhaseDates;
+    const phases = [];
+
+    const regex = /\d+/;
+
+    // Add phase dates using the complete date of other phases
+    const phaseKeys = Object.keys(previousPhases);
+    phaseKeys.forEach(phaseKey => {
+      const phase = Number(phaseKey.match(regex)[0]) + 1;
+      phases.push({
+        id: phase,
+        type: 'phase_entered',
+        // We are assuming here that each phaseKey is of the format:
+        // phaseXCompleteDate, where X is some integer between 1 and 7
+        // NOTE: Adding 1 because the a phases completion date is
+        // analagous to the phase after it's start date
+        // eg. phase1CompleteDate = start date for phase 2
+        phase,
+        description: getPhaseItemDescription(currentPhaseBack, phase),
+        date: previousPhases[phaseKey],
+      });
+    });
+
+    // Add initial phase date since its not inculded in previousPhases
+    phases.push({
+      id: 1,
+      type: 'filed',
+      phase: 1,
+      description: getPhaseItemDescription(currentPhaseBack, 1),
+      date: claim.attributes.claimDate,
+    });
+
+    // When cst_claim_phases is enabled then we remove steps 4-6 and only keep step 3
+    const firstPass = cstClaimPhasesEnabled
+      ? phases
+      : phases.filter(isEventOrPrimaryPhase);
+
+    const currentPhase = getPhaseFromStatus(
+      claim.attributes.claimPhaseDates.latestPhaseType,
+    );
+    // TODO What is the purpose of this and is it needed?
+    return firstPass.filter(phase => isCurrentOrPastPhase(phase, currentPhase));
+  };
+
+  const getSortedItems = () => {
+    // Get items from trackedItems and claimPhaseDates
+    const trackedItems = generateTrackedItems();
+    const phaseItems = generatePhaseItems();
+    const items = [...trackedItems, ...phaseItems];
+
+    return items.sort((item1, item2) => {
+      return moment(item2.date) - moment(item1.date);
+    });
+  };
+
   const [currentPage, setCurrentPage] = useState(1);
-  const items = getSortedItems(claim);
+  const items = getSortedItems();
   const pageLength = items.length;
   const numPages = Math.ceil(pageLength / ITEMS_PER_PAGE);
   const shouldPaginate = numPages > 1;
