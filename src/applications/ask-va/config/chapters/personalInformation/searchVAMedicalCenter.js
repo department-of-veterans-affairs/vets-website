0040ab5899fc25049ec8af49a18bfecb3153a18b
Hemesh Patel <hemesh.v.patel@gmail.com>
2024-07-01 09:38:35 -0500

    update health facilities api call due to fix on BE, update search results format

--- src/applications/ask-va/config/chapters/personalInformation/searchVAMedicalCenter.js
+++ src/applications/ask-va/config/chapters/personalInformation/searchVAMedicalCenter.js
@@ -1,21 +1,23 @@
+import FormElementTitle from '../../../components/FormElementTitle';
 import MedicalFacilitySearch from '../../../components/MedicalFacilitySearch';
 import PageFieldSummary from '../../../components/PageFieldSummary';
-import FormElementTitle from '../../../components/FormElementTitle';
 
 const searchVAMedicalCenterPage = {
   uiSchema: {
     'ui:title': FormElementTitle({
       title: 'Your VA health facility',
     }),
+    'ui:description':
+      'Search by city, postal code, or use your current location.',
     'ui:objectViewField': PageFieldSummary,
     vaMedicalCenter: {
-      'ui:title': 'Search by city, postal code, or use your current location.',
+      'ui:title': ' ',
       'ui:widget': MedicalFacilitySearch,
     },
   },
   schema: {
     type: 'object',
-    required: ['vaMedicalCenter'],
+    required: [],
     properties: {
       vaMedicalCenter: {
         type: 'string',
