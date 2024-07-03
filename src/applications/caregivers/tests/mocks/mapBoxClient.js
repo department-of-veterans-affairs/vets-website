63ee49b85aa294e0938a192f139e7f85a5593e63
Matt Long <longmd@me.com>
2024-07-03 16:55:17 -0400

    Update test coverage for mapbox actions in Caregiver application

--- src/applications/caregivers/tests/mocks/mapBoxClient.js
+++ src/applications/caregivers/tests/mocks/mapBoxClient.js
@@ -1,24 +1,9 @@
-// Mock MapBox client for testing purposes
-export const mockMapBoxClient = (
-  longitude1 = null,
-  latitude1 = null,
-  longitude2 = null,
-  latitude2 = null,
-) => ({
+export const mockMapBoxClient = () => ({
   forwardGeocode() {
     return Object.create({
       send() {
         return new Promise((resolve, reject) => {
-          resolve({
-            body: {
-              features: [
-                {
-                  bbox: [longitude1, latitude1, longitude2, latitude2],
-                },
-              ],
-            },
-          });
-
+          resolve({});
           reject(new Error('Some bad error occurred.'));
         });
       },
