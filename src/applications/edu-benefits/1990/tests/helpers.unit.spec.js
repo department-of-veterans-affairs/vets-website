374c9da0c92757eb1428ccfd04f05d2b18c5ff78
fatmakhan0395 <Fatma.khan@va.gov>
2024-07-04 21:58:17 -0700

    added unit tests

--- src/applications/edu-benefits/1990/tests/helpers.unit.spec.js
+++ src/applications/edu-benefits/1990/tests/helpers.unit.spec.js
@@ -1,5 +1,7 @@
 import { expect } from 'chai';
 import { prefillTransformer } from '../helpers.jsx';
+import { transformForSubmit } from 'platform/forms-system/src/js/helpers';
+import sinon from 'sinon';
 
 describe('1990 helpers', () => {
   describe('prefillTransformer', () => {
@@ -73,6 +75,92 @@ describe('1990 helpers', () => {
       expect(result.formData['view:otherContactInfo'].mobilePhone).to.eql(
         formData.mobilePhone,
       );
+    });
+  });
+  describe('transform', () => {
+    let transformForSubmitStub;
+  
+    beforeEach(() => {
+      transformForSubmitStub = sinon.stub(transformModule, 'transformForSubmit');
+    });
+  
+    afterEach(() => {
+      transformForSubmitStub.restore();
+    });
+  
+    it('should transform form data correctly for chapter33', () => {
+      const formConfig = {};
+      const form = {};
+  
+      transformForSubmitStub.returns(JSON.stringify({ chapter33: 'chapter33' }));
+  
+      const result = transform(formConfig, form);
+  
+      expect(JSON.parse(result)).to.deep.equal({
+        educationBenefitsClaim: {
+          form: JSON.stringify({ chapter33: true })
+        }
+      });
+    });
+  
+    it('should transform form data correctly for chapter30', () => {
+      const formConfig = {};
+      const form = {};
+  
+      transformForSubmitStub.returns(JSON.stringify({ chapter33: 'chapter30' }));
+  
+      const result = transform(formConfig, form);
+  
+      expect(JSON.parse(result)).to.deep.equal({
+        educationBenefitsClaim: {
+          form: JSON.stringify({ chapter30: true })
+        }
+      });
+    });
+  
+    it('should transform form data correctly for chapter1606', () => {
+      const formConfig = {};
+      const form = {};
+  
+      transformForSubmitStub.returns(JSON.stringify({ chapter33: 'chapter1606' }));
+  
+      const result = transform(formConfig, form);
+  
+      expect(JSON.parse(result)).to.deep.equal({
+        educationBenefitsClaim: {
+          form: JSON.stringify({ chapter1606: true })
+        }
+      });
+    });
+  
+    it('should not modify chapter33 if it is not a string', () => {
+      const formConfig = {};
+      const form = {};
+  
+      transformForSubmitStub.returns(JSON.stringify({ chapter33: true }));
+  
+      const result = transform(formConfig, form);
+  
+      expect(JSON.parse(result)).to.deep.equal({
+        educationBenefitsClaim: {
+          form: JSON.stringify({ chapter33: true })
+        }
+      });
+    });
+  
+    it('should delete chapter33 if it is a string but not a known chapter', () => {
+      const formConfig = {};
+      const form = {};
+  
+      transformForSubmitStub.returns(JSON.stringify({ chapter33: 'someUnknownChapter' }));
+  
+      const result = transform(formConfig, form);
+  
+      expect(JSON.parse(result)).to.deep.equal({
+        educationBenefitsClaim: {
+          form: JSON.stringify({})
+        }
+      });
     });
   });
 });
