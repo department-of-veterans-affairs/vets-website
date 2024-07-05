7889c3287523903d4ce2d20bb113fc0b3843564f
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2024-07-05 10:06:36 -0400

    VYE-refixed issue with accessibilitiy (#30692)

--- src/applications/verify-your-enrollment/tests/components/Alert.unit.spec.jsx
+++ src/applications/verify-your-enrollment/tests/components/Alert.unit.spec.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { expect } from 'chai';
-import { act, waitFor } from '@testing-library/react';
+import { waitFor } from '@testing-library/react';
 import { mount } from 'enzyme';
 import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
-import sinon from 'sinon';
 import Alert from '../../components/Alert';
 
 // Create a mock store
@@ -12,8 +11,6 @@ const mockStore = configureMockStore();
 const store = mockStore({});
 
 describe('<Alert />', () => {
-  let clock;
-
   it('renders with status and message props', async () => {
     const wrapper = mount(
       <Provider store={store}>
@@ -39,26 +36,6 @@ describe('<Alert />', () => {
     );
     await waitFor(() => {
       expect(wrapper.find('h2').text()).to.equal('some title');
-      wrapper.unmount();
-    });
-  });
-  it('dispatches RESET_ERROR and RESET_SUCCESS_MESSAGE after 15 seconds', () => {
-    clock = sinon.useFakeTimers({
-      toFake: ['setTimeout', 'clearTimeout'],
-    });
-    const wrapper = mount(
-      <Provider store={store}>
-        <Alert status="info" message="Test message" />
-      </Provider>,
-    );
-
-    act(() => {
-      clock.tick(15000);
-    });
-    const actions = store.getActions();
-    act(() => {
-      expect(actions).to.deep.include({ type: 'RESET_ERROR' });
-      expect(actions).to.deep.include({ type: 'RESET_SUCCESS_MESSAGE' });
       wrapper.unmount();
     });
   });
