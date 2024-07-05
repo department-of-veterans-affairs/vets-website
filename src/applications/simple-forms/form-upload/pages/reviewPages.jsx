1276e897b75a1771f88ac6f086d555c15256e2bd
Jacob Penner <jacob.penner@adhocteam.us>
2024-07-05 11:49:35 -0400

    WIP

--- src/applications/simple-forms/form-upload/pages/reviewPages.jsx
+++ src/applications/simple-forms/form-upload/pages/reviewPages.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
-
-import { VaButtonPair } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+// import React, { useRef, useState } from 'react';
+// import { formatSSN } from 'platform/utilities/ui';
+import {
+  VaButtonPair,
+  VaTextInput,
+} from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { useHistory, useLocation } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+// import ssnUI from 'platform/forms-system/src/js/definitions/ssn';
+// import SsnField from 'platform/forms-system/src/js/web-component-fields/SsnField';
 import { getFormNumber } from '../helpers';
 import FormPage from '../containers/FormPage';
 
@@ -27,13 +34,99 @@ export const IdentificationInfoPage = () => {
     setSsn(e.target.value);
   };
 
+  // const SocialSecurityField = ({ props }) => SsnField(props);
+
+  // const maskSSN = (ssnString = '') => {
+  //   const strippedSSN = ssnString.replace(/[- ]/g, '');
+  //   const maskedSSN = strippedSSN.replace(/^\d{1,5}/, digit =>
+  //     digit.replace(/\d/g, '●'),
+  //   );
+
+  //   return formatSSN(maskedSSN);
+  // };
+
+  // const SsnField = ({ props }) => {
+  //   const [val, setVal] = useState('');
+  //   const [displayVal, setDisplayVal] = useState('');
+  //   const vaTextInput = useRef();
+  //   const handleChange = event => {
+  //     const { value } = event.target;
+  //     let strippedSSN;
+  //     if (value) {
+  //       strippedSSN = value.replace(/[- ]/g, '');
+  //     }
+
+  //     setVal(value);
+  //     setDisplayVal(value);
+  //     props.onInput(event, strippedSSN);
+  //   };
+
+  //   const handleBlur = () => {
+  //     setDisplayVal(maskSSN(val));
+  //     props.onBlur(props.id);
+  //   };
+
+  //   const handleFocus = () => {
+  //     setDisplayVal(val);
+  //   };
+
+  //   return (
+  //     <VaTextInput
+  //       {...props}
+  //       hint={null}
+  //       value={displayVal}
+  //       onInput={handleChange}
+  //       onBlur={handleBlur}
+  //       onFocus={handleFocus}
+  //       ref={vaTextInput}
+  //     />
+  //   );
+  // };
+
   return (
     <FormPage currentLocation={2} pageTitle="Identification information">
       <p className="vads-u-margin-top--0">
         You must enter either a Social Security number or VA File number.
       </p>
       <div className="vads-u-margin-bottom--5">
-        <va-text-input
+        {/* <SsnField
+          {...ssnUI}
+          hint={null}
+          label="Social Security number"
+          // message-aria-describedby="Optional description text for screen readers"
+          name="social-security-number"
+          required
+        /> */}
+        {/* {SsnField({
+          uiSchema: {
+            ...ssnUI,
+            'ui:autocomplete': null,
+            onInput: onSsnChange,
+            hint: null,
+            label: 'Social Security number',
+            // message-aria-describedby="Optional description text for screen readers"
+            name: 'social-security-number',
+            description: 'Description',
+            required: true,
+          },
+          childrenProps: {
+            idSchema: PropTypes.object,
+            schema: { type: 'object' },
+          },
+        })} */}
+        {/* {{
+          uiSchema: {
+            ...titleUI('Your identification information'),
+            userId: ssnOrVaFileNumberUI(),
+          },
+          schema: {
+            type: 'object',
+            properties: {
+              userId: ssnOrVaFileNumberSchema,
+            },
+          },
+        }} */}
+        <VaTextInput
           hint={null}
           label="Social Security number"
           message-aria-describedby="Optional description text for screen readers"
@@ -41,7 +134,7 @@ export const IdentificationInfoPage = () => {
           onInput={onSsnChange}
           required
         />
-        <va-text-input
+        <VaTextInput
           hint={null}
           label="VA file number"
           message-aria-describedby="Optional description text for screen readers"
