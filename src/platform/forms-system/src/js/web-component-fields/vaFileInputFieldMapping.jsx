adddf85c74090a5d1c8d096c8f5cca867a25b617
Jacob Penner <jacob.penner@adhocteam.us>
2024-07-08 11:31:04 -0400

    build out file input pattern logic

---
+++ src/platform/forms-system/src/js/web-component-fields/vaFileInputFieldMapping.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import commonFieldMapping from './commonFieldMapping';
+import formsPatternFieldMapping from './formsPatternFieldMapping';
+
+/** @param {WebComponentFieldProps} props */
+export default function vaFileInputFieldMapping(props) {
+  const {
+    description,
+    textDescription,
+    DescriptionField,
+    uiOptions,
+    index,
+    childrenProps,
+  } = props;
+
+  const commonFieldProps = commonFieldMapping(props);
+  const { formsPatternProps, formDescriptionSlot } = formsPatternFieldMapping(
+    props,
+  );
+
+  return {
+    ...commonFieldProps,
+    ...formsPatternProps,
+    value:
+      typeof childrenProps.formData === 'undefined'
+        ? ''
+        : childrenProps.formData,
+    messageAriaDescribedby:
+      commonFieldProps.messageAriaDescribedby || textDescription || undefined,
+    onVaChange: (event, value) => {
+      // redux value or input value
+      let newVal = value || event.target.value;
+      // pattern validation will trigger if you have '',
+      // so set as undefined instead.
+      newVal = newVal === '' ? undefined : newVal;
+      childrenProps.onVaChange(newVal);
+    },
+    onBlur: () => childrenProps.onBlur(childrenProps.idSchema.$id),
+    children: (
+      <>
+        {formDescriptionSlot}
+        {textDescription && <p>{textDescription}</p>}
+        {DescriptionField && (
+          <DescriptionField options={uiOptions} index={index} />
+        )}
+        {!textDescription && !DescriptionField && description}
+      </>
+    ),
+  };
+}
