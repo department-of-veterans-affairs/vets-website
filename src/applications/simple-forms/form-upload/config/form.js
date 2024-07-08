160a18afa3158919f4f4e273bd08f1e268dda27b
Jacob Penner <jacob.penner@adhocteam.us>
2024-07-08 11:26:29 -0400

    continue form generation

---
+++ src/applications/simple-forms/form-upload/config/form.js
@@ -0,0 +1,85 @@
+// import fullSchema from 'vets-json-schema/dist/FORM-UPLOAD-schema.json';
+
+import manifest from '../manifest.json';
+
+import IntroductionPage from '../containers/IntroductionPage';
+import ConfirmationPage from '../containers/ConfirmationPage';
+
+// const { } = fullSchema.properties;
+
+// const { } = fullSchema.definitions;
+
+const formConfig = {
+  rootUrl: manifest.rootUrl,
+  urlPrefix: '/',
+  // submitUrl: '/v0/api',
+  submit: () =>
+    Promise.resolve({ attributes: { confirmationNumber: '123123123' } }),
+  trackingPrefix: 'form-upload-flow-',
+  introduction: IntroductionPage,
+  confirmation: ConfirmationPage,
+  formId: 'FORM-UPLOAD',
+  saveInProgress: {
+    messages: {
+      inProgress: 'Your form upload is in progress.',
+      expired:
+        'Your form upload has expired. If you want to upload a form, please start a new request.',
+      saved: 'Your form upload has been saved.',
+    },
+  },
+  version: 0,
+  prefillEnabled: true,
+  savedFormMessages: {
+    notFound: 'Please start over to upload your form.',
+    noAuth: 'Please sign in again to continue uploading your form.',
+  },
+  title: 'Upload VA Form 21-0779',
+  defaultDefinitions: {},
+  v3SegmentedProgressBar: true,
+  chapters: {
+    uploadChapter: {
+      title: 'Upload',
+      pages: {
+        uploadPage: {
+          path: 'upload',
+          title: 'Upload Your File',
+          uiSchema: {},
+          schema: {
+            type: 'object',
+            properties: {},
+          },
+        },
+      },
+    },
+    reviewChapter: {
+      title: 'Review',
+      pages: {
+        reviewPage: {
+          path: 'review',
+          title: 'Review Your Information',
+          uiSchema: {},
+          schema: {
+            type: 'object',
+            properties: {},
+          },
+        },
+      },
+    },
+    submitChapter: {
+      title: 'Submit',
+      pages: {
+        submitPage: {
+          path: 'submit',
+          title: 'Submit Your Form',
+          uiSchema: {},
+          schema: {
+            type: 'object',
+            properties: {},
+          },
+        },
+      },
+    },
+  },
+};
+
+export default formConfig;
