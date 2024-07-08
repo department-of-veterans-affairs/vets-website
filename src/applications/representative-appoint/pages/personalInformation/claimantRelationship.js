b5688e62ec5680448c24325eea1da3faaa68151b
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-07-08 11:30:50 -0400

    Added claimantRelationship screen

--- src/applications/representative-appoint/pages/personalInformation/claimantRelationship.js
+++ src/applications/representative-appoint/pages/personalInformation/claimantRelationship.js
@@ -3,9 +3,9 @@ import {
   radioSchema,
   radioUI,
 } from '~/platform/forms-system/src/js/web-component-patterns';
+import { claimantRelationships } from '../../definitions/constants';
 
 /** @type {UISchemaOptions} */
-
 export const uiSchema = {
   'ui:description': () => {
     return (
@@ -14,22 +14,30 @@ export const uiSchema = {
       </>
     );
   },
-  authorizeInsideVARadio: radioUI({
+  claimantRelationship: radioUI({
     title: 'What’s your relationship to the Veteran?',
+    labels: {
+      [claimantRelationships.veteran]:
+        'I’m a Veteran, and I intend to file a VA claim for myself',
+      [claimantRelationships.spouseOrChild]:
+        'I’m the spouse or child of a Veteran, and I intend to file a VA claim for myself',
+      [claimantRelationships.veteranRepresentative]:
+        'I’m representing a Veteran who intends to file a VA claim',
+      [claimantRelationships.spouseOrChildRepresentative]:
+        'I’m representing a Veteran’s spouse or child who intends to file a VA claim (called the claimant in this form)',
+    },
+    errorMessages: {
+      enum: 'Select one that best describes you',
+      required: 'Select one that best describes you',
+    },
   }),
 };
 
 /** @type {UISchemaOptions} */
 export const schema = {
   type: 'object',
-  required: ['authorizeInsideVARadio'],
+  required: ['claimantRelationship'],
   properties: {
-    authorizeInsideVARadio: radioSchema([
-      'I’m the Veteran’s spouse',
-      'I’m the Veteran’s child',
-      'I’m the Veteran’s parent',
-      'I’m the Veteran’s accredited representative',
-      'We don’t have a relationship that’s listed here',
-    ]),
+    claimantRelationship: radioSchema(Object.values(claimantRelationships)),
   },
 };
