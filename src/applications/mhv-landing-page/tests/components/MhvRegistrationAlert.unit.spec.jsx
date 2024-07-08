ee357eea9d30bc7234da608d63a9c6ebce0da807
Carlos Felix <carlos.felix@agile6.com>
2024-07-08 19:44:26 -0400

    Added tests

--- src/applications/mhv-landing-page/tests/components/MhvRegistrationAlert.unit.spec.jsx
+++ src/applications/mhv-landing-page/tests/components/MhvRegistrationAlert.unit.spec.jsx
@@ -2,14 +2,34 @@ import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 import { expect } from 'chai';
 import sinon from 'sinon';
+import { Provider } from 'react-redux';
 
+import { mhvUrl } from '~/platform/site-wide/mhv/utilities.js';
 import MhvRegistrationAlert from '../../components/MhvRegistrationAlert';
 
 const defaultHeadline = MhvRegistrationAlert.defaultProps.headline;
 
 describe('MhvRegistrationAlert', () => {
+  const mockStore = ({ ssoe = true } = {}) => ({
+    getState: () => ({
+      user: {
+        profile: {
+          session: {
+            ssoe,
+          },
+        },
+      },
+    }),
+    subscribe: () => {},
+    dispatch: () => {},
+  });
+
   it('renders', () => {
-    const { getByRole } = render(<MhvRegistrationAlert />);
+    const { getByRole } = render(
+      <Provider store={mockStore({ ssoe: true })}>
+        <MhvRegistrationAlert />
+      </Provider>,
+    );
     getByRole('heading', { name: defaultHeadline });
   });
 
@@ -22,10 +42,41 @@ describe('MhvRegistrationAlert', () => {
     };
     const recordEventSpy = sinon.spy();
     const props = { recordEvent: recordEventSpy };
-    render(<MhvRegistrationAlert {...props} />);
+    render(
+      <Provider store={mockStore({ ssoe: true })}>
+        <MhvRegistrationAlert {...props} />
+      </Provider>,
+    );
     await waitFor(() => {
       expect(recordEventSpy.calledOnce).to.be.true;
       expect(recordEventSpy.calledWith(event)).to.be.true;
+    });
+  });
+
+  describe('MHV registration URL', () => {
+    const linkText = /Register with My HealtheVet/;
+    const linkPath = 'home';
+
+    it('registration URL for SSOe users', () => {
+      const { getByRole } = render(
+        <Provider store={mockStore({ ssoe: true })}>
+          <MhvRegistrationAlert />
+        </Provider>,
+      );
+      const link = getByRole('link', { name: linkText });
+      expect(link).to.exist;
+      expect(link.href).to.eql(mhvUrl(true, linkPath));
+    });
+
+    it('registration URL for non-SSOe users', () => {
+      const { getByRole } = render(
+        <Provider store={mockStore({ ssoe: false })}>
+          <MhvRegistrationAlert />
+        </Provider>,
+      );
+      const link = getByRole('link', { name: linkText });
+      expect(link).to.exist;
+      expect(link.href).to.eql(mhvUrl(false, linkPath));
     });
   });
 });
