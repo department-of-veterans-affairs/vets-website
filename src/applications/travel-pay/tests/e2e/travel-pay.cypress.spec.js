7201d36e89d03a64f20a050e5a9990e01f922338
Dan Hinze <hinzed1127@gmail.com>
2024-07-09 13:03:43 -0500

    Add cypress testing for date filtering

--- src/applications/travel-pay/tests/e2e/travel-pay.cypress.spec.js
+++ src/applications/travel-pay/tests/e2e/travel-pay.cypress.spec.js
@@ -1,4 +1,5 @@
 /* eslint-disable @department-of-veterans-affairs/axe-check-required */
+import MockDate from 'mockdate';
 import { appName, rootUrl } from '../../manifest.json';
 import user from '../fixtures/user.json';
 import ApiInitializer from './utilities/ApiInitializer';
@@ -14,15 +15,12 @@ const testStatuses = [
   'ON_HOLD',
 ];
 
-Cypress.Commands.add('openFiltersAndUncheckStatuses', () => {
+Cypress.Commands.add('openFilters', () => {
   cy.get('va-accordion-item[data-testid="filters-accordion"]')
     .shadow()
     .find('h2 button[aria-controls="content"]')
     .eq(0)
     .click({ waitForAnimations: true });
-  testStatuses.forEach(status => {
-    cy.selectVaCheckbox(`${status}_checkbox`, false);
-  });
 });
 
 describe(`${appName} -- Status Page`, () => {
@@ -33,6 +31,14 @@ describe(`${appName} -- Status Page`, () => {
     cy.login(user);
     cy.visit(rootUrl);
     cy.injectAxeThenAxeCheck();
+  });
+
+  before(() => {
+    MockDate.set('2024-06-25');
+  });
+
+  after(() => {
+    MockDate.reset();
   });
 
   it('shows the help text before the claims', () => {
@@ -81,7 +87,7 @@ describe(`${appName} -- Status Page`, () => {
   });
 
   it('filters the claims by status and preserves default sort', () => {
-    cy.openFiltersAndUncheckStatuses();
+    cy.openFilters();
     cy.selectVaCheckbox('SAVED_checkbox', true);
     cy.get('va-button[data-testid="apply_filters"]').click({
       waitForAnimations: true,
@@ -97,7 +103,7 @@ describe(`${appName} -- Status Page`, () => {
   });
 
   it('filters the claims by multiple statuses and preserves default sort', () => {
-    cy.openFiltersAndUncheckStatuses();
+    cy.openFilters();
     cy.selectVaCheckbox('SAVED_checkbox', true);
     cy.selectVaCheckbox('INCOMPLETE_checkbox', true);
     cy.get('va-button[data-testid="apply_filters"]').click({
@@ -113,8 +119,49 @@ describe(`${appName} -- Status Page`, () => {
       .should('include.text', 'March 27, 2022');
   });
 
+  it('filters the claims by a date range preset', () => {
+    cy.openFilters();
+
+    cy.get('select[name="claimsDates"]').should('have.value', 'all');
+    cy.get('select[name="claimsDates"]').select('Past 3 Months');
+    cy.get('select[name="claimsDates"]').should('have.value', 'Past 3 Months');
+
+    cy.get('va-button[data-testid="apply_filters"]').click({
+      waitForAnimations: true,
+    });
+
+    cy.get('h2[data-testid="travel-claim-details"]').should('have.length', 1);
+
+    cy.get('h2[data-testid="travel-claim-details"]')
+      .first()
+      .should('include.text', 'May 15, 2024');
+  });
+
+  it('filters by multiple properties with non-default sorting', () => {
+    cy.get('select[name="claimsOrder"]').select('oldest');
+    cy.get('select[name="claimsOrder"]').should('have.value', 'oldest');
+    cy.get('va-button[data-testid="Sort travel claims"]').click();
+
+    cy.openFilters();
+    cy.selectVaCheckbox('CLAIM_SUBMITTED_checkbox', true);
+    cy.get('select[name="claimsDates"]').select('All of 2023');
+    cy.get('select[name="claimsDates"]').should('have.value', 'All of 2023');
+
+    cy.get('va-button[data-testid="apply_filters"]').click({
+      waitForAnimations: true,
+    });
+
+    cy.get('h2[data-testid="travel-claim-details"]').should('have.length', 3);
+    cy.get('h2[data-testid="travel-claim-details"]')
+      .first()
+      .should('include.text', 'May 17, 2023');
+    cy.get('h2[data-testid="travel-claim-details"]')
+      .eq(2)
+      .should('include.text', 'December 28, 2023');
+  });
+
   it('resets the filters when button is pressed', () => {
-    cy.openFiltersAndUncheckStatuses();
+    cy.openFilters();
     cy.selectVaCheckbox('SAVED_checkbox', true);
     cy.get('va-button[data-testid="apply_filters"]').click({
       waitForAnimations: true,
@@ -137,7 +184,7 @@ describe(`${appName} -- Status Page`, () => {
   it('preserves sort order when filters are applied', () => {
     cy.get('select[name="claimsOrder"]').select('oldest');
     cy.get('va-button[data-testid="Sort travel claims"]').click();
-    cy.openFiltersAndUncheckStatuses();
+    cy.openFilters();
     cy.selectVaCheckbox('SAVED_checkbox', true);
     cy.get('va-button[data-testid="apply_filters"]').click({
       waitForAnimations: true,
