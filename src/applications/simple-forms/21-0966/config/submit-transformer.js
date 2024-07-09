9649335ce5a8b7070b25c5a108b641e13056c73f
Eric Tillberg <Thrillberg@users.noreply.github.com>
2024-07-09 15:20:59 -0400

    Supply the data with the right keys for 21-0966 submit transformer

--- src/applications/simple-forms/21-0966/config/submit-transformer.js
+++ src/applications/simple-forms/21-0966/config/submit-transformer.js
@@ -5,9 +5,41 @@ import {
 } from '../definitions/constants';
 
 export default function transformForSubmit(formConfig, form) {
-  const transformedData = JSON.parse(
+  const sharedTransformedData = JSON.parse(
     sharedTransformForSubmit(formConfig, form),
   );
+
+  // We need to transform the data this way because the prefill keys don't have the 'veteran'
+  // prefix. The backend expects 'veteranFullName' and not 'fullName', for example.
+  const transformedData = {
+    ...sharedTransformedData,
+    veteranFullName:
+      sharedTransformedData.veteranFullName || sharedTransformedData.fullName,
+    veteranDateOfBirth:
+      sharedTransformedData.veteranDateOfBirth ||
+      sharedTransformedData.dateOfBirth,
+    veteranId: {
+      ssn: sharedTransformedData.veteranId.ssn || sharedTransformedData.ssn,
+      vaFileNumber: sharedTransformedData.veteranId.vaFileNumber,
+    },
+    veteranMailingAddress:
+      sharedTransformedData.veteranMailingAddress ||
+      sharedTransformedData.address,
+    veteranPhone:
+      sharedTransformedData.veteranPhone ||
+      (sharedTransformedData.homePhone || sharedTransformedData.mobilePhone),
+    veteranEmail:
+      sharedTransformedData.veteranEmail || sharedTransformedData.email,
+  };
+
+  // These properties are supplied by prefill, not the user
+  delete transformedData.fullName;
+  delete transformedData.dateOfBirth;
+  delete transformedData.ssn;
+  delete transformedData.address;
+  delete transformedData.homePhone;
+  delete transformedData.mobilePhone;
+  delete transformedData.email;
 
   return JSON.stringify({
     ...transformedData,
