76c0f5997f6b1c28be67094d42090790c42f8269
Hemesh Patel <hemesh.v.patel@gmail.com>
2024-07-09 09:31:39 -0500

    fix breadcrumbs

--- src/applications/ask-va/containers/App.jsx
+++ src/applications/ask-va/containers/App.jsx
@@ -1,12 +1,16 @@
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import React from 'react';
-import formConfig from '../config/form';
 import BreadCrumbs from '../components/BreadCrumbs';
+import formConfig from '../config/form';
 
 export default function App({ location, children }) {
   return (
     <>
-      <BreadCrumbs currentLocation={location.pathname} />
+      <div className="row">
+        <div className="usa-width-two-thirds medium-8 columns">
+          <BreadCrumbs currentLocation={location.pathname} />
+        </div>
+      </div>
       <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
         {children}
       </RoutedSavableApp>
