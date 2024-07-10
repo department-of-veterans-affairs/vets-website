7b0b33c95d0fff71d07dc7666d3967fc9b56f64f
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-07-10 14:54:20 -0400

    Removed bank account from prefill-transformer

--- src/applications/representative-appoint/prefill-transformer.js
+++ src/applications/representative-appoint/prefill-transformer.js
@@ -1,7 +1,3 @@
-import { prefillBankInformation } from 'platform/forms-system/src/js/definitions/directDeposit';
-import _ from 'lodash';
-import { deviewifyFields } from './utilities/helpers';
-
 export function prefillTransformer(pages, formData, metadata) {
   const {
     veteranFullName,
@@ -12,10 +8,9 @@ export function prefillTransformer(pages, formData, metadata) {
     email,
     address,
     dateOfBirth,
-    bankAccount,
   } = formData;
 
-  let newFormData = {
+  const newFormData = {
     'view:applicantInformation': {
       veteranFullName,
       veteranSsnLastFour,
@@ -31,34 +26,6 @@ export function prefillTransformer(pages, formData, metadata) {
       address,
     },
   };
-
-  if (bankAccount) {
-    const prefillBankInfo = prefillBankInformation({
-      ...bankAccount,
-      bankName: bankAccount?.bankName || 'fakeBank', // so that the check in the function doesn't fail if no bankName, omitted down below
-    });
-    const prefillBankAccount = _.get(
-      prefillBankInfo,
-      'view:originalBankAccount',
-      {},
-    );
-    const originalBankAccount = {
-      ...prefillBankAccount,
-      'view:accountType': prefillBankAccount['view:accountType']?.toLowerCase(),
-      'view:bankName': undefined, // need this so that prefill display messages will show correctly, remove if bankName is added
-    };
-
-    newFormData = {
-      ...newFormData,
-      'view:originalBankAccount': originalBankAccount,
-      'view:directDeposit': {
-        bankAccount: {
-          ...prefillBankInfo.bankAccount,
-          ...deviewifyFields(_.omit(originalBankAccount, ['view:bankName'])),
-        },
-      },
-    };
-  }
 
   return {
     metadata,
