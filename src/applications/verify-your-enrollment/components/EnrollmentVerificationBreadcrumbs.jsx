cb92f2d3c50c61bc771e5556232d75b2ba1b2450
Andrew Steele <andrew.steele@adhocteam.us>
2024-07-10 16:57:04 -0400

    WIP final commit of crumb conversions, next is tests

--- src/applications/verify-your-enrollment/components/EnrollmentVerificationBreadcrumbs.jsx
+++ src/applications/verify-your-enrollment/components/EnrollmentVerificationBreadcrumbs.jsx
@@ -9,21 +9,13 @@ import {
 
 export default function EnrollmentVerificationBreadcrumbs() {
   const breadcrumbs = [
-    <a href="/" key="home">
-      Home
-    </a>,
-    <a href="/education/" key="education-and-training">
-      Education and training
-    </a>,
-    <a
-      href="/education/verify-school-enrollment/"
-      key="verify-school-enrollment"
-    >
-      Verify your school enrollment for GI Bill benefits
-    </a>,
-    <a href={BASE_URL} key="enrollment-verification-page">
-      Montgomery GI Bill enrollment verification
-    </a>,
+    { href: '/', label: 'Home' },
+    { href: '/education/', label: 'Education and training' },
+    {
+      href: '/education/verify-school-enrollment/',
+      label: 'Verify your school enrollment for GI Bill benefits',
+    },
+    { href: BASE_URL, label: 'Montgomery GI Bill enrollment verification' },
   ];
 
   // Get the last non-empty segment of the URL.
@@ -33,19 +25,19 @@ export default function EnrollmentVerificationBreadcrumbs() {
     .find(s => !!s.trim() && !s.startsWith('?'));
 
   if ([BENEFITS_PROFILE_URL_SEGMENT].includes(page)) {
-    breadcrumbs.push(
-      <a href={BENEFITS_PROFILE_URL} key="BenefitsProfilePage">
-        Your Montgomery GI Bill benefits information
-      </a>,
-    );
+    breadcrumbs.push({
+      href: BENEFITS_PROFILE_URL,
+      label: 'Your Montgomery GI Bill benefits information',
+    });
   }
 
   if ([VERIFICATION_REVIEW_URL_SEGMENT].includes(page)) {
-    breadcrumbs.push(
-      <a href={VERIFICATION_PROFILE_URL} key="VerificationReviewPage">
-        Verify your enrollment
-      </a>,
-    );
+    breadcrumbs.push({
+      href: VERIFICATION_PROFILE_URL,
+      label: 'Verify your enrollment',
+    });
   }
-  return <va-breadcrumbs uswds="false">{breadcrumbs}</va-breadcrumbs>;
+
+  const bcString = JSON.stringify(breadcrumbs);
+  return <va-breadcrumbs breadcrumb-list={bcString} />;
 }
