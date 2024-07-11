6caa66b89367ac753b089e339ef632bae8478a82
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2024-07-11 11:29:23 -0500

    VFEP=983 complete Training And webinar div section.

--- src/applications/sco/components/MainContent/Update/TrainingAndWebinar.jsx
+++ src/applications/sco/components/MainContent/Update/TrainingAndWebinar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MainContentSubDiv from '../../HubRail/shared/mainContentSubDiv';
+import LiSpanAndVaLinkAndPTag from '../../HubRail/shared/liSpanAndVaLinkAndPTag';
 
 const TrainingAndWebinar = () => {
   return (
@@ -7,7 +8,26 @@ const TrainingAndWebinar = () => {
       id="trainings-and-webinars"
       header="Trainings and webinars"
     >
-      <h3>Sample Body Will Go Here.</h3>
+      <LiSpanAndVaLinkAndPTag
+        href="https://www.benefits.va.gov/gibill/resources/education_resources/school_certifying_officials/online_sco_training.asp"
+        hrefText="Training Requirements"
+        pText="Essential training for VA student enrollment certifications and compliance."
+      />
+      <LiSpanAndVaLinkAndPTag
+        href="https://vba-tpss.vbatraining.org/assess/trkSignIn?refid=XSCO"
+        hrefText="SCO Training Portal"
+        pText="Access to the training portal for school officials."
+      />
+      <LiSpanAndVaLinkAndPTag
+        href="https://www.benefits.va.gov/gibill/resources/education_resources/school_certifying_officials/presentations.asp"
+        hrefText="Office Hours and Webinars"
+        pText="Join our office hours and webinars for information on the GI Bill, related legislation, and processes."
+      />
+      <LiSpanAndVaLinkAndPTag
+        href="https://public.govdelivery.com/accounts/USVAVBA/subscriber/new"
+        hrefText="Sign up for trainings, webinars, and office hour updates"
+        pText="Subscribe to the GovDelivery mailing list to receive updates and other routine communications from Education Service about trainings, office hours, and more."
+      />
     </MainContentSubDiv>
   );
 };
