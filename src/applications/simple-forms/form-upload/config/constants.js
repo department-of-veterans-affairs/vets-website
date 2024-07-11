675881a172c3e21b929a78fb94018c92aa449a2c
Jacob Penner <jacob.penner@adhocteam.us>
2024-07-11 10:55:05 -0400

    clean up submit page

--- src/applications/simple-forms/form-upload/config/constants.js
+++ src/applications/simple-forms/form-upload/config/constants.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import {
-  VaCard,
-  VaIcon,
-} from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 
 export const PrimaryActionLink = ({ href = '/', children, onClick = null }) => (
   <div className="action-bar-arrow">
@@ -40,65 +36,5 @@ export const UPLOAD_GUIDELINES = Object.freeze(
         <li>Your file should be no larger than 25MB</li>
       </ul>
     </div>
-  </>,
-);
-
-export const SUBMIT_PAGE_CONTENT = Object.freeze(
-  <>
-    <div className="vads-u-margin-top--2">
-      <div className="vads-u-margin-y--1 vads-u-color--gray">Your file</div>
-      <VaCard style={{ maxWidth: '50%' }}>
-        <div className="vads-u-display--flex vads-u-flex-direction--row">
-          <span className="vads-u-color--primary">
-            <VaIcon
-              size={6}
-              icon="file_present"
-              className="vads-u-margin-right--1"
-              srtext="icon representing a file"
-              aria-hidden="true"
-            />
-          </span>
-          <div className="vads-u-display--flex vads-u-flex-direction--column">
-            <span className="vads-u-font-weight--bold">
-              UPLOADED FILE
-              {/* file?.name */}
-            </span>{' '}
-            <span className="vads-u-color--gray-darker">
-              25K
-              {/* getFileSize(file?.size) */}
-            </span>
-          </div>
-        </div>
-      </VaCard>
-    </div>
-    <div className="vads-u-border-bottom--1px vads-u-margin-top--1 vads-u-margin-bottom--4">
-      <h3>Your personal information</h3>
-    </div>
-    <div className="vads-u-border-left--4px vads-u-border-color--primary vads-u-padding-left--1">
-      <p>
-        <b>
-          JOHN SMITH
-          {/* capitalize(fullName.first)} {capitalize(fullName.last) */}
-        </b>
-      </p>
-      {/* {veteran && (
-        <>
-          <p>
-            Social Security number:{' '}
-            <span
-              className="dd-privacy-mask"
-              data-dd-action-name="Veteran's SSN"
-            >
-              {mask(veteran.ssn)}
-            </span>
-          </p>
-          <p>Zip code: {veteran.address?.postalCode}</p>
-        </>
-      )} */}
-    </div>
-    <p className="vads-u-margin-bottom--5">
-      <b>Note:</b> If you need to update your personal information, please call
-      us at 800-827-1000. We’re here Monday through Friday, 8:00am to 9:00pm ET.
-    </p>
   </>,
 );
