5f1e35fef3a5bb1ce71c32629fb708c45e6a1814
Daniel Cloud <daniel.cloud@adhocteam.us>
2024-07-11 14:07:10 -0400

    Rename App to LandingPageContainer.

--- src/applications/mhv-landing-page/routes.jsx
+++ src/applications/mhv-landing-page/routes.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import PageNotFound from '@department-of-veterans-affairs/platform-site-wide/PageNotFound';
-import App from './containers/App';
+import LandingPageContainer from './containers/LandingPageContainer';
 import MedicalRecordsContainer from './containers/MedicalRecordsContainer';
 
 const routes = (
   <Switch>
     <Route exact path="/" key="mhvLandingPage">
-      <App />
+      <LandingPageContainer />
     </Route>
     <Route exact path="/records" key="mhvMedicalRecords">
       <MedicalRecordsContainer />
