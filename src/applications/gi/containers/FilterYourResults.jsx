524ea93b69bb30eef1403ff4318e61d6d63d8c36
fatmakhan0395 <Fatma.khan@va.gov>
2024-07-11 22:38:59 -0700

    vfep-1248 fixed flip filters on refresh

--- src/applications/gi/containers/FilterYourResults.jsx
+++ src/applications/gi/containers/FilterYourResults.jsx
@@ -108,7 +108,9 @@ export function FilterYourResults({
 
   const facets =
     search.tab === TABS.name ? search.name.facets : search.location.facets;
-  const [nameValue, setNameValue] = useState(search.query.name);
+  const [nameValue, setNameValue] = useState(
+    search.query.name || search.query.location,
+  );
   // const { isCleared, setIsCleared, loading } = useFilterBtn(true);
   const recordCheckboxEvent = e => {
     recordEvent({
