7f6e204b4605d409d6df6501d98f42ed9baffec8
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2024-07-12 17:30:19 -0500

    VFEP-983 'Finish Other resources for schools' sections.

--- src/applications/sco/components/MainContent/Update/OtherResources.jsx
+++ src/applications/sco/components/MainContent/Update/OtherResources.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MainContentSubDiv from '../../HubRail/shared/mainContentSubDiv';
 import MainContentSubSection from '../../HubRail/shared/mainContentSubSection';
 import LiSpanAndVaLinkAndPTag from '../../HubRail/shared/liSpanAndVaLinkAndPTag';
+import LiSpanAndVaLink from '../../HubRail/shared/liSpanAndVaLink';
 
 const OtherResources = () => {
   return (
@@ -11,46 +12,164 @@ const OtherResources = () => {
     >
       <va-accordion uswds>
         <va-accordion-item
+          open
           header="Enrollment Manager"
           subheader="Access Enrollment Manager  and essential training for VA student enrollment certifications and compliance."
         >
-          <p>
-            Congress shall make no law respecting an establishment of religion,
-            or prohibiting the free exercise thereof; or abridging the freedom
-            of speech, or of the press; or the right of the people peaceably to
-            assemble, and to petition the Government for a redress of
-            grievances.
-          </p>
+          Access Enrollment Manager
+          <ul>
+            <LiSpanAndVaLink
+              hrefText="Launch VA Education Platform Portal"
+              class="hydrated"
+              href="https://iam.education.va.gov/auth/realms/dgib/protocol/openid-connect/auth?response_type=code&amp;scope=openid+profile+email&amp;client_id=apigw&amp;redirect_uri=https://iam.education.va.gov:443/_codexch&amp;nonce=GtVNXpMNDc0rQreRf8kTvzlqvDM-zVIsBcz4n2gCn-E&amp;state=0"
+            />
+          </ul>
+          User guides and essential information
+          <ul>
+            <LiSpanAndVaLink
+              hrefText="Enrollment Manager User Guide (PPTX, 315 pages)"
+              href="https://benefits.va.gov/gibill/enrollment-manager/enrollment-manager-sco-user-guide.pptx"
+            />
+            <LiSpanAndVaLink
+              hrefText="Enrollment Manager FAQs"
+              class="hydrated"
+              href="https://benefits.va.gov/gibill/enrollment-manager/enrollment-manager-frequently-asked-questions.asp"
+            />
+          </ul>
+          Quick Start Guides
+          <ul>
+            <LiSpanAndVaLink
+              hrefText="Flight - Enrollment Manager Quick Start Guide (PDF, 4 pages)"
+              href="https://benefits.va.gov/gibill/docs/guides/flight-quick-start-guide.pdf"
+            />
+            <LiSpanAndVaLink
+              hrefText="Institutions of Higher Learning - Enrollment Manager Quick Start Guide (PDF, 3 pages)"
+              href="https://benefits.va.gov/gibill/docs/guides/ihl-quick-start-guide.pdf"
+            />
+            <LiSpanAndVaLink
+              hrefText="Non-College Degree Programs - Enrollment Manager Quick Start Guide (PDF, 3 pages)"
+              href="https://benefits.va.gov/gibill/docs/guides/ncd-quick-start-guide.pdf"
+            />
+            <LiSpanAndVaLink
+              hrefText="On-the-Job Training/Apprenticeship - Enrollment Manager Quick Start Guide (PDF, 4 pages)"
+              href="https://benefits.va.gov/gibill/docs/guides/ojtapp-quick-start-guide.pdf"
+            />
+          </ul>
+          Updates
+          <ul>
+            <LiSpanAndVaLink
+              hrefText="Enrollment Manager System Updates"
+              href="https://benefits.va.gov/GIBILL/enrollment-manager/enrollment-manager-system-updates.asp"
+            />
+            <LiSpanAndVaLink
+              hrefText="VA Paper Based Forms to Enrollment Manager Crosswalk (PPTX, 124 pages)"
+              href="https://benefits.va.gov/GIBILL/enrollment-manager/paper-based-enrollment-manager-crosswalk.pptx"
+            />
+          </ul>
         </va-accordion-item>
         <va-accordion-item
+          open
           header="Payment and debt"
           subheader="Find information about managing debt related to VA education benefits."
         >
-          <p>
-            A well regulated Militia, being necessary to the security of a free
-            State, the right of the people to keep and bear Arms, shall not be
-            infringed.
-          </p>
+          <ul>
+            <LiSpanAndVaLink
+              hrefText="GI Bill Overpayments and Debt"
+              href="https://www.benefits.va.gov/gibill/resources/education_resources/debt_info.asp"
+            />
+            <LiSpanAndVaLink
+              hrefText="VA Debt Management Center"
+              href="https://www.va.gov/resources/va-debt-management/"
+            />
+            <LiSpanAndVaLink
+              hrefText="Treasury Offset Program (TOP)"
+              href="https://www.fiscal.treasury.gov/TOP/"
+            />
+          </ul>
         </va-accordion-item>
         <va-accordion-item
+          open
           header="Veteran Readiness and Employment (VR&E) Chapter 31"
           subheader="The Veteran Readiness and Employment program assists Veterans with service-connected disabilities to explore employment options and address education and/or training needs."
         >
-          <p>
-            No Soldier shall, in time of peace be quartered in any house,
-            without the consent of the Owner, nor in time of war, but in a
-            manner to be prescribed by law.
-          </p>
+          For schools
+          <ul>
+            <LiSpanAndVaLink
+              hrefText="VR&amp;E School Certifying Official Handbook"
+              href="https://www.knowva.ebenefits.va.gov/system/templates/selfservice/va_ssnew/help/customer/locale/en-US/portal/554400000001018/content/554400000260919/VRE-School-Certifying-Official-Handbook"
+            />
+            <LiSpanAndVaLink
+              hrefText="Login to Tungsten"
+              href="https://authentication.tungsten-network.com/login"
+            />
+            <LiSpanAndVaLink
+              hrefText="VR&amp;E Tungsten Invoicing (PDF, 14 pages)"
+              href="https://www.benefits.va.gov/GIBILL/docs/job_aids/VRE_Tungsten_Invoicing.pdf"
+            />
+            <LiSpanAndVaLink
+              hrefText="VR&amp;E Tungsten Customer Campaign"
+              href="https://www.tungsten-network.com/customer-campaigns/vre/"
+            />
+          </ul>
+          Resources to share with students
+          <ul>
+            <LiSpanAndVaLink
+              hrefText="Veteran Readiness and Employment (Chapter 31)"
+              href="https://www.va.gov/careers-employment/vocational-rehabilitation/"
+            />
+            <LiSpanAndVaLink
+              hrefText="Eligibility for VR&amp;E"
+              href="https://www.va.gov/careers-employment/vocational-rehabilitation/eligibility/"
+            />
+            <LiSpanAndVaLink
+              hrefText="How to apply for VR&amp;E"
+              href="https://www.va.gov/careers-employment/vocational-rehabilitation/how-to-apply/"
+            />
+          </ul>
         </va-accordion-item>
         <va-accordion-item
+          open
           header="85/15"
           subheader="Information on the 85 percent rule, or 85/15 rule, which requires that at least 15 percent of students enrolled in an approved program are self-supported."
         >
-          <p>
-            No Soldier shall, in time of peace be quartered in any house,
-            without the consent of the Owner, nor in time of war, but in a
-            manner to be prescribed by law.
-          </p>
+          <ul>
+            <LiSpanAndVaLink
+              hrefText="The 85/15 Rule"
+              href="https://benefits.va.gov/GIBILL/85_15/85_15_homepage.asp"
+            />
+            <LiSpanAndVaLink
+              hrefText="Reporting Requirements"
+              href="https://benefits.va.gov/GIBILL/85_15/Reporting_Requirements.asp"
+            />
+            <LiSpanAndVaLink
+              hrefText="35 Percent Exemption"
+              href="https://benefits.va.gov/GIBILL/85_15/35_percent_exemption.asp"
+            />
+            <LiSpanAndVaLink
+              hrefText="Department of Defense (DoD) Exemption"
+              href="https://benefits.va.gov/GIBILL/85_15/dod_exemption.asp"
+            />
+            <LiSpanAndVaLink
+              hrefText="Education Service Waiver"
+              href="https://benefits.va.gov/GIBILL/85_15/education_service_waiver.asp"
+            />
+            <LiSpanAndVaLink
+              hrefText="Supported and Non-Supported Students"
+              href="https://benefits.va.gov/GIBILL/85_15/supported_non_supported_students.asp"
+            />
+            <LiSpanAndVaLink
+              hrefText="Restricted Aid"
+              href="https://benefits.va.gov/GIBILL/85_15/restricted_aid.asp"
+            />
+            <LiSpanAndVaLink
+              hrefText="Suspension and Review"
+              href="https://benefits.va.gov/GIBILL/85_15/Suspension_and_Review.asp"
+            />
+            <LiSpanAndVaLink
+              hrefText="85/15 FAQs"
+              href="https://benefits.va.gov/gibill/85_15_faqs.asp"
+            />
+          </ul>
         </va-accordion-item>
       </va-accordion>
       <MainContentSubSection
