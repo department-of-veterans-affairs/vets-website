001d02e5582d379e7399abe47d1f86176b1a86ec
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2024-07-12 13:25:00 -0500

    VFEP-983 Finish Enrollment Manager Accordion.

--- src/applications/sco/components/MainContent/Update/UploadFileToVa.jsx
+++ src/applications/sco/components/MainContent/Update/UploadFileToVa.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MainContentSubDiv from '../../HubRail/shared/mainContentSubDiv';
 import LiSpanAndVaLinkAndPTag from '../../HubRail/shared/liSpanAndVaLinkAndPTag';
+import LiSpanAndVaLink from '../../HubRail/shared/liSpanAndVaLink';
 
 const UploadFileToVa = () => {
   return (
@@ -12,19 +13,51 @@ const UploadFileToVa = () => {
           pText="Upload forms and other accepted documents supporting compliance actions, designating or removing certifying officials, and more."
         />
       </MainContentSubDiv>
-      <va-accordion id="upload-files-accordion" uswds>
+      <va-accordion open uswds>
         <va-accordion-item
+          open
           uswds
           header="Forms library and other accepted documents"
           subheader="Check out the forms and documents which you can submit to VA via the Education File Upload Portal."
         >
-          <p>
-            Congress shall make no law respecting an establishment of religion,
-            or prohibiting the free exercise thereof; or abridging the freedom
-            of speech, or of the press; or the right of the people peaceably to
-            assemble, and to petition the Government for a redress of
-            grievances.
-          </p>
+          Accepted forms
+          <ul>
+            <LiSpanAndVaLink
+              hrefText="Designation of Certifying Official(s) - VA Form 22-8794 (PDF, 3 pages)"
+              href="https://www.vba.va.gov/pubs/forms/VBA-22-8794-ARE.pdf"
+            />
+            <LiSpanAndVaLink
+              hrefText="Statement of Assurance of Compliance with 85 Percent Enrollment Ratios - VA Form 22-10215 (PDF, 4 pages)"
+              href="https://www.vba.va.gov/pubs/forms/vba-22-10215-are.pdf"
+            />
+            <LiSpanAndVaLink
+              hrefText="Statement of Assurance of Compliance with 85 Percent Enrollment Ratios Continuation Sheet - VA Form 22-10215a (PDF, 2 pages)"
+              href="https://www.vba.va.gov/pubs/forms/vba-22-10215a-are.pdf"
+            />
+            <LiSpanAndVaLink
+              hrefText="35% Exemption Request from 85/15 Reporting Requirement - VA Form 22-10216 (PDF, 2 pages)"
+              href="https://www.vba.va.gov/pubs/forms/vba-22-10216-are.pdf"
+            />
+            <LiSpanAndVaLink
+              hrefText="Conflicting Interests Certification for Proprietary Schools - VA Form 22-1919 (PDF, 1 page)"
+              href="https://www.vba.va.gov/pubs/forms/VBA-22-1919-ARE.pdf"
+            />
+          </ul>
+          Other accepted documents
+          <ul>
+            <LiSpanAndVaLink
+              hrefText="Compliance documents requested to support a scheduled compliance activity at your institution"
+              href="N/A"
+            />
+            <LiSpanAndVaLink
+              hrefText="Electronic Fund Transfer and Tax ID Updates"
+              href="N/A"
+            />
+            <LiSpanAndVaLink
+              hrefText="Requests for centralized certification"
+              href="N/A"
+            />
+          </ul>
         </va-accordion-item>
       </va-accordion>
     </div>
