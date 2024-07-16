72f526a625c48ae6236dcdb853480933f79adaa5
Matt Long <longmd@me.com>
2024-07-16 09:46:29 -0500

    Update test coverage for the Caregivers app confirmation page (#30762)

--- src/applications/caregivers/tests/unit/components/ConfirmationPage/ConfirmationPrintView.unit.spec.js
+++ src/applications/caregivers/tests/unit/components/ConfirmationPage/ConfirmationPrintView.unit.spec.js
@@ -3,50 +3,41 @@ import { render } from '@testing-library/react';
 import { expect } from 'chai';
 
 import ConfirmationPrintView from '../../../../components/ConfirmationPage/ConfirmationPrintView';
+import content from '../../../../locales/en/content.json';
 
 describe('CG <ConfirmationPrintView>', () => {
-  const defaultProps = {
-    name: {
-      first: 'John',
-      middle: 'Marjorie',
-      last: 'Smith',
-      suffix: 'Sr.',
+  const getData = ({ timestamp = undefined }) => ({
+    props: {
+      name: { first: 'John', middle: 'Marjorie', last: 'Smith', suffix: 'Sr.' },
+      timestamp,
     },
-    timestamp: undefined,
-  };
+  });
+  const subject = ({ props }) => render(<ConfirmationPrintView {...props} />);
 
   it('should render with default props', () => {
-    const view = render(<ConfirmationPrintView {...defaultProps} />);
+    const { props } = getData({});
+    const { container } = subject({ props });
     const selectors = {
-      image: view.container.querySelector('.vagov-logo'),
-      title: view.container.querySelector('h1'),
-      subtitles: view.container.querySelectorAll('h2'),
-      veteranName: view.container.querySelector(
-        '[data-testid="cg-veteranfullname"]',
-      ),
+      image: container.querySelector('.vagov-logo'),
+      title: container.querySelector('h1'),
+      name: container.querySelector('[data-testid="cg-veteranfullname"]'),
     };
     expect(selectors.image).to.exist;
-    expect(selectors.title).to.contain.text(
-      'Apply for the Program of Comprehensive Assistance for Family Caregivers',
-    );
-    expect(selectors.subtitles).to.have.lengthOf(2);
-    expect(selectors.veteranName).to.contain.text('John Marjorie Smith Sr.');
+    expect(selectors.title).to.contain.text(content['app-title']);
+    expect(selectors.name).to.contain.text('John Marjorie Smith Sr.');
   });
 
   it('should not render timestamp in `application information` section when not provided', () => {
-    const view = render(<ConfirmationPrintView {...defaultProps} />);
-    const selector = view.container.querySelector(
-      '[data-testid="cg-timestamp"]',
-    );
+    const { props } = getData({});
+    const { container } = subject({ props });
+    const selector = container.querySelector('[data-testid="cg-timestamp"]');
     expect(selector).to.not.exist;
   });
 
   it('should render timestamp in `application information` section when provided', () => {
-    const props = { ...defaultProps, timestamp: 1666887649663 };
-    const view = render(<ConfirmationPrintView {...props} />);
-    const selector = view.container.querySelector(
-      '[data-testid="cg-timestamp"]',
-    );
+    const { props } = getData({ timestamp: 1666887649663 });
+    const { container } = subject({ props });
+    const selector = container.querySelector('[data-testid="cg-timestamp"]');
     expect(selector).to.exist;
     expect(selector).to.contain.text('Oct. 27, 2022');
   });
