87a889bcf2824fad735e1958877bc2e9f2995d41
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2024-07-17 23:37:56 -0400

    #VEBT-118 #Comments added proptypes to remove warnings

--- src/applications/edu-benefits/1995/Form1995App.jsx
+++ src/applications/edu-benefits/1995/Form1995App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-
+import PropTypes from 'prop-types';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import { useFeatureToggle } from 'platform/utilities/feature-toggles';
 
@@ -10,8 +10,7 @@ export default function Form1995Entry({ location, children }) {
   const { useToggleValue, TOGGLE_NAMES } = useFeatureToggle();
 
   const toggleValue = useToggleValue(TOGGLE_NAMES.merge1995And5490);
-  // eslint-disable-next-line no-unused-vars
-  const [_, setLocalToggleValue] = useLocalStorage('toggleValue', toggleValue);
+  const [setLocalToggleValue] = useLocalStorage('toggleValue', toggleValue);
 
   useEffect(
     () => {
@@ -25,3 +24,11 @@ export default function Form1995Entry({ location, children }) {
     </RoutedSavableApp>
   );
 }
+
+Form1995Entry.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  location: PropTypes.object,
+};
