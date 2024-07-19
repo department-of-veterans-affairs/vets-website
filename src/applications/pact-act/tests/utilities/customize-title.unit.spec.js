90bd03dddaff71a1a739102f4cc925443d261a70
Eli Selkin <eli.j.selkin@gmail.com>
2024-07-19 14:53:59 -0700

    title and test

--- src/applications/pact-act/tests/utilities/customize-title.unit.spec.js
+++ src/applications/pact-act/tests/utilities/customize-title.unit.spec.js
@@ -4,11 +4,13 @@ import { customizeTitle } from '../../utilities/customize-title';
 describe('utilities: customize title', () => {
   it('should return the correct title with the H1', () => {
     expect(customizeTitle('This is a test H1')).to.equal(
-      'This is a test H1 | PACT Act | Veterans Affairs',
+      'Learn how the PACT Act may effect you | Veterans Affairs',
     );
   });
 
   it('should return the correct title without an H1', () => {
-    expect(customizeTitle()).to.equal('PACT Act | Veterans Affairs');
+    expect(customizeTitle()).to.equal(
+      'Learn how the PACT Act may effect you | Veterans Affairs',
+    );
   });
 });
--- src/applications/pact-act/utilities/customize-title.js
+++ src/applications/pact-act/utilities/customize-title.js
@@ -1,9 +1,6 @@
 // Used to customize the <title> element in the head of the document
 // for accessibility
-export const customizeTitle = h1 => {
-  if (h1) {
-    return `${h1} | PACT Act | Veterans Affairs`;
-  }
-
-  return 'PACT Act | Veterans Affairs';
+export const customizeTitle = () => {
+  // ignores provided h1 parameter from previous behavior
+  return 'Learn how the PACT Act may effect you | Veterans Affairs';
 };
