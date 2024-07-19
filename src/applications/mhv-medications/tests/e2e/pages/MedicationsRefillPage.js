a12b2466874f872e91b5fb6a8d4944fe67dfeca1
Carlos Felix <carlos.felix@agile6.com>
2024-07-19 14:10:50 -0400

    Simplified changes to medications e2e

--- src/applications/mhv-medications/tests/e2e/pages/MedicationsRefillPage.js
+++ src/applications/mhv-medications/tests/e2e/pages/MedicationsRefillPage.js
@@ -121,11 +121,11 @@ class MedicationsRefillPage {
 
   clickMedicationsLandingPageBreadcrumbsOnRefillPage = () => {
     cy.get('[data-testid="rx-breadcrumb"]').should('be.visible');
-    cy.get('va-breadcrumbs').within(() => {
-      cy.get(`[href="${medicationsUrls.MEDICATIONS_ABOUT}"]`).click({
+    cy.get('[data-testid="rx-breadcrumb"]')
+      .find(`[href="${medicationsUrls.MEDICATIONS_ABOUT}"]`)
+      .click({
         waitForAnimations: true,
       });
-    });
   };
 
   verifyShippedMedicationOnRefillPage = () => {
