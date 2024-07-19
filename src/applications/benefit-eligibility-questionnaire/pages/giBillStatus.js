aef3aaaf00e82760fbf0bdbd153938653c70c375
Ian <ianseabock@gmail.com>
2024-07-19 11:07:43 -0700

    Vets Transition: Benefits Questionnaire - GI Bill Status

---
+++ src/applications/benefit-eligibility-questionnaire/pages/giBillStatus.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default {
+  uiSchema: {
+    giBillStatus: {
+      'ui:title': (
+        <>
+          <p>
+            <b>Have you applied for and beeen approved for GI Bill benefits?</b>
+          </p>
+          <p>
+            This includes the Post-9/11 GI Bill, Montgomery GI Bill Active Duty
+            (MGIB-AD), and the Montgomery GI Bill Selected Reserve (MGIB-SR).
+          </p>
+        </>
+      ),
+      'ui:widget': 'radio',
+      'ui:options': {
+        widgetProps: {
+          YES: { giBillStatus: 'Yes' },
+          NO: { giBillStatus: 'No' },
+        },
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      giBillStatus: {
+        type: 'string',
+        enum: ['Yes', 'No'],
+      },
+    },
+  },
+};
