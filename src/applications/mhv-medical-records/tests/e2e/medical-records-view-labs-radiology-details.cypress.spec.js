377d1f864563fdf51ebd93de9b28ed244d6c26e8
Aaron Read <Aaron.Read@va.gov>
2024-07-19 15:29:46 -0600

    Update medical-records-view-labs-radiology-details.cypress.spec.js

--- src/applications/mhv-medical-records/tests/e2e/medical-records-view-labs-radiology-details.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-view-labs-radiology-details.cypress.spec.js
@@ -4,58 +4,53 @@ import LabsAndTestsListPage from './pages/LabsAndTestsListPage';
 import labsAndTests from '../fixtures/labsAndTests.json';
 import radiologyRecordsMhv from '../fixtures/radiologyRecordsMhv.json';
 
-for (let i = 0; i < 200; i += 1) {
-  describe('Medical Records Redirect Users to MHV Classic to view images', () => {
-    const site = new MedicalRecordsSite();
+describe('Medical Records Redirect Users to MHV Classic to view images', () => {
+  const site = new MedicalRecordsSite();
 
-    before(() => {
-      site.login();
-      // cy.visit('my-health/medical-records/labs-and-tests');
-      LabsAndTestsListPage.goToLabsAndTests();
-      cy.reload();
-    });
-
-    it('Navigate to MHV Classic to view their Radiology Images', () => {
-      LabsAndTestsListPage.loadVAPaginationNext();
-      // I think the second parameter doesn't do anything in this case...
-      LabsAndTestsListPage.clickLabsAndTestsDetailsLink(
-        5,
-        labsAndTests.entry[2],
-      );
-
-      RadiologyDetailsPage.verifyTitle(radiologyRecordsMhv[2].procedureName);
-      // why is the date value in radiologyRecordsMHV.json
-      // a large number, like "eventDate": 976929600003, ???
-      // can I test against the JSON or do I neeed to hard-code the date in the test?
-      RadiologyDetailsPage.verifyDate('December 15, 2000');
-      RadiologyDetailsPage.verifyRadiologyReason('None noted');
-
-      // Regex: replace \r\n line terminating characters, with spaces
-      // then eliminate multiple spaces
-      RadiologyDetailsPage.verifyRadiologyClinicalHistory(
-        `${radiologyRecordsMhv[2].clinicalHistory
-          .replace(/[\r\n]+/gm, ' ')
-          .replace(/ +(?= )/g, '')}`,
-      );
-
-      RadiologyDetailsPage.verifyRadiologyOrderedBy(
-        radiologyRecordsMhv[2].requestingProvider, // 'DOE, JOHN A'
-      );
-
-      RadiologyDetailsPage.verifyRadiologyImagingLocation(
-        radiologyRecordsMhv[2].performingLocation,
-      );
-
-      RadiologyDetailsPage.verifyRadiologyImagingProvider(
-        radiologyRecordsMhv[2].radiologist,
-      );
-
-      RadiologyDetailsPage.verifyRadiologyResults(
-        radiologyRecordsMhv[2].impressionText.replace(/[\r\n]+/gm, ' '),
-      );
-
-      cy.injectAxe();
-      cy.axeCheck('main', {});
-    });
+  before(() => {
+    site.login();
+    // cy.visit('my-health/medical-records/labs-and-tests');
+    LabsAndTestsListPage.goToLabsAndTests();
   });
-}
+
+  it('Navigate to MHV Classic to view their Radiology Images', () => {
+    cy.reload();
+    LabsAndTestsListPage.loadVAPaginationNext();
+    // I think the second parameter doesn't do anything in this case...
+    LabsAndTestsListPage.clickLabsAndTestsDetailsLink(5, labsAndTests.entry[2]);
+
+    RadiologyDetailsPage.verifyTitle(radiologyRecordsMhv[2].procedureName);
+    // why is the date value in radiologyRecordsMHV.json
+    // a large number, like "eventDate": 976929600003, ???
+    // can I test against the JSON or do I neeed to hard-code the date in the test?
+    RadiologyDetailsPage.verifyDate('December 15, 2000');
+    RadiologyDetailsPage.verifyRadiologyReason('None noted');
+
+    // Regex: replace \r\n line terminating characters, with spaces
+    // then eliminate multiple spaces
+    RadiologyDetailsPage.verifyRadiologyClinicalHistory(
+      `${radiologyRecordsMhv[2].clinicalHistory
+        .replace(/[\r\n]+/gm, ' ')
+        .replace(/ +(?= )/g, '')}`,
+    );
+
+    RadiologyDetailsPage.verifyRadiologyOrderedBy(
+      radiologyRecordsMhv[2].requestingProvider, // 'DOE, JOHN A'
+    );
+
+    RadiologyDetailsPage.verifyRadiologyImagingLocation(
+      radiologyRecordsMhv[2].performingLocation,
+    );
+
+    RadiologyDetailsPage.verifyRadiologyImagingProvider(
+      radiologyRecordsMhv[2].radiologist,
+    );
+
+    RadiologyDetailsPage.verifyRadiologyResults(
+      radiologyRecordsMhv[2].impressionText.replace(/[\r\n]+/gm, ' '),
+    );
+
+    cy.injectAxe();
+    cy.axeCheck('main', {});
+  });
+});
