a92ff2b88f6038c585edfc1fbbcca6a70a17a514
Eli Selkin <eli.j.selkin@gmail.com>
2024-07-22 16:34:17 -0700

    remove function, use string instead of fn result for title

--- src/applications/pact-act/utilities/customize-title.js
+++
@@ -1,6 +0,0 @@
-// Used to customize the <title> element in the head of the document
-// for accessibility
-export const customizeTitle = () => {
-  // ignores provided h1 parameter from previous behavior
-  return 'Learn how the PACT Act may effect you | Veterans Affairs';
-};
