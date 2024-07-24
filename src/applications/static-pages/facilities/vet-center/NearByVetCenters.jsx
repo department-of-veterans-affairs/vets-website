e8f2054c9a04c997cccb9c7506aad0c55197a4b5
Eli Selkin <eli.j.selkin@gmail.com>
2024-07-24 15:26:35 -0700

    add test for no nearby locations

--- src/applications/static-pages/facilities/vet-center/NearByVetCenters.jsx
+++ src/applications/static-pages/facilities/vet-center/NearByVetCenters.jsx
@@ -231,7 +231,8 @@ const NearByVetCenters = props => {
         <h2
           className="vads-u-font-size--xl vads-u-margin-top--3 medium-screen:vads-u-margin-top--5 vads-u-margin-bottom--2p5
                   medium-screen:vads-u-margin-bottom--3"
-          id="other-near-locations"
+          id="other-near-locations-not-found"
+          data-testid="other-near-locations-not-found"
         >
           Other nearby Vet Centers
         </h2>
