f8eac8bea8b5e4000d80b2f3cc3cd34bd084e3aa
James Zaccardo <james.zaccardo@adhocteam.us>
2024-07-25 19:36:45 -0400

    Revert "resolving merge conflicts in the github UI caused problems"
    This reverts commit 02e64322ec7e168efaedaed8c29cb86aff20bd74.

--- src/applications/mhv-landing-page/components/LandingPage.jsx
+++ src/applications/mhv-landing-page/components/LandingPage.jsx
@@ -34,30 +34,35 @@ const LandingPage = ({
   showVerifyAndRegisterAlert = showVerifyAndRegisterAlertFn,
 }) => {
   const { cards = [], hubs = [] } = data;
-  const verified = useSelector(isLOA3);
-  const registered = useSelector(isVAPatient) && verified;
+  const userVerified = useSelector(isLOA3);
+  const vaPatient = useSelector(isVAPatient);
+  const userRegistered = userVerified && vaPatient;
   const signInService = useSelector(signInServiceName);
   const userHasMhvAccount = useSelector(hasMhvAccount);
   const userHasMhvBasicAccount = useSelector(hasMhvBasicAccount);
   const showWelcomeMessage = useSelector(personalizationEnabled);
+  const showHelpdeskInfo = useSelector(helpdeskInfoEnabled) && registered;
   const showsVerifyAndRegisterAlert = useSelector(showVerifyAndRegisterAlert);
   const serviceLabel = SERVICE_PROVIDERS[signInService]?.label;
   const unVerifiedHeadline = `Verify your identity to use your ${serviceLabel} account on My HealtheVet`;
 
-  useEffect(() => {
-    if (!verified) {
-      recordEvent({
-        event: 'nav-alert-box-load',
-        action: 'load',
-        'alert-box-headline': unVerifiedHeadline,
-        'alert-box-status': 'continue',
-      });
-    }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  useEffect(
+    () => {
+      if (!userVerified) {
+        recordEvent({
+          event: 'nav-alert-box-load',
+          action: 'load',
+          'alert-box-headline': unVerifiedHeadline,
+          'alert-box-status': 'continue',
+        });
+      }
+    },
+    [recordEvent, unVerifiedHeadline, userVerified],
+  );
 
   return (
     <>
-      {registered && <MhvSecondaryNav />}
+      {userRegistered && <MhvSecondaryNav />}
       <div
         className="vads-u-margin-y--3 medium-screen:vads-u-margin-y--5"
         data-testid="landing-page-container"
@@ -67,7 +72,10 @@ const LandingPage = ({
             dependencies={[externalServices.mhvPlatform]}
             render={renderMHVDowntime}
           />
-          <HeaderLayout showWelcomeMessage={showWelcomeMessage} />
+          <HeaderLayout
+            showWelcomeMessage={showWelcomeMessage}
+            showLearnMore={userRegistered}
+          />
           <LandingPageAlerts
             registered={registered}
             verified={verified}
@@ -77,9 +85,9 @@ const LandingPage = ({
             signInService={signInService}
             unVerifiedHeadline={unVerifiedHeadline}
           />
-          {registered && <CardLayout data={cards} />}
+          {registered && <CardLayout data={cards} />
         </div>
-        {registered && (
+        {userRegistered && (
           <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
             <div className="vads-l-row vads-u-margin-top--3">
               <div className="vads-l-col medium-screen:vads-l-col--8">
@@ -88,8 +96,8 @@ const LandingPage = ({
             </div>
           </div>
         )}
-        <HubLinks hubs={hubs} />
-        <NewsletterSignup />
+        {userRegistered && <HubLinks hubs={hubs} />}
+        {userRegistered && <NewsletterSignup />}
       </div>
     </>
   );
