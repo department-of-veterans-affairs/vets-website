526479770d101de5255a0ac1dbee2ab699dbae5f
Michael Clement <michael.clement@adhocteam.us>
2024-07-25 08:12:56 -0500

    updated form signature headings

--- src/applications/ivc-champva/10-7959C/config/form.js
+++ src/applications/ivc-champva/10-7959C/config/form.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import environment from '@department-of-veterans-affairs/platform-utilities/environment';
 import get from 'platform/utilities/data/get';
 import manifest from '../manifest.json';
@@ -90,6 +91,11 @@ const formConfig = {
     required: true,
     CustomComponent: CustomAttestation,
   },
+  customText: {
+    reviewPageTitle: 'Review form',
+    appType: 'form',
+  },
+  CustomReviewTopContent: () => <h3>Review and sign</h3>,
   saveInProgress: {
     messages: {
       inProgress:
@@ -465,7 +471,7 @@ const formConfig = {
       },
     },
     formSignature: {
-      title: 'Signer information',
+      title: 'Form signature',
       pages: {
         formSignature: {
           path: 'form-signature',
