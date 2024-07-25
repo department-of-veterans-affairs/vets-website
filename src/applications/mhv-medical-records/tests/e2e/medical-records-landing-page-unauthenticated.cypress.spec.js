3b5d8db7fcfdd8a507caa13c440d0a6f6e0ce31b
Richard Davis <richard.davis@agile6.com>
2024-07-25 16:47:11 -0400

    Updates unauthenticated  e2e spec

--- src/applications/mhv-medical-records/tests/e2e/medical-records-landing-page-unauthenticated.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-landing-page-unauthenticated.cypress.spec.js
@@ -1,26 +1,14 @@
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
-// import VitalsDetailsPage from './pages/VitalsDetailsPage';
 
-import nonMrUser from './fixtures/non_mr_user.json';
-
-describe.skip('Medical Records Unauthenticated Users', () => {
-  it('Visits Medical Records Unauthenticated Users', () => {
-    const site = new MedicalRecordsSite();
-    // Unauthenticated users implement visibility restrictions
-    site.login(nonMrUser);
-    site.loadPage();
-    cy.url().should('contain', 'health-care/get-medical-records');
-    cy.visit('my-health/medical-records');
-
-    // Authenticated users
-    /*
-    site.login();
-    cy.visit('my-health/medical-records/vitals');
-    // Verify "Vitals" Page title Text
-    VitalsDetailsPage.verifyVitalsPageText('Vitals');
-    */
-    // Axe check
-    cy.injectAxe();
-    cy.axeCheck('main');
+describe('Medical Records Landing Page', () => {
+  describe('as an unauthenticated user', () => {
+    it('prompts the user to sign in', () => {
+      const site = new MedicalRecordsSite();
+      site.mockFeatureToggles();
+      site.mockVamcEhr();
+      cy.visit('/my-health/medical-records');
+      cy.findByRole('heading', { level: 1, name: 'Sign in' });
+      cy.injectAxeThenAxeCheck();
+    });
   });
 });
