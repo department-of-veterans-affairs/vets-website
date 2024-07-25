a96d35c58d809911d9c9e3557087d1e5ffb34cf2
Vic Saleem <victoria.saleem@va.gov>
2024-07-25 11:10:27 -0500

    cleanup draftDetails unit test

--- src/applications/mhv-secure-messaging/tests/actions/draftDetails.unit.spec.jsx
+++ src/applications/mhv-secure-messaging/tests/actions/draftDetails.unit.spec.jsx
@@ -103,11 +103,18 @@ describe('draftDetails actions', () => {
     mockApiRequest(saveDraftResponse);
     const store = mockStore({ sm: {} });
     await store
-      .dispatch(saveReplyDraft('1234', requestMessageData, 'manual'))
+      .dispatch(
+        saveReplyDraft(
+          1234,
+          requestMessageData,
+          'manual',
+          saveDraftResponse.data.id,
+        ),
+      )
       .then(() => {
         expect(store.getActions()).to.deep.include({
           type: Actions.Thread.DRAFT_SAVE_STARTED,
-          payload: { messageId: undefined },
+          payload: { messageId: saveDraftResponse.data.id },
         });
         expect(store.getActions()).to.deep.include({
           type: Actions.Draft.CREATE_SUCCEEDED,
@@ -154,11 +161,18 @@ describe('draftDetails actions', () => {
     mockApiRequest(saveDraftResponse);
     const store = mockStore({ sm: {} });
     await store
-      .dispatch(saveReplyDraft('1234', requestMessageData, 'auto'))
+      .dispatch(
+        saveReplyDraft(
+          1234,
+          requestMessageData,
+          'auto',
+          saveDraftResponse.data.id,
+        ),
+      )
       .then(() => {
         expect(store.getActions()).to.deep.include({
           type: Actions.Thread.DRAFT_SAVE_STARTED,
-          payload: { messageId: undefined },
+          payload: { messageId: saveDraftResponse.data.id },
         });
         expect(store.getActions()).to.deep.include({
           type: Actions.Draft.CREATE_SUCCEEDED,
