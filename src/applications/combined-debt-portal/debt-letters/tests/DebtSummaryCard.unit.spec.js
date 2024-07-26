caac7fd9f5e26f44f10edd6eacd1188dd6ce5267
Joseph Lee <joseph@cityfriends.tech>
2024-07-26 15:38:13 -0700

    Updated unit test to account for new heading structure

--- src/applications/combined-debt-portal/debt-letters/tests/DebtSummaryCard.unit.spec.js
+++ src/applications/combined-debt-portal/debt-letters/tests/DebtSummaryCard.unit.spec.js
@@ -97,8 +97,6 @@ describe('DebtSummaryCard', () => {
       </Provider>,
     );
 
-    expect(wrapper.find('h3').text()).to.equal(
-      '$10,000.00 Chapter 35 education debt',
-    );
+    expect(wrapper.find('h2').text()).to.equal('Chapter 35 education debt');
   });
 });
