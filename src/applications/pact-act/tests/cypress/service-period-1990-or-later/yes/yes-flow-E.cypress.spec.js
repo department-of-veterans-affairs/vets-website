546bb5b2c1270a1128cbcb402519e9e645c65e24
Randi Mays <randimays1024@gmail.com>
2024-07-26 10:33:25 -0500

    VACMS-16128 PACT Act content updates (#30966)

--- src/applications/pact-act/tests/cypress/service-period-1990-or-later/yes/yes-flow-E.cypress.spec.js
+++ src/applications/pact-act/tests/cypress/service-period-1990-or-later/yes/yes-flow-E.cypress.spec.js
@@ -1,16 +1,24 @@
-import * as h from '../helpers';
-import { ROUTES } from '../../../constants';
+import * as h from '../../helpers';
+import { ROUTES } from '../../../../constants';
+
+// Flow E
+// Service Period - 1990 or later
+// Burn Pit 2.1 - No
+// Burn Pit 2.1.1 - No
+// Burn Pit 2.1.2 - No
+// Burn Pit 2.1.3 - No
+// Main Flow 2.5 - Yes
+// Results 4
 
 // Note: anything requiring a VA button click is tested here as unit tests cannot
 // target the shadow DOM
 describe('PACT Act', () => {
-  describe(`1990 or later - "I'm not sure" to all questions (Results Screen 3)`, () => {
+  describe('1990 or later -  "Yes" to the main flow question (Results page 4)', () => {
     it('navigates through the flow forward and backward successfully', () => {
       cy.visit(h.ROOT);
 
       // Home
       h.verifyUrl(ROUTES.HOME);
-      h.verifyElement(h.START_LINK);
       cy.injectAxeThenAxeCheck();
       h.clickStart();
 
@@ -21,23 +29,41 @@ describe('PACT Act', () => {
 
       // BURN_PIT_2_1
       h.verifyUrl(ROUTES.BURN_PIT_2_1);
-      h.selectRadio(h.BURN_PIT_2_1_INPUT, 2);
+      h.selectRadio(h.BURN_PIT_2_1_INPUT, 1);
       h.clickContinue();
 
       // BURN_PIT_2_1_1
       h.verifyUrl(ROUTES.BURN_PIT_2_1_1);
-      h.selectRadio(h.BURN_PIT_2_1_1_INPUT, 2);
+      h.selectRadio(h.BURN_PIT_2_1_1_INPUT, 1);
       h.clickContinue();
 
       // BURN_PIT_2_1_2
       h.verifyUrl(ROUTES.BURN_PIT_2_1_2);
-      h.selectRadio(h.BURN_PIT_2_1_2_INPUT, 2);
+      h.selectRadio(h.BURN_PIT_2_1_2_INPUT, 1);
       h.clickContinue();
 
-      // RESULTS 3
-      h.verifyUrl(ROUTES.RESULTS_3);
-      h.verifyElement(h.RESULTS_3_HEADER);
+      // BURN_PIT_2_1_3
+      h.verifyUrl(ROUTES.BURN_PIT_2_1_3);
+      h.selectRadio(h.BURN_PIT_2_1_3_INPUT, 1);
+      h.clickContinue();
+
+      // MAIN_FLOW_2_5
+      h.verifyUrl(ROUTES.MAIN_FLOW_2_5);
+      h.selectRadio(h.MAIN_FLOW_2_5_INPUT, 0);
+      h.clickContinue();
+
+      // RESULTS 4
+      h.verifyUrl(ROUTES.RESULTS_4);
+      h.verifyElement(h.RESULTS_4_HEADER);
       h.clickResultsBack();
+
+      // MAIN_FLOW_2_5
+      h.verifyUrl(ROUTES.MAIN_FLOW_2_5);
+      h.clickBack();
+
+      // BURN_PIT_2_1_3
+      h.verifyUrl(ROUTES.BURN_PIT_2_1_3);
+      h.clickBack();
 
       // BURN_PIT_2_1_2
       h.verifyUrl(ROUTES.BURN_PIT_2_1_2);
