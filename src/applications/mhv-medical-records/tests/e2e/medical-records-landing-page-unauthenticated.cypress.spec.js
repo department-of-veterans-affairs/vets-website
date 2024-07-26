9c62de3656e742ef197268de9086ab1080954c26
Richard Davis <richard.davis@agile6.com>
2024-07-26 11:25:10 -0400

    Updates

--- src/applications/mhv-medical-records/tests/e2e/medical-records-landing-page-unauthenticated.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-landing-page-unauthenticated.cypress.spec.js
@@ -1,15 +1,20 @@
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
 
 describe('Medical Records Landing Page', () => {
+  beforeEach(() => {
+    cy.intercept('/?next=*', cy.spy().as('rootPath')).as('redirect');
+  });
+
   describe('as an unauthenticated user', () => {
-    it('prompts the user to sign in', () => {
+    // eslint-disable-next-line @department-of-veterans-affairs/axe-check-required
+    it('redirects the user to /?next=* and prompts to sign in', () => {
       const site = new MedicalRecordsSite();
       site.mockFeatureToggles();
       site.mockVamcEhr();
       cy.visit('/my-health/medical-records/');
-      cy.wait(['@featureToggles', '@vamcEhr']);
+      cy.wait(['@featureToggles', '@vamcEhr', '@redirect']);
+      cy.get('@rootPath').should('have.been.called');
       cy.findByRole('heading', { level: 1, name: 'Sign in' });
-      cy.injectAxeThenAxeCheck();
     });
   });
 });
