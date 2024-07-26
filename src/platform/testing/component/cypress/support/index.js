0314dbf5c898421319222ec9c5aa697735d9f7a6
Adrian Rollett <acrollet@gmail.com>
2024-07-26 16:44:38 +0000

    Load web components for tests

--- src/platform/testing/component/cypress/support/index.js
+++ src/platform/testing/component/cypress/support/index.js
@@ -1,6 +1,6 @@
 import { mount } from 'cypress/react'
+import '../../../../site-wide/wc-loader.js';
 
 Cypress.Commands.add('mount', (component, options) => {
   return mount(component, options)
 });
-
