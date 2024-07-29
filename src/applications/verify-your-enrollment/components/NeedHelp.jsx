713c0ead7458da846541cacc2a5e2aa189a4c2ef
fatmakhan0395 <Fatma.khan@va.gov>
2024-07-28 21:00:12 -0700

    deleted second instance of tty

--- src/applications/verify-your-enrollment/components/NeedHelp.jsx
+++ src/applications/verify-your-enrollment/components/NeedHelp.jsx
@@ -7,7 +7,7 @@ const NeedHelp = () => {
         <div slot="content">
           <p>
             Call us at <va-telephone contact="8884424551" /> (
-            <span className="vads-u-margin-left--0p5">
+            <span>
               <va-telephone contact="711" tty="true" />
             </span>
             ). We’re here Monday through Friday, 8:00 a.m to 7:00 p.m. ET.
