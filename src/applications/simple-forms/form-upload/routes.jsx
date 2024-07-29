7dfcd0df99aefaefc1e2f63b88af1fa8ff3c6777
Jacob Penner <161746194+pennja@users.noreply.github.com>
2024-07-29 13:51:38 -0400

    update introduction page configurations (#31060)

--- src/applications/simple-forms/form-upload/routes.jsx
+++ src/applications/simple-forms/form-upload/routes.jsx
@@ -1,23 +1,34 @@
 import { createRoutesWithSaveInProgress } from 'platform/forms/save-in-progress/helpers';
+import environment from '~/platform/utilities/environment';
 import formConfig from './config/form';
-import App from './containers/App.jsx';
+import App from './containers/App';
+
+// Add any new form-upload forms to this list
+const formUploadForms = ['21-0779'];
 
 const config = formConfig();
-
-const routes = [
-  {
-    path: '/21-0779',
-    component: App,
-    indexRoute: { onEnter: (nextState, replace) => replace('/introduction') },
-    childRoutes: createRoutesWithSaveInProgress(config),
+const baseUrl = environment.BASE_URL.replace(/3001/g, '3002');
+const indexRouteByForm = formId => ({
+  onEnter: () => {
+    window.location.href = `${baseUrl}/find-forms/about-form-${formId}/`;
   },
-  // or dynamic
-  // {
-  //   path: '/:formId',
-  //   component: App,
-  //   indexRoute: {},
-  //   childRoutes: createRoutesWithSaveInProgress(config),
-  // },
-];
+});
+
+const routes = formUploadForms.map(formId => {
+  return {
+    path: `/${formId}`,
+    component: App,
+    indexRoute: indexRouteByForm(formId),
+    childRoutes: createRoutesWithSaveInProgress(config),
+  };
+});
+
+// or dynamic
+// {
+//   path: '/:formId',
+//   component: App,
+//   indexRoute: indexRouteByForm(':formId'),
+//   childRoutes: createRoutesWithSaveInProgress(config),
+// },
 
 export default routes;
