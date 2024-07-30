2af6026ef4f3a689d4a116dde8f9965f2e751e1e
James Zaccardo <james.zaccardo@adhocteam.us>
2024-07-30 16:43:30 -0400

    fix link text svg rendering on next line when screen size is inbetween desktop and phone

--- src/applications/mhv-landing-page/components/NavCard.jsx
+++ src/applications/mhv-landing-page/components/NavCard.jsx
@@ -27,18 +27,22 @@ const NavCard = ({
         }}
         rel="noreferrer"
       >
-        <span
-          className={ariaLabel?.includes('unread') ? 'mhv-c-indicator' : ''}
-        >
-          {text} {isExternal && externalLinkText}
+        <span className="mhv-c-link-content">
+          <span
+            className={`mhv-c-link-text ${
+              ariaLabel?.includes('unread') ? 'mhv-c-indicator' : ''
+            }`}
+          >
+            {text} {isExternal && externalLinkText}
+          </span>
+          {!isExternal && (
+            <va-icon
+              class="link-icon vads-u-margin-right--neg1 medium-screen:vads-u-margin-right--0"
+              icon="navigate_next"
+              size={4}
+            />
+          )}
         </span>
-        {!isExternal && (
-          <va-icon
-            class="vads-u-margin-right--neg1 medium-screen:vads-u-margin-right--0"
-            icon="navigate_next"
-            size={4}
-          />
-        )}
       </a>
     </li>
   ));
