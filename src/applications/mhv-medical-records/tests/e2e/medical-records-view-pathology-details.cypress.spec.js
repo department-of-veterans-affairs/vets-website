e6b1d8ba34ece83ec9f7c2b3e58c7ca34266a700
mattwrightva <107576133+mattwrightva@users.noreply.github.com>
2024-07-30 09:27:20 -0600

    MHV-48192: Radiology data gaps fixed for mhv data (#30723)
    * MHV-48192: Radiology data gaps fixed for mhv data
    
    * MHV-48192: Unit tests written
    
    * MHV-48192: Filtered out FHIR radiology results from FHIR data
    
    * Revised radiology details test, for data gap fixes
    
    * Update medical-records-view-labs-radiology-details.cypress.spec.js
    
    * Update to medical-records-view-labs-radiology-images
    
    * Update LabsAndTestsListPage.js
    
    * Update LabsAndTestsListPage.js
    
    look for showing records
    
    * Update medical-records-view-labs-radiology-details.cypress.spec.js
    
    * Update medical-records-view-labs-radiology-details.cypress.spec.js
    
    * Update view-pathology-details test
    
    * MHV-48192: Sorting by date added to labs and tests reducer
    
    * Update medical-records-view-labs-radiology-details.cypress.spec.js
    
    * Update medical-records-download-txt-functionality-microbiology.cypress.spec.js
    
    * skipping EKG tests
    
    * Update medical-records-download-txt-functionality-Pathology.cypress.spec.js
    
    * update labsAndTests.json, radiology details test
    
    * Update medical-records-download-txt-functionality-microbiology.cypress.spec.js
    
    * Update medical-records-understanding-your-results-chem-hem-detail-page.cypress.spec.js
    
    * Update medical-records-understanding-your-results-radiology-detail-page.cypress.spec.js
    
    * update view-labs-chem-hem-details
    
    * Update view-labs-microbiology-details
    
    * update view-pathology-details, microbio.json
    
    * MHV-48192: Unit test fixes
    
    * MHV-48192: MHV radiology get one API call removed
    
    * MHV-48192: MHV radiology get one action fix
    
    ---------
    
    Co-authored-by: Matthew Wright <matwright2010@gmail.com>
    Co-authored-by: Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
    Co-authored-by: Aaron Read <Aaron.Read@va.gov>

--- src/applications/mhv-medical-records/tests/e2e/medical-records-view-pathology-details.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-view-pathology-details.cypress.spec.js
@@ -1,21 +1,25 @@
+import moment from 'moment';
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
 import LabsAndTestsListPage from './pages/LabsAndTestsListPage';
 import PathologyDetailsPage from './pages/PathologyDetailsPage';
-import labsAndTests from '../fixtures/labsAndTests.json';
+import pathology from './fixtures/labs-and-tests/pathology.json';
 
 describe('Medical Records View Labs And Tests', () => {
   it('Visits Medical Records View Labs And Tests Details', () => {
     const site = new MedicalRecordsSite();
     site.login();
-    // cy.visit('my-health/medical-records/labs-and-tests');
     LabsAndTestsListPage.goToLabsAndTests();
-    LabsAndTestsListPage.clickLabsAndTestsDetailsLink(9, labsAndTests.entry[9]);
-    PathologyDetailsPage.verifyLabName('LR SURGICAL PATHOLOGY REPORT');
-    PathologyDetailsPage.verifyLabDate('August 10, 2000');
+    LabsAndTestsListPage.clickLabsAndTestsDetailsLink(9, pathology);
+    PathologyDetailsPage.verifyLabName(pathology.code.text);
+    PathologyDetailsPage.verifyLabDate(
+      moment(pathology.effectiveDateTime).format('MMMM D, YYYY'),
+    );
     PathologyDetailsPage.verifySampleTested('None noted');
     PathologyDetailsPage.verifyLabLocation('None noted');
-    PathologyDetailsPage.verifyDateCompleted('August 10, 2000');
-    PathologyDetailsPage.verifyReport('OLD HARDWARE LEFT FOOT X2');
+    PathologyDetailsPage.verifyDateCompleted(
+      moment(pathology.effectiveDateTime).format('MMMM D, YYYY'),
+    );
+    PathologyDetailsPage.verifyReport(pathology.conclusion);
     // Axe check
     cy.injectAxe();
     cy.axeCheck('main');
