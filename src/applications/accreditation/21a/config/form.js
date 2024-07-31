36d04e4ad0d3cedc09ee70411df77bcdbd078cfc
William Phelps <williamphelps13@gmail.com>
2024-07-31 07:52:57 -0700

    Add transformForSubmit to allow partial address (also adds dev tools)

--- src/applications/accreditation/21a/config/form.js
+++ src/applications/accreditation/21a/config/form.js
@@ -2,10 +2,11 @@ import environment from '@department-of-veterans-affairs/platform-utilities/envi
 import FormFooter from 'platform/forms/components/FormFooter';
 import { VA_FORM_IDS } from 'platform/forms/constants';
 
-import GetFormHelp from '../components/common/GetFormHelp';
+import manifest from '../manifest.json';
+import transformForSubmit from './submit-transformer';
 import ConfirmationPage from '../containers/ConfirmationPage';
 import IntroductionPage from '../containers/IntroductionPage';
-import manifest from '../manifest.json';
+import GetFormHelp from '../components/common/GetFormHelp';
 import personalInformationChapter from '../pages/01-personal-information-chapter';
 import militaryServiceChapter from '../pages/02-military-service-chapter';
 import employmentInformationChapter from '../pages/03-employment-information-chapter';
@@ -14,11 +15,13 @@ import lawPracticeInformationChapter from '../pages/05-law-practice-information-
 import backgroundInformationChapter from '../pages/06-background-information-chapter';
 import characterReferencesChapter from '../pages/07-character-references-chapter';
 
+/** @type {FormConfig} */
 const formConfig = {
   formId: VA_FORM_IDS.FORM_21A,
   version: 0,
   rootUrl: manifest.rootUrl,
   urlPrefix: '/',
+  transformForSubmit,
   submitUrl: `${
     environment.API_URL
   }/accredited_representative_portal/v0/form21a`,
@@ -32,14 +35,18 @@ const formConfig = {
   getHelp: GetFormHelp,
   errorText: '',
   prefillEnabled: true,
+  dev: {
+    collapsibleNavLinks: true,
+    showNavLinks: !window.Cypress,
+  },
   preSubmitInfo: {
-    // statementOfTruth: {
-    //   body:
-    //     'I confirm that the identifying information in this form is accurate has been represented correctly.',
-    //   messageAriaDescribedby:
-    //     'I confirm that the identifying information in this form is accurate has been represented correctly.',
-    //   fullNamePath: 'fullName',
-    // },
+    statementOfTruth: {
+      body:
+        'I confirm that the identifying information in this form is accurate has been represented correctly.',
+      messageAriaDescribedby:
+        'I confirm that the identifying information in this form is accurate has been represented correctly.',
+      fullNamePath: 'fullName',
+    },
   },
   saveInProgress: {
     messages: {
@@ -60,12 +67,12 @@ const formConfig = {
   defaultDefinitions: {},
   chapters: {
     personalInformationChapter,
-    // militaryServiceChapter,
-    // employmentInformationChapter,
-    // educationHistoryChapter,
-    // lawPracticeInformationChapter,
-    // backgroundInformationChapter,
-    // characterReferencesChapter,
+    militaryServiceChapter,
+    employmentInformationChapter,
+    educationHistoryChapter,
+    lawPracticeInformationChapter,
+    backgroundInformationChapter,
+    characterReferencesChapter,
   },
 };
 
