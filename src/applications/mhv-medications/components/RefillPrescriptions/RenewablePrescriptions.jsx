f5fc69f48bb5c85117bd48da38b17e012eac3567
agravell047 <anthony.gravell@bylight.com>
2024-07-31 11:20:12 -0400

    fix console error for rx

--- src/applications/mhv-medications/components/RefillPrescriptions/RenewablePrescriptions.jsx
+++ src/applications/mhv-medications/components/RefillPrescriptions/RenewablePrescriptions.jsx
@@ -80,7 +80,6 @@ const RenewablePrescriptions = ({ renewablePrescriptionsList = [] }) => {
         </Link>
         <p>Or you may need to renew your prescription to get more refills.</p>
         <Link
-          class="vads-u-margin-y--0"
           to={medicationsUrls.MEDICATIONS_ABOUT_ACCORDION_RENEW.replace(
             medicationsUrls.MEDICATIONS_URL,
             '',
