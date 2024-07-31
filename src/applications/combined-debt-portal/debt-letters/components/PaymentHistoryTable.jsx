24e9cabeb915b20731bce9ab33fd7d2ac3d2f9bc
Brandyn Sullins <brandyn.sullins@gmail.com>
2024-07-31 12:31:05 -0400

    85887 - Payment History VBA Pre-MVP 1.0 - Implement page(s) V2 (#30863)
    * Pair with Aaron to complete initial laydown of Payment history table.
    
    * Table style and detail tweaks
    
    - Cleaned up the spacing and sizing of HTML details and made them more semantic
    - Fixed mobile display for details part
    - Fixed column width for table dates by adding a custom class
    
    * Made debt details more semantic and tweaked custom css
    
    * Added table header for a11y purposes
    
    * re-add joe css
    
    * Remove unused angle right css.
    
    * Update object keys per FE team convo
    
    ---------
    
    Co-authored-by: Joseph Lee <joseph@cityfriends.tech>
    Co-authored-by: Andrew Rodiek <andrew.rodiek@gmail.com>

---
+++ src/applications/combined-debt-portal/debt-letters/components/PaymentHistoryTable.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { renderPaymentHistoryDescription } from '../const/diary-codes';
+import { deductionCodes } from '../const/deduction-codes';
+import { currency } from '../utils/page';
+
+const PaymentHistoryTable = ({ currentDebt }) => {
+  const { paymentHistory } = currentDebt;
+  return (
+    <div className="vads-u-margin-y--4">
+      <va-table table-title="Transaction history">
+        <va-table-row slot="headers">
+          <span>Date</span>
+          <span>Description</span>
+          <span>Amount</span>
+        </va-table-row>
+        {paymentHistory.map((payment, index) => (
+          <va-table-row key={`${payment.transactionDate}-${index}`}>
+            <span className="vads-u-width--fit">{payment.transactionDate}</span>
+            <span>
+              <div className="vads-u-margin-top--0">
+                {renderPaymentHistoryDescription(payment.transactionFiscalCode)}
+              </div>
+            </span>
+            <span>{payment.transactionTotalAmount}</span>
+          </va-table-row>
+        ))}
+        <va-table-row>
+          {/* This is the default row that will always be displayed for initial
+          debt creation */}
+          <span className="vads-u-width--fit">
+            {currentDebt.firstPaymentDate}
+          </span>
+          <span>
+            <strong>
+              Overpayment for{' '}
+              {deductionCodes[currentDebt.deductionCode] ||
+                currentDebt.benefitType}
+            </strong>
+          </span>
+          <span>{currency.format(parseFloat(currentDebt.originalAr))}</span>
+        </va-table-row>
+      </va-table>
+    </div>
+  );
+};
+
+PaymentHistoryTable.propTypes = {
+  currentDebt: PropTypes.object,
+};
+
+export default PaymentHistoryTable;
