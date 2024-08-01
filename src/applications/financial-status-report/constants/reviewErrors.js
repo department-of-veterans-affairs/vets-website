5e7889ef2ad4183bc9b13de2463950e1133b9fb8
Aaron Ponce <aaron.m.ponce@gmail.com>
2024-08-01 16:32:30 -0700

    add assets error messages

--- src/applications/financial-status-report/constants/reviewErrors.js
+++ src/applications/financial-status-report/constants/reviewErrors.js
@@ -1,3 +1,6 @@
+// Link text for review & submit page errors
+// key = "name" from `form.formErrors.errors`
+// see src/platform/forms-system/docs/reviewErrors.md
 import numberToWords from 'platform/forms-system/src/js/utilities/data/numberToWords';
 
 export default {
@@ -20,17 +23,13 @@ export default {
   },
   monthlyHousingExpenses:
     'Please enter a valid dollar amount for your monthly housing expenses',
-
   // Household Assets error messages
-  cashOnHand: 'Please enter a valid dollar amount for cash on hand',
-  cashInBank: 'Please enter a valid dollar amount for cash in bank',
   monetaryAssets: 'Please provide valid information for all monetary assets',
   realEstateValue:
     'Please enter a valid dollar amount for your real estate value',
   recVehicleAmount:
     'Please enter a valid dollar amount for your recreational vehicle',
-  automobiles: 'Please provide valid information for all vehicles',
-  otherAssets: 'Please indicate if you have any other assets',
+  otherAssets: 'Please provide valid information for all other assets',
 
   _override: (error, fullError) => {
     if (error === 'questions') {
@@ -51,20 +50,6 @@ export default {
         pageKey: 'resolutionComment',
       };
     }
-
-    // Household Assets
-    if (error === 'assets.cashOnHand') {
-      return {
-        chapterKey: 'householdAssetsChapter',
-        pageKey: 'cashOnHand',
-      };
-    }
-    if (error === 'assets.cashInBank') {
-      return {
-        chapterKey: 'householdAssetsChapter',
-        pageKey: 'cashInBank',
-      };
-    }
     if (error.startsWith('assets.monetaryAssets')) {
       return {
         chapterKey: 'householdAssetsChapter',
@@ -77,7 +62,6 @@ export default {
         pageKey: 'enhancedRealEstateRecords',
       };
     }
-
     if (error === 'assets.recVehicleAmount') {
       return {
         chapterKey: 'householdAssetsChapter',
@@ -90,21 +74,6 @@ export default {
         pageKey: 'otherAssetsSummary',
       };
     }
-
-    if (
-      error === 'questions.hasVehicle' ||
-      error.includes('automobiles') ||
-      (fullError?.assets?.automobiles &&
-        fullError.assets.automobiles.some(
-          auto => auto.value === null || auto.value === undefined,
-        ))
-    ) {
-      return {
-        chapterKey: 'householdAssetsChapter',
-        pageKey: 'vehiclesSummary',
-      };
-    }
-
     // always return null for non-matches
     return null;
   },
