eb8788a8df737a6c6a78802b0c3702fedec4f4b1
Ryan Shaw <ryan.shaw@adhocteam.us>
2024-08-01 05:57:28 -0700

    Updates unit tests
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/components/tests/VideoLink.unit.spec.js
+++ src/applications/vaos/components/tests/VideoLink.unit.spec.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { expect } from 'chai';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import MockDate from 'mockdate';
 import moment from 'moment-timezone';
+import { getTestDate } from '../../tests/mocks/setup';
+
 import VideoLink from '../VideoLink';
+
+beforeEach(() => {
+  MockDate.set(getTestDate());
+});
+
+afterEach(() => {
+  MockDate.reset();
+});
 
 describe('VAOS Component: VideoLink', () => {
   it('does not render join appoinment link when not 30 minutes before or 4 hours after start time', () => {
@@ -12,84 +23,53 @@ describe('VAOS Component: VideoLink', () => {
         vistaId: '983',
         locationId: '983',
       },
-      start: moment.tz(now, 'America/Anchorage').add(240, 'minutes'),
+      start: moment.tz(now, 'America/New_York').subtract(600, 'minutes'),
       videoData: {
         url: 'test.com',
       },
     };
-    const props = { appointment };
-    const wrapper = render(<VideoLink {...props} />);
-    expect(wrapper.queryByText('Join appointment')).to.exist;
-    expect(wrapper.container.querySelector('.usa-button')).to.exist;
-    expect(wrapper.container.querySelector('.usa-button-disabled')).to.not
-      .exist;
-    // fireEvent.click returns false if event is cancelable, and at least one of the event
-    // handlers which received event called Event.preventDefault(). Otherwise true.
-    // We do not expect preventDefault() to be called if the link is active.
-    expect(fireEvent.click(wrapper.container.querySelector('.usa-button'))).to
-      .be.true;
-  });
-  it('renders disabled join appoinment link 30 minutes prior to start time', () => {
-    // const now = moment();
-    // const appointment = {
-    //   location: {
-    //     vistaId: '983',
-    //     locationId: '983',
-    //   },
-    //   start: moment.tz(now, 'America/New_York').subtract(35, 'minutes'),
-    //   videoData: {
-    //     url: 'test.com',
-    //   },
-    // };
-    // const props = { appointment };
-    // const wrapper = render(<VideoLink {...props} />);
-    // expect(wrapper.queryByText('Join appointment')).to.exist;
-    // expect(wrapper.container.querySelector('.usa-button')).to.exist;
-    // expect(wrapper.container.querySelector('.usa-button-disabled')).to.exist;
-    return true;
-  });
-  it('renders join appoinment link 4 hours after start time', () => {
-    // const now = moment();
-    // const appointment = {
-    //   location: {
-    //     vistaId: '983',
-    //     locationId: '983',
-    //   },
-    //   start: moment(now).add(242, 'minutes'),
-    //   videoData: {
-    //     url: 'test.com',
-    //   },
-    // };
-    // const props = { appointment };
-    // const wrapper = render(<VideoLink {...props} />);
-    // expect(wrapper.queryByText('Join appointment')).to.exist;
-    // expect(wrapper.container.querySelector('.usa-button')).to.exist;
-    // expect(wrapper.container.querySelector('.usa-button-disabled')).to.exist;
-    return true;
+
+    const wrapper = render(<VideoLink appointment={appointment} />);
+
+    expect(wrapper.queryByText('Join appointment')).not.to.exist;
+    expect(
+      wrapper.queryByText(
+        "/We'll add the link to join this appointment 30 minutes before your appointment time/i",
+      ),
+    );
   });
 
-  it('call preventDefault function', () => {
-    // const now = moment();
-    // const appointment = {
-    //   location: {
-    //     vistaId: '983',
-    //     locationId: '983',
-    //   },
-    //   start: moment(now).subtract(35, 'minutes'),
-    //   videoData: {
-    //     url: 'test.com',
-    //   },
-    // };
-    // const props = { appointment };
-    // const wrapper = render(<VideoLink {...props} />);
-    // expect(wrapper.queryByText('Join appointment')).to.exist;
-    // expect(wrapper.container.querySelector('.usa-button')).to.exist;
-    // expect(wrapper.container.querySelector('.usa-button-disabled')).to.exist;
-    // // fireEvent.click returns false if event is cancelable, and at least one of the event
-    // // handlers which received event called Event.preventDefault(). Otherwise true.
-    // // We expect preventDefault() to be called if the link is disabled.
-    // expect(fireEvent.click(wrapper.container.querySelector('.usa-button'))).to
-    //   .be.false;
-    return true;
+  it('renders join appoinment link 30 minutes prior to start time', () => {
+    const now = moment();
+    const appointment = {
+      location: {
+        vistaId: '983',
+        locationId: '983',
+      },
+      start: moment.tz(now, 'America/New_York').subtract(30, 'minutes'),
+      videoData: {
+        url: 'test.com',
+      },
+    };
+
+    const wrapper = render(<VideoLink appointment={appointment} />);
+    expect(wrapper.queryByText('Join appointment')).to.exist;
+  });
+
+  it('renders join appoinment link 4 hours after start time', () => {
+    const now = moment();
+    const appointment = {
+      location: {
+        vistaId: '983',
+        locationId: '983',
+      },
+      start: moment(now).add(240, 'minutes'),
+      videoData: {
+        url: 'test.com',
+      },
+    };
+
+    const wrapper = render(<VideoLink appointment={appointment} />);
+    expect(wrapper.queryByText('Join appointment')).to.exist;
   });
 });
