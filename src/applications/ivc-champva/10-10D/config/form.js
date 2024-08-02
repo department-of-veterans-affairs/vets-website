7269a1ede3b105633cbeed36caa43aa293e25c81
Michael Clement <michael.clement@adhocteam.us>
2024-08-02 10:09:21 -0500

    89589 Refactor and cleanup - form 10-10d (#31133)
    * removed unused markup and associated fn calls + test arguments
    
    * removed unused OMB component
    
    * deleted unnecessary sponsorInfo component
    
    * streamlined unnecessary import
    
    * eliminated unnecesary custom schema file
    
    * removed redundant third party info comp + unused sectioncomplete
    
    * moved required/optional file obj to constants file
    
    * removing commented out tests
    
    * moved signaturekeyname to utilities file

--- src/applications/ivc-champva/10-10D/config/form.js
+++ src/applications/ivc-champva/10-10D/config/form.js
@@ -28,7 +28,6 @@ import get from '@department-of-veterans-affairs/platform-forms-system/get';
 import { blankSchema } from 'platform/forms-system/src/js/utilities/data/profile';
 // import { fileUploadUi as fileUploadUI } from '../components/File/upload';
 
-import { customRelationshipSchema } from '../components/CustomRelationshipPattern';
 import { ssnOrVaFileNumberCustomUI } from '../components/CustomSsnPattern';
 
 import transformForSubmit from './submitTransformer';
@@ -37,13 +36,13 @@ import manifest from '../manifest.json';
 import IntroductionPage from '../containers/IntroductionPage';
 import ApplicantField from '../../shared/components/applicantLists/ApplicantField';
 import ConfirmationPage from '../containers/ConfirmationPage';
-import getNameKeyForSignature from '../helpers/signatureKeyName';
 import {
   isInRange,
   onReviewPage,
-  MAX_APPLICANTS,
   applicantListSchema,
+  getNameKeyForSignature,
 } from '../helpers/utilities';
+import { MAX_APPLICANTS } from './constants';
 import { applicantWording, getAgeInYears } from '../../shared/utilities';
 import {
   sponsorWording,
@@ -53,10 +52,9 @@ import { sponsorNameDobConfig } from '../pages/Sponsor/sponsorInfoConfig';
 import {
   thirdPartyInfoUiSchema,
   thirdPartyInfoSchema,
-} from '../components/ThirdPartyInfo';
+} from '../../shared/components/ThirdPartyInfo';
 import { acceptableFiles } from '../components/Sponsor/sponsorFileUploads';
 import {
-  // marriageDocumentList,
   applicantBirthCertConfig,
   applicantSchoolCertConfig,
   applicantSchoolCertUploadUiSchema,
@@ -83,7 +81,6 @@ import {
   applicantOhiCardsUploadUiSchema,
   applicantOtherInsuranceCertificationUploadUiSchema,
 } from '../components/Applicant/applicantFileUpload';
-import { homelessInfo, noPhoneInfo } from '../components/Sponsor/sponsorAlerts';
 import GetFormHelp from '../../shared/components/GetFormHelp';
 
 import {
@@ -254,7 +251,6 @@ const formConfig = {
               'Your mailing address',
               'We’ll send any important information about this application to your address',
             ),
-            // ...homelessInfo.uiSchema,
             certifierAddress: addressUI(),
           },
           schema: {
@@ -262,7 +258,6 @@ const formConfig = {
             required: ['certifierAddress'],
             properties: {
               titleSchema,
-              // ...homelessInfo.schema,
               certifierAddress: addressSchema(),
             },
           },
@@ -276,7 +271,6 @@ const formConfig = {
               'Your contact information',
               'We use this information to contact you if we have more questions.',
             ),
-            ...noPhoneInfo.uiSchema,
             certifierPhone: phoneUI(),
           },
           schema: {
@@ -284,7 +278,6 @@ const formConfig = {
             required: ['certifierPhone'],
             properties: {
               titleSchema,
-              ...noPhoneInfo.schema,
               certifierPhone: phoneSchema,
             },
           },
@@ -475,7 +468,6 @@ const formConfig = {
               ({ formData }) => `${sponsorWording(formData)} mailing address`,
               'We’ll send any important information about this application to this address.',
             ),
-            ...homelessInfo.uiSchema,
             sponsorAddress: {
               ...addressUI({
                 labels: {
@@ -490,7 +482,6 @@ const formConfig = {
             required: ['sponsorAddress'],
             properties: {
               titleSchema,
-              ...homelessInfo.schema,
               sponsorAddress: addressSchema(),
             },
           },
@@ -510,7 +501,6 @@ const formConfig = {
                 return `Having this information helps us contact ${first} faster if we have questions about ${second} information.`;
               },
             ),
-            ...noPhoneInfo.uiSchema,
             sponsorPhone: {
               ...phoneUI({
                 title: 'Phone number',
@@ -523,7 +513,6 @@ const formConfig = {
             required: ['sponsorPhone'],
             properties: {
               titleSchema,
-              ...noPhoneInfo.schema,
               sponsorPhone: phoneSchema,
             },
           },
@@ -670,7 +659,6 @@ const formConfig = {
                   'ui:description':
                     'We’ll send any important information about your application to this address.',
                 },
-                ...homelessInfo.uiSchema,
                 applicantAddress: {
                   ...addressUI({
                     labels: {
@@ -685,7 +673,6 @@ const formConfig = {
           schema: applicantListSchema([], {
             titleSchema,
             'view:description': blankSchema,
-            ...homelessInfo.schema,
             applicantAddress: addressSchema(),
           }),
         },
@@ -703,7 +690,6 @@ const formConfig = {
                     `${applicantWording(formData)} contact information`,
                   'This information helps us contact you faster if we need to follow up with you about your application',
                 ),
-                ...noPhoneInfo.uiSchema,
                 applicantPhone: phoneUI(),
                 applicantEmailAddress: emailUI(),
               },
@@ -711,7 +697,6 @@ const formConfig = {
           },
           schema: applicantListSchema(['applicantPhone'], {
             titleSchema,
-            ...noPhoneInfo.schema,
             applicantPhone: phoneSchema,
             applicantEmailAddress: emailSchema,
           }),
@@ -791,11 +776,17 @@ const formConfig = {
           schema: applicantListSchema([], {
             titleSchema,
             'ui:description': blankSchema,
-            applicantRelationshipOrigin: customRelationshipSchema([
-              'blood',
-              'adoption',
-              'step',
-            ]),
+            applicantRelationshipOrigin: {
+              type: 'object',
+              properties: {
+                relationshipToVeteran: radioSchema([
+                  'blood',
+                  'adoption',
+                  'step',
+                ]),
+                otherRelationshipToVeteran: { type: 'string' },
+              },
+            },
           }),
         },
         page18a: {
