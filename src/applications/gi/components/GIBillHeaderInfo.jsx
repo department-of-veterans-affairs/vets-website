f3f866bb21a312e869ae896a816f7a20d6864c81
Michael Ghisilieri <michael.ghisilieri@va.gov>
2024-08-02 15:00:05 -0700

    GI Bill header styling updated to use VA design system

--- src/applications/gi/components/GIBillHeaderInfo.jsx
+++ src/applications/gi/components/GIBillHeaderInfo.jsx
@@ -11,7 +11,7 @@ export const renderPTag = (automatedTest = false) => {
     );
   }
   return (
-    <p className="vads-u-font-size--h3 vads-u-color--gray-dark main-text-subtext">
+    <p className="vads-u-font-size--h3 vads-u-color--gray-dark">
       Learn about and compare your GI Bill benefits at approved schools and
       employers.
     </p>
@@ -20,8 +20,10 @@ export const renderPTag = (automatedTest = false) => {
 
 export default function GIBillHeaderInfo() {
   return (
-    <div className="tool-description">
-      <h1 className="main-title">GI Bill® Comparison Tool</h1>
+    <div className="row vads-u-padding-x--2p5 small-screen:vads-u-padding-x--0 vads-u-padding-bottom--2 small-screen:vads-u-padding-bottom--4">
+      <h1 className="vads-u-text-align--center small-screen:vads-u-text-align--left">
+        GI Bill® Comparison Tool
+      </h1>
       {renderPTag()}
     </div>
   );
