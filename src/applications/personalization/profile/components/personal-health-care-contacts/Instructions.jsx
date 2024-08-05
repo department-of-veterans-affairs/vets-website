d62146e4a6e336f2c3e13607a1732e094f435126
Richard Davis <richard.davis@agile6.com>
2024-08-05 10:49:20 -0400

    Renames var

--- src/applications/personalization/profile/components/personal-health-care-contacts/Instructions.jsx
+++ src/applications/personalization/profile/components/personal-health-care-contacts/Instructions.jsx
@@ -4,15 +4,15 @@ import { VaTelephone } from '@department-of-veterans-affairs/component-library/d
 import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
 
 const Instructions = ({ testId, contactType, description }) => {
-  const isEmergency = contactType === 'emergency contact';
+  const isEc = contactType === 'emergency contact';
   return (
     <div data-testid={testId}>
       <p className="vads-u-color--gray-medium vads-u-margin-top--0p5 vads-u-margin-bottom--0p5">
         {description}
       </p>
       <p className="vads-u-margin-top--2 vads-u-margin-bottom--1">
-        To add {isEmergency ? 'an' : 'a'} {contactType}, call the Health
-        Eligibility Center at <VaTelephone contact={CONTACTS['222_VETS']} /> (
+        To add {isEc ? 'an' : 'a'} {contactType}, call the Health Eligibility
+        Center at <VaTelephone contact={CONTACTS['222_VETS']} /> (
         <VaTelephone contact={CONTACTS['711']} tty />
         ). We’re available Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
       </p>
