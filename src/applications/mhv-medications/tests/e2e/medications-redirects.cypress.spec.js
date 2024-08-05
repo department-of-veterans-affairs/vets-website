edce67f30ea4d21d0ab338336edfafd2925deef7
Richard Davis <richard.davis@agile6.com>
2024-08-05 16:53:22 -0400

    Simplifies spec

--- src/applications/mhv-medications/tests/e2e/medications-redirects.cypress.spec.js
+++ src/applications/mhv-medications/tests/e2e/medications-redirects.cypress.spec.js
@@ -9,25 +9,15 @@ describe('Medications redirects', () => {
     site.mockFeatureToggles();
     site.mockVamcEhr();
 
-    cy.intercept('/?next=*', cy.spy().as('rootPath')).as(
-      'unauthenticatedRedirect',
-    );
-
-    cy.intercept('/my-health', cy.spy().as('myHealthPath')).as(
-      'myHealthRedirect',
-    );
-
-    cy.intercept(
-      '/my-health/medications/about',
-      cy.spy().as('staticPagePath'),
-    ).as('staticPageRedirect');
+    cy.intercept('/?next=*', cy.spy().as('rootPath'));
+    cy.intercept('/my-health', cy.spy().as('myHealthPath'));
+    cy.intercept('/my-health/medications/about', cy.spy().as('staticPagePath'));
   });
 
   describe('as an unauthenticated user', () => {
     // eslint-disable-next-line @department-of-veterans-affairs/axe-check-required
     it('redirects the user to /?next=* and prompts to sign in', () => {
       cy.visit('/my-health/medications');
-      cy.wait(['@featureToggles', '@vamcEhr', '@unauthenticatedRedirect']);
       cy.get('@rootPath').should('have.been.called');
     });
   });
@@ -37,12 +27,6 @@ describe('Medications redirects', () => {
     it('redirects the user to /my-health to render an alert', () => {
       cy.login(mockUserNoMhvAccount);
       cy.visit('/my-health/medications');
-      cy.wait([
-        '@featureToggles',
-        '@vamcEhr',
-        '@mockUser',
-        '@myHealthRedirect',
-      ]);
       cy.get('@myHealthPath').should('have.been.called');
       cy.get('@staticPagePath').should('have.not.been.called');
     });
