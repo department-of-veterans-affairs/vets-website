7a9ab3dc5f467e40b1da3b6e9a6a56c960f737f3
Richard Davis <richard.davis@agile6.com>
2024-08-06 16:23:45 -0400

    Toggles VA Health Chat link with toggleEnabled() fn, removes resovleLinks() fn

--- src/applications/mhv-landing-page/utilities/data/index.js
+++ src/applications/mhv-landing-page/utilities/data/index.js
@@ -1,7 +1,7 @@
+import FEATURE_FLAG_NAMES from '@department-of-veterans-affairs/platform-utilities/featureFlagNames';
 import { mhvUrl } from '@department-of-veterans-affairs/platform-site-wide/utilities';
 // Links to MHV subdomain need to use `mhvUrl`. Va.gov links can just be paths
 import { HEALTH_TOOL_HEADINGS, HEALTH_TOOL_LINKS } from '../../constants';
-import resolveLinks from './resolveLinks';
 
 const countUnreadMessages = folders => {
   if (Array.isArray(folders?.data)) {
@@ -25,138 +25,127 @@ const resolveUnreadMessageAriaLabel = unreadMessageCount => {
     : null;
 };
 
+const toggleEnabled = (featureToggles, toggle) =>
+  Object.prototype.hasOwnProperty.call(featureToggles, toggle) &&
+  featureToggles[toggle] === true;
+
 const resolveLandingPageLinks = (
   authdWithSSOe = false,
   featureToggles,
   unreadMessageAriaLabel,
   registered = false,
 ) => {
-  const messagesLinks = resolveLinks(
-    [
-      {
-        ...HEALTH_TOOL_LINKS.MESSAGES[0],
-        ariaLabel: unreadMessageAriaLabel,
-      },
-      {
-        ...HEALTH_TOOL_LINKS.MESSAGES[1],
-      },
-      {
-        ...HEALTH_TOOL_LINKS.MESSAGES[2],
-      },
-    ],
-    featureToggles,
-  );
+  const messagesLinks = [
+    {
+      ...HEALTH_TOOL_LINKS.MESSAGES[0],
+      ariaLabel: unreadMessageAriaLabel,
+    },
+    {
+      ...HEALTH_TOOL_LINKS.MESSAGES[1],
+    },
+    {
+      ...HEALTH_TOOL_LINKS.MESSAGES[2],
+    },
+  ];
 
-  const medicalRecordsLinks = resolveLinks(
-    [
-      {
-        href: mhvUrl(authdWithSSOe, '/download-my-data'),
-        text: 'Download medical record (Blue Button®)',
-      },
-      {
-        href: mhvUrl(authdWithSSOe, '/labs-tests'),
-        text: 'Lab and test results',
-      },
-      {
-        href: mhvUrl(authdWithSSOe, '/health-history'),
-        text: 'Health history',
-      },
-    ],
-    featureToggles,
-  );
+  const medicalRecordsLinks = [
+    {
+      href: mhvUrl(authdWithSSOe, '/download-my-data'),
+      text: 'Download medical record (Blue Button®)',
+    },
+    {
+      href: mhvUrl(authdWithSSOe, '/labs-tests'),
+      text: 'Lab and test results',
+    },
+    {
+      href: mhvUrl(authdWithSSOe, '/health-history'),
+      text: 'Health history',
+    },
+  ];
 
-  const myVaHealthBenefitsLinks = resolveLinks(
-    [
-      {
-        href: '/health-care/copay-rates/',
-        text: 'Current Veteran copay rates',
-      },
-      {
-        href: '/health-care/health-needs-conditions/mental-health',
-        text: 'Mental health services',
-      },
-      {
-        href: '/health-care/about-va-health-benefits/dental-care/',
-        text: 'Dental care',
-      },
-      {
-        href: '/COMMUNITYCARE/programs/veterans/index.asp',
-        text: 'Community care',
-      },
-      registered && {
-        href:
-          '/my-health/update-benefits-information-form-10-10ezr/introduction',
-        text: 'Update health benefits info (10-10EZR)',
-      },
-      {
-        href: mhvUrl(authdWithSSOe, 'health-information-card'),
-        text: 'Veteran health information card',
-      },
-      // {
-      //   href: '#FIXME-need-link',
-      //   text: 'Download my IRS 1095-B form',
-      // },
-    ],
-    featureToggles,
-  );
+  const myVaHealthBenefitsLinks = [
+    {
+      href: '/health-care/copay-rates/',
+      text: 'Current Veteran copay rates',
+    },
+    {
+      href: '/health-care/health-needs-conditions/mental-health',
+      text: 'Mental health services',
+    },
+    {
+      href: '/health-care/about-va-health-benefits/dental-care/',
+      text: 'Dental care',
+    },
+    {
+      href: '/COMMUNITYCARE/programs/veterans/index.asp',
+      text: 'Community care',
+    },
+    registered && {
+      href: '/my-health/update-benefits-information-form-10-10ezr/introduction',
+      text: 'Update health benefits info (10-10EZR)',
+    },
+    {
+      href: mhvUrl(authdWithSSOe, 'health-information-card'),
+      text: 'Veteran health information card',
+    },
+    // {
+    //   href: '#FIXME-need-link',
+    //   text: 'Download my IRS 1095-B form',
+    // },
+  ];
 
-  const moreResourcesLinks = resolveLinks(
-    [
-      {
-        href: '/resources/the-pact-act-and-your-va-benefits/',
-        text: 'The PACT Act and your benefits',
-      },
-      {
-        href: mhvUrl(authdWithSSOe, 'check-your-mental-health'),
-        text: 'Check your mental health',
-      },
-      {
-        href: 'https://www.veteranshealthlibrary.va.gov/',
-        text: 'Veterans Health Library',
-      },
-      {
-        href: 'https://www.myhealth.va.gov/healthy-living-centers',
-        text: 'Healthy Living Centers',
-      },
-      {
-        href: 'https://www.myhealth.va.gov/mhv-community',
-        text: 'The My HealtheVet community',
-      },
-      {
-        href: '/wholehealth/',
-        text: 'VA’s Whole Health living',
-      },
-      {
-        href: mhvUrl(authdWithSSOe, 'ss20200320-va-video-connect'),
-        text: 'How to use VA Video Connect',
-      },
-    ],
-    featureToggles,
-  );
+  const moreResourcesLinks = [
+    toggleEnabled(
+      featureToggles,
+      FEATURE_FLAG_NAMES.mhvVaHealthChatEnabled,
+    ) && {
+      href: 'https://veteran.apps.va.gov/users/v2/login?redirect_uri=/cirrusmd',
+      text: 'Chat live with a health professional on VA Health Chat',
+    },
+    {
+      href: '/resources/the-pact-act-and-your-va-benefits/',
+      text: 'The PACT Act and your benefits',
+    },
+    {
+      href: mhvUrl(authdWithSSOe, 'check-your-mental-health'),
+      text: 'Check your mental health',
+    },
+    {
+      href: 'https://www.veteranshealthlibrary.va.gov/',
+      text: 'Veterans Health Library',
+    },
+    {
+      href: 'https://www.myhealth.va.gov/healthy-living-centers',
+      text: 'Healthy Living Centers',
+    },
+    {
+      href: 'https://www.myhealth.va.gov/mhv-community',
+      text: 'The My HealtheVet community',
+    },
+    {
+      href: '/wholehealth/',
+      text: 'VA’s Whole Health living',
+    },
+    {
+      href: mhvUrl(authdWithSSOe, 'ss20200320-va-video-connect'),
+      text: 'How to use VA Video Connect',
+    },
+  ];
 
-  const spotlightLinks = resolveLinks(
-    [
-      {
-        text: 'Make the Most of Your VA Appointment',
-        href: mhvUrl(authdWithSSOe, 'ss20240415-make-most-your-va-appointment'),
-      },
-      {
-        text: 'VA Health Care Expands to Millions of Veterans',
-        href: mhvUrl(
-          authdWithSSOe,
-          'ss20240315-va-health-care-expands-millions',
-        ),
-      },
-      {
-        text: 'Hearing Aids Connect with Telehealth',
-        href: mhvUrl(
-          authdWithSSOe,
-          'ss20211015-fix-your-hearing-aid-over-video',
-        ),
-      },
-    ],
-    featureToggles,
-  );
+  const spotlightLinks = [
+    {
+      text: 'Make the Most of Your VA Appointment',
+      href: mhvUrl(authdWithSSOe, 'ss20240415-make-most-your-va-appointment'),
+    },
+    {
+      text: 'VA Health Care Expands to Millions of Veterans',
+      href: mhvUrl(authdWithSSOe, 'ss20240315-va-health-care-expands-millions'),
+    },
+    {
+      text: 'Hearing Aids Connect with Telehealth',
+      href: mhvUrl(authdWithSSOe, 'ss20211015-fix-your-hearing-aid-over-video'),
+    },
+  ];
 
   const cards = [
     {
@@ -191,6 +180,7 @@ const resolveLandingPageLinks = (
       links: HEALTH_TOOL_LINKS.MEDICAL_SUPPLIES,
     },
   ];
+
   const hubs = [
     {
       title: registered ? 'My VA health benefits' : 'VA health benefits',
