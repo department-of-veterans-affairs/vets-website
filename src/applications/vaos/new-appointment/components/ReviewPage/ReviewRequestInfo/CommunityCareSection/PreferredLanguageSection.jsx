32d37fd26a22493f3f09dd8d8eec1573d9c8acdd
Jun Tao Luo <john.luo@agile6.com>
2024-08-06 10:59:11 -0400

    Fix PreferredLanguageSection

--- src/applications/vaos/new-appointment/components/ReviewPage/ReviewRequestInfo/CommunityCareSection/PreferredLanguageSection.jsx
+++ src/applications/vaos/new-appointment/components/ReviewPage/ReviewRequestInfo/CommunityCareSection/PreferredLanguageSection.jsx
@@ -5,22 +5,23 @@ import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { LANGUAGES } from '../../../../../utils/constants';
 import getNewAppointmentFlow from '../../../../newAppointmentFlow';
 
-function handleClick(history, pageFlow) {
-  const { home, ccPreferences } = pageFlow;
+function handleClick(history, home, ccLanguage) {
+  return e => {
+    // Stop default behavior for anchor tag since we are using React routing.
+    e.preventDefault();
 
-  return () => {
     if (
       history.location.pathname.endsWith('/') ||
-      (ccPreferences.url.endsWith('/') && ccPreferences.url !== home.url)
+      (ccLanguage.url.endsWith('/') && ccLanguage.url !== home.url)
     )
-      history.push(`../${ccPreferences.url}`);
-    else history.push(ccPreferences.url);
+      history.push(`../${ccLanguage.url}`);
+    else history.push(ccLanguage.url);
   };
 }
 
 export default function PreferredLanguageSection({ data }) {
   const history = useHistory();
-  const pageFlow = useSelector(getNewAppointmentFlow);
+  const { home, root, ccLanguage } = useSelector(getNewAppointmentFlow);
 
   return (
     <div className="vads-l-grid-container vads-u-padding--0">
@@ -36,11 +37,11 @@ export default function PreferredLanguageSection({ data }) {
         </div>
         <div>
           <va-link
-            onClick={handleClick(history, pageFlow)}
-            aria-label="Edit prefered language"
+            href={`${root.url}/schedule/community-request/${ccLanguage.url}`}
+            onClick={handleClick(history, home, ccLanguage)}
+            aria-label="Edit preferred language"
             text="Edit"
             data-testid="edit-preferred-language"
-            role="link"
           />
         </div>
       </div>
