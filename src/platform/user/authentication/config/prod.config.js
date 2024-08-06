2dc834583b5f8d573fe8e58d7c12c841014edc90
Gabriel Zurita <gabezurita@gmail.com>
2024-08-06 11:35:33 -0600

    test 2

--- src/platform/user/authentication/config/prod.config.js
+++ src/platform/user/authentication/config/prod.config.js
@@ -5,7 +5,6 @@ import {
   defaultMobileQueryParams,
   defaultWebOAuthOptions,
   defaultMobileOAuthOptions,
-  arpWebOAuthOptions,
 } from './constants';
 
 export default {
@@ -85,28 +84,5 @@ export default {
     requiresVerification: true,
     oAuthOptions: defaultMobileOAuthOptions,
     externalRedirectUrl: EXTERNAL_REDIRECTS[EXTERNAL_APPS.VA_OCC_MOBILE],
-  },
-  [EXTERNAL_APPS.ARP]: {
-    allowedSignInProviders: {
-      idme: true,
-      logingov: true,
-    },
-    allowedSignUpProviders: {
-      idme: true,
-      logingov: true,
-    },
-    isMobile: false,
-    queryParams: {
-      allowOAuth: true,
-      allowPostLogin: true,
-      allowRedirect: false,
-    },
-    oAuthOptions: {
-      ...arpWebOAuthOptions,
-      clientId: 'fe0d4b2cac7935e7eec5946b8ee31643',
-    },
-    OAuthEnabled: true,
-    requiresVerification: false,
-    externalRedirectUrl: EXTERNAL_REDIRECTS[EXTERNAL_APPS.ARP],
   },
 };
