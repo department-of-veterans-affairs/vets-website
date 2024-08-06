54b987cc1c1bad33e16b7528bb9607a2d53c6910
Richard Davis <richard.davis@agile6.com>
2024-08-06 16:30:50 -0400

    Disables feature toggle for mocks

--- src/applications/mhv-landing-page/mocks/api/feature-toggles/index.js
+++ src/applications/mhv-landing-page/mocks/api/feature-toggles/index.js
@@ -2,7 +2,7 @@ const { snakeCase } = require('lodash');
 
 // Please, keep these feature toggle settings up-to-date with production's feature toggles settings.
 const APPLICATION_FEATURE_TOGGLES = Object.freeze({
-  mhvVaHealthChatEnabled: false,
+  mhvVaHealthChatEnabled: true,
   mhvLandingPagePersonalization: false,
   mhvTransitionalMedicalRecordsLandingPage: true,
 });
