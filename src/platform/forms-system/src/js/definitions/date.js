ae065e3dc33601f289855cd4052b3e385ed72b64
Adam Antonioli <adam.antonioli@va.gov>
2024-08-07 09:48:53 -0400

    MBMS-43488 add custom memorable date component to pre-need integration form

--- src/platform/forms-system/src/js/definitions/date.js
+++ src/platform/forms-system/src/js/definitions/date.js
@@ -1,13 +1,32 @@
-import { validateDate } from '../validation';
+// import { validateDate } from '../validation';
+import MemorableDateField from 'applications/pre-need-integration/components/MemorableDateField';
 
 export default function uiSchema(title = 'Date') {
   return {
     'ui:title': title,
-    'ui:widget': 'date',
-    'ui:validations': [validateDate],
-    'ui:errorMessages': {
-      pattern: 'Please enter a valid date',
-      required: 'Please enter a date',
+    // 'ui:widget': 'date',
+    'ui:webComponentField': MemorableDateField,
+    // 'ui:validations': [validateDate],
+    // 'ui:errorMessages': {
+    //   pattern: 'Please enter a valid date',
+    //   required: 'Please enter a date',
+    // },
+    'ui:options': {
+      monthSelect: false,
     },
   };
 }
+
+// import { validateDate } from '../validation';
+// import MemorableDateField from '../../../../../applications/pre-need-integration/components/MemorableDateField';
+// // '../components/MemorableDateField';
+
+// export default function uiSchema(title = 'Date') {
+//   return {
+//     'ui:title': title,
+//     'ui:webComponentField': MemorableDateField,
+//     'ui:options': {
+//       monthSelect: false,
+//     },
+//   };
+// }
