7280a5fb6e72596126684556fe44b9f8a2b1171b
Jacob Penner <jacob.penner@adhocteam.us>
2024-08-08 15:39:18 -0400

    WIP

--- src/applications/simple-forms/mock-simple-forms-patterns/containers/ConfirmationPage.new.jsx
+++ src/applications/simple-forms/mock-simple-forms-patterns/containers/ConfirmationPage.new.jsx
@@ -13,47 +13,15 @@ const content = {
 };
 
 const ConfirmationPage = () => {
-  const form = {
-    data: {
-      statementType: 'not-listed',
-      fullName: {
-        first: 'John',
-        last: 'Veteran',
-      },
-      dateOfBirth: '1980-01-01',
-      idNumber: {
-        ssn: '321540987',
-      },
-      mailingAddress: {
-        country: 'USA',
-        street: '123 Any St',
-        city: 'Anytown',
-        state: 'CA',
-        postalCode: '12345',
-      },
-      phone: '1234567890',
-      statement:
-        'John stood at the towering gates of bureaucracy, a maze of forms and lines that seemed endless. Clutched in his hand was the VA Form 21-4138, a cryptic relic said to grant access to essential benefits. As he stepped forward, he encountered gatekeepers—the clerks—each with riddles to solve and documents to verify. Armed with a pen and a folder filled with proof, he navigated the maze, dodging a storm of paper, crossing the chasm of deadlines, and fighting the dragon of waiting times. In the end, he emerged triumphant, his form stamped with approval, the quest for benefits achieved. The adventure, though arduous, had its reward.',
-    },
-    submission: {
-      timestamp: 1721999975,
-      response: { confirmationNumber: '2106a7e8-27dd-46b4-886a-29b780723f4c' },
-    },
-  };
-
-  // const form = {
-  //   data: { veteran: { fullName: { first: 'John', last: 'Veteran' } } },
-  //   submission: {
-  //     timestamp: 1721999975,
-  //     response: { confirmationNumber: '2106a7e8-27dd-46b4-886a-29b780723f4c' },
-  //   },
-  // };
+  const form = require('../tests/e2e/fixtures/mocks/mock-form.json');
+  const formConfig = require('../tests/e2e/fixtures/mocks/mock-form-config.json');
   const { timestamp, response = {} } = form.submission;
   const submitterFullName = form.data?.veteran?.fullName;
 
   return (
     <ConfirmationPageView
       formType="submission"
+      formConfig={formConfig}
       submitterHeader="Who submitted this form"
       submitterName={submitterFullName}
       submitDate={timestamp}
