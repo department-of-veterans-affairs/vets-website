d32f6cec4ab1f824222f8c29e4958c6e8772e06d
OptionSelect <brandyn.sullins@gmail.com>
2024-08-08 14:11:55 -0400

    Replace mock data with api response data

--- src/applications/combined-debt-portal/debt-letters/components/PaymentHistoryTable.jsx
+++ src/applications/combined-debt-portal/debt-letters/components/PaymentHistoryTable.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { renderPaymentHistoryDescription } from '../const/diary-codes';
 import { deductionCodes } from '../const/deduction-codes';
 import { currency } from '../utils/page';
 
 const PaymentHistoryTable = ({ currentDebt }) => {
-  const { paymentHistory } = currentDebt;
-  if (paymentHistory.length === 0) {
+  const getPaymentHistoryDescription = transactionDescription => {
+    if (
+      transactionDescription.startsWith('Increase to AR') ||
+      transactionDescription.startsWith('Increase to New AR')
+    ) {
+      return 'Balance increase';
+    }
+    if (transactionDescription.startsWith('AR Decrease')) {
+      return 'Balance decrease';
+    }
+    if (transactionDescription.includes('Write Off')) {
+      return 'Write Off';
+    }
+    if (
+      transactionDescription.includes('Reversal') ||
+      transactionDescription.includes('TOP Reversal')
+    ) {
+      return 'Reversal';
+    }
+    return 'Other';
+  };
+
+  const renderPaymentHistoryDescription = transactionDescription => {
+    return (
+      <>
+        <p className="vads-u-margin--0 vads-u-font-size-md vads-u-color--gray-dark">
+          <strong>
+            {getPaymentHistoryDescription(transactionDescription)}
+          </strong>
+        </p>
+      </>
+    );
+  };
+
+  const { fiscalTransactionData } = currentDebt;
+  if (fiscalTransactionData.length === 0) {
     return null;
   }
   return (
@@ -16,12 +48,12 @@ const PaymentHistoryTable = ({ currentDebt }) => {
         <span>Description</span>
         <span>Amount</span>
       </va-table-row>
-      {paymentHistory.map((payment, index) => (
+      {fiscalTransactionData.map((payment, index) => (
         <va-table-row key={`${payment.transactionDate}-${index}`}>
           <span className="vads-u-width--fit">{payment.transactionDate}</span>
           <span>
             <div className="vads-u-margin-top--0">
-              {renderPaymentHistoryDescription(payment.transactionFiscalCode)}
+              {renderPaymentHistoryDescription(payment.transactionDescription)}
             </div>
           </span>
           <span>{payment.transactionTotalAmount}</span>
@@ -44,10 +76,6 @@ const PaymentHistoryTable = ({ currentDebt }) => {
       </va-table-row>
     </va-table>
   );
-};
-
-PaymentHistoryTable.propTypes = {
-  currentDebt: PropTypes.object,
 };
 
 export default PaymentHistoryTable;
