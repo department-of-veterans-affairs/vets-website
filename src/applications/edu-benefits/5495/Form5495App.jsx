fb781d7ffb6ca8f1248f3774a40cd18dcebf3516
fatmakhan0395 <Fatma.khan@va.gov>
2024-08-08 10:40:42 -0700

    disabled 5495

--- src/applications/edu-benefits/5495/Form5495App.jsx
+++ src/applications/edu-benefits/5495/Form5495App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+import { useFeatureToggle } from 'platform/utilities/feature-toggles';
 import formConfig from './config/form';
+import { useSetToggleParam } from '../hooks/useSetToggleParam';
 
 export default function Form5495Entry({ location, children }) {
+  const { useToggleValue, TOGGLE_NAMES } = useFeatureToggle();
+  const toggleValue = useToggleValue(TOGGLE_NAMES.merge1995And5490);
+
+  useSetToggleParam(toggleValue);
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
