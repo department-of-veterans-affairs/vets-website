0cdd6267e786bd0564f71f45e3d902783abd9a85
Derek Houck <derek.houck@agile6.com>
2024-08-08 15:29:43 -0500

    Format chapters

--- src/applications/form-renderer/tests/unit/utils/formConfig.unit.spec.js
+++ src/applications/form-renderer/tests/unit/utils/formConfig.unit.spec.js
@@ -1,14 +1,62 @@
+/* eslint-disable prefer-destructuring */
+
 import { expect } from 'chai';
 import { normalizedForm } from '../../../_config/formConfig';
 import { createFormConfig } from '../../../utils/formConfig';
 
 describe('createFormConfig', () => {
-  it('returns a properly formatted Form Config object', () => {
-    const formConfig = createFormConfig(normalizedForm);
+  let formConfig;
 
+  beforeEach(() => {
+    formConfig = createFormConfig(normalizedForm);
+  });
+
+  it('returns a properly formatted Form Config object', () => {
     expect(formConfig.title).to.eq('Form with Two Steps');
     expect(formConfig.formId).to.eq('2121212');
     expect(formConfig.subTitle).to.eq('VA Form 2121212');
-    expect(formConfig.chapters.length).to.eq(2);
+    expect(Object.keys(formConfig.chapters).length).to.eq(2);
+  });
+
+  it('properly formats each chapter', () => {
+    const testChapter = formConfig.chapters[158253];
+    const page = testChapter.pages[158253];
+
+    expect(testChapter.title).to.eq('First Step');
+    expect(Object.keys(testChapter.pages).length).to.eq(1);
+    expect(page.path).to.eq('158253');
+    expect(page.title).to.eq('Name and Date of Birth');
+    expect(page.schema).not.to.eq(undefined);
+    expect(page.uiSchema['ui:title']).not.to.eq(undefined);
+  });
+
+  context('with Name and Date of Birth pattern', () => {
+    let dobIncluded;
+    let nameOnly;
+
+    beforeEach(() => {
+      // eslint-disable-next-line prefer-destructuring
+      dobIncluded = formConfig.chapters[158253].pages[158253];
+      nameOnly = formConfig.chapters[158254].pages[158254];
+    });
+
+    it('contains fullName', () => {
+      expect(dobIncluded.schema.properties.fullName).to.not.eq(undefined);
+      expect(dobIncluded.uiSchema.fullName).to.not.eq(undefined);
+    });
+
+    context('when includeDateOfBirth is true', () => {
+      it('contains dateOfBirth', () => {
+        expect(dobIncluded.schema.properties.dateOfBirth).to.not.eq(undefined);
+        expect(dobIncluded.uiSchema.dateOfBirth).to.not.eq(undefined);
+      });
+    });
+
+    context('when includeDateOfBirth is false', () => {
+      it('does not contain dateOfBirth', () => {
+        expect(nameOnly.schema.properties.dateOfBirth).to.eq(undefined);
+        expect(nameOnly.uiSchema.dateOfBirth).to.eq(undefined);
+      });
+    });
   });
 });
--- src/applications/form-renderer/utils/formConfig.js
+++ src/applications/form-renderer/utils/formConfig.js
@@ -1,8 +1,58 @@
+import {
+  dateOfBirthSchema,
+  dateOfBirthUI,
+  fullNameSchema,
+  fullNameUI,
+  titleUI,
+} from 'platform/forms-system/src/js/web-component-patterns';
+
+const selectSchemas = ({ pageTitle, additionalFields }) => {
+  const schemas = {
+    schema: {
+      type: 'object',
+      properties: {
+        fullName: fullNameSchema,
+      },
+    },
+    uiSchema: {
+      ...titleUI(pageTitle),
+      fullName: fullNameUI(),
+    },
+  };
+
+  if (additionalFields.includeDateOfBirth) {
+    schemas.schema.properties.dateOfBirth = dateOfBirthSchema;
+    schemas.uiSchema.dateOfBirth = dateOfBirthUI();
+  }
+
+  return schemas;
+};
+
+const formatChapters = chapters => {
+  const formattedChapters = {};
+
+  chapters.forEach(chapter => {
+    const pages = {};
+    pages[chapter.id] = {
+      path: chapter.id.toString(),
+      title: chapter.pageTitle,
+      ...selectSchemas(chapter),
+    };
+
+    formattedChapters[chapter.id] = {
+      title: chapter.chapterTitle,
+      pages,
+    };
+  });
+
+  return formattedChapters;
+};
+
 export const createFormConfig = ({ chapters, formId, title }) => {
   return {
     formId,
     title,
     subTitle: `VA Form ${formId}`,
-    chapters,
+    chapters: formatChapters(chapters),
   };
 };
