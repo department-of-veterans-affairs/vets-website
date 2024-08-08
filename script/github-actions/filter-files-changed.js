083b1d67c4f9b8ec508f307de76a22151a67fcd6
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2024-08-08 15:00:52 -0400

    add fetch depth

--- script/github-actions/filter-files-changed.js
+++ script/github-actions/filter-files-changed.js
@@ -2,8 +2,9 @@
 const fs = require('fs');
 const core = require('@actions/core');
 
-const args = process.argv.slice(2);
-const files = args[0]
+console.log(process.env.CHANGED_FILE_PATHS).split(' ');
+// const args = process.argv.slice(2);
+const files = process.env.CHANGED_FILE_PATHS.split(' ')
   .slice(1, -1) // remove unnecessary characters
   .split(',')
   .filter(file => fs.existsSync(file)); // remove files that don't exist
