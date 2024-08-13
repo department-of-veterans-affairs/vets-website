7c71431809a22ecf85a91ac59890b357ea1c408d
Richard Davis <richard.davis@agile6.com>
2024-08-13 16:06:31 -0400

    Uses CompatRoute

--- src/applications/mhv-landing-page/routes.jsx
+++ src/applications/mhv-landing-page/routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { CompatRoute } from 'react-router-dom-v5-compat';
 import PageNotFound from '@department-of-veterans-affairs/platform-site-wide/PageNotFound';
 import AppConfig from './containers/AppConfig';
 import LandingPageContainer from './containers/LandingPageContainer';
@@ -8,15 +9,9 @@ import MedicalRecordsContainer from './containers/MedicalRecordsContainer';
 const routes = (
   <AppConfig>
     <Switch>
-      <Route exact path="/" key="mhvLandingPage">
-        <LandingPageContainer />
-      </Route>
-      <Route exact path="/records" key="mhvMedicalRecords">
-        <MedicalRecordsContainer />
-      </Route>
-      <Route>
-        <PageNotFound />
-      </Route>
+      <CompatRoute exact path="/" component={LandingPageContainer} />
+      <CompatRoute exact path="/records" component={MedicalRecordsContainer} />
+      <CompatRoute exact path="*" component={PageNotFound} />
     </Switch>
   </AppConfig>
 );
