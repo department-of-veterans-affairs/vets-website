96993c2409711976fbcdc8ef6dfc79339b7896c3
Rachel <rachele.eiting@copilot.llc>
2024-08-13 12:53:24 -0400

    put toggler in main app

--- src/applications/ivc-champva/10-7959f-1/containers/App.jsx
+++ src/applications/ivc-champva/10-7959f-1/containers/App.jsx
@@ -1,25 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Toggler } from 'platform/utilities/feature-toggles';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+import WIP from '../../shared/components/WIP';
 import formConfig from '../config/form';
 
 export default function App({ location, children }) {
+  const breadcrumbList = [
+    { href: '/', label: 'Home' },
+    { href: '/health-care', label: 'Health care' },
+    {
+      href: '/health-care/foreign-medical-program',
+      label: 'Foreign Medical Program (FMP)',
+    },
+    {
+      href: '/health-care/foreign-medical-program/',
+      label: 'Register for the Foreign Medical Program (FMP)',
+    },
+  ];
+  const bcString = JSON.stringify(breadcrumbList);
   return (
     <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
-      <va-breadcrumbs uswds="false">
-        <a href="/">Home</a>
-        <a href="/health-care">Health care</a>
-        <a href="/health-care/foreign-medical-program">
-          Foreign Medical Program (FMP)
-        </a>
-        <a href="/health-care/foreign-medical-program/">
-          Register for the Foreign Medical Program (FMP)
-        </a>
-      </va-breadcrumbs>
-      <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-        {children}
-      </RoutedSavableApp>
+      <Toggler toggleName={Toggler.TOGGLE_NAMES.form107959F1}>
+        <Toggler.Enabled>
+          <va-breadcrumbs breadcrumb-list={bcString} />
+          <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+            {children}
+          </RoutedSavableApp>
+        </Toggler.Enabled>
+        <Toggler.Disabled>
+          <br />
+          <WIP
+            content={{
+              description:
+                'We’re rolling out the Foreign Medical Program (FMP) registration (VA Form 10-7959f-1) in stages. It’s not quite ready yet. Please check back again soon.',
+              redirectLink: '/',
+              redirectText: 'Return to VA home page',
+            }}
+          />
+        </Toggler.Disabled>
+      </Toggler>
     </div>
   );
 }
