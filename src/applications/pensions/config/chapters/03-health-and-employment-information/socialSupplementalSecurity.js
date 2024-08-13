a2dcf3a0fa529af76c1fbc33491dde8de37e7405
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2024-08-13 13:14:29 -0600

    Update path names to help debug

--- src/applications/pensions/config/chapters/03-health-and-employment-information/socialSupplementalSecurity.js
+++ src/applications/pensions/config/chapters/03-health-and-employment-information/socialSupplementalSecurity.js
@@ -9,7 +9,7 @@ const { socialSecurityDisability } = fullSchemaPensions.properties;
 
 const path = showMedicalEvidenceClarification()
   ? 'medical/history/social-security-disability'
-  : 'temporarily-hidden';
+  : 'temporarily-hidden-social-security-disability';
 
 /** @type {PageSchema} */
 export default {
