e1a9a4f1a1bef955141b0d02f7279863a6971666
Tyler Spangler <tylerspangler@Tyler-Spangler.local>
2024-08-13 10:41:11 -0600

    added css class to NewDisability component

--- src/applications/disability-benefits/all-claims/components/NewDisability.jsx
+++ src/applications/disability-benefits/all-claims/components/NewDisability.jsx
@@ -3,7 +3,7 @@ import { NULL_CONDITION_STRING } from '../constants';
 
 export default function NewDisability({ formData }) {
   return (
-    <div className="vads-u-flex--fill vads-u-padding-right--2 word-break">
+    <div className="vads-u-flex--fill vads-u-padding-right--2 word-break capitalize-first-letter">
       {typeof formData?.condition === 'string'
         ? formData.condition
         : NULL_CONDITION_STRING}
