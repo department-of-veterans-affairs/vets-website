f37726e32805b59af32372426f0d5ec3260ac459
mattwrightva <107576133+mattwrightva@users.noreply.github.com>
2024-08-14 09:03:09 -0600

    MHV-60214: Health conditions mocks complete (#31374)
    Co-authored-by: Matthew Wright <matwright2010@gmail.com>

--- src/platform/mhv/api/mocks/index.js
+++ src/platform/mhv/api/mocks/index.js
@@ -26,6 +26,7 @@ const status = require('./medical-records/status');
 const labsAndTests = require('./medical-records/labs-and-tests');
 const mhvRadiology = require('./medical-records/mhv-radiology');
 const careSummariesAndNotes = require('./medical-records/care-summaries-and-notes');
+const healthConditions = require('./medical-records/health-conditions');
 const allergies = require('./medical-records/allergies');
 const vaccines = require('./medical-records/vaccines');
 
@@ -95,6 +96,8 @@ const responses = {
   'GET /my_health/v1/medical_records/clinical_notes': careSummariesAndNotes.all,
   'GET /my_health/v1/medical_records/clinical_notes/:id':
     careSummariesAndNotes.single,
+  'GET /my_health/v1/medical_records/conditions': healthConditions.all,
+  'GET /my_health/v1/medical_records/conditions/:id': healthConditions.single,
   'GET /my_health/v1/medical_records/allergies': allergies.all,
   'GET /my_health/v1/medical_records/allergies/:id': allergies.single,
   'GET /my_health/v1/medical_records/vaccines': vaccines.all,
