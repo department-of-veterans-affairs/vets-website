99e42887d8f8bae1fff254ea48033aabb8d5672a
Eric Tillberg <Thrillberg@users.noreply.github.com>
2024-08-14 08:20:47 -0400

    Simplify EditLink per feedback

--- src/applications/simple-forms/form-upload/containers/EditLink.jsx
+++ src/applications/simple-forms/form-upload/containers/EditLink.jsx
@@ -1,34 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
 import { VaLink } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 
 const EditLink = ({ href, router }) => {
-  const [link, setLink] = useState(null);
-
-  useEffect(
-    () => {
-      setLink(href);
-    },
-    [href],
-  );
-
   function onClick(e) {
     e.preventDefault();
-    router.push(link);
+    router.push(href);
   }
 
   return <VaLink href={href} onClick={onClick} text="Edit" />;
 };
 
-function mapStateToProps(state) {
-  return {
-    form: state.form,
-  };
-}
-
-export default withRouter(connect(mapStateToProps)(EditLink));
+export default EditLink;
 
 EditLink.propTypes = {
   href: PropTypes.string,
