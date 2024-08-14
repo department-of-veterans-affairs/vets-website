cc790f9706d953bdb113a548567032e8f4ed0506
William Phelps <williamphelps13@gmail.com>
2024-08-14 07:59:09 -0700

    Fix: Update title; Remove inlineTitle instead using labelHeaderLevel and description fo a11y

--- src/applications/accreditation/21a/pages/08-supplementary-statements-chapter/statements.js
+++ src/applications/accreditation/21a/pages/08-supplementary-statements-chapter/statements.js
@@ -1,30 +1,30 @@
 import {
   textareaSchema,
   textareaUI,
-  inlineTitleUI,
-  inlineTitleSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
 
 /** @type {PageSchema} */
 export default {
-  title: 'Supplemental statements',
+  title: 'Supplementary statements',
   path: 'supplementary-statements',
   uiSchema: {
-    supplementalTitle: inlineTitleUI('Optional supplemental statement'),
-    supplementalStatement: textareaUI(
-      'Provide any necessary explanation for your answers and include the associated question number.',
-    ),
-    personalTitle: inlineTitleUI('Optional personal statement'),
-    personalStatement: textareaUI(
-      'If you would like to, you may provide additional information for consideration such as a personal statement explaining why you are interested in VA accreditation.',
-    ),
+    supplementalStatement: textareaUI({
+      title: 'Optional supplemental statement',
+      labelHeaderLevel: 3,
+      description:
+        'Provide any necessary explanation for your answers and include the associated question number.',
+    }),
+    personalStatement: textareaUI({
+      title: 'Optional personal statement',
+      labelHeaderLevel: 3,
+      description:
+        'If you would like to, you may provide additional information for consideration such as a personal statement explaining why you are interested in VA accreditation.',
+    }),
   },
   schema: {
     type: 'object',
     properties: {
-      supplementalTitle: inlineTitleSchema,
       supplementalStatement: textareaSchema,
-      personalTitle: inlineTitleSchema,
       personalStatement: textareaSchema,
     },
   },
