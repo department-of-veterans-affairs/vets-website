815d5033421cb9173bb3920a35bb85eb504eee03
Midge-dev <sean.midgley.dev@gmail.com>
2024-08-14 15:31:50 -0700

    additional check

--- src/applications/disability-benefits/686c-674-v2/containers/App.jsx
+++ src/applications/disability-benefits/686c-674-v2/containers/App.jsx
@@ -2,19 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import { VA_FORM_IDS } from '@department-of-veterans-affairs/platform-forms/constants';
-// import manifest from '../manifest.json';
 import formConfig from '../config/form';
 import { DOC_TITLE } from '../config/constants';
 
-function App({
-  location,
-  children,
-  // isLoggedIn,
-  isLoading,
-  // vaFileNumber,
-  featureToggles,
-  savedForms,
-}) {
+function App({ location, children, isLoading, featureToggles, savedForms }) {
   // Must match the H1
   document.title = DOC_TITLE;
   // Handle loading
@@ -22,16 +13,13 @@ function App({
     return <va-loading-indicator message="Loading your information..." />;
   }
 
-  const flipperV2 = featureToggles.vaDependentsV2;
-  const hasV1Form = savedForms.some(
-    form => form.form === VA_FORM_IDS.FORM_21_686C,
-  );
-  const hasV2Form = savedForms.some(
-    form => form.form === VA_FORM_IDS.FORM_21_686CV2,
+  const isFlipperV2Enabled = featureToggles.vaDependentsV2;
+
+  const hasV1InProgress = savedForms?.some(
+    ({ form }) => form === VA_FORM_IDS.FORM_21_686C,
   );
 
-  const shouldUseV2 = hasV2Form || (flipperV2 && !hasV1Form);
-  if (!shouldUseV2) {
+  if (!isFlipperV2Enabled || hasV1InProgress) {
     window.location.href = '/view-change-dependents/add-remove-form-21-686c/';
     return <></>;
   }
@@ -43,27 +31,6 @@ function App({
       </RoutedSavableApp>
     </article>
   );
-  // If on intro page, just return
-  // if (location.pathname === '/introduction') {
-  //   return content;
-  // }
-
-  // // TODO: Re-enable once Introduction page has been built and in place
-
-  // // If a user is not logged in OR
-  // // a user is logged in, but hasn't gone through va file number validation
-  // // redirect them to the introduction page.
-  // if (
-  //   !isLoggedIn ||
-  //   (isLoggedIn && !vaFileNumber?.hasVaFileNumber?.VALIDVAFILENUMBER)
-  // ) {
-  //   document.location.replace(`${manifest.rootUrl}`);
-  //   return (
-  //     <va-loading-indicator message="Redirecting to introduction page..." />
-  //   );
-  // }
-
-  // return content;
 }
 
 const mapStateToProps = state => {
