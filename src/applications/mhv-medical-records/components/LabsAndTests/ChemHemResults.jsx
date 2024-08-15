bcb4f27c5c0350e4ec4e283bb3584e35c8bb3e75
mattwrightva <107576133+mattwrightva@users.noreply.github.com>
2024-08-15 08:45:05 -0600

    Mhv 60214 codespaces fixes (#31405)

--- src/applications/mhv-medical-records/components/LabsAndTests/ChemHemResults.jsx
+++ src/applications/mhv-medical-records/components/LabsAndTests/ChemHemResults.jsx
@@ -32,7 +32,7 @@ const ChemHemResults = props => {
                 {result.status}
               </p>
               <h4 className="vads-u-font-size--base vads-u-font-family--sans vads-u-margin--0">
-                Lab comments
+                Interpretation
               </h4>
               <p className="vads-u-margin--0 vads-u-padding-bottom--2">
                 {result.labComments}
