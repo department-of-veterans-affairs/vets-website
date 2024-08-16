0a0881d1c12699e8cbc72560ca814b2d347d2dc3
Brandon Cooper <brandon.cooper@adhocteam.us>
2024-08-16 09:31:57 -0400

    check if process is undefined for cypress test

--- src/applications/caregivers/utils/mapbox/mapboxToken.js
+++ src/applications/caregivers/utils/mapbox/mapboxToken.js
@@ -1,3 +1,3 @@
 export const mapboxToken =
-  process?.env?.MAPBOX_TOKEN ||
+  (typeof process !== 'undefined' && process.env.MAPBOX_TOKEN) ||
   'pk.eyJ1IjoiYWRob2MiLCJhIjoiY2wyZjNwM3dxMDZ4YjNjbzVwbTZ5aWQ1dyJ9.D8TZ1a4WobqcdYLWntXV_w';
