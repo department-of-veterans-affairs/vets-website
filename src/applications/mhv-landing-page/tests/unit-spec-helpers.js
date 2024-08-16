0c34cc0dd5bab979b3ee70a5b8a86c1749a9c868
Richard Davis <richard.davis@agile6.com>
2024-08-16 12:59:26 -0400

    Adds jsdocs to spec helper functions

--- src/applications/mhv-landing-page/tests/unit-spec-helpers.js
+++ src/applications/mhv-landing-page/tests/unit-spec-helpers.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { render as rtlRender } from '@testing-library/react';
+
+import TestingLibrary from '@testing-library/react';
+
 import { MemoryRouter, Routes, Route } from 'react-router-dom-v5-compat';
 
 import { renderInReduxProvider } from '@department-of-veterans-affairs/platform-testing/react-testing-library-helpers';
 
+/**
+ * Unit spec helper that renders ui within a react-router@v6 <Route />.
+ * @param {React.ReactElement} ui
+ * @returns {TestingLibrary.RenderResult}
+ */
 export const renderWithRouter = ui =>
-  rtlRender(
+  TestingLibrary.render(
     <MemoryRouter>
       <Routes>
         <Route index element={ui} />
@@ -13,12 +20,19 @@ export const renderWithRouter = ui =>
     </MemoryRouter>,
   );
 
-export const render = (ui, props) =>
+/**
+ * Unit spec helper that renders ui within a react-router@v6 <Route /> with redux.
+ *   Calls renderInReduxProvider from src/platform/testing/unit/react-testing-library-helpers.js
+ * @param {React.ReactElement} ui
+ * @param {Object} options
+ * @returns {TestingLibrary.RenderResult}
+ */
+export const render = (ui, options = {}) =>
   renderInReduxProvider(
     <MemoryRouter>
       <Routes>
         <Route index element={ui} />
       </Routes>
     </MemoryRouter>,
-    props,
+    options,
   );
