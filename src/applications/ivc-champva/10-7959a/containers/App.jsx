9dd5570c34c46f3f8bc7d1a16e8738a87a4d47f3
Michael Clement <michael.clement@adhocteam.us>
2024-08-19 12:33:27 -0500

    added 10-7959a feature toggle to frontend

--- src/applications/ivc-champva/10-7959a/containers/App.jsx
+++ src/applications/ivc-champva/10-7959a/containers/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { VaBreadcrumbs } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+import { Toggler } from 'platform/utilities/feature-toggles';
 import formConfig from '../config/form';
+import WIP from '../../shared/components/WIP';
 
 const breadcrumbList = [
   { href: '/', label: 'Home' },
@@ -19,10 +21,25 @@ const breadcrumbList = [
 export default function App({ location, children }) {
   return (
     <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
-      <VaBreadcrumbs breadcrumbList={breadcrumbList} />
-      <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-        {children}
-      </RoutedSavableApp>
+      <Toggler toggleName={Toggler.TOGGLE_NAMES.form107959a}>
+        <Toggler.Enabled>
+          <VaBreadcrumbs breadcrumbList={breadcrumbList} />
+          <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+            {children}
+          </RoutedSavableApp>
+        </Toggler.Enabled>
+        <Toggler.Disabled>
+          <br />
+          <WIP
+            content={{
+              description:
+                'We’re rolling out the CHAMPVA Claim Form (VA Form 10-7959a) in stages. It’s not quite ready yet. Please check back again soon.',
+              redirectLink: '/',
+              redirectText: 'Return to VA home page',
+            }}
+          />
+        </Toggler.Disabled>
+      </Toggler>
     </div>
   );
 }
