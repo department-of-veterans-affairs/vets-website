1b87fb244a307d338d6c15bd1f5197058211a89d
William Phelps <williamphelps13@gmail.com>
2024-08-19 13:54:07 -0700

    Update standingWithBar to lawLicense based on addition of page title

--- src/applications/accreditation/21a/pages/01-personal-information-chapter/lawLicense.js
+++ src/applications/accreditation/21a/pages/01-personal-information-chapter/lawLicense.js
@@ -6,19 +6,19 @@ import {
 
 /** @type {PageSchema} */
 export default {
-  title: 'Standing with the bar',
-  path: 'standing-with-bar',
+  title: 'Law license',
+  path: 'law-license',
   uiSchema: {
     ...titleUI('Law license'),
-    standingWithBar: yesNoUI(
+    lawLicense: yesNoUI(
       'Are you currently an active member in good standing of the bar of the highest court of a state or territory of the United States?',
     ),
   },
   schema: {
     type: 'object',
     properties: {
-      standingWithBar: yesNoSchema,
+      lawLicense: yesNoSchema,
     },
-    required: ['standingWithBar'],
+    required: ['lawLicense'],
   },
 };
