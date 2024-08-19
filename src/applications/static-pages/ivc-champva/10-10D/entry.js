11105a98e371353dbd9a9186812f82236d1f447e
Michael Clement <michael.clement@adhocteam.us>
2024-08-19 11:40:18 -0500

    updated widget IDs in webpack chunk comments

--- src/applications/static-pages/ivc-champva/10-10D/entry.js
+++ src/applications/static-pages/ivc-champva/10-10D/entry.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 export default function create1010DAccess(store, widgetType) {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   if (root) {
-    import(/* webpackChunkName: "form-10-10d" */ './App').then(module => {
+    import(/* webpackChunkName: "form1010d" */ './App').then(module => {
       const App = module.default;
       ReactDOM.render(
         <Provider store={store}>
