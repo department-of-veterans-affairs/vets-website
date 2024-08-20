ddfd75ec488c61ea4afccb0657fff27ace92c5bb
fazilqa <127263470+fazilqa@users.noreply.github.com>
2024-08-20 08:59:39 -0700

    MHV 60903 verify filter accordion ff and clear filters button (#31469)
    * add filter buttons and dropdowns test
    
    * update selectors and methods

--- src/applications/mhv-secure-messaging/tests/e2e/sort-filter-test/secure-messaging-inbox-folder-add-filter-errors.cypress.spec.js
+++ src/applications/mhv-secure-messaging/tests/e2e/sort-filter-test/secure-messaging-inbox-folder-add-filter-errors.cypress.spec.js
@@ -1,8 +1,8 @@
-import SecureMessagingSite from './sm_site/SecureMessagingSite';
-import PatientInboxPage from './pages/PatientInboxPage';
-import mockMessages from './fixtures/messages-response.json';
-import { Locators } from './utils/constants';
-import PatientErrorPage from './pages/PatientErrorPage';
+import SecureMessagingSite from '../sm_site/SecureMessagingSite';
+import PatientInboxPage from '../pages/PatientInboxPage';
+import mockMessages from '../fixtures/messages-response.json';
+import { AXE_CONTEXT, Locators } from '../utils/constants';
+import PatientErrorPage from '../pages/PatientErrorPage';
 
 describe('Secure Messaging Inbox', () => {
   it('Secure Messaging Inbox Filter Validation', () => {
@@ -12,22 +12,29 @@ describe('Secure Messaging Inbox', () => {
     PatientInboxPage.loadInboxMessages(mockMessages, messageDetails);
     PatientInboxPage.clickAdditionalFilterButton();
     PatientInboxPage.selectDateRange('Custom');
+
     cy.get(Locators.FROM_TO_DATES_CONTAINER)
       .find('legend')
       .eq(0)
       .should('have.text', 'Start date (*Required)');
+
     cy.get(Locators.FROM_TO_DATES_CONTAINER)
       .find('legend')
       .eq(1)
       .should('have.text', 'End date (*Required)');
+
     cy.get(Locators.BUTTONS.FILTER).click();
+
     PatientErrorPage.verifyFromToDateErrorMessageText(
       0,
       'Please enter a start date.',
     );
+
     PatientErrorPage.verifyFromToDateErrorMessageText(
       1,
       'Please enter an end date.',
     );
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT);
   });
 });
