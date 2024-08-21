320a2443c8d61d2f52218932b3faa2c9eee7687e
Midge-dev <sean.midgley.dev@gmail.com>
2024-08-21 15:13:44 -0700

    Init

--- src/applications/disability-benefits/686c-674-v2/config/chapters/report-divorce/former-spouse-information/formerSpouseInformation.js
+++ src/applications/disability-benefits/686c-674-v2/config/chapters/report-divorce/former-spouse-information/formerSpouseInformation.js
@@ -1,112 +1,92 @@
-import merge from 'lodash/merge';
-import currentOrPastDateUI from 'platform/forms-system/src/js/definitions/currentOrPastDate';
-import ssnUI from 'platform/forms-system/src/js/definitions/ssn';
-import environment from 'platform/utilities/environment';
-import { validateName, reportDivorce } from '../../../utilities';
-import { TASK_KEYS } from '../../../constants';
 import {
-  isChapterFieldRequired,
-  PensionIncomeRemovalQuestionTitle,
-} from '../../../helpers';
-import { locationUISchema } from '../../../location-schema';
+  titleUI,
+  fullNameNoSuffixUI,
+  fullNameNoSuffixSchema,
+  ssnUI,
+  ssnSchema,
+  dateOfBirthUI,
+  dateOfBirthSchema,
+} from 'platform/forms-system/src/js/web-component-patterns';
+import { TASK_KEYS } from '../../../constants';
+import { isChapterFieldRequired } from '../../../helpers';
 
 export const schema = {
   type: 'object',
   properties: {
-    reportDivorce,
+    reportDivorce: {
+      type: 'object',
+      properties: {
+        fullName: fullNameNoSuffixSchema,
+        ssn: ssnSchema,
+        birthDate: dateOfBirthSchema,
+      },
+    },
   },
 };
 
 export const uiSchema = {
   reportDivorce: {
+    ...titleUI('Divorced spouse’s information'),
     fullName: {
-      'ui:validations': [validateName],
-      first: {
-        'ui:title': 'Former spouse’s first name',
-        'ui:errorMessages': {
-          required: 'Enter a first name',
-          pattern: 'This field accepts alphabetic characters only',
-        },
-        'ui:required': formData =>
-          isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
-      },
-      middle: {
-        'ui:title': 'Former spouse’s middle name',
-        'ui:options': {
-          hideEmptyValueInReview: true,
-        },
-        'ui:errorMessages': {
-          pattern: 'This field accepts alphabetic characters only',
-        },
-      },
-      last: {
-        'ui:title': 'Former spouse’s last name',
-        'ui:errorMessages': {
-          required: 'Enter a last name',
-          pattern: 'This field accepts alphabetic characters only',
-        },
-        'ui:required': formData =>
-          isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
-      },
-      suffix: {
-        'ui:title': 'Former spouse’s suffix',
-        'ui:options': {
-          widgetClassNames: 'form-select-medium',
-          hideIf: () => true,
-        },
+      ...fullNameNoSuffixUI(title => `Former spouse’s ${title}`),
+      'ui:options': {
+        width: 'md',
       },
     },
     ssn: {
-      ...ssnUI,
-      'ui:title': 'Former spouse’s Social Security number',
+      ...ssnUI('Former spouse’s Social Security number'),
       'ui:required': formData =>
         isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
-    },
-    birthDate: merge(currentOrPastDateUI('Former spouse’s date of birth'), {
-      'ui:required': formData =>
-        isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
-    }),
-    date: merge(currentOrPastDateUI('Date of divorce'), {
-      'ui:required': formData =>
-        isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
-    }),
-    location: locationUISchema(
-      'reportDivorce',
-      'location',
-      false,
-      'Where did this marriage end?',
-      TASK_KEYS.reportDivorce,
-    ),
-    reasonMarriageEnded: {
-      'ui:required': formData =>
-        isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
-      'ui:title': 'Reason marriage ended',
-      'ui:widget': 'radio',
-      'ui:errorMessages': {
-        required: 'Select an option',
-      },
       'ui:options': {
-        updateSchema: () => ({
-          enumNames: ['Divorce', 'Annulment or other'],
-        }),
+        width: 'md',
       },
     },
-    explanationOfOther: {
-      'ui:title': 'Give a brief explanation',
+    birthDate: {
+      ...dateOfBirthUI('Former spouse’s date of birth'),
       'ui:required': formData =>
-        formData?.reportDivorce?.reasonMarriageEnded === 'Other',
-      'ui:options': {
-        expandUnder: 'reasonMarriageEnded',
-        expandUnderCondition: 'Other',
-      },
+        isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
     },
-    spouseIncome: {
-      'ui:options': {
-        hideIf: () => environment.isProduction(),
-        hideEmptyValueInReview: true,
-      },
-      'ui:title': PensionIncomeRemovalQuestionTitle,
-      'ui:widget': 'yesNo',
-    },
+    // date: merge(currentOrPastDateUI('Date of divorce'), {
+    //   'ui:required': formData =>
+    //     isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
+    // }),
+    // location: locationUISchema(
+    //   'reportDivorce',
+    //   'location',
+    //   false,
+    //   'Where did this marriage end?',
+    //   TASK_KEYS.reportDivorce,
+    // ),
+    // reasonMarriageEnded: {
+    //   'ui:required': formData =>
+    //     isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
+    //   'ui:title': 'Reason marriage ended',
+    //   'ui:widget': 'radio',
+    //   'ui:errorMessages': {
+    //     required: 'Select an option',
+    //   },
+    //   'ui:options': {
+    //     updateSchema: () => ({
+    //       enumNames: ['Divorce', 'Annulment or other'],
+    //     }),
+    //   },
+    // },
+    // explanationOfOther: {
+    //   'ui:title': 'Give a brief explanation',
+    //   'ui:required': formData =>
+    //     formData?.reportDivorce?.reasonMarriageEnded === 'Other',
+    //   'ui:options': {
+    //     expandUnder: 'reasonMarriageEnded',
+    //     expandUnderCondition: 'Other',
+    //   },
+    // },
+    // spouseIncome: {
+    //   'ui:options': {
+    //     hideIf: () => environment.isProduction(),
+    //     hideEmptyValueInReview: true,
+    //   },
+    //   'ui:title': PensionIncomeRemovalQuestionTitle,
+    //   'ui:widget': 'yesNo',
+    // },
   },
 };
