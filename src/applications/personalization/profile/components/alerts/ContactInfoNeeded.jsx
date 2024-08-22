0d511b5bf3a65a8c00707665c6edf0afd11eab9f
Nick Sayre <nick.sayre@adhocteam.us>
2024-08-22 13:52:06 -0500

    846 - replace magic strings with constants; change conditional to be falsey; change nesting of two conditionals for probably efficiency; remove superfluous proptypes; simplify export

--- src/applications/personalization/profile/components/alerts/ContactInfoNeeded.jsx
+++ src/applications/personalization/profile/components/alerts/ContactInfoNeeded.jsx
@@ -6,41 +6,38 @@ import {
   VaLinkAction,
 } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { Toggler } from 'platform/utilities/feature-toggles';
+import { FIELD_NAMES } from 'platform/user/profile/vap-svc/constants';
 
-const ContactInfoNeeded = () => {
-  const email = useSelector(state => selectVAPContactInfoField(state, 'email'));
+export const ContactInfoNeeded = () => {
+  const email = useSelector(state =>
+    selectVAPContactInfoField(state, FIELD_NAMES.EMAIL),
+  );
   const mailingAddress = useSelector(state =>
-    selectVAPContactInfoField(state, 'mailingAddress'),
+    selectVAPContactInfoField(state, FIELD_NAMES.MAILING_ADDRESS),
   );
   const mobilePhone = useSelector(state =>
-    selectVAPContactInfoField(state, 'mobilePhone'),
+    selectVAPContactInfoField(state, FIELD_NAMES.MOBILE_PHONE),
   );
 
   const contactInfoIsNeeded =
-    email?.emailAddress === null ||
-    mailingAddress?.addressLine1 === null ||
-    mobilePhone?.phoneNumber === null;
+    !email?.emailAddress ||
+    !mailingAddress?.addressLine1 ||
+    !mobilePhone?.phoneNumber;
 
-  return (
+  return contactInfoIsNeeded ? (
     <Toggler toggleName={Toggler.TOGGLE_NAMES.veteranOnboardingContactInfoFlow}>
       <Toggler.Enabled>
-        {contactInfoIsNeeded && (
-          <div className="vads-u-margin-bottom--4">
-            <VaAlert data-testid="account-blocked-alert" status="info" uswds>
-              <h2 slot="headline">We need your contact information</h2>
-              <p>
-                Add your contact information to your profile so that we can
-                contact you about your VA benefits and services.
-              </p>
-              <VaLinkAction href="" text="Add your contact information" />
-            </VaAlert>
-          </div>
-        )}
+        <div className="vads-u-margin-bottom--4">
+          <VaAlert data-testid="account-blocked-alert" status="info" uswds>
+            <h2 slot="headline">We need your contact information</h2>
+            <p>
+              Add your contact information to your profile so that we can
+              contact you about your VA benefits and services.
+            </p>
+            <VaLinkAction href="" text="Add your contact information" />
+          </VaAlert>
+        </div>
       </Toggler.Enabled>
     </Toggler>
-  );
+  ) : null;
 };
-
-ContactInfoNeeded.propTypes = {};
-
-export { ContactInfoNeeded };
