484b4ccaa1773449819d1b96dfd1b34de3849e3b
jerekshoe <jerek.shoemaker@adhocteam.us>
2024-08-22 09:41:04 -0500

    Fixing issues with FilesPage unit tests

--- src/applications/claims-status/tests/components/FilesPage.unit.spec.jsx
+++ src/applications/claims-status/tests/components/FilesPage.unit.spec.jsx
@@ -11,14 +11,9 @@ import { FilesPage } from '../../containers/FilesPage';
 import * as AdditionalEvidencePage from '../../components/claim-files-tab/AdditionalEvidencePage';
 import { renderWithRouter } from '../utils';
 
-const getStore = (
-  cstUseClaimDetailsV2Enabled = true,
-  cst5103UpdateEnabled = false,
-) =>
+const getStore = (cst5103UpdateEnabled = false) =>
   createStore(() => ({
     featureToggles: {
-      // eslint-disable-next-line camelcase
-      cst_use_claim_details_v2: cstUseClaimDetailsV2Enabled,
       // eslint-disable-next-line camelcase
       cst_5103_update_enabled: cst5103UpdateEnabled,
     },
@@ -33,6 +28,18 @@ const props = {
 };
 
 describe('<FilesPage>', () => {
+  let stub;
+  before(() => {
+    // Stubbing out AdditionalEvidencePage because we're not interested
+    // in setting up all of the redux state needed to test it
+    stub = sinon.stub(AdditionalEvidencePage, 'default');
+    stub.returns(<div data-testid="additional-evidence-page" />);
+  });
+
+  after(() => {
+    stub.restore();
+  });
+
   it('should render loading state', () => {
     const { container } = renderWithRouter(
       <FilesPage
@@ -157,231 +164,214 @@ describe('<FilesPage>', () => {
     expect(clearNotification.called).to.be.true;
   });
 
-  context('cstUseClaimDetailsV2 feature flag enabled', () => {
-    let stub;
-    before(() => {
-      // Stubbing out AdditionalEvidencePage because we're not interested
-      // in setting up all of the redux state needed to test it
-      stub = sinon.stub(AdditionalEvidencePage, 'default');
-      stub.returns(<div data-testid="additional-evidence-page" />);
+  context('when claim is open', () => {
+    it('should render files page, showing additional evidence section without alerts, and docs filed section', () => {
+      const claim = {
+        id: '1',
+        type: 'claim',
+        attributes: {
+          claimDate: '2023-01-01',
+          claimPhaseDates: {
+            currentPhaseBack: false,
+            phaseChangeDate: '2023-02-08',
+            latestPhaseType: 'INITIAL_REVIEW',
+            previousPhases: {
+              phase1CompleteDate: '2023-02-08',
+            },
+          },
+          closeDate: null,
+          documentsNeeded: false,
+          decisionLetterSent: false,
+          status: 'INITIAL_REVIEW',
+          supportingDocuments: [],
+          trackedItems: [],
+        },
+      };
+
+      const { container, getByTestId } = renderWithRouter(
+        <Provider store={getStore()}>
+          <FilesPage
+            claim={claim}
+            message={{ title: 'Test', body: 'Body' }}
+            clearNotification={() => {}}
+          />
+        </Provider>,
+      );
+      const filesPage = $('#tabPanelFiles', container);
+
+      expect(filesPage).to.exist;
+      expect($('.claim-file-header-container', container)).to.exist;
+      expect(getByTestId('additional-evidence-page')).to.exist;
+      expect($('.documents-filed-container', container)).to.exist;
+      expect($('.claims-requested-files-container', container)).not.to.exist;
     });
 
-    after(() => {
-      stub.restore();
+    it('should render files page, showing additional evidence section with alerts, and docs filed section when using lighthouse', () => {
+      const claim = {
+        id: '1',
+        type: 'claim',
+        attributes: {
+          claimDate: '2023-01-01',
+          claimPhaseDates: {
+            currentPhaseBack: false,
+            phaseChangeDate: '2023-02-08',
+            latestPhaseType: 'INITIAL_REVIEW',
+            previousPhases: {
+              phase1CompleteDate: '2023-02-08',
+            },
+          },
+          closeDate: null,
+          documentsNeeded: true,
+          decisionLetterSent: false,
+          status: 'INITIAL_REVIEW',
+          supportingDocuments: [],
+          trackedItems: [
+            {
+              id: 1,
+              status: 'NEEDED_FROM_YOU',
+              displayName: 'Test',
+              description: 'Test',
+              suspenseDate: '2024-02-01',
+              date: '2023-01-01',
+            },
+          ],
+        },
+      };
+      const { container, getByTestId } = renderWithRouter(
+        <Provider store={getStore()}>
+          <FilesPage
+            claim={claim}
+            message={{ title: 'Test', body: 'Body' }}
+            clearNotification={() => {}}
+          />
+        </Provider>,
+      );
+      const filesPage = $('#tabPanelFiles', container);
+
+      expect(filesPage).to.exist;
+      expect($('.claim-file-header-container', container)).to.exist;
+      expect(getByTestId('additional-evidence-page')).to.exist;
+      expect($('.documents-filed-container', container)).to.exist;
+      expect($('.claims-requested-files-container', container)).to.not.exist;
     });
 
-    context('when claim is open', () => {
-      it('should render files page, showing additional evidence section without alerts, and docs filed section', () => {
+    context('when cst5103UpdateEnabled feature flag is disabled', () => {
+      it('should render ask va to decide component', () => {
         const claim = {
-          id: '1',
+          id: 1,
           type: 'claim',
           attributes: {
-            claimDate: '2023-01-01',
             claimPhaseDates: {
               currentPhaseBack: false,
-              phaseChangeDate: '2023-02-08',
-              latestPhaseType: 'INITIAL_REVIEW',
+              phaseChangeDate: '2023-03-04',
+              latestPhaseType: 'GATHERING_OF_EVIDENCE',
               previousPhases: {
                 phase1CompleteDate: '2023-02-08',
+                phase2CompleteDate: '2023-03-04',
               },
             },
-            closeDate: null,
             documentsNeeded: false,
             decisionLetterSent: false,
-            status: 'INITIAL_REVIEW',
+            evidenceWaiverSubmitted5103: false,
+            status: 'EVIDENCE_GATHERING_REVIEW_DECISION',
             supportingDocuments: [],
             trackedItems: [],
           },
         };
 
-        const { container, getByTestId } = renderWithRouter(
+        const { getByText } = renderWithRouter(
           <Provider store={getStore()}>
             <FilesPage
               claim={claim}
               message={{ title: 'Test', body: 'Body' }}
               clearNotification={() => {}}
             />
-            ,
           </Provider>,
         );
-        const filesPage = $('#tabPanelFiles', container);
 
-        expect(filesPage).to.exist;
-        expect($('.claim-file-header-container', container)).to.exist;
-        expect(getByTestId('additional-evidence-page')).to.exist;
-        expect($('.documents-filed-container', container)).to.exist;
-        expect($('.claims-requested-files-container', container)).not.to.exist;
-      });
-
-      it('should render files page, showing additional evidence section with alerts, and docs filed section when using lighthouse', () => {
-        const claim = {
-          id: '1',
-          type: 'claim',
-          attributes: {
-            claimDate: '2023-01-01',
-            claimPhaseDates: {
-              currentPhaseBack: false,
-              phaseChangeDate: '2023-02-08',
-              latestPhaseType: 'INITIAL_REVIEW',
-              previousPhases: {
-                phase1CompleteDate: '2023-02-08',
-              },
-            },
-            closeDate: null,
-            documentsNeeded: true,
-            decisionLetterSent: false,
-            status: 'INITIAL_REVIEW',
-            supportingDocuments: [],
-            trackedItems: [
-              {
-                id: 1,
-                status: 'NEEDED_FROM_YOU',
-                displayName: 'Test',
-                description: 'Test',
-                suspenseDate: '2024-02-01',
-                date: '2023-01-01',
-              },
-            ],
-          },
-        };
-        const { container, getByTestId } = renderWithRouter(
-          <Provider store={getStore()}>
-            <FilesPage
-              claim={claim}
-              message={{ title: 'Test', body: 'Body' }}
-              clearNotification={() => {}}
-            />
-            ,
-          </Provider>,
-        );
-        const filesPage = $('#tabPanelFiles', container);
-
-        expect(filesPage).to.exist;
-        expect($('.claim-file-header-container', container)).to.exist;
-        expect(getByTestId('additional-evidence-page')).to.exist;
-        expect($('.documents-filed-container', container)).to.exist;
-        expect($('.claims-requested-files-container', container)).to.not.exist;
-      });
-
-      context('when cst5103UpdateEnabled feature flag is disabled', () => {
-        it('should render ask va to decide component', () => {
-          const claim = {
-            id: 1,
-            type: 'claim',
-            attributes: {
-              claimPhaseDates: {
-                currentPhaseBack: false,
-                phaseChangeDate: '2023-03-04',
-                latestPhaseType: 'GATHERING_OF_EVIDENCE',
-                previousPhases: {
-                  phase1CompleteDate: '2023-02-08',
-                  phase2CompleteDate: '2023-03-04',
-                },
-              },
-              documentsNeeded: false,
-              decisionLetterSent: false,
-              evidenceWaiverSubmitted5103: false,
-              status: 'EVIDENCE_GATHERING_REVIEW_DECISION',
-              supportingDocuments: [],
-              trackedItems: [],
-            },
-          };
-
-          const { getByText } = renderWithRouter(
-            <Provider store={getStore()}>
-              <FilesPage
-                claim={claim}
-                message={{ title: 'Test', body: 'Body' }}
-                clearNotification={() => {}}
-              />
-            </Provider>,
-          );
-
-          getByText('Ask for your Claim Decision');
-        });
-      });
-
-      context('when cst5103UpdateEnabled feature flag is enabled', () => {
-        it('should not render ask va to decide component', () => {
-          const claim = {
-            id: 1,
-            type: 'claim',
-            attributes: {
-              claimPhaseDates: {
-                currentPhaseBack: false,
-                phaseChangeDate: '2023-03-04',
-                latestPhaseType: 'GATHERING_OF_EVIDENCE',
-                previousPhases: {
-                  phase1CompleteDate: '2023-02-08',
-                  phase2CompleteDate: '2023-03-04',
-                },
-              },
-              documentsNeeded: false,
-              decisionLetterSent: false,
-              evidenceWaiverSubmitted5103: false,
-              status: 'EVIDENCE_GATHERING_REVIEW_DECISION',
-              supportingDocuments: [],
-              trackedItems: [],
-            },
-          };
-
-          const { queryByText } = renderWithRouter(
-            <Provider store={getStore(true, true)}>
-              <FilesPage
-                claim={claim}
-                message={{ title: 'Test', body: 'Body' }}
-                clearNotification={() => {}}
-              />
-            </Provider>,
-          );
-
-          expect(queryByText('Ask for your Claim Decision')).to.not.exist;
-        });
+        getByText('Ask for your Claim Decision');
       });
     });
 
-    context('when claim is closed', () => {
-      it('should render files page, showing additional evidence section, and docs filed section', () => {
+    context('when cst5103UpdateEnabled feature flag is enabled', () => {
+      it('should not render ask va to decide component', () => {
         const claim = {
-          id: '1',
+          id: 1,
           type: 'claim',
           attributes: {
-            claimDate: '2023-01-01',
             claimPhaseDates: {
               currentPhaseBack: false,
-              phaseChangeDate: '2023-01-31',
-              latestPhaseType: 'COMPLETE',
+              phaseChangeDate: '2023-03-04',
+              latestPhaseType: 'GATHERING_OF_EVIDENCE',
               previousPhases: {
-                phase7CompleteDate: '2023-02-08',
+                phase1CompleteDate: '2023-02-08',
+                phase2CompleteDate: '2023-03-04',
               },
             },
-            closeDate: '2023-01-31',
             documentsNeeded: false,
             decisionLetterSent: false,
-            status: 'COMPLETE',
+            evidenceWaiverSubmitted5103: false,
+            status: 'EVIDENCE_GATHERING_REVIEW_DECISION',
             supportingDocuments: [],
             trackedItems: [],
           },
         };
 
-        const { container, getByTestId } = renderWithRouter(
-          <Provider store={getStore()}>
+        const { queryByText } = renderWithRouter(
+          <Provider store={getStore(true)}>
             <FilesPage
               claim={claim}
               message={{ title: 'Test', body: 'Body' }}
               clearNotification={() => {}}
             />
-            ,
           </Provider>,
         );
-        const filesPage = $('#tabPanelFiles', container);
 
-        expect(filesPage).to.exist;
-        expect($('.claim-file-header-container', container)).to.exist;
-        expect(getByTestId('additional-evidence-page')).to.exist;
-        expect($('.documents-filed-container', container)).to.exist;
-        expect($('.claims-requested-files-container', container)).not.to.exist;
+        expect(queryByText('Ask for your Claim Decision')).to.not.exist;
       });
+    });
+  });
+
+  context('when claim is closed', () => {
+    it('should render files page, showing additional evidence section, and docs filed section', () => {
+      const claim = {
+        id: '1',
+        type: 'claim',
+        attributes: {
+          claimDate: '2023-01-01',
+          claimPhaseDates: {
+            currentPhaseBack: false,
+            phaseChangeDate: '2023-01-31',
+            latestPhaseType: 'COMPLETE',
+            previousPhases: {
+              phase7CompleteDate: '2023-02-08',
+            },
+          },
+          closeDate: '2023-01-31',
+          documentsNeeded: false,
+          decisionLetterSent: false,
+          status: 'COMPLETE',
+          supportingDocuments: [],
+          trackedItems: [],
+        },
+      };
+
+      const { container, getByTestId } = renderWithRouter(
+        <Provider store={getStore()}>
+          <FilesPage
+            claim={claim}
+            message={{ title: 'Test', body: 'Body' }}
+            clearNotification={() => {}}
+          />
+        </Provider>,
+      );
+      const filesPage = $('#tabPanelFiles', container);
+
+      expect(filesPage).to.exist;
+      expect($('.claim-file-header-container', container)).to.exist;
+      expect(getByTestId('additional-evidence-page')).to.exist;
+      expect($('.documents-filed-container', container)).to.exist;
+      expect($('.claims-requested-files-container', container)).not.to.exist;
     });
   });
 });
