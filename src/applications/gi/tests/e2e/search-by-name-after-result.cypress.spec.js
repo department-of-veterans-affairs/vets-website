dc2a4b7ce685a55759e1d1af5ba5d6fcc2bad660
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2024-08-22 14:55:11 -0400

    added cypress test for after result for search by Name

--- src/applications/gi/tests/e2e/search-by-name-after-result.cypress.spec.js
+++ src/applications/gi/tests/e2e/search-by-name-after-result.cypress.spec.js
@@ -8,16 +8,23 @@ describe('go bill CT Rearch By Name After Result', () => {
     });
     cy.intercept('GET', '/data/cms/vamc-ehr.json', { statusCode: 200 });
     cy.visit('education/gi-bill-comparison-tool/');
+    cy.get('[data-testid="ct-input"]').type('Texas');
+    cy.get('[data-testid="search-btn"]').click();
   });
   it('should show reslut when user types School, employer, or training provider in the search input and hits Search button', () => {
     cy.injectAxeThenAxeCheck();
-    cy.get('[data-testid="ct-input"]').type('Texas');
-    cy.get('[data-testid="search-btn"]').click();
     cy.get('[id="name-search-results-count"]').as('searchResults');
     cy.get('@searchResults').should('be.focused');
     cy.get('@searchResults').should(
       'contain',
       'Showing 183 search results for "Texas"',
     );
+  });
+  it('should show Update Tuition accordion not expanded and when user click on it, it should expanded', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.get(
+      'button[id="update-tuition,-housing,-and-monthly-benefit-estimates-accordion-button"]',
+    ).as('updateTuition');
+    cy.get('@updateTuition').should('not.have.attr', 'aria-expanded', 'true');
   });
 });
