c9a400810f98c035241eb06ed31fc4fbbf733228
Midge-dev <sean.midgley.dev@gmail.com>
2024-08-23 10:49:44 -0700

    everything

--- src/applications/disability-benefits/686c-674-v2/config/chapters/report-divorce/former-spouse-information/formerSpouseInformation.js
+++ src/applications/disability-benefits/686c-674-v2/config/chapters/report-divorce/former-spouse-information/formerSpouseInformation.js
@@ -2,13 +2,7 @@ import {
   titleUI,
   fullNameNoSuffixUI,
   fullNameNoSuffixSchema,
-  ssnUI,
-  ssnSchema,
-  dateOfBirthUI,
-  dateOfBirthSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
-import { TASK_KEYS } from '../../../constants';
-import { isChapterFieldRequired } from '../../../helpers';
 
 export const schema = {
   type: 'object',
@@ -17,8 +11,6 @@ export const schema = {
       type: 'object',
       properties: {
         fullName: fullNameNoSuffixSchema,
-        ssn: ssnSchema,
-        birthDate: dateOfBirthSchema,
       },
     },
   },
@@ -29,64 +21,6 @@ export const uiSchema = {
     ...titleUI('Divorced spouse’s information'),
     fullName: {
       ...fullNameNoSuffixUI(title => `Former spouse’s ${title}`),
-      'ui:options': {
-        width: 'md',
-      },
     },
-    ssn: {
-      ...ssnUI('Former spouse’s Social Security number'),
-      'ui:required': formData =>
-        isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
-      'ui:options': {
-        width: 'md',
-      },
-    },
-    birthDate: {
-      ...dateOfBirthUI('Former spouse’s date of birth'),
-      'ui:required': formData =>
-        isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
-    },
-    // date: merge(currentOrPastDateUI('Date of divorce'), {
-    //   'ui:required': formData =>
-    //     isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
-    // }),
-    // location: locationUISchema(
-    //   'reportDivorce',
-    //   'location',
-    //   false,
-    //   'Where did this marriage end?',
-    //   TASK_KEYS.reportDivorce,
-    // ),
-    // reasonMarriageEnded: {
-    //   'ui:required': formData =>
-    //     isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
-    //   'ui:title': 'Reason marriage ended',
-    //   'ui:widget': 'radio',
-    //   'ui:errorMessages': {
-    //     required: 'Select an option',
-    //   },
-    //   'ui:options': {
-    //     updateSchema: () => ({
-    //       enumNames: ['Divorce', 'Annulment or other'],
-    //     }),
-    //   },
-    // },
-    // explanationOfOther: {
-    //   'ui:title': 'Give a brief explanation',
-    //   'ui:required': formData =>
-    //     formData?.reportDivorce?.reasonMarriageEnded === 'Other',
-    //   'ui:options': {
-    //     expandUnder: 'reasonMarriageEnded',
-    //     expandUnderCondition: 'Other',
-    //   },
-    // },
-    // spouseIncome: {
-    //   'ui:options': {
-    //     hideIf: () => environment.isProduction(),
-    //     hideEmptyValueInReview: true,
-    //   },
-    //   'ui:title': PensionIncomeRemovalQuestionTitle,
-    //   'ui:widget': 'yesNo',
-    // },
   },
 };
--- src/applications/disability-benefits/686c-674-v2/config/chapters/report-divorce/former-spouse-information/formerSpouseInformationPartTwo.js
+++ src/applications/disability-benefits/686c-674-v2/config/chapters/report-divorce/former-spouse-information/formerSpouseInformationPartTwo.js
@@ -1,68 +1,86 @@
-// import {
-//   titleUI,
-//   currentOrPastDateUI,
-//   currentOrPastDateSchema,
-//   fullNameNoSuffixUI,
-//   fullNameNoSuffixSchema,
-//   ssnUI,
-//   ssnSchema,
-//   dateOfBirthUI,
-//   dateOfBirthSchema,
-// } from 'platform/forms-system/src/js/web-component-patterns';
-// import { TASK_KEYS } from '../../../constants';
-// import { isChapterFieldRequired } from '../../../helpers';
+import {
+  titleUI,
+  textUI,
+  textSchema,
+  currentOrPastDateUI,
+  currentOrPastDateSchema,
+  radioUI,
+  radioSchema,
+} from 'platform/forms-system/src/js/web-component-patterns';
+import VaTextInputField from 'platform/forms-system/src/js/web-component-fields/VaTextInputField';
+import VaSelectField from 'platform/forms-system/src/js/web-component-fields/VaSelectField';
+import VaCheckboxField from 'platform/forms-system/src/js/web-component-fields/VaCheckboxField';
+import { customLocationSchema } from '../../../helpers';
 
-// export const schema = {
-//   type: 'object',
-//   properties: {
-//     reportDivorce: {
-//       type: 'object',
-//       properties: {
-//         date: currentOrPastDateSchema,
-//         location: {},
-//         reasonMarriageEnded: {},
-//         explanationOfOther: {},
-//       },
-//     },
-//   },
-// };
+export const schema = {
+  type: 'object',
+  properties: {
+    reportDivorce: {
+      type: 'object',
+      properties: {
+        date: currentOrPastDateSchema,
+        divorceLocation: customLocationSchema,
+        reasonMarriageEnded: radioSchema(['Divorce', 'Other']),
+        explanationOfOther: textSchema,
+      },
+    },
+  },
+};
 
-// export const uiSchema = {
-//   reportDivorce: {
-//     ...titleUI('Divorced spouse’s information'),
-//     date: merge(currentOrPastDateUI('Date of divorce'), {
-//       'ui:required': formData =>
-//         isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
-//     }),
-//     location: locationUISchema(
-//       'reportDivorce',
-//       'location',
-//       false,
-//       'Where did this marriage end?',
-//       TASK_KEYS.reportDivorce,
-//     ),
-//     reasonMarriageEnded: {
-//       'ui:required': formData =>
-//         isChapterFieldRequired(formData, TASK_KEYS.reportDivorce),
-//       'ui:title': 'Reason marriage ended',
-//       'ui:widget': 'radio',
-//       'ui:errorMessages': {
-//         required: 'Select an option',
-//       },
-//       'ui:options': {
-//         updateSchema: () => ({
-//           enumNames: ['Divorce', 'Annulment or other'],
-//         }),
-//       },
-//     },
-//     explanationOfOther: {
-//       'ui:title': 'Give a brief explanation',
-//       'ui:required': formData =>
-//         formData?.reportDivorce?.reasonMarriageEnded === 'Other',
-//       'ui:options': {
-//         expandUnder: 'reasonMarriageEnded',
-//         expandUnderCondition: 'Other',
-//       },
-//     },
-//   },
-// };
+export const uiSchema = {
+  reportDivorce: {
+    ...titleUI('When, where, and why did this marriage end?'),
+    date: {
+      ...currentOrPastDateUI('Date of divorce'),
+      'ui:required': () => true,
+    },
+    divorceLocation: {
+      outsideUsa: {
+        'ui:title': 'This occurred outside the U.S.',
+        'ui:webComponentField': VaCheckboxField,
+      },
+      location: {
+        city: {
+          'ui:title': 'City',
+          'ui:required': () => true,
+          'ui:autocomplete': 'address-level2',
+          'ui:errorMessages': {
+            required: 'Enter the city where you were married',
+          },
+          'ui:webComponentField': VaTextInputField,
+        },
+        state: {
+          'ui:title': 'State',
+          'ui:webComponentField': VaSelectField,
+          'ui:required': formData =>
+            !formData?.reportDivorce?.divorceLocation?.outsideUsa,
+          'ui:errorMessages': {
+            required: 'Select a state',
+          },
+          'ui:options': {
+            hideIf: formData =>
+              formData?.reportDivorce?.divorceLocation?.outsideUsa,
+          },
+        },
+      },
+    },
+    reasonMarriageEnded: radioUI({
+      title: 'Reason marriage ended',
+      required: () => true,
+      labels: {
+        Divorce: 'Divorce',
+        Other: 'Annulment or other',
+      },
+    }),
+    explanationOfOther: {
+      ...textUI('Briefly describe how the marriage ended'),
+      'ui:required': formData =>
+        formData?.reportDivorce?.reasonMarriageEnded === 'Other',
+      'ui:options': {
+        expandUnder: 'reasonMarriageEnded',
+        expandUnderCondition: 'Other',
+        keepInPageOnReview: true,
+      },
+    },
+  },
+};
