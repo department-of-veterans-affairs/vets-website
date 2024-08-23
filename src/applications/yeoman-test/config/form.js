3873fc0b8ad08563e7b3c4d9c333b553cafe9b4f
Robert Hasselle <robert.hasselle@oddball.io>
2024-08-23 16:37:34 -0500

    Test yeoman generator changes

---
+++ src/applications/yeoman-test/config/form.js
@@ -0,0 +1,88 @@
+import footerContent from 'platform/forms/components/FormFooter';
+import { TITLE, SUBTITLE } from '../constants';
+import manifest from '../manifest.json';
+import IntroductionPage from '../containers/IntroductionPage';
+import ConfirmationPage from '../containers/ConfirmationPage';
+
+import nameAndDateOfBirth from '../pages/nameAndDateOfBirth';
+import identificationInformation from '../pages/identificationInformation';
+import mailingAddress from '../pages/mailingAddress';
+import phoneAndEmailAddress from '../pages/phoneAndEmailAddress';
+
+/** @type {FormConfig} */
+const formConfig = {
+  rootUrl: manifest.rootUrl,
+  urlPrefix: '/',
+  submitUrl: '/v0/api',
+  submit: () =>
+    Promise.resolve({ attributes: { confirmationNumber: '123123123' } }),
+  trackingPrefix: 'yeoman-test-',
+  introduction: IntroductionPage,
+  confirmation: ConfirmationPage,
+  dev: {
+    showNavLinks: true,
+    collapsibleNavLinks: true,
+  },
+  formId: '33-333',
+  saveInProgress: {
+    // messages: {
+    //   inProgress: 'Your benefits application (33-333) is in progress.',
+    //   expired: 'Your saved benefits application (33-333) has expired. If you want to apply for benefits, please start a new application.',
+    //   saved: 'Your benefits application has been saved.',
+    // },
+  },
+  version: 0,
+  prefillEnabled: true,
+  savedFormMessages: {
+    notFound: 'Please start over to apply for benefits.',
+    noAuth: 'Please sign in again to continue your application for benefits.',
+  },
+  title: TITLE,
+  subtitle: SUBTITLE,
+  defaultDefinitions: {},
+  chapters: {
+    personalInformationChapter: {
+      title: 'Your personal information',
+      pages: {
+        nameAndDateOfBirth: {
+          path: 'name-and-date-of-birth',
+          title: 'Name and date of birth',
+          uiSchema: nameAndDateOfBirth.uiSchema,
+          schema: nameAndDateOfBirth.schema,
+        },
+        identificationInformation: {
+          path: 'identification-information',
+          title: 'Identification information',
+          uiSchema: identificationInformation.uiSchema,
+          schema: identificationInformation.schema,
+        },
+      },
+    },
+    mailingAddressChapter: {
+      title: 'Mailing address',
+      pages: {
+        mailingAddress: {
+          path: 'mailing-address',
+          title: 'Mailing address',
+          uiSchema: mailingAddress.uiSchema,
+          schema: mailingAddress.schema,
+        },
+      },
+    },
+    contactInformationChapter: {
+      title: 'Contact information',
+      pages: {
+        phoneAndEmailAddress: {
+          path: 'phone-and-email-address',
+          title: 'Phone and email address',
+          uiSchema: phoneAndEmailAddress.uiSchema,
+          schema: phoneAndEmailAddress.schema,
+        },
+      },
+    },
+  },
+  // getHelp,
+  footerContent,
+};
+
+export default formConfig;
---
+++ src/applications/yeoman-test/constants.js
@@ -0,0 +1,2 @@
+export const TITLE = 'Yeoman Test';
+export const SUBTITLE = 'Subtitle (VA Form 33-333)';
