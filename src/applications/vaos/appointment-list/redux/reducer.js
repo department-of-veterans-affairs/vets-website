645ade9f21009983c36a726db9dced2c2ba37ab6
John Woolschlager <wullaski@gmail.com>
2024-08-23 16:57:37 -0400

    Add actions and reducers

--- src/applications/vaos/appointment-list/redux/reducer.js
+++ src/applications/vaos/appointment-list/redux/reducer.js
@@ -27,6 +27,9 @@ import {
   FETCH_FACILITY_SETTINGS_SUCCEEDED,
   FETCH_FACILITY_SETTINGS,
   FETCH_PROVIDER_SUCCEEDED,
+  FETCH_CLAIM_SUCCEEDED,
+  FETCH_CLAIM_FAILED,
+  FETCH_CLAIM,
 } from './actions';
 
 import {
@@ -49,6 +52,7 @@ const initialState = {
   cancelAppointmentStatus: FETCH_STATUS.notStarted,
   appointmentDetails: {},
   appointmentDetailsStatus: FETCH_STATUS.notStarted,
+  fetchClaimStatus: FETCH_STATUS.notStarted,
   appointmentToCancel: null,
   facilityData: {},
   systemClinicToFacilityMap: {},
@@ -306,6 +310,28 @@ export default function appointmentsReducer(state = initialState, action) {
       return {
         ...state,
         providerData: action.providerData,
+      };
+    case FETCH_CLAIM:
+      return {
+        ...state,
+        fetchClaimStatus: FETCH_STATUS.loading,
+      };
+    case FETCH_CLAIM_SUCCEEDED:
+      return {
+        ...state,
+        fetchClaimStatus: FETCH_STATUS.succeeded,
+        appointmentDetails: {
+          ...state.appointmentDetails,
+          [action.id]: {
+            ...state.appointmentDetails[action.id],
+            claimId: action.claimId,
+          },
+        },
+      };
+    case FETCH_CLAIM_FAILED:
+      return {
+        ...state,
+        fetchClaimStatus: FETCH_STATUS.failed,
       };
     default:
       return state;
