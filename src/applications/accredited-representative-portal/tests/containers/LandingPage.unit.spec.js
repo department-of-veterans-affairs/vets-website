2717bafbaebbd7316d8e1216487713f9073621b1
Oren Mittman <nihil2501@gmail.com>
2024-08-26 01:25:42 -0400

    [ARF-87799] ARP 21a: Extract `TestAppContainer` spec helper.

--- src/applications/accredited-representative-portal/tests/containers/LandingPage.unit.spec.js
+++ src/applications/accredited-representative-portal/tests/containers/LandingPage.unit.spec.js
@@ -1,37 +1,31 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
-import { MemoryRouter } from 'react-router-dom-v5-compat';
-import { Provider } from 'react-redux';
-import LandingPage from '../../containers/LandingPage';
 import createReduxStore from '../../store';
+
+import LandingPage from '../../containers/LandingPage';
+import { TestAppContainer } from '../helpers';
 import {
   FETCH_USER_SUCCESS,
   FETCH_USER_FAILURE,
   FETCH_USER,
 } from '../../actions/user';
 
-const createLandingPageRenderFn = action => {
+function renderTestApp({ initAction } = {}) {
   const store = createReduxStore();
-  if (action) store.dispatch(action);
+  if (initAction) store.dispatch(initAction);
 
-  return () => {
-    return render(
-      <Provider store={store}>
-        <MemoryRouter>
-          <LandingPage />
-        </MemoryRouter>
-      </Provider>,
-    );
-  };
-};
+  return render(
+    <TestAppContainer store={store}>
+      <LandingPage />
+    </TestAppContainer>,
+  );
+}
 
 describe('LandingPage', () => {
   describe('when component renders', () => {
-    const renderLandingPage = createLandingPageRenderFn();
-
     it('renders main heading', () => {
-      const { getByTestId } = renderLandingPage();
+      const { getByTestId } = renderTestApp();
       expect(getByTestId('landing-page-heading').textContent).to.eq(
         'Welcome to the Accredited Representative Portal',
       );
@@ -39,30 +33,30 @@ describe('LandingPage', () => {
   });
 
   describe('when user is being fetched', () => {
-    const renderLandingPage = createLandingPageRenderFn({
+    const initAction = {
       type: FETCH_USER,
-    });
+    };
 
     it('renders a loading spinner', () => {
-      const { getByTestId } = renderLandingPage();
+      const { getByTestId } = renderTestApp({ initAction });
       expect(getByTestId('landing-page-loading-indicator')).to.exist;
     });
   });
 
   describe('when user fetch fails', () => {
-    const renderLandingPage = createLandingPageRenderFn({
+    const initAction = {
       type: FETCH_USER_FAILURE,
-    });
+    };
 
     it('renders the sign in or create account section', () => {
-      const { getByTestId } = renderLandingPage();
+      const { getByTestId } = renderTestApp({ initAction });
       expect(getByTestId('landing-page-create-account-text').textContent).to.eq(
         'Create an account to start managing power of attorney.',
       );
     });
 
     it('renders the link to sign in', () => {
-      const { getByTestId } = renderLandingPage();
+      const { getByTestId } = renderTestApp({ initAction });
       expect(getByTestId('landing-page-sign-in-link').textContent).to.eq(
         'Sign in or create account',
       );
@@ -70,21 +64,21 @@ describe('LandingPage', () => {
   });
 
   describe('when user fetch succeeds', () => {
-    const renderLandingPage = createLandingPageRenderFn({
+    const initAction = {
       type: FETCH_USER_SUCCESS,
       payload: {
         account: {},
         profile: {},
       },
-    });
+    };
 
     it('does not render the sign in or create account section', () => {
-      const { queryByTestId } = renderLandingPage();
+      const { queryByTestId } = renderTestApp({ initAction });
       expect(queryByTestId('landing-page-create-account-text')).to.not.exist;
     });
 
     it('does not render the link to sign in', () => {
-      const { queryByTestId } = renderLandingPage();
+      const { queryByTestId } = renderTestApp({ initAction });
       expect(queryByTestId('landing-page-sign-in-link')).to.not.exist;
     });
   });
