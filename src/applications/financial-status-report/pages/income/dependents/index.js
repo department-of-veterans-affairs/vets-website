85b57df5c1ee8989d23db5ec3b68953a2a59040d
Aaron <aaron.ponce@va.gov>
2024-08-26 14:34:32 -0700

    Add label to hasDependents section of review

--- src/applications/financial-status-report/pages/income/dependents/index.js
+++ src/applications/financial-status-report/pages/income/dependents/index.js
@@ -1,3 +1,21 @@
+import { validateIsNumber } from '../../../utils/validations';
+
+export const uiSchemaEnhanced = {
+  questions: {
+    'ui:options': {
+      hideOnReview: false, // change this to true to hide this question on review page
+    },
+    hasDependents: {
+      'ui:title': 'Number of dependents',
+      'ui:required': () => true,
+      'ui:errorMessages': {
+        required: 'Please enter your dependent(s) information.',
+      },
+      'ui:validations': [validateIsNumber],
+    },
+  },
+};
+
 export const schemaEnhanced = {
   type: 'object',
   properties: {
