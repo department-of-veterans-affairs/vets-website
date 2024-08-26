a4d82b08f53b76aaa62efb99a651d6eb7e0ad530
Oren Mittman <nihil2501@gmail.com>
2024-08-26 01:01:30 -0400

    [ARF-87799] ARP 21a: Use `null` for falsy user profile selector value.

--- src/applications/accredited-representative-portal/selectors/user.js
+++ src/applications/accredited-representative-portal/selectors/user.js
@@ -1,4 +1,4 @@
 export const selectUserIsLoading = state => state.user.profile.loading;
 export const selectUserIsLoggedIn = state => state.user.login.currentlyLoggedIn;
 export const selectUserProfile = state =>
-  selectUserIsLoggedIn(state) && state.user.profile;
+  selectUserIsLoggedIn(state) ? state.user.profile : null;
