f555e1ff2ef9a3d033b162acc289e8b24e46585f
Robin Garrison <robin.garrison@adhocteam.us>
2024-08-26 16:06:35 -0500

    Fix page focusing

--- src/applications/appeals/testing/sc/config/form.js
+++ src/applications/appeals/testing/sc/config/form.js
@@ -138,6 +138,8 @@ const formConfig = {
   // when true, initial focus on page to H3s by default, and enable page
   // scrollAndFocusTarget (selector string or function to scroll & focus)
   useCustomScrollAndFocus: true,
+  scrollAndFocusTarget: focusH3,
+
   // Fix double headers (only show v3)
   v3SegmentedProgressBar: true,
 
@@ -160,7 +162,6 @@ const formConfig = {
           path: 'veteran-information',
           uiSchema: veteranInfo.uiSchema,
           schema: veteranInfo.schema,
-          scrollAndFocusTarget: focusH3,
           initialData: maximalData.data, // FOR NON-AUTH TESTING ONLY
         },
 
@@ -230,7 +231,6 @@ const formConfig = {
           uiSchema: contestableIssues.uiSchema,
           schema: contestableIssues.schema,
           appStateSelector,
-          scrollAndFocusTarget: focusH3,
           onContinue: focusOnAlert,
         },
         addIssue: {
@@ -242,14 +242,12 @@ const formConfig = {
           uiSchema: {},
           schema: blankSchema,
           returnUrl: `/${CONTESTABLE_ISSUES_PATH}`,
-          scrollAndFocusTarget: focusH3,
         },
         issueSummary: {
           title: 'Issue summary',
           path: 'issue-summary',
           uiSchema: issueSummary.uiSchema,
           schema: issueSummary.schema,
-          scrollAndFocusTarget: focusH3,
         },
         optIn: {
           title: 'Opt in',
@@ -257,7 +255,6 @@ const formConfig = {
           depends: mayHaveLegacyAppeals,
           uiSchema: optIn.uiSchema,
           schema: optIn.schema,
-          scrollAndFocusTarget: focusH3,
         },
         optionForMst: {
           title: 'Option for claims related to MST',
@@ -330,7 +327,6 @@ const formConfig = {
           CustomPageReview: null,
           uiSchema: evidencePrivateRecordsAuthorization.uiSchema,
           schema: evidencePrivateRecordsAuthorization.schema,
-          scrollAndFocusTarget: focusH3,
         },
         evidencePrivateRecords: {
           title: 'Non-VA medical records',
@@ -350,7 +346,6 @@ const formConfig = {
           CustomPageReview: null,
           uiSchema: blankUiSchema,
           schema: blankSchema,
-          scrollAndFocusTarget: focusH3,
         },
         evidenceWillUpload: {
           title: 'Upload new and relevant evidence',
@@ -365,7 +360,6 @@ const formConfig = {
           depends: hasOtherEvidence,
           uiSchema: evidenceUpload.uiSchema,
           schema: evidenceUpload.schema,
-          scrollAndFocusTarget: focusH3,
         },
         evidenceSummary: {
           title: 'Summary of evidence',
@@ -374,7 +368,6 @@ const formConfig = {
           CustomPageReview: EvidenceSummaryReview,
           uiSchema: evidenceSummary.uiSchema,
           schema: evidenceSummary.schema,
-          scrollAndFocusTarget: focusH3,
         },
       },
     },
