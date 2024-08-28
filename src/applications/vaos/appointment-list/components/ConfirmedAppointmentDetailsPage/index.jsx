906aef996e5e9b40bab49c54e2c9f85ab4ec3175
John Woolschlager <wullaski@gmail.com>
2024-08-28 13:44:04 -0400

    Add scroll and focus call back to original useEffect

--- src/applications/vaos/appointment-list/components/ConfirmedAppointmentDetailsPage/index.jsx
+++ src/applications/vaos/appointment-list/components/ConfirmedAppointmentDetailsPage/index.jsx
@@ -72,6 +72,7 @@ export default function ConfirmedAppointmentDetailsPage() {
   useEffect(
     () => {
       dispatch(fetchConfirmedAppointmentDetails(id, appointmentTypePrefix));
+      scrollAndFocus();
     },
     [id, dispatch, appointmentTypePrefix],
   );
