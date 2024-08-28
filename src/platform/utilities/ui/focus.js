158d43181a7e6fe3cb37e23df772ff08e815354c
Robin Garrison <robin.garrison@adhocteam.us>
2024-08-28 16:07:49 -0500

    Skip waitForRenderThenFocus timer in Cypress

--- src/platform/utilities/ui/focus.js
+++ src/platform/utilities/ui/focus.js
@@ -1,5 +1,7 @@
 import { isWebComponent, querySelectorWithShadowRoot } from './webComponents';
 
+import environment from '../environment';
+
 // .nav-header > h2 contains "Step {index} of {total}: {page title}"
 export const defaultFocusSelector =
   '.nav-header > h2, va-segmented-progress-bar[heading-text][header-level="2"]';
@@ -55,56 +57,6 @@ export function focusElement(selectorOrElement, options, root) {
 }
 
 /**
- * Web components may not have their shadow DOM rendered right away, so we need
- * to wait & check before setting focus on the selector; if not found after max
- * iterations, then fall back to the default selector (step _ of _ h2)
- * Discussion: https://dsva.slack.com/archives/CBU0KDSB1/p1676479946812439
- * @param {String} selector - focus target selector
- * @param {Element} root - starting element of the querySelector
- * @param {Number} timeInterval - time in milliseconds to delay
- * @param {String} internalSelector - selector pointing to an element inside the
- *  component we're waiting for (could be an element in shadow DOM)
- * @example waitForRenderThenFocus('h3', document.querySelector('va-radio').shadowRoot);
- */
-const isCypressRunning = typeof Cypress !== 'undefined' && Cypress.env('CI');
-const defaultTime = isCypressRunning ? 0 : 250;
-const defaultMax = isCypressRunning ? 1 : 6;
-export function waitForRenderThenFocus(
-  selector,
-  root = document,
-  timeInterval = defaultTime,
-  // added because we first need to wait for a component to be rendered, then we
-  // need to target an element inside the component (in regular or in a web
-  // component's shadow DOM)
-  internalSelector,
-) {
-  const maxIterations = defaultMax; // 6 iterations * 250 ms = 1.5 seconds
-  let count = 0;
-
-  let interval = setInterval(() => {
-    const el = (root || document).querySelector(selector);
-    if (el) {
-      clearInterval(interval);
-      interval = null;
-      if (internalSelector) {
-        focusElement(internalSelector, {}, el);
-      } else {
-        focusElement(el);
-      }
-    } else if (interval && count >= maxIterations) {
-      clearInterval(interval);
-      interval = null;
-
-      // Don't set default focus if something is already focused
-      if (document.activeElement === document.body) {
-        focusElement(defaultFocusSelector); // fallback to breadcrumbs
-      }
-    }
-    count += 1;
-  }, timeInterval);
-}
-
-/**
  * Focus on first found element within the list; we're ignoreing DOM order, i.e.
  * using focusElement('h3, h2') will always focus on the h2 (higher on the page)
  * @param {String|Array} selectors - selectors in the desired order; if the
@@ -128,5 +80,62 @@ export function focusByOrder(selectors, root) {
       }
       return false;
     });
+  }
+}
+
+/**
+ * Web components may not have their shadow DOM rendered right away, so we need
+ * to wait & check before setting focus on the selector; if not found after max
+ * iterations, then fall back to the default selector (step _ of _ h2)
+ * Discussion: https://dsva.slack.com/archives/CBU0KDSB1/p1676479946812439
+ * @param {String} selector - focus target selector
+ * @param {Element} root - starting element of the querySelector
+ * @param {Number} timeInterval - time in milliseconds to delay
+ * @param {String} internalSelector - selector pointing to an element inside the
+ *  component we're waiting for (could be an element in shadow DOM)
+ * @example waitForRenderThenFocus('h3', document.querySelector('va-radio').shadowRoot);
+ */
+const isCypressRunning =
+  typeof Cypress !== 'undefined' &&
+  (Cypress.env('CI') || environment.isLocalhost());
+const defaultTime = isCypressRunning ? 0 : 250;
+const defaultMax = isCypressRunning ? 1 : 6;
+
+export function waitForRenderThenFocus(
+  selector,
+  root = document,
+  timeInterval = defaultTime,
+  // added because we first need to wait for a component to be rendered, then we
+  // need to target an element inside the component (in regular or in a web
+  // component's shadow DOM)
+  internalSelector,
+) {
+  const maxIterations = defaultMax; // 6 iterations * 250 ms = 1.5 seconds
+  let count = 0;
+
+  if (!timeInterval) {
+    focusByOrder([selector, defaultFocusSelector]);
+  } else {
+    let interval = setInterval(() => {
+      const el = (root || document).querySelector(selector);
+      if (el) {
+        clearInterval(interval);
+        interval = null;
+        if (internalSelector) {
+          focusElement(internalSelector, {}, el);
+        } else {
+          focusElement(el);
+        }
+      } else if (interval && count >= maxIterations) {
+        clearInterval(interval);
+        interval = null;
+
+        // Don't set default focus if something is already focused
+        if (document.activeElement === document.body) {
+          focusElement(defaultFocusSelector); // fallback to breadcrumbs
+        }
+      }
+      count += 1;
+    }, timeInterval);
   }
 }
