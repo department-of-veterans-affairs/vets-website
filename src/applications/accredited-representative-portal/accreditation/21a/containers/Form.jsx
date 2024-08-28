08f0826ab36b85d3f4ad9da2f9ac9863ca304f4d
Oren Mittman <nihil2501@gmail.com>
2024-08-28 14:37:10 -0400

    [ARF-87799] ARP 21a: Rename 21a `App` container to `Form`.

--- src/applications/accredited-representative-portal/accreditation/21a/containers/Form.jsx
+++ src/applications/accredited-representative-portal/accreditation/21a/containers/Form.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-
 import formConfig from '../config/form';
 
-const App = ({ location, children }) => {
+function Form({ location, children }) {
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
     </RoutedSavableApp>
   );
-};
+}
 
-App.propTypes = {
+Form.propTypes = {
   children: PropTypes.node.isRequired,
   location: PropTypes.object.isRequired,
 };
 
-export default App;
+export default Form;
