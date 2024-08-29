de48cbd8c6e7419bfc20a3ef68b362f533dae329
Brad Bergeron <bergeron.bradley@gmail.com>
2024-08-29 10:20:41 -0400

    Adjusts hideIf Logic for Parent guardian signature field

--- src/applications/toe/helpers.jsx
+++ src/applications/toe/helpers.jsx
@@ -1,6 +1,5 @@
 import { cloneDeep } from 'lodash';
 
-import { isValidCurrentOrPastDate } from 'platform/forms-system/src/js/utilities/validations';
 import {
   formFields,
   SPONSOR_NOT_LISTED_LABEL,
@@ -78,32 +77,6 @@ export const formatReadableDate = rawDate => {
   }
 
   return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
-};
-
-export const hideUnder18Field = (formData, fieldName) => {
-  if (!formData || !formData[fieldName]) {
-    return true;
-  }
-
-  const dateParts = formData && formData[fieldName].split('-');
-
-  if (!dateParts || dateParts.length !== 3) {
-    return true;
-  }
-  const birthday = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
-  const today18YearsAgo = new Date(
-    new Date(new Date().setFullYear(new Date().getFullYear() - 18)).setHours(
-      0,
-      0,
-      0,
-      0,
-    ),
-  );
-
-  return (
-    !isValidCurrentOrPastDate(dateParts[2], dateParts[1], dateParts[0]) ||
-    birthday.getTime() <= today18YearsAgo.getTime()
-  );
 };
 
 export const addWhitespaceOnlyError = (field, errors, errorMessage) => {
