d0e5eac635f7fbed95ae9f60c05edac517ae4689
Jacob Penner <jacob.penner@adhocteam.us>
2024-08-30 10:50:12 -0400

    add CustomReviewTopContent unit tests

--- src/applications/simple-forms/form-upload/tests/unit/components/EditLink.unit.spec.jsx
+++ src/applications/simple-forms/form-upload/tests/unit/components/EditLink.unit.spec.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
 import { $ } from 'platform/forms-system/src/js/utilities/ui';
-
 import EditLink from '../../../components/EditLink';
 
 describe('EditLink', () => {
