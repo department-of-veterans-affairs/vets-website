bc5116537a9691b0e4060e71d422b827006fb8b0
Eli Selkin <eli@eliselkin.com>
2024-08-30 12:49:44 -0700

    rename fn

--- src/platform/site-wide/drupal-static-data/source-files/va-health-services/connect/index.js
+++ src/platform/site-wide/drupal-static-data/source-files/va-health-services/connect/index.js
@@ -1,7 +1,7 @@
 import { connectDrupalStaticDataFile } from '../../../connect';
 import { preProcessHealthServicesData } from './preProcess';
 
-export const connectDrupalStaticDataFileVamcEhr = dispatch => {
+export const connectDrupalStaticDataFileVaHealthServices = dispatch => {
   connectDrupalStaticDataFile(dispatch, {
     fileName: 'va-healthcare-services.json',
     preProcess: preProcessHealthServicesData,
