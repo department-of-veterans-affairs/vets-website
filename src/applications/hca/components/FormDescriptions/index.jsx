c9f5293dc9af3a6759e4dfe5307f400b4a851f16
Matt Long <longmd@me.com>
2024-09-03 11:27:49 -0400

    Update health insurance introduction in the Health Care Application

--- src/applications/hca/components/FormDescriptions/index.jsx
+++ src/applications/hca/components/FormDescriptions/index.jsx
@@ -554,49 +554,6 @@ export const GroupCodeDescription = (
   </div>
 );
 
-export const HealthInsuranceDescription = (
-  <p className="vads-u-margin-bottom--3">
-    Health insurance includes any coverage that you get through a spouse or
-    significant other. Health insurance also includes Medicare, private
-    insurance, or insurance from your employer.
-  </p>
-);
-
-export const HealthInsuranceCoverageDescription = (
-  <va-additional-info
-    trigger="Why we ask this information"
-    class="vads-u-margin-top--2 vads-u-margin-bottom--3"
-    uswds
-  >
-    <div>
-      <p className="vads-u-margin-top--0">
-        We ask for this information for billing purposes only. Your health
-        insurance coverage doesn’t affect the VA health care benefits you can
-        get.
-      </p>
-
-      <p>
-        Giving us your health insurance information helps you for these reasons:
-      </p>
-
-      <ul className="vads-u-margin-bottom--0">
-        <li>
-          We must bill your private health insurance provider for any care,
-          supplies, or medicines we provide to treat your non-service-connected
-          conditions. If you have a VA copayment, we may be able to use the
-          payments from your provider to cover some or all of your copayment.
-        </li>
-        <li>
-          Your private insurance provider may apply your VA health care charges
-          toward your annual deductible. Your annual deductible is the amount of
-          money you pay toward your care each year before your insurance starts
-          to pay for care.
-        </li>
-      </ul>
-    </div>
-  </va-additional-info>
-);
-
 export const MedicaidDescription = (
   <>
     <p className="vads-u-margin-top--0">
