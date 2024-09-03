3f7247f5a70fcbc474053d8e2f970b355deb8a30
Michael Ghisilieri <michael.ghisilieri@va.gov>
2024-09-03 13:18:05 -0700

    Added link for 'Work study site supervisor guidance'

--- src/applications/sco/tests/accessibility.cypress.spec.js
+++ src/applications/sco/tests/accessibility.cypress.spec.js
@@ -98,7 +98,7 @@ describe('Accessibility', () => {
     cy.focused().should('contain.text', '85/15');
     cy.repeatKey('Tab', 10);
     cy.focused().should('contain.text', 'About GI Bill benefits');
-    cy.repeatKey('Tab', 5);
+    cy.repeatKey('Tab', 6);
     // Should be focused on right panel now
     cy.focused().should('contain.text', 'Access Enrollment Manager');
     cy.realPress('Tab');
