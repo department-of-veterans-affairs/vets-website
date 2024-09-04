57fefa076fe902c2c2641027ea0cf6e65936c1dd
Aaron Kriegler <aaron.kriegler@va.gov>
2024-09-04 12:43:38 -0400

    Add unit tests for redux actions

---
+++ src/applications/benefit-eligibility-questionnaire/tests/unit/mocks/mockFormData.js
@@ -0,0 +1,14 @@
+export const mockFormData = {
+  giBillStatus: 'No',
+  disabilityRating: 'No',
+  'view:disabilityEligibility': {},
+  characterOfDischarge: 'honorable',
+  separation: 'upTo6mo',
+  militaryServiceCompleted: 'No',
+  militaryServiceCurrentlyServing: 'No',
+  militaryServiceTotalTimeServed: 'More than 3 years',
+  checkboxGroupGoals: {
+    setACareerPath: true,
+  },
+  privacyAgreementAccepted: true,
+};
