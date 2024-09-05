a23bc2116b06e171fde919d6f492e3de034efc6d
Darius Dzien <dariusz.dzien@gsa.gov>
2024-09-05 11:33:33 -0500

    Quick cleanup

--- src/applications/accreditation/21a/config/submit-transformer.js
+++ src/applications/accreditation/21a/config/submit-transformer.js
@@ -30,140 +30,156 @@ const transformForSubmit = (formConfig, form) => {
    } = form.data;
 
    const setAccreditationTypeId = () => {
-    transformedData['accreditationTypeId'] = ACCREDITATION_TYPE_ID;
+    transformedData.accreditationTypeId = ACCREDITATION_TYPE_ID;
    };
 
   const setName = () => {
-    transformedData['firstName'] = fullName.first;
-    transformedData['lastName'] = fullName.last;
+    transformedData.firstName = fullName.first;
+    transformedData.lastName = fullName.last;
     
-    if(fullName?.middle) transformedData['middleName'] = fullName.middle;
-    if(fullName?.suffix) transformedData['suffix'] = fullName.suffix;
+    if(fullName?.middle) transformedData.middleName = fullName.middle;
+    if(fullName?.suffix) transformedData.suffix = fullName.suffix;
   };
 
   const setBirth = () => {
-    transformedData['birthdate'] = form.data.dateOfBirth;
-    transformedData['birthCity'] = placeOfBirth.city;
-    transformedData['birthState'] = placeOfBirth.state;
-    transformedData['birthCountry'] = placeOfBirth.country;
+    transformedData.birthdate = form.data.dateOfBirth;
+    transformedData.birthCity = placeOfBirth.city;
+    transformedData.birthState = placeOfBirth.state;
+    transformedData.birthCountry = placeOfBirth.country;
   };
 
   const setHomeAddress = () => {
-    transformedData['homeAddress'] = {};
-    transformedData['homeAddress'].addressType = homeAddress.isMilitary;
-    transformedData['homeAddress'].line1 = homeAddress.street;
-    if(homeAddress?.street2) transformedData['homeAddress'].line2 = homeAddress.street2;
-    transformedData['homeAddress'].city = homeAddress.city;
-    transformedData['homeAddress'].postalCode = homeAddress.postalCode;
-    transformedData['homeAddress'].country = homeAddress.country;
+    transformedData.homeAddress = {
+      addressType: homeAddress.isMilitary,
+      line1: homeAddress.street,
+      city: homeAddress.city,
+      postalCode: homeAddress.postalCode,
+      country: homeAddress.country,
+    };
+
+    if(homeAddress?.street2) transformedData.homeAddress.line2 = homeAddress.street2;
   }
 
   const setContactInfo = () => {
-    transformedData['homePhone'] = form.data.phone;
-    transformedData['phoneType'] = {};
-    transformedData['phoneType'].name = form.data.typeOfPhone;
-    transformedData['homeEmail'] = form.data.email;
+    transformedData.homePhone = form.data.phone;
+    transformedData.phoneType = {
+      name: form.data.typeOfPhone,
+    };
+    transformedData.homeEmail = form.data.email;
   };
 
   const setEmployment = () => {
-    transformedData['employmentStatus'] = form.employmentStatus;
-    transformedData['employmentStatusExplanation'] = form.describeEmployment;
-    transformedData['businessAddress'] = {};
-    transformedData['businessAddress'].addressType = workAddress.isMilitary;
-    transformedData['businessAddress'].line1 = workAddress.street;
-    transformedData['businessAddress'].line2 = workAddress.street2;
-    transformedData['businessAddress'].city = workAddress.city;
-    transformedData['businessAddress'].state = workAddress.state;
-    transformedData['businessAddress'].postalCode = workAddress.postalCode;
-    transformedData['businessAddress'].country = workAddress.country;
+    transformedData.employmentStatus = form.employmentStatus;
+    transformedData.employmentStatusExplanation = form.describeEmployment;
+    transformedData.businessAddress = {
+      addressType: workAddress.isMilitary,
+      line1: workAddress.street,
+      city: workAddress.city,
+      state: workAddress.state,
+      postalCode: workAddress.postalCode,
+      country: workAddress.country,
+    };
+
+    if(workAddress.street2) transformedData.businessAddress.line2 = workAddress.street2;
+
   };
 
   const setMilitaryService = () => {
-    transformedData['militaryServices'] = [];
+    transformedData.militaryServices = [];
 
     for (let i = 0; i < militaryServiceExperiences.length; i += 1) {
-      let service = {};
+      let service = {
+        dischargeTypeExplanation: militaryServiceExperiences[i].explanationOfDischarge,
+        entryDate: militaryServiceExperiences[i].dateRange,
+        dischargeDate: militaryServiceExperiences[i].dateRange,
+        serviceBranch: {
+          name: militaryServiceExperiences[i].branch,
+        },
+        dischargeType: {
+          name: militaryServiceExperiences[i].characterOfDischarge,
+        }
+      };
 
-      service['serviceBranch'].name = militaryServiceExperiences[i].branch;
-      service['dischargeType'].name = militaryServiceExperiences[i].characterOfDischarge;
-      service['dischargeTypeExplanation'] = militaryServiceExperiences[i].explanationOfDischarge;
-      service['entryDate'] = militaryServiceExperiences[i].dateRange;
-      service['dischargeDate'] = militaryServiceExperiences[i].dateRange;
-
-      transformedData['militaryServices'].push(service);
+      transformedData.militaryServices.push(service);
     }
   };
 
   const setEmploymentHistory = () => {
-    transformedData['employment'] = [];
+    transformedData.employment = [];
 
     for (let i = 0; i < employers.length; i += 1) {
-      let employer = {};
+      let employer = {
+        phoneNumber: employers[i].phone,
+        phoneExtension: employers[i].extension,
+        positionTitle: employers[i].positionTitle,
+        supervisorName: employers[i].supervisorName,
+        employerAddress: {
+          addressType: employers[i].address.isMilitary,
+          line1: employers[i].address.street,
+          city: employers[i].address.city,
+          state: employers[i].address.state,
+          postalCode: employers[i].address.postalCode,
+          country: employers[i].address.country,
+        }
+      };
 
-      employer['employerAddress'].addressType = employers[i].address.isMilitary;
-      employer['employerAddress'].line1 = employers[i].address.street;
-      employer['employerAddress'].line2 = employers[i].address.street2;
-      employer['employerAddress'].city = employers[i].address.city;
-      employer['employerAddress'].state = employers[i].address.state;
-      employer['employerAddress'].postalCode = employers[i].address.postalCode;
-      employer['employerAddress'].country = employers[i].address.country;
-      employer['phoneNumber'] = employers[i].phone;
-      employer['phoneExtension'] = employers[i].extension;
-      employer['positionTitle'] = employers[i].positionTitle;
-      employer['startDate'] = employers[i].dateRange;
-      employer['supervisorName'] = employers[i].supervisorName;
+      if(employers[i].address?.street2) employer.employerAddress.line2 = employers[i].address.street2;
 
-      transformedData['employment'].push(employer);
+      transformedData.employment.push(employer);
     }
   };
 
   const setEducation = () => {
-    transformedData['education'] = [];
+    transformedData.education = [];
 
     for (let i = 0; i < educationalInstitutions.length; i += 1) {
-      let institution = {};
+      let institution = {
+        wasDegreeReceived: educationalInstitutions[i].degreeReceived,
+        major: educationalInstitutions[i].major,
+        degreeType: {
+          name: educationalInstitutions[i].degree
+        },
+        institutionAddress: {
+          addressType: educationalInstitutions[i].address.isMilitary,
+          line1: educationalInstitutions[i].address.street,
+          city: educationalInstitutions[i].address.city,
+          state: educationalInstitutions[i].address.state,
+          postalCode: educationalInstitutions[i].address.postalCode,
+          country: educationalInstitutions[i].address.country
+        }
+      };
 
-      institution['institutionAddress'].addressType = educationalInstitutions[i].address.isMilitary;
-      institution['institutionAddress'].line1 = educationalInstitutions[i].address.street;
-      institution['institutionAddress'].line2 = educationalInstitutions[i].address.street2;
-      institution['institutionAddress'].city = educationalInstitutions[i].address.city;
-      institution['institutionAddress'].state = educationalInstitutions[i].address.state;
-      institution['institutionAddress'].postalCode = educationalInstitutions[i].address.postalCode;
-      institution['institutionAddress'].country = educationalInstitutions[i].address.country;
-      institution['wasDegreeReceived'] = educationalInstitutions[i].degreeReceived;
-      institution['degreeType'].name = educationalInstitutions[i].degree;
-      institution['major'] = educationalInstitutions[i].major;
-      institution['startDate'] = educationalInstitutions[i].dateRange;
+      if(educationalInstitutions[i].address?.street2) institution.institutionAddress.line2 = educationalInstitutions[i].address.street2;
 
-      transformedData['education'].push(institution);
+      transformedData.education.push(institution);
     }
   };
 
   const setJurisdictions = () => {
-    transformedData['jurisdictions'] = [];
+    transformedData.jurisdictions = [];
 
     for (let i = 0; i < jurisdictions.length; i += 1) {
-      let jurisdiction = {};
+      let jurisdiction = {
+        name: jurisdictions[i].jurisdiction,
+        admissionDate: jurisdictions[i].admissionDate,
+        membershipRegistrationNumber: jurisdictions[i].membershipOrRegistrationNumber,
+      };
 
-      jurisdiction['name'] = jurisdictions[i].jurisdiction;
-      jurisdiction['admissionDate'] = jurisdictions[i].admissionDate;
-      jurisdiction['membershipRegistrationNumber'] = jurisdictions[i].membershipOrRegistrationNumber;
-
-      transformedData['jurisdictions'].push(jurisdiction);
+      transformedData.jurisdictions.push(jurisdiction);
     }
   };
 
   const setAgencies = () => {
-    transformedData['agencies'] = [];
+    transformedData.agencies = [];
 
     for (let i = 0; i < agenciesOrCourts.length; i += 1) {
-      let agency = {};
+      let agency = {
+        name: agenciesOrCourts[i].name,
+        admissionDate: agenciesOrCourts[i].admissionDate,
+        membershipRegistrationNumber: agenciesOrCourts[i].membershipOrRegistrationNumber,
+      };
 
-      agency['name'] = agenciesOrCourts[i].name;
-      agency['admissionDate'] = agenciesOrCourts[i].admissionDate;
-      agency['membershipRegistrationNumber'] = agenciesOrCourts[i].membershipOrRegistrationNumber;
-
-      transformedData['agencies'].push(agency);
+      transformedData.agencies.push(agency);
     }
   };
 
