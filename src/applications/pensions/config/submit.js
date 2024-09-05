13cffb36809b5b41ca32bd9ad63210be98738c92
Scott <51915366+scottsdevelopment@users.noreply.github.com>
2024-09-05 15:19:45 -0400

    Pension 88332 Use date-fn library in the pensions application (#31599)

--- src/applications/pensions/config/submit.js
+++ src/applications/pensions/config/submit.js
@@ -1,7 +1,7 @@
-import moment from 'moment';
 import environment from 'platform/utilities/environment';
 import { apiRequest } from 'platform/utilities/api';
 import { transformForSubmit } from 'platform/forms-system/src/js/helpers';
+import { format } from 'date-fns-tz';
 
 const usaPhoneKeys = ['phone', 'mobilePhone', 'dayPhone', 'nightPhone'];
 
@@ -32,7 +32,7 @@ export function transform(formConfig, form) {
       form: formData,
     },
     // can’t use toISOString because we need the offset
-    localTime: moment().format('Y-MM-DD[T]kk:mm:ssZZ'),
+    localTime: format(new Date(), "yyyy-MM-dd'T'HH:mm:ssXXX"),
   });
 }
 
