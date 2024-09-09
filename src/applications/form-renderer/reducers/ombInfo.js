b8438f9d1ccd1fe2e188a72456d55c6ac0cd310d
Derek Houck <derek.houck@agile6.com>
2024-09-09 11:31:33 -0500

    Remove ombInfo action

--- src/applications/form-renderer/reducers/ombInfo.js
+++ src/applications/form-renderer/reducers/ombInfo.js
@@ -1,15 +1,9 @@
-import { OMB_INFO_LOADED } from '../actions/ombInfo';
-
 export const initialState = {
   expDate: null,
   ombNumber: null,
   resBurden: null,
 };
 
-export default (state = initialState, action) => {
-  if (action?.type === OMB_INFO_LOADED) {
-    return { ...state, ...action.payload.ombInfo };
-  }
-
+export default (state = initialState) => {
   return state;
 };
