f0d7d42cdaee6e7c9d6bd428a50fbe39a3ea6c63
Derek Houck <derek.houck@agile6.com>
2024-09-09 09:23:13 -0500

    Switch ombInfo to a prop on IntroductionPage

--- src/applications/form-renderer/tests/unit/containers/IntroductionPage.unit.spec.js
+++ src/applications/form-renderer/tests/unit/containers/IntroductionPage.unit.spec.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { expect } from 'chai';
 import { render } from '@testing-library/react';
-import configureStore from 'redux-mock-store';
 import sinon from 'sinon';
-import thunk from 'redux-thunk';
 
 import * as SaveInProgressIntro from 'platform/forms/save-in-progress/SaveInProgressIntro';
-import { Provider } from 'react-redux';
 import IntroductionPage from '../../../containers/IntroductionPage';
 import { formConfig1 } from '../../../_config/formConfig';
 
@@ -16,24 +13,20 @@ describe('<IntroductionPage /> component', () => {
   beforeEach(() => sinon.stub(SaveInProgressIntro, 'default').value('div'));
 
   it('renders OMB info from props', () => {
-    const mockStore = configureStore([thunk]);
     const ombInfo = {
       expDate: '8/29/2025',
       ombNumber: '1212-1212',
       resBurden: 30,
     };
 
-    const store = mockStore({ ombInfo });
-
     const screen = render(
-      <Provider store={store}>
-        <IntroductionPage
-          route={{
-            formConfig: formConfig1,
-            pageList: [],
-          }}
-        />
-      </Provider>,
+      <IntroductionPage
+        ombInfo={ombInfo}
+        route={{
+          formConfig: formConfig1,
+          pageList: [],
+        }}
+      />,
     );
 
     const vaOmbInfo = screen.getByTestId('va-omb-info');
