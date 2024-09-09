f96480fb6306c9ba26ade9363df680fa79964ca2
Chad Chotikasatien <chad.chotikasatien@va.gov.com>
2024-09-09 10:00:58 -0700

    #vebt-470 #comment Removed constants for 22-1990N

--- src/applications/edu-benefits/components/createEducationApplicationStatus.jsx
+++ src/applications/edu-benefits/components/createEducationApplicationStatus.jsx
@@ -10,7 +10,6 @@ const eduForms = new Set([
   VA_FORM_IDS.FORM_22_5490,
   VA_FORM_IDS.FORM_22_5495,
   VA_FORM_IDS.FORM_22_1990E,
-  VA_FORM_IDS.FORM_22_1990N,
 ]);
 
 export default function createEducationApplicationStatus(store, widgetType) {
