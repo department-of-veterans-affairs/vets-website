78ae8deb253f07217cea1416dd9f69b5861285ab
Derek Houck <derek.houck@agile6.com>
2024-09-09 11:29:24 -0500

    Remove ombInfoLoaded action

--- src/applications/form-renderer/actions/ombInfo.js
+++ src/applications/form-renderer/actions/ombInfo.js
@@ -1,6 +1,1 @@
 export const OMB_INFO_LOADED = 'FORM_RENDERER/OMB_INFO_LOADED';
-
-export const ombInfoLoaded = ombInfo => ({
-  type: OMB_INFO_LOADED,
-  payload: { ombInfo },
-});
--- src/applications/form-renderer/tests/unit/reducers/ombInfo.unit.spec.js
+++ src/applications/form-renderer/tests/unit/reducers/ombInfo.unit.spec.js
@@ -2,21 +2,9 @@ import { expect } from 'chai';
 import reducer, {
   initialState,
 } from 'applications/form-renderer/reducers/ombInfo';
-import { ombInfoLoaded } from 'applications/form-renderer/actions/ombInfo';
 
 describe('ombInfo reducer', () => {
   it('should return the initial state', () => {
     expect(reducer()).to.deep.equal(initialState);
-  });
-
-  it('should handle OMB_INFO_LOADED', () => {
-    const ombInfo = {
-      expDate: '8/29/2025',
-      ombNumber: '1212-1212',
-      resBurden: 30,
-    };
-    const action = ombInfoLoaded(ombInfo);
-
-    expect(reducer(initialState, action)).to.deep.equal(ombInfo);
   });
 });
