d2021913dfee92cad71d3c593c1f146571ebea6e
Ryan Shaw <ryan.shaw@adhocteam.us>
2024-09-09 08:03:39 -0700

    Adds patient relationship to vaos service
    Signed-off-by: Ryan Shaw <ryan.shaw@adhocteam.us>

--- src/applications/vaos/services/vaos/index.js
+++ src/applications/vaos/services/vaos/index.js
@@ -10,6 +10,7 @@ import {
 const acheronHeader = {
   headers: { ACHERON_REQUESTS: 'true' },
 };
+
 export function postAppointment(appointment) {
   return apiRequestWithUrl('/vaos/v2/appointments', {
     method: 'POST',
@@ -69,6 +70,18 @@ export function getClinics({ locationId, clinicIds, typeOfCareId }) {
       url,
       // eslint-disable-next-line camelcase
       { clinic_ids: clinicIds, clinical_service: typeOfCareId },
+      { removeNull: true },
+    ),
+  ).then(parseApiList);
+}
+
+export function getPatientRelationships({ facilityId, typeOfCareId }) {
+  const url = '/vaos/v2/relationships';
+  return apiRequestWithUrl(
+    appendQuery(
+      url,
+      // eslint-disable-next-line camelcase
+      { facility_id: facilityId, clinical_service_id: typeOfCareId },
       { removeNull: true },
     ),
   ).then(parseApiList);
