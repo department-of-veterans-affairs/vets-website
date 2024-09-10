890a1b0ba210b80a4f51968844899294ad7d8067
Jami Gibbs <jami.gibbs@adhocteam.us>
2024-09-10 10:10:54 -0500

    convert small-screen to mobile-lg breakpoint

--- src/applications/gi/components/GIBillHeaderInfo.jsx
+++ src/applications/gi/components/GIBillHeaderInfo.jsx
@@ -12,7 +12,7 @@ export const renderPTag = (automatedTest = false) => {
 export default function GIBillHeaderInfo() {
   return (
     <div className="row vads-u-padding-x--2p5 small-screen:vads-u-padding-x--0 vads-u-padding-bottom--2 small-screen:vads-u-padding-bottom--4">
-      <h1 className="vads-u-text-align--center small-screen:vads-u-text-align--left">
+      <h1 className="vads-u-text-align--center mobile-lg:vads-u-text-align--left">
         GI Bill® Comparison Tool
       </h1>
       {renderPTag()}
