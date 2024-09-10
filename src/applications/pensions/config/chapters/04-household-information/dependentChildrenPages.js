a13c8df07dfe4fd1cb4beeb1b7c100db529334f2
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2024-09-10 13:46:32 -0600

    Remove conditional

--- src/applications/pensions/config/chapters/04-household-information/dependentChildrenPages.js
+++ src/applications/pensions/config/chapters/04-household-information/dependentChildrenPages.js
@@ -30,7 +30,6 @@ import {
   SchoolAttendanceAlert,
 } from '../../../components/FormAlerts';
 import { childRelationshipLabels } from '../../../labels';
-import { isBetween18And23 } from './helpers';
 import {
   DependentSeriouslyDisabledDescription,
   formatFullName,
@@ -383,11 +382,7 @@ export const dependentChildrenPages = arrayBuilderPages(
     dependentChildAttendingSchoolPage: pageBuilder.itemPage({
       title: 'Dependent children',
       path: 'household/dependents/:index/school',
-      depends: (formData, index) =>
-        showMultiplePageResponse() &&
-        isBetween18And23(
-          get(['dependents', index, 'childDateOfBirth'], formData),
-        ),
+      depends: () => showMultiplePageResponse(),
       uiSchema: attendingSchoolPage.uiSchema,
       schema: attendingSchoolPage.schema,
     }),
