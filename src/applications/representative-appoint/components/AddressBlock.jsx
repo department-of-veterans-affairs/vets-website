a24fb66daef6ac0e8dd9667330ba12a7251b09f4
Holden Hinkle <holden@holdenhinkle.com>
2024-09-10 11:17:33 -0400

    wip AddressBlock

---
+++ src/applications/representative-appoint/components/AddressBlock.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function AddressBlock({ repName, orgName, address }) {
+  return (
+    <p className="va-address-block">
+      <strong>{repName}</strong>
+      <br />
+      {orgName}
+      <br />
+      Attention: C-123 Claims
+      <br />
+      <br />
+      {address.address1}
+      <br />
+      {address.address2}
+      <br />
+      {address.city}, {address.state} {address.zip}
+      <br />
+    </p>
+  );
+}
