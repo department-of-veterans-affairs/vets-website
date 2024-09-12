49fb956c38257547028c17f18d06ec7a8953ac57
Randi Mays <randimays1024@gmail.com>
2024-09-12 14:27:03 -0500

    VACMS-18855 Add Find Forms download modal back to form detail page (#31395)
    * VACMS-18855 Add Find Forms download modal back to form detail page
    
    * adding more changes
    
    * Finishing up adding the modal
    
    * changing structure
    
    * Addressing PR feedback

--- src/applications/find-forms/tests/cypress/find-forms-results.cypress.spec.js
+++ src/applications/find-forms/tests/cypress/find-forms-results.cypress.spec.js
@@ -1,37 +1,15 @@
-/**
- * [TestRail-integrated] Spec for Find a VA Form - required-fields.
- * @testrailinfo projectId 30
- * @testrailinfo suiteId 136
- * @testrailinfo groupId 3026
- * @testrailinfo runName FF-e2e-Required
- */
 import chunk from 'lodash/chunk';
 import { FAF_SORT_OPTIONS } from '../../constants';
 import stub from '../../constants/stub.json';
-
-const SELECTORS = {
-  WIDGET: '[data-widget-type="find-va-forms"]',
-  INPUT_ROOT: 'va-search-input',
-  SEARCH_FORM: '[data-e2e-id="find-form-search-form"]',
-  SEARCH_RESULT_TITLE: '[data-e2e-id="result-title"]',
-  NEXT_PAGE: '.usa-pagination__list > li[aria-label="Next page"] > a',
-  SORT_SELECT_WIDGET: 'va-select[name="findFormsSortBySelect"]',
-};
+import { SELECTORS as s } from './helpers';
 
 describe('functionality of Find Forms', () => {
-  beforeEach(() => {
+  it('search the form and expect dom to have elements', () => {
     cy.intercept('GET', '/v0/forms?query=health', stub).as('getFindAForm');
-  });
-
-  it('search the form and expect dom to have elements - C3994', () => {
-    // navigate to find-forms and make axe check on browser
     cy.visit('/find-forms/');
+    cy.get(s.APP);
 
-    // Ensure form is present
-    cy.get(SELECTORS.SEARCH_FORM);
-
-    // Search the form
-    cy.get(SELECTORS.INPUT_ROOT)
+    cy.get(s.FINDFORM_INPUT_ROOT)
       .shadow()
       .find('input')
       .scrollIntoView()
@@ -40,7 +18,7 @@ describe('functionality of Find Forms', () => {
       .type('health', { force: true })
       .should('not.be.disabled');
 
-    cy.get(SELECTORS.INPUT_ROOT)
+    cy.get(s.FINDFORM_INPUT_ROOT)
       .shadow()
       .find('button')
       .should('exist')
@@ -48,8 +26,7 @@ describe('functionality of Find Forms', () => {
 
     cy.wait('@getFindAForm');
 
-    // Ensure at least 1 title is present
-    cy.get(`${SELECTORS.SEARCH_RESULT_TITLE}`).should('exist');
+    cy.get(`${s.SEARCH_RESULT_TITLE}`).should('exist');
 
     cy.injectAxe();
     cy.axeCheck();
@@ -69,28 +46,32 @@ describe('functionality of Find Forms', () => {
       const hasNextPage = nextPage < pages.length;
 
       if (hasNextPage) {
-        cy.get(SELECTORS.NEXT_PAGE)
+        cy.get(s.NEXT_PAGE)
           .click()
           .then(() => cy.axeCheck());
       }
     });
 
     // Ensure Sort Widget exists
-    cy.get(`${SELECTORS.SORT_SELECT_WIDGET}`);
-    cy.get(`${SELECTORS.SORT_SELECT_WIDGET}`)
+    cy.get(`${s.SORT_SELECT_WIDGET}`);
+    cy.get(`${s.SORT_SELECT_WIDGET}`)
       .shadow()
       .get(`option`)
       // Finds both the shadow DOM option and the React Fiber option, so have to multiply 'expected' by 2,
       // and plus 1 due to the DST component adding a default select option to the dropdown
       .should('have.length', FAF_SORT_OPTIONS.length * 2 + 1);
-    cy.get(`${SELECTORS.SORT_SELECT_WIDGET}`)
+    cy.get(`${s.SORT_SELECT_WIDGET}`)
       .shadow()
       .get('option')
       .should('be.selected')
       .should('contain', FAF_SORT_OPTIONS[0]);
   });
 
-  it('opens PDF modal - C12431', () => {
+  it('opens PDF modal', () => {
+    cy.intercept('GET', '/v0/forms?query=health', { data: [stub.data[0]] }).as(
+      'getFindAForm',
+    );
+
     cy.visit('/find-forms/?q=health');
     cy.get('button[data-testid^="pdf-link"]').then($links => {
       const randomIndex = Math.floor(Math.random() * $links.length);
@@ -99,9 +80,29 @@ describe('functionality of Find Forms', () => {
         .scrollIntoView()
         .click({ force: true });
     });
-    cy.get('va-modal', { timeout: 25000 })
-      .get('.usa-modal__heading')
-      .should('contain.text', 'PDF');
+
+    const modal = () => cy.get(s.MODAL, { timeout: 25000 });
+
+    modal()
+      .scrollIntoView()
+      .within(() => {
+        cy.get(s.ADOBE_LINK)
+          .should('contain.text', 'Get Acrobat Reader for free from Adobe')
+          .should('have.attr', 'href')
+          .and('include', 'https://get.adobe.com/reader/');
+
+        cy.get(s.MODAL_DOWNLOAD_LINK)
+          .should('contain.text', 'Download VA Form 10-252')
+          .should('have.attr', 'href')
+          .and(
+            'include',
+            'https://www.va.gov/vaforms/medical/pdf/10-252%20Authorization%20To%20Release%20Protected%20Health%20Information%20To%20State%20Local%20Public%20Authorities.pdf',
+          );
+
+        cy.get(s.MODAL_CLOSE_BUTTON).click();
+      });
+
+    cy.get(s.MODAL).should('not.be.visible');
 
     cy.injectAxe();
     cy.axeCheck();
