54161768de6e6f5ae4c9045559ba2404b2f0691d
fatmakhan0395 <Fatma.khan@va.gov>
2024-09-12 08:37:27 -0700

    updating style for omb info and res burden

--- src/applications/verify-your-enrollment/components/VaOmbInfo.jsx
+++ src/applications/verify-your-enrollment/components/VaOmbInfo.jsx
@@ -11,18 +11,9 @@ const VaOmbInfo = () => {
         const { shadowRoot } = shadowHost;
         shadowRoot.append(
           Object.assign(document.createElement('STYLE'), {
-            innerText: `va-button {display:none} div{font-weight: bold} div strong{font-weight: normal}
-                                                      `,
+            innerText: `va-button {display:none}`,
           }),
         );
-        if (shadowRoot) {
-          const firstDiv = shadowRoot.querySelector('div strong');
-
-          if (firstDiv) {
-            firstDiv.innerText = '1 minute';
-            observer.disconnect();
-          }
-        }
       });
       observer.observe(document, { childList: true, subtree: true });
       return () => {
@@ -32,14 +23,11 @@ const VaOmbInfo = () => {
     return undefined;
   }, []);
   return (
-    <div
-      className="omb-info-custom-class omb-info--container vads-u-margin-bottom--3"
-      style={{ paddingLeft: '0px' }}
-    >
+    <div className="omb-info-custom-class omb-info--container vads-u-margin-bottom--3">
       <va-omb-info
         exp-date="03/31/2026"
         omb-number="2900-0465"
-        res-burden={1}
+        res-burden={2}
       />
     </div>
   );
