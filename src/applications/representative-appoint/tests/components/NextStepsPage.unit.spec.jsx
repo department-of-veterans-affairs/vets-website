de5c513f657f35455dfd218f22a8dbd1350c2b6a
Holden Hinkle <holden@holdenhinkle.com>
2024-09-11 22:38:52 -0400

    write unit tests for NextStepsPage

---
+++ src/applications/representative-appoint/tests/components/NextStepsPage.unit.spec.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import { expect } from 'chai';
+
+import { $ } from 'platform/forms-system/src/js/utilities/ui';
+import NextStepsPage from '../../containers/NextStepsPage';
+
+const getData = ({
+  formData = {
+    selectedRepName: 'Steven McBob',
+    selectedOrgName: 'Best VSO',
+    address1: '123 Main Street',
+    address2: '',
+    city: 'Anytown',
+    state: 'VT',
+    zip: '05495',
+    repTypeRadio: 'Attorney',
+  },
+} = {}) => ({
+  props: {},
+  mockStore: {
+    getState: () => ({
+      form: {
+        data: formData,
+      },
+    }),
+    subscribe: () => {},
+    dispatch: () => {},
+  },
+});
+
+describe('NextStepsPage', () => {
+  it('should render with correct heading and text', () => {
+    const { props, mockStore } = getData();
+    const { container } = render(
+      <Provider store={mockStore}>
+        <NextStepsPage {...props} />
+      </Provider>,
+    );
+
+    expect($('h1', container).textContent).to.contain(
+      'Fill out your form to appoint a VA accredited representative or VSO',
+    );
+    expect($('h2', container).textContent).to.contain('Your next steps');
+    expect($('p', container).textContent).to.contain(
+      'Both you and the accredited attorney will need to sign your form.',
+    );
+  });
+
+  it('should render the AddressBlock with correct data', () => {
+    const { props, mockStore } = getData();
+    const { container } = render(
+      <Provider store={mockStore}>
+        <NextStepsPage {...props} />
+      </Provider>,
+    );
+
+    expect($('.va-address-block', container).textContent).to.contain(
+      'Steven McBob',
+    );
+    expect($('.va-address-block', container).textContent).to.contain(
+      'Best VSO',
+    );
+    expect($('.va-address-block', container).textContent).to.contain(
+      '123 Main Street',
+    );
+    expect($('.va-address-block', container).textContent).to.contain(
+      'Anytown, VT 05495',
+    );
+  });
+
+  it('should display representative type correctly', () => {
+    const { props, mockStore } = getData();
+    const { container } = render(
+      <Provider store={mockStore}>
+        <NextStepsPage {...props} />
+      </Provider>,
+    );
+
+    expect($('p', container).textContent).to.contain('attorney');
+  });
+
+  it('should render the NeedHelp component with phone numbers', () => {
+    const { props, mockStore } = getData();
+    const { container } = render(
+      <Provider store={mockStore}>
+        <NextStepsPage {...props} />
+      </Provider>,
+    );
+
+    expect($('va-telephone[contact="8006982411"]', container)).to.exist;
+    expect($('va-telephone[contact="711"]', container)).to.exist;
+  });
+});
