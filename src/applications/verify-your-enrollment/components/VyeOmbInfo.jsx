c8632943bf13600cd0c71d34ce9ff6a05463885b
fatmakhan0395 <Fatma.khan@va.gov>
2024-09-13 07:40:53 -0700

    updated description for omb info

--- src/applications/verify-your-enrollment/components/VyeOmbInfo.jsx
+++ src/applications/verify-your-enrollment/components/VyeOmbInfo.jsx
@@ -5,7 +5,7 @@ export default function VyeOmbInfo() {
     <div>
       <p className="vads-u-margin--0">
         <span className="vads-u-font-weight--bold">Respondent burden:</span> 1
-        Minute
+        minute
       </p>
       <p className="vads-u-margin--0">
         <span className="vads-u-font-weight--bold">OMB Control #:</span>{' '}
