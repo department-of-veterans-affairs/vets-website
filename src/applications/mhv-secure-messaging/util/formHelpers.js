229db70108f2bf796ec56ad162c703052ab77171
Alex Morgun <oleksii.morgun@va.gov>
2024-09-13 15:10:32 -0400

    MHV-61274 checkbox error focus state

--- src/applications/mhv-secure-messaging/util/formHelpers.js
+++ src/applications/mhv-secure-messaging/util/formHelpers.js
@@ -1,15 +1,17 @@
 import { focusElement } from '@department-of-veterans-affairs/platform-utilities/ui';
 
 export const focusOnErrorField = () => {
-  const errors = document.querySelectorAll('[error]:not([error=""])');
-  const firstError =
-    errors.length > 0 &&
-    (errors[0]?.shadowRoot?.querySelector('select, input, textarea') ||
-      errors[0]
-        ?.querySelector('va-checkbox')
-        ?.shadowRoot?.querySelector('input') ||
-      errors[0].querySelector('input'));
-  if (firstError) {
-    focusElement(firstError);
-  }
+  setTimeout(() => {
+    const errors = document.querySelectorAll('[error]:not([error=""])');
+    const firstError =
+      errors.length > 0 &&
+      (errors[0]?.shadowRoot?.querySelector('select, input, textarea') ||
+        errors[0]
+          ?.querySelector('va-checkbox')
+          ?.shadowRoot?.querySelector('input') ||
+        errors[0].querySelector('input'));
+    if (firstError) {
+      focusElement(firstError);
+    }
+  }, 300);
 };
