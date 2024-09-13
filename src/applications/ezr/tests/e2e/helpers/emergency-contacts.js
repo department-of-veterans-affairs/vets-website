de852c2a1343050d25320a36c6b0cd3b84791759
Derrick Ellerbie <derrick.ellerbie@agile6.com>
2024-09-13 11:53:37 -0700

    EZR: Refactor maximal test

--- src/applications/ezr/tests/e2e/helpers/emergency-contacts.js
+++ src/applications/ezr/tests/e2e/helpers/emergency-contacts.js
@@ -1,4 +1,9 @@
-import { goToNextPage, selectYesNoWebComponent } from '.';
+import {
+  goToNextPage,
+  fillTextWebComponent,
+  selectYesNoWebComponent,
+  selectDropdownWebComponent,
+} from '.';
 
 export const advanceToEmergencyContacts = () => {
   cy.get('[href="#start"]')
@@ -10,4 +15,24 @@ export const advanceToEmergencyContacts = () => {
 
   goToNextPage('/veteran-information/contact-information');
   cy.tabToContinueForm();
+};
+
+export const fillEmergencyContactPersonalInfo = contact => {
+  fillTextWebComponent('fullName_first', contact.fullName.first);
+  fillTextWebComponent('fullName_last', contact.fullName.last);
+  fillTextWebComponent('primaryPhone', contact.primaryPhone);
+  selectDropdownWebComponent('relationship', contact.relationship);
+  selectYesNoWebComponent('view:hasEmergencyContactAddress', true);
+  cy.injectAxeThenAxeCheck();
+  goToNextPage();
+};
+
+export const fillEmergencyContactAddress = contact => {
+  selectDropdownWebComponent('address_country', contact.address.country);
+  fillTextWebComponent('address_street', contact.address.street);
+  fillTextWebComponent('address_city', contact.address.city);
+  selectDropdownWebComponent('address_state', contact.address.state);
+  fillTextWebComponent('address_postalCode', contact.address.postalCode);
+  cy.injectAxeThenAxeCheck();
+  goToNextPage();
 };
