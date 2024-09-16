39d720ef379f605e1c913ed79ebe1017e4fdebec
John Woolschlager <wullaski@gmail.com>
2024-09-16 16:10:11 -0400

    Add simple unti test for subtitle content

---
+++ src/applications/vaos/tests/referral-appointments/components/ReferralReview.unit.spec.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import ReferralReview from '../../../referral-appointments/ReferralReview';
+import { createTestStore, renderWithStoreAndRouter } from '../../mocks/setup';
+import { referral } from '../../../referral-appointments/temp-data/referral';
+
+describe('ReferralReview component', () => {
+  const store = createTestStore();
+  it('should display the subtitle correctly given different numbers of appointments', async () => {
+    const sandbox = sinon.createSandbox();
+    sandbox.stub(referral, 'appointmentCount').value(1);
+    const screen = renderWithStoreAndRouter(<ReferralReview />, {
+      store,
+    });
+    const subtitle = await screen.findByTestId('subtitle');
+    expect(subtitle).to.contain.text('1 appointment');
+    sandbox.restore();
+  });
+  it('should display the subtitle correctly given 2 appointments', async () => {
+    const sandbox = sinon.createSandbox();
+    sandbox.stub(referral, 'appointmentCount').value(2);
+    referral.appointmentCount = 2;
+    const screen = renderWithStoreAndRouter(<ReferralReview />, {
+      store,
+    });
+    const subtitle = await screen.findByTestId('subtitle');
+    expect(subtitle).to.contain.text('2 appointments');
+    sandbox.restore();
+  });
+});
