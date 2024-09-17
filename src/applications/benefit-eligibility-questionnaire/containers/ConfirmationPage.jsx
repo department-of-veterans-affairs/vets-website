6fe4719d57a197160371b43ca673e284708ed5cd
Aaron Kriegler <aaron.kriegler@va.gov>
2024-09-17 15:37:30 -0400

    Place learn more and apply now links under time sensitive benefit

--- src/applications/benefit-eligibility-questionnaire/containers/ConfirmationPage.jsx
+++ src/applications/benefit-eligibility-questionnaire/containers/ConfirmationPage.jsx
@@ -222,14 +222,31 @@ export class ConfirmationPage extends React.Component {
                       .filter(benefit => benefit.isTimeSensitive)
                       .map(b => (
                         <li key={b.id}>
-                          <a
-                            href={b.learnMoreURL}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            {b.name}
-                          </a>
+                          <strong>{b.name}</strong>
                           <p>{b.description}</p>
+                          {b.learnMoreURL ? (
+                            <div>
+                              <a
+                                href={b.learnMoreURL}
+                                target="_blank"
+                                rel="noreferrer"
+                              >
+                                Learn more
+                              </a>
+                            </div>
+                          ) : null}
+                          ,
+                          {b.applyNowURL ? (
+                            <div>
+                              <a
+                                href={b.applyNowURL}
+                                target="_blank"
+                                rel="noreferrer"
+                              >
+                                Apply now
+                              </a>
+                            </div>
+                          ) : null}
                         </li>
                       ))}
                 </ul>
