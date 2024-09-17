f68bbcef6e62945bb461f8a2bf48d89647fefad5
Aaron Kriegler <159946297+vetskrieg@users.noreply.github.com>
2024-09-17 11:29:03 -0400

    Adding unit tests to increase code coverage (#31962)

--- src/applications/benefit-eligibility-questionnaire/tests/unit/config/form.unit.spec.js
+++ src/applications/benefit-eligibility-questionnaire/tests/unit/config/form.unit.spec.js
@@ -59,6 +59,30 @@ describe('Questionnaire Form - Title Function', () => {
   });
 });
 
+describe('Questionnaire Form - SubTitle Function', () => {
+  describe('title function', () => {
+    it('should return blank when on the confirmation page', () => {
+      const currentLocation = { pathname: '/confirmation' };
+      const title = formConfig.subTitle({ currentLocation });
+      expect(title).to.equal('');
+    });
+
+    it('should return blank when on the review page', () => {
+      const currentLocation = { pathname: '/review-and-submit' };
+      const title = formConfig.subTitle({ currentLocation });
+      expect(title).to.equal('');
+    });
+
+    it('should return "Please answer the questions to help us recommend helpful resources and benefits." by default', () => {
+      const currentLocation = { pathname: '/some-other-page' };
+      const title = formConfig.subTitle({ currentLocation });
+      expect(title).to.equal(
+        'Please answer the questions to help us recommend helpful resources and benefits.',
+      );
+    });
+  });
+});
+
 describe('Questionnaire Form - Chapter 1: Goals', () => {
   const { chapters } = formConfig;
   const { chapter1 } = chapters;
