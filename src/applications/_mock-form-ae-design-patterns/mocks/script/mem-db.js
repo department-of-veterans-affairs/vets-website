d1b6adbcd450d745c5177e1af1055f205a5a6f4b
Adam Whitlock <adam.whitlock@adhocteam.us>
2024-09-23 22:09:04 -0600

    clean up mem db functionality

--- src/applications/_mock-form-ae-design-patterns/mocks/script/mem-db.js
+++ src/applications/_mock-form-ae-design-patterns/mocks/script/mem-db.js
@@ -3,66 +3,64 @@ const _ = require('lodash');
 
 const user = require('../endpoints/user');
 
-// sanitize user input
-const sanitize = dirty => {
-  // if input is a number, return it as is
-  if (!Number.isNaN(dirty)) {
-    return dirty;
-  }
-
-  if (typeof dirty !== 'string') {
-    throw new Error('Input must be a string');
-  }
-
-  // remove all html tags
-  const sanitized = dirty.replace(/<[^>]*>/g, '');
-
-  return _.escape(sanitized).trim();
-};
-
+// in memory db
 const memDb = {
   user: user.loa3User72,
 };
 
-const potentialAddressUpdateKeys = [
-  'addressLine1',
-  'addressLine2',
-  'addressLine3',
-  'city',
-  'stateCode',
-  'zipCode',
-];
-
-const profileHomePhoneUpdateKeys = [
-  'areaCode',
-  'countryCode',
-  'extension',
-  'phoneNumber',
-];
-
-const createAddressUpdateToUser = address => {
-  for (const key of potentialAddressUpdateKeys) {
-    if (_.hasIn(address, key)) {
-      memDb.user.data.attributes.vet360ContactInformation.mailingAddress[
-        key
-      ] = sanitize(address[key]);
-    }
+// sanitize user input
+const sanitize = dirty => {
+  // if input is a number, return it as is
+  if (typeof dirty === 'number' && !Number.isNaN(dirty)) return dirty;
+  if (typeof dirty !== 'string') {
+    throw new Error('Cannot sanitize, input must be a string');
   }
-
-  memDb.user.data.attributes.vet360ContactInformation.mailingAddress.updatedAt = new Date().toISOString();
+  return _.escape(dirty.replace(/<[^>]*>/g, '')).trim();
 };
 
-const createHomePhoneUpdateToUser = mobilePhone => {
-  for (const key of profileHomePhoneUpdateKeys) {
-    if (_.hasIn(mobilePhone, key)) {
-      memDb.user.data.attributes.vet360ContactInformation.homePhone[
-        key
-      ] = sanitize(mobilePhone[key]);
-    }
-  }
-
-  memDb.user.data.attributes.vet360ContactInformation.homePhone.updatedAt = new Date().toISOString();
+const updateFields = (target, source, fields) => {
+  return fields.reduce(
+    (updatedTarget, field) => {
+      if (_.has(source, field)) {
+        return {
+          ...updatedTarget,
+          [field]: sanitize(source[field]),
+        };
+      }
+      return updatedTarget;
+    },
+    { ...target, updatedAt: new Date().toISOString() },
+  );
 };
+
+// the path in the user object to update and the fields to update in that path
+const updateConfig = {
+  mailingAddress: {
+    path: 'data.attributes.vet360ContactInformation.mailingAddress',
+    fields: [
+      'addressLine1',
+      'addressLine2',
+      'addressLine3',
+      'city',
+      'stateCode',
+      'zipCode',
+    ],
+  },
+  homePhone: {
+    path: 'data.attributes.vet360ContactInformation.homePhone',
+    fields: ['areaCode', 'countryCode', 'extension', 'phoneNumber'],
+  },
+};
+
+const createUpdate = type => data => {
+  const config = updateConfig[type];
+  const target = _.get(memDb.user, config.path);
+  const updatedTarget = updateFields(target, data, config.fields);
+  _.set(memDb.user, config.path, updatedTarget);
+};
+
+const createMailingAddressUpdate = createUpdate('mailingAddress');
+const createHomePhoneUpdate = createUpdate('homePhone');
 
 const updateMemDb = (req, resJson) => {
   const key = `${req.method} ${req.url}`;
@@ -73,20 +71,23 @@ const updateMemDb = (req, resJson) => {
     return memDb.user;
   }
 
-  if (
-    key === 'PUT /v0/profile/addresses' &&
-    resJson.data.attributes.transactionId ===
-      'mock-update-mailing-address-success-transaction-id'
-  ) {
-    createAddressUpdateToUser(body);
-  }
+  const updates = {
+    'PUT /v0/profile/addresses': {
+      condition:
+        resJson.data.attributes.transactionId ===
+        'mock-update-mailing-address-success-transaction-id',
+      action: () => createMailingAddressUpdate(body),
+    },
+    'PUT /v0/profile/telephones': {
+      condition:
+        resJson.data.attributes.transactionId ===
+        'mock-update-home-phone-success-transaction-id',
+      action: () => createHomePhoneUpdate(body),
+    },
+  };
 
-  if (
-    key === 'PUT /v0/profile/telephones' &&
-    resJson.data.attributes.transactionId ===
-      'mock-update-home-phone-success-transaction-id'
-  ) {
-    createHomePhoneUpdateToUser(body);
+  if (updates[key]?.condition) {
+    updates[key].action();
   }
 
   return resJson;
