d3117a1357bdce09b03a96cb4eb49231a61a8810
Robin Garrison <robin.garrison@adhocteam.us>
2024-09-24 14:45:57 -0500

    Use reviewEditFocusOnHeaders in DR forms

--- src/applications/appeals/testing/sc/utils/focus.js
+++ src/applications/appeals/testing/sc/utils/focus.js
@@ -3,6 +3,7 @@ import {
   scrollTo,
   scrollToFirstError,
 } from 'platform/utilities/ui';
+import { focusReview } from 'platform/forms-system/src/js/utilities/ui/focus-review';
 import { $ } from 'platform/forms-system/src/js/utilities/ui';
 
 export const focusEvidence = (_index, root) => {
@@ -18,7 +19,14 @@ export const focusEvidence = (_index, root) => {
   });
 };
 
-export const focusH3AfterAlert = () => {
-  scrollTo('topContentElement');
-  focusElement('h3#header');
+export const focusH3AfterAlert = ({ name, onReviewPage } = {}) => {
+  if (name && onReviewPage) {
+    // name, editing (alert only visible in edit mode), reviewEditFocusOnHeaders
+    const editing = true; // alert only visible in edit mode
+    const reviewEditFocusOnHeaders = true; // from form/config.js setting
+    focusReview(name, editing, reviewEditFocusOnHeaders);
+  } else {
+    scrollTo('topContentElement');
+    focusElement('h3#header');
+  }
 };
