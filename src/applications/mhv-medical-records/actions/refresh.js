54b8130cc6fa5ac0a5b85fd0be0aed9aae56015b
Mike Moyer <michael.moyer@va.gov>
2024-09-24 10:50:43 -0400

    MHV-59744 Initial attempt at reducing status calls

--- src/applications/mhv-medical-records/actions/refresh.js
+++ src/applications/mhv-medical-records/actions/refresh.js
@@ -3,5 +3,13 @@ import { getRefreshStatus } from '../api/MrApi';
 
 export const fetchRefreshStatus = () => async dispatch => {
   const response = await getRefreshStatus();
+  window.localStorage.setItem('lastPhrRefreshDate', response?.lastRefreshDate);
   dispatch({ type: Actions.Refresh.GET_STATUS, payload: response });
+};
+
+export const setStatusPollBeginDate = statusPollBeginDate => async dispatch => {
+  dispatch({
+    type: Actions.Refresh.SET_STATUS_POLL_BEGIN,
+    payload: statusPollBeginDate,
+  });
 };
--- src/applications/mhv-medical-records/components/shared/PhrRefresh.jsx
+++ src/applications/mhv-medical-records/components/shared/PhrRefresh.jsx
@@ -1,6 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchRefreshStatus } from '../../actions/refresh';
+import PropTypes from 'prop-types';
+import {
+  fetchRefreshStatus,
+  setStatusPollBeginDate,
+} from '../../actions/refresh';
 import { STATUS_POLL_INTERVAL, refreshPhases } from '../../util/constants';
 import { Actions } from '../../util/actionTypes';
 
@@ -9,7 +13,7 @@ import { Actions } from '../../util/actionTypes';
  *
  * @returns null
  */
-const PhrRefresh = () => {
+const PhrRefresh = ({ statusPollBeginDate }) => {
   const dispatch = useDispatch();
   const refresh = useSelector(state => state.mr.refresh);
   const [endPollingDate, setEndPollingDate] = useState(null);
@@ -17,20 +21,56 @@ const PhrRefresh = () => {
   /** How long to poll the status endpoint, in milliseconds */
   const POLL_DURATION = 120000;
 
+  const PHR_REFRESH_DURATION = 3600000;
+
+  /**
+   * Read the PHR status polling timeout localStorage. Then use that value to update
+   * endPollingDate in the state to keep things in sync. If there is no value in localStorage,
+   * use the current time as the baseline for adding the POLL_DURATION.
+   */
+  const updateEndPollingDate = useCallback(
+    () => {
+      let lastPhrRefreshDate = +localStorage.getItem('lastPhrRefreshDate');
+      if (!lastPhrRefreshDate) {
+        lastPhrRefreshDate = new Date().getTime();
+      }
+      const updatedEndPollingDate = lastPhrRefreshDate + POLL_DURATION;
+      if (updatedEndPollingDate !== endPollingDate?.getTime()) {
+        setEndPollingDate(new Date(updatedEndPollingDate));
+      }
+      return updatedEndPollingDate;
+    },
+    [endPollingDate],
+  );
+
   useEffect(
     /**
-     * Fetch the refresh status from the backend when the app loads.
+     * Fetch the refresh status from the backend when the app loads. Store the time that this
+     * happens, and do not refresh the status again until the app is reloaded, or until it's
+     * possible that the PHR refresh was re-run (1 hour).
      */
     () => {
-      dispatch(fetchRefreshStatus())
-        .then(() => {
-          setEndPollingDate(new Date(new Date().getTime() + POLL_DURATION));
-        })
-        .catch(() => {
-          // If needed in the future, we can set a timer on failure to try again later.
-        });
+      if (
+        statusPollBeginDate === null ||
+        new Date().getTime() >
+          statusPollBeginDate.getTime() + PHR_REFRESH_DURATION
+      ) {
+        dispatch(fetchRefreshStatus())
+          .then(() => {
+            updateEndPollingDate();
+            if (!statusPollBeginDate) {
+              // Note the time that we started polling the status endpoint.
+              dispatch(setStatusPollBeginDate(new Date()));
+            }
+          })
+          .catch(() => {
+            // If needed in the future, we can set a timer on failure to try again later.
+          });
+      }
     },
-    [dispatch],
+    // Adding updateEndPollingDate here causes the useEffect to run several extra times.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [statusPollBeginDate, dispatch],
   );
 
   useEffect(
@@ -40,12 +80,15 @@ const PhrRefresh = () => {
      */
     () => {
       let timeoutId;
+      // Update the polling timeout from localStorage. Do this every time, because that date may
+      // change when the action is run.
+      const updatedEndPollingDate = updateEndPollingDate();
       if (
-        endPollingDate &&
+        updatedEndPollingDate &&
         refresh.status &&
         refresh.phase !== refreshPhases.CURRENT
       ) {
-        if (new Date() <= endPollingDate) {
+        if (new Date() <= updatedEndPollingDate) {
           timeoutId = setTimeout(() => {
             dispatch(fetchRefreshStatus());
           }, STATUS_POLL_INTERVAL);
@@ -60,10 +103,14 @@ const PhrRefresh = () => {
         }
       };
     },
-    [refresh.status, refresh.phase, endPollingDate, dispatch],
+    [refresh.status, refresh.phase, dispatch, updateEndPollingDate],
   );
 
   return null;
 };
 
 export default PhrRefresh;
+
+PhrRefresh.propTypes = {
+  statusPollBeginDate: PropTypes.instanceOf(Date),
+};
