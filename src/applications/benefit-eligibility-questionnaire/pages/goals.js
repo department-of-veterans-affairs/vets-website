99daf4d15236ac986afe65b00379f7e595a29cf6
Ian <ianseabock@gmail.com>
2024-09-26 12:45:22 -0700

    goals title showing

--- src/applications/benefit-eligibility-questionnaire/pages/goals.js
+++ src/applications/benefit-eligibility-questionnaire/pages/goals.js
@@ -1,24 +1,23 @@
 import {
   checkboxGroupSchema,
   checkboxGroupUI,
-  // textSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
-import { goalTypeLabels } from '../constants/benefits';
+import { goalTypes, goalTypeLabels } from '../constants/benefits';
 
 /** @type {PageSchema} */
 export default {
   uiSchema: {
-    'view:goals': {
-      ...checkboxGroupUI({
-        title: 'What goal(s) do you want to accomplish?',
-        hint: 'You may only select this for one employer address.',
-        required: false,
-        labels: goalTypeLabels,
-      }),
-    },
+    goals: checkboxGroupUI({
+      title: 'What goal(s) do you want to accomplish?',
+      hint: 'You may only select this for one employer address.',
+      required: false,
+      labels: goalTypeLabels,
+    }),
   },
   schema: {
     type: 'object',
-    properties: checkboxGroupSchema(Object.values(goalTypeLabels)).properties,
+    properties: {
+      goals: checkboxGroupSchema(Object.keys(goalTypes)),
+    },
   },
 };
