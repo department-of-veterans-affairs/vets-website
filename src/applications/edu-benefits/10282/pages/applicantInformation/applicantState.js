837432c9042c8fe69a6e1ac0bc57b4c353e0d7e7
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2024-09-26 14:10:19 -0400

    updated states

--- src/applications/edu-benefits/10282/pages/applicantInformation/applicantState.js
+++ src/applications/edu-benefits/10282/pages/applicantInformation/applicantState.js
@@ -1,5 +1,6 @@
 import { VaSelectField } from 'platform/forms-system/src/js/web-component-fields';
 import fullSchema10282 from 'vets-json-schema/dist/22-10282-schema.json';
+import constants from 'vets-json-schema/dist/constants.json';
 
 const { state } = fullSchema10282.definitions;
 
@@ -12,11 +13,13 @@ export const uiSchema = {
     },
   },
 };
-
 export const schema = {
   type: 'object',
   required: ['state'],
   properties: {
-    state,
+    state: {
+      ...state,
+      enum: constants.states.USA.map(st => st.label),
+    },
   },
 };
