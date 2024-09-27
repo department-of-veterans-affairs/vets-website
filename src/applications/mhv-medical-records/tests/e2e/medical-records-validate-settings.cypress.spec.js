1fdd5040ce5521640c37f86718964fc982ae34b0
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2024-09-27 14:07:07 -0700

    draft, intercept opted in status, intercept post

--- src/applications/mhv-medical-records/tests/e2e/medical-records-validate-settings.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-validate-settings.cypress.spec.js
@@ -1,16 +1,71 @@
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
-import optedIn from './fixtures/opted-in.json';
+// import optedIn from './fixtures/opted-in-status.json';
+// import optedOut from './fixtures/opted-out-status.json';
+import postOptOutResponse from './fixtures/post-opt-out-response.json';
 
 describe('Medical Records validate settings page', () => {
   it('visits settings page', () => {
     const site = new MedicalRecordsSite();
     site.login();
-    cy.intercept(
-      'GET',
-      'my_health/v1/health_records/sharing/status',
-      optedIn,
-    ).as('optedIn');
+
+    // // Intercept GET status #1 (opted in)
+    // cy.intercept(
+    //   'GET',
+    //   'my_health/v1/health_records/sharing/status',
+    //   optedIn,
+    // ).as('statusOptedIn');
+
+    cy.intercept('my_health/v1/health_records/sharing/status', {
+      // fixture: './fixtures/opted-in-status.json', // optedIn,
+      fixture:
+        './applications/mhv-medical-records/tests/e2e/fixtures/opted-in-status.json',
+      times: 1,
+    }).as('statusOptedIn');
+
+    // Visit settings page
     cy.visit('my-health/medical-records/settings');
+    cy.wait('@statusOptedIn');
+
+    // // Intercept POST opt out
+    // cy.intercept(
+    //   'POST',
+    //   '/health_records/sharing/optout',
+    //   postOptOutResponse,
+    // ).as('postOptOut');
+    // cy.wait(2000);
+    // Intercept POST opt out
+    cy.intercept(
+      'POST',
+      'my_health/v1/health_records/sharing/optout',
+      postOptOutResponse,
+    ).as('postOptOut');
+    // cy.wait(2000);
+
+    // // Intercept GET status #2 (opted out)
+    // cy.intercept(
+    //   'GET',
+    //   'my_health/v1/health_records/sharing/status',
+    //   optedOut,
+    // ).as('statusOptedOut');
+    cy.intercept('my_health/v1/health_records/sharing/status', {
+      // fixture: './fixtures/opted-in-status.json', // optedIn,
+      fixture:
+        './applications/mhv-medical-records/tests/e2e/fixtures/opted-out-status.json',
+      times: 1,
+    }).as('statusOptedOut');
+
+    cy.get('[data-testid="open-opt-in-out-modal-button"]').click();
+    // cy.wait(2000);
+    cy.get('button:contains("Yes, opt out")').click();
+    // cy.wait(5000);
+    //   .find('button')
+    //   .click();
+    //   .find('[type="button"]')
+    //   .click();
+
+    // cy.wait('@postOptOut');
+    // cy.wait('@statusOptedOut');
+
     cy.injectAxe();
     cy.axeCheck('main', {
       rules: {
