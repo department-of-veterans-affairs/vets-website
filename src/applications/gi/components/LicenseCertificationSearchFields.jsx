3c1fe4f513e2ffbde91570653b2ae6e64073a562
Nick Hibbits <nicholashibbits1@gmail.com>
2024-09-30 13:44:46 -0700

    Add constant dropdown values for cleaner markup

--- src/applications/gi/components/LicenseCertificationSearchFields.jsx
+++ src/applications/gi/components/LicenseCertificationSearchFields.jsx
@@ -1,58 +1,54 @@
 import React from 'react';
 import Dropdown from './Dropdown';
 
+const dropdowns = [
+  {
+    label: 'Category',
+    options: [
+      { optionValue: 'License', optionLabel: 'License' },
+      { optionValue: 'Certification', optionLabel: 'Certification' },
+    ],
+    alt: 'Category Type',
+  },
+  {
+    label: 'Country',
+    options: [{ optionValue: 'Country', optionLabel: 'Country' }],
+    alt: 'Country',
+  },
+  {
+    label: 'State',
+    options: [{ optionValue: 'State', optionLabel: 'State' }],
+    alt: 'State',
+  },
+];
+
 function LicenseCertificationSearchFields() {
   const handleChange = e => {
     return e.target;
   };
+
   return (
     <>
       <div className="row">
         <va-text-input label="License/Certification Name" />
       </div>
-      <div className="row">
-        <Dropdown
-          disabled={false}
-          // required
-          label="Category Type"
-          visible
-          name="Cateogry"
-          options={[
-            { optionValue: 'License', optionLabel: 'License' },
-            { optionValue: 'Certification', optionLabel: 'Certification' },
-          ]}
-          value=""
-          onChange={handleChange}
-          alt="Category Type"
-          selectClassName="lc-dropdown-filter"
-        />
-      </div>
-      <div className="row">
-        <Dropdown
-          disabled={false}
-          label="Country"
-          visible
-          name="Country"
-          options={[{ optionValue: 'Country', optionLabel: 'Country' }]}
-          value=""
-          onChange={handleChange}
-          alt="Country"
-          selectClassName="lc-dropdown-filter"
-        />
-      </div>
-      <div className="row">
-        <Dropdown
-          disabled={false}
-          label="State"
-          visible
-          name="State"
-          options={[{ optionValue: 'State', optionLabel: 'State' }]}
-          value=""
-          onChange={handleChange}
-          alt="State"
-          selectClassName="lc-dropdown-filter"
-        />
-      </div>
+      {dropdowns.map((dropdown, index) => {
+        return (
+          <div className="row" key={index}>
+            <Dropdown
+              disabled={false}
+              label={dropdown.label}
+              visible
+              name="Cateogry"
+              options={dropdown.options}
+              value=""
+              onChange={handleChange}
+              alt={dropdown.alt}
+              selectClassName="lc-dropdown-filter"
+            />
+          </div>
+        );
+      })}
     </>
   );
 }
