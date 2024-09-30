09c5d759d286334b9f79d4d3b46ebc46ab958de1
fazilqa <fazil.mammadov@bylight.com>
2024-09-30 12:54:48 -0700

    add E2E for compose page maintenance banner

--- src/applications/mhv-secure-messaging/tests/e2e/secure-messaging-maintenance-downtime-banner.cypress.spec.js
+++ src/applications/mhv-secure-messaging/tests/e2e/secure-messaging-maintenance-downtime-banner.cypress.spec.js
@@ -35,10 +35,10 @@ describe('SM Inbox Maintenance Banner', () => {
       .should(`be.visible`)
       .and(`have.text`, Alerts.MAINTENANCE.ACTIVE);
 
-    cy.contains(`Start`)
+    cy.contains(`Start:`)
       .parent(`p`)
       .should(`contain.text`, GeneralFunctionsPage.getDateFormat(currentDate));
-    cy.contains(`End`)
+    cy.contains(`End:`)
       .parent(`p`)
       .should(`contain.text`, GeneralFunctionsPage.getDateFormat(endDate));
 
@@ -60,10 +60,80 @@ describe('SM Inbox Maintenance Banner', () => {
       .should(`be.visible`)
       .and(`have.text`, Alerts.MAINTENANCE.UPCOMING);
 
-    cy.contains(`Start`)
+    cy.contains(`Start:`)
       .parent(`p`)
       .should(`contain.text`, GeneralFunctionsPage.getDateFormat(upcomingDate));
-    cy.contains(`End`)
+    cy.contains(`End:`)
+      .parent(`p`)
+      .should(`contain.text`, GeneralFunctionsPage.getDateFormat(endDate));
+
+    cy.injectAxe();
+    cy.axeCheck(AXE_CONTEXT);
+  });
+});
+
+describe('SM Compose Maintenance Banner', () => {
+  const currentDate = new Date();
+  const upcomingDate = new Date(currentDate);
+  upcomingDate.setHours(upcomingDate.getHours() + 1);
+  const endDate = new Date(currentDate);
+  endDate.setHours(endDate.getHours() + 5);
+
+  const activeMaintenanceData = PatientInboxPage.maintenanceWindowResponse(
+    currentDate,
+    endDate,
+  );
+
+  const upcomingMaintenanceData = PatientInboxPage.maintenanceWindowResponse(
+    upcomingDate,
+    endDate,
+  );
+
+  it(`verify active maintenance`, () => {
+    cy.intercept(
+      `GET`,
+      Paths.INTERCEPT.MAINTENANCE_WINDOWS,
+      activeMaintenanceData,
+    ).as(`maintenance_windows`);
+
+    SecureMessagingSite.login();
+    cy.visit(`/my-health/secure-messages/new-message/`);
+
+    cy.get(Locators.ALERTS.VA_ALERT)
+      .find(`h2`)
+      .should(`be.visible`)
+      .and(`have.text`, Alerts.MAINTENANCE.ACTIVE);
+
+    cy.contains(`Start:`)
+      .parent(`p`)
+      .should(`contain.text`, GeneralFunctionsPage.getDateFormat(currentDate));
+    cy.contains(`End:`)
+      .parent(`p`)
+      .should(`contain.text`, GeneralFunctionsPage.getDateFormat(endDate));
+
+    // axeCheck() verification could not be added due to page content absence
+  });
+
+  it(`verify upcoming maintenance`, () => {
+    cy.intercept(
+      `GET`,
+      Paths.INTERCEPT.MAINTENANCE_WINDOWS,
+      upcomingMaintenanceData,
+    ).as(`maintenance_windows`);
+
+    SecureMessagingSite.login();
+    PatientInboxPage.loadInboxMessages();
+    PatientInboxPage.navigateToComposePage();
+
+    cy.get(Locators.ALERTS.VA_ALERT)
+      .find(`h2`)
+      .should(`be.visible`)
+      .and(`have.text`, Alerts.MAINTENANCE.UPCOMING);
+
+    cy.contains(`Start:`)
+      .parent(`p`)
+      .should(`contain.text`, GeneralFunctionsPage.getDateFormat(upcomingDate));
+    cy.contains(`End:`)
       .parent(`p`)
       .should(`contain.text`, GeneralFunctionsPage.getDateFormat(endDate));
 
