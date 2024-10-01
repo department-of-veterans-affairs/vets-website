ada3f2defa54025f6b542e63d3ee91ee8451c579
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2024-10-01 09:20:01 -0700

    new location of alert, settings page object

--- src/applications/mhv-medical-records/tests/e2e/medical-records-validate-settings.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-validate-settings.cypress.spec.js
@@ -1,73 +1,92 @@
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
-import optedIn from './fixtures/opted-in-status.json';
+import SettingsPage from './pages/SettingsPage';
+// import optedIn from './fixtures/opted-in-status.json';
 // import optedOut from './fixtures/opted-out-status.json';
-import postOptOutResponse from './fixtures/post-opt-out-response.json';
-import postOptInResponse from './fixtures/post-opt-in-response.json';
+// import postOptOutResponse from './fixtures/post-opt-out-response.json';
+// import postOptInResponse from './fixtures/post-opt-in-response.json';
 
 describe('Medical Records validate settings page', () => {
   it('visits settings page', () => {
     const site = new MedicalRecordsSite();
     site.login();
 
-    // Intercept GET status (opted in)
-    cy.intercept(
-      'GET',
-      'my_health/v1/health_records/sharing/status',
-      optedIn,
-    ).as('statusOptedIn');
+    // // Intercept GET status (opted in)
+    // cy.intercept(
+    //   'GET',
+    //   'my_health/v1/health_records/sharing/status',
+    //   optedIn,
+    // ).as('statusOptedIn');
 
-    // Visit settings page
-    cy.visit('my-health/medical-records/settings');
-    cy.wait('@statusOptedIn');
-    // cy.wait(2000);
+    // // Visit settings page
+    // cy.visit('my-health/medical-records/settings');
+    // cy.wait('@statusOptedIn');
 
-    // Verify opted-in status
-    cy.get('va-card')
-      .find('h3')
-      .contains('Your sharing setting: Opted in');
+    SettingsPage.visitSettingsPage();
 
-    // Intercept POST opt out
-    cy.intercept(
-      'POST',
-      'my_health/v1/health_records/sharing/optout',
-      postOptOutResponse,
-    ).as('postOptOut');
+    // // Verify opted-in status
+    // cy.get('va-card')
+    //   .find('h3')
+    //   .contains('Your sharing setting: Opted in');
 
-    // Select opt out
-    cy.get('[data-testid="open-opt-in-out-modal-button"]').click();
-    // cy.wait(2000);
-    cy.get('button:contains("Yes, opt out")').click();
-    // cy.wait(2000);
-    cy.wait('@postOptOut');
+    SettingsPage.verifyOptedInStatus();
 
-    // Verify opted-out status
-    cy.get('va-card')
-      .find('h3')
-      .contains('Your sharing setting: Opted out');
-    // cy.get('va-alert').contains('You’ve opted out of sharing');
-    cy.get('div.settings')
-      .children()
-      .first()
-      .contains('You’ve opted out of sharing');
+    // // Intercept POST opt out
+    // cy.intercept(
+    //   'POST',
+    //   'my_health/v1/health_records/sharing/optout',
+    //   postOptOutResponse,
+    // ).as('postOptOut');
 
-    cy.intercept(
-      'POST',
-      'my_health/v1/health_records/sharing/optin',
-      postOptInResponse,
-    ).as('postOptIn');
-    // cy.wait(2000);
+    // // Select opt out
+    // cy.get('[data-testid="open-opt-in-out-modal-button"]').click();
 
-    // Select opt in
-    cy.get('[data-testid="open-opt-in-out-modal-button"]').click(); // is this the right selector?
-    // cy.wait(2000);
-    cy.get('button:contains("Yes, opt in")').click();
-    cy.wait('@postOptIn');
+    // cy.get('button:contains("Yes, opt out")').click();
 
-    // Verify opted-in status
-    cy.get('va-card')
-      .find('h3')
-      .contains('Your sharing setting: Opted in');
-    cy.get('va-alert').contains('You’ve opted back in to sharing');
+    // cy.wait('@postOptOut');
+
+    SettingsPage.selectOptOut();
+
+    // // Verify opted-out status
+    // cy.get('va-card')
+    //   .find('h3')
+    //   .contains('Your sharing setting: Opted out');
+
+    SettingsPage.verifyOptedOutStatus();
+
+    // Verify opted-out alert
+    // cy.get('section')
+    //   .eq(1)
+    //   .find('va-alert')
+    //   .contains('You’ve opted out of sharing');
+
+    SettingsPage.verifyOptedOutAlert();
+
+    // cy.intercept(
+    //   'POST',
+    //   'my_health/v1/health_records/sharing/optin',
+    //   postOptInResponse,
+    // ).as('postOptIn');
+
+    // // Select opt in
+    // cy.get('[data-testid="open-opt-in-out-modal-button"]').click(); // is this the right selector?
+    // cy.get('button:contains("Yes, opt in")').click();
+    // cy.wait('@postOptIn');
+
+    SettingsPage.selectOptIn();
+
+    // // Verify opted-in status
+    // cy.get('va-card')
+    //   .find('h3')
+    //   .contains('Your sharing setting: Opted in');
+
+    SettingsPage.verifyOptedInStatus();
+
+    // cy.get('section')
+    //   .eq(1)
+    //   .find('va-alert')
+    //   .contains('You’ve opted back in to sharing');
+
+    SettingsPage.verifyOptedInAlert();
 
     cy.injectAxe();
     cy.axeCheck('main', {
