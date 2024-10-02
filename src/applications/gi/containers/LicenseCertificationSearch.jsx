5b39d41e337047551268d1da05ad1850e2272ebe
Nick Hibbits <nicholashibbits1@gmail.com>
2024-10-02 10:52:43 -0700

    refactor to use va style classes

--- src/applications/gi/containers/LicenseCertificationSearch.jsx
+++ src/applications/gi/containers/LicenseCertificationSearch.jsx
@@ -4,7 +4,7 @@ import LicenseCertificationSearchFields from '../components/LicenseCertification
 function LicenseCertificationSearch() {
   return (
     <div>
-      <section className="lc-wrapper vads-u-padding-x--2p5 small-screen:vads-u-padding-x--2">
+      <section className="vads-u-display--flex vads-u-flex-direction--column vads-u-padding-x--2p5 mobile-lg:vads-u-padding-x--2">
         <div className="row">
           <h1 className="vads-u-text-align--center mobile-lg:vads-u-text-align--left">
             Licenses and Certifications
