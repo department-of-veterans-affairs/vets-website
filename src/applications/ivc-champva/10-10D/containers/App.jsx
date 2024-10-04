5bc1d162adf6804c1515d740b59a90a03d7436f1
Ray Messina <rmessina10@gmail.com>
2024-10-04 01:12:06 -0500

    remove flipper logic from 1010d app

--- src/applications/ivc-champva/10-10D/containers/App.jsx
+++ src/applications/ivc-champva/10-10D/containers/App.jsx
@@ -6,9 +6,7 @@ import {
 import PropTypes from 'prop-types';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import { VaBreadcrumbs } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import { Toggler } from 'platform/utilities/feature-toggles';
 import formConfig from '../config/form';
-import WIP from '../../shared/components/WIP';
 import { addStyleToShadowDomOnPages } from '../../shared/utilities';
 
 const breadcrumbList = [
@@ -40,30 +38,15 @@ export default function App({ location, children }) {
 
   return (
     <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
-      <Toggler toggleName={Toggler.TOGGLE_NAMES.form1010d}>
-        <Toggler.Enabled>
-          <VaBreadcrumbs breadcrumbList={breadcrumbList} />
-          <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-            <DowntimeNotification
-              appTitle={`CHAMPVA Form ${formConfig.formId}`}
-              dependencies={[externalServices.pega]}
-            >
-              {children}
-            </DowntimeNotification>
-          </RoutedSavableApp>
-        </Toggler.Enabled>
-        <Toggler.Disabled>
-          <br />
-          <WIP
-            content={{
-              description:
-                'We’re rolling out the CHAMPVA application (VA Form 10-10d) in stages. It’s not quite ready yet. Please check back again soon.',
-              redirectLink: '/',
-              redirectText: 'Return to VA home page',
-            }}
-          />
-        </Toggler.Disabled>
-      </Toggler>
+      <VaBreadcrumbs breadcrumbList={breadcrumbList} />
+      <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+        <DowntimeNotification
+          appTitle={`CHAMPVA Form ${formConfig.formId}`}
+          dependencies={[externalServices.pega]}
+        >
+          {children}
+        </DowntimeNotification>
+      </RoutedSavableApp>
     </div>
   );
 }
