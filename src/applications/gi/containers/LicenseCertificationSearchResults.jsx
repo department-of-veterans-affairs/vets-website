dd428ee3acc8fc5d3c4bb8733218d44bc94591a7
Nick Hibbits <nicholashibbits1@gmail.com>
2024-10-05 16:17:31 -0500

    add va-accordions to search results page

--- src/applications/gi/containers/LicenseCertificationSearchResults.jsx
+++ src/applications/gi/containers/LicenseCertificationSearchResults.jsx
@@ -1,5 +1,58 @@
 import React from 'react';
 
+const results = [
+  {
+    title: 'Certification in Forensic Odontology',
+    type: 'Certification',
+    tests: [
+      { testName: 'Part 1', fee: 220.0 },
+      { testName: 'Part 2', fee: 220.0 },
+      { testName: 'Oral Exam', fee: 1000.0 },
+    ],
+    boardInfo: {
+      name: 'AMERICAN BOARD OF FORENSIC ODONTOLOGY',
+      phone: '205-902-2478',
+      address: {
+        street: '410 North 21st Street',
+        city: 'Colorado Springs',
+        state: 'CO',
+        zip: '80904',
+        country: 'United States of America',
+      },
+    },
+    formInfo: {
+      formNumber: '22-0803',
+      description:
+        'Print and fill out to request reimbursement for licenses and certifications.',
+    },
+  },
+  {
+    title: 'License in Forensic Dentistry',
+    type: 'License',
+    tests: [
+      { testName: 'Part 1', fee: 220.0 },
+      { testName: 'Part 2', fee: 220.0 },
+      { testName: 'Oral Exam', fee: 1000.0 },
+    ],
+    boardInfo: {
+      name: 'AMERICAN BOARD OF FORENSIC ODONTOLOGY',
+      phone: '205-902-2478',
+      address: {
+        street: '410 North 21st Street',
+        city: 'Colorado Springs',
+        state: 'CO',
+        zip: '80904',
+        country: 'United States of America',
+      },
+    },
+    formInfo: {
+      formNumber: '22-0803',
+      description:
+        'Print and fill out to request reimbursement for licenses and certifications.',
+    },
+  },
+];
+
 function LicenseCertificationSearchResults() {
   return (
     <div>
@@ -19,6 +72,22 @@ function LicenseCertificationSearchResults() {
             <strong>Country:</strong>
             "USA"
           </p>
+        </div>
+        <div className="row">
+          <va-accordion>
+            {results.map((result, index) => {
+              return (
+                <va-accordion-item
+                  key={index}
+                  id={index}
+                  header={result.title}
+                  subheader={result.type}
+                >
+                  <p>{result.type}</p>
+                </va-accordion-item>
+              );
+            })}
+          </va-accordion>
         </div>
       </section>
     </div>
