0f9d1f29f8d5dcba63a1482b5f6b5395da328bed
Rob Garrison <wowmotty@gmail.com>
2024-10-08 15:24:41 -0500

    Add focusOnAlertRole formOption (#32315)

--- src/platform/forms-system/src/js/utilities/ui/index.js
+++ src/platform/forms-system/src/js/utilities/ui/index.js
@@ -2,6 +2,7 @@ import Scroll from 'react-scroll';
 import {
   focusElement,
   focusByOrder,
+  scrollToFirstError,
   getScrollOptions,
 } from '../../../../../utilities/ui';
 import { webComponentList } from '../../web-component-fields/webComponentList';
@@ -22,7 +23,7 @@ export const $$ = (selector, root) => [
 
 /**
  * fixSelector
- * When a page name includes a color, e.g. "view:exampleFoo", the name ends up
+ * When a page name includes a colon, e.g. "view:exampleFoo", the name ends up
  * in a scroll element with name="view:exampleFooScrollElement". Using
  * querySelector without escaping the colon will fail to find the target
  * Note: We shouldn't be passing in something like `:not([name="test"])`
@@ -30,7 +31,13 @@ export const $$ = (selector, root) => [
 const REGEXP_COLON = /:/g;
 export const fixSelector = selector => selector.replace(REGEXP_COLON, '\\:');
 
-export { focusElement, focusByOrder };
+export {
+  focusElement,
+  focusByOrder,
+  scrollToFirstError,
+  getScrollOptions,
+  ERROR_ELEMENTS,
+};
 
 /**
  * Find all the focusable elements within a block
@@ -120,41 +127,4 @@ export function setGlobalScroll() {
       smooth: true,
     },
   };
-}
-
-// Duplicate of function in platform/utilities/ui/scroll
-export function scrollToFirstError() {
-  // [error] will focus any web-components with an error message
-  const errorEl = document.querySelector(ERROR_ELEMENTS.join(','));
-  if (errorEl) {
-    // document.body.scrollTop doesn’t work with all browsers, so we’ll cover them all like so:
-    const currentPosition =
-      window.pageYOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop ||
-      0;
-    const position = errorEl.getBoundingClientRect().top + currentPosition;
-    // Don't animate the scrolling if there is an open modal on the page. This
-    // prevents the page behind the modal from scrolling if there is an error in
-    // modal's form.
-
-    // We have to search the shadow root of web components that have a slotted va-modal
-    const isShadowRootModalOpen = Array.from(
-      document.querySelectorAll('va-omb-info'),
-    ).some(ombInfo =>
-      ombInfo.shadowRoot?.querySelector(
-        'va-modal[visible]:not([visible="false"])',
-      ),
-    );
-
-    const isModalOpen =
-      document.body.classList.contains('modal-open') ||
-      document.querySelector('va-modal[visible]:not([visible="false"])') ||
-      isShadowRootModalOpen;
-
-    if (!isModalOpen) {
-      Scroll.animateScroll.scrollTo(position - 10, getScrollOptions());
-    }
-    focusElement(errorEl);
-  }
 }
