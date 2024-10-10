078d991684c95346f2d0c628d77969f61f2ee1db
Midge-dev <sean.midgley.dev@gmail.com>
2024-10-10 11:49:10 -0700

    student array pages 1

--- src/applications/disability-benefits/686c-674-v2/config/chapters/674/addStudentsArrayPages.js
+++ src/applications/disability-benefits/686c-674-v2/config/chapters/674/addStudentsArrayPages.js
@@ -2,28 +2,44 @@
 import { capitalize } from 'lodash';
 import {
   titleUI,
-  //   textUI,
-  //   textSchema,
+  textUI,
+  textSchema,
+  addressUI,
+  addressSchema,
   arrayBuilderItemFirstPageTitleUI,
   arrayBuilderYesNoSchema,
   arrayBuilderYesNoUI,
-  // arrayBuilderItemSubsequentPageTitleUI,
+  arrayBuilderItemSubsequentPageTitleUI,
+  yesNoUI,
+  yesNoSchema,
   fullNameNoSuffixUI,
   fullNameNoSuffixSchema,
+  ssnUI,
+  ssnSchema,
   //   radioUI,
   //   radioSchema,
-  //   currentOrPastDateUI,
-  //   currentOrPastDateSchema,
+  currentOrPastDateUI,
+  currentOrPastDateSchema,
+  checkboxGroupUI,
+  checkboxGroupSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
 // import VaTextInputField from 'platform/forms-system/src/js/web-component-fields/VaTextInputField';
 // import VaSelectField from 'platform/forms-system/src/js/web-component-fields/VaSelectField';
 // import VaCheckboxField from 'platform/forms-system/src/js/web-component-fields/VaCheckboxField';
-import //   marriageEnums,
-//   spouseFormerMarriageLabels,
-//   customLocationSchema,
-//   generateHelpText,
-'../../helpers';
-import { AddStudentsIntro } from './helpers';
+// import //   marriageEnums,
+// //   spouseFormerMarriageLabels,
+// //   customLocationSchema,
+// //   generateHelpText,
+// '../../helpers';
+import {
+  AccreditedSchool,
+  AddStudentsIntro,
+  benefitSchemaLabels,
+  benefitUiLabels,
+  ProgramExamples,
+  TermDateHint,
+} from './helpers';
+import { generateHelpText } from '../../helpers';
 
 /* NOTE: 
  * In "Add mode" of the array builder, formData represents the entire formData object.
@@ -38,16 +54,12 @@ export const addStudentsOptions = {
   nounSingular: 'student',
   nounPlural: 'students',
   required: true,
-  isItemIncomplete: item => !item?.fullName?.first || !item?.fullName?.last,
-  //     !item?.startDate ||
-  //     !item?.endDate ||
-  //     !item?.reasonMarriageEnded ||
-  //     !item?.startLocation?.location?.city ||
-  //     !item?.endLocation?.location?.city ||
-  //     (item?.startLocation?.outsideUsa === false &&
-  //       !item?.startLocation?.location?.state) ||
-  //     (item?.endLocation?.outsideUsa === false &&
-  //       !item?.endLocation?.location?.state),
+  isItemIncomplete: item =>
+    !item?.fullName?.first ||
+    !item?.fullName?.last ||
+    !item?.birthDate ||
+    !item?.ssn ||
+    !item?.isParent,
   maxItems: 7,
   text: {
     summaryTitle: 'Review your students',
@@ -155,18 +167,416 @@ export const addStudentsSummaryPage = {
 };
 
 /** @returns {PageSchema} */
-export const studentFullNamePage = {
+export const studentInformationPage = {
   uiSchema: {
     ...arrayBuilderItemFirstPageTitleUI({
       title: 'Add a student',
       nounSingular: addStudentsOptions.nounSingular,
     }),
-    fullName: fullNameNoSuffixUI(),
+    fullName: fullNameNoSuffixUI(title => `Student’s ${title}`),
+    birthDate: {
+      ...currentOrPastDateUI('Student’s date of birth'),
+      'ui:required': () => true,
+    },
   },
   schema: {
     type: 'object',
     properties: {
       fullName: fullNameNoSuffixSchema,
+      birthDate: currentOrPastDateSchema,
+    },
+  },
+};
+
+/** @returns {PageSchema} */
+export const studentIDInformationPage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(() => 'Student’s information'),
+    ssn: {
+      ...ssnUI('Student’s Social Security number'),
+      'ui:required': () => true,
+    },
+    isParent: {
+      ...yesNoUI('Are you this student’s parent?'),
+      'ui:required': () => true,
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      ssn: ssnSchema,
+      isParent: yesNoSchema,
+    },
+  },
+};
+
+/** @returns {PageSchema} */
+export const studentIncomePage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(() => 'Student’s information'),
+    studentIncome: {
+      ...yesNoUI('Did this student earn an income in the last 365 days?'),
+      'ui:description': generateHelpText(
+        'Answer this question only if you are adding this dependent to your pension.',
+      ),
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      studentIncome: yesNoSchema,
+    },
+  },
+};
+
+/** @returns {PageSchema} */
+export const studentAddressPage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(() => 'Student’s address'),
+    address: addressUI({
+      labels: {
+        militaryCheckbox:
+          'They receive mail outside of the United States on a U.S. military base.',
+      },
+    }),
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      address: addressSchema(),
+    },
+  },
+};
+
+/** @returns {PageSchema} */
+export const studentMaritalStatusPage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(() => 'Student’s marital status'),
+    wasMarried: {
+      ...yesNoUI('Has this student ever been married?'),
+      'ui:required': () => true,
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      wasMarried: yesNoSchema,
+    },
+  },
+};
+
+/** @returns {PageSchema} */
+export const studentEducationBenefitsPage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(
+      () => 'Student’s education benefits',
+    ),
+    typeOfProgramOrBenefit: {
+      ...checkboxGroupUI({
+        title:
+          'Does the student currently receive education benefits from any of these programs?',
+        labels: benefitUiLabels,
+        required: () => false,
+        description: generateHelpText('Check all that the student receives'),
+      }),
+    },
+    tuitionIsPaidByGovAgency: {
+      ...yesNoUI(
+        'Is the student enrolled in a program or school that’s entirely funded by the federal government?',
+      ),
+      'ui:required': () => true,
+    },
+    'view:programExamples': {
+      'ui:description': ProgramExamples,
+      'ui:options': {
+        hideOnReview: true,
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      typeOfProgramOrBenefit: checkboxGroupSchema(benefitSchemaLabels),
+      tuitionIsPaidByGovAgency: yesNoSchema,
+      'view:programExamples': {
+        type: 'object',
+        properties: {},
+      },
+    },
+  },
+};
+
+/** @returns {PageSchema} */
+export const studentEducationBenefitsStartDatePage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(
+      () => 'Student’s education benefits',
+    ),
+    benefitPaymentDate: {
+      ...currentOrPastDateUI(
+        'When did the student start receiving education benefit payments?',
+      ),
+      'ui:required': () => true,
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      benefitPaymentDate: currentOrPastDateSchema,
+    },
+  },
+};
+
+/** @returns {PageSchema} */
+export const studentProgramInfoPage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(
+      () => 'Student’s program or school',
+    ),
+    schoolInformation: {
+      name: {
+        ...textUI(
+          'What’s the name of the school or trade program the student attends?',
+        ),
+        'ui:required': () => true,
+        'ui:options': {
+          width: 'xl',
+        },
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      schoolInformation: {
+        type: 'object',
+        properties: {
+          name: textSchema,
+        },
+      },
+    },
+  },
+};
+
+export const studentAttendancePage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(
+      () => 'Additional information for this student',
+    ),
+    schoolInformation: {
+      studentIsEnrolledFullTime: yesNoUI({
+        title:
+          'Has the student attended school continuously since they started school?',
+        required: () => true,
+        description: generateHelpText(
+          'Attending school continuously means they didn’t stop attending school, except for normal breaks during the school year, like winter break or summer break',
+        ),
+      }),
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      schoolInformation: {
+        type: 'object',
+        properties: {
+          studentIsEnrolledFullTime: yesNoSchema,
+        },
+      },
+    },
+  },
+};
+
+export const studentStoppedAttendingDatePage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(
+      () => 'Additional information for this student',
+    ),
+    schoolInformation: {
+      dateFullTimeEnded: {
+        ...currentOrPastDateUI(
+          'When did the student stop attending school continuously?',
+        ),
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      schoolInformation: {
+        type: 'object',
+        properties: {
+          dateFullTimeEnded: currentOrPastDateSchema,
+        },
+      },
+    },
+  },
+};
+
+export const schoolAccreditationPage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(
+      () => 'Additional information for this student',
+    ),
+    schoolInformation: {
+      isSchoolAccredited: yesNoUI({
+        title: 'Is the student’s school accredited?',
+        required: () => true,
+      }),
+      'view:accredited': {
+        'ui:description': AccreditedSchool,
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      schoolInformation: {
+        type: 'object',
+        properties: {
+          isSchoolAccredited: yesNoSchema,
+          'view:accredited': {
+            type: 'object',
+            properties: {},
+          },
+        },
+      },
+    },
+  },
+};
+
+export const studentTermDatesPage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(() => 'Student’s term dates'),
+    schoolInformation: {
+      currentTermDates: {
+        officialSchoolStartDate: {
+          ...currentOrPastDateUI(
+            'When did the student’s regular school term or course officially start?',
+          ),
+          'ui:required': () => true,
+          'ui:description': TermDateHint,
+        },
+        expectedStudentStartDate: {
+          ...currentOrPastDateUI(
+            'When did the student start or expect to start their course?',
+          ),
+          'ui:required': () => true,
+        },
+        expectedGraduationDate: {
+          ...currentOrPastDateUI('When does the student expect to graduate?'),
+          'ui:required': () => true,
+        },
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      schoolInformation: {
+        type: 'object',
+        properties: {
+          currentTermDates: {
+            type: 'object',
+            properties: {
+              officialSchoolStartDate: currentOrPastDateSchema,
+              expectedStudentStartDate: currentOrPastDateSchema,
+              expectedGraduationDate: currentOrPastDateSchema,
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+export const previousTermQuestionPage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(() => 'Student’s term dates'),
+    schoolInformation: {
+      studentDidAttendSchoolLastTerm: yesNoUI({
+        title: 'Did the student attend school last term?',
+        description: generateHelpText(
+          'This includes any kind type of school or training, including high school.',
+        ),
+        required: () => true,
+      }),
+      // lastTermSchoolInformation: {
+      //   // 'ui:options': {
+      //   //   hideIf: (formData, index) =>
+      //   //     !formData?.studentInformation[index]?.schoolInformation
+      //   //       ?.studentDidAttendSchoolLastTerm,
+      //   // },
+      //   termBegin: {
+      //     ...currentOrPastDateUI('When did the previous school term start?'),
+      //     // 'ui:required': (formData, index) =>
+      //     //   formData?.studentInformation[index]?.schoolInformation
+      //     //     ?.studentDidAttendSchoolLastTerm,
+      //   },
+      //   dateTermEnded: {
+      //     ...currentOrPastDateUI('When did the previous school term end?'),
+      //     // 'ui:required': (formData, index) =>
+      //     //   formData?.studentInformation[index]?.schoolInformation
+      //     //     ?.studentDidAttendSchoolLastTerm,
+      //   },
+      // },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      schoolInformation: {
+        type: 'object',
+        properties: {
+          studentDidAttendSchoolLastTerm: yesNoSchema,
+          // lastTermSchoolInformation: {
+          //   type: 'object',
+          //   properties: {
+          //     termBegin: currentOrPastDateSchema,
+          //     dateTermEnded: currentOrPastDateSchema,
+          //   },
+          // },
+        },
+      },
+    },
+  },
+};
+
+export const previousTermDatesPage = {
+  uiSchema: {
+    ...arrayBuilderItemSubsequentPageTitleUI(() => 'Student’s term dates'),
+    schoolInformation: {
+      lastTermSchoolInformation: {
+        termBegin: {
+          ...currentOrPastDateUI('When did the previous school term start?'),
+          // 'ui:required': (formData, index) =>
+          //   formData?.studentInformation[index]?.schoolInformation
+          //     ?.studentDidAttendSchoolLastTerm,
+        },
+        dateTermEnded: {
+          ...currentOrPastDateUI('When did the previous school term end?'),
+          // 'ui:required': (formData, index) =>
+          //   formData?.studentInformation[index]?.schoolInformation
+          //     ?.studentDidAttendSchoolLastTerm,
+        },
+      },
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      schoolInformation: {
+        type: 'object',
+        properties: {
+          lastTermSchoolInformation: {
+            type: 'object',
+            properties: {
+              termBegin: currentOrPastDateSchema,
+              dateTermEnded: currentOrPastDateSchema,
+            },
+          },
+        },
+      },
     },
   },
 };
--- src/applications/disability-benefits/686c-674-v2/config/chapters/674/helpers.jsx
+++ src/applications/disability-benefits/686c-674-v2/config/chapters/674/helpers.jsx
@@ -30,3 +30,57 @@ export const AddStudentsIntro = () => (
     </p>
   </div>
 );
+
+export const benefitSchemaLabels = ['ch35', 'fry', 'feca', 'other'];
+
+export const benefitUiLabels = {
+  ch35: 'Chapter 35 (also known as Dependent’s Educational Assistance program)',
+  fry: 'Fry Scholarship',
+  feca: 'Federal Employees’ Compensation Act (FECA)',
+  other: 'Another program',
+};
+
+export const ProgramExamples = (
+  <va-additional-info trigger="Examples of federally funded programs or schools">
+    <ul>
+      <li>U.S Department of Labor’s Job Corps program</li>
+      <li>Military academies</li>
+      <li>Federally funded Native American schools</li>
+    </ul>
+  </va-additional-info>
+);
+
+export const AccreditedSchool = (
+  <va-additional-info trigger="How to tell if a school is accredited">
+    <p>
+      You can go to the U.S. Department of Education’s website to check if a
+      school is accredited.
+    </p>
+    <a
+      href="https://ope.ed.gov/dapip/#/home"
+      rel="noopener noreferrer"
+      aria-label="Go to the U.S. Department of Education’s website (opens in new tab)"
+      target="_blank"
+    >
+      Go to the U.S. Department of Education’s website (opens in new tab)
+    </a>
+  </va-additional-info>
+);
+
+export const TermDateHint = (
+  <va-additional-info
+    className="vads-u-margin-top--2"
+    trigger="What is a regular school term or course?"
+  >
+    <p>
+      For example, if the student’s school uses semesters, tell us when the
+      student’s current semester started. Or if the student’s school uses
+      quarters, tell us when the student’s current quarter started.
+    </p>
+    <br />
+    <p>
+      If the student is currently on academic break between semesters, tell us
+      when the student’s previous term started
+    </p>
+  </va-additional-info>
+);
