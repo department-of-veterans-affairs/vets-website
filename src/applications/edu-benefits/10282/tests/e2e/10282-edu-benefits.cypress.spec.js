74ef0f070a5f2172037fcba72d43ab7e0e722f50
fatmakhan0395 <Fatma.khan@va.gov>
2024-10-11 08:42:13 -0700

    vebt-559 22-10282 cypress test

---
+++ src/applications/edu-benefits/10282/tests/e2e/10282-edu-benefits.cypress.spec.js
@@ -0,0 +1,195 @@
+describe('22-10282 Edu form', () => {
+  beforeEach(() => {
+    cy.visit('/education/apply-for-education-benefits/10282/');
+  });
+  it('should show form title', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.get('h1[data-testid="form-title"]').should('exist');
+  });
+  it('should go start form when user clicks "Start your application" ', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.get('[text="Start your application"]').click();
+    cy.get('[data-testid="full-name"]').should('exist');
+    cy.location('href').should('contain', '/applicant/information');
+  });
+  it('should show errors if Continue button is clicked and First name or last Name fields empty"', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.get('[text="Start your application"]').click();
+    cy.get('[data-testid="full-name"]').should('exist');
+    cy.get('[class="usa-button-primary"]').click();
+    cy.get('[id="root_veteranFullName_first-error-message"]').should(
+      'contain',
+      'Please enter a first name',
+    );
+    cy.get('[id="root_veteranFullName_last-error-message"]').should(
+      'contain',
+      'Please enter a last name',
+    );
+  });
+  describe('Which of these best describes you page', () => {
+    beforeEach(() => {
+      cy.get('[text="Start your application"]').click();
+      cy.get('[id="root_veteranFullName_first"]').type('John');
+      cy.get('[id="root_veteranFullName_last"]').type('Doe');
+      cy.get('[class="usa-button-primary"]').click();
+    });
+    it('should go to next page when required field not empty', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[data-dtestid="veteran-description"]').should(
+        'contain',
+        'Which of these best describes you?',
+      );
+    });
+    it('should show error if veteran description field is not selected', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[id="root_veteranDesc-error-message"]').should(
+        'contain',
+        'You must select one of the options',
+      );
+    });
+    it('should go to contact information page when required field is selceted', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[id="root_veteranDesc_0"]').click();
+      cy.get('[id="root_veteranDesc_0"]').should('be.checked');
+      cy.get('[class="usa-button-primary"]').click();
+    });
+  });
+  describe('Phone and email address', () => {
+    beforeEach(() => {
+      cy.get('[text="Start your application"]').click();
+      cy.get('[id="root_veteranFullName_first"]').type('John');
+      cy.get('[id="root_veteranFullName_last"]').type('Doe');
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[id="root_veteranDesc_0"]').click();
+      cy.get('[id="root_veteranDesc_0"]').should('be.checked');
+      cy.get('[class="usa-button-primary"]').click();
+    });
+    it('show required error if email field is empty', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[id="input-error-message"]').should(
+        'contain',
+        'Please enter an email address',
+      );
+    });
+    it('should show error when email address in not in the correct format', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[name="root_contactInfo_email"]')
+        .first()
+        .type('someEmail', { delay: 0, waitForAnimations: false });
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[id="input-error-message"]').should(
+        'contain',
+        'Enter a valid email address using the format email@domain.com. Your email address can only have letters, numbers, the @ symbol and a period, with no spaces.',
+      );
+    });
+    it('shoud show Country field when there is no errors with email', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[name="root_contactInfo_email"]')
+        .first()
+        .type('someEmail@mail.com', { delay: 0, waitForAnimations: false });
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[data-testid="country-field"]').should('exist');
+    });
+  });
+  describe('Country and State fields', () => {
+    beforeEach(() => {
+      cy.get('[text="Start your application"]').click();
+      cy.get('[id="root_veteranFullName_first"]').type('John');
+      cy.get('[id="root_veteranFullName_last"]').type('Doe');
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[id="root_veteranDesc_0"]').click();
+      cy.get('[id="root_veteranDesc_0"]').should('be.checked');
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[name="root_contactInfo_email"]')
+        .first()
+        .type('someEmail@mail.com', { delay: 0, waitForAnimations: false });
+      cy.get('[class="usa-button-primary"]').click();
+    });
+    it('should show error if country field is not selected', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[id="input-error-message"]').should(
+        'contain',
+        'You must select a country',
+      );
+    });
+    it('should show State filed in the next page when country field is selected to United States', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[name="root_country"]')
+        .first()
+        .select('United States');
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[data-testid="state-title"]').should(
+        'contain',
+        'What state do you live in?',
+      );
+    });
+    it('should show error if state field is not selected', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[name="root_country"]')
+        .first()
+        .select('United States');
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[id="input-error-message"]').should(
+        'contain',
+        'You must select a state',
+      );
+    });
+    it('should skip state field when country field is selected to Canada or other country', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[name="root_country"]')
+        .first()
+        .select('Canada');
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[data-testid="optional-demographic"]').should('exist');
+    });
+  });
+  describe('Other optional questions', () => {
+    beforeEach(() => {
+      cy.get('[text="Start your application"]').click();
+      cy.get('[id="root_veteranFullName_first"]').type('John');
+      cy.get('[id="root_veteranFullName_last"]').type('Doe');
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[id="root_veteranDesc_0"]').click();
+      cy.get('[id="root_veteranDesc_0"]').should('be.checked');
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[name="root_contactInfo_email"]')
+        .first()
+        .type('someEmail@mail.com', { delay: 0, waitForAnimations: false });
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[name="root_country"]')
+        .first()
+        .select('Canada');
+      cy.get('[class="usa-button-primary"]').click();
+    });
+    it('should skip demographic question to education question if no or nothing is selected', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[class="usa-button-primary"]').click();
+    });
+    it('should show the question about demographic if yes is selected', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[id="root_raceAndGender_0"]').click();
+      cy.get('[id="root_raceAndGender_0"]').should('be.checked');
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[ data-testid="ethnicity-and-race"]').should(
+        'contain',
+        'Your ethnicity and race',
+      );
+    });
+    it('should have demographic as not required', () => {
+      cy.injectAxeThenAxeCheck();
+      cy.get('[id="root_raceAndGender_0"]').click();
+      cy.get('[id="root_raceAndGender_0"]').should('be.checked');
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[class="usa-button-primary"]').click();
+      cy.get('[data-testid="gender"]').should(
+        'contain',
+        'How would you describe your gender?',
+      );
+    });
+  });
+});
