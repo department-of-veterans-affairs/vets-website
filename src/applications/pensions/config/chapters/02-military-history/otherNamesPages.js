2ac6b7b028ec86f1aa3dd616378488f2f2208dee
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2024-10-11 11:17:40 -0600

    Add text overrides for single field responses

--- src/applications/pensions/config/chapters/02-military-history/otherNamesPages.js
+++ src/applications/pensions/config/chapters/02-military-history/otherNamesPages.js
@@ -23,8 +23,6 @@ const options = {
         ? formatFullName(item.previousFullName)
         : undefined,
     summaryTitleWithoutItems: 'Other Service names',
-    deleteTitle: 'Delete previous name',
-    cancelEditTitle: 'Cancel editing this previous name',
   },
 };
 
