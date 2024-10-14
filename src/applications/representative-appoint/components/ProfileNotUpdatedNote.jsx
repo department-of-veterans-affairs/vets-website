f2e8531cae4a308476477e2b083805d6f9b5c2c2
Holden Hinkle <holden@holdenhinkle.com>
2024-10-14 15:28:37 -0400

    only show if logged in

--- src/applications/representative-appoint/components/ProfileNotUpdatedNote.jsx
+++ src/applications/representative-appoint/components/ProfileNotUpdatedNote.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
+import { isLoggedIn } from 'platform/user/selectors';
 
 export default function ProfileNotUpdatedNote({
   includePrefix = false,
@@ -10,29 +11,33 @@ export default function ProfileNotUpdatedNote({
 }) {
   return (
     <>
-      <p>
-        {includePrefix && <strong>Note: </strong>} This is the information we
-        have in your VA.gov profile. Any changes you make on this screen will
-        only affect this application.
-      </p>
-      {includePhone && (
+      {isLoggedIn && (
         <>
           <p>
-            If you need to change this information in your VA.gov profile, you
-            can call us at{' '}
-            <va-telephone contact={CONTACTS.VA_BENEFITS} extension={0} /> (
-            <va-telephone contact={CONTACTS['711']} tty />
-            ). We’re here 24/7.
+            {includePrefix && <strong>Note: </strong>} This is the information
+            we have in your VA.gov profile. Any changes you make on this screen
+            will only affect this application.
           </p>
-        </>
-      )}
-      {includeLink && (
-        <>
-          <va-link
-            href="https://va.gov"
-            text="Find out how to change you address in your VA.gov profile (opens in
+          {includePhone && (
+            <>
+              <p>
+                If you need to change this information in your VA.gov profile,
+                you can call us at{' '}
+                <va-telephone contact={CONTACTS.VA_BENEFITS} extension={0} /> (
+                <va-telephone contact={CONTACTS['711']} tty />
+                ). We’re here 24/7.
+              </p>
+            </>
+          )}
+          {includeLink && (
+            <>
+              <va-link
+                href="https://va.gov"
+                text="Find out how to change you address in your VA.gov profile (opens in
             new tab)"
-          />
+              />
+            </>
+          )}
         </>
       )}
     </>
