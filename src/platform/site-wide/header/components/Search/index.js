1eb5dec22f42ae5da612714e9c73b17484cafa78
Ian <harrison.ian.t@gmail.com>
2024-10-15 16:53:47 -0500

    swap variables

--- src/platform/site-wide/header/components/Search/index.js
+++ src/platform/site-wide/header/components/Search/index.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import * as Sentry from '@sentry/browser';
+import { apiRequest } from '~/platform/utilities/api';
 import recordEvent from '~/platform/monitoring/record-event';
 import { toggleValues } from '~/platform/site-wide/feature-toggles/selectors';
 import FEATURE_FLAG_NAMES from '~/platform/utilities/feature-toggles/featureFlagNames';
-import SearchDropdownComponent from './SearchDropdownComponent';
+import SearchDropdownComponent from '~/applications/search/components/SearchDropdown/SearchDropdownComponent';
 import { replaceWithStagingDomain } from '../../../../utilities/environment/stagingDomains';
 
 export const Search = ({ searchDropdownComponentEnabled }) => {
@@ -32,6 +34,40 @@ export const Search = ({ searchDropdownComponentEnabled }) => {
     });
 
     window.location.href = searchResultsPage;
+  };
+
+  // TA2.0
+  const fetchDropDownSuggestions = async inputValue => {
+    // encode user input for query to suggestions url
+    const encodedInput = encodeURIComponent(inputValue);
+
+    // fetch suggestions
+    try {
+      const apiRequestOptions = {
+        method: 'GET',
+      };
+      const fetchedSuggestions = await apiRequest(
+        `/search_typeahead?query=${encodedInput}`,
+        apiRequestOptions,
+      );
+
+      if (fetchedSuggestions.length !== 0) {
+        return fetchedSuggestions.sort((a, b) => {
+          return a.length - b.length;
+        });
+      }
+
+      return [];
+      // if we fail to fetch suggestions
+    } catch (error) {
+      if (error?.error?.code === 'OVER_RATE_LIMIT') {
+        Sentry.captureException(
+          new Error(`"OVER_RATE_LIMIT" - Search Typeahead`),
+        );
+      }
+      Sentry.captureException(error);
+    }
+    return [];
   };
 
   const onInputSubmit = componentState => {
@@ -126,6 +162,7 @@ export const Search = ({ searchDropdownComponentEnabled }) => {
         startingValue=""
         submitOnClick
         submitOnEnter
+        fetchSuggestions={fetchDropDownSuggestions}
         onInputSubmit={onInputSubmit}
         onSuggestionSubmit={onSuggestionSubmit}
       />
