bdc697c2b8bd762a7d39128ccc155a172f69835b
Isaac Gallup <igallup@thesocompany.com>
2024-10-15 14:37:27 -0500

    fixed shell script

--- script/github-actions/cypress-integration-test.sh
+++ script/github-actions/cypress-integration-test.sh
@@ -2,12 +2,6 @@
 
 integrationHelper() {
   local product="$1"
-  local app_path="src/applications/${product}"
-
-  if [[ ! -d "$app_path" ]]; then
-    echo "Error: Directory '${app_path}' does not exist."
-    exit 1
-  fi
 
   local slack_group
   slack_group=$(grep -A 3 "\"rootFolder\": \"$product\"" ./config/changed-apps-build.json | awk -F'"' '/slackGroup/ {print $4}')
