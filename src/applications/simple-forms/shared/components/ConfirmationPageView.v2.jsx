abfcecefd68e14f68118183724dadaea42e3442b
Jeana Clark <117098918+jeana-adhoc@users.noreply.github.com>
2024-10-15 13:19:18 -0500

    Add anchor link to My VA link on confirmation page (#32378)

--- src/applications/simple-forms/shared/components/ConfirmationPageView.v2.jsx
+++ src/applications/simple-forms/shared/components/ConfirmationPageView.v2.jsx
@@ -116,7 +116,7 @@ export const ConfirmationPageView = props => {
           confirmation number is ${confirmationNumber}.`}
         </p>
         <VaLinkAction
-          href="/my-va"
+          href="/my-va#benefit-applications"
           text="Check the status of your form on My VA"
           onClick={onCheckVaStatusClick}
         />
@@ -162,7 +162,10 @@ export const ConfirmationPageView = props => {
               update the status on My VA.
             </p>
             <p>
-              <a href="/my-va" onClick={onCheckVaStatusClick}>
+              <a
+                href="/my-va#benefit-applications"
+                onClick={onCheckVaStatusClick}
+              >
                 Check the status of your form on My VA
               </a>
             </p>
