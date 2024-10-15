ee38c3a3a08568bb303946c73179d1fd5a5032a7
Ian <harrison.ian.t@gmail.com>
2024-10-15 16:44:58 -0500

    swap variables

---
+++ src/applications/search/constants/index.js
@@ -0,0 +1,5 @@
+export const BASE_URI = 'https://search.usa.gov/api/v2/search';
+export const AFFILIATE = 'vets.gov_search';
+export const ACCESS_KEY = '_____YOUR ACCESS KEY HERE_____';
+export const UTF8 = '&#x2713';
+export const PAGE_SIZE = 20;
