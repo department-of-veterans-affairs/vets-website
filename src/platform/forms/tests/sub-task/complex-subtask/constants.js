c5ad4c39861c35db83b6fd554f993a17cd7b7cc8
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2024-10-16 13:48:10 -0400

    remove forms tests and see how stability review does

--- src/platform/forms/tests/sub-task/complex-subtask/constants.js
+++
@@ -1,9 +0,0 @@
-export const FORM_STATUS_BDD = 'formStatusBdd';
-
-export const SAVED_SEPARATION_DATE = 'savedSeparationDate';
-
-export const BDD_INFO_URL =
-  '/disability/how-to-file-claim/when-to-file/pre-discharge-claim/';
-
-export const DISABILITY_526_V2_ROOT_URL =
-  '/disability/file-disability-claim-form-21-526ez';
