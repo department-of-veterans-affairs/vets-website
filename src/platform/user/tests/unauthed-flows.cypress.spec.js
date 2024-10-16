0e42dc32c415438f0067c32852c74d5c37cc1ba5
taras <reloded2004@hotmail.com>
2024-10-16 16:16:38 -0400

    edm 242 url update cypress test file

--- src/platform/user/tests/unauthed-flows.cypress.spec.js
+++ src/platform/user/tests/unauthed-flows.cypress.spec.js
@@ -2,7 +2,7 @@ describe('Unauthed User Flow Test', () => {
   it('Provides the correct experience', () => {
     const appPaths = [
       // While the page is in maintenance, it doesn't need authed
-      '/education/gi-bill/post-9-11/ch-33-benefit/status',
+      '/education/check-post-9-11-gi-bill-benefits/status',
       '/records/download-va-letters/letters',
       '/track-claims',
     ];
