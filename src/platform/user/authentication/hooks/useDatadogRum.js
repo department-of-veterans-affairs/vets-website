24eb464fc177fba968e4baa9fa2fb206b1faf574
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2024-10-17 15:11:15 -0400

    slight tweak based on environment

--- src/platform/user/authentication/hooks/useDatadogRum.js
+++ src/platform/user/authentication/hooks/useDatadogRum.js
@@ -4,14 +4,21 @@ import { datadogRum } from '@datadog/browser-rum';
 import environment from 'platform/utilities/environment';
 
 const initDDRum = () => {
+  const env = environment.vspEnvironment();
+
+  const { sessionSampleRate, sessionReplaySampleRate } = {
+    vagovstaging: { sessionSampleRate: 20, sessionReplaySampleRate: 1 },
+    vagovprod: { sessionSampleRate: 100, sessionReplaySampleRate: 10 },
+  }[env];
+
   datadogRum.init({
     applicationId: '73e0e2fb-7b2a-4d4a-8231-35ef2123f607',
     clientToken: 'pub2dfeb9f2606a756df3ddd4bd5c8a6b3c',
     site: 'ddog-gov.com',
     service: 'identity',
-    env: environment.vspEnvironment(),
-    sessionSampleRate: 100,
-    sessionReplaySampleRate: 10,
+    env,
+    sessionSampleRate,
+    sessionReplaySampleRate,
     trackInteractions: true,
     trackFrustrations: true,
     trackResources: true,
@@ -26,7 +33,7 @@ export const useDatadogRum = () => {
     // 1. Prevents RUM from running on local/CI environments.
     // 2. Prevents re-initializing the SDK.
     if (
-      environment.BASE_URL.indexOf('localhost') < 0 &&
+      (environment.isStaging() || environment.isProd()) &&
       !window.DD_RUM?.getInitConfiguration() &&
       !window.Mocha
     ) {
