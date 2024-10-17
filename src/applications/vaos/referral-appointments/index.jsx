74f50a433e74a261b8bcbb5dfe5b85e59a059705
Brian Seek <brian.seek@civicactions.com>
2024-10-17 15:43:16 -0700

    redirect to appointments if feature is off

--- src/applications/vaos/referral-appointments/index.jsx
+++ src/applications/vaos/referral-appointments/index.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { Switch, Route, useRouteMatch, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import ScheduleReferral from './ScheduleReferral';
 import ConfirmApprovedPage from './ConfirmApprovedPage';
 import ChooseDateAndTime from './ChooseDateAndTime';
 import useManualScrollRestoration from '../hooks/useManualScrollRestoration';
+import { selectFeatureCCDirectScheduling } from '../redux/selectors';
 
 export default function ReferralAppointments() {
   useManualScrollRestoration();
   const basePath = useRouteMatch();
+  const featureCCDirectScheduling = useSelector(
+    selectFeatureCCDirectScheduling,
+  );
   return (
-    <Switch>
-      <Route path={`${basePath.url}/review`} component={ConfirmApprovedPage} />
-      <Route path={`${basePath.url}/date-time`} component={ChooseDateAndTime} />
-      <Route path={`${basePath.url}/:id`} component={ScheduleReferral} />
-    </Switch>
+    <>
+      {!featureCCDirectScheduling && <Redirect from={basePath.url} to="/" />}
+      <Switch>
+        <Route
+          path={`${basePath.url}/review`}
+          component={ConfirmApprovedPage}
+        />
+        <Route
+          path={`${basePath.url}/date-time`}
+          component={ChooseDateAndTime}
+        />
+        <Route path={`${basePath.url}/:id`} component={ScheduleReferral} />
+      </Switch>
+    </>
   );
 }
