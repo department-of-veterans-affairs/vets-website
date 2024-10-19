96413e7102f0e356447a748c03cf7bfbceb815db
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2024-10-19 04:05:47 -0400

    sort enrollemnt by month order

--- src/applications/verify-your-enrollment/components/DGIBEnrollmentCard.jsx
+++ src/applications/verify-your-enrollment/components/DGIBEnrollmentCard.jsx
@@ -22,26 +22,27 @@ const DGIBEnrollmentCard = ({
       const myUUID = uuidv4();
       return (
         <div key={`Enrollment-to-be-verified-${myUUID}`}>
-          {isVerificationEndDateValid(enrollment.verificationEndDate) && (
-            <div>
-              <div className="vads-u-margin-top--3">
-                <h2 className="vye-highlighted-title-container">
-                  {translateDateIntoMonthYearFormat(
-                    enrollment?.verificationBeginDate,
-                  )}
-                </h2>
-              </div>
-              <div className="vye-highlighted-content-container">
-                {!confirmationPage &&
-                  `This is the enrollment information we have on file for you for ${translateDateIntoMonthYearFormat(
-                    enrollment?.verificationBeginDate,
-                  )}.`}
-                <div key={`period-${index}`}>
-                  {getPeriodsToVerify2(enrollmentVerifications)[index]}
+          {isVerificationEndDateValid(enrollment.verificationEndDate) &&
+            !enrollment.verificationMethod && (
+              <div>
+                <div className="vads-u-margin-top--3">
+                  <h2 className="vye-highlighted-title-container">
+                    {translateDateIntoMonthYearFormat(
+                      enrollment?.verificationBeginDate,
+                    )}
+                  </h2>
+                </div>
+                <div className="vye-highlighted-content-container">
+                  {!confirmationPage &&
+                    `This is the enrollment information we have on file for you for ${translateDateIntoMonthYearFormat(
+                      enrollment?.verificationBeginDate,
+                    )}.`}
+                  <div key={`period-${index}`}>
+                    {getPeriodsToVerify2(enrollmentVerifications)[index]}
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
         </div>
       );
     });
