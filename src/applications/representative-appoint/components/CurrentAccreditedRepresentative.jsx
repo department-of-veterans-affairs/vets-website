a8e6c34ee339c6b46e3f912c30f7cad001717515
Holden Hinkle <holden@holdenhinkle.com>
2024-10-21 12:24:28 -0400

    refactor CurrentAccreditedRepresentative

--- src/applications/representative-appoint/components/CurrentAccreditedRepresentative.jsx
+++ src/applications/representative-appoint/components/CurrentAccreditedRepresentative.jsx
@@ -1,37 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import Email from './Email';
+import Phone from './Phone';
+import GoogleMapLink from './GoogleMapLink';
+import { addressExists, getRepType } from '../utilities/helpers';
 import { parsePhoneNumber } from '../utilities/parsePhoneNumber';
 
-const CurrentAccreditedRepresentative = ({
-  representativeName,
-  type,
-  addressLine1,
-  addressLine2,
-  addressLine3,
-  city,
-  stateCode,
-  zipCode,
-  phone,
-  email,
-}) => {
-  const { contact, extension } = parsePhoneNumber(phone);
+const CurrentAccreditedRepresentative = ({ currentRep }) => {
+  const repAttributes = currentRep?.attributes;
+  const repName = repAttributes?.fullName;
+  const { contact, extension } = parsePhoneNumber(repAttributes?.phone);
+  const email = repAttributes?.email;
 
-  const addressExists = addressLine1 || city || stateCode || zipCode;
+  const recordContactLinkClick = () => {
+    // pending analytics event
+  };
 
-  const address =
-    [
-      (addressLine1 || '').trim(),
-      (addressLine2 || '').trim(),
-      (addressLine3 || '').trim(),
-    ]
-      .filter(Boolean)
-      .join(' ') +
-    (city ? ` ${city},` : '') +
-    (stateCode ? ` ${stateCode}` : '') +
-    (zipCode ? ` ${zipCode}` : '');
-
-  const parseRepType = repType => {
+  const parseRepType = () => {
+    const repType = getRepType(currentRep);
     const parsedRep = {};
+
     switch (repType) {
       case 'organization':
         parsedRep.title = 'Veterans Service Organization (VSO)';
@@ -49,6 +38,7 @@ const CurrentAccreditedRepresentative = ({
         parsedRep.title = 'accredited representative';
         parsedRep.subTitle = 'Accredited representative';
     }
+
     return parsedRep;
   };
 
@@ -57,19 +47,14 @@ const CurrentAccreditedRepresentative = ({
       <div className="representative-result-card-content">
         <div className="representative-info-heading">
           <h3 className="vads-u-margin-y--0">
-            Your current {parseRepType(type).title}
+            Your current {parseRepType().title}
           </h3>
-          {representativeName && (
+          {repName && (
             <>
               <h4 className="vads-u-font-family--serif vads-u-margin-top--1p5 vads-u-margin-bottom--0p5">
-                {representativeName}
+                {repName}
               </h4>
-              <p className="vads-u-margin-y--0">
-                {parseRepType(type).subTitle}
-              </p>
-              {/* {accreditedOrganizations?.length === 1 && (
-                <p style={{ marginTop: 0 }}>{accreditedOrganizations[0]}</p>
-              )} */}
+              <p className="vads-u-margin-y--0">{parseRepType().subTitle}</p>
             </>
           )}
         </div>
@@ -77,55 +62,22 @@ const CurrentAccreditedRepresentative = ({
           <strong>Note:</strong> You can work with any accredited VSO
           representative at this organization.
         </p>
-
         <div className="representative-contact-section vads-u-margin-top--3">
-          {addressExists && (
-            <div className="address-link vads-u-display--flex">
-              <va-icon icon="location_on" size="3" />
-              <a
-                href={`https://maps.google.com?&daddr=${address}`}
-                tabIndex="0"
-                className="address-anchor vads-u-margin-left--1"
-                // onClick={() => recordContactLinkClick()}
-                target="_blank"
-                rel="noreferrer"
-                aria-label={`${address} (opens in a new tab)`}
-              >
-                {addressLine1}{' '}
-                {addressLine2 ? (
-                  <>
-                    <br /> {addressLine2}
-                  </>
-                ) : null}{' '}
-                <br />
-                {city}, {stateCode} {zipCode}
-              </a>
-            </div>
-          )}
-          {contact && (
-            <div className="vads-u-margin-top--1p5 vads-u-display--flex">
-              <va-icon icon="phone" size="3" />
-              <div className="vads-u-margin-left--1">
-                <va-telephone
-                  contact={contact}
-                  extension={extension}
-                  //   onClick={() => recordContactLinkClick()}
-                  disable-analytics
-                />
-              </div>
-            </div>
+          {addressExists(currentRep) && (
+            <GoogleMapLink
+              addressData={currentRep}
+              recordClick={recordContactLinkClick}
+            />
           )}
           {email && (
-            <div className="vads-u-margin-top--1p5 vads-u-display--flex">
-              <va-icon icon="mail" size="3" />
-              <a
-                href={`mailto:${email}`}
-                // onClick={() => recordContactLinkClick()}
-                className="vads-u-margin-left--1"
-              >
-                {email}
-              </a>
-            </div>
+            <Email email={email} recordClick={recordContactLinkClick} />
+          )}
+          {contact && (
+            <Phone
+              contact={contact}
+              extension={extension}
+              recordClick={recordContactLinkClick}
+            />
           )}
         </div>
       </div>
@@ -134,18 +86,7 @@ const CurrentAccreditedRepresentative = ({
 };
 
 CurrentAccreditedRepresentative.propTypes = {
-  addressLine1: PropTypes.string,
-  addressLine2: PropTypes.string,
-  addressLine3: PropTypes.string,
-  accreditedOrganizations: PropTypes.array,
-  city: PropTypes.string,
-  email: PropTypes.string,
-  representativeName: PropTypes.string,
-  phone: PropTypes.string,
-  stateCode: PropTypes.string,
-  type: PropTypes.string,
-  zipCode: PropTypes.string,
-  setFormData: PropTypes.func.isRequired,
+  currentRep: PropTypes.object,
 };
 
 export default CurrentAccreditedRepresentative;
