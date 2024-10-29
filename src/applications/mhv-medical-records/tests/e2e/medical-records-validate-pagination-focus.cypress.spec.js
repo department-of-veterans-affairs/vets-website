71bcd6eaec7340b9af82c8dd961e434ce8e8cb50
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2024-10-29 07:13:58 -0700

    pagination focus test

---
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-validate-pagination-focus.cypress.spec.js
@@ -0,0 +1,33 @@
+// import moment from 'moment';
+import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
+import VitalsListPage from './pages/VitalsListPage';
+// import vitals from '../fixtures/vitals.json';
+// import defaultVitals from '../fixtures/vitals.json';
+
+describe('Medical Records View Vitals', () => {
+  const site = new MedicalRecordsSite();
+
+  beforeEach(() => {
+    site.login();
+    cy.visit('my-health/medical-records');
+  });
+
+  it('Visits View Vitals List', () => {
+    VitalsListPage.goToVitals();
+
+    // VitalsListPage.verifyVitalOnListPage(
+    //   0,
+    //   'Blood pressure',
+    //   `${defaultVitals.entry[0].resource.component[0].valueQuantity.value}/${
+    //     defaultVitals.entry[0].resource.component[1].valueQuantity.value
+    //   }`,
+    //   moment
+    //     .parseZone(defaultVitals.entry[0].resource.effectiveDateTime)
+    //     .format('MMMM D, YYYY, h:mm'),
+    // );
+
+    // Axe check
+    cy.injectAxe();
+    cy.axeCheck('main');
+  });
+});
