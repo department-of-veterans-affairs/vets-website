f313901c27e1b143be492fdfe103274847aa7bd8
Brad Bergeron <bergeron.bradley@gmail.com>
2024-10-29 17:36:04 -0400

    Removes commented code, simplifies rendering, passing default date

--- src/applications/survivor-dependent-education-benefit/22-5490/containers/ConfirmationPage.jsx
+++ src/applications/survivor-dependent-education-benefit/22-5490/containers/ConfirmationPage.jsx
@@ -4,33 +4,16 @@ import PropTypes from 'prop-types';
 import scrollToTop from 'platform/utilities/ui/scrollToTop';
 import { focusElement } from 'platform/utilities/ui';
 import UnderReviewConfirmation from '../components/confirmation/UnderReviewConfirmation';
-// import { fetchClaimStatus } from '../actions';
 
 const ConfirmationPage = ({ form }) => {
-  // const [apiError, setApiError] = useState(false); // Track if a 400 or 500 error happens
   const { formId, data } = form;
   const { fullName } = data;
-  let newReceivedDate;
-
-  // // Fetch claim status if not already available
-  // useEffect(
-  //   () => {
-  //     if (!claimStatus && !apiError) {
-  //       getClaimStatus('Chapter35')
-  //         .then(response => {
-  //           // Only trigger error if response has a status code >= 400
-  //           if (response?.status >= 400) {
-  //             setApiError(true); // Set error if the response status indicates a failure
-  //           }
-  //         })
-  //         .catch(() => {
-  //           // Catch any network errors and set error to true
-  //           setApiError(true);
-  //         });
-  //     }
-  //   },
-  //   [getClaimStatus, claimStatus, apiError],
-  // );
+  // Set a default received date to today's date if claimStatus is not available
+  const newReceivedDate = new Date().toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
 
   // Set up scroll and focus when the component mounts
   useEffect(() => {
@@ -43,39 +26,13 @@ const ConfirmationPage = ({ form }) => {
     window.print();
   }, []);
 
-  // // Handle API errors
-  // if (apiError) {
-  //   return (
-  //     <UnderReviewConfirmation
-  //       user={`${fullName.first} ${fullName.middle || ''} ${
-  //         fullName.last
-  //       } ${fullName.suffix || ''}`}
-  //       dateReceived={newReceivedDate}
-  //       formId={formId}
-  //       printPage={printPage}
-  //     />
-  //   );
-  // }
-
-  // // Show loading indicator while waiting for claim status
-  // if (!claimStatus) {
-  //   return (
-  //     <va-loading-indicator
-  //       class="vads-u-margin-y--5"
-  //       label="Loading"
-  //       message="Loading your results..."
-  //       set-focus
-  //     />
-  //   );
-  // }
-
-  // Render the UnderReviewConfirmation component once the claim status is available
+  // Render the UnderReviewConfirmation component with the default date
   return (
     <UnderReviewConfirmation
       user={`${fullName.first} ${fullName.middle || ''} ${
         fullName.last
       } ${fullName.suffix || ''}`}
-      dateReceived={newReceivedDate}
+      dateReceived={newReceivedDate} // Pass the default date
       formId={formId}
       printPage={printPage}
     />
@@ -83,20 +40,10 @@ const ConfirmationPage = ({ form }) => {
 };
 
 const mapStateToProps = state => ({
-  // claimStatus: state?.data?.claimStatus,
   form: state.form,
 });
 
-// const mapDispatchToProps = {
-//   getClaimStatus: fetchClaimStatus,
-// };
-
 ConfirmationPage.propTypes = {
-  // getClaimStatus: PropTypes.func.isRequired,
-  // claimStatus: PropTypes.shape({
-  //   claimStatus: PropTypes.string,
-  //   receivedDate: PropTypes.string,
-  // }),
   form: PropTypes.shape({
     data: PropTypes.shape({
       fullName: PropTypes.shape({
@@ -110,7 +57,4 @@ ConfirmationPage.propTypes = {
   }),
 };
 
-export default connect(
-  mapStateToProps,
-  // mapDispatchToProps,
-)(ConfirmationPage);
+export default connect(mapStateToProps)(ConfirmationPage);
