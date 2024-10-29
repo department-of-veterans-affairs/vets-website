d63bd162f42e5ff205af7e408903f010cdb23037
Brad Bergeron <bergeron.bradley@gmail.com>
2024-10-29 18:04:22 -0400

    Uses chosenBenefit Logic to display specific confirmation component

--- src/applications/survivor-dependent-education-benefit/22-5490/containers/ConfirmationPage.jsx
+++ src/applications/survivor-dependent-education-benefit/22-5490/containers/ConfirmationPage.jsx
@@ -3,11 +3,13 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import scrollToTop from 'platform/utilities/ui/scrollToTop';
 import { focusElement } from 'platform/utilities/ui';
-import UnderReviewConfirmation from '../components/confirmation/UnderReviewConfirmation';
+import UnderReviewConfirmationFry from '../components/confirmation/UnderReviewConfirmationFry';
+import UnderReviewConfirmationDEAChapter35 from '../components/confirmation/UnderReviewConfirmationDEAChapter35';
 
 const ConfirmationPage = ({ form }) => {
   const { formId, data } = form;
-  const { fullName } = data;
+  const { fullName, chosenBenefit } = data;
+
   // Set a default received date to today's date if claimStatus is not available
   const newReceivedDate = new Date().toLocaleDateString('en-US', {
     month: 'long',
@@ -26,13 +28,40 @@ const ConfirmationPage = ({ form }) => {
     window.print();
   }, []);
 
-  // Render the UnderReviewConfirmation component with the default date
+  // Render the appropriate component based on the chosenBenefit value
+  if (chosenBenefit === 'fry') {
+    return (
+      <UnderReviewConfirmationFry
+        user={`${fullName.first} ${fullName.middle || ''} ${
+          fullName.last
+        } ${fullName.suffix || ''}`}
+        dateReceived={newReceivedDate}
+        formId={formId}
+        printPage={printPage}
+      />
+    );
+  }
+
+  if (chosenBenefit === 'dea') {
+    return (
+      <UnderReviewConfirmationDEAChapter35
+        user={`${fullName.first} ${fullName.middle || ''} ${
+          fullName.last
+        } ${fullName.suffix || ''}`}
+        dateReceived={newReceivedDate}
+        formId={formId}
+        printPage={printPage}
+      />
+    );
+  }
+
+  // Default to showing the Fry confirmation if no matching benefit type is found
   return (
-    <UnderReviewConfirmation
+    <UnderReviewConfirmationFry
       user={`${fullName.first} ${fullName.middle || ''} ${
         fullName.last
       } ${fullName.suffix || ''}`}
-      dateReceived={newReceivedDate} // Pass the default date
+      dateReceived={newReceivedDate}
       formId={formId}
       printPage={printPage}
     />
@@ -52,6 +81,7 @@ ConfirmationPage.propTypes = {
         last: PropTypes.string,
         suffix: PropTypes.string,
       }),
+      chosenBenefit: PropTypes.string, // Prop for chosen benefit
     }),
     formId: PropTypes.string,
   }),
