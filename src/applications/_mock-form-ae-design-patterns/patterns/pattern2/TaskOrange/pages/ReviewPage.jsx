98cf89a90913eb9f4680914849e9d287e78b98d8
Adam Whitlock <8332986+adamwhitlock1@users.noreply.github.com>
2024-10-29 07:55:10 -0600

    revert revert... oops

--- src/applications/_mock-form-ae-design-patterns/patterns/pattern2/TaskOrange/pages/ReviewPage.jsx
+++ src/applications/_mock-form-ae-design-patterns/patterns/pattern2/TaskOrange/pages/ReviewPage.jsx
@@ -10,6 +10,7 @@ import { setupPages } from '../utils/reviewPage';
 import { formConfigForOrangeTask } from '../config/form';
 
 const ReviewPage = props => {
+  const { location } = props;
   const [privacyCheckbox, setPrivacyCheckbox] = useState(false);
   const [submitted, setSubmitted] = useState(false);
 
@@ -60,7 +61,9 @@ const ReviewPage = props => {
               </div>
 
               {getChapterPagesFromChapterIndex(index).map(page => {
-                const depends = page.depends ? page.depends(props.data) : true;
+                const depends = page.depends
+                  ? page.depends({ ...props.data, location })
+                  : true;
                 return page.review && depends
                   ? Object.entries(page.review(props)).map(([label, value]) => (
                       <div className="page-value" key={label}>
