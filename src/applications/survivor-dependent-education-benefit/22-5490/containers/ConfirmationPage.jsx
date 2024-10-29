febbf71254ee6d05c21b82e089adb027ba8c9d61
Brad Bergeron <bergeron.bradley@gmail.com>
2024-10-29 17:17:18 -0400

    Removes claim status polling

--- src/applications/survivor-dependent-education-benefit/22-5490/containers/ConfirmationPage.jsx
+++ src/applications/survivor-dependent-education-benefit/22-5490/containers/ConfirmationPage.jsx
@@ -1,36 +1,36 @@
-import React, { useEffect, useCallback, useState } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import scrollToTop from 'platform/utilities/ui/scrollToTop';
 import { focusElement } from 'platform/utilities/ui';
 import UnderReviewConfirmation from '../components/confirmation/UnderReviewConfirmation';
-import { fetchClaimStatus } from '../actions';
+// import { fetchClaimStatus } from '../actions';
 
-const ConfirmationPage = ({ form, getClaimStatus, claimStatus }) => {
-  const [apiError, setApiError] = useState(false); // Track if a 400 or 500 error happens
+const ConfirmationPage = ({ form }) => {
+  // const [apiError, setApiError] = useState(false); // Track if a 400 or 500 error happens
   const { formId, data } = form;
   const { fullName } = data;
   let newReceivedDate;
 
-  // Fetch claim status if not already available
-  useEffect(
-    () => {
-      if (!claimStatus && !apiError) {
-        getClaimStatus('Chapter35')
-          .then(response => {
-            // Only trigger error if response has a status code >= 400
-            if (response?.status >= 400) {
-              setApiError(true); // Set error if the response status indicates a failure
-            }
-          })
-          .catch(() => {
-            // Catch any network errors and set error to true
-            setApiError(true);
-          });
-      }
-    },
-    [getClaimStatus, claimStatus, apiError],
-  );
+  // // Fetch claim status if not already available
+  // useEffect(
+  //   () => {
+  //     if (!claimStatus && !apiError) {
+  //       getClaimStatus('Chapter35')
+  //         .then(response => {
+  //           // Only trigger error if response has a status code >= 400
+  //           if (response?.status >= 400) {
+  //             setApiError(true); // Set error if the response status indicates a failure
+  //           }
+  //         })
+  //         .catch(() => {
+  //           // Catch any network errors and set error to true
+  //           setApiError(true);
+  //         });
+  //     }
+  //   },
+  //   [getClaimStatus, claimStatus, apiError],
+  // );
 
   // Set up scroll and focus when the component mounts
   useEffect(() => {
@@ -43,31 +43,31 @@ const ConfirmationPage = ({ form, getClaimStatus, claimStatus }) => {
     window.print();
   }, []);
 
-  // Handle API errors
-  if (apiError) {
-    return (
-      <UnderReviewConfirmation
-        user={`${fullName.first} ${fullName.middle || ''} ${
-          fullName.last
-        } ${fullName.suffix || ''}`}
-        dateReceived={newReceivedDate}
-        formId={formId}
-        printPage={printPage}
-      />
-    );
-  }
+  // // Handle API errors
+  // if (apiError) {
+  //   return (
+  //     <UnderReviewConfirmation
+  //       user={`${fullName.first} ${fullName.middle || ''} ${
+  //         fullName.last
+  //       } ${fullName.suffix || ''}`}
+  //       dateReceived={newReceivedDate}
+  //       formId={formId}
+  //       printPage={printPage}
+  //     />
+  //   );
+  // }
 
-  // Show loading indicator while waiting for claim status
-  if (!claimStatus) {
-    return (
-      <va-loading-indicator
-        class="vads-u-margin-y--5"
-        label="Loading"
-        message="Loading your results..."
-        set-focus
-      />
-    );
-  }
+  // // Show loading indicator while waiting for claim status
+  // if (!claimStatus) {
+  //   return (
+  //     <va-loading-indicator
+  //       class="vads-u-margin-y--5"
+  //       label="Loading"
+  //       message="Loading your results..."
+  //       set-focus
+  //     />
+  //   );
+  // }
 
   // Render the UnderReviewConfirmation component once the claim status is available
   return (
@@ -83,20 +83,20 @@ const ConfirmationPage = ({ form, getClaimStatus, claimStatus }) => {
 };
 
 const mapStateToProps = state => ({
-  claimStatus: state?.data?.claimStatus,
+  // claimStatus: state?.data?.claimStatus,
   form: state.form,
 });
 
-const mapDispatchToProps = {
-  getClaimStatus: fetchClaimStatus,
-};
+// const mapDispatchToProps = {
+//   getClaimStatus: fetchClaimStatus,
+// };
 
 ConfirmationPage.propTypes = {
-  getClaimStatus: PropTypes.func.isRequired,
-  claimStatus: PropTypes.shape({
-    claimStatus: PropTypes.string,
-    receivedDate: PropTypes.string,
-  }),
+  // getClaimStatus: PropTypes.func.isRequired,
+  // claimStatus: PropTypes.shape({
+  //   claimStatus: PropTypes.string,
+  //   receivedDate: PropTypes.string,
+  // }),
   form: PropTypes.shape({
     data: PropTypes.shape({
       fullName: PropTypes.shape({
@@ -112,5 +112,5 @@ ConfirmationPage.propTypes = {
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  // mapDispatchToProps,
 )(ConfirmationPage);
