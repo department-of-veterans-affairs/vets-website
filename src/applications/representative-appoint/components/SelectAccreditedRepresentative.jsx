021136f1aeb436cfbcca3c76b0f9ea66eaf85453
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-10-30 17:58:54 -0400

    Copy update

--- src/applications/representative-appoint/components/SelectAccreditedRepresentative.jsx
+++ src/applications/representative-appoint/components/SelectAccreditedRepresentative.jsx
@@ -167,7 +167,7 @@ const SelectAccreditedRepresentative = props => {
       </p>
       <va-link
         href="/get-help-from-accredited-representative/find-rep"
-        text="Find an accredited representative or VSO"
+        text="Find a VA accredited representative or VSO (opens in new tab)"
       />
       <FormNavButtons goBack={handleGoBack} goForward={handleGoForward} />
     </div>
