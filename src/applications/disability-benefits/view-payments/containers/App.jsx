0f685914cfdb7fe994d99e3f0cdde4a2f85bad2a
Jane Cho <87551323+janechodance@users.noreply.github.com>
2024-10-30 11:46:52 -0400

    focus when payament history page load

--- src/applications/disability-benefits/view-payments/containers/App.jsx
+++ src/applications/disability-benefits/view-payments/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -7,10 +7,16 @@ import DowntimeNotification, {
   externalServices,
 } from 'platform/monitoring/DowntimeNotification';
 import backendServices from 'platform/user/profile/constants/backendServices';
-
 import ViewPaymentsLists from '../components/view-payments-lists/ViewPaymentsLists';
+import { setFocus } from '../utils';
 
 function ViewPaymentsApp(props) {
+  useEffect(() => {
+    setTimeout(() => {
+      setFocus('.your-va-payments-header');
+    }, 500);
+  }, []);
+
   return (
     <RequiredLoginView
       serviceRequired={backendServices.USER_PROFILE}
@@ -30,7 +36,9 @@ function ViewPaymentsApp(props) {
           <div className="vads-l-grid-container vads-u-padding--0">
             <div className="vads-l-row">
               <div className="vads-l-col--12 medium-screen:vads-l-col--12 large-screen:vads-l-col--12 vads-u-padding--1p5 desktop-lg:vads-u-padding--0">
-                <h1>Your VA payments</h1>
+                <h1 className="your-va-payments-header" tabIndex="-1">
+                  Your VA payments
+                </h1>
                 <ViewPaymentsLists />
               </div>
             </div>
