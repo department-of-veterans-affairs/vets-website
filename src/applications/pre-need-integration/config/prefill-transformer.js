626e22b0a1991e4c489aa43a6ccb6b8ee6030ac0
jordanBoardmanCodes <thomas.boardman@va.gov>
2024-10-30 14:50:01 -0400

    MBMS-58075 got the prefill working in preparer flow. just need to double check payload in the review section

--- src/applications/pre-need-integration/config/prefill-transformer.js
+++ src/applications/pre-need-integration/config/prefill-transformer.js
@@ -8,12 +8,25 @@ export default function prefillTransformer(pages, formData, metadata) {
             first: formData.application.claimant.name.first,
             last: formData.application.claimant.name.last,
           },
-          //   street: formData.application.claimant.mailingAddress.street,
+          'view:applicantInfo': {
+            mailingAddress: {
+              street: formData.application.claimant.address.street,
+              city: formData.application.claimant.address.city,
+              state: formData.application.claimant.address.state,
+              postalCode: formData.application.claimant.address.postalCode,
+            },
+          },
         },
         claimant: {
           name: {
             first: formData.application.claimant.name.first,
             last: formData.application.claimant.name.last,
+          },
+          address: {
+            street: formData.application.claimant.address.street,
+            city: formData.application.claimant.address.city,
+            state: formData.application.claimant.address.state,
+            postalCode: formData.application.claimant.address.postalCode,
           },
         },
       },
