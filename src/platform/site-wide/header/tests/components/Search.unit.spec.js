f19dcac43cfe3f43d90e3c520fa469c2f3b52ef5
Chris Kim <chriskim@Chriss-MacBook-Pro-2.local>
2024-10-29 22:52:57 -0700

    update tests

--- src/platform/site-wide/header/tests/components/Search.unit.spec.js
+++ src/platform/site-wide/header/tests/components/Search.unit.spec.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import { Search } from '../../components/Search';
+import Search from '../../components/Search';
 
 describe('Header <Search>', () => {
   it('renders correct content with no props', () => {
     const wrapper = shallow(<Search />);
-
-    expect(wrapper.find('form')).to.have.length(1);
-    expect(wrapper.find('label')).to.have.length(1);
-    expect(wrapper.find('input[type="text"]')).to.have.length(1);
-    expect(wrapper.find('button[type="submit"]')).to.have.length(1);
-
+    expect(wrapper.find('#search-header-dropdown')).to.have.length(1);
     wrapper.unmount();
   });
 });
