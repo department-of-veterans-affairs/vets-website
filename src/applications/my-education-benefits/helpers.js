d539748d3dac230f957db6266f531d8e10d915f9
s-caso <sebastian.caso@accenturefederal.com>
2024-10-31 20:58:20 -0400

    add new contact info page for when flag is on

--- src/applications/my-education-benefits/helpers.js
+++ src/applications/my-education-benefits/helpers.js
@@ -849,6 +849,7 @@ export const formPages = {
     mailingAddress: 'mailingAddress',
     mailingAddressMilitaryBaseUpdates: 'mailingAddressMilitaryBaseUpdates',
     preferredContactMethod: 'preferredContactMethod',
+    newPreferredContactMethod: 'newPreferredContactMethod',
   },
   serviceHistory: 'serviceHistory',
   benefitSelectionLegacy: 'benefitSelectionLegacy',
