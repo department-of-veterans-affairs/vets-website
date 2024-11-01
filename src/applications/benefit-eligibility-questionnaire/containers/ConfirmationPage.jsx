6866083297f0ac745361717c0a84cf362adb8961
Ian <ianseabock@gmail.com>
2024-11-01 08:09:19 -0700

    break out Heading

--- src/applications/benefit-eligibility-questionnaire/containers/ConfirmationPage.jsx
+++ src/applications/benefit-eligibility-questionnaire/containers/ConfirmationPage.jsx
@@ -13,6 +13,7 @@ import BenefitCard from '../components/BenefitCard';
 import GetFormHelp from '../components/GetFormHelp';
 import SaveResultsModal from '../components/SaveResultsModal';
 import { BENEFITS_LIST } from '../constants/benefits';
+import { Heading } from '../components/Heading';
 
 export class ConfirmationPage extends React.Component {
   sortBenefitObj(benefitObj, sortKey) {
@@ -242,25 +243,7 @@ export class ConfirmationPage extends React.Component {
     return (
       <div>
         <article>
-          <div role="heading" aria-level="2">
-            <p>
-              <b>
-                Note: This tool is not an application for VA benefits and it
-                doesn't determine your eligibility for benefits.
-              </b>{' '}
-              After you use this tool, you can learn more about eligibility and
-              how to apply.
-            </p>
-            <p>
-              To find VA benefits that may be relevant for you, answer a few
-              questions about your goals and experiences.
-            </p>
-            <p>
-              This is our first version. Right now, this tool focuses on
-              education and career benefits. We'll add more types of benefits
-              soon.
-            </p>
-          </div>
+          <Heading />
         </article>
 
         <h2 className="vads-u-font-size--h3">Benefits to explore</h2>
