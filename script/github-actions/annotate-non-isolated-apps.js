6087e1b30457f7d3be2c002dd392ec169e262d82
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2024-11-04 16:37:22 -0500

    test isolation app annotations

--- script/github-actions/annotate-non-isolated-apps.js
+++ script/github-actions/annotate-non-isolated-apps.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 /* eslint-disable camelcase */
 
-// const core = require('@actions/core');
+const core = require('@actions/core');
 // const fs = require('fs');
 // const path = require('path');
 
@@ -10,13 +10,7 @@ const APPS_NOT_ISOLATED = process.env.APPS_NOT_ISOLATED
   : [];
 const CHANGED_FILES = process.env.CHANGED_FILES
   ? process.env.CHANGED_FILES.split(' ')
-  : // .map(filePath =>
-    //     filePath
-    //       .split('/')
-    //       .slice(0, 3)
-    //       .join('/'),
-    //   )
-    [];
+  : [];
 console.log('apps not isolated: ', APPS_NOT_ISOLATED);
 console.log('changed apps: ', CHANGED_FILES);
 
@@ -34,24 +28,24 @@ const matchingFiles = CHANGED_FILES.filter(filePath =>
 console.log('apps not isolated being updated: ', matchingApps);
 console.log('files belonging to apps not isolated: ', matchingFiles);
 
-// if (APPS_NOT_ISOLATED.length > 0) {
-//   const annotationsJson = TESTS_BLOCKING_MERGE.map(spec => {
-//     return {
-//       path: spec,
-//       start_line: 1,
-//       end_line: 1,
-//       title: `${mergeCheckType} Test Stability Allow List Merge Blocked`,
-//       message: `*MERGE BLOCK NOTICE* This PR contains changes related to this test spec which has been disabled for flakiness.
-//                 \n As of Nov 6th, 2023, merging is blocked for PRs in products that have flaky Unit/E2E tests associated with them.
-//                 \n Please resolve these tests to remove this blocker.
-//                 \n More information is available at: https://depo-platform-documentation.scrollhelp.site/developer-docs/test-stability-review`,
-//       annotation_level: 'failure',
-//     };
-//   });
-//   core.exportVariable(
-//     `ISOLATION_ANNOTATIONS_JSON`,
-//     JSON.stringify(annotationsJson),
-//   );
-// } else {
-//   core.exportVariable(`ISOLATION_ANNOTATIONS_JSON`, JSON.stringify([]));
-// }
+if (APPS_NOT_ISOLATED.length > 0) {
+  const annotationsJson = matchingFiles.map(filePath => {
+    return {
+      path: filePath,
+      start_line: 1,
+      end_line: 1,
+      title: `Staged Continuous Deployment App Isolation Conflict`,
+      message: `*WARNING* This PR contains changes related to an application that is currently not isolated.
+                \n As of Feb 3, 2025 deployment may no longer be possible for apps that are not isolated.
+                \n Please isolate this app from other directories in 'src/applications' to prevent future deployment issues.
+                \n More information will be forthcoming`,
+      annotation_level: 'warning',
+    };
+  });
+  core.exportVariable(
+    `ISOLATION_ANNOTATIONS_JSON`,
+    JSON.stringify(annotationsJson),
+  );
+} else {
+  core.exportVariable(`ISOLATION_ANNOTATIONS_JSON`, JSON.stringify([]));
+}
