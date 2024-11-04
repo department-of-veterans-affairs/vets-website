6055178901bf31325c9891790db0b8bdab3b9463
Curt Bonade <cbonade@Curts-MacBook-Pro.local>
2024-11-04 16:52:47 -0500

    Log annotations json

--- script/github-actions/annotate-non-isolated-apps.js
+++ script/github-actions/annotate-non-isolated-apps.js
@@ -11,22 +11,10 @@ const APPS_NOT_ISOLATED = process.env.APPS_NOT_ISOLATED
 const CHANGED_FILES = process.env.CHANGED_FILES
   ? process.env.CHANGED_FILES.split(' ')
   : [];
-console.log('apps not isolated: ', APPS_NOT_ISOLATED);
-console.log('changed apps: ', CHANGED_FILES);
-
-const matchingApps = APPS_NOT_ISOLATED.filter(app =>
-  CHANGED_FILES.some(filePath => filePath.includes(app)),
-);
 
 const matchingFiles = CHANGED_FILES.filter(filePath =>
   APPS_NOT_ISOLATED.some(app => filePath.includes(app)),
 );
-
-// if (matchingApps && matchingApps.length > 0) {
-// }
-
-console.log('apps not isolated being updated: ', matchingApps);
-console.log('files belonging to apps not isolated: ', matchingFiles);
 
 if (APPS_NOT_ISOLATED.length > 0) {
   const annotationsJson = matchingFiles.map(filePath => {
@@ -38,10 +26,12 @@ if (APPS_NOT_ISOLATED.length > 0) {
       message: `*WARNING* This PR contains changes related to an application that is currently not isolated.
                 \n As of Feb 3, 2025 deployment may no longer be possible for apps that are not isolated.
                 \n Please isolate this app from other directories in 'src/applications' to prevent future deployment issues.
-                \n More information will be forthcoming`,
+                \n More information on your app's status can be seen here: https://department-of-veterans-affairs.github.io/veteran-facing-services-tools/frontend-support-dashboard/cross-app-import-report`,
       annotation_level: 'warning',
     };
   });
+
+  console.log(annotationsJson);
   core.exportVariable(
     `ISOLATION_ANNOTATIONS_JSON`,
     JSON.stringify(annotationsJson),
