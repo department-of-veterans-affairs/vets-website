27a3a80800151d91379e712eadfd0700c6dd886b
Ryan Koch <ryan.koch.0213@gmail.com>
2024-11-04 21:15:27 -0600

    Adds unit test for FormRenderer when formConfig is set.

--- src/applications/simple-forms-form-engine/shared/tests/unit/containers/FormRenderer.unit.spec.js
+++ src/applications/simple-forms-form-engine/shared/tests/unit/containers/FormRenderer.unit.spec.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import sinon from 'sinon';
+import { render, waitFor } from '@testing-library/react';
 import { expect } from 'chai';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import FormRenderer from '../../../containers/FormRenderer';
+import * as routes from '../../../routes';
 
 const renderApp = (store, formId) => {
   return render(
@@ -44,5 +46,28 @@ describe('<FormRenderer /> component', () => {
     });
     const { getByText } = renderApp(store, 'some-form-id');
     expect(getByText(/Loading.*some-form-id/)).to.exist;
+  });
+
+  it('does not renders an error if formConfig is set', async () => {
+    const store = mockStore({
+      formLoad: {
+        isLoading: false,
+        isError: false,
+        isSuccess: true,
+        formConfig: {
+          someKey: 'someVal',
+        },
+      },
+    });
+    // mock the injectReducer function
+    store.injectReducer = () => {};
+
+    // mock getRoutesFromFormConfig
+    sinon.stub(routes, 'getRoutesFromFormConfig').returns({});
+
+    const { queryByText } = renderApp(store, 'some-form-id');
+    await waitFor(() => {
+      expect(queryByText(/Error.*/)).not.to.exist;
+    });
   });
 });
