74628b92f440129b21595d713f2bcd4040026f7b
Jun Tao Luo <john.luo@agile6.com>
2024-11-06 20:57:05 -0500

    Remove unused class attribute

--- src/applications/vaos/new-appointment/components/TypeOfCarePage/index.jsx
+++ src/applications/vaos/new-appointment/components/TypeOfCarePage/index.jsx
@@ -103,7 +103,7 @@ export default function TypeOfCarePage() {
     <div className="vaos-form__radio-field">
       <h1 className="vads-u-font-size--h2">
         {pageTitle}
-        <span className="schemaform-required-span vaos-calendar__page_header vads-u-font-size--base vads-u-font-family--sans vads-u-font-weight--normal">
+        <span className="schemaform-required-span vads-u-font-size--base vads-u-font-family--sans vads-u-font-weight--normal">
           (*Required)
         </span>
       </h1>
