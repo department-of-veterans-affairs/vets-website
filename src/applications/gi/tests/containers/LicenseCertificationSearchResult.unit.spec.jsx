ae06b17922d9b9d285d6f96d7f3216d33987faab
Nick Hibbits <nicholashibbits1@gmail.com>
2024-11-08 09:35:42 -0800

    cleanup comments

--- src/applications/gi/tests/containers/LicenseCertificationSearchResult.unit.spec.jsx
+++ src/applications/gi/tests/containers/LicenseCertificationSearchResult.unit.spec.jsx
@@ -26,46 +26,4 @@ describe('<LicenseCertificationSearchResult />', () => {
       expect(screen).to.not.be.null;
     });
   });
-
-  // it('should show result info if hasFetchedResult is true', async () => {
-  //   const result = {
-  //     link: '/sample-link',
-  //     type: 'Sample Type',
-  //     name: 'Sample Name',
-  //   };
-
-  //   const resultInfo = {
-  //     institution: {
-  //       name: 'Sample Institution',
-  //       phone: '123-456-7890',
-  //       physicalStreet: '123 Main St',
-  //       physicalCity: 'Sample City',
-  //       physicalState: 'CA',
-  //       physicalZip: '90210',
-  //       physicalCountry: 'USA',
-  //     },
-  //     tests: [
-  //       { name: 'Sample Test 1', fee: 200 },
-  //       { name: 'Sample Test 2', fee: 300 },
-  //     ],
-  //   };
-
-  //   const screen = renderWithStoreAndRouter(
-  //     <LicenseCertificationSearchResult result={result} />,
-  //     {
-  //       initialState: {
-  //         hasFetchedResult: true,
-  //         resultInfo: resultInfo,
-  //       },
-  //     },
-  //   );
-
-  //   await waitFor(() => {
-  //     const resultInfoComponent = screen.queryByText(/Loading/);
-  //     expect(resultInfoComponent).to.be.null;
-  //     expect(
-  //       screen.getByText(/Physical address and mailing address are the same/),
-  //     ).to.exist;
-  //   });
-  // });
 });
