2d94828e64f3f66cace913c66f5fd191b815bd5b
Randi Mays <19175324+randimays@users.noreply.github.com>
2024-11-08 13:07:20 -0600

    Add null check to static page entry

--- src/applications/static-pages/homepage-email-signup/index.js
+++ src/applications/static-pages/homepage-email-signup/index.js
@@ -7,6 +7,10 @@ import './email-signup.scss';
 export default (store, widgetType) => {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
 
+  if (!root) {
+    return;
+  }
+
   ReactDOM.render(
     <Provider store={store}>
       <EmailSignup />
