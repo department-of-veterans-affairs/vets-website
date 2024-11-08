6646f99b69ee54eb5026f1bcc5d9a7debed0b59d
Brian Seek <brian.seek@civicactions.com>
2024-11-07 16:29:26 -0800

    added new mock for provider using generator.

--- src/applications/vaos/services/mocks/index.js
+++ src/applications/vaos/services/mocks/index.js
@@ -57,7 +57,12 @@ const specialtyGroups = require('./epsApi/specialtyGroups.json');
 const providerOrgs = require('./epsApi/providerOrganizations.json');
 const providerServices = require('./epsApi/providerServices.json');
 const providerSlots = require('./epsApi/providerServicesSlots.json');
+<<<<<<< HEAD
 const referralUtils = require('../../referral-appointments/utils/referrals');
+=======
+const referrals = require('./epsApi/referrals.json');
+const providerUtils = require('../../referral-appointments/utils/provider');
+>>>>>>> 543cd95040 (added new mock for provider using generator.)
 
 // Returns the meta object without any backend service errors
 const meta = require('./v2/meta.json');
@@ -665,6 +670,19 @@ const responses = {
     ];
     return res.json({
       data: getSlot.find(slot => slot?.id === req.params.slotId),
+    });
+  },
+  'GET /vaos/v2/epsApi/providerDetails': (req, res) => {
+    return res.json({ data: providerUtils.getProviderDetails(5) });
+  },
+  'GET /vaos/v2/epsApi/referrals': (req, res) => {
+    return res.json({ data: referrals });
+  },
+  'GET /vaos/v2/epsApi/referrals/:referralId': (req, res) => {
+    return res.json({
+      data: referrals.referrals.find(
+        referral => referral?.id === req.params.referralId,
+      ),
     });
   },
   'GET /v0/user': {
