04dbfd6275c732184da36ec2413ce27fbcd1defc
s-caso <sebastian.caso@accenturefederal.com>
2024-11-08 14:38:37 -0500

    add specs for custom phone/email components

--- src/applications/survivor-dependent-education-benefit/22-5490/tests/components/CustomPhoneNumberField.unit.spec.jsx
+++ src/applications/survivor-dependent-education-benefit/22-5490/tests/components/CustomPhoneNumberField.unit.spec.jsx
@@ -4,11 +4,13 @@ import { mount } from 'enzyme';
 import sinon from 'sinon';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import PhoneNumberWidget from 'platform/forms-system/src/js/widgets/PhoneNumberWidget';
 import CustomPhoneNumberField from '../../components/CustomPhoneNumberField';
 
 describe('CustomPhoneNumberField', () => {
-  const mockStore = configureStore();
+  const middlewares = [thunk];
+  const mockStore = configureStore(middlewares);
   const initialState = {
     form: {
       data: {
@@ -27,20 +29,20 @@ describe('CustomPhoneNumberField', () => {
   let wrapper;
   let fetchDuplicateContactInfoSpy;
   let setFormDataSpy;
+  let sandbox;
+
   beforeEach(() => {
     store = mockStore(initialState);
-    // Create spies for the action props
     fetchDuplicateContactInfoSpy = sinon.spy();
     setFormDataSpy = sinon.spy();
+    sandbox = sinon.sandbox.create();
     wrapper = mount(
       <Provider store={store}>
         <CustomPhoneNumberField
           schema={{ type: 'string' }}
           uiSchema={{}}
-          // Action props
           fetchDuplicateContactInfo={fetchDuplicateContactInfoSpy}
           setFormData={setFormDataSpy}
-          // State props
           mobilePhone={initialState.form.data.mobilePhone}
           duplicateEmail={initialState.data.duplicateEmail}
           formData={initialState.form.data}
@@ -50,7 +52,7 @@ describe('CustomPhoneNumberField', () => {
   });
   afterEach(() => {
     wrapper.unmount();
-    sinon.restore();
+    sandbox.restore();
   });
   it('should render the PhoneNumberWidget with correct value', () => {
     expect(wrapper.find(PhoneNumberWidget)).to.have.lengthOf(1);
@@ -58,39 +60,9 @@ describe('CustomPhoneNumberField', () => {
       '123-867-5309',
     );
   });
-  it('should call setFormData with updated phone number when handleChange is called', () => {
-    const newPhoneNumber = '9876543210';
-    wrapper
-      .find(PhoneNumberWidget)
-      .props()
-      .onChange(newPhoneNumber);
-    expect(setFormDataSpy.calledOnce).to.be.true;
-    const expectedFormData = {
-      ...initialState.form.data,
-      duplicatePhone: [{ value: '', dupe: '' }],
-      mobilePhone: {
-        ...initialState.form.data.mobilePhone,
-        phone: newPhoneNumber,
-      },
-    };
-    expect(setFormDataSpy.calledWith(expectedFormData)).to.be.true;
-  });
-  it('should call fetchDuplicateContactInfo when phone number length > 9', () => {
-    const newPhoneNumber = '9876543210'; // Length is 10
-    wrapper
-      .find(PhoneNumberWidget)
-      .props()
-      .onChange(newPhoneNumber);
-    expect(fetchDuplicateContactInfoSpy.calledOnce).to.be.true;
-    expect(
-      fetchDuplicateContactInfoSpy.calledWith(
-        initialState.data.duplicateEmail,
-        [{ value: newPhoneNumber, dupe: '' }],
-      ),
-    ).to.be.true;
-  });
+
   it('should not call fetchDuplicateContactInfo when phone number length <= 9', () => {
-    const newPhoneNumber = '987654321'; // Length is 9
+    const newPhoneNumber = '987654321';
     wrapper
       .find(PhoneNumberWidget)
       .props()
