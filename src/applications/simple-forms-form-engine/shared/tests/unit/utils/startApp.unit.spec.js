e375a3b16124a4bb05f97060af374a629bb1af1c
Ryan Koch <ryan.koch.0213@gmail.com>
2024-11-12 12:41:34 -0600

    Changes startApp to export a default, and updates import to slightly different name.

--- src/applications/simple-forms-form-engine/shared/tests/unit/utils/startApp.unit.spec.js
+++ src/applications/simple-forms-form-engine/shared/tests/unit/utils/startApp.unit.spec.js
@@ -1,18 +1,18 @@
 // TODO: Update this test file to work.
 // Importing from @department-of-veterans-affairs/platform-startup/exports
 // in this test is causing issues. The problem exists trying to import
-// directly (to stub), but also exists trying to import startFormEngineFormApp
+// directly (to stub), but also exists trying to import startFormEngineApp
 // because that file imports from @department-of-veterans-affairs/platform-startup/exports.
 // So it's effectively not possible to test anything without figuring this out.
 
 // import * as startup from '@department-of-veterans-affairs/platform-startup/exports';
 // import sinon from 'sinon';
-// import { startFormEngineFormApp } from '../../../utils/startApp';
+// import startFormEngineApp from '../../../utils/startApp';
 
-// describe('startFormEngineFormApp', () => {
+// describe('startFormEngineApp', () => {
 //   it('calls startAppFromIndex', () => {
 //     const startAppFromIndexStub = sinon.stub(startup, 'startAppFromIndex');
-//     startFormEngineFormApp({
+//     startFormEngineApp({
 //       formId: 'some-form-id',
 //       rootUrl: '/root-url',
 //       trackingPrefix: 'tracking-prefix-',
