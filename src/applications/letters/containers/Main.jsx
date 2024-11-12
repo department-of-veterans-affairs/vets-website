51b57b53e74e150166e883e6534f4f0c32b282d4
Jane Cho <87551323+janechodance@users.noreply.github.com>
2024-11-12 14:51:08 -0500

    update need help section styling

--- src/applications/letters/containers/Main.jsx
+++ src/applications/letters/containers/Main.jsx
@@ -33,15 +33,16 @@ const {
 export class Main extends React.Component {
   componentDidMount() {
     const { shouldUseLighthouse, shouldUseLettersDiscrepancies } = this.props;
-
     // eslint-disable-next-line -- LH_MIGRATION
     const LH_MIGRATION__options = LH_MIGRATION__getOptions(shouldUseLighthouse);
-
-    if (!this.props.emptyAddress) {
-      // eslint-disable-next-line -- LH_MIGRATION
-      return this.props.getLetterListAndBSLOptions(LH_MIGRATION__options, shouldUseLettersDiscrepancies);
-    }
-    return this.props.profileHasEmptyAddress();
+    // if (!this.props.emptyAddress) {
+    //   // eslint-disable-next-line -- LH_MIGRATION
+    return this.props.getLetterListAndBSLOptions(
+      LH_MIGRATION__options,
+      shouldUseLettersDiscrepancies,
+    );
+    // }
+    // return this.props.profileHasEmptyAddress();
   }
 
   render() {
