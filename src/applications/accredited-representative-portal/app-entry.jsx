0c8d0e18c456b3dcbdb418e70d0fc57a82c74ffb
Oren Mittman <nihil2501@gmail.com>
2024-11-12 09:22:22 -0500

    [art/restore-poa-requests] restore 2122 code (#32934)

---
+++ src/applications/accredited-representative-portal/app-entry.jsx
@@ -0,0 +1,25 @@
+import '@department-of-veterans-affairs/platform-polyfills';
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom-v5-compat';
+
+import startReactApp from '@department-of-veterans-affairs/platform-startup/react';
+import { connectFeatureToggle } from 'platform/utilities/feature-toggles';
+
+import './sass/accredited-representative-portal.scss';
+
+import manifest from './manifest.json';
+import routes from './routes';
+import createReduxStore from './store';
+import rootReducer from './reducers';
+
+window.appName = manifest.entryName;
+const store = createReduxStore(rootReducer);
+connectFeatureToggle(store.dispatch);
+
+startReactApp(
+  <Provider store={store}>
+    <BrowserRouter basename={manifest.rootUrl}>{routes}</BrowserRouter>
+  </Provider>,
+);
