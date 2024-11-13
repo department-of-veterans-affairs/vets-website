775e8e387d6a616b6a4d232c58540e90248b1bc0
jquispe-oddball <jennifer.quispe@oddball.io>
2024-11-13 14:23:40 -0500

    96497 - removing patch from before, restoring original file

--- src/applications/accredited-representative-portal/actions/user.js
+++ src/applications/accredited-representative-portal/actions/user.js
@@ -1,3 +1,6 @@
+import { apiRequest } from '@department-of-veterans-affairs/platform-utilities/api';
+import environment from '~/platform/utilities/environment';
+
 export const FETCH_USER = 'FETCH_USER';
 export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS';
 export const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE';
@@ -9,26 +12,14 @@ export function fetchUser() {
     });
 
     try {
+      const path = '/accredited_representative_portal/v0/user';
+      const user = await apiRequest(`${environment.API_URL}${path}`);
+
       /**
        * This is an even stricter success condition than having a user. We
        * additionally require what is needed for access token refreshing to
        * function.
        */
-      const user = {
-        account: {
-          accountUuid: '88f572d491af46efa393cba6c351e252',
-        },
-        profile: {
-          firstName: 'William',
-          lastName: 'Phelps',
-          verified: true,
-          signIn: {
-            serviceName: 'idme',
-          },
-        },
-        prefillsAvailable: [],
-        inProgressForms: [],
-      };
       const serviceName = user?.profile?.signIn?.serviceName;
       if (!serviceName)
         throw new Error('Missing user with sign in service name.');
