92025b7e9478ff275e078cd5657b9e1450d487ba
Matt Long <6738544+longmd@users.noreply.github.com>
2024-11-14 14:49:51 -0500

    Update spec coverage for submit transformer in Health Care Application

--- src/applications/hca/tests/unit/utils/helpers/helpers.unit.spec.js
+++ src/applications/hca/tests/unit/utils/helpers/helpers.unit.spec.js
@@ -1,7 +1,5 @@
 import { expect } from 'chai';
-
 import {
-  transformAttachments,
   getInsuranceAriaLabel,
   normalizeFullName,
   parseVeteranDob,
@@ -12,57 +10,6 @@ import {
 } from '../../../../utils/helpers';
 
 describe('hca helpers', () => {
-  describe('when `transformAttachments` executes', () => {
-    describe('when there are no attachements', () => {
-      it('should do nothing', () => {
-        const inputData = { firstName: 'Pat' };
-        const transformedData = transformAttachments(inputData);
-        expect(transformedData).to.deep.equal(inputData);
-      });
-    });
-
-    describe('when there are attachements', () => {
-      it('should transform `attachmentId`s to `dd214` booleans', () => {
-        const inputData = {
-          firstName: 'Pat',
-          attachments: [
-            {
-              name: 'file1',
-              size: 1,
-              confirmationCode: 'uuid123',
-              attachmentId: '1',
-            },
-            {
-              name: 'file2',
-              size: 1,
-              confirmationCode: 'uuid456',
-              attachmentId: '2',
-            },
-          ],
-        };
-        const expectedOutputData = {
-          firstName: 'Pat',
-          attachments: [
-            {
-              name: 'file1',
-              size: 1,
-              confirmationCode: 'uuid123',
-              dd214: true,
-            },
-            {
-              name: 'file2',
-              size: 1,
-              confirmationCode: 'uuid456',
-              dd214: false,
-            },
-          ],
-        };
-        const transformedData = transformAttachments(inputData);
-        expect(transformedData).to.deep.equal(expectedOutputData);
-      });
-    });
-  });
-
   describe('when `getInsuranceAriaLabel` executes', () => {
     describe('when the provider name is not provided', () => {
       it('should return a generic label', () => {
