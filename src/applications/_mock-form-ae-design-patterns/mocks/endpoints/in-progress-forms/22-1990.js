716bcacdd3e45deea4c2b146026c3bab7a60d4c7
Adam Whitlock <8332986+adamwhitlock1@users.noreply.github.com>
2024-11-15 12:32:53 -0700

    update 22-1990 prefill

--- src/applications/_mock-form-ae-design-patterns/mocks/endpoints/in-progress-forms/22-1990.js
+++ src/applications/_mock-form-ae-design-patterns/mocks/endpoints/in-progress-forms/22-1990.js
@@ -1,24 +1,30 @@
+const { USER } = require('../../constants/user');
+
 const responses = {
   FORM_22_1990: {
     minimal: {
       formData: {
         veteranFullName: {
-          first: 'Mitchell',
-          middle: 'George',
-          last: 'Jenkins',
+          first: USER.FIRST_NAME,
+          middle: USER.MIDDLE_NAME,
+          last: USER.LAST_NAME,
         },
-        gender: 'M',
-        veteranDateOfBirth: '1956-07-10',
-        veteranSocialSecurityNumber: '123456789',
-        homePhone: '5558081234',
-        mobilePhone: '5554044567',
-        email: 'Mitchell.Jenkins.Test@gmail.com',
+        gender: USER.GENDER,
+        veteranDateOfBirth: USER.BIRTH_DATE,
+        veteranSocialSecurityNumber: USER.SSN_LAST_FOUR,
+        homePhone: `${USER.HOME_PHONE.AREA_CODE}${
+          USER.HOME_PHONE.PHONE_NUMBER
+        }`,
+        mobilePhone: `${USER.MOBILE_PHONE.AREA_CODE}${
+          USER.MOBILE_PHONE.PHONE_NUMBER
+        }`,
+        email: USER.EMAIL,
         veteranAddress: {
-          street: '125 Main St.',
-          city: 'Fulton',
-          state: 'NY',
-          country: 'USA',
-          postalCode: '97063',
+          street: USER.MAILING_ADDRESS.ADDRESS_LINE1,
+          city: USER.MAILING_ADDRESS.CITY,
+          state: USER.MAILING_ADDRESS.STATE_CODE,
+          country: USER.MAILING_ADDRESS.COUNTRY_CODE_ISO2,
+          postalCode: USER.MAILING_ADDRESS.ZIP_CODE,
           isMilitary: false,
         },
         toursOfDuty: [
