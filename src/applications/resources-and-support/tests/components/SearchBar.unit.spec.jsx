7ae1775dce4fedf93ce30f0dbb20014916c34baa
Randi Mays <19175324+randimays@users.noreply.github.com>
2024-11-15 12:45:26 -0600

    VACMS-16191 Resources & Support test coverage

--- src/applications/resources-and-support/tests/components/SearchBar.unit.spec.jsx
+++ src/applications/resources-and-support/tests/components/SearchBar.unit.spec.jsx
@@ -1,45 +1,57 @@
-// Node modules
 import React from 'react';
+import { render } from '@testing-library/react';
+
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
-// Relative imports
 import SearchBar from '../../components/SearchBar';
 
 describe('SearchBar', () => {
-  it('renders without crashing', () => {
-    let testInput = '';
-    const setTestInput = str => {
-      testInput = str;
-    };
+  const searchBarProps = {
+    onInputChange: () => {},
+    previousValue: 'test',
+    setSearchData: () => {},
+    userInput: 'new test',
+  };
 
-    const wrapper = shallow(
-      <SearchBar
-        userInput={testInput}
-        onInputChange={setTestInput}
-        useDefaultFormSearch
-      />,
-    );
+  it('should render the correct view', () => {
+    render(<SearchBar {...searchBarProps} />);
 
-    expect(wrapper).to.exist;
-    wrapper.unmount();
+    
   });
+  // it('renders without crashing', () => {
+  //   let testInput = '';
+  //   const setTestInput = str => {
+  //     testInput = str;
+  //   };
 
-  it('renders without crashing with a search term', () => {
-    let testInput = 'health';
-    const setTestInput = str => {
-      testInput = str;
-    };
+  //   const wrapper = shallow(
+  //     <SearchBar
+  //       userInput={testInput}
+  //       onInputChange={setTestInput}
+  //       useDefaultFormSearch
+  //     />,
+  //   );
 
-    const wrapper = shallow(
-      <SearchBar
-        userInput={testInput}
-        onInputChange={setTestInput}
-        useDefaultFormSearch
-      />,
-    );
+  //   expect(wrapper).to.exist;
+  //   wrapper.unmount();
+  // });
 
-    expect(wrapper).to.exist;
-    wrapper.unmount();
-  });
+  // it('renders without crashing with a search term', () => {
+  //   let testInput = 'health';
+  //   const setTestInput = str => {
+  //     testInput = str;
+  //   };
+
+  //   const wrapper = shallow(
+  //     <SearchBar
+  //       userInput={testInput}
+  //       onInputChange={setTestInput}
+  //       useDefaultFormSearch
+  //     />,
+  //   );
+
+  //   expect(wrapper).to.exist;
+  //   wrapper.unmount();
+  // });
 });
