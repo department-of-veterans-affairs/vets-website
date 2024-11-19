db5cd71f3f67e22644509cf34553a3310f4c9fa6
jquispe-oddball <jennifer.quispe@oddball.io>
2024-11-19 14:08:29 -0500

    96498 - request details page

--- src/applications/accredited-representative-portal/routes.jsx
+++ src/applications/accredited-representative-portal/routes.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom-v5-compat';
-
 import App from './containers/App';
 import LandingPage from './containers/LandingPage';
 import POARequestsPage from './containers/POARequestsPage';
 import SignedInLayoutWrapper from './containers/SignedInLayoutWrapper';
+import POARequestDetails from './components/POARequestDetails/POARequestDetails';
 
 const routes = (
   <Routes>
@@ -12,6 +12,7 @@ const routes = (
       <Route index element={<LandingPage />} />
       <Route element={<SignedInLayoutWrapper />}>
         <Route path="poa-requests" element={<POARequestsPage />} />
+        <Route path="poa-requests/:id" element={<POARequestDetails />} />
       </Route>
     </Route>
   </Routes>
