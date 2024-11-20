07d9c4c0de530008192defdd693cc3974f98b862
robertbylight <107279507+robertbylight@users.noreply.github.com>
2024-11-20 14:44:29 -0500

    [mhv-64025] fixed filter urls

--- src/applications/mhv-medications/util/constants.js
+++ src/applications/mhv-medications/util/constants.js
@@ -44,22 +44,21 @@ export const filterOptions = {
     name: 'filter option',
     description: 'Active prescriptions and non-VA medications',
     url:
-      '&filter[[disp_status][eq]]=Active,Active: Refill in Process,Active: Non-VA,Active: On hold,Active: Parked,Active: Refill in process,Active: Submitted',
+      '&filter[[disp_status][eq]]=Active,Active: Refill in Process,Active: Non-VA,Active: On hold,Active: Parked,Active: Submitted',
   },
   RECENTLY_REQUESTED: {
     label: 'Recently requested',
     name: 'filter option',
     description: 'Refill requests in process or shipped in the last 15 days',
     url:
-      '&filter[[disp_status][eq]]=Active: Refill in process,Active: Submitted',
+      '&filter[[disp_status][eq]]=Active: Refill in Process,Active: Submitted',
   },
   RENEWAL: {
     label: 'Renewal needed before refill',
     name: 'filter option',
     description:
       'Prescriptions that just ran out of refills or became too old to refill (expired)',
-    url:
-      '&filter[[disp_status][eq]]=Active: Refill in process,Expired&filter[[is_refillable][eq]]=false',
+    url: '&filter[[disp_status][eq]]=Active,Expired',
   },
   NON_ACTIVE: {
     label: 'Non-active',
