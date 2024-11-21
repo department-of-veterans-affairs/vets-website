69afa8a35336da532d3df10b1b0f37de8750ed4f
Brian Seek <brian.seek@civicactions.com>
2024-11-21 10:41:05 -0800

    added feature flag into referral hook

--- src/applications/vaos/referral-appointments/hooks/useGetReferralFromId.jsx
+++ src/applications/vaos/referral-appointments/hooks/useGetReferralFromId.jsx
@@ -3,26 +3,33 @@ import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { getReferral } from '../redux/selectors';
 import { fetchReferralById } from '../redux/actions';
 import { FETCH_STATUS } from '../../utils/constants';
+import { selectFeatureCCDirectScheduling } from '../../redux/selectors';
 
 const useGetReferralFromId = id => {
   const [referralNotFound, setReferralNotFound] = useState(false);
   const [currentReferral, setCurrentReferral] = useState(null);
   const dispatch = useDispatch();
+  const featureCCDirectScheduling = useSelector(
+    selectFeatureCCDirectScheduling,
+  );
   const { referrals, referralFetchStatus } = useSelector(
     state => getReferral(state),
     shallowEqual,
   );
   useEffect(
     () => {
-      if (referralFetchStatus === FETCH_STATUS.succeeded && !referrals.length) {
+      if (
+        featureCCDirectScheduling &&
+        (referralFetchStatus === FETCH_STATUS.succeeded && !referrals.length)
+      ) {
         setReferralNotFound(true);
       }
     },
-    [referralFetchStatus, referrals],
+    [featureCCDirectScheduling, referralFetchStatus, referrals],
   );
   useEffect(
     () => {
-      if (referrals.length && id) {
+      if (featureCCDirectScheduling && (referrals.length && id)) {
         const referralFromParam = referrals.find(ref => ref.UUID === id);
         if (referralFromParam) {
           setCurrentReferral(referralFromParam);
@@ -31,18 +38,19 @@ const useGetReferralFromId = id => {
         }
       }
     },
-    [id, referrals, referralFetchStatus],
+    [id, referrals, referralFetchStatus, featureCCDirectScheduling],
   );
   useEffect(
     () => {
       if (
+        featureCCDirectScheduling &&
         !referrals.length &&
         referralFetchStatus === FETCH_STATUS.notStarted
       ) {
         dispatch(fetchReferralById(id));
       }
     },
-    [dispatch, id, referralFetchStatus, referrals],
+    [dispatch, featureCCDirectScheduling, id, referralFetchStatus, referrals],
   );
   return {
     currentReferral,
