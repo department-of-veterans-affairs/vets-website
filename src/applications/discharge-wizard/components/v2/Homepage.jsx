30d2a842612509a7ac79d65174b4b2e44ff933af
Chris Kim <chriskim@Chriss-MacBook-Pro-2.local>
2024-11-21 15:04:05 -0800

    updates

--- src/applications/discharge-wizard/components/v2/Homepage.jsx
+++ src/applications/discharge-wizard/components/v2/Homepage.jsx
@@ -6,6 +6,7 @@ import { ROUTES } from '../../constants';
 import { updateIntroPageViewed } from '../../actions';
 import { pageSetup } from '../../utilities/page-setup';
 import { QUESTION_MAP } from '../../constants/question-data-map';
+import { NeedHelp } from './NeedHelp';
 
 const HomePage = ({ router, setIntroPageViewed }) => {
   const H1 = QUESTION_MAP.HOME;
@@ -203,15 +204,7 @@ const HomePage = ({ router, setIntroPageViewed }) => {
             </p>
           </va-accordion-item>
         </va-accordion>
-        <va-need-help>
-          <div slot="content">
-            <p>
-              Call us at <va-telephone contact="8008271000" />. We’re here
-              Monday through Friday, 8:00 a.m to 9:00 p.m ET. If you have
-              hearing loss, call <va-telephone contact="711" tty="true" />.
-            </p>
-          </div>
-        </va-need-help>
+        <NeedHelp />
       </div>
     </>
   );
---
+++ src/applications/discharge-wizard/components/v2/NeedHelp.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const NeedHelp = () => {
+  return (
+    <va-need-help>
+      <div slot="content">
+        <p>
+          Call us at <va-telephone contact="8008271000" />. We’re here Monday
+          through Friday, 8:00 a.m to 9:00 p.m ET. If you have hearing loss,
+          call <va-telephone contact="711" tty="true" />.
+        </p>
+      </div>
+    </va-need-help>
+  );
+};
