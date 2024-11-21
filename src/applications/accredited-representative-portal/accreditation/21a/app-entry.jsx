aa99fe89d998b1ef5bc470cff2fcc70091de1805
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2024-11-21 17:45:19 -0500

    simplifying for install to accomodate build

--- src/applications/accredited-representative-portal/accreditation/21a/app-entry.jsx
+++ src/applications/accredited-representative-portal/accreditation/21a/app-entry.jsx
@@ -1,16 +1,5 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 import startReactApp from '@department-of-veterans-affairs/platform-startup/react';
-import App from './containers/App';
 import manifest from './manifest.json';
 
-// TODO: Update the basename value to the correct path
-const customHistory = createBrowserHistory({ basename: '/' });
-
-startReactApp(
-  <Router history={customHistory}>
-    <App />
-  </Router>,
-  { entryName: manifest.entryName },
-);
+startReactApp(<div />, { entryName: manifest.entryName });
