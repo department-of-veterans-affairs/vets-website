b3c8d8e46b545abfb2d50d636bb4aed6f1e2cce6
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2024-11-21 16:49:15 -0500

    replacing removed useHistory

--- src/applications/accredited-representative-portal/accreditation/21a/app-entry.jsx
+++ src/applications/accredited-representative-portal/accreditation/21a/app-entry.jsx
@@ -1,31 +1,16 @@
-import '@department-of-veterans-affairs/platform-polyfills';
-
 import React from 'react';
-import { Provider } from 'react-redux';
-import { Router, useRouterHistory } from 'react-router';
-import { createHistory } from 'history';
-
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import startReactApp from '@department-of-veterans-affairs/platform-startup/react';
-import { connectFeatureToggle } from 'platform/utilities/feature-toggles';
-
-import './sass/21a.scss';
-
+import App from './containers/App';
 import manifest from './manifest.json';
-import routes from './routes';
-import createReduxStore from '../../store';
-import rootReducer from './reducers';
 
-window.appName = manifest.entryName;
-const store = createReduxStore(rootReducer);
-connectFeatureToggle(store.dispatch);
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const history = useRouterHistory(createHistory)({
-  basename: manifest.rootUrl,
-});
+// TODO: Update the basename value to the correct path
+const customHistory = createBrowserHistory({ basename: '/' });
 
 startReactApp(
-  <Provider store={store}>
-    <Router history={history} routes={routes} />
-  </Provider>,
+  <Router history={customHistory}>
+    <App />
+  </Router>,
+  { entryName: manifest.entryName },
 );
