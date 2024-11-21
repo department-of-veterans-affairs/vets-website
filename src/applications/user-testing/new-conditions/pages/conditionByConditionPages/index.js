be3b8bdb5d199d8e20acd0d4519414766b0e34e8
William Phelps <williamphelps13@gmail.com>
2024-11-21 09:43:15 -0800

    Update from onNavForward/Back to depends

--- src/applications/user-testing/new-conditions/pages/conditionByConditionPages/index.js
+++ src/applications/user-testing/new-conditions/pages/conditionByConditionPages/index.js
@@ -1,6 +1,4 @@
-import { stringifyUrlParams } from '@department-of-veterans-affairs/platform-forms-system/helpers';
 import { arrayBuilderPages } from 'platform/forms-system/src/js/patterns/array-builder';
-import { getArrayIndexFromPathName } from 'platform/forms-system/src/js/patterns/array-builder/helpers';
 
 import causePage from './cause';
 import causeFollowUpPage from './causeFollowUp';
@@ -12,79 +10,60 @@ import summaryPage from './summary';
 import { arrayBuilderOptions, hasSideOfBody } from './utils';
 import { CONDITION_BY_CONDITION } from '../../constants';
 
+const isActiveDemo = formData => formData.demo === 'CONDITION_BY_CONDITION';
+
 const conditionByConditionPages = arrayBuilderPages(
   arrayBuilderOptions,
-  (pageBuilder, helpers) => ({
+  pageBuilder => ({
     conditionByConditionIntro: pageBuilder.introPage({
       title: 'New conditions intro',
       path: `new-conditions-${CONDITION_BY_CONDITION}-intro`,
-      depends: formData => formData.demo === 'CONDITION_BY_CONDITION',
+      depends: isActiveDemo,
       uiSchema: introPage.uiSchema,
       schema: introPage.schema,
     }),
     conditionByConditionSummary: pageBuilder.summaryPage({
       title: 'Review your new conditions',
       path: `new-conditions-${CONDITION_BY_CONDITION}-summary`,
-      depends: formData => formData.demo === 'CONDITION_BY_CONDITION',
+      depends: isActiveDemo,
       uiSchema: summaryPage.uiSchema,
       schema: summaryPage.schema,
     }),
     conditionByConditionCondition: pageBuilder.itemPage({
       title: 'Claim a new condition',
       path: `new-conditions-${CONDITION_BY_CONDITION}/:index/condition`,
-      depends: formData => formData.demo === 'CONDITION_BY_CONDITION',
+      depends: isActiveDemo,
       uiSchema: conditionPage.uiSchema,
       schema: conditionPage.schema,
-      onNavForward: props => {
-        const { formData, pathname, urlParams, goPath } = props;
-        const index = getArrayIndexFromPathName(pathname);
-        const urlParamsString = stringifyUrlParams(urlParams) || '';
-
-        // TODO: This fixed bug where side of body was not being cleared when condition was edited which could result in 'Asthma, right'
-        // However, with this fix, when user doesn't change condition, side of body is cleared which could confuse users
-        formData.sideOfBody = undefined;
-
-        return hasSideOfBody(formData, index)
-          ? helpers.navForwardKeepUrlParams(props)
-          : goPath(
-              `new-conditions-${CONDITION_BY_CONDITION}/${index}/date${urlParamsString}`,
-            );
-      },
     }),
     conditionByConditionSideOfBody: pageBuilder.itemPage({
       title: 'Side of body of new condition',
       path: `new-conditions-${CONDITION_BY_CONDITION}/:index/side-of-body`,
-      depends: formData => formData.demo === 'CONDITION_BY_CONDITION',
+      // TODO: Side of body is not being cleared when condition is edited which could result in 'Asthma, right'
+      // If possible if user doesn't change condition while going through edit flow, side of body should not be cleared
+      depends: (formData, index) =>
+        isActiveDemo(formData) && hasSideOfBody(formData, index),
       uiSchema: sideOfBodyPage.uiSchema,
       schema: sideOfBodyPage.schema,
     }),
     conditionByConditionDate: pageBuilder.itemPage({
       title: 'Date of new condition',
       path: `new-conditions-${CONDITION_BY_CONDITION}/:index/date`,
-      depends: formData => formData.demo === 'CONDITION_BY_CONDITION',
+      depends: isActiveDemo,
       uiSchema: datePage.uiSchema,
       schema: datePage.schema,
-      onNavBack: props => {
-        const { formData, pathname, urlParams, goPath } = props;
-        const index = getArrayIndexFromPathName(pathname);
-        const urlParamsString = stringifyUrlParams(urlParams) || '';
-
-        return hasSideOfBody(formData, index)
-          ? helpers.navBackKeepUrlParams(props)
-          : goPath(`new-conditions/${index}/condition${urlParamsString}`);
-      },
     }),
     conditionByConditionCause: pageBuilder.itemPage({
       title: 'Cause of new condition',
       path: `new-conditions-${CONDITION_BY_CONDITION}/:index/cause`,
-      depends: formData => formData.demo === 'CONDITION_BY_CONDITION',
+      depends: isActiveDemo,
       uiSchema: causePage.uiSchema,
       schema: causePage.schema,
     }),
     conditionByConditionCauseFollowUp: pageBuilder.itemPage({
       title: 'Cause follow up for new condition',
       path: `new-conditions-${CONDITION_BY_CONDITION}/:index/cause-follow-up`,
-      depends: formData => formData.demo === 'CONDITION_BY_CONDITION',
+      depends: isActiveDemo,
       uiSchema: causeFollowUpPage.uiSchema,
       schema: causeFollowUpPage.schema,
     }),
--- src/applications/user-testing/new-conditions/pages/conditionsFirstPages/index.js
+++ src/applications/user-testing/new-conditions/pages/conditionsFirstPages/index.js
@@ -1,78 +1,47 @@
-import { stringifyUrlParams } from '@department-of-veterans-affairs/platform-forms-system/helpers';
 import { arrayBuilderPages } from 'platform/forms-system/src/js/patterns/array-builder';
-import { getArrayIndexFromPathName } from 'platform/forms-system/src/js/patterns/array-builder/helpers';
 
 import conditionPage from './condition';
-import datePage from './date';
 import introPage from './intro';
 import sideOfBodyPage from './sideOfBody';
 import summaryPage from './summary';
 import { arrayBuilderOptions, hasSideOfBody } from './utils';
 import { CONDITIONS_FIRST } from '../../constants';
 
+const isActiveDemo = formData => formData.demo === 'CONDITIONS_FIRST';
+
 const conditionsFirstPages = arrayBuilderPages(
   arrayBuilderOptions,
-  (pageBuilder, helpers) => ({
+  pageBuilder => ({
     conditionsFirstIntro: pageBuilder.introPage({
       title: 'New conditions intro',
       path: `new-conditions-${CONDITIONS_FIRST}-intro`,
-      depends: formData => formData.demo === 'CONDITIONS_FIRST',
+      depends: isActiveDemo,
       uiSchema: introPage.uiSchema,
       schema: introPage.schema,
     }),
     conditionsFirstSummary: pageBuilder.summaryPage({
       title: 'Review your new conditions',
       path: `new-conditions-${CONDITIONS_FIRST}-summary`,
-      depends: formData => formData.demo === 'CONDITIONS_FIRST',
+      depends: isActiveDemo,
       uiSchema: summaryPage.uiSchema,
       schema: summaryPage.schema,
     }),
     conditionsFirstCondition: pageBuilder.itemPage({
       title: 'Claim a new condition',
       path: `new-conditions-${CONDITIONS_FIRST}/:index/condition`,
-      depends: formData => formData.demo === 'CONDITIONS_FIRST',
+      depends: isActiveDemo,
       uiSchema: conditionPage.uiSchema,
       schema: conditionPage.schema,
-      onNavForward: props => {
-        const { formData, pathname, urlParams, goPath } = props;
-        const index = getArrayIndexFromPathName(pathname);
-        const urlParamsString = stringifyUrlParams(urlParams) || '';
-
-        // TODO: This fixed bug where side of body was not being cleared when condition was edited which could result in 'Asthma, right'
-        // However, with this fix, when user doesn't change condition, side of body is cleared which could confuse users
-        formData.sideOfBody = undefined;
-
-        return hasSideOfBody(formData, index)
-          ? helpers.navForwardKeepUrlParams(props)
-          : goPath(
-              `new-conditions-${CONDITIONS_FIRST}/${index}/date${urlParamsString}`,
-            );
-      },
     }),
     conditionsFirstSideOfBody: pageBuilder.itemPage({
       title: 'Side of body of new condition',
       path: `new-conditions-${CONDITIONS_FIRST}/:index/side-of-body`,
-      depends: formData => formData.demo === 'CONDITIONS_FIRST',
+      // TODO: Side of body is not being cleared when condition is edited which could result in 'Asthma, right'
+      // If possible if user doesn't change condition while going through edit flow, side of body should not be cleared
+      depends: (formData, index) =>
+        isActiveDemo(formData) && hasSideOfBody(formData, index),
       uiSchema: sideOfBodyPage.uiSchema,
       schema: sideOfBodyPage.schema,
-    }),
-    newConditionsJustConditionsDate: pageBuilder.itemPage({
-      title: 'Date of new condition',
-      path: `new-conditions-${CONDITIONS_FIRST}/:index/date`,
-      depends: formData => formData.demo === 'CONDITIONS_FIRST',
-      uiSchema: datePage.uiSchema,
-      schema: datePage.schema,
-      onNavBack: props => {
-        const { formData, pathname, urlParams, goPath } = props;
-        const index = getArrayIndexFromPathName(pathname);
-        const urlParamsString = stringifyUrlParams(urlParams) || '';
-
-        return hasSideOfBody(formData, index)
-          ? helpers.navBackKeepUrlParams(props)
-          : goPath(
-              `new-conditions-${CONDITIONS_FIRST}/${index}/condition${urlParamsString}`,
-            );
-      },
     }),
   }),
 );
