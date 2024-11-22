f6fef09eb39a82fd73c2fdb0fa20d05ac71e4a4b
Brad Bergeron <bergeron.bradley@gmail.com>
2024-11-22 10:32:08 -0500

    cleans up unit test

--- src/applications/my-education-benefits/tests/form.unit.spec.js
+++ src/applications/my-education-benefits/tests/form.unit.spec.js
@@ -7,13 +7,8 @@ describe('Service History Chapter', () => {
     uiSchema,
   } = formConfig?.chapters?.serviceHistoryChapter?.pages?.serviceHistory;
 
-  it('renders the old Service History header', () => {
+  it('renders Service History header', () => {
     expect(uiSchema['view:subHeading']).to.exist;
     expect(schema.properties['view:subHeading']).to.exist;
-  });
-
-  it('renders the new Service History header', () => {
-    expect(uiSchema['view:newSubHeading']).to.exist;
-    expect(schema.properties['view:newSubHeading']).to.exist;
   });
 });
