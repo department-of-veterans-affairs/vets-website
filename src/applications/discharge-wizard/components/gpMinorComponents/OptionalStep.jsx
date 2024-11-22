9aab081fda3d83076194907249891204d5ad3f21
Jami Gibbs <jami.gibbs@adhocteam.us>
2024-11-22 11:36:53 -0600

    convert .feature to va-summary-box

--- src/applications/discharge-wizard/components/gpMinorComponents/OptionalStep.jsx
+++ src/applications/discharge-wizard/components/gpMinorComponents/OptionalStep.jsx
@@ -9,42 +9,44 @@ const OptionalStep = ({ formValues }) => {
 
   if (validReason && validYear) {
     return (
-      <section className="feature">
-        <h4>
-          (Optional): Apply to the Physical Disability Board of Review (PDBR)
-        </h4>
-        <p>
-          If you believe your disability rating for TBI, PTSD, or mental health
-          conditions is too low, consider applying to the Physical Disability
-          Board of Review (PDBR). The DoD created the PDBR specifically to
-          review appeals about low disability ratings for Veterans discharged
-          between 2001 and 2009. Some Veterans discharged during this period of
-          time received lower disability ratings than they deserved, especially
-          if they suffered from TBI, PTSD, or other mental health conditions. If
-          you were discharged during this period of time and previously received
-          a disability rating of 20% or lower, you’re eligible to apply to the
-          PDBR for review. The PDBR does not issue discharge upgrades and cannot
-          review conditions not listed in your military record before your
-          separation. But, if the PDBR finds that your disability rating was
-          unjustly low, it may help you make your case to upgrade your
-          discharge.{' '}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://health.mil/PDBR"
-          >
-            Learn more about PBDR reviews (opens in a new tab)
-          </a>
-          .{' '}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://health.mil/Military-Health-Topics/Conditions-and-Treatments/Physical-Disability/Disability-Evaluation/Physical-Disability-Board-of-Review/PDBR-Application-Process"
-          >
-            Apply for a PBDR review (opens in a new tab)
-          </a>
-          .
-        </p>
+      <section className="vads-u-margin-top--3">
+        <va-summary-box>
+          <h4 slot="headline">
+            (Optional): Apply to the Physical Disability Board of Review (PDBR)
+          </h4>
+          <p>
+            If you believe your disability rating for TBI, PTSD, or mental
+            health conditions is too low, consider applying to the Physical
+            Disability Board of Review (PDBR). The DoD created the PDBR
+            specifically to review appeals about low disability ratings for
+            Veterans discharged between 2001 and 2009. Some Veterans discharged
+            during this period of time received lower disability ratings than
+            they deserved, especially if they suffered from TBI, PTSD, or other
+            mental health conditions. If you were discharged during this period
+            of time and previously received a disability rating of 20% or lower,
+            you’re eligible to apply to the PDBR for review. The PDBR does not
+            issue discharge upgrades and cannot review conditions not listed in
+            your military record before your separation. But, if the PDBR finds
+            that your disability rating was unjustly low, it may help you make
+            your case to upgrade your discharge.{' '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://health.mil/PDBR"
+            >
+              Learn more about PBDR reviews (opens in a new tab)
+            </a>
+            .{' '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://health.mil/Military-Health-Topics/Conditions-and-Treatments/Physical-Disability/Disability-Evaluation/Physical-Disability-Board-of-Review/PDBR-Application-Process"
+            >
+              Apply for a PBDR review (opens in a new tab)
+            </a>
+            .
+          </p>
+        </va-summary-box>
       </section>
     );
   }
