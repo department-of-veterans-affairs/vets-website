465f23bcefc6307dc1ee95482a50cdbed7ee098e
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2024-11-22 13:13:17 -0500

    fixing build

--- src/applications/accredited-representative-portal/reducers/index.js
+++ src/applications/accredited-representative-portal/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 import { FeatureToggleReducer } from 'platform/site-wide/feature-toggles/reducers';
-import userReducer from './user';
 
 const rootReducer = combineReducers({
-  user: userReducer,
   featureToggles: FeatureToggleReducer,
 });
 
