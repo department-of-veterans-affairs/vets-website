3155ff5459133f8a638564a96aec49203da105e7
Vitalii Menshutin <Vitalii.Menshutin@bylight.com>
2024-11-21 19:07:42 -0500

    MHV-64582: Print/Download fixes; MHV-64580: Loading issue brings in all meds after filtered

--- src/applications/mhv-medications/containers/Prescriptions.jsx
+++ src/applications/mhv-medications/containers/Prescriptions.jsx
@@ -308,7 +308,7 @@ const Prescriptions = () => {
         dispatch(
           getPaginatedFilteredList(
             storedPageNumber,
-            storedFilterOption,
+            filterOptions[storedFilterOption],
             sortEndpoint,
           ),
         ).then(() => updateLoadingStatus(false, ''));
