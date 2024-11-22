74e11fbec1811c8acf9c848f1ea57870d44f102d
Trevor Pierce <934879+1Copenut@users.noreply.github.com>
2024-11-22 13:47:56 -0600

    Temporary fix for Download letter voice control.

--- src/applications/letters/components/DownloadLetterLink.jsx
+++ src/applications/letters/components/DownloadLetterLink.jsx
@@ -39,7 +39,7 @@ export class DownloadLetterLink extends React.Component {
         break;
       case DOWNLOAD_STATUSES.success:
         buttonText = 'Download letter';
-        buttonDisabled = false;
+        // buttonDisabled = false;
         message = (
           <va-alert status="success" role="alert">
             <h4 slot="headline">Your letter has successfully downloaded.</h4>
@@ -52,7 +52,7 @@ export class DownloadLetterLink extends React.Component {
         break;
       case DOWNLOAD_STATUSES.failure:
         buttonText = 'Retry download';
-        buttonDisabled = false;
+        // buttonDisabled = false;
         message = (
           <va-alert status="error" role="alert">
             <h4 slot="headline">Your letter didn’t download.</h4>
@@ -65,7 +65,7 @@ export class DownloadLetterLink extends React.Component {
         break;
       default:
         buttonText = 'Download letter';
-        buttonDisabled = false;
+      // buttonDisabled = false;
     }
 
     return (
