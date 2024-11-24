b7492e56d2733be7a70566e7f5aecec94d4fdedf
William Phelps <williamphelps13@gmail.com>
2024-11-24 14:03:27 -0800

    Update conditionFirst to use conditionByCondition files that have same content to aid in consistency

--- src/applications/user-testing/new-conditions/pages/conditionsFirstPages/index.js
+++ src/applications/user-testing/new-conditions/pages/conditionsFirstPages/index.js
@@ -1,14 +1,12 @@
-import { stringifyUrlParams } from '@department-of-veterans-affairs/platform-forms-system/helpers';
 import { arrayBuilderPages } from 'platform/forms-system/src/js/patterns/array-builder';
 import { getArrayIndexFromPathName } from 'platform/forms-system/src/js/patterns/array-builder/helpers';
 
-import conditionPage from './condition';
-import datePage from './date';
-import introPage from './intro';
-import sideOfBodyPage from './sideOfBody';
-import summaryPage from './summary';
-import { arrayBuilderOptions, hasSideOfBody } from './utils';
 import { CONDITIONS_FIRST } from '../../constants';
+import introPage from '../conditionByConditionPages/intro'; // Same content as conditionByCondition so using to ensure consistency
+import sideOfBodyPage from '../conditionByConditionPages/sideOfBody'; // Same content as conditionByCondition so using to ensure consistency
+import summaryPage from '../conditionByConditionPages/summary'; // Same content as conditionByCondition so using to ensure consistency
+import conditionPage from './condition';
+import { arrayBuilderOptions, hasSideOfBody } from './utils';
 
 const conditionsFirstPages = arrayBuilderPages(
   arrayBuilderOptions,
@@ -34,9 +32,8 @@ const conditionsFirstPages = arrayBuilderPages(
       uiSchema: conditionPage.uiSchema,
       schema: conditionPage.schema,
       onNavForward: props => {
-        const { formData, pathname, urlParams, goPath } = props;
+        const { formData, pathname } = props;
         const index = getArrayIndexFromPathName(pathname);
-        const urlParamsString = stringifyUrlParams(urlParams) || '';
 
         // TODO: This fixed bug where side of body was not being cleared when condition was edited which could result in 'Asthma, right'
         // However, with this fix, when user doesn't change condition, side of body is cleared which could confuse users
@@ -44,9 +41,7 @@ const conditionsFirstPages = arrayBuilderPages(
 
         return hasSideOfBody(formData, index)
           ? helpers.navForwardKeepUrlParams(props)
-          : goPath(
-              `new-conditions-${CONDITIONS_FIRST}/${index}/date${urlParamsString}`,
-            );
+          : helpers.navForwardSummary(props);
       },
     }),
     conditionsFirstSideOfBody: pageBuilder.itemPage({
@@ -55,24 +50,6 @@ const conditionsFirstPages = arrayBuilderPages(
       depends: formData => formData.demo === 'CONDITIONS_FIRST',
       uiSchema: sideOfBodyPage.uiSchema,
       schema: sideOfBodyPage.schema,
-    }),
-    newConditionsJustConditionsDate: pageBuilder.itemPage({
-      title: 'Date of new condition',
-      path: `new-conditions-${CONDITIONS_FIRST}/:index/date`,
-      depends: formData => formData.demo === 'CONDITIONS_FIRST',
-      uiSchema: datePage.uiSchema,
-      schema: datePage.schema,
-      onNavBack: props => {
-        const { formData, pathname, urlParams, goPath } = props;
-        const index = getArrayIndexFromPathName(pathname);
-        const urlParamsString = stringifyUrlParams(urlParams) || '';
-
-        return hasSideOfBody(formData, index)
-          ? helpers.navBackKeepUrlParams(props)
-          : goPath(
-              `new-conditions-${CONDITIONS_FIRST}/${index}/condition${urlParamsString}`,
-            );
-      },
     }),
   }),
 );
