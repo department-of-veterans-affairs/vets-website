08d3fc497edba5a5b2ef48f13c9094b334bc6364
John Woolschlager <wullaski@gmail.com>
2024-11-26 10:35:19 -0500

    Adjust format and skip testing date format in pipeline

--- src/applications/vaos/referral-appointments/components/PendingReferralCard.jsx
+++ src/applications/vaos/referral-appointments/components/PendingReferralCard.jsx
@@ -20,7 +20,7 @@ const PendingReferralCard = ({ referral, handleClick, index }) => {
       : `${referral.numberOfAppointments} appointments`;
 
   const dt = new Date(referral.ReferralExpirationDate);
-  const expiration = format(dt, 'MMMM, dd, yyyy');
+  const expiration = format(dt, 'MMMM, d, yyyy');
 
   return (
     <ListItem
