18fdee39b90017fb3f20b544d5769eaf743b2bee
Ian <harrison.ian.t@gmail.com>
2024-11-26 16:25:04 -0600

    fix test in profile and update helpers

--- src/applications/personalization/profile/components/alerts/AccountBlocked.jsx
+++ src/applications/personalization/profile/components/alerts/AccountBlocked.jsx
@@ -1,4 +1,4 @@
-import { CONTACTS } from '@department-of-veterans-affairs/component-library';
+import { C as CONTACTS } from '@department-of-veterans-affairs/component-library/dist/components/contacts';
 import React from 'react';
 
 import PropTypes from 'prop-types';
