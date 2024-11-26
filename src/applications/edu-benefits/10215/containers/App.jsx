4d7816577b2cb5f50fcaa63ad372c84a0da28edb
Michael Ghisilieri <175352110+mghisilieri@users.noreply.github.com>
2024-11-26 13:26:55 -0800

    helper function to hide currentOrPastDateUI hint element

--- src/applications/edu-benefits/10215/containers/App.jsx
+++ src/applications/edu-benefits/10215/containers/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from '../config/form';
+import { addStyleToShadowDomOnPages } from '../../utils/helpers';
 
 export default function App({ location, children }) {
+  useEffect(() => {
+    // Insert CSS to hide 'For example: January 19 2000' hint on memorable dates
+    // (can't be overridden by passing 'hint' to uiOptions):
+    addStyleToShadowDomOnPages(
+      [''],
+      ['va-memorable-date'],
+      '#dateHint {display: none}',
+    );
+  });
+
   return (
     <div className="form-22-10215-container">
       <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
