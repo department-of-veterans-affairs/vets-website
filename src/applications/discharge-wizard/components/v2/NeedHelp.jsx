d5d67b653b96a054ab48d9fede552d13d81a5232
Chris Kim <42885441+chriskim2311@users.noreply.github.com>
2024-11-29 14:55:16 -0800

    updates

--- src/applications/discharge-wizard/components/v2/NeedHelp.jsx
+++ src/applications/discharge-wizard/components/v2/NeedHelp.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 export const NeedHelp = () => {
   return (
-    <va-need-help>
-      <div slot="content">
-        <p>
-          Call us at <va-telephone contact="8008271000" />. We’re here Monday
-          through Friday, 8:00 a.m to 9:00 p.m ET. If you have hearing loss,
-          call <va-telephone contact="711" tty="true" />.
-        </p>
+    <div className="row need-help-padding-top ">
+      <div className="usa-width-one-whole medium-8 columns">
+        <va-need-help>
+          <div slot="content">
+            <p>
+              Call us at <va-telephone contact="8008271000" />. We’re here
+              Monday through Friday, 8:00 a.m to 9:00 p.m ET. If you have
+              hearing loss, call <va-telephone contact="711" tty="true" />.
+            </p>
+          </div>
+        </va-need-help>
       </div>
-    </va-need-help>
+    </div>
   );
 };
