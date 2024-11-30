06c89d25728747c1aa29d65a1cc94f4e8f5ad116
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2024-11-29 20:01:20 -0800

    Update MR landing page test

--- src/applications/mhv-medical-records/tests/e2e/medical-records-landing-page-authenticated.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-landing-page-authenticated.cypress.spec.js
@@ -1,5 +1,7 @@
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
 import LandingPage from './pages/MedicalRecordsLandingPage';
+import sessionStatus from './fixtures/session-status.json';
+
 // import VitalsDetailsPage from './pages/VitalsDetailsPage';
 
 describe('Medical Records Authenticated Users', () => {
@@ -7,10 +9,24 @@ describe('Medical Records Authenticated Users', () => {
     const site = new MedicalRecordsSite();
     site.login();
     site.loadPage();
+    cy.intercept('POST', '/my_health/v1/medical_records/session', {
+      statusCode: 204,
+      body: {},
+    }).as('session');
+    cy.intercept('GET', '/my_health/v1/medical_records/session/status', {
+      statusCode: 200,
+      body: sessionStatus, // status response copied from staging
+    }).as('status');
     cy.visit('my-health/medical-records');
     LandingPage.verifyDownloadOnMhvLink(
       'Go back to the previous version of My HealtheVet to download your records',
     );
+    LandingPage.verifyLabsAndTestsLink();
+    LandingPage.verifyNotesLink();
+    LandingPage.verifyVaccinesLink();
+    LandingPage.verifyAllergiesLink();
+    LandingPage.verifyConditionsLink();
+    LandingPage.verifyVitalsLink();
     // Axe check
     cy.injectAxe();
     cy.axeCheck('main');
--- src/applications/mhv-medical-records/tests/e2e/pages/MedicalRecordsLandingPage.js
+++ src/applications/mhv-medical-records/tests/e2e/pages/MedicalRecordsLandingPage.js
@@ -17,5 +17,41 @@ class MedicalRecordsLandingPage {
         'myhealth.va.gov/mhv-portal-web/download-my-data', // mhv-syst.
       );
   };
+
+  verifyLabsAndTestsLink = () => {
+    cy.get('[data-testid="labs-and-tests-landing-page-link"]')
+      .should('have.attr', 'href')
+      .and('include', '/my-health/medical-records/labs-and-tests');
+  };
+
+  verifyNotesLink = () => {
+    cy.get('[data-testid="notes-landing-page-link"]')
+      .should('have.attr', 'href')
+      .and('include', '/my-health/medical-records/summaries-and-notes');
+  };
+
+  verifyVaccinesLink = () => {
+    cy.get('[data-testid="vaccines-landing-page-link"]')
+      .should('have.attr', 'href')
+      .and('include', '/my-health/medical-records/vaccines');
+  };
+
+  verifyAllergiesLink = () => {
+    cy.get('[data-testid="allergies-landing-page-link"]')
+      .should('have.attr', 'href')
+      .and('include', '/my-health/medical-records/allergies');
+  };
+
+  verifyConditionsLink = () => {
+    cy.get('[data-testid="conditions-landing-page-link"]')
+      .should('have.attr', 'href')
+      .and('include', '/my-health/medical-records/conditions');
+  };
+
+  verifyVitalsLink = () => {
+    cy.get('[data-testid="vitals-landing-page-link"]')
+      .should('have.attr', 'href')
+      .and('include', '/my-health/medical-records/vitals');
+  };
 }
 export default new MedicalRecordsLandingPage();
