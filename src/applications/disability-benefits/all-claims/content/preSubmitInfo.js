76f96e2390ad9937514a9a29aeace921d2ab2b95
Christine <127446042+christinec-fftc@users.noreply.github.com>
2024-12-02 14:18:11 -0800

    84062 add link

--- src/applications/disability-benefits/all-claims/content/preSubmitInfo.js
+++ src/applications/disability-benefits/all-claims/content/preSubmitInfo.js
@@ -13,9 +13,13 @@ const body = (
       any privilege that may apply and would otherwise make the information
       confidential and not disclosable.
     </p>
+    <p>I certify I have received the evidence requirements for my claim.</p>
     <p>
-      I certify I have received the notice on this application titled,{' '}
-      <strong>Notice of Evidence Needed</strong>.
+      <va-link
+        external
+        href="https://www.va.gov/disability/how-to-file-claim/evidence-needed/"
+        text="Review the evidence requirements"
+      />
     </p>
     <p>
       I certify I have enclosed all the information or evidence that will
@@ -37,13 +41,14 @@ const plainBody =
   'any information about me. For the limited purpose of providing VA with this ' +
   'information as it may relate to my claim, I waive any privilege that may ' +
   'apply and would otherwise make the information confidential and not ' +
-  'disclosable. I certify I have received the notice on this application ' +
-  'titled, Notice of Evidence Needed. I certify I have enclosed all the ' +
-  'information or evidence that will support my claim, to include an ' +
-  'identification of relevant records available at a Federal facility such as a ' +
-  'VA medical center; OR, I have no information or evidence to give VA to ' +
-  'support my claim; OR, I plan to submit additional evidence in support of my ' +
-  'claim and will have my claim processed under the standard claim process.';
+  'disclosable. I certify I have received the evidence requirements for my claim. ' +
+  'Review the evidence requirements https://www.va.gov/disability/how-to-file-claim/evidence-needed/ ' +
+  'I certify I have enclosed all the information or evidence that will support ' +
+  'my claim, to include an identification of relevant records available at a ' +
+  'Federal facility such as a VA medical center; OR, I have no information or ' +
+  'evidence to give VA to support my claim; OR, I plan to submit additional ' +
+  'evidence in support of my claim and will have my claim processed under the ' +
+  'standard claim process.';
 
 export default function getPreSubmitInfo() {
   if (!environment.isLocalhost()) {
