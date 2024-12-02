dd084e17ec91c3035c6130c1aad83d853d97119e
Mark Dewey <mdewey@users.noreply.github.com>
2024-12-02 15:52:56 -0500

    Tests-mhv-mr-accelerated-vitals (#33250)
    * (mocks) added some mocks for vitals toggle
    
    * Add feature flag for MHV accelerated delivery of vital signs
    
    * (refactor) renamed allergies and added vitals toggle
    
    * (refactor) moved landing page to using new hook and allow for independent toggle
    
    * (test) added a test before doing the dev work to show vitals
    
    * (feature) fixed gated logic for vitals
    
    * (test) moved accelerated tests to own folder; added test for all domains enabled
    
    * (feature) added using the oh param
    
    * (tests) added tests gate disabled, added test for vitals list with some vital LH data
    
    * fix login method
    
    * Fix defaults
    
    * Add datepicker to vitals page
    
    * Add more vitals mocks and ability to filter by date
    
    * Fix date range in getAcceleratedVitals and add validation for date updates in Vitals component
    
    * Refactor vital types and display logic to handle multiple record types and improve no-records handling
    
    * Add feature flag for accelerated delivery and update related logic in useAcceleratedData hook
    
    * Fix missing record type names
    
    * Fix linting issue
    
    * (mocks) added some mocks and fixed date filter
    
    * (feature) added constant for pain_severity
    
    * (mocks) fixed test data
    
    * (feature) added redirect)
    
    * (bug) fixed the API call not using the latest date
    
    * (mocks) fixed mocks?
    
    * (bug) fixed the resize oberserver bug
    
    * (fix) fixed the vitals from changing back
    
    * (mock) added some more test data
    
    * (feature) added the submit button and loading indicator
    
    * (feature) hiding new record box
    
    * (feature) adding viewing records message
    
    * (mock) updated mock data
    
    * (feature) updated to match design
    
    * (feature) removed time from vital
    
    * (e2e) added blood pressue e2e test
    
    * (feautre) added default value to show on date rage
    
    * (test) updated test ids to me more dynamic
    
    * (test) added test for blood pressure and heart rate
    
    * (test) updated test values
    
    * (mock) user
    
    * (mocks) updated time
    
    * (test) fixed e2e tests
    
    * (tests) added breating rate e2e and Vitals help class
    
    * (test) added body wieght tests
    
    * (test) added test for body weight
    
    * (test) data
    
    * (test) moved and refactored the allergies test
    
    * (test) refactor intercepts
    
    * (test) added tests for temperature
    
    * (test) added tests for blood oxygen
    
    * (test) added tests for pain severity
    
    * (bug) addressed loading bug uncovered by tests
    
    * (revert) reverted bug fix for seperate PR
    
    ---------
    
    Co-authored-by: Adrian Rollett <acrollet@gmail.com>
    Co-authored-by: mdewey <1793923+mdewey@users.noreply.github.com>

--- src/applications/mhv-medical-records/tests/e2e/accelerated/vitals/view-vitals-list.oracle-health.domain.body-weight.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/accelerated/vitals/view-vitals-list.oracle-health.domain.body-weight.cypress.spec.js
@@ -1,9 +1,9 @@
 import MedicalRecordsSite from '../../mr_site/MedicalRecordsSite';
 import Vitals from '../pages/Vitals';
 import oracleHealthUser from '../../fixtures/user/oracle-health.json';
-import vitalsData from '../../fixtures/vitals/blood-pressure.json';
+import vitalsData from '../../fixtures/vitals/body-weight.json';
 
-describe('Medical Records View Blood Pressure', () => {
+describe('Medical Records View Body Weight', () => {
   const site = new MedicalRecordsSite();
 
   beforeEach(() => {
@@ -24,25 +24,22 @@ describe('Medical Records View Blood Pressure', () => {
     Vitals.selectMonthAndYear({ month: '3', year: 2024 });
     Vitals.verifySelectedDate({ dateString: 'March 2024' });
     // check for latest id
-    cy.get('[data-testid="vital-blood-pressure-measurement"]').should(
+    cy.get('[data-testid="vital-body-weight-measurement"]').should(
       'be.visible',
     );
-    cy.get('[data-testid="vital-blood-pressure-measurement"]').contains(
-      '130/81',
-    );
+    cy.get('[data-testid="vital-body-weight-measurement"]').contains('88.6 kg');
 
-    cy.get('[data-testid="vital-blood-pressure-date-timestamp"]').should(
+    cy.get('[data-testid="vital-body-weight-date-timestamp"]').should(
       'be.visible',
     );
-    cy.get('[data-testid="vital-blood-pressure-date-timestamp"]').contains(
-      'March 26, 2024',
+    cy.get('[data-testid="vital-body-weight-date-timestamp"]').contains(
+      'January 1, 2014',
     );
 
-    cy.get('[data-testid="vital-blood-pressure-review-over-time"]').should(
+    cy.get('[data-testid="vital-body-weight-review-over-time"]').should(
       'be.visible',
     );
 
-    // Axe check
     cy.injectAxeThenAxeCheck();
   });
 });
