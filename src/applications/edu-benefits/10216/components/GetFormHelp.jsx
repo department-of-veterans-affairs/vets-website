6238c8077777613deaa12d40d44112c11ebf1861
govcioChad <chad.chotikasatien@va.gov>
2024-12-04 07:57:45 -0800

    #vebt-844 #comment Skeleton for ConfirmationPage 22-10216

---
+++ src/applications/edu-benefits/10216/components/GetFormHelp.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export function GetFormHelp() {
+  return (
+    <>
+      <p>
+        <strong>If you have trouble using this online form,</strong> call us at
+        1-888-442-4551
+      </p>
+      <p>
+        <strong>
+          If you need help gathering your information or filling out your form,
+        </strong>{' '}
+        <va-link
+          href=""
+          text="visit Education Liaison Representatives- Education and Training."
+        />
+      </p>
+    </>
+  );
+}
+
+export default GetFormHelp;
--- src/applications/edu-benefits/10216/containers/ConfirmationPage.jsx
+++ src/applications/edu-benefits/10216/containers/ConfirmationPage.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { format, isValid } from 'date-fns';
 import { connect } from 'react-redux';
-
 import scrollToTop from 'platform/utilities/ui/scrollToTop';
 import { focusElement } from 'platform/utilities/ui';
+import { GetFormHelp } from '../components/GetFormHelp';
 
 export class ConfirmationPage extends React.Component {
   componentDidMount() {
@@ -13,52 +12,76 @@ export class ConfirmationPage extends React.Component {
   }
 
   render() {
-    const { form } = this.props;
-    const { submission, formId, data } = form;
-    const submitDate = new Date(submission?.timestamp);
-    const { fullName } = data;
+    // const { form } = this.props;
+    // const { submission, formId, data } = form;
+    // const submitDate = new Date(submission?.timestamp);
+    // const { fullName } = data;
 
     return (
       <div>
-        <div className="print-only">
-          <img
-            src="https://www.va.gov/img/design/logo/logo-black-and-white.png"
-            alt="VA logo"
-            width="300"
-          />
-          <h2>Application for Mock Form</h2>
-        </div>
-        <h2 className="vads-u-font-size--h3">
-          Your application has been submitted
+        <h2 className="vads-u-font-size--h3 vads-u-margin-bottom--2">
+          To submit your form, follow the steps below
         </h2>
-        <p>We may contact you for more information or documents.</p>
-        <p className="screen-only">Please print this page for your records.</p>
-        <div className="inset">
-          <h3 className="vads-u-margin-top--0 vads-u-font-size--h4">
-            22-10216 5% EXEMPTION REQUEST FROM 85/15 REPORTING REQUIREMENT
-            GENERAL INFORMATION Claim{' '}
-            <span className="vads-u-font-weight--normal">(Form {formId})</span>
-          </h3>
-          {fullName ? (
-            <span>
-              for {fullName.first} {fullName.middle} {fullName.last}
-              {fullName.suffix ? `, ${fullName.suffix}` : null}
-            </span>
-          ) : null}
-
-          {isValid(submitDate) ? (
+        <va-process-list>
+          <va-process-list-item header="Download and save your form">
             <p>
-              <strong>Date submitted</strong>
-              <br />
-              <span>{format(submitDate, 'MMMM d, yyyy')}</span>
+              Make sure that your completed form is saved as a PDF on your
+              device.
             </p>
-          ) : null}
-          <va-button
-            onClick={window.print}
-            text="Print this for your records"
-            uswds
-          />
-        </div>
+            <va-link
+              download
+              filetype="PDF"
+              href=""
+              // fileName={''}
+              text="Download VA Form 22-10216"
+            />
+          </va-process-list-item>
+          <va-process-list-item header="Upload the form to the VA education portal">
+            <p>
+              Visit the{' '}
+              <va-link
+                external
+                text="VA Education File Upload Portal(opens in a new tab)"
+                href="https://www.va.gov/education/"
+              >
+                VA education portal
+              </va-link>
+              , and upload your saved VA Form 22-10216.
+            </p>
+          </va-process-list-item>
+          <va-process-list-item header="Submit your form">
+            <p>Once uploaded, click submit to finalize your request.</p>
+          </va-process-list-item>
+        </va-process-list>
+        <va-button
+          uswds
+          secondary
+          class="screen-only vads-u-margin-top--1"
+          text="Print this page"
+          onClick={() => window.print()}
+        />
+        <h2 className="vads-u-font-size--h2 vads-u-margin-top--8">
+          What are my next steps?
+        </h2>
+        <p>
+          After submitting your exemption request, we will review your
+          submission within 7-10 business days. Once we complete the review, we
+          will email your school a letter with the decision. If we accept your
+          request, we will include a copy of WEAMS form 1998 as confirmation in
+          the letter. If we deny your request, we will explain the reason for
+          rejection in the letter and provide further instructions for
+          resubmission or additional steps.
+        </p>
+        <va-link-action
+          href="/"
+          text="Go back to VA.gov"
+          class="vads-u-margin-top--1p5 vads-u-margin-bottom--3"
+        />
+        <va-need-help class="vads-u-margin-top--6">
+          <div slot="content">
+            <GetFormHelp />
+          </div>
+        </va-need-help>
       </div>
     );
   }
