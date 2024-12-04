ca00f4caa4f38ec66ef2ac8884b3bc2be19fc1e0
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2024-12-04 14:00:15 -0800

    Moved typedef’s

--- src/applications/vaos/services/patient/index.js
+++ src/applications/vaos/services/patient/index.js
@@ -11,40 +11,6 @@ import { getAvailableHealthcareServices } from '../healthcare-service';
 import { getPatientEligibility } from '../vaos';
 import { getLongTermAppointmentHistoryV2 } from '../appointment';
 
-/**
- * @typedef PatientEligibilityForType
- * @global
- *
- * @property {boolean} hasRequiredAppointmentHistory Has had appointment in the past that meets VATS requirements
- *   - Mapped from past visits check
- * @property {?boolean} isEligibleForNewAppointmentRequest Is under the request limit
- *   - Mapped from request limits check
- */
-
-/**
- * @typedef PatientEligibility
- * @global
- *
- * @property {?PatientEligibilityForType} direct Patient eligibility for direct scheduling
- * @property {?PatientEligibilityForType} request Patient eligibility for requests
- */
-
-/**
- * @typedef {'error'|'overRequestLimit'|'noEnabled'|'notSupported'|'noRecentVisit'|
- *   'noClinics'|'noMatchingClinics'} EligibilityReason
- * @global
- */
-
-/**
- * @typedef FlowEligibility
- * @global
- *
- * @property {boolean} direct Can the patient use the direct schedule flow
- * @property {Array<EligibilityReason>} directReason The reason the patient isn't eligible for direct flow
- * @property {boolean} request Can the patient use the request flow
- * @property {Array<EligibilityReason>} requestReason The reason the patient isn't eligible for request flow
- */
-
 function createErrorHandler(errorKey) {
   return data => {
     captureError(data, true);
--- src/applications/vaos/services/patient/types.js
+++ src/applications/vaos/services/patient/types.js
@@ -14,3 +14,37 @@
  * @property {String} vistaId The three digit VistA id
  * @property {string} lastSeen Date in ISO format of the when the patient was last seen by this provider
  */
+
+/**
+ * @typedef PatientEligibilityForType
+ * @global
+ *
+ * @property {boolean} hasRequiredAppointmentHistory Has had appointment in the past that meets VATS requirements
+ *   - Mapped from past visits check
+ * @property {?boolean} isEligibleForNewAppointmentRequest Is under the request limit
+ *   - Mapped from request limits check
+ */
+
+/**
+ * @typedef PatientEligibility
+ * @global
+ *
+ * @property {?PatientEligibilityForType} direct Patient eligibility for direct scheduling
+ * @property {?PatientEligibilityForType} request Patient eligibility for requests
+ */
+
+/**
+ * @typedef {'error'|'overRequestLimit'|'noEnabled'|'notSupported'|'noRecentVisit'|
+ *   'noClinics'|'noMatchingClinics'} EligibilityReason
+ * @global
+ */
+
+/**
+ * @typedef FlowEligibility
+ * @global
+ *
+ * @property {boolean} direct Can the patient use the direct schedule flow
+ * @property {Array<EligibilityReason>} directReason The reason the patient isn't eligible for direct flow
+ * @property {boolean} request Can the patient use the request flow
+ * @property {Array<EligibilityReason>} requestReason The reason the patient isn't eligible for request flow
+ */
