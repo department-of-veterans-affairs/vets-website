d142e291b5da4bf3481feca74d5aad47b5fa684c
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2024-12-04 13:53:56 -0800

    Updated types

--- src/applications/vaos/services/patient/types.js
+++ src/applications/vaos/services/patient/types.js
@@ -6,4 +6,11 @@
  * @typedef {Object} PatientProviderRelationship
  *
  * @property {'PatientProviderRelationship'} resourceType Static resource type string
+ * @property {String} providerName The full name of the provider
+ * @property {String} providerId The Cerner id for the provider
+ * @property {String} serviceType The service type code
+ * @property {String} loctionName The VA facility name
+ * @property {String} clinicName The clinic name
+ * @property {String} vistaId The three digit VistA id
+ * @property {string} lastSeen Date in ISO format of the when the patient was last seen by this provider
  */
