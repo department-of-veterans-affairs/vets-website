aabfaf9df61cca7ead116817c2b81fa95b070c87
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2024-12-04 13:35:51 -0800

    Stub out types

---
+++ src/applications/vaos/services/patient/types.js
@@ -0,0 +1,9 @@
+/**
+ * @summary
+ *
+ * A Patient Provider Relationship
+ *
+ * @typedef {Object} PatientProviderRelationship
+ *
+ * @property {'PatientProviderRelationship'} resourceType Static resource type string
+ */
