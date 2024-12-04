c283562c2dace0049f874e5c690ed7fcd35459c6
Liz Townsend <liz.townsend@adhocteam.us>
2024-12-04 13:27:38 -0600

    remove loading spinner

--- src/applications/travel-pay/containers/pages/ClaimStatusExplainerPage.jsx
+++ src/applications/travel-pay/containers/pages/ClaimStatusExplainerPage.jsx
@@ -6,28 +6,16 @@ import BreadCrumbs from '../../components/Breadcrumbs';
 const ClaimStatusExplainerPage = () => {
   const {
     useToggleValue,
-    useToggleLoadingValue,
+    // useToggleLoadingValue,
     TOGGLE_NAMES,
   } = useFeatureToggle();
 
-  const toggleIsLoading = useToggleLoadingValue();
+  // const toggleIsLoading = useToggleLoadingValue();
   const appEnabled = useToggleValue(TOGGLE_NAMES.travelPayPowerSwitch);
 
   if (!appEnabled) {
     window.location.replace('/');
     return null;
-  }
-
-  if (toggleIsLoading) {
-    return (
-      <div className="vads-l-grid-container vads-u-padding-y--3">
-        <va-loading-indicator
-          label="Loading"
-          message="Please wait while we load the application for you."
-          data-testid="travel-pay-loading-indicator"
-        />
-      </div>
-    );
   }
 
   return (
