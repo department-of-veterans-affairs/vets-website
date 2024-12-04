8a15ed6a4f64d78d0467d8eecceabedb97ea3704
Liz Wendling <170662489+GovCIOLiz@users.noreply.github.com>
2024-12-04 10:48:41 -0800

    10282 tests

---
+++ src/applications/edu-benefits/10282/tests/pages/applicantState.unit.spec.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+
+import { DefinitionTester } from 'platform/testing/unit/schemaform-utils';
+import formConfig from '../../config/form';
+
+const definitions = formConfig.defaultDefinitions;
+
+describe('Edu 10282 contact information', () => {
+  const {
+    schema,
+    uiSchema,
+  } = formConfig.chapters.personalInformation.pages.applicantState;
+  it('renders the correct amount of inputs', () => {
+    const form = mount(
+      <DefinitionTester
+        definitions={definitions}
+        schema={schema}
+        uiSchema={uiSchema}
+      />,
+    );
+    expect(form.find('va-select').length).to.equal(1);
+    form.unmount();
+  });
+
+  it('should show errors when required fields are empty', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        schema={schema}
+        onSubmit={onSubmit}
+        uiSchema={uiSchema}
+        definitions={definitions}
+        // data={formData}
+      />,
+    );
+
+    form.find('form').simulate('submit');
+    expect(form.find('va-select[error]').length).to.equal(1);
+    expect(onSubmit.called).to.be.false;
+    form.unmount();
+  });
+});
