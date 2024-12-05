1c903661707be5a1b68acc85dede53e0f55abc16
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-12-05 09:23:47 -0500

    Added test to render organization for selectAccreditedRepresentativeReview

--- src/applications/representative-appoint/tests/pages/representative/selectAccreditedRepresentativeReview.unit.spec.jsx
+++ src/applications/representative-appoint/tests/pages/representative/selectAccreditedRepresentativeReview.unit.spec.jsx
@@ -4,6 +4,7 @@ import { expect } from 'chai';
 import { render } from '@testing-library/react';
 import SelectedAccreditedRepresentativeReview from '../../../components/SelectAccreditedRepresentativeReview';
 import repResults from '../../fixtures/data/representative-results.json';
+import orgResult from '../../fixtures/data/organization-type.json';
 
 describe('<SelectAccreditedRepresentativeReview>', () => {
   const getProps = () => {
@@ -24,14 +25,49 @@ describe('<SelectAccreditedRepresentativeReview>', () => {
     };
   };
 
-  it('should render component', () => {
-    const { props, mockStore } = getProps();
-
+  const renderContainer = (props, mockStore) => {
     const { container } = render(
       <Provider store={mockStore}>
         <SelectedAccreditedRepresentativeReview {...props} />
       </Provider>,
     );
+
+    return container;
+  };
+
+  it('should render component', () => {
+    const { props, mockStore } = getProps();
+
+    const container = renderContainer(props, mockStore);
+
     expect(container).to.exist;
+  });
+
+  it('should display review-row when orgName is present', () => {
+    const { props, mockStore } = getProps();
+
+    const container = renderContainer(props, mockStore);
+
+    const reviewRow = container.querySelector('.review-row');
+    expect(reviewRow).to.not.be.null;
+  });
+
+  it('should render org for org type', () => {
+    const { mockStore } = getProps();
+
+    const props = {
+      data: {
+        'view:selectedRepresentative': orgResult,
+      },
+    };
+
+    const container = renderContainer(props, mockStore);
+
+    const ddElement = Array.from(container.querySelectorAll('dd')).find(
+      dd => dd.textContent === 'Organization',
+    );
+
+    expect(ddElement).to.not.be.null;
+    expect(ddElement).to.have.text('Organization');
   });
 });
