c3c9f0df996858165124346167a82c7f6903e1b1
Richard Davis <richard.davis@agile6.com>
2024-12-05 16:18:32 -0500

    Corrects case of title

--- src/applications/mhv-supply-reordering/constants.js
+++ src/applications/mhv-supply-reordering/constants.js
@@ -1,6 +1,6 @@
 const { freeze } = Object;
 
-export const TITLE = 'Order Medical Supplies';
+export const TITLE = 'Order medical supplies';
 export const SUBTITLE =
   'Use this form to order hearing aid batteries and accessories and CPAP supplies';
 
