fb20b9544be1f34681025aa6f907443bbdce4800
Alex Person <alex.person@thoughtworks.com>
2024-12-05 16:01:28 -0600

    [#1957] Add DD logging to useWaitForCsrfToken and tests

--- src/applications/virtual-agent/hooks/useWaitForCsrfToken.js
+++ src/applications/virtual-agent/hooks/useWaitForCsrfToken.js
@@ -2,19 +2,24 @@ import { useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import * as Sentry from '@sentry/browser';
 import selectLoadingFeatureToggle from '../selectors/selectFeatureTogglesLoading';
+import { logErrorToDatadog } from '../utils/logging';
+import { useDatadogLogging } from './useDatadogLogging';
 
 export function useWaitForCsrfToken(props) {
   // Once the feature toggles have loaded, the csrf token updates
   const csrfTokenLoading = useSelector(selectLoadingFeatureToggle);
   const [csrfTokenLoadingError, setCsrfTokenLoadingError] = useState(false);
+  const isDatadogLoggingEnabled = useDatadogLogging();
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       if (csrfTokenLoading) {
         setCsrfTokenLoadingError(true);
-        Sentry.captureException(
-          new Error('Could not load feature toggles within timeout'),
+        const error = new Error(
+          'Could not load feature toggles within timeout',
         );
+        Sentry.captureException(error);
+        logErrorToDatadog(isDatadogLoggingEnabled, error.message, error);
       }
     }, props.timeout);
     return function cleanup() {
