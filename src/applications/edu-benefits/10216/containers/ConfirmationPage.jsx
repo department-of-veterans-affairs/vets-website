13fda81a4cb5460d6bee9795733a72d59125d34d
govcioChad <162637891+govcioChad@users.noreply.github.com>
2024-12-06 12:45:00 -0800

    #vebt-844 #comment Added router go back function and alert placeholder

--- src/applications/edu-benefits/10216/containers/ConfirmationPage.jsx
+++ src/applications/edu-benefits/10216/containers/ConfirmationPage.jsx
@@ -4,91 +4,7 @@ import { connect, useSelector } from 'react-redux';
 
 import { ConfirmationView } from 'platform/forms-system/src/js/components/ConfirmationView';
 import { GetFormHelp } from '../components/GetFormHelp';
-
-const childContent = (
-  <div>
-    <h1 data-testid="form-title">
-      Request exemption from the 85/15 Rule reporting requirements
-    </h1>
-    <p>
-      35% Exemption Request from 85/15 Reporting Requirement (VA Form 22-10216)
-    </p>
-    {/* {true &&
-          <va-alert status="warning" uswds>
-            <div className="usa-alert-body">
-              <h3 className="vads-u-margin-top--neg0p5">
-                Additional form needed
-              </h3>  
-                Our records indicate your school is not accredited. After submitting this
-                form, you will need to complete and submit VA Form 22-10215, in
-                addition to VA Form 22-10216.
-            </div>
-          </va-alert>} */}
-    <h2 className="vads-u-font-size--h3 vads-u-margin-bottom--2">
-      To submit your forms, follow the steps below
-    </h2>
-    <va-process-list>
-      <va-process-list-item header="Download and save both forms">
-        <p>
-          First, complete and save your VA Form 22-10216 as a PDF. If you didn’t
-          do that on on the previous page, go back and do that now.
-          <div className="vads-u-margin-y--2">
-            <va-link
-              download
-              filetype="PDF"
-              href=""
-              // fileName={''}
-              text="Download VA Form 22-10216"
-            />
-          </div>
-          Then, navigate to{' '}
-          <va-link
-            external
-            text="VA Form 22-10215"
-            href="/education/apply-for-education-benefits/application/10215"
-          />{' '}
-          to fill it out. Once completed, save it as a PDF on your device.
-        </p>
-      </va-process-list-item>
-      <va-process-list-item header="Upload the forms to the VA education portal">
-        <p>
-          Visit the{' '}
-          <va-link
-            external
-            text="VA Education File Upload Portal"
-            href="https://www.my.va.gov/EducationFileUploads/s/"
-          />
-          , and upload both your saved VA Form 22-10216 and VA Form 22-10215.
-        </p>
-      </va-process-list-item>
-      <va-process-list-item header="Submit your forms">
-        <p>Once uploaded, click submit to finalize your request.</p>
-      </va-process-list-item>
-    </va-process-list>
-    <va-button
-      uswds
-      back
-      class="screen-only vads-u-margin-top--1"
-      onClick={() => {}}
-    />
-    <h2 className="vads-u-font-size--h2 vads-u-margin-top--4">
-      What are my next steps?
-    </h2>
-    <p>
-      After submitting your exemption request, we will review your submission
-      within 7-10 business days. Once we complete the review, we will email your
-      school a letter with the decision. If we accept your request, we will
-      include a copy of WEAMS form 1998 as confirmation in the letter. If we
-      deny your request, we will explain the reason for rejection in the letter
-      and provide further instructions for resubmission or additional steps.
-    </p>
-    <va-link-action
-      href="/education/apply-for-education-benefits/application/10215"
-      text="Go to VA Form 22-10215 now"
-      class="vads-u-margin-top--1p5 vads-u-margin-bottom--2"
-    />
-  </div>
-);
+// import { Alert } from '../components/Alert';
 
 export const ConfirmationPage = props => {
   const form = useSelector(state => state.form || {});
@@ -96,6 +12,86 @@ export const ConfirmationPage = props => {
   const { formConfig } = props?.route;
   const submitDate = submission.timestamp;
   const confirmationNumber = submission.response?.confirmationNumber;
+
+  const childContent = (
+    <div>
+      <h1 data-testid="form-title">
+        Request exemption from the 85/15 Rule reporting requirements
+      </h1>
+      <p>
+        35% Exemption Request from 85/15 Reporting Requirement (VA Form
+        22-10216)
+      </p>
+      {/* {props?.isAccredited &&
+           <Alert/>} */}
+      <h2 className="vads-u-font-size--h3 vads-u-margin-bottom--2">
+        To submit your forms, follow the steps below
+      </h2>
+      <va-process-list>
+        <va-process-list-item header="Download and save both forms">
+          <p>
+            First, complete and save your VA Form 22-10216 as a PDF. If you
+            didn’t do that on on the previous page, go back and do that now.
+            <div className="vads-u-margin-y--2">
+              <va-link
+                download
+                filetype="PDF"
+                href=""
+                // fileName={''}
+                text="Download VA Form 22-10216"
+              />
+            </div>
+            Then, navigate to{' '}
+            <va-link
+              external
+              text="VA Form 22-10215"
+              href="/education/apply-for-education-benefits/application/10215"
+            />{' '}
+            to fill it out. Once completed, save it as a PDF on your device.
+          </p>
+        </va-process-list-item>
+        <va-process-list-item header="Upload the forms to the VA education portal">
+          <p>
+            Visit the{' '}
+            <va-link
+              external
+              text="VA Education File Upload Portal"
+              href="https://www.my.va.gov/EducationFileUploads/s/"
+            />
+            , and upload both your saved VA Form 22-10216 and VA Form 22-10215.
+          </p>
+        </va-process-list-item>
+        <va-process-list-item header="Submit your forms">
+          <p>Once uploaded, click submit to finalize your request.</p>
+        </va-process-list-item>
+      </va-process-list>
+      <va-button
+        uswds
+        back
+        class="screen-only vads-u-margin-top--1"
+        onClick={() => {
+          props?.router.goBack();
+        }}
+      />
+      <h2 className="vads-u-font-size--h2 vads-u-margin-top--4">
+        What are my next steps?
+      </h2>
+      <p>
+        After submitting your exemption request, we will review your submission
+        within 7-10 business days. Once we complete the review, we will email
+        your school a letter with the decision. If we accept your request, we
+        will include a copy of WEAMS form 1998 as confirmation in the letter. If
+        we deny your request, we will explain the reason for rejection in the
+        letter and provide further instructions for resubmission or additional
+        steps.
+      </p>
+      <va-link-action
+        href="/education/apply-for-education-benefits/application/10215"
+        text="Go to VA Form 22-10215 now"
+        class="vads-u-margin-top--1p5 vads-u-margin-bottom--2"
+      />
+    </div>
+  );
 
   return (
     <ConfirmationView
