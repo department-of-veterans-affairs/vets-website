605584e88b607872af53387b5904070bfebeb345
Midge-dev <sean.midgley.dev@gmail.com>
2024-12-06 13:08:31 -0800

    Init

--- src/applications/disability-benefits/686c-674-v2/components/GetFormHelp.jsx
+++ src/applications/disability-benefits/686c-674-v2/components/GetFormHelp.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
 
 const GetFormHelp = () => (
-  <p className="help-talk">
-    Need help filling out the form or have questions about eligibility? Call VA
-    Benefits and Services at <va-telephone contact={CONTACTS.VA_BENEFITS} />.
-    <br />
-    <br />
-    If you have hearing loss, call TTY: <va-telephone contact={CONTACTS[711]} />
-    .
-  </p>
+  <>
+    <p className="help-talk">
+      <strong>If you have trouble using this online form</strong>, call us at{' '}
+      <va-telephone international contact="8006982411" /> (
+      <va-telephone tty contact="711" />
+      ). We’re here 24/7.
+    </p>
+    <p>
+      <strong>
+        If you need help gathering your information or filling out your form
+      </strong>
+      , contact a local Veterans Service Organization (VSO).
+    </p>
+    <va-link
+      text="Find a local
+        Veterans Service Organization"
+      label="Find a local
+        Veterans Service Organization"
+      href="/get-help-from-accredited-representative"
+    />
+  </>
 );
 
 export default GetFormHelp;
--- src/applications/disability-benefits/686c-674-v2/containers/ConfirmationPage.jsx
+++ src/applications/disability-benefits/686c-674-v2/containers/ConfirmationPage.jsx
@@ -1,183 +1,189 @@
-import React from 'react';
-import moment from 'moment';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { format } from 'date-fns';
 import PropTypes from 'prop-types';
 
 import scrollToTop from 'platform/utilities/ui/scrollToTop';
 import { focusElement } from 'platform/utilities/ui';
-import ServiceProvidersText, {
-  ServiceProvidersTextCreateAcct,
-} from 'platform/user/authentication/components/ServiceProvidersText';
+import GetFormHelp from '../components/GetFormHelp';
 
 import manifest from '../manifest.json';
 
-export class ConfirmationPage extends React.Component {
-  componentDidMount() {
+export default function ConfirmationPage() {
+  const form = useSelector(state => state?.form);
+
+  const { submission, data } = form;
+  const formSubmissionId = submission?.response?.formSubmissionId;
+  const veteranFirstName = data?.veteranInformation?.fullName?.first || '';
+  const veteranLastName = data?.veteranInformation?.fullName?.last || '';
+
+  const dateSubmitted = submission?.timestamp
+    ? format(new Date(submission?.timestamp), 'MMMM d, yyyy')
+    : '';
+
+  useEffect(() => {
     focusElement('#thank-you-message');
     scrollToTop('topScrollElement');
-  }
+  }, []);
 
-  handlePrintClick = () => {
-    window.print();
-  };
-
-  render() {
-    const { submission, data } = this.props.form;
-    const { response } = submission;
-    const veteranFirstName = data?.veteranInformation?.fullName?.first || '';
-    const veteranLastName = data?.veteranInformation?.fullName?.last || '';
-    const dateSubmitted = moment(response?.timestamp);
-
-    return (
-      <>
-        <div className="print-only">
-          <img
-            src="https://www.va.gov/img/design/logo/logo-black-and-white.png"
-            alt="VA logo"
-            width="300"
+  return (
+    <>
+      <va-alert status="success" class="vads-u-margin-bottom--4" uswds>
+        <h3>Form submission started on August 15, 2024</h3>
+        <p className="vads-u-margin-y--0">Your submission is in progress.</p>
+        <p>
+          It can take up to 10 days for us to receive your form. Your
+          confirmation number is {formSubmissionId}.
+        </p>
+        <va-link-action
+          href="/claim-or-appeal-status/"
+          text="Check the status of your form on My VA"
+          label="Check the status of your form on My VA"
+          message-aria-describedby="Check the status of your form on My VA"
+        />
+      </va-alert>
+      <va-summary-box>
+        <h3 slot="headline">Your submission information</h3>
+        <p>
+          <strong>Your name</strong>
+        </p>
+        <p>
+          {veteranFirstName} {veteranLastName}
+        </p>
+        <p>
+          <strong>Date submitted</strong>
+        </p>
+        <p data-testid="dateSubmitted">{dateSubmitted}</p>
+        <va-button
+          text="Print this page for your records"
+          message-aria-describedby="Print this page for your records"
+          onClick={() => {
+            window.print();
+          }}
+        />
+      </va-summary-box>
+      <section>
+        <h2>What to expect</h2>
+        <va-process-list>
+          <va-process-list-item
+            active
+            header="We’ll confirm that we’ve received your form"
+          >
+            <p>
+              This can take up to 10 days. When we receive your form, we'll
+              update the status on My VA.
+            </p>
+            <va-link
+              href="/claim-or-appeal-status"
+              text="Check the status of your form on My VA"
+              label="Check the status of your form on My VA"
+            />
+          </va-process-list-item>
+          <va-process-list-item pending header="Next, we’ll review your form">
+            <p>
+              If we need more information after reviewing your form, we’ll
+              contact you.
+            </p>
+          </va-process-list-item>
+        </va-process-list>
+      </section>
+      <section>
+        <h2>What if I have additional evidence I need to submit?</h2>
+        <p>
+          If you still need to submit additional supporting documents, you can
+          submit them in one of these 2 ways:
+        </p>
+        <h3>Option 1: Upload your documents using the Claim Status Tool</h3>
+        <p>
+          It may take 10 days for your request to appear in the Claim Status
+          Tool. After your request appears, you can upload your documents in the
+          Files tab.
+        </p>
+        <va-link
+          href="/track-claims"
+          text="Use the Claim Status Tool to upload your documents"
+          label="Use the Claim Status Tool to upload your documents"
+        />
+        <h3>Option 2: Mail us copies of your documents</h3>
+        <p>
+          <strong>If your request is related to disability compensation</strong>
+          , send copies of your documents to this address:
+        </p>
+        <p className="va-address-block">
+          U.S. Department of Veterans Affairs <br />
+          Evidence Intake Center
+          <br />
+          P.O. Box 4444
+          <br />
+          Janesville, WI 53547-4444
+          <br />
+          United States of America
+        </p>
+        <p>
+          <strong>If your request is related to pension benefits</strong>, send
+          copies of your documents to this address:
+        </p>
+        <p className="va-address-block">
+          U.S. Department of Veterans Affairs <br />
+          Pension Intake Center
+          <br />
+          P.O. Box 5365
+          <br />
+          Janesville, WI 53547-5365
+          <br />
+          United States of America
+        </p>
+        <p>
+          <strong>Note:</strong> Don't send us your original documents. We can't
+          return them. Send us copies of your documents only.
+        </p>
+      </section>
+      <section>
+        <h2>What if I need to add or remove another dependent later?</h2>
+        <p>
+          If you need to add or remove another dependent, complete and submit
+          another dependency claim.
+        </p>
+        <va-link
+          href={manifest.rootUrl}
+          text="Start a new dependency claim"
+          label="Start a new dependency claim"
+        />
+      </section>
+      <section>
+        <h2>How to contact us if you have questions</h2>
+        <p>
+          Call us at <va-telephone international contact="8008271000" /> (
+          <va-telephone tty contact="711" />
+          ). We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
+        </p>
+        <p>
+          Or you can ask us a question online through Ask VA. Select the
+          category and topic for the VA benefit this form is related to.
+        </p>
+        <va-link
+          text="Contact us online
+        through Ask VA"
+          label="Contact us online
+        through Ask VA"
+          href="ask.va.gov"
+        />
+        <div className="vads-u-margin-top--3 vads-u-margin-bottom--6">
+          <va-link-action
+            text="Go back to VA.gov"
+            label="Go back to VA.gov"
+            href="/"
+            type="secondary"
           />
-          <h2 className="vads-u-margin-top--2">
-            Add or remove a dependent on your VA disability benefits
-          </h2>
-          <div className="vads-u-margin-bottom--2">
-            VA Form 21-686c (with 21P-527EZ and 21-674)
-          </div>
         </div>
-        <div className="inset">
-          <h2
-            id="thank-you-message"
-            className="vads-u-font-size--h3 vads-u-font-family--serif vads-u-margin-top--1"
-          >
-            Thank you for submitting your application
-          </h2>
-          <dl>
-            <dt>
-              <strong>
-                Application for Declaration of Status of Dependents (Form
-                21-686c), and/or
-                <br />
-                Request for Approval of School Attendance (Form 21-674), and/or
-                <br />
-                Application for Veterans Pension (Form 21P-527EZ)
-              </strong>
-            </dt>
-            <dd>
-              {veteranFirstName || veteranLastName
-                ? `for ${veteranFirstName} ${veteranLastName}`
-                : ''}
-            </dd>
-            <dt>
-              <strong>Date submitted</strong>
-            </dt>
-            <dd>
-              {dateSubmitted.isValid()
-                ? dateSubmitted.format('MMMM D, YYYY')
-                : ''}
-            </dd>
-          </dl>
-          <button
-            type="button"
-            className="usa-button screen-only"
-            onClick={this.handlePrintClick}
-          >
-            Print this page for your records
-          </button>
-        </div>
-        <div>
-          <h2 className="vads-u-font-size--h3 vads-u-font-family--serif">
-            How long will it take VA to make a decision on my application?
-          </h2>
+      </section>
 
-          <p>
-            We usually decide on applications within <strong>1 week</strong>. If
-            we need you to provide more information or documents, we’ll contact
-            you by mail.
-          </p>
-          <p>
-            If we haven’t contacted you within a week after you submitted your
-            application, <strong>don’t apply again</strong>. Instead, call our
-            toll-free hotline at{' '}
-            <va-telephone contact="8772228387" vanity="VETS" />. We’re here
-            Monday through Friday, 8:00 am to 8:00 pm ET
-          </p>
-
-          <h2 className="vads-u-font-size--h3 vads-u-font-family--serif">
-            How can I check the status of my application?
-          </h2>
-          <ol className="process">
-            <li className="process-step list-one">
-              <h3 className="vads-u-font-size--h4">Sign in to VA.gov</h3>
-              <p>
-                You can sign in with your existing <ServiceProvidersText />
-                account. <ServiceProvidersTextCreateAcct isFormBased />
-              </p>
-            </li>
-            <li className="process-step list-two">
-              <h3 className="vads-u-font-size--h4">
-                If you haven’t yet verified your identity, complete this process
-                when prompted
-              </h3>
-              <p>
-                This helps keep you information safe, and prevents fraud and
-                identity theft. If you’ve already verified your identity with
-                us, you won’t need to do this again..
-              </p>
-            </li>
-            <li className="process-step list-three">
-              <h3 className="vads-u-font-size--h4">
-                Go to your personalized My VA homepage
-              </h3>
-              <p>
-                Once you’re signed in, you can go to your homepage by clicking
-                on the <strong>My VA link</strong> near the top right of any
-                VA.gov page. You’ll find your application status information in
-                the <strong>Your Applications</strong> section of you homepage.
-              </p>
-              <p>
-                <strong>Note:</strong> Your application status may take some
-                time to appear on our homepage. If you don’t see it there right
-                away, check back later.
-              </p>
-            </li>
-          </ol>
-          <h2 className="vads-u-font-size--h3 vads-u-font-family--serif vads-u-margin-top--0">
-            How will I know if my application to add or remove dependents is
-            approved?
-          </h2>
-          <p className="vads-u-margin-bottom--6">
-            We’ll send you a packet by U.S. mail that includes details of the
-            decision on your claim. If you check your status online and see a
-            decision, allow 7 to 10 business days for your packet to arrive
-            before contacting a VA call center.
-          </p>
-          <h2 className="vads-u-font-size--h3 vads-u-font-family--serif">
-            What if I need to add or remove another dependent now or at a later
-            time?
-          </h2>
-          <p className="vads-u-margin-bottom--6">
-            If something changes in your family status let VA know. Return to
-            the <a href={manifest.rootUrl}>21-686c</a> form, select the option
-            that describes your family status change and complete the form. This
-            will update our records and your benefits pay will be adjusted
-            accordingly.
-          </p>
-          <h2 className="vads-u-font-size--h3 vads-u-font-family--serif">
-            What if I have more questions?
-          </h2>
-          <p className="vads-u-margin-bottom--6">
-            Call <va-telephone contact="8772228387" vanity="VETS" /> and select
-            2. We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
-          </p>
-        </div>
-      </>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    form: state.form,
-  };
+      <h2 className="vads-u-margin-bottom--0 vads-u-padding-bottom--0p5 vads-u-font-size--h3 vads-u-border-bottom--2px vads-u-border-color--primary">
+        Need help?
+      </h2>
+      <GetFormHelp />
+    </>
+  );
 }
 
 ConfirmationPage.propTypes = {
@@ -198,5 +204,3 @@ ConfirmationPage.propTypes = {
     }),
   }),
 };
-
-export default connect(mapStateToProps)(ConfirmationPage);
