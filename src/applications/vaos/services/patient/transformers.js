f31e53b6a45d66763300e0aebbe82037ccbe1932
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2024-12-06 13:30:54 -0800

    Updates transformer to deal with array in response

--- src/applications/vaos/services/patient/transformers.js
+++ src/applications/vaos/services/patient/transformers.js
@@ -1,22 +1,27 @@
 /**
+ * @module services/Patient/transformers
+ */
+
+/**
  * Transforms a patient relationship object returned from the VPG endpoint to the
  * VAOS PatientProviderRelationship format
  *
  * @export
- * @param {patientRelationship} patientRelationship A patient relationship object
- * @returns {PatientProviderRelationship} A PatientRelatiobship resource
+ * @param {patientRelationshisp} patientRelationships A list of patient relationshiops from VPG
+ * @returns {Array<PatientProviderRelationship} Returns an array of PatientProviderRelationship objects
  */
 
-export function transformPatientRelationship(patientRelationship) {
-  const attrs = patientRelationship.attributes.attributes;
-  return {
+export function transformPatientRelationships(patientRelationships) {
+  const relationships = patientRelationships.attributes.attributes;
+
+  return relationships.map(relationship => ({
     resourceType: 'PatientProviderRelationship',
-    providerName: attrs.provider.name,
-    providerId: attrs.provider.cernerId,
-    serviceType: attrs.serviceType.coding.code,
-    locationName: attrs.location.name,
-    clinicName: attrs.clinic.name,
-    vistaId: attrs.clinic.vistaSite,
-    lastSeen: attrs.lastSeen,
-  };
+    providerName: relationship.provider.name,
+    providerId: relationship.provider.cernerId,
+    serviceType: relationship.serviceType.coding.code,
+    locationName: relationship.location.name,
+    clinicName: relationship.clinic.name,
+    vistaId: relationship.clinic.vistaSite,
+    lastSeen: relationship.lastSeen,
+  }));
 }
