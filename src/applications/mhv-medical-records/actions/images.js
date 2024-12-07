ebb36f9902c72f91dfd1ca94a47a49d4afc55300
Mike Moyer <87040148+mmoyer-va@users.noreply.github.com>
2024-12-07 17:47:30 -0500

    MHV-44894 Placeholder for error handling

--- src/applications/mhv-medical-records/actions/images.js
+++ src/applications/mhv-medical-records/actions/images.js
@@ -6,16 +6,36 @@ import {
 } from '../api/MrApi';
 
 export const fetchImageRequestStatus = () => async dispatch => {
-  const response = await getImageRequestStatus();
-  dispatch({ type: Actions.Images.GET_IMAGE_REQUEST_STATUS, response });
+  try {
+    const response = await getImageRequestStatus();
+    dispatch({ type: Actions.Images.GET_IMAGE_REQUEST_STATUS, response });
+  } catch (error) {
+    // TODO: How do we handle failure to get the list of study job statuses?
+  }
 };
 
 export const fetchImageList = studyId => async dispatch => {
-  const response = await getImageList(studyId);
-  dispatch({ type: Actions.Images.GET_IMAGE_LIST, response });
+  try {
+    const response = await getImageList(studyId);
+    dispatch({ type: Actions.Images.GET_IMAGE_LIST, response });
+  } catch (error) {
+    // TODO: How do we handle failure to get the image list for a study?
+  }
 };
 
 export const fetchBbmiNotificationStatus = () => async dispatch => {
-  const response = await getBbmiNotificationStatus();
-  dispatch({ type: Actions.Images.GET_NOTIFICATION_STATUS, payload: response });
+  try {
+    const response = await getBbmiNotificationStatus();
+    dispatch({
+      type: Actions.Images.GET_NOTIFICATION_STATUS,
+      payload: response,
+    });
+  } catch (error) {
+    // TODO: How do we handle failure to get notification status?
+    // For now, dispatching 'null' will give same results as 'false'
+    dispatch({
+      type: Actions.Images.GET_NOTIFICATION_STATUS,
+      payload: null,
+    });
+  }
 };
