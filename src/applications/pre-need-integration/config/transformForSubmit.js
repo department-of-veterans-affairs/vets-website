e7ef1c625b8c96d80f9692d3b707bda497179242
Derek Houck <12766168+derekhouck@users.noreply.github.com>
2024-12-09 13:49:20 -0600

    Refactor escapedCharacterReplacers

--- src/applications/pre-need-integration/config/transformForSubmit.js
+++ src/applications/pre-need-integration/config/transformForSubmit.js
@@ -1,12 +1,11 @@
-import { transformForSubmit as formsSystemTransformForSubmit } from 'platform/forms-system/src/js/helpers';
+import {
+  replaceEscapedCharacters,
+  transformForSubmit as formsSystemTransformForSubmit,
+} from 'platform/forms-system/src/js/helpers';
 
 const escapedCharacterReplacer = (_key, value) => {
   if (typeof value === 'string') {
-    return value
-      .replaceAll('"', "'")
-      .replace(/(?:\r\n|\n\n|\r|\n)/g, '; ')
-      .replace(/(?:\t|\f|\b)/g, '')
-      .replace(/\\(?!(f|n|r|t|[u,U][\d,a-fA-F]{4}))/gm, '/');
+    return replaceEscapedCharacters(value);
   }
 
   return value;
