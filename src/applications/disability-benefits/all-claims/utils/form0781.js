6b5ca845dd9243b392b0818c7c52ba5e14fda333
Sam Stuckey <sam.n.stuckey@gmail.com>
2024-12-09 09:14:02 -0700

    [DBX-97063] 0781 Paper Sync boilerplate (#33210)
    * [DBX-97062/97063] Add flipper protected entrypoint for modern 0781 sync
    
    * general save
    
    * general save
    
    * general save
    
    * general save
    
    * general save
    
    * general save
    
    * general save

---
+++ src/applications/disability-benefits/all-claims/utils/form0781.js
@@ -0,0 +1,25 @@
+// All flippers for the 0781 Papersync should be added to this file
+import { isClaimingNew } from '.';
+
+/**
+ * Checks if the modern 0781 flow should be shown if the flipper is active for this veteran
+ * All 0781 page-specific flippers should include a check against this top level flipper
+ *
+ * @returns
+ *   TRUE if
+ *     - is set on form via the backend
+ *     - Veteran is claiming a new disability
+ *     - Veteran has selected connected condition choices on 'screener page'
+ *   else
+ *     - returns false
+ */
+export function showForm0781Pages(formData) {
+  const conditions = formData?.mentalHealth?.conditions || {};
+  return (
+    formData?.syncModern0781Flow === true &&
+    isClaimingNew(formData) &&
+    Object.entries(conditions).some(
+      ([key, value]) => key !== 'none' && value === true,
+    )
+  );
+}
