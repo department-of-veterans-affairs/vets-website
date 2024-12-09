42f6da1c30bb169134a0704293c5ad12c7944622
jquispe-oddball <jennifer.quispe@oddball.io>
2024-12-09 12:56:24 -0500

    97602 - arp header

--- src/applications/accredited-representative-portal/components/common/Header/UserNav.jsx
+++ src/applications/accredited-representative-portal/components/common/Header/UserNav.jsx
@@ -1,26 +1,21 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-
 import { Link } from 'react-router-dom-v5-compat';
-import { SIGN_IN_URL, SIGN_OUT_URL } from '../../../constants';
+import UserNavLinks from './UserNavLinks';
+
 import {
   selectUserProfile,
   selectIsUserLoading,
 } from '../../../selectors/user';
-
-const generateUniqueId = () =>
-  `account-menu-${Math.random()
-    .toString(36)
-    .substring(2, 11)}`;
+import { SIGN_IN_URL } from '../../../constants';
 
 const UserNav = ({ isMobile }) => {
   const profile = useSelector(selectUserProfile);
   const isLoading = useSelector(selectIsUserLoading);
-  const uniqueId = useRef(generateUniqueId());
 
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const dropdownRef = useRef(null);
+  const dropdownRef = useRef([]);
 
   const toggleDropdown = () => setDropdownOpen(!isDropdownOpen);
 
@@ -47,6 +42,7 @@ const UserNav = ({ isMobile }) => {
   );
 
   let content;
+
   if (isLoading) {
     content = (
       <div className="loading-icon-container">
@@ -56,121 +52,108 @@ const UserNav = ({ isMobile }) => {
         />
       </div>
     );
-  } else if (!profile && isMobile) {
+  } else if (!profile) {
     content = (
-      <a
-        href={SIGN_IN_URL}
-        data-testid="user-nav-mobile-sign-in-link"
-        className="nav__sign-in usa-button"
+      <Link
+        data-testid="user-nav-sign-in-link"
+        className="usa-button usa-button-primary nav__sign-in"
+        to={SIGN_IN_URL}
       >
         Sign in
-      </a>
+      </Link>
     );
-  } else if (!profile && !isMobile) {
+  } else if (profile && isMobile) {
     content = (
-      <a
-        data-testid="user-nav-wider-than-mobile-sign-in-link"
-        className="usa-button usa-button-primary"
-        href={SIGN_IN_URL}
-      >
-        Sign in
-      </a>
-    );
-  } else if (profile) {
-    content = (
-      <div className="vads-u-display--flex vads-u-flex-justify--center">
+      <div className="vads-u-display--flex vads-u-justify-content--center user-nav">
         <div className="va-dropdown" ref={dropdownRef}>
           {/* eslint-disable-next-line @department-of-veterans-affairs/prefer-button-component */}
           <button
             data-testid="user-nav-dropdown-panel-button"
-            className="sign-in-drop-down-panel-button va-btn-withicon va-dropdown-trigger"
-            aria-controls={uniqueId.current}
+            className="usa-button usa-button-secondary nav__user-btn nav__user-btn--menu"
+            aria-controls="menu-dropdown"
             aria-expanded={isDropdownOpen}
             onClick={toggleDropdown}
             type="button"
           >
-            <img
-              data-testid="menu-login"
-              className="nav__profile-icon"
-              src="/img/icons/fa-user.svg"
-              alt="profile dropdown"
+            Menu
+            <va-icon
+              icon="menu"
+              size={2}
+              srtext="toggle menu"
+              class="nav__user-btn-icon"
             />
           </button>
           <div
             className={`va-dropdown-panel ${isDropdownOpen ? '' : 'hidden'}`}
-            id={uniqueId.current}
+            id="menu-dropdown"
           >
-            <ul>
+            <ul className="nav__user-list">
               <li>
-                <Link data-testid="user-nav-profile-link" to="/insert-link">
-                  Profile
+                <Link
+                  data-testid="user-nav-poa-requests-link"
+                  className="vads-u-color--black"
+                  to="/poa-requests"
+                >
+                  POA Requests
                 </Link>
               </li>
               <li>
-                <Link data-testid="user-nav-sign-out-link" to={SIGN_OUT_URL}>
-                  Sign Out
+                <Link
+                  data-testid="user-nav-profile-link"
+                  className="vads-u-color--black"
+                  to="/get-help"
+                >
+                  Get Help
                 </Link>
               </li>
             </ul>
           </div>
         </div>
+      </div>
+    );
+  } else if (profile && !isMobile) {
+    content = (
+      <div className="vads-u-display--flex vads-u-justify-content--center user-nav vads-u-align-items--center">
+        <Link to="/" className="usa-button nav__user-btn nav__user-btn--user ">
+          Get Help
+        </Link>
         <div className="va-dropdown" ref={dropdownRef}>
           {/* eslint-disable-next-line @department-of-veterans-affairs/prefer-button-component */}
-          <va-button
-            onClick={function noRefCheck() {}}
-            secondary
-            text="Secondary"
-          />
           <button
             data-testid="user-nav-dropdown-panel-button"
-            className="sign-in-drop-down-panel-button va-btn-withicon va-dropdown-trigger"
-            aria-controls={uniqueId.current}
+            className="usa-button nav__user-btn nav__user-btn--user"
+            aria-controls="dropdown"
             aria-expanded={isDropdownOpen}
             onClick={toggleDropdown}
             type="button"
           >
-            <span>
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                className="vads-u-display--block vads-u-margin-right--0 medium-screen:vads-u-margin-right--0p5 icon"
-                viewBox="0 2 21 21"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill="#fff"
-                  d="M12 12c-1.1 0-2.04-.4-2.82-1.18A3.85 3.85 0 0 1 8 8c0-1.1.4-2.04 1.18-2.83A3.85 3.85 0 0 1 12 4c1.1 0 2.04.4 2.82 1.17A3.85 3.85 0 0 1 16 8c0 1.1-.4 2.04-1.18 2.82A3.85 3.85 0 0 1 12 12Zm-8 8v-2.8c0-.57.15-1.09.44-1.56a2.9 2.9 0 0 1 1.16-1.09 13.76 13.76 0 0 1 9.65-1.16c1.07.26 2.12.64 3.15 1.16.48.25.87.61 1.16 1.09.3.47.44 1 .44 1.56V20H4Z"
-                />
-              </svg>
-              <div
-                data-testid="user-nav-user-name"
-                className="user-dropdown-email"
-                data-dd-privacy="mask"
-                data-dd-action-name="First Name"
-              >
-                Menu
-              </div>
-            </span>
+            <va-icon
+              icon="person"
+              size={2}
+              srtext="profile dropdown"
+              data-testid="menu-login"
+            />
+            <div
+              data-testid="user-nav-user-name"
+              className="user-dropdown-email"
+              data-dd-privacy="mask"
+              data-dd-action-name="First Name"
+            >
+              {`${profile.firstName}${isMobile ? '' : ` ${profile.lastName}`}`}
+            </div>
+            <va-icon
+              icon="chevron_left"
+              size={2}
+              srtext="profile dropdown"
+              data-testid="menu-user-dropdown"
+              class="user-nav__chevron"
+            />
           </button>
           <div
             className={`va-dropdown-panel ${isDropdownOpen ? '' : 'hidden'}`}
-            id={uniqueId.current}
+            id="dropdown"
           >
-            <ul>
-              <li>
-                <a
-                  data-testid="user-nav-poa-requests-link"
-                  href="/poa-requests"
-                >
-                  POA Requests
-                </a>
-              </li>
-              <li>
-                <a data-testid="user-nav-profile-link" href="/get-help">
-                  Get Help
-                </a>
-              </li>
-            </ul>
+            <UserNavLinks />
           </div>
         </div>
       </div>
