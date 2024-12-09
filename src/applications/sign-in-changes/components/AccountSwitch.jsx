963f90355b9b9afb64846e70bb68d2574912e138
Caitlin <78328496+CaitHawk@users.noreply.github.com>
2024-12-09 08:01:17 -0800

    suggested changes

--- src/applications/sign-in-changes/components/AccountSwitch.jsx
+++ src/applications/sign-in-changes/components/AccountSwitch.jsx
@@ -23,14 +23,14 @@ export default function AccountSwitch({ userEmails }) {
   const userHasIdme = userEmails.idme;
   const userHasLogingov = userEmails.logingov;
   const userHasBoth = userHasIdme && userHasLogingov;
-  const headingText = userHasLogingov ? 'Login.gov' : 'ID.me';
-  const strongTag = (
-    <strong>{userHasBoth ? 'Login.gov or ID.me' : headingText}</strong>
-  );
+  const cspText = userHasLogingov ? 'Login.gov' : 'ID.me';
+  const headingText = `Start using your ${
+    userHasBoth ? 'Login.gov or ID.me' : cspText
+  } account now`;
   return (
     <div>
       <h2 className="vads-u-margin-y--0" id="accountSwitchH2">
-        Start using your {strongTag} account now
+        {headingText}
       </h2>
       {userHasLogingov && (
         <CspDisplay
