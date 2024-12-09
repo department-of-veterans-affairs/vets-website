89494c68978a00d9e58e6b170117b3e2628ca8c1
Matthew Wright <107576133+mattwrightva@users.noreply.github.com>
2024-12-08 17:28:48 -0700

    MHV-64869: Demographics, family history, and food journal done

--- src/applications/mhv-medical-records/util/pdfHelpers/sei/familyHistory.js
+++ src/applications/mhv-medical-records/util/pdfHelpers/sei/familyHistory.js
@@ -1,12 +1,37 @@
 export const generateFamilyHistoryContent = record => {
   return {
-    title: record.asdf,
+    title: record.relationship,
     details: [
       {
         items: [
           {
-            title: 'asdf',
-            value: record.asdf,
+            title: 'First name',
+            value: record.firstName,
+            inline: true,
+          },
+          {
+            title: 'Last name',
+            value: record.lastName,
+            inline: true,
+          },
+          {
+            title: 'Living or deceased',
+            value: record.livingOrDeceased,
+            inline: true,
+          },
+          {
+            title: 'Health issues',
+            value: record.healthIssues,
+            inline: true,
+          },
+          {
+            title: 'Other health issues',
+            value: record.otherHealthIssues,
+            inline: true,
+          },
+          {
+            title: 'Comments',
+            value: record.comments,
             inline: true,
           },
         ],
--- src/applications/mhv-medical-records/util/pdfHelpers/sei/index.js
+++ src/applications/mhv-medical-records/util/pdfHelpers/sei/index.js
@@ -1,6 +1,9 @@
 import { selfEnteredTypes } from '../../constants';
 import { generateActivityJournalContent } from './activityJournal';
 import { generateAllergiesContent } from './allergies';
+import { generateDemographicsContent } from './demographics';
+import { generateFamilyHistoryContent } from './familyHistory';
+import { generateFoodJournalContent } from './foodJournal';
 
 export const generateSelfEnteredData = ({
   activityJournal,
@@ -8,16 +11,16 @@ export const generateSelfEnteredData = ({
   demographics,
   familyHistory,
   foodJournal,
-  goals,
-  providers,
-  healthInsurance,
-  testEntries,
-  medicalEvents,
-  medications,
-  militaryHistory,
-  treatmentFacilities,
-  vaccines,
-  vitals,
+  // goals,
+  // providers,
+  // healthInsurance,
+  // testEntries,
+  // medicalEvents,
+  // medications,
+  // militaryHistory,
+  // treatmentFacilities,
+  // vaccines,
+  // vitals,
 }) => {
   const data = [];
 
@@ -47,44 +50,137 @@ export const generateSelfEnteredData = ({
   }
 
   if (demographics) {
-    // console.log('demographics: ', demographics);
-  }
-  if (familyHistory) {
-    // console.log('familyHistory: ', familyHistory);
-  }
-  if (foodJournal) {
-    // console.log('foodJournal: ', foodJournal);
-  }
-  if (goals) {
-    // console.log('goals: ', goals);
-  }
-  if (providers) {
-    // console.log('providers: ', providers);
-  }
-  if (healthInsurance) {
-    // console.log('healthInsurance: ', healthInsurance);
-  }
-  if (testEntries) {
-    // console.log('testEntries: ', testEntries);
-  }
-  if (medicalEvents) {
-    // console.log('medicalEvents: ', medicalEvents);
-  }
-  if (medications) {
-    // console.log('medications: ', medications);
-  }
-  if (militaryHistory) {
-    // console.log('militaryHistory: ', militaryHistory);
-  }
-  if (treatmentFacilities) {
-    // console.log('treatmentFacilities: ', treatmentFacilities);
-  }
-  if (vaccines) {
-    // console.log('vaccines: ', vaccines);
-  }
-  if (vitals) {
-    // console.log('vitals: ', vitals);
+    data.push({
+      type: selfEnteredTypes.DEMOGRAPHICS,
+      title: `Self-entered ${selfEnteredTypes.DEMOGRAPHICS}`,
+      records: generateDemographicsContent(demographics),
+    });
   }
 
+  if (familyHistory) {
+    data.push({
+      type: selfEnteredTypes.FAMILY_HISTORY,
+      title: `Self-entered ${selfEnteredTypes.FAMILY_HISTORY}`,
+      subtitles: [
+        `Showing ${
+          familyHistory.length
+        } records, alphabetically by relationship`,
+      ],
+      records: familyHistory.map(record =>
+        generateFamilyHistoryContent(record),
+      ),
+    });
+  }
+
+  if (foodJournal) {
+    data.push({
+      type: selfEnteredTypes.FOOD_JOURNAL,
+      title: `Self-entered ${selfEnteredTypes.FOOD_JOURNAL}`,
+      subtitles: [
+        `Showing ${foodJournal.length} records, from newest to oldest`,
+      ],
+      records: foodJournal.map(record => generateFoodJournalContent(record)),
+    });
+  }
+
+  // if (goals) {
+  //   console.log('goals: ', goals);
+  //   data.push({
+  //     type: selfEnteredTypes.ASDF,
+  //     title: `Self-entered ${selfEnteredTypes.ASDF}`,
+  //     subtitles: [`Showing ${ASDF.length} records, from newest to oldest`],
+  //     records: ASDF.map(record => generateASDFContent(record)),
+  //   });
+  // }
+
+  // if (providers) {
+  //   console.log('providers: ', providers);
+  //   data.push({
+  //     type: selfEnteredTypes.ASDF,
+  //     title: `Self-entered ${selfEnteredTypes.ASDF}`,
+  //     subtitles: [`Showing ${ASDF.length} records, from newest to oldest`],
+  //     records: ASDF.map(record => generateASDFContent(record)),
+  //   });
+  // }
+
+  // if (healthInsurance) {
+  //   console.log('healthInsurance: ', healthInsurance);
+  //   data.push({
+  //     type: selfEnteredTypes.ASDF,
+  //     title: `Self-entered ${selfEnteredTypes.ASDF}`,
+  //     subtitles: [`Showing ${ASDF.length} records, from newest to oldest`],
+  //     records: ASDF.map(record => generateASDFContent(record)),
+  //   });
+  // }
+
+  // if (testEntries) {
+  //   console.log('testEntries: ', testEntries);
+  //   data.push({
+  //     type: selfEnteredTypes.ASDF,
+  //     title: `Self-entered ${selfEnteredTypes.ASDF}`,
+  //     subtitles: [`Showing ${ASDF.length} records, from newest to oldest`],
+  //     records: ASDF.map(record => generateASDFContent(record)),
+  //   });
+  // }
+
+  // if (medicalEvents) {
+  //   console.log('medicalEvents: ', medicalEvents);
+  //   data.push({
+  //     type: selfEnteredTypes.ASDF,
+  //     title: `Self-entered ${selfEnteredTypes.ASDF}`,
+  //     subtitles: [`Showing ${ASDF.length} records, from newest to oldest`],
+  //     records: ASDF.map(record => generateASDFContent(record)),
+  //   });
+  // }
+
+  // if (medications) {
+  //   console.log('medications: ', medications);
+  //   data.push({
+  //     type: selfEnteredTypes.ASDF,
+  //     title: `Self-entered ${selfEnteredTypes.ASDF}`,
+  //     subtitles: [`Showing ${ASDF.length} records, from newest to oldest`],
+  //     records: ASDF.map(record => generateASDFContent(record)),
+  //   });
+  // }
+
+  // if (militaryHistory) {
+  //   console.log('militaryHistory: ', militaryHistory);
+  //   data.push({
+  //     type: selfEnteredTypes.ASDF,
+  //     title: `Self-entered ${selfEnteredTypes.ASDF}`,
+  //     subtitles: [`Showing ${ASDF.length} records, from newest to oldest`],
+  //     records: ASDF.map(record => generateASDFContent(record)),
+  //   });
+  // }
+
+  // if (treatmentFacilities) {
+  //   console.log('treatmentFacilities: ', treatmentFacilities);
+  //   data.push({
+  //     type: selfEnteredTypes.ASDF,
+  //     title: `Self-entered ${selfEnteredTypes.ASDF}`,
+  //     subtitles: [`Showing ${ASDF.length} records, from newest to oldest`],
+  //     records: ASDF.map(record => generateASDFContent(record)),
+  //   });
+  // }
+
+  // if (vaccines) {
+  //   console.log('vaccines: ', vaccines);
+  //   data.push({
+  //     type: selfEnteredTypes.ASDF,
+  //     title: `Self-entered ${selfEnteredTypes.ASDF}`,
+  //     subtitles: [`Showing ${ASDF.length} records, from newest to oldest`],
+  //     records: ASDF.map(record => generateASDFContent(record)),
+  //   });
+  // }
+
+  // if (vitals) {
+  //   console.log('vitals: ', vitals);
+  //   data.push({
+  //     type: selfEnteredTypes.ASDF,
+  //     title: `Self-entered ${selfEnteredTypes.ASDF}`,
+  //     subtitles: [`Showing ${ASDF.length} records, from newest to oldest`],
+  //     records: ASDF.map(record => generateASDFContent(record)),
+  //   });
+  // }
   return data;
 };
