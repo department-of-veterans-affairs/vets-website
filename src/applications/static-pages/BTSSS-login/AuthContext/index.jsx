3c0672183ea182c7b844b8104c7e9d121e5d8686
Liz Townsend <liz.townsend@adhocteam.us>
2024-12-10 08:51:34 -0600

    add toggle for travel pay power switch

--- src/applications/static-pages/BTSSS-login/AuthContext/index.jsx
+++ src/applications/static-pages/BTSSS-login/AuthContext/index.jsx
@@ -1,22 +1,58 @@
 import React from 'react';
+import { useFeatureToggle } from 'platform/utilities/feature-toggles/useFeatureToggle';
 
-const AuthContext = () => (
-  <>
-    <p>
-      You can file a claim online through the Beneficiary Travel Self Service
-      System (BTSSS).
-    </p>
-    <va-link-action
-      href="https://dvagov-btsss.dynamics365portals.us/signin"
-      text="Go to BTSSS to file a claim"
-    />
-    <p>You can also check your travel claim status here on VA.gov</p>
-    <va-link-action
-      type="secondary"
-      href="/my-health/travel-claim-status"
-      text="Check your travel claim status"
-    />
-  </>
-);
+const AuthContext = () => {
+  const {
+    useToggleValue,
+    useToggleLoadingValue,
+    TOGGLE_NAMES,
+  } = useFeatureToggle();
+
+  const toggleIsLoading = useToggleLoadingValue();
+  const appEnabled = useToggleValue(TOGGLE_NAMES.travelPayPowerSwitch);
+
+  if (toggleIsLoading) {
+    return (
+      <div className="vads-l-grid-container vads-u-padding-y--3">
+        <va-loading-indicator
+          label="Loading"
+          message="Please wait while we load the application for you."
+          data-testid="travel-pay-loading-indicator"
+        />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <p>
+        You can file a claim online through the Beneficiary Travel Self Service
+        System (BTSSS).
+      </p>
+      <va-link-action
+        href="https://dvagov-btsss.dynamics365portals.us/signin"
+        text="Go to BTSSS to file a claim"
+      />
+      <p>You can also check your travel claim status here on VA.gov</p>
+      {appEnabled && (
+        <va-link-action
+          type="secondary"
+          href="/my-health/travel-claim-status"
+          text="Check your travel claim status"
+        />
+      )}
+    </>
+  );
+};
+
+// AuthContext.propTypes = {
+//   appEnabled: PropTypes.bool,
+// };
+
+// const mapStateToProps = store => ({
+//   appEnabled: toggleValues(store)[FEATURE_FLAG_NAMES.travelPayPowerSwitch],
+// });
 
 export default AuthContext;
+
+// export default connect(mapStateToProps)(AuthContext);
