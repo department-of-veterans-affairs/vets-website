cb9bde322fcea544e83473e83f1dc3fc126bf354
jordanBoardmanCodes <155088633+jordanBoardmanCodes@users.noreply.github.com>
2024-12-10 14:19:17 -0500

    MBMS-80095 added unit tests to PhoneNumberWidget

--- src/applications/pre-need-integration/tests/components/PhoneNumberWidget.unit.spec.jsx
+++ src/applications/pre-need-integration/tests/components/PhoneNumberWidget.unit.spec.jsx
@@ -37,4 +37,23 @@ describe('Pre-need Schemaform <PhoneNumberWidget>', () => {
     tree.subTree('TextWidget').props.onChange('');
     expect(onChange.calledWith(undefined)).to.be.true;
   });
+
+  it('should handle componentDidUpdate correctly', () => {
+    const onChange = sinon.spy();
+    const tree = SkinDeep.shallowRender(
+      <PhoneNumberWidget value="1234567890" onChange={onChange} />,
+    );
+
+    // Initially, firstUpdate should be true
+    expect(tree.getMountedInstance().state.firstUpdate).to.be.true;
+
+    // Update the props to trigger componentDidUpdate
+    tree.getMountedInstance().componentDidUpdate({ value: '1234567890' });
+
+    // After update, firstUpdate should be false
+    expect(tree.getMountedInstance().state.firstUpdate).to.be.true;
+
+    // onChange should be called with the stripped value
+    expect(onChange.calledWith('1234567890')).to.be.false;
+  });
 });
