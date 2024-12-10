795dbb77cbb30475eb03f02a158d80d4c1772141
Eric Tillberg <Thrillberg@users.noreply.github.com>
2024-12-10 09:23:53 -0500

    Form Upload retains file after nav away

--- src/platform/forms-system/src/js/web-component-fields/vaFileInputFieldHelpers.jsx
+++ src/platform/forms-system/src/js/web-component-fields/vaFileInputFieldHelpers.jsx
@@ -3,19 +3,6 @@ import { uploadFile } from 'platform/forms-system/src/js/actions';
 const MAX_FILE_SIZE_MB = 25;
 const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1000 ** 2;
 
-export const areFilesEqual = (file1, file2) => {
-  if (!file1 || !file2) {
-    return false;
-  }
-
-  return (
-    file1.name === file2.name &&
-    file1.size === file2.size &&
-    file1.type === file2.type &&
-    file1.lastModified === file2.lastModified
-  );
-};
-
 const createPayload = (file, formId) => {
   const payload = new FormData();
   payload.set('form_id', formId);
@@ -34,7 +21,7 @@ export const uploadScannedForm = (
     fileTypes: ['pdf', 'jpg', 'jpeg', 'png'],
     maxSize: MAX_FILE_SIZE_BYTES,
     createPayload,
-    parseResponse: ({ data }) => data?.attributes,
+    parseResponse: ({ data }, file) => ({ ...data?.attributes, file }),
   };
 
   return dispatch => {
