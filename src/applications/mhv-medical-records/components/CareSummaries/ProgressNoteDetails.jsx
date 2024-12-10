4d6bfe15e89c9776f019d7c5a7bb3dda594b5a55
Alex Morgun <87077843+oleksii-morgun@users.noreply.github.com>
2024-12-10 10:34:13 -0500

    MHV-63291 care summaries tags

--- src/applications/mhv-medical-records/components/CareSummaries/ProgressNoteDetails.jsx
+++ src/applications/mhv-medical-records/components/CareSummaries/ProgressNoteDetails.jsx
@@ -110,11 +110,15 @@ ${record.note}`;
 
       {downloadStarted && <DownloadSuccessAlert />}
       <PrintDownload
+        description="CS&N Detail"
         downloadPdf={generateCareNotesPDF}
         downloadTxt={generateCareNotesTxt}
         allowTxtDownloads={allowTxtDownloads}
       />
-      <DownloadingRecordsInfo allowTxtDownloads={allowTxtDownloads} />
+      <DownloadingRecordsInfo
+        description="CS&N Detail"
+        allowTxtDownloads={allowTxtDownloads}
+      />
 
       <div className="test-details-container max-80">
         <h2>Details</h2>
