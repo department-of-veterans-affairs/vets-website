fbc84e28cd207444f0e17a5211ca2594ab0133c1
Nick Sayre <nick.sayre@adhocteam.us>
2024-12-10 17:40:08 -0600

    1181 - ensure card size and layout works on all screen sizes

--- src/applications/personalization/profile/components/proof-of-veteran-status/ProofOfVeteranStatusCard/ProofOfVeteranStatusCard.jsx
+++ src/applications/personalization/profile/components/proof-of-veteran-status/ProofOfVeteranStatusCard/ProofOfVeteranStatusCard.jsx
@@ -9,7 +9,7 @@ const ProofOfVeteranStatusCard = ({
   totalDisabilityRating,
 }) => {
   return (
-    <div className="proofOfVeteranStatusCard vads-l-col--12 medium-screen:vads-l-col--9 vads-u-margin-bottom--2 vads-u-background-color--primary-darker vads-u-color--white ">
+    <div className="proof-of-veteran-status-card vads-u-margin-bottom--2 vads-u-background-color--primary-darker vads-u-color--white ">
       <h3 className="vads-u-background-color--primary-dark vads-u-margin-top--0 vads-u-padding-top--2p5 vads-u-padding-left--2 vads-u-padding-bottom--2">
         Proof of Veteran status
       </h3>
@@ -27,8 +27,8 @@ const ProofOfVeteranStatusCard = ({
             Latest period of service
           </dt>
           <dd>{latestService}</dd>
-          <div className="medium-screen:vads-l-col--9 vads-u-margin-top--1">
-            <div className="vads-l-row vads-u-justify-content--space-between">
+          <div className="vads-u-margin-top--1">
+            <div className="status-card-row">
               <div>
                 <dt className="vads-u-font-weight--bold">DOD ID Number</dt>
                 <dd>{edipi}</dd>
