0918a6c9ed37547bacb2c3eab876fcaa5892f7af
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2024-12-11 16:11:58 -0500

    Veteran unit tests

---
+++ src/applications/representative-appoint/tests/pages/veteran/veteranContactMailing.unit.spec.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { expect } from 'chai';
+import { DefinitionTester } from 'platform/testing/unit/schemaform-utils';
+
+import mockFormData from '../../fixtures/data/form-data.json';
+
+import formConfig from '../../../config/form';
+
+describe('Veteran Contact Mailing page', () => {
+  const {
+    schema,
+    uiSchema,
+  } = formConfig.chapters.claimantInfo.pages.veteranContactMailing;
+
+  it('should render', () => {
+    const { container } = render(
+      <DefinitionTester
+        definitions={{}}
+        schema={schema}
+        uiSchema={uiSchema}
+        data={mockFormData}
+        formData={mockFormData}
+      />,
+    );
+
+    expect(container.querySelector('button[type="submit"]')).to.exist;
+  });
+});
