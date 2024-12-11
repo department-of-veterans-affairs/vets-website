8dd476a071fa6e16cd5de60cc3dcb41935fa1a77
Liz Townsend <liz.townsend@adhocteam.us>
2024-12-11 08:25:36 -0600

    move shared text to wrapper, add test for auth

--- src/applications/static-pages/BTSSS-login/AuthContext/index.jsx
+++ src/applications/static-pages/BTSSS-login/AuthContext/index.jsx
@@ -25,21 +25,21 @@ const AuthContext = () => {
 
   return (
     <>
-      <p>
-        You can file a claim online through the Beneficiary Travel Self Service
-        System (BTSSS).
-      </p>
       <va-link-action
+        data-testid="btsss-link"
         href="https://dvagov-btsss.dynamics365portals.us/signin"
         text="Go to BTSSS to file a claim"
       />
-      <p>You can also check your travel claim status here on VA.gov</p>
       {appEnabled && (
-        <va-link-action
-          type="secondary"
-          href="/my-health/travel-claim-status"
-          text="Check your travel claim status"
-        />
+        <>
+          <p>You can also check your travel claim status here on VA.gov</p>
+          <va-link-action
+            data-testid="vagov-travel-pay-link"
+            type="secondary"
+            href="/my-health/travel-claim-status"
+            text="Check your travel claim status"
+          />
+        </>
       )}
     </>
   );
