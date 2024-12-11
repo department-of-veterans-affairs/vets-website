b174ffa0bdf4ac7d33efc800ee1b16c17a9504ac
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2024-12-11 02:47:02 -0500

    udate test in utilities

--- src/applications/edu-benefits/10216/tests/utilities.unit.spec.js
+++ src/applications/edu-benefits/10216/tests/utilities.unit.spec.js
@@ -1,15 +1,22 @@
 import { expect } from 'chai';
-import sinon from 'sinon';
 import { validateFacilityCode } from '../utilities';
 
 global.window = { location: { href: '' } };
 
+const mockApiRequest = async url => {
+  if (url.includes('31850932')) {
+    return { data: { attributes: { accredited: true } } };
+  }
+  if (url.includes('123')) {
+    throw new Error('API error');
+  }
+  return null;
+};
+
 describe('Utilities Tests', () => {
   describe('validateFacilityCode', () => {
-    let apiRequestStub;
-
     beforeEach(() => {
-      apiRequestStub = sinon.stub();
+      global.apiRequest = mockApiRequest;
     });
 
     it('should return accredited status from API', async () => {
@@ -20,17 +27,14 @@ describe('Utilities Tests', () => {
           startDate: '2024-01-01',
         },
       };
-      const mockResponse = { data: { attributes: { accredited: true } } };
-      apiRequestStub.returns(Promise.resolve(mockResponse));
 
       const result = await validateFacilityCode(field);
 
       expect(result).to.be.true;
     });
+
     it('should return false on API error', async () => {
       const field = { institutionDetails: { facilityCode: '123' } };
-
-      apiRequestStub.returns(Promise.reject(new Error('API error')));
 
       const result = await validateFacilityCode(field);
 
