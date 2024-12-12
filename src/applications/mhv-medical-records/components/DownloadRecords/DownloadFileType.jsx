fb770080ecae2151c9d1f6f85e9ab7ed80d92117
Mike Moyer <87040148+mmoyer-va@users.noreply.github.com>
2024-12-12 17:31:25 -0500

    MHV-65059 Error handling for BB download (#33535)
    * MHV-65059 Don't overwrite data if key is missing from the action payload
    
    * MHV-65059 Modified action to only call necessary APIs
    
    * MHV-65059 Harmonized flow for generating BB PDF
    
    * MHV-65059 Revert test data
    
    * MHV-65059 Started adding error-handling
    
    * MHV-65059 More work on error handling
    
    * MHV-65059 Fixed mismatched field
    
    * MHV-65059 Added error alert for failed APIs
    
    * MHV-65059 Fixed unit tests
    
    * MHV-65059 Added a test for failed calls
    
    * MHV-65059 Fixed URL for appointments

--- src/applications/mhv-medical-records/components/DownloadRecords/DownloadFileType.jsx
+++ src/applications/mhv-medical-records/components/DownloadRecords/DownloadFileType.jsx
@@ -54,6 +54,7 @@ const DownloadFileType = props => {
   const accountSummary = useSelector(
     state => state.mr.blueButton.accountSummary,
   );
+  const failedDomains = useSelector(state => state.mr.blueButton.failedDomains);
 
   const recordFilter = useSelector(state => state.mr.downloads?.recordFilter);
   const dateFilter = useSelector(state => state.mr.downloads?.dateFilter);
@@ -102,8 +103,29 @@ const DownloadFileType = props => {
         accountSummary,
       };
 
-      // Check if all domains in the recordFilter are truthy
-      return recordFilter?.every(filter => !!dataMap[filter]);
+      // Map the recordFilter keys to the option list
+      const optionsMap = {
+        labTests: 'labsAndTests',
+        careSummaries: 'notes',
+        vaccines: 'vaccines',
+        allergies: 'allergies',
+        conditions: 'conditions',
+        vitals: 'vitals',
+        medications: 'medications',
+        upcomingAppts: 'appointments',
+        pastAppts: 'appointments',
+        demographics: 'demographics',
+        militaryService: 'militaryService',
+        accountSummary: 'patient',
+      };
+
+      // Check if all domains in the recordFilter were fetched or failed
+      return recordFilter?.every(filter => {
+        const optionDomain = optionsMap[filter];
+        const isFetched = !!dataMap[filter];
+        const hasFailed = failedDomains.includes(optionDomain);
+        return isFetched || hasFailed;
+      });
     },
     [
       labsAndTests,
@@ -117,6 +139,7 @@ const DownloadFileType = props => {
       demographics,
       militaryService,
       accountSummary,
+      failedDomains,
       recordFilter,
     ],
   );
@@ -124,7 +147,7 @@ const DownloadFileType = props => {
   useEffect(
     () => {
       const options = {
-        labs: recordFilter?.includes('labTests'),
+        labsAndTests: recordFilter?.includes('labTests'),
         notes: recordFilter?.includes('careSummaries'),
         vaccines: recordFilter?.includes('vaccines'),
         allergies:
@@ -138,6 +161,7 @@ const DownloadFileType = props => {
           recordFilter?.includes('pastAppts'),
         demographics: recordFilter?.includes('demographics'),
         militaryService: recordFilter?.includes('militaryService'),
+        patient: true,
       };
 
       if (!isDataFetched) {
@@ -151,30 +175,40 @@ const DownloadFileType = props => {
     () => {
       if (isDataFetched) {
         return {
-          labsAndTests: recordFilter?.includes('labTests')
-            ? labsAndTests.filter(rec => filterByDate(rec.sortDate))
-            : null,
-          notes: recordFilter?.includes('careSummaries')
-            ? notes.filter(rec => filterByDate(rec.sortByDate))
-            : null,
-          vaccines: recordFilter?.includes('vaccines')
-            ? vaccines.filter(rec => filterByDate(rec.date))
-            : null,
+          labsAndTests:
+            labsAndTests && recordFilter?.includes('labTests')
+              ? labsAndTests.filter(rec => filterByDate(rec.sortDate))
+              : null,
+          notes:
+            notes && recordFilter?.includes('careSummaries')
+              ? notes.filter(rec => filterByDate(rec.sortByDate))
+              : null,
+          vaccines:
+            vaccines && recordFilter?.includes('vaccines')
+              ? vaccines.filter(rec => filterByDate(rec.date))
+              : null,
           allergies:
-            recordFilter?.includes('allergies') ||
-            recordFilter?.includes('medications')
+            allergies &&
+            (recordFilter?.includes('allergies') ||
+              recordFilter?.includes('medications'))
               ? allergies
               : null,
-          conditions: recordFilter?.includes('conditions') ? conditions : null,
-          vitals: recordFilter?.includes('vitals')
-            ? vitals.filter(rec => filterByDate(rec.date))
-            : null,
-          medications: recordFilter?.includes('medications')
-            ? medications.filter(rec => filterByDate(rec.lastFilledOn))
-            : null,
+          conditions:
+            conditions && recordFilter?.includes('conditions')
+              ? conditions
+              : null,
+          vitals:
+            vitals && recordFilter?.includes('vitals')
+              ? vitals.filter(rec => filterByDate(rec.date))
+              : null,
+          medications:
+            medications && recordFilter?.includes('medications')
+              ? medications.filter(rec => filterByDate(rec.lastFilledOn))
+              : null,
           appointments:
-            recordFilter?.includes('upcomingAppts') ||
-            recordFilter?.includes('pastAppts')
+            appointments &&
+            (recordFilter?.includes('upcomingAppts') ||
+              recordFilter?.includes('pastAppts'))
               ? appointments.filter(
                   rec =>
                     filterByDate(rec.date) &&
@@ -183,12 +217,14 @@ const DownloadFileType = props => {
                       (recordFilter.includes('pastAppts') && !rec.isUpcoming)),
                 )
               : null,
-          demographics: recordFilter?.includes('demographics')
-            ? demographics
-            : null,
-          militaryService: recordFilter?.includes('militaryService')
-            ? militaryService
-            : null,
+          demographics:
+            demographics && recordFilter?.includes('demographics')
+              ? demographics
+              : null,
+          militaryService:
+            militaryService && recordFilter?.includes('militaryService')
+              ? militaryService
+              : null,
           accountSummary,
         };
       }
