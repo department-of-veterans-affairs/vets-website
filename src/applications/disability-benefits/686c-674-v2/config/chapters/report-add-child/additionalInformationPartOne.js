b6e9f5809a7568c8db8fc5d81c7e8f5b83764c0b
Dakota Larson <dakota.larson@oddball.io>
2024-12-12 12:11:07 -0800

    doc submission info wip

--- src/applications/disability-benefits/686c-674-v2/config/chapters/report-add-child/additionalInformationPartOne.js
+++ src/applications/disability-benefits/686c-674-v2/config/chapters/report-add-child/additionalInformationPartOne.js
@@ -1,45 +1,34 @@
+import React from 'react';
 import {
   titleUI,
-  radioUI,
-  radioSchema,
+  yesNoUI,
+  yesNoSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
 
 export const additionalInformationPartOne = {
   uiSchema: {
     ...titleUI('Additional information about this child'),
 
-    doesChildLiveWithYou: radioUI({
+    doesChildLiveWithYou: yesNoUI({
       title: 'Does this child live with you?',
       required: () => true,
-      labels: {
-        Y: 'Yes',
-        N: 'No',
-      },
       errorMessages: {
         required: 'You must answer this question.',
       },
     }),
 
-    hasChildEverBeenMarried: radioUI({
+    hasChildEverBeenMarried: yesNoUI({
       title: 'Has this child ever been married?',
       required: () => true,
-      labels: {
-        Y: 'Yes',
-        N: 'No',
-      },
       errorMessages: {
         required: 'You must answer this question.',
       },
     }),
 
-    isChildPermanentlyUnableToSupport: radioUI({
+    isChildPermanentlyUnableToSupport: yesNoUI({
       title:
         'Is this child permanently unable to support themselves because they developed a permanent mental or physical disability before they turned 18 years old?',
-      // required: () => false,
-      labels: {
-        Y: 'Yes',
-        N: 'No',
-      },
+      required: () => true,
       errorMessages: {
         required: 'You must answer this question.',
       },
@@ -50,51 +39,43 @@ export const additionalInformationPartOne = {
       },
     }),
 
-    // documentSubmissionInfo: {
-    //   'ui:description': (
-    //     <div>
-    //       <p>
-    //         We’ll ask you to submit these documents at the end of this form:
-    //       </p>
-    //       <ul>
-    //         <li>
-    //           Copies of medical records that document your child’s permanent
-    //           physical disability, <strong>and</strong>
-    //         </li>
-    //         <li>
-    //           A statement from your child’s doctor that shows the type{' '}
-    //           <strong>and</strong> severity of the child's physical or mental
-    //           disability.
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   ),
-    //   'ui:options': {
-    //     expandUnder: 'isChildPermanentlyUnableToSupport',
-    //     expandUnderCondition: 'Y',
-    //     hideIf: (formData, _index) => {
-    //       const { addDisabledChild } =
-    //       formData?.['view:selectable686Options'] ?? {};
-    //       return !addDisabledChild;
-    //     },
-    //   },
-    // },
+    'view:documentSubmissionInfo': {
+      'ui:description': (
+        <div>
+          <p>
+            We’ll ask you to submit these documents at the end of this form:
+          </p>
+          <ul>
+            <li>
+              Copies of medical records that document your child’s permanent
+              physical disability, <strong>and</strong>
+            </li>
+            <li>
+              A statement from your child’s doctor that shows the type{' '}
+              <strong>and</strong> severity of their physical or mental
+              disability.
+            </li>
+          </ul>
+        </div>
+      ),
+      'ui:options': {
+        hideIf: (formData, index) => {
+          return formData?.childrenToAdd?.[index]
+            ?.isChildPermanentlyUnableToSupport;
+        },
+      },
+    },
   },
   schema: {
     type: 'object',
     properties: {
-      doesChildLiveWithYou: radioSchema(['Y', 'N']),
-      hasChildEverBeenMarried: radioSchema(['Y', 'N']),
-      // isChildPermanentlyUnableToSupport: radioSchema(['Y', 'N']),
-      // documentSubmissionInfo: {
-      //   type: 'object',
-      //   properties: {},
-      // },
+      doesChildLiveWithYou: yesNoSchema,
+      hasChildEverBeenMarried: yesNoSchema,
+      isChildPermanentlyUnableToSupport: yesNoSchema,
+      'view:documentSubmissionInfo': {
+        type: 'object',
+        properties: {},
+      },
     },
-    required: [
-      'doesChildLiveWithYou',
-      'hasChildEverBeenMarried',
-      // 'isChildPermanentlyUnableToSupport',
-    ],
   },
 };
