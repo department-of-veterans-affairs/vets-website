b05a463b68db411f57f2b6d39f5ad161f1f9649c
Alex Morgun <87077843+oleksii-morgun@users.noreply.github.com>
2024-12-13 11:19:38 -0500

    MHV-62747 vaccines updates with action name

--- src/applications/mhv-medical-records/components/RecordList/VaccinesListItem.jsx
+++ src/applications/mhv-medical-records/components/RecordList/VaccinesListItem.jsx
@@ -32,24 +32,30 @@ const VaccinesListItem = props => {
         className="print-only vads-u-margin-bottom--1 vads-u-margin-top--0"
         aria-hidden="true"
         data-dd-privacy="mask"
-        data-dd-action-name
+        data-dd-action-name="[vaccines list - name]"
       >
         {record.name}
       </h2>
 
       {/* fields */}
-      <div className="print-indent" data-dd-privacy="mask" data-dd-action-name>
+      <div className="print-indent">
         <span className="vads-u-display--inline print-bold">
           Date received:
         </span>{' '}
-        <span className="vads-u-display--inline">{record.date}</span>
+        <span
+          className="vads-u-display--inline"
+          data-dd-privacy="mask"
+          data-dd-action-name="[vaccines list - date]"
+        >
+          {record.date}
+        </span>
       </div>
       <div className="print-only print-indent">
         <span className="vads-u-display--inline print-bold">Location:</span>{' '}
         <span
           className="vads-u-display--inline"
           data-dd-privacy="mask"
-          data-dd-action-name
+          data-dd-action-name="[vaccines list - location]"
         >
           {record.location}
         </span>
--- src/applications/mhv-medical-records/containers/VaccineDetails.jsx
+++ src/applications/mhv-medical-records/containers/VaccineDetails.jsx
@@ -131,8 +131,9 @@ Location: ${record.location}\n`;
           <h1
             className="vads-u-margin-bottom--0p5"
             aria-describedby="vaccine-date"
-            data-dd-privacy="mask"
             data-testid="vaccine-name"
+            data-dd-privacy="mask"
+            data-dd-action-name="[vaccine details - name]"
           >
             Vaccines: {record.name}
           </h1>
@@ -159,8 +160,9 @@ Location: ${record.location}\n`;
             </h2>
             <p
               className="vads-u-margin-top--0"
-              data-dd-privacy="mask"
               data-testid="vaccine-location"
+              data-dd-privacy="mask"
+              data-dd-action-name="[vaccine details - location]"
             >
               {record.location}
             </p>
