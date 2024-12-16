4f0625c9028fc6c8cd8395bbe5e0c15080a19c48
Kevin Suarez <kjsuarez94@gmail.com>
2024-12-16 10:15:26 -0500

    changes related to conflict resolution

--- src/applications/mhv-landing-page/reducers/index.js
+++ src/applications/mhv-landing-page/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import personalInformation from './personalInformation';
 import accountStatus from './account';
 
 const myHealth = combineReducers({
-  personalInformation,
   accountStatus,
 });
 
