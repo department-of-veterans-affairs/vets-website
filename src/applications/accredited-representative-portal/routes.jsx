ea043319a57524e7fe6ad1887c1035cf0acc5b5f
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2024-12-17 13:50:48 -0500

    loading wrapper

--- src/applications/accredited-representative-portal/routes.jsx
+++ src/applications/accredited-representative-portal/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, useNavigation, Outlet } from 'react-router-dom';
 
+import { VaLoadingIndicator } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import App from './containers/App';
 import LandingPage from './containers/LandingPage';
 import POARequestsPage, {
@@ -11,6 +12,16 @@ import POARequestDetailsPage, {
   poaRequestLoader,
 } from './containers/POARequestDetailsPage';
 import ErrorMessage from './components/common/ErrorMessage';
+
+const LoadingWrapper = () => {
+  const navigation = useNavigation();
+
+  if (navigation.state === 'loading') {
+    return <VaLoadingIndicator message="Loading..." />;
+  }
+
+  return <Outlet />;
+};
 
 const router = createBrowserRouter(
   [
@@ -25,16 +36,21 @@ const router = createBrowserRouter(
           element: <SignedInLayoutWrapper />,
           children: [
             {
-              path: 'poa-requests',
-              element: <POARequestsPage />,
-              loader: poaRequestsLoader,
-              errorElement: <ErrorMessage />,
-            },
-            {
-              path: 'poa-requests/:id',
-              element: <POARequestDetailsPage />,
-              loader: poaRequestLoader,
-              errorElement: <ErrorMessage />,
+              element: <LoadingWrapper />, // Wrap routes with LoadingWrapper
+              children: [
+                {
+                  path: 'poa-requests',
+                  element: <POARequestsPage />,
+                  loader: poaRequestsLoader,
+                  errorElement: <ErrorMessage />,
+                },
+                {
+                  path: 'poa-requests/:id',
+                  element: <POARequestDetailsPage />,
+                  loader: poaRequestLoader,
+                  errorElement: <ErrorMessage />,
+                },
+              ],
             },
           ],
         },
