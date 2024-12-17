c7f36d28ec7bac111bffdc97f04c72cc7daba00a
rjohnson2011 <ryan.johnson@oddball.io>
2024-12-17 06:29:30 -0700

    Conditional to modify your vs veterans dob

--- src/applications/representative-appoint/pages/veteran/veteranContactPhoneEmail.js
+++ src/applications/representative-appoint/pages/veteran/veteranContactPhoneEmail.js
@@ -8,10 +8,17 @@ import {
   titleUI,
   titleSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
+
+import { preparerIsVeteran } from '../../utilities/helpers';
 import ProfileNotUpdatedNote from '../../components/ProfileNotUpdatedNote';
 
 export const uiSchema = {
-  ...titleUI(() => 'Your phone number and email address'),
+  ...titleUI(
+    ({ formData }) =>
+      `${
+        preparerIsVeteran({ formData }) ? 'Your' : 'Veteran’s'
+      } name and date of birth`,
+  ),
   profileNotUpdatedNote: {
     'ui:description': formData => (
       <ProfileNotUpdatedNote formData={formData} includePhone />
