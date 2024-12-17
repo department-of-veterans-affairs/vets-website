dd91489a5e2d6d6d54eba41d2b7e96d62f534cfd
Ni Chia <ni.chia@adhocteam.us>
2024-12-17 09:09:03 -0500

    update verify alert content to use service branded buttons

--- src/applications/mhv-landing-page/tests/components/alerts/AlertMhvBasicAccount.unit.spec.jsx
+++ src/applications/mhv-landing-page/tests/components/alerts/AlertMhvBasicAccount.unit.spec.jsx
@@ -1,47 +1,48 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render, waitFor } from '@testing-library/react';
 import { expect } from 'chai';
 import sinon from 'sinon';
-
-import { logoutUrl } from '@department-of-veterans-affairs/platform-user/authentication/utilities';
-import { logoutUrlSiS } from '~/platform/utilities/oauth/utilities';
 
 import AlertMhvBasicAccount from '../../../components/alerts/AlertMhvBasicAccount';
 
 const { defaultProps } = AlertMhvBasicAccount;
 
+const generateStore = ({
+  csp = 'logingov',
+  verified = false,
+  loading = false,
+} = {}) => ({
+  getState: () => ({
+    user: {
+      profile: {
+        loading,
+        signIn: { serviceName: csp },
+        verified,
+        session: { authBroker: 'iam' },
+      },
+    },
+  }),
+  dispatch: () => {},
+  subscribe: () => {},
+});
+
 describe('<AlertMhvBasicAccount />', () => {
   it('renders', async () => {
+    const store = generateStore();
     const recordEvent = sinon.spy();
     const props = { ...defaultProps, recordEvent };
     const { getByRole, getByTestId } = render(
-      <AlertMhvBasicAccount {...props} />,
+      <Provider store={store}>
+        <AlertMhvBasicAccount {...props} />,
+      </Provider>,
     );
     getByTestId(defaultProps.testId);
+
     getByRole('heading', { name: defaultProps.headline });
     await waitFor(() => {
       expect(recordEvent.calledOnce).to.be.true;
       expect(recordEvent.calledTwice).to.be.false;
-    });
-  });
-
-  describe('Sign out button', () => {
-    it('redirects to logoutUrlSis() when ssoe is false', async () => {
-      const props = { ...defaultProps, ssoe: false };
-      const { getByTestId } = render(<AlertMhvBasicAccount {...props} />);
-      getByTestId('mhv-button--sign-out').click();
-      await waitFor(() => {
-        expect(window.location).to.equal(logoutUrlSiS());
-      });
-    });
-
-    it('redirects to logoutUrl() when ssoe is true', async () => {
-      const props = { ...defaultProps, ssoe: true };
-      const { getByTestId } = render(<AlertMhvBasicAccount {...props} />);
-      getByTestId('mhv-button--sign-out').click();
-      await waitFor(() => {
-        expect(window.location).to.equal(logoutUrl());
-      });
     });
   });
 });
--- src/applications/mhv-landing-page/tests/components/alerts/AlertVerifyAndRegister.unit.spec.jsx
+++ src/applications/mhv-landing-page/tests/components/alerts/AlertVerifyAndRegister.unit.spec.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render, waitFor } from '@testing-library/react';
 import { expect } from 'chai';
 import sinon from 'sinon';
@@ -7,11 +8,35 @@ import AlertVerifyAndRegister from '../../../components/alerts/AlertVerifyAndReg
 
 const { defaultProps } = AlertVerifyAndRegister;
 
+const generateStore = ({
+  csp = 'logingov',
+  verified = false,
+  loading = false,
+} = {}) => ({
+  getState: () => ({
+    user: {
+      profile: {
+        loading,
+        signIn: { serviceName: csp },
+        verified,
+        session: { authBroker: 'iam' },
+      },
+    },
+  }),
+  dispatch: () => {},
+  subscribe: () => {},
+});
+
 describe('<AlertVerifyAndRegister />', () => {
   it('renders', async () => {
+    const store = generateStore();
     const recordEvent = sinon.spy();
     const props = { recordEvent };
-    const { getByTestId } = render(<AlertVerifyAndRegister {...props} />);
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <AlertVerifyAndRegister {...props} />
+      </Provider>,
+    );
     getByTestId(defaultProps.testId);
     await waitFor(() => {
       expect(recordEvent.calledOnce).to.be.true;
