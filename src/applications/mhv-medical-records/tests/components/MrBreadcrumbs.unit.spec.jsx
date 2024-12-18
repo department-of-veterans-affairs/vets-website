3e02b459be6e67e35ae84f922994283d42fbb49e
mdewey <1793923+mdewey@users.noreply.github.com>
2024-12-18 13:03:07 -0500

    (test) branches up to 80%

--- src/applications/mhv-medical-records/tests/components/MrBreadcrumbs.unit.spec.jsx
+++ src/applications/mhv-medical-records/tests/components/MrBreadcrumbs.unit.spec.jsx
@@ -104,6 +104,50 @@ describe('MrBreadcrumbs component', () => {
     expect(header).to.exist;
   });
 
+  it('tests the default useEffect case', () => {
+    const initialState = {
+      mr: {
+        breadcrumbs: {
+          crumbsList: [
+            {
+              href: '/',
+              label: 'VA.gov home',
+            },
+            {
+              href: '/my-health',
+              label: 'My HealtheVet',
+            },
+            {
+              href: '/',
+              label: 'Medical records',
+              isRouterLink: true,
+            },
+          ],
+        },
+      },
+      featureToggles: {
+        // eslint-disable-next-line camelcase
+        mhv_integration_medical_records_to_phase_1: true,
+      },
+    };
+    window.document.querySelector = Sinon.stub().returns({
+      textContent: 'test',
+    });
+    const screen = renderInReduxProvider(
+      <MemoryRouter initialEntries={[`/vitals/blood-pressure-history`]}>
+        <Route path="/vitals/blood-pressure-history">
+          <MrBreadcrumbs />
+        </Route>
+      </MemoryRouter>,
+      {
+        initialState,
+        reducers: reducer,
+      },
+    );
+    const header = screen.getByTestId('breadcrumbs');
+    expect(header).to.exist;
+  });
+
   it('phase 1 disabled && no crumbs list && on the home page, should display the no-crumbs div', () => {
     const initialState = {
       mr: {
