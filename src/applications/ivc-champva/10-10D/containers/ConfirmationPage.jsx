6e4a44110632ab194d48f88a9c2a684d62732948
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2024-12-18 15:18:07 -0600

    updated

--- src/applications/ivc-champva/10-10D/containers/ConfirmationPage.jsx
+++ src/applications/ivc-champva/10-10D/containers/ConfirmationPage.jsx
@@ -138,11 +138,9 @@ export function ConfirmationPage(props) {
       <h2>What to expect next</h2>
       <p>
         {/* It will take about 90 days to process your application. */}
-        <b>
-          Note: Right now there's a delay in processing CHAMPVA applications.
-        </b>{' '}
-        We expect this delay to be temporary. If you have questions about the
-        status of your application, call us at{' '}
+        <b>Note:</b> Right now there's a delay in processing CHAMPVA{' '}
+        applications. We expect this delay to be temporary. If you have
+        questions about the status of your application, call us at{' '}
         <VaTelephone contact="800-733-8387" />
         (TTY: 711). We’re here Monday through Friday, 8:05 a.m. to 7:30 p.m. ET.
         <br />
