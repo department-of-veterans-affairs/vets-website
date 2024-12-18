a078960eec41e08443fa4b543c215aff85ad0300
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2024-12-18 15:58:37 -0600

    space

--- src/applications/ivc-champva/10-10D/containers/ConfirmationPage.jsx
+++ src/applications/ivc-champva/10-10D/containers/ConfirmationPage.jsx
@@ -141,8 +141,8 @@ export function ConfirmationPage(props) {
         <b>Note:</b> Right now there's a delay in processing CHAMPVA{' '}
         applications. We expect this delay to be temporary. If you have
         questions about the status of your application, call us at{' '}
-        <VaTelephone contact="800-733-8387" />
-        (TTY: 711). We’re here Monday through Friday, 8:05 a.m. to 7:30 p.m. ET.
+        <VaTelephone contact="800-733-8387" /> (TTY: 711). We’re here Monday
+        through Friday, 8:05 a.m. to 7:30 p.m. ET.
         <br />
         <br />
         If we have any questions or need additional information, we’ll contact
