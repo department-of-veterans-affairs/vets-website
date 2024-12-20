240bbc1c26f000d29e550d96cbd9c6da547edb38
Liz Townsend <liz.townsend@adhocteam.us>
2024-12-20 10:24:28 -0600

    reuse props object

--- src/applications/travel-pay/tests/components/TravelClaimCard.unit.spec.jsx
+++ src/applications/travel-pay/tests/components/TravelClaimCard.unit.spec.jsx
@@ -6,20 +6,22 @@ import { renderWithStoreAndRouter } from '@department-of-veterans-affairs/platfo
 import TravelClaimCard from '../../components/TravelClaimCard';
 
 describe('TravelClaimCard', () => {
+  const props = {
+    id: '6cecf332-65af-4495-b18e-7fd28ccb546a',
+    claimNumber: '39b7b38f-b7cf-4d19-91cf-fb5360c0b8b8',
+    claimName: '3583ec0e-34e0-4cf5-99d6-78930c2be969',
+    claimStatus: 'Saved',
+    appointmentDateTime: '2023-09-21T17:11:43.034Z',
+    appointmentName: 'Medical imaging',
+    appointmentLocation: 'Tomah VA Medical Center',
+    createdOn: '2023-09-22T17:11:43.034Z',
+    modifiedOn: '2023-09-27T17:11:43.034Z',
+    canViewClaimDetails: false,
+  };
+
   it('should still render when an unknown status is given', () => {
-    const props = {
-      id: '6cecf332-65af-4495-b18e-7fd28ccb546a',
-      claimNumber: '39b7b38f-b7cf-4d19-91cf-fb5360c0b8b8',
-      claimName: '3583ec0e-34e0-4cf5-99d6-78930c2be969',
-      claimStatus: 'Something',
-      appointmentDateTime: '2023-09-21T17:11:43.034Z',
-      appointmentName: 'Medical imaging',
-      appointmentLocation: 'Tomah VA Medical Center',
-      createdOn: '2023-09-22T17:11:43.034Z',
-      modifiedOn: '2023-09-27T17:11:43.034Z',
-    };
     const screen = renderWithStoreAndRouter(
-      <TravelClaimCard {...props} canViewClaimDetails={false} />,
+      <TravelClaimCard {...props} claimStatus="Something" />,
       {
         initialState: {},
       },
@@ -29,19 +31,8 @@ describe('TravelClaimCard', () => {
   });
 
   it('should render proper text when a null appointment date is given', () => {
-    const props = {
-      id: '6cecf332-65af-4495-b18e-7fd28ccb546a',
-      claimNumber: '39b7b38f-b7cf-4d19-91cf-fb5360c0b8b8',
-      claimName: '3583ec0e-34e0-4cf5-99d6-78930c2be969',
-      claimStatus: 'Saved',
-      appointmentDateTime: null,
-      appointmentName: 'Medical imaging',
-      appointmentLocation: 'Tomah VA Medical Center',
-      createdOn: '2023-09-22T17:11:43.034Z',
-      modifiedOn: '2023-09-27T17:11:43.034Z',
-    };
     const screen = renderWithStoreAndRouter(
-      <TravelClaimCard {...props} canViewClaimDetails={false} />,
+      <TravelClaimCard {...props} appointmentDateTime={null} />,
       {
         initialState: {},
       },
@@ -51,17 +42,6 @@ describe('TravelClaimCard', () => {
   });
 
   it('should render link to claim details when toggle is true', () => {
-    const props = {
-      id: '6cecf332-65af-4495-b18e-7fd28ccb546a',
-      claimNumber: '39b7b38f-b7cf-4d19-91cf-fb5360c0b8b8',
-      claimName: '3583ec0e-34e0-4cf5-99d6-78930c2be969',
-      claimStatus: 'Saved',
-      appointmentDateTime: '2023-09-21T17:11:43.034Z',
-      appointmentName: 'Medical imaging',
-      appointmentLocation: 'Tomah VA Medical Center',
-      createdOn: '2023-09-22T17:11:43.034Z',
-      modifiedOn: '2023-09-27T17:11:43.034Z',
-    };
     const screen = renderWithStoreAndRouter(
       <TravelClaimCard {...props} canViewClaimDetails />,
       {
