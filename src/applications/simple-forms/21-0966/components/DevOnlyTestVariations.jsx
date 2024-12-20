34a81ef9fda212c46e863daeef8617427f6d6630
Robert Hasselle <robert.hasselle@oddball.io>
2024-12-20 10:22:34 -0600

    fix dev only condition

--- src/applications/simple-forms/21-0966/components/DevOnlyTestVariations.jsx
+++ src/applications/simple-forms/21-0966/components/DevOnlyTestVariations.jsx
@@ -64,7 +64,7 @@ export const DevOnlyTestVariations = ({
     });
   };
 
-  if (!(environment.isLocalhost() || environment.isDevelopment())) {
+  if (!(environment.isLocalhost() || environment.isDev())) {
     return null;
   }
 
