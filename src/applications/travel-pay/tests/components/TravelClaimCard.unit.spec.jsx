5fb3c8f31174a277ad8dcfc41cf118c9b47cf756
Liz Townsend <liz.townsend@adhocteam.us>
2024-12-20 10:21:50 -0600

    use render with store and router in claim card unit test

--- src/applications/travel-pay/tests/components/TravelClaimCard.unit.spec.jsx
+++ src/applications/travel-pay/tests/components/TravelClaimCard.unit.spec.jsx
@@ -1,39 +1,74 @@
 import React from 'react';
 import { expect } from 'chai';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import { renderWithStoreAndRouter } from '@department-of-veterans-affairs/platform-testing/react-testing-library-helpers';
 
 import TravelClaimCard from '../../components/TravelClaimCard';
 
-it('should still render when an unknown status is given', () => {
-  const props = {
-    id: '6cecf332-65af-4495-b18e-7fd28ccb546a',
-    claimNumber: '39b7b38f-b7cf-4d19-91cf-fb5360c0b8b8',
-    claimName: '3583ec0e-34e0-4cf5-99d6-78930c2be969',
-    claimStatus: 'Something',
-    appointmentDateTime: '2023-09-21T17:11:43.034Z',
-    appointmentName: 'Medical imaging',
-    appointmentLocation: 'Tomah VA Medical Center',
-    createdOn: '2023-09-22T17:11:43.034Z',
-    modifiedOn: '2023-09-27T17:11:43.034Z',
-  };
-  const screen = render(<TravelClaimCard {...props} />);
+describe('TravelClaimCard', () => {
+  it('should still render when an unknown status is given', () => {
+    const props = {
+      id: '6cecf332-65af-4495-b18e-7fd28ccb546a',
+      claimNumber: '39b7b38f-b7cf-4d19-91cf-fb5360c0b8b8',
+      claimName: '3583ec0e-34e0-4cf5-99d6-78930c2be969',
+      claimStatus: 'Something',
+      appointmentDateTime: '2023-09-21T17:11:43.034Z',
+      appointmentName: 'Medical imaging',
+      appointmentLocation: 'Tomah VA Medical Center',
+      createdOn: '2023-09-22T17:11:43.034Z',
+      modifiedOn: '2023-09-27T17:11:43.034Z',
+    };
+    const screen = renderWithStoreAndRouter(
+      <TravelClaimCard {...props} canViewClaimDetails={false} />,
+      {
+        initialState: {},
+      },
+    );
 
-  expect(screen.getByText('Claim status: Something')).to.exist;
-});
+    expect(screen.getByText('Claim status: Something')).to.exist;
+  });
 
-it('should render proper text when a null appointment date is given', () => {
-  const props = {
-    id: '6cecf332-65af-4495-b18e-7fd28ccb546a',
-    claimNumber: '39b7b38f-b7cf-4d19-91cf-fb5360c0b8b8',
-    claimName: '3583ec0e-34e0-4cf5-99d6-78930c2be969',
-    claimStatus: 'Saved',
-    appointmentDateTime: null,
-    appointmentName: 'Medical imaging',
-    appointmentLocation: 'Tomah VA Medical Center',
-    createdOn: '2023-09-22T17:11:43.034Z',
-    modifiedOn: '2023-09-27T17:11:43.034Z',
-  };
-  const screen = render(<TravelClaimCard {...props} />);
+  it('should render proper text when a null appointment date is given', () => {
+    const props = {
+      id: '6cecf332-65af-4495-b18e-7fd28ccb546a',
+      claimNumber: '39b7b38f-b7cf-4d19-91cf-fb5360c0b8b8',
+      claimName: '3583ec0e-34e0-4cf5-99d6-78930c2be969',
+      claimStatus: 'Saved',
+      appointmentDateTime: null,
+      appointmentName: 'Medical imaging',
+      appointmentLocation: 'Tomah VA Medical Center',
+      createdOn: '2023-09-22T17:11:43.034Z',
+      modifiedOn: '2023-09-27T17:11:43.034Z',
+    };
+    const screen = renderWithStoreAndRouter(
+      <TravelClaimCard {...props} canViewClaimDetails={false} />,
+      {
+        initialState: {},
+      },
+    );
 
-  expect(screen.getByText('Appointment information not available')).to.exist;
+    expect(screen.getByText('Appointment information not available')).to.exist;
+  });
+
+  it('should render link to claim details when toggle is true', () => {
+    const props = {
+      id: '6cecf332-65af-4495-b18e-7fd28ccb546a',
+      claimNumber: '39b7b38f-b7cf-4d19-91cf-fb5360c0b8b8',
+      claimName: '3583ec0e-34e0-4cf5-99d6-78930c2be969',
+      claimStatus: 'Saved',
+      appointmentDateTime: '2023-09-21T17:11:43.034Z',
+      appointmentName: 'Medical imaging',
+      appointmentLocation: 'Tomah VA Medical Center',
+      createdOn: '2023-09-22T17:11:43.034Z',
+      modifiedOn: '2023-09-27T17:11:43.034Z',
+    };
+    const screen = renderWithStoreAndRouter(
+      <TravelClaimCard {...props} canViewClaimDetails />,
+      {
+        initialState: {},
+      },
+    );
+
+    expect(screen.getByText('Travel reimbursement claim details')).to.exist;
+  });
 });
