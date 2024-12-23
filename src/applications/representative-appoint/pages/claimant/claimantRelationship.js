e062c24195f05de7a40e9e8325ada40bc4b24286
rjohnson2011 <ryan.johnson@oddball.io>
2024-12-23 10:54:42 -0700

    Fix spec

--- src/applications/representative-appoint/pages/claimant/claimantRelationship.js
+++ src/applications/representative-appoint/pages/claimant/claimantRelationship.js
@@ -32,6 +32,7 @@ export const uiSchema = {
       expandUnder: 'claimantRelationship',
       expandUnderCondition: 'RELATIONSHIP_NOT_LISTED',
       expandedContentFocus: true,
+      maxLength: 42,
     },
     'ui:errorMessages': {
       required: `Please enter your relationship to the Veteran`,
