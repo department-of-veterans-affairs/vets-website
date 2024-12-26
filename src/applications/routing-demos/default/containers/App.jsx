93a6829f435c5c7ae73ce7adfefed699410596ea
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2024-12-26 15:32:57 -0700

    Implement default routing app with feature toggle and user authentication

--- src/applications/routing-demos/default/containers/App.jsx
+++ src/applications/routing-demos/default/containers/App.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
-export default function App({ children }) {
-  return <div>{children}</div>;
+import { RequiredLoginView } from '@department-of-veterans-affairs/platform-user/RequiredLoginView';
+import backendServices from '@department-of-veterans-affairs/platform-user/profile/backendServices';
+import { selectUser } from '@department-of-veterans-affairs/platform-user/selectors';
+
+import {
+  DowntimeNotification,
+  externalServices,
+} from '@department-of-veterans-affairs/platform-monitoring/DowntimeNotification';
+
+import { useFeatureToggle } from '~/platform/utilities/feature-toggles';
+
+export default function App() {
+  const user = useSelector(selectUser);
+
+  const { useToggleValue, TOGGLE_NAMES } = useFeatureToggle();
+  const useNewVersion = useToggleValue(
+    TOGGLE_NAMES.defaultRoutingUseNewVersion,
+  );
+
+  return (
+    <>
+      <h1>Default Routing App</h1>
+      <RequiredLoginView
+        serviceRequired={[backendServices.USER_PROFILE]}
+        user={user}
+        showProfileErrorMessage
+      >
+        <DowntimeNotification
+          appTitle="Default Routing App"
+          dependencies={[externalServices.mhvPlatform]}
+        >
+          {useNewVersion ? (
+            <div>If you can see this, the app is using the new version.</div>
+          ) : (
+            <div>If you can see this, the app is using the old version.</div>
+          )}
+        </DowntimeNotification>
+      </RequiredLoginView>
+    </>
+  );
 }
-
-App.propTypes = {
-  children: PropTypes.node,
-};
