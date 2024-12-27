7ff04a244ad61b18463a00386222cc7114df2840
Juan Castillo <juan.cast@agile6.com>
2024-12-26 18:12:27 -0800

    Referral Expired error

--- src/applications/vaos/referral-appointments/components/ReferralTaskCardWithReferral.jsx
+++ src/applications/vaos/referral-appointments/components/ReferralTaskCardWithReferral.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { isAfter } from 'date-fns';
 
 import { useGetReferralById } from '../hooks/useGetReferralById';
 import ReferralTaskCard from './ReferralTaskCard';
 import { FETCH_STATUS } from '../../utils/constants';
+
+const isExpired = referral => {
+  const expirationDate = referral.ReferralExpirationDate;
+  const now = new Date();
+  const expiration = new Date(expirationDate);
+  return isAfter(now, expiration);
+};
 
 export default function ReferralTaskCardWithReferral() {
   const { search } = useLocation();
@@ -30,6 +38,29 @@ export default function ReferralTaskCardWithReferral() {
           again later.
         </p>
       </va-alert>
+    );
+  }
+
+  if (isExpired(currentReferral)) {
+    return (
+      <va-alert-expandable
+        status="warning"
+        trigger="Your community care referral has expired"
+        disable-analytics="false"
+        class="vads-u-margin-y--2"
+        uswds
+        data-testid="expired-alert"
+      >
+        <p>
+          Call your facility to request a new referral.
+          <br />
+          <va-link
+            href="/find-locations/?facilityType=health"
+            target="_blank"
+            text="Find your VA health facility"
+          />
+        </p>
+      </va-alert-expandable>
     );
   }
 
