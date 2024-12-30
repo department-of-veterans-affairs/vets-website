0f7f93e6ecd7790f3328b67b9501ea60ece2a899
Liz Wendling <170662489+GovCIOLiz@users.noreply.github.com>
2024-12-30 08:28:38 -0800

    Form 10215 Updates (#33771)
    * Spelling mistake
    
    * 10215 calcs page
    
    * Review page submit btn custom text
    
    * added more unit test to bring coverage up (above 80%)
    
    ---------
    
    Co-authored-by: wafimohamed <158514667+wafimohamed@users.noreply.github.com>

--- src/applications/edu-benefits/10215/tests/pages/calcs.unit.spec.js
+++ src/applications/edu-benefits/10215/tests/pages/calcs.unit.spec.js
@@ -1,14 +1,19 @@
-// src/applications/edu-benefits/10215/pages/calcs.test.js
 import React from 'react';
-import { mount } from 'enzyme';
 import { expect } from 'chai';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import { render, waitFor } from '@testing-library/react';
+import sinon from 'sinon';
+import * as webComponents from 'platform/utilities/ui/webComponents';
 import Calcs from '../../pages/calcs';
+import * as helpers from '../../helpers';
 
 const mockStore = configureStore();
 
 describe('<Calcs />', () => {
+  let querySelectorStub;
+  let getFTECalcsStub;
+
   const mockData = {
     programs: [
       {
@@ -17,65 +22,144 @@ describe('<Calcs />', () => {
         total: 10,
         supportedFTEPercent: 100,
       },
+      {
+        supported: true,
+        nonSupported: true,
+        total: 20,
+        supportedFTEPercent: 50,
+      },
     ],
   };
 
+  beforeEach(() => {
+    querySelectorStub = sinon.stub(
+      webComponents,
+      'querySelectorWithShadowRoot',
+    );
+    getFTECalcsStub = sinon.stub(helpers, 'getFTECalcs').returns({
+      total: 8,
+      supportedFTEPercent: '62.5%',
+    });
+  });
+
+  afterEach(() => {
+    querySelectorStub.restore();
+    getFTECalcsStub.restore();
+  });
+
   it('should render correctly with given props', () => {
     const store = mockStore({ form: { data: mockData } });
-    const wrapper = mount(
+    const { getByTestId } = render(
       <Provider store={store}>
         <Calcs data={mockData} />
       </Provider>,
     );
 
-    expect(
-      wrapper
-        .find('label')
-        .at(0)
-        .text(),
-    ).to.equal('Total Enrolled FTE');
-    expect(
-      wrapper
-        .find('span')
-        .at(0)
-        .text(),
-    ).to.equal('--');
-    expect(
-      wrapper
-        .find('label')
-        .at(1)
-        .text(),
-    ).to.equal('Supported student percentage FTE');
-    expect(
-      wrapper
-        .find('span')
-        .at(1)
-        .text(),
-    ).to.equal('--%');
-    wrapper.unmount();
+    expect(getByTestId('num-fte').textContent).to.equal('Total Enrolled FTE');
+    expect(getByTestId('nonSupported').textContent).to.equal('--');
+    expect(getByTestId('percentage-FTE').textContent).to.equal(
+      'Supported student percentage FTE',
+    );
+    expect(getByTestId('supportedFTEPercent').textContent).to.equal('62.5%');
   });
 
   it('should render "--" when no data is available', () => {
     const emptyData = { programs: [] };
     const store = mockStore({ form: { data: emptyData } });
-    const wrapper = mount(
+    const { getByTestId } = render(
       <Provider store={store}>
         <Calcs data={emptyData} />
       </Provider>,
     );
 
-    expect(
-      wrapper
-        .find('span')
-        .at(0)
-        .text(),
-    ).to.equal('--');
-    expect(
-      wrapper
-        .find('span')
-        .at(1)
-        .text(),
-    ).to.equal('--%');
-    wrapper.unmount();
+    expect(getByTestId('nonSupported').textContent).to.equal('--');
+    expect(getByTestId('supportedFTEPercent').textContent).to.equal('62.5%');
+  });
+
+  it('should update programData when updateData is called', async () => {
+    const store = mockStore({ form: { data: mockData } });
+    const mockSupportedInput = { shadowRoot: { querySelector: sinon.stub() } };
+    const mockNonSupportedInput = {
+      shadowRoot: { querySelector: sinon.stub() },
+    };
+
+    mockSupportedInput.shadowRoot.querySelector
+      .withArgs('input')
+      .returns({ value: '5' });
+    mockNonSupportedInput.shadowRoot.querySelector
+      .withArgs('input')
+      .returns({ value: '3' });
+
+    querySelectorStub
+      .withArgs('va-text-input[name="root_fte_supported"]', document)
+      .resolves(mockSupportedInput);
+    querySelectorStub
+      .withArgs('va-text-input[name="root_fte_nonSupported"]', document)
+      .resolves(mockNonSupportedInput);
+
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <Calcs data={mockData} />
+      </Provider>,
+    );
+
+    await webComponents.querySelectorWithShadowRoot();
+
+    expect(getByTestId('nonSupported').textContent).to.equal('--');
+    expect(getByTestId('supportedFTEPercent').textContent).to.equal('62.5%');
+  });
+  it('should set programData if none exists and data is provided', async () => {
+    const data = {
+      programs: [
+        {
+          supported: 'initialValue1',
+          nonSupported: 'initialValue2',
+        },
+        {
+          supported: '3',
+          nonSupported: '5',
+        },
+      ],
+    };
+    const store2 = mockStore({ form: { data } });
+    const mockSupportedInput = {
+      shadowRoot: {
+        querySelector: sinon.stub().returns({ value: '5' }),
+      },
+    };
+
+    const mockNonSupportedInput = {
+      shadowRoot: {
+        querySelector: sinon.stub().returns({ value: '3' }),
+      },
+    };
+
+    querySelectorStub
+      .withArgs('va-text-input[name="root_fte_supported"]', document)
+      .resolves(mockSupportedInput);
+    querySelectorStub
+      .withArgs('va-text-input[name="root_fte_nonSupported"]', document)
+      .resolves(mockNonSupportedInput);
+
+    const originalLocation = window.location;
+    delete window.location;
+    Object.defineProperty(window, 'location', {
+      value: { ...originalLocation, href: 'https://fake.url/somePage1' },
+      writable: true,
+    });
+    render(
+      <Provider store={store2}>
+        <Calcs data={mockData} />
+      </Provider>,
+    );
+
+    await waitFor(() => {
+      const lastCallArg = getFTECalcsStub.lastCall.args[0];
+      expect(lastCallArg).to.deep.equal({ supported: '3', nonSupported: '5' });
+    });
+    Object.defineProperty(window, 'location', {
+      value: originalLocation,
+      writable: false,
+    });
   });
 });
