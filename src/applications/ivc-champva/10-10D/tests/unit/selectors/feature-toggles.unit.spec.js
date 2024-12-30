9621320281f5b0841bdd82084546e52657b8b379
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2024-12-30 07:24:31 -0600

    updated naming

--- src/applications/ivc-champva/10-10D/tests/unit/selectors/feature-toggles.unit.spec.js
+++ src/applications/ivc-champva/10-10D/tests/unit/selectors/feature-toggles.unit.spec.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { makeSelectFeatureToggles } from '../../../selectors/feature-toggles';
 
-describe('ezr FeatureToggles selector', () => {
+describe('10-10d RUM FeatureToggles selector', () => {
   const state = {
     featureToggles: {
       /* eslint-disable camelcase */
