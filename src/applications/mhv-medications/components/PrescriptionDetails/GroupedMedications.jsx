eb7908b5bc54d61bda6a1fbe8112050d39976dfc
robertbylight <107279507+robertbylight@users.noreply.github.com>
2024-12-31 15:13:35 -0500

    [mhv-64987] removed truncatedGroupedMedications from dependency list to avoid infinite loop.

--- src/applications/mhv-medications/components/PrescriptionDetails/GroupedMedications.jsx
+++ src/applications/mhv-medications/components/PrescriptionDetails/GroupedMedications.jsx
@@ -31,7 +31,7 @@ const GroupedMedications = props => {
       );
       setCurrentGroupedList(currentItems);
     },
-    [currentPage, groupedMedicationsList, truncatedGroupedMedicationsList],
+    [currentPage, groupedMedicationsList],
   );
 
   const onPageChange = page => {
