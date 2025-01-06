bd9671d61bc831c829d7e357624a6c3fb52015a8
Nick Sayre <nick.sayre@adhocteam.us>
2025-01-06 12:30:58 -0600

    1348 - add comments, build out test

--- src/applications/personalization/review-information/tests/welcome-va-setup-review-information-2.cypress.spec.js
+++ src/applications/personalization/review-information/tests/welcome-va-setup-review-information-2.cypress.spec.js
@@ -22,30 +22,43 @@ describe('Welcome to My VA Review Contact Information form', () => {
   };
   const startApplication = () => {
     cy.wait('@mockSip');
-    cy.location('pathname').should(
-      'include',
-      '/my-va/welcome-va-setup/review-information/contact-information',
-    );
+    // Ensure we've navigated to the contact information form
+    cy.location('pathname').should('match', /\/contact-information$/);
   };
 
-  context('when editing the mobile phone', () => {
+  context('when navigating the form', () => {
     beforeEach(() => {
       setupAuthUser();
       startApplication();
       cy.intercept('POST', '/v0/profile/initialize_vet360_id', mockNickData);
     });
 
-    it('should succeed', () => {
+    it('should be completable', () => {
+      cy.injectAxe();
+
+      // Ensure that the contact information page is accessible
+      cy.axeCheck();
+
+      // Click the edit button
       cy.get('#edit-mobile-phone').click();
-      // cy.get('#edit-email').click();
-      // cy.get('#edit-address').click();
+
+      // Ensure we've navigated to the right page
       cy.location('pathname').should('include', '/edit-mobile-phone');
 
-      // cy.get('[name="root_inputPhoneNumber"]').type(testData.mobilePhone);
-      cy.get('[data-testid="save-edit-button"]').click();
-
-      cy.injectAxe();
+      // Ensure that the edit mobile phone page is accessible
       cy.axeCheck();
+
+      // Type a new value
+      // cy.get('[name="root_inputPhoneNumber"]').type(testData.mobilePhone);
+
+      // Save the  mobile phone page
+      // cy.get('[data-testid="save-edit-button"]').click();
+
+      // cy.get('#edit-email').click();
+      // cy.get('#edit-address').click();
+
+      // Click the save button
+      // cy.get('#2-continueButton').click();
     });
   });
 });
