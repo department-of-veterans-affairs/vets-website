d7d22a5d26c596ef0ae98fa562cc38d51ad67d0a
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-01-06 08:44:33 -0600

    98828 Add initial RUM implementation - form 10-10d (#33772)
    * adds RUM monitoring for form 10-10d
    
    * updated naming

--- src/applications/ivc-champva/10-10D/containers/App.jsx
+++ src/applications/ivc-champva/10-10D/containers/App.jsx
@@ -3,12 +3,13 @@ import {
   DowntimeNotification,
   externalServices,
 } from '@department-of-veterans-affairs/platform-monitoring/DowntimeNotification';
+import { VaBreadcrumbs } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import PropTypes from 'prop-types';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-import { VaBreadcrumbs } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { Toggler } from 'platform/utilities/feature-toggles';
 import formConfig from '../config/form';
 import WIP from '../../shared/components/WIP';
+import { useBrowserMonitoring } from '../helpers/useBrowserMonitoring';
 import { addStyleToShadowDomOnPages } from '../../shared/utilities';
 
 const breadcrumbList = [
@@ -41,6 +42,9 @@ export default function App({ location, children }) {
       '#dateHint {display: none}',
     );
   });
+
+  // Add Datadog RUM to the app
+  useBrowserMonitoring();
 
   return (
     <div className="vads-l-grid-container desktop-lg:vads-u-padding-x--0">
