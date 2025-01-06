ce8e8fcb9e04b88231e018571e25238699021e22
Alison Jones <1816769+ajones446@users.noreply.github.com>
2025-01-06 14:14:33 -0600

    DBEX 0781 consent page (#33804)
    * Consent page setup
    
    * Add basic content for consent page
    
    * Add additionalInformationPage as placeholder file
    
    * Unit tests for consentPage
    
    * Cleanup, move indicator choices to a constant
    
    * Add content to consent page
    
    * Add more specs
    
    * Add depends function for consent page
    
    * PR suggestion: use curly apostrophe
    
    Co-authored-by: Scott Regenthal <scottregenthal@gmail.com>
    
    * PR suggestions
    
    * Add optional chaining to function
    
    ---------
    
    Co-authored-by: Scott Regenthal <scottregenthal@gmail.com>

---
+++ src/applications/disability-benefits/all-claims/pages/form0781/additionalInformationPage.js
@@ -0,0 +1,9 @@
+// TODO: this is a placeholder. Structure will be added to this page in this ticket #97085
+export const uiSchema = {
+  'ui:description': 'Placeholder text for additional information page skeleton',
+};
+
+export const schema = {
+  type: 'object',
+  properties: {},
+};
