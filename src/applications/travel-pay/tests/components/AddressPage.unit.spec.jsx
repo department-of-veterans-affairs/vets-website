266c4df1ef43449ca61020291f9b930b82f23dff
Liz Townsend <liz.townsend@adhocteam.us>
2025-01-06 10:28:39 -0600

    add address and cant file pages unit tests

---
+++ src/applications/travel-pay/tests/components/AddressPage.unit.spec.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { expect } from 'chai';
+import { render } from '@testing-library/react';
+import { $ } from 'platform/forms-system/src/js/utilities/ui';
+
+import AddressPage from '../../components/submit-flow/pages/AddressPage';
+
+it('should render with back and continue buttons', () => {
+  const props = {
+    handlers: {
+      onBack: () => {},
+      onNext: () => {},
+    },
+  };
+  const screen = render(<AddressPage {...props} />);
+
+  expect(screen.getByText('Address page')).to.exist;
+  expect($('va-button-pair')).to.exist;
+});
