e0b95c8cb5e9d8a2ffe973e47ede01c2c2b38f1f
Kevin Suarez <kjsuarez94@gmail.com>
2025-01-09 16:38:16 -0500

    100524 If AC-API error is missing a code, don't display that empty code to the user

--- src/applications/mhv-landing-page/components/alerts/AlertAccountApiAlert.jsx
+++ src/applications/mhv-landing-page/components/alerts/AlertAccountApiAlert.jsx
@@ -66,13 +66,20 @@ const AlertAccountApiAlert = ({
             page. Or check back later.
           </p>
 
-          <p>
-            If the problem persists, call the My HealtheVet helpdesk at
-            877-327-0022 (TTY: 711). We’re here Monday through Friday, 8:00 a.m.
-            to 8 p.m. ET. Tell the representative that you received{' '}
-            <b>error code {errorCode}</b>.
-          </p>
-
+          {errorCode.length > 0 ? (
+            <p>
+              If the problem persists, call the My HealtheVet helpdesk at
+              877-327-0022 (TTY: 711). We’re here Monday through Friday, 8:00
+              a.m. to 8 p.m. ET. Tell the representative that you received{' '}
+              <b>error code {errorCode}</b>.
+            </p>
+          ) : (
+            <p>
+              If the problem persists, call the My HealtheVet helpdesk at
+              877-327-0022 (TTY: 711). We’re here Monday through Friday, 8:00
+              a.m. to 8 p.m. ET.
+            </p>
+          )}
           <p>
             If you need to contact your care team now, call your VA health
             facility.
@@ -89,7 +96,7 @@ const AlertAccountApiAlert = ({
 
 AlertAccountApiAlert.defaultProps = {
   title: 'Error code 000: Contact the My HealtheVet help desk',
-  errorCode: 'unknown',
+  errorCode: '',
   recordEvent: recordEventFn,
   testId: 'mhv-alert--mhv-registration',
 };
