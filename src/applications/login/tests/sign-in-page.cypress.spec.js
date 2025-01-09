974798d0523cd6b84aae1c75ed14fc3063dd7a4d
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2025-01-09 12:56:22 -0500

    last e2e test fix

--- src/applications/login/tests/sign-in-page.cypress.spec.js
+++ src/applications/login/tests/sign-in-page.cypress.spec.js
@@ -27,33 +27,4 @@ describe('Unified Sign-in Page', () => {
       });
     },
   );
-
-  context(
-    `when 'mhv_credential_button_disabled: false' feature toggle is enabled`,
-    () => {
-      beforeEach(() => {
-        cy.intercept('GET', '/v0/feature_toggles*', {
-          data: {
-            type: 'feature_toggles',
-            features: [
-              {
-                name: 'mhvCredentialButtonDisabled',
-                value: false,
-              },
-            ],
-          },
-        }).as('featureToggles');
-        cy.intercept('GET', '/data/cms/vamc-ehr.json', { statusCode: 200 });
-      });
-
-      it(`display correct sign-in content`, () => {
-        cy.visit('/sign-in/?oauth=false');
-        cy.wait('@featureToggles');
-        cy.get('body').should('be.visible');
-        cy.get('#mhvH3').should('exist');
-        cy.get('#dslogonH3').should('be.visible');
-        cy.injectAxeThenAxeCheck();
-      });
-    },
-  );
 });
