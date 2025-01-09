43856f7473f48393b1d87024ae74e018eec46f7d
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2025-01-08 22:07:22 -0800

    Removes unused stuff

--- src/applications/vaos/new-appointment/components/ProviderSelectPage/index.jsx
+++ src/applications/vaos/new-appointment/components/ProviderSelectPage/index.jsx
@@ -1,11 +1,6 @@
 import React, { useEffect } from 'react';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import FormButtons from '../../../components/FormButtons';
-import { getFormPageInfo } from '../../redux/selectors';
+import { useSelector } from 'react-redux';
 import { scrollAndFocus } from '../../../utils/scrollAndFocus';
-import { routeToPreviousAppointmentPage } from '../../redux/actions';
 import { getPageTitle } from '../../newAppointmentFlow';
 import ProviderCard from './ProviderCard';
 import ScheduleWithDifferentProvider from './ScheduleWithDifferentProvider';
@@ -25,15 +20,8 @@ const providers = [
 
 export default function SelectProviderPage() {
   const pageTitle = useSelector(state => getPageTitle(state, pageKey));
-  const dispatch = useDispatch();
-  const { pageChangeInProgress } = useSelector(
-    state => getFormPageInfo(state, pageKey),
-    shallowEqual,
-  );
-  const history = useHistory();
 
   useEffect(() => {
-    // dispatch(openFormPage(pageKey, initialSchema));
     document.title = `${pageTitle} | Veterans Affairs`;
     scrollAndFocus();
   }, []);
@@ -41,13 +29,6 @@ export default function SelectProviderPage() {
   return (
     <div>
       <h1 className="vads-u-font-size--h2">{pageTitle}</h1>
-      <FormButtons
-        onBack={() =>
-          dispatch(routeToPreviousAppointmentPage(history, pageKey))
-        }
-        pageChangeInProgress={pageChangeInProgress}
-        loadingText="Page change in progress"
-      />
       {providers.map((provider, index) => (
         <ProviderCard key={index} provider={provider} />
       ))}
@@ -56,7 +37,3 @@ export default function SelectProviderPage() {
     </div>
   );
 }
-
-SelectProviderPage.propTypes = {
-  changeCrumb: PropTypes.func,
-};
