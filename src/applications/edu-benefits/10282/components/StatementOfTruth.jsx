073f01ec73412d579a40ec63401aa7894733452f
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2025-01-09 14:28:23 -0500

    vebt-995 added full name to error message

--- src/applications/edu-benefits/10282/components/StatementOfTruth.jsx
+++ src/applications/edu-benefits/10282/components/StatementOfTruth.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { VaStatementOfTruth } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-
+import React from 'react';
+import FormSignature from 'platform/forms-system/src/js/components/FormSignature';
 /**
  * Checks that the provided signature string matches the existing
  * applicantName in the formData.
@@ -10,74 +8,62 @@ import { VaStatementOfTruth } from '@department-of-veterans-affairs/component-li
  * @param {object} formData standard formData object
  * @returns Either a string representing an error, or undefined (representing a match)
  */
-// export function signatureValidator(signatureName, formData) {
-//   const firstName = formData?.veteranFullName.first || '';
-//   const lastName = formData?.veteranFullName.last || '';
-//   const fullName = `${firstName} ${lastName}`;
-//   const name = fullName
-//     .split(' ')
-//     .filter(el => el)
-//     .join('')
-//     .toLowerCase();
+export function signatureValidator(signatureName, formData) {
+  const firstName = formData?.veteranFullName.first || '';
+  const middle = formData?.veteranFullName.middle || '';
+  const lastName = formData?.veteranFullName.last || '';
+  const fullName = `${firstName} ${middle} ${lastName}`;
+  const name =
+    Object.values(fullName)
+      .filter(el => el)
+      .join('')
+      .toLowerCase() || '';
 
-//   const processedSignatureName = signatureName
-//     .replaceAll(' ', '')
-//     .toLowerCase();
-//   const processedName = name.replaceAll(' ', '');
-//   if (processedSignatureName !== processedName) {
-//     return `Please enter your full name exactly as entered on the form: ${fullName}`;
-//   }
-//   return undefined;
-// }
+  const processedSignatureName = signatureName
+    .replaceAll(' ', '')
+    .toLowerCase();
+  const processedName = name.replaceAll(' ', '');
+  if (processedSignatureName !== processedName) {
+    return `Please enter your full name exactly as entered on the form: ${name}`;
+  }
+  return undefined;
+}
 
-export default function StatementOfTruth() {
-  const formData = useSelector(state => state.form.data);
-  const [signatureName, setSignatureName] = useState('');
-  const [inputError, setInputError] = useState('');
-  const [checked, setChecked] = useState(false);
+export default function StatementOfTruth(signatureProps) {
+  const pp = (
+    <span>
+      I have read and accept the{' '}
+      <va-link external href="/privacy-policy/" text="privacy policy" />
+    </span>
+  );
 
-  const handleChange = e => {
-    setSignatureName(e.detail.value);
-  };
+  const isCorrect = (
+    <p>
+      I confirm that the identifying information in this form is accurate and
+      has been represented correctly.
+    </p>
+  );
 
-  const handleBlur = () => {
-    const firstName = formData?.veteranFullName.first || '';
-    const lastName = formData?.veteranFullName.last || '';
-    const fullName = `${firstName} ${lastName}`;
-    if (signatureName !== fullName) {
-      setInputError(
-        `Please enter your full name exactly as entered on the form: ${fullName}`,
-      );
-    } else {
-      setInputError('');
-    }
-  };
+  const content = (
+    <>
+      {isCorrect}
+      {pp}
+    </>
+  );
 
-  const handleCheckboxChange = e => {
-    setChecked(e.detail.checked);
-  };
+  const validators = [signatureValidator];
 
   return (
-    <div style={{ maxWidth: 600 }}>
-      <VaStatementOfTruth
-        checkboxError={
-          checked
-            ? ''
-            : 'Please check the box to certify the information is correct'
-        }
-        checkboxLabel="I certify the information above is correct and true to the best of my knowledge and belief."
-        heading="Statement of truth"
-        inputError={inputError}
-        inputLabel="Your full name"
-        inputMessageAriaDescribedby=""
-        inputValue={signatureName}
-        onVaCheckboxChange={handleCheckboxChange}
-        onVaInputBlur={handleBlur}
-        onVaInputChange={handleChange}
-      >
-        I confirm that the identifying information in this form is accurate and
-        has been represented correctly.
-      </VaStatementOfTruth>
+    <div className="vads-u-margin-top--3">
+      <section className="box vads-u-background-color--gray-lightest vads-u-padding--3">
+        <h3 className="vads-u-margin-top--0">Statement of truth</h3>
+        {content}
+        <FormSignature
+          {...signatureProps}
+          signatureLabel="Your full name"
+          validations={validators}
+        />
+      </section>
     </div>
   );
 }
