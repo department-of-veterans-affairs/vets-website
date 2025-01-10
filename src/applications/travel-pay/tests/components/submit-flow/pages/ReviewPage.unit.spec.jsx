dbd10166af293171fba184a29bd66854342e8179
Liz Townsend <liz.townsend@adhocteam.us>
2025-01-10 14:13:57 -0600

    flesh out review page unit test

--- src/applications/travel-pay/tests/components/submit-flow/pages/ReviewPage.unit.spec.jsx
+++ src/applications/travel-pay/tests/components/submit-flow/pages/ReviewPage.unit.spec.jsx
@@ -5,16 +5,49 @@ import { $ } from 'platform/forms-system/src/js/utilities/ui';
 
 import ReviewPage from '../../../../components/submit-flow/pages/ReviewPage';
 
-it('should render with back and submit buttons', () => {
-  const props = {
-    handlers: {
-      onBack: () => {},
-      onSubmit: () => {},
-    },
-  };
-  const screen = render(<ReviewPage {...props} />);
+const appointment = require('../../../fixtures/appointment.json');
 
-  expect(screen.getByText('Review your travel claim')).to.exist;
-  expect($('va-button[text="Back"')).to.exist;
-  expect($('va-button[text="Submit"')).to.exist;
+const homeAddress = {
+  addressLine1: '345 Home Address St.',
+  addressLine2: null,
+  addressLine3: null,
+  addressPou: 'RESIDENCE/CHOICE',
+  addressType: 'DOMESTIC',
+  city: 'San Francisco',
+  countryName: 'United States',
+  countryCodeIso2: 'US',
+  countryCodeIso3: 'USA',
+  countryCodeFips: null,
+  countyCode: null,
+  countyName: null,
+  internationalPostalCode: null,
+  sourceSystemUser: null,
+  stateCode: 'CA',
+  zipCode: '94118',
+};
+
+describe('Revew page', () => {
+  const props = {
+    address: homeAddress,
+    appointment,
+    onSubmit: () => {},
+    isAgreementChecked: false,
+    setIsAgreementChecked: () => {},
+    setPageIndex: () => {},
+    setYesNo: () => {},
+  };
+  it('should render properly with all data', () => {
+    const screen = render(<ReviewPage {...props} />);
+
+    expect(screen.getByText('Review your travel claim')).to.exist;
+    expect(screen.findByText(/What you’re claiming/i)).to.exist;
+    expect(screen.findByText(/How you traveled/)).to.exist;
+    expect(screen.findByText(/Where you traveled from/)).to.exist;
+    expect(screen.findByText(/345 Home Address St./i)).to.exist;
+    expect($('va-button[text="View beneficiary travel agreement"]')).to.exist;
+    expect(
+      screen.findByText(/You must accept the beneficiary travel agreement/i),
+    ).to.exist;
+    expect($('va-button-pair')).to.exist;
+  });
 });
