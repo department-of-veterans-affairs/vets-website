8f1c3a75f1852209e2e3c5d86e6d38068986262b
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2025-01-13 16:18:01 -0500

    Update unit test

--- src/applications/static-pages/health-care-manage-benefits/schedule-view-va-appointments-page/components/App/index.unit.spec.js
+++ src/applications/static-pages/health-care-manage-benefits/schedule-view-va-appointments-page/components/App/index.unit.spec.js
@@ -8,11 +8,26 @@ import UnauthContent from '../UnauthContent';
 import { App } from './index';
 
 describe('Get Medical Records Page <App>', () => {
-  it('renders what we expect when not a Cerner patient', () => {
+  it('renders what we expect when not authenticated and not LOA3', () => {
     const wrapper = shallow(
       <App
         showNewScheduleViewAppointmentsPage
-        facilities={[{ usesCernerAppointments: false }]}
+        authenticatedWithSSOe={false}
+        hasLOA3={false}
+      />,
+    );
+    expect(wrapper.find(UnauthContent)).to.have.lengthOf(1);
+    expect(wrapper.find(AuthContent)).to.have.lengthOf(0);
+    wrapper.unmount();
+  });
+
+  it('renders what we expect when authenticated but not LOA3', () => {
+    // Must be both authed and LOA3 to see the Auth CTA
+    const wrapper = shallow(
+      <App
+        showNewScheduleViewAppointmentsPage
+        authenticatedWithSSOe
+        hasLOA3={false}
       />,
     );
     expect(wrapper.find(UnauthContent)).to.have.lengthOf(1);
@@ -22,10 +37,7 @@ describe('Get Medical Records Page <App>', () => {
 
   it('renders what we expect when a Cerner patient', () => {
     const wrapper = shallow(
-      <App
-        showNewScheduleViewAppointmentsPage
-        facilities={[{ usesCernerAppointments: true }]}
-      />,
+      <App showNewScheduleViewAppointmentsPage authenticatedWithSSOe hasLOA3 />,
     );
     expect(wrapper.find(UnauthContent)).to.have.lengthOf(0);
     expect(wrapper.find(AuthContent)).to.have.lengthOf(1);
