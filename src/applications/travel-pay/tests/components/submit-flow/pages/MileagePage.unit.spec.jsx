34c5b28ce1be753fea5ebbf06c61fa583e8fdf25
Liz Townsend <72234279+liztownd@users.noreply.github.com>
2025-01-13 07:49:50 -0600

    Travel Pay / flesh out SMOC introduction page (#33888)
    * add README
    
    * fix cypress test
    
    * bare-bones basics of smoc
    
    * simple it up some more
    
    * fix cypress test for breadcrumb link
    
    * add feature toggle redirect, combine handlers
    
    * destructure props
    
    * add spacing on how to base route page
    
    * wrap explainer page in feature flipper
    
    * remove unneccessary divs and fragments
    
    * update help text content
    
    * add mocked data for appt + user
    
    * add date util fn, add content to intro page
    
    * add appt to submit wrapper
    
    * update cypress test
    
    * tidying after merging main
    
    * add smoc specific cypress test + mock data
    
    * add unit tests + mock appt data; add appt to intro prop types
    
    * update appt start time
    
    * onNext > onStart
    
    * fix linting errors
    
    * update text in unit test to match updated appt date
    
    * update getDaysLeft; add tests
    
    * use calendar days instead, update test
    
    * use isUnsupportedClaimType instead of cantFile
    
    * update cantFile test
    
    * elaborate TODO in Breadcrumbs
    
    * rename cantFilePage
    
    * add date test for exactly 30 days
    
    * move cy.clock into before to fix flaky test

---
+++ src/applications/travel-pay/tests/components/submit-flow/pages/MileagePage.unit.spec.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { expect } from 'chai';
+import { render } from '@testing-library/react';
+import { $ } from 'platform/forms-system/src/js/utilities/ui';
+
+import MileagePage from '../../../../components/submit-flow/pages/MileagePage';
+
+it('should render with back and continue buttons', () => {
+  const props = {
+    handlers: {
+      onBack: () => {},
+      onNext: () => {},
+    },
+  };
+  const screen = render(<MileagePage {...props} />);
+
+  expect(screen.getByText('Mileage page')).to.exist;
+  expect($('va-button-pair')).to.exist;
+});
