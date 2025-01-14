f1d36d9702c833a8dfbd249c7b896f927e4ebe2a
Adam Antonioli <adam.antonioli@va.gov>
2025-01-14 10:04:03 -0500

    MBMS-82183 fix unit tests for pre need integration

--- src/applications/pre-need-integration/tests/config/servicePeriods.unit.spec.jsx
+++ src/applications/pre-need-integration/tests/config/servicePeriods.unit.spec.jsx
@@ -1,20 +1,11 @@
-// import {
-//     arrayBuilderItemFirstPageTitleUI,
-//     textUI,
-//     textSchema,
-//   } from '~/platform/forms-system/src/js/web-component-patterns';
+import React from 'react';
 import { expect } from 'chai';
-import {
-  testNumberOfErrorsOnSubmitForWebComponents,
-  // testNumberOfFieldsByType,
-  testNumberOfWebComponentFields,
-  // testSubmitsWithoutErrors,
-  testRender,
-} from './pageTests.spec';
+import { mount } from 'enzyme';
+import { DefinitionTester } from '@department-of-veterans-affairs/platform-testing/schemaform-utils';
+import sinon from 'sinon';
+
 import formConfig from '../../config/form';
-//   import { federalMedicalCentersPages } from '../../../../config/chapters/03-health-and-employment-information/federalMedicalCentersPages';
 import {
-  servicePeriodsPagesVeteran,
   servicePeriodInformationPage,
   handleGetItemName,
   handleAlertMaxItems,
@@ -37,194 +28,155 @@ import {
   handlePreparerNonVeteranDepends,
 } from '../../config/pages/servicePeriodsPages';
 
-describe('service periods summary page', () => {
-  // const { federalMedicalCentersSummary } = federalMedicalCentersPages;
-  const { servicePeriodsSummaryVeteran } = servicePeriodsPagesVeteran;
-  const pageTitle = 'federal medical centers summary';
-  const expectedNumberOfFields = 1;
-  testNumberOfWebComponentFields(
-    formConfig,
-    servicePeriodsSummaryVeteran.schema,
-    servicePeriodsSummaryVeteran.uiSchema,
-    expectedNumberOfFields,
-    pageTitle,
-  );
-
-  const expectedNumberOfErrors = 1;
-  testNumberOfErrorsOnSubmitForWebComponents(
-    formConfig,
-    servicePeriodsSummaryVeteran.schema,
-    servicePeriodsSummaryVeteran.uiSchema,
-    expectedNumberOfErrors,
-    pageTitle,
-  );
-
-  // testSubmitsWithoutErrors(
-  //   formConfig,
-  //   servicePeriodsSummaryVeteran.schema,
-  //   servicePeriodsSummaryVeteran.uiSchema,
-  //   pageTitle,
-  // );
-
-  // testNumberOfFieldsByType(
-  //   formConfig,
-  //   servicePeriodsSummaryVeteran.schema,
-  //   servicePeriodsSummaryVeteran.uiSchema,
-  //   {
-  //     'va-radio': 1,
-  //   },
-  //   pageTitle,
-  // );
-});
-
 describe('pension add federal medical centers page', () => {
-  const pageTitle = 'federal medical center ';
-  // const expectedNumberOfFields = 1;
-  // testNumberOfWebComponentFields(
-  //   formConfig,
-  //   servicePeriodInformationPage(true, false).schema,
-  //   servicePeriodInformationPage(true, false).uiSchema,
-  //   expectedNumberOfFields,
-  //   pageTitle,
-  // );
+  it('should render', () => {
+    const form = mount(
+      <DefinitionTester
+        schema={servicePeriodInformationPage(true, false).schema}
+        definitions={formConfig.defaultDefinitions}
+        uiSchema={servicePeriodInformationPage(true, false).uiSchema}
+      />,
+    );
 
-  // const expectedNumberOfErrors = 1;
-  // testNumberOfErrorsOnSubmitForWebComponents(
-  //   formConfig,
-  //   servicePeriodInformationPage(true, false).schema,
-  //   servicePeriodInformationPage(true, false).uiSchema,
-  //   expectedNumberOfErrors,
-  //   pageTitle,
-  // );
+    expect(form.find('select').length).to.equal(5);
+    expect(form.find('input').length).to.equal(4);
+    form.unmount();
+  });
 
-  // testSubmitsWithoutErrors(
-  //   formConfig,
-  //   servicePeriodInformationPage(true, false).schema,
-  //   servicePeriodInformationPage(true, false).uiSchema,
-  //   pageTitle,
-  //   { medicalCenter: 'Generic Medical Center' },
-  // );
+  it('should not submit empty form', () => {
+    const onSubmit = sinon.spy();
+    const form = mount(
+      <DefinitionTester
+        schema={servicePeriodInformationPage(true, false).schema}
+        definitions={formConfig.defaultDefinitions}
+        onSubmit={onSubmit}
+        uiSchema={servicePeriodInformationPage(true, false).uiSchema}
+      />,
+    );
 
-  // testNumberOfFieldsByType(
-  //   formConfig,
-  //   federalMedicalCenterPage.schema,
-  //   federalMedicalCenterPage.uiSchema,
-  //   {
-  //     'va-text-input': 1,
-  //   },
-  //   pageTitle,
-  // );
+    form.find('form').simulate('submit');
 
-  testRender(
-    formConfig,
-    servicePeriodInformationPage(true, false).schema,
-    servicePeriodInformationPage(true, false).uiSchema,
-    pageTitle,
-  );
+    expect(form.find('.usa-input-error').length).to.equal(1);
+    expect(onSubmit.called).to.be.false;
+    form.unmount();
+  });
 
-  expect(handleGetItemName({ serviceBranch: 'AC' })).to.equal(
-    'U.S. Army Air Corps',
-  );
+  it('should handle page text', () => {
+    expect(handleGetItemName({ serviceBranch: 'AC' })).to.equal(
+      'U.S. Army Air Corps',
+    );
 
-  expect(handleAlertMaxItems()).to.equal(
-    'You have added the maximum number of allowed service periods for this application. You may edit or delete a service period or choose to continue the application.',
-  );
+    expect(handleAlertMaxItems()).to.equal(
+      'You have added the maximum number of allowed service periods for this application. You may edit or delete a service period or choose to continue the application.',
+    );
 
-  expect(
-    handleCardDescription({ dateRange: { from: '19500315', to: '20000523' } }),
-  ).to.equal('03/15/1950 - 05/23/2000');
+    expect(
+      handleCardDescription({
+        dateRange: { from: '19500315', to: '20000523' },
+      }),
+    ).to.equal('03/15/1950 - 05/23/2000');
 
-  expect(
-    handleCancelAddTitle({
-      getItemName: handleGetItemName,
-      itemData: { serviceBranch: 'AC' },
-    }),
-  ).to.equal('Cancel adding U.S. Army Air Corps service period');
-  expect(
-    handleCancelAddTitle({
-      getItemName: handleGetItemName,
-      itemData: { serviceBranch: null },
-    }),
-  ).to.equal('Cancel adding this service period');
+    expect(
+      handleCancelAddTitle({
+        getItemName: handleGetItemName,
+        itemData: { serviceBranch: 'AC' },
+      }),
+    ).to.equal('Cancel adding U.S. Army Air Corps service period');
 
-  expect(handleCancelAddNo()).to.equal('No, keep this');
+    expect(
+      handleCancelAddTitle({
+        getItemName: handleGetItemName,
+        itemData: { serviceBranch: null },
+      }),
+    ).to.equal('Cancel adding this service period');
 
-  expect(
-    handleDeleteTitle({
-      getItemName: handleGetItemName,
-      itemData: { serviceBranch: 'AC' },
-    }),
-  ).to.equal(
-    'Are you sure you want to remove this U.S. Army Air Corps service period?',
-  );
+    expect(handleCancelAddNo()).to.equal('No, keep this');
 
-  expect(
-    handleDeleteDescription({
-      getItemName: handleGetItemName,
-      itemData: { serviceBranch: 'AC' },
-    }),
-  ).to.equal(
-    'This will remove U.S. Army Air Corps and all the information from the service period records.',
-  );
+    expect(
+      handleDeleteTitle({
+        getItemName: handleGetItemName,
+        itemData: { serviceBranch: 'AC' },
+      }),
+    ).to.equal(
+      'Are you sure you want to remove this U.S. Army Air Corps service period?',
+    );
 
-  expect(handleDeleteNeedAtLeastOneDescription()).to.equal(
-    'If you remove this service period, we’ll take you to a screen where you can add another service period. You’ll need to list at least one service period for us to process this form.',
-  );
+    expect(
+      handleDeleteDescription({
+        getItemName: handleGetItemName,
+        itemData: { serviceBranch: 'AC' },
+      }),
+    ).to.equal(
+      'This will remove U.S. Army Air Corps and all the information from the service period records.',
+    );
 
-  expect(handleDeleteYes()).to.equal('Yes, remove this');
+    expect(handleDeleteNeedAtLeastOneDescription()).to.equal(
+      'If you remove this service period, we’ll take you to a screen where you can add another service period. You’ll need to list at least one service period for us to process this form.',
+    );
 
-  expect(handleDeleteNo()).to.equal('No, keep this');
+    expect(handleDeleteYes()).to.equal('Yes, remove this');
 
-  expect(
-    handleCancelEditTitle({
-      getItemName: handleGetItemName,
-      itemData: { serviceBranch: 'AC' },
-    }),
-  ).to.equal('Cancel editing U.S. Army Air Corps service period?');
+    expect(handleDeleteNo()).to.equal('No, keep this');
 
-  expect(handleCancelEditDescription()).to.equal(
-    'If you cancel, you’ll lose any changes you made on this screen and you will be returned to the service periods review page.',
-  );
+    expect(
+      handleCancelEditTitle({
+        getItemName: handleGetItemName,
+        itemData: { serviceBranch: 'AC' },
+      }),
+    ).to.equal('Cancel editing U.S. Army Air Corps service period?');
 
-  expect(handleCancelEditYes()).to.equal('Yes, cancel');
+    expect(handleCancelEditDescription()).to.equal(
+      'If you cancel, you’ll lose any changes you made on this screen and you will be returned to the service periods review page.',
+    );
 
-  expect(handleCancelEditNo()).to.equal('No, keep this');
+    expect(handleCancelEditYes()).to.equal('Yes, cancel');
 
-  expect(handleSummaryTitle({})).to.equal('Review service period records');
+    expect(handleCancelEditNo()).to.equal('No, keep this');
 
-  expect(
-    handleVeteranDepends({
-      application: {
-        claimant: { relationshipToVet: 'veteran' },
-        applicant: { applicantRelationshipToClaimant: 'Authorized Agent/Rep' },
-      },
-    }),
-  ).to.equal(false);
+    expect(handleSummaryTitle({})).to.equal('Review service period records');
 
-  expect(
-    handlePreparerVeteranDepends({
-      application: {
-        claimant: { relationshipToVet: 'veteran' },
-        applicant: { applicantRelationshipToClaimant: 'Authorized Agent/Rep' },
-      },
-    }),
-  ).to.equal(true);
+    expect(
+      handleVeteranDepends({
+        application: {
+          claimant: { relationshipToVet: 'veteran' },
+          applicant: {
+            applicantRelationshipToClaimant: 'Authorized Agent/Rep',
+          },
+        },
+      }),
+    ).to.equal(false);
 
-  expect(
-    handleNonVeteranDepends({
-      application: {
-        claimant: { relationshipToVet: 'veteran' },
-        applicant: { applicantRelationshipToClaimant: 'Authorized Agent/Rep' },
-      },
-    }),
-  ).to.equal(false);
+    expect(
+      handlePreparerVeteranDepends({
+        application: {
+          claimant: { relationshipToVet: 'veteran' },
+          applicant: {
+            applicantRelationshipToClaimant: 'Authorized Agent/Rep',
+          },
+        },
+      }),
+    ).to.equal(true);
 
-  expect(
-    handlePreparerNonVeteranDepends({
-      application: {
-        claimant: { relationshipToVet: 'veteran' },
-        applicant: { applicantRelationshipToClaimant: 'Authorized Agent/Rep' },
-      },
-    }),
-  ).to.equal(false);
+    expect(
+      handleNonVeteranDepends({
+        application: {
+          claimant: { relationshipToVet: 'veteran' },
+          applicant: {
+            applicantRelationshipToClaimant: 'Authorized Agent/Rep',
+          },
+        },
+      }),
+    ).to.equal(false);
+
+    expect(
+      handlePreparerNonVeteranDepends({
+        application: {
+          claimant: { relationshipToVet: 'veteran' },
+          applicant: {
+            applicantRelationshipToClaimant: 'Authorized Agent/Rep',
+          },
+        },
+      }),
+    ).to.equal(false);
+  });
 });
