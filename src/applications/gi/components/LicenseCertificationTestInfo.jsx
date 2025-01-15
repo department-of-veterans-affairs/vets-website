e92a4e90a01243dd68d780795df228e2a2447657
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-01-15 14:51:13 -0800

    Fix table width issue and add pagination

--- src/applications/gi/components/LicenseCertificationTestInfo.jsx
+++ src/applications/gi/components/LicenseCertificationTestInfo.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
+import { VaPagination } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { formatCurrency } from '../utils/helpers';
 
 function LcTestInfo({ tests }) {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useLayoutEffect(
+    // eslint-disable-next-line consistent-return
+    () => {
+      // error check required at this point?
+      // if (!error) {
+      const observer = new MutationObserver(() => {
+        const vaTableInner = document.querySelector(
+          '.table-wrapper va-table-inner',
+        );
+        if (vaTableInner?.shadowRoot) {
+          const { shadowRoot } = vaTableInner;
+          const usaTable = shadowRoot.querySelector('.usa-table');
+          if (usaTable) {
+            usaTable.style.width = '100%';
+          }
+        }
+      });
+      const vaTable = document.querySelector('.table-wrapper va-table');
+      if (vaTable) {
+        observer.observe(vaTable, {
+          attributes: true,
+          childList: true,
+          subtree: true,
+        });
+      }
+      return () => observer.disconnect();
+      // }
+    },
+    [tests],
+  );
+
+  const itemsPerPage = 10;
+
+  const totalPages = Math.ceil(tests.length / itemsPerPage);
+  const currentResults = tests.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage,
+  );
+
+  const handlePageChange = page => {
+    setCurrentPage(page);
+  };
+
   return (
     <>
       <h3>Test info</h3>
@@ -11,8 +57,8 @@ function LcTestInfo({ tests }) {
             <span>Test Name</span>
             <span>Fees</span>
           </va-table-row>
-          {tests &&
-            tests.map((test, index) => {
+          {currentResults &&
+            currentResults.map((test, index) => {
               return (
                 <va-table-row key={index}>
                   <span>{test.name}</span>
@@ -21,6 +67,15 @@ function LcTestInfo({ tests }) {
               );
             })}
         </va-table>
+        {/* {tests.length > currentResults.length && ( */}
+        <VaPagination
+          onPageSelect={e => handlePageChange(e.detail.page)}
+          page={currentPage}
+          pages={totalPages}
+          maxPageListLength={itemsPerPage}
+          // showLastPage
+        />
+        {/* )} */}
       </div>
     </>
   );
