2ca0929689ba03e32eec8fe8a231ba46de2d2fed
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2025-01-15 11:12:07 -0500

    Update unit test to check for CallToActionWidget, which has its own tests

--- src/applications/static-pages/health-care-manage-benefits/schedule-view-va-appointments-page/components/App/index.unit.spec.js
+++ src/applications/static-pages/health-care-manage-benefits/schedule-view-va-appointments-page/components/App/index.unit.spec.js
@@ -2,45 +2,14 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import CallToActionWidget from 'applications/static-pages/cta-widget';
 // Relative imports.
-import AuthContent from '../AuthContent';
-import UnauthContent from '../UnauthContent';
 import { App } from './index';
 
-describe('Get Medical Records Page <App>', () => {
-  it('renders what we expect when not authenticated and not LOA3', () => {
-    const wrapper = shallow(
-      <App
-        showNewScheduleViewAppointmentsPage
-        authenticatedWithSSOe={false}
-        hasLOA3={false}
-      />,
-    );
-    expect(wrapper.find(UnauthContent)).to.have.lengthOf(1);
-    expect(wrapper.find(AuthContent)).to.have.lengthOf(0);
-    wrapper.unmount();
-  });
-
-  it('renders what we expect when authenticated but not LOA3', () => {
-    // Must be both authed and LOA3 to see the Auth CTA
-    const wrapper = shallow(
-      <App
-        showNewScheduleViewAppointmentsPage
-        authenticatedWithSSOe
-        hasLOA3={false}
-      />,
-    );
-    expect(wrapper.find(UnauthContent)).to.have.lengthOf(1);
-    expect(wrapper.find(AuthContent)).to.have.lengthOf(0);
-    wrapper.unmount();
-  });
-
-  it('renders what we expect when a Cerner patient', () => {
-    const wrapper = shallow(
-      <App showNewScheduleViewAppointmentsPage authenticatedWithSSOe hasLOA3 />,
-    );
-    expect(wrapper.find(UnauthContent)).to.have.lengthOf(0);
-    expect(wrapper.find(AuthContent)).to.have.lengthOf(1);
+describe('Appointments Page <App>', () => {
+  it('renders the CallToActionWidget', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(CallToActionWidget)).to.have.lengthOf(1);
     wrapper.unmount();
   });
 });
