cc3caf0cfc3039af2de7c272293f8a030d69ec98
Richard Davis <richard.davis@agile6.com>
2025-01-16 15:47:29 -0500

    Adds fixtures

--- src/applications/mhv-supply-reordering/tests/e2e/setup.js
+++ src/applications/mhv-supply-reordering/tests/e2e/setup.js
@@ -1,9 +1,10 @@
-import userMock from '../fixtures/mocks/user.json';
+import featureToggles from '../fixtures/mocks/feature_toggles.json';
 import ipfMdotGetMock from '../fixtures/mocks/ipf_mdot_get.json';
 import ipfMdotPutMock from '../fixtures/mocks/ipf_mdot_put.json';
+import userMock from '../fixtures/mocks/user.json';
 
 const initializeApi = () => {
-  cy.intercept('GET', '/v0/feature_toggles*', {});
+  cy.intercept('GET', '/v0/feature_toggles*', featureToggles);
   cy.intercept('GET', '/v0/in_progress_forms/MDOT', ipfMdotGetMock);
   cy.intercept('PUT', '/v0/in_progress_forms/MDOT', ipfMdotPutMock);
   cy.intercept('GET', '/v0/user', userMock);
