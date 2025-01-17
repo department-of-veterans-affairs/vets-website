b934892f754a19c8453be678a57029b949962fbb
Rob Garrison <136959+Mottie@users.noreply.github.com>
2025-01-17 12:50:11 -0600

    Add VaDateField to web component fields

--- src/platform/forms-system/src/js/web-component-fields/index.js
+++ src/platform/forms-system/src/js/web-component-fields/index.js
@@ -1,6 +1,7 @@
 export { default as SsnField } from './SsnField';
 export { default as VaCheckboxField } from './VaCheckboxField';
 export { default as VaFileInputField } from './VaFileInputField';
+export { default as VaDateField } from './VaDateField';
 export { default as VaMemorableDateField } from './VaMemorableDateField';
 export { default as VaRadioField } from './VaRadioField';
 export { default as VaSelectField } from './VaSelectField';
