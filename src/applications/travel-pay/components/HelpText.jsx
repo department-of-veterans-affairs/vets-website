5a4318ede1b2ed2163dcc4579ef4b6093372f78d
Liz Townsend <liz.townsend@adhocteam.us>
2025-01-17 11:24:25 -0600

    update HelpText

--- src/applications/travel-pay/components/HelpText.jsx
+++ src/applications/travel-pay/components/HelpText.jsx
@@ -19,9 +19,9 @@ export const HelpTextManage = () => {
         .
       </p>
       <p className="vads-u-margin-top--2">
-        Or call <va-telephone contact="8555747292" /> from 7 a.m. to 7 p.m.
-        Monday through Friday. Have your claim number ready to share when you
-        call.
+        Or call the BTSSS call center at <va-telephone contact="8555747292" />{' '}
+        Monday through Friday, 8:00 a.m. to 8:00 p.m. ET. Have your claim number
+        ready to share when you call.
       </p>
     </div>
   );
