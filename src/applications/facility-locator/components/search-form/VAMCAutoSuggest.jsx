ec35bd4d27fb12819ee685f66ab1d25adab58686
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-01-17 15:56:10 -0600

    Working on manual value extraction for combo box

--- src/applications/facility-locator/components/search-form/VAMCAutoSuggest.jsx
+++ src/applications/facility-locator/components/search-form/VAMCAutoSuggest.jsx
@@ -6,21 +6,39 @@ import useServiceType from '../../hooks/useServiceType';
 const VAMCAutoSuggest = () => {
   const [serviceType, setServiceType] = useState('');
 
-  console.log('serviceType: ', serviceType);
-
   useEffect(() => {
-    if (document) {
-      const comboBoxInput = document.querySelector(
-        '.usa-combo-box [role="combobox"]',
-      );
+    const comboBox = document?.querySelector('va-combo-box');
+    const handleInput = event => {
+      setServiceType(event.target.value);
+    };
 
-      if (comboBoxInput) {
-        console.log('comboBoxInput: ', comboBoxInput.value);
-      }
-    }
-    // if (serviceType && serviceType?.length > 2) {
-    //   console.log('matches: ', useServiceType.serviceTypeFilter(serviceType));
-    // }
+    setTimeout(() => {
+      const input = comboBox.shadowRoot?.querySelector('input');
+      
+      console.log('input: ', input);
+    }, [500])
+
+    const observer = new MutationObserver(() => {
+
+    //   if (input) {
+    //     input.addEventListener('input', handleInput);
+    //     console.log('Input found and event listener attached.');
+
+    //     observer.disconnect();
+    //   }
+    // });
+
+    // observer.observe(comboBox, { childList: true, subtree: true });
+
+    // return () => {
+    //   observer.disconnect();
+
+    //   const input = comboBox.shadowRoot?.querySelector('input');
+
+    //   if (input) {
+    //     input.removeEventListener('input', handleInput);
+    //   }
+    // };
   }, []);
 
   return (
@@ -28,11 +46,11 @@ const VAMCAutoSuggest = () => {
       label="Service type"
       name="service-type"
       onVaSelect={e => {
-        console.log('e: ', e);
+        // console.log('e: ', e);
         // setServiceType(e);
       }}
       onKeyDown={e => {
-        console.log(e);
+        // console.log(e);
       }}
       required
       value={serviceType}
