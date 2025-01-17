3bb33e7f6cafbf9fcf3a6061c499b817d9570578
Midge-dev <sean.midgley.dev@gmail.com>
2025-01-17 12:47:29 -0800

    marriageDate

--- src/applications/686c-674/config/chapters/taskWizard/wizard/addDependentOptions.js
+++ src/applications/686c-674/config/chapters/taskWizard/wizard/addDependentOptions.js
@@ -6,22 +6,24 @@ import { TASK_KEYS } from '../../../constants';
 import { addDependentOptions } from './helpers';
 
 export const uiSchema = {
-  'view:selectable686Options': checkboxGroupUI({
-    title:
-      'Who do you want to add as a dependent? Check everyone you want to add.',
-    required: form => form?.['view:addOrRemoveDependents']?.remove,
-    tile: true,
-    labelHeaderLevel: '3',
-    labels: {
-      addSpouse: addDependentOptions.addSpouse,
-      addChild: addDependentOptions.addChild,
-      report674: addDependentOptions.report674,
-      addDisabledChild: addDependentOptions.addDisabledChild,
-    },
-    errorMessages: {
-      required: 'Select at least one option',
-    },
-  }),
+  'view:selectable686Options': {
+    ...checkboxGroupUI({
+      title:
+        'Who do you want to add as a dependent? Check everyone you want to add.',
+      required: form => form?.['view:addOrRemoveDependents']?.remove,
+      tile: true,
+      labelHeaderLevel: '3',
+      labels: {
+        addSpouse: addDependentOptions.addSpouse,
+        addChild: addDependentOptions.addChild,
+        report674: addDependentOptions.report674,
+        addDisabledChild: addDependentOptions.addDisabledChild,
+      },
+      errorMessages: {
+        required: 'Select at least one option',
+      },
+    }),
+  },
 };
 
 export const schema = {
