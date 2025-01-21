c9f4d4d388ace913843c08f8107614a360f0b05a
Sean Midgley <57480791+Midge-dev@users.noreply.github.com>
2025-01-21 10:12:05 -0800

    resolve bad merge (#33992)

--- src/applications/686c-674/config/form.js
+++ src/applications/686c-674/config/form.js
@@ -824,7 +824,7 @@ export const formConfig = {
             schema: deceasedDependentChildTypePage.schema,
             depends: (formData, index) =>
               isChapterFieldRequired(formData, TASK_KEYS.reportDeath) &&
-              formData?.deaths?.[index]?.dependentType === 'child',
+              formData?.deaths?.[index]?.dependentType === 'CHILD',
           }),
           dependentAdditionalInformationPartFour: pageBuilder.itemPage({
             title: 'Information needed to remove a dependent who has died',
