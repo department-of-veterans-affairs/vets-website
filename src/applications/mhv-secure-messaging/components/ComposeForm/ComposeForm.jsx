60ff72b7442862228aa08ac48eed2d9d0765a7a5
Kyle Cardwell <kyle.cardwell@acommsolutions.com>
2025-01-21 11:44:50 -0700

    removes beforeunload on ComposeForm if session expiration has passed

--- src/applications/mhv-secure-messaging/components/ComposeForm/ComposeForm.jsx
+++ src/applications/mhv-secure-messaging/components/ComposeForm/ComposeForm.jsx
@@ -726,8 +726,27 @@ const ComposeForm = props => {
 
   useEffect(
     () => {
-      window.addEventListener('beforeunload', beforeUnloadHandler);
+      const checkSessionExpiration = () => {
+        const sessionExpiration = localStorage.getItem('sessionExpiration');
+        if (sessionExpiration) {
+          const expirationTime = new Date(sessionExpiration);
+          const currentTime = new Date();
+
+          if (currentTime >= expirationTime) {
+            window.removeEventListener('beforeunload', beforeUnloadHandler);
+            return;
+          }
+        }
+        window.addEventListener('beforeunload', beforeUnloadHandler);
+      };
+
+      checkSessionExpiration();
+
+      // timer to check session expiration periodically
+      const interval = setInterval(checkSessionExpiration, 1000);
+
       return () => {
+        clearInterval(interval);
         window.removeEventListener('beforeunload', beforeUnloadHandler);
         window.onbeforeunload = null;
         noTimeout();
