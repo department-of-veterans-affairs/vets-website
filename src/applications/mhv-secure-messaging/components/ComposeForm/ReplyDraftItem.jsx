2c8aea681af8d5783bfdc098fb051799c52d0658
Kyle Cardwell <kyle.cardwell@acommsolutions.com>
2025-01-21 12:09:29 -0700

    removes beforeunload on Reply Drafts if session expiration has passed

--- src/applications/mhv-secure-messaging/components/ComposeForm/ReplyDraftItem.jsx
+++ src/applications/mhv-secure-messaging/components/ComposeForm/ReplyDraftItem.jsx
@@ -140,8 +140,26 @@ const ReplyDraftItem = props => {
 
   useEffect(
     () => {
-      window.addEventListener('beforeunload', beforeUnloadHandler);
+      const checkSessionExpiration = () => {
+        const sessionExpiration = localStorage.getItem('sessionExpiration');
+        if (sessionExpiration) {
+          const expirationTime = new Date(sessionExpiration);
+          const currentTime = new Date();
+
+          if (currentTime >= expirationTime) {
+            window.removeEventListener('beforeunload', beforeUnloadHandler);
+            return;
+          }
+        }
+        window.addEventListener('beforeunload', beforeUnloadHandler);
+      };
+
+      checkSessionExpiration();
+
+      // timer to check session expiration periodically
+      const interval = setInterval(checkSessionExpiration, 1000);
       return () => {
+        clearInterval(interval);
         window.removeEventListener('beforeunload', beforeUnloadHandler);
         window.onbeforeunload = null;
         noTimeout();
