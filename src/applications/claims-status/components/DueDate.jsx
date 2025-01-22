1b6d5067f1934a33f0ccafa03a3aafde11755f35
Adam Whitlock <8332986+adamwhitlock1@users.noreply.github.com>
2025-01-22 11:56:01 -0700

    migrate to date-fns and fix unit test

--- src/applications/claims-status/components/DueDate.jsx
+++ src/applications/claims-status/components/DueDate.jsx
@@ -1,18 +1,20 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import moment from 'moment';
-
+import { isBefore, formatDistanceToNow, parseISO } from 'date-fns';
 import { buildDateFormatter } from '../utils/helpers';
 
 export default function DueDate({ date }) {
-  const now = moment();
-  const dueDate = moment(date);
-  const pastDueDate = dueDate.isBefore(now);
+  const now = new Date();
+  const dueDate = parseISO(date);
+  const pastDueDate = isBefore(dueDate, now);
   const className = pastDueDate ? 'past-due' : 'due-file';
 
   const formattedClaimDate = buildDateFormatter()(date);
+
   const dueDateHeader = pastDueDate
-    ? `Needed from you by ${formattedClaimDate} - Due ${dueDate.fromNow()}`
+    ? `Needed from you by ${formattedClaimDate} - Due ${formatDistanceToNow(
+        dueDate,
+      )} ago`
     : `Needed from you by ${formattedClaimDate}`;
 
   return (
