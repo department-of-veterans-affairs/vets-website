32d5491d7e1fbbd92d567e813b4f8d07897062b3
Jacob Penner <japenner@gmail.com>
2025-01-22 10:26:13 -0500

    updates to radio options on statement type page

--- src/applications/simple-forms/21-4138/pages/statementType.js
+++ src/applications/simple-forms/21-4138/pages/statementType.js
@@ -6,6 +6,7 @@ import {
   ESCAPE_HATCH,
   STATEMENT_TYPES,
   STATEMENT_TYPE_LABELS,
+  STATEMENT_TYPE_DESCRIPTIONS,
 } from '../config/constants';
 
 /** @type {PageSchema} */
@@ -18,6 +19,7 @@ export const statementTypePage = {
           "We’ve improved how we process certain types of statements and requests. Before you continue with VA Form 21-4138, tell us what you’re trying to do and we'll check if there's a quicker way to help you.",
         label: 'Select the option that describes what you want to do',
         labels: STATEMENT_TYPE_LABELS,
+        descriptions: STATEMENT_TYPE_DESCRIPTIONS,
         tile: true,
         errorMessages: {
           required: "Select the kind of statement you'd like to submit",
