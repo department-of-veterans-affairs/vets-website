c2819f4eec14b8ff9c2cd5c75a79a9f64cce83ee
Kevin Suarez <kjsuarez94@gmail.com>
2025-01-23 11:36:57 -0500

    99422 wipe out preferred name on signout

--- src/platform/site-wide/user-nav/selectors.js
+++ src/platform/site-wide/user-nav/selectors.js
@@ -12,6 +12,7 @@ export const selectUserGreeting = createSelector(
   () => localStorage.getItem('userFirstName'),
   state => selectProfile(state)?.preferredName,
   (name, email, sessionPreferredName, sessionFirstName, preferredName) => {
+    if (preferredName) localStorage.setItem('preferredName', preferredName);
     if (
       preferredName ||
       sessionPreferredName ||
