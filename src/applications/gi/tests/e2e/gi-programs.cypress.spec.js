2c45a8959d2f1876a89d6379b54d5be48340be1b
taras <reloded2004@hotmail.com>
2025-01-23 13:28:38 -0500

    edm-580 cypress testing for programs

---
+++ src/applications/gi/tests/e2e/gi-programs.cypress.spec.js
@@ -0,0 +1,93 @@
+import data from '../data/calculator-constants.json';
+
+describe('GI Bill Comparison Tool - Search by Location', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'v1/gi/calculator_constants', {
+      statusCode: 200,
+      body: data,
+    });
+    cy.intercept('GET', '/data/cms/vamc-ehr.json', {
+      statusCode: 200,
+    });
+    cy.visit('education/gi-bill-comparison-tool/institution/318Z0032/');
+    cy.intercept('GET', '/v0/feature_toggles?*', {
+      data: {
+        type: 'feature_toggles',
+        features: [
+          {
+            name: 'toggleGiProgramsFlag',
+            value: true,
+          },
+        ],
+      },
+    });
+    cy.get('[data-testid="program-link"]').should('exist');
+    cy.get('[data-testid="program-link"]')
+      .first()
+      .click();
+  });
+  it('shows the list of programs (more than 20) with search controls', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.url().should('include', '/institution/318Z0032/');
+    cy.get('h1').should('contain', 'NEW YORK');
+    cy.get('#search-input').should('exist');
+    cy.contains('button', 'Search').should('exist');
+    cy.contains('button', 'Reset search').should('exist');
+  });
+  it('should show a "no results" message when an invalid program name is searched', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.get('#search-input')
+      .shadow()
+      .find('input')
+      .type('SomeRandomProgramName');
+    cy.contains('button', 'Search').click();
+    cy.get('#no-results-message')
+      .should('be.visible')
+      .and('contain', 'We didn’t find any results for');
+  });
+  it('should clear the search query and display all programs when "Reset search" is clicked', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.get('#search-input')
+      .shadow()
+      .find('input')
+      .type('ACCOUNTING');
+    cy.contains('button', 'Search').click();
+    cy.contains('button', 'Reset search').click();
+    cy.get('#search-input')
+      .shadow()
+      .find('input')
+      .should('have.value', '')
+      .should('be.focused');
+    cy.get('[data-testid="program-list-item"]').should('have.length', 20);
+  });
+  it('should display relevant results when a user searches for "ACCOUNTING"', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.get('#search-input')
+      .shadow()
+      .find('input')
+      .type('ACCOUNTING');
+    cy.contains('button', 'Search').click();
+    cy.get('#results-summary').should('contain', 'ACCOUNTING');
+    cy.get('[data-testid="program-list-item"]').should('have.length', 4);
+    cy.get('[data-testid="program-list-item"]')
+      .first()
+      .should('contain', 'ACCOUNTING-CPA TRACK-BS');
+  });
+  it('displays an error if the user tries to search with an empty input', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.contains('button', 'Search').click();
+    cy.contains('Please fill in a program name and then select search.').should(
+      'exist',
+    );
+  });
+  it('paginates correctly when there are more than 20 programs', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.get('va-pagination').should('exist');
+    cy.get('#results-summary').should('contain', 'Showing 1-20');
+    cy.get('va-pagination')
+      .shadow()
+      .find('[aria-label="Next page"]')
+      .click();
+    cy.get('#results-summary').should('contain', 'Showing 21-');
+  });
+});
