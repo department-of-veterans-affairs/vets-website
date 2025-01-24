5255b8e9d6a72a4ad7ee3bdf61c7912cff48b350
Jacob Penner <161746194+pennja@users.noreply.github.com>
2025-01-24 12:13:26 -0500

    add fieldset and legend to upload component (#34264)
    Co-authored-by: Jacob Penner <japenner@gmail.com>

--- src/applications/simple-forms/form-upload/config/constants.js
+++ src/applications/simple-forms/form-upload/config/constants.js
@@ -55,14 +55,16 @@ export const MUST_MATCH_ALERT = (variant, onCloseEvent, formData) => {
 };
 
 export const UPLOAD_GUIDELINES = Object.freeze(
-  <>
-    <h3 className="vads-u-margin-bottom--3">Your file</h3>
-    <p>
-      <span className="vads-u-font-weight--bold">Note:</span> After you upload
-      your file, you’ll need to continue to the next screen to submit it. If you
-      leave before you submit it, you’ll need to upload it again.
-    </p>
-  </>,
+  <fieldset>
+    <legend className="vads-u-font-weight--normal vads-u-padding-bottom--0">
+      <h3 className="vads-u-margin-bottom--3">Your file</h3>
+      <p>
+        <span className="vads-u-font-weight--bold">Note:</span> After you upload
+        your file, you’ll need to continue to the next screen to submit it. If
+        you leave before you submit it, you’ll need to upload it again.
+      </p>
+    </legend>
+  </fieldset>,
 );
 
 export const SAVE_IN_PROGRESS_CONFIG = {
