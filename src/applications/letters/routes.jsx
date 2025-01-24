99ff0b8a33ac91b070b68025420138a79e124260
Jane Cho <87551323+janechodance@users.noreply.github.com>
2025-01-24 11:34:24 -0500

    add letter page feature toggle

--- src/applications/letters/routes.jsx
+++ src/applications/letters/routes.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom-v5-compat';
 
+import { Toggler } from 'platform/utilities/feature-toggles';
 import AddressSection from './containers/AddressSection';
 import App from './containers/App';
 import DownloadLetters from './containers/DownloadLetters';
 import LetterList from './containers/LetterList';
 import Main from './containers/Main';
+import { LetterPage } from './containers/LetterPage';
+
+const letterPageFeatureFlagHandler = (
+  <Toggler.Hoc toggleName={Toggler.TOGGLE_NAMES.lettersPageNewDesign}>
+    {toggleValue =>
+      toggleValue ? <LetterPage /> : <Navigate to="confirm-address" replace />
+    }
+  </Toggler.Hoc>
+);
 
 const routes = (
   <Routes>
     <Route path="/" element={<App />}>
-      <Route index element={<Navigate to="confirm-address" replace />} />
+      <Route index element={letterPageFeatureFlagHandler} />
       <Route element={<DownloadLetters />}>
         <Route element={<AddressSection />} path="confirm-address" />
         <Route element={<Main />}>
