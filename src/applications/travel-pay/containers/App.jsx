890394643f3cf351d4adad6beb5b3e10c45c2efd
Liz Townsend <liz.townsend@adhocteam.us>
2025-01-24 15:26:02 -0600

    adjust routes, add Outlet to render child routes

--- src/applications/travel-pay/containers/App.jsx
+++ src/applications/travel-pay/containers/App.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Outlet } from 'react-router-dom-v5-compat';
+import { connect } from 'react-redux';
 
-import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-import formConfig from '../config/form';
+import { isLOA3 } from 'platform/user/selectors';
+import backendServices from '@department-of-veterans-affairs/platform-user/profile/backendServices';
+import { RequiredLoginView } from '@department-of-veterans-affairs/platform-user/RequiredLoginView';
+import { MhvSecondaryNav } from '@department-of-veterans-affairs/mhv/exports';
 
-export default function App({ location, children }) {
+const App = ({ identityVerified, user }) => {
   return (
-    <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-      {children}
-    </RoutedSavableApp>
+    <RequiredLoginView
+      serviceRequired={[backendServices.USER_PROFILE]}
+      user={user}
+    >
+      {identityVerified ? (
+        <>
+          <MhvSecondaryNav />
+          <Outlet />
+        </>
+      ) : (
+        // TODO: incorporate VerifyIdentityAlert from access error PR
+        <p>You must verify your identity</p>
+      )}
+    </RequiredLoginView>
   );
+};
+
+App.propTypes = {
+  identityVerified: PropTypes.bool,
+  user: PropTypes.object,
+};
+
+function mapStateToProps(state) {
+  return {
+    identityVerified: isLOA3(state),
+    user: state.user,
+  };
 }
+
+export default connect(mapStateToProps)(App);
