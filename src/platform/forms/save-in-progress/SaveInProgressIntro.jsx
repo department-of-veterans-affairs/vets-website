81a47b0fff784ba2db50c306876f000d550dd38c
Belle Poopongpanit <belle.poopongpanit@agile6.com>
2025-01-24 09:03:15 -0800

    [#286] Updated authenticated prefill intro alert to use dynamic variable {appType} (#34209)
    * Updated authenticated prefill intro alert to use dynamic variable
    
    * Updated SaveInProgressIntro unit test
    
    ---------
    
    Co-authored-by: Belle P <belle@Belle-Poopongpanit.local>

--- src/platform/forms/save-in-progress/SaveInProgressIntro.jsx
+++ src/platform/forms/save-in-progress/SaveInProgressIntro.jsx
@@ -170,9 +170,9 @@ class SaveInProgressIntro extends React.Component {
           <div>
             <va-alert status="info" visible>
               <h3>We've prefilled some of your information</h3>
-              Since you’re signed in, we can prefill part of your application
-              based on your profile details. You can also save your application
-              in progress and come back later to finish filling it out.
+              Since you’re signed in, we can prefill part of your {appType}{' '}
+              based on your profile details. You can also save your {appType} in
+              progress and come back later to finish filling it out.
             </va-alert>
             <br />
           </div>
