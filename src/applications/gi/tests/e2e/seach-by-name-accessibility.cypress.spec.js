5bd7d4fe383662caa8b5ace3a1e8230fd398faa8
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2025-01-24 10:35:11 -0500

    vebt-992 fixed cypress test

--- src/applications/gi/tests/e2e/seach-by-name-accessibility.cypress.spec.js
+++ src/applications/gi/tests/e2e/seach-by-name-accessibility.cypress.spec.js
@@ -38,7 +38,7 @@ describe('CT before search by name accessibility', () => {
     );
     cy.realPress(['Shift', 'Tab']);
     cy.focused().should('contain.text', 'Search');
-    cy.repeatKey('Tab', 13);
+    cy.repeatKey('Tab', 14);
     cy.focused().should('contain.text', 'Go to community focus details');
     cy.realPress('Enter');
     cy.get('[part="accordion-header"]').should(
