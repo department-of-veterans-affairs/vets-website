0f44af6454c997fc1bfdd9840c71ec7b5bd21c3c
Richard Davis <richard.davis@agile6.com>
2025-01-27 14:13:08 -0500

    Updates confirmation page

--- src/applications/mhv-supply-reordering/containers/ConfirmationPage.jsx
+++ src/applications/mhv-supply-reordering/containers/ConfirmationPage.jsx
@@ -3,14 +3,27 @@ import PropTypes from 'prop-types';
 import { format, isValid } from 'date-fns';
 import { useSelector } from 'react-redux';
 import { scrollTo, waitForRenderThenFocus } from 'platform/utilities/ui';
+import DlcTelephoneLink from '../components/DlcTelephoneLink';
+import DlcEmailLink from '../components/DlcEmailLink';
 
 export const ConfirmationPage = () => {
   const alertRef = useRef(null);
   const form = useSelector(state => state.form || {});
   const { submission, formId, data = {} } = form;
-  const { fullName } = data;
-  const submitDate = submission?.timestamp;
-  const confirmationNumber = submission?.response?.confirmationNumber;
+  const { fullName, emailAddress, chosenSupplies, supplies, permanentAddress } = data;
+
+  const submittedAt = submission?.timestamp
+    ? format(new Date(submission?.timestamp), 'MMMM d, yyyy')
+    : '';
+
+  const orderIds = submission?.response
+    .map(({ orderId }) => orderId)
+    .join(', ');
+
+  const productNames = Object.keys(chosenSupplies || {})
+    .filter(id => chosenSupplies[id])
+    .map(id => supplies.find(({ productId }) => id == productId))
+    .map(({ productName }) => productName);
 
   useEffect(
     () => {
@@ -32,40 +45,78 @@ export const ConfirmationPage = () => {
         />
       </div>
 
-      <va-alert status="success" ref={alertRef}>
-        <h2 slot="headline">Your application has been submitted</h2>
+      <va-alert status="success" class="vads-u-margin-bottom--4" ref={alertRef}>
+        <h2 slot="headline">You’ve submitted your medical supplies order</h2>
+        <p>
+          We’ll send you a confirmation email about your order to{' '}
+          {emailAddress}. Your order will be processed in the next business day.
+        </p>
       </va-alert>
 
-      <p>We may contact you for more information or documents.</p>
-      <p className="screen-only">Please print this page for your records.</p>
-      <div className="inset">
-        <h3 className="vads-u-margin-top--0 vads-u-font-size--h4">
-          Order Medical Supplies Claim{' '}
-          <span className="vads-u-font-weight--normal">(Form {formId})</span>
-        </h3>
-        {fullName ? (
-          <span>
-            for {fullName.first} {fullName.middle} {fullName.last}
-            {fullName.suffix ? `, ${fullName.suffix}` : null}
-          </span>
-        ) : null}
+      <va-summary-box>
+        <h3 slot="headline">Your order information</h3>
 
-        {confirmationNumber ? (
-          <>
-            <h4>Confirmation number</h4>
-            <p>{confirmationNumber}</p>
-          </>
-        ) : null}
+        <h4>Who submitted this form</h4>
+        <p>
+          {fullName ? (
+            <span>
+              for {fullName.first} {fullName.middle} {fullName.last}
+              {fullName.suffix ? `, ${fullName.suffix}` : null}
+            </span>
+          ) : null}
+        </p>
 
-        {isValid(submitDate) ? (
-          <p>
-            <strong>Date submitted</strong>
-            <br />
-            <span>{format(submitDate, 'MMMM d, yyyy')}</span>
-          </p>
-        ) : null}
+        <h4>Confirmation number</h4>
+        <p>{orderIds}</p>
 
-        <va-button onClick={window.print} text="Print this for your records" />
+        <h4>Supplies ordered</h4>
+        <ul>
+          {productNames.map(name => (
+            // eslint-disable-next-line react/jsx-key
+            <li>{name}</li>
+          ))}
+        </ul>
+
+        <h4>Shipping address</h4>
+        <p className="vads-u-margin-y--0 dd-privacy-mask">
+          {permanentAddress?.street} {permanentAddress?.street2 || ''}
+        </p>
+        <p className="vads-u-margin-top--0 dd-privacy-mask">
+          {`${permanentAddress?.city},
+            ${permanentAddress?.state || permanentAddress?.province} ${' '}
+            ${permanentAddress?.postalCode ||
+              permanentAddress?.internationalPostalCode}
+          `}
+        </p>
+
+        <h4>Date submitted</h4>
+        <p data-testid="dateSubmitted">{submittedAt}</p>
+
+        <h4>Confirmation for your records</h4>
+        <p>You can print this confirmation page for your records</p>
+
+        <va-button
+          class="vads-u-margin-top--4"
+          text="Print this page"
+          message-aria-describedby="Print this page for your records"
+          onClick={() => {
+            window.print();
+          }}
+        />
+      </va-summary-box>
+      <div>
+        <h2>How long will it take to receive my order?</h2>
+        <p>
+          Your order will be processed after your order submission. Orders
+          typically arrive within 7 to 10 business days.
+        </p>
+      </div>
+      <div>
+        <h2>What if I have more questions about my order?</h2>
+        <p>
+          If you have questions about your order, please call us at{' '}
+          <DlcTelephoneLink /> or <DlcEmailLink />
+        </p>
       </div>
       <a className="vads-c-action-link--green vads-u-margin-bottom--4" href="/">
         Go back to VA.gov
