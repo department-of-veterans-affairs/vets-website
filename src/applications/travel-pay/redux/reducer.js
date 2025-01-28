5f7189866fa861320ca6884a63d46ed5822689ec
Liz Townsend <liz.townsend@adhocteam.us>
2025-01-28 13:22:47 -0600

    add appointment call to actions + reducer

--- src/applications/travel-pay/redux/reducer.js
+++ src/applications/travel-pay/redux/reducer.js
@@ -2,33 +2,67 @@ import {
   FETCH_TRAVEL_CLAIMS_STARTED,
   FETCH_TRAVEL_CLAIMS_SUCCESS,
   FETCH_TRAVEL_CLAIMS_FAILURE,
+  FETCH_APPOINTMENT_STARTED,
+  FETCH_APPOINTMENT_SUCCESS,
+  FETCH_APPOINTMENT_FAILURE,
 } from './actions';
 
 const initialState = {
+  hasFetchedData: false,
   isLoading: false,
   isError: false,
-  travelClaims: [],
   error: null,
+  travelClaims: [],
+  hasFetchedAppointment: false,
+  isLoadingAppointment: false,
+  isAppointmentError: false,
+  appointmentData: {},
+  appointmentError: null,
 };
 
-function travelClaimsReducer(state = initialState, action) {
+function travelPayReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_TRAVEL_CLAIMS_STARTED:
       return {
         ...state,
+        hasFetchedData: false,
         isLoading: true,
       };
     case FETCH_TRAVEL_CLAIMS_SUCCESS:
       return {
         ...state,
+        hasFetchedData: true,
         isLoading: false,
         travelClaims: action.payload,
       };
     case FETCH_TRAVEL_CLAIMS_FAILURE:
       return {
         ...state,
+        hasFetchedData: true,
         isLoading: false,
+        isError: true,
         error: action.error,
+      };
+    case FETCH_APPOINTMENT_STARTED:
+      return {
+        ...state,
+        hasFetchedAppointment: false,
+        isLoadingAppointment: true,
+      };
+    case FETCH_APPOINTMENT_SUCCESS:
+      return {
+        ...state,
+        hasFetchedAppointment: true,
+        isLoadingAppointment: false,
+        appointmentData: action.payload,
+      };
+    case FETCH_APPOINTMENT_FAILURE:
+      return {
+        ...state,
+        hasFetchedAppointment: true,
+        isLoadingAppointment: false,
+        isAppointmentError: true,
+        appointmentError: action.error,
       };
     default:
       return state;
@@ -36,5 +70,5 @@ function travelClaimsReducer(state = initialState, action) {
 }
 
 export default {
-  travelPay: travelClaimsReducer,
+  travelPay: travelPayReducer,
 };
