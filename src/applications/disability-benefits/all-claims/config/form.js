79cb472eaf580976a76c5d9adb2c761a499bffa6
mreed101 <13966636+mreed101@users.noreply.github.com>
2025-01-29 12:06:25 -0500

    removes unused additional forms

--- src/applications/disability-benefits/all-claims/config/form.js
+++ src/applications/disability-benefits/all-claims/config/form.js
@@ -40,7 +40,6 @@ import {
   isUploadingSTR,
   needsToEnter781,
   needsToEnter781a,
-  showAdditionalFormsChapter,
   showPtsdCombat,
   showPtsdNonCombat,
   showSeparationLocation,
@@ -110,7 +109,6 @@ import {
   veteranInfo,
   workBehaviorChanges,
 } from '../pages';
-import * as additionalFormsChapterWrapper from '../pages/additionalFormsChapterWrapper';
 
 import { toxicExposurePages } from '../pages/toxicExposure/toxicExposurePages';
 import { form0781PagesConfig } from './form0781/index';
@@ -622,15 +620,8 @@ const formConfig = {
       },
     },
     additionalForms: {
-      title: 'Additional Forms',
+      title: 'Mental health statement',
       pages: {
-        additionalFormsChapterWrapper: {
-          title: 'Additional forms to support your claim',
-          path: 'additional-forms',
-          depends: formData => showAdditionalFormsChapter(formData),
-          uiSchema: additionalFormsChapterWrapper.uiSchema,
-          schema: additionalFormsChapterWrapper.schema,
-        },
         ...form0781PagesConfig,
       },
     },
