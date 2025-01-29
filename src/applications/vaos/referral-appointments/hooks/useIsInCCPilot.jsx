82d78d5ad6e669a96996d60b3628d1d17f42b3ac
Juan Castillo <juan.cast@agile6.com>
2025-01-29 10:15:22 -0800

    Update tests

--- src/applications/vaos/referral-appointments/hooks/useIsInCCPilot.jsx
+++ src/applications/vaos/referral-appointments/hooks/useIsInCCPilot.jsx
@@ -3,16 +3,18 @@ import { selectPatientFacilities } from '@department-of-veterans-affairs/platfor
 import { selectFeatureCCDirectScheduling } from '../../redux/selectors';
 import { getIsInCCPilot } from '../utils/pilot';
 
+const emptyPatientFacilities = [];
+
 const useIsInCCPilot = () => {
   const featureCCDirectScheduling = useSelector(
     selectFeatureCCDirectScheduling,
   );
-  const patentFacilities = useSelector(selectPatientFacilities);
+  const patientFacilities = useSelector(selectPatientFacilities);
 
   return {
     isInCCPilot: getIsInCCPilot(
       featureCCDirectScheduling,
-      patentFacilities || [],
+      patientFacilities || emptyPatientFacilities,
     ),
   };
 };
