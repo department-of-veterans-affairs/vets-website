2b5137338483dfe494cf84d0c44e78ca7657c014
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2025-01-29 16:37:09 -0500

    creates verify-alert

--- src/applications/personalization/profile/tests/components/direct-deposit/VerifyIdentity.unit.spec.jsx
+++ src/applications/personalization/profile/tests/components/direct-deposit/VerifyIdentity.unit.spec.jsx
@@ -1,91 +1,47 @@
 import React from 'react';
-import { cleanup, waitFor } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import { expect } from 'chai';
-import { mockCrypto } from 'platform/utilities/oauth/mockCrypto';
 import { renderInReduxProvider } from '~/platform/testing/unit/react-testing-library-helpers';
 import VerifyIdentity from '../../../components/direct-deposit/alerts/VerifyIdentity';
 
-const oldCrypto = global.window.crypto;
-
-const initialState = {
-  featureToggles: {
-    profileShowCredentialRetirementMessaging: true,
-  },
-  user: {
-    profile: {
-      signIn: {
-        service: 'idme',
-      },
-    },
-  },
-};
-
 describe('authenticated experience -- profile -- direct deposit', () => {
   describe('VerifyIdentity', () => {
-    beforeEach(() => {
-      global.window.crypto = mockCrypto;
-      window.location = new URL('https://dev.va.gov/');
-    });
+    afterEach(cleanup);
 
-    afterEach(() => {
-      global.window.crypto = oldCrypto;
-      cleanup();
-    });
-
-    it('renders the proper URLs for VerifyIdentity (SAML)', async () => {
-      const screen = renderInReduxProvider(
-        <VerifyIdentity useOAuth={false} />,
-        { initialState },
-      );
-      const loginGovAnchor = await screen.findByTestId('logingov');
-      const idmeAnchor = await screen.findByTestId('idme');
-
-      await waitFor(() =>
-        expect(loginGovAnchor.href).to.include(`logingov_signup_verified`),
-      );
-      await waitFor(() =>
-        expect(idmeAnchor.href).to.include('idme_signup_verified'),
-      );
-      screen.unmount();
-    });
-
-    it('renders the proper URLs for VerifyIdentity (OAuth)', async () => {
-      const screen = renderInReduxProvider(<VerifyIdentity useOAuth />, {
-        initialState,
+    it('shoud render the va-alert-sign-in: idme', () => {
+      const { container } = renderInReduxProvider(<VerifyIdentity />, {
+        initialState: {
+          user: { profile: { signIn: { serviceName: 'idme' } } },
+        },
       });
-      const loginGovAnchor = await screen.findByTestId('logingov');
-      const idmeAnchor = await screen.findByTestId('idme');
 
-      await waitFor(() =>
-        expect(loginGovAnchor.href).to.include(`type=logingov`),
-      );
-      await waitFor(() => expect(loginGovAnchor.href).to.include(`acr=min`));
-      await waitFor(() =>
-        expect(loginGovAnchor.href).to.include(`client_id=vaweb`),
-      );
-      await waitFor(() => expect(loginGovAnchor.href).to.include('/authorize'));
-      await waitFor(() =>
-        expect(loginGovAnchor.href).to.include('response_type=code'),
-      );
-      await waitFor(() =>
-        expect(loginGovAnchor.href).to.include('code_challenge='),
-      );
-      await waitFor(() => expect(loginGovAnchor.href).to.include('state='));
+      const signInAlert = container.querySelector('va-alert-sign-in');
+      expect(signInAlert).to.exist;
+      expect(signInAlert.getAttribute('variant')).to.eql('verifyIdMe');
+    });
 
-      await waitFor(() => expect(idmeAnchor.href).to.include(`type=idme`));
-      await waitFor(() => expect(idmeAnchor.href).to.include(`acr=min`));
-      await waitFor(() =>
-        expect(idmeAnchor.href).to.include(`client_id=vaweb`),
-      );
-      await waitFor(() => expect(idmeAnchor.href).to.include('/authorize'));
-      await waitFor(() =>
-        expect(idmeAnchor.href).to.include('response_type=code'),
-      );
-      await waitFor(() =>
-        expect(idmeAnchor.href).to.include('code_challenge='),
-      );
-      await waitFor(() => expect(idmeAnchor.href).to.include('state='));
-      screen.unmount();
+    it('shoud render the va-alert-sign-in: logingov', () => {
+      const { container } = renderInReduxProvider(<VerifyIdentity />, {
+        initialState: {
+          user: { profile: { signIn: { serviceName: 'logingov' } } },
+        },
+      });
+
+      const signInAlert = container.querySelector('va-alert-sign-in');
+      expect(signInAlert).to.exist;
+      expect(signInAlert.getAttribute('variant')).to.eql('verifyLoginGov');
+    });
+
+    it('shoud render the va-alert-sign-in: mhv', () => {
+      const { container } = renderInReduxProvider(<VerifyIdentity />, {
+        initialState: {
+          user: { profile: { signIn: { serviceName: 'mhv' } } },
+        },
+      });
+
+      const signInAlert = container.querySelector('va-alert-sign-in');
+      expect(signInAlert).to.exist;
+      expect(signInAlert.getAttribute('variant')).to.eql('signInEither');
     });
   });
 });
