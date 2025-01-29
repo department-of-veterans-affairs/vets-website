0e021bc23cbbdc6fd292a485a8922cab7cfdc808
Matthew Kerns <matthewkerns@Matthew-Kerns.lan>
2025-01-29 12:25:46 -0500

    implement route-based auth guard for /my-health paths

--- src/applications/ezr/containers/IntroductionPage.jsx
+++ src/applications/ezr/containers/IntroductionPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 import { focusElement } from 'platform/utilities/ui';
 import {
@@ -8,6 +9,7 @@ import {
   externalServices,
 } from 'platform/monitoring/DowntimeNotification';
 import VerifyAlert from 'platform/user/authorization/components/VerifyAlert';
+import RequiredLoginView from 'platform/user/authorization/components/RequiredLoginView';
 
 import { fetchEnrollmentStatus as fetchEnrollmentStatusAction } from '../utils/actions/enrollment-status';
 import { selectEnrollmentStatus } from '../utils/selectors/entrollment-status';
@@ -20,6 +22,7 @@ import content from '../locales/en/content.json';
 const IntroductionPage = ({ fetchEnrollmentStatus, route }) => {
   const { isLoading } = useSelector(selectEnrollmentStatus);
   const { isUserLOA1, isUserLOA3 } = useSelector(selectAuthStatus);
+  const location = useLocation();
   const { formConfig, pageList } = route;
   const sipProps = { formConfig, pageList };
 
@@ -34,35 +37,41 @@ const IntroductionPage = ({ fetchEnrollmentStatus, route }) => {
     [isUserLOA3],
   );
 
-  return (
-    <>
-      <div className="ezr-intro schemaform-intro">
-        <DowntimeNotification
-          appTitle={content['form-title']}
-          dependencies={[externalServices['1010ezr']]}
-        >
-          {isLoading ? (
-            <va-loading-indicator message={content['load-enrollment-status']} />
-          ) : (
-            <>
-              <ProcessDescription />
-              {isUserLOA1 ? (
-                <div className="vads-u-margin-y--4">
-                  <VerifyAlert
-                    headingLevel={3}
-                    dataTestId="ezr-identity-alert"
-                  />
-                </div>
-              ) : (
-                <SaveInProgressInfo {...sipProps} />
-              )}
-              <OMBInfo />
-            </>
-          )}
-        </DowntimeNotification>
-      </div>
-    </>
+  const pageContent = (
+    <div className="ezr-intro schemaform-intro">
+      <DowntimeNotification
+        appTitle={content['form-title']}
+        dependencies={[externalServices['1010ezr']]}
+      >
+        {isLoading ? (
+          <va-loading-indicator message={content['load-enrollment-status']} />
+        ) : (
+          <>
+            <ProcessDescription />
+            {isUserLOA1 ? (
+              <div className="vads-u-margin-y--4">
+                <VerifyAlert headingLevel={3} dataTestId="ezr-identity-alert" />
+              </div>
+            ) : (
+              <SaveInProgressInfo {...sipProps} />
+            )}
+            <OMBInfo />
+          </>
+        )}
+      </DowntimeNotification>
+    </div>
   );
+
+  // Only require login for /my-health routes
+  if (location.pathname.startsWith('/my-health')) {
+    return (
+      <RequiredLoginView authRequired={1} serviceRequired="user-profile">
+        {pageContent}
+      </RequiredLoginView>
+    );
+  }
+
+  return pageContent;
 };
 
 IntroductionPage.propTypes = {
