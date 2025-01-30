bd8417d242559ef79e07714eb3a76548f9f74be8
Scott Regenthal <scottregenthal@gmail.com>
2025-01-30 09:28:40 -0700

    [DBEX] Events list summary page

---
+++ src/applications/disability-benefits/all-claims/content/traumaticEventsList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const eventsListPageTitle =
+  'Traumatic events from your military service';
+
+export const eventsListDescription = () => {
+  return (
+    <>
+      <p>
+        Any information you provide about your event will help us understand
+        your situation and identify evidence to support your claim.
+      </p>
+      <p>
+        You can tell us about a single event, or a recurring or ongoing
+        experience.
+      </p>
+      <p>
+        It’s also OK if you don’t add an event. You can skip this question if
+        you don’t feel comfortable answering. Select <strong>Continue</strong>{' '}
+        to go to the next question.
+      </p>
+    </>
+  );
+};
