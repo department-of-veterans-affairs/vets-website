84bfdb639ead0acb0b79c4da6851f19ecfc9d0c2
Micah Chiang <15097156+micahchiang@users.noreply.github.com>
2025-01-31 15:42:21 -0800

    Revert "Update component-library to v49.0.0 (#34404)"
    This reverts commit ab015fb25aab7c9a83d8e82cc96f8406a2883854.

--- src/applications/_mock-form-ae-design-patterns/utils/data/tabs.js
+++ src/applications/_mock-form-ae-design-patterns/utils/data/tabs.js
@@ -28,7 +28,7 @@ export const tabsConfig = {
       path: '/2/task-orange',
 
       description: 'Pattern 2 - Orange Task',
-      bgColor: '--vads-color-orange',
+      bgColor: '--uswds-system-color-orange-40',
       textColor: '--vads-color-black',
     },
     {
