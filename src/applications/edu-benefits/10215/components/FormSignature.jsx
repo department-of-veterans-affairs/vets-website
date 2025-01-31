6da22f8d56eccf397167e142fb3b418bdb870f69
govcioChad <162637891+govcioChad@users.noreply.github.com>
2025-01-31 12:47:41 -0800

    #vebt-901 #comment Switched setFormData to dispatch

--- src/applications/edu-benefits/10215/components/FormSignature.jsx
+++ src/applications/edu-benefits/10215/components/FormSignature.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setData } from 'platform/forms-system/src/js/actions';
 import set from 'platform/utilities/data/set';
 import { VaTextInput } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
@@ -12,13 +12,14 @@ export const FormSignature = ({
   setSignatureError,
   signatureLabel,
   formData,
-  setFormData,
   signaturePath,
   required,
   showError,
   validations,
   requiredErrorMessage,
 }) => {
+  const dispatch = useDispatch();
+
   useEffect(
     () => {
       // Required validation always comes first
@@ -47,13 +48,10 @@ export const FormSignature = ({
     ],
   );
 
-  // Update signature in formData
   useEffect(
     () => {
-      setFormData(set(signaturePath, signature.value, formData));
+      dispatch(setData(set(signaturePath, signature.value, formData)));
     },
-    // Don't re-execute when formData changes because this changes formData
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     [signature, signaturePath],
   );
 
@@ -76,36 +74,17 @@ export const FormSignature = ({
 };
 
 FormSignature.propTypes = {
-  signature: PropTypes.object,
   setSignature: PropTypes.func.isRequired,
-  signatureError: PropTypes.string,
   setSignatureError: PropTypes.func.isRequired,
   formData: PropTypes.object.isRequired,
-  // eslint-disable-next-line react/sort-prop-types
-  setFormData: PropTypes.func.isRequired,
   showError: PropTypes.bool.isRequired,
   required: PropTypes.bool,
-  /**
-   * The label for the signature input
-   */
   signatureLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-  /**
-   * The path in the formData to the signature value
-   */
   signaturePath: PropTypes.string,
-  /**
-   * An array of validator functions. Each function returns a string for the
-   * validation message if the input is invalid, or undefined if the input is
-   * valid.
-   *
-   * Subsequent validation functions do not run if previous validators
-   * return an error message.
-   *
-   * Validator function have the following signature:
-   *   function validator(signatureValue: string, formData: Object): string | undefined
-   */
   validations: PropTypes.arrayOf(PropTypes.func),
   requiredErrorMessage: PropTypes.string,
+  signature: PropTypes.object,
+  signatureError: PropTypes.string,
 };
 
 FormSignature.defaultProps = {
@@ -114,15 +93,6 @@ FormSignature.defaultProps = {
   requiredErrorMessage: 'Please enter your name',
   required: true,
   validations: [],
-  // eslint-disable-next-line react/default-props-match-prop-types
-  setFormData: () => {},
 };
 
-const mapDispatchToProps = {
-  setFormData: setData,
-};
-
-export default connect(
-  null,
-  mapDispatchToProps,
-)(FormSignature);
+export default FormSignature;
