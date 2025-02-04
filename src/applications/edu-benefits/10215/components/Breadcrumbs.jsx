5914991120b43b84d910904515ae19613dbf8c34
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2025-02-04 09:59:57 -0500

    vebt-898 added unit test

--- src/applications/edu-benefits/10215/components/Breadcrumbs.jsx
+++ src/applications/edu-benefits/10215/components/Breadcrumbs.jsx
@@ -43,7 +43,7 @@ const Breadcrumbs = () => {
   ];
   return (
     <div className="row">
-      <VaBreadcrumbs uswds breadcrumbList={crumbs} />
+      <VaBreadcrumbs uswds breadcrumbList={crumbs} data-testid="breadcrumbs" />
     </div>
   );
 };
