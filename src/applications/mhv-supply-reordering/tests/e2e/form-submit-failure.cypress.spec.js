c845ee4be5e4417fd51285da1af1dc412e8551c3
Richard Davis <richard.davis@agile6.com>
2025-02-04 16:38:19 -0500

    Adds specs for failing sumbit

--- src/applications/mhv-supply-reordering/tests/e2e/form-submit-failure.cypress.spec.js
+++ src/applications/mhv-supply-reordering/tests/e2e/form-submit-failure.cypress.spec.js
@@ -1,38 +1,12 @@
 import { appName, rootUrl } from '../../manifest.json';
 import { initializeApi, userMock } from './setup';
 
-let heading;
-
 describe(`${appName} -- form submit failure`, () => {
-  before(() => initializeApi());
   beforeEach(() => {
+    initializeApi();
     cy.viewportPreset('va-top-mobile-1');
     cy.login(userMock);
     cy.visit(rootUrl);
-  });
-
-  it('handles a 500 response', () => {
-    cy.intercept('POST', '/v0/mdot/supplies', { statusCode: 500 });
-    // introduction
-    cy.findByText(/^Start a new order$/).click();
-
-    // choose supplies
-    cy.selectVaCheckbox('root_chosenSupplies_6584', true);
-    cy.findByText(/^Continue$/).click();
-
-    // contact information
-    cy.findByText(/^Continue$/).click();
-
-    // review
-    cy.findByText(/^Submit order$/).click();
-
-    // confirmation
-    cy.injectAxeThenAxeCheck();
-    heading = {
-      level: 2,
-      name: /^Your application has been submitted$/,
-    };
-    cy.findByRole('heading', heading);
   });
 
   it('handles a 422 response', () => {
@@ -42,20 +16,32 @@ describe(`${appName} -- form submit failure`, () => {
 
     // choose supplies
     cy.selectVaCheckbox('root_chosenSupplies_6584', true);
-    cy.findByText(/^Continue$/).click();
+    cy.clickContinue();
 
     // contact information
-    cy.findByText(/^Continue$/).click();
+    cy.clickContinue();
 
     // review
-    cy.findByText(/^Submit order$/).click();
-
-    // confirmation
+    cy.findByText(/^Submit$/).click();
+    cy.findByText(/We’re sorry/);
     cy.injectAxeThenAxeCheck();
-    heading = {
-      level: 2,
-      name: /^Your application has been submitted$/,
-    };
-    cy.findByRole('heading', heading);
+  });
+
+  it('handles a 500 response', () => {
+    cy.intercept('POST', '/v0/mdot/supplies', { statusCode: 500 });
+    // introduction
+    cy.findByText(/^Start a new order$/).click();
+
+    // choose supplies
+    cy.selectVaCheckbox('root_chosenSupplies_6584', true);
+    cy.clickContinue();
+
+    // contact information
+    cy.clickContinue();
+
+    // review
+    cy.findByText(/^Submit$/).click();
+    cy.findByText(/We’re sorry/);
+    cy.injectAxeThenAxeCheck();
   });
 });
