1893f72693c94c530a1146fc0ac423a93a0b2f17
taras <reloded2004@hotmail.com>
2025-02-05 09:32:06 -0500

    cypress update

--- src/applications/gi/tests/e2e/gi-programs.cypress.spec.js
+++ src/applications/gi/tests/e2e/gi-programs.cypress.spec.js
@@ -9,14 +9,21 @@ describe('GI Bill Comparison Tool - Programs List', () => {
     cy.intercept('GET', '/data/cms/vamc-ehr.json', {
       statusCode: 200,
     });
-
-    // .as('featureToggles');
-    cy.intercept(
-      'GET',
-      '**/v0/gi/institution_programs/search?type=NCD&facility_code=31800132&disable_pagination=true*',
-      {
-        statusCode: 200,
-        body: [
+    cy.intercept('GET', '/v0/feature_toggles?*', {
+      data: {
+        type: 'feature_toggles',
+        features: [
+          {
+            name: 'gi_comparison_tool_programs_toggle_flag',
+            value: true,
+          },
+        ],
+      },
+    }).as('featureToggles');
+    cy.intercept('GET', '**/v0/gi/institution_programs/search*', {
+      statusCode: 200,
+      body: {
+        data: [
           {
             id: '2614928',
             type: 'institution_programs',
@@ -218,6 +225,246 @@ describe('GI Bill Comparison Tool - Programs List', () => {
             },
           },
           {
+            id: '2614934',
+            type: 'institution_programs',
+            attributes: {
+              programType: 'NCD',
+              description: 'BUILDINGS AND CONSTRUTION CERT',
+              facilityCode: '31800132',
+              institutionName: 'NEW YORK UNIVERSITY',
+              city: 'NEW YORK',
+              state: 'NY',
+              country: 'USA',
+              preferredProvider: false,
+              vaBah: 4525,
+              dodBah: 4407,
+              schoolClosing: false,
+              schoolClosingOn: null,
+              cautionFlags: [],
+              ojtAppType: null,
+            },
+          },
+          {
+            id: '2614935',
+            type: 'institution_programs',
+            attributes: {
+              programType: 'NCD',
+              description: 'CLEAN ENERGY CERTIFICATE',
+              facilityCode: '31800132',
+              institutionName: 'NEW YORK UNIVERSITY',
+              city: 'NEW YORK',
+              state: 'NY',
+              country: 'USA',
+              preferredProvider: false,
+              vaBah: 4525,
+              dodBah: 4407,
+              schoolClosing: false,
+              schoolClosingOn: null,
+              cautionFlags: [],
+              ojtAppType: null,
+            },
+          },
+          {
+            id: '2614936',
+            type: 'institution_programs',
+            attributes: {
+              programType: 'NCD',
+              description: 'COMMERICAL PROPERTY MANAGEMENT CERT',
+              facilityCode: '31800132',
+              institutionName: 'NEW YORK UNIVERSITY',
+              city: 'NEW YORK',
+              state: 'NY',
+              country: 'USA',
+              preferredProvider: false,
+              vaBah: 4525,
+              dodBah: 4407,
+              schoolClosing: false,
+              schoolClosingOn: null,
+              cautionFlags: [],
+              ojtAppType: null,
+            },
+          },
+          {
+            id: '2614937',
+            type: 'institution_programs',
+            attributes: {
+              programType: 'NCD',
+              description: 'CORPORATE FINANCE CERTIFICATE',
+              facilityCode: '31800132',
+              institutionName: 'NEW YORK UNIVERSITY',
+              city: 'NEW YORK',
+              state: 'NY',
+              country: 'USA',
+              preferredProvider: false,
+              vaBah: 4525,
+              dodBah: 4407,
+              schoolClosing: false,
+              schoolClosingOn: null,
+              cautionFlags: [],
+              ojtAppType: null,
+            },
+          },
+          {
+            id: '2614934',
+            type: 'institution_programs',
+            attributes: {
+              programType: 'NCD',
+              description: 'BUILDINGS AND CONSTRUTION CERT',
+              facilityCode: '31800132',
+              institutionName: 'NEW YORK UNIVERSITY',
+              city: 'NEW YORK',
+              state: 'NY',
+              country: 'USA',
+              preferredProvider: false,
+              vaBah: 4525,
+              dodBah: 4407,
+              schoolClosing: false,
+              schoolClosingOn: null,
+              cautionFlags: [],
+              ojtAppType: null,
+            },
+          },
+          {
+            id: '2614935',
+            type: 'institution_programs',
+            attributes: {
+              programType: 'NCD',
+              description: 'CLEAN ENERGY CERTIFICATE',
+              facilityCode: '31800132',
+              institutionName: 'NEW YORK UNIVERSITY',
+              city: 'NEW YORK',
+              state: 'NY',
+              country: 'USA',
+              preferredProvider: false,
+              vaBah: 4525,
+              dodBah: 4407,
+              schoolClosing: false,
+              schoolClosingOn: null,
+              cautionFlags: [],
+              ojtAppType: null,
+            },
+          },
+          {
+            id: '2614936',
+            type: 'institution_programs',
+            attributes: {
+              programType: 'NCD',
+              description: 'COMMERICAL PROPERTY MANAGEMENT CERT',
+              facilityCode: '31800132',
+              institutionName: 'NEW YORK UNIVERSITY',
+              city: 'NEW YORK',
+              state: 'NY',
+              country: 'USA',
+              preferredProvider: false,
+              vaBah: 4525,
+              dodBah: 4407,
+              schoolClosing: false,
+              schoolClosingOn: null,
+              cautionFlags: [],
+              ojtAppType: null,
+            },
+          },
+          {
+            id: '2614937',
+            type: 'institution_programs',
+            attributes: {
+              programType: 'NCD',
+              description: 'CORPORATE FINANCE CERTIFICATE',
+              facilityCode: '31800132',
+              institutionName: 'NEW YORK UNIVERSITY',
+              city: 'NEW YORK',
+              state: 'NY',
+              country: 'USA',
+              preferredProvider: false,
+              vaBah: 4525,
+              dodBah: 4407,
+              schoolClosing: false,
+              schoolClosingOn: null,
+              cautionFlags: [],
+              ojtAppType: null,
+            },
+          },
+          {
+            id: '2614934',
+            type: 'institution_programs',
+            attributes: {
+              programType: 'NCD',
+              description: 'BUILDINGS AND CONSTRUTION CERT',
+              facilityCode: '31800132',
+              institutionName: 'NEW YORK UNIVERSITY',
+              city: 'NEW YORK',
+              state: 'NY',
+              country: 'USA',
+              preferredProvider: false,
+              vaBah: 4525,
+              dodBah: 4407,
+              schoolClosing: false,
+              schoolClosingOn: null,
+              cautionFlags: [],
+              ojtAppType: null,
+            },
+          },
+          {
+            id: '2614935',
+            type: 'institution_programs',
+            attributes: {
+              programType: 'NCD',
+              description: 'CLEAN ENERGY CERTIFICATE',
+              facilityCode: '31800132',
+              institutionName: 'NEW YORK UNIVERSITY',
+              city: 'NEW YORK',
+              state: 'NY',
+              country: 'USA',
+              preferredProvider: false,
+              vaBah: 4525,
+              dodBah: 4407,
+              schoolClosing: false,
+              schoolClosingOn: null,
+              cautionFlags: [],
+              ojtAppType: null,
+            },
+          },
+          {
+            id: '2614936',
+            type: 'institution_programs',
+            attributes: {
+              programType: 'NCD',
+              description: 'COMMERICAL PROPERTY MANAGEMENT CERT',
+              facilityCode: '31800132',
+              institutionName: 'NEW YORK UNIVERSITY',
+              city: 'NEW YORK',
+              state: 'NY',
+              country: 'USA',
+              preferredProvider: false,
+              vaBah: 4525,
+              dodBah: 4407,
+              schoolClosing: false,
+              schoolClosingOn: null,
+              cautionFlags: [],
+              ojtAppType: null,
+            },
+          },
+          {
+            id: '2614937',
+            type: 'institution_programs',
+            attributes: {
+              programType: 'NCD',
+              description: 'CORPORATE FINANCE CERTIFICATE',
+              facilityCode: '31800132',
+              institutionName: 'NEW YORK UNIVERSITY',
+              city: 'NEW YORK',
+              state: 'NY',
+              country: 'USA',
+              preferredProvider: false,
+              vaBah: 4525,
+              dodBah: 4407,
+              schoolClosing: false,
+              schoolClosingOn: null,
+              cautionFlags: [],
+              ojtAppType: null,
+            },
+          },
+          {
             id: '2614938',
             type: 'institution_programs',
             attributes: {
@@ -297,179 +544,47 @@ describe('GI Bill Comparison Tool - Programs List', () => {
               ojtAppType: null,
             },
           },
-          {
-            id: '2614942',
-            type: 'institution_programs',
-            attributes: {
-              programType: 'NCD',
-              description: 'DIVERSITY-EQUALITY-INCLUSION CERTIFICATE',
-              facilityCode: '31800132',
-              institutionName: 'NEW YORK UNIVERSITY',
-              city: 'NEW YORK',
-              state: 'NY',
-              country: 'USA',
-              preferredProvider: false,
-              vaBah: 4525,
-              dodBah: 4407,
-              schoolClosing: false,
-              schoolClosingOn: null,
-              cautionFlags: [],
-              ojtAppType: null,
-            },
-          },
-          {
-            id: '2614943',
-            type: 'institution_programs',
-            attributes: {
-              programType: 'NCD',
-              description: 'EFFECTIVE BUSINESS WRITING CERTIFICATE',
-              facilityCode: '31800132',
-              institutionName: 'NEW YORK UNIVERSITY',
-              city: 'NEW YORK',
-              state: 'NY',
-              country: 'USA',
-              preferredProvider: false,
-              vaBah: 4525,
-              dodBah: 4407,
-              schoolClosing: false,
-              schoolClosingOn: null,
-              cautionFlags: [],
-              ojtAppType: null,
-            },
-          },
-          {
-            id: '2614944',
-            type: 'institution_programs',
-            attributes: {
-              programType: 'NCD',
-              description: 'EXECUTIVE COACHING CERTIFICATE',
-              facilityCode: '31800132',
-              institutionName: 'NEW YORK UNIVERSITY',
-              city: 'NEW YORK',
-              state: 'NY',
-              country: 'USA',
-              preferredProvider: false,
-              vaBah: 4525,
-              dodBah: 4407,
-              schoolClosing: false,
-              schoolClosingOn: null,
-              cautionFlags: [],
-              ojtAppType: null,
-            },
-          },
-          {
-            id: '2614945',
-            type: 'institution_programs',
-            attributes: {
-              programType: 'NCD',
-              description: 'FINANCIAL PLANNING CERTIFICATE',
-              facilityCode: '31800132',
-              institutionName: 'NEW YORK UNIVERSITY',
-              city: 'NEW YORK',
-              state: 'NY',
-              country: 'USA',
-              preferredProvider: false,
-              vaBah: 4525,
-              dodBah: 4407,
-              schoolClosing: false,
-              schoolClosingOn: null,
-              cautionFlags: [],
-              ojtAppType: null,
-            },
-          },
-          {
-            id: '2614946',
-            type: 'institution_programs',
-            attributes: {
-              programType: 'NCD',
-              description: 'FINTECH CERTIFICATE',
-              facilityCode: '31800132',
-              institutionName: 'NEW YORK UNIVERSITY',
-              city: 'NEW YORK',
-              state: 'NY',
-              country: 'USA',
-              preferredProvider: false,
-              vaBah: 4525,
-              dodBah: 4407,
-              schoolClosing: false,
-              schoolClosingOn: null,
-              cautionFlags: [],
-              ojtAppType: null,
-            },
-          },
-          {
-            id: '2614947',
-            type: 'institution_programs',
-            attributes: {
-              programType: 'NCD',
-              description: 'FULL STACK WEB DEVELOPMENT CERT',
-              facilityCode: '31800132',
-              institutionName: 'NEW YORK UNIVERSITY',
-              city: 'NEW YORK',
-              state: 'NY',
-              country: 'USA',
-              preferredProvider: false,
-              vaBah: 4525,
-              dodBah: 4407,
-              schoolClosing: false,
-              schoolClosingOn: null,
-              cautionFlags: [],
-              ojtAppType: null,
-            },
-          },
-          {
-            id: '2614948',
-            type: 'institution_programs',
-            attributes: {
-              programType: 'NCD',
-              description: 'FUNDRAISING CERTIFICATE',
-              facilityCode: '31800132',
-              institutionName: 'NEW YORK UNIVERSITY',
-              city: 'NEW YORK',
-              state: 'NY',
-              country: 'USA',
-              preferredProvider: false,
-              vaBah: 4525,
-              dodBah: 4407,
-              schoolClosing: false,
-              schoolClosingOn: null,
-              cautionFlags: [],
-              ojtAppType: null,
-            },
-          },
         ],
-      },
-    );
-    cy.intercept('GET', '/v0/feature_toggles?*', {
-      data: {
-        type: 'feature_toggles',
-        features: [
-          {
-            name: 'gi_comparison_tool_programs_toggle_flag',
-            value: true,
+        meta: {
+          version: {
+            number: 502,
+            createdAt: '2025-01-29T14:11:53.094Z',
+            preview: false,
           },
-        ],
+          count: 47,
+          facets: {
+            type: {
+              ncd: 47,
+            },
+            state: {
+              ny: 47,
+            },
+            provider: [
+              {
+                name: 'NEW YORK UNIVERSITY',
+                count: 47,
+              },
+            ],
+            country: [
+              {
+                name: 'USA',
+                count: 47,
+              },
+            ],
+          },
+        },
       },
-    });
-    // .as('institutionPrograms');
+    }).as('institutionPrograms');
 
-    // cy.visit(
-    //   'education/gi-bill-comparison-tool/institution/31800132/non-college-degree',
-    // );
-
-    // cy.wait('@institutionPrograms');
-    // cy.wait('@featureToggles');
-    // cy.get('a[data-testid="program-link"]', {
-    //   timeout: 10000,
-    // }).should('exist');
-    // cy.get('a[data-testid="program-link"]', { timeout: 10000 })
-    //   .first()
-    //   .click();
-  });
-  it('should show a "no results" message when an invalid program name is searched', () => {
     cy.visit(
       'education/gi-bill-comparison-tool/institution/31800132/non-college-degree',
     );
+
+    cy.wait('@institutionPrograms');
+    cy.wait('@featureToggles');
+  });
+
+  it('should show a "no results" message when an invalid program name is searched', () => {
     cy.injectAxeThenAxeCheck();
     cy.get('#search-input')
       .shadow()
@@ -480,21 +595,21 @@ describe('GI Bill Comparison Tool - Programs List', () => {
       .should('be.visible')
       .and('contain', 'We didn’t find any results for');
   });
-  // it('should clear the search query and display all programs when "Reset search" is clicked', () => {
-  //   cy.injectAxeThenAxeCheck();
-  //   cy.get('#search-input')
-  //     .shadow()
-  //     .find('input')
-  //     .type('ACCOUNTING');
-  //   cy.contains('button', 'Search').click();
-  //   cy.contains('button', 'Reset search').click();
-  //   cy.get('#search-input')
-  //     .shadow()
-  //     .find('input')
-  //     .should('have.value', '')
-  //     .should('be.focused');
-  //   cy.get('[data-testid="program-list-item"]').should('have.length', 20);
-  // });
+  it('should clear the search query and display all programs when "Reset search" is clicked', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.get('#search-input')
+      .shadow()
+      .find('input')
+      .type('ACCOUNTING');
+    cy.contains('button', 'Search').click();
+    cy.contains('button', 'Reset search').click();
+    cy.get('#search-input')
+      .shadow()
+      .find('input')
+      .should('have.value', '')
+      .should('be.focused');
+    cy.get('[data-testid="program-list-item"]').should('have.length', 20);
+  });
   // it('should display relevant results when a user searches for "ACCOUNTING"', () => {
   //   cy.injectAxeThenAxeCheck();
   //   cy.get('#search-input')
@@ -508,28 +623,28 @@ describe('GI Bill Comparison Tool - Programs List', () => {
   //     .first()
   //     .should('contain', 'ACCOUNTING-CPA TRACK-BS');
   // });
-  // it('displays an error if the user tries to search with an empty input', () => {
-  //   cy.injectAxeThenAxeCheck();
-  //   cy.get('#search-input')
-  //     .shadow()
-  //     .find('input')
-  //     .type(' ');
-  //   cy.contains('button', 'Search').click();
-  //   cy.get('[class="usa-error-message"]')
-  //     .should(
-  //       'contain',
-  //       'Please fill in a program name and then select search.',
-  //     )
-  //     .should('exist');
-  // });
-  // it('paginates correctly when there are more than 20 programs', () => {
-  //   cy.injectAxeThenAxeCheck();
-  //   cy.get('va-pagination').should('exist');
-  //   cy.get('#results-summary').should('contain', 'Showing 1-20');
-  //   cy.get('va-pagination')
-  //     .shadow()
-  //     .find('[aria-label="Next page"]')
-  //     .click();
-  //   cy.get('#results-summary').should('contain', 'Showing 21-');
-  // });
+  it('displays an error if the user tries to search with an empty input', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.get('#search-input')
+      .shadow()
+      .find('input')
+      .type(' ');
+    cy.contains('button', 'Search').click();
+    cy.get('[class="usa-error-message"]')
+      .should(
+        'contain',
+        'Please fill in a program name and then select search.',
+      )
+      .should('exist');
+  });
+  it('paginates correctly when there are more than 20 programs', () => {
+    cy.injectAxeThenAxeCheck();
+    cy.get('va-pagination').should('exist');
+    cy.get('#results-summary').should('contain', 'Showing 1-20');
+    cy.get('va-pagination')
+      .shadow()
+      .find('[aria-label="Next page"]')
+      .click();
+    cy.get('#results-summary').should('contain', 'Showing 21-');
+  });
 });
