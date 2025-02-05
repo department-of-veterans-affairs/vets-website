50ff48da865d8140d77001cfad2718af9795af61
Taras Kurilo <reloded2004@hotmail.com>
2025-02-05 11:32:30 -0500

    Tk/edm 580 cypress (#34344)
    * edm-580 cypress testing for programs
    
    * remove code to check if pipeline passes
    
    * remove code to check if pipeline passes again
    
    * add code back in
    
    * code fix
    
    * remove code to pass test
    
    * add code to pass test
    
    * update test name
    
    * comment code out for test to pass
    
    * add code back
    
    * cypress test flag update
    
    * temporary remove link click for tests to pass
    
    * cypress test update
    
    * cypress test update again
    
    * cypress test update retest
    
    * cypress test update 2
    
    * cypress test update 3
    
    * cypress test update 4
    
    * cypress test fix
    
    * add commented out code
    
    * feature toggle cypress update
    
    * feature toggle cypress update again
    
    * cypress test update
    
    * cypress test update
    
    * cypress test that passes locally
    
    * console log to review toggle respose
    
    * feature toggle test
    
    * remove code
    
    * edm-580 cypress test update
    
    * remove toggle to see if test passes
    
    * remove toggle to see if test passes
    
    * remove failing test
    
    * use class for link
    
    * cypress that passes locally
    
    * updated link tag
    
    * added timeout for link click
    
    * added mock response
    
    * cypress link update
    
    * cypress link update
    
    * cypress url update
    
    * cypress update
    
    * cypress update again
    
    * cypress reverted to original
    
    * added mock response
    
    * cypress update
    
    * cypress update 2
    
    * cypress update 3
    
    * cypress update
    
    * cypress add more test
    
    * moved mock into a json file
    
    ---------
    
    Co-authored-by: Ray Messina <48040208+rmessina1010@users.noreply.github.com>

--- src/applications/gi/containers/ProgramsList.jsx
+++ src/applications/gi/containers/ProgramsList.jsx
@@ -228,7 +228,11 @@ const ProgramsList = ({ match }) => {
       {/* eslint-disable-next-line jsx-a11y/no-redundant-roles */}
       <ul className="remove-bullets" role="list">
         {currentPrograms.map(program => (
-          <li className="vads-u-margin-bottom--2" key={program.id}>
+          <li
+            className="vads-u-margin-bottom--2"
+            data-testid="program-list-item"
+            key={program.id}
+          >
             {program.attributes.programType === 'OJT'
               ? `${program.attributes.ojtAppType} ${
                   program.attributes.description
