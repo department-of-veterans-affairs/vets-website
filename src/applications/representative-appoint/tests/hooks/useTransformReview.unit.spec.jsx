20a3647c25a5843eb39f51cbf1ff48831724dbc8
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-02-05 10:56:05 -0500

    Refactor with beforeEach

--- src/applications/representative-appoint/tests/hooks/useTransformReview.unit.spec.jsx
+++ src/applications/representative-appoint/tests/hooks/useTransformReview.unit.spec.jsx
@@ -11,61 +11,62 @@ const mockFormStore = state => ({
 });
 
 describe('useTransformForReview', () => {
-  const getProps = () => {
-    return {
-      props: {
-        formData: {
-          'view:selectedRepresentative': {
-            attributes: { fullName: 'John Doe' },
-            type: 'Attorney',
-          },
-          selectedAccreditedOrganizationName: 'Veterans Organization',
-          'view:applicantIsVeteran': 'No',
-          veteranFullName: { first: 'Jane', middle: 'A', last: 'Doe' },
-          veteranSocialSecurityNumber: '123-45-6789',
-          veteranVAFileNumber: '987654321',
-          veteranDateOfBirth: '1990-01-01',
-          serviceNumber: 'S123456789',
-          serviceBranch: 'Army',
-          veteranHomeAddress: {
-            street: '123 Main St',
-            city: 'Anytown',
-            state: 'TX',
-            postalCode: '12345',
-          },
-          primaryPhone: '555-1234',
-          veteranEmail: 'jane.doe@example.com',
-          applicantName: { first: 'John', middle: 'B', last: 'Doe' },
-          applicantDOB: '1990-01-01',
-          claimantRelationship: 'Spouse',
-          homeAddress: {
-            street: '456 Oak St',
-            city: 'Anytown',
-            state: 'TX',
-            postalCode: '54321',
-          },
-          applicantPhone: '555-5678',
-          applicantEmail: 'john.doe@example.com',
-        },
-      },
-      mockStore: mockFormStore({
-        form: {
-          submission: {
-            timestamp: 'mockTimestamp',
-          },
-          data: {},
-        },
-      }),
-    };
-  };
+  let props;
+  let mockStore;
+  let container;
 
-  const renderContainer = (props, mockStore) => {
-    return render(
+  beforeEach(() => {
+    props = {
+      formData: {
+        'view:selectedRepresentative': {
+          attributes: { fullName: 'John Doe' },
+          type: 'Attorney',
+        },
+        selectedAccreditedOrganizationName: 'Veterans Organization',
+        'view:applicantIsVeteran': 'No',
+        veteranFullName: { first: 'Jane', middle: 'A', last: 'Doe' },
+        veteranSocialSecurityNumber: '123-45-6789',
+        veteranVAFileNumber: '987654321',
+        veteranDateOfBirth: '1990-01-01',
+        serviceNumber: 'S123456789',
+        serviceBranch: 'Army',
+        veteranHomeAddress: {
+          street: '123 Main St',
+          city: 'Anytown',
+          state: 'TX',
+          postalCode: '12345',
+        },
+        primaryPhone: '555-1234',
+        veteranEmail: 'jane.doe@example.com',
+        applicantName: { first: 'John', middle: 'B', last: 'Doe' },
+        applicantDOB: '1990-01-01',
+        claimantRelationship: 'Spouse',
+        homeAddress: {
+          street: '456 Oak St',
+          city: 'Anytown',
+          state: 'TX',
+          postalCode: '54321',
+        },
+        applicantPhone: '555-5678',
+        applicantEmail: 'john.doe@example.com',
+      },
+    };
+    mockStore = mockFormStore({
+      form: {
+        submission: {
+          timestamp: 'mockTimestamp',
+        },
+        data: {},
+      },
+    });
+
+    const renderResult = render(
       <Provider store={mockStore}>
         <TestComponent {...props} />
       </Provider>,
     );
-  };
+    container = renderResult.container;
+  });
 
   const TestComponent = ({ formData }) => {
     const transformForReview = useTransformForReview(formData);
@@ -73,32 +74,23 @@ describe('useTransformForReview', () => {
   };
 
   it('should render the representative name', () => {
-    const { props, mockStore } = getProps();
-    const { container } = renderContainer(props, mockStore);
-
     const content = container.querySelector('div');
     expect(content.textContent).to.contain('John');
   });
 
   it('should render the organization name', () => {
-    const { props, mockStore } = getProps();
-    const { container } = renderContainer(props, mockStore);
-
     const content = container.querySelector('div');
     expect(content.textContent).to.contain('Veterans Organization');
   });
 
   context('when applicant is a veteran', () => {
     it('should render the veteran information', () => {
-      const { props, mockStore } = getProps();
       props.formData['view:applicantIsVeteran'] = 'Yes';
       props.formData.applicantName = {
         first: 'Jane',
         middle: 'A',
         last: 'Doe',
       };
-
-      const { container } = renderContainer(props, mockStore);
 
       const content = container.querySelector('div');
       expect(content.textContent).to.contain('Your informationFirst nameJane');
@@ -108,9 +100,6 @@ describe('useTransformForReview', () => {
 
   context('when applicant is not a veteran', () => {
     it('should render claimant information', () => {
-      const { props, mockStore } = getProps();
-      const { container } = renderContainer(props, mockStore);
-
       const content = container.querySelector('div');
       expect(content.textContent).to.contain('Your informationFirst nameJohn');
       expect(content.textContent).to.contain('Spouse');
