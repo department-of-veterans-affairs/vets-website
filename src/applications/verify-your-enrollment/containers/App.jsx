2acebbfca485097c9ae663293b2d6332b772e190
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2025-02-06 17:07:32 -0500

    vey fixing issue with DGIB

--- src/applications/verify-your-enrollment/containers/App.jsx
+++ src/applications/verify-your-enrollment/containers/App.jsx
@@ -3,14 +3,55 @@ import PropTypes from 'prop-types';
 import EnrollmentVerificationPageWrapper from './EnrollmentVerificationPageWrapper';
 
 import BenefitsProfileWrapper from './BenefitsProfilePageWrapper';
+import { useData } from '../hooks/useData';
 
 export default function App({ children }) {
+  const {
+    loading = false,
+    expirationDate = '',
+    updated = '',
+    month = '',
+    day = '',
+    indicator: applicantChapter = '',
+    pendingDocuments = [],
+    latestAddress = '',
+    claimantId = '',
+    fullName = '',
+    indicator = '',
+    enrollmentVerifications = [],
+  } = useData();
   return (
     <>
-      <EnrollmentVerificationPageWrapper>
+      <EnrollmentVerificationPageWrapper
+        data={{
+          expirationDate,
+          updated,
+          month,
+          day,
+          loading,
+          indicator,
+          enrollmentVerifications,
+          claimantId,
+        }}
+      >
         {children}
       </EnrollmentVerificationPageWrapper>
-      <BenefitsProfileWrapper>{children}</BenefitsProfileWrapper>
+      <BenefitsProfileWrapper
+        data={{
+          loading,
+          expirationDate,
+          updated,
+          month,
+          day,
+          indicator: applicantChapter,
+          pendingDocuments,
+          latestAddress,
+          claimantId,
+          fullName,
+        }}
+      >
+        {children}
+      </BenefitsProfileWrapper>
     </>
   );
 }
