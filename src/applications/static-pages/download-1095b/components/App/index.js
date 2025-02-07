fba2fc9b6f1e04f32e97e6e823a740c3f0dda125
Kris Pethtel <kris.pethtel@gmail.com>
2025-02-07 15:23:15 -0600

    1398: update download 1095b form

--- src/applications/static-pages/download-1095b/components/App/index.js
+++ src/applications/static-pages/download-1095b/components/App/index.js
@@ -8,37 +8,28 @@ import { toggleLoginModal as toggleLoginModalAction } from 'platform/site-wide/u
 import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
 import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
 import { CONTACTS } from '@department-of-veterans-affairs/component-library';
-import ServiceProvidersText, {
-  ServiceProvidersTextCreateAcct,
-} from 'platform/user/authentication/components/ServiceProvidersText';
 import {
-  VaRadio,
-  VaRadioOption,
+  VaCard,
+  VaLink,
 } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import recordEvent from '~/platform/monitoring/record-event';
 
 import {
   notFoundComponent,
-  radioOptions,
   unavailableComponent,
   phoneComponent,
-  dateOptions,
   LastUpdatedComponent,
-  formatTimeString,
 } from './utils';
+
+import '../../sass/download-1095b.scss';
 
 export const App = ({ loggedIn, toggleLoginModal, displayToggle }) => {
   const [lastUpdated, updateLastUpdated] = useState('');
   const [year, updateYear] = useState(0);
   const [formError, updateFormError] = useState({ error: false, type: '' }); // types: "not found", "download error"
-  const [formType, updateFormType] = useState('pdf');
-  const [formDownloaded, updateFormDownloaded] = useState({
-    downloaded: false,
-    timeStamp: '',
-  });
 
-  const getContent = () => {
-    return apiRequest(`/form1095_bs/download_${formType}/${year}`)
+  const getContent = format => {
+    return apiRequest(`/form1095_bs/download_${format}/${year}`)
       .then(response => response.blob())
       .then(blob => {
         return window.URL.createObjectURL(blob);
@@ -49,7 +40,7 @@ export const App = ({ loggedIn, toggleLoginModal, displayToggle }) => {
       });
   };
 
-  const getLastUpdatedOn = () => {
+  const fetchAvailableForms = () => {
     return apiRequest('/form1095_bs/available_forms')
       .then(response => {
         if (response.errors || !response.availableForms.length) {
@@ -60,8 +51,8 @@ export const App = ({ loggedIn, toggleLoginModal, displayToggle }) => {
       .catch(() => updateFormError({ error: true, type: 'not found' }));
   };
 
-  const callLastUpdated = () => {
-    getLastUpdatedOn().then(result => {
+  const callAvailableForms = () => {
+    fetchAvailableForms().then(result => {
       if (result.lastUpdated && result.year) {
         const date = new Date(result.lastUpdated);
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -74,80 +65,28 @@ export const App = ({ loggedIn, toggleLoginModal, displayToggle }) => {
     });
   };
 
-  const callGetContent = () => {
-    getContent().then(result => {
+  const callGetContent = format => {
+    getContent(format).then(result => {
       if (result) {
         const a = document.createElement('a');
         a.href = result;
         a.target = '_blank';
 
-        if (formType === 'txt') a.download = `1095B-${year}.txt`; // download text file directly
+        if (format === 'txt') a.download = `1095B-${year}.${format}`; // download text file directly
 
         document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
         a.click();
         a.remove(); // removes element from the DOM
-        const date = new Date();
         updateFormError({ error: false, type: '' });
-        updateFormDownloaded({
-          downloaded: true,
-          timeStamp: formatTimeString(
-            date.toLocaleDateString(undefined, dateOptions),
-          ),
-        });
       }
     });
   };
 
   useEffect(
     () => {
-      callLastUpdated();
+      callAvailableForms();
     },
     [loggedIn],
-  );
-
-  const radioComponent = (
-    <>
-      <h2>Choose your file format and download your document</h2>
-      <VaRadio
-        id="1095-download-options"
-        label="We offer two file format options for this form. Choose the option that best meets your needs."
-        onRadioOptionSelected={e => {
-          e.preventDefault();
-          updateFormType(e.target.value);
-        }}
-      >
-        {radioOptions.map(({ value, label }) => (
-          <VaRadioOption
-            id={value}
-            key={value}
-            checked={value === formType}
-            label={label}
-            value={value}
-            name="1095b-form-select"
-          />
-        ))}
-      </VaRadio>
-    </>
-  );
-
-  const downloadButton = (
-    <p>
-      <button
-        className="usa-button-primary va-button"
-        onClick={function() {
-          recordEvent({
-            event: 'int-radio-button-option-click',
-            'radio-button-label':
-              'Choose your file format and download your document',
-            'radio-button-option-click-label': `${formType} 1095B downloaded`,
-          });
-          callGetContent();
-        }}
-        id="download-url"
-      >
-        Download your 1095-B tax form{' '}
-      </button>
-    </p>
   );
 
   const errorComponent = (
@@ -167,7 +106,6 @@ export const App = ({ loggedIn, toggleLoginModal, displayToggle }) => {
           </p>
         </div>
       </va-alert>
-      {downloadButton}
     </>
   );
 
@@ -178,47 +116,58 @@ export const App = ({ loggedIn, toggleLoginModal, displayToggle }) => {
     return errorComponent;
   };
 
-  const successComponent = (
-    <>
-      <LastUpdatedComponent lastUpdated={lastUpdated} />
-      <va-alert
-        close-btn-aria-label="Close notification"
-        status="success"
-        visible
-      >
-        <h2 slot="headline">Download Complete</h2>
-        <div>
-          <p>
-            You successfully downloaded your 1095-B tax form. Please check your
-            files. &nbsp;
-            {formDownloaded.timeStamp}
-          </p>
+  const downloadForm = (
+    <VaCard>
+      <div>
+        <h4 className="vads-u-margin-bottom--0 vads-u-margin-top--0">
+          1095-B Proof of VA health coverage
+        </h4>
+        <span className="vads-u-font-size--h5">
+          <b>Tax year:</b> {year}
+        </span>
+      </div>
+      <div className="download-break vads-u-font-size--h5">
+        <div className="vads-u-padding-bottom--1">
+          <VaLink
+            download
+            label="Download PDF (best for printing)"
+            text="Download PDF (best for printing)"
+            filetype="PDF"
+            onClick={e => {
+              e.preventDefault();
+              callGetContent('pdf');
+            }}
+          />
         </div>
-      </va-alert>
-      {downloadButton}
-    </>
-  );
-
-  const loggedInComponent = (
-    <>
-      <LastUpdatedComponent lastUpdated={lastUpdated} />
-      {radioComponent}
-      {downloadButton}
-    </>
+        <div className="vads-u-padding-top--1">
+          <VaLink
+            download
+            label="Download Text file (best for screen readers, enlargers, and refreshable Braille displays)"
+            text="Download Text file (best for screen readers, enlargers, and refreshable Braille displays)"
+            filetype="TEXT"
+            onClick={e => {
+              e.preventDefault();
+              callGetContent('txt');
+            }}
+          />
+        </div>
+      </div>
+    </VaCard>
   );
 
   const loggedOutComponent = (
-    <va-alert
-      close-btn-aria-label="Close notification"
-      status="continue"
-      visible
-    >
+    <va-alert close-btn-aria-label="Close notification" status="info" visible >
       <h2 slot="headline">
-        Please sign in to download your 1095-B tax document
+        Sign in with a verified account to download your documents
       </h2>
       <div>
-        Sign in with your existing <ServiceProvidersText isBold /> account.{' '}
-        <ServiceProvidersTextCreateAcct />
+        You'll need to sign in with an identity-verified account through one of our account
+        providers. Identity verification helps us protect all Veterans' information and prevent
+        scammers from stealing your benefits.
+        <b>Don't yet have an account?</b> Create a <b>Login.gov</b> or <b>ID.me</b> account. We'll help you
+        verify your account now.
+        <b>Not sure if your account is verified?</b> Sign in here. If you still need to verify your identity,
+        we'll help you do that now.
       </div>
       <va-button
         onClick={() => toggleLoginModal(true)}
@@ -236,10 +185,7 @@ export const App = ({ loggedIn, toggleLoginModal, displayToggle }) => {
     if (formError.error) {
       return getErrorComponent();
     }
-    if (formDownloaded.downloaded) {
-      return successComponent;
-    }
-    return loggedInComponent;
+    return downloadForm;
   }
   return loggedOutComponent;
 };
@@ -251,8 +197,8 @@ App.propTypes = {
 };
 
 const mapStateToProps = state => ({
-  loggedIn: state?.user?.login?.currentlyLoggedIn || null,
-  displayToggle: toggleValues(state)[FEATURE_FLAG_NAMES.showDigitalForm1095b],
+  loggedIn: true,
+  displayToggle: true,
 });
 
 const mapDispatchToProps = dispatch => ({
