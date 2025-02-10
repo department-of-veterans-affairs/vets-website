2b1c018f0ce9c7a9c5e910ec8e60f456b5426577
Alison Jones <1816769+ajones446@users.noreply.github.com>
2025-02-10 17:48:42 -0600

    WIP: summarizing

--- src/applications/disability-benefits/all-claims/pages/form0781/behaviorSummaryPage.js
+++ src/applications/disability-benefits/all-claims/pages/form0781/behaviorSummaryPage.js
@@ -3,14 +3,16 @@ import {
   form0781HeadingTag,
   mentalHealthSupportAlert,
 } from '../../content/form0781';
-import { behaviorSummaryPageTitle } from '../../content/form0781/behaviorListPages';
+import { behaviorSummaryPageTitle, summarizeBehaviors } from '../../content/form0781/behaviorListPages';
 
 export const uiSchema = {
   'ui:title': titleWithTag(behaviorSummaryPageTitle, form0781HeadingTag),
+  'ui:description': ({ formData }) => summarizeBehaviors(formData),
   'view:mentalHealthSupportAlert': {
     'ui:description': mentalHealthSupportAlert,
   },
 };
+
 
 export const schema = {
   type: 'object',
