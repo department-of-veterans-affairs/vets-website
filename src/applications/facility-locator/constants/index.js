3f432e001255f274b0ad304db34d6e6195b2fdd5
Bryan Thompson <18094023+SnowboardTechie@users.noreply.github.com>
2025-02-10 13:52:05 -0800

    adjust burial status constant to lorem ipsum for testing while awaiting final wording

--- src/applications/facility-locator/constants/index.js
+++ src/applications/facility-locator/constants/index.js
@@ -118,7 +118,8 @@ export const BurialStatusDisplay = {
   },
   BURIALS_CLOSED: {
     statusTitle: 'Closed',
-    statusDescription: 'Call for burial status',
+    statusDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   BURIALS_UNDER_CONSTRUCTION: {
     statusTitle: 'Under construction',
