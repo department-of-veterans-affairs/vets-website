8308312f48d9b78cb6f5197a9cc64bec479816e9
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-02-11 14:54:55 -0500

    toggled the max grouped list count with a feature flag.

--- src/applications/mhv-medications/util/selectors.js
+++ src/applications/mhv-medications/util/selectors.js
@@ -11,3 +11,8 @@ export const selectFilterFlag = state =>
 
 export const selectGroupingFlag = state =>
   state.featureToggles[FEATURE_FLAG_NAMES.mhvMedicationsDisplayGrouping];
+
+export const selectGroupingListCountFlag = state =>
+  state.featureToggles[
+    FEATURE_FLAG_NAMES.mhvMedicationsGroupingListPaginationCount
+  ];
