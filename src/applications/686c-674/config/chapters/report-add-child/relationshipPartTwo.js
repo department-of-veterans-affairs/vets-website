d782372309cd1308447da135a5eeb76b4a0093f8
Dakota Larson <dakota.larson@oddball.io>
2025-02-11 18:31:37 -0800

    conditional disability page & evidence updates

--- src/applications/686c-674/config/chapters/report-add-child/relationshipPartTwo.js
+++ src/applications/686c-674/config/chapters/report-add-child/relationshipPartTwo.js
@@ -44,7 +44,7 @@ const StepchildAdditionalEvidence = () => {
   );
 };
 
-export const relationship = {
+export const relationshipPartTwo = {
   uiSchema: {
     ...titleUI({
       title: 'Your relationship to this child',
@@ -54,7 +54,6 @@ export const relationship = {
       'ui:description': 'Check all that apply',
       'ui:webComponentField': VaCheckboxField,
       labels: {
-        biological: 'They’re my biological child',
         adopted: 'They’re my adopted child',
         stepchild: 'They’re my stepchild',
       },
@@ -109,11 +108,7 @@ export const relationship = {
   schema: {
     type: 'object',
     properties: {
-      relationshipToChild: checkboxGroupSchema([
-        'biological',
-        'adopted',
-        'stepchild',
-      ]),
+      relationshipToChild: checkboxGroupSchema(['adopted', 'stepchild']),
       'view:commonEvidenceInfo': {
         type: 'object',
         properties: {},
