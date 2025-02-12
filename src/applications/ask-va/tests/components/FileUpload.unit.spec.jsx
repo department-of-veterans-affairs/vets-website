14d74fbd0ad8ada31b57aa72bd81bd259ff462e1
ediiotero <e.otero81@gmail.com>
2025-02-11 20:56:28 -0500

    fix broken test and update attachment links for reply page

--- src/applications/ask-va/tests/components/FileUpload.unit.spec.jsx
+++ src/applications/ask-va/tests/components/FileUpload.unit.spec.jsx
@@ -2,11 +2,31 @@ import { fireEvent, waitFor } from '@testing-library/dom';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import FileUpload from '../../components/FileUpload';
 
+const mockStore = configureStore([]);
+
 describe('<FileUpload />', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      navigation: {
+        route: {
+          path: '/test-path',
+        },
+      },
+    });
+  });
+
   it('allows the user to add a file', async () => {
-    const screen = render(<FileUpload />);
+    const screen = render(
+      <Provider store={store}>
+        <FileUpload />
+      </Provider>,
+    );
 
     const file = new File(['hello'], 'hello.png', { type: 'image/png' });
     const input = screen.getByTestId('askVA_upload_first');
