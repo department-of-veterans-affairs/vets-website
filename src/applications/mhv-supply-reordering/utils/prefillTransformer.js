29fcbb7130e6794dc0eaa8bb6a003a507fc8b162
Ni Chia <ni.chia@adhocteam.us>
2025-02-12 16:43:27 -0800

    Supply Reordering: Add specs to meet test coverage requirements (#34638)
    * add test coverage
    
    * add test coverage
    
    * increase test coverage

--- src/applications/mhv-supply-reordering/utils/prefillTransformer.js
+++ src/applications/mhv-supply-reordering/utils/prefillTransformer.js
@@ -1,11 +1,12 @@
-import {
-  countryNameToValue,
-  isMilitaryState,
-  isTerritory,
-} from '../utils/addresses';
+import { countryNameToValue, isMilitaryState, isTerritory } from './addresses';
 
 const prefillTransformer = (pages, formData, metadata) => {
   const newFormData = formData;
+
+  // prefill permanentAddress if empty
+  if (Object.keys(formData?.permanentAddress || {}).length === 0) {
+    newFormData.permanentAddress = { ...formData.temporaryAddress };
+  }
 
   if (newFormData.permanentAddress?.country) {
     newFormData.permanentAddress.country =
