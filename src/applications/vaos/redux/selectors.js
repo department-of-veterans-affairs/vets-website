c5e2d1707a7077ddd21137c0c0b7ec490a7c5c22
John Luo <john.luo@agile6.com>
2025-02-13 10:39:51 -0500

    Add feature flag for VADate/VaDateField adoption (#34664)

--- src/applications/vaos/redux/selectors.js
+++ src/applications/vaos/redux/selectors.js
@@ -114,6 +114,9 @@ export const selectFeatureOHRequest = state =>
 export const selectFeatureRemovePodiatry = state =>
   toggleValues(state).vaOnlineSchedulingRemovePodiatry;
 
+export const selectFeatureUseVaDate = state =>
+  toggleValues(state).vaOnlineSchedulingUseVaDate;
+
 export const selectFeatureTravelPayViewClaimDetails = state =>
   toggleValues(state).travelPayViewClaimDetails;
 
