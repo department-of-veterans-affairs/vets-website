1b6be41f8a9d868987dae0896c1902b24495cea6
Alison Jones <1816769+ajones446@users.noreply.github.com>
2025-02-13 13:24:42 -0600

    Update behavior description page spec

--- src/applications/disability-benefits/all-claims/tests/pages/form0781/behaviorDescriptionPages.unit.spec.js
+++ src/applications/disability-benefits/all-claims/tests/pages/form0781/behaviorDescriptionPages.unit.spec.js
@@ -11,13 +11,13 @@ import * as reassignmentDescriptionPage from '../../../pages/form0781/behaviorCh
 
 import * as unlistedDescriptionPage from '../../../pages/form0781/behaviorChangeDescriptions/unlistedDescriptionPage';
 
-import {
-  behaviorDescriptionPageDescription,
-  unlistedDescriptionPageDescription,
-} from '../../../content/form0781/behaviorListPages';
+import * as behaviorListPages from '../../../content/form0781/behaviorListPages';
 
 describe('Behavioral description pages', () => {
-  const allBehaviorDescriptionPages = [reassignmentDescriptionPage];
+  const allBehaviorDescriptionPages = [
+    reassignmentDescriptionPage,
+    unlistedDescriptionPage,
+  ];
 
   allBehaviorDescriptionPages.forEach(page => {
     const { schema, uiSchema } = page;
@@ -46,9 +46,17 @@ describe('Behavioral description pages', () => {
       const textarea = $('va-textarea', container);
       // Not required
       expect(textarea.getAttribute('required')).to.eq('false');
-      expect(textarea.getAttribute('label')).to.eq(
-        behaviorDescriptionPageDescription,
-      );
+
+      // unlisted page has its own description, every other page uses the same description
+      if (!uiSchema.behaviorsDetails.unlisted) {
+        expect(textarea.getAttribute('label')).to.eq(
+          behaviorListPages.behaviorDescriptionPageDescription,
+        );
+      } else {
+        expect(textarea.getAttribute('label')).to.eq(
+          behaviorListPages.unlistedDescriptionPageDescription,
+        );
+      }
     });
 
     it('should submit without entering any text', () => {
@@ -87,75 +95,5 @@ describe('Behavioral description pages', () => {
       userEvent.click(getByText('Submit'));
       expect(onSubmit.called).to.be.true;
     });
-  });
-});
-
-describe('Unlisted behavioral description page', () => {
-  const { schema, uiSchema } = unlistedDescriptionPage;
-
-  it('should define a uiSchema object', () => {
-    expect(uiSchema).to.be.an('object');
-  });
-
-  it('should define a schema object', () => {
-    expect(schema).to.be.an('object');
-  });
-
-  it('Displays a text area', () => {
-    const onSubmit = Sinon.spy();
-    const { container } = render(
-      <DefinitionTester
-        schema={schema}
-        uiSchema={uiSchema}
-        data={{}}
-        definitions={{}}
-        formData={{}}
-        onSubmit={onSubmit}
-      />,
-    );
-
-    const textarea = $('va-textarea', container);
-    // Not required
-    expect(textarea.getAttribute('required')).to.eq('false');
-    expect(textarea.getAttribute('label')).to.eq(
-      unlistedDescriptionPageDescription,
-    );
-  });
-
-  it('should submit without entering any text', () => {
-    const onSubmit = Sinon.spy();
-
-    const { getByText } = render(
-      <DefinitionTester
-        schema={schema}
-        uiSchema={uiSchema}
-        data={{}}
-        onSubmit={onSubmit}
-      />,
-    );
-    userEvent.click(getByText('Submit'));
-    expect(onSubmit.calledOnce).to.be.true;
-  });
-
-  it('should submit if text entered', () => {
-    const onSubmit = Sinon.spy();
-
-    const { container, getByText } = render(
-      <DefinitionTester
-        schema={schema}
-        uiSchema={uiSchema}
-        data={{}}
-        onSubmit={onSubmit}
-      />,
-    );
-
-    inputVaTextInput(
-      container,
-      'Here is a description of a behavioral change',
-      'va-textarea',
-    );
-
-    userEvent.click(getByText('Submit'));
-    expect(onSubmit.called).to.be.true;
   });
 });
