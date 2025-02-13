8d9c61f1f1037097d712b01b547acaec3eed837f
jordanBoardmanCodes <155088633+jordanBoardmanCodes@users.noreply.github.com>
2025-02-13 10:34:51 -0500

    MBMS-82681 more screen 2 work

--- src/applications/medallions/pages/applicantRelationToVet.js
+++ src/applications/medallions/pages/applicantRelationToVet.js
@@ -1,26 +1,42 @@
 import {
-  addressSchema,
-  addressUI,
-  titleUI,
+  radioUI,
+  radioSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
 
 /** @type {PageSchema} */
 export default {
   uiSchema: {
-    ...titleUI(
-      'Mailing address',
-      'We’ll send any important information about your application to this address.',
-    ),
-    address: addressUI({
-      omit: ['street3'],
+    // ...titleUI(
+    //   'Mailing address',
+    //   'We’ll send any important information about your application to this address.',
+    // ),
+    relationToVetRadio: radioUI({
+      title: 'What’s your relationship to the Veteran?',
+      labels: {
+        familyMember: 'Family member',
+        personalRep: 'Personal representative',
+        repOfVSO: 'Representative of Veterans Service Organization (VSO)',
+        repOfCemetary: 'Representative of a cemetery',
+        repOfFuneralHome: 'Representative of a funeral home',
+        other: 'Other',
+      },
+      required: () => true,
+      errorMessages: {
+        required: 'Please select an animal',
+      },
     }),
   },
   schema: {
     type: 'object',
     properties: {
-      address: addressSchema({
-        omit: ['street3'],
-      }),
+      relationToVetRadio: radioSchema([
+        'familyMember',
+        'personalRep',
+        'repOfVSO',
+        'repOfCemetary',
+        'repOfFuneralHome',
+        'other',
+      ]),
     },
   },
 };
