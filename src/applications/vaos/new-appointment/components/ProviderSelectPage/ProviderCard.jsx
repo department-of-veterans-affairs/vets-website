e9adcb9713d3d282d81d79817e2b682ddecf18a7
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2025-02-14 08:37:49 -0800

    Adds no available slots logic to OH provider select page (#34694)
    * Destructures args
    
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
    
    * Fixes transformer
    
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
    
    * Adds conditional display of no time slots availability
    
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
    
    * Adds unit tests
    
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>

--- src/applications/vaos/new-appointment/components/ProviderSelectPage/ProviderCard.jsx
+++ src/applications/vaos/new-appointment/components/ProviderSelectPage/ProviderCard.jsx
@@ -3,16 +3,34 @@ import PropTypes from 'prop-types';
 import { format } from 'date-fns';
 
 export default function ProviderCard({ provider }) {
+  const { lastSeen, providerName, hasAvailability } = provider;
   return (
     <div>
       <h2 className="vads-u-font-size--h3 vads-u-margin-bottom--0 vads-u-margin-top--2">
-        {provider.providerName}
+        {providerName}
       </h2>
       <p className="vads-u-margin-top--0 vads-u-margin-bottom--1">
-        Your last appointment was on{' '}
-        {format(new Date(provider.lastSeen), 'M/d/yyyy')}
+        Your last appointment was on {format(new Date(lastSeen), 'M/d/yyyy')}
       </p>
-      <va-link active href="#" text="Choose your preferred date and time" />
+
+      {!hasAvailability && (
+        <va-additional-info
+          data-testid="no-appointments-available"
+          trigger="Why you can't schedule online with this provider"
+        >
+          This provider has no appointments available for online scheduling.
+        </va-additional-info>
+      )}
+
+      {hasAvailability && (
+        <va-link
+          active
+          data-testid="choose-date-time"
+          href="#"
+          text="Choose your preferred date and time"
+        />
+      )}
+
       <hr
         aria-hidden="true"
         className="vads-u-margin-bottom--2 vads-u-margin-top--2"
--- src/applications/vaos/new-appointment/components/ProviderSelectPage/index.unit.spec.js
+++ src/applications/vaos/new-appointment/components/ProviderSelectPage/index.unit.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mockFetch } from '@department-of-veterans-affairs/platform-testing/helpers';
+import { waitFor } from '@testing-library/dom';
 import SelectProviderPage from './index';
 import {
   createTestStore,
@@ -38,6 +39,7 @@ const defaultState = {
         clinicName: 'Zanesville Primary Care',
         vistaId: '534',
         lastSeen: '2024-11-26T00:32:34.216Z',
+        hasAvailability: true,
       },
       {
         resourceType: 'PatientProviderRelationship',
@@ -48,6 +50,7 @@ const defaultState = {
         clinicName: 'Zanesville Primary Care',
         vistaId: '534',
         lastSeen: '2024-10-15T00:32:34.216Z',
+        hasAvailability: true,
       },
     ],
   },
@@ -56,6 +59,48 @@ const defaultState = {
 describe('VAOS Page: ProviderSelectPage', () => {
   beforeEach(() => {
     mockFetch();
+  });
+
+  describe('when a provider has no availability', () => {
+    const store = createTestStore({
+      ...defaultState,
+      newAppointment: {
+        ...defaultState.newAppointment,
+        patientProviderRelationships: [
+          {
+            resourceType: 'PatientProviderRelationship',
+            providerName: 'Doe, John D, MD',
+            providerId: 'Practitioner/123456',
+            serviceType: 'Routine Follow-up',
+            locationName: 'Marion VA Clinic',
+            clinicName: 'Zanesville Primary Care',
+            vistaId: '534',
+            lastSeen: '2024-11-26T00:32:34.216Z',
+            hasAvailability: false,
+          },
+        ],
+      },
+    });
+
+    it('should not display the schedule link', async () => {
+      const screen = renderWithStoreAndRouter(<SelectProviderPage />, {
+        store,
+      });
+
+      await waitFor(() => {
+        expect(screen.queryByTestId('choose-date-time')).to.not.exist;
+      });
+    });
+
+    it('should display additional info', async () => {
+      const screen = renderWithStoreAndRouter(<SelectProviderPage />, {
+        store,
+      });
+
+      await waitFor(() => {
+        expect(screen.queryByTestId('no-appointments-available')).to.exist;
+      });
+    });
   });
 
   describe('when there is a single provider', () => {
@@ -74,6 +119,7 @@ describe('VAOS Page: ProviderSelectPage', () => {
               clinicName: 'Zanesville Primary Care',
               vistaId: '534',
               lastSeen: '2024-11-26T00:32:34.216Z',
+              hasAvailability: true,
             },
           ],
         },
