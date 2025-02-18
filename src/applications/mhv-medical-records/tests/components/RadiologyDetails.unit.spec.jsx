5b648bba7484e2e335074ed868f33aebace01a1a
tynyttie <ty_barnes_jobs@yahoo.com>
2025-02-18 09:47:47 -0500

    Removed datadog component from spec

--- src/applications/mhv-medical-records/tests/components/RadiologyDetails.unit.spec.jsx
+++ src/applications/mhv-medical-records/tests/components/RadiologyDetails.unit.spec.jsx
@@ -1,6 +1,5 @@
 import { expect } from 'chai';
 import React from 'react';
-import sinon from 'sinon';
 import { renderWithStoreAndRouter } from '@department-of-veterans-affairs/platform-testing/react-testing-library-helpers';
 import { beforeEach, it } from 'mocha';
 import { fireEvent, waitFor } from '@testing-library/dom';
@@ -12,7 +11,6 @@ import radiologyMhvWithImagesNew from '../fixtures/radiologyMhvWithImagesNew.jso
 import radiologyMhvWithImageError from '../fixtures/radiologyMhvWithImageError.json';
 import images from '../fixtures/images.json';
 import radiologyWithMissingFields from '../fixtures/radiologyWithMissingFields.json';
-import * as helpers from '../../util/helpers';
 import {
   convertCvixRadiologyRecord,
   convertMhvRadiologyRecord,
@@ -330,16 +328,6 @@ describe('Radiology details component', () => {
   it('should display a download started message when the download txt file button is clicked', () => {
     fireEvent.click(screen.getByTestId('printButton-2'));
     expect(screen.getByTestId('download-success-alert-message')).to.exist;
-  });
-
-  it('should show a request images button', async () => {
-    const sendDataDogActionStub = sinon.stub(helpers, 'sendDataDogAction');
-
-    fireEvent.click(screen.getByTestId('radiology-images-link'));
-    await waitFor(() => {
-      expect(sendDataDogActionStub.calledOnce).to.be.true;
-      sendDataDogActionStub.restore();
-    });
   });
 });
 
