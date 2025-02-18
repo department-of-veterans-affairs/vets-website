7ba6dfc1bc4f135254f241c7d09a4d96581ead73
fatmakhan0395 <145523589+fatmakhan0395@users.noreply.github.com>
2025-02-18 13:35:25 -0800

    updating past date check on calc date (#34729)

--- src/applications/edu-benefits/10216/utilities.js
+++ src/applications/edu-benefits/10216/utilities.js
@@ -30,7 +30,9 @@ export const calculatedPercentage = formData => {
 export const isDateThirtyDaysOld = (dateOfCalculation, termStartDate) => {
   const dateOfCalculationObj = new Date(dateOfCalculation);
   const termStartDateObj = new Date(termStartDate);
-  const diffTime = dateOfCalculationObj.getTime() - termStartDateObj.getTime();
+  const diffTime = Math.abs(
+    termStartDateObj.getTime() - dateOfCalculationObj.getTime(),
+  );
   const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
   return diffDays >= 30;
 };
