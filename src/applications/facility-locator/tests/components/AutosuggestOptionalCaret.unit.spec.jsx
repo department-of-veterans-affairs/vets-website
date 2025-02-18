ea8d5ea61805d80f801b600925c022ffc634d574
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2025-02-18 08:08:36 -0800

    VACMS 20241 - autosuggest/typeahead/combobox address for facility locator (#34361)
    Autosuggest component used by AddressAutosuggest in Facility Locator behind a flipper. Adds unit and e2e tests.

---
+++ src/applications/facility-locator/tests/components/AutosuggestOptionalCaret.unit.spec.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { expect } from 'chai';
+import Autosuggest from '../../components/Autosuggest';
+
+describe('<Autosuggest inputId="any">', () => {
+  it('Autosuggest should render with input with down caret.', () => {
+    const screen = render(
+      <Autosuggest inputId="any" inputValue="" options={[]} showDownCaret />,
+    );
+    expect(screen.getByTestId('down-caret')).to.be.displayed;
+  });
+
+  it('Autosuggest should render with input without down caret.', () => {
+    const screen = render(
+      <Autosuggest
+        inputId="any"
+        inputValue="any"
+        options={[]}
+        showDownCaret={false}
+      />,
+    );
+    // no down-caret test id
+    expect(screen.queryAllByTestId('down-caret')).to.be.have.lengthOf(0);
+  });
+});
