636f8a595c77efb0bcde9f6881065612a2c83ca7
Vic Saleem <59583325+vsaleem@users.noreply.github.com>
2025-02-18 11:59:57 -0600

    cleanup

--- src/applications/mhv-secure-messaging/components/Footer.jsx
+++ src/applications/mhv-secure-messaging/components/Footer.jsx
@@ -23,33 +23,32 @@ const Footer = () => {
   );
 
   const folderId = useSelector(state => state.sm.folders.folder.folderId);
-  return (
-    removeLandingPageFF &&
-    folderId === INBOX.id && (
-      <footer
-        className="vads-u-padding-top--3 vads-u-padding-bottom--3"
-        data-testid="inbox-footer"
-      >
-        <div className="vads-l-grid-container">
-          <div className="vads-l-row">
-            <div className="vads-l-col--12 medium-screen:vads-l-col--12">
-              <p className="vads-u-margin--0 vads-u-font-size--lg vads-u-font-family--serif vads-u-font-weight--bold">
-                {NEED_HELP}
-              </p>
-              <HorizontalRule />
-              <p className="vads-u-margin--0">{HAVE_QUESTIONS}</p>
-              <p className="vads-u-margin-top--2">
-                <a href="/health-care/secure-messaging">{LEARN_MORE}</a>
-              </p>
-              <p className="vads-u-margin-top--1">{CONTACT_FACILITY}</p>
-              <p className="vads-u-margin-top--1">
-                <a href="/find-locations">{FIND_FACILITY}</a>
-              </p>
-            </div>
+  return removeLandingPageFF && folderId === INBOX.id ? (
+    <footer
+      className="vads-u-padding-top--3 vads-u-padding-bottom--3"
+      data-testid="inbox-footer"
+    >
+      <div className="vads-l-grid-container">
+        <div className="vads-l-row">
+          <div className="vads-l-col--12 medium-screen:vads-l-col--12">
+            <p className="vads-u-margin--0 vads-u-font-size--lg vads-u-font-family--serif vads-u-font-weight--bold">
+              {NEED_HELP}
+            </p>
+            <HorizontalRule />
+            <p className="vads-u-margin--0">{HAVE_QUESTIONS}</p>
+            <p className="vads-u-margin-top--2">
+              <a href="/health-care/secure-messaging">{LEARN_MORE}</a>
+            </p>
+            <p className="vads-u-margin-top--1">{CONTACT_FACILITY}</p>
+            <p className="vads-u-margin-top--1">
+              <a href="/find-locations">{FIND_FACILITY}</a>
+            </p>
           </div>
         </div>
-      </footer>
-    )
+      </div>
+    </footer>
+  ) : (
+    <></>
   );
 };
 
