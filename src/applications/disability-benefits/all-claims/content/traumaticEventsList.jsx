752bc7cfaf503b01adfbea6917f506c2cad4971d
Scott Regenthal <scottregenthal@gmail.com>
2025-02-18 12:08:44 -0700

    [DBEX] Update events description and max alert content

--- src/applications/disability-benefits/all-claims/content/traumaticEventsList.jsx
+++ src/applications/disability-benefits/all-claims/content/traumaticEventsList.jsx
@@ -12,12 +12,50 @@ export const eventsListDescription = () => {
       </p>
       <p>
         You can tell us about a single event, or a recurring or ongoing
-        experience.
+        experience.{' '}
+        <strong>
+          You can add up to 20 events to your disability application.
+        </strong>
       </p>
       <p>
         It’s also OK if you don’t add an event. You can skip this question if
         you don’t feel comfortable answering. Select <strong>Continue</strong>{' '}
         to go to the next question.
+      </p>
+    </>
+  );
+};
+
+export const maxEventsAlert = () => {
+  return (
+    <>
+      <p>You’ve added the maximum number of traumatic events for this claim.</p>
+      <ul>
+        <li>
+          To revise your traumatic events you may edit or delete an event before
+          continuing.
+        </li>
+        <li>
+          You may also print this form, fill it out, and mail it to us.{' '}
+          <va-link
+            href="https://www.va.gov/find-forms/about-form-21-0781/"
+            text="Get VA Form 21-0781"
+          />
+        </li>
+      </ul>
+      <p>
+        <strong>
+          Send your completed form and supporting documents to this address:
+        </strong>
+      </p>
+      <p>
+        Department of Veterans Affairs
+        <br />
+        Evidence Intake Center
+        <br />
+        P.O. Box 4444
+        <br />
+        Janesville, WI 53547-4444
       </p>
     </>
   );
