2ae301e8d8f87bbbb34fe6824eb74458e161909a
Jane Cho <87551323+janechodance@users.noreply.github.com>
2025-02-19 14:16:37 -0500

    edit mailing address page

---
+++ src/applications/letters/containers/EditAddress.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { EditAddress } from '../../_mock-form-ae-design-patterns/patterns/pattern2/TaskBlue/EditContactInfo';
+
+export function TestEditAddress() {
+  return (
+    <div className="usa-width-three-fourths letters vads-u-margin-top--2 ">
+      <h2>Edit mailing address</h2>
+      <EditAddress />
+    </div>
+  );
+}
+
+export default EditAddress;
