1f9dbb02cacb122bcd6ec530580704d0d9fda630
Richard Davis <richard.davis@agile6.com>
2025-02-20 12:05:33 -0500

    Targets h1 or testid for PageNotFound component in App specs (#34754)

--- src/applications/mhv-secure-messaging/tests/containers/App.unit.spec.jsx
+++ src/applications/mhv-secure-messaging/tests/containers/App.unit.spec.jsx
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 import { renderWithStoreAndRouter } from '@department-of-veterans-affairs/platform-testing/react-testing-library-helpers';
 import backendServices from '@department-of-veterans-affairs/platform-user/profile/backendServices';
 import { createServiceMap } from '@department-of-veterans-affairs/platform-monitoring';
+import { pageNotFoundHeading } from '@department-of-veterans-affairs/platform-site-wide/PageNotFound';
 import sinon from 'sinon';
 import { addDays, subDays, format } from 'date-fns';
 import { waitFor } from '@testing-library/dom';
@@ -303,7 +304,7 @@ describe('App', () => {
       path: `/sdfsdf`,
     });
     expect(
-      screen.getByText('Sorry — we can’t find that page', {
+      screen.getByText(pageNotFoundHeading, {
         selector: 'h1',
         exact: true,
       }),
