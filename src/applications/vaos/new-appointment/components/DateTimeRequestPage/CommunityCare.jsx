360b424116218216bd0ec5ad27a4f8526ba7f147
Jun Tao Luo <john.luo@agile6.com>
2025-02-21 11:12:40 -0500

    Fixup required label

--- src/applications/vaos/new-appointment/components/DateTimeRequestPage/CommunityCare.jsx
+++ src/applications/vaos/new-appointment/components/DateTimeRequestPage/CommunityCare.jsx
@@ -77,7 +77,12 @@ export default function CCRequest() {
 
   return (
     <div className="vaos-form__detailed-radio">
-      <h1 className="vaos__dynamic-font-size--h2">{pageTitle}</h1>
+      <h1 className="vaos__dynamic-font-size--h2">
+        {pageTitle}
+        <span className="schemaform-required-span vaos-calendar__page_header vads-u-font-family--sans vads-u-font-weight--normal">
+          (*Required)
+        </span>
+      </h1>
       <p>You can select up to 3 preferred timeframes.</p>
       <p>
         <strong>Note:</strong> We’ll contact you to finish scheduling the
