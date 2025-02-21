e5fbdce07877fa0a2cbb388dce80f64d9b0b6f6e
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-02-21 16:06:50 -0600

    93065 Update static page widget content - form 10-7959c (#34785)
    * updated widget content
    
    * adding timeout extension per 50da60323c8c1529246fdbf2803902f1b5bff297

--- src/platform/testing/e2e/cypress/support/form-tester/index.js
+++ src/platform/testing/e2e/cypress/support/form-tester/index.js
@@ -228,7 +228,7 @@ const defaultPostHook = pathname => {
   return () => {
     cy.findByText(/continue/i, {
       selector: 'button',
-      timeout: 1000,
+      timeout: 10000,
     })
       .should('be.visible')
       .click(FORCE_OPTION);
