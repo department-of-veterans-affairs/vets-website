e82d466e948b787e4e1c609662b591cc03f4691e
Kevin Suarez <kjsuarez94@gmail.com>
2025-02-21 16:27:47 -0500

    102993 misunderstood how mapStateToProps works

--- src/applications/static-pages/health-care-manage-benefits/modern-secure-messaging-page/components/App/index.js
+++ src/applications/static-pages/health-care-manage-benefits/modern-secure-messaging-page/components/App/index.js
@@ -9,15 +9,15 @@ import {
 // Relative imports.
 import { MhvSimpleSigninCallToAction } from 'applications/static-pages/mhv-simple-signin-cta';
 
-export const App = (userIsLoggedIn, userIsVerified) => {
+export const App = state => {
   return (
     <MhvSimpleSigninCallToAction
       headingLevel="3"
       serviceDescription="send secure messages"
       linkText="Go to your messages"
       linkUrl="/my-health/secure-messages/inbox"
-      userIsLoggedIn={userIsLoggedIn}
-      userIsVerified={userIsVerified}
+      userIsLoggedIn={state.userIsLoggedIn}
+      userIsVerified={state.userIsVerified}
     />
   );
 };
