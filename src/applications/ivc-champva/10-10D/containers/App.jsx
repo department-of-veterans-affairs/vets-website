696817dc0ee42f2b5c478d6dd6553f265538889c
Kyle Brost <170366505+brostk@users.noreply.github.com>
2025-02-24 12:47:46 -0500

    remove 1010d feature toggle (#34758)
    remove 1010d toggle

--- src/applications/ivc-champva/10-10D/containers/App.jsx
+++ src/applications/ivc-champva/10-10D/containers/App.jsx
@@ -6,9 +6,7 @@ import {
 import { VaBreadcrumbs } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import PropTypes from 'prop-types';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-import { Toggler } from 'platform/utilities/feature-toggles';
 import formConfig from '../config/form';
-import WIP from '../../shared/components/WIP';
 import { useBrowserMonitoring } from '../helpers/useBrowserMonitoring';
 import { addStyleToShadowDomOnPages } from '../../shared/utilities';
 
@@ -49,32 +47,15 @@ export default function App({ location, children }) {
 
   return (
     <div className="vads-l-grid-container desktop-lg:vads-u-padding-x--0">
-      <Toggler toggleName={Toggler.TOGGLE_NAMES.form1010d}>
-        <Toggler.Enabled>
-          <VaBreadcrumbs wrapping breadcrumbList={breadcrumbList} />
-          <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-            <DowntimeNotification
-              appTitle={`CHAMPVA Form ${formConfig.formId}`}
-              dependencies={[externalServices.pega, externalServices.form1010d]}
-            >
-              {children}
-            </DowntimeNotification>
-          </RoutedSavableApp>
-        </Toggler.Enabled>
-        <Toggler.Disabled>
-          <br />
-          <WIP
-            content={{
-              headline: 'We’re still working on this form',
-              description:
-                'We’re working on a new online application form for CHAMPVA benefits. Check back soon. If you want to apply for CHAMPVA benefits now, you can use our PDF form.',
-              redirectLink:
-                '/family-and-caregiver-benefits/health-and-disability/champva/',
-              redirectText: 'Learn about CHAMPVA benefits and how to apply',
-            }}
-          />
-        </Toggler.Disabled>
-      </Toggler>
+      <VaBreadcrumbs wrapping breadcrumbList={breadcrumbList} />
+      <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+        <DowntimeNotification
+          appTitle={`CHAMPVA Form ${formConfig.formId}`}
+          dependencies={[externalServices.pega]}
+        >
+          {children}
+        </DowntimeNotification>
+      </RoutedSavableApp>
     </div>
   );
 }
