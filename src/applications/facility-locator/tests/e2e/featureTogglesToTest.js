313e0b33db4190bfb9d8b066a6251b5b73ef29cc
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2025-02-24 13:33:02 -0800

    documentation of featureTogglesToTest

--- src/applications/facility-locator/tests/e2e/featureTogglesToTest.js
+++ src/applications/facility-locator/tests/e2e/featureTogglesToTest.js
@@ -6,7 +6,14 @@ function arrayChooseK(arr, k, state = []) {
   );
 }
 
-function createObjects(togglesCombinations, toggleNames) {
+/**
+ * creates objects from combination array
+ * @param {*} togglesCombinations combination array of strings (flipper names)
+ * @param {*} toggleNames original list of flipper names (strings)
+ * @returns - array of objects with name and value - where true for flipper present in combination
+ *  and false for not present in the combinations
+ */
+function createFlipperFeatureSet(togglesCombinations, toggleNames) {
   return togglesCombinations.map(toggles => {
     const toggleSet = [];
     toggleNames.forEach(name => {
@@ -19,7 +26,12 @@ function createObjects(togglesCombinations, toggleNames) {
     return toggleSet;
   });
 }
-
+/**
+ * Creates all possible flipper combinations in feature sets to see if there are interactions
+ * @param {string[]} toggleNames array of strings (flipper names)
+ * @returns 2d array of all possible flipper combinations in feature sets
+ * (to be returned from mocked features endpoint)
+ */
 export function featureCombinationsTogglesToTest(toggleNames = []) {
   if (toggleNames.length === 0) {
     return [];
@@ -29,15 +41,21 @@ export function featureCombinationsTogglesToTest(toggleNames = []) {
     const toggles = arrayChooseK(toggleNames, i);
     togglesCombinations.push(...toggles);
   }
-  return createObjects(togglesCombinations, toggleNames);
+  return createFlipperFeatureSet(togglesCombinations, toggleNames);
 }
 
+// Used as: featureSet.some(isFeatureEnabled('featureName'))
+// to check if feature is enabled in the currently tested featureSet when
+// iterating over feature sets in case the test needs to know whether a
+// specific feature is enabled
 export function isFeatureEnabled(featureName) {
   return function isEnabled(feature) {
     return feature.name === featureName && feature.value;
   };
 }
 
+// Used in the block/describe/test message/name to show which features are enabled
+// in the current feature set - useful for checking where the errors happen
 export function enabledFeatures(features) {
   return features
     .filter(feature => feature.value)
