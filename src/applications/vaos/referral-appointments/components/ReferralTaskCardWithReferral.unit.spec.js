4796348757503ef9987bdb3c9751a1f5c98da90c
Bill Tran <bill@billjohntran.com>
2025-02-24 13:10:35 -0800

    fix unit tests

--- src/applications/vaos/referral-appointments/components/ReferralTaskCardWithReferral.unit.spec.js
+++ src/applications/vaos/referral-appointments/components/ReferralTaskCardWithReferral.unit.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import MockDate from 'mockdate';
+import { waitFor } from '@testing-library/dom';
 import {
   renderWithStoreAndRouter,
   createTestStore,
@@ -46,96 +47,115 @@ describe('VAOS Component: ReferralTaskCardWithReferral', () => {
     expect(await screen.findByTestId('referral-task-card')).to.exist;
   });
 
-  // it('should not display anything when url paramter is not populated', async () => {
-  //   const store = createTestStore();
-  //   const useGetReferralByIdStub = sinon
-  //     .stub(useGetReferralByIdModule, 'useGetReferralById')
-  //     .returns({
-  //       referral: undefined,
-  //       referralFetchStatus: FETCH_STATUS.notStarted,
-  //     });
-  //   const screen = renderWithStoreAndRouter(<ReferralTaskCardWithReferral />, {
-  //     store,
-  //     path: '/?id=',
-  //   });
+  it('should not display anything when url parameter is not populated', async () => {
+    const store = createTestStore();
+    fetchStub.resolves(
+      new Response(
+        JSON.stringify({
+          data: createReferralById(
+            '2024-11-29',
+            'add2f0f4-a1ea-4dea-a504-a54ab57c6801',
+          ),
+        }),
+        { status: 200 },
+      ),
+    );
 
-  //   const taskCard = screen.queryByTestId('referral-task-card');
-  //   const error = screen.queryByTestId('referral-error');
-  //   const loading = screen.queryByTestId('loading-indicator');
-  //   const expired = screen.queryByTestId('expired-alert');
-  //   expect(taskCard).to.be.null;
-  //   expect(error).to.be.null;
-  //   expect(loading).to.be.null;
-  //   expect(expired).to.be.null;
-  //   useGetReferralByIdStub.restore();
-  // });
+    const screen = renderWithStoreAndRouter(<ReferralTaskCardWithReferral />, {
+      store,
+      path: '/?id=',
+    });
 
-  // it('should display the expired alert when referral is expired', async () => {
-  //   const store = createTestStore();
-  //   const useGetReferralByIdStub = sinon
-  //     .stub(useGetReferralByIdModule, 'useGetReferralById')
-  //     .returns({
-  //       referral: createReferralById(
-  //         '2024-11-29',
-  //         '445e2d1b-7150-4631-97f2-f6f473bdef00',
-  //         '111',
-  //         '2024-12-01',
-  //       ),
-  //       referralFetchStatus: FETCH_STATUS.succeeded,
-  //     });
-  //   const screen = renderWithStoreAndRouter(<ReferralTaskCardWithReferral />, {
-  //     store,
-  //     path: '/?id=445e2d1b-7150-4631-97f2-f6f473bdef00',
-  //   });
-  //   expect(screen.getByTestId('expired-alert')).to.exist;
-  //   useGetReferralByIdStub.restore();
-  // });
+    const taskCard = screen.queryByTestId('referral-task-card');
+    const error = screen.queryByTestId('referral-error');
+    const loading = screen.queryByTestId('loading-indicator');
+    const expired = screen.queryByTestId('expired-alert');
+    expect(taskCard).to.be.null;
+    expect(error).to.be.null;
+    expect(loading).to.be.null;
+    expect(expired).to.be.null;
+  });
 
-  // it('should display the loading component when fetch status is loading', async () => {
-  //   const store = createTestStore();
-  //   const useGetReferralByIdStub = sinon
-  //     .stub(useGetReferralByIdModule, 'useGetReferralById')
-  //     .returns({
-  //       referral: undefined,
-  //       referralFetchStatus: FETCH_STATUS.loading,
-  //     });
-  //   const screen = renderWithStoreAndRouter(<ReferralTaskCardWithReferral />, {
-  //     store,
-  //     path: '/?id=1234',
-  //   });
-  //   expect(screen.getByTestId('loading-indicator')).to.exist;
-  //   useGetReferralByIdStub.restore();
-  // });
+  it('should display the expired alert when referral is expired', async () => {
+    const store = createTestStore();
 
-  // it('should display the loading component when fetch status is notStarted', async () => {
-  //   const store = createTestStore();
-  //   const useGetReferralByIdStub = sinon
-  //     .stub(useGetReferralByIdModule, 'useGetReferralById')
-  //     .returns({
-  //       referral: undefined,
-  //       referralFetchStatus: FETCH_STATUS.notStarted,
-  //     });
-  //   const screen = renderWithStoreAndRouter(<ReferralTaskCardWithReferral />, {
-  //     store,
-  //     path: '/?id=1234',
-  //   });
-  //   expect(screen.getByTestId('loading-indicator')).to.exist;
-  //   useGetReferralByIdStub.restore();
-  // });
+    fetchStub.resolves(
+      new Response(
+        JSON.stringify({
+          data: createReferralById(
+            '2024-11-29',
+            'add2f0f4-a1ea-4dea-a504-a54ab57c6801',
+            '111',
+            '2024-12-01',
+          ),
+        }),
+        { status: 200 },
+      ),
+    );
 
-  // it('should display the error alert when fetch fails', async () => {
-  //   const store = createTestStore();
-  //   const useGetReferralByIdStub = sinon
-  //     .stub(useGetReferralByIdModule, 'useGetReferralById')
-  //     .returns({
-  //       referral: undefined,
-  //       referralFetchStatus: FETCH_STATUS.failed,
-  //     });
-  //   const screen = renderWithStoreAndRouter(<ReferralTaskCardWithReferral />, {
-  //     store,
-  //     path: '/?id=error',
-  //   });
-  //   expect(screen.getByTestId('referral-error')).to.exist;
-  //   useGetReferralByIdStub.restore();
-  // });
+    const screen = renderWithStoreAndRouter(<ReferralTaskCardWithReferral />, {
+      store,
+      path: '/?id=445e2d1b-7150-4631-97f2-f6f473bdef00',
+    });
+
+    waitFor(() => {
+      expect(screen.getByTestId('expired-alert')).to.exist;
+    });
+  });
+
+  it('should display the loading component when fetch status is loading', async () => {
+    const store = createTestStore();
+    fetchStub.resolves(
+      new Response(
+        JSON.stringify({
+          data: createReferralById(
+            '2024-11-29',
+            'add2f0f4-a1ea-4dea-a504-a54ab57c6801',
+          ),
+        }),
+        { status: 200 },
+      ),
+    );
+
+    const screen = renderWithStoreAndRouter(<ReferralTaskCardWithReferral />, {
+      store,
+      path: '/?id=1234',
+    });
+    expect(screen.getByTestId('loading-indicator')).to.exist;
+  });
+
+  it('should display the loading component when fetch status is notStarted', async () => {
+    const store = createTestStore();
+    fetchStub.resolves(
+      new Response(
+        JSON.stringify({
+          data: createReferralById(
+            '2024-11-29',
+            'add2f0f4-a1ea-4dea-a504-a54ab57c6801',
+          ),
+        }),
+        { status: 200 },
+      ),
+    );
+
+    const screen = renderWithStoreAndRouter(<ReferralTaskCardWithReferral />, {
+      store,
+      path: '/?id=1234',
+    });
+    expect(screen.getByTestId('loading-indicator')).to.exist;
+  });
+
+  it('should display the error alert when fetch fails', async () => {
+    const store = createTestStore();
+    fetchStub.rejects(new Error('Internal Server Error'));
+
+    const screen = renderWithStoreAndRouter(<ReferralTaskCardWithReferral />, {
+      store,
+      path: '/?id=error',
+    });
+
+    waitFor(() => {
+      expect(screen.getByTestId('referral-error')).to.exist;
+    });
+  });
 });
