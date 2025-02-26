17bbc914ee60198d12e110e51b96c2d9c7721ac4
Nicholas Hibbits <172406954+nicholashibbits@users.noreply.github.com>
2025-02-26 07:45:14 -0800

    Edm 529 finalize build 3 (#34917)
    * consolidate effects fetching and filtering results
    
    * make dedicated FAQs ui component
    
    * move all form logic into form component
    
    * update url name, refactor breadcrumbs and routes accordingly
    
    * refactor function updating checkboxes
    
    * Abstract lacp search info to own component | update function names and url params
    
    * add test coverage for l&c helpers
    
    * add unit test for UI components
    
    * Expand unit test coverage
    
    * refactor failing unit tests
    
    ---------
    
    Co-authored-by: Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>

--- src/applications/gi/tests/containers/LicenseCertificationSearchResults.unit.spec.jsx
+++ src/applications/gi/tests/containers/LicenseCertificationSearchResults.unit.spec.jsx
@@ -1,32 +1,147 @@
 import React from 'react';
 import { expect } from 'chai';
-import { waitFor } from '@testing-library/react';
-import { shallow } from 'enzyme';
-// import { renderWithStoreAndRouter } from '../helpers';
-import createCommonStore from 'platform/startup/store';
+import { mount } from 'enzyme';
+import { cleanup } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { MemoryRouter, Route } from 'react-router-dom';
+import sinon from 'sinon';
 import LicenseCertificationSearchResults from '../../containers/LicenseCertificationSearchResults';
-import reducer from '../../reducers';
 
-const commonStore = createCommonStore(reducer);
+const mockStore = configureStore([thunk]);
 
-describe('<LicenseCertificationSearchResults />', () => {
-  it('should render', () => {
-    const wrapper = shallow(
-      <Provider store={commonStore}>
-        <LicenseCertificationSearchResults
-          dispatchFetchLicenseCertificationResults={() => {}}
-          lcResults={[]}
-          fetchingLc
-          hasFetchedOnce={false}
-        />
+describe('LicenseCertificationSearchResults', () => {
+  let store;
+  let initialState;
+  let addEventListenerSpy;
+  let removeEventListenerSpy;
+  let originalInnerWidth;
+
+  beforeEach(() => {
+    initialState = {
+      licenseCertificationSearch: {
+        hasFetchedOnce: false,
+        fetchingLc: false,
+        filteredResults: [],
+        lcResults: [],
+        error: null,
+      },
+    };
+    store = mockStore(initialState);
+
+    addEventListenerSpy = sinon.spy(window, 'addEventListener');
+    removeEventListenerSpy = sinon.spy(window, 'removeEventListener');
+    originalInnerWidth = global.innerWidth;
+  });
+
+  afterEach(() => {
+    cleanup();
+    addEventListenerSpy.restore();
+    removeEventListenerSpy.restore();
+    global.innerWidth = originalInnerWidth;
+  });
+
+  const mountComponent = (
+    path = '/licenses-certifications-and-prep-courses/results',
+  ) => {
+    return mount(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[path]}>
+          <Route path="/licenses-certifications-and-prep-courses/results">
+            <LicenseCertificationSearchResults />
+          </Route>
+        </MemoryRouter>
       </Provider>,
     );
+  };
 
-    waitFor(() => {
-      expect(wrapper).to.not.be.null;
+  it('should display a loading indicator when fetchingLc is true', () => {
+    store = mockStore({
+      licenseCertificationSearch: {
+        ...initialState.licenseCertificationSearch,
+        fetchingLc: true,
+      },
     });
 
+    const wrapper = mountComponent();
+    expect(wrapper.find('va-loading-indicator').exists()).to.be.true;
+    expect(wrapper.find('va-loading-indicator').prop('message')).to.equal(
+      'Loading...',
+    );
     wrapper.unmount();
+  });
+
+  it('should display search results when data is available', () => {
+    const mockResults = [
+      {
+        lacNm: 'Test Certification',
+        eduLacTypeNm: 'Certification',
+        enrichedId: '123',
+        state: 'VA',
+      },
+      {
+        lacNm: 'Test License',
+        eduLacTypeNm: 'License',
+        enrichedId: '456',
+        state: 'CA',
+      },
+    ];
+
+    store = mockStore({
+      licenseCertificationSearch: {
+        ...initialState.licenseCertificationSearch,
+        hasFetchedOnce: true,
+        filteredResults: mockResults,
+      },
+    });
+
+    const wrapper = mountComponent();
+    expect(wrapper.find('h1').text()).to.equal('Search results');
+    expect(wrapper.find('va-card')).to.have.lengthOf(2);
+    expect(
+      wrapper
+        .find('h3')
+        .at(1)
+        .text(),
+    ).to.equal('Test Certification');
+    expect(
+      wrapper
+        .find('h4')
+        .first()
+        .text(),
+    ).to.equal('Certification');
+    wrapper.unmount();
+  });
+
+  it('should display pagination when results exceed itemsPerPage', () => {
+    const mockResults = Array(11)
+      .fill()
+      .map((_, i) => ({
+        lacNm: `Test ${i}`,
+        eduLacTypeNm: 'Certification',
+        enrichedId: `${i}`,
+        state: 'VA',
+      }));
+
+    store = mockStore({
+      licenseCertificationSearch: {
+        ...initialState.licenseCertificationSearch,
+        hasFetchedOnce: true,
+        filteredResults: mockResults,
+      },
+    });
+
+    const wrapper = mountComponent();
+    expect(wrapper.find('VaPagination').exists()).to.be.true;
+    expect(wrapper.find('va-card')).to.have.lengthOf(10); // First page should show 10 items
+    wrapper.unmount();
+  });
+
+  it('should handle filter updates', () => {
+    const wrapper = mountComponent();
+    expect(addEventListenerSpy.calledWith('resize')).to.be.true;
+    wrapper.unmount();
+    expect(removeEventListenerSpy.calledWith('resize')).to.be.true;
   });
 });
