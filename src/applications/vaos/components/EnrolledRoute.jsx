cce0e40794bb0ec7432868d2ed0f9f7a19af9a2c
Simi Adebowale <47654119+simiadebowale@users.noreply.github.com>
2025-02-27 14:12:28 -0500

    Revert "101531 Route guard user to MHV if user is not ID-verified (#34655)" (#34956)
    This reverts commit 5e376a3ff843669b721f6503c1b0fe8ffb479f56.

--- src/applications/vaos/components/EnrolledRoute.jsx
+++ src/applications/vaos/components/EnrolledRoute.jsx
@@ -1,31 +1,19 @@
-import environment from '@department-of-veterans-affairs/platform-utilities/environment';
-import { RequiredLoginView } from 'platform/user/authorization/components/RequiredLoginView';
-import { selectPatientFacilities } from 'platform/user/cerner-dsot/selectors';
-import backendServices from 'platform/user/profile/constants/backendServices';
-import { selectUser } from 'platform/user/selectors';
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Route } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { selectAuthStatus } from '../redux/selectors';
+import { useSelector } from 'react-redux';
+import { selectUser } from 'platform/user/selectors';
+import { selectPatientFacilities } from 'platform/user/cerner-dsot/selectors.js';
+import backendServices from 'platform/user/profile/constants/backendServices';
+import { RequiredLoginView } from 'platform/user/authorization/components/RequiredLoginView';
+import environment from '@department-of-veterans-affairs/platform-utilities/environment';
+import NoRegistrationMessage from './NoRegistrationMessage';
 import { useDatadogRum } from '../utils/useDatadogRum';
 
 export default function EnrolledRoute({ component: RouteComponent, ...rest }) {
   const user = useSelector(selectUser);
   const sites = useSelector(selectPatientFacilities);
-  const { isUserLOA3 } = useSelector(selectAuthStatus);
-
   const hasRegisteredSystems = sites?.length > 0;
   useDatadogRum();
-
-  if (!user.profile.verified || !isUserLOA3 || !hasRegisteredSystems) {
-    // Using this approach to redirect since 'basename' is set to 'appointments'
-    // for the router.
-    // Ex. (!hasRegisteredSystems || !isUserLOA3) && ( <Redirect to={{ pathname: '/my-health' }} /> )
-    // results in: http://localhost:3001/my-health/appointments/my-health
-    window.location.replace(`/my-health`);
-  }
-
   return (
     <RequiredLoginView
       serviceRequired={[
@@ -35,10 +23,10 @@ export default function EnrolledRoute({ component: RouteComponent, ...rest }) {
       user={user}
       verify={!environment.isLocalhost()}
     >
-      <Route {...rest}>{hasRegisteredSystems && <RouteComponent />}</Route>
+      <Route {...rest}>
+        {!hasRegisteredSystems && <NoRegistrationMessage />}
+        {hasRegisteredSystems && <RouteComponent />}
+      </Route>
     </RequiredLoginView>
   );
 }
-EnrolledRoute.propTypes = {
-  component: PropTypes.func,
-};
