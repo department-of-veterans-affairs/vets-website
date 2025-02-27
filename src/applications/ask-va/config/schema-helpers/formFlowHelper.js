b3671e206f8cab20fb1808ea5bf15c4f8ab12191
Hemesh Patel <49699643+hemeshvpatel@users.noreply.github.com>
2025-02-27 13:09:00 -0600

    update searchSchools conditional

--- src/applications/ask-va/config/schema-helpers/formFlowHelper.js
+++ src/applications/ask-va/config/schema-helpers/formFlowHelper.js
@@ -215,13 +215,13 @@ const ch3Pages = {
     uiSchema: searchSchoolsPage.uiSchema,
     schema: searchSchoolsPage.schema,
     depends: form =>
-      (form.selectCategory === CategoryDebt &&
-        form.selectTopic === TopicEducationBenefitOverpayments) ||
-      ((form.useSchoolInProfile === schoolInYourProfileOptions.NO ||
-        !form.schoolInfo?.schoolName) &&
-        (form.yourRole === yourRoleOptionsEducation.SCO ||
-          form.yourRole ===
-            yourRoleOptionsEducation.TRAINING_OR_APPRENTICESHIP_SUP)),
+      (form.useSchoolInProfile === schoolInYourProfileOptions.NO ||
+        !form.schoolInfo?.schoolName ||
+        (form.selectCategory === CategoryDebt &&
+          form.selectTopic === TopicEducationBenefitOverpayments)) &&
+      (form.yourRole === yourRoleOptionsEducation.SCO ||
+        form.yourRole ===
+          yourRoleOptionsEducation.TRAINING_OR_APPRENTICESHIP_SUP),
   },
   schoolStOrResidency: {
     title: CHAPTER_3.SCHOOL.TITLE,
