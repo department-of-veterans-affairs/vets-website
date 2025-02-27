9001cd5cc640547c2ede1e515fee724bffd3e2c9
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2025-02-27 08:23:35 -0800

    Adds eligibility and facility phone to patient relationship hook
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>

--- src/applications/vaos/new-appointment/hooks/useGetPatientRelationships.js
+++ src/applications/vaos/new-appointment/hooks/useGetPatientRelationships.js
@@ -3,6 +3,7 @@ import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { FETCH_STATUS } from '../../utils/constants';
 import { useOHDirectScheduling } from './useOHDirectScheduling';
 import { getPatientRelationships } from '../redux/actions';
+import { getFacilityPhone } from '../../services/location';
 import {
   selectPatientProviderRelationships,
   getFacilityPageV2Info,
@@ -23,10 +24,12 @@ export function useGetPatientRelationships() {
     state => selectPatientProviderRelationships(state),
     shallowEqual,
   );
-  const { typeOfCare, selectedFacility } = useSelector(
+  const { typeOfCare, selectedFacility, eligibility } = useSelector(
     state => getFacilityPageV2Info(state),
     shallowEqual,
   );
+
+  const facilityPhone = getFacilityPhone(selectedFacility);
 
   useEffect(
     () => {
@@ -63,5 +66,7 @@ export function useGetPatientRelationships() {
     patientProviderRelationshipsStatus,
     typeOfCare,
     selectedFacility,
+    eligibility,
+    facilityPhone,
   };
 }
