7e54c716330aea05f190e8d07b712cfbf66bcbaf
Jennie McGibney <54327023+jenniemc@users.noreply.github.com>
2025-02-27 11:56:41 -0800

    Update past appointment date dropdown options (#34949)

--- src/applications/vaos/appointment-list/pages/PastAppointmentsPage/PastAppointmentsDateDropdown.unit.spec.js
+++ src/applications/vaos/appointment-list/pages/PastAppointmentsPage/PastAppointmentsDateDropdown.unit.spec.js
@@ -2,14 +2,17 @@ import { expect } from 'chai';
 import moment from 'moment';
 import React from 'react';
 import sinon from 'sinon';
-import { getPastAppointmentDateRangeOptions } from '.';
+import {
+  getPastAppointmentDateRangeOptions,
+  getMaximumPastAppointmentDateRange,
+} from '.';
 import { renderWithStoreAndRouter } from '../../../tests/mocks/setup';
 import PastAppointmentsDateDropdown from './PastAppointmentsDateDropdown';
 
-const ranges = getPastAppointmentDateRangeOptions(moment('2020-02-02'));
-
 describe('VAOS Component: PastAppointmentsDateDropDown', () => {
-  it('should trigger spy when a new date range is selected', async () => {
+  it('getPastAppointmentDateRangeOptions: should trigger spy when date range is selected', async () => {
+    const ranges = getPastAppointmentDateRangeOptions(moment('2020-02-02'));
+
     const initialState = {
       featureToggles: {},
     };
@@ -27,6 +30,36 @@ describe('VAOS Component: PastAppointmentsDateDropDown', () => {
       },
     );
     expect(screen.getAllByRole('option')).to.have.lengthOf(6);
+    expect(screen.queryAllByRole('button')).to.have.lengthOf(0);
+    // select a different date range
+    const selectDate = screen.container
+      .querySelector('#date-dropdown')
+      .__events.vaSelect({
+        detail: { value: '4' },
+      });
+    await selectDate;
+    expect(callback.calledOnce).to.be.true;
+  });
+  it('getMaximumPastAppointmentDateRange: should trigger spy when date range is selected', async () => {
+    const ranges = getMaximumPastAppointmentDateRange();
+
+    const initialState = {
+      featureToggles: {},
+    };
+
+    const callback = sinon.spy();
+
+    const screen = renderWithStoreAndRouter(
+      <PastAppointmentsDateDropdown
+        currentRange={0}
+        onChange={callback}
+        options={ranges}
+      />,
+      {
+        initialState,
+      },
+    );
+    expect(screen.getAllByRole('option')).to.have.lengthOf(4);
     expect(screen.queryAllByRole('button')).to.have.lengthOf(0);
     // select a different date range
     const selectDate = screen.container
