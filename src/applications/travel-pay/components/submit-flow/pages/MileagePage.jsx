545481cfdfe1dada0edea95f580d9cf8b4a3a011
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-02-28 11:39:41 -0800

    [#102881] Travel Pay, SMOC radio component (#34828)
    * Add SmocRadio component
    
    * Use component on address, mileage, vehicle SMOC flow pages

--- src/applications/travel-pay/components/submit-flow/pages/MileagePage.jsx
+++ src/applications/travel-pay/components/submit-flow/pages/MileagePage.jsx
@@ -2,15 +2,13 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 
-import {
-  VaButtonPair,
-  VaRadio,
-} from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import { VaButtonPair } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { focusElement, scrollToTop } from 'platform/utilities/ui';
 
 import { formatDateTime } from '../../../util/dates';
 import { BTSSS_PORTAL_URL } from '../../../constants';
 import { selectAppointment } from '../../../redux/selectors';
+import SmocRadio from '../../SmocRadio';
 
 const MileagePage = ({
   pageIndex,
@@ -47,18 +45,14 @@ const MileagePage = ({
   };
   return (
     <div>
-      <VaRadio
-        id="mileage"
-        onVaValueChange={e => {
+      <SmocRadio
+        name="mileage"
+        value={yesNo.mileage}
+        label="Are you claiming only mileage?"
+        error={requiredAlert}
+        onValueChange={e => {
           setYesNo({ ...yesNo, mileage: e.detail.value });
         }}
-        value={yesNo.mileage}
-        data-testid="mileage-test-id"
-        error={requiredAlert ? 'You must make a selection to continue.' : null}
-        header-aria-describedby={null}
-        hint={null}
-        label="Are you claiming only mileage?"
-        label-header-level="1"
       >
         <div className="vads-u-margin-y--2">
           <hr aria-hidden="true" className="vads-u-margin-y--0" />
@@ -74,21 +68,7 @@ const MileagePage = ({
           <p>{data.reasonForAppointment}</p>
           <hr aria-hidden="true" className="vads-u-margin-y--0" />
         </div>
-        <va-radio-option
-          label="Yes"
-          value="yes"
-          key="mileage-yes"
-          name="mileage"
-          checked={yesNo.mileage === 'yes'}
-        />
-        <va-radio-option
-          key="mileage-no"
-          name="mileage"
-          checked={yesNo.mileage === 'no'}
-          label="No"
-          value="no"
-        />
-      </VaRadio>
+      </SmocRadio>
 
       <va-additional-info
         class="vads-u-margin-y--3"
