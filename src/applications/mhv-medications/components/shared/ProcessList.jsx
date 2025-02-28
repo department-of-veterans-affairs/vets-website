1c388cb2cd9a8fd20e2249726078be0d8c2dc3c7
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-02-27 20:13:12 -0500

    [mhv-66686] create progress list component.

---
+++ src/applications/mhv-medications/components/shared/ProcessList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ProcessList = ({ stepGuideProps }) => {
+  const { title, processSteps } = stepGuideProps;
+  return (
+    <section>
+      <div className="no-print vads-u-margin-y--3 mobile-lg:vads-u-margin-y--4 vads-u-border-bottom--2px vads-u-border-color--gray-light" />
+      <h2 className="vads-u-margin-top--0 vads-u-margin-bottom--3">{title}</h2>
+      <va-process-list>
+        {processSteps.map((step, index) => (
+          <va-process-list-item key={index} header={step.header}>
+            {step.content}
+          </va-process-list-item>
+        ))}
+      </va-process-list>
+    </section>
+  );
+};
+
+export default ProcessList;
