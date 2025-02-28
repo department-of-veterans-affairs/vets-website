8eec6d1e2d0cfcfe8bc1b13735bafe66b5e74976
raji.venkatesh <raji.venkatesh@acommsolutions.com>
2025-02-28 12:13:48 -0600

    e2e test for static progress list
    added tests to validate static progress list for refill page with and without rx

--- src/applications/mhv-medications/components/shared/ProcessList.jsx
+++ src/applications/mhv-medications/components/shared/ProcessList.jsx
@@ -6,7 +6,12 @@ const ProcessList = ({ stepGuideProps }) => {
   return (
     <section>
       <div className="no-print vads-u-margin-y--3 mobile-lg:vads-u-margin-y--4 vads-u-border-bottom--2px vads-u-border-color--gray-light" />
-      <h2 className="vads-u-margin-top--0 vads-u-margin-bottom--3">{title}</h2>
+      <h2
+        className="vads-u-margin-top--0 vads-u-margin-bottom--3"
+        data-testid="progress-list-header"
+      >
+        {title}
+      </h2>
       <va-process-list>
         {processSteps.map((step, index) => (
           <va-process-list-item key={index} header={step.header}>
