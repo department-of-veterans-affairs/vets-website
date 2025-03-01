1b0db589524255e37e56e64ad94ba3c838ed4898
Alex Morgun <87077843+oleksii-morgun@users.noreply.github.com>
2025-03-01 08:55:50 -0500

    revert SmApi.js changes

--- src/applications/mhv-secure-messaging/api/SmApi.js
+++ src/applications/mhv-secure-messaging/api/SmApi.js
@@ -1,6 +1,5 @@
 import environment from '@department-of-veterans-affairs/platform-utilities/environment';
 import { apiRequest } from '@department-of-veterans-affairs/platform-utilities/exports';
-import { API_ROUTES } from 'platform/user/profile/vap-svc/constants';
 import { DefaultFolders, threadSortingOptions } from '../util/constants';
 
 const apiBasePath = `${environment.API_URL}/my_health/v1`;
@@ -405,7 +404,7 @@ export const searchFolderAdvanced = (folderId, query, isPilot) => {
  * @returns {Object} signature object {data: {signatureName, includeSignature, signatureTitle}, errors:{}, metadata: {}}
  */
 export const getSignature = () => {
-  return apiRequest(`${apiBasePath}${API_ROUTES.MESSAGING_SIGNATURE}`, {
+  return apiRequest(`${apiBasePath}/messaging/preferences/signature`, {
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
