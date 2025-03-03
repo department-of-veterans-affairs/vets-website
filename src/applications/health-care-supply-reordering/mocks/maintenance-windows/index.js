7b885783f987972d961d01b89b2a7bc1eef95ed6
Richard Davis <richard.davis@agile6.com>
2025-03-03 15:02:11 -0500

    Adds e2e spec

--- src/applications/health-care-supply-reordering/mocks/maintenance-windows/index.js
+++ src/applications/health-care-supply-reordering/mocks/maintenance-windows/index.js
@@ -5,8 +5,8 @@ module.exports = {
       type: 'maintenance_window',
       attributes: {
         externalService: 'mdot',
-        startTime: '2025-03-03T19:30:00.000Z',
-        endTime: '2025-03-03T20:00:00.000Z',
+        startTime: '2025-03-03T20:00:00.000Z',
+        endTime: '2025-03-03T20:15:00.000Z',
         description: 'A downtime event',
       },
     },
