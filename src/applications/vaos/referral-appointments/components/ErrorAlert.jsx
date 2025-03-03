bacd9667c9bc8e1ef0fa52c7b9c31c78ad0ee394
Bill John Tran <bill@billjohntran.com>
2025-03-03 06:48:55 -0800

    Referrals/102617 general error update (#34975)
    * update error message

--- src/applications/vaos/referral-appointments/components/ErrorAlert.jsx
+++ src/applications/vaos/referral-appointments/components/ErrorAlert.jsx
@@ -6,8 +6,7 @@ const ErrorAlert = ({ body }) => {
     <va-alert data-testid="error" status="error">
       <h2>We’re sorry. We’ve run into a problem.</h2>
       <p data-testid="error-body">
-        {body ||
-          'Something went wrong on our end. Please refresh the page and try again.'}
+        {body || 'Something went wrong on our end. Please try again later.'}
       </p>
     </va-alert>
   );
