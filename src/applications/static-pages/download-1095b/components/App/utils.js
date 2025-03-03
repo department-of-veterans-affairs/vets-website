2cd047b6421afe2aa9109c24c3d1498825640202
Nick Sayre <nick.sayre@adhocteam.us>
2025-03-03 10:47:37 -0600

    1422 - change functions to variables

--- src/applications/static-pages/download-1095b/components/App/utils.js
+++ src/applications/static-pages/download-1095b/components/App/utils.js
@@ -16,38 +16,34 @@ export const phoneComponent = number => {
   );
 };
 
-export const notFoundComponent = () => {
-  return (
-    <va-alert close-btn-aria-label="Close notification" status="info" visible>
-      <h4 slot="headline">
-        You don’t have a 1095-B tax form available right now
-      </h4>
-      <p className="vads-u-margin-y--0">
-        You do not have a 1095-B tax form available. This could be because you
-        weren’t enrolled in VA healthcare in 2024. If you think you were
-        enrolled, call us at {phoneComponent(CONTACTS['222_VETS'])}. We’re here
-        Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
-      </p>
-    </va-alert>
-  );
-};
+export const notFoundComponent = (
+  <va-alert close-btn-aria-label="Close notification" status="info" visible>
+    <h4 slot="headline">
+      You don’t have a 1095-B tax form available right now
+    </h4>
+    <p className="vads-u-margin-y--0">
+      You do not have a 1095-B tax form available. This could be because you
+      weren’t enrolled in VA healthcare in 2024. If you think you were enrolled,
+      call us at {phoneComponent(CONTACTS['222_VETS'])}. We’re here Monday
+      through Friday, 8:00 a.m. to 8:00 p.m. ET.
+    </p>
+  </va-alert>
+);
 
-export const unavailableComponent = () => {
-  return (
-    <va-alert close-btn-aria-label="Close notification" status="info" visible>
-      <h2 slot="headline">
-        Your 1095-B form isn’t available to download right now
-      </h2>
-      <div>
-        <p>
-          Check back later. Or, if you need help with this form now, call us at{' '}
-          {phoneComponent(CONTACTS['222_VETS'])}. We’re here Monday through
-          Friday, 8:00 a.m. to 8:00 p.m. ET.
-        </p>
-      </div>
-    </va-alert>
-  );
-};
+export const unavailableComponent = (
+  <va-alert close-btn-aria-label="Close notification" status="info" visible>
+    <h2 slot="headline">
+      Your 1095-B form isn’t available to download right now
+    </h2>
+    <div>
+      <p>
+        Check back later. Or, if you need help with this form now, call us at{' '}
+        {phoneComponent(CONTACTS['222_VETS'])}. We’re here Monday through
+        Friday, 8:00 a.m. to 8:00 p.m. ET.
+      </p>
+    </div>
+  </va-alert>
+);
 
 export const downloadErrorComponent = (
   <div id="downloadError" className="vads-u-margin-bottom--2p5">
