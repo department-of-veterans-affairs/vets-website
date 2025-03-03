5fdfada37486549f5148ea4633726ae22cd2aca5
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-03-03 12:45:57 -0600

    VACMS-18911 Prep for adding autosuggest for VA health services (#34982)
    * VACMS-18911 Prep for adding autosuggest for VA health services
    
    * Fix import

--- src/applications/facility-locator/tests/e2e/providerSearch.cypress.spec.js
+++ src/applications/facility-locator/tests/e2e/providerSearch.cypress.spec.js
@@ -1,10 +1,13 @@
+/* eslint-disable @department-of-veterans-affairs/axe-check-required */
+
 import mockGeocodingData from '../../constants/mock-geocoding-data.json';
-import CcpHelpers from '../ccp-helpers-cypress';
+import CcpHelpers from './helpers/ccp-helpers-cypress';
 import {
   featureCombinationsTogglesToTest,
   isFeatureEnabled,
   enabledFeatures,
 } from './featureTogglesToTest';
+import * as h from './helpers';
 
 const CC_PROVIDER = 'Community providers (in VA’s network)';
 const NON_VA_URGENT_CARE = 'In-network community urgent care';
@@ -13,15 +16,26 @@ const featureSetsToTest = featureCombinationsTogglesToTest([
   'facilities_use_address_typeahead',
 ]);
 
+const loadAndSelectCCProviders = () => {
+  cy.visit(h.ROOT_URL);
+  cy.injectAxeThenAxeCheck();
+
+  h.typeInCityStateInput('Austin, TX{esc}');
+  h.vaSelectSelect(h.FACILITY_TYPES.CC_PRO, h.FACILITY_TYPE_DROPDOWN);
+
+  // Wait for services to be saved to state and input field to not be disabled
+  cy.wait('@mockServices');
+};
+
 for (const featureSet of featureSetsToTest) {
   const isProgDiscEnabled = featureSet.some(
     isFeatureEnabled('facilities_use_fl_progressive_disclosure'),
   );
 
-  const facilityDropdown = '#facility-type-dropdown';
   const serviceDropdown = isProgDiscEnabled
     ? '.service-type-dropdown-desktop'
     : '.service-type-dropdown-tablet';
+
   describe(`Provider search - ${enabledFeatures(featureSet)}`, () => {
     beforeEach(() => {
       cy.intercept('GET', '/v0/feature_toggles?*', {
@@ -30,6 +44,7 @@ for (const featureSet of featureSetsToTest) {
           features: featureSet,
         },
       });
+
       cy.intercept('GET', '/v0/maintenance_windows', []);
       CcpHelpers.initApplicationMock('1223X2210X', 'searchDentistsProvider');
       CcpHelpers.initApplicationMock('261QE0002X', 'searchFacilitiesProvider');
@@ -38,147 +53,120 @@ for (const featureSet of featureSetsToTest) {
     });
 
     it('renders "Search for available service" prompt', () => {
-      cy.visit('/find-locations');
+      loadAndSelectCCProviders();
 
-      cy.get('#street-city-state-zip').type('Austin, TX{esc}', {
-        waitForAnimations: true,
-      });
-      cy.get(facilityDropdown)
-        .shadow()
-        .find('select')
-        .select('Community providers (in VA’s network)');
-      cy.wait('@mockServices');
-      // Wait for services to be saved to state and input field to not be disabled
-      cy.get('#service-type-ahead-input')
-        .should('not.be.disabled')
-        .focus();
+      h.verifyElementIsNotDisabled(h.CCP_SERVICE_TYPE_INPUT);
+      h.focusElement(h.CCP_SERVICE_TYPE_INPUT);
 
       if (!isProgDiscEnabled) {
-        cy.get('#search-available-service-prompt').should('exist');
+        h.verifyElementExists(h.SEARCH_AVAILABLE);
       }
 
-      cy.get('#service-type-ahead-input').type('D');
+      h.typeInCCPServiceTypeInput('D');
 
       if (!isProgDiscEnabled) {
-        cy.get('#search-available-service-prompt').should('exist');
+        h.verifyElementExists(h.SEARCH_AVAILABLE);
       }
 
-      cy.get('#service-type-ahead-input').type('De'); // 2nd and 3rd character -
-      cy.get('#search-available-service-prompt').should('not.exist');
+      h.typeInCCPServiceTypeInput('De'); // 2nd and 3rd character -
+      h.verifyElementDoesNotExist(h.SEARCH_AVAILABLE);
     });
 
     it("renders `We couldn't find that, please try another service ` prompt", () => {
-      cy.visit('/find-locations');
+      loadAndSelectCCProviders();
 
-      cy.get('#street-city-state-zip').type('Austin, TX{esc}');
-      cy.get(facilityDropdown)
-        .shadow()
-        .find('select')
-        .select('Community providers (in VA’s network)');
-      cy.wait('@mockServices');
-      cy.get('#service-type-ahead-input').type('djf');
-      cy.get('#could-not-find-service-prompt').should('exist');
+      h.typeInCCPServiceTypeInput('djf');
+      h.verifyElementExists(h.NO_SERVICE);
     });
 
     it('finds community dentists', () => {
-      cy.visit('/find-locations');
+      loadAndSelectCCProviders();
 
-      cy.get('#street-city-state-zip').type('Austin, TX{esc}');
-      cy.get(facilityDropdown)
-        .shadow()
-        .find('select')
-        .select(CC_PROVIDER);
-      cy.wait('@mockServices');
-      cy.get('#service-type-ahead-input').type('Dentist');
-      cy.get('#downshift-1-item-0').click({ waitForAnimations: true });
+      h.typeInCCPServiceTypeInput('Dentist');
+      h.clickElement('#downshift-1-item-0');
+      h.submitSearchForm();
 
-      cy.get('#facility-search').click({ waitForAnimations: true });
       cy.wait('@searchDentistsProvider');
 
-      cy.get('#search-results-subheader').contains(
+      h.verifyElementShouldContainString(
+        h.SEARCH_RESULTS_SUMMARY,
         `results for "${CC_PROVIDER}", "Dentist - Orofacial Pain" near "Austin, Texas"`,
       );
-      cy.get('#other-tools').should('exist');
 
-      cy.get('.facility-result h3').contains('Kerr, Max Olen');
-
-      cy.get('.va-pagination').should('not.exist');
-      cy.injectAxe();
-      cy.axeCheck();
+      h.verifyElementExists('#other-tools');
+      h.verifyElementShouldContainText('.facility-result h3', 'Kerr, Max Olen');
+      h.verifyElementDoesNotExist('.va-pagination');
     });
 
     it('finds community urgent care - Clinic/Center', () => {
-      cy.visit('/find-locations');
+      loadAndSelectCCProviders();
 
-      cy.get('#street-city-state-zip').type('Austin, TX{esc}', {
-        waitForAnimations: true,
-      });
-      cy.get(facilityDropdown)
-        .shadow()
-        .find('select')
-        .select(CC_PROVIDER);
-      cy.wait('@mockServices');
-      cy.get('#service-type-ahead-input').type('Clinic/Center - Urgent Care');
-      cy.get('#downshift-1-item-0').click({ waitForAnimations: true });
+      h.typeInCCPServiceTypeInput('Clinic/Center - Urgent Care');
+      h.clickElement('#downshift-1-item-0');
+      h.submitSearchForm();
 
-      cy.get('#facility-search').click({ waitForAnimations: true });
       cy.wait('@searchUrgentCare'); // only CCP Urgent care this way
-      cy.get('#search-results-subheader').contains(
+
+      h.verifyElementShouldContainString(
+        h.SEARCH_RESULTS_SUMMARY,
         `results for "${CC_PROVIDER}", "Clinic/Center - Urgent Care" near "Austin, Texas"`,
       );
-      cy.get('#other-tools').should('exist');
 
-      cy.get('.facility-result h3').contains('MinuteClinic');
-      cy.get('.va-pagination').should('not.exist');
-      cy.injectAxe();
-      cy.axeCheck();
+      h.verifyElementExists('#other-tools');
+      h.verifyElementShouldContainText('.facility-result h3', 'MinuteClinic');
+      h.verifyElementDoesNotExist('.va-pagination');
     });
 
     it('finds community urgent care', () => {
-      cy.visit('/find-locations');
+      cy.visit(h.ROOT_URL);
+      cy.injectAxeThenAxeCheck();
 
-      cy.get('#street-city-state-zip').type('Austin, TX{esc}');
-      cy.get(facilityDropdown)
-        .shadow()
-        .find('select')
-        .select('Urgent care');
+      h.typeInCityStateInput('Austin, TX{esc}');
+      h.vaSelectSelect(h.FACILITY_TYPES.URGENT, h.FACILITY_TYPE_DROPDOWN);
+
       cy.get(serviceDropdown)
         .find('select')
         .select(NON_VA_URGENT_CARE);
-      cy.get('#facility-search').click({ waitForAnimations: true });
+
+      h.submitSearchForm();
+
       cy.wait('@mockUrgentCare');
-      cy.get('#search-results-subheader').contains(
+
+      h.verifyElementShouldContainString(
+        h.SEARCH_RESULTS_SUMMARY,
         `Results for "Urgent care", "${NON_VA_URGENT_CARE}" near "Austin, Texas"`,
       );
-      cy.get('#other-tools').should('exist');
 
-      cy.get('.facility-result h3').contains('MinuteClinic');
-      cy.get('.va-pagination').should('not.exist');
-      cy.injectAxe();
-      cy.axeCheck();
+      h.verifyElementExists('#other-tools');
+      h.verifyElementShouldContainText('.facility-result h3', 'MinuteClinic');
+      h.verifyElementDoesNotExist('.va-pagination');
     });
 
     it('finds In-network community emergency care', () => {
-      cy.visit('/find-locations');
+      cy.visit(h.ROOT_URL);
+      cy.injectAxeThenAxeCheck();
 
-      cy.get('#street-city-state-zip').type('Austin, TX{esc}');
-      cy.get(facilityDropdown)
-        .shadow()
-        .find('select')
-        .select('Emergency care');
+      h.typeInCityStateInput('Austin, TX{esc}');
+      h.vaSelectSelect(h.FACILITY_TYPES.EMERGENCY, h.FACILITY_TYPE_DROPDOWN);
+
       cy.get(serviceDropdown)
         .find('select')
         .select('In-network community emergency care');
-      cy.get('#facility-search').click({ waitForAnimations: true });
+
+      h.submitSearchForm();
+
       cy.wait('@searchFacilitiesProvider');
-      cy.get('#search-results-subheader').contains(
+
+      h.verifyElementShouldContainString(
+        h.SEARCH_RESULTS_SUMMARY,
         `Results for "Emergency Care", "In-network community emergency care" near "Austin, Texas"`,
       );
-      cy.get('#emergency-care-info-note').should('exist');
-      cy.get('.facility-result h3').contains('DELL SETON MEDICAL CENTER AT UT');
 
-      cy.injectAxe();
-      cy.axeCheck();
+      h.verifyElementExists('#emergency-care-info-note');
+      h.verifyElementShouldContainText(
+        '.facility-result h3',
+        'DELL SETON MEDICAL CENTER AT UT',
+      );
     });
   });
 }
