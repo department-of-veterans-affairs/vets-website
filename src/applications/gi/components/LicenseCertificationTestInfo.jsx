cbde27c42b80d1be393dbbdca141a7775424c7b1
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-03-04 14:06:36 -0800

    update l&c cypress test

--- src/applications/gi/components/LicenseCertificationTestInfo.jsx
+++ src/applications/gi/components/LicenseCertificationTestInfo.jsx
@@ -134,7 +134,7 @@ function LcTestInfo({ tests }) {
         <div className="single-test-wrapper">
           <h4 className="vads-u-margin-top--1p5">Test name: {tests[0].name}</h4>
           {/* <p className="fee">Fee {formatCurrency(tests[0].fee)}</p> */}
-          <p className="vads-u-margin-y--0">
+          <p className="fee vads-u-margin-y--0">
             Fee:{' '}
             {formatDollarAmountWithCents(
               tests[0].fee,
