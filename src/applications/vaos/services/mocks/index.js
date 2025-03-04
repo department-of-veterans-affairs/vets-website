18b4a435e4597a9c3809d8ee51ea0b03cbbd08a2
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2025-03-04 07:45:59 -0800

    Fixes mocks
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>

--- src/applications/vaos/services/mocks/index.js
+++ src/applications/vaos/services/mocks/index.js
@@ -294,9 +294,8 @@ const responses = {
     // Request, not Facility 983, not Facility 692 (OH)
     if (
       !isDirect &&
-      (!req.query.facility_id.startsWith('983') ||
+      (!req.query.facility_id.startsWith('983') &&
         !req.query.facility_id.startsWith('692'))
-      // req.query.facility_id.startsWith('692GA')
     ) {
       ineligibilityReasons.push({
         coding: [
