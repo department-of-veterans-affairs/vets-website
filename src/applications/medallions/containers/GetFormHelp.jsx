435c51cff76cdfa04384739824b6a719e108dcea
Adam Antonioli <adam.antonioli@va.gov>
2025-03-04 15:34:25 -0500

    MBMS-82276 add help footer to medallions form

--- src/applications/medallions/containers/GetFormHelp.jsx
+++ src/applications/medallions/containers/GetFormHelp.jsx
@@ -5,17 +5,19 @@ export default function GetFormHelp() {
   return (
     <div>
       <p className="help-talk">
-        Call the National Cemetery Scheduling Office at{' '}
-        <va-telephone contact={CONTACTS.NCA} /> (
-        <va-telephone contact={CONTACTS['711']} tty />
-        ), and select option 4. We’re here Monday through Friday, 8:00 a.m. to
-        5:30 p.m. ET.
-      </p>
-      <p className="help-talk">
-        For benefit-related questions, call VA Benefits and Services at{' '}
-        <va-telephone contact={CONTACTS.VA_BENEFITS} /> (
+        <b>If you have questions about burial benefits, </b>
+        call VA Benefits and Services at <va-telephone contact="8008276947" /> (
         <va-telephone contact={CONTACTS['711']} tty />
         ). We’re here Monday through Friday, 8:00 a.m. to 7:30 p.m. ET.
+      </p>
+      <p className="help-talk">
+        <b>If you need help filling out your form, </b>
+        contact a local Veterans Service Organization (VSO).
+      </p>
+      <p>
+        <a href="/get-help-from-accredited-representative/find-rep/">
+          Find a local Veterans Service Organization
+        </a>
       </p>
     </div>
   );
