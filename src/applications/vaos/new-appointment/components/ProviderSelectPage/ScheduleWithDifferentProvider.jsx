200134f6b2042178e77104b1165ece7dd0df9ef9
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2025-03-04 08:55:01 -0800

    Updates unit tests
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>

--- src/applications/vaos/new-appointment/components/ProviderSelectPage/ScheduleWithDifferentProvider.jsx
+++ src/applications/vaos/new-appointment/components/ProviderSelectPage/ScheduleWithDifferentProvider.jsx
@@ -30,7 +30,10 @@ export default function ScheduleWithDifferentProvider({
       <h2 className="vads-u-font-size--h3 vads-u-margin-bottom--0 vads-u-margin-top--2">
         If you want to schedule with a different provider
       </h2>
-      <h3 className="vads-u-font-size--h4 vads-u-margin-bottom--0 vads-u-margin-top--1">
+      <h3
+        className="vads-u-font-size--h4 vads-u-margin-bottom--0 vads-u-margin-top--1"
+        data-testid="cc-eligible-header"
+      >
         Option 1: Call the facility
       </h3>
       <p className="vads-u-margin-y--0">
