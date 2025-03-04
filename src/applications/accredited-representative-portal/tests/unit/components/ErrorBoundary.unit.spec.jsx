46f35472e403f1562b91de02572bad815700e82e
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2025-03-04 09:58:01 -0500

    fixing spec

--- src/applications/accredited-representative-portal/tests/unit/components/ErrorBoundary.unit.spec.jsx
+++ src/applications/accredited-representative-portal/tests/unit/components/ErrorBoundary.unit.spec.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
+import sinon from 'sinon';
+
+// Mock the Header component
+import * as Header from '../../../components/Header';
+
+sinon.stub(Header, 'default').returns(<div data-testid="mock-header" />);
 
 import ErrorBoundary from '../../../components/ErrorBoundary';
 
 describe('ErrorBoundary', () => {
+  let sandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
   const getErrorBoundary = () => render(<ErrorBoundary />);
 
   it('renders error message', () => {
