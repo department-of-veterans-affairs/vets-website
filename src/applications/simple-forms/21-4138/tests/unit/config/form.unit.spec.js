c3967f75a9ddb90d294ba70586de5ca07975097c
Jacob Penner <japenner@gmail.com>
2025-03-06 15:59:39 -0500

    add full coverage for 4138 form config file

--- src/applications/simple-forms/21-4138/tests/unit/config/form.unit.spec.js
+++ src/applications/simple-forms/21-4138/tests/unit/config/form.unit.spec.js
@@ -14,23 +14,23 @@ describe('formConfig', () => {
 
   it('should have required top-level properties', () => {
     expect(formConfig).to.include.keys(
-      'rootUrl',
-      'urlPrefix',
-      'submitUrl',
-      'transformForSubmit',
-      'dev',
-      'trackingPrefix',
-      'introduction',
-      'confirmation',
-      'formId',
-      'saveInProgress',
-      'version',
-      'prefillEnabled',
-      'hideUnauthedStartLink',
-      'savedFormMessages',
-      'title',
-      'subTitle',
       'chapters',
+      'confirmation',
+      'dev',
+      'formId',
+      'hideUnauthedStartLink',
+      'introduction',
+      'prefillEnabled',
+      'rootUrl',
+      'savedFormMessages',
+      'saveInProgress',
+      'submitUrl',
+      'subTitle',
+      'title',
+      'trackingPrefix',
+      'transformForSubmit',
+      'urlPrefix',
+      'version',
     );
   });
 
@@ -38,90 +38,473 @@ describe('formConfig', () => {
     it('should be an object', () => {
       expect(formConfig.chapters).to.be.an('object');
     });
-  });
 
-  describe('statementTypePage', () => {
-    it('should have required properties', () => {
-      const {
-        statementTypePage,
-      } = formConfig.chapters.statementTypeChapter.pages;
-      expect(statementTypePage).to.include.keys(
-        'path',
-        'title',
-        'uiSchema',
-        'schema',
-        'pageClass',
-        'initialData',
-      );
-    });
-  });
+    describe('statementTypeChapter', () => {
+      describe('pages', () => {
+        let formData;
+        let stubEligibleForDecisionReview;
 
-  describe('dependent pages', () => {
-    let formData;
-    let stubEligibleForDecisionReview;
-
-    beforeEach(() => {
-      formData = {
-        statementType: null,
-        decisionDate: null,
-        decisionReviewType: null,
-      };
-      stubEligibleForDecisionReview = sinon
-        .stub(helpers, 'isEligibleForDecisionReview')
-        .returns(false);
-    });
-
-    afterEach(() => {
-      stubEligibleForDecisionReview.restore();
-    });
-
-    context('when statementType is BUDDY_STATEMENT', () => {
-      beforeEach(() => {
-        formData.statementType = STATEMENT_TYPES.BUDDY_STATEMENT;
-      });
-
-      it('should show layWitnessStatementPage', () => {
-        const {
-          layWitnessStatementPage,
-        } = formConfig.chapters.statementTypeChapter.pages;
-        expect(layWitnessStatementPage.depends(formData)).to.be.true;
-      });
-    });
-
-    context('when statementType is DECISION_REVIEW', () => {
-      beforeEach(() => {
-        formData.statementType = STATEMENT_TYPES.DECISION_REVIEW;
-      });
-
-      it('should show decisionReviewPage', () => {
-        const {
-          decisionReviewPage,
-        } = formConfig.chapters.statementTypeChapter.pages;
-        expect(decisionReviewPage.depends(formData)).to.be.true;
-      });
-
-      it('should show newSupplementalClaimPage', () => {
-        const {
-          newSupplementalClaimPage,
-        } = formConfig.chapters.statementTypeChapter.pages;
-        expect(newSupplementalClaimPage.depends(formData)).to.be.true;
-      });
-
-      context('when eligible for decision review', () => {
         beforeEach(() => {
-          stubEligibleForDecisionReview.returns(true);
+          formData = {
+            statementType: null,
+            decisionDate: null,
+            decisionReviewType: null,
+          };
+          stubEligibleForDecisionReview = sinon
+            .stub(helpers, 'isEligibleForDecisionReview')
+            .returns(false);
         });
 
-        context('when review type is NEW_EVIDENCE', () => {
-          beforeEach(() => {
-            formData.decisionReviewType = DECISION_REVIEW_TYPES.NEW_EVIDENCE;
+        afterEach(() => {
+          stubEligibleForDecisionReview.restore();
+        });
+
+        describe('statementTypePage', () => {
+          const {
+            statementTypePage,
+          } = formConfig.chapters.statementTypeChapter.pages;
+          it('should have required properties', () => {
+            expect(statementTypePage).to.include.keys(
+              'initialData',
+              'pageClass',
+              'path',
+              'schema',
+              'title',
+              'uiSchema',
+            );
+          });
+        });
+
+        describe('layWitnessStatementPage', () => {
+          const {
+            layWitnessStatementPage,
+          } = formConfig.chapters.statementTypeChapter.pages;
+
+          context('when statementType is BUDDY_STATEMENT', () => {
+            beforeEach(() => {
+              formData.statementType = STATEMENT_TYPES.BUDDY_STATEMENT;
+            });
+
+            it('should display the page', () => {
+              expect(layWitnessStatementPage.depends(formData)).to.be.true;
+            });
+          });
+        });
+
+        describe('decisionReviewPage', () => {
+          const {
+            decisionReviewPage,
+          } = formConfig.chapters.statementTypeChapter.pages;
+
+          context('when statementType is DECISION_REVIEW', () => {
+            beforeEach(() => {
+              formData.statementType = STATEMENT_TYPES.DECISION_REVIEW;
+            });
+
+            it('should display the page', () => {
+              expect(decisionReviewPage.depends(formData)).to.be.true;
+            });
+          });
+        });
+
+        describe('newSupplementalClaimPage', () => {
+          const {
+            newSupplementalClaimPage,
+          } = formConfig.chapters.statementTypeChapter.pages;
+
+          context('when statementType is DECISION_REVIEW', () => {
+            beforeEach(() => {
+              formData.statementType = STATEMENT_TYPES.DECISION_REVIEW;
+            });
+
+            context('when decision review is not eligible', () => {
+              beforeEach(() => {
+                stubEligibleForDecisionReview.returns(false);
+              });
+
+              it('should display the page', () => {
+                expect(newSupplementalClaimPage.depends(formData)).to.be.true;
+              });
+            });
+          });
+        });
+
+        describe('supplementalClaimPage', () => {
+          const {
+            supplementalClaimPage,
+          } = formConfig.chapters.statementTypeChapter.pages;
+
+          context('when statementType is DECISION_REVIEW', () => {
+            beforeEach(() => {
+              formData.statementType = STATEMENT_TYPES.DECISION_REVIEW;
+            });
+
+            context('when eligible for decision review', () => {
+              beforeEach(() => {
+                stubEligibleForDecisionReview.returns(true);
+              });
+
+              context('when decision review type is NEW_EVIDENCE', () => {
+                beforeEach(() => {
+                  formData.decisionReviewType =
+                    DECISION_REVIEW_TYPES.NEW_EVIDENCE;
+                });
+
+                it('should display the page', () => {
+                  expect(supplementalClaimPage.depends(formData)).to.be.true;
+                });
+              });
+            });
+          });
+        });
+
+        describe('higherLevelReviewPage', () => {
+          const {
+            higherLevelReviewPage,
+          } = formConfig.chapters.statementTypeChapter.pages;
+
+          context('when statementType is DECISION_REVIEW', () => {
+            beforeEach(() => {
+              formData.statementType = STATEMENT_TYPES.DECISION_REVIEW;
+            });
+
+            context('when eligible for decision review', () => {
+              beforeEach(() => {
+                stubEligibleForDecisionReview.returns(true);
+              });
+
+              context('when decision review type is ERROR_MADE', () => {
+                beforeEach(() => {
+                  formData.decisionReviewType =
+                    DECISION_REVIEW_TYPES.ERROR_MADE;
+                });
+
+                it('should display the page', () => {
+                  expect(higherLevelReviewPage.depends(formData)).to.be.true;
+                });
+              });
+            });
+          });
+        });
+
+        describe('boardAppealPage', () => {
+          const {
+            boardAppealPage,
+          } = formConfig.chapters.statementTypeChapter.pages;
+
+          context('when statementType is DECISION_REVIEW', () => {
+            beforeEach(() => {
+              formData.statementType = STATEMENT_TYPES.DECISION_REVIEW;
+            });
+
+            context('when eligible for decision review', () => {
+              beforeEach(() => {
+                stubEligibleForDecisionReview.returns(true);
+              });
+
+              context('when decision review type is BVA_REQUEST', () => {
+                beforeEach(() => {
+                  formData.decisionReviewType =
+                    DECISION_REVIEW_TYPES.BVA_REQUEST;
+                });
+
+                it('should display the page', () => {
+                  expect(boardAppealPage.depends(formData)).to.be.true;
+                });
+              });
+            });
+          });
+        });
+
+        describe('priorityProcessingPage', () => {
+          const {
+            priorityProcessingPage,
+          } = formConfig.chapters.statementTypeChapter.pages;
+
+          context('when statementType is PRIORITY_PROCESSING', () => {
+            beforeEach(() => {
+              formData.statementType = STATEMENT_TYPES.PRIORITY_PROCESSING;
+            });
+
+            it('should display the page', () => {
+              expect(priorityProcessingPage.depends(formData)).to.be.true;
+            });
+          });
+        });
+
+        describe('personalRecordsRequestPage', () => {
+          const {
+            personalRecordsRequestPage,
+          } = formConfig.chapters.statementTypeChapter.pages;
+
+          context('when statementType is PERSONAL_RECORDS', () => {
+            beforeEach(() => {
+              formData.statementType = STATEMENT_TYPES.PERSONAL_RECORDS;
+            });
+
+            it('should display the page', () => {
+              expect(personalRecordsRequestPage.depends(formData)).to.be.true;
+            });
+          });
+        });
+
+        describe('claimStatusToolPage', () => {
+          const {
+            claimStatusToolPage,
+          } = formConfig.chapters.statementTypeChapter.pages;
+
+          context('when statementType is NEW_EVIDENCE', () => {
+            beforeEach(() => {
+              formData.statementType = STATEMENT_TYPES.NEW_EVIDENCE;
+            });
+
+            it('should display the page', () => {
+              expect(claimStatusToolPage.depends(formData)).to.be.true;
+            });
+          });
+        });
+      });
+    });
+
+    describe('personalInformationChapter', () => {
+      describe('pages', () => {
+        let formData;
+        let stubEligibleToSubmitStatement;
+
+        beforeEach(() => {
+          formData = {
+            statementType: null,
+            decisionDate: null,
+            decisionReviewType: null,
+          };
+          stubEligibleToSubmitStatement = sinon
+            .stub(helpers, 'isEligibleToSubmitStatement')
+            .returns(false);
+        });
+
+        afterEach(() => {
+          stubEligibleToSubmitStatement.restore();
+        });
+
+        describe('personalInformationPage', () => {
+          const {
+            personalInformationPage,
+          } = formConfig.chapters.personalInformationChapter.pages;
+
+          it('should have required properties', () => {
+            expect(personalInformationPage).to.include.keys(
+              'depends',
+              'pageClass',
+              'path',
+              'schema',
+              'title',
+              'uiSchema',
+            );
           });
 
-          it('should show supplementalClaimPage', () => {
-            const {
-              supplementalClaimPage,
-            } = formConfig.chapters.statementTypeChapter.pages;
-            expect(supplementalClaimPage.depends(formData)).to.be.true;
+          context('when eligible to submit statement', () => {
+            beforeEach(() => {
+              stubEligibleToSubmitStatement.returns(true);
+            });
+
+            it('should display the page', () => {
+              expect(personalInformationPage.depends(formData)).to.be.true;
+            });
+          });
+        });
+      });
+    });
+
+    describe('identificationChapter', () => {
+      describe('pages', () => {
+        let formData;
+        let stubEligibleToSubmitStatement;
+
+        beforeEach(() => {
+          formData = {
+            statementType: null,
+            decisionDate: null,
+            decisionReviewType: null,
+          };
+          stubEligibleToSubmitStatement = sinon
+            .stub(helpers, 'isEligibleToSubmitStatement')
+            .returns(false);
+        });
+
+        afterEach(() => {
+          stubEligibleToSubmitStatement.restore();
+        });
+
+        describe('identificationInformationPage', () => {
+          const {
+            identificationInformationPage,
+          } = formConfig.chapters.identificationChapter.pages;
+
+          it('should have required properties', () => {
+            expect(identificationInformationPage).to.include.keys(
+              'depends',
+              'pageClass',
+              'path',
+              'schema',
+              'title',
+              'uiSchema',
+            );
+          });
+
+          context('when eligible to submit statement', () => {
+            beforeEach(() => {
+              stubEligibleToSubmitStatement.returns(true);
+            });
+
+            it('should display the page', () => {
+              expect(identificationInformationPage.depends(formData)).to.be
+                .true;
+            });
+          });
+        });
+      });
+    });
+
+    describe('mailingAddressChapter', () => {
+      describe('pages', () => {
+        let formData;
+        let stubEligibleToSubmitStatement;
+
+        beforeEach(() => {
+          formData = {
+            statementType: null,
+            decisionDate: null,
+            decisionReviewType: null,
+          };
+          stubEligibleToSubmitStatement = sinon
+            .stub(helpers, 'isEligibleToSubmitStatement')
+            .returns(false);
+        });
+
+        afterEach(() => {
+          stubEligibleToSubmitStatement.restore();
+        });
+
+        describe('mailingAddressPage', () => {
+          const {
+            mailingAddressPage,
+          } = formConfig.chapters.mailingAddressChapter.pages;
+
+          it('should have required properties', () => {
+            expect(mailingAddressPage).to.include.keys(
+              'depends',
+              'pageClass',
+              'path',
+              'schema',
+              'title',
+              'uiSchema',
+            );
+          });
+
+          context('when eligible to submit statement', () => {
+            beforeEach(() => {
+              stubEligibleToSubmitStatement.returns(true);
+            });
+
+            it('should display the page', () => {
+              expect(mailingAddressPage.depends(formData)).to.be.true;
+            });
+          });
+        });
+      });
+    });
+
+    describe('contactInformationChapter', () => {
+      describe('pages', () => {
+        let formData;
+        let stubEligibleToSubmitStatement;
+
+        beforeEach(() => {
+          formData = {
+            statementType: null,
+            decisionDate: null,
+            decisionReviewType: null,
+          };
+          stubEligibleToSubmitStatement = sinon
+            .stub(helpers, 'isEligibleToSubmitStatement')
+            .returns(false);
+        });
+
+        afterEach(() => {
+          stubEligibleToSubmitStatement.restore();
+        });
+
+        describe('contactInformationPage', () => {
+          const {
+            contactInformationPage,
+          } = formConfig.chapters.contactInformationChapter.pages;
+
+          it('should have required properties', () => {
+            expect(contactInformationPage).to.include.keys(
+              'depends',
+              'pageClass',
+              'path',
+              'schema',
+              'title',
+              'uiSchema',
+            );
+          });
+
+          context('when eligible to submit statement', () => {
+            beforeEach(() => {
+              stubEligibleToSubmitStatement.returns(true);
+            });
+
+            it('should display the page', () => {
+              expect(contactInformationPage.depends(formData)).to.be.true;
+            });
+          });
+        });
+      });
+    });
+
+    describe('statementChapter', () => {
+      describe('pages', () => {
+        let formData;
+        let stubEligibleToSubmitStatement;
+
+        beforeEach(() => {
+          formData = {
+            statementType: null,
+            decisionDate: null,
+            decisionReviewType: null,
+          };
+          stubEligibleToSubmitStatement = sinon
+            .stub(helpers, 'isEligibleToSubmitStatement')
+            .returns(false);
+        });
+
+        afterEach(() => {
+          stubEligibleToSubmitStatement.restore();
+        });
+
+        describe('statementPage', () => {
+          const { statementPage } = formConfig.chapters.statementChapter.pages;
+
+          it('should have required properties', () => {
+            expect(statementPage).to.include.keys(
+              'depends',
+              'pageClass',
+              'path',
+              'schema',
+              'title',
+              'uiSchema',
+            );
+          });
+
+          context('when eligible to submit statement', () => {
+            beforeEach(() => {
+              stubEligibleToSubmitStatement.returns(true);
+            });
+
+            it('should display the page', () => {
+              expect(statementPage.depends(formData)).to.be.true;
+            });
           });
         });
       });
