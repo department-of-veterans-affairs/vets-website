b2fbab5e5c0f6b2a1b6854128463ac49c2697fb2
Vitalii Menshutin <133991282+vmenshutin-bylight@users.noreply.github.com>
2025-03-07 10:52:16 -0500

    MHV-67988: Add redirect for About Medications page (#35045)
    * MHV-67988: Add redirect for About Medications page
    
    * updated the redirect link
    
    * MHV-67988: Add redirect for About Medications page
    
    * updated the redirect link
    
    * e2e updates for remove about meds page
    
    updated e2e to navigate to medications list page
    
    * e2e updates
    
    updated test to remove about medication page methods
    
    * more e2e updates
    
    updated methods to replace landing page methods
    
    * test updates for about meds redirect
    
    fixed methods to replace landing page methods
    
    * e2e delete landing page tests
    
    updated methods to navigate to medications list page and deleted tests for landing page
    
    * test fixes for nav changes
    
    updated tests to navigate to list page
    
    * more test fixes to nav to list page
    
    updated method to nav to medications list page
    
    * e2e updates for nav changes
    
    fixed method to nav to list page instead of landing page and deleted tests for landing page validations
    
    * e2e test updates for nav to list page
    
    fixed method to nav to list page instead of landing page
    
    * e2e redirect to list page
    
    test added to validate redirection to medications list page
    
    ---------
    
    Co-authored-by: raji.venkatesh <raji.venkatesh@acommsolutions.com>

--- src/applications/mhv-medications/tests/e2e/medications-download-pdf-details-page.cypress.spec.js
+++ src/applications/mhv-medications/tests/e2e/medications-download-pdf-details-page.cypress.spec.js
@@ -3,17 +3,15 @@ import MedicationsSite from './med_site/MedicationsSite';
 import MedicationsDetailsPage from './pages/MedicationsDetailsPage';
 import MedicationsListPage from './pages/MedicationsListPage';
 import mockPrescriptionDetails from './fixtures/prescription-details.json';
-import MedicationsLandingPage from './pages/MedicationsLandingPage';
+import rxList from './fixtures/listOfPrescriptions.json';
 
 describe('Medications Details Page Download', () => {
   it('visits Medications Details Page Download PDF Dropdown', () => {
     const site = new MedicationsSite();
     const listPage = new MedicationsListPage();
     const detailsPage = new MedicationsDetailsPage();
-    const landingPage = new MedicationsLandingPage();
     site.login();
-    landingPage.visitLandingPageURL();
-    listPage.clickGotoMedicationsLink();
+    listPage.visitMedicationsListPageURL(rxList);
     detailsPage.clickMedicationHistoryAndDetailsLink(mockPrescriptionDetails);
     listPage.clickPrintOrDownloadThisListDropDown();
     detailsPage.verifyFocusOnPrintOrDownloadDropdownButtonOnDetailsPage();
