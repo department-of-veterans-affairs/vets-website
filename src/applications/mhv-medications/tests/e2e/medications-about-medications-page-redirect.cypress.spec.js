baae5350981a939a6ac5b10675b8b39ec79081da
Vitalii Menshutin <133991282+vmenshutin-bylight@users.noreply.github.com>
2025-03-07 10:52:16 -0500

    MHV-67988: Add redirect for About Medications page (#35045)
    * MHV-67988: Add redirect for About Medications page
    
    * updated the redirect link
    
    * MHV-67988: Add redirect for About Medications page
    
    * updated the redirect link
    
    * e2e updates for remove about meds page
    
    updated e2e to navigate to medications list page
    
    * e2e updates
    
    updated test to remove about medication page methods
    
    * more e2e updates
    
    updated methods to replace landing page methods
    
    * test updates for about meds redirect
    
    fixed methods to replace landing page methods
    
    * e2e delete landing page tests
    
    updated methods to navigate to medications list page and deleted tests for landing page
    
    * test fixes for nav changes
    
    updated tests to navigate to list page
    
    * more test fixes to nav to list page
    
    updated method to nav to medications list page
    
    * e2e updates for nav changes
    
    fixed method to nav to list page instead of landing page and deleted tests for landing page validations
    
    * e2e test updates for nav to list page
    
    fixed method to nav to list page instead of landing page
    
    * e2e redirect to list page
    
    test added to validate redirection to medications list page
    
    ---------
    
    Co-authored-by: raji.venkatesh <raji.venkatesh@acommsolutions.com>

--- src/applications/mhv-medications/tests/e2e/medications-about-medications-page-redirect.cypress.spec.js
+++ src/applications/mhv-medications/tests/e2e/medications-about-medications-page-redirect.cypress.spec.js
@@ -1,18 +1,16 @@
 import MedicationsSite from './med_site/MedicationsSite';
 import rxList from './fixtures/listOfPrescriptions.json';
 import MedicationsListPage from './pages/MedicationsListPage';
-import unknownRx from './fixtures/unknown-prescription-details.json';
 
-describe('Medications List Page Pharmacy Phone Number for Unknown Rx', () => {
-  it('visits Medications List Page Pharmacy Phone Number for Unknown Rx', () => {
+describe('Medications About Medications Redirect to List Page', () => {
+  it('visits Medications List Page After Redirect', () => {
     const site = new MedicationsSite();
     const listPage = new MedicationsListPage();
     site.login();
+    cy.visit('/my-health/medications/about-medications');
     listPage.visitMedicationsListPageURL(rxList);
     cy.injectAxe();
     cy.axeCheck('main');
-    listPage.verifyUnknownRxPhoneNumberOnListPage(
-      unknownRx.data.attributes.dialCmopDivisionPhone,
-    );
+    listPage.verifyMedicationsListPageTitle();
   });
 });
