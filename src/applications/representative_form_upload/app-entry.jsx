438845b3f8904f14d286a4c9e8dd3154b204b88b
Patrick Brown <patrick.brown@oddball.io>
2025-03-08 13:29:15 -0500

    arf/representative-form-upload-step-2

--- src/applications/representative_form_upload/app-entry.jsx
+++ src/applications/representative_form_upload/app-entry.jsx
@@ -1,9 +1,9 @@
 import 'platform/polyfills';
-import '../simple-forms/form-upload/sass/form-upload.scss';
+import './sass/form-upload.scss';
 
 import startApp from 'platform/startup';
-import routes from '../simple-forms/form-upload/routes';
-import reducer from '../simple-forms/form-upload/reducers';
+import routes from './routes';
+import reducer from './reducers';
 import manifest from './manifest.json';
 
 startApp({
