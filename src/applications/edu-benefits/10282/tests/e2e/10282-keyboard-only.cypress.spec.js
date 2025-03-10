3c7f38e314d8cb4d0ecefbbcd51d4574b9aae413
Michael Ghisilieri <175352110+mghisilieri@users.noreply.github.com>
2025-03-10 09:22:04 -0700

    22-10282 keyboard-only tests beforeEach added back

--- src/applications/edu-benefits/10282/tests/e2e/10282-keyboard-only.cypress.spec.js
+++ src/applications/edu-benefits/10282/tests/e2e/10282-keyboard-only.cypress.spec.js
@@ -2,6 +2,10 @@ import maximalData from '../fixtures/data/maximal-test.json';
 import formConfig from '../../config/form';
 
 describe('22-10282 Edu form', () => {
+  beforeEach(function() {
+    if (Cypress.env('CI')) this.skip();
+  });
+
   it('should be keyboard-only navigable', () => {
     // Go to application, should go to intro page
     cy.visit(
@@ -262,6 +266,6 @@ describe('22-10282 Edu form', () => {
     cy.tabToSubmitForm();
 
     // Confirmation page
-    // cy.location('pathname').should('include', '/confirmation');
+    cy.location('pathname').should('include', '/confirmation');
   });
 });
