4a715e9c63e452a3f177f04102e1e597b5ac0449
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-03-10 07:26:21 -0400

    [MHV 66597] add IPE reducer to reducer index.

--- src/applications/mhv-medications/reducers/index.js
+++ src/applications/mhv-medications/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 
 import { prescriptionsReducer } from './prescriptions';
 import { allergiesReducer } from './allergies';
+import { inProductEducationReducer } from './inProductEducation';
 
 const rootReducer = {
   rx: combineReducers({
     prescriptions: prescriptionsReducer,
     // TODO: consider re-using this from medical-records
     allergies: allergiesReducer,
+    inProductEducation: inProductEducationReducer,
   }),
 };
 
