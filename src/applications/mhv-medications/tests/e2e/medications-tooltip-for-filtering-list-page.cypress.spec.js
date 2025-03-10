2a2fb80810f8348e86ae8db60c58ecd17a1a4e88
raji.venkatesh <raji.venkatesh@acommsolutions.com>
2025-03-10 15:23:14 -0500

    e2e tests for tooltip
    added tests to validate tooltip for filtering

---
+++ src/applications/mhv-medications/tests/e2e/medications-tooltip-for-filtering-list-page.cypress.spec.js
@@ -0,0 +1,40 @@
+import MedicationsSite from './med_site/MedicationsSite';
+import rxList from './fixtures/listOfPrescriptions.json';
+import MedicationsListPage from './pages/MedicationsListPage';
+import { Data } from './utils/constants';
+
+describe('Medications List Page ToolTip for Filtering', () => {
+  const listPage = new MedicationsListPage();
+  beforeEach(() => {
+    const site = new MedicationsSite();
+    site.login();
+    listPage.visitMedicationsListPageURL(rxList);
+  });
+  it('visits Medications List ToolTip Filter Collapsed', () => {
+    listPage.verifyToolTipTextOnListPage(Data.TOOL_TIP_TEXT);
+    listPage.verifyFilterCollapsedOnListPage();
+    cy.injectAxe();
+    cy.axeCheck('main');
+  });
+  it('visits Medications List ToolTip Filter Expanded', () => {
+    listPage.clickfilterAccordionDropdownOnListPage();
+    listPage.verifyToolTipTextOnListPage(Data.TOOL_TIP_TEXT);
+    listPage.verifyFilterButtonWhenAccordionExpanded();
+    cy.injectAxe();
+    cy.axeCheck('main');
+  });
+  it('visits Medications List ToolTip close', () => {
+    listPage.verifyToolTipTextOnListPage(Data.TOOL_TIP_TEXT);
+    listPage.clickToolTipCloseButtonOnListPage();
+    listPage.verifyToolTipNotVisibleOnListPage();
+    cy.injectAxe();
+    cy.axeCheck('main');
+  });
+  it('visits Medications List ToolTip Stop Hint Button', () => {
+    listPage.verifyToolTipTextOnListPage(Data.TOOL_TIP_TEXT);
+    listPage.clickStopShowingThisHintLinkOnListPage();
+    listPage.verifyToolTipNotVisibleOnListPage();
+    cy.injectAxe();
+    cy.axeCheck('main');
+  });
+});
