b49abb8342595fce8a20b430e6f74121cf952d0b
Midge-dev <sean.midgley.dev@gmail.com>
2025-03-10 12:29:08 -0700

    Init

--- src/applications/686c-674/tests/config/chapters/veteran-information/veteranInformation.unit.spec.jsx
+++ src/applications/686c-674/tests/config/chapters/veteran-information/veteranInformation.unit.spec.jsx
@@ -8,15 +8,94 @@ import VeteranInformationComponent from '../../../../config/chapters/veteran-inf
 const defaultStore = createCommonStore();
 
 describe('Veteran Information Component', () => {
-  it('Should Render a div with the specific class name', () => {
+  it('Should render a div with the specific class names', () => {
     const { container } = render(
       <Provider store={defaultStore}>
         <VeteranInformationComponent />
       </Provider>,
     );
 
-    const divElement = container.querySelector('.blue-bar-block');
+    const divElement = container.querySelector(
+      '.vads-u-border--1px.vads-u-border-color--gray-medium.vads-u-padding-x--2.vads-u-padding-y--1',
+    );
+
     expect(divElement).to.not.be.null;
+  });
+
+  it('should render empty profile object', () => {
+    const formData = { veteranInformation: { ssnLastFour: '1234' } };
+    const store = {
+      getState: () => ({
+        user: {
+          profile: {},
+        },
+      }),
+      dispatch: () => {},
+      subscribe: () => {},
+    };
+    const { queryByText } = render(
+      <Provider store={store}>
+        <VeteranInformationComponent formData={formData} />
+      </Provider>,
+    );
+
+    expect(queryByText(/Name/)).to.not.be.null;
+  });
+
+  it('should render name and suffix', () => {
+    const formData = { veteranInformation: { ssnLastFour: '1234' } };
+    const store = {
+      getState: () => ({
+        user: {
+          profile: {
+            dob: '1975-11-06T22:21:04.417Z',
+            gender: 'M',
+            userFullName: {
+              first: 'Bob',
+              middle: undefined,
+              last: 'Hope',
+              suffix: 'Senior',
+            },
+          },
+        },
+      }),
+      dispatch: () => {},
+      subscribe: () => {},
+    };
+    const { queryByText } = render(
+      <Provider store={store}>
+        <VeteranInformationComponent formData={formData} />
+      </Provider>,
+    );
+    expect(queryByText(/Name/)).to.not.be.null;
+  });
+
+  it('should render name w/o suffix', () => {
+    const formData = { veteranInformation: { ssnLastFour: '1234' } };
+    const store = {
+      getState: () => ({
+        user: {
+          profile: {
+            dob: '1975-11-06T22:21:04.417Z',
+            gender: 'M',
+            userFullName: {
+              first: 'Bob',
+              middle: undefined,
+              last: 'Hope',
+              suffix: undefined,
+            },
+          },
+        },
+      }),
+      dispatch: () => {},
+      subscribe: () => {},
+    };
+    const { queryByText } = render(
+      <Provider store={store}>
+        <VeteranInformationComponent formData={formData} />
+      </Provider>,
+    );
+    expect(queryByText(/Name/)).to.not.be.null;
   });
 
   it('should render ssn last four', () => {
@@ -44,22 +123,23 @@ describe('Veteran Information Component', () => {
         <VeteranInformationComponent formData={formData} />
       </Provider>,
     );
-    expect(queryByText(/Social Security number/)).to.not.be.null;
+    expect(queryByText(/Last 4 digits of Social Security number/)).to.not.be
+      .null;
   });
 
-  it('should render vaFile last 4', () => {
-    const formData = { veteranInformation: { vaFileLastFour: '1234' } };
+  it('should render dob', () => {
+    const formData = { veteranInformation: { ssnLastFour: '1234' } };
     const store = {
       getState: () => ({
         user: {
           profile: {
-            dob: 'something',
+            dob: '1975-11-06T22:21:04.417Z',
             gender: 'M',
             userFullName: {
               first: 'Bob',
               middle: undefined,
               last: 'Hope',
-              suffix: 'Jr',
+              suffix: undefined,
             },
           },
         },
@@ -72,6 +152,6 @@ describe('Veteran Information Component', () => {
         <VeteranInformationComponent formData={formData} />
       </Provider>,
     );
-    expect(queryByText(/VA file number/)).to.not.be.null;
+    expect(queryByText(/Date of birth/)).to.not.be.null;
   });
 });
