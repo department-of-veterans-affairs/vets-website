202479a5783db5053f3fd673f5902f7bb88dfa78
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-03-11 07:33:50 -0700

    Add context

---
+++ src/applications/travel-pay/context/SmocContext.jsx
@@ -0,0 +1,54 @@
+import React, { createContext, useState } from 'react';
+
+const defaultContext = {
+  pageIndex: 0,
+  setPageIndex: () => {},
+  yesNo: {
+    mileage: '',
+    vehicle: '',
+    address: '',
+  },
+  isUnsupportedClaimType: false,
+  setIsUnsupportedClaimType: () => {},
+  isAgreementChecked: false,
+  setIsAgreementChecked: () => {},
+  isAgreementError: false,
+  setIsAgreementError: () => {},
+};
+
+export const SmocContext = createContext(defaultContext);
+
+const SmocContextProvider = ({ children }) => {
+  const [yesNo, setYesNo] = useState(defaultContext.yesNo);
+  const [pageIndex, setPageIndex] = useState(defaultContext.pageIndex);
+  const [isUnsupportedClaimType, setIsUnsupportedClaimType] = useState(
+    defaultContext.isUnsupportedClaimType,
+  );
+  const [isAgreementChecked, setIsAgreementChecked] = useState(
+    defaultContext.isAgreementChecked,
+  );
+  const [isAgreementError, setIsAgreementError] = useState(
+    defaultContext.isAgreementError,
+  );
+
+  return (
+    <SmocContext.Provider
+      value={{
+        pageIndex,
+        setPageIndex,
+        yesNo,
+        setYesNo,
+        isUnsupportedClaimType,
+        setIsUnsupportedClaimType,
+        isAgreementChecked,
+        setIsAgreementChecked,
+        isAgreementError,
+        setIsAgreementError,
+      }}
+    >
+      {children}
+    </SmocContext.Provider>
+  );
+};
+
+export default SmocContextProvider;
