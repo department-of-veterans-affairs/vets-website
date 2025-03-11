64fbfe1de9fda5d0e099e61bf702968568f13a0d
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2025-03-11 10:25:35 -0700

    missed import

--- src/applications/facility-locator/tests/components/search-form/location/index.unit.spec.jsx
+++ src/applications/facility-locator/tests/components/search-form/location/index.unit.spec.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
-import LocationInput from '../../../../components/search-form/location';
+import AddressAutosuggest from '../../../../components/search-form/location/AddressAutosuggest';
 
 describe('LocationInput', () => {
   describe('when using the address typeahead feature and progressive disclosure', () => {
     it('should correctly render the autosuggest feature instead of the regular location input field', () => {
       const screen = render(
-        <LocationInput
+        <AddressAutosuggest
           currentQuery={{
             locationChanged: false,
             geolocationInProgress: false,
