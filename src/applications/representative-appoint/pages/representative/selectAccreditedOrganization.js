a27e4343037b1d9c2086a2c83030043dfae54f0d
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-03-12 15:35:04 -0400

    Update selectAccreditedOrganization.js

--- src/applications/representative-appoint/pages/representative/selectAccreditedOrganization.js
+++ src/applications/representative-appoint/pages/representative/selectAccreditedOrganization.js
@@ -1,4 +1,5 @@
 import SelectOrganization from '../../components/SelectOrganization';
+import { pageDepends as isDigiSubmission } from './representativeSubmissionMethod';
 
 export const uiSchema = {
   selectedAccreditedOrganizationId: {
@@ -22,12 +23,15 @@ export const schema = {
 };
 
 export const pageDepends = formData => {
+  // in digital submission flow, display even if there's only one org
+  const minimumOrgs = isDigiSubmission() ? 0 : 1;
+
   return (
     !!formData['view:selectedRepresentative'] &&
     ['representative', 'veteran_service_officer'].includes(
       formData['view:selectedRepresentative'].attributes?.individualType,
     ) &&
     formData['view:selectedRepresentative'].attributes?.accreditedOrganizations
-      ?.data?.length > 0
+      ?.data?.length > minimumOrgs
   );
 };
