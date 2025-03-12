fc2e4efa7e883c95d59c69daacb996285e4e4e81
mdewey <1793923+mdewey@users.noreply.github.com>
2025-03-12 14:47:34 -0400

    Rename selectors for clarity: hasEHRM to profileHasEHRM and hasVista to profileHasVista

--- src/applications/mhv-landing-page/tests/selectors/facilities.unit.spec.js
+++ src/applications/mhv-landing-page/tests/selectors/facilities.unit.spec.js
@@ -1,38 +1,38 @@
 import { expect } from 'chai';
-import { hasEHRM, hasVista } from '../../selectors/facilities';
+import { profileHasEHRM, profileHasVista } from '../../selectors/facilities';
 
 describe('facilities selectors', () => {
-  describe('hasEHRM', () => {
+  describe('profileHasEHRM', () => {
     it('should return true if the profile has EHRM facilities', () => {
       const profile = { facilities: [{ isCerner: true }] };
-      expect(hasEHRM({ profile })).to.be.true;
+      expect(profileHasEHRM({ profile })).to.be.true;
     });
 
     it('should return false if the profile has no EHRM facilities', () => {
       const profile = { facilities: [{ isCerner: false }] };
-      expect(hasEHRM({ profile })).to.be.false;
+      expect(profileHasEHRM({ profile })).to.be.false;
     });
 
     it('should return false if the profile is empty', () => {
       const profile = { facilities: [] };
-      expect(hasEHRM({ profile })).to.be.false;
+      expect(profileHasEHRM({ profile })).to.be.false;
     });
   });
 
-  describe('hasVista', () => {
+  describe('profileHasVista', () => {
     it('should return true if the profile has VistA facilities', () => {
       const profile = { facilities: [{ isCerner: false }] };
-      expect(hasVista({ profile })).to.be.true;
+      expect(profileHasVista({ profile })).to.be.true;
     });
 
     it('should return false if the profile has no VistA facilities', () => {
       const profile = { facilities: [{ isCerner: true }] };
-      expect(hasVista({ profile })).to.be.false;
+      expect(profileHasVista({ profile })).to.be.false;
     });
 
     it('should return false if the profile is empty', () => {
       const profile = { facilities: [] };
-      expect(hasVista({ profile })).to.be.false;
+      expect(profileHasVista({ profile })).to.be.false;
     });
   });
 });
