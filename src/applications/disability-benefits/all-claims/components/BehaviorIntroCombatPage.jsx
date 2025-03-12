a17ae9a67ac7087bf30691fdceb0fb0b9555d175
Nathan Burgess <burgess.nathan28@gmail.com>
2025-03-12 16:58:47 -0400

    Working checkboxes

--- src/applications/disability-benefits/all-claims/components/BehaviorIntroCombatPage.jsx
+++ src/applications/disability-benefits/all-claims/components/BehaviorIntroCombatPage.jsx
@@ -1,10 +1,10 @@
 import { VaRadio } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import React from 'react';
+import React, { useState } from 'react';
 
 import FormNavButtons from '~/platform/forms-system/src/js/components/FormNavButtons';
 import { mentalHealthSupportAlert } from '../content/form0781';
 
-const BehaviorIntroCombatPage = ({ goBack }) => {
+const BehaviorIntroCombatPage = ({ goBack, data, setFormData }) => {
   // TODO: MOVE TO CONTENT FILE
   const combatIntroTitle =
     'Do you want to answer the optional questions about behavorial changes?';
@@ -14,6 +14,36 @@ const BehaviorIntroCombatPage = ({ goBack }) => {
 
   const answerCombatQuestionsChoice = 'Yes, I want to answer these questions.';
   const optOutOfCombatQuestionsChoice = 'No, I want to skip these questions.';
+
+  const [optIn, setOptIn] = useState(
+    data?.['view:answerCombatBehaviorQuestions'],
+    null,
+  );
+
+  const handlers = {
+    onSelection: event => {
+      const { selection } = event?.detail || {};
+      if (selection) {
+        setOptIn(selection);
+
+        const formData = {
+          ...data,
+          'view:answerCombatBehaviorQuestions': event.detail?.value,
+        };
+        setFormData(formData);
+        // ?????? setFormData lags a little, so check updated data
+        // checkErrors(formData);
+
+        // Think this is DOMO stuff:
+        // recordEvent({
+        //   event: 'int-radio-button-option-click',
+        //   'radio-button-label': content.label,
+        //   'radio-button-optionLabel': content[`${value}Label`],
+        //   'radio-button-required': false,
+        // });
+      }
+    },
+  };
 
   return (
     <div className="vads-u-margin-y--2">
@@ -35,7 +65,7 @@ const BehaviorIntroCombatPage = ({ goBack }) => {
           label={combatIntroTitle}
           label-header-level={4}
           // error={}
-          onVaValueChange={{}}
+          onVaValueChange={handlers.onSelection}
           required
           // Think we need this but not sure why:
           uswds
@@ -46,7 +76,7 @@ const BehaviorIntroCombatPage = ({ goBack }) => {
             // id={'yes-choice'}
             // These are strings in the existing implementation:
             value={'true'}
-            // checked={true}
+            checked={optIn === 'true'}
             uswds
           />
 
@@ -55,7 +85,7 @@ const BehaviorIntroCombatPage = ({ goBack }) => {
             // id={'no-choice'}
             label={optOutOfCombatQuestionsChoice}
             value={'false'}
-            // checked={true}
+            checked={optIn === 'false'}
             uswds
           />
         </VaRadio>
@@ -66,5 +96,7 @@ const BehaviorIntroCombatPage = ({ goBack }) => {
     </div>
   );
 };
+
+// TODO: PROPS VALIDATION
 
 export default BehaviorIntroCombatPage;
