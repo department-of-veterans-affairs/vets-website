1c6b6d6b9a862a025f435e0ece56b3c1abf967b7
jquispe-oddball <jennifer.quispe@oddball.io>
2025-03-13 16:18:32 -0400

    99582 - feature toggle

--- src/applications/accredited-representative-portal/components/SortForm.jsx
+++ src/applications/accredited-representative-portal/components/SortForm.jsx
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
 import { Form, useNavigate } from 'react-router-dom';
 import { VaSelect } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import { toggleValues } from 'platform/site-wide/feature-toggles/selectors';
+import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
+import { connect } from 'react-redux';
 import api from '../utilities/api';
 
 const SEARCH_PARAMS = {
   STATUS: 'status',
   SORT: 'sort',
 };
-const SortForm = ({ asc, desc, ascOption, descOption }) => {
+
+const mapStateToProps = state => ({
+  accreditedRepresentativePortalSortBy: toggleValues(state)[
+    FEATURE_FLAG_NAMES.accreditedRepresentativePortalSortBy
+  ],
+});
+
+const SortForm = ({
+  asc,
+  desc,
+  ascOption,
+  descOption,
+  accreditedRepresentativePortalSortBy,
+}) => {
   const navigate = useNavigate();
   const url = new URL(window.location);
   const params = new URLSearchParams(url.search);
@@ -30,31 +46,31 @@ const SortForm = ({ asc, desc, ascOption, descOption }) => {
   };
 
   return (
-    <Form
-      id="search-form"
-      role="search"
-      className="poa-request__sort-by vads-u-display--none"
-    >
-      <VaSelect
-        onVaSelect={handleChange}
-        label="Sort by"
-        message-aria-describedby="Sort by date"
-        name="options"
-        className="poa-request__select"
-        value={sortby || asc}
-      >
-        <option value={asc}>{ascOption}</option>
-        <option value={desc}>{descOption}</option>
-      </VaSelect>
-      <button
-        type="button"
-        className="usa-button-secondary poa-request__apply"
-        onClick={handleSorting}
-      >
-        Sort
-      </button>
-    </Form>
+    <>
+      {!accreditedRepresentativePortalSortBy.accreditedRepresentativePortalSortBy && (
+        <Form id="search-form" role="search" className="poa-request__sort-by">
+          <VaSelect
+            onVaSelect={handleChange}
+            label="Sort by"
+            message-aria-describedby="Sort by date"
+            name="options"
+            className="poa-request__select"
+            value={sortby || asc}
+          >
+            <option value={asc}>{ascOption}</option>
+            <option value={desc}>{descOption}</option>
+          </VaSelect>
+          <button
+            type="button"
+            className="usa-button-secondary poa-request__apply"
+            onClick={handleSorting}
+          >
+            Sort
+          </button>
+        </Form>
+      )}
+    </>
   );
 };
 
-export default SortForm;
+export default connect(mapStateToProps)(SortForm);
