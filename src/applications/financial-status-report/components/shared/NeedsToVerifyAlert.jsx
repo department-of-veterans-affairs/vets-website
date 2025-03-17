9c9b74307418deee080eee7edaf650efbbacba12
Aaron Ponce <3916436+amponce@users.noreply.github.com>
2025-03-17 09:39:44 -0700

    Prefill Logic
    Added missing alerts and updated ShowAlertorSip to mirror appeals app.
    
    TODO: Still has a basename error that needs to be fixed.
    
    fix the prefill logic

---
+++ src/applications/financial-status-report/components/shared/NeedsToVerifyAlert.jsx
@@ -0,0 +1,36 @@
+import React, { useRef, useEffect } from 'react';
+
+import { focusElement } from 'platform/utilities/ui';
+import VerifyAlert from 'platform/user/authorization/components/VerifyAlert';
+import recordEvent from 'platform/monitoring/record-event';
+
+export const heading =
+  'You’ll need to verify your identity to access more VA.gov tools and features';
+
+const NeedsToVerify = () => {
+  const alertRef = useRef(null);
+
+  useEffect(
+    () => {
+      if (alertRef?.current) {
+        focusElement(alertRef.current);
+      }
+    },
+    [alertRef],
+  );
+
+  recordEvent({
+    event: 'visible-alert-box',
+    'alert-box-type': 'continue',
+    'alert-box-heading': heading,
+    'error-key': 'not_verified',
+    'alert-box-full-width': false,
+    'alert-box-background-only': false,
+    'alert-box-closeable': false,
+    'reason-for-alert': `Not verified`,
+  });
+
+  return <VerifyAlert headingLevel={2} ref={alertRef} />;
+};
+
+export default NeedsToVerify;
