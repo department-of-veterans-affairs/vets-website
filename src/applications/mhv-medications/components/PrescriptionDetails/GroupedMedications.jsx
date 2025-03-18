22c3b64f089e235f0cb368de954d1bcf606f56d4
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-03-18 10:03:41 -0400

    used isStating function to determine page list size for grouping list.

--- src/applications/mhv-medications/components/PrescriptionDetails/GroupedMedications.jsx
+++ src/applications/mhv-medications/components/PrescriptionDetails/GroupedMedications.jsx
@@ -1,5 +1,6 @@
 import { VaPagination } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { waitForRenderThenFocus } from '@department-of-veterans-affairs/platform-utilities/ui';
+import environment from 'platform/utilities/environment';
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { datadogRum } from '@datadog/browser-rum';
@@ -8,7 +9,7 @@ import { dateFormat, fromToNumbs } from '../../util/helpers';
 import LastFilledInfo from '../shared/LastFilledInfo';
 import { dataDogActionNames } from '../../util/dataDogConstants';
 
-const MAX_PAGE_LIST_LENGTH = 2;
+const MAX_PAGE_LIST_LENGTH = environment.isStaging() ? 2 : 10;
 const MAX_GROUPED_LIST_LENGTH = 26;
 
 const GroupedMedications = props => {
@@ -92,7 +93,7 @@ const GroupedMedications = props => {
         {totalListCount > MAX_PAGE_LIST_LENGTH && (
           <VaPagination
             onPageSelect={e => onPageChange(e.detail.page)}
-            max-page-list-length={3}
+            max-page-list-length={MAX_PAGE_LIST_LENGTH}
             className="vads-u-justify-content--center no-print vads-u-margin-top--3"
             page={currentPage}
             pages={Math.ceil(totalListCount / MAX_PAGE_LIST_LENGTH)}
