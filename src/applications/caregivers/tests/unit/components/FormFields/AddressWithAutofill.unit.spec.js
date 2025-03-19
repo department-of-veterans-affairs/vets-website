f35237c8a4ccc5f13b32a9342984b67022bcc652
Matt Long <6738544+longmd@users.noreply.github.com>
2025-03-19 14:51:22 -0400

    10-10CG | Upgrade address autofill component performance (#35311)

--- src/applications/caregivers/tests/unit/components/FormFields/AddressWithAutofill.unit.spec.js
+++ src/applications/caregivers/tests/unit/components/FormFields/AddressWithAutofill.unit.spec.js
@@ -1,9 +1,9 @@
+import { omit } from 'lodash';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { fireEvent, render, waitFor } from '@testing-library/react';
 import { expect } from 'chai';
 import sinon from 'sinon';
-
 import { inputVaTextInput } from 'platform/testing/unit/helpers';
 import { fullSchema } from '../../../../utils/imports';
 import { addressWithAutofillSchema } from '../../../../definitions/sharedSchema';
@@ -12,13 +12,20 @@ import content from '../../../../locales/en/content.json';
 
 const { address } = fullSchema.definitions;
 
+const errorSchemas = {
+  empty: { __errors: [] },
+  required: { __errors: [content['validation-default-required']] },
+};
+
+const postalCode = { valid: '46220', invalid: '462205678' };
+
 describe('CG <AddressWithAutofill>', () => {
-  const errorSchemas = {
-    empty: { __errors: [] },
-    required: { __errors: [content['validation-default-required']] },
-  };
-  const getData = ({ autofill = false, reviewMode = false }) => ({
-    props: {
+  const subject = ({
+    autofill = false,
+    reviewMode = false,
+    onChange = f => f,
+  } = {}) => {
+    const props = {
       formContext: { reviewMode, submitted: undefined },
       formData: {
         street: '1350 I St. NW',
@@ -46,13 +53,13 @@ describe('CG <AddressWithAutofill>', () => {
         state: { $id: 'root_caregiverAddress_state' },
         street: { $id: 'root_caregiverAddress_street' },
         street2: { $id: 'root_caregiverAddress_street2' },
-        'view:autofill': { $id: 'root_caregiverAddress_view:autofill' },
+        'view:autofill': { $id: 'root_caregiverAddress_autofill' },
       },
       schema: addressWithAutofillSchema(address),
-      onChange: sinon.spy(),
       name: 'caregiverAddress',
-    },
-    mockStore: {
+      onChange,
+    };
+    const mockStore = {
       getState: () => ({
         form: {
           data: {
@@ -68,9 +75,7 @@ describe('CG <AddressWithAutofill>', () => {
       }),
       subscribe: () => {},
       dispatch: () => {},
-    },
-  });
-  const subject = ({ mockStore, props }) => {
+    };
     const { container } = render(
       <Provider store={mockStore}>
         <AddressWithAutofill {...props} />
@@ -80,163 +85,140 @@ describe('CG <AddressWithAutofill>', () => {
     const selectors = () => ({
       fieldset: container.querySelector('.cg-address-with-autofill'),
       inputs: container.querySelectorAll(expectedFieldTypes),
-      reviewRow: container.querySelectorAll('.review-row'),
+      reviewRows: container.querySelectorAll('.review-row'),
       vaCheckbox: container.querySelector('#root_caregiverAddress_autofill'),
       vaTextInput: container.querySelector('#root_caregiverAddress_postalCode'),
       streetInput: container.querySelector('#root_caregiverAddress_street'),
     });
-    return { container, selectors };
+    return { container, selectors, formData: props.formData };
   };
+  let onChangeSpy;
 
-  context('when the component renders on form page', () => {
-    const { mockStore, props } = getData({});
+  beforeEach(() => {
+    onChangeSpy = sinon.spy();
+  });
 
-    it('should render the appropriate number of form fields', () => {
-      const { selectors } = subject({ mockStore, props });
-      expect(selectors().fieldset).to.exist;
-      expect(selectors().fieldset).to.not.be.empty;
-      expect(selectors().inputs).to.have.lengthOf(7);
+  it('should render the form fields when not in review mode', () => {
+    const { selectors } = subject();
+    const { fieldset, inputs, reviewRows } = selectors();
+    expect(fieldset).to.not.be.empty;
+    expect(inputs).to.have.lengthOf(7);
+    expect(reviewRows).to.not.have.length;
+  });
+
+  it('should render the review rows when in review mode', () => {
+    const { selectors } = subject({ reviewMode: true });
+    const { inputs, reviewRows } = selectors();
+    expect(inputs).to.not.have.length;
+    expect(reviewRows).to.have.length;
+  });
+
+  it('should call the `onChange` method with the correct form data when the user clicks the autofill checkbox', async () => {
+    const { selectors, formData } = subject({
+      autofill: true,
+      onChange: onChangeSpy,
     });
-
-    it('should not render the review rows', () => {
-      const { selectors } = subject({ mockStore, props });
-      expect(selectors().reviewRow).to.not.have.length;
+    const { vaCheckbox } = selectors();
+    await waitFor(() => {
+      const expectedOutput = omit(formData, 'county');
+      vaCheckbox.__events.vaChange({ target: { checked: true } });
+      expect(onChangeSpy.calledWith(expectedOutput)).to.be.true;
+    });
+    await waitFor(() => {
+      const expectedOutput = { 'view:autofill': false };
+      vaCheckbox.__events.vaChange({ target: { checked: false } });
+      expect(onChangeSpy.calledWith(expectedOutput)).to.be.true;
     });
   });
 
-  context('when the component renders in review mode', () => {
-    const { mockStore, props } = getData({ reviewMode: true });
+  it('should reset the `view:autofill` datapoint when a change is made to the an autofilled address', () => {
+    const { container, selectors, formData } = subject({
+      autofill: true,
+      onChange: onChangeSpy,
+    });
+    const expectedOutput = {
+      ...formData,
+      postalCode: postalCode.valid,
+      'view:autofill': false,
+    };
+    inputVaTextInput(container, postalCode.valid, selectors().vaTextInput);
+    expect(onChangeSpy.calledWith(expectedOutput)).to.be.true;
+  });
 
-    it('should render the review rows', () => {
-      const { selectors } = subject({ mockStore, props });
-      expect(selectors().reviewRow).to.have.length;
+  it('should call the `onChange` method with the correct form data when a change is made', async () => {
+    const { container, selectors, formData } = subject({
+      onChange: onChangeSpy,
+    });
+    const { vaTextInput } = selectors();
+    await waitFor(() => {
+      const expectedOutput = { ...formData, postalCode: postalCode.valid };
+      inputVaTextInput(container, postalCode.valid, vaTextInput);
+      expect(onChangeSpy.calledWith(expectedOutput)).to.be.true;
+    });
+    await waitFor(() => {
+      fireEvent.blur(vaTextInput);
+      expect(vaTextInput).to.not.have.attr('error');
     });
   });
 
-  context('when the user clicks the autofill checkbox', () => {
-    it('should call the `onChange` method with the correct form data', async () => {
-      const { mockStore, props } = getData({ autofill: true });
-      const { selectors } = subject({ mockStore, props });
-      await waitFor(() => {
-        selectors().vaCheckbox.__events.vaChange({
-          target: { checked: true },
-        });
-        expect(props.onChange.calledWith(props.formData)).to.be.true;
-      });
-      await waitFor(() => {
-        const formData = { 'view:autofill': false };
-        selectors().vaCheckbox.__events.vaChange({
-          target: { checked: false },
-        });
-        expect(props.onChange.calledWith(formData)).to.be.true;
-      });
+  it('should call all `onChange` methods to update the form data in case of autocomplete', async () => {
+    const { container, selectors, formData } = subject({
+      onChange: onChangeSpy,
     });
-  });
-
-  context('when the user makes an update to non-checkbox field', () => {
-    const postalCode = { valid: '46220', invalid: '462205678' };
-
-    it('should reset the `view:autofill` datapoint when a change is made to the an autofilled address', () => {
-      const { mockStore, props } = getData({ autofill: true });
-      const { container, selectors } = subject({ mockStore, props });
-      const formData = {
-        ...props.formData,
+    const { streetInput, vaTextInput } = selectors();
+    await waitFor(() => {
+      const blurEvent = new CustomEvent('blur');
+      const expectedOutput = {
+        ...formData,
         postalCode: postalCode.valid,
-        'view:autofill': false,
+        street: '123 Fake St.',
       };
-      inputVaTextInput(container, formData.postalCode, selectors().vaTextInput);
-      expect(props.onChange.calledWith(formData)).to.be.true;
+      streetInput.value = expectedOutput.street;
+      inputVaTextInput(container, postalCode.valid, vaTextInput);
+
+      vaTextInput.dispatchEvent(blurEvent);
+      expect(onChangeSpy.calledWith(expectedOutput)).to.be.true;
+
+      fireEvent.blur(vaTextInput);
+      expect(vaTextInput).to.not.have.attr('error');
     });
+  });
 
-    it('should call the `onChange` method with the correct form data when a change is made', async () => {
-      const { mockStore, props } = getData({});
-      const { container, selectors } = subject({ mockStore, props });
-      const formData = { ...props.formData, postalCode: postalCode.valid };
-
-      await waitFor(() => {
-        inputVaTextInput(
-          container,
-          formData.postalCode,
-          selectors().vaTextInput,
-        );
-        expect(props.onChange.calledWith(formData)).to.be.true;
-      });
-
-      await waitFor(() => {
-        fireEvent.blur(selectors().vaTextInput);
-        expect(selectors().vaTextInput).to.not.have.attr('error');
-      });
+  it('should render error when `blur` event is fired with invalid data', async () => {
+    const { container, selectors, formData } = subject({
+      onChange: onChangeSpy,
     });
-
-    it('should call all `onChange` methods to update the form data in case of autocomplete', async () => {
-      const { mockStore, props } = getData({});
-      const { container, selectors } = subject({ mockStore, props });
-      const formData = {
-        ...props.formData,
-        postalCode: postalCode.valid,
-        street: '123 Fake st.',
-      };
-
-      await waitFor(() => {
-        selectors().streetInput.value = formData.street;
-        inputVaTextInput(
-          container,
-          formData.postalCode,
-          selectors().vaTextInput,
-        );
-        const blurEvent = new CustomEvent('blur');
-        selectors().vaTextInput.dispatchEvent(blurEvent);
-        expect(props.onChange.calledWith(formData)).to.be.true;
-
-        fireEvent.blur(selectors().vaTextInput);
-        expect(selectors().vaTextInput).to.not.have.attr('error');
-      });
+    const { vaTextInput } = selectors();
+    await waitFor(() => {
+      const expectedOutput = { ...formData, postalCode: '' };
+      inputVaTextInput(container, '', vaTextInput);
+      expect(onChangeSpy.calledWith(expectedOutput)).to.be.true;
     });
-
-    it('should render error when `blur` event is fired with invalid data', async () => {
-      const { mockStore, props } = getData({});
-      const { container, selectors } = subject({ mockStore, props });
-      const formData = { ...props.formData, postalCode: '' };
-
-      await waitFor(() => {
-        inputVaTextInput(
-          container,
-          formData.postalCode,
-          selectors().vaTextInput,
-        );
-        expect(props.onChange.calledWith(formData)).to.be.true;
-      });
-
-      await waitFor(() => {
-        fireEvent.blur(selectors().vaTextInput);
-        expect(selectors().vaTextInput).to.have.attr(
-          'error',
-          content['validation-address--postalCode-required'],
-        );
-      });
+    await waitFor(() => {
+      fireEvent.blur(vaTextInput);
+      expect(vaTextInput).to.have.attr(
+        'error',
+        content['validation-address--postalCode-required'],
+      );
     });
+  });
 
-    it('should render error when form data is invalid with a pattern mismatch', async () => {
-      const { mockStore, props } = getData({});
-      const { container, selectors } = subject({ mockStore, props });
-      const formData = { ...props.formData, postalCode: postalCode.invalid };
-
-      await waitFor(() => {
-        inputVaTextInput(
-          container,
-          formData.postalCode,
-          selectors().vaTextInput,
-        );
-        expect(props.onChange.calledWith(formData)).to.be.true;
-      });
-
-      await waitFor(() => {
-        fireEvent.blur(selectors().vaTextInput);
-        expect(selectors().vaTextInput).to.have.attr(
-          'error',
-          content['validation-address--postalCode-pattern'],
-        );
-      });
+  it('should render error when form data is invalid with a pattern mismatch', async () => {
+    const { container, selectors, formData } = subject({
+      onChange: onChangeSpy,
+    });
+    const { vaTextInput } = selectors();
+    await waitFor(() => {
+      const expectedOutput = { ...formData, postalCode: postalCode.invalid };
+      inputVaTextInput(container, postalCode.invalid, vaTextInput);
+      expect(onChangeSpy.calledWith(expectedOutput)).to.be.true;
+    });
+    await waitFor(() => {
+      fireEvent.blur(vaTextInput);
+      expect(vaTextInput).to.have.attr(
+        'error',
+        content['validation-address--postalCode-pattern'],
+      );
     });
   });
 });
