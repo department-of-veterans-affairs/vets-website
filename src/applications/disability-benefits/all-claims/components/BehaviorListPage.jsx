91fd93dce3eccc5e81cf2c3d8fbbe554b89e0a88
Alison Jones <1816769+ajones446@users.noreply.github.com>
2025-03-19 15:37:18 -0500

    Fill in behavior list content

--- src/applications/disability-benefits/all-claims/components/BehaviorListPage.jsx
+++ src/applications/disability-benefits/all-claims/components/BehaviorListPage.jsx
@@ -26,7 +26,12 @@ import {
   BEHAVIOR_CHANGES_WORK,
   BEHAVIOR_CHANGES_HEALTH,
   BEHAVIOR_CHANGES_OTHER,
+  BEHAVIOR_LIST_HINTS,
 } from '../constants';
+import {
+  form0781HeadingTag,
+  mentalHealthSupportAlert,
+} from '../content/form0781';
 
 const BehaviorListPage = ({ goBack }) => {
   return (
@@ -34,12 +39,120 @@ const BehaviorListPage = ({ goBack }) => {
       <div className="vads-u-margin-y--2">
         <>
           <h3 className="vads-u-font-family--sans vads-u-font-size--base vads-u-font-weight--normal vads-u-margin--0">
-            VA FORM 21-0781
+            {form0781HeadingTag}
           </h3>
           <h3 className="vads-u-font-size--h3 vads-u-color--base vads-u-margin--0">
-            CUSTOM FORM! Types of behavioral changes. CUSTOM FORM!! ~~
+            {behaviorListPageTitle}
           </h3>
         </>
+        {behaviorListDescription}
+
+        <VaCheckboxGroup
+          label={BEHAVIOR_LIST_SECTION_SUBTITLES.work}
+          label-header-level={4}
+          name="workBehaviors"
+          hint={BEHAVIOR_LIST_HINTS.work}
+          // onVaChange={handlers.onIssueChange}
+          // onBlur={handlers.onBlur}
+          // error={showError('issues')}
+          // required
+          uswds
+        >
+          {Object.entries(BEHAVIOR_CHANGES_WORK).map(([key, value, index]) => (
+            <va-checkbox
+              key={key}
+              name="workBehaviors"
+              // class="dd-privacy-hidden"
+              // data-dd-action-name="issue name"
+              label={value}
+              value={key}
+              checked={false}
+              // TODO checked={(formData?.workBehaviors || []).includes(behavior)}
+              uswds
+            />
+          ))}
+        </VaCheckboxGroup>
+        <VaCheckboxGroup
+          label={BEHAVIOR_LIST_SECTION_SUBTITLES.health}
+          label-header-level={4}
+          name="healthBehaviors"
+          hint={BEHAVIOR_LIST_HINTS.health}
+          // onVaChange={handlers.onIssueChange}
+          // onBlur={handlers.onBlur}
+          // error={showError('issues')}
+          // required
+          uswds
+        >
+          {Object.entries(BEHAVIOR_CHANGES_HEALTH).map(
+            ([key, value, index]) => (
+              <va-checkbox
+                key={key}
+                name="healthBehaviors"
+                // class="dd-privacy-hidden"
+                // data-dd-action-name="issue name"
+                label={value}
+                value={key}
+                checked={false}
+                // TODO checked={(formData?.workBehaviors || []).includes(behavior)}
+                uswds
+              />
+            ),
+          )}
+        </VaCheckboxGroup>
+        <VaCheckboxGroup
+          label={BEHAVIOR_LIST_SECTION_SUBTITLES.other}
+          label-header-level={4}
+          name="otherBehaviors"
+          form-heading-level={4}
+          hint={BEHAVIOR_LIST_HINTS.other}
+          // onVaChange={handlers.onIssueChange}
+          // onBlur={handlers.onBlur}
+          // error={showError('issues')}
+          // required
+          uswds
+        >
+          {Object.entries(BEHAVIOR_CHANGES_OTHER).map(
+            ([key, value, index]) => (
+              <va-checkbox
+                key={key}
+                name="otherBehaviors"
+                // class="dd-privacy-hidden"
+                // data-dd-action-name="issue name"
+                label={value}
+                value={key}
+                checked={false}
+                // TODO checked={(formData?.workBehaviors || []).includes(behavior)}
+                uswds
+              />
+            ),
+          )}
+        </VaCheckboxGroup>
+        <VaCheckboxGroup
+          label={BEHAVIOR_LIST_SECTION_SUBTITLES.none}
+          label-header-level={4}
+          name="noBehaviors"
+          hint={BEHAVIOR_LIST_HINTS.none}
+          // onVaChange={handlers.onIssueChange}
+          // onBlur={handlers.onBlur}
+          // error={showError('issues')}
+          // required
+          uswds
+        >
+          <va-checkbox
+            key="TODOkey"
+            name="noBehaviorChanges"
+            // class="dd-privacy-hidden"
+            // data-dd-action-name="issue name"
+            label={behaviorListNoneLabel}
+            value="none"
+            checked={false}
+            // TODO checked={(formData?.workBehaviors || []).includes(behavior)}
+            uswds
+          />
+        </VaCheckboxGroup>
+
+        {behaviorListAdditionalInformation}
+        <>{mentalHealthSupportAlert()}</>
       </div>
       <FormNavButtons goBack={goBack} goForward={() => { }} />
     </>
