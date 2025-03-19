e8bab02f1b70d549c8c4ed20ce0d2a156da0a831
Vitalii Menshutin <133991282+vmenshutin-bylight@users.noreply.github.com>
2025-03-19 15:26:18 -0400

    MHV-68045: Refill update content changes (#35287)
    * MHV-68045: Refill update content changes
    
    * Updated the unit test
    
    * fixed typo
    
    * fixed another typo
    
    * e2e updates for refill process
    
    updates assertions for refill process
    
    * e2e updates for refill process
    
    updated and added e2e tests for refill process updates
    
    * e2e updates for active refill in process
    
    added e2e updates for refill in process
    
    ---------
    
    Co-authored-by: RajiVenkatesh18 <114511630+RajiVenkatesh18@users.noreply.github.com>
    Co-authored-by: raji.venkatesh <raji.venkatesh@acommsolutions.com>

--- src/applications/mhv-medications/tests/e2e/pages/MedicationsListPage.js
+++ src/applications/mhv-medications/tests/e2e/pages/MedicationsListPage.js
@@ -954,6 +954,27 @@ class MedicationsListPage {
   verifyFilterAccordionDropDownIsFocused = () => {
     cy.get('[data-testid="rx-filter"]').should('have.focus');
   };
+
+  updatedRefillDates = data => {
+    const currentDate = new Date();
+    return {
+      ...data,
+      data: data.data.map(item => {
+        const newRefillDate = new Date(currentDate);
+        newRefillDate.setDate(currentDate.getDate() + 6);
+        return {
+          ...item,
+          attributes: {
+            ...item.attributes,
+            refillDate:
+              item.attributes.refillDate != null
+                ? newRefillDate.toISOString()
+                : null,
+          },
+        };
+      }),
+    };
+  };
 }
 
 export default MedicationsListPage;
