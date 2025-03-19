9649fb89ed92f83cc34bd29ef10e0c072b199b69
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-03-19 10:55:04 -0400

    Added VerifyAlert for LOA1 state

--- src/applications/static-pages/representative-status/components/App/index.jsx
+++ src/applications/static-pages/representative-status/components/App/index.jsx
@@ -1,43 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+
 import { toggleLoginModal as toggleLoginModalAction } from '@department-of-veterans-affairs/platform-site-wide/actions';
 import {
   isLOA1,
+  isLOA3,
+  isLoggedIn,
   selectProfile,
 } from '@department-of-veterans-affairs/platform-user/selectors';
-import { Auth } from '../States/Auth';
+
+import VerifyAlert from 'platform/user/authorization/components/VerifyAlert';
+
 import { Unauth } from '../States/Unauth';
+import { Auth } from '../States/Auth';
 import { useRepresentativeStatus } from '../../hooks/useRepresentativeStatus';
 
-export const App = ({ baseHeader, toggleLoginModal, isLoggedIn }) => {
+export const App = ({
+  baseHeader,
+  toggleLoginModal,
+  isUserLOA1,
+  isUserLOA3,
+  loggedIn,
+}) => {
   const DynamicHeader = `h${baseHeader}`;
   const DynamicSubheader = `h${baseHeader + 1}`;
 
-  const loggedIn = isLoggedIn;
-
   return (
     <>
-      {loggedIn ? (
-        <div
-          aria-live="polite"
-          aria-atomic
-          tabIndex="-1"
-          className="poa-display"
-        >
-          <Auth
-            DynamicHeader={DynamicHeader}
-            DynamicSubheader={DynamicSubheader}
-            useRepresentativeStatus={useRepresentativeStatus}
-          />
-        </div>
-      ) : (
-        <>
-          <Unauth
-            toggleLoginModal={toggleLoginModal}
-            headingLevel={baseHeader}
-          />
-        </>
+      {!loggedIn && (
+        <Unauth toggleLoginModal={toggleLoginModal} headingLevel={baseHeader} />
+      )}
+      {isUserLOA1 && <VerifyAlert />}
+
+      {isUserLOA3 && (
+        <Auth
+          DynamicHeader={DynamicHeader}
+          DynamicSubheader={DynamicSubheader}
+          useRepresentativeStatus={useRepresentativeStatus}
+        />
       )}
     </>
   );
@@ -47,13 +48,16 @@ App.propTypes = {
   toggleLoginModal: PropTypes.func.isRequired,
   baseHeader: PropTypes.number,
   hasRepresentative: PropTypes.bool,
-  isLoggedIn: PropTypes.bool,
+  isUserLOA1: PropTypes.bool,
+  isUserLOA3: PropTypes.bool,
+  loggedIn: PropTypes.bool,
 };
 
 const mapStateToProps = state => ({
   hasRepresentative: state?.user?.login?.hasRepresentative || null,
-  isLoggedIn: state?.user?.login?.currentlyLoggedIn || false,
+  loggedIn: isLoggedIn(state),
   isUserLOA1: isLOA1(state),
+  isUserLOA3: isLOA3(state),
   signInServiceName: selectProfile(state).signIn?.serviceName,
 });
 
