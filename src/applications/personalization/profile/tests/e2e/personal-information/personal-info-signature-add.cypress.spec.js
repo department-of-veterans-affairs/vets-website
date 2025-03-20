df5782df40212b811f8e8098ad03f7fb4db31086
fazilqa <127263470+fazilqa@users.noreply.github.com>
2025-03-20 11:11:50 -0700

    add constants.js

--- src/applications/personalization/profile/tests/e2e/personal-information/personal-info-signature-add.cypress.spec.js
+++ src/applications/personalization/profile/tests/e2e/personal-information/personal-info-signature-add.cypress.spec.js
@@ -1,5 +1,6 @@
 import PersonalInformationPage from '../pages/PersonalInformationPage';
 import mockSignature from '../../fixtures/personal-information-signature.json';
+import { Locators, Data, Paths } from '../../fixtures/constants';
 
 describe('PERSONAL INFORMATION ADD SIGNATURE', () => {
   beforeEach(() => {
@@ -26,45 +27,43 @@ describe('PERSONAL INFORMATION ADD SIGNATURE', () => {
   });
 
   it('verify user can cancel adding signature', () => {
-    cy.get(`#edit-messages-signature`).click();
-    cy.get(`#root_signatureName-label`)
+    cy.get(Locators.SIGNATURE.EDIT_BTN).click();
+    cy.get(Locators.SIGNATURE.NAME_LABEL)
       .should('be.visible')
       .and('contain.text', `(*Required)`);
-    cy.get(`#root_signatureTitle-label`)
+    cy.get(Locators.SIGNATURE.TITLE_LABEL)
       .should('be.visible')
       .and('contain.text', `(*Required)`);
 
-    cy.get(`[data-testid="cancel-edit-button"]`).click();
-    cy.get(`#edit-messages-signature`).should(`be.focused`);
+    cy.get(Locators.SIGNATURE.CANCEL_BTN).click();
+    cy.get(Locators.SIGNATURE.EDIT_BTN).should(`be.focused`);
 
     cy.injectAxeThenAxeCheck();
   });
 
   it(`verify user can add and save signature`, () => {
-    cy.get(`#edit-messages-signature`).click();
-    cy.get(`#root_signatureName`)
+    cy.get(Locators.SIGNATURE.EDIT_BTN).click();
+    cy.get(Locators.SIGNATURE.NAME_FIELD)
       .should(`be.focused`)
       .type('Name');
-    cy.get(`#root_signatureTitle`).type('TestTitle');
+    cy.get(Locators.SIGNATURE.TITLE_FIELD).type('TestTitle');
 
-    cy.intercept(
-      `POST`,
-      `/my_health/v1/messaging/preferences/signature`,
-      mockSignature,
-    ).as('updatedSignature');
+    cy.intercept(`POST`, Paths.INTERCEPT.SIGNATURE, mockSignature).as(
+      'updatedSignature',
+    );
 
-    cy.get(`[data-testid="save-edit-button"]`).click();
+    cy.get(Locators.SIGNATURE.SAVE_BTN).click();
 
-    cy.get(`[data-testid="messagingSignature"]`).should(
+    cy.get(Locators.SIGNATURE.GENERAL).should(
       `contain.text`,
       `${mockSignature.data.attributes.signatureName +
         mockSignature.data.attributes.signatureTitle}`,
     );
 
-    cy.get(`#messagingSignature-alert`)
+    cy.get(Locators.SIGNATURE.ALERT)
       .should(`be.visible`)
-      .and('have.text', `Update saved.`);
-    cy.get(`#edit-messages-signature`).should(`be.focused`);
+      .and('have.text', Data.SIGNATURE.UDATE_SAVED);
+    cy.get(Locators.SIGNATURE.EDIT_BTN).should(`be.focused`);
 
     cy.injectAxeThenAxeCheck();
   });
---
+++ src/applications/personalization/profile/tests/fixtures/constants.js
@@ -0,0 +1,25 @@
+export const Locators = {
+  SIGNATURE: {
+    GENERAL: `[data-testid="messagingSignature"]`,
+    EDIT_BTN: `#edit-messages-signature`,
+    CANCEL_BTN: `[data-testid="cancel-edit-button"]`,
+    SAVE_BTN: `[data-testid="save-edit-button"]`,
+    NAME_LABEL: `#root_signatureName-label`,
+    TITLE_LABEL: `#root_signatureTitle-label`,
+    NAME_FIELD: `#root_signatureName`,
+    TITLE_FIELD: `#root_signatureTitle`,
+    ALERT: `#messagingSignature-alert`,
+  },
+};
+
+export const Data = {
+  SIGNATURE: {
+    UDATE_SAVED: `Update saved.`,
+  },
+};
+
+export const Paths = {
+  INTERCEPT: {
+    SIGNATURE: `/my_health/v1/messaging/preferences/signature`,
+  },
+};
