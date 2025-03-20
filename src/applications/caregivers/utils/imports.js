65456f8f30d95e5a754176a4d7944f52fcd21d9f
Matt Long <6738544+longmd@users.noreply.github.com>
2025-03-20 13:54:25 -0400

    Fix react binding exports

--- src/applications/caregivers/utils/imports.js
+++ src/applications/caregivers/utils/imports.js
@@ -1,9 +1,25 @@
 import FULL_SCHEMA from 'vets-json-schema/dist/10-10CG-schema.json';
 import SCHEMA_CONSTANTS from 'vets-json-schema/dist/constants.json';
 import CAREGIVER_FACILITIES from 'vets-json-schema/dist/caregiverProgramFacilities.json';
-import REACT_BINDINGS from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
+import {
+  VaCheckbox,
+  VaRadio,
+  VaRadioOption,
+  VaSearchInput,
+  VaSelect,
+  VaTextInput,
+} from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 
 export const STATES_USA = SCHEMA_CONSTANTS.states.USA;
 
-export { CONTACTS, FULL_SCHEMA, REACT_BINDINGS, CAREGIVER_FACILITIES };
+export const REACT_BINDINGS = {
+  VaCheckbox,
+  VaRadio,
+  VaRadioOption,
+  VaSearchInput,
+  VaSelect,
+  VaTextInput,
+};
+
+export { CONTACTS, FULL_SCHEMA, CAREGIVER_FACILITIES };
