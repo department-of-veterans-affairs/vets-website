31f70f9dde1a7d593d70b08e67cfa93721ec4869
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-03-21 11:04:09 -0400

    Disable camelcase in entire file

--- src/applications/static-pages/representative-status/utilities/representativeTypeMap.js
+++ src/applications/static-pages/representative-status/utilities/representativeTypeMap.js
@@ -1,15 +1,12 @@
+/* eslint-disable camelcase */
+
 export const representativeTypeMap = input => {
   const mapping = {
     attorney: 'accredited attorney',
-    /* eslint-disable-next-line camelcase */
     claims_agent: 'accredited claims agent',
-    /* eslint-disable-next-line camelcase */
     claim_agents: 'accredited claims agent',
-    /* eslint-disable-next-line camelcase */
     representative: 'Veteran Service Organization (VSO)',
-    /* eslint-disable-next-line camelcase */
     veteran_service_officer: 'Veteran Service Organization (VSO)',
-    /* eslint-disable-next-line camelcase */
     organization: 'Veteran Service Organization (VSO)',
   };
 
