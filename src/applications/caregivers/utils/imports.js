5081dc5e72fcd83a63b810e75447af26fd996c91
Matt Long <6738544+longmd@users.noreply.github.com>
2025-03-22 10:47:47 -0400

    Dry up contacts import/export

--- src/applications/caregivers/utils/imports.js
+++ src/applications/caregivers/utils/imports.js
@@ -1,7 +1,6 @@
 import FULL_SCHEMA from 'vets-json-schema/dist/10-10CG-schema.json';
 import SCHEMA_CONSTANTS from 'vets-json-schema/dist/constants.json';
 import CAREGIVER_FACILITIES from 'vets-json-schema/dist/caregiverProgramFacilities.json';
-import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
 import {
   VaCheckbox,
   VaRadio,
@@ -10,6 +9,10 @@ import {
   VaSelect,
   VaTextInput,
 } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+
+export {
+  CONTACTS,
+} from '@department-of-veterans-affairs/component-library/contacts';
 
 export const STATES_USA = SCHEMA_CONSTANTS.states.USA;
 
@@ -22,4 +25,4 @@ export const REACT_BINDINGS = {
   VaTextInput,
 };
 
-export { CONTACTS, FULL_SCHEMA, CAREGIVER_FACILITIES };
+export { FULL_SCHEMA, CAREGIVER_FACILITIES };
