ae3bba839f5f3abafc955e5b57bd2c8b3bf84448
Napoleon K. <95312667+GcioNapoleon@users.noreply.github.com>
2025-03-24 15:13:44 -0500

    Vebt 10216 cypress test (#35422)
    * vebt-10216-cypress-test all cypress test are now passing
    
    * remove console
    
    * refactor url constant into a seperate file
    
    * vebt resolve warnings in the constants file
    
    * replace inline data with json object in fixtures folder
    
    * vebt-10216-cypress-test improve test coverage for CustomReviewField function.
    
    * vebt-10216-cypress-test get all 10216 test  back above 80%.

--- src/applications/edu-benefits/10216/tests/config/form.unit.spec.jsx
+++ src/applications/edu-benefits/10216/tests/config/form.unit.spec.jsx
@@ -1,10 +1,19 @@
 import { expect } from 'chai';
-import formConfig from '../../config/form';
+import formConfig, { subTitle, submitFormLogic } from '../../config/form';
 import manifest from '../../manifest.json';
+// import {subTitle} from "../../../../appeals/996/content/title";
 
 describe('22-10216 Form Config', () => {
   it('should render', () => {
     expect(formConfig).to.be.an('object');
+  });
+  it('should test subTitle function', () => {
+    const mySubTitle = subTitle();
+    expect(mySubTitle).to.not.be.undefined;
+  });
+  it('should test submitFormLogic function', () => {
+    const submitForm = submitFormLogic();
+    expect(submitForm).to.not.be.undefined;
   });
   it('should have a required properties', () => {
     expect(formConfig.rootUrl).to.contain(manifest.rootUrl);
