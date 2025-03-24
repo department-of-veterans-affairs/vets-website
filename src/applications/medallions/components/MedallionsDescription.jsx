33efc9f5341acdc465c09a77bd2a429b99eee3ef
122034971+ConnorJeff@users.noreply.github.com <122034971+ConnorJeff@users.noreply.github.com>
2025-03-24 10:25:32 -0400

    Optimized the medallions description

--- src/applications/medallions/components/MedallionsDescription.jsx
+++ src/applications/medallions/components/MedallionsDescription.jsx
@@ -1,14 +1,25 @@
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setData } from 'platform/forms-system/src/js/actions';
 
 export const MedallionsDescription = (formContext, description = '') => {
   const dispatch = useDispatch();
   const { isLoggedIn } = formContext.formContext;
-  const updatedFormData = {
-    ...formContext.formData,
-    isLoggedIn,
-  };
 
-  dispatch(setData(updatedFormData));
+  useEffect(
+    () => {
+      if (formContext.formData.isLoggedIn !== isLoggedIn) {
+        dispatch(
+          setData({
+            ...formContext.formData,
+            isLoggedIn,
+          }),
+        );
+      }
+      // Only run the effect when these things change
+    },
+    [dispatch, formContext.formData, isLoggedIn],
+  );
+
   return description;
 };
