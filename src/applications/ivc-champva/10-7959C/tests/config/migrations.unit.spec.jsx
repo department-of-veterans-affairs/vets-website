c7f6719256693c0a4693cc21746394949e26e170
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-03-24 16:27:07 -0500

    migration for new key changes

---
+++ src/applications/ivc-champva/10-7959C/tests/config/migrations.unit.spec.jsx
@@ -0,0 +1,90 @@
+import { expect } from 'chai';
+import { migrateCardUploadKeys } from '../../config/migrations';
+
+const EXAMPLE_SIP_METADATA = {
+  version: 0,
+  prefill: true,
+  returnUrl: '/form-signature',
+};
+
+describe('migrateCardUploadKeys ', () => {
+  it('should create a front and back property when `applicantMedicarePartAPartBCard` is present', () => {
+    const props = {
+      formData: {
+        applicantMedicarePartAPartBCard: [
+          {
+            name: 'front.png',
+            confirmationCode: 'a49136ca-377c-4b7e-b7ee-e7320ed2ca0b',
+            attachmentId: 'Front of insurance card',
+            isEncrypted: false,
+          },
+          {
+            name: 'back.png',
+            confirmationCode: 'b49136ca-377c-4b7e-b7ee-e7320ed2ca0b',
+            attachmentId: 'Back of insurance card',
+            isEncrypted: false,
+          },
+        ],
+      },
+      metadata: EXAMPLE_SIP_METADATA,
+      formId: '10-7959c',
+    };
+    // Deep copying since migration operates on input data
+    const propsDeepCopy = JSON.parse(JSON.stringify(props));
+    const migrated = migrateCardUploadKeys(propsDeepCopy);
+    expect(
+      migrated.formData.applicantMedicarePartAPartBCardFront,
+    ).to.exist.and.to.have.lengthOf(1);
+    expect(
+      migrated.formData.applicantMedicarePartAPartBCardBack,
+    ).to.exist.and.to.have.lengthOf(1);
+  });
+  it('should not modify form data when card uploads are not present', () => {
+    const props = {
+      formData: {
+        applicantName: { first: 'John', last: 'Lastname' },
+      },
+      metadata: EXAMPLE_SIP_METADATA,
+      formId: '10-7959c',
+    };
+    // Deep copying since migration operates on input data
+    const propsDeepCopy = JSON.parse(JSON.stringify(props));
+    const migrated = migrateCardUploadKeys(propsDeepCopy);
+    expect(JSON.stringify(migrated.formData)).to.eq(
+      JSON.stringify(props.formData),
+    );
+  });
+  it('should remove all old keynames', () => {
+    const props = {
+      formData: {
+        applicantName: { first: 'John', last: 'Lastname' },
+        applicantMedicarePartAPartBCard: [
+          {
+            name: 'front.png',
+            confirmationCode: 'a49136ca-377c-4b7e-b7ee-e7320ed2ca0b',
+            attachmentId: 'Front of insurance card',
+            isEncrypted: false,
+          },
+        ],
+        primaryInsuranceCard: [
+          {
+            name: 'back.png',
+            confirmationCode: 'b49136ca-377c-4b7e-b7ee-e7320ed2ca0b',
+            attachmentId: 'Back of insurance card',
+            isEncrypted: false,
+          },
+        ],
+      },
+      metadata: EXAMPLE_SIP_METADATA,
+      formId: '10-7959c',
+    };
+    // Deep copying since migration operates on input data
+    const propsDeepCopy = JSON.parse(JSON.stringify(props));
+    const migrated = migrateCardUploadKeys(propsDeepCopy);
+    expect(migrated.formData.applicantMedicarePartAPartBCard).to.be.undefined;
+    expect(migrated.formData.applicantMedicarePartAPartBCardFront).not.to.be
+      .undefined;
+    expect(migrated.formData.primaryInsuranceCard).to.be.undefined;
+    expect(migrated.formData.primaryInsuranceCardBack).not.to.be.undefined;
+  });
+});
