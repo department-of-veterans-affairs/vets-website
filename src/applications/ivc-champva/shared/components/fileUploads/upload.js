cc534448b7de2d8fc8443fef8f719b34017ae93e
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-03-25 11:01:47 -0500

    reverting removal of max/minsize since it's in another PR

--- src/applications/ivc-champva/shared/components/fileUploads/upload.js
+++ src/applications/ivc-champva/shared/components/fileUploads/upload.js
@@ -2,7 +2,7 @@ import environment from 'platform/utilities/environment';
 import fileUploadUI from 'platform/forms-system/src/js/definitions/file';
 import { focusElement } from 'platform/utilities/ui';
 import VaSelectField from 'platform/forms-system/src/js/web-component-fields/VaSelectField';
-import { fileTypes } from './attachments';
+import { fileTypes, maxSize, minSize } from './attachments';
 
 // Modified version of the file upload from applications/appeals/995
 
@@ -50,6 +50,8 @@ export const fileUploadUi = content => {
     addAnotherLabel,
     buttonText: content.buttonText || 'Upload file',
     fileTypes,
+    maxSize,
+    minSize,
     createPayload,
     parseResponse: (response, file) => {
       setTimeout(() => {
