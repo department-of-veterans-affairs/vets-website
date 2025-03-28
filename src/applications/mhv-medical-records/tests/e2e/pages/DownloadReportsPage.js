370e9478680e84ea0a7652a4259c366bd44ece23
Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>
2025-03-28 12:41:21 -0700

    Update DownloadReportsPage.js

--- src/applications/mhv-medical-records/tests/e2e/pages/DownloadReportsPage.js
+++ src/applications/mhv-medical-records/tests/e2e/pages/DownloadReportsPage.js
@@ -21,19 +21,37 @@ class DownloadReportsPage {
 
   clickCcdDownloadXmlFileButton = (
     ccdGenerateResponse,
-    ccdDownloadResponse,
+    // ccdDownloadResponse,
   ) => {
     cy.intercept(
       'GET',
       '/my_health/v1/medical_records/ccd/generate',
       ccdGenerateResponse,
     ).as('ccdGenerateResponse');
+    // cy.intercept(
+    //   'GET',
+    //   // '/my_health/v1/medical_records/ccd/download?2025-03-19T15:39:37.000-0400',
+    //   '/my_health/v1/medical_records/ccd/download?date=2024-11-20T11:57:07.000-0500', // '/my_health/v1/medical_records/ccd/download**',
+    //   ccdDownloadResponse,
+    // ).as('ccdDownloadResponse');
+
+    // cy.intercept(
+    //   'GET',
+    //   // '/my_health/v1/medical_records/ccd/download?2025-03-19T15:39:37.000-0400',
+    //   '/my_health/v1/medical_records/ccd/download?date=2025-03-19T15:39:37.000-0400', // '/my_health/v1/medical_records/ccd/download**',
+    //   {
+    //     // header: 'application/xml',
+    //     body: 'testabcd1234',
+    //   },
+    // ).as('ccdDownloadResponse');
+
     cy.intercept(
       'GET',
-      '/my_health/v1/medical_records/ccd/download?2025-03-19T15:39:37.000-0400',
-      // '/my_health/v1/medical_records/ccd/download?date=2024-11-20T11:57:07.000-0500', // '/my_health/v1/medical_records/ccd/download**',
-      ccdDownloadResponse,
+      // '/my_health/v1/medical_records/ccd/download?2025-03-19T15:39:37.000-0400',
+      '/my_health/v1/medical_records/ccd/download?date=2025-03-19T15:39:37.000-0400', // '/my_health/v1/medical_records/ccd/download**',
+      'abcd 1234',
     ).as('ccdDownloadResponse');
+
     cy.get('[data-testid="generateCcdButton"]').click();
     cy.wait('@ccdGenerateResponse');
     cy.wait('@ccdDownloadResponse');
