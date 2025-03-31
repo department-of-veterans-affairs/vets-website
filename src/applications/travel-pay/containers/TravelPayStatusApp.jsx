dbc3cc703488b36fd289da02ab4f3bf66e21325a
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-03-31 11:45:44 -0700

    Use va-telephone

--- src/applications/travel-pay/containers/TravelPayStatusApp.jsx
+++ src/applications/travel-pay/containers/TravelPayStatusApp.jsx
@@ -346,9 +346,11 @@ export default function TravelPayStatusApp({ children }) {
                   >
                     <div>
                       <p className="vads-u-margin-top--0">
-                        You can call the BTSSS call center at 855-574-7292 (TTY:
-                        711) Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
-                        Have your claim number ready to share when you call.
+                        You can call the BTSSS call center at{' '}
+                        <va-telephone contact="8555747292" /> (
+                        <va-telephone tty contact="711" />) Monday through
+                        Friday, 8:00 a.m. to 8:00 p.m. ET. Have your claim
+                        number ready to share when you call.
                       </p>
                       <va-link
                         data-testid="status-explainer-link"
