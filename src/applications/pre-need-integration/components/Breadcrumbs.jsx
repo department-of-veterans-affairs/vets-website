7e785c7a5855ccfbd08ea83d96c4c8ac53df5e11
Kenny Santiago <50601724+ksantiagoBAH@users.noreply.github.com>
2025-03-31 10:36:48 -0500

    MBMS-88859 Added breadcrumbs to preneed int

---
+++ src/applications/pre-need-integration/components/Breadcrumbs.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { VaBreadcrumbs } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+
+export const wrapWithBreadcrumb = component => (
+  <>
+    <div className="row">
+      <VaBreadcrumbs
+        uswds
+        label="Breadcrumb"
+        breadcrumbList={[
+          { href: '/', label: 'Home' },
+          {
+            label: 'VA burial benefits and memorial items',
+            href: '/burials-memorials',
+          },
+          {
+            label: 'Apply for pre-need eligibility determination',
+            href: '/burials-and-memorials/pre-need-integration/introduction',
+          },
+        ]}
+      />
+    </div>
+    {component}
+  </>
+);
