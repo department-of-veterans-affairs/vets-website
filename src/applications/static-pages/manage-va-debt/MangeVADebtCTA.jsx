9c4f1150a0298e2f854244fa654ac9d07c325d3b
Derek Dyer <4297274+digitaldrk@users.noreply.github.com>
2025-04-01 09:10:48 -0400

    Updates

--- src/applications/static-pages/manage-va-debt/MangeVADebtCTA.jsx
+++ src/applications/static-pages/manage-va-debt/MangeVADebtCTA.jsx
@@ -7,28 +7,25 @@ const breadcrumbs = [
   { href: '/', label: 'Home' },
   { href: '/manage-va-debt', label: 'Manage your VA debt' },
 ];
-
 const bcString = JSON.stringify(breadcrumbs);
 
 const ManageVADebtCTA = () => (
   <>
     <va-breadcrumbs breadcrumb-list={bcString} />
+    {/* This page is only for local development purposes, on staging/prod
+     this page is rendered from the content-build in drupal. */}
     <h1>Manage your VA debt for benefit overpayments and copay bills</h1>
-
     <p>
       Review your current VA benefit debt or copay bill balances online. And
       find out how to make payments or request help now.
     </p>
-
     <h2>Review your benefit debt and copay bills online</h2>
     <p>
       <a target="_self" href={cdpUrl} className="vads-c-action-link--green">
         Manage your VA debt
       </a>
     </p>
-
     <hr />
-
     <h2>Request help with VA debt (VA Form 5655)</h2>
     <p>
       <a target="_self" href={fsrUrl} className="vads-c-action-link--green">
