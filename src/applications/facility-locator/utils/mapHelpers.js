e27afce7c1392fd1ee7b3012815e8e5a6a329f0d
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-04-01 10:13:34 -0500

    Catching up with main

--- src/applications/facility-locator/utils/mapHelpers.js
+++ src/applications/facility-locator/utils/mapHelpers.js
@@ -6,8 +6,6 @@ import {
   isPostcode,
 } from 'platform/utilities/facilities-and-mapbox';
 
-import { BOUNDING_RADIUS } from '../constants';
-
 const mbxClient = mbxGeo(mapboxClient);
 
 /** ****************************************************
@@ -97,33 +95,6 @@ export const reverseGeocodeBox = (
 ) => {
   const { lon, lat } = getBoxCenter(bounds);
   return reverseGeocode(lon, lat, types.split(','));
-};
-
-/**
- * Generates search criteria from lat/long geocoordinates.
- */
-export const searchCriteraFromCoords = async (longitude, latitude) => {
-  const response = await mbxClient
-    .reverseGeocode({
-      query: [longitude, latitude],
-      types: ['address'],
-    })
-    .send();
-
-  const { features } = response.body;
-  const placeName = features[0].place_name;
-  const coordinates = features[0].center;
-
-  return {
-    bounds: features[0].bbox || [
-      coordinates[0] - BOUNDING_RADIUS,
-      coordinates[1] - BOUNDING_RADIUS,
-      coordinates[0] + BOUNDING_RADIUS,
-      coordinates[1] + BOUNDING_RADIUS,
-    ],
-    searchString: placeName,
-    position: { longitude, latitude },
-  };
 };
 
 export const searchAddresses = async addressTerm => {
