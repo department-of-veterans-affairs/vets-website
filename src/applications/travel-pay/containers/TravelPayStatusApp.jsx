e183f43746805471fc49a0a932f3f6dcba63bdd3
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-04-01 07:35:37 -0700

    Put status page changes behind feature flag

--- src/applications/travel-pay/containers/TravelPayStatusApp.jsx
+++ src/applications/travel-pay/containers/TravelPayStatusApp.jsx
@@ -16,6 +16,7 @@ import { scrollTo } from 'platform/utilities/ui/scroll';
 import Breadcrumbs from '../components/Breadcrumbs';
 import TravelClaimCard from '../components/TravelClaimCard';
 import TravelPayClaimFilters from '../components/TravelPayClaimFilters';
+import { HelpTextManage } from '../components/HelpText';
 import { getTravelClaims } from '../redux/actions';
 import { getDateFilters } from '../util/dates';
 import ErrorAlert from '../components/alerts/ErrorAlert';
@@ -163,6 +164,9 @@ export default function TravelPayStatusApp({ children }) {
   const canViewClaimDetails = useToggleValue(
     TOGGLE_NAMES.travelPayViewClaimDetails,
   );
+  const smocEnabled = useToggleValue(
+    TOGGLE_NAMES.travelPaySubmitMileageExpense,
+  );
 
   useEffect(
     () => {
@@ -293,38 +297,68 @@ export default function TravelPayStatusApp({ children }) {
       <article className="usa-grid-full vads-u-padding-bottom--0">
         <Breadcrumbs />
         <h1 tabIndex="-1" data-testid="header">
-          Travel reimbursement claims
+          {smocEnabled
+            ? 'Travel reimbursement claims'
+            : 'Check your travel reimbursement claim status'}
         </h1>
         <div className="vads-l-col--12 medium-screen:vads-l-col--8">
-          <p className="vads-u-font-family--serif vads-u-font-size--lg">
-            File new claims for travel reimbursement and review the status of
-            all your travel claims.
-          </p>
-          <h2 className="vads-u-margin-top--2">
-            File a new claim for travel reimbursement online
-          </h2>
-          <p>
-            If you’re claiming mileage only, you can file a travel claim for
-            eligible past appointments here on VA.gov.
-          </p>
-          <va-link-action
-            href="/my-health/appointments/past"
-            text="Go to your past appointments"
-            class="vads-u-margin-y--1"
-          />
-          <p>
-            <strong>
-              If you need to submit receipts for other expenses, like tolls,
-              meals, or lodging
-            </strong>
-            , you can file your travel claim through the{' '}
-            <va-link
-              external
-              href={BTSSS_PORTAL_URL}
-              text="Beneficiary Travel Self-Service System"
-            />
-            .
-          </p>
+          {smocEnabled ? (
+            <>
+              <p className="vads-u-font-family--serif vads-u-font-size--lg">
+                File new claims for travel reimbursement and review the status
+                of all your travel claims.
+              </p>
+              <h2 className="vads-u-margin-top--2">
+                File a new claim for travel reimbursement online
+              </h2>
+              <p>
+                If you’re claiming mileage only, you can file a travel claim for
+                eligible past appointments here on VA.gov.
+              </p>
+              <va-link-action
+                href="/my-health/appointments/past"
+                text="Go to your past appointments"
+                class="vads-u-margin-y--1"
+              />
+              <p>
+                <strong>
+                  If you need to submit receipts for other expenses, like tolls,
+                  meals, or lodging
+                </strong>
+                , you can file your travel claim through the{' '}
+                <va-link
+                  external
+                  href={BTSSS_PORTAL_URL}
+                  text="Beneficiary Travel Self-Service System"
+                />
+                .
+              </p>
+            </>
+          ) : (
+            <>
+              <h2 className="vads-u-font-size--h4">
+                You can use this tool to check the status of your VA travel
+                claims.
+              </h2>
+              {!error &&
+                !isLoading && (
+                  <va-additional-info
+                    class="vads-u-margin-y--3"
+                    trigger="How to manage your claims or get more information"
+                  >
+                    <>
+                      <HelpTextManage />
+                      <va-link
+                        data-testid="status-explainer-link"
+                        href="/my-health/travel-pay/help"
+                        text="What does my claim status mean?"
+                      />
+                    </>
+                  </va-additional-info>
+                )}
+            </>
+          )}
+
           {isLoading && (
             <va-loading-indicator
               label="Loading"
@@ -340,25 +374,27 @@ export default function TravelPayStatusApp({ children }) {
                     This list shows all the appointments you've filed a travel
                     claim for.
                   </p>
-                  <va-additional-info
-                    class="vads-u-margin-y--3"
-                    trigger="How to manage your claims or get more information"
-                  >
-                    <div>
-                      <p className="vads-u-margin-top--0">
-                        You can call the BTSSS call center at{' '}
-                        <va-telephone contact="8555747292" /> (
-                        <va-telephone tty contact="711" />) Monday through
-                        Friday, 8:00 a.m. to 8:00 p.m. ET. Have your claim
-                        number ready to share when you call.
-                      </p>
-                      <va-link
-                        data-testid="status-explainer-link"
-                        href="/my-health/travel-pay/help"
-                        text="What does my claim status mean?"
-                      />
-                    </div>
-                  </va-additional-info>
+                  {smocEnabled && (
+                    <va-additional-info
+                      class="vads-u-margin-y--3"
+                      trigger="How to manage your claims or get more information"
+                    >
+                      <div>
+                        <p className="vads-u-margin-top--0">
+                          You can call the BTSSS call center at{' '}
+                          <va-telephone contact="8555747292" /> (
+                          <va-telephone tty contact="711" />) Monday through
+                          Friday, 8:00 a.m. to 8:00 p.m. ET. Have your claim
+                          number ready to share when you call.
+                        </p>
+                        <va-link
+                          data-testid="status-explainer-link"
+                          href="/my-health/travel-pay/help"
+                          text="What does my claim status mean?"
+                        />
+                      </div>
+                    </va-additional-info>
+                  )}
                   <label
                     htmlFor="claimsOrder"
                     className="vads-u-margin-bottom--0 vads-u-margin-top--0"
