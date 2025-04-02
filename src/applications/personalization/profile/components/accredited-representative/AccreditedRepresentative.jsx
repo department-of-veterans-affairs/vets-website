f018b42f541ab75c8de9247a722337ea080b3220
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-04-02 11:38:29 -0400

    Refactored accredited rep component, removed norep component

--- src/applications/personalization/profile/components/accredited-representative/AccreditedRepresentative.jsx
+++ src/applications/personalization/profile/components/accredited-representative/AccreditedRepresentative.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
+
+import environment from '@department-of-veterans-affairs/platform-utilities/environment';
+
 import { useStore, connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import repStatusLoader from 'platform/user/widgets/representative-status';
 
-import NoRepresentative from './NoRepresentative';
-
 const AccreditedRepresentative = ({ powerOfAttorney }) => {
+  const { BASE_URL } = environment;
   const hasRepresentative = !!powerOfAttorney?.data?.id;
   const store = useStore();
 
@@ -19,15 +21,47 @@ const AccreditedRepresentative = ({ powerOfAttorney }) => {
     [hasRepresentative],
   );
 
+  const renderHasRepresentative = () => {
+    return (
+      <>
+        <div data-widget-type="representative-status" />
+        <h3>How to replace your current representative</h3>
+        <p>
+          If you appoint a new accredited representative, they will replace your
+          current one.
+        </p>
+        <va-link
+          class="home__link"
+          href={`${BASE_URL}/get-help-from-accredited-representative`}
+          text="Find and appoint a new accredited representative or VSO"
+        />
+      </>
+    );
+  };
+
+  const renderNoRepresentative = () => {
+    return (
+      <>
+        <p>You don’t have an accredited representative.</p>
+        <p>
+          An accredited attorney, claims agent, or Veterans Service Organization
+          (VSO) representative can help you file a claim or request a decision
+          review.
+        </p>
+        <va-link
+          class="home__link"
+          href={`${BASE_URL}/get-help-from-accredited-representative`}
+          text="Get help from an accredited representative"
+        />
+      </>
+    );
+  };
+
   return (
     <>
       <h2>Accredited Representative or VSO</h2>
 
-      {hasRepresentative ? (
-        <div data-widget-type="representative-status" />
-      ) : (
-        <NoRepresentative />
-      )}
+      {hasRepresentative ? renderHasRepresentative() : renderNoRepresentative()}
     </>
   );
 };
