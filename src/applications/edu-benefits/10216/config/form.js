a2de255462227fd91fa688bb4d73d3cac0511674
govcioChad <162637891+govcioChad@users.noreply.github.com>
2025-04-07 09:37:41 -0700

    #vebt-1457 #comment Updated save in progress messages for 22-10216 form (#35707)

--- src/applications/edu-benefits/10216/config/form.js
+++ src/applications/edu-benefits/10216/config/form.js
@@ -52,23 +52,26 @@ const formConfig = {
   formId: '22-10216',
   saveInProgress: {
     messages: {
-      inProgress:
-        'Your education benefits application (22-10216) is in progress.',
+      inProgress: 'Your form (22-10216) is in progress.',
       expired:
-        'Your saved education benefits application (22-10216) has expired. If you want to apply for education benefits, please start a new application.',
-      saved: 'Your education benefits application has been saved.',
+        'Your saved form (22-10216) has expired. Please start a new form.',
+      saved: 'Your form has been saved.',
     },
   },
   customText: {
     reviewPageTitle: 'Review',
     submitButtonText: 'Continue',
+    appSavedSuccessfullyMessage: 'We’ve saved your form.',
+    appType: 'form',
+    continueAppButtonText: 'Continue your form',
+    finishAppLaterMessage: 'Finish this form later',
+    startNewAppButtonText: 'Start a new form',
   },
   version: 0,
   prefillEnabled: true,
   savedFormMessages: {
-    notFound: 'Please start over to apply for education benefits.',
-    noAuth:
-      'Please sign in again to continue your application for education benefits.',
+    notFound: 'Please start over.',
+    noAuth: 'Please sign in again to continue your form.',
   },
   title: 'Request exemption from the 85/15 Rule reporting requirements',
   subTitle,
