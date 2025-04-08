725f93b068f2c95518455e1063408fb2e3c27b54
Colin <143013011+cosu419@users.noreply.github.com>
2025-04-08 08:05:30 -0700

    Profile tab - Accredited Representative (#35634)
    * Added accredited representative to constants
    
    * Added nav route for accredited representative
    
    * Added HubCard for accredited representative
    
    * Added power of attorney actions and reducers
    
    * Added AccreditedRepresentative and NoRepresentative components
    
    * Fixture for POA success
    
    * Added poa to mock server
    
    * Added unit tests for poa actions
    
    * Added feature toggle for profile POA work
    
    * Refactored accredited rep component, removed norep component
    
    * Repaired imports
    
    * Added initial unit tests for AccreditedRepresentative component
    
    * Moved hub card behind feature toggle
    
    * Added fetchPowerOfAttorney to failing tests
    
    * Conditionally pushing route depending on feature toggle
    
    * Fixed unit test
    
    * Enabled feature toggle in e2e test
    
    * Refactored rep status feature toggle
    
    * Removing path based on feature toggle

--- src/applications/personalization/profile/mocks/server.js
+++ src/applications/personalization/profile/mocks/server.js
@@ -20,6 +20,7 @@ const {
 const { generateFeatureToggles } = require('./endpoints/feature-toggles');
 const mockDisabilityCompensations = require('./endpoints/disability-compensations');
 const directDeposits = require('./endpoints/direct-deposits');
+const powerOfAttorney = require('./endpoints/power-of-attorney');
 const bankAccounts = require('./endpoints/bank-accounts');
 const serviceHistory = require('./endpoints/service-history');
 const vetVerificationStatus = require('./endpoints/vet-verification-status');
@@ -94,6 +95,7 @@ const responses = {
           generateFeatureToggles({
             authExpVbaDowntimeMessage: false,
             profileHideDirectDeposit: false,
+            representativeStatusEnableV2Features: true,
             profileShowCredentialRetirementMessaging: true,
             profileShowPaymentsNotificationSetting: true,
             profileShowNewBenefitOverpaymentDebtNotificationSetting: false,
@@ -215,6 +217,13 @@ const responses = {
       //   res
       //     .status(422)
       //     .json(directDeposits.updates.errors.paymentRestrictionsPresent),
+      secondsOfDelay,
+    );
+  },
+  'GET /v0/representation_management/v0/power_of_attorney': (_req, res) => {
+    const secondsOfDelay = 2;
+    delaySingleResponse(
+      () => res.status(200).json(powerOfAttorney.organization),
       secondsOfDelay,
     );
   },
