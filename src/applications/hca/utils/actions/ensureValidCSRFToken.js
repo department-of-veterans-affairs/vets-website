bb49426b3d74380bc02cff9986d97e6b343e53ed
Matt Long <6738544+longmd@users.noreply.github.com>
2025-04-08 11:11:09 -0400

    10-10EZ | Clean up unused/unnecessary logging (#35729)

--- src/applications/hca/utils/actions/ensureValidCSRFToken.js
+++ src/applications/hca/utils/actions/ensureValidCSRFToken.js
@@ -1,47 +1,31 @@
 import * as Sentry from '@sentry/browser';
 import { apiRequest } from 'platform/utilities/api';
-import recordEvent from 'platform/monitoring/record-event';
-import environment from 'platform/utilities/environment';
+import { API_ENDPOINTS } from '../constants';
 
 const fetchNewCSRFToken = async methodName => {
   const message = `No csrfToken when making ${methodName} call.`;
-  const url = '/v0/maintenance_windows';
-  recordEvent({
-    event: 'hca-10-10ez-fetch-csrf-token-empty',
-  });
+  const url = API_ENDPOINTS.csrfCheck;
 
-  Sentry.withScope(scope => {
-    scope.setLevel(Sentry.Severity.Log);
-    Sentry.captureMessage(`${message} Calling ${url} to generate new one.`);
-  });
-
-  return apiRequest(`${environment.API_URL}${url}`, { method: 'HEAD' })
-    .then(() => {
-      Sentry.withScope(scope => {
-        scope.setLevel(Sentry.Severity.Log);
-        Sentry.captureMessage(
-          `${message} ${url} successfully called to generate token.`,
-        );
-      });
-    })
-    .catch(error => {
-      Sentry.withScope(scope => {
-        scope.setLevel(Sentry.Severity.Log);
-        scope.setExtra('error', error);
-        Sentry.captureMessage(
-          `${message} ${url} failed when called to generate token.`,
-        );
-      });
+  try {
+    await apiRequest(url, { method: 'HEAD' });
+    Sentry.withScope(scope => {
+      scope.setLevel(Sentry.Severity.Log);
+      Sentry.captureMessage(
+        `${message} ${url} successfully called to generate token.`,
+      );
     });
+  } catch (error) {
+    Sentry.withScope(scope => {
+      scope.setLevel(Sentry.Severity.Log);
+      scope.setExtra('error', error);
+      Sentry.captureMessage(
+        `${message} ${url} failed when called to generate token.`,
+      );
+    });
+  }
 };
 
 export const ensureValidCSRFToken = async methodName => {
   const csrfToken = localStorage.getItem('csrfToken');
-  if (!csrfToken) {
-    await fetchNewCSRFToken(methodName);
-  } else {
-    recordEvent({
-      event: 'hca-10-10ez-fetch-csrf-token-present',
-    });
-  }
+  if (!csrfToken) await fetchNewCSRFToken(methodName);
 };
