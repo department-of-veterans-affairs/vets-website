ffa8b17e15ceb914e541bb93c0dd0145db4af75b
Matt Long <6738544+longmd@users.noreply.github.com>
2025-04-09 07:49:12 -0400

    Update CSRF action

--- src/applications/caregivers/actions/ensureValidCSRFToken.js
+++ src/applications/caregivers/actions/ensureValidCSRFToken.js
@@ -2,16 +2,22 @@ import recordEvent from 'platform/monitoring/record-event';
 import { apiRequest } from 'platform/utilities/api';
 import { API_ENDPOINTS } from '../utils/constants';
 
-const fetchNewCSRFToken = async () => {
+const fetchNewCSRFToken = async methodName => {
   try {
     await apiRequest(API_ENDPOINTS.csrfCheck, { method: 'HEAD' });
-    recordEvent({ event: 'caregivers-csrf-token-fetch--success' });
+    recordEvent({
+      event: 'caregivers-csrf-token-fetch--success',
+      method: methodName,
+    });
   } catch (error) {
-    recordEvent({ event: 'caregivers-csrf-token-fetch--failure' });
+    recordEvent({
+      event: 'caregivers-csrf-token-fetch--failure',
+      method: methodName,
+    });
   }
 };
 
-export const ensureValidCSRFToken = async () => {
+export const ensureValidCSRFToken = async methodName => {
   const csrfToken = localStorage.getItem('csrfToken');
-  if (!csrfToken) await fetchNewCSRFToken();
+  if (!csrfToken) await fetchNewCSRFToken(methodName);
 };
--- src/applications/caregivers/tests/unit/actions/ensureValidCSRFToken.unit.spec.jsx
+++ src/applications/caregivers/tests/unit/actions/ensureValidCSRFToken.unit.spec.jsx
@@ -26,7 +26,7 @@ describe('CG ensureValidCSRFToken action', () => {
   it('should not make request to refresh csrfToken when token exists', async () => {
     localStorage.setItem('csrfToken', 'my-token');
 
-    await ensureValidCSRFToken();
+    await ensureValidCSRFToken('myMethod');
     await waitFor(() => {
       expect(apiRequestStub.called).to.be.false;
       expect(recordEventStub.called).to.be.false;
@@ -34,27 +34,35 @@ describe('CG ensureValidCSRFToken action', () => {
   });
 
   it('should successfully make `HEAD` request to refresh csrfToken when no token exists', async () => {
-    const event = 'caregivers-csrf-token-fetch--success';
+    const event = {
+      event: 'caregivers-csrf-token-fetch--success',
+      method: 'myMethod',
+    };
+
     apiRequestStub.onFirstCall().resolves({ meta: {} });
 
-    await ensureValidCSRFToken();
+    await ensureValidCSRFToken(event.method);
     await waitFor(() => {
       expect(apiRequestStub.firstCall.args[0]).to.equal(url);
       expect(apiRequestStub.callCount).to.equal(1);
       expect(recordEventStub.callCount).to.equal(1);
-      expect(recordEventStub.calledWith({ event })).to.be.true;
+      expect(recordEventStub.calledWith(event)).to.be.true;
     });
   });
 
   it('should return error when request to refresh csrfToken fails', async () => {
-    const event = 'caregivers-csrf-token-fetch--failure';
+    const event = {
+      event: 'caregivers-csrf-token-fetch--failure',
+      method: 'myMethod',
+    };
+
     apiRequestStub.onFirstCall().rejects({ bad: 'some error' });
 
-    await ensureValidCSRFToken();
+    await ensureValidCSRFToken(event.method);
     await waitFor(() => {
       expect(apiRequestStub.callCount).to.equal(1);
       expect(recordEventStub.callCount).to.equal(1);
-      expect(recordEventStub.calledWith({ event })).to.be.true;
+      expect(recordEventStub.calledWith(event)).to.be.true;
     });
   });
 });
