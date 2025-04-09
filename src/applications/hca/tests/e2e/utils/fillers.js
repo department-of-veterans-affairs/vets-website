6b1b1a94c754259a8a4e8ac151bc9c81591d5a44
Brandon Cooper <brandon.cooper@adhocteam.us>
2025-04-09 13:28:50 -0400

    update fillers logic for view data in form

--- src/applications/hca/tests/e2e/utils/fillers.js
+++ src/applications/hca/tests/e2e/utils/fillers.js
@@ -42,9 +42,15 @@ export const fillDependentBasicInformation = testData => {
 };
 
 export const fillDependentIncome = testData => {
-  cy.fill('[name="root_view:grossIncome_grossIncome"]', testData.grossIncome);
-  cy.fill('[name="root_view:netIncome_netIncome"]', testData.netIncome);
-  cy.fill('[name="root_view:otherIncome_otherIncome"]', testData.otherIncome);
+  cy.fill(
+    '[name="root_view:grossIncome_grossIncome"]',
+    testData['view:grossIncome'],
+  );
+  cy.fill('[name="root_view:netIncome_netIncome"]', testData['view:netIncome']);
+  cy.fill(
+    '[name="root_view:otherIncome_otherIncome"]',
+    testData['view:otherIncome'],
+  );
 };
 
 export const fillSpousalBasicInformation = testData => {
