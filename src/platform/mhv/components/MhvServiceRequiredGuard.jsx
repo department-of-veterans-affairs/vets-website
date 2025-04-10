c6d342fec1b05cb627a6652e04ebe15ecde8011d
Alex Morgun <87077843+oleksii-morgun@users.noreply.github.com>
2025-04-10 15:52:28 -0400

    enhance service guard logic

--- src/platform/mhv/components/MhvServiceRequiredGuard.jsx
+++ src/platform/mhv/components/MhvServiceRequiredGuard.jsx
@@ -4,25 +4,41 @@ import PropTypes from 'prop-types';
 const MhvServiceRequiredGuard = ({ children, serviceRequired, user }) => {
   const userServices = user.profile.services; // mhv_messaging_policy.rb defines if messaging service is avaialble when a user is in Premium status upon structuring user services from the user profile in services.rb
 
-  const hasRequiredService = serviceRequired.some(service =>
-    userServices.includes(service),
-  );
+  const [hasRequiredService, setHasRequiredService] = useState(null);
+  const [isVerified, setIsVerified] = useState(null);
   const [authorized, setAuthorized] = useState(false);
 
   useEffect(
     () => {
-      if (!user.profile.verified || !hasRequiredService) {
-        window.location.replace('/my-health');
-      } else {
-        setAuthorized(true);
+      setHasRequiredService(
+        serviceRequired.some(service => userServices.includes(service)),
+      );
+      setIsVerified(user.profile.verified);
+    },
+    [serviceRequired, userServices, user.profile.verified],
+  );
+
+  useEffect(
+    () => {
+      if (isVerified !== null && hasRequiredService !== null) {
+        if (!isVerified || !hasRequiredService) {
+          window.location.replace('/my-health');
+        } else {
+          setAuthorized(true);
+        }
       }
     },
-    [hasRequiredService, user, userServices],
+    [isVerified, hasRequiredService],
   );
+
+  if (isVerified === null || hasRequiredService === null) {
+    return null; // Do not render children or redirect until values are fetched
+  }
 
   if (!authorized) {
     return null;
   }
+
   return <>{children}</>;
 };
 
