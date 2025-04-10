d45bc0120dbebb0112f8a5f8baee55dacc646c7e
Kolby Balch <108146636+KolbyVA@users.noreply.github.com>
2025-04-10 13:02:38 -0500

    mhv-67417 added sort to allergies and conditions (#35345)
    * mhv-67417 added sort to allergies and conditions
    
    * mhv-67417 fixed record list rerender issue
    
    * mhv-67417 added unit test
    
    * update allergiest e2e test, add feature toggle
    
    * mhv-67417 fixed allergies downloads
    
    * mhv-67417 fixed sorting in chrome
    
    * mhv-67417 new blank sort behavior
    
    * auto-test: sort allergies, verify order
    
    * Verify conditions sort - E2E test
    
    * Update medical-records-view-allergies-list.cypress.spec.js
    
    * add select sort function for E2E test
    
    * mhv-67417 added required label tag
    
    * mhv-67417 fixed unit tests
    
    * Update allergies E2E tests (after sorting added)
    
    * mhv-67417 fixed e2e tests
    
    * mhv-67417 fixed another e2e test
    
    * mhv-67417 fixed another e2e test
    
    ---------
    
    Co-authored-by: Timothy Steele <107265904+timothy-steele-va@users.noreply.github.com>

--- src/applications/mhv-medical-records/util/constants.js
+++ src/applications/mhv-medical-records/util/constants.js
@@ -519,3 +519,20 @@ export const CernerAlertContent = {
     pageName: 'vitals',
   },
 };
+
+export const SortTypes = {
+  ALPHABETICAL: {
+    value: 'alphatetically',
+    label: 'alphabetically',
+  },
+  ASC_DATE: {
+    value: 'ascDate',
+    label: 'newest to oldest',
+    labelWithDateEntered: 'newest to oldest (date entered)',
+  },
+  DSC_DATE: {
+    value: 'dscDate',
+    label: 'oldest to newest',
+    labelWithDateEntered: 'oldest to newest (date entered)',
+  },
+};
