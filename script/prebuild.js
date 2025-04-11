912eea1aed7eba6b8edfbe70dde3896ca55741a6
Ray Messina <rmessina10@gmail.com>
2025-04-11 14:38:59 -0500

    attempt at revert es module import;  use require resolve

--- script/prebuild.js
+++ script/prebuild.js
@@ -1,27 +1,29 @@
-import { compare } from 'semver';
-import { readFileSync, existsSync, linkSync } from 'fs';
-import { join } from 'path';
+const pathToSemver = require.resolve('semver');
+// eslint-disable-next-line import/no-dynamic-require
+const semver = require(pathToSemver);
+const fs = require('fs');
+const path = require('path');
 
 // Debug: Verify where semver is resolved from
 // eslint-disable-next-line no-console
 console.log('Resolved semver from:', require.resolve('semver'));
 
-const nodeVersion = join(__dirname, '../.nvmrc');
-const minimumNodeVersion = readFileSync(nodeVersion).toString();
+const nodeVersion = path.join(__dirname, '../.nvmrc');
+const minimumNodeVersion = fs.readFileSync(nodeVersion).toString();
 
 if (process.env.INSTALL_HOOKS !== 'no') {
   // Make sure git pre-commit hooks are installed
   ['pre-commit'].forEach(hook => {
-    const src = join(__dirname, `hooks/${hook}`);
-    const dest = join(__dirname, `../.git/hooks/${hook}`);
-    if (existsSync(src) && !existsSync(dest)) {
+    const src = path.join(__dirname, `hooks/${hook}`);
+    const dest = path.join(__dirname, `../.git/hooks/${hook}`);
+    if (fs.existsSync(src) && !fs.existsSync(dest)) {
       // Install hooks
-      linkSync(src, dest);
+      fs.linkSync(src, dest);
     }
   });
 }
 
-if (compare(process.version, minimumNodeVersion) === -1) {
+if (semver.compare(process.version, minimumNodeVersion) === -1) {
   process.stdout.write(`Node.js version (minimum): v${minimumNodeVersion}\n`);
   process.stdout.write(`Node.js version (installed): ${process.version}\n`);
   process.exit(1);
