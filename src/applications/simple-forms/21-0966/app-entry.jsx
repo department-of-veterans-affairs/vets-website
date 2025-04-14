db50b111aab18a4ad34689f223be6a9bdf7e7a58
Robert Hasselle <robert.hasselle@oddball.io>
2025-04-14 09:50:39 -0500

    ci test

--- src/applications/simple-forms/21-0966/app-entry.jsx
+++ src/applications/simple-forms/21-0966/app-entry.jsx
@@ -2,7 +2,6 @@ import 'platform/polyfills';
 import './sass/21-0966-intent-to-file-a-claim.scss';
 
 import startApp from 'platform/startup';
-
 import routes from './routes';
 import reducer from './reducers';
 import manifest from './manifest.json';
