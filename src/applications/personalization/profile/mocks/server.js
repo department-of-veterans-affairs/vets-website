f4d191f8cd42981244ca6b9a12490048f2f22970
Nick Sayre <nick.sayre@adhocteam.us>
2025-04-14 14:22:46 -0500

    1575 - veteran status datadog monitoring for PDF generation failures

--- src/applications/personalization/profile/mocks/server.js
+++ src/applications/personalization/profile/mocks/server.js
@@ -108,6 +108,7 @@ const responses = {
             profileShowNoValidationKeyAddressAlert: false,
             profileUseExperimental: false,
             profileShowPrivacyPolicy: true,
+            vetStatusPdfLogging: true,
             veteranStatusCardUseLighthouse: true,
             veteranStatusCardUseLighthouseFrontend: true,
           }),
