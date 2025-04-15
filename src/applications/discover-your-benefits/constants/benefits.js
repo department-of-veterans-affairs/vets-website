f6f3a52ab1f46831fbb953ae959bd82526b86b26
macalemid <mahmed@ironarchtech.com>
2025-04-15 14:04:00 -0400

    Change link from 'how-to-file-a-claim' to 'how-to-file-claim'

--- src/applications/discover-your-benefits/constants/benefits.js
+++ src/applications/discover-your-benefits/constants/benefits.js
@@ -635,7 +635,7 @@ export const BENEFITS_LIST = [
       [mappingTypes.DISABILITY_RATING]: [anyType.ANY],
     },
     learnMoreURL: 'https://www.va.gov/disability/',
-    applyNowURL: 'https://www.va.gov/disability/how-to-file-a-claim',
+    applyNowURL: 'https://www.va.gov/disability/how-to-file-claim',
   },
   {
     name: 'VA-backed home loans',
