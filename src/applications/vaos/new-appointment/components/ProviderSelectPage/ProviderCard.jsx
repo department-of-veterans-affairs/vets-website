3745b79b8e3987efd427a24807a54430d022a2c1
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2025-04-14 19:10:52 -0700

    Adds state and flow update for OH selected provider
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>

--- src/applications/vaos/new-appointment/components/ProviderSelectPage/ProviderCard.jsx
+++ src/applications/vaos/new-appointment/components/ProviderSelectPage/ProviderCard.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { format } from 'date-fns';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { VaLink } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import {
+  routeToNextAppointmentPage,
+  startDirectScheduleFlow,
+  updateSelectedProvider,
+} from '../../redux/actions';
+import { getFormData } from '../../redux/selectors';
+
+const pageKey = 'selectProvider';
+
+function handleClick({ history, dispatch, data, provider }) {
+  return () => {
+    dispatch(startDirectScheduleFlow({ isRecordEvent: false }));
+    dispatch(routeToNextAppointmentPage(history, pageKey, data));
+    dispatch(updateSelectedProvider(provider));
+  };
+}
 
 export default function ProviderCard({ provider }) {
   const { lastSeen, providerName, hasAvailability } = provider;
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const data = useSelector(getFormData);
+
   return (
     <div>
       <h2 className="vads-u-font-size--h3 vads-u-margin-bottom--0 vads-u-margin-top--2">
@@ -23,11 +46,17 @@ export default function ProviderCard({ provider }) {
       )}
 
       {hasAvailability && (
-        <va-link
+        <VaLink
           active
           data-testid="choose-date-time"
           href="#"
           text="Choose your preferred date and time"
+          onClick={handleClick({
+            history,
+            dispatch,
+            data,
+            provider,
+          })}
         />
       )}
 
