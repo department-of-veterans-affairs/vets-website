3751a390b3861b401311166e0977ab1a1839f704
ediiotero <e.otero81@gmail.com>
2025-04-15 18:40:19 -0400

    block routes before toggle is set

--- src/applications/ask-va/containers/App.jsx
+++ src/applications/ask-va/containers/App.jsx
@@ -1,14 +1,15 @@
 import { useFeatureToggle } from 'platform/utilities/feature-toggles';
 import cookie from 'js-cookie';
-import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
-import PropTypes from 'prop-types';
+// import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+// import PropTypes from 'prop-types';
 import React from 'react';
 import { VaAlert } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import BreadCrumbs from '../components/BreadCrumbs';
-import ProgressBar from '../components/ProgressBar';
-import formConfig from '../config/form';
+// import BreadCrumbs from '../components/BreadCrumbs';
+// import ProgressBar from '../components/ProgressBar';
+// import formConfig from '../config/form';
 
-export default function App({ location, children }) {
+// export default function App({ location, children }) {
+export default function App() {
   const {
     TOGGLE_NAMES,
     useToggleLoadingValue,
@@ -20,12 +21,12 @@ export default function App({ location, children }) {
     cookie.get('askVaCanaryReleaseOverride') === 'true';
 
   const toggleName = TOGGLE_NAMES.askVaCanaryRelease;
-  const toggleOldPortalAlert = TOGGLE_NAMES.askVaAlertLinkToOldPortal;
+  // const toggleOldPortalAlert = TOGGLE_NAMES.askVaAlertLinkToOldPortal;
   const isCanaryEnabled = useToggleValue(toggleName);
-  const isOldPortalAlertEnabled = useToggleValue(toggleOldPortalAlert);
+  // const isOldPortalAlertEnabled = useToggleValue(toggleOldPortalAlert);
   const isLoadingFeatureFlags = useToggleLoadingValue(toggleName);
-  const showAlertAndHideForm =
-    !isLoadingFeatureFlags && isOldPortalAlertEnabled;
+  // const showAlertAndHideForm =
+  //   !isLoadingFeatureFlags && isOldPortalAlertEnabled;
   const performRedirect =
     !isLoadingFeatureFlags && !(isCanaryEnabled || isCanaryEnabledViaCookie);
 
@@ -34,40 +35,40 @@ export default function App({ location, children }) {
     return <></>;
   }
 
-  if (showAlertAndHideForm) {
-    return (
-      <VaAlert
-        className="vads-u-margin-x--2 vads-u-margin-top--3  vads-u-margin-bottom--9"
-        close-btn-aria-label="Close notification"
-        status="warning"
-        uswds
-      >
-        <h2 id="track-your-status-on-mobile" slot="headline">
-          This version of Ask VA isn't working right now
-        </h2>
-        <p className="vads-u-margin-y--0">
-          We're making some updates to Ask VA on VA.gov. We're sorry it's not
-          working right now. You can still use the{' '}
-          <a href="https://ask.va.gov/">previous version of Ask VA</a>
-        </p>
-      </VaAlert>
-    );
-  }
-
-  return !isLoadingFeatureFlags ? (
-    <>
-      <BreadCrumbs currentLocation={location.pathname} />
-      <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-        <ProgressBar pathname={location.pathname} />
-        {children}
-      </RoutedSavableApp>
-    </>
-  ) : (
-    <></>
+  // if (showAlertAndHideForm) {
+  return (
+    <VaAlert
+      className="vads-u-margin-x--2 vads-u-margin-top--3  vads-u-margin-bottom--9"
+      close-btn-aria-label="Close notification"
+      status="warning"
+      uswds
+    >
+      <h2 id="track-your-status-on-mobile" slot="headline">
+        This version of Ask VA isn't working right now
+      </h2>
+      <p className="vads-u-margin-y--0">
+        We're making some updates to Ask VA on VA.gov. We're sorry it's not
+        working right now. You can still use the{' '}
+        <a href="https://ask.va.gov/">previous version of Ask VA</a>
+      </p>
+    </VaAlert>
   );
+  // }
+
+  // return !isLoadingFeatureFlags ? (
+  //   <>
+  //     <BreadCrumbs currentLocation={location.pathname} />
+  //     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+  //       <ProgressBar pathname={location.pathname} />
+  //       {children}
+  //     </RoutedSavableApp>
+  //   </>
+  // ) : (
+  //   <></>
+  // );
 }
 
-App.propTypes = {
-  children: PropTypes.node,
-  location: PropTypes.object,
-};
+// App.propTypes = {
+//   children: PropTypes.node,
+//   location: PropTypes.object,
+// };
