e9d1cda42ed9173feaed6d18eb6f1064b9b4a141
Ni Chia <ni.chia@adhocteam.us>
2025-04-17 13:30:04 -0700

    remove copy of asyncLoader

--- src/applications/mhv-landing-page/routes.jsx
+++ src/applications/mhv-landing-page/routes.jsx
@@ -1,47 +1,38 @@
 /* eslint-disable @department-of-veterans-affairs/no-cross-app-imports */
 import React from 'react';
 import { Route, Routes } from 'react-router-dom-v5-compat';
+import asyncLoader from '@department-of-veterans-affairs/platform-utilities/asyncLoader';
 import PageNotFound from '@department-of-veterans-affairs/platform-site-wide/PageNotFound';
 import AppConfig from './containers/AppConfig';
 import LandingPageContainer from './containers/LandingPageContainer';
 
-// Custom asyncLoader function
-function asyncLoader(getComponent, message) {
-  return class AsyncComponent extends React.Component {
-    static Component = null;
+//     /* eslint-disable-next-line camelcase */
+//     UNSAFE_componentWillMount() {
+//       if (!this.state.Component) {
+//         this.componentPromise = getComponent()
+//           .then(m => (m.default ? m.default : m))
+//           .then(Component => {
+//             AsyncComponent.Component = Component;
+//             this.setState({ Component });
+//           });
+//       } else if (!this.componentPromise) {
+//         this.componentPromise = Promise.resolve();
+//       }
+//     }
 
-    constructor(props) {
-      super(props);
-      this.state = { Component: AsyncComponent.Component };
-    }
-
-    /* eslint-disable-next-line camelcase */
-    UNSAFE_componentWillMount() {
-      if (!this.state.Component) {
-        this.componentPromise = getComponent()
-          .then(m => (m.default ? m.default : m))
-          .then(Component => {
-            AsyncComponent.Component = Component;
-            this.setState({ Component });
-          });
-      } else if (!this.componentPromise) {
-        this.componentPromise = Promise.resolve();
-      }
-    }
-
-    render() {
-      const { Component } = this.state;
-      if (Component) {
-        return <Component {...this.props} />;
-      }
-      return (
-        <div className="async-loader">
-          <va-loading-indicator message={message || 'Loading page...'} />
-        </div>
-      );
-    }
-  };
-}
+//     render() {
+//       const { Component } = this.state;
+//       if (Component) {
+//         return <Component {...this.props} />;
+//       }
+//       return (
+//         <div className="async-loader">
+//           <va-loading-indicator message={message || 'Loading page...'} />
+//         </div>
+//       );
+//     }
+//   };
+// }
 
 // const AsyncSecureMessagingRoutes = store =>
 //   asyncLoader(() =>
@@ -66,13 +57,13 @@ const AsyncAppointmentsRoutes = asyncLoader(() =>
   }),
 );
 
-const routes = store => (
+const routes = () => (
   <AppConfig>
     <Routes>
       <Route path="/" element={<LandingPageContainer />} />
       <Route
         path="/my-secure-messages/*"
-        element={<AsyncSecureMessagingRoutes store={store} />}
+        element={<AsyncSecureMessagingRoutes />}
         key="mhvSecureMessages"
       />
       <Route
