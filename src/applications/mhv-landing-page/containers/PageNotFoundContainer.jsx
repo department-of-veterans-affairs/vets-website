e0e42e9b936233cabb54517ab09b530964991874
radavis <1299034+radavis@users.noreply.github.com>
2025-04-17 12:38:58 -0400

    Adds jsdoc

--- src/applications/mhv-landing-page/containers/PageNotFoundContainer.jsx
+++ src/applications/mhv-landing-page/containers/PageNotFoundContainer.jsx
@@ -4,6 +4,15 @@ import PageNotFound from '@department-of-veterans-affairs/platform-site-wide/Pag
 import { MhvSecondaryNav } from '@department-of-veterans-affairs/mhv/exports';
 import { isLOA3, isVAPatient } from '../selectors';
 
+/**
+ * PageNotFoundContainer component.
+ *
+ * Renders a "Page Not Found" message with the MHV secondary navigation
+ * if the user is verified (LOA3) and is a VA patient.
+ *
+ * @component
+ * @returns {JSX.Element} The rendered PageNotFoundContainer component.
+ */
 const PageNotFoundContainer = () => {
   const isVerified = useSelector(isLOA3);
   const isAPatient = useSelector(isVAPatient);
