d9c66aa3e0d6f9291235b1afcec6ad7a42cb4d23
Ni Chia <ni.chia@adhocteam.us>
2025-04-17 12:59:25 -0700

    replace react.lazy with dynamic import

--- src/applications/mhv-landing-page/routes.jsx
+++ src/applications/mhv-landing-page/routes.jsx
@@ -1,49 +1,87 @@
 /* eslint-disable @department-of-veterans-affairs/no-cross-app-imports */
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom-v5-compat';
 import PageNotFound from '@department-of-veterans-affairs/platform-site-wide/PageNotFound';
 import AppConfig from './containers/AppConfig';
+import LandingPageContainer from './containers/LandingPageContainer';
 
-// Lazy-loaded components
-const LandingPageContainer = lazy(() =>
-  import('./containers/LandingPageContainer'),
-);
-const MhvSecureMessagingRoutes = lazy(() =>
-  import('../mhv-secure-messaging/routes'),
-);
-const AppointmentsRoutes = lazy(() => import('../vaos/routes'));
+// Custom asyncLoader function
+function asyncLoader(getComponent, message) {
+  return class AsyncComponent extends React.Component {
+    static Component = null;
 
-// Loading component to display while lazy-loaded components are being fetched
-const Loading = () => (
-  <va-loading-indicator
-    message="Loading..."
-    set-focus
-    data-testid="loading-indicator"
-  />
+    constructor(props) {
+      super(props);
+      this.state = { Component: AsyncComponent.Component };
+    }
+
+    /* eslint-disable-next-line camelcase */
+    UNSAFE_componentWillMount() {
+      if (!this.state.Component) {
+        this.componentPromise = getComponent()
+          .then(m => (m.default ? m.default : m))
+          .then(Component => {
+            AsyncComponent.Component = Component;
+            this.setState({ Component });
+          });
+      } else if (!this.componentPromise) {
+        this.componentPromise = Promise.resolve();
+      }
+    }
+
+    render() {
+      const { Component } = this.state;
+      if (Component) {
+        return <Component {...this.props} />;
+      }
+      return (
+        <div className="async-loader">
+          <va-loading-indicator message={message || 'Loading page...'} />
+        </div>
+      );
+    }
+  };
+}
+
+// const AsyncSecureMessagingRoutes = store =>
+//   asyncLoader(() =>
+//     import('../mhv-secure-messaging/routes').then(
+//       ({ default: MhvSecureMessagingRoutes, reducer }) => {
+//         store.injectReducer('MhvSecureMessagingRoutes', reducer);
+//         return MhvSecureMessagingRoutes;
+//       },
+//     ),
+//   );
+const AsyncSecureMessagingRoutes = asyncLoader(() =>
+  import('../mhv-secure-messaging/routes').then(
+    ({ default: MhvSecureMessagingRoutes }) => {
+      return MhvSecureMessagingRoutes;
+    },
+  ),
 );
 
-const routes = (
+const AsyncAppointmentsRoutes = asyncLoader(() =>
+  import('../vaos/routes').then(({ default: AppointmentsRoutes }) => {
+    return AppointmentsRoutes;
+  }),
+);
+
+const routes = store => (
   <AppConfig>
-    <Suspense fallback={<Loading />}>
-      <Routes>
-        <Route
-          path="/"
-          element={<LandingPageContainer />}
-          key="mhvLandingPage"
-        />
-        <Route
-          path="/my-secure-messages/*"
-          element={<MhvSecureMessagingRoutes />}
-          key="mhvSecureMessages"
-        />
-        <Route
-          path="/my-appointments/*"
-          element={<AppointmentsRoutes />}
-          key="appointments"
-        />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route path="/" element={<LandingPageContainer />} />
+      <Route
+        path="/my-secure-messages/*"
+        element={<AsyncSecureMessagingRoutes store={store} />}
+        key="mhvSecureMessages"
+      />
+      <Route
+        path="/my-appointments/*"
+        element={<AsyncAppointmentsRoutes />}
+        key="appointments"
+      />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
   </AppConfig>
 );
 
