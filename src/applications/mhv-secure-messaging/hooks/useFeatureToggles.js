45e448927be544e49acc731c6d0d892e05b98500
Kyle Cardwell <79024398+KyleCardwell@users.noreply.github.com>
2025-04-18 10:58:13 -0600

    add combo box to compose form (#35386)
    * add combo box to compose form
    
    * combo box select update
    
    * add combo box tests
    
    * add delete draft
    
    * add send message test
    
    * fix failed tests
    
    * updated component-library to 50.1.0
    
    * add combo box alerts test
    
    * fix tests
    
    * fix tests
    
    * id and class cleanup, add featureTogglesLoading check
    
    * error handling
    
    * saveDraft message valid false on empty recipient fix
    
    * invalid vs blank error handling for combo box
    
    * add test
    
    * fix failed tests
    
    * added unit tests
    
    * add focus assertions & fix failed tests
    
    * exclude flaky test
    
    * update methods
    
    * updated PropTypes
    
    ---------
    
    Co-authored-by: fazilqa <127263470+fazilqa@users.noreply.github.com>

--- src/applications/mhv-secure-messaging/hooks/useFeatureToggles.js
+++ src/applications/mhv-secure-messaging/hooks/useFeatureToggles.js
@@ -2,7 +2,11 @@ import { useSelector } from 'react-redux';
 import FEATURE_FLAG_NAMES from '@department-of-veterans-affairs/platform-utilities/featureFlagNames';
 
 const useFeatureToggles = () => {
-  const { featureTogglesLoading, isSignatureSettingsEnabled } = useSelector(
+  const {
+    featureTogglesLoading,
+    isSignatureSettingsEnabled,
+    isComboBoxEnabled,
+  } = useSelector(
     state => {
       return {
         featureTogglesLoading: state.featureToggles.loading,
@@ -10,12 +14,20 @@ const useFeatureToggles = () => {
           state.featureToggles[
             FEATURE_FLAG_NAMES.mhvSecureMessagingSignatureSettings
           ],
+        isComboBoxEnabled:
+          state.featureToggles[
+            FEATURE_FLAG_NAMES.mhvSecureMessagingRecipientCombobox
+          ],
       };
     },
     state => state.featureToggles,
   );
 
-  return { featureTogglesLoading, isSignatureSettingsEnabled };
+  return {
+    featureTogglesLoading,
+    isSignatureSettingsEnabled,
+    isComboBoxEnabled,
+  };
 };
 
 export default useFeatureToggles;
