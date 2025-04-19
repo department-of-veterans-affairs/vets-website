30bf1747bd12f62a8eb14361d622c5fa07be2b5f
Chikaodi Adibemma <chika.adibemma@adhocteam.us>
2025-04-19 05:14:43 -0400

    updated personalInformation section of form

--- src/applications/simple-forms/21-4142/pages/personalInformation1.js
+++ src/applications/simple-forms/21-4142/pages/personalInformation1.js
@@ -1,36 +1,29 @@
-import React from 'react';
-import fullSchema from 'vets-json-schema/dist/21-4142-schema.json';
-import dateUI from 'platform/forms-system/src/js/definitions/date';
-import { intersection, pick } from 'lodash';
-import { fullNameDeprecatedUI } from '../../shared/definitions/rjsfPatterns';
-import { veteranFields } from '../definitions/constants';
-
-const { required, properties } = fullSchema.properties[
-  veteranFields.parentObject
-];
-const pageFields = [veteranFields.fullName, veteranFields.dateOfBirth];
+// import React from 'react';
+import {
+  dateOfBirthSchema,
+  dateOfBirthUI,
+  fullNameSchema,
+  fullNameUI,
+  titleUI,
+} from '~/platform/forms-system/src/js/web-component-patterns';
+import { getFullNameLabels } from '../helpers';
 
 /** @type {PageSchema} */
 export default {
   uiSchema: {
-    [veteranFields.parentObject]: {
-      'ui:title': (
-        <h3 className="vads-u-color--gray-dark vads-u-margin-top--0">
-          Tell us about the Veteran connected to this authorization
-        </h3>
-      ),
-      [veteranFields.fullName]: fullNameDeprecatedUI,
-      [veteranFields.dateOfBirth]: dateUI('Date of birth'),
-    },
+    ...titleUI({
+      title: 'Tell us about the Veteran connected to this authorization',
+      headerLevel: 3,
+    }),
+    fullName: fullNameUI(label => getFullNameLabels(label, false)),
+    dateOfBirth: dateOfBirthUI(),
   },
   schema: {
     type: 'object',
     properties: {
-      [veteranFields.parentObject]: {
-        type: 'object',
-        required: intersection(required, pageFields),
-        properties: pick(properties, pageFields),
-      },
+      fullName: fullNameSchema,
+      dateOfBirth: dateOfBirthSchema,
     },
+    required: ['fullName', 'dateOfBirth'],
   },
 };
