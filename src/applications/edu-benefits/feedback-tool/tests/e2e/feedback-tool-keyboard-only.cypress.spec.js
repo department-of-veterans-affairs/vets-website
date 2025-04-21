8a746870578a4584ec3c13e3800345f5ff21751c
fatmakhan0395 <145523589+fatmakhan0395@users.noreply.github.com>
2025-04-21 11:49:20 -0700

    vebt-1493 e2e tests for ft

--- src/applications/edu-benefits/feedback-tool/tests/e2e/feedback-tool-keyboard-only.cypress.spec.js
+++ src/applications/edu-benefits/feedback-tool/tests/e2e/feedback-tool-keyboard-only.cypress.spec.js
@@ -28,9 +28,18 @@ describe('Feedback Tool Keyboard Test', () => {
       },
     });
 
-    vaosSetup();
-
     cy.visit(manifest.rootUrl);
+    cy.injectAxe();
+    cy.axeCheck('main', {
+      rules: {
+        'aria-required-children': {
+          enabled: false,
+        },
+        'link-name': {
+          enabled: false,
+        },
+      },
+    });
     cy.get('body').should('be.visible');
     cy.get('.schemaform-title').should('be.visible');
     cy.get('.schemaform-start-button')
@@ -80,7 +89,53 @@ describe('Feedback Tool Keyboard Test', () => {
       '1990',
       '1990',
     );
-
-    // To be continued...
+    cy.realPress('Tab');
+    cy.allyEvaluateSelectMenu(
+      '[name="root_serviceDateRange_toMonth"]',
+      'mar',
+      'March',
+    );
+    cy.realPress('Tab');
+    cy.allyEvaluateInput('[name="root_serviceDateRange_toDay"]', '31', '31');
+    cy.realPress('Tab');
+    cy.allyEvaluateInput(
+      '[name="root_serviceDateRange_toYear"]',
+      '2010',
+      '2010',
+    );
+    cy.repeatKey('Tab', 2);
+    cy.realPress('Enter');
+    cy.url().should('not.include', '/service-information');
+    cy.url().should('include', '/contact-information');
+    cy.get('[name="root_address_country"]').select('United States');
+    cy.repeatKey('Tab', 1);
+    cy.allyEvaluateInput('#root_address_street', '11233 Nowhere St');
+    cy.repeatKey('Tab', 2);
+    cy.allyEvaluateInput('#root_address_city', 'Long Beach');
+    cy.repeatKey('Tab', 2);
+    cy.get('[name="root_address_state"]').select('CA');
+    cy.realPress('Tab');
+    cy.allyEvaluateInput('#root_address_postalCode', '90712');
+    cy.realPress('Tab');
+    cy.allyEvaluateInput('#root_applicantEmail', 'test@va.gov');
+    cy.realPress('Tab');
+    cy.get('[name="root_view:applicantEmailConfirmation"]').type('test@va.gov');
+    cy.repeatKey('Tab', 3);
+    cy.realPress('Enter');
+    cy.url().should('not.include', '/contact-information');
+    cy.url().should('include', '/benefits-information');
+    cy.get('input[name="root_educationDetails_programs_chapter33"]').check();
+    cy.get('input[name="root_educationDetails_programs_chapter30"]').check();
+    cy.get('input[name="root_educationDetails_programs_chapter31"]').check();
+    cy.repeatKey('Tab', 6);
+    cy.realPress('Enter');
+    cy.url().should('not.include', '/benefits-information');
+    cy.url().should('include', '/school-information');
+    cy.get('input[name="school-search-input"]')
+      .should('exist')
+      .type('foothill high');
+    cy.realPress('Tab');
+    cy.realPress('Enter');
+    cy.repeatKey('Tab', 3);
   });
 });
