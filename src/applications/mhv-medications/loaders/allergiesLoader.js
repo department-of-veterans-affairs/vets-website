034eabff5ff8768459cb79d759a8698f0d1a33dd
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-04-22 15:46:37 -0600

    Fix double network call

--- src/applications/mhv-medications/loaders/allergiesLoader.js
+++ src/applications/mhv-medications/loaders/allergiesLoader.js
@@ -2,8 +2,6 @@ import { store } from '../store';
 import { getAllergies } from '../api/allergiesApi';
 
 export const allergiesLoader = async () => {
-  await store
-    .dispatch(getAllergies.initiate(undefined, { subscribe: false }))
-    .unwrap(); // Surface any errors to the router.
+  await store.dispatch(getAllergies.initiate(undefined)).unwrap(); // Surface any errors to the router.
   return null;
 };
