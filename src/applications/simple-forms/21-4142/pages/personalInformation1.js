c0e76d1f1007c3a32986a388743a8ffb63130d3c
Chikaodi Adibemma <chika.adibemma@adhocteam.us>
2025-04-22 16:22:49 -0400

    reincluded old schema

--- src/applications/simple-forms/21-4142/pages/personalInformation1.js
+++ src/applications/simple-forms/21-4142/pages/personalInformation1.js
@@ -1,4 +1,7 @@
-// import React from 'react';
+import React from 'react';
+import fullSchema from 'vets-json-schema/dist/21-4142-schema.json';
+import dateUI from 'platform/forms-system/src/js/definitions/date';
+import { intersection, pick } from 'lodash';
 import {
   dateOfBirthSchema,
   dateOfBirthUI,
@@ -6,24 +9,52 @@ import {
   fullNameUI,
   titleUI,
 } from '~/platform/forms-system/src/js/web-component-patterns';
+import environment from 'platform/utilities/environment';
+import { fullNameDeprecatedUI } from '../../shared/definitions/rjsfPatterns';
+import { veteranFields } from '../definitions/constants';
 import { getFullNameLabels } from '../helpers';
+
+const { required, properties } = fullSchema.properties[
+  veteranFields.parentObject
+];
+const pageFields = [veteranFields.fullName, veteranFields.dateOfBirth];
 
 /** @type {PageSchema} */
 export default {
-  uiSchema: {
-    ...titleUI({
-      title: 'Tell us about the Veteran connected to this authorization',
-      headerLevel: 3,
-    }),
-    fullName: fullNameUI(label => getFullNameLabels(label, false)),
-    dateOfBirth: dateOfBirthUI(),
-  },
+  uiSchema: environment.isProduction()
+    ? {
+        [veteranFields.parentObject]: {
+          'ui:title': (
+            <h3 className="vads-u-color--gray-dark vads-u-margin-top--0">
+              Tell us about the Veteran connected to this authorization
+            </h3>
+          ),
+          [veteranFields.fullName]: fullNameDeprecatedUI,
+          [veteranFields.dateOfBirth]: dateUI('Date of birth'),
+        },
+      }
+    : {
+        ...titleUI({
+          title: 'Tell us about the Veteran connected to this authorization',
+          headerLevel: 3,
+        }),
+        fullName: fullNameUI(label => getFullNameLabels(label, false)),
+        dateOfBirth: dateOfBirthUI(),
+      },
   schema: {
     type: 'object',
-    properties: {
-      fullName: fullNameSchema,
-      dateOfBirth: dateOfBirthSchema,
-    },
+    properties: environment.isProduction()
+      ? {
+          [veteranFields.parentObject]: {
+            type: 'object',
+            required: intersection(required, pageFields),
+            properties: pick(properties, pageFields),
+          },
+        }
+      : {
+          fullName: fullNameSchema,
+          dateOfBirth: dateOfBirthSchema,
+        },
     required: ['fullName', 'dateOfBirth'],
   },
 };
