61d1f14e2d1095fb59a3089719ad82d775919d63
KerryMin <kerrym175@gmail.com>
2025-04-23 13:22:37 -0400

    skip empty test suite

--- config/mocha-multi-reporter.js
+++ config/mocha-multi-reporter.js
@@ -1,13 +1,11 @@
 module.exports = {
   reporterEnabled: 'mocha-junit-reporter, mochawesome',
+  mochaJunitReporterReporterOptions: {
+    mochaFile: 'mocha/results/junit-[hash].xml',
+    skipEmptyTestSuite: true,
+  },
   mochawesomeReporterOptions: {
-    consoleReporter: 'min',
     reportDir: 'mocha/results',
-    reportFilename: `${process.env.STEP}_${new Date()
-      .toISOString()
-      .replace(/[.:]/g, '')}`,
-    overwrite: false,
-    html: false,
-    json: true,
+    inlineAssets: true,
   },
 };
