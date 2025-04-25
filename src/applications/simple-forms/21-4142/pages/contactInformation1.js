ed0a252b6c70a662266a9d363a3c349fe922983e
Chikaodi Adibemma <chika.adibemma@adhocteam.us>
2025-04-25 15:28:03 -0400

    ch3 update wip

--- src/applications/simple-forms/21-4142/pages/contactInformation1.js
+++ src/applications/simple-forms/21-4142/pages/contactInformation1.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { intersection, pick } from 'lodash';
 import fullSchema from 'vets-json-schema/dist/21-4142-schema.json';
 import addressUiSchema from 'platform/forms-system/src/js/definitions/profileAddress';
+import environment from 'platform/utilities/environment';
 import { veteranFields } from '../definitions/constants';
 
 const { required, properties } = fullSchema.properties[
@@ -11,26 +12,32 @@ const pageFields = [veteranFields.address];
 
 /** @type {PageSchema} */
 export default {
-  uiSchema: {
-    [veteranFields.parentObject]: {
-      'ui:description': (
-        <p>We’ll send any updates about your authorization to this address</p>
-      ),
-      [veteranFields.address]: addressUiSchema(
-        `${[veteranFields.parentObject]}.${[veteranFields.address]}`,
-        'The Veteran lives on a United States military base outside of the U.S.',
-        () => true,
-      ),
-    },
-  },
+  uiSchema: environment.isProduction()
+    ? {
+        [veteranFields.parentObject]: {
+          'ui:description': (
+            <p>
+              We’ll send any updates about your authorization to this address
+            </p>
+          ),
+          [veteranFields.address]: addressUiSchema(
+            `${[veteranFields.parentObject]}.${[veteranFields.address]}`,
+            'The Veteran lives on a United States military base outside of the U.S.',
+            () => true,
+          ),
+        },
+      }
+    : {},
   schema: {
     type: 'object',
-    properties: {
-      [veteranFields.parentObject]: {
-        type: 'object',
-        required: intersection(required, pageFields),
-        properties: pick(properties, pageFields),
-      },
-    },
+    properties: environment.isProduction()
+      ? {
+          [veteranFields.parentObject]: {
+            type: 'object',
+            required: intersection(required, pageFields),
+            properties: pick(properties, pageFields),
+          },
+        }
+      : {},
   },
 };
