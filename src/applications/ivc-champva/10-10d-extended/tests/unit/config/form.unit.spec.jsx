fc44aa7280d7c85690ceabd31c5cdfa4ba73a1c8
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-04-25 14:37:34 -0500

    spec

--- src/applications/ivc-champva/10-10d-extended/tests/unit/config/form.unit.spec.jsx
+++ src/applications/ivc-champva/10-10d-extended/tests/unit/config/form.unit.spec.jsx
@@ -28,3 +28,12 @@ testNumberOfWebComponentFields(
   'Applicant - SSN',
   { applicants: mockData.data.applicants },
 );
+
+testNumberOfWebComponentFields(
+  formConfig,
+  formConfig.chapters.applicantInformation.pages.page15.schema,
+  formConfig.chapters.applicantInformation.pages.page15.uiSchema,
+  8,
+  'Applicant - mailing address',
+  { applicants: mockData.data.applicants },
+);
