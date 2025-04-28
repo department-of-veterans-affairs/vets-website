ac9a4e3b79138bd98c146f71639cdb3efea0e671
radavis <1299034+radavis@users.noreply.github.com>
2025-04-28 12:25:02 -0400

    Replaces PageNotFound with MhvPageNotFound component

--- src/applications/mhv-medical-records/tests/e2e/medical-records-page-not-found.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-page-not-found.cypress.spec.js
@@ -1,15 +1,14 @@
-import { pageNotFoundHeading } from '@department-of-veterans-affairs/platform-site-wide/PageNotFound';
 import { rootUrl } from '../../manifest.json';
 
 describe('Page Not Found', () => {
   it('Visit an unsupported URL and get a page not found', () => {
     cy.visit(`${rootUrl}/path1`);
     cy.injectAxeThenAxeCheck();
-    cy.findByRole('heading', { name: pageNotFoundHeading }).should.exist;
+    cy.get('[data-testid="mhv-page-not-found"]').should('exist');
     cy.get('[data-testid="mhv-mr-navigation"]').should('not.exist');
 
     cy.visit(`${rootUrl}/path1/path2`);
-    cy.findByRole('heading', { name: pageNotFoundHeading }).should.exist;
+    cy.get('[data-testid="mhv-page-not-found"]').should('exist');
     cy.get('[data-testid="mhv-mr-navigation"]').should('not.exist');
   });
 });
