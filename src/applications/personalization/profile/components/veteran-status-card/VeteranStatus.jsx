cd5e61883529f47e9d0a8a4590c26bb7fef75068
Matthew Guest <187806202+matt-guest-wilcore@users.noreply.github.com>
2025-04-28 12:39:30 -0700

    1572 - Creating a new page for the Veteran Status Card (#36020)
    * Added the Veteran Status Card to profile and created a new page for the ProofOfVeteranStatus component. Updated some scss for the card and some text too. Also, incorporated the new veteran status feature flag.
    
    * Updated the Veteran Status Card to have its own folder. Most of the code is duplicated over from ProofOfVeteranStatus but modified to fit the new design. This was done to avoid modifying existing, shared code when incorporating the feature flag. Now when the FF is turned on we won't see it in the Profile list and the VSC in MilitaryInformation will keep its own look. Also, renamed from ProofOfVeteranStatus to VeteranStatus, we will use this name going forward.
    
    * Updated routesForNav to use the VeteranStatus component instead of VeteranStatusCardPage. VeteranStatusCardPage is a redundant component and I consolidated the Headline and description to the VeteranStatus and updated the unit test accordingly.
    
    ---------
    
    Co-authored-by: Matthew Guest - Work <matthewguest-wilcore@Matthews-MacBook-Pro.local>

---
+++ src/applications/personalization/profile/components/veteran-status-card/VeteranStatus.jsx
@@ -0,0 +1,311 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { focusElement } from '~/platform/utilities/ui';
+import { captureError } from '~/platform/user/profile/vap-svc/util/analytics';
+import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
+import { apiRequest } from '~/platform/utilities/api';
+import { formatFullName } from '../../../common/helpers';
+import { getServiceBranchDisplayName } from '../../helpers';
+import Headline from '../ProfileSectionHeadline';
+import VeteranStatusCard from './VeteranStatusCard/VeteranStatusCard';
+
+const VeteranStatus = ({
+  serviceHistory = [],
+  vetStatusEligibility = {},
+  totalDisabilityRating,
+  userFullName = {
+    first: '',
+    middle: '',
+    last: '',
+    suffix: '',
+  },
+  edipi,
+}) => {
+  const [errors, setErrors] = useState([]);
+  const [data, setData] = useState(null);
+  const [shouldFocusError, setShouldFocusError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const { first, middle, last, suffix } = userFullName;
+
+  const formattedFullName = formatFullName({
+    first,
+    middle,
+    last,
+    suffix,
+  });
+
+  const getLatestService = () => {
+    if (serviceHistory.length) {
+      const latestServiceItem = serviceHistory.length
+        ? serviceHistory.reduce((latest, current) => {
+            return new Date(current.endDate) > new Date(latest.endDate)
+              ? current
+              : latest;
+          })
+        : null;
+      const serviceStartYear = latestServiceItem.beginDate
+        ? latestServiceItem.beginDate.substring(0, 4)
+        : '';
+      const serviceEndYear = latestServiceItem.endDate
+        ? latestServiceItem.endDate.substring(0, 4)
+        : '';
+      const latestServiceDateRange =
+        serviceStartYear.length || serviceEndYear.length
+          ? `${serviceStartYear}–${serviceEndYear}`
+          : '';
+      return `${getServiceBranchDisplayName(
+        latestServiceItem.branchOfService,
+      )} • ${latestServiceDateRange}`;
+    }
+    return null;
+  };
+
+  const latestService = getLatestService();
+
+  const userHasRequiredCardData = !!(
+    serviceHistory.length && formattedFullName
+  );
+  const hasConfirmationData = !!(data && data.attributes);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchVerificationStatus = async () => {
+      setIsLoading(true);
+
+      try {
+        const path = '/profile/vet_verification_status';
+        const response = await apiRequest(path);
+        if (isMounted) {
+          setData(response.data);
+        }
+      } catch (error) {
+        if (isMounted) {
+          setErrors([
+            "We're sorry. There's a problem with our system. We can't show your Veteran status card right now. Try again later.",
+          ]);
+          captureError(error, { eventName: 'vet-status-fetch-verification' });
+        }
+      } finally {
+        if (isMounted) {
+          setIsLoading(false);
+        }
+      }
+    };
+    fetchVerificationStatus();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  useEffect(
+    () => {
+      if (shouldFocusError && errors?.length > 0) {
+        focusElement('.vet-status-pdf-download-error');
+        setShouldFocusError(false);
+      }
+    },
+    [shouldFocusError, errors],
+  );
+
+  const isVetStatusEligibilityPopulated =
+    Object.keys(vetStatusEligibility).length !== 0;
+
+  const buildContactElements = item => {
+    const contactNumber = `${CONTACTS.DS_LOGON.slice(
+      0,
+      3,
+    )}-${CONTACTS.DS_LOGON.slice(3, 6)}-${CONTACTS.DS_LOGON.slice(6)}`;
+    const startIndex = item.indexOf(contactNumber);
+
+    if (startIndex === -1) {
+      return item;
+    }
+
+    const before = item.slice(0, startIndex);
+    const telephone = item.slice(
+      startIndex,
+      startIndex + contactNumber.length + 11,
+    );
+    const after = item.slice(startIndex + telephone.length);
+
+    return (
+      <>
+        {before}
+        <va-telephone contact={contactNumber} /> (
+        <va-telephone contact={CONTACTS[711]} tty />){after}
+      </>
+    );
+  };
+
+  const componentizedMessage = isVetStatusEligibilityPopulated
+    ? vetStatusEligibility?.message.map(item => {
+        return buildContactElements(item);
+      })
+    : null;
+
+  const contactInfoElements = data?.message?.map(item => {
+    return buildContactElements(item);
+  });
+
+  const systemErrrorAlert = (
+    <va-alert close-btn-aria-label="Close notification" status="error" visible>
+      <p className="vads-u-margin-top--0 vads-u-margin-bottom--0">
+        We’re sorry. There’s a problem with our system. We can’t show your
+        Veteran status card right now. Try again later.
+      </p>
+    </va-alert>
+  );
+
+  const lighthouseApiErrorMessage = (
+    <va-alert
+      close-btn-aria-label="Close notification"
+      status="warning"
+      visible
+    >
+      {contactInfoElements?.map((message, i) => {
+        if (i === 0) {
+          return (
+            <p key={i} className="vads-u-margin-top--0">
+              {message}
+            </p>
+          );
+        }
+        return <p key={i}>{message}</p>;
+      })}
+    </va-alert>
+  );
+
+  const profileApiErrorMessage = (
+    <va-alert
+      close-btn-aria-label="Close notification"
+      status="warning"
+      visible
+    >
+      {componentizedMessage.map((message, i) => {
+        if (i === 0) {
+          return (
+            <p key={i} className="vads-u-margin-top--0">
+              {message}
+            </p>
+          );
+        }
+        return <p key={i}>{message}</p>;
+      })}
+    </va-alert>
+  );
+
+  return (
+    <>
+      <Headline>Veteran Status Card</Headline>
+      <p className="veteran-status-description">
+        This card makes it easy to prove your service and access Veteran
+        discounts, all while keeping your personal information secure.
+      </p>
+      <div id="proof-of-veteran-status">
+        {isLoading ? (
+          <va-loading-indicator
+            set-focus
+            message="Checking your eligibility..."
+            data-testid="proof-of-status-loading-indicator"
+          />
+        ) : (
+          <>
+            {userHasRequiredCardData ? (
+              <>
+                {hasConfirmationData ? (
+                  <>
+                    {data?.attributes?.veteranStatus === 'confirmed' ? (
+                      <>
+                        {errors?.length > 0 ? (
+                          <div className="vet-status-pdf-download-error vads-u-padding-y--2">
+                            <va-alert status="error" uswds>
+                              {errors[0]}
+                            </va-alert>
+                          </div>
+                        ) : null}
+                        <div className="vads-l-grid-container--full">
+                          <div className="vads-l-row">
+                            <VeteranStatusCard
+                              edipi={edipi}
+                              formattedFullName={formattedFullName}
+                              latestService={latestService}
+                              totalDisabilityRating={totalDisabilityRating}
+                            />
+                          </div>
+                        </div>
+                      </>
+                    ) : null}
+
+                    {isVetStatusEligibilityPopulated &&
+                    data?.attributes?.veteranStatus === 'not confirmed' &&
+                    data?.message?.length > 0 ? (
+                      <>{lighthouseApiErrorMessage}</>
+                    ) : null}
+                  </>
+                ) : null}
+
+                {!hasConfirmationData ? <>{systemErrrorAlert}</> : null}
+              </>
+            ) : null}
+
+            {!userHasRequiredCardData ? (
+              <>
+                {!formattedFullName ? (
+                  <>{systemErrrorAlert}</>
+                ) : (
+                  <>
+                    {errors?.length > 0 ? (
+                      <>
+                        <div className="vet-status-pdf-download-error vads-u-padding-y--2">
+                          <va-alert status="error" uswds>
+                            {errors[0]}
+                          </va-alert>
+                        </div>
+                      </>
+                    ) : null}
+
+                    {data?.attributes?.veteranStatus === 'confirmed' ? (
+                      <>{profileApiErrorMessage}</>
+                    ) : null}
+                    {data?.attributes?.veteranStatus === 'not confirmed' ? (
+                      <>{lighthouseApiErrorMessage}</>
+                    ) : null}
+                  </>
+                )}
+              </>
+            ) : null}
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+VeteranStatus.propTypes = {
+  edipi: PropTypes.number,
+  serviceHistory: PropTypes.arrayOf(
+    PropTypes.shape({
+      branchOfService: PropTypes.string,
+      beginDate: PropTypes.string,
+      endDate: PropTypes.string,
+    }).isRequired,
+  ),
+  totalDisabilityRating: PropTypes.number,
+  userFullName: PropTypes.object,
+  vetStatusEligibility: PropTypes.object,
+};
+
+const mapStateToProps = state => ({
+  serviceHistory:
+    state.vaProfile?.militaryInformation.serviceHistory.serviceHistory,
+  vetStatusEligibility:
+    state.vaProfile?.militaryInformation.serviceHistory.vetStatusEligibility,
+  totalDisabilityRating: state.totalRating?.totalDisabilityRating,
+  userFullName: state.vaProfile?.hero?.userFullName,
+  edipi: state.user?.profile?.edipi,
+});
+
+export default connect(mapStateToProps)(VeteranStatus);
