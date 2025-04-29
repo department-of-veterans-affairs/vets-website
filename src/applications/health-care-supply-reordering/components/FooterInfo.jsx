8fd9f72542b19114315ae15bc9472834828e0ad7
radavis <1299034+radavis@users.noreply.github.com>
2025-04-29 12:29:39 -0400

    Updates DLC phone number in form footer

--- src/applications/health-care-supply-reordering/components/FooterInfo.jsx
+++ src/applications/health-care-supply-reordering/components/FooterInfo.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
+import { DLC_PHONE } from '../constants';
 
 const FooterInfo = () => (
   <section className="need-help-footer row vads-u-padding-x--1p5">
     For benefit-related questions, or if the form isn’t working right, please
-    call VA Benefits and Services at{' '}
-    <va-telephone contact={CONTACTS.VA_BENEFITS} />. If you have hearing loss,
-    call <va-telephone contact={CONTACTS['711']} tty />.
+    call VA Benefits and Services at <va-telephone contact={DLC_PHONE} />. If
+    you have hearing loss, call <va-telephone contact={CONTACTS['711']} tty />.
   </section>
 );
 
