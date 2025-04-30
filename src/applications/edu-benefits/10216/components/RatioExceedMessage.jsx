62421cd9693b26b22c9af8cbc0b2993ce2072a11
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-04-30 11:37:03 -0700

    revert ratio exceeded error message | update mock data to pass e2e test

--- src/applications/edu-benefits/10216/components/RatioExceedMessage.jsx
+++ src/applications/edu-benefits/10216/components/RatioExceedMessage.jsx
@@ -5,9 +5,9 @@ function RatioExceedMessage() {
     <div className="schemaform-field-template">
       <va-alert status="error" visible>
         <p className="vads-u-margin-top--0">
-          The calculated percentage of VA beneficiaries exceeds 35%. Please
-          check your numbers, and if you believe this calculation is an error,
-          contact your ELR.
+          The calculated percentage of VA beneficiaries exceeds 35%, so you may
+          not proceed. Please check your numbers, and if you believe this
+          calculation is an error, contact your ELR.
         </p>
       </va-alert>
     </div>
