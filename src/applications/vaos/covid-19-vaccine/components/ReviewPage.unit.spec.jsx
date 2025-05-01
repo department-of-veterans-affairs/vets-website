b7da3e2f8d4eb9addce4b784cc6adac22d9c0de5
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2025-05-01 12:17:44 -0700

    Updates unit tests
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>

--- src/applications/vaos/covid-19-vaccine/components/ReviewPage.unit.spec.jsx
+++ src/applications/vaos/covid-19-vaccine/components/ReviewPage.unit.spec.jsx
@@ -98,7 +98,7 @@ describe('VAOS vaccine flow: ReviewPage', () => {
     userEvent.click(screen.getByText(/Confirm appointment/i));
     await waitFor(() => {
       expect(screen.history.push.lastCall.args[0]).to.equal(
-        '/fake_id?confirmMsg=true',
+        '/1?confirmMsg=true',
       );
     });
 
