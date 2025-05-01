37b3f7868dff98d108cdc588463ceb428264e39b
Matt Long <6738544+longmd@users.noreply.github.com>
2025-05-01 12:18:54 -0400

    Update naming for readability

--- src/applications/hca/hooks/useBrowserMonitoring.jsx
+++ src/applications/hca/hooks/useBrowserMonitoring.jsx
@@ -37,7 +37,7 @@ const initializeRealUserMonitoring = user => {
       beforeSend: ({ type, resource }) => {
         return !(
           type === 'resource' &&
-          EXCLUDED_DOMAINS.some(d => resource.url.includes(d))
+          EXCLUDED_DOMAINS.some(domain => resource.url.includes(domain))
         );
       },
     });
@@ -60,7 +60,10 @@ const initializeBrowserLogging = () => {
       ...DEFAULT_CONFIG,
       forwardErrorsToLogs: true,
       beforeSend: ({ http }) => {
-        return !(http?.url && EXCLUDED_DOMAINS.some(d => http.url.includes(d)));
+        return !(
+          http?.url &&
+          EXCLUDED_DOMAINS.some(domain => http.url.includes(domain))
+        );
       },
     });
   }
