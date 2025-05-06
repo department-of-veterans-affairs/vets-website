90c1bf9e5e1e02c15334dc54b309ecea8fab6ed1
William Phelps <williamphelps13@gmail.com>
2025-05-06 09:08:53 -0700

    Add id to H2 to appear in va-on-this-page

--- src/applications/claims-status/components/TravelClaimsSection.jsx
+++ src/applications/claims-status/components/TravelClaimsSection.jsx
@@ -11,7 +11,7 @@ export default function TravelClaimsSection() {
 
   return (
     <div className="vads-u-margin-y--4">
-      <h2>Your travel claims</h2>
+      <h2 id="your-travel-claims">Your travel claims</h2>
       <a
         className="active-va-link"
         href="/my-health/travel-pay/claims"
