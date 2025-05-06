0062a2dd5838dd54b21c289f6ad4e937c773a34a
Ni Chia <ni.chia@adhocteam.us>
2025-05-06 13:05:38 -0700

    add feature toggle to redirect supply redering app between class & new

--- src/applications/mhv-supply-reordering/containers/App.jsx
+++ src/applications/mhv-supply-reordering/containers/App.jsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 
 import { RequiredLoginView } from '@department-of-veterans-affairs/platform-user/RequiredLoginView';
 import backendServices from '@department-of-veterans-affairs/platform-user/profile/backendServices';
+import FEATURE_FLAG_NAMES from '@department-of-veterans-affairs/platform-utilities/featureFlagNames';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import { MhvSecondaryNav } from '@department-of-veterans-affairs/mhv/exports';
 import MhvRegisteredUserGuard from 'platform/mhv/components/MhvRegisteredUserGuard';
@@ -24,6 +25,24 @@ const App = ({ location, children }) => {
   const showMhvSecondaryNav =
     location.pathname.includes('/introduction') ||
     location.pathname.includes('/confirmation');
+
+  const { isMhvSupplyReorderingEnabled, featureTogglesLoading } = useSelector(
+    state => {
+      return {
+        featureTogglesLoading: state.featureToggles.loading,
+        isMhvSupplyReorderingEnabled:
+          state.featureToggles[FEATURE_FLAG_NAMES.mhvSupplyReorderingEnabled],
+      };
+    },
+  );
+
+  // Redirect to classic app and render nothing if feature toggle is not enabled.
+  if (!isMhvSupplyReorderingEnabled && !featureTogglesLoading) {
+    window.location.replace(
+      '/health-care/order-hearing-aid-or-CPAP-supplies-form',
+    );
+    return null;
+  }
 
   return (
     <RequiredLoginView
