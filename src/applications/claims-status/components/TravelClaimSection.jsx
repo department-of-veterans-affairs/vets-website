7d377147c71a04630388c4dc66fee99332618a7f
William Phelps <williamphelps13@gmail.com>
2025-05-05 20:55:41 -0700

    Add recordEvent handler to track link clicks with GA

--- src/applications/claims-status/components/TravelClaimSection.jsx
+++ src/applications/claims-status/components/TravelClaimSection.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import recordEvent from '@department-of-veterans-affairs/platform-monitoring/record-event';
 
-const TravelClaimsSection = () => (
-  <>
-    <h2>Your travel claims</h2>
-    <a
-      className="active-va-link"
-      href="https://va.gov/my-health/travel-pay/claims"
-    >
-      Review and file travel claims
-      <va-icon icon="chevron_right" size={3} aria-hidden="true" />
-    </a>
-    <p>
-      File new claims for travel reimbursement and review the status of all your
-      travel claims.
-    </p>
-  </>
-);
+export default function TravelClaimsSection() {
+  const handler = {
+    recordLinkClick: () => {
+      recordEvent({
+        event: 'nav-link-click',
+        'link-label': 'Review and file travel claims',
+        'link-destination': '/my-health/travel-pay/claims',
+      });
+    },
+  };
 
-export default TravelClaimsSection;
+  return (
+    <>
+      <h2>Your travel claims</h2>
+      <a
+        className="active-va-link"
+        href="/my-health/travel-pay/claims"
+        onClick={handler.recordLinkClick}
+      >
+        Review and file travel claims
+        <va-icon icon="chevron_right" size={3} aria-hidden="true" />
+      </a>
+      <p>
+        File new claims for travel reimbursement and review the status of all
+        your travel claims.
+      </p>
+    </>
+  );
+}
