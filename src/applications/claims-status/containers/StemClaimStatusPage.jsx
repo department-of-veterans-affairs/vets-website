633b10f67eec913921ff5f7a22ed1e58f86627ed
Ari Perez <ari@coa.solutions>
2025-05-06 10:55:57 -0400

    Update StemClaimStatusPage.jsx

--- src/applications/claims-status/containers/StemClaimStatusPage.jsx
+++ src/applications/claims-status/containers/StemClaimStatusPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -15,76 +15,80 @@ const setTitle = () => {
   setDocumentTitle('Your Edith Nourse Rogers STEM Scholarship application');
 };
 
-class StemClaimStatusPage extends React.Component {
-  componentDidMount() {
-    setTitle();
-    setUpPage();
-    this.props.getStemClaims();
-  }
+const StemClaimStatusPage = ({
+  claim,
+  loading,
+  getStemClaims: loadStemClaims,
+}) => {
+  useEffect(
+    () => {
+      setTitle();
+      setUpPage();
+      loadStemClaims();
+    },
+    [loadStemClaims],
+  );
 
-  render() {
-    const { claim, loading } = this.props;
-    let content;
-    if (loading) {
-      content = (
-        <va-loading-indicator
-          set-focus
-          message="Loading your claim information..."
-        />
-      );
-    } else if (claimAvailable(claim)) {
-      const {
-        deniedAt,
-        isEnrolledStem,
-        isPursuingTeachingCert,
-      } = claim.attributes;
+  let content;
+  if (loading) {
+    content = (
+      <va-loading-indicator
+        set-focus
+        message="Loading your claim information..."
+      />
+    );
+  } else if (claimAvailable(claim)) {
+    const {
+      deniedAt,
+      isEnrolledStem,
+      isPursuingTeachingCert,
+    } = claim.attributes;
 
-      content = (
-        <StemDeniedDetails
-          deniedAt={deniedAt}
-          isEnrolledStem={isEnrolledStem}
-          isPursuingTeachingCert={isPursuingTeachingCert}
-        />
-      );
-    } else {
-      content = (
-        <>
-          <h1>We encountered a problem</h1>
-          <ClaimsUnavailable headerLevel={2} />
-        </>
-      );
-    }
-
-    const crumb = {
-      href: `../status`,
-      label: 'Your Rogers STEM Scholarship application status details',
-      isRouterLink: true,
-    };
-
-    return (
-      <div>
-        <div name="topScrollElement" />
-        <div className="row">
-          <div className="usa-width-two-thirds medium-8 column">
-            <ClaimsBreadcrumbs crumbs={[crumb]} />
-          </div>
-        </div>
-        <div className="row">
-          <div className="usa-width-two-thirds medium-8 column">{content}</div>
-          <div className="usa-width-one-third medium-4 column">
-            <StemAskVAQuestions />
-          </div>
-        </div>
-      </div>
+    content = (
+      <StemDeniedDetails
+        deniedAt={deniedAt}
+        isEnrolledStem={isEnrolledStem}
+        isPursuingTeachingCert={isPursuingTeachingCert}
+      />
+    );
+  } else {
+    content = (
+      <>
+        <h1>We encountered a problem</h1>
+        <ClaimsUnavailable headerLevel={2} />
+      </>
     );
   }
-}
+
+  const crumb = {
+    href: `../status`,
+    label: 'Your Rogers STEM Scholarship application status details',
+    isRouterLink: true,
+  };
+
+  return (
+    <div>
+      <div name="topScrollElement" />
+      <div className="row">
+        <div className="usa-width-two-thirds medium-8 column">
+          <ClaimsBreadcrumbs crumbs={[crumb]} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="usa-width-two-thirds medium-8 column">{content}</div>
+        <div className="usa-width-one-third medium-4 column">
+          <StemAskVAQuestions />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function mapStateToProps(state, ownProps) {
   const claimsState = state.disability.status;
-  const claim = claimsState.claimsV2.stemClaims.filter(
+  const claim = claimsState.claimsV2.stemClaims.find(
     stemClaim => stemClaim.id === ownProps.params.id,
-  )[0];
+  );
   return {
     loading: claimsState.claimsV2.stemClaimsLoading,
     claim,
@@ -95,18 +99,18 @@ const mapDispatchToProps = {
   getStemClaims,
 };
 
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps,
-  )(StemClaimStatusPage),
-);
-
 StemClaimStatusPage.propTypes = {
   claim: PropTypes.object,
   getStemClaims: PropTypes.func,
   loading: PropTypes.bool,
   params: PropTypes.object,
 };
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(StemClaimStatusPage),
+);
 
 export { StemClaimStatusPage };
