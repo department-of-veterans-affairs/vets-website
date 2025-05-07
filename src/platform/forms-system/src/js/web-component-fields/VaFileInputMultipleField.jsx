6e7ff9c7fe912a94fd0c3b2622f024c277e50914
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-05-07 15:46:41 -0500

    missing thumbnails fix

--- src/platform/forms-system/src/js/web-component-fields/VaFileInputMultipleField.jsx
+++ src/platform/forms-system/src/js/web-component-fields/VaFileInputMultipleField.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { VaFileInputMultiple } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import PropTypes from 'prop-types';
@@ -39,44 +39,72 @@ const file = [];
 const VaFileInputMultipleField = props => {
   const mappedProps = vaFileInputFieldMapping(props);
   const dispatch = useDispatch();
+  const [localFile, setLocalFile] = useState(null);
   const [uploadInProgress, setUploadInProgress] = useState(false);
   const { formNumber } = props?.uiOptions;
   const { fileUploadUrl } = mappedProps;
 
+  useEffect(() => {
+    // Since the multi-file input doesn't "remember" the thumbnails of
+    // uploaded files when you navigate away and back, this re-produces them
+    // by creating File objects from the files stored in the formData object.
+    const fd = props.childrenProps.formData;
+    if (fd.length === 0) return;
+
+    const fetchFile = async () => {
+      const representations = await Promise.all(
+        fd.map(async el => {
+          const response = await fetch(el?.localFilePath);
+          const blob = await response.blob();
+          return new File([blob], el?.name, { type: el.type });
+        }),
+      );
+      setLocalFile(representations);
+    };
+
+    fetchFile();
+  }, []);
+
   const onFileUploaded = async uploadedFile => {
+    // debugger;
     if (uploadedFile.file) {
-      const {
-        confirmationCode,
-        isEncrypted,
-        name,
-        size,
-        warnings,
-      } = uploadedFile;
-      file.push(uploadedFile.file);
-      debugger;
+      const localFilePath = URL.createObjectURL(uploadedFile.file);
+      // De-nesting the properties contained in uploadedFile.file
+      const fileObj = {
+        lastModified: uploadedFile.file.lastModified,
+        lastModifiedDate: uploadedFile.file.lastModifiedDate,
+        type: uploadedFile.file.type,
+        name: uploadedFile.name,
+        size: uploadedFile.size,
+        warnings: uploadedFile.warnings,
+        confirmationCode: uploadedFile.confirmationCode,
+        isEncrypted: uploadedFile.isEncrypted,
+        localFilePath,
+      };
+
+      // Extend our list of uploaded files for this instance of the uploader
+      file.push(fileObj);
+      setLocalFile(uploadedFile.file); // for thumbnail on nav back
       setUploadInProgress(false);
-      props.childrenProps.onChange({
-        confirmationCode,
-        isEncrypted,
-        name,
-        size,
-        warnings,
-      });
+
+      // Commit list of uploaded files
+      props.childrenProps.onChange(file);
     }
   };
 
   const handleVaChange = e => {
     const fileFromEvent = e.detail.files[0];
     if (!fileFromEvent) {
-      // file = null;
+      props.childrenProps.onChange({ localFilePath: '' });
+      setLocalFile(null);
       setUploadInProgress(false);
       props.childrenProps.onChange(file);
       return;
     }
 
     if (
-      file?.slice(-1).lastModified === fileFromEvent.lastModified &&
-      file?.slice(-1).size === fileFromEvent.size
+      localFile?.slice(-1).lastModified === fileFromEvent.lastModified &&
+      localFile?.slice(-1).size === fileFromEvent.size
     ) {
       // This guard clause protects against infinite looping/updating if the localFile and fileFromEvent are identical
       return;
@@ -97,7 +125,7 @@ const VaFileInputMultipleField = props => {
     <VaFileInputMultiple
       {...mappedProps}
       error={uploadInProgress ? '' : mappedProps.error}
-      value={file}
+      value={localFile}
       onVaChange={handleVaChange}
     />
   );
