a16b80428fba7293a44ba1556ba977db2d205d16
macalemid <mahmed@ironarchtech.com>
2025-05-07 13:34:47 -0400

    Fixed spacing issues with displaying benefits card

--- src/applications/discover-your-benefits/components/BenefitCard.jsx
+++ src/applications/discover-your-benefits/components/BenefitCard.jsx
@@ -23,37 +23,44 @@ const BenefitCard = ({ benefit }) => {
   };
 
   return (
-    <va-card class="vacard" className="benefit-card vads-u-margin-bottom--2">
-      <>
-        {isTimeSensitive && (
-          <div className="blue-heading">
-            <span>
-              <b>Time-sensitive benefit</b>
-            </span>
-          </div>
-        )}
-      </>
-      <h3 className="vads-u-margin-top--0">
-        <span className="usa-label category-label">{category}</span>
-        <br />
-        <br />
-        <span>{name}</span>
-      </h3>
-      <p className="vads-u-margin-y--0">{description}</p>
-      <div className="link-container">
-        <div className="vads-u-margin-right--2 vads-u-margin-bottom--1">
-          {renderLink(
-            learnMoreURL,
-            'Learn more',
-            `Learn more about ${name}`,
-            false,
+    <div className="benefit-card vads-u-margin-bottom--2">
+      <va-card class="vacard">
+        <>
+          {isTimeSensitive && (
+            <div className="blue-heading">
+              <span>
+                <b>Time-sensitive benefit</b>
+              </span>
+            </div>
           )}
+        </>
+        <h3 className="vads-u-margin-top--1">
+          <span className="usa-label category-label">{category}</span>
+          <br />
+          <br />
+          <span>{name}</span>
+        </h3>
+        <p className="vads-u-margin-y--0">{description}</p>
+        <div className="link-container">
+          <div className="vads-u-margin-right--2 vads-u-margin-bottom--1">
+            {renderLink(
+              learnMoreURL,
+              'Learn more',
+              `Learn more about ${name}`,
+              false,
+            )}
+          </div>
+          <div>
+            {renderLink(
+              applyNowURL,
+              'Apply now',
+              `Apply now for ${name}`,
+              true,
+            )}
+          </div>
         </div>
-        <div>
-          {renderLink(applyNowURL, 'Apply now', `Apply now for ${name}`, true)}
-        </div>
-      </div>
-    </va-card>
+      </va-card>
+    </div>
   );
 };
 
