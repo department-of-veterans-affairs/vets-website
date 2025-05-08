f68bcc7a470bdbb568774c2f92c25834326b836a
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-05-08 14:56:12 -0400

    Resolved focus issue

--- src/applications/personalization/profile/components/accredited-representative/AccreditedRepresentative.jsx
+++ src/applications/personalization/profile/components/accredited-representative/AccreditedRepresentative.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useStore } from 'react-redux';
+import { focusElement } from '@department-of-veterans-affairs/platform-utilities/ui';
 
 import repStatusLoader from 'platform/user/widgets/representative-status';
 
@@ -7,6 +8,7 @@ const AccreditedRepresentative = () => {
   const store = useStore();
 
   useEffect(() => {
+    focusElement('.rep-section-header');
     repStatusLoader(store, 'representative-status', 2);
   }, []);
 
@@ -14,7 +16,7 @@ const AccreditedRepresentative = () => {
     <>
       <h1
         tabIndex="-1"
-        className="vads-u-font-size--h2 vads-u-margin-y--2 medium-screen:vads-u-margin-bottom--4 medium-screen:vads-u-margin-top--3"
+        className="vads-u-font-size--h2 vads-u-margin-y--2 medium-screen:vads-u-margin-bottom--4 medium-screen:vads-u-margin-top--3 rep-section-header"
         data-focus-target
       >
         Accredited Representative or VSO
