35c540cd1b95d30318179a92e41e23792787e93c
Chikaodi Adibemma <chika.adibemma@adhocteam.us>
2025-05-08 21:11:07 -0400

    e2e fix - added element check

--- src/applications/simple-forms/21-4142/tests/e2e/4142-medial-release.cypress.spec.js
+++ src/applications/simple-forms/21-4142/tests/e2e/4142-medial-release.cypress.spec.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import testForm from 'platform/testing/e2e/cypress/support/form-tester';
 import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-tester/utilities';
-import environment from 'platform/utilities/environment';
 import featureToggles from '../../../shared/tests/e2e/fixtures/mocks/feature-toggles.json';
 import user from './fixtures/mocks/user.json';
 import mockSubmit from '../../../shared/tests/e2e/fixtures/mocks/application-submit.json';
@@ -28,44 +27,44 @@ const testConfig = createTestConfig(
         });
       },
       'contact-information-1': ({ afterHook }) => {
-        if (environment.isProduction() && !environment.isTest()) {
-          cy.injectAxeThenAxeCheck();
-          afterHook(() => {
-            cy.get('@testData').then(data => {
-              cy.fillPage();
-              // fillPage doesn't catch state select, so select state manually
-              cy.get('select#root_veteran_address_state').select(
-                data.veteran.address.state,
-              );
-              if (data.veteran.address.city) {
-                if (data.veteran.address.isMilitary) {
-                  // there is a select dropdown instead when military is checked
-                  cy.get('select#root_veteran_address_city').select(
-                    data.veteran.address.city,
-                  );
-                } else {
-                  cy.get('#root_veteran_address_city').type(
-                    data.veteran.address.city,
-                  );
+        cy.injectAxeThenAxeCheck();
+        afterHook(() => {
+          cy.get('@testData').then(data => {
+            cy.get('body').then(body => {
+              // if the environment is production
+              if (
+                !body.find(`va-text-input[name="root_veteran_address_street"]`)
+                  .length
+              ) {
+                cy.fillPage();
+                // fillPage doesn't catch state select, so select state manually
+                cy.get('select#root_veteran_address_state').select(
+                  data.veteran.address.state,
+                );
+                if (data.veteran.address.city) {
+                  if (data.veteran.address.isMilitary) {
+                    // there is a select dropdown instead when military is checked
+                    cy.get('select#root_veteran_address_city').select(
+                      data.veteran.address.city,
+                    );
+                  } else {
+                    cy.get('#root_veteran_address_city').type(
+                      data.veteran.address.city,
+                    );
+                  }
                 }
+              } else {
+                fillAddressWebComponentPattern(
+                  'veteran_address',
+                  data.veteran.address,
+                );
               }
-              cy.axeCheck();
-              cy.findByText(/continue/i, { selector: 'button' }).click();
             });
+            cy.findByText(/continue/i, { selector: 'button' })
+              .last()
+              .click();
           });
-        } else {
-          afterHook(() => {
-            cy.get('@testData').then(data => {
-              fillAddressWebComponentPattern(
-                'veteran_address',
-                data.veteran.address,
-              );
-              cy.findByText(/continue/i, { selector: 'button' })
-                .last()
-                .click();
-            });
-          });
-        }
+        });
       },
       'patient-identification-1': ({ afterHook }) => {
         cy.injectAxeThenAxeCheck();
