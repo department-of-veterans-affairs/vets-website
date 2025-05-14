60d41f0b176e94cc1c81aa96fad5d49a194e351a
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-05-14 13:52:27 -0600

    Adjust component order

--- src/applications/mhv-medications/containers/Prescriptions.jsx
+++ src/applications/mhv-medications/containers/Prescriptions.jsx
@@ -812,6 +812,7 @@ const Prescriptions = () => {
               {!isLoading && renderMedicationsList()}
               {!isLoading && (
                 <>
+                  <BeforeYouDownloadDropdown page={pageType.LIST} />
                   <PrintDownload
                     onDownload={handleFullListDownload}
                     isSuccess={
@@ -825,7 +826,6 @@ const Prescriptions = () => {
                     }
                     list
                   />
-                  <BeforeYouDownloadDropdown page={pageType.LIST} />
                 </>
               )}
             </>
