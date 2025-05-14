a225d46ee492bed17045be59d0d308ef9969e2ed
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2025-05-14 08:39:07 -0600

    Use Personal Information Page Pattern

--- src/applications/income-and-asset-statement/config/chapters/01-veteran-and-claimant-information/index.js
+++ src/applications/income-and-asset-statement/config/chapters/01-veteran-and-claimant-information/index.js
@@ -1,17 +1,40 @@
+import { personalInformationPage } from 'platform/forms-system/src/js/components/PersonalInformation';
 import claimantType from './claimantType';
-import personalInformation from './personalInformation';
 import veteranInformation from './veteranInformation';
 import claimantInformation from './claimantInformation';
+import contactInformation from './contactInformation';
+import editEmailAddress from './editEmailAddress';
 import emailAddress from './emailAddress';
 import phoneNumber from './phoneNumber';
 import incomeNetWorthDateRange from './incomeNetWorthDateRange';
+
+const customConfig = {
+  key: 'personalInformation',
+  path: 'personal/information',
+  depends: formData => {
+    const hasSession = localStorage.getItem('hasSession') === 'true';
+    return formData?.claimantType === 'VETERAN' && hasSession;
+  },
+  personalInfoConfig: {
+    name: { show: true, required: true },
+    ssn: { show: true, required: true },
+    vaFileNumber: { show: true, required: false },
+  },
+  // Temporarily use form data until pre-fill is wired up
+  dataAdapter: {
+    ssnPath: 'veteranSocialSecurityNumber',
+    vaFileNumber: 'vaFileNumber',
+  },
+};
 
 export default {
   title: 'Veteran and claimant information',
   pages: {
     claimantType,
-    personalInformation,
+    ...personalInformationPage(customConfig),
     claimantInformation,
+    contactInformation,
+    editEmailAddress,
     emailAddress,
     phoneNumber,
     veteranInformation,
