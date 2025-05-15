5dd9b91e6f38c4ef672b907180e7a531564927b3
Trevor Pierce <934879+1Copenut@users.noreply.github.com>
2025-05-15 15:09:32 -0500

    Added JSDoc to assertChildText helper method.

--- src/platform/testing/e2e/cypress/support/commands/textHelpers.js
+++ src/platform/testing/e2e/cypress/support/commands/textHelpers.js
@@ -1,5 +1,14 @@
-Cypress.Commands.add('assertChildText', (parentSelect, childSelector, text) => {
-  cy.get(parentSelect)
-    .find(childSelector)
-    .should('contain', text);
-});
+/**
+ * @description Asserts that a child element contains the specified text.
+ * @param {string} parentSelector - Parent selector passed to cy.get().
+ * @param {string} childSelector - Child selector passed to cy.find().
+ * @param {string} text - The text to assert exists in the child element.
+ */
+Cypress.Commands.add(
+  'assertChildText',
+  (parentSelector, childSelector, text) => {
+    cy.get(parentSelector)
+      .find(childSelector)
+      .should('contain', text);
+  },
+);
