adbac0976a44692d8a820fe3e4dc6a3ad1822683
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-05-19 12:29:09 -0600

    Update app-entry.jsx

--- src/applications/mhv-medical-records/app-entry.jsx
+++ src/applications/mhv-medical-records/app-entry.jsx
@@ -2,6 +2,7 @@ import '@department-of-veterans-affairs/platform-polyfills';
 import './sass/medical-records.scss';
 import '~/platform/mhv/secondary-nav/sass/mhv-sec-nav.scss';
 
+// test comment
 import startApp from '@department-of-veterans-affairs/platform-startup/router';
 import routes from './routes';
 import reducer from './reducers';
