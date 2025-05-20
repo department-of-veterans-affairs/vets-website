0e27ff043f67f8426a34fc254fbb6f34785f904d
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2025-05-20 09:28:03 -0700

    Updates jsdoc
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>

--- src/applications/vaos/services/slot/index.js
+++ src/applications/vaos/services/slot/index.js
@@ -20,11 +20,18 @@ import { transformV2Slots } from './transformers';
  * Fetch appointment slots based on start/end date times based on a VistA sites
  * availability for a particular type of care
  *
+ * If siteId is a facility that uses VistA for scheduling then the clinicId parameter is required and the typeOfCareId
+ * parameter is ignored if present.
+ *
+ * If siteId is a facility that uses Oracle Health (Cerner) for scheduling then the typeOfCareId parameter is required and
+ * the clinicId is ignored if present.
+ *
  * @export
  * @async
  * @param {Object} slotsRequest - An object containing the parameters necessary to retrieve appointment slots
  * @param {string} slotsRequest.siteId 3 digit facility ID
  * @param {string} slotsRequest.clinicId clinic id
+ * @param {string} slotsRequest.typeOfCareId The type of care id (e.g. 'primaryCare')
  * @param {string} slotsRequest.startDate start date to search for appointments lots formatted as YYYY-MM-DD
  * @param {string} slotsRequest.endDate end date to search for appointments lots formatted as YYYY-MM-DD
  * @returns {Array<Slot>} A list of Slot resources
