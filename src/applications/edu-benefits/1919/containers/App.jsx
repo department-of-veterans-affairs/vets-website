8bd41d5f0a20c0e06453ab4ecd1d12c6698f0ed3
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-05-20 09:02:00 -0700

    add initial components and markup

--- src/applications/edu-benefits/1919/containers/App.jsx
+++ src/applications/edu-benefits/1919/containers/App.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from '../config/form';
+import Breadcrumbs from '../components/Breadcrumbs';
 
 export default function App({ location, children }) {
   return (
-    <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-      {children}
-    </RoutedSavableApp>
+    <div className="form-22-10215-container row">
+      <div className="vads-u-padding-left--0">
+        <Breadcrumbs />
+      </div>
+      <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+        {children}
+      </RoutedSavableApp>
+    </div>
   );
 }
