bc6b93bd18ab1ae72debc53d8667493556bb0859
Bryan Alexander <bryan.alexander@adhocteam.us>
2025-05-20 12:16:39 -0400

    107830: Remove unused function

--- src/applications/burials-ez/utils/validation.js
+++ src/applications/burials-ez/utils/validation.js
@@ -1,25 +1,3 @@
-import { isBefore, parseISO } from 'date-fns';
-
-export function validateBurialAndDeathDates(errors, page) {
-  const { burialDate, deathDate, veteranDateOfBirth } = page;
-  if (
-    burialDate &&
-    deathDate &&
-    isBefore(parseISO(burialDate), parseISO(deathDate))
-  ) {
-    errors.burialDate.addError(
-      'Date of burial must be on or after date of death',
-    );
-  }
-  if (
-    deathDate &&
-    veteranDateOfBirth &&
-    isBefore(parseISO(deathDate), parseISO(veteranDateOfBirth))
-  ) {
-    errors.deathDate.addError('Date of death must be after date of birth');
-  }
-}
-
 // Benefits Intake API metadata name validation only accepts a-z, A-Z, hyphen, and whitespace.
 // When generating the metadata, we erase all invalid characters. If this leaves an empty string,
 // the submission will fail.
