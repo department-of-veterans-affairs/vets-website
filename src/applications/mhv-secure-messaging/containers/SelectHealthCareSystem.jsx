3c913aab9e20f7a2d827843402aa9c535eeca78a
Alex Morgun <87077843+alex-morgun-1@users.noreply.github.com>
2025-05-22 13:25:15 -0400

    MHV-71327 placeholder component for select health care

---
+++ src/applications/mhv-secure-messaging/containers/SelectHealthCareSystem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SelectHealthCareSystem = () => {
+  return (
+    <div
+      className="vads-l-col--12
+      medium-screen:vads-l-col--9"
+      data-testid="secure-messaging"
+    >
+      <h1>Select Health Care System</h1>
+      <p>This is a placeholder for the Select Health Care System page.</p>
+    </div>
+  );
+};
+
+export default SelectHealthCareSystem;
