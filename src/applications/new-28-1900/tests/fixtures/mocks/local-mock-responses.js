8d08cd0d66b476f5b9a50d5c34d16f3153adef6d
Nick Sayre <nick.sayre@adhocteam.us>
2025-05-22 14:29:41 -0500

    1478 - clean up 3

--- src/applications/new-28-1900/tests/fixtures/mocks/local-mock-responses.js
+++ src/applications/new-28-1900/tests/fixtures/mocks/local-mock-responses.js
@@ -1,10 +1,9 @@
 // use this file to mock api responses for local development
-// yarn mock-api --responses ./src/applications/ivc-champva/10-7959f-1/tests/e2e/fixtures/mocks/local-mock-responses.js
+// yarn mock-api --responses src/applications/new-28-1900/tests/fixtures/mocks/local-mock-responses.js
 const mockUser = require('./user.json');
 const mockFeatureToggles = require('./featureToggles.json');
-const mockSipPut = require('./sip-put.json');
-const mockSipGet = require('./sip-get.json');
-const mockUpload = require('./upload.json');
+// const mockSipPut = require('./sip-put.json');
+// const mockSipGet = require('./sip-get.json');
 const submissionStatues = require('./submission-statuses.json');
 
 
@@ -30,13 +29,7 @@ const responses = {
   },
   'GET /v0/onsite_notifications': { data: [] },
   'GET /v0/debts': { data: [] },
-  'GET /v0/health_care_applications/enrollment_status': {
-    applicationDate: '2006-01-30T00:00:00.000-06:00',
-    enrollmentDate: '2006-03-20T00:00:00.000-06:00',
-    preferredFacility: '626A4 - ALVIN C. YORK VAMC',
-    effectiveDate: '2018-04-28T18:21:56.000-05:00',
-    parsedStatus: 'enrolled',
-  },
+  'GET /v0/health_care_applications/enrollment_status': {},
   'GET /v0/medical_copays': { data: [] },
   'GET /v0/my_va/submission_statuses': submissionStatues,
 };
