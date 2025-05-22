e6fc27da94976397fe50698cec92ba31066ee3ed
govcioChad <162637891+govcioChad@users.noreply.github.com>
2025-05-22 10:18:56 -0700

    #vebt-1942 #comment Added checkbox and title logic

--- src/applications/edu-benefits/8794/pages/primaryOfficialTraining.js
+++ src/applications/edu-benefits/8794/pages/primaryOfficialTraining.js
@@ -1,49 +1,13 @@
-import React from 'react';
 import {
-  titleUI,
   currentOrPastDateUI,
   currentOrPastDateSchema,
-  descriptionUI,
 } from 'platform/forms-system/src/js/web-component-patterns';
-import VaCheckboxField from 'platform/forms-system/src/js/web-component-fields/VaCheckboxField';
 import PrimaryOfficialExemptInfo from '../components/primaryOfficialExemptInfo';
-
-const titleLabel = formData => {
-  // console.log('formdata', formData);
-  if (formData?.primaryOfficial) {
-    return `${formData.primaryOfficial?.first} ${
-      formData.primaryOfficial?.last
-    }'s Section 305 training`;
-  }
-  return 'Section 305 training';
-};
+import PrimaryOfficialTrainingInfo from '../components/primaryOfficialTrainingInfo';
 
 const uiSchema = {
   primaryOfficialTraining: {
-    ...titleUI(titleLabel),
-    'ui:description': (
-      <>
-        <p className="vads-u-margin-top--2">
-          <strong>New School Certifying Officials:</strong> All newly designated
-          certifying officials must complete required online training for new
-          certifying officials based on their type of facility and provide a
-          copy of their training certificate when submitting this form. Enter
-          the date the new certifying official training was completed.
-        </p>
-        <p className="vads-u-margin-top--2">
-          <strong>Existing School Certifying Officials:</strong> Existing SCOs
-          at covered educational institutions must complete a certain number of
-          training modules/training events based on their facility or program
-          type, annually, to maintain their access to certifying enrollments to
-          VA.
-          <va-link
-            href="https://www.benefits.va.gov/gibill/resources/education_resources/school_certifying_officials/online_sco_training.asp#existing"
-            text="Go to this page to find out what's required"
-            external
-          />
-        </p>
-      </>
-    ),
+    'ui:description': PrimaryOfficialTrainingInfo,
     trainingDate: {
       ...currentOrPastDateUI({
         title:
@@ -54,35 +18,24 @@ const uiSchema = {
         },
       }),
     },
-    'view:trainingExempt': {
-      ...descriptionUI(PrimaryOfficialExemptInfo),
-      'ui:webComponentField': VaCheckboxField,
+    trainingExempt: {
+      'ui:field': PrimaryOfficialExemptInfo,
     },
+    'ui:options': {
+      updateSchema: (formData, formSchema) => {
+        if (formData.primaryOfficialTraining?.trainingExempt) {
+          return {
+            ...formSchema,
+            required: [],
+          };
+        }
 
-    // 'ui:options': {
-    //   updateSchema: (formData, formSchema) => {
-    //     console.log('formdata in update', formData);
-    //     // if (formData.primaryOfficial?.phoneType === 'us') {
-    //     //   return {
-    //     //     ...formSchema,
-    //     //     required: ['title', 'phoneType', 'phoneNumber', 'emailAddress'],
-    //     //   };
-    //     // }
-    //     // if (formData.primaryOfficial?.phoneType === 'intl') {
-    //     //   return {
-    //     //     ...formSchema,
-    //     //     required: [
-    //     //       'title',
-    //     //       'phoneType',
-    //     //       'internationalPhoneNumber',
-    //     //       'emailAddress',
-    //     //     ],
-    //     //   };
-    //     // }
-
-    //     return { ...formSchema };
-    //   },
-    // },
+        return {
+          ...formSchema,
+          required: ['trainingDate'],
+        };
+      },
+    },
   },
 };
 
@@ -93,10 +46,8 @@ const schema = {
       type: 'object',
       properties: {
         trainingDate: currentOrPastDateSchema,
-        'view:trainingExempt': {
-          type: 'object',
-          required: [],
-          properties: {},
+        trainingExempt: {
+          type: 'boolean',
         },
       },
       required: ['trainingDate'],
