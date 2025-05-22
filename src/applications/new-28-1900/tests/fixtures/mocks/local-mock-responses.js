c6964488b5bed67dcf2daa6e4c85aabbd4bf6cff
Nick Sayre <nick.sayre@adhocteam.us>
2025-05-22 12:04:04 -0500

    1478 - attempt to get SIP draft to display on profile 2

--- src/applications/new-28-1900/tests/fixtures/mocks/local-mock-responses.js
+++ src/applications/new-28-1900/tests/fixtures/mocks/local-mock-responses.js
@@ -1,13 +1,26 @@
-// yarn mock-api --responses ./src/applications/{application}/tests/e2e/fixtures/mocks/local-mock-responses.js
+// use this file to mock api responses for local development
+// yarn mock-api --responses ./src/applications/ivc-champva/10-7959f-1/tests/e2e/fixtures/mocks/local-mock-responses.js
 const mockUser = require('./user.json');
+const mockFeatureToggles = require('./featureToggles.json');
 const mockSipPut = require('./sip-put.json');
 const mockSipGet = require('./sip-get.json');
-const mockSubmissionStatuses = require('./submission-statuses.json');
+const mockUpload = require('./upload.json');
+
+const mockSubmit = {
+  confirmationNumber: '48fac28c-b332-4549-a45b-3423297111f4',
+};
 
 const responses = {
   'GET /v0/user': mockUser,
-  'GET /v0/in_progress_forms/28-1900-V2': mockSipGet,
-  'PUT /v0/in_progress_forms/28-1900-V2': mockSipPut,
+
+  'OPTIONS /v0/maintenance_windows': 'OK',
+  'GET /v0/maintenance_windows': { data: [] },
+  'GET /v0/feature_toggles': mockFeatureToggles,
+  'GET /v0/in_progress_forms/10-7959F-1': mockSipGet,
+  'PUT /v0/in_progress_forms/10-7959F-1': mockSipPut,
+  'POST /ivc_champva/v1/forms/submit_supporting_documents': mockUpload,
+  'POST /ivc_champva/v1/forms': mockSubmit,
+
   // Mock responses for the profile page
   'GET /v0/profile/full_name': {
     id: '',
@@ -29,10 +42,7 @@ const responses = {
     parsedStatus: 'enrolled',
   },
   'GET /v0/medical_copays': { data: [] },
-  'GET /v0/maintenance_windows': { data: [] },
-  'GET /v0/feature_toggles': { data: [] },
-
-  'GET /v0/my_va/submission_statuses': mockSubmissionStatuses,
 };
 
 module.exports = responses;
+/* eslint-enable camelcase */
