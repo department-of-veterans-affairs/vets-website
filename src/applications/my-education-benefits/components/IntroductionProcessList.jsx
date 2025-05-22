4c1c94521dc13d40102b64cd49cddd3f98c702e5
Brad Bergeron <bergeron.bradley@gmail.com>
2025-05-22 13:00:10 -0400

    chore(meb): additional text polishing in IntroductionProcessList

--- src/applications/my-education-benefits/components/IntroductionProcessList.jsx
+++ src/applications/my-education-benefits/components/IntroductionProcessList.jsx
@@ -61,7 +61,7 @@ export const IntroductionProcessList = ({ showTextUpdate }) => {
                 Reserve; <strong>or</strong>
               </li>
               <li>
-                You’re an officer in the Selected Reserves who agreed to serve 6
+                You’re an officer in the Selected Reserve who agreed to serve 6
                 years beyond your initial service obligation;{' '}
                 <strong>and</strong>
               </li>
