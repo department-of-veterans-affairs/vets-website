44d58a839bccfa90ef07d1a780aa860b2dbb1c49
Trevor Pierce <934879+1Copenut@users.noreply.github.com>
2025-05-23 09:55:13 -0500

    Added new layout tests to DownloadLetterLink component.

--- src/applications/letters/tests/components/DownloadLetterLink.unit.spec.jsx
+++ src/applications/letters/tests/components/DownloadLetterLink.unit.spec.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
 import sinon from 'sinon';
-import { expect } from 'chai';
-import set from 'platform/utilities/data/set';
+import { render, fireEvent } from '@testing-library/react';
 import { createStore } from 'redux';
+import { expect } from 'chai';
 import { Provider } from 'react-redux';
 
+import set from 'platform/utilities/data/set';
 import { getFormDOM } from 'platform/testing/unit/schemaform-utils';
 import { DownloadLetterLink } from '../../components/DownloadLetterLink.jsx';
 import { DOWNLOAD_STATUSES } from '../../utils/constants';
@@ -134,6 +134,125 @@ describe('[TOGGLER OFF]: <DownloadLetterLink>', () => {
     );
     expect(tree.textContent).to.contain(
       'Your letter has successfully downloaded.',
+    );
+  });
+
+  it('should show failure message', () => {
+    const props = {
+      ...defaultProps,
+      downloadStatus: DOWNLOAD_STATUSES.failure,
+    };
+    const { container } = render(
+      <Provider store={getStore()}>
+        <DownloadLetterLink {...props} />,
+      </Provider>,
+    );
+    const tree = getFormDOM(container);
+
+    expect(tree.getElement('va-button').text).to.contain('Retry download');
+    expect(tree.textContent).to.contain('Your letter didn’t download.');
+  });
+});
+
+describe('[TOGGLER ON]: <DownloadLetterLink>', () => {
+  it('should render', () => {
+    const { container } = render(
+      <Provider store={getStore(true)}>
+        <DownloadLetterLink {...defaultProps} />,
+      </Provider>,
+    );
+    const tree = getFormDOM(container);
+    expect(tree.getElement('div')).to.not.be.null;
+  });
+
+  it('should show download button', () => {
+    const props = {
+      letterTitle: 'Download Benefit Summary Letter',
+      letterType: 'benefit_summary',
+    };
+
+    const { container } = render(
+      <Provider store={getStore(true)}>
+        <DownloadLetterLink {...props} />,
+      </Provider>,
+    );
+    const tree = getFormDOM(container);
+    expect(tree.getElement('va-button').text).to.equal(
+      'Download Benefit Summary Letter (PDF)',
+    );
+  });
+
+  it('should call getLetterPdf when clicked', () => {
+    const oldDataLayer = global.window.dataLayer;
+    global.window.dataLayer = [];
+
+    const getLetterPdf = sinon.spy();
+    const props = set(
+      'getLetterPdf',
+      getLetterPdf,
+      defaultProps,
+      lhMigrationOptions,
+    );
+    const { container } = render(
+      <Provider store={getStore(true)}>
+        <DownloadLetterLink {...props} />,
+      </Provider>,
+    );
+    const button = container.querySelector('va-button');
+    fireEvent.click(button);
+
+    expect(getLetterPdf.called).to.be.true;
+    expect(getLetterPdf.callCount).to.equal(1);
+
+    expect(getLetterPdf.args[0]).to.eql([
+      defaultProps.letterType,
+      defaultProps.letterTitle,
+      undefined,
+      undefined,
+    ]);
+    expect(global.window.dataLayer[0]).to.eql({
+      event: 'letter-download',
+      'letter-type': defaultProps.letterType,
+    });
+
+    // Cleanup on aisle 5
+    global.window.dataLayer = oldDataLayer;
+  });
+
+  it('should update button when status is downloading', () => {
+    const props = {
+      ...defaultProps,
+      downloadStatus: DOWNLOAD_STATUSES.downloading,
+    };
+    const { container } = render(
+      <Provider store={getStore(true)}>
+        <DownloadLetterLink {...props} />,
+      </Provider>,
+    );
+    const tree = getFormDOM(container);
+    const button = tree.getElement('va-button');
+
+    expect(button.text).to.equal('Downloading...');
+    expect(button.disabled).to.be.true;
+  });
+
+  it('should show success message', () => {
+    const props = {
+      ...defaultProps,
+      downloadStatus: DOWNLOAD_STATUSES.success,
+    };
+    const { container } = render(
+      <Provider store={getStore(true)}>
+        <DownloadLetterLink {...props} />,
+      </Provider>,
+    );
+    const tree = getFormDOM(container);
+
+    expect(tree.getElement('va-button').text).to.equal(
+      'Commissary Letter (PDF)',
+    );
+    expect(tree.textContent).to.contain(
+      'Your letter is readyIf you want to download your letter again, press the Download Benefit Summary button below.',
     );
   });
 
