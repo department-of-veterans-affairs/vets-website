4a433bcf29d175db644b755b93c28677da88fa30
fazilqa <127263470+fazilqa@users.noreply.github.com>
2025-05-27 14:24:14 -0700

    simplify the test

--- src/applications/mhv-secure-messaging/tests/e2e/pilot-env-tests/secure-messaging-pilot-new-message-urls.cypress.spec.js
+++ src/applications/mhv-secure-messaging/tests/e2e/pilot-env-tests/secure-messaging-pilot-new-message-urls.cypress.spec.js
@@ -2,8 +2,9 @@ import SecureMessagingSite from '../sm_site/SecureMessagingSite';
 import { AXE_CONTEXT, Paths } from '../utils/constants';
 import mockFeatureToggles from '../fixtures/toggles-response.json';
 import PilotEnvPage from '../pages/PilotEnvPage';
+import GeneralFunctionsPage from '../pages/GeneralFunctionsPage';
 
-describe('Secure Messaging Pilot feature flag', () => {
+describe('SM PILOT NEW MESSAGE', () => {
   const pilotFeatureFlag = {
     name: 'mhv_secure_messaging_cerner_pilot',
     value: true,
@@ -16,22 +17,18 @@ describe('Secure Messaging Pilot feature flag', () => {
     },
   };
 
-  it('pilot ON inbox page view', () => {
+  it('verify url by direct navigation', () => {
     SecureMessagingSite.login(mockPilotFeatureToggles);
-
     PilotEnvPage.loadInboxMessages();
 
-    PilotEnvPage.verifyUrl(Paths.UI_PILOT);
-    cy.get(`[data-testid="compose-message-link"]`).click();
-    cy.get(`[data-testid="continue-button"]`).click();
-    cy.url().should('include', `select-health-care-system`);
-    cy.get('h1').should(
-      'have.text',
-      'Which VA health care system do you want to send a message to?',
+    cy.visit(`${Paths.UI_PILOT}/new-message/select-health-care-system`);
+    GeneralFunctionsPage.verifyPageHeader(
+      `Which VA health care system do you want to send a message to?`,
     );
-    cy.get(`va-radio-option`).should('have.length.at.least', 1);
 
-    cy.injectAxe();
-    cy.axeCheck(AXE_CONTEXT);
+    cy.visit(`${Paths.UI_PILOT}/new-message/start-message`);
+    GeneralFunctionsPage.verifyPageHeader(`Start a new message`);
+
+    cy.injectAxeThenAxeCheck(AXE_CONTEXT);
   });
 });
