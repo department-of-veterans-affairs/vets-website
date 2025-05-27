70c41fabafca52791d6faeb5294f72f63b90fc6d
Patrick Brown <patrick.brown@oddball.io>
2025-05-27 16:54:45 -0400

    arf/form_upload_e2e_tests -passing locally without rails server running

--- src/applications/representative-form-upload/tests/e2e/form-upload.cypress.spec.js
+++ src/applications/representative-form-upload/tests/e2e/form-upload.cypress.spec.js
@@ -1,6 +1,7 @@
 import user from './fixtures/mocks/user.json';
 import prefillTransformer from './fixtures/data/transformed/prefill-transformer.json';
 import mockScannedFormUpload from './fixtures/mocks/scanned-form-upload.json';
+import { setFeatureToggles } from './intercepts/features-toggles';
 
 const mockSubmit = JSON.stringify({
   // eslint-disable-next-line camelcase
@@ -12,12 +13,34 @@ const fillTextWebComponent = (fieldName, value) => {
 };
 
 const data = prefillTransformer.formData;
+
 Cypress.Commands.add('loginArpUser', () => {
   cy.intercept('GET', '/accredited_representative_portal/v0/user', {
     statusCode: 200,
     body: user,
   }).as('fetchUser');
 });
+
+Cypress.Commands.add('denyArpUser', () => {
+  cy.intercept('GET', '/accredited_representative_portal/v0/user', {
+    statusCode: 401,
+    body: user,
+  }).as('denyUser');
+});
+
+const vamcUser = {
+  data: {
+    nodeQuery: {
+      count: 0,
+      entities: [],
+    },
+  },
+};
+
+const setUpIntercepts = featureToggles => {
+  cy.intercept('GET', '/data/cms/vamc-ehr.json', vamcUser).as('vamcUser');
+  setFeatureToggles(featureToggles);
+};
 
 const uploadImgPath =
   'src/applications/representative-form-upload/tests/e2e/fixtures/data/vba_21_686c.pdf';
@@ -26,6 +49,10 @@ describe('Representative Form Upload', () => {
   describe('Authorized VSO Rep', () => {
     beforeEach(() => {
       cy.loginArpUser();
+      setUpIntercepts({
+        isAppEnabled: true,
+        isInPilot: true,
+      });
       cy.intercept(
         'POST',
         '/accredited_representative_portal/v0/representative_form_upload',
@@ -39,24 +66,29 @@ describe('Representative Form Upload', () => {
     });
 
     it('allows veteran claimant submission', () => {
-      cy.visit('/representative/representative-form-upload/21-686c/');
+      cy.visit('/representative/representative-form-upload/21-686c');
       cy.location('pathname').should(
         'eq',
         '/representative/representative-form-upload/21-686c/introduction',
       );
+
       cy.get('a[href="#start"]')
         .contains('Start application')
         .click();
+
       cy.location('pathname').should(
         'eq',
         '/representative/representative-form-upload/21-686c/is-veteran',
       );
+
       cy.findByLabelText(/^Yes$/).click();
       cy.findByRole('button', { name: /^Continue$/ }).click();
+
       cy.location('pathname').should(
         'eq',
         '/representative/representative-form-upload/21-686c/veteran-information',
       );
+
       fillTextWebComponent('veteranFullName_first', data.veteranFullName.first);
       fillTextWebComponent('veteranFullName_last', data.veteranFullName.last);
       fillTextWebComponent('address_postalCode', data.address.postalCode);
@@ -68,26 +100,30 @@ describe('Representative Form Upload', () => {
       cy.fillVaTextInput(`root_email`, data.email);
 
       cy.findByRole('button', { name: /^Continue$/ }).click();
+
       cy.location('pathname').should(
         'eq',
         '/representative/representative-form-upload/21-686c/upload',
       );
+
       cy.get('va-file-input')
         .shadow()
         .find('input')
-        .selectFile(uploadImgPath, {
-          force: true,
-        });
+        .selectFile(uploadImgPath, { force: true });
       // eslint-disable-next-line cypress/no-unnecessary-waiting
       cy.wait(1000);
+
       cy.findByRole('button', { name: /^Continue$/ }).click();
+
       cy.location('pathname').should(
         'eq',
         '/representative/representative-form-upload/21-686c/review-and-submit',
       );
+
       cy.findByText(/^Submit form/, { selector: 'button' })
         .last()
         .click();
+
       cy.location('pathname').should(
         'eq',
         '/representative/representative-form-upload/21-686c/confirmation',
@@ -165,6 +201,11 @@ describe('Representative Form Upload', () => {
 
   describe('Unauthorized VSO Rep', () => {
     it('should not allow access to the form upload page', () => {
+      cy.denyArpUser();
+      setUpIntercepts({
+        isAppEnabled: true,
+        isInPilot: true,
+      });
       cy.visit('/representative/representative-form-upload/21-686c/');
       cy.location('pathname').should('eq', '/sign-in/');
     });
