210698fc430d429a4d914860d7154096f43e8da9
Jun Tao Luo <john.luo@agile6.com>
2025-05-28 19:23:33 -0400

    Remove unused exports

--- src/applications/vaos/redux/selectors.js
+++ src/applications/vaos/redux/selectors.js
@@ -99,9 +99,6 @@ export const selectFeatureFeSourceOfTruthModality = state =>
 export const selectFeatureFeSourceOfTruthTelehealth = state =>
   toggleValues(state).vaOnlineSchedulingFeSourceOfTruthTelehealth;
 
-export const selectFeatureMhvRouteGuards = state =>
-  toggleValues(state).vaOnlineSchedulingMHVRouteGuards;
-
 export const selectFeatureDirectScheduleAppointmentConflict = state =>
   toggleValues(state).vaOnlineSchedulingDirectScheduleAppointmentConflict;
 
