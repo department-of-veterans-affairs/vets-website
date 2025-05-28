899a872b80759241dd0c7b4f6742def234c04f37
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-05-28 12:20:07 -0700

    EZR: Fix unit tests for emergencyContactUtils and nextOfKinUtils.

--- src/applications/ezr/tests/unit/utils/helpers/emergencyContactUtils.unit.spec.jsx
+++ src/applications/ezr/tests/unit/utils/helpers/emergencyContactUtils.unit.spec.jsx
@@ -97,56 +97,60 @@ describe('Emergency Contact Utils', () => {
   describe('getCancelAddDescription', () => {
     it('should return the cancel add description text', () => {
       const result = getCancelAddDescription();
-      expect(result).to.equal('emergency-contact-cancel-add-description-text');
+      expect(result).to.equal(
+        'If you cancel, we won’t add this person as your emergency contact. You’ll return to a page where you can add a new emergency contact.',
+      );
     });
   });
 
   describe('getCancelEditDescription', () => {
     it('should return the cancel edit description text', () => {
       const result = getCancelEditDescription();
-      expect(result).to.equal('emergency-contact-cancel-edit-description-text');
+      expect(result).to.equal(
+        'If you cancel, you’ll lose any changes you made on this screen. You’ll return to a page where you can review your emergency contact.',
+      );
     });
   });
 
   describe('getCancelEditTitle', () => {
     it('should return the cancel edit title text', () => {
       const result = getCancelEditTitle();
-      expect(result).to.equal('emergency-contact-cancel-edit-title-text');
+      expect(result).to.equal('Cancel editing your emergency contact?');
     });
   });
 
   describe('getCancelAddTitle', () => {
     it('should return the cancel add title text', () => {
       const result = getCancelAddTitle();
-      expect(result).to.equal('emergency-contact-cancel-add-title-text');
+      expect(result).to.equal('Cancel adding your emergency contact?');
     });
   });
 
   describe('getCancelAddYes', () => {
     it('should return the cancel add yes button text', () => {
       const result = getCancelAddYes();
-      expect(result).to.equal('emergency-contact-cancel-add-yes');
+      expect(result).to.equal('Yes, cancel adding');
     });
   });
 
   describe('getCancelAddNo', () => {
     it('should return the cancel add no button text', () => {
       const result = getCancelAddNo();
-      expect(result).to.equal('emergency-contact-cancel-add-no');
+      expect(result).to.equal('No, continue adding this');
     });
   });
 
   describe('getCancelEditYes', () => {
     it('should return the cancel edit yes button text', () => {
       const result = getCancelEditYes();
-      expect(result).to.equal('emergency-contact-cancel-edit-yes');
+      expect(result).to.equal('Yes, cancel editing');
     });
   });
 
   describe('getCancelEditNo', () => {
     it('should return the cancel edit no button text', () => {
       const result = getCancelEditNo();
-      expect(result).to.equal('emergency-contact-cancel-edit-no');
+      expect(result).to.equal('No, continue editing');
     });
   });
 });
