8eeb1fa7f16cfd2c7e06c5dd47bcf1e51f466907
jquispe-oddball <jennifer.quispe@oddball.io>
2025-05-28 14:33:52 -0400

    110135 - error page update

--- src/applications/accredited-representative-portal/components/ErrorBoundary.jsx
+++ src/applications/accredited-representative-portal/components/ErrorBoundary.jsx
@@ -5,11 +5,26 @@ const ErrorBoundary = () => {
   return (
     <ErrorPage>
       <va-alert data-testid="error-message" status="error" visible>
-        <h2 slot="headline">We’re sorry. Something went wrong.</h2>
+        <h2 slot="headline">
+          We couldn’t find an active accreditation linked to your identity.
+        </h2>
         <div>
           <p className="vads-u-margin-y--0">
-            Please refresh this page or check back later. You can also check the
-            system status on the VA.gov homepage.
+            If you are a representative currently accredited with VA, connect
+            the email on file with VA’s Office of General Counsel to your{' '}
+            <a href="https://login.gov/">Login.gov</a>. You can utilize the{' '}
+            <a href="https://www.va.gov/get-help-from-accredited-representative/find-rep/">
+              Find a VA Accredited Representative or VSO tool
+            </a>{' '}
+            to see which email address they have on file.
+          </p>
+          <p className="vads-u-margin-y--0">
+            If you have updated and/or verified your email address and are still
+            seeing this message, contact{' '}
+            <a href="mailto:RepresentativePortalHelp@va.gov">
+              RepresentativePortalHelp@va.gov
+            </a>
+            .
           </p>
         </div>
       </va-alert>
