4a65968e8eb2f7bcafac9d568adb34974cb60360
Brandon Cooper <brandon.cooper@adhocteam.us>
2025-05-29 15:28:25 -0400

    make new FacilityAddress component

---
+++ src/applications/caregivers/tests/unit/components/FacilityAddress.unit.spec.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { expect } from 'chai';
+import { mockFetchFacilitiesResponse } from '../../mocks/fetchFacility';
+import FacilityAddress from '../../../components/FacilityAddress';
+
+describe('CG <FacilityAddress>', () => {
+  const { facilities } = mockFetchFacilitiesResponse;
+  const facility = facilities[0];
+
+  const subject = ({ facility: facilityProp = facility } = {}) => {
+    const { queryByText } = render(<FacilityAddress facility={facilityProp} />);
+    return { queryByText };
+  };
+
+  context('when the pgae renders in the normal form flow', () => {
+    it('should render the facility name and address when data is provied', () => {
+      const { queryByText } = subject();
+      const address = facility.address.physical;
+      expect(queryByText(new RegExp(facility.name))).to.exist;
+      expect(queryByText(new RegExp(address.address1))).to.exist;
+      expect(queryByText(new RegExp(address.address2))).to.exist;
+      expect(queryByText(new RegExp(address.address3))).to.exist;
+    });
+
+    it('should render just the facility name when address is omitted from facility data', () => {
+      const { queryByText } = subject({
+        facility: { ...facility, address: undefined },
+      });
+      const address = facility.address.physical;
+      expect(queryByText(new RegExp(facility.name))).to.exist;
+      expect(queryByText(new RegExp(address.address1))).to.not.exist;
+      expect(queryByText(new RegExp(address.address2))).to.not.exist;
+      expect(queryByText(new RegExp(address.address3))).to.not.exist;
+    });
+  });
+
+  context('when the page renders outside of the normal form flow', () => {
+    it('should gracefully render without any facility data', () => {
+      const { queryByText } = subject({ facility: null });
+      expect(queryByText(new RegExp(facility.name))).to.not.exist;
+    });
+  });
+});
