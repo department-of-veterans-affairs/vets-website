eaa8dc86a76fe5b60c80673cdc277f106b8db90d
Giles Wells <giles@mediagearhead.com>
2025-06-02 14:33:07 -0400

    Quick conditional refactor

--- src/applications/personalization/profile/components/ProfileSubNavItems.jsx
+++ src/applications/personalization/profile/components/ProfileSubNavItems.jsx
@@ -14,7 +14,7 @@ function ProfileSubNavItems({ routes, isLOA3, isInMVI, clickHandler = null }) {
   // or having isBlocked state selector return true
   const filteredRoutes = routes.filter(route => {
     // loa3 check and isBlocked check
-    if ((route.requiresLOA3 && !isLOA3) || (route.requiresLOA3 && isBlocked)) {
+    if (route.requiresLOA3 && (!isLOA3 || isBlocked)) {
       return false;
     }
 
