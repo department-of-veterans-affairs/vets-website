427297f2dd45be908825ba5324238a0042863524
Robert Hasselle <robert.hasselle@oddball.io>
2025-06-02 17:00:21 -0500

    test revert 4142 for ci

--- src/applications/simple-forms/21-4142/pages/personalInformation1.js
+++ src/applications/simple-forms/21-4142/pages/personalInformation1.js
@@ -2,17 +2,8 @@ import React from 'react';
 import fullSchema from 'vets-json-schema/dist/21-4142-schema.json';
 import dateUI from 'platform/forms-system/src/js/definitions/date';
 import { intersection, pick } from 'lodash';
-import {
-  dateOfBirthSchema,
-  dateOfBirthUI,
-  fullNameSchema,
-  fullNameUI,
-  titleUI,
-} from '~/platform/forms-system/src/js/web-component-patterns';
-import environment from 'platform/utilities/environment';
 import { fullNameDeprecatedUI } from '../../shared/definitions/rjsfPatterns';
 import { veteranFields } from '../definitions/constants';
-import { getFullNameLabels } from '../helpers';
 
 const { required, properties } = fullSchema.properties[
   veteranFields.parentObject
@@ -21,48 +12,25 @@ const pageFields = [veteranFields.fullName, veteranFields.dateOfBirth];
 
 /** @type {PageSchema} */
 export default {
-  uiSchema: environment.isProduction()
-    ? {
-        [veteranFields.parentObject]: {
-          'ui:title': (
-            <h3 className="vads-u-color--gray-dark vads-u-margin-top--0">
-              Tell us about the Veteran connected to this authorization
-            </h3>
-          ),
-          [veteranFields.fullName]: fullNameDeprecatedUI,
-          [veteranFields.dateOfBirth]: dateUI('Date of birth'),
-        },
-      }
-    : {
-        [veteranFields.parentObject]: {
-          ...titleUI({
-            title: 'Tell us about the Veteran connected to this authorization',
-          }),
-          [veteranFields.fullName]: fullNameUI(label =>
-            getFullNameLabels(label, false),
-          ),
-          [veteranFields.dateOfBirth]: dateOfBirthUI(),
-        },
-      },
+  uiSchema: {
+    [veteranFields.parentObject]: {
+      'ui:title': (
+        <h3 className="vads-u-color--gray-dark vads-u-margin-top--0">
+          Tell us about the Veteran connected to this authorization
+        </h3>
+      ),
+      [veteranFields.fullName]: fullNameDeprecatedUI,
+      [veteranFields.dateOfBirth]: dateUI('Date of birth'),
+    },
+  },
   schema: {
     type: 'object',
-    properties: environment.isProduction()
-      ? {
-          [veteranFields.parentObject]: {
-            type: 'object',
-            required: intersection(required, pageFields),
-            properties: pick(properties, pageFields),
-          },
-        }
-      : {
-          [veteranFields.parentObject]: {
-            type: 'object',
-            properties: {
-              [veteranFields.fullName]: fullNameSchema,
-              [veteranFields.dateOfBirth]: dateOfBirthSchema,
-            },
-            required: ['fullName', 'dateOfBirth'],
-          },
-        },
+    properties: {
+      [veteranFields.parentObject]: {
+        type: 'object',
+        required: intersection(required, pageFields),
+        properties: pick(properties, pageFields),
+      },
+    },
   },
 };
--- src/applications/simple-forms/21-4142/tests/unit/pages/personalInformation2.unit.spec.jsx
+++ src/applications/simple-forms/21-4142/tests/unit/pages/personalInformation2.unit.spec.jsx
@@ -1,7 +1,4 @@
-import environment from 'platform/utilities/environment';
 import {
-  testNumberOfErrorsOnSubmitForWebComponents,
-  testNumberOfWebComponentFields,
   testNumberOfErrorsOnSubmit,
   testNumberOfFields,
 } from '../../../../shared/tests/pages/pageTests.spec';
@@ -14,40 +11,20 @@ const {
 
 const pageTitle = 'personal information 2';
 
-if (environment.isProduction()) {
-  const expectedNumberOfFields = 3;
-  testNumberOfFields(
-    formConfig,
-    schema,
-    uiSchema,
-    expectedNumberOfFields,
-    pageTitle,
-  );
+const expectedNumberOfFields = 3;
+testNumberOfFields(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfFields,
+  pageTitle,
+);
 
-  const expectedNumberOfErrors = 1;
-  testNumberOfErrorsOnSubmit(
-    formConfig,
-    schema,
-    uiSchema,
-    expectedNumberOfErrors,
-    pageTitle,
-  );
-} else {
-  const expectedNumberOfFields = 3;
-  testNumberOfWebComponentFields(
-    formConfig,
-    schema,
-    uiSchema,
-    expectedNumberOfFields,
-    pageTitle,
-  );
-
-  const expectedNumberOfErrors = 1;
-  testNumberOfErrorsOnSubmitForWebComponents(
-    formConfig,
-    schema,
-    uiSchema,
-    expectedNumberOfErrors,
-    pageTitle,
-  );
-}
+const expectedNumberOfErrors = 1;
+testNumberOfErrorsOnSubmit(
+  formConfig,
+  schema,
+  uiSchema,
+  expectedNumberOfErrors,
+  pageTitle,
+);
