1a860006caab399e70efdd3cd6d2107e18d4ef2c
Robert Hasselle <robert.hasselle@oddball.io>
2025-06-02 16:41:33 -0500

    test ci

--- src/applications/simple-forms/21-4138/tests/e2e/4138-ss.cypress.spec.js
+++ src/applications/simple-forms/21-4138/tests/e2e/4138-ss.cypress.spec.js
@@ -2,7 +2,6 @@ import path from 'path';
 
 import testForm from '~/platform/testing/e2e/cypress/support/form-tester';
 import { createTestConfig } from '~/platform/testing/e2e/cypress/support/form-tester/utilities';
-
 import {
   fillDateWebComponentPattern,
   fillFullNameWebComponentPattern,
