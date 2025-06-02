98ebb6349049657a70dd031063fa95e8890fe2b4
Alex Taker <a.taker@gmail.com>
2025-06-02 10:59:47 -0400

    add mailing address tests

--- src/platform/user/profile/vap-svc/tests/e2e/ProfileInformationFieldController.cypress.spec.jsx
+++ src/platform/user/profile/vap-svc/tests/e2e/ProfileInformationFieldController.cypress.spec.jsx
@@ -232,4 +232,159 @@ describe('Welcome VA Setup - Contact info', () => {
       cy.findAllByRole('alert', { status: 'error' }).should('exist');
     });
   });
+
+  context('Mailing Address - API returns 200', () => {
+    beforeEach(() => {
+      cy.intercept('/v0/profile/addresses', {
+        statusCode: 200,
+        body: {
+          data: {
+            id: '',
+            type:
+              'async_transaction_va_profile_asynctransaction::vaprofile::addresstransaction_transactions',
+            attributes: {
+              transactionId:
+                'mock-update-mailing-address-success-transaction-id',
+              transactionStatus: 'RECEIVED',
+              type: 'AsyncTransaction::VAProfile::EmailTransaction',
+              metadata: [],
+            },
+          },
+        },
+      });
+
+      cy.intercept(
+        '/v0/profile/status/mock-update-mailing-address-success-transaction-id',
+        {
+          statusCode: 200,
+          body: {
+            data: {
+              id: '',
+              type: 'async_transaction_va_profile_mock_transactions',
+              attributes: {
+                transactionId:
+                  'mock-update-mailing-address-success-transaction-id',
+                transactionStatus: 'COMPLETED_SUCCESS',
+                type: 'AsyncTransaction::VAProfile::MockTransaction',
+                metadata: [],
+              },
+            },
+          },
+        },
+      );
+      cy.intercept('/v0/profile/address_validation', {
+        statusCode: 200,
+        body: {
+          addresses: [
+            {
+              address: {
+                addressLine1: '345 Mailing Address St.',
+                city: 'Fulton',
+                stateCode: 'NY',
+                zipCode: '97063',
+              },
+              addressMetaData: {
+                confidenceScore: 88,
+                addressType: 'Domestic',
+                deliveryPointValidation: 'CONFIRMED',
+                residentialDeliveryIndicator: 'RESIDENTIAL',
+              },
+              validationKey: -1565212962,
+            },
+          ],
+        },
+      });
+    });
+
+    it('changing mailing address succeeds - low confidence on address validation', () => {
+      cy.visit('my-va/welcome-va-setup/contact-information');
+
+      cy.findByRole('link', {
+        name: /Edit mailing address/i,
+      }).click();
+
+      cy.injectAxeThenAxeCheck();
+
+      cy.get('va-text-input[name="root_addressLine1"]')
+        .shadow()
+        .find('input')
+        .as('streetInput');
+
+      cy.injectAxeThenAxeCheck();
+
+      cy.get('@streetInput').clear();
+      cy.get('@streetInput').type('12 Madison Ave');
+      cy.findByTestId('save-edit-button').click();
+
+      // goes to confirm maiing address page
+      cy.findByTestId('confirm-address-button').click();
+
+      // redirect to previous page and show save alert
+      cy.url().should('not.contain', 'edit-email-address');
+      cy.findByText('Email address updated').should('exist');
+
+      cy.injectAxeThenAxeCheck();
+    });
+  });
+
+  context('Mailing Address - API returns 500', () => {
+    beforeEach(() => {
+      cy.intercept('/v0/profile/addresses', {
+        statusCode: 500,
+        body: { status: 500, error: 'Internal Server Error', exception: {} },
+      });
+      cy.intercept('/v0/profile/address_validation', {
+        statusCode: 200,
+        body: {
+          addresses: [
+            {
+              address: {
+                addressLine1: '345 Mailing Address St.',
+                city: 'Fulton',
+                stateCode: 'NY',
+                zipCode: '97063',
+              },
+              addressMetaData: {
+                confidenceScore: 88,
+                addressType: 'Domestic',
+                deliveryPointValidation: 'CONFIRMED',
+                residentialDeliveryIndicator: 'RESIDENTIAL',
+              },
+              validationKey: -1565212962,
+            },
+          ],
+        },
+      });
+    });
+    it('changing mailing address fails - low confidence on address validation', () => {
+      cy.visit('my-va/welcome-va-setup/contact-information');
+
+      cy.findByRole('link', {
+        name: /Edit mailing address/i,
+      }).click();
+
+      cy.injectAxeThenAxeCheck();
+
+      cy.get('va-text-input[name="root_addressLine1"]')
+        .shadow()
+        .find('input')
+        .as('streetInput');
+
+      cy.injectAxeThenAxeCheck();
+
+      cy.get('@streetInput').clear();
+      cy.get('@streetInput').type('12 Madison Ave');
+      cy.findByTestId('save-edit-button').click();
+
+      // goes to confirm maiing address page
+      cy.findByTestId('confirm-address-button').click();
+
+      // stay on current page and show error alert
+      cy.url().should(
+        'contain',
+        '/welcome-va-setup/contact-information/edit-mailing-address',
+      );
+      cy.findAllByRole('alert', { status: 'error' }).should('exist');
+    });
+  });
 });
