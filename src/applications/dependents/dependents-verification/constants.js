6e55f1f81bde2d9f6111d7ea1e83d3118d19ffe9
Midge-dev <sean.midgley.dev@gmail.com>
2025-06-02 12:14:44 -0700

    update form titles

--- src/applications/dependents/dependents-verification/constants.js
+++ src/applications/dependents/dependents-verification/constants.js
@@ -1,5 +1,5 @@
-export const TITLE = '21-0538 Dependents verification';
-export const SUBTITLE = 'dependent-benefits (VA Form 21-0538)';
+export const TITLE = 'Verify your VA dependents';
+export const SUBTITLE = 'VA Form 21-0538';
 
 export const FORMAT_YMD_DATE_FNS = 'yyyy-MM-dd';
 export const FORMAT_COMPACT_DATE_FNS = 'MMM d, yyyy';
