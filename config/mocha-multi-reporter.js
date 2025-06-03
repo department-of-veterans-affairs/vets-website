7ca8ab16adfb20a8ad1d4e8d21fa84003f43ff0f
Isaac Gallup <igallup@thesocompany.com>
2025-06-03 14:40:28 -0500

    switching up approach with changes to mocha-multi-reporter filename generation

--- config/mocha-multi-reporter.js
+++ config/mocha-multi-reporter.js
@@ -1,9 +1,11 @@
+const matrix = process.env.STEP || '0';
+
 module.exports = {
   reporterEnabled: 'mocha-junit-reporter, mochawesome',
   mochawesomeReporterOptions: {
     consoleReporter: 'min',
     reportDir: 'mocha/results',
-    reportFilename: 'unit-tests',
+    reportFilename: `unit-tests-${matrix}`,
     overwrite: true,
     html: false,
     json: true,
