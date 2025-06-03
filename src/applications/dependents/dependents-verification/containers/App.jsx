b77f2ba84f15a8ae8ba878edc23a10dbaa95f2ee
Midge-dev <sean.midgley.dev@gmail.com>
2025-06-03 12:43:06 -0700

    remove what isn't needed

--- src/applications/dependents/dependents-verification/containers/App.jsx
+++ src/applications/dependents/dependents-verification/containers/App.jsx
@@ -6,14 +6,10 @@ import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from '../config/form';
 import NoFormPage from '../components/NoFormPage';
 
-export default function App({ location, children, isLoading }) {
+export default function App({ location, children }) {
   const featureToggle = useSelector(
     state => state?.featureToggles?.vaDependentsVerification,
   );
-
-  if (isLoading) {
-    return <va-loading-indicator message="Loading your information..." />;
-  }
 
   return featureToggle ? (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
