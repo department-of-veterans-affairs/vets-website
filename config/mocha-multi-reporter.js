94c43b8ae913597659319aa601c77621814f7f96
Isaac Gallup <igallup@thesocompany.com>
2025-06-04 15:53:29 -0500

    reverting changes to mocha-multi-reporter.js

--- config/mocha-multi-reporter.js
+++ config/mocha-multi-reporter.js
@@ -1,11 +1,9 @@
-const matrix = process.env.STEP || '0';
-
 module.exports = {
   reporterEnabled: 'mocha-junit-reporter, mochawesome',
   mochawesomeReporterOptions: {
     consoleReporter: 'min',
     reportDir: 'mocha/results',
-    reportFilename: `unit-tests-${matrix}`,
+    reportFilename: 'unit-tests',
     overwrite: true,
     html: false,
     json: true,
