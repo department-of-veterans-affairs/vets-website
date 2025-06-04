77bd22c1c7feb7e46839b3f7655669d71f2b90e7
fazilqa <127263470+fazilqa@users.noreply.github.com>
2025-06-04 10:44:20 -0700

    add axe checks

--- src/applications/mhv-secure-messaging/tests/e2e/secure-messaging-thread-load-errors.cypress.spec.js
+++ src/applications/mhv-secure-messaging/tests/e2e/secure-messaging-thread-load-errors.cypress.spec.js
@@ -1,33 +1,30 @@
 import SecureMessagingSite from './sm_site/SecureMessagingSite';
 import PatientErrorPage from './pages/PatientErrorPage';
-import { Paths } from './utils/constants';
+import { AXE_CONTEXT, Paths } from './utils/constants';
 
 describe('Thread list load error', () => {
   it('verify error on particular folder', () => {
     SecureMessagingSite.login();
     PatientErrorPage.loadParticularFolderError();
     PatientErrorPage.verifyAlertMessageText();
+
+    cy.injectAxeThenAxeCheck(AXE_CONTEXT);
   });
 
   it('verify error in My folders', () => {
     SecureMessagingSite.login();
     PatientErrorPage.loadMyFoldersError();
     PatientErrorPage.verifyAlertMessageText();
+
+    cy.injectAxeThenAxeCheck(AXE_CONTEXT);
   });
 
   it('verify 404 page', () => {
     SecureMessagingSite.login();
 
-    cy.visit(`${Paths.UI_MAIN}404`);
-    PatientErrorPage.verifyPageNotFoundContent();
-
     cy.visit(`${Paths.UI_MAIN}/inbox404`);
     PatientErrorPage.verifyPageNotFoundContent();
 
-    cy.visit(`${Paths.UI_MAIN}/new-message/404`);
-    PatientErrorPage.verifyPageNotFoundContent();
-
-    cy.visit(`${Paths.UI_MAIN}/foldersxyz`);
-    PatientErrorPage.verifyPageNotFoundContent();
+    cy.injectAxeThenAxeCheck(AXE_CONTEXT);
   });
 });
