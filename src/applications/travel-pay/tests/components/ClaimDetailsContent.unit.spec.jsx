49f79c86c6827ad8a9390f4daa59833e6e9e0260
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-06-04 10:53:14 -0700

    Test updates

--- src/applications/travel-pay/tests/components/ClaimDetailsContent.unit.spec.jsx
+++ src/applications/travel-pay/tests/components/ClaimDetailsContent.unit.spec.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { expect } from 'chai';
-import { fireEvent } from '@testing-library/react';
 
 import { $ } from 'platform/forms-system/src/js/utilities/ui';
 import { renderWithStoreAndRouter } from '@department-of-veterans-affairs/platform-testing/react-testing-library-helpers';
@@ -16,6 +15,7 @@ describe('ClaimDetailsContent', () => {
     facilityName: 'Tomah VA Medical Center',
     createdOn: '2024-05-27T16:40:45.781Z',
     modifiedOn: '2024-05-31T16:40:45.781Z',
+    totalCostRequested: 50.99,
   };
 
   const getState = ({
@@ -93,10 +93,7 @@ describe('ClaimDetailsContent', () => {
     );
 
     expect(screen.getByText('Claim status: Unexpected status')).to.exist;
-
-    fireEvent.click(
-      $(`va-additional-info[trigger="What does this status mean"]`),
-    );
+    expect(screen.queryByText('What does this status mean')).to.not.exist;
     expect(screen.getByText(/If you need help understanding your claim/i)).to
       .exist;
   });
@@ -120,7 +117,7 @@ describe('ClaimDetailsContent', () => {
       <ClaimDetailsContent
         {...claimDetailsProps}
         claimStatus="Denied"
-        reimbursementAmount={1.0}
+        reimbursementAmount={46.93}
         documents={[
           {
             filename: 'Decision Letter.docx',
@@ -140,9 +137,8 @@ describe('ClaimDetailsContent', () => {
     expect(
       $('va-link[text="Appeal the claim decision"][href="/decision-reviews"]'),
     ).to.not.exist;
-    expect($(`va-additional-info[trigger="What does this status mean?"]`)).to
-      .not.exist;
-    expect(screen.queryByText('Reimbursement amount of $1.00')).to.not.exist;
+    expect(screen.queryByText('What does this status mean')).to.not.exist;
+    expect(screen.queryByText('Reimbursement amount of $46.93')).to.not.exist;
     expect($('va-link[text="Download your decision letter"]')).to.not.exist;
     expect($('va-link[text="screenshot.png"]')).to.not.exist;
   });
