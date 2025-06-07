64da52a30ecec0bdf2b904480b897ddb6c990319
radavis <1299034+radavis@users.noreply.github.com>
2025-06-07 17:01:45 -0400

    Migrates AlertUnregistered component from jsx to tsx

--- src/applications/mhv-landing-page/tests/components/alerts/AlertUnregistered.unit.spec.jsx
+++ src/applications/mhv-landing-page/tests/components/alerts/AlertUnregistered.unit.spec.jsx
@@ -3,16 +3,16 @@ import { render, waitFor } from '@testing-library/react';
 import { expect } from 'chai';
 import sinon from 'sinon';
 
-import AlertUnregistered from '../../../components/alerts/AlertUnregistered';
-
-const { defaultProps } = AlertUnregistered;
+import AlertUnregistered, {
+  defaultProps,
+} from '../../../components/alerts/AlertUnregistered';
 
 describe('<AlertUnregistered />', () => {
   it('renders', async () => {
     const recordEvent = sinon.spy();
     const props = { ...defaultProps, recordEvent };
     const { getByTestId } = render(<AlertUnregistered {...props} />);
-    getByTestId(defaultProps.testId);
+    getByTestId('mhv-alert--unregistered');
     await waitFor(() => {
       expect(recordEvent.calledOnce).to.be.true;
       expect(recordEvent.calledTwice).to.be.false;
