6bba860bc75ba68c986da6deea16c9707ac4db36
Mike Moyer <87040148+mmoyer-va@users.noreply.github.com>
2025-06-09 13:30:05 -0400

    MHV-71368 Paginate vaccines in the backend (#37008)
    * MHV-71341 Added support for the new Vaccines & Conditions models
    
    * MHV-71341 Handle case where no records are returned
    
    * MHV-71341 Fixed null logic and added unit tests for Vaccines
    
    * MHV-71341 Updated null logic and added unit tests
    
    * MHV-71341 Small refactor to use an existing constant
    
    * MHV-71368 First pass at paginating Vaccines in the backend
    
    * MHV-71368 Don't have a default pagination
    
    * MHV-71368 Updated the testHelper for PHR Refresh and added comments
    
    * MHV-71368 Logic changes to support NewRecordsIndicator
    
    * MHV-71368 Forgot to add the page param in recursive call
    
    * Shortened unneessarily long wait times in unit test
    
    * MHV-71368 Added some tests
    
    * MHV-71368 Don't use cached records when checking for update
    
    * MHV-71368 Added feature flags to Cypress tests
    
    * MHV-71368 Removed dangling ? that was messing with tests

--- src/applications/mhv-medical-records/reducers/vaccines.js
+++ src/applications/mhv-medical-records/reducers/vaccines.js
@@ -22,6 +22,12 @@ const initialState = {
    * @type {Array}
    */
   vaccinesList: undefined,
+
+  /** Information about list pagination, sorting, filtering, etc. */
+  listMetadata: undefined,
+
+  updateNeeded: false,
+
   /**
    * New list of records retrieved. This list is NOT displayed. It must manually be copied into the display list.
    * @type {Array}
@@ -155,8 +161,10 @@ export const vaccineReducer = (state = initialState, action) => {
       };
     }
     case Actions.Vaccines.GET_LIST: {
+      const { useBackendPagination } = action;
       const oldList = state.vaccinesList;
       let newList;
+      let metadata;
       if (action.response.resourceType) {
         newList =
           action.response.entry
@@ -169,14 +177,34 @@ export const vaccineReducer = (state = initialState, action) => {
         newList = action.response.data?.map(record =>
           convertNewVaccine(record.attributes),
         );
+        metadata = action.response.meta;
+      }
+
+      let vaccinesList = typeof oldList === 'undefined' ? newList : oldList;
+      let updatedList = typeof oldList !== 'undefined' ? newList : undefined;
+      if (useBackendPagination) {
+        vaccinesList = newList;
+        updatedList = undefined;
       }
 
       return {
         ...state,
         listCurrentAsOf: action.isCurrent ? new Date() : null,
         listState: loadStates.FETCHED,
-        vaccinesList: typeof oldList === 'undefined' ? newList : oldList,
-        updatedList: typeof oldList !== 'undefined' ? newList : undefined,
+        vaccinesList,
+        updatedList,
+        listMetadata: metadata,
+        updateNeeded: false,
+      };
+    }
+    case Actions.Vaccines.CHECK_FOR_UPDATE: {
+      const metadata = action.response.meta;
+      return {
+        ...state,
+        updateNeeded:
+          metadata?.pagination?.totalEntries &&
+          metadata?.pagination?.totalEntries !==
+            state.listMetadata?.pagination?.totalEntries,
       };
     }
     case Actions.Vaccines.COPY_UPDATED_LIST: {
