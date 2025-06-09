7362e0d77bd2c3ab8b0cdace7cab5da8177b150b
Jane Cho <87551323+janechodance@users.noreply.github.com>
2025-06-09 16:31:26 -0400

    add unit tests to letter app

--- src/applications/letters/tests/containers/LetterPageWrapper.unit.spec.jsx
+++ src/applications/letters/tests/containers/LetterPageWrapper.unit.spec.jsx
@@ -3,6 +3,7 @@ import SkinDeep from 'skin-deep';
 import { expect } from 'chai';
 import { MemoryRouter } from 'react-router-dom-v5-compat';
 
+import { render } from '@testing-library/react';
 import LetterPageWrapper from '../../containers/LetterPageWrapper';
 
 describe('<LetterPageWrapper>', () => {
@@ -14,5 +15,14 @@ describe('<LetterPageWrapper>', () => {
     );
     const vdom = tree.getRenderOutput();
     expect(vdom).to.exist;
+  });
+  it('should render an empty address warning on the view screen and disable the View letters button', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={[`/confirm-address`]}>
+        <LetterPageWrapper />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Your VA letters and documents').exist);
   });
 });
