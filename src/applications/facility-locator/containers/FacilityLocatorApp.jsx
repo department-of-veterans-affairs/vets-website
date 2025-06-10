617507df7a7cf09d4d8fae0d0d2987ed8997448a
Darius Dzien <dariusz.dzien@adhocteam.us>
2025-06-10 13:08:36 -0500

    WIP Deeplink demo

--- src/applications/facility-locator/containers/FacilityLocatorApp.jsx
+++ src/applications/facility-locator/containers/FacilityLocatorApp.jsx
@@ -1,15 +1,30 @@
 import { connect } from 'react-redux';
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import appendQuery from 'append-query';
 import DowntimeNotification, {
   externalServices,
 } from 'platform/monitoring/DowntimeNotification';
 import { VaBreadcrumbs } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { validateIdString } from '../utils/helpers';
+export const VA_MOBILE_DEEP_LINK = 'vamobile://claimLetters';
 
-class FacilityLocatorApp extends React.Component {
-  renderBreadcrumbs(location, selectedResult) {
+function FacilityLocatorApp({ location, selectedResult, searchQuery, children }) {
+  const renderBreadcrumbs = (location, selectedResult) => {
     // Map and name props for the search query object
+
+    useEffect(
+      () => {
+        // Get current URL search parameters
+        const searchParams = new URLSearchParams(window.location.search);
+        
+        // Check if 'mobile' parameter exists in the URL
+        if (searchParams.has('mobile')) {
+          window.location.replace(VA_MOBILE_DEEP_LINK);
+        }
+      },
+      [] // Empty dependency array so it only runs once on component mount
+    );
+
     const {
       currentPage: page,
       context,
@@ -17,7 +32,7 @@ class FacilityLocatorApp extends React.Component {
       searchString: address,
       serviceType,
       zoomLevel,
-    } = this.props.searchQuery;
+    } = searchQuery;
 
     // Build the query object in the expected order
     const searchQueryObj = {
@@ -57,30 +72,26 @@ class FacilityLocatorApp extends React.Component {
     }
 
     return crumbs;
-  }
+  };
 
-  render() {
-    const { location, selectedResult } = this.props;
-
-    return (
-      <div className="row">
-        <div className="facility-locator">
-          <VaBreadcrumbs
-            uswds
-            label="Breadcrumbs"
-            breadcrumbList={this.renderBreadcrumbs(location, selectedResult)}
-          />
-        </div>
-        <DowntimeNotification
-          appTitle="facility locator tool"
-          customText={{ appType: 'tool' }}
-          dependencies={[externalServices.arcgis]}
-        >
-          <div className="facility-locator">{this.props.children}</div>
-        </DowntimeNotification>
+  return (
+    <div className="row">
+      <div className="facility-locator">
+        <VaBreadcrumbs
+          uswds
+          label="Breadcrumbs"
+          breadcrumbList={renderBreadcrumbs(location, selectedResult)}
+        />
       </div>
-    );
-  }
+      <DowntimeNotification
+        appTitle="facility locator tool"
+        customText={{ appType: 'tool' }}
+        dependencies={[externalServices.arcgis]}
+      >
+        <div className="facility-locator">{children}</div>
+      </DowntimeNotification>
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
