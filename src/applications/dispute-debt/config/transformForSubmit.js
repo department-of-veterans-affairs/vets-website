fb3be2d5df0ecb044a6081949ab812cbc2dafd5e
Andrew Rodiek <origin>
2025-06-11 08:46:38 -0400

    working on submit and downloading for now

--- src/applications/dispute-debt/config/transformForSubmit.js
+++ src/applications/dispute-debt/config/transformForSubmit.js
@@ -1,10 +1,34 @@
 const transformForSubmit = (formConfig, form) => {
   const { data } = form;
+  const { veteran } = data;
+  const { fullName } = veteran;
 
+  // TODO: localization?
+  const submissionTime = new Date();
+
+  // return formatted data to generate the PDFs
   return {
     ...data,
-    veteranInformation: {
-      ...data.veteran,
+    veteran: {
+      dob: veteran.dateOfBirth,
+      email: veteran.email,
+      ssnLastFour: veteran.ssn,
+      vaFileLastFour: veteran.fileNumber,
+      veteranFullName: {
+        first: fullName.first,
+        middle: fullName.middle,
+        last: fullName.last,
+        suffix: fullName.suffix,
+      },
+      mailingAddress: {
+        ...veteran.mailingAddress,
+      },
+      mobilePhone: {
+        ...veteran.mobilePhone,
+      },
+    },
+    submissionDetails: {
+      submissionDateTime: submissionTime,
     },
   };
 };
