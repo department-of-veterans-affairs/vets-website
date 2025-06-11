8a5b21fec258ed16280bb16972c7dbd6419b17f7
govcioChad <162637891+govcioChad@users.noreply.github.com>
2025-06-11 07:43:12 -0700

    #vebt-2203 #comment Updated GetFormHelp unit test

--- src/applications/edu-benefits/10215/tests/components/GetFormHelp.unit.spec.jsx
+++ src/applications/edu-benefits/10215/tests/components/GetFormHelp.unit.spec.jsx
@@ -16,11 +16,11 @@ describe('<GetFormHelp />', () => {
   it('should contain va-link component', () => {
     const wrapper = shallow(<GetFormHelp />);
 
-    expect(wrapper.find('va-link').length).to.equal(1);
-    expect(wrapper.find('va-link').props().text).to.contain(
+    expect(wrapper.find('a').length).to.equal(1);
+    expect(wrapper.find('a').text()).to.equal(
       'contact your Education Liaison Representative.',
     );
-    expect(wrapper.find('va-link').props().href).to.contain(
+    expect(wrapper.find('a').props().href).to.contain(
       'https://www.benefits.va.gov/gibill/resources/education_resources/school_certifying_officials/elr.asp',
     );
 
