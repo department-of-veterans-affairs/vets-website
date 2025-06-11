8273d43b6423c226778495ea9450761c7e83f2e0
fazilqa <127263470+fazilqa@users.noreply.github.com>
2025-06-11 09:58:15 -0700

    add test

--- src/applications/mhv-secure-messaging/tests/e2e/triage-team-tests/secure-messaging-thread-no-association.cypress.spec.js
+++ src/applications/mhv-secure-messaging/tests/e2e/triage-team-tests/secure-messaging-thread-no-association.cypress.spec.js
@@ -5,6 +5,7 @@ import mockMessages from '../fixtures/threads-response.json';
 import mockSingleMessage from '../fixtures/inboxResponse/single-message-response.json';
 import mockRecipients from '../fixtures/recipientsResponse/recipients-response.json';
 import mockThread from '../fixtures/thread-response.json';
+import mockOHThread from '../fixtures/thread-OH-response.json';
 
 describe('Verify thread - No association with particular Triage Group', () => {
   const updatedData = mockRecipients.data.slice(1);
@@ -22,21 +23,17 @@ describe('Verify thread - No association with particular Triage Group', () => {
       mockSingleMessage,
       removedFirstRecipientsList,
     );
-    cy.injectAxe();
-    cy.axeCheck(AXE_CONTEXT, {
-      rules: {
-        'aria-required-children': {
-          enabled: false,
-        },
-      },
-    });
+
+    cy.injectAxeThenAxeCheck(AXE_CONTEXT);
 
     cy.get(Locators.LINKS.CREATE_NEW_MESSAGE).click({
       waitForAnimations: true,
     });
+
     cy.findByTestId(Locators.BUTTONS.CONTINUE).click({
       waitForAnimations: true,
     });
+
     cy.get(Locators.ALERTS.REPT_SELECT).should(
       'not.contain',
       mockRecipients.data[0].attributes.name,
@@ -68,14 +65,7 @@ describe('Verify thread - No association with particular Triage Group', () => {
     );
     PatientInboxPage.loadSingleThread(threadWithNoAssociatedTG);
 
-    cy.injectAxe();
-    cy.axeCheck(AXE_CONTEXT, {
-      rules: {
-        'aria-required-children': {
-          enabled: false,
-        },
-      },
-    });
+    cy.injectAxeThenAxeCheck(AXE_CONTEXT);
 
     cy.get(Locators.ALERTS.BLOCKED_GROUP)
       .shadow()
@@ -113,5 +103,37 @@ describe('Verify thread - No association with particular Triage Group', () => {
       .and('have.text', Alerts.NO_ASSOCIATION.LINK);
 
     cy.get(Locators.BUTTONS.REPLY).should('not.exist');
+  });
+
+  it('verify OH user can send a message to no associated team', () => {
+    const threadWithNoAssociatedTG = {
+      ...mockOHThread,
+      data: [
+        {
+          ...mockOHThread.data[0],
+          attributes: {
+            ...mockOHThread.data[0].attributes,
+            recipientName: mockRecipients.data[0].attributes.name,
+            triageGroupName: mockRecipients.data[0].attributes.name,
+            recipientId: mockRecipients.data[0].attributes.triageTeamId,
+          },
+        },
+        ...mockOHThread.data,
+      ],
+    };
+    SecureMessagingSite.login();
+
+    PatientInboxPage.loadInboxMessages(
+      mockMessages,
+      mockSingleMessage,
+      removedFirstRecipientsList,
+    );
+
+    PatientInboxPage.loadSingleThread(threadWithNoAssociatedTG);
+
+    cy.get(Locators.ALERTS.BLOCKED_GROUP).should('not.exist');
+    cy.get(Locators.BUTTONS.REPLY).should('be.visible');
+
+    cy.injectAxeThenAxeCheck(AXE_CONTEXT);
   });
 });
