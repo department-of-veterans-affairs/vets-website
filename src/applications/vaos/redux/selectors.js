a72fb3c9b6d5ca85fe436e53603ebe72bc8ab579
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2025-06-13 14:04:13 -0700

    Prepare for using va_online_scheduling_mental_health_history_filtering (#37112)
    Selectors for va_online_scheduling_mental_health_history_filtering

--- src/applications/vaos/redux/selectors.js
+++ src/applications/vaos/redux/selectors.js
@@ -124,3 +124,6 @@ export function getRequestedAppointmentListInfo(state) {
 }
 export const selectFeatureConvertSlotsToUtc = state =>
   toggleValues(state).vaOnlineSchedulingConvertSlotsToUtc;
+
+export const selectFeatureMentalHealthHistoryFiltering = state =>
+  toggleValues(state).vaOnlineSchedulingMentalHealthHistoryFiltering;
