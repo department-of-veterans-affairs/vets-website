4b6294a387ba9ec8a0ed35e6797a5f671b428eff
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-06-13 09:07:31 -0700

    clean directory | create unit tests for list and loop pages

--- src/applications/edu-benefits/1919/pages/index.js
+++ src/applications/edu-benefits/1919/pages/index.js
@@ -1,7 +1,6 @@
 import * as certifyingOfficials from './certifyingOfficials';
 import * as aboutYourInstitution from './aboutYourInstitution';
 import * as institutionDetails from './institutionDetails';
-import * as isProfitConflictOfInterest from './isProfitConflictOfInterest';
 import * as conflictOfInterestCertifyingOfficial from './conflictOfInterestCertifyingOfficial';
 import * as conflictOfInterestFileNumber from './conflictOfInterestFileNumber';
 import * as conflictOfInterestEnrollmentPeriod from './conflictOfInterestEnrollmentPeriod';
@@ -14,7 +13,6 @@ export {
   certifyingOfficials,
   aboutYourInstitution,
   institutionDetails,
-  isProfitConflictOfInterest,
   affiliatedIndividualsSummary,
   affiliatedIndividualsAssociation,
   conflictOfInterestCertifyingOfficial,
