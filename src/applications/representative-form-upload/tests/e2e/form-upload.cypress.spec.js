dadfa42f7f2ae43545b8f99b5d33bd28ec04ebbf
Patrick Brown <patrick.brown@oddball.io>
2025-06-17 13:25:15 -0400

    multiform-upload-frontend-2 - added e2e test

--- src/applications/representative-form-upload/tests/e2e/form-upload.cypress.spec.js
+++ src/applications/representative-form-upload/tests/e2e/form-upload.cypress.spec.js
@@ -60,6 +60,11 @@ describe('Representative Form Upload', () => {
       );
       cy.intercept(
         'POST',
+        '/accredited_representative_portal/v0/upload_supporting_documents',
+        mockScannedFormUpload,
+      );
+      cy.intercept(
+        'POST',
         '/accredited_representative_portal/v0/submit_representative_form',
         mockSubmit,
       );
@@ -110,6 +115,77 @@ describe('Representative Form Upload', () => {
         .shadow()
         .find('input')
         .selectFile(uploadImgPath, { force: true });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(1000);
+
+      cy.findByRole('button', { name: /^Continue$/ }).click();
+
+      cy.location('pathname').should(
+        'eq',
+        '/representative/representative-form-upload/21-686c/review-and-submit',
+      );
+
+      cy.findByText(/^Submit form/, { selector: 'button' })
+        .last()
+        .click();
+
+      cy.location('pathname').should(
+        'eq',
+        '/representative/representative-form-upload/21-686c/confirmation',
+      );
+    });
+
+    it('allows veteran claimant submission with supporting evidence', () => {
+      cy.visit('/representative/representative-form-upload/21-686c');
+      cy.location('pathname').should(
+        'eq',
+        '/representative/representative-form-upload/21-686c/introduction',
+      );
+
+      cy.get('a[href="#start"]')
+        .contains('Start application')
+        .click();
+
+      cy.location('pathname').should(
+        'eq',
+        '/representative/representative-form-upload/21-686c/is-veteran',
+      );
+
+      cy.findByLabelText(/^The claimant is the Veteran$/).click();
+      cy.findByRole('button', { name: /^Continue$/ }).click();
+
+      cy.location('pathname').should(
+        'eq',
+        '/representative/representative-form-upload/21-686c/veteran-information',
+      );
+
+      fillTextWebComponent('veteranFullName_first', data.veteranFullName.first);
+      fillTextWebComponent('veteranFullName_last', data.veteranFullName.last);
+      fillTextWebComponent('address_postalCode', data.address.postalCode);
+      fillTextWebComponent('veteranSsn', data.ssn);
+
+      cy.get('select[name="root_veteranDateOfBirthMonth"]').select('February');
+      cy.get('input[name="root_veteranDateOfBirthDay"]').type('15');
+      cy.get('input[name="root_veteranDateOfBirthYear"]').type('1990');
+      cy.fillVaTextInput(`root_email`, data.email);
+
+      cy.findByRole('button', { name: /^Continue$/ }).click();
+
+      cy.location('pathname').should(
+        'eq',
+        '/representative/representative-form-upload/21-686c/upload',
+      );
+
+      cy.get('va-file-input')
+        .shadow()
+        .find('input')
+        .selectFile(uploadImgPath, { force: true });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(1000);
+
+      cy.get('input#root_supportingDocuments').selectFile(uploadImgPath, {
+        force: true,
+      });
       // eslint-disable-next-line cypress/no-unnecessary-waiting
       cy.wait(1000);
 
