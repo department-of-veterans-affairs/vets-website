4138acd37081daf2b6be5be115df5e492dfcbb4a
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-06-17 16:04:12 -0600

    add back changes

--- src/platform/forms/constants.js
+++ src/platform/forms/constants.js
@@ -156,7 +156,7 @@ export const getAllFormLinks = getAppUrlImpl => {
     [VA_FORM_IDS.FORM_21_10210]: `${tryGetAppUrl(
       '10210-lay-witness-statement',
     )}/`,
-    // [VA_FORM_IDS.FORM_21A]: `${tryGetAppUrl('representative-21a')}/`,
+    [VA_FORM_IDS.FORM_21A]: `${tryGetAppUrl('representative-21a')}/`,
     [VA_FORM_IDS.FORM_21_22]: `${tryGetAppUrl('appoint-a-representative')}/`,
     [VA_FORM_IDS.FORM_21_22A]: `${tryGetAppUrl('appoint-a-representative')}/`,
     [VA_FORM_IDS.FORM_21_4142]: `${tryGetAppUrl('21-4142-medical-release')}/`,
@@ -434,13 +434,13 @@ export const MY_VA_SIP_FORMS = [
     description: 'lay/witness statement application (21-10210)',
     trackingPrefix: 'lay-witness-10210-',
   },
-  // {
-  //   id: VA_FORM_IDS.FORM_21A,
-  //   benefit: `Application for Accreditation as a Claims Agent or Attorney`,
-  //   title: `Application for Accreditation as a Claims Agent or Attorney (21a)`,
-  //   description: 'Claims Agent or Attorney accreditation application (21a)',
-  //   trackingPrefix: '21a-',
-  // },
+  {
+    id: VA_FORM_IDS.FORM_21A,
+    benefit: `Application for Accreditation as a Claims Agent or Attorney`,
+    title: `Application for Accreditation as a Claims Agent or Attorney (21a)`,
+    description: 'Claims Agent or Attorney accreditation application (21a)',
+    trackingPrefix: '21a-',
+  },
   {
     id: VA_FORM_IDS.FORM_21_22,
     benefit: `Appointment of Veterans Service Organization as Claimant's Representative`,
