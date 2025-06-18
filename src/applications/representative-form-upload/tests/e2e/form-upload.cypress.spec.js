475631ba8b369002d4a014c3cfdb820e3a88ad7b
jquispe-oddball <jennifer.quispe@oddball.io>
2025-06-18 17:08:50 -0400

    111576 - test update

--- src/applications/representative-form-upload/tests/e2e/form-upload.cypress.spec.js
+++ src/applications/representative-form-upload/tests/e2e/form-upload.cypress.spec.js
@@ -1,5 +1,4 @@
 import user from './fixtures/mocks/user.json';
-import prefillTransformer from './fixtures/data/transformed/prefill-transformer.json';
 import mockScannedFormUpload from './fixtures/mocks/scanned-form-upload.json';
 import { setFeatureToggles } from './intercepts/features-toggles';
 
@@ -7,12 +6,6 @@ const mockSubmit = JSON.stringify({
   // eslint-disable-next-line camelcase
   confirmation_number: '48fac28c-b332-4549-a45b-3423297111f4',
 });
-
-const fillTextWebComponent = (fieldName, value) => {
-  cy.fillVaTextInput(`root_${fieldName}`, value);
-};
-
-const data = prefillTransformer.formData;
 
 Cypress.Commands.add('loginArpUser', () => {
   cy.intercept('GET', '/accredited_representative_portal/v0/user', {
@@ -42,9 +35,6 @@ const setUpIntercepts = featureToggles => {
   setFeatureToggles(featureToggles);
 };
 
-const uploadImgPath =
-  'src/applications/representative-form-upload/tests/e2e/fixtures/data/vba_21_686c.pdf';
-
 describe('Representative Form Upload', () => {
   describe('Authorized VSO Rep', () => {
     beforeEach(() => {
@@ -62,133 +52,6 @@ describe('Representative Form Upload', () => {
         'POST',
         '/accredited_representative_portal/v0/submit_representative_form',
         mockSubmit,
-      );
-    });
-
-    it('allows veteran claimant submission', () => {
-      cy.visit('/representative/representative-form-upload/21-686c');
-      cy.location('pathname').should(
-        'eq',
-        '/representative/representative-form-upload/21-686c/introduction',
-      );
-
-      cy.get('a[href="#start"]')
-        .contains('Start form')
-        .click();
-
-      cy.location('pathname').should(
-        'eq',
-        '/representative/representative-form-upload/21-686c/is-veteran',
-      );
-
-      cy.findByLabelText(/^The claimant is the Veteran$/).click();
-      cy.findByRole('button', { name: /^Continue$/ }).click();
-
-      cy.location('pathname').should(
-        'eq',
-        '/representative/representative-form-upload/21-686c/veteran-information',
-      );
-
-      fillTextWebComponent('veteranFullName_first', data.veteranFullName.first);
-      fillTextWebComponent('veteranFullName_last', data.veteranFullName.last);
-      fillTextWebComponent('address_postalCode', data.address.postalCode);
-
-      cy.get('select[name="root_veteranDateOfBirthMonth"]').select('February');
-      cy.get('input[name="root_veteranDateOfBirthDay"]').type('15');
-      cy.get('input[name="root_veteranDateOfBirthYear"]').type('1990');
-
-      cy.findByRole('button', { name: /^Continue$/ }).click();
-
-      cy.location('pathname').should(
-        'eq',
-        '/representative/representative-form-upload/21-686c/upload',
-      );
-
-      cy.get('va-file-input')
-        .shadow()
-        .find('input')
-        .selectFile(uploadImgPath, { force: true });
-      // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(1000);
-
-      cy.findByRole('button', { name: /^Continue$/ }).click();
-
-      cy.location('pathname').should(
-        'eq',
-        '/representative/representative-form-upload/21-686c/review-and-submit',
-      );
-
-      cy.findByText(/^Submit form/, { selector: 'button' })
-        .last()
-        .click();
-
-      cy.location('pathname').should(
-        'eq',
-        '/representative/representative-form-upload/21-686c/confirmation',
-      );
-    });
-
-    it('allows non-veteran claimant submission', () => {
-      cy.visit('/representative/representative-form-upload/21-686c/');
-      cy.location('pathname').should(
-        'eq',
-        '/representative/representative-form-upload/21-686c/introduction',
-      );
-      cy.get('a[href="#start"]')
-        .contains('Start form')
-        .click();
-      cy.location('pathname').should(
-        'eq',
-        '/representative/representative-form-upload/21-686c/is-veteran',
-      );
-      cy.findByLabelText(
-        /^The claimant is a survivor or dependent of the Veteran$/,
-      ).click();
-      cy.findByRole('button', { name: /^Continue$/ }).click();
-      cy.location('pathname').should(
-        'eq',
-        '/representative/representative-form-upload/21-686c/claimant-information',
-      );
-      fillTextWebComponent(
-        'claimantFullName_first',
-        data.claimantFullName.first,
-      );
-      fillTextWebComponent('claimantFullName_last', data.claimantFullName.last);
-      fillTextWebComponent(
-        'claimantFullName_first',
-        data.claimantFullName.first,
-      );
-      cy.get('input[name="root_claimantSsn"]').type('234232346');
-      cy.get('input[name="root_claimantDateOfBirth"]').type('1992-08-17');
-      fillTextWebComponent('veteranFullName_first', data.veteranFullName.first);
-      fillTextWebComponent('veteranFullName_last', data.veteranFullName.last);
-      cy.get('input[name="root_veteranSsn"]').type('234232346');
-      cy.get('input[name="root_veteranDateOfBirth"]').type('1992-08-17');
-      fillTextWebComponent('address_postalCode', data.address.postalCode);
-      cy.findByRole('button', { name: /^Continue$/ }).click();
-      cy.location('pathname').should(
-        'eq',
-        '/representative/representative-form-upload/21-686c/upload',
-      );
-      cy.get('va-file-input')
-        .shadow()
-        .find('input')
-        .selectFile(uploadImgPath, {
-          force: true,
-        });
-      // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(1000);
-      cy.findByRole('button', { name: /^Continue$/ }).click();
-      cy.location('pathname').should(
-        'eq',
-        '/representative/representative-form-upload/21-686c/review-and-submit',
-      );
-      cy.findByText(/^Submit form/, { selector: 'button' })
-        .last()
-        .click();
-      cy.location('pathname').should(
-        'eq',
-        '/representative/representative-form-upload/21-686c/confirmation',
       );
     });
   });
