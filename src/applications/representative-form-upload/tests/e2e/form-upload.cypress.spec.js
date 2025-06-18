2a3ed3aa6411636d4340b6255347494c099b8ffb
jquispe-oddball <jennifer.quispe@oddball.io>
2025-06-18 15:36:42 -0400

    111576 - updating select to type

--- src/applications/representative-form-upload/tests/e2e/form-upload.cypress.spec.js
+++ src/applications/representative-form-upload/tests/e2e/form-upload.cypress.spec.js
@@ -159,7 +159,7 @@ describe('Representative Form Upload', () => {
       fillTextWebComponent('veteranFullName_last', data.veteranFullName.last);
       fillTextWebComponent('address_postalCode', data.address.postalCode);
 
-      cy.get('input[name="root_veteranDateOfBirth"]').select('1992-08-17');
+      cy.get('input[name="root_veteranDateOfBirth"]').type('1992-08-17');
       cy.fillVaTextInput(`root_email`, data.email);
 
       cy.findByRole('button', { name: /^Continue$/ }).click();
