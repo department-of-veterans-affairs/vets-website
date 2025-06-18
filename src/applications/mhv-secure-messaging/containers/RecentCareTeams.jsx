6698c479973499870513322a708208cc389d62d0
Alex Morgun <87077843+alex-morgun-1@users.noreply.github.com>
2025-06-18 12:20:13 -0400

    MHV-71774 Refactor RecentCareTeams component to manage state and handle radio button selection

--- src/applications/mhv-secure-messaging/containers/RecentCareTeams.jsx
+++ src/applications/mhv-secure-messaging/containers/RecentCareTeams.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+  VaRadio,
+  VaRadioOption,
+} from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import EmergencyNote from '../components/EmergencyNote';
+import * as Constants from '../util/constants';
 
 const RADIO_BUTTON_SET_LABEL =
   "Select a team from those you've sent messages to in the past 6 months. Or select A different care team to find another team.";
 const OTHER_VALUE = 'other';
+const { Paths } = Constants;
 
 const RecentCareTeams = () => {
+  const history = useHistory();
+  const [selectedCareTeam, setSelectedCareTeam] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleContinue = useCallback(
+    () => {
+      if (!selectedCareTeam) {
+        setError('Select a care team');
+        return;
+      }
+      history.push(`${Paths.COMPOSE}${Paths.SELECT_CARE_TEAM}/`);
+    },
+    [history, selectedCareTeam],
+  );
+
+  const handleRadioChange = useCallback(event => {
+    const { value } = event.detail;
+    setSelectedCareTeam(value);
+  }, []);
+
   return (
     <>
       <h1 className="vads-u-margin-bottom--3">Recent care teams</h1>
       <EmergencyNote dropDownFlag />
-      <va-radio
+      <VaRadio
         class="vads-u-margin-bottom--3"
-        error={null}
+        error={error}
         label={RADIO_BUTTON_SET_LABEL}
         required
+        onVaValueChange={handleRadioChange}
       >
-        <va-radio-option
-          label="A different care team"
-          tile
-          value={OTHER_VALUE}
-        />
-      </va-radio>
-      <va-button continue onClick={() => {}} text="Continue" />
+        <VaRadioOption label="A different care team" tile value={OTHER_VALUE} />
+      </VaRadio>
+      <va-button continue onClick={handleContinue} text="Continue" />
     </>
   );
 };
