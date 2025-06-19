31e61b31597eb67063878bc1c297daa5994645a2
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-06-18 21:44:10 -0700

    10-10EZR: Custom page wil be used for maritalStatus. These changes represent moving in that direction.

--- src/applications/ezr/components/FormPages/MaritalStatusPage.jsx
+++ src/applications/ezr/components/FormPages/MaritalStatusPage.jsx
@@ -1,136 +1,135 @@
-import React, { useState, useEffect } from 'react';
-import {
-  VaModal,
-  VaSelect,
-} from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import ezrSchema from 'vets-json-schema/dist/10-10EZR-schema.json';
+/* eslint-disable react/sort-prop-types */
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import PrefillMessage from 'platform/forms/save-in-progress/PrefillMessage';
+import { connect } from 'react-redux';
+import SchemaForm from '@department-of-veterans-affairs/platform-forms-system/SchemaForm';
+import { VaModal } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import { getFormContext } from 'platform/forms/save-in-progress/selectors';
+import content from 'applications/ezr/locales/en/content.json';
+import { replaceStrValues } from 'applications/ezr/utils/helpers/general';
 
-const MaritalStatusPage = props => {
+/**
+ * This page is used to change the marital status of the veteran.
+ * It displays a modal to confirm the change.
+ * It also updates the marital status in the form data.
+ */
+/** @type {CustomPageType} */
+function MaritalStatusPage(props) {
   const {
-    goForward,
-    goBack,
-    setFormData,
+    name,
+    title,
     data,
-    NavButtons,
+    appStateData,
+    schema,
+    uiSchema,
+    pagePerItemIndex,
+    formContext,
+    trackingPrefix,
     contentBeforeButtons,
     contentAfterButtons,
+    onChange,
+    onSubmit,
+    NavButtons: FormNavButtons,
   } = props;
   const [modal, setModal] = useState(false);
-  const [selectedStatus, setSelectedStatus] = useState(
-    data['view:maritalStatus'].maritalStatus,
-  );
-
-  useEffect(
-    () => {
-      setSelectedStatus(data['view:maritalStatus'].maritalStatus);
-    },
-    [data],
-  );
-
-  // Current marital status from form data.
-  const currentMaritalStatus = data['view:maritalStatus'].maritalStatus;
-
-  // Check if marital status is Married or Separated.
-  const isMarriedOrSeparated = status =>
-    status === 'Married' || status === 'Separated';
-
-  // Marital status schema from form schema.
-  const { maritalStatus: maritalStatusSchema } = ezrSchema.properties;
-  const prefilled = data?.metadata?.prefill || false;
-
+  const [newMaritalStatus, setNewMaritalStatus] = useState(null);
   // Modal content.
-  const modalTitle = 'Marital Status Changed';
-  const modalCancelDescription =
-    'Your marital status has been updated. This may affect your benefits eligibility.';
-  const primaryButtonText = 'Continue';
-  const secondaryButtonText = 'Cancel';
+  const modalTitle = content['household-spouse-status-change-modal-title'];
+  const modalCancelDescription = replaceStrValues(
+    content['household-spouse-status-change-modal-text'],
+    newMaritalStatus,
+  );
+  const primaryButtonText =
+    content['household-spouse-status-change-modal-continue-button-text'];
+  const secondaryButtonText =
+    content['household-spouse-status-change-modal-cancel-button-text'];
 
-  // Event handlers.
   const handlers = {
     onCancel: () => {
       setModal(false);
-      setSelectedStatus(currentMaritalStatus);
     },
     onConfirm: () => {
       setModal(false);
-      goForward(data);
+      onSubmit(data);
     },
     onChange: event => {
-      setSelectedStatus(event.detail.value);
+      onChange(event);
+      setNewMaritalStatus(event['view:maritalStatus'].maritalStatus);
+      setModal(true);
     },
-    handleGoForward: () => {
-      if (
-        isMarriedOrSeparated(currentMaritalStatus) &&
-        !isMarriedOrSeparated(selectedStatus)
-      ) {
-        setModal(true);
-      } else {
-        setFormData({
-          ...data,
-          'view:maritalStatus': {
-            maritalStatus: selectedStatus,
-          },
-        });
-        goForward(data);
-      }
+    onSubmit: () => {
+      onSubmit(data);
+    },
+    onGoForward: () => {
+      onSubmit(data);
+    },
+    onGoBack: () => {
+      // Handle go back logic if needed.
     },
   };
 
   return (
-    <div>
-      <PrefillMessage formContext={{ prefilled }}>
-        <p>
-          We’ve prefilled some of your information from your account. If you
-          need to correct anything, you can edit the form fields below.
-        </p>
-      </PrefillMessage>
-      <VaSelect
-        onVaSelect={handlers.onChange}
-        required
-        label="Marital Status"
-        name="maritalStatus"
-        options={maritalStatusSchema.enum}
-        value={selectedStatus}
+    <>
+      <SchemaForm
+        name={name}
+        title={title}
+        data={data}
+        appStateData={appStateData}
+        schema={schema}
+        uiSchema={uiSchema}
+        pagePerItemIndex={pagePerItemIndex}
+        formContext={formContext}
+        trackingPrefix={trackingPrefix}
+        onChange={handlers.onChange}
+        onSubmit={handlers.onSubmit}
       >
-        {maritalStatusSchema.enum.map(status => (
-          <option key={status} value={status}>
-            {status}
-          </option>
-        ))}
-      </VaSelect>
-      <VaModal
-        modalTitle={modalTitle}
-        visible={modal}
-        primaryButtonText={primaryButtonText}
-        secondaryButtonText={secondaryButtonText}
-        onPrimaryButtonClick={handlers.onConfirm}
-        onSecondaryButtonClick={handlers.onCancel}
-        onCloseEvent={handlers.onCancel}
-        status="warning"
-        clickToClose
-        uswds
-      >
-        <p className="vads-u-margin--0">{modalCancelDescription}</p>
-      </VaModal>
-      {contentBeforeButtons}
-      <NavButtons goForward={handlers.handleGoForward} goBack={goBack} />
-      {contentAfterButtons}
-    </div>
+        {/* Modal for marital status change confirmation */}
+        <VaModal
+          modalTitle={modalTitle}
+          visible={modal}
+          primaryButtonText={primaryButtonText}
+          secondaryButtonText={secondaryButtonText}
+          onPrimaryButtonClick={handlers.onConfirm}
+          onSecondaryButtonClick={handlers.onCancel}
+          onCloseEvent={handlers.onCancel}
+          status="warning"
+          clickToClose
+          uswds
+        >
+          <p className="vads-u-margin--0">{modalCancelDescription}</p>
+        </VaModal>
+        {/* contentBeforeButtons = save-in-progress links */}
+        {contentBeforeButtons}
+        <FormNavButtons
+          goBack={handlers.onGoBack}
+          goForward={handlers.onGoForward}
+          submitToContinue
+        />
+        {contentAfterButtons}
+      </SchemaForm>
+    </>
   );
-};
+}
 
 MaritalStatusPage.propTypes = {
+  data: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  schema: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired,
+  uiSchema: PropTypes.object.isRequired,
+  appStateData: PropTypes.object,
+  contentAfterButtons: PropTypes.node,
+  contentBeforeButtons: PropTypes.node,
+  formContext: PropTypes.object,
+  pagePerItemIndex: PropTypes.number,
+  trackingPrefix: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
   NavButtons: PropTypes.func.isRequired,
-  data: PropTypes.shape({
-    'view:maritalStatus': PropTypes.shape({
-      maritalStatus: PropTypes.string,
-    }),
-  }).isRequired,
-  goBack: PropTypes.func.isRequired,
-  goForward: PropTypes.func.isRequired,
-  setFormData: PropTypes.func.isRequired,
 };
 
-export default MaritalStatusPage;
+const mapStateToProps = state => ({
+  formContext: getFormContext(state),
+});
+
+export default connect(mapStateToProps)(MaritalStatusPage);
