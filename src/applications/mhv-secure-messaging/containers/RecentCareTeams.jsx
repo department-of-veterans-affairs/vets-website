d4cf8fce092f937588709e72d2abfcb24f9e3f2a
Alex Morgun <87077843+alex-morgun-1@users.noreply.github.com>
2025-06-20 14:52:09 -0400

    MHV-71774 render recent recipients in a component

--- src/applications/mhv-secure-messaging/containers/RecentCareTeams.jsx
+++ src/applications/mhv-secure-messaging/containers/RecentCareTeams.jsx
@@ -1,11 +1,13 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   VaRadio,
   VaRadioOption,
 } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import EmergencyNote from '../components/EmergencyNote';
 import * as Constants from '../util/constants';
+import { getRecentRecipients } from '../actions/recipients';
 
 const RADIO_BUTTON_SET_LABEL =
   "Select a team from those you've sent messages to in the past 6 months. Or select A different care team to find another team.";
@@ -14,8 +16,28 @@ const { Paths } = Constants;
 
 const RecentCareTeams = () => {
   const history = useHistory();
+  const dispatch = useDispatch();
   const [selectedCareTeam, setSelectedCareTeam] = useState(null);
   const [error, setError] = useState(null);
+  const { recipients } = useSelector(state => state.sm);
+  const { recentRecipients } = recipients;
+
+  useEffect(
+    () => {
+      dispatch(getRecentRecipients(6));
+    },
+    [dispatch],
+  );
+
+  useEffect(
+    () => {
+      // If recentRecipients is null (fetched but none present), redirect
+      if (recentRecipients === null) {
+        history.replace(`${Paths.COMPOSE}${Paths.NEW_MESSAGE}/`);
+      }
+    },
+    [recentRecipients, history],
+  );
 
   const handleContinue = useCallback(
     () => {
@@ -42,6 +64,10 @@ const RecentCareTeams = () => {
     setError(null); // Clear error on selection
   }, []);
 
+  if (recentRecipients === null) {
+    return null;
+  }
+
   return (
     <>
       <h1 className="vads-u-margin-bottom--3">Recent care teams</h1>
@@ -53,6 +79,19 @@ const RecentCareTeams = () => {
         required
         onVaValueChange={handleRadioChange}
       >
+        {(() => {
+          if (Array.isArray(recentRecipients) && recentRecipients.length > 0) {
+            return recentRecipients.map(recipient => (
+              <VaRadioOption
+                tile
+                key={recipient.triageTeamId}
+                label={recipient.name}
+                value={recipient.triageTeamId}
+              />
+            ));
+          }
+          return null;
+        })()}
         <VaRadioOption label="A different care team" tile value={OTHER_VALUE} />
       </VaRadio>
       <va-button continue onClick={handleContinue} text="Continue" />
