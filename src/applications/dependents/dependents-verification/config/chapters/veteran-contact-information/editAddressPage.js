bbc8e84a5460cbf85a2545f409b5b309be599833
Rob Garrison <136959+Mottie@users.noreply.github.com>
2025-06-23 10:35:30 -0500

    Remove address depends

--- src/applications/dependents/dependents-verification/config/chapters/veteran-contact-information/editAddressPage.js
+++ src/applications/dependents/dependents-verification/config/chapters/veteran-contact-information/editAddressPage.js
@@ -8,17 +8,22 @@ import EditMailingAddress from '../../../components/EditMailingAddress';
 export default {
   title: 'Edit mailing address',
   path: 'veteran-contact-information/mailing-address',
-  depends: () => false,
+  // depends: () => false,
   CustomPage: EditMailingAddress,
   CustomPageReview: null,
   uiSchema: {
-    address: addressUI({ omit: ['street3'] }),
+    address: {
+      ...addressUI(),
+      'ui:options': {
+        hideOnReview: true,
+      },
+    },
   },
   schema: {
     type: 'object',
     properties: {
       // 'view:pageTitle': blankSchema,
-      address: addressSchema({ omit: ['street3'] }),
+      address: addressSchema(),
     },
   },
 };
