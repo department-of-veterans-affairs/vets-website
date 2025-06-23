5da3955c6d1631f18b6b7e44e1354ed10bb7952f
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-06-23 10:14:49 -0700

    Fix formatting

--- src/applications/edu-benefits/1919/components/PrivacyActStatement.jsx
+++ src/applications/edu-benefits/1919/components/PrivacyActStatement.jsx
@@ -30,22 +30,22 @@ function PrivacyActStatement({ showRespondentBurden }) {
         </p>
       )}
       <p data-testid="privacy-act-notice">
-        VA will not disclose information collected on this form to any source
-        other than what has been authorized under the Privacy Act of 1974 or
-        Title 38, Code of Federal Regulations 1.576 for routine uses as
-        identified in the VA system of records, 58VA21/22/28, Compensation,
-        Pension, Education, Veteran Readiness and Employment Records - VA,
-        published in the Federal Register. An example of a routine use (e.g., VA
-        sends educational forms or letters with a Veteran's identifying
-        information to the Veteran's school or training establishment to (1)
-        assist the Veteran in the completion of claims forms or (2) for VA to
-        obtain further information as may be necessary from the school for VA to
-        properly process the Veteran's education claim or to monitor his or her
-        progress during training). Your obligation to respond is required to
-        obtain or retain education benefits. The responses you provide are
-        considered confidential (38 U.S.C. 5701). Any information provided by
-        applicants, recipients, and others is subject to verification through
-        computer matching programs with other agencies.
+        <strong>Privacy Act Notice:</strong> VA will not disclose information
+        collected on this form to any source other than what has been authorized
+        under the Privacy Act of 1974 or Title 38, Code of Federal Regulations
+        1.576 for routine uses as identified in the VA system of records,
+        58VA21/22/28, Compensation, Pension, Education, Veteran Readiness and
+        Employment Records - VA, published in the Federal Register. An example
+        of a routine use (e.g., VA sends educational forms or letters with a
+        Veteran's identifying information to the Veteran's school or training
+        establishment to (1) assist the Veteran in the completion of claims
+        forms or (2) for VA to obtain further information as may be necessary
+        from the school for VA to properly process the Veteran's education claim
+        or to monitor his or her progress during training). Your obligation to
+        respond is required to obtain or retain education benefits. The
+        responses you provide are considered confidential (38 U.S.C. 5701). Any
+        information provided by applicants, recipients, and others is subject to
+        verification through computer matching programs with other agencies.
       </p>
     </>
   );
