f35a066c823ca8d2cfc166fa539f28321e5a67bf
Daniel Sasser <221539+dsasser@users.noreply.github.com>
2025-06-23 00:16:41 -0700

    10-10EZR: Updates marital status page to handle form states correctly. Adds a helper function to make retrieving marital satatus a centralized method.

--- src/applications/ezr/components/FormPages/MaritalStatusPage.jsx
+++ src/applications/ezr/components/FormPages/MaritalStatusPage.jsx
@@ -4,67 +4,136 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import SchemaForm from '@department-of-veterans-affairs/platform-forms-system/SchemaForm';
 import { VaModal } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import { replaceStrValues } from 'applications/ezr/utils/helpers/general';
+import { useEditOrAddForm } from 'platform/forms-system/src/js/patterns/array-builder';
+import { isVeteranMarriedOrSeparated } from 'applications/ezr/utils/helpers/household';
 import { getFormContext } from 'platform/forms/save-in-progress/selectors';
 import content from 'applications/ezr/locales/en/content.json';
-import { replaceStrValues } from 'applications/ezr/utils/helpers/general';
 
 /**
- * This page is used to change the marital status of the veteran.
- * It displays a modal to confirm the change.
- * It also updates the marital status in the form data.
+ * This page is used to change the marital status of a Veteran, and if the
+ * Veteran marriage status is changed from married/separated to unmarried, or
+ * from unmarried to married/separated, it displays a modal to confirm the
+ * change. It also updates the marital status in the form data.
+ *
+ * @type {CustomPageType}
+ *
+ * @param {Object} props
+ *   The props passed to the component.
+ * @param {string} props.name
+ *   The name of the form.
+ * @param {string} props.title
+ *   The title of the form.
+ * @param {Object} props.appStateData
+ *   The app state data.
+ * @param {number} props.pagePerItemIndex
+ *   The page per item index.
+ * @param {Object} props.formContext
+ *   The form context.
+ * @param {string} props.trackingPrefix
+ *   The tracking prefix.
+ * @param {React.ReactNode} props.contentBeforeButtons
+ *   The content before the buttons.
+ * @param {React.ReactNode} props.contentAfterButtons
+ *   The content after the buttons.
+ * @param {() => void} props.goBack
+ *   The function to go back.
+ * @param {() => void} props.NavButtons
  */
-/** @type {CustomPageType} */
 function MaritalStatusPage(props) {
   const {
     name,
     title,
-    data,
     appStateData,
-    schema,
-    uiSchema,
     pagePerItemIndex,
     formContext,
     trackingPrefix,
     contentBeforeButtons,
     contentAfterButtons,
-    onChange,
-    onSubmit,
-    NavButtons: FormNavButtons,
+    goBack,
+    NavButtons,
   } = props;
+  const { data, schema, uiSchema, onChange, onSubmit } = useEditOrAddForm({
+    isEdit: false,
+    schema: props.schema,
+    uiSchema: props.uiSchema,
+    data: props.data,
+    onChange: props.onChange,
+    onSubmit: props.onSubmit,
+  });
   const [modal, setModal] = useState(false);
-  const [newMaritalStatus, setNewMaritalStatus] = useState(null);
-  // Modal content.
+  const [pendingMaritalStatus, setPendingMaritalStatus] = useState(null);
+  const [tempFormData, setTempFormData] = useState(null);
+  const currentMaritalStatus = data['view:maritalStatus']?.maritalStatus || '';
+
   const modalTitle = content['household-spouse-status-change-modal-title'];
   const modalCancelDescription = replaceStrValues(
     content['household-spouse-status-change-modal-text'],
-    newMaritalStatus,
+    pendingMaritalStatus,
   );
   const primaryButtonText =
     content['household-spouse-status-change-modal-continue-button-text'];
   const secondaryButtonText =
     content['household-spouse-status-change-modal-cancel-button-text'];
 
+  /**
+   * Determines if a modal should be shown based on the current and new marital
+   * statuses.
+   *
+   * @param {string} currentStatus
+   *   The current marital status of the Veteran.
+   * @param {string} newStatus
+   *   The new marital status of the Veteran.
+   *
+   * @returns {boolean}
+   *   True if the modal should be shown, false otherwise.
+   */
+  const shouldShowModal = (currentStatus, newStatus) => {
+    if (currentStatus.length === 0 || newStatus.length === 0) {
+      return false;
+    }
+    const isCurrentlyMarried = isVeteranMarriedOrSeparated(currentStatus);
+    const isNewlyMarried = isVeteranMarriedOrSeparated(newStatus);
+    const statusChanged = currentStatus !== newStatus;
+    const isMaritalStatusTransition = isCurrentlyMarried !== isNewlyMarried;
+    return statusChanged && isMaritalStatusTransition;
+  };
+
+  /**
+   * Form event handlers. Not to be confused with methods provided by
+   * useEditOrAddForm().
+   */
   const handlers = {
     onCancel: () => {
       setModal(false);
+      setPendingMaritalStatus(null);
+      setTempFormData(null);
     },
     onConfirm: () => {
+      // User confirmed the change, apply the temporary data.
+      if (tempFormData) {
+        onChange(tempFormData);
+      }
       setModal(false);
-      onSubmit(data);
+      setPendingMaritalStatus(null);
+      setTempFormData(null);
     },
-    onChange: event => {
-      onChange(event);
-      setNewMaritalStatus(event['view:maritalStatus'].maritalStatus);
-      setModal(true);
-    },
-    onSubmit: () => {
-      onSubmit(data);
-    },
-    onGoForward: () => {
-      onSubmit(data);
-    },
-    onGoBack: () => {
-      // Handle go back logic if needed.
+    onChange: eventData => {
+      const incomingMaritalStatus =
+        eventData?.['view:maritalStatus']?.maritalStatus;
+
+      const shouldShow = shouldShowModal(
+        currentMaritalStatus,
+        incomingMaritalStatus,
+      );
+
+      if (shouldShow) {
+        setPendingMaritalStatus(incomingMaritalStatus);
+        setTempFormData(eventData);
+        setModal(true);
+      } else {
+        onChange(eventData);
+      }
     },
   };
 
@@ -73,7 +142,7 @@ function MaritalStatusPage(props) {
       <SchemaForm
         name={name}
         title={title}
-        data={data}
+        data={tempFormData || data}
         appStateData={appStateData}
         schema={schema}
         uiSchema={uiSchema}
@@ -81,9 +150,8 @@ function MaritalStatusPage(props) {
         formContext={formContext}
         trackingPrefix={trackingPrefix}
         onChange={handlers.onChange}
-        onSubmit={handlers.onSubmit}
+        onSubmit={onSubmit}
       >
-        {/* Modal for marital status change confirmation */}
         <VaModal
           modalTitle={modalTitle}
           visible={modal}
@@ -98,13 +166,8 @@ function MaritalStatusPage(props) {
         >
           <p className="vads-u-margin--0">{modalCancelDescription}</p>
         </VaModal>
-        {/* contentBeforeButtons = save-in-progress links */}
         {contentBeforeButtons}
-        <FormNavButtons
-          goBack={handlers.onGoBack}
-          goForward={handlers.onGoForward}
-          submitToContinue
-        />
+        <NavButtons goBack={goBack} submitToContinue />
         {contentAfterButtons}
       </SchemaForm>
     </>
@@ -125,6 +188,8 @@ MaritalStatusPage.propTypes = {
   trackingPrefix: PropTypes.string,
   onChange: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
+  goForward: PropTypes.func.isRequired,
+  goBack: PropTypes.func.isRequired,
   NavButtons: PropTypes.func.isRequired,
 };
 
