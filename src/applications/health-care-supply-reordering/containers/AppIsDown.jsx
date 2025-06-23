031076ad548e87ca5c40bc89dc72d03f1939e2f2
radavis <1299034+radavis@users.noreply.github.com>
2025-06-23 14:38:02 -0400

    Updates copy

--- src/applications/health-care-supply-reordering/containers/AppIsDown.jsx
+++ src/applications/health-care-supply-reordering/containers/AppIsDown.jsx
@@ -4,9 +4,8 @@ const AppIsDown = () => (
   <va-alert class="vads-u-margin-bottom--4" visible status="warning">
     <h3 slot="headline">This application is down for maintenance</h3>
     <p>
-      {`We’re making some updates to this application to help make it even
-      better for Veterans, service members, and family members like you. We’re
-      sorry it’s not working right now. Please check back soon.`}
+      {`We’re making some updates to this tool. We’re sorry it’s not working
+      right now. Please check back soon.`}
     </p>
   </va-alert>
 );
