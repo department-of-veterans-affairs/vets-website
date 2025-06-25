c429cdab66d1610834d0cffc6289e678b24818fd
Dan Lim <danlim715@gmail.com>
2025-06-25 12:35:16 -0500

    spec fixes and formatting changes

--- src/applications/income-and-asset-statement/containers/IntroductionPage.jsx
+++ src/applications/income-and-asset-statement/containers/IntroductionPage.jsx
@@ -1,100 +1,91 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
 import { focusElement } from 'platform/utilities/ui';
-import { useSelector } from 'react-redux';
 import { isLoggedIn } from 'platform/user/selectors';
+
 import FormTitle from 'platform/forms-system/src/js/components/FormTitle';
 import SaveInProgressIntro from 'platform/forms/save-in-progress/SaveInProgressIntro';
 
-export const IntroductionPage = props => {
+export const IntroductionPage = ({ route }) => {
+  const loggedIn = useSelector(isLoggedIn);
+  const { formConfig, pageList } = route;
+
   useEffect(() => {
     focusElement('h1');
   }, []);
 
-  const { route } = props;
-  const { formConfig, pageList } = route;
-  const isUserLoggedIn = useSelector(isLoggedIn);
+  const renderIfVeteranContent = () => (
+    <>
+      <p>You’ll need to report income and assets for these individuals:</p>
+      <ul>
+        <li>
+          Yourself, <strong>and</strong>
+        </li>
+        <li>
+          Your spouse (unless you live apart, and you are estranged, and you do
+          not contribute to your spouse’s support), <strong>and</strong>
+        </li>
+        <li>
+          Your child or children (unless you do not have custody or contribute
+          to your child’s or children’s support)
+        </li>
+      </ul>
+    </>
+  );
 
-  const renderIfVeteranContent = () => {
-    return (
-      <p>
-        You’ll need to report income and assets for these individuals:
+  const renderAccordionContent = () => (
+    <va-accordion>
+      <va-accordion-item header="If you’re the Veteran">
+        {renderIfVeteranContent()}
+      </va-accordion-item>
+      <va-accordion-item header="If you’re the surviving spouse">
+        <p>
+          You’ll need to report income and assets for these individuals:
+          <ul>
+            <li>
+              Yourself, <strong>and</strong>
+            </li>
+            <li>
+              Your child or children (unless you do not have custody or not
+              contribute to your child’s or children’s support)
+            </li>
+          </ul>
+        </p>
+      </va-accordion-item>
+      <va-accordion-item header="If you’re the surviving child or custodian of a surviving child">
+        <p>You’ll need to report income and assets for these individuals:</p>
         <ul>
           <li>
-            Yourself, <b>and</b>
+            Yourself and/or the surviving child, <strong>and</strong>
           </li>
           <li>
-            Your spouse (unless you live apart, and you are estranged, and you
-            do not contribute to your spouse’s support), <b>and</b>
+            Child’s custodian (unless the child’s custodian is an institution),{' '}
+            <strong>and</strong>
+          </li>
+          <li>Custodian’s spouse</li>
+        </ul>
+      </va-accordion-item>
+      <va-accordion-item header="If you’re the parent">
+        <p>You’ll need to report income and assets for these individuals:</p>
+        <ul>
+          <li>
+            Yourself, <strong>and</strong>
           </li>
           <li>
-            Your child or children (unless you do not have custody or contribute
-            to your child’s or children’s support)
+            Your spouse (even if your spouse is the Veteran’s other parent. If
+            your spouse is the Veteran’s other parent, you should file separate
+            claims.)
           </li>
         </ul>
-      </p>
-    );
-  };
-
-  const renderAccordionContent = () => {
-    return (
-      <va-accordion uswds>
-        <va-accordion-item header="If you're the Veteran" uswds>
-          {renderIfVeteranContent()}
-        </va-accordion-item>
-        <va-accordion-item header="If you're the surviving spouse" uswds>
-          <p>
-            You’ll need to report income and assets for these individuals:
-            <ul>
-              <li>
-                Yourself, <b>and</b>
-              </li>
-              <li>
-                Your child or children (unless you do not have custody or not
-                contribute to your child’s or children’s support)
-              </li>
-            </ul>
-          </p>
-        </va-accordion-item>
-        <va-accordion-item
-          header="If you're the surviving child or custodian of a surviving child"
-          uswds
-        >
-          <p>
-            You’ll need to report income and assets for these individuals:
-            <ul>
-              <li>
-                Yourself and/or the surviving child, <b>and</b>
-              </li>
-              <li>
-                Child’s custodian (unless the child’s custodian is an
-                institution), <b>and</b>
-              </li>
-              <li>Custodian’s spouse</li>
-            </ul>
-          </p>
-        </va-accordion-item>
-        <va-accordion-item header="If you're the parent" uswds>
-          <p>
-            You’ll need to report income and assets for these individuals:
-            <ul>
-              <li>
-                Yourself, <b>and</b>
-              </li>
-              <li>
-                Your spouse (even if your spouse is the Veteran’s other parent.
-                If your spouse is the Veteran’s other parent, you should file
-                separate claims.)
-              </li>
-            </ul>
-            <b>Note: </b> Parents’ DIC claimants don’t need to report or provide
-            documentation of their assets.
-          </p>
-        </va-accordion-item>
-      </va-accordion>
-    );
-  };
+        <p>
+          <strong>Note: </strong> Parents’ DIC claimants don’t need to report or
+          provide documentation of their assets.
+        </p>
+      </va-accordion-item>
+    </va-accordion>
+  );
 
   return (
     <article className="schemaform-intro">
@@ -104,72 +95,74 @@ export const IntroductionPage = props => {
       />
       <p>
         Complete this form if you need to report or verify income and net worth
-        worth to support your Claim for Pension or Parents' Dependency and
-        Indemnity Compensation (DIC).
+        to support your Claim for Pension or Parents’ Dependency Indemnity
+        Compensation (DIC).
       </p>
       <h2 className="vads-u-font-size--h3 vad-u-margin-top--0">
         What to know before you fill out this form
       </h2>
-      <p>
-        There are different reasons why you may need to submit this form:
-        <ul>
-          <li>
-            As a supporting form requested by your Application for Veterans
-            Pension (VA21P-527 or VA21P-527EZ), <b>or</b>
-          </li>
-          <li>
-            As a supporting form requested by your DIC, Survivors' Pension
-            and/or Accrued Benefits (VA21P-534 or 21P-534EZ), <b>or</b>
-          </li>
-          <li>
-            You have an overflow of assets, <b>or</b>
-          </li>
-          <li>
-            You need to report an irregular or unusual change in your income
-            assets
-          </li>
-        </ul>
-      </p>
-      <p>
-        <a href="/pension/eligibility/">
-          Find out if you’re eligible for Veterans Pension benefits
-        </a>
-      </p>
-      {!isUserLoggedIn && (
-        <p>
-          <a href="/family-and-caregiver-benefits/survivor-compensation/dependency-indemnity-compensation/#am-i-eligible-for-va-dic-as-a-">
-            Find out if you’re eligible for VA Dependency and Indemnity
-            Compensation (DIC)
-          </a>
-        </p>
+      <p>There are different reasons why you may need to submit this form:</p>
+      <ul>
+        <li>
+          As a supporting form requested by your Application for Veterans
+          Pension (VA21P-527 or VA21P-527EZ), <strong>or</strong>
+        </li>
+        <li>
+          As a supporting form requested by your DIC, Survivors’ Pension and/or
+          Accrued Benefits (VA21P-534 or 21P-534EZ), <strong>or</strong>
+        </li>
+        <li>
+          You have an overflow of assets, <strong>or</strong>
+        </li>
+        <li>
+          You need to report an irregular or unusual change in your income
+          assets
+        </li>
+      </ul>
+      <div className="vads-u-margin-y--2">
+        <va-link
+          href="/pension/eligibility/"
+          text="Find out if you’re eligible for Veterans Pension benefits"
+        />
+      </div>
+      {!loggedIn && (
+        <div className="vads-u-margin-y--2">
+          <va-link
+            href="/family-and-caregiver-benefits/survivor-compensation/dependency-indemnity-compensation/#am-i-eligible-for-va-dic-as-a-"
+            text="Find out if you’re eligible for VA Dependency and Indemnity
+          Compensation (DIC)"
+          />
+        </div>
       )}
-      <p>
-        <va-additional-info trigger="What we consider an asset" uswds>
-          Assets include the fair market value of all property that you own,
-          minus the amount of any mortgages you may have. Assets don’t include
-          your or your dependents' primary residence and lot (up to 2 acres).
-        </va-additional-info>
-      </p>
+      <va-additional-info trigger="What we consider an asset">
+        Assets include the fair market value of all property that you own, minus
+        the amount of any mortgages you may have. Assets don’t include your or
+        your dependents’ primary residence and lot (up to 2 acres).
+      </va-additional-info>
       <h2 className="vads-u-font-size--h3 vad-u-margin-top--0">
         What you’ll need to report
       </h2>
-      {isUserLoggedIn ? renderIfVeteranContent() : renderAccordionContent()}
-      <p>
-        <va-additional-info trigger="What we consider child custody" uswds>
-          We define child custody for pension purposes in 38 C.F.R.§ 3.57(d).
-          natural or adoptive parent has custody of a child unless custody is
-          legally removed. For pension purposes, a child who has attained age 18
-          remains in the custody of the person who had custody before the child
-          turned 18 unless custody is legally removed.
-        </va-additional-info>
-      </p>
+      {loggedIn ? renderIfVeteranContent() : renderAccordionContent()}
+
+      <va-additional-info
+        class="vads-u-margin-y--3"
+        trigger="What we consider child custody"
+      >
+        We define child custody for pension purposes in 38 C.F.R.§ 3.57(d).
+        Natural or adoptive parent has custody of a child unless custody is
+        legally removed. For pension purposes, a child who has attained age 18
+        remains in the custody of the person who had custody before the child
+        turned 18 unless custody is legally removed.
+      </va-additional-info>
+
       <SaveInProgressIntro
         headingLevel={2}
-        prefillEnabled={route.formConfig.prefillEnabled}
+        prefillEnabled={formConfig.prefillEnabled}
         messages={formConfig.savedFormMessages}
         pageList={pageList}
         startText="Start the Income and Asset Statement application"
       />
+
       <div className="vads-u-margin-top--2">
         <va-omb-info
           res-burden={30}
@@ -190,7 +183,6 @@ IntroductionPage.propTypes = {
       savedFormMessages: PropTypes.object,
     }),
   }),
-  router: PropTypes.object,
 };
 
 export default IntroductionPage;
