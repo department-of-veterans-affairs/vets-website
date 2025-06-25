31bbb2fcfa0f11c1a363f86c0d12903e0508a249
William Phelps <williamphelps13@gmail.com>
2025-06-25 15:58:27 -0700

    Revert to only look at select data on submit (but with updated variable names)

--- src/applications/claims-status/components/claim-files-tab/NewAddFilesForm.jsx
+++ src/applications/claims-status/components/claim-files-tab/NewAddFilesForm.jsx
@@ -6,24 +6,24 @@ import {
 
 export default function AddFilesForm() {
   const fileInputMultipleRef = useRef(null);
-  const [filesWithMeta, setFilesWithMeta] = useState([]);
+  const [fileState, setFileState] = useState([]);
 
-  const updateFiles = fileState => {
+  const handleFileChange = e => setFileState(e.detail.state);
+
+  const buildPayload = () => {
     const fileInputs = Array.from(
       fileInputMultipleRef.current.shadowRoot.querySelectorAll('va-file-input'),
     );
 
-    setFilesWithMeta(
-      fileState.map((f, i) => ({
+    return fileState.map((f, i) => {
+      const fileTypeId = fileInputs[i]?.querySelector('va-select')?.value ?? '';
+      return {
         file: f.file,
         password: f.password ?? '',
-        fileTypeId: fileInputs[i]?.querySelector('va-select')?.value ?? '',
-      })),
-    );
+        fileTypeId,
+      };
+    });
   };
-
-  const handleFiles = e => updateFiles(e.detail.state);
-  const handleSelect = () => updateFiles(filesWithMeta);
 
   return (
     <>
@@ -32,22 +32,18 @@ export default function AddFilesForm() {
         name="my-file-input"
         label="Select a file to upload"
         hint="You can upload a .pdf, .gif, .jpg, .bmp, or .txt file."
-        enableAnalytics={false}
-        onVaMultipleChange={handleFiles}
-        onVaSelect={handleSelect}
+        onVaMultipleChange={handleFileChange}
       >
-        <VaSelect name="fileType" label="What kind of file is this?" required>
-          <option value="1">Public Document</option>
-          <option value="2">Private Document</option>
+        <VaSelect label="What kind of file is this?" required name="fileType">
+          <option value="1">1</option>
+          <option value="2">2</option>
         </VaSelect>
       </VaFileInputMultiple>
+
       <va-button
-        enableAnalytics={false}
         text="Submit files"
-        onClick={() => console.log('payload', filesWithMeta)}
+        onClick={() => console.log('payload', buildPayload())}
       />
     </>
   );
 }
-
-// BUG: The fileTypes were accurate: [{file: File, fileTypeId: '2', password: ''}, {file: File, fileTypeId: '1', password: ''}]. I then deleted the first file and didn't change the seconds select and got: [{file: File, fileTypeId: '2', password: ''}]. It should be [{file: File, fileTypeId: '2', password: ''}]
