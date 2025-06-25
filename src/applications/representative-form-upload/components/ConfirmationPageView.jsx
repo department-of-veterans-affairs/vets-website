5e0ece38efad821236c0fba4090ceedb0d6ec57e
Patrick Brown <patrick.brown@oddball.io>
2025-06-25 15:57:32 -0400

    arp/custom-review-page - fixed confirmation page

--- src/applications/representative-form-upload/components/ConfirmationPageView.jsx
+++ src/applications/representative-form-upload/components/ConfirmationPageView.jsx
@@ -44,7 +44,13 @@ export const ConfirmationPageView = ({
             <strong>{formattedSubmitDate}</strong>
           </p>
         ) : null}
-        <p>Your confirmation number is: {confirmationNumber}.</p>
+        <p>
+          Your confirmation number is: <strong>{confirmationNumber}</strong>
+        </p>
+        <p>
+          <strong>Note:</strong> Print this page or copy the confirmation number
+          for your records.
+        </p>
       </va-alert>
       <section>
         <h2>What to expect</h2>
