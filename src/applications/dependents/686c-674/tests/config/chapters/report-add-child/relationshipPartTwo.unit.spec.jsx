13b19a7e8993184a9299eed3cf93676908a79b82
Midge-dev <sean.midgley.dev@gmail.com>
2025-06-25 09:55:54 -0700

    add coverage

--- src/applications/dependents/686c-674/tests/config/chapters/report-add-child/relationshipPartTwo.unit.spec.jsx
+++ src/applications/dependents/686c-674/tests/config/chapters/report-add-child/relationshipPartTwo.unit.spec.jsx
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react';
+import React from 'react';
+import sinon from 'sinon';
+import { render, waitFor, cleanup } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { expect } from 'chai';
-import React from 'react';
 import createCommonStore from '@department-of-veterans-affairs/platform-startup/store';
 import {
   DefinitionTester,
@@ -9,12 +10,15 @@ import {
 } from 'platform/testing/unit/schemaform-utils';
 import formConfig from '../../../../config/form';
 
-const defaultStore = createCommonStore();
+// const scrollSpy = sinon.spy();
 
-const formData = {
-  'view:selectable686Options': {
-    addChild: true,
-  },
+// before(() => {
+//   Element.prototype.scrollIntoView = scrollSpy;
+// });
+
+const defaultStore = createCommonStore();
+const baseFormData = {
+  'view:selectable686Options': { addChild: true },
   childrenToAdd: [{}],
 };
 
@@ -24,6 +28,16 @@ describe('686 add child relationship step two', () => {
     uiSchema,
   } = formConfig.chapters.addChild.pages.addChildRelationshipPartTwo;
 
+  const scrollSpy = sinon.spy();
+
+  before(() => {
+    Element.prototype.scrollIntoView = scrollSpy;
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
   it('should render', () => {
     const form = render(
       <Provider store={defaultStore}>
@@ -31,14 +45,72 @@ describe('686 add child relationship step two', () => {
           schema={schema}
           definitions={formConfig.defaultDefinitions}
           uiSchema={uiSchema}
-          data={formData}
+          data={baseFormData}
           arrayPath="childrenToAdd"
           pagePerItemIndex={0}
         />
       </Provider>,
     );
-
     const formDOM = getFormDOM(form);
     expect(formDOM.querySelectorAll('va-checkbox').length).to.eq(2);
+  });
+
+  it('should scroll checkboxes into view when evidence appears', async () => {
+    let form = render(
+      <Provider store={defaultStore}>
+        <DefinitionTester
+          schema={schema}
+          definitions={formConfig.defaultDefinitions}
+          uiSchema={uiSchema}
+          data={baseFormData}
+          arrayPath="childrenToAdd"
+          pagePerItemIndex={0}
+        />
+      </Provider>,
+    );
+    let formDOM = getFormDOM(form);
+
+    const group = formDOM.querySelector('.relationship-checkbox-group');
+    expect(group).to.exist;
+    group.getBoundingClientRect = () => ({
+      top: -100, // Simulate "above the viewport"
+      bottom: -50,
+      left: 0,
+      right: 0,
+      width: 100,
+      height: 50,
+    });
+
+    const checkedData = {
+      ...baseFormData,
+      relationshipToChild: { adopted: true },
+    };
+
+    form = render(
+      <Provider store={defaultStore}>
+        <DefinitionTester
+          schema={schema}
+          definitions={formConfig.defaultDefinitions}
+          uiSchema={uiSchema}
+          data={checkedData}
+          arrayPath="childrenToAdd"
+          pagePerItemIndex={0}
+        />
+      </Provider>,
+    );
+    formDOM = getFormDOM(form);
+
+    await waitFor(() => {
+      expect(formDOM.textContent).to.include(
+        'Based on your answers, you’ll need to submit additional evidence',
+      );
+    });
+
+    if (group.scrollIntoView)
+      group.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+    await waitFor(() => {
+      expect(scrollSpy.called).to.be.true;
+    });
   });
 });
