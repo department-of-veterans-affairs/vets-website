4ecd3448edebfa3788ee447da71b4cb2a883a1ca
Matt Long <6738544+longmd@users.noreply.github.com>
2025-06-30 13:17:51 -0400

    Revert props updates

--- src/applications/caregivers/tests/unit/components/ConfirmationPage/ConfirmationScreenView.unit.spec.jsx
+++ src/applications/caregivers/tests/unit/components/ConfirmationPage/ConfirmationScreenView.unit.spec.jsx
@@ -5,8 +5,10 @@ import { expect } from 'chai';
 import sinon from 'sinon-v20';
 import ConfirmationScreenView from '../../../../components/ConfirmationPage/ConfirmationScreenView';
 
+const TIMESTAMP = 1666887649663;
+
 describe('CG <ConfirmationScreenView>', () => {
-  const subject = ({ timestamp = 1666887649663 } = {}) => {
+  const subject = ({ timestamp = undefined } = {}) => {
     const props = {
       name: { first: 'John', middle: 'Marjorie', last: 'Smith', suffix: 'Sr.' },
       route: { formConfig: {} },
@@ -56,19 +58,19 @@ describe('CG <ConfirmationScreenView>', () => {
   });
 
   it('should render Veteran name & application date when provided', () => {
-    const { selectors } = subject();
+    const { selectors } = subject({ timestamp: TIMESTAMP });
     const { submissionDate } = selectors();
     expect(submissionDate).to.contain.text('Oct. 27, 2022');
   });
 
   it('should not render timestamp in `application information` section when not provided', () => {
-    const { selectors } = subject({ timestamp: undefined });
+    const { selectors } = subject();
     const { submissionDate } = selectors();
     expect(submissionDate).to.not.exist;
   });
 
   it('should fire the correct event when the print button is clicked', () => {
-    const { selectors } = subject();
+    const { selectors } = subject({ timestamp: TIMESTAMP });
     const { printBtn } = selectors();
     fireEvent.click(printBtn);
     sinon.assert.calledOnce(printSpy);
