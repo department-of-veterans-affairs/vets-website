4001e24bbee3cef77183c726c1d2a2ef5aec3115
fazilqa <127263470+fazilqa@users.noreply.github.com>
2025-06-30 13:16:31 -0700

    refactoring FF updating

--- src/applications/mhv-secure-messaging/tests/e2e/pilot-env-tests/secure-messaging-pilot-new-message-urls.cypress.spec.js
+++ src/applications/mhv-secure-messaging/tests/e2e/pilot-env-tests/secure-messaging-pilot-new-message-urls.cypress.spec.js
@@ -1,25 +1,19 @@
 import SecureMessagingSite from '../sm_site/SecureMessagingSite';
 import { AXE_CONTEXT, Paths, Data } from '../utils/constants';
-import mockFeatureToggles from '../fixtures/toggles-response.json';
 import PilotEnvPage from '../pages/PilotEnvPage';
 import GeneralFunctionsPage from '../pages/GeneralFunctionsPage';
 import PatientComposePage from '../pages/PatientComposePage';
 
 describe('SM PILOT NEW MESSAGE', () => {
-  const pilotFeatureFlag = {
-    name: 'mhv_secure_messaging_cerner_pilot',
-    value: true,
-  };
-  const mockPilotFeatureToggles = {
-    ...mockFeatureToggles,
-    data: {
-      ...mockFeatureToggles.data,
-      features: [...mockFeatureToggles.data.features, pilotFeatureFlag],
+  const updatedFeatureToggles = GeneralFunctionsPage.updateFeatureToggles([
+    {
+      name: 'mhv_secure_messaging_cerner_pilot',
+      value: true,
     },
-  };
+  ]);
 
   it('verify url by direct navigation', () => {
-    SecureMessagingSite.login(mockPilotFeatureToggles);
+    SecureMessagingSite.login(updatedFeatureToggles);
     PilotEnvPage.loadInboxMessages();
 
     cy.visit(`${Paths.UI_PILOT}/new-message/select-care-team`);
