a3f86c485dcadeb766496c12b688e5382f6dfdf6
Midge-dev <sean.midgley.dev@gmail.com>
2025-07-02 10:16:00 -0700

    add auth redirect

--- src/applications/dependents/dependents-verification/containers/App.jsx
+++ src/applications/dependents/dependents-verification/containers/App.jsx
@@ -5,19 +5,37 @@ import PropTypes from 'prop-types';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from '../config/form';
 import NoFormPage from '../components/NoFormPage';
+import manifest from '../manifest.json';
 
 export default function App({ location, children }) {
   const featureToggle = useSelector(
     state => state?.featureToggles?.vaDependentsVerification,
   );
+  const { loading } = useSelector(state => state?.externalServiceStatuses);
+  const hasSession = () => JSON.parse(localStorage.getItem('hasSession'));
 
-  return featureToggle ? (
+  const content = (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
     </RoutedSavableApp>
-  ) : (
-    <NoFormPage />
   );
+
+  // Handle loading
+  if (loading) {
+    return <va-loading-indicator message="Loading your information..." />;
+  }
+
+  // If on intro page, just return content
+  if (location?.pathname === '/introduction') {
+    return content;
+  }
+
+  if (!hasSession() && !location?.pathname?.includes('/introduction')) {
+    window.location.replace(`${manifest.rootUrl}/introduction`);
+    return <va-loading-indicator message="Loading your information..." />;
+  }
+
+  return featureToggle ? content : <NoFormPage />;
 }
 
 App.propTypes = {
