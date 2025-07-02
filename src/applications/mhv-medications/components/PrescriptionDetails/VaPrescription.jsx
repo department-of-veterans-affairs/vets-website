c70d5cc5f10c76504acfb04072f9b8916aea9b7b
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-07-02 11:39:48 -0400

    [MHV 72198] remove undeeded note.

--- src/applications/mhv-medications/components/PrescriptionDetails/VaPrescription.jsx
+++ src/applications/mhv-medications/components/PrescriptionDetails/VaPrescription.jsx
@@ -632,10 +632,6 @@ const VaPrescription = prescription => {
                                     width="350"
                                     height="350"
                                   />
-                                  <p>
-                                    <strong>Note:</strong> Images on this page
-                                    are for identification purposes only…
-                                  </p>
                                 </>
                               ) : (
                                 <p
