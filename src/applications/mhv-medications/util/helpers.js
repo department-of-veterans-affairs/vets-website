6c55fef3fe5576dc4e10a541d2f8fb09f853148f
agravell047 <anthony.gravell@bylight.com>
2025-07-02 11:19:56 -0400

    Refactor prescriptions API: remove unused refill alert logic and add filtering for recently requested prescriptions

--- src/applications/mhv-medications/util/helpers.js
+++ src/applications/mhv-medications/util/helpers.js
@@ -656,3 +656,33 @@ export const determineRefillLabel = (isPartialFill, rxHistory, i) => {
   }
   return i + 1 === rxHistory.length ? 'Original fill' : 'Refill';
 };
+
+/**
+ * Convert a prescription resource from the API response into the expected format
+ * @param {Object} prescription - The prescription data from API
+ * @returns {Object} - Formatted prescription object
+ */
+export const convertPrescription = prescription => {
+  // Handle the case where prescription might be null/undefined
+  if (!prescription) return null;
+
+  // Extract from attributes if available, otherwise use the prescription object directly
+  return prescription.attributes || prescription;
+};
+
+/**
+ * Filter recently requested prescriptions to only include those taking longer than expected
+ * @param {Array} recentlyRequested - Array of recently requested prescriptions
+ * @returns {Array} - Filtered array of prescriptions taking longer than expected
+ */
+export const filterRecentlyRequestedForAlerts = recentlyRequested => {
+  if (!Array.isArray(recentlyRequested)) return [];
+
+  return recentlyRequested.reduce((alertList, prescription) => {
+    const rx = convertPrescription(prescription);
+    if (isRefillTakingLongerThanExpected(rx)) {
+      alertList.push(rx);
+    }
+    return alertList;
+  }, []);
+};
