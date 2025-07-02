cc461b8eda0c327cfd50a8b75c99df3d0b2f105b
fazilqa <127263470+fazilqa@users.noreply.github.com>
2025-07-02 09:24:08 -0700

    add navigation and error tests

--- src/applications/mhv-secure-messaging/tests/e2e/curated-list-tests/secure-messaging-curated-list-recent-recipients.cypress.spec.js
+++ src/applications/mhv-secure-messaging/tests/e2e/curated-list-tests/secure-messaging-curated-list-recent-recipients.cypress.spec.js
@@ -3,6 +3,7 @@ import { AXE_CONTEXT, Locators, Data } from '../utils/constants';
 import GeneralFunctionsPage from '../pages/GeneralFunctionsPage';
 import PilotEnvPage from '../pages/PilotEnvPage';
 import mockThreadsResponse from '../fixtures/pilot-responses/threads-recent-recipients-response.json';
+import PatientComposePage from '../pages/PatientComposePage';
 
 describe('SM CURATED LIST MAIN FLOW', () => {
   beforeEach(() => {
@@ -51,5 +52,48 @@ describe('SM CURATED LIST MAIN FLOW', () => {
       });
 
     cy.injectAxeThenAxeCheck(AXE_CONTEXT);
+  });
+
+  it('verify user navigates to new message page', () => {
+    PilotEnvPage.navigateToRecentCareTeamsPage();
+
+    cy.get(`va-radio-option`)
+      .eq(0)
+      .click();
+    PatientComposePage.interceptSentFolder();
+
+    cy.contains(`Continue`).click();
+
+    GeneralFunctionsPage.verifyPageHeader(`Start message`);
+    cy.url().should('include', '/start-message');
+  });
+
+  it('verify user navigates to select care team page', () => {
+    PilotEnvPage.navigateToRecentCareTeamsPage();
+
+    cy.get(`va-radio-option`)
+      .eq(4)
+      .click();
+    PatientComposePage.interceptSentFolder();
+
+    cy.contains(`Continue`).click();
+
+    GeneralFunctionsPage.verifyPageHeader(`Select care team`);
+    cy.url().should('include', '/select-care-team');
+  });
+
+  it('verify selection error', () => {
+    PilotEnvPage.navigateToRecentCareTeamsPage();
+
+    cy.get(`.usa-legend`)
+      .find(`span`)
+      .should(`be.visible`)
+      .and(`have.text`, `(*Required)`);
+
+    cy.contains(`Continue`).click();
+
+    cy.get(`.usa-error-message`)
+      .should('be.visible')
+      .and('contain.text', 'Select a care team');
   });
 });
