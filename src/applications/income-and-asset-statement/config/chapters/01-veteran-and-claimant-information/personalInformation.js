855b0db7f66b28074dace26bef31d11e689b9d24
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2025-07-02 09:13:43 -0600

    Add unit tests

--- src/applications/income-and-asset-statement/config/chapters/01-veteran-and-claimant-information/personalInformation.js
+++ src/applications/income-and-asset-statement/config/chapters/01-veteran-and-claimant-information/personalInformation.js
@@ -1,50 +1,6 @@
-import React from 'react';
-import {
-  PersonalInformation,
-  PersonalInformationHeader,
-} from 'platform/forms-system/src/js/components/PersonalInformation/PersonalInformation';
-import { PersonalInformationReview } from 'platform/forms-system/src/js/components/PersonalInformation/PersonalInformationReview';
-
+import CustomPersonalInfo from '../../../components/CustomPersonalInfo';
+import CustomPersonalInfoReview from '../../../components/CustomPersonalInfoReview';
 import { hasSession } from '../../../helpers';
-
-const CustomPersonalInfo = props => (
-  <PersonalInformation
-    {...props}
-    config={{
-      name: { show: true, required: true },
-      ssn: { show: true, required: true },
-      vaFileNumber: { show: true, required: false },
-      dateOfBirth: { show: false },
-    }}
-    dataAdapter={{
-      ssnPath: 'veteranSocialSecurityNumber',
-      vaFileNumberPath: 'vaFileNumber',
-    }}
-  >
-    <PersonalInformationHeader>
-      <h1 className="vads-u-margin-bottom--3 vads-u-font-size--h2">
-        Confirm the personal information we have on file for you
-      </h1>
-    </PersonalInformationHeader>
-  </PersonalInformation>
-);
-
-const CustomPersonalInfoReview = props => (
-  <PersonalInformationReview
-    {...props}
-    config={{
-      name: { show: true },
-      ssn: { show: true },
-      vaFileNumber: { show: true },
-      dateOfBirth: { show: false },
-    }}
-    dataAdapter={{
-      ssnPath: 'veteranSocialSecurityNumber',
-      vaFileNumberPath: 'vaFileNumber',
-    }}
-    title="Personal information"
-  />
-);
 
 /** @type {PageSchema} */
 export default {
