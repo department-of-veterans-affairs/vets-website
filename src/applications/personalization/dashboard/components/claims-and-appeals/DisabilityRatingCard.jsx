f0722409dae4978562f0ac8bab2943851a9a6340
Giles Wells <giles@mediagearhead.com>
2025-07-02 14:01:17 -0400

    On page text changes

--- src/applications/personalization/dashboard/components/claims-and-appeals/DisabilityRatingCard.jsx
+++ src/applications/personalization/dashboard/components/claims-and-appeals/DisabilityRatingCard.jsx
@@ -8,11 +8,16 @@ const DisabilityRatingCard = ({
   totalDisabilityRating,
   totalDisabilityRatingError,
 }) => {
+  const Heading = () => (
+    <h3 className="vads-u-margin-top--0 vads-u-margin-bottom--2">
+      Disability rating
+    </h3>
+  );
   const noRatingReviewLink = (
     <p className="vads-u-margin-top--2 vads-u-margin-bottom--0">
       <va-link
         href="/disability/view-disability-rating"
-        text="Review your VA disability rating"
+        text="Review disability rating information"
       />
     </p>
   );
@@ -21,25 +26,29 @@ const DisabilityRatingCard = ({
     if (totalDisabilityRating != null) {
       // User has a disability rating
       return (
-        <va-card>
-          <h4 className="vads-u-margin-y--0 vads-u-padding-bottom--1">
-            Your combined disability rating is {totalDisabilityRating}%
-          </h4>
-          <p className="vads-u-margin-y--0 vads-u-margin-top--0p5 vads-u-padding-y--1">
-            <CTALink
-              href="/disability/view-disability-rating/rating"
-              text="Review your VA disability rating"
-              className="vads-u-font-weight--bold"
-              showArrow
-            />
-          </p>
-        </va-card>
+        <>
+          <Heading />
+          <va-card>
+            <h4 className="vads-u-margin-y--0 vads-u-padding-bottom--1">
+              Your combined disability rating is {totalDisabilityRating}%
+            </h4>
+            <p className="vads-u-margin-y--0 vads-u-margin-top--0p5 vads-u-padding-y--1">
+              <CTALink
+                href="/disability/view-disability-rating/rating"
+                text="Review your VA disability rating"
+                className="vads-u-font-weight--bold"
+                showArrow
+              />
+            </p>
+          </va-card>
+        </>
       );
     }
 
     // User does not have a disability rating
     return (
       <>
+        <Heading />
         <p className="vads-u-margin-y--neg1">
           We don’t have a combined disability rating on file for you.
         </p>
@@ -52,6 +61,7 @@ const DisabilityRatingCard = ({
   // Default fallback
   return (
     <>
+      <Heading />
       <va-alert status="warning" slim className="vads-u-margin-bottom--2">
         We can’t show your disability rating right now. Refresh this page or try
         again later.
