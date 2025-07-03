0e19900eb47e7baf5ce953d42f71452c0ef419d9
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2025-07-03 11:28:35 -0600

    Initialize pre-fill data to form data (#37490)
    * Initialize pre-fill data to form data
    
    * Update unit test
    
    * Remove email and phone
    
    * Update unit test

--- src/applications/income-and-asset-statement/tests/unit/components/CustomPersonalInfo.unit.spec.jsx
+++ src/applications/income-and-asset-statement/tests/unit/components/CustomPersonalInfo.unit.spec.jsx
@@ -1,66 +1,29 @@
-/* eslint-disable @department-of-veterans-affairs/enzyme-unmount */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import { expect } from 'chai';
+
 import CustomPersonalInfo from '../../../components/CustomPersonalInfo';
 
-describe('CustomPersonalInfo shallow render', () => {
-  it('should render the PersonalInformation wrapper', () => {
-    const wrapper = shallow(
-      <CustomPersonalInfo
-        data={{
-          veteranSocialSecurityNumber: '1234',
-          vaFileNumber: '5678',
-        }}
-      />,
+describe('CustomPersonalInfo Component', () => {
+  const middleware = [thunk];
+  const mockStore = configureStore(middleware);
+
+  it('renders PersonalInformation component', () => {
+    const wrapper = mount(
+      <Provider store={mockStore({})}>
+        <CustomPersonalInfo
+          data={{
+            veteranSocialSecurityNumber: '1234',
+            vaFileNumber: '5678',
+          }}
+        />
+      </Provider>,
     );
 
     expect(wrapper.find('PersonalInformation')).to.have.lengthOf(1);
-  });
-
-  it('should include a PersonalInformationHeader as child', () => {
-    const wrapper = shallow(
-      <CustomPersonalInfo
-        data={{
-          veteranSocialSecurityNumber: '1234',
-          vaFileNumber: '5678',
-        }}
-      />,
-    );
-
-    const header = wrapper.find('PersonalInformationHeader');
-    expect(header).to.have.lengthOf(1);
-  });
-
-  it('should pass dataAdapter prop correctly', () => {
-    const wrapper = shallow(
-      <CustomPersonalInfo
-        data={{
-          veteranSocialSecurityNumber: '1234',
-          vaFileNumber: '5678',
-        }}
-      />,
-    );
-
-    expect(wrapper.props().dataAdapter).to.deep.equal({
-      ssnPath: 'veteranSocialSecurityNumber',
-      vaFileNumberPath: 'vaFileNumber',
-    });
-  });
-
-  it('should pass required fields in config prop', () => {
-    const wrapper = shallow(
-      <CustomPersonalInfo
-        data={{
-          veteranSocialSecurityNumber: '1234',
-          vaFileNumber: '5678',
-        }}
-      />,
-    );
-
-    expect(wrapper.props().config.name).to.deep.equal({
-      show: true,
-      required: true,
-    });
+    wrapper.unmount();
   });
 });
