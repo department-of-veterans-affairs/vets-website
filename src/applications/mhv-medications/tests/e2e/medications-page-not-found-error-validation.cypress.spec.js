b5960df0d8a8e3771699b68f3ad46ee5b341a05a
radavis <1299034+radavis@users.noreply.github.com>
2025-07-07 14:42:36 -0400

    Updates Rx app to use MhvPageNotFound

--- src/applications/mhv-medications/tests/e2e/medications-page-not-found-error-validation.cypress.spec.js
+++ src/applications/mhv-medications/tests/e2e/medications-page-not-found-error-validation.cypress.spec.js
@@ -1,10 +1,9 @@
-import { pageNotFoundHeading } from '@department-of-veterans-affairs/platform-site-wide/PageNotFound';
 import { rootUrl } from '../../manifest.json';
 
 describe('Medications unsupported URL', () => {
   it('Visit unsupported URL and get a page not found', () => {
     cy.visit(`${rootUrl}/dummy/dummy`);
     cy.injectAxeThenAxeCheck();
-    cy.findByRole('heading', { name: pageNotFoundHeading }).should.exist;
+    cy.findByTestId('mhv-page-not-found');
   });
 });
