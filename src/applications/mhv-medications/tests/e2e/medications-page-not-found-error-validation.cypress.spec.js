4d2276c5637a7d345d2c87d0904a7537675c7df8
radavis <1299034+radavis@users.noreply.github.com>
2025-07-07 15:28:43 -0400

    Adds specs for paths that should render MhvPageNotFound under /my-health/medications

--- src/applications/mhv-medications/tests/e2e/medications-page-not-found-error-validation.cypress.spec.js
+++
@@ -1,9 +0,0 @@
-import { rootUrl } from '../../manifest.json';
-
-describe('Medications unsupported URL', () => {
-  it('Visit unsupported URL and get a page not found', () => {
-    cy.visit(`${rootUrl}/dummy/dummy`);
-    cy.injectAxeThenAxeCheck();
-    cy.findByTestId('mhv-page-not-found');
-  });
-});
