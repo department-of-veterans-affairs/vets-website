f23123d96fba54aaaf2812482ff8f53065956e54
Kevin Suarez <kjsuarez94@gmail.com>
2025-07-07 13:28:27 -0400

    111193 Add phone number and content to the "Need help?" section of the Supply Reorder app

--- src/applications/health-care-supply-reordering/components/FooterInfo.jsx
+++ src/applications/health-care-supply-reordering/components/FooterInfo.jsx
@@ -3,10 +3,14 @@ import { CONTACTS } from '@department-of-veterans-affairs/component-library/cont
 
 const FooterInfo = () => (
   <section className="need-help-footer row vads-u-padding-x--1p5">
-    For benefit-related questions, or if the form isn’t working right, please
+    For help filling out this form, or if the form isn’t working right, please
     call VA Benefits and Services at{' '}
     <va-telephone contact={CONTACTS.VA_BENEFITS} />. If you have hearing loss,
-    call <va-telephone contact={CONTACTS['711']} tty />.
+    call <va-telephone contact={CONTACTS['711']} tty />. For help ordering
+    hearing aid or CPAP supplies, please call the DLC Customer Service Section
+    at <va-telephone contact="8776778710" /> (
+    <va-telephone contact={CONTACTS['711']} tty />) or email{' '}
+    <a href="mailto:dalc.css@va.gov">dalc.css@va.gov</a>.
   </section>
 );
 
