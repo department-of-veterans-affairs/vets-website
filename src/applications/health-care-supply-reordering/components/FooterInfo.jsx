c3992605fb5815f21d2a6622860598a127558212
Kevin Suarez <kjsuarez94@gmail.com>
2025-07-08 15:14:14 -0400

    111193 change requests

--- src/applications/health-care-supply-reordering/components/FooterInfo.jsx
+++ src/applications/health-care-supply-reordering/components/FooterInfo.jsx
@@ -3,14 +3,21 @@ import { CONTACTS } from '@department-of-veterans-affairs/component-library/cont
 
 const FooterInfo = () => (
   <section className="need-help-footer row vads-u-padding-x--1p5">
-    <b>For help filling out this form, or if the form isn’t working right,</b>{' '}
-    please call VA Benefits and Services at{' '}
-    <va-telephone contact={CONTACTS.VA_BENEFITS} />. If you have hearing loss,
-    call <va-telephone contact={CONTACTS['711']} tty />.<br />
-    <b>For help ordering hearing aid or CPAP supplies,</b> please call the DLC
-    Customer Service Section at <va-telephone contact="8776778710" /> (
-    <va-telephone contact={CONTACTS['711']} tty />) or email{' '}
-    <a href="mailto:dalc.css@va.gov">dalc.css@va.gov</a>.
+    <p>
+      <strong>
+        For help filling out this form, or if the form isn’t working right,
+      </strong>{' '}
+      please call VA Benefits and Services at{' '}
+      <va-telephone contact={CONTACTS.VA_BENEFITS} />. If you have hearing loss,
+      call <va-telephone contact={CONTACTS['711']} tty />.
+    </p>
+    <p>
+      <strong>For help ordering hearing aid or CPAP supplies,</strong> please
+      call the DLC Customer Service Section at{' '}
+      <va-telephone contact="8776778710" /> (
+      <va-telephone contact={CONTACTS['711']} tty />) or email{' '}
+      <a href="mailto:dalc.css@va.gov">dalc.css@va.gov</a>.
+    </p>
   </section>
 );
 
