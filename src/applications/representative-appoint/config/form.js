a611858f177803005e229bd157dfd9e9701355c2
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-07-10 16:54:13 -0400

    deleted api files

--- src/applications/representative-appoint/config/form.js
+++ src/applications/representative-appoint/config/form.js
@@ -6,8 +6,7 @@ import configService from '../utilities/configService';
 import manifest from '../manifest.json';
 import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
-import { generatePDF } from '../api/generatePDF';
-import { submitPOARequest } from '../api/submitPOARequest';
+import { submitRequest } from '../api/submitRequest';
 import PreSubmitInfo from '../containers/PreSubmitInfo';
 import {
   claimantPersonalInformation,
@@ -21,8 +20,6 @@ import SubmissionError from '../components/SubmissionError';
 
 const { fullName, ssn, date, dateRange, usaPhone } = commonDefinitions;
 
-const formConfigFromService = configService.getFormConfig();
-
 const formConfig = {
   rootUrl: manifest.rootUrl,
   urlPrefix: '/',
@@ -31,10 +28,8 @@ const formConfig = {
     submitButtonText: 'Continue',
   },
   submit: async form => {
-    await generatePDF(form.data);
-
     if (form.data.representativeSubmissionMethod === 'digital') {
-      await submitPOARequest(form.data);
+      await submitRequest(form.data);
     }
 
     return Promise.resolve({ attributes: { confirmationNumber: '123123123' } }); // I'm not sure what this confirmation number is about
@@ -71,7 +66,7 @@ const formConfig = {
       'Please sign in again to continue your application for VA accredited representative appointment.',
   },
   title: 'Request help from a VA accredited representative or VSO',
-  subTitle: formConfigFromService.subTitle || 'VA Forms 21-22 and 21-22a',
+  subTitle: 'VA Forms 21-22 and 21-22a',
   defaultDefinitions: {
     fullName,
     ssn,
