e5a5fc0a9d8a5bf60293291d2d940f088e9a3546
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-07-10 16:04:23 -0400

    stable intro page

--- src/applications/representative-appoint/containers/IntroductionPage.jsx
+++ src/applications/representative-appoint/containers/IntroductionPage.jsx
@@ -24,25 +24,8 @@ const IntroductionPage = props => {
   return (
     <article className="schemaform-intro">
       <div className="title-section">
-        <FormTitle
-          title="Request help from a VA accredited representative or VSO"
-          subTitle="VA Form 21-22 and VA Form 21-22a"
-        />
-        <p>
-          A VA accredited representative can help you file a claim or request a
-          decision review.
-        </p>
-        <p>
-          Use our online tool to pre-fill your VA Form 21-22 to appoint a
-          Veteran Service Organization (VSO). Or, use our online tool to
-          pre-fill your VA Form 21-22a to appoint a VA accredited attorney or
-          claims agent.
-        </p>
-        <>
-          <div tabIndex="-1">
-            <div data-widget-type="representative-status" />
-          </div>
-        </>
+        <FormTitle title="Form title" subTitle="Form subtitle" />
+        <p>blah blah</p>
       </div>
       {loggedIn && (
         <>
@@ -62,65 +45,21 @@ const IntroductionPage = props => {
       <h2>Follow these steps to pre-fill your form</h2>
       <div className="vads-u-padding-left--2">
         <va-process-list uswds>
-          <va-process-list-item
-            header="Contact the accredited representative"
-            level="3"
-          >
-            <p>
-              You’ll need to contact the accredited representative you’d like to
-              appoint to ask if they’re available to help you.
-            </p>
-            <p>
-              If you’d like to work with an accredited VSO representative,
-              you’ll need to appoint their organization. You should contact them
-              to confirm which organization they’re accredited by. You’ll need
-              to select this organization on your form.
-            </p>
-            <va-additional-info trigger="If you don't know who you'd like to appoint">
-              <p>
-                You can use our search tool to find an accredited
-                representative.
-              </p>
-              <p />
-              <a
-                href="/get-help-from-accredited-representative/find-rep"
-                target="_blank"
-              >
-                Find an accredited representative or VSO
-              </a>
-            </va-additional-info>
+          <va-process-list-item header="Example header" level="3">
+            <p>blah blah</p>
+            <p>blah blah blah blah blah</p>
           </va-process-list-item>
           <va-process-list-item header="Gather your information" level="3">
             <p>Here’s what you’ll need to complete this form:</p>
             <ul>
-              <li>Your Social Security number or VA file number</li>
-              <li>Your date of birth</li>
-              <li>Your mailing address and phone number</li>
-              <li>
-                Your branch of service (only if you’d like to appoint an
-                attorney or claims agent)
-              </li>
-              <li>
-                The name of the organization you’d like to appoint, if you’d
-                like to work with an accredited VSO representative
-              </li>
+              <li>blah</li>
+              <li>blach</li>
             </ul>
-            <p>
-              If you’re a dependent or surviving spouse, we’ll ask you for some
-              personal information about the Veteran or service member you’re
-              connected to.
-            </p>
           </va-process-list-item>
           <va-process-list-item header="Start your form" level="3">
             <p>
               We’ll take you through each step of our online tool. It should
               take about 5 minutes.
-            </p>
-            <p>
-              After you complete your form, you’ll need to download, print, and
-              sign it. Then mail or bring your form to the accredited
-              representative you’re appointing. The accredited representative
-              will sign your form and submit it for you.
             </p>
           </va-process-list-item>
         </va-process-list>
