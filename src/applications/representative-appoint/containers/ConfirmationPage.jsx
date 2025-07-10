6c9e3205929b5f5f8c8c63890d119053f2a8e4af
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-07-10 16:05:36 -0400

    Confirmation page

--- src/applications/representative-appoint/containers/ConfirmationPage.jsx
+++ src/applications/representative-appoint/containers/ConfirmationPage.jsx
@@ -1,80 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
 import PropTypes from 'prop-types';
-
-import { VaCheckbox } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import { scrollTo } from 'platform/utilities/scroll';
-import sendNextStepsEmail from '../api/sendNextStepsEmail';
-import { getFormNumber, getFormName } from '../utilities/helpers';
 
 import GetFormHelp from '../components/GetFormHelp';
 
-import ConfirmationDigitalSubmission from './ConfirmationDigitalSubmission';
-
-export default function ConfirmationPage({ router }) {
-  const checkboxRef = useRef(null);
-  const [signedForm, setSignedForm] = useState(false);
-  const [signedFormError, setSignedFormError] = useState(false);
-  const { data: formData } = useSelector(state => state.form);
-  const selectedEntity = formData['view:selectedRepresentative'];
-  const sendNextStepsEmailPayload = {
-    formNumber: getFormNumber(formData),
-    formName: getFormName(formData),
-    firstName: formData.applicantName?.first || formData.veteranFullName.first,
-    emailAddress: formData.applicantEmail || formData.veteranEmail,
-    entityId: selectedEntity.id,
-    entityType:
-      selectedEntity.type === 'organization' ? 'organization' : 'individual',
-  };
-
-  const isDigitalSubmission =
-    formData.representativeSubmissionMethod === 'digital';
-
-  const v2IsEnabled = formData?.['view:v2IsEnabled'];
-  useEffect(() => {
-    scrollTo('topScrollElement');
-  }, []);
-
-  const handlers = {
-    onChangeSignedFormCheckbox: () => {
-      setSignedForm(prevState => !prevState);
-
-      if (signedFormError) setSignedFormError(false);
-    },
-    onClickContinueButton: async () => {
-      if (signedForm) {
-        try {
-          await sendNextStepsEmail(sendNextStepsEmailPayload);
-        } catch (error) {
-          // Don't do anything if we fail to send the email
-        }
-
-        router.push('/next-steps');
-      } else {
-        setSignedFormError(true);
-
-        if (checkboxRef.current) {
-          checkboxRef.current.scrollIntoView({
-            behavior: 'smooth',
-            block: 'center',
-          });
-
-          const { shadowRoot } = checkboxRef.current;
-          const inputElement = shadowRoot?.querySelector(
-            'input[type="checkbox"]',
-          );
-          if (inputElement) {
-            inputElement.focus();
-          }
-        }
-      }
-    },
-  };
-
-  if (isDigitalSubmission && v2IsEnabled) {
-    return <ConfirmationDigitalSubmission />;
-  }
-
+export default function ConfirmationPage() {
   return (
     <>
       <h2 className="vads-u-font-size--h3">
@@ -85,28 +14,10 @@ export default function ConfirmationPage({ router }) {
         download
         filetype="PDF"
         href={localStorage.getItem('pdfUrl')}
-        filename={`VA Form ${getFormNumber(formData)}`}
+        filename="VA form"
         label="Download your form"
         text="Download your form"
       />
-      <p className="vads-u-margin-top--4">
-        Then, you’ll need to print and sign your form.
-      </p>
-      <VaCheckbox
-        ref={checkboxRef}
-        checked={signedForm}
-        className="vads-u-margin-bottom--4"
-        error={
-          signedFormError
-            ? "Please confirm that you've downloaded, printed, and signed your form."
-            : null
-        }
-        label="I’ve downloaded, printed, and signed my form"
-        name="signedForm"
-        required
-        onVaChange={handlers.onChangeSignedFormCheckbox}
-      />
-      <va-button continue onClick={handlers.onClickContinueButton} />
 
       <div>
         <h2 className="help-heading">Need help?</h2>
