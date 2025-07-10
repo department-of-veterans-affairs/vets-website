a53e79ff3ea42933eb36a283250eb079f47aaf48
William Phelps <williamphelps13@gmail.com>
2025-07-10 06:56:26 -0700

    Clean up file upload implementation and move components to proper locations
    - Remove development-specific files and debug components
    - Move fileValidation.js and DebugInfo.jsx to parent directory
    - Remove unused test files and component variants
    - Update imports to reflect new file structure
    - Clean up documentation and markup files used during development

--- src/applications/claims-status/components/claim-files-tab/AdditionalEvidencePage.jsx
+++ src/applications/claims-status/components/claim-files-tab/AdditionalEvidencePage.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 import { getScrollOptions, Element, scrollTo } from 'platform/utilities/scroll';
 
-import NewAddFilesForm from './file-input-multiple/NewAddFilesForm';
+import AddFilesForm from './AddFilesForm';
 import Notification from '../Notification';
 import FilesOptional from './FilesOptional';
 import FilesNeeded from './FilesNeeded';
@@ -144,7 +144,7 @@ class AdditionalEvidencePage extends React.Component {
               {this.props.filesOptional.map(item => (
                 <FilesOptional key={item.id} id={claim.id} item={item} />
               ))}
-              <NewAddFilesForm
+              <AddFilesForm
                 uploading={this.props.uploading}
                 progress={this.props.progress}
                 onCancel={this.props.cancelUpload}
