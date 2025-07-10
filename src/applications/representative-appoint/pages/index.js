f478729e5e7eaa72acf4c3c856cd56f898ea5f65
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-07-10 16:35:46 -0400

    Replaced index page

--- src/applications/representative-appoint/pages/index.js
+++ src/applications/representative-appoint/pages/index.js
@@ -1,53 +1,21 @@
-import * as authorizeMedical from './authorizations/authorizeMedical';
 import * as authorizeMedicalSelect from './authorizations/authorizeMedicalSelect';
 import * as authorizeAddress from './authorizations/authorizeAddress';
-import * as authorizeInsideVA from './authorizations/authorizeInsideVA';
-import * as authorizeOutsideVA from './authorizations/authorizeOutsideVA';
-import * as authorizeOutsideVANames from './authorizations/authorizeOutsideVANames';
-import * as claimantType from './claimant/claimantType';
 import * as claimantContactPhoneEmail from './claimant/claimantContactPhoneEmail';
-import * as claimantRelationship from './claimant/claimantRelationship';
 import * as claimantPersonalInformation from './claimant/claimantPersonalInformation';
 import * as claimantContactMailing from './claimant/claimantContactMailing';
-import * as veteranPersonalInformation from './veteran/veteranPersonalInformation';
-import * as veteranContactPhoneEmail from './veteran/veteranContactPhoneEmail';
-import * as veteranContactPhoneEmailClaimant from './veteran/veteranContactPhoneEmailClaimant';
-import * as veteranContactMailing from './veteran/veteranContactMailing';
-import * as veteranContactMailingClaimant from './veteran/veteranContactMailingClaimant';
 import * as veteranIdentification from './veteran/veteranIdentification';
 import * as veteranServiceInformation from './veteran/veteranServiceInformation';
-import * as selectAccreditedRepresentative from './representative/selectAccreditedRepresentative';
-import * as replaceAccreditedRepresentative from './representative/replaceAccreditedRepresentative';
-import * as selectedAccreditedOrganizationId from './representative/selectAccreditedOrganization';
-import * as contactAccreditedRepresentative from './representative/contactAccreditedRepresentative';
-import * as representativeSubmissionMethod from './representative/representativeSubmissionMethod';
 import * as checkboxPage from './checkboxPage';
 import * as radioPage from './radioPage';
 
 export {
-  authorizeMedical,
   authorizeMedicalSelect,
   authorizeAddress,
-  authorizeInsideVA,
-  authorizeOutsideVA,
-  authorizeOutsideVANames,
-  claimantType,
   claimantContactPhoneEmail,
-  claimantRelationship,
-  claimantPersonalInformation,
-  claimantContactMailing,
-  veteranPersonalInformation,
-  veteranContactPhoneEmail,
-  veteranContactPhoneEmailClaimant,
-  veteranContactMailing,
-  veteranContactMailingClaimant,
-  veteranIdentification,
-  veteranServiceInformation,
-  selectAccreditedRepresentative,
-  replaceAccreditedRepresentative,
-  selectedAccreditedOrganizationId,
-  contactAccreditedRepresentative,
-  representativeSubmissionMethod,
   checkboxPage,
   radioPage,
+  claimantPersonalInformation,
+  claimantContactMailing,
+  veteranIdentification,
+  veteranServiceInformation,
 };
