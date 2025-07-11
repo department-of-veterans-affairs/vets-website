94c355a4622369509ff8070e81820ce2c416b46a
Adam King <adam.king0126@gmail.com>
2025-07-11 12:44:42 -0500

    update pre-need app to use node22 compatible tests

--- src/applications/pre-need/tests/config/nonVeteranApplicantDetails.unit.spec.jsx
+++ src/applications/pre-need/tests/config/nonVeteranApplicantDetails.unit.spec.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { mount } from 'enzyme';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 
 import {
   DefinitionTester,
@@ -33,9 +34,9 @@ describe('Pre-need applicant non veteran applicant details', () => {
     form.unmount();
   });
 
-  it('should not submit empty form', () => {
+  it('should not submit empty form', async () => {
     const onSubmit = sinon.spy();
-    const form = mount(
+    const { container } = render(
       <DefinitionTester
         schema={schema}
         definitions={formConfig.defaultDefinitions}
@@ -43,12 +44,13 @@ describe('Pre-need applicant non veteran applicant details', () => {
         uiSchema={uiSchema}
       />,
     );
+    fireEvent.submit(container.querySelector('form'));
 
-    form.find('form').simulate('submit');
-
-    expect(form.find('.usa-input-error').length).to.equal(4);
-    expect(onSubmit.called).to.be.false;
-    form.unmount();
+    await waitFor(() => {
+      const errorElements = container.querySelectorAll('.usa-input-error');
+      expect(errorElements.length).to.equal(4);
+      expect(onSubmit.called).to.be.false;
+    });
   });
 
   it('should submit with required information', () => {
