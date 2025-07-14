5faa36258589f46654d433bb25b061f07c485704
Phuc Tran (Alexander) <115654176+TranScripted@users.noreply.github.com>
2025-07-14 12:08:59 -0400

    refactor: error message content for toxic exposure dates (#37665)
    * refactor: error message content for toxic exposure dates
    
    * updated content and renamed variables

--- src/applications/disability-benefits/all-claims/utils/validations.js
+++ src/applications/disability-benefits/all-claims/utils/validations.js
@@ -2,19 +2,21 @@ import { convertToDateField } from '~/platform/forms-system/src/js/validation';
 import { isValidDateRange } from '~/platform/forms/validations';
 
 const messages = {
-  invalidRange: 'Enter a service start date that’s before your end date.',
-  endDate1990: 'Enter a service end date after August 2, 1990.',
-  endDate2001: 'Enter a service end date after September 11, 2001.',
+  invalidRange:
+    'Enter a service start date that occurs earlier than your end date',
+  endDate1990: 'Enter a service end date after August 2, 1990',
+  endDate2001: 'Enter a service end date after September 11, 2001',
   // This is largely redundant because the component validation already prevents
   // inputting dates in the future. However, this is in place just in case, because...
   // TODO: The current schema validation says that the Veteran can input a year
   // up to 100 years in the future. That is incorrect and should be corrected/removed.
-  futureDate: 'Enter a service year that’s before or during the current year.',
+  endServiceDate: 'Enter a service end date no later than today’s date',
+  startServiceDate: 'Enter a service start date no later than today’s date',
   // Schema validations will display an error when a value is missing, however it
   // will not properly prevent the Veteran from progressing, necessitating this.
   // TODO: The error message here can be made more specific, but that will somewhat
   // depend on what decisions (and schema updates) are made in regards to partial dates.
-  missingValue: 'Enter a service date that includes the month, day, and year.',
+  missingValue: 'Enter a service date that includes the month, day, and year',
 };
 
 export function validateRange(errors, startDate, endDate) {
@@ -26,11 +28,11 @@ export function validateRange(errors, startDate, endDate) {
   }
 
   if (startDateApproximate > Date.now()) {
-    errors.startDate.addError(messages.futureDate);
+    errors.startDate.addError(messages.startServiceDate);
   }
 
   if (endDateApproximate > Date.now()) {
-    errors.endDate.addError(messages.futureDate);
+    errors.endDate.addError(messages.endServiceDate);
   }
 }
 
