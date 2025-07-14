4c92c3fc633c12e4ca1369d4d69281c569e6c27e
Nick Sayre <nick.sayre@adhocteam.us>
2025-07-14 11:42:54 -0500

    1673 - add SIP support

---
+++ src/applications/new-chapter-36-form/tests/e2e/save-in-progress.cypress.spec.jsx
@@ -0,0 +1,51 @@
+import testForm from 'platform/testing/e2e/cypress/support/form-tester';
+import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-tester/utilities';
+
+import formConfig from '../../config/form';
+import manifest from '../../manifest.json';
+import userSip from '../fixtures/mocks/user-sip.json';
+import submit from '../fixtures/mocks/submit.json';
+import featureToggles from '../fixtures/mocks/featureToggles.json';
+import minimalFlow from '../fixtures/data/minimal-flow.json';
+import mockPrefill from '../fixtures/mocks/sip-get.json';
+import mockInProgress from '../fixtures/mocks/sip-put.json';
+
+// This test ensures that the save-in-progress functionality of the 28-1900 form works as expected.
+const testConfig = createTestConfig(
+  {
+    dataPrefix: 'data',
+    dataDir: null,
+    dataSets: [{ title: 'save in progress', data: minimalFlow }],
+
+    pageHooks: {
+      introduction: ({ afterHook }) => {
+        afterHook(() => {
+          cy.injectAxeThenAxeCheck();
+          cy.contains('button', 'Continue your application').click();
+        });
+      },
+      'name-and-date-of-birth': ({ afterHook }) => {
+        afterHook(() => {
+          cy.injectAxeThenAxeCheck();
+          cy.get('@testData').then(() => {
+            cy.fillPage();
+            cy.findByText(/continue/i, { selector: 'button' }).click();
+          });
+        });
+      },
+    },
+
+    setupPerTest: () => {
+      cy.intercept('GET', '/v0/user', userSip);
+      cy.intercept('/v0/feature_toggles*', featureToggles);
+      cy.intercept('PUT', '/v0/in_progress_forms/28-1900_V2', mockInProgress);
+      cy.intercept('GET', '/v0/in_progress_forms/28-1900_V2', mockPrefill);
+      cy.intercept('POST', '/v0/veteran_readiness_employment_claims', submit);
+      cy.login(userSip);
+    },
+  },
+  manifest,
+  formConfig,
+);
+
+testForm(testConfig);
