5d08cfa80a320cfc2f76778ff697784660a37267
Dakota Larson <133443504+DLarson-Oddball@users.noreply.github.com>
2025-07-18 13:49:31 -0700

    DMT | Update View Dependents Content (#37827)
    * DMT | Update View Dependents Content
    
    * remove pdf suffix

--- src/applications/dependents/view-dependents/layouts/ViewDependentsLayoutV2.jsx
+++ src/applications/dependents/view-dependents/layouts/ViewDependentsLayoutV2.jsx
@@ -2,14 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ViewDependentsLists from './ViewDependentsListsV2';
-import ViewDependentsSidebar from '../components/ViewDependentsSidebar/ViewDependentsSidebar';
 import ViewDependentsHeader from '../components/ViewDependentsHeader/ViewDependentsHeaderV2';
-import ViewDependentsSidebarBlock from '../components/ViewDependentsSidebar/ViewDependentsSidebarBlock';
-import {
-  firstSidebarBlock,
-  secondSidebarBlock,
-  thirdSidebarBlock,
-} from '../components/ViewDependentsSidebar/ViewDependentsSidebarBlockStates/ViewDependentSidebarBlockStates';
 import { isServerError, isClientError } from '../util';
 import { errorFragment, infoFragment } from './helpers';
 
@@ -42,28 +35,10 @@ function ViewDependentsLayout(props) {
   }
 
   const layout = (
-    <div className="vads-l-row">
-      <div className="vads-l-col--12 medium-screen:vads-l-col--8">
-        <ViewDependentsHeader updateDiariesStatus={props.updateDiariesStatus} />
-        {mainContent}
-      </div>
-      <div className="vads-l-col--12 medium-screen:vads-l-col--4">
-        <ViewDependentsSidebar>
-          <ViewDependentsSidebarBlock
-            heading={firstSidebarBlock.heading}
-            content={firstSidebarBlock.content}
-          />
-          <ViewDependentsSidebarBlock
-            heading={secondSidebarBlock.heading}
-            content={secondSidebarBlock.content}
-          />
-          <ViewDependentsSidebarBlock
-            heading={thirdSidebarBlock.heading}
-            content={thirdSidebarBlock.content}
-          />
-        </ViewDependentsSidebar>
-      </div>
-    </div>
+    <>
+      <ViewDependentsHeader updateDiariesStatus={props.updateDiariesStatus} />
+      {mainContent}
+    </>
   );
 
   return <div>{layout}</div>;
