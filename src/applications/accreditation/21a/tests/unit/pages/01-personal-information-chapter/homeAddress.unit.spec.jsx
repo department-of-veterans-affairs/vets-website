64ffa278c79249929d51821f5a0d504581bfbd07
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-07-21 16:23:32 -0600

    update tests

--- src/applications/accreditation/21a/tests/unit/pages/01-personal-information-chapter/homeAddress.unit.spec.jsx
+++ src/applications/accreditation/21a/tests/unit/pages/01-personal-information-chapter/homeAddress.unit.spec.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { expect } from 'chai';
 import { render } from '@testing-library/react';
+
+import { $ } from '@department-of-veterans-affairs/platform-forms-system/ui';
 import SchemaForm from 'platform/forms-system/src/js/components/SchemaForm';
+
 import homeAddressPage from '../../../../pages/01-personal-information-chapter/homeAddress';
 
 describe('Home address page', () => {
@@ -26,11 +29,25 @@ describe('Home address page', () => {
       />,
     );
 
-    expect(getByText('Primary home address')).to.exist;
-    const html = container.innerHTML;
-    expect(html).to.include('label="Street address"');
-    expect(html).to.include('label="city"');
-    expect(html).to.include('label="state"');
-    expect(html).to.include('label="Postal code"');
+    getByText('Primary home address');
+
+    expect($('va-checkbox', container).getAttribute('label')).to.eq(
+      'I live on a U.S. military base outside of the United States.',
+    );
+    expect($('va-checkbox', container).getAttribute('checked')).to.eq('false');
+
+    expect($('va-select[label="Country"]', container)).to.exist;
+    expect($('va-text-input[label="Street address"]', container)).to.exist;
+    // expect($('va-text-input[label="Street address line 2"]', container)).to
+    //   .exist;
+    expect($('va-text-input[label="City"]', container)).to.exist;
+    expect($('va-select[label="State"]', container)).to.exist;
+    // expect($('va-text-input[label="Postal code"]', container)).to.exist;
+
+    // const html = container.innerHTML;
+    // expect(html).to.include('label="Street address"');
+    // expect(html).to.include('label="city"');
+    // expect(html).to.include('label="state"');
+    // expect(html).to.include('label="Postal code"');
   });
 });
