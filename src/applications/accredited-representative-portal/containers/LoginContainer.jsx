f3462d3c5685f8d24c66553124325fc155e559d9
jquispe-oddball <jennifer.quispe@oddball.io>
2025-07-21 16:03:02 -0400

    104001 - minor fe updates

--- src/applications/accredited-representative-portal/containers/LoginContainer.jsx
+++ src/applications/accredited-representative-portal/containers/LoginContainer.jsx
@@ -27,20 +27,45 @@ const LoginContainer = () => {
   };
 
   return (
-    <>
+    <section className="login">
       {hasError && renderErrorMessage()}
-      <div className="vads-l-grid-container large-screen:vads-u-padding-x--0">
+      <div className="arp-container">
         <div className="vads-l-row">
           <div className="vads-l-col--12 vads-u-padding-y--5">
-            <h1>Sign in to the Accredited Representative Portal</h1>
+            <h1 className="columns vads-u-margin--0">
+              Sign in or create an account
+            </h1>
             <LoginActions
               externalApplication={EXTERNAL_APPS.ARP}
               isUnifiedSignIn
             />
           </div>
         </div>
+        <div className="vads-l-row">
+          <div className="columns vads-l-col--12 vads-u-padding-bottom--5">
+            <h2 className="vads-u-margin-top--0">Help and support</h2>
+
+            <ul className="vads-u-padding-bottom--3">
+              <li className="vads-u-margin--0">
+                <a href="/resources/verifying-your-identity-on-vagov/">
+                  Verifying your identity
+                </a>
+              </li>
+              <li className="vads-u-margin--0">
+                <a href="/resources/can-i-delete-my-logingov-or-idme-account">
+                  Deleting your account
+                </a>
+              </li>
+              <li className="vads-u-margin--0">
+                <a href="/resources/support-for-common-logingov-and-idme-issues/">
+                  Common issues with Login.gov or ID.me
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </>
+    </section>
   );
 };
 
