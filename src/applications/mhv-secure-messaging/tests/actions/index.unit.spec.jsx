617774ec77b99df07b663dee4877196523ecff84
Carlos Felix <63804190+carlosfelix2@users.noreply.github.com>
2025-07-22 17:13:33 -0400

    Changed Secure Messaging to use a feature toggle instead of URL for pilot (#37801)

--- src/applications/mhv-secure-messaging/tests/actions/index.unit.spec.jsx
+++ src/applications/mhv-secure-messaging/tests/actions/index.unit.spec.jsx
@@ -1,13 +1,12 @@
 import { expect } from 'chai';
+import FEATURE_FLAG_NAMES from '@department-of-veterans-affairs/platform-utilities/featureFlagNames';
 import { getIsPilotFromState } from '../../actions';
 
 describe('getIsPilotFromState', () => {
   it('should return the value of isPilot from the state', () => {
     const getState = () => ({
-      sm: {
-        app: {
-          isPilot: true,
-        },
+      featureToggles: {
+        [FEATURE_FLAG_NAMES.mhvSecureMessagingCernerPilot]: true,
       },
     });
 
@@ -16,7 +15,7 @@ describe('getIsPilotFromState', () => {
   });
 
   it('should return false if isPilot is not in the state', () => {
-    const getState = () => ({ sm: {} });
+    const getState = () => ({ featureToggles: {} });
 
     const isPilot = getIsPilotFromState(getState);
     expect(isPilot).to.equal(false);
