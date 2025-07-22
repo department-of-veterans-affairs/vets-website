3465a0a5cd819431b1ff8977f72c2aa4ce07129e
122034971+ConnorJeff@users.noreply.github.com <122034971+ConnorJeff@users.noreply.github.com>
2025-07-22 12:20:04 -0400

    Added prod flag

--- src/applications/simple-forms/40-0247/pages/applicantContactInfo.js
+++ src/applications/simple-forms/40-0247/pages/applicantContactInfo.js
@@ -1,18 +1,23 @@
 import {
-  titleUI,
   emailSchema,
   emailUI,
   phoneUI,
   phoneSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
+import { titleUI } from 'platform/forms-system/src/js/web-component-patterns/titlePattern';
+import environment from 'platform/utilities/environment';
 
 /** @type {PageSchema} */
 export default {
   uiSchema: {
-    ...titleUI(
-      'Contact details',
-      'Tell us how we can reach you if there’s a question about your request.',
-    ),
+    ...(environment.isProduction()
+      ? titleUI(
+          'Tell us how we can reach you if there’s a question about your request',
+        )
+      : titleUI(
+          'Contact details',
+          'Tell us how we can reach you if there’s a question about your request.',
+        )),
     applicantPhone: phoneUI(),
     applicantEmail: emailUI(),
   },
