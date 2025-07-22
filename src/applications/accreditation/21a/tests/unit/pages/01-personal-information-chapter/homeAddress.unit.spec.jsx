8587183551be3bed4c8a24c026da4ad42b1028c7
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-07-22 13:54:14 -0600

    add better test coverage

--- src/applications/accreditation/21a/tests/unit/pages/01-personal-information-chapter/homeAddress.unit.spec.jsx
+++ src/applications/accreditation/21a/tests/unit/pages/01-personal-information-chapter/homeAddress.unit.spec.jsx
@@ -8,46 +8,166 @@ import SchemaForm from 'platform/forms-system/src/js/components/SchemaForm';
 import homeAddressPage from '../../../../pages/01-personal-information-chapter/homeAddress';
 
 describe('Home address page', () => {
-  it('renders the title and home address fields', () => {
-    const { container, getByText } = render(
-      <SchemaForm
-        name="homeAddress"
-        title={homeAddressPage.title}
-        schema={homeAddressPage.schema}
-        uiSchema={homeAddressPage.uiSchema}
-        data={{
-          homeAddress: {
-            street: '123 Main St',
-            city: 'Springfield',
-            state: 'IL',
-            postalCode: '12345',
-            country: 'United States',
-          },
-        }}
-        onChange={() => {}}
-        onSubmit={() => {}}
-      />,
-    );
+  context('when isMilitary is false', () => {
+    const data = {
+      homeAddress: {
+        street: '123 Main St',
+        street2: '',
+        city: 'Springfield',
+        state: 'IL',
+        postalCode: '12345',
+        country: 'United States',
+        isMilitary: false,
+      },
+    };
 
-    getByText('Primary home address');
+    it('renders the title and home address fields', () => {
+      const { container, getByText } = render(
+        <SchemaForm
+          name="homeAddress"
+          title={homeAddressPage.title}
+          schema={homeAddressPage.schema}
+          uiSchema={homeAddressPage.uiSchema}
+          data={{}}
+          onChange={() => {}}
+          onSubmit={() => {}}
+        />,
+      );
 
-    expect($('va-checkbox', container).getAttribute('label')).to.eq(
-      'I live on a U.S. military base outside of the United States.',
-    );
-    expect($('va-checkbox', container).getAttribute('checked')).to.eq('false');
+      getByText('Primary home address');
+      expect($('va-checkbox', container).getAttribute('label')).to.eq(
+        'I live on a U.S. military base outside of the United States.',
+      );
+      expect($('va-select[name="root_homeAddress_country"]', container)).to
+        .exist;
+      expect($('va-text-input[name="root_homeAddress_street"]', container)).to
+        .exist;
+      expect($('va-text-input[name="root_homeAddress_street2"]', container)).to
+        .exist;
+      expect($('va-text-input[name="root_homeAddress_city"]', container)).to
+        .exist;
+      expect($('va-text-input[name="root_homeAddress_state"]', container)).to
+        .exist;
+      expect($('va-text-input[name="root_homeAddress_postalCode"]', container))
+        .to.exist;
+    });
 
-    expect($('va-select[label="Country"]', container)).to.exist;
-    expect($('va-text-input[label="Street address"]', container)).to.exist;
-    // expect($('va-text-input[label="Street address line 2"]', container)).to
-    //   .exist;
-    expect($('va-text-input[label="City"]', container)).to.exist;
-    expect($('va-select[label="State"]', container)).to.exist;
-    // expect($('va-text-input[label="Postal code"]', container)).to.exist;
+    it('verifies values from the form data are populating the page', () => {
+      const { container } = render(
+        <SchemaForm
+          name="homeAddress"
+          title={homeAddressPage.title}
+          schema={homeAddressPage.schema}
+          uiSchema={homeAddressPage.uiSchema}
+          data={data}
+          onChange={() => {}}
+          onSubmit={() => {}}
+        />,
+      );
 
-    // const html = container.innerHTML;
-    // expect(html).to.include('label="Street address"');
-    // expect(html).to.include('label="city"');
-    // expect(html).to.include('label="state"');
-    // expect(html).to.include('label="Postal code"');
+      expect($('va-checkbox', container).getAttribute('checked')).to.eq(
+        'false',
+      );
+      expect(
+        $('va-select[name="root_homeAddress_country"]', container).getAttribute(
+          'value',
+        ),
+      ).to.eq(data.homeAddress.country);
+      expect(
+        $(
+          'va-text-input[name="root_homeAddress_street"]',
+          container,
+        ).getAttribute('value'),
+      ).to.eq(data.homeAddress.street);
+      expect(
+        $(
+          'va-text-input[name="root_homeAddress_street2"]',
+          container,
+        ).getAttribute('value'),
+      ).to.eq(data.homeAddress.street2);
+      expect(
+        $(
+          'va-text-input[name="root_homeAddress_city"]',
+          container,
+        ).getAttribute('value'),
+      ).to.eq(data.homeAddress.city);
+      expect(
+        $(
+          'va-text-input[name="root_homeAddress_state"]',
+          container,
+        ).getAttribute('value'),
+      ).to.eq(data.homeAddress.state);
+      expect(
+        $(
+          'va-text-input[name="root_homeAddress_postalCode"]',
+          container,
+        ).getAttribute('value'),
+      ).to.eq(data.homeAddress.postalCode);
+    });
+  });
+
+  context('when isMilitary is true', () => {
+    const data = {
+      homeAddress: {
+        street: '123 Main St',
+        street2: '',
+        city: 'APO',
+        state: 'AA',
+        postalCode: '34010',
+        country: 'United States',
+        isMilitary: true,
+      },
+    };
+
+    it('verifies values from the form data are populating the page when isMilitary is true', () => {
+      const { container } = render(
+        <SchemaForm
+          name="homeAddress"
+          title={homeAddressPage.title}
+          schema={homeAddressPage.schema}
+          uiSchema={homeAddressPage.uiSchema}
+          data={data}
+          onChange={() => {}}
+          onSubmit={() => {}}
+        />,
+      );
+
+      expect($('va-checkbox', container).getAttribute('checked')).to.eq('true');
+      expect(
+        $('va-select[name="root_homeAddress_country"]', container).getAttribute(
+          'value',
+        ),
+      ).to.eq(data.homeAddress.country);
+      expect(
+        $(
+          'va-text-input[name="root_homeAddress_street"]',
+          container,
+        ).getAttribute('value'),
+      ).to.eq(data.homeAddress.street);
+      expect(
+        $(
+          'va-text-input[name="root_homeAddress_street2"]',
+          container,
+        ).getAttribute('value'),
+      ).to.eq(data.homeAddress.street2);
+      expect(
+        $(
+          'va-text-input[name="root_homeAddress_city"]',
+          container,
+        ).getAttribute('value'),
+      ).to.eq(data.homeAddress.city);
+      expect(
+        $(
+          'va-text-input[name="root_homeAddress_state"]',
+          container,
+        ).getAttribute('value'),
+      ).to.eq(data.homeAddress.state);
+      expect(
+        $(
+          'va-text-input[name="root_homeAddress_postalCode"]',
+          container,
+        ).getAttribute('value'),
+      ).to.eq(data.homeAddress.postalCode);
+    });
   });
 });
