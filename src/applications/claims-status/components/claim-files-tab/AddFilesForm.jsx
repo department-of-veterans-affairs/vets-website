6f0fec3f87748f6d2799c48be2a77dbff726cfd7
William Phelps <williamphelps13@gmail.com>
2025-07-23 14:20:56 -0700

    Update file size limit for PDF uploads in the hint to align with validation

--- src/applications/claims-status/components/claim-files-tab/AddFilesForm.jsx
+++ src/applications/claims-status/components/claim-files-tab/AddFilesForm.jsx
@@ -20,7 +20,7 @@ import UploadStatus from '../UploadStatus';
 
 export const LABEL_TEXT = 'Upload additional evidence';
 export const HINT_TEXT =
-  'You can upload a .pdf, .gif, .jpg, .jpeg, .bmp, or .txt file. Your file should be no larger than 50 MB (non-PDF) or 150 MB (PDF only).';
+  'You can upload a .pdf, .gif, .jpg, .jpeg, .bmp, or .txt file. Your file should be no larger than 50 MB (non-PDF) or 99 MB (PDF only).';
 export const VALIDATION_ERROR = 'Please select a file first';
 export const PASSWORD_ERROR = 'Please provide a password to decrypt this file';
 export const DOC_TYPE_ERROR = 'Please provide a response';
