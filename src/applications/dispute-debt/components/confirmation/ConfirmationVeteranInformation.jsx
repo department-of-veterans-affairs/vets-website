3954186bd1ab60b292a2d8b08907114446b2b912
Andrew Rodiek <25368370+Dr-Pongo@users.noreply.github.com>
2025-07-23 14:23:44 -0400

    creating confirmation veteran information component

---
+++ src/applications/dispute-debt/components/confirmation/ConfirmationVeteranInformation.jsx
@@ -0,0 +1,188 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { reviewEntry } from 'platform/forms-system/src/js/components/ConfirmationView/ChapterSectionCollection';
+
+const ConfirmationVeteranInformation = ({ formData }) => {
+  const { veteran } = formData;
+  const { email, fullName, ssn, dateOfBirth } = veteran || {};
+
+  const {
+    mobilePhone: { phoneNumber, countryCode, areaCode, extension },
+  } = veteran;
+  const formattedPhoneNumber = `${countryCode || ''} ${areaCode ||
+    ''} ${phoneNumber || ''}${extension ? ` ext. ${extension}` : ''}`;
+
+  const {
+    mailingAddress: {
+      addressLine1,
+      addressLine2,
+      addressLine3,
+      city,
+      countryName,
+      zipCode,
+      stateCode,
+    },
+  } = veteran;
+
+  return (
+    <>
+      <h4>Veteran’s personal information</h4>
+      {fullName?.first &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-name-first`,
+          {},
+          'First name',
+          fullName?.first,
+        )}
+      {fullName?.middle &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-name-middle`,
+          {},
+          'Middle name',
+          fullName?.middle,
+        )}
+      {fullName?.last &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-name-last`,
+          {},
+          'Last name',
+          fullName?.last,
+        )}
+      {fullName?.suffix &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-name-suffix`,
+          {},
+          'Suffix',
+          fullName?.suffix,
+        )}
+      {dateOfBirth &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-dob`,
+          {},
+          'Date of birth',
+          dateOfBirth,
+        )}
+      <h4>Veteran’s identification information</h4>
+      {ssn &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-ssn`,
+          {},
+          'Social Security number',
+          `●●●–●●–${ssn}`,
+        )}{' '}
+      <h4>Veteran’s mailing address</h4>
+      {countryName &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-country`,
+          {},
+          'Country',
+          countryName,
+        )}
+      {addressLine1 &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-address-line-1`,
+          {},
+          'Street address',
+          addressLine1,
+        )}
+      {addressLine2 &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-address-line-1`,
+          {},
+          'Street address line 2',
+          addressLine2,
+        )}
+      {addressLine3 &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-address-line-1`,
+          {},
+          'Street address line 3',
+          addressLine3,
+        )}
+      {city &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-city`,
+          {},
+          'City',
+          city,
+        )}
+      {stateCode &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-state`,
+          {},
+          'State',
+          stateCode,
+        )}
+      {zipCode &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-zip`,
+          {},
+          'Postal code',
+          zipCode,
+        )}
+      <h4>Veteran’s contact information</h4>
+      {formattedPhoneNumber &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-phone`,
+          {},
+          'Phone number',
+          formattedPhoneNumber,
+        )}
+      {email &&
+        reviewEntry(
+          null,
+          `veteran-personal-information-email`,
+          {},
+          'Email address',
+          email,
+        )}
+    </>
+  );
+};
+
+ConfirmationVeteranInformation.propTypes = {
+  formData: PropTypes.shape({
+    veteran: PropTypes.shape({
+      email: PropTypes.string,
+      fullName: PropTypes.shape({
+        first: PropTypes.string,
+        middle: PropTypes.string,
+        last: PropTypes.string,
+        suffix: PropTypes.string,
+      }),
+      ssn: PropTypes.string,
+      dateOfBirth: PropTypes.string,
+      mobilePhone: PropTypes.shape({
+        phoneNumber: PropTypes.string,
+        countryCode: PropTypes.string,
+        areaCode: PropTypes.string,
+        extension: PropTypes.string,
+      }),
+      mailingAddress: PropTypes.shape({
+        addressLine1: PropTypes.string,
+        addressLine2: PropTypes.string,
+        addressLine3: PropTypes.string,
+        city: PropTypes.string,
+        countryName: PropTypes.string,
+        zipCode: PropTypes.string,
+        stateCode: PropTypes.string,
+      }),
+    }),
+  }).isRequired,
+};
+
+export default ConfirmationVeteranInformation;
