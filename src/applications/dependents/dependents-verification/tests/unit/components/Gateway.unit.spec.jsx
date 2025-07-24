4f89959ae8c55536d3330c510e301e8525311bbf
Midge-dev <sean.midgley.dev@gmail.com>
2025-07-24 16:44:48 -0700

    WIP

--- src/applications/dependents/dependents-verification/tests/unit/components/Gateway.unit.spec.jsx
+++ src/applications/dependents/dependents-verification/tests/unit/components/Gateway.unit.spec.jsx
@@ -1,1 +1,64 @@
-// Tests go here
+import React from 'react';
+import { renderInReduxProvider } from 'platform/testing/unit/react-testing-library-helpers';
+import { expect } from 'chai';
+
+import formConfig from '../../../config/form';
+import Gateway from '../../../components/Gateway';
+
+describe('Gateway', () => {
+  const mockStore = ({
+    loggedIn = true,
+    isVerified = true,
+    dependents = { loading: true, data: [] },
+  } = {}) => ({
+    user: {
+      login: {
+        currentlyLoggedIn: loggedIn,
+      },
+      profile: {
+        signIn: { serviceName: 'idme' },
+        loa: { current: isVerified ? 3 : 1 },
+      },
+    },
+    dependents,
+  });
+
+  it('should render `VerifyAlert` if user is not-verified', () => {
+    const { container } = renderInReduxProvider(
+      <Gateway
+        top
+        route={{ formConfig, pageList: [{ path: '/introduction' }] }}
+      />,
+      {
+        initialState: mockStore({ isVerified: false }),
+      },
+    );
+
+    expect(container.querySelector('va-alert-sign-in')).to.not.be.null;
+  });
+
+  it('should not render `VerifyAlert` when top prop is false', () => {
+    const { container } = renderInReduxProvider(
+      <Gateway route={{ formConfig, pageList: [{ path: '/introduction' }] }} />,
+      {
+        initialState: mockStore({ isVerified: false }),
+      },
+    );
+
+    expect(container.querySelector('va-alert-sign-in')).to.be.null;
+  });
+
+  it('should render the `va-loading-indicator` if not authenticated', () => {
+    const { container } = renderInReduxProvider(
+      <Gateway
+        top
+        route={{ formConfig, pageList: [{ path: '/introduction' }] }}
+      />,
+      {
+        initialState: mockStore({ loggedIn: true }),
+      },
+    );
+
+    expect(container.querySelector('va-loading-indicator')).to.not.be.null;
+  });
+});
