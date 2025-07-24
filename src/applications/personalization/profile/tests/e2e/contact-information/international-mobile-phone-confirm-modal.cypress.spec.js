53761e5471993eac5d40fb62a80044fec43bc506
Adam Suharja <adam.suharja@oddball.io>
2025-07-23 20:28:15 -0700

    Test comments

--- src/applications/personalization/profile/tests/e2e/contact-information/international-mobile-phone-confirm-modal.cypress.spec.js
+++ src/applications/personalization/profile/tests/e2e/contact-information/international-mobile-phone-confirm-modal.cypress.spec.js
@@ -9,6 +9,8 @@ describe('International mobile phone number confirm modal', () => {
 
     cy.login(user36);
 
+    // Mock the intl mobile phone update transaction and follow-up requests
+
     cy.intercept('PUT', '/v0/profile/telephones', {
       statusCode: 200,
       body: {
@@ -59,6 +61,7 @@ describe('International mobile phone number confirm modal', () => {
       'v0/profile/direct_deposits',
     ]);
 
+    // Enable the intl feature
     mockFeatureToggles(() => ({
       data: {
         type: 'feature_toggles',
@@ -79,12 +82,16 @@ describe('International mobile phone number confirm modal', () => {
 
     cy.get('va-loading-indicator').should('not.exist');
 
+    // Edit the mobile phone number
+
     cy.get('#edit-mobile-phone-number').click();
 
     cy.get('va-telephone-input[label="Mobile phone number"]').should('exist');
     cy.get('va-telephone-input[label="Mobile phone number"]').scrollIntoView();
 
     cy.get('va-combo-box').should('exist');
+
+    // Interact with the country code combobox dropdown and number input
 
     cy.get('button.usa-combo-box__toggle-list').click();
 
@@ -96,12 +103,14 @@ describe('International mobile phone number confirm modal', () => {
       .get('#inputField')
       .type('201234567');
 
+    // Trigger save
     cy.findByTestId('save-edit-button').click();
 
+    // Proceed through the confirmation modal
     cy.findByTestId('confirm-international-mobile-save-modal').should('exist');
-
     cy.contains('button', 'Save the number you entered').click();
 
+    // Update was good
     cy.get('va-alert[status="success"]').should('exist');
   });
 });
