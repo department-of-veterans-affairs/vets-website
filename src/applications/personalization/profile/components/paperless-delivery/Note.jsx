8c77cbea55032e1802c4795c94fe833e2e38163f
Andrés Rivera Toro <36863582+andresriveratoro@users.noreply.github.com>
2025-07-24 10:32:06 -0400

    add bold around text

--- src/applications/personalization/profile/components/paperless-delivery/Note.jsx
+++ src/applications/personalization/profile/components/paperless-delivery/Note.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 export const Note = () => (
   <p>
-    Note: We have limited paperless delivery options available at this time, but
-    more will be added in the future as we continue to expand this service. If
-    you’d like to enroll in additional paperless delivery options, please
-    contact your Benefits Office for assistance.
+    <span className="vads-u-font-weight--bold">Note:</span> We have limited
+    paperless delivery options available at this time, but more will be added in
+    the future as we continue to expand this service. If you’d like to enroll in
+    additional paperless delivery options, please contact your Benefits Office
+    for assistance.
   </p>
 );
