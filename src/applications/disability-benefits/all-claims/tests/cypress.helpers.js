a6677b7730f75505a4035a23d426d1e121a22ff9
Tommasina Miller <144388524+tommasina-va@users.noreply.github.com>
2025-07-24 16:11:29 -0500

    Remove unneeded path check

--- src/applications/disability-benefits/all-claims/tests/cypress.helpers.js
+++ src/applications/disability-benefits/all-claims/tests/cypress.helpers.js
@@ -466,8 +466,6 @@ export const pageHooks = (cy, testOptions) => ({
             // Should have moved past all follow-up pages
             cy.url().should('not.match', /new-disabilities\/follow-up/);
           }
-          // Verify we've completed all follow-up pages and moved to the next section
-          cy.url().should('include', 'toxic-exposure/conditions');
         }
       });
     });
