673a9bc107f27bc46199e71e6b67bee84e7b584d
Midge-dev <sean.midgley.dev@gmail.com>
2025-07-24 12:34:09 -0700

    WIP

--- src/applications/dependents/dependents-verification/tests/e2e/0538-dependents-verification.cypress.spec.js
+++ src/applications/dependents/dependents-verification/tests/e2e/0538-dependents-verification.cypress.spec.js
@@ -4,29 +4,40 @@ import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-test
 import formConfig from '../../config/form';
 import manifest from '../../manifest.json';
 import user from './user.json';
+import mockDependents from './fixtures/mocks/mock-dependents.json';
+import maximalTestData from './fixtures/data/maximal-test.json';
 
 Cypress.config('waitForAnimations', true);
+
+const FORM_ID = '21-0538';
 
 const testConfig = createTestConfig(
   {
     dataPrefix: 'data',
     dataDir: path.join(__dirname, 'fixtures', 'data'),
-    dataSets: ['minimal-test'],
+    dataSets: ['maximal-test'],
 
     setupPerTest: () => {
+      cy.intercept('GET', '/v0/dependents_applications/show', mockDependents);
       cy.intercept('GET', '/v0/feature_toggles?*', {
         data: {
           type: 'feature_toggles',
-          features: [
-            {
-              name: 'vaDependentsVerification',
-              value: true,
-            },
-          ],
+          features: [{ name: 'vaDependentsVerification', value: true }],
         },
       });
       cy.intercept('GET', '/v0/user', user);
-      cy.intercept('POST', formConfig.submitUrl, { status: 200 });
+
+      cy.intercept('GET', `/v0/in_progress_forms/${FORM_ID}`, {
+        body: {
+          formData: maximalTestData,
+          metadata: {},
+        },
+      });
+
+      cy.intercept('POST', '/dependents_verification/v0/claims', {
+        statusCode: 200,
+        body: {},
+      });
       cy.login(user);
     },
 
@@ -34,6 +45,20 @@ const testConfig = createTestConfig(
       introduction: ({ afterHook }) => {
         afterHook(() => {
           cy.clickStartForm();
+        });
+      },
+
+      'veteran-information': ({ afterHook }) => {
+        afterHook(() => {
+          cy.clickFormContinue();
+        });
+      },
+
+      // eslint-disable-next-line prettier/prettier
+      'dependents': ({ afterHook }) => {
+        afterHook(() => {
+          cy.get('va-radio-option[value="Y"]').click();
+          cy.clickFormContinue();
         });
       },
 
@@ -53,8 +78,7 @@ const testConfig = createTestConfig(
         });
       },
     },
-    // Skip tests in CI until the form is released.
-    // Remove this setting when the form has a content page in production.
+
     skip: Cypress.env('CI'),
   },
   manifest,
