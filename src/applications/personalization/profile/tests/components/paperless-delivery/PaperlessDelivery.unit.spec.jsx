d717874adae0c52206e9996186c0190816ec4c4b
Andrés Rivera Toro <36863582+andresriveratoro@users.noreply.github.com>
2025-07-24 11:01:17 -0400

    add tests

---
+++ src/applications/personalization/profile/tests/components/paperless-delivery/PaperlessDelivery.unit.spec.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { cleanup, render, screen } from '@testing-library/react';
+import { expect } from 'chai';
+import { PaperlessDelivery } from '../../../components/paperless-delivery/PaperlessDelivery';
+
+describe('PaperlessDelivery', () => {
+  beforeEach(() => {
+    render(<PaperlessDelivery />);
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('should render the heading', () => {
+    const heading = screen.getByRole('heading', { level: 1 });
+    expect(heading).to.exist;
+    expect(heading).to.have.text('Paperless delivery');
+  });
+
+  it('should render the description', () => {
+    expect(
+      screen.getByText(
+        /When you sign up, you’ll start receiving fewer documents by mail/,
+      ),
+    );
+  });
+
+  it('should render the note', () => {
+    expect(screen.getByText(/enroll in additional paperless delivery options/));
+  });
+});
