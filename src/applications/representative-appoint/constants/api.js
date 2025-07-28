300a059b03c31fb00b457a0214af0723f510cced
Josh Fike <josh.fike@oddball.io>
2025-07-28 15:54:31 -0500

    Don't use the new endpoint in cypress tests
    The new endpoint doesn't have data yet and the test will fail if it is
    used.

--- src/applications/representative-appoint/constants/api.js
+++ src/applications/representative-appoint/constants/api.js
@@ -11,9 +11,16 @@ const isLocalOrStaging =
   (useStagingDataLocally && environment.BASE_URL === 'http://localhost:3001') ||
   new URL(baseUrl).host === 'staging-api.va.gov';
 
-export const REPRESENTATIVES_API = isLocalOrStaging
-  ? '/representation_management/v0/accredited_entities_for_appoint' // Accreditation API data endpoint
-  : '/representation_management/v0/original_entities';
+export const REPRESENTATIVES_API = () => {
+  // When we were testing with isCypressTest, this value needs to be set to the legacy endpoint.
+  // At the time of writing the Accreditation API had no data for VSO Representatives.
+  const isCypressTest = typeof Cypress !== 'undefined';
+  if (isCypressTest) return '/representation_management/v0/original_entities';
+
+  if (isLocalOrStaging)
+    return '/representation_management/v0/accredited_entities_for_appoint'; // Accreditation API data endpoint
+  return '/representation_management/v0/original_entities';
+};
 
 export const NEXT_STEPS_EMAIL_API =
   '/representation_management/v0/next_steps_email';
