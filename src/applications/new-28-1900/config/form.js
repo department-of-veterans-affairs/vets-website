1182fbd250ef91a40b4beed0cf305536d6216d43
John Rodriguez <146031450+john-rodriguez-adhocteam@users.noreply.github.com>
2025-07-30 15:36:42 -0400

    [VA-IIR 1484] fix test error

--- src/applications/new-28-1900/config/form.js
+++ src/applications/new-28-1900/config/form.js
@@ -40,7 +40,7 @@ const formConfig = {
     showNavLinks: true,
     collapsibleNavLinks: true,
   },
-  formId: VA_FORM_IDS.FORM_28_1900_V2,
+  formId: VA_FORM_IDS.FORM_28_1900,
   saveInProgress: {
     messages: {
       inProgress:
