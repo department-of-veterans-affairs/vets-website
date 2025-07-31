d8e33051fffe9178fe2434992331fb910130bd56
Midge-dev <sean.midgley.dev@gmail.com>
2025-07-31 14:55:19 -0700

    WIP

--- src/applications/dependents/686c-674/tests/config/chapters/report-add-child/childAddressPartOne.unit.spec.jsx
+++ src/applications/dependents/686c-674/tests/config/chapters/report-add-child/childAddressPartOne.unit.spec.jsx
@@ -1,7 +1,7 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { expect } from 'chai';
-import React from 'react';
+import { expect, fireEvent, waitFor } from 'chai';
 import createCommonStore from '@department-of-veterans-affairs/platform-startup/store';
 import {
   DefinitionTester,
@@ -39,5 +39,41 @@ describe('686 add child child address part one', () => {
 
     const formDOM = getFormDOM(form);
     expect(formDOM.querySelectorAll('va-text-input').length).to.eq(6);
+  });
+
+  it('should render custom city error', async () => {
+    const { container } = render(
+      <Provider store={defaultStore}>
+        <DefinitionTester
+          schema={schema}
+          definitions={formConfig.defaultDefinitions}
+          uiSchema={uiSchema}
+          data={{
+            'view:selectable686Options': { addChild: true },
+            childrenToAdd: [
+              {
+                doesChildLiveWithYou: false,
+                address: { city: 'APO', isMilitary: false },
+              },
+            ],
+          }}
+          arrayPath="childrenToAdd"
+          pagePerItemIndex={0}
+        />
+      </Provider>,
+    );
+
+    const form = container?.querySelector('form');
+    fireEvent.submit(form);
+
+    await waitFor(() => {
+      const cityInput = container.querySelector(
+        'va-text-input[name*="_address_city"]',
+      );
+      expect(cityInput).to.exist;
+      expect(cityInput.getAttribute('error')).to.equal(
+        'Enter a valid city name',
+      );
+    });
   });
 });
