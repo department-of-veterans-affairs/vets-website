930f6daefb8c13a33c1c6e11b5c24f75a65e44c8
Scott Regenthal <50811220+freeheeling@users.noreply.github.com>
2025-08-01 14:01:46 -0600

    test(specifyOtherExposures): add date validation tests

--- src/applications/disability-benefits/all-claims/tests/pages/toxicExposure/specifyOtherExposures.unit.spec.jsx
+++ src/applications/disability-benefits/all-claims/tests/pages/toxicExposure/specifyOtherExposures.unit.spec.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
+import { format, subYears, addYears } from 'date-fns';
 import { DefinitionTester } from '@department-of-veterans-affairs/platform-testing/schemaform-utils';
 import { $ } from '@department-of-veterans-affairs/platform-forms-system/ui';
 import formConfig from '../../../config/form';
@@ -99,5 +100,223 @@ describe('Specify Other Exposures', () => {
       formData,
       true,
     );
+  });
+
+  describe('date validation', () => {
+    it('should accept current date for startDate', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = format(
+        new Date(),
+        'yyyy-MM-dd',
+      );
+      data.toxicExposure.specifyOtherExposures.endDate = format(
+        subYears(new Date(), 1),
+        'yyyy-MM-dd',
+      );
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        true,
+      );
+    });
+
+    it('should accept past date for startDate', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = format(
+        subYears(new Date(), 5),
+        'yyyy-MM-dd',
+      );
+      data.toxicExposure.specifyOtherExposures.endDate = format(
+        subYears(new Date(), 1),
+        'yyyy-MM-dd',
+      );
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        true,
+      );
+    });
+
+    it('should reject future date for startDate', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = format(
+        addYears(new Date(), 1),
+        'yyyy-MM-dd',
+      );
+      data.toxicExposure.specifyOtherExposures.endDate = format(
+        subYears(new Date(), 1),
+        'yyyy-MM-dd',
+      );
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        false,
+      );
+    });
+
+    it('should reject date before 1900 for startDate', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = '1899-12-31';
+      data.toxicExposure.specifyOtherExposures.endDate = format(
+        subYears(new Date(), 1),
+        'yyyy-MM-dd',
+      );
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        false,
+      );
+    });
+
+    it('should reject invalid date format for startDate', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = 'invalid-date';
+      data.toxicExposure.specifyOtherExposures.endDate = format(
+        subYears(new Date(), 1),
+        'yyyy-MM-dd',
+      );
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        false,
+      );
+    });
+
+    it('should accept current date for endDate', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = format(
+        subYears(new Date(), 2),
+        'yyyy-MM-dd',
+      );
+      data.toxicExposure.specifyOtherExposures.endDate = format(
+        new Date(),
+        'yyyy-MM-dd',
+      );
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        true,
+      );
+    });
+
+    it('should accept past date for endDate', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = format(
+        subYears(new Date(), 5),
+        'yyyy-MM-dd',
+      );
+      data.toxicExposure.specifyOtherExposures.endDate = format(
+        subYears(new Date(), 1),
+        'yyyy-MM-dd',
+      );
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        true,
+      );
+    });
+
+    it('should reject future date for endDate', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = format(
+        subYears(new Date(), 2),
+        'yyyy-MM-dd',
+      );
+      data.toxicExposure.specifyOtherExposures.endDate = format(
+        addYears(new Date(), 1),
+        'yyyy-MM-dd',
+      );
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        false,
+      );
+    });
+
+    it('should reject date before 1900 for endDate', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = format(
+        subYears(new Date(), 2),
+        'yyyy-MM-dd',
+      );
+      data.toxicExposure.specifyOtherExposures.endDate = '1899-12-31';
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        false,
+      );
+    });
+
+    it('should reject invalid date format for endDate', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = format(
+        subYears(new Date(), 2),
+        'yyyy-MM-dd',
+      );
+      data.toxicExposure.specifyOtherExposures.endDate = 'invalid-date';
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        false,
+      );
+    });
+
+    it('should accept valid date range (to after from)', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = format(
+        subYears(new Date(), 2),
+        'yyyy-MM-dd',
+      );
+      data.toxicExposure.specifyOtherExposures.endDate = format(
+        subYears(new Date(), 1),
+        'yyyy-MM-dd',
+      );
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        true,
+      );
+    });
+
+    it('should reject non-leap year February 29', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = '2021-02-29';
+      data.toxicExposure.specifyOtherExposures.endDate = format(
+        subYears(new Date(), 1),
+        'yyyy-MM-dd',
+      );
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        false,
+      );
+    });
+
+    it('should accept leap year February 29', () => {
+      const data = JSON.parse(JSON.stringify(formData));
+      data.toxicExposure.specifyOtherExposures.startDate = '2020-02-29';
+      data.toxicExposure.specifyOtherExposures.endDate = format(
+        subYears(new Date(), 1),
+        'yyyy-MM-dd',
+      );
+
+      pageSubmitTest(
+        formConfig.chapters.disabilities.pages.specifyOtherExposures,
+        data,
+        true,
+      );
+    });
   });
 });
