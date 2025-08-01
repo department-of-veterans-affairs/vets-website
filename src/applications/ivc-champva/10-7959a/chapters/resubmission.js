044aa77c692bca0a8826890156c56ddd3de4a0c0
Matt Long <6738544+longmd@users.noreply.github.com>
2025-08-01 12:13:32 -0400

    Update for grammatical errors

--- src/applications/ivc-champva/10-7959a/chapters/resubmission.js
+++ src/applications/ivc-champva/10-7959a/chapters/resubmission.js
@@ -24,8 +24,8 @@ const additionalInfoDescription = (
   <div className="vads-u-margin-top--4">
     <p>
       <strong>For PDI numbers</strong> you don’t need to include the date in the
-      beginning of the PDI number. Enter the 2 letters and the all of the
-      numbers following it.
+      beginning of the PDI number. Enter the 2 letters and all of the numbers
+      following it.
     </p>
     <p>
       <strong>For control numbers</strong> include all of the numbers listed
@@ -62,8 +62,8 @@ const additionalInfoDescription = (
       <span>
         <p className="vads-u-margin-top--0">
           The control number is located on the CHAMPVA Explanation of Benefits
-          we mailed you. It is a 12-digit code or may begin with a the letter
-          “M” followed by an 11-digit code (example: M00001234567).
+          we mailed you. It is a 12-digit code or may begin with the letter “M”
+          followed by an 11-digit code (example: M00001234567).
         </p>
         <p className="vads-u-margin-bottom--0">
           If you can’t find the control number, call us at{' '}
