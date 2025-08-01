52366f0b1207c7f42979b3756d5063e4a42a1045
Midge-dev <sean.midgley.dev@gmail.com>
2025-08-01 10:57:52 -0700

    fix date validation

--- src/applications/dependents/686c-674/config/chapters/674/addStudentsArrayPages.js
+++ src/applications/dependents/686c-674/config/chapters/674/addStudentsArrayPages.js
@@ -583,7 +583,10 @@ export const studentTermDatesPage = {
             properties: {
               officialSchoolStartDate: currentOrPastDateSchema,
               expectedStudentStartDate: currentOrPastDateSchema,
-              expectedGraduationDate: currentOrPastDateSchema,
+              expectedGraduationDate: {
+                type: 'string',
+                pattern: '^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$',
+              },
             },
           },
         },
