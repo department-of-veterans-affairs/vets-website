b0ac3082d79797db15c66698e8c6937791575152
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-08-01 10:22:34 -0600

    Add tests around the intro page

---
+++ src/applications/accreditation/21a/tests/unit/pages/05-professional-affiliations-chapter/professionalAffiliations.unit.spec.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { expect } from 'chai';
+import { render } from '@testing-library/react';
+import ProfessionalAffiliationsIntro from '../../../../components/05-professional-affiliations-chapter/ProfessionalAffiliationsIntro';
+
+context('Intro page', () => {
+  it('renders the intro paragraph and all list items', () => {
+    const { getByText, getAllByRole } = render(
+      <ProfessionalAffiliationsIntro />,
+    );
+    expect(
+      getByText(
+        /Over the next few pages, we will ask you to provide information about any jurisdictions, agencies, or courts you are admitted to practice before. For each jurisdiction, agency, or court, you will need to provide the following information:/,
+      ),
+    ).to.exist;
+    const items = [
+      'Name of jurisdiction, agency, or court',
+      'Date of admission (month/year)',
+      'Membership or registration number',
+    ];
+    const listItems = getAllByRole('listitem');
+    expect(listItems.length).to.equal(items.length);
+    items.forEach((text, idx) => {
+      expect(listItems[idx].textContent).to.equal(text);
+    });
+  });
+});
