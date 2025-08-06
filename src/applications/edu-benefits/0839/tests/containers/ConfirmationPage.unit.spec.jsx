596798ea4317e87492d166bfda2a2997e0664e01
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-08-06 09:14:22 -0700

    fix failing unit test

--- src/applications/edu-benefits/0839/tests/containers/ConfirmationPage.unit.spec.jsx
+++ src/applications/edu-benefits/0839/tests/containers/ConfirmationPage.unit.spec.jsx
@@ -2,46 +2,57 @@ import React from 'react';
 import { expect } from 'chai';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { cleanup, render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import { createInitialState } from '@department-of-veterans-affairs/platform-forms-system/state/helpers';
+
 import formConfig from '../../config/form';
 import ConfirmationPage from '../../containers/ConfirmationPage';
 
 const mockStore = state => createStore(() => state);
 
-const initConfirmationPage = ({ formData } = {}) => {
-  const store = mockStore({
-    form: {
-      ...createInitialState(formConfig),
-      submission: {
-        response: {
-          confirmationNumber: '1234567890',
-        },
-        timestamp: new Date(),
-      },
-      data: formData,
-    },
-  });
+before(() => {
+  if (!global.scrollTo) global.scrollTo = () => {};
+});
 
-  return render(
-    <Provider store={store}>
+const getPage = submission =>
+  render(
+    <Provider
+      store={mockStore({
+        form: {
+          ...createInitialState(formConfig),
+          submission,
+        },
+      })}
+    >
       <ConfirmationPage route={{ formConfig }} />
     </Provider>,
   );
-};
 
 describe('ConfirmationPage', () => {
-  afterEach(() => {
-    cleanup();
+  afterEach(cleanup);
+
+  it('shows success alert, heading, and confirmation number', () => {
+    const { container, getByText } = getPage({
+      response: { confirmationNumber: '1234567890' },
+      timestamp: new Date().toISOString(),
+    });
+
+    expect(container.querySelector('va-alert')).to.have.attribute(
+      'status',
+      'success',
+    );
+    getByText(/Form submission started/i);
+    getByText(/1234567890/);
   });
 
-  it('should show success alert, h2, and confirmation number if present', () => {
-    const { container } = initConfirmationPage();
-    const alert = container.querySelector('va-alert');
-    expect(alert).to.have.attribute('status', 'success');
-    expect(alert.querySelector('h2')).to.contain.text(
-      'Form submission started',
+  it('renders safely when submission object is empty (defaults kick in)', () => {
+    const { container, queryByText } = getPage({});
+
+    expect(container.querySelector('va-alert')).to.have.attribute(
+      'status',
+      'success',
     );
-    expect(alert).to.contain.text('Your confirmation number is 1234567890');
+
+    expect(queryByText(/\d{6,}/)).to.be.null;
   });
 });
