6e90978efe922c39c348ac969bfaf5b98b74ea17
Jami Gibbs <jami.gibbs@adhocteam.us>
2025-08-07 12:13:22 -0500

    Revert "Update CSS-Library version and migrate usa-media-block classes (#38059)"
    This reverts commit fb2e1078c8b99ba2521e054a7d3bda81744e4e0e.

--- src/platform/site-wide/header/components/OfficialGovtWebsite/index.js
+++ src/platform/site-wide/header/components/OfficialGovtWebsite/index.js
@@ -24,69 +24,47 @@ export const OfficialGovtWebsite = () => {
           className="header-us-flag vads-u-margin-right--1"
           src="https://www.va.gov/img/tiny-usa-flag.png"
         />
-        <button
+        <va-button
           aria-controls="official-govt-site-explanation"
           aria-expanded={expanded ? 'true' : 'false'}
           className="expand-official-govt-explanation va-button-link vads-u-text-decoration--none vads-u-display--flex vads-u-align-items--center"
+          text="An official website of the United States government."
           onClick={onToggle}
-        >
-          An official website of the United States government.
-          {/* down caret icon */}
-          {/* Convert to va-icon when injected header/footer split is in prod: https://github.com/department-of-veterans-affairs/vets-website/pull/27590 */}
-          <svg
-            aria-hidden="true"
-            className={`vads-u-margin-left--0p5${
-              expanded ? ' govt-expanded-arrow' : ''
-            }`}
-            focusable="false"
-            width="12"
-            viewBox="6 6 12 12"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill="#005ea2"
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M16.59 8.59L12 13.17L7.41 8.59L6 10L12 16L18 10L16.59 8.59Z"
-            />
-          </svg>
-        </button>
+        />
       </div>
       {/* Expanded section */}
       {expanded && (
         <div
           aria-hidden={expanded ? 'false' : 'true'}
-          className="usa-banner-content vads-grid-container usa-accordion-content vads-u-background-color--gray-lightest"
+          className="vads-u-background-color--gray-lightest vads-u-display--flex vads-u-flex-direction--column vads-u-padding--1p5 vads-u-padding-y--2"
           id="official-govt-site-explanation"
         >
-          <div className="vads-grid-row">
-            <div className="usa-media-block desktop-lg:vads-grid-col-6">
-              <img
-                alt="Dot gov"
-                className="usa-banner-icon usa-media_block-img"
-                src="https://www.va.gov/img/icon-dot-gov.svg"
-              />
-              <p className="usa-media-block__body vads-u-margin-top--0">
-                <strong>The .gov means it’s official.</strong>
-                <br />
-                Federal government websites often end in .gov or .mil. Before
-                sharing sensitive information, make sure you’re on a federal
-                government site.
-              </p>
-            </div>
-            <div className="usa-media-block desktop-lg:vads-grid-col-6">
-              <img
-                alt="SSL"
-                className="usa-banner-icon usa-media_block-img"
-                src="https://www.va.gov/img/icon-https.svg"
-              />
-              <p className="usa-media-block__body vads-u-margin-top--0">
-                <strong>The site is secure.</strong>
-                <br /> The <strong>https://</strong> ensures that you’re
-                connecting to the official website and that any information you
-                provide is encrypted and sent securely.
-              </p>
-            </div>
+          <div className="vads-u-display--flex vads-u-flex-direction--row vads-u-align-items--flex-start">
+            <img
+              alt="Dot gov"
+              className="usa-banner-icon usa-media_block-img"
+              src="https://www.va.gov/img/icon-dot-gov.svg"
+            />
+            <p className="vads-u-margin-top--0">
+              <strong>The .gov means it’s official.</strong>
+              <br />
+              Federal government websites often end in .gov or .mil. Before
+              sharing sensitive information, make sure you’re on a federal
+              government site.
+            </p>
+          </div>
+          <div className="vads-u-display--flex vads-u-flex-direction--row vads-u-align-items--flex-start vads-u-margin-top--2">
+            <img
+              alt="SSL"
+              className="usa-banner-icon usa-media_block-img"
+              src="https://www.va.gov/img/icon-https.svg"
+            />
+            <p className="vads-u-margin-top--0">
+              <strong>The site is secure.</strong>
+              <br /> The <strong>https://</strong> ensures that you’re
+              connecting to the official website and that any information you
+              provide is encrypted and sent securely.
+            </p>
           </div>
         </div>
       )}
