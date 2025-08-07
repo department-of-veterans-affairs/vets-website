4a8564d16a4eec5ce8c0de852af02882815ab8d6
Alex Morgun <87077843+alex-morgun-1@users.noreply.github.com>
2025-08-07 14:45:49 -0400

    correct interstital page button label

--- src/applications/mhv-secure-messaging/containers/InterstitialPage.jsx
+++ src/applications/mhv-secure-messaging/containers/InterstitialPage.jsx
@@ -52,13 +52,15 @@ const InterstitialPage = props => {
           reply.
         </p>
 
-        <va-button
+        <button
           className="continue-button vads-u-padding-y--1p5 vads-u-padding-x--2p5 vads-u-margin-top--0 vads-u-margin-bottom--3"
-          text=". Page content will change."
           data-testid="continue-button"
           onClick={handleContinueButton}
           data-dd-action-name={`${continueButtonText} button on Interstitial Page`}
-        />
+        >
+          {continueButtonText}
+          <span className="sr-only">. Page content will change.</span>
+        </button>
 
         <h2 className="vads-u-font-size--h3 vads-u-margin-top--0 vads-u-margin-bottom--2">
           If you need help sooner, use one of these urgent communications
