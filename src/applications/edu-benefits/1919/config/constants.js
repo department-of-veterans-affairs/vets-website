b8671ac36103e067921f61518f34514e77298e51
govcioChad <162637891+govcioChad@users.noreply.github.com>
2025-08-08 14:25:31 -0700

    #vebt-1360 #comment Remooved hard code submit url to staging

--- src/applications/edu-benefits/1919/config/constants.js
+++ src/applications/edu-benefits/1919/config/constants.js
@@ -1,7 +1,5 @@
-// import environment from '~/platform/utilities/environment';
+import environment from '~/platform/utilities/environment';
 
-// export const SUBMIT_URL = `${
-//   environment.API_URL
-// }/v0/education_benefits_claims/1919`;
-
-export const SUBMIT_URL = `https://staging-api.va.gov/v0/education_benefits_claims/1919`;
+export const SUBMIT_URL = `${
+  environment.API_URL
+}/v0/education_benefits_claims/1919`;
--- src/applications/edu-benefits/1919/config/submitForm.js
+++ src/applications/edu-benefits/1919/config/submitForm.js
@@ -1,18 +1,7 @@
 import { submitToUrl } from 'platform/forms-system/src/js/actions';
-// import { transformForSubmit } from 'platform/forms-system/src/js/helpers';
 
 const submitForm = (form, formConfig) => {
   const body = formConfig.transformForSubmit(formConfig, form);
-  // console.log('form.data', form.data);
-  // const transformedData =  transformForSubmit(formConfig, { ...form, data: form.data });
-
-  //     const body = JSON.stringify({
-  //     educationBenefitsClaim: {
-  //       form: transformedData,
-  //     },
-  //   });
-
-  // console.log('body', body);
 
   return submitToUrl(
     body,
