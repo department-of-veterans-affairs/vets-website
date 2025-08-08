6ec20ab04f9d4823ffb2f98fb4b8c2d376236bc3
govcioChad <162637891+govcioChad@users.noreply.github.com>
2025-08-08 12:23:59 -0700

    #vebt-1360 #comment added clean up transform

--- src/applications/edu-benefits/1919/config/submit-transformer.js
+++ src/applications/edu-benefits/1919/config/submit-transformer.js
@@ -6,24 +6,35 @@ export function transform(formConfig, form) {
   const addressTransform = formData => {
     let clonedData = _.cloneDeep(formData);
     delete clonedData.institutionDetails.loader;
-    delete clonedData.statementOfTruthCertified;
 
     clonedData = {
       ...clonedData,
       institutionDetails: {
         ...clonedData.institutionDetails,
       },
+    };
+    return clonedData;
+  };
+
+  const cleanUpTransform = formData => {
+    let clonedData = _.cloneDeep(formData);
+    delete clonedData.statementOfTruthCertified;
+
+    clonedData = {
+      ...clonedData,
       dateSigned: dateSigned(),
     };
     return clonedData;
   };
+
   const usFormTransform = formData =>
     transformForSubmit(formConfig, { ...form, data: formData });
 
-  const transformedData = [addressTransform, usFormTransform].reduce(
-    (formData, transformer) => transformer(formData),
-    form.data,
-  );
+  const transformedData = [
+    addressTransform,
+    cleanUpTransform,
+    usFormTransform,
+  ].reduce((formData, transformer) => transformer(formData), form.data);
 
   return JSON.stringify({
     educationBenefitsClaim: {
