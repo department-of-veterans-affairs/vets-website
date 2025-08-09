d0a921d66953fbed9d2b0691613412942da77c9d
Ian Seabock <iseabock@MacBook-Pro.local>
2025-08-08 17:32:18 -0700

    114684[refactor] MHV-Medications - Refactor PDF Generator

--- src/applications/mhv-medications/containers/Prescriptions.jsx
+++ src/applications/mhv-medications/containers/Prescriptions.jsx
@@ -13,14 +13,12 @@ import PropTypes from 'prop-types';
 import {
   usePrintTitle,
   updatePageTitle,
-  reportGeneratedBy,
 } from '@department-of-veterans-affairs/mhv/exports';
 import { isAuthenticatedWithSSOe } from '~/platform/user/authentication/selectors';
 import MedicationsList from '../components/MedicationsList/MedicationsList';
 import MedicationsListSort from '../components/MedicationsList/MedicationsListSort';
 import {
   dateFormat,
-  generateMedicationsPDF,
   generateTextFile,
   getErrorTypeFromFormat,
 } from '../util/helpers';
@@ -66,6 +64,8 @@ import {
   setFilterOption,
   setPageNumber,
 } from '../redux/preferencesSlice';
+import { buildPdfData } from '../util/pdfData';
+import { generateMedicationsPdfFile } from '../util/pdfService';
 
 const Prescriptions = () => {
   const { search } = useLocation();
@@ -365,86 +365,6 @@ const Prescriptions = () => {
     [dispatch, selectedFilterOption],
   );
 
-  const pdfData = useCallback(
-    (rxList, allergiesList) => {
-      return {
-        subject: 'Full Medications List',
-        headerBanner: [
-          {
-            text:
-              'If you’re ever in crisis and need to talk with someone right away, call the Veterans Crisis Line at ',
-          },
-          {
-            text: '988',
-            weight: 'bold',
-          },
-          {
-            text: '. Then select 1.',
-          },
-        ],
-        headerLeft: userName.first
-          ? `${userName.last}, ${userName.first}`
-          : `${userName.last || ' '}`,
-        headerRight: `Date of birth: ${dateFormat(dob, 'MMMM D, YYYY')}`,
-        footerLeft: reportGeneratedBy,
-        footerRight: 'Page %PAGE_NUMBER% of %TOTAL_PAGES%',
-        title: 'Medications',
-        preface: [
-          {
-            value: `This is a list of prescriptions and other medications in your VA medical records. When you download medication records, we also include a list of allergies and reactions in your VA medical records.`,
-          },
-        ],
-        results: [
-          {
-            header: 'Medications list',
-            preface: `Showing ${
-              rxList?.length
-            } medications, ${rxListSortingOptions[
-              selectedSortOption
-            ].LABEL.toLowerCase()}`,
-            list: rxList,
-          },
-          {
-            header: 'Allergies',
-            ...(allergiesList &&
-              allergiesList.length > 0 && {
-                preface: [
-                  {
-                    value:
-                      'This list includes all allergies, reactions, and side effects in your VA medical records. This includes medication side effects (also called adverse drug reactions). If you have allergies or reactions that are missing from this list, tell your care team at your next appointment.',
-                  },
-                  {
-                    value: `Showing ${
-                      allergiesList.length
-                    } records from newest to oldest`,
-                  },
-                ],
-              }),
-            list: allergiesList || [],
-            ...(allergiesList &&
-              !allergiesList.length && {
-                preface:
-                  'There are no allergies or reactions in your VA medical records. If you have allergies or reactions that are missing from your records, tell your care team at your next appointment.',
-              }),
-            ...(!allergiesList && {
-              preface: [
-                {
-                  value:
-                    'We couldn’t access your allergy records when you downloaded this list. We’re sorry. There was a problem with our system. Try again later.',
-                },
-                {
-                  value:
-                    'If it still doesn’t work, call us at 877-327-0022 (TTY: 711). We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.',
-                },
-              ],
-            }),
-          },
-        ],
-      };
-    },
-    [userName, dob, selectedSortOption, selectedFilterOption],
-  );
-
   const txtData = useCallback(
     (rxList, allergiesList) => {
       return (
@@ -472,18 +392,18 @@ const Prescriptions = () => {
   );
 
   const generatePDF = useCallback(
-    (rxList, allergiesList) => {
-      generateMedicationsPDF(
-        'medications',
-        `VA-medications-list-${
-          userName.first ? `${userName.first}-${userName.last}` : userName.last
-        }-${dateFormat(Date.now(), 'M-D-YYYY_hmmssa').replace(/\./g, '')}`,
-        pdfData(rxList, allergiesList),
-      ).then(() => {
-        setPdfTxtGenerateStatus({ status: PDF_TXT_GENERATE_STATUS.Success });
+    async (rxList, allergiesList) => {
+      const pdfDataObj = buildPdfData({
+        userName,
+        dob,
+        selectedSortOption,
+        rxList,
+        allergiesList,
       });
+      await generateMedicationsPdfFile({ userName, pdfData: pdfDataObj });
+      setPdfTxtGenerateStatus({ status: PDF_TXT_GENERATE_STATUS.Success });
     },
-    [userName, pdfData, setPdfTxtGenerateStatus],
+    [userName, dob, selectedSortOption, setPdfTxtGenerateStatus],
   );
 
   const generateTXT = useCallback(
