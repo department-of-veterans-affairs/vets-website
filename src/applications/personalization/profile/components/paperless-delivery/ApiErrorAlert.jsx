0cb8bffb208c1997669be13c842e588a81b80e29
Andrés Rivera Toro <36863582+andresriveratoro@users.noreply.github.com>
2025-08-08 22:54:53 -0400

    add role directly to component

--- src/applications/personalization/profile/components/paperless-delivery/ApiErrorAlert.jsx
+++ src/applications/personalization/profile/components/paperless-delivery/ApiErrorAlert.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { VaAlert } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 
 export const ApiErrorAlert = () => (
-  <div role="alert">
-    <VaAlert status="warning" visible>
-      <h2 slot="headline">This page isn’t available right now</h2>
-      <p className="vads-u-margin-y--0">
-        We’re sorry. Something went wrong on our end. Refresh this page or try
-        again later.
-      </p>
-    </VaAlert>
-  </div>
+  <VaAlert role="alert" status="warning" visible>
+    <h2 slot="headline">This page isn’t available right now</h2>
+    <p className="vads-u-margin-y--0">
+      We’re sorry. Something went wrong on our end. Refresh this page or try
+      again later.
+    </p>
+  </VaAlert>
 );
--- src/applications/personalization/profile/components/paperless-delivery/CheckboxAlert.jsx
+++ src/applications/personalization/profile/components/paperless-delivery/CheckboxAlert.jsx
@@ -5,24 +5,20 @@ import PropTypes from 'prop-types';
 export const CheckboxAlert = ({ error, success }) => {
   if (error) {
     return (
-      <div role="alert">
-        <VaAlert slim status="error" visible>
-          <p className="vads-u-margin-y--0">
-            We’re sorry. We can’t update your information right now. We’re
-            working to fix this problem. Try again later.
-          </p>
-        </VaAlert>
-      </div>
+      <VaAlert role="alert" slim status="error" visible>
+        <p className="vads-u-margin-y--0">
+          We’re sorry. We can’t update your information right now. We’re working
+          to fix this problem. Try again later.
+        </p>
+      </VaAlert>
     );
   }
 
   if (success) {
     return (
-      <div role="alert">
-        <VaAlert slim status="success" visible>
-          <p className="vads-u-margin-y--0">Update saved</p>
-        </VaAlert>
-      </div>
+      <VaAlert role="alert" slim status="success" visible>
+        <p className="vads-u-margin-y--0">Update saved</p>
+      </VaAlert>
     );
   }
 
