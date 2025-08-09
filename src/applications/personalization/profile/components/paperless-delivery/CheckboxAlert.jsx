fc37c199f82a9ed9ff1868cfc9072481cb9e979f
Andrés Rivera Toro <36863582+andresriveratoro@users.noreply.github.com>
2025-08-08 22:24:58 -0400

    add alert roles

--- src/applications/personalization/profile/components/paperless-delivery/CheckboxAlert.jsx
+++ src/applications/personalization/profile/components/paperless-delivery/CheckboxAlert.jsx
@@ -5,20 +5,24 @@ import PropTypes from 'prop-types';
 export const CheckboxAlert = ({ error, success }) => {
   if (error) {
     return (
-      <VaAlert data-role="alert" slim status="error" visible>
-        <p className="vads-u-margin-y--0">
-          We’re sorry. We can’t update your information right now. We’re working
-          to fix this problem. Try again later.
-        </p>
-      </VaAlert>
+      <div role="alert">
+        <VaAlert slim status="error" visible>
+          <p className="vads-u-margin-y--0">
+            We’re sorry. We can’t update your information right now. We’re
+            working to fix this problem. Try again later.
+          </p>
+        </VaAlert>
+      </div>
     );
   }
 
   if (success) {
     return (
-      <VaAlert data-role="alert" slim status="success" visible>
-        <p className="vads-u-margin-y--0">Update saved</p>
-      </VaAlert>
+      <div role="alert">
+        <VaAlert slim status="success" visible>
+          <p className="vads-u-margin-y--0">Update saved</p>
+        </VaAlert>
+      </div>
     );
   }
 
--- src/applications/personalization/profile/components/paperless-delivery/NotEnrolledAlert.jsx
+++ src/applications/personalization/profile/components/paperless-delivery/NotEnrolledAlert.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { VaAlert } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 
 export const NotEnrolledAlert = () => (
-  <VaAlert status="info" visible>
-    <h2 slot="headline">Paperless delivery not available yet</h2>
-    <p className="vads-u-margin-y--0">
-      You’re not enrolled in any VA benefits that offer paperless delivery
-      options.
-    </p>
-  </VaAlert>
+  <div role="alert">
+    <VaAlert status="info" visible>
+      <h2 slot="headline">Paperless delivery not available yet</h2>
+      <p className="vads-u-margin-y--0">
+        You’re not enrolled in any VA benefits that offer paperless delivery
+        options.
+      </p>
+    </VaAlert>
+  </div>
 );
