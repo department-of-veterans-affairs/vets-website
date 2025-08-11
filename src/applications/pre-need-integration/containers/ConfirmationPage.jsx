92e6504da72361c35e1a388b586b6fda14f206bd
122034971+ConnorJeff@users.noreply.github.com <122034971+ConnorJeff@users.noreply.github.com>
2025-08-11 15:52:00 -0400

    MBMS-92613 Pre need integration confirmation page initial commit

--- src/applications/pre-need-integration/containers/ConfirmationPage.jsx
+++ src/applications/pre-need-integration/containers/ConfirmationPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import moment from 'moment';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { scrollToTop } from 'platform/utilities/scroll';
 import { focusElement } from 'platform/utilities/ui';
+import { ConfirmationView } from 'platform/forms-system/src/js/components/ConfirmationView';
 
 class ConfirmationPage extends React.Component {
   componentDidMount() {
@@ -12,105 +13,52 @@ class ConfirmationPage extends React.Component {
   }
 
   render() {
-    const { form } = this.props;
-    const { name } = form.data.application.claimant;
-    const submittedAt = moment(form.submission.submittedAt);
+    const { form, route } = this.props;
+    const { formConfig } = route;
+    const { submission } = form;
+    const { response } = submission || {};
+
+    const submitDate = new Date(form.submission.submittedAt);
+    const confirmationNumber = response?.confirmationNumber || '';
 
     return (
-      <div>
-        <h2 className="confirmation-page-title">
-          You’ve submitted your application
-        </h2>
-        <p>
-          You’ll receive a confirmation email shortly. We’ll let you know by
-          mail or phone if we need more details.
-          <br />
-          <em>Please print this page for your records.</em>
-        </p>
-
-        <div className="inset">
-          <h3 className="schemaform-confirmation-claim-header">
-            Your application details
-          </h3>
-          <ul className="claim-list">
-            <li>
-              <strong>Applicant Name</strong>
-              <div>
-                {name.first} {name.middle} {name.last} {name.suffix}
-              </div>
-            </li>
-            <li>
-              <strong>Form name</strong>
-              <br />
-              <div>Burial Pre-Need Claim (Form 40-10007)</div>
-            </li>
-
-            <li>
-              <strong>Date submitted</strong>
-              <br />
-              <span>{submittedAt.format('MMM D, YYYY')}</span>
-            </li>
-          </ul>
-
-          <va-button
-            class="screen-only"
-            onClick={window.print}
-            text="Print this page"
-          />
-        </div>
-        <h4>Do you have more documents you need to submit?</h4>
-        <p className="mail-or-fax-message">
-          To mail or fax additional documents:
-        </p>
-        <ol className="mail-or-fax-steps">
-          <li className="mail-or-fax-step">Make copies of the documents.</li>
-          <li className="mail-or-fax-step">
-            Make sure you write your name and confirmation number on every page.
-          </li>
-          <li className="mail-or-fax-step">
-            <span>
-              Submit application and supporting documents to the VA by mail:
-            </span>
-            <p>
-              <div className="mail-fax-address">
-                <div>NCA Intake Center</div>
-                <div>P.O. Box 5237</div>
-                <div>Janesville, WI 53547</div>
-              </div>
-            </p>
-            <strong>Or</strong>
-            <p>
-              Fax them to the National Cemetery Scheduling Office:{' '}
-              <va-telephone contact="8558408299" />.
-            </p>
-          </li>
-        </ol>
-        <div className="confirmation-guidance-container">
-          <h4 className="confirmation-guidance-heading">
-            What happens after I apply?
-          </h4>
-          <a
-            className="confirmation-guidance-message"
-            href="/burials-memorials/pre-need-eligibility/after-you-apply/"
-          >
-            Find out what happens after you apply
-          </a>
-        </div>
-        <p>
-          <div className="row form-progress-buttons schemaform-back-buttons">
-            <div className="small-6 usa-width-one-half medium-6 columns">
-              <a href="/">
-                <button className="usa-button-primary">
-                  Go back to VA.gov
-                </button>
-              </a>
-            </div>
-          </div>
-        </p>
-      </div>
+      <ConfirmationView
+        submitDate={submitDate}
+        confirmationNumber={confirmationNumber}
+        formConfig={formConfig}
+      >
+        <ConfirmationView.SubmissionAlert
+          title="Your pre-need determination request has been submitted"
+          content="We'll review your request and send you a letter in the mail with our decision."
+          actions={null}
+        />
+        <ConfirmationView.PrintThisPage />
+        <ConfirmationView.WhatsNextProcessList
+          item1Header="We'll review your request"
+          item1Content="We'll determine if you're eligible for burial in a VA national cemetery based on the information you provided."
+          item2Header="We'll send you our decision by mail"
+          item2Content="You should receive our decision within 60 days after we receive your request."
+        />
+        <ConfirmationView.HowToContact />
+        <ConfirmationView.GoBackLink />
+        <ConfirmationView.NeedHelp />
+      </ConfirmationView>
     );
   }
 }
+
+ConfirmationPage.propTypes = {
+  form: PropTypes.shape({
+    data: PropTypes.object,
+    submission: PropTypes.shape({
+      submittedAt: PropTypes.string,
+      response: PropTypes.object,
+    }),
+  }),
+  route: PropTypes.shape({
+    formConfig: PropTypes.object,
+  }),
+};
 
 function mapStateToProps(state) {
   return {
