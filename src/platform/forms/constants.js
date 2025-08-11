598e90d1906c02668ea1cbf7f085e66fc8008ddb
Nicholas Hibbits <172406954+nicholashibbits@users.noreply.github.com>
2025-08-11 08:38:53 -0700

    VEBT-2933 (#38137)
    * scaffold 0839
    
    * commit updated manifest
    
    * fix failing unit test
    
    * add 0839 to missingFromVetsJsonSchema
    
    * edit tracking prefix
    
    ---------
    
    Co-authored-by: Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>

--- src/platform/forms/constants.js
+++ src/platform/forms/constants.js
@@ -104,6 +104,7 @@ export const VA_FORM_IDS = Object.freeze({
   FORM_21_651_UPLOAD: '21-651-UPLOAD',
   FORM_21P_4185_UPLOAD: '21P-4185-UPLOAD',
   FORM_22_10297: '22-10297',
+  FORM_22_0839: '22-0839',
   FORM_22_10275: '22-10275',
 });
 
@@ -198,6 +199,7 @@ export const getAllFormLinks = getAppUrlImpl => {
       '0538-dependents-verification',
     )}/`,
     [VA_FORM_IDS.FORM_22_10297]: `${tryGetAppUrl('22-10297')}/`,
+    [VA_FORM_IDS.FORM_22_0839]: `${tryGetAppUrl('22-0839')}/`,
     [VA_FORM_IDS.FORM_22_10275]: `${tryGetAppUrl('22-10275')}/`,
   };
 };
@@ -738,6 +740,13 @@ export const MY_VA_SIP_FORMS = [
     title: '10297',
     description: 'education benefits',
     trackingPrefix: 'edu-10297',
+  },
+  {
+    id: VA_FORM_IDS.FORM_22_0839,
+    benefit: 'education benefits',
+    title: '0839',
+    description: 'education benefits',
+    trackingPrefix: '0839-edu-benefits',
   },
   {
     id: VA_FORM_IDS.FORM_22_10275,
