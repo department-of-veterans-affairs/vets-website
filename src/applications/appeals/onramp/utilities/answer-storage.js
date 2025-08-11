b2749279d1a0b17b901c83cc890156fdba3587f0
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-08-11 12:40:33 -0500

    114834 Onramp utilities (#38204)
    * DR 114834: Add utility functions to assist in navigation
    
    * Continuing to add tests for utilities

--- src/applications/appeals/onramp/utilities/answer-storage.js
+++ src/applications/appeals/onramp/utilities/answer-storage.js
@@ -28,3 +28,31 @@ export const setShortNameValue = (SHORT_NAME, state, value) => {
     },
   };
 };
+
+/** ================================================================
+ * Clear out responses in the store that are not needed for the current flow.
+ * Only run when current question's answer has been changed from previous;
+ * everything ahead of it is cleared out.
+ *
+ * @param {array} allQuestionShortNames - all question SHORT_NAMEs in the app
+ * @param {func} updateCleanedFormStore - Redux action to update the answers in the store
+ * @param {string} currentQuestionName - SHORT_NAME for question
+ */
+export const cleanUpAnswers = (
+  allQuestionShortNames,
+  updateCleanedFormStore,
+  currentQuestionName,
+) => {
+  const currentQuestionIndex = allQuestionShortNames.indexOf(
+    currentQuestionName,
+  );
+
+  const formUpdate = {};
+  const questionsAhead = allQuestionShortNames.slice(currentQuestionIndex + 1);
+
+  questionsAhead.forEach(question => {
+    formUpdate[question] = null;
+  });
+
+  updateCleanedFormStore(formUpdate);
+};
