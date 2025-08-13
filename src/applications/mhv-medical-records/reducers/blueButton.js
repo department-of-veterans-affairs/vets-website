6f4733e46421ca33809a8dd64de4f16a48e48207
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2025-08-13 10:54:22 -0500

    Handle Null Facilities edge case.

--- src/applications/mhv-medical-records/reducers/blueButton.js
+++ src/applications/mhv-medical-records/reducers/blueButton.js
@@ -270,19 +270,21 @@ export const convertAccountSummary = data => {
   const { facilities = [], ipas } = data;
 
   // Map facilities
-  const mappedFacilities = facilities
-    .filter(facility => facility?.facilityInfo?.treatment)
-    .map(facility => ({
-      facilityName: facility.facilityInfo?.name || 'Unknown facility',
-      stationNumber: facility.facilityInfo?.stationNumber || 'Unknown ID',
-      type: facility.facilityInfo?.treatment ? 'Treatment' : 'VAMC',
-    }));
+  const mappedFacilities = !facilities
+    ? []
+    : facilities
+        ?.filter(facility => facility?.facilityInfo?.treatment)
+        .map(facility => ({
+          facilityName: facility.facilityInfo?.name || 'Unknown facility',
+          stationNumber: facility.facilityInfo?.stationNumber || 'Unknown ID',
+          type: facility.facilityInfo?.treatment ? 'Treatment' : 'VAMC',
+        }));
 
   // Extract user profile details
   const ipa = ipas && ipas[0];
   const authenticatingFacility =
     ipa?.authenticatingFacilityId &&
-    facilities.find(
+    facilities?.find(
       facility => facility.facilityInfo?.id === ipa.authenticatingFacilityId,
     );
 
