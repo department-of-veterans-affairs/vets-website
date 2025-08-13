d90523605c5f9e58d81599fc5178e6ee52211e96
Michael Ghisilieri <175352110+mghisilieri@users.noreply.github.com>
2025-08-13 11:07:06 -0700

    22-1919 useFakeTimers moved to describe block

--- src/applications/edu-benefits/1919/tests/config/transform.unit.spec.jsx
+++ src/applications/edu-benefits/1919/tests/config/transform.unit.spec.jsx
@@ -5,14 +5,19 @@ import transformTestData from '../fixtures/data/transform-test-data.json';
 import transform from '../../config/transform';
 
 describe('22-1919 Transform Function', () => {
-  it('should transform the form data on the ui to match the json schema', () => {
-    // Mock Date.now() to always return a fixed value in 2024
-    const fixedTimestamp = new Date('2025-08-11T00:00:00Z').getTime();
-    const clock = sinon.useFakeTimers({
-      now: fixedTimestamp,
-      toFake: ['Date'],
-    });
+  let clock;
 
+  beforeEach(() => {
+    // Mock Date.now() to always return a fixed value in 2025
+    const fixedTimestamp = new Date('2025-08-11T00:00:00Z').getTime();
+    clock = sinon.useFakeTimers({ now: fixedTimestamp, toFake: ['Date'] });
+  });
+
+  afterEach(() => {
+    clock.restore();
+  });
+
+  it('should transform the form data on the ui to match the json schema', () => {
     let submitData = {};
 
     // Facility code provided for institution
@@ -34,7 +39,5 @@ describe('22-1919 Transform Function', () => {
     expect(JSON.parse(submitData.educationBenefitsClaim.form)).to.deep.equal(
       transformTestData.afterTransform2,
     );
-
-    clock.restore();
   });
 });
