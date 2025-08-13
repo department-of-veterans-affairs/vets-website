05edfdc2d233393c869bef26a951c90c0f2adac8
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-08-12 17:58:46 -0700

    finish initial setup

--- src/applications/edu-benefits/10297/config/submit-transformer.js
+++ src/applications/edu-benefits/10297/config/submit-transformer.js
@@ -1,7 +1,26 @@
-// import _ from 'lodash';
+import _ from 'lodash';
+import { transformForSubmit } from 'platform/forms-system/src/js/helpers';
 
 export function transform(formConfig, form) {
   // console.log({ formConfig, form });
+  const eligibilityTransform = formData => {
+    // Transfrom form data in state to match schema
 
-  return { formConfig, form };
+    return _.cloneDeep(formData);
+  };
+
+  //  is this function required?
+  const usFormTransform = formData =>
+    transformForSubmit(formConfig, { ...form, data: formData });
+
+  const transformedData = [eligibilityTransform, usFormTransform].reduce(
+    (formData, transformer) => transformer(formData),
+    form.data,
+  );
+
+  return JSON.stringify({
+    educationBenefitsClaim: {
+      form: transformedData,
+    },
+  });
 }
