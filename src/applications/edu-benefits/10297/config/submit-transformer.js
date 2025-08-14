94163e3a0e5b317900931e82d6bb11463495edf5
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-08-14 14:02:01 -0700

    start transform logic

--- src/applications/edu-benefits/10297/config/submit-transformer.js
+++ src/applications/edu-benefits/10297/config/submit-transformer.js
@@ -1,22 +1,50 @@
 import _ from 'lodash';
 import { transformForSubmit } from 'platform/forms-system/src/js/helpers';
 
+// create test object
 export function transform(formConfig, form) {
   // console.log({ formConfig, form });
-  const eligibilityTransform = formData => {
-    // Transfrom form data in state to match schema
 
-    return _.cloneDeep(formData);
+  const contactInfoTransform = formData => {
+    const clonedData = _.cloneDeep(formData);
+    let { homePhone, mobilePhone } = clonedData.contactInfo;
+
+    // Transfrom form data in state to match schema
+    homePhone = homePhone?.replace(/[^0-9]/g, '');
+    mobilePhone = mobilePhone?.replace(/[^0-9]/g, '');
+
+    return {
+      ...clonedData,
+      contactInfo: {
+        ...clonedData.contactInfo,
+        homePhone,
+        mobilePhone,
+      },
+    };
   };
 
-  //  is this function required?
+  const eligibilityTransform = formData => {
+    const clonedData = _.cloneDeep(formData);
+
+    return {
+      ...clonedData,
+    };
+  };
+
+  // is this function required? -- see transform in form 10203
   const usFormTransform = formData =>
     transformForSubmit(formConfig, { ...form, data: formData });
 
-  const transformedData = [eligibilityTransform, usFormTransform].reduce(
-    (formData, transformer) => transformer(formData),
-    form.data,
-  );
+  const transformedData = [
+    contactInfoTransform,
+    eligibilityTransform,
+    usFormTransform,
+  ].reduce((formData, transformer) => {
+    // console.log('PRETRANSFORM 🥌', formData.data);
+    return transformer(formData);
+  }, form.data);
+
+  // console.log({ transformedData });
 
   return JSON.stringify({
     educationBenefitsClaim: {
