3a50d26d7324f63267a19dce0e2c17e505c0365f
jquispe-oddball <jennifer.quispe@oddball.io>
2025-08-14 17:08:09 -0400

    113651 - adding rep filter

--- src/applications/accredited-representative-portal/components/SortForm.jsx
+++ src/applications/accredited-representative-portal/components/SortForm.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Form, useNavigate, useSearchParams } from 'react-router-dom';
-import { VaSelect } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import { SEARCH_PARAMS } from '../utilities/poaRequests';
+import {
+  VaSelect,
+  VaCheckbox,
+} from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import {
+  SEARCH_PARAMS,
+  addStyleToShadowDomOnPages,
+} from '../utilities/poaRequests';
 
 const SortForm = ({ options, defaults }) => {
+  useEffect(() => {
+    // Insert CSS to hide 'For example: January 19 2000' hint on memorable dates
+    // (can't be overridden by passing 'hint' to uiOptions):
+    addStyleToShadowDomOnPages(
+      [''],
+      ['va-checkbox'],
+      '.va-checkbox__container {margin: 0;}',
+    );
+  });
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const sortby = searchParams.get(SEARCH_PARAMS.SORTBY) || defaults.SORT_BY;
@@ -20,6 +35,17 @@ const SortForm = ({ options, defaults }) => {
     navigate(
       `?${statusLabel}sortOrder=${sortOrder}&sortBy=${sortBy}&pageNumber=${number}&pageSize=${size}`,
     );
+  };
+
+  const toggleRep = e => {
+    navigate(
+      `?status=${status}&sortOrder=${sort}&sortBy=${sortby}&pageNumber=${number}&pageSize=${size}&as_selected_individual=${
+        e.detail.checked
+      }`,
+    );
+  };
+  const isChecked = () => {
+    return searchParams.get(SEARCH_PARAMS.SELECTED_INDIVIDUAL);
   };
 
   return (
@@ -45,6 +71,13 @@ const SortForm = ({ options, defaults }) => {
           </option>
         ))}
       </VaSelect>
+      <VaCheckbox
+        class="poa-request__checkbox"
+        id="privacy-agreement"
+        label="Show only requests for you"
+        checked={isChecked()}
+        onVaChange={toggleRep}
+      />
     </Form>
   );
 };
