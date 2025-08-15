192e29e2817e5221746dabe75edd2fd42835e752
John Woolschlager <wullaski@gmail.com>
2025-08-15 15:21:55 -0400

    Created new components for displaying appointment date and time for use around the entire appointments app.

---
+++ src/applications/vaos/components/AppointmentTime.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { formatInTimeZone } from 'date-fns-tz';
+import { stripDST } from '../utils/timezone';
+
+/**
+ * Component for displaying appointment time in a consistent format with timezone
+ */
+export default function AppointmentTime({ date, timezone }) {
+  const formattedTime = stripDST(
+    formatInTimeZone(new Date(date), timezone, 'h:mm aaaa'),
+  );
+  const timezoneAbbreviation = stripDST(
+    formatInTimeZone(new Date(date), timezone, 'zzz'),
+  );
+  const timezoneDescription = stripDST(
+    formatInTimeZone(new Date(date), timezone, 'zzzz'),
+  );
+  return (
+    <div data-dd-privacy="mask" data-testid="appointment-time">
+      {formattedTime}
+      <span aria-hidden="true" data-testid="appointment-time-abbreviation">
+        {timezoneAbbreviation}
+      </span>
+      <span className="sr-only" data-testid="appointment-time-description">
+        {formattedTime} {timezoneAbbreviation}
+        {`Appointment time in ${timezoneDescription}`}
+      </span>
+    </div>
+  );
+}
+
+AppointmentTime.propTypes = {
+  date: PropTypes.string.isRequired,
+  timezone: PropTypes.string.isRequired,
+};
