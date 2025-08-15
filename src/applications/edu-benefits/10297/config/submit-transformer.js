bfe46cfcb3298c6c3df1b3b190164023d16c84b4
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-08-15 14:26:23 -0700

    complete transform logic

--- src/applications/edu-benefits/10297/config/submit-transformer.js
+++ src/applications/edu-benefits/10297/config/submit-transformer.js
@@ -9,7 +9,6 @@ export function transform(formConfig, form) {
     const clonedData = _.cloneDeep(formData);
     let { homePhone, mobilePhone } = clonedData.contactInfo;
 
-    // Transfrom form data in state to match schema
     homePhone = homePhone?.replace(/[^0-9]/g, '');
     mobilePhone = mobilePhone?.replace(/[^0-9]/g, '');
 
@@ -23,24 +22,59 @@ export function transform(formConfig, form) {
     };
   };
 
+  /*
+  - There is a naming mismatch between hasCompletedActiveDuty and activeDutyDuringHitechVets.
+  - dutyRequirement corresponds to hasCompletedActiveDuty on the schema.
+  - hasCompletedActiveDuty corresponds to activeDutyDuringHitechVets on the schema.
+  - activeDutyDuringHitechVets is not a value in formData.
+  - verify this is correct
+  */
+
   const eligibilityTransform = formData => {
     const clonedData = _.cloneDeep(formData);
+    const { dutyRequirement } = clonedData;
 
     return {
       ...clonedData,
+      hasCompletedByDischarge: dutyRequirement === 'byDischarge',
+      hasCompletedActiveDuty: dutyRequirement === 'atLeast3Years',
     };
   };
 
-  // is this function required? -- see transform in form 10203
+  const identificationTransform = formData => {
+    const clonedData = _.cloneDeep(formData);
+    const { hasCompletedActiveDuty } = clonedData;
+
+    return {
+      ...clonedData,
+      activeDutyDuringHitechVets: hasCompletedActiveDuty,
+    };
+  };
+
+  const trainingProviderTransform = formData => {
+    const clonedData = _.cloneDeep(formData);
+    const { plannedStartDate, trainingProviders } = clonedData;
+
+    return {
+      ...clonedData,
+      trainingProviders: trainingProviders.map(provider => ({
+        ...provider,
+        plannedStartDate,
+      })),
+    };
+  };
+
   const usFormTransform = formData =>
     transformForSubmit(formConfig, { ...form, data: formData });
 
   const transformedData = [
     contactInfoTransform,
     eligibilityTransform,
+    identificationTransform,
+    trainingProviderTransform,
     usFormTransform,
   ].reduce((formData, transformer) => {
-    // console.log('PRETRANSFORM 🥌', formData.data);
+    // console.log('PRETRANSFORM 🥌', formData);
     return transformer(formData);
   }, form.data);
 
