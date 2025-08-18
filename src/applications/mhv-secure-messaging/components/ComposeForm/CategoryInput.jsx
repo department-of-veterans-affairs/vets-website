9653f3b7f87a0d25dbd9dffed9d727dd56fcfe42
Adam Stoler <Adam.Stoler@va.gov>
2025-08-18 15:34:07 -0400

    feat: Add feature-flagged dropdown variant for sm message category selection
    Updates CategoryInput to conditionally render VaSelect or VaRadio based on cernerPilotSmFeatureFlag, with full test coverage for both variants.

--- src/applications/mhv-secure-messaging/components/ComposeForm/CategoryInput.jsx
+++ src/applications/mhv-secure-messaging/components/ComposeForm/CategoryInput.jsx
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types';
 import {
   VaRadio,
   VaRadioOption,
+  VaSelect,
 } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { useDispatch } from 'react-redux';
-import { RadioCategories } from '../../util/inputContants';
+import { Categories } from '../../util/inputContants';
 import { updateDraftInProgress } from '../../actions/threadDetails';
+import useFeatureToggles from '../../hooks/useFeatureToggles';
 
 const CategoryInput = props => {
   const {
@@ -18,6 +20,8 @@ const CategoryInput = props => {
     setUnsavedNavigationError,
   } = props;
   const dispatch = useDispatch();
+
+  const { cernerPilotSmFeatureFlag } = useFeatureToggles();
 
   const categoryChangeHandler = e => {
     setCategory(e.detail.value);
@@ -34,39 +38,68 @@ const CategoryInput = props => {
     <>
       {categories === undefined && <va-loading-indicator />}
 
-      {categories?.length > 0 && (
-        <VaRadio
-          required
-          enable-analytics
-          data-testid="compose-message-categories"
-          label="Category"
-          className=" fieldset-input message-category"
-          error={categoryError}
-          onVaValueChange={categoryChangeHandler}
-        >
-          {categories?.map((item, i) => (
-            <VaRadioOption
-              data-dd-privacy="mask"
-              data-testid="compose-category-radio-button"
-              data-dd-action-name={`${RadioCategories[item].label}: ${
-                RadioCategories[item].description
-              } Radio Button`}
-              style={{ display: 'flex' }}
-              key={i}
-              label={
-                RadioCategories[item]
-                  ? `${RadioCategories[item].label}: ${
-                      RadioCategories[item].description
-                    }`
-                  : item
-              }
-              name="compose-message-categories"
-              value={item}
-              checked={category === item}
-            />
-          ))}
-        </VaRadio>
-      )}
+      {categories?.length > 0 &&
+        (!cernerPilotSmFeatureFlag ? (
+          <VaRadio
+            required
+            enable-analytics
+            data-testid="compose-message-categories"
+            label="Category"
+            className=" fieldset-input message-category"
+            error={categoryError}
+            onVaValueChange={categoryChangeHandler}
+          >
+            {categories?.map((item, i) => (
+              <VaRadioOption
+                data-dd-privacy="mask"
+                data-testid="compose-category-radio-button"
+                data-dd-action-name={`${Categories[item].label}: ${
+                  Categories[item].description
+                } Radio Button`}
+                style={{ display: 'flex' }}
+                key={i}
+                label={
+                  Categories[item]
+                    ? `${Categories[item].label}: ${
+                        Categories[item].description
+                      }`
+                    : item
+                }
+                name="compose-message-categories"
+                value={item}
+                checked={category === item}
+              />
+            ))}
+          </VaRadio>
+        ) : (
+          <VaSelect
+            required
+            enable-analytics
+            data-testid="compose-message-categories"
+            className="fieldset-input message-category"
+            error={categoryError}
+            label="Category"
+            name="compose-message-categories"
+            value={category}
+            onVaSelect={categoryChangeHandler}
+          >
+            {categories?.map((item, i) => (
+              <option
+                key={i}
+                value={item}
+                data-dd-privacy="mask"
+                data-testid="compose-category-dropdown-select"
+                data-dd-action-name={`${Categories[item]?.label}: ${
+                  Categories[item]?.description
+                } Dropdown Select`}
+              >
+                {Categories[item]
+                  ? `${Categories[item].label}: ${Categories[item].description}`
+                  : item}
+              </option>
+            ))}
+          </VaSelect>
+        ))}
     </>
   );
 };
