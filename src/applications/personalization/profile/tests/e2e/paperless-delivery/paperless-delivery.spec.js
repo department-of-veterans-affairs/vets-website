79b24b30be0debe2e3bb159ac0540ba355c38d66
Andrés Rivera Toro <36863582+andresriveratoro@users.noreply.github.com>
2025-08-18 09:21:09 -0400

    add e2e test

---
+++ src/applications/personalization/profile/tests/e2e/paperless-delivery/paperless-delivery.spec.js
@@ -0,0 +1,69 @@
+import { makeUserObject } from '~/applications/personalization/common/helpers';
+import mockCommunicationPreferences from '@@profile/tests/fixtures/communication-preferences/get-200-maximal.json';
+import { PROFILE_PATHS } from '@@profile/constants';
+import {
+  mockNotificationSettingsAPIs,
+  registerCypressHelpers,
+} from '../helpers';
+
+registerCypressHelpers();
+
+describe('Paperless Delivery', () => {
+  beforeEach(() => {
+    mockNotificationSettingsAPIs();
+    cy.intercept('GET', '/v0/profile/communication_preferences', {
+      statusCode: 200,
+      body: mockCommunicationPreferences,
+    });
+  });
+
+  it('should render the page with no accessibility violations', () => {
+    cy.axeCheck();
+  });
+
+  context('when user is enrolled in health care', () => {
+    it('should show the Paperless delivery group', () => {
+      cy.login(
+        makeUserObject({
+          isPatient: true,
+          facilities: [{ facilityId: '983' }],
+        }),
+      );
+      cy.visit(PROFILE_PATHS.PAPERLESS_DELIVERY);
+      cy.findByRole('heading', {
+        name: 'Paperless delivery',
+        level: 1,
+      }).should('exist');
+
+      cy.loadingIndicatorWorks();
+
+      cy.findByText(
+        /With paperless delivery, you can choose which documents you no longer want to get by mail/,
+      ).should('exist');
+
+      cy.findByText('vets.gov.user+36@gmail.com').should('exist');
+      cy.findByText('Update your email address').should('exist');
+
+      cy.findByText(
+        /We’ll always store secure, digital copies of these documents on VA.gov/,
+      ).should('exist');
+
+      cy.findByRole('heading', {
+        name: 'Documents available for paperless delivery',
+        level: 2,
+      }).should('exist');
+      cy.findByText(
+        /We have limited documents available for paperless delivery at this time/,
+      ).should('exist');
+      cy.findByRole('heading', {
+        name: /Select the document you no longer want to get by mail. You can change this at any time/,
+        level: 3,
+      }).should('exist');
+      cy.get('va-checkbox')
+        .shadow()
+        .find('input[type="checkbox"]')
+        .check({ force: true });
+      cy.injectAxeThenAxeCheck();
+    });
+  });
+});
