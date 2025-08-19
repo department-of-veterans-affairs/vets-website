8d075ab889865d8313aed50ee9d8fa9807d03d25
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-08-18 20:42:08 -0600

    update TestDocumentUploadPage to have MultipleDocumentUpload component

--- src/applications/travel-pay/containers/pages/TestDocumentUploadPage.jsx
+++ src/applications/travel-pay/containers/pages/TestDocumentUploadPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import DocumentUpload from '../../components/DocumentUpload';
+import MultipleDocumentUpload from '../../components/MultipleDocumentUpload';
 
 const TestDocumentUploadPage = () => {
   return (
     <div className="vads-l-grid-container vads-u-padding-y--3">
       <h1>Test Document Upload</h1>
+      <h2>Single Document Upload</h2>
       <DocumentUpload />
+      <h2>Multiple Document Upload</h2>
+      <MultipleDocumentUpload />
     </div>
   );
 };
