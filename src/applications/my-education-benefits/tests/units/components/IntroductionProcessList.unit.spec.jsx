0b5df370847f83a2047c8caf2e7bb714c1e99d2f
Brad Bergeron <bergeron.bradley@gmail.com>
2025-08-19 15:25:21 -0400

    Update IntroductionProcessList unit tests

--- src/applications/my-education-benefits/tests/units/components/IntroductionProcessList.unit.spec.jsx
+++ src/applications/my-education-benefits/tests/units/components/IntroductionProcessList.unit.spec.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import FEATURE_FLAG_NAMES from 'platform/utilities/feature-toggles/featureFlagNames';
 import IntroductionProcessList from '../../../components/IntroductionProcessList';
 
-const mockStore = configureStore([]);
-
-describe('IntroductionProcessList (connected)', () => {
+describe('IntroductionProcessList', () => {
   let wrapper;
-  const mountWithFlag = flag => {
-    const store = mockStore({
-      featureToggles: {
-        [FEATURE_FLAG_NAMES.showMeb54901990eTextUpdate]: flag,
-      },
-    });
-    wrapper = mount(
-      <Provider store={store}>
-        <IntroductionProcessList />
-      </Provider>,
-    );
-    return wrapper;
-  };
+
+  beforeEach(() => {
+    wrapper = mount(<IntroductionProcessList />);
+  });
 
   afterEach(() => {
     if (wrapper && wrapper.unmount) {
@@ -30,23 +16,46 @@ describe('IntroductionProcessList (connected)', () => {
     }
   });
 
-  it('renders original content when flag is off', () => {
-    const w = mountWithFlag(false);
-    expect(
-      w
-        .find('va-additional-info')
-        .first()
-        .prop('trigger'),
-    ).to.equal('What are the Post-9/11 GI Bill eligibility requirements?');
+  it('renders the process list with three steps', () => {
+    const processListItems = wrapper.find('va-process-list-item');
+    expect(processListItems).to.have.length(3);
   });
 
-  it('renders updated content when flag is on', () => {
-    const w = mountWithFlag(true);
-    expect(
-      w
-        .find('va-additional-info')
-        .first()
-        .prop('trigger'),
-    ).to.include('(Chapter 33)');
+  it('renders Check your eligibility step with Chapter 33 requirements', () => {
+    const firstStep = wrapper.find('va-process-list-item').at(0);
+    expect(firstStep.prop('header')).to.equal('Check your eligibility');
+
+    const additionalInfos = firstStep.find('va-additional-info');
+    expect(additionalInfos).to.have.length(3);
+
+    expect(additionalInfos.at(0).prop('trigger')).to.include('(Chapter 33)');
+    expect(additionalInfos.at(1).prop('trigger')).to.include('(Chapter 30)');
+    expect(additionalInfos.at(2).prop('trigger')).to.include('(Chapter 1606)');
+  });
+
+  it('renders Gather your information step with all required items', () => {
+    const secondStep = wrapper.find('va-process-list-item').at(1);
+    expect(secondStep.prop('header')).to.equal('Gather your information');
+
+    const listItems = secondStep.find('li');
+    expect(listItems).to.have.length(3);
+    expect(listItems.at(0).text()).to.include('military service history');
+    expect(listItems.at(1).text()).to.include(
+      'address and contact information',
+    );
+    expect(listItems.at(2).text()).to.include(
+      'Bank account direct deposit information',
+    );
+  });
+
+  it('renders Start your application step with what happens after applying info', () => {
+    const thirdStep = wrapper.find('va-process-list-item').at(2);
+    expect(thirdStep.prop('header')).to.equal('Start your application');
+
+    const additionalInfo = thirdStep.find('va-additional-info');
+    expect(additionalInfo).to.have.length(1);
+    expect(additionalInfo.prop('trigger')).to.equal(
+      'What happens after I apply?',
+    );
   });
 });
