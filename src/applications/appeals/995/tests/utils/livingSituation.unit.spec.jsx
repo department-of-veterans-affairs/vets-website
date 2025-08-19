1f3c10efffb3abc8ad6dbad9a54be2eb08b62144
Jerry Sea <Jsea@kindsys.us>
2025-08-19 11:38:45 -0700

    Remove feature flag from living situation check

--- src/applications/appeals/995/tests/utils/livingSituation.unit.spec.jsx
+++ src/applications/appeals/995/tests/utils/livingSituation.unit.spec.jsx
@@ -5,21 +5,12 @@ import {
   hasOtherHousingRisk,
 } from '../../utils/livingSituation';
 
-import { SC_NEW_FORM_DATA } from '../../constants';
-
 describe('hasHousingRisk', () => {
   it('should return expected value', () => {
-    expect(
-      hasHousingRisk({ [SC_NEW_FORM_DATA]: undefined, housingRisk: false }),
-    ).to.be.undefined;
-    expect(hasHousingRisk({ [SC_NEW_FORM_DATA]: true, housingRisk: false })).to
-      .be.false;
-    expect(hasHousingRisk({ [SC_NEW_FORM_DATA]: true, housingRisk: true })).to
-      .be.true;
-    expect(hasHousingRisk({ [SC_NEW_FORM_DATA]: false, housingRisk: false })).to
-      .be.false;
-    expect(hasHousingRisk({ [SC_NEW_FORM_DATA]: false, housingRisk: true })).to
-      .be.false;
+    expect(hasHousingRisk({ housingRisk: false })).to.be.false;
+    expect(hasHousingRisk({ housingRisk: true })).to.be.true;
+    expect(hasHousingRisk({ housingRisk: undefined })).to.be.undefined;
+    expect(hasHousingRisk({})).to.be.undefined;
   });
 });
 
@@ -27,7 +18,6 @@ describe('hasOtherHousingRisk', () => {
   it('should return expected value', () => {
     expect(
       hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: undefined,
         housingRisk: undefined,
         livingSituation: { other: false },
       }),
@@ -35,57 +25,13 @@ describe('hasOtherHousingRisk', () => {
 
     expect(
       hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: undefined,
-        housingRisk: true,
-        livingSituation: { other: false },
-      }),
-    ).to.be.undefined;
-    expect(
-      hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: undefined,
-        housingRisk: true,
+        housingRisk: undefined,
         livingSituation: { other: true },
-      }),
-    ).to.be.undefined;
-    expect(
-      hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: undefined,
-        housingRisk: false,
-        livingSituation: { other: false },
-      }),
-    ).to.be.undefined;
-    expect(
-      hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: undefined,
-        housingRisk: false,
-        livingSituation: { other: false },
       }),
     ).to.be.undefined;
 
     expect(
       hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: false,
-        housingRisk: true,
-        livingSituation: { other: false },
-      }),
-    ).to.be.false;
-    expect(
-      hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: false,
-        housingRisk: true,
-        livingSituation: { other: true },
-      }),
-    ).to.be.false;
-    expect(
-      hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: false,
-        housingRisk: false,
-        livingSituation: { other: false },
-      }),
-    ).to.be.false;
-    expect(
-      hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: false,
         housingRisk: false,
         livingSituation: { other: false },
       }),
@@ -93,31 +39,36 @@ describe('hasOtherHousingRisk', () => {
 
     expect(
       hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: true,
+        housingRisk: false,
+        livingSituation: { other: true },
+      }),
+    ).to.be.false;
+
+    expect(
+      hasOtherHousingRisk({
         housingRisk: true,
         livingSituation: { other: false },
       }),
     ).to.be.false;
+
     expect(
       hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: true,
         housingRisk: true,
         livingSituation: { other: true },
       }),
     ).to.be.true;
+
     expect(
       hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: true,
-        housingRisk: false,
-        livingSituation: { other: false },
+        housingRisk: true,
       }),
-    ).to.be.false;
+    ).to.be.undefined;
+
     expect(
       hasOtherHousingRisk({
-        [SC_NEW_FORM_DATA]: true,
-        housingRisk: false,
-        livingSituation: { other: false },
+        housingRisk: true,
+        livingSituation: {},
       }),
-    ).to.be.false;
+    ).to.be.undefined;
   });
 });
