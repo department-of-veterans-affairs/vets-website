d49bbcb4a10e5b77ff671e90e858e65584547a86
Michael Ghisilieri <175352110+mghisilieri@users.noreply.github.com>
2025-08-20 10:33:00 -0700

    22-1919 InstitutionAddress to make use of addressSchema

--- src/applications/edu-benefits/1919/pages/aboutYourInstitution.js
+++ src/applications/edu-benefits/1919/pages/aboutYourInstitution.js
@@ -13,6 +13,7 @@ const updateFormData = (oldData, formData) => {
       ...formData,
       institutionDetails: {
         ...formData.institutionDetails,
+        facilityCode: '',
         institutionName: '',
         institutionAddress: {},
       },
