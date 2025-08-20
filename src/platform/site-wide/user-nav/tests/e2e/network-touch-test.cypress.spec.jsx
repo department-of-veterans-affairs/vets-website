9da96dc820e69f4c1302febf278249fc422c3951
Isaac Gallup <igallup@thesocompany.com>
2025-08-20 15:56:55 -0500

    swapping debug spec logic

--- src/platform/site-wide/user-nav/tests/e2e/network-touch-test.cypress.spec.jsx
+++ src/platform/site-wide/user-nav/tests/e2e/network-touch-test.cypress.spec.jsx
@@ -1,28 +1,9 @@
-describe('annotator debug', () => {
-  it('confirms hook + flips touched', () => {
-    cy.task('annotatorCanary', 'spec booted');
-    cy.visit('/');
-
-    // (A) support hook present??
-    cy.window().then(win => {
-      expect(Boolean(win.__annotator__ && win.__annotator__.wrapped)).to.eq(
-        true,
-      );
-    });
-
-    // (B) cause a real XHR (should flip 'touched' via XHR.prototype.open)
-    cy.window().then(win => {
-      const xhr = new win.XMLHttpRequest();
-      xhr.open('GET', 'https://httpbin.org/status/204');
-      try {
-        xhr.send();
-        // eslint-disable-next-line no-empty
-      } catch (_) {}
-    });
-
-    // (C) verify the flag flipped
-    cy.window().then(win => {
-      expect(win.__annotator__.getTouched(), 'touched flipped').to.eq(true);
+describe('network-touch smoke (XHR)', () => {
+  describe('annotation smoke (forced)', () => {
+    it('always produces a PR warning for this spec', () => {
+      cy.visit('/'); // any page; not important
+      // Force the annotation for THIS spec file
+      cy.task('recordNetworkTouch', Cypress.spec.absolute, { log: false });
     });
   });
 });
