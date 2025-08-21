b2ca8cac7b73ce945edcf81d326517770568424d
122034971+ConnorJeff@users.noreply.github.com <122034971+ConnorJeff@users.noreply.github.com>
2025-08-21 11:27:01 -0400

    MBMS-101797 Confirmation page content changes

--- src/applications/pre-need-integration/containers/ConfirmationPage.jsx
+++ src/applications/pre-need-integration/containers/ConfirmationPage.jsx
@@ -5,6 +5,29 @@ import { useSelector } from 'react-redux';
 import { scrollToTop } from 'platform/utilities/scroll';
 import { focusElement } from 'platform/utilities/ui';
 import { ConfirmationView } from 'platform/forms-system/src/js/components/ConfirmationView';
+import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
+
+const HowToContactContent = () => (
+  <>
+    <p>
+      Call us at <va-telephone contact={CONTACTS.VA_BENEFITS} /> (
+      <va-telephone tty="true" contact={CONTACTS[711]} />) We’re here Monday
+      through Friday, 8:00 a.m. to 7:30 p.m. ET, and Saturday 9:00 a.m. to 5:30
+      p.m E.T.
+    </p>
+    <p>
+      Or, check our resources and support section for answers to common
+      questions.
+    </p>
+    <p>
+      <va-link
+        external
+        href="https://ask.va.gov/"
+        text="Go to resources and support section on VA.gov"
+      />
+    </p>
+  </>
+);
 
 const ConfirmationPage = props => {
   const form = useSelector(state => state.form || {});
@@ -35,36 +58,30 @@ const ConfirmationPage = props => {
       />
       <ConfirmationView.SavePdfDownload />
       <ConfirmationView.ChapterSectionCollection />
-      <h4>Do you have more documents you need to submit?</h4>
-      <p className="mail-or-fax-message">
-        To mail or fax additional documents:
-      </p>
-      <ol className="mail-or-fax-steps">
-        <li className="mail-or-fax-step">Make copies of the documents.</li>
-        <li className="mail-or-fax-step">
-          Make sure you write your name and confirmation number on every page.
-        </li>
-        <li className="mail-or-fax-step">
-          <span>
-            Submit application and supporting documents to the VA by mail:
-          </span>
-          <p>
-            <div className="mail-fax-address">
-              <div>NCA Intake Center</div>
-              <div>P.O. Box 5237</div>
-              <div>Janesville, WI 53547</div>
-            </div>
-          </p>
-          <strong>Or</strong>
-          <p>
-            Fax them to the National Cemetery Scheduling Office:{' '}
-            <va-telephone contact="8558408299" />.
-          </p>
-        </li>
-      </ol>
       <ConfirmationView.PrintThisPage />
-      <ConfirmationView.WhatsNextProcessList />
-      <ConfirmationView.HowToContact />
+      <ConfirmationView.WhatsNextProcessList
+        item1Header="We’ll review your application"
+        item1Content="If we need more information about your information, we’ll contact you."
+        item1Actions={null}
+        item2Header="We’ll reach a decision"
+        item2Content="You’ll receive a pre-need decision letter notifying you of your eligibility for burial in a VA national cemetery."
+      />
+      <h3>If you need to submit supporting documents</h3>
+      <p className="mail-or-fax-message">
+        You can mail your supporting documents to this address:
+      </p>
+      <p className="va-address-block">
+        NCA FP Evidence Intake Center <br />
+        P.O. Box 5237
+        <br />
+        Janesville, WI 53547
+        <br />
+      </p>
+      <p>
+        Or you can fax your supporting documents to{' '}
+        <va-telephone contact="8558408299" />.
+      </p>
+      <ConfirmationView.HowToContact content={<HowToContactContent />} />
       <ConfirmationView.GoBackLink />
     </ConfirmationView>
   );
