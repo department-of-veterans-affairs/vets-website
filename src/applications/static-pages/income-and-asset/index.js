a0f98c86be31e9f039d960c31a1b2ea0a678a385
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2025-08-21 14:22:46 -0600

    Add to static page entries

--- src/applications/static-pages/income-and-asset/index.js
+++ src/applications/static-pages/income-and-asset/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { connectFeatureToggle } from 'platform/utilities/feature-toggles';
 
-export default (store, widgetType) => {
+export default async (store, widgetType) => {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   if (root) {
-    import(/* webpackChunkName: "income-and-asset-statement-form-21p-0969" */
+    import(/* webpackChunkName: "income-and-asset-statement-staged-entry" */
     './components/App').then(module => {
       const App = module.default;
-      connectFeatureToggle(store.dispatch);
-
       ReactDOM.render(
         <Provider store={store}>
           <App />
