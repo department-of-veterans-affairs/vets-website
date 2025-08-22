d6cb0787c9a1fef08476fe1eb0da96042de0df8a
Tai Corraggio <21046714+TaiWilkin@users.noreply.github.com>
2025-08-22 12:25:02 -0400

    Add more App tests

--- src/applications/dependents/686c-674/tests/containers/App.unit.spec.jsx
+++ src/applications/dependents/686c-674/tests/containers/App.unit.spec.jsx
@@ -1,77 +1,166 @@
-import { expect } from 'chai';
 import React from 'react';
+import { expect } from 'chai';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 import sinon from 'sinon';
-import { renderWithStoreAndRouter } from '@department-of-veterans-affairs/platform-testing/react-testing-library-helpers';
-
-// Import modules to stub
-import * as datadogExports from 'platform/monitoring/Datadog/';
-import * as featureToggleExports from 'platform/utilities/feature-toggles';
-import * as redirectExports from '../../utils/redirect';
 
 import App from '../../containers/App';
 
-let sandbox;
+const mockStore = configureMockStore();
 
-describe('Dependents 686c-674 <App>', () => {
-  const getInitialState = ({
-    isLoggedIn = true,
-    isLoading = false,
-    vaDependentsV2 = true,
-    hasVaFileNumber = true,
-  } = {}) => {
-    return {
-      initialState: {
-        user: {
-          login: {
-            currentlyLoggedIn: isLoggedIn,
-          },
-          profile: {
-            loading: isLoading,
-            savedForms: [],
-          },
+function getDefaultState({
+  featureToggles = { loading: false },
+  hasSession = true,
+} = {}) {
+  if (typeof localStorage !== 'undefined') {
+    localStorage.setItem('hasSession', JSON.stringify(hasSession));
+  }
+
+  return {
+    vaFileNumber: {},
+    form: {
+      formId: '686c-674',
+      loadedStatus: 'success',
+      savedStatus: '',
+      loadedData: {
+        metadata: {},
+      },
+      data: {},
+      submission: {
+        response: {},
+        timestamp: null,
+      },
+    },
+    user: {
+      login: {
+        currentlyLoggedIn: true,
+      },
+      profile: {
+        loading: false,
+        savedForms: [],
+        prefillsAvailable: [],
+        loa: {
+          current: 3,
+          highest: 3,
         },
-        featureToggles: {
-          loading: isLoading,
-          vaDependentsV2,
+        verified: true,
+        dob: '1990-01-01',
+        userFullName: {
+          first: 'Test',
+          last: 'User',
         },
-        vaFileNumber: {
-          hasVaFileNumber: {
-            VALIDVAFILENUMBER: hasVaFileNumber,
-          },
+        claims: {
+          appeals: false,
         },
       },
-    };
+    },
+    scheduledDowntime: {
+      globalDowntime: null,
+      isReady: true,
+      isPending: false,
+      serviceMap: { get: () => null },
+      dismissedDowntimeWarnings: [],
+    },
+    featureToggles: {
+      loading: false,
+      vaDependentsV2: true,
+      ...featureToggles,
+    },
+  };
+}
+
+function renderApp({
+  pathname = '/introduction',
+  search = '',
+  hash = '',
+  featureToggles,
+  hasSession,
+} = {}) {
+  const state = getDefaultState({
+    featureToggles,
+    hasSession,
+  });
+  const store = mockStore(state);
+
+  const _location = {
+    ...window.location,
+    pathname,
+    search,
+    hash,
   };
 
-  beforeEach(() => {
-    sandbox = sinon.createSandbox();
+  return render(
+    <Provider store={store}>
+      <App location={_location}>
+        <div data-testid="children-content">Child content</div>
+      </App>
+    </Provider>,
+  );
+}
 
-    // Stub the hooks used by the App component
-    sandbox.stub(datadogExports, 'useBrowserMonitoring');
-    sandbox.stub(featureToggleExports, 'useFeatureToggle').returns({
-      useFormFeatureToggleSync: sinon.stub(),
-      useToggleValue: sinon.stub().returns(true),
-      TOGGLE_NAMES: { dependentsModuleEnabled: 'dependentsModuleEnabled' },
-    });
-    sandbox.stub(redirectExports, 'getShouldUseV2').returns(true);
-  });
+describe('App container logic', () => {
+  const oldLocation = global.window.location;
 
   afterEach(() => {
-    sandbox.restore();
+    global.window.location = oldLocation;
+    localStorage.removeItem('hasSession');
   });
 
-  it('should render loading indicator when feature toggles are loading', () => {
-    const screen = renderWithStoreAndRouter(
-      <App location={{ pathname: '/introduction' }}>
-        <p data-testid="test-content">Test content</p>
-      </App>,
-      getInitialState({ isLoading: true }),
-    );
+  it('should show loading indicator while waiting for toggles', () => {
+    const { container, queryByTestId } = renderApp({
+      featureToggles: { loading: true },
+    });
 
-    const loadingIndicator = screen.container.querySelector(
-      'va-loading-indicator',
-    );
-    expect(loadingIndicator).to.exist;
-    expect(screen.queryByTestId('test-content')).to.be.null;
+    const loadingIndicator = container.querySelector('va-loading-indicator');
+    expect(loadingIndicator).to.not.be.null;
+    expect(queryByTestId('children-content')).to.be.null;
+  });
+
+  it('should render RoutedSavableApp with children when on intro page (with session)', () => {
+    const { getByTestId } = renderApp();
+    expect(getByTestId('children-content')).to.exist;
+  });
+
+  it('should render RoutedSavableApp with children when on intro page (without session)', () => {
+    const { getByTestId } = renderApp({
+      hasSession: false,
+    });
+
+    expect(getByTestId('children-content')).to.exist;
+  });
+
+  it('should always render breadcrumbs', () => {
+    const { container } = renderApp();
+    const breadcrumbs = container.querySelector('va-breadcrumbs');
+    expect(breadcrumbs).to.exist;
+  });
+
+  it('should not redirect when on intro page (with session)', () => {
+    const mockReplace = sinon.stub();
+    delete window.location;
+    window.location = { replace: mockReplace };
+
+    renderApp({
+      pathname: '/introduction',
+      hasSession: true,
+      dependentsLoading: true,
+    });
+
+    expect(mockReplace.called).to.be.false;
+  });
+
+  it('should not redirect when on intro page (without session)', () => {
+    const mockReplace = sinon.stub();
+    delete window.location;
+    window.location = { replace: mockReplace };
+
+    renderApp({
+      pathname: '/introduction',
+      hasSession: false,
+      dependentsLoading: true,
+    });
+
+    expect(mockReplace.called).to.be.false;
   });
 });
