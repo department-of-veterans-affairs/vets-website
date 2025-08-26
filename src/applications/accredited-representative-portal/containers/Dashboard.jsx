dbaa314c3d337832bbfd682c281471d9c0af688f
jquispe-oddball <jennifer.quispe@oddball.io>
2025-08-26 09:38:10 -0400

    116754 - dashboard progress

---
+++ src/applications/accredited-representative-portal/containers/Dashboard.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { VaBreadcrumbs } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import { dashboardBC, DASHBOARD_BC_LABEL } from '../utilities/poaRequests';
+import Unauthorized from '../components/Dashboard/Unauthorized';
+import Authorized from '../components/Dashboard/Authorized';
+
+const Dashboard = title => {
+  useEffect(
+    () => {
+      document.title = title.title;
+    },
+    [title],
+  );
+  return (
+    <section className="dashboard">
+      <div className="arp-container">
+        <VaBreadcrumbs
+          breadcrumbList={dashboardBC}
+          label={DASHBOARD_BC_LABEL}
+          homeVeteransAffairs={false}
+        />
+        {/* import this is user is unauthorized */}
+        <Unauthorized />
+        <Authorized />
+      </div>
+    </section>
+  );
+};
+
+export default Dashboard;
