cbd0bdfa4de6e2535050ce561a33bb16cdcabf6d
mdewey <1793923+mdewey@users.noreply.github.com>
2025-08-26 10:53:11 -0400

    Refactor LabsAndTests and Vitals components to conditionally update time frame on initial load; add Cypress tests for Labs and Tests page and Vitals list page to verify URL behavior.

--- src/applications/mhv-medical-records/containers/LabsAndTests.jsx
+++ src/applications/mhv-medical-records/containers/LabsAndTests.jsx
@@ -145,18 +145,26 @@ const LabsAndTests = () => {
 
   useEffect(
     () => {
-      // Only update if there is no time frame. This is only for on initial page load.
-      const timeFrame = new URLSearchParams(location.search).get('timeFrame');
-      if (!timeFrame) {
-        const searchParams = new URLSearchParams(location.search);
-        searchParams.set('timeFrame', acceleratedLabsAndTestDate);
-        history.push({
-          pathname: location.pathname,
-          search: searchParams.toString(),
-        });
+      if (isAcceleratingLabsAndTests) {
+        // Only update if there is no time frame. This is only for on initial page load.
+        const timeFrame = new URLSearchParams(location.search).get('timeFrame');
+        if (!timeFrame) {
+          const searchParams = new URLSearchParams(location.search);
+          searchParams.set('timeFrame', acceleratedLabsAndTestDate);
+          history.push({
+            pathname: location.pathname,
+            search: searchParams.toString(),
+          });
+        }
       }
     },
-    [acceleratedLabsAndTestDate, history, location.pathname, location.search],
+    [
+      acceleratedLabsAndTestDate,
+      history,
+      isAcceleratingLabsAndTests,
+      location.pathname,
+      location.search,
+    ],
   );
   const updateDate = event => {
     const [year, month] = event.target.value.split('-');
