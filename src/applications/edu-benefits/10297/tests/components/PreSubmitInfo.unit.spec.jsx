a04fb4637bef0c45696e92a3391212b649c284bf
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-08-27 08:59:26 -0500

    add test coverage

--- src/applications/edu-benefits/10297/tests/components/PreSubmitInfo.unit.spec.jsx
+++ src/applications/edu-benefits/10297/tests/components/PreSubmitInfo.unit.spec.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { expect } from 'chai';
-import { render } from '@testing-library/react';
+import { render, waitFor, fireEvent } from '@testing-library/react';
+import sinon from 'sinon';
+import * as webComponents from 'platform/utilities/ui/webComponents';
 
 import PreSubmitInfo from '../../components/PreSubmitInfo';
 
 describe('<PreSubmitInfo />', () => {
+  let querySelectorStub;
+
+  afterEach(() => {
+    if (querySelectorStub) querySelectorStub.restore();
+  });
+
   it('should render privacy policy link/button', () => {
     const { container } = render(<PreSubmitInfo />);
     const selector = container.querySelector('va-link');
@@ -40,5 +48,59 @@ describe('<PreSubmitInfo />', () => {
     expect(modal)
       .to.have.attr('visible')
       .to.equal('true');
+  });
+
+  it('should set clarifying text when label element exists', async () => {
+    const hiddenParagraph = { setAttribute: sinon.spy() };
+    const checkbox = {};
+    const labelEl = { innerHTML: '' };
+
+    querySelectorStub = sinon
+      .stub(webComponents, 'querySelectorWithShadowRoot')
+
+      .onFirstCall()
+      .resolves(hiddenParagraph)
+
+      .onSecondCall()
+      .resolves(checkbox)
+
+      .onThirdCall()
+      .resolves(labelEl);
+
+    const { container } = render(<PreSubmitInfo />);
+
+    await waitFor(() => {
+      const modal = container.querySelector('va-modal');
+      expect(modal).to.exist; // ensure component rendered
+      expect(hiddenParagraph.setAttribute.calledWith('style', 'display:none;'))
+        .to.be.true;
+      expect(labelEl.innerHTML).to.contain(
+        'I certify that the information I have provided is true and correct',
+      );
+    });
+  });
+
+  it('should open modal on Enter keydown and ignore other keys', async () => {
+    const { container } = render(<PreSubmitInfo />);
+    const link = container.querySelector('va-link');
+    const modal = container.querySelector('va-modal');
+
+    expect(modal)
+      .to.have.attr('visible')
+      .to.equal('false');
+
+    fireEvent.keyDown(link, { key: ' ' });
+
+    expect(modal)
+      .to.have.attr('visible')
+      .to.equal('false');
+
+    fireEvent.keyDown(link, { key: 'Enter' });
+
+    await waitFor(() => {
+      expect(modal)
+        .to.have.attr('visible')
+        .to.equal('true');
+    });
   });
 });
