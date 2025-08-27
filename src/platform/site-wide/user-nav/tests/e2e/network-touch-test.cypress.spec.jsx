953f798d05a4bf7fa4f562eb53c1e663541e9739
Isaac Gallup <igallup@thesocompany.com>
2025-08-27 11:19:45 -0500

    Removed the intentionally-failing network touch test Cypress spec

--- src/platform/site-wide/user-nav/tests/e2e/network-touch-test.cypress.spec.jsx
+++
@@ -1,9 +0,0 @@
-describe('network-touch smoke (XHR)', () => {
-  describe('annotation smoke (forced)', () => {
-    it('always produces a PR warning for this spec', () => {
-      cy.task('recordNetworkTouch', Cypress.spec.absolute, { log: false });
-      // Emit the annotation immediately so GitHub captures it even if after:spec misses
-      cy.task('emitAnnotationNow', Cypress.spec.absolute, { log: false });
-    });
-  });
-});
