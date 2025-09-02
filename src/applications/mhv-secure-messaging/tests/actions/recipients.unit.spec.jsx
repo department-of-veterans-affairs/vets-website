2f01c5fdd1a59469e6e39b0ff8917983d6ea1c3e
Adam Stoler <89949977+6StringCode@users.noreply.github.com>
2025-09-02 09:54:04 -0400

    updated recipients.unit.specs

--- src/applications/mhv-secure-messaging/tests/actions/recipients.unit.spec.jsx
+++ src/applications/mhv-secure-messaging/tests/actions/recipients.unit.spec.jsx
@@ -42,6 +42,178 @@ describe('triageTeam actions', () => {
       });
     });
   });
+
+  describe('signature requirement logic', () => {
+    it('should add signatureRequired=true for Privacy Issue_Admin recipients', async () => {
+      const mockResponse = {
+        data: [
+          {
+            id: '1',
+            attributes: { name: 'Privacy Issue_Admin' },
+          },
+        ],
+      };
+
+      const store = mockStore();
+      mockApiRequest(mockResponse);
+
+      store.dispatch(getAllTriageTeamRecipients());
+      const actions = store.getActions();
+
+      expect(actions[0].response.data[0].attributes.signatureRequired).to.equal(
+        true,
+      );
+    });
+
+    it('should add signatureRequired=true for Record Amendment_Admin recipients', async () => {
+      const mockResponse = {
+        data: [
+          {
+            id: '1',
+            attributes: { name: 'Record Amendment_Admin' },
+          },
+        ],
+      };
+
+      const store = mockStore();
+      mockApiRequest(mockResponse);
+
+      store.dispatch(getAllTriageTeamRecipients());
+      const actions = store.getActions();
+
+      expect(actions[0].response.data[0].attributes.signatureRequired).to.equal(
+        true,
+      );
+    });
+
+    it('should add signatureRequired=true for Release of Information Medical Records_Admin recipients', async () => {
+      const mockResponse = {
+        data: [
+          {
+            id: '1',
+            attributes: {
+              name: 'Release of Information Medical Records_Admin',
+            },
+          },
+        ],
+      };
+
+      const store = mockStore();
+      mockApiRequest(mockResponse);
+
+      store.dispatch(getAllTriageTeamRecipients());
+      const actions = store.getActions();
+
+      expect(actions[0].response.data[0].attributes.signatureRequired).to.equal(
+        true,
+      );
+    });
+
+    it('should add signatureRequired=true for Oracle Health Release of Information patterns (without _Admin)', async () => {
+      const mockResponse = {
+        data: [
+          {
+            id: '1',
+            attributes: { name: 'VHA 123 Release of Information (ROI)' },
+          },
+          {
+            id: '2',
+            attributes: {
+              name: 'State City Release of Information – Medical Records',
+            },
+          },
+          {
+            id: '3',
+            attributes: { name: 'Some Release of Information Team' },
+          },
+        ],
+      };
+
+      const store = mockStore();
+      mockApiRequest(mockResponse);
+
+      store.dispatch(getAllTriageTeamRecipients());
+      const actions = store.getActions();
+
+      // All Oracle Health ROI patterns should require signature
+      expect(actions[0].response.data[0].attributes.signatureRequired).to.equal(
+        true,
+      );
+      expect(actions[0].response.data[1].attributes.signatureRequired).to.equal(
+        true,
+      );
+      expect(actions[0].response.data[2].attributes.signatureRequired).to.equal(
+        true,
+      );
+    });
+
+    it('should NOT add signatureRequired for regular team names', async () => {
+      const mockResponse = {
+        data: [
+          {
+            id: '1',
+            attributes: { name: 'Regular Team Name' },
+          },
+          {
+            id: '2',
+            attributes: { name: 'Privacy Issue Department' }, // Missing _Admin
+          },
+          {
+            id: '3',
+            attributes: { name: 'Cardiology Team' },
+          },
+        ],
+      };
+
+      const store = mockStore();
+      mockApiRequest(mockResponse);
+
+      store.dispatch(getAllTriageTeamRecipients());
+      const actions = store.getActions();
+
+      // Regular teams should not have signatureRequired property
+      expect(actions[0].response.data[0].attributes.signatureRequired).to.be
+        .undefined;
+      expect(actions[0].response.data[1].attributes.signatureRequired).to.be
+        .undefined;
+      expect(actions[0].response.data[2].attributes.signatureRequired).to.be
+        .undefined;
+    });
+
+    it('should handle mixed recipient types correctly', async () => {
+      const mockResponse = {
+        data: [
+          {
+            id: '1',
+            attributes: { name: 'Privacy Issues_Admin' }, // Should require signature
+          },
+          {
+            id: '2',
+            attributes: { name: 'Regular Team' }, // Should not require signature
+          },
+          {
+            id: '3',
+            attributes: { name: 'VHA 456 Release of Information' }, // Should require signature (Oracle Health)
+          },
+        ],
+      };
+
+      const store = mockStore();
+      mockApiRequest(mockResponse);
+
+      store.dispatch(getAllTriageTeamRecipients());
+      const actions = store.getActions();
+
+      expect(actions[0].response.data[0].attributes.signatureRequired).to.equal(
+        true,
+      );
+      expect(actions[0].response.data[1].attributes.signatureRequired).to.be
+        .undefined;
+      expect(actions[0].response.data[2].attributes.signatureRequired).to.equal(
+        true,
+      );
+    });
+  });
 });
 
 describe('isSignatureRequired function (tested through getAllTriageTeamRecipients)', () => {
