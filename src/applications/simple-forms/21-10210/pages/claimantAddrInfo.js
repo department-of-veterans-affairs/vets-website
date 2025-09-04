851f866a9f8004bd129bde0286495db838f3ef8a
Chikaodi Adibemma <chika.adibemma@adhocteam.us>
2025-09-03 20:54:12 -0400

    updated address pages to match figma

--- src/applications/simple-forms/21-10210/pages/claimantAddrInfo.js
+++ src/applications/simple-forms/21-10210/pages/claimantAddrInfo.js
@@ -1,10 +1,11 @@
 import {
-  addressNoMilitarySchema,
-  addressNoMilitaryUI,
+  addressSchema,
+  addressUI,
+  titleUI,
 } from 'platform/forms-system/src/js/web-component-patterns';
 
-const pdfSchema = addressNoMilitarySchema({
-  omit: ['isMilitary', 'street3'],
+const pdfSchema = addressSchema({
+  omit: ['street3'],
 });
 pdfSchema.properties.street.maxLength = 30;
 pdfSchema.properties.street2.maxLength = 30;
@@ -13,8 +14,9 @@ pdfSchema.properties.city.maxLength = 18;
 /** @type {PageSchema} */
 export default {
   uiSchema: {
-    claimantMailingAddress: addressNoMilitaryUI({
-      omit: ['isMilitary', 'street3'],
+    ...titleUI('Mailing address'),
+    claimantMailingAddress: addressUI({
+      omit: ['street3'],
       required: true,
     }),
   },
