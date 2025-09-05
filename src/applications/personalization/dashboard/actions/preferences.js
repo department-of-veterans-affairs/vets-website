0d0cf59e2eed52528689bc17171a9c84ead15b76
Giles Wells <giles@mediagearhead.com>
2025-09-05 15:20:06 -0400

    Align naming convention

--- src/applications/personalization/dashboard/actions/preferences.js
+++ src/applications/personalization/dashboard/actions/preferences.js
@@ -1,6 +1,6 @@
 // import { recordEvent } from '@department-of-veterans-affairs/platform-monitoring/exports';
 
-export const SET_MYVA_LAYOUT_PREFERENCE = 'SET_MYVA_LAYOUT_PREFERENCE';
+export const SET_MY_VA_LAYOUT_PREFERENCE = 'SET_MY_VA_LAYOUT_PREFERENCE';
 
 export const updateMyVaLayoutVersion = selectedVersion => {
   // Record the event when a user changes
@@ -10,7 +10,7 @@ export const updateMyVaLayoutVersion = selectedVersion => {
   // };
   // recordEvent(event);
   return {
-    type: SET_MYVA_LAYOUT_PREFERENCE,
+    type: SET_MY_VA_LAYOUT_PREFERENCE,
     layout: {
       version: selectedVersion,
     },
--- src/applications/personalization/dashboard/reducers/preferences.js
+++ src/applications/personalization/dashboard/reducers/preferences.js
@@ -1,10 +1,10 @@
 import set from 'platform/utilities/data/set';
-import { SET_MYVA_LAYOUT_PREFERENCE } from '../actions/preferences';
+import { SET_MY_VA_LAYOUT_PREFERENCE } from '../actions/preferences';
 
 const initialState = {};
 
 export default function preferencesReducer(state = initialState, action) {
-  if (action.type === SET_MYVA_LAYOUT_PREFERENCE) {
+  if (action.type === SET_MY_VA_LAYOUT_PREFERENCE) {
     return set('layout.version', action.layout.version, state);
   }
 
