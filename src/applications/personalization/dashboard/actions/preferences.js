257ddca80112328a79b476d1c12f0a66ac9dd315
Giles Wells <giles@mediagearhead.com>
2025-09-09 14:03:07 -0400

    Remove commented code

--- src/applications/personalization/dashboard/actions/preferences.js
+++ src/applications/personalization/dashboard/actions/preferences.js
@@ -3,12 +3,6 @@
 export const SET_MY_VA_LAYOUT_PREFERENCE = 'SET_MY_VA_LAYOUT_PREFERENCE';
 
 export const updateMyVaLayoutVersion = selectedVersion => {
-  // Record the event when a user changes
-  // const event = {
-  //   event: 'dashboard-myva-layout-version-changed',
-  //   'myva-layout-version': selectedVersion,
-  // };
-  // recordEvent(event);
   return {
     type: SET_MY_VA_LAYOUT_PREFERENCE,
     layout: {
