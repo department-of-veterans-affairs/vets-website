c1cbd8d8db1a6c9312744d8f96f0a3f0ff8bdb99
Giles Wells <giles@mediagearhead.com>
2025-09-09 13:43:54 -0400

    Remove mocked redesign flag default

--- src/applications/personalization/dashboard/mocks/server.js
+++ src/applications/personalization/dashboard/mocks/server.js
@@ -45,7 +45,6 @@ const responses = {
       myVaFormSubmissionStatuses: true,
       myVaFormPdfLink: true,
       veteranOnboardingShowWelcomeMessageToNewUsers: true,
-      myVaAuthExpRedesignEnabled: false,
       myVaAuthExpRedesignAvailableToOptIn: true,
     },
     true,
