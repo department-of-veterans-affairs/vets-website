937736f71fd78ede8f58c703a1228a9efbfd246e
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-09-11 15:17:45 -0500

    PR feedback

--- src/applications/appeals/onramp/tests/cypress/results-sc/path-cfi-4.cypress.spec.js
+++ src/applications/appeals/onramp/tests/cypress/results-sc/path-cfi-4.cypress.spec.js
@@ -7,26 +7,20 @@ import * as c from '../../../constants/results-content/dr-screens/card-content';
 const {
   Q_1_1_CLAIM_DECISION,
   Q_1_2_CLAIM_DECISION,
-  Q_1_3_CLAIM_CONTESTED,
-  Q_2_IS_1_SERVICE_CONNECTED,
-  Q_2_IS_2_CONDITION_WORSENED,
-  Q_2_IS_4_DISAGREE_DECISION,
-  Q_2_0_CLAIM_TYPE,
-  Q_2_S_1_NEW_EVIDENCE,
+  Q_1_2A_1_SERVICE_CONNECTED,
+  Q_1_2A_CONDITION_WORSENED,
+  Q_1_2A_2_DISAGREE_DECISION,
 } = SHORT_NAME_MAP;
 const { RESULTS_2_S_1B } = RESULTS_NAME_MAP;
 
 // Results SC: Supplemental Claim recommended (CFI)
 // 1.1 - Yes
-// 1.2 - Yes
-// 1.3 - No
-// 2.IS.1 - Yes
-// 2.IS.2 - Yes
-// 2.IS.4 - Yes
-// 2.0 - Board
-// 2.S.1 - Yes
+// 1.2 - No
+// 1.2A.1 - Yes
+// 1.2A - Yes
+// 1.2A.2 - Yes
 describe('Decision Reviews Onramp', () => {
-  describe('Results SC (CFI, path 3)', () => {
+  describe('Results SC (CFI, path 4)', () => {
     it('navigates through the flow forward and backward successfully', () => {
       cy.visit(h.ROOT);
 
@@ -42,37 +36,22 @@ describe('Decision Reviews Onramp', () => {
 
       // Q_1_2_CLAIM_DECISION
       h.verifyUrl(ROUTES.Q_1_2_CLAIM_DECISION);
-      h.selectRadio(Q_1_2_CLAIM_DECISION, 0);
+      h.selectRadio(Q_1_2_CLAIM_DECISION, 1);
       h.clickContinue();
 
-      // Q_1_3_CLAIM_CONTESTED
-      h.verifyUrl(ROUTES.Q_1_3_CLAIM_CONTESTED);
-      h.selectRadio(Q_1_3_CLAIM_CONTESTED, 1);
+      // Q_1_2A_1_SERVICE_CONNECTED
+      h.verifyUrl(ROUTES.Q_1_2A_1_SERVICE_CONNECTED);
+      h.selectRadio(Q_1_2A_1_SERVICE_CONNECTED, 0);
       h.clickContinue();
 
-      // Q_2_IS_1_SERVICE_CONNECTED
-      h.verifyUrl(ROUTES.Q_2_IS_1_SERVICE_CONNECTED);
-      h.selectRadio(Q_2_IS_1_SERVICE_CONNECTED, 0);
+      // Q_1_2A_CONDITION_WORSENED
+      h.verifyUrl(ROUTES.Q_1_2A_CONDITION_WORSENED);
+      h.selectRadio(Q_1_2A_CONDITION_WORSENED, 0);
       h.clickContinue();
 
-      // Q_2_IS_2_CONDITION_WORSENED
-      h.verifyUrl(ROUTES.Q_2_IS_2_CONDITION_WORSENED);
-      h.selectRadio(Q_2_IS_2_CONDITION_WORSENED, 0);
-      h.clickContinue();
-
-      // Q_2_IS_4_DISAGREE_DECISION
-      h.verifyUrl(ROUTES.Q_2_IS_4_DISAGREE_DECISION);
-      h.selectRadio(Q_2_IS_4_DISAGREE_DECISION, 0);
-      h.clickContinue();
-
-      // Q_2_0_CLAIM_TYPE
-      h.verifyUrl(ROUTES.Q_2_0_CLAIM_TYPE);
-      h.selectRadio(Q_2_0_CLAIM_TYPE, 3);
-      h.clickContinue();
-
-      // Q_2_S_1_NEW_EVIDENCE
-      h.verifyUrl(ROUTES.Q_2_S_1_NEW_EVIDENCE);
-      h.selectRadio(Q_2_S_1_NEW_EVIDENCE, 0);
+      // Q_1_2A_2_DISAGREE_DECISION
+      h.verifyUrl(ROUTES.Q_1_2A_2_DISAGREE_DECISION);
+      h.selectRadio(Q_1_2A_2_DISAGREE_DECISION, 0);
       h.clickContinue();
 
       // RESULTS
@@ -82,45 +61,23 @@ describe('Decision Reviews Onramp', () => {
       h.checkGoodFitCards([
         {
           type: c.CARD_SC,
-          content: [
-            c.CARD_REVIEW_BOARD,
-            c.CARD_NEW_EVIDENCE,
-            c.CARD_NOT_CONTESTED,
-            c.CARD_SUBMITTED_BOARD_APPEAL,
-          ],
+          content: [c.CARD_SUBMITTED_BOARD_APPEAL],
         },
       ]);
-      h.checkNotGoodFitCards([
-        {
-          type: c.CARD_HLR,
-          content: [c.CARD_CANNOT_SUBMIT_EVIDENCE],
-        },
-      ]);
-      h.verifyOutsideDROptionPresent();
+      h.verifyNotGoodFitCardsNotPresent();
+      h.verifyOutsideDROptionNotPresent();
       cy.go('back');
 
-      // Q_2_S_1_NEW_EVIDENCE
-      h.verifyUrl(ROUTES.Q_2_S_1_NEW_EVIDENCE);
+      // Q_1_2A_2_DISAGREE_DECISION
+      h.verifyUrl(ROUTES.Q_1_2A_2_DISAGREE_DECISION);
       h.clickBack();
 
-      // Q_2_0_CLAIM_TYPE
-      h.verifyUrl(ROUTES.Q_2_0_CLAIM_TYPE);
+      // Q_1_2A_CONDITION_WORSENED
+      h.verifyUrl(ROUTES.Q_1_2A_CONDITION_WORSENED);
       h.clickBack();
 
-      // Q_2_IS_4_DISAGREE_DECISION
-      h.verifyUrl(ROUTES.Q_2_IS_4_DISAGREE_DECISION);
-      h.clickBack();
-
-      // Q_2_IS_2_CONDITION_WORSENED
-      h.verifyUrl(ROUTES.Q_2_IS_2_CONDITION_WORSENED);
-      h.clickBack();
-
-      // Q_2_IS_1_SERVICE_CONNECTED
-      h.verifyUrl(ROUTES.Q_2_IS_1_SERVICE_CONNECTED);
-      h.clickBack();
-
-      // Q_1_3_CLAIM_CONTESTED
-      h.verifyUrl(ROUTES.Q_1_3_CLAIM_CONTESTED);
+      // Q_1_2A_1_SERVICE_CONNECTED
+      h.verifyUrl(ROUTES.Q_1_2A_1_SERVICE_CONNECTED);
       h.clickBack();
 
       // Q_1_2_CLAIM_DECISION
