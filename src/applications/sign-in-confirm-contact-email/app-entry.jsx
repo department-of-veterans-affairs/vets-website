b14e2879d4c6b20732a500e5a1f9c42c8fca9b8a
Caitlin <78328496+CaitHawk@users.noreply.github.com>
2025-09-12 10:30:02 -0700

    updates startAppFromIndex

--- src/applications/sign-in-confirm-contact-email/app-entry.jsx
+++ src/applications/sign-in-confirm-contact-email/app-entry.jsx
@@ -1,13 +1,13 @@
 import '@department-of-veterans-affairs/platform-polyfills';
 import './sass/confirm-contact-email.scss';
 
-import { startApp } from '@department-of-veterans-affairs/platform-startup/exports';
+import { startAppFromIndex } from '@department-of-veterans-affairs/platform-startup/exports';
 
 import routes from './routes';
 import reducer from './reducers';
 import manifest from './manifest.json';
 
-startApp({
+startAppFromIndex({
   entryName: manifest.entryName,
   url: manifest.rootUrl,
   reducer,
