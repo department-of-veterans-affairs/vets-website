d945ec86b1172384a91f85d38b35dcb7c1ad9769
govcioChad <162637891+govcioChad@users.noreply.github.com>
2025-09-12 14:24:41 -0700

    #vebt-2507 #comment Added aria live to age alert

--- src/applications/edu-benefits/10297/components/AgeEligibility.jsx
+++ src/applications/edu-benefits/10297/components/AgeEligibility.jsx
@@ -9,7 +9,7 @@ export default function AgeEligibility() {
     >
       <h2 slot="headline">You may not qualify</h2>
       <React.Fragment key=".1">
-        <p className="vads-u-margin-y--0">
+        <p className="vads-u-margin-y--0" aria-live="polite">
           If you’re over age 62, you may not be eligible for this program. You
           can still fill out the application. We’ll review your information and
           let you know if you don’t qualify.
