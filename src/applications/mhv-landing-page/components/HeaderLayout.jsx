b9deef3c524452b6ad3c77a8a8a4e23d27362dff
Daniel Cloud <279327+dcloud@users.noreply.github.com>
2025-09-12 16:58:51 -0400

    Add and use mhv_email_confirmation feature toggle, per department-of-veterans-affairs/vets-website/pull/38755

--- src/applications/mhv-landing-page/components/HeaderLayout.jsx
+++ src/applications/mhv-landing-page/components/HeaderLayout.jsx
@@ -3,6 +3,7 @@ import { datadogRum } from '@datadog/browser-rum';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import { VaLink } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import { useFeatureToggle } from '~/platform/utilities/feature-toggles';
 import recordEvent from '~/platform/monitoring/record-event';
 import WelcomeContainer from '../containers/WelcomeContainer';
 import { myVAHealthPortalLink } from '../utilities/data';
@@ -18,91 +19,99 @@ const ledeContent = `Welcome. You can now manage your health care
   here on VA.gov. Here, you’ll find new, improved versions of your trusted
   health tools and more features.`;
 
-const HeaderLayout = ({ showWelcomeMessage = false, isCerner = false }) => (
-  <>
-    <div
-      className={classnames(
-        'vads-u-display--flex',
-        'vads-u-justify-content--space-between',
-        'vads-u-margin-bottom--2',
-        'vads-u-align-items--flex-start',
-      )}
-      data-testid="mhv-header-layout--milestone-2"
-    >
-      <div className="vads-l-col medium-screen:vads-l-col--8">
-        <div className="vads-l-col">
-          <div className="vads-l-row">
-            <div className="vads-l-col-6 ">
-              <h1 className="vads-u-margin-y--0">My HealtheVet</h1>
+const HeaderLayout = ({ showWelcomeMessage = false, isCerner = false }) => {
+  const { useToggleValue, TOGGLE_NAMES } = useFeatureToggle();
+  const mhvEmailConfirmationEnabled = useToggleValue(
+    TOGGLE_NAMES.mhvEmailConfirmation,
+  );
+  return (
+    <>
+      <div
+        className={classnames(
+          'vads-u-display--flex',
+          'vads-u-justify-content--space-between',
+          'vads-u-margin-bottom--2',
+          'vads-u-align-items--flex-start',
+        )}
+        data-testid="mhv-header-layout--milestone-2"
+      >
+        <div className="vads-l-col medium-screen:vads-l-col--8">
+          <div className="vads-l-col">
+            <div className="vads-l-row">
+              <div className="vads-l-col-6 ">
+                <h1 className="vads-u-margin-y--0">My HealtheVet</h1>
+              </div>
             </div>
           </div>
+          {mhvEmailConfirmationEnabled && (
+            <div>
+              <ConfirmEmailLink />
+            </div>
+          )}
+          <div>
+            <p className="vads-u-font-family--serif vads-u-line-height--5 medium-screen:vads-u-font-size--lg medium-screen:vads-u-line-height--6 vads-u-margin-top--1 vads-u-margin-bottom--2">
+              {ledeContent}
+            </p>
+          </div>
+          {isCerner && (
+            <p className="vads-u-font-family--serif vads-u-line-height--5 medium-screen:vads-u-font-size--lg medium-screen:vads-u-line-height--6 vads-u-margin-y--2">
+              Does your facility use the My VA Health portal?
+              <br />
+              <a
+                onClick={() => {
+                  datadogRum.addAction('Click on My VA Health portal link');
+                  recordEvent({
+                    event: 'nav-link-click',
+                    action: 'click',
+                    'link-label': 'Go to the My VA Health portal',
+                    'link-destination': myVAHealthPortalLink,
+                    'link-origin': window.location.href,
+                  });
+                }}
+                className="vads-u-font-family--serif medium-screen:vads-u-font-size--lg"
+                data-testid="mhv-go-back-1"
+                href={myVAHealthPortalLink}
+              >
+                Go to the My VA Health portal
+              </a>
+            </p>
+          )}
+          <div>
+            <p className="vads-u-margin-y--2">
+              Want to learn more about what’s new? <VaLink {...learnMoreLink} />
+            </p>
+          </div>
         </div>
-        <div>
-          <ConfirmEmailLink />
-        </div>
-        <div>
-          <p className="vads-u-font-family--serif vads-u-line-height--5 medium-screen:vads-u-font-size--lg medium-screen:vads-u-line-height--6 vads-u-margin-top--1 vads-u-margin-bottom--2">
-            {ledeContent}
-          </p>
-        </div>
-        {isCerner && (
-          <p className="vads-u-font-family--serif vads-u-line-height--5 medium-screen:vads-u-font-size--lg medium-screen:vads-u-line-height--6 vads-u-margin-y--2">
-            Does your facility use the My VA Health portal?
-            <br />
-            <a
-              onClick={() => {
-                datadogRum.addAction('Click on My VA Health portal link');
-                recordEvent({
-                  event: 'nav-link-click',
-                  action: 'click',
-                  'link-label': 'Go to the My VA Health portal',
-                  'link-destination': myVAHealthPortalLink,
-                  'link-origin': window.location.href,
-                });
-              }}
-              className="vads-u-font-family--serif medium-screen:vads-u-font-size--lg"
-              data-testid="mhv-go-back-1"
-              href={myVAHealthPortalLink}
-            >
-              Go to the My VA Health portal
-            </a>
-          </p>
-        )}
-        <div>
-          <p className="vads-u-margin-y--2">
-            Want to learn more about what’s new? <VaLink {...learnMoreLink} />
-          </p>
+        <div
+          className={classnames(
+            'vads-u-display--none',
+            'medium-screen:vads-u-display--block',
+            'vads-l-col--4',
+            'vads-u-text-align--right',
+            'vads-u-margin-y--2p5',
+          )}
+        >
+          <img
+            src="/img/mhv-logo.png"
+            className="mhv-logo"
+            alt="My HealtheVet Logo"
+          />
         </div>
       </div>
-      <div
-        className={classnames(
-          'vads-u-display--none',
-          'medium-screen:vads-u-display--block',
-          'vads-l-col--4',
-          'vads-u-text-align--right',
-          'vads-u-margin-y--2p5',
-        )}
-      >
-        <img
-          src="/img/mhv-logo.png"
-          className="mhv-logo"
-          alt="My HealtheVet Logo"
-        />
-      </div>
-    </div>
-    {showWelcomeMessage && (
-      <div
-        className={classnames(
-          'vads-u-border-color--gray-light',
-          'vads-u-border-bottom--1px',
-          'vads-u-margin-bottom--3',
-        )}
-      >
-        <WelcomeContainer />
-      </div>
-    )}
-  </>
-);
+      {showWelcomeMessage && (
+        <div
+          className={classnames(
+            'vads-u-border-color--gray-light',
+            'vads-u-border-bottom--1px',
+            'vads-u-margin-bottom--3',
+          )}
+        >
+          <WelcomeContainer />
+        </div>
+      )}
+    </>
+  );
+};
 
 HeaderLayout.propTypes = {
   isCerner: PropTypes.bool,
