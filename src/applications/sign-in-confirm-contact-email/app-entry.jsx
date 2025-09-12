5c23ce3cc537ba6e03f1f6fda793c4d8ec401e88
Caitlin <78328496+CaitHawk@users.noreply.github.com>
2025-09-12 11:19:41 -0700

    removes unused files

--- src/applications/sign-in-confirm-contact-email/app-entry.jsx
+++ src/applications/sign-in-confirm-contact-email/app-entry.jsx
@@ -1,15 +1,12 @@
 import '@department-of-veterans-affairs/platform-polyfills';
-import './sass/confirm-contact-email.scss';
 
 import { startAppFromIndex } from '@department-of-veterans-affairs/platform-startup/exports';
 
 import routes from './routes';
-import reducer from './reducers';
 import manifest from './manifest.json';
 
 startAppFromIndex({
   entryName: manifest.entryName,
   url: manifest.rootUrl,
-  reducer,
   routes,
 });
