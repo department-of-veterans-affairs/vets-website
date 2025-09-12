2680750bdb216090c37b29e94fed43f25cef94f5
Liz Wendling <170662489+GovCIOLiz@users.noreply.github.com>
2025-09-12 07:19:45 -0700

    URL issue, update breadcrumbs, update browser title

--- src/applications/edu-benefits/10297/components/Breadcrumbs.jsx
+++ src/applications/edu-benefits/10297/components/Breadcrumbs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { VaBreadcrumbs } from '@department-of-veterans-affairs/web-components/react-bindings';
 import manifest from '../manifest.json';
+import { TITLE } from '../constants';
 
 const Breadcrumbs = () => {
   const crumbs = [
@@ -22,13 +23,17 @@ const Breadcrumbs = () => {
     },
     {
       href: manifest.rootUrl,
-      label: 'Apply for VET TEC 2.0',
+      label: TITLE,
     },
   ];
   return (
     <div className="row">
       <div className="vads-u-margin-left--2 mobile-lg:vads-u-margin-left--1">
-        <VaBreadcrumbs breadcrumbList={crumbs} data-testid="breadcrumbs" />
+        <VaBreadcrumbs
+          breadcrumbList={crumbs}
+          data-testid="breadcrumbs"
+          wrapping
+        />
       </div>
     </div>
   );
