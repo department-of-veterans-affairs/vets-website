a0353ef7d82dd52a3bd56bfc336f080774951755
Alison Jones <1816769+ajones446@users.noreply.github.com>
2025-09-15 14:15:29 -0500

    Do no show toxic exposure conditions if none present, add comments

--- src/applications/disability-benefits/all-claims/components/ConfirmationFields/ToxicExposureConditions.jsx
+++ src/applications/disability-benefits/all-claims/components/ConfirmationFields/ToxicExposureConditions.jsx
@@ -5,23 +5,28 @@ import { reviewEntry } from 'platform/forms-system/src/js/components/Confirmatio
 const ToxicExposureConditions = ({ formData }) => {
   const conditions = formData?.toxicExposure?.conditions || {};
 
+  // Return null if no conditions are present (either 'none' selected or specific conditions selected)
+  if (
+    !conditions.none &&
+    !Object.keys(conditions).some(
+      key => key !== 'none' && conditions[key] === true,
+    )
+  ) {
+    return null;
+  }
+
   const claimedKeys = Object.keys(conditions).filter(
     key => key !== 'none' && conditions[key] === true,
   );
+
+  // TODO - make toxic exposure conditions user-readable. Currently displays all in one word as the label as ptsdposttraumaticstressdisorder Claimed
+  // TODO - check with design/content on if it should say "claimed" or "selected" or anything else
 
   return (
     <li>
       <h4>Toxic Exposure </h4>
       <ul className="vads-u-padding--0" style={{ listStyle: 'none' }}>
-        {conditions?.none === true
-          ? reviewEntry(
-              null,
-              'toxicExposureNone',
-              {},
-              'Toxic exposure conditions',
-              'None claimed',
-            )
-          : claimedKeys.map(key => reviewEntry(null, key, {}, key, 'Claimed'))}
+        {claimedKeys.map(key => reviewEntry(null, key, {}, key, 'Claimed'))}
       </ul>
     </li>
   );
