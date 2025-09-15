2f5f359805250643548f9dd823e56c5a18965ea0
Ian <harrison.ian.t@gmail.com>
2025-09-15 15:38:19 -0500

    debugging...

--- src/platform/forms-system/src/js/utilities/navigation/navigationState.js
+++ src/platform/forms-system/src/js/utilities/navigation/navigationState.js
@@ -9,14 +9,26 @@ const navigationState = {
   setNavigationEvent() {
     this._navigationEvent = true;
     this._currentPath = window.location.href;
+    // eslint-disable-next-line no-console
+    console.log('SET navigation path:', this._currentPath);
 
     setTimeout(() => {
       this._navigationEvent = false;
       this._currentPath = null;
+      // eslint-disable-next-line no-console
+      console.log('CLEARED navigation state');
     }, 0);
   },
 
   getNavigationEventStatus() {
+    // eslint-disable-next-line no-console
+    console.log('CHECK navigation:', {
+      navigationEvent: this._navigationEvent,
+      storedPath: this._currentPath,
+      currentPath: window.location.href,
+      match:
+        this._navigationEvent && this._currentPath === window.location.href,
+    });
     // only return true if the navigation happened on the current page
     return this._navigationEvent && this._currentPath === window.location.href;
   },
