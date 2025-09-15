d43bfeb128ca10b2fc04ef93479a53fc0e7f9f73
sdbars <sami.barshoon@gmail.com>
2025-09-15 13:27:16 -0400

    added unit tests to confirmation, condition and conditions pages

--- src/applications/user-testing/disability-benefits-all-claims-conditions-chapter/pages/shared/causeNew.js
+++ src/applications/user-testing/disability-benefits-all-claims-conditions-chapter/pages/shared/causeNew.js
@@ -15,7 +15,7 @@ const causeNewPage = {
     ),
     primaryDescription: textareaUI({
       title:
-        'Briefly describe the injury, event, disease, or exposure that caused your condition. ',
+        'Briefly describe the exposure, event, injury, or onset of disease during your military service that caused your condition. ',
       hint:
         'For example, I operated loud machinery while in the service, and this caused me to lose my hearing.',
       updateUiSchema: (_formData, fullData, index) => ({
