548c207647ab2d92be128e9d4becfe068dca6192
Jun Tao Luo <john.luo@agile6.com>
2025-09-16 08:02:53 -0400

    Update TypeOfEyeCarePage

--- src/applications/vaos/tests/mocks/setup.js
+++ src/applications/vaos/tests/mocks/setup.js
@@ -170,11 +170,7 @@ export async function setTypeOfEyeCare(store, label) {
   const screen = renderWithStoreAndRouter(<TypeOfEyeCarePage />, { store });
   await screen.findByText(/Continue/i);
 
-  const radioSelector = screen.container.querySelector('va-radio');
-  const changeEvent = new CustomEvent('selected', {
-    detail: { value: label },
-  });
-  radioSelector.__events.vaValueChange(changeEvent);
+  fireEvent.click(await screen.findByLabelText(label));
   fireEvent.click(screen.getByText(/Continue/));
   await waitFor(() => expect(screen.history.push.called).to.be.true);
   await cleanup();
