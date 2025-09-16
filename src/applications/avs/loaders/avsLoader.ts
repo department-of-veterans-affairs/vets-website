06aa18e00fb788f679fc01b1cc67a87fc164591c
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-09-16 16:03:22 -0600

    Add AvsApiResponse type and update avsLoader to use typed response

--- src/applications/avs/loaders/avsLoader.ts
+++ src/applications/avs/loaders/avsLoader.ts
@@ -1,14 +1,14 @@
 import { defer } from 'react-router-dom-v5-compat';
 import { apiRequest } from '@department-of-veterans-affairs/platform-utilities/api';
 import { environment } from '@department-of-veterans-affairs/platform-utilities/exports';
-import type { RouteParams } from '../types';
+import type { RouteParams, AvsApiResponse } from '../types';
 
 const apiBasePath = `${environment.API_URL}/avs/v0`;
 
 export async function avsLoader({ params }: { params: RouteParams }) {
   const { id } = params;
   try {
-    const data = apiRequest(`${apiBasePath}/avs/${id}`).then(response => {
+    const data = apiRequest(`${apiBasePath}/avs/${id}`).then((response: AvsApiResponse) => {
       return response.data.attributes;
     });
     return defer({
--- src/applications/avs/types/index.ts
+++ src/applications/avs/types/index.ts
@@ -256,6 +256,21 @@ export interface AvsData {
   [key: string]: any;
 }
 
+// API Response types
+export interface ApiResponseData<T = any> {
+  id: string;
+  type: string;
+  attributes: T;
+}
+
+export interface ApiResponse<T = any> {
+  data: ApiResponseData<T>;
+}
+
+export interface AvsApiResponse extends ApiResponse<AvsData> {
+  data: ApiResponseData<AvsData>;
+}
+
 // Loader return type
 export interface AvsLoaderData {
   avs: AvsData;
