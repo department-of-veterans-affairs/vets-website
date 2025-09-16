851d2721a9235c315cfaa733c3507a9824978ad2
sdbars <sami.barshoon@gmail.com>
2025-09-16 12:11:06 -0400

    updated text for cause new and rated disabilitydate

--- src/applications/user-testing/disability-benefits-all-claims-conditions-chapter/pages/shared/causeNew.js
+++ src/applications/user-testing/disability-benefits-all-claims-conditions-chapter/pages/shared/causeNew.js
@@ -17,7 +17,7 @@ const causeNewPage = {
       title:
         'Briefly describe the exposure, event, injury, or onset of disease during your military service that caused your condition. ',
       hint:
-        'For example, I operated loud machinery while in the service, and this caused me to lose my hearing.',
+        'For example, "I operated loud machinery while in the service, and this caused me to lose my hearing."',
       updateUiSchema: (_formData, fullData, index) => ({
         'ui:title': `Briefly describe the injury, event, disease, or exposure that caused ${createNewConditionName(
           fullData?.[arrayBuilderOptions.arrayPath]?.[index],
