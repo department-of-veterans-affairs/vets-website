6ab251362c46e777d281fcbd740844131f4b1b8f
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-09-18 08:37:57 -0700

    Revise claim details downtime alert usage

--- src/applications/travel-pay/components/TravelClaimDetails.jsx
+++ src/applications/travel-pay/components/TravelClaimDetails.jsx
@@ -78,32 +78,34 @@ export default function TravelClaimDetails() {
     <Element name="topScrollElement">
       <article className="usa-grid-full vads-u-padding-bottom--0">
         <Breadcrumbs />
-        {error && <h1>There was an error loading the claim details.</h1>}
-        <DowntimeWindowAlert appTitle="Travel Pay" />
+        <DowntimeWindowAlert appTitle="Travel Pay">
+          <>
+            {error && <h1>There was an error loading the claim details.</h1>}
+            {data[id] && <ClaimDetailsContent {...data[id]} />}
+            <hr />
 
-        {data[id] && <ClaimDetailsContent {...data[id]} />}
-        <hr />
-
-        <div className="vads-u-margin-bottom--4">
-          <p>
-            If you’re eligible for reimbursement, we’ll deposit your
-            reimbursement in your bank account.
-          </p>
-          <va-link
-            href={REIMBURSEMENT_URL}
-            text="Learn how to set up direct deposit for travel pay reimbursement"
-          />
-          <p>
-            <strong>Note:</strong> Even if you already set up direct deposit for
-            your VA benefits, you’ll need to set up another direct deposit for
-            VA travel pay reimbursements.
-          </p>
-        </div>
-        <va-need-help>
-          <div slot="content">
-            <HelpTextManage />
-          </div>
-        </va-need-help>
+            <div className="vads-u-margin-bottom--4">
+              <p>
+                If you’re eligible for reimbursement, we’ll deposit your
+                reimbursement in your bank account.
+              </p>
+              <va-link
+                href={REIMBURSEMENT_URL}
+                text="Learn how to set up direct deposit for travel pay reimbursement"
+              />
+              <p>
+                <strong>Note:</strong> Even if you already set up direct deposit
+                for your VA benefits, you’ll need to set up another direct
+                deposit for VA travel pay reimbursements.
+              </p>
+            </div>
+            <va-need-help>
+              <div slot="content">
+                <HelpTextManage />
+              </div>
+            </va-need-help>
+          </>
+        </DowntimeWindowAlert>
       </article>
     </Element>
   );
