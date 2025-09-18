51b1a1b6e17b235fe3d0e0ce35984f7ac5a0ac0c
Adam Antonioli <132934480+adam-antonioli@users.noreply.github.com>
2025-09-17 21:09:49 -0400

    MBMS-92617 fix linting errors for pre-need integration form

--- src/applications/pre-need-integration/config/pages/veteranApplicantDetailsPreparer.jsx
+++ src/applications/pre-need-integration/config/pages/veteranApplicantDetailsPreparer.jsx
@@ -1,6 +1,5 @@
 import fullSchemaPreNeed from 'vets-json-schema/dist/40-10007-INTEGRATION-schema.json';
 
-import VaTextInputField from 'platform/forms-system/src/js/web-component-fields/VaTextInputField';
 import { ssnOrVaFileNumberNoHintSchema } from 'platform/forms-system/src/js/web-component-patterns';
 
 import { merge, pick } from 'lodash';
