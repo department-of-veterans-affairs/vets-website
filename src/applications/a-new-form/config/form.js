c788e8301c704f600d7544af668e622095ae0245
Robert Hasselle <robert.hasselle@oddball.io>
2025-09-18 17:17:51 -0500

    yarn new:app no changes

---
+++ src/applications/a-new-form/config/form.js
@@ -0,0 +1,68 @@
+import { minimalHeaderFormConfigOptions } from 'platform/forms-system/src/js/patterns/minimal-header';
+import footerContent from 'platform/forms/components/FormFooter';
+import { VA_FORM_IDS } from 'platform/forms/constants';
+import { TITLE, SUBTITLE } from '../constants';
+import manifest from '../manifest.json';
+import IntroductionPage from '../containers/IntroductionPage';
+import ConfirmationPage from '../containers/ConfirmationPage';
+
+import nameAndDateOfBirth from '../pages/nameAndDateOfBirth';
+
+/** @type {FormConfig} */
+const formConfig = {
+  rootUrl: manifest.rootUrl,
+  urlPrefix: '/',
+  submitUrl: '/v0/api',
+  submit: () =>
+    Promise.resolve({ attributes: { confirmationNumber: '123123123' } }),
+  trackingPrefix: 'a-new-form-',
+  introduction: IntroductionPage,
+  confirmation: ConfirmationPage,
+  dev: {
+    showNavLinks: true,
+    collapsibleNavLinks: true,
+  },
+  ...minimalHeaderFormConfigOptions({
+    breadcrumbList: [
+      { href: '/', label: 'VA.gov home' },
+      {
+        href: '/a-new-form',
+        label: 'A new form',
+      },
+    ],
+  }),
+  formId: VA_FORM_IDS.FORM_34_2344,
+  saveInProgress: {
+    // messages: {
+    //   inProgress: 'Your benefits application (34-2344) is in progress.',
+    //   expired: 'Your saved benefits application (34-2344) has expired. If you want to apply for benefits, please start a new application.',
+    //   saved: 'Your benefits application has been saved.',
+    // },
+  },
+  version: 0,
+  prefillEnabled: true,
+  savedFormMessages: {
+    notFound: 'Please start over to apply for benefits.',
+    noAuth: 'Please sign in again to continue your application for benefits.',
+  },
+  title: TITLE,
+  subTitle: SUBTITLE,
+  defaultDefinitions: {},
+  chapters: {
+    personalInformationChapter: {
+      title: 'Your personal information',
+      pages: {
+        nameAndDateOfBirth: {
+          path: 'name-and-date-of-birth',
+          title: 'Name and date of birth',
+          uiSchema: nameAndDateOfBirth.uiSchema,
+          schema: nameAndDateOfBirth.schema,
+        },
+      },
+    },
+  },
+  // getHelp,
+  footerContent,
+};
+
+export default formConfig;
