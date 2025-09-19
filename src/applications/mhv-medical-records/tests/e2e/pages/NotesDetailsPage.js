05f9e6897151b2e319d649d6fe04158ee90ebdba
Carlos Felix <carlos.felix@agile6.com>
2025-09-19 10:12:44 -0400

    Refactored one find

--- src/applications/mhv-medical-records/tests/e2e/pages/NotesDetailsPage.js
+++ src/applications/mhv-medical-records/tests/e2e/pages/NotesDetailsPage.js
@@ -4,12 +4,11 @@ import BaseDetailsPage from './BaseDetailsPage';
 class NotesDetailsPage extends BaseDetailsPage {
   clickProgressNoteLink = (progressNoteHeading, index = 0) => {
     // First make sure the heading is visible
-    cy.contains(progressNoteHeading, { includeShadowDom: true }).then(() => {
-      cy.findAllByTestId('note-name')
-        .filter(`:contains("${progressNoteHeading}")`)
-        .eq(index)
-        .click();
-    });
+    cy.contains(progressNoteHeading, { includeShadowDom: true });
+    cy.findAllByTestId('note-name')
+      .filter(`:contains("${progressNoteHeading}")`)
+      .eq(index)
+      .click();
   };
 
   clickDischargeSummaryLink = (index = 0) => {
