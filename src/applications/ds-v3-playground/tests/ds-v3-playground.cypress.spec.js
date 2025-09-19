eae908b6d8911d77e78b4394d7ed790193d878f6
Jami Gibbs <jami.gibbs@adhocteam.us>
2025-09-19 13:47:12 -0500

    try test update again

--- src/applications/ds-v3-playground/tests/ds-v3-playground.cypress.spec.js
+++ src/applications/ds-v3-playground/tests/ds-v3-playground.cypress.spec.js
@@ -1,18 +1,17 @@
 /* eslint-disable @department-of-veterans-affairs/axe-check-required */
-import manifest from '../manifest.json';
 
-describe(manifest.appName, () => {
+describe('DS V3 Playground', () => {
   before(() => {
     if (Cypress.env('CI')) this.skip();
   });
 
   it('is accessible', () => {
-    cy.visit(manifest.rootUrl);
+    cy.visit('/ds-v3-playground');
     cy.injectAxeThenAxeCheck();
   });
 
   it('loads the playground page successfully', () => {
-    cy.visit(manifest.rootUrl);
+    cy.visit('/ds-v3-playground');
     cy.get('h1').should('contain.text', 'VA Design System Component Demos');
   });
 });
