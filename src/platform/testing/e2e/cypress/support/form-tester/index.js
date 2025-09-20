8c2ad760adbc2349ecb59344a13df7a2a01e3856
Robert Hasselle <robert.hasselle@oddball.io>
2025-09-19 19:05:57 -0500

    Make address pattern auto fill in cypress

--- src/platform/testing/e2e/cypress/support/form-tester/index.js
+++ src/platform/testing/e2e/cypress/support/form-tester/index.js
@@ -3,6 +3,11 @@ import path from 'path';
 import get from 'platform/utilities/data/get';
 
 import disableFTUXModals from 'platform/user/tests/disableFTUXModals';
+import {
+  processPatternFieldGroups,
+  fillIndividualFields,
+  handleArrayItemAddition,
+} from './utilities';
 
 const APP_SELECTOR = '#react-root';
 const ARRAY_ITEM_SELECTOR =
@@ -520,6 +525,7 @@ Cypress.Commands.add('fillPage', () => {
     .then(({ arrayItemPath }) => {
       const touchedFields = new Set();
       const snapshot = {};
+      let fillAvailableFields;
 
       /**
        * Fills out a field (or set of fields) using the created field object,
@@ -551,43 +557,59 @@ Cypress.Commands.add('fillPage', () => {
         });
       };
 
-      const fillAvailableFields = () => {
+      const countFormElements = (fieldSelector, $form) => {
+        snapshot.arrayItemCount = $form.find(ARRAY_ITEM_SELECTOR).length;
+        snapshot.fieldCount = $form.find(fieldSelector).length;
+      };
+
+      const fillFormPatterns = () => {
+        return cy.get(APP_SELECTOR, NO_LOG_OPTION).then($form => {
+          return processPatternFieldGroups($form, arrayItemPath, touchedFields);
+        });
+      };
+
+      const fillFormFields = fieldSelector => {
+        return cy
+          .get(APP_SELECTOR, NO_LOG_OPTION)
+          .within(NO_LOG_OPTION, $form => {
+            return fillIndividualFields(
+              $form,
+              fieldSelector,
+              processFieldObject,
+              createFieldObject,
+            ).then(() => {
+              handleArrayItemAddition(
+                $form,
+                fieldSelector,
+                snapshot,
+                addNewArrayItem,
+              );
+
+              return cy.wrap($form, NO_LOG_OPTION);
+            });
+          });
+      };
+
+      const checkForAdditionalFields = fieldSelector => {
+        return cy.get(APP_SELECTOR, NO_LOG_OPTION).then($form => {
+          const { arrayItemCount, fieldCount } = snapshot;
+          const fieldsNeedInput =
+            arrayItemCount !== $form.find(ARRAY_ITEM_SELECTOR).length ||
+            fieldCount !== $form.find(fieldSelector).length;
+
+          if (fieldsNeedInput) {
+            fillAvailableFields();
+          }
+        });
+      };
+
+      fillAvailableFields = () => {
         getFieldSelectors().then(fieldSelector => {
           cy.get(APP_SELECTOR, NO_LOG_OPTION)
-            .then($form => {
-              // Get the starting number of array items and fields to compare
-              // after filling out all currently visible fields, as new fields
-              // may get added or expanded after this iteration.
-              snapshot.arrayItemCount = $form.find(ARRAY_ITEM_SELECTOR).length;
-              snapshot.fieldCount = $form.find(fieldSelector).length;
-            })
-            .within(NO_LOG_OPTION, $form => {
-              // Fill out every field that's currently on the page.
-              const fields = $form.find(fieldSelector);
-              if (!fields.length) return;
-              cy.wrap(fields).each(element => {
-                cy.wrap(createFieldObject(element), NO_LOG_OPTION).then(
-                  processFieldObject,
-                );
-              });
-
-              // Once all currently visible fields have been filled, add an array
-              // item if there are more to be added according to the test data.
-              if (snapshot.fieldCount === $form.find(fieldSelector).length) {
-                addNewArrayItem($form);
-              }
-
-              cy.wrap($form, NO_LOG_OPTION);
-            })
-            .then($form => {
-              // If there are new array items or fields to be filled,
-              // iterate through the page again.
-              const { arrayItemCount, fieldCount } = snapshot;
-              const fieldsNeedInput =
-                arrayItemCount !== $form.find(ARRAY_ITEM_SELECTOR).length ||
-                fieldCount !== $form.find(fieldSelector).length;
-              if (fieldsNeedInput) fillAvailableFields();
-            });
+            .then($form => countFormElements(fieldSelector, $form))
+            .then(() => fillFormPatterns())
+            .then(() => fillFormFields(fieldSelector))
+            .then(() => checkForAdditionalFields(fieldSelector));
         });
       };
 
