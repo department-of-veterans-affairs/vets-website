8fea47041af70aadf04b8d2b943bb9690e03f9a5
OptionSelect <29178824+OptionSelect@users.noreply.github.com>
2025-09-22 14:37:35 -0400

    Update titles per convo with Marcela and change zero alert to paragraphs

--- src/applications/combined-debt-portal/combined/components/ComboAlerts.jsx
+++ src/applications/combined-debt-portal/combined/components/ComboAlerts.jsx
@@ -90,7 +90,37 @@ ComboAlert.Zero = () => {
 const ComboAlerts = ({ alertType }) => {
   switch (alertType) {
     case ALERT_TYPES.ZERO:
-      return <ComboAlert.Zero />;
+      return (
+        <>
+          <h3>You don’t have any current VA debt or copay bills</h3>
+          <p>
+            Our records show you don’t have any current VA benefit debt and you
+            haven’t received a copay bill in the past 6 months.
+          </p>
+          <h3>What to do if you think you have a VA debt or copay bill</h3>
+          <div>
+            <p>
+              For benefit debts, call the Debt Management Center (DMC) at{' '}
+              <va-telephone contact={CONTACTS.DMC} /> (
+              <va-telephone tty contact="711" />
+              ). We’re here Monday through Friday, 7:30 a.m. to 7:00 p.m. ET.
+            </p>
+            <p>
+              For medical copay bills, call the VA Health Resource Center at{' '}
+              <va-telephone contact={CONTACTS.HEALTH_RESOURCE_CENTER} /> (
+              <va-telephone contact={CONTACTS[711]} tty />
+              ). We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
+            </p>{' '}
+            <va-link
+              active
+              class="vads-u-margin-top--2"
+              data-testid="return-to-va-link"
+              href="https://va.gov"
+              text="Return to VA.gov"
+            />
+          </div>
+        </>
+      );
     case ALERT_TYPES.ERROR:
     default:
       recordEvent({
