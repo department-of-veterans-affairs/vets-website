34780100f3340f774ebf8078b0f49e519c41e2f6
OptionSelect <29178824+OptionSelect@users.noreply.github.com>
2025-09-22 15:26:10 -0400

    Create shared component.

--- src/applications/combined-debt-portal/debt-letters/containers/DebtLettersSummary.jsx
+++ src/applications/combined-debt-portal/debt-letters/containers/DebtLettersSummary.jsx
@@ -14,6 +14,7 @@ import DebtCardsList from '../components/DebtCardsList';
 import OtherVADebts from '../../combined/components/OtherVADebts';
 import alertMessage from '../../combined/utils/alert-messages';
 import useHeaderPageTitle from '../../combined/hooks/useHeaderPageTitle';
+import ZeroDebtsCopaysSection from '../../combined/components/ZeroDebtsCopaysSection';
 
 const renderAlert = (alertType, statements) => {
   const alertInfo = alertMessage(alertType, APP_TYPES.DEBT);
@@ -21,35 +22,7 @@ const renderAlert = (alertType, statements) => {
   const showVAReturnLink = !showOther && alertType !== ALERT_TYPES.ALL_ERROR;
 
   return alertType === ALERT_TYPES.ALL_ZERO ? (
-    <>
-      <h3>You don’t have any current VA debt or copay bills</h3>
-      <p>
-        Our records show you don’t have any current VA benefit debt and you
-        haven’t received a copay bill in the past 6 months.
-      </p>
-      <h3>What to do if you think you have a VA debt or copay bill</h3>
-      <div>
-        <p>
-          For benefit debts, call the Debt Management Center (DMC) at{' '}
-          <va-telephone contact={CONTACTS.DMC} /> (
-          <va-telephone tty contact="711" />
-          ). We’re here Monday through Friday, 7:30 a.m. to 7:00 p.m. ET.
-        </p>
-        <p>
-          For medical copay bills, call the VA Health Resource Center at{' '}
-          <va-telephone contact={CONTACTS.HEALTH_RESOURCE_CENTER} /> (
-          <va-telephone contact={CONTACTS[711]} tty />
-          ). We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
-        </p>{' '}
-      </div>
-      <va-link
-        active
-        class="vads-u-margin-top--2"
-        data-testid="return-to-va-link"
-        href="https://va.gov"
-        text="Return to VA.gov"
-      />
-    </>
+    <ZeroDebtsCopaysSection />
   ) : (
     <va-alert data-testid={alertInfo.testID} status={alertInfo.alertStatus}>
       <h2 className="vads-u-font-size--h3" slot="headline">
