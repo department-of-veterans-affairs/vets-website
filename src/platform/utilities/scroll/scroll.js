b096810ff5bb0769e84fd25b1a84b11941ad4753
Robert Hasselle <robert.hasselle@oddball.io>
2025-09-22 09:28:08 -0500

    fix optional default

--- src/platform/utilities/scroll/scroll.js
+++ src/platform/utilities/scroll/scroll.js
@@ -49,10 +49,10 @@ export const scrollToElement = async (el, scrollOptions) =>
 
 /**
  * Scroll to top of the page
- * @param {String|Number|Element} [el]='topScrollElement' - top scroll element, or
+ * @param {String|Number|Element} [position='topScrollElement'] - top scroll element, or
  *  selector, id, name, class name, number, or DOM element to position
  * @param {ScrollOptions} [scrollOptions] - settings & overrides
- * @returns Promise
+ * @returns {Promise<void>}
  */
 export const scrollToTop = async (
   position = `top${SCROLL_ELEMENT_SUFFIX}`,
