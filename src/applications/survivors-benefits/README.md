e6eba328c2b0d2bdb65650969c2fc1d127ed220a
Daniel Gading <2748839+dgading@users.noreply.github.com>
2025-09-23 14:26:13 -0400

    Add new generated form for 534EZ

---
+++ src/applications/survivors-benefits/README.md
@@ -0,0 +1,7 @@
+# 534EZ Survivors Benefits
+
+## Commands:
+Site: http://localhost:3001/supporting-forms-for-claims/apply-form-21p-534ez/
+Watch: yarn watch --env entry=survivors-benefits
+Mock API: yarn mock-api --responses src/applications/survivors-benefits/tests/fixtures/mocks/local-mock-responses.js
+Unit test: yarn test:unit --app-folder survivors-benefits --log-level all
--- src/platform/forms/constants.js
+++ src/platform/forms/constants.js
@@ -112,6 +112,7 @@ export const VA_FORM_IDS = Object.freeze({
   FORM_21_0779: '21-0779',
   FORM_21P_530A: '21P-530A',
   FORM_21P_0537: '21P-0537',
+  FORM_21P_534EZ: '21P-534EZ',
 });
 
 export const VA_FORM_IDS_SKIP_INFLECTION = Object.freeze([
@@ -214,6 +215,7 @@ export const getAllFormLinks = getAppUrlImpl => {
     [VA_FORM_IDS.FORM_21_0779]: `${tryGetAppUrl('21-0779')}/`,
     [VA_FORM_IDS.FORM_21P_530A]: `${tryGetAppUrl('21P-530A')}/`,
     [VA_FORM_IDS.FORM_21P_0537]: `${tryGetAppUrl('21P-0537')}/`,
+    [VA_FORM_IDS.FORM_21P_534EZ]: `${tryGetAppUrl('21P-534EZ')}/`,
   };
 };
 
@@ -820,6 +822,13 @@ export const MY_VA_SIP_FORMS = [
     description:
       'marital status questionnaire for DIC recipients application (21P-0537)',
     trackingPrefix: '21p-0537-dic-marital-status-',
+  },
+  {
+    id: VA_FORM_IDS.FORM_21P_534EZ,
+    benefit: 'benefits',
+    title: '534EZ Survivors benefits',
+    description: 'benefits',
+    trackingPrefix: 'survivors-534ez',
   },
 ];
 
