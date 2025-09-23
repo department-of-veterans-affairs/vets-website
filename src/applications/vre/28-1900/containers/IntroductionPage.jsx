07685d1357a078deb405da92832a2af9b9c02d08
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-09-23 10:18:05 -0700

    final url revision

--- src/applications/vre/28-1900/containers/IntroductionPage.jsx
+++ src/applications/vre/28-1900/containers/IntroductionPage.jsx
@@ -19,7 +19,7 @@ const ProcessList = () => {
           Make sure you meet our eligibility requirements before you apply.
         </p>
         <va-link
-          href="/careers-employment/vocational-rehabilitation/eligibility"
+          href="/careers-employment/vocational-rehabilitation/eligibility/"
           text="Find out if you’re eligible for VR&E benefits"
         />
       </va-process-list-item>
