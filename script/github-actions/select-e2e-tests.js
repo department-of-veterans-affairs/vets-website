e3644d757c8b737e549f98798dee8d71e94cc367
Isaac Gallup <igallup@thesocompany.com>
2025-09-23 16:08:08 -0500

    again

--- script/github-actions/select-e2e-tests.js
+++ script/github-actions/select-e2e-tests.js
@@ -56,10 +56,11 @@ function selectTests(pathsOfChangedFiles) {
         tests.push(...glob.sync(selectedTestsPattern));
       });
 
-    // Custom logic needed to ensure that changes to the array builder inside the
-    // platform directory trigger the e2e specs that live in the simple forms app
-
+      // Custom logic needed to ensure that changes to the array builder inside the
+      // platform directory trigger the e2e specs that live in the simple forms app
+      
       const ARRAY_BUILDER = 'src/platform/forms-system/src/js/patterns/array-builder';
+      
       if (filteredChangedFiles.some(p => p.includes(ARRAY_BUILDER))) {
         const neededSimpleFormsPattern = path.join(
           __dirname,
