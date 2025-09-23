a830f9f2c1e4ef879b8a2ca02d34d3b7645e8cc5
Chris Kim <42885441+chriskim2311@users.noreply.github.com>
2025-09-23 10:31:30 -0700

    lint

--- src/applications/static-pages/i18Select/tests/getNonActiveLinkUrls.unit.spec.jsx
+++ src/applications/static-pages/i18Select/tests/getNonActiveLinkUrls.unit.spec.jsx
@@ -10,7 +10,6 @@ describe('getNonActiveLinkUrls util', () => {
     });
 
     expect(result.length).to.equal(34);
-
   });
 
   it('should not return any "-esp" suffixed links when "es" is the active language code', () => {
@@ -21,7 +20,6 @@ describe('getNonActiveLinkUrls util', () => {
     });
 
     expect(result.length).to.equal(34);
-
   });
 
   it('should not return any "-tag" suffixed links when "tl" is the active language code', () => {
@@ -32,6 +30,5 @@ describe('getNonActiveLinkUrls util', () => {
     });
 
     expect(result.length).to.equal(56);
-
   });
 });
