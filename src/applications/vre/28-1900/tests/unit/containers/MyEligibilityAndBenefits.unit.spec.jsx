9a9a25cd5510da49bc32e98109f94c0235d99df7
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-09-23 09:41:00 -0700

    update page title to pass unit test

--- src/applications/vre/28-1900/tests/unit/containers/MyEligibilityAndBenefits.unit.spec.jsx
+++ src/applications/vre/28-1900/tests/unit/containers/MyEligibilityAndBenefits.unit.spec.jsx
@@ -3,10 +3,10 @@ import { render } from '@testing-library/react';
 import { expect } from 'chai';
 import MyEligibilityAndBenefits from '../../../containers/MyEligibilityAndBenefits';
 
-describe('CheckEligibilityAndApply', () => {
+describe('MyEligibilityAndBenefits', () => {
   it('renders without crashing', () => {
     const { getByText } = render(<MyEligibilityAndBenefits />);
 
-    expect(getByText('Check your eligibility and apply')).to.exist;
+    expect(getByText('My eligibility and benefits')).to.exist;
   });
 });
