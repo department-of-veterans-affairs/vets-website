738dfe9fd8e50dd46d3ffd13ac0d97f83b25f737
Alex Morgun <87077843+alex-morgun-1@users.noreply.github.com>
2025-09-24 10:29:33 -0400

    Add ohTriageGroup attribute handling in RecentCareTeams component and tests

--- src/applications/mhv-secure-messaging/tests/containers/RecentCareTeams.unit.spec.jsx
+++ src/applications/mhv-secure-messaging/tests/containers/RecentCareTeams.unit.spec.jsx
@@ -7,6 +7,8 @@ import sinon from 'sinon';
 import RecentCareTeams from '../../containers/RecentCareTeams';
 import reducer from '../../reducers';
 import * as Constants from '../../util/constants';
+import { selectVaRadio } from '../../util/testUtils';
+import * as threadDetailsActions from '../../actions/threadDetails';
 
 const { Paths } = Constants;
 
@@ -202,6 +204,71 @@ describe('RecentCareTeams component', () => {
 
       // Component should still render
       expect(screen.getByText('Recent care teams')).to.exist;
+    });
+
+    it('should dispatch ohTriageGroup attribute for care system', async () => {
+      const customState = {
+        ...defaultState,
+        sm: {
+          ...defaultState.sm,
+          recipients: {
+            recentRecipients: [
+              {
+                triageTeamId: 636,
+                name: 'VA Boston',
+                healthCareSystemName: 'Test Facility 1',
+                stationNumber: '636',
+                ohTriageGroup: true,
+              },
+              {
+                triageTeamId: 662,
+                name: 'VA Seattle',
+                healthCareSystemName: 'Test Facility 2',
+                stationNumber: '662',
+                ohTriageGroup: false,
+              },
+            ],
+          },
+        },
+      };
+      const updateDraftInProgressStub = sandbox.stub(
+        threadDetailsActions,
+        'updateDraftInProgress',
+      );
+      const screen = renderComponent(customState);
+
+      expect(screen.getByText('Recent care teams')).to.exist;
+
+      // await waitFor(() => {
+      selectVaRadio(screen.container, '636');
+      // });
+      // await waitFor(() => {
+      //   expect(updateDraftInProgressStub.called).to.be.true;
+      // });
+      await waitFor(() => {
+        const callArgs = updateDraftInProgressStub.lastCall.args[0];
+
+        expect(callArgs).to.include({
+          recipientId: '636',
+          recipientName: 'VA Boston',
+          careSystemVhaId: '636',
+          careSystemName: 'Test Facility 1',
+          ohTriageGroup: true,
+        });
+      });
+
+      selectVaRadio(screen.container, '662');
+      await waitFor(() => {
+        const callArgs = updateDraftInProgressStub.lastCall.args[0];
+
+        expect(callArgs).to.include({
+          careSystemName: 'Test Facility 2',
+          careSystemVhaId: '662',
+          ohTriageGroup: false,
+          recipientId: '662',
+          recipientName: 'VA Seattle',
+        });
+      });
     });
   });
 
