acabd77f7f141ae16590a08c5a458150fbea08da
Alex Morgun <87077843+alex-morgun-1@users.noreply.github.com>
2025-09-24 13:51:13 -0400

    Add ohTriageGroup to recipient state updates in RecipientsSelect; enhance unit tests to verify selection behavior

--- src/applications/mhv-secure-messaging/components/ComposeForm/RecipientsSelect.jsx
+++ src/applications/mhv-secure-messaging/components/ComposeForm/RecipientsSelect.jsx
@@ -179,6 +179,7 @@ const RecipientsSelect = ({
         updateDraftInProgress({
           recipientName: recipient.name,
           recipientId: recipient.id,
+          ohTriageGroup: recipient.ohTriageGroup,
         }),
       );
 
--- src/applications/mhv-secure-messaging/tests/components/Compose/RecipientsSelect.unit.spec.jsx
+++ src/applications/mhv-secure-messaging/tests/components/Compose/RecipientsSelect.unit.spec.jsx
@@ -8,6 +8,7 @@ import reducer from '../../../reducers';
 import RecipientsSelect from '../../../components/ComposeForm/RecipientsSelect';
 import * as Constants from '../../../util/constants';
 import { selectVaSelect } from '../../../util/testUtils';
+import * as threadDetailsActions from '../../../actions/threadDetails';
 
 describe('RecipientsSelect', () => {
   const initialState = {
@@ -46,12 +47,14 @@ describe('RecipientsSelect', () => {
       name: 'Recipient 1',
       stationNumber: '552',
       signatureRequired: true,
+      ohTriageGroup: true,
     },
     {
       id: 2,
       name: 'Recipient 2',
       stationNumber: '402',
       signatureRequired: false,
+      ohTriageGroup: false,
     },
   ];
 
@@ -229,5 +232,34 @@ describe('RecipientsSelect', () => {
     expect(options).to.have.lengthOf(2);
     expect(options[0].textContent).to.equal('Recipient 1');
     expect(options[1].textContent).to.equal('Recipient 2');
+  });
+
+  it('dispatches selected recipient to state on selection', () => {
+    const sandbox = sinon.createSandbox();
+    const threadDetailsSpy = sandbox.spy(
+      threadDetailsActions,
+      'updateDraftInProgress',
+    );
+    const screen = setup({});
+    selectVaSelect(screen.container, '1');
+
+    waitFor(() => {
+      expect(threadDetailsSpy.calledOnce).to.be.true;
+      expect(threadDetailsSpy.lastCall.args[0]).to.deep.equal({
+        ohTriageGroup: true,
+        recipientId: 1,
+        recipientName: 'Recipient 1',
+      });
+    });
+    selectVaSelect(screen.container, '2');
+    waitFor(() => {
+      expect(threadDetailsSpy.calledTwice).to.be.true;
+      expect(threadDetailsSpy.lastCall.args[0]).to.deep.equal({
+        ohTriageGroup: false,
+        recipientId: 2,
+        recipientName: 'Recipient 2',
+      });
+    });
+    sandbox.restore();
   });
 });
