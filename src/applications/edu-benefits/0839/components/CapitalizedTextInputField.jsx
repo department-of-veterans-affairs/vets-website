8b3677e0d45588f5c0ebea7446611c7972f95810
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-09-23 18:44:36 -0700

    refactor intials input and uiSchema

---
+++ src/applications/edu-benefits/0839/components/CapitalizedTextInputField.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { VaTextInput } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import vaTextInputFieldMapping from 'platform/forms-system/src/js/web-component-fields/vaTextInputFieldMapping';
+
+/**
+ * A VaTextInputField that automatically capitalizes text on blur
+ * @param {WebComponentFieldProps} props
+ */
+
+export default function CapitalizedTextInputField(props) {
+  const mappedProps = vaTextInputFieldMapping(props);
+  const [displayValue, setDisplayValue] = useState(mappedProps.value || '');
+
+  const handleBlur = event => {
+    const currentValue = event.target.value;
+    const capitalizedValue = currentValue.toUpperCase();
+
+    setDisplayValue(capitalizedValue);
+
+    if (mappedProps.onBlur) {
+      mappedProps.onBlur();
+    }
+
+    if (mappedProps.onInput) {
+      mappedProps.onInput(event, capitalizedValue);
+    }
+  };
+
+  const handleInput = (event, value) => {
+    const inputValue = value || event.target.value;
+    setDisplayValue(inputValue);
+
+    if (mappedProps.onInput) {
+      mappedProps.onInput(event, value);
+    }
+  };
+
+  return (
+    <VaTextInput
+      {...mappedProps}
+      value={displayValue}
+      onInput={handleInput}
+      onBlur={handleBlur}
+    />
+  );
+}
--- src/applications/edu-benefits/0839/pages/acknowledgements.js
+++ src/applications/edu-benefits/0839/pages/acknowledgements.js
@@ -1,23 +1,35 @@
+import React from 'react';
 import {
   titleUI,
+  textUI,
   textSchema,
   checkboxUI,
   checkboxRequiredSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
-import CapitalizingTextInputField from '../components/CapitalizingTextInput';
+import CapitalizedTextInputField from '../components/CapitalizedTextInputField';
 
 const uiSchema = {
   ...titleUI(
     'Initial each statement to acknowledge the Yellow Ribbon Program terms',
   ),
+
+  'view:additionalInstructions': {
+    'ui:description': (
+      <va-link
+        text="Review additional instructions for the Yellow Ribbon Program Agreement "
+        href="/school-administrators/submit-yellow-ribbon-program-agreement-form-22-0839/yellow-ribbon-instructions"
+        external
+      />
+    ),
+  },
+
   'view:statement1': {
     'ui:description':
       'The Institution of Higher Learning (IHL) agrees that this Yellow Ribbon Program agreement is open-ended and will continue until terminated by either party. The IHL agrees to provide contributions to eligible individuals during the enrollment periods of March 15 through May 15 for U.S. schools and June 1 through July 31 for foreign schools, or the Monday following these dates if they fall on a weekend. The IHL agrees that any modifications to this agreement will be posted on www.gibill.va.gov.',
   },
   statement1Initial: {
-    'ui:title': 'Initial here',
-    'ui:autocomplete': 'off',
-    'ui:webComponentField': CapitalizingTextInputField,
+    ...textUI('Initial here'),
+    'ui:webComponentField': CapitalizedTextInputField,
     'ui:options': {
       width: 'small',
       classNames: 'vads-u-margin-bottom--6',
@@ -31,9 +43,8 @@ const uiSchema = {
       'The IHL agrees to provide contributions to eligible individuals on a first-come-first-serve basis. The IHL agrees that contributions may be provided from any source not already allocated for non-Yellow Ribbon purposes. The IHL agrees that student ledgers will denote contributions as "Yellow Ribbon," not as grants or scholarships.',
   },
   statement2Initial: {
-    'ui:title': 'Initial here',
-    'ui:autocomplete': 'off',
-    'ui:webComponentField': CapitalizingTextInputField,
+    ...textUI('Initial here'),
+    'ui:webComponentField': CapitalizedTextInputField,
     'ui:options': {
       width: 'small',
       classNames: 'vads-u-margin-bottom--6',
@@ -47,9 +58,8 @@ const uiSchema = {
       'The IHL agrees to provide contributions for participating individuals during the current academic year and subsequent academic years, provided the individual maintains satisfactory progress, conduct, and attendance. The IHL agrees that if an agreement modification reduces the contribution, the IHL will maintain the prior amount for individuals in good standing who previously received Yellow Ribbon benefits.',
   },
   statement3Initial: {
-    'ui:title': 'Initial here',
-    'ui:autocomplete': 'off',
-    'ui:webComponentField': CapitalizingTextInputField,
+    ...textUI('Initial here'),
+    'ui:webComponentField': CapitalizedTextInputField,
     'ui:options': {
       width: 'small',
       classNames: 'vads-u-margin-bottom--6',
@@ -63,9 +73,8 @@ const uiSchema = {
       'The IHL agrees that the maximum amount of contributions payable toward the net cost for each participating individual per term, quarter, or semester will not exceed the maximum dollar amount payable during the academic year.',
   },
   statement4Initial: {
-    'ui:title': 'Initial here',
-    'ui:autocomplete': 'off',
-    'ui:webComponentField': CapitalizingTextInputField,
+    ...textUI('Initial here'),
+    'ui:webComponentField': CapitalizedTextInputField,
     'ui:options': {
       width: 'small',
       classNames: 'vads-u-margin-bottom--6',
@@ -93,6 +102,7 @@ const schema = {
     'agreementCheckbox',
   ],
   properties: {
+    'view:additionalInstructions': { type: 'object', properties: {} },
     'view:statement1': { type: 'object', properties: {} },
     statement1Initial: {
       ...textSchema,
