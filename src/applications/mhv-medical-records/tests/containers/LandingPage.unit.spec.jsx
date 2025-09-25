b85227ed36205cdbc1cfeae2c009244d8e79365c
Napoleon K <95312667+GovNapoleon@users.noreply.github.com>
2025-09-25 10:40:22 -0500

    119746 remove skip from it('should log medical records accessed event when landing page loads'

--- src/applications/mhv-medical-records/tests/containers/LandingPage.unit.spec.jsx
+++ src/applications/mhv-medical-records/tests/containers/LandingPage.unit.spec.jsx
@@ -303,7 +303,7 @@ describe('Landing Page', () => {
       });
     });
 
-    it.skip('should log medical records accessed event when landing page loads', () => {
+    it('should log medical records accessed event when landing page loads', () => {
       expect(
         logUniqueUserMetricsEventsStub.calledWith(
           uniqueUserMetrics.EVENT_REGISTRY.MEDICAL_RECORDS_ACCESSED,
