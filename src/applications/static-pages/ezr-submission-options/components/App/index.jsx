4b76304bb97ca67f315ccf28002c872c852baf70
Matt Long <6738544+longmd@users.noreply.github.com>
2025-09-26 14:10:05 -0400

    1010d | Create Apply content widget

--- src/applications/static-pages/ezr-submission-options/components/App/index.jsx
+++ src/applications/static-pages/ezr-submission-options/components/App/index.jsx
@@ -1,83 +1,168 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import {
-  InPersonDescription,
-  MailDescription,
-  PhoneDescription,
-  ModifiableHealthBenefitsInformation,
-} from '../Descriptions';
+import React, { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 
-export const App = ({ isEzrEnabled }) => {
-  return isEzrEnabled ? (
+export const MERGED_URL =
+  '/family-and-caregiver-benefits/health-and-disability/champva/apply-champva-form-10-10d';
+export const STANDALONE_URL =
+  '/family-and-caregiver-benefits/health-and-disability/champva/apply-form-10-10d';
+
+export const App = () => {
+  const isMergedFormEnabled = useSelector(
+    state => state.featureToggles?.form1010dExtended,
+  );
+  const appUrl = isMergedFormEnabled ? MERGED_URL : STANDALONE_URL;
+
+  const downloadLink = useMemo(
+    () => (
+      <va-link
+        href="/find-forms/about-form-10-10d"
+        text="Get VA Form 10-10d to download"
+      />
+    ),
+    [],
+  );
+
+  return (
     <>
       <p>
-        You can update your health benefits information online, by phone, by
-        mail, or in person.
+        You can apply online, by mail, or by fax. Make sure to submit the
+        required supporting documents with your application.
       </p>
 
-      <h3>Option 1: Online</h3>
-      <p>
-        You’ll need to sign in to VA.gov to update your health benefits
-        information online.
-      </p>
-      <p>
-        <a
-          className="vads-c-action-link--green"
-          href="/my-health/update-benefits-information-form-10-10ezr/"
-        >
-          Update your health benefits information online
-        </a>
-      </p>
-      <p>
-        <strong>Note:</strong> You can also update some information (like your
-        address and other contact information) in your VA.gov profile.
-      </p>
       <p>
         <va-link
-          href="/resources/managing-your-vagov-profile/"
-          text="Learn more about managing your VA.gov profile"
+          href="/family-and-caregiver-benefits/health-and-disability/champva/#supporting-documents-for-your-"
+          text="Find out what supporting documents you need"
         />
       </p>
 
-      <h3>Option 2: By phone</h3>
-      <PhoneDescription />
-
-      <h3>Option 3: By mail</h3>
-      <MailDescription />
-
-      <h3>Option 4: In person</h3>
-      <InPersonDescription />
-
-      <ModifiableHealthBenefitsInformation isEzrEnabled={isEzrEnabled} />
-    </>
-  ) : (
-    <>
+      <h3>Option 1: Online</h3>
+      <p>You can apply online now.</p>
       <p>
-        You can update your health benefits information by phone, by mail, or in
-        person.
+        <va-link-action href={appUrl} text="Apply for CHAMPVA online" />
       </p>
 
-      <h3>Option 1: By phone</h3>
-      <PhoneDescription />
-
       <h3>Option 2: By mail</h3>
-      <MailDescription />
+      <p>
+        You’ll need to fill out an Application for CHAMPVA Benefits (VA Form
+        10-10d).
+      </p>
+      <p>{downloadLink}</p>
+      <p>
+        Mail your completed application and supporting documents to this
+        address:
+      </p>
+      <p className="va-address-block">
+        VHA Office of Community Care
+        <br role="presentation" />
+        CHAMPVA Eligibility
+        <br role="presentation" />
+        PO Box 137
+        <br role="presentation" />
+        Spring City, PA 19475
+      </p>
 
-      <h3>Option 3: In person</h3>
-      <InPersonDescription />
+      <h3>Option 3: By fax</h3>
+      <p>
+        You’ll need to fill out an Application for CHAMPVA Benefits (VA Form
+        10-10d).
+      </p>
+      <p>{downloadLink}</p>
+      <p>
+        Fax your completed application and supporting documents to{' '}
+        <va-telephone contact="3033317809" />.
+      </p>
 
-      <ModifiableHealthBenefitsInformation isEzrEnabled={isEzrEnabled} />
+      <h3>If you’re age 65 or older and you don’t qualify for Medicare</h3>
+      <p>
+        You’ll need to submit a document called a “notice of disallowance” from
+        the Social Security Administration. This document confirms that you
+        don’t qualify for Medicare benefits under anyone’s Social Security
+        number.
+      </p>
+
+      <h3>If you’re the spouse or surviving spouse of a Veteran</h3>
+      <p>
+        To help us process your application faster, submit a copy of one of
+        these optional supporting documents:
+      </p>
+      <ul>
+        <li>
+          Marriage certificate, <strong>or</strong>
+        </li>
+        <li>
+          Certificate of civil union, <strong>or</strong>
+        </li>
+        <li>Common-law marriage affidavit</li>
+      </ul>
+      <p>
+        <strong>
+          If you’re a surviving spouse who remarried before age 55 and your
+          remarriage has ended,
+        </strong>{' '}
+        submit a copy of one of these optional supporting documents:
+      </p>
+      <ul>
+        <li>
+          Divorce decree, <strong>or</strong>
+        </li>
+        <li>
+          Annulment decree, <strong>or</strong>
+        </li>
+        <li>Death certificate</li>
+      </ul>
+      <h3>If you’re a dependent child or applying for a dependent child</h3>
+      <p>
+        To help us process your application faster, submit a copy of the child’s
+        birth certificate. You may need to submit additional supporting
+        documents in certain cases.
+      </p>
+
+      <p>
+        <strong>If the child is adopted,</strong> you’ll need to submit a copy
+        of the adoption papers.
+      </p>
+
+      <p>
+        <strong>If the child is the Veteran sponsor’s stepchild,</strong> you’ll
+        need to submit proof of the marriage between the Veteran sponsor and the
+        child’s other parent.
+      </p>
+
+      <p>
+        <strong>
+          If the child is between ages 18 to 23 and enrolled in school,
+        </strong>{' '}
+        you’ll need to submit a school certification letter as proof of
+        enrollment. And you’ll need to recertify once a year.
+      </p>
+
+      <p>
+        <va-link
+          href="/resources/getting-care-through-champva#champva-and-school-enrollment-"
+          text="Learn how to submit a school certification letter"
+        />
+      </p>
+
+      <p>
+        <strong>
+          If the child became permanently unable to support themselves before
+          age 18,
+        </strong>{' '}
+        you can submit a disability rating letter for the child (sometimes
+        called a “helpless child” rating) to help us process your application
+        faster. Contact your nearest VA regional office and ask how to get this
+        rating letter.
+      </p>
+
+      <p>
+        <va-link
+          href="/find-locations/?page=1&facilityType=benefits"
+          text="Find a VA regional office near you"
+        />
+      </p>
     </>
   );
 };
 
-App.propTypes = {
-  isEzrEnabled: PropTypes.bool,
-};
-
-const mapStateToProps = state => ({
-  isEzrEnabled: state.featureToggles.ezrProdEnabled,
-});
-
-export default connect(mapStateToProps)(App);
+export default App;
