b781c460e6cd644f8696a89abe73063a73f3de0f
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-09-26 10:16:40 -0500

    Adding data and additional Cypress testing

--- src/applications/appeals/995/config/form.js
+++ src/applications/appeals/995/config/form.js
@@ -74,7 +74,6 @@ import {
   LIMITED_CONSENT_PROMPT_PATH,
   EVIDENCE_ADDITIONAL_PATH,
   EVIDENCE_UPLOAD_PATH,
-  MST_OPTION,
   SC_NEW_FORM_DATA,
 } from '../constants';
 import { SUBMIT_URL } from '../constants/apis';
@@ -378,7 +377,7 @@ const formConfig = {
           path: 'option-indicator',
           uiSchema: optionIndicator.uiSchema,
           schema: optionIndicator.schema,
-          depends: formData => formData?.[MST_OPTION],
+          depends: hasMstOption,
         },
       },
     },
