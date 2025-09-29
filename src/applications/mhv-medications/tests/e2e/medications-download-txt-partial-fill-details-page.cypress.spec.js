e658a91833d63e49b4ca8be8724f7cb64112d67c
Ian Seabock <iseabock@MacBook-Pro.local>
2025-09-29 13:28:16 -0700

    added e2e test for partial fill

---
+++ src/applications/mhv-medications/tests/e2e/medications-download-txt-partial-fill-details-page.cypress.spec.js
@@ -0,0 +1,42 @@
+import MedicationsSite from './med_site/MedicationsSite';
+import MedicationsDetailsPage from './pages/MedicationsDetailsPage';
+import MedicationsListPage from './pages/MedicationsListPage';
+import partialRxDetails from './fixtures/partial-prescription-details.json';
+import partialRxList from './fixtures/partial-refills-med-list.json';
+import { Data } from './utils/constants';
+
+describe('Medications Partial Fill Details Page TXT Download', () => {
+  it('visits Medications Details Page with Partial Fill and Downloads TXT', () => {
+    const site = new MedicationsSite();
+    const listPage = new MedicationsListPage();
+    const detailsPage = new MedicationsDetailsPage();
+    const cardNumber = 2;
+
+    site.login();
+    listPage.visitMedicationsListPageURL(partialRxList);
+    detailsPage.clickMedicationDetailsLink(partialRxDetails, cardNumber);
+
+    // Verify partial fill content is displayed before download
+    detailsPage.clickRefillHistoryAccordionOnDetailsPage();
+    detailsPage.verifyRefillAccordionHeaderForPartialFillOnDetailsPage(
+      'Partial fill',
+      Data.DATE_EMPTY,
+    );
+    detailsPage.verifyPartialFillTextInRefillAccordionOnDetailsPage(
+      Data.PARTIAL_FILL_TEXT,
+    );
+    detailsPage.verifyQuantityForPartialFillOnDetailsPage(Data.QUANTITY_EMPTY);
+
+    listPage.clickPrintOrDownloadThisListDropDown();
+    detailsPage.verifyFocusOnPrintOrDownloadDropdownButtonOnDetailsPage();
+    detailsPage.clickDownloadMedicationsDetailsAsTxtOnDetailsPage();
+
+    listPage.verifyDownloadCompleteSuccessMessageBanner(
+      Data.DOWNLOAD_SUCCESS_ALERT_CONTENT,
+    );
+    listPage.verifyFocusOnDownloadAlertSuccessBanner();
+
+    cy.injectAxe();
+    cy.axeCheck('main');
+  });
+});
