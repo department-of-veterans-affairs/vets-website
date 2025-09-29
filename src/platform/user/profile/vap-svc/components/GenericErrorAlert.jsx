1aaf9f6e7c507c85927ebec7e0a4407e7a557926
emisparkles <emisargent@gmail.com>
2025-09-29 11:29:58 -0700

    Replace inline error message with va-alert for failed transactions

---
+++ src/platform/user/profile/vap-svc/components/GenericErrorAlert.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { DEFAULT_ERROR_MESSAGE } from '../constants';
+
+const GenericErrorAlert = ({ title }) => {
+  const id = `${title?.toLowerCase().replace(/\s+/g, '-')}-error-alert`;
+
+  return (
+    <>
+      <va-alert
+        background-only
+        class="vads-u-margin-y--1"
+        close-btn-aria-label="Close notification"
+        disable-analytics="false"
+        status="error"
+        visible="true"
+        full-width
+        slim
+        uswds
+        role="alert"
+        tabindex="-1"
+      >
+        <p className="vads-u-margin-y--0" id={id}>
+          {DEFAULT_ERROR_MESSAGE}
+        </p>
+      </va-alert>
+    </>
+  );
+};
+
+GenericErrorAlert.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default GenericErrorAlert;
