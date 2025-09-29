b90efbd6b1ad56dcd803de9096f1eee976b2c5ed
Ian Harrison <ian.harrison@adhocteam.us>
2025-09-29 15:30:45 -0500

    remove ci check

--- src/platform/forms-system/src/js/utilities/navigation/navigationState.js
+++ src/platform/forms-system/src/js/utilities/navigation/navigationState.js
@@ -1,5 +1,3 @@
-import environment from '@department-of-veterans-affairs/platform-utilities/environment';
-
 /**
  * Use this module in a validation function to determine if validation
  * was triggered by user attempting to navigate form page
@@ -19,14 +17,6 @@ const navigationState = {
   },
 
   getNavigationEventStatus() {
-    /**
-     * in CI checking path equality sometimes causes problems
-     * the path check is relevant only to file input / multiple file input / international telephone
-     * in which navigation status is checked on initial page load validation
-     */
-    if (environment.isTest() && !environment.isUnitTest()) {
-      return this._navigationEvent;
-    }
     // only return true if the navigation happened on the current page
     return this._navigationEvent && this._currentPath === window.location.href;
   },
