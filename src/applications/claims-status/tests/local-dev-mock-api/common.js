82ae606f8da3bc08778a9fb88d99a851520eecbb
Joshua <joshing_you1@yahoo.com>
2025-09-30 13:33:39 -0400

    Updated mock data

--- src/applications/claims-status/tests/local-dev-mock-api/common.js
+++ src/applications/claims-status/tests/local-dev-mock-api/common.js
@@ -160,6 +160,133 @@ const appealData1 = {
   },
 };
 
+const appealData = {
+  data: [
+    ...appealData1,
+    {
+      id: '2765759',
+      type: 'legacyAppeal',
+      attributes: {
+        appealIds: ['2765759'],
+        updated: '2021-03-04T19:55:21-05:00',
+        incompleteHistory: false,
+        type: 'original',
+        // this determines if the appeal is open or closed
+        active: true,
+        description: 'Benefits as a result of VA error (Section 1151)',
+        aod: false,
+        location: 'bva',
+        aoj: 'vba',
+        programArea: 'compensation',
+        status: { type: 'on_docket', details: {} },
+        alerts: [],
+        docket: {
+          front: false,
+          total: 140135,
+          ahead: 101381,
+          ready: 16432,
+          month: '2012-04-01',
+          docketMonth: '2011-01-01',
+          eta: null,
+        },
+        issues: [
+          {
+            description: 'Benefits as a result of VA error (Section 1151)',
+            diagnosticCode: null,
+            active: true,
+            lastAction: 'withdrawn',
+            date: new Date().toISOString(),
+          },
+          {
+            description: null,
+            diagnosticCode: null,
+            active: false,
+            lastAction: 'withdrawn',
+            date: new Date().toISOString(),
+          },
+          {
+            description: null,
+            diagnosticCode: null,
+            active: false,
+            lastAction: null,
+            date: null,
+          },
+          {
+            description: null,
+            diagnosticCode: null,
+            active: false,
+            lastAction: null,
+            date: null,
+          },
+          {
+            description: 'Benefits as a result of VA error (Section 1151)',
+            diagnosticCode: null,
+            active: false,
+            lastAction: null,
+            date: null,
+          },
+        ],
+        events: [
+          { type: 'nod', date: '2012-02-02' },
+          { type: 'soc', date: '2012-03-03' },
+          { type: 'form9', date: '2012-04-04' },
+          { type: 'hearing_held', date: '2023-01-11' },
+        ],
+        evidence: [],
+      },
+    },
+    {
+      id: 'HLR4196',
+      type: 'higherLevelReview',
+      attributes: {
+        appealIds: ['HLR4196'],
+        updated: '2025-09-26T10:48:46-04:00',
+        incompleteHistory: false,
+        active: true,
+        description: '1 medical issue',
+        location: 'aoj',
+        aoj: 'vha',
+        programArea: 'medical',
+        status: {
+          type: 'hlr_received',
+          details: {},
+        },
+        alerts: [],
+        issues: [
+          {
+            active: true,
+            lastAction: null,
+            date: null,
+            description: 'Beneficiary Travel - This is a test',
+            diagnosticCode: null,
+          },
+          {
+            active: true,
+            lastAction: null,
+            date: null,
+            description: null,
+            diagnosticCode: null,
+          },
+          {
+            active: true,
+            lastAction: null,
+            date: null,
+            description: null,
+            diagnosticCode: null,
+          },
+        ],
+        events: [
+          {
+            type: 'hlr_request',
+            date: '2023-01-11',
+          },
+        ],
+        evidence: [],
+      },
+    },
+  ],
+};
+
 const baseClaims = [
   // closedClaimAlert
   createClaim('1', {
@@ -563,7 +690,7 @@ const responses = {
   'GET /v0/benefits_claims/4': getClaimDataById('4'),
 
   'GET /v0/appeals': {
-    data: [appealData1],
+    appealData,
     meta: {
       pagination: {
         currentPage: 1,
