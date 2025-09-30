f1833f105b379c576fc06f65e993db8235cce973
Aswin Malla <aswinmalla12@gmail.com>
2025-09-30 08:34:21 -0600

    Update Allergies e2e test to test for OH data too

--- src/applications/mhv-medical-records/tests/e2e/accelerated/pages/Allergies.js
+++ src/applications/mhv-medical-records/tests/e2e/accelerated/pages/Allergies.js
@@ -3,7 +3,7 @@ import MedicalRecordsLandingPage from '../../pages/MedicalRecordsLandingPage';
 import vamc from '../../fixtures/facilities/vamc-ehr.json';
 
 class Allergies {
-  setIntercepts = ({ allergiesData } = {}) => {
+  setIntercepts = ({ allergiesData, useOhData = true } = {}) => {
     cy.intercept('POST', '/v0/datadog_action', {}).as('datadogAction');
 
     cy.intercept('POST', '/my_health/v1/medical_records/session', {}).as(
@@ -16,6 +16,11 @@ class Allergies {
     cy.intercept('GET', '/data/cms/vamc-ehr.json', vamc).as('vamcEhr');
 
     cy.intercept('GET', '/my_health/v1/medical_records/allergies*', req => {
+      if (useOhData) {
+        expect(req.url).to.contain('use_oh_data_path=1');
+      } else {
+        expect(req.url).to.not.contain('use_oh_data_path=1');
+      }
       req.reply(allergiesData);
     }).as('allergies-list');
 
