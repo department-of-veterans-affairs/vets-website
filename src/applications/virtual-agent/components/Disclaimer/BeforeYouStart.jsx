3bc3625e48be7c0efbef8da122a686ff4aa19c3e
Adam Whitlock <8332986+adamwhitlock1@users.noreply.github.com>
2025-10-01 12:53:01 -0600

    [department-of-veterans-affairs/va-virtual-agent/issues/2617] update margin for 'before you start' to match figma

--- src/applications/virtual-agent/components/Disclaimer/BeforeYouStart.jsx
+++ src/applications/virtual-agent/components/Disclaimer/BeforeYouStart.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function BeforeYouStart() {
   return (
     <>
-      <h2>Before you start</h2>
+      <h2 className="vads-u-margin-top--4">Before you start</h2>
       <p>
         <b>If you think your life or health is in danger, </b>
         go to the nearest emergency room or call 911. If you’re not sure if your
