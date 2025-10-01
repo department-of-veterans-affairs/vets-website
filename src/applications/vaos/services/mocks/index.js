7ab0778d785241cb6a1f08284d015699a7c1a454
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2025-10-01 06:03:48 -0700

    Adds mock endpoint for OH slots
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>

--- src/applications/vaos/services/mocks/index.js
+++ src/applications/vaos/services/mocks/index.js
@@ -369,6 +369,17 @@ const responses = {
       data: slots,
     });
   },
+  'GET /vaos/v2/locations/:facility_id/slots': (req, res) => {
+    const start = new Date(req.query.start);
+    const end = new Date(req.query.end);
+    const slots = appointmentSlotsV2.data.filter(slot => {
+      const slotStartDate = new Date(slot.attributes.start);
+      return isWithinInterval(slotStartDate, { start, end });
+    });
+    return res.json({
+      data: slots,
+    });
+  },
   'GET /vaos/v2/patients': (req, res) => {
     return res.json({
       data: {
