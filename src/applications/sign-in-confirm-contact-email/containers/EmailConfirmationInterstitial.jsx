5420425c7084f943cdd097eb7bb584c7d06eadff
Caitlin <78328496+CaitHawk@users.noreply.github.com>
2025-10-03 10:54:48 -0700

    builds email interstitial compoenents

---
+++ src/applications/sign-in-confirm-contact-email/containers/EmailConfirmationInterstitial.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '@department-of-veterans-affairs/platform-user/selectors';
+import { apiRequest } from '@department-of-veterans-affairs/platform-utilities/exports';
+import { AUTHN_SETTINGS } from '@department-of-veterans-affairs/platform-user/exports';
+import SuccessConfirm from '../components/alerts/SuccessConfirm';
+import ErrorConfirm from '../components/alerts/ErrorConfirm';
+import ConfirmAddBtnGroup from '../components/ConfirmAddBtnGroup';
+
+export default function EmailConfirmationInterstitial() {
+  const [confirmationSuccess, setConfirmationSuccess] = useState(false);
+  const [confirmationError, setConfirmationError] = useState(false);
+
+  // Mocked value for testing/styling purposes below
+  // const confirmationSuccess = false;
+  // const confirmationError = false;
+
+  // Removed for testing purposes below
+  useEffect(() => {
+    document.title = 'Confirm your contact email address';
+    if (!localStorage.getItem('hasSession')) {
+      window.location.pathname = '/';
+    }
+  }, []);
+
+  const contactEmail =
+    useSelector(selectUser).vet360ContactInformation?.email?.emailAddress ||
+    'No email provided';
+  // Mocked value for testing/styling purposes below
+  // const contactEmail = 'caitlin.neathawk@va.gov'
+
+  const returnUrl =
+    sessionStorage.getItem(AUTHN_SETTINGS.RETURN_URL) || '/my-va';
+
+  const handleConfirmation = () => {
+    apiRequest('/v0/profile/email_addresses', {
+      method: 'PUT',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(() => {
+        setConfirmationSuccess(true);
+        setConfirmationError(false);
+      })
+      .catch(() => {
+        setConfirmationError(true);
+        setConfirmationSuccess(false);
+      });
+  };
+
+  // Mocked value for testing purposes below
+  // const handleConfirmation = () => { console.log('hello')};
+
+  return (
+    <>
+      <section className="container row login vads-u-padding--3">
+        <h1>Confirm your contact email</h1>
+        {confirmationSuccess && <SuccessConfirm />}
+        {confirmationError && <ErrorConfirm />}
+        <div className="vads-u-margin-y--1">
+          <va-card icon-name="" className="vads-u-width--100">
+            <h2 className="vads-u-margin-top--0">Contact email address</h2>
+            <p>
+              We'll send notifications about your VA health care and benefits to
+              this email.
+            </p>
+            <p>
+              <strong>{contactEmail}</strong>
+            </p>
+          </va-card>
+        </div>
+        {!confirmationSuccess && (
+          <ConfirmAddBtnGroup
+            email={contactEmail}
+            handleConfirmation={{ handleConfirmation }}
+          />
+        )}
+        {confirmationSuccess ? (
+          <va-link-action
+            href={returnUrl}
+            label="continue to va.gov"
+            text="Continue to VA.gov"
+            type="primary"
+          />
+        ) : (
+          <div className="vads-u-margin-y--2">
+            <va-link text="Skip for now and go to VA.gov" href={returnUrl} />
+          </div>
+        )}
+        <div>
+          <h2 className="vads-u-margin-top--0">What's changing</h2>
+          <p>
+            We'll send all VA notifications to the contact email address listed
+            in your VA.gov profile. We wont't send any more notifications to the
+            email listed in the previous MyHealtheVet experience. Make sure the
+            contact email address listed in your VA.gov profile is the one you
+            want us to send notifications to.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
