588f3ad0cf0357c75c091fe084d34233dc41b6ce
Andrew Rodiek <25368370+Dr-Pongo@users.noreply.github.com>
2025-10-03 12:19:40 -0400

    Bleepblorping for veteran/claimant chapter

--- src/applications/benefits-optimization-aquia/21-0779-nursing-home-information/pages/index.js
+++ src/applications/benefits-optimization-aquia/21-0779-nursing-home-information/pages/index.js
@@ -4,8 +4,18 @@
  * Exports all page components for easy importing
  */
 
+// Original pages (to be removed after form config is updated)
 export { VeteranIdentificationPage } from './veteran-identification';
 export { ClaimantIdentificationPage } from './claimant-identification';
+
+// New split pages
+export { ClaimantQuestionPage } from './claimant-question';
+export { ClaimantPersonalInfoPage } from './claimant-personal-info';
+export { ClaimantIdentificationInfoPage } from './claimant-identification-info';
+export { VeteranPersonalInfoPage } from './veteran-personal-info';
+export { VeteranIdentificationInfoPage } from './veteran-identification-info';
+
+// Other pages
 export { NursingHomeDetailsPage } from './nursing-home-details';
 export { MedicaidAndCostPage } from './medicaid-and-cost';
 export { CertificationLevelOfCarePage } from './certification-level-of-care';
