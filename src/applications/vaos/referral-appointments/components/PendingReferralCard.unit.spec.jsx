a9318cabdd538610902e54e5604d1adb807343c6
Chad Laflamme <216556326+sixercjl@users.noreply.github.com>
2025-10-03 10:02:00 -0400

    update pending referral list

--- src/applications/vaos/referral-appointments/components/PendingReferralCard.unit.spec.jsx
+++ src/applications/vaos/referral-appointments/components/PendingReferralCard.unit.spec.jsx
@@ -53,4 +53,21 @@ describe('VAOS Component: PendingReferralCard', () => {
     );
     expect(screen.getByTestId('referral-not-available-alert')).to.exist;
   });
+  it('should render basic list item with schedule appointment button for pilot stations', () => {
+    referral.stationId = '648GK';
+    screen = render(
+      <PendingReferralCard referral={referral} handleClick={handleClick} />,
+    );
+    expect(screen.getByTestId('schedule-appointment-link')).to.exist;
+    referral.stationId = '648GE';
+    screen = render(
+      <PendingReferralCard referral={referral} handleClick={handleClick} />,
+    );
+    expect(screen.getByTestId('schedule-appointment-link')).to.exist;
+    referral.stationId = '657GQ';
+    screen = render(
+      <PendingReferralCard referral={referral} handleClick={handleClick} />,
+    );
+    expect(screen.getByTestId('schedule-appointment-link')).to.exist;
+  });
 });
