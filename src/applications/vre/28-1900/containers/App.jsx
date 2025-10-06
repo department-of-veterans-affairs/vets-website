6e87298efe60f53bc220aae50c583101bb0973ab
Nick Sayre <nick.sayre@adhocteam.us>
2025-10-06 16:27:23 -0500

    2027 - add support for chapter 31 downtime notifications

--- src/applications/vre/28-1900/containers/App.jsx
+++ src/applications/vre/28-1900/containers/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+import {
+  DowntimeNotification,
+  externalServices,
+} from '@department-of-veterans-affairs/platform-monitoring/DowntimeNotification';
 import formConfig from '../config/form';
 
 export default function App({ location, children }) {
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-      {children}
+      <DowntimeNotification
+        appTitle="28-1900"
+        dependencies={[externalServices.chapter31]}
+      >
+        {children}
+      </DowntimeNotification>
     </RoutedSavableApp>
   );
 }
+
+App.propTypes = {
+  children: PropTypes.node,
+  location: PropTypes.object,
+};
