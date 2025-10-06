2e09e411b35d3f6ae07dce7e0cc5d8ab5d1f7558
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-10-06 15:11:01 -0700

    Add submit flow wrapper

---
+++ src/applications/travel-pay/containers/ComplexClaimSubmitFlowWrapper.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useParams } from 'react-router-dom-v5-compat';
+
+import { Element } from 'platform/utilities/scroll';
+
+import ConfirmationPage from '../components/complex-claims/pages/ConfirmationPage';
+
+const ComplexClaimSubmitFlowWrapper = () => {
+  const { apptId } = useParams();
+
+  return (
+    <Element name="topScrollElement">
+      <article className="usa-grid-full vads-u-margin-bottom--0">
+        <div className="vads-u-padding-top--2p5 vads-u-padding-bottom--4">
+          <va-link
+            back
+            data-testid="complex-claim-back-link"
+            disable-analytics
+            href={`/my-health/appointments/past/${apptId}`}
+            text="Back to your appointment"
+          />
+        </div>
+        <div className="vads-l-col--12 medium-screen:vads-l-col--8">
+          <ConfirmationPage />
+        </div>
+      </article>
+    </Element>
+  );
+};
+
+export default ComplexClaimSubmitFlowWrapper;
