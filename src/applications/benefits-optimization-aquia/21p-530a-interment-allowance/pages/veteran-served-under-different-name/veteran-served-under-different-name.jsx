0e5bd1816f750dc432560a332555b791bff1417a
Justin Linn <justin@linnjs.com>
2025-10-05 23:49:18 -0400

    feat(21-530a): Add final section

---
+++ src/applications/benefits-optimization-aquia/21p-530a-interment-allowance/pages/veteran-served-under-different-name/veteran-served-under-different-name.jsx
@@ -0,0 +1,89 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { RadioField } from '@bio-aquia/shared/components/atoms';
+import { PageTemplate } from '@bio-aquia/shared/components/templates';
+
+import { z } from 'zod';
+import { hasAlternateNamesSchema } from '../../schemas';
+
+/**
+ * Schema for veteran served under different name page
+ */
+const veteranServedUnderDifferentNameSchema = z.object({
+  veteranServedUnderDifferentName: hasAlternateNamesSchema,
+});
+
+/**
+ * Veteran Served Under Different Name page component for the interment allowance form.
+ * Asks if the veteran served under a different name to conditionally show the previous names page.
+ * This is part of the "Military history" chapter.
+ *
+ * @component
+ * @param {Object} props - Component props
+ * @param {Object} [props.data] - Initial form data from the form system
+ * @param {Function} [props.goBack] - Function to navigate to the previous page
+ * @param {Function} props.goForward - Function to navigate to the next page
+ * @param {Function} [props.setFormData] - Function to update the form data in the form system
+ * @returns {JSX.Element} Veteran served under different name form page
+ *
+ * @example
+ * ```jsx
+ * <VeteranServedUnderDifferentNamePage
+ *   data={formData}
+ *   goForward={handleGoForward}
+ *   goBack={handleGoBack}
+ *   setFormData={setFormData}
+ * />
+ * ```
+ */
+export const VeteranServedUnderDifferentNamePage = ({
+  data,
+  setFormData,
+  goForward,
+  goBack,
+}) => {
+  const formDataToUse =
+    data && typeof data === 'object' && !Array.isArray(data) ? data : {};
+
+  return (
+    <PageTemplate
+      title="Previous names"
+      data={formDataToUse}
+      setFormData={setFormData}
+      goForward={goForward}
+      goBack={goBack}
+      schema={veteranServedUnderDifferentNameSchema}
+      sectionName="veteranServedUnderDifferentName"
+      defaultData={{
+        veteranServedUnderDifferentName: '',
+      }}
+    >
+      {({ localData, handleFieldChange, errors, formSubmitted }) => (
+        <>
+          <RadioField
+            name="veteranServedUnderDifferentName"
+            label="Did the Veteran serve under a different name?"
+            value={localData.veteranServedUnderDifferentName}
+            onChange={handleFieldChange}
+            required
+            error={errors.veteranServedUnderDifferentName}
+            forceShowError={formSubmitted}
+            schema={hasAlternateNamesSchema}
+            options={[
+              { value: 'yes', label: 'Yes' },
+              { value: 'no', label: 'No' },
+            ]}
+          />
+        </>
+      )}
+    </PageTemplate>
+  );
+};
+
+VeteranServedUnderDifferentNamePage.propTypes = {
+  data: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+  goBack: PropTypes.func,
+  goForward: PropTypes.func.isRequired,
+  setFormData: PropTypes.func,
+};
