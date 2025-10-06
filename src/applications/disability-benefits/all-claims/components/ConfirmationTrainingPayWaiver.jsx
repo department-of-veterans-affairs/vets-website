f27c43e9a2bef650530d5ec7e9fe61aedde43873
Tommasina Miller <144388524+tommasina-va@users.noreply.github.com>
2025-10-06 14:49:32 -0500

    Add guard around training pay waiver, and add specs

--- src/applications/disability-benefits/all-claims/components/ConfirmationTrainingPayWaiver.jsx
+++ src/applications/disability-benefits/all-claims/components/ConfirmationTrainingPayWaiver.jsx
@@ -7,29 +7,33 @@ const ConfirmationTrainingPayWaiver = ({ formData }) => {
       'I don’t want to get VA compensation pay for the days I receive training pay.',
     N: 'I want to get VA compensation pay instead of training pay.',
   };
-  return (
-    <li>
-      <h4>Training pay waiver</h4>
-      <ul className="vads-u-padding--0" style={{ listStyle: 'none' }}>
-        <li>
-          {formData.waiveTrainingPay ? (
-            <>
-              <div className="vads-u-color--gray">Selected</div>
-              <div>{labels.Y}</div>
-            </>
-          ) : (
-            <>
-              <div className="vads-u-color--gray">Not selected</div>
-              <div>{labels.N}</div>
-            </>
-          )}
-        </li>
-      </ul>
-    </li>
-  );
+  if (formData?.hasTrainingPay) {
+    return (
+      <li>
+        <h4>Training pay waiver</h4>
+        <ul className="vads-u-padding--0" style={{ listStyle: 'none' }}>
+          <li>
+            {formData.waiveTrainingPay ? (
+              <>
+                <div className="vads-u-color--gray">Selected</div>
+                <div>{labels.Y}</div>
+              </>
+            ) : (
+              <>
+                <div className="vads-u-color--gray">Not selected</div>
+                <div>{labels.N}</div>
+              </>
+            )}
+          </li>
+        </ul>
+      </li>
+    );
+  }
+  return null;
 };
 ConfirmationTrainingPayWaiver.propTypes = {
   formData: PropTypes.shape({
+    hasTrainingPay: PropTypes.bool,
     waiveTrainingPay: PropTypes.bool,
   }),
 };
