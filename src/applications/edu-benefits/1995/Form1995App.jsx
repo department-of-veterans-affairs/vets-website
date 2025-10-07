0a6dd34c935f71fd592451912bb15cf6ec423d73
s-caso <sebastian.caso@accenturefederal.com>
2025-10-07 12:41:23 -0400

    add reroute feature flag to 1995

--- src/applications/edu-benefits/1995/Form1995App.jsx
+++ src/applications/edu-benefits/1995/Form1995App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import { useFeatureToggle } from 'platform/utilities/feature-toggles';
@@ -9,9 +9,20 @@ export default function Form1995Entry({ location, children }) {
   const { useToggleValue, TOGGLE_NAMES } = useFeatureToggle();
   const toggleValue = useToggleValue(TOGGLE_NAMES.merge1995And5490);
   const showRudisill1995 = useToggleValue(TOGGLE_NAMES.showRudisill1995);
-  // const mebReRoute = useToggleValue(TOGGLE_NAMES.meb1995ReReoute);
+  const mebReRoute = useToggleValue(TOGGLE_NAMES.meb1995ReReoute);
 
   useSetToggleParam(toggleValue, showRudisill1995);
+
+  useEffect(
+    () => {
+      if (typeof mebReRoute === 'boolean') {
+        sessionStorage.setItem('meb1995ReRoute', JSON.stringify(mebReRoute));
+      } else {
+        sessionStorage.setItem('meb1995ReRoute', 'false');
+      }
+    },
+    [mebReRoute],
+  );
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       {children}
