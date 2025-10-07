325a5635485a1a88cfa25e9e4d3334106997cff9
David Becker <david@beckersite.com>
2025-10-07 12:55:46 -0600

    Account for in progress form data for householdIncome

--- src/applications/dependents/686c-674/config/chapters/household-income/householdIncome.js
+++ src/applications/dependents/686c-674/config/chapters/household-income/householdIncome.js
@@ -22,10 +22,20 @@ export const uiSchema = {
     updateSchema: (formData, formSchema) => {
       // Use 'view:householdIncome' as UI value and householdIncome as RBPS value
       // Set householdIncome to the opposite of view:householdIncome (as per RBPS)
+
+      const updated = formData;
       // If view:householdIncome is undefined (user hasn't seen the question yet), leave householdIncome alone
+      // If view:householdIncome is defined, set householdIncome to the opposite value
+      // If householdIncome is defined but view:householdIncome is undefined (user hasn't seen the question yet but in-progress form exists),
+      // set view:householdIncome to the opposite value of householdIncome
       if (formData['view:householdIncome'] !== undefined) {
-        const updated = formData;
         updated.householdIncome = !formData['view:householdIncome'];
+      }
+      if (
+        formData['view:householdIncome'] === undefined &&
+        formData.householdIncome !== undefined
+      ) {
+        updated['view:householdIncome'] = !formData.householdIncome;
       }
 
       return formSchema;
