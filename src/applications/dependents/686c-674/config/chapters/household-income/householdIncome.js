b7f838c6274b91212c9c61e1abb4e17bbd2d5087
David Becker <david@beckersite.com>
2025-10-07 11:57:01 -0600

    ask networth opposite way but keep submission value the same

--- src/applications/dependents/686c-674/config/chapters/household-income/householdIncome.js
+++ src/applications/dependents/686c-674/config/chapters/household-income/householdIncome.js
@@ -8,7 +8,7 @@ import { whatAreAssets, netWorthTitle } from './helpers';
 export const schema = {
   type: 'object',
   properties: {
-    householdIncome: yesNoSchema,
+    'view:householdIncome': yesNoSchema,
   },
 };
 
@@ -18,7 +18,20 @@ export const uiSchema = {
     'If you currently receive VA pension benefits, we need to know your net worth. Your net worth includes your assets and your annual income. If you’re married, include the value of your spouse’s assets and annual income too.',
   ),
   'ui:description': whatAreAssets,
-  householdIncome: yesNoUI({
+  'ui:options': {
+    updateSchema: (formData, formSchema) => {
+      // Use 'view:householdIncome' as UI value and householdIncome as RBPS value
+      // Set householdIncome to the opposite of view:householdIncome (as per RBPS)
+      // If view:householdIncome is undefined (user hasn't seen the question yet), leave householdIncome alone
+      if (formData['view:householdIncome'] !== undefined) {
+        const updated = formData;
+        updated.householdIncome = !formData['view:householdIncome'];
+      }
+
+      return formSchema;
+    },
+  },
+  'view:householdIncome': yesNoUI({
     title: netWorthTitle(),
     enableAnalytics: true,
     updateUiSchema: formData => ({
