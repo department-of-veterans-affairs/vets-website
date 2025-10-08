85f06c7bf80627f3fc0a2658c5e263cfa772b1e8
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-10-08 09:38:50 -0700

    Update Confirm page content

--- src/applications/travel-pay/components/complex-claims/pages/ConfirmationPage.jsx
+++ src/applications/travel-pay/components/complex-claims/pages/ConfirmationPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import { TRAVEL_PAY_INFO_LINK } from '../../../constants';
 import { formatDateTime } from '../../../util/dates';
 
 const ConfirmationPage = () => {
   // TODO: Remove placeholder data when wired up
-  const error = false;
   const data = {
     localStartTime: '2025-03-20T16:30:00.000-08:00',
     location: {
@@ -20,36 +18,18 @@ const ConfirmationPage = () => {
   return (
     <>
       <h1>We’re processing your travel reimbursement claim</h1>
-      {error || !data ? (
-        <va-alert closable="false" status="error" visible>
-          <h2 slot="headline">Something went wrong on our end</h2>
-          <p className="vads-u-margin-y--0">
-            We’re sorry. We couldn’t file your travel reimbursement claim in
-            this tool right now. Please try again later.
-          </p>
-          <p>
-            Or you can still file for this appointment through the Beneficiary
-            Travel Self Service System (BTSSS).
-          </p>
-          <va-link
-            href={TRAVEL_PAY_INFO_LINK}
-            text="Find out how to file for travel reimbursement"
-          />
-        </va-alert>
-      ) : (
-        <va-alert status="success" visible>
-          <h2 slot="headline">Claim submitted</h2>
-          <p className="vads-u-margin-y--0">Claim number: #######</p>
-          <p>
-            This claim is for your appointment{' '}
-            {data.location?.attributes?.name
-              ? `at ${data.location.attributes.name}`
-              : ''}{' '}
-            {data.practitionerName ? `with ${data.practitionerName}` : ''} on{' '}
-            {formattedDate} at {formattedTime}.
-          </p>
-        </va-alert>
-      )}
+      <va-alert status="success" visible>
+        <h2 slot="headline">Claim submitted</h2>
+        <p className="vads-u-margin-y--0">Claim number: #######</p>
+        <p>
+          This claim is for your appointment{' '}
+          {data.location?.attributes?.name
+            ? `at ${data.location.attributes.name}`
+            : ''}{' '}
+          {data.practitionerName ? `with ${data.practitionerName}` : ''} on{' '}
+          {formattedDate} at {formattedTime}.
+        </p>
+      </va-alert>
 
       <h2 className="vads-u-margin-top--4">Print this confirmation page</h2>
       <p>
@@ -82,7 +62,7 @@ const ConfirmationPage = () => {
             for travel reimbursement, no additional steps are needed.
           </p>
           <va-link
-            href="/my-health/travel-reimbursement-claims/direct-deposit"
+            href="/resources/how-to-set-up-direct-deposit-for-va-travel-pay-reimbursement/"
             text="Set up direct deposit"
           />
         </va-process-list-item>
@@ -97,15 +77,18 @@ const ConfirmationPage = () => {
         How to contact us if you have questions
       </h2>
       <p>
-        Call us at <va-telephone contact="8008271000" /> (
-        <va-telephone contact="711" tty />
-        ). We’re here Monday through Friday, 8:00 a.m. to 9:00 p.m. ET.
+        Call us at <va-telephone contact="8555747292" /> (
+        <va-telephone tty contact="711" />
+        ). We’re here Monday through Friday, 8:00 a.m. to 8:00 p.m. ET.
       </p>
       <p>
         Or you can ask us a question online through Ask VA. Select the category
-        and topic for the fastest response.
+        and topic for the VA benefit this form is related to.
       </p>
-      <va-link href="https://ask.va.gov/" text="Ask us a question online" />
+      <va-link
+        href="https://ask.va.gov/"
+        text="Contact us online through Ask VA"
+      />
     </>
   );
 };
