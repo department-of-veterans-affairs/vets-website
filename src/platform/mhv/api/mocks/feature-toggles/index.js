2cd226352bedb3df7999109c1964a7aff20514f7
Ni Chia <ni.chia@adhocteam.us>
2025-10-08 15:25:48 -0700

    fix conflicts

--- src/platform/mhv/api/mocks/feature-toggles/index.js
+++ src/platform/mhv/api/mocks/feature-toggles/index.js
@@ -126,10 +126,6 @@ const generateFeatureToggles = (toggles = {}) => {
           name: 'mhv_medical_records_to_va_gov_release',
           value: mhvMedicalRecordsToVaGovRelease,
         },
-        {
-          name: 'mhv_medical_records_update_landing_page',
-          value: mhvMedicalRecordsMarch17Updates,
-        },
         // secure messaging
         {
           name: 'mhv_secure_messaging_triage_group_plain_language',
