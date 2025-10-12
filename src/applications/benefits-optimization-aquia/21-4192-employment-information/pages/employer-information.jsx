9b9daa03481d3b5b848bafe43ff8553ce08c8836
Justin Linn <justin@linnjs.com>
2025-10-11 23:53:12 -0400

    feat(21-4192): Add Veteran info and emploument info pages

--- src/applications/benefits-optimization-aquia/21-4192-employment-information/pages/employer-information.jsx
+++ src/applications/benefits-optimization-aquia/21-4192-employment-information/pages/employer-information.jsx
@@ -1,21 +1,140 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
+import { FormField, PhoneField } from '@bio-aquia/shared/components/atoms';
+import { AddressField } from '@bio-aquia/shared/components/molecules';
+import { PageTemplate } from '@bio-aquia/shared/components/templates';
+
+import {
+  employerAddressSchema,
+  employerInformationSchema,
+  employerNameSchema,
+  phoneNumberSchema,
+} from '../schemas';
+
 /**
- * Employer Information page component for VA Form 21-4192
- * @module pages/employer-information
+ * Employer Information page component for the employment information form
+ * This page collects employer's name, address, and contact information
+ * @param {Object} props - Component props
+ * @param {Object} props.data - Initial form data
+ * @param {Function} props.setFormData - Function to update form data
+ * @param {Function} props.goForward - Function to proceed to next page
+ * @param {Function} props.goBack - Function to go to previous page
+ * @returns {JSX.Element} Employer information form page
  */
-const EmployerInformationPage = () => {
+export const EmployerInformationPage = ({
+  data,
+  setFormData,
+  goForward,
+  goBack,
+}) => {
+  const formDataToUse =
+    data && typeof data === 'object' && !Array.isArray(data) ? data : {};
+
   return (
-    <div className="vads-u-margin-y--4">
-      <va-alert status="info" visible>
-        <h2 slot="headline">Under Development</h2>
-        <p>
-          This page is currently under development. Please check back later for
-          updates.
-        </p>
-      </va-alert>
-    </div>
+    <PageTemplate
+      title="Employers Information"
+      data={formDataToUse}
+      setFormData={setFormData}
+      goForward={goForward}
+      goBack={goBack}
+      schema={employerInformationSchema}
+      sectionName="employerInformation"
+      defaultData={{
+        employerName: '',
+        employerAddress: {
+          street: '',
+          street2: '',
+          street3: '',
+          city: '',
+          state: '',
+          country: 'USA',
+          postalCode: '',
+          isMilitary: false,
+        },
+        employerPhone: '',
+      }}
+    >
+      {({ localData, handleFieldChange, errors, formSubmitted }) => (
+        <>
+          <FormField
+            name="employerName"
+            label="Employer's name"
+            value={localData.employerName}
+            onChange={handleFieldChange}
+            error={errors.employerName}
+            forceShowError={formSubmitted}
+            schema={employerNameSchema}
+            required
+          />
+
+          <AddressField
+            name="employerAddress"
+            label="Employer's address"
+            value={localData.employerAddress}
+            onChange={handleFieldChange}
+            schema={employerAddressSchema}
+            errors={
+              formSubmitted && errors.employerAddress
+                ? {
+                    street:
+                      errors.employerAddress.street ||
+                      errors.employerAddress?.street,
+                    street2:
+                      errors.employerAddress.street2 ||
+                      errors.employerAddress?.street2,
+                    city:
+                      errors.employerAddress.city ||
+                      errors.employerAddress?.city,
+                    state:
+                      errors.employerAddress.state ||
+                      errors.employerAddress?.state,
+                    country:
+                      errors.employerAddress.country ||
+                      errors.employerAddress?.country,
+                    postalCode:
+                      errors.employerAddress.postalCode ||
+                      errors.employerAddress?.postalCode,
+                  }
+                : {}
+            }
+            touched={
+              formSubmitted
+                ? {
+                    street: true,
+                    street2: true,
+                    street3: true,
+                    city: true,
+                    state: true,
+                    country: true,
+                    postalCode: true,
+                  }
+                : {}
+            }
+            required
+          />
+
+          <PhoneField
+            name="employerPhone"
+            label="Employer's phone number"
+            value={localData.employerPhone}
+            onChange={handleFieldChange}
+            schema={phoneNumberSchema}
+            error={errors.employerPhone}
+            forceShowError={formSubmitted}
+            required
+          />
+        </>
+      )}
+    </PageTemplate>
   );
+};
+
+EmployerInformationPage.propTypes = {
+  goForward: PropTypes.func.isRequired,
+  data: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+  goBack: PropTypes.func,
+  setFormData: PropTypes.func,
 };
 
 export default EmployerInformationPage;
--- src/applications/benefits-optimization-aquia/21-4192-employment-information/pages/veteran-information.jsx
+++ src/applications/benefits-optimization-aquia/21-4192-employment-information/pages/veteran-information.jsx
@@ -1,21 +1,123 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
+import {
+  FormField,
+  MemorableDateField,
+  SSNField,
+} from '@bio-aquia/shared/components/atoms';
+import { FullnameField } from '@bio-aquia/shared/components/molecules';
+import { PageTemplate } from '@bio-aquia/shared/components/templates';
+import { transformDates } from '@bio-aquia/shared/forms';
+
+import {
+  dateOfBirthSchema,
+  ssnSchema,
+  vaFileNumberSchema,
+  veteranInformationSchema,
+} from '../schemas';
+
 /**
- * Veteran Information page component for VA Form 21-4192
- * @module pages/veteran-information
+ * Data processor to ensure date values are properly formatted strings
  */
-const VeteranInformationPage = () => {
+const ensureDateStrings = formData => {
+  return transformDates(formData, ['dateOfBirth']);
+};
+
+/**
+ * Veteran Information page component for the employment information form
+ * This page collects veteran's personal identification information
+ * @param {Object} props - Component props
+ * @param {Object} props.data - Initial form data
+ * @param {Function} props.setFormData - Function to update form data
+ * @param {Function} props.goForward - Function to proceed to next page
+ * @param {Function} props.goBack - Function to go to previous page
+ * @returns {JSX.Element} Veteran information form page
+ */
+export const VeteranInformationPage = ({
+  data,
+  setFormData,
+  goForward,
+  goBack,
+}) => {
+  const formDataToUse =
+    data && typeof data === 'object' && !Array.isArray(data) ? data : {};
+
   return (
-    <div className="vads-u-margin-y--4">
-      <va-alert status="info" visible>
-        <h2 slot="headline">Under Development</h2>
-        <p>
-          This page is currently under development. Please check back later for
-          updates.
-        </p>
-      </va-alert>
-    </div>
+    <PageTemplate
+      title="Veteran's information"
+      data={formDataToUse}
+      setFormData={setFormData}
+      goForward={goForward}
+      goBack={goBack}
+      schema={veteranInformationSchema}
+      sectionName="veteranInformation"
+      dataProcessor={ensureDateStrings}
+      defaultData={{
+        fullName: {
+          first: '',
+          middle: '',
+          last: '',
+        },
+        dateOfBirth: '',
+        ssn: '',
+        vaFileNumber: '',
+      }}
+    >
+      {({ localData, handleFieldChange, errors, formSubmitted }) => (
+        <>
+          <FullnameField
+            name="fullName"
+            value={localData.fullName}
+            onChange={handleFieldChange}
+            errors={errors.fullName || {}}
+            forceShowError={formSubmitted}
+            label="Veteran's name"
+            required
+          />
+
+          <MemorableDateField
+            name="dateOfBirth"
+            label="Date of birth"
+            schema={dateOfBirthSchema}
+            value={localData.dateOfBirth}
+            onChange={handleFieldChange}
+            required
+            error={errors.dateOfBirth}
+            forceShowError={formSubmitted}
+          />
+
+          <SSNField
+            name="ssn"
+            label="Social security number"
+            schema={ssnSchema}
+            value={localData.ssn}
+            onChange={handleFieldChange}
+            required
+            error={errors.ssn}
+            forceShowError={formSubmitted}
+          />
+
+          <FormField
+            name="vaFileNumber"
+            label="VA file number (if applicable)"
+            value={localData.vaFileNumber}
+            onChange={handleFieldChange}
+            error={errors.vaFileNumber}
+            forceShowError={formSubmitted}
+            schema={vaFileNumberSchema}
+          />
+        </>
+      )}
+    </PageTemplate>
   );
+};
+
+VeteranInformationPage.propTypes = {
+  goForward: PropTypes.func.isRequired,
+  data: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+  goBack: PropTypes.func,
+  setFormData: PropTypes.func,
 };
 
 export default VeteranInformationPage;
