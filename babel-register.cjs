f670b5cbcffe545b8745abb22824d0f2a2c1c727
Isaac Gallup <igallup@thesocompany.com>
2025-10-15 09:20:55 -0500

    modifying build command for gha script

--- babel-register.cjs
+++ babel-register.cjs
@@ -1,4 +1,9 @@
 require('@babel/register')({
-  extensions: ['.js', '.jsx'],
-  rootMode: 'upward',
+  extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  ignore: [/node_modules/],
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-react', { runtime: 'automatic' }],
+  ],
+  cache: false,
 });
