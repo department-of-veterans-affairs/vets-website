e37f9e23387fcaa8bb4b0a416b629160c2c5bc0f
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-10-14 22:59:03 -0700

    add components and custom hook

--- src/applications/edu-benefits/0839/pages/institutionDetailsFacility.js
+++ src/applications/edu-benefits/0839/pages/institutionDetailsFacility.js
@@ -1,12 +1,90 @@
-import { titleUI } from 'platform/forms-system/src/js/web-component-patterns';
+import {
+  addressSchema,
+  titleUI,
+  textUI,
+  textSchema,
+} from 'platform/forms-system/src/js/web-component-patterns';
+import InstitutionName from '../components/InstitutionName';
+import InstitutionAddress from '../components/InstitutionAddress';
+
+const facilityCodeUIValidation = (errors, fieldData, formData) => {
+  const details = formData?.institutionDetails || {};
+  const code = (fieldData || '').trim();
+
+  const badFormat = code.length > 0 && !/^[a-zA-Z0-9]{8}$/.test(code);
+  const notFound = details.institutionName === 'not found';
+  const ineligible = details.poeEligible === false;
+
+  if (badFormat || notFound) {
+    errors.addError(
+      'Please enter a valid 8-character facility code. To determine your facility code, refer to your WEAMS 22-1998 Report or contact your ELR.',
+    );
+  }
+  if (ineligible) {
+    errors.addError(
+      'This institution is unable to participate in the Principles of Excellence.',
+    );
+  }
+};
 
 const uiSchema = {
   ...titleUI("Please enter your institution's facility code"),
+  facilityCode: {
+    ...textUI({
+      title: 'Facility code',
+      hint: '',
+      errorMessages: {
+        required:
+          'Please enter a valid 8-character facility code. To determine your facility code, refer to your WEAMS 22-1998 Report or contact your ELR.',
+      },
+    }),
+    'ui:validations': [facilityCodeUIValidation],
+  },
+  institutionName: {
+    'ui:title': 'Institution name',
+    'ui:field': InstitutionName,
+    'ui:options': {
+      classNames: 'vads-u-margin-top--2',
+    },
+  },
+  institutionAddress: {
+    'ui:title': '',
+    'ui:field': InstitutionAddress,
+    'ui:options': {
+      classNames: 'vads-u-margin-top--2',
+      hideLabelText: true,
+    },
+  },
 };
 
 const schema = {
   type: 'object',
-  properties: {},
+  properties: {
+    facilityCode: textSchema,
+    institutionName: {
+      type: 'string',
+    },
+    institutionAddress: {
+      type: 'object',
+      properties: {
+        country: addressSchema().properties.country,
+        street: addressSchema().properties.street,
+        street2: {
+          ...addressSchema().properties.street2,
+          minLength: 0,
+        },
+        street3: {
+          ...addressSchema().properties.street3,
+          minLength: 0,
+        },
+        city: addressSchema().properties.city,
+        state: addressSchema().properties.state,
+        postalCode: addressSchema().properties.postalCode,
+      },
+      required: ['street', 'city', 'state', 'postalCode', 'country'],
+    },
+  },
+  required: ['facilityCode', 'institutionName', 'institutionAddress'],
 };
 
 export { uiSchema, schema };
