d1d5d8b4bfefa9cd9af2c05df7f1ddc388d186bc
Ni Chia <ni.chia@adhocteam.us>
2025-10-15 14:22:43 -0700

    remove initialState info from  Allergies test

--- src/applications/mhv-medical-records/tests/containers/Allergies.unit.spec.jsx
+++ src/applications/mhv-medical-records/tests/containers/Allergies.unit.spec.jsx
@@ -11,30 +11,7 @@ import { convertAllergy } from '../../reducers/allergies';
 
 describe('Allergies list container', () => {
   const initialState = {
-    featureToggles: {
-      /* eslint-disable camelcase */
-      mhv_accelerated_delivery_enabled: false,
-      mhv_accelerated_delivery_allergies_enabled: false,
-      /* eslint-enable camelcase */
-      loading: false,
-    },
-    drupalStaticData: {
-      vamcEhrData: {
-        loading: false,
-      },
-    },
-    user: {
-      ...user,
-      profile: {
-        ...user.profile,
-        facilities: [
-          {
-            facilityId: '983',
-            isCerner: false,
-          },
-        ],
-      },
-    },
+    user,
     mr: {
       allergies: {
         allergiesList: allergies.entry.map(item =>
