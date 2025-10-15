a66c6cabe24bc7847f8263410451fafdff507ee8
Ni Chia <ni.chia@adhocteam.us>
2025-10-15 14:39:52 -0700

    put back the initialState for Allergies test

--- src/applications/mhv-medical-records/tests/containers/Allergies.unit.spec.jsx
+++ src/applications/mhv-medical-records/tests/containers/Allergies.unit.spec.jsx
@@ -11,7 +11,30 @@ import { convertAllergy } from '../../reducers/allergies';
 
 describe('Allergies list container', () => {
   const initialState = {
-    user,
+    featureToggles: {
+      /* eslint-disable camelcase */
+      mhv_accelerated_delivery_enabled: false,
+      mhv_accelerated_delivery_allergies_enabled: false,
+      /* eslint-enable camelcase */
+      loading: false,
+    },
+    drupalStaticData: {
+      vamcEhrData: {
+        loading: false,
+      },
+    },
+    user: {
+      ...user,
+      profile: {
+        ...user.profile,
+        facilities: [
+          {
+            facilityId: '983',
+            isCerner: false,
+          },
+        ],
+      },
+    },
     mr: {
       allergies: {
         allergiesList: allergies.entry.map(item =>
