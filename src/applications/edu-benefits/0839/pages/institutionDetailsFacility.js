f6aa3c2e3cdc80c5ee23b2bfe5506eb3edbfb2fd
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-10-15 08:59:52 -0700

    refactor config for institutionDetails

--- src/applications/edu-benefits/0839/pages/institutionDetailsFacility.js
+++ src/applications/edu-benefits/0839/pages/institutionDetailsFacility.js
@@ -29,55 +29,57 @@ const facilityCodeUIValidation = (errors, fieldData, formData) => {
 };
 
 const uiSchema = {
-  ...titleUI("Please enter your institution's facility code"),
-  'view:additionalInstructions': {
-    'ui:description': (
-      <va-link
-        text="Review additional instructions for the Yellow Ribbon Program Agreement"
-        href="/school-administrators/submit-yellow-ribbon-program-agreement-form-22-0839/yellow-ribbon-instructions"
-        external
-      />
-    ),
-  },
-  facilityCode: {
-    ...textUI({
-      title: 'Facility code',
-      hint: '',
-      errorMessages: {
-        required:
-          'Please enter a valid 8-character facility code. To determine your facility code, refer to your WEAMS 22-1998 Report or contact your ELR.',
+  institutionDetails: {
+    ...titleUI("Please enter your institution's facility code"),
+    'view:additionalInstructions': {
+      'ui:description': (
+        <va-link
+          text="Review additional instructions for the Yellow Ribbon Program Agreement"
+          href="/school-administrators/submit-yellow-ribbon-program-agreement-form-22-0839/yellow-ribbon-instructions"
+          external
+        />
+      ),
+    },
+    facilityCode: {
+      ...textUI({
+        title: 'Facility code',
+        hint: '',
+        errorMessages: {
+          required:
+            'Please enter a valid 8-character facility code. To determine your facility code, refer to your WEAMS 22-1998 Report or contact your ELR.',
+        },
+      }),
+      'ui:validations': [facilityCodeUIValidation],
+    },
+    institutionName: {
+      'ui:title': 'Institution name and address',
+      'ui:field': InstitutionName,
+      'ui:options': {
+        classNames: 'vads-u-margin-top--2',
       },
-    }),
-    'ui:validations': [facilityCodeUIValidation],
-  },
-  institutionName: {
-    'ui:title': 'Institution name and address',
-    'ui:field': InstitutionName,
-    'ui:options': {
-      classNames: 'vads-u-margin-top--2',
     },
-  },
-  institutionAddress: {
-    'ui:title': '',
-    'ui:field': InstitutionAddress,
-    'ui:options': {
-      classNames: 'vads-u-margin-top--2',
-      hideLabelText: true,
+    institutionAddress: {
+      'ui:title': '',
+      'ui:field': InstitutionAddress,
+      'ui:options': {
+        classNames: 'vads-u-margin-top--2',
+        hideLabelText: true,
+      },
     },
-  },
-  'view:warningBanner': {
-    'ui:description': formData => {
-      const notIHL = formData?.institutionDetails?.ihlEligible === false;
-      if (notIHL) {
-        return (
-          <va-alert
-            status="warning"
-            visible
-            headline="This institution is unable to participate in the Yellow Ribbon Program."
-          />
-        );
-      }
-      return null;
+    'view:warningBanner': {
+      'ui:description': formData => {
+        const notIHL = formData?.institutionDetails?.ihlEligible === false;
+        if (notIHL) {
+          return (
+            <va-alert
+              status="warning"
+              visible
+              headline="This institution is unable to participate in the Yellow Ribbon Program."
+            />
+          );
+        }
+        return null;
+      },
     },
   },
 };
@@ -85,39 +87,44 @@ const uiSchema = {
 const schema = {
   type: 'object',
   properties: {
-    'view:additionalInstructions': {
-      type: 'object',
-      properties: {},
-    },
-    facilityCode: textSchema,
-    institutionName: {
-      type: 'string',
-    },
-    institutionAddress: {
+    institutionDetails: {
       type: 'object',
       properties: {
-        country: addressSchema().properties.country,
-        street: addressSchema().properties.street,
-        street2: {
-          ...addressSchema().properties.street2,
-          minLength: 0,
+        'view:additionalInstructions': {
+          type: 'object',
+          properties: {},
         },
-        street3: {
-          ...addressSchema().properties.street3,
-          minLength: 0,
+        facilityCode: textSchema,
+        institutionName: {
+          type: 'string',
         },
-        city: addressSchema().properties.city,
-        state: addressSchema().properties.state,
-        postalCode: addressSchema().properties.postalCode,
+        institutionAddress: {
+          type: 'object',
+          properties: {
+            country: addressSchema().properties.country,
+            street: addressSchema().properties.street,
+            street2: {
+              ...addressSchema().properties.street2,
+              minLength: 0,
+            },
+            street3: {
+              ...addressSchema().properties.street3,
+              minLength: 0,
+            },
+            city: addressSchema().properties.city,
+            state: addressSchema().properties.state,
+            postalCode: addressSchema().properties.postalCode,
+          },
+          required: ['street', 'city', 'state', 'postalCode', 'country'],
+        },
+        'view:warningBanner': {
+          type: 'object',
+          properties: {},
+        },
       },
-      required: ['street', 'city', 'state', 'postalCode', 'country'],
-    },
-    'view:warningBanner': {
-      type: 'object',
-      properties: {},
+      required: ['facilityCode'],
     },
   },
-  required: ['facilityCode'],
 };
 
 export { uiSchema, schema };
