a9525f22eb913b4d332753b526ab01cd139c83ef
Isaac Gallup <igallup@thesocompany.com>
2025-10-15 11:15:08 -0500

    replace babel-register with package reference

--- babel-register.cjs
+++
@@ -1,9 +0,0 @@
-require('@babel/register')({
-  extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  ignore: [/node_modules/],
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    ['@babel/preset-react', { runtime: 'automatic' }],
-  ],
-  cache: false,
-});
