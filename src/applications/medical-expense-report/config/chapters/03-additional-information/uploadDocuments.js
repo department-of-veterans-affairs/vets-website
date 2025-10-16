04e5e147f28463f1b6cfbd8eaa6cf0ea236083dd
Daniel Gading <2748839+dgading@users.noreply.github.com>
2025-10-16 08:54:42 -0400

    Add new drop down with document selection to uploads

--- src/applications/medical-expense-report/config/chapters/03-additional-information/uploadDocuments.js
+++ src/applications/medical-expense-report/config/chapters/03-additional-information/uploadDocuments.js
@@ -4,10 +4,123 @@ import {
   fileInputMultipleUI,
   fileInputMultipleSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
+import { VaSelect } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import environment from 'platform/utilities/environment';
 
-const MAX_FILE_SIZE_MB = 20;
-const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1000 ** 2;
+const documentList = [
+  {
+    id: '1',
+    name: 'Birth Certificate',
+  },
+  {
+    id: '2',
+    name: 'Copy of a DD214',
+  },
+  {
+    id: '3',
+    name: 'Court papers / documents',
+  },
+  {
+    id: '4',
+    name: 'Death Certificate',
+  },
+  {
+    id: '5',
+    name: 'Disability Benefits Questionnaire (DBQ)',
+  },
+  {
+    id: '6',
+    name: 'Divorce Decree',
+  },
+  {
+    id: '7',
+    name: 'Goldmann Perimetry Chart / Field of Vision Chart',
+  },
+  {
+    id: '8',
+    name: 'Marriage Certificate',
+  },
+  {
+    id: '9',
+    name: 'Medical Treatment Record - Non-Government Facility',
+  },
+  {
+    id: '10',
+    name: 'Military Personnel Record',
+  },
+  {
+    id: '11',
+    name: 'Photographs',
+  },
+  {
+    id: '12',
+    name: 'VA Form 21-686c - Declaration of Status of Dependents',
+  },
+  {
+    id: '13',
+    name: 'VA Form 21-674 - Request for Approval of School Attendance',
+  },
+  {
+    id: '14',
+    name: 'VA Form 21-4142 - Authorization To Disclose Information',
+  },
+  {
+    id: '15',
+    name: 'VA Form 21-4142a - General Release for Medical Provider Information',
+  },
+  {
+    id: '16',
+    name:
+      'VA Form 21-4502 - Application for Automobile or Other Conveyance and Adaptive Equipment Under 38 U.S.C 3901-3904',
+  },
+  {
+    id: '17',
+    name:
+      'VA Form 21-8940 - Veterans Application for Increased Compensation Based on Un-employability',
+  },
+  {
+    id: '18',
+    name:
+      'VA Form 26-4555 - Application in Acquiring Specially Adapted Housing or Special Home Adaption Grant',
+  },
+  {
+    id: '19',
+    name:
+      'VA Form 21-4192 - Request for Employment Information in Connection with Claim for Disability',
+  },
+  {
+    id: '20',
+    name:
+      'VA Form 21-0779 - Request for Nursing home Information in Connection with Claim for Aid & Attendance',
+  },
+  {
+    id: '21',
+    name:
+      'VA Form 21-2680 - Examination for Household Status or Permanent Need for Regular Aid & Attendance',
+  },
+  {
+    id: '22',
+    name:
+      'VA Form 21-0781 - Statement in Support of Claimed Mental Health Disorder(s) Due to an In-Service Traumatic Event(s)',
+  },
+  {
+    id: '23',
+    name:
+      'VA Form 21-0781a - Statement in Support of Claim for PTSD Secondary to Personal Assault',
+  },
+  {
+    id: '24',
+    name: 'Other Correspondence',
+  },
+  {
+    id: '25',
+    name: 'STR - Dental - Photocopy',
+  },
+  {
+    id: '26',
+    name: 'STR - Medical - Photocopy',
+  },
+];
 
 const UploadMessage = (
   <p>
@@ -29,9 +142,35 @@ export default {
         'You can upload a .pdf, .jpg, .jpeg, or .png file. Your file should be no larger than 50 MB (non-PDF) or 99 MB (PDF only).',
       required: false,
       fileUploadUrl: `${environment.API_URL}/v0/claim_attachments`,
-      maxSize: MAX_FILE_SIZE_BYTES,
+      maxFileSize: 1024 * 1024 * 5,
       accept: '.pdf,.jpg,.jpeg,.png',
       formNumber: '21P-8416',
+      errorMessages: {
+        additionalInput: 'Select a type of document',
+      },
+      additionalInputRequired: true,
+      additionalInput: () => {
+        return (
+          <VaSelect required label="What type of document is this?">
+            {documentList.map(doc => (
+              <option key={doc.id} value={doc.name}>
+                {doc.name}
+              </option>
+            ))}
+          </VaSelect>
+        );
+      },
+      additionalInputUpdate: (instance, error, data) => {
+        instance.setAttribute('error', error);
+        if (data) {
+          instance.setAttribute('value', data.documentStatus);
+        }
+      },
+      handleAdditionalInput: e => {
+        const { value } = e.detail;
+        if (value === '') return null;
+        return { documentStatus: e.detail.value };
+      },
     }),
     'view:uploadMessage': {
       'ui:description': UploadMessage,
