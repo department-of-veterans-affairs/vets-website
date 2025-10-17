b632f9230cdcc37c7a424c9bebb554db8e16fe90
John Woolschlager <wullaski@gmail.com>
2025-10-17 13:54:15 -0400

    Replaced missing export

--- src/applications/vaos/tests/mocks/constants.js
+++ src/applications/vaos/tests/mocks/constants.js
@@ -1,4 +1,5 @@
-const mockToday = new Date('2025-06-02T12:00:00Z');
+export const mockToday = new Date('2025-06-02T12:00:00Z');
+export const mockTodayPlus6Hours = new Date('2025-06-02T18:00:00Z');
 
 module.exports = {
   mockToday,
