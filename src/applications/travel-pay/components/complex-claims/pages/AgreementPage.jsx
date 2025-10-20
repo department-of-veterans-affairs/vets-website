94023ac7b4118756220e8621e2eab432e08ee0b2
Peri-Ann McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-10-20 09:09:36 -0600

    Add complex claims agreement page (#39449)
    * added flipper
    
    * added complex claim flow wrapper
    
    * fix typo in file name
    
    * add complex claim submit flow wrapper and agreement page
    
    * set headers to blod paragraphs instead
    
    * added tests
    
    * updated logic and added testids

---
+++ src/applications/travel-pay/components/complex-claims/pages/AgreementPage.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+import {
+  VaCheckbox,
+  VaButtonPair,
+} from '@department-of-veterans-affairs/component-library/dist/react-bindings';
+import TravelAgreementContent from '../../TravelAgreementContent';
+
+const AgreementPage = () => {
+  const [isAgreementChecked, setIsAgreementChecked] = useState(false);
+  const [isAgreementError, setIsAgreementError] = useState(false);
+  const onSubmit = () => {
+    if (!isAgreementChecked) {
+      setIsAgreementError(true);
+    } else {
+      setIsAgreementError(false);
+      // TODO Add logic for Submitting the claim
+    }
+  };
+  const onBack = () => {
+    // TODO Add logic to go to previous Review Page
+  };
+  return (
+    <>
+      <h1>Beneficiary travel agreement</h1>
+      <p className="vads-u-font-weight--bold vads-u-font-family--sans vads-u-display--inline">
+        Penalty statement:
+      </p>{' '}
+      <p className="vads-u-display--inline">
+        There are severe criminal and civil penalties, including a fine,
+        imprisonment, or both, for knowingly submitting a false, fictitious, or
+        fraudulent claim.
+      </p>
+      <p>
+        By submitting this claim, you agree to the beneficiary travel agreement.
+      </p>
+      <TravelAgreementContent />
+      <VaCheckbox
+        data-testid="agreement-checkbox"
+        className="vads-u-margin-x--1 vads-u-margin-y--2"
+        checked={isAgreementChecked}
+        name="accept-agreement"
+        description={null}
+        error={
+          isAgreementError
+            ? 'You must accept the beneficiary travel agreement before continuing.'
+            : null
+        }
+        hint={null}
+        label="I confirm that the information is true and correct to the best of my knowledge and belief. I’ve read and I accept the beneficiary travel agreement."
+        onVaChange={() => setIsAgreementChecked(!isAgreementChecked)}
+        required
+      />
+      <VaButtonPair
+        data-testid="agreement-button-pair"
+        className="vads-u-margin-top--2"
+        continue
+        disable-analytics
+        rightButtonText="Submit claim"
+        leftButtonText="Back"
+        onPrimaryClick={onSubmit}
+        onSecondaryClick={onBack}
+      />
+    </>
+  );
+};
+
+export default AgreementPage;
