bb1dce57b5904a85a4a39e221233a353b99cebc0
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-10-20 10:43:25 -0700

    Update content in out of bounds alert

--- src/applications/travel-pay/components/alerts/OutOfBoundsAppointmentAlert.jsx
+++ src/applications/travel-pay/components/alerts/OutOfBoundsAppointmentAlert.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import { HelpTextModalities, HelpTextGeneral } from '../HelpText';
+import { HelpTextGeneral } from '../HelpText';
 
 export default function OutOfBoundsAppointmentAlert() {
   return (
     <va-alert closeable="false" status="warning" role="status" visible>
       <h2 slot="headline">Your appointment is older than 30 days</h2>
       <p className="vads-u-margin-y--2">
-        We can't file this claim in this tool at this time.
+        We still encourage you to file now, but we may not be able to approve
+        your claim.
       </p>
-      <HelpTextModalities />
       <p className="vads-u-font-weight--bold">
         How can I get help with my claim?
       </p>
