b3a9e4b72f51795ced61642b64f34c98c41a7d1f
Justin Linn <justin@linnjs.com>
2025-10-20 10:35:51 -0400

    fix(2680): Fix linter warnings

--- src/applications/benefits-optimization-aquia/21-2680-house-bound-status/pages/index.js
+++ src/applications/benefits-optimization-aquia/21-2680-house-bound-status/pages/index.js
@@ -3,6 +3,18 @@
  * @description Barrel export file for form page configurations
  */
 
+export { BenefitTypePage } from './benefit-type';
+export { VeteranIdentityPage } from './veteran-identity';
+export { ClaimantIdentityPage } from './claimant-identity';
+export { HospitalizationPage } from './hospitalization';
+export { ClaimantSignaturePage } from './claimant-signature';
+export { ExaminerIdentificationPage } from './examiner-identification';
+export { MedicalDiagnosisPage } from './medical-diagnosis';
+export { ADLAssessmentPage } from './adl-assessment';
+export { FunctionalLimitationsPage } from './functional-limitations';
+export { NarrativeAssessmentPage } from './narrative-assessment';
+export { ExaminerSignaturePage } from './examiner-signature';
+
 /** @exports {PageSchema} identificationInformation - SSN/VA file number page */
 export {
   default as identificationInformation,
