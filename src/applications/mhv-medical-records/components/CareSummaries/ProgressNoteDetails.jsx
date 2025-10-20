8955592b3f0f9220c30a630d8852a730f0473a86
Napoleon K. <95312667+GovNapoleon@users.noreply.github.com>
2025-10-20 15:51:29 -0500

    improve print accessibility in src/applications/mhv-medical-records/components/CareSummaries/ProgressNoteDetails.jsx

--- src/applications/mhv-medical-records/components/CareSummaries/ProgressNoteDetails.jsx
+++ src/applications/mhv-medical-records/components/CareSummaries/ProgressNoteDetails.jsx
@@ -111,12 +111,12 @@ ${record.note}`;
         />
 
         {downloadStarted && <DownloadSuccessAlert />}
+        <DownloadingRecordsInfo description="CS&N Detail" />
         <PrintDownload
           description="CS&N Detail"
           downloadPdf={generateCareNotesPDF}
           downloadTxt={generateCareNotesTxt}
         />
-        <DownloadingRecordsInfo description="CS&N Detail" />
 
         <div className="test-details-container max-80">
           <HeaderSection header="Details">
