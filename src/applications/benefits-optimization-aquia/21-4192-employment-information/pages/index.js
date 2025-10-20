4d594fee7f6810fa0f52b4bc03489c8733d5bd07
Justin Linn <justin@linnjs.com>
2025-10-20 10:54:46 -0400

    fix(bio-shared-aq): Fix linter warnings

--- src/applications/benefits-optimization-aquia/21-4192-employment-information/pages/index.js
+++ src/applications/benefits-optimization-aquia/21-4192-employment-information/pages/index.js
@@ -12,10 +12,3 @@ export { EmploymentLastPaymentPage } from './employment-last-payment';
 export { DutyStatusPage } from './duty-status';
 export { BenefitsInformationPage } from './benefits-information';
 export { RemarksPage } from './remarks';
-
-export { default as nameAndDateOfBirth } from './name-and-date-of-birth';
-export {
-  default as identificationInformation,
-} from './identification-information';
-export { default as mailingAddress } from './mailing-address';
-export { default as phoneAndEmailAddress } from './phone-and-email-address';
