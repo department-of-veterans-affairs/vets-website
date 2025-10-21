40ebcabc3e950b96a4d9e78cb7f8c884758cb477
Ian Harrison <ian.harrison@adhocteam.us>
2025-10-20 19:31:39 -0500

    remove logs

--- src/platform/testing/e2e/cypress/support/commands/webComponents.js
+++ src/platform/testing/e2e/cypress/support/commands/webComponents.js
@@ -118,13 +118,11 @@ Cypress.Commands.add('selectVaComboBox', (field, value) => {
         ? cy.get(`va-combo-box[name="${field}"]`)
         : cy.wrap(field);
 
-    cy.task('log', 'got the element');
     element
       .shadow()
       .find('input')
       .as('inputElement');
 
-    cy.task('log', 'got the input');
     cy.get('@inputElement').click();
 
     cy.get('@inputElement').clear(DELAY_OPTION);
@@ -139,22 +137,15 @@ Cypress.Commands.add('selectVaComboBox', (field, value) => {
       .find('select')
       .as('selectElement');
 
-    cy.task('log', 'got the select');
-
     cy.get('@selectElement')
       .find(`option[value="${strValue}"]`)
       .invoke('text')
       .as('optionLabel');
 
-    cy.task('log', 'got the option label');
-
     cy.get('@optionLabel').then(label => {
-      cy.task('log', `typing the label ${label}`);
       cy.get('@inputElement').type(label, FORCE_OPTION);
       cy.get('@inputElement').type('{enter}');
-      cy.task('log', 'selected the option');
     });
-    cy.task('log', 'all done');
   }
 });
 
