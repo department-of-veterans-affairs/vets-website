a15fefda787f9cf3fa1f249a769de11ac43964f2
Brandyn Sullins <29178824+OptionSelect@users.noreply.github.com>
2025-10-21 08:13:42 -0400

    Combined Debt Portal - OTPP Feedback Batch 2025 (#39398)
    * 121470 and 121468
    
    * 121463
    
    * 121467
    
    * 121466
    
    * Update to lettercode via Dave Formanek feedback.
    
    * Remove sr-only changes per Joe feedback.
    
    * Update routes and breadcrumbs
    
    * Update tests.
    
    * Fix resolve cypress.
    
    * Update HowDoIPay to support Receivable ID and UI enhancements
    
    Added conditional display for 'Receivable ID' in place of 'File Number' when available, including updated field descriptions. Introduced a new icon and related styles for Pay Online faux action link, and updated propTypes to include receivableId. Also rewrote the 'Person entitled' definition for plain language feedback.
    
    * Update page titles and breadcrumbs for clarity
    
    Standardized titles and breadcrumb labels by removing 'Current' from 'Current overpayment balances' and 'Current copay balances', now using 'Overpayment balances' and 'Copay balances' throughout debt and copay portal pages. Also updated resolve page titles and breadcrumb labels for consistency and improved user understanding.
    
    * Update FinancialHelp text and va-link-action props
    
    Replaces 'debt relief options' with 'relief options' for clarity and updates the 'secondary' prop to 'type="secondary"' on va-link-action components to match expected prop usage.
    
    * Update financial help options to match copays
    
    Revised the list of financial relief options to provide clearer descriptions for repayment plans, compromise offers, and waivers. Improved readability and user understanding of each option.
    
    * Update test assertions for page titles in CDP OTPP e2e tests
    
    Modified Cypress test assertions to check for updated page titles: 'Resolve your copay' and 'Resolve your overpayment' instead of the previous, more specific titles. This aligns the tests with recent UI text changes.
    
    * Replace va-link-action with va-link in HowToPay
    
    Updated the HowToPay component to use the `va-link` component instead of `va-link-action` for the pay.gov link to be able to open in a new tab. Added a `va-icon` and improved accessibility and styling with appropriate attributes and classes.
    
    * Refactor Need Help section into reusable component
    
    Extracted the 'Need Help' section into a new NeedHelpCopay component and replaced duplicate code in DetailCopayPage, HTMLStatementPage, ResolvePage, and SummaryPage. This improves code reuse and maintainability.
    
    * Fix cypress test id.
    
    ---------
    
    Co-authored-by: Joseph Lee <joseph@cityfriends.tech>

--- src/applications/combined-debt-portal/debt-letters/components/HowDoIGetHelp.jsx
+++ src/applications/combined-debt-portal/debt-letters/components/HowDoIGetHelp.jsx
@@ -20,19 +20,22 @@ const HowDoIGetHelp = ({
           How to get financial help
         </h2>
 
-        <p className="vads-u-margin-top--2">
-          If you need financial help, you can request:
-        </p>
-
+        <p>You can request these relief options:</p>
         <ul>
           <li>
-            An extended monthly payment plan, <strong>or</strong>
+            <strong>Repayment plan.</strong> This would allow you to repay the
+            debt in smaller monthly amounts over time.
           </li>
           <li>
-            A compromise (ask us to accept a lower amount of money as full
-            payment of the debt), <strong>or</strong>
+            <strong>Compromise offer.</strong> This means you offer a lesser
+            one-time lump sum amount as full payment of the debt. If we approve
+            your request, you’ll have to pay the one-time amount within 30 days.
           </li>
-          <li>A waiver (ask us to stop collection on the debt)</li>
+          <li>
+            <strong>Waiver.</strong> This means you ask us to forgive (or
+            “waive”) part or all of the debt. If we approve your request, you
+            won’t have to pay the amount waived.
+          </li>
         </ul>
         {showVHAPaymentHistory || showOneThingPerPage ? (
           <p>
@@ -52,6 +55,7 @@ const HowDoIGetHelp = ({
           href="/manage-va-debt/request-debt-help-form-5655/"
           message-aria-describedby="Opens pay.va.gov"
           text="Request help with your debt"
+          type="secondary"
         />
       </section>
       {disputeDebtActive && <HowDoIDispute />}
