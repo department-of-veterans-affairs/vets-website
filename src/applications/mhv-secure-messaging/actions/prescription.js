43b44032d4942218938771379ce0c0538109bba6
Alex Morgun <87077843+alex-morgun-1@users.noreply.github.com>
2025-10-22 11:05:48 -0400

    Refactor getPrescriptionById action and add unit tests for prescription actions

--- src/applications/mhv-secure-messaging/actions/prescription.js
+++ src/applications/mhv-secure-messaging/actions/prescription.js
@@ -2,20 +2,26 @@ import { Actions } from '../util/actionTypes';
 import { getPrescriptionById as apiGetPrescriptionById } from '../api/RxApi';
 
 export const getPrescriptionById = prescriptionId => async dispatch => {
+  dispatch({ type: Actions.Prescriptions.CLEAR_PRESCRIPTION });
   try {
-    dispatch({ type: Actions.Prescriptions.CLEAR_PRESCRIPTION });
     dispatch({ type: Actions.Prescriptions.IS_LOADING });
+    if (!prescriptionId || prescriptionId === 'undefined') {
+      throw new Error('Prescription ID is required');
+    }
     const response = await apiGetPrescriptionById(prescriptionId);
     if (response?.errors) {
-      throw new Error('API returned errors');
+      throw new Error(response.errors[0]?.title || 'API returned errors');
     }
     dispatch({
       type: Actions.Prescriptions.GET_PRESCRIPTION_BY_ID,
       payload: response.data?.attributes,
     });
   } catch (e) {
-    const error = e.errors[0] || e;
-    const errorPayload = error?.status === '404' ? error.title : error;
+    const error = e?.errors?.[0] || e;
+    const errorPayload =
+      error?.status === '404'
+        ? error.title
+        : error.title || error.detail || error.message || error;
     dispatch({
       type: Actions.Prescriptions.GET_PRESCRIPTION_BY_ID_ERROR,
       payload: errorPayload,
