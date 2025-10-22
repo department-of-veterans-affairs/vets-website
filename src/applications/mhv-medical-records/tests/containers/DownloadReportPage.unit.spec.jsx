264d169ee3c76f01a91cc2de789db33067c335b6
Aswin Malla <aswinmalla12@gmail.com>
2025-10-22 07:54:54 -0600

    Fix test: Use getByRole for My VA Health link query

--- src/applications/mhv-medical-records/tests/containers/DownloadReportPage.unit.spec.jsx
+++ src/applications/mhv-medical-records/tests/containers/DownloadReportPage.unit.spec.jsx
@@ -473,7 +473,8 @@ describe('DownloadRecordsPage for Cerner users', () => {
     });
 
     expect(screen.getByTestId('cerner-facilities-alert')).to.exist;
-    expect(screen.getByText('Go to My VA Health')).to.exist;
+    const link = screen.getByRole('link', { name: 'Go to My VA Health' });
+    expect(link).to.exist;
   });
 });
 
