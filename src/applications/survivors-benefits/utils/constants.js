5c26b75c4f2ee637a28effbaff240f22cfb39f48
Daniel Gading <2748839+dgading@users.noreply.github.com>
2025-10-23 12:38:16 -0400

    Update subtitle and README

--- src/applications/survivors-benefits/utils/constants.js
+++ src/applications/survivors-benefits/utils/constants.js
@@ -1,4 +1,4 @@
 export const TITLE =
   'Apply for DIC, Survivors Pension, or accrued benefits online';
 export const SUBTITLE =
-  'For spouses and children of deceased Veterans only (VA Form 21-534EZ)';
+  'For spouses and children of deceased Veterans only (VA Form 21P-534EZ)';
