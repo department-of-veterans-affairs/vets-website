3e1bfef4115d04fe81a2ca20856fcfb01544a5f4
Victoria (Vic) Saleem <59583325+vsaleem@users.noreply.github.com>
2025-10-24 08:39:12 -0500

    Update uploadDocuments.js

--- src/applications/survivors-benefits/config/chapters/07-additional-information/uploadDocuments.js
+++ src/applications/survivors-benefits/config/chapters/07-additional-information/uploadDocuments.js
@@ -23,11 +23,11 @@ export default {
     files: fileInputMultipleUI({
       title: 'Select a file to upload',
       hint:
-        'You can upload a .pdf, .jpg, .jpeg, or .png file. Your file should be no larger than 50 MB (non-PDF) or 99 MB (PDF only).',
+        'You can upload a .pdf, .jpg, .jpeg, .bmp, or .txt file. Your file should be no larger than 50 MB (non-PDF) or 99 MB (PDF only).',
       required: false,
       fileUploadUrl: `${environment.API_URL}/v0/claim_attachments`,
       maxFileSize: 103809024, // 99 MB for PDFs
-      accept: '.pdf,.jpg,.jpeg,.png',
+      accept: '.pdf,.jpg,.jpeg,.bmp,.txt',
       formNumber: '21-534EZ',
     }),
     'view:uploadMessage': {
