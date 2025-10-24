e9bd0694f997d7ec7e72c35bc8ce759c33ffefd5
Adam Suharja <adam.suharja@oddball.io>
2025-10-24 09:49:31 -0700

    Revert "Slim alert"
    This reverts commit c80271deecf8ffe8112da89ecc70e4edd66a93c5.

--- src/platform/user/profile/vap-svc/components/base/VAPServiceEditModalErrorMessage.jsx
+++ src/platform/user/profile/vap-svc/components/base/VAPServiceEditModalErrorMessage.jsx
@@ -75,7 +75,7 @@ export default function VAPServiceEditModalErrorMessage({
 
   return (
     <>
-      <va-alert background-only status="error" visible uswds slim>
+      <va-alert background-only status="error" visible uswds>
         <div className="vads-u-display--flex vads-u-align-items--baseline">
           <span className="sr-only">Alert: </span>
           <div role="alert">{content}</div>
