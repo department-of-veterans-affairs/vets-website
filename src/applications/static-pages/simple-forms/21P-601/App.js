ffe57c5242eb6667942dc79033061f5bbc0aad1c
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2025-10-24 16:11:21 -0500

    content updates

--- src/applications/static-pages/simple-forms/21P-601/App.js
+++ src/applications/static-pages/simple-forms/21P-601/App.js
@@ -13,42 +13,57 @@ const App = ({ formEnabled }) => {
     return <va-loading-indicator message="Loading..." />;
   }
 
-  const headerText = 'If you’re requesting reimbursement for expenses';
-  const dlLink = (
-    <va-link
-      href="https://www.va.gov/find-forms/about-form-21p-601/"
-      text="Get VA Form 21P-601 to download"
-    />
-  );
-
-  if (formEnabled) {
-    return (
-      <>
-        <h2>{headerText}</h2>
-        <p>
-          You should fill out an Application for Accrued Amounts Due a Deceased
-          Beneficiary (VA Form 21P-601). You can submit this form online or by
-          mail.
-        </p>
-
-        <va-link-action
-          href={manifest.rootUrl}
-          text="Apply for accrued benefits online"
-          type="secondary"
-        />
-        {dlLink}
-      </>
-    );
-  }
-
   return (
     <>
-      <h2>{headerText}</h2>
+      {/* 534EZ */}
+      <h2>If you’re the surviving spouse or child of a Veteran</h2>
+      <p>
+        You should fill out an Application for DIC, Survivors Pension, and/or
+        Accrued Benefits (VA Form 21P-534EZ). You can submit this form by mail.
+      </p>
+
+      <va-link
+        href="https://www.va.gov/find-forms/about-form-21p-534ez/"
+        text="Get VA Form 21P-534EZ to download"
+      />
+
+      {/* 535 */}
+      <h2>If you’re the surviving parent of a Veteran</h2>
+      <p>
+        You should fill out an Application for Dependency and Indemnity
+        Compensation by Parent(s) (VA Form 21P-535). You submit this form by
+        mail.
+      </p>
+
+      <va-link
+        href="https://www.va.gov/find-forms/about-form-21p-535/"
+        text="Get VA Form 21P-535 to download"
+      />
+
+      {/* 601 */}
+      <h2>If you’re requesting reimbursement for expenses</h2>
       <p>
         You should fill out an Application for Accrued Amounts Due a Deceased
-        Beneficiary (VA Form 21P-601). You can submit this form by mail.
+        Beneficiary (VA Form 21P-601). You can submit this form{' '}
+        {formEnabled ? 'online or ' : ''}
+        by mail.
       </p>
-      {dlLink}
+
+      {formEnabled && (
+        <>
+          <va-link-action
+            href={manifest.rootUrl}
+            text="Apply for accrued benefits online"
+            type="secondary"
+          />
+          <br />
+        </>
+      )}
+
+      <va-link
+        href="https://www.va.gov/find-forms/about-form-21p-601/"
+        text="Get VA Form 21P-601 to download"
+      />
     </>
   );
 };
