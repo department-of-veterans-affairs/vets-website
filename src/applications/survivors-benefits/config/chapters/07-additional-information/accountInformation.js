02895b6a81224f3786cec8effe2d95fdfff8cac0
Victoria (Vic) Saleem <59583325+vsaleem@users.noreply.github.com>
2025-10-24 08:36:42 -0500

    Refactor direct deposit helpers and update import
    Moved the usingDirectDeposit function to a new helpers.js file and updated its import in accountInformation.js. Also refactored accountInformation.js for improved readability by separating uiSchema and schema definitions.

--- src/applications/survivors-benefits/config/chapters/07-additional-information/accountInformation.js
+++ src/applications/survivors-benefits/config/chapters/07-additional-information/accountInformation.js
@@ -4,29 +4,27 @@ import {
   titleUI,
 } from 'platform/forms-system/src/js/web-component-patterns';
 import { AccountInformationAlert } from '../../../components/FormAlerts';
-import { usingDirectDeposit } from './helper';
+import { usingDirectDeposit } from './helpers';
+
+const uiSchema = {
+  ...titleUI('Account information for direct deposit', AccountInformationAlert),
+  bankAccount: bankAccountUI({
+    labels: {
+      accountNumberLabel: 'Account number',
+      routingNumberLabel: 'Routing number',
+    },
+  }),
+};
+
+const schema = {
+  type: 'object',
+  properties: {
+    bankAccount: bankAccountSchema(),
+  },
+};
 
 export default {
-  title: 'Account information for direct deposit',
-  path: 'additional-information/account-information',
-  initialData: {},
   depends: usingDirectDeposit,
-  uiSchema: {
-    ...titleUI(
-      'Account information for direct deposit',
-      AccountInformationAlert,
-    ),
-    bankAccount: bankAccountUI({
-      labels: {
-        accountNumberLabel: 'Account number',
-        routingNumberLabel: 'Routing number',
-      },
-    }),
-  },
-  schema: {
-    type: 'object',
-    properties: {
-      bankAccount: bankAccountSchema(),
-    },
-  },
+  uiSchema,
+  schema,
 };
