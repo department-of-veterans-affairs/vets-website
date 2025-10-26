81e4b8305a2717ef42ce0fb03d4bef5ef8b561eb
Napoleon K. <95312667+GovNapoleon@users.noreply.github.com>
2025-10-26 13:25:43 -0500

    121762 send relativePath to datadogRum

--- src/applications/mhv-medical-records/actions/conditions.js
+++ src/applications/mhv-medical-records/actions/conditions.js
@@ -35,9 +35,8 @@ export const getConditionsList = (
   } catch (error) {
     dispatch(addAlert(Constants.ALERT_TYPE_ERROR, error));
     datadogRum.addError(error, {
-      app: 'mhv-medical-records',
-      folder: 'action',
-      file: 'conditions.js',
+      relativePath:
+        'src/applications/mhv-medical-records/actions/conditions.js',
       functionName: 'getConditionsList',
     });
   }
