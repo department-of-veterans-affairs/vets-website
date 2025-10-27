0a70bfb0de31606150cc6cb55509fbfc138f5fde
Matt Long <6738544+longmd@users.noreply.github.com>
2025-10-27 11:33:49 -0400

    10-10d extended | Update applicant section content (#39565)

--- src/applications/ivc-champva/10-10d-extended/constants.js
+++ src/applications/ivc-champva/10-10d-extended/constants.js
@@ -1,1 +1,10 @@
-export const MAX_APPLICANTS = 25;
+export const APPLICANTS_MAX = 25;
+
+export const ATTACHMENT_IDS = Object.freeze({
+  marriageCert: 'Marriage certificate',
+  schoolCertificationForm: 'School enrollment certification form',
+  schoolEnrollmentLetter: 'Enrollment letter',
+});
+
+export const FILE_SIZE_MAX = '20MB';
+export const FILE_TYPES_ACCEPTED = ['jpg', 'pdf', 'png'];
