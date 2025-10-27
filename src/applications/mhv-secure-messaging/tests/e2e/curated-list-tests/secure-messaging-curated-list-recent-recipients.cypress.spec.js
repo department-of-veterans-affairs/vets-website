099ee3bf0fc68f1ba466d9ef95ca53696711f7d1
Sofia Cordero <sofiadcordero@gmail.com>
2025-10-27 14:43:25 -0400

    Update test assertion based on new header

--- src/applications/mhv-secure-messaging/tests/e2e/curated-list-tests/secure-messaging-curated-list-recent-recipients.cypress.spec.js
+++ src/applications/mhv-secure-messaging/tests/e2e/curated-list-tests/secure-messaging-curated-list-recent-recipients.cypress.spec.js
@@ -138,9 +138,7 @@ describe('SM CURATED LIST MAIN FLOW', () => {
   it('validate selecting a recent care team and continuing to compose message', () => {
     PatientInboxPage.clickCreateNewMessage();
     PatientInterstitialPage.continueToRecentRecipients();
-    GeneralFunctionsPage.verifyPageHeader(
-      `Care teams you recently sent messages to`,
-    );
+    GeneralFunctionsPage.verifyPageHeader(Data.RECENT_RECIPIENTS_HEADER);
 
     // Select the first recent care team
     cy.get(`[label="${recentCareTeams[0]}"]`).click();
