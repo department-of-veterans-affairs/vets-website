5a830e22f1322b5c7788591c670cc0869f382a65
Matt Long <6738544+longmd@users.noreply.github.com>
2025-10-27 08:16:21 -0400

    Remove unused constants

--- src/applications/ivc-champva/10-10d-extended/constants.js
+++ src/applications/ivc-champva/10-10d-extended/constants.js
@@ -7,5 +7,4 @@ export const ATTACHMENT_IDS = Object.freeze({
 });
 
 export const FILE_SIZE_MAX = '20MB';
-export const FILE_SIZE_MIN = '1KB';
 export const FILE_TYPES_ACCEPTED = ['jpg', 'pdf', 'png'];
