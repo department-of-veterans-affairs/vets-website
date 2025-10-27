fe25ebb997878d74a155b8e83ac61ad8987c36d6
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-10-27 12:14:16 -0700

    Add selectors

--- src/applications/travel-pay/redux/selectors.js
+++ src/applications/travel-pay/redux/selectors.js
@@ -1,1 +1,12 @@
 export const selectAppointment = state => state.travelPay.appointment;
+
+export const selectClaimDetails = (state, id) => ({
+  ...state.travelPay.claimDetails,
+  data: state.travelPay.claimDetails.data[id],
+});
+
+export const selectExpense = (state, claimId, expenseId) =>
+  state.travelPay.claimDetails[claimId].expenses.find(e => e.id === expenseId);
+
+export const selectExpenseLoadingState = state =>
+  state.travelPay.expense.isLoading;
