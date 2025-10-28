f61ac770161a863cbc97357b46ae8471200ced32
Bill Tran <bill@billjohntran.com>
2025-10-28 07:34:51 -0700

    add inforalert

--- src/applications/vaos/referral-appointments/components/ReferralErrorLayout.jsx
+++ src/applications/vaos/referral-appointments/components/ReferralErrorLayout.jsx
@@ -2,21 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReferralLayout from './ReferralLayout';
 import FindCommunityCareOfficeLink from './FindCCFacilityLink';
+import InfoAlert from '../../components/InfoAlert';
 
 /**
  * Common error layout component for referral appointment pages
  * when there's an issue fetching referral data
  */
+
 const ReferralErrorLayout = ({
   message = "We're having trouble getting your appointment information. Please try again later or call your facility's community care office.",
   showFindLink = true,
 }) => {
   return (
-    <ReferralLayout hasEyebrow heading="We're sorry. We've run into a problem">
-      <div>
-        <p>{message}</p>
-        {showFindLink && <FindCommunityCareOfficeLink />}
-      </div>
+    <ReferralLayout>
+      <InfoAlert
+        status="error"
+        headline="We’re sorry. We’ve run into a problem"
+      >
+        {message}
+      </InfoAlert>
+      {showFindLink && <FindCommunityCareOfficeLink />}
     </ReferralLayout>
   );
 };
