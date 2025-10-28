c4f83f1b1a282364e4478ab51cd9f1de3fad11d9
Aaron <3916436+amponce@users.noreply.github.com>
2025-10-28 06:27:39 -0700

    fix: your info and update cypress

---
+++ src/applications/simple-forms/21P-601/pages/waiverOfSubstitution.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  titleUI,
+  yesNoUI,
+  yesNoSchema,
+} from 'platform/forms-system/src/js/web-component-patterns';
+
+export default {
+  uiSchema: {
+    ...titleUI('Waiver of substitution'),
+    'view:substitutionInfo1': {
+      'ui:description': (
+        <p>
+          If the beneficiary had a pending claim or appeal, you can ask to take
+          over that claim or appeal on their behalf (also called substitution).
+          This lets you submit more evidence to support the claim or appeal for
+          potential accrued benefits.
+        </p>
+      ),
+    },
+    'view:substitutionInfo2': {
+      'ui:description': (
+        <p>
+          You can also waive your right to substitution. If you do, we'll still
+          consider any claims or appeals with the evidence we already have.
+        </p>
+      ),
+    },
+    wantsToWaiveSubstitution: yesNoUI({
+      title: 'Do you want to waive your right to substitution?',
+      labels: {
+        Y: 'Yes, I want to waive my right to substitution',
+        N: 'No, I want to take over pending claims or appeals',
+      },
+    }),
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      'view:substitutionInfo1': {
+        type: 'object',
+        properties: {},
+      },
+      'view:substitutionInfo2': {
+        type: 'object',
+        properties: {},
+      },
+      wantsToWaiveSubstitution: yesNoSchema,
+    },
+  },
+};
