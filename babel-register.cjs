27d0cac786a36ac65e07df177024e0a3f8ad1f70
Isaac Gallup <igallup@thesocompany.com>
2025-10-30 10:48:40 -0500

    adding node_path env to unit-tests job and added supporting code to babel-register.cjs

--- babel-register.cjs
+++ babel-register.cjs
@@ -1,3 +1,12 @@
+const path = require('path');
+const Module = require('module');
+
+const srcPath = path.join(__dirname, 'src');
+process.env.NODE_PATH = [srcPath, process.env.NODE_PATH]
+  .filter(Boolean)
+  .join(path.delimiter);
+Module._initPaths();
+
 const presets = [
   ['@babel/preset-env', { targets: { node: 'current' } }],
   ['@babel/preset-react', { runtime: 'automatic' }],
