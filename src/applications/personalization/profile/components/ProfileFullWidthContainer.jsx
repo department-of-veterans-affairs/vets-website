d918c14ddac4d4576b8a03d99f7103e8b4e881a0
Giles Wells <giles@mediagearhead.com>
2025-10-30 16:04:38 -0400

    9 col width on desktop

--- src/applications/personalization/profile/components/ProfileFullWidthContainer.jsx
+++ src/applications/personalization/profile/components/ProfileFullWidthContainer.jsx
@@ -6,7 +6,7 @@ export const ProfileFullWidthContainer = ({ children, profile2Enabled }) => {
   return (
     <div className="vads-l-grid-container vads-u-padding-x--0">
       <div className="vads-l-row">
-        <div className="vads-l-col--12 vads-u-padding-bottom--4 vads-u-padding-x--1 medium-screen:vads-u-padding-x--2 medium-screen:vads-u-padding-bottom--6">
+        <div className="vads-l-col--12 vads-u-padding-bottom--4 vads-u-padding-x--1 medium-screen:vads-l-col--9 medium-screen:vads-u-padding-x--2 medium-screen:vads-u-padding-bottom--6">
           {profile2Enabled && <ProfileBreadcrumbs />}
           {/* children will be passed in from React Router one level up */}
           {children}
