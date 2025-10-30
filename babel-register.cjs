1ef76186839fef5b7ac9f74b624d15ad41ec1e47
Isaac Gallup <igallup@thesocompany.com>
2025-10-30 15:22:16 -0500

    rip out old bandaid for babel transpiling

--- babel-register.cjs
+++ babel-register.cjs
@@ -1,12 +1,3 @@
-const path = require('path');
-const Module = require('module');
-
-const srcPath = path.join(__dirname, 'src');
-process.env.NODE_PATH = [srcPath, process.env.NODE_PATH]
-  .filter(Boolean)
-  .join(path.delimiter);
-Module._initPaths();
-
 const presets = [
   ['@babel/preset-env', { targets: { node: 'current' } }],
   ['@babel/preset-react', { runtime: 'automatic' }],
