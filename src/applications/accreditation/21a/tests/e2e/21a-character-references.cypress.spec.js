451a21b2585b1d4ae1ea630810727d5760040809
Patrick Brown <patrick.brown@oddball.io>
2025-10-30 13:42:20 -0400

    arp/character_reference_min_count

---
+++ src/applications/accreditation/21a/tests/e2e/21a-character-references.cypress.spec.js
@@ -0,0 +1,181 @@
+import user from './fixtures/mocks/user.json';
+import { setFeatureToggles } from './intercepts/feature-toggles';
+import inProgressFormsResponse from './fixtures/mocks/in-progress-forms-response.json';
+
+Cypress.Commands.add('loginArpUser', () => {
+  cy.intercept('GET', '/accredited_representative_portal/v0/user', {
+    statusCode: 200,
+    body: user,
+  }).as('fetchUser');
+});
+
+Cypress.Commands.add('addNewCharacterReference', () => {
+  cy.get('input[name="root_view:hasCharacterReferences"][value="Y"]').check({
+    force: true,
+  });
+
+  cy.findByRole('button', { name: /^Continue$/ }).click();
+});
+
+Cypress.Commands.add('goToNextPage', () => {
+  cy.get('input[name="root_view:hasCharacterReferences"][value="N"]').check({
+    force: true,
+  });
+  cy.findByRole('button', { name: /^Continue$/ }).click();
+});
+
+Cypress.Commands.add(
+  'createCharacterReference',
+  (first = 'Harry', last = 'Potter') => {
+    cy.fillVaTextInput('root_fullName_first', first);
+    cy.fillVaTextInput('root_fullName_last', last);
+    cy.findByRole('button', { name: /^Continue$/ }).click();
+
+    cy.get('select[name="root_address_country"]').select('USA');
+    cy.fillVaTextInput('root_address_street', '123 Main St');
+    cy.fillVaTextInput('root_address_city', 'Springfield');
+    cy.fillVaTextInput('root_address_postalCode', '12345');
+    cy.get('select[name="root_address_state"]').select('NY');
+    cy.findByRole('button', { name: /^Continue$/i }).click();
+
+    cy.get('va-telephone-input[label*="phone" i]')
+      .should('exist')
+      .shadow()
+      .find('input[type="tel"]')
+      .clear()
+      .type('5551234567');
+
+    cy.fillVaTextInput('root_email', 'test@example.com');
+    cy.findByRole('button', { name: /^Continue$/i }).click();
+
+    cy.get('select[name="root_relationship"]').select('Friend');
+    cy.findByRole('button', { name: /^Continue$/i }).click();
+  },
+);
+
+const baseUrl = '/representative/accreditation/attorney-claims-agent-form-21a/';
+const introUrl = `${baseUrl}introduction`;
+const personalInfoUrl = `${baseUrl}personal-information-intro`;
+const characterReferencesUrl = `${baseUrl}character-references`;
+const characterReferencesSummaryUrl = `${baseUrl}character-references-summary`;
+const supplementaryStatementsIntroUrl = `${baseUrl}supplementary-statements-intro`;
+
+const vamcUser = {
+  data: {
+    nodeQuery: {
+      count: 0,
+      entities: [],
+    },
+  },
+};
+
+const setUpIntercepts = (
+  featureToggles = { isAppEnabled: true, isInPilot: true },
+) => {
+  cy.intercept('GET', '/data/cms/vamc-ehr.json', vamcUser).as('vamcUser');
+  setFeatureToggles(featureToggles);
+};
+
+describe('21A smoke: ARP user can reach introduction page', () => {
+  beforeEach(() => {
+    cy.loginArpUser();
+    setUpIntercepts({
+      isAppEnabled: true,
+      isInPilot: true,
+      isForm21Enabled: true,
+    });
+    cy.intercept(
+      'PUT',
+      '/accredited_representative_portal/v0/in_progress_forms/21a',
+      inProgressFormsResponse,
+    ).as('saveInProgressForm');
+  });
+
+  it('allows the user to move forward with 3 references', () => {
+    cy.visit('/representative');
+    cy.location('pathname').should('eq', '/representative/');
+    cy.visit(baseUrl);
+    cy.location('pathname').should('eq', introUrl);
+    cy.findByRole('link', { name: /start your application/i }).click();
+    cy.location('pathname', { timeout: 1500 }).should('eq', personalInfoUrl);
+    cy.visit(characterReferencesUrl);
+    cy.findByRole('button', { name: /^Continue$/ }).click();
+    cy.createCharacterReference('Harry', 'Potter');
+    cy.addNewCharacterReference();
+    cy.createCharacterReference('Ron', 'Weasley');
+    cy.addNewCharacterReference();
+    cy.createCharacterReference('Hermione', 'Granger');
+    cy.goToNextPage();
+    cy.location('pathname', { timeout: 1500 }).should(
+      'eq',
+      supplementaryStatementsIntroUrl,
+    );
+  });
+
+  it('allows the user to move forward with 4 references', () => {
+    cy.visit('/representative');
+    cy.location('pathname').should('eq', '/representative/');
+    cy.visit(baseUrl);
+    cy.location('pathname').should('eq', introUrl);
+    cy.findByRole('link', { name: /start your application/i }).click();
+    cy.location('pathname', { timeout: 1500 }).should('eq', personalInfoUrl);
+    cy.visit(characterReferencesUrl);
+    cy.findByRole('button', { name: /^Continue$/ }).click();
+    cy.createCharacterReference('Harry', 'Potter');
+    cy.addNewCharacterReference();
+    cy.createCharacterReference('Ron', 'Weasley');
+    cy.addNewCharacterReference();
+    cy.createCharacterReference('Hermione', 'Granger');
+    cy.addNewCharacterReference();
+    cy.createCharacterReference('Severus', 'Snape');
+    cy.findByRole('button', { name: /^Continue$/i }).click();
+    cy.location('pathname', { timeout: 1500 }).should(
+      'eq',
+      supplementaryStatementsIntroUrl,
+    );
+  });
+
+  it('shows error if there are only 2 references', () => {
+    cy.visit('/representative');
+    cy.location('pathname').should('eq', '/representative/');
+    cy.visit(baseUrl);
+    cy.location('pathname').should('eq', introUrl);
+    cy.findByRole('link', { name: /start your application/i }).click();
+    cy.location('pathname', { timeout: 1500 }).should('eq', personalInfoUrl);
+    cy.visit(characterReferencesUrl);
+    cy.findByRole('button', { name: /^Continue$/ }).click();
+    cy.createCharacterReference('Harry', 'Potter');
+    cy.addNewCharacterReference();
+    cy.createCharacterReference('Ron', 'Weasley');
+    cy.goToNextPage();
+    cy.contains(
+      'span.usa-error-message',
+      'You must add at least 3 character references. You currently have 2.',
+    ).should('be.visible');
+    cy.location('pathname', { timeout: 1500 }).should(
+      'eq',
+      characterReferencesSummaryUrl,
+    );
+  });
+
+  it('shows error if there is only 1 references', () => {
+    cy.visit('/representative');
+    cy.location('pathname').should('eq', '/representative/');
+    cy.visit(baseUrl);
+    cy.location('pathname').should('eq', introUrl);
+    cy.findByRole('link', { name: /start your application/i }).click();
+    cy.location('pathname', { timeout: 1500 }).should('eq', personalInfoUrl);
+    cy.visit(characterReferencesUrl);
+    cy.findByRole('button', { name: /^Continue$/ }).click();
+    cy.createCharacterReference('Harry', 'Potter');
+    cy.goToNextPage();
+    cy.contains(
+      'span.usa-error-message',
+      'You must add at least 3 character references. You currently have 1.',
+    ).should('be.visible');
+    cy.location('pathname', { timeout: 1500 }).should(
+      'eq',
+      characterReferencesSummaryUrl,
+    );
+  });
+});
