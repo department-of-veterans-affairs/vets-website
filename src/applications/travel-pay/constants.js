e90781e2dd0078617053a2fbf1eb5e582a70245a
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-10-30 12:16:22 -0600

    update and added constants

--- src/applications/travel-pay/constants.js
+++ src/applications/travel-pay/constants.js
@@ -175,48 +175,56 @@ export const STATUS_GROUPINGS = [
 export const EXPENSE_TYPES = Object.freeze({
   Airtravel: {
     addButtonText: 'air travel',
+    expensePageText: 'airfare',
     name: 'air travel',
     title: 'Airfare',
     route: 'air-travel',
   },
   Commoncarrier: {
     addButtonText: 'public transportation, taxi, or rideshare',
+    expensePageText: 'public transportation, taxi, or rideshare',
     name: 'common carrier',
     title: 'Public transportation, taxi, or rideshare',
     route: 'common-carrier',
   },
   Lodging: {
     addButtonText: 'lodging',
+    expensePageText: 'lodging',
     name: 'lodging',
     title: 'Lodging',
     route: 'lodging',
   },
   Meal: {
     addButtonText: 'meal',
+    expensePageText: 'meal',
     name: 'meal',
     title: 'Meals',
     route: 'meal',
   },
   Mileage: {
     addButtonText: 'mileage',
+    expensePageText: 'mileage',
     name: 'mileage',
     title: 'Mileage',
     route: 'mileage',
   },
   Parking: {
     addButtonText: 'parking',
+    expensePageText: 'parking',
     name: 'parking',
     title: 'Parking',
     route: 'parking',
   },
   Other: {
     addButtonText: 'travel',
+    expensePageText: 'other travel',
     name: 'other',
     title: 'Other travel expenses',
     route: 'other',
   },
   Toll: {
     addButtonText: 'toll',
+    expensePageText: 'toll',
     name: 'toll',
     title: 'Tolls',
     route: 'toll',
@@ -242,3 +250,5 @@ export const TRANSPORTATION_REASONS = Object.freeze({
     label: 'Other',
   },
 });
+
+export const TRIP_OPTIONS = Object.freeze(['Round trip', 'Oneway']);
