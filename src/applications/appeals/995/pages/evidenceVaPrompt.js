53f8ffd2364b0146db43bedc70261ee780a7b427
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-10-31 17:00:41 -0500

    DR 117051 SC redesign content updates

--- src/applications/appeals/995/pages/evidenceVaPrompt.js
+++ src/applications/appeals/995/pages/evidenceVaPrompt.js
@@ -1,21 +1,37 @@
+import React from 'react';
 import {
   yesNoSchema,
   yesNoUI,
 } from 'platform/forms-system/src/js/web-component-patterns';
-
-import {
-  requestVaRecordsHint,
-  requestVaRecordsTitle,
-} from '../content/evidenceVaPrompt';
-
 import { EVIDENCE_VA } from '../constants';
 import errorMessages from '../../shared/content/errorMessages';
+
+export const title =
+  'Do you want us to get your VA medical records or military health records?';
 
 export default {
   uiSchema: {
     [EVIDENCE_VA]: yesNoUI({
-      title: requestVaRecordsTitle,
-      hint: requestVaRecordsHint,
+      title,
+      description: (
+        <>
+          <p>
+            We can collect your VA medical records or military health records
+            from any of these sources to support your claim:
+          </p>
+          <ul>
+            <li>VA medical center</li>
+            <li>Community-based outpatient clinic</li>
+            <li>Department of Defense military treatment facility</li>
+            <li>Community care provider paid for by VA</li>
+          </ul>
+          <p>We’ll ask you the names of the treatment locations to include.</p>
+          <p>
+            <strong>Note:</strong> Later in this form, we'll ask about your
+            private (non-VA) provider medical records.
+          </p>
+        </>
+      ),
       enableAnalytics: true,
       classNames: 'vads-u-margin-bottom--4',
       labelHeaderLevel: '3',
--- src/applications/appeals/995/tests/pages/evidenceVaPrompt.unit.spec.jsx
+++ src/applications/appeals/995/tests/pages/evidenceVaPrompt.unit.spec.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { expect } from 'chai';
 import { fireEvent, waitFor, render } from '@testing-library/react';
-import sinon from 'sinon';
+import sinon from 'sinon-v20';
 import { DefinitionTester } from 'platform/testing/unit/schemaform-utils';
 import { $, $$ } from 'platform/forms-system/src/js/utilities/ui';
 import formConfig from '../../config/form';
 import { EVIDENCE_VA } from '../../constants';
-import {
-  requestVaRecordsTitle,
-  requestVaRecordsHint,
-} from '../../content/evidenceVaPrompt';
+import { title } from '../../pages/evidenceVaPrompt';
 import errorMessages from '../../../shared/content/errorMessages';
 
 const mockStore = () => ({
@@ -21,94 +18,128 @@ const mockStore = () => ({
   dispatch: () => {},
 });
 
-describe('Supplemental Claims VA evidence request page', () => {
-  const {
-    schema,
-    uiSchema,
-  } = formConfig.chapters.evidence.pages.evidenceVaPrompt;
+const {
+  schema,
+  uiSchema,
+} = formConfig.chapters.evidence.pages.evidenceVaPrompt;
 
-  it('should render', () => {
-    const { container } = render(
-      <Provider store={mockStore()}>
-        <DefinitionTester
-          definitions={{}}
-          schema={schema}
-          uiSchema={uiSchema}
-          data={{}}
-          formData={{}}
-        />
-      </Provider>,
-    );
+describe('VA evidence request page', () => {
+  let onSubmit;
 
-    const radio = $('va-radio', container);
-    expect(radio.getAttribute('label')).to.eq(requestVaRecordsTitle);
-    expect(radio.getAttribute('hint')).to.eq(requestVaRecordsHint);
-    expect($$('va-radio-option', container).length).to.eq(2);
+  beforeEach(() => {
+    onSubmit = sinon.spy();
   });
 
-  it('should not allow submit with radios unselected (required)', async () => {
-    const onSubmit = sinon.spy();
-    const { container } = render(
-      <Provider store={mockStore()}>
-        <DefinitionTester
-          definitions={{}}
-          schema={schema}
-          uiSchema={uiSchema}
-          data={{}}
-          formData={{}}
-          onSubmit={onSubmit}
-        />
-      </Provider>,
-    );
-    fireEvent.submit($('form', container));
-
-    await waitFor(() => {
-      const radios = $$('[error]', container);
-      expect(radios.length).to.equal(1);
-      expect(radios[0].getAttribute('error')).to.eq(
-        errorMessages.requiredYesNo,
-      );
-      expect(onSubmit.called).to.be.false;
-    });
+  afterEach(() => {
+    sinon.restore();
   });
 
-  it('should allow submit with one radio selected', () => {
-    const onSubmit = sinon.spy();
-    const { container } = render(
+  const renderContainer = (data = {}, formData = {}) => {
+    return render(
       <Provider store={mockStore()}>
-        <DefinitionTester
-          definitions={{}}
-          schema={schema}
-          uiSchema={uiSchema}
-          data={{ [EVIDENCE_VA]: true }}
-          formData={{}}
-          onSubmit={onSubmit}
-        />
-      </Provider>,
-    );
-    fireEvent.submit($('form', container));
-    expect(container.innerHTML).to.contain('value="Y" checked');
-    expect($('[error]', container)).to.not.exist;
-    expect(onSubmit.called).to.be.true;
-  });
-
-  it('should render the proper content', () => {
-    const store = mockStore();
-    const { data } = store.getState().form;
-    const { container } = render(
-      <Provider store={store}>
         <DefinitionTester
           definitions={{}}
           schema={schema}
           uiSchema={uiSchema}
           data={data}
-          formData={data}
+          formData={formData}
+          onSubmit={onSubmit}
         />
       </Provider>,
     );
+  };
 
-    const radio = $('va-radio', container);
-    expect(radio.getAttribute('label')).to.eq(requestVaRecordsTitle);
-    expect(radio.getAttribute('hint')).to.eq(requestVaRecordsHint);
+  describe('initial render', () => {
+    it('should render with correct contents', () => {
+      const { container } = renderContainer();
+      const radio = $('va-radio', container);
+      const options = $$('va-radio-option', container);
+      const errorElements = $$('[error]', container);
+
+      expect(radio).to.exist;
+      expect(radio.getAttribute('label')).to.eq(title);
+      expect(options.length).to.eq(2);
+      expect(errorElements.length).to.eq(0);
+    });
+
+    it('should have no option selected by default', () => {
+      const { container } = renderContainer();
+      const checkedOptions = $$('va-radio-option[checked="true"]', container);
+
+      expect(checkedOptions.length).to.eq(0);
+    });
+  });
+
+  describe('validation', () => {
+    it('should not allow submit with radios unselected (required)', async () => {
+      const { container } = renderContainer();
+      fireEvent.submit($('form', container));
+
+      await waitFor(() => {
+        const radios = $$('[error]', container);
+
+        expect(radios.length).to.equal(1);
+        expect(radios[0].getAttribute('error')).to.eq(
+          errorMessages.requiredYesNo,
+        );
+        expect(onSubmit.called).to.be.false;
+      });
+    });
+
+    it('should clear error when option is selected after validation error', async () => {
+      const { container } = renderContainer();
+
+      fireEvent.submit($('form', container));
+
+      await waitFor(() => {
+        expect($$('[error]', container).length).to.equal(1);
+      });
+
+      const radio = $('va-radio', container);
+      radio.__events.vaValueChange({
+        detail: { value: 'Y' },
+      });
+
+      await waitFor(() => {
+        expect($('[error]', container)).to.not.exist;
+      });
+    });
+  });
+
+  describe('submit behavior', () => {
+    it('should allow submit with an option selected', () => {
+      const { container } = renderContainer({ [EVIDENCE_VA]: true });
+
+      fireEvent.submit($('form', container));
+
+      expect(container.innerHTML).to.contain('value="Y" checked="true"');
+      expect($('[error]', container)).to.not.exist;
+      expect(onSubmit.called).to.be.true;
+    });
+  });
+
+  describe('form data persistence', () => {
+    it('should render with previously saved data', () => {
+      const savedData = { [EVIDENCE_VA]: false };
+      const { container } = renderContainer(savedData, savedData);
+
+      expect(container.innerHTML).to.contain('value="N" checked');
+    });
+  });
+
+  describe('error messaging', () => {
+    it('should show error message when validation fails', async () => {
+      const { container } = renderContainer();
+      fireEvent.submit($('form', container));
+
+      await waitFor(() => {
+        const radioWithError = $('[error]', container);
+
+        expect(radioWithError).to.exist;
+        expect(radioWithError.getAttribute('error')).to.contain(
+          'You must answer yes or no',
+        );
+      });
+    });
   });
 });
