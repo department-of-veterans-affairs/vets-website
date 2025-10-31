216a0cfbffd03b37a134151c375c6eee6e7f75e1
Daniel Gading <2748839+dgading@users.noreply.github.com>
2025-10-31 17:44:26 -0400

    Update content on chapter 5

--- src/applications/survivors-benefits/utils/labels.jsx
+++ src/applications/survivors-benefits/utils/labels.jsx
@@ -2,11 +2,10 @@
 // Always use keys for data storage
 
 export const dicOptions = {
-  DIC: 'D.I.C.',
-  DIC_1151:
-    'D.I.C. under U.S.C. 1151 (Note: D.I.C. under 38 U.S.C. is a rare benefit. Please refer to the Instructions page 5 for guidance on 38 U.S.C. 1151)',
+  DIC: 'DIC',
+  DIC_1151: 'DIC under U.S.C. 1151',
   DIC_REEVALUATION:
-    'D.I.C. due to claimant election of a re-evaluation of a previously denied claim based on expanded eligibility under PL 117-168 (PACT Act)',
+    'DIC due to claimant election of a re-evaluation of a previously denied claim based on expanded eligibility under PL 117-168 (PACT Act)',
 };
 
 export const servicesOptions = {
