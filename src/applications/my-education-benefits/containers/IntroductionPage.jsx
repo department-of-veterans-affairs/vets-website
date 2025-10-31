d5d40f9d3b1be44808fd64bb77a96cad8c80c04b
Brad Bergeron <bergeron.bradley@gmail.com>
2025-10-31 10:10:13 -0400

    Updates Verbiage on Introduction Page and sub-components for post 1995 redirect

--- src/applications/my-education-benefits/containers/IntroductionPage.jsx
+++ src/applications/my-education-benefits/containers/IntroductionPage.jsx
@@ -8,15 +8,23 @@ import IntroductionLogin from '../components/IntroductionLogin';
 import LoadingIndicator from '../components/LoadingIndicator';
 import IntroductionProcessList from '../components/IntroductionProcessList';
 
-export const IntroductionPage = ({ featureTogglesLoaded, route }) => {
+export const IntroductionPage = ({
+  featureTogglesLoaded,
+  meb1995Reroute,
+  route,
+}) => {
   return (
     <div className="schemaform-intro">
       <>
         <h1 className="vads-u-margin-bottom--1p5">
-          Apply for VA education benefits Form 22-1990
+          {meb1995Reroute
+            ? 'Apply for VA education benefits'
+            : 'Apply for VA education benefits Form 22-1990'}
         </h1>
         <h2 className="vads-u-font-size--h3 vads-u-font-family--sans vads-u-font-weight--normal vads-u-margin-y--0">
-          Equal to VA Form 22-1990 (Application for VA Education Benefits)
+          {meb1995Reroute
+            ? 'Application for VA Education Benefits (VA Form 22-1990)'
+            : 'Equal to VA Form 22-1990 (Application for VA Education Benefits)'}
         </h2>
       </>
       <HowToApplyPost911GiBill route={route} />
@@ -39,6 +47,7 @@ IntroductionPage.propTypes = {
     pageList: PropTypes.array,
   }).isRequired,
   featureTogglesLoaded: PropTypes.bool,
+  meb1995Reroute: PropTypes.bool,
 };
 
 const mapStateToProps = state => getAppData(state);
