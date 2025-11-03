f4b802e60c2392e35da371ecd8bcf075fb1fc299
Nick Sayre <nicksayre@gmail.com>
2025-11-03 14:11:17 -0600

    Add downtime notifications to Chapter 31 form (#39473)
    * 2027 - add support for chapter 31 downtime notifications
    
    * 2027 - remove file
    
    * 2027 - update app title
    
    * 2027 - correct downtime dependency

--- src/applications/vre/28-1900/containers/App.jsx
+++ src/applications/vre/28-1900/containers/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+import {
+  DowntimeNotification,
+  externalServices,
+} from '@department-of-veterans-affairs/platform-monitoring/DowntimeNotification';
 import formConfig from '../config/form';
 
 export default function App({ location, children }) {
   return (
     <>
+      {/* <Breadcrumbs /> */}
       <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-        {children}
+        <DowntimeNotification
+          appTitle="Veteran Readiness"
+          dependencies={[externalServices.vre]}
+        >
+          {children}
+        </DowntimeNotification>
       </RoutedSavableApp>
     </>
   );
 }
+
+App.propTypes = {
+  children: PropTypes.node,
+  location: PropTypes.object,
+};
