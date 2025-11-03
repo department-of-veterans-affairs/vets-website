f4acd959c873c2c92bbc323be3ece8cd0c22c14b
Josh Alsalihi <joshua.alsalihi@pingwind.com>
2025-11-03 10:06:08 -0500

    added viewElements.jsx

--- src/applications/benefits-optimization-pingwind/21-8940/components/viewElements.jsx
+++ src/applications/benefits-optimization-pingwind/21-8940/components/viewElements.jsx
@@ -12,14 +12,14 @@ export const DisabilityView = ({ formData }) => {
 };
 
 export const DateRangeView = ({ formData }) => {
-  
- /* const dateLabel = formData.startDate ? `Date range starting ${formData.startDate}` : 'Date range';*/
-  
+  /* const dateLabel = formData.startDate ? `Date range starting ${formData.startDate}` : 'Date range';*/
+
   return (
     <div className="vads-u-padding--2">
       {/*<strong>{dateLabel}</strong>*/}
       <p>
-        Duration: {formData.startDate || 'Not provided'} &mdash; {formData.endDate || 'Not provided'}
+        Duration: {formData.startDate || 'Not provided'} &mdash;{' '}
+        {formData.endDate || 'Not provided'}
       </p>
     </div>
   );
@@ -33,13 +33,15 @@ export const DoctorView = ({ formData }) => {
         Address: {formData.doctorAddress?.street || ''}{' '}
         {formData.doctorAddress?.street2 || ''}
         {formData.doctorAddress?.city ? `, ${formData.doctorAddress.city}` : ''}
-        {formData.doctorAddress?.state ? `, ${formData.doctorAddress.state}` : ''}
-        {formData.doctorAddress?.postalCode ? ` ${formData.doctorAddress.postalCode}` : ''}
+        {formData.doctorAddress?.state
+          ? `, ${formData.doctorAddress.state}`
+          : ''}
+        {formData.doctorAddress?.postalCode
+          ? ` ${formData.doctorAddress.postalCode}`
+          : ''}
       </p>
       {formData.connectedDisabilities && (
-        <p>
-          Connected disabilities: {formData.connectedDisabilities}
-        </p>
+        <p>Connected disabilities: {formData.connectedDisabilities}</p>
       )}
     </div>
   );
@@ -52,14 +54,18 @@ export const HospitalView = ({ formData }) => {
       <p>
         Address: {formData.hospitalAddress?.street || ''}{' '}
         {formData.hospitalAddress?.street2 || ''}
-        {formData.hospitalAddress?.city ? `, ${formData.hospitalAddress.city}` : ''}
-        {formData.hospitalAddress?.state ? `, ${formData.hospitalAddress.state}` : ''}
-        {formData.hospitalAddress?.postalCode ? ` ${formData.hospitalAddress.postalCode}` : ''}
+        {formData.hospitalAddress?.city
+          ? `, ${formData.hospitalAddress.city}`
+          : ''}
+        {formData.hospitalAddress?.state
+          ? `, ${formData.hospitalAddress.state}`
+          : ''}
+        {formData.hospitalAddress?.postalCode
+          ? ` ${formData.hospitalAddress.postalCode}`
+          : ''}
       </p>
       {formData.connectedDisabilities && (
-        <p>
-          Connected disabilities: {formData.connectedDisabilities}
-        </p>
+        <p>Connected disabilities: {formData.connectedDisabilities}</p>
       )}
     </div>
   );
@@ -69,14 +75,22 @@ export const EmployerView = ({ formData }) => (
   <div className="vads-u-padding--2">
     <strong>{formData.employerName || 'Employer name not provided'}</strong>
     <p>
-      Type of work: {formData.typeOfWork || 'Not provided'}<br />
+      Type of work: {formData.typeOfWork || 'Not provided'}
+      <br />
       Date applied: {formData.dateApplied || 'Not provided'}
     </p>
     <p>
-      Address: {formData.employerAddress?.street || ''} {formData.employerAddress?.street2 || ''}
-      {formData.employerAddress?.city ? `, ${formData.employerAddress.city}` : ''}
-      {formData.employerAddress?.state ? `, ${formData.employerAddress.state}` : ''}
-      {formData.employerAddress?.postalCode ? ` ${formData.employerAddress.postalCode}` : ''}
+      Address: {formData.employerAddress?.street || ''}{' '}
+      {formData.employerAddress?.street2 || ''}
+      {formData.employerAddress?.city
+        ? `, ${formData.employerAddress.city}`
+        : ''}
+      {formData.employerAddress?.state
+        ? `, ${formData.employerAddress.state}`
+        : ''}
+      {formData.employerAddress?.postalCode
+        ? ` ${formData.employerAddress.postalCode}`
+        : ''}
     </p>
   </div>
 );
@@ -85,19 +99,31 @@ export const EmploymentHistoryView = ({ formData }) => (
   <div className="vads-u-padding--2">
     <strong>{formData.employerName || 'Employer name not provided'}</strong>
     <p>
-      Type of work: {formData.typeOfWork || 'Not provided'}<br />
-      Hours per week: {formData.hoursPerWeek || 'Not provided'}<br />
-      Employment dates: {formData.startDate || 'Start date not provided'} - {formData.endDate || 'End date not provided'}
+      Type of work: {formData.typeOfWork || 'Not provided'}
+      <br />
+      Hours per week: {formData.hoursPerWeek || 'Not provided'}
+      <br />
+      Employment dates: {formData.startDate || 'Start date not provided'} -{' '}
+      {formData.endDate || 'End date not provided'}
     </p>
     <p>
-      Time lost from illness: {formData.lostTime || 'Not provided'} hours<br />
-      Highest gross earnings per month: ${formData.highestIncome || 'Not provided'}
+      Time lost from illness: {formData.lostTime || 'Not provided'} hours
+      <br />
+      Highest gross earnings per month: $
+      {formData.highestIncome || 'Not provided'}
     </p>
     <p>
-      Address: {formData.employerAddress?.street || ''} {formData.employerAddress?.street2 || ''}
-      {formData.employerAddress?.city ? `, ${formData.employerAddress.city}` : ''}
-      {formData.employerAddress?.state ? `, ${formData.employerAddress.state}` : ''}
-      {formData.employerAddress?.postalCode ? ` ${formData.employerAddress.postalCode}` : ''}
+      Address: {formData.employerAddress?.street || ''}{' '}
+      {formData.employerAddress?.street2 || ''}
+      {formData.employerAddress?.city
+        ? `, ${formData.employerAddress.city}`
+        : ''}
+      {formData.employerAddress?.state
+        ? `, ${formData.employerAddress.state}`
+        : ''}
+      {formData.employerAddress?.postalCode
+        ? ` ${formData.employerAddress.postalCode}`
+        : ''}
     </p>
   </div>
 );
@@ -106,7 +132,9 @@ export const EducationView = ({ formData }) => (
   <div className="vads-u-padding--2">
     <strong>{formData.typeOfEducation || 'Education type not provided'}</strong>
     <p>
-      Training dates: {formData.datesOfTraining?.from || 'Start date not provided'} - {formData.datesOfTraining?.to || 'End date not provided'}
+      Training dates:{' '}
+      {formData.datesOfTraining?.from || 'Start date not provided'} -{' '}
+      {formData.datesOfTraining?.to || 'End date not provided'}
     </p>
   </div>
 );
