727b771da335f660768b3b4e54b53d44e0f3602b
Daniel Gading <2748839+dgading@users.noreply.github.com>
2025-11-03 20:31:38 -0500

    21P-534ez update asset page content (#39803)
    * Update content and form alerts
    
    * Update content in accordions

--- src/applications/survivors-benefits/config/chapters/06-financial-information/incomeAndAssets/additionalLandValue.js
+++ src/applications/survivors-benefits/config/chapters/06-financial-information/incomeAndAssets/additionalLandValue.js
@@ -1,21 +1,14 @@
-import React from 'react';
 import {
   titleUI,
   currencyUI,
   currencySchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
 
-const Description = () => (
-  <div>
-    <p>Don’t include the value of the residence or the first 2 acres</p>
-  </div>
-);
-
 const uiSchema = {
-  ...titleUI('Value of additional land', Description),
+  ...titleUI('Value of additional land'),
   additionalLandValue: currencyUI({
     title: 'What’s the value of the land that’s more than 2 acres?',
-    'ui:required': true,
+    hint: 'Don’t include the value of the residence or the first 2 acres',
   }),
 };
 
