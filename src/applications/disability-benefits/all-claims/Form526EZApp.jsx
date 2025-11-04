ad7eadbb07f469c95f1fb29a299583be30c4f931
Justin Linn <justin@linnjs.com>
2025-11-04 16:14:34 -0500

    fix(526): Remove duplicate toggle

--- src/applications/disability-benefits/all-claims/Form526EZApp.jsx
+++ src/applications/disability-benefits/all-claims/Form526EZApp.jsx
@@ -163,7 +163,6 @@ export const Form526Entry = ({
   useFormFeatureToggleSync([
     'disability526Enable2024Form4142',
     'disability526ToxicExposureOptOutDataPurge',
-    'disability526Enable2024Form4142',
     'disabilityCompNewConditionsWorkflow',
   ]);
 
