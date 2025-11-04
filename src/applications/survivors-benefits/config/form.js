f12bdb6acb98310c3395b720f15313a7004f70cf
Daniel Gading <2748839+dgading@users.noreply.github.com>
2025-11-04 15:49:10 -0500

    534ez income benefit type flow (#39843)
    * 534ez add survivor pension check for income asset pages
    
    * Add test coverage for depend functions

--- src/applications/survivors-benefits/config/form.js
+++ src/applications/survivors-benefits/config/form.js
@@ -333,59 +333,73 @@ const formConfig = {
         incomeAndAssets: {
           title: 'Income and assets',
           path: 'financial-information/income-and-assets',
+          depends: formData => formData?.claims?.survivorPension === true,
           uiSchema: incomeAndAssets.uiSchema,
           schema: incomeAndAssets.schema,
         },
         submitSupportingDocs: {
           title: 'Submit supporting documents',
           path: 'financial-information/submit-supporting-documents',
-          depends: formData => formData?.hasAssetsOverThreshold === true,
+          depends: formData =>
+            formData?.hasAssetsOverThreshold === true &&
+            formData?.claims?.survivorPension === true,
           uiSchema: submitSupportingDocs.uiSchema,
           schema: submitSupportingDocs.schema,
         },
         totalAssets: {
           title: 'Total assets',
           path: 'financial-information/total-assets',
-          depends: formData => formData?.hasAssetsOverThreshold === false,
+          depends: formData =>
+            formData?.hasAssetsOverThreshold === false &&
+            formData?.claims?.survivorPension === true,
           uiSchema: totalAssets.uiSchema,
           schema: totalAssets.schema,
         },
         transferredAssets: {
           title: 'Transferred assets',
           path: 'financial-information/transferred-assets',
+          depends: formData => formData?.claims?.survivorPension === true,
           uiSchema: transferredAssets.uiSchema,
           schema: transferredAssets.schema,
         },
         homeOwnership: {
           title: 'Homeownership',
           path: 'financial-information/homeownership',
+          depends: formData => formData?.claims?.survivorPension === true,
           uiSchema: homeOwnership.uiSchema,
           schema: homeOwnership.schema,
         },
         landLotSize: {
           title: 'Land lot size',
           path: 'financial-information/land-lot-size',
-          depends: formData => formData?.homeOwnership === true,
+          depends: formData =>
+            formData?.homeOwnership === true &&
+            formData?.claims?.survivorPension === true,
           uiSchema: landLotSize.uiSchema,
           schema: landLotSize.schema,
         },
         additionalLandValue: {
           title: 'Value of additional land',
           path: 'financial-information/additional-land-value',
-          depends: formData => formData?.landLotSize === true,
+          depends: formData =>
+            formData?.landLotSize === true &&
+            formData?.claims?.survivorPension === true,
           uiSchema: additionalLandValue.uiSchema,
           schema: additionalLandValue.schema,
         },
         marketableLand: {
           title: 'Marketable land',
           path: 'financial-information/marketable-land',
-          depends: formData => formData?.landLotSize === true,
+          depends: formData =>
+            formData?.landLotSize === true &&
+            formData?.claims?.survivorPension === true,
           uiSchema: marketableLand.uiSchema,
           schema: marketableLand.schema,
         },
         incomeSources: {
           title: 'Income sources',
           path: 'financial-information/income-sources',
+          depends: formData => formData?.claims?.survivorPension === true,
           uiSchema: incomeSources.uiSchema,
           schema: incomeSources.schema,
         },
---
+++ src/applications/survivors-benefits/tests/unit/form.unit.spec.jsx
@@ -0,0 +1,77 @@
+import { expect } from 'chai';
+import formConfig from '../../config/form';
+
+it('should have the correct setting for financialInformation', () => {
+  const { financialInformation } = formConfig.chapters;
+  const { pages } = financialInformation;
+
+  const {
+    incomeAndAssets,
+    submitSupportingDocs,
+    totalAssets,
+    transferredAssets,
+    homeOwnership,
+    landLotSize,
+    additionalLandValue,
+    marketableLand,
+    incomeSources,
+  } = pages;
+
+  const noSurvivorPension = { claims: { survivorPension: false } };
+  expect(incomeAndAssets.depends(noSurvivorPension)).to.be.false;
+  expect(submitSupportingDocs.depends(noSurvivorPension)).to.be.false;
+  expect(totalAssets.depends(noSurvivorPension)).to.be.false;
+  expect(transferredAssets.depends(noSurvivorPension)).to.be.false;
+  expect(homeOwnership.depends(noSurvivorPension)).to.be.false;
+  expect(landLotSize.depends(noSurvivorPension)).to.be.false;
+  expect(additionalLandValue.depends(noSurvivorPension)).to.be.false;
+  expect(marketableLand.depends(noSurvivorPension)).to.be.false;
+  expect(incomeSources.depends(noSurvivorPension)).to.be.false;
+
+  const withSurvivorPension = { claims: { survivorPension: true } };
+  expect(incomeAndAssets.depends(withSurvivorPension)).to.be.true;
+  expect(submitSupportingDocs.depends(withSurvivorPension)).to.be.false;
+  expect(totalAssets.depends(withSurvivorPension)).to.be.false;
+  expect(transferredAssets.depends(withSurvivorPension)).to.be.true;
+  expect(homeOwnership.depends(withSurvivorPension)).to.be.true;
+  expect(landLotSize.depends(withSurvivorPension)).to.be.false;
+  expect(additionalLandValue.depends(withSurvivorPension)).to.be.false;
+  expect(marketableLand.depends(withSurvivorPension)).to.be.false;
+  expect(incomeSources.depends(withSurvivorPension)).to.be.true;
+
+  const survivorPensionWithAllTrue = {
+    claims: {
+      survivorPension: true,
+    },
+    hasAssetsOverThreshold: true,
+    homeOwnership: true,
+    landLotSize: true,
+  };
+  expect(incomeAndAssets.depends(survivorPensionWithAllTrue)).to.be.true;
+  expect(submitSupportingDocs.depends(survivorPensionWithAllTrue)).to.be.true;
+  expect(totalAssets.depends(survivorPensionWithAllTrue)).to.be.false;
+  expect(transferredAssets.depends(survivorPensionWithAllTrue)).to.be.true;
+  expect(homeOwnership.depends(survivorPensionWithAllTrue)).to.be.true;
+  expect(landLotSize.depends(survivorPensionWithAllTrue)).to.be.true;
+  expect(additionalLandValue.depends(survivorPensionWithAllTrue)).to.be.true;
+  expect(marketableLand.depends(survivorPensionWithAllTrue)).to.be.true;
+  expect(incomeSources.depends(survivorPensionWithAllTrue)).to.be.true;
+
+  const survivorPensionWithAllFalse = {
+    claims: {
+      survivorPension: true,
+    },
+    hasAssetsOverThreshold: false,
+    homeOwnership: false,
+    landLotSize: false,
+  };
+  expect(incomeAndAssets.depends(survivorPensionWithAllFalse)).to.be.true;
+  expect(submitSupportingDocs.depends(survivorPensionWithAllFalse)).to.be.false;
+  expect(totalAssets.depends(survivorPensionWithAllFalse)).to.be.true;
+  expect(transferredAssets.depends(survivorPensionWithAllFalse)).to.be.true;
+  expect(homeOwnership.depends(survivorPensionWithAllFalse)).to.be.true;
+  expect(landLotSize.depends(survivorPensionWithAllFalse)).to.be.false;
+  expect(additionalLandValue.depends(survivorPensionWithAllFalse)).to.be.false;
+  expect(marketableLand.depends(survivorPensionWithAllFalse)).to.be.false;
+  expect(incomeSources.depends(survivorPensionWithAllFalse)).to.be.true;
+});
