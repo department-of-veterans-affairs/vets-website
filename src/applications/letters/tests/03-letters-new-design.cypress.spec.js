207610c8e3ddd29a646b96585fb282c8ef9e6072
Andrés Rivera Toro <36863582+andresriveratoro@users.noreply.github.com>
2025-11-05 01:16:29 -0500

    add e2e tests

--- src/applications/letters/tests/03-letters-new-design.cypress.spec.js
+++ src/applications/letters/tests/03-letters-new-design.cypress.spec.js
@@ -9,6 +9,7 @@ import {
   mockUserData,
   benefitSummaryLetter,
 } from './e2e/fixtures/mocks/lh_letters';
+import { tsaLetter } from './e2e/fixtures/mocks/tsa-letter';
 
 describe('New letters page design', () => {
   beforeEach(() => {
@@ -136,5 +137,90 @@ describe('New letters page design', () => {
       .first()
       .should('have.text', 'Your letter includes the one topic you selected.');
     cy.axeCheck('main');
+  });
+
+  context('when TSA Safe Travel Letter feature flag is enabled', () => {
+    const tsaLetterTitle = 'TSA PreCheck Application Fee Waiver Letter';
+
+    beforeEach(() => {
+      cy.intercept('GET', '/v0/feature_toggles*', {
+        data: {
+          type: 'feature_toggles',
+          features: [
+            {
+              name: 'tsa_safe_travel_letter',
+              value: true,
+            },
+          ],
+        },
+      }).as('featureToggles');
+    });
+
+    it('allows downloading TSA letter', () => {
+      cy.intercept('GET', '/v0/tsa_letter', tsaLetter);
+      cy.intercept(
+        'POST',
+        `v0/tsa_letter/${tsaLetter.data[0].attributes.document_id}`,
+        '@letterPDFBlob',
+      );
+      cy.injectAxeThenAxeCheck();
+      cy.get('[data-test-id="letters-accordion"]', {
+        timeout: Timeouts.slow,
+      }).should('be.visible');
+      cy.get('va-accordion-item', tsaLetterTitle)
+        .shadow()
+        .find('button[aria-expanded=false]')
+        .click({ force: true });
+      cy.get('va-button')
+        .shadow()
+        .find('button')
+        .contains(`Download ${tsaLetterTitle} (PDF)`)
+        .click();
+      cy.get('@letterPDFBlob').then(blob => {
+        expect(blob).to.exist;
+      });
+    });
+
+    it('displays alert if determining TSA letter eligibility fails', () => {
+      cy.intercept('GET', '/v0/tsa_letter', {
+        statusCode: 500,
+      });
+      cy.injectAxeThenAxeCheck();
+      cy.get('va-alert[status="warning"]', {
+        timeout: Timeouts.slow,
+      })
+        .should('be.visible')
+        .as('warningAlert');
+      cy.get('@warningAlert')
+        .find('h4')
+        .should('have.text', 'Some letters may not be available');
+    });
+
+    it('displays alert if downloading TSA letter fails', () => {
+      cy.intercept('GET', '/v0/tsa_letter', tsaLetter);
+      cy.intercept(
+        'GET',
+        `v0/tsa_letter/${tsaLetter.data[0].attributes.document_id}`,
+        {
+          statusCode: 500,
+        },
+      );
+      cy.injectAxeThenAxeCheck();
+      cy.get('[data-test-id="letters-accordion"]', {
+        timeout: Timeouts.slow,
+      }).should('be.visible');
+      cy.get('va-accordion-item', tsaLetterTitle)
+        .shadow()
+        .find('button[aria-expanded=false]')
+        .click({ force: true });
+      cy.get('va-alert[status="error"]', {
+        timeout: Timeouts.slow,
+      })
+        .should('be.visible')
+        .as('errorAlert');
+      cy.get('@errorAlert')
+        .find('h4')
+        .should('have.text', `Your ${tsaLetterTitle} is currently unavailable`);
+    });
   });
 });
---
+++ src/applications/letters/tests/e2e/fixtures/mocks/tsa-letter.js
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+export const tsaLetter = {
+  data: [
+    {
+      id: '',
+      type: 'tsa_letter',
+      attributes: {
+        document_id: '{3F7A891C-2E4B-4D29-9C6E-5B8F14A07E32}',
+        doc_type: '34',
+        type_description: 'Correspondence',
+        received_at: '2025-09-09',
+      },
+    },
+  ],
+};
