3c629f2580e8260d33c4789e7e053a3f5d20e60b
fatmakhan0395 <145523589+fatmakhan0395@users.noreply.github.com>
2025-11-06 07:12:23 -0800

    VEBT-2080: E2E test 10275

--- src/applications/edu-benefits/10275/tests/10275-edu-benefits.cypress.spec.js
+++ src/applications/edu-benefits/10275/tests/10275-edu-benefits.cypress.spec.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import testForm from 'platform/testing/e2e/cypress/support/form-tester';
 import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-tester/utilities';
-import mockUser from './fixtures/mocks/user.json';
 import formConfig from '../config/form';
 import manifest from '../manifest.json';
 
@@ -9,22 +8,60 @@ const testConfig = createTestConfig(
   {
     dataPrefix: 'data',
     dataDir: path.join(__dirname, 'fixtures', 'data'),
-    dataSets: ['minimal-test'],
+    dataSets: ['maximal-test'],
     pageHooks: {
       introduction: ({ afterHook }) => {
         afterHook(() => {
-          cy.findAllByText(/^start/i, { selector: 'a[href="#start"]' })
-            .last()
-            .click({ force: true });
+          cy.get('.schemaform-start-button')
+            .first()
+            .click();
         });
       },
     },
+    '/school-administrators/commit-principles-of-excellence-form-22-10275/new-commitment-institution-details': ({
+      afterHook,
+    }) => {
+      afterHook(() => {
+        cy.fillVaTextInput('root_institutionDetails_facilityCode', '10002000');
 
-    setupPerTest: () => {
-      cy.intercept('GET', '/v0/user', mockUser);
-      cy.intercept('POST', formConfig.submitUrl, { status: 200 });
-      cy.login(mockUser);
+        cy.get('va-loading-indicator', { timeout: 10000 }).should('not.exist');
+
+        cy.get('#institutionHeading', { timeout: 10000 }).should(
+          'contain',
+          'INSTITUTE OF TESTING',
+        );
+        cy.tabToSubmitForm();
+      });
     },
+    '/school-administrators/commit-principles-of-excellence-form-22-10275/new-commitment-authorizing-official': ({
+      afterHook,
+    }) => {
+      afterHook(() => {
+        cy.fillVaTextInput('root_authorizedOfficial_fullName_first', 'Jane');
+        cy.tabToSubmitForm();
+      });
+    },
+    // '/school-administrators/commit-principles-of-excellence-form-22-10275/new-commitment-authorizing-official': ({
+    //   afterHook,
+    // }) => {
+    //   afterHook(() => {
+    //     cy.fillVaTextInput('root_authorizedOfficial_fullName_first', 'Jane');
+    //     cy.fillVaTextInput('root_certifyingOfficial_last', 'Miller');
+    //     cy.fillVaTextInput(
+    //       'root_certifyingOfficial_title',
+    //       'Certifying Official',
+    //     );
+    //     //   cy.tabToSubmitForm();
+    //     cy.selectVaRadioOption('root_authorizedOfficial_view:isPOC', 'N');
+    //     cy.tabToSubmitForm();
+    //   });
+    // },
+
+    // setupPerTest: () => {
+    //   cy.intercept('GET', '/v0/user', mockUser);
+    //   cy.intercept('POST', formConfig.submitUrl, { status: 200 });
+    //   cy.login(mockUser);
+    // },
 
     // Skip tests in CI until the form is released.
     // Remove this setting when the form has a content page in production.
