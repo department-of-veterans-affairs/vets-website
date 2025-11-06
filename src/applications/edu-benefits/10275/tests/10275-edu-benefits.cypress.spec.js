2ffaaff5b7f37fdc3747e608e782c88adec0f2ae
fatmakhan0395 <145523589+fatmakhan0395@users.noreply.github.com>
2025-11-06 15:03:42 -0800

    VEBT-2080 E2E TEST

--- src/applications/edu-benefits/10275/tests/10275-edu-benefits.cypress.spec.js
+++ src/applications/edu-benefits/10275/tests/10275-edu-benefits.cypress.spec.js
@@ -17,50 +17,84 @@ const testConfig = createTestConfig(
             .click();
         });
       },
-    },
-    '/school-administrators/commit-principles-of-excellence-form-22-10275/new-commitment-institution-details': ({
-      afterHook,
-    }) => {
-      afterHook(() => {
-        cy.fillVaTextInput('root_institutionDetails_facilityCode', '10002000');
+      '/school-administrators/commit-principles-of-excellence-form-22-10275/additional-locations/0/point-of-contact?add=true': ({
+        afterHook,
+      }) => {
+        afterHook(() => {
+          cy.get('[label="John William Doe"]')
+            .should('exist')
+            .click();
+          // cy.selectVaRadioOption(
+          //   '[label="John William Doe"]',
+          //   'authorizedOfficial',
+          // );
+          cy.tabToSubmitForm();
+        });
+      },
+      // '/school-administrators/commit-principles-of-excellence-form-22-10275/new-commitment-institution-details': ({
+      //   afterHook,
+      // }) => {
+      //   afterHook(() => {
+      //     cy.fillVaTextInput(
+      //       'root_institutionDetails_facilityCode',
+      //       '31860132',
+      //     );
 
-        cy.get('va-loading-indicator', { timeout: 10000 }).should('not.exist');
+      //     cy.get('va-loading-indicator', { timeout: 10000 }).should(
+      //       'not.exist',
+      //     );
 
-        cy.get('#institutionHeading', { timeout: 10000 }).should(
-          'contain',
-          'INSTITUTE OF TESTING',
-        );
-        cy.tabToSubmitForm();
-      });
+      //     cy.get('#institutionHeading', { timeout: 10000 }).should(
+      //       'contain',
+      //       'TESTING INSTITUTE OF HIGHER LEARNING',
+      //     );
+      //     cy.wait('@getInstitutionByFacilityCode');
+      //     cy.tabToSubmitForm();
+      //   });
+      // },
     },
-    '/school-administrators/commit-principles-of-excellence-form-22-10275/new-commitment-authorizing-official': ({
-      afterHook,
-    }) => {
-      afterHook(() => {
-        cy.fillVaTextInput('root_authorizedOfficial_fullName_first', 'Jane');
-        cy.tabToSubmitForm();
-      });
-    },
+
     // '/school-administrators/commit-principles-of-excellence-form-22-10275/new-commitment-authorizing-official': ({
     //   afterHook,
     // }) => {
     //   afterHook(() => {
     //     cy.fillVaTextInput('root_authorizedOfficial_fullName_first', 'Jane');
-    //     cy.fillVaTextInput('root_certifyingOfficial_last', 'Miller');
-    //     cy.fillVaTextInput(
-    //       'root_certifyingOfficial_title',
-    //       'Certifying Official',
-    //     );
-    //     //   cy.tabToSubmitForm();
-    //     cy.selectVaRadioOption('root_authorizedOfficial_view:isPOC', 'N');
+    //     cy.tabToSubmitForm();
+    //   });
+    // },
+    // '/school-administrators/commit-principles-of-excellence-form-22-10275/additional-locations/:index/point-of-contact?add=true': ({
+    //   afterHook,
+    // }) => {
+    //   afterHook(() => {
+    //     cy.selectVaRadioOption('[name="pointOfContact"]', 'authorizedOfficial');
     //     cy.tabToSubmitForm();
     //   });
     // },
 
     // setupPerTest: () => {
-    //   cy.intercept('GET', '/v0/user', mockUser);
-    //   cy.intercept('POST', formConfig.submitUrl, { status: 200 });
-    //   cy.login(mockUser);
+    // cy.intercept('GET', '**/gi/institutions/31860132', {
+    //   statusCode: 200,
+    //   body: {
+    //     data: {
+    //       id: '31860132',
+    //       type: 'institution',
+    //       attributes: {
+    //         name: 'INSTITUTE OF TESTING',
+    //         address1: '123 STREET WAY',
+    //         address2: '',
+    //         address3: '',
+    //         city: 'SAN FRANCISCO',
+    //         state: 'CA',
+    //         zip: '13579',
+    //         country: 'USA',
+    //         programTypes: ['IHL', 'NCD'],
+    //       },
+    //     },
+    //   },
+    // }).as('getInstitutionByFacilityCode');
+    // cy.intercept('GET', '/v0/user', mockUser);
+    // cy.intercept('POST', formConfig.submitUrl, { status: 200 });
+    // cy.login(mockUser);
     // },
 
     // Skip tests in CI until the form is released.
