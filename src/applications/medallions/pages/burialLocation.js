4603afbd5ab769a26551cad5e361aa30f4d86f24
122034971+ConnorJeff@users.noreply.github.com <122034971+ConnorJeff@users.noreply.github.com>
2025-11-06 13:40:59 -0500

    MBMS-86368 medallions burial information section

---
+++ src/applications/medallions/pages/burialLocation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  radioUI,
+  radioSchema,
+  titleUI,
+} from 'platform/forms-system/src/js/web-component-patterns';
+
+/** @type {PageSchema} */
+export default {
+  uiSchema: {
+    ...titleUI('Burial location'),
+    'view:remainsWarning': {
+      'ui:description': (
+        <va-alert status="warning" class="vads-u-margin-bottom--2">
+          <h3 slot="headline">Check medallion eligibility</h3>
+          <p>
+            The Veteran&apos;s remains must be buried in their grave to be
+            eligible for a medallion.
+          </p>
+        </va-alert>
+      ),
+      'ui:options': {
+        hideIf: formData => formData.remainsBuriedInGrave !== 'no',
+      },
+    },
+    remainsBuriedInGrave: radioUI({
+      title: "Are the Veteran's remains buried in their grave?",
+      labels: {
+        yes: 'Yes',
+        no: 'No',
+      },
+    }),
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      'view:remainsWarning': {
+        type: 'object',
+        properties: {},
+      },
+      remainsBuriedInGrave: radioSchema(['yes', 'no']),
+    },
+  },
+};
