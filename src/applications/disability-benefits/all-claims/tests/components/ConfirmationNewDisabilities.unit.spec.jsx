9966c9e41e8e0984954606391637ae9bd73d60ae
joedyfelts <4977491+Grendel78@users.noreply.github.com>
2025-11-06 16:30:04 -0500

    initial commit

--- src/applications/disability-benefits/all-claims/tests/components/ConfirmationNewDisabilities.unit.spec.jsx
+++ src/applications/disability-benefits/all-claims/tests/components/ConfirmationNewDisabilities.unit.spec.jsx
@@ -11,6 +11,7 @@ describe('ConfirmationNewDisabilities', () => {
           condition: 'Condition 1',
           cause: 'NEW',
           primaryDescription: 'Primary description 1',
+          conditionDate: '2020-01-15',
         },
         {
           condition: 'Condition 2',
@@ -23,16 +24,34 @@ describe('ConfirmationNewDisabilities', () => {
         },
       ],
     };
-    const { container, getByText } = render(
+    const { container, getByText, getAllByText } = render(
       <ConfirmationNewDisabilities formData={formData} />,
     );
     expect(container.querySelectorAll('h4')).to.have.length(2);
     expect(getByText('Condition 1')).to.exist;
-    expect(getByText(/new condition; Primary description 1/i)).to.exist;
+    expect(getAllByText('Type of condition')).to.have.length(2);
+    expect(getByText('New condition')).to.exist;
+    expect(getAllByText('Cause')).to.have.length(2);
+    expect(
+      getByText(
+        'My condition was caused by an injury or exposure during my military service.',
+      ),
+    ).to.exist;
+    expect(getAllByText('Description')).to.have.length(3);
+    expect(getByText('Primary description 1')).to.exist;
+    expect(getByText('Date')).to.exist;
+    expect(getByText('January 15, 2020')).to.exist;
     expect(getByText('Condition 2')).to.exist;
-    expect(getByText(/secondary condition; Primary description 2/i)).to.exist;
-    expect(getByText(/caused by Condition A; Caused by description A/i)).to
-      .exist;
+    expect(getByText('Secondary condition')).to.exist;
+    expect(
+      getByText(
+        'My condition was caused by another service-connected disability I already have. (For example, I have a limp that caused lower-back problems.)',
+      ),
+    ).to.exist;
+    expect(getByText('Primary description 2')).to.exist;
+    expect(getByText('Caused by')).to.exist;
+    expect(getByText('Condition A')).to.exist;
+    expect(getByText('Caused by description A')).to.exist;
   });
 
   it('should render nothing when no new disabilities are provided', () => {
