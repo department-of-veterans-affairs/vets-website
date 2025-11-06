00498900b695b74ab569246a4178457843736671
emisparkles <emisargent@gmail.com>
2025-11-06 12:27:23 -0800

    Rename to scheduling preferences

--- src/applications/personalization/profile/components/health-care-settings/SchedulingPreferences.jsx
+++ src/applications/personalization/profile/components/health-care-settings/SchedulingPreferences.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { PROFILE_PATH_NAMES } from '../../constants';
 import Headline from '../ProfileSectionHeadline';
 
-const AppointmentPreferences = () => {
+const SchedulingPreferences = () => {
   return (
     <>
       <Headline level="2" className="vads-u-margin-bottom--2">
-        {PROFILE_PATH_NAMES.APPOINTMENT_PREFERENCES}
+        {PROFILE_PATH_NAMES.SCHEDULING_PREFERENCES}
       </Headline>
       <p>Empty page, for now.</p>
     </>
   );
 };
 
-AppointmentPreferences.propTypes = {};
+SchedulingPreferences.propTypes = {};
 
-export default AppointmentPreferences;
+export default SchedulingPreferences;
