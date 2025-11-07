16c1b227409de9f0bd0cc3c7cd82ef897255f8b7
Victoria (Vic) Saleem <59583325+vsaleem@users.noreply.github.com>
2025-11-07 15:17:21 -0600

    Expand DIC benefits page unit test coverage

--- src/applications/survivors-benefits/tests/unit/pages/05-claim-information/dicBenefits.unit.spec.jsx
+++ src/applications/survivors-benefits/tests/unit/pages/05-claim-information/dicBenefits.unit.spec.jsx
@@ -8,9 +8,12 @@ import {
 import { $, $$ } from 'platform/forms-system/src/js/utilities/ui';
 import dicBenefits from '../../../../config/chapters/05-claim-information/dicBenefits';
 import { dicOptions } from '../../../../utils/labels';
+import { radioQuestions } from '../../../../utils/constants';
 
 describe('DIC Benefits Page', () => {
   const { schema, uiSchema } = dicBenefits;
+  const { CHAPTER_5 } = radioQuestions;
+
   it('renders the DIC benefits options', async () => {
     const form = render(
       <DefinitionTester schema={schema} uiSchema={uiSchema} data={{}} />,
@@ -19,16 +22,28 @@ describe('DIC Benefits Page', () => {
     const vaRadio = $('va-radio', formDOM);
     const options = $$('va-radio-option', formDOM);
     const optionKeys = Object.keys(dicOptions);
+    const vaAccordions = $$('va-accordion', formDOM);
+    const vaAccordionItems = $$('va-accordion-item', formDOM);
 
     expect(form.getByRole('heading')).to.have.text('DIC benefits');
-    expect(vaRadio.getAttribute('label')).to.equal(
-      'What Dependency and indemnity compensation (DIC) benefit are you claiming?',
-    );
+    expect(vaRadio.getAttribute('label')).to.equal(CHAPTER_5.dicBenefits);
     expect(vaRadio.getAttribute('required')).to.equal('true');
 
     expect(options.length).to.equal(optionKeys.length);
     optionKeys.forEach((key, index) => {
       expect(options[index].getAttribute('label')).to.equal(dicOptions[key]);
     });
+
+    expect(vaAccordions.length).to.equal(1);
+    expect(vaAccordionItems.length).to.equal(3);
+    expect(vaAccordionItems[0].getAttribute('header')).to.equal(
+      'When to claim DIC',
+    );
+    expect(vaAccordionItems[1].getAttribute('header')).to.equal(
+      'When to claim DIC under Title 38 U.S.C. 1151',
+    );
+    expect(vaAccordionItems[2].getAttribute('header')).to.equal(
+      'When to claim DIC re-evaluation based on the PACT Act',
+    );
   });
 });
