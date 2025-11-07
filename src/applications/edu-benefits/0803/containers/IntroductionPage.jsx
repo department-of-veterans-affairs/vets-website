755010297ece03fad33efb906e905e2949b616c2
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2025-11-07 12:35:01 -0800

    add accordion and contact info to intro page

--- src/applications/edu-benefits/0803/containers/IntroductionPage.jsx
+++ src/applications/edu-benefits/0803/containers/IntroductionPage.jsx
@@ -136,11 +136,75 @@ export const IntroductionPage = props => {
         />
       )}
       <p />
-      <va-omb-info
-        res-burden={OMB_RES_BURDEN}
-        omb-number={OMB_NUMBER}
-        exp-date={OMB_EXP_DATE}
-      />
+      <div className="vads-u-margin-bottom--1p5">
+        <p className="vads-u-margin--0">
+          <span>Respondent burden:</span>{' '}
+          <span className="vads-u-font-weight--bold">{OMB_RES_BURDEN}</span>
+        </p>
+        <p className="vads-u-margin--0">
+          <span>OMB Control #:</span>{' '}
+          <span className="vads-u-font-weight--bold">{OMB_NUMBER}</span>
+        </p>
+        <p className="vads-u-margin--0">
+          <span>Expiration date:</span>{' '}
+          <span className="vads-u-font-weight--bold">{OMB_EXP_DATE}</span>
+        </p>
+      </div>
+      <va-accordion open-single>
+        <va-accordion-item header="View Privacy Act Statement" id="first">
+          <p>
+            VA will not disclose information collected on this form to any
+            source other than what has been authorized under the Privacy Act of
+            1974 or title 38, Code of Federal Regulations 1.576 for routine uses
+            (i.e., VA sends educational forms or letters with a veteran’s
+            identifying information to the veteran’s school or training
+            establishment to (1) assist the veteran in the completion of claims
+            forms or (2) VA obtains further information as may be necessary from
+            the school for VA to properly process the veteran’s education claim
+            or to monitor his or her progress during training) as identified in
+            the VA system or records, 58VA21/22/28, Compensation, Pension,
+            Education, and Veteran Readiness and Employment Records - VA,
+            published in the Federal Register. Your obligation to respond is
+            required to obtain or retain benefits (licensing and certification
+            test reimbursement). While you do not have to respond, VA cannot
+            reimburse you any licensing and certification test fees until we
+            receive this information (38 U.S.C. 3452(b) and 3501 (a)). Your
+            responses are confidential (38 U.S.C. 5701). Information submitted
+            is subject to verification through computer matching programs with
+            other agencies.
+          </p>
+        </va-accordion-item>
+        <va-accordion-item header="View Respondent Burden" id="second">
+          <p>
+            An agency may not conduct or sponsor, and a person is not required
+            to respond to, a collection of information unless it displays a
+            currently valid OMB control number. The OMB control number for this
+            project is 2900-0695, and it expires 01/31/2028. Public reporting
+            burden for this collection of information is estimated to average 15
+            minutes per respondent, per year, including the time for reviewing
+            instructions, searching existing data sources, gathering and
+            maintaining the data needed, and completing and reviewing the
+            collection of information. Send comments regarding this burden
+            estimate and any other aspect of this collection of information,
+            including suggestions for reducing the burden, to VA Reports
+            Clearance Officer at
+            <a href="mailto:VACOPaperworkReduAct@va.gov" rel="noreferrer">
+              VACOPaperworkReduAct@va.gov
+            </a>
+            . Please refer to OMB Control No. 2900-0695 in any correspondence.
+            Do not send your completed VA Form 22-0803 to this email address.
+          </p>
+        </va-accordion-item>
+      </va-accordion>
+      <va-need-help>
+        <div slot="content">
+          <p>
+            If you need help in completing this form, call VA TOLL-FREE at{' '}
+            <va-telephone contact="8884424551" vanity="888-GIBILL-1" />. If you
+            have hearing loss, call <va-telephone contact="711" tty />.
+          </p>
+        </div>
+      </va-need-help>
     </article>
   );
 };
