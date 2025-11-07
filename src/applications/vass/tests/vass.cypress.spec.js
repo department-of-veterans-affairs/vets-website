077007925f1b4d9aad9bba9c29da540963223bde
Juan Castillo <juan.cast@agile6.com>
2025-11-07 11:48:07 -0800

    Update cypress tests

--- src/applications/vass/tests/vass.cypress.spec.js
+++ src/applications/vass/tests/vass.cypress.spec.js
@@ -3,13 +3,12 @@ import manifest from '../manifest.json';
 describe(manifest.appName, () => {
   // Skip tests in CI until the app is released.
   // Remove this block when the app has a content page in production.
-  before(() => {
+  beforeEach(() => {
     if (Cypress.env('CI')) this.skip();
   });
 
   it('is accessible', () => {
-    cy.visit(manifest.rootUrl)
-      .injectAxe()
-      .axeCheck();
+    cy.visit(manifest.rootUrl);
+    cy.injectAxeThenAxeCheck();
   });
 });
