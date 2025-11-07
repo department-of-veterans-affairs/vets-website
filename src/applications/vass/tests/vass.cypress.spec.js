e4afb6aa9dbef735bc19ae23e02ca9b968bbb3d3
Juan Castillo <juan.cast@agile6.com>
2025-11-07 10:37:44 -0800

    Refactor VASS application entry name and update related files
    - Changed entry name from "vass-entry" to "vass" in manifest and app-entry files.
    - Updated import paths in app-entry and README for consistency.
    - Added new SCSS file for VASS styling.
    - Created Cypress test file for VASS application accessibility checks.

--- src/applications/vass/tests/vass.cypress.spec.js
+++ src/applications/vass/tests/vass.cypress.spec.js
@@ -1,7 +1,15 @@
 import manifest from '../manifest.json';
 
 describe(manifest.appName, () => {
-  it.skip('is accessible', () => {
-    // Skip tests in CI until the app is released.
+  // Skip tests in CI until the app is released.
+  // Remove this block when the app has a content page in production.
+  before(() => {
+    if (Cypress.env('CI')) this.skip();
+  });
+
+  it('is accessible', () => {
+    cy.visit(manifest.rootUrl)
+      .injectAxe()
+      .axeCheck();
   });
 });
