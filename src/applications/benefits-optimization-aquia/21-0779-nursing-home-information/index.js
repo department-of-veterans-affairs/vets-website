e5ac2603a226eb1f4696d036dcd36ac13ca7d790
Justin Linn <justin@linnjs.com>
2025-11-08 08:37:48 -0500

    docs(0779): Update README.md docs

--- src/applications/benefits-optimization-aquia/21-0779-nursing-home-information/index.js
+++ src/applications/benefits-optimization-aquia/21-0779-nursing-home-information/index.js
@@ -3,9 +3,22 @@
  * @description Main barrel export file for VA Form 21-0779 application
  */
 
-export { TITLE, SUBTITLE } from './constants';
-export { formConfig } from './config';
-export { default as routes } from './routes';
-export { default as reducer } from './reducers';
-export { App, IntroductionPage, ConfirmationPage } from './containers';
-export { GetHelp } from './components';
+export {
+  TITLE,
+  SUBTITLE,
+} from '@bio-aquia/21-0779-nursing-home-information/constants';
+export { formConfig } from '@bio-aquia/21-0779-nursing-home-information/config';
+export {
+  default as routes,
+} from '@bio-aquia/21-0779-nursing-home-information/routes';
+export {
+  default as reducer,
+} from '@bio-aquia/21-0779-nursing-home-information/reducers';
+export {
+  App,
+  IntroductionPage,
+  ConfirmationPage,
+} from '@bio-aquia/21-0779-nursing-home-information/containers';
+export {
+  GetHelp,
+} from '@bio-aquia/21-0779-nursing-home-information/components';
