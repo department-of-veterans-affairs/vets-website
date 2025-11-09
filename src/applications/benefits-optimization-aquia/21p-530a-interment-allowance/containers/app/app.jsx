b6df0538ce53c5ef48826bc75af20fab0f20f172
Justin Linn <justin@linnjs.com>
2025-11-08 21:57:37 -0500

    docs(bio-aquia): Update README.md for all migrated forms

--- src/applications/benefits-optimization-aquia/21p-530a-interment-allowance/containers/app/app.jsx
+++ src/applications/benefits-optimization-aquia/21p-530a-interment-allowance/containers/app/app.jsx
@@ -1,4 +1,3 @@
-import { VaBreadcrumbs } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -7,20 +6,10 @@ import { SaveInProgress } from '@bio-aquia/shared/components';
 
 import formConfig from '@bio-aquia/21p-530a-interment-allowance/config/form';
 
-const breadcrumbList = [
-  { href: '/', label: 'Home' },
-  {
-    href: '/submit-state-interment-allowance-form-21p-530a',
-    label:
-      'Submit a state or tribal organization request for interment allowance',
-  },
-];
-
 export const App = ({ location, router, children }) => {
   return (
     <SaveInProgress formConfig={formConfig} location={location} router={router}>
       <div className="vads-l-grid-container desktop-lg:vads-u-padding-x--0">
-        <VaBreadcrumbs breadcrumbList={breadcrumbList} />
         <RoutedSavableApp
           formConfig={formConfig}
           currentLocation={location}
