064e7a8642c7d5a9e1f4fd8c2c58b6ac6bc69f26
Sami Barghshoon <sami.barghshoon@aquia.us>
2025-11-10 17:47:04 -0500

    [122237] New condition and disability benefits workflow updates (#39766)
    * several updates to address disability benefits and new conditions workflows
    
    * removed commented out code

--- src/applications/disability-benefits/all-claims/pages/toxicExposure/toxicExposureConditions.js
+++ src/applications/disability-benefits/all-claims/pages/toxicExposure/toxicExposureConditions.js
@@ -10,13 +10,38 @@ import {
 import { formTitle, makeConditionsUI } from '../../utils';
 import ToxicExposureConditions from '../../components/ConfirmationFields/ToxicExposureConditions';
 
+const getCleanTEConditionsSchema = (...args) => {
+  const schema = makeTEConditionsSchema(...args);
+
+  if (schema?.properties) {
+    const slug = s =>
+      String(s || '')
+        .toLowerCase()
+        .replace(/[^a-z0-9]/g, '');
+    const out = {};
+
+    for (const [key, def] of Object.entries(schema.properties)) {
+      const keySlug = slug(key);
+      const titleSlug = slug(def?.title);
+      if (keySlug === 'rateddisability' || titleSlug === 'rateddisability')
+        // eslint-disable-next-line no-continue
+        continue;
+      out[key] = def;
+    }
+
+    schema.properties = out;
+  }
+
+  return schema;
+};
+
 export const uiSchema = {
   'ui:title': formTitle(conditionsPageTitle),
   toxicExposure: {
     conditions: makeConditionsUI({
       title: conditionsQuestion,
       description: conditionsDescription,
-      replaceSchema: makeTEConditionsSchema,
+      replaceSchema: getCleanTEConditionsSchema,
       updateUiSchema: makeTEConditionsUISchema,
     }),
   },
