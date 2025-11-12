eddc8c5604e8912479758d88eb401423de92a8df
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-11-12 08:45:09 -0700

    add content changes to document upload

--- src/applications/travel-pay/components/complex-claims/pages/DocumentUpload.jsx
+++ src/applications/travel-pay/components/complex-claims/pages/DocumentUpload.jsx
@@ -9,28 +9,22 @@ const DocumentUpload = ({ handleDocumentUpload }) => {
     <>
       <VaFileInput
         accept={ACCEPTED_FILE_TYPES.map(type => `${type}`).join(',')}
-        hint={`Accepted file types: ${ACCEPTED_FILE_TYPES.map(
-          ext => `${ext}`,
-        ).join(', ')} (up to 5 MB)`}
-        label="Upload a document for your expense"
+        hint={`You can upload a ${ACCEPTED_FILE_TYPES.join(', ').replace(
+          /, ([^,]*)$/,
+          ', or $1',
+        )} file. Your file should be no larger than 5MB.`}
+        label="Select a file to upload"
         maxFileSize={5200000} // Based on platform cals have to use 5200000 to get 5MB
         minFileSize={0}
         name="travel-pay-claim-document-upload"
         onVaChange={handleDocumentUpload}
         required
       />
-      <va-additional-info trigger="How to upload files">
-        <ul>
-          <li>
-            Use a {ACCEPTED_FILE_TYPES.map(type => `${type}`).join(', ')} file
-            format
-          </li>
-          <li>The size of your file must be no more than 5 MB</li>
-          <li>
-            If you only have a paper copy, scan or take a photo and upload the
-            image
-          </li>
-        </ul>
+      <va-additional-info trigger="How to upload paper copies">
+        <p>
+          If you only have a paper copy, scan or take a photo and upload the
+          image.
+        </p>
       </va-additional-info>
     </>
   );
