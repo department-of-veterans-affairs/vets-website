73eb42ece168012ffe4386c583bb598bc847a1fc
emisparkles <emisargent@gmail.com>
2025-11-12 14:54:37 -0800

    Add scheduling preferences blank and error states to health care settings

--- src/applications/personalization/profile/components/health-care-settings/SchedulingPreferences.jsx
+++ src/applications/personalization/profile/components/health-care-settings/SchedulingPreferences.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { PROFILE_PATH_NAMES } from '../../constants';
 import Headline from '../ProfileSectionHeadline';
+import LoadFail from '../alerts/LoadFail';
 
 const SchedulingPreferences = () => {
+  const error = false; // TODO: get error state from redux when implemented
+
   return (
     <>
-      <Headline level="2" className="vads-u-margin-bottom--2">
+      <Headline
+        dataTestId="scheduling-preferences-page-headline"
+        classes={error ? 'vads-u-margin-bottom--4' : 'vads-u-margin-bottom--1'}
+      >
         {PROFILE_PATH_NAMES.SCHEDULING_PREFERENCES}
       </Headline>
-      <p>Empty page, for now.</p>
+      {error && <LoadFail />}
+      {!error && (
+        <p className="vads-u-margin-top--0 vads-u-margin-bottom--4">
+          Manage your scheduling preferences for health care appointments.
+          Whenever possible, schedulers will consider your preferences when
+          making appointments.
+        </p>
+      )}
     </>
   );
 };
