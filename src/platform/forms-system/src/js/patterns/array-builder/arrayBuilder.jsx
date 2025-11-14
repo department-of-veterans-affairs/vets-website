25f55e0200684c0c15e8593c881f1004947fe457
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-11-14 14:01:26 -0600

    WIP configs

--- src/platform/forms-system/src/js/patterns/array-builder/arrayBuilder.jsx
+++ src/platform/forms-system/src/js/patterns/array-builder/arrayBuilder.jsx
@@ -699,6 +699,7 @@ export function arrayBuilderPages(options, pageBuilderCallback) {
     if (!pageConfig.uiSchema) {
       page.uiSchema = {};
     }
+
     if (!pageConfig.schema || !Object.keys(pageConfig.schema).length) {
       page.schema = {
         type: 'object',
