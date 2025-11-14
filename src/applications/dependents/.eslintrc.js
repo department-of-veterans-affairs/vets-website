0c8155844bf36b89692c23c44571b6b3a0fa8a57
Matthew Knight <matt.knight@coforma.io>
2025-11-14 13:37:47 -0500

    Updated to warn

--- src/applications/dependents/.eslintrc.js
+++ src/applications/dependents/.eslintrc.js
@@ -1,16 +1,20 @@
 /* 
+The VA is using an old version of ESLint, which is why we are sticking with .eslintrc.js files.
+Once they upgrade, we can consider moving to other formats:
+https://eslint.org/docs/latest/use/configure/configuration-files
+
 require-jsdoc is deprecated in ESLint v8 and will be removed in v9
 but we are keeping it for now to enforce JSDoc comments in this project.
 
 When ESLINT on vets-website is updated, consider using:
 https://eslint.org/docs/latest/rules/require-jsdoc
 */
+
 module.exports = {
   rules: {
-    'prefer-const': 'error', // https://eslint.org/docs/latest/rules/prefer-const
-    'require-jsdoc': 'error', // https://eslint.org/docs/latest/rules/require-jsdoc
-    'valid-jsdoc': 'error', // https://eslint.org/docs/latest/rules/valid-jsdoc
-    'no-unused-vars': 'error', // https://eslint.org/docs/latest/rules/no-unused-vars
+    'prefer-const': 'warn', // https://eslint.org/docs/latest/rules/prefer-const
+    'require-jsdoc': 'warn', // https://eslint.org/docs/latest/rules/require-jsdoc
+    'valid-jsdoc': 'warn', // https://eslint.org/docs/latest/rules/valid-jsdoc
+    'no-unused-vars': 'warn', // https://eslint.org/docs/latest/rules/no-unused-vars
   },
-  ignorePatterns: ['686c-674-old/*'], // <<< ignore all files in the old 686c-674 directory which will be removed at a later date
 };
