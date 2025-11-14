313e90faa8dcdd291268f7ae31566fb5a14685a4
Chad Laflamme <216556326+sixercjl@users.noreply.github.com>
2025-11-14 10:54:30 -0500

    basic checkbox setup

--- src/applications/vass/pages/TopicSelection.jsx
+++ src/applications/vass/pages/TopicSelection.jsx
@@ -1,13 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { VaCheckbox } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import Wrapper from '../layout/Wrapper';
 
+const topics = [
+  'Burial',
+  'Chapter 36-PCPG',
+  'Compensation',
+  'Dental',
+  'Discharge-Upgrade',
+  'Education',
+  'Finance',
+  'Health care',
+  'Insurance',
+  'Legal',
+  'Loan Gauranty',
+  'Mental Health Support/Resources',
+  'Pension',
+  'VA Records',
+  'Veterans Readiness and Employment',
+  'Woman Veterans Coordinator',
+  'General VA Benefits',
+];
+
 const TopicSelection = () => {
+  const [selectedTopics, setSelectedTopics] = useState([]);
+
+  const handleTopicChange = event => {
+    const { checked } = event.detail;
+    if (checked) {
+      setSelectedTopics([...selectedTopics, event.target.value]);
+    } else {
+      setSelectedTopics(
+        selectedTopics.filter(topic => topic !== event.target.value),
+      );
+    }
+  };
   return (
-    <Wrapper pageTitle="What topic would you like to talk about?">
+    <Wrapper pageTitle="What topic would you like to talk about?" required>
       <va-link
         href="/service-member/benefits/solid-start/schedule/review"
         text="Continue"
       />
+      <va-checkbox-group
+        error={null}
+        hint={null}
+        label="Check all that apply"
+        label-header-level=""
+        message-aria-describedby={null}
+      >
+        {topics.map(topic => (
+          <VaCheckbox
+            key={topic}
+            label={topic}
+            name="topic"
+            value={topic}
+            onVaChange={handleTopicChange}
+            checked={selectedTopics.includes(topic)}
+          />
+        ))}
+      </va-checkbox-group>
     </Wrapper>
   );
 };
