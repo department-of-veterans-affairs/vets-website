a30121e2c36566458a2dc73c660dc977dda69f93
John Woolschlager <wullaski@gmail.com>
2025-11-14 13:40:23 -0500

    Small fix for padding

--- src/applications/vass/pages/DateTimeSelection.jsx
+++ src/applications/vass/pages/DateTimeSelection.jsx
@@ -113,7 +113,10 @@ const DateTimeSelection = () => {
   );
 
   return (
-    <Wrapper pageTitle="What date and time do you want for this appointment?">
+    <Wrapper
+      pageTitle="What date and time do you want for this appointment?"
+      classNames="vads-u-margin-top--4"
+    >
       <div data-testid="content">
         <p>
           Select an available date and time from the calendar below. Appointment
@@ -151,6 +154,7 @@ const DateTimeSelection = () => {
         data-testid="continue-button"
         continue
         onClick={handleContinue}
+        text={null}
       />
     </Wrapper>
   );
