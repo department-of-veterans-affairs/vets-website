c42606ddb6d80d75e897ef2ada3cf1ed838d557e
Andrés Rivera Toro <36863582+andresriveratoro@users.noreply.github.com>
2025-11-14 19:17:32 -0500

    record result of tsa letter eligibility on network request

--- src/applications/letters/containers/LetterList.jsx
+++ src/applications/letters/containers/LetterList.jsx
@@ -46,10 +46,12 @@ export class LetterList extends React.Component {
   getTsaLetter() {
     return apiRequest(GET_TSA_LETTER_ELIGIBILITY_ENDPOINT)
       .then(response => {
-        if (Array.isArray(response.data) && response.data.length > 0) {
+        const hasTSALetter =
+          Array.isArray(response.data) && response.data.length > 0;
+        if (hasTSALetter) {
           const latestLetter = response.data.reduce((latest, current) => {
-            const latestDate = latest.attributes?.receivedAt || 0;
-            const currentDate = current.attributes?.receivedAt || 0;
+            const latestDate = latest.attributes?.receivedAt || '0';
+            const currentDate = current.attributes?.receivedAt || '0';
             return currentDate > latestDate ? current : latest;
           });
           this.setState({ tsaLetter: latestLetter });
@@ -58,6 +60,7 @@ export class LetterList extends React.Component {
           event: 'api_call',
           'api-name': 'GET /v0/tsa_letter',
           'api-status': 'successful',
+          'has-letter': hasTSALetter,
         });
       })
       .catch(() => {
