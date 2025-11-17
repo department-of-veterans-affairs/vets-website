387315321ddc447429eeff90e42244ea4329d663
Jerry Sea <Jsea@kindsys.us>
2025-11-17 08:59:38 -0800

    Update error message

--- src/applications/appeals/shared/content/errorMessages.js
+++ src/applications/appeals/shared/content/errorMessages.js
@@ -16,7 +16,7 @@ const errorMessages = {
 
   decisions: {
     blankDate: 'You must enter a decision date',
-    pastDate: 'You must add a decision date that’s in the past',
+    pastDate: cutoffDate => `Enter a date after ${cutoffDate}`,
     recentDate:
       'You must add an issue with a decision date that’s less than a year old',
     newerDate:
