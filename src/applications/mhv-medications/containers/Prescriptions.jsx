2c13e89a99b1e79f66a6548df293f4bbd6f0863e
Cam Keif <ckeif@ironarchtech.com>
2025-11-17 11:33:05 -0500

    fix accidental removal

--- src/applications/mhv-medications/containers/Prescriptions.jsx
+++ src/applications/mhv-medications/containers/Prescriptions.jsx
@@ -77,6 +77,7 @@ import {
 import { buildPdfData } from '../util/buildPdfData';
 import { generateMedicationsPdfFile } from '../util/generateMedicationsPdfFile';
 import FilterAriaRegion from '../components/MedicationsList/FilterAriaRegion';
+import RxRenewalDeleteDraftSuccessAlert from '../components/shared/RxRenewalDeleteDraftSuccessAlert';
 import { useURLPagination } from '../hooks/useURLPagination';
 import { usePageTitle } from '../hooks/usePageTitle';
 
@@ -89,6 +90,7 @@ const Prescriptions = () => {
   const hasMedsByMailFacility = useSelector(selectHasMedsByMailFacility);
   const [searchParams] = useSearchParams();
   const rxRenewalMessageSuccess = searchParams.get('rxRenewalMessageSuccess');
+  const deleteDraftSuccess = searchParams.get('draftDeleteSuccess');
 
   // Get sort/filter selections from store.
   const selectedSortOption = useSelector(selectSortOption);
@@ -539,9 +541,10 @@ const Prescriptions = () => {
   };
 
   const renderRxRenewalMessageSuccess = () => {
-    if (!rxRenewalMessageSuccess) return null;
+    if (deleteDraftSuccess) return <RxRenewalDeleteDraftSuccessAlert />;
 
-    return <RxRenewalMessageSuccessAlert />;
+    if (rxRenewalMessageSuccess) return <RxRenewalMessageSuccessAlert />;
+    return null;
   };
 
   const renderHeader = () => {
