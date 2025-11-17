e824d0abb3a23f56ac25c8cc00a473449a2cba1a
Nicholas Hibbits <172406954+nicholashibbits@users.noreply.github.com>
2025-11-17 09:30:11 -0600

    VEBT-3159: 22-0839 Step 7 POCs (#40101)
    * develop points of contact chapter
    
    * add unit tests
    
    * update error message
    
    * change field description to hint text
    
    * update error message in unit test
    
    ---------
    
    Co-authored-by: Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>

--- src/applications/edu-benefits/0839/helpers.js
+++ src/applications/edu-benefits/0839/helpers.js
@@ -407,3 +407,39 @@ export const facilityCodeUIValidation = (errors, fieldData, formData) => {
     }
   }
 };
+
+export const showAdditionalPointsOfContact = formData => {
+  const isYellowRibbonProgramPointOfContact =
+    formData?.pointsOfContact?.roles?.isYellowRibbonProgramPointOfContact ===
+    true;
+  const isSchoolFinancialRepresentative =
+    formData?.pointsOfContact?.roles?.isSchoolFinancialRepresentative === true;
+  const isSchoolCertifyingOfficial =
+    formData?.pointsOfContact?.roles?.isSchoolCertifyingOfficial === true;
+
+  if (
+    (isYellowRibbonProgramPointOfContact || isSchoolFinancialRepresentative) &&
+    isSchoolCertifyingOfficial
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+export const getAdditionalContactTitle = formData => {
+  const isYellowRibbonProgramPointOfContact =
+    formData?.pointsOfContact?.roles?.isYellowRibbonProgramPointOfContact ===
+    true;
+  const isSchoolFinancialRepresentative =
+    formData?.pointsOfContact?.roles?.isSchoolFinancialRepresentative === true;
+
+  if (
+    !isSchoolFinancialRepresentative &&
+    !isYellowRibbonProgramPointOfContact
+  ) {
+    return 'Add Yellow Ribbon Program point of contact';
+  }
+
+  return 'Add school certifying official';
+};
