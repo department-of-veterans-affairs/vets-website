06ab61d6c32c1b6ff44d0279f985910548b31384
Luis Metzger <luismetzger@me.com>
2025-11-18 11:13:31 -0500

    [#124441] Align confirmation field custom components naming convention for CoS (#40076)
    * [#124441] Align confirmation field custom components naming convention for CoS.

--- src/applications/disability-benefits/all-claims/tests/components/ConfirmationNewDisabilities.unit.spec.jsx
+++ src/applications/disability-benefits/all-claims/tests/components/ConfirmationNewDisabilities.unit.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
-import ConfirmationNewDisabilities from '../../components/ConfirmationNewDisabilities';
+import ConfirmationNewDisabilities from '../../components/confirmationFields/ConfirmationNewDisabilities';
 
 describe('ConfirmationNewDisabilities', () => {
   describe('NEW conditions', () => {
