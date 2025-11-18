4cc97e51d8674f08a4311edbb40eb1a952395bd8
Liz Townsend <liz.townsend@adhocteam.us>
2025-11-18 16:02:00 -0600

    update mock data

--- src/applications/mhv-medications/mocks/api/feature-toggles/index.js
+++ src/applications/mhv-medications/mocks/api/feature-toggles/index.js
@@ -5,6 +5,8 @@ const generateFeatureToggles = (toggles = {}) => {
     mhvMedicationsDisplayPendingMeds = true,
     mhvMedicationsPartialFillContent = true,
     mhvMedicationsDontIncrementIpeCount = true,
+    mhvAcceleratedDeliveryEnabled = true,
+    mhvAcceleratedDeliveryAllergiesEnabled = true,
     mhvMedicationsDisplayNewCernerFacilityAlert = true,
     mhvSecureMessagingMedicationsRenewalRequest = false,
   } = toggles;
@@ -32,6 +34,14 @@ const generateFeatureToggles = (toggles = {}) => {
         {
           name: 'mhv_medications_dont_increment_ipe_count',
           value: mhvMedicationsDontIncrementIpeCount,
+        },
+        {
+          name: 'mhv_accelerated_delivery_enabled',
+          value: mhvAcceleratedDeliveryEnabled,
+        },
+        {
+          name: 'mhv_accelerated_delivery_allergies_enabled',
+          value: mhvAcceleratedDeliveryAllergiesEnabled,
         },
         {
           name: 'mhv_medications_display_new_cerner_facility_alert',
