0c6987f43fe5959b1395ab3752df369a46336019
emisparkles <emisargent@gmail.com>
2025-11-18 13:37:15 -0800

    Add base layout and content

--- src/applications/personalization/profile/components/health-care-settings/SchedulingPreferences.jsx
+++ src/applications/personalization/profile/components/health-care-settings/SchedulingPreferences.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
+
 import { PROFILE_PATH_NAMES } from '../../constants';
 import Headline from '../ProfileSectionHeadline';
-import LoadFail from '../alerts/LoadFail';
+import SchedulingPreferencesContent from './SchedulingPreferencesContent';
 
 const SchedulingPreferences = () => {
-  const error = false; // TODO: get error state from redux when implemented
+  const hasSchedulingPreferencesError = false; // TODO: get error state from redux when implemented
 
   return (
     <>
       <Headline
         dataTestId="scheduling-preferences-page-headline"
-        classes={error ? 'vads-u-margin-bottom--4' : 'vads-u-margin-bottom--1'}
+        classes={
+          hasSchedulingPreferencesError
+            ? 'vads-u-margin-bottom--4'
+            : 'vads-u-margin-bottom--1'
+        }
       >
         {PROFILE_PATH_NAMES.SCHEDULING_PREFERENCES}
       </Headline>
-      {error && <LoadFail />}
-      {!error && (
-        <p className="vads-u-margin-top--0 vads-u-margin-bottom--4">
-          Manage your scheduling preferences for health care appointments.
-          Whenever possible, schedulers will consider your preferences when
-          making appointments.
-        </p>
-      )}
+      <SchedulingPreferencesContent
+        hasSchedulingPreferencesError={hasSchedulingPreferencesError}
+      />
     </>
   );
 };
