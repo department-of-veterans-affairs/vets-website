7bce772c580414ecca5ec2f729a55fb7a3466b4a
Liz Wendling <170662489+GovCIOLiz@users.noreply.github.com>
2025-11-19 08:57:59 -0800

    Wip

--- src/applications/edu-benefits/0839/components/YellowRibbonProgramDescription.jsx
+++ src/applications/edu-benefits/0839/components/YellowRibbonProgramDescription.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { VaAlert } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { useSelector } from 'react-redux';
 
 const YellowRibbonProgramDescription = () => {
@@ -17,8 +16,12 @@ const YellowRibbonProgramDescription = () => {
       <ul className="vads-u-width--full">
         {isUsaSchool ? (
           <>
-            <li className="vads-u-margin-bottom--1 vads-u-margin-left--1 vads-u-padding-left--2p5" data-testid="us-school-text">
-              The maximum number or unlimited number of students your school will support
+            <li
+              className="vads-u-margin-bottom--1 vads-u-margin-left--1 vads-u-padding-left--2p5"
+              data-testid="us-school-text"
+            >
+              The maximum number or unlimited number of students your school
+              will support
             </li>
             <li className="vads-u-margin-bottom--1 vads-u-margin-left--1 vads-u-padding-left--2p5">
               The degree level
@@ -26,25 +29,37 @@ const YellowRibbonProgramDescription = () => {
             <li className="vads-u-margin-bottom--1 vads-u-margin-left--1 vads-u-padding-left--2p5">
               The name of the college or professional school, if applicable
             </li>
-            {isUsaSchool && <li className="vads-u-margin-bottom--1 vads-u-margin-left--1 vads-u-padding-left--2p5">
-              The maximum annual contribution amount per student, or the amount to 
-              pay toward remaining tuition that the Post-9/11 GI Bill doesn't cover 
-              (unlimited). Enter the total amount your school plans to contribute each year, 
-              not by term or credit hour. If the amount entered is over $99,999, the
-              system will treat it as an unlimited contribution.
-            </li> }
-            {!isUsaSchool && <li className="vads-u-margin-bottom--1 vads-u-margin-left--1 vads-u-padding-left--2p5">
-              The maximum annual contribution amount per student, or the amount to
-              pay toward remaining tuition that the Post-9/11 GI Bill doesn't cover 
-              (unlimited). Enter the total amount your school plans to contribute each year
-              in your institution's official billing currency, not by term or credit hour.
-              If the amount entered is over $99,999 USD, the system will treat is as an
-              unlimited contribution.
-            </li> }
+            {isUsaSchool && (
+              <li className="vads-u-margin-bottom--1 vads-u-margin-left--1 vads-u-padding-left--2p5">
+                The maximum annual contribution amount per student, or the
+                amount to pay toward remaining tuition that the Post-9/11 GI
+                Bill doesn’t cover (unlimited). Enter the total amount your
+                school plans to contribute each year, not by term or credit
+                hour. If the amount entered is over $99,999, the system will
+                treat it as an unlimited contribution.
+              </li>
+            )}
+            {!isUsaSchool && (
+              <li
+                cla
+                sName="vads-u-margin-bottom--1 vads-u-margin-left--1 vads-u-padding-left--2p5"
+              >
+                The maximum annual contribution amount per student, or the
+                amount to pay toward remaining tuition that the Post-9/11 GI
+                Bill doesn’t cover (unlimited). Enter the total amount your
+                school plans to contribute each year in your institution’s
+                official billing currency, not by term or credit hour. If the
+                amount entered is over $99,999 USD, the system will treat is as
+                an unlimited contribution.
+              </li>
+            )}
           </>
         ) : (
           <>
-            <li className="vads-u-margin-bottom--1 vads-u-margin-left--1 vads-u-padding-left--2p5" data-testid="foreign-school-text">
+            <li
+              className="vads-u-margin-bottom--1 vads-u-margin-left--1 vads-u-padding-left--2p5"
+              data-testid="foreign-school-text"
+            >
               The maximum number of students your school will support. If your
               school plans to support an unlimited number of qualifying
               students, you can enter “unlimited.”
@@ -70,12 +85,17 @@ const YellowRibbonProgramDescription = () => {
   return (
     <div className="yellow-ribbon-program-description">
       {uiList}
-      {isUsaSchool ? <p>
-        You can add more entries if your school offers different contributions
-        based on degree level or other factors.
-      </p> : <p>You may add more entries if your school offers different contributions
-        based on degree level or other factors.
-      </p>}
+      {isUsaSchool ? (
+        <p>
+          You can add more entries if your school offers different contributions
+          based on degree level or other factors.
+        </p>
+      ) : (
+        <p>
+          You may add more entries if your school offers different contributions
+          based on degree level or other factors.
+        </p>
+      )}
       {!isUsaSchool && (
         <p data-testid="foreign-school-text">
           Before continuing, please confirm that your school agrees to provide
