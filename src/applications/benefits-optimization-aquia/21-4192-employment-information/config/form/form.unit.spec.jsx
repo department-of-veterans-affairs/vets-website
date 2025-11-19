1cd8bf65d93fb4306e2233d731ef4cdccfc1df0d
Justin Linn <justin@linnjs.com>
2025-11-19 10:56:04 -0500

    fix(4192): Fix unit tests

--- src/applications/benefits-optimization-aquia/21-4192-employment-information/config/form/form.unit.spec.jsx
+++ src/applications/benefits-optimization-aquia/21-4192-employment-information/config/form/form.unit.spec.jsx
@@ -478,28 +478,16 @@ describe('Form Configuration', () => {
       expect(formConfig.preSubmitInfo).to.exist;
     });
 
-    it('should have statementOfTruth configuration', () => {
-      expect(formConfig.preSubmitInfo.statementOfTruth).to.exist;
+    it('should use CustomComponent for signature validation', () => {
+      expect(formConfig.preSubmitInfo.CustomComponent).to.exist;
     });
 
-    it('should have statementOfTruth body text', () => {
-      expect(formConfig.preSubmitInfo.statementOfTruth.body).to.equal(
-        'I confirm that the identifying information in this form is accurate and has been represented correctly.',
-      );
+    it('should use PreSubmitInfo component', () => {
+      expect(formConfig.preSubmitInfo.CustomComponent).to.be.a('function');
     });
 
-    it('should have statementOfTruth messageAriaDescribedby', () => {
-      expect(
-        formConfig.preSubmitInfo.statementOfTruth.messageAriaDescribedby,
-      ).to.equal(
-        'I confirm that the identifying information in this form is accurate and has been represented correctly.',
-      );
-    });
-
-    it('should have fullNamePath set to veteranInformation.veteranFullName', () => {
-      expect(formConfig.preSubmitInfo.statementOfTruth.fullNamePath).to.equal(
-        'veteranInformation.veteranFullName',
-      );
+    it('should not have statementOfTruth configuration (uses CustomComponent instead)', () => {
+      expect(formConfig.preSubmitInfo.statementOfTruth).to.not.exist;
     });
   });
 
