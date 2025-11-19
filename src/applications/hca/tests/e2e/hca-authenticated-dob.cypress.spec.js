ce8b406eab274f3b01159b0d05b01bc03d66b61f
rmessina1010 <rmessina10@gmail.com>
2025-11-19 16:32:41 -0600

    hca-authenticated-dob.cypress attempt

--- src/applications/hca/tests/e2e/hca-authenticated-dob.cypress.spec.js
+++ src/applications/hca/tests/e2e/hca-authenticated-dob.cypress.spec.js
@@ -1,35 +1,48 @@
 import mockUserInvalidDob from './fixtures/mocks/user.invalidDob.json';
 import mockUserNoDob from './fixtures/mocks/user.noDob.json';
-import { goToNextPage, setupForAuth, startAsAuthUser } from './utils';
+import mockUserValidDob from './fixtures/mocks/user.json';
+import { goToNextPage, setupForAuth } from './utils';
 
 describe('HCA-User-Authenticated: No DOB value', () => {
   beforeEach(() => {
+    cy.intercept('GET', '/v0/profile', {
+      body: mockUserInvalidDob,
+    }).as('mockPrefill');
     setupForAuth({ user: mockUserNoDob });
-    startAsAuthUser();
+    // startAsAuthUser();
+    cy.wait('@mockPrefill');
   });
 
   it('works with profile data that has no date of birth ', () => {
-    goToNextPage('/veteran-information/profile-information-dob');
+    goToNextPage('veteran-information/birth-information');
     cy.injectAxeThenAxeCheck();
   });
 });
 
 describe('HCA-User-Authenticated: Invalid DOB value', () => {
   beforeEach(() => {
+    cy.intercept('GET', '/v0/profile', {
+      body: mockUserInvalidDob,
+    }).as('mockPrefill');
     setupForAuth({ user: mockUserInvalidDob });
-    startAsAuthUser();
+    // startAsAuthUser();
+    cy.wait('@mockPrefill');
   });
 
   it('works with profile data that has an invalid date of birth ', () => {
-    goToNextPage('/veteran-information/profile-information-dob');
+    goToNextPage('veteran-information/birth-information');
     cy.injectAxeThenAxeCheck();
   });
 });
 
 describe('HCA-User-Authenticated: With DOB value', () => {
   beforeEach(() => {
+    cy.intercept('GET', '/v0/profile', {
+      body: mockUserValidDob,
+    }).as('mockPrefill');
     setupForAuth();
-    startAsAuthUser();
+    // startAsAuthUser();
+    cy.wait('@mockPrefill');
   });
 
   it('works with profile data that has valid date of birth ', () => {
