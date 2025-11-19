37a3d26811ea8b92d08b7ddac3d0cefea2b9e0f5
rmessina1010 <rmessina10@gmail.com>
2025-11-19 17:32:14 -0600

    hca-authenticated-dob.cypress attempt, pt. 2

--- src/applications/hca/tests/e2e/hca-authenticated-dob.cypress.spec.js
+++ src/applications/hca/tests/e2e/hca-authenticated-dob.cypress.spec.js
@@ -1,7 +1,7 @@
 import mockUserInvalidDob from './fixtures/mocks/user.invalidDob.json';
 import mockUserNoDob from './fixtures/mocks/user.noDob.json';
 import mockUserValidDob from './fixtures/mocks/user.json';
-import { goToNextPage, setupForAuth } from './utils';
+import { goToNextPage, setupForAuth, startAsAuthUser } from './utils';
 
 describe('HCA-User-Authenticated: No DOB value', () => {
   beforeEach(() => {
@@ -9,8 +9,8 @@ describe('HCA-User-Authenticated: No DOB value', () => {
       body: mockUserInvalidDob,
     }).as('mockPrefill');
     setupForAuth({ user: mockUserNoDob });
-    // startAsAuthUser();
-    cy.wait('@mockPrefill');
+    startAsAuthUser();
+    // cy.wait('@mockPrefill');
   });
 
   it('works with profile data that has no date of birth ', () => {
@@ -25,8 +25,8 @@ describe('HCA-User-Authenticated: Invalid DOB value', () => {
       body: mockUserInvalidDob,
     }).as('mockPrefill');
     setupForAuth({ user: mockUserInvalidDob });
-    // startAsAuthUser();
-    cy.wait('@mockPrefill');
+    startAsAuthUser();
+    // cy.wait('@mockPrefill');
   });
 
   it('works with profile data that has an invalid date of birth ', () => {
@@ -41,8 +41,8 @@ describe('HCA-User-Authenticated: With DOB value', () => {
       body: mockUserValidDob,
     }).as('mockPrefill');
     setupForAuth();
-    // startAsAuthUser();
-    cy.wait('@mockPrefill');
+    startAsAuthUser();
+    // cy.wait('@mockPrefill');
   });
 
   it('works with profile data that has valid date of birth ', () => {
