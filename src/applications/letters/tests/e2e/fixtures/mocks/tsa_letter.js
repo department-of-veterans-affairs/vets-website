63a65944a9b143f0007dc7c4acfb27b4fc8eb305
Andrés Rivera Toro <36863582+andresriveratoro@users.noreply.github.com>
2025-11-19 15:51:53 -0500

    [CVE-2142] add e2e tests (#40004)
    * add endpoints
    
    * add letter accordion
    
    * fix ref logic
    
    * fix ref logic
    
    * update content
    
    * refactor
    
    * revert change
    
    * display eligibility error on tsa failure
    
    * refactor feature flag logic
    
    * fix tests
    
    * fix unit test
    
    * remove filter on response
    
    * add feature flag tests
    
    * fix bad merge conflict
    
    * add e2e tests
    
    * add test

---
+++ src/applications/letters/tests/e2e/fixtures/mocks/tsa_letter.js
@@ -0,0 +1,14 @@
+export const tsaLetter = {
+  data: [
+    {
+      id: '',
+      type: 'tsa_letter',
+      attributes: {
+        documentId: '{3F7A891C-2E4B-4D29-9C6E-5B8F14A07E32}',
+        docType: '34',
+        typeDescription: 'Correspondence',
+        receivedAt: '2025-09-09',
+      },
+    },
+  ],
+};
