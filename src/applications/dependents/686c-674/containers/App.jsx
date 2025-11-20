8cd269e9b37da882e7796c1666a4087ab0b99a04
Dakota Larson <dakota.larson@oddball.io>
2025-11-20 14:30:54 -0800

    Revert "update dep loading logic"
    This reverts commit f066a0071ca6c77ea3559c926c583c37c94093c1.

--- src/applications/dependents/686c-674/containers/App.jsx
+++ src/applications/dependents/686c-674/containers/App.jsx
@@ -42,7 +42,7 @@ function App({
 
   useEffect(
     () => {
-      if (isLoggedIn && !isPrefill && isIntroPage) {
+      if (isLoggedIn && !isPrefill && !isIntroPage) {
         if (loadingDependents) {
           fetchDependents();
         } else {
