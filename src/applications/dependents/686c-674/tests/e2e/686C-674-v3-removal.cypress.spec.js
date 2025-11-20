ee50026a6006bbdcc598e0fad707732e2909ea4d
Rob Garrison <136959+Mottie@users.noreply.github.com>
2025-11-20 10:11:16 -0600

    Update v3 remove only e2e test

--- src/applications/dependents/686c-674/tests/e2e/686C-674-v3-removal.cypress.spec.js
+++ src/applications/dependents/686c-674/tests/e2e/686C-674-v3-removal.cypress.spec.js
@@ -3,51 +3,73 @@ import testForm from 'platform/testing/e2e/cypress/support/form-tester';
 import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-tester/utilities';
 
 import formConfig from '../../config/form';
+import { PICKLIST_DATA } from '../../config/constants';
 import manifest from '../../manifest.json';
 import mockVaFileNumber from './fixtures/va-file-number.json';
 import mockDependents from './fixtures/mock-dependents.json';
 import user from './user.json';
-import {
-  fillDateWebComponentPattern,
-  fillStandardTextInput,
-  fillSelectWebComponent,
-  selectRadioWebComponent,
-  signAndSubmit,
-} from './cypress.helpers';
+import { signAndSubmit } from './cypress.helpers';
 
 Cypress.config('waitForAnimations', true);
 
 /**
  * Helper to select checkboxes in the V3 picklist
  * @param {string} key - The dependent key (e.g., 'spousy-3332')
+ * @returns {void}
  */
 const selectPicklistDependent = key => {
   cy.get(`va-checkbox[data-key="${key}"]`)
-        .shadow()
-        .find('label')
-        .click({ force: true });
+    .shadow()
+    .find('label')
+    .click({ force: true });
 };
 
 /**
- * Helper to click continue on picklist page and navigate to followup
+ * Fill out picklist dependent; works for any followup page
+ * @param {Object} dependent - The dependent data to fill
+ * @returns {void}
  */
-const continueFromPicklist = () => {
-  cy.clickFormContinue();
+const fillOutPicklistPage = dependent => {
+  // Only fill out the field if it exists on the page
+  const checkElAndFill = (name, fillFnName) => {
+    cy.get(
+      'va-radio-option, va-checkbox, va-text-input, va-memorable-date, va-select',
+    ).each($el => {
+      if ($el[0].getAttribute('name') === name) {
+        cy[fillFnName](name, dependent[name]);
+      }
+    });
+  };
+
+  checkElAndFill('isStepchild', 'selectVaRadioOption');
+  checkElAndFill('removalReason', 'selectVaRadioOption');
+  checkElAndFill('childHasPermanentDisability', 'selectVaRadioOption');
+  checkElAndFill('stepchildFinancialSupport', 'selectVaRadioOption');
+
+  checkElAndFill('endType', 'selectVaRadioOption');
+  checkElAndFill('endAnnulmentOrVoidDescription', 'fillVaTextInput');
+  checkElAndFill('endDate', 'fillVaMemorableDate');
+
+  checkElAndFill('endOutsideUS', 'selectVaCheckbox');
+  checkElAndFill('endCity', 'fillVaTextInput');
+  checkElAndFill('endProvince', 'fillVaTextInput');
+  checkElAndFill('endCountry', 'selectVaSelect');
+  checkElAndFill('endState', 'selectVaSelect');
+  cy.injectAxeThenAxeCheck();
 };
 
 /**
  * Helper to get the current dependent from picklist based on URL index param
  * @param {Object} data - The test data
  * @param {Function} callback - Callback function with dependent data
+ * @returns {void}
  */
-const getCurrentDependent = (data, callback) => {
-  const picklistData = data['view:removeDependentPickList'] || [];
-
-  cy.url().then(url => {
-    const urlObj = new URL(url);
-    const index = parseInt(urlObj.searchParams.get('index'), 10);
-    const dependent = picklistData[index];
-    callback(dependent);
+const processCurrentDependent = data => {
+  return cy.location('search').then(search => {
+    const urlParams = new URLSearchParams(search);
+    const index = parseInt(urlParams.get('index') || '0', 10);
+    const dependent = data[index];
+    fillOutPicklistPage(dependent);
   });
 };
 
@@ -96,9 +118,11 @@ const testConfig = createTestConfig(
         }).as('mockDependents');
 
         // Mock dependents_applications/show endpoint to prevent "Unknown error"
-        cy.intercept('GET', '/v0/dependents_applications/show', mockDependents).as(
-          'mockDependentsShow',
-        );
+        cy.intercept(
+          'GET',
+          '/v0/dependents_applications/show',
+          mockDependents,
+        ).as('mockDependentsShow');
 
         // In-progress form GET must return formData and metadata structure
         cy.intercept('GET', '/v0/in_progress_forms/686C-674-V2', {
@@ -149,32 +173,68 @@ const testConfig = createTestConfig(
       },
 
       // V3 Picklist selection page
-      'options-selection/remove-active-dependents': ({ beforeHook, afterHook }) => {
+      'options-selection/remove-active-dependents': ({ afterHook }) => {
         afterHook(() => {
           // Only select 1 parent, 1 child, and 1 spouse for testing
-          // selectPicklistDependent('spousy-3332'); // spouse with marriage ended
+          selectPicklistDependent('spousy-3332'); // spouse with marriage ended
           selectPicklistDependent('penny-3479'); // child who married
           selectPicklistDependent('peter-0104'); // parent who died
 
-          continueFromPicklist();
+          cy.clickFormContinue();
         });
       },
 
-      // Spouse followup pages
-      'remove-dependent/spouse-reason-to-remove': () => {
-        // Don't use afterHook - it causes autofill to run first
-        // Instead, manually interact with the page and click continue
-        cy.wait(500); // Wait for React rendering
-        cy.get('@testData').then(data => {
-          getCurrentDependent(data, spouse => {
-            if (spouse?.removalReason) {
-              selectRadioWebComponent('removalReason', spouse.removalReason);
-            }
-            cy.clickFormContinue();
+      // This pagehook is only called once for this 'remove-dependent' followup
+      // page. The search parameter changes for each dependent and page, so we
+      // need to get the search index param to figure out the dependent to
+      // process. The processCurrentDependent helper looks for any expected
+      // fields and fills them in, so we don't need to check the actual page
+      // name
+      'remove-dependent': ({ afterHook }) => {
+        afterHook(() => {
+          // TO DO: We should dynamically update all the child dependents date
+          // of births in the "mock-dependents.json" file otherwise the reason
+          // to remove options may not appear as expected; this current setup
+          // will continue to work because Penny's age > 15 years old will
+          // always show the marriage option
+          cy.get('@testData').then(data => {
+            const picklistData = data[PICKLIST_DATA] || [];
+            // Spousy: reason to remove
+            processCurrentDependent(picklistData);
+            cy.get('va-button[continue]').click();
+
+            // Spousy: marriage ended details
+            processCurrentDependent(picklistData);
+            cy.get('va-button[continue]').click();
+
+            // Penny: stepchild question
+            processCurrentDependent(picklistData);
+            cy.get('va-button[continue]').click();
+
+            // Penny: reason to remove
+            processCurrentDependent(picklistData);
+            cy.get('va-button[continue]').click();
+
+            // Penny: marriage date
+            processCurrentDependent(picklistData);
+            cy.get('va-button[continue]').click();
+
+            // Peter: reason to remove
+            processCurrentDependent(picklistData);
+            cy.get('va-button[continue]').click();
+
+            // Peter: date of death
+            processCurrentDependent(picklistData);
+            cy.get('va-button[continue]').click();
           });
         });
       },
 
+      'review-and-submit': ({ afterHook }) => {
+        afterHook(() => {
+          signAndSubmit();
+        });
+      },
     },
     // skip: Cypress.env('CI'),
   },
