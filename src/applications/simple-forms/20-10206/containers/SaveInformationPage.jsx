c988df13d8af39f6610a36915f7a1c17a7d61eea
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2025-11-21 17:07:19 -0500

    Styling form buttons

--- src/applications/simple-forms/20-10206/containers/SaveInformationPage.jsx
+++ src/applications/simple-forms/20-10206/containers/SaveInformationPage.jsx
@@ -21,41 +21,53 @@ const SaveInformationPage = props => {
   };
 
   return (
-    <div className="usa-width-two-thirds columns">
-      <h1>
-        Your information will be saved automatically after every change you
-        make.
-      </h1>
-      <p>
-        If you want to take a break from filling out this form, you can stop
-        anytime using the "Finish later" button.
-      </p>
-      <p>
-        When you’re ready to resume the form, reopen it from My VA or visit the
-        form introduction page.
-      </p>
+    <>
+      <div className="row">
+        <div className="usa-width-two-thirds columns print-full-width vads-u-padding-left--2">
+          <div className="progress-box progress-box-schemaform">
+            <div className="form-panel preparer-type-page">
+              <h1>
+                Your information will be saved automatically after every change
+                you make.
+              </h1>
+              <p>
+                If you want to take a break from filling out this form, you can
+                stop anytime using the "Finish later" button.
+              </p>
+              <p>
+                When you’re ready to resume the form, reopen it from My VA or
+                visit the form introduction page.
+              </p>
 
-      <div>
-        <va-link
-          href={`${environment.BASE_URL}/my-va`}
-          text="Find out how to change your address in your VA.gov profile"
-        />
+              <div>
+                <va-link
+                  href={`${environment.BASE_URL}/my-va`}
+                  text="Find out how to change your address in your VA.gov profile"
+                />
+              </div>
+              <div className="vads-u-margin-top--1">
+                <va-link
+                  href={`${
+                    environment.BASE_URL
+                  }/records/request-personal-records-form-20-10206`}
+                  text="VA Form 20-10206 Introduction (opens in a new window)"
+                />
+              </div>
+              <FormNavButtons
+                goBack={handleGoBack}
+                goForward={handleGoForward}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="vads-u-margin-top--1">
-        <va-link
-          href={`${
-            environment.BASE_URL
-          }/records/request-personal-records-form-20-10206`}
-          text="VA Form 20-10206 Introduction (opens in a new window)"
-        />
+      <div className="row vads-u-padding-left--2">
+        <h2 className="vads-u-margin-bottom--0 vads-u-padding-bottom--0p5 vads-u-font-size--h3 vads-u-border-bottom--2px vads-u-border-color--primary">
+          Need help?
+        </h2>
+        <GetFormHelp />
       </div>
-      <FormNavButtons goBack={handleGoBack} goForward={handleGoForward} />
-
-      <h2 className="vads-u-margin-bottom--0 vads-u-padding-bottom--0p5 vads-u-font-size--h3 vads-u-border-bottom--2px vads-u-border-color--primary">
-        Need help?
-      </h2>
-      <GetFormHelp />
-    </div>
+    </>
   );
 };
 
