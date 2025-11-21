1e2c90d12516fb9e9e0cbb0fae1db369624a3773
Chad Laflamme <216556326+sixercjl@users.noreply.github.com>
2025-11-21 12:50:22 -0500

    add datetime format test

--- src/applications/vass/components/DateTime.unit.spec.jsx
+++ src/applications/vass/components/DateTime.unit.spec.jsx
@@ -12,4 +12,16 @@ describe('VASS Component: DateTime', () => {
 
     expect(getByTestId('date-time-description')).to.exist;
   });
+
+  it('should format date in correct format', () => {
+    const { getByTestId } = render(<DateTime dateTime={mockDateTime} />);
+
+    const dateTimeElement = getByTestId('date-time-description');
+    const text = dateTimeElement.textContent;
+
+    // Check for weekday, month, day, year format (e.g., "Monday, November 17, 2025")
+    expect(text).to.match(/\w+,\s\w+\s\d{1,2},\s\d{4}/);
+    // Check for time format (e.g., "03:00 PM EST" or "3:00 pm EST")
+    expect(text).to.match(/\d{1,2}:\d{2}\s[APap][Mm]/);
+  });
 });
