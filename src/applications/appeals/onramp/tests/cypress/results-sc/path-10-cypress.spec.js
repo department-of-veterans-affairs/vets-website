01b3babd59bf23c353b2faa7d889734cd68e9289
Jerry Sea <jsea@kindsys.us>
2025-11-21 08:24:05 -0800

    launch-blocking Onramp design iterations (#40224)
    * Split radio buttons and update/create new tests
    
    * Update spec description
    
    * Update spec files
    
    * Update other spec files for missing assertions
    
    * Update content for radio buttons

--- src/applications/appeals/onramp/tests/cypress/results-sc/path-10-cypress.spec.js
+++ src/applications/appeals/onramp/tests/cypress/results-sc/path-10-cypress.spec.js
@@ -3,28 +3,30 @@ import { ROUTES } from '../../../constants';
 import { RESULTS_NAME_MAP } from '../../../constants/results-data-map';
 import * as c from '../../../constants/results-content/dr-screens/card-content';
 
-const { RESULTS_2_IS_1C } = RESULTS_NAME_MAP;
+const { RESULTS_2_S_1A } = RESULTS_NAME_MAP;
 
-// Results HLR: Higher-Level Review recommended (Non-CFI)
+// Results SC: Supplemental Claim recommended (Non-CFI)
 // 1.1 - Yes
 // 1.2 - Yes
 // 1.3 - No
-// 2.IS.1 - No
-// 2.0 - Initial
+// 2.IS.1 - Yes
+// 2.IS.2 - No
+// 2.0 - Claim for Increase
 // 2.IS.1A - No
-// 2.IS.1B - No
+// 2.IS.1B - Yes
 const path = {
   Q_1_1_CLAIM_DECISION: 0,
   Q_1_2_CLAIM_DECISION: 0,
   Q_1_3_CLAIM_CONTESTED: 1,
-  Q_2_IS_1_SERVICE_CONNECTED: 1,
-  Q_2_0_CLAIM_TYPE: 0,
+  Q_2_IS_1_SERVICE_CONNECTED: 0,
+  Q_2_IS_2_CONDITION_WORSENED: 1,
+  Q_2_0_CLAIM_TYPE: 1,
   Q_2_IS_1A_LAW_POLICY_CHANGE: 1,
-  Q_2_IS_1B_NEW_EVIDENCE: 1,
+  Q_2_IS_1B_NEW_EVIDENCE: 0,
 };
 
 describe('Decision Reviews Onramp', () => {
-  describe('Results HLR (path 1)', () => {
+  describe('Results SC (path 10)', () => {
     it('navigates through the flow forward and backward successfully', () => {
       cy.visit(h.ROOT);
       cy.injectAxeThenAxeCheck();
@@ -33,27 +35,26 @@ describe('Decision Reviews Onramp', () => {
 
       // RESULTS
       h.verifyUrl(ROUTES.RESULTS_DR);
-      h.verifyDrResultsHeader(RESULTS_2_IS_1C);
+      h.verifyDrResultsHeader(RESULTS_2_S_1A);
       h.checkOverviewPanel([
-        c.TITLE_HLR,
-        c.TITLE_BOARD_DIRECT,
+        c.TITLE_SC,
+        c.TITLE_BOARD_EVIDENCE,
         c.TITLE_BOARD_HEARING,
       ]);
       h.checkGoodFitCards([
         {
-          type: c.CARD_HLR,
+          type: c.CARD_SC,
           content: [
             c.CARD_GF_REVIEW_INIT,
-            c.CARD_GF_NO_EVIDENCE,
+            c.CARD_GF_YES_EVIDENCE,
             c.CARD_GF_NOT_CONTESTED,
-            c.CARD_GF_NO_LAW_POLICY,
           ],
         },
         {
-          type: c.CARD_BOARD_DIRECT,
+          type: c.CARD_BOARD_EVIDENCE,
           content: [
             c.CARD_GF_REVIEW_INIT,
-            c.CARD_GF_NO_EVIDENCE,
+            c.CARD_GF_YES_EVIDENCE,
             c.CARD_GF_NO_LAW_POLICY,
           ],
         },
@@ -61,7 +62,7 @@ describe('Decision Reviews Onramp', () => {
           type: c.CARD_BOARD_HEARING,
           content: [
             c.CARD_GF_REVIEW_INIT,
-            c.CARD_GF_NO_EVIDENCE,
+            c.CARD_GF_YES_EVIDENCE,
             c.CARD_GF_NO_LAW_POLICY,
           ],
         },
@@ -69,12 +70,12 @@ describe('Decision Reviews Onramp', () => {
       h.verifyClaimForIncreaseCardNotPresent();
       h.checkNotGoodFitCards([
         {
-          type: c.CARD_SC,
-          content: [c.CARD_NGF_NEED_EVIDENCE, c.CARD_NGF_NO_LAW_POLICY],
+          type: c.CARD_HLR,
+          content: [c.CARD_NGF_CANNOT_SUBMIT_EVIDENCE],
         },
         {
-          type: c.CARD_BOARD_EVIDENCE,
-          content: [c.CARD_NGF_NEED_EVIDENCE],
+          type: c.CARD_BOARD_DIRECT,
+          content: [c.CARD_NGF_CANNOT_SUBMIT_EVIDENCE],
         },
       ]);
       h.verifyOutsideDROptionNotPresent();
