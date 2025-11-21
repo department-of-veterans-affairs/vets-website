49d33a4978943936d0fecd301f40722eed782ef1
Midge-dev <sean.midgley.dev@gmail.com>
2025-11-21 10:50:26 -0800

    WIP

--- src/applications/sign-in-health-portal/routes.jsx
+++ src/applications/sign-in-health-portal/routes.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route } from 'react-router';
-// import App from './containers/App';
 import PortalRemovalNotice from './components/PortalRemovalNotice';
 
 const routes = <Route path="/" component={PortalRemovalNotice} />;
