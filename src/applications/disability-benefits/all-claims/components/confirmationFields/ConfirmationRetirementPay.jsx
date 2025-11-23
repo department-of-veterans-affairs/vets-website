d37663f5790433e7d998ada812b5b11acb26a519
Oren Mittman <nihil2501@gmail.com>
2025-11-22 20:25:40 -0500

    confirmation-page.refactor/case-studies/a/phase-1/application

--- src/applications/disability-benefits/all-claims/components/confirmationFields/ConfirmationRetirementPay.jsx
+++ src/applications/disability-benefits/all-claims/components/confirmationFields/ConfirmationRetirementPay.jsx
@@ -2,24 +2,37 @@ import React from 'react';
 
 const ConfirmationRetirementPay = (title, yesNoTitle, branchTitle) => ({
   formData,
+  showPageTitles,
 }) => {
   const branchValue = formData.militaryRetiredPayBranch;
   const yesNoValue = branchValue ? 'Yes' : 'No';
+
+  const fields = [];
+
+  fields.push(
+    <li>
+      <div className="vads-u-color--gray">{yesNoTitle}</div>
+      <div>{yesNoValue}</div>
+    </li>,
+  );
+
+  if (branchValue) {
+    fields.push(
+      <li>
+        <div className="vads-u-color--gray">{branchTitle}</div>
+        <div>{branchValue}</div>
+      </li>,
+    );
+  }
+
+  if (fields.length === 0) return [];
+  if (!showPageTitles) return fields;
 
   return (
     <li>
       <h4>{title}</h4>
       <ul className="vads-u-padding--0" style={{ listStyle: 'none' }}>
-        <li>
-          <div className="vads-u-color--gray">{yesNoTitle}</div>
-          <div>{yesNoValue}</div>
-        </li>
-        {branchValue && (
-          <li>
-            <div className="vads-u-color--gray">{branchTitle}</div>
-            <div>{branchValue}</div>
-          </li>
-        )}
+        {fields}
       </ul>
     </li>
   );
