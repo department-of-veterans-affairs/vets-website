9fb5f907df1791d88e0208497e194cf8e04da93a
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-11-24 11:17:10 -0600

    Add subtitle to review page

---
+++ src/applications/edu-benefits/0839/components/CustomReviewTopContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CustomReviewTopContent = () => {
+  return (
+    <h2 className="vads-u-font-size--h3 vads-u-margin-top--0 vads-u-margin-bottom--3">
+      Review your form
+    </h2>
+  );
+};
+
+export default CustomReviewTopContent;
