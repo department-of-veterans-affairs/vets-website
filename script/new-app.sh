15355ba3f72ac42938c00861134073f00e7b0b59
Robert Hasselle <robert.hasselle@oddball.io>
2025-11-24 13:45:30 -0600

    update info about new app

--- script/new-app.sh
+++ script/new-app.sh
@@ -5,8 +5,27 @@
 # Usage interactive:
 #   yarn new:app
 #
+# or ...
 # Usage CLI:
-#   yarn new:app --force --appName="My App" ...  # CLI mode (with arguments)
+# yarn new:app \
+#   --force \
+#   --appName="[APP_NAME]" \
+#   --folderName="[FOLDER_NAME]" \
+#   --entryName="[ENTRY_NAME]" \
+#   --rootUrl="/[ROOT_URL]" \
+#   --isForm=true \
+#   --slackGroup="[SLACK_GROUP]" \
+#   --contentLoc="../vagov-content" \
+#   --formNumber="[FORM_NUMBER]" \
+#   --trackingPrefix="[TRACKING_PREFIX]" \
+#   --respondentBurden="[MINUTES]" \
+#   --ombNumber="[OMB_NUMBER]" \
+#   --expirationDate="[MM/DD/YYYY]" \
+#   --benefitDescription="[DESCRIPTION]" \
+#   --usesVetsJsonSchema=[true/false] \
+#   --usesMinimalHeader=[true/false] \
+#   --addToMyVaSip=[true/false] \
+#   --templateType="WITH_1_PAGE"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
