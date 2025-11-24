f0edf4a304df27be63a809e8975fbd84829ca324
Afia Caruso <afia.caruso@oddball.io>
2025-11-24 14:49:06 -0500

    reverted as per instruction

--- src/platform/user/authorization/components/VerifyAlert.jsx
+++ src/platform/user/authorization/components/VerifyAlert.jsx
@@ -1,46 +1,58 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { VaAlertSignIn } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
-import {
-  CSP_IDS,
-  ACTIVE_SERVICE_PROVIDERS,
-} from '~/platform/user/authentication/constants';
+
+import { CSP_IDS } from '~/platform/user/authentication/constants';
 import { signInServiceName } from '~/platform/user/authentication/selectors';
 import {
   VerifyIdmeButton,
   VerifyLogingovButton,
 } from '~/platform/user/authentication/components/VerifyButton';
 
-const BUTTON_BY_POLICY = {
-  idme: VerifyIdmeButton,
-  logingov: VerifyLogingovButton,
-};
-
-const VARIANT_BY_POLICY = {
-  idme: 'verifyIdMe',
-  logingov: 'verifyLoginGov',
-};
-
-const SLOT_BY_POLICY = {
-  idme: 'IdMeVerifyButton',
-  logingov: 'LoginGovVerifyButton',
-};
-
 export default function VerifyAlert({ headingLevel = 2, dataTestId }) {
   const csp = useSelector(signInServiceName);
 
-  if (csp === CSP_IDS.DS_LOGON || csp === CSP_IDS.MHV) return null;
+  if (csp === CSP_IDS.DS_LOGON) {
+    return null;
+  }
 
-  const provider = ACTIVE_SERVICE_PROVIDERS[csp];
-  if (!provider) return null;
+  if (csp === CSP_IDS.MHV) {
+    return (
+      <VaAlertSignIn
+        variant="signInEither"
+        visible
+        headingLevel={headingLevel}
+        data-testid={dataTestId}
+      >
+        <span slot="IdMeSignInButton">
+          <VerifyIdmeButton
+            queryParams={{ operation: 'verify_cta_authenticated' }}
+          />
+        </span>
+        <span slot="LoginGovSignInButton">
+          <VerifyLogingovButton
+            queryParams={{ operation: 'verify_cta_authenticated' }}
+          />
+        </span>
+      </VaAlertSignIn>
+    );
+  }
 
-  const { policy } = provider;
-
-  const ButtonComponent = BUTTON_BY_POLICY[policy];
-  const variant = VARIANT_BY_POLICY[policy];
-  const slot = SLOT_BY_POLICY[policy];
-
-  const queryParams = { operation: 'verify_cta_authenticated' };
+  const variant = csp === CSP_IDS.LOGIN_GOV ? 'verifyLoginGov' : 'verifyIdMe';
+  const spanSlot =
+    csp === CSP_IDS.LOGIN_GOV ? (
+      <span slot="LoginGovVerifyButton">
+        <VerifyLogingovButton
+          queryParams={{ operation: 'verify_cta_authenticated' }}
+        />
+      </span>
+    ) : (
+      <span slot="IdMeVerifyButton">
+        <VerifyIdmeButton
+          queryParams={{ operation: 'verify_cta_authenticated' }}
+        />
+      </span>
+    );
 
   return (
     <VaAlertSignIn
@@ -49,9 +61,7 @@ export default function VerifyAlert({ headingLevel = 2, dataTestId }) {
       headingLevel={headingLevel}
       data-testid={dataTestId}
     >
-      <span slot={slot}>
-        <ButtonComponent queryParams={queryParams} />
-      </span>
+      {spanSlot}
     </VaAlertSignIn>
   );
 }
