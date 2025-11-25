154a0da63c6414166d56d53eb3a5e004eefabe38
122034971+ConnorJeff@users.noreply.github.com <122034971+ConnorJeff@users.noreply.github.com>
2025-11-25 09:15:36 -0500

    MBMS-107252 resolved merged conflicts

--- src/applications/medallions/pages/applicantRelationToVetOrg2.js
+++ src/applications/medallions/pages/applicantRelationToVetOrg2.js
@@ -2,13 +2,11 @@ import {
   textUI,
   titleUI,
 } from 'platform/forms-system/src/js/web-component-patterns';
-import { MedallionsDescription } from '../components/MedallionsDescription';
 
 /** @type {PageSchema} */
 export default {
   uiSchema: {
     ...titleUI('Your organization'),
-    'ui:description': formContext => MedallionsDescription(formContext),
     yourOrganizationText: textUI({
       title:
         'Enter the name of the Veterans Service Organization (VSO) you represent',
