508b0193d3aca427114172f063169676723eda0e
Midge-dev <sean.midgley.dev@gmail.com>
2025-11-25 11:58:00 -0800

    WIP

--- src/applications/sign-in-health-portal/tests/sign-in-health-portal.cypress.spec.jsx
+++ src/applications/sign-in-health-portal/tests/sign-in-health-portal.cypress.spec.jsx
@@ -1,4 +1,21 @@
 describe('Sign In Health Portal', () => {
+  [false, true].forEach(value => {
+    beforeEach(() => {
+      cy.intercept('GET', '/v0/feature_toggles*', {
+        data: {
+          type: 'feature_toggles',
+          features: [
+            {
+              name: 'dslogon_button_disabled',
+              value,
+            },
+          ],
+        },
+      }).as('featureToggles');
+      cy.intercept('GET', '/data/cms/vamc-ehr.json', { statusCode: 200 });
+    });
+  });
+
   it('should render the portal removal notice page', () => {
     cy.visit('/sign-in-health-portal/');
     cy.get('h1').should('be.visible');
