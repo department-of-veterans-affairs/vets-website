feb851761db2ee385cb4975236915178f740ae31
Matt Long <6738544+longmd@users.noreply.github.com>
2025-11-28 12:14:58 -0500

    10-7959c | Add Part A only configs for updated PDF flow

---
+++ src/applications/ivc-champva/10-7959C/chapters/medicare/PartAEffectiveDate.js
@@ -0,0 +1,39 @@
+import {
+  titleUI,
+  currentOrPastDateUI,
+  currentOrPastDateSchema,
+} from 'platform/forms-system/src/js/web-component-patterns';
+import { nameWording, privWrapper } from '../../../shared/utilities';
+
+export default {
+  uiSchema: {
+    ...titleUI(({ formData }) =>
+      privWrapper(`${nameWording(formData)} Medicare Part A effective date`),
+    ),
+    'view:medicarePartAEffectiveDate': {
+      ...titleUI({
+        title: 'Medicare Part A',
+        headerLevel: 2,
+        headerStyleLevel: 3,
+      }),
+      applicantMedicarePartAEffectiveDate: currentOrPastDateUI({
+        title: 'Effective date',
+        hint:
+          'You may find the effective date on the front of the Medicare card near “Coverage starts” or “Effective date.”',
+        classNames: 'vads-u-margin-top--neg1p5',
+      }),
+    },
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      'view:medicarePartAEffectiveDate': {
+        type: 'object',
+        required: ['applicantMedicarePartAEffectiveDate'],
+        properties: {
+          applicantMedicarePartAEffectiveDate: currentOrPastDateSchema,
+        },
+      },
+    },
+  },
+};
