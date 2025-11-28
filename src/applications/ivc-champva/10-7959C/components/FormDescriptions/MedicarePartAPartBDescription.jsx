96b24f8d37d2329d9d6b397e2daa4ab53152e1cd
Matt Long <6738544+longmd@users.noreply.github.com>
2025-11-28 12:42:51 -0500

    10-7959c | Add Parts A & B page configs for new PDF flow

---
+++ src/applications/ivc-champva/10-7959C/components/FormDescriptions/MedicarePartAPartBDescription.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const MedicarePartADescription = (
+  <>
+    <p>
+      You’ll need to submit a copy of the beneficiary’s Original Medicare Health
+      card, sometimes called the “red, white, and blue” Medicare card.
+    </p>
+    <p>
+      <strong>Your card should include this information:</strong>
+    </p>
+    <ul>
+      <li>
+        Medicare Part A (listed as HOSPITAL), <strong>and</strong>
+      </li>
+      <li>
+        Medicare Part B (listed as MEDICAL), <strong>and</strong>
+      </li>
+      <li>The date your coverage begins</li>
+    </ul>
+  </>
+);
+
+export default MedicarePartADescription;
