d48d010948eebf36eaef4ab46a22bbb26eaf9b87
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2025-11-29 13:52:53 -0800

    0803 intro page and general config options

---
+++ src/applications/edu-benefits/0803/components/Breadcrumbs.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { VaBreadcrumbs } from '@department-of-veterans-affairs/web-components/react-bindings';
+import manifest from '../manifest.json';
+import { TITLE } from '../constants';
+
+const Breadcrumbs = () => {
+  const crumbs = [
+    {
+      href: '/',
+      label: 'Home',
+    },
+    {
+      href: '/education',
+      label: 'Education and training',
+    },
+    {
+      href: '/education/other-va-education-benefits',
+      label: 'About GI bill benefits',
+    },
+    {
+      href: '/education/other-va-education-benefits',
+      label: 'How to use your GI bill benefits',
+    },
+    {
+      href: 'education/other-va-education-benefits/vet-tec-2',
+      label: 'Licensing and certification test prep courses',
+    },
+    {
+      href: manifest.rootUrl,
+      label: TITLE,
+    },
+  ];
+  return (
+    <div className="row">
+      <div className="vads-u-margin-left--2 mobile-lg:vads-u-margin-left--1">
+        <VaBreadcrumbs
+          breadcrumbList={crumbs}
+          data-testid="breadcrumbs"
+          wrapping
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Breadcrumbs;
