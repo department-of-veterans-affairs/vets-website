d3f1be09b2bbf1c8658efd7e64c98bb94707099c
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2025-11-30 08:26:40 -0800

    introduction page for form 0976

--- src/applications/edu-benefits/0976/containers/App.jsx
+++ src/applications/edu-benefits/0976/containers/App.jsx
@@ -3,12 +3,28 @@ import PropTypes from 'prop-types';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from '../config/form';
+import Breadcrumbs from '../components/Breadcrumbs';
 
 export default function App({ location, children }) {
   return (
-    <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
-      {children}
-    </RoutedSavableApp>
+    <div className="form-22-0976-container row">
+      <div className="vads-u-padding-left--0">
+        <Breadcrumbs />
+      </div>
+      <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+        {children}
+      </RoutedSavableApp>
+      <va-need-help>
+        <div slot="content">
+          <p>
+            Call us at
+            <va-telephone contact="8008271000" />. We're here Monday through
+            Friday, 8:00 a.m. to 9:00 p.m. EST. If you have hearing loss, call{' '}
+            <va-telephone contact="711" tty />.
+          </p>
+        </div>
+      </va-need-help>
+    </div>
   );
 }
 
