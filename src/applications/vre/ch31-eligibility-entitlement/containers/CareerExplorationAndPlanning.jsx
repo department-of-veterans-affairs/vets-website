c6c74fc4909be47812bba42c9420b4178b7c2b81
Mahfuzul Haque <mahfuzul.haque1@va.gov>
2025-12-01 12:58:38 -0500

    Consolidated feature flag logic into a higher order component to avoid repetition of code.

--- src/applications/vre/ch31-eligibility-entitlement/containers/CareerExplorationAndPlanning.jsx
+++ src/applications/vre/ch31-eligibility-entitlement/containers/CareerExplorationAndPlanning.jsx
@@ -1,29 +1,9 @@
 import React from 'react';
-import { useFeatureToggle } from 'platform/utilities/feature-toggles';
 import AssessYourInterestsSection from '../components/AssessYourInterestsSection';
 import FindAPathSection from '../components/FindAPathSection';
 import FindAJobSection from '../components/FindAJobSection';
 
 export default function CareerExplorationAndPlanning() {
-  const { useToggleValue, TOGGLE_NAMES } = useFeatureToggle();
-
-  const showCareerExplorationAndPlanningPage = useToggleValue(
-    TOGGLE_NAMES.vre_eligibility_status_phase_2_updates,
-  );
-
-  if (!showCareerExplorationAndPlanningPage) {
-    return (
-      <div className="row">
-        <div className="usa-width-two-thirds vads-u-margin-top--0p5 vads-u-margin-x--1 medium-screen:vads-u-margin-x--0">
-          <h1>Career Exploration and Planning</h1>
-          <p className="vads-u-color--gray-medium">
-            This page isn't available right now.
-          </p>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="row">
       <article className="usa-width-two-thirds vads-u-margin-bottom--4 vads-u-margin-top--0p5 vads-u-margin-x--1 medium-screen:vads-u-margin-x--0 ">
