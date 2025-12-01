443201bffb39e3b2a6df081d5aeb71bc20f8ef08
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-12-01 11:29:17 -0600

    Continuing date component work

--- src/platform/forms-system/src/js/web-component-patterns/datePatterns.jsx
+++ src/platform/forms-system/src/js/web-component-patterns/datePatterns.jsx
@@ -43,8 +43,6 @@ const currentOrPastDateUI = options => {
     ...uiOptions
   } = typeof options === 'object' ? options : { title: options };
 
-  console.log('uiOptions: ', uiOptions);
-
   // if monthYearOnly is used, the schema pattern also needs
   // to be updated, so prefer to use currentOrPastMonthYearDateUI
   // and currentOrPastMonthYearDateSchema rather than passing
