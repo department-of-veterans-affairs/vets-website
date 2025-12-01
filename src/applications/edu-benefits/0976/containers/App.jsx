3a0def5e5af6ce4b0c562715edd5241123f9030c
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2025-11-30 19:38:27 -0800

    add missing space

--- src/applications/edu-benefits/0976/containers/App.jsx
+++ src/applications/edu-benefits/0976/containers/App.jsx
@@ -17,10 +17,9 @@ export default function App({ location, children }) {
       <va-need-help>
         <div slot="content">
           <p>
-            Call us at
-            <va-telephone contact="8008271000" />. We're here Monday through
-            Friday, 8:00 a.m. to 9:00 p.m. EST. If you have hearing loss, call{' '}
-            <va-telephone contact="711" tty />.
+            Call us at <va-telephone contact="8008271000" />. We're here Monday
+            through Friday, 8:00 a.m. to 9:00 p.m. EST. If you have hearing
+            loss, call <va-telephone contact="711" tty />.
           </p>
         </div>
       </va-need-help>
