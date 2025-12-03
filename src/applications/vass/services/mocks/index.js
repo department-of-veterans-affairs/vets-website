bc1e2526ade0c2b933c00667a725da4bbda8ed1a
Chad Laflamme <216556326+sixercjl@users.noreply.github.com>
2025-12-03 14:45:27 -0500

    update mock data for multiple local test users

--- src/applications/vass/services/mocks/index.js
+++ src/applications/vass/services/mocks/index.js
@@ -2,16 +2,19 @@
 /* eslint-disable camelcase */
 const delay = require('mocker-api/lib/delay');
 
-const mockUser = {
-  uuid: 'c0ffee-1234-beef-5678',
-  lastname: 'Smith',
-  dob: '1935-04-07',
-  otc: '123456',
-};
+const mockUsers = [
+  {
+    uuid: 'c0ffee-1234-beef-5678',
+    lastname: 'Smith',
+    dob: '1935-04-07',
+    otc: '123456',
+  },
+];
 
 const responses = {
   'POST /vass/v0/authenticate': (req, res) => {
-    const { lastname, dob } = req.body;
+    const { uuid, lastname, dob } = req.body;
+    const mockUser = mockUsers.find(user => user.uuid === uuid);
     if (lastname === mockUser.lastname && dob === mockUser.dob) {
       return res.json({
         data: {
@@ -30,7 +33,8 @@ const responses = {
     });
   },
   'POST /vass/v0/authenticate-otc': (req, res) => {
-    const { otc, lastname, dob } = req.body;
+    const { otc, uuid, lastname, dob } = req.body;
+    const mockUser = mockUsers.find(user => user.uuid === uuid);
     if (
       otc === mockUser.otc &&
       lastname === mockUser.lastname &&
