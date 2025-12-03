ae057ca1a1bb60f63b841ddf14d9666094a8fff0
Chad Laflamme <216556326+sixercjl@users.noreply.github.com>
2025-12-03 14:14:16 -0500

    update paylods and mock responses

--- src/applications/vass/services/mocks/index.js
+++ src/applications/vass/services/mocks/index.js
@@ -2,26 +2,57 @@
 /* eslint-disable camelcase */
 const delay = require('mocker-api/lib/delay');
 
+const mockUser = {
+  uuid: 'c0ffee-1234-beef-5678',
+  lastname: 'Smith',
+  dob: '1935-04-07',
+  otc: '123456',
+};
+
 const responses = {
-  'POST /vass/v0/authenticate': {
-    data: {
-      id: '123',
-      type: 'authenticate',
-      todo: 'TODO: rebuild with real mocked data',
-      attributes: {
-        token: '123',
-      },
-    },
+  'POST /vass/v0/authenticate': (req, res) => {
+    const { lastname, dob } = req.body;
+    if (lastname === mockUser.lastname && dob === mockUser.dob) {
+      return res.json({
+        data: {
+          message: 'OTC sent to registered email address',
+          expiresIn: 600,
+        },
+      });
+    }
+    return res.json({
+      errors: [
+        {
+          code: 'invalid_credentials',
+          detail: 'Unable to verify identity. Please check your information.',
+        },
+      ],
+    });
   },
-  'POST /vass/v0/TODO_GET_OTC_ENDPOINT': {
-    data: {
-      id: '123',
-      type: 'otc',
-      todo: 'TODO: rebuild with real mocked data',
-      attributes: {
-        otc: '123',
-      },
-    },
+  'POST /vass/v0/authenticate-otc': (req, res) => {
+    const { otc, lastname, dob } = req.body;
+    if (
+      otc === mockUser.otc &&
+      lastname === mockUser.lastname &&
+      dob === mockUser.dob
+    ) {
+      return res.json({
+        data: {
+          token: '<JWT token string>',
+          expiresIn: 3600,
+          tokenType: 'Bearer',
+        },
+      });
+    }
+    return res.json({
+      errors: [
+        {
+          code: 'invalid_otc',
+          detail: 'Invalid or expired OTC.  Please try again.',
+          attemptsRemaining: 3,
+        },
+      ],
+    });
   },
 };
 
