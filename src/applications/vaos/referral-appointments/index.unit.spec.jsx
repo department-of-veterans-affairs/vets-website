87663dd87d588b308695a9ecdea2420729a798e6
Bill Tran <bill@billjohntran.com>
2025-12-02 16:32:46 -0800

    remove files and remove unneccesary tests

--- src/applications/vaos/referral-appointments/index.unit.spec.jsx
+++ src/applications/vaos/referral-appointments/index.unit.spec.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import { expect } from 'chai';
 import sinon from 'sinon';
 
-import { waitFor } from '@testing-library/react';
 import ReferralAppointments from './index';
 import { renderWithStoreAndRouter } from '../tests/mocks/setup';
-import { FETCH_STATUS } from '../utils/constants';
-import { createReferralById } from './utils/referrals';
-import MockReferralListResponse from '../tests/fixtures/MockReferralListResponse';
 import * as useManualScrollRestoration from '../hooks/useManualScrollRestoration';
 import * as useIsInPilotUserStations from './hooks/useIsInPilotUserStations';
-import * as vaosApi from '../redux/api/vaosApi';
 
-const initialStateVAOSService = {
+const initialState = {
   featureToggles: {
     vaOnlineSchedulingCancel: true,
     vaOnlineSchedulingCCDirectScheduling: true,
@@ -21,135 +16,41 @@ const initialStateVAOSService = {
 
 describe('ReferralAppointments', () => {
   const sandbox = sinon.createSandbox();
-  let useGetReferralByIdQueryStub;
+  let useIsInPilotUserStationsStub;
 
   beforeEach(() => {
-    useGetReferralByIdQueryStub = sandbox
-      .stub(vaosApi, 'useGetReferralByIdQuery')
-      .returns({
-        data: createReferralById(
-          '2024-11-29',
-          'add2f0f4-a1ea-4dea-a504-a54ab57c6801',
-        ),
-        error: null,
-        isLoading: false,
-      });
     sandbox.stub(useManualScrollRestoration, 'default');
-    sandbox.stub(useIsInPilotUserStations, 'useIsInPilotUserStations').returns({
-      isInPilotUserStations: true,
-    });
+    useIsInPilotUserStationsStub = sandbox
+      .stub(useIsInPilotUserStations, 'useIsInPilotUserStations')
+      .returns({
+        isInPilotUserStations: true,
+      });
   });
 
   afterEach(() => {
     sandbox.restore();
   });
 
-  it('should render loading layout when referral is loading', async () => {
-    useGetReferralByIdQueryStub.returns({
-      data: null,
-      error: null,
-      isLoading: true,
+  it('should redirect to home when user is not in pilot stations', () => {
+    useIsInPilotUserStationsStub.returns({
+      isInPilotUserStations: false,
     });
-    const initialState = {
-      ...initialStateVAOSService,
-      referral: {
-        referralsFetchStatus: FETCH_STATUS.loading,
-      },
-    };
 
     const screen = renderWithStoreAndRouter(<ReferralAppointments />, {
       initialState,
-      path: '/?id=445e2d1b-7150-4631-97f2-f6f473bdef00',
+      path: '/schedule-referral',
     });
 
-    expect(screen.getByTestId('loading-container')).to.exist;
+    expect(screen.history.location.pathname).to.equal('/');
   });
 
-  it('should render error layout when there is an error fetching referral', async () => {
-    useGetReferralByIdQueryStub.returns({
-      data: null,
-      error: true,
-      isLoading: false,
-    });
-    const initialState = {
-      ...initialStateVAOSService,
-      referral: {
-        referralsFetchStatus: FETCH_STATUS.succeeded,
-      },
-    };
-
+  it('should render routes when user is in pilot stations', () => {
     const screen = renderWithStoreAndRouter(<ReferralAppointments />, {
       initialState,
-      path: '/?id=445e2d1b-7150-4631-97f2-f6f473bdef00',
+      path: '/schedule-referral',
     });
 
-    await waitFor(() => {
-      expect(screen.getByText('We’re sorry. We’ve run into a problem.')).to
-        .exist;
-    });
-
-    // Check the back link
-    expect(screen.getByTestId('back-link')).to.have.attribute(
-      'text',
-      'Back to appointments',
-    );
-    expect(screen.getByTestId('referral-layout-heading')).to.have.text(
-      'Something went wrong on our end',
-    );
-    expect(screen.getByTestId('referral-community-care-office')).to.exist;
-  });
-
-  it('should render ScheduleReferral for the base path', async () => {
-    const initialState = {
-      ...initialStateVAOSService,
-      referral: {
-        referralsFetchStatus: FETCH_STATUS.succeeded,
-      },
-    };
-
-    const screen = renderWithStoreAndRouter(<ReferralAppointments />, {
-      initialState,
-      path: '/?id=add2f0f4-a1ea-4dea-a504-a54ab57c6801',
-    });
-
-    await waitFor(() => {
-      // Looking for elements that would be in ScheduleReferral component
-      expect(screen.getByTestId('schedule-appointment-button')).to.exist;
-    });
-  });
-
-  it('should redirect to referrals-requests page if referral has appointments', async () => {
-    const referralWithAppointments = createReferralById(
-      '2024-11-29',
-      'add2f0f4-a1ea-4dea-a504-a54ab57c6801',
-    );
-    referralWithAppointments.attributes.hasAppointments = true;
-
-    useGetReferralByIdQueryStub.returns({
-      data: referralWithAppointments,
-      error: null,
-      isLoading: false,
-    });
-
-    const referralsResponse = new MockReferralListResponse({
-      numberOfReferrals: 3,
-    }).toJSON();
-
-    const initialState = {
-      ...initialStateVAOSService,
-      referral: {
-        referrals: referralsResponse.data,
-        referralsFetchStatus: FETCH_STATUS.succeeded,
-      },
-    };
-
-    const screen = renderWithStoreAndRouter(<ReferralAppointments />, {
-      initialState,
-      path: '/date-time?id=add2f0f4-a1ea-4dea-a504-a54ab57c6801',
-    });
-
-    await waitFor(() => {
-      expect(screen.history.location.pathname).to.equal('/referrals-requests');
-    });
+    // Should not redirect when isInPilotUserStations is true
+    expect(screen.history.location.pathname).to.equal('/schedule-referral');
   });
 });
