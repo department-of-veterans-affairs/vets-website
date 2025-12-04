e88dd37f54ce298a52cb9c51e3aeb3d762586753
Mitch Saltykov <mitch.saltykov@agile6.com>
2025-12-04 16:10:17 -0500

    SPIKE - add dateOfMarriage line to summary

--- src/applications/ezr/components/FormDescriptions/SpouseSummaryCardDescription.jsx
+++ src/applications/ezr/components/FormDescriptions/SpouseSummaryCardDescription.jsx
@@ -4,7 +4,16 @@ import { useSelector } from 'react-redux';
 const SpouseSummaryCardDescription = item => {
   const { data: formData } = useSelector(state => state.form);
   const { maritalStatus } = formData['view:maritalStatus'];
-  return item && <p className="vads-u-margin-bottom--0">{maritalStatus}</p>;
+  const { dateOfMarriage } = formData;
+  return (
+    item && (
+      <p className="vads-u-margin-bottom--0">
+        {maritalStatus}
+        <br />
+        Date of Marriage: {dateOfMarriage}
+      </p>
+    )
+  );
 };
 
 export default SpouseSummaryCardDescription;
