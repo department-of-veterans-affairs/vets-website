73ee44f6e716914036eda604dba4abb92e5947ca
dustin-cintron <D.Cint88@gmail.com>
2025-12-04 11:16:57 -0500

    Created pages directory for personalInfo and contactInfo prefill/profile components

---
+++ src/applications/simple-forms/mock-form-prefill/pages/personalInformation.js
@@ -0,0 +1,7 @@
+import { profilePersonalInfoPage } from 'platform/forms-system/src/js/patterns/prefill';
+
+const personalInformation = {
+  ...profilePersonalInfoPage(),
+};
+
+export default personalInformation;
