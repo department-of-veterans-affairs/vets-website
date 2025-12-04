fd1749171830269b8d1753f444d9766104c85f17
Giles Wells <giles@mediagearhead.com>
2025-12-04 09:55:12 -0500

    Simplify remove modals

--- src/platform/user/profile/vap-svc/components/ContactInformationFieldInfo/ConfirmRemoveModal.jsx
+++ src/platform/user/profile/vap-svc/components/ContactInformationFieldInfo/ConfirmRemoveModal.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { VaModal } from '@department-of-veterans-affairs/web-components/react-bindings';
-import LoadingButton from '~/platform/site-wide/loading-button/LoadingButton';
 import { FIELD_NAMES } from '~/platform/user/profile/vap-svc/constants';
 
 const ConfirmRemoveModal = ({
@@ -10,109 +9,71 @@ const ConfirmRemoveModal = ({
   title,
   fieldName,
   isEnrolledInVAHealthCare,
-  isLoading,
   isVisible,
   onHide,
 }) => {
   if (!isVisible) {
     return null;
   }
-  return (
+  let modalContent = (
     <>
-      {fieldName === FIELD_NAMES.MESSAGING_SIGNATURE ? (
-        <VaModal
-          modalTitle="Remove signature?"
-          className="overflow-auto"
-          status="warning"
-          visible={isVisible}
-          onCloseEvent={onHide}
-          uswds
-        >
-          <p className="vads-u-margin-top--1">
-            Your signature will no longer appear on outgoing secure messages.
-          </p>
-          <p className="vads-u-margin-top--1">
-            You can always come back to your profile later if you want to add
-            this signature again.
-          </p>
-          <div>
-            <LoadingButton
-              isLoading={isLoading}
-              onClick={deleteAction}
-              aria-label="Yes, remove my signature"
-              loadingText="Removing your signature"
-            >
-              Yes, remove my signature
-            </LoadingButton>
-
-            {!isLoading && (
-              <va-button
-                secondary
-                onClick={cancelAction}
-                text="No, cancel this change"
-              />
-            )}
-          </div>
-        </VaModal>
-      ) : (
-        <VaModal
-          modalTitle={`Remove your ${title.toLowerCase()}?`}
-          className="overflow-auto"
-          status="warning"
-          visible={isVisible}
-          onCloseEvent={onHide}
-          uswds
-        >
-          <div>
-            This will remove your {title.toLowerCase()} across these VA benefits
-            and services:
-          </div>
-          <ul>
-            {isEnrolledInVAHealthCare && (
-              <li>
-                VA health care (including prescriptions, appointment reminders,
-                lab and test results, and communications from your VA medical
-                center)
-              </li>
-            )}
-            <li>Disability compensation</li>
-            <li>Pension benefits</li>
-            <li>Claims and appeals</li>
-            <li>Veteran Readiness and Employment (VR&E)</li>
-            {(fieldName === FIELD_NAMES.EMAIL ||
-              fieldName === FIELD_NAMES.MOBILE_PHONE) && (
-              <li>
-                Some VA notifications. This means you’ll stop getting any VA{' '}
-                {fieldName === FIELD_NAMES.EMAIL ? 'email' : 'text'}{' '}
-                notifications you signed up for.
-              </li>
-            )}
-          </ul>
-          <p className="vads-u-margin-top--1">
-            {`You can always add another ${title.toLowerCase()} any time.`}
-          </p>
-          <div>
-            <va-button
-              loading={isLoading}
-              onClick={deleteAction}
-              text={isLoading ? '' : 'Remove'}
-              class="vads-u-margin-top--1 vads-u-margin-bottom--1 vads-u-width--full mobile-lg:vads-u-width--auto"
-              data-testid="confirm-remove-button"
-            />
-
-            {!isLoading && (
-              <va-button
-                secondary
-                onClick={cancelAction}
-                text="Cancel change"
-                class="vads-u-margin-top--1 vads-u-width--full mobile-lg:vads-u-width--auto"
-                data-testid="cancel-remove-button"
-              />
-            )}
-          </div>
-        </VaModal>
-      )}
+      <div>
+        This will remove your {title.toLowerCase()} across these VA benefits and
+        services:
+      </div>
+      <ul>
+        {isEnrolledInVAHealthCare && (
+          <li>
+            VA health care (including prescriptions, appointment reminders, lab
+            and test results, and communications from your VA medical center)
+          </li>
+        )}
+        <li>Disability compensation</li>
+        <li>Pension benefits</li>
+        <li>Claims and appeals</li>
+        <li>Veteran Readiness and Employment (VR&E)</li>
+        {(fieldName === FIELD_NAMES.EMAIL ||
+          fieldName === FIELD_NAMES.MOBILE_PHONE) && (
+          <li>
+            Some VA notifications. This means you’ll stop getting any VA{' '}
+            {fieldName === FIELD_NAMES.EMAIL ? 'email' : 'text'} notifications
+            you signed up for.
+          </li>
+        )}
+      </ul>
+      <p className="vads-u-margin-top--1">
+        {`You can always add another ${title.toLowerCase()} any time.`}
+      </p>
     </>
+  );
+  if (fieldName === FIELD_NAMES.MESSAGING_SIGNATURE) {
+    modalContent = (
+      <>
+        <p className="vads-u-margin-top--1">
+          Your signature will no longer appear on outgoing secure messages.
+        </p>
+        <p className="vads-u-margin-top--1">
+          You can always come back to your profile later if you want to add this
+          signature again.
+        </p>
+      </>
+    );
+  }
+  return (
+    <VaModal
+      modalTitle={`Remove your ${title.toLowerCase()}?`}
+      className="overflow-auto"
+      status="warning"
+      visible={isVisible}
+      onCloseEvent={onHide}
+      primaryButtonText="Remove"
+      secondaryButtonText="Cancel change"
+      onPrimaryButtonClick={deleteAction}
+      onSecondaryButtonClick={cancelAction}
+      uswds
+    >
+      {modalContent}
+    </VaModal>
   );
 };
 
