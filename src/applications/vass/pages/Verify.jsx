52b08044c5dc9b854090a83b9c9fbbf002565cc9
Juan Castillo <juan.cast@agile6.com>
2025-12-04 11:17:07 -0800

    Add record not found error state

--- src/applications/vass/pages/Verify.jsx
+++ src/applications/vass/pages/Verify.jsx
@@ -1,37 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom-v5-compat';
 import { VaMemorableDate } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import Wrapper from '../layout/Wrapper';
 
+// TODO: remove this and use mock data from the API
+const mockUser = {
+  lastname: 'Smith',
+  dob: '1935-04-07',
+  uuid: 'c0ffee-1234-beef-5678',
+};
+
 const Verify = () => {
   const navigate = useNavigate();
 
+  const [lastname, setLastname] = useState('');
+  const [dob, setDob] = useState('');
+
+  const [error, setError] = useState(false);
+  const [lastnameError, setLastnameError] = useState(undefined);
+  const [dobError, setDobError] = useState(undefined);
+
   const handleSubmit = () => {
-    // confirm auth here
-    navigate('/enter-otc');
+    if (lastname === '' || dob === '') {
+      if (lastname === '') {
+        setLastnameError(true);
+      }
+      if (dob === '') {
+        setDobError(true);
+      }
+      return;
+    }
+    // confirm auth here and validate fields aren't empty
+    if (lastname === mockUser.lastname && dob === mockUser.dob) {
+      setError(false);
+      navigate('/enter-otc');
+    } else {
+      setError(true);
+    }
   };
+
   return (
     <Wrapper pageTitle="Schedule a call to learn about VA benefits and health care">
       <p data-testid="verify-intro-text">
         First, we’ll need your information so we can send you a one-time
         verification code to verify your identity.
       </p>
+      {error && (
+        <div className="vads-u-margin-bottom--2">
+          <va-alert data-testid="verify-error-alert" status="error">
+            We’re sorry. We couldn’t find a record that matches that last name
+            or date of birth. Please try again.
+          </va-alert>
+        </div>
+      )}
       <va-text-input
         data-testid="last-name-input"
         label="Your last name"
         name="last-name"
-        onBlur={() => {}}
-        onInput={() => {}}
+        onBlur={e => {
+          // Clear the error if the user has entered a value
+          if (e.target.value !== '') {
+            setLastnameError(undefined);
+          }
+        }}
+        onInput={e => {
+          setLastname(e.target.value);
+        }}
         required
+        error={lastnameError}
         show-input-error
       />
       <VaMemorableDate
         id="dob-input"
         data-testid="dob-input"
         label="Date of birth"
-        onDateBlur={() => {}}
-        onDateChange={() => {}}
+        onDateBlur={e => {
+          // Clear the error if the user has entered a value
+          if (e.target.value !== '') {
+            setDobError(undefined);
+          }
+        }}
+        onDateChange={e => {
+          setDob(e.target.value);
+        }}
         name="date-of-birth"
+        error={dobError}
         required
         onKeyDown={() => {}}
         monthSelect={false}
