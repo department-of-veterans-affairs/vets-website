ee937fd20196e1dbdeb9acdfc9b30ad607f2cd93
Brad Bergeron <bergeron.bradley@gmail.com>
2025-12-05 12:45:21 -0500

    Updates unit tests for introduction login

--- src/applications/my-education-benefits/tests/units/components/IntroductionLogin.unit.spec.jsx
+++ src/applications/my-education-benefits/tests/units/components/IntroductionLogin.unit.spec.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
+import SaveInProgressIntro from 'platform/forms/save-in-progress/SaveInProgressIntro';
 import { IntroductionLogin } from '../../../components/IntroductionLogin';
+import LoadingIndicator from '../../../components/LoadingIndicator';
 
 describe('IntroductionLogin', () => {
   const baseProps = {
@@ -54,6 +56,57 @@ describe('IntroductionLogin', () => {
 
     expect(alertSignIn).to.have.lengthOf(1);
     expect(alertSignIn.prop('variant')).to.equal('signInOptional');
+    wrapper.unmount();
+  });
+
+  it('never shows the loading indicator', () => {
+    const wrapper = shallow(<IntroductionLogin {...baseProps} />);
+    const loadingIndicator = wrapper.find(LoadingIndicator);
+
+    expect(loadingIndicator).to.have.lengthOf(0);
+    wrapper.unmount();
+  });
+
+  it("displays 'Sign in or create an account' on the sign-in button when meb1995Reroute is true", () => {
+    const wrapper = shallow(
+      <IntroductionLogin {...baseProps} meb1995Reroute />,
+    );
+    const button = wrapper.find('va-button');
+
+    expect(button).to.have.lengthOf(1);
+    expect(button.prop('text')).to.equal('Sign in or create an account');
+    wrapper.unmount();
+  });
+
+  it('displays the default message on the sign-in button when meb1995Reroute is false', () => {
+    const wrapper = shallow(
+      <IntroductionLogin {...baseProps} meb1995Reroute={false} />,
+    );
+    const button = wrapper.find('va-button');
+
+    expect(button).to.have.lengthOf(1);
+    expect(button.prop('text')).to.equal('Sign in to start your application');
+    wrapper.unmount();
+  });
+
+  it('renders SaveInProgressIntro when isLoggedIn and isLOA3 are true and isPersonalInfoFetchFailed and shouldShowMaintenanceAlert are false', () => {
+    const wrapper = shallow(
+      <IntroductionLogin
+        {...baseProps}
+        isLoggedIn
+        isLOA3
+        isPersonalInfoFetchFailed={false}
+        showMeb1990EZMaintenanceAlert={false}
+      />,
+    );
+    const saveInProgressIntro = wrapper.find(SaveInProgressIntro);
+
+    expect(saveInProgressIntro).to.have.lengthOf(1);
+    expect(saveInProgressIntro.prop('headingLevel')).to.equal(2);
+    expect(saveInProgressIntro.prop('hideUnauthedStartLink')).to.be.true;
+    expect(saveInProgressIntro.prop('startText')).to.equal(
+      'Start your benefits application',
+    );
     wrapper.unmount();
   });
 });
