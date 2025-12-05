26b531a8d26723251028393fd7b68cfb83a11560
James Taggart <jtaggart@kindsys.us>
2025-12-05 15:57:07 -0800

    Added different flags for logging and RUM.

--- src/applications/letters/containers/App.jsx
+++ src/applications/letters/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Outlet } from 'react-router-dom-v5-compat';
 import { connect } from 'react-redux';
@@ -7,30 +7,68 @@ import backendServices from '@department-of-veterans-affairs/platform-user/profi
 import { RequiredLoginView } from '@department-of-veterans-affairs/platform-user/RequiredLoginView';
 import { externalServices } from '@department-of-veterans-affairs/platform-monitoring/DowntimeNotification';
 import { DowntimeBanner } from '@department-of-veterans-affairs/platform-monitoring/exports';
-import { useBrowserMonitoring } from 'platform/monitoring/Datadog';
 import { useFeatureToggle } from 'platform/utilities/feature-toggles';
+import {
+  initializeBrowserLogging,
+  initializeRealUserMonitoring,
+} from 'platform/monitoring/Datadog';
 
 import AppContent from '../components/AppContent';
 import { isLoadingFeatures } from '../selectors';
 
-export function App({ featureFlagsLoading, user, loggedIn }) {
-  const { useToggleValue, TOGGLE_NAMES } = useFeatureToggle();
-  const toggleValue = useToggleValue(TOGGLE_NAMES.lettersClientSideMonitoring);
+const DATA_DOG_LOGGING_TOGGLE = 'lettersClientSideMonitoring';
+const DATA_DOG_LOGGING_ID = '{APP_UUID}';
+const DATA_DOG_LOGGING_SERVICE = '{APP_DASHBOARD_NAME}';
+const DATA_DOG_LOGGING_TOKEN = 'pub{TOKEN_ID}';
+const DATA_DOG_LOGGING_VERSION = '1.0.0';
 
-  // Need to figure out how to conditionally RUM dashboard, get error on eslint, the built in filtering
-  // might not be enough, but we need this enabled all the time for logs. So many two monitors? One for
-  // logs and one for RUM?
-  useBrowserMonitoring({
-    loggedIn,
-    version: '1.0.0',
-    applicationId: '',
-    clientToken: '',
-    service: 'benefits-letters-and-documents-tool',
-  });
+const DATA_DOG_RUM_TOGGLE = 'lettersRumDashboard';
+const DATA_DOG_RUM_ID = '{APP_UUID}';
+const DATA_DOG_RUM_SERVICE = '{APP_DASHBOARD_NAME}';
+const DATA_DOG_RUM_TOKEN = 'pub{TOKEN_ID}';
+const DATA_DOG_RUM_VERSION = '1.0.0';
 
-  if (toggleValue) {
-    // This is temporary, just testing feature flag.
-  }
+export function App({ featureFlagsLoading, user }) {
+  const {
+    TOGGLE_NAMES,
+    useToggleValue,
+    useToggleLoadingValue,
+  } = useFeatureToggle();
+  const isLoadingFeatureFlags = useToggleLoadingValue();
+  const isLoggingEnabled = useToggleValue(
+    TOGGLE_NAMES[DATA_DOG_LOGGING_TOGGLE],
+  );
+  const isRUMEnabled = useToggleValue(TOGGLE_NAMES[DATA_DOG_RUM_TOGGLE]);
+
+  useEffect(() => {
+    if (isLoadingFeatureFlags) {
+      return;
+    }
+    if (isLoggingEnabled) {
+      initializeBrowserLogging({
+        applicationId: DATA_DOG_LOGGING_ID,
+        clientToken: DATA_DOG_LOGGING_TOKEN,
+        service: DATA_DOG_LOGGING_SERVICE,
+        version: DATA_DOG_LOGGING_VERSION,
+      });
+    }
+  }, [isLoggingEnabled, isLoadingFeatureFlags]);
+
+  useEffect(() => {
+    if (isLoadingFeatureFlags) {
+      return;
+    }
+    if (isRUMEnabled) {
+      initializeRealUserMonitoring({
+        applicationId: DATA_DOG_RUM_ID,
+        clientToken: DATA_DOG_RUM_TOKEN,
+        service: DATA_DOG_RUM_SERVICE,
+        version: DATA_DOG_RUM_VERSION,
+      });
+    } else {
+      delete window.DD_RUM;
+    }
+  }, [isRUMEnabled, isLoadingFeatureFlags]);
 
   return (
     <RequiredLoginView
