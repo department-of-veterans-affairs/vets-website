4e53f6d73df82b3a043541e78c9d498a80c70cb1
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2025-12-05 10:06:38 -0800

    0803 page 3: test information

---
+++ src/applications/edu-benefits/0803/pages/TestCost.js
@@ -0,0 +1,62 @@
+// @ts-check
+import React from 'react';
+import {
+  currencyUI,
+  currencySchema,
+  titleUI,
+} from 'platform/forms-system/src/js/web-component-patterns';
+
+const uiSchema = {
+  ...titleUI('Test cost'),
+  'view:explanation': {
+    'ui:description': (
+      <div>
+        <p>
+          Enter the cost of the test you took, including any required fees. (We
+          can only reimburse you for required test fees.) We have no authority
+          to reimburse you for any optional costs related to the test process.
+        </p>
+        <p>
+          Test fees that VA will reimburse include "registration fees," fees for
+          specialized tests, and administrative fees such as a proctoring fee.
+        </p>
+        <p>
+          Fees that VA has no authority to reimburse include fees to take
+          pre-tests, fees to receive scores quickly, or other costs or fees for
+          optional items that are not required to take an approved test.
+        </p>
+      </div>
+    ),
+  },
+  testCost: {
+    ...currencyUI({
+      title: 'Total test cost',
+      min: 0,
+      required: () => true,
+      errorMessages: {
+        required: 'Enter the total cost of the test',
+      },
+    }),
+  },
+  'view:understandsUploadRequirement2': {
+    'ui:description': (
+      <va-alert tabindex="0">
+        When you submit this form on QuickSubmit or by mail, you will need to
+        attach a copy of your test results to your submission. If you do not
+        have any test results but have a copy of your license or certification
+        that clearly displays your name, you can attach that.
+      </va-alert>
+    ),
+  },
+};
+const schema = {
+  type: 'object',
+  properties: {
+    'view:explanation': { type: 'object', properties: {} },
+    testCost: currencySchema,
+    'view:understandsUploadRequirement2': { type: 'object', properties: {} },
+  },
+  required: ['testCost'],
+};
+
+export { schema, uiSchema };
