1bf454f35873026c9f7100f295346372483cb068
Tommasina Miller <144388524+tommasina-va@users.noreply.github.com>
2025-12-08 16:29:16 -0600

    WIP address wrapper

--- src/applications/disability-benefits/all-claims/pages/contactInformation.js
+++ src/applications/disability-benefits/all-claims/pages/contactInformation.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import merge from 'lodash/merge';
-import omit from 'platform/utilities/data/omit';
+// import React from 'react';
+// import merge from 'lodash/merge';
+// import omit from 'platform/utilities/data/omit';
+// import {
+//   addressSchema,
+//   addressUI,
+//   updateFormDataAddress,
+// } from 'platform/forms-system/src/js/web-component-patterns';
+import {
+  mappedAddressUI,
+  mappedAddressSchema,
+  updateMappedFormDataAddress,
+} from 'platform/forms-system/src/js/web-component-patterns/addressPattern';
+
 import fullSchema from 'vets-json-schema/dist/21-526EZ-ALLCLAIMS-schema.json';
 import phoneUI from 'platform/forms-system/src/js/definitions/phone';
 import emailUI from 'platform/forms-system/src/js/definitions/email';
@@ -13,62 +24,76 @@ import {
   phoneEmailViewField,
 } from '../content/contactInformation';
 
-import { addressUISchema } from '../utils/schemas';
+// import { addressUISchema } from '../utils/schemas';
 
-import {
-  ADDRESS_PATHS,
-  USA,
-  MILITARY_STATE_LABELS,
-  MILITARY_STATE_VALUES,
-  MILITARY_CITIES,
-  STATE_LABELS,
-  STATE_VALUES,
-} from '../constants';
+// import {
+//   ADDRESS_PATHS,
+//   USA,
+//   MILITARY_STATE_LABELS,
+//   MILITARY_STATE_VALUES,
+//   MILITARY_CITIES,
+//   STATE_LABELS,
+//   STATE_VALUES,
+// } from '../constants';
 
-import {
-  validateMilitaryCity,
-  validateMilitaryState,
-  validateZIP,
-} from '../validations';
+// import {
+//   validateMilitaryCity,
+//   validateMilitaryState,
+//   validateZIP,
+// } from '../validations';
 
 const {
   // forwardingAddress,
   phoneAndEmail,
 } = fullSchema.properties;
 
-const mailingAddress = merge(
-  {
-    properties: {
-      'view:livesOnMilitaryBase': {
-        type: 'boolean',
-      },
-      'view:livesOnMilitaryBaseInfo': {
-        type: 'object',
-        properties: {},
-      },
-    },
-  },
-  fullSchema.definitions.address,
-);
+// const mailingAddress = merge(
+//   {
+//     properties: {
+//       'view:livesOnMilitaryBase': {
+//         type: 'boolean',
+//       },
+//       'view:livesOnMilitaryBaseInfo': {
+//         type: 'object',
+//         properties: {},
+//       },
+//     },
+//   },
+//   fullSchema.definitions.address,
+// );
 
-const countryEnum = fullSchema.definitions.country.enum;
-const citySchema = fullSchema.definitions.address.properties.city;
+// const countryEnum = fullSchema.definitions.country.enum;
+// const citySchema = fullSchema.definitions.address.properties.city;
 
 /**
  * Return state of mailing address military base checkbox
  * @param {object} data - Complete form data
  * @returns {boolean} - military base checkbox state
  */
-const getMilitaryValue = data =>
-  data.mailingAddress?.['view:livesOnMilitaryBase'];
+// const getMilitaryValue = data =>
+//   data.mailingAddress?.['view:livesOnMilitaryBase'];
 
-// Temporary storage for city & state if military base checkbox is toggled more
-// than once
-const savedAddress = {
-  city: '',
-  state: '',
+// // Temporary storage for city & state if military base checkbox is toggled more
+// // than once
+// const savedAddress = {
+//   city: '',
+//   state: '',
+// };
+
+export const updateFormData = (oldFormData, formData) => {
+  return updateMappedFormDataAddress(
+    oldFormData,
+    formData,
+    ['mailingAddress'],
+    null,
+    {
+      street: 'addressLine1',
+      street2: 'addressLine2',
+      street3: 'addressLine3',
+      postalCode: 'zipCode',
+    },
+  );
 };
-
 /**
  * Update form data to remove selected military city & state and restore any
  * previously set city & state when the "I live on a U.S. military base"
@@ -78,33 +103,35 @@ const savedAddress = {
  * @returns {object} - updated Form data with manipulated mailing address if the
  * military base checkbox state changes
  */
-export const updateFormData = (oldFormData, formData) => {
-  let { city, state } = formData.mailingAddress;
-  const onMilitaryBase = getMilitaryValue(formData);
-  if (getMilitaryValue(oldFormData) !== onMilitaryBase) {
-    if (onMilitaryBase) {
-      savedAddress.city = oldFormData.mailingAddress.city || '';
-      savedAddress.state = oldFormData.mailingAddress.state || '';
-      city = '';
-      state = '';
-    } else {
-      city = MILITARY_CITIES.includes(oldFormData.mailingAddress.city)
-        ? savedAddress.city
-        : city || savedAddress.city;
-      state = MILITARY_STATE_VALUES.includes(oldFormData.mailingAddress.state)
-        ? savedAddress.state
-        : state || savedAddress.state;
-    }
-  }
-  return {
-    ...formData,
-    mailingAddress: {
-      ...formData.mailingAddress,
-      city,
-      state,
-    },
-  };
-};
+// export const updateFormData = (oldFormData, formData) => {
+//   updateFormDataAddress(oldFormData, formData, ['mailingAddress']);
+//   return formData;
+// let { city, state } = formData.mailingAddress;
+// const onMilitaryBase = getMilitaryValue(formData);
+// if (getMilitaryValue(oldFormData) !== onMilitaryBase) {
+//   if (onMilitaryBase) {
+//     savedAddress.city = oldFormData.mailingAddress.city || '';
+//     savedAddress.state = oldFormData.mailingAddress.state || '';
+//     city = '';
+//     state = '';
+//   } else {
+//     city = MILITARY_CITIES.includes(oldFormData.mailingAddress.city)
+//       ? savedAddress.city
+//       : city || savedAddress.city;
+//     state = MILITARY_STATE_VALUES.includes(oldFormData.mailingAddress.state)
+//       ? savedAddress.state
+//       : state || savedAddress.state;
+//   }
+// }
+// return {
+//   ...formData,
+//   mailingAddress: {
+//     ...formData.mailingAddress,
+//     city,
+//     state,
+//   },
+// };
+// };
 
 export const uiSchema = {
   'ui:title': 'Contact information',
@@ -118,161 +145,172 @@ export const uiSchema = {
     primaryPhone: phoneUI('Phone number'),
     emailAddress: emailUI(),
   },
-  mailingAddress: {
-    ...omit(
-      ['ui:order'],
-      addressUISchema(ADDRESS_PATHS.mailingAddress, 'Mailing address', true),
-    ),
-    'view:livesOnMilitaryBase': {
-      'ui:title':
-        'I live on a United States military base outside of the United States',
+  mailingAddress: mappedAddressUI({
+    keyMap: {
+      street: 'addressLine1',
+      street2: 'addressLine2',
+      street3: 'addressLine3',
+      postalCode: 'zipCode',
     },
-    'view:livesOnMilitaryBaseInfo': {
-      'ui:description': () => (
-        <div className="vads-u-padding-x--2p5">
-          <va-additional-info trigger="Learn more about military base addresses">
-            <span>
-              The United States is automatically chosen as your country if you
-              live on a military base outside of the country.
-            </span>
-          </va-additional-info>
-        </div>
-      ),
-    },
-    country: {
-      'ui:title': 'Country',
-      'ui:autocomplete': 'country',
-      'ui:options': {
-        updateSchema: (formData, schema, uiSchemaCountry) => {
-          const uiSchemaDisabled = uiSchemaCountry;
+  }),
+  // mailingAddress: {
+  //   ...addressUI(),
+  // },
+  // mailingAddress: {
+  //   ...omit(
+  //     ['ui:order'],
+  //     addressUISchema(ADDRESS_PATHS.mailingAddress, 'Mailing address', true),
+  //   ),
+  //   'view:livesOnMilitaryBase': {
+  //     'ui:title':
+  //       'I live on a United States military base outside of the United States',
+  //   },
+  //   'view:livesOnMilitaryBaseInfo': {
+  //     'ui:description': () => (
+  //       <div className="vads-u-padding-x--2p5">
+  //         <va-additional-info trigger="Learn more about military base addresses">
+  //           <span>
+  //             The United States is automatically chosen as your country if you
+  //             live on a military base outside of the country.
+  //           </span>
+  //         </va-additional-info>
+  //       </div>
+  //     ),
+  //   },
+  //   country: {
+  //     'ui:title': 'Country',
+  //     'ui:autocomplete': 'country',
+  //     'ui:options': {
+  //       updateSchema: (formData, schema, uiSchemaCountry) => {
+  //         const uiSchemaDisabled = uiSchemaCountry;
 
-          if (formData.mailingAddress?.['view:livesOnMilitaryBase']) {
-            const formDataMailingAddress = formData.mailingAddress;
-            formDataMailingAddress.country = USA;
+  //         if (formData.mailingAddress?.['view:livesOnMilitaryBase']) {
+  //           const formDataMailingAddress = formData.mailingAddress;
+  //           formDataMailingAddress.country = USA;
 
-            uiSchemaDisabled['ui:disabled'] = true;
+  //           uiSchemaDisabled['ui:disabled'] = true;
 
-            return {
-              enum: [USA],
-            };
-          }
-          uiSchemaDisabled['ui:disabled'] = false;
-          return {
-            enum: countryEnum,
-          };
-        },
-      },
-    },
-    addressLine1: {
-      'ui:title': 'Street address (20 characters maximum)',
-      'ui:autocomplete': 'address-line1',
-      'ui:errorMessages': {
-        required: 'Please enter a street address',
-      },
-    },
-    addressLine2: {
-      'ui:title': 'Street address line 2 (20 characters maximum)',
-      'ui:autocomplete': 'address-line2',
-      'ui:errorMessages': {
-        pattern: 'Please enter a valid street address',
-      },
-    },
-    addressLine3: {
-      'ui:title': 'Street address line 3 (20 characters maximum)',
-      'ui:autocomplete': 'address-line3',
-      'ui:errorMessages': {
-        pattern: 'Please enter a valid street address',
-      },
-    },
-    city: {
-      'ui:autocomplete': 'address-level2',
-      'ui:errorMessages': {
-        pattern: 'Please enter a valid city',
-        required: 'Please enter a city',
-      },
-      'ui:options': {
-        replaceSchema: formData => {
-          if (formData.mailingAddress?.['view:livesOnMilitaryBase'] === true) {
-            return {
-              type: 'string',
-              title: 'APO/FPO/DPO',
-              enum: MILITARY_CITIES,
-            };
-          }
-          return merge(
-            {
-              title: 'City',
-            },
-            citySchema,
-          );
-        },
-      },
-      'ui:validations': [
-        {
-          options: { addressPath: 'mailingAddress' },
-          // pathWithIndex is called in validateMilitaryCity
-          validator: validateMilitaryCity,
-        },
-      ],
-    },
-    state: {
-      'ui:title': 'State',
-      'ui:autocomplete': 'address-level1',
-      'ui:options': {
-        hideIf: formData =>
-          !formData.mailingAddress?.['view:livesOnMilitaryBase'] &&
-          formData.mailingAddress.country !== USA,
-        updateSchema: formData => {
-          if (
-            formData.mailingAddress?.['view:livesOnMilitaryBase'] ||
-            MILITARY_CITIES.includes(formData.mailingAddress.city)
-          ) {
-            return {
-              enum: MILITARY_STATE_VALUES,
-              enumNames: MILITARY_STATE_LABELS,
-            };
-          }
-          return {
-            enum: STATE_VALUES,
-            enumNames: STATE_LABELS,
-          };
-        },
-      },
-      'ui:required': formData =>
-        formData.mailingAddress?.['view:livesOnMilitaryBase'] ||
-        formData.mailingAddress.country === USA,
-      'ui:validations': [
-        {
-          options: { addressPath: 'mailingAddress' },
-          // pathWithIndex is called in validateMilitaryState
-          validator: validateMilitaryState,
-        },
-      ],
-      'ui:errorMessages': {
-        pattern: 'Please enter a valid state',
-        required: 'Please enter a state',
-      },
-    },
-    zipCode: {
-      'ui:title': 'Postal code',
-      'ui:autocomplete': 'postal-code',
-      'ui:validations': [validateZIP],
-      'ui:required': formData =>
-        formData.mailingAddress?.['view:livesOnMilitaryBase'] ||
-        formData.mailingAddress.country === USA,
-      'ui:errorMessages': {
-        required: 'Please enter a postal code',
-        pattern:
-          'Please enter a valid 5- or 9-digit postal code (dashes allowed)',
-      },
-      'ui:options': {
-        widgetClassNames: 'va-input-medium-large',
-        hideIf: formData =>
-          !formData.mailingAddress?.['view:livesOnMilitaryBase'] &&
-          formData.mailingAddress.country !== USA,
-      },
-    },
-  },
+  //           return {
+  //             enum: [USA],
+  //           };
+  //         }
+  //         uiSchemaDisabled['ui:disabled'] = false;
+  //         return {
+  //           enum: countryEnum,
+  //         };
+  //       },
+  //     },
+  //   },
+  //   addressLine1: {
+  //     'ui:title': 'Street address (20 characters maximum)',
+  //     'ui:autocomplete': 'address-line1',
+  //     'ui:errorMessages': {
+  //       required: 'Please enter a street address',
+  //     },
+  //   },
+  //   addressLine2: {
+  //     'ui:title': 'Street address line 2 (20 characters maximum)',
+  //     'ui:autocomplete': 'address-line2',
+  //     'ui:errorMessages': {
+  //       pattern: 'Please enter a valid street address',
+  //     },
+  //   },
+  //   addressLine3: {
+  //     'ui:title': 'Street address line 3 (20 characters maximum)',
+  //     'ui:autocomplete': 'address-line3',
+  //     'ui:errorMessages': {
+  //       pattern: 'Please enter a valid street address',
+  //     },
+  //   },
+  //   city: {
+  //     'ui:autocomplete': 'address-level2',
+  //     'ui:errorMessages': {
+  //       pattern: 'Please enter a valid city',
+  //       required: 'Please enter a city',
+  //     },
+  //     'ui:options': {
+  //       replaceSchema: formData => {
+  //         if (formData.mailingAddress?.['view:livesOnMilitaryBase'] === true) {
+  //           return {
+  //             type: 'string',
+  //             title: 'APO/FPO/DPO',
+  //             enum: MILITARY_CITIES,
+  //           };
+  //         }
+  //         return merge(
+  //           {
+  //             title: 'City',
+  //           },
+  //           citySchema,
+  //         );
+  //       },
+  //     },
+  //     'ui:validations': [
+  //       {
+  //         options: { addressPath: 'mailingAddress' },
+  //         // pathWithIndex is called in validateMilitaryCity
+  //         validator: validateMilitaryCity,
+  //       },
+  //     ],
+  //   },
+  //   state: {
+  //     'ui:title': 'State',
+  //     'ui:autocomplete': 'address-level1',
+  //     'ui:options': {
+  //       hideIf: formData =>
+  //         !formData.mailingAddress?.['view:livesOnMilitaryBase'] &&
+  //         formData.mailingAddress.country !== USA,
+  //       updateSchema: formData => {
+  //         if (
+  //           formData.mailingAddress?.['view:livesOnMilitaryBase'] ||
+  //           MILITARY_CITIES.includes(formData.mailingAddress.city)
+  //         ) {
+  //           return {
+  //             enum: MILITARY_STATE_VALUES,
+  //             enumNames: MILITARY_STATE_LABELS,
+  //           };
+  //         }
+  //         return {
+  //           enum: STATE_VALUES,
+  //           enumNames: STATE_LABELS,
+  //         };
+  //       },
+  //     },
+  //     'ui:required': formData =>
+  //       formData.mailingAddress?.['view:livesOnMilitaryBase'] ||
+  //       formData.mailingAddress.country === USA,
+  //     'ui:validations': [
+  //       {
+  //         options: { addressPath: 'mailingAddress' },
+  //         // pathWithIndex is called in validateMilitaryState
+  //         validator: validateMilitaryState,
+  //       },
+  //     ],
+  //     'ui:errorMessages': {
+  //       pattern: 'Please enter a valid state',
+  //       required: 'Please enter a state',
+  //     },
+  //   },
+  //   zipCode: {
+  //     'ui:title': 'Postal code',
+  //     'ui:autocomplete': 'postal-code',
+  //     'ui:validations': [validateZIP],
+  //     'ui:required': formData =>
+  //       formData.mailingAddress?.['view:livesOnMilitaryBase'] ||
+  //       formData.mailingAddress.country === USA,
+  //     'ui:errorMessages': {
+  //       required: 'Please enter a postal code',
+  //       pattern:
+  //         'Please enter a valid 5- or 9-digit postal code (dashes allowed)',
+  //     },
+  //     'ui:options': {
+  //       widgetClassNames: 'va-input-medium-large',
+  //       hideIf: formData =>
+  //         !formData.mailingAddress?.['view:livesOnMilitaryBase'] &&
+  //         formData.mailingAddress.country !== USA,
+  //     },
+  //   },
+  // },
   'view:contactInfoDescription': {
     'ui:description': contactInfoUpdateHelpDescription,
   },
@@ -282,7 +320,18 @@ export const schema = {
   type: 'object',
   properties: {
     phoneAndEmail,
-    mailingAddress,
+    // mailingAddress,
+    // mailingAddress: addressUISchema,
+    // mailingAddress: addressSchema(),
+    mailingAddress: mappedAddressSchema({
+      keyMap: {
+        street: 'addressLine1',
+        street2: 'addressLine2',
+        street3: 'addressLine3',
+        postalCode: 'zipCode',
+      },
+      // omit: ['street3'],
+    }),
     'view:contactInfoDescription': {
       type: 'object',
       properties: {},
