f7fa54ccf86863115c6ef6bf15f544bd523a6aef
amyleadem <amyleadem@gmail.com>
2025-12-08 10:30:08 -0700

    White space changes to trigger e2e tests

--- src/applications/simple-forms/mock-simple-forms-patterns-v3/app-entry.jsx
+++ src/applications/simple-forms/mock-simple-forms-patterns-v3/app-entry.jsx
@@ -2,7 +2,6 @@ import 'platform/polyfills';
 import './sass/mock-form-patterns.scss';
 
 import startApp from 'platform/startup';
-
 import routes from './routes';
 import reducer from './reducers';
 import manifest from './manifest.json';
