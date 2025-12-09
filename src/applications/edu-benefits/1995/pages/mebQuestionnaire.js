6778818ca205f3cb1679cc4fac2fb7a4ac2f7dc6
s.caso <s.caso@afs.com>
2025-12-09 14:26:35 -0500

    block display

--- src/applications/edu-benefits/1995/pages/mebQuestionnaire.js
+++ src/applications/edu-benefits/1995/pages/mebQuestionnaire.js
@@ -115,7 +115,7 @@ const ResultDescription = ({
         {answers.map((answer, index) => (
           <li
             key={index}
-            className="vads-u-display--flex vads-u-align-items--start vads-u-margin-bottom--2"
+            className="vads-u-display--block vads-u-align-items--start vads-u-margin-bottom--2"
           >
             <va-icon
               icon="check"
@@ -183,7 +183,7 @@ export const benefitSwitchPage = () => ({
     mebBenefitSelection: {
       'ui:title': (
         <span
-          className="vads-u-font-size--h2 vads-u-font-family--serif vads-u-font-weight--bold vads-u-padding-bottom--2 vads-u-display--inline-block"
+          className="vads-u-font-size--h2 vads-u-font-family--serif vads-u-font-weight--bold vads-u-padding-bottom--2 vads-u-display--block"
           style={{ fontSize: '1.5rem' }}
         >
           Benefit you want to change to
