fe84769ec275d051e7fc74d1a0c8d643002d2232
Peri McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-12-09 16:49:27 -0700

    added a WhatHappensNextSection and tests

--- src/applications/travel-pay/components/complex-claims/pages/ConfirmationPage.jsx
+++ src/applications/travel-pay/components/complex-claims/pages/ConfirmationPage.jsx
@@ -11,11 +11,8 @@ import {
 } from '../../../redux/selectors';
 import { ComplexClaimsHelpSection } from '../../HelpText';
 import ExpensesAccordion from './ExpensesAccordion';
-import {
-  TRAVEL_PAY_INFO_LINK,
-  BTSSS_PORTAL_URL,
-  FORM_103542_LINK,
-} from '../../../constants';
+import { TRAVEL_PAY_INFO_LINK } from '../../../constants';
+import WhatHappensNextSection from './WhatHappensNextSection';
 
 const ConfirmationPage = () => {
   const { claimId } = useParams();
@@ -121,65 +118,7 @@ const ConfirmationPage = () => {
               <ExpensesAccordion expenses={expenses} documents={documents} />
             </>
           )}
-          <h2 className="vads-u-margin-top--4">What happens next</h2>
-          {submitError ? (
-            <div>
-              <p>
-                You can still file a claim within 30 days of this appointment
-                these other ways:
-              </p>
-              <ul>
-                <li>
-                  <p className="vads-u-margin-y--2">
-                    Online 24/7 through the Beneficiary Travel Self Service
-                    System (BTSSS)
-                  </p>
-                  <va-link
-                    external
-                    href={BTSSS_PORTAL_URL}
-                    text="File a travel claim online"
-                  />
-                </li>
-                <li>
-                  <p className="vads-u-margin-y--2">
-                    VA Form 10-3542 by mail, fax, email, or in person
-                  </p>
-                  <va-link
-                    href={FORM_103542_LINK}
-                    text="Learn more about VA Form 10-3542"
-                  />
-                </li>
-              </ul>
-            </div>
-          ) : (
-            <>
-              <va-process-list>
-                <va-process-list-item header="VA will review your claim">
-                  <p>
-                    You can check the status of this claim or review all your
-                    travel claims on your travel reimbursement claims page.
-                  </p>
-                  <va-link
-                    href="/my-health/travel-pay/claims/"
-                    text="Check your travel reimbursement claim status"
-                  />
-                </va-process-list-item>
-                <va-process-list-item header="If we approve your claim, we’ll send your pay through direct deposit">
-                  <p>
-                    You must have direct deposit set up in order to receive your
-                    funds. Direct deposit for travel pay is different than the
-                    direct deposit used for other VA claims. If you’ve already
-                    set up direct deposit for travel pay, no additional other
-                    are needed.
-                  </p>
-                  <va-link
-                    href="/resources/how-to-set-up-direct-deposit-for-va-travel-pay-reimbursement/"
-                    text="Set up direct deposit for travel pay"
-                  />
-                </va-process-list-item>
-              </va-process-list>
-            </>
-          )}
+          <WhatHappensNextSection isError={submitError} />
           {!submitError && (
             <div className="vads-u-margin-top--2">
               <va-link-action
