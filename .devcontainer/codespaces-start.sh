eb738106f5fea10b73a88c5e651fff62108089a4
Jeana <jeana.clark@adhocteam.us>
2025-12-09 15:33:47 -0600

    undo last commit

--- .devcontainer/codespaces-start.sh
+++ .devcontainer/codespaces-start.sh
@@ -59,9 +59,11 @@ if [ "$MAKE_APP_PUBLIC" == "YES" ]; then
     printf "\n\n##### Setup complete. Your servers are running and ports are public. #####\n"
 
     # Enable content-build port
-    # gh codespace ports visibility 3002:public -c "$CODESPACE_NAME"
-    # npx http-server build/localhost -s -c-1 -p 3002
-    # printf "\n\n##### Content build is now running and ports public. #####\n"
+    gh codespace ports visibility 3002:public -c "$CODESPACE_NAME"
+
+    npx http-server build/localhost -s -c-1 -p 3002
+
+    printf "\n\n##### Content build is now running and ports public. #####\n"
 
     wait
 
