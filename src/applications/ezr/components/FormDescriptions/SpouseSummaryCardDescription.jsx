d157ae5f59e176cd9c7cb673001c0b26366192b9
rmessina1010 <rmessina10@gmail.com>
2025-12-10 09:13:15 -0600

    requested changes and style update

--- src/applications/ezr/components/FormDescriptions/SpouseSummaryCardDescription.jsx
+++ src/applications/ezr/components/FormDescriptions/SpouseSummaryCardDescription.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-export const spousedStatusesToHideDate = ['married', 'separated'];
+export const maritalStatusesWithDateOfMarriage = ['married', 'separated'];
 
 const SpouseSummaryCardDescription = () => {
   const { data: formData } = useSelector(state => state.form);
   const { maritalStatus } = formData['view:maritalStatus'];
   const { dateOfMarriage } = formData;
-  const renderMarriageDate = spousedStatusesToHideDate.includes(
-    maritalStatus?.toLowerCase(),
-  ) ? (
-    <>
-      {' '}
-      <br />
-      Date of Marriage: {dateOfMarriage}
-    </>
-  ) : null;
+  const allDetails = [maritalStatus];
+  if (
+    maritalStatusesWithDateOfMarriage.includes(maritalStatus?.toLowerCase())
+  ) {
+    allDetails.push(`Date of Marriage: ${dateOfMarriage || ''}`);
+  }
   return (
-    <p className="vads-u-margin-bottom--0">
-      {maritalStatus}
-      {renderMarriageDate}
-    </p>
+    <ul className="ezr-list-reset">
+      {allDetails.map(detail => (
+        <li key={detail}>{detail}</li>
+      ))}
+    </ul>
   );
 };
 
--- src/applications/ezr/tests/unit/components/FormDescriptions/SpouseSummaryCardDescription.unit.spec.jsx
+++ src/applications/ezr/tests/unit/components/FormDescriptions/SpouseSummaryCardDescription.unit.spec.jsx
@@ -1,12 +1,12 @@
 import { expect } from 'chai';
 import React from 'react';
 import SpouseSummaryCardDescription, {
-  spousedStatusesToHideDate,
+  maritalStatusesWithDateOfMarriage,
 } from '../../../../components/FormDescriptions/SpouseSummaryCardDescription';
 import { renderProviderWrappedComponent } from '../../../helpers';
 
 describe('ezr <SpouseSummaryCardDescription>', () => {
-  spousedStatusesToHideDate.forEach(status => {
+  maritalStatusesWithDateOfMarriage.forEach(status => {
     context(
       `when marital status is ${status} and there is NO MARRIAGE DATE`,
       () => {
@@ -23,20 +23,22 @@ describe('ezr <SpouseSummaryCardDescription>', () => {
             },
             <SpouseSummaryCardDescription />,
           );
-          expect(container).to.not.be.empty;
-          expect(container.textContent.trim()).to.equal(
-            `${status} Date of Marriage:`,
+          const ul = container.querySelector('ul');
+          const items = Array.from(ul.children);
+          const match = items.filter(i =>
+            i.textContent.includes('Date of Marriage:'),
           );
+          expect(match.length).to.be.greaterThan(0);
         });
       },
     );
   });
 
-  spousedStatusesToHideDate.forEach(status => {
+  maritalStatusesWithDateOfMarriage.forEach(status => {
     context(
       `when marital status is ${status} and there IS a marriage date`,
       () => {
-        it('should render the marital status and a blank date of marriage', () => {
+        it('should render the marital status and the date of marriage', () => {
           const { container } = renderProviderWrappedComponent(
             {
               form: {
@@ -50,17 +52,19 @@ describe('ezr <SpouseSummaryCardDescription>', () => {
             },
             <SpouseSummaryCardDescription />,
           );
-          expect(container).to.not.be.empty;
-          expect(container.textContent.trim()).to.equal(
-            `${status} Date of Marriage: 1995-02-22`,
+          const ul = container.querySelector('ul');
+          const items = Array.from(ul.children);
+          const match = items.filter(i =>
+            i.textContent.includes('Date of Marriage: 1995-02-22'),
           );
+          expect(match.length).to.be.greaterThan(0);
         });
       },
     );
   });
 
   context(`when marital status is single`, () => {
-    it('should render the marital status and a blank date of marriage', () => {
+    it('should render ONLY the marital status', () => {
       const { container } = renderProviderWrappedComponent(
         {
           form: {
