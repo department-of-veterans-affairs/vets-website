713976b74211d5860cd70b89e76cfc9c536bfd01
Justin Linn <justin@linnjs.com>
2025-12-10 11:07:58 -0500

    fix(bio-aquia): Fix CI warning for file extension on import

--- src/applications/static-pages/benefits-optimization-aquia/21-2680/entry.js
+++ src/applications/static-pages/benefits-optimization-aquia/21-2680/entry.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 export default function create212680Access(store, widgetType) {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   if (root) {
-    import(/* webpackChunkName: "form-21-2680" */ './App.js').then(module => {
+    import(/* webpackChunkName: "form-21-2680" */ './App').then(module => {
       const App = module.default;
       ReactDOM.render(
         <Provider store={store}>
--- src/applications/static-pages/benefits-optimization-aquia/21P-530a/entry.js
+++ src/applications/static-pages/benefits-optimization-aquia/21P-530a/entry.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 export default function create21P530aAccess(store, widgetType) {
   const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
   if (root) {
-    import(/* webpackChunkName: "form-21P-530a" */ './App.js').then(module => {
+    import(/* webpackChunkName: "form-21P-530a" */ './App').then(module => {
       const App = module.default;
       ReactDOM.render(
         <Provider store={store}>
