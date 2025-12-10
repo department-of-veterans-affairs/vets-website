835f4124b36e721e33e3706b6f49cc7ba1626229
Luis Metzger <luismetzger@me.com>
2025-12-10 14:53:23 -0700

    [#125246] Remove whitespace.

--- src/applications/disability-benefits/all-claims/pages/trainingPayWaiver.js
+++ src/applications/disability-benefits/all-claims/pages/trainingPayWaiver.js
@@ -5,7 +5,7 @@ import ConfirmationTrainingPayWaiver from '../components/confirmationFields/Conf
 export const uiSchema = {
   'ui:title': 'Training pay waiver',
   waiveTrainingPay: yesNoUI({
-    title: ' ',
+    title: '',
     description: waiveTrainingPayDescription,
     yesNoReverse: true,
     labels: {
