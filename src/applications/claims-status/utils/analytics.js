091b17e0ba7451b428689f4ccdf921ad926a0b1a
Joshua <joshing_you1@yahoo.com>
2025-12-10 15:23:06 -0500

    Fixed import issue

--- src/applications/claims-status/utils/analytics.js
+++ src/applications/claims-status/utils/analytics.js
@@ -1,4 +1,4 @@
-import { recordEvent } from '@department-of-veterans-affairs/platform-monitoring/exports';
+import recordEvent from '@department-of-veterans-affairs/platform-monitoring/record-event';
 
 /**
  * Session storage keys for upload tracking
