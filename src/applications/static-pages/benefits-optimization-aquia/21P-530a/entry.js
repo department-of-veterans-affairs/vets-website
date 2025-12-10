4b475290da3f63869d69dae2ae1c4de3c83f6d12
Justin Linn <justin@linnjs.com>
2025-12-10 10:46:18 -0500

    feat(bio-aquia): Create form widget CTAs for all forms

---
+++ src/applications/static-pages/benefits-optimization-aquia/21P-530a/entry.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+
+export default function create21P530aAccess(store, widgetType) {
+  const root = document.querySelector(`[data-widget-type="${widgetType}"]`);
+  if (root) {
+    import(/* webpackChunkName: "form-21P-530a" */ './App.js').then(module => {
+      const App = module.default;
+      ReactDOM.render(
+        <Provider store={store}>
+          <App />
+        </Provider>,
+        root,
+      );
+    });
+  }
+}
