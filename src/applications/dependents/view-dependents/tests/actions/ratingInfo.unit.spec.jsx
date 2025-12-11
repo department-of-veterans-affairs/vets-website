515dbe2454f4eefed48e0803ac04d30cac4023d0
aafmisra <aafmisra@gmail.com>
2025-12-11 16:11:55 -0500

    use disability rating to limit verification alert

--- src/applications/dependents/view-dependents/tests/actions/ratingInfo.unit.spec.jsx
+++ src/applications/dependents/view-dependents/tests/actions/ratingInfo.unit.spec.jsx
@@ -4,23 +4,23 @@ import { mockFetch, setFetchJSONResponse } from 'platform/testing/unit/helpers';
 import { dependents } from './helpers';
 
 import {
-  FETCH_ALL_DEPENDENTS_SUCCESS,
-  FETCH_ALL_DEPENDENTS_FAILED,
-  fetchAllDependents,
-} from '../../actions';
+  FETCH_RATING_INFO_SUCCESS,
+  FETCH_RATING_INFO_FAILED,
+  fetchRatingInfo,
+} from '../../actions/ratingInfo';
 
-describe('View Disabilities actions: fetchAllDependents', () => {
+describe('View Disabilities actions: fetchRatingInfo', () => {
   beforeEach(() => mockFetch());
 
   it('should fetch rated disabilities', () => {
     setFetchJSONResponse(global.fetch.onCall(0), dependents);
-    const makeTheFetch = fetchAllDependents();
+    const makeTheFetch = fetchRatingInfo();
 
     const dispatchSpy = sinon.spy();
     const dispatch = action => {
       dispatchSpy(action);
       expect(dispatchSpy.firstCall.args[0].type).to.equal(
-        FETCH_ALL_DEPENDENTS_SUCCESS,
+        FETCH_RATING_INFO_SUCCESS,
       );
       expect(dispatchSpy.firstCall.args[1].errors).to.equal(undefined);
     };
@@ -37,13 +37,13 @@ describe('View Disabilities actions: fetchAllDependents', () => {
       ],
     };
     setFetchJSONResponse(global.fetch.onCall(0), response);
-    const thunk = fetchAllDependents();
+    const thunk = fetchRatingInfo();
     const dispatchSpy = sinon.spy();
     const dispatch = action => {
       dispatchSpy(action);
       expect(dispatchSpy.calledOnce).to.be.true;
       expect(dispatchSpy.firstCall.args[0].type).to.equal(
-        FETCH_ALL_DEPENDENTS_FAILED,
+        FETCH_RATING_INFO_FAILED,
       );
       expect(dispatchSpy.firstCall.args[1]).to.equal(response);
     };
