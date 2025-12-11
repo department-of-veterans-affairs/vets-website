3d4b92f0a41dcd915338da0d08b344cd391d832b
James Taggart <jtaggart@kindsys.us>
2025-12-11 14:45:36 -0800

    Mocked value is snake case.

--- src/applications/letters/tests/containers/App.unit.spec.jsx
+++ src/applications/letters/tests/containers/App.unit.spec.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import sinon from 'sinon';
 
+import * as datadog from 'platform/monitoring/Datadog';
 import { App } from '../../containers/App';
 
 const mockStore = configureStore([]);
@@ -19,7 +20,7 @@ describe('<App />', () => {
     sandbox.restore();
   });
 
-  it('renders without crashing', () => {
+  it('Check component renders without crashing.', () => {
     const store = mockStore({
       user: {},
       letters: {},
@@ -39,5 +40,92 @@ describe('<App />', () => {
         <App {...props} />
       </Provider>,
     );
+  });
+
+  it('Checks feature flags are enabled if loaded and on.', () => {
+    const store = mockStore({
+      user: {},
+      letters: {},
+      featureToggles: {
+        loading: false,
+        /* eslint-disable camelcase */
+        letters_client_side_monitoring: true,
+      },
+    });
+    const loggingStub = sandbox.stub(datadog, 'initializeBrowserLogging');
+    const props = {
+      featureFlagsLoading: false,
+      user: {
+        login: {},
+        profile: {
+          services: [],
+          verified: true,
+        },
+      },
+    };
+    render(
+      <Provider store={store}>
+        <App {...props} />
+      </Provider>,
+    );
+    sinon.assert.calledOnce(loggingStub);
+  });
+
+  it('Checks logging was not enabled if feature flags are loading.', () => {
+    const store = mockStore({
+      user: {},
+      letters: {},
+      featureToggles: {
+        loading: true,
+        /* eslint-disable camelcase */
+        letters_client_side_monitoring: undefined,
+      },
+    });
+    const loggingStub = sandbox.stub(datadog, 'initializeBrowserLogging');
+    const props = {
+      featureFlagsLoading: false,
+      user: {
+        login: {},
+        profile: {
+          services: [],
+          verified: true,
+        },
+      },
+    };
+    render(
+      <Provider store={store}>
+        <App {...props} />
+      </Provider>,
+    );
+    sinon.assert.notCalled(loggingStub);
+  });
+
+  it('Check logging not enabled if feature flag loaded and off.', () => {
+    const store = mockStore({
+      user: {},
+      letters: {},
+      featureToggles: {
+        loading: false,
+        /* eslint-disable camelcase */
+        letters_client_side_monitoring: false,
+      },
+    });
+    const loggingStub = sandbox.stub(datadog, 'initializeBrowserLogging');
+    const props = {
+      featureFlagsLoading: false,
+      user: {
+        login: {},
+        profile: {
+          services: [],
+          verified: true,
+        },
+      },
+    };
+    render(
+      <Provider store={store}>
+        <App {...props} />
+      </Provider>,
+    );
+    sinon.assert.notCalled(loggingStub);
   });
 });
