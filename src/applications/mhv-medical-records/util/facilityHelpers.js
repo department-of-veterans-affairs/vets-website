063f827fbea28335a5d981dec0a1429f55631d54
Napoleon K. <95312667+GovNapoleon@users.noreply.github.com>
2025-12-11 13:49:16 -0600

    Update JSDoc

--- src/applications/mhv-medical-records/util/facilityHelpers.js
+++ src/applications/mhv-medical-records/util/facilityHelpers.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
 /**
- * Formats an array of facility names into an unordered List of facility names.
- *
+ * Formats an array of facility names into an unordered list of facility names.
  *
  * @param {Array<string>} facilities - Array of facility names
- * @returns {Element} Formatted as an unordered list of facility names
+ * @returns {JSX.Element|string} Formatted as an unordered list of facility names, or empty string if no facilities
  *
- * Examples:
- * - 1 facility: "VA Western New York health care"
- * - 2 facilities: "VA Western New York health care and VA Pacific Islands health care"
- * - 3+ facilities: "VA Western New York health care, VA Pacific Islands health care, and VA Central Ohio health care"
+ * @example
+ * // Returns empty string for empty/null array
+ * formatFacilityUnorderedList([]) // ''
+ * formatFacilityUnorderedList(null) // ''
+ *
+ * @example
+ * // Returns JSX unordered list for array with items
+ * formatFacilityUnorderedList(['VA Western New York health care'])
+ * // <ul>
+ * //   <li>VA Western New York health care</li>
+ * // </ul>
+ *
+ * @example
+ * // Multiple facilities
+ * formatFacilityUnorderedList(['VA Western New York health care', 'VA Pacific Islands health care'])
+ * // <ul>
+ * //   <li>VA Western New York health care</li>
+ * //   <li>VA Pacific Islands health care</li>
+ * // </ul>
  */
 export const formatFacilityUnorderedList = facilities => {
   if (!facilities || facilities.length === 0) return '';
