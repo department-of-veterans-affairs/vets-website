217c8a6b3aa83450c5540d360094fd8ec6241c3c
James Taggart <jtaggart@kindsys.us>
2025-12-11 12:26:47 -0800

    Working render test.

--- src/applications/letters/tests/containers/App.unit.spec.jsx
+++ src/applications/letters/tests/containers/App.unit.spec.jsx
@@ -2,21 +2,41 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import sinon from 'sinon';
+
 import { App } from '../../containers/App';
 
 const mockStore = configureStore([]);
 
 describe('<App />', () => {
+  let sandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
   it('renders without crashing', () => {
     const store = mockStore({
       user: {},
       letters: {},
-      // ...add more if needed...
     });
-
+    const props = {
+      featureFlagsLoading: true,
+      user: {
+        login: {},
+        profile: {
+          services: [],
+          verified: true,
+        },
+      },
+    };
     render(
       <Provider store={store}>
-        <App letterTitle="Test Letter" letterType="test_letter" />
+        <App {...props} />
       </Provider>,
     );
   });
