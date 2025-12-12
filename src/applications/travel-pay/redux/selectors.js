0e105faaf43d7f8a19dbdbf796f8267e317f48c1
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-12-12 10:01:32 -0800

    [#125420] Travel Pay, Complex claims breadcrumb functionality (#40397)
    * Set storage item in claim details route
    
    * Add conditional url and text for complex claims breadcrumb
    
    * Add tests
    
    * Add hasUnsavedChanges to the store
    
    * Use hasUnsavedChanges in wrapper
    
    * Add unsaved changes modal
    
    * Use setUnsavedExpenseChanges in expense page and mileage
    
    * Tests
    
    * Add constant for session storage and update usage
    
    * Test updates
    
    * DRY up the backHref with a helper
    
    * Swap secondary and primary in the unsaved changes modal
    
    * Fix cypress test
    
    * Add shared constants to platform constants
    
    * Update usages to import platform constant
    
    * Fix client/browser routing handling in complex claims wrapper

--- src/applications/travel-pay/redux/selectors.js
+++ src/applications/travel-pay/redux/selectors.js
@@ -54,4 +54,7 @@ export const selectComplexClaimSubmissionState = state =>
 export const selectComplexClaimFetchLoadingState = state =>
   state.travelPay.complexClaim.claim.fetch?.isLoading || false;
 
+export const selectHasUnsavedExpenseChanges = state =>
+  state.travelPay.complexClaim.expenses.hasUnsavedChanges || false;
+
 export const selectReviewPageAlert = state => state.travelPay.reviewPageAlert;
