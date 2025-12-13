cee36d87af62a681225c3e2c607fbbe0a77bebdb
Napoleon K. <95312667+GovNapoleon@users.noreply.github.com>
2025-12-12 18:54:54 -0600

    remove class vads-u-margin--0

--- src/applications/mhv-medical-records/containers/ccdContent/OHOnlyIntroText.jsx
+++ src/applications/mhv-medical-records/containers/ccdContent/OHOnlyIntroText.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const OHOnlyIntroText = () => (
   <>
     <h1>Download your medical records report</h1>
-    <p className="vads-u-margin--0">
+    <p>
       Download your Continuity of Care Document (CCD), a summary of your VA
       medical records.
     </p>
