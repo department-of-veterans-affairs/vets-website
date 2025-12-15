40a6d6d15c217650ff2cff53f9d43c25dbe5c704
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2025-12-15 13:50:19 -0800

    cleanup comments

--- src/applications/vaos/utils/scrollAndFocus.js
+++ src/applications/vaos/utils/scrollAndFocus.js
@@ -2,8 +2,6 @@
 
 import { focusElement } from 'platform/utilities/ui';
 
-// To keep selector but add a delay, make sure to pass undefined and NOT null, as it will override the default parameter
-// e.g. scrollAndFocus(undefined, 50);
 export function scrollAndFocus(selector = 'h1,.loading-indicator') {
   const el = document.querySelector(selector);
   focusElement(el);
