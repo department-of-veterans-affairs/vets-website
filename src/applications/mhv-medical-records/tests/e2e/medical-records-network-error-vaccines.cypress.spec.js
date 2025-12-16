0721bb1d316415783f77c0b9df9505918365879f
Ni Chia <ni.chia@adhocteam.us>
2025-12-15 20:18:45 -0800

    update e2e test specs for vaccines

--- src/applications/mhv-medical-records/tests/e2e/medical-records-network-error-vaccines.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-network-error-vaccines.cypress.spec.js
@@ -1,17 +1,18 @@
 import MedicalRecordsSite from './mr_site/MedicalRecordsSite';
+import oracleHealthUser from './accelerated/fixtures/user/oracle-health.json';
 
 describe('Medical Records View Vaccines', () => {
   const site = new MedicalRecordsSite();
 
   beforeEach(() => {
-    site.login();
+    site.login(oracleHealthUser, false);
+    site.mockFeatureToggles();
   });
 
   it('Visits Medical Records, Views Network Error On Vaccines List', () => {
     // const site = new MedicalRecordsSite();
     // site.login();
-
-    cy.intercept('GET', '/my_health/v1/medical_records/vaccines', {
+    cy.intercept('GET', '/my_health/v2/medical_records/immunization*', {
       statusCode: 400,
       body: {
         alertType: 'error',
@@ -22,7 +23,7 @@ describe('Medical Records View Vaccines', () => {
           content: 'err.detail',
         },
       },
-    }).as('folder');
+    }).as('vaccines-list');
 
     cy.visit('my-health/medical-records');
     cy.intercept('POST', '/my_health/v1/medical_records/session').as('session');
