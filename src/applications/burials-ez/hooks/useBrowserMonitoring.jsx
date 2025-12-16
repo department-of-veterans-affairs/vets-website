b5dfefb24ceedc61503dd17bd878242649c034a8
Chris Kim <42885441+chriskim2311@users.noreply.github.com>
2025-12-15 20:50:10 -0800

    revert

--- src/applications/burials-ez/hooks/useBrowserMonitoring.jsx
+++ src/applications/burials-ez/hooks/useBrowserMonitoring.jsx
@@ -17,17 +17,7 @@ const VERSION = '1.0.0';
 const botPattern =
   '(googlebot/|bot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)';
 const botRegex = new RegExp(botPattern, 'i');
-
-/**
- * Get conditional sample rate based on whether the user agent is a bot
- * @returns {number} 0 if bot detected, 100 if not
- */
-const getConditionalSampleRate = () => {
-  if (typeof navigator === 'undefined' || !navigator.userAgent) {
-    return 100; // Default to full sampling if navigator is not available
-  }
-  return botRegex.test(navigator.userAgent) ? 0 : 100;
-};
+const conditionalSampleRate = botRegex.test(navigator.userAgent) ? 0 : 100;
 
 const initializeRealUserMonitoring = () => {
   // Prevent RUM from re-initializing the SDK OR running on local/CI environments.
@@ -43,7 +33,7 @@ const initializeRealUserMonitoring = () => {
       // default settings
       site: 'ddog-gov.com',
       env: environment.vspEnvironment(),
-      sessionSampleRate: getConditionalSampleRate(),
+      sessionSampleRate: conditionalSampleRate,
       sessionReplaySampleRate: 20,
       trackUserInteractions: true,
       trackResources: true,
@@ -69,7 +59,7 @@ const initializeBrowserLogging = () => {
       // default settings
       site: 'ddog-gov.com',
       env: environment.vspEnvironment(),
-      sessionSampleRate: getConditionalSampleRate(),
+      sessionSampleRate: conditionalSampleRate,
       forwardErrorsToLogs: true,
       forwardConsoleLogs: ['error'],
       forwardReports: [],
