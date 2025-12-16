1150b9b7a99f927b9e945263b5bba2ded3b37184
Robert Hasselle <123402053+rhasselle-oddball@users.noreply.github.com>
2025-12-16 11:45:51 -0600

    trigger form tests

--- src/applications/disability-benefits/all-claims/tests/all-claims.cypress.spec.js
+++ src/applications/disability-benefits/all-claims/tests/all-claims.cypress.spec.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import testForm from 'platform/testing/e2e/cypress/support/form-tester';
 import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-tester/utilities';
-
 import mockUser from './fixtures/mocks/user.json';
 import mockFeatureToggles from './fixtures/mocks/feature-toggles.json';
 
