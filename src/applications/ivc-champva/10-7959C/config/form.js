f1e3ecd151f1d18f3a389e3fb0db56a007d4ad28
Matt Long <6738544+longmd@users.noreply.github.com>
2025-12-18 10:53:07 -0500

    ArrayBuilder | Fix file upload error

--- src/applications/ivc-champva/10-7959C/config/form.js
+++ src/applications/ivc-champva/10-7959C/config/form.js
@@ -56,7 +56,7 @@ import { MissingFileConsentPage } from '../components/MissingFileConsentPage';
 import NotEnrolledPage from '../components/FormPages/NotEnrolledPage';
 import { FEATURE_TOGGLES } from '../hooks/useDefaultFormData';
 
-// import mockdata from '../tests/e2e/fixtures/data/test-data.json';
+//  import mockdata from '../tests/e2e/fixtures/data/test-data.json';
 
 // Control whether we show the file overview page by calling `hasReq` to
 // determine if any files have not been uploaded. Defaults to false (hide the page)
