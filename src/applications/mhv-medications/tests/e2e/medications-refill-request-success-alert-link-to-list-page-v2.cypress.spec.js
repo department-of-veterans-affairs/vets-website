c54e24cd42452ab6bdc03443eb43b693bfbf79ce
Liz Townsend <liz.townsend@adhocteam.us>
2025-12-18 10:31:50 -0600

    update 2 cerner e2e tests

--- src/applications/mhv-medications/tests/e2e/medications-refill-request-success-alert-link-to-list-page-v2.cypress.spec.js
+++ src/applications/mhv-medications/tests/e2e/medications-refill-request-success-alert-link-to-list-page-v2.cypress.spec.js
@@ -2,35 +2,20 @@ import MedicationsSite from './med_site/MedicationsSite';
 import MedicationsRefillPage from './pages/MedicationsRefillPage';
 import prescriptions from './fixtures/listOfPrescriptions.json';
 import successRequest from './fixtures/refill-success.v2.json';
-import mockToggles from './fixtures/toggles-response.json';
 
 describe('Medications Refill Success Alert Message Link (v2)', () => {
   beforeEach(() => {
     const site = new MedicationsSite();
-    // Mock feature flag for v2 endpoint
-    const baseFeatures = mockToggles.data.features.filter(
-      f => f.name !== 'mhv_medications_cerner_pilot',
-    );
-    const mockTogglesV2 = {
-      data: {
-        type: 'feature_toggles',
-        features: [
-          ...baseFeatures,
-          { name: 'mhv_medications_cerner_pilot', value: true },
-        ],
-      },
-    };
 
     cy.intercept('GET', `my_health/v1/prescriptions/*`, () => {
       // fail the test if v1 endpoint is called
       throw new Error(
-        'v1 endpoint should not be called when Cerner pilot flag is enabled',
+        'v1 endpoint should not be called when accelerating medications',
       );
     });
-    site.login();
-    cy.intercept('GET', '/v0/feature_toggles?*', mockTogglesV2).as(
-      'featureToggles',
-    );
+
+    // Login with accelerated medications enabled (which includes the Cerner pilot toggle)
+    site.login(true, false, true);
   });
 
   it('visits Medications List Link on Success Alert with v2 endpoint', () => {
