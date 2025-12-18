c15774d4930d943da260089e9461b28c16c50ad3
Christopher Floess <christopher.floess@adhocteam.us>
2025-12-18 15:17:58 -0500

    chore(script): add sleep to review-entrypoint.sh

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -10,5 +10,7 @@ npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' -
 cd ../content-build
 cp .env.example .env && yarn install --production=false
 npm run fetch-drupal-cache
+
+sleep 100000
 # npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' --apps-directory-name=application
 # npm run heroku-serve -- build/localhost -p 3002
