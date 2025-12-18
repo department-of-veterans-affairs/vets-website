418f2e623deefb2e3ddbb0f58cb2b456bd45b02b
Christopher Floess <christopher.floess@adhocteam.us>
2025-12-18 13:25:50 -0500

    fix(script): comment out problematic lines

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -10,5 +10,5 @@ npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' -
 cd ../content-build
 cp .env.example .env && yarn install --production=false
 npm run fetch-drupal-cache
-npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' --apps-directory-name=application
-npm run heroku-serve -- build/localhost -p 3002
+# npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' --apps-directory-name=application
+# npm run heroku-serve -- build/localhost -p 3002
