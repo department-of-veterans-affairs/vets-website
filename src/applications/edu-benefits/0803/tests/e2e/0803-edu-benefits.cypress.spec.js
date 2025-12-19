2c4e962bd411314bb3640e925a2496bcf9f347e4
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2025-12-19 15:24:11 -0800

    fill out testConfig for 0803 e2e testing

--- src/applications/edu-benefits/0803/tests/e2e/0803-edu-benefits.cypress.spec.js
+++ src/applications/edu-benefits/0803/tests/e2e/0803-edu-benefits.cypress.spec.js
@@ -4,40 +4,86 @@ import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-test
 import featureToggles from '../fixtures/mocks/feature-toggles.json';
 import user from '../fixtures/mocks/user.json';
 import mockSubmit from '../fixtures/mocks/application-submit.json';
+import prefilledForm from '../fixtures/mocks/prefilled-form.json';
+import sip from '../fixtures/mocks/sip-put.json';
 import formConfig from '../../config/form';
 import manifest from '../../manifest.json';
 
 const testConfig = createTestConfig(
   {
     dataPrefix: 'data',
-    dataSets: ['minimal-test', 'maximal-test'],
+    dataSets: ['minimal-test'],
     dataDir: path.join(__dirname, '..', 'fixtures', 'data'),
     pageHooks: {
       introduction: ({ afterHook }) => {
         afterHook(() => {
-          cy.findAllByText(/^start/i, { selector: 'a[href="#start"]' })
-            .last()
-            .click({ force: true });
+          cy.get('va-link-action[href="#start"]').click();
         });
       },
-      // Example page hook
-      // All paths are already automatically filled out based on fixtures.
-      // But if you want to manually test a page add the path.
-      // 'name-and-date-of-birth': ({ afterHook }) => {
-      //   cy.injectAxeThenAxeCheck();
-      //   afterHook(() => {
-      //     cy.get('@testData').then(() => {
-      //       cy.fillPage(); // fills all fields based on fixtures.
-      //       cy.axeCheck();
-      //       cy.findByText(/continue/i, { selector: 'button' }).click();
-      //     });
-      //   });
-      // },
+      'previously-applied': ({ afterHook }) => {
+        afterHook(() => {
+          cy.selectVaRadioOption('root_hasPreviouslyApplied', 'Y');
+          cy.tabToSubmitForm();
+        });
+      },
+      'select-va-benefit-program': ({ afterHook }) => {
+        afterHook(() => {
+          cy.selectVaRadioOption('root_vaBenefitProgram', 'chapter33');
+          cy.tabToSubmitForm();
+        });
+      },
+      'mailing-address': ({ afterHook }) => {
+        afterHook(() => {
+          cy.selectVaSelect('root_mailingAddress_country', 'USA');
+          cy.fillVaTextInput('root_mailingAddress_street', 'The Street');
+          cy.fillVaTextInput('root_mailingAddress_city', 'City');
+          cy.selectVaSelect('root_mailingAddress_state', 'AL');
+          cy.fillVaTextInput('root_mailingAddress_postalCode', '12345');
+          cy.tabToSubmitForm();
+        });
+      },
+      'test-name-and-date': ({ afterHook }) => {
+        afterHook(() => {
+          cy.fillVaTextInput('root_testName', 'Philosophy');
+          cy.fillVaMemorableDate('root_testDate', '2020-01-01', false);
+          cy.tabToSubmitForm();
+        });
+      },
+      'organization-info': ({ afterHook }) => {
+        afterHook(() => {
+          cy.fillVaTextInput('root_organizationName', 'Acme Inc.');
+          cy.fillVaTextInput('root_organizationAddress_street', 'The Street');
+          cy.fillVaTextInput('root_organizationAddress_city', 'City');
+          cy.selectVaSelect('root_organizationAddress_state', 'AL');
+          cy.fillVaTextInput('root_organizationAddress_postalCode', '12345');
+          cy.tabToSubmitForm();
+        });
+      },
+      'test-cost': ({ afterHook }) => {
+        afterHook(() => {
+          cy.fillVaTextInput('root_testCost', '123.00');
+          cy.tabToSubmitForm();
+        });
+      },
+      'review-and-submit': ({ afterHook }) => {
+        afterHook(() => {
+          cy.get('va-text-input[id="veteran-signature"]').then(el => {
+            cy.fillVaTextInput(el, 'John Doe');
+          });
+          cy.get('va-checkbox[id="veteran-certify"]').then(el => {
+            cy.selectVaCheckbox(el, true);
+          });
+          cy.tabToSubmitForm();
+        });
+      },
     },
     setupPerTest: () => {
       cy.intercept('GET', '/v0/user', user);
       cy.intercept('GET', '/v0/feature_toggles?*', featureToggles);
+      cy.intercept('GET', '/data/cms/vamc-ehr.json', {});
       cy.intercept('POST', formConfig.submitUrl, mockSubmit);
+      cy.intercept('GET', '/v0/in_progress_forms/22-0803', prefilledForm);
+      cy.intercept('PUT', '/v0/in_progress_forms/22-0803', sip);
       cy.login(user);
     },
     skip: Cypress.env('CI'), // Skip CI initially until content-build is merged
