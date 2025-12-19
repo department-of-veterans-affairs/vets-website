a66a4c3df6127c51ac7df6fec2e0ab9534084fe9
Jami Gibbs <jami.gibbs@adhocteam.us>
2025-12-19 16:07:28 -0600

    try to get full tests to run

--- src/platform/forms/constants.js
+++ src/platform/forms/constants.js
@@ -1036,3 +1036,6 @@ export const TRACKING_PREFIXES = MY_VA_SIP_FORMS.reduce((acc, form) => {
   acc[form.id] = form.trackingPrefix;
   return acc;
 }, {});
+
+// Temporary update for running tests
+export const EXAMPLE = 'example';
