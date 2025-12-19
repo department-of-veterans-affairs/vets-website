56fb4f5ea9571ff4d5155e94ec35b9b7365ef067
Peri-Ann McLaren <141954992+pmclaren19@users.noreply.github.com>
2025-12-19 12:45:50 -0700

    Added description to mileage expense (#40984)
    * added description to mileage expense
    
    * update tests
    
    * update code for node 22 tests

--- src/applications/travel-pay/components/complex-claims/pages/Mileage.jsx
+++ src/applications/travel-pay/components/complex-claims/pages/Mileage.jsx
@@ -46,10 +46,13 @@ const Mileage = () => {
         : selectExpenseCreationLoadingState(state),
   );
 
-  const initialFormStateRef = useRef({ departureAddress: '', tripType: '' });
+  const initialFormStateRef = useRef({
+    departureAddress: '',
+    tripType: '',
+  });
   const previousHasChangesRef = useRef(false);
 
-  const [formState, setFormState] = useState({});
+  const [formState, setFormState] = useState({ description: 'Mileage' });
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [showTripTypeError, setShowTripTypeError] = useState(false);
   const [showDepartureAddressError, setShowDepartureAddresError] = useState(
@@ -90,6 +93,7 @@ const Mileage = () => {
         const initialState = {
           departureAddress: 'home-address',
           tripType: TRIP_TYPES.ROUND_TRIP.value,
+          description: 'Mileage',
         };
         setFormState(initialState);
         initialFormStateRef.current = initialState;
