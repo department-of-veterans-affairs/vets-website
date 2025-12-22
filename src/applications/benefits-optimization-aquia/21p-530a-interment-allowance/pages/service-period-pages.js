b5f14537e741afebb04ae415f575eb7e4b160e0c
Calvin Costa <108481161+costacalvin@users.noreply.github.com>
2025-12-22 16:46:26 -0500

    removed service locations and rank requirements (#41008)
    * removed service locations and rank requirements
    
    * remove optional fields from isItemIncomplete list
    
    ---------
    
    Co-authored-by: Calvin Costa <calvin.costa@va.com>

--- src/applications/benefits-optimization-aquia/21p-530a-interment-allowance/pages/service-period-pages.js
+++ src/applications/benefits-optimization-aquia/21p-530a-interment-allowance/pages/service-period-pages.js
@@ -36,12 +36,7 @@ const servicePeriodOptions = {
   nounPlural: 'service periods',
   required: false,
   isItemIncomplete: item =>
-    !item.serviceBranch &&
-    !item.dateEnteredService &&
-    !item.placeEnteredService &&
-    !item.rankAtSeparation &&
-    !item.dateLeftService &&
-    !item.placeLeftService,
+    !item.serviceBranch && !item.dateEnteredService && !item.dateLeftService,
   text: {
     summaryTitle: "Review the Veteran's service periods",
     getItemName: item =>
@@ -111,7 +106,6 @@ const serviceLocationsAndRankPage = {
   },
   schema: {
     type: 'object',
-    required: ['placeEnteredService', 'placeLeftService', 'rankAtSeparation'],
     properties: {
       placeEnteredService: textSchema,
       placeLeftService: textSchema,
