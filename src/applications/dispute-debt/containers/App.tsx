a8e7b04bc606c1bc67a88fea1446adaea79e6505
Rebecca Weir <rebecca.weir@agile6.com>
2025-12-22 15:19:12 -0500

    typescript conversion wip

---
+++ src/applications/dispute-debt/containers/App.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import { isLoggedIn, selectProfile } from 'platform/user/selectors';
+import { useFeatureToggle } from 'platform/utilities/feature-toggles';
+import { I18nextProvider } from 'react-i18next';
+
+import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+import { DowntimeNotification } from 'platform/monitoring/DowntimeNotification';
+import i18nDebtApp from '../i18n';
+import formConfig from '../config/form';
+import { fetchDebts } from '../actions';
+import useDocumentTitle from '../hooks/useDocumentTitle';
+
+import type { DisputeDebtState } from '../types/state';
+
+type Location = {
+  pathname: string;
+  href?: string;
+  search?: string;
+  [key: string]: unknown;
+};
+
+interface DisputeDebtProps extends React.PropsWithChildren {
+  location: Location;
+}
+
+export const App = ({ children, location }: DisputeDebtProps) => {
+  const dispatch = useDispatch();
+
+  const userLoggedIn = useSelector((state: DisputeDebtState) => isLoggedIn(state));
+  const isVerified = useSelector(
+    (state: DisputeDebtState) => selectProfile(state)?.verified || false,
+  );
+  const { isDebtPending } = useSelector((state: DisputeDebtState) => state.availableDebts);
+  const { useToggleLoadingValue } = useFeatureToggle();
+  const isLoadingFeatures = useToggleLoadingValue();
+
+  const formData = useSelector((state: DisputeDebtState) => state.form?.data);
+  useDocumentTitle(location, formData);
+
+  useEffect(
+    () => {
+      if (userLoggedIn && isVerified) {
+        fetchDebts(dispatch);
+      }
+    },
+    [dispatch, userLoggedIn, isVerified],
+  );
+
+  // only need to show loading for debt pending if user is logged in
+  if ((userLoggedIn && isDebtPending) || isLoadingFeatures) {
+    return (
+      <va-loading-indicator
+        label="Loading"
+        message="Loading application..."
+        set-focus
+      />
+    );
+  }
+
+  return (
+    <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
+      <DowntimeNotification
+        appTitle="dispute debt system"
+        dependencies={formConfig.downtime.dependencies}
+      >
+        <I18nextProvider i18n={i18nDebtApp}>{children}</I18nextProvider>
+      </DowntimeNotification>
+    </RoutedSavableApp>
+  );
+}
+
+App.propTypes = {
+  children: PropTypes.element,
+  isDebtPending: PropTypes.bool,
+  location: PropTypes.object,
+};
