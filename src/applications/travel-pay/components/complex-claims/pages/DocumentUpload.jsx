3c6fc26b6c83a836d05570bca91fc971a1f02161
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2025-12-22 12:18:48 -0800

    [#128505] Travel Pay, GET expense (#41018)
    * Redux updates
    
    * Constants updates
    
    * More mock updates
    
    * Redux updates
    
    * Show whole page loader for fetching expense
    
    * Simplify redux structure for fetch
    
    * Expense page useEffect checkpoint
    
    * Const transportation options update, mock data fix
    
    * Add get expense call for isEditMode on ExpensePage
    
    * Clean up unused loading state
    
    * Update tests
    
    * Clean expenseType route matching
    
    * Revert constants change to avoid collision with another PR

--- src/applications/travel-pay/components/complex-claims/pages/DocumentUpload.jsx
+++ src/applications/travel-pay/components/complex-claims/pages/DocumentUpload.jsx
@@ -7,7 +7,6 @@ import { ACCEPTED_FILE_TYPES } from '../../../constants';
 const DocumentUpload = ({
   currentDocument,
   handleDocumentChange,
-  loading,
   uploadError,
 }) => {
   // Only pass a string if there is an error, otherwise undefined
@@ -15,29 +14,21 @@ const DocumentUpload = ({
 
   return (
     <>
-      {loading ? (
-        <va-loading-indicator
-          label="Loading"
-          message="Please wait while we load the expense receipt for you."
-          data-testid="travel-pay-document-loading-indicator"
-        />
-      ) : (
-        <VaFileInput
-          accept={ACCEPTED_FILE_TYPES.join(',')}
-          hint={`You can upload a ${ACCEPTED_FILE_TYPES.join(', ').replace(
-            /, ([^,]*)$/,
-            ', or $1',
-          )} file. Your file should be no larger than 5MB.`}
-          label="Select a file to upload"
-          maxFileSize={5200000}
-          minFileSize={0}
-          name="travel-pay-claim-document-upload"
-          onVaChange={handleDocumentChange}
-          required
-          error={errorMessage}
-          value={currentDocument}
-        />
-      )}
+      <VaFileInput
+        accept={ACCEPTED_FILE_TYPES.join(',')}
+        hint={`You can upload a ${ACCEPTED_FILE_TYPES.join(', ').replace(
+          /, ([^,]*)$/,
+          ', or $1',
+        )} file. Your file should be no larger than 5MB.`}
+        label="Select a file to upload"
+        maxFileSize={5200000}
+        minFileSize={0}
+        name="travel-pay-claim-document-upload"
+        onVaChange={handleDocumentChange}
+        required
+        error={errorMessage}
+        value={currentDocument}
+      />
 
       <va-additional-info trigger="How to upload paper copies">
         <p>
@@ -52,7 +43,6 @@ const DocumentUpload = ({
 DocumentUpload.propTypes = {
   currentDocument: PropTypes.object,
   handleDocumentChange: PropTypes.func,
-  loading: PropTypes.bool,
   uploadError: PropTypes.string,
 };
 
