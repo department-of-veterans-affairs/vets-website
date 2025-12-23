944007a543d22771f1edde8ff3b0167fdfbd9c3f
Joshua <joshing_you1@yahoo.com>
2025-12-23 15:29:18 -0500

    Fixed a failing test

--- src/applications/claims-status/tests/e2e/page-objects/TrackClaimsPageV2.js
+++ src/applications/claims-status/tests/e2e/page-objects/TrackClaimsPageV2.js
@@ -981,7 +981,7 @@ class TrackClaimsPageV2 {
   verifyUploadType2ErrorAlert() {
     cy.get('va-alert[status="error"]').should('be.visible');
     cy.get('va-alert[status="error"]')
-      .find('h3')
+      .find('h4')
       .should('contain', 'We need you to submit files by mail or in person');
   }
 
