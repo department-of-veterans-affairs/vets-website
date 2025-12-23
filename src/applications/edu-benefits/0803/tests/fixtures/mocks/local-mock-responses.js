da62593b6b8d3b8717d97a0eff12929f7a3c2cea
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2025-12-23 15:07:55 -0800

    implement contact info prefill pattern for 0803

--- src/applications/edu-benefits/0803/tests/fixtures/mocks/local-mock-responses.js
+++ src/applications/edu-benefits/0803/tests/fixtures/mocks/local-mock-responses.js
@@ -1,8 +1,14 @@
 // yarn mock-api --responses ./src/applications/{application}/tests/e2e/fixtures/mocks/local-mock-responses.js
-const mockUser = require('./user.json');
+const standardMocks = require('../../../../../personalization/profile/mocks/server');
+const prefillInfo = require('./prefill-info.json');
+const user = require('./user.json');
+const sip = require('./sip.json');
 
 const responses = {
-  'GET /v0/user': mockUser,
+  ...standardMocks,
+  'GET /v0/in_progress_forms/22-0803': prefillInfo,
+  'PUT /v0/in_progress_forms/22-0803': sip,
+  'GET /v0/user': user,
 };
 
 module.exports = responses;
