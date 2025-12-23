74416cc268c5a77fd88958cc4e1d84b95aa0028c
rilorie <warmwaffles123@gmail.com>
2025-12-23 11:50:17 -0800

    adding application

---
+++ src/applications/form-renderer/reducers/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+  // whatever initial state you have
+};
+
+function myNewApplication(state = initialState) {
+  return state;
+}
+
+export default {
+  myNewApplication,
+};
