df7eebd7297938c13e442a720bdea3130a27fdfc
rmessina1010 <rmessina10@gmail.com>
2025-12-23 10:35:34 -0600

    removes superflous tag and scrollTops

--- src/applications/ezr/components/ConfirmationPage/ConfirmationScreenView.jsx
+++ src/applications/ezr/components/ConfirmationPage/ConfirmationScreenView.jsx
@@ -2,15 +2,11 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { format } from 'date-fns';
 import { focusElement } from 'platform/utilities/ui';
-import { scrollToTop } from 'platform/utilities/scroll';
 import ApplicationDownloadLink from '../ApplicationDownloadLink';
 import content from '../../locales/en/content.json';
 
 const ConfirmationScreenView = ({ name, timestamp }) => {
-  useEffect(() => {
-    focusElement('.ezr-success-message');
-    scrollToTop();
-  }, []);
+  useEffect(() => focusElement('h2', {}, 'va-alert'), []);
 
   return (
     <>
--- src/applications/ezr/components/FormAlerts/SubmissionErrorAlert.jsx
+++ src/applications/ezr/components/FormAlerts/SubmissionErrorAlert.jsx
@@ -12,15 +12,13 @@ const SubmissionErrorAlert = () => {
     <div className="ezr-error-message vads-u-margin-top--5">
       <va-alert status="error" uswds>
         <h3 slot="headline">{content['alert-submission-title']}</h3>
-        <div>
-          <p>{content['alert-submission-primary-message']}</p>
-        </div>
+        <p>{content['alert-submission-primary-message']}</p>
       </va-alert>
       <va-card background="true" class="vads-u-margin-top--2">
         <h3 className="vads-u-margin-top--0 vads-u-font-size--h4">
           Other ways to update
         </h3>
-        <ul data-testid="hca-agreement-statements">
+        <ul data-testid="ezr-agreement-statements">
           <li>
             Call us at <va-telephone contact="8772228387" />, Monday through
             Friday, 8:00 a.m. to 8:00 p.m. ET.,
