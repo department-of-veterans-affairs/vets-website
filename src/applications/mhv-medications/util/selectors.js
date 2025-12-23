8257b1bc812ed8d3f62ba4ffcf7afc947721b8fc
Chris Donelson <cdonelson@ironarchtech.com>
2025-12-23 16:22:29 -0600

    removed V2 status mapping feature flag and commented out related tests

--- src/applications/mhv-medications/util/selectors.js
+++ src/applications/mhv-medications/util/selectors.js
@@ -28,5 +28,5 @@ export const selectSecureMessagingMedicationsRenewalRequestFlag = state =>
 export const selectCernerPilotFlag = state =>
   state.featureToggles[FEATURE_FLAG_NAMES.mhvMedicationsCernerPilot];
 
-export const selectV2StatusMappingFlag = state =>
-  state.featureToggles[FEATURE_FLAG_NAMES.mhvMedicationsV2StatusMapping];
+// Hardcoded to true temporarily until feature flag is added in separate PR
+export const selectV2StatusMappingFlag = () => true;
