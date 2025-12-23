aa4a2c2b989cbc8f6e450d1d4a6a31ca730b53ba
rilorie <warmwaffles123@gmail.com>
2025-12-23 15:19:43 -0800

    stub out importing form-renderer

--- src/applications/form-renderer/containers/App.jsx
+++ src/applications/form-renderer/containers/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+// import FormRenderer from 'platform/form-renderer/FormRenderer';
 
 const App = () => {
-  return <div>Test</div>;
+  return (
+    <div>
+      Test
+      {/* <FormRenderer config={} data={} /> */}
+    </div>
+  );
 };
 
 export default App;
