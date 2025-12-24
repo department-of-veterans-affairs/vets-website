ec04fc1af4da4d0b4b4a95b7c4b6539352aecc4b
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2025-12-24 08:45:55 -0800

    remove cross-application import

--- src/applications/edu-benefits/0803/tests/fixtures/mocks/local-mock-responses.js
+++ src/applications/edu-benefits/0803/tests/fixtures/mocks/local-mock-responses.js
@@ -1,11 +1,9 @@
 // yarn mock-api --responses ./src/applications/{application}/tests/e2e/fixtures/mocks/local-mock-responses.js
-const standardMocks = require('../../../../../personalization/profile/mocks/server');
 const prefillInfo = require('./prefill-info.json');
 const user = require('./user.json');
 const sip = require('./sip.json');
 
 const responses = {
-  ...standardMocks,
   'GET /v0/in_progress_forms/22-0803': prefillInfo,
   'PUT /v0/in_progress_forms/22-0803': sip,
   'GET /v0/user': user,
