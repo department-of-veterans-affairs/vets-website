9ceacda01497da31fbac916568e41fcf4fa7a1ec
Matt Long <6738544+longmd@users.noreply.github.com>
2025-12-26 13:56:47 -0500

    10-10EZR | Update financial info hint text

--- src/applications/ezr/definitions/veteranAnnualIncome.js
+++ src/applications/ezr/definitions/veteranAnnualIncome.js
@@ -31,7 +31,10 @@ export const VeteranAnnualIncomePage = () => ({
     'view:veteranGrossIncome': {
       ...inlineTitleUI(
         content['household-income-gross-title'],
-        content['household-income-gross-description'],
+        replaceStrValues(
+          content['household-income-gross-description'],
+          LAST_YEAR,
+        ),
       ),
       'ui:description': () => GrossIncomeDescription('veteran'),
       veteranGrossIncome: currencyUI(
@@ -44,7 +47,10 @@ export const VeteranAnnualIncomePage = () => ({
     'view:veteranNetIncome': {
       ...inlineTitleUI(
         content['household-income-net-title'],
-        content['household-income-net-description'],
+        replaceStrValues(
+          content['household-income-net-description'],
+          LAST_YEAR,
+        ),
       ),
       'ui:description': () => PreviousNetIncome('veteran'),
       veteranNetIncome: currencyUI(
@@ -57,7 +63,10 @@ export const VeteranAnnualIncomePage = () => ({
     'view:veteranOtherIncome': {
       ...inlineTitleUI(
         content['household-income-other-title'],
-        content['household-income-other-description'],
+        replaceStrValues(
+          content['household-income-other-description'],
+          LAST_YEAR,
+        ),
       ),
       'ui:description': () => OtherIncomeDescription('veteran'),
       veteranOtherIncome: currencyUI(
