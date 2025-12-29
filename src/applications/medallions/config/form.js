073e6bd006c58d0533d792ba87faab0f8ec40031
jordanBoardmanCodes <155088633+jordanBoardmanCodes@users.noreply.github.com>
2025-12-29 15:16:11 -0500

    MBMS-89062 initial commit

--- src/applications/medallions/config/form.js
+++ src/applications/medallions/config/form.js
@@ -336,6 +336,7 @@ const formConfig = {
           uiSchema: supportingDocuments.uiSchema,
           schema: supportingDocuments.schema,
         },
+        // FIX LIVES HERE
         supportingDocumentsUpload: {
           path: 'supporting-documents-upload',
           title: 'Supporting Documents',
