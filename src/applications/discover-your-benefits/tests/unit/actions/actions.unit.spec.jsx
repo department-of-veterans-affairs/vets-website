cf85b5f57fdf16188fe3779ff0d0b580931c4257
macalemid <mahmed@ironarchtech.com>
2025-12-31 12:56:53 -0500

    combined burial benefits

--- src/applications/discover-your-benefits/tests/unit/actions/actions.unit.spec.jsx
+++ src/applications/discover-your-benefits/tests/unit/actions/actions.unit.spec.jsx
@@ -1468,11 +1468,7 @@ describe('actions', () => {
     validGoals.forEach(goal => {
       const formData = {
         [mappingTypes.GOALS]: { [goal]: true },
-        [mappingTypes.TITLE_TEN_ACTIVE_DUTY]: true,
         [mappingTypes.CHARACTER_OF_DISCHARGE]: formatData(validDischarge),
-        [militaryBranchTypes.ARMY]: {
-          [militaryBranchComponentTypes.ACTIVE_DUTY]: true,
-        },
       };
       it(`should return true with goal: ${goal}`, () => {
         const result = actions.mapBenefitFromFormInputData(benefit, formData);
@@ -1484,7 +1480,6 @@ describe('actions', () => {
       const formData = {
         [mappingTypes.GOALS]: formatData(validGoals),
         [mappingTypes.CHARACTER_OF_DISCHARGE]: discharge,
-        [mappingTypes.TITLE_TEN_ACTIVE_DUTY]: true,
       };
       it(`should return true with discharge: ${discharge}`, () => {
         const result = actions.mapBenefitFromFormInputData(benefit, formData);
@@ -1496,7 +1491,6 @@ describe('actions', () => {
       const formData = {
         [mappingTypes.GOALS]: formatData(invalidGoals),
         [mappingTypes.CHARACTER_OF_DISCHARGE]: formatData(validDischarge),
-        [mappingTypes.TITLE_TEN_ACTIVE_DUTY]: true,
       };
       const result = actions.mapBenefitFromFormInputData(benefit, formData);
       expect(result).to.be.false;
@@ -1506,21 +1500,6 @@ describe('actions', () => {
       const formData = {
         [mappingTypes.GOALS]: formatData(validGoals),
         [mappingTypes.CHARACTER_OF_DISCHARGE]: formatData(invalidDischarge),
-        [mappingTypes.TITLE_TEN_ACTIVE_DUTY]: true,
-      };
-      const result = actions.mapBenefitFromFormInputData(benefit, formData);
-      expect(result).to.be.false;
-    });
-
-    it('should return false with no title ten and no active duty', () => {
-      const formData = {
-        [mappingTypes.GOALS]: formatData(validGoals),
-        [mappingTypes.CHARACTER_OF_DISCHARGE]: formatData(validDischarge),
-        [mappingTypes.BRANCH_COMPONENT.TITLE_TEN_ACTIVE_DUTY]: false,
-        [militaryBranchTypes.ARMY]: {
-          [militaryBranchComponentTypes.NATIONAL_GUARD_SERVICE]: true,
-          [militaryBranchComponentTypes.RESERVE_SERVICE]: true,
-        },
       };
       const result = actions.mapBenefitFromFormInputData(benefit, formData);
       expect(result).to.be.false;
