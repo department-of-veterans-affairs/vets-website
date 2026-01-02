fb29157510b8016f80685a5c4b1377b4d9f89fde
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2026-01-02 10:38:22 -0800

    make textarea conditionally shown on acknowledgement 4

--- src/applications/edu-benefits/0976/pages/acknowledgement4.js
+++ src/applications/edu-benefits/0976/pages/acknowledgement4.js
@@ -6,6 +6,7 @@ import {
   yesNoUI,
   yesNoSchema,
   titleUI,
+  descriptionUI,
 } from 'platform/forms-system/src/js/web-component-patterns';
 
 /** @type {PageSchema} */
@@ -13,28 +14,43 @@ export default {
   uiSchema: {
     acknowledgement10a: {
       ...titleUI('Institution Acknowledgements (4 of 5)'),
+      ...descriptionUI(
+        <div>
+          <p>
+            The facility is financially sound and fully capable of meeting its
+            obligations related to training activities. As part of this
+            application, the facility agrees to submit financial documentation
+            that substantiates its financial stability.(Examples of
+            documentation may include copies of tax returns, or financial
+            reports that reports the financial position of the institution or
+            establishment, as prepared by any appropriate third-party entity.
+            New facilities are requested to submit 24 months of financial data
+            to determine financial soundness. Please contact your Education
+            Liaison Representative (ELR)/State Approving Agency (SAA) for
+            additional guidance if needed).
+          </p>
+          <p>
+            Note: USE Department of Education, Title IV school participants are
+            not required to provide financial statement with the application.
+          </p>
+        </div>,
+      ),
       financiallySound: {
         ...yesNoUI({
-          title:
-            'The facility is financially sound and fully capable of meeting its obligations related to training activities. As part of this application, the facility agrees to submit financial documentation that substantiates its financial stability.(Examples of documentation may include copies of tax returns, or  financial reports that reports the financial position of the institution or establishment, as prepared by any appropriate third-party entity. New facilities are requested to  submit 24 months of financial data to determine financial soundness. Please contact your Education Liaison Representative (ELR)/State Approving Agency (SAA) for  additional guidance if needed).',
-          description: (
-            <p>
-              <strong>Note:</strong> USE Department of Education, Title IV
-              school participants are not required to provide financial
-              statement with the application.
-            </p>
-          ),
+          hideLabelText: true,
           labels: {
             Y:
               'Yes, the institution is capable of fulfilling its commitments for training.',
             N:
               'No, the institution is not capable of fulfilling its commitments for training.',
           },
+          errorMessages: {
+            required: 'You must make a selection.',
+          },
         }),
       },
       financialSoundnessExplanation: {
         ...textareaUI({
-          hideLabelText: true,
           required: formData =>
             formData.acknowledgement10a?.financiallySound === false,
           errorMessages: {
@@ -42,6 +58,12 @@ export default {
               'You must specify a reason the institution is not capable of fulfilling its commitments for training.',
           },
         }),
+        'ui:options': {
+          charcount: true,
+          hideLabelText: true,
+          expandUnder: 'financiallySound',
+          expandUnderCondition: false,
+        },
       },
     },
   },
