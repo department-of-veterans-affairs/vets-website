2b55d81e97a1af332c7b927dce212f058767fd17
Thomas Krenzke <196683837+thomaskrenzke@users.noreply.github.com>
2026-01-02 10:44:04 -0800

    VEBT-3792 Form 0976 step 2 part 1 (#41134)
    * 0976 page 1 step 1
    
    * update need help component of 0976
    
    * 0976 what to expect page
    
    * form 0976 first acknowledgement page
    
    * a couple more acknowledgement pages for 0976
    
    * fourth acknowledgement page for 0976
    
    * fifth acknowledgement page for 0976
    
    * use custom component for initials input to make sure its uppercase
    
    * refactor acknowledgement 1 to use same components as other steps
    
    * make sure link is external within form page
    
    * has va facility code page
    
    * 0976 form facility code lookup for main institution
    
    * remove unneeded additional-info box and add test coverage
    
    * undo bad merge commit
    
    * fix typo
    
    * change page path in 0976 form config

--- src/applications/edu-benefits/0976/tests/fixtures/mocks/local-mock-responses.js
+++ src/applications/edu-benefits/0976/tests/fixtures/mocks/local-mock-responses.js
@@ -1,8 +1,14 @@
 // yarn mock-api --responses ./src/applications/{application}/tests/e2e/fixtures/mocks/local-mock-responses.js
 const mockUser = require('./user.json');
+const institutionSuccess = require('./institution-facility-code-success.json');
+const institutionFail = require('./institution-facility-code-fail.json');
 
 const responses = {
   'GET /v0/user': mockUser,
+  'GET /v0/gi/institutions/12345678': institutionSuccess,
+  'GET /v0/gi/institutions/87654321': (_req, res) => {
+    res.status(404).json(institutionFail);
+  },
 };
 
 module.exports = responses;
