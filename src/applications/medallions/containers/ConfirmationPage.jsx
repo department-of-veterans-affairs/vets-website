7ca7e7cfe152e37525bacac33474f503de6c04df
Adam Antonioli <132934480+adam-antonioli@users.noreply.github.com>
2026-01-05 16:05:09 -0500

    MBMS-85949 copy pre-need integration confirmation screen to medallions

--- src/applications/medallions/containers/ConfirmationPage.jsx
+++ src/applications/medallions/containers/ConfirmationPage.jsx
@@ -1,101 +1,112 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { format, isValid } from 'date-fns';
 import { useSelector } from 'react-redux';
-import { waitForRenderThenFocus } from 'platform/utilities/ui/focus';
-import { scrollTo } from 'platform/utilities/scroll';
 
-export const ConfirmationPage = () => {
-  const alertRef = useRef(null);
+import { scrollToTop } from 'platform/utilities/scroll';
+import { focusElement } from 'platform/utilities/ui';
+import { ConfirmationView } from 'platform/forms-system/src/js/components/ConfirmationView';
+import { CONTACTS } from '@department-of-veterans-affairs/component-library/contacts';
+
+const HowToContactContent = () => (
+  <>
+    <p>
+      Call us at <va-telephone contact="8005351117" /> (
+      <va-telephone tty="true" contact={CONTACTS[711]} />
+      ). We’re here Monday through Friday, 8:00 a.m. to 7:30 p.m. ET, and
+      Saturday 9:00 a.m. to 5:30 p.m. ET.
+    </p>
+    <p>
+      Or, check our resources and support section for answers to common
+      questions.
+    </p>
+    <p>
+      <va-link
+        external
+        href="https://ask.va.gov/"
+        text="Go to resources and support section on VA.gov"
+      />
+    </p>
+  </>
+);
+
+const ConfirmationPage = props => {
   const form = useSelector(state => state.form || {});
-  const { submission, formId, data = {} } = form;
-  const { fullName } = data;
-  const submitDate = submission?.timestamp;
-  const confirmationNumber = submission?.response?.confirmationNumber;
+  const { submission } = form;
+  const { formConfig } = props?.route;
+  const submitDate = submission?.timestamp || submission?.submittedAt;
+  const confirmationNumber = submission?.response?.confirmationNumber || '';
 
-  useEffect(
-    () => {
-      if (alertRef?.current) {
-        scrollTo('topScrollElement');
-        waitForRenderThenFocus('h2', alertRef.current);
-      }
-    },
-    [alertRef],
-  );
+  useEffect(() => {
+    scrollToTop('topScrollElement');
+    focusElement('.confirmation-page-title');
+  }, []);
 
   return (
-    <div>
-      <div className="print-only">
-        <img
-          src="https://www.va.gov/img/design/logo/logo-black-and-white.png"
-          alt="VA logo"
-          width="300"
+    <ConfirmationView
+      submitDate={submitDate}
+      confirmationNumber={confirmationNumber}
+      formConfig={formConfig}
+      pdfUrl={
+        submission?.response?.pdfUrl ||
+        'https://www.va.gov/vaforms/va/pdf/VA%20Form%2040-10007.pdf'
+      }
+    >
+      <ConfirmationView.SubmissionAlert
+        title="You’ve submitted your application"
+        content="You’ll receive a confirmation email shortly. We’ll let you know by mail or phone if we need more details."
+        actions={null}
+      />
+      <ConfirmationView.SavePdfDownload
+        title="Save a copy of your application"
+        content="If you’d like a PDF copy of your completed application, you can download it."
+      />
+      <ConfirmationView.ChapterSectionCollection header="Information you submitted on this application" />
+      <ConfirmationView.PrintThisPage />
+      <ConfirmationView.WhatsNextProcessList
+        item1Header="We’ll review your application"
+        item1Content="If we need more information about your information, we’ll contact you."
+        item1Actions={null}
+        item2Header="We’ll reach a decision"
+        item2Content="You’ll receive a pre-need decision letter notifying you of your eligibility for burial in a VA national cemetery."
+      />
+      <h2>If you need to submit supporting documents</h2>
+      <p className="mail-or-fax-message">
+        You can mail your supporting documents to this address:
+      </p>
+      <p className="va-address-block">
+        NCA FP Evidence Intake Center <br />
+        P.O. Box 5237
+        <br />
+        Janesville, WI 53547
+        <br />
+      </p>
+      <p>
+        Or, you can fax your supporting documents to{' '}
+        <va-telephone contact="8558408299" />.
+      </p>
+      <p>
+        <strong>Note: </strong>
+        Don’t submit your original documents. We can’t return them. Submit
+        copies of your documents only.
+      </p>
+      <p>
+        <va-link
+          external
+          href="https://www.cem.va.gov/hmm/discharge_documents.asp"
+          text="Learn more about the supporting documents you can submit"
         />
-      </div>
-
-      <va-alert status="success" ref={alertRef}>
-        <h2 slot="headline">Your application has been submitted</h2>
-      </va-alert>
-
-      <p>We may contact you for more information or documents.</p>
-      <p className="screen-only">Please print this page for your records.</p>
-      <div className="inset">
-        <h3 className="vads-u-margin-top--0 vads-u-font-size--h4">
-          1330M Apply for a medallion in a private cemetery Claim{' '}
-          <span className="vads-u-font-weight--normal">(Form {formId})</span>
-        </h3>
-        {fullName ? (
-          <span>
-            for {fullName.first} {fullName.middle} {fullName.last}
-            {fullName.suffix ? `, ${fullName.suffix}` : null}
-          </span>
-        ) : null}
-
-        {confirmationNumber ? (
-          <>
-            <h4>Confirmation number</h4>
-            <p>{confirmationNumber}</p>
-          </>
-        ) : null}
-
-        {isValid(submitDate) ? (
-          <p>
-            <strong>Date submitted</strong>
-            <br />
-            <span>{format(submitDate, 'MMMM d, yyyy')}</span>
-          </p>
-        ) : null}
-
-        <va-button onClick={window.print} text="Print this for your records" />
-      </div>
-      <a className="vads-c-action-link--green vads-u-margin-bottom--4" href="/">
-        Go back to VA.gov
-      </a>
-
-      {/* <div className="help-footer-box">
-        <h2 className="help-heading">Need help?</h2>
-        <GetFormHelp />
-      </div> */}
-    </div>
+      </p>
+      <ConfirmationView.HowToContact content={<HowToContactContent />} />
+      <ConfirmationView.GoBackLink />
+    </ConfirmationView>
   );
 };
 
 ConfirmationPage.propTypes = {
-  form: PropTypes.shape({
-    data: PropTypes.shape({
-      fullName: {
-        first: PropTypes.string,
-        middle: PropTypes.string,
-        last: PropTypes.string,
-        suffix: PropTypes.string,
-      },
-    }),
-    formId: PropTypes.string,
-    submission: PropTypes.shape({
-      timestamp: PropTypes.string,
-    }),
+  route: PropTypes.shape({
+    formConfig: PropTypes.object,
   }),
-  name: PropTypes.string,
 };
 
 export default ConfirmationPage;
+export { ConfirmationPage };
