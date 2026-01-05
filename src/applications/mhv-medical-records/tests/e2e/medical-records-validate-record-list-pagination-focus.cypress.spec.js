ad2753508491bb1ba02596a2b224d32613f25681
Ni Chia <ni.chia@adhocteam.us>
2026-01-05 16:51:34 -0700

    scroll pagination into view before clicking in focus test

--- src/applications/mhv-medical-records/tests/e2e/medical-records-validate-record-list-pagination-focus.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-validate-record-list-pagination-focus.cypress.spec.js
@@ -24,8 +24,9 @@ describe('Medical Records View Vaccines', () => {
     // With no page specified, default is page 1 but focus should be on <h1>
     cy.get('h1').should('be.focused');
 
-    // Wait for pagination to be visible before interacting
-    cy.get('va-pagination').should('exist');
+    // Scroll pagination into view and wait for it to be visible before interacting
+    cy.get('va-pagination').scrollIntoView();
+    cy.get('va-pagination').should('be.visible');
 
     // Click page 2 in the pagination
     cy.get('va-pagination')
