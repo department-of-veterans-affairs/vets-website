cab4b1046b7c0895b155bb004a2a0951a67292bf
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2026-01-06 11:59:23 -0800

    Fix CernerFacilityAlert To at the start of title and make mocks work with new alerts for VAOS

--- src/applications/vaos/services/mocks/index.js
+++ src/applications/vaos/services/mocks/index.js
@@ -89,6 +89,11 @@ const MockReferralSubmitAppointmentResponse = require('../../tests/fixtures/Mock
 // Returns the meta object without any backend service errors
 const meta = require('./v2/meta.json');
 const features = require('./featureFlags');
+const {
+  // defaultUser,
+  // acceleratedCernerUser,
+  cernerUser,
+} = require('../../../../platform/mhv/api/mocks/user');
 
 const mockAppts = [];
 let currentMockId = 1;
@@ -681,32 +686,7 @@ const responses = {
           'rx',
           'messaging',
         ],
-        va_profile: {
-          status: 'OK',
-          birth_date: '19511118',
-          family_name: 'Hunter',
-          gender: 'M',
-          given_names: ['Julio', 'E'],
-          active_status: 'active',
-          facilities: [
-            {
-              facility_id: '556',
-              is_cerner: false,
-            },
-            {
-              facility_id: '984',
-              is_cerner: false,
-            },
-            {
-              facility_id: '983',
-              is_cerner: false,
-            },
-            {
-              facility_id: '692',
-              is_cerner: true,
-            },
-          ],
-        },
+        vaProfile: cernerUser.data.attributes.vaProfile,
         vet360ContactInformation: {
           email: {
             createdAt: '2018-04-20T17:24:13.000Z',
