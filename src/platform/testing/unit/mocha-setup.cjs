861ba68ee0d6d68269b2dbc9a690b7c2d02addfb
Isaac Gallup <igallup@thesocompany.com>
2026-01-07 11:15:07 -0600

    missing platform-level mocha-setup.cjs file

---
+++ src/platform/testing/unit/mocha-setup.cjs
@@ -0,0 +1,169 @@
+/**
+ * Global set up code for the Mocha unit testing environment
+ *
+ * If you're looking to add polyfills for all unit tests, this is the place.
+ */
+if (globalThis.window?.navigator) {
+  Object.defineProperty(globalThis.window.navigator, 'userAgent', {
+    configurable: true,
+    get() { return 'Mozilla/5.0 (Node; HappyDOM)'; },
+  });
+  if (!('languages' in globalThis.window.navigator)) {
+    Object.defineProperty(globalThis.window.navigator, 'languages', {
+      configurable: true,
+      get() { return ['en-US', 'en']; },
+    });
+  }
+}
+
+const os = require('os');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const chaiDOM = require('chai-dom');
+const { Window } = require('happy-dom');
+require('../../site-wide/moment-setup');
+const ENVIRONMENTS = require('../../../site/constants/environments');
+const Sentry = require('@sentry/browser');
+const { configure } = require('@testing-library/dom');
+const chaiAxe = require('./axe-plugin');
+const { sentryTransport } = require('./sentry.cjs');
+
+const isStressTest = process.env.IS_STRESS_TEST || 'false';
+const DISALLOWED_SPECS = process.env.DISALLOWED_TESTS || [];
+Sentry.init({
+  autoSessionTracking: false,
+  dsn: 'http://one@fake/dsn/0',
+  transport: sentryTransport,
+});
+
+configure({ defaultHidden: true });
+
+global.__BUILDTYPE__ = process.env.BUILDTYPE || ENVIRONMENTS.VAGOVDEV;
+global.__API__ = null;
+global.__MEGAMENU_CONFIG__ = null;
+global.__REGISTRY__ = [];
+
+chai.use(chaiAsPromised);
+chai.use(chaiDOM);
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target),
+  });
+}
+
+function filterStackTrace(trace) {
+  return trace
+    .split(os.EOL)
+    .filter(line => !line.includes('node_modules'))
+    .join(os.EOL);
+}
+
+function resetFetch() {
+  if (global.fetch.isSinonProxy) {
+    global.fetch.restore();
+  }
+}
+
+function initHappyDom() {
+  // Prevent warnings from displaying
+  /* eslint-disable no-console */
+  if (process.env.LOG_LEVEL === 'debug') {
+    console.error = (error, reactError) => {
+      if (reactError instanceof Error) {
+        console.log(filterStackTrace(reactError.stack));
+      } else if (error instanceof Response) {
+        console.log(`Error ${error.status}: ${error.url}`);
+      } else if (error instanceof Error) {
+        console.log(filterStackTrace(error.stack));
+      } else if (error?.includes?.('The above error occurred')) {
+        console.log(error);
+      }
+    };
+    console.warn = () => {};
+  } else if (process.env.LOG_LEVEL === 'log') {
+    console.error = () => {};
+    console.warn = () => {};
+  }
+  const window = new Window({
+    url: 'http://localhost',
+  });
+  const { document } = window;
+
+  global.window = window;
+  global.document = document;
+  // global.navigator = {
+  //   userAgent: 'node.js'
+  // }
+
+  global.requestAnimationFrame = function(callback) {
+    return setTimeout(callback, 0);
+  };
+  global.cancelAnimationFrame = function(id) {
+    clearTimeout(id);
+  };
+  global.Blob = window.Blob;
+
+  /* sets up `window` for testing */
+  const scroll = { duration: 0, delay: 0, smooth: false };
+  window.dataLayer = [];
+  window.matchMedia = () => ({
+    matches: false,
+    addListener: f => f,
+    removeListener: f => f,
+  });
+  window.scrollTo = () => {};
+  window.VetsGov = { scroll };
+  window.Forms = { scroll };
+  window.getSelection = () => '';
+  window.Mocha = true;
+
+  copyProps(window, global);
+}
+
+initHappyDom();
+
+const checkAllowList = testContext => {
+  const file = testContext.currentTest.file.slice(
+    testContext.currentTest.file.indexOf('src'),
+  );
+  if (DISALLOWED_SPECS.indexOf(file) > -1 && file.includes('src')) {
+    /* eslint-disable-next-line no-console */
+    console.log('Test skipped due to flakiness: ', file);
+    testContext.skip();
+  }
+};
+// This needs to be after JSDom has been setup, otherwise
+// axe has strange issues with globals not being set up
+chai.use(chaiAxe);
+
+const cleanupStorage = () => {
+  localStorage.clear();
+  sessionStorage.clear();
+};
+
+function flushPromises() {
+  return new Promise(resolve => setImmediate(resolve));
+}
+
+exports.mochaHooks = {
+  beforeEach() {
+    initHappyDom();
+    resetFetch();
+    cleanupStorage();
+    if (isStressTest == 'false') {
+      checkAllowList(this);
+    }
+    if (process.env.CI || ['trace', 'debug'].includes(process.env.LOG_LEVEL)) {
+      console.log(
+        'running: ',
+        this.currentTest.file.slice(this.currentTest.file.indexOf('src')),
+      );
+    }
+  },
+  afterEach() {
+    cleanupStorage();
+    flushPromises();
+  },
+};
