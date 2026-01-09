72bbc88b4b737d8c3501e46c69349ac81f12e494
Daniel Gading <2748839+dgading@users.noreply.github.com>
2026-01-09 14:55:36 -0500

    Fill out middle initial truncate and add tests

--- src/applications/survivors-benefits/utils/transformers/truncateMiddleInitials.js
+++ src/applications/survivors-benefits/utils/transformers/truncateMiddleInitials.js
@@ -1,5 +1,15 @@
-export function splitVaSsnField(formData) {
+export function truncateMiddleInitials(formData) {
   const parsedFormData = JSON.parse(formData);
   const transformedValue = parsedFormData;
+  if (parsedFormData?.veteranFullName?.middle) {
+    transformedValue.veteranFullName.middle = parsedFormData?.veteranFullName?.middle.charAt(
+      0,
+    );
+  }
+  if (parsedFormData?.claimantFullName?.middle) {
+    transformedValue.claimantFullName.middle = parsedFormData?.claimantFullName?.middle.charAt(
+      0,
+    );
+  }
   return JSON.stringify(transformedValue);
 }
