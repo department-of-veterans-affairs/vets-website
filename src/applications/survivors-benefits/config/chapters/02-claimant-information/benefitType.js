4480dbc81c10a31c2f8499fb30c7201d69eb3f55
Daniel Gading <2748839+dgading@users.noreply.github.com>
2026-01-09 09:53:00 -0500

    Fix unit test and schema after merge conflict

--- src/applications/survivors-benefits/config/chapters/02-claimant-information/benefitType.js
+++ src/applications/survivors-benefits/config/chapters/02-claimant-information/benefitType.js
@@ -23,7 +23,7 @@ export default {
     required: [],
     properties: {
       claims: checkboxGroupSchema([
-        'DIC',
+        'dependencyIndemnityComp',
         'survivorsPension',
         'accruedBenefits',
       ]),
