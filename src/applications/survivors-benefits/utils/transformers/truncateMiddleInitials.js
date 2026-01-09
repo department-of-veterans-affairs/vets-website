94a5e640f7e4efa7b3b62722f59e97332983021a
Daniel Gading <2748839+dgading@users.noreply.github.com>
2026-01-09 12:54:41 -0500

    21P-534ez add new middle initial submit transformer

---
+++ src/applications/survivors-benefits/utils/transformers/truncateMiddleInitials.js
@@ -0,0 +1,5 @@
+export function splitVaSsnField(formData) {
+  const parsedFormData = JSON.parse(formData);
+  const transformedValue = parsedFormData;
+  return JSON.stringify(transformedValue);
+}
