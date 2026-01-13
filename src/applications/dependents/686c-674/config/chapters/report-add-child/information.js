a86ccaf6c400e5e7b58a6bb1f2b3c01f8ae19fa7
aafmisra <aafmisra@gmail.com>
2026-01-13 17:01:36 -0500

    no SSN functionality to add-child flow

--- src/applications/dependents/686c-674/config/chapters/report-add-child/information.js
+++ src/applications/dependents/686c-674/config/chapters/report-add-child/information.js
@@ -1,9 +1,16 @@
+import React from 'react';
 import {
   fullNameNoSuffixUI,
   fullNameNoSuffixSchema,
   currentOrPastDateUI,
   currentOrPastDateSchema,
   arrayBuilderItemFirstPageTitleUI,
+  checkboxUI,
+  checkboxSchema,
+  radioUI,
+  radioSchema,
+  ssnUI,
+  ssnSchema,
 } from 'platform/forms-system/src/js/web-component-patterns';
 import { arrayBuilderOptions } from './config';
 
@@ -13,19 +20,56 @@ export const information = {
       title: 'Add a child',
       nounSingular: arrayBuilderOptions.nounSingular,
     }),
+    'view:childNameTitle': {
+      'ui:description': <h4>Child’s name</h4>,
+    },
     fullName: fullNameNoSuffixUI(title => `Child’s ${title}`),
     birthDate: currentOrPastDateUI({
       title: 'Child’s date of birth',
+      labelHeaderLevel: '4',
       dataDogHidden: true,
       required: () => true,
     }),
+    'view:childIdTitle': {
+      'ui:description': <h4>Child’s identification information</h4>,
+    },
+    noSSN: checkboxUI({
+      title: 'Child doesn’t have a Social Security number',
+      required: () => false,
+      hideIf: formData => !formData?.vaDependentsNoSSN, // check feature flag
+    }),
+    noSSNReason: radioUI({
+      title: 'Why doesn’t your child have a Social Security number?',
+      labels: {
+        NONRESIDENT_ALIEN: 'Nonresident alien',
+        NONE_ASSIGNED: 'No SSN has been assigned or requested',
+      },
+      required: (_chapterData, _index, formData) =>
+        formData?.childrenToAdd[_index]?.noSSN === true,
+      hideIf: (formData, _index) =>
+        formData?.childrenToAdd[_index]?.noSSN !== true,
+    }),
+    ssn: {
+      ...ssnUI('Child’s Social Security number'),
+      'ui:required': (_chapterData, _index, formData) =>
+        formData?.childrenToAdd[_index]?.noSSN === false,
+      'ui:options': {
+        hideIf: (formData, _index) =>
+          formData?.childrenToAdd[_index]?.noSSN === true,
+      },
+    },
   },
   schema: {
     type: 'object',
     required: ['fullName', 'birthDate'],
     properties: {
+      'view:childNameTitle': { type: 'object', properties: {} },
       fullName: fullNameNoSuffixSchema,
       birthDate: currentOrPastDateSchema,
+      'view:childIdTitle': { type: 'object', properties: {} },
+      noSSN: checkboxSchema,
+      noSSNReason: radioSchema(['NONRESIDENT_ALIEN', 'NONE_ASSIGNED']),
+      ssn: ssnSchema,
     },
   },
 };
