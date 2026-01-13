ebf02fb1a977386627ff4506976a9c00842efa4e
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2026-01-13 10:02:26 -0800

    Changed useOHDirectScheduling hook, removes clinicalPharmacyPrimaryCare from hook
    clinicalPharmacyPrimaryCare is already present in the constants array that is checked so we don’t need to include it here.
    
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>

--- src/applications/vaos/new-appointment/hooks/useOHDirectScheduling.js
+++ src/applications/vaos/new-appointment/hooks/useOHDirectScheduling.js
@@ -15,10 +15,9 @@ export function useOHDirectScheduling() {
     selectFeatureRemoveFacilityConfigCheck,
   );
 
-  // NOTE: Add 'clinicalPharmacyPrimaryCare' to OH_ENABLED_TYPES_OF_CARE array when flag is removed.
-  return featureOHDirectSchedule && featureRemoveFacilityConfigCheck
-    ? [...OH_ENABLED_TYPES_OF_CARE, 'clinicalPharmacyPrimaryCare'].includes(
-        typeOfCare.idV2,
-      )
-    : OH_ENABLED_TYPES_OF_CARE.includes(typeOfCare.idV2);
+  return (
+    featureOHDirectSchedule &&
+    featureRemoveFacilityConfigCheck &&
+    OH_ENABLED_TYPES_OF_CARE.includes(typeOfCare.idV2)
+  );
 }
