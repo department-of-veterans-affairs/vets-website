bde85064bbfa0a20aac60161f8ce4349e820e922
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2026-01-13 10:05:29 -0800

    Adds more unit tests to useOHDirectScheduling hook
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>

--- src/applications/vaos/new-appointment/hooks/useOHDirectScheduling.unit.spec.jsx
+++ src/applications/vaos/new-appointment/hooks/useOHDirectScheduling.unit.spec.jsx
@@ -29,4 +29,55 @@ describe('VAOS Hook: useOHDirectScheduling', () => {
 
     expect(result.current).to.be.false;
   });
+
+  it('should return false when typeOfCare is not in OH_ENABLED_TYPES_OF_CARE', () => {
+    useSelectorStub.onCall(0).returns(true); // featureOHDirectSchedule
+    useSelectorStub.onCall(1).returns({}); // data
+    getTypeOfCareStub.returns({ idV2: 'primaryCare' });
+
+    const { result } = renderHook(() => useOHDirectScheduling());
+
+    expect(result.current).to.be.false;
+  });
+
+  it('should return true when feature flag is enabled and typeOfCare is foodAndNutrition', () => {
+    useSelectorStub.onCall(0).returns(true); // featureOHDirectSchedule
+    useSelectorStub.onCall(1).returns({}); // data
+    getTypeOfCareStub.returns({ idV2: 'foodAndNutrition' });
+
+    const { result } = renderHook(() => useOHDirectScheduling());
+
+    expect(result.current).to.be.true;
+  });
+
+  it('should return false when typeOfCare is undefined', () => {
+    useSelectorStub.onCall(0).returns(true); // featureOHDirectSchedule
+    useSelectorStub.onCall(1).returns({}); // data
+    getTypeOfCareStub.returns(undefined);
+
+    const { result } = renderHook(() => useOHDirectScheduling());
+
+    expect(result.current).to.be.false;
+  });
+
+  it('should return false when typeOfCare.idV2 is undefined', () => {
+    useSelectorStub.onCall(0).returns(true); // featureOHDirectSchedule
+    useSelectorStub.onCall(1).returns({}); // data
+    getTypeOfCareStub.returns({ idV2: undefined });
+
+    const { result } = renderHook(() => useOHDirectScheduling());
+
+    expect(result.current).to.be.false;
+  });
+
+  it('should call getTypeOfCare with the form data', () => {
+    const mockData = { typeOfCareId: '123' };
+    useSelectorStub.onCall(0).returns(true); // featureOHDirectSchedule
+    useSelectorStub.onCall(1).returns(mockData); // data
+    getTypeOfCareStub.returns({ idV2: 'foodAndNutrition' });
+
+    renderHook(() => useOHDirectScheduling());
+
+    expect(getTypeOfCareStub.calledWith(mockData)).to.be.true;
+  });
 });
