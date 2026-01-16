959b8b1e42f91f708db2b0107b3ccb54f4cdaf15
dustin-cintron <D.Cint88@gmail.com>
2026-01-15 20:52:00 -0500

    Redirect will not occur on local

--- src/applications/simple-forms/mock-form-prefill/containers/App.jsx
+++ src/applications/simple-forms/mock-form-prefill/containers/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
+import environment from 'platform/utilities/environment';
 import manifest from '../manifest.json';
 import formConfig from '../config/form';
 
@@ -9,8 +10,12 @@ function App({ location, children, isLoggedIn, formData }) {
   const isIntroPage = location.pathname === '/introduction';
   const noSaveInProgressData = !formData?.ssn;
 
-  // Redirect to introduction page if not logged in or no ssn
-  if (!isIntroPage && (!isLoggedIn || (isLoggedIn && noSaveInProgressData))) {
+  // Redirect to introduction page if not logged in or no ssn on higher envs
+  if (
+    !environment.isLocalhost() &&
+    !isIntroPage &&
+    (!isLoggedIn || (isLoggedIn && noSaveInProgressData))
+  ) {
     document.location.replace(manifest.rootUrl);
     return (
       <va-loading-indicator message="Redirecting to introduction page..." />
