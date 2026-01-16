ba35d8e6aa3d5c5a0319b7a8dd8f4eda6d3d20bc
govcioChad <162637891+govcioChad@users.noreply.github.com>
2026-01-16 12:59:41 -0500

    Vebt 2545 Added step 3 person information (#41447)
    * #vebt-2545 #comment Added third party person name and address pages
    
    * #vebt-2545 #comment Added unit test
    
    * Attempting to revert
    
    * Reverting changes
    
    * Reverting changes
    
    * Reverting changes
    
    * Reverting changes

---
+++ src/applications/edu-benefits/10278/tests/pages/thirdPartyPersonName.unit.spec.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { expect } from 'chai';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import { DefinitionTester } from 'platform/testing/unit/schemaform-utils';
+import { $$ } from 'platform/forms-system/src/js/utilities/ui';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import formConfig from '../../config/form';
+
+describe('Authorizing official page', () => {
+  const {
+    schema,
+    uiSchema,
+  } = formConfig.chapters.thirdPartyContactInformation.pages.thirdPartyPersonName;
+
+  const mockStore = configureStore();
+
+  it('Renders the page with the correct number of inputs', async () => {
+    const renderPage = (formData = { thirdPartyPersonName: {} }) => {
+      const store = mockStore({ form: { data: formData } });
+      return render(
+        <Provider store={store}>
+          <DefinitionTester
+            definitions={formConfig.defaultDefinitions}
+            schema={schema}
+            uiSchema={uiSchema}
+            data={formData}
+          />
+        </Provider>,
+      );
+    };
+
+    const { container, getByRole } = renderPage();
+
+    expect($$('va-text-input', container).length).to.equal(3);
+
+    fireEvent.click(getByRole('button', { name: /submit/i }));
+
+    await waitFor(() => {
+      expect($$('va-text-input[error]', container).length).to.equal(2);
+    });
+  });
+});
