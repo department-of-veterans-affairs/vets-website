77dd1a6976133ae2e291c597cac69a4f8f77336f
Simi Adebowale <47654119+simiadebowale@users.noreply.github.com>
2026-01-21 10:43:53 -0600

    uncomment-mocks

--- src/applications/vaos/services/mocks/index.js
+++ src/applications/vaos/services/mocks/index.js
@@ -19,8 +19,8 @@ const { getMockSlots } = require('./utils/slots');
 // v2
 const ccProviders = require('./v2/cc_providers.json');
 const facilitiesV2 = require('./v2/facilities.json');
-const schedulingConfigurationsCC = require('./v2/scheduling_configurations_cc.json');
-const schedulingConfigurations = require('./v2/scheduling_configurations.json');
+// const schedulingConfigurationsCC = require('./v2/scheduling_configurations_cc.json');
+// const schedulingConfigurations = require('./v2/scheduling_configurations.json');
 
 // Generate dynamic slots with conflicts based on confirmed appointments
 const mockConfirmedAppointments = getMockConfirmedAppointments();
@@ -74,7 +74,7 @@ const requestsV2 = require('./v2/requests.json');
 
 // Oracle Health appointment requests
 const requestsOh = require('./v2/requests_oh.json');
-// const schedulingConfigurationsVPG = require('./v2/scheduling_configurations_vpg.json');
+const schedulingConfigurationsVPG = require('./v2/scheduling_configurations_vpg.json');
 
 const appointmentRequests = {
   data: requestsV2.data.concat(requestsOh.data),
@@ -327,13 +327,13 @@ const responses = {
   'GET /vaos/v2/scheduling/configurations': (req, res) => {
     if (req.query.cc_enabled === 'true') {
       // Return VPG scheduling configurations
-      // return res.json(schedulingConfigurationsVPG);
-      return res.json(schedulingConfigurationsCC);
+      return res.json(schedulingConfigurationsVPG);
+      // return res.json(schedulingConfigurationsCC);
     }
 
     // Return VPG scheduling configurations
-    // return res.json(schedulingConfigurationsVPG);
-    return res.json(schedulingConfigurations);
+    return res.json(schedulingConfigurationsVPG);
+    // return res.json(schedulingConfigurations);
   },
   'GET /vaos/v2/community_care/eligibility/:id': (req, res) => {
     return res.json({
