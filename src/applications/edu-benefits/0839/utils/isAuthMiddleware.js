1a0fc716ee0555d47daf4b39cea022931b836e48
Liz Wendling <170662489+GovCIOLiz@users.noreply.github.com>
2026-01-23 11:41:21 -0800

    Adding isauth middleware

--- src/applications/edu-benefits/0839/utils/isAuthMiddleware.js
+++ src/applications/edu-benefits/0839/utils/isAuthMiddleware.js
@@ -2,7 +2,7 @@ import { setData } from 'platform/forms-system/src/js/actions';
 
 export const isAuthMiddleware = store => next => action => {
   if (action.type === 'UPDATE_LOGGEDIN_STATUS') {
-    next(
+    store.dispatch(
       setData({ ...store.getState().form.data, isAuthenticated: action.value }),
     );
   }
