d82604aa543366b3b055dd20d836e0e44154ffbf
Brandyn Sullins <29178824+OptionSelect@users.noreply.github.com>
2026-01-23 15:46:13 -0500

    Working w Rebecca to refactor

--- src/applications/combined-debt-portal/combined/utils/helpers.js
+++ src/applications/combined-debt-portal/combined/utils/helpers.js
@@ -128,15 +128,21 @@ export const transform = data => {
   });
 };
 
-export const setPageFocus = selector => {
-  const el = document.querySelector(selector);
+export const isAnyElementFocused = () => {
+  return document.activeElement && document.activeElement !== document.body;
+};
+
+export const focusElement = el => {
   if (el) {
     el.setAttribute('tabIndex', -1);
     el.focus();
-  } else {
-    document.querySelector('#main h1').setAttribute('tabIndex', -1);
-    document.querySelector('#main h1').focus();
   }
+};
+
+export const setPageFocus = selector => {
+  const el =
+    document.querySelector(selector) || document.querySelector('#main h1');
+  focusElement(el);
 };
 
 // 'Manually' generating PDF instead of using generatePdf so we can
