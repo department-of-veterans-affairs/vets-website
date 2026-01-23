33f02b60129e582003b65b1eab45c7f618687c97
Micah Alconcel <micahalconcel@Micahs-MacBook-Pro.local>
2026-01-23 10:07:32 -0800

    changes migration to use save-in-progress values for validation instead of always assuming true

--- src/applications/benefits-optimization-aquia/21-2680-house-bound-status/config/migrations/01-validate-international-phone-numbers.js
+++ src/applications/benefits-optimization-aquia/21-2680-house-bound-status/config/migrations/01-validate-international-phone-numbers.js
@@ -1,16 +1,18 @@
 export default function validateInternationalPhoneNumbers(savedData) {
   const { formData, metadata } = savedData;
-  // add _isValid, _error, and _touched to claimantPhoneNumber
   if (
     formData?.claimantContact?.claimantPhoneNumber &&
     typeof formData.claimantContact.claimantPhoneNumber === 'object' &&
     formData.claimantContact.claimantPhoneNumber.contact
   ) {
-    formData.claimantContact.claimantPhoneNumber._isValid = true;
-    formData.claimantContact.claimantPhoneNumber._error = null;
-    formData.claimantContact.claimantPhoneNumber._touched = false;
+    const { claimantPhoneNumber } = formData.claimantContact;
+    formData.claimantContact.claimantPhoneNumber._isValid =
+      claimantPhoneNumber.IsValid;
+    formData.claimantContact.claimantPhoneNumber._error =
+      claimantPhoneNumber.Error;
+    formData.claimantContact.claimantPhoneNumber._touched =
+      claimantPhoneNumber.Touched;
   }
-
   return {
     formData,
     metadata,
