dd42d4594d2835f65c347b9c1f3fe8c41efd63a5
steveg-IAT <sgallagher@ironarchtech.com>
2026-01-23 16:20:21 -0500

    removed extension from export statement

--- src/platform/mhv/exportsFile.js
+++ src/platform/mhv/exportsFile.js
@@ -1,9 +1,13 @@
 export { useBackToTop } from './hooks/useBackToTop';
 export { default as useAcceleratedData } from './hooks/useAcceleratedData';
 export { default as MHVDown } from './downtime/components/MHVDown';
-export { default as MHVDowntimeApproaching } from './downtime/components/MHVDowntimeApproaching';
+export {
+  default as MHVDowntimeApproaching,
+} from './downtime/components/MHVDowntimeApproaching';
 export { default as MHVDowntime } from './downtime/containers/MHVDowntime';
-export { default as MhvSecondaryNav } from './secondary-nav/containers/MhvSecondaryNav';
+export {
+  default as MhvSecondaryNav,
+} from './secondary-nav/containers/MhvSecondaryNav';
 export {
   default as MhvPageNotFound,
   MhvPageNotFoundContent,
@@ -47,9 +51,11 @@ export {
 export { trapFocus } from './util/ui/index';
 export { generateMilitaryServicePdf } from './dod-history/military-service-pdf';
 export { generateSEIPdf } from './self-entered/generate-sei-pdf';
-export { default as MissingRecordsError } from './self-entered/MissingRecordsError';
+export {
+  default as MissingRecordsError,
+} from './self-entered/MissingRecordsError';
 export {
   default as MhvAlertConfirmEmail,
   ProfileAlertConfirmEmail,
 } from './components/MhvAlertConfirmEmail';
-export { useBreadcrumbFocus } from './hooks/useBreadcrumbFocus.jsx';
+export { useBreadcrumbFocus } from './hooks/useBreadcrumbFocus';
