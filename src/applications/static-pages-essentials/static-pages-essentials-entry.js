356c96b58904f35755ff750e803755c3663f6102
Patrick Wolfert <patrickrwolfert@gmail.com>
2026-01-23 16:13:48 -0800

    Add a comment to the new essentials entry explaining it

--- src/applications/static-pages-essentials/static-pages-essentials-entry.js
+++ src/applications/static-pages-essentials/static-pages-essentials-entry.js
@@ -1,3 +1,21 @@
+/**
+ * static-pages-essentials.entry.js (alternative to static-pages.entry.js)
+ *
+ * This bundle contains sitewide components and Redux store setup that are
+ * required on every page of the static site. It's part of a two-bundle
+ * strategy to replace the monolithic static-pages.entry.js bundle.
+ *
+ * Strategy: This "essentials" bundle is loaded and cached on every page, while
+ * page-specific widgets are loaded in separate bundles only when needed. This
+ * reduces initial bundle size and improves cache efficiency since changes to
+ * individual widgets don't invalidate the entire bundle cache.
+ *
+ * Contents:
+ * - Redux store (exposed globally for use by widget bundles)
+ * - Sitewide components: Header, Footer, Side Nav, Maintenance Banner,
+ *   Situation Updates Banner
+ */
+
 import 'platform/polyfills';
 
 import * as Sentry from '@sentry/browser';
