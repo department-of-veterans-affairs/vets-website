f7f2514cf0132315576bd56e2bc22b54ddd42f64
Alex Tran <115654176+TranScripted@users.noreply.github.com>
2026-01-26 09:45:31 -0500

    feat: 0781 V3 input file compoent

--- src/applications/disability-benefits/all-claims/pages/additionalDocuments.js
+++ src/applications/disability-benefits/all-claims/pages/additionalDocuments.js
@@ -1,44 +1,73 @@
-import full526EZSchema from 'vets-json-schema/dist/21-526EZ-ALLCLAIMS-schema.json';
+// import full526EZSchema from 'vets-json-schema/dist/21-526EZ-ALLCLAIMS-schema.json';
 
-import { UploadDescription } from '../content/fileUploadDescriptions';
-import { ancillaryFormUploadUi } from '../utils/schemas';
-import { selfAssessmentAlert } from '../content/selfAssessmentAlert';
-import { isBDD } from '../utils';
+// import { UploadDescription } from '../content/fileUploadDescriptions';
+// import { ancillaryFormUploadUi } from '../utils/schemas';
+// import { selfAssessmentAlert } from '../content/selfAssessmentAlert';
+// import { isBDD } from '../utils';
+import { evidenceChoiceUploadContent } from './form0781/supportingEvidenceEnhancement/evidenceChoiceUploadPage';
+import { standardTitle } from '../content/form0781';
+import UploadFiles from '../components/supportingEvidenceUpload/uploadFiles';
 
-const { attachments } = full526EZSchema.properties;
+// const { attachments } = full526EZSchema.properties;
 
 export const uiSchema = {
-  'view:selfAssessmentAlert': {
-    'ui:title': selfAssessmentAlert,
-    'ui:options': {
-      hideIf: formData => !isBDD(formData),
-    },
-  },
-  additionalDocuments: {
-    ...ancillaryFormUploadUi(
-      'Supporting (lay) statements or other evidence',
-      'Adding additional evidence:',
-      {
-        addAnotherLabel: 'Add another file',
-        buttonText: 'Upload file',
-      },
+  // 'view:selfAssessmentAlert': {
+  //   'ui:title': selfAssessmentAlert,
+  //   'ui:options': {
+  //     hideIf: formData => !isBDD(formData),
+  //   },
+  // },
+  // additionalDocuments: {
+  //   ...ancillaryFormUploadUi(
+  //     'Supporting (lay) statements or other evidence',
+  //     'Adding additional evidence:',
+  //     {
+  //       addAnotherLabel: 'Add another file',
+  //       buttonText: 'Upload file',
+  //     },
+  //   ),
+  //   'ui:description': UploadDescription,
+  //   'ui:confirmationField': ({ formData }) => ({
+  //     data: formData?.map(item => item.name || item.fileName),
+  //     label: 'Uploaded file(s)',
+  //   }),
+  // },
+  'view:evidenceChoiceUpload': {
+    'ui:title': standardTitle(
+      'Upload supporting documents and additional forms',
     ),
-    'ui:description': UploadDescription,
-    'ui:confirmationField': ({ formData }) => ({
-      data: formData?.map(item => item.name || item.fileName),
-      label: 'Uploaded file(s)',
-    }),
+    'ui:description': evidenceChoiceUploadContent,
+  },
+  uploadedFiles: {
+    'ui:title': ' ',
+    'ui:field': UploadFiles,
+    'ui:required': () => true,
+    'ui:errorMessages': {
+      required: 'Please upload at least one supporting document',
+    },
   },
 };
 
 export const schema = {
   type: 'object',
-  required: ['additionalDocuments'],
+  required: ['uploadedFiles'],
   properties: {
-    'view:selfAssessmentAlert': {
+    'view:evidenceChoiceUpload': {
       type: 'object',
       properties: {},
     },
-    additionalDocuments: attachments,
+    // additionalDocuments: attachments,
+    // 'view:evidenceChoiceUpload': {
+    //   type: 'object',
+    //   properties: {},
+    // },
+    uploadedFiles: {
+      type: 'array',
+      minItems: 1,
+      items: {
+        type: 'object',
+        properties: {},
+      },
+    },
   },
 };
