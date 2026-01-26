8645d7d7fbe9c9ec8721e2188431d8f7f76fbe87
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2026-01-26 15:26:54 -0600

    wip

--- src/applications/edu-benefits/0839/pages/acknowledgements.js
+++ src/applications/edu-benefits/0839/pages/acknowledgements.js
@@ -9,6 +9,24 @@ import {
 import CapitalizedTextInputField from '../containers/CapitalizedTextInputField';
 import { validateInitials } from '../helpers';
 
+const STATEMENT_1 =
+  'For all U.S. schools, once this agreement is accepted by VA, it will be considered an open-ended agreement that is in effect for the entire upcoming academic year and all future academic years unless VA or the institution notifies the other party that changes are requested during the the annual open season enrollment period, March 15 - May 15 (or the following Monday if May 15 falls on a Saturday or Sunday.) The annual open season enrollment period for foreign schools is June 1 - July 31 (or the following Monday if if July 31 falls on a Saturday or Sunday.) Withdrawal or modifications to the terms shall be indicated in Agreement Type. Modifications made during the open enrollment period will go into effect for the subsequent academic year and will be posted to our website at www.gibill.va.gov when the open enrollment period ends. ';
+const STATEMENT_2 =
+  'The IHL agrees to provide contributions to eligible individuals who apply for such program at the institution (in a manner prescribed by the institution) on a first-come-first-serve basis. Funds for Yellow Ribbon contributions may derive from any source of institutional funding that is not already allocated or awarded for a non-Yellow Ribbon purpose. Student ledgers must denote contributions as "Yellow Ribbon." Yellow Ribbon funds cannot be denoted as any type of grant, scholarship or other fund sources that would be applied to the student\'s account regardless of Yellow Ribbon program participation.';
+const STATEMENT_3 =
+  "The IHL agrees to provide contributions on behalf of a participating individual during the current academic year and all subsequent academic years in which the IHL participates in the Yellow Ribbon Program, provided that the individual maintains satisfactory progress, conduct, and attendance according to the regularly prescribed standards of the institution. If modification to the existing agreement reduces the contribution amount, the IHL agrees to maintain the prior agreement's contribution amount for any individuals in good standing who were in receipt of Yellow Ribbon for prior academic years. ";
+const STATEMENT_4 =
+  'The IHL agrees to provide the maximum amount of contributions payable toward the net cost for each participating individual during each term, quarter, or semester does not exceed the maximum dollar amount payable during the academic year. ';
+
+const createInitialReviewField = description => ({ children }) => (
+  <dl className="review-row">
+    <dt>
+      <p>{description}</p>
+    </dt>
+    <dd>{children}</dd>
+  </dl>
+);
+
 const validateInitialsMatch = (errors, fieldData, formData) => {
   const firstName = formData?.authorizedOfficial?.fullName?.first || '';
   const lastName = formData?.authorizedOfficial?.fullName?.last || '';
@@ -35,11 +53,12 @@ const uiSchema = {
   },
 
   'view:statement1': {
-    'ui:description':
-      'For all U.S. schools, once this agreement is accepted by VA, it will be considered an open-ended agreement that is in effect for the entire upcoming academic year and all future academic years unless VA or the institution notifies the other party that changes are requested during the the annual open season enrollment period, March 15 - May 15 (or the following Monday if May 15 falls on a Saturday or Sunday.) The annual open season enrollment period for foreign schools is June 1 - July 31 (or the following Monday if if July 31 falls on a Saturday or Sunday.) Withdrawal or modifications to the terms shall be indicated in Agreement Type. Modifications made during the open enrollment period will go into effect for the subsequent academic year and will be posted to our website at www.gibill.va.gov when the open enrollment period ends. ',
+    'ui:description': STATEMENT_1,
   },
   statement1Initial: {
     ...textUI('Initial here'),
+    'ui:description': STATEMENT_1,
+    'ui:reviewField': createInitialReviewField(STATEMENT_1),
     'ui:webComponentField': CapitalizedTextInputField,
     'ui:options': {
       width: 'small',
@@ -53,11 +72,12 @@ const uiSchema = {
     'ui:validations': [validateInitialsMatch],
   },
   'view:statement2': {
-    'ui:description':
-      'The IHL agrees to provide contributions to eligible individuals who apply for such program at the institution (in a manner prescribed by the institution) on a first-come-first-serve basis. Funds for Yellow Ribbon contributions may derive from any source of institutional funding that is not already allocated or awarded for a non-Yellow Ribbon purpose. Student ledgers must denote contributions as “Yellow Ribbon.” Yellow Ribbon funds cannot be denoted as any type of grant, scholarship or other fund sources that would be applied to the student’s account regardless of Yellow Ribbon program participation.',
+    'ui:description': STATEMENT_2,
   },
   statement2Initial: {
     ...textUI('Initial here'),
+    'ui:description': STATEMENT_2,
+    'ui:reviewField': createInitialReviewField(STATEMENT_2),
     'ui:webComponentField': CapitalizedTextInputField,
     'ui:options': {
       width: 'small',
@@ -71,11 +91,12 @@ const uiSchema = {
     'ui:validations': [validateInitialsMatch],
   },
   'view:statement3': {
-    'ui:description':
-      'The IHL agrees to provide contributions on behalf of a participating individual during the current academic year and all subsequent academic years in which the IHL participates in the Yellow Ribbon Program, provided that the individual maintains satisfactory progress, conduct, and attendance according to the regularly prescribed standards of the institution. If modification to the existing agreement reduces the contribution amount, the IHL agrees to maintain the prior agreement’s contribution amount for any individuals in good standing who were in receipt of Yellow Ribbon for prior academic years. ',
+    'ui:description': STATEMENT_3,
   },
   statement3Initial: {
     ...textUI('Initial here'),
+    'ui:description': STATEMENT_3,
+    'ui:reviewField': createInitialReviewField(STATEMENT_3),
     'ui:webComponentField': CapitalizedTextInputField,
     'ui:options': {
       width: 'small',
@@ -89,11 +110,12 @@ const uiSchema = {
     'ui:validations': [validateInitialsMatch],
   },
   'view:statement4': {
-    'ui:description':
-      'The IHL agrees to provide the maximum amount of contributions payable toward the net cost for each participating individual during each term, quarter, or semester does not exceed the maximum dollar amount payable during the academic year. ',
+    'ui:description': STATEMENT_4,
   },
   statement4Initial: {
     ...textUI('Initial here'),
+    'ui:description': STATEMENT_4,
+    'ui:reviewField': createInitialReviewField(STATEMENT_4),
     'ui:webComponentField': CapitalizedTextInputField,
     'ui:options': {
       width: 'small',
