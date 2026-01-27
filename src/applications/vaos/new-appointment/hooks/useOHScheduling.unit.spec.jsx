aa746a383ad2fcf22966335197477473f5690fd6
Simi Adebowale <47654119+simiadebowale@users.noreply.github.com>
2026-01-27 10:53:31 -0600

    #130764: Consolidate all OH flags into a single Feature flag (#41729)
    * Consolidate OH feature flags to use vaOnlineSchedulingUseVpg across components and hooks
    
    * Refactor OH scheduling feature flags: consolidate useOHDirectScheduling into useOHScheduling and update related tests
    
    * update e2e tests

--- src/applications/vaos/new-appointment/hooks/useOHScheduling.unit.spec.jsx
+++ src/applications/vaos/new-appointment/hooks/useOHScheduling.unit.spec.jsx
@@ -2,10 +2,10 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import * as redux from 'react-redux';
 import { renderHook } from '@testing-library/react-hooks';
-import { useOHRequestScheduling } from './useOHRequestScheduling';
+import { useOHScheduling } from './useOHScheduling';
 import * as selectors from '../redux/selectors';
 
-describe('VAOS Hook: useOHRequestScheduling', () => {
+describe('VAOS Hook: useOHScheduling', () => {
   let sandbox;
   let useSelectorStub;
   let getTypeOfCareStub;
@@ -21,62 +21,62 @@ describe('VAOS Hook: useOHRequestScheduling', () => {
   });
 
   it('should return false when feature flag is disabled', () => {
-    useSelectorStub.onCall(0).returns(false); // featureOHRequest
+    useSelectorStub.onCall(0).returns(false); // featureUseVpg
     useSelectorStub.onCall(1).returns({}); // data
     getTypeOfCareStub.returns({ idV2: 'foodAndNutrition' });
 
-    const { result } = renderHook(() => useOHRequestScheduling());
+    const { result } = renderHook(() => useOHScheduling());
 
     expect(result.current).to.be.false;
   });
 
   it('should return false when typeOfCare is not in OH_ENABLED_TYPES_OF_CARE', () => {
-    useSelectorStub.onCall(0).returns(true); // featureOHRequest
+    useSelectorStub.onCall(0).returns(true); // featureUseVpg
     useSelectorStub.onCall(1).returns({}); // data
     getTypeOfCareStub.returns({ idV2: 'primaryCare' });
 
-    const { result } = renderHook(() => useOHRequestScheduling());
+    const { result } = renderHook(() => useOHScheduling());
 
     expect(result.current).to.be.false;
   });
 
   it('should return true when feature flag is enabled and typeOfCare is foodAndNutrition', () => {
-    useSelectorStub.onCall(0).returns(true); // featureOHRequest
+    useSelectorStub.onCall(0).returns(true); // featureUseVpg
     useSelectorStub.onCall(1).returns({}); // data
     getTypeOfCareStub.returns({ idV2: 'foodAndNutrition' });
 
-    const { result } = renderHook(() => useOHRequestScheduling());
+    const { result } = renderHook(() => useOHScheduling());
 
     expect(result.current).to.be.true;
   });
 
   it('should return false when typeOfCare is undefined', () => {
-    useSelectorStub.onCall(0).returns(true); // featureOHRequest
+    useSelectorStub.onCall(0).returns(true); // featureUseVpg
     useSelectorStub.onCall(1).returns({}); // data
     getTypeOfCareStub.returns(undefined);
 
-    const { result } = renderHook(() => useOHRequestScheduling());
+    const { result } = renderHook(() => useOHScheduling());
 
     expect(result.current).to.be.false;
   });
 
   it('should return false when typeOfCare.idV2 is undefined', () => {
-    useSelectorStub.onCall(0).returns(true); // featureOHRequest
+    useSelectorStub.onCall(0).returns(true); // featureUseVpg
     useSelectorStub.onCall(1).returns({}); // data
     getTypeOfCareStub.returns({ idV2: undefined });
 
-    const { result } = renderHook(() => useOHRequestScheduling());
+    const { result } = renderHook(() => useOHScheduling());
 
     expect(result.current).to.be.false;
   });
 
   it('should call getTypeOfCare with the form data', () => {
     const mockData = { typeOfCareId: '123' };
-    useSelectorStub.onCall(0).returns(true); // featureOHRequest
+    useSelectorStub.onCall(0).returns(true); // featureUseVpg
     useSelectorStub.onCall(1).returns(mockData); // data
     getTypeOfCareStub.returns({ idV2: 'foodAndNutrition' });
 
-    renderHook(() => useOHRequestScheduling());
+    renderHook(() => useOHScheduling());
 
     expect(getTypeOfCareStub.calledWith(mockData)).to.be.true;
   });
