4c5a58af96ed9a6ffb4881ca759a3c0cd08f36e4
Zach Button <zachrey.button@gmail.com>
2026-01-27 15:46:02 -0800

    content-build run install-safe

--- script/review-entrypoint.sh
+++ script/review-entrypoint.sh
@@ -8,7 +8,7 @@ npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' -
 
 # Build content-build and serve site
 cd ../content-build
-cp .env.example .env && yarn install --production=false
+cp .env.example .env && yarn install-safe --production=false
 npm run fetch-drupal-cache
 npm run build -- --buildtype=localhost --api='${API_URL}' --host='${WEB_HOST}' --port='${WEB_PORT}' --apps-directory-name=application
 npm run heroku-serve -- build/localhost -p 3002
