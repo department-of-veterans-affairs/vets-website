eb5ad837070e13e8f3194a48366cef9325226b55
Victoria (Vic) Saleem <59583325+vsaleem@users.noreply.github.com>
2026-01-27 01:32:56 -0600

    Refactor routes to support testing scenarios
    Replaces the single route export with an array of routes, including additional routes for testing with different 'loggedIn' query parameters. Introduces a route HOC to wrap components with ConfigProvider for context support.

--- src/applications/survivors-benefits/routes.jsx
+++ src/applications/survivors-benefits/routes.jsx
@@ -1,12 +1,43 @@
+import React from 'react';
 import { createRoutesWithSaveInProgress } from 'platform/forms/save-in-progress/helpers';
 import formConfig from './config/form';
 import SurvivorsBenefitsApp from './containers/SurvivorsBenefitsApp';
 
-const route = {
-  path: '/',
-  component: SurvivorsBenefitsApp,
-  indexRoute: { onEnter: (nextState, replace) => replace('/introduction') },
-  childRoutes: createRoutesWithSaveInProgress(formConfig),
-};
+import { ConfigProvider } from './shared/context/ConfigContext';
 
-export default route;
+export const routeHoc = Component => props => (
+  <ConfigProvider {...props}>
+    <Component {...props} />
+  </ConfigProvider>
+);
+
+const testingRoutes = [
+  {
+    path: '/',
+    component: routeHoc(SurvivorsBenefitsApp),
+    indexRoute: {
+      onEnter: (nextState, replace) => replace('/introduction?loggedIn=false'),
+    },
+    childRoutes: createRoutesWithSaveInProgress(formConfig),
+  },
+  {
+    path: '/',
+    component: routeHoc(SurvivorsBenefitsApp),
+    indexRoute: {
+      onEnter: (nextState, replace) => replace('/introduction?loggedIn=true'),
+    },
+    childRoutes: createRoutesWithSaveInProgress(formConfig),
+  },
+];
+
+const routes = [
+  {
+    path: '/',
+    component: routeHoc(SurvivorsBenefitsApp),
+    indexRoute: { onEnter: (nextState, replace) => replace('/introduction') },
+    childRoutes: createRoutesWithSaveInProgress(formConfig),
+  },
+  ...testingRoutes,
+];
+
+export default routes;
