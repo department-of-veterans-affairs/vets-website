f3340219eaafe212e4b1e7da404ace7b2a071c8f
Michael Moyer <87040148+mmoyer-va@users.noreply.github.com>
2026-01-27 15:43:13 -0500

    DRYed up code; use flag to remove images in L&T

--- src/applications/mhv-medical-records/reducers/radiology.js
+++ src/applications/mhv-medical-records/reducers/radiology.js
@@ -1,20 +1,13 @@
 import { parseISO } from 'date-fns';
 import { Actions } from '../util/actionTypes';
+import { buildInitialDateRange } from '../util/helpers';
+import { loadStates, DEFAULT_DATE_RANGE } from '../util/constants';
 import {
-  formatDateInLocalTimezone,
-  formatNameFirstToLast,
-  buildInitialDateRange,
-} from '../util/helpers';
-import {
-  areDatesEqualToMinute,
-  parseRadiologyReport,
-} from '../util/radiologyUtil';
-import {
-  labTypes,
-  EMPTY_FIELD,
-  loadStates,
-  DEFAULT_DATE_RANGE,
-} from '../util/constants';
+  convertMhvRadiologyRecord,
+  convertCvixRadiologyRecord,
+  mergeRadiologyLists,
+  mergeRadiologyDetails,
+} from '../util/imagesUtil';
 
 const initialState = {
   /**
@@ -45,107 +38,6 @@ const initialState = {
    * The selected date range for displaying radiology records
    * */
   dateRange: buildInitialDateRange(DEFAULT_DATE_RANGE),
-};
-
-export const buildRadiologyResults = record => {
-  const reportText = record?.reportText || '\n';
-  const impressionText = record?.impressionText || '\n';
-  return `Report:\n${reportText
-    .replace(/\r\n|\r/g, '\n')
-    .replace(/^/gm, '  ')}  
-Impression:\n${impressionText.replace(/\r\n|\r/g, '\n').replace(/^/gm, '  ')}`;
-};
-
-export const convertMhvRadiologyRecord = record => {
-  const orderedBy = formatNameFirstToLast(record.requestingProvider);
-  const imagingProvider = formatNameFirstToLast(record.radiologist);
-  return {
-    id: `r${record.id}-${record.hash}`,
-    name: record.procedureName,
-    type: labTypes.RADIOLOGY,
-    reason: record.reasonForStudy || EMPTY_FIELD,
-    orderedBy: orderedBy || EMPTY_FIELD,
-    clinicalHistory: record?.clinicalHistory?.trim() || EMPTY_FIELD,
-    imagingLocation: record.performingLocation,
-    date: record.eventDate
-      ? formatDateInLocalTimezone(record.eventDate, true)
-      : EMPTY_FIELD,
-    sortDate: record.eventDate,
-    imagingProvider: imagingProvider || EMPTY_FIELD,
-    results: buildRadiologyResults(record),
-  };
-};
-
-export const convertCvixRadiologyRecord = record => {
-  const parsedReport = parseRadiologyReport(record.reportText);
-  return {
-    id: `r${record.id}-${record.hash}`,
-    name: record.procedureName,
-    type: labTypes.CVIX_RADIOLOGY,
-    reason: parsedReport['Reason for Study'] || EMPTY_FIELD,
-    orderedBy: parsedReport['Req Phys'] || EMPTY_FIELD,
-    clinicalHistory: parsedReport['Clinical History'] || EMPTY_FIELD,
-    imagingLocation: record.facilityInfo?.name || EMPTY_FIELD,
-    date: record.performedDatePrecise
-      ? formatDateInLocalTimezone(record.performedDatePrecise, true)
-      : EMPTY_FIELD,
-    sortDate: record.performedDatePrecise
-      ? `${new Date(record.performedDatePrecise).toISOString().split('.')[0]}Z`
-      : EMPTY_FIELD,
-    imagingProvider: EMPTY_FIELD,
-    results: buildRadiologyResults({
-      reportText: parsedReport.Report,
-      impressionText: parsedReport.Impression,
-    }),
-    studyId: record.studyIdUrn,
-    imageCount: record.imageCount,
-  };
-};
-
-const mergeRadiologyRecords = (phrRecord, cvixRecord) => {
-  if (phrRecord && cvixRecord) {
-    return {
-      ...phrRecord,
-      studyId: cvixRecord.studyId,
-      imageCount: cvixRecord.imageCount,
-    };
-  }
-  return phrRecord || cvixRecord || null;
-};
-
-/**
- * Create a union of the radiology reports from PHR and CVIX. This function will merge
- * duplicates between the two lists.
- *
- * @param {Array} phrRadiologyTestsList - List of PHR radiology records.
- * @param {Array} cvixRadiologyTestsList - List of CVIX radiology records.
- * @returns {Array} - The merged list of radiology records.
- */
-export const mergeRadiologyLists = (
-  phrRadiologyTestsList,
-  cvixRadiologyTestsList,
-) => {
-  const mergedArray = [];
-  const matchedCvixIds = new Set();
-
-  for (const phrRecord of phrRadiologyTestsList) {
-    let matchingCvix = null;
-    for (const cvixRecord of cvixRadiologyTestsList) {
-      if (areDatesEqualToMinute(phrRecord.sortDate, cvixRecord.sortDate)) {
-        matchingCvix = cvixRecord;
-        matchedCvixIds.add(matchingCvix.id);
-        break;
-      }
-    }
-    if (matchingCvix) {
-      mergedArray.push(mergeRadiologyRecords(phrRecord, matchingCvix));
-    } else {
-      mergedArray.push(phrRecord);
-    }
-  }
-  return mergedArray.concat(
-    cvixRadiologyTestsList.filter(record => !matchedCvixIds.has(record.id)),
-  );
 };
 
 function sortByDate(array) {
@@ -191,16 +83,12 @@ export const radiologyReducer = (state = initialState, action) => {
       };
     }
     case Actions.Radiology.GET: {
-      const { phrDetails, cvixDetails } = action.response;
-      const convertedPhr = phrDetails
-        ? convertMhvRadiologyRecord(phrDetails)
-        : null;
-      const convertedCvix = cvixDetails
-        ? convertCvixRadiologyRecord(cvixDetails)
-        : null;
       return {
         ...state,
-        radiologyDetails: mergeRadiologyRecords(convertedPhr, convertedCvix),
+        radiologyDetails: mergeRadiologyDetails(
+          action.response.phrDetails,
+          action.response.cvixDetails,
+        ),
       };
     }
     case Actions.Radiology.GET_FROM_LIST: {
