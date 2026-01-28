b09830c298a53b1eaa904994ccea507b923a256c
Chad Laflamme <216556326+sixercjl@users.noreply.github.com>
2026-01-28 12:45:34 -0500

    appointment dates unavailable after cancel

--- src/applications/vass/pages/CancelAppointment.jsx
+++ src/applications/vass/pages/CancelAppointment.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import { VaButtonPair } from '@department-of-veterans-affairs/component-library/dist/react-bindings';
 import { useNavigate, useParams } from 'react-router-dom-v5-compat';
 import Wrapper from '../layout/Wrapper';
@@ -8,10 +9,12 @@ import {
   useCancelAppointmentMutation,
 } from '../redux/api/vassApi';
 import { URLS } from '../utils/constants';
+import { setSelectedDate } from '../redux/slices/formSlice';
 
 const CancelAppointment = () => {
   const { appointmentId } = useParams();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const { data: appointmentData, isLoading } = useGetAppointmentQuery({
     appointmentId,
   });
@@ -20,13 +23,23 @@ const CancelAppointment = () => {
   const onCancelAppointment = useCallback(
     async () => {
       try {
+        if (appointmentData?.startUTC) {
+          // After canceling the appointment, the dates are cleared on the VASS end. We save them to render on the confirmation page.
+          dispatch(setSelectedDate(new Date(appointmentData.startUTC)));
+        }
         await cancelAppointment({ appointmentId });
         navigate(`${URLS.CANCEL_APPOINTMENT_CONFIRMATION}/${appointmentId}`);
       } catch (error) {
         // TODO: handle error
       }
     },
-    [appointmentId, cancelAppointment, navigate],
+    [
+      appointmentData?.startUTC,
+      appointmentId,
+      cancelAppointment,
+      dispatch,
+      navigate,
+    ],
   );
 
   if (isLoading || !appointmentData) {
