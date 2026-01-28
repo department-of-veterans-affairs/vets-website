519614cb2df051476e21ca95464022f7ffd2d93a
Michael Ghisilieri <175352110+mghisilieri@users.noreply.github.com>
2026-01-28 10:10:29 -0800

    22-10272 Step 2: Pages 1-3 implementation

--- src/applications/edu-benefits/10272/config/form.js
+++ src/applications/edu-benefits/10272/config/form.js
@@ -1,5 +1,10 @@
 import footerContent from 'platform/forms/components/FormFooter';
 import { VA_FORM_IDS } from 'platform/forms/constants';
+
+import { profilePersonalInfoPage } from 'platform/forms-system/src/js/patterns/prefill/PersonalInformation';
+import { profileContactInfoPages } from 'platform/forms-system/src/js/patterns/prefill/ContactInfo';
+import { getContent } from 'platform/forms-system/src/js/utilities/data/profile';
+
 import { TITLE, SUBTITLE } from '../constants';
 import manifest from '../manifest.json';
 
@@ -12,7 +17,10 @@ import {
   educationBenefitsElibility,
   educationBenefitsHistory,
   hasPreviouslyApplied,
+  payeeNumber,
 } from '../pages';
+
+import prefillTransform from './prefillTransform';
 
 /** @type {FormConfig} */
 const formConfig = {
@@ -40,6 +48,7 @@ const formConfig = {
   },
   version: 0,
   prefillEnabled: true,
+  prefillTransformer: prefillTransform,
   savedFormMessages: {
     notFound: 'Please start over.',
     noAuth: 'Please sign in again to continue your request.',
@@ -79,6 +88,34 @@ const formConfig = {
           schema: educationBenefitsElibility.schema,
           depends: formData => formData?.hasPreviouslyApplied === false,
         },
+      },
+    },
+    personalInformationChapter: {
+      title: 'Your personal information',
+      pages: {
+        ...profilePersonalInfoPage({
+          personalInfoConfig: {
+            name: { show: true, required: true },
+            ssn: { show: true, required: true },
+            dateOfBirth: { show: true, required: false },
+          },
+          dataAdapter: {
+            ssnPath: 'vaFileNumber',
+          },
+        }),
+        payeeNumber: {
+          path: 'payee-number',
+          title: 'Your VA payee number',
+          uiSchema: payeeNumber.uiSchema,
+          schema: payeeNumber.schema,
+        },
+        ...profileContactInfoPages({
+          content: {
+            ...getContent('request'),
+            title: 'Confirm the contact information we have on file for you',
+          },
+          contactInfoRequiredKeys: ['mailingAddress'],
+        }),
       },
     },
   },
--- src/applications/edu-benefits/10272/pages/index.js
+++ src/applications/edu-benefits/10272/pages/index.js
@@ -1,9 +1,11 @@
 import * as hasPreviouslyApplied from './hasPreviouslyApplied';
 import * as educationBenefitsHistory from './educationBenefitsHistory';
 import * as educationBenefitsElibility from './educationBenefitsEligibility';
+import * as payeeNumber from './payeeNumber';
 
 export {
   hasPreviouslyApplied,
   educationBenefitsHistory,
   educationBenefitsElibility,
+  payeeNumber,
 };
