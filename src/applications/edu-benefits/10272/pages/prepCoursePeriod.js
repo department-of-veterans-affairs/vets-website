b717a811b925fadce8eb0315b03b14c2058c177e
govcioChad <162637891+govcioChad@users.noreply.github.com>
2026-01-29 13:48:29 -0800

    fixed lint issues

--- src/applications/edu-benefits/10272/pages/prepCoursePeriod.js
+++ src/applications/edu-benefits/10272/pages/prepCoursePeriod.js
@@ -1,49 +1,60 @@
 import React from 'react';
 import {
-    titleUI,
-    descriptionUI,
-    currentOrPastDateUI,
-    currentOrPastDateSchema,
+  titleUI,
+  descriptionUI,
+  currentOrPastDateUI,
+  currentOrPastDateSchema,
 } from '~/platform/forms-system/src/js/web-component-patterns';
-import {
-    validatePrepCourseStartDate,
-} from '../helpers';
 import { validateCurrentOrPastMemorableDate } from 'platform/forms-system/src/js/validation';
-
+import { validatePrepCourseStartDate } from '../helpers';
 
 const uiSchema = {
-    ...titleUI('Provide the start and end dates of your prep course'),
-    ...descriptionUI(<><strong>Note: </strong>You can apply for reimbursement after your prep course has started.</>),
-    prepCourseStartDate: {
-        ...currentOrPastDateUI({
-            title: 'Course start date',
-            monthSelect: false,
-            required: () => true,
-            'ui:description':
-                'Enter 1 or 2 digits for the month and day and 4 digits for the year.',
-            errorMessages: { pattern: 'Enter a valid date', required: 'Enter a start date' },
-        }),
-        'ui:validations': [validateCurrentOrPastMemorableDate, validatePrepCourseStartDate],
-    },
-    prepCourseEndDate: {
-        ...currentOrPastDateUI({
-            title: 'Course end date',
-            monthSelect: false,
-            required: () => true,
-            'ui:description':
-                'Enter 1 or 2 digits for the month and day and 4 digits for the year.',
-            errorMessages: { pattern: 'Enter a valid date', required: 'Enter an end date' },
-        }),
-    },
+  ...titleUI('Provide the start and end dates of your prep course'),
+  ...descriptionUI(
+    <>
+      <strong>Note: </strong>
+      You can apply for reimbursement after your prep course has started.
+    </>,
+  ),
+  prepCourseStartDate: {
+    ...currentOrPastDateUI({
+      title: 'Course start date',
+      monthSelect: false,
+      required: () => true,
+      'ui:description':
+        'Enter 1 or 2 digits for the month and day and 4 digits for the year.',
+      errorMessages: {
+        pattern: 'Enter a valid date',
+        required: 'Enter a start date',
+      },
+    }),
+    'ui:validations': [
+      validateCurrentOrPastMemorableDate,
+      validatePrepCourseStartDate,
+    ],
+  },
+  prepCourseEndDate: {
+    ...currentOrPastDateUI({
+      title: 'Course end date',
+      monthSelect: false,
+      required: () => true,
+      'ui:description':
+        'Enter 1 or 2 digits for the month and day and 4 digits for the year.',
+      errorMessages: {
+        pattern: 'Enter a valid date',
+        required: 'Enter an end date',
+      },
+    }),
+  },
 };
 
 const schema = {
-    type: 'object',
-    properties: {
-        prepCourseStartDate: currentOrPastDateSchema,
-        prepCourseEndDate: currentOrPastDateSchema,
-    },
-    required: ['prepCourseStartDate', 'prepCourseEndDate'],
+  type: 'object',
+  properties: {
+    prepCourseStartDate: currentOrPastDateSchema,
+    prepCourseEndDate: currentOrPastDateSchema,
+  },
+  required: ['prepCourseStartDate', 'prepCourseEndDate'],
 };
 
 export { schema, uiSchema };
