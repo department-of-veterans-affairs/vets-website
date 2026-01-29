a93c494836c49a7427afd743884249186c3ba817
Michael Clement <18408628+michaelclement@users.noreply.github.com>
2026-01-29 09:19:11 -0600

    simplified 537 cypress test

--- src/applications/simple-forms/21p-0537/tests/21p-0537.cypress.spec.js
+++ src/applications/simple-forms/21p-0537/tests/21p-0537.cypress.spec.js
@@ -7,24 +7,9 @@ import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-test
 import formConfig from '../config/form';
 import manifest from '../manifest.json';
 
-import {
-  fillVeteranName,
-  fillVeteranIdentifier,
-  fillRemarriageStatus,
-  fillMarriageInfo,
-  fillSpouseVeteranStatus,
-  fillSpouseVeteranId,
-  fillTerminationStatus,
-  fillTerminationDetails,
-  fillContactInfo,
-} from './e2e/utils/fillers';
+import { fillMarriageInfo, fillTerminationDetails } from './e2e/utils/fillers';
 
-import {
-  goToNextPage,
-  startAsAuthUser,
-  completeReviewAndSubmit,
-  checkAccessibility,
-} from './e2e/utils/helpers';
+import { goToNextPage, checkAccessibility } from './e2e/utils/helpers';
 
 import { setupForAuth } from './e2e/utils/setup';
 
@@ -43,39 +28,9 @@ const testConfig = createTestConfig(
     pageHooks: {
       introduction: ({ afterHook }) => {
         afterHook(() => {
-          startAsAuthUser();
+          cy.clickStartForm();
         });
       },
-      'veteran-name': ({ afterHook }) => {
-        afterHook(() => {
-          cy.get('@testData').then(data => {
-            checkAccessibility();
-            fillVeteranName(data);
-            goToNextPage();
-          });
-        });
-      },
-
-      'veteran-identifier': ({ afterHook }) => {
-        afterHook(() => {
-          cy.get('@testData').then(data => {
-            checkAccessibility();
-            fillVeteranIdentifier(data);
-            goToNextPage();
-          });
-        });
-      },
-
-      'remarriage-status': ({ afterHook }) => {
-        afterHook(() => {
-          cy.get('@testData').then(data => {
-            checkAccessibility();
-            fillRemarriageStatus(data);
-            goToNextPage();
-          });
-        });
-      },
-
       'marriage-info': ({ afterHook }) => {
         afterHook(() => {
           cy.get('@testData').then(data => {
@@ -86,37 +41,8 @@ const testConfig = createTestConfig(
         });
       },
 
-      'spouse-veteran-status': ({ afterHook }) => {
-        afterHook(() => {
-          cy.get('@testData').then(data => {
-            checkAccessibility();
-            fillSpouseVeteranStatus(data);
-            goToNextPage();
-          });
-        });
-      },
-
-      'spouse-veteran-id': ({ afterHook }) => {
-        afterHook(() => {
-          cy.get('@testData').then(data => {
-            checkAccessibility();
-            fillSpouseVeteranId(data);
-            goToNextPage();
-          });
-        });
-      },
-
-      'remarriage-end-status': ({ afterHook }) => {
-        afterHook(() => {
-          cy.get('@testData').then(data => {
-            checkAccessibility();
-            fillTerminationStatus(data);
-            goToNextPage();
-          });
-        });
-      },
-
       'remarriage-end-details': ({ afterHook }) => {
+        cy.injectAxeThenAxeCheck();
         afterHook(() => {
           cy.get('@testData').then(data => {
             checkAccessibility();
@@ -125,20 +51,14 @@ const testConfig = createTestConfig(
           });
         });
       },
-
-      'contact-info': ({ afterHook }) => {
-        afterHook(() => {
-          cy.get('@testData').then(data => {
-            checkAccessibility();
-            fillContactInfo(data);
-            goToNextPage();
-          });
-        });
-      },
       'review-and-submit': ({ afterHook }) => {
         afterHook(() => {
           cy.get('@testData').then(data => {
-            completeReviewAndSubmit(data);
+            cy.fillVaStatementOfTruth({
+              fullName: data.signature,
+              checked: true,
+            });
+            cy.get('va-button[text*="submit" i]').click();
           });
         });
       },
@@ -146,9 +66,6 @@ const testConfig = createTestConfig(
     setupPerTest: () => {
       setupForAuth();
     },
-    // Skip tests in CI until the form is released.
-    // Remove this setting when the form has a content page in production.
-    // skip: Cypress.env('CI'),
   },
   manifest,
   formConfig,
