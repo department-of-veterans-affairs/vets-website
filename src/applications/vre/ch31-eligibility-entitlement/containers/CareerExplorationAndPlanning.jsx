b3c3c31555ec311b6fdfb2dd508cf61ccda45656
flex2016 <26524099+flex2016@users.noreply.github.com>
2026-01-29 16:36:50 -0500

    vre spec test pr

--- src/applications/vre/ch31-eligibility-entitlement/containers/CareerExplorationAndPlanning.jsx
+++ src/applications/vre/ch31-eligibility-entitlement/containers/CareerExplorationAndPlanning.jsx
@@ -22,7 +22,7 @@ export default function CareerExplorationAndPlanning() {
     return (
       <div className="row">
         <div className="usa-width-two-thirds vads-u-margin-top--0p5 vads-u-margin-x--1 medium-screen:vads-u-margin-x--0">
-          <h1>Career Exploration and Planning</h1>
+          <h1>Career Exploration and Planning test</h1>
           <p className="vads-u-color--gray-medium">
             This page isn't available right now.
           </p>
