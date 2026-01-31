a0725d4a8c6fda23e80e094a8130ae6b342614c3
micah-frazier-oddball-io <micah.frazier82@gmail.com>
2026-01-30 17:31:40 -0700

    Removed double wrapping and cleaned up call areas

--- src/applications/personalization/profile/components/direct-deposit/DirectDeposit.jsx
+++ src/applications/personalization/profile/components/direct-deposit/DirectDeposit.jsx
@@ -7,7 +7,6 @@ import { useDirectDeposit, useDirectDepositEffects } from '@@profile/hooks';
 import Headline from '@@profile/components/ProfileSectionHeadline';
 import { ProfileInfoSection } from '@@profile/components/ProfileInfoSection';
 import LoadFail from '@@profile/components/alerts/LoadFail';
-import InitializeVAPServiceID from '@@vap-svc/containers/InitializeVAPServiceID';
 
 import VerifyIdentity from '@@profile/components/direct-deposit/alerts/VerifyIdentity';
 import { TemporaryOutage } from '@@profile/components/direct-deposit/alerts/TemporaryOutage';
@@ -188,64 +187,60 @@ export const DirectDeposit = () => {
   );
 
   return (
-    <InitializeVAPServiceID>
-      <div>
-        <Prompt
-          message="Are you sure you want to leave? If you leave, your in-progress work won’t be saved."
-          when={hasUnsavedFormEdits}
-        />
+    <div>
+      <Prompt
+        message="Are you sure you want to leave? If you leave, your in-progress work won't be saved."
+        when={hasUnsavedFormEdits}
+      />
 
-        <Wrapper>
-          <DowntimeNotification
-            appTitle="direct deposit information page"
-            dependencies={[externalServices.LIGHTHOUSE_DIRECT_DEPOSIT]}
-          >
-            <Toggler toggleName={TOGGLE_NAMES.profile2Enabled}>
-              <Toggler.Enabled>
-                <h2 className="vads-u-margin-top--4">
-                  Bank account information
-                </h2>
-                <p>
-                  We’ll send payments for your disability compensation, pension,
-                  and education benefits to this bank account.
-                </p>
-                <MontgomeryGiBillDescription className="vads-u-margin-top--2" />
-                <ProfileInfoSection
-                  data={[{ value: cardDataValue }]}
-                  namedAnchor={cardHeadingId}
-                  level={2}
-                />
-              </Toggler.Enabled>
-              <Toggler.Disabled>
-                <ProfileInfoSection
-                  title="Bank account information"
-                  data={[{ value: cardDataValue }]}
-                  namedAnchor={cardHeadingId}
-                  level={2}
-                />
-                <MontgomeryGiBillDescription className="vads-u-margin-top--4" />
-              </Toggler.Disabled>
-            </Toggler>
-          </DowntimeNotification>
-          <DirectDepositDevWidget
-            debugData={{
-              controlInformation,
-              paymentAccount,
-              ui,
-              isIdentityVerified,
-              isBlocked,
-              useOAuth,
-              showUpdateSuccess,
-              formData,
-              saveError,
-              loadError,
-              hasUnsavedFormEdits,
-              setFormData,
-            }}
-          />
-          <FraudVictimSummary />
-        </Wrapper>
-      </div>
-    </InitializeVAPServiceID>
+      <Wrapper>
+        <DowntimeNotification
+          appTitle="direct deposit information page"
+          dependencies={[externalServices.LIGHTHOUSE_DIRECT_DEPOSIT]}
+        >
+          <Toggler toggleName={TOGGLE_NAMES.profile2Enabled}>
+            <Toggler.Enabled>
+              <h2 className="vads-u-margin-top--4">Bank account information</h2>
+              <p>
+                We’ll send payments for your disability compensation, pension,
+                and education benefits to this bank account.
+              </p>
+              <MontgomeryGiBillDescription className="vads-u-margin-top--2" />
+              <ProfileInfoSection
+                data={[{ value: cardDataValue }]}
+                namedAnchor={cardHeadingId}
+                level={2}
+              />
+            </Toggler.Enabled>
+            <Toggler.Disabled>
+              <ProfileInfoSection
+                title="Bank account information"
+                data={[{ value: cardDataValue }]}
+                namedAnchor={cardHeadingId}
+                level={2}
+              />
+              <MontgomeryGiBillDescription className="vads-u-margin-top--4" />
+            </Toggler.Disabled>
+          </Toggler>
+        </DowntimeNotification>
+        <DirectDepositDevWidget
+          debugData={{
+            controlInformation,
+            paymentAccount,
+            ui,
+            isIdentityVerified,
+            isBlocked,
+            useOAuth,
+            showUpdateSuccess,
+            formData,
+            saveError,
+            loadError,
+            hasUnsavedFormEdits,
+            setFormData,
+          }}
+        />
+        <FraudVictimSummary />
+      </Wrapper>
+    </div>
   );
 };
