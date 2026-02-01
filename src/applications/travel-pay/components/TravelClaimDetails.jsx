76dfd8cb8a266a1d20ee6398e27633a357070f9b
Samuel Cohen <s.cohen@theta-llc.com>
2026-02-01 10:20:25 -0800

    move claim details and associated appointment fetch to hook

--- src/applications/travel-pay/components/TravelClaimDetails.jsx
+++ src/applications/travel-pay/components/TravelClaimDetails.jsx
@@ -3,37 +3,21 @@ import React, { useEffect } from 'react';
 import { focusElement } from 'platform/utilities/ui';
 import { useFeatureToggle } from 'platform/utilities/feature-toggles/useFeatureToggle';
 import { Element, scrollTo } from 'platform/utilities/scroll';
-import { useDispatch, useSelector } from 'react-redux';
 
 import Breadcrumbs from './Breadcrumbs';
 import DowntimeWindowAlert from '../containers/DownTimeWindowAlert';
 import TravelClaimDetailsContent from './TravelClaimDetailsContent';
 import { useParams } from 'react-router-dom-v5-compat';
-import { clearAppointmentData, getAppointmentDataByDateTime, getClaimDetails } from '../redux/actions';
+import { useClaimDetails } from '../hooks/useClaimDetails';
 
 export default function TravelClaimDetails() {
-  console.log('TravelClaimDetails | mount');
+  const { id } = useParams();
 
-  const dispatch = useDispatch();
-
-  const { id: claimId } = useParams();
-
-  console.log('TravelClaimDetails | render: claimId: ', claimId);
-  
   const {
-    isLoading: claimDetailsLoading,
-    data: claimDetailsData,
-    error: claimDetailsError
-  } = useSelector(
-    state => state.travelPay.claimDetails,
-  );
-  const {
-    isLoading: appointmentLoading,
-    data: appointmentData,
-    error: appointmentError
-  } = useSelector(
-    state => state.travelPay.appointment,
-  );
+    claimData,
+    isLoading,
+    error,
+  } = useClaimDetails(id);
 
   useEffect(() => {
     focusElement('h1');
@@ -46,10 +30,6 @@ export default function TravelClaimDetails() {
     TOGGLE_NAMES,
   } = useFeatureToggle();
 
-  const complexClaimsEnabled = useToggleValue(
-    TOGGLE_NAMES.travelPayEnableComplexClaims,
-  );
-
   const canViewClaimDetails = useToggleValue(
     TOGGLE_NAMES.travelPayViewClaimDetails,
   );
@@ -57,60 +37,9 @@ export default function TravelClaimDetails() {
     TOGGLE_NAMES.travelPayPowerSwitch,
   );
 
-  const appointmentDateTime = claimDetailsData[claimId]?.appointment?.appointmentDateTime;
-
   const featureFlagIsLoading = useToggleLoadingValue();
 
-  // Clear appointment data when component mounts
-  useEffect(() => {
-    dispatch(clearAppointmentData());
-  }
-  , [dispatch]);
-    
-  useEffect(
-    () => {
-      if (claimId && !claimDetailsData[claimId] && !claimDetailsError) {
-        console.log('TravelClaimDetails| fetching claim details for id:', claimId);
-        dispatch(getClaimDetails(claimId));
-      }
-    },
-    [dispatch, claimDetailsData, claimDetailsError, claimId],
-  );
-  
-  console.log('TravelClaimDetails | appointment dependencies ---');
-  console.log('complexClaimsEnabled:', complexClaimsEnabled);
-  console.log('appointmentDateTime:', appointmentDateTime);
-  console.log('appointmentError:', appointmentError);
-  console.log('appointmentData:', appointmentData);
-  console.log('---');
-
-  useEffect(
-    () => {
-      if (
-        complexClaimsEnabled &&
-        appointmentDateTime &&
-        !appointmentError &&
-        !appointmentData
-      ) {
-        console.log('appointmentDateTime:', appointmentDateTime);
-        console.log('TravelClaimDetails | fetching appointment data for appointmentDateTime:', appointmentDateTime);
-        dispatch(getAppointmentDataByDateTime(appointmentDateTime));
-      }
-    },
-    [
-      dispatch,
-      complexClaimsEnabled,
-      appointmentDateTime,
-      appointmentData,
-      appointmentError,
-      claimId,
-    ],
-  );
-
-  console.log('TravelClaimDetails | claimDetailsData:', claimDetailsData);
-  console.log('TravelClaimDetails | appointmentData:', appointmentData);
-
-  if (claimDetailsLoading || appointmentLoading || featureFlagIsLoading) {
+  if (isLoading || featureFlagIsLoading) {
     return (
       <div className="vads-l-grid-container vads-u-padding-y--3">
         <va-loading-indicator
@@ -134,7 +63,6 @@ export default function TravelClaimDetails() {
     return null;
   }
 
-  console.log('TravelClaimDetails | render');
   return (
     <Element name="topScrollElement">
       <article className="usa-grid-full vads-u-padding-bottom--0">
@@ -142,8 +70,8 @@ export default function TravelClaimDetails() {
         <DowntimeWindowAlert appTitle="Travel Pay">
           <div className="vads-l-col--12 medium-screen:vads-l-col--8">
             <TravelClaimDetailsContent
-              claimDetails={claimDetailsData[claimId]}
-              hasError={claimDetailsError || appointmentError}
+              claimDetails={claimData}
+              hasError={!!error}
             />
           </div>
         </DowntimeWindowAlert>
---
+++ src/applications/travel-pay/hooks/useClaimDetails.js
@@ -0,0 +1,80 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useFeatureToggle } from 'platform/utilities/feature-toggles/useFeatureToggle';
+import {
+  getClaimDetails,
+  getAppointmentDataByDateTime,
+  clearAppointmentData,
+} from '../redux/actions';
+
+/**
+ * Hook to fetch and manage claim details and associated appointment data
+ * @param {string} claimId - The ID of the claim to fetch
+ * @returns {Object} Object containing claimData, appointmentData, isLoading, and error
+ */
+export const useClaimDetails = claimId => {
+  const dispatch = useDispatch();
+  const { useToggleValue, TOGGLE_NAMES } = useFeatureToggle();
+
+  const complexClaimsEnabled = useToggleValue(
+    TOGGLE_NAMES.travelPayEnableComplexClaims,
+  );
+
+  const {
+    data: claimDetailsData,
+    isLoading: isLoadingDetails,
+    error: claimDetailsError,
+  } = useSelector(state => state.travelPay.claimDetails);
+
+  const {
+    data: appointmentData,
+    isLoading: isLoadingAppointment,
+    error: appointmentError,
+  } = useSelector(state => state.travelPay.appointment);
+
+  const claimData = claimDetailsData[claimId];
+  const appointmentDateTime = claimData?.appointment?.appointmentDateTime;
+
+  // Clear appointment data
+  useEffect(() => {
+    dispatch(clearAppointmentData());
+  }, [dispatch]);
+
+  // Fetch claim details if not already loaded
+  useEffect(
+    () => {
+      if (claimId && !claimDetailsData[claimId] && !claimDetailsError) {
+        dispatch(getClaimDetails(claimId));
+      }
+    },
+    [dispatch, claimDetailsData, claimDetailsError, claimId],
+  );
+
+  // Fetch appointment data
+  useEffect(
+    () => {
+      if (
+        complexClaimsEnabled &&
+        appointmentDateTime &&
+        !appointmentError &&
+        !appointmentData
+      ) {
+        dispatch(getAppointmentDataByDateTime(appointmentDateTime));
+      }
+    },
+    [
+      dispatch,
+      complexClaimsEnabled,
+      appointmentDateTime,
+      appointmentData,
+      appointmentError,
+    ],
+  );
+
+  return {
+    claimData,
+    appointmentData,
+    isLoading: isLoadingDetails || isLoadingAppointment,
+    error: claimDetailsError || appointmentError,
+  };
+};
