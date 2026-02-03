6d869608673c1e7145cd5e4e2cb8a432682584f8
dustin-cintron <D.Cint88@gmail.com>
2026-02-03 11:49:31 -0500

    Added more details to mock-form-prefill prefillTransformer to fix form submit

--- src/applications/simple-forms/mock-form-prefill/config/prefill-transformer.js
+++ src/applications/simple-forms/mock-form-prefill/config/prefill-transformer.js
@@ -1,10 +1,50 @@
 export const prefillTransformer = (pages, formData, metadata) => {
-  const { ssn, vaFileNumber } = formData?.data?.attributes?.veteran || {};
+  const {
+    ssn,
+    vaFileNumber,
+    firstName,
+    middleName,
+    lastName,
+    suffix,
+    dateOfBirth,
+    gender,
+    address = {},
+    homePhone,
+    mobilePhone,
+    emailAddressText,
+    lastServiceBranch,
+  } = formData?.data?.attributes?.veteran || {};
+
   return {
     metadata,
     formData: {
       ssn,
       vaFileNumber,
+      fullName: {
+        first: firstName,
+        middle: middleName,
+        last: lastName,
+        suffix,
+      },
+      dateOfBirth,
+      gender,
+      veteran: {
+        mailingAddress: {
+          street: address.addressLine1,
+          street2: address.addressLine2,
+          street3: address.addressLine3,
+          city: address.city,
+          state: address.stateCode,
+          postalCode: address.zipCode5,
+          country: address.countryName,
+        },
+        homePhone,
+        mobilePhone,
+        email: emailAddressText
+          ? { emailAddress: emailAddressText }
+          : undefined,
+      },
+      lastServiceBranch,
     },
     pages,
   };
