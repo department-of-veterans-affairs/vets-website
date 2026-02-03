db264b016f6af2355a579985db0b22dd8ef776ee
Kristen Brown <11942904+kristen-brown@users.noreply.github.com>
2026-02-03 16:40:58 -0500

    131821: Fix page load bug due to duplicate form paths

--- src/platform/forms-system/src/js/types.js
+++ src/platform/forms-system/src/js/types.js
@@ -22,6 +22,7 @@
  * @typedef {Object} FormConfig
  * @property {Array<Object>} [additionalRoutes]
  * @property {string} [ariaDescribedBySubmit]
+ * @property {boolean} [allowDuplicatePaths]
  * @property {string} [backLinkText]
  * @property {Record<string, FormConfigChapter>} [chapters]
  * @property {(props: any) => JSX.Element} [confirmation]
