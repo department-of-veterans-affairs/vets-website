ed3138437febd21bd048821bd1136512526b9ae3
s-caso <sebastian.caso@accenturefederal.com>
2026-02-04 09:52:53 -0500

    add claimant info call and wire it into prefill

--- src/applications/edu-benefits/10297/containers/App.jsx
+++ src/applications/edu-benefits/10297/containers/App.jsx
@@ -1,18 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import { isLoggedIn } from 'platform/user/selectors';
-import { useSelector } from 'react-redux';
 import formConfig from '../config/form';
 import { addStyleToShadowDomOnPages } from '../../utils/helpers';
 import NeedHelp from '../components/NeedHelp';
 import Breadcrumbs from '../components/Breadcrumbs';
 import manifest from '../manifest.json';
 import { TITLE } from '../constants';
+import { fetchPersonalInformation } from '../actions';
 
-export default function App({ location, children }) {
-  const userLoggedIn = useSelector(state => isLoggedIn(state));
+function App({
+  location,
+  children,
+  userLoggedIn,
+  getPersonalInformation,
+  user,
+}) {
+  const [fetchedUserInfo, setFetchedUserInfo] = useState(false);
+
   useEffect(() => {
     // Insert CSS to hide 'For example: January 19 2000' hint on memorable dates
     // (can't be overridden by passing 'hint' to uiOptions):
@@ -23,6 +31,7 @@ export default function App({ location, children }) {
     );
     document.title = `${TITLE} | Veterans Affairs`;
   });
+
   useEffect(
     () => {
       if (!userLoggedIn && location.pathname !== '/introduction') {
@@ -31,6 +40,20 @@ export default function App({ location, children }) {
     },
     [userLoggedIn, location],
   );
+
+  useEffect(
+    () => {
+      if (!user?.login?.currentlyLoggedIn) {
+        return;
+      }
+      if (!fetchedUserInfo) {
+        setFetchedUserInfo(true);
+        getPersonalInformation();
+      }
+    },
+    [fetchedUserInfo, getPersonalInformation, user?.login?.currentlyLoggedIn],
+  );
+
   return (
     <div className="form-22-10297-container row">
       <div className="vads-u-padding-left--0">
@@ -46,5 +69,22 @@ export default function App({ location, children }) {
 
 App.propTypes = {
   children: PropTypes.node,
+  getPersonalInformation: PropTypes.func,
   location: PropTypes.object,
+  user: PropTypes.object,
+  userLoggedIn: PropTypes.bool,
 };
+
+const mapStateToProps = state => ({
+  userLoggedIn: isLoggedIn(state),
+  user: state.user,
+});
+
+const mapDispatchToProps = {
+  getPersonalInformation: fetchPersonalInformation,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
