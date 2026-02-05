6264d9fce658d4832aebce660ba58292b2162bde
govcioChad <162637891+govcioChad@users.noreply.github.com>
2026-02-05 07:49:46 -0800

    added comma

--- src/applications/edu-benefits/10272/config/form.js
+++ src/applications/edu-benefits/10272/config/form.js
@@ -167,7 +167,7 @@ const formConfig = {
       pages: {
         remarks: {
           path: 'remarks',
-          title: 'Enter your remarks'
+          title: 'Enter your remarks',
           uiSchema: remarks.uiSchema,
           schema: remarks.schema,
         },
