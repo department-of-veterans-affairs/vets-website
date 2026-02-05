fdb393d972bc3be4cf1b51d5d94d87d4db8fb15b
Chris Kim <42885441+chriskim2311@users.noreply.github.com>
2026-02-05 10:02:23 -0800

    lint

--- src/applications/discharge-wizard/components/Homepage.jsx
+++ src/applications/discharge-wizard/components/Homepage.jsx
@@ -243,4 +243,7 @@ const mapDispatchToProps = {
   setIntroPageViewed: updateIntroPageViewed,
 };
 
-export default connect(null, mapDispatchToProps)(HomePage);
+export default connect(
+  null,
+  mapDispatchToProps
+)(HomePage);
