d34df6780f9f8b648f9d40c04b2aae3b706a4fe8
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2026-02-08 07:47:31 -0800

    a11y fixes: confirmation page h2 focus, yes/no labels

--- src/applications/edu-benefits/0803/containers/ConfirmationPage.jsx
+++ src/applications/edu-benefits/0803/containers/ConfirmationPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { ConfirmationView } from 'platform/forms-system/src/js/components/ConfirmationView';
@@ -21,9 +21,32 @@ export const getClaimIdFromLocalStage = () => {
 };
 
 function AlertBox() {
+  const ref = useRef(null);
+  const scrollToH2 = () => {
+    const headerH2 = document.querySelector('#next-steps-header');
+    scrollAndFocus(headerH2);
+  };
+  useEffect(() => {
+    const alertNode = ref.current;
+    if (alertNode) {
+      alertNode.addEventListener('va-component-did-load', scrollToH2);
+    }
+    return function cleanup() {
+      if (alertNode) {
+        alertNode.removeEventListener('va-component-did-load', scrollToH2);
+      }
+    };
+  }, []);
   return (
-    <va-alert close-btn-aria-label="Close notification" status="into" visible>
-      <h2 slot="headline">Complete all submission steps</h2>
+    <va-alert
+      ref={ref}
+      close-btn-aria-label="Close notification"
+      status="into"
+      visible
+    >
+      <h2 id="next-steps-header" slot="headline">
+        Complete all submission steps
+      </h2>
       <p className="vads-u-margin-y--0">
         This form requires additional steps for successful submission. Follow
         the instructions below carefully to ensure your form is submitted
@@ -67,14 +90,6 @@ export const ConfirmationPage = props => {
     [submission],
   );
 
-  useEffect(() => {
-    // make sure we scroll to and focus on the
-    // alert notifying the user that additional
-    // steps are required
-    const firsth2 = document.querySelector('va-alert > h2');
-    scrollAndFocus(firsth2);
-  }, []);
-
   return (
     <ConfirmationView
       formConfig={props.route?.formConfig}
@@ -84,75 +99,72 @@ export const ConfirmationPage = props => {
         showButtons: true,
       }}
     >
-      <div data-testid="download-link">
-        <AlertBox />
-        <h2 className="vads-u-font-size--h2 vad-u-margin-top--0">
-          To submit your form, follow the steps below
-        </h2>
-        <ConfirmationProcessList
-          pdfUrl={`${
-            environment.API_URL
-          }/v0/education_benefits_claims/download_pdf/${claimId}`}
-          trackingPrefix={route?.formConfig?.trackingPrefix}
-        />
-        <va-alert status="warning" visible={!form.data.hasPreviouslyApplied}>
-          <h2 slot="headline">Additional form needed</h2>
-          <p>
-            You’ll need to apply and be found eligible for the VA education
-            benefit under which you want your reimbursement processed.
-          </p>
-          <p>
-            <va-link
-              external
-              href="https://www.va.gov/education/apply-for-gi-bill-form-22-1990/introduction"
-              text="Application for VA Education Benefits Form 22-1990"
-            />
-            , <strong>or</strong>
-          </p>
-          <p>
-            <va-link
-              external
-              href="https://www.va.gov/family-and-caregiver-benefits/education-and-careers/apply-for-dea-fry-form-22-5490/introduction"
-              text="Apply for VA education benefits as a dependent using Form 22-5490"
-            />
-            , <strong>or</strong>
-          </p>
-          <p>
-            <va-link
-              external
-              href="https://www.va.gov/family-and-caregiver-benefits/education-and-careers/transferred-gi-bill-benefits/apply-form-22-1990e/introduction"
-              text="Apply to use transferred education benefits using Form 22-1990e"
-            />
-          </p>
-        </va-alert>
+      <AlertBox />
+      <h2 id="foo" className="vads-u-font-size--h2 vad-u-margin-top--0">
+        To submit your form, follow the steps below
+      </h2>
+      <ConfirmationProcessList
+        pdfUrl={`${
+          environment.API_URL
+        }/v0/education_benefits_claims/download_pdf/${claimId}`}
+        trackingPrefix={route?.formConfig?.trackingPrefix}
+      />
+      <va-alert status="warning" visible={!form.data.hasPreviouslyApplied}>
+        <h2 slot="headline">Additional form needed</h2>
         <p>
-          <va-button
-            secondary
-            text="Print this page"
-            data-testid="print-page"
-            onClick={() => window.print()}
-          />
+          You’ll need to apply and be found eligible for the VA education
+          benefit under which you want your reimbursement processed.
         </p>
         <p>
           <va-link
-            onClick={goBack}
-            class="screen-only vads-u-margin-top--1 vads-u-font-weight--bold"
-            data-testid="back-button"
-            text="Back"
-            href="#"
+            external
+            href="https://www.va.gov/education/apply-for-gi-bill-form-22-1990/introduction"
+            text="Application for VA Education Benefits Form 22-1990"
+          />
+          , <strong>or</strong>
+        </p>
+        <p>
+          <va-link
+            external
+            href="https://www.va.gov/family-and-caregiver-benefits/education-and-careers/apply-for-dea-fry-form-22-5490/introduction"
+            text="Apply for VA education benefits as a dependent using Form 22-5490"
+          />
+          , <strong>or</strong>
+        </p>
+        <p>
+          <va-link
+            external
+            href="https://www.va.gov/family-and-caregiver-benefits/education-and-careers/transferred-gi-bill-benefits/apply-form-22-1990e/introduction"
+            text="Apply to use transferred education benefits using Form 22-1990e"
           />
         </p>
-        <h2>Regional Processing Office mailing addresses</h2>
-        <RegionalAccordion />
-        <h2 className="vads-u-font-size--h2 vad-u-margin-top--0">
-          What are my next steps?
-        </h2>
-        <p>
-          After you successfully submit your form, we will review your
-          documents. You should hear back within 30 days about your
-          reimbursement.
-        </p>
-      </div>
+      </va-alert>
+      <p>
+        <va-button
+          secondary
+          text="Print this page"
+          data-testid="print-page"
+          onClick={() => window.print()}
+        />
+      </p>
+      <p>
+        <va-link
+          onClick={goBack}
+          class="screen-only vads-u-margin-top--1 vads-u-font-weight--bold"
+          data-testid="back-button"
+          text="Back"
+          href="#"
+        />
+      </p>
+      <h2>Regional Processing Office mailing addresses</h2>
+      <RegionalAccordion />
+      <h2 className="vads-u-font-size--h2 vad-u-margin-top--0">
+        What are my next steps?
+      </h2>
+      <p>
+        After you successfully submit your form, we will review your documents.
+        You should hear back within 30 days about your reimbursement.
+      </p>
     </ConfirmationView>
   );
 };
