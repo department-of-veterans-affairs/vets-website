f8d2765c0529fcdb6c72413d62d4177732029c4c
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2026-02-09 13:37:12 -0800

    130977 - Adds new AVS PDF fetch and update to details pages to support the new two-step method (#42073)
    * Adds mocks for new /vaos/v2/appointments/avs_binaries/:appointment_id?:doc_ids endpoint
    * Separates data for initial metadata fetch only for AVS
    * Adds method to check for valid and correct data in AVS PDF objects or errors
    * Handles errors for AVS PDF data in the way that was previously handled (checking with UX on this)
    * Adds tests for all new functions and branches.
    * Fixes and adds AVS cypress tests for error handling

---
+++ src/applications/vaos/services/avs/index.js
@@ -0,0 +1,49 @@
+import { apiRequestWithUrl, parseApiList } from '../utils';
+
+/**
+ * @typedef {import('../../utils/avs').AvsObj} AvsObj
+ * @typedef {import('../../utils/avs').AvsFetchedData} AvsFetchedData
+ */
+
+/**
+ * Merges fetched AVS PDF binaries with existing AVS PDF metadata (ids and codes and NO binaries).
+ * @param {AvsObj[]} avsPdfArray AVS data from appointment list
+ * @returns {function(AvsFetchedData[]): AvsObj[]}
+ */
+function mergeAvsPdfData(avsPdfArray) {
+  /**
+   * Merge fetched AVS PDF binaries with existing AVS PDF metadata.
+   * @param {AvsFetchedData[]} fetchedAvsPdfs Fetched AVS PDFs
+   * @returns {AvsObj[]} Merged AVS PDFs with binaries and errors if they exist
+   */
+  return function withData(fetchedAvsPdfs) {
+    return avsPdfArray.map(avsPdfObj => {
+      const fetchedPdf = fetchedAvsPdfs.find(
+        fetched => fetched.docId === avsPdfObj.id,
+      );
+      return {
+        ...avsPdfObj,
+        binary: fetchedPdf.binary ?? null,
+        error: fetchedPdf.error ?? null,
+      };
+    });
+  };
+}
+
+/**
+ * Fetch AVS PDF binaries for a given appointment and merge with existing AVS PDF metadata.
+ * @param {string} appointmentId
+ * @param {AvsObj[]} avsPdfArray
+ * @returns {Promise<AvsObj[]>} Merged AVS PDFs with binaries and errors if they exist
+ */
+export async function fetchAvsPdfBinaries(appointmentId, avsPdfArray) {
+  const ids = avsPdfArray.map(avsPdfObj => avsPdfObj.id).join(',');
+  return apiRequestWithUrl(
+    `/vaos/v2/appointments/avs_binaries/${appointmentId}?doc_ids=${ids}`,
+    {
+      method: 'GET',
+    },
+  )
+    .then(parseApiList)
+    .then(mergeAvsPdfData(avsPdfArray));
+}
