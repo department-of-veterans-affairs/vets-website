86d65e1ed9b832de75a704f6ebbba9e9bb81cda5
aafmisra <aafmisra@gmail.com>
2026-02-09 12:55:20 -0500

    allow for combined option content

--- src/applications/dependents/686c-674/components/AddDependentsOptionsDescription.jsx
+++ src/applications/dependents/686c-674/components/AddDependentsOptionsDescription.jsx
@@ -15,20 +15,7 @@ export const AddDependentsOptionsDescription = () => {
     formData?.['view:addDependentOptions']?.addDisabledChild;
 
   let description = <></>;
-  if (isAddingStudent) {
-    description = (
-      <div>
-        <strong>
-          If you’re adding a child 18 to 23 years old who’ll be attending
-          school, and
-        </strong>{' '}
-        if you never received benefits for this child, you must also select the
-        “An unmarried child under age 18” checkbox so they can be added to the
-        system. <strong>You will enter this child twice.</strong>
-      </div>
-    );
-  }
-  if (isAddingDisabledChild) {
+  if (isAddingStudent && isAddingDisabledChild) {
     description = (
       <div>
         We’ll ask you to submit these documents at the end of this form:
@@ -50,6 +37,35 @@ export const AddDependentsOptionsDescription = () => {
         if you never received benefits for this child, you must also select the
         “An unmarried child under age 18” checkbox so they can be added to the
         system. <strong>You will enter this child twice.</strong>
+      </div>
+    );
+  } else if (isAddingStudent) {
+    description = (
+      <div>
+        <strong>
+          If you’re adding a child 18 to 23 years old who’ll be attending
+          school, and
+        </strong>{' '}
+        if you never received benefits for this child, you must also select the
+        “An unmarried child under age 18” checkbox so they can be added to the
+        system. <strong>You will enter this child twice.</strong>
+      </div>
+    );
+  } else if (isAddingDisabledChild) {
+    description = (
+      <div>
+        We’ll ask you to submit these documents at the end of this form:
+        <ul>
+          <li>
+            Copies of medical records that document your child’s permanent
+            physical or mental disability <strong>and</strong>
+          </li>
+          <li>
+            A statement from your child’s doctor that shows the type{' '}
+            <strong>and</strong> severity of the child’s permanent physical or
+            mental disability
+          </li>
+        </ul>
       </div>
     );
   }
