10069d37aef8da4dae4766508b3764b1209fd09e
Ian Harrison <ian.harrison@adhocteam.us>
2026-02-10 16:23:07 -0600

    update comments

--- src/applications/simple-forms/mock-simple-forms-patterns-v3/pages/supportingDocuments.js
+++ src/applications/simple-forms/mock-simple-forms-patterns-v3/pages/supportingDocuments.js
@@ -14,12 +14,12 @@ export default {
       title: 'Supporting documents',
       required: true,
       accept: '.png,.pdf,.txt,.jpg,.jpeg',
-      hint: 'Upload a file that is between 1KB and 100MB',
+      hint: 'Upload a file that is between 1B and 100MB',
       headerSize: '4',
       formNumber: '31-4159',
       skipUpload: true,
       maxFileSize: 1024 * 1024 * 100, // 100MB
-      minFileSize: 1, // 1KB
+      minFileSize: 1, // 1B
       errorMessages: {
         additionalInput: 'Choose a document status',
       },
