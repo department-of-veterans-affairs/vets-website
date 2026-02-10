53483a45cc6ea575ecb734c3b8de2a27e1e4fa8f
Matt Floyd <233156714+mfloyd-iat@users.noreply.github.com>
2026-02-10 16:35:49 -0500

    Last attempt.  Only remaining problem was the Leave site dialog box.  Couldn't make that disappear after an hour, so I proceeded

--- src/applications/ask-va/containers/BusinessPersonal.jsx
+++ src/applications/ask-va/containers/BusinessPersonal.jsx
@@ -6,6 +6,8 @@ import { connect, useDispatch } from 'react-redux';
 import { withRouter } from 'react-router';
 import FormNavButtons from '~/platform/forms-system/src/js/components/FormNavButtons';
 import { ServerErrorAlert } from '../config/helpers';
+// import { Link } from 'react-router-dom';
+// import { Link } from 'platform/site-wide/routing';
 
 const BusinessPersonal = ({
   formData,
@@ -13,47 +15,100 @@ const BusinessPersonal = ({
   const [error] = useState(false);
   const dispatch = useDispatch();
 
-  useEffect(
-    () => {
-      focusElement('h2');
-    },
-    [formData.aboutYourself],
-  );
+  useEffect(() => {
+      const handleBeforeUnload = (event) => {
+        event.preventDefault();
+        event.returnValue
+      }
+      window.addEventListener('beforeunload', handleBeforeUnload)
+  })
+
 
   return !error ? (
     <>
-        <div display="flex" flex-direction="row" gap="4">
-            <VaCard 
-                icon-name="account_circle"
-            >
-            <div>
-                <h3 className="vads-u-margin-top--1">
-                Personal Support
-                </h3>
-                <p>
-                Use this option if you're a Veteran, family member, caregiver, or survivor - or if your asking a question for a veteran you know personally.
-                </p>
-                <VaLink className="vads-c-action-link--blue" to="/your-personal-information">
-                Ask a question
-                </VaLink>
-            </div>
-            </VaCard>
-            <VaCard 
-                icon-name="work"
-            >
-            <div>
-                <h3 className="vads-u-margin-top--1">
-                Work-related Support
-                </h3>
-                <p>
-                Use this option if you are from an organization contacting VA about a Veteran as a part of your job (for example, a VSO, provider or case manager).
-                </p>
-                <VaLink className="vads-c-action-link--blue" to="/">
-                Ask a question
-                </VaLink>
-            </div>
-            </VaCard>
-        </div>
+    <div className="vads-l-grid-container">
+    <div className="vads-l-row vads-u-justify-content--center vads-u-margin-bottom--10">
+      {/* Card 1: 12 columns on mobile (stacked), 6 columns on tablet+ (side-by-side) */}
+      <div className="vads-l-col--12 medium-screen:vads-l-col--6 vads-u-padding--2 vads-u-background-color--yellow">
+        <VaCard 
+                  icon-name="account_circle"
+              >
+              <div>
+                  <h3 className="vads-u-margin-top--1">
+                  Personal Support
+                  </h3>
+                  <p>
+                  Use this option if you're a Veteran, family member, caregiver, or survivor - or if your asking a question for a veteran you know personally.
+                  </p>
+                  {/* <a 
+                    href="/contact-us/ask-va/initial-question"
+                    onClick={handleNavigation}
+                    className="vads-c-action-link--blue"
+                  >
+                    Ask a new question
+                  </a> */}
+                  {/* <button 
+                    type="button"
+                    onClick={handleClick}
+                    className="vads-c-action-link--blue vads-u-padding--0 vads-u-margin--0"
+                    style={{ background: 'none', border: 'none', textAlign: 'left' }}
+                  >
+                    Ask a new question
+                  </button> */}
+                  {/* <va-link
+                    active
+                    href="/contact-us/ask-va/initial-question"
+                    text="Ask a new question"
+                  /> */}
+                  {/* <div className="vads-u-margin-top--4">
+                  <Link 
+                    to="/contact-us/ask-va/initial-question" 
+                    className="vads-c-action-link--blue"
+                  >
+                    Ask a new question
+                  </Link>
+                  </div> */}
+                  <va-link-action
+                    href="/contact-us/ask-va/initial-question"
+                    text="Ask a new question"
+                    type="secondary"
+                    external={false}
+                  />
+                  {/* <a href="/initial-question" class="vads-c-action-link--blue">Ask a new question</a> */}
+                  {/* <VaLink className="vads-c-action-link--blue" to="">
+                  Ask a question
+                  </VaLink> */}
+              </div>
+              </VaCard>
+      </div>
+
+      {/* Card 2 */}
+      <div className="vads-l-col--12 medium-screen:vads-l-col--6 vads-u-padding--2">
+        <VaCard 
+                  icon-name="work"
+              >
+              <div>
+                  <h3 className="vads-u-margin-top--1">
+                  Work-related Support
+                  </h3>
+                  <p>
+                  Use this option if you are from an organization contacting VA about a Veteran as a part of your job (for example, a VSO, provider or case manager).
+                  </p>
+                  <va-link-action
+                    href="/contact-us/ask-va/"
+                    text="Ask a new question"
+                    type="secondary"
+                    external={false}
+                  />
+                   {/* <a href="/" class="vads-c-action-link--blue">Ask a new question</a> */}
+                  {/* <VaLink className="vads-c-action-link--blue" to="/">
+                  Ask a question
+                  </VaLink> */}
+              </div>
+              </VaCard>
+      </div>
+    </div>
+    </div>
     </>
   ) : (
     <ServerErrorAlert />
