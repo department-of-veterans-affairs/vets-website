7f0367f2dc23a8e3c01760d219cccfdc2588dae6
Matt Long <6738544+longmd@users.noreply.github.com>
2026-02-10 15:50:23 -0500

    10-10d | Permanently redirect to v2

--- src/applications/ivc-champva/10-10D/containers/App.jsx
+++ src/applications/ivc-champva/10-10D/containers/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { getAppUrl } from 'platform/utilities/registry-helpers';
@@ -7,7 +7,7 @@ import { VaBreadcrumbs } from '@department-of-veterans-affairs/component-library
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from '../config/form';
 import { useBrowserMonitoring } from '../helpers/useBrowserMonitoring';
-import { addStyleToShadowDomOnPages, isExpired } from '../../shared/utilities';
+import { addStyleToShadowDomOnPages } from '../../shared/utilities';
 
 const breadcrumbList = [
   { href: '/', label: 'Home' },
@@ -30,23 +30,9 @@ const breadcrumbList = [
 ];
 
 export default function App({ location, children }) {
-  const {
-    isLoadingFeatureFlags,
-    isLoadingProfile,
-    isMergedFormEnabled,
-    savedForms,
-  } = useSelector(state => ({
-    isLoadingFeatureFlags: state?.featureToggles?.loading,
-    isMergedFormEnabled: state.featureToggles.form1010dExtended,
-    isLoadingProfile: state.user?.profile?.loading,
-    savedForms: state.user?.profile?.savedForms ?? [],
-  }));
-  const isAppLoading = useMemo(
-    () => isLoadingFeatureFlags || isLoadingProfile,
-    [isLoadingFeatureFlags, isLoadingProfile],
+  const isAppLoading = useSelector(
+    state => state?.featureToggles?.loading || state.user?.profile?.loading,
   );
-
-  const [routeChecked, setRouteChecked] = useState(false);
 
   document.title = `${formConfig.title} | Veterans Affairs`;
   useEffect(() => {
@@ -63,24 +49,15 @@ export default function App({ location, children }) {
   useLayoutEffect(
     () => {
       if (isAppLoading) return;
-      const hasSavedForm = savedForms.some(
-        ({ form, metaData }) =>
-          form === formConfig.formId && !isExpired(metaData?.expiresAt),
-      );
-      if (isMergedFormEnabled && !hasSavedForm) {
-        window.location.replace(getAppUrl('10-10d-extended'));
-        return;
-      }
-      setRouteChecked(true);
+      window.location.replace(getAppUrl('10-10d-extended'));
     },
-    [isAppLoading, isMergedFormEnabled, savedForms],
+    [isAppLoading],
   );
 
   // Add Datadog RUM to the app
   useBrowserMonitoring();
 
-  const showLoadingIndicator = isAppLoading || !routeChecked;
-  return showLoadingIndicator ? (
+  return isAppLoading ? (
     <va-loading-indicator
       message="Loading application..."
       class="vads-u-margin-y--4"
