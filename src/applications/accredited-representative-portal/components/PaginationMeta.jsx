feb51278cb009a6cc1739ab29eec9a9e491a9185
jquispe-oddball <jennifer.quispe@oddball.io>
2026-02-11 14:18:17 -0500

    131743 - failing tests update

--- src/applications/accredited-representative-portal/components/PaginationMeta.jsx
+++ src/applications/accredited-representative-portal/components/PaginationMeta.jsx
@@ -32,15 +32,14 @@ const PaginationMeta = ({ meta, results, resultType, defaults }) => {
 
   const userName = profile ? (
     <span className="poa-request__user-name">
-      {`"You (${profile.firstName.toLowerCase()} ${profile.lastName.toLowerCase()}
-      )"`}
+      {`"You (${profile.firstName.toLowerCase()} ${profile.lastName.toLowerCase()})"`}
     </span>
   ) : null;
   return (
     <p className="poa-request__meta" role="text">
-      {`Showing ${totalCount > 0 && initCount} - ${totalCount > 0 &&
-        pageSizeCount} of ${totalCount} ${searchStatus || ''} ${resultType ||
-        ''} ${selectedIndividual === 'you' ? 'for ' : ''}`}
+      {`Showing ${totalCount > 0 &&
+        `${initCount}-${pageSizeCount} of`} ${totalCount} ${searchStatus ||
+        ''} ${resultType || ''} ${selectedIndividual === 'you' ? 'for ' : ''}`}
       {selectedIndividual === 'you' ? userName : ''}
       {` sorted by `}“
       <strong>
