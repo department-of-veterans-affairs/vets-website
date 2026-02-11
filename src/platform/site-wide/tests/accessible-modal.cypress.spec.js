5cc415fbd4fbd407c6f90b5fc596d48cb108e40e
Kerry Powell <1776069+powellkerry@users.noreply.github.com>
2026-02-11 10:29:02 -0700

    fix tests

--- src/platform/site-wide/tests/accessible-modal.cypress.spec.js
+++ src/platform/site-wide/tests/accessible-modal.cypress.spec.js
@@ -1,48 +1,82 @@
-const overlay = '#modal-crisisline';
-const firstModalItem = 'a[href="tel:988"]';
-const closeControl = '.va-crisis-panel.va-modal-inner button';
-const firstOpenControl = '[data-show="#modal-crisisline"]';
-const thirdOpenControl = 'footer .va-button-link.va-overlay-trigger';
-const lastModalItem = 'a[href="https://www.veteranscrisisline.net/"]';
-
 describe('Accessible Modal Test', () => {
+  let overlay;
+  let firstModalItem;
+  let closeControl;
+  let firstOpenControl;
+  let thirdOpenControl;
+  let lastModalItem;
+  beforeEach(() => {
+    overlay = () =>
+      cy
+        .get('va-crisis-line-modal')
+        .first()
+        .shadow()
+        .find('va-modal');
+    firstModalItem = () =>
+      cy
+        .get('va-crisis-line-modal')
+        .first()
+        .shadow()
+        .find('a[href="tel:988"]');
+    closeControl = () =>
+      cy
+        .get('va-crisis-line-modal')
+        .first()
+        .shadow()
+        .find('button.va-modal-close');
+    firstOpenControl = () =>
+      cy
+        .get('va-crisis-line-modal')
+        .first()
+        .shadow()
+        .find('button.va-crisis-line.va-overlay-trigger');
+    thirdOpenControl = () =>
+      cy.get('footer .va-button-link.va-overlay-trigger');
+    lastModalItem = () =>
+      cy
+        .get('va-crisis-line-modal')
+        .first()
+        .shadow()
+        .find('a[href="https://www.veteranscrisisline.net/"]');
+  });
   it('Modal behaves appropriately in line with key presses', () => {
     cy.visit('/');
 
-    // Open modal
-    cy.get(firstOpenControl)
-      .first()
+    // Open modal with keyboard
+    firstOpenControl()
+      .should('be.visible')
+      .scrollIntoView()
       .focus()
       .realPress('Enter');
+    // Wait for modal to open
+    overlay().should('be.visible');
     cy.injectAxeThenAxeCheck();
 
-    // Trap backward traversal
-    cy.get(firstModalItem)
+    closeControl()
       .should('be.focused')
-      .repeatKey(['Shift', 'Tab'], 2);
-    cy.get(lastModalItem).should('be.focused');
+      .repeatKey(['Shift', 'Tab'], 1);
+
+    lastModalItem().should('be.focused');
 
     // Trap forward traversal
     cy.realPress('Tab');
-    cy.get(closeControl).should('be.focused');
+    cy.realPress('Tab');
+
+    firstModalItem().should('be.focused');
 
     // Escape modal
     cy.realPress('Escape');
-    cy.get(overlay).should('not.have.class', 'va-overlay--open');
-    cy.get('body').should('not.have.class', 'va-pos-fixed');
+    overlay().should('not.be.visible');
 
     // Return focus to appropriate open controls
-    cy.get(firstOpenControl)
-      .first()
-      .should('be.focused');
+    firstOpenControl().should('be.focused');
 
-    cy.get(thirdOpenControl)
-      .first()
+    thirdOpenControl()
       .focus()
       .realPress('Enter');
-    cy.get(firstModalItem).should('be.focused');
+    closeControl().should('be.focused');
 
     cy.realPress('Escape');
-    cy.get(thirdOpenControl).should('be.focused');
+    thirdOpenControl().should('be.focused');
   });
 });
