427b7253a3bbb3628c693d32d53da3d6f3471c07
Daniel Gading <2748839+dgading@users.noreply.github.com>
2026-02-11 16:34:53 -0500

    534ez add camel case claim type in submit transformer

--- src/applications/survivors-benefits/utils/migrations/validateDICLabel.js
+++ src/applications/survivors-benefits/utils/migrations/validateDICLabel.js
@@ -2,7 +2,6 @@ export default function validateDICLabel(savedData) {
   const { formData, metadata } = savedData;
   if (formData?.claims?.dic) {
     formData.claims.DIC = formData.claims.dic;
-    delete formData.claims.dic;
   }
   return {
     formData,
