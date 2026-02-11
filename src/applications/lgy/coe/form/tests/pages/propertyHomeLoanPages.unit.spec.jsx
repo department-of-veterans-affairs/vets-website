d29fc2ca19c0b73d5055a7d09cb3f7de47d7fecb
Andrés Rivera Toro <36863582+andresriveratoro@users.noreply.github.com>
2026-02-11 02:57:43 -0500

    add tests

---
+++ src/applications/lgy/coe/form/tests/pages/propertyHomeLoanPages.unit.spec.jsx
@@ -0,0 +1,40 @@
+import { expect } from 'chai';
+import { propertiesHomeLoansPages } from '../../pages/propertiesHomeLoansPages';
+
+describe('COE VA home loans array builder pages', () => {
+  it('includes summary and item pages', () => {
+    expect(propertiesHomeLoansPages).to.have.property(
+      'propertiesHomeLoansSummary',
+    );
+    expect(propertiesHomeLoansPages).to.have.property(
+      'propertyHomeLoanAddressPage',
+    );
+    expect(propertiesHomeLoansPages).to.have.property(
+      'propertyHomeLoanDetailsPage',
+    );
+    expect(propertiesHomeLoansPages).to.have.property(
+      'propertyHomeLoanEntitlementRestorationPage',
+    );
+    expect(propertiesHomeLoansPages).to.have.property(
+      'propertyHomeLoanDisasterDamagePage',
+    );
+  });
+
+  it('uses the rebuild paths', () => {
+    expect(propertiesHomeLoansPages.propertiesHomeLoansSummary.path).to.equal(
+      'relevant-prior-loans-summary',
+    );
+    expect(propertiesHomeLoansPages.propertyHomeLoanAddressPage.path).to.equal(
+      'relevant-prior-loans/:index/property-address',
+    );
+    expect(propertiesHomeLoansPages.propertyHomeLoanDetailsPage.path).to.equal(
+      'relevant-prior-loans/:index/property-details',
+    );
+    expect(
+      propertiesHomeLoansPages.propertyHomeLoanEntitlementRestorationPage.path,
+    ).to.equal('relevant-prior-loans/:index/entitlement-restoration');
+    expect(
+      propertiesHomeLoansPages.propertyHomeLoanDisasterDamagePage.path,
+    ).to.equal('relevant-prior-loans/:index/disaster-damage');
+  });
+});
