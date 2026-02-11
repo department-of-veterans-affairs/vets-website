137ec642bb5cef7abdb277afb0eef4d25246730a
Michael Moyer <87040148+mmoyer-va@users.noreply.github.com>
2026-02-11 11:54:17 -0500

    Attempting to fix Node 22 test issues

--- src/applications/mhv-medical-records/tests/e2e/medical-records-network-error-labs-and-tests.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/medical-records-network-error-labs-and-tests.cypress.spec.js
@@ -8,6 +8,7 @@ describe('Medical Records View Labs and Tests', () => {
   });
 
   it('Visits Medical Records, Views Network Error On Labs and Tests List', () => {
+    // Intercept v1 endpoints
     cy.intercept('GET', '/my_health/v1/medical_records/labs_and_tests*', {
       statusCode: 404,
     });
@@ -17,12 +18,21 @@ describe('Medical Records View Labs and Tests', () => {
     cy.intercept('GET', '/my_health/v1/medical_records/imaging*', {
       statusCode: 404,
     });
+    // Intercept v2 endpoints
+    cy.intercept('GET', '/my_health/v2/medical_records/labs_and_tests*', {
+      statusCode: 404,
+    });
+    cy.intercept('GET', '/my_health/v2/medical_records/imaging*', {
+      statusCode: 404,
+    });
     cy.visit('my-health/medical-records');
     cy.intercept('POST', '/my_health/v1/medical_records/session').as('session');
     cy.wait('@session');
 
     cy.visit('my-health/medical-records/labs-and-tests');
-    cy.get('[data-testid="expired-alert-message"]').should('be.visible');
+    cy.get('[data-testid="expired-alert-message"]', { timeout: 10000 }).should(
+      'be.visible',
+    );
 
     // Axe check
     cy.injectAxe();
