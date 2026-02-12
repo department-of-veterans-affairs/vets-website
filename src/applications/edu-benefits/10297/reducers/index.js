d2a2f3302829ef04adbfef7e64c986c17a09812d
s-caso <94458706+s-caso@users.noreply.github.com>
2026-02-12 11:54:35 -0500

    add note to prefill file (#42036)
    * add note to prefill file
    
    * add claimant info call and wire it into prefill
    
    * remove ssn from submission data and prevent personal info blocking user
    
    * remove claimantType variable
    
    * remove refernce to claimantType from test file
    
    * Add useProfileFullName to fix statement of truth validation mismatch
    
    * use fallback profile ssn
    
    * add prefill to mapState to props
    
    * change claimant info call to meb controller
    
    * add temp type to claimant call in 10297
    
    * add country code conversion
    
    * lint issue
    
    ---------
    
    Co-authored-by: s.caso <s.caso@afs.com>

--- src/applications/edu-benefits/10297/reducers/index.js
+++ src/applications/edu-benefits/10297/reducers/index.js
@@ -1,6 +1,46 @@
 import { createSaveInProgressFormReducer } from 'platform/forms/save-in-progress/reducers';
 import formConfig from '../config/form';
 
+import {
+  FETCH_PERSONAL_INFORMATION,
+  FETCH_PERSONAL_INFORMATION_SUCCESS,
+  FETCH_PERSONAL_INFORMATION_FAILED,
+} from '../actions';
+
+const initialState = {
+  formData: {},
+  personalInfoFetchInProgress: false,
+  personalInfoFetchComplete: false,
+  isPersonalInfoFetchFailed: false,
+};
+
 export default {
   form: createSaveInProgressFormReducer(formConfig),
+  data: (state = initialState, action) => {
+    switch (action.type) {
+      case FETCH_PERSONAL_INFORMATION:
+        return {
+          ...state,
+          personalInfoFetchInProgress: true,
+        };
+      case FETCH_PERSONAL_INFORMATION_SUCCESS:
+        return {
+          ...state,
+          isPersonalInfoFetchFailed: false,
+          personalInfoFetchComplete: true,
+          personalInfoFetchInProgress: false,
+          formData: action?.response || {},
+        };
+      case FETCH_PERSONAL_INFORMATION_FAILED:
+        return {
+          ...state,
+          isPersonalInfoFetchFailed: true,
+          personalInfoFetchComplete: true,
+          personalInfoFetchInProgress: false,
+          formData: action?.response || {},
+        };
+      default:
+        return state;
+    }
+  },
 };
