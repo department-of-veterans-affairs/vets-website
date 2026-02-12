4997e8b192059c14496a46c6433c8ded545da094
Garlon Garth <garlon.garth@govcio.com>
2026-02-12 16:58:18 -0500

    Needed to add styling support via HTML tags in i18n for bolded text. Add in action_type_map that helps put in what action to display on the details alert based off of diary code. Added global namespace for CDP app to improve code maintenance.

---
+++ src/applications/combined-debt-portal/debt-letters/const/actionTypeMap.js
@@ -0,0 +1,12 @@
+export const ACTION_TYPE_MAP = {
+  '811': 'compromise offer',
+  '821': 'dispute',
+  '822': 'dispute',
+  '825': 'dispute',
+  '801': 'waiver request',
+  '802': 'waiver request',
+  '803': 'waiver request',
+  '804': 'waiver request',
+  '809': 'waiver request',
+  '820': 'waiver request',
+};
