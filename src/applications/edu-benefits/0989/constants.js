cf7418f0e26d68177c78b8002b9e88727fdcaf7c
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2026-02-13 14:34:33 -0800

    VEBT-2891 intro page and auth setup for 0989

--- src/applications/edu-benefits/0989/constants.js
+++ src/applications/edu-benefits/0989/constants.js
@@ -1,2 +1,8 @@
-export const TITLE = '22-0989 Entitlement restoration';
-export const SUBTITLE = 'entitlement restoration (VA Form 22-0989)';
+export const TITLE =
+  'Request education benefit entitlement restoration due to school closure, program suspension or withdrawal';
+export const SUBTITLE =
+  'Education benefit entitlement restoration request due to school closure, program suspension or withdrawal (VA Form 22-0989)';
+
+export const OMB_RES_BURDEN = 15;
+export const OMB_NUMBER = '2900-0859';
+export const OMB_EXP_DATE = '05/31/2027';
