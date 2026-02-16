a923f40df5192459c2acc41d376bd0ff78e1af39
Andrés Rivera Toro <36863582+andresriveratoro@users.noreply.github.com>
2026-02-16 11:50:52 -0500

    CVE 2358 step 3 ab (#42246)
    * add step 3 item pages
    
    * add array builder for step 3
    
    * add tests
    
    * update field name
    
    * fix test
    
    * fix test
    
    * remove jsx fragments
    
    * use different address pattern

---
+++ src/applications/lgy/coe/form/components/PropertyAddress.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const PropertyAddress = ({ formData }) => {
+  const { city, postalCode, state, street1, street2, street3 } =
+    formData?.propertyAddress || {};
+
+  if (!street1 || !city || !state || !postalCode) {
+    return null;
+  }
+
+  return (
+    <p className="va-address-block vads-u-margin-top--6 vads-u-margin-left--0">
+      <span className="vads-u-font-weight--bold">Property Address</span>
+      <br />
+      {street1}
+      <br />
+      {street2 && (
+        <>
+          {street2}
+          <br />
+        </>
+      )}
+      {street3 && (
+        <>
+          {street3}
+          <br />
+        </>
+      )}
+      {city}, {state} {postalCode} <br />
+    </p>
+  );
+};
+
+PropertyAddress.propTypes = {
+  formData: PropTypes.object,
+};
