94ef35ae24ccea17959afe9551e66041eac6ab1d
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2026-02-17 16:48:30 -0500

    linter

--- src/applications/simple-forms/21-4138/tests/unit/config/form.unit.spec.jsx
+++ src/applications/simple-forms/21-4138/tests/unit/config/form.unit.spec.jsx
@@ -276,7 +276,8 @@ describe('formConfig', () => {
             });
 
             it('should display the page', () => {
-              expect(claimStatusToolPageNonVeteran.depends(formData)).to.be.true;
+              expect(claimStatusToolPageNonVeteran.depends(formData)).to.be
+              .true;
             });
           });
         });
