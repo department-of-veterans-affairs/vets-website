8a70434b4f0e0f8d519ad59dd4aa07be4f2cdf4e
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2026-02-17 23:29:36 +0000

    ci: switch build archive compression from bzip2 to zstd
    Replace single-threaded bzip2 with multithreaded zstd for build artifact
    compression/decompression, significantly reducing archive step time.
    
    - Update tar flags from -cjf/-xjf to --zstd -cf/--zstd -xf
    - Change artifact filenames from .tar.bz2 to .tar.zst
    - Add zstd detection to deploy.sh and partial-deploy.sh
    - Replace bzip2 with zstd in Cypress container apt-get installs
    - Update pe-get-content.js S3 URL to use .tar.zst

--- script/github-actions/pe-get-content.js
+++ script/github-actions/pe-get-content.js
@@ -5,6 +5,6 @@ const mostRecent = process.env.MOST_RECENT_CONTENT;
 const AWS_URL = `https://vetsgov-website-builds-s3-upload.s3-us-gov-west-1.amazonaws.com/${mostRecent.substring(
   0,
   mostRecent.lastIndexOf('/'),
-)}/vagovprod.tar.bz2`;
+)}/vagovprod.tar.zst`;
 
 core.exportVariable('AWS_URL', AWS_URL);
