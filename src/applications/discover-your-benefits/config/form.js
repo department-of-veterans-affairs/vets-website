0320dc3e4aa7a109c53c22fa826a486f4394e641
Mikal Ahmed <mikalyusufahmed@gmail.com>
2026-02-17 12:33:45 -0500

    Results page feature flag (#42224)
    * Modified code so that we can display the results page changes on staging/dev and keep the old version on production.

--- src/applications/discover-your-benefits/config/form.js
+++ src/applications/discover-your-benefits/config/form.js
@@ -16,6 +16,7 @@ import manifest from '../manifest.json';
 
 import IntroductionPage from '../containers/IntroductionPage';
 import ConfirmationPage from '../containers/ConfirmationPage';
+import ConfirmationPageCopy from '../containers/ConfirmationPageRemoveAfterTesting';
 
 // pages
 import goals from '../pages/goals';
@@ -47,7 +48,11 @@ export const formConfig = {
   submit: submitHandler,
   trackingPrefix: 'discover-your-benefits-',
   introduction: IntroductionPage,
-  confirmation: ConfirmationPage,
+  // Need to insert a feature flag here
+  confirmation:
+    !environment.isProduction() || environment.isTest()
+      ? ConfirmationPage
+      : ConfirmationPageCopy,
   v3SegmentedProgressBar: true,
   stepLabels: 'Goals;Service;Separation;Discharge;Disability;Review',
   formId: 'T-QSTNR',
--- src/applications/discover-your-benefits/constants/urls.js
+++ src/applications/discover-your-benefits/constants/urls.js
@@ -1,25 +1,41 @@
 export const URLS = Object.freeze({
   GIB_LEARN: 'https://www.va.gov/education/about-gi-bill-benefits/',
+  GIB_APPLY: 'https://www.va.gov/education/how-to-apply/',
   DBP_LEARN: 'https://skillbridge.osd.mil/',
   ECC_LEARN:
     'https://www.va.gov/careers-employment/education-and-career-counseling/',
+  ECC_APPLY:
+    'https://www.va.gov/careers-employment/education-and-career-counseling/apply-career-guidance-form-25-8832/introduction/',
   FHV_LEARN: 'https://www.opm.gov/fedshirevets/',
   SVC_LEARN:
     'https://www.va.gov/careers-employment/veteran-owned-business-support/',
   TAP_LEARN: 'https://www.dodtap.mil/dodtap/app/home',
   VRE_LEARN: 'https://www.va.gov/careers-employment/vocational-rehabilitation/',
+  VRE_APPLY:
+    'https://www.va.gov/careers-employment/vocational-rehabilitation/how-to-apply',
   VSC_LEARN: 'https://www.va.gov/careers-employment/vetsuccess-on-campus/',
   DHS_LEARN: 'https://www.va.gov/housing-assistance/disability-housing-grants/',
+  DHS_APPLY:
+    'https://www.va.gov/housing-assistance/disability-housing-grants/how-to-apply/',
   VAP_LEARN: 'https://www.va.gov/pension/eligibility/',
+  VAP_APPLY: 'https://www.va.gov/pension/how-to-apply/',
   MHC_LEARN:
     'https://www.va.gov/health-care/health-needs-conditions/mental-health/',
   FMP_LEARN: 'https://www.va.gov/health-care/foreign-medical-program/',
+  FMP_APPLY:
+    'https://www.va.gov/health-care/foreign-medical-program/register-form-10-7959f-1/introduction/',
   VGL_LEARN: 'https://www.va.gov/life-insurance/options-eligibility/vgli/',
   VAL_LEARN: 'https://www.va.gov/life-insurance/options-eligibility/valife/',
+  VAL_APPLY: 'https://insurance.va.gov/VALIFE/Fiduciary/',
   DIS_LEARN: 'https://www.va.gov/disability/',
+  DIS_APPLY: 'https://www.va.gov/disability/how-to-file-claim/',
   COE_LEARN: 'https://www.va.gov/housing-assistance/home-loans/',
+  COE_APPLY:
+    'https://www.va.gov/housing-assistance/home-loans/how-to-request-coe/',
   VAH_LEARN: 'https://www.va.gov/health-care/',
+  VAH_APPLY: 'https://www.va.gov/health-care/how-to-apply/',
   BUR_LEARN: 'https://www.va.gov/burials-memorials/eligibility/',
+  BUR_APPLY: 'https://www.va.gov/burials-memorials/pre-need-eligibility/',
   TGI_LEARN:
     'https://www.va.gov/education/transfer-post-9-11-gi-bill-benefits/',
   DCU_LEARN: 'https://www.va.gov/discharge-upgrade-instructions/',
