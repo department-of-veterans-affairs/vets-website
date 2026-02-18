9cc64ae8bc5af769bb2bd692b3ece5e27b2a0ad9
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2026-02-18 15:12:46 +0000

    ci: hybrid zstd/bz2 approach - recompress to bz2 for S3, remove apt-get from Cypress jobs
    - Build step: compress with zstd (~2s vs ~100s bzip2)
    - Cypress jobs: remove apt-get install step (zstd already in image)
    - Cypress jobs: replace curl with wget (already in image)
    - Archive job: recompress zstd -> bz2 with pbzip2 before S3 upload
    - S3 archives remain .tar.bz2 - zero compatibility risk
    - Deploy scripts and pe-get-content.js keep .tar.bz2 references

--- script/github-actions/pe-get-content.js
+++ script/github-actions/pe-get-content.js
@@ -5,6 +5,6 @@ const mostRecent = process.env.MOST_RECENT_CONTENT;
 const AWS_URL = `https://vetsgov-website-builds-s3-upload.s3-us-gov-west-1.amazonaws.com/${mostRecent.substring(
   0,
   mostRecent.lastIndexOf('/'),
-)}/vagovprod.tar.zst`;
+)}/vagovprod.tar.bz2`;
 
 core.exportVariable('AWS_URL', AWS_URL);
