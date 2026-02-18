c67fad72545e7e3cb8a140910d4f9710c70142e5
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2026-02-18 19:04:40 +0000

    refactor(avs): remove unused AppState interface

--- src/applications/avs/types/common.ts
+++ src/applications/avs/types/common.ts
@@ -8,12 +8,6 @@ export interface ManifestType {
   productId: string;
 }
 
-// Define the application state structure
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface AppState {
-  // Currently the reducer is empty, but will be expanded when state management is added
-}
-
 // Route parameters
 export interface RouteParams {
   id?: string;
