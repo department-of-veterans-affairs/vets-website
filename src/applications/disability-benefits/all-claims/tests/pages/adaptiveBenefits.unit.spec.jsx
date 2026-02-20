f6117b3a4eaa10b6818c3ffc0a19c5501ed4a33f
= <=>
2026-02-20 09:18:18 -0600

    yesNoUI VADS to replace imposter component

--- src/applications/disability-benefits/all-claims/tests/pages/adaptiveBenefits.unit.spec.jsx
+++ src/applications/disability-benefits/all-claims/tests/pages/adaptiveBenefits.unit.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { expect } from 'chai';
 import { DefinitionTester } from 'platform/testing/unit/schemaform-utils';
-import { mount } from 'enzyme';
+import { render } from '@testing-library/react';
 import formConfig from '../../config/form';
 
 describe('526 adaptive benefits page', () => {
@@ -11,7 +11,7 @@ describe('526 adaptive benefits page', () => {
   } = formConfig.chapters.disabilities.pages.adaptiveBenefits;
 
   it('should render', () => {
-    const form = mount(
+    const { container } = render(
       <DefinitionTester
         definitions={formConfig.defaultDefinitions}
         schema={schema}
@@ -19,13 +19,15 @@ describe('526 adaptive benefits page', () => {
         data={{}}
       />,
     );
-    expect(form.find('input').length).to.equal(4);
-    expect(form.find('va-alert').length).to.equal(0);
-    form.unmount();
+    // Should have 2 va-radio components (modifyingHome and modifyingCar)
+    expect(container.querySelectorAll('va-radio').length).to.equal(2);
+    // Should have 4 va-radio-option elements (2 yes/no pairs)
+    expect(container.querySelectorAll('va-radio-option').length).to.equal(4);
+    expect(container.querySelector('va-alert')).to.not.exist;
   });
 
   it('should show the double vehicle allowance alert', () => {
-    const form = mount(
+    const { container } = render(
       <DefinitionTester
         definitions={formConfig.defaultDefinitions}
         schema={schema}
@@ -38,8 +40,10 @@ describe('526 adaptive benefits page', () => {
         }}
       />,
     );
-    expect(form.find('input').length).to.equal(6);
-    expect(form.find('va-alert').length).to.equal(1);
-    form.unmount();
+    // Should have 3 va-radio components (modifyingHome, modifyingCar, and alreadyClaimedVehicleAllowance)
+    expect(container.querySelectorAll('va-radio').length).to.equal(3);
+    // Should have 6 va-radio-option elements (3 yes/no pairs)
+    expect(container.querySelectorAll('va-radio-option').length).to.equal(6);
+    expect(container.querySelector('va-alert')).to.exist;
   });
 });
--- src/applications/disability-benefits/all-claims/tests/pages/aidAndAttendance.unit.spec.jsx
+++ src/applications/disability-benefits/all-claims/tests/pages/aidAndAttendance.unit.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { expect } from 'chai';
 import { DefinitionTester } from 'platform/testing/unit/schemaform-utils';
-import { mount } from 'enzyme';
+import { render } from '@testing-library/react';
 import formConfig from '../../config/form';
 
 describe('526 aid and attendance page', () => {
@@ -11,7 +11,7 @@ describe('526 aid and attendance page', () => {
   } = formConfig.chapters.disabilities.pages.aidAndAttendance;
 
   it('should render', () => {
-    const form = mount(
+    const { container } = render(
       <DefinitionTester
         definitions={formConfig.defaultDefinitions}
         schema={schema}
@@ -19,7 +19,7 @@ describe('526 aid and attendance page', () => {
         data={{}}
       />,
     );
-    expect(form.find('input').length).to.equal(2);
-    form.unmount();
+    expect(container.querySelector('va-radio')).to.exist;
+    expect(container.querySelectorAll('va-radio-option').length).to.equal(2);
   });
 });
