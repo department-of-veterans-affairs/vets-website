2a8737a9b8f824054743bf6ffd0e74f8780afea8
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2026-02-20 13:35:25 -0700

    Add mulit-provider tests

--- src/applications/claims-status/tests/components/ClaimPage.unit.spec.jsx
+++ src/applications/claims-status/tests/components/ClaimPage.unit.spec.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { expect } from 'chai';
 import sinon from 'sinon';
+import * as reactRouterDom from 'react-router-dom-v5-compat';
+import * as featureToggles from '~/platform/utilities/feature-toggles';
 
 import { ClaimPage } from '../../containers/ClaimPage';
 import { renderWithRouter } from '../utils';
@@ -37,5 +39,88 @@ describe('<ClaimPage>', () => {
 
     unmount();
     expect(props.clearClaim.called).to.be.true;
+  });
+
+  describe('multi-provider functionality', () => {
+    let useSearchParamsStub;
+    let useFeatureToggleStub;
+    let mockSearchParams;
+
+    beforeEach(() => {
+      mockSearchParams = {
+        get: sinon.stub(),
+      };
+      useSearchParamsStub = sinon
+        .stub(reactRouterDom, 'useSearchParams')
+        .returns([mockSearchParams]);
+    });
+
+    afterEach(() => {
+      useSearchParamsStub.restore();
+      if (useFeatureToggleStub) {
+        useFeatureToggleStub.restore();
+      }
+    });
+
+    it('passes provider to getClaim when flag enabled and type param exists', () => {
+      mockSearchParams.get.withArgs('type').returns('lighthouse');
+      useFeatureToggleStub = sinon
+        .stub(featureToggles, 'useFeatureToggle')
+        .returns({
+          TOGGLE_NAMES: { cstMultiClaimProvider: 'cst_multi_claim_provider' },
+          useToggleValue: sinon.stub().returns(true),
+        });
+
+      props.getClaim = sinon.spy();
+
+      renderWithRouter(
+        <ClaimPage {...props}>
+          <div />
+        </ClaimPage>,
+      );
+
+      expect(props.getClaim.calledWith(1, sinon.match.any, 'lighthouse')).to.be
+        .true;
+    });
+
+    it('does not pass provider to getClaim when flag disabled', () => {
+      mockSearchParams.get.withArgs('type').returns('lighthouse');
+      useFeatureToggleStub = sinon
+        .stub(featureToggles, 'useFeatureToggle')
+        .returns({
+          TOGGLE_NAMES: { cstMultiClaimProvider: 'cst_multi_claim_provider' },
+          useToggleValue: sinon.stub().returns(false),
+        });
+
+      props.getClaim = sinon.spy();
+
+      renderWithRouter(
+        <ClaimPage {...props}>
+          <div />
+        </ClaimPage>,
+      );
+
+      expect(props.getClaim.calledWith(1, sinon.match.any, null)).to.be.true;
+    });
+
+    it('passes null provider when no type param in URL', () => {
+      mockSearchParams.get.withArgs('type').returns(null);
+      useFeatureToggleStub = sinon
+        .stub(featureToggles, 'useFeatureToggle')
+        .returns({
+          TOGGLE_NAMES: { cstMultiClaimProvider: 'cst_multi_claim_provider' },
+          useToggleValue: sinon.stub().returns(true),
+        });
+
+      props.getClaim = sinon.spy();
+
+      renderWithRouter(
+        <ClaimPage {...props}>
+          <div />
+        </ClaimPage>,
+      );
+
+      expect(props.getClaim.calledWith(1, sinon.match.any, null)).to.be.true;
+    });
   });
 });
