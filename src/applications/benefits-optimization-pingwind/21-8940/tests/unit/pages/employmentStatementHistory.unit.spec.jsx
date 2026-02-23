38b594468736c92983b09b1df0266aa86a779094
tim <20232041+Tchase44@users.noreply.github.com>
2026-02-23 12:25:18 -0500

    [BIO] 21-8940 expand test coverage

--- src/applications/benefits-optimization-pingwind/21-8940/tests/unit/pages/employmentStatementHistory.unit.spec.jsx
+++ src/applications/benefits-optimization-pingwind/21-8940/tests/unit/pages/employmentStatementHistory.unit.spec.jsx
@@ -1,46 +1,8 @@
-import {
-  testNumberOfErrorsOnSubmitForWebComponents,
-  testNumberOfWebComponentFields,
-} from 'platform/forms-system/test/pageTestHelpers.spec';
 import { expect } from 'chai';
 import { employmentAppliedFields } from '../../../definitions/constants';
 import { employmentStatementHistoryOptions } from '../../../pages/employmentStatementHistory';
-import formConfig from '../../../config/form';
 
-const {
-  schema,
-  uiSchema,
-} = formConfig.chapters.employmentAppliedChapter.pages.employmentHistorySummary;
-
-const pageTitle = 'employment application record';
-
-const mockDataForVeteranIsSelf = {
-  [employmentAppliedFields.parentObject]: {
-    [employmentAppliedFields.hasTriedEmployment]: true,
-  },
-};
-
-const expectedNumberOfFields = 1;
-testNumberOfWebComponentFields(
-  formConfig,
-  schema,
-  uiSchema,
-  expectedNumberOfFields,
-  pageTitle,
-  mockDataForVeteranIsSelf,
-);
-
-const expectedNumberOfErrors = 1;
-testNumberOfErrorsOnSubmitForWebComponents(
-  formConfig,
-  schema,
-  uiSchema,
-  expectedNumberOfErrors,
-  pageTitle,
-  mockDataForVeteranIsSelf,
-);
-
-describe('employmentStatementHistory options', () => {
+describe('21-8940 employmentStatementHistory options', () => {
   const { isItemIncomplete, text } = employmentStatementHistoryOptions;
 
   it('detects incomplete items', () => {
