5f4fe7d5d03e74c703417db82910f448349b34a1
Matt Long <6738544+longmd@users.noreply.github.com>
2026-02-24 12:03:01 -0500

    10-10d | Update sponsor chapter content (#42586)

--- src/applications/ivc-champva/10-10d-extended/chapters/sponsor/identityInformation.js
+++ src/applications/ivc-champva/10-10d-extended/chapters/sponsor/identityInformation.js
@@ -6,7 +6,7 @@ import content from '../../locales/en/content.json';
 import { titleWithRoleUI } from '../../utils/titles';
 import { validateSponsorSsn } from '../../utils/validations';
 
-const TITLE_TEXT = '%s identification information';
+const TITLE_TEXT = content['sponsor--identity-info-title'];
 
 const OPTS = { matchRole: 'sponsor', other: content['noun--veteran'] };
 const PAGE_TITLE = titleWithRoleUI(TITLE_TEXT, null, OPTS);
