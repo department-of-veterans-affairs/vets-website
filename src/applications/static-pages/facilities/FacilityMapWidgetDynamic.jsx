d7e31101b87effe5b7eabec32a6c62713f69e7e6
Bryan Thompson <18094023+bryan-thompsoncodes@users.noreply.github.com>
2026-02-24 10:39:42 -0800

    fix: auto-format files to satisfy updated prettier from main
    Prettier was updated via #42568 (yeoman dep refresh). The stricter
    version flags pre-existing formatting in files our PR touches.

--- src/applications/static-pages/facilities/FacilityMapWidgetDynamic.jsx
+++ src/applications/static-pages/facilities/FacilityMapWidgetDynamic.jsx
@@ -125,9 +125,9 @@ const mapStateToProps = store => ({
 
 FacilityMapWidgetDynamic.propTypes = {
   facilities: PropTypes.object,
-  multiLoading: PropTypes.object,
-  multiError: PropTypes.object,
   facilityID: PropTypes.string,
+  multiError: PropTypes.object,
+  multiLoading: PropTypes.object,
 };
 
 const mapDispatchToProps = {
