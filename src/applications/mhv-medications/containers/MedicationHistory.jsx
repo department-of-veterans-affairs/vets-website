b3a06fbc60f88d3ebdeb8a6e78bf3c2895e8ac9f
Langston Payne <20720474+oisho75@users.noreply.github.com>
2026-02-24 11:51:32 -0500

    Add new url and component for the Meds History page (#42462)
    * Add empty componenet and Url
    
    * Add custom hook based of Meds List
    
    * Specs for the new component with feature flag check
    
    * Update routes
    
    * Typo/delete comment. Add Page Type history
    
    * spec for new hook
    
    * Typo fix
    
    * Update link for med
    
    * Update link for test
    
    * BreadCrumbs and test
    
    * Added rx bread crumbs spec

--- src/applications/mhv-medications/containers/MedicationHistory.jsx
+++ src/applications/mhv-medications/containers/MedicationHistory.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom-v5-compat';
 import { focusElement } from '@department-of-veterans-affairs/platform-utilities/ui';
-import InProgressMedicationsProcessList from '../components/PrescriptionsInProgress/InProgressMedicationsProcessList';
 import NeedHelp from '../components/shared/NeedHelp';
 import ApiErrorNotification from '../components/shared/ApiErrorNotification';
-import useFetchPrescriptionsInProgress from '../hooks/PrescriptionsInProgress/useFetchPrescriptionsInProgress';
+import { useFetchMedicationHistory } from '../hooks/MedicationHistory/useFetchMedicationHistory';
 import { pageType } from '../util/dataDogConstants';
 
-const PrescriptionsInProgress = () => {
+const MedicationHistory = () => {
   const {
-    prescriptions,
+    // prescriptions,
     prescriptionsApiError,
     isLoading,
-  } = useFetchPrescriptionsInProgress();
+  } = useFetchMedicationHistory();
 
   const renderContent = () => {
     if (isLoading) {
@@ -26,13 +25,12 @@ const PrescriptionsInProgress = () => {
         </div>
       );
     }
-
     if (prescriptionsApiError) {
       return <ApiErrorNotification errorType="access" content="medications" />;
     }
-
-    // TODO: Implement empty state design for when there are no in-progress medications
-    return <InProgressMedicationsProcessList prescriptions={prescriptions} />;
+    // TODO: List of medications and sort
+    // console.log('prescriptions', prescriptions);
+    return <></>;
   };
 
   useEffect(() => {
@@ -41,21 +39,15 @@ const PrescriptionsInProgress = () => {
 
   return (
     <div>
-      <h1 data-testid="in-progress-medications-heading">
-        In-progress medications
-      </h1>
-      <p>
-        Medications that are shipped will remain in this list for 15 days from
-        the date of shipping. To review all your medications, go to your
-        medication history.
-      </p>
-      <Link to="/history">Go to your medication history</Link>
+      <h1 data-testid="medication-history-heading">Medication history</h1>
+      {/* TODO verify link destination */}
+      <Link to="/in-progress">Go to your in-progress medications</Link>
       <span className="vads-u-margin-x--1">|</span>
       <Link to="/refill">Refill medications</Link>
       {renderContent()}
-      <NeedHelp page={pageType.IN_PROGRESS} headingLevel={2} />
+      <NeedHelp page={pageType.HISTORY} headingLevel={2} />
     </div>
   );
 };
 
-export default PrescriptionsInProgress;
+export default MedicationHistory;
