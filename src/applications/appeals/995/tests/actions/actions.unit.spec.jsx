500db2f76ea24d1ee8f3de736ecc3679c4b65ef9
Randi Mays <19175324+randimays@users.noreply.github.com>
2026-02-24 12:34:12 -0600

    DR 122511 Refactor contestable issues calls to share between DR apps (#42054)

--- src/applications/appeals/995/tests/actions/actions.unit.spec.jsx
+++ src/applications/appeals/995/tests/actions/actions.unit.spec.jsx
@@ -1,12 +1,7 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-
-import * as apiUtils from 'platform/utilities/api';
-
 import { mockApiRequest } from 'platform/testing/unit/helpers';
-
 import {
-  getContestableIssues,
   fetchITF,
   ITF_FETCH_INITIATED,
   ITF_FETCH_SUCCEEDED,
@@ -16,80 +11,6 @@ import {
   ITF_CREATION_SUCCEEDED,
   ITF_CREATION_FAILED,
 } from '../../actions';
-
-import { CONTESTABLE_ISSUES_API } from '../../constants/apis';
-
-import {
-  FETCH_CONTESTABLE_ISSUES_INIT,
-  FETCH_CONTESTABLE_ISSUES_SUCCEEDED,
-  FETCH_CONTESTABLE_ISSUES_FAILED,
-} from '../../../shared/actions';
-
-describe('fetch contestable issues action', () => {
-  const mockData = { data: 'asdf' };
-  it('should dispatch an init action', () => {
-    const benefitType = 'compensation';
-    mockApiRequest(mockData);
-    const dispatch = sinon.spy();
-    return getContestableIssues({ benefitType })(dispatch).then(() => {
-      expect(dispatch.firstCall.args[0].type).to.equal(
-        FETCH_CONTESTABLE_ISSUES_INIT,
-      );
-      expect(dispatch.secondCall.args[0]).to.eql({
-        type: FETCH_CONTESTABLE_ISSUES_SUCCEEDED,
-        response: {
-          ...mockData,
-        },
-        benefitType,
-      });
-    });
-  });
-
-  it('should dispatch an add person failed action', () => {
-    const props = { benefitType: 'compensation' };
-    mockApiRequest(mockData, false);
-    const dispatch = sinon.spy();
-    return getContestableIssues(props)(dispatch).then(() => {
-      expect(dispatch.firstCall.args[0].type).to.equal(
-        FETCH_CONTESTABLE_ISSUES_INIT,
-      );
-      expect(dispatch.secondCall.args[0].type).to.equal(
-        FETCH_CONTESTABLE_ISSUES_FAILED,
-      );
-    });
-  });
-
-  it('should dispatch failed action from unsupported benefitType', () => {
-    mockApiRequest(mockData, false);
-    const dispatch = sinon.spy();
-    return getContestableIssues({ benefitType: 'foo' })(dispatch).then(() => {
-      expect(dispatch.firstCall.args[0].type).to.equal(
-        FETCH_CONTESTABLE_ISSUES_INIT,
-      );
-      expect(dispatch.secondCall.args[0].type).to.equal(
-        FETCH_CONTESTABLE_ISSUES_FAILED,
-      );
-    });
-  });
-
-  describe('test new apiRequest', () => {
-    let apiRequestSpy;
-    beforeEach(() => {
-      apiRequestSpy = sinon.stub(apiUtils, 'apiRequest').resolves(mockData);
-    });
-    afterEach(() => {
-      apiRequestSpy.restore();
-    });
-    it('should dispatch an init action', () => {
-      mockApiRequest(mockData);
-      const dispatch = sinon.spy();
-      return getContestableIssues()(dispatch).then(() => {
-        // Original API
-        expect(apiRequestSpy.args[0][0]).to.contain(CONTESTABLE_ISSUES_API);
-      });
-    });
-  });
-});
 
 describe('ITF actions', () => {
   const mockExtraProps = {
