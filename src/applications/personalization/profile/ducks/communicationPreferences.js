18262eabbccb38a346d0ad1b2e2a0c97280c92c9
246108663+jesse-b-wilcore@users.noreply.github.com <246108663+jesse-b-wilcore@users.noreply.github.com>
2026-02-24 08:05:22 -0700

    fixing casing

--- src/applications/personalization/profile/ducks/communicationPreferences.js
+++ src/applications/personalization/profile/ducks/communicationPreferences.js
@@ -67,6 +67,7 @@ const groupItemReducer = facilities => (groups, group) => {
   const newGroup = { ...group };
   const filteredItems = newGroup.communicationItems
     .filter(item => {
+      // Removes the RX tracking item if there are no facilities that support that
       if (item.id === 4) {
         return facilities.some(facility =>
           RX_TRACKING_SUPPORTING_FACILITIES.has(facility.facilityId),
