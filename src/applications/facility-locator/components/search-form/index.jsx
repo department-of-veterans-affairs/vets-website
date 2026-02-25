028f37621171ec3803cf78d65e00491dd8bfd934
Bryan Thompson <18094023+bryan-thompsoncodes@users.noreply.github.com>
2026-02-25 10:11:37 -0800

    refactor(facility-locator): Replace eslint-disable with ref pattern for sync effects
    Replace 3 eslint-disable-next-line react-hooks/exhaustive-deps with
    prev-value refs to track Redux changes, making dependency arrays
    fully declared. Reword validation comment to explain UX rationale
    instead of referencing test expectations.

--- src/applications/facility-locator/components/search-form/index.jsx
+++ src/applications/facility-locator/components/search-form/index.jsx
@@ -157,9 +157,11 @@ export const SearchForm = props => {
   useEffect(
     () => {
       if (
-        currentQuery.serviceType !== draftFormState.serviceType ||
-        currentQuery.vamcServiceDisplay !== draftFormState.vamcServiceDisplay
+        currentQuery.serviceType !== prevServiceTypeRef.current ||
+        currentQuery.vamcServiceDisplay !== prevVamcServiceDisplayRef.current
       ) {
+        prevServiceTypeRef.current = currentQuery.serviceType;
+        prevVamcServiceDisplayRef.current = currentQuery.vamcServiceDisplay;
         setDraftFormState(prev => ({
           ...prev,
           serviceType: currentQuery.serviceType || null,
@@ -167,7 +169,6 @@ export const SearchForm = props => {
         }));
       }
     },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     [currentQuery.serviceType, currentQuery.vamcServiceDisplay],
   );
 
