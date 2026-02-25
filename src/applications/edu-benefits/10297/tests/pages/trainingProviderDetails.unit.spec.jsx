ddaab9e0202952f2b1d060d0e0d9ef3d0d47c223
kevin-b-huynh <173199521+kevin-b-huynh@users.noreply.github.com>
2026-02-25 16:28:03 -0500

    fix failing test

--- src/applications/edu-benefits/10297/tests/pages/trainingProviderDetails.unit.spec.jsx
+++ src/applications/edu-benefits/10297/tests/pages/trainingProviderDetails.unit.spec.jsx
@@ -26,14 +26,17 @@ describe('Training Provider Step 3 - Page 2 Details', () => {
       <DefinitionTester
         schema={schema}
         uiSchema={uiSchema}
-        data={{ trainingProvider: [] }}
+        data={{
+          trainingProvider: [{ providerAddress: {}, providerName: undefined }],
+        }}
       />,
     );
 
     await userEvent.click(getByRole('button'));
+
     expect(container.querySelectorAll('va-text-input[error]')).to.have.lengthOf(
       4,
     );
-    // expect(container.querySelectorAll('va-select[error]')).to.have.lengthOf(1);
+    expect(container.querySelectorAll('va-select[error]')).to.have.lengthOf(1);
   });
 });
