faa3544b62eb321d3a3875cd37e714715b4d37b5
Randi Mays <19175324+randimays@users.noreply.github.com>
2026-02-26 15:07:12 -0600

    chore: add new contact information page

--- src/applications/appeals/995/pages/contactInformation.js
+++ src/applications/appeals/995/pages/contactInformation.js
@@ -1,3 +1,7 @@
+import {
+  profilePersonalInfoPage,
+  profileContactInfoPages,
+} from 'platform/forms-system/src/js/patterns/prefill';
 import profileContactInfo from 'platform/forms-system/src/js/definitions/profileContactInfo';
 import { getContent } from 'platform/forms-system/src/js/utilities/data/profile';
 import set from 'platform/utilities/data/set';
@@ -7,22 +11,24 @@ import { contactInfo995Validation } from '../../shared/validations/contactInfo';
 export const allContacts = ['mobilePhone', 'homePhone', 'address', 'email'];
 export const noAddressContacts = ['mobilePhone', 'homePhone', 'email'];
 
-export default profileContactInfo({
-  content: getContent('appeal'),
-  addressKey: 'address',
-  // Override defaults because of addressKey change
-  included: allContacts,
-  contactInfoRequiredKeys: [],
-  contactInfoUiSchema: {
-    'ui:options': {
-      updateSchema: (formData = {}, schema) =>
-        set(
-          'properties.veteran.required',
-          formData?.housingRisk ? noAddressContacts : allContacts,
-          schema,
-        ),
-    },
-    'ui:required': () => true,
-    'ui:validations': [contactInfo995Validation],
-  },
-});
+// export default profileContactInfo({
+//   content: getContent('appeal'),
+//   addressKey: 'address',
+//   // Override defaults because of addressKey change
+//   included: allContacts,
+//   contactInfoRequiredKeys: [],
+//   contactInfoUiSchema: {
+//     'ui:options': {
+//       updateSchema: (formData = {}, schema) =>
+//         set(
+//           'properties.veteran.required',
+//           formData?.housingRisk ? noAddressContacts : allContacts,
+//           schema,
+//         ),
+//     },
+//     'ui:required': () => true,
+//     'ui:validations': [contactInfo995Validation],
+//   },
+// });
+
+export default profileContactInfoPages();
