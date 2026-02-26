3446e14d03a3cccb87f42873ccf64498b93eded5
Liz Townsend <liz.townsend@adhocteam.us>
2026-02-26 15:49:35 -0600

    add site.loadPage to start new cypress test instead of setting up all new intercepts

--- src/applications/mhv-medical-records/tests/e2e/accelerated/conditions/view-condition-details-not-found.cypress.spec.js
+++ src/applications/mhv-medical-records/tests/e2e/accelerated/conditions/view-condition-details-not-found.cypress.spec.js
@@ -16,13 +16,16 @@ describe('Medical Records Condition Details - Not Found Redirect', () => {
   });
 
   it('Redirects to conditions list when condition ID is not in the list', () => {
-    // Navigate directly to a condition details page with a non-existent ID
+    // Visit the main MR landing page first
+    // (Short cut since it has all the user, session, etc. intercepts set up)
+    site.loadPage();
+    // Then navigate directly to a condition details page with a non-existent ID
     cy.visit('my-health/medical-records/conditions/non-existent-condition-id');
-    cy.wait(['@vamcEhr', '@mockUser', '@featureToggles', '@session']);
 
     // Should redirect to the conditions list page
     cy.url().should('include', '/conditions');
     cy.url().should('not.include', 'non-existent-condition-id');
+    cy.findAllByTestId('record-list-item').should('be.visible');
 
     // Accessibility check
     cy.injectAxe();
