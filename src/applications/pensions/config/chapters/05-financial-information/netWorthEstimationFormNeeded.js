68b11bfa007ee2f056c24fcc8a40f68ffc3c426e
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2026-02-27 12:07:19 -0700

    Add unit tests

--- src/applications/pensions/config/chapters/05-financial-information/netWorthEstimationFormNeeded.js
+++ src/applications/pensions/config/chapters/05-financial-information/netWorthEstimationFormNeeded.js
@@ -1,13 +1,13 @@
 import { titleUI } from 'platform/forms-system/src/js/web-component-patterns';
 import { NetWorthEstimationFormNeededDescription } from '../../../components/Descriptions';
-import { hideIfUnderThreshold } from '../../../helpers';
+import { netWorthEstimateIsOverThreshold } from '../../../helpers';
 
 export default {
   title: 'Additional form needed',
   path: 'financial/net-worth-estimation/additional-form-needed',
   initialData: {},
   depends: formData =>
-    formData.totalNetWorth || !hideIfUnderThreshold(formData),
+    formData.totalNetWorth || netWorthEstimateIsOverThreshold(formData),
   uiSchema: {
     ...titleUI(
       'Additional form needed',
