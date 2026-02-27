1b26fced5b82157100839fbe6eb1c7822a0a6e37
rmessina1010 <rmessina10@gmail.com>
2026-02-27 12:17:34 -0600

    updated copy to latest revision

--- src/applications/ezr/components/FormDescriptions/SupportingDocumentsDescription.jsx
+++ src/applications/ezr/components/FormDescriptions/SupportingDocumentsDescription.jsx
@@ -4,44 +4,54 @@ import TipsForUploading from './TipsForUploading';
 const ToxicExposureDescription = (
   <>
     <p>
-      If you’re updating your service period or providing information about
-      toxic exposure or hazards, you can upload supporting documents here.
+      We recommend uploading any supporting documents that may help us confirm
+      details about your military service.
     </p>
-    <h2 className="vads-u-font-size--h5">
-      You may upload one or more of the following:
-    </h2>
-    <h3 className="vads-u-font-size--h5">Service and discharge documents</h3>
+    <p>
+      It’s your choice whether to upload them. But they may help us process your
+      form faster.
+    </p>
+    <h3 className="vads-u-font-size--h5">Military service documents</h3>
+    <p>You can upload copies of any of these documents:</p>
     <ul>
+      <li>Your DD214, DD215, or other separation documents</li>
       <li>
-        Military discharge paper (such as DD214, DD256, DD 257, NGB22 or other
-        separation documents
+        Military service records or documents, like proof of military awards or
+        your disability rating letter
       </li>
-      <li>Military orders or service records</li>
-    </ul>
-    <h3 className="vads-u-font-size--h5">
-      Toxic exposure or hazard-related documents
-    </h3>
-    <ul>
-      <li>
-        What you were exposed to, where you were exposed, and when (month and
-        year)
-      </li>
-      <li>
-        Type of activity you were in engaged in during exposure, such as basic
-        training
-      </li>
-      <li>
-        Written statements, such as a personal statement or a buddy statement
-      </li>
-      <li>Unit histories</li>
-      <li>News articles</li>
-      <li>Additional evidence, like photos, a personal journal, diary</li>
+      <li>Military orders or unit histories</li>
     </ul>
     <p>
-      Uploading these documents may help speed up your form processing time.
+      <b>Note:</b>
+      If you have more than 1 discharge document, upload the document with the
+      highest character of discharge.
     </p>
+    <h3 className="vads-u-font-size--h5">
+      If you updated information about your exposure to any toxins or other
+      hazards
+    </h3>
+    <p>Here’s the type of information your document can include:</p>
+    <ul>
+      <li>The toxins or other hazards you may have been exposed to</li>
+      <li>When and where you may have been exposed</li>
+      <li>
+        The type of activity you were engaged in when you were exposed, like
+        basic training
+      </li>
+    </ul>
+    <p>
+      These are the types of documents you can submit that may have this
+      information:
+    </p>
+    <ul>
+      <li>The military service documents listed in the previous section</li>
+      <li>
+        A written statement, like a personal statement or a buddy statement
+      </li>
+      <li>Photos, journal entries, or news articles</li>
+    </ul>
     <TipsForUploading />
-    <p>Upload a document (one at a time)</p>
+    <p>Select a file to upload</p>
   </>
 );
 
