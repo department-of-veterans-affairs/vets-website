e153e349d8ac64fcecabd8cac086fc95eed115de
Ryan Shaw <587812+ryanshaw@users.noreply.github.com>
2026-03-01 09:27:49 -0800

    Updates hook
    Signed-off-by: Ryan Shaw <587812+ryanshaw@users.noreply.github.com>

--- src/applications/vaos/new-appointment/hooks/useOHScheduling.js
+++ src/applications/vaos/new-appointment/hooks/useOHScheduling.js
@@ -7,5 +7,22 @@ export function useOHScheduling() {
   const featureUseVpg = useSelector(selectFeatureUseVpg);
   const data = useSelector(getFormData);
   const typeOfCare = getTypeOfCare(data);
-  return featureUseVpg && OH_ENABLED_TYPES_OF_CARE.includes(typeOfCare?.idV2);
+
+  // Enable all types of care in all environments other than production, in
+  // production utilize the constants that are defined in src/applications/vaos/utils/constants.js
+  const enableAllTypesOfCare =
+    process.env.NODE_ENV !== 'production' || process.env.NODE_ENV !== 'test';
+
+  if (!featureUseVpg) {
+    // OH only works when VPG is used
+    return false;
+  }
+
+  if (enableAllTypesOfCare) {
+    // env is not production or test
+    return true;
+  }
+
+  // Otherwise check constant for enabled types of care
+  return OH_ENABLED_TYPES_OF_CARE.includes(typeOfCare?.idV2);
 }
