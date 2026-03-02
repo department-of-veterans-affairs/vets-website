32cc4e6fd035ec61848b6b6b58571e299dcc2283
thomaskrenzke <196683837+thomaskrenzke@users.noreply.github.com>
2026-03-02 13:22:22 -0800

    more 0803 spec coverage

--- src/applications/edu-benefits/0803/tests/config/form.unit.spec.jsx
+++ src/applications/edu-benefits/0803/tests/config/form.unit.spec.jsx
@@ -8,4 +8,34 @@ describe('22-0839 Form Config', () => {
     expect(formConfig.rootUrl).to.contain(manifest.rootUrl);
     expect(formConfig).to.have.property('chapters');
   });
+
+  it('has the right depends for benefits information', () => {
+    const {
+      chapters: {
+        benefitsInformationChapter: { pages },
+      },
+    } = formConfig;
+
+    const { selectVABenefit, vaBenefitWarning } = pages;
+    const data = {
+      hasPreviouslyApplied: true,
+    };
+    expect(selectVABenefit.depends(data)).to.eq(true);
+    expect(vaBenefitWarning.depends(data)).to.eq(false);
+  });
+
+  it('has the right depends for personal information', () => {
+    const {
+      chapters: {
+        personalInformationChapter: { pages },
+      },
+    } = formConfig;
+
+    const { payeeNumber } = pages;
+    const data = {
+      vaBenefitProgram: 'chapter35',
+      vaFileNumber: '123456789',
+    };
+    expect(payeeNumber.depends(data)).to.eq(true);
+  });
 });
