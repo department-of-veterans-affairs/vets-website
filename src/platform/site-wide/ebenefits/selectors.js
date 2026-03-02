b01f66c11e9d9cc7ade822c3cdc668d0cbf23aff
Alexander Garcia <67602137+asg5704@users.noreply.github.com>
2026-03-02 11:04:18 -0500

    removes site-wide/ebenefits

--- src/platform/site-wide/ebenefits/selectors.js
+++
@@ -1,4 +0,0 @@
-import { isAuthenticatedWithSSOe } from 'platform/user/authentication/selectors';
-
-// User must be authenticated with SSOe for the proxied/eauth function to work
-export const shouldUseProxyUrl = state => isAuthenticatedWithSSOe(state);
