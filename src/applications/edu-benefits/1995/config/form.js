f4e6aa9980e767731b333f5ef040b5c7c35ed467
kevin-b-huynh <173199521+kevin-b-huynh@users.noreply.github.com>
2026-03-02 17:09:01 -0500

    make confirmation form config dummy component

--- src/applications/edu-benefits/1995/config/form.js
+++ src/applications/edu-benefits/1995/config/form.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import FormFooter from 'platform/forms/components/FormFooter';
 import { VA_FORM_IDS } from 'platform/forms/constants';
 import { urlMigration } from '../../config/migrations';
@@ -17,6 +19,7 @@ const baseConfig = {
   disableSave: true,
   version: 1,
   migrations: [urlMigration('/1995')],
+  confirmation: React.Component, // needs to be a React component or else src/platform/forms/tests/forms-config-validator.unit.spec.jsx fails
   title: 'Change your education benefits',
   footerContent: FormFooter,
   getHelp: GetFormHelp1995,
