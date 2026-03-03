e44088b29e693f7c57a7251776b4b940dac606e5
Michael Ghisilieri <175352110+mghisilieri@users.noreply.github.com>
2026-03-03 08:34:18 -0800

    22-10272 form tester utility

--- src/applications/edu-benefits/10272/tests/e2e/10272-edu-benefits.cypress.spec.js
+++ src/applications/edu-benefits/10272/tests/e2e/10272-edu-benefits.cypress.spec.js
@@ -4,6 +4,8 @@ import { createTestConfig } from 'platform/testing/e2e/cypress/support/form-test
 import featureToggles from '../fixtures/mocks/feature-toggles.json';
 import user from '../fixtures/mocks/user.json';
 import mockSubmit from '../fixtures/mocks/application-submit.json';
+import prefilledForm from '../fixtures/mocks/prefilled-form.json';
+import sip from '../fixtures/mocks/sip-put.json';
 import formConfig from '../../config/form';
 import manifest from '../../manifest.json';
 
@@ -15,29 +17,58 @@ const testConfig = createTestConfig(
     pageHooks: {
       introduction: ({ afterHook }) => {
         afterHook(() => {
-          cy.findAllByText(/^start/i, { selector: 'a[href="#start"]' })
-            .last()
-            .click({ force: true });
+          cy.get('va-link-action[text="Start your request for reimbursement"')
+            .first()
+            .click();
         });
       },
-      // Example page hook
-      // All paths are already automatically filled out based on fixtures.
-      // But if you want to manually test a page add the path.
-      // 'name-and-date-of-birth': ({ afterHook }) => {
-      //   cy.injectAxeThenAxeCheck();
-      //   afterHook(() => {
-      //     cy.get('@testData').then(() => {
-      //       cy.fillPage(); // fills all fields based on fixtures.
-      //       cy.axeCheck();
-      //       cy.findByText(/continue/i, { selector: 'button' }).click();
-      //     });
-      //   });
-      // },
+      'education-benefits-history': ({ afterHook }) => {
+        afterHook(() => {
+          cy.waitForTextarea();
+          cy.fillPage();
+          cy.tabToSubmitForm();
+        });
+      },
+      remarks: ({ afterHook }) => {
+        afterHook(() => {
+          cy.get('@testData').then(({ remarks }) => {
+            if (remarks) {
+              cy.waitForTextarea();
+              cy.fillPage();
+            }
+          });
+
+          cy.tabToSubmitForm();
+        });
+      },
+      'review-and-submit': ({ afterHook }) => {
+        afterHook(() => {
+          cy.get('#inputField').type('Rita Ann Jackson', { force: true });
+          cy.get('#checkbox-element').check({ force: true });
+          cy.findByText(/Continue/i, { selector: 'button' }).click();
+        });
+      },
     },
     setupPerTest: () => {
-      cy.intercept('GET', '/v0/user', user);
+      Cypress.Commands.add('waitForTextarea', () => {
+        cy.get('#input-type-textarea')
+          .should('be.visible')
+          .and('not.be.disabled')
+          .clear({
+            delay: 250,
+            waitForAnimations: false,
+          });
+      });
+
+      // Default endpoints to intercept
       cy.intercept('GET', '/v0/feature_toggles?*', featureToggles);
+      cy.intercept('GET', '/data/cms/vamc-ehr.json', {});
       cy.intercept('POST', formConfig.submitUrl, mockSubmit);
+      cy.intercept('PUT', '/v0/in_progress_forms/22-10272', sip);
+
+      // Authenticated endpoints required for login and prefill
+      cy.intercept('GET', '/v0/user', user);
+      cy.intercept('GET', '/v0/in_progress_forms/22-10272', prefilledForm);
       cy.login(user);
     },
     skip: Cypress.env('CI'), // Skip CI initially until content-build is merged
