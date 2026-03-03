38412094767ecb91245a25ac084aa17299a23bb0
John Woolschlager <wullaski@gmail.com>
2026-03-03 10:44:59 -0500

    Fix for feature flags

--- src/applications/survivors-benefits/tests/fixtures/mocks/feature-flags.js
+++ src/applications/survivors-benefits/tests/fixtures/mocks/feature-flags.js
@@ -1,6 +1,6 @@
 const APPLICATION_FEATURE_FLAGS = Object.freeze({
   survivorsBenefitsFormEnabled: true,
-  survivorsBenefitsForm2025VersionEnabled: false,
+  survivorsBenefitsForm2025VersionEnabled: true,
 });
 
 const generateFeatureFlags = ({ toggles = APPLICATION_FEATURE_FLAGS } = {}) => {
