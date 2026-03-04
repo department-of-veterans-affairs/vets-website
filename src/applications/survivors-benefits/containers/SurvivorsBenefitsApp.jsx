3a02d14877750418ff2079b3b22ca8a252fcc72e
Daniel Gading <2748839+dgading@users.noreply.github.com>
2026-03-04 11:17:18 -0500

    remove feature flag setup

--- src/applications/survivors-benefits/containers/SurvivorsBenefitsApp.jsx
+++ src/applications/survivors-benefits/containers/SurvivorsBenefitsApp.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useFeatureToggle } from 'platform/utilities/feature-toggles';
 import IntentToFile from 'platform/shared/itf/IntentToFile';
 import RoutedSavableApp from 'platform/forms/save-in-progress/RoutedSavableApp';
 import formConfig from '../config/form';
 
 export default function SurvivorsBenefitsApp({ location, children }) {
-  const TOGGLE_KEY = 'survivorsBenefitsForm2025VersionEnabled';
-  const { useFormFeatureToggleSync } = useFeatureToggle();
-  useFormFeatureToggleSync([
-    {
-      toggleName: TOGGLE_KEY,
-      formKey: 'form2025Version',
-    },
-  ]);
-
   return (
     <RoutedSavableApp formConfig={formConfig} currentLocation={location}>
       <IntentToFile itfType="pension" location={location} disableAutoFocus />
