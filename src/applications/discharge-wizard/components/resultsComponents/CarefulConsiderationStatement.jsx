82121e1b1dbec82f3002622babd693d1e9c33950
Chris Kim <42885441+chriskim2311@users.noreply.github.com>
2026-03-05 10:11:45 -0800

    updates

--- src/applications/discharge-wizard/components/resultsComponents/CarefulConsiderationStatement.jsx
+++ src/applications/discharge-wizard/components/resultsComponents/CarefulConsiderationStatement.jsx
@@ -38,44 +38,50 @@ const CarefulConsiderationStatement = ({ formResponses, router }) => {
             <p>
               You answered that your discharge is due to your sexual
               orientation. In 2011, DOD recognized that it discharged many
-              Veterans only because of their sexual orientation. If you apply
-              for a discharge upgrade, DOD will apply "liberal consideration" to
-              your case.
+              Veterans only because of their sexual orientation.
             </p>
           );
         }
         if (dischargeType === RESPONSES.DISCHARGE_HONORABLE) {
           return (
-            <p>
-              You answered that your discharge is due to your sexual
-              orientation. In 2011, DOD recognized that many Veterans received
-              discharges only because of their sexual orientation. <br />{' '}
-              <strong>Note:</strong> You’ll need to show that your discharge was
-              solely due to your sexual orientation and related events. If the
-              events weren’t related, you may still receive an upgrade. But
-              you’ll have to prove your discharge was unjust punishment for
-              those events.
-            </p>
+            <>
+              <p>
+                You answered that your discharge is due to your sexual
+                orientation. In 2011, DOD recognized that many Veterans received
+                discharges only because of their sexual orientation.
+              </p>
+              <p>
+                <strong>Note:</strong> You’ll need to show that your discharge
+                was solely due to your sexual orientation and related events. If
+                the events weren’t related, you may still receive an upgrade.
+                But you’ll have to prove your discharge was unjust punishment
+                for those events.
+              </p>
+            </>
           );
         }
         return null;
       case RESPONSES.REASON_SEXUAL_ASSAULT:
         return (
-          <p>
-            You answered that your discharge is related to sexual assault or
-            harassment. In 2017, the Defense Department (DOD) recognized that it
-            discharged many Veterans due to sexual assault or harassment. And
-            that many Veterans unfairly received discharges that weren’t
-            honorable. If you apply for a discharge upgrade, DOD will apply
-            "liberal consideration" to your case. That means you’ll be able to
-            explain any extenuating circumstances around your discharge.{' '}
-            <strong>Note:</strong> You’ll need to show that your discharge was
-            solely the result of sexual assault or harassment and events
-            specifically related to it. If the events leading to your discharge
-            weren’t related to sexual assault or harassment, you may still
-            receive an upgrade. But you’ll have to prove your discharge was
-            unjust punishment for those events.
-          </p>
+          <>
+            <p>
+              You answered that your discharge is related to sexual assault or
+              harassment. In 2017, the Defense Department (DOD) recognized that
+              it discharged many Veterans due to sexual assault or harassment.
+              And that many Veterans unfairly received discharges that weren’t
+              honorable. If you apply for a discharge upgrade, DOD will apply
+              "liberal consideration" to your case. That means you’ll be able to
+              explain any extenuating circumstances around your discharge.
+            </p>
+            <p>
+              <strong>Note:</strong> You’ll need to show that your discharge was
+              solely the result of sexual assault or harassment and events
+              specifically related to it. If the events leading to your
+              discharge weren’t related to sexual assault or harassment, you may
+              still receive an upgrade. But you’ll have to prove your discharge
+              was unjust punishment for those events.
+            </p>
+          </>
         );
       case RESPONSES.REASON_TRANSGENDER:
         return (
