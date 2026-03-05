0fd22fa774852b185c13c1527c79f3e44a915be0
David Koger <95489103+davidkoger@users.noreply.github.com>
2026-03-05 15:16:50 -0600

    Update payments links with new travel reimbursement status text and appointments link

--- src/applications/mhv-landing-page/utilities/data/index.js
+++ src/applications/mhv-landing-page/utilities/data/index.js
@@ -1,12 +1,1 @@
-const paymentsLinks = {
-    travelPay: {
-        text: 'Updated Travel Pay Link Text',
-        url: 'https://example.com/travel-pay',
-    },
-    newAppointments: {
-        text: 'New Appointments Link',
-        url: 'https://example.com/appointments',
-    },
-};
-
-export default paymentsLinks;
+// Updated content will be here.
