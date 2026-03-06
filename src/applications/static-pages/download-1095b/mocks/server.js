40520fe0e1999a72ffaf49acbdc8c0e1e0e4345a
jasonekstrom <jasonekstromdev@gmail.com>
2026-03-06 10:38:55 -0800

    update mock server

--- src/applications/static-pages/download-1095b/mocks/server.js
+++ src/applications/static-pages/download-1095b/mocks/server.js
@@ -4,12 +4,43 @@ const { delaySingleResponse } = require('./script/utils');
 // const error500 = require('../tests/fixtures/500.json');
 
 const responses = {
+  'GET /v0/feature_toggles': (req, res) => {
+    const queryValue = req?.query?.form1095bMultipleYears;
+    const envValue = process?.env?.FORM_1095B_MULTIPLE_YEARS;
+    let isForm1095bMultipleYears = envValue === 'true';
+
+    if (queryValue === 'true') {
+      isForm1095bMultipleYears = true;
+    } else if (queryValue === 'false') {
+      isForm1095bMultipleYears = false;
+    }
+
+    return res.json({
+      data: {
+        type: 'feature_toggles',
+        features: [
+          {
+            name: 'form1095b_multiple_years',
+            value: isForm1095bMultipleYears,
+          },
+        ],
+      },
+    });
+  },
+  'GET /v0/mock_session': (_req, res) => {
+    return res.json({ hasSession: true });
+  },
+  'GET /data/cms/vamc-ehr.json': (_req, res) => {
+    return res.json({});
+  },
   'GET /v0/form1095_bs/available_forms': (_req, res) => {
     const secondsOfDelay = 1;
     delaySingleResponse(() => {
       res.json({
         availableForms: [
-          { year: 2024, lastUpdated: '2025-02-03T18:50:40.548Z' },
+          { year: 2025, lastUpdated: '2025-02-03T18:50:40.548Z' },
+          { year: 2024, lastUpdated: '2024-02-03T18:50:40.548Z' },
+          { year: 2023, lastUpdated: '2023-02-03T18:50:40.548Z' },
         ],
       });
     }, secondsOfDelay);
