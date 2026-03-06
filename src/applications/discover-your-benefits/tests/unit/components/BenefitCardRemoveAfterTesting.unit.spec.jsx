c52ca71c13b46f2ca0b03de2df69ed2f3fe4e118
macalemid <mahmed@ironarchtech.com>
2026-03-06 14:31:07 -0500

    Added more code coverage

--- src/applications/discover-your-benefits/tests/unit/components/BenefitCardRemoveAfterTesting.unit.spec.jsx
+++ src/applications/discover-your-benefits/tests/unit/components/BenefitCardRemoveAfterTesting.unit.spec.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { expect } from 'chai';
+import sinon from 'sinon';
+import * as recordEventModule from 'platform/monitoring/record-event';
 import { BENEFITS_LIST } from '../../../constants/benefitsRemoveAfterTesting';
 
 import BenefitCard from '../../../components/BenefitCardRemoveAfterTesting';
@@ -24,5 +26,35 @@ describe('<BenefitCard>', () => {
     expect(container.querySelector('p')).to.have.text(benefit.description);
     expect(container.querySelector('div.blue-heading')).to.exist;
     expect(container.querySelectorAll('va-link')).to.have.lengthOf(1);
+  });
+});
+
+describe('Analytics tests', () => {
+  let recordEventStub;
+  beforeEach(function() {
+    recordEventStub = sinon.stub(recordEventModule, 'default');
+  });
+
+  afterEach(function() {
+    recordEventStub.restore();
+  });
+
+  it('calls recordEvent when learn more link is clicked', () => {
+    const benefit = BENEFITS_LIST[0];
+    const { container } = render(
+      <BenefitCard benefit={benefit} isBenefitRecommended={() => false} />,
+    );
+
+    const link = container.querySelector('va-link');
+
+    fireEvent.click(link);
+
+    expect(recordEventStub.calledOnce).to.be.true;
+    expect(recordEventStub.firstCall.args[0]).to.deep.equal({
+      event: 'nav-link-click',
+      'link-label': 'Learn more',
+      'link-destination': benefit.learnMoreURL,
+      'link-origin': `Learn more about ${benefit.name}`,
+    });
   });
 });
