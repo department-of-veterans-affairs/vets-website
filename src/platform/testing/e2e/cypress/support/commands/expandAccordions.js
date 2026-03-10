fed49e1ded63f885091d624168bd97dce12080c8
Matt Long <6738544+longmd@users.noreply.github.com>
2026-03-09 22:44:07 -0400

    Update accodion expander logic

--- src/platform/testing/e2e/cypress/support/commands/expandAccordions.js
+++ src/platform/testing/e2e/cypress/support/commands/expandAccordions.js
@@ -1,3 +1,12 @@
+const expandCollapsedElement = (el, selector) => {
+  cy.wrap(el)
+    .shadow()
+    .then($shadow => {
+      const $collapsed = $shadow.find(selector);
+      if ($collapsed.length) cy.wrap($collapsed).click({ force: true });
+    });
+};
+
 const waitForAccordionHydration = () => {
   cy.get('va-accordion-item', { timeout: 5000 })
     .should('have.length.at.least', 1)
@@ -19,27 +28,23 @@ Cypress.Commands.add('expandAccordions', () => {
   cy.get('main').then($main => {
     if ($main.find('va-accordion-item').length > 0) {
       waitForAccordionHydration();
-
-      cy.get('va-accordion-item')
-        .shadow()
-        .find('button[aria-expanded=false]')
-        .each($button => {
-          cy.wrap($button).click({ force: true });
-        });
+      cy.get('va-accordion-item').each($item => {
+        expandCollapsedElement($item, 'button[aria-expanded="false"]');
+      });
     }
 
     if ($main.find('va-additional-info').length > 0) {
-      cy.get('va-additional-info')
-        .shadow()
-        .find('a[role="button"][aria-expanded=false]')
-        .each($button => {
-          cy.wrap($button).click({ force: true });
-        });
+      cy.get('va-additional-info').each($info => {
+        expandCollapsedElement(
+          $info,
+          'a[role="button"][aria-expanded="false"]',
+        );
+      });
     }
 
-    if ($main.find('button[aria-expanded=false]').length > 0) {
+    if ($main.find('button[aria-expanded="false"]').length > 0) {
       cy.get('main')
-        .find('button[aria-expanded=false]')
+        .find('button[aria-expanded="false"]')
         .each($button => {
           cy.wrap($button).click({ force: true });
         });
