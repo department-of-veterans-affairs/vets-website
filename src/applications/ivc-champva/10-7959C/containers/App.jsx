8236607e68306adcd9a98534425689450591a5cb
Matt Long <6738544+longmd@users.noreply.github.com>
2026-03-11 12:21:45 -0400

    CHAMPVA | Update app settings for Datadog RUM

--- src/applications/ivc-champva/10-7959C/containers/App.jsx
+++ src/applications/ivc-champva/10-7959C/containers/App.jsx
@@ -8,32 +8,45 @@ import { useBrowserMonitoring } from 'platform/monitoring/Datadog';
 import { useDefaultFormData } from '../hooks/useDefaultFormData';
 import formConfig from '../config/form';
 
+const EXCLUDED_DOMAINS = [
+  'resource.digital.voice.va.gov',
+  'browser-intake-ddog-gov.com',
+  'google-analytics.com',
+  'eauth.va.gov',
+  'api.va.gov',
+];
+
+const BROWSER_MONITORING_PROPS = {
+  toggleName: 'form107959cBrowserMonitoringEnabled',
+  applicationId: '3e211ba8-dbcd-4a8d-b3eb-18950d5a46bc',
+  clientToken: 'pub383f4e654ef2030cd6045c8532593afc',
+  service: 'ivc-ohi-10-7959c',
+  version: '1.0.0',
+  env: environment.vspEnvironment(),
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 100,
+  trackUserInteractions: true,
+  trackFrustrations: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: 'mask-user-input',
+  beforeSend: ({ action, type, resource }) => {
+    // eslint-disable-next-line no-param-reassign
+    if (action?.type === 'click') action.target.name = 'Form item';
+    return !(
+      type === 'resource' &&
+      EXCLUDED_DOMAINS.some(domain => resource.url.includes(domain))
+    );
+  },
+};
+
 const App = ({ location, children }) => {
   const isAppLoading = useSelector(
     state => state.featureToggles?.loading || state.user?.profile?.loading,
   );
 
   useDefaultFormData();
-  useBrowserMonitoring({
-    loggedIn: undefined,
-    toggleName: 'form107959cBrowserMonitoringEnabled',
-    applicationId: '3e211ba8-dbcd-4a8d-b3eb-18950d5a46bc',
-    clientToken: 'pub383f4e654ef2030cd6045c8532593afc',
-    service: 'ivc-ohi-10-7959c',
-    version: '1.0.0',
-    // record 100% of staging sessions, but only 20% of production
-    sessionReplaySampleRate:
-      environment.vspEnvironment() === 'staging' ? 100 : 20,
-    sessionSampleRate: 50,
-    beforeSend: event => {
-      // Prevent PII from being sent to Datadog with click actions.
-      if (event.action?.type === 'click') {
-        // eslint-disable-next-line no-param-reassign
-        event.action.target.name = 'Clicked item';
-      }
-      return true;
-    },
-  });
+  useBrowserMonitoring(BROWSER_MONITORING_PROPS);
 
   return isAppLoading ? (
     <va-loading-indicator
