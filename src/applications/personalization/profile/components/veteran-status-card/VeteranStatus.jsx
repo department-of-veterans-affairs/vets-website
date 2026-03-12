0e7752924c26956d6dded2a7f733605af25f7f89
246108663+jesse-b-wilcore@users.noreply.github.com <246108663+jesse-b-wilcore@users.noreply.github.com>
2026-03-12 10:43:44 -0600

    fix feature flag logic

--- src/applications/personalization/profile/components/veteran-status-card/VeteranStatus.jsx
+++ src/applications/personalization/profile/components/veteran-status-card/VeteranStatus.jsx
@@ -178,7 +178,7 @@ const VeteranStatus = ({
   const createPdf = async () => {
     try {
       await generatePdf(
-        'veteranStatusNew',
+        'veteranStatus',
         'Veteran status card',
         pdfData,
         !isMobile,
