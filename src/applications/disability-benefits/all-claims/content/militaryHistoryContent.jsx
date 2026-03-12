63318f73451b10dc0705ef2afb0e0b60afe7c3dc
= <=>
2026-03-12 15:16:50 -0500

    Fix lint formatting in military history notice

--- src/applications/disability-benefits/all-claims/content/militaryHistoryContent.jsx
+++ src/applications/disability-benefits/all-claims/content/militaryHistoryContent.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 export const serviceNoticeContent = (
   <div>
     <p>
-      <strong>Note:</strong> Your active service end date is also known as your separation date. This
-      can be found in the record of service section of your DD214. If your
-      service is or was in the National Guard or Reserve, your active service
-      end date could be different from when your contract or service obligation
-      ends.
+      <strong>Note:</strong> Your active service end date is also known as your
+      separation date. This can be found in the record of service section of
+      your DD214. If your service is or was in the National Guard or Reserve,
+      your active service end date could be different from when your contract or
+      service obligation ends.
     </p>
   </div>
 );
